a71c1ab50a2a0f4dd9834bf5a917a2f064535c6b
Stephen Rothwell <sfr@canb.auug.org.au>
2005-05-01 08:59:08 -0700

    [PATCH] consolidate SIGEV_PAD_SIZE
    Discussing with Matthew Wilcox some of his outstanding patches lead me to
    this patch (among others).
    
    The preamble in struct sigevent can be expressed independently of the
    architecture.
    
    Also use __ARCH_SI_PREAMBLE_SIZE on ia64.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-ia64/siginfo.h
+++ include/asm-ia64/siginfo.h
@@ -8,9 +8,7 @@
  *	David Mosberger-Tang <davidm@hpl.hp.com>, Hewlett-Packard Co
  */
 
-#define SI_PAD_SIZE	((SI_MAX_SIZE/sizeof(int)) - 4)
-
-#define SIGEV_PAD_SIZE	((SIGEV_MAX_SIZE/sizeof(int)) - 4)
+#define __ARCH_SI_PREAMBLE_SIZE	(4 * sizeof(int))
 
 #define HAVE_ARCH_SIGINFO_T
 #define HAVE_ARCH_COPY_SIGINFO
