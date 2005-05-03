5c3073e691b56dabbdec60dda4258b4e50d64872
Russell King <rmk@dyn-67.arm.linux.org.uk>
2005-05-03 12:20:29 +0100

    [PATCH] ARM: cleanup vmalloc start/offset macros
    VMALLOC_START and VMALLOC_OFFSET are common between all ARM
    machine classes.  Move them into include/asm-arm/pgtable.h,
    but allow a machine class to override them if required.
    
    Signed-off-by: Russell King <rmk@arm.linux.org.uk>

--- include/asm-arm/arch-s3c2410/vmalloc.h
+++ include/asm-arm/arch-s3c2410/vmalloc.h
@@ -19,18 +19,6 @@
 #ifndef __ASM_ARCH_VMALLOC_H
 #define __ASM_ARCH_VMALLOC_H
 
-/*
- * Just any arbitrary offset to the start of the vmalloc VM area: the
- * current 8MB value just means that there will be a 8MB "hole" after the
- * physical memory until the kernel virtual memory starts.  That means that
- * any out-of-bounds memory accesses will hopefully be caught.
- * The vmalloc() routines leaves a hole of 4kB between each vmalloced
- * area for the same reason. ;)
- */
-
-#define VMALLOC_OFFSET	  (8*1024*1024)
-#define VMALLOC_START	  (((unsigned long)high_memory + VMALLOC_OFFSET) & ~(VMALLOC_OFFSET-1))
-#define VMALLOC_VMADDR(x) ((unsigned long)(x))
 #define VMALLOC_END	  (0xE0000000)
 
 #endif /* __ASM_ARCH_VMALLOC_H */
