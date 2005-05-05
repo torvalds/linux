5fcbab355e1528545671a5221666ef640b8250c8
Dean Roehrich <roehrich@sgi.com>
2005-05-05 13:27:19 -0700

    [XFS] Add ATTR_NOLOCK for xfs_setattr to indicate that XFS_IOLOCK is held
    SGI Modid: xfs-linux:xfs-kern:190711a
    
    Signed-off-by: Dean Roehrich <roehrich@sgi.com>
    Signed-off-by: Christoph Hellwig <hch@sgi.com>
    .

--- fs/xfs/linux-2.6/xfs_vnode.h
+++ fs/xfs/linux-2.6/xfs_vnode.h
@@ -626,6 +626,7 @@ static inline int VN_BAD(struct vnode *vp)
 #define	ATTR_DMI	0x08	/* invocation from a DMI function */
 #define	ATTR_LAZY	0x80	/* set/get attributes lazily */
 #define	ATTR_NONBLOCK	0x100	/* return EAGAIN if operation would block */
+#define ATTR_NOLOCK	0x200	/* Don't grab any conflicting locks */
 
 /*
  * Flags to VOP_FSYNC and VOP_RECLAIM.
