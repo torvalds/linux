637716a3825e186555361574aa1fa3c0ebf8018b
Andi Kleen <ak@suse.de>
2005-05-16 21:53:20 -0700

    [PATCH] x86_64: Add a guard page at the end of the 47bit address space
    This works around a bug in the AMD K8 CPUs.
    
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-x86_64/processor.h
+++ include/asm-x86_64/processor.h
@@ -159,9 +159,9 @@ static inline void clear_in_cr4 (unsigned long mask)
 
 
 /*
- * User space process size. 47bits.
+ * User space process size. 47bits minus one guard page.
  */
-#define TASK_SIZE	(0x800000000000UL)
+#define TASK_SIZE	(0x800000000000UL - 4096)
 
 /* This decides where the kernel will search for a free chunk of vm
  * space during mmap's.
