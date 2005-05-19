fdb551a4c59945f868b8855e5baf161c9f1c0518
Ralf Baechle <ralf@linux-mips.org>
2005-05-19 12:08:04 +0000

    Bugs are now a configuration option.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/bug.h
+++ include/asm-mips/bug.h
@@ -1,6 +1,10 @@
 #ifndef __ASM_BUG_H
 #define __ASM_BUG_H
 
+#include <linux/config.h>
+
+#ifdef CONFIG_BUG
+
 #include <asm/break.h>
 
 #ifdef CONFIG_BUG
@@ -14,3 +18,5 @@ do {									\
 #include <asm-generic/bug.h>
 
 #endif
+
+#endif /* __ASM_BUG_H */
