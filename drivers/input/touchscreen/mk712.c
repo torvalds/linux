de1b963a416232bf429550ee475d6b9a34b66309
Dmitry Torokhov <dtor_core@ameritech.net>
2005-05-29 02:28:50 -0500

    Input: whitespace fixes in drivers/input/touchscreen
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/touchscreen/mk712.c
+++ drivers/input/touchscreen/mk712.c
@@ -17,7 +17,7 @@
  * found in Gateway AOL Connected Touchpad computers.
  *
  * Documentation for ICS MK712 can be found at:
- * 	http://www.icst.com/pdf/mk712.pdf
+ *	http://www.icst.com/pdf/mk712.pdf
  */
 
 /*
