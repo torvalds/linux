71387bd77f662a83b18ff8de676e9d9531c58894
Vojtech Pavlik <vojtech@suse.cz>
2005-05-29 02:28:14 -0500

    Input: Fix a warning in hid-core.
    Signed-off-by: Vojtech Pavlik <vojtech@suse.cz>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/usb/input/hid-core.c
+++ drivers/usb/input/hid-core.c
@@ -1235,7 +1235,7 @@ int hid_wait_io(struct hid_device *hid)
 
 static int hid_set_idle(struct usb_device *dev, int ifnum, int report, int idle)
 {
-	usb_control_msg(dev, usb_sndctrlpipe(dev, 0),
+	return usb_control_msg(dev, usb_sndctrlpipe(dev, 0),
 		HID_REQ_SET_IDLE, USB_TYPE_CLASS | USB_RECIP_INTERFACE, (idle << 8) | report,
 		ifnum, NULL, 0, USB_CTRL_SET_TIMEOUT);
 }
