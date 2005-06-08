1ccc67d692c52dcc02e70206338ff36ac145a939
Takashi Iwai <tiwai@suse.de>
2005-06-08 14:49:31 +0200

    [ALSA] Disable MPU401 on SIS7018
    Trident driver
    Disable MPU401 support on SIS7018 since it results in kernel freeze.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/trident/trident.c
+++ sound/pci/trident/trident.c
@@ -143,7 +143,8 @@ static int __devinit snd_trident_probe(struct pci_dev *pci,
 			return err;
 		}
 	}
-	if ((err = snd_mpu401_uart_new(card, 0, MPU401_HW_TRID4DWAVE,
+	if (trident->device != TRIDENT_DEVICE_ID_SI7018 &&
+	    (err = snd_mpu401_uart_new(card, 0, MPU401_HW_TRID4DWAVE,
 				       trident->midi_port, 1,
 				       trident->irq, 0, &trident->rmidi)) < 0) {
 		snd_card_free(card);
