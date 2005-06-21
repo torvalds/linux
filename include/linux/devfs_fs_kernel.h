d8deac5094988c7ad1127ee61f52c59a952fcabb
Greg Kroah-Hartman <gregkh@suse.de>
2005-06-20 21:15:16 -0700

    [PATCH] devfs: Remove devfs from the kernel tree
    This is the first patch in a series of patches that removes devfs
    support from the kernel.  This patch removes the core devfs code, and
    its private header file.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/devfs_fs_kernel.h
+++ include/linux/devfs_fs_kernel.h
@@ -4,25 +4,8 @@
 #include <linux/fs.h>
 #include <linux/spinlock.h>
 #include <linux/types.h>
-
 #include <asm/semaphore.h>
 
-#define DEVFS_SUPER_MAGIC                0x1373
-
-#ifdef CONFIG_DEVFS_FS
-extern int devfs_mk_bdev(dev_t dev, umode_t mode, const char *fmt, ...)
-    __attribute__ ((format(printf, 3, 4)));
-extern int devfs_mk_cdev(dev_t dev, umode_t mode, const char *fmt, ...)
-    __attribute__ ((format(printf, 3, 4)));
-extern int devfs_mk_symlink(const char *name, const char *link);
-extern int devfs_mk_dir(const char *fmt, ...)
-    __attribute__ ((format(printf, 1, 2)));
-extern void devfs_remove(const char *fmt, ...)
-    __attribute__ ((format(printf, 1, 2)));
-extern int devfs_register_tape(const char *name);
-extern void devfs_unregister_tape(int num);
-extern void mount_devfs_fs(void);
-#else				/*  CONFIG_DEVFS_FS  */
 static inline int devfs_mk_bdev(dev_t dev, umode_t mode, const char *fmt, ...)
 {
 	return 0;
@@ -53,5 +36,4 @@ static inline void mount_devfs_fs(void)
 {
 	return;
 }
-#endif				/*  CONFIG_DEVFS_FS  */
 #endif				/*  _LINUX_DEVFS_FS_KERNEL_H  */
