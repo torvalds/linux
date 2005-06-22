c56c2750229154f6a1cfee533e0a911da3923b5a
Reuben Farrelly <reuben-lkml@reub.net>
2005-06-22 17:16:28 +0000

    [PATCH] NFSv4: Fix build warning
     From: Reuben Farrelly <reuben-lkml@reub.net>
    
     With gcc-4.0:
    
     fs/nfs/nfs4proc.c:2976: error: static declaration of
     'nfs4_file_inode_operations' follows non-static declaration
     fs/nfs/nfs4_fs.h:179: error: previous declaration of
     'nfs4_file_inode_operations' was here
    
     Signed-off-by: Andrew Morton <akpm@osdl.org>
     Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/nfs4_fs.h
+++ fs/nfs/nfs4_fs.h
@@ -176,7 +176,6 @@ struct nfs4_state_recovery_ops {
 
 extern struct dentry_operations nfs4_dentry_operations;
 extern struct inode_operations nfs4_dir_inode_operations;
-extern struct inode_operations nfs4_file_inode_operations;
 
 /* inode.c */
 extern ssize_t nfs4_getxattr(struct dentry *, const char *, void *, size_t);
