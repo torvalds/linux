9a18664506dbce5e23f3c5de7b1c5a042dd26520
Dave Airlie <airlied@starflyer.(none)>
2005-06-23 21:29:18 +1000

    drm: 32/64-bit DRM ioctl compatibility patch
    The patch is against a 2.6.11 kernel tree.  I am running this with a
    32-bit X server (compiled up from X.org CVS as of a couple of weeks
    ago) and 32-bit DRI libraries and clients.  All the userland stuff is
    identical to what I am using under a 32-bit kernel on my G4 powerbook
    (which is a 32-bit machine of course).  I haven't tried compiling up a
    64-bit X server or clients yet.
    
    In the compatibility routines I have assumed that the kernel can
    safely access user addresses after set_fs(KERNEL_DS).  That is, where
    an ioctl argument structure contains pointers to other structures, and
    those other structures are already compatible between the 32-bit and
    64-bit ABIs (i.e. they only contain things like chars, shorts or
    ints), I just check the address with access_ok() and then pass it
    through to the 64-bit ioctl code.  I believe this approach may not
    work on sparc64, but it does work on ppc64 and x86_64 at least.
    
    One tricky area which may need to be revisited is the question of how
    to handle the handles which we pass back to userspace to identify
    mappings.  These handles are generated in the ADDMAP ioctl and then
    passed in as the offset value to mmap.  However, offset values for
    mmap seem to be generated in other ways as well, particularly for AGP
    mappings.
    
    The approach I have ended up with is to generate a fake 32-bit handle
    only for _DRM_SHM mappings.  The handles for other mappings (AGP, REG,
    FB) are physical addresses which are already limited to 32 bits, and
    generating fake handles for them created all sorts of problems in the
    mmap/nopage code.
    
    This patch has been updated to use the new compatibility ioctls.
    
    From: Paul Mackerras <paulus@samba.org>
    Signed-off-by: Dave Airlie <airlied@linux.ie>

--- drivers/char/drm/drm_bufs.c
+++ drivers/char/drm/drm_bufs.c
@@ -60,6 +60,15 @@ int drm_order( unsigned long size )
 }
 EXPORT_SYMBOL(drm_order);
 
+#ifdef CONFIG_COMPAT
+/*
+ * Used to allocate 32-bit handles for _DRM_SHM regions
+ * The 0x10000000 value is chosen to be out of the way of
+ * FB/register and GART physical addresses.
+ */
+static unsigned int map32_handle = 0x10000000;
+#endif
+
 /**
  * Ioctl to specify a range of memory that is available for mapping by a non-root process.
  *
@@ -187,16 +196,18 @@ int drm_addmap( struct inode *inode, struct file *filp,
 
 	down(&dev->struct_sem);
 	list_add(&list->head, &dev->maplist->head);
+#ifdef CONFIG_COMPAT
+	/* Assign a 32-bit handle for _DRM_SHM mappings */
+	/* We do it here so that dev->struct_sem protects the increment */
+	if (map->type == _DRM_SHM)
+		map->offset = map32_handle += PAGE_SIZE;
+#endif
  	up(&dev->struct_sem);
 
 	if ( copy_to_user( argp, map, sizeof(*map) ) )
 		return -EFAULT;
-	if ( map->type != _DRM_SHM ) {
-		if ( copy_to_user( &argp->handle,
-				   &map->offset,
-				   sizeof(map->offset) ) )
-			return -EFAULT;
-	}
+	if (copy_to_user(&argp->handle, &map->offset, sizeof(map->offset)))
+		return -EFAULT;
 	return 0;
 }
 
@@ -240,7 +251,7 @@ int drm_rmmap(struct inode *inode, struct file *filp,
 		r_list = list_entry(list, drm_map_list_t, head);
 
 		if(r_list->map &&
-		   r_list->map->handle == request.handle &&
+		   r_list->map->offset == (unsigned long) request.handle &&
 		   r_list->map->flags & _DRM_REMOVABLE) break;
 	}
 
