48b9d03c5f20a0585bb6f7d8c4abad3661df5d75
J.A. Magallon <jamagallon@able.es>
2005-06-25 14:59:22 -0700

    [PATCH] Kill signed chars
    scripts/ is full of mismatches between char* params an signed char* arguments,
    and viceversa.  gcc4 now complaints loud about this.  Patch below deletes all
    those 'signed'.
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- scripts/kconfig/mconf.c
+++ scripts/kconfig/mconf.c
@@ -255,8 +255,8 @@ search_help[] = N_(
 	"          USB$ => find all CONFIG_ symbols ending with USB\n"
 	"\n");
 
-static signed char buf[4096], *bufptr = buf;
-static signed char input_buf[4096];
+static char buf[4096], *bufptr = buf;
+static char input_buf[4096];
 static char filename[PATH_MAX+1] = ".config";
 static char *args[1024], **argptr = args;
 static int indent;
