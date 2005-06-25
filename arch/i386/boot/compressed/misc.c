3d345e3fc9e9177deb7c82e5c79e32d77eb63cce
Eric W. Biederman <ebiederm@xmission.com>
2005-06-25 14:57:49 -0700

    [PATCH] kexec: x86: add CONFIG_PYSICAL_START
    For one kernel to report a crash another kernel has created we need
    to have 2 kernels loaded simultaneously in memory.  To accomplish this
    the two kernels need to built to run at different physical addresses.
    
    This patch adds the CONFIG_PHYSICAL_START option to the x86 kernel
    so we can do just that.  You need to know what you are doing and
    the ramifications are before changing this value, and most users
    won't care so I have made it depend on CONFIG_EMBEDDED
    
    bzImage kernels will work and run at a different address when compiled
    with this option but they will still load at 1MB.  If you need a kernel
    loaded at a different address as well you need to boot a vmlinux.
    
    Signed-off-by: Eric Biederman <ebiederm@xmission.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/i386/boot/compressed/misc.c
+++ arch/i386/boot/compressed/misc.c
@@ -13,6 +13,7 @@
 #include <linux/vmalloc.h>
 #include <linux/tty.h>
 #include <asm/io.h>
+#include <asm/page.h>
 
 /*
  * gzip declarations
@@ -308,7 +309,7 @@ static void setup_normal_output_buffer(void)
 #else
 	if ((RM_ALT_MEM_K > RM_EXT_MEM_K ? RM_ALT_MEM_K : RM_EXT_MEM_K) < 1024) error("Less than 2MB of memory");
 #endif
-	output_data = (char *)0x100000; /* Points to 1M */
+	output_data = (char *)__PHYSICAL_START; /* Normally Points to 1M */
 	free_mem_end_ptr = (long)real_mode;
 }
 
@@ -333,8 +334,8 @@ static void setup_output_buffer_if_we_run_high(struct moveparams *mv)
 	low_buffer_size = low_buffer_end - LOW_BUFFER_START;
 	high_loaded = 1;
 	free_mem_end_ptr = (long)high_buffer_start;
-	if ( (0x100000 + low_buffer_size) > ((ulg)high_buffer_start)) {
-		high_buffer_start = (uch *)(0x100000 + low_buffer_size);
+	if ( (__PHYSICAL_START + low_buffer_size) > ((ulg)high_buffer_start)) {
+		high_buffer_start = (uch *)(__PHYSICAL_START + low_buffer_size);
 		mv->hcount = 0; /* say: we need not to move high_buffer */
 	}
 	else mv->hcount = -1;
