b9df978f1974fea373741367b5d79a2ed3b7dcf9
Luca Risolia <luca.risolia@studio.unibo.it>
2005-06-25 16:30:24 +0200

    [PATCH] USB: SN9C10x driver updates
    SN9C10x driver updates.
    
    Changes: + new, - removed, * cleanup, @ bugfix
    
    @ Remove bad get_ctrl()'s
    * Documentation updates
    + Add 0x0c45/0x602d to the list of SN9C10x based devices
    + Add support for OV7630 image sensors
    
    Signed-off-by: Luca Risolia <luca.risolia@studio.unibo.it>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/media/sn9c102_core.c
+++ drivers/usb/media/sn9c102_core.c
@@ -429,7 +429,7 @@ sn9c102_i2c_try_read(struct sn9c102_device* cam,
 }
 
 
-static int 
+int
 sn9c102_i2c_try_write(struct sn9c102_device* cam,
                       struct sn9c102_sensor* sensor, u8 address, u8 value)
 {
