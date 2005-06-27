1a8ceafce5f1e68c28c7ba52fd7e72ab0008eb46
Dominik Brodowski <linux@dominikbrodowski.net>
2005-06-27 16:29:00 -0700

    [PATCH] pcmcia: properly handle all errors of register_chrdev
    register_chrdev() can return errors (negative) other then -EBUSY, so check
    for any negative error code.
    
    Signed-off-by: Randy Dunlap <rdunlap@xenotime.net>
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/pcmcia/pcmcia_ioctl.c
+++ drivers/pcmcia/pcmcia_ioctl.c
@@ -760,9 +760,9 @@ void __init pcmcia_setup_ioctl(void) {
 
 	/* Set up character device for user mode clients */
 	i = register_chrdev(0, "pcmcia", &ds_fops);
-	if (i == -EBUSY)
+	if (i < 0)
 		printk(KERN_NOTICE "unable to find a free device # for "
-		       "Driver Services\n");
+		       "Driver Services (error=%d)\n", i);
 	else
 		major_dev = i;
 
