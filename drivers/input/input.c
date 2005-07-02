beffbdc2211826b174c68307b1b48c93c05d7ded
Andrew Morton <akpm@osdl.org>
2005-07-01 23:54:30 -0500

    Input: cannot refer to __exit from within __init.
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -581,7 +581,7 @@ static int __init input_proc_init(void)
 	return -ENOMEM;
 }
 
-static void __exit input_proc_exit(void)
+static void input_proc_exit(void)
 {
 	remove_proc_entry("devices", proc_bus_input_dir);
 	remove_proc_entry("handlers", proc_bus_input_dir);
