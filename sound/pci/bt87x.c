159f597a8bd0f1d7650d5e580c93a2666c9c26d1
Dave Jones <davej@redhat.com>
2005-07-06 15:29:09 -0400

    [PATCH] Fix bt87x.c build problem for real
    Just the declaration fix wasn't enough to fix things in bt78x.c
    
    Signed-off-by: Dave Jones <davej@redhat.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- sound/pci/bt87x.c
+++ sound/pci/bt87x.c
@@ -806,7 +806,7 @@ static int __devinit snd_bt87x_detect_card(struct pci_dev *pci)
 	int i;
 	const struct pci_device_id *supported;
 
-	supported = pci_match_device(driver, pci);
+	supported = pci_match_device(&driver, pci);
 	if (supported)
 		return supported->driver_data;
 
