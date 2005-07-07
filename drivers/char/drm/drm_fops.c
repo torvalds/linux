0c7b525c344bc29a760c37053f8d5c80292ee1be
Dave Airlie <airlied@starflyer.(none)>
2005-07-07 20:16:08 +1000

    drm: fix minor issues caused by core conversion
    The conversion to core/driver got this check in-correct.
    
    Signed-off-by: Dave Airlie <airlied@linux.ie>

--- drivers/char/drm/drm_fops.c
+++ drivers/char/drm/drm_fops.c
@@ -251,7 +251,7 @@ int drm_release( struct inode *inode, struct file *filp )
 		}
 	}
 	
-	if (drm_core_check_feature(dev, DRIVER_HAVE_DMA))
+	if (drm_core_check_feature(dev, DRIVER_HAVE_DMA) && !dev->driver->release)
 	{
 		dev->driver->reclaim_buffers(dev, filp);
 	}
