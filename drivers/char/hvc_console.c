e51d8c90a5ead16810302b2dc5127724c9045e77
Milton Miller <miltonm@bga.com>
2005-07-07 17:56:21 -0700

    [PATCH] hvc_console: Add missing include
    hvc_console checks MAGIC_SYSRQ and XMON config vars.
    
    Signed-off-by: Milton Miller <miltonm@bga.com>
    Signed-off-by: Anton Blanchard <anton@samba.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/char/hvc_console.c
+++ drivers/char/hvc_console.c
@@ -22,6 +22,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  */
 
+#include <linux/config.h>
 #include <linux/console.h>
 #include <linux/cpumask.h>
 #include <linux/init.h>
