fdd0edf2aca72e374d10e7a0957a006f18bffaf4
James Bottomley <James.Bottomley@steeleye.com>
2005-08-04 13:28:40 -0500

    [SCSI] fix aic7xxx performance issues since 2.6.12-rc2
    Several people noticed we dropped quite a bit on benchmark figures.
    OK, it was my fault but unfortunately I discovered I ran out of brown
    paper bags a while ago and forgot to reorder them.
    
    The issue is that a construct introduced in the conversion of the
    driver to use the transport class keyed off whether the block request
    was tagged or not.  However, the aic7xxx driver doesn't properly set
    up the block layer TCQ (it uses the wrong API), so the driver now
    things all requests are untagged and we keep it to a queue depth of a
    single element.  Oops.
    
    The fix is to use the correct TCQ API.
    
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/aic7xxx/aic7xxx_osm.c
+++ drivers/scsi/aic7xxx/aic7xxx_osm.c
@@ -1264,14 +1264,12 @@ ahc_platform_set_tags(struct ahc_softc *ahc, struct ahc_devinfo *devinfo,
 	}
 	switch ((dev->flags & (AHC_DEV_Q_BASIC|AHC_DEV_Q_TAGGED))) {
 	case AHC_DEV_Q_BASIC:
-		scsi_adjust_queue_depth(sdev,
-					MSG_SIMPLE_TASK,
-					dev->openings + dev->active);
+		scsi_set_tag_type(sdev, MSG_SIMPLE_TAG);
+		scsi_activate_tcq(sdev, dev->openings + dev->active);
 		break;
 	case AHC_DEV_Q_TAGGED:
-		scsi_adjust_queue_depth(sdev,
-					MSG_ORDERED_TASK,
-					dev->openings + dev->active);
+		scsi_set_tag_type(sdev, MSG_ORDERED_TAG);
+		scsi_activate_tcq(sdev, dev->openings + dev->active);
 		break;
 	default:
 		/*
@@ -1280,9 +1278,7 @@ ahc_platform_set_tags(struct ahc_softc *ahc, struct ahc_devinfo *devinfo,
 		 * serially on the controller/device.  This should
 		 * remove some latency.
 		 */
-		scsi_adjust_queue_depth(sdev,
-					/*NON-TAGGED*/0,
-					/*queue depth*/2);
+		scsi_deactivate_tcq(sdev, 2);
 		break;
 	}
 }
