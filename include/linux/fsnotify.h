00dd1e433967872f3997a45d5adf35056fdf2f56
John McCutchan <ttb@tentacle.dhs.org>
2005-08-08 22:13:05 -0400

    [PATCH] fsnotify-cleanups
    This removes the now unused fsnotify_unlink & fsnotify_rmdir code.
    Compile tested.
    
    Signed-off-by: John McCutchan <ttb@tentacle.dhs.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/fsnotify.h
+++ include/linux/fsnotify.h
@@ -44,30 +44,6 @@ static inline void fsnotify_move(struct inode *old_dir, struct inode *new_dir,
 }
 
 /*
- * fsnotify_unlink - file was unlinked
- */
-static inline void fsnotify_unlink(struct dentry *dentry, struct inode *inode, struct inode *dir)
-{
-	inode_dir_notify(dir, DN_DELETE);
-	inotify_inode_queue_event(dir, IN_DELETE, 0, dentry->d_name.name);
-	inotify_inode_queue_event(inode, IN_DELETE_SELF, 0, NULL);
-
-	inotify_inode_is_dead(inode);
-}
-
-/*
- * fsnotify_rmdir - directory was removed
- */
-static inline void fsnotify_rmdir(struct dentry *dentry, struct inode *inode,
-				  struct inode *dir)
-{
-	inode_dir_notify(dir, DN_DELETE);
-	inotify_inode_queue_event(dir,IN_DELETE|IN_ISDIR,0,dentry->d_name.name);
-	inotify_inode_queue_event(inode, IN_DELETE_SELF | IN_ISDIR, 0, NULL);
-	inotify_inode_is_dead(inode);
-}
-
-/*
  * fsnotify_nameremove - a filename was removed from a directory
  */
 static inline void fsnotify_nameremove(struct dentry *dentry, int isdir)
