e0c93cf311b3eaa04bfb5769cff38861d4e748c8
Daniel Horchner <dbjh@gmx.net>
2005-08-25 13:02:24 +0200

    [ALSA] intel8x0 - add AC97_TUNE_MUTE_LED quirk for HP nx8220 laptop 103c:0934
    Intel8x0 driver
    
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/pci/intel8x0.c
+++ sound/pci/intel8x0.c
@@ -1826,6 +1826,12 @@ static struct ac97_quirk ac97_quirks[] __devinitdata = {
 	},
 	{
 		.subvendor = 0x103c,
+		.subdevice = 0x0934,
+		.name = "HP nx8220",
+		.type = AC97_TUNE_MUTE_LED
+	},
+	{
+		.subvendor = 0x103c,
 		.subdevice = 0x099c,
 		.name = "HP nx6110",	/* AD1981B */
 		.type = AC97_TUNE_HP_ONLY
