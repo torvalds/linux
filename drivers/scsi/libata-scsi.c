6f1062330499cee10396bf3fc66a03eb228c5fad
Jeff Garzik <jgarzik@pobox.com>
2005-08-30 21:52:18 -0400

    [libata] fix ATAPI-enable typo
    Dumb typo spotted by Mark Lord.

--- drivers/scsi/libata-scsi.c
+++ drivers/scsi/libata-scsi.c
@@ -1470,7 +1470,7 @@ ata_scsi_find_dev(struct ata_port *ap, struct scsi_device *scsidev)
 	if (unlikely(!ata_dev_present(dev)))
 		return NULL;
 
-	if (atapi_enabled) {
+	if (!atapi_enabled) {
 		if (unlikely(dev->class == ATA_DEV_ATAPI))
 			return NULL;
 	}
