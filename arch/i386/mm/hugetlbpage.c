02b0ccef903e85673ead74ddb7c431f2f7ce183d
Adam Litke <agl@us.ibm.com>
2005-09-03 15:55:01 -0700

    [PATCH] hugetlb: check p?d_present in huge_pte_offset()
    For demand faulting, we cannot assume that the page tables will be
    populated.  Do what the rest of the architectures do and test p?d_present()
    while walking down the page table.
    
    Signed-off-by: Adam Litke <agl@us.ibm.com>
    Cc: <linux-mm@kvack.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/i386/mm/hugetlbpage.c
+++ arch/i386/mm/hugetlbpage.c
@@ -46,8 +46,11 @@ pte_t *huge_pte_offset(struct mm_struct *mm, unsigned long addr)
 	pmd_t *pmd = NULL;
 
 	pgd = pgd_offset(mm, addr);
-	pud = pud_offset(pgd, addr);
-	pmd = pmd_offset(pud, addr);
+	if (pgd_present(*pgd)) {
+		pud = pud_offset(pgd, addr);
+		if (pud_present(*pud))
+			pmd = pmd_offset(pud, addr);
+	}
 	return (pte_t *) pmd;
 }
 
