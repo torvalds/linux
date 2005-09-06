c8d127418d78aaeeb1a417ef7453dc09c9118146
Christoph Hellwig <hch@lst.de>
2005-09-06 15:17:27 -0700

    [PATCH] remove asm-*/hdreg.h
    unused and useless..
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-generic/hdreg.h
+++
@@ -1,8 +0,0 @@
-#warning <asm/hdreg.h> is obsolete, please do not use it
-
-#ifndef __ASM_GENERIC_HDREG_H
-#define __ASM_GENERIC_HDREG_H
-
-typedef unsigned long ide_ioreg_t;
-
-#endif /* __ASM_GENERIC_HDREG_H */
