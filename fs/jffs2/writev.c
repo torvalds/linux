8acff5e93488e4da653097bd5e50662ee0985867
Ferenc Havasi <havasi@inf.u-szeged.hu>
2005-09-09 16:12:01 +0100

    [JFFS2] Call summary collector for all mtd devices with writev support
    Do the summary collection in the right place. If the device
    was not writebuffered but had c->mtd->writev function
    (e.g. blkmtd) the summary collector function was not called.
    
    Signed-off-by: Ferenc Havasi <havasi@inf.u-szeged.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- fs/jffs2/writev.c
+++ fs/jffs2/writev.c
@@ -7,7 +7,7 @@
  *
  * For licensing information, see the file 'LICENCE' in this directory.
  *
- * $Id: writev.c,v 1.7 2005/09/07 08:34:55 havasi Exp $
+ * $Id: writev.c,v 1.8 2005/09/09 15:11:58 havasi Exp $
  *
  */
 
@@ -42,18 +42,19 @@ static inline int mtd_fake_writev(struct mtd_info *mtd, const struct kvec *vecs,
 int jffs2_flash_direct_writev(struct jffs2_sb_info *c, const struct kvec *vecs,
 			      unsigned long count, loff_t to, size_t *retlen)
 {
-	if (c->mtd->writev)
-		return c->mtd->writev(c->mtd, vecs, count, to, retlen);
-	else {
+	if (!jffs2_is_writebuffered(c)) {
 		if (jffs2_sum_active()) {
 			int res;
-
 			res = jffs2_sum_add_kvec(c, vecs, count, (uint32_t) to);
 			if (res) {
 				return res;
 			}
 		}
+	}
 
+	if (c->mtd->writev)
+		return c->mtd->writev(c->mtd, vecs, count, to, retlen);
+	else {
 		return mtd_fake_writev(c->mtd, vecs, count, to, retlen);
 	}
 }
