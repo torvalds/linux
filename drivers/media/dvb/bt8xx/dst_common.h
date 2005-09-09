a427de6f72bc0d83ebb1d87f9003c5e1009f21cd
Manu Abraham <manu@linuxtv.org>
2005-09-09 13:03:00 -0700

    [PATCH] dvb: dst: dprrintk cleanup
    Code Cleanup:
    o Remove debug noise
    o Remove debug module parameter
      debug level is achieved using the verbosity level
    o Updated to kernel coding style
      (case labels should not be indented)
    
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Johannes Stezenbach <js@linuxtv.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/media/dvb/bt8xx/dst_common.h
+++ drivers/media/dvb/bt8xx/dst_common.h
@@ -61,7 +61,6 @@
 #define	DST_TYPE_HAS_ANALOG	64	/*	Analog inputs	*/
 #define DST_TYPE_HAS_SESSION	128
 
-
 #define RDC_8820_PIO_0_DISABLE	0
 #define RDC_8820_PIO_0_ENABLE	1
 #define RDC_8820_INT		2
@@ -124,14 +123,11 @@ struct dst_types {
 	u32 dst_feature;
 };
 
-
-
 struct dst_config
 {
 	/* the ASIC i2c address */
 	u8 demod_address;
 };
-
 
 int rdc_reset_state(struct dst_state *state);
 int rdc_8820_reset(struct dst_state *state);
