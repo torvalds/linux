9c1ee9387c0ce06d573e2d27de10cbc24179941e
Greg Ungerer <gerg@snapgear.com>
2005-09-12 11:18:10 +1000

    [PATCH] m68knommu: change addr arg to const in bitops.h/find_next_zero_bit()
    Change addr arg to find_next_zero_bit to be a const.
    Cleans up compiler warning.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-m68knommu/bitops.h
+++ include/asm-m68knommu/bitops.h
@@ -259,7 +259,7 @@ static __inline__ int __test_bit(int nr, const volatile unsigned long * addr)
 #define find_first_bit(addr, size) \
         find_next_bit((addr), (size), 0)
 
-static __inline__ int find_next_zero_bit (void * addr, int size, int offset)
+static __inline__ int find_next_zero_bit (const void * addr, int size, int offset)
 {
 	unsigned long *p = ((unsigned long *) addr) + (offset >> 5);
 	unsigned long result = offset & ~31UL;
