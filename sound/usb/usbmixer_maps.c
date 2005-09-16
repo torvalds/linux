460dc98afc6db255344a06c8da1196e2a2744758
Clemens Ladisch <clemens@ladisch.de>
2005-09-16 16:56:01 +0200

    [ALSA] usb-audio: ignore Hercules DJ Console mixer errors
    USB generic driver
    Add a quirk entry for the Hercules DJ Console to ignore timeouts on
    some mixer control transfers.
    
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>

--- sound/usb/usbmixer_maps.c
+++ sound/usb/usbmixer_maps.c
@@ -238,6 +238,11 @@ static struct usbmix_ctl_map usbmix_ctl_maps[] = {
 		.selector_map = audigy2nx_selectors,
 	},
 	{
+		/* Hercules DJ Console */
+		.id = USB_ID(0x06f8, 0xd002),
+		.ignore_ctl_error = 1,
+	},
+	{
 		.id = USB_ID(0x08bb, 0x2702),
 		.map = linex_map,
 		.ignore_ctl_error = 1,
