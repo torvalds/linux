8a4a10ae996b67c622b9f3f6a4dfea5de4500a11
Kumar Gala <galak@freescale.com>
2005-09-20 16:33:54 -0500

    [PATCH] powerpc: merge include/asm-ppc*/spinlock_types.h into include/asm-powerpc/spinlock_types.h
    Signed-off-by: Kumar Gala <kumar.gala@freescale.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/spinlock_types.h
+++ include/asm-powerpc/spinlock_types.h
@@ -1,12 +1,12 @@
-#ifndef __ASM_SPINLOCK_TYPES_H
-#define __ASM_SPINLOCK_TYPES_H
+#ifndef _ASM_POWERPC_SPINLOCK_TYPES_H
+#define _ASM_POWERPC_SPINLOCK_TYPES_H
 
 #ifndef __LINUX_SPINLOCK_TYPES_H
 # error "please don't include this file directly"
 #endif
 
 typedef struct {
-	volatile unsigned long lock;
+	volatile unsigned int slock;
 } raw_spinlock_t;
 
 #define __RAW_SPIN_LOCK_UNLOCKED	{ 0 }
