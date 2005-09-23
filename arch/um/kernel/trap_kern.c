546fe1cbf91d4d62e3849517c31a2327c992e5c5
Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
2005-09-22 21:44:16 -0700

    [PATCH] uml: fix hang in TT mode on fault
    The current code doesn't handle well general protection faults on the host -
    it thinks that cr2 is always the address of a page fault.  While actually, on
    general protection faults, that address is not accessible, so we'd better
    assume we couldn't satisfy the fault.  Currently instead we think we've fixed
    it, so we go back, retry the instruction and fault again endlessly.
    
    This leads to the kernel hanging when doing copy_from_user(dest, -1, ...) in
    TT mode, since reading *(-1) causes a GFP, and we don't support kernel
    preemption.
    
    Thanks to Luo Xin for testing UML with LTP and reporting the failures he got.
    
    Cc: Luo Xin <luothing@sina.com>
    Signed-off-by: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    Cc: Jeff Dike <jdike@addtoit.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/um/kernel/trap_kern.c
+++ arch/um/kernel/trap_kern.c
@@ -18,6 +18,7 @@
 #include "asm/a.out.h"
 #include "asm/current.h"
 #include "asm/irq.h"
+#include "sysdep/sigcontext.h"
 #include "user_util.h"
 #include "kern_util.h"
 #include "kern.h"
@@ -125,7 +126,15 @@ unsigned long segv(struct faultinfo fi, unsigned long ip, int is_user, void *sc)
         }
 	else if(current->mm == NULL)
 		panic("Segfault with no mm");
-	err = handle_page_fault(address, ip, is_write, is_user, &si.si_code);
+
+	if (SEGV_IS_FIXABLE(&fi))
+		err = handle_page_fault(address, ip, is_write, is_user, &si.si_code);
+	else {
+		err = -EFAULT;
+		/* A thread accessed NULL, we get a fault, but CR2 is invalid.
+		 * This code is used in __do_copy_from_user() of TT mode. */
+		address = 0;
+	}
 
 	catcher = current->thread.fault_catcher;
 	if(!err)
