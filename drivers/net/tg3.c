ed39f731ab2e77e58122232f6e27333331d7793d
David S. Miller <davem@sunset.davemloft.net>
2005-10-03 16:25:23 -0700

    [TG3]: Update driver version and release date.
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/tg3.c
+++ drivers/net/tg3.c
@@ -67,8 +67,8 @@
 
 #define DRV_MODULE_NAME		"tg3"
 #define PFX DRV_MODULE_NAME	": "
-#define DRV_MODULE_VERSION	"3.41"
-#define DRV_MODULE_RELDATE	"September 27, 2005"
+#define DRV_MODULE_VERSION	"3.42"
+#define DRV_MODULE_RELDATE	"Oct 3, 2005"
 
 #define TG3_DEF_MAC_MODE	0
 #define TG3_DEF_RX_MODE		0
