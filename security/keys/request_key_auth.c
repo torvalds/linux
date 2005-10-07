f1a9badcf6ecad9975240d94514721cb93932151
David Howells <dhowells@redhat.com>
2005-10-07 15:04:52 +0100

    [PATCH] Keys: Add request-key process documentation
    The attached patch adds documentation for the process by which request-key
    works, including how it permits helper processes to gain access to the
    requestor's keyrings.
    
    Signed-Off-By: David Howells <dhowells@redhat.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- security/keys/request_key_auth.c
+++ security/keys/request_key_auth.c
@@ -7,6 +7,8 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version
  * 2 of the License, or (at your option) any later version.
+ *
+ * See Documentation/keys-request-key.txt
  */
 
 #include <linux/module.h>
