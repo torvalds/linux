9d624ea474a3ddf3a0702d9b47e428ce1f8488a6
Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
2005-10-11 21:01:01 +0200

    [PATCH] uml: compile-time fix recent patch
    Give an empty definition for clear_can_do_skas() when it is not needed.
    Thanks to Junichi Uekawa <dancer@netfort.gr.jp> for reporting the
    breakage and providing a fix (I re-fixed it in an IMHO cleaner way).
    
    Signed-off-by: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/um/include/os.h
+++ arch/um/include/os.h
@@ -6,6 +6,7 @@
 #ifndef __OS_H__
 #define __OS_H__
 
+#include "uml-config.h"
 #include "asm/types.h"
 #include "../os/include/file.h"
 
@@ -159,7 +160,11 @@ extern int can_do_skas(void);
 
 /* Make sure they are clear when running in TT mode. Required by
  * SEGV_MAYBE_FIXABLE */
+#ifdef UML_CONFIG_MODE_SKAS
 #define clear_can_do_skas() do { ptrace_faultinfo = proc_mm = 0; } while (0)
+#else
+#define clear_can_do_skas() do {} while (0)
+#endif
 
 /* mem.c */
 extern int create_mem_file(unsigned long len);
