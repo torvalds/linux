89edce0b4e71f6f78e27d7c18c63e957469b74d3
Stephen Rothwell <sfr@canb.auug.org.au>
2005-10-25 16:59:03 +1000

    [PATCH] powerpc: merge scatterlist.h
    This depends on the 64bit dma_addr_t patch.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/scatterlist.h
+++ include/asm-powerpc/scatterlist.h
@@ -1,6 +1,5 @@
-#ifndef _PPC64_SCATTERLIST_H
-#define _PPC64_SCATTERLIST_H
-
+#ifndef _ASM_POWERPC_SCATTERLIST_H
+#define _ASM_POWERPC_SCATTERLIST_H
 /*
  * Copyright (C) 2001 PPC64 Team, IBM Corp
  *
@@ -10,6 +9,7 @@
  * 2 of the License, or (at your option) any later version.
  */
 
+#ifdef __KERNEL__
 #include <linux/types.h>
 #include <asm/dma.h>
 
@@ -23,9 +23,23 @@ struct scatterlist {
 	u32 dma_length;
 };
 
+/*
+ * These macros should be used after a dma_map_sg call has been done
+ * to get bus addresses of each of the SG entries and their lengths.
+ * You should only work with the number of sg entries pci_map_sg
+ * returns, or alternatively stop on the first sg_dma_len(sg) which
+ * is 0.
+ */
 #define sg_dma_address(sg)	((sg)->dma_address)
+#ifdef __powerpc64__
 #define sg_dma_len(sg)		((sg)->dma_length)
+#else
+#define sg_dma_len(sg)		((sg)->length)
+#endif
 
+#ifdef __powerpc64__
 #define ISA_DMA_THRESHOLD	(~0UL)
+#endif
 
-#endif /* !(_PPC64_SCATTERLIST_H) */
+#endif /* __KERNEL__ */
+#endif /* _ASM_POWERPC_SCATTERLIST_H */
