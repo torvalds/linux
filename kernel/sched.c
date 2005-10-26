bb32051532fed727de0d513a9a578b54c0b7ea5a
Andrew Morton <akpm@osdl.org>
2005-10-26 01:59:01 -0700

    [PATCH] export cpu_online_map
    With CONFIG_SMP=n:
    
    *** Warning: "cpu_online_map" [drivers/firmware/dcdbas.ko] undefined!
    
    due to set_cpus_allowed().
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/sched.c
+++ kernel/sched.c
@@ -3879,6 +3879,7 @@ EXPORT_SYMBOL(cpu_present_map);
 
 #ifndef CONFIG_SMP
 cpumask_t cpu_online_map = CPU_MASK_ALL;
+EXPORT_SYMBOL_GPL(cpu_online_map);
 cpumask_t cpu_possible_map = CPU_MASK_ALL;
 #endif
 
