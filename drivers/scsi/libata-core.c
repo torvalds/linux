005a5a06a6dd13a0ca3f2c6a0218e8d94ed36d8a
Jeff Garzik <jgarzik@pobox.com>
2005-10-30 23:31:48 -0500

    [libata] locking rewrite (== fix)
    A lot of power packed into a little patch.
    
    This change eliminates the sharing between our controller-wide spinlock
    and the SCSI core's Scsi_Host lock.  As the locking in libata was
    already highly compartmentalized, always referencing our own lock, and
    never scsi_host::host_lock.
    
    As a side effect, this change eliminates a deadlock from calling
    scsi_finish_command() while inside our spinlock.

--- drivers/scsi/libata-core.c
+++ drivers/scsi/libata-core.c
@@ -4089,8 +4089,6 @@ static void ata_host_init(struct ata_port *ap, struct Scsi_Host *host,
 	host->unique_id = ata_unique_id++;
 	host->max_cmd_len = 12;
 
-	scsi_assign_lock(host, &host_set->lock);
-
 	ap->flags = ATA_FLAG_PORT_DISABLED;
 	ap->id = host->unique_id;
 	ap->host = host;
