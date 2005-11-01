19fe04755aca56e7d143f7f95b41008138619c5c
Arnd Bergmann <arndb@de.ibm.com>
2005-10-31 20:08:38 -0500

    [PATCH] powerpc: create a new arch/powerpc/platforms/cell/smp.c
    During the conversion to the merge tree, the Cell specific
    SMP initialization was removed from the pSeries code.
    
    This creates a new Cell specific SMP implementation file.
    
    Signed-off-by: Arnd Bergmann <arndb@de.ibm.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-ppc64/smp.h
+++ include/asm-ppc64/smp.h
@@ -64,6 +64,7 @@ extern cpumask_t cpu_sibling_map[NR_CPUS];
 
 void smp_init_iSeries(void);
 void smp_init_pSeries(void);
+void smp_init_cell(void);
 
 extern int __cpu_disable(void);
 extern void __cpu_die(unsigned int cpu);
