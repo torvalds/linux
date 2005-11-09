d4c34aa024aef2be3f3deca751c335f72c3e32b6
Kirk Lapray <kirk.lapray@gmail.com>
2005-11-08 21:38:40 -0800

    [PATCH] V4L: 901: added function for nxt200x to change pll input
    Added function for nxt200x to change pll input
    
    Signed-off-by: Kirk Lapray <kirk.lapray@gmail.com>
    Signed-off-by: Michael Krufky <mkrufky@m1k.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@brturbo.com.br>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/media/video/cx88/cx88-dvb.c
+++ drivers/media/video/cx88/cx88-dvb.c
@@ -296,10 +296,20 @@ static int nxt200x_set_ts_param(struct dvb_frontend* fe,
 	return 0;
 }
 
+static int nxt200x_set_pll_input(u8* buf, int input)
+{
+	if (input)
+		buf[3] |= 0x08;
+	else
+		buf[3] &= ~0x08;
+	return 0;
+}
+
 static struct nxt200x_config ati_hdtvwonder = {
 	.demod_address    = 0x0a,
 	.pll_address      = 0x61,
 	.pll_desc         = &dvb_pll_tuv1236d,
+	.set_pll_input    = nxt200x_set_pll_input,
 	.set_ts_params    = nxt200x_set_ts_param,
 };
 #endif
