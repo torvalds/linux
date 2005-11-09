93a14f15d35cd60742220995d3413d256ebde9ee
Raymond Mantchala <raymond.mantchala@streamvision.ft>
2005-11-08 21:35:17 -0800

    [PATCH] dvb: dst: ASN.1 length field Fix
    ASN.1 length field Fix
    
    Signed-off-by: Raymond Mantchala <raymond.mantchala@streamvision.ft>
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Cc: Johannes Stezenbach <js@linuxtv.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/media/dvb/bt8xx/dst_ca.c
+++ drivers/media/dvb/bt8xx/dst_ca.c
@@ -328,7 +328,8 @@ u32 asn_1_decode(u8 *asn_1_array)
 	} else {
 		word_count = length_field & 0x7f;
 		for (count = 0; count < word_count; count++) {
-			length = (length | asn_1_array[count + 1]) << 8;
+			length = length  << 8;
+			length += asn_1_array[count + 1];
 			dprintk(verbose, DST_CA_DEBUG, 1, " Length=[%04x]", length);
 		}
 	}
