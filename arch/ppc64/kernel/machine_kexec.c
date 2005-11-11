c5e24354efae9f962e0e369d875d45f47e0bb9aa
Michael Ellerman <michael@ellerman.id.au>
2005-11-12 00:06:05 +1100

    [PATCH] powerpc: Turn cpu_irq_down into kexec_cpu_down
    We currently have a ppc_md member called cpu_irq_down, which disables IRQs
    for the cpu in question. The only caller of cpu_irq_down is the kexec code.
    
    On pSeries we need to do more than just teardown IRQs at kexec time, so rename
    the ppc_md member to kexec_cpu_down and expand it. The pSeries code needs to
    know, and other platforms might too, whether we're doing a crash shutdown (ie.
    panicking) or a regular kexec, so add a flag for that.
    
    The pSeries implementation of kexec_cpu_down does an unregister VPA call, which
    tells the Hypervisor to stop writing stuff into our pacas. Without this we can
    get weird memory corruption bugs when we kexec, caused by the Hypervisor
    writing into the first kernel's pacas which happens to be somewhere interesting
    in the second kernel's memory.
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/ppc64/kernel/machine_kexec.c
+++ arch/ppc64/kernel/machine_kexec.c
@@ -185,8 +185,8 @@ void kexec_copy_flush(struct kimage *image)
  */
 void kexec_smp_down(void *arg)
 {
-	if (ppc_md.cpu_irq_down)
-		ppc_md.cpu_irq_down(1);
+	if (ppc_md.kexec_cpu_down)
+		ppc_md.kexec_cpu_down(0, 1);
 
 	local_irq_disable();
 	kexec_smp_wait();
@@ -233,8 +233,8 @@ static void kexec_prepare_cpus(void)
 	}
 
 	/* after we tell the others to go down */
-	if (ppc_md.cpu_irq_down)
-		ppc_md.cpu_irq_down(0);
+	if (ppc_md.kexec_cpu_down)
+		ppc_md.kexec_cpu_down(0, 0);
 
 	put_cpu();
 
@@ -255,8 +255,8 @@ static void kexec_prepare_cpus(void)
 	 * UP to an SMP kernel.
 	 */
 	smp_release_cpus();
-	if (ppc_md.cpu_irq_down)
-		ppc_md.cpu_irq_down(0);
+	if (ppc_md.kexec_cpu_down)
+		ppc_md.kexec_cpu_down(0, 0);
 	local_irq_disable();
 }
 
