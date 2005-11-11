c0be4d240483f3ebd138db467b5e8fbe15c520e2
Dave Airlie <airlied@starflyer.(none)>
2005-11-11 23:10:18 +1100

    drm: remove exports that modules shouldn't use.
    Modules should go via the new drm_agp_ functions.
    
    Signed-off-by: Dave Airlie <airlied@linux.ie>

--- drivers/char/drm/drm_memory.c
+++ drivers/char/drm/drm_memory.c
@@ -145,15 +145,11 @@ DRM_AGP_MEM *drm_alloc_agp(drm_device_t * dev, int pages, u32 type)
 	return drm_agp_allocate_memory(dev->agp->bridge, pages, type);
 }
 
-EXPORT_SYMBOL(drm_alloc_agp);
-
 /** Wrapper around agp_free_memory() */
 int drm_free_agp(DRM_AGP_MEM * handle, int pages)
 {
 	return drm_agp_free_memory(handle) ? 0 : -EINVAL;
 }
-
-EXPORT_SYMBOL(drm_free_agp);
 
 /** Wrapper around agp_bind_memory() */
 int drm_bind_agp(DRM_AGP_MEM * handle, unsigned int start)
@@ -161,14 +157,10 @@ int drm_bind_agp(DRM_AGP_MEM * handle, unsigned int start)
 	return drm_agp_bind_memory(handle, start);
 }
 
-EXPORT_SYMBOL(drm_bind_agp);
-
 /** Wrapper around agp_unbind_memory() */
 int drm_unbind_agp(DRM_AGP_MEM * handle)
 {
 	return drm_agp_unbind_memory(handle);
 }
-
-EXPORT_SYMBOL(drm_unbind_agp);
 #endif				/* agp */
 #endif				/* debug_memory */
