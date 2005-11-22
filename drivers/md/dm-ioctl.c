c4cc66351a24da5feec298be2da59a85f68dd3ea
Alasdair G Kergon <agk@redhat.com>
2005-11-21 21:32:33 -0800

    [PATCH] device-mapper: list_versions fix
    In some circumstances the LIST_VERSIONS output is truncated because the size
    calculation forgets about a 'uint32_t' in each structure - but the inclusion
    of the whole of ALIGN_MASK frequently compensates for the omission.
    
    This is a quick workaround to use an upper bound.  (The code ought to be fixed
    to supply the actual size.)
    
    Running 'dmsetup targets' may demonstrate the problem: when I run it, the last
    line comes out as 'erro' instead of 'error'.  Consequently, 'lvcreate --type
    error' doesn't work.
    
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/md/dm-ioctl.c
+++ drivers/md/dm-ioctl.c
@@ -425,8 +425,8 @@ static void list_version_get_needed(struct target_type *tt, void *needed_param)
 {
     size_t *needed = needed_param;
 
+    *needed += sizeof(struct dm_target_versions);
     *needed += strlen(tt->name);
-    *needed += sizeof(tt->version);
     *needed += ALIGN_MASK;
 }
 
