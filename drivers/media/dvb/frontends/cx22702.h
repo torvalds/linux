9b9225f0f002c39f340da2ed1e7efec6885ca312
Steven Toth <stoth@hauppauge.com>
2005-12-01 00:51:53 -0800

    [PATCH] DVB: Update Steve's email address.
    Update Steve's email address.
    
    Signed-off-by: Steven Toth <stoth@hauppauge.com>
    Signed-off-by: Michael Krufky <mkrufky@m1k.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@brturbo.com.br>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/media/dvb/frontends/cx22702.h
+++ drivers/media/dvb/frontends/cx22702.h
@@ -7,7 +7,7 @@
     Copyright (C) 2001-2002 Convergence Integrated Media GmbH
 	  Holger Waechtler <holger@convergence.de>
 
-    Copyright (C) 2004 Steven Toth <steve@toth.demon.co.uk>
+    Copyright (C) 2004 Steven Toth <stoth@hauppauge.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
