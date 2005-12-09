52d0df153c987e4ad57d15f5df91848f65858e5d
Jack Morgenstein <jackm@mellanox.co.il>
2005-12-09 13:48:50 -0800

    IB/mthca: fix memory user DB table leak
    Free the memory allocated in mthca_init_user_db_tab() when releasing
    the db_tab in mthca_cleanup_user_db_tab().
    
    Signed-off-by: Jack Morgenstein <jackm@mellanox.co.il>
    Signed-off-by: Michael S. Tsirkin <mst@mellanox.co.il>
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/hw/mthca/mthca_memfree.c
+++ drivers/infiniband/hw/mthca/mthca_memfree.c
@@ -485,6 +485,8 @@ void mthca_cleanup_user_db_tab(struct mthca_dev *dev, struct mthca_uar *uar,
 			put_page(db_tab->page[i].mem.page);
 		}
 	}
+
+	kfree(db_tab);
 }
 
 int mthca_alloc_db(struct mthca_dev *dev, enum mthca_db_type type,
