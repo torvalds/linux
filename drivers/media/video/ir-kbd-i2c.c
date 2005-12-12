dd815408106f3c56c3050493dda97f9355aa4971
Mauro Carvalho Chehab <mchehab@brturbo.com.br>
2005-12-12 00:37:31 -0800

    [PATCH] V4L/DVB: (3151) I2C ID renamed to I2C_DRIVERID_INFRARED
    I2C ID renamed to I2C_DRIVERID_INFRARED
    
    Acked-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@brturbo.com.br>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/media/video/ir-kbd-i2c.c
+++ drivers/media/video/ir-kbd-i2c.c
@@ -279,7 +279,7 @@ static int ir_probe(struct i2c_adapter *adap);
 
 static struct i2c_driver driver = {
 	.name           = "ir remote kbd driver",
-	.id             = I2C_DRIVERID_I2C_IR,
+	.id             = I2C_DRIVERID_INFRARED,
 	.flags          = I2C_DF_NOTIFY,
 	.attach_adapter = ir_probe,
 	.detach_client  = ir_detach,
