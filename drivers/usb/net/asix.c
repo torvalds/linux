5e0f76c6bbc0d26cd9625876f7beeb7b002f39bf
David Hollis <dhollis@davehollis.com>
2005-12-19 13:58:38 -0500

    [PATCH] USB: asix.c - Add Linksys USB200M Rev 2 ids
    Attached patch adds device IDs for the Linksys USB200M Rev 2 device
    which uses the AX88772 chipset.
    
    Signed-off-by: David Hollis <dhollis@davehollis.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/net/asix.c
+++ drivers/usb/net/asix.c
@@ -912,6 +912,10 @@ static const struct usb_device_id	products [] = {
 	// ASIX AX88772 10/100
         USB_DEVICE (0x0b95, 0x7720),
         .driver_info = (unsigned long) &ax88772_info,
+}, {
+	// Linksys USB200M Rev 2
+	USB_DEVICE (0x13b1, 0x0018),
+	.driver_info = (unsigned long) &ax88772_info,
 },
 	{ },		// END
 };
