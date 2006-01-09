42d3b83fecb7b576d477244347982baa02fa4f44
Jim Cromie <jim.cromie@gmail.com>
2006-01-09 23:22:24 +0100

    [PATCH] hwmon: Allow sensor attributes arrays
    This patch refactors SENSOR_DEVICE_ATTR macro.  First it creates a new
    macro SENSOR_ATTR() which expands to an initialization expression, then
    it uses that in SENSOR_DEVICE_ATTR, which declares and initializes a
    struct sensor_device_attribute.
    
    IOW, SENSOR_ATTR() imitates __ATTR() in include/linux/device.h.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/hwmon-sysfs.h
+++ include/linux/hwmon-sysfs.h
@@ -27,11 +27,13 @@ struct sensor_device_attribute{
 #define to_sensor_dev_attr(_dev_attr) \
 	container_of(_dev_attr, struct sensor_device_attribute, dev_attr)
 
-#define SENSOR_DEVICE_ATTR(_name,_mode,_show,_store,_index)	\
-struct sensor_device_attribute sensor_dev_attr_##_name = {	\
-	.dev_attr =	__ATTR(_name,_mode,_show,_store),	\
-	.index =	_index,					\
-}
+#define SENSOR_ATTR(_name, _mode, _show, _store, _index)	\
+	{ .dev_attr = __ATTR(_name, _mode, _show, _store),	\
+	  .index = _index }
+
+#define SENSOR_DEVICE_ATTR(_name, _mode, _show, _store, _index)	\
+struct sensor_device_attribute sensor_dev_attr_##_name		\
+	= SENSOR_ATTR(_name, _mode, _show, _store, _index)
 
 struct sensor_device_attribute_2 {
 	struct device_attribute dev_attr;
