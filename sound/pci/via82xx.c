d0bd41e289768ff851066f1e952ae4c802c4dca7
Takashi Iwai <tiwai@suse.de>
2006-01-09 12:36:53 +0100

    [ALSA] via82xx - Add dxs_support entry
    Modules: VIA82xx driver
    
    Added a dxs_support entry for Jetway K8M8MS.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/via82xx.c
+++ sound/pci/via82xx.c
@@ -2372,6 +2372,7 @@ static int __devinit check_dxs_list(struct pci_dev *pci)
 		{ .subvendor = 0x161f, .subdevice = 0x2032, .action = VIA_DXS_48K }, /* m680x machines */
 		{ .subvendor = 0x1631, .subdevice = 0xe004, .action = VIA_DXS_ENABLE }, /* Easy Note 3174, Packard Bell */
 		{ .subvendor = 0x1695, .subdevice = 0x3005, .action = VIA_DXS_ENABLE }, /* EPoX EP-8K9A */
+		{ .subvendor = 0x16f3, .subdevice = 0x6405, .action = VIA_DXS_SRC }, /* Jetway K8M8MS */
 		{ .subvendor = 0x1849, .subdevice = 0x3059, .action = VIA_DXS_NO_VRA }, /* ASRock K7VM2 */
 		{ .subvendor = 0x1849, .subdevice = 0x9761, .action = VIA_DXS_SRC }, /* ASRock mobo(?) */
 		{ .subvendor = 0x1919, .subdevice = 0x200a, .action = VIA_DXS_NO_VRA }, /* Soltek SL-K8Tpro-939 */
