e329113ca437e44ec399b7ffe114ed36e84ccf5e
Ben Gardner <gardner.ben@gmail.com>
2006-01-09 20:51:29 -0800

    [PATCH] i386: GPIO driver for AMD CS5535/CS5536
    A simple driver for the CS5535 and CS5536 that allows a user-space program
    to manipulate GPIO pins.  The CS5535/CS5536 chips are Geode processor
    companion devices.
    
    Signed-off-by: Ben Gardner <bgardner@wabtec.com>
    Signed-off-by: Richard Knutsson <ricknu-0@student.ltu.se>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- sound/pci/cs5535audio/cs5535audio.c
+++ sound/pci/cs5535audio/cs5535audio.c
@@ -385,7 +385,7 @@ static struct pci_driver driver = {
 
 static int __init alsa_card_cs5535audio_init(void)
 {
-	return pci_module_init(&driver);
+	return pci_register_driver(&driver);
 }
 
 static void __exit alsa_card_cs5535audio_exit(void)
