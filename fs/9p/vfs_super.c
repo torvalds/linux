0d456fa4261f43433287a10fe3ec04a9818fac64
Christoph Hellwig <hch@lst.de>
2006-01-09 20:52:15 -0800

    [PATCH] 9p: remove superflous MS_NODIRATIME assignment
    MS_NOATIME implies MS_NODIRATIME
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Cc: Eric Van Hensbergen <ericvh@ericvh.myip.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/9p/vfs_super.c
+++ fs/9p/vfs_super.c
@@ -91,7 +91,7 @@ v9fs_fill_super(struct super_block *sb, struct v9fs_session_info *v9ses,
 	sb->s_op = &v9fs_super_ops;
 
 	sb->s_flags = flags | MS_ACTIVE | MS_SYNCHRONOUS | MS_DIRSYNC |
-	    MS_NODIRATIME | MS_NOATIME;
+	    MS_NOATIME;
 }
 
 /**
