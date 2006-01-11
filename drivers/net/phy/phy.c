a4d00f179fcec7065fe5742e9cebd6500886070f
Kumar Gala <galak@gate.crashing.org>
2006-01-11 11:27:33 -0800

    [PATCH] phy: Added a macro to represent the string format used to match a phy device
    Add the PHY_ID_FMT macro to ensure that the format of the id string used by a
    driver to match to its specific phy is consistent between the mdio_bus and the
    driver.
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- drivers/net/phy/phy.c
+++ drivers/net/phy/phy.c
@@ -42,7 +42,7 @@
  */
 void phy_print_status(struct phy_device *phydev)
 {
-	pr_info("%s: Link is %s", phydev->dev.bus_id,
+	pr_info("PHY: %s - Link is %s", phydev->dev.bus_id,
 			phydev->link ? "Up" : "Down");
 	if (phydev->link)
 		printk(" - %d/%s", phydev->speed,
