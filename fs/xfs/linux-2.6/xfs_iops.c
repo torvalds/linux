216d3b2acba469a9bee98a09bb957e012ba7bc25
Tim Shimmin <tes@sgi.com>
2006-01-11 15:37:38 +1100

    [XFS] take out the call to vn_mark_bad() used when acl inherit fails and it needs to back out the inode creation. Tested by xfs_tests/077.
    SGI-PV: 930841
    SGI-Modid: xfs-linux-melb:xfs-kern:24842a
    
    Signed-off-by: Tim Shimmin <tes@sgi.com>
    Signed-off-by: Nathan Scott <nathans@sgi.com>

--- fs/xfs/linux-2.6/xfs_iops.c
+++ fs/xfs/linux-2.6/xfs_iops.c
@@ -320,8 +320,6 @@ linvfs_mknod(
 				teardown.d_inode = ip = LINVFS_GET_IP(vp);
 				teardown.d_name = dentry->d_name;
 
-				vn_mark_bad(vp);
-				
 				if (S_ISDIR(mode))
 					VOP_RMDIR(dvp, &teardown, NULL, err2);
 				else
