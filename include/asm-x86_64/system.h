60917a3832547886549962ab9ee753253cf5e44c
Benjamin LaHaise <bcrl@kvack.org>
2006-01-11 22:43:15 +0100

    [PATCH] x86_64: don't save eflags in x86-64 switch_to()
    As discussed, the flags register on x86-64 is saved and restored by the
    assembly code which sets up struct pt_regs, so we do not need to save
    and restore it in the inline assembler which already informs gcc that
    we're clobbering the flags.  This patch has been sanity booted and works
    okay here.
    
    Signed-off-by: Benjamin LaHaise <benjamin.c.lahaise@intel.com>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-x86_64/system.h
+++ include/asm-x86_64/system.h
@@ -20,8 +20,8 @@
 #define __RESTORE(reg,offset) "movq (14-" #offset ")*8(%%rsp),%%" #reg "\n\t"
 
 /* frame pointer must be last for get_wchan */
-#define SAVE_CONTEXT    "pushfq ; pushq %%rbp ; movq %%rsi,%%rbp\n\t"
-#define RESTORE_CONTEXT "movq %%rbp,%%rsi ; popq %%rbp ; popfq\n\t" 
+#define SAVE_CONTEXT    "pushq %%rbp ; movq %%rsi,%%rbp\n\t"
+#define RESTORE_CONTEXT "movq %%rbp,%%rsi ; popq %%rbp\n\t"
 
 #define __EXTRA_CLOBBER  \
 	,"rcx","rbx","rdx","r8","r9","r10","r11","r12","r13","r14","r15"
