fba7b940194465ac7a8f0cdf793959fb5fbb8834
David Teigland <teigland@redhat.com>
2006-01-16 11:48:01 +0000

    [PATCH] Add fs_subsys to enable filesystems to use sysfs
    This creates an "fs" subdirectory in sysfs so that GFS2 (or any other
    filesystem, come to that) can make use of sysfs.
    
    Signed-off-by: David Teigland <teigland@redhat.com>
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1293,6 +1293,9 @@ extern int vfs_statfs(struct super_block *, struct kstatfs *);
 #define FLOCK_VERIFY_READ  1
 #define FLOCK_VERIFY_WRITE 2
 
+/* /sys/fs */
+extern struct subsystem fs_subsys;
+
 extern int locks_mandatory_locked(struct inode *);
 extern int locks_mandatory_area(int, struct inode *, struct file *, loff_t, size_t);
 
