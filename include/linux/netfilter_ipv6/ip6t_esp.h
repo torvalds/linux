e0069caede8387c585060b7e2e87729e9efcebc6
Yasuyuki Kozakai <yasuyuki.kozakai@toshiba.co.jp>
2006-01-17 02:39:19 -0800

    [NETFILTER] ip6tables: remove unused definitions
    These definitions ware used for only internal use in kernel <= 2.6.13,
    which had not introduced the unified parser of IPv6 extension header yet.
    
    Signed-off-by: Yasuyuki Kozakai <yasuyuki.kozakai@toshiba.co.jp>
    Signed-off-by: Harald Welte <laforge@netfilter.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netfilter_ipv6/ip6t_esp.h
+++ include/linux/netfilter_ipv6/ip6t_esp.h
@@ -7,15 +7,6 @@ struct ip6t_esp
 	u_int8_t  invflags;			/* Inverse flags */
 };
 
-#define MASK_HOPOPTS    128
-#define MASK_DSTOPTS    64
-#define MASK_ROUTING    32
-#define MASK_FRAGMENT   16
-#define MASK_AH         8
-#define MASK_ESP        4
-#define MASK_NONE       2
-#define MASK_PROTO      1
-
 /* Values for "invflags" field in struct ip6t_esp. */
 #define IP6T_ESP_INV_SPI		0x01	/* Invert the sense of spi. */
 #define IP6T_ESP_INV_MASK	0x01	/* All possible flags. */
