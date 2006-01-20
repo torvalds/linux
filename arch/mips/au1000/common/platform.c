76fa9a240de4294a097235c9ddd470c21eb3449e
Jordan Crouse <jordan.crouse@amd.com>
2006-01-20 14:06:09 -0800

    [PATCH] USB: EHCI for AU1200
    ALCHEMY:  Add EHCI support for AU1200
    
    Updated by removing the OHCI support
    
    Signed-off-by: Jordan Crouse <jordan.crouse@amd.com>
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- arch/mips/au1000/common/platform.c
+++ arch/mips/au1000/common/platform.c
@@ -278,9 +278,7 @@ static struct platform_device *au1xxx_platform_devices[] __initdata = {
 	&au1100_lcd_device,
 #endif
 #ifdef CONFIG_SOC_AU1200
-#if 0	/* fixme */
 	&au1xxx_usb_ehci_device,
-#endif
 	&au1xxx_usb_gdt_device,
 	&au1xxx_usb_otg_device,
 	&au1200_lcd_device,
