5457f38e01ae2d296ff49db42254679018f13fa9
Dave Airlie <airlied@starflyer.(none)>
2006-01-25 14:34:33 +1100

    drm: add i945GM PCI ID
    From: Charles F. Johnson <charles.f.johnson@intel.com>
    Signed-off-by: Dave Airlie <airlied@linux.ie>

--- drivers/char/drm/drm_pciids.h
+++ drivers/char/drm/drm_pciids.h
@@ -242,5 +242,6 @@
 	{0x8086, 0x2582, PCI_ANY_ID, PCI_ANY_ID, 0, 0, 0}, \
 	{0x8086, 0x2592, PCI_ANY_ID, PCI_ANY_ID, 0, 0, 0}, \
 	{0x8086, 0x2772, PCI_ANY_ID, PCI_ANY_ID, 0, 0, 0}, \
+	{0x8086, 0x27a2, PCI_ANY_ID, PCI_ANY_ID, 0, 0, 0}, \
 	{0, 0, 0}
 
