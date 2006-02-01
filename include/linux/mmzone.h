79046ae07ae21245520ca0aab985ee6678a879f8
Andy Whitcroft <apw@shadowen.org>
2006-02-01 03:05:26 -0800

    [PATCH] GFP_ZONETYPES: add commentry on how to calculate
    GFP_ZONETYPES define using GFP_ZONEMASK and add commentry
    
    Add commentry explaining the optimisation that we can apply to GFP_ZONETYPES
    when the leftmost bit is a 'loaner', it can only be set in isolation.
    
    Signed-off-by: Andy Whitcroft <apw@shadowen.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -91,6 +91,14 @@ struct per_cpu_pageset {
  * be 8 (2 ** 3) zonelists.  GFP_ZONETYPES defines the number of possible
  * combinations of zone modifiers in "zone modifier space".
  *
+ * As an optimisation any zone modifier bits which are only valid when
+ * no other zone modifier bits are set (loners) should be placed in
+ * the highest order bits of this field.  This allows us to reduce the
+ * extent of the zonelists thus saving space.  For example in the case
+ * of three zone modifier bits, we could require up to eight zonelists.
+ * If the left most zone modifier is a "loner" then the highest valid
+ * zonelist would be four allowing us to allocate only five zonelists.
+ *
  * NOTE! Make sure this matches the zones in <linux/gfp.h>
  */
 #define GFP_ZONEMASK	0x07
