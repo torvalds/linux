159fe8a8358c7012d4f1fdacfcf69009ea15b52e
Komuro <komurojun-mbn@nifty.com>
2006-02-05 09:56:59 +0100

    [PATCH] pcmcia: remove wrong comment in ciscode.h
    Remove misleading comment.
    
    Signed-off-by: Komuro <komurojun-mbn@nifty.com>
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- include/pcmcia/ciscode.h
+++ include/pcmcia/ciscode.h
@@ -1,5 +1,5 @@
 /*
- * ciscode.h -- Definitions for bulk memory services
+ * ciscode.h
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
