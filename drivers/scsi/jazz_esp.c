0320503dce208c03f296a09d9a18a992ab88cbd5
Ralf Baechle <ralf@linux-mips.org>
2006-02-09 11:15:49 -0500

    [SCSI] jazz_esp: Delete useless prototype
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/jazz_esp.c
+++ drivers/scsi/jazz_esp.c
@@ -52,7 +52,6 @@ static volatile unsigned char cmd_buffer[16];
 				 * via PIO.
 				 */
 
-int jazz_esp_detect(struct scsi_host_template *tpnt);
 static int jazz_esp_release(struct Scsi_Host *shost)
 {
 	if (shost->irq)
