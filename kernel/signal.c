c70d3d703ad94727dab2a3664aeee33d71e00715
Oleg Nesterov <oleg@tv-sign.ru>
2006-02-09 22:41:41 +0300

    [PATCH] sys_signal: initialize ->sa_mask
    Pointed out by Linus Torvalds.
    
    sys_signal() forgets to initialize ->sa_mask.
    
    ( I suspect arch/ia64/ia32/ia32_signal.c:sys32_signal()
      also needs this fix )
    
    Signed-off-by: Oleg Nesterov <oleg@tv-sign.ru>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/signal.c
+++ kernel/signal.c
@@ -2702,6 +2702,7 @@ sys_signal(int sig, __sighandler_t handler)
 
 	new_sa.sa.sa_handler = handler;
 	new_sa.sa.sa_flags = SA_ONESHOT | SA_NOMASK;
+	sigemptyset(&new_sa.sa.sa_mask);
 
 	ret = do_sigaction(sig, &new_sa, &old_sa);
 
