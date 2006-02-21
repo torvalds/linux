9b0f8b040acd8dfd23860754c0d09ff4f44e2cbc
Christoph Lameter <clameter@engr.sgi.com>
2006-02-20 18:27:52 -0800

    [PATCH] Terminate process that fails on a constrained allocation
    Some allocations are restricted to a limited set of nodes (due to memory
    policies or cpuset constraints).  If the page allocator is not able to find
    enough memory then that does not mean that overall system memory is low.
    
    In particular going postal and more or less randomly shooting at processes
    is not likely going to help the situation but may just lead to suicide (the
    whole system coming down).
    
    It is better to signal to the process that no memory exists given the
    constraints that the process (or the configuration of the process) has
    placed on the allocation behavior.  The process may be killed but then the
    sysadmin or developer can investigate the situation.  The solution is
    similar to what we do when running out of hugepages.
    
    This patch adds a check before we kill processes.  At that point
    performance considerations do not matter much so we just scan the zonelist
    and reconstruct a list of nodes.  If the list of nodes does not contain all
    online nodes then this is a constrained allocation and we should kill the
    current process.
    
    Signed-off-by: Christoph Lameter <clameter@sgi.com>
    Cc: Nick Piggin <nickpiggin@yahoo.com.au>
    Cc: Andi Kleen <ak@muc.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/char/sysrq.c
+++ drivers/char/sysrq.c
@@ -243,7 +243,7 @@ static struct sysrq_key_op sysrq_term_op = {
 
 static void moom_callback(void *ignored)
 {
-	out_of_memory(GFP_KERNEL, 0);
+	out_of_memory(&NODE_DATA(0)->node_zonelists[ZONE_NORMAL], GFP_KERNEL, 0);
 }
 
 static DECLARE_WORK(moom_work, moom_callback, NULL);
