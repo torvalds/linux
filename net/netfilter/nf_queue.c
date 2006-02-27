e121e9ecb08c3a9843243f461290869ff08be900
Patrick McHardy <kaber@trash.net>
2006-02-27 13:03:39 -0800

    [NETFILTER]: nf_queue: remove unnecessary check for outfn
    The only point of registering a queue handler is to provide an outfn,
    so there is no need to check for it.
    
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netfilter/nf_queue.c
+++ net/netfilter/nf_queue.c
@@ -120,7 +120,7 @@ int nf_queue(struct sk_buff **skb,
 
 	/* QUEUE == DROP if noone is waiting, to be safe. */
 	read_lock(&queue_handler_lock);
-	if (!queue_handler[pf] || !queue_handler[pf]->outfn) {
+	if (!queue_handler[pf]) {
 		read_unlock(&queue_handler_lock);
 		kfree_skb(*skb);
 		return 1;
