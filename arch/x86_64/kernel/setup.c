d51761233d9e3be4cdf10f7482a50463bbd78c78
Brian Magnuson <magnuson@rcn.com>
2006-02-27 04:02:04 +0100

    [PATCH] fix build on x86_64 with !CONFIG_HOTPLUG_CPU
    The commit e2c0388866dc12bef56b178b958f9b778fe6c687 added
    setup_additional_cpus to setup.c but this is only defined if
    CONFIG_HOTPLUG_CPU is set.  This patch changes the #ifdef to reflect that.
    
    Signed-off-by: Brian Magnuson <magnuson@rcn.com>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/x86_64/kernel/setup.c
+++ arch/x86_64/kernel/setup.c
@@ -424,7 +424,7 @@ static __init void parse_cmdline_early (char ** cmdline_p)
 			elfcorehdr_addr = memparse(from+11, &from);
 #endif
 
-#ifdef CONFIG_SMP
+#ifdef CONFIG_HOTPLUG_CPU
 		else if (!memcmp(from, "additional_cpus=", 16))
 			setup_additional_cpus(from+16);
 #endif
