dadaff2db46aae917ccdbceb2cd2ecac75767435
Mauro Carvalho Chehab <mchehab@infradead.org>
2006-02-27 00:09:41 -0300

    V4L/DVB (3392a): XC3028 code removed from -git versions
    - Current xc3028 support is still experimental, requiring more work to be
      sent to mainstream. It will be kept only at mercurial tree on
      http://linuxtv.org/hg until fixed.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/em28xx/em28xx-cards.c
+++ drivers/media/video/em28xx/em28xx-cards.c
@@ -154,76 +154,6 @@ struct em28xx_board em28xx_boards[] = {
 			.amux     = 1,
 		}},
 	},
-#ifdef CONFIG_XC3028
-	[EM2880_BOARD_HAUPPAUGE_WINTV_HVR_900] = {
-		.name         = "Hauppauge WinTV HVR 900",
-		.vchannels    = 3,
-		.norm         = VIDEO_MODE_PAL,
-		.tda9887_conf = TDA9887_PRESENT,
-		.tuner_type   = TUNER_XCEIVE_XC3028,
-		.has_tuner    = 1,
-		.decoder      = EM28XX_TVP5150,
-		.input          = {{
-			.type     = EM28XX_VMUX_COMPOSITE1,
-			.vmux     = 2,
-			.amux     = 1,
-		},{
-			.type     = EM28XX_VMUX_TELEVISION,
-			.vmux     = 0,
-			.amux     = 0,
-		},{
-			.type     = EM28XX_VMUX_SVIDEO,
-			.vmux     = 9,
-			.amux     = 1,
-		}},
-	},
-	[EM2880_BOARD_TERRATEC_HYBRID_XS] = {
-		.name         = "Terratec Hybrid XS",
-		.vchannels    = 3,
-		.norm         = VIDEO_MODE_PAL,
-		.tda9887_conf = TDA9887_PRESENT,
-		.has_tuner    = 1,
-		.tuner_type   = TUNER_XCEIVE_XC3028,
-		.decoder      = EM28XX_TVP5150,
-		.input          = {{
-			.type     = EM28XX_VMUX_TELEVISION,
-			.vmux     = 0,
-			.amux     = 0,
-		},{
-			.type     = EM28XX_VMUX_COMPOSITE1,
-			.vmux     = 2,
-			.amux     = 1,
-		},{
-			.type     = EM28XX_VMUX_SVIDEO,
-			.vmux     = 9,
-			.amux     = 1,
-		}},
-	},
-	/* maybe there's a reason behind it why Terratec sells the Hybrid XS as Prodigy XS with a
-	 * different PID, let's keep it separated for now maybe we'll need it lateron */
-	[EM2880_BOARD_TERRATEC_PRODIGY_XS] = {
-		.name         = "Terratec Prodigy XS",
-		.vchannels    = 3,
-		.norm         = VIDEO_MODE_PAL,
-		.tda9887_conf = TDA9887_PRESENT,
-		.has_tuner    = 1,
-		.tuner_type   = TUNER_XCEIVE_XC3028,
-		.decoder      = EM28XX_TVP5150,
-		.input          = {{
-			.type     = EM28XX_VMUX_TELEVISION,
-			.vmux     = 0,
-			.amux     = 0,
-		},{
-			.type     = EM28XX_VMUX_COMPOSITE1,
-			.vmux     = 2,
-			.amux     = 1,
-		},{
-			.type     = EM28XX_VMUX_SVIDEO,
-			.vmux     = 9,
-			.amux     = 1,
-		}},
-	},
-#endif
 	[EM2820_BOARD_MSI_VOX_USB_2] = {
 		.name		= "MSI VOX USB 2.0",
 		.vchannels	= 3,
@@ -342,11 +272,6 @@ struct usb_device_id em28xx_id_table [] = {
 	{ USB_DEVICE(0x2304, 0x0208), .driver_info = EM2820_BOARD_PINNACLE_USB_2 },
 	{ USB_DEVICE(0x2040, 0x4200), .driver_info = EM2820_BOARD_HAUPPAUGE_WINTV_USB_2 },
 	{ USB_DEVICE(0x2304, 0x0207), .driver_info = EM2820_BOARD_PINNACLE_DVC_90 },
-#ifdef CONFIG_XC3028
-	{ USB_DEVICE(0x2040, 0x6500), .driver_info = EM2880_BOARD_HAUPPAUGE_WINTV_HVR_900 },
-	{ USB_DEVICE(0x0ccd, 0x0042), .driver_info = EM2880_BOARD_TERRATEC_HYBRID_XS },
-	{ USB_DEVICE(0x0ccd, 0x0047), .driver_info = EM2880_BOARD_TERRATEC_PRODIGY_XS },
-#endif
 	{ },
 };
 
