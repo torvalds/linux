2c906ae67b5b2fc3585230b16406400a363b42e4
Dave Jones <davej@redhat.com>
2006-02-28 00:36:32 -0500

    [CPUFREQ] Silence powernow-k8 warning on k7's.
    Signed-off-by: Dave Jones <davej@redhat.com>

--- arch/i386/kernel/cpu/cpufreq/powernow-k8.c
+++ arch/i386/kernel/cpu/cpufreq/powernow-k8.c
@@ -474,8 +474,10 @@ static int check_supported_cpu(unsigned int cpu)
 		goto out;
 
 	eax = cpuid_eax(CPUID_PROCESSOR_SIGNATURE);
+	if ((eax & CPUID_XFAM) != CPUID_XFAM_K8)
+		goto out;
+
 	if (((eax & CPUID_USE_XFAM_XMOD) != CPUID_USE_XFAM_XMOD) ||
-	    ((eax & CPUID_XFAM) != CPUID_XFAM_K8) ||
 	    ((eax & CPUID_XMOD) > CPUID_XMOD_REV_G)) {
 		printk(KERN_INFO PFX "Processor cpuid %x not supported\n", eax);
 		goto out;
