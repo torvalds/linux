ac57b84984859f3e1d567c031556d3de872c1a91
Alexey Dobriyan <adobriyan@gmail.com>
2006-03-06 13:21:30 +0100

    [ALSA] vx - Fix memory leak on error path
    Modules: Digigram VX core
    
    Noticed by Eric Sesterhenn on kernel-janitors@
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/drivers/vx/vx_pcm.c
+++ sound/drivers/vx/vx_pcm.c
@@ -1253,9 +1253,13 @@ static int vx_init_audio_io(struct vx_core *chip)
 
 	/* allocate pipes */
 	chip->playback_pipes = kmalloc(sizeof(struct vx_pipe *) * chip->audio_outs, GFP_KERNEL);
-	chip->capture_pipes = kmalloc(sizeof(struct vx_pipe *) * chip->audio_ins, GFP_KERNEL);
-	if (! chip->playback_pipes || ! chip->capture_pipes)
+	if (!chip->playback_pipes)
 		return -ENOMEM;
+	chip->capture_pipes = kmalloc(sizeof(struct vx_pipe *) * chip->audio_ins, GFP_KERNEL);
+	if (!chip->capture_pipes) {
+		kfree(chip->playback_pipes);
+		return -ENOMEM;
+	}
 
 	memset(chip->playback_pipes, 0, sizeof(struct vx_pipe *) * chip->audio_outs);
 	memset(chip->capture_pipes, 0, sizeof(struct vx_pipe *) * chip->audio_ins);
