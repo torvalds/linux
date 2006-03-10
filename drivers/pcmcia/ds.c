b2f51a1cb4e1d72061d7a3f7905affdb7d074fec
Hugh Dickins <hugh@veritas.com>
2006-03-10 21:15:19 +0000

    [PATCH] pcmcia: fix pcmcia_device_remove oops
    Fix pcmcia_device_remove NULL pointer dereference at shutdown.
    
    Signed-off-by: Hugh Dickins <hugh@veritas.com>
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- drivers/pcmcia/ds.c
+++ drivers/pcmcia/ds.c
@@ -464,7 +464,7 @@ static int pcmcia_device_remove(struct device * dev)
 	 * all devices
 	 */
 	did = (struct pcmcia_device_id *) p_dev->dev.driver_data;
-	if ((did->match_flags & PCMCIA_DEV_ID_MATCH_DEVICE_NO) &&
+	if (did && (did->match_flags & PCMCIA_DEV_ID_MATCH_DEVICE_NO) &&
 	    (p_dev->socket->device_count != 0) &&
 	    (p_dev->device_no == 0))
 		pcmcia_card_remove(p_dev->socket, p_dev);
