e0e8eb54d8ae0c4cfd1d297f6351b08a7f635c5f
Eric W. Biederman <ebiederm@xmission.com>
2006-03-16 10:31:38 -0700

    [PATCH] unshare: Use rcu_assign_pointer when setting sighand
    The sighand pointer only needs the rcu_read_lock on the
    read side.  So only depending on task_lock protection
    when setting this pointer is not enough.  We also need
    a memory barrier to ensure the initialization is seen first.
    
    Use rcu_assign_pointer as it does this for us, and clearly
    documents that we are setting an rcu readable pointer.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Acked-by: Paul E. McKenney <paulmck@us.ibm.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1569,7 +1569,7 @@ asmlinkage long sys_unshare(unsigned long unshare_flags)
 
 		if (new_sigh) {
 			sigh = current->sighand;
-			current->sighand = new_sigh;
+			rcu_assign_pointer(current->sighand, new_sigh);
 			new_sigh = sigh;
 		}
 
