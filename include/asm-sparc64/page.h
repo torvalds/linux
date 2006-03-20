f6b83f070e9b7ad9075f7cc5646260e56c7d0219
David S. Miller <davem@davemloft.net>
2006-03-20 01:17:17 -0800

    [SPARC64]: Fix 2 bugs in huge page support.
    1) huge_pte_offset() did not check the page table hierarchy
       elements as being empty correctly, resulting in an OOPS
    
    2) Need platform specific hugetlb_get_unmapped_area() to handle
       the top-down vs. bottom-up address space allocation strategies.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc64/page.h
+++ include/asm-sparc64/page.h
@@ -104,6 +104,7 @@ typedef unsigned long pgprot_t;
 #define HUGETLB_PAGE_ORDER	(HPAGE_SHIFT - PAGE_SHIFT)
 #define ARCH_HAS_SETCLEAR_HUGE_PTE
 #define ARCH_HAS_HUGETLB_PREFAULT_HOOK
+#define HAVE_ARCH_HUGETLB_UNMAPPED_AREA
 #endif
 
 #define TASK_UNMAPPED_BASE	(test_thread_flag(TIF_32BIT) ? \
