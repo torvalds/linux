096455a22acac06fb6d0d75f276170ab72d55ba6
J. Bruce Fields <bfields@fieldses.org>
2006-03-20 23:23:42 -0500

    NFSv4: Dont list system.nfs4_acl for filesystems that don't support it.
    Thanks to Frank Filz for pointing out that we list system.nfs4_acl extended
    attribute even on filesystems where we don't actually support nfs4_acl.
    This is inconsistent with the e.g. ext3 POSIX ACL behaviour, and seems to
    annoy cp.
    
    Signed-off-by: J. Bruce Fields <bfields@citi.umich.edu>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -3559,6 +3559,8 @@ ssize_t nfs4_listxattr(struct dentry *dentry, char *buf, size_t buflen)
 {
 	size_t len = strlen(XATTR_NAME_NFSV4_ACL) + 1;
 
+	if (!nfs4_server_supports_acls(NFS_SERVER(dentry->d_inode)))
+		return 0;
 	if (buf && buflen < len)
 		return -ERANGE;
 	if (buf)
