73a6c6306876c83ed992f1b15069a0ee9b3fa73b
Andrew Morton <akpm@osdl.org>
2006-03-20 22:33:41 -0800

    [IRDA]: remove MODULE_PARM()
    MODULE_PARM() is deprecated and is about to go away altogether.
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/irda/toim3232-sir.c
+++ drivers/net/irda/toim3232-sir.c
@@ -125,18 +125,18 @@
 
 #include "sir-dev.h"
 
-MODULE_PARM(toim3232delay, "i");
-MODULE_PARM_DESC(toim3232delay, "toim3232 dongle write complete delay");
 static int toim3232delay = 150;	/* default is 150 ms */
+module_param(toim3232delay, int, 0);
+MODULE_PARM_DESC(toim3232delay, "toim3232 dongle write complete delay");
 
 #if 0
-MODULE_PARM(toim3232flipdtr, "i");
-MODULE_PARM_DESC(toim3232flipdtr, "toim3232 dongle invert DTR (Reset)");
 static int toim3232flipdtr = 0;	/* default is DTR high to reset */
+module_param(toim3232flipdtr, int, 0);
+MODULE_PARM_DESC(toim3232flipdtr, "toim3232 dongle invert DTR (Reset)");
 
-MODULE_PARM(toim3232fliptrs, "i");
-MODULE_PARM_DESC(toim3232fliprts, "toim3232 dongle invert RTS (BR/D)");
 static int toim3232fliprts = 0;	/* default is RTS high for baud change */
+module_param(toim3232fliptrs, int, 0);
+MODULE_PARM_DESC(toim3232fliprts, "toim3232 dongle invert RTS (BR/D)");
 #endif
 
 static int toim3232_open(struct sir_dev *);
