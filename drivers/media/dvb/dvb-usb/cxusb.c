afdebc94ea86168973102fa77128764ac034d569
Michael Krufky <mkrufky@linuxtv.org>
2006-03-23 00:15:55 -0300

    V4L/DVB (3573): Cxusb: remove FIXME: comment in bluebird_patch_dvico_firmware_download
    Removed the FIXME comment from bluebird_patch_dvico_firmware_download:
     /* FIXME: are we allowed to change the fw-data ? */
    Yes, we are allowed. DViCO's Windows driver also does the same thing.
    A single firmware image is used to support all of the bluebird boxes.
    The firmware sets all devices to PID: d700. Instead of using that, the
    driver replaces the d700 with the cold PID+1 before the download.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-usb/cxusb.c
+++ drivers/media/dvb/dvb-usb/cxusb.c
@@ -411,7 +411,6 @@ static int bluebird_patch_dvico_firmware_download(struct usb_device *udev, const
 	if (fw->data[BLUEBIRD_01_ID_OFFSET] == (USB_VID_DVICO & 0xff) &&
 	    fw->data[BLUEBIRD_01_ID_OFFSET + 1] == USB_VID_DVICO >> 8) {
 
-		/* FIXME: are we allowed to change the fw-data ? */
 		fw->data[BLUEBIRD_01_ID_OFFSET + 2] = udev->descriptor.idProduct + 1;
 		fw->data[BLUEBIRD_01_ID_OFFSET + 3] = udev->descriptor.idProduct >> 8;
 
