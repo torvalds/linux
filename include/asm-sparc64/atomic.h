0b2fcfdb8b4e7e379192f24ea2203163ddf5df1d
Nick Piggin <npiggin@suse.de>
2006-03-23 03:01:02 -0800

    [PATCH] atomic: add_unless cmpxchg optimise
    Without branch hints, the very unlikely chance of the loop repeating due to
    cmpxchg failure is unrolled with gcc-4 that I have tested.
    
    Improve this for architectures with a native cas/cmpxchg.  llsc archs
    should try to implement this natively.
    
    Signed-off-by: Nick Piggin <npiggin@suse.de>
    Cc: Andi Kleen <ak@muc.de>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Roman Zippel <zippel@linux-m68k.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-sparc64/atomic.h
+++ include/asm-sparc64/atomic.h
@@ -78,9 +78,15 @@ extern int atomic64_sub_ret(int, atomic64_t *);
 ({								\
 	int c, old;						\
 	c = atomic_read(v);					\
-	while (c != (u) && (old = atomic_cmpxchg((v), c, c + (a))) != c) \
+	for (;;) {						\
+		if (unlikely(c == (u)))				\
+			break;					\
+		old = atomic_cmpxchg((v), c, c + (a));		\
+		if (likely(old == c))				\
+			break;					\
 		c = old;					\
-	c != (u);						\
+	}							\
+	likely(c != (u));					\
 })
 #define atomic_inc_not_zero(v) atomic_add_unless((v), 1, 0)
 
