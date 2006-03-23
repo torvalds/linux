7d420896256a4bffe44202f282fbdd4c74d779a8
Lennert Buytenhek <buytenh@wantstofly.org>
2006-03-23 12:59:08 +0000

    [ARM] 3391/1: use PLAT8250_DEV_PLATFORM{,1} for platform device id instead of 0/1
    Patch from Lennert Buytenhek
    
    This patch changes iop3xx and omap2 and to use PLAT8250_DEV_PLATFORM{,1}
    as platform device id instead of just hardcoding 0/1 directly.
    
    Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-omap2/serial.c
+++ arch/arm/mach-omap2/serial.c
@@ -167,7 +167,7 @@ void __init omap_serial_init()
 
 static struct platform_device serial_device = {
 	.name			= "serial8250",
-	.id			= 0,
+	.id			= PLAT8250_DEV_PLATFORM,
 	.dev			= {
 		.platform_data	= serial_platform_data,
 	},
