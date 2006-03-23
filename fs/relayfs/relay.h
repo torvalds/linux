b86ff981a8252d83d6a7719ae09f3a05307e3592
Jens Axboe <axboe@suse.de>
2006-03-23 19:56:55 +0100

    [PATCH] relay: migrate from relayfs to a generic relay API
    Original patch from Paul Mundt, sysfs parts removed by me since they
    were broken.
    
    Signed-off-by: Jens Axboe <axboe@suse.de>

--- fs/relayfs/relay.h
+++
@@ -1,8 +0,0 @@
-#ifndef _RELAY_H
-#define _RELAY_H
-
-extern int relayfs_remove(struct dentry *dentry);
-extern int relay_buf_empty(struct rchan_buf *buf);
-extern void relay_destroy_channel(struct kref *kref);
-
-#endif /* _RELAY_H */
