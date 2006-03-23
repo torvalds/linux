a20c522498330ba0f4970a9bcd11890312277ae2
Hans Verkuil <hverkuil@xs4all.nl>
2006-03-23 19:37:58 -0300

    V4L/DVB (3598): Add bit algorithm adapter for the Conexant CX2341X boards.
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- include/linux/i2c-id.h
+++ include/linux/i2c-id.h
@@ -184,6 +184,7 @@
 #define I2C_HW_B_SAVAGE		0x01001d /* savage framebuffer driver */
 #define I2C_HW_B_RADEON		0x01001e /* radeon framebuffer driver */
 #define I2C_HW_B_EM28XX		0x01001f /* em28xx video capture cards */
+#define I2C_HW_B_CX2341X	0x010020 /* Conexant CX2341X MPEG encoder cards */
 
 /* --- PCF 8584 based algorithms					*/
 #define I2C_HW_P_LP		0x020000 /* Parallel port interface */
