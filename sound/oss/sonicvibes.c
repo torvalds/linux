20548664aa2d98ba0ee3dde4482b0a96b9273b6f
Richard Knutsson <ricknu-0@student.ltu.se>
2006-03-26 01:39:00 -0800

    [PATCH] oss/sonicvibes.c defines its own hweight32
    sound/oss/sonicvibes.c:421: error: static declaration of hweight32 follows non-static declaration
    include/asm-generic/bitops/hweight.h:6: error: previous declaration of hweight32 was here
    
    Signed-off-by: Richard Knutsson <ricknu-0@student.ltu.se>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- sound/oss/sonicvibes.c
+++ sound/oss/sonicvibes.c
@@ -407,24 +407,6 @@ static inline unsigned ld2(unsigned int x)
 	return r;
 }
 
-/*
- * hweightN: returns the hamming weight (i.e. the number
- * of bits set) of a N-bit word
- */
-
-#ifdef hweight32
-#undef hweight32
-#endif
-
-static inline unsigned int hweight32(unsigned int w)
-{
-        unsigned int res = (w & 0x55555555) + ((w >> 1) & 0x55555555);
-        res = (res & 0x33333333) + ((res >> 2) & 0x33333333);
-        res = (res & 0x0F0F0F0F) + ((res >> 4) & 0x0F0F0F0F);
-        res = (res & 0x00FF00FF) + ((res >> 8) & 0x00FF00FF);
-        return (res & 0x0000FFFF) + ((res >> 16) & 0x0000FFFF);
-}
-
 /* --------------------------------------------------------------------- */
 
 /*
