7a1e524a5fe6c5bf9dd4488e946fa835fda8c3d9
Andrew Morton <akpm@osdl.org>
2006-03-28 01:56:22 -0800

    [PATCH] alpha: make poll flags the same as other architectures
    Renumber the recently-added POLLREMOVE and POLLRDHUP to line up with the other
    architectures.
    
    Cc: Davide Libenzi <davidel@xmailserver.org>
    Cc: Ulrich Drepper <drepper@redhat.com>
    Cc: Ivan Kokshaysky <ink@jurassic.park.msu.ru>
    Cc: Richard Henderson <rth@twiddle.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-alpha/poll.h
+++ include/asm-alpha/poll.h
@@ -12,8 +12,8 @@
 #define POLLWRNORM	(1 << 8)
 #define POLLWRBAND	(1 << 9)
 #define POLLMSG		(1 << 10)
-#define POLLREMOVE	(1 << 11)
-#define POLLRDHUP       (1 << 12)
+#define POLLREMOVE	(1 << 12)
+#define POLLRDHUP       (1 << 13)
 
 
 struct pollfd {
