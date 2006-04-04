8bf4b8a1083694d5aac292f92705ddd3aec29be6
Herbert Xu <herbert@gondor.apana.org.au>
2006-04-04 12:51:05 -0700

    [IPSEC]: Check x->encap before dereferencing it
    We need to dereference x->encap before dereferencing it for encap_type.
    If it's absent then the encap_type is zero.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/xfrm4_input.c
+++ net/ipv4/xfrm4_input.c
@@ -90,7 +90,7 @@ int xfrm4_rcv_encap(struct sk_buff *skb, __u16 encap_type)
 		if (unlikely(x->km.state != XFRM_STATE_VALID))
 			goto drop_unlock;
 
-		if (x->encap->encap_type != encap_type)
+		if ((x->encap ? x->encap->encap_type : 0) != encap_type)
 			goto drop_unlock;
 
 		if (x->props.replay_window && xfrm_replay_check(x, seq))
