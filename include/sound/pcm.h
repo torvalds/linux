12831c15f35dcc9f55fa63d50fd4892c7c6a0a1c
Adrian Bunk <bunk@stusta.de>
2006-04-11 11:12:46 +0200

    [ALSA] sound/core/pcm.c: make snd_pcm_format_name() static
    Modules: PCM Midlevel
    
    This patch makes the needlessly global snd_pcm_format_name() static.
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/sound/pcm.h
+++ include/sound/pcm.h
@@ -898,7 +898,6 @@ ssize_t snd_pcm_format_size(snd_pcm_format_t format, size_t samples);
 const unsigned char *snd_pcm_format_silence_64(snd_pcm_format_t format);
 int snd_pcm_format_set_silence(snd_pcm_format_t format, void *buf, unsigned int frames);
 snd_pcm_format_t snd_pcm_build_linear_format(int width, int unsignd, int big_endian);
-const char *snd_pcm_format_name(snd_pcm_format_t format);
 
 void snd_pcm_set_ops(struct snd_pcm * pcm, int direction, struct snd_pcm_ops *ops);
 void snd_pcm_set_sync(struct snd_pcm_substream *substream);
