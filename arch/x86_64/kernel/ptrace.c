97a4d00388db4f0bfa37425b31d7d9751ea649db
Andi Kleen <ak@suse.de>
2006-04-11 12:54:39 +0200

    [PATCH] x86_64: Remove check for canonical RIP
    As pointed out by Linus it is useless now because entry.S should
    handle it correctly in all cases.
    
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/x86_64/kernel/ptrace.c
+++ arch/x86_64/kernel/ptrace.c
@@ -274,11 +274,6 @@ static int putreg(struct task_struct *child,
 				return -EIO;
 			value &= 0xffff;
 			break;
-		case offsetof(struct user_regs_struct, rip):
-			/* Check if the new RIP address is canonical */
-			if (value >= TASK_SIZE_OF(child))
-				return -EIO;
-			break;
 	}
 	put_stack_long(child, regno - sizeof(struct pt_regs), value);
 	return 0;
