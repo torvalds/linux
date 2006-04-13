473f54276a814810d4a06616b223a7d6829e1ad5
Andrew de Quincey <adq_dvb@lidskialf.net>
2006-04-13 17:29:07 -0300

    V4L/DVB (3805): Fix KNC1 card frontend detection
    Since I reordered the CI/frontend detection, it turns out the frontend needs
    to have a GPIO set to power it on; otherwise frontend init fails.
    
    Signed-off-by: Andrew de Quincey <adq_dvb@lidskialf.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/ttpci/budget-av.c
+++ drivers/media/dvb/ttpci/budget-av.c
@@ -1017,12 +1017,14 @@ static void frontend_init(struct budget_av *budget_av)
 	struct saa7146_dev * saa = budget_av->budget.dev;
 	struct dvb_frontend * fe = NULL;
 
+	/* Enable / PowerON Frontend */
+	saa7146_setgpio(saa, 0, SAA7146_GPIO_OUTLO);
+
+	/* additional setup necessary for the PLUS cards */
 	switch (saa->pci->subsystem_device) {
 		case SUBID_DVBS_KNC1_PLUS:
 		case SUBID_DVBC_KNC1_PLUS:
 		case SUBID_DVBT_KNC1_PLUS:
-			// Enable / PowerON Frontend
-			saa7146_setgpio(saa, 0, SAA7146_GPIO_OUTLO);
 			saa7146_setgpio(saa, 3, SAA7146_GPIO_OUTHI);
 			break;
 	}
