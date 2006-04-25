4d17ffda331ba6030bb8c233c73d6a87954d8ea7
Kay Sievers <kay.sievers@vrfy.org>
2006-04-25 15:37:26 +0200

    [PATCH] Kobject: fix build error
    This fixes a build error for various odd combinations of CONFIG_HOTPLUG
    and CONFIG_NET.
    
    Signed-off-by: Kay Sievers <kay.sievers@vrfy.org>
    Cc: Nigel Cunningham <ncunningham@cyclades.com>
    Cc: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/kobject.h
+++ include/linux/kobject.h
@@ -259,7 +259,7 @@ struct subsys_attribute {
 extern int subsys_create_file(struct subsystem * , struct subsys_attribute *);
 extern void subsys_remove_file(struct subsystem * , struct subsys_attribute *);
 
-#if defined(CONFIG_HOTPLUG) && defined(CONFIG_NET)
+#if defined(CONFIG_HOTPLUG)
 void kobject_uevent(struct kobject *kobj, enum kobject_action action);
 
 int add_uevent_var(char **envp, int num_envp, int *cur_index,
