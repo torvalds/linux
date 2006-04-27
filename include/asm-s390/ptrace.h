274f5946dcb7c970455067be9e13a22174787592
David Woodhouse <dwmw2@infradead.org>
2006-04-27 04:47:10 +0100

    Don't include implementation details from asm-s390/ptrace.h and page.h
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- include/asm-s390/ptrace.h
+++ include/asm-s390/ptrace.h
@@ -183,8 +183,6 @@
 #ifndef __ASSEMBLY__
 #include <linux/stddef.h>
 #include <linux/types.h>
-#include <asm/setup.h>
-#include <asm/page.h>
 
 typedef union
 {
@@ -300,6 +298,9 @@ typedef struct
 } s390_regs;
 
 #ifdef __KERNEL__
+#include <asm/setup.h>
+#include <asm/page.h>
+
 /*
  * The pt_regs struct defines the way the registers are stored on
  * the stack during a system call.
