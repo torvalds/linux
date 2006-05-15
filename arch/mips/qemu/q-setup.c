c583122c26ad04bb2379933dc5acc8b9479d6c67
Thiemo Seufer <ths@networkno.de>
2006-05-15 18:59:34 +0100

    [MIPS] Qemu system shutdown support
    Signed-off-by: Thiemo Seufer <ths@networkno.de>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/qemu/q-setup.c
+++ arch/mips/qemu/q-setup.c
@@ -2,6 +2,8 @@
 #include <asm/io.h>
 #include <asm/time.h>
 
+extern void qemu_reboot_setup(void);
+
 #define QEMU_PORT_BASE 0xb4000000
 
 const char *get_system_type(void)
@@ -22,4 +24,6 @@ void __init plat_setup(void)
 {
 	set_io_port_base(QEMU_PORT_BASE);
 	board_timer_setup = qemu_timer_setup;
+
+	qemu_reboot_setup();
 }
