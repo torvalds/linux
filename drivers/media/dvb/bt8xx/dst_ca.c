07f640f0ddd72e6158fca7f0694dbaf95cee4d2c
Mauro Carvalho Chehab <mchehab@infradead.org>
2006-05-22 10:31:57 -0300

    V4L/DVB (3973a): Return value is ssize_t
    This patch fix a difference between v4l/dvb tree and kernel tree.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/bt8xx/dst_ca.c
+++ drivers/media/dvb/bt8xx/dst_ca.c
@@ -582,7 +582,7 @@ static int dst_ca_release(struct inode *inode, struct file *file)
 
 static ssize_t dst_ca_read(struct file *file, char __user *buffer, size_t length, loff_t *offset)
 {
-	int bytes_read = 0;
+	ssize_t bytes_read = 0;
 
 	dprintk(verbose, DST_CA_DEBUG, 1, " Device read.");
 
