eb79b4fda4654eaa7e6421ad41b0a75c7b4f0608
Ian Abbott <abbotti@mev.co.uk>
2006-05-30 12:36:30 +0100

    [PATCH] USB: ftdi_sio: add support for Yost Engineering ServoCenter3.1
    This patch adds support for Yost Engineering Inc's ServoCenter 3.1 USB
    product to the ftdi_sio driver's device ID table.  The PID was supplied
    by Aaron Prose of Yost Engineering on the ftdi-usb-sio-devel list.  The
    PID 0xE050 matches the Windows INF files for this device.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio.c
+++ drivers/usb/serial/ftdi_sio.c
@@ -500,6 +500,7 @@ static struct usb_device_id id_table_combined [] = {
 	{ USB_DEVICE(ICOM_ID1_VID, ICOM_ID1_PID) },
 	{ USB_DEVICE(PAPOUCH_VID, PAPOUCH_TMU_PID) },
 	{ USB_DEVICE(FTDI_VID, FTDI_ACG_HFDUAL_PID) },
+	{ USB_DEVICE(FTDI_VID, FTDI_YEI_SERVOCENTER31_PID) },
 	{ },					/* Optional parameter entry */
 	{ }					/* Terminating entry */
 };
