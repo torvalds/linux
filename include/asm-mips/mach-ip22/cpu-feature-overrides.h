c3b1c2de2254aa4721e7e3c8d418fa9302e6acb0
Kumba <kumba@gentoo.org>
2006-06-18 02:17:01 -0400

    [MIPS] Fix R4K cache macro names
    Several machines have the R4K cache macro name spelled incorrectly.  Namely,
    they have cpu_has_4kcache defined instead of cpu_has_4k_cache.
    
    Signed-off-by: Joshua Kinard <kumba@gentoo.org>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/mach-ip22/cpu-feature-overrides.h
+++ include/asm-mips/mach-ip22/cpu-feature-overrides.h
@@ -13,7 +13,7 @@
  */
 #define cpu_has_tlb		1
 #define cpu_has_4kex		1
-#define cpu_has_4kcache		1
+#define cpu_has_4k_cache	1
 #define cpu_has_fpu		1
 #define cpu_has_32fpr		1
 #define cpu_has_counter		1
