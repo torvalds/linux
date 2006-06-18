a805bad5daae8d4f92ce46f467484d4867e996d4
Nathan Scott <nathans@sgi.com>
2006-06-19 08:40:27 +1000

    [XFS] Remove unneeded conditional code on NFS export interface related code paths.
    SGI-PV: 904196
    SGI-Modid: xfs-linux-melb:xfs-kern:26250a
    
    Signed-off-by: Nathan Scott <nathans@sgi.com>

--- fs/xfs/linux-2.6/xfs_super.c
+++ fs/xfs/linux-2.6/xfs_super.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000-2005 Silicon Graphics, Inc.
+ * Copyright (c) 2000-2006 Silicon Graphics, Inc.
  * All Rights Reserved.
  *
  * This program is free software; you can redistribute it and/or
@@ -796,9 +796,7 @@ xfs_fs_fill_super(
 	}
 
 	sb_min_blocksize(sb, BBSIZE);
-#ifdef CONFIG_XFS_EXPORT
 	sb->s_export_op = &xfs_export_operations;
-#endif
 	sb->s_qcop = &xfs_quotactl_operations;
 	sb->s_op = &xfs_super_operations;
 
