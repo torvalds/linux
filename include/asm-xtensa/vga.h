4f1bcaf094ccc512c23e10104c05a6f8e5b7a9e4
Bjorn Helgaas <bjorn.helgaas@hp.com>
2006-06-22 14:47:32 -0700

    [PATCH] vgacon: make VGA_MAP_MEM take size, remove extra use
    VGA_MAP_MEM translates to ioremap() on some architectures.  It makes sense
    to do this to vga_vram_base, because we're going to access memory between
    vga_vram_base and vga_vram_end.
    
    But it doesn't really make sense to map starting at vga_vram_end, because
    we aren't going to access memory starting there.  On ia64, which always has
    to be different, ioremapping vga_vram_end gives you something completely
    incompatible with ioremapped vga_vram_start, so vga_vram_size ends up being
    nonsense.
    
    As a bonus, we often know the size up front, so we can use ioremap()
    correctly, rather than giving it a zero size.
    
    Signed-off-by: Bjorn Helgaas <bjorn.helgaas@hp.com>
    Cc: "Antonino A. Daplas" <adaplas@pol.net>
    Cc: "Luck, Tony" <tony.luck@intel.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-xtensa/vga.h
+++ include/asm-xtensa/vga.h
@@ -11,7 +11,7 @@
 #ifndef _XTENSA_VGA_H
 #define _XTENSA_VGA_H
 
-#define VGA_MAP_MEM(x) (unsigned long)phys_to_virt(x)
+#define VGA_MAP_MEM(x,s) (unsigned long)phys_to_virt(x)
 
 #define vga_readb(x)	(*(x))
 #define vga_writeb(x,y)	(*(y) = (x))
