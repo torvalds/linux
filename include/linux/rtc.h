8232212e0b4ee4eb3e407f5a9b098f6377820164
Andrew Victor <andrew@sanpeople.com>
2006-06-25 05:48:25 -0700

    [PATCH] RTC: Add rtc_year_days() to calculate tm_yday
    RTC: Add exported function rtc_year_days() to calculate the tm_yday value.
    
    Signed-off-by: Andrew Victor <andrew@sanpeople.com>
    Signed-off-by: Alessandro Zummo <a.zummo@towertech.it>
    Cc: Russell King <rmk@arm.linux.org.uk>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/rtc.h
+++ include/linux/rtc.h
@@ -102,6 +102,7 @@ struct rtc_pll_info {
 #include <linux/interrupt.h>
 
 extern int rtc_month_days(unsigned int month, unsigned int year);
+extern int rtc_year_days(unsigned int day, unsigned int month, unsigned int year);
 extern int rtc_valid_tm(struct rtc_time *tm);
 extern int rtc_tm_to_time(struct rtc_time *tm, unsigned long *time);
 extern void rtc_time_to_tm(unsigned long time, struct rtc_time *tm);
