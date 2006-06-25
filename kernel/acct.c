11e64757f9fb32f13f51596bbf01988f42fca764
Matt Helsley <matthltc@us.ibm.com>
2006-06-25 05:48:03 -0700

    [PATCH] Remove unecessary NULL check in kernel/acct.c
    copy_process() appears to be the only caller of acct_clear_integrals() and
    does not pass in NULL task pointers.  Remove the unecessary check.
    
    Signed-off-by: Matt Helsley <matthltc@us.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/acct.c
+++ kernel/acct.c
@@ -599,9 +599,7 @@ void acct_update_integrals(struct task_struct *tsk)
  */
 void acct_clear_integrals(struct task_struct *tsk)
 {
-	if (tsk) {
-		tsk->acct_stimexpd = 0;
-		tsk->acct_rss_mem1 = 0;
-		tsk->acct_vm_mem1 = 0;
-	}
+	tsk->acct_stimexpd = 0;
+	tsk->acct_rss_mem1 = 0;
+	tsk->acct_vm_mem1 = 0;
 }
