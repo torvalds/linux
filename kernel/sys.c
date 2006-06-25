bbb1747d4e44ce49acc73daa8d66e5f6bd546f1b
Alan Stern <stern@rowland.harvard.edu>
2006-06-25 05:47:15 -0700

    [PATCH] Allow raw_notifier callouts to unregister themselves
    Since raw_notifier chains don't benefit from any centralized locking
    protections, they shouldn't suffer from the associated limitations.  Under
    some circumstances it might make sense for a raw_notifier callout routine
    to unregister itself from the notifier chain.  This patch (as678) changes
    the notifier core to allow for such things.
    
    Signed-off-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/sys.c
+++ kernel/sys.c
@@ -137,14 +137,15 @@ static int __kprobes notifier_call_chain(struct notifier_block **nl,
 		unsigned long val, void *v)
 {
 	int ret = NOTIFY_DONE;
-	struct notifier_block *nb;
+	struct notifier_block *nb, *next_nb;
 
 	nb = rcu_dereference(*nl);
 	while (nb) {
+		next_nb = rcu_dereference(nb->next);
 		ret = nb->notifier_call(nb, val, v);
 		if ((ret & NOTIFY_STOP_MASK) == NOTIFY_STOP_MASK)
 			break;
-		nb = rcu_dereference(nb->next);
+		nb = next_nb;
 	}
 	return ret;
 }
