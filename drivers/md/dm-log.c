29121bd0b00ebb9524971a583fea4a2f7afe8041
Alasdair G Kergon <agk@redhat.com>
2006-06-26 00:27:29 -0700

    [PATCH] dm mirror log: bitset_size fix
    Fix the 'sizeof' in the region log bitmap size calculation: it's uint32_t, not
    unsigned long - this breaks on some archs.
    
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/md/dm-log.c
+++ drivers/md/dm-log.c
@@ -295,10 +295,10 @@ static int create_log_context(struct dirty_log *log, struct dm_target *ti,
 	 * Work out how many "unsigned long"s we need to hold the bitset.
 	 */
 	bitset_size = dm_round_up(region_count,
-				  sizeof(unsigned long) << BYTE_SHIFT);
+				  sizeof(*lc->clean_bits) << BYTE_SHIFT);
 	bitset_size >>= BYTE_SHIFT;
 
-	lc->bitset_uint32_count = bitset_size / 4;
+	lc->bitset_uint32_count = bitset_size / sizeof(*lc->clean_bits);
 
 	/*
 	 * Disk log?
