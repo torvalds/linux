9e8e30a0cc0ccb43773d14d8b8b84bcc585e9cc1
Johannes Berg <johannes@sipsolutions.net>
2006-06-26 01:49:55 -0400

    Input: via-pmu - add input device support
    Add an input device for the button and lid switch so that userspace gets
    notified about the user pressing them via the standard input layer.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

---
+++ drivers/macintosh/via-pmu-event.h
@@ -0,0 +1,8 @@
+#ifndef __VIA_PMU_EVENT_H
+#define __VIA_PMU_EVENT_H
+
+#define PMU_EVT_POWER	0
+#define PMU_EVT_LID	1
+extern void via_pmu_event(int key, int down);
+
+#endif /* __VIA_PMU_EVENT_H */
