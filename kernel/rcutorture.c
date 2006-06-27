29766f1eb3d8d6cfaf1d6623fb4c3f7e5a822fe4
Paul E. McKenney <paulmck@us.ibm.com>
2006-06-27 02:54:02 -0700

    [PATCH] rcutorture: catchup doc fixes for idle-hz tests
    This just catches the RCU torture documentation up with the recent fixes
    that test RCU for architectures that turn of the scheduling-clock interrupt
    for idle CPUs and the addition of a SUCCESS/FAILURE indication, fixing up
    an obsolete comment as well.
    
    Signed-off-by: Paul E. McKenney <paulmck@us.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/rcutorture.c
+++ kernel/rcutorture.c
@@ -1,5 +1,5 @@
 /*
- * Read-Copy Update /proc-based torture test facility
+ * Read-Copy Update module-based torture test facility
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
