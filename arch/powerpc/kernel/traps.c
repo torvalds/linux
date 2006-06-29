eac8392f9591b359847b8a6abb75ca60967bcecd
David Wilder <dwilder@us.ibm.com>
2006-06-29 15:17:30 -0700

    [POWERPC] Make secondary CPUs call into kdump on reset exception
    In the case of a system hang, the user will invoke soft-reset to
    initiate the kdump boot.  If xmon is enabled, the CPU(s) enter into the
    xmon debugger.   Unfortunately, the secondary CPU(s) will return to the
    hung state when they exit from the debugger (returned from die() ->
    system_reset_exception()).  This causes a problem in kdump since the
    hung CPU(s) will not respond to the IPI sent from kdump.  This patch
    fixes the issue by calling crash_kexec_secondary() directly from
    system_reset_exception() without returning to the previous state.  These
    secondary CPUs wait 5ms until the kdump boot is started by the primary
    CPU.   In the case we exited from the debugger to "recover" (command 'x'
    in xmon) the primary and the secondary CPUs will all return from die()
    -> system_reset_exception() ->crash_kexec_secondary() wait 5ms, then
    return to the previous state.  A kdump boot is not started in this case.
    
    Signed-off-by: Haren Myneni <haren@us.ibm.com>
    Signed-off-by: David Wilder <dwilder@us.ibm.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kernel/traps.c
+++ arch/powerpc/kernel/traps.c
@@ -211,6 +211,19 @@ void system_reset_exception(struct pt_regs *regs)
 
 	die("System Reset", regs, SIGABRT);
 
+	/*
+	 * Some CPUs when released from the debugger will execute this path.
+	 * These CPUs entered the debugger via a soft-reset. If the CPU was
+	 * hung before entering the debugger it will return to the hung
+	 * state when exiting this function.  This causes a problem in
+	 * kdump since the hung CPU(s) will not respond to the IPI sent
+	 * from kdump. To prevent the problem we call crash_kexec_secondary()
+	 * here. If a kdump had not been initiated or we exit the debugger
+	 * with the "exit and recover" command (x) crash_kexec_secondary()
+	 * will return after 5ms and the CPU returns to its previous state.
+	 */
+	crash_kexec_secondary(regs);
+
 	/* Must die if the interrupt is not recoverable */
 	if (!(regs->msr & MSR_RI))
 		panic("Unrecoverable System Reset");
