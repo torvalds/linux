1ee0dc51fb68d2d25888250c554492c4926c5ec1
Stefan Richter <stefanr@s5r6.in-berlin.de>
2006-07-03 12:02:33 -0400

    [PATCH] ieee1394: nodemgr: remove unnecessary includes
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>
    Signed-off-by: Ben Collins <bcollins@ubuntu.com>

--- drivers/ieee1394/nodemgr.c
+++ drivers/ieee1394/nodemgr.c
@@ -12,12 +12,8 @@
 #include <linux/kernel.h>
 #include <linux/list.h>
 #include <linux/slab.h>
-#include <linux/smp_lock.h>
-#include <linux/interrupt.h>
-#include <linux/kmod.h>
 #include <linux/completion.h>
 #include <linux/delay.h>
-#include <linux/pci.h>
 #include <linux/moduleparam.h>
 #include <asm/atomic.h>
 
