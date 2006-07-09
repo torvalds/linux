ece2246ebda3d86cedb9414e7771862a6c9a054c
Ralf Baechle <ralf@linux-mips.org>
2006-07-09 22:27:23 +0100

    [MIPS] Atlas, Malta, SEAD: Don't disable interrupts in mips_time_init().
    By the time it's called from time_init interrupts are still disabled.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mips-boards/generic/time.c
+++ arch/mips/mips-boards/generic/time.c
@@ -228,9 +228,7 @@ unsigned long __init mips_rtc_get_time(void)
 
 void __init mips_time_init(void)
 {
-	unsigned int est_freq, flags;
-
-	local_irq_save(flags);
+	unsigned int est_freq;
 
         /* Set Data mode - binary. */
         CMOS_WRITE(CMOS_READ(RTC_CONTROL) | RTC_DM_BINARY, RTC_CONTROL);
@@ -241,8 +239,6 @@ void __init mips_time_init(void)
 	       (est_freq%1000000)*100/1000000);
 
         cpu_khz = est_freq / 1000;
-
-	local_irq_restore(flags);
 }
 
 void __init plat_timer_setup(struct irqaction *irq)
