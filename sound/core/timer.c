6ed5eff025b72cb84a884d4be05f854f13b1542f
Clemens Ladisch <clemens@ladisch.de>
2006-07-17 16:51:37 +0200

    [ALSA] system timer: accumulate correction for multiple lost ticks
    When multiple timer interrupts arrive too late, correct for all delays
    instead of ignoring the earlier ones.
    
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/core/timer.c
+++ sound/core/timer.c
@@ -971,7 +971,7 @@ static void snd_timer_s_function(unsigned long data)
 	struct snd_timer_system_private *priv = timer->private_data;
 	unsigned long jiff = jiffies;
 	if (time_after(jiff, priv->last_expires))
-		priv->correction = (long)jiff - (long)priv->last_expires;
+		priv->correction += (long)jiff - (long)priv->last_expires;
 	snd_timer_interrupt(timer, (long)jiff - (long)priv->last_jiffies);
 }
 
