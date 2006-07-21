64d2f0855e50a7185546ee1fbc03c2badc31330f
Adrian Bunk <bunk@stusta.de>
2006-07-21 14:49:49 -0700

    [I/OAT]: net/core/user_dma.c should #include <net/netdma.h>
    Every file should #include the headers containing the prototypes for
    its global functions.
    
    Especially in cases like this one where gcc can tell us through a
    compile error that the prototype was wrong...
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/netdma.h
+++ include/net/netdma.h
@@ -37,7 +37,7 @@ static inline struct dma_chan *get_softnet_dma(void)
 }
 
 int dma_skb_copy_datagram_iovec(struct dma_chan* chan,
-		const struct sk_buff *skb, int offset, struct iovec *to,
+		struct sk_buff *skb, int offset, struct iovec *to,
 		size_t len, struct dma_pinned_list *pinned_list);
 
 #endif /* CONFIG_NET_DMA */
