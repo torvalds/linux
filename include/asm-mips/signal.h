6b3e5f44b56745daad8cd913ccc7bcd9a9ece5ea
Yoichi Yuasa <yoichi_yuasa@tripeaks.co.jp>
2006-07-31 23:01:37 +0900

    [MIPS] Use common definitions from asm-generic/signal.h
    Signed-off-by: Yoichi Yuasa <yoichi_yuasa@tripeaks.co.jp>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/signal.h
+++ include/asm-mips/signal.h
@@ -111,14 +111,7 @@ typedef unsigned long old_sigset_t;		/* at least 32 bits */
 #define SIG_SETMASK32	256	/* Goodie from SGI for BSD compatibility:
 				   set only the low 32 bit of the sigset.  */
 
-/* Type of a signal handler.  */
-typedef void __signalfn_t(int);
-typedef __signalfn_t __user *__sighandler_t;
-
-/* Fake signal functions */
-#define SIG_DFL	((__sighandler_t)0)	/* default signal handling */
-#define SIG_IGN	((__sighandler_t)1)	/* ignore signal */
-#define SIG_ERR	((__sighandler_t)-1)	/* error return from signal */
+#include <asm-generic/signal.h>
 
 struct sigaction {
 	unsigned int	sa_flags;
