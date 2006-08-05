11ea3173d5f2de71d037ef58ac43395795fed2bc
Rick Koch <n1gp@hotmail.com>
2006-08-05 00:32:30 -0400

    Input: add driver for Touchwin serial touchscreens
    Signed-off-by: Rick Koch <n1gp@hotmail.com>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- include/linux/serio.h
+++ include/linux/serio.h
@@ -219,5 +219,6 @@ static inline void serio_unpin_driver(struct serio *serio)
 #define SERIO_MICROTOUCH	0x30
 #define SERIO_PENMOUNT	0x31
 #define SERIO_TOUCHRIGHT	0x32
+#define SERIO_TOUCHWIN	0x33
 
 #endif
