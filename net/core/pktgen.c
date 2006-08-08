69d8c28c9578ce78b3dc1b9be36926d962282898
David S. Miller <davem@sunset.davemloft.net>
2006-08-07 20:52:10 -0700

    [PKTGEN]: Make sure skb->{nh,h} are initialized in fill_packet_ipv6() too.
    Mirror the bug fix from fill_packet_ipv4()
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/pktgen.c
+++ net/core/pktgen.c
@@ -2462,6 +2462,8 @@ static struct sk_buff *fill_packet_ipv6(struct net_device *odev,
 	skb->protocol = protocol;
 	skb->dev = odev;
 	skb->pkt_type = PACKET_HOST;
+	skb->nh.ipv6h = iph;
+	skb->h.uh = udph;
 
 	if (pkt_dev->nfrags <= 0)
 		pgh = (struct pktgen_hdr *)skb_put(skb, datalen);
