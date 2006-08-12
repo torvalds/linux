0d7466146fa59cfa4b992a52cf274d603b475fd1
Andrew de Quincey <adq_dvb@lidskialf.net>
2006-08-12 09:09:44 -0300

    V4L/DVB (4500): Fix KNC1 DVBC support
    This actually needs the same configuration as the other DVBC cards; simply
    no one had managed to test before to find out.
    
    Signed-off-by: Andrew de Quincey <adq_dvb@lidskialf.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/ttpci/budget-av.c
+++ drivers/media/dvb/ttpci/budget-av.c
@@ -1141,15 +1141,6 @@ static void frontend_init(struct budget_av *budget_av)
 		break;
 
 	case SUBID_DVBC_KNC1:
-		budget_av->reinitialise_demod = 1;
-		fe = dvb_attach(tda10021_attach, &philips_cu1216_config,
-				     &budget_av->budget.i2c_adap,
-				     read_pwm(budget_av));
-		if (fe) {
-			fe->ops.tuner_ops.set_params = philips_cu1216_tuner_set_params;
-		}
-		break;
-
 	case SUBID_DVBC_KNC1_PLUS:
 	case SUBID_DVBC_CINERGY1200:
 		budget_av->reinitialise_demod = 1;
