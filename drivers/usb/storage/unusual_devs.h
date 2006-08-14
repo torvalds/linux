94918ff68a7c78fdd3241d13e18444ef80b8ff4a
Alan Stern <stern@rowland.harvard.edu>
2006-08-14 11:40:46 -0400

    [PATCH] unusual_devs update for UCR-61S2B
    The existing unusual_devs entry for the UCR-61S2B appears to have too
    wide a revision range.  It matches at least one device that doesn't
    respond to the initialization sequence.  Perhaps the sequence needs to
    be updated, or perhaps something else can be done.  For now, this patch
    (as764) restricts the range to include only the revision mentioned in
    the original comment.
    
    This resolves (for now!) Bugzilla entry #6950.
    
    Signed-off-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/storage/unusual_devs.h
+++ drivers/usb/storage/unusual_devs.h
@@ -1261,7 +1261,7 @@ UNUSUAL_DEV(  0x0fce, 0xd008, 0x0000, 0x0000,
  * Tested on hardware version 1.10.
  * Entry is needed only for the initializer function override.
  */
-UNUSUAL_DEV(  0x1019, 0x0c55, 0x0000, 0x9999,
+UNUSUAL_DEV(  0x1019, 0x0c55, 0x0110, 0x0110,
 		"Desknote",
 		"UCR-61S2B",
 		US_SC_DEVICE, US_PR_DEVICE, usb_stor_ucr61s2b_init,
