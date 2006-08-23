a969fd5a4e162c4485ae8f3e49d674656a18fa36
Trond Myklebust <Trond.Myklebust@netapp.com>
2006-08-22 20:06:04 -0400

    VFS: Remove redundant open-coded mode bit checks in open_exec().
    The check in open_exec() for inode->i_mode & 0111 has been made
    redundant by the fix to permission().
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    (cherry picked from 1d3741c5d991686699f100b65b9956f7ee7ae0ae commit)

--- fs/exec.c
+++ fs/exec.c
@@ -486,8 +486,6 @@ struct file *open_exec(const char *name)
 		if (!(nd.mnt->mnt_flags & MNT_NOEXEC) &&
 		    S_ISREG(inode->i_mode)) {
 			int err = vfs_permission(&nd, MAY_EXEC);
-			if (!err && !(inode->i_mode & 0111))
-				err = -EACCES;
 			file = ERR_PTR(err);
 			if (!err) {
 				file = nameidata_to_filp(&nd, O_RDONLY);
