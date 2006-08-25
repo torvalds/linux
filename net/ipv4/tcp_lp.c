298969727e7b855d53f3becfa92c055914082ec4
Alexey Dobriyan <adobriyan@gmail.com>
2006-08-25 00:37:24 -0700

    [TCP] tcp_lp: use BUILD_BUG_ON
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_lp.c
+++ net/ipv4/tcp_lp.c
@@ -327,7 +327,7 @@ static struct tcp_congestion_ops tcp_lp = {
 
 static int __init tcp_lp_register(void)
 {
-	BUG_ON(sizeof(struct lp) > ICSK_CA_PRIV_SIZE);
+	BUILD_BUG_ON(sizeof(struct lp) > ICSK_CA_PRIV_SIZE);
 	return tcp_register_congestion_control(&tcp_lp);
 }
 
