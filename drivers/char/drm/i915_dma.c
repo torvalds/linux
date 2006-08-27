46acbf13fb280e69c5cafe7c837d5a27c6e380c3
Dave Airlie <airlied@linux.ie>
2006-08-27 11:09:46 +1000

    drm: fixup i915 error codes
    Frederik Deweerdt <deweerdt@free.fr> pointed this out, I fixed a missing
    DRM error wrapper also.
    
    Signed-off-by: Dave Airlie <airlied@linux.ie>

--- drivers/char/drm/i915_dma.c
+++ drivers/char/drm/i915_dma.c
@@ -260,7 +260,7 @@ static int i915_dma_init(DRM_IOCTL_ARGS)
 		retcode = i915_dma_resume(dev);
 		break;
 	default:
-		retcode = -EINVAL;
+		retcode = DRM_ERR(EINVAL);
 		break;
 	}
 
@@ -391,7 +391,7 @@ static int i915_emit_box(drm_device_t * dev,
 	RING_LOCALS;
 
 	if (DRM_COPY_FROM_USER_UNCHECKED(&box, &boxes[i], sizeof(box))) {
-		return EFAULT;
+		return DRM_ERR(EFAULT);
 	}
 
 	if (box.y2 <= box.y1 || box.x2 <= box.x1 || box.y2 <= 0 || box.x2 <= 0) {
