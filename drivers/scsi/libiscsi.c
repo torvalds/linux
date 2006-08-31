db98ccde0881b8247acb52dece6d94ed770a7aa5
Mike Christie <michaelc@cs.wisc.edu>
2006-08-31 18:09:31 -0400

    [SCSI] libiscsi: only check burst lengths when sending unsol data
    The first burst length is only relevant if immedate data = Yes
    or if Initial R2T is No
    
    Signed-off-by: Mike Christie <michaelc@cs.wisc.edu>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/libiscsi.c
+++ drivers/scsi/libiscsi.c
@@ -1593,7 +1593,8 @@ int iscsi_conn_start(struct iscsi_cls_conn *cls_conn)
 		return -EPERM;
 	}
 
-	if (session->first_burst > session->max_burst) {
+	if ((session->imm_data_en || !session->initial_r2t_en) &&
+	     session->first_burst > session->max_burst) {
 		printk("iscsi: invalid burst lengths: "
 		       "first_burst %d max_burst %d\n",
 		       session->first_burst, session->max_burst);
