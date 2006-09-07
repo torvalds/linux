92cd24fea4feefb9bde8efab3fdb25f525220672
Andrew de Quincey <adq_dvb@lidskialf.net>
2006-09-07 00:31:55 -0300

    V4L/DVB (4610): Add tua6100 config wrapper
    I had forgotten to add the wrapper round the tua6100_attach function if its
    disabled in Kconfig
    
    Signed-off-by: Andrew de Quincey <adq_dvb@lidskialf.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/tua6100.h
+++ drivers/media/dvb/frontends/tua6100.h
@@ -34,14 +34,14 @@
 #include <linux/i2c.h>
 #include "dvb_frontend.h"
 
-/**
- * Attach a tua6100 pll to the supplied frontend structure.
- *
- * @param fe Frontend to attach to.
- * @param addr i2c address of the pll.
- * @param i2c i2c adapter to use.
- * @return FE pointer on success, NULL on failure.
- */
+#if defined(CONFIG_DVB_TUA6100) || defined(CONFIG_DVB_TUA6100_MODULE)
 extern struct dvb_frontend *tua6100_attach(struct dvb_frontend *fe, int addr, struct i2c_adapter *i2c);
+#else
+static inline struct dvb_frontend* tua6100_attach(struct dvb_frontend *fe, int addr, struct i2c_adapter *i2c)
+{
+	printk(KERN_WARNING "%s: driver disabled by Kconfig\n", __FUNCTION__);
+	return NULL;
+}
+#endif // CONFIG_DVB_TUA6100
 
 #endif
