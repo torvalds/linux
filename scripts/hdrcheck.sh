120e2a97268c312bf81ee3d8cacc75d1f75ef8b5
Alexey Dobriyan <adobriyan@gmail.com>
2006-09-12 20:36:00 -0700

    [PATCH] headers_check: improve #include regexp
    The following combinations of pp-tokens are used
    
    	#include
    	 #include
    	# include
    
    so, script'd better check for all of them.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- scripts/hdrcheck.sh
+++ scripts/hdrcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for FILE in `grep '^#include <' $2 | cut -f2 -d\< | cut -f1 -d\> | egrep ^linux\|^asm` ; do
+for FILE in `grep '^[ \t]*#[ \t]*include[ \t]*<' $2 | cut -f2 -d\< | cut -f1 -d\> | egrep ^linux\|^asm` ; do
     if [ ! -r $1/$FILE ]; then
 	echo $2 requires $FILE, which does not exist
 	exit 1
