cf2b5d3fcab77a9390293920ec5b49e67eced200
Doug Ledford <dledford@redhat.com>
2006-09-17 07:38:15 +0200

    [SCSI] aic7xxx: pause sequencer before touching SBLKCTL
    Some cards need to pause the sequencer before the SBLKCTL register is
    touched.  This fixes a PCI related oops seen on powerpc macs with this
    card caused by trying to ascertain the bus signalling before beginning
    domain validation.
    
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/aic7xxx/aic7xxx_osm.c
+++ drivers/scsi/aic7xxx/aic7xxx_osm.c
@@ -2539,6 +2539,7 @@ static void ahc_linux_set_iu(struct scsi_target *starget, int iu)
 static void ahc_linux_get_signalling(struct Scsi_Host *shost)
 {
 	struct ahc_softc *ahc = *(struct ahc_softc **)shost->hostdata;
+	unsigned long flags;
 	u8 mode;
 
 	if (!(ahc->features & AHC_ULTRA2)) {
@@ -2550,7 +2551,11 @@ static void ahc_linux_get_signalling(struct Scsi_Host *shost)
 		return;
 	}
 
+	ahc_lock(ahc, &flags);
+	ahc_pause(ahc);
 	mode = ahc_inb(ahc, SBLKCTL);
+	ahc_unpause(ahc);
+	ahc_unlock(ahc, &flags);
 
 	if (mode & ENAB40)
 		spi_signalling(shost) = SPI_SIGNAL_LVD;
