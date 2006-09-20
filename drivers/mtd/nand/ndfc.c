6a545a0d6021a4d759ba6d0c1082d1abf8d64c84
Frank Haverkamp <haver@vnet.ibm.com>
2006-09-20 17:24:52 +0200

    [MTD NAND] Fix in typo ndfc.c causing wrong ECC layout
    Due to this typo, a wrong ECC layout table is chosen.
    
    Signed-off-by: Frank Haverkamp <haver@vnet.ibm.com>
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- drivers/mtd/nand/ndfc.c
+++ drivers/mtd/nand/ndfc.c
@@ -168,7 +168,7 @@ static void ndfc_chip_init(struct ndfc_nand_mtd *mtd)
 	chip->ecc.mode = NAND_ECC_HW;
 	chip->ecc.size = 256;
 	chip->ecc.bytes = 3;
-	chip->ecclayout = mtd->pl_chip->ecclayout;
+	chip->ecclayout = chip->ecc.layout = mtd->pl_chip->ecclayout;
 	mtd->mtd.priv = chip;
 	mtd->mtd.owner = THIS_MODULE;
 }
