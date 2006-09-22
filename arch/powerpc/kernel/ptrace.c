4b9c876a812fd3e2b17e2d1c94082ee4cf31608f
David Woodhouse <dwmw2@infradead.org>
2006-09-22 09:23:53 +0100

    [POWERPC] Fix audit syscall success/failure reporting on PowerPC
    Due to my stupidity, we were checking for the wrong bit in CCR when
    attempting to determine whether a syscall succeeded or not. Remedy the
    symptom, if not the cause.
    
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kernel/ptrace.c
+++ arch/powerpc/kernel/ptrace.c
@@ -553,7 +553,7 @@ void do_syscall_trace_leave(struct pt_regs *regs)
 #endif
 
 	if (unlikely(current->audit_context))
-		audit_syscall_exit((regs->ccr&0x1000)?AUDITSC_FAILURE:AUDITSC_SUCCESS,
+		audit_syscall_exit((regs->ccr&0x10000000)?AUDITSC_FAILURE:AUDITSC_SUCCESS,
 				   regs->result);
 
 	if ((test_thread_flag(TIF_SYSCALL_TRACE)
