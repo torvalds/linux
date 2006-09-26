d60674eb5d961b2421db16cc373dc163f38cc105
Hyok S. Choi <hyok.choi@samsung.com>
2006-09-26 17:38:18 +0900

    [ARM] nommu: add ARM940T core support
    This patch adds ARM940T core support which has 4KB D-cache, 4KB I-cache
    and a MPU.
    
    Signed-off-by: Hyok S. Choi <hyok.choi@samsung.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/cacheflush.h
+++ include/asm-arm/cacheflush.h
@@ -56,6 +56,14 @@
 # endif
 #endif
 
+#if defined(CONFIG_CPU_ARM940T)
+# ifdef _CACHE
+#  define MULTI_CACHE 1
+# else
+#  define _CACHE arm940
+# endif
+#endif
+
 #if defined(CONFIG_CPU_SA110) || defined(CONFIG_CPU_SA1100)
 # ifdef _CACHE
 #  define MULTI_CACHE 1
--- include/asm-arm/proc-fns.h
+++ include/asm-arm/proc-fns.h
@@ -105,6 +105,14 @@
 #   define CPU_NAME cpu_arm926
 #  endif
 # endif
+# ifdef CONFIG_CPU_ARM940T
+#  ifdef CPU_NAME
+#   undef  MULTI_CPU
+#   define MULTI_CPU
+#  else
+#   define CPU_NAME cpu_arm940
+#  endif
+# endif
 # ifdef CONFIG_CPU_SA110
 #  ifdef CPU_NAME
 #   undef  MULTI_CPU
