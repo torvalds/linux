658fdbef66e5e9be79b457edc2cbbb3add840aa9
Andi Kleen <ak@suse.de>
2006-09-26 10:52:41 +0200

    [PATCH] Don't leak NT bit into next task
    SYSENTER can cause a NT to be set which might cause crashes on the IRET
    in the next task.
    
    Following similar i386 patch from Linus.
    
    Signed-off-by: Andi Kleen <ak@suse.de>

--- arch/x86_64/kernel/setup64.c
+++ arch/x86_64/kernel/setup64.c
@@ -180,6 +180,8 @@ void __cpuinit check_efer(void)
         }       
 }
 
+unsigned long kernel_eflags;
+
 /*
  * cpu_init() initializes state that is per-CPU. Some data is already
  * initialized (naturally) in the bootstrap process, such as the GDT
@@ -281,4 +283,6 @@ void __cpuinit cpu_init (void)
 	set_debugreg(0UL, 7);
 
 	fpu_init(); 
+
+	raw_local_save_flags(kernel_eflags);
 }
