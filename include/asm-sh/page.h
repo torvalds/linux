8c12b5dc13bf8516303a8224ab4e9708b33d5b00
Paul Mundt <lethal@linux-sh.org>
2006-09-27 18:31:06 +0900

    sh: Clean up PAGE_SIZE definition for assembly use.
    We want to be able to use PAGE_SIZE all over the place,
    this is the same approach adopted by other architectures..
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/page.h
+++ include/asm-sh/page.h
@@ -16,7 +16,13 @@
 
 /* PAGE_SHIFT determines the page size */
 #define PAGE_SHIFT	12
+
+#ifdef __ASSEMBLY__
 #define PAGE_SIZE	(1 << PAGE_SHIFT)
+#else
+#define PAGE_SIZE	(1UL << PAGE_SHIFT)
+#endif
+
 #define PAGE_MASK	(~(PAGE_SIZE-1))
 #define PTE_MASK	PAGE_MASK
 
