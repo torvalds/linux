8f0472d331619d5d74927978d0dde5b4935e41a5
Sean Hefty <sean.hefty@intel.com>
2006-09-29 11:57:09 -0700

    RDMA/cma: Set status correctly on route resolution error
    On reporting a route error, also include the status for the error,
    rather than indicating a status of 0 when an error has occurred.
    
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/core/cma.c
+++ drivers/infiniband/core/cma.c
@@ -1308,6 +1308,7 @@ static void cma_query_handler(int status, struct ib_sa_path_rec *path_rec,
 		work->old_state = CMA_ROUTE_QUERY;
 		work->new_state = CMA_ADDR_RESOLVED;
 		work->event.event = RDMA_CM_EVENT_ROUTE_ERROR;
+		work->event.status = status;
 	}
 
 	queue_work(cma_wq, &work->work);
