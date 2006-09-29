368bdb3d616fa352971f45b423ae6344715e620b
Alexey Dobriyan <adobriyan@gmail.com>
2006-09-29 02:01:05 -0700

    [PATCH] cramfs: make cramfs_uncompress_exit() return void
    It always returns 0, so relying on it is useless.  The only caller isn't
    checking return value.  In general, un-, de-, -free functions should return
    void.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/cramfs_fs.h
+++ include/linux/cramfs_fs.h
@@ -87,6 +87,6 @@ struct cramfs_super {
 /* Uncompression interfaces to the underlying zlib */
 int cramfs_uncompress_block(void *dst, int dstlen, void *src, int srclen);
 int cramfs_uncompress_init(void);
-int cramfs_uncompress_exit(void);
+void cramfs_uncompress_exit(void);
 
 #endif
