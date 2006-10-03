a09fd48f8fb6ecb8bf85efdd884b21b6201ef9cc
Geert Uytterhoeven <geert@linux-m68k.org>
2006-10-03 01:15:11 -0700

    [PATCH] fbdev: correct buffer size limit in fbmem_read_proc()
    Address http://bugzilla.kernel.org/show_bug.cgi?id=7189
    
    It should check `clen', not `len'.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: <jurij@wooyd.org>
    Cc: "Antonino A. Daplas" <adaplas@pol.net>
    Cc: <stable@kernel.org>
    Cc: Willy Tarreau <w@1wt.eu>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/video/fbmem.c
+++ drivers/video/fbmem.c
@@ -554,7 +554,8 @@ static int fbmem_read_proc(char *buf, char **start, off_t offset,
 	int clen;
 
 	clen = 0;
-	for (fi = registered_fb; fi < &registered_fb[FB_MAX] && len < 4000; fi++)
+	for (fi = registered_fb; fi < &registered_fb[FB_MAX] && clen < 4000;
+	     fi++)
 		if (*fi)
 			clen += sprintf(buf + clen, "%d %s\n",
 				        (*fi)->node,
