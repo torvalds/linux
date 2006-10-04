e78d01693be38bf93dd6bb49b86e143da450de86
Eric W. Biederman <ebiederm@xmission.com>
2006-10-04 02:16:54 -0700

    [PATCH] Add Hypertransport capability defines
    This adds defines for the hypertransport capability subtypes and starts
    using them a little.
    
    [akpm@osdl.org: fix typo]
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Greg KH <greg@kroah.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/powerpc/sysdev/mpic.c
+++ arch/powerpc/sysdev/mpic.c
@@ -341,7 +341,7 @@ static void __init mpic_scan_ht_pic(struct mpic *mpic, u8 __iomem *devbase,
 		u8 id = readb(devbase + pos + PCI_CAP_LIST_ID);
 		if (id == PCI_CAP_ID_HT) {
 			id = readb(devbase + pos + 3);
-			if (id == 0x80)
+			if (id == HT_CAPTYPE_IRQ)
 				break;
 		}
 	}
