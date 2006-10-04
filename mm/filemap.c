b2abacf3a2699a8020829c85c16f358ba85cecaf
Henrik Kretzschmar <henne@nachtwindheim.de>
2006-10-04 02:15:22 -0700

    [PATCH] mm: fix in kerneldoc
    Fixes an kerneldoc error.
    
    Signed-off-by: Henrik Kretzschmar <henne@nachtwindheim.de>
    Cc: "Randy.Dunlap" <rdunlap@xenotime.net>
    Acked-by: Christoph Hellwig <hch@infradead.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- mm/filemap.c
+++ mm/filemap.c
@@ -1139,11 +1139,11 @@ success:
 }
 
 /**
- * __generic_file_aio_read - generic filesystem read routine
+ * generic_file_aio_read - generic filesystem read routine
  * @iocb:	kernel I/O control block
  * @iov:	io vector request
  * @nr_segs:	number of segments in the iovec
- * @ppos:	current file position
+ * @pos:	current file position
  *
  * This is the "read()" routine for all filesystems
  * that can use the page cache directly.
