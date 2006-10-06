96e1a87392293cf895ea99ab70758453897c7bbf
Pekka Enberg <penberg@cs.helsinki.fi>
2006-10-06 00:44:06 -0700

    [PATCH] um: irq changes break build
    Fixup broken UML build due to 7d12e780e003f93433d49ce78cfedf4b4c52adc5
    "IRQ: Maintain regs pointer globally rather than passing to IRQ handlers".
    
    Cc: David Howells <dhowells@redhat.com>
    Cc: Jeff Dike <jdike@addtoit.com>
    Cc: Paolo "Blaisorblade" Giarrusso <blaisorblade@yahoo.it>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/um/kernel/irq.c
+++ arch/um/kernel/irq.c
@@ -356,7 +356,7 @@ void forward_interrupts(int pid)
 unsigned int do_IRQ(int irq, union uml_pt_regs *regs)
 {
        irq_enter();
-       __do_IRQ(irq, (struct pt_regs *)regs);
+       __do_IRQ(irq);
        irq_exit();
        return 1;
 }
