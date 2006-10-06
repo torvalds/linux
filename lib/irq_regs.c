efbc52f945696702a97a78b12812093f61880c2c
Andrew Morton <akpm@osdl.org>
2006-10-06 00:43:50 -0700

    [PATCH] irq_reqs: export __irq_regs
    Modules might want this.
    
    Cc: David Howells <dhowells@redhat.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- lib/irq_regs.c
+++ lib/irq_regs.c
@@ -8,8 +8,10 @@
  * as published by the Free Software Foundation; either version
  * 2 of the License, or (at your option) any later version.
  */
+#include <linux/module.h>
 #include <asm/irq_regs.h>
 
 #ifndef ARCH_HAS_OWN_IRQ_REGS
 DEFINE_PER_CPU(struct pt_regs *, __irq_regs);
+EXPORT_PER_CPU_SYMBOL(__irq_regs);
 #endif
