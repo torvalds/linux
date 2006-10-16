39af114377bf80d2a88e47be33d578d1fa9b0775
Randy Dunlap <randy.dunlap@oracle.com>
2006-10-16 09:01:46 -0700

    [PATCH] fix epoll_pwait when EPOLL=n
    Fixes http://bugzilla.kernel.org/show_bug.cgi?id=7371
    
    sys_epoll_pwait needs to be listed as a conditional (weak)
    entry point for CONFIG_EPOLL=n.
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/sys_ni.c
+++ kernel/sys_ni.c
@@ -49,6 +49,7 @@ cond_syscall(compat_sys_get_robust_list);
 cond_syscall(sys_epoll_create);
 cond_syscall(sys_epoll_ctl);
 cond_syscall(sys_epoll_wait);
+cond_syscall(sys_epoll_pwait);
 cond_syscall(sys_semget);
 cond_syscall(sys_semop);
 cond_syscall(sys_semtimedop);
