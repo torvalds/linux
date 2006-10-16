29f3eb64634cf96903a3cdb56b1f9a80bebad17d
Alan Cox <alan@lxorguk.ukuu.org.uk>
2006-10-16 16:20:21 -0700

    pci: Additional search functions
    In order to finish converting to pci_get_* interfaces we need to add a couple
    of bits of missing functionaility
    
    pci_get_bus_and_slot() provides the equivalent to pci_find_slot()
    (pci_get_slot is already taken as a name for something similar but not the
    same)
    
    pci_get_device_reverse() is the equivalent of pci_find_device_reverse but
    refcounting
    
    Signed-off-by: Alan Cox <alan@redhat.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -452,13 +452,14 @@ struct pci_dev *pci_find_slot (unsigned int bus, unsigned int devfn);
 int pci_find_capability (struct pci_dev *dev, int cap);
 int pci_find_next_capability (struct pci_dev *dev, u8 pos, int cap);
 int pci_find_ext_capability (struct pci_dev *dev, int cap);
-struct pci_bus * pci_find_next_bus(const struct pci_bus *from);
+struct pci_bus *pci_find_next_bus(const struct pci_bus *from);
 
 struct pci_dev *pci_get_device (unsigned int vendor, unsigned int device, struct pci_dev *from);
 struct pci_dev *pci_get_subsys (unsigned int vendor, unsigned int device,
 				unsigned int ss_vendor, unsigned int ss_device,
 				struct pci_dev *from);
 struct pci_dev *pci_get_slot (struct pci_bus *bus, unsigned int devfn);
+struct pci_dev *pci_get_bus_and_slot (unsigned int bus, unsigned int devfn);
 struct pci_dev *pci_get_class (unsigned int class, struct pci_dev *from);
 int pci_dev_present(const struct pci_device_id *ids);
 
