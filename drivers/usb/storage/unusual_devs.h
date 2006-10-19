bc724b98c5e782c2d6781428ed87768daa34921d
Phil Dibowitz <phil@ipom.com>
2006-10-19 00:11:17 -0700

    USB: usb-storage: Unusual_dev update
    The protocol in this entry is needed for some versions of the device but
    not others. This adds the NEED_OVERRIDE flag to prevent it complaining
    to users who don't need it.
    
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- drivers/usb/storage/unusual_devs.h
+++ drivers/usb/storage/unusual_devs.h
@@ -1236,7 +1236,7 @@ UNUSUAL_DEV( 0x0e21, 0x0520, 0x0100, 0x0100,
 		"Cowon Systems",
 		"iAUDIO M5",
 		US_SC_DEVICE, US_PR_BULK, NULL,
-		0 ),
+		US_FL_NEED_OVERRIDE ),
 
 /* Submitted by Antoine Mairesse <antoine.mairesse@free.fr> */
 UNUSUAL_DEV( 0x0ed1, 0x6660, 0x0100, 0x0300,
