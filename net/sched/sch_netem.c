798b6b19d7a4b6e1ea5340ec8b3b92811e05b81b
Stephen Hemminger <shemminger@osdl.org>
2006-10-22 20:16:57 -0700

    [PATCH] skge, sky2, et all. gplv2 only
    I don't want my code to downgraded to GPLv3 because of
    cut-n-pasted the comments. These files which I hold copyright
    on were started before it was clear what GPLv3 was going to be.
    
    Signed-off-by: Stephen Hemminger <shemminger@osdl.org>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- net/sched/sch_netem.c
+++ net/sched/sch_netem.c
@@ -4,7 +4,7 @@
  * 		This program is free software; you can redistribute it and/or
  * 		modify it under the terms of the GNU General Public License
  * 		as published by the Free Software Foundation; either version
- * 		2 of the License, or (at your option) any later version.
+ * 		2 of the License.
  *
  *  		Many of the algorithms and ideas for this came from
  *		NIST Net which is not copyrighted. 
