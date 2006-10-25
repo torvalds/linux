f4d4c354bca18210296cc0a8f592c0cdb720bf20
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2006-10-25 13:22:27 +1000

    [POWERPC] Fix CHRP platforms with only 8259
    On CHRP platforms with only a 8259 controller, we should set the
    default IRQ host to the 8259 driver's one for the IRQ probing
    fallbacks to work in case the IRQ tree is incorrect (like on
    Pegasos for example). Without this fix, we get a bunch of WARN_ON's
    during boot.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/i8259.h
+++ include/asm-powerpc/i8259.h
@@ -7,6 +7,7 @@
 #ifdef CONFIG_PPC_MERGE
 extern void i8259_init(struct device_node *node, unsigned long intack_addr);
 extern unsigned int i8259_irq(void);
+extern struct irq_host *i8259_get_host(void);
 #else
 extern void i8259_init(unsigned long intack_addr, int offset);
 extern int i8259_irq(void);
