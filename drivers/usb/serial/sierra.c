baafe37c6a58d4ddb8c2c62cd0f20340b4c66b35
Jan Luebbe <jluebbe@lasnet.de>
2006-10-27 18:59:24 +0200

    USB: sierra: Fix id for Sierra Wireless MC8755 in new table
    The new version of sierra.c has introduced tables for the 1 port and 3
    port variants. The device id i added in my last patch needs to be added
    to the 3 port table.
    
    Signed-off-by: Jan Luebbe <jluebbe@lasnet.de>
    Cc: Kevin Lloyd <linux@sierrawireless.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/sierra.c
+++ drivers/usb/serial/sierra.c
@@ -62,6 +62,7 @@ static struct usb_device_id id_table_3port [] = {
 	{ USB_DEVICE(0x1199, 0x0218) },	/* Sierra Wireless MC5720 */
 	{ USB_DEVICE(0x1199, 0x6802) },	/* Sierra Wireless MC8755 */
 	{ USB_DEVICE(0x1199, 0x6803) },	/* Sierra Wireless MC8765 */
+	{ USB_DEVICE(0x1199, 0x6804) },	/* Sierra Wireless MC8755 for Europe */
 	{ USB_DEVICE(0x1199, 0x6812) },	/* Sierra Wireless MC8775 */
 	{ USB_DEVICE(0x1199, 0x6820) },	/* Sierra Wireless AirCard 875 */
 	{ }
