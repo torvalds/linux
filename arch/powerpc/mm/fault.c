a416dd8d9cd3a3fa77b9839e0e6fc3c54927d8c3
Michael Ellerman <michael@ellerman.id.au>
2006-11-08 10:22:59 +1100

    [PATCH] Do a single one-line printk in bad_page_fault()
    bad_page_fault() prints a message telling the user what type of bad
    fault we took. The first line of this message is currently implemented
    as two separate printks. This has the unfortunate effect that if
    several cpus simultaneously take a bad fault, the first and second parts
    of the printk get jumbled up, which looks dodge and is hard to read.
    
    So do a single one-line printk for each fault type.
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/mm/fault.c
+++ arch/powerpc/mm/fault.c
@@ -426,18 +426,21 @@ void bad_page_fault(struct pt_regs *regs, unsigned long address, int sig)
 
 	/* kernel has accessed a bad area */
 
-	printk(KERN_ALERT "Unable to handle kernel paging request for ");
 	switch (regs->trap) {
-		case 0x300:
-		case 0x380:
-			printk("data at address 0x%08lx\n", regs->dar);
-			break;
-		case 0x400:
-		case 0x480:
-			printk("instruction fetch\n");
-			break;
-		default:
-			printk("unknown fault\n");
+	case 0x300:
+	case 0x380:
+		printk(KERN_ALERT "Unable to handle kernel paging request for "
+			"data at address 0x%08lx\n", regs->dar);
+		break;
+	case 0x400:
+	case 0x480:
+		printk(KERN_ALERT "Unable to handle kernel paging request for "
+			"instruction fetch\n");
+		break;
+	default:
+		printk(KERN_ALERT "Unable to handle kernel paging request for "
+			"unknown fault\n");
+		break;
 	}
 	printk(KERN_ALERT "Faulting instruction address: 0x%08lx\n",
 		regs->nip);
