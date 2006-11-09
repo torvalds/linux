2f4713036114dd13d1f4fe433b7f236250b65f5a
NeilBrown <neilb@suse.de>
2006-11-08 17:44:47 -0800

    [PATCH] md: change ONLINE/OFFLINE events to a single CHANGE event
    It turns out that CHANGE is preferred to ONLINE/OFFLINE for various reasons
    (not least of which being that udev understands it already).
    
    So remove the recently added KOBJ_OFFLINE (no-one is likely to care anyway)
    and change the ONLINE to a CHANGE event
    
    Cc: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Neil Brown <neilb@suse.de>
    Cc: Greg KH <greg@kroah.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -3200,7 +3200,7 @@ static int do_md_run(mddev_t * mddev)
 
 	mddev->changed = 1;
 	md_new_event(mddev);
-	kobject_uevent(&mddev->gendisk->kobj, KOBJ_ONLINE);
+	kobject_uevent(&mddev->gendisk->kobj, KOBJ_CHANGE);
 	return 0;
 }
 
@@ -3314,7 +3314,6 @@ static int do_md_stop(mddev_t * mddev, int mode)
 
 			module_put(mddev->pers->owner);
 			mddev->pers = NULL;
-			kobject_uevent(&mddev->gendisk->kobj, KOBJ_OFFLINE);
 			if (mddev->ro)
 				mddev->ro = 0;
 		}
