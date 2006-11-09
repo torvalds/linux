5974eaaedee49c25d7231cc72dc0b7b9d2ef2fd7
Russell King <rmk@dyn-67.arm.linux.org.uk>
2006-11-09 14:37:06 +0000

    [ARM] Move HWCAP_* definitions to asm/elf.h
    Move HWCAP_* definitions into asm/elf.h, where they should belong.
    Since userspace wants to get at these definitions by including
    asm/procinfo.h, include asm/elf.h from this file if __KERNEL__
    is not defined, and issue a warning suggesting to fix the program
    up to use asm/elf.h instead.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/elf.h
+++ include/asm-arm/elf.h
@@ -39,9 +39,21 @@ typedef struct user_fp elf_fpregset_t;
 #endif
 #define ELF_ARCH	EM_ARM
 
-#ifdef __KERNEL__
-#include <asm/procinfo.h>
+/*
+ * HWCAP flags - for elf_hwcap (in kernel) and AT_HWCAP
+ */
+#define HWCAP_SWP	1
+#define HWCAP_HALF	2
+#define HWCAP_THUMB	4
+#define HWCAP_26BIT	8	/* Play it safe */
+#define HWCAP_FAST_MULT	16
+#define HWCAP_FPA	32
+#define HWCAP_VFP	64
+#define HWCAP_EDSP	128
+#define HWCAP_JAVA	256
+#define HWCAP_IWMMXT	512
 
+#ifdef __KERNEL__
 #ifndef __ASSEMBLY__
 /*
  * This yields a mask that user programs can use to figure out what
