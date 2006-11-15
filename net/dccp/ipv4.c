2bda2853150e20ca2a44627d00b5f85af0b24a42
Al Viro <viro@zeniv.linux.org.uk>
2006-11-14 21:28:51 -0800

    [NET]: Annotate csum_tcpudp_magic() callers in net/*
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dccp/ipv4.c
+++ net/dccp/ipv4.c
@@ -344,7 +344,7 @@ out:
 	sock_put(sk);
 }
 
-static inline u16 dccp_v4_csum_finish(struct sk_buff *skb,
+static inline __sum16 dccp_v4_csum_finish(struct sk_buff *skb,
 				      __be32 src, __be32 dst)
 {
 	return csum_tcpudp_magic(src, dst, skb->len, IPPROTO_DCCP, skb->csum);
