9b531ce242159bd26e09b55d8fb4be3f4bf75e24
Russell King <rmk@dyn-67.arm.linux.org.uk>
2006-11-30 14:39:14 +0000

    [ARM] Remove "translated" io macros
    The "translated" io macros were never really used.  Remove them.
    Preserve the L7200 inb() and friends by defining the __io()
    macro, so that the generic versions can be used instead.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-l7200/io.h
+++ include/asm-arm/arch-l7200/io.h
@@ -17,32 +17,11 @@
 /*
  * There are not real ISA nor PCI buses, so we fake it.
  */
+static inline void __iomem *__io(unsigned long addr)
+{
+	return (void __iomem *)addr;
+}
+#define __io(a)	__io(a)
 #define __mem_pci(a)		(a)
-
-/*
- * Translated address IO functions
- *
- * IO address has already been translated to a virtual address
- */
-#define outb_t(v,p)		(*(volatile unsigned char *)(p) = (v))
-#define inb_t(p)		(*(volatile unsigned char *)(p))
-#define outw_t(v,p)		(*(volatile unsigned int *)(p) = (v))
-#define inw_t(p)		(*(volatile unsigned int *)(p))
-#define outl_t(v,p)		(*(volatile unsigned long *)(p) = (v))
-#define inl_t(p)		(*(volatile unsigned long *)(p))
-
-/*
- * FIXME - These are to allow for linking. On all the other
- *         ARM platforms, the entire IO space is contiguous.
- *         The 7200 has three separate IO spaces. The below
- *         macros will eventually become more involved. Use
- *         with caution and don't be surprised by kernel oopses!!!
- */
-#define inb(p)		 	inb_t(p)
-#define inw(p)	 		inw_t(p)
-#define inl(p)	 		inl_t(p)
-#define outb(v,p)		outb_t(v,p)
-#define outw(v,p)		outw_t(v,p)
-#define outl(v,p)		outl_t(v,p)
 
 #endif
