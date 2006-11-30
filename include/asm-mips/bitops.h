0b7883f49810ec91755caa222b3b28f047b8c93b
Ralf Baechle <ralf@linux-mips.org>
2006-11-30 01:14:51 +0000

    [MIPS] Remove unused definition of cpu_to_lelongp()
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/bitops.h
+++ include/asm-mips/bitops.h
@@ -23,13 +23,11 @@
 #define SZLONG_MASK 31UL
 #define __LL		"ll	"
 #define __SC		"sc	"
-#define cpu_to_lelongp(x) cpu_to_le32p((__u32 *) (x))
 #elif (_MIPS_SZLONG == 64)
 #define SZLONG_LOG 6
 #define SZLONG_MASK 63UL
 #define __LL		"lld	"
 #define __SC		"scd	"
-#define cpu_to_lelongp(x) cpu_to_le64p((__u64 *) (x))
 #endif
 
 /*
