b62e8ec2ac580b47c11eb76e8852ac1ec7d617cd
Chen, Kenneth W <kenneth.w.chen@intel.com>
2006-12-06 20:40:43 -0800

    [PATCH] aio: kill pointless ki_nbytes assignment in aio_setup_single_vector
    io_submit_one assigns ki_left = ki_nbytes = iocb->aio_nbytes, then calls
    down to aio_setup_iocb, then to aio_setup_single_vector.  In there,
    ki_nbytes is reassigned to the same value it got two call stack above it.
    There is no need to do so.
    
    Signed-off-by: Ken Chen <kenneth.w.chen@intel.com>
    Acked-by: Zach Brown <zach.brown@oracle.com>
    Cc: Benjamin LaHaise <bcrl@kvack.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/aio.c
+++ fs/aio.c
@@ -1413,7 +1413,6 @@ static ssize_t aio_setup_single_vector(struct kiocb *kiocb)
 	kiocb->ki_iovec->iov_len = kiocb->ki_left;
 	kiocb->ki_nr_segs = 1;
 	kiocb->ki_cur_seg = 0;
-	kiocb->ki_nbytes = kiocb->ki_left;
 	return 0;
 }
 
