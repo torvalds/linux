298ec1e2ac85cecce3eddd167286359358c44d5d
Christoph Lameter <clameter@sgi.com>
2006-12-06 20:32:47 -0800

    [PATCH] Move sighand_cachep to include/signal.h
    Move sighand_cachep definitioni to linux/signal.h
    
    The sighand cache is only used in fs/exec.c and kernel/fork.c.  It is defined
    in kernel/fork.c but only used in fs/exec.c.
    
    The sighand_cachep is related to signal processing.  So add the definition to
    signal.h.
    
    Signed-off-by: Christoph Lameter <clameter@sgi.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/slab.h
+++ include/linux/slab.h
@@ -314,7 +314,6 @@ extern kmem_cache_t	*names_cachep;
 extern kmem_cache_t	*files_cachep;
 extern kmem_cache_t	*filp_cachep;
 extern kmem_cache_t	*fs_cachep;
-extern kmem_cache_t	*sighand_cachep;
 
 #endif	/* __KERNEL__ */
 
