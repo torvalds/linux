eef88d16a2cb641d9915bfdf6377e70fccec9fde
Adrian Bunk <bunk@stusta.de>
2006-12-06 20:34:55 -0800

    [PATCH] fix v850 compilation
    More fallout of the post 2.6.19-rc1 IRQ changes...
    
          CC      init/main.o
        In file included from
        /home/bunk/linux/kernel-2.6/linux-2.6.19-rc6-mm2/include/linux/rtc.h:102,
                         from
        /home/bunk/linux/kernel-2.6/linux-2.6.19-rc6-mm2/include/linux/efi.h:19,
                         from
        /home/bunk/linux/kernel-2.6/linux-2.6.19-rc6-mm2/init/main.c:43:
        /home/bunk/linux/kernel-2.6/linux-2.6.19-rc6-mm2/include/linux/interrupt.h:67:
        error: conflicting types for 'irq_handler_t'
        include2/asm/irq.h:49: error: previous declaration of 'irq_handler_t' was here
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Cc: Miles Bader <uclinux-v850@lsi.nec.co.jp>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-v850/irq.h
+++ include/asm-v850/irq.h
@@ -46,8 +46,6 @@ extern void
 init_irq_handlers (int base_irq, int num, int interval,
 		   struct hw_interrupt_type *irq_type);
 
-typedef void (*irq_handler_t)(int irq, void *data, struct pt_regs *regs);
-
 /* Handle interrupt IRQ.  REGS are the registers at the time of ther
    interrupt.  */
 extern unsigned int handle_irq (int irq, struct pt_regs *regs);
