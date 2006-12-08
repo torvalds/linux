55eec11a50f4c5f53421f49e407e2c92cf25c3ca
Yoshinori Sato <ysato@users.sourceforge.jp>
2006-12-08 14:47:02 +0900

    sh: Kill off unused SE7619 I/O ops.
    This can use the generic routines, so kill off the board-specific ones.
    
    Signed-off-by: Yoshinori Sato <ysato@users.sourceforge.jp>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/boards/se/7619/setup.c
+++ arch/sh/boards/se/7619/setup.c
@@ -8,7 +8,6 @@
 
 #include <linux/init.h>
 #include <linux/platform_device.h>
-#include <asm/se7619.h>
 #include <asm/io.h>
 #include <asm/machvec.h>
 
@@ -19,25 +18,5 @@
 struct sh_machine_vector mv_se __initmv = {
 	.mv_name		= "SolutionEngine",
 	.mv_nr_irqs		= 108,
-	.mv_inb			= se7619_inb,
-	.mv_inw			= se7619_inw,
-	.mv_inl			= se7619_inl,
-	.mv_outb		= se7619_outb,
-	.mv_outw		= se7619_outw,
-	.mv_outl		= se7619_outl,
-
-	.mv_inb_p		= se7619_inb_p,
-	.mv_inw_p		= se7619_inw,
-	.mv_inl_p		= se7619_inl,
-	.mv_outb_p		= se7619_outb_p,
-	.mv_outw_p		= se7619_outw,
-	.mv_outl_p		= se7619_outl,
-
-	.mv_insb		= se7619_insb,
-	.mv_insw		= se7619_insw,
-	.mv_insl		= se7619_insl,
-	.mv_outsb		= se7619_outsb,
-	.mv_outsw		= se7619_outsw,
-	.mv_outsl		= se7619_outsl,
 };
 ALIAS_MV(se)
