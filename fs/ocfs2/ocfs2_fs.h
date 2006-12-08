8903901dbf46bbdf1f70ffe7bc09cb6b97e6728a
Mark Fasheh <mark.fasheh@oracle.com>
2006-12-07 18:05:37 -0800

    ocfs2: Synchronize feature incompat flags in ocfs2_fs.h
    These got a little bit out of date with ocfs2-tools, make things consistent
    again. We reserve a flag for sparse allocation code as that's pretty close
    to testable at this point.
    
    Signed-off-by: Mark Fasheh <mark.fasheh@oracle.com>

--- fs/ocfs2/ocfs2_fs.h
+++ fs/ocfs2/ocfs2_fs.h
@@ -96,9 +96,18 @@
  */
 #define OCFS2_FEATURE_INCOMPAT_HEARTBEAT_DEV	0x0002
 
+/*
+ * tunefs sets this incompat flag before starting the resize and clears it
+ * at the end. This flag protects users from inadvertently mounting the fs
+ * after an aborted run without fsck-ing.
+ */
+#define OCFS2_FEATURE_INCOMPAT_RESIZE_INPROG    0x0004
 
 /* Used to denote a non-clustered volume */
 #define OCFS2_FEATURE_INCOMPAT_LOCAL_MOUNT	0x0008
+
+/* Support for sparse allocation in b-trees */
+#define OCFS2_FEATURE_INCOMPAT_SPARSE_ALLOC	0x0010
 
 /*
  * Flags on ocfs2_dinode.i_flags
