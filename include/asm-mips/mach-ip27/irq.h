2f3643aecd1e4e6a4c34cb32eb0410340f34e97f
Ralf Baechle <ralf@linux-mips.org>
2006-12-11 04:00:37 +0000

    [MIPS] IP27: Don't include <asm/sn/arch.h>.
    Nothing <asm/sn/arch.h> defines is used.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/mach-ip27/irq.h
+++ include/asm-mips/mach-ip27/irq.h
@@ -10,8 +10,6 @@
 #ifndef __ASM_MACH_IP27_IRQ_H
 #define __ASM_MACH_IP27_IRQ_H
 
-#include <asm/sn/arch.h>
-
 /*
  * A hardwired interrupt number is completly stupid for this system - a
  * large configuration might have thousands if not tenthousands of
