335302618f47b43e1204d24a3edb65a3e5bc8c03
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2006-12-12 10:26:55 +0100

    [PATCH] remove unnecessary blk_queue_bounce in SG_IO
    When I converted the original patch, I left unnecessary blk_queue_bounce in
    SG_IO.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: Mike Christie <michaelc@cs.wisc.edu>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- block/scsi_ioctl.c
+++ block/scsi_ioctl.c
@@ -271,13 +271,6 @@ static int sg_io(struct file *file, request_queue_t *q,
 
 	rq->cmd_type = REQ_TYPE_BLOCK_PC;
 
-	/*
-	 * bounce this after holding a reference to the original bio, it's
-	 * needed for proper unmapping
-	 */
-	if (rq->bio)
-		blk_queue_bounce(q, &rq->bio);
-
 	rq->timeout = jiffies_to_msecs(hdr->timeout);
 	if (!rq->timeout)
 		rq->timeout = q->sg_timeout;
