b453917ac45d6077955c096d8d5503754b0c13b4
Magnus Damm <damm@opensource.se>
2006-12-13 15:36:13 +0900

    pcmcia: Remove unused header file code
    pcmcia: Remove unused header file code
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- include/pcmcia/cs_types.h
+++ include/pcmcia/cs_types.h
@@ -34,9 +34,6 @@ typedef u_int	event_t;
 typedef u_char	cisdata_t;
 typedef u_short	page_t;
 
-struct pcmcia_device;
-typedef struct pcmcia_device *client_handle_t;
-
 struct window_t;
 typedef struct window_t *window_handle_t;
 
