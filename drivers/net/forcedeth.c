87046e5062c483820f20c1fa6ed0826bb38fd4f0
Ayaz Abdulla <aabdulla@nvidia.com>
2006-12-19 23:33:32 -0500

    forcedeth: modified comment header
    This patch removes comment that forcedeth is not supported by NVIDIA.
    
    Signed-Off-By: Ayaz Abdulla <aabdulla@nvidia.com>
    
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/forcedeth.c
+++ drivers/net/forcedeth.c
@@ -3,8 +3,7 @@
  *
  * Note: This driver is a cleanroom reimplementation based on reverse
  *      engineered documentation written by Carl-Daniel Hailfinger
- *      and Andrew de Quincey. It's neither supported nor endorsed
- *      by NVIDIA Corp. Use at your own risk.
+ *      and Andrew de Quincey.
  *
  * NVIDIA, nForce and other NVIDIA marks are trademarks or registered
  * trademarks of NVIDIA Corporation in the United States and other
@@ -14,7 +13,7 @@
  * Copyright (C) 2004 Andrew de Quincey (wol support)
  * Copyright (C) 2004 Carl-Daniel Hailfinger (invalid MAC handling, insane
  *		IRQ rate fixes, bigendian fixes, cleanups, verification)
- * Copyright (c) 2004 NVIDIA Corporation
+ * Copyright (c) 2004,5,6 NVIDIA Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
