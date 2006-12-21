b23e3536667373e44f52a907f63cb55f75969490
Al Viro <viro@zeniv.linux.org.uk>
2006-12-21 13:15:18 -0800

    [IPV6]: Dumb typo in generic csum_ipv6_magic()
    ... duh
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/ip6_checksum.h
+++ include/net/ip6_checksum.h
@@ -87,7 +87,7 @@ static __inline__ __sum16 csum_ipv6_magic(const struct in6_addr *saddr,
 	carry = (sum < uproto);
 	sum += carry;
 
-	return csum_fold((__force __wsum)csum);
+	return csum_fold((__force __wsum)sum);
 }
 
 #endif
