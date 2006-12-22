67af63a6ab4ce064f807bdce614fe0fa2bcea252
Tim Chen <tim.c.chen@linux.intel.com>
2006-12-22 01:07:50 -0800

    [PATCH] sched: remove __cpuinitdata anotation to cpu_isolated_map
    The structure cpu_isolated_map is used not only during initialization.
    Multi-core scheduler configuration changes and exclusive cpusets
    use this during run time.  During setting of sched_mc_power_savings
     policy, this structure is accessed to update sched_domains.
    
    Signed-off-by: Tim Chen <tim.c.chen@intel.com>
    Acked-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Acked-by: Ingo Molnar <mingo@elte.hu>
    Cc: <stable@kernel.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/sched.c
+++ kernel/sched.c
@@ -5610,7 +5610,7 @@ static void cpu_attach_domain(struct sched_domain *sd, int cpu)
 }
 
 /* cpus with isolated domains */
-static cpumask_t __cpuinitdata cpu_isolated_map = CPU_MASK_NONE;
+static cpumask_t cpu_isolated_map = CPU_MASK_NONE;
 
 /* Setup the mask of cpus configured for isolated domains */
 static int __init isolated_cpu_setup(char *str)
