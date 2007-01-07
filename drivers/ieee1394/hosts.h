d06170a9ba9c39ac0768676e268cb17f9f68a622
Stefan Richter <stefanr@s5r6.in-berlin.de>
2007-01-07 21:51:48 +0100

    ieee1394: save one word in struct hpsb_host
    hpsb_host.config_roms is a bitfield of which only one bit is currently
    used.  hpsb_host.update_config_rom is only a Boolean.  Neither one is
    accessed in hot code paths or with alignment requirements.
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/ieee1394/hosts.h
+++ drivers/ieee1394/hosts.h
@@ -61,9 +61,9 @@ struct hpsb_host {
 	struct device device;
 	struct class_device class_dev;
 
-	int update_config_rom;
 	struct delayed_work delayed_reset;
-	unsigned int config_roms;
+	unsigned config_roms:31;
+	unsigned update_config_rom:1;
 
 	struct list_head addr_space;
 	u64 low_addr_space;	/* upper bound of physical DMA area */
