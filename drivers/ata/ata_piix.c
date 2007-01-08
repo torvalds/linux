babfb682c93ca78b74d7f3bb07ee0e13831c6f46
J J <jakub007@go2.pl>
2007-01-09 02:26:30 +0900

    ata_piix: add ICH7 on Acer 3682WLMi to laptop list
    In Acer Aspire hdd is connected to ICH7 via 40c cable, however it is
    short cable and it is UDMA66 capable.
    
    Signed-off-by: J J <jakub007@go2.pl>
    Signed-off-by: Tejun Heo <htejun@gmail.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/ata/ata_piix.c
+++ drivers/ata/ata_piix.c
@@ -569,6 +569,7 @@ struct ich_laptop {
 static const struct ich_laptop ich_laptop[] = {
 	/* devid, subvendor, subdev */
 	{ 0x27DF, 0x0005, 0x0280 },	/* ICH7 on Acer 5602WLMi */
+	{ 0x27DF, 0x1025, 0x0110 },	/* ICH7 on Acer 3682WLMi */
 	/* end marker */
 	{ 0, }
 };
