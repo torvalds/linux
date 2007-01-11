56fb5fe9f16f630e3ee663c47c5aff26ac424d05
Michael Buesch <mb@bu3sch.de>
2007-01-10 23:15:43 -0800

    [PATCH] Fix HWRNG built-in initcalls priority
    This changes all HWRNG driver initcalls to module_init().  We must probe
    the RNGs after the major kernel subsystems are already up and running (like
    PCI).
    
    This fixes Bug 7730.
    http://bugzilla.kernel.org/show_bug.cgi?id=7730
    
    Signed-off-by: Michael Buesch <mb@bu3sch.de>
    Cc: Jan Beulich <jbeulich@novell.com>
    Cc: Jeff Garzik <jeff@garzik.org>
    Cc: Greg KH <greg@kroah.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/char/hw_random/intel-rng.c
+++ drivers/char/hw_random/intel-rng.c
@@ -378,7 +378,7 @@ static void __exit mod_exit(void)
 	iounmap(mem);
 }
 
-subsys_initcall(mod_init);
+module_init(mod_init);
 module_exit(mod_exit);
 
 MODULE_DESCRIPTION("H/W RNG driver for Intel chipsets");
