11354d03afe9dd0d114e078057158baad4b4eee9
Pierre Ossman <drzeus@drzeus.cx>
2007-01-14 01:41:45 +0100

    mmc: let host be parent of cards
    Change the parent of cards to be a specific host (a class
    device), not the physical controller. This is particularly
    useful when the hardware has multiple slots, meaning
    multiple hosts.
    
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -127,6 +127,7 @@ static inline void *mmc_priv(struct mmc_host *host)
 }
 
 #define mmc_dev(x)	((x)->parent)
+#define mmc_classdev(x)	(&(x)->class_dev)
 #define mmc_hostname(x)	((x)->class_dev.bus_id)
 
 extern int mmc_suspend_host(struct mmc_host *, pm_message_t);
