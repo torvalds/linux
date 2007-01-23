c5e7655297b5de47a279abcdcd1065690aa25c56
Helge Deller <deller@gmx.de>
2007-01-23 20:50:59 +0100

    [PARISC] implement standard ENTRY(), END() and ENDPROC()
    Use the macros in entry.S
    
    Signed-off-by: Helge Deller <deller@gmx.de>
    Signed-off-by: Kyle McMartin <kyle@parisc-linux.org>

--- include/asm-parisc/linkage.h
+++ include/asm-parisc/linkage.h
@@ -1,6 +1,18 @@
-#ifndef __ASM_LINKAGE_H
-#define __ASM_LINKAGE_H
+#ifndef __ASM_PARISC_LINKAGE_H
+#define __ASM_PARISC_LINKAGE_H
 
-/* Nothing to see here... */
-
+#ifndef __ALIGN
+#define __ALIGN         .align 4
+#define __ALIGN_STR     ".align 4"
 #endif
+
+/*
+ * In parisc assembly a semicolon marks a comment.
+ * Because of that we use an exclamation mark to seperate independend lines.
+ */
+#define ENTRY(name) \
+	.globl name !\
+	ALIGN !\
+name:
+
+#endif  /* __ASM_PARISC_LINKAGE_H */
