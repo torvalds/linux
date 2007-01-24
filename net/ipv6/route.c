6a2b9ce0a383059492c93682bc094cce0f705fff
Noriaki TAKAMIYA <takamiya@po.ntts.co.jp>
2007-01-23 22:09:41 -0800

    [IPV6]: Fixed the size of the netlink message notified by inet6_rt_notify().
    I think the return value of rt6_nlmsg_size() should includes the
    amount of RTA_METRICS.
    
    Signed-off-by: Noriaki TAKAMIYA <takamiya@po.ntts.co.jp>
    Acked-by: Thomas Graf <tgraf@suug.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -2017,6 +2017,7 @@ static inline size_t rt6_nlmsg_size(void)
 	       + nla_total_size(4) /* RTA_IIF */
 	       + nla_total_size(4) /* RTA_OIF */
 	       + nla_total_size(4) /* RTA_PRIORITY */
+	       + RTAX_MAX * nla_total_size(4) /* RTA_METRICS */
 	       + nla_total_size(sizeof(struct rta_cacheinfo));
 }
 
