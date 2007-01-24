0a9b0db19262dbb09f3a34195e68cafd5dc3fa10
Paul Mundt <lethal@linux-sh.org>
2007-01-24 21:56:20 +0900

    [APM] SH: Convert to use shared APM emulation.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/sh/boards/hp6xx/hp6xx_apm.c
+++ arch/sh/boards/hp6xx/hp6xx_apm.c
@@ -7,12 +7,11 @@
  * modify it under the terms of the GNU General Public License.
  */
 #include <linux/module.h>
-#include <linux/apm_bios.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
-#include <asm/io.h>
-#include <asm/apm.h>
+#include <linux/apm-emulation.h>
+#include <linux/io.h>
 #include <asm/adc.h>
 #include <asm/hp6xx.h>
 
@@ -27,60 +26,41 @@
 
 #define MODNAME "hp6x0_apm"
 
-static int hp6x0_apm_get_info(char *buf, char **start, off_t fpos, int length)
+static void hp6x0_apm_get_power_status(struct apm_power_info *info)
 {
+	int battery, backup, charging, percentage;
 	u8 pgdr;
-	char *p;
-	int battery_status;
-	int battery_flag;
-	int ac_line_status;
-	int time_units = APM_BATTERY_LIFE_UNKNOWN;
 
-	int battery = adc_single(ADC_CHANNEL_BATTERY);
-	int backup = adc_single(ADC_CHANNEL_BACKUP);
-	int charging = adc_single(ADC_CHANNEL_CHARGE);
-	int percentage;
+	battery		= adc_single(ADC_CHANNEL_BATTERY);
+	backup		= adc_single(ADC_CHANNEL_BACKUP);
+	charging	= adc_single(ADC_CHANNEL_CHARGE);
 
 	percentage = 100 * (battery - HP680_BATTERY_MIN) /
 			   (HP680_BATTERY_MAX - HP680_BATTERY_MIN);
 
-	ac_line_status = (battery > HP680_BATTERY_AC_ON) ?
+	info->ac_line_status = (battery > HP680_BATTERY_AC_ON) ?
 			 APM_AC_ONLINE : APM_AC_OFFLINE;
-
-	p = buf;
 
 	pgdr = ctrl_inb(SH7709_PGDR);
 	if (pgdr & PGDR_MAIN_BATTERY_OUT) {
-		battery_status = APM_BATTERY_STATUS_NOT_PRESENT;
-		battery_flag = 0x80;
-		percentage = -1;
-	} else if (charging < 8 ) {
-		battery_status = APM_BATTERY_STATUS_CHARGING;
-		battery_flag = 0x08;
-		ac_line_status = 0xff;
+		info->battery_status	= APM_BATTERY_STATUS_NOT_PRESENT;
+		info->battery_flag	= 0x80;
+	} else if (charging < 8) {
+		info->battery_status	= APM_BATTERY_STATUS_CHARGING;
+		info->battery_flag	= 0x08;
+		info->ac_line_status = 0xff;
 	} else if (percentage <= APM_CRITICAL) {
-		battery_status = APM_BATTERY_STATUS_CRITICAL;
-		battery_flag = 0x04;
+		info->battery_status	= APM_BATTERY_STATUS_CRITICAL;
+		info->battery_flag	= 0x04;
 	} else if (percentage <= APM_LOW) {
-		battery_status = APM_BATTERY_STATUS_LOW;
-		battery_flag = 0x02;
+		info->battery_status	= APM_BATTERY_STATUS_LOW;
+		info->battery_flag	= 0x02;
 	} else {
-		battery_status = APM_BATTERY_STATUS_HIGH;
-		battery_flag = 0x01;
+		info->battery_status	= APM_BATTERY_STATUS_HIGH;
+		info->battery_flag	= 0x01;
 	}
 
-	p += sprintf(p, "1.0 1.2 0x%02x 0x%02x 0x%02x 0x%02x %d%% %d %s\n",
-		     APM_32_BIT_SUPPORT,
-		     ac_line_status,
-		     battery_status,
-		     battery_flag,
-		     percentage,
-		     time_units,
-		     "min");
-	p += sprintf(p, "bat=%d backup=%d charge=%d\n",
-		     battery, backup, charging);
-
-	return p - buf;
+	info->units = 0;
 }
 
 static irqreturn_t hp6x0_apm_interrupt(int irq, void *dev)
@@ -96,14 +76,14 @@ static int __init hp6x0_apm_init(void)
 	int ret;
 
 	ret = request_irq(HP680_BTN_IRQ, hp6x0_apm_interrupt,
-			  IRQF_DISABLED, MODNAME, 0);
+			  IRQF_DISABLED, MODNAME, NULL);
 	if (unlikely(ret < 0)) {
 		printk(KERN_ERR MODNAME ": IRQ %d request failed\n",
 		       HP680_BTN_IRQ);
 		return ret;
 	}
 
-	apm_get_info = hp6x0_apm_get_info;
+	apm_get_power_status = hp6x0_apm_get_power_status;
 
 	return ret;
 }
@@ -111,7 +91,7 @@ static int __init hp6x0_apm_init(void)
 static void __exit hp6x0_apm_exit(void)
 {
 	free_irq(HP680_BTN_IRQ, 0);
-	apm_get_info = 0;
+	apm_get_info = NULL;
 }
 
 module_init(hp6x0_apm_init);
