b6558c4a2378af06f2beca6c8a3304e21d1cf135
Roland McGrath <roland@redhat.com>
2007-01-26 00:56:47 -0800

    [PATCH] Fix gate_vma.vm_flags
    This patch fixes the initialization of gate_vma.vm_flags and
    gate_vma.vm_page_prot to reflect reality.  This makes the "[vdso]" line in
    /proc/PID/maps correctly show r-xp instead of ---p, when gate_vma is used
    (CONFIG_COMPAT_VDSO on i386).
    
    Signed-off-by: Roland McGrath <roland@redhat.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Andi Kleen <ak@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/memory.c
+++ mm/memory.c
@@ -2606,8 +2606,8 @@ static int __init gate_vma_init(void)
 	gate_vma.vm_mm = NULL;
 	gate_vma.vm_start = FIXADDR_USER_START;
 	gate_vma.vm_end = FIXADDR_USER_END;
-	gate_vma.vm_page_prot = PAGE_READONLY;
-	gate_vma.vm_flags = 0;
+	gate_vma.vm_flags = VM_READ | VM_MAYREAD | VM_EXEC | VM_MAYEXEC;
+	gate_vma.vm_page_prot = __P101;
 	return 0;
 }
 __initcall(gate_vma_init);
