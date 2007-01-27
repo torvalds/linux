8000d49076f5d1767b6fee9c74b452f488ed89ad
Geoff Levand <geoffrey.levand@am.sony.com>
2007-01-26 19:08:26 -0800

    [POWERPC] ps3: remove unneeded header include
    Remove an unneeded header include from ps3.h.
    
    Signed-off-by: Geoff Levand <geoffrey.levand@am.sony.com>
    Acked-by: Arnd Bergmann <arnd.bergmann@de.ibm.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/ps3.h
+++ include/asm-powerpc/ps3.h
@@ -21,7 +21,6 @@
 #if !defined(_ASM_POWERPC_PS3_H)
 #define _ASM_POWERPC_PS3_H
 
-#include <linux/compiler.h> /* for __deprecated */
 #include <linux/init.h>
 #include <linux/types.h>
 #include <linux/device.h>
