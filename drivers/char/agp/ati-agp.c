7707ea3b784195315366e6e4b5c73ca6933ff9b0
Dave Jones <davej@redhat.com>
2007-01-28 17:50:17 -0500

    [AGPGART] Remove pointless assignment.
    No point in clearing local pointers then returning.
    Also fix up some CodingStyle nits.
    
    Signed-off-by: Dave Jones <davej@redhat.com>

--- drivers/char/agp/ati-agp.c
+++ drivers/char/agp/ati-agp.c
@@ -124,18 +124,18 @@ static int ati_create_gatt_pages(int nr_tables)
 	for (i = 0; i < nr_tables; i++) {
 		entry = kzalloc(sizeof(struct ati_page_map), GFP_KERNEL);
 		if (entry == NULL) {
-			while (i>0) {
-				kfree (tables[i-1]);
+			while (i > 0) {
+				kfree(tables[i-1]);
 				i--;
 			}
-			kfree (tables);
-			tables = NULL;
+			kfree(tables);
 			retval = -ENOMEM;
 			break;
 		}
 		tables[i] = entry;
 		retval = ati_create_page_map(entry);
-		if (retval != 0) break;
+		if (retval != 0)
+			break;
 	}
 	ati_generic_private.num_tables = nr_tables;
 	ati_generic_private.gatt_pages = tables;
