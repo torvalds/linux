d7480a9feaa970d9c37462f21bc27ebab2c56b0b
Ishizaki Kou <kou.ishizaki@toshiba.co.jp>
2007-02-02 16:43:21 +0900

    [POWERPC] Celleb: support udbg
    This patch adds udbg support for Celleb platform.
    
    Signed-off-by: Kou Ishizaki <kou.ishizaki@toshiba.co.jp>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kernel/udbg.c
+++ arch/powerpc/kernel/udbg.c
@@ -45,6 +45,8 @@ void __init udbg_early_init(void)
 #elif defined(CONFIG_PPC_EARLY_DEBUG_ISERIES)
 	/* For iSeries - hit Ctrl-x Ctrl-x to see the output */
 	udbg_init_iseries();
+#elif defined(CONFIG_PPC_EARLY_DEBUG_BEAT)
+	udbg_init_debug_beat();
 #endif
 }
 
--- include/asm-powerpc/udbg.h
+++ include/asm-powerpc/udbg.h
@@ -44,6 +44,7 @@ extern void __init udbg_init_maple_realmode(void);
 extern void __init udbg_init_iseries(void);
 extern void __init udbg_init_rtas_panel(void);
 extern void __init udbg_init_rtas_console(void);
+extern void __init udbg_init_debug_beat(void);
 
 #endif /* __KERNEL__ */
 #endif /* _ASM_POWERPC_UDBG_H */
