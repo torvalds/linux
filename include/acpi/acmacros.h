987c21a0b0081d480ec3cd04875509cdc10e15e7
Bob Moore <robert.moore@intel.com>
2007-02-02 19:48:23 +0300

    ACPICA: Add ACPI_MAX macro
    Signed-off-by: Alexey Starikovskiy <alexey.y.starikovskiy@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- include/acpi/acmacros.h
+++ include/acpi/acmacros.h
@@ -55,6 +55,7 @@
 #define ACPI_SET_BIT(target,bit)        ((target) |= (bit))
 #define ACPI_CLEAR_BIT(target,bit)      ((target) &= ~(bit))
 #define ACPI_MIN(a,b)                   (((a)<(b))?(a):(b))
+#define ACPI_MAX(a,b)                   (((a)>(b))?(a):(b))
 
 /* Size calculation */
 
