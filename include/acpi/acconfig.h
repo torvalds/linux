2b705a8abbce1753c1e5af5ae2ed97e374277654
Bob Moore <robert.moore@intel.com>
2007-02-02 19:48:21 +0300

    ACPICA: Update version to 20060927
    Signed-off-by: Alexey Starikovskiy <alexey.y.starikovskiy@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- include/acpi/acconfig.h
+++ include/acpi/acconfig.h
@@ -63,7 +63,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20060912
+#define ACPI_CA_VERSION                 0x20060927
 
 /*
  * OS name, used for the _OS object.  The _OS object is essentially obsolete,
