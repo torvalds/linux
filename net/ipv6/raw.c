f48d5ff1e44562a0ee87ce8ea3b798ce9d84370d
Masahide NAKAMURA <nakam@linux-ipv6.org>
2007-02-07 00:07:39 -0800

    [IPV6] RAW: Add checksum default defines for MH.
    Add checksum default defines for mobility header(MH) which
    goes through raw socket. As the result kernel's behavior is
    to handle MH checksum as default.
    
    This patch also removes verifying inbound MH checksum at
    mip6_mh_filter() since it did not consider user specified
    checksum offset and was redundant check with raw socket code.
    
    Signed-off-by: Masahide NAKAMURA <nakam@linux-ipv6.org>
    Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/raw.c
+++ net/ipv6/raw.c
@@ -1094,10 +1094,19 @@ static void rawv6_close(struct sock *sk, long timeout)
 
 static int rawv6_init_sk(struct sock *sk)
 {
-	if (inet_sk(sk)->num == IPPROTO_ICMPV6) {
-		struct raw6_sock *rp = raw6_sk(sk);
+	struct raw6_sock *rp = raw6_sk(sk);
+
+	switch (inet_sk(sk)->num) {
+	case IPPROTO_ICMPV6:
 		rp->checksum = 1;
 		rp->offset   = 2;
+		break;
+	case IPPROTO_MH:
+		rp->checksum = 1;
+		rp->offset   = 4;
+		break;
+	default:
+		break;
 	}
 	return(0);
 }
