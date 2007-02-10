d78112e1f1da2a8be1ac0d7b583dcc25aa4f079c
Michael Hanselmann <linux-kernel@hansmi.ch>
2007-02-10 00:47:44 -0500

    ACPI: video: Fix null pointer in appledisplay driver
    commit "ACPI: video: Add dev argument for backlight_device_register"
    519ab5f2be65b72cf12ae99c89752bbe79b44df6
    broke the apple display driver.
    
    Signed-off-by: Michael Hanselmann <linux-kernel@hansmi.ch>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/usb/misc/appledisplay.c
+++ drivers/usb/misc/appledisplay.c
@@ -281,7 +281,7 @@ static int appledisplay_probe(struct usb_interface *iface,
 	/* Register backlight device */
 	snprintf(bl_name, sizeof(bl_name), "appledisplay%d",
 		atomic_inc_return(&count_displays) - 1);
-	pdata->bd = backlight_device_register(bl_name, NULL, NULL,
+	pdata->bd = backlight_device_register(bl_name, NULL, pdata
 						&appledisplay_bl_data);
 	if (IS_ERR(pdata->bd)) {
 		err("appledisplay: Backlight registration failed");
