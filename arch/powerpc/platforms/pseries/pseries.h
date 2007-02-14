8c0238b3f1a7849b89707ac6b7b0c84e1ed2df70
Michael Ellerman <michael@ellerman.id.au>
2007-02-14 16:08:05 +1100

    [POWERPC] Fix cut and paste breakage in arch/powerpc/platforms/pseries/pseries.h
    My "cleanup" patch (dce623e0827e8d0ad60ce7f385c3394bf1b0bae0) had a cut
    and paste error for the !CONFIG_KEXEC case. Fifty lashes for me.
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/platforms/pseries/pseries.h
+++ arch/powerpc/platforms/pseries/pseries.h
@@ -29,8 +29,8 @@ static inline smp_init_pseries_xics(void) { };
 extern void setup_kexec_cpu_down_xics(void);
 extern void setup_kexec_cpu_down_mpic(void);
 #else
-static inline setup_kexec_cpu_down_xics(void) { };
-static inline setup_kexec_cpu_down_mpic(void) { };
+static inline void setup_kexec_cpu_down_xics(void) { }
+static inline void setup_kexec_cpu_down_mpic(void) { }
 #endif
 
 #endif /* _PSERIES_PSERIES_H */
