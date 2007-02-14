4b998d4f6e559fb8eba8141263b2fddc9556391b
Eric W. Biederman <ebiederm@xmission.com>
2007-02-14 00:33:35 -0800

    [PATCH] sysctl: cdrom: don't set de->owner
    There is no need for open files in /proc/sys/XXX to hold a reference count on
    the module that provides the file to prevent module unload races.  While there
    is code active in the module p->used in the sysctl_table_header is
    incremented, preventing the sysctl from being unregisted.  Once the sysctl is
    unregistered it cannot be found.  Open files are also not a problem as they
    revalidate the sysctl information and bump p->used before accessing module
    code.
    
    So setting de->owner is unnecessary, makes for a bad example and gets in my
    way of removing ctl_table->de.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Acked-by: Jens Axboe <jens.axboe@oracle.com>
    Acked-by: James Bottomley <James.Bottomley@SteelEye.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/cdrom/cdrom.c
+++ drivers/cdrom/cdrom.c
@@ -3554,8 +3554,6 @@ static void cdrom_sysctl_register(void)
 		return;
 
 	cdrom_sysctl_header = register_sysctl_table(cdrom_root_table, 0);
-	if (cdrom_root_table->ctl_name && cdrom_root_table->child->de)
-		cdrom_root_table->child->de->owner = THIS_MODULE;
 
 	/* set the defaults */
 	cdrom_sysctl_settings.autoclose = autoclose;
