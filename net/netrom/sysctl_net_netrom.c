6fe6b1755c3d234af076c0d264dc8478acf4510c
Eric W. Biederman <ebiederm@xmission.com>
2007-02-14 00:33:26 -0800

    [PATCH] sysctl: netrom: remove unnecessary insert_at_head flag
    The sysctl numbers used are unique so setting the insert_at_head flag serves
    no semantic purpose, so it is just confusing.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Acked-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- net/netrom/sysctl_net_netrom.c
+++ net/netrom/sysctl_net_netrom.c
@@ -192,7 +192,7 @@ static ctl_table nr_root_table[] = {
 
 void __init nr_register_sysctl(void)
 {
-	nr_table_header = register_sysctl_table(nr_root_table, 1);
+	nr_table_header = register_sysctl_table(nr_root_table, 0);
 }
 
 void nr_unregister_sysctl(void)
