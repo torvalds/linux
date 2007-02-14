208367eeec8d6f6fcee1a0672af2149aeee7b658
Eric W. Biederman <ebiederm@xmission.com>
2007-02-14 00:33:34 -0800

    [PATCH] sysctl: cdrom: remove unnecessary insert_at_head flag
    With unique binary sysctl numbers setting insert_at_head to override other
    sysctl entries is pointless.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Acked-by: James Bottomley <James.Bottomley@SteelEye.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/cdrom/cdrom.c
+++ drivers/cdrom/cdrom.c
@@ -3553,7 +3553,7 @@ static void cdrom_sysctl_register(void)
 	if (initialized == 1)
 		return;
 
-	cdrom_sysctl_header = register_sysctl_table(cdrom_root_table, 1);
+	cdrom_sysctl_header = register_sysctl_table(cdrom_root_table, 0);
 	if (cdrom_root_table->ctl_name && cdrom_root_table->child->de)
 		cdrom_root_table->child->de->owner = THIS_MODULE;
 
