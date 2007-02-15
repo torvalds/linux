c52daa29760818772ee4211be4ee8d1c78b888d5
Steve Wise <swise@opengridcomputing.com>
2007-02-15 13:54:22 -0600

    RDMA/cxgb3: Remove Open Grid Computing copyrights in iw_cxgb3 driver
    Remove the Open Grid Computing copyright.  It shouldn't be there.
    
    Signed-off-by: Steve Wise <swise@opengridcomputing.com>
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/hw/cxgb3/iwch_qp.c
+++ drivers/infiniband/hw/cxgb3/iwch_qp.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2006 Chelsio, Inc. All rights reserved.
- * Copyright (c) 2006 Open Grid Computing, Inc. All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
