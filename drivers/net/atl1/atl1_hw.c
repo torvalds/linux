a62daa4c7544a0f0ca94d61458e3f78f630e0c05
Al Viro <viro@zeniv.linux.org.uk>
2007-02-14 20:13:39 -0600

    atl1: fix bad ioread address
    An ioread32 statement reads the wrong address.  Fix it.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Jay Cliburn <jacliburn@bellsouth.net>
    Signed-off-by: Chris Snook <csnook@redhat.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/atl1/atl1_hw.c
+++ drivers/net/atl1/atl1_hw.c
@@ -357,7 +357,7 @@ void atl1_hash_set(struct atl1_hw *hw, u32 hash_value)
 	 */
 	hash_reg = (hash_value >> 31) & 0x1;
 	hash_bit = (hash_value >> 26) & 0x1F;
-	mta = ioread32((hw + REG_RX_HASH_TABLE) + (hash_reg << 2));
+	mta = ioread32((hw->hw_addr + REG_RX_HASH_TABLE) + (hash_reg << 2));
 	mta |= (1 << hash_bit);
 	iowrite32(mta, (hw->hw_addr + REG_RX_HASH_TABLE) + (hash_reg << 2));
 }
