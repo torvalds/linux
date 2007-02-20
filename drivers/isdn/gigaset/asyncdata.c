d5c1682f9f8f4aa064f81fda67ea7b91fd829512
Adrian Bunk <bunk@stusta.de>
2007-02-20 13:58:17 -0800

    [PATCH] drivers/isdn/gigaset/: build asyncdata.o into the gigaset module
      LD      drivers/isdn/gigaset/built-in.o
    drivers/isdn/gigaset/ser_gigaset.o: In function `gigaset_m10x_send_skb':
    (.text+0xe50): multiple definition of `gigaset_m10x_send_skb'
    drivers/isdn/gigaset/usb_gigaset.o:(.text+0x0): first defined here
    drivers/isdn/gigaset/ser_gigaset.o: In function `gigaset_m10x_input':
    (.text+0x1121): multiple definition of `gigaset_m10x_input'
    drivers/isdn/gigaset/usb_gigaset.o:(.text+0x2d1): first defined here
    make[4]: *** [drivers/isdn/gigaset/built-in.o] Error 1
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Cc: Tilman Schmidt <tilman@imap.cc>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/isdn/gigaset/asyncdata.c
+++ drivers/isdn/gigaset/asyncdata.c
@@ -444,6 +444,7 @@ nextbyte:
 		atomic_set(&inbuf->head, head);
 	}
 }
+EXPORT_SYMBOL_GPL(gigaset_m10x_input);
 
 
 /* == data output ========================================================== */
@@ -591,3 +592,4 @@ int gigaset_m10x_send_skb(struct bc_state *bcs, struct sk_buff *skb)
 
 	return len;	/* ok so far */
 }
+EXPORT_SYMBOL_GPL(gigaset_m10x_send_skb);
