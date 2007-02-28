684b7fe976554d12e6266d7280c87a0f3feff02e
Martin K. Petersen <martin.petersen@oracle.com>
2007-02-27 22:39:44 -0500

    [SCSI] constants.c: cleanup, verbose result printing
    Clean up constants.c and make result printing more user friendly:
    
     - Refactor the command and sense functions so that the actual
       formatting can be called from the various helper functions with the
       correct prefix.
    
     - Replace scsi_print_hostbyte() and scsi_print_driverbyte() with
       scsi_print_result() which is verbose when CONFIG_SCSI_CONSTANTS is
       on.
    
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- include/scsi/scsi_dbg.h
+++ include/scsi/scsi_dbg.h
@@ -5,14 +5,16 @@ struct scsi_cmnd;
 struct scsi_sense_hdr;
 
 extern void scsi_print_command(struct scsi_cmnd *);
-extern void scsi_print_sense_hdr(const char *, struct scsi_sense_hdr *);
 extern void __scsi_print_command(unsigned char *);
-extern void scsi_print_sense(const char *, struct scsi_cmnd *);
+extern void scsi_show_extd_sense(unsigned char, unsigned char);
+extern void scsi_show_sense_hdr(struct scsi_sense_hdr *);
+extern void scsi_print_sense_hdr(const char *, struct scsi_sense_hdr *);
+extern void scsi_print_sense(char *, struct scsi_cmnd *);
 extern void __scsi_print_sense(const char *name,
 			       const unsigned char *sense_buffer,
 			       int sense_len);
-extern void scsi_print_driverbyte(int);
-extern void scsi_print_hostbyte(int);
+extern void scsi_show_result(int);
+extern void scsi_print_result(struct scsi_cmnd *);
 extern void scsi_print_status(unsigned char);
 extern const char *scsi_sense_key_string(unsigned char);
 extern const char *scsi_extd_sense_format(unsigned char, unsigned char);
