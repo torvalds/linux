1463fdbcc797dfcb8574ababbd39cf6205f6ed00
Jeff Dike <jdike@addtoit.com>
2007-02-28 20:13:33 -0800

    [PATCH] uml: pte_mkread fix
    Fix the fact that pte_mkread set _PAGE_RW instead of _PAGE_USER (the logic is
    copied from i386 in most place, so it is really as bad as you're thinking).
    
    Thus currently page tables are more permissive than they should.
    
    Such a change may trigger other latent bugs, so be careful with this.
    
    Signed-off-by: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    Signed-off-by: Jeff Dike <jdike@addtoit.com>
    Cc: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-um/pgtable.h
+++ include/asm-um/pgtable.h
@@ -270,7 +270,7 @@ static inline pte_t pte_wrprotect(pte_t pte)
 
 static inline pte_t pte_mkread(pte_t pte)
 { 
-	pte_set_bits(pte, _PAGE_RW);
+	pte_set_bits(pte, _PAGE_USER);
 	return(pte_mknewprot(pte)); 
 }
 
