c4c31fe0e2298529afa1357ebd9dad54b44c4e19
David S. Miller <davem@sunset.davemloft.net>
2007-03-01 18:10:25 -0800

    [SPARC]: Provide 'get_property()' alias for of_get_property().
    Another powerpc compatibility item, this will allow us to share
    more code with them.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc64/prom.h
+++ include/asm-sparc64/prom.h
@@ -99,6 +99,7 @@ extern struct property *of_find_property(struct device_node *np,
 extern int of_device_is_compatible(struct device_node *device, const char *);
 extern void *of_get_property(struct device_node *node, const char *name,
 			     int *lenp);
+#define get_property(node,name,lenp) of_get_property(node,name,lenp)
 extern int of_set_property(struct device_node *node, const char *name, void *val, int len);
 extern int of_getintprop_default(struct device_node *np,
 				 const char *name,
