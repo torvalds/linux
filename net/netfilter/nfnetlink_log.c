dd16704eba171b32ef0cded3a4f562b33b911066
Michal Miroslaw <mirq-linux@rere.qmqm.pl>
2007-03-04 15:59:20 -0800

    [NETFILTER]: nfnetlink_log: fix possible NULL pointer dereference
    Eliminate possible NULL pointer dereference in nfulnl_recv_config().
    
    Signed-off-by: Michal Miroslaw <mirq-linux@rere.qmqm.pl>
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netfilter/nfnetlink_log.c
+++ net/netfilter/nfnetlink_log.c
@@ -858,6 +858,9 @@ nfulnl_recv_config(struct sock *ctnl, struct sk_buff *skb,
 			ret = -EINVAL;
 			break;
 		}
+
+		if (!inst)
+			goto out;
 	} else {
 		if (!inst) {
 			UDEBUG("no config command, and no instance for "
@@ -911,6 +914,7 @@ nfulnl_recv_config(struct sock *ctnl, struct sk_buff *skb,
 
 out_put:
 	instance_put(inst);
+out:
 	return ret;
 }
 
