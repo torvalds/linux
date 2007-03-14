ac0f1d9894650d900af99bdaed83e110d9dce025
Pablo Neira Ayuso <pablo@netfilter.org>
2007-03-14 16:41:28 -0700

    [NETFILTER]: nfnetlink: remove unrequired check in nfnetlink_get_subsys
    subsys_table is initialized to NULL, therefore just returns NULL in case
    that it is not set.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netfilter/nfnetlink.c
+++ net/netfilter/nfnetlink.c
@@ -94,8 +94,7 @@ static inline struct nfnetlink_subsystem *nfnetlink_get_subsys(u_int16_t type)
 {
 	u_int8_t subsys_id = NFNL_SUBSYS_ID(type);
 
-	if (subsys_id >= NFNL_SUBSYS_COUNT
-	    || subsys_table[subsys_id] == NULL)
+	if (subsys_id >= NFNL_SUBSYS_COUNT)
 		return NULL;
 
 	return subsys_table[subsys_id];
