94985134b7b46848267ed6b734320db01c974e72
Rafael J. Wysocki <rjw@sisk.pl>
2007-03-16 13:38:06 -0800

    [PATCH] swsusp: disable nonboot CPUs before entering platform suspend
    Prevent the WARN_ON() in arch/x86_64/kernel/acpi/sleep.c:init_low_mapping()
    from triggering by disabling nonboot CPUs before we finally enter the
    platform suspend.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/power/user.c
+++ kernel/power/user.c
@@ -398,9 +398,10 @@ static int snapshot_ioctl(struct inode *inode, struct file *filp,
 
 		case PMOPS_ENTER:
 			if (data->platform_suspend) {
+				disable_nonboot_cpus();
 				kernel_shutdown_prepare(SYSTEM_SUSPEND_DISK);
 				error = pm_ops->enter(PM_SUSPEND_DISK);
-				error = 0;
+				enable_nonboot_cpus();
 			}
 			break;
 
