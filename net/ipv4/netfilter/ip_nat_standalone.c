848c29fd648e78fa87d0e399223826ce5dfc1b7a
Patrick McHardy <kaber@trash.net>
2007-03-22 12:30:29 -0700

    [NETFILTER]: nat: avoid rerouting packets if only XFRM policy key changed
    Currently NAT not only reroutes packets in the OUTPUT chain when the
    routing key changed, but also if only the non-routing part of the
    IPsec policy key changed. This breaks ping -I since it doesn't use
    SO_BINDTODEVICE but IP_PKTINFO cmsg to specify the output device, and
    this information is lost.
    
    Only do full rerouting if the routing key changed, and just do a new
    policy lookup with the old route if only the ports changed.
    
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/netfilter/ip_nat_standalone.c
+++ net/ipv4/netfilter/ip_nat_standalone.c
@@ -253,14 +253,17 @@ ip_nat_local_fn(unsigned int hooknum,
 		enum ip_conntrack_dir dir = CTINFO2DIR(ctinfo);
 
 		if (ct->tuplehash[dir].tuple.dst.ip !=
-		    ct->tuplehash[!dir].tuple.src.ip
-#ifdef CONFIG_XFRM
-		    || ct->tuplehash[dir].tuple.dst.u.all !=
-		       ct->tuplehash[!dir].tuple.src.u.all
-#endif
-		    )
+		    ct->tuplehash[!dir].tuple.src.ip) {
 			if (ip_route_me_harder(pskb, RTN_UNSPEC))
 				ret = NF_DROP;
+		}
+#ifdef CONFIG_XFRM
+		else if (ct->tuplehash[dir].tuple.dst.u.all !=
+			 ct->tuplehash[!dir].tuple.src.u.all)
+			if (ip_xfrm_me_harder(pskb))
+				ret = NF_DROP;
+#endif
+
 	}
 	return ret;
 }
