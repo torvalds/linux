9c5b406b9a857a67caf778f096bfc7f4e6b0401a
Paul Mundt <lethal@linux-sh.org>
2007-03-28 17:24:47 +0900

    sh: Kill bogus GCC4 symbol exports.
    __sdivsi3_i4i, __udiv_qrnnd_16, and __udivsi3_i4i don't exist
    outside of the ST compiler, so kill them off.
    
    This causes compile failures with other GCC4 compilers.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/sh_ksyms.c
+++ arch/sh/kernel/sh_ksyms.c
@@ -82,9 +82,6 @@ DECLARE_EXPORT(__movstr);
 DECLARE_EXPORT(__movmem_i4_even);
 DECLARE_EXPORT(__movmem_i4_odd);
 DECLARE_EXPORT(__movmemSI12_i4);
-DECLARE_EXPORT(__sdivsi3_i4i);
-DECLARE_EXPORT(__udiv_qrnnd_16);
-DECLARE_EXPORT(__udivsi3_i4i);
 #else /* GCC 3.x */
 DECLARE_EXPORT(__movstr_i4_even);
 DECLARE_EXPORT(__movstr_i4_odd);
