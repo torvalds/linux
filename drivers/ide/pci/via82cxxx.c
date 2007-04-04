e48059225c2edc6f1e5a2008261f1efdf606f247
Olaf Hering <olaf@aepfle.de>
2007-04-04 18:20:04 +0200

    [POWERPC] Add correct interrupt property for pegasos ide
    The firmware assigns irq 20/21 to the VIA IDE device on Pegasos.
    But the required interrupt is 14/15.
    Maybe someone confused decimal vs. hexadecimal values.
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- drivers/ide/pci/via82cxxx.c
+++ drivers/ide/pci/via82cxxx.c
@@ -35,10 +35,6 @@
 #include <linux/ide.h>
 #include <asm/io.h>
 
-#ifdef CONFIG_PPC_CHRP
-#include <asm/processor.h>
-#endif
-
 #include "ide-timing.h"
 
 #define DISPLAY_VIA_TIMINGS
@@ -435,13 +431,6 @@ static void __devinit init_hwif_via82cxxx(ide_hwif_t *hwif)
 
 	hwif->tuneproc = &via82cxxx_tune_drive;
 	hwif->speedproc = &via_set_drive;
-
-
-#ifdef CONFIG_PPC_CHRP
-	if(machine_is(chrp) && _chrp_type == _CHRP_Pegasos) {
-		hwif->irq = hwif->channel ? 15 : 14;
-	}
-#endif
 
 	for (i = 0; i < 2; i++) {
 		hwif->drives[i].io_32bit = 1;
