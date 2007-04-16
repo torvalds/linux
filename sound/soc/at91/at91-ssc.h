36b8a8bbb402911e59acf13b5074eb8915e47a6a
Frank Mandarino <fmandarino@endrelia.com>
2007-04-16 17:18:52 +0200

    [ALSA] ASoC AT91xxxx - SSC port DSP support
    This patch series by Frank Madarino updates the AT91xxxx core to add
    DSP/PCM audio hardware formats.
    Changes:-
     o Rename at19-i2s.c -> at91-ssc.c
     o Rename at91-i2s.h -> at91-ssc.h
     o Add DSP hardware formats.
     o Rename various I2S labels to SSC
    
    Signed-off-by: Frank Mandarino <fmandarino@endrelia.com>
    Signed-off-by: Liam Girdwood <lg@opensource.wolfsonmicro.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/soc/at91/at91-ssc.h
+++ sound/soc/at91/at91-ssc.h
@@ -1,5 +1,5 @@
 /*
- * at91-i2s.h - ALSA I2S interface for the Atmel AT91 SoC
+ * at91-ssc.h - ALSA SSC interface for the Atmel AT91 SoC
  *
  * Author:	Frank Mandarino <fmandarino@endrelia.com>
  *		Endrelia Technologies Inc.
@@ -10,18 +10,18 @@
  * published by the Free Software Foundation.
  */
 
-#ifndef _AT91_I2S_H
-#define _AT91_I2S_H
+#ifndef _AT91_SSC_H
+#define _AT91_SSC_H
 
-/* I2S system clock ids */
+/* SSC system clock ids */
 #define AT91_SYSCLK_MCK		0 /* SSC uses AT91 MCK as system clock */
 
-/* I2S divider ids */
+/* SSC divider ids */
 #define AT91SSC_CMR_DIV		0 /* MCK divider for BCLK */
 #define AT91SSC_TCMR_PERIOD	1 /* BCLK divider for transmit FS */
 #define AT91SSC_RCMR_PERIOD	2 /* BCLK divider for receive FS */
 
-extern struct snd_soc_cpu_dai at91_i2s_dai[];
+extern struct snd_soc_cpu_dai at91_ssc_dai[];
 
-#endif /* _AT91_I2S_H */
+#endif /* _AT91_SSC_H */
 
