2933d42cb7b0f14e0f83f6f231c966e97c79cdc9
Larry Finger <Larry.Finger@lwfinger.net>
2007-04-17 10:28:47 -0500

    [PATCH] ieee80211: add missing global needed by IEEE80211_DEBUG_XXXX
    Using any of the IEEE80211_DEBUG_XXXX macros in any ieee80211_crypt
    routine built as a module results in a missing global for
    'ieee80211_debug_level'.  The fix is to export the symbol in ieee80211.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/ieee80211/ieee80211_module.c
+++ net/ieee80211/ieee80211_module.c
@@ -229,6 +229,7 @@ void free_ieee80211(struct net_device *dev)
 
 static int debug = 0;
 u32 ieee80211_debug_level = 0;
+EXPORT_SYMBOL_GPL(ieee80211_debug_level);
 static struct proc_dir_entry *ieee80211_proc = NULL;
 
 static int show_debug_level(char *page, char **start, off_t offset,
