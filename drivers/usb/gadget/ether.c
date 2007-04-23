d2eef1fc99640f7de302dcdbcfbdfcec2eef13f6
Li Yang <leoli@freescale.com>
2007-04-23 10:37:36 -0700

    USB: update gadget files for fsl_usb2_udc driver
    Update gadget_chip.c, ether.c for newly added Freescale Highspeed USB
    device driver.
    
    Signed-off-by: Li Yang <leoli@freescale.com>
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/ether.c
+++ drivers/usb/gadget/ether.c
@@ -282,6 +282,9 @@ MODULE_PARM_DESC(host_addr, "Host Ethernet Address");
 #define DEV_CONFIG_CDC
 #endif
 
+#ifdef CONFIG_USB_GADGET_FSL_USB2
+#define DEV_CONFIG_CDC
+#endif
 
 /* For CDC-incapable hardware, choose the simple cdc subset.
  * Anything that talks bulk (without notable bugs) can do this.
