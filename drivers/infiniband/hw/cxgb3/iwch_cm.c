60be4b5966e22040f97db9dada72841bf90479d1
Steve Wise <swise@opengridcomputing.com>
2007-04-26 15:21:15 -0500

    RDMA/cxgb3: Initialize cpu_idx field in cpl_close_listserv_req message
    Signed-off-by: Steve Wise <swise@opengridcomputing.com>
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/hw/cxgb3/iwch_cm.c
+++ drivers/infiniband/hw/cxgb3/iwch_cm.c
@@ -1189,6 +1189,7 @@ static int listen_stop(struct iwch_listen_ep *ep)
 	}
 	req = (struct cpl_close_listserv_req *) skb_put(skb, sizeof(*req));
 	req->wr.wr_hi = htonl(V_WR_OP(FW_WROPCODE_FORWARD));
+	req->cpu_idx = 0;
 	OPCODE_TID(req) = htonl(MK_OPCODE_TID(CPL_CLOSE_LISTSRV_REQ, ep->stid));
 	skb->priority = 1;
 	ep->com.tdev->send(ep->com.tdev, skb);
