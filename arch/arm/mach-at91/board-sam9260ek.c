3e18c8dd0d3456f96e13bc2e72197bfae11d8501
Sam Ravnborg <sam@ravnborg.org>
2007-04-26 00:04:39 -0700

    [ARM] fix section mismatch warning in board-sam9260
    Andrew Morton found a section mismatch warning in x86_64 triggered by a
    wrongly placed __initdata marker.
    
    git grep "struct __initdata" revealed that board-sam9260.c had the same
    problem.
    
    This patch fixes this by placing the __initdata marker correct.  It was
    checked with objdump that the variable was moved to .init.data by this
    change.
    
    Fixed an unrelated section mismatch warning while touching the file.
    
    Both changes are only compile tested but obvious correct.
    [Used at91sam9260ek_defconfig to get compile coverage]
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-at91/board-sam9260ek.c
+++ arch/arm/mach-at91/board-sam9260ek.c
@@ -118,7 +118,7 @@ static struct spi_board_info ek_spi_devices[] = {
 /*
  * MACB Ethernet device
  */
-static struct __initdata at91_eth_data ek_macb_data = {
+static struct at91_eth_data __initdata ek_macb_data = {
 	.phy_irq_pin	= AT91_PIN_PA7,
 	.is_rmii	= 1,
 };
@@ -140,7 +140,7 @@ static struct mtd_partition __initdata ek_nand_partition[] = {
 	},
 };
 
-static struct mtd_partition *nand_partitions(int size, int *num_partitions)
+static struct mtd_partition * __init nand_partitions(int size, int *num_partitions)
 {
 	*num_partitions = ARRAY_SIZE(ek_nand_partition);
 	return ek_nand_partition;
