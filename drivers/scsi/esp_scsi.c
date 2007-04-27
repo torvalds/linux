e1f2a094bdfa8e7ecc31f048e6c6dbea2b4f5f74
Alexey Dobriyan <adobriyan@gmail.com>
2007-04-27 15:19:27 -0700

    [SCSI] esp_scsi.c: Fix compilation.
    irqreturn.h for irqreturn_t and dma_addr_t being u128 warnings ;-)
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/scsi/esp_scsi.c
+++ drivers/scsi/esp_scsi.c
@@ -13,6 +13,7 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/init.h>
+#include <linux/irqreturn.h>
 
 #include <asm/irq.h>
 #include <asm/io.h>
@@ -1706,17 +1707,17 @@ again:
 		if (!dma_len) {
 			printk(KERN_ERR PFX "esp%d: DMA length is zero!\n",
 			       esp->host->unique_id);
-			printk(KERN_ERR PFX "esp%d: cur adr[%08x] len[%08x]\n",
+			printk(KERN_ERR PFX "esp%d: cur adr[%08llx] len[%08x]\n",
 			       esp->host->unique_id,
-			       esp_cur_dma_addr(ent, cmd),
+			       (unsigned long long)esp_cur_dma_addr(ent, cmd),
 			       esp_cur_dma_len(ent, cmd));
 			esp_schedule_reset(esp);
 			return 0;
 		}
 
-		esp_log_datastart("ESP: start data addr[%08x] len[%u] "
+		esp_log_datastart("ESP: start data addr[%08llx] len[%u] "
 				  "write(%d)\n",
-				  dma_addr, dma_len, write);
+				  (unsigned long long)dma_addr, dma_len, write);
 
 		esp->ops->send_dma_cmd(esp, dma_addr, dma_len, dma_len,
 				       write, ESP_CMD_DMA | ESP_CMD_TI);
