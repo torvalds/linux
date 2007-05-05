8ae5d31263c746f1680d005b33a82d167cdb9eb6
David Woodhouse <dwmw2@infradead.org>
2007-05-05 17:50:25 +0100

    [JFFS2] Fix BUG() caused by failing to discard xattrs on deleted files.
    When we cannot mark nodes as obsolete, such as on NAND flash, we end up 
    having to delete inodes with !nlink in jffs2_build_remove_unlinked_inode().
    However, jffs2_build_xattr_subsystem() runs later than this, and will
    attach an xref to the dead inode. Then later when the last nodes of that
    dead inode are erased we hit a BUG() in jffs2_del_ino_cache() 
    because we're not supposed to get there with an xattr still attached to 
    the inode which is being killed.
    
    The simple fix is to refrain from attaching xattrs to inodes with zero 
    nlink, in jffs2_build_xattr_subsystem(). It's it's OK to trust nlink 
    here because the file system isn't actually mounted yet, so there's no 
    chance that a zero-nlink file could actually be alive still because 
    it's open.
    
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- fs/jffs2/xattr.c
+++ fs/jffs2/xattr.c
@@ -825,7 +825,7 @@ void jffs2_build_xattr_subsystem(struct jffs2_sb_info *c)
 			   ref->xd and ref->ic are not valid yet. */
 			xd = jffs2_find_xattr_datum(c, ref->xid);
 			ic = jffs2_get_ino_cache(c, ref->ino);
-			if (!xd || !ic) {
+			if (!xd || !ic || !ic->nlink) {
 				dbg_xattr("xref(ino=%u, xid=%u, xseqno=%u) is orphan.\n",
 					  ref->ino, ref->xid, ref->xseqno);
 				ref->xseqno |= XREF_DELETE_MARKER;
