a5fd22ebc71eecb7b61e542d34ac50a417b3031b
Olof Johansson <olof@lixom.net>
2007-05-08 00:48:02 -0500

    pasemi_mac: Use local-mac-address instead of mac-address if available
    Use local-mac-address in the device tree instead. Fall back to mac-address
    for older firmware.
    
    Signed-off-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/pasemi_mac.c
+++ drivers/net/pasemi_mac.c
@@ -94,7 +94,12 @@ static int pasemi_get_mac_addr(struct pasemi_mac *mac)
 		return -ENOENT;
 	}
 
-	maddr = get_property(dn, "mac-address", NULL);
+	maddr = get_property(dn, "local-mac-address", NULL);
+
+	/* Fall back to mac-address for older firmware */
+	if (maddr == NULL)
+		maddr = get_property(dn, "mac-address", NULL);
+
 	if (maddr == NULL) {
 		dev_warn(&pdev->dev,
 			 "no mac address in device tree, not configuring\n");
