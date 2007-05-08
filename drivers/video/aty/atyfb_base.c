3a2842480bbef42c3c90e14c1f378360d8c20a0c
Antonino A. Daplas <adaplas@gmail.com>
2007-05-08 00:37:34 -0700

    atyfb: kill dead code
    Coverity Bug 68, kill dead code.
    
    Signed-off-by: Antonino Daplas <adaplas@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/aty/atyfb_base.c
+++ drivers/video/aty/atyfb_base.c
@@ -2338,6 +2338,7 @@ static int __devinit aty_init(struct fb_info *info)
 		case DAC_IBMRGB514:
 			par->dac_ops = &aty_dac_ibm514;
 			break;
+#ifdef CONFIG_ATARI
 		case DAC_ATI68860_B:
 		case DAC_ATI68860_C:
 			par->dac_ops = &aty_dac_ati68860b;
@@ -2346,6 +2347,7 @@ static int __devinit aty_init(struct fb_info *info)
 		case DAC_ATT21C498:
 			par->dac_ops = &aty_dac_att21c498;
 			break;
+#endif
 		default:
 			PRINTKI("aty_init: DAC type not implemented yet!\n");
 			par->dac_ops = &aty_dac_unsupported;
