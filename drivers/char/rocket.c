8d5916d3e0e1cff2e9ecf7fc2d191a2cf2146bd5
Jiri Slaby <jirislaby@gmail.com>
2007-05-08 00:27:05 -0700

    Char: rocket, add MODULE_DEVICE_TABLE
    rocket, add MODULE_DEVICE_TABLE
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/char/rocket.c
+++ drivers/char/rocket.c
@@ -1848,6 +1848,12 @@ static void rp_flush_buffer(struct tty_struct *tty)
 
 #ifdef CONFIG_PCI
 
+static struct pci_device_id __devinitdata rocket_pci_ids[] = {
+	{ PCI_DEVICE(PCI_VENDOR_ID_RP, PCI_ANY_ID) },
+	{ }
+};
+MODULE_DEVICE_TABLE(pci, rocket_pci_ids);
+
 /*
  *  Called when a PCI card is found.  Retrieves and stores model information,
  *  init's aiopic and serial port hardware.
