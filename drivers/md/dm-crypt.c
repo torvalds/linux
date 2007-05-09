9c89f8be1a7d14aad9d2c3f7d90d7d88f82c61e2
Milan Broz <mbroz@redhat.com>
2007-05-09 02:32:51 -0700

    dm crypt: disable barriers
    Disable barriers in dm-crypt because of current workqueue processing can
    reorder requests.
    
    This must be addresed later but for now disabling barriers is needed to
    prevent data corruption.
    
    Signed-off-by: Milan Broz <mbroz@redhat.com>
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>
    Cc: Jens Axboe <jens.axboe@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -954,6 +954,9 @@ static int crypt_map(struct dm_target *ti, struct bio *bio,
 	struct crypt_config *cc = ti->private;
 	struct crypt_io *io;
 
+	if (bio_barrier(bio))
+		return -EOPNOTSUPP;
+
 	io = mempool_alloc(cc->io_pool, GFP_NOIO);
 	io->target = ti;
 	io->base_bio = bio;
