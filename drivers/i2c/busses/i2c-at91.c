c91e23c63f77a7643d857a082248cf2cdb71b67c
Alexey Dobriyan <adobriyan@gmail.com>
2007-05-10 03:15:58 -0700

    i2c-at91: compile fix (IS_ERR)
      CC      drivers/i2c/busses/i2c-at91.o
    drivers/i2c/busses/i2c-at91.c: In function 'at91_i2c_probe':
    drivers/i2c/busses/i2c-at91.c:213: warning: implicit declaration of function 'IS_ERR'
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: David Brownell <david-b@pacbell.net>
    Acked-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/i2c/busses/i2c-at91.c
+++ drivers/i2c/busses/i2c-at91.c
@@ -16,6 +16,7 @@
 #include <linux/module.h>
 #include <linux/version.h>
 #include <linux/kernel.h>
+#include <linux/err.h>
 #include <linux/slab.h>
 #include <linux/types.h>
 #include <linux/delay.h>
