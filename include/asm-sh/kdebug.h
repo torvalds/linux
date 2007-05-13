fce692e7986f9689a8f414a74e58c4a6d35d5551
Christoph Hellwig <hch@lst.de>
2007-05-14 08:18:14 +0900

    sh: revert addition of page fault notifiers
    Just at the time you added them on sh we're removing them from other
    architectures. As there's no user yet this patch just removes them
    completely. Once you actually have a kprobes patch it should follow
    the direct call to kprobes_fault_handler model that powerpc, s390 and
    sparc64 employ in 2.6.22-rc1 and that I'm updating other architectures
    to.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/kdebug.h
+++ include/asm-sh/kdebug.h
@@ -6,10 +6,6 @@
 /* Grossly misnamed. */
 enum die_val {
 	DIE_TRAP,
-	DIE_PAGE_FAULT,
 };
-
-int register_page_fault_notifier(struct notifier_block *nb);
-int unregister_page_fault_notifier(struct notifier_block *nb);
 
 #endif /* __ASM_SH_KDEBUG_H */
