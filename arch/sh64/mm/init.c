6c645ac72582bacb85b90a1cf88e81a13045aba4
Paul Mundt <lethal@linux-sh.org>
2007-05-14 09:55:35 +0900

    sh64: generic quicklist support.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh64/mm/init.c
+++ arch/sh64/mm/init.c
@@ -84,7 +84,7 @@ void show_mem(void)
 	printk("%d reserved pages\n",reserved);
 	printk("%d pages shared\n",shared);
 	printk("%d pages swap cached\n",cached);
-	printk("%ld pages in page table cache\n",pgtable_cache_size);
+	printk("%ld pages in page table cache\n", quicklist_total_size());
 }
 
 /*
