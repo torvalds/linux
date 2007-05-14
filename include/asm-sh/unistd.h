26bbfda8b1a4d491a86c60b3a97948a4f49fb86a
Paul Mundt <lethal@linux-sh.org>
2007-05-14 09:29:55 +0900

    sh: Wire up signalfd/timerfd/eventfd syscalls.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/unistd.h
+++ include/asm-sh/unistd.h
@@ -329,8 +329,11 @@
 #define __NR_getcpu		318
 #define __NR_epoll_pwait	319
 #define __NR_utimensat		320
+#define __NR_signalfd		321
+#define __NR_timerfd		322
+#define __NR_eventfd		323
 
-#define NR_syscalls 321
+#define NR_syscalls 324
 
 #ifdef __KERNEL__
 
