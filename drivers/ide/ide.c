793a97228d3da876f42b7fb4d4a52cc8cc86dc81
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2007-05-16 00:51:43 +0200

    ide: always disable DMA before tuning it
    ide_start_power_step() and set_using_dma() were missing ->dma_off_quietly
    call (comment in probe_hwif() states that DMA should be always cleared before
    tuning is attempted).  Fix it.
    
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/ide.c
+++ drivers/ide/ide.c
@@ -910,6 +910,7 @@ int set_using_dma(ide_drive_t *drive, int arg)
 	err = 0;
 
 	if (arg) {
+		hwif->dma_off_quietly(drive);
 		if (ide_set_dma(drive) || hwif->ide_dma_on(drive))
 			err = -EIO;
 	} else
