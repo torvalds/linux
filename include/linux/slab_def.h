3ca12ee549f7837b8a685dddc9515f9fc28434ee
Christoph Lameter <clameter@sgi.com>
2007-05-16 22:10:52 -0700

    SLAB: Move two remaining SLAB specific definitions to slab_def.h
    Two definitions remained in slab.h that are particular to the SLAB allocator.
    Move to slab_def.h
    
    Signed-off-by: Christoph Lameter <clameter@sgi.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/slab_def.h
+++ include/linux/slab_def.h
@@ -109,4 +109,7 @@ found:
 
 #endif	/* CONFIG_NUMA */
 
+extern const struct seq_operations slabinfo_op;
+ssize_t slabinfo_write(struct file *, const char __user *, size_t, loff_t *);
+
 #endif	/* _LINUX_SLAB_DEF_H */
