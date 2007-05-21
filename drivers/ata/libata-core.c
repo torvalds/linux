39f198868dfd4216ff221d741c72a009d995a7ba
Dave Jones <davej@redhat.com>
2007-05-21 14:31:03 -0400

    libata: Add Seagate STT20000A to DMA blacklist.
    http://bugzilla.kernel.org/show_bug.cgi?id=1044 points out an
    additional hard disk that doesn't handle DMA transfers correctly.
    This patch is the libata variant of the earlier patch to drivers/ide/
    
    Signed-off-by: Dave Jones <davej@redhat.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -3771,6 +3771,7 @@ static const struct ata_blacklist_entry ata_device_blacklist [] = {
 	{ "ATAPI CD-ROM DRIVE 40X MAXIMUM",NULL,ATA_HORKAGE_NODMA },
 	{ "_NEC DV5800A", 	NULL,		ATA_HORKAGE_NODMA },
 	{ "SAMSUNG CD-ROM SN-124","N001",	ATA_HORKAGE_NODMA },
+	{ "Seagate STT20000A", NULL,		ATA_HORKAGE_NODMA },
 
 	/* Weird ATAPI devices */
 	{ "TORiSAN DVD-ROM DRD-N216", NULL,	ATA_HORKAGE_MAX_SEC_128 |
