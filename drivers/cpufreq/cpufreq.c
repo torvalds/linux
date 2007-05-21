0a4b2ccc555fa2ca6873d60219047104e4805d45
Thomas Renninger <trenn@suse.de>
2007-05-21 07:20:04 -0500

    [CPUFREQ] check return value of sysfs_create_file
    Eliminate build warning (sysfs_create_file return value must be checked)
    
    Signed-off-by: Thomas Renninger <trenn@suse.de>
    Signed-off-by: Dave Jones <davej@redhat.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -826,13 +826,18 @@ static int cpufreq_add_dev (struct sys_device * sys_dev)
 	/* set up files for this cpu device */
 	drv_attr = cpufreq_driver->attr;
 	while ((drv_attr) && (*drv_attr)) {
-		sysfs_create_file(&policy->kobj, &((*drv_attr)->attr));
+		if (sysfs_create_file(&policy->kobj, &((*drv_attr)->attr)))
+			goto err_out_driver_exit;
 		drv_attr++;
 	}
-	if (cpufreq_driver->get)
-		sysfs_create_file(&policy->kobj, &cpuinfo_cur_freq.attr);
-	if (cpufreq_driver->target)
-		sysfs_create_file(&policy->kobj, &scaling_cur_freq.attr);
+	if (cpufreq_driver->get){
+		if (sysfs_create_file(&policy->kobj, &cpuinfo_cur_freq.attr))
+			goto err_out_driver_exit;
+	}
+	if (cpufreq_driver->target){
+		if (sysfs_create_file(&policy->kobj, &scaling_cur_freq.attr))
+			goto err_out_driver_exit;
+	}
 
 	spin_lock_irqsave(&cpufreq_driver_lock, flags);
 	for_each_cpu_mask(j, policy->cpus) {
