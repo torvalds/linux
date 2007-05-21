31d106c68b1af88835a474556052d6efbfec99c5
Paul Mundt <lethal@linux-sh.org>
2007-05-21 15:10:04 +0900

    sh: Fix dreamcast build for IRQ changes.
    When the irq.h changes went in, the dreamcast code was still
    referencing an old value. Switch it back to the IRQ number,
    which fixes this:
    
    arch/sh/boards/dreamcast/irq.c: In function `disable_systemasic_irq':
    arch/sh/boards/dreamcast/irq.c:59: error: `OFFCHIP_IRQ_BASE' undeclared (first
    use in this function)
    arch/sh/boards/dreamcast/irq.c:59: error: (Each undeclared identifier is reported only once
    arch/sh/boards/dreamcast/irq.c:59: error: for each function it appears in.)
    
    Reported-by: Adrian McMenamin <adrian@newgolddream.dyndns.info>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/dreamcast/sysasic.h
+++ include/asm-sh/dreamcast/sysasic.h
@@ -23,7 +23,7 @@
    takes.
 */
 
-#define HW_EVENT_IRQ_BASE  OFFCHIP_IRQ_BASE /* 48 */
+#define HW_EVENT_IRQ_BASE  48
 
 /* IRQ 13 */
 #define HW_EVENT_VSYNC     (HW_EVENT_IRQ_BASE +  5) /* VSync */
