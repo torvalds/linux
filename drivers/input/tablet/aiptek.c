0112db36ef95e5632db071297f7df130725c47b9
Rene van Paassen <rene.vanpaassen@gmail.com>
2007-05-21 00:32:12 -0400

    Input: aiptek - tolerate newlines in sysfs files
    Now echo "some value" > /sys/......./somefile is also acceptable.
    
    Signed-off-by: Rene van Paassen <rene.vanpaassen@gmail.com>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/tablet/aiptek.c
+++ drivers/input/tablet/aiptek.c
@@ -375,8 +375,11 @@ static int map_str_to_val(const struct aiptek_map *map, const char *str, size_t 
 {
 	const struct aiptek_map *p;
 
+	if (str[count - 1] == '\n')
+		count--;
+
 	for (p = map; p->string; p++)
-		if (!strncmp(str, p->string, count))
+	        if (!strncmp(str, p->string, count))
 			return p->value;
 
 	return AIPTEK_INVALID_VALUE;
