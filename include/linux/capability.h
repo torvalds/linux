b7add02d6247bff34005e040347d81777c80931c
Andrew Morton <akpm@linux-foundation.org>
2007-05-23 13:57:39 -0700

    capability.h warning fix
    include/linux/capability.h:397: warning: "struct task_struct" declared inside parameter list
    include/linux/capability.h:397: warning: its scope is only this definition or declaration, which is probably not what you want
    
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/capability.h
+++ include/linux/capability.h
@@ -16,6 +16,8 @@
 #include <linux/types.h>
 #include <linux/compiler.h>
 
+struct task_struct;
+
 /* User-level do most of the mapping between kernel and user
    capabilities based on the version tag given by the kernel. The
    kernel might be somewhat backwards compatible, but don't bet on
