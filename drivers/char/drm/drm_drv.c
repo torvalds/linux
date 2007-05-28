6f710105f5a4971078c46ae258cbb76db1ee967a
Dave Airlie <airlied@linux.ie>
2007-05-28 19:41:35 +1000

    drm: fix typo on code drm getsarea
    Signed-off-by: Dave Airlie <airlied@linux.ie>

--- drivers/char/drm/drm_drv.c
+++ drivers/char/drm/drm_drv.c
@@ -522,7 +522,7 @@ drm_local_map_t *drm_getsarea(struct drm_device *dev)
 {
 	drm_map_list_t *entry;
 
-	list_for_each_entry(entry, &dev->maplist->head, head) {
+	list_for_each_entry(entry, &dev->maplist, head) {
 		if (entry->map && entry->map->type == _DRM_SHM &&
 		    (entry->map->flags & _DRM_CONTAINS_LOCK)) {
 			return entry->map;
