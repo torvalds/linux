d22d9b3a1d129349518c807e7997922f869e1f48
Paul Mundt <lethal@linux-sh.org>
2007-06-01 14:21:13 +0900

    sh: Register multiple nodes in topology_init().
    If we have multiple nodes, register these at topology_init() time.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/topology.c
+++ arch/sh/kernel/topology.c
@@ -2,12 +2,19 @@
 #include <linux/cpumask.h>
 #include <linux/init.h>
 #include <linux/percpu.h>
+#include <linux/node.h>
+#include <linux/nodemask.h>
 
 static DEFINE_PER_CPU(struct cpu, cpu_devices);
 
 static int __init topology_init(void)
 {
 	int i, ret;
+
+#ifdef CONFIG_NEED_MULTIPLE_NODES
+	for_each_online_node(i)
+		register_one_node(i);
+#endif
 
 	for_each_present_cpu(i) {
 		ret = register_cpu(&per_cpu(cpu_devices, i), i);
