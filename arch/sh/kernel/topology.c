05a117847b43d44f336bbf272a1063661431a5e5
Paul Mundt <lethal@linux-sh.org>
2007-06-07 11:29:37 +0900

    sh: Fix up cpu to node mapping in sysfs.
    Currently cpu_to_node() is always 0 in the UP case, though
    we do want to have the CPU association linked in under sysfs
    even in the cases where we're only on a single CPU.
    
    Fix this up, so we have the cpu0 link on all of the available
    nodes that don't already have a CPU link of their own.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/topology.c
+++ arch/sh/kernel/topology.c
@@ -1,3 +1,12 @@
+/*
+ * arch/sh/kernel/topology.c
+ *
+ *  Copyright (C) 2007  Paul Mundt
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ */
 #include <linux/cpu.h>
 #include <linux/cpumask.h>
 #include <linux/init.h>
@@ -22,6 +31,18 @@ static int __init topology_init(void)
 			printk(KERN_WARNING "%s: register_cpu %d failed (%d)\n",
 			       __FUNCTION__, i, ret);
 	}
+
+#if defined(CONFIG_NUMA) && !defined(CONFIG_SMP)
+	/*
+	 * In the UP case, make sure the CPU association is still
+	 * registered under each node. Without this, sysfs fails
+	 * to make the connection between nodes other than node0
+	 * and cpu0.
+	 */
+	for_each_online_node(i)
+		if (i != numa_node_id())
+			register_cpu_under_node(raw_smp_processor_id(), i);
+#endif
 
 	return 0;
 }
