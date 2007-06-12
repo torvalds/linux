6cbd55707802b98843f953d1ae6d8f5bcd9a76c0
Chris Mason <chris.mason@oracle.com>
2007-06-12 09:07:21 -0400

    Btrfs: add GPLv2
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/hash.h
+++ fs/btrfs/hash.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2007 Oracle.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License v2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 021110-1307, USA.
+ */
+
 #ifndef __HASH__
 #define __HASH__
 int btrfs_name_hash(const char *name, int len, u64 *hash_result);
