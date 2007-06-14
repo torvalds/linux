cc0a910b942d11069d35f52b2c0ed0e229e2fb46
Arnaldo Carvalho de Melo <acme@redhat.com>
2007-06-14 17:41:28 -0300

    [DCCP] loss_interval: Move ccid3_hc_rx_update_li to loss_interval
    Renaming it to dccp_li_update_li.
    
    Also based on previous work by Ian McDonald.
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@ghostprotocols.net>

--- net/dccp/ccids/lib/loss_interval.h
+++ net/dccp/ccids/lib/loss_interval.h
@@ -54,4 +54,11 @@ extern u32 dccp_li_hist_calc_i_mean(struct list_head *list);
 
 extern int dccp_li_hist_interval_new(struct dccp_li_hist *hist,
    struct list_head *list, const u64 seq_loss, const u8 win_loss);
+
+extern void dccp_li_update_li(struct sock *sk, struct dccp_li_hist *li_hist,
+			      struct list_head *li_hist_list,
+			      struct list_head *hist_list,
+			      struct timeval *last_feedback, u16 s,
+			      u32 bytes_recv, u32 previous_x_recv,
+			      u64 seq_loss, u8 win_loss);
 #endif /* _DCCP_LI_HIST_ */
