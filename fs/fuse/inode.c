edad01e2a1c527af3baf22dc6977aeb058881150
Alexey Dobriyan <adobriyan@gmail.com>
2007-06-16 10:16:05 -0700

    fuse: ->fs_flags fixlet
    fs/fuse/inode.c:658:3: error: Initializer entry defined twice
    fs/fuse/inode.c:661:3:   also defined here
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Acked-by: Miklos Szeredi <mszeredi@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -655,10 +655,9 @@ static int fuse_get_sb_blk(struct file_system_type *fs_type,
 static struct file_system_type fuseblk_fs_type = {
 	.owner		= THIS_MODULE,
 	.name		= "fuseblk",
-	.fs_flags	= FS_HAS_SUBTYPE,
 	.get_sb		= fuse_get_sb_blk,
 	.kill_sb	= kill_block_super,
-	.fs_flags	= FS_REQUIRES_DEV,
+	.fs_flags	= FS_REQUIRES_DEV | FS_HAS_SUBTYPE,
 };
 
 static inline int register_fuseblk(void)
