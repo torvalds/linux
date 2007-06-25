bebeaea0a54869b59b45ea22a93f325ce0369d61
Antoine Jacquet <royale@zerezo.com>
2007-06-25 16:00:34 -0300

    V4L/DVB (5792): Zr364xx: add support for Trust Powerc@m 970Z
    Add Trust Powerc@m 970Z (0x06d6:0x003b) to the list of supported devices.
    
    Signed-off-by: Antoine Jacquet <royale@zerezo.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/zr364xx.c
+++ drivers/media/video/zr364xx.c
@@ -92,6 +92,7 @@ static struct usb_device_id device_table[] = {
 	{USB_DEVICE(0x0784, 0x0040), .driver_info = METHOD1 },
 	{USB_DEVICE(0x06d6, 0x0034), .driver_info = METHOD0 },
 	{USB_DEVICE(0x0a17, 0x0062), .driver_info = METHOD2 },
+	{USB_DEVICE(0x06d6, 0x003b), .driver_info = METHOD0 },
 	{}			/* Terminating entry */
 };
 
