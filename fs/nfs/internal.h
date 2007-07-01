9eaa67c6a5b77f248c4703d81c4a6c6434e35385
Chuck Lever <chuck.lever@oracle.com>
2007-07-01 12:12:19 -0400

    NFS: Clean-up: use correct type when converting NFS blocks to local blocks
    inode->i_blocks is a blkcnt_t these days, which can be a u64 or unsigned
    long, depending on the setting of CONFIG_LSF.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/internal.h
+++ fs/nfs/internal.h
@@ -183,9 +183,9 @@ unsigned long nfs_block_bits(unsigned long bsize, unsigned char *nrbitsp)
 /*
  * Calculate the number of 512byte blocks used.
  */
-static inline unsigned long nfs_calc_block_size(u64 tsize)
+static inline blkcnt_t nfs_calc_block_size(u64 tsize)
 {
-	loff_t used = (tsize + 511) >> 9;
+	blkcnt_t used = (tsize + 511) >> 9;
 	return (used > ULONG_MAX) ? ULONG_MAX : used;
 }
 
