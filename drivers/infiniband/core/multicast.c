43506d954e43933cd6fdcab679f6ab057e7607c6
Roland Dreier <rolandd@cisco.com>
2007-07-09 16:17:32 -0700

    IB: Remove garbage non-ASCII characters from comments
    A few files had 0xa0 characters in comments.  Remove them so that the 
    files are clean ASCII text.
    
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/core/multicast.c
+++ drivers/infiniband/core/multicast.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006 Intel Corporation.  All rights reserved.
+ * Copyright (c) 2006 Intel Corporation.  All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
