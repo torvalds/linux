4cf92a3cd9117cb90ad9441172b9f42b67ee9d26
Jean Tourrilhes <jt@hpl.hp.com>
2007-07-09 20:37:36 -0500

    [PATCH] softmac: Fix ESSID problem
    Victor Porton reported that the SoftMAC layer had random problem when setting the ESSID :
    http://bugzilla.kernel.org/show_bug.cgi?id=8686 After investigation, it turned out to be
    worse, the SoftMAC layer is left in an inconsistent state. The fix is pretty trivial.
    
    Signed-off-by: Jean Tourrilhes <jt@hpl.hp.com>
    Acked-by: Michael Buesch <mb@bu3sch.df>
    Acked-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/ieee80211/softmac/ieee80211softmac_assoc.c
+++ net/ieee80211/softmac/ieee80211softmac_assoc.c
@@ -271,8 +271,11 @@ ieee80211softmac_assoc_work(struct work_struct *work)
 			 */
 			dprintk(KERN_INFO PFX "Associate: Scanning for networks first.\n");
 			ieee80211softmac_notify(mac->dev, IEEE80211SOFTMAC_EVENT_SCAN_FINISHED, ieee80211softmac_assoc_notify_scan, NULL);
-			if (ieee80211softmac_start_scan(mac))
+			if (ieee80211softmac_start_scan(mac)) {
 				dprintk(KERN_INFO PFX "Associate: failed to initiate scan. Is device up?\n");
+				mac->associnfo.associating = 0;
+				mac->associnfo.associated = 0;
+			}
 			goto out;
 		} else {
 			mac->associnfo.associating = 0;
