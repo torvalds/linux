a2e96b627b58efe2d618a415f34b9b9d9f829bd6
Yoichi Yuasa <yoichi_yuasa@tripeaks.co.jp>
2007-07-11 11:36:56 +0900

    [MTD] Remove Ocelot G support from DiskOnChip drivers.
    This patch has removed Ocelot G support from MTD.
    Ocelot G support has already removed since May 2007.
    
    Signed-off-by: Yoichi Yuasa <yoichi_yuasa@tripeaks.co.jp>
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- drivers/mtd/nand/diskonchip.c
+++ drivers/mtd/nand/diskonchip.c
@@ -59,8 +59,6 @@ static unsigned long __initdata doc_locations[] = {
 #elif defined(CONFIG_MOMENCO_OCELOT)
 	0x2f000000,
 	0xff000000,
-#elif defined(CONFIG_MOMENCO_OCELOT_G)
-	0xff000000,
 #else
 #warning Unknown architecture for DiskOnChip. No default probe locations defined
 #endif
