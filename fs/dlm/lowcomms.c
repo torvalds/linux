25720c2d73058f4f929f16093f60817ed52a285c
Patrick Caulfield <pcaulfie@redhat.com>
2007-07-11 13:39:43 +0100

    [DLM] Clear othercon pointers when a connection is closed
    This patch clears the othercon pointer and frees the memory when a connnection
    is closed. This could cause a small memory leak when nodes leave the cluster.
    
    Signed-Off-By: Patrick Caulfield <pcaulfie@redhat.com>
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/dlm/lowcomms.c
+++ fs/dlm/lowcomms.c
@@ -327,6 +327,8 @@ static void close_connection(struct connection *con, bool and_other)
 	if (con->othercon && and_other) {
 		/* Will only re-enter once. */
 		close_connection(con->othercon, false);
+		kmem_cache_free(con_cache, con->othercon);
+		con->othercon = NULL;
 	}
 	if (con->rx_page) {
 		__free_page(con->rx_page);
