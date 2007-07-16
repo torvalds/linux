a6739af8b9e8bf0fd1fb3f4f8406a9f650cb733a
Jan Kara <jack@suse.cz>
2007-07-15 23:40:22 -0700

    ext2: fix a comment when ext2_release_file() is called
    Signed-off-by: Jan Kara <jack@suse.cz>
    Acked-by: "Theodore Ts'o" <tytso@mit.edu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/ext2/file.c
+++ fs/ext2/file.c
@@ -24,9 +24,9 @@
 #include "acl.h"
 
 /*
- * Called when an inode is released. Note that this is different
- * from ext2_open_file: open gets called at every open, but release
- * gets called only when /all/ the files are closed.
+ * Called when filp is released. This happens when all file descriptors
+ * for a single struct file are closed. Note that different open() calls
+ * for the same file yield different struct file structures.
  */
 static int ext2_release_file (struct inode * inode, struct file * filp)
 {
