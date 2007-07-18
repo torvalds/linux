6160f63518406485c7009cb0f2e1588ea3abccc1
David S. Miller <davem@sunset.davemloft.net>
2007-07-17 23:03:47 -0700

    [SPARC64]: Massively simplify VIO device layer and support hot add/remove.
    Create and destroy VIO devices in response to MD update events.  These
    run synchronously inside of the MD update mutex so the VIO layer
    doesn't need to do internal locking of any sort.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc64/vio.h
+++ include/asm-sparc64/vio.h
@@ -264,7 +264,7 @@ static inline u32 vio_dring_avail(struct vio_dring_state *dr,
 		((dr->prod - dr->cons) & (ring_size - 1)));
 }
 
-#define VIO_MAX_TYPE_LEN	64
+#define VIO_MAX_TYPE_LEN	32
 #define VIO_MAX_COMPAT_LEN	64
 
 struct vio_dev {
