bdef40a6af64a0140a65df49bf504124d57094a9
Jeremy Fitzhardinge <jeremy@xensource.com>
2007-07-17 18:37:04 -0700

    paravirt: export __supported_pte_mask
    __supported_pte_mask is needed when constructing pte values.  Xen
    device drivers need to do this to make mappings of foreign pages (ie,
    pages granted to us by other domains).
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy@xensource.com>

--- arch/i386/mm/init.c
+++ arch/i386/mm/init.c
@@ -473,6 +473,7 @@ void zap_low_mappings (void)
 
 static int disable_nx __initdata = 0;
 u64 __supported_pte_mask __read_mostly = ~_PAGE_NX;
+EXPORT_SYMBOL_GPL(__supported_pte_mask);
 
 /*
  * noexec = on|off
