cb32da0416b823b7f4b65e7e85d6cba16ca4d1e1
Paul Mundt <lethal@linux-sh.org>
2007-07-18 09:18:36 +0900

    slob: Kill off duplicate kzalloc() definition.
    With the slab zeroing allocations cleanups Christoph stubbed in a generic
    kzalloc(), which was missed on SLOB. Follow the SLAB/SLUB changes and
    kill off the __kzalloc() wrapper that SLOB was using.
    
    Reported-by: Jan Engelhardt <jengelh@computergmbh.de>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/slob_def.h
+++ include/linux/slob_def.h
@@ -33,14 +33,4 @@ static inline void *__kmalloc(size_t size, gfp_t flags)
 	return kmalloc(size, flags);
 }
 
-/**
- * kzalloc - allocate memory. The memory is set to zero.
- * @size: how many bytes of memory are required.
- * @flags: the type of memory to allocate (see kcalloc).
- */
-static inline void *kzalloc(size_t size, gfp_t flags)
-{
-	return __kzalloc(size, flags);
-}
-
 #endif /* __LINUX_SLOB_DEF_H */
