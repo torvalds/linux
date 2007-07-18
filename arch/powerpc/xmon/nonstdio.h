776568d4c93fe8def5ab4060344af554fe2b44be
Ishizaki Kou <kou.ishizaki@toshiba.co.jp>
2007-07-18 19:30:29 +1000

    [POWERPC] Make xmon_write accept a const buffer
    Because xmon_write doesn't change the buffer, we should add 'const'
    qualifier to the argument which points it.
    
    Signed-off-by: Kou Ishizaki <kou.ishizaki@toshiba.co.jp>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/xmon/nonstdio.h
+++ arch/powerpc/xmon/nonstdio.h
@@ -9,6 +9,6 @@ extern char *xmon_gets(char *, int);
 extern void xmon_printf(const char *, ...);
 extern void xmon_map_scc(void);
 extern int xmon_expect(const char *str, unsigned long timeout);
-extern int xmon_write(void *ptr, int nb);
+extern int xmon_write(const void *ptr, int nb);
 extern int xmon_readchar(void);
 extern int xmon_read_poll(void);
