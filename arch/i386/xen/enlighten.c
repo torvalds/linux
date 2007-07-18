15c84731d647c34d1491793fa6be96f5de3432eb
Jeremy Fitzhardinge <jeremy@xensource.com>
2007-07-17 18:37:05 -0700

    xen: time implementation
    Xen maintains a base clock which measures nanoseconds since system
    boot.  This is provided to guests via a shared page which contains a
    base time in ns, a tsc timestamp at that point and tsc frequency
    parameters.  Guests can compute the current time by reading the tsc
    and using it to extrapolate the current time from the basetime.  The
    hypervisor makes sure that the frequency parameters are updated
    regularly, paricularly if the tsc changes rate or stops.
    
    This is implemented as a clocksource, so the interface to the rest of
    the kernel is a simple clocksource which simply returns the current
    time directly in nanoseconds.
    
    Xen also provides a simple timer mechanism, which allows a timeout to
    be set in the future.  When that time arrives, a timer event is sent
    to the guest.  There are two timer interfaces:
     - An old one which also delivers a stream of (unused) ticks at 100Hz,
       and on the same event, the actual timer events.  The 100Hz ticks
       cause a lot of spurious wakeups, but are basically harmless.
     - The new timer interface doesn't have the 100Hz ticks, and can also
       fail if the specified time is in the past.
    
    This code presents the Xen timer as a clockevent driver, and uses the
    new interface by preference.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy@xensource.com>
    Signed-off-by: Chris Wright <chrisw@sous-sol.org>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Thomas Gleixner <tglx@linutronix.de>

--- arch/i386/xen/enlighten.c
+++ arch/i386/xen/enlighten.c
@@ -609,6 +609,12 @@ static const struct paravirt_ops xen_paravirt_ops __initdata = {
 	.arch_setup = xen_arch_setup,
 	.init_IRQ = xen_init_IRQ,
 
+	.time_init = xen_time_init,
+	.set_wallclock = xen_set_wallclock,
+	.get_wallclock = xen_get_wallclock,
+	.get_cpu_khz = xen_cpu_khz,
+	.sched_clock = xen_clocksource_read,
+
 	.cpuid = xen_cpuid,
 
 	.set_debugreg = xen_set_debugreg,
