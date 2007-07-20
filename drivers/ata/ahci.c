badc2341579511a247f5993865aa68379e283c5c
su henry <henry.su.ati@gmail.com>
2007-07-20 08:07:46 -0400

    The SATA controller device ID is different according to the onchip SATA type set in the system BIOS: Device                              Device ID SATA in IDE mode             0x4390 SATA in AHCI mode           0x4391 SATA in non-raid5 driver     0x4392 SATA in raid5 driver           0x4393
    Although the device ID is different, they use the same AHCI driver
    .The attached file is the patch for adding these device
    IDs for ATI SB700.
    
    Signed-off-by: henry.su.ati@gmail.com
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/ata/ahci.c
+++ drivers/ata/ahci.c
@@ -418,7 +418,10 @@ static const struct pci_device_id ahci_pci_tbl[] = {
 
 	/* ATI */
 	{ PCI_VDEVICE(ATI, 0x4380), board_ahci_sb600 }, /* ATI SB600 */
-	{ PCI_VDEVICE(ATI, 0x4390), board_ahci_sb600 }, /* ATI SB700 */
+	{ PCI_VDEVICE(ATI, 0x4390), board_ahci_sb600 }, /* ATI SB700 IDE */
+	{ PCI_VDEVICE(ATI, 0x4391), board_ahci_sb600 }, /* ATI SB700 AHCI */
+	{ PCI_VDEVICE(ATI, 0x4392), board_ahci_sb600 }, /* ATI SB700 nraid5 */
+	{ PCI_VDEVICE(ATI, 0x4393), board_ahci_sb600 }, /* ATI SB700 raid5 */
 
 	/* VIA */
 	{ PCI_VDEVICE(VIA, 0x3349), board_ahci_vt8251 }, /* VIA VT8251 */
