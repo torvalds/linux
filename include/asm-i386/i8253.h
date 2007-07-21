bef9f9de32ec201ea5372caf2c76e7c71f80ee35
Chris Wright <chrisw@sous-sol.org>
2007-07-21 17:10:08 +0200

    i386: remove pit_interrupt_hook
    Remove pit_interrupt_hook as it adds just an extra layer.
    
    Signed-off-by: Chris Wright <chrisw@sous-sol.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-i386/i8253.h
+++ include/asm-i386/i8253.h
@@ -7,15 +7,4 @@ extern spinlock_t i8253_lock;
 
 extern struct clock_event_device *global_clock_event;
 
-/**
- * pit_interrupt_hook - hook into timer tick
- * @regs:	standard registers from interrupt
- *
- * Call the global clock event handler.
- **/
-static inline void pit_interrupt_hook(void)
-{
-	global_clock_event->event_handler(global_clock_event);
-}
-
 #endif	/* __ASM_I8253_H__ */
