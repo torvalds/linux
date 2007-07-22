0bd8acd1a7fbb5492c1de8af271b6964a7fd5274
Andi Kleen <ak@suse.de>
2007-07-22 11:12:34 +0200

    x86_64: Set K8 CPUID flag for K8/Fam10h/Fam11h
    Previously this flag was only used on 32bit, but some shared code can use
    it now.
    
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/x86_64/kernel/setup.c
+++ arch/x86_64/kernel/setup.c
@@ -608,6 +608,9 @@ static void __cpuinit init_amd(struct cpuinfo_x86 *c)
 	else
 		num_cache_leaves = 3;
 
+	if (c->x86 == 0xf || c->x86 == 0x10 || c->x86 == 0x11)
+		set_bit(X86_FEATURE_K8, &c->x86_capability);
+
 	/* RDTSC can be speculated around */
 	clear_bit(X86_FEATURE_SYNC_RDTSC, &c->x86_capability);
 
