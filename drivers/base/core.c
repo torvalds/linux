27907689eedd799a6a0c1a2f18a3563c6e9e9767
Cornelia Huck <cornelia.huck@de.ibm.com>
2007-07-25 09:58:08 +0200

    driver core: revert "device" link creation check
    driver core: revert "device" link creation check
    
    Commit 2ee97caf0a6602f749ddbfdb1449e383e1212707 introduced an extra
    check on when to create the "device" symlink. Unfortunately, this
    breaks input, so let's revert to the old behaviour.
    
    Signed-off-by: Cornelia Huck <cornelia.huck@de.ibm.com>
    Acked-by: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -678,8 +678,7 @@ static int device_add_class_symlinks(struct device *dev)
 		if (error)
 			goto out_subsys;
 	}
-	/* only bus-device parents get a "device"-link */
-	if (dev->parent && dev->parent->bus) {
+	if (dev->parent) {
 		error = sysfs_create_link(&dev->kobj, &dev->parent->kobj,
 					  "device");
 		if (error)
