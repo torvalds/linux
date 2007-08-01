313b0305b5a1e7e0fb39383befbf79558ce68a9c
Francois Romieu <romieu@fr.zoreil.com>
2007-08-02 00:00:48 +0200

    r8169: avoid needless NAPI poll scheduling
    Theory  : though needless, it should not have hurt.
    Practice: it does not play nice with DEBUG_SHIRQ + LOCKDEP + UP
    (see https://bugzilla.redhat.com/bugzilla/show_bug.cgi?id=242572).
    
    The patch makes sense in itself but I should dig why it has an effect
    on #242572 (assuming that NAPI do not change in a near future).
    
    Signed-off-by: Francois Romieu <romieu@fr.zoreil.com>
    Cc: Edward Hsu <edward_hsu@realtek.com.tw>

--- drivers/net/r8169.c
+++ drivers/net/r8169.c
@@ -2767,14 +2767,16 @@ static irqreturn_t rtl8169_interrupt(int irq, void *dev_instance)
 			rtl8169_check_link_status(dev, tp, ioaddr);
 
 #ifdef CONFIG_R8169_NAPI
-		RTL_W16(IntrMask, tp->intr_event & ~tp->napi_event);
-		tp->intr_mask = ~tp->napi_event;
+		if (status & tp->napi_event) {
+			RTL_W16(IntrMask, tp->intr_event & ~tp->napi_event);
+			tp->intr_mask = ~tp->napi_event;
 
-		if (likely(netif_rx_schedule_prep(dev)))
-			__netif_rx_schedule(dev);
-		else if (netif_msg_intr(tp)) {
-			printk(KERN_INFO "%s: interrupt %04x taken in poll\n",
-			       dev->name, status);
+			if (likely(netif_rx_schedule_prep(dev)))
+				__netif_rx_schedule(dev);
+			else if (netif_msg_intr(tp)) {
+				printk(KERN_INFO "%s: interrupt %04x in poll\n",
+				       dev->name, status);
+			}
 		}
 		break;
 #else
