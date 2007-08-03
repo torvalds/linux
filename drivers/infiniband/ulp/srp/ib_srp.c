3d1ff48da760968793f3c36672961ffd23088787
Raghava Kondapalli <rakondap@cisco.com>
2007-08-03 10:45:18 -0700

    IB/srp: Add OUI for new Cisco targets
    New Cisco IB SRP targets use the Cisco OUI 00-1b-0d but still need the
    Topspin workarounds.  Add this OUI to srp_target_is_topspin().
    
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/ulp/srp/ib_srp.c
+++ drivers/infiniband/ulp/srp/ib_srp.c
@@ -107,9 +107,11 @@ static const char *srp_target_info(struct Scsi_Host *host)
 static int srp_target_is_topspin(struct srp_target_port *target)
 {
 	static const u8 topspin_oui[3] = { 0x00, 0x05, 0xad };
+	static const u8 cisco_oui[3]   = { 0x00, 0x1b, 0x0d };
 
 	return topspin_workarounds &&
-		!memcmp(&target->ioc_guid, topspin_oui, sizeof topspin_oui);
+		(!memcmp(&target->ioc_guid, topspin_oui, sizeof topspin_oui) ||
+		 !memcmp(&target->ioc_guid, cisco_oui, sizeof cisco_oui));
 }
 
 static int srp_target_is_mellanox(struct srp_target_port *target)
