73089cbfdf0c69e061a4fa90d614679e630c6727
Chris Zankel <chris@zankel.net>
2007-08-04 09:27:30 -0700

    [XTENSA] Move pre-initialized structures to init_task.c
    Move all initialization structures for the initial task to it's own file.
    
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- arch/xtensa/kernel/process.c
+++ arch/xtensa/kernel/process.c
@@ -30,6 +30,7 @@
 #include <linux/init_task.h>
 #include <linux/module.h>
 #include <linux/mqueue.h>
+#include <linux/fs.h>
 
 #include <asm/pgtable.h>
 #include <asm/uaccess.h>
@@ -44,20 +45,6 @@
 #include <asm/regs.h>
 
 extern void ret_from_fork(void);
-
-static struct fs_struct init_fs = INIT_FS;
-static struct files_struct init_files = INIT_FILES;
-static struct signal_struct init_signals = INIT_SIGNALS(init_signals);
-static struct sighand_struct init_sighand = INIT_SIGHAND(init_sighand);
-struct mm_struct init_mm = INIT_MM(init_mm);
-EXPORT_SYMBOL(init_mm);
-
-union thread_union init_thread_union
-	__attribute__((__section__(".data.init_task"))) =
-{ INIT_THREAD_INFO(init_task) };
-
-struct task_struct init_task = INIT_TASK(init_task);
-EXPORT_SYMBOL(init_task);
 
 struct task_struct *current_set[NR_CPUS] = {&init_task, };
 
