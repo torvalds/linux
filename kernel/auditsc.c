175fc484256e9c85e043f599ec2f6bc0d2e6c443
Al Viro <viro@ftp.linux.org.uk>
2007-08-08 00:01:46 +0100

    fix oops in __audit_signal_info()
    	The check for audit_signals is misplaced and the check for
    audit_dummy_context() is missing; as the result, if we send a signal to
    auditd from task with NULL ->audit_context while we have audit_signals
    != 0 we end up with an oops.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Acked-by: James Morris <jmorris@namei.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/auditsc.c
+++ kernel/auditsc.c
@@ -1992,18 +1992,18 @@ int __audit_signal_info(int sig, struct task_struct *t)
 	extern uid_t audit_sig_uid;
 	extern u32 audit_sig_sid;
 
-	if (audit_pid && t->tgid == audit_pid &&
-	    (sig == SIGTERM || sig == SIGHUP || sig == SIGUSR1)) {
-		audit_sig_pid = tsk->pid;
-		if (ctx)
-			audit_sig_uid = ctx->loginuid;
-		else
-			audit_sig_uid = tsk->uid;
-		selinux_get_task_sid(tsk, &audit_sig_sid);
+	if (audit_pid && t->tgid == audit_pid) {
+		if (sig == SIGTERM || sig == SIGHUP || sig == SIGUSR1) {
+			audit_sig_pid = tsk->pid;
+			if (ctx)
+				audit_sig_uid = ctx->loginuid;
+			else
+				audit_sig_uid = tsk->uid;
+			selinux_get_task_sid(tsk, &audit_sig_sid);
+		}
+		if (!audit_signals || audit_dummy_context())
+			return 0;
 	}
-
-	if (!audit_signals) /* audit_context checked in wrapper */
-		return 0;
 
 	/* optimize the common case by putting first signal recipient directly
 	 * in audit_context */
