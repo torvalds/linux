246d95ba051101e515670a1cbe2907a88d360b88
Andrew Morton <akpm@linux-foundation.org>
2007-08-09 00:53:50 -0700

    nfsd warning fix
    fs/nfsd/nfsctl.c: In function 'write_filehandle':
    fs/nfsd/nfsctl.c:301: warning: 'maxsize' may be used uninitialized in this function
    
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: "J. Bruce Fields" <bfields@citi.umich.edu>
    Acked-by:  Neil Brown <neilb@suse.de>

--- fs/nfsd/nfsctl.c
+++ fs/nfsd/nfsctl.c
@@ -298,7 +298,7 @@ static ssize_t write_filehandle(struct file *file, char *buf, size_t size)
 	 * qword quoting is used, so filehandle will be \x....
 	 */
 	char *dname, *path;
-	int maxsize;
+	int uninitialized_var(maxsize);
 	char *mesg = buf;
 	int len;
 	struct auth_domain *dom;
