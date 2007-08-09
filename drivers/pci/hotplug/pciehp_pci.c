9fe8164536c4fa6e630c706c667a2c6e8456d143
Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
2007-08-09 16:09:37 -0700

    pciehp: remove trailing whitespace form pciehp_pci.c
    Remove trailing whitespaces from pciehp_pci.c.
    
    Signed-off-by: Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
    Signed-off-by: Kristen Carlson Accardi <kristen.c.accardi@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/pci/hotplug/pciehp_pci.c
+++ drivers/pci/hotplug/pciehp_pci.c
@@ -280,12 +280,12 @@ int pciehp_unconfigure_device(struct slot *p_slot)
 		pci_remove_bus_device(temp);
 		pci_dev_put(temp);
 	}
-	/* 
+	/*
 	 * Some PCI Express root ports require fixup after hot-plug operation.
 	 */
-	if (pcie_mch_quirk) 
+	if (pcie_mch_quirk)
 		pci_fixup_device(pci_fixup_final, p_slot->ctrl->pci_dev);
-	
+
 	return rc;
 }
 
