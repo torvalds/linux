0cb65f2262776e07c2b8ec1121738b97f8941dae
Takashi Iwai <tiwai@suse.de>
2007-08-16 12:32:45 +0200

    [ALSA] hda-intel - Add position_fix quirk for Dell Precision 390
    Dell Precision 390 needs position_fix=1 as default (ALSA bug#3295).
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -1642,6 +1642,7 @@ static int azx_dev_free(struct snd_device *device)
  */
 static struct snd_pci_quirk position_fix_list[] __devinitdata = {
 	SND_PCI_QUIRK(0x1028, 0x01cc, "Dell D820", POS_FIX_NONE),
+	SND_PCI_QUIRK(0x1028, 0x01de, "Dell Precision 390", POS_FIX_NONE),
 	{}
 };
 
