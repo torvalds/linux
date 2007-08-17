65d916d95314566f426cc40ff0f17b754a773b0b
Edward Hsu <edward_hsu@realtek.com.tw>
2007-08-17 10:14:36 +0200

    r8169: correct phy parameters for the 8110SC
    The phys of the 8110SC (RTL_GIGA_MAC_VER_{05/06}) act abnormally in
    gigabit mode if they are applied the parameters in rtl8169_hw_phy_config
    which actually aim the 8110S/SB.
    
    It is ok to return early from rtl8169_hw_phy_config as it does not
    apply to the 8101 and 8168 families.
    
    Signed-off-by: Edward Hsu <edward_hsu@realtek.com.tw>
    Signed-off-by: Francois Romieu <romieu@fr.zoreil.com>

--- drivers/net/r8169.c
+++ drivers/net/r8169.c
@@ -1228,7 +1228,10 @@ static void rtl8169_hw_phy_config(struct net_device *dev)
 		return;
 	}
 
-	/* phy config for RTL8169s mac_version C chip */
+	if ((tp->mac_version != RTL_GIGA_MAC_VER_02) &&
+	    (tp->mac_version != RTL_GIGA_MAC_VER_03))
+		return;
+
 	mdio_write(ioaddr, 31, 0x0001);			//w 31 2 0 1
 	mdio_write(ioaddr, 21, 0x1000);			//w 21 15 0 1000
 	mdio_write(ioaddr, 24, 0x65c7);			//w 24 15 0 65c7
