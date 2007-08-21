8b713c67bccef7cabb0ee915f47eb33d82636af9
Peter Chubb <peterc@chubb.wattle.id.au>
2007-08-21 13:51:45 +1000

    [IA64] Enable early console for Ski simulator
    This patch cleans up the `enable early console for SKI' patch
    (471e7a44848f467c9b83adc3463d019d2fa8817f), and
    1. potentially allows the gensparse_defconfig to work again.
       (there are other problems running a generic kernel on Ski)
    2. fixes the `console registered twice' problem.
    3. Cleans up the code by moving the `extern hpsim_cons' declaration to
       a new asm/hpsim.h file.
    
    Thanks to Jes for comments.
    
    Signed-off-by: Peter Chubb <peterc@gelato.unsw.edu.au>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

---
+++ include/asm-ia64/hpsim.h
@@ -0,0 +1,10 @@
+#ifndef _ASMIA64_HPSIM_H
+#define _ASMIA64_HPSIM_H
+
+#ifndef CONFIG_HP_SIMSERIAL_CONSOLE
+static inline int simcons_register(void) { return 1; }
+#else
+int simcons_register(void);
+#endif
+
+#endif
