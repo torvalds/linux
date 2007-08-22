918f02383fb9ff5dba29709f3199189eeac55021
NeilBrown <neilb@suse.de>
2007-08-22 14:01:52 -0700

    md: make sure a re-add after a restart honours bitmap when resyncing
    Commit 1757128438d41670ded8bc3bc735325cc07dc8f9 was slightly bad.  If an array
    has a write-intent bitmap, and you remove a drive, then readd it, only the
    changed parts should be resynced.  However after the above commit, this only
    works if the array has not been shut down and restarted.
    
    This is because it sets 'fullsync' at little more often than it should.  This
    patch is more careful.
    
    Signed-off-by: Neil Brown <neilb@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/md/raid1.c
+++ drivers/md/raid1.c
@@ -1972,7 +1972,8 @@ static int run(mddev_t *mddev)
 		    !test_bit(In_sync, &disk->rdev->flags)) {
 			disk->head_position = 0;
 			mddev->degraded++;
-			conf->fullsync = 1;
+			if (disk->rdev)
+				conf->fullsync = 1;
 		}
 	}
 	if (mddev->degraded == conf->raid_disks) {
