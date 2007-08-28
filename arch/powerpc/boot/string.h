0ae0b54565a8dcc2b98de694b998e765de15b713
David Gibson <david@gibson.dropbear.id.au>
2007-08-28 14:52:57 +1000

    [POWERPC] Move bootwrapper's strchr() and strncmp() from .h to string.S
    Currently the bootwrapper has implementations of strchr() and
    strncmp(), but they're inlines in flatdevtree_env.h, rather than in
    string.S with all the rest of the string functions.  This moves
    them to string.S.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/boot/string.h
+++ arch/powerpc/boot/string.h
@@ -5,7 +5,9 @@
 extern char *strcpy(char *dest, const char *src);
 extern char *strncpy(char *dest, const char *src, size_t n);
 extern char *strcat(char *dest, const char *src);
+extern char *strchr(const char *s, int c);
 extern int strcmp(const char *s1, const char *s2);
+extern int strncmp(const char *s1, const char *s2, size_t n);
 extern size_t strlen(const char *s);
 extern size_t strnlen(const char *s, size_t count);
 
