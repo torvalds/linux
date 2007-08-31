b4154d4a01c6cc98b97dc239ca4302316c8f98da
Ondrej Zary <linux@rainbow-software.org>
2007-09-01 00:55:21 +0200

    Fix sata_via write errors on PATA drive connected to VT6421
    I think that I've found and fixed the problem. There is a copy/paste bug in
    vt6421_set_dma_mode() function which causes wrong values to be written to
    PATA_UDMA_TIMING register.
    
    This patch fixes a copy/paste bug that breaks DMA modes on VT6421 PATA port.
    
    Signed-off-by: Ondrej Zary <linux@rainbow-software.org>
    Acked-by: Alan Cox <alan@redhat.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/ata/sata_via.c
+++ drivers/ata/sata_via.c
@@ -371,7 +371,7 @@ static void vt6421_set_dma_mode(struct ata_port *ap, struct ata_device *adev)
 {
 	struct pci_dev *pdev = to_pci_dev(ap->host->dev);
 	static const u8 udma_bits[] = { 0xEE, 0xE8, 0xE6, 0xE4, 0xE2, 0xE1, 0xE0, 0xE0 };
-	pci_write_config_byte(pdev, PATA_UDMA_TIMING, udma_bits[adev->pio_mode - XFER_UDMA_0]);
+	pci_write_config_byte(pdev, PATA_UDMA_TIMING, udma_bits[adev->dma_mode - XFER_UDMA_0]);
 }
 
 static const unsigned int svia_bar_sizes[] = {
