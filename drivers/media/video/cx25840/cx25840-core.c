f61b48f7a838bb3d089b12fc9d03fd53c9ef5022
Michael Krufky <mkrufky@linuxtv.org>
2007-09-01 02:02:51 -0300

    V4L/DVB (6142): cx25840: fix build warning
    Fix the following build warning:
    
     CC [M]  cx25840-core.o
    
    cx25840-core.c: In function 'init_dll1':
    cx25840-core.c:147: warning: implicit declaration of function 'udelay'
    
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/cx25840/cx25840-core.c
+++ drivers/media/video/cx25840/cx25840-core.c
@@ -34,6 +34,7 @@
 #include <linux/slab.h>
 #include <linux/videodev2.h>
 #include <linux/i2c.h>
+#include <linux/delay.h>
 #include <media/v4l2-common.h>
 #include <media/v4l2-chip-ident.h>
 #include <media/cx25840.h>
