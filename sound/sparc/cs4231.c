7b85b4f088de8856c5cc5fee18630708ab28dfe8
Krzysztof Helt <krzysztof.h1@wp.pl>
2007-09-05 15:07:57 +0200

    [ALSA] sun-cs4231: memory management fix
    The chip structure is now allocated by snd_card_new()
    and it must not be released by separate kfree().
    
    Signed-off-by: Krzysztof Helt <krzysztof.h1@wp.pl>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/sparc/cs4231.c
+++ sound/sparc/cs4231.c
@@ -1865,8 +1865,6 @@ static int snd_cs4231_sbus_free(struct snd_cs4231 *chip)
 	if (chip->port)
 		sbus_iounmap(chip->port, chip->regs_size);
 
-	kfree(chip);
-
 	return 0;
 }
 
@@ -2036,8 +2034,6 @@ static int snd_cs4231_ebus_free(struct snd_cs4231 *chip)
 
 	if (chip->port)
 		iounmap(chip->port);
-
-	kfree(chip);
 
 	return 0;
 }
