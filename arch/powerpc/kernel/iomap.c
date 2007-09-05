472b5b43bec45e7e79f86094ca9091bd9eb474ed
Valentine Barshak <vbarshak@ru.mvista.com>
2007-09-06 03:30:16 +1000

    [POWERPC] Add 64-bit resources support to pci_iomap
    The patch adds support for the 64-bit resources to the PCI
    iomap code.
    
    Signed-off-by: Valentine Barshak <vbarshak@ru.mvista.com>
    Acked-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Josh Boyer <jwboyer@linux.vnet.ibm.com>

--- arch/powerpc/kernel/iomap.c
+++ arch/powerpc/kernel/iomap.c
@@ -119,8 +119,8 @@ EXPORT_SYMBOL(ioport_unmap);
 
 void __iomem *pci_iomap(struct pci_dev *dev, int bar, unsigned long max)
 {
-	unsigned long start = pci_resource_start(dev, bar);
-	unsigned long len = pci_resource_len(dev, bar);
+	resource_size_t start = pci_resource_start(dev, bar);
+	resource_size_t len = pci_resource_len(dev, bar);
 	unsigned long flags = pci_resource_flags(dev, bar);
 
 	if (!len)
