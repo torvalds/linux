e606c109c485c6f8e27dd6ddcd2c0b58c96fc5c9
Ralf Baechle <ralf@linux-mips.org>
2007-09-12 16:31:12 +0100

    [MIPS] rtlx: fix int vs. long bug.
      CC      arch/mips/kernel/rtlx.o
    arch/mips/kernel/rtlx.c: In function 'rtlx_init':
    arch/mips/kernel/rtlx.c:114: warning: format '%x' expects type 'unsigned int', but argument 3 has type 'long unsigned int'
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/kernel/rtlx.c
+++ arch/mips/kernel/rtlx.c
@@ -111,7 +111,7 @@ static void __used dump_rtlx(void)
 static int rtlx_init(struct rtlx_info *rtlxi)
 {
 	if (rtlxi->id != RTLX_ID) {
-		printk(KERN_ERR "no valid RTLX id at 0x%p 0x%x\n", rtlxi, rtlxi->id);
+		printk(KERN_ERR "no valid RTLX id at 0x%p 0x%lx\n", rtlxi, rtlxi->id);
 		return -ENOEXEC;
 	}
 
