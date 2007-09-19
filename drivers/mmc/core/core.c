f9996aee36921e8f1d499de1b2ea380855cf6d97
Pierre Ossman <drzeus@drzeus.cx>
2007-09-19 18:38:50 +0200

    mmc: increase power up delay
    Increase delay for power up in order to support some slower boards.
    
    Also add some comments about why the delays are there.
    
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -460,12 +460,20 @@ static void mmc_power_up(struct mmc_host *host)
 	host->ios.timing = MMC_TIMING_LEGACY;
 	mmc_set_ios(host);
 
-	mmc_delay(1);
+	/*
+	 * This delay should be sufficient to allow the power supply
+	 * to reach the minimum voltage.
+	 */
+	mmc_delay(2);
 
 	host->ios.clock = host->f_min;
 	host->ios.power_mode = MMC_POWER_ON;
 	mmc_set_ios(host);
 
+	/*
+	 * This delay must be at least 74 clock sizes, or 1 ms, or the
+	 * time required to reach a stable voltage.
+	 */
 	mmc_delay(2);
 }
 
