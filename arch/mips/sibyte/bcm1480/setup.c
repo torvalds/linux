da51f9e10c48599bff498b378d040c4063adfe9d
Ralf Baechle <ralf@linux-mips.org>
2007-09-21 09:01:15 +0100

    [MIPS] BCM1480: include <linux/init.h>.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/sibyte/bcm1480/setup.c
+++ arch/mips/sibyte/bcm1480/setup.c
@@ -15,6 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+#include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/reboot.h>
