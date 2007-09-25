68db2bc98cc1594a1cc487755aff4340fd4f1611
Tony Jones <tonyj@suse.de>
2007-09-25 02:03:03 +0200

    adb: Convert from class_device to device
    struct class_device is going away, this converts the code to use struct
    device instead.
    
    Signed-off-by: Tony Jones <tonyj@suse.de>
    Cc: Joshua Thompson <funaho@jurai.org>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/macintosh/adb.c
+++ drivers/macintosh/adb.c
@@ -875,5 +875,5 @@ adbdev_init(void)
 	adb_dev_class = class_create(THIS_MODULE, "adb");
 	if (IS_ERR(adb_dev_class))
 		return;
-	class_device_create(adb_dev_class, NULL, MKDEV(ADB_MAJOR, 0), NULL, "adb");
+	device_create(adb_dev_class, NULL, MKDEV(ADB_MAJOR, 0), "adb");
 }
