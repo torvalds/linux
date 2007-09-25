e99d00cb176dde1d0c140b06492b6a729367e745
Manu Abraham <abraham.manu@gmail.com>
2007-09-25 17:48:59 -0300

    V4L/DVB (9405): Practical tests show that the TT S2 3200 has I/Q inputs inverted, similar to the KNC1.
    Don't have this hardware and cannot verify this for myself, but from the reports this
    looks to be quite a reasonable and correct hypothesis.
    
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/ttpci/budget-ci.c
+++ drivers/media/dvb/ttpci/budget-ci.c
@@ -1658,7 +1658,7 @@ static struct stb0899_config tt3200_config = {
 	.demod_address 		= 0x68,
 
 	.xtal_freq		= 27000000,
-	.inversion		= IQ_SWAP_OFF, /* 1 */
+	.inversion		= IQ_SWAP_ON, /* 1 */
 
 	.esno_ave		= TT3200_DVBS2_ESNO_AVE,
 	.esno_quant		= TT3200_DVBS2_ESNO_QUANT,
