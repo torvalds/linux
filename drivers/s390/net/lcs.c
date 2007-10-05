59579da329b22bcddc5da0c22c61d6dc1f9eb96a
Klaus D. Wacker <kdwacker@de.ibm.com>
2007-10-05 16:45:47 +0200

    lcs: Channel errors drive lcs_recovery which leads to kernel panic.
    When the lcs irq routine detects channel failures it drives device recovery.
    After this event the device is no longer usable for shutdown requests,
    because the lcs_irq routine may get wrong channel status information.
    In such a case the lcs_irq routine marks the channel in 'error' state.
    The channel state comes back to 'running' after restarting the channels.
    
    Signed-off-by: Klaus D. Wacker <kdwacker@de.ibm.com>
    Signed-off-by: Ursula Braun <braunu@de.ibm.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/s390/net/lcs.c
+++ drivers/s390/net/lcs.c
@@ -1400,10 +1400,13 @@ lcs_irq(struct ccw_device *cdev, unsigned long intparm, struct irb *irb)
 		PRINT_WARN("check on device %s, dstat=0x%X, cstat=0x%X \n",
 			    cdev->dev.bus_id, dstat, cstat);
 		if (rc) {
-			lcs_schedule_recovery(card);
-			wake_up(&card->wait_q);
-			return;
+			channel->state = LCS_CH_STATE_ERROR;
 		}
+	}
+	if (channel->state == LCS_CH_STATE_ERROR) {
+		lcs_schedule_recovery(card);
+		wake_up(&card->wait_q);
+		return;
 	}
 	/* How far in the ccw chain have we processed? */
 	if ((channel->state != LCS_CH_STATE_INIT) &&
@@ -1708,6 +1711,8 @@ lcs_stopcard(struct lcs_card *card)
 
 	if (card->read.state != LCS_CH_STATE_STOPPED &&
 	    card->write.state != LCS_CH_STATE_STOPPED &&
+	    card->read.state != LCS_CH_STATE_ERROR &&
+	    card->write.state != LCS_CH_STATE_ERROR &&
 	    card->state == DEV_STATE_UP) {
 		lcs_clear_multicast_list(card);
 		rc = lcs_send_stoplan(card,LCS_INITIATOR_TCPIP);
