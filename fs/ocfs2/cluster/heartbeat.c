bc7e97cbdd4bef162e5772c74ee2cc4487a2d997
Jan Kara <jack@suse.cz>
2007-10-10 16:25:42 +0200

    [PATCH] Fix possibly too long write in o2hb_setup_one_bio()
    We should subtract start of our IO from PAGE_CACHE_SIZE to get the right
    length of the write we want to perform.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Mark Fasheh <mark.fasheh@oracle.com>

--- fs/ocfs2/cluster/heartbeat.c
+++ fs/ocfs2/cluster/heartbeat.c
@@ -267,7 +267,7 @@ static struct bio *o2hb_setup_one_bio(struct o2hb_region *reg,
 		current_page = cs / spp;
 		page = reg->hr_slot_data[current_page];
 
-		vec_len = min(PAGE_CACHE_SIZE,
+		vec_len = min(PAGE_CACHE_SIZE - vec_start,
 			      (max_slots-cs) * (PAGE_CACHE_SIZE/spp) );
 
 		mlog(ML_HB_BIO, "page %d, vec_len = %u, vec_start = %u\n",
