7ca16d269a1a4b96d98968b48f137977bcab1522
Ralf Baechle <ralf@linux-mips.org>
2007-10-11 23:46:03 +0100

    [MIPS] floppy: Rewrite fd_cacheflush() to use dma_cache_sync().
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/floppy.h
+++ include/asm-mips/floppy.h
@@ -10,9 +10,11 @@
 #ifndef _ASM_FLOPPY_H
 #define _ASM_FLOPPY_H
 
+#include <linux/dma-mapping.h>
+
 static inline void fd_cacheflush(char * addr, long size)
 {
-	dma_cache_wback_inv((unsigned long)addr,size);
+	dma_cache_sync(NULL, addr, size, DMA_BIDIRECTIONAL);
 }
 
 #define MAX_BUFFER_SECTORS 24
