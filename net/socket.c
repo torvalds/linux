fa8705b00aeca19d91a1437b8a5cf865999b28f6
Tony Battersby <tonyb@cybernetics.com>
2007-10-10 21:09:04 -0700

    [NET]: sanitize kernel_accept() error path
    If kernel_accept() returns an error, it may pass back a pointer to
    freed memory (which the caller should ignore).  Make it pass back NULL
    instead for better safety.
    
    Signed-off-by: Tony Battersby <tonyb@cybernetics.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/socket.c
+++ net/socket.c
@@ -2235,6 +2235,7 @@ int kernel_accept(struct socket *sock, struct socket **newsock, int flags)
 	err = sock->ops->accept(sock, *newsock, flags);
 	if (err < 0) {
 		sock_release(*newsock);
+		*newsock = NULL;
 		goto done;
 	}
 
