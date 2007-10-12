9b4b8feb962f4b3e74768b7205f1f8f6cce87238
David Gibson <david@gibson.dropbear.id.au>
2007-10-12 12:36:20 +1000

    [POWERPC] Add memchr() to the bootwrapper
    This adds a memchr() implementation to the bootwrapper, which will
    be needed when libfdt is merged in.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/boot/string.h
+++ arch/powerpc/boot/string.h
@@ -14,6 +14,7 @@ extern size_t strnlen(const char *s, size_t count);
 extern void *memset(void *s, int c, size_t n);
 extern void *memmove(void *dest, const void *src, unsigned long n);
 extern void *memcpy(void *dest, const void *src, unsigned long n);
+extern void *memchr(const void *s, int c, size_t n);
 extern int memcmp(const void *s1, const void *s2, size_t n);
 
 #endif	/* _PPC_BOOT_STRING_H_ */
