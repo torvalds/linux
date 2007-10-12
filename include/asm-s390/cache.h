5a291321d6b7ab40923b9a42b36746f7b5ca0055
Heiko Carstens <heiko.carstens@de.ibm.com>
2007-10-12 16:11:37 +0200

    [S390] Get rid of ARCH_KMALLOC_MINALIGN.
    slab cache debugging code has been changed so that we always get a
    minimum alignment of the alignment of a 64-integer. Since this is
    8 on s390/s390x there is no need of ARCH_KMALLOC_MINALIGN anymore.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- include/asm-s390/cache.h
+++ include/asm-s390/cache.h
@@ -14,8 +14,6 @@
 #define L1_CACHE_BYTES     256
 #define L1_CACHE_SHIFT     8
 
-#define ARCH_KMALLOC_MINALIGN	8
-
 #define __read_mostly __attribute__((__section__(".data.read_mostly")))
 
 #endif
