bef1bc95f32bd7603c3682dbb8695cf4d5841732
Al Viro <viro@ftp.linux.org.uk>
2007-10-14 19:20:30 +0100

    skb->tail in ibm_newemac should be skb_tail_pointer()
    ... since that sucker is not 32bit-only and on 64bit skb->tail is an
    offset, not a pointer.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/net/ibm_newemac/core.c
+++ drivers/net/ibm_newemac/core.c
@@ -1534,7 +1534,7 @@ static inline int emac_rx_sg_append(struct emac_instance *dev, int slot)
 			dev_kfree_skb(dev->rx_sg_skb);
 			dev->rx_sg_skb = NULL;
 		} else {
-			cacheable_memcpy(dev->rx_sg_skb->tail,
+			cacheable_memcpy(skb_tail_pointer(dev->rx_sg_skb),
 					 dev->rx_skb[slot]->data, len);
 			skb_put(dev->rx_sg_skb, len);
 			emac_recycle_rx_skb(dev, slot, len);
