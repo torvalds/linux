106619c440a7cd5a100eaba38665231ab8c762b8
Thomas Gleixner <tglx@linutronix.de>
2007-10-15 23:28:20 +0200

    x86: unify include/asm/cache_32/64.h
    Same file, except for whitespace, comment formatting and the two variants
    of fb_is_primary_device()
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/fb.h
+++ include/asm-x86/fb.h
@@ -1,5 +1,21 @@
+#ifndef _ASM_X86_FB_H
+#define _ASM_X86_FB_H
+
+#include <linux/fb.h>
+#include <linux/fs.h>
+#include <asm/page.h>
+
+static inline void fb_pgprotect(struct file *file, struct vm_area_struct *vma,
+				unsigned long off)
+{
+	if (boot_cpu_data.x86 > 3)
+		pgprot_val(vma->vm_page_prot) |= _PAGE_PCD;
+}
+
 #ifdef CONFIG_X86_32
-# include "fb_32.h"
+extern int fb_is_primary_device(struct fb_info *info);
 #else
-# include "fb_64.h"
+static inline int fb_is_primary_device(struct fb_info *info) { return 0; }
 #endif
+
+#endif /* _ASM_X86_FB_H */
