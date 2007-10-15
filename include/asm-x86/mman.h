5c8eec501968cf9b608bba22748d7ca1a91dadf1
Thomas Gleixner <tglx@linutronix.de>
2007-10-15 23:28:21 +0200

    x86: unify include/asm/mman_32/64.h
    Same file, except for the extra 64bit MAP_32BIT define, which does not hurt
    for 32 bit compiles.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/mman.h
+++ include/asm-x86/mman.h
@@ -1,13 +1,19 @@
-#ifdef __KERNEL__
-# ifdef CONFIG_X86_32
-#  include "mman_32.h"
-# else
-#  include "mman_64.h"
-# endif
-#else
-# ifdef __i386__
-#  include "mman_32.h"
-# else
-#  include "mman_64.h"
-# endif
-#endif
+#ifndef _ASM_X86_MMAN_H
+#define _ASM_X86_MMAN_H
+
+#include <asm-generic/mman.h>
+
+#define MAP_32BIT	0x40		/* only give out 32bit addresses */
+
+#define MAP_GROWSDOWN	0x0100		/* stack-like segment */
+#define MAP_DENYWRITE	0x0800		/* ETXTBSY */
+#define MAP_EXECUTABLE	0x1000		/* mark it as an executable */
+#define MAP_LOCKED	0x2000		/* pages are locked */
+#define MAP_NORESERVE	0x4000		/* don't check for reservations */
+#define MAP_POPULATE	0x8000		/* populate (prefault) pagetables */
+#define MAP_NONBLOCK	0x10000		/* do not block on IO */
+
+#define MCL_CURRENT	1		/* lock all current mappings */
+#define MCL_FUTURE	2		/* lock all future mappings */
+
+#endif /* _ASM_X86_MMAN_H */
