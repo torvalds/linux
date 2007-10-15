24f287e412ae90de8d281543c8b1043b6ed6c019
David S. Miller <davem@sunset.davemloft.net>
2007-10-15 16:41:44 -0700

    [SPARC64]: Implement atomic backoff.
    When the cpu count is high and contention hits an atomic object, the
    processors can synchronize such that some cpus continually get knocked
    out and cannot complete the atomic update.
    
    So implement an exponential backoff when SMP.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ include/asm-sparc64/backoff.h
@@ -0,0 +1,28 @@
+#ifndef _SPARC64_BACKOFF_H
+#define _SPARC64_BACKOFF_H
+
+#define BACKOFF_LIMIT	(4 * 1024)
+
+#ifdef CONFIG_SMP
+
+#define BACKOFF_SETUP(reg)	\
+	mov	1, reg
+
+#define BACKOFF_SPIN(reg, tmp, label)	\
+	mov	reg, tmp; \
+88:	brnz,pt	tmp, 88b; \
+	 sub	tmp, 1, tmp; \
+	cmp	reg, BACKOFF_LIMIT; \
+	bg,pn	%xcc, label; \
+	 nop; \
+	ba,pt	%xcc, label; \
+	 sllx	reg, 1, reg;
+
+#else
+
+#define BACKOFF_SETUP(reg)
+#define BACKOFF_SPIN(reg, tmp, label)
+
+#endif
+
+#endif /* _SPARC64_BACKOFF_H */
