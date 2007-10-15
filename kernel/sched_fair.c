75c28ace9f2b2f403674e045939424a77c95b47c
Srivatsa Vaddagiri <vatsa@linux.vnet.ibm.com>
2007-10-15 17:00:09 +0200

    sched: print &rq->cfs stats
    - Print &rq->cfs statistics as well (useful for group scheduling)
    
    Signed-off-by: Srivatsa Vaddagiri <vatsa@linux.vnet.ibm.com>
    Signed-off-by: Dhaval Giani <dhaval@linux.vnet.ibm.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -1024,6 +1024,9 @@ static void print_cfs_stats(struct seq_file *m, int cpu)
 {
 	struct cfs_rq *cfs_rq;
 
+#ifdef CONFIG_FAIR_GROUP_SCHED
+	print_cfs_rq(m, cpu, &cpu_rq(cpu)->cfs);
+#endif
 	for_each_leaf_cfs_rq(cpu_rq(cpu), cfs_rq)
 		print_cfs_rq(m, cpu, cfs_rq);
 }
