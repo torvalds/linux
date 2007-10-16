dab8f4963ae0a79b945b8ed297e30d390a9d9f50
Hirokazu Takata <takata@linux-m32r.org>
2007-10-16 01:26:36 -0700

    m32r: serial: remove M32R_SIO_SHARE_IRQS
    Remove an unused symbol M32R_SIO_SHARE_IRQS from drivers/serial/m32r_sio.h.
    
    Signed-off-by: Hirokazu Takata <takata@linux-m32r.org>
    Cc: "Robert P. J. Day" <rpjday@mindspring.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/serial/m32r_sio.c
+++ drivers/serial/m32r_sio.c
@@ -539,7 +539,7 @@ static void serial_do_unlink(struct irq_info *i, struct uart_sio_port *up)
 static int serial_link_irq_chain(struct uart_sio_port *up)
 {
 	struct irq_info *i = irq_lists + up->port.irq;
-	int ret, irq_flags = up->port.flags & UPF_SHARE_IRQ ? IRQF_SHARED : 0;
+	int ret, irq_flags = 0;
 
 	spin_lock_irq(&i->lock);
 
--- drivers/serial/m32r_sio.h
+++ drivers/serial/m32r_sio.h
@@ -46,9 +46,3 @@ struct old_serial_port {
 #define PROBE_ANY	(~0)
 
 #define HIGH_BITS_OFFSET ((sizeof(long)-sizeof(int))*8)
-
-#ifdef CONFIG_SERIAL_SIO_SHARE_IRQ
-#define M32R_SIO_SHARE_IRQS 1
-#else
-#define M32R_SIO_SHARE_IRQS 0
-#endif
