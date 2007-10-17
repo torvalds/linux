68fdc55c48fd2e8f4938a1e815216c25baf8a17e
Thomas Gleixner <tglx@linutronix.de>
2007-10-17 17:19:30 +0200

    x86: unify include/asm/bug_32/64.h
    Same file, except for whitespace, comment formatting and the
    .long/.quad delta which can be solved by a define.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/bug.h
+++ include/asm-x86/bug.h
@@ -1,5 +1,42 @@
+#ifndef _ASM_X86_BUG_H
+#define _ASM_X86_BUG_H
+
+#ifdef CONFIG_BUG
+#define HAVE_ARCH_BUG
+
+#ifdef CONFIG_DEBUG_BUGVERBOSE
+
 #ifdef CONFIG_X86_32
-# include "bug_32.h"
+# define __BUG_C0	"2:\t.long 1b, %c0\n"
 #else
-# include "bug_64.h"
+# define __BUG_C0	"2:\t.quad 1b, %c0\n"
+#endif
+
+#define BUG()								\
+	do {								\
+		asm volatile("1:\tud2\n"				\
+			     ".pushsection __bug_table,\"a\"\n"		\
+			     __BUG_C0					\
+			     "\t.word %c1, 0\n"				\
+			     "\t.org 2b+%c2\n"				\
+			     ".popsection"				\
+			     : : "i" (__FILE__), "i" (__LINE__),	\
+			     "i" (sizeof(struct bug_entry)));		\
+		for(;;) ;						\
+	} while(0)
+
+#else
+#define BUG()								\
+	do {								\
+		asm volatile("ud2");					\
+		for(;;) ;						\
+	} while(0)
+#endif
+
+void out_of_line_bug(void);
+#else /* CONFIG_BUG */
+static inline void out_of_line_bug(void) { }
+#endif /* !CONFIG_BUG */
+
+#include <asm-generic/bug.h>
 #endif
