ffecad95eed621a82e8131b929cfcc3bb2a10d46
Satyam Sharma <satyam@infradead.org>
2007-10-17 18:04:38 +0200

    i386: fix argument signedness warnings
    These build warnings:
    
    In file included from include/asm/thread_info.h:16,
    from include/linux/thread_info.h:21,
    from include/linux/preempt.h:9,
    from include/linux/spinlock.h:49,
    from include/linux/vmalloc.h:4,
    from arch/i386/boot/compressed/misc.c:14:
    include/asm/processor.h: In function cpuid_count
    include/asm/processor.h:615: warning: pointer targets in passing argument 1 of native_cpuid differ in signedness
    include/asm/processor.h:615: warning: pointer targets in passing argument 2 of native_cpuid differ in signedness
    include/asm/processor.h:615: warning: pointer targets in passing argument 3 of native_cpuid differ in signedness
    include/asm/processor.h:615: warning: pointer targets in passing argument 4 of native_cpuid differ in signedness
    
    come because the arguments have been specified as pointers to (signed) int
    types, not unsigned. So let's specify those as unsigned. Do some codingstyle
    here and there while at it.
    
    [ tglx: arch/x86 adaptation ]
    
    Signed-off-by: Satyam Sharma <satyam@infradead.org>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/processor_32.h
+++ include/asm-x86/processor_32.h
@@ -595,7 +595,9 @@ static inline void load_esp0(struct tss_struct *tss, struct thread_struct *threa
  * clear %ecx since some cpus (Cyrix MII) do not set or clear %ecx
  * resulting in stale register contents being returned.
  */
-static inline void cpuid(unsigned int op, unsigned int *eax, unsigned int *ebx, unsigned int *ecx, unsigned int *edx)
+static inline void cpuid(unsigned int op,
+			 unsigned int *eax, unsigned int *ebx,
+			 unsigned int *ecx, unsigned int *edx)
 {
 	*eax = op;
 	*ecx = 0;
@@ -603,8 +605,9 @@ static inline void cpuid(unsigned int op, unsigned int *eax, unsigned int *ebx, 
 }
 
 /* Some CPUID calls want 'count' to be placed in ecx */
-static inline void cpuid_count(int op, int count, int *eax, int *ebx, int *ecx,
-			       int *edx)
+static inline void cpuid_count(unsigned int op, int count,
+			       unsigned int *eax, unsigned int *ebx,
+			       unsigned int *ecx, unsigned int *edx)
 {
 	*eax = op;
 	*ecx = count;
