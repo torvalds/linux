6d43be8ea8c92a41557dacde94ae73565cbc01f0
Thomas Gleixner <tglx@linutronix.de>
2007-10-17 18:04:36 +0200

    x86: remove reminder of i386 irqstat per cpu conversion
    The i386 irqstat per cpu conversion left an bogus export of the old
    irqstat array in the header file. Remove it.
    
    [ tglx: arch/x86 adaptation ]
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/hardirq_32.h
+++ include/asm-x86/hardirq_32.h
@@ -13,7 +13,6 @@ typedef struct {
 } ____cacheline_aligned irq_cpustat_t;
 
 DECLARE_PER_CPU(irq_cpustat_t, irq_stat);
-extern irq_cpustat_t irq_stat[];
 
 #define __ARCH_IRQ_STAT
 #define __IRQ_STAT(cpu, member) (per_cpu(irq_stat, cpu).member)
