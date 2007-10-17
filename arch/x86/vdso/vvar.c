06f99ac87ef3f1f706cd1eb794b16dfbfe60ee29
Roland McGrath <roland@redhat.com>
2007-10-17 18:04:40 +0200

    x86: vdso put vars in rodata
    This adds a const to the definitions vvar.c makes, so that the vdso_*
    variables go into .rodata instead of .data.  This is essentially a
    cosmetic change, just giving the section headers in the vDSO file more
    pleasing flags. These variables are read-only from the perspective of
    the vDSO itself and user mode, even though the contents of the DSO image
    were adjusted at boot.
    
    Signed-off-by: Roland McGrath <roland@redhat.com>
    Cc: Andi Kleen <ak@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/vdso/vvar.c
+++ arch/x86/vdso/vvar.c
@@ -8,5 +8,5 @@
 #include <asm/timex.h>
 #include <asm/vgtod.h>
 
-#define VEXTERN(x) typeof (__ ## x) *vdso_ ## x = (void *)VMAGIC;
+#define VEXTERN(x) typeof (__ ## x) *const vdso_ ## x = (void *)VMAGIC;
 #include "vextern.h"
