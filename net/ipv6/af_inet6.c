dc8a82ad285dcd2831feb2fd8f7b41ce1f82e243
Pavel Emelyanov <xemul@openvz.org>
2007-10-17 19:30:40 -0700

    [IPV6]: Fix memory leak in cleanup_ipv6_mibs()
    The icmpv6msg mib statistics is not freed.
    
    This is almost not critical for current kernel, since ipv6
    module is unloadable, but this can happen on load error and 
    will happen every time we stop the network namespace (when 
    we have one, of course).
    
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>
    Acked-by: David L Stevens <dlstevens@us.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/af_inet6.c
+++ net/ipv6/af_inet6.c
@@ -747,6 +747,7 @@ static void cleanup_ipv6_mibs(void)
 {
 	snmp_mib_free((void **)ipv6_statistics);
 	snmp_mib_free((void **)icmpv6_statistics);
+	snmp_mib_free((void **)icmpv6msg_statistics);
 	snmp_mib_free((void **)udp_stats_in6);
 	snmp_mib_free((void **)udplite_stats_in6);
 }
