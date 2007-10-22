9c1c6a1ba786d58bd03e27ee49f89a5685e8e07b
Alexey Starikovskiy <astarikovskiy@suse.de>
2007-10-22 14:18:06 +0400

    ACPI: sleep: Fix GPE suspend cleanup
    Commit 9b039330808b83acac3597535da26f47ad1862ce removed
    acpi_gpe_sleep_prepare(), the only function used at S5 transition
    Add call to generic acpi_enable_wake_device().
    
    Reference: https://bugzilla.novell.com/show_bug.cgi?id=299882
    
    Signed-off-by: Alexey Starikovskiy <astarikovskiy@suse.de>
    Acked-by: Rafael J. Wysocki <rjw@sisk.pl>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/sleep/main.c
+++ drivers/acpi/sleep/main.c
@@ -410,6 +410,7 @@ static void acpi_power_off(void)
 	/* acpi_sleep_prepare(ACPI_STATE_S5) should have already been called */
 	printk("%s called\n", __FUNCTION__);
 	local_irq_disable();
+	acpi_enable_wakeup_device(ACPI_STATE_S5);
 	acpi_enter_sleep_state(ACPI_STATE_S5);
 }
 
