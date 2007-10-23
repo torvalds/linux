f186c9ad181ab9ba35e28cc9bbaafe226dbaec8c
Greg Ungerer <gerg@snapgear.com>
2007-10-23 14:37:54 +1000

    m68knommu: local module/elf definitions
    Up to now m68knommu has been using the asm-m68k/module.h instead of
    defining its own. There are recent changes there that we don't need
    (fixups specifically). We don't need much support here so it makes
    sense to have an m68knommu specific one now.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-m68knommu/module.h
+++ include/asm-m68knommu/module.h
@@ -1,1 +1,11 @@
-#include <asm-m68k/module.h>
+#ifndef ASM_M68KNOMMU_MODULE_H
+#define ASM_M68KNOMMU_MODULE_H
+
+struct mod_arch_specific {
+};
+
+#define Elf_Shdr Elf32_Shdr
+#define Elf_Sym Elf32_Sym
+#define Elf_Ehdr Elf32_Ehdr
+
+#endif /* ASM_M68KNOMMU_MODULE_H */
