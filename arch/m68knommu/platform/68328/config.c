0e298ebec4ddc84741340fb085887ad8732f727b
Greg Ungerer <gerg@snapgear.com>
2007-10-24 12:03:16 +1000

    m68knommu: cleanup 68328 init code
    Clean up 68328 timer support code. Removed header includes not needed.
    Remove use of old m68knommu timer function pointers.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/m68knommu/platform/68328/config.c
+++ arch/m68knommu/platform/68328/config.c
@@ -15,28 +15,14 @@
 
 /***************************************************************************/
 
-#include <asm/dbg.h>
-#include <stdarg.h>
 #include <linux/types.h>
 #include <linux/kernel.h>
-#include <linux/mm.h>
-#include <linux/tty.h>
-#include <linux/console.h>
-#include <linux/interrupt.h>
-#include <asm/current.h>
-
-#include <asm/setup.h>
 #include <asm/system.h>
-#include <asm/pgtable.h>
-#include <asm/irq.h>
 #include <asm/machdep.h>
 #include <asm/MC68328.h>
 
 /***************************************************************************/
 
-void m68328_timer_init(irq_handler_t timer_routine);
-void m68328_timer_tick(void);
-unsigned long m68328_timer_gettimeoffset(void);
 void m68328_timer_gettod(int *year, int *mon, int *day, int *hour, int *min, int *sec);
 
 /***************************************************************************/
@@ -59,14 +45,8 @@ void config_BSP(char *command, int len)
   printk(KERN_INFO "68328 support Kenneth Albanowski <kjahds@kjshds.com>\n");
   printk(KERN_INFO "68328/Pilot support Bernhard Kuhn <kuhn@lpr.e-technik.tu-muenchen.de>\n");
 
-  mach_sched_init      = m68328_timer_init;
-  mach_tick            = m68328_timer_tick;
-  mach_gettimeoffset   = m68328_timer_gettimeoffset;
-  mach_gettod          = m68328_timer_gettod;
-  mach_hwclk           = NULL;
-  mach_set_clock_mmss  = NULL;
-  mach_reset           = m68328_reset;
-  *command = '\0';
+  mach_gettod = m68328_timer_gettod;
+  mach_reset = m68328_reset;
 }
 
 /***************************************************************************/
