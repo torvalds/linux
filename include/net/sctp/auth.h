8ad7c62b752483982a678c78a52a70f498b84cbb
Adrian Bunk <bunk@kernel.org>
2007-10-26 04:21:23 -0700

    [SCTP] net/sctp/auth.c: make 3 functions static
    This patch makes three needlessly global functions static.
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Acked-by: Vlad Yasevich <vladislav.yasevich@hp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/sctp/auth.h
+++ include/net/sctp/auth.h
@@ -88,7 +88,6 @@ static inline void sctp_auth_key_hold(struct sctp_auth_bytes *key)
 
 void sctp_auth_key_put(struct sctp_auth_bytes *key);
 struct sctp_shared_key *sctp_auth_shkey_create(__u16 key_id, gfp_t gfp);
-void sctp_auth_shkey_free(struct sctp_shared_key *sh_key);
 void sctp_auth_destroy_keys(struct list_head *keys);
 int sctp_auth_asoc_init_active_key(struct sctp_association *asoc, gfp_t gfp);
 struct sctp_shared_key *sctp_auth_get_shkey(
