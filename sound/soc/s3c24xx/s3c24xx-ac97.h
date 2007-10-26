040956fabbc16b9ce746a03d2b589052e1771138
Ivan Kuten <ivan.kuten@promwad.com>
2007-10-26 14:53:47 +0200

    [ALSA] soc - ln2440sbc ac97 support
    This patch adds ac97 support for ln2440sbc board from LittleChips.
    This board is based on s3c2440 SoC + AC97 Realtek ALC650 codec.
    Existing s3c2443 implementation is slightly modified because s3c2440
    and s3c2443 have different AC97 interrupts.
    
    Signed-off-by: Ivan Kuten <ivan.kuten@promwad.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- sound/soc/s3c24xx/s3c24xx-ac97.h
+++ sound/soc/s3c24xx/s3c24xx-ac97.h
@@ -20,6 +20,12 @@
 #define AC_CMD_ADDR(x) (x << 16)
 #define AC_CMD_DATA(x) (x & 0xffff)
 
+#ifdef CONFIG_CPU_S3C2440
+#define IRQ_S3C244x_AC97 IRQ_S3C2440_AC97
+#else
+#define IRQ_S3C244x_AC97 IRQ_S3C2443_AC97
+#endif
+
 extern struct snd_soc_cpu_dai s3c2443_ac97_dai[];
 
 #endif /*S3C24XXAC97_H_*/
