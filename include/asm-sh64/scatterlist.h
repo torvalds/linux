cbd627a44928ba20b5c22b21b8ce2be27733a9d7
Robert P. J. Day <rpjday@crashcourse.ca>
2007-10-30 13:08:47 +0900

    sh64: Move DMA macros from pci.h to scatterlist.h.
    In order to be more consistent with other architectures, move two
    DMA-related scatterlist macros from pci.h to scatterlist.h.
    
    Signed-off-by: Robert P. J. Day <rpjday@crashcourse.ca>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh64/scatterlist.h
+++ include/asm-sh64/scatterlist.h
@@ -23,6 +23,15 @@ struct scatterlist {
     unsigned int length;
 };
 
+/* These macros should be used after a pci_map_sg call has been done
+ * to get bus addresses of each of the SG entries and their lengths.
+ * You should only work with the number of sg entries pci_map_sg
+ * returns, or alternatively stop on the first sg_dma_len(sg) which
+ * is 0.
+ */
+#define sg_dma_address(sg)	((sg)->dma_address)
+#define sg_dma_len(sg)		((sg)->length)
+
 #define ISA_DMA_THRESHOLD (0xffffffff)
 
 #endif /* !__ASM_SH64_SCATTERLIST_H */
