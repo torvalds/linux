aa91c72566a5a6d52f11b2f8d98bcf1774eeccfd
ashish kalra <ashish.kalra@freescale.com>
2007-10-31 19:28:04 +0800

    ata/sata_fsl: Remove ata_scsi_suspend/resume callbacks
    Signed-off-by: ashish kalra <ashish.kalra@freescale.com>
    Signed-off-by: Li Yang <leoli@freescale.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/ata/sata_fsl.c
+++ drivers/ata/sata_fsl.c
@@ -1223,10 +1223,6 @@ static struct scsi_host_template sata_fsl_sht = {
 	.slave_configure = ata_scsi_slave_config,
 	.slave_destroy = ata_scsi_slave_destroy,
 	.bios_param = ata_std_bios_param,
-#ifdef CONFIG_PM
-	.suspend = ata_scsi_device_suspend,
-	.resume = ata_scsi_device_resume,
-#endif
 };
 
 static const struct ata_port_operations sata_fsl_ops = {
