a8399c512b5fa2cf80831f5b4cd3adffd299fbe3
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2007-11-04 00:44:31 -0400

    Input: i8042 - use synchronize_irq() instead of synchronize_sched()
    RT guys advised me that in their kernels synchronize_sched() will not
    work to ensure that all IRQ handlers run to their completion and that
    synchronize_irq() should be used instead.
    
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/serio/i8042.c
+++ drivers/input/serio/i8042.c
@@ -280,7 +280,14 @@ static void i8042_stop(struct serio *serio)
 	struct i8042_port *port = serio->port_data;
 
 	port->exists = 0;
-	synchronize_sched();
+
+	/*
+	 * We synchronize with both AUX and KBD IRQs because there is
+	 * a (very unlikely) chance that AUX IRQ is raised for KBD port
+	 * and vice versa.
+	 */
+	synchronize_irq(I8042_AUX_IRQ);
+	synchronize_irq(I8042_KBD_IRQ);
 	port->serio = NULL;
 }
 
