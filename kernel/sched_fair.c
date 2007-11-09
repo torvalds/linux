10b777246c6953100099af1870d35c8b24d49b12
Peter Zijlstra <a.p.zijlstra@chello.nl>
2007-11-09 22:39:37 +0100

    sched: fix vslice
    vslice was missing a factor NICE_0_LOAD, as weight is in
    weight*NICE_0_LOAD units.
    
    the effect of this bug was larger initial slices and
    thus latency-noisier forks.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -259,6 +259,7 @@ static u64 __sched_vslice(unsigned long rq_weight, unsigned long nr_running)
 {
 	u64 vslice = __sched_period(nr_running);
 
+	vslice *= NICE_0_LOAD;
 	do_div(vslice, rq_weight);
 
 	return vslice;
