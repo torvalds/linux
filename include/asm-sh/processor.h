343ac72248d360f1fae72176aca1117be19189ec
Paul Mundt <lethal@linux-sh.org>
2007-11-11 18:11:18 +0900

    sh: Move over the SH-5 entry.S.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/processor.h
+++ include/asm-sh/processor.h
@@ -3,6 +3,8 @@
 
 #include <asm/cpu-features.h>
 
+#ifndef __ASSEMBLY__
+
 /*
  *  CPU type and hardware bug flags. Kept separately for each CPU.
  *
@@ -40,11 +42,8 @@ enum cpu_type {
 	CPU_SH_NONE
 };
 
-#ifdef CONFIG_SUPERH32
-# include "processor_32.h"
-#else
-# include "processor_64.h"
-#endif
+/* Forward decl */
+struct sh_cpuinfo;
 
 /* arch/sh/kernel/setup.c */
 const char *get_cpu_subtype(struct sh_cpuinfo *c);
@@ -53,6 +52,14 @@ const char *get_cpu_subtype(struct sh_cpuinfo *c);
 int vsyscall_init(void);
 #else
 #define vsyscall_init() do { } while (0)
+#endif
+
+#endif /* __ASSEMBLY__ */
+
+#ifdef CONFIG_SUPERH32
+# include "processor_32.h"
+#else
+# include "processor_64.h"
 #endif
 
 #endif /* __ASM_SH_PROCESSOR_H */
