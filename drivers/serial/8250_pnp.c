7105458563213b6f6fb523065474cfe1d6c22a67
Damian Jurd <dnj@Allotrope.com.au>
2007-11-14 16:59:04 -0800

    8250_pnp: add support for "LG C1 EXPRESS DUAL" machines
    The following is an extra entry to enable the touch screen on the new LG
    C1 EXPRESS DUAL machine.
    
    Cc: Russell King <rmk@arm.linux.org.uk>
    Cc: Bjorn Helgaas <bjorn.helgaas@hp.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/serial/8250_pnp.c
+++ drivers/serial/8250_pnp.c
@@ -347,6 +347,11 @@ static const struct pnp_device_id pnp_dev_table[] = {
 	/* Fujitsu Wacom Tablet PC devices */
 	{	"FUJ02E5",		0	},
 	{	"FUJ02E6",		0	},
+	/*
+	 * LG C1 EXPRESS DUAL (C1-PB11A3) touch screen (actually a FUJ02E6 in
+	 * disguise)
+	 */
+	{	"LTS0001",		0       },
 	/* Rockwell's (PORALiNK) 33600 INT PNP */
 	{	"WCI0003",		0	},
 	/* Unkown PnP modems */
