145dea009828df7b091e7f7f24497ceb12dbbb3d
David S. Miller <davem@sunset.davemloft.net>
2007-11-16 02:57:01 -0800

    [SPARC]: Merge asm-sparc{,64}/bugs.h
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc64/bugs.h
+++ include/asm-sparc64/bugs.h
@@ -1,10 +1,1 @@
-/* bugs.h: Sparc64 probes for various bugs.
- *
- * Copyright (C) 1996, 2007 David S. Miller (davem@davemloft.net)
- */
-#include <asm/sstate.h>
-
-static void __init check_bugs(void)
-{
-	sstate_running();
-}
+#include <asm-sparc/bugs.h>
