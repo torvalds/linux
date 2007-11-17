8d2e24c3c1ecac394c3a9a1514bc883a868851d6
Adrian Bunk <bunk@kernel.org>
2007-11-17 23:05:06 +0800

    Blackfin arch: unexport get_wchan
    The only user of get_wchan I was able to find is the proc fs - and proc
    can't be built modular.
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Bryan Wu <bryan.wu@analog.com>

--- arch/blackfin/kernel/bfin_ksyms.c
+++ arch/blackfin/kernel/bfin_ksyms.c
@@ -65,7 +65,6 @@ EXPORT_SYMBOL(memset);
 EXPORT_SYMBOL(memcmp);
 EXPORT_SYMBOL(memmove);
 EXPORT_SYMBOL(memchr);
-EXPORT_SYMBOL(get_wchan);
 
 /*
  * libgcc functions - functions that are used internally by the
