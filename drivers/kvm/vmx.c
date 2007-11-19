15b00f32d53f31fe194b17ca0384a37756b55be2
Jan Kiszka <jan.kiszka@siemens.com>
2007-11-19 10:21:45 +0100

    KVM: VMX: Force seg.base == (seg.sel << 4) in real  mode
    Ensure that segment.base == segment.selector << 4 when entering the real
    mode on Intel so that the CPU will not bark at us.  This fixes some old
    protected mode demo from http://www.x86.org/articles/pmbasics/tspec_a1_doc.htm.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- drivers/kvm/vmx.c
+++ drivers/kvm/vmx.c
@@ -1165,7 +1165,8 @@ static void fix_rmode_seg(int seg, struct kvm_save_segment *save)
 	save->base = vmcs_readl(sf->base);
 	save->limit = vmcs_read32(sf->limit);
 	save->ar = vmcs_read32(sf->ar_bytes);
-	vmcs_write16(sf->selector, vmcs_readl(sf->base) >> 4);
+	vmcs_write16(sf->selector, save->base >> 4);
+	vmcs_write32(sf->base, save->base & 0xfffff);
 	vmcs_write32(sf->limit, 0xffff);
 	vmcs_write32(sf->ar_bytes, 0xf3);
 }
