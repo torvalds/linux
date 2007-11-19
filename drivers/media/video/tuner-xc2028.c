e155d908f72cc429b538c101ee8ffcd10a44b69b
Chris Pascoe <c.pascoe@itee.uq.edu.au>
2007-11-19 04:16:47 -0300

    V4L/DVB (6632): xc2028: fix inverted logic in audio standard check
    strcasecmp returns 0 on match, not true.
    
    Signed-off-by: Chris Pascoe <c.pascoe@itee.uq.edu.au>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/tuner-xc2028.c
+++ drivers/media/video/tuner-xc2028.c
@@ -200,17 +200,17 @@ void dump_firm_type(unsigned int type)
 
 static  v4l2_std_id parse_audio_std_option(void)
 {
-	if (strcasecmp(audio_std, "A2"))
+	if (strcasecmp(audio_std, "A2") == 0)
 		return V4L2_STD_A2;
-	if (strcasecmp(audio_std, "A2/A"))
+	if (strcasecmp(audio_std, "A2/A") == 0)
 		return V4L2_STD_A2_A;
-	if (strcasecmp(audio_std, "A2/B"))
+	if (strcasecmp(audio_std, "A2/B") == 0)
 		return V4L2_STD_A2_B;
-	if (strcasecmp(audio_std, "NICAM"))
+	if (strcasecmp(audio_std, "NICAM") == 0)
 		return V4L2_STD_NICAM;
-	if (strcasecmp(audio_std, "NICAM/A"))
+	if (strcasecmp(audio_std, "NICAM/A") == 0)
 		return V4L2_STD_NICAM_A;
-	if (strcasecmp(audio_std, "NICAM/B"))
+	if (strcasecmp(audio_std, "NICAM/B") == 0)
 		return V4L2_STD_NICAM_B;
 
 	return 0;
