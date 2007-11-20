c2672f62e734292bca4ce4ab5e89c7e04ecb70dc
Paul Mundt <lethal@linux-sh.org>
2007-11-20 18:29:00 +0900

    sh: Add the SH-5 cpu type symbolic names.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/setup.c
+++ arch/sh/kernel/setup.c
@@ -307,7 +307,9 @@ static const char *cpu_name[] = {
 	[CPU_SH7770]	= "SH7770",	[CPU_SH7780]	= "SH7780",
 	[CPU_SH7781]	= "SH7781",	[CPU_SH7343]	= "SH7343",
 	[CPU_SH7785]	= "SH7785",	[CPU_SH7722]	= "SH7722",
-	[CPU_SHX3]	= "SH-X3",	[CPU_SH_NONE]	= "Unknown"
+	[CPU_SHX3]	= "SH-X3",
+	[CPU_SH5_101]	= "SH5-101",	[CPU_SH5_103]	= "SH5-103",
+	[CPU_SH_NONE]	= "Unknown"
 };
 
 const char *get_cpu_subtype(struct sh_cpuinfo *c)
