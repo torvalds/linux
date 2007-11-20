0d8a4e0cd688ad0de6430ce3425c7849cfec1c2d
Mark Fasheh <mark.fasheh@oracle.com>
2007-11-20 11:48:41 -0800

    ocfs2: Fix comparison in ocfs2_size_fits_inline_data()
    This was causing us to prematurely push out inline data by one byte.
    
    Signed-off-by: Mark Fasheh <mark.fasheh@oracle.com>

--- fs/ocfs2/aops.c
+++ fs/ocfs2/aops.c
@@ -1514,7 +1514,7 @@ int ocfs2_size_fits_inline_data(struct buffer_head *di_bh, u64 new_size)
 {
 	struct ocfs2_dinode *di = (struct ocfs2_dinode *)di_bh->b_data;
 
-	if (new_size < le16_to_cpu(di->id2.i_data.id_count))
+	if (new_size <= le16_to_cpu(di->id2.i_data.id_count))
 		return 1;
 	return 0;
 }
