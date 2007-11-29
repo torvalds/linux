d0186b25e65d4d786727a03044b8aafe2ba118ee
Miklos Szeredi <mszeredi@suse.cz>
2007-11-28 16:22:01 -0800

    fuse: fix FUSE_FILE_OPS sending
    FUSE_FILE_OPS is meant to signal that the kernel will send the open file to to
    the userspace filesystem for operations on open files, so that sillyrenaming
    unlinked files becomes unnecessary.
    
    However this needs VFS changes, which won't make it into 2.6.24.
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -562,8 +562,7 @@ static void fuse_send_init(struct fuse_conn *fc, struct fuse_req *req)
 	arg->major = FUSE_KERNEL_VERSION;
 	arg->minor = FUSE_KERNEL_MINOR_VERSION;
 	arg->max_readahead = fc->bdi.ra_pages * PAGE_CACHE_SIZE;
-	arg->flags |= FUSE_ASYNC_READ | FUSE_POSIX_LOCKS | FUSE_FILE_OPS |
-		FUSE_ATOMIC_O_TRUNC;
+	arg->flags |= FUSE_ASYNC_READ | FUSE_POSIX_LOCKS | FUSE_ATOMIC_O_TRUNC;
 	req->in.h.opcode = FUSE_INIT;
 	req->in.numargs = 1;
 	req->in.args[0].size = sizeof(*arg);
