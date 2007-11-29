b7e0fe9f81e19c4f2a1369b324c3c062c1738be4
Vlad Yasevich <vladislav.yasevich@hp.com>
2007-11-29 09:53:52 -0500

    SCTP: Fix build issues with SCTP AUTH.
    SCTP-AUTH requires selection of CRYPTO, HMAC and SHA1 since
    SHA1 is a MUST requirement for AUTH.  We also support SHA256,
    but that's optional, so fix the code to treat it as such.
    
    Signed-off-by: Vlad Yasevich <vladislav.yasevich@hp.com>

--- net/sctp/auth.c
+++ net/sctp/auth.c
@@ -54,11 +54,13 @@ static struct sctp_hmac sctp_hmac_list[SCTP_AUTH_NUM_HMACS] = {
 		/* id 2 is reserved as well */
 		.hmac_id = SCTP_AUTH_HMAC_ID_RESERVED_2,
 	},
+#if defined (CONFIG_CRYPTO_SHA256) || defined (CONFIG_CRYPTO_SHA256_MODULE)
 	{
 		.hmac_id = SCTP_AUTH_HMAC_ID_SHA256,
 		.hmac_name="hmac(sha256)",
 		.hmac_len = SCTP_SHA256_SIG_SIZE,
 	}
+#endif
 };
 
 
