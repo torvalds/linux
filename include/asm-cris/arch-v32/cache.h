738af38bbc8bb4a5b081935c47744fdb7bf0f70b
Jesper Nilsson <jesper.nilsson@axis.com>
2007-11-30 10:11:43 +0100

    CRIS v32: Add prototypes for cache flushing
    We need these to work around some cache bugs in CRISv32 chips.

--- include/asm-cris/arch-v32/cache.h
+++ include/asm-cris/arch-v32/cache.h
@@ -1,8 +1,19 @@
 #ifndef _ASM_CRIS_ARCH_CACHE_H
 #define _ASM_CRIS_ARCH_CACHE_H
 
+#include <asm/arch/hwregs/dma.h>
+
 /* A cache-line is 32 bytes. */
 #define L1_CACHE_BYTES 32
 #define L1_CACHE_SHIFT 5
+
+void flush_dma_list(dma_descr_data *descr);
+void flush_dma_descr(dma_descr_data *descr, int flush_buf);
+
+#define flush_dma_context(c) \
+  flush_dma_list(phys_to_virt((c)->saved_data));
+
+void cris_flush_cache_range(void *buf, unsigned long len);
+void cris_flush_cache(void);
 
 #endif /* _ASM_CRIS_ARCH_CACHE_H */
