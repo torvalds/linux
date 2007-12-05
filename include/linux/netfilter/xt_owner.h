0265ab44bacc1a1e0e3f5873d8ca2d5a29e33db2
Jan Engelhardt <jengelh@computergmbh.de>
2007-12-04 23:27:38 -0800

    [NETFILTER]: merge ipt_owner/ip6t_owner in xt_owner
    xt_owner merges ipt_owner and ip6t_owner, and adds a flag to match
    on socket (non-)existence.
    
    Signed-off-by: Jan Engelhardt <jengelh@computergmbh.de>
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ include/linux/netfilter/xt_owner.h
@@ -0,0 +1,16 @@
+#ifndef _XT_OWNER_MATCH_H
+#define _XT_OWNER_MATCH_H
+
+enum {
+	XT_OWNER_UID    = 1 << 0,
+	XT_OWNER_GID    = 1 << 1,
+	XT_OWNER_SOCKET = 1 << 2,
+};
+
+struct xt_owner_match_info {
+	u_int32_t uid;
+	u_int32_t gid;
+	u_int8_t match, invert;
+};
+
+#endif /* _XT_OWNER_MATCH_H */
