6c0cd7c000dc0851035c5003bf9d47733d0b257b
Dor Laor <dor.laor@qumranet.com>
2007-12-16 15:19:43 +0200

    virtio_net: parametrize the napi_weight for virtio receive queue.
    It is done in order to improve performance.
    
    Signed-off-by: Dor Laor <dor.laor@qumranet.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -24,6 +24,9 @@
 #include <linux/virtio_net.h>
 #include <linux/scatterlist.h>
 
+static int napi_weight = 128;
+module_param(napi_weight, int, 0444);
+
 static int csum = 1, gso = 1;
 module_param(csum, bool, 0444);
 module_param(gso, bool, 0444);
@@ -353,7 +356,7 @@ static int virtnet_probe(struct virtio_device *vdev)
 
 	/* Set up our device-specific information */
 	vi = netdev_priv(dev);
-	netif_napi_add(dev, &vi->napi, virtnet_poll, 16);
+	netif_napi_add(dev, &vi->napi, virtnet_poll, napi_weight);
 	vi->dev = dev;
 	vi->vdev = vdev;
 
