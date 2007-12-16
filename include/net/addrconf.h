3db8cda362dced00caf19865ffda3fa1028c59bc
Joe Perches <joe@perches.com>
2007-12-16 13:43:24 -0800

    [IPV4] include/net: Use ipv4_is_<type>
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/addrconf.h
+++ include/net/addrconf.h
@@ -252,10 +252,12 @@ static inline int ipv6_addr_is_ll_all_routers(const struct in6_addr *addr)
 
 static inline int ipv6_isatap_eui64(u8 *eui, __be32 addr)
 {
-	eui[0] = (ZERONET(addr) || PRIVATE_10(addr) || LOOPBACK(addr) ||
-		  LINKLOCAL_169(addr) || PRIVATE_172(addr) || TEST_192(addr) ||
-		  ANYCAST_6TO4(addr) || PRIVATE_192(addr) || TEST_198(addr) ||
-		  MULTICAST(addr) || BADCLASS(addr)) ? 0x00 : 0x02;
+	eui[0] = (ipv4_is_zeronet(addr) || ipv4_is_private_10(addr) ||
+		  ipv4_is_loopback(addr) || ipv4_is_linklocal_169(addr) ||
+		  ipv4_is_private_172(addr) || ipv4_is_test_192(addr) ||
+		  ipv4_is_anycast_6to4(addr) || ipv4_is_private_192(addr) ||
+		  ipv4_is_test_198(addr) || ipv4_is_multicast(addr) ||
+		  ipv4_is_badclass(addr)) ? 0x00 : 0x02;
 	eui[1] = 0;
 	eui[2] = 0x5E;
 	eui[3] = 0xFE;
