e2f82ac3fcffffca59751b65124544d11ed8be4a
Eric Dumazet <dada1@cosmosbay.com>
2007-12-17 22:45:13 -0800

    [NETFILTER]: xt_hashlimit: speedup hash_dst()
    1) Using jhash2() instead of jhash() is a litle bit faster if applicable.
    
    2) Thanks to jhash, hash value uses full 32 bits.
       Instead of returning hash % size (implying a divide)
       we return the high 32 bits of the (hash * size) that will
       give results between [0 and size-1] and same hash distribution.
    
      On most cpus, a multiply is less expensive than a divide, by an order
      of magnitude.
    
    Signed-off-by: Eric Dumazet <dada1@cosmosbay.com>
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netfilter/xt_hashlimit.c
+++ net/netfilter/xt_hashlimit.c
@@ -105,7 +105,16 @@ static inline bool dst_cmp(const struct dsthash_ent *ent,
 static u_int32_t
 hash_dst(const struct xt_hashlimit_htable *ht, const struct dsthash_dst *dst)
 {
-	return jhash(dst, sizeof(*dst), ht->rnd) % ht->cfg.size;
+	u_int32_t hash = jhash2((const u32 *)dst,
+				sizeof(*dst)/sizeof(u32),
+				ht->rnd);
+	/*
+	 * Instead of returning hash % ht->cfg.size (implying a divide)
+	 * we return the high 32 bits of the (hash * ht->cfg.size) that will
+	 * give results between [0 and cfg.size-1] and same hash distribution,
+	 * but using a multiply, less expensive than a divide
+	 */
+	return ((u64)hash * ht->cfg.size) >> 32;
 }
 
 static struct dsthash_ent *
