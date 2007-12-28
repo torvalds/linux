4195e31780a20e09c6e793c2d96390e05309e226
Oliver Hartkopp <oliver@hartkopp.net>
2007-12-27 16:50:06 -0800

    [CAN]: Fix plain integer definitions in userspace header.
    This patch fixes the use of plain integers instead of __u32 in a struct
    that is visible from kernel space and user space.
    
    Thanks to Sam Ravnborg for pointing out the wrong plain int usage.
    
    Signed-off-by: Oliver Hartkopp <oliver@hartkopp.net>
    Acked-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/can/bcm.h
+++ include/linux/can/bcm.h
@@ -26,12 +26,12 @@
  * @frames:    array of CAN frames.
  */
 struct bcm_msg_head {
-	int opcode;
-	int flags;
-	int count;
+	__u32 opcode;
+	__u32 flags;
+	__u32 count;
 	struct timeval ival1, ival2;
 	canid_t can_id;
-	int nframes;
+	__u32 nframes;
 	struct can_frame frames[0];
 };
 
