087980295082ccaa816330bc69c29a2ff53a244c
Yi Yang <yi.y.yang@intel.com>
2007-12-27 22:04:26 -0500

    ACPI: /proc/acpi/alarm parsing: handle large numbers properly
    In function acpi_system_write_alarm in file drivers/acpi/sleep/proc.c,
    big sec, min, hr, mo, day and yr are counted twice to get reasonable
    values, that is very superfluous, we can do that only once.
    
    In additon, /proc/acpi/alarm can set a related value which can be
    specified as YYYY years MM months DD days HH hours MM minutes SS
    senconds, it isn't a date, so you can specify as +0000-00-00 96:00:00
    , that means 3 days later, current code can't handle such a case.
    
    This patch removes unnecessary code and does with the aforementioned
    situation.
    
    Before applying this patch:
    
    [root@localhost /]# cat /proc/acpi/alarm
    2007-12-00 00:00:00
    [root@localhost /]# echo "0000-00-00 96:180:180" > /proc/acpi/alarm
    [root@localhost /]# cat /proc/acpi/alarm
    0007-12-02 **:**:**
    [root@localhost /]#
    
    After applying this patch:
    
    [root@localhost ~]# echo "2007-12-00 00:00:00" > /proc/acpi/alarm
    [root@localhost ~]# cat /proc/acpi/alarm
    2007-12-00 00:00:00
    [root@localhost ~]# echo "0000-00-00 96:180:180" > /proc/acpi/alarm
    [root@localhost ~]# cat /proc/acpi/alarm
    0007-12-04 03:03:00
    [root@localhost ~]#
    
    Signed-off-by: Yi Yang <yi.y.yang@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/sleep/proc.c
+++ drivers/acpi/sleep/proc.c
@@ -251,27 +251,6 @@ acpi_system_write_alarm(struct file *file,
 	if ((result = get_date_field(&p, &sec)))
 		goto end;
 
-	if (sec > 59) {
-		min += 1;
-		sec -= 60;
-	}
-	if (min > 59) {
-		hr += 1;
-		min -= 60;
-	}
-	if (hr > 23) {
-		day += 1;
-		hr -= 24;
-	}
-	if (day > 31) {
-		mo += 1;
-		day -= 31;
-	}
-	if (mo > 12) {
-		yr += 1;
-		mo -= 12;
-	}
-
 	spin_lock_irq(&rtc_lock);
 
 	rtc_control = CMOS_READ(RTC_CONTROL);
@@ -288,24 +267,24 @@ acpi_system_write_alarm(struct file *file,
 	spin_unlock_irq(&rtc_lock);
 
 	if (sec > 59) {
-		min++;
-		sec -= 60;
+		min += sec/60;
+		sec = sec%60;
 	}
 	if (min > 59) {
-		hr++;
-		min -= 60;
+		hr += min/60;
+		min = min%60;
 	}
 	if (hr > 23) {
-		day++;
-		hr -= 24;
+		day += hr/24;
+		hr = hr%24;
 	}
 	if (day > 31) {
-		mo++;
-		day -= 31;
+		mo += day/32;
+		day = day%32;
 	}
 	if (mo > 12) {
-		yr++;
-		mo -= 12;
+		yr += mo/13;
+		mo = mo%13;
 	}
 
 	spin_lock_irq(&rtc_lock);
