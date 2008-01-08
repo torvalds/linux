faa6511109026f1549619ebce972be00d1b921ce
Stephen Neuendorffer <stephen.neuendorffer@xilinx.com>
2008-01-09 06:35:06 +1100

    [POWERPC] Xilinx: Add correct compatible list for device tree bus bindings.
    Includes both flavors of plb, opb, dcr, and a pseudo 'compound' bus
    for representing compound peripherals containing more than one logical
    device.
    
    Signed-off-by: Stephen Neuendorffer <stephen.neuendorffer@xilinx.com>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- arch/powerpc/platforms/40x/virtex.c
+++ arch/powerpc/platforms/40x/virtex.c
@@ -15,9 +15,19 @@
 #include <asm/time.h>
 #include <asm/xilinx_intc.h>
 
+static struct of_device_id xilinx_of_bus_ids[] __initdata = {
+	{ .compatible = "xlnx,plb-v46-1.00.a", },
+	{ .compatible = "xlnx,plb-v34-1.01.a", },
+	{ .compatible = "xlnx,plb-v34-1.02.a", },
+	{ .compatible = "xlnx,opb-v20-1.10.c", },
+	{ .compatible = "xlnx,dcr-v29-1.00.a", },
+	{ .compatible = "xlnx,compound", },
+	{}
+};
+
 static int __init virtex_device_probe(void)
 {
-	of_platform_bus_probe(NULL, NULL, NULL);
+	of_platform_bus_probe(NULL, xilinx_of_bus_ids, NULL);
 
 	return 0;
 }
