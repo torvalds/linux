09566765efd034feba45611f9d0ae9a702f8bb1d
Avi Kivity <avi@qumranet.com>
2008-01-23 18:14:23 +0200

    KVM: Only x86 has pio
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -678,8 +678,10 @@ static int kvm_vcpu_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
 
 	if (vmf->pgoff == 0)
 		page = virt_to_page(vcpu->run);
+#ifdef CONFIG_X86
 	else if (vmf->pgoff == KVM_PIO_PAGE_OFFSET)
 		page = virt_to_page(vcpu->arch.pio_data);
+#endif
 	else
 		return VM_FAULT_SIGBUS;
 	get_page(page);
