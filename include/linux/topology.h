32525d022ad52a5c14e80e130260431e16e294b6
Ingo Molnar <mingo@elte.hu>
2008-01-25 21:08:20 +0100

    sched: whitespace cleanups in topology.h
    whitespace cleanups in topology.h.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/topology.h
+++ include/linux/topology.h
@@ -5,7 +5,7 @@
  *
  * Copyright (C) 2002, IBM Corp.
  *
- * All rights reserved.          
+ * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
