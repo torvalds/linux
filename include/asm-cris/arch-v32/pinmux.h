230d69cf2ba6505fccdb7216da9eedc924c00d19
Jesper Nilsson <jesper.nilsson@axis.com>
2008-01-28 18:06:11 +0100

    CRIS v32: Add prototype for crisv32_pinmux_dealloc_fixed in asm-cris/arch-v32/pinmux.h
    Deallocation was not possible before, but is now.

--- include/asm-cris/arch-v32/pinmux.h
+++ include/asm-cris/arch-v32/pinmux.h
@@ -34,6 +34,7 @@ int crisv32_pinmux_init(void);
 int crisv32_pinmux_alloc(int port, int first_pin, int last_pin, enum pin_mode);
 int crisv32_pinmux_alloc_fixed(enum fixed_function function);
 int crisv32_pinmux_dealloc(int port, int first_pin, int last_pin);
+int crisv32_pinmux_dealloc_fixed(enum fixed_function function);
 void crisv32_pinmux_dump(void);
 
 #endif
