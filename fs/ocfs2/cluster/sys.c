6b11d8179d1c6e560edc02c40a53b65fde83bf3f
Joel Becker <Joel.Becker@oracle.com>
2008-01-28 18:52:04 -0800

    ocfs2: Fix userspace ABI breakage in sysfs
    The userspace ABI of ocfs2's internal cluster stack (o2cb) was broken by
    commit c60b71787982cefcf9fa09aa281fa8c4c685d557 "kset: convert ocfs2 to
    use kset_create".  Specifically, the '/sys/o2cb' kset was moved to
    '/sys/fs/o2cb'.  This breaks all ocfs2 tools and renders the
    filesystem unmountable.
    
    This fix moves '/sys/o2cb' back where it belongs.
    
    Signed-off-by: Joel Becker <joel.becker@oracle.com>
    Signed-off-by: Mark Fasheh <mark.fasheh@oracle.com>

--- fs/ocfs2/cluster/sys.c
+++ fs/ocfs2/cluster/sys.c
@@ -64,7 +64,7 @@ int o2cb_sys_init(void)
 {
 	int ret;
 
-	o2cb_kset = kset_create_and_add("o2cb", NULL, fs_kobj);
+	o2cb_kset = kset_create_and_add("o2cb", NULL, NULL);
 	if (!o2cb_kset)
 		return -ENOMEM;
 
