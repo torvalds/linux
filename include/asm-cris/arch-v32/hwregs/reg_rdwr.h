1791f539cd441c3f7926f2c449487af2b95466a1
Jesper Nilsson <jesper.nilsson@axis.com>
2008-01-29 10:43:05 +0100

    CRIS v32: Minor changes to avoid errors in asm-cris/arch-v32/hwregs/reg_rdwr.h
    - Add ifdef around macros to read and write hardware registers
    - Add parens around REG_READ expression to avoid possible precedence errors.
    - Remove useless CVS id tag.

--- include/asm-cris/arch-v32/hwregs/reg_rdwr.h
+++ include/asm-cris/arch-v32/hwregs/reg_rdwr.h
@@ -1,15 +1,17 @@
-/* $Id: reg_rdwr.h,v 1.6 2005/04/24 18:30:58 starvik Exp $
- *
+/*
  * Read/write register macros used by *_defs.h
  */
 
 #ifndef reg_rdwr_h
 #define reg_rdwr_h
 
+#ifndef REG_READ
+#define REG_READ(type, addr) (*((volatile type *) (addr)))
+#endif
 
-#define REG_READ(type, addr) *((volatile type *) (addr))
-
+#ifndef REG_WRITE
 #define REG_WRITE(type, addr, val) \
    do { *((volatile type *) (addr)) = (val); } while(0)
+#endif
 
 #endif
