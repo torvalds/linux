3578facf737dc9a60ed19a83458bd9cfdc2af233
Thomas Gleixner <tglx@linutronix.de>
2008-01-30 13:30:38 +0100

    x86: merge resume-trace.h variants
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/asm.h
+++ include/asm-x86/asm.h
@@ -6,12 +6,14 @@
 
 # define _ASM_PTR	" .long "
 # define _ASM_ALIGN	" .balign 4 "
+# define _ASM_MOV_UL	" movl "
 
 #else
 /* 64 bits */
 
 # define _ASM_PTR	" .quad "
 # define _ASM_ALIGN	" .balign 8 "
+# define _ASM_MOV_UL	" movq "
 
 #endif /* CONFIG_X86_32 */
 
--- include/asm-x86/resume-trace.h
+++ include/asm-x86/resume-trace.h
@@ -1,5 +1,20 @@
-#ifdef CONFIG_X86_32
-# include "resume-trace_32.h"
-#else
-# include "resume-trace_64.h"
+#ifndef _ASM_X86_RESUME_TRACE_H
+#define _ASM_X86_RESUME_TRACE_H
+
+#include <asm/asm.h>
+
+#define TRACE_RESUME(user) do {					\
+	if (pm_trace_enabled) {					\
+		void *tracedata;				\
+		asm volatile(_ASM_MOV_UL " $1f,%0\n"		\
+			".section .tracedata,\"a\"\n"		\
+			"1:\t.word %c1\n\t"			\
+			_ASM_PTR " %c2\n"			\
+			".previous"				\
+			:"=r" (tracedata)			\
+			: "i" (__LINE__), "i" (__FILE__));	\
+		generate_resume_trace(tracedata, user);		\
+	}							\
+} while (0)
+
 #endif
