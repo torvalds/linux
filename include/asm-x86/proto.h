84c873ed2a00eab3e8ac49dc7889d7aad142ce22
Thomas Gleixner <tglx@linutronix.de>
2008-01-30 13:30:19 +0100

    x86: remove obsolte declarations from proto.h
    Nuke duplicate and obsolete crap from this ugly dump bin.
    There are still some entries left which need to be sorted out,
    but I'm tired of that puzzle game right now.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/proto.h
+++ include/asm-x86/proto.h
@@ -5,31 +5,18 @@
 
 /* misc architecture specific prototypes */
 
-struct cpuinfo_x86; 
-struct pt_regs;
-
 extern void early_idt_handler(void);
 
 extern void init_memory_mapping(unsigned long start, unsigned long end);
 
-extern void system_call(void); 
-extern int kernel_syscall(void);
+extern void system_call(void);
 extern void syscall_init(void);
 
 extern void ia32_syscall(void);
-extern void ia32_cstar_target(void); 
-extern void ia32_sysenter_target(void); 
-
-extern void config_acpi_tables(void);
-extern void ia32_syscall(void);
+extern void ia32_cstar_target(void);
+extern void ia32_sysenter_target(void);
 
 extern void reserve_bootmem_generic(unsigned long phys, unsigned len);
-
-extern void load_gs_index(unsigned gs);
-
-extern void exception_table_check(void);
-
-extern void swap_low_mappings(void);
 
 extern void syscall32_cpu_init(void);
 
@@ -37,11 +24,7 @@ extern void check_efer(void);
 
 extern unsigned long table_start, table_end;
 
-extern int exception_trace;
-
 extern int reboot_force;
-
-extern int gsi_irq_sharing(int gsi);
 
 long do_arch_prctl(struct task_struct *task, int code, unsigned long addr);
 
