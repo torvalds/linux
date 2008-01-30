0aac03f07b37da96e00371e66973d5ffaae578a4
Andrea Arcangeli <andrea@qumranet.com>
2008-01-30 19:57:35 +0100

    KVM: Disable pagefaults during copy_from_user_inatomic()
    With CONFIG_PREEMPT=n, this is needed in order to disable the fault-in
    code from sleeping.
    
    Signed-off-by: Andrea Arcangeli <andrea@qumranet.com>
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -554,7 +554,9 @@ int kvm_read_guest_atomic(struct kvm *kvm, gpa_t gpa, void *data,
 	addr = gfn_to_hva(kvm, gfn);
 	if (kvm_is_error_hva(addr))
 		return -EFAULT;
+	pagefault_disable();
 	r = __copy_from_user_inatomic(data, (void __user *)addr + offset, len);
+	pagefault_enable();
 	if (r)
 		return -EFAULT;
 	return 0;
