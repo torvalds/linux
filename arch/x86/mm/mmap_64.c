954683a2c19696114894384e34f858bb9f455b11
Andrew Morton <akpm@linux-foundation.org>
2008-01-30 13:31:07 +0100

    x86: PIE executable randomization, uninlining
    Cc: "Luck, Tony" <tony.luck@intel.com>
    Cc: Arjan van de Ven <arjan@infradead.org>
    Cc: Jakub Jelinek <jakub@redhat.com>
    Cc: Jiri Kosina <jkosina@suse.cz>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: Roland McGrath <roland@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/mm/mmap_64.c
+++ arch/x86/mm/mmap_64.c
@@ -42,7 +42,7 @@
 #define MIN_GAP (128*1024*1024)
 #define MAX_GAP (TASK_SIZE/6*5)
 
-static inline unsigned long mmap_base(void)
+static unsigned long mmap_base(void)
 {
 	unsigned long gap = current->signal->rlim[RLIMIT_STACK].rlim_cur;
 
@@ -54,7 +54,7 @@ static inline unsigned long mmap_base(void)
 	return TASK_SIZE - (gap & PAGE_MASK);
 }
 
-static inline int mmap_is_32(void)
+static int mmap_is_32(void)
 {
 #ifdef CONFIG_IA32_EMULATION
 	if (test_thread_flag(TIF_IA32))
@@ -63,7 +63,7 @@ static inline int mmap_is_32(void)
 	return 0;
 }
 
-static inline int mmap_is_legacy(void)
+static int mmap_is_legacy(void)
 {
 	if (current->personality & ADDR_COMPAT_LAYOUT)
 		return 1;
