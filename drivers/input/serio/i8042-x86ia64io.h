3f79b1e94002791a42837a46b5817e87a0ac0873
Jiri Kosina <jkosina@suse.cz>
2008-01-30 16:34:52 -0500

    Input: i8042 - add Fujitsu-Siemens Amilo Pro 2010 to nomux list
    Reported-by: Hans Aschauer <Hans.Aschauer@web.de>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/serio/i8042-x86ia64io.h
+++ drivers/input/serio/i8042-x86ia64io.h
@@ -186,6 +186,13 @@ static struct dmi_system_id __initdata i8042_dmi_nomux_table[] = {
 		},
 	},
 	{
+		.ident = "Fujitsu-Siemens Amilo Pro 2010",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "FUJITSU SIEMENS"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "AMILO Pro V2010"),
+		},
+	},
+	{
 		/*
 		 * No data is coming from the touchscreen unless KBC
 		 * is in legacy mode.
