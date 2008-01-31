e903fbd41e9a723da194e91816bfb69d442eb116
Reinette Chatre <reinette.chatre@intel.com>
2008-01-30 22:05:15 -0800

    iwlwifi: fix merge sequence: exit on error before state change
    The intention behind the original patch:
    "iwlwifi: fix possible read attempt on ucode that is not available"
    was to exit before any state is changed. Due to its submission directly
    to 2.6.24 it was not clear how this relates to the latest iwlwifi work.
    
    This patch does exactly the same as the previous patch, just earlier to
    prevent any state from being changed if there is an error.
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl4965-base.c
+++ drivers/net/wireless/iwlwifi/iwl4965-base.c
@@ -6755,6 +6755,11 @@ static int __iwl4965_up(struct iwl4965_priv *priv)
 		return -ENODEV;
 	}
 
+	if (!priv->ucode_data_backup.v_addr || !priv->ucode_data.v_addr) {
+		IWL_ERROR("ucode not available for device bringup\n");
+		return -EIO;
+	}
+
 	/* If platform's RF_KILL switch is NOT set to KILL */
 	if (iwl4965_read32(priv, CSR_GP_CNTRL) &
 				CSR_GP_CNTRL_REG_FLAG_HW_RF_KILL_SW)
@@ -6765,11 +6770,6 @@ static int __iwl4965_up(struct iwl4965_priv *priv)
 			IWL_WARNING("Radio disabled by HW RF Kill switch\n");
 			return -ENODEV;
 		}
-	}
-
-	if (!priv->ucode_data_backup.v_addr || !priv->ucode_data.v_addr) {
-		IWL_ERROR("ucode not available for device bringup\n");
-		return -EIO;
 	}
 
 	iwl4965_write32(priv, CSR_INT, 0xFFFFFFFF);
