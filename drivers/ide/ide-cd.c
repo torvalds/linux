972560fb9d9ea7bc7082c0c79c99c24e3f56606c
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2008-02-01 23:09:23 +0100

    ide-cd: move VERBOSE_IDE_CD_ERRORS code to ide-cd_verbose.c
    * Rename ide-cd kernel module to ide-cd_mod in preparation to moving code out
      from ide-cd.[c,h].   Add MODULE_ALIAS("ide-cd") to preserve compatibility.
    
    * Move VERBOSE_IDE_CD_ERRORS code from ide-cd.[c,h] to ide-cd_verbose.c.
    
      ide-cd_verbose.c is IDE subsystem independent and may be easily converted
      into generic library usable by other drivers (i.e. libata) if needed.
    
    * Add CONFIG_BLK_DEV_IDECD_VERBOSE_ERRORS config option to drivers/ide/Kconfig
      replacing VERBOSE_IDE_CD_ERRORS define.  Make this config option enabled by
      default and visible only if CONFIG_EMBEDDED is defined.
    
    before the patch:
       text    data     bss     dec     hex filename
      22841     360    1056   24257    5ec1 drivers/ide/ide-cd.o
    
    after the patch w/ CONFIG_BLK_DEV_IDECD_VERBOSE_ERRORS=y:
       text    data     bss     dec     hex filename
      22857     360    1056   24273    5ed1 drivers/ide/ide-cd_mod.o
    
    after the patch w/ CONFIG_BLK_DEV_IDECD_VERBOSE_ERRORS=n:
       text    data     bss     dec     hex filename
      15091     360    1056   16507    407b drivers/ide/ide-cd_mod.o
    
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/ide-cd.c
+++ drivers/ide/ide-cd.c
@@ -199,129 +199,8 @@ void cdrom_analyze_sense_data(ide_drive_t *drive,
 			}
  		}
  	}
-#if VERBOSE_IDE_CD_ERRORS
-	{
-		int i;
-		const char *s = "bad sense key!";
-		char buf[80];
 
-		printk(KERN_ERR "ATAPI device %s:\n", drive->name);
-		if (sense->error_code == 0x70)
-			printk(KERN_CONT "  Error: ");
-		else if (sense->error_code == 0x71)
-			printk("  Deferred Error: ");
-		else if (sense->error_code == 0x7f)
-			printk(KERN_CONT "  Vendor-specific Error: ");
-		else
-			printk(KERN_CONT "  Unknown Error Type: ");
-
-		if (sense->sense_key < ARRAY_SIZE(sense_key_texts))
-			s = sense_key_texts[sense->sense_key];
-
-		printk(KERN_CONT "%s -- (Sense key=0x%02x)\n",
-				 s, sense->sense_key);
-
-		if (sense->asc == 0x40) {
-			sprintf(buf, "Diagnostic failure on component 0x%02x",
-				sense->ascq);
-			s = buf;
-		} else {
-			int lo = 0, mid, hi = ARRAY_SIZE(sense_data_texts);
-			unsigned long key = (sense->sense_key << 16);
-
-			key |= (sense->asc << 8);
-			if (!(sense->ascq >= 0x80 && sense->ascq <= 0xdd))
-				key |= sense->ascq;
-			s = NULL;
-
-			while (hi > lo) {
-				mid = (lo + hi) / 2;
-				if (sense_data_texts[mid].asc_ascq == key ||
-				    sense_data_texts[mid].asc_ascq == (0xff0000|key)) {
-					s = sense_data_texts[mid].text;
-					break;
-				} else if (sense_data_texts[mid].asc_ascq > key)
-					hi = mid;
-				else
-					lo = mid + 1;
-			}
-		}
-
-		if (s == NULL) {
-			if (sense->asc > 0x80)
-				s = "(vendor-specific error)";
-			else
-				s = "(reserved error code)";
-		}
-
-		printk(KERN_ERR "  %s -- (asc=0x%02x, ascq=0x%02x)\n",
-				s, sense->asc, sense->ascq);
-
-		if (failed_command != NULL) {
-			int lo = 0, mid, hi = ARRAY_SIZE(packet_command_texts);
-			s = NULL;
-
-			while (hi > lo) {
-				mid = (lo + hi) / 2;
-				if (packet_command_texts[mid].packet_command ==
-				    failed_command->cmd[0]) {
-					s = packet_command_texts[mid].text;
-					break;
-				}
-				if (packet_command_texts[mid].packet_command >
-				    failed_command->cmd[0])
-					hi = mid;
-				else
-					lo = mid + 1;
-			}
-
-			printk(KERN_ERR "  The failed \"%s\" packet command "
-					"was: \n  \"", s);
-			for (i = 0; i < sizeof(failed_command->cmd); i++)
-				printk(KERN_CONT "%02x ",
-						 failed_command->cmd[i]);
-			printk(KERN_CONT "\"\n");
-		}
-
-		/* The SKSV bit specifies validity of the sense_key_specific
-		 * in the next two commands. It is bit 7 of the first byte.
-		 * In the case of NOT_READY, if SKSV is set the drive can
-		 * give us nice ETA readings.
-		 */
-		if (sense->sense_key == NOT_READY && (sense->sks[0] & 0x80)) {
-			int progress = (sense->sks[1] << 8 | sense->sks[2]) * 100;
-
-			printk(KERN_ERR "  Command is %02d%% complete\n",
-					progress / 0xffff);
-		}
-
-		if (sense->sense_key == ILLEGAL_REQUEST &&
-		    (sense->sks[0] & 0x80) != 0) {
-			printk(KERN_ERR "  Error in %s byte %d",
-					(sense->sks[0] & 0x40) != 0 ?
-					"command packet" : "command data",
-					(sense->sks[1] << 8) + sense->sks[2]);
-
-			if ((sense->sks[0] & 0x40) != 0)
-				printk(KERN_CONT " bit %d",
-						 sense->sks[0] & 0x07);
-
-			printk(KERN_CONT "\n");
-		}
-	}
-#else /* not VERBOSE_IDE_CD_ERRORS */
-	/* Suppress printing unit attention and `in progress of becoming ready'
-	   errors when we're not being verbose. */
-	if (sense->sense_key == UNIT_ATTENTION ||
-	    (sense->sense_key == NOT_READY && (sense->asc == 4 ||
-						sense->asc == 0x3a)))
-		return;
-
-	printk(KERN_ERR "%s: error code: 0x%02x  sense_key: 0x%02x  "
-			"asc: 0x%02x  ascq: 0x%02x\n",
-			drive->name, sense->error_code, sense->sense_key,
-			sense->asc, sense->ascq);
-#endif /* not VERBOSE_IDE_CD_ERRORS */
+	ide_cd_log_error(drive->name, failed_command, sense);
 }
 
 /*
@@ -3189,6 +3068,7 @@ static int __init ide_cdrom_init(void)
 }
 
 MODULE_ALIAS("ide:*m-cdrom*");
+MODULE_ALIAS("ide-cd");
 module_init(ide_cdrom_init);
 module_exit(ide_cdrom_exit);
 MODULE_LICENSE("GPL");
