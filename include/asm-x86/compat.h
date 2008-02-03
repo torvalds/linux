8b3de0df4eefc47ca2aa459b72dd8b2711f3640c
Marcin Ślusarz <marcin.slusarz@gmail.com>
2008-02-03 16:32:51 +0200

    asm-*/compat.h: fix typo in comment
    Signed-off-by: Marcin Ślusarz <marcin.slusarz@gmail.com>
    Signed-off-by: Adrian Bunk <bunk@kernel.org>

--- include/asm-x86/compat.h
+++ include/asm-x86/compat.h
@@ -190,7 +190,7 @@ typedef struct user_regs_struct32 compat_elf_gregset_t;
  * A pointer passed in from user mode. This should not
  * be used for syscall parameters, just declare them
  * as pointers because the syscall entry code will have
- * appropriately comverted them already.
+ * appropriately converted them already.
  */
 typedef	u32		compat_uptr_t;
 
