9e60fdcf0c2905d7a8fc4cb2b3711ea5c5acaae1
eric miao <eric.miao@marvell.com>
2008-02-04 22:28:26 -0800

    gpiolib: pca9539 i2c gpio expander support
    This adds a new-style I2C driver with basic support for the sixteen bit
    PCA9539 GPIO expanders.  These chips have multiple registers, push-pull output
    drivers, and (not supported in this patch) pin change interrupts.
    
    Board-specific code must provide "pca9539_platform_data" with each chip's
    "i2c_board_info".  That provides the GPIO numbers to be used by that chip, and
    callbacks for board-specific setup/teardown logic.
    
    Derived from drivers/i2c/chips/pca9539.c (which has no current known users).
    This is faster and simpler; it uses 16-bit register access, and cache the
    OUTPUT and DIRECTION registers for fast access
    
    Signed-off-by: eric miao <eric.miao@marvell.com>
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Acked-by: Jean Delvare <khali@linux-fr.org>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Cc: Haavard Skinnemoen <hskinnemoen@atmel.com>
    Cc: Philipp Zabel <philipp.zabel@gmail.com>
    Cc: Russell King <rmk@arm.linux.org.uk>
    Cc: Ben Gardner <bgardner@wabtec.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

---
+++ include/linux/i2c/pca9539.h
@@ -0,0 +1,18 @@
+/* platform data for the PCA9539 16-bit I/O expander driver */
+
+struct pca9539_platform_data {
+	/* number of the first GPIO */
+	unsigned	gpio_base;
+
+	/* initial polarity inversion setting */
+	uint16_t	invert;
+
+	void		*context;	/* param to setup/teardown */
+
+	int		(*setup)(struct i2c_client *client,
+				unsigned gpio, unsigned ngpio,
+				void *context);
+	int		(*teardown)(struct i2c_client *client,
+				unsigned gpio, unsigned ngpio,
+				void *context);
+};
