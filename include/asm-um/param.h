7281ff952c7b3cbefb14847460e5fe73a2d240e4
Jeff Dike <jdike@addtoit.com>
2008-02-04 22:31:19 -0800

    uml: add back CONFIG_HZ
    avoid-overflows-in-kernel-timec.patch makes CONFIG_HZ necessary for a
    successful build.  UML lacks a definition, so this patch adds one.  It also
    changes the hard-wired definition of HZ to CONFIG_HZ.
    
    Note: this patch is a good idea even in the absence of hpa's time fixes.
    
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Signed-off-by: Jeff Dike <jdike@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-um/param.h
+++ include/asm-um/param.h
@@ -10,7 +10,7 @@
 #define MAXHOSTNAMELEN  64      /* max length of hostname */
 
 #ifdef __KERNEL__
-#define HZ 100
+#define HZ CONFIG_HZ
 #define USER_HZ	100	   /* .. some user interfaces are in "ticks" */
 #define CLOCKS_PER_SEC (USER_HZ)  /* frequency at which times() counts */
 #endif
