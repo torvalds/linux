bdff746a3915f109bd13730b6847e33e17e91ed3
Andrew Morton <akpm@linux-foundation.org>
2008-02-04 22:27:22 -0800

    clone: prepare to recycle CLONE_STOPPED
    Ulrich says that we never used this clone flags and that nothing should be
    using it.
    
    As we're down to only a single bit left in clone's flags argument, let's add a
    warning to check that no userspace is actually using it.  Hopefully we will
    be able to recycle it.
    
    Roland said:
    
      CLONE_STOPPED was previously used by some NTPL versions when under
      thread_db (i.e.  only when being actively debugged by gdb), but not for a
      long time now, and it never worked reliably when it was used.  Removing it
      seems fine to me.
    
    [akpm@linux-foundation.org: it looks like CLONE_DETACHED is being used]
    Cc: Ulrich Drepper <drepper@redhat.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Roland McGrath <roland@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1450,6 +1450,23 @@ long do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
+	/*
+	 * We hope to recycle these flags after 2.6.26
+	 */
+	if (unlikely(clone_flags & CLONE_STOPPED)) {
+		static int __read_mostly count = 100;
+
+		if (count > 0 && printk_ratelimit()) {
+			char comm[TASK_COMM_LEN];
+
+			count--;
+			printk(KERN_INFO "fork(): process `%s' used deprecated "
+					"clone flags 0x%lx\n",
+				get_task_comm(comm, current),
+				clone_flags & CLONE_STOPPED);
+		}
+	}
+
 	if (unlikely(current->ptrace)) {
 		trace = fork_traceflag (clone_flags);
 		if (trace)
