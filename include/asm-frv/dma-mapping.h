82b12e232d2a71f566dca48df4ae4548e6d8d53d
Robert P. J. Day <rpjday@crashcourse.ca>
2008-02-04 22:29:54 -0800

    FRV: move DMA macros to scatterlist.h for consistency.
    To be consistent with other architectures, these two DMA macros should
    be defined in scatterlist.h as opposed to dma-mapping.h
    
    Signed-off-by: Robert P. J. Day <rpjday@crashcourse.ca>
    Acked-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-frv/dma-mapping.h
+++ include/asm-frv/dma-mapping.h
@@ -17,16 +17,6 @@ void *dma_alloc_coherent(struct device *dev, size_t size, dma_addr_t *dma_handle
 void dma_free_coherent(struct device *dev, size_t size, void *vaddr, dma_addr_t dma_handle);
 
 /*
- * These macros should be used after a pci_map_sg call has been done
- * to get bus addresses of each of the SG entries and their lengths.
- * You should only work with the number of sg entries pci_map_sg
- * returns, or alternatively stop on the first sg_dma_len(sg) which
- * is 0.
- */
-#define sg_dma_address(sg)	((sg)->dma_address)
-#define sg_dma_len(sg)		((sg)->length)
-
-/*
  * Map a single buffer of the indicated size for DMA in streaming mode.
  * The 32-bit bus address to use is returned.
  *
