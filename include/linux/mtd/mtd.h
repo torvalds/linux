388bbb09b991c792310af2f6b49f6c55edb3dff0
Richard Purdie <rpurdie@rpsys.net>
2008-02-06 10:17:15 +0000

    [MTD] Add mtd panic_write function pointer
    MTDs are well suited for logging critical data and the mtdoops driver
    allows kernel panics/oops to be written to flash in a blackbox flight
    recorder fashion allowing better debugging and analysis of crashes.
    
    Any kernel oops in user context can be easily handled since the kernel
    continues as normal and any queued mtd writes are scheduled. Any kernel
    oops in interrupt context results in a panic and the delayed writes will
    not be scheduled however. The existing mtd->write function cannot be
    called in interrupt context so these messages can never be written to
    flash.
    
    This patch adds a panic_write function pointer that drivers can
    optionally implement which can be called in interrupt context. It is
    only intended to be called when its known the kernel is about to panic
    and we need to write to succeed. Since the kernel is not going to be
    running for much longer, this function can break locks and delay to
    ensure the write succeeds (but not sleep).
    
    Signed-off-by: Richard Purdie <rpurdie@rpsys.net>
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- include/linux/mtd/mtd.h
+++ include/linux/mtd/mtd.h
@@ -152,6 +152,15 @@ struct mtd_info {
 	int (*read) (struct mtd_info *mtd, loff_t from, size_t len, size_t *retlen, u_char *buf);
 	int (*write) (struct mtd_info *mtd, loff_t to, size_t len, size_t *retlen, const u_char *buf);
 
+	/* In blackbox flight recorder like scenarios we want to make successful
+	   writes in interrupt context. panic_write() is only intended to be
+	   called when its known the kernel is about to panic and we need the
+	   write to succeed. Since the kernel is not going to be running for much
+	   longer, this function can break locks and delay to ensure the write
+	   succeeds (but not sleep). */
+
+	int (*panic_write) (struct mtd_info *mtd, loff_t to, size_t len, size_t *retlen, const u_char *buf);
+
 	int (*read_oob) (struct mtd_info *mtd, loff_t from,
 			 struct mtd_oob_ops *ops);
 	int (*write_oob) (struct mtd_info *mtd, loff_t to,
