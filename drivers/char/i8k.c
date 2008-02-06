a9000d037d7dd08ac46168560b3a3d3bb743bfa6
Nick Warne <nick@ukfsn.org>
2008-02-06 01:37:47 -0800

    ik8: add Dell UK 6400 Inspiron model (MM061)
    Add the Dell UK 6400 Inspiron model (MM061) to allow the i8k module to load
    correctly without using 'force=1'
    
    Signed-off-by: "Nick Warne" <nick@ukfsn.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/char/i8k.c
+++ drivers/char/i8k.c
@@ -439,6 +439,13 @@ static struct dmi_system_id __initdata i8k_dmi_table[] = {
 			DMI_MATCH(DMI_PRODUCT_NAME, "Latitude"),
 		},
 	},
+	{	/* UK Inspiron 6400  */
+		.ident = "Dell Inspiron 3",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Dell Inc."),
+			DMI_MATCH(DMI_PRODUCT_NAME, "MM061"),
+		},
+	},
 	{ }
 };
 
