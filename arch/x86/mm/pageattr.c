2d684cd6d9cf0c6a0e28978362671b6e2d8fb56c
Ingo Molnar <mingo@elte.hu>
2008-02-06 22:39:44 +0100

    x86: remove X2 workaround
    With the spurious handler fix, the X2 does not lock up anymore.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/mm/pageattr.c
+++ arch/x86/mm/pageattr.c
@@ -260,17 +260,6 @@ try_preserve_large_page(pte_t *kpte, unsigned long address,
 	pgprot_t old_prot, new_prot;
 	int level, do_split = 1;
 
-	/*
-	 * An Athlon 64 X2 showed hard hangs if we tried to preserve
-	 * largepages and changed the PSE entry from RW to RO.
-	 *
-	 * As AMD CPUs have a long series of erratas in this area,
-	 * (and none of the known ones seem to explain this hang),
-	 * disable this code until the hang can be debugged:
-	 */
-	if (boot_cpu_data.x86_vendor == X86_VENDOR_AMD)
-		return 1;
-
 	spin_lock_irqsave(&pgd_lock, flags);
 	/*
 	 * Check for races, another CPU might have split this page
