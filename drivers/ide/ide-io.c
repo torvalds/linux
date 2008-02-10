3b0e044d5a881c937293a045158149514b86783c
Kiyoshi Ueda <k-ueda@ct.jp.nec.com>
2008-02-11 00:32:11 +0100

    ide: another possible ide panic fix for blk-end-request
    I have reviewed all blk-end-request patches again to confirm whether
    there are any similar problems with the last week's ide-cd panic:
        http://lkml.org/lkml/2008/1/29/140
    
    And I found a possible similar bug in ide-io change:
    ide_end_drive_cmd() could be called for blk_pc_request() which could
    have bios.  To complete such requests correctly, we need to pass
    the actual size of the request.
    Otherwise, __blk_end_request() returns 1 because the request still has
    bios, and the system will BUG() unnecessarily.
    
    The following patch fixes the bug and should be applied on top of
    Linus' git.
    
    Signed-off-by: Kiyoshi Ueda <k-ueda@ct.jp.nec.com>
    Signed-off-by: Jun'ichi Nomura <j-nomura@ce.jp.nec.com>
    Cc: Borislav Petkov <petkovbb@googlemail.com>
    Cc: Jens Axboe <jens.axboe@oracle.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/ide-io.c
+++ drivers/ide/ide-io.c
@@ -388,7 +388,8 @@ void ide_end_drive_cmd (ide_drive_t *drive, u8 stat, u8 err)
 	spin_lock_irqsave(&ide_lock, flags);
 	HWGROUP(drive)->rq = NULL;
 	rq->errors = err;
-	if (__blk_end_request(rq, (rq->errors ? -EIO : 0), 0))
+	if (unlikely(__blk_end_request(rq, (rq->errors ? -EIO : 0),
+				       blk_rq_bytes(rq))))
 		BUG();
 	spin_unlock_irqrestore(&ide_lock, flags);
 }
