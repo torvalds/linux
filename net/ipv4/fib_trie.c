ec28cf738d899e9d0652108e1986101771aacb2e
Stephen Hemminger <shemminger@vyatta.com>
2008-02-11 21:12:49 -0800

    fib_trie: handle empty tree
    This fixes possible problems when trie_firstleaf() returns NULL
    to trie_leafindex().
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/fib_trie.c
+++ net/ipv4/fib_trie.c
@@ -1762,11 +1762,9 @@ static struct leaf *trie_leafindex(struct trie *t, int index)
 {
 	struct leaf *l = trie_firstleaf(t);
 
-	while (index-- > 0) {
+	while (l && index-- > 0)
 		l = trie_nextleaf(l);
-		if (!l)
-			break;
-	}
+
 	return l;
 }
 
