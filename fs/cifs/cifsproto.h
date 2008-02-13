c1ce264470f000ccd5965d3718f7d905d559fd64
Steve French <sfrench@us.ibm.com>
2008-02-13 02:59:36 +0000

    [CIFS] fix typo
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/cifsproto.h
+++ fs/cifs/cifsproto.h
@@ -102,7 +102,7 @@ extern int mode_to_acl(struct inode *inode, const char *path, __u64);
 extern int cifs_mount(struct super_block *, struct cifs_sb_info *, char *,
 			const char *);
 extern int cifs_umount(struct super_block *, struct cifs_sb_info *);
-#ifdef CONFIG_CIFS_DFS
+#ifdef CONFIG_CIFS_DFS_UPCALL
 extern void dfs_shrink_umount_helper(struct vfsmount *vfsmnt);
 #else
 static inline void dfs_shrink_umount_helper(struct vfsmount *vfsmnt)
