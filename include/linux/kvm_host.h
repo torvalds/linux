ef2979bd98dac86ea6a4cd9bdd6820a466108017
Avi Kivity <avi@qumranet.com>
2008-02-20 12:04:47 +0200

    KVM: Increase the number of user memory slots per vm
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -25,7 +25,7 @@
 #include <asm/kvm_host.h>
 
 #define KVM_MAX_VCPUS 16
-#define KVM_MEMORY_SLOTS 8
+#define KVM_MEMORY_SLOTS 32
 /* memory slots that does not exposed to userspace */
 #define KVM_PRIVATE_MEM_SLOTS 4
 
