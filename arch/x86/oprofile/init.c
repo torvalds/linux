d717ca84c02f4b60a85502c23bca5698801dfaae
Paolo Ciarrocchi <paolo.ciarrocchi@gmail.com>
2008-02-22 23:09:50 +0100

    x86: coding style fixes to arch/x86/oprofile/init.c
    Before:
       total: 5 errors, 4 warnings, 48 lines checked
    After:
       total: 0 errors, 4 warnings, 49 lines checked
    
    No code changed:
    
    arch/x86/oprofile/init.o:
    
       text	   data	    bss	    dec	    hex	filename
         42	      0	      0	     42	     2a	init.o.before
         42	      0	      0	     42	     2a	init.o.after
    
    md5:
       74c94c315cfbf245aeba36eceac57e66  init.o.before.asm
       74c94c315cfbf245aeba36eceac57e66  init.o.after.asm
    
    Signed-off-by: Paolo Ciarrocchi <paolo.ciarrocchi@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/oprofile/init.c
+++ arch/x86/oprofile/init.c
@@ -10,18 +10,19 @@
 #include <linux/oprofile.h>
 #include <linux/init.h>
 #include <linux/errno.h>
- 
-/* We support CPUs that have performance counters like the Pentium Pro
+
+/*
+ * We support CPUs that have performance counters like the Pentium Pro
  * with the NMI mode driver.
  */
- 
-extern int op_nmi_init(struct oprofile_operations * ops);
-extern int op_nmi_timer_init(struct oprofile_operations * ops);
+
+extern int op_nmi_init(struct oprofile_operations *ops);
+extern int op_nmi_timer_init(struct oprofile_operations *ops);
 extern void op_nmi_exit(void);
 extern void x86_backtrace(struct pt_regs * const regs, unsigned int depth);
 
 
-int __init oprofile_arch_init(struct oprofile_operations * ops)
+int __init oprofile_arch_init(struct oprofile_operations *ops)
 {
 	int ret;
 
