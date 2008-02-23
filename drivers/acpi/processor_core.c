d0ce46f550ebbd765881e8c48f43b66285d798b0
Zhang Rui <rui.zhang@intel.com>
2008-02-23 01:53:09 -0500

    ACPI Exception (): AE_NOT_FOUND, Processor Device is not present (update)
    update cfaf3747ff3d431fba33f75083b7f50f58ae22ff
    ACPI: ACPI Exception (): AE_NOT_FOUND, Processor Device is not present
    
    is_processor_present is only called in the processor hotplug case,
    and _STA method is mandatory at this time.
    
    We should ignore those processors that are disabled in the MADT
    and don't have _STA methods.
    Because they will never exist in this system.
    For the processors that don't physically exist but can be
    hot plugged later, we still need this debug info.
    
    http://bugzilla.kernel.org/show_bug.cgi?id=8570
    
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/processor_core.c
+++ drivers/acpi/processor_core.c
@@ -840,17 +840,19 @@ static int is_processor_present(acpi_handle handle)
 
 
 	status = acpi_evaluate_integer(handle, "_STA", NULL, &sta);
-	/*
-	 * if a processor object does not have an _STA object,
-	 * OSPM assumes that the processor is present.
-	 */
-	if (status == AE_NOT_FOUND)
-		return 1;
 
 	if (ACPI_SUCCESS(status) && (sta & ACPI_STA_DEVICE_PRESENT))
 		return 1;
 
-	ACPI_EXCEPTION((AE_INFO, status, "Processor Device is not present"));
+	/*
+	 * _STA is mandatory for a processor that supports hot plug
+	 */
+	if (status == AE_NOT_FOUND)
+		ACPI_DEBUG_PRINT((ACPI_DB_INFO,
+				"Processor does not support hot plug\n"));
+	else
+		ACPI_EXCEPTION((AE_INFO, status,
+				"Processor Device is not present"));
 	return 0;
 }
 
