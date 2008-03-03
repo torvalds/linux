c27cfeffad436816ecd500b8dc94acf348182b13
Glauber Costa <gcosta@redhat.com>
2008-03-03 14:12:29 -0300

    x86: commonize smp.h
    this is the first step of integrating smp.h between x86_64
    and i386
    
    Signed-off-by: Glauber Costa <gcosta@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/smp.h
+++ include/asm-x86/smp.h
@@ -1,5 +1,12 @@
+#ifndef _ASM_X86_SMP_H_
+#define _ASM_X86_SMP_H_
+#ifndef __ASSEMBLY__
+
 #ifdef CONFIG_X86_32
 # include "smp_32.h"
 #else
 # include "smp_64.h"
+#endif
+
+#endif /* __ASSEMBLY__ */
 #endif
