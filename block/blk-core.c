9d7f1e6b9b2c2e4fe029ff35f4ca1e2879864208
Adrian Bunk <bunk@kernel.org>
2008-03-04 11:23:47 +0100

    unexport blk_{get,put}_queue
    This patch removes the unused exports of blk_{get,put}_queue.
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -424,7 +424,6 @@ void blk_put_queue(struct request_queue *q)
 {
 	kobject_put(&q->kobj);
 }
-EXPORT_SYMBOL(blk_put_queue);
 
 void blk_cleanup_queue(struct request_queue *q)
 {
@@ -592,7 +591,6 @@ int blk_get_queue(struct request_queue *q)
 
 	return 1;
 }
-EXPORT_SYMBOL(blk_get_queue);
 
 static inline void blk_free_request(struct request_queue *q, struct request *rq)
 {
