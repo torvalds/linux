ad0caae0ded1af2a0a41f93356587e1c24d76725
Andrew Morton <akpm@linux-foundation.org>
2008-03-04 15:23:47 -0800

    sh: export copy-page() to modules
    ERROR: "copy_page" [fs/unionfs/unionfs.ko] undefined!
    
    like all the other architectures.
    
    Cc: Erez Zadok <ezk@cs.sunysb.edu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/sh_ksyms_32.c
+++ arch/sh/kernel/sh_ksyms_32.c
@@ -146,5 +146,6 @@ EXPORT_SYMBOL(csum_partial_copy_generic);
 EXPORT_SYMBOL(csum_ipv6_magic);
 #endif
 EXPORT_SYMBOL(clear_page);
+EXPORT_SYMBOL(copy_page);
 EXPORT_SYMBOL(__clear_user);
 EXPORT_SYMBOL(_ebss);
