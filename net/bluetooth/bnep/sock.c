a4e2acf01a7e5fcd960fc332335ca10313641f4b
Tobias Klauser <tklauser@distanz.ch>
2008-03-05 18:47:40 -0800

    bluetooth: make bnep_sock_cleanup() return void
    bnep_sock_cleanup() always returns 0 and its return value isn't used
    anywhere in the code.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bluetooth/bnep/sock.c
+++ net/bluetooth/bnep/sock.c
@@ -257,12 +257,10 @@ error:
 	return err;
 }
 
-int __exit bnep_sock_cleanup(void)
+void __exit bnep_sock_cleanup(void)
 {
 	if (bt_sock_unregister(BTPROTO_BNEP) < 0)
 		BT_ERR("Can't unregister BNEP socket");
 
 	proto_unregister(&bnep_proto);
-
-	return 0;
 }
