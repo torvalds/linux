842078054da2d754c6b998b116d7c468abbfaaca
Arnaud Patard <arnaud.patard@rtp-net.org>
2008-03-10 11:43:48 -0700

    gpio/pca953x bugfix: mark as can_sleep
    The pca953x driver is an I2C driver so gpio_chip->can_sleep should be set.
    This lets upper layers know they should use the gpio_*_cansleep() calls to
    access values, and may not access them from nonsleeping contexts.
    
    Signed-off-by: Arnaud Patard <arnaud.patard@rtp-net.org>
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Acked-by: "eric miao" <eric.y.miao@gmail.com>
    Cc: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/gpio/pca953x.c
+++ drivers/gpio/pca953x.c
@@ -184,6 +184,7 @@ static void pca953x_setup_gpio(struct pca953x_chip *chip, int gpios)
 	gc->direction_output = pca953x_gpio_direction_output;
 	gc->get = pca953x_gpio_get_value;
 	gc->set = pca953x_gpio_set_value;
+	gc->can_sleep = 1;
 
 	gc->base = chip->gpio_start;
 	gc->ngpio = gpios;
