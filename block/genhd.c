ee27a558ae0ff5063ccd6c47ca102c0bb0e3ba27
Roland McGrath <roland@redhat.com>
2008-03-11 17:13:15 -0700

    genhd must_check warning fix
    Fixes:
    
    	block/genhd.c:361: warning: ignoring return value of ‘class_register’, declared with attribute warn_unused_result
    
    Signed-off-by: Roland McGrath <roland@redhat.com>
    Acked-by: Jeff Garzik <jeff@garzik.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- block/genhd.c
+++ block/genhd.c
@@ -360,7 +360,9 @@ static struct kobject *base_probe(dev_t devt, int *part, void *data)
 
 static int __init genhd_device_init(void)
 {
-	class_register(&block_class);
+	int error = class_register(&block_class);
+	if (unlikely(error))
+		return error;
 	bdev_map = kobj_map_init(base_probe, &block_class_lock);
 	blk_dev_init();
 
