a5ae1c372dc5bbaee905bcede524d7180d22b362
Cyrill Gorcunov <gorcunov@gmail.com>
2008-03-13 19:44:56 +0300

    x86: processor.h - use PAGE_SIZE instead of numeric value
    This patch replaces numeric constant with an appropriate macro
    
    Also 0x800000000000UL is changed to bit shifting which is complement
    to the code comment (thanks hpa for notice)
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/processor.h
+++ include/asm-x86/processor.h
@@ -870,7 +870,7 @@ extern unsigned long thread_saved_pc(struct task_struct *tsk);
 /*
  * User space process size. 47bits minus one guard page.
  */
-#define TASK_SIZE64		(0x800000000000UL - 4096)
+#define TASK_SIZE64	((1UL << 47) - PAGE_SIZE)
 
 /* This decides where the kernel will search for a free chunk of vm
  * space during mmap's.
