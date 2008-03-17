0794c66d49885a2f615618ce4940434b5b067d84
Stephen Smalley <sds@tycho.nsa.gov>
2008-03-17 08:55:18 -0400

    selinux: handle files opened with flags 3 by checking ioctl permission
    Handle files opened with flags 3 by checking ioctl permission.
    
    Default to returning FILE__IOCTL from file_to_av() if the f_mode has neither
    FMODE_READ nor FMODE_WRITE, and thus check ioctl permission on exec or
    transfer, thereby validating such descriptors early as with normal r/w
    descriptors and catching leaks of them prior to attempted usage.
    
    Signed-off-by:  Stephen Smalley <sds@tycho.nsa.gov>
    Acked-by: Eric Paris <eparis@redhat.com>
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -1630,6 +1630,12 @@ static inline u32 file_to_av(struct file *file)
 		else
 			av |= FILE__WRITE;
 	}
+	if (!av) {
+		/*
+		 * Special file opened with flags 3 for ioctl-only use.
+		 */
+		av = FILE__IOCTL;
+	}
 
 	return av;
 }
