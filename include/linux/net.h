0098b7273e968fb9989a6e1e4e4c024cd081fe0d
Rusty Russell <rusty@rustcorp.com.au>
2008-03-22 17:18:47 -0700

    [NET]: NPROTO is redundant; it's equal to AF_MAX/PF_MAX.
    DaveM pointed out NPROTO exposed to userspace, so keep it around,
    just make sure it stays in sync.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/net.h
+++ include/linux/net.h
@@ -19,6 +19,7 @@
 #define _LINUX_NET_H
 
 #include <linux/wait.h>
+#include <linux/socket.h>
 #include <asm/socket.h>
 
 struct poll_table_struct;
@@ -26,7 +27,7 @@ struct pipe_inode_info;
 struct inode;
 struct net;
 
-#define NPROTO		34		/* should be enough for now..	*/
+#define NPROTO		AF_MAX
 
 #define SYS_SOCKET	1		/* sys_socket(2)		*/
 #define SYS_BIND	2		/* sys_bind(2)			*/
