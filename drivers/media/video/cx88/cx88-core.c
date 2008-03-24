87a1738919ef028d16c462437e1480fc67338cd3
Frej Drejhammar <frej.drejhammar@gmail.com>
2008-03-23 22:43:22 -0300

    V4L/DVB (7452): cx88: Enable chroma AGC by default for all non-SECAM modes
    An enabled chroma AGC will not degrade picture quality if enabled on a
    color PAL or NTSC signal with nominal signal levels. It will give a
    significant color reproduction improvement if the chroma signals
    diverge from nominal levels. Therefore enable chroma AGC by default
    for PAL and NTSC standards.
    
    Signed-off-by: "Frej Drejhammar <frej.drejhammar@gmail.com>"
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/cx88/cx88-core.c
+++ drivers/media/video/cx88/cx88-core.c
@@ -929,11 +929,10 @@ int cx88_set_tvnorm(struct cx88_core *core, v4l2_std_id norm)
 
 	dprintk(1,"set_tvnorm: MO_INPUT_FORMAT  0x%08x [old=0x%08x]\n",
 		cxiformat, cx_read(MO_INPUT_FORMAT) & 0x0f);
-	/* Chroma AGC must be disabled if SECAM is used */
-	if (norm & V4L2_STD_SECAM)
-		cx_andor(MO_INPUT_FORMAT, 0x40f, cxiformat);
-	else
-		cx_andor(MO_INPUT_FORMAT, 0xf, cxiformat);
+	/* Chroma AGC must be disabled if SECAM is used, we enable it
+	   by default on PAL and NTSC */
+	cx_andor(MO_INPUT_FORMAT, 0x40f,
+		 norm & V4L2_STD_SECAM ? cxiformat : cxiformat | 0x400);
 
 	// FIXME: as-is from DScaler
 	dprintk(1,"set_tvnorm: MO_OUTPUT_FORMAT 0x%08x [old=0x%08x]\n",
