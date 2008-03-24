4e1b36fb485dd81b0818ef1bc8fb5c0f2923a283
Al Viro <viro@zeniv.linux.org.uk>
2008-03-24 00:16:03 -0400

    [PATCH] umount_tree() will unhash everything itself
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/pnode.c
+++ fs/pnode.c
@@ -212,8 +212,7 @@ int propagate_mnt(struct vfsmount *dest_mnt, struct dentry *dest_dentry,
 out:
 	spin_lock(&vfsmount_lock);
 	while (!list_empty(&tmp_list)) {
-		child = list_entry(tmp_list.next, struct vfsmount, mnt_hash);
-		list_del_init(&child->mnt_hash);
+		child = list_first_entry(&tmp_list, struct vfsmount, mnt_hash);
 		umount_tree(child, 0, &umount_list);
 	}
 	spin_unlock(&vfsmount_lock);
