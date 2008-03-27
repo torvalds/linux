2ba95bcbe68d692f549fb10809f15681a25ff6fb
Glauber Costa <gcosta@redhat.com>
2008-03-27 14:05:57 -0300

    x86: provide bogus hard_smp_processor_id
    We provide a bogus macro for x86_64 in case CONFIG_X86_LOCAL_APIC
    is not set. It will always be set for x86_64, so the effect
    is just to make the code equal to i386.
    
    Signed-off-by: Glauber Costa <gcosta@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/smp_64.h
+++ include/asm-x86/smp_64.h
@@ -46,6 +46,8 @@ static inline int num_booting_cpus(void)
 
 #define safe_smp_processor_id()		smp_processor_id()
 
+#ifdef CONFIG_X86_LOCAL_APIC
+
 static inline int logical_smp_processor_id(void)
 {
 	/* we don't want to mark this access volatile - bad code generation */
@@ -62,6 +64,14 @@ static inline int hard_smp_processor_id(void)
 	return GET_APIC_ID(*(u32 *)(APIC_BASE + APIC_ID));
 }
 # endif /* APIC_DEFINITION */
+
+#else /* CONFIG_X86_LOCAL_APIC */
+
+# ifndef CONFIG_SMP
+#  define hard_smp_processor_id()	0
+# endif
+
+#endif /* CONFIG_X86_LOCAL_APIC */
 
 #endif
 
