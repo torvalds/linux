f9e8b782608021c0cbd936923cc0e509521b594d
Florian Fainelli <florian.fainelli@telecomint.eu>
2008-04-01 15:53:25 +0200

    [MIPS] XSS1500: Fix compilation
    This patch fixes the compilation of the Au1000 XSS1500
    board setup and irqmap code.
    
    Signed-off-by: Florian Fainelli <florian.fainelli@telecomint.eu>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/au1000/xxs1500/board_setup.c
+++ arch/mips/au1000/xxs1500/board_setup.c
@@ -33,11 +33,10 @@
 #include <asm/cpu.h>
 #include <asm/bootinfo.h>
 #include <asm/irq.h>
-#include <asm/keyboard.h>
 #include <asm/mipsregs.h>
 #include <asm/reboot.h>
 #include <asm/pgtable.h>
-#include <asm/au1000.h>
+#include <asm/mach-au1x00/au1000.h>
 
 void board_reset(void)
 {
