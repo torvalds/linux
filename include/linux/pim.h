2e8046271f68198dd37451017c1a4a2432e4ec68
YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
2008-04-03 09:22:09 +0900

    [IPV4] MROUTE: Move PIM definitions to <linux/pim.h>.
    Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>

---
+++ include/linux/pim.h
@@ -0,0 +1,29 @@
+#ifndef __LINUX_PIM_H
+#define __LINUX_PIM_H
+
+#include <asm/byteorder.h>
+
+/* Message types - V1 */
+#define PIM_V1_VERSION		__constant_htonl(0x10000000)
+#define PIM_V1_REGISTER		1
+
+/* Message types - V2 */
+#define PIM_VERSION		2
+#define PIM_REGISTER		1
+
+#if defined(__KERNEL__)
+#define PIM_NULL_REGISTER	__constant_htonl(0x40000000)
+
+/* PIMv2 register message header layout (ietf-draft-idmr-pimvsm-v2-00.ps */
+struct pimreghdr
+{
+	__u8	type;
+	__u8	reserved;
+	__be16	csum;
+	__be32	flags;
+};
+
+struct sk_buff;
+extern int pim_rcv_v1(struct sk_buff *);
+#endif
+#endif
