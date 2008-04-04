d66462f5314b0e70ddad8032eb76099475ca5571
Alexander van Heukelum <heukelum@mailshack.com>
2008-04-04 20:49:30 +0200

    x86: finalize bitops unification
    include/asm-x86/bitops_32.h and include/asm-x86/bitops_64.h are now
    almost identical. The 64-bit version sets ARCH_HAS_FAST_MULTIPLIER
    and has an extra inline function set_bit_string. The define currently
    has no influence on the generated code, but it can be argued that
    setting it on i386 is the right thing to do anyhow. The addition
    of the extra inline function on i386 does not hurt either.
    
    Signed-off-by: Alexander van Heukelum <heukelum@fastmail.fm>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/bitops.h
+++ include/asm-x86/bitops.h
@@ -403,10 +403,38 @@ static inline int fls(int x)
 
 #undef ADDR
 
-#ifdef CONFIG_X86_32
-# include "bitops_32.h"
-#else
-# include "bitops_64.h"
-#endif
+static inline void set_bit_string(unsigned long *bitmap,
+		unsigned long i, int len)
+{
+	unsigned long end = i + len;
+	while (i < end) {
+		__set_bit(i, bitmap);
+		i++;
+	}
+}
 
+#ifdef __KERNEL__
+
+#include <asm-generic/bitops/sched.h>
+
+#define ARCH_HAS_FAST_MULTIPLIER 1
+
+#include <asm-generic/bitops/hweight.h>
+
+#endif /* __KERNEL__ */
+
+#include <asm-generic/bitops/fls64.h>
+
+#ifdef __KERNEL__
+
+#include <asm-generic/bitops/ext2-non-atomic.h>
+
+#define ext2_set_bit_atomic(lock, nr, addr)			\
+	test_and_set_bit((nr), (unsigned long *)(addr))
+#define ext2_clear_bit_atomic(lock, nr, addr)			\
+	test_and_clear_bit((nr), (unsigned long *)(addr))
+
+#include <asm-generic/bitops/minix.h>
+
+#endif /* __KERNEL__ */
 #endif	/* _ASM_X86_BITOPS_H */
