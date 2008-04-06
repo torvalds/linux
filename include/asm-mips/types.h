23cf11ddb5099f8c7f7cb3eb154bff0faf31cae9
H. Peter Anvin <hpa@zytor.com>
2008-04-06 10:35:06 -0700

    mips: types: use <asm-generic/int-*.h> for the mips architecture
    This modifies <asm-mips/types.h> to use the <asm-generic/int-*.h>
    generic include files.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>
    Cc: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/types.h
+++ include/asm-mips/types.h
@@ -9,35 +9,15 @@
 #ifndef _ASM_TYPES_H
 #define _ASM_TYPES_H
 
+#if _MIPS_SZLONG == 64
+# include <asm-generic/int-l64.h>
+#else
+# include <asm-generic/int-ll64.h>
+#endif
+
 #ifndef __ASSEMBLY__
 
 typedef unsigned short umode_t;
-
-/*
- * __xx is ok: it doesn't pollute the POSIX namespace. Use these in the
- * header files exported to user space
- */
-
-typedef __signed__ char __s8;
-typedef unsigned char __u8;
-
-typedef __signed__ short __s16;
-typedef unsigned short __u16;
-
-typedef __signed__ int __s32;
-typedef unsigned int __u32;
-
-#if (_MIPS_SZLONG == 64)
-
-typedef __signed__ long __s64;
-typedef unsigned long __u64;
-
-#else
-
-#if defined(__GNUC__)
-__extension__ typedef __signed__ long long __s64;
-__extension__ typedef unsigned long long __u64;
-#endif
 
 #endif
 
@@ -51,30 +31,6 @@ __extension__ typedef unsigned long long __u64;
 #define BITS_PER_LONG _MIPS_SZLONG
 
 #ifndef __ASSEMBLY__
-
-
-typedef __signed char s8;
-typedef unsigned char u8;
-
-typedef __signed short s16;
-typedef unsigned short u16;
-
-typedef __signed int s32;
-typedef unsigned int u32;
-
-#if (_MIPS_SZLONG == 64)
-
-typedef __signed__ long s64;
-typedef unsigned long u64;
-
-#else
-
-#if defined(__GNUC__) && !defined(__STRICT_ANSI__)
-typedef __signed__ long long s64;
-typedef unsigned long long u64;
-#endif
-
-#endif
 
 #if (defined(CONFIG_HIGHMEM) && defined(CONFIG_64BIT_PHYS_ADDR)) \
     || defined(CONFIG_64BIT)
