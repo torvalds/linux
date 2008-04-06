a3727dc6c21cd0bed64dbc97212c39d2b391f5af
H. Peter Anvin <hpa@zytor.com>
2008-04-06 10:35:12 -0700

    sparc: types: use <asm-generic/int-*.h> for the sparc architecture
    This modifies <asm-sparc/types.h> to use the <asm-generic/int-*.h>
    generic include files.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>
    Cc: William L. Irwin <wli@holomorphy.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc/types.h
+++ include/asm-sparc/types.h
@@ -3,33 +3,17 @@
 #define _SPARC_TYPES_H
 
 /*
- * _xx is ok: it doesn't pollute the POSIX namespace. Use these in the
- * header files exported to user space.
- */
-
-/*
  * This file is never included by application software unless
  * explicitly requested (e.g., via linux/types.h) in which case the
  * application is Linux specific so (user-) name space pollution is
  * not a major issue.  However, for interoperability, libraries still
  * need to be careful to avoid a name clashes.
  */
+#include <asm-generic/int-ll64.h>
 
 #ifndef __ASSEMBLY__
 
 typedef unsigned short umode_t;
-
-typedef __signed__ char __s8;
-typedef unsigned char __u8;
-
-typedef __signed__ short __s16;
-typedef unsigned short __u16;
-
-typedef __signed__ int __s32;
-typedef unsigned int __u32;
-
-typedef __signed__ long long __s64;
-typedef unsigned long long __u64;
 
 #endif /* __ASSEMBLY__ */
 
@@ -38,18 +22,6 @@ typedef unsigned long long __u64;
 #define BITS_PER_LONG 32
 
 #ifndef __ASSEMBLY__
-
-typedef __signed__ char s8;
-typedef unsigned char u8;
-
-typedef __signed__ short s16;
-typedef unsigned short u16;
-
-typedef __signed__ int s32;
-typedef unsigned int u32;
-
-typedef __signed__ long long s64;
-typedef unsigned long long u64;
 
 typedef u32 dma_addr_t;
 typedef u32 dma64_addr_t;
