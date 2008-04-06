8523437b4c664cfc0f11998c4274846b95182000
H. Peter Anvin <hpa@zytor.com>
2008-04-06 10:35:07 -0700

    mn10300: types: use <asm-generic/int-*.h> for the mn10300 architecture
    This modifies <asm-mn10300/types.h> to use the <asm-generic/int-*.h>
    generic include files.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>
    Cc: David Howells <dhowells@redhat.com>
    Cc: Koichi Yasutake <yasutake.koichi@jp.panasonic.com>

--- include/asm-mn10300/types.h
+++ include/asm-mn10300/types.h
@@ -11,28 +11,11 @@
 #ifndef _ASM_TYPES_H
 #define _ASM_TYPES_H
 
+#include <asm-generic/int-ll64.h>
+
 #ifndef __ASSEMBLY__
 
 typedef unsigned short umode_t;
-
-/*
- * __xx is ok: it doesn't pollute the POSIX namespace. Use these in the
- * header files exported to user space
- */
-
-typedef __signed__ char __s8;
-typedef unsigned char __u8;
-
-typedef __signed__ short __s16;
-typedef unsigned short __u16;
-
-typedef __signed__ int __s32;
-typedef unsigned int __u32;
-
-#if defined(__GNUC__) && !defined(__STRICT_ANSI__)
-typedef __signed__ long long __s64;
-typedef unsigned long long __u64;
-#endif
 
 #endif /* __ASSEMBLY__ */
 
@@ -44,18 +27,6 @@ typedef unsigned long long __u64;
 #define BITS_PER_LONG 32
 
 #ifndef __ASSEMBLY__
-
-typedef signed char s8;
-typedef unsigned char u8;
-
-typedef signed short s16;
-typedef unsigned short u16;
-
-typedef signed int s32;
-typedef unsigned int u32;
-
-typedef signed long long s64;
-typedef unsigned long long u64;
 
 /* Dma addresses are 32-bits wide.  */
 typedef u32 dma_addr_t;
