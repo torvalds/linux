6adb4f733e9996b4fd68a6db50dd51bd2463ccac
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2008-04-07 22:33:57 -0700

    [TCP]: Don't allow FRTO to take place while MTU is being probed
    MTU probe can cause some remedies for FRTO because the normal
    packet ordering may be violated allowing FRTO to make a wrong
    decision (it might not be that serious threat for anything
    though). Thus it's safer to not run FRTO while MTU probe is
    underway.
    
    It seems that the basic FRTO variant should also look for an
    skb at probe_seq.start to check if that's retransmitted one
    but I didn't implement it now (plain seqno in window check
    isn't robust against wraparounds).
    
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -1691,9 +1691,14 @@ static inline void tcp_reset_reno_sack(struct tcp_sock *tp)
 int tcp_use_frto(struct sock *sk)
 {
 	const struct tcp_sock *tp = tcp_sk(sk);
+	const struct inet_connection_sock *icsk = inet_csk(sk);
 	struct sk_buff *skb;
 
 	if (!sysctl_tcp_frto)
+		return 0;
+
+	/* MTU probe and F-RTO won't really play nicely along currently */
+	if (icsk->icsk_mtup.probe_size)
 		return 0;
 
 	if (IsSackFrto())
