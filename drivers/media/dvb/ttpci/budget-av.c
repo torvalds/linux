c4e3bcb688dd561ae219a957e0d924f718335cab
Christoph Pfister <pfister@linuxtv.org>
2008-04-09 17:37:36 -0300

    V4L/DVB (7531): budget-av: Fix CI interface on (some) KNC1 DVBS cards
    Quoting the commit introducing reinitialise_demod (3984 / by adq):
    "These cards [KNC1 DVBT and DVBC] need special handling for CI - reinitialising the frontend
    device when the CI module is reset."
    Apparently my 1894:0010 also needs that fix, because once you initialise CI/CAM you lose lock.
    
    Signed-off-by: Christoph Pfister <pfister@linuxtv.org>
    Signed-off-by: Oliver Endriss <o.endriss@gmx.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/ttpci/budget-av.c
+++ drivers/media/dvb/ttpci/budget-av.c
@@ -941,6 +941,12 @@ static void frontend_init(struct budget_av *budget_av)
 	switch (saa->pci->subsystem_device) {
 
 	case SUBID_DVBS_KNC1:
+		/*
+		 * maybe that setting is needed for other dvb-s cards as well,
+		 * but so far it has been only confirmed for this type
+		 */
+		budget_av->reinitialise_demod = 1;
+		/* fall through */
 	case SUBID_DVBS_KNC1_PLUS:
 	case SUBID_DVBS_EASYWATCH_1:
 		if (saa->pci->subsystem_vendor == 0x1894) {
