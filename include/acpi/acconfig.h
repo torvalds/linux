8c49c235774002708bd0da1c28c570073ebd963b
Bob Moore <robert.moore@intel.com>
2008-04-10 19:06:42 +0400

    ACPICA: Update version to 20080123
    Update version to 20080123.
    
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Alexey Starikovskiy <astarikovskiy@suse.de>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- include/acpi/acconfig.h
+++ include/acpi/acconfig.h
@@ -63,7 +63,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20071219
+#define ACPI_CA_VERSION                 0x20080123
 
 /*
  * OS name, used for the _OS object.  The _OS object is essentially obsolete,
