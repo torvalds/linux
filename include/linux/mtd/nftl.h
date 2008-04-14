51ee83df6151a3e618e65236e304e00ac8d95607
Adrian Bunk <bunk@kernel.org>
2008-04-14 17:20:00 +0300

    [MTD] proper prototypes for nftl_{read,write}_oob()
    This patch adds proper prototypes for nftl_{read,write}_oob() in
    include/linux/mtd/nftl.h
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- include/linux/mtd/nftl.h
+++ include/linux/mtd/nftl.h
@@ -43,6 +43,11 @@ struct NFTLrecord {
 int NFTL_mount(struct NFTLrecord *s);
 int NFTL_formatblock(struct NFTLrecord *s, int block);
 
+int nftl_read_oob(struct mtd_info *mtd, loff_t offs, size_t len,
+		  size_t *retlen, uint8_t *buf);
+int nftl_write_oob(struct mtd_info *mtd, loff_t offs, size_t len,
+		   size_t *retlen, uint8_t *buf);
+
 #ifndef NFTL_MAJOR
 #define NFTL_MAJOR 93
 #endif
