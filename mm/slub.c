62f75532b583c03840f31e40386ce2df73be9ca0
Pekka Enberg <penberg@cs.helsinki.fi>
2008-04-14 18:50:44 +0300

    slub: Initialize per-cpu stats
    As spotted by kmemcheck, we need to initialize the per-CPU ->stat array before
    using it.
    
    [kmem_cache_cpu structures are usually allocated from arrays defined via
    DEFINE_PER_CPU that are zeroed so we have not noticed this so far --cl].
    
    Reported-by: Vegard Nossum <vegard.nossum@gmail.com>
    Signed-off-by: Christoph Lameter <clameter@sgi.com>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- mm/slub.c
+++ mm/slub.c
@@ -1886,6 +1886,9 @@ static void init_kmem_cache_cpu(struct kmem_cache *s,
 	c->node = 0;
 	c->offset = s->offset / sizeof(void *);
 	c->objsize = s->objsize;
+#ifdef CONFIG_SLUB_STATS
+	memset(c->stat, 0, NR_SLUB_STAT_ITEMS * sizeof(unsigned));
+#endif
 }
 
 static void init_kmem_cache_node(struct kmem_cache_node *n)
