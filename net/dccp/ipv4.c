fc5f8580d31d52890f17526ea739b124bd4e56f4
Pavel Emelyanov <xemul@openvz.org>
2008-04-13 22:31:05 -0700

    [NETNS][DCCPV4]: Enable DCCPv4 in net namespaces.
    This enables sockets creation with IPPROTO_DCCP and enables
    the ip level to pass DCCP packets to the DCCP level.
    
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>
    Acked-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dccp/ipv4.c
+++ net/dccp/ipv4.c
@@ -952,6 +952,7 @@ static struct net_protocol dccp_v4_protocol = {
 	.handler	= dccp_v4_rcv,
 	.err_handler	= dccp_v4_err,
 	.no_policy	= 1,
+	.netns_ok	= 1,
 };
 
 static const struct proto_ops inet_dccp_ops = {
