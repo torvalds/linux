9a248205b3a09a8ea2dcdb9cd5bd9b431c278f42
San Mehat <san@android.com>
2008-04-14 15:22:49 -0700

    mmc: Add concept of an 'embedded' SDIO device.
    This is required to support chips which use SDIO for signaling/
    communication but do not implement the various card enumeration registers
    as required for full SD / SDIO cards.
    
    mmc: sdio: Fix bug where we're freeing the CIS tables we never allocated when using EMBEDDED_SDIO
    mmc: Add max_blksize to embedded SDIO data
    
    Signed-off-by: San Mehat <san@google.com>

--- drivers/mmc/core/sdio_bus.c
+++ drivers/mmc/core/sdio_bus.c
@@ -23,6 +23,10 @@
 #include "sdio_cis.h"
 #include "sdio_bus.h"
 
+#ifdef CONFIG_MMC_EMBEDDED_SDIO
+#include <linux/mmc/host.h>
+#endif
+
 /* show configuration fields */
 #define sdio_config_attr(field, format_string)				\
 static ssize_t								\
@@ -260,7 +264,14 @@ static void sdio_release_func(struct device *dev)
 {
 	struct sdio_func *func = dev_to_sdio_func(dev);
 
-	sdio_free_func_cis(func);
+#ifdef CONFIG_MMC_EMBEDDED_SDIO
+	/*
+	 * If this device is embedded then we never allocated
+	 * cis tables for this func
+	 */
+	if (!func->card->host->embedded_sdio_data.funcs)
+#endif
+		sdio_free_func_cis(func);
 
 	if (func->info)
 		kfree(func->info);
