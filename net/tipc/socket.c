a198d3a200313bca8261e30e6daaad790937fd7e
Allan Stephens <allan.stephens@windriver.com>
2008-04-15 00:07:15 -0700

    [TIPC]: Allow stream receive to read from multiple TIPC messages
    This patch allows a stream socket to receive data from multiple
    TIPC messages in its receive queue, without requiring the use of
    the MSG_WAITALL flag.
    
    Acknowledgements to Florian Westphal <fw-tipc@strlen.de> for
    identifying this issue and suggesting how to correct it.
    
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/tipc/socket.c
+++ net/tipc/socket.c
@@ -1065,7 +1065,9 @@ restart:
 	/* Loop around if more data is required */
 
 	if ((sz_copied < buf_len)    /* didn't get all requested data */
-	    && (flags & MSG_WAITALL) /* ... and need to wait for more */
+	    && (!skb_queue_empty(&sock->sk->sk_receive_queue) ||
+		(flags & MSG_WAITALL))
+				     /* ... and more is ready or required */
 	    && (!(flags & MSG_PEEK)) /* ... and aren't just peeking at data */
 	    && (!err)                /* ... and haven't reached a FIN */
 	    )
