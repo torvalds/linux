703a8808cce6c97ca9bcb824bef77a5a306bbe92
Arve Hjønnevåg <arve@android.com>
2008-04-14 21:35:25 -0700

    printk: Fix log_buf_copy termination.
    If idx was non-zero and the log had wrapped, len did not get truncated
    to stop at the last byte written to the log.

--- kernel/printk.c
+++ kernel/printk.c
@@ -290,8 +290,8 @@ int log_buf_copy(char *dest, int idx, int len)
 	if (idx < 0 || idx >= max) {
 		ret = -1;
 	} else {
-		if (len > max)
-			len = max;
+		if (len > max - idx)
+			len = max - idx;
 		ret = len;
 		idx += (log_end - max);
 		while (len-- > 0)
