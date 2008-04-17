2661b13a21f609b279b4048978f8625d518df6e1
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2008-04-18 00:46:29 +0200

    ppc/mpc8xx: remove ppc_ide_md hooks
    * Initialize IDE ports in mpc8xx_ide_probe().
    
    * Remove m8xx_ide_init() and ppc_ide_md hooks - no need for them
      (IDE mpc8xx host driver takes care of all this setup).
    
    * Remove needless 'if (irq)' and 'if (data_port >= MAX_HWIFS)' checks
      from m8xx_ide_init_hwif_ports().
    
    * Remove 'ctrl_port' and 'irq' arguments from m8xx_ide_init_hwif_ports().
    
    * Rename m8xx_ide_init_hwif_ports() to m8xx_ide_init_ports().
    
    * Add __init tag to m8xx_ide_init_ports().
    
    This patch fixes hwif->irq always being overriden to 0 (== auto-probe, is
    this even working on PPC?) because of ide_init_default_irq() call in ide.c.
    
    There should be no other functional changes.
    
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Acked-by: Vitaly Bordug <vitb@kernel.crashing.org>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- arch/ppc/syslib/m8xx_setup.c
+++ arch/ppc/syslib/m8xx_setup.c
@@ -87,8 +87,6 @@ void m8xx_calibrate_decr(void);
 
 unsigned char __res[sizeof(bd_t)];
 
-extern void m8xx_ide_init(void);
-
 extern unsigned long find_available_memory(void);
 extern void m8xx_cpm_reset(void);
 extern void m8xx_wdt_handler_install(bd_t *bp);
@@ -474,8 +472,4 @@ platform_init(unsigned long r3, unsigned long r4, unsigned long r5,
 
 	ppc_md.find_end_of_memory	= m8xx_find_end_of_memory;
 	ppc_md.setup_io_mappings	= m8xx_map_io;
-
-#if defined(CONFIG_BLK_DEV_MPC8xx_IDE)
-	m8xx_ide_init();
-#endif
 }
