26c4fc26d0af9b16a6a234318d15ee0b3896a63d
Roland Dreier <rolandd@cisco.com>
2008-04-16 21:01:08 -0700

    RDMA/amso1100: Endian annotate mqsq allocator
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/hw/amso1100/c2.h
+++ drivers/infiniband/hw/amso1100/c2.h
@@ -545,7 +545,7 @@ extern void c2_ae_event(struct c2_dev *c2dev, u32 mq_index);
 extern int c2_init_mqsp_pool(struct c2_dev *c2dev, gfp_t gfp_mask,
 			     struct sp_chunk **root);
 extern void c2_free_mqsp_pool(struct c2_dev *c2dev, struct sp_chunk *root);
-extern u16 *c2_alloc_mqsp(struct c2_dev *c2dev, struct sp_chunk *head,
-			  dma_addr_t *dma_addr, gfp_t gfp_mask);
-extern void c2_free_mqsp(u16 * mqsp);
+extern __be16 *c2_alloc_mqsp(struct c2_dev *c2dev, struct sp_chunk *head,
+			     dma_addr_t *dma_addr, gfp_t gfp_mask);
+extern void c2_free_mqsp(__be16* mqsp);
 #endif
