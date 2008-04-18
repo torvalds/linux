d7f864be8323e5394040e2877594645b0e7da85d
Catalin Marinas <catalin.marinas@arm.com>
2008-04-18 22:43:06 +0100

    ARMv7: Add support for the ThumbEE state saving/restoring
    This patch adds the detection and handling of the ThumbEE extension on
    ARMv7 CPUs.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm/kernel/asm-offsets.c
+++ arch/arm/kernel/asm-offsets.c
@@ -58,6 +58,9 @@ int main(void)
   DEFINE(TI_TP_VALUE,		offsetof(struct thread_info, tp_value));
   DEFINE(TI_FPSTATE,		offsetof(struct thread_info, fpstate));
   DEFINE(TI_VFPSTATE,		offsetof(struct thread_info, vfpstate));
+#ifdef CONFIG_ARM_THUMBEE
+  DEFINE(TI_THUMBEE_STATE,	offsetof(struct thread_info, thumbee_state));
+#endif
 #ifdef CONFIG_IWMMXT
   DEFINE(TI_IWMMXT_STATE,	offsetof(struct thread_info, fpstate.iwmmxt));
 #endif
