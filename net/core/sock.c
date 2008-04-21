5309fbcc475084e6c1566084f770cef927937b7b
Rusty Russell <rusty@rustcorp.com.au>
2008-04-21 22:17:12 +0000

    Remove documentation of non-existent sk_alloc arg
    As you can see, there's no zero_it arg (in fact code always uses __GFP_ZERO).
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Signed-off-by: Jesper Juhl <jesper.juhl@gmail.com>

--- net/core/sock.c
+++ net/core/sock.c
@@ -942,7 +942,6 @@ static void sk_prot_free(struct proto *prot, struct sock *sk)
  *	@family: protocol family
  *	@priority: for allocation (%GFP_KERNEL, %GFP_ATOMIC, etc)
  *	@prot: struct proto associated with this new sock instance
- *	@zero_it: if we should zero the newly allocated sock
  */
 struct sock *sk_alloc(struct net *net, int family, gfp_t priority,
 		      struct proto *prot)
