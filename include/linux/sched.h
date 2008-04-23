8ae121ac8666b0421aa20fd80d4597ec66fa54bc
Gregory Haskins <ghaskins@novell.com>
2008-04-23 07:13:29 -0400

    sched: fix RT task-wakeup logic
    Dmitry Adamushko pointed out a logic error in task_wake_up_rt() where we
    will always evaluate to "true".  You can find the thread here:
    
    http://lkml.org/lkml/2008/4/22/296
    
    In reality, we only want to try to push tasks away when a wake up request is
    not going to preempt the current task.  So lets fix it.
    
    Note: We introduce test_tsk_need_resched() instead of open-coding the flag
    check so that the merge-conflict with -rt should help remind us that we
    may need to support NEEDS_RESCHED_DELAYED in the future, too.
    
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>
    CC: Dmitry Adamushko <dmitry.adamushko@gmail.com>
    CC: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1977,6 +1977,11 @@ static inline void clear_tsk_need_resched(struct task_struct *tsk)
 	clear_tsk_thread_flag(tsk,TIF_NEED_RESCHED);
 }
 
+static inline int test_tsk_need_resched(struct task_struct *tsk)
+{
+	return unlikely(test_tsk_thread_flag(tsk,TIF_NEED_RESCHED));
+}
+
 static inline int signal_pending(struct task_struct *p)
 {
 	return unlikely(test_tsk_thread_flag(p,TIF_SIGPENDING));
@@ -1991,7 +1996,7 @@ static inline int fatal_signal_pending(struct task_struct *p)
 
 static inline int need_resched(void)
 {
-	return unlikely(test_thread_flag(TIF_NEED_RESCHED));
+	return unlikely(test_tsk_need_resched(current));
 }
 
 /*
