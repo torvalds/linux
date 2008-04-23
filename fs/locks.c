190855576743a510219fc67886dace29b825d8cb
David M. Richter <richterd@citi.umich.edu>
2008-04-23 16:29:01 -0400

    leases: move lock allocation earlier in generic_setlease()
    In generic_setlease(), the struct file_lock is allocated after tests for the
    presence of conflicting readers/writers is done, despite the fact that the
    allocation might block; this patch moves the allocation earlier.  A subsequent
    set of patches will rely on this behavior to properly serialize between a
    modified __break_lease() and generic_setlease().
    
    Signed-off-by: David M. Richter <richterd@citi.umich.edu>
    Signed-off-by: J. Bruce Fields <bfields@citi.umich.edu>

--- fs/locks.c
+++ fs/locks.c
@@ -1368,17 +1368,17 @@ int generic_setlease(struct file *filp, long arg, struct file_lock **flp)
 	lease = *flp;
 
 	if (arg != F_UNLCK) {
+		error = -ENOMEM;
+		new_fl = locks_alloc_lock();
+		if (new_fl == NULL)
+			goto out;
+
 		error = -EAGAIN;
 		if ((arg == F_RDLCK) && (atomic_read(&inode->i_writecount) > 0))
 			goto out;
 		if ((arg == F_WRLCK)
 		    && ((atomic_read(&dentry->d_count) > 1)
 			|| (atomic_read(&inode->i_count) > 1)))
-			goto out;
-
-		error = -ENOMEM;
-		new_fl = locks_alloc_lock();
-		if (new_fl == NULL)
 			goto out;
 	}
 
