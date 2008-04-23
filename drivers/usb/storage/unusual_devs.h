cef03f8f1d5042e85de431d739eeded89d79999b
Andrew Lunn <andrew@lunn.ch>
2008-04-23 22:04:30 +0200

    USB: storage: Update mailling list address
    drivers/usb/storage/unusual_devs.h lists the address
    linux-usb-devel@lists.sourceforge.net for patches to that file. This
    address results in a bounce and a pointer to vger. This patch updates
    the address in the header file.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/storage/unusual_devs.h
+++ drivers/usb/storage/unusual_devs.h
@@ -44,7 +44,8 @@
  *	  running with this patch.
  * Send your submission to either Phil Dibowitz <phil@ipom.com> or
  * Alan Stern <stern@rowland.harvard.edu>, and don't forget to CC: the
- * USB development list <linux-usb-devel@lists.sourceforge.net>.
+ * USB development list <linux-usb@vger.kernel.org> and the USB storage list
+ * <usb-storage@lists.one-eyed-alien.net>
  */
 
 /* patch submitted by Vivian Bregier <Vivian.Bregier@imag.fr>
