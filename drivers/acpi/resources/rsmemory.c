75a44ce00b312f57264f42a0a985d17cd9994b98
Len Brown <len.brown@intel.com>
2008-04-23 23:00:13 -0400

    ACPICA: update Intel copyright
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/resources/rsmemory.c
+++ drivers/acpi/resources/rsmemory.c
@@ -5,7 +5,7 @@
  ******************************************************************************/
 
 /*
- * Copyright (C) 2000 - 2007, R. Byron Moore
+ * Copyright (C) 2000 - 2008, Intel Corp.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
