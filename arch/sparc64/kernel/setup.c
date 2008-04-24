db9a7fb12c8d05104e8a541003593d62a42ade8f
David S. Miller <davem@davemloft.net>
2008-04-23 22:22:29 -0700

    [SPARC64]: PROM debug console can be CON_ANYTIME.
    No per-cpu or similar resources need to be setup before
    we can use this console device.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/setup.c
+++ arch/sparc64/kernel/setup.c
@@ -82,7 +82,7 @@ unsigned long cmdline_memory_size = 0;
 static struct console prom_early_console = {
 	.name =		"earlyprom",
 	.write =	prom_console_write,
-	.flags =	CON_PRINTBUFFER | CON_BOOT,
+	.flags =	CON_PRINTBUFFER | CON_BOOT | CON_ANYTIME,
 	.index =	-1,
 };
 
