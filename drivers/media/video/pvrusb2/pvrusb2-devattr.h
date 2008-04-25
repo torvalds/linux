0e3cbe81d52d18d83d068935512bd623a8765c12
Harvey Harrison <harvey.harrison@gmail.com>
2008-04-25 02:19:44 -0300

    V4L/DVB (7746): pvrusb2: make signed one-bit bitfields unsigned
    Single-bit signed bitfields can only take 0/-1 rather than 0/1 as the
    drivers seems to assume...add unsigned.
    Noticed by sparse:
    drivers/media/video/pvrusb2/pvrusb2-devattr.h:107:34: error: dubious one-bit signed bitfield
    drivers/media/video/pvrusb2/pvrusb2-devattr.h:114:37: error: dubious one-bit signed bitfield
    drivers/media/video/pvrusb2/pvrusb2-devattr.h:117:30: error: dubious one-bit signed bitfield
    drivers/media/video/pvrusb2/pvrusb2-devattr.h:120:23: error: dubious one-bit signed bitfield
    drivers/media/video/pvrusb2/pvrusb2-devattr.h:124:24: error: dubious one-bit signed bitfield
    drivers/media/video/pvrusb2/pvrusb2-devattr.h:128:23: error: dubious one-bit signed bitfield
    drivers/media/video/pvrusb2/pvrusb2-devattr.h:138:36: error: dubious one-bit signed bitfield
    drivers/media/video/pvrusb2/pvrusb2-devattr.h:143:24: error: dubious one-bit signed bitfield
    drivers/media/video/pvrusb2/pvrusb2-devattr.h:144:28: error: dubious one-bit signed bitfield
    drivers/media/video/pvrusb2/pvrusb2-devattr.h:145:26: error: dubious one-bit signed bitfield
    drivers/media/video/pvrusb2/pvrusb2-devattr.h:146:23: error: dubious one-bit signed bitfield
    
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>
    Signed-off-by: Mike Isely <isely@pobox.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/pvrusb2/pvrusb2-devattr.h
+++ drivers/media/video/pvrusb2/pvrusb2-devattr.h
@@ -104,28 +104,28 @@ struct pvr2_device_desc {
 	unsigned char digital_control_scheme;
 
 	/* If set, we don't bother trying to load cx23416 firmware. */
-	int flag_skip_cx23416_firmware:1;
+	unsigned int flag_skip_cx23416_firmware:1;
 
 	/* If set, the encoder must be healthy in order for digital mode to
 	   work (otherwise we assume that digital streaming will work even
 	   if we fail to locate firmware for the encoder).  If the device
 	   doesn't support digital streaming then this flag has no
 	   effect. */
-	int flag_digital_requires_cx23416:1;
+	unsigned int flag_digital_requires_cx23416:1;
 
 	/* Device has a hauppauge eeprom which we can interrogate. */
-	int flag_has_hauppauge_rom:1;
+	unsigned int flag_has_hauppauge_rom:1;
 
 	/* Device does not require a powerup command to be issued. */
-	int flag_no_powerup:1;
+	unsigned int flag_no_powerup:1;
 
 	/* Device has a cx25840 - this enables special additional logic to
 	   handle it. */
-	int flag_has_cx25840:1;
+	unsigned int flag_has_cx25840:1;
 
 	/* Device has a wm8775 - this enables special additional logic to
 	   ensure that it is found. */
-	int flag_has_wm8775:1;
+	unsigned int flag_has_wm8775:1;
 
 	/* Device has IR hardware that can be faked into looking like a
 	   normal Hauppauge i2c IR receiver.  This is currently very
@@ -135,15 +135,15 @@ struct pvr2_device_desc {
 	   to virtualize the presence of the non-existant IR receiver chip and
 	   implement the virtual receiver in terms of appropriate FX2
 	   commands. */
-	int flag_has_hauppauge_custom_ir:1;
+	unsigned int flag_has_hauppauge_custom_ir:1;
 
 	/* These bits define which kinds of sources the device can handle.
 	   Note: Digital tuner presence is inferred by the
 	   digital_control_scheme enumeration. */
-	int flag_has_fmradio:1;       /* Has FM radio receiver */
-	int flag_has_analogtuner:1;   /* Has analog tuner */
-	int flag_has_composite:1;     /* Has composite input */
-	int flag_has_svideo:1;        /* Has s-video input */
+	unsigned int flag_has_fmradio:1;       /* Has FM radio receiver */
+	unsigned int flag_has_analogtuner:1;   /* Has analog tuner */
+	unsigned int flag_has_composite:1;     /* Has composite input */
+	unsigned int flag_has_svideo:1;        /* Has s-video input */
 };
 
 extern struct usb_device_id pvr2_device_table[];
