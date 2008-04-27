403ae52ac047eb339f2b7e8cdf93a3b8077914db
Robert Reif <reif@earthlink.net>
2008-04-26 22:29:43 -0700

    sparc: fix drivers/video/tcx.c warning
    Fix compile warning:
    
    CC drivers/video/tcx.o
    drivers/video/tcx.c: In function ‘tcx_init_one’:
    drivers/video/tcx.c:477: warning: format ‘%lx’ expects type ‘long 
    unsigned int’, but argument 4 has type ‘resource_size_t’
    
    This was the only sparc driver to use the resource directly in the
    printk so I changed it to physbase like the other drivers.
    Boot tested on SS4.
    
    Signed-off-by: Robert Reif <reif@earthlink.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/video/tcx.c
+++ drivers/video/tcx.c
@@ -419,7 +419,7 @@ static int __devinit tcx_init_one(struct of_device *op)
 		par->mmap_map[6].size = SBUS_MMAP_EMPTY;
 	}
 
-	par->physbase = 0;
+	par->physbase = op->resource[0].start;
 	par->which_io = op->resource[0].flags & IORESOURCE_BITS;
 
 	for (i = 0; i < TCX_MMAP_ENTRIES; i++) {
@@ -473,7 +473,7 @@ static int __devinit tcx_init_one(struct of_device *op)
 	printk("%s: TCX at %lx:%lx, %s\n",
 	       dp->full_name,
 	       par->which_io,
-	       op->resource[0].start,
+	       par->physbase,
 	       par->lowdepth ? "8-bit only" : "24-bit depth");
 
 	return 0;
