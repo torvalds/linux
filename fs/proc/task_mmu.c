9d02dbc8137759e4c2f91db0b7f9c8a1ec2a9276
Adrian Bunk <bunk@kernel.org>
2008-04-28 02:12:11 -0700

    make swap_pte_to_pagemap_entry() static
    Make the needlessly global swap_pte_to_pagemap_entry() static.
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Acked-by: Matt Mackall <mpm@selenic.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -579,7 +579,7 @@ static int pagemap_pte_hole(unsigned long start, unsigned long end,
 	return err;
 }
 
-u64 swap_pte_to_pagemap_entry(pte_t pte)
+static u64 swap_pte_to_pagemap_entry(pte_t pte)
 {
 	swp_entry_t e = pte_to_swp_entry(pte);
 	return swp_type(e) | (swp_offset(e) << MAX_SWAPFILES_SHIFT);
