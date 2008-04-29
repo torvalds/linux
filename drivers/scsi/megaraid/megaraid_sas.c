127ce971adeb4514bc4edc5bf45f79beb0c94aa5
bo yang <bo.yang@lsi.com>
2008-04-29 03:55:33 -0400

    [SCSI] megaraid_sas; Update the Version and Changelog
    Update the Version and Changelog for megaraid_sas Driver
    
    Signed-off-by: Bo Yang<bo.yang@lsi.com>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- drivers/scsi/megaraid/megaraid_sas.c
+++ drivers/scsi/megaraid/megaraid_sas.c
@@ -10,7 +10,7 @@
  *	   2 of the License, or (at your option) any later version.
  *
  * FILE		: megaraid_sas.c
- * Version	: v00.00.03.16-rc1
+ * Version	: v00.00.03.20-rc1
  *
  * Authors:
  *	(email-id : megaraidlinux@lsi.com)
