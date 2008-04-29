faebe9fdf35058bb8421e4c09f6f70994eaf8db2
Pavel Emelyanov <xemul@openvz.org>
2008-04-29 01:00:18 -0700

    memcgroups: add a document describing the resource counter abstraction
    The resource counter is supposed to facilitate the resource accounting of
    arbitrary resource (and it already does this for memory controller).
    
    However, it is about to be used in other resources controllers (swap, kernel
    memory, networking, etc), so provide a doc describing how to work with it.
    This will eliminate all the possible future duplications in the appropriate
    controllers' docs.
    
    Fixed errors pointed out by Randy.
    
    [akpm@linux-foundation.org: fix documentation tpyo]
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>
    Cc: Randy Dunlap <randy.dunlap@oracle.com>
    Cc: Balbir Singh <balbir@linux.vnet.ibm.com>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: Li Zefan <lizf@cn.fujitsu.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/res_counter.h
+++ include/linux/res_counter.h
@@ -9,6 +9,8 @@
  *
  * Author: Pavel Emelianov <xemul@openvz.org>
  *
+ * See Documentation/controllers/resource_counter.txt for more
+ * info about what this counter is.
  */
 
 #include <linux/cgroup.h>
