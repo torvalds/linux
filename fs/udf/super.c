221e583a735fc5d879d83c2a76b8ee5afcbdf146
Rasmus Rohde <rohde@duff.dk>
2008-04-30 17:22:06 +0200

    udf: Make udf exportable
    Cc: Christoph Hellwig <hch@infradead.org>
    Signed-off-by: Rasmus Rohde <rohde@duff.dk>
    Signed-off-by: Jan Kara <jack@suse.cz>

--- fs/udf/super.c
+++ fs/udf/super.c
@@ -1933,6 +1933,7 @@ static int udf_fill_super(struct super_block *sb, void *options, int silent)
 
 	/* Fill in the rest of the superblock */
 	sb->s_op = &udf_sb_ops;
+	sb->s_export_op = &udf_export_ops;
 	sb->dq_op = NULL;
 	sb->s_dirt = 0;
 	sb->s_magic = UDF_SUPER_MAGIC;
