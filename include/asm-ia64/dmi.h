ef3fb66ced1a890769d71a4f4ae9f68b8afa98b6
Jean Delvare <khali@linux-fr.org>
2008-05-01 04:35:01 -0700

    dmi: clean-up dmi helper declarations
    The declaration of dmi helper functions is a bit messy and inconsistent at the
    moment:
    
    * On ia64 they are declared in <asm/io.h>.
    * On x86-64 they are declared in <asm/dmi.h>.
    * On i386 they are declared both in <asm/io.h> and <asm/dmi.h>.
    
    Fix the header files so that the dmi helper functions are consistently
    defined in <asm/dmi.h>.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Cc: Matt Domsch <Matt_Domsch@dell.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-ia64/dmi.h
+++ include/asm-ia64/dmi.h
@@ -3,4 +3,9 @@
 
 #include <asm/io.h>
 
+/* Use normal IO mappings for DMI */
+#define dmi_ioremap ioremap
+#define dmi_iounmap(x,l) iounmap(x)
+#define dmi_alloc(l) kmalloc(l, GFP_ATOMIC)
+
 #endif
