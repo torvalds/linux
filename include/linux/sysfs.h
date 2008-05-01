2850699c59d513a0cd0c68f60f75609a5f9d4d32
Randy Dunlap <randy.dunlap@oracle.com>
2008-05-01 04:34:43 -0700

    sysfs: sysfs_update_group stub for CONFIG_SYSFS=n
    scsi_transport_spi uses sysfs_update_group() when CONFIG_SYSFS=n, so provide a
    stub for it.
    
    next-20080423/drivers/scsi/scsi_transport_spi.c:1467: error: implicit declaration of function 'sysfs_update_group'
    make[3]: *** [drivers/scsi/scsi_transport_spi.o] Error 1
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Cc: Greg KH <greg@kroah.com>
    Cc: James Bottomley <James.Bottomley@HansenPartnership.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/sysfs.h
+++ include/linux/sysfs.h
@@ -196,6 +196,12 @@ static inline int sysfs_update_group(struct kobject *kobj,
 	return 0;
 }
 
+static inline int sysfs_update_group(struct kobject *kobj,
+				const struct attribute_group *grp)
+{
+	return 0;
+}
+
 static inline void sysfs_remove_group(struct kobject *kobj,
 				      const struct attribute_group *grp)
 {
