adafbedf0c31ae1cde62035c82857f5e376af553
David Howells <dhowells@redhat.com>
2008-05-01 04:34:24 -0700

    frv: unbreak misalignment handling changes
    Fix a reference in a arch/frv/mm/Makefile to unaligned.c which has now been
    deleted.
    
    Also revert the change to the guard macro name in include/asm-frv/unaligned.h.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Cc: Harvey Harrison <harvey.harrison@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-frv/unaligned.h
+++ include/asm-frv/unaligned.h
@@ -9,8 +9,8 @@
  * 2 of the License, or (at your option) any later version.
  */
 
-#ifndef _ASM_FRV_UNALIGNED_H
-#define _ASM_FRV_UNALIGNED_H
+#ifndef _ASM_UNALIGNED_H
+#define _ASM_UNALIGNED_H
 
 #include <linux/unaligned/le_byteshift.h>
 #include <linux/unaligned/be_byteshift.h>
@@ -19,4 +19,4 @@
 #define get_unaligned	__get_unaligned_be
 #define put_unaligned	__put_unaligned_be
 
-#endif /* _ASM_FRV_UNALIGNED_H */
+#endif /* _ASM_UNALIGNED_H */
