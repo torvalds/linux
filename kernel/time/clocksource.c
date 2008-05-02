4359a023a8c3b247b348c310bf510b23f3c1ab64
Heiko Carstens <heiko.carstens@de.ibm.com>
2008-05-02 12:49:40 +0200

    clocksource: Fix permissions for available_clocksource
    File permissions for
    /sys/devices/system/clocksource/clocksource0/available_clocksource
    are 600 which allows write access. But this is in fact a read only
    file. So change permissions to 400.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: John Stultz <johnstul@us.ibm.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/time/clocksource.c
+++ kernel/time/clocksource.c
@@ -474,7 +474,7 @@ sysfs_show_available_clocksources(struct sys_device *dev, char *buf)
 static SYSDEV_ATTR(current_clocksource, 0600, sysfs_show_current_clocksources,
 		   sysfs_override_clocksource);
 
-static SYSDEV_ATTR(available_clocksource, 0600,
+static SYSDEV_ATTR(available_clocksource, 0400,
 		   sysfs_show_available_clocksources, NULL);
 
 static struct sysdev_class clocksource_sysclass = {
