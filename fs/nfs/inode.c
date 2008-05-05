b0b539739fe9b7d75002412a787cfdf4efddbc33
Trond Myklebust <Trond.Myklebust@netapp.com>
2008-05-05 11:45:41 -0400

    NFS: Ensure that 'noac' and/or 'actimeo=0' turn off attribute caching
    Both the 'noac' and 'actimeo=0' mount options should ensure that attributes
    are not cached, however a bug in nfs_attribute_timeout() means that
    currently, the attributes may in fact get cached for up to one jiffy. This
    has been seen to cause corruption in some applications.
    
    The reason for the bug is that the time_in_range() test returns 'true' as
    long as the current time lies between nfsi->read_cache_jiffies and
    nfsi->read_cache_jiffies + nfsi->attrtimeo. In other words, if jiffies
    equals nfsi->read_cache_jiffies, then we still cache the attribute data.
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/inode.c
+++ fs/nfs/inode.c
@@ -707,6 +707,13 @@ int nfs_attribute_timeout(struct inode *inode)
 
 	if (nfs_have_delegation(inode, FMODE_READ))
 		return 0;
+	/*
+	 * Special case: if the attribute timeout is set to 0, then always
+	 * 		 treat the cache as having expired (unless holding
+	 * 		 a delegation).
+	 */
+	if (nfsi->attrtimeo == 0)
+		return 1;
 	return !time_in_range(jiffies, nfsi->read_cache_jiffies, nfsi->read_cache_jiffies + nfsi->attrtimeo);
 }
 
