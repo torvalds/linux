af6061af0d9f84a4665f88186dc1ff9e4fb78330
Dave Airlie <airlied@linux.ie>
2008-05-07 12:15:39 +1000

    Revert "drm/vbl rework: rework how the drm deals with vblank."
    This reverts commit ac741ab71bb39e6977694ac0cc26678d8673cda4.
    
    Okay this looks like wasn't as fully baked as I'd led myself to believe.
    
    Revert for now for further baking.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/char/drm/i915_drv.c
+++ drivers/char/drm/i915_drv.c
@@ -533,7 +533,8 @@ static struct drm_driver driver = {
 	 */
 	.driver_features =
 	    DRIVER_USE_AGP | DRIVER_REQUIRE_AGP | /* DRIVER_USE_MTRR |*/
-	    DRIVER_HAVE_IRQ | DRIVER_IRQ_SHARED,
+	    DRIVER_HAVE_IRQ | DRIVER_IRQ_SHARED | DRIVER_IRQ_VBL |
+	    DRIVER_IRQ_VBL2,
 	.load = i915_driver_load,
 	.unload = i915_driver_unload,
 	.lastclose = i915_driver_lastclose,
@@ -541,9 +542,8 @@ static struct drm_driver driver = {
 	.suspend = i915_suspend,
 	.resume = i915_resume,
 	.device_is_agp = i915_driver_device_is_agp,
-	.get_vblank_counter = i915_get_vblank_counter,
-	.enable_vblank = i915_enable_vblank,
-	.disable_vblank = i915_disable_vblank,
+	.vblank_wait = i915_driver_vblank_wait,
+	.vblank_wait2 = i915_driver_vblank_wait2,
 	.irq_preinstall = i915_driver_irq_preinstall,
 	.irq_postinstall = i915_driver_irq_postinstall,
 	.irq_uninstall = i915_driver_irq_uninstall,
