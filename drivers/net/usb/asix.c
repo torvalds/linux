ac7b77f13f2f33270276f88ad0f427e031552e04
Mattia Dongili <malattia@linux.it>
2008-05-06 20:42:35 -0700

    usb/asix: add Buffalo LUA-U2-GT 10/100/1000
    The USB net adapter Buffalo LUA-U2-GT (0411:006e) carries a AX88178 chip.
    Tested on the above HW.
    
    Signed-off-by: Mattia Dongili <malattia@linux.it>
    Acked-off-by: David Hollis <dhollis@davehollis.com>
    Cc: Greg KH <greg@kroah.com>
    Acked-by: Jeff Garzik <jeff@garzik.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/net/usb/asix.c
+++ drivers/net/usb/asix.c
@@ -1381,6 +1381,10 @@ static const struct usb_device_id	products [] = {
 	USB_DEVICE (0x0411, 0x003d),
 	.driver_info =  (unsigned long) &ax8817x_info,
 }, {
+	// Buffalo LUA-U2-GT 10/100/1000
+	USB_DEVICE (0x0411, 0x006e),
+	.driver_info =  (unsigned long) &ax88178_info,
+}, {
 	// Sitecom LN-029 "USB 2.0 10/100 Ethernet adapter"
 	USB_DEVICE (0x6189, 0x182d),
 	.driver_info =  (unsigned long) &ax8817x_info,
