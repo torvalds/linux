e46b66bc42b6b1430b04cc5c207ecb2b2f4553dc
Ben Hutchings <bhutchings@solarflare.com>
2008-05-08 02:53:17 -0700

    net: Added ASSERT_RTNL() to dev_open() and dev_close().
    dev_open() and dev_close() must be called holding the RTNL, since they
    call device functions and netdevice notifiers that are promised the RTNL.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -994,6 +994,8 @@ int dev_open(struct net_device *dev)
 {
 	int ret = 0;
 
+	ASSERT_RTNL();
+
 	/*
 	 *	Is it already up?
 	 */
@@ -1060,6 +1062,8 @@ int dev_open(struct net_device *dev)
  */
 int dev_close(struct net_device *dev)
 {
+	ASSERT_RTNL();
+
 	might_sleep();
 
 	if (!(dev->flags & IFF_UP))
