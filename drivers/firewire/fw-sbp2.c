2635f96f9086409de0ec882a210f374c012bffc3
Stefan Richter <stefanr@s5r6.in-berlin.de>
2008-05-11 00:36:47 +0200

    firewire: fw-sbp2: spin disks down on suspend and shutdown
    This instructs sd_mod to send START STOP UNIT on suspend and resume,
    and on driver unbinding or unloading (including when the system is shut
    down).
    
    We don't do this though if multiple initiators may log in to the target.
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>
    Tested-by: Tino Keitel <tino.keitel@gmx.de>

--- drivers/firewire/fw-sbp2.c
+++ drivers/firewire/fw-sbp2.c
@@ -1545,6 +1545,9 @@ static int sbp2_scsi_slave_configure(struct scsi_device *sdev)
 
 	sdev->use_10_for_rw = 1;
 
+	if (sbp2_param_exclusive_login)
+		sdev->manage_start_stop = 1;
+
 	if (sdev->type == TYPE_ROM)
 		sdev->use_10_for_ms = 1;
 
