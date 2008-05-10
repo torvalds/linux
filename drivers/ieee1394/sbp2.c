82f06e86117680ada35fdb76c8852268d994cd99
Stefan Richter <stefanr@s5r6.in-berlin.de>
2008-05-11 00:37:14 +0200

    ieee1394: sbp2: spin disks down on suspend and shutdown
    This instructs sd_mod to send START STOP UNIT on suspend and resume,
    and on driver unbinding or unloading (including when the system is shut
    down).
    
    We don't do this though if multiple initiators may log in to the target.
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/ieee1394/sbp2.c
+++ drivers/ieee1394/sbp2.c
@@ -2009,6 +2009,8 @@ static int sbp2scsi_slave_configure(struct scsi_device *sdev)
 
 	sdev->use_10_for_rw = 1;
 
+	if (sbp2_exclusive_login)
+		sdev->manage_start_stop = 1;
 	if (sdev->type == TYPE_ROM)
 		sdev->use_10_for_ms = 1;
 	if (sdev->type == TYPE_DISK &&
