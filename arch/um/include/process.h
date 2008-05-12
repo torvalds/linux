fe2cc53ee013a4d4d0317d418e7019fe6533a5a8
Jeff Dike <jdike@addtoit.com>
2008-05-12 14:02:00 -0700

    uml: track and make up lost ticks
    Alarm delivery could be noticably late in the !CONFIG_NOHZ case because lost
    ticks weren't being taken into account.  This is now treated more carefully,
    with the time between ticks being calculated and the appropriate number of
    ticks delivered to the timekeeping system.
    
    Cc: Nix <nix@esperi.org.uk>
    Signed-off-by: Jeff Dike <jdike@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/um/include/process.h
+++ arch/um/include/process.h
@@ -11,7 +11,7 @@
 /* Copied from linux/compiler-gcc.h since we can't include it directly */
 #define barrier() __asm__ __volatile__("": : :"memory")
 
-extern void sig_handler(int sig, struct sigcontext sc);
-extern void alarm_handler(int sig, struct sigcontext sc);
+extern void sig_handler(int sig, struct sigcontext *sc);
+extern void alarm_handler(int sig, struct sigcontext *sc);
 
 #endif
