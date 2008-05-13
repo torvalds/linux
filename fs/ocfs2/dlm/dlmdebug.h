959040c37a8cae8117907d4aed87f1b01ff1ea19
Sunil Mushran <sunil.mushran@oracle.com>
2008-05-12 18:31:36 -0700

    [PATCH 2/3] ocfs2/dlm: Silence build warnings
    This patch silences the build warnings concerning dlm_debug_init()
    and friends when building without CONFIG_DEBUG_FS enabled.
    
    Signed-off-by: Sunil Mushran <sunil.mushran@oracle.com>
    Signed-off-by: Mark Fasheh <mfasheh@suse.com>

--- fs/ocfs2/dlm/dlmdebug.h
+++ fs/ocfs2/dlm/dlmdebug.h
@@ -60,25 +60,25 @@ void dlm_destroy_debugfs_root(void);
 
 #else
 
-static int dlm_debug_init(struct dlm_ctxt *dlm)
+static inline int dlm_debug_init(struct dlm_ctxt *dlm)
 {
 	return 0;
 }
-static void dlm_debug_shutdown(struct dlm_ctxt *dlm)
+static inline void dlm_debug_shutdown(struct dlm_ctxt *dlm)
 {
 }
-static int dlm_create_debugfs_subroot(struct dlm_ctxt *dlm)
-{
-	return 0;
-}
-static void dlm_destroy_debugfs_subroot(struct dlm_ctxt *dlm)
-{
-}
-static int dlm_create_debugfs_root(void)
+static inline int dlm_create_debugfs_subroot(struct dlm_ctxt *dlm)
 {
 	return 0;
 }
-static void dlm_destroy_debugfs_root(void)
+static inline void dlm_destroy_debugfs_subroot(struct dlm_ctxt *dlm)
+{
+}
+static inline int dlm_create_debugfs_root(void)
+{
+	return 0;
+}
+static inline void dlm_destroy_debugfs_root(void)
 {
 }
 
