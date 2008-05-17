dedd4915af40cff6614707c50dcae43d17beadec
Ingo Molnar <mingo@elte.hu>
2008-05-17 08:28:33 +0200

    bitops: fix build in struct thread_info
    we can move flags from u32 to natural size - assembly code uses
    offsetof.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/thread_info.h
+++ include/asm-x86/thread_info.h
@@ -24,10 +24,10 @@ struct exec_domain;
 struct thread_info {
 	struct task_struct	*task;		/* main task structure */
 	struct exec_domain	*exec_domain;	/* execution domain */
-	__u32			flags;		/* low level flags */
+	unsigned long		flags;		/* low level flags */
 	__u32			status;		/* thread synchronous flags */
 	__u32			cpu;		/* current CPU */
-	int 			preempt_count;	/* 0 => preemptable,
+	int			preempt_count;	/* 0 => preemptable,
 						   <0 => BUG */
 	mm_segment_t		addr_limit;
 	struct restart_block    restart_block;
