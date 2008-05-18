42ece6c1f8162cd782b44dc4863679e888531df5
Stas Sergeev <stsp@aknet.ru>
2008-05-18 18:30:03 +0200

    snd-pcsp: silent misleading warning
    It appears that alsa allows a sound buffer with size not
    evenly devided by the period size. This triggers a warning in
    snd-pcsp and floods the log. As a quick fix, the warning should
    be disabled.
    
    Signed-off-by: Stas Sergeev <stsp@aknet.ru>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/drivers/pcsp/pcsp_lib.c
+++ sound/drivers/pcsp/pcsp_lib.c
@@ -119,9 +119,11 @@ enum hrtimer_restart pcsp_do_timer(struct hrtimer *handle)
 	chip->playback_ptr += PCSP_INDEX_INC() * fmt_size;
 	periods_elapsed = chip->playback_ptr - chip->period_ptr;
 	if (periods_elapsed < 0) {
-		printk(KERN_WARNING "PCSP: playback_ptr inconsistent "
+#if PCSP_DEBUG
+		printk(KERN_INFO "PCSP: buffer_bytes mod period_bytes != 0 ? "
 			"(%zi %zi %zi)\n",
 			chip->playback_ptr, period_bytes, buffer_bytes);
+#endif
 		periods_elapsed += buffer_bytes;
 	}
 	periods_elapsed /= period_bytes;
