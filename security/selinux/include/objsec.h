feb2a5b82d87fbdc01c00b7e9413e4b5f4c1f0c1
James Morris <jmorris@namei.org>
2008-05-20 09:42:33 +1000

    SELinux: remove inherit field from inode_security_struct
    Remove inherit field from inode_security_struct, per Stephen Smalley:
    "Let's just drop inherit altogether - dead field."
    
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/selinux/include/objsec.h
+++ security/selinux/include/objsec.h
@@ -43,7 +43,6 @@ struct inode_security_struct {
 	u32 sid;		/* SID of this object */
 	u16 sclass;		/* security class of this object */
 	unsigned char initialized;	/* initialization flag */
-	unsigned char inherit;	/* inherit SID from parent entry */
 	struct mutex lock;
 };
 
