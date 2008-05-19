213060a4d6991a95d0b9344406d195be3464accf
Isaku Yamahata <yamahata@valinux.co.jp>
2008-05-19 22:13:40 +0900

    [IA64] pvops: paravirtualize NR_IRQS
    Make NR_IRQ overridable by each pv instances.
    Pv instance may need each own number of irqs so that
    NR_IRQS should be the maximum number of nr_irqs each
    pv instances need.
    
    Cc: Jes Sorensen <jes@sgi.com>
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- include/asm-ia64/irq.h
+++ include/asm-ia64/irq.h
@@ -13,14 +13,7 @@
 
 #include <linux/types.h>
 #include <linux/cpumask.h>
-
-#define NR_VECTORS	256
-
-#if (NR_VECTORS + 32 * NR_CPUS) < 1024
-#define NR_IRQS (NR_VECTORS + 32 * NR_CPUS)
-#else
-#define NR_IRQS 1024
-#endif
+#include <asm-ia64/nr-irqs.h>
 
 static __inline__ int
 irq_canonicalize (int irq)
