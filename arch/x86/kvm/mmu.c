3c9155106d589584f67b026ec444e69c4a68d7dc
Avi Kivity <avi@qumranet.com>
2008-05-20 16:21:13 +0300

    KVM: MMU: Fix is_empty_shadow_page() check
    The check is only looking at one of two possible empty ptes.
    
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- arch/x86/kvm/mmu.c
+++ arch/x86/kvm/mmu.c
@@ -658,7 +658,7 @@ static int is_empty_shadow_page(u64 *spt)
 	u64 *end;
 
 	for (pos = spt, end = pos + PAGE_SIZE / sizeof(u64); pos != end; pos++)
-		if (*pos != shadow_trap_nonpresent_pte) {
+		if (is_shadow_present_pte(*pos)) {
 			printk(KERN_ERR "%s: %p %llx\n", __func__,
 			       pos, *pos);
 			return 0;
