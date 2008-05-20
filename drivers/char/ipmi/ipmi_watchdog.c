af96f0103d332be92c42a44accf731da667ecc03
Arnd Bergmann <arnd@arndb.de>
2008-05-20 19:16:04 +0200

    ipmi-watchdog: BKL pushdown
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/char/ipmi/ipmi_watchdog.c
+++ drivers/char/ipmi/ipmi_watchdog.c
@@ -35,6 +35,7 @@
 #include <linux/moduleparam.h>
 #include <linux/ipmi.h>
 #include <linux/ipmi_smi.h>
+#include <linux/smp_lock.h>
 #include <linux/watchdog.h>
 #include <linux/miscdevice.h>
 #include <linux/init.h>
@@ -818,6 +819,8 @@ static int ipmi_open(struct inode *ino, struct file *filep)
 	case WATCHDOG_MINOR:
 		if (test_and_set_bit(0, &ipmi_wdog_open))
 			return -EBUSY;
+
+		cycle_kernel_lock();
 
 		/*
 		 * Don't start the timer now, let it start on the
