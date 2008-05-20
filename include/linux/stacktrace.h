acc6be5405b90c9f0fb0eb8a74ec4d4b7b5bf48f
Vegard Nossum <vegard.nossum@gmail.com>
2008-05-20 11:15:43 +0200

    x86: add save_stack_trace_bp() for tracing from a specific stack frame
    This will help kmemcheck (and possibly other debugging tools) since we
    can now simply pass regs->bp to the stack tracer instead of specifying
    the number of stack frames to skip, which is unreliable if gcc decides
    to inline functions, etc.
    
    Note that this makes the API incomplete for other architectures, but I
    expect that those can be updated lazily, e.g. when they need it.
    
    Cc: Arjan van de Ven <arjan@linux.intel.com>
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>

--- include/linux/stacktrace.h
+++ include/linux/stacktrace.h
@@ -11,6 +11,7 @@ struct stack_trace {
 };
 
 extern void save_stack_trace(struct stack_trace *trace);
+extern void save_stack_trace_bp(struct stack_trace *trace, unsigned long bp);
 extern void save_stack_trace_tsk(struct task_struct *tsk,
 				struct stack_trace *trace);
 
