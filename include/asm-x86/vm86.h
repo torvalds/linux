6330a30a76c1e62d4b4ec238368957f8febf9113
Vegard Nossum <vegard.nossum@gmail.com>
2008-05-28 09:46:19 +0200

    x86: break mutual header inclusion
    This breaks up the mutual inclusion between headers ptrace.h and vm86.h
    by moving some small part of vm86.h which is needed by ptrace.h into
    processor-flags.h.
    
    We also try to move #include lines to the top.
    
    This has been compile tested on x86_32 and x86_64 defconfig, and run
    through 'make headers_check'.
    
    Cc: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/vm86.h
+++ include/asm-x86/vm86.h
@@ -115,7 +115,6 @@ struct vm86plus_info_struct {
 	unsigned long is_vm86pus:1;	      /* for vm86 internal use */
 	unsigned char vm86dbg_intxxtab[32];   /* for debugger */
 };
-
 struct vm86plus_struct {
 	struct vm86_regs regs;
 	unsigned long flags;
@@ -128,11 +127,7 @@ struct vm86plus_struct {
 
 #ifdef __KERNEL__
 
-#ifdef CONFIG_VM86
-#define X86_VM_MASK	X86_EFLAGS_VM
-#else
-#define X86_VM_MASK	0 /* No VM86 support */
-#endif
+#include <asm/ptrace.h>
 
 /*
  * This is the (kernel) stack-layout when we have done a "SAVE_ALL" from vm86
@@ -142,7 +137,6 @@ struct vm86plus_struct {
  * at the end of the structure. Look at ptrace.h to see the "normal"
  * setup. For user space layout see 'struct vm86_regs' above.
  */
-#include <asm/ptrace.h>
 
 struct kernel_vm86_regs {
 /*
