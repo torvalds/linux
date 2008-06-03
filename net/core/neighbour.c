b9f5f52cca3e94f1e7509f366aa250ebbe1ed0b5
Stephen Hemminger <shemminger@vyatta.com>
2008-06-03 16:03:15 -0700

    net: neighbour table ABI problem
    The neighbor table time of last use information is returned in the
    incorrect unit. Kernel to user space ABI's need to use USER_HZ (or
    milliseconds), otherwise the application has to try and discover the
    real system HZ value which is problematic.  Linux has standardized on
    keeping USER_HZ consistent (100hz) even when kernel is running
    internally at some other value.
    
    This change is small, but it breaks the ABI for older version of
    iproute2 utilities.  But these utilities are already broken since they
    are looking at the psched_hz values which are completely different. So
    let's just go ahead and fix both kernel and user space. Older
    utilities will just print wrong values.
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/neighbour.c
+++ net/core/neighbour.c
@@ -2057,9 +2057,9 @@ static int neigh_fill_info(struct sk_buff *skb, struct neighbour *neigh,
 		goto nla_put_failure;
 	}
 
-	ci.ndm_used	 = now - neigh->used;
-	ci.ndm_confirmed = now - neigh->confirmed;
-	ci.ndm_updated	 = now - neigh->updated;
+	ci.ndm_used	 = jiffies_to_clock_t(now - neigh->used);
+	ci.ndm_confirmed = jiffies_to_clock_t(now - neigh->confirmed);
+	ci.ndm_updated	 = jiffies_to_clock_t(now - neigh->updated);
 	ci.ndm_refcnt	 = atomic_read(&neigh->refcnt) - 1;
 	read_unlock_bh(&neigh->lock);
 
