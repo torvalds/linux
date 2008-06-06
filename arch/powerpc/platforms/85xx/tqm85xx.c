6dd1b64a26d9209c09488e9fa257e7744823bf29
Wolfgang Grandegger <wg@grandegger.com>
2008-06-06 13:50:04 +0200

    powerpc/85xx: add board support for the TQM8548 modules
    This patch adds support for the TQM8548 modules from TQ-Components
    GmbH (http://www.tqc.de).
    
    Signed-off-by: Wolfgang Grandegger <wg@grandegger.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/platforms/85xx/tqm85xx.c
+++ arch/powerpc/platforms/85xx/tqm85xx.c
@@ -120,8 +120,18 @@ static void __init tqm85xx_setup_arch(void)
 #endif
 
 #ifdef CONFIG_PCI
-	for_each_compatible_node(np, "pci", "fsl,mpc8540-pci")
-		fsl_add_bridge(np, 1);
+	for_each_node_by_type(np, "pci") {
+		if (of_device_is_compatible(np, "fsl,mpc8540-pci") ||
+		    of_device_is_compatible(np, "fsl,mpc8548-pcie")) {
+			struct resource rsrc;
+			if (!of_address_to_resource(np, 0, &rsrc)) {
+				if ((rsrc.start & 0xfffff) == 0x8000)
+					fsl_add_bridge(np, 1);
+				else
+					fsl_add_bridge(np, 0);
+			}
+		}
+	}
 #endif
 }
 
@@ -167,6 +177,7 @@ static int __init tqm85xx_probe(void)
 
 	if ((of_flat_dt_is_compatible(root, "tqc,tqm8540")) ||
 	    (of_flat_dt_is_compatible(root, "tqc,tqm8541")) ||
+	    (of_flat_dt_is_compatible(root, "tqc,tqm8548")) ||
 	    (of_flat_dt_is_compatible(root, "tqc,tqm8555")) ||
 	    (of_flat_dt_is_compatible(root, "tqc,tqm8560")))
 		return 1;
