7c4285d8362641b815ea71018139619db965ab34
Sam Ravnborg <sam@ravnborg.org>
2008-06-06 21:06:11 +0200

    sparc: Merge asm-sparc{,64}/setup.h
    COMMAND_LINE_SIZE differ for 32 and 64 bit.
    256 versus 2048
    
    :$ diff -u include/asm-sparc/setup.h include/asm-sparc64/setup.h
    :-- include/asm-sparc/setup.h	2008-06-13 06:42:07.000000000 +0200
    :++ include/asm-sparc64/setup.h	2008-06-13 06:42:07.000000000 +0200
    :@@ -2,9 +2,9 @@
    :  *	Just a place holder.
    :  */
    :
    :-#ifndef _SPARC_SETUP_H
    :-#define _SPARC_SETUP_H
    :+#ifndef _SPARC64_SETUP_H
    :+#define _SPARC64_SETUP_H
    :
    :-#define COMMAND_LINE_SIZE	256
    :+#define COMMAND_LINE_SIZE	2048
    :
    :-#endif /* _SPARC_SETUP_H */
    :+#endif /* _SPARC64_SETUP_H */
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>

--- include/asm-sparc/setup.h
+++ include/asm-sparc/setup.h
@@ -5,6 +5,10 @@
 #ifndef _SPARC_SETUP_H
 #define _SPARC_SETUP_H
 
-#define COMMAND_LINE_SIZE	256
+#if defined(__sparc__) && defined(__arch64__)
+# define COMMAND_LINE_SIZE 2048
+#else
+# define COMMAND_LINE_SIZE 256
+#endif
 
 #endif /* _SPARC_SETUP_H */
--- include/asm-sparc64/setup.h
+++ include/asm-sparc64/setup.h
@@ -1,10 +1,1 @@
-/*
- *	Just a place holder. 
- */
-
-#ifndef _SPARC64_SETUP_H
-#define _SPARC64_SETUP_H
-
-#define COMMAND_LINE_SIZE	2048
-
-#endif /* _SPARC64_SETUP_H */
+#include <asm-sparc/setup.h>
