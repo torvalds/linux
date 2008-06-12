cfc53f65f56f9f33c0cf522124045ac5a64076b3
Philippe De Muyter <phdm@macqel.be>
2008-06-12 15:21:46 -0700

    driver/char/generic_nvram: fix banner
    The generic nvram driver announces itself as
    	'Macintosh non-volatile memory driver'
    instead of 'Generic non-volatile memory driver'.  Fix that.
    
    Signed-off-by: Philippe De Muyter <phdm@macqel.be>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Arjan van de Ven <arjan@linux.intel.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/char/generic_nvram.c
+++ drivers/char/generic_nvram.c
@@ -133,7 +133,7 @@ static struct miscdevice nvram_dev = {
 
 int __init nvram_init(void)
 {
-	printk(KERN_INFO "Macintosh non-volatile memory driver v%s\n",
+	printk(KERN_INFO "Generic non-volatile memory driver v%s\n",
 		NVRAM_VERSION);
 	return misc_register(&nvram_dev);
 }
