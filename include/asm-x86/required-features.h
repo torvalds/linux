4f30cb0262847392d8d006042f24bd90abd24f9d
Jeremy Fitzhardinge <jeremy@goop.org>
2008-06-25 00:19:21 -0400

    x86, 64-bit: PSE no longer a hard requirement
    Because Xen doesn't support PSE mappings in guests, all code which
    assumed the presence of PSE has been changed to fall back to smaller
    mappings if necessary.  As a result, PSE is optional rather than
    required (though still used whereever possible).
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    Cc: xen-devel <xen-devel@lists.xensource.com>
    Cc: Stephen Tweedie <sct@redhat.com>
    Cc: Eduardo Habkost <ehabkost@redhat.com>
    Cc: Mark McLoughlin <markmc@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/required-features.h
+++ include/asm-x86/required-features.h
@@ -42,7 +42,7 @@
 #endif
 
 #ifdef CONFIG_X86_64
-#define NEED_PSE	(1<<(X86_FEATURE_PSE & 31))
+#define NEED_PSE	0
 #define NEED_MSR	(1<<(X86_FEATURE_MSR & 31))
 #define NEED_PGE	(1<<(X86_FEATURE_PGE & 31))
 #define NEED_FXSR	(1<<(X86_FEATURE_FXSR & 31))
