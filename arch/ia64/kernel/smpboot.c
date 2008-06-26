f27b433ef32a77c8cb76f018507453df7c03e552
Jens Axboe <jens.axboe@oracle.com>
2008-06-26 11:22:30 +0200

    ia64: convert to generic helpers for IPI function calls
    This converts ia64 to use the new helpers for smp_call_function() and
    friends, and adds support for smp_call_function_single().
    
    Cc: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- arch/ia64/kernel/smpboot.c
+++ arch/ia64/kernel/smpboot.c
@@ -395,14 +395,14 @@ smp_callin (void)
 
 	fix_b0_for_bsp();
 
-	lock_ipi_calllock();
+	ipi_call_lock_irq();
 	spin_lock(&vector_lock);
 	/* Setup the per cpu irq handling data structures */
 	__setup_vector_irq(cpuid);
 	cpu_set(cpuid, cpu_online_map);
 	per_cpu(cpu_state, cpuid) = CPU_ONLINE;
 	spin_unlock(&vector_lock);
-	unlock_ipi_calllock();
+	ipi_call_unlock_irq();
 
 	smp_setup_percpu_timer();
 
