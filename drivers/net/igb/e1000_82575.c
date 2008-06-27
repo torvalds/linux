5c3cad754cba4252f5b4e74fb217e3bdd0c03662
Jeff Kirsher <jeffrey.t.kirsher@intel.com>
2008-06-27 10:59:33 -0700

    igb: limit EEPROM access
    EEPROM access beyond the base eeprom page is unsupported.
    
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Auke Kok <auke-jan.h.kok@intel.com>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/net/igb/e1000_82575.c
+++ drivers/net/igb/e1000_82575.c
@@ -171,6 +171,10 @@ static s32 igb_get_invariants_82575(struct e1000_hw *hw)
 	 * for setting word_size.
 	 */
 	size += NVM_WORD_SIZE_BASE_SHIFT;
+
+	/* EEPROM access above 16k is unsupported */
+	if (size > 14)
+		size = 14;
 	nvm->word_size = 1 << size;
 
 	/* setup PHY parameters */
