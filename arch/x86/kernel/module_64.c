5e5a29bf2624a5984e1c36c3a2481ee91249ec9c
Anders H Kaseorg <andersk@MIT.EDU>
2008-06-28 18:25:41 -0400

    x86, 64-bit: patch paravirt inline replacements when loading modules
    small speedup.
    
    Paravirt replacements were added to the i386 module loader by commit
    139ec7c416248b9ea227d21839235344edfee1e0.  This adds the same code to
    the x86_64 module loader.
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>
    Acked-by: "H. Peter Anvin" <hpa@zytor.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/module_64.c
+++ arch/x86/kernel/module_64.c
@@ -150,7 +150,8 @@ int module_finalize(const Elf_Ehdr *hdr,
                     const Elf_Shdr *sechdrs,
                     struct module *me)
 {
-	const Elf_Shdr *s, *text = NULL, *alt = NULL, *locks = NULL;
+	const Elf_Shdr *s, *text = NULL, *alt = NULL, *locks = NULL,
+		*para = NULL;
 	char *secstrings = (void *)hdr + sechdrs[hdr->e_shstrndx].sh_offset;
 
 	for (s = sechdrs; s < sechdrs + hdr->e_shnum; s++) {
@@ -160,6 +161,8 @@ int module_finalize(const Elf_Ehdr *hdr,
 			alt = s;
 		if (!strcmp(".smp_locks", secstrings + s->sh_name))
 			locks= s;
+		if (!strcmp(".parainstructions", secstrings + s->sh_name))
+			para = s;
 	}
 
 	if (alt) {
@@ -173,6 +176,11 @@ int module_finalize(const Elf_Ehdr *hdr,
 		alternatives_smp_module_add(me, me->name,
 					    lseg, lseg + locks->sh_size,
 					    tseg, tseg + text->sh_size);
+	}
+
+	if (para) {
+		void *pseg = (void *)para->sh_addr;
+		apply_paravirt(pseg, pseg + para->sh_size);
 	}
 
 	return module_bug_finalize(hdr, sechdrs, me);
