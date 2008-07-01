ec04fd60fd74a9db9c63fe11d519be3642cabfdd
Rick Farrington <rickdic@hotmail.com>
2008-07-01 09:20:33 +0800

    iwlwifi: fix incorrect 5GHz rates reported in monitor mode
    This patch fixes the rates reported in monitor mode operation
    (Wireshark) for iwlwifi.
    
    Previously, packets with rates of 6M..24M would be reported
    incorrectly and packets with rates of 36M..54M would not passed
    up the stack.
    
    Signed-off-by: Rick Farrington <rickdic@hotmail.com>
    Signed-off-by: Zhu Yi <yi.zhu@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-3945.c
+++ drivers/net/wireless/iwlwifi/iwl-3945.c
@@ -588,8 +588,12 @@ static void iwl3945_add_radiotap(struct iwl3945_priv *priv,
 
 	if (rate == -1)
 		iwl3945_rt->rt_rate = 0;
-	else
+	else {
+		if (stats->band == IEEE80211_BAND_5GHZ)
+			rate += IWL_FIRST_OFDM_RATE;
+
 		iwl3945_rt->rt_rate = iwl3945_rates[rate].ieee;
+	}
 
 	/* antenna number */
 	antenna = phy_flags_hw & RX_RES_PHY_FLAGS_ANTENNA_MSK;
