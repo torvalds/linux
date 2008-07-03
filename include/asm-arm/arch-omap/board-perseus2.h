44f78f43b349d19d378a996c4a2d9fcdff771b1e
Tony Lindgren <tony@atomide.com>
2008-07-03 12:24:41 +0300

    ARM: OMAP: Clean up interrupt lines to fix warnings for multi-omap
    If boards with different NR_IRQS are compiled together, tons of
    compiler warnings are emitted about redefining NR_IRQS.
    
    This patch fixes the problem by adding up NR_IRQS in a common place.
    
    Patch also removes quite a bit of now unnecessary code.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- include/asm-arm/arch-omap/board-perseus2.h
+++ include/asm-arm/arch-omap/board-perseus2.h
@@ -36,10 +36,4 @@
 #define OMAP_SDRAM_DEVICE		D256M_1X16_4B
 #endif
 
-#define MAXIRQNUM		IH_BOARD_BASE
-#define MAXFIQNUM		MAXIRQNUM
-#define MAXSWINUM		MAXIRQNUM
-
-#define NR_IRQS			(MAXIRQNUM + 1)
-
 #endif
