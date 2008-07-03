4ad88901dd675acb9c8d1eca1f083c3d22cbbd4d
Adrian Bunk <bunk@kernel.org>
2008-07-03 12:10:52 -0400

    Input: q40kbd - make q40kbd_lock static
    This patch makes the needlessly global q40kbd_lock static.
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/serio/q40kbd.c
+++ drivers/input/serio/q40kbd.c
@@ -47,7 +47,7 @@ MODULE_AUTHOR("Vojtech Pavlik <vojtech@ucw.cz>");
 MODULE_DESCRIPTION("Q40 PS/2 keyboard controller driver");
 MODULE_LICENSE("GPL");
 
-DEFINE_SPINLOCK(q40kbd_lock);
+static DEFINE_SPINLOCK(q40kbd_lock);
 static struct serio *q40kbd_port;
 static struct platform_device *q40kbd_device;
 
