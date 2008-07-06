4b5a698ef423eebc37cfacc6d3376d6dffd5bf83
Patrick McHardy <kaber@trash.net>
2008-07-06 15:49:08 -0700

    net: fix dev_set_promiscuity() breakage
    Commit dad9b335 (netdevice: Fix promiscuity and allmulti overflow) broke
    dev_set_promiscuity() by returning on success without reprogramming the
    device.
    
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -2859,7 +2859,7 @@ int dev_set_promiscuity(struct net_device *dev, int inc)
 	int err;
 
 	err = __dev_set_promiscuity(dev, inc);
-	if (!err)
+	if (err < 0)
 		return err;
 	if (dev->flags != old_flags)
 		dev_set_rx_mode(dev);
