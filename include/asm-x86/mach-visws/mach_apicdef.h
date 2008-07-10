b70d9c2473ffbe327f5d7364bfbf67e94c440af8
Ingo Molnar <mingo@elte.hu>
2008-07-10 15:12:05 +0200

    x86, VisWS: turn into generic arch, update asm-x86/mach-visws/mach_apicdef.h
    update asm-x86/mach-visws/mach_apicdef.h to the generic version.
    
    This should work fine as VISWS has a standard local APIC and thus
    its mach_apicdef.h copy is just an ancient version of the generic code.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/mach-visws/mach_apicdef.h
+++ include/asm-x86/mach-visws/mach_apicdef.h
@@ -1,12 +1,24 @@
 #ifndef __ASM_MACH_APICDEF_H
 #define __ASM_MACH_APICDEF_H
 
-#define         APIC_ID_MASK            (0xF<<24)
+#include <asm/apic.h>
 
-static inline unsigned get_apic_id(unsigned long x)
+#ifdef CONFIG_X86_64
+#define	APIC_ID_MASK		(0xFFu<<24)
+#define GET_APIC_ID(x)          (((x)>>24)&0xFFu)
+#define	SET_APIC_ID(x)		(((x)<<24))
+#else
+#define		APIC_ID_MASK		(0xF<<24)
+static inline unsigned get_apic_id(unsigned long x) 
 {
-	        return (((x)>>24)&0xF);
-}
-#define         GET_APIC_ID(x)  get_apic_id(x)
+	unsigned int ver = GET_APIC_VERSION(apic_read(APIC_LVR));
+	if (APIC_XAPIC(ver))
+		return (((x)>>24)&0xFF);
+	else
+		return (((x)>>24)&0xF);
+} 
+
+#define		GET_APIC_ID(x)	get_apic_id(x)
+#endif
 
 #endif
