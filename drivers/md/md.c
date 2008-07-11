05710466c9ef2e3ee55166934c801a2393c32f80
Andre Noll <maan@systemlinux.org>
2008-07-11 22:02:20 +1000

    md: Simplify uuid_equal().
    Signed-off-by: Andre Noll <maan@systemlinux.org>
    Signed-off-by: Neil Brown <neilb@suse.de>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -543,16 +543,11 @@ fail:
 
 static int uuid_equal(mdp_super_t *sb1, mdp_super_t *sb2)
 {
-	if (	(sb1->set_uuid0 == sb2->set_uuid0) &&
-		(sb1->set_uuid1 == sb2->set_uuid1) &&
-		(sb1->set_uuid2 == sb2->set_uuid2) &&
-		(sb1->set_uuid3 == sb2->set_uuid3))
-
-		return 1;
-
-	return 0;
+	return 	sb1->set_uuid0 == sb2->set_uuid0 &&
+		sb1->set_uuid1 == sb2->set_uuid1 &&
+		sb1->set_uuid2 == sb2->set_uuid2 &&
+		sb1->set_uuid3 == sb2->set_uuid3;
 }
-
 
 static int sb_equal(mdp_super_t *sb1, mdp_super_t *sb2)
 {
