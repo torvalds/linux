caadbdce240c43e3e46c82fce6c00eb7f01e1beb
Yinghai Lu <yhlu.kernel@gmail.com>
2008-07-15 00:03:44 -0700

    x86: enable memory tester support on 32-bit
    only supports memory below max_low_pfn.
    
    Signed-off-by: Yinghai Lu <yhlu.kernel@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/mm/init_32.c
+++ arch/x86/mm/init_32.c
@@ -844,6 +844,9 @@ unsigned long __init_refok init_memory_mapping(unsigned long start,
 		reserve_early(table_start << PAGE_SHIFT,
 				 table_end << PAGE_SHIFT, "PGTABLE");
 
+	if (!after_init_bootmem)
+		early_memtest(start, end);
+
 	return end >> PAGE_SHIFT;
 }
 
