41ab8592ca35a20580665cae18c172816236b21e
Alexey Dobriyan <adobriyan@gmail.com>
2008-07-16 21:29:02 +0400

    SLUB: simplify re on_each_cpu()
    on_each_cpu() expands to function call on UP, too.
    
    Acked-by: Christoph Lameter <cl@linux-foundation.org>
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- mm/slub.c
+++ mm/slub.c
@@ -1495,15 +1495,7 @@ static void flush_cpu_slab(void *d)
 
 static void flush_all(struct kmem_cache *s)
 {
-#ifdef CONFIG_SMP
 	on_each_cpu(flush_cpu_slab, s, 1);
-#else
-	unsigned long flags;
-
-	local_irq_save(flags);
-	flush_cpu_slab(s);
-	local_irq_restore(flags);
-#endif
 }
 
 /*
