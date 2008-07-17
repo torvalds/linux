8a34c5dc3a7c6431f1cd94c0904be81b296e08ca
David S. Miller <davem@davemloft.net>
2008-07-17 00:47:45 -0700

    pkt_sched: Perform bulk of qdisc destruction in RCU.
    This allows less strict control of access to the qdisc attached to a
    netdev_queue.  It is even allowed to enqueue into a qdisc which is
    in the process of being destroyed.  The RCU handler will toss out
    those packets.
    
    We will need this to handle sharing of a qdisc amongst multiple
    TX queues.  In such a setup the lock has to be shared, so will
    be inside of the qdisc itself.  At which point the netdev_queue
    lock cannot be used to hard synchronize access to the ->qdisc
    pointer.
    
    One operation we have to keep inside of qdisc_destroy() is the list
    deletion.  It is the only piece of state visible after the RCU quiesce
    period, so we have to undo it early and under the appropriate locking.
    
    The operations in the RCU handler do not need any looking because the
    qdisc tree is no longer visible to anything at that point.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/sch_generic.c
+++ net/sched/sch_generic.c
@@ -545,20 +545,8 @@ EXPORT_SYMBOL(qdisc_reset);
 static void __qdisc_destroy(struct rcu_head *head)
 {
 	struct Qdisc *qdisc = container_of(head, struct Qdisc, q_rcu);
-	kfree((char *) qdisc - qdisc->padded);
-}
-
-/* Under queue->lock and BH! */
-
-void qdisc_destroy(struct Qdisc *qdisc)
-{
 	const struct Qdisc_ops  *ops = qdisc->ops;
 
-	if (qdisc->flags & TCQ_F_BUILTIN ||
-	    !atomic_dec_and_test(&qdisc->refcnt))
-		return;
-
-	list_del(&qdisc->list);
 	gen_kill_estimator(&qdisc->bstats, &qdisc->rate_est);
 	if (ops->reset)
 		ops->reset(qdisc);
@@ -567,6 +555,20 @@ void qdisc_destroy(struct Qdisc *qdisc)
 
 	module_put(ops->owner);
 	dev_put(qdisc_dev(qdisc));
+
+	kfree((char *) qdisc - qdisc->padded);
+}
+
+/* Under queue->lock and BH! */
+
+void qdisc_destroy(struct Qdisc *qdisc)
+{
+	if (qdisc->flags & TCQ_F_BUILTIN ||
+	    !atomic_dec_and_test(&qdisc->refcnt))
+		return;
+
+	list_del(&qdisc->list);
+
 	call_rcu(&qdisc->q_rcu, __qdisc_destroy);
 }
 EXPORT_SYMBOL(qdisc_destroy);
