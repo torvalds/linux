2fb42b11f61cbcef7dfc225c1d26c4511436583d
Adrian Hunter <ext-adrian.hunter@nokia.com>
2008-07-18 17:56:37 +0300

    UBIFS: ensure UBIFS switches to read-only on error
    UBI transparently handles write errors by automatically copying
    and remapping the affected eraseblock. If UBI is unable to do
    that, for example its pool of eraseblocks reserved for bad block
    handling is empty, then the error is propagated to UBIFS. UBIFS
    must protect the media from falling into an inconsistent state
    by immediately switching to read-only mode. In the case of log
    updates, this was not being done.
    
    Signed-off-by: Adrian Hunter <ext-adrian.hunter@nokia.com>

--- fs/ubifs/log.c
+++ fs/ubifs/log.c
@@ -317,6 +317,8 @@ int ubifs_add_bud_to_log(struct ubifs_info *c, int jhead, int lnum, int offs)
 	return 0;
 
 out_unlock:
+	if (err != -EAGAIN)
+		ubifs_ro_mode(c, err);
 	mutex_unlock(&c->log_mutex);
 	kfree(ref);
 	kfree(bud);
