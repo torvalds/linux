ba42059fbd0aa1ac91b582412b5fedb1258f241f
Ingo Molnar <mingo@elte.hu>
2008-07-20 11:02:06 +0200

    sched: hrtick_enabled() should use cpu_active()
    Peter pointed out that hrtick_enabled() should use cpu_active().
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched.c
+++ kernel/sched.c
@@ -1006,7 +1006,7 @@ static inline int hrtick_enabled(struct rq *rq)
 {
 	if (!sched_feat(HRTICK))
 		return 0;
-	if (!cpu_online(cpu_of(rq)))
+	if (!cpu_active(cpu_of(rq)))
 		return 0;
 	return hrtimer_is_hres_active(&rq->hrtick_timer);
 }
