175ff20f6a681effa82d67e3656c4cc3e7117600
Marc Zyngier <maz@misterjones.org>
2008-07-22 16:59:44 +0200

    Add support for the Arcom/Eurotech Viper SBC onboard ethernet.
    Signed-off-by: Marc Zyngier <marc.zyngier@altran.com>

--- drivers/net/smc91x.h
+++ drivers/net/smc91x.h
@@ -43,7 +43,8 @@
 #if defined(CONFIG_ARCH_LUBBOCK) ||\
     defined(CONFIG_MACH_MAINSTONE) ||\
     defined(CONFIG_MACH_ZYLONITE) ||\
-    defined(CONFIG_MACH_LITTLETON)
+    defined(CONFIG_MACH_LITTLETON) ||\
+    defined(CONFIG_ARCH_VIPER)
 
 #include <asm/mach-types.h>
 
