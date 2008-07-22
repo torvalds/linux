026e2c05ef58ef413e2d52696f125d5ea1aa8bce
Ingo Molnar <mingo@elte.hu>
2008-07-22 11:58:14 +0200

    x86, cyrix: debug
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/processor-cyrix.h
+++ include/asm-x86/processor-cyrix.h
@@ -28,3 +28,11 @@ static inline void setCx86(u8 reg, u8 data)
 	outb(reg, 0x22);
 	outb(data, 0x23);
 }
+
+#define getCx86_old(reg) ({ outb((reg), 0x22); inb(0x23); })
+
+#define setCx86_old(reg, data) do { \
+	outb((reg), 0x22); \
+	outb((data), 0x23); \
+} while (0)
+
