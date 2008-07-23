d8e64406a037a64444175730294e449c9e21f5ec
Dan Williams <dan.j.williams@intel.com>
2008-07-23 13:09:48 -0700

    md: delay notification of 'active_idle' to the recovery thread
    sysfs_notify might sleep, so do not call it from md_safemode_timeout.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -3483,7 +3483,7 @@ static void md_safemode_timeout(unsigned long data)
 	if (!atomic_read(&mddev->writes_pending)) {
 		mddev->safemode = 1;
 		if (mddev->external)
-			sysfs_notify(&mddev->kobj, NULL, "array_state");
+			set_bit(MD_NOTIFY_ARRAY_STATE, &mddev->flags);
 	}
 	md_wakeup_thread(mddev->thread);
 }
@@ -6050,6 +6050,9 @@ void md_check_recovery(mddev_t *mddev)
 
 	if (mddev->bitmap)
 		bitmap_daemon_work(mddev->bitmap);
+
+	if (test_and_clear_bit(MD_NOTIFY_ARRAY_STATE, &mddev->flags))
+		sysfs_notify(&mddev->kobj, NULL, "array_state");
 
 	if (mddev->ro)
 		return;
