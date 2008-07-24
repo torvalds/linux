e5b13acf563e98ffc5dea8bebf80299a2a4ea088
Mauro Carvalho Chehab <mchehab@infradead.org>
2008-07-24 14:37:55 -0300

    V4L/DVB (8451): dw2102: fix in-kernel compilation
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-usb/dw2102.h
+++ drivers/media/dvb/dvb-usb/dw2102.h
@@ -2,7 +2,7 @@
 #define _DW2102_H_
 
 #define DVB_USB_LOG_PREFIX "dw2102"
-#include <dvb-usb.h>
+#include "dvb-usb.h"
 
 extern int dvb_usb_dw2102_debug;
 #define deb_xfer(args...) dprintk(dvb_usb_dw2102_debug, 0x02, args)
