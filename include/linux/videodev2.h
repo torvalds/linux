c1d7f4f1648cb8efd87f1b9560c40af2297e7c05
Hans Verkuil <hverkuil@xs4all.nl>
2008-07-26 08:33:47 -0300

    V4L/DVB (8524): videodev: copy the VID_TYPE defines to videodev.h
    The VID_TYPE defines are V4L1 specific, so copy them back to videodev.h.
    In videodev2.h ensure that they are not used in the kernel (you need
    to include videodev.h instead) and mark them are deprecated.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- include/linux/videodev2.h
+++ include/linux/videodev2.h
@@ -71,6 +71,11 @@
  */
 #define VIDEO_MAX_FRAME               32
 
+#ifndef __KERNEL__
+
+/* These defines are V4L1 specific and should not be used with the V4L2 API!
+   They will be removed from this header in the future. */
+
 #define VID_TYPE_CAPTURE	1	/* Can capture */
 #define VID_TYPE_TUNER		2	/* Can tune */
 #define VID_TYPE_TELETEXT	4	/* Does teletext */
@@ -85,6 +90,7 @@
 #define VID_TYPE_MPEG_ENCODER	2048	/* Can encode MPEG streams */
 #define VID_TYPE_MJPEG_DECODER	4096	/* Can decode MJPEG streams */
 #define VID_TYPE_MJPEG_ENCODER	8192	/* Can encode MJPEG streams */
+#endif
 
 /*
  *	M I S C E L L A N E O U S
