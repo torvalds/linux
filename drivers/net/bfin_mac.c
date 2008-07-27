ee02fee8f698aee72f43b3ee5fd818393b110402
Michael Hennerich <michael.hennerich@analog.com>
2008-07-27 22:45:05 +0800

    Blackfin EMAC Driver: Functional power management support
    Reprogram MAC address after resume from Suspend Mem
    (Blackfin Hibernate looses all CORE and SYSTEM register content)
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    Signed-off-by: Bryan Wu <cooloney@kernel.org>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/net/bfin_mac.c
+++ drivers/net/bfin_mac.c
@@ -971,6 +971,7 @@ static int bfin_mac_open(struct net_device *dev)
 	phy_start(lp->phydev);
 	phy_write(lp->phydev, MII_BMCR, BMCR_RESET);
 	setup_system_regs(dev);
+	setup_mac_addr(dev->dev_addr);
 	bfin_mac_disable();
 	bfin_mac_enable();
 	pr_debug("hardware init finished\n");
