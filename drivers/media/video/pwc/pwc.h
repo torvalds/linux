38413fd2d82b0e75ae0492518f1b80a5cfd81956
Mauro Carvalho Chehab <mchehab@infradead.org>
2008-07-27 19:02:30 -0300

    V4L/DVB (8548): pwc: Fix compilation
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/pwc/pwc.h
+++ drivers/media/video/pwc/pwc.h
@@ -32,6 +32,7 @@
 #include <linux/smp_lock.h>
 #include <linux/version.h>
 #include <linux/mutex.h>
+#include <linux/mm.h>
 #include <asm/errno.h>
 #include <linux/videodev.h>
 #include <media/v4l2-common.h>
