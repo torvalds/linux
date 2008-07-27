f3409f71a76838b1bc985f753eed787a3f17bc2c
Mauro Carvalho Chehab <mchehab@infradead.org>
2008-07-27 19:30:46 -0300

    V4L/DVB (8549): mxl5007: Fix an error at include file
    mxl5007 was forcing for its compilation:
    
    In file included from drivers/media/common/tuners/mxl5007t.c:25:drivers/media/common/tuners/mxl5007t.h:80:1: warning: "CONFIG_MEDIA_TUNER_MXL5007T" redefined
    In file included from <command-line>:0:
    ./include/linux/autoconf.h:2782:1: warning: this is the location of the previous definition
    
    Probably, some temporary hack for testing.
    
    Cc: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/common/tuners/mxl5007t.h
+++ drivers/media/common/tuners/mxl5007t.h
@@ -77,7 +77,6 @@ struct mxl5007t_config {
 	unsigned int clk_out_enable:1;
 };
 
-#define CONFIG_MEDIA_TUNER_MXL5007T
 #if defined(CONFIG_MEDIA_TUNER_MXL5007T) || (defined(CONFIG_MEDIA_TUNER_MXL5007T_MODULE) && defined(MODULE))
 extern struct dvb_frontend *mxl5007t_attach(struct dvb_frontend *fe,
 					    struct i2c_adapter *i2c, u8 addr,
