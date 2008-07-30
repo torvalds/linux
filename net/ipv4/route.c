a1bc6eb4b499ae67ada9a01660010580b6569403
Al Viro <viro@zeniv.linux.org.uk>
2008-07-30 06:32:52 -0400

    [PATCH] ipv4_static_sysctl_init() should be under CONFIG_SYSCTL
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -3216,6 +3216,7 @@ int __init ip_rt_init(void)
 	return rc;
 }
 
+#ifdef CONFIG_SYSCTL
 /*
  * We really need to sanitize the damn ipv4 init order, then all
  * this nonsense will go away.
@@ -3224,6 +3225,7 @@ void __init ip_static_sysctl_init(void)
 {
 	register_sysctl_paths(ipv4_route_path, ipv4_route_table);
 }
+#endif
 
 EXPORT_SYMBOL(__ip_select_ident);
 EXPORT_SYMBOL(ip_route_input);
