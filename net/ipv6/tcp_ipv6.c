00b1304c4ca81dd893973cc620b87a5c3ff3f660
Adam Langley <agl@imperialviolet.org>
2008-07-31 21:36:07 -0700

    tcp: MD5: Fix IPv6 signatures
    Reported by Stefanos Harhalakis; although 2.6.27-rc1 talks to itself using IPv6
    TCP MD5 packets just fine, Stefanos noted that tcpdump claimed that the
    signatures were invalid.
    
    I broke this in 49a72dfb8814c2d65bd9f8c9c6daf6395a1ec58d ("tcp: Fix MD5
    signatures for non-linear skbs"), it was just a typo.
    
    Note that tcpdump will still sometimes claim that the signatures are incorrect.
    A patch to tcpdump has been submitted for this[1].
    
    [1] http://tinyurl.com/6a4fl2
    
    Signed-off-by: Adam Langley <agl@imperialviolet.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -748,7 +748,7 @@ static int tcp_v6_md5_hash_pseudoheader(struct tcp_md5sig_pool *hp,
 	ipv6_addr_copy(&bp->saddr, saddr);
 	ipv6_addr_copy(&bp->daddr, daddr);
 	bp->protocol = cpu_to_be32(IPPROTO_TCP);
-	bp->len = cpu_to_be16(nbytes);
+	bp->len = cpu_to_be32(nbytes);
 
 	sg_init_one(&sg, bp, sizeof(*bp));
 	return crypto_hash_update(&hp->md5_desc, &sg, sizeof(*bp));
