f6ed6f78d46b43b6d908b39ed3322f7cda23f4a8
Pieter du Preez <pdupreez@gmail.com>
2008-08-01 10:06:40 +0100

    Fix rename of at91_nand -> atmel_nand
    Structs called at91_nand_data where renamed to atmel_nand_data
    and configs called *MTD_NAND_AT91* where renamed to
    *MTD_NAND_ATMEL*. This was unfortunately not done consistently,
    causing NAND chips not being initialised on several ARM boards.
    
    I am aware that the author of the original change did not rename
    MTD_NAND_AT91_BUSWIDTH to MTD_NAND_ATMEL_BUSWIDTH, for example.
    All *MTD_NAND_AT91* where renamed to *MTD_NAND_ATMEL* in order
    to keep naming consistency.
    
    This patch was only tested on a MACH_SAM9_L9260, as this is the
    only ARM board I have to my disposal.
    
    Before this patch:
    
    $ git-ls-files |xargs grep atmel_nand |wc -l
    105
    $ git-ls-files |xargs grep at91_nand |wc -l
    4
    $ git-ls-files |xargs grep MTD_NAND_ATMEL |wc -l
    8
    $ git-ls-files |xargs grep MTD_NAND_AT91 |wc -l
    47
    
    After this patch:
    
    $ git-ls-files |xargs grep atmel_nand |wc -l
    109
    $ git-ls-files |xargs grep at91_nand |wc -l
    0
    $ git-ls-files |xargs grep MTD_NAND_ATMEL |wc -l
    55
    $ git-ls-files |xargs grep MTD_NAND_AT91 |wc -l
    0
    
    Signed-off-by: Pieter du Preez <pdupreez@gmail.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- arch/arm/mach-at91/board-sam9g20ek.c
+++ arch/arm/mach-at91/board-sam9g20ek.c
@@ -143,13 +143,13 @@ static struct mtd_partition * __init nand_partitions(int size, int *num_partitio
 }
 
 /* det_pin is not connected */
-static struct at91_nand_data __initdata ek_nand_data = {
+static struct atmel_nand_data __initdata ek_nand_data = {
 	.ale		= 21,
 	.cle		= 22,
 	.rdy_pin	= AT91_PIN_PC13,
 	.enable_pin	= AT91_PIN_PC14,
 	.partition_info	= nand_partitions,
-#if defined(CONFIG_MTD_NAND_AT91_BUSWIDTH_16)
+#if defined(CONFIG_MTD_NAND_ATMEL_BUSWIDTH_16)
 	.bus_width_16	= 1,
 #else
 	.bus_width_16	= 0,
