8a4fbe013fb6a2a65a4eddcddb888ebe38d0270d
Ian Molton <spyro@f2s.com>
2008-08-04 18:06:18 +0200

    mfd: t7l66 MMC platform data
    The tmio MMC driver needs the cell to be passed as a platform data.
    
    Signed-off-by: Samuel Ortiz <sameo@openedhand.com>

--- drivers/mfd/t7l66xb.c
+++ drivers/mfd/t7l66xb.c
@@ -343,6 +343,11 @@ static int t7l66xb_probe(struct platform_device *dev)
 	t7l66xb_cells[T7L66XB_CELL_NAND].data_size =
 		sizeof(t7l66xb_cells[T7L66XB_CELL_NAND]);
 
+	t7l66xb_cells[T7L66XB_CELL_MMC].platform_data =
+		&t7l66xb_cells[T7L66XB_CELL_MMC];
+	t7l66xb_cells[T7L66XB_CELL_MMC].data_size =
+		sizeof(t7l66xb_cells[T7L66XB_CELL_MMC]);
+
 	ret = mfd_add_devices(&dev->dev, dev->id,
 			      t7l66xb_cells, ARRAY_SIZE(t7l66xb_cells),
 			      iomem, t7l66xb->irq_base);
