430cd47fa98178b5199105fab4fb74e04bd0635c
Larry Finger <Larry.Finger@lwfinger.net>
2008-08-14 18:57:11 -0500

    b43: Fix for another Bluetooth Coexistence SPROM Programming error for BCM4306
    In trying to help users on the Ubuntu Bugzilla, I discovered another
    BCM4306 with the Bluetooth Coexistence programming error in the SPROM.
    
    This patch is contingent on the one that added the Linksys device with
    subdevice code of 0x0014.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Cc: Stable <stable@kernel.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/b43/main.c
+++ drivers/net/wireless/b43/main.c
@@ -4615,6 +4615,7 @@ static void b43_sprom_fixup(struct ssb_bus *bus)
 	if (bus->bustype == SSB_BUSTYPE_PCI) {
 		pdev = bus->host_pci;
 		if (IS_PDEV(pdev, BROADCOM, 0x4318, ASUSTEK, 0x100F) ||
+		    IS_PDEV(pdev, BROADCOM, 0x4320,    DELL, 0x0003) ||
 		    IS_PDEV(pdev, BROADCOM, 0x4320, LINKSYS, 0x0015) ||
 		    IS_PDEV(pdev, BROADCOM, 0x4320, LINKSYS, 0x0014) ||
 		    IS_PDEV(pdev, BROADCOM, 0x4320, LINKSYS, 0x0013))
