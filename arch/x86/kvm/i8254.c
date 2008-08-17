dc7404cea34ef997dfe89ca94d16358e9d29c8d8
Avi Kivity <avi@qumranet.com>
2008-08-17 16:03:46 +0300

    KVM: Handle spurious acks for PIT interrupts
    Spurious acks can be generated, for example if the PIC is being reset.
    Handle those acks gracefully rather than flooding the log with warnings.
    
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- arch/x86/kvm/i8254.c
+++ arch/x86/kvm/i8254.c
@@ -228,7 +228,7 @@ void kvm_pit_ack_irq(struct kvm_irq_ack_notifier *kian)
 						 irq_ack_notifier);
 	spin_lock(&ps->inject_lock);
 	if (atomic_dec_return(&ps->pit_timer.pending) < 0)
-		WARN_ON(1);
+		atomic_inc(&ps->pit_timer.pending);
 	ps->irq_ack = 1;
 	spin_unlock(&ps->inject_lock);
 }
