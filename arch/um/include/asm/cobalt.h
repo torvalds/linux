1de1502c96c3c163c06419bfe838c73632fa8d9c
Al Viro <viro@zeniv.linux.org.uk>
2008-08-17 22:54:55 -0400

    x86, um: now we can get rid of trivial uml headers
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/um/include/asm/cobalt.h
+++
@@ -1,6 +0,0 @@
-#ifndef __UM_COBALT_H
-#define __UM_COBALT_H
-
-#include "asm/arch/cobalt.h"
-
-#endif
--- arch/um/include/asm/msgbuf.h
+++
@@ -1,6 +0,0 @@
-#ifndef __UM_MSGBUF_H
-#define __UM_MSGBUF_H
-
-#include "asm/arch/msgbuf.h"
-
-#endif
