615f996fb8185a0bc02812ebd72cb77ded5645f1
David Woodhouse <David.Woodhouse@intel.com>
2008-08-19 19:21:57 +0100

    Switch btrfs_name_hash() to crc32c
    Date: Tue, 19 Aug 2008 19:21:57 +0100
    Using a 64-bit hash as the readdir cookie is just asking for trouble.
    And gets it, when we try to export the file system by NFS.
    
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/hash.h
+++ fs/btrfs/hash.h
@@ -18,5 +18,10 @@
 
 #ifndef __HASH__
 #define __HASH__
-u64 btrfs_name_hash(const char *name, int len);
+
+#include "crc32c.h"
+static inline u64 btrfs_name_hash(const char *name, int len)
+{
+	return btrfs_crc32c((u32)~1, name, len);
+}
 #endif
