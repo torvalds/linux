a16b20da879430fdf245ed45461ed40ffef8db3c
Avi Kivity <avi@qumranet.com>
2008-08-20 15:48:27 +0300

    KVM: VMX: Change segment dpl at reset to 3
    This is more emulation friendly, if not 100% correct.
    
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -1991,7 +1991,7 @@ static void seg_setup(int seg)
 	vmcs_write16(sf->selector, 0);
 	vmcs_writel(sf->base, 0);
 	vmcs_write32(sf->limit, 0xffff);
-	vmcs_write32(sf->ar_bytes, 0x93);
+	vmcs_write32(sf->ar_bytes, 0xf3);
 }
 
 static int alloc_apic_access_page(struct kvm *kvm)
