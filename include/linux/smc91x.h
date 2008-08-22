fb683f1627745e937ef199edd3428ac4b2ef1e08
Marc Zyngier <maz@misterjones.org>
2008-08-22 16:36:28 +0200

    Export smc91x led definitions
    Now that we can configure smc91x leds from its platform data,
    it seems rather useful to move the led definitions to the
    externally visible header file.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@altran.com>

--- include/linux/smc91x.h
+++ include/linux/smc91x.h
@@ -16,6 +16,15 @@
 
 #define SMC91X_USE_DMA		(1 << 6)
 
+#define RPC_LED_100_10	(0x00)	/* LED = 100Mbps OR's with 10Mbps link detect */
+#define RPC_LED_RES	(0x01)	/* LED = Reserved */
+#define RPC_LED_10	(0x02)	/* LED = 10Mbps link detect */
+#define RPC_LED_FD	(0x03)	/* LED = Full Duplex Mode */
+#define RPC_LED_TX_RX	(0x04)	/* LED = TX or RX packet occurred */
+#define RPC_LED_100	(0x05)	/* LED = 100Mbps link dectect */
+#define RPC_LED_TX	(0x06)	/* LED = TX packet occurred */
+#define RPC_LED_RX	(0x07)	/* LED = RX packet occurred */
+
 struct smc91x_platdata {
 	unsigned long flags;
 	unsigned char leda;
