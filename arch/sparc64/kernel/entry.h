fe06ccaad20257e3bd348b2df9e811fd92211a80
David S. Miller <davem@davemloft.net>
2008-08-24 20:10:23 -0700

    sparc64: Split syscall_trace() into two functions.
    Christoph Hellwig noticed that having both entry and exit
    logic in one function no longer makes sense, and having
    seperate ones simplifies things a lot.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/entry.h
+++ arch/sparc64/kernel/entry.h
@@ -22,7 +22,8 @@ extern void do_notify_resume(struct pt_regs *regs,
 			     unsigned long orig_i0,
 			     unsigned long thread_info_flags);
 
-extern asmlinkage int syscall_trace(struct pt_regs *regs, int syscall_exit_p);
+extern asmlinkage int syscall_trace_enter(struct pt_regs *regs);
+extern asmlinkage void syscall_trace_leave(struct pt_regs *regs);
 
 extern void bad_trap_tl1(struct pt_regs *regs, long lvl);
 
