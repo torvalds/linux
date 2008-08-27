fe439dd09d3e4da6a44d35df7371b9c6a2661b99
Jarek Poplawski <jarkao2@gmail.com>
2008-08-27 02:27:10 -0700

    pkt_sched: Fix sch_tree_lock()
    Use new qdisc_root_sleeping_lock() instead of qdisc_root_lock() as
    sch_tree_lock() because this lock could be used while dev is
    deactivated, but we never need to use this with noop_qdisc as a root.
    
    Signed-off-by: Jarek Poplawski <jarkao2@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/sch_generic.h
+++ include/net/sch_generic.h
@@ -232,12 +232,12 @@ static inline struct net_device *qdisc_dev(struct Qdisc *qdisc)
 
 static inline void sch_tree_lock(struct Qdisc *q)
 {
-	spin_lock_bh(qdisc_root_lock(q));
+	spin_lock_bh(qdisc_root_sleeping_lock(q));
 }
 
 static inline void sch_tree_unlock(struct Qdisc *q)
 {
-	spin_unlock_bh(qdisc_root_lock(q));
+	spin_unlock_bh(qdisc_root_sleeping_lock(q));
 }
 
 #define tcf_tree_lock(tp)	sch_tree_lock((tp)->q)
