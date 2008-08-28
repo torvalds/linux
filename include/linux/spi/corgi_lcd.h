b18250a8f66050bd2a52287cd543fb93100e8ee0
Eric Miao <eric.miao@marvell.com>
2008-08-29 04:21:44 +0800

    lcd: add SPI-based LCD and backlight driver for SHARP corgi/spitz
    The driver is based on different source files including corgi_ssp.c,
    corgi_lcd.c and corgi_bl.c, previously authored by Richard Purdie
    and many others.
    
    The LCD and Backlight device actually share the same SPI device, so
    they are made into this single driver.
    
    Signed-off-by: Eric Miao <eric.miao@marvell.com>
    Cc: Richard Purdie <rpurdie@rpsys.net>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

---
+++ include/linux/spi/corgi_lcd.h
@@ -0,0 +1,16 @@
+#ifndef __LINUX_SPI_CORGI_LCD_H
+#define __LINUX_SPI_CORGI_LCD_H
+
+#define CORGI_LCD_MODE_QVGA	1
+#define CORGI_LCD_MODE_VGA	2
+
+struct corgi_lcd_platform_data {
+	int	init_mode;
+	int	max_intensity;
+	int	default_intensity;
+
+	void (*notify)(int intensity);
+	void (*kick_battery)(void);
+};
+
+#endif /* __LINUX_SPI_CORGI_LCD_H */
