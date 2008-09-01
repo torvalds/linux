748c7f80acc266a54ade4d6863229402fd3cd363
Hans de Goede <j.w.r.degoede@hhs.nl>
2008-09-01 15:28:23 -0300

    V4L/DVB (8880): PATCH: Fix parents on some webcam drivers
    While doing some testing using Luca Risolia's sonix driver I noticed that
    the video device did not get ACL's set to allow access by locally logged in
    users, nor does it show up as a video device in lshal, causing cheese to not
    see it.
    
    This turns out to be caused by all of Luca Risolia's drivers not setting
    the parent member of the video_device struct. This patch fixes this.
    
    Cc: Luca Risolia <luca.risolia@studio.unibo.it>
    Signed-off-by: Hans de Goede <j.w.r.degoede@hhs.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/et61x251/et61x251_core.c
+++ drivers/media/video/et61x251/et61x251_core.c
@@ -2588,6 +2588,7 @@ et61x251_usb_probe(struct usb_interface* intf, const struct usb_device_id* id)
 	cam->v4ldev->fops = &et61x251_fops;
 	cam->v4ldev->minor = video_nr[dev_nr];
 	cam->v4ldev->release = video_device_release;
+	cam->v4ldev->parent = &udev->dev;
 	video_set_drvdata(cam->v4ldev, cam);
 
 	init_completion(&cam->probe);
