243294dae09c909c0442c8f04d470b69c3c19d6e
Tejun Heo <tj@kernel.org>
2008-09-04 09:17:31 +0200

    block: fix duplicate headers for /proc/partitions
    seqf can be started multiple times for a read and the header should be
    printed only for the initial one.  Fix it.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- block/genhd.c
+++ block/genhd.c
@@ -697,7 +697,7 @@ static void *show_partition_start(struct seq_file *seqf, loff_t *pos)
 	static void *p;
 
 	p = disk_seqf_start(seqf, pos);
-	if (!IS_ERR(p) && p)
+	if (!IS_ERR(p) && p && !*pos)
 		seq_puts(seqf, "major minor  #blocks  name\n\n");
 	return p;
 }
