96706600de83966812b01a3cb310a13da2a1a4e9
David Brownell <dbrownell@users.sourceforge.net>
2008-09-06 20:19:02 -0700

    ngw100: export J15 through sysfs
    The NGW100 board has jumper J15 (near the reset button) which
    is unused.  This patch exports it through the GPIO sysfs support
    (as /sys/class/gpio/gpio62/value) so that it's easily queried
    by boot scripts or whatever might want to know if the jumper
    has been installed (value = 0) or not (value = 1, "default").
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    [haavard.skinnemoen@atmel.com: add missing include]
    Signed-off-by: Haavard Skinnemoen <haavard.skinnemoen@atmel.com>

--- arch/avr32/boards/atngw100/setup.c
+++ arch/avr32/boards/atngw100/setup.c
@@ -9,6 +9,7 @@
  */
 #include <linux/clk.h>
 #include <linux/etherdevice.h>
+#include <linux/gpio.h>
 #include <linux/irq.h>
 #include <linux/i2c.h>
 #include <linux/i2c-gpio.h>
@@ -204,6 +205,15 @@ postcore_initcall(atngw100_init);
 
 static int __init atngw100_arch_init(void)
 {
+	/* PB30 is the otherwise unused jumper on the mainboard, with an
+	 * external pullup; the jumper grounds it.  Use it however you
+	 * like, including letting U-Boot or Linux tweak boot sequences.
+	 */
+	at32_select_gpio(GPIO_PIN_PB(30), 0);
+	gpio_request(GPIO_PIN_PB(30), "j15");
+	gpio_direction_input(GPIO_PIN_PB(30));
+	gpio_export(GPIO_PIN_PB(30), false);
+
 	/* set_irq_type() after the arch_initcall for EIC has run, and
 	 * before the I2C subsystem could try using this IRQ.
 	 */
