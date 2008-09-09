aee14ceb5230afb5c17a4e28222ab9734ffd5002
Jouni Malinen <j@w1.fi>
2008-09-09 16:33:15 +0200

    mac80211: Reorder debugfs calls during netdev deinit
    ieee80211_free_keys() must be called before
    ieee80211_debugfs_remove_netdev() in order to make sure that the
    possible default_key symlink is removed before attempting to
    remove the netdev debugfs directory.
    
    Signed-off-by: Jouni Malinen <jouni.malinen@atheros.com>
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/iface.c
+++ net/mac80211/iface.c
@@ -31,10 +31,10 @@ static void ieee80211_teardown_sdata(struct net_device *dev)
 	int flushed;
 	int i;
 
-	ieee80211_debugfs_remove_netdev(sdata);
-
 	/* free extra data */
 	ieee80211_free_keys(sdata);
+
+	ieee80211_debugfs_remove_netdev(sdata);
 
 	for (i = 0; i < IEEE80211_FRAGMENT_MAX; i++)
 		__skb_queue_purge(&sdata->fragments[i].skb_list);
