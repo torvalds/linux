1ea6dbb5067c5b0d0d75da730ae94af52150ce12
Arve Hjønnevåg <arve@android.com>
2008-09-09 22:14:34 -0700

    PM: Implement wakelock api.
    PM: wakelock: Replace expire work with a timer
    
    The expire work function did not work in the normal case.
    
    Signed-off-by: Arve Hjønnevåg <arve@android.com>

--- kernel/power/power.h
+++ kernel/power/power.h
@@ -236,3 +236,10 @@ static inline void suspend_thaw_processes(void)
 {
 }
 #endif
+
+#ifdef CONFIG_WAKELOCK
+/* kernel/power/wakelock.c */
+extern struct workqueue_struct *suspend_work_queue;
+extern struct wake_lock main_wake_lock;
+extern suspend_state_t requested_suspend_state;
+#endif
