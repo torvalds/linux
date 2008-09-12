eecfffc154ffbfe70686a9905c090b488778c28e
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2008-09-12 19:42:33 +0900

    iommu: add iommu_device_max_index IOMMU helper function
    This function helps IOMMUs to know the highest address that a device
    can access to.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/iommu-helper.h
+++ include/linux/iommu-helper.h
@@ -1,3 +1,13 @@
+static inline unsigned long iommu_device_max_index(unsigned long size,
+						   unsigned long offset,
+						   u64 dma_mask)
+{
+	if (size + offset > dma_mask)
+		return dma_mask - offset + 1;
+	else
+		return size;
+}
+
 extern int iommu_is_span_boundary(unsigned int index, unsigned int nr,
 				  unsigned long shift,
 				  unsigned long boundary_size);
