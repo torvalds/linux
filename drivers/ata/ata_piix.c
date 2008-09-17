d09addf65cb5b3b19a536aa3329efeedbc6bb56c
Herton Ronaldo Krzesinski <herton@mandriva.com.br>
2008-09-17 14:29:05 -0300

    ata_piix: add Hercules EC-900 mini-notebook to ich_laptop short cable list
    Signed-off-by: Herton Ronaldo Krzesinski <herton@mandriva.com.br>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/ata/ata_piix.c
+++ drivers/ata/ata_piix.c
@@ -584,6 +584,7 @@ static const struct ich_laptop ich_laptop[] = {
 	{ 0x27DF, 0x1025, 0x0110 },	/* ICH7 on Acer 3682WLMi */
 	{ 0x27DF, 0x1043, 0x1267 },	/* ICH7 on Asus W5F */
 	{ 0x27DF, 0x103C, 0x30A1 },	/* ICH7 on HP Compaq nc2400 */
+	{ 0x27DF, 0x1071, 0xD221 },	/* ICH7 on Hercules EC-900 */
 	{ 0x24CA, 0x1025, 0x0061 },	/* ICH4 on ACER Aspire 2023WLMi */
 	{ 0x24CA, 0x1025, 0x003d },	/* ICH4 on ACER TM290 */
 	{ 0x266F, 0x1025, 0x0066 },	/* ICH6 on ACER Aspire 1694WLMi */
