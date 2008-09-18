9c95c43de81d5556237fbc82c4e90403dfe468db
David Woodhouse <dwmw2@infradead.org>
2008-09-18 13:37:13 -0700

    ALSA: hda - Add PCI subsystem ID for MacBookPro4,1
    This seems to work if I tell it that it's a mbp3. Not sure if there are
    actually any differences.
    
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -6659,6 +6659,7 @@ static int patch_alc882(struct hda_codec *codec)
 			board_config = ALC885_IMAC24;
 			break;
 		case 0x106b00a1: /* Macbook (might be wrong - PCI SSID?) */
+		case 0x106b00a4: /* MacbookPro4,1 */
 		case 0x106b2c00: /* Macbook Pro rev3 */
 		case 0x106b3600: /* Macbook 3.1 */
 			board_config = ALC885_MBP3;
