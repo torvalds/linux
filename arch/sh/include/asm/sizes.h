d3ea00a36da2e715217f0e31944dd220deefa38c
Paul Mundt <lethal@linux-sh.org>
2008-09-21 10:31:57 +0900

    sh: Add a few more definitions to asm/sizes.h.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/sizes.h
+++ arch/sh/include/asm/sizes.h
@@ -25,6 +25,9 @@
 
 /* handy sizes */
 #define SZ_16				0x00000010
+#define SZ_32				0x00000020
+#define SZ_64				0x00000040
+#define SZ_128				0x00000080
 #define SZ_256				0x00000100
 #define SZ_512				0x00000200
 
@@ -32,6 +35,7 @@
 #define SZ_4K                           0x00001000
 #define SZ_8K                           0x00002000
 #define SZ_16K                          0x00004000
+#define SZ_32K				0x00008000
 #define SZ_64K                          0x00010000
 #define SZ_128K                         0x00020000
 #define SZ_256K                         0x00040000
@@ -42,6 +46,7 @@
 #define SZ_4M                           0x00400000
 #define SZ_8M                           0x00800000
 #define SZ_16M                          0x01000000
+#define SZ_26M				0x01a00000
 #define SZ_32M                          0x02000000
 #define SZ_64M                          0x04000000
 #define SZ_128M                         0x08000000
