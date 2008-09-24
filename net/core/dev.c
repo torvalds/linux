96ca4a2cc1454cf633a1e0796b7ef39d937b87ec
Oliver Hartkopp <oliver@hartkopp.net>
2008-09-23 21:23:19 -0700

    net: remove ifalias on empty given alias
    This patch removes the potentially allocated ifalias when the (new) given alias is empty.
    
    E.g. when setting
    
    echo "" > /sys/class/net/eth0/ifalias
    
    Signed-off-by: Oliver Hartkopp <oliver@hartkopp.net>
    Acked-by: Stephen Hemminger <shemminger@vyatta.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -967,6 +967,14 @@ int dev_set_alias(struct net_device *dev, const char *alias, size_t len)
 	if (len >= IFALIASZ)
 		return -EINVAL;
 
+	if (!len) {
+		if (dev->ifalias) {
+			kfree(dev->ifalias);
+			dev->ifalias = NULL;
+		}
+		return 0;
+	}
+
 	dev->ifalias = krealloc(dev->ifalias, len+1, GFP_KERNEL);
 	if (!dev->ifalias)
 		return -ENOMEM;
