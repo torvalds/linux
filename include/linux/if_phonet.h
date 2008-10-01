6e50e8a2136f1a90de251c653226ded447c5c915
Remi Denis-Courmont <remi.denis-courmont@nokia.com>
2008-10-01 01:30:19 -0700

    phonet: Protect if_phonet.h against multiple inclusions.
    From: Remi Denis-Courmont <remi.denis-courmont@nokia.com>
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/if_phonet.h
+++ include/linux/if_phonet.h
@@ -5,14 +5,15 @@
  *
  * Copyright (C) 2008 Nokia Corporation. All rights reserved.
  */
+#ifndef LINUX_IF_PHONET_H
+#define LINUX_IF_PHONET_H
 
-#define PHONET_HEADER_LEN	8	/* Phonet header length */
-
-#define PHONET_MIN_MTU		6
-/* 6 bytes header + 65535 bytes payload */
-#define PHONET_MAX_MTU		65541
+#define PHONET_MIN_MTU		6	/* pn_length = 0 */
+#define PHONET_MAX_MTU		65541	/* pn_length = 0xffff */
 #define PHONET_DEV_MTU		PHONET_MAX_MTU
 
 #ifdef __KERNEL__
 extern struct header_ops phonet_header_ops;
+#endif
+
 #endif
