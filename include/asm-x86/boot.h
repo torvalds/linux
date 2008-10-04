ba31a5f88b6f907e715ff43db06403e12465b703
Paul Bolle <pebolle@tiscali.nl>
2008-10-04 21:21:44 +0200

    x86 setup: remove DEF_INITSEG and DEF_SETUPSEG
    Since v.2.6.23 DEF_INITSEG and DEF_SETUPSEG are unused. Commit c397368
    ("Remove old i386 setup code") dropped their usage for i386. They did not
    return in the x86 tree. (Something similar must have happened for x86_64.)
    Remove these.
    
    Signed-off-by: Paul Bolle <pebolle@tiscali.nl>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- include/asm-x86/boot.h
+++ include/asm-x86/boot.h
@@ -2,9 +2,7 @@
 #define _ASM_BOOT_H
 
 /* Don't touch these, unless you really know what you're doing. */
-#define DEF_INITSEG	0x9000
 #define DEF_SYSSEG	0x1000
-#define DEF_SETUPSEG	0x9020
 #define DEF_SYSSIZE	0x7F00
 
 /* Internal svga startup constants */
