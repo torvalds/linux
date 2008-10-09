6c3fc69a173868c0a3c20bc1bba5ede463b5029c
Mike Frysinger <vapier.adi@gmail.com>
2008-10-09 17:13:39 +0800

    Blackfin arch: avoid using actual config name in comment
    avoid using actual config name in comment as a text search
    is done to see what files need to be rebuilt
    
    Signed-off-by: Mike Frysinger <vapier.adi@gmail.com>
    Signed-off-by: Bryan Wu <cooloney@kernel.org>

--- arch/blackfin/include/asm/bfin-global.h
+++ arch/blackfin/include/asm/bfin-global.h
@@ -106,7 +106,7 @@ extern char _stext_l1[], _etext_l1[], _sdata_l1[], _edata_l1[], _sbss_l1[],
 	_stext_l2[], _etext_l2[], _sdata_l2[], _edata_l2[], _sbss_l2[],
 	_ebss_l2[], _l2_lma_start[];
 
-/* only used when CONFIG_MTD_UCLINUX */
+/* only used when MTD_UCLINUX */
 extern unsigned long memory_mtd_start, memory_mtd_end, mtd_size;
 
 #ifdef CONFIG_BFIN_ICACHE_LOCK
