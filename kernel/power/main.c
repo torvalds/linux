507fedc71561e41a09d057f9b3c474b9738fb7db
Arve Hjønnevåg <arve@android.com>
2008-10-09 21:01:46 -0700

    PM: Add user-space wake lock api.
    This adds /sys/power/wake_lock and /sys/power/wake_unlock.
    Writing a string to wake_lock creates a wake lock the
    first time is sees a string and locks it. Optionally, the
    string can be followed by a timeout.
    To unlock the wake lock, write the same string to wake_unlock.
    
    Change-Id: I66c6e3fe6487d17f9c2fafde1174042e57d15cd7

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -294,6 +294,11 @@ pm_trace_store(struct kobject *kobj, struct kobj_attribute *attr,
 power_attr(pm_trace);
 #endif /* CONFIG_PM_TRACE */
 
+#ifdef CONFIG_USER_WAKELOCK
+power_attr(wake_lock);
+power_attr(wake_unlock);
+#endif
+
 static struct attribute * g[] = {
 	&state_attr.attr,
 #ifdef CONFIG_PM_TRACE
@@ -304,6 +309,10 @@ static struct attribute * g[] = {
 	&wakeup_count_attr.attr,
 #ifdef CONFIG_PM_DEBUG
 	&pm_test_attr.attr,
+#endif
+#ifdef CONFIG_USER_WAKELOCK
+	&wake_lock_attr.attr,
+	&wake_unlock_attr.attr,
 #endif
 #endif
 	NULL,
