fd64bbf28e28526f608df0061175829338ee94cc
Robert Love <rlove@google.com>
2008-10-15 15:35:44 -0400

    Paranoid network.
    With CONFIG_ANDROID_PARANOID_NETWORK, require specific uids/gids to instantiate
    network sockets.
    
    Signed-off-by: Robert Love <rlove@google.com>
    
    paranoid networking: Use in_egroup_p() to check group membership
    
    The previous group_search() caused trouble for partners with module builds.
    in_egroup_p() is also cleaner.
    
    Signed-off-by: Nick Pelly <npelly@google.com>
    
    Fix 2.6.29 build.
    
    Signed-off-by: Arve Hjønnevåg <arve@android.com>
    
    net: Fix compilation of the IPv6 module
    
    Fix compilation of the IPv6 module -- current->euid does not exist anymore,
    current_euid() is what needs to be used.
    
    Signed-off-by: Steinar H. Gunderson <sesse@google.com>

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -119,6 +119,9 @@
 #include <linux/mroute.h>
 #endif
 
+#ifdef CONFIG_ANDROID_PARANOID_NETWORK
+#include <linux/android_aid.h>
+#endif
 
 /* The inetsw table contains everything that inet_create needs to
  * build a new socket.
@@ -270,6 +273,29 @@ void build_ehash_secret(void)
 }
 EXPORT_SYMBOL(build_ehash_secret);
 
+#ifdef CONFIG_ANDROID_PARANOID_NETWORK
+static inline int current_has_network(void)
+{
+	return (!current_euid() || in_egroup_p(AID_INET) ||
+		in_egroup_p(AID_NET_RAW));
+}
+static inline int current_has_cap(struct net *net, int cap)
+{
+	if (cap == CAP_NET_RAW && in_egroup_p(AID_NET_RAW))
+		return 1;
+	return ns_capable(net->user_ns, cap);
+}
+# else
+static inline int current_has_network(void)
+{
+	return 1;
+}
+static inline int current_has_cap(struct net *net, int cap)
+{
+	return ns_capable(net->user_ns, cap);
+}
+#endif
+
 /*
  *	Create an inet socket.
  */
@@ -285,6 +311,9 @@ static int inet_create(struct net *net, struct socket *sock, int protocol,
 	char answer_no_check;
 	int try_loading_module = 0;
 	int err;
+
+	if (!current_has_network())
+		return -EACCES;
 
 	if (unlikely(!inet_ehash_secret))
 		if (sock->type != SOCK_RAW && sock->type != SOCK_DGRAM)
@@ -339,7 +368,7 @@ lookup_protocol:
 
 	err = -EPERM;
 	if (sock->type == SOCK_RAW && !kern &&
-	    !ns_capable(net->user_ns, CAP_NET_RAW))
+	    !current_has_cap(net, CAP_NET_RAW))
 		goto out_rcu_unlock;
 
 	sock->ops = answer->ops;
