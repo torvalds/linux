459fc208abd1b365fa013c17d433dfb5b4bc1e3a
Dominik Brodowski <linux@dominikbrodowski.net>
2008-10-16 00:11:04 +0200

    cpufreq: remove policy->governor setting in drivers initialization
    As policy->governor is already set to CPUFREQ_DEFAULT_GOVERNOR in the
    (always built-in) cpufreq core, we do not need to set it in the drivers.
    This fixes the sparc64 allmodconfig build failure.
    
    Also, remove a totally useles setting of ->policy in cpufreq-pxa3xx.c.
    
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>
    Acked-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/arm/mach-integrator/cpu.c
+++ arch/arm/mach-integrator/cpu.c
@@ -184,7 +184,6 @@ static int integrator_cpufreq_init(struct cpufreq_policy *policy)
 {
 
 	/* set default policy and cpuinfo */
-	policy->governor = CPUFREQ_DEFAULT_GOVERNOR;
 	policy->cpuinfo.max_freq = 160000;
 	policy->cpuinfo.min_freq = 12000;
 	policy->cpuinfo.transition_latency = 1000000; /* 1 ms, assumed */
