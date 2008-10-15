7c3b1dcf13d5660152e02c6dea47b0bd9fd5d871
Arjan van de Ven <arjan@linux.intel.com>
2008-10-15 10:52:34 +0100

    tty: make sure that proc_clear_tty stores the cpu flags
    proc_clear_tty() gets called with interrupts off (while holding the task list
    lock) from sys_setid. This means that it needs the _irqsave version of the
    locking primitives.
    
    Reported-by: Marcin Slusarz <marcin.slusarz@gmail.com>
    Tested-by: Marcin Slusarz <marcin.slusarz@gmail.com>
    Signed-off-by: Arjan van de Ven <arjan@linux.intel.com>
    Signed-off-by: Alan Cox <alan@redhat.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/char/tty_io.c
+++ drivers/char/tty_io.c
@@ -3032,11 +3032,12 @@ EXPORT_SYMBOL(tty_devnum);
 
 void proc_clear_tty(struct task_struct *p)
 {
+	unsigned long flags;
 	struct tty_struct *tty;
-	spin_lock_irq(&p->sighand->siglock);
+	spin_lock_irqsave(&p->sighand->siglock, flags);
 	tty = p->signal->tty;
 	p->signal->tty = NULL;
-	spin_unlock_irq(&p->sighand->siglock);
+	spin_unlock_irqrestore(&p->sighand->siglock, flags);
 	tty_kref_put(tty);
 }
 
