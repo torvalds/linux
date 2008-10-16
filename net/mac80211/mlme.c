4a68ec535ef1043319928f601d633f78e650a16f
Johannes Berg <johannes@sipsolutions.net>
2008-10-16 21:44:44 +0200

    mac80211: inform userspace of probe/auth/assoc timeout
    I noticed that when for some reason [1] the probe or auth times
    out, wpa_supplicant doesn't realise this and only tries the next
    AP when it runs into its own timeout, which is ten seconds, and
    that's quite long. Fix this by making mac80211 notify userspace
    that it didn't associate.
    
    [1] my wrt350n in mixed B/G/HT mode often runs into this, maybe
    it's because one of the antennas is broken off and for whatever
    reason it decides to use that antenna to transmit the response
    frames (auth, probe); I do see beacons fine so it's not totally
    broken. Works fine in pure-G mode.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -759,6 +759,7 @@ static void ieee80211_direct_probe(struct ieee80211_sub_if_data *sdata,
 		printk(KERN_DEBUG "%s: direct probe to AP %pM timed out\n",
 		       sdata->dev->name, ifsta->bssid);
 		ifsta->state = IEEE80211_STA_MLME_DISABLED;
+		ieee80211_sta_send_apinfo(sdata, ifsta);
 		return;
 	}
 
@@ -789,6 +790,7 @@ static void ieee80211_authenticate(struct ieee80211_sub_if_data *sdata,
 		       " timed out\n",
 		       sdata->dev->name, ifsta->bssid);
 		ifsta->state = IEEE80211_STA_MLME_DISABLED;
+		ieee80211_sta_send_apinfo(sdata, ifsta);
 		return;
 	}
 
@@ -909,6 +911,7 @@ static void ieee80211_associate(struct ieee80211_sub_if_data *sdata,
 		       " timed out\n",
 		       sdata->dev->name, ifsta->bssid);
 		ifsta->state = IEEE80211_STA_MLME_DISABLED;
+		ieee80211_sta_send_apinfo(sdata, ifsta);
 		return;
 	}
 
