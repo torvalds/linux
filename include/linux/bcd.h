5a85a7dda15f88b7f9c96c67fe826b5d0486d601
Adrian Bunk <bunk@kernel.org>
2008-10-18 20:28:48 -0700

    include/linux/bcd.h: remove comments
    - the macros are gone
    - there's no more code in this file,
      LGPL + GPL = GPL,
      and the code that was moved to lib/bcd.c is anyway trivial
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/bcd.h
+++ include/linux/bcd.h
@@ -1,12 +1,3 @@
-/* Permission is hereby granted to copy, modify and redistribute this code
- * in terms of the GNU Library General Public License, Version 2 or later,
- * at your option.
- */
-
-/* macros to translate to/from binary and binary-coded decimal (frequently
- * found in RTC chips).
- */
-
 #ifndef _BCD_H
 #define _BCD_H
 
