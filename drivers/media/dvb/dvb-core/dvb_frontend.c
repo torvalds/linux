3b37a15c2d75585cc0da49b8e69345af91e227ce
Manu Abraham <abraham.manu@gmail.com>
2008-10-20 18:14:14 -0300

    V4L/DVB (9346): Optimization: Enable gate in a symmetric/disciplined way,
    rather than implementing different ways leading to confusion.
    
    This allows multiple gate_enable/disable's in the tuner_read/write
    functions, thereby lesser number of I/O operations throughout,
    eventually leading to better results. As a side effect demods that
    detect the STOP bit for auto closing of the gate can be avoided, thereby
    a very minimal gain in disabling the auto detect feature as well.
    Improves readability on the device control.
    
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/dvb-core/dvb_frontend.c
+++ drivers/media/dvb/dvb-core/dvb_frontend.c
@@ -223,6 +223,8 @@ static void dvb_frontend_init(struct dvb_frontend *fe)
 	if (fe->ops.init)
 		fe->ops.init(fe);
 	if (fe->ops.tuner_ops.init) {
+		if (fe->ops.i2c_gate_ctrl)
+			fe->ops.i2c_gate_ctrl(fe, 1);
 		fe->ops.tuner_ops.init(fe);
 		if (fe->ops.i2c_gate_ctrl)
 			fe->ops.i2c_gate_ctrl(fe, 0);
