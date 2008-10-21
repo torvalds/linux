550109378450e7982101f299d41a80e8efc759ea
Paul Mundt <lethal@linux-sh.org>
2008-10-21 17:48:40 +0900

    sh: gpio: Stub in dummy GPIO<->IRQ mapping routines.
    IRQ modes are not presently supported, so stub in dummy definitions
    for now (taken from linux/gpio.h).
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/gpio.h
+++ arch/sh/include/asm/gpio.h
@@ -12,6 +12,8 @@
 #ifndef __ASM_SH_GPIO_H
 #define __ASM_SH_GPIO_H
 
+#include <linux/errno.h>
+
 #if defined(CONFIG_CPU_SH3)
 #include <cpu/gpio.h>
 #endif
@@ -99,6 +101,19 @@ int gpio_direction_input(unsigned gpio);
 int gpio_direction_output(unsigned gpio, int value);
 int gpio_get_value(unsigned gpio);
 void gpio_set_value(unsigned gpio, int value);
+
+/* IRQ modes are unspported */
+static inline int gpio_to_irq(unsigned gpio)
+{
+	WARN_ON(1);
+	return -EINVAL;
+}
+
+static inline int irq_to_gpio(unsigned irq)
+{
+	WARN_ON(1);
+	return -EINVAL;
+}
 
 #include <asm-generic/gpio.h>
 
