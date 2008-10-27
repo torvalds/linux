2f1e593d4209d0194f9639c5d11aa91171435963
Alan Cox <alan@lxorguk.ukuu.org.uk>
2008-10-27 15:21:19 +0000

    sound: use a common working email address
    Signed-off-by: Alan Cox <alan@redhat.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/oss/kahlua.c
+++ sound/oss/kahlua.c
@@ -1,7 +1,7 @@
 /*
  *	Initialisation code for Cyrix/NatSemi VSA1 softaudio
  *
- *	(C) Copyright 2003 Red Hat Inc <alan@redhat.com>
+ *	(C) Copyright 2003 Red Hat Inc <alan@lxorguk.ukuu.org.uk>
  *
  * XpressAudio(tm) is used on the Cyrix MediaGX (now NatSemi Geode) systems.
  * The older version (VSA1) provides fairly good soundblaster emulation
