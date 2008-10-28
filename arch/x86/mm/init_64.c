fe8b868eccb9f85a0e231e35f0abac5b39bac801
Gary Hade <garyhade@us.ibm.com>
2008-10-28 16:43:14 -0700

    x86: remove debug code from arch_add_memory()
    Impact: remove incorrect WARN_ON(1)
    
    Gets rid of dmesg spam created during physical memory hot-add which
    will very likely confuse users.  The change removes what appears to
    be debugging code which I assume was unintentionally included in:
    
      x86: arch/x86/mm/init_64.c printk fixes
      commit 10f22dde556d1ed41d55355d1fb8ad495f9810c8
    
    Signed-off-by: Gary Hade <garyhade@us.ibm.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/mm/init_64.c
+++ arch/x86/mm/init_64.c
@@ -858,7 +858,7 @@ int arch_add_memory(int nid, u64 start, u64 size)
 		max_pfn_mapped = last_mapped_pfn;
 
 	ret = __add_pages(zone, start_pfn, nr_pages);
-	WARN_ON(1);
+	WARN_ON_ONCE(ret);
 
 	return ret;
 }
