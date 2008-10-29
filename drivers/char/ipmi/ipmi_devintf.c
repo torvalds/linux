6c89161b10f5771ee0b51ada0fce0e8835e72ade
Scott James Remnant <scott@canonical.com>
2008-10-29 14:01:18 -0700

    ipmi: add MODULE_ALIAS to load ipmi_devintf with ipmi_si
    The ipmi_devintf module contains the userspace interface for IPMI devices,
    yet will not be loaded automatically with a system interface handler
    driver.
    
    Add a MODULE_ALIAS for the "platform:ipmi_si" MODALIAS exported by the
    ipmi_si driver, so that userspace knows of the recommendation.
    
    Signed-off-by: Scott James Remnant <scott@ubuntu.com>
    Cc: Tim Gardner <tcanonical@tpi.com>
    Cc: Corey Minyard <minyard@acm.org>
    Cc: <stable@kernel.org>		[2.6.27.x, maybe earlier?]
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/char/ipmi/ipmi_devintf.c
+++ drivers/char/ipmi/ipmi_devintf.c
@@ -957,3 +957,4 @@ module_exit(cleanup_ipmi);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Corey Minyard <minyard@mvista.com>");
 MODULE_DESCRIPTION("Linux device interface for the IPMI message handler.");
+MODULE_ALIAS("platform:ipmi_si");
