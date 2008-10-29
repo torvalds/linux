afeb12b7478fee31888e7c34804bee2f658e7765
Randy Dunlap <randy.dunlap@oracle.com>
2008-10-29 14:13:20 -0700

    fujitsu-laptop: fix section mismatch warning
    Could fix a bug in a hotplug add scenario.
    
    WARNING: drivers/misc/fujitsu-laptop.o(.text+0xbde): Section mismatch in reference from the function acpi_fujitsu_add() to the variable .init.data:fujitsu_dmi_table
    The function acpi_fujitsu_add() references
    the variable __initdata fujitsu_dmi_table.
    This is often because acpi_fujitsu_add lacks a __initdata
    annotation or the annotation of fujitsu_dmi_table is wrong.
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Acked-by: Jonathan Woithe <jwoithe@physics.adelaide.edu.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/misc/fujitsu-laptop.c
+++ drivers/misc/fujitsu-laptop.c
@@ -473,7 +473,7 @@ static int dmi_check_cb_p8010(const struct dmi_system_id *id)
 	return 0;
 }
 
-static struct dmi_system_id __initdata fujitsu_dmi_table[] = {
+static struct dmi_system_id fujitsu_dmi_table[] = {
 	{
 	 .ident = "Fujitsu Siemens S6410",
 	 .matches = {
