d95f98d0691d3aba5e35850011946a08c9b36428
Peter Zijlstra <a.p.zijlstra@chello.nl>
2008-11-04 21:25:08 +0100

    sched: fix fair preempt check
    Impact: fix cross-class preemption
    
    Inter-class wakeup preemptions should go on class order.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Acked-by: Mike Galbraith <efault@gmx.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -1329,6 +1329,9 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int sync)
 		return;
 	}
 
+	if (unlikely(p->sched_class != &fair_sched_class))
+		return;
+
 	if (unlikely(se == pse))
 		return;
 
