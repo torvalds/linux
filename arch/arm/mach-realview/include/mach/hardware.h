877d1f338912c1bf49a026eeca1d411755efece5
Catalin Marinas <catalin.marinas@arm.com>
2008-11-10 14:10:14 +0000

    RealView: Make more vmalloc space by changing IO_ADDRESS
    Since boards like PB11MPCore have 512MB of RAM and 128MB of flash, the
    virtual space is short on vmalloc memory and mapping the flash fails.
    This patch changes the IO_ADDRESS so that static mappings start at
    0xfc000000 and moves the PCI virtual addresses higher up. The new
    VMALLOC_END is set to 0xf8000000 (512MB higher than the old value).
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm/mach-realview/include/mach/hardware.h
+++ arch/arm/mach-realview/include/mach/hardware.h
@@ -25,7 +25,14 @@
 #include <asm/sizes.h>
 
 /* macro to get at IO space when running virtually */
-#define IO_ADDRESS(x)		(((x) & 0x0fffffff) + 0xf0000000)
+/*
+ * Statically mapped addresses:
+ *
+ * 10xx xxxx -> fbxx xxxx
+ * 1exx xxxx -> fdxx xxxx
+ * 1fxx xxxx -> fexx xxxx
+ */
+#define IO_ADDRESS(x)		(((x) & 0x03ffffff) + 0xfb000000)
 #define __io_address(n)		__io(IO_ADDRESS(n))
 
 #endif
