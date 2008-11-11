cb96cf1ad641334ca605cdf25841ac020d6ae01c
Sascha Hauer <s.hauer@pengutronix.de>
2008-11-11 15:15:39 +0100

    [ARM] MX3: add NAND support
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-mx3/devices.c
+++ arch/arm/mach-mx3/devices.c
@@ -160,3 +160,22 @@ struct platform_device mxc_w1_master_device = {
 	.num_resources = ARRAY_SIZE(mxc_w1_master_resources),
 	.resource = mxc_w1_master_resources,
 };
+
+static struct resource mxc_nand_resources[] = {
+	{
+		.start	= NFC_BASE_ADDR,
+		.end	= NFC_BASE_ADDR + 0xfff,
+		.flags	= IORESOURCE_MEM
+	}, {
+		.start	= MXC_INT_NANDFC,
+		.end	= MXC_INT_NANDFC,
+		.flags	= IORESOURCE_IRQ
+	},
+};
+
+struct platform_device mxc_nand_device = {
+	.name = "mxc_nand",
+	.id = 0,
+	.num_resources = ARRAY_SIZE(mxc_nand_resources),
+	.resource = mxc_nand_resources,
+};
