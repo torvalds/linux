0426c166424ea6d3d0412f47879c8ba268f874c4
Herbert Xu <herbert@gondor.apana.org.au>
2008-11-11 12:20:06 +0800

    libcrc32c: Add crc32c_le macro
    The bnx2x driver actually uses the crc32c_le name so this patch
    restores the crc32c_le symbol through a macro.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/linux/crc32c.h
+++ include/linux/crc32c.h
@@ -5,4 +5,7 @@
 
 extern u32 crc32c(u32 crc, const void *address, unsigned int length);
 
+/* This macro exists for backwards-compatibility. */
+#define crc32c_le crc32c
+
 #endif	/* _LINUX_CRC32C_H */
