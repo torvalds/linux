2378982487c492541d17adc0a870e7e83b07ba43
Alexey Dobriyan <adobriyan@gmail.com>
2008-11-12 23:25:32 -0800

    net: ifdef struct sock::sk_async_wait_queue
    Every user is under CONFIG_NET_DMA already, so ifdef field as well.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/sock.h
+++ include/net/sock.h
@@ -239,7 +239,9 @@ struct sock {
 	int			sk_sndbuf;
 	struct sk_buff_head	sk_receive_queue;
 	struct sk_buff_head	sk_write_queue;
+#ifdef CONFIG_NET_DMA
 	struct sk_buff_head	sk_async_wait_queue;
+#endif
 	int			sk_wmem_queued;
 	int			sk_forward_alloc;
 	gfp_t			sk_allocation;
