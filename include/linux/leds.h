e2387d6c20752ccdb2895ba5de664fa39652f4cc
Wolfram Sang <w.sang@pengutronix.de>
2008-11-17 14:35:44 +0000

    leds: Make header variable naming consistent
    There is one place where the struct led_classdev as the function
    argument is named differently. Fix it.
    
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>
    Signed-off-by: Richard Purdie <rpurdie@linux.intel.com>

--- include/linux/leds.h
+++ include/linux/leds.h
@@ -62,7 +62,7 @@ struct led_classdev {
 
 extern int led_classdev_register(struct device *parent,
 				 struct led_classdev *led_cdev);
-extern void led_classdev_unregister(struct led_classdev *lcd);
+extern void led_classdev_unregister(struct led_classdev *led_cdev);
 extern void led_classdev_suspend(struct led_classdev *led_cdev);
 extern void led_classdev_resume(struct led_classdev *led_cdev);
 
