1e9931146c748420343aeefadb3bb17bd1c14a37
Eduardo Habkost <ehabkost@redhat.com>
2008-11-17 19:03:17 -0200

    x86: asm/virtext.h: add cpu_vmxoff() inline function
    Unfortunately we can't use exactly the same code from vmx
    hardware_disable(), because the KVM function uses the
    __kvm_handle_fault_on_reboot() tricks.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/include/asm/virtext.h
+++ arch/x86/include/asm/virtext.h
@@ -18,6 +18,8 @@
 #include <asm/processor.h>
 #include <asm/system.h>
 
+#include <asm/vmx.h>
+
 /*
  * VMX functions:
  */
@@ -26,6 +28,19 @@ static inline int cpu_has_vmx(void)
 {
 	unsigned long ecx = cpuid_ecx(1);
 	return test_bit(5, &ecx); /* CPUID.1:ECX.VMX[bit 5] -> VT */
+}
+
+
+/** Disable VMX on the current CPU
+ *
+ * vmxoff causes a undefined-opcode exception if vmxon was not run
+ * on the CPU previously. Only call this function if you know VMX
+ * is enabled.
+ */
+static inline void cpu_vmxoff(void)
+{
+	asm volatile (ASM_VMX_VMXOFF : : : "cc");
+	write_cr4(read_cr4() & ~X86_CR4_VMXE);
 }
 
 #endif /* _ASM_X86_VIRTEX_H */
