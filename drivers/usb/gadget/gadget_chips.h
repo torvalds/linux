aa69a8093ff985873cb44fe1157bd6db29a20fe4
David Lopo <dlopo@chipidea.mips.com>
2008-11-17 14:14:51 -0800

    USB: gadget: MIPS ci13xxx_udc
    MIPS USB IP core family device controller
    Currently it only supports IP part number CI13412.
    
    [dbrownell@users.sourceforge.net: minor comment tweaks]
    
    Signed-off-by: David Lopo <dlopo@chipidea.mips.com>
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/gadget_chips.h
+++ drivers/usb/gadget/gadget_chips.h
@@ -158,6 +158,11 @@
 #define gadget_is_fsl_qe(g)	0
 #endif
 
+#ifdef CONFIG_USB_GADGET_CI13XXX
+#define gadget_is_ci13xxx(g)	(!strcmp("ci13xxx_udc", (g)->name))
+#else
+#define gadget_is_ci13xxx(g)	0
+#endif
 
 // CONFIG_USB_GADGET_SX2
 // CONFIG_USB_GADGET_AU1X00
@@ -225,6 +230,8 @@ static inline int usb_gadget_controller_number(struct usb_gadget *gadget)
 		return 0x21;
 	else if (gadget_is_fsl_qe(gadget))
 		return 0x22;
+	else if (gadget_is_ci13xxx(gadget))
+		return 0x23;
 	return -ENOENT;
 }
 
