6b41e7dd90c6a628ab5fb8d781302d60a243b2ce
Eric Dumazet <dada1@cosmosbay.com>
2008-11-19 15:48:09 -0800

    net: af_unix should use KERN_INFO instead of KERN_DEBUG
    As spotted by Joe Perches, we should use KERN_INFO in unix_sock_destructor()
    
    Signed-off-by: Eric Dumazet <dada1@cosmosbay.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -353,7 +353,7 @@ static void unix_sock_destructor(struct sock *sk)
 	WARN_ON(!sk_unhashed(sk));
 	WARN_ON(sk->sk_socket);
 	if (!sock_flag(sk, SOCK_DEAD)) {
-		printk(KERN_DEBUG "Attempt to release alive unix socket: %p\n", sk);
+		printk(KERN_INFO "Attempt to release alive unix socket: %p\n", sk);
 		return;
 	}
 
