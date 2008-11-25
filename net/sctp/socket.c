5bc0b3bfa758e4ae49c94e31c1edb9a5f10a8060
Eric Dumazet <dada1@cosmosbay.com>
2008-11-25 13:53:27 -0800

    net: Make sure BHs are disabled in sock_prot_inuse_add()
    prot->destroy is not called with BH disabled. So we must add
    explicit BH disable around call to sock_prot_inuse_add()
    in sctp_destroy_sock()
    
    Signed-off-by: Eric Dumazet <dada1@cosmosbay.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sctp/socket.c
+++ net/sctp/socket.c
@@ -3633,7 +3633,9 @@ SCTP_STATIC void sctp_destroy_sock(struct sock *sk)
 	ep = sctp_sk(sk)->ep;
 	sctp_endpoint_free(ep);
 	atomic_dec(&sctp_sockets_allocated);
+	local_bh_disable();
 	sock_prot_inuse_add(sock_net(sk), sk->sk_prot, -1);
+	local_bh_enable();
 }
 
 /* API 4.1.7 shutdown() - TCP Style Syntax
