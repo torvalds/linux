e4ea803ab0fcf985c2a2d24bc905a3cf9138e251
Kyungmin Park <kyungmin.park@samsung.com>
2008-11-25 16:59:54 +0900

    [ARM] S3C64XX: Show uncompress messages
    Fix the initialisation of the fifo data in the uncompression serial
    routines to ensure that if the FIFO is enabled, that the serial output
    is not corrupted.
    
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    [ben-linux@fluff.org: edit description to add more detail]
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/mach-s3c6400/include/mach/uncompress.h
+++ arch/arm/mach-s3c6400/include/mach/uncompress.h
@@ -21,6 +21,8 @@
 static void arch_detect_cpu(void)
 {
 	/* we do not need to do any cpu detection here at the moment. */
+	fifo_mask = S3C2440_UFSTAT_TXMASK;
+	fifo_max = 63 << S3C2440_UFSTAT_TXSHIFT;
 }
 
 #endif /* __ASM_ARCH_UNCOMPRESS_H */
