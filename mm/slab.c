8759ec50a6cad7ca5a6d63e657d25b85ab5ba44a
Pekka Enberg <penberg@cs.helsinki.fi>
2008-11-26 10:01:31 +0200

    slab: remove GFP_THISNODE clearing from alloc_slabmgmt()
    Commit 6cb062296f73e74768cca2f3eaf90deac54de02d ("Categorize GFP flags")
    left one call-site in alloc_slabmgmt() to clear GFP_THISNODE instead of
    GFP_CONSTRAINT_MASK. Unfortunately, that ends up clearing __GFP_NOWARN
    and __GFP_NORETRY as well which is not what we want. As the only caller
    of alloc_slabmgmt() already clears GFP_CONSTRAINT_MASK before passing
    local_flags to it, we can just remove the clearing of GFP_THISNODE.
    
    This patch should fix spurious page allocation failure warnings on the
    mempool_alloc() path. See the following URL for the original discussion
    of the bug:
    
      http://lkml.org/lkml/2008/10/27/100
    
    Acked-by: Christoph Lameter <cl@linux-foundation.org>
    Reported-by: Miklos Szeredi <miklos@szeredi.hu>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- mm/slab.c
+++ mm/slab.c
@@ -2609,7 +2609,7 @@ static struct slab *alloc_slabmgmt(struct kmem_cache *cachep, void *objp,
 	if (OFF_SLAB(cachep)) {
 		/* Slab management obj is off-slab. */
 		slabp = kmem_cache_alloc_node(cachep->slabp_cache,
-					      local_flags & ~GFP_THISNODE, nodeid);
+					      local_flags, nodeid);
 		if (!slabp)
 			return NULL;
 	} else {
