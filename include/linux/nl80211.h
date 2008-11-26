10ec4f1d0851eb97cd53db66150835dd7f64829d
Luis R. Rodriguez <lrodriguez@atheros.com>
2008-11-26 13:03:08 -0800

    nl80211: relicense nl80211.h under the ISC
    We have a few BSD/ISC licensed userspace applications which
    include nl80211.h from the kernel. To avoid legal ambiguity
    for usage of the header file in these projects we rather simply
    relicense the header file under the ISC. We've received consent
    from all contributors to it.
    
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>
    Acked-by: Johannes Berg <johannes@sipsolutions.net>
    Acked-by: Michael Wu <flamingice@sourmilk.net>
    Acked-by: Luis Carlos Cobo <luisca@cozybit.com>
    Acked-by: Michael Buesch <mb@bu3sch.de>
    Acked-by: Jouni Malinen <jouni.malinen@atheros.com>
    Acked-by: Colin McCabe <colin@cozybit.com>
    Acked-by: Javier Cardona <javier@cozybit.com>
    Cc: johannes@sipsolutions.net
    Cc: altape@eden.rutgers.edu
    Cc: luisca@cozybit.com
    Cc: mb@bu3sch.de
    Cc: jouni.malinen@atheros.com
    Cc: colin@cozybit.com
    Cc: javier@cozybit.com
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- include/linux/nl80211.h
+++ include/linux/nl80211.h
@@ -3,7 +3,26 @@
 /*
  * 802.11 netlink interface public header
  *
- * Copyright 2006, 2007 Johannes Berg <johannes@sipsolutions.net>
+ * Copyright 2006, 2007, 2008 Johannes Berg <johannes@sipsolutions.net>
+ * Copyright 2008 Michael Wu <flamingice@sourmilk.net>
+ * Copyright 2008 Luis Carlos Cobo <luisca@cozybit.com>
+ * Copyright 2008 Michael Buesch <mb@bu3sch.de>
+ * Copyright 2008 Luis R. Rodriguez <lrodriguez@atheros.com>
+ * Copyright 2008 Jouni Malinen <jouni.malinen@atheros.com>
+ * Copyright 2008 Colin McCabe <colin@cozybit.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
  */
 
 /**
