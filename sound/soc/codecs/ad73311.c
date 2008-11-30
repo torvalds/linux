fa5c76978cee331b25e6d271482cf8e76f51e68b
Mark Brown <broonie@opensource.wolfsonmicro.com>
2008-11-30 22:55:46 +0000

    ASoC: Remove in-code changelog from AD73311 driver
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/ad73311.c
+++ sound/soc/codecs/ad73311.c
@@ -8,9 +8,6 @@
  *  under  the terms of  the GNU General  Public License as published by the
  *  Free Software Foundation;  either version 2 of the  License, or (at your
  *  option) any later version.
- *
- *  Revision history
- *    25th Sep 2008   Initial version.
  */
 
 #include <linux/init.h>
