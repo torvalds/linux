cd32a48dc5c2f415790bfbc4fe564b2b0e9c2414
Russell King <rmk@dyn-67.arm.linux.org.uk>
2008-11-30 11:44:11 +0000

    [ARM] integrator: parenthesize __virt_to_bus/__bus_to_virt argument
    Macro arguments should be parenthesized to avoid unexpected side
    effects.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-integrator/include/mach/memory.h
+++ arch/arm/mach-integrator/include/mach/memory.h
@@ -26,7 +26,7 @@
 #define PHYS_OFFSET	UL(0x00000000)
 
 #define BUS_OFFSET	UL(0x80000000)
-#define __virt_to_bus(x)	(x - PAGE_OFFSET + BUS_OFFSET)
-#define __bus_to_virt(x)	(x - BUS_OFFSET + PAGE_OFFSET)
+#define __virt_to_bus(x)	((x) - PAGE_OFFSET + BUS_OFFSET)
+#define __bus_to_virt(x)	((x) - BUS_OFFSET + PAGE_OFFSET)
 
 #endif
