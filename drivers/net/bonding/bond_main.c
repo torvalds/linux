244ef9b9176c7c7a095f4738d353a3a60b88097d
Wang Chen <wangchen@cn.fujitsu.com>
2008-12-03 21:14:04 -0800

    bond: Kill directly reference of netdev->priv
    Simply replace netdev->priv with netdev_priv().
    
    Signed-off-by: Wang Chen <wangchen@cn.fujitsu.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -1992,7 +1992,7 @@ void bond_destroy(struct bonding *bond)
 
 static void bond_destructor(struct net_device *bond_dev)
 {
-	struct bonding *bond = bond_dev->priv;
+	struct bonding *bond = netdev_priv(bond_dev);
 
 	if (bond->wq)
 		destroy_workqueue(bond->wq);
