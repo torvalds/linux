6b3aa6ab787fe545c58554b7ce5c94de53526b83
Jouni Malinen <j@w1.fi>
2008-12-05 20:43:27 +0200

    ath9k: Disable staggered Beacon frame scheduling
    It looks like there are some issues in the current ath9k version as
    far as staggered Beacon frame scheduling is concerned. This results in
    Beacon frame timestamp being off by 25 milliseconds or so which can
    cause issues with multicast power save buffering in AP mode. Some
    client stations fail to receive multicast frames when there is this
    large an offset between TBTT and actual Beacon transmission time.
    
    Since ath9k does not yet support multiple BSSes anyway, the staggered
    Beacon frame scheduling is just increasing the number of interrupts at
    this point. The easiest fix for the timestamp offset is to disable
    staggered Beacon frames for now. We need to redesign beacon.c anyway
    when adding multi-BSS support with mac80211 since the existing code
    depends on different mechanism for Beacon frame transmission.
    
    Signed-off-by: Jouni Malinen <jouni.malinen@atheros.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath9k/core.h
+++ drivers/net/wireless/ath9k/core.h
@@ -489,7 +489,7 @@ struct ath_vap {
  * number of beacon intervals, the game's up.
  */
 #define BSTUCK_THRESH           	(9 * ATH_BCBUF)
-#define	ATH_BCBUF               	4
+#define	ATH_BCBUF               	1
 #define ATH_DEFAULT_BINTVAL     	100 /* TU */
 #define ATH_DEFAULT_BMISS_LIMIT 	10
 #define IEEE80211_MS_TO_TU(x)           (((x) * 1000) / 1024)
