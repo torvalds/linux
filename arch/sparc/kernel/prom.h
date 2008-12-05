5fce09c6f636449d6df971971af1bd4328a21890
David S. Miller <davem@davemloft.net>
2008-12-05 00:43:03 -0800

    sparc: Move irq_trans_init() and support code into seperate file.
    All sparc64 specific, so only build this file on sparc64.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/kernel/prom.h
+++ arch/sparc/kernel/prom.h
@@ -9,4 +9,8 @@ extern rwlock_t devtree_lock;	/* temporary while merging */
 
 extern void * prom_early_alloc(unsigned long size);
 
+#ifdef CONFIG_SPARC64
+extern void irq_trans_init(struct device_node *dp);
+#endif
+
 #endif /* __PROM_H */
