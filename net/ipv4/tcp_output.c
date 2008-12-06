a2acde07711f7d8b19928245c555bce60f91482a
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2008-12-05 22:49:37 -0800

    tcp: fix tso_should_defer in 64bit
    Since jiffies is unsigned long, the types get expanded into
    that and after long enough time the difference will therefore
    always be > 1 (and that probably happens near boot as well as
    iirc the first jiffies wrap is scheduler close after boot to
    find out problems related to that early).
    
    This was originally noted by Bill Fink in Dec'07 but nobody
    never ended fixing it.
    
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -1334,7 +1334,7 @@ static int tcp_tso_should_defer(struct sock *sk, struct sk_buff *skb)
 
 	/* Defer for less than two clock ticks. */
 	if (tp->tso_deferred &&
-	    ((jiffies << 1) >> 1) - (tp->tso_deferred >> 1) > 1)
+	    (((u32)jiffies << 1) >> 1) - (tp->tso_deferred >> 1) > 1)
 		goto send_now;
 
 	in_flight = tcp_packets_in_flight(tp);
