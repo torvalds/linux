1393fce7189427bdb4d6115ca5566ca8d0fc86f3
CSÉCSY László <boobaa@frugalware.org>
2008-12-09 23:39:14 +0100

    USB: storage: unusual_devs.h: Nokia 3109c addition
    2.6.26(.x, cannot remember) could handle the microSD card in my Nokia
    3109c attached via USB as mass storage, 2.6.27(.x, up to and included
    2.6.27.8) cannot. Please find the attached patch which fixes this
    regression, and a copy of /proc/bus/usb/devices with my phone plugged in
    running with this patch on Frugalware.
    
    T:  Bus=02 Lev=01 Prnt=01 Port=01 Cnt=02 Dev#=  4 Spd=12  MxCh= 0
    D:  Ver= 2.00 Cls=00(>ifc ) Sub=00 Prot=00 MxPS=64 #Cfgs=  1
    P:  Vendor=0421 ProdID=0063 Rev= 6.01
    S:  Manufacturer=Nokia
    S:  Product=Nokia 3109c
    S:  SerialNumber=359561013742570
    C:* #Ifs= 1 Cfg#= 1 Atr=c0 MxPwr=100mA
    I:* If#= 0 Alt= 0 #EPs= 2 Cls=08(stor.) Sub=06 Prot=50 Driver=usb-storage
    E:  Ad=81(I) Atr=02(Bulk) MxPS=  64 Ivl=0ms
    E:  Ad=01(O) Atr=02(Bulk) MxPS=  64 Ivl=0ms
    
    From: CSÉCSY László <boobaa@frugalware.org>
    Cc: Phil Dibowitz <phil@ipom.com>
    Cc: stable <stable@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/storage/unusual_devs.h
+++ drivers/usb/storage/unusual_devs.h
@@ -167,6 +167,13 @@ UNUSUAL_DEV(  0x0421, 0x005d, 0x0001, 0x0600,
 		US_SC_DEVICE, US_PR_DEVICE, NULL,
 		US_FL_FIX_CAPACITY ),
 
+/* Reported by CSECSY Laszlo <boobaa@frugalware.org> */
+UNUSUAL_DEV(  0x0421, 0x0063, 0x0001, 0x0601,
+		"Nokia",
+		"Nokia 3109c",
+		US_SC_DEVICE, US_PR_DEVICE, NULL,
+		US_FL_FIX_CAPACITY ),
+
 /* Patch for Nokia 5310 capacity */
 UNUSUAL_DEV(  0x0421, 0x006a, 0x0000, 0x0591,
 		"Nokia",
