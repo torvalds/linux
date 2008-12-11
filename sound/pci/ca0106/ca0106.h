f649a7145b8b67121e8be0b50f7861755f21aa3b
Ben Stanley <Ben.Stanley@exemail.com.au>
2008-12-12 09:47:13 +1100

    ALSA: ca0106 Add comments to snd_ca0106_details struct
    Takashi wrote an email [1] explaining the fields of snd_ca0106_details,
    so I captured the information into the ca0106.h header file.
    
    [1] http://article.gmane.org/gmane.linux.alsa.devel/56783/match=takashi+gpio_type
    
    Signed-off-by: Ben Stanley <Ben.Stanley@exemail.com.au>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/ca0106/ca0106.h
+++ sound/pci/ca0106/ca0106.h
@@ -664,10 +664,14 @@ struct snd_ca0106_pcm {
 struct snd_ca0106_details {
         u32 serial;
         char * name;
-        int ac97;
-	int gpio_type;
-	int i2c_adc;
-	int spi_dac;
+	int ac97;	/* ac97 = 0 -> Select MIC, Line in, TAD in, AUX in.
+			   ac97 = 1 -> Default to AC97 in. */
+	int gpio_type;	/* gpio_type = 1 -> shared mic-in/line-in
+			   gpio_type = 2 -> shared side-out/line-in. */
+	int i2c_adc;	/* with i2c_adc=1, the driver adds some capture volume
+			   controls, phone, mic, line-in and aux. */
+	int spi_dac;	/* spi_dac=1 adds the mute switch for each analog
+			   output, front, rear, etc. */
 };
 
 // definition of the chip-specific record
