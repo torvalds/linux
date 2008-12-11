64ce2907b1966593d3b4ce5396adb17d7348637d
Paul Walmsley <paul@pwsan.com>
2008-12-10 17:36:34 -0800

    ARM: OMAP2: skip unnecessary TLDR write during non-autoreload for gptimer
    The GPTIMER TLDR register does not need to be written if the GPTIMER
    is not in autoreload mode.  This is the usual case for dynamic tick-enabled
    kernels.
    
    Simulation data indicate that skipping the read that occurs as part of
    the write should save at least 300-320 ns for each GPTIMER1 timer
    reprogram.  (This assumes L4-Wakeup is at 19MHz and GPTIMER write
    posting is enabled.)  Skipping the write itself probably won't have
    much impact since it should be posted on the OCP interconnect.
    
    Tested on 2430SDP and 3430SDP.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Cc: Richard Woodruff <r-woodruff2@ti.com>
    Acked-by: Kevin Hilman <khilman@deeprootsystems.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/dmtimer.c
+++ arch/arm/plat-omap/dmtimer.c
@@ -549,14 +549,15 @@ void omap_dm_timer_set_load_start(struct omap_dm_timer *timer, int autoreload,
 	u32 l;
 
 	l = omap_dm_timer_read_reg(timer, OMAP_TIMER_CTRL_REG);
-	if (autoreload)
+	if (autoreload) {
 		l |= OMAP_TIMER_CTRL_AR;
-	else
+		omap_dm_timer_write_reg(timer, OMAP_TIMER_LOAD_REG, load);
+	} else {
 		l &= ~OMAP_TIMER_CTRL_AR;
+	}
 	l |= OMAP_TIMER_CTRL_ST;
 
 	omap_dm_timer_write_reg(timer, OMAP_TIMER_COUNTER_REG, load);
-	omap_dm_timer_write_reg(timer, OMAP_TIMER_LOAD_REG, load);
 	omap_dm_timer_write_reg(timer, OMAP_TIMER_CTRL_REG, l);
 }
 
