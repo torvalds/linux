a79b7a2a758c39315344f0d86b5adb21d90d786e
Jeremy Fitzhardinge <jeremy@goop.org>
2008-12-16 12:17:25 -0800

    x86: remove unused iommu_nr_pages
    Impact: cleanup, remove dead code
    
    The last usage was removed by the patch set culminating in
    
    | commit e3c449f526cebb8d287241c7e82faafd9709668b
    | Author: Joerg Roedel <joerg.roedel@amd.com>
    | Date:   Wed Oct 15 22:02:11 2008 -0700
    |
    |     x86, AMD IOMMU: convert driver to generic iommu_num_pages function
    
    Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/iommu.h
+++ arch/x86/include/asm/iommu.h
@@ -7,8 +7,6 @@ extern struct dma_mapping_ops nommu_dma_ops;
 extern int force_iommu, no_iommu;
 extern int iommu_detected;
 
-extern unsigned long iommu_nr_pages(unsigned long addr, unsigned long len);
-
 /* 10 seconds */
 #define DMAR_OPERATION_TIMEOUT ((cycles_t) tsc_khz*10*1000)
 
