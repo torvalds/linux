40aa4a30d0fd075fb934de4ee8163056827052ab
Mark Brown <broonie@opensource.wolfsonmicro.com>
2008-12-16 10:15:12 +0000

    ASoC: Add WM8350 AudioPlus codec driver
    The WM8350 is an integrated audio and power management subsystem which
    provides a single-chip solution for portable audio and multimedia systems.
    
    The integrated audio CODEC provides all the necessary functions for
    high-quality stereo recording and playback. Programmable on-chip
    amplifiers allow for the direct connection of headphones and microphones
    with a minimum of external components. A programmable low-noise bias
    voltage is available to feed one or more electret microphones.
    Additional audio features include programmable high-pass filter in the
    ADC input path.
    
    This driver was originally written by Liam Girdwood with further updates
    from me.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

---
+++ sound/soc/codecs/wm8350.h
@@ -0,0 +1,20 @@
+/*
+ * wm8350.h - WM8903 audio codec interface
+ *
+ * Copyright 2008 Wolfson Microelectronics PLC.
+ *
+ *  This program is free software; you can redistribute  it and/or modify it
+ *  under  the terms of  the GNU General  Public License as published by the
+ *  Free Software Foundation;  either version 2 of the  License, or (at your
+ *  option) any later version.
+ */
+
+#ifndef _WM8350_H
+#define _WM8350_H
+
+#include <sound/soc.h>
+
+extern struct snd_soc_dai wm8350_dai;
+extern struct snd_soc_codec_device soc_codec_dev_wm8350;
+
+#endif
