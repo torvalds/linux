38d59ef61c11cafc50a66787bdbbe80d58bbd9c0
Tiger Yang <tiger.yang@oracle.com>
2008-12-17 10:22:56 +0800

    ocfs2: Add xattr support checking in init_security
    We must check whether ocfs2 volume support xattr in init_security,
    if not support xattr and security is enable, would cause failure of mknod.
    
    Signed-off-by: Tiger Yang <tiger.yang@oracle.com>
    Signed-off-by: Mark Fasheh <mfasheh@suse.com>

--- fs/ocfs2/xattr.c
+++ fs/ocfs2/xattr.c
@@ -5324,6 +5324,9 @@ int ocfs2_init_security_get(struct inode *inode,
 			    struct inode *dir,
 			    struct ocfs2_security_xattr_info *si)
 {
+	/* check whether ocfs2 support feature xattr */
+	if (!ocfs2_supports_xattr(OCFS2_SB(dir->i_sb)))
+		return -EOPNOTSUPP;
 	return security_inode_init_security(inode, dir, &si->name, &si->value,
 					    &si->value_len);
 }
