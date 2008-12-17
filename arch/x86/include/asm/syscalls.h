7c9c160c54fc545efc23881344593868e5f717bd
Jaswinder Singh <jaswinder@infradead.org>
2008-12-17 23:18:52 +0530

    x86: tls.c declare sys_set_thread_area and sys_get_thread_area before they get used
    Impact: cleanup
    
    In asm/syscalls.h move out sys_set_thread_area() and sys_get_thread_area()
    as they are common for both 32 and 64 bit.
    
    Signed-off-by: Jaswinder Singh <jaswinder@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/syscalls.h
+++ arch/x86/include/asm/syscalls.h
@@ -22,6 +22,10 @@ asmlinkage long sys_ioperm(unsigned long, unsigned long, int);
 /* kernel/ldt.c */
 asmlinkage int sys_modify_ldt(int, void __user *, unsigned long);
 
+/* kernel/tls.c */
+asmlinkage int sys_set_thread_area(struct user_desc __user *);
+asmlinkage int sys_get_thread_area(struct user_desc __user *);
+
 /* X86_32 only */
 #ifdef CONFIG_X86_32
 /* kernel/process_32.c */
@@ -53,10 +57,6 @@ struct old_utsname;
 asmlinkage int sys_uname(struct old_utsname __user *);
 struct oldold_utsname;
 asmlinkage int sys_olduname(struct oldold_utsname __user *);
-
-/* kernel/tls.c */
-asmlinkage int sys_set_thread_area(struct user_desc __user *);
-asmlinkage int sys_get_thread_area(struct user_desc __user *);
 
 /* kernel/vm86_32.c */
 asmlinkage int sys_vm86old(struct pt_regs);
