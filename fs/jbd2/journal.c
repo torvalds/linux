d7cfa4684d82f58e5d7cb73b8a3c88c169937f25
Theodore Ts'o <tytso@mit.edu>
2008-12-17 00:20:45 -0500

    ext4: display average commit time
    Display the average commit time (which is used by the ext4 fsync
    batching patch) in /proc/fs/jbd2/*/info for performance tuning
    purposes.
    
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/jbd2/journal.c
+++ fs/jbd2/journal.c
@@ -40,6 +40,7 @@
 
 #include <asm/uaccess.h>
 #include <asm/page.h>
+#include <asm/div64.h>
 
 EXPORT_SYMBOL(jbd2_journal_start);
 EXPORT_SYMBOL(jbd2_journal_restart);
@@ -824,6 +825,8 @@ static int jbd2_seq_info_show(struct seq_file *seq, void *v)
 	    jiffies_to_msecs(s->stats->u.run.rs_flushing / s->stats->ts_tid));
 	seq_printf(seq, "  %ums logging transaction\n",
 	    jiffies_to_msecs(s->stats->u.run.rs_logging / s->stats->ts_tid));
+	seq_printf(seq, "  %luus average transaction commit time\n",
+		   do_div(s->journal->j_average_commit_time, 1000));
 	seq_printf(seq, "  %lu handles per transaction\n",
 	    s->stats->u.run.rs_handle_count / s->stats->ts_tid);
 	seq_printf(seq, "  %lu blocks per transaction\n",
