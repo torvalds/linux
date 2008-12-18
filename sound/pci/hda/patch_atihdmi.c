3218c178b41b420cb7e0d120c7a137a3969242e5
Takashi Iwai <tiwai@suse.de>
2008-12-18 09:17:56 +0100

    ALSA: hda - Remove duplicated strings from codec name
    Remove codec vendor names from the codec name strings.
    The vendor name is already given from the vendor name table, so
    displayed doubly.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/patch_atihdmi.c
+++ sound/pci/hda/patch_atihdmi.c
@@ -187,10 +187,10 @@ static int patch_atihdmi(struct hda_codec *codec)
  * patch entries
  */
 static struct hda_codec_preset snd_hda_preset_atihdmi[] = {
-	{ .id = 0x1002793c, .name = "ATI RS600 HDMI", .patch = patch_atihdmi },
-	{ .id = 0x10027919, .name = "ATI RS600 HDMI", .patch = patch_atihdmi },
-	{ .id = 0x1002791a, .name = "ATI RS690/780 HDMI", .patch = patch_atihdmi },
-	{ .id = 0x1002aa01, .name = "ATI R6xx HDMI", .patch = patch_atihdmi },
+	{ .id = 0x1002793c, .name = "RS600 HDMI", .patch = patch_atihdmi },
+	{ .id = 0x10027919, .name = "RS600 HDMI", .patch = patch_atihdmi },
+	{ .id = 0x1002791a, .name = "RS690/780 HDMI", .patch = patch_atihdmi },
+	{ .id = 0x1002aa01, .name = "R6xx HDMI", .patch = patch_atihdmi },
 	{ .id = 0x10951390, .name = "SiI1390 HDMI", .patch = patch_atihdmi },
 	{ .id = 0x17e80047, .name = "Chrontel HDMI",  .patch = patch_atihdmi },
 	{} /* terminator */
