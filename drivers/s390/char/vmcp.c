a114a9d69d1362546b9dda651028acb311ddb7af
Christian Borntraeger <borntraeger@de.ibm.com>
2008-12-25 13:38:34 +0100

    [S390] vmcp: remove BKL
    The vmcp driver uses the session->mutex for concurrent access of the data
    structures. Therefore, the BKL in vmcp_open does not protect against any
    other function in the driver.
    The BLK in vmcp_open would protect concurrent access to the module init
    but all necessary steps ave finished before misc_register is called.
    We can safely remove the lock_kernel from vcmp.
    
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/char/vmcp.c
+++ drivers/s390/char/vmcp.c
@@ -16,7 +16,6 @@
 #include <linux/kernel.h>
 #include <linux/miscdevice.h>
 #include <linux/module.h>
-#include <linux/smp_lock.h>
 #include <asm/cpcmd.h>
 #include <asm/debug.h>
 #include <asm/uaccess.h>
@@ -41,13 +40,11 @@ static int vmcp_open(struct inode *inode, struct file *file)
 	if (!session)
 		return -ENOMEM;
 
-	lock_kernel();
 	session->bufsize = PAGE_SIZE;
 	session->response = NULL;
 	session->resp_size = 0;
 	mutex_init(&session->mutex);
 	file->private_data = session;
-	unlock_kernel();
 	return nonseekable_open(inode, file);
 }
 
