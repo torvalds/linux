dfe6b7d9406c631d697f8bbe1eae5569b808154f
Hannes Eder <hannes@hanneseder.net>
2008-12-30 18:49:13 +0300

    proc: fix sparse warning
    fs/proc/base.c:312:4: warning: do-while statement is not a compound statement
    
    Signed-off-by: Hannes Eder <hannes@hanneseder.net>
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -306,9 +306,9 @@ static int proc_pid_auxv(struct task_struct *task, char *buffer)
 	struct mm_struct *mm = get_task_mm(task);
 	if (mm) {
 		unsigned int nwords = 0;
-		do
+		do {
 			nwords += 2;
-		while (mm->saved_auxv[nwords - 2] != 0); /* AT_NULL */
+		} while (mm->saved_auxv[nwords - 2] != 0); /* AT_NULL */
 		res = nwords * sizeof(mm->saved_auxv[0]);
 		if (res > PAGE_SIZE)
 			res = PAGE_SIZE;
