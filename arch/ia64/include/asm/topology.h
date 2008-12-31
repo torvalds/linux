333af15341b2f6cd813c054e1b441d7b6d8e9318
Rusty Russell <rusty@rustcorp.com.au>
2009-01-01 10:12:21 +1030

    cpumask: Introduce topology_core_cpumask()/topology_thread_cpumask(): ia64
    Impact: New API
    
    The old topology_core_siblings() and topology_thread_siblings() return
    a cpumask_t; these new ones return a (const) struct cpumask *.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Signed-off-by: Mike Travis <travis@sgi.com>

--- arch/ia64/include/asm/topology.h
+++ arch/ia64/include/asm/topology.h
@@ -112,6 +112,8 @@ void build_cpu_to_node_map(void);
 #define topology_core_id(cpu)			(cpu_data(cpu)->core_id)
 #define topology_core_siblings(cpu)		(cpu_core_map[cpu])
 #define topology_thread_siblings(cpu)		(per_cpu(cpu_sibling_map, cpu))
+#define topology_core_cpumask(cpu)		(&cpu_core_map[cpu])
+#define topology_thread_cpumask(cpu)		(&per_cpu(cpu_sibling_map, cpu))
 #define smt_capable() 				(smp_num_siblings > 1)
 #endif
 
