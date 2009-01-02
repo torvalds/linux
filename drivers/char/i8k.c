bef2a508b4276fd7897b2cb27df037d26361842c
Federico Heinz <fheinz@vialibre.org.ar>
2009-01-02 16:19:23 +0000

    i8k: Add Dell Vostro systems
    This trivial patch adds support for i8k on the new Dell Vostro models.
    I tested it on my Vostro 1400, and it works. It does print a warning
    when loading the module:
    
    	i8k: unable to get SMM BIOS version
    
    But I couldn't figure out how to fix that. The module seems to work fine,
    anyway...
    
    Signed-off-by: Federico Heinz <fheinz@vialibre.org.ar>
    Signed-off-by: Alan Cox <alan@redhat.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/char/i8k.c
+++ drivers/char/i8k.c
@@ -492,7 +492,14 @@ static struct dmi_system_id __initdata i8k_dmi_table[] = {
 			DMI_MATCH(DMI_PRODUCT_NAME, "Precision"),
 		},
 	},
-	{ }
+	{
+		.ident = "Dell Vostro",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Dell Inc."),
+			DMI_MATCH(DMI_PRODUCT_NAME, "Vostro"),
+		},
+	},
+        { }
 };
 
 /*
