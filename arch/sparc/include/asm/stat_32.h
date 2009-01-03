104e28059d771274b545b4772a27c5c8f9af2767
Sam Ravnborg <sam@ravnborg.org>
2009-01-02 18:39:10 -0800

    sparc32: drop __old_kernel_stat
    sparc32 does not define __ARCH_WANT_OLD_STAT so
    we do not use this structure neither do we support it.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/asm/stat_32.h
+++ arch/sparc/include/asm/stat_32.h
@@ -3,20 +3,6 @@
 
 #include <linux/types.h>
 
-struct __old_kernel_stat {
-	unsigned short st_dev;
-	unsigned short st_ino;
-	unsigned short st_mode;
-	unsigned short st_nlink;
-	unsigned short st_uid;
-	unsigned short st_gid;
-	unsigned short st_rdev;
-	unsigned long  st_size;
-	unsigned long  st_atime;
-	unsigned long  st_mtime;
-	unsigned long  st_ctime;
-};
-
 struct stat {
 	unsigned short	st_dev;
 	unsigned long	st_ino;
