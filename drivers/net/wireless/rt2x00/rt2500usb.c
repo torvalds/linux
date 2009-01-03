f1dd2b23badfe8a28910a78be24452c627c4b6f2
Ivo van Doorn <ivdoorn@gmail.com>
2009-01-03 16:27:14 +0100

    rt2x00: Fix rt2500usb HW crypto: WEP 128 & AES
    The TXD_W0_CIPHER field is a 1-bit field. It only acts as boolean value
    to indicate if the frame must be encrypted or not.
    
    The way rt2x00_set_field32() worked it would grab the least signifcant bit
    from txdesc->cipher and use that as value. Because of that WEP 64 and TKIP
    worked since they had odd-numbered values, while WEP 128 and AES were
    even numbers and didn't work.
    
    Correctly booleanize the txdecs->cipher value to allow the hardware to
    encrypt the outgoing data. After this we can enable HW crypto by default again.
    
    Signed-off-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt2500usb.c
+++ drivers/net/wireless/rt2x00/rt2500usb.c
@@ -38,7 +38,7 @@
 /*
  * Allow hardware encryption to be disabled.
  */
-static int modparam_nohwcrypt = 1;
+static int modparam_nohwcrypt = 0;
 module_param_named(nohwcrypt, modparam_nohwcrypt, bool, S_IRUGO);
 MODULE_PARM_DESC(nohwcrypt, "Disable hardware encryption.");
 
@@ -1181,7 +1181,7 @@ static void rt2500usb_write_tx_desc(struct rt2x00_dev *rt2x00dev,
 			   test_bit(ENTRY_TXD_FIRST_FRAGMENT, &txdesc->flags));
 	rt2x00_set_field32(&word, TXD_W0_IFS, txdesc->ifs);
 	rt2x00_set_field32(&word, TXD_W0_DATABYTE_COUNT, skb->len);
-	rt2x00_set_field32(&word, TXD_W0_CIPHER, txdesc->cipher);
+	rt2x00_set_field32(&word, TXD_W0_CIPHER, !!txdesc->cipher);
 	rt2x00_set_field32(&word, TXD_W0_KEY_ID, txdesc->key_idx);
 	rt2x00_desc_write(txd, 0, word);
 }
