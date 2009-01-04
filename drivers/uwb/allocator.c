04c470adb01c62bb9bd663cfc4875cf0a4eb01ab
Huang Weiyi <weiyi.huang@gmail.com>
2009-01-04 11:13:50 +0800

    uwb: remove unused #include <version.h>'s
    Remove unused #include <version.h>'s in file(s) below,
      drivers/uwb/allocator.c
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: David Vrabel <david.vrabel@csr.com>

--- drivers/uwb/allocator.c
+++ drivers/uwb/allocator.c
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <linux/version.h>
 #include <linux/kernel.h>
 #include <linux/uwb.h>
 
