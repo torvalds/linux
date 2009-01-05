ca4787b779dd698a2a33a328aa5fa90a3e954077
Tim Abbott <tabbott@MIT.EDU>
2009-01-05 08:40:10 -0600

    kernel/module.c: compare symbol values when marking symbols as exported in /proc/kallsyms.
    When there are two symbols in a module with the same name, one of which is
    exported, both will be marked as exported in /proc/kallsyms.  There aren't
    any instances of this in the current kernel, but it is easy to construct a
    simple module with two compilation units that exhibits the problem.
    
    $ objdump -j .text -t testmod.ko | grep foo
    00000000 l     F .text	00000032 foo
    00000080 g     F .text	00000001 foo
    $ sudo insmod testmod.ko
    $ grep "T foo" /proc/kallsyms
    c28e8000 T foo	[testmod]
    c28e8080 T foo	[testmod]
    
    Fix this by comparing the symbol values once we've found the exported
    symbol table entry matching the symbol name.  Tested using Ksplice:
    
    $ ksplice-create --patch=this_commit.patch --id=bar .
    $ sudo ksplice-apply ksplice-bar.tar.gz
    Done!
    $ grep "T foo" /proc/kallsyms
    c28e8080 T foo	[testmod]
    
    Signed-off-by: Tim Abbott <tabbott@mit.edu>
    Cc: Rusty Russell <rusty@rustcorp.com.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- kernel/module.c
+++ kernel/module.c
@@ -1725,15 +1725,15 @@ static const struct kernel_symbol *lookup_symbol(const char *name,
 	return NULL;
 }
 
-static int is_exported(const char *name, const struct module *mod)
+static int is_exported(const char *name, unsigned long value,
+		       const struct module *mod)
 {
-	if (!mod && lookup_symbol(name, __start___ksymtab, __stop___ksymtab))
-		return 1;
+	const struct kernel_symbol *ks;
+	if (!mod)
+		ks = lookup_symbol(name, __start___ksymtab, __stop___ksymtab);
 	else
-		if (mod && lookup_symbol(name, mod->syms, mod->syms + mod->num_syms))
-			return 1;
-		else
-			return 0;
+		ks = lookup_symbol(name, mod->syms, mod->syms + mod->num_syms);
+	return ks != NULL && ks->value == value;
 }
 
 /* As per nm */
@@ -2504,7 +2504,7 @@ int module_get_kallsym(unsigned int symnum, unsigned long *value, char *type,
 			strlcpy(name, mod->strtab + mod->symtab[symnum].st_name,
 				KSYM_NAME_LEN);
 			strlcpy(module_name, mod->name, MODULE_NAME_LEN);
-			*exported = is_exported(name, mod);
+			*exported = is_exported(name, *value, mod);
 			preempt_enable();
 			return 0;
 		}
