080ba929651a32f1840751d2b862e64c8ee1f0c6
Mike Frysinger <vapier@gentoo.org>
2009-01-06 14:41:25 -0800

    checkpatch: try to catch missing VMLINUX_SYMBOL() in vmlinux.lds.h
    Seems like every other release we have someone who updates vmlinux.lds.h
    and adds C-visible symbols without VMLINUX_SYMBOL() around them.  So start
    checking the file and reject assignments which have plain symbols on
    either side.
    
    [apw@canonical.com: soften the check, add tests]
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -2224,6 +2224,15 @@ sub process {
 			}
 		}
 
+# make sure symbols are always wrapped with VMLINUX_SYMBOL() ...
+# all assignments may have only one of the following with an assignment:
+#	.
+#	ALIGN(...)
+#	VMLINUX_SYMBOL(...)
+		if ($realfile eq 'vmlinux.lds.h' && $line =~ /(?:(?:^|\s)$Ident\s*=|=\s*$Ident(?:\s|$))/) {
+			WARN("vmlinux.lds.h needs VMLINUX_SYMBOL() around C-visible symbols\n" . $herecurr);
+		}
+
 # check for redundant bracing round if etc
 		if ($line =~ /(^.*)\bif\b/ && $1 !~ /else\s*$/) {
 			my ($level, $endln, @chunks) =
