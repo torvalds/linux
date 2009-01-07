a0dcfb16e606ca095eb1e9e789aff5e41e9adb1a
Graf Yang <graf.yang@analog.com>
2009-01-07 23:14:39 +0800

    Blackfin arch: Fix bug - IrDA SIR build failed for BF533.
    Signed-off-by: Graf Yang <graf.yang@analog.com>
    Signed-off-by: Bryan Wu <cooloney@kernel.org>

--- arch/blackfin/mach-bf533/include/mach/dma.h
+++ arch/blackfin/mach-bf533/include/mach/dma.h
@@ -16,8 +16,8 @@
 #define CH_SPORT1_RX    3
 #define CH_SPORT1_TX    4
 #define CH_SPI          5
-#define CH_UART_RX      6
-#define CH_UART_TX      7
+#define CH_UART0_RX     6
+#define CH_UART0_TX     7
 #define CH_MEM_STREAM0_DEST     8	 /* TX */
 #define CH_MEM_STREAM0_SRC      9	 /* RX */
 #define CH_MEM_STREAM1_DEST     10	 /* TX */
