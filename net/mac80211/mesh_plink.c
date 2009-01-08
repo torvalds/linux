1acc97b63a3f32481ebbb4e831323e9aa8834f66
Jouni Malinen <j@w1.fi>
2009-01-08 13:32:07 +0200

    mac80211: 802.11w - Do not force Action frames to disable encryption
    When sending out Action frames, allow ieee80211_tx_skb() to send them
    without enforcing do_not_encrypt. These frames will be encrypted if
    MFP has been negotiated.
    
    Signed-off-by: Jouni Malinen <jouni.malinen@atheros.com>
    Acked-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/mesh_plink.c
+++ net/mac80211/mesh_plink.c
@@ -218,7 +218,7 @@ static int mesh_plink_frame_tx(struct ieee80211_sub_if_data *sdata,
 		memcpy(pos, &reason, 2);
 	}
 
-	ieee80211_tx_skb(sdata, skb, 0);
+	ieee80211_tx_skb(sdata, skb, 1);
 	return 0;
 }
 
