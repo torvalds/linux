a14a07b8018b714e03a39ff2180c66e307ef4238
Shaohua Li <shaohua.li@intel.com>
2009-01-09 11:29:49 +0800

    ftrace, ia64: IA64 dynamic ftrace support
    IA64 dynamic ftrace support.
    The original _mcount stub for each function is like:
    	alloc r40=ar.pfs,12,8,0
    	mov r43=r0;;
    	mov r42=b0
    	mov r41=r1
    	nop.i 0x0
    	br.call.sptk.many b0 = _mcount;;
    
    The patch convert it to below for nop:
    	[MII] nop.m 0x0
    	mov r3=ip
    	nop.i 0x0
    	[MLX] nop.m 0x0
    	nop.x 0x0;;
    This isn't completely nop, as there is one instuction 'mov r3=ip', but
    it should be light and harmless for code follow it.
    
    And below is for call
    	[MII] nop.m 0x0
    	mov r3=ip
    	nop.i 0x0
    	[MLX] nop.m 0x0
    	brl.many .;;
    In this way, only one instruction is changed to convert code between nop
    and call. This should meet dyn-ftrace's requirement.
    But this requires CPU support brl instruction, so dyn-ftrace isn't
    supported for old Itanium system. Assume there are quite few such old
    system running.
    
    Signed-off-by: Shaohua Li <shaohua.li@intel.com>
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/ia64/include/asm/ftrace.h
+++ arch/ia64/include/asm/ftrace.h
@@ -8,6 +8,19 @@
 extern void _mcount(unsigned long pfs, unsigned long r1, unsigned long b0, unsigned long r0);
 #define mcount _mcount
 
+#include <asm/kprobes.h>
+/* In IA64, MCOUNT_ADDR is set in link time, so it's not a constant at compile time */
+#define MCOUNT_ADDR (((struct fnptr *)mcount)->ip)
+#define FTRACE_ADDR (((struct fnptr *)ftrace_caller)->ip)
+
+static inline unsigned long ftrace_call_adjust(unsigned long addr)
+{
+	/* second bundle, insn 2 */
+	return addr - 0x12;
+}
+
+struct dyn_arch_ftrace {
+};
 #endif
 
 #endif /* CONFIG_FUNCTION_TRACER */
