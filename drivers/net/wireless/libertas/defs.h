d2b21f191753abd12c4063776cb1a3d635397509
Colin McCabe <colin@cozybit.com>
2009-01-09 14:58:09 -0800

    libertas: if_spi, driver for libertas GSPI devices
    Add initial support for libertas devices using a GSPI interface.  This has
    been tested with the 8686.
    
    GSPI is intended to be used on embedded systems. Board-specific parameters are
    required (see libertas_spi.h).
    
    Thanks to everyone who took a look at the earlier versions of the patch.
    
    Signed-off-by: Colin McCabe <colin@cozybit.com>
    Signed-off-by: Andrey Yurovsky <andrey@cozybit.com>
    Acked-by: Dan Williams <dcbw@redhat.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/defs.h
+++ drivers/net/wireless/libertas/defs.h
@@ -41,6 +41,7 @@
 #define LBS_DEB_HEX	0x00200000
 #define LBS_DEB_SDIO	0x00400000
 #define LBS_DEB_SYSFS	0x00800000
+#define LBS_DEB_SPI	0x01000000
 
 extern unsigned int lbs_debug;
 
@@ -84,6 +85,7 @@ do { if ((lbs_debug & (grp)) == (grp)) \
 #define lbs_deb_thread(fmt, args...)    LBS_DEB_LL(LBS_DEB_THREAD, " thread", fmt, ##args)
 #define lbs_deb_sdio(fmt, args...)      LBS_DEB_LL(LBS_DEB_SDIO, " sdio", fmt, ##args)
 #define lbs_deb_sysfs(fmt, args...)     LBS_DEB_LL(LBS_DEB_SYSFS, " sysfs", fmt, ##args)
+#define lbs_deb_spi(fmt, args...)       LBS_DEB_LL(LBS_DEB_SPI, " spi", fmt, ##args)
 
 #define lbs_pr_info(format, args...) \
 	printk(KERN_INFO DRV_NAME": " format, ## args)
