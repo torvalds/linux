93423b8665f43a0c7a006a1d5be048b99db56d32
Ingo Molnar <mingo@elte.hu>
2009-01-11 05:15:21 +0100

    smp_call_function_single(): be slightly less stupid, fix
    Impact: build fix on Alpha
    
     kernel/up.c: In function 'smp_call_function_single':
     kernel/up.c:12: error: 'cpuid' undeclared (first use in this function)
     kernel/up.c:12: error: (Each undeclared identifier is reported only once
     kernel/up.c:12: error: for each function it appears in.)
    
    The typo didnt show up on x86 because 'cpuid' happens to be a
    function address as well ...
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/up.c
+++ kernel/up.c
@@ -9,10 +9,12 @@
 int smp_call_function_single(int cpu, void (*func) (void *info), void *info,
 				int wait)
 {
-	WARN_ON(cpuid != 0);
+	WARN_ON(cpu != 0);
+
 	local_irq_disable();
 	(func)(info);
 	local_irq_enable();
+
 	return 0;
 }
 EXPORT_SYMBOL(smp_call_function_single);
