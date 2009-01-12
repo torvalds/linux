0762a4824d6c6f8eb5d2646dfda95581d99afaa5
Hannes Reinecke <hare@suse.de>
2009-01-12 09:28:55 +0100

    [SCSI] Check for deleted device in scsi_device_online()
    scsi_device_online() is not just a negation of SDEV_OFFLINE,
    also devices in state SDEV_DEL are actually offline.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- include/scsi/scsi_device.h
+++ include/scsi/scsi_device.h
@@ -401,7 +401,8 @@ static inline unsigned int sdev_id(struct scsi_device *sdev)
  */
 static inline int scsi_device_online(struct scsi_device *sdev)
 {
-	return sdev->sdev_state != SDEV_OFFLINE;
+	return (sdev->sdev_state != SDEV_OFFLINE &&
+		sdev->sdev_state != SDEV_DEL);
 }
 static inline int scsi_device_blocked(struct scsi_device *sdev)
 {
