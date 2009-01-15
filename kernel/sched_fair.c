6272d68cc6a5f90c6b1a2228cf0f67b895305d17
Lin Ming <ming.m.lin@intel.com>
2009-01-15 17:17:15 +0100

    sched: sched_slice() fixlet
    Mike's change: 0a582440f "sched: fix sched_slice())" broke group
    scheduling by forgetting to reload cfs_rq on each loop.
    
    This patch fixes aim7 regression and specjbb2005 regression becomes
    less than 1.5% on 8-core stokley.
    
    Signed-off-by: Lin Ming <ming.m.lin@intel.com>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Tested-by: Jayson King <dev@jaysonking.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -429,7 +429,10 @@ static u64 sched_slice(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	u64 slice = __sched_period(cfs_rq->nr_running + !se->on_rq);
 
 	for_each_sched_entity(se) {
-		struct load_weight *load = &cfs_rq->load;
+		struct load_weight *load;
+
+		cfs_rq = cfs_rq_of(se);
+		load = &cfs_rq->load;
 
 		if (unlikely(!se->on_rq)) {
 			struct load_weight lw = cfs_rq->load;
