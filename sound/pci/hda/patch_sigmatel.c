e0c0e943af71c0f840a1f6a32a8cf0b61ebc61e5
Takashi Iwai <tiwai@suse.de>
2009-01-22 12:58:11 +0100

    ALSA: hda - Add model entry for HP dv4
    Added model=hp-dv5 for HP dv4 (103c:30f7).
    
    Reference: kernel bug #12440
    	http://bugzilla.kernel.org/show_bug.cgi?id=12440
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/patch_sigmatel.c
+++ sound/pci/hda/patch_sigmatel.c
@@ -1797,6 +1797,8 @@ static struct snd_pci_quirk stac92hd71bxx_cfg_tbl[] = {
 		      "HP dv5", STAC_HP_M4),
 	SND_PCI_QUIRK(PCI_VENDOR_ID_HP, 0x30f4,
 		      "HP dv7", STAC_HP_M4),
+	SND_PCI_QUIRK(PCI_VENDOR_ID_HP, 0x30f7,
+		      "HP dv4", STAC_HP_DV5),
 	SND_PCI_QUIRK(PCI_VENDOR_ID_HP, 0x30fc,
 		      "HP dv7", STAC_HP_M4),
 	SND_PCI_QUIRK(PCI_VENDOR_ID_HP, 0x3603,
