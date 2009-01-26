26285ba35813063ade9abd2c2eaaddba9354f587
Daniele Venzano <venza@brownhat.org>
2009-01-26 12:24:38 -0800

    isdn: Fix missing ifdef in isdn_ppp
    The following patch fixes a warning caused by a missing ifdef in
    isdn_ppp.c. A function was defined, but never used if CONFIG_IPPP_FILTER
    was not defined.
    
    The warning was: 'get_filter' defined but not used
    Patch is against 2.6.28.1
    
    Signed-off-by: Daniele Venzano <venza@brownhat.org>
    Acked-by: Karsten Keil <kkeil@suse.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/isdn/i4l/isdn_ppp.c
+++ drivers/isdn/i4l/isdn_ppp.c
@@ -431,6 +431,7 @@ set_arg(void __user *b, void *val,int len)
 	return 0;
 }
 
+#ifdef CONFIG_IPPP_FILTER
 static int get_filter(void __user *arg, struct sock_filter **p)
 {
 	struct sock_fprog uprog;
@@ -465,6 +466,7 @@ static int get_filter(void __user *arg, struct sock_filter **p)
 	*p = code;
 	return uprog.len;
 }
+#endif /* CONFIG_IPPP_FILTER */
 
 /*
  * ippp device ioctl
