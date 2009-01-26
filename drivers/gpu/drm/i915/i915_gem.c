aeb565dfc3ac4c8b47c5049085b4c7bfb2c7d5d7
Linus Torvalds <torvalds@linux-foundation.org>
2009-01-26 10:01:53 -0800

    Fix annoying DRM_ERROR() string warning
    Use '%zu' to print out a size_t variable, not '%d'.  Another case of the
    "let's keep at least Linus' defconfig compile warningless" rule.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -3427,7 +3427,7 @@ i915_gem_attach_phys_object(struct drm_device *dev,
 		ret = i915_gem_init_phys_object(dev, id,
 						obj->size);
 		if (ret) {
-			DRM_ERROR("failed to init phys object %d size: %d\n", id, obj->size);
+			DRM_ERROR("failed to init phys object %d size: %zu\n", id, obj->size);
 			goto out;
 		}
 	}
