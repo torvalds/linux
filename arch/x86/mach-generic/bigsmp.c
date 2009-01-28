d8a3539e64f8e27b0ab5bb7e7ba3b8f34b739224
Ingo Molnar <mingo@elte.hu>
2009-01-28 04:16:01 +0100

    x86, apic: remove BIGSMP_IRQ_DELIVERY_MODE and BIGSMP_IRQ_DEST_MODE
    Impact: cleanup
    
    They were only used in a single place and obscured the apic_bigsmp
    driver template.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/mach-generic/bigsmp.c
+++ arch/x86/mach-generic/bigsmp.c
@@ -64,8 +64,9 @@ struct genapic apic_bigsmp = {
 	.acpi_madt_oem_check		= NULL,
 	.apic_id_registered		= bigsmp_apic_id_registered,
 
-	.irq_delivery_mode		= BIGSMP_IRQ_DELIVERY_MODE,
-	.irq_dest_mode			= BIGSMP_IRQ_DEST_MODE,
+	.irq_delivery_mode		= dest_Fixed,
+	/* phys delivery to target CPU: */
+	.irq_dest_mode			= 0,
 
 	.target_cpus			= target_cpus,
 	.ESR_DISABLE			= esr_disable,
