6f7607ccd175518a3ee7dccc1620f3a086689668
Russell King <rmk@dyn-67.arm.linux.org.uk>
2009-01-28 10:22:50 +0000

    [ARM] omap: hsmmc: new short connection id names
    ... rather than the clock names themselves.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -349,11 +349,11 @@ static void __init omap_hsmmc_reset(void)
 
 		dummy_pdev.id = i;
 		dev_set_name(&dummy_pdev.dev, "mmci-omap-hs.%d", i);
-		iclk = clk_get(dev, "mmchs_ick");
+		iclk = clk_get(dev, "ick");
 		if (iclk && clk_enable(iclk))
 			iclk = NULL;
 
-		fclk = clk_get(dev, "mmchs_fck");
+		fclk = clk_get(dev, "fck");
 		if (fclk && clk_enable(fclk))
 			fclk = NULL;
 
