d63f3a5857906851b9c1a39e3871a97f4acc1005
Paul Mundt <lethal@linux-sh.org>
2009-01-29 18:10:13 +0900

    sh: Fix up MTU2 support for SH7203.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/timers/timer-mtu2.c
+++ arch/sh/kernel/timers/timer-mtu2.c
@@ -35,7 +35,8 @@
 #define MTU2_TSR_1	0xfffe4385
 #define MTU2_TCNT_1	0xfffe4386	/* 16-bit counter */
 
-#if defined(CONFIG_CPU_SUBTYPE_SH7201)
+#if defined(CONFIG_CPU_SUBTYPE_SH7201) || \
+    defined(CONFIG_CPU_SUBTYPE_SH7203)
 #define MTU2_TGRA_1	0xfffe4388
 #else
 #define MTU2_TGRA_1	0xfffe438a
