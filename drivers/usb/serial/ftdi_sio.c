e38c287447e5a3ff905a59dd81269c14cd12ffa1
Stephane Clerambault <stephane.clerambault@free.fr>
2009-02-02 13:39:12 -0800

    USB: ftdi_sio: add support for the NDI Polaris system
    Add support for the NDI Polaris system *http://www.ndigital.com/).
    
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Cc: Oliver Neukum <oneukum@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio.c
+++ drivers/usb/serial/ftdi_sio.c
@@ -662,6 +662,7 @@ static struct usb_device_id id_table_combined [] = {
 	{ USB_DEVICE(FTDI_VID, FTDI_DOMINTELL_DUSB_PID) },
 	{ USB_DEVICE(ALTI2_VID, ALTI2_N3_PID) },
 	{ USB_DEVICE(FTDI_VID, DIEBOLD_BCS_SE923_PID) },
+	{ USB_DEVICE(FTDI_VID, FTDI_NDI_HUC_PID) },
 	{ },					/* Optional parameter entry */
 	{ }					/* Terminating entry */
 };
