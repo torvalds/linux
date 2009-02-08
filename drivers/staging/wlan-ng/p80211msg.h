c27f880c34ed8e702f3c9f0414489e5ebdde84e1
Moritz Muehlenhoff <jmm@debian.org>
2009-02-08 02:20:58 +0100

    Staging: wlan-ng: p80211msg.h: Coding style cleanups
    Signed-off-by: Moritz Muehlenhoff <jmm@debian.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/wlan-ng/p80211msg.h
+++ drivers/staging/wlan-ng/p80211msg.h
@@ -55,25 +55,15 @@
 #include "wlan_compat.h"
 #endif
 
-/*================================================================*/
-/* Constants */
-
 #define WLAN_DEVNAMELEN_MAX	16
-
-/*================================================================*/
-/* Types */
-
-/*--------------------------------------------------------------------*/
-/*----- Message Structure Types --------------------------------------*/
 
 /*--------------------------------------------------------------------*/
 /* Prototype msg type */
 
-typedef struct p80211msg
-{
-	u32	msgcode;
-	u32	msglen;
-	u8	devname[WLAN_DEVNAMELEN_MAX];
-} __attribute__((packed)) p80211msg_t;
+typedef struct p80211msg {
+	u32 msgcode;
+	u32 msglen;
+	u8 devname[WLAN_DEVNAMELEN_MAX];
+} __attribute__ ((packed)) p80211msg_t;
 
-#endif  /* _P80211MSG_H */
+#endif /* _P80211MSG_H */
