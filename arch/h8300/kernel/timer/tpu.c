b04f376151d17cb7cad48c932ba516622c167b44
Tobias Klauser <tklauser@distanz.ch>
2009-02-09 21:39:32 +0100

    trivial: h8300: Storage class should be before const qualifier
    The C99 specification states in section 6.11.5:
    
    The placement of a storage-class specifier other than at the beginning
    of the declaration specifiers in a declaration is an obsolescent
    feature.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- arch/h8300/kernel/timer/tpu.c
+++ arch/h8300/kernel/timer/tpu.c
@@ -68,7 +68,7 @@ static struct irqaction tpu_irq = {
 	.mask		= CPU_MASK_NONE,
 };
 
-const static int __initdata divide_rate[] = {
+static const int __initdata divide_rate[] = {
 #if CONFIG_H8300_TPU_CH == 0
 	1,4,16,64,0,0,0,0,
 #elif (CONFIG_H8300_TPU_CH == 1) || (CONFIG_H8300_TPU_CH == 5)
