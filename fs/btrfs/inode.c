b335b0034e252e79ec2e9c6697f5d663c4627bec
Yan Zheng <zheng.yan@oracle.com>
2009-02-12 10:06:04 -0500

    Btrfs: Avoid using __GFP_HIGHMEM with slab allocator
    btrfs_releasepage may call kmem_cache_alloc indirectly,
    and provide same GFP flags it gets to kmem_cache_alloc.
    So it's possible to use __GFP_HIGHMEM with the slab
    allocator.
    
    Signed-off-by: Yan Zheng <zheng.yan@oracle.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -4263,7 +4263,7 @@ static int btrfs_releasepage(struct page *page, gfp_t gfp_flags)
 {
 	if (PageWriteback(page) || PageDirty(page))
 		return 0;
-	return __btrfs_releasepage(page, gfp_flags);
+	return __btrfs_releasepage(page, gfp_flags & GFP_NOFS);
 }
 
 static void btrfs_invalidatepage(struct page *page, unsigned long offset)
