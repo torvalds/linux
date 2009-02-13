20db7cb0acd0ba5a3b12f686148d670294a69366
Takashi Iwai <tiwai@suse.de>
2009-02-13 08:18:48 +0100

    ALSA: hda - Add forced codec-slots for ASUS W5F
    ASUS W5F needs the fixed codec-slots to probe to override the BIOS
    problem.
    
    Tested-by: Giovanni Moser Frainer <giovanni@redix.com.br>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -2098,6 +2098,8 @@ static struct snd_pci_quirk probe_mask_list[] __devinitdata = {
 	SND_PCI_QUIRK(0x1028, 0x20ac, "Dell Studio Desktop", 0x01),
 	/* including bogus ALC268 in slot#2 that conflicts with ALC888 */
 	SND_PCI_QUIRK(0x17c0, 0x4085, "Medion MD96630", 0x01),
+	/* forced codec slots */
+	SND_PCI_QUIRK(0x1046, 0x1262, "ASUS W5F", 0x103),
 	{}
 };
 
