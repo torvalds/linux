fc68ed4fd7fdfafbefb270cf44194096156c7943
Hannes Eder <hannes@hanneseder.net>
2009-02-14 11:50:06 +0000

    drivers/net/wireless/b43: fix warning: format not a string literal and no ...
    Fix this compilation warning:
      drivers/net/wireless/b43/main.c: In function 'b43_print_fw_helptext':
      drivers/net/wireless/b43/main.c:1971: warning: format not a string literal and no format arguments
      drivers/net/wireless/b43/main.c:1973: warning: format not a string literal and no format arguments
    
    Signed-off-by: Hannes Eder <hannes@hanneseder.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/wireless/b43/main.c
+++ drivers/net/wireless/b43/main.c
@@ -1961,12 +1961,12 @@ static void b43_release_firmware(struct b43_wldev *dev)
 
 static void b43_print_fw_helptext(struct b43_wl *wl, bool error)
 {
-	const char *text;
+	const char text[] =
+		"You must go to " \
+		"http://wireless.kernel.org/en/users/Drivers/b43#devicefirmware " \
+		"and download the correct firmware for this driver version. " \
+		"Please carefully read all instructions on this website.\n";
 
-	text = "You must go to "
-	       "http://wireless.kernel.org/en/users/Drivers/b43#devicefirmware "
-	       "and download the correct firmware for this driver version. "
-	       "Please carefully read all instructions on this website.\n";
 	if (error)
 		b43err(wl, text);
 	else
