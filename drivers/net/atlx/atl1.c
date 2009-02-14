b79d8fff0e54a19e96cbde2b1f79e0fd503c2c53
Hannes Eder <hannes@hanneseder.net>
2009-02-14 11:15:17 +0000

    drivers/net/atlx: fix sparse warnings: fix signedness
    Impact: While being at it: statics do not need to be initialized with 0.
    
    Fix this sparse warnings:
      drivers/net/atlx/atl1.c:109:1: warning: incorrect type in initializer (different signedness)
      drivers/net/atlx/atl2.c:2870:1: warning: incorrect type in initializer (different signedness)
      drivers/net/atlx/atl2.c:2880:1: warning: incorrect type in initializer (different signedness)
      drivers/net/atlx/atl2.c:2894:1: warning: incorrect type in initializer (different signedness)
      drivers/net/atlx/atl2.c:2904:1: warning: incorrect type in initializer (different signedness)
      drivers/net/atlx/atl2.c:2913:1: warning: incorrect type in initializer (different signedness)
    
    Signed-off-by: Hannes Eder <hannes@hanneseder.net>
    Acked-by: Jay Cliburn <jcliburn@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/atlx/atl1.c
+++ drivers/net/atlx/atl1.c
@@ -105,7 +105,7 @@
  * Default Value: 100 (200us)
  */
 static int __devinitdata int_mod_timer[ATL1_MAX_NIC+1] = ATL1_PARAM_INIT;
-static int num_int_mod_timer;
+static unsigned int num_int_mod_timer;
 module_param_array_named(int_mod_timer, int_mod_timer, int,
 	&num_int_mod_timer, 0);
 MODULE_PARM_DESC(int_mod_timer, "Interrupt moderator timer");
