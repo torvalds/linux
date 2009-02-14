5ed1e981bd243d78f95738a355bd117cf1f6daa1
Hannes Eder <hannes@hanneseder.net>
2009-02-14 11:48:25 +0000

    drivers/net/wan: fix sparse warning: symbol shadows an earlier one
    Impact: Rename function scope variable and while being at it fix some
    coding style issues.
    
    Fix this sparse warning:
      drivers/net/wan/sbni.c:1526:14: warning: symbol 'num' shadows an earlier one
      drivers/net/wan/sbni.c:160:13: originally declared here
    
    Signed-off-by: Hannes Eder <hannes@hanneseder.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/wan/sbni.c
+++ drivers/net/wan/sbni.c
@@ -1520,17 +1520,18 @@ int __init init_module( void )
 }
 
 void
-cleanup_module( void )
+cleanup_module(void)
 {
-	struct net_device  *dev;
-	int  num;
+	int i;
 
-	for( num = 0;  num < SBNI_MAX_NUM_CARDS;  ++num )
-		if( (dev = sbni_cards[ num ]) != NULL ) {
-			unregister_netdev( dev );
-			release_region( dev->base_addr, SBNI_IO_EXTENT );
-			free_netdev( dev );
+	for (i = 0;  i < SBNI_MAX_NUM_CARDS;  ++i) {
+		struct net_device *dev = sbni_cards[i];
+		if (dev != NULL) {
+			unregister_netdev(dev);
+			release_region(dev->base_addr, SBNI_IO_EXTENT);
+			free_netdev(dev);
 		}
+	}
 }
 
 #else	/* MODULE */
