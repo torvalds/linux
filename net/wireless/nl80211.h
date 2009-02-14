13e967b2926a51e1913ea42711eaf4108372fd44
Randy Dunlap <randy.dunlap@oracle.com>
2009-02-13 16:39:35 -0800

    wireless: fix for CONFIG_NL80211=n
    Add empty function for case of CONFIG_NL80211=n:
    
    net/wireless/scan.c:35: error: implicit declaration of function 'nl80211_send_scan_aborted'
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Acked-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/wireless/nl80211.h
+++ net/wireless/nl80211.h
@@ -27,6 +27,10 @@ static inline void
 nl80211_send_scan_done(struct cfg80211_registered_device *rdev,
 		       struct net_device *netdev)
 {}
+static inline void nl80211_send_scan_aborted(
+					struct cfg80211_registered_device *rdev,
+					struct net_device *netdev)
+{}
 #endif /* CONFIG_NL80211 */
 
 #endif /* __NET_WIRELESS_NL80211_H */
