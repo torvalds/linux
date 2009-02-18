59544d33ff3118f22a484d8be06cdf5cfc2fdca5
Mark Brown <broonie@opensource.wolfsonmicro.com>
2009-02-18 11:36:44 +0000

    ASoC: Remove version display from the WM8753 driver
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm8753.c
+++ sound/soc/codecs/wm8753.c
@@ -51,8 +51,6 @@
 
 #include "wm8753.h"
 
-#define WM8753_VERSION "0.16"
-
 static int caps_charge = 2000;
 module_param(caps_charge, int, 0);
 MODULE_PARM_DESC(caps_charge, "WM8753 cap charge time (msecs)");
@@ -1777,8 +1775,6 @@ static int wm8753_probe(struct platform_device *pdev)
 	struct snd_soc_codec *codec;
 	struct wm8753_priv *wm8753;
 	int ret = 0;
-
-	pr_info("WM8753 Audio Codec %s", WM8753_VERSION);
 
 	setup = socdev->codec_data;
 	codec = kzalloc(sizeof(struct snd_soc_codec), GFP_KERNEL);
