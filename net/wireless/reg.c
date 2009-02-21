d335fe6391c2d86582cf71ef5773a161ee604608
Luis R. Rodriguez <lrodriguez@atheros.com>
2009-02-21 00:04:27 -0500

    cfg80211: protect first access of last_request on 11d hint under mutex
    We were not protecting last_request there is a small possible race
    between an 11d hint and another routine which calls reset_regdomains()
    which can prevent a valid country IE from being processed. This is
    not critical as it will still be procesed soon after but locking prior
    to it is correct.
    
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/wireless/reg.c
+++ net/wireless/reg.c
@@ -1261,10 +1261,12 @@ void regulatory_hint_11d(struct wiphy *wiphy,
 	u32 checksum = 0;
 	enum environment_cap env = ENVIRON_ANY;
 
-	if (!last_request)
-		return;
-
 	mutex_lock(&cfg80211_mutex);
+
+	if (unlikely(!last_request)) {
+		mutex_unlock(&cfg80211_mutex);
+		return;
+	}
 
 	/* IE len must be evenly divisible by 2 */
 	if (country_ie_len & 0x01)
