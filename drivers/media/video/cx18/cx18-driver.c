006e7179577b40be0b97a93c11def536a64abac0
Andy Walls <awalls@radix.net>
2009-02-21 22:44:50 -0300

    V4L/DVB (10764): cx18: Disable AC3 controls as the firmware doesn't support AC3
    Signed-off-by: Andy Walls <awalls@radix.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx18/cx18-driver.c
+++ drivers/media/video/cx18/cx18-driver.c
@@ -587,7 +587,7 @@ static int __devinit cx18_init_struct1(struct cx18 *cx)
 		(cx->params.video_median_filter_type << 2);
 	cx->params.port = CX2341X_PORT_MEMORY;
 	cx->params.capabilities =
-	  CX2341X_CAP_HAS_TS | CX2341X_CAP_HAS_AC3 | CX2341X_CAP_HAS_SLICED_VBI;
+				CX2341X_CAP_HAS_TS | CX2341X_CAP_HAS_SLICED_VBI;
 	init_waitqueue_head(&cx->cap_w);
 	init_waitqueue_head(&cx->mb_apu_waitq);
 	init_waitqueue_head(&cx->mb_cpu_waitq);
