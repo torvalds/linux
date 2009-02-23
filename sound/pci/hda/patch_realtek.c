2d4663816064fabb68935f920bbd7ccdc7f9392d
Luke Yelavich <themuso@ubuntu.com>
2009-02-23 13:00:33 +1100

    ALSA: hda - add another MacBook Pro 3,1 SSID
    Reference: Ubuntu bug #33245
        https://bugs.launchpad.net/bugs/332456
    
    Signed-off-by: Luke Yelavich <themuso@ubuntu.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -7017,6 +7017,7 @@ static int patch_alc882(struct hda_codec *codec)
 		case 0x106b3e00: /* iMac 24 Aluminium */
 			board_config = ALC885_IMAC24;
 			break;
+		case 0x106b00a0: /* MacBookPro3,1 - Another revision */
 		case 0x106b00a1: /* Macbook (might be wrong - PCI SSID?) */
 		case 0x106b00a4: /* MacbookPro4,1 */
 		case 0x106b2c00: /* Macbook Pro rev3 */
