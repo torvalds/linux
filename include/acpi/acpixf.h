2affa28605fa5387192c72d9889a00c9c51aa712
Bob Moore <robert.moore@intel.com>
2009-02-23 10:27:07 +0800

    ACPICA: Update version to 20090220
    Version 20090220.
    
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lin Ming <ming.m.lin@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- include/acpi/acpixf.h
+++ include/acpi/acpixf.h
@@ -47,7 +47,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20090123
+#define ACPI_CA_VERSION                 0x20090220
 
 #include "actypes.h"
 #include "actbl.h"
