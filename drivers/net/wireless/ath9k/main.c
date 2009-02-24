222d0b33b3778a8fa156f3fca284d0a71f8e966b
Jouni Malinen <jouni.malinen@atheros.com>
2009-02-24 13:40:01 +0200

    ath9k: Unlock sc->mutex on error path
    An error path in ath9k_add_interface() did not unlock the sc->mutex and
    could leave the driver in quite unresponsive state.
    
    Signed-off-by: Jouni Malinen <jouni.malinen@atheros.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath9k/main.c
+++ drivers/net/wireless/ath9k/main.c
@@ -2145,6 +2145,7 @@ static int ath9k_add_interface(struct ieee80211_hw *hw,
 	default:
 		DPRINTF(sc, ATH_DBG_FATAL,
 			"Interface type %d not yet supported\n", conf->type);
+		mutex_unlock(&sc->mutex);
 		return -EOPNOTSUPP;
 	}
 
