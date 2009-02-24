531660ef5604c75de6fdead9da1304051af17c09
Russell King <rmk+kernel@arm.linux.org.uk>
2009-02-24 19:19:50 +0100

    Add i2c_board_info for RiscPC PCF8583
    Add the necessary i2c_board_info structure to fix the lack of PCF8583
    RTC on RiscPC.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Cc: Alessandro Zummo <a.zummo@towertech.it>

--- drivers/i2c/busses/i2c-acorn.c
+++ drivers/i2c/busses/i2c-acorn.c
@@ -83,6 +83,7 @@ static struct i2c_algo_bit_data ioc_data = {
 };
 
 static struct i2c_adapter ioc_ops = {
+	.nr			= 0,
 	.algo_data		= &ioc_data,
 };
 
@@ -90,7 +91,7 @@ static int __init i2c_ioc_init(void)
 {
 	force_ones = FORCE_ONES | SCL | SDA;
 
-	return i2c_bit_add_bus(&ioc_ops);
+	return i2c_bit_add_numbered_bus(&ioc_ops);
 }
 
 module_init(i2c_ioc_init);
