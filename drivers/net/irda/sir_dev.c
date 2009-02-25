875b48297b8ab2f402680821da47d23bd6bddc55
Wei Yongjun <yjwei@cn.fujitsu.com>
2009-02-25 00:14:27 +0000

    irda: remove some pointless conditionals before kfree_skb()
    Remove some pointless conditionals before kfree_skb().
    
    Signed-off-by: Wei Yongjun <yjwei@cn.fujitsu.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/irda/sir_dev.c
+++ drivers/net/irda/sir_dev.c
@@ -780,8 +780,7 @@ static int sirdev_alloc_buffers(struct sir_dev *dev)
 
 static void sirdev_free_buffers(struct sir_dev *dev)
 {
-	if (dev->rx_buff.skb)
-		kfree_skb(dev->rx_buff.skb);
+	kfree_skb(dev->rx_buff.skb);
 	kfree(dev->tx_buff.head);
 	dev->rx_buff.head = dev->tx_buff.head = NULL;
 	dev->rx_buff.skb = NULL;
