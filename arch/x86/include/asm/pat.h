7880f7464546842ee14179bef16a6e14381ea638
Venkatesh Pallipadi <venkatesh.pallipadi@intel.com>
2009-02-24 17:35:13 -0800

    gpu/drm, x86, PAT: routine to keep identity map in sync
    Add a function to check and keep identity maps in sync, when changing
    any memory type. One of the follow on patches will also use this
    routine.
    
    Signed-off-by: Venkatesh Pallipadi <venkatesh.pallipadi@intel.com>
    Signed-off-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Cc: Dave Airlie <airlied@redhat.com>
    Cc: Jesse Barnes <jbarnes@virtuousgeek.org>
    Cc: Eric Anholt <eric@anholt.net>
    Cc: Keith Packard <keithp@keithp.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/pat.h
+++ arch/x86/include/asm/pat.h
@@ -19,4 +19,7 @@ extern int free_memtype(u64 start, u64 end);
 
 extern void pat_disable(char *reason);
 
+extern int kernel_map_sync_memtype(u64 base, unsigned long size,
+		unsigned long flag);
+
 #endif /* _ASM_X86_PAT_H */
