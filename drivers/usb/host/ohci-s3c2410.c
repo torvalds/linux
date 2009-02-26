2dfa319a649b9de029777994b6af201c1fe81d53
Ben Dooks <ben@simtec.co.uk>
2009-02-26 23:03:15 +0000

    USB: ohci-s3c2410: fix name of bus clock
    The USB bus clock is usb-bus-host, so print the correct name in the
    dev_err() statement if we cannot find it.
    
    Signed-off-by: Ben Dooks <ben@simtec.co.uk>
    Acked-by: David Brownell <dbrownell@users.sourceforge.net>

--- drivers/usb/host/ohci-s3c2410.c
+++ drivers/usb/host/ohci-s3c2410.c
@@ -371,7 +371,7 @@ static int usb_hcd_s3c2410_probe (const struct hc_driver *driver,
 
 	usb_clk = clk_get(&dev->dev, "usb-bus-host");
 	if (IS_ERR(usb_clk)) {
-		dev_err(&dev->dev, "cannot get usb-host clock\n");
+		dev_err(&dev->dev, "cannot get usb-bus-host clock\n");
 		retval = -ENOENT;
 		goto err_clk;
 	}
