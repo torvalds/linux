eb594e45f6979cd10b18d87f7b3f02119e00a108
Steven Rostedt <srostedt@redhat.com>
2009-02-27 17:36:06 -0500

    tracing: move trace point formats to files in include/trace directory
    Impact: clean up
    
    To further facilitate the ease of adding trace points for developers, this
    patch creates include/trace/trace_events.h and
    include/trace/trace_event_types.h.
    
    The former file will hold the trace/<type>.h files and the latter will hold
    the trace/<type>_event_types.h files.
    
    To create new tracepoints and to have them automatically
    appear in the event tracer, a developer makes the trace/<type>.h file
    which includes <linux/tracepoint.h> and the trace/<type>_event_types.h file.
    
    The trace/<type>_event_types.h file will hold the TRACE_FORMAT
    macros.
    
    Then add the trace/<type>.h file to trace/trace_events.h,
    and add the trace/<type>_event_types.h to the trace_event_types.h file.
    
    No need to modify files elsewhere.
    
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>

--- kernel/trace/events.c
+++ kernel/trace/events.c
@@ -1,15 +1,9 @@
 /*
  * This is the place to register all trace points as events.
- * Include the trace/<type>.h at the top.
- * Include the trace/<type>_event_types.h at the bottom.
  */
 
-/* trace/<type>.h here */
-#include <trace/sched.h>
-#include <trace/irq.h>
+#include <trace/trace_events.h>
 
 #include "trace_events.h"
 
-/* trace/<type>_event_types.h here */
-#include <trace/sched_event_types.h>
-#include <trace/irq_event_types.h>
+#include <trace/trace_event_types.h>
