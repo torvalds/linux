7f82b6dd7015aabca2fd55fb690248f742cd67f3
Axel Wachtler <axel.wachtler@atmel.com>
2009-03-05 14:09:22 -0800

    USB: serial: add FTDI USB/Serial converter devices
    Add the following devices to the USB FTDI SIO device table:
    
     Bus 001 Device 009: ID 03eb:2109 Atmel Corp.
     http://www.atmel.com/dyn/products/tools_card.asp?tool_id=4187
    
     Bus 001 Device 008: ID 1cf1:0001
     http://www.dresden-elektronik.de/shop/prod75.html
    
     Bus 001 Device 007: ID 1c1f:0004
     http://www.dresden-elektronik.de/shop/prod64.html
    
    Signed-off-by: Axel Wachtler <axel.wachtler@atmel.com>
    Signed-off-by: Robert Richter <robert.richter@amd.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: stable <stable@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio.h
+++ drivers/usb/serial/ftdi_sio.h
@@ -893,6 +893,19 @@
 #define DIEBOLD_BCS_SE923_PID	0xfb99
 
 /*
+ * Atmel STK541
+ */
+#define ATMEL_VID		0x03eb /* Vendor ID */
+#define STK541_PID		0x2109 /* Zigbee Controller */
+
+/*
+ * Dresden Elektronic Sensor Terminal Board
+ */
+#define DE_VID			0x1cf1 /* Vendor ID */
+#define STB_PID			0x0001 /* Sensor Terminal Board */
+#define WHT_PID			0x0004 /* Wireless Handheld Terminal */
+
+/*
  *   BmRequestType:  1100 0000b
  *   bRequest:       FTDI_E2_READ
  *   wValue:         0
