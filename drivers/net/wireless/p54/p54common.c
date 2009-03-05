efeada2c0aa1219b15787da48cfa282803e9d99e
Christian Lamparter <chunkeey@web.de>
2009-03-05 21:31:05 +0100

    p54: fix iwconfig txpower off
    Disabling the receiver logic with P54_FILTER_TYPE_RX_DISABLED is not
    supported by all firmwares. However we have an alternative: hibernation.
    And the only side effect - so far - is a bit less power consumption. WIN!
    
    Signed-off-by: Christian Lamparter <chunkeey@web.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/p54/p54common.c
+++ drivers/net/wireless/p54/p54common.c
@@ -1680,7 +1680,7 @@ static int p54_setup_mac(struct ieee80211_hw *dev)
 			mode = P54_FILTER_TYPE_PROMISCUOUS;
 			break;
 		default:
-			mode = P54_FILTER_TYPE_NONE;
+			mode = P54_FILTER_TYPE_HIBERNATE;
 			break;
 		}
 
@@ -1693,7 +1693,7 @@ static int p54_setup_mac(struct ieee80211_hw *dev)
 		    (mode != P54_FILTER_TYPE_PROMISCUOUS))
 			mode |= P54_FILTER_TYPE_TRANSPARENT;
 	} else
-		mode = P54_FILTER_TYPE_RX_DISABLED;
+		mode = P54_FILTER_TYPE_HIBERNATE;
 
 	setup->mac_mode = cpu_to_le16(mode);
 	memcpy(setup->mac_addr, priv->mac_addr, ETH_ALEN);
