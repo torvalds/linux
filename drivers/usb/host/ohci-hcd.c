3ba5f38f3d5143a879de132a9df71814d1f3cff0
Ben Dooks <ben@simtec.co.uk>
2009-03-07 11:44:10 +0000

    USB: ohci-hcd: Add ARCH_S3C24XX to the ohci-s3c2410.c glue
    The ohci-s3c2410.c glue supports both CONFIG_ARCH_S3C2410 and
    CONFIG_ARCH_S3C64XX so add it to the build of ohci-s3c2410.c
    
    Signed-off-by: Ben Dooks <ben@simtec.co.uk>
    Cc: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/host/ohci-hcd.c
+++ drivers/usb/host/ohci-hcd.c
@@ -997,7 +997,7 @@ MODULE_LICENSE ("GPL");
 #define SA1111_DRIVER		ohci_hcd_sa1111_driver
 #endif
 
-#ifdef CONFIG_ARCH_S3C2410
+#if defined(CONFIG_ARCH_S3C2410) || defined(CONFIG_ARCH_S3C64XX)
 #include "ohci-s3c2410.c"
 #define PLATFORM_DRIVER		ohci_hcd_s3c2410_driver
 #endif
