3ab8d295156a43fc2751483d2f82a63c97489f89
Mike Isely <isely@pobox.com>
2009-03-07 01:37:58 -0300

    V4L/DVB (11185): pvrusb2: Fix uninitialized counter
    Signed-off-by: Mike Isely <isely@pobox.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/pvrusb2/pvrusb2-hdw.c
+++ drivers/media/video/pvrusb2/pvrusb2-hdw.c
@@ -1979,7 +1979,7 @@ static unsigned int pvr2_copy_i2c_addr_list(
 	unsigned short *dst, const unsigned char *src,
 	unsigned int dst_max)
 {
-	unsigned int cnt;
+	unsigned int cnt = 0;
 	if (!src) return 0;
 	while (src[cnt] && (cnt + 1) < dst_max) {
 		dst[cnt] = src[cnt];
