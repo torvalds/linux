df7f54c012b92ec93d56b68547351dcdf8a163d3
Eric Paris <eparis@redhat.com>
2009-03-09 14:35:58 -0400

    SELinux: inode_doinit_with_dentry drop no dentry printk
    Drop the printk message when an inode is found without an associated
    dentry.  This should only happen when userspace can't be accessing those
    inodes and those labels will get set correctly on the next d_instantiate.
    Thus there is no reason to send this message.
    
    Signed-off-by: Eric Paris <eparis@redhat.com>
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -1263,9 +1263,15 @@ static int inode_doinit_with_dentry(struct inode *inode, struct dentry *opt_dent
 			dentry = d_find_alias(inode);
 		}
 		if (!dentry) {
-			printk(KERN_WARNING "SELinux: %s:  no dentry for dev=%s "
-			       "ino=%ld\n", __func__, inode->i_sb->s_id,
-			       inode->i_ino);
+			/*
+			 * this is can be hit on boot when a file is accessed
+			 * before the policy is loaded.  When we load policy we
+			 * may find inodes that have no dentry on the
+			 * sbsec->isec_head list.  No reason to complain as these
+			 * will get fixed up the next time we go through
+			 * inode_doinit with a dentry, before these inodes could
+			 * be used again by userspace.
+			 */
 			goto out_unlock;
 		}
 
