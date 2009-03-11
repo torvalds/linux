fd9377ee6c351b3fd27bcc56fd5e24622df180bb
Reinette Chatre <reinette.chatre@intel.com>
2009-03-11 11:17:58 -0700

    iwl3945: unmap previously mapped memory
    During preparation of TX we create DMA mapping to TX command as part of
    preparing the TFD. This mapping needs to be cleared at the time TFD is
    freed.
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-3945.c
+++ drivers/net/wireless/iwlwifi/iwl-3945.c
@@ -741,7 +741,8 @@ int iwl3945_hw_txq_attach_buf_to_tfd(struct iwl_priv *priv,
 void iwl3945_hw_txq_free_tfd(struct iwl_priv *priv, struct iwl_tx_queue *txq)
 {
 	struct iwl3945_tfd *tfd_tmp = (struct iwl3945_tfd *)txq->tfds;
-	struct iwl3945_tfd *tfd = &tfd_tmp[txq->q.read_ptr];
+	int index = txq->q.read_ptr;
+	struct iwl3945_tfd *tfd = &tfd_tmp[index];
 	struct pci_dev *dev = priv->pci_dev;
 	int i;
 	int counter;
@@ -758,6 +759,13 @@ void iwl3945_hw_txq_free_tfd(struct iwl_priv *priv, struct iwl_tx_queue *txq)
 		/* @todo issue fatal error, it is quite serious situation */
 		return;
 	}
+
+	/* Unmap tx_cmd */
+	if (counter)
+		pci_unmap_single(dev,
+				pci_unmap_addr(&txq->cmd[index]->meta, mapping),
+				pci_unmap_len(&txq->cmd[index]->meta, len),
+				PCI_DMA_TODEVICE);
 
 	/* unmap chunks if any */
 
