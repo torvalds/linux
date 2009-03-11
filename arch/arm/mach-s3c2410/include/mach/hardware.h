a2b03461cb072eb6097a55ec0289294b09382284
Mark Brown <broonie@opensource.wolfsonmicro.com>
2009-03-11 11:02:33 +0000

    [ARM] Revert extraneous changes from the S3C audio header move
    These changes were included in the S3C audio header move but are not
    directly related to it.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- arch/arm/mach-s3c2410/include/mach/hardware.h
+++ arch/arm/mach-s3c2410/include/mach/hardware.h
@@ -131,4 +131,7 @@ extern int s3c2412_gpio_set_sleepcfg(unsigned int pin, unsigned int state);
 
 /* machine specific hardware definitions should go after this */
 
+/* currently here until moved into config (todo) */
+#define CONFIG_NO_MULTIWORD_IO
+
 #endif /* __ASM_ARCH_HARDWARE_H */
