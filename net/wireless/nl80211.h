633e24ed95b6c87b42f201ecb65c12a75a5a7eef
Reinette Chatre <reinette.chatre@intel.com>
2009-03-12 09:20:40 -0700

    cfg80211/nl80211: remove usage of CONFIG_NL80211
    The scan capability added to cfg80211/nl80211 introduced a
    dependency on nl80211 by cfg80211. We can thus no longer have
    just cfg80211 without nl80211. Specifically, cfg80211_scan_done()
    calls nl80211_send_scan_aborted() or nl80211_send_scan_done().
    
    Now we remove the option for user to select nl80211. It will always
    be compiled if user selects cfg80211.
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/wireless/nl80211.h
+++ net/wireless/nl80211.h
@@ -3,7 +3,6 @@
 
 #include "core.h"
 
-#ifdef CONFIG_NL80211
 extern int nl80211_init(void);
 extern void nl80211_exit(void);
 extern void nl80211_notify_dev_rename(struct cfg80211_registered_device *rdev);
@@ -12,30 +11,5 @@ extern void nl80211_send_scan_done(struct cfg80211_registered_device *rdev,
 extern void nl80211_send_scan_aborted(struct cfg80211_registered_device *rdev,
 				      struct net_device *netdev);
 extern void nl80211_send_reg_change_event(struct regulatory_request *request);
-#else
-static inline int nl80211_init(void)
-{
-	return 0;
-}
-static inline void nl80211_exit(void)
-{
-}
-static inline void nl80211_notify_dev_rename(
-	struct cfg80211_registered_device *rdev)
-{
-}
-static inline void
-nl80211_send_scan_done(struct cfg80211_registered_device *rdev,
-		       struct net_device *netdev)
-{}
-static inline void nl80211_send_scan_aborted(
-					struct cfg80211_registered_device *rdev,
-					struct net_device *netdev)
-{}
-static inline void
-nl80211_send_reg_change_event(struct regulatory_request *request)
-{
-}
-#endif /* CONFIG_NL80211 */
 
 #endif /* __NET_WIRELESS_NL80211_H */
