2da03ecee6308ea174e8a02b92a3c4ec92e886c8
Steven Rostedt <srostedt@redhat.com>
2009-03-12 18:57:51 -0400

    tracing: fix stack tracer header
    The stack tracer use to look like this:
    
     # cat /debug/tracing/stack_trace
             Depth  Size      Location    (57 entries)
             -----  ----      --------
      0)     5088      16   mempool_alloc_slab+0x16/0x18
      1)     5072     144   mempool_alloc+0x4d/0xfe
      2)     4928      16   scsi_sg_alloc+0x48/0x4a [scsi_mod]
    
    Now it looks like this:
    
     # cat /debug/tracing/stack_trace
    
            Depth    Size      Location    (57 entries)
            -----    ----      --------
      0)     5088      16   mempool_alloc_slab+0x16/0x18
      1)     5072     144   mempool_alloc+0x4d/0xfe
      2)     4928      16   scsi_sg_alloc+0x48/0x4a [scsi_mod]
    
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>

--- kernel/trace/trace_stack.c
+++ kernel/trace/trace_stack.c
@@ -251,9 +251,9 @@ static int t_show(struct seq_file *m, void *v)
 	int size;
 
 	if (v == SEQ_START_TOKEN) {
-		seq_printf(m, "        Depth   Size      Location"
+		seq_printf(m, "        Depth    Size      Location"
 			   "    (%d entries)\n"
-			   "        -----   ----      --------\n",
+			   "        -----    ----      --------\n",
 			   max_stack_trace.nr_entries);
 		return 0;
 	}
