3bcb1255bae39cae1b4d2660f2b5a6ab2f404c10
Divy Le Ray <divy@chelsio.com>
2009-03-12 21:14:29 +0000

    cxgb3: update driver version
    update driver version to 1.1.1-ko
    
    Signed-off-by: Divy Le Ray <divy@chelsio.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/cxgb3/version.h
+++ drivers/net/cxgb3/version.h
@@ -35,7 +35,7 @@
 #define DRV_DESC "Chelsio T3 Network Driver"
 #define DRV_NAME "cxgb3"
 /* Driver version */
-#define DRV_VERSION "1.1.1-ko"
+#define DRV_VERSION "1.1.2-ko"
 
 /* Firmware version */
 #define FW_VERSION_MAJOR 7
