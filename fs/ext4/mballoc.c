8d03c7a0c550e7ab24cadcef5e66656bfadec8b9
Eric Sandeen <sandeen@redhat.com>
2009-03-14 11:51:46 -0400

    ext4: fix bogus BUG_ONs in in mballoc code
    Thiemo Nagel reported that:
    
    # dd if=/dev/zero of=image.ext4 bs=1M count=2
    # mkfs.ext4 -v -F -b 1024 -m 0 -g 512 -G 4 -I 128 -N 1 \
      -O large_file,dir_index,flex_bg,extent,sparse_super image.ext4
    # mount -o loop image.ext4 mnt/
    # dd if=/dev/zero of=mnt/file
    
    oopsed, with a BUG_ON in ext4_mb_normalize_request because
    size == EXT4_BLOCKS_PER_GROUP
    
    It appears to me (esp. after talking to Andreas) that the BUG_ON
    is bogus; a request of exactly EXT4_BLOCKS_PER_GROUP should
    be allowed, though larger sizes do indicate a problem.
    
    Fix that an another (apparently rare) codepath with a similar check.
    
    Reported-by: Thiemo Nagel <thiemo.nagel@ph.tum.de>
    Signed-off-by: Eric Sandeen <sandeen@redhat.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -1447,7 +1447,7 @@ static void ext4_mb_measure_extent(struct ext4_allocation_context *ac,
 	struct ext4_free_extent *gex = &ac->ac_g_ex;
 
 	BUG_ON(ex->fe_len <= 0);
-	BUG_ON(ex->fe_len >= EXT4_BLOCKS_PER_GROUP(ac->ac_sb));
+	BUG_ON(ex->fe_len > EXT4_BLOCKS_PER_GROUP(ac->ac_sb));
 	BUG_ON(ex->fe_start >= EXT4_BLOCKS_PER_GROUP(ac->ac_sb));
 	BUG_ON(ac->ac_status != AC_STATUS_CONTINUE);
 
@@ -3292,7 +3292,7 @@ ext4_mb_normalize_request(struct ext4_allocation_context *ac,
 	}
 	BUG_ON(start + size <= ac->ac_o_ex.fe_logical &&
 			start > ac->ac_o_ex.fe_logical);
-	BUG_ON(size <= 0 || size >= EXT4_BLOCKS_PER_GROUP(ac->ac_sb));
+	BUG_ON(size <= 0 || size > EXT4_BLOCKS_PER_GROUP(ac->ac_sb));
 
 	/* now prepare goal request */
 
