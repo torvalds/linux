a6bc3262c561780d2a6587aa3d5715b1e7d8fa13
Stephen Rothwell <sfr@canb.auug.org.au>
2009-03-16 18:52:56 +1100

    sparseirq, powerpc/cell: fix unused variable warning in interrupt.c
    This new compiler warning:
    
       arch/powerpc/platforms/cell/interrupt.c: In function 'handle_iic_irq':
       arch/powerpc/platforms/cell/interrupt.c:240: warning: unused variable 'cpu'
    
    Triggers because the local variable 'cpu' became unused due to commit:
    
       dee4102: sparseirq: use kstat_irqs_cpu instead
    
    Remove the variable.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: Yinghai Lu <yinghai@kernel.org>
    Cc: ppc-dev <linuxppc-dev@ozlabs.org>
    LKML-Reference: <20090316185256.4a160374.sfr@canb.auug.org.au>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/powerpc/platforms/cell/interrupt.c
+++ arch/powerpc/platforms/cell/interrupt.c
@@ -237,8 +237,6 @@ extern int noirqdebug;
 
 static void handle_iic_irq(unsigned int irq, struct irq_desc *desc)
 {
-	const unsigned int cpu = smp_processor_id();
-
 	spin_lock(&desc->lock);
 
 	desc->status &= ~(IRQ_REPLAY | IRQ_WAITING);
