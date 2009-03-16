3a5203ab3c0c31e0f1434c69e893bfb85c6e6657
Tyler Hicks <tyhicks@linux.vnet.ibm.com>
2009-03-16 12:35:12 -0500

    eCryptfs: Print FNEK sig properly in /proc/mounts
    The filename encryption key signature is not properly displayed in
    /proc/mounts.  The "ecryptfs_sig=" mount option name is displayed for
    all global authentication tokens, included those for filename keys.
    
    This patch checks the global authentication token flags to determine if
    the key is a FEKEK or FNEK and prints the appropriate mount option name
    before the signature.
    
    Signed-off-by: Tyler Hicks <tyhicks@linux.vnet.ibm.com>

--- fs/ecryptfs/super.c
+++ fs/ecryptfs/super.c
@@ -170,7 +170,10 @@ static int ecryptfs_show_options(struct seq_file *m, struct vfsmount *mnt)
 	list_for_each_entry(walker,
 			    &mount_crypt_stat->global_auth_tok_list,
 			    mount_crypt_stat_list) {
-		seq_printf(m, ",ecryptfs_sig=%s", walker->sig);
+		if (walker->flags & ECRYPTFS_AUTH_TOK_FNEK)
+			seq_printf(m, ",ecryptfs_fnek_sig=%s", walker->sig);
+		else
+			seq_printf(m, ",ecryptfs_sig=%s", walker->sig);
 	}
 	mutex_unlock(&mount_crypt_stat->global_auth_tok_list_mutex);
 
