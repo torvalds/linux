534841b3c110b0f149513c7695751879951d3f22
Brian King <brking@linux.vnet.ibm.com>
2009-03-20 15:44:40 -0500

    [SCSI] ibmvfc: Driver version 1.0.5
    Bump driver version to 1.0.5.
    
    Signed-off-by: Brian King <brking@linux.vnet.ibm.com>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- drivers/scsi/ibmvscsi/ibmvfc.h
+++ drivers/scsi/ibmvscsi/ibmvfc.h
@@ -29,8 +29,8 @@
 #include "viosrp.h"
 
 #define IBMVFC_NAME	"ibmvfc"
-#define IBMVFC_DRIVER_VERSION		"1.0.4"
-#define IBMVFC_DRIVER_DATE		"(November 14, 2008)"
+#define IBMVFC_DRIVER_VERSION		"1.0.5"
+#define IBMVFC_DRIVER_DATE		"(March 19, 2009)"
 
 #define IBMVFC_DEFAULT_TIMEOUT	60
 #define IBMVFC_ADISC_CANCEL_TIMEOUT	45
