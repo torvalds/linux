bd50a0ffca0bbb9baa60ab1ef2c09cf7561e1223
Yegor Yefremov <yegorslists@googlemail.com>
2009-03-20 18:50:26 +0000

    [MTD] [CHIPS] Add JEDEC probe support for the SST 39VF3201 flash chip
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/chips/jedec_probe.c
+++ drivers/mtd/chips/jedec_probe.c
@@ -159,6 +159,7 @@
 #define SST39LF800	0x2781
 #define SST39LF160	0x2782
 #define SST39VF1601	0x234b
+#define SST39VF3201	0x235b
 #define SST39LF512	0x00D4
 #define SST39LF010	0x00D5
 #define SST39LF020	0x00D6
@@ -1486,6 +1487,21 @@ static const struct amd_flash_info jedec_table[] = {
 		.cmd_set	= P_ID_AMD_STD,
 		.nr_regions	= 2,
 		.regions	= {
+			ERASEINFO(0x1000,256),
+			ERASEINFO(0x1000,256)
+		}
+	}, {
+		.mfr_id		= MANUFACTURER_SST,     /* should be CFI */
+		.dev_id		= SST39VF3201,
+		.name		= "SST 39VF3201",
+		.devtypes	= CFI_DEVICETYPE_X16,
+		.uaddr		= MTD_UADDR_0xAAAA_0x5555,
+		.dev_size	= SIZE_4MiB,
+		.cmd_set	= P_ID_AMD_STD,
+		.nr_regions	= 4,
+		.regions	= {
+			ERASEINFO(0x1000,256),
+			ERASEINFO(0x1000,256),
 			ERASEINFO(0x1000,256),
 			ERASEINFO(0x1000,256)
 		}
