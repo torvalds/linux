14c6b5e7add9ec393ad61bceb6106b47c7f14bd3
Eric Miao <eric.miao@marvell.com>
2009-03-20 12:50:22 +0800

    [ARM] pxa: add base support for Marvell PXA910
    Signed-off-by: Bin Yang <bin.yang@marvell.com>
    Signed-off-by: Eric Miao <eric.miao@marvell.com>

--- arch/arm/mach-mmp/include/mach/devices.h
+++ arch/arm/mach-mmp/include/mach/devices.h
@@ -24,4 +24,14 @@ struct pxa_device_desc pxa168_device_##_name __initdata = {		\
 	.dma		= { _dma },					\
 };
 
+#define PXA910_DEVICE(_name, _drv, _id, _irq, _start, _size, _dma...)	\
+struct pxa_device_desc pxa910_device_##_name __initdata = {		\
+	.dev_name	= "pxa910-" #_name,				\
+	.drv_name	= _drv,						\
+	.id		= _id,						\
+	.irq		= IRQ_PXA910_##_irq,				\
+	.start		= _start,					\
+	.size		= _size,					\
+	.dma		= { _dma },					\
+};
 extern int pxa_register_device(struct pxa_device_desc *, void *, size_t);
