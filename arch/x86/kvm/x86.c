7fe29e0faacb650d31b9e9f538203a157bec821d
Amit Shah <amit.shah@redhat.com>
2009-03-20 12:39:00 +0530

    KVM: x86: Ignore reads to EVNTSEL MSRs
    We ignore writes to the performance counters and performance event
    selector registers already. Kaspersky antivirus reads the eventsel
    MSR causing it to crash with the current behaviour.
    
    Return 0 as data when the eventsel registers are read to stop the
    crash.
    
    Signed-off-by: Amit Shah <amit.shah@redhat.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -895,6 +895,8 @@ int kvm_get_msr_common(struct kvm_vcpu *vcpu, u32 msr, u64 *pdata)
 	case MSR_IA32_LASTINTFROMIP:
 	case MSR_IA32_LASTINTTOIP:
 	case MSR_VM_HSAVE_PA:
+	case MSR_P6_EVNTSEL0:
+	case MSR_P6_EVNTSEL1:
 		data = 0;
 		break;
 	case MSR_MTRRcap:
