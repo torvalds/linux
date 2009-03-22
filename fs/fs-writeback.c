16586560b6b0c6035f02c5a73929029b248cf348
San Mehat <san@android.com>
2009-03-21 18:48:54 -0700

    fs: block_dump: Don't display inode changes if block_dump < 2
    Signed-off-by: San Mehat <san@android.com>

--- fs/fs-writeback.c
+++ fs/fs-writeback.c
@@ -1141,7 +1141,7 @@ void __mark_inode_dirty(struct inode *inode, int flags)
 	if ((inode->i_state & flags) == flags)
 		return;
 
-	if (unlikely(block_dump))
+	if (unlikely(block_dump > 1))
 		block_dump___mark_inode_dirty(inode);
 
 	spin_lock(&inode->i_lock);
