5393f3162d3a85317e1e22c33539905fa5258e5f
Josh Stone <jistone@redhat.com>
2009-03-24 09:44:28 +0000

    net: Add dependent headers to trace/skb.h
    The tracing header needs to include definitions for the macros used and
    the types referenced.  This lets automated tracing tools like SystemTap
    make use of the tracepoint without any specific knowledge of its
    meaning (leaving that to the user).
    
    Signed-off-by: Josh Stone <jistone@redhat.com>
    CC: Neil Horman <nhorman@tuxdriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/trace/skb.h
+++ include/trace/skb.h
@@ -1,6 +1,9 @@
 #ifndef _TRACE_SKB_H_
 #define _TRACE_SKB_H_
 
+#include <linux/skbuff.h>
+#include <linux/tracepoint.h>
+
 DECLARE_TRACE(kfree_skb,
 	TPPROTO(struct sk_buff *skb, void *location),
 	TPARGS(skb, location));
