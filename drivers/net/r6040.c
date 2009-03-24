8ca51986be0c19c8a48ef8f36568c5a3c2c0ac50
Florian Fainelli <florian@openwrt.org>
2009-03-24 23:34:38 +0000

    Bump release date to 25Mar2009 and version to 0.22
    This patch bumps the driver release date to March 25th 2009
    and release version to 0.22.
    
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/r6040.c
+++ drivers/net/r6040.c
@@ -49,8 +49,8 @@
 #include <asm/processor.h>
 
 #define DRV_NAME	"r6040"
-#define DRV_VERSION	"0.21"
-#define DRV_RELDATE	"09Jan2009"
+#define DRV_VERSION	"0.22"
+#define DRV_RELDATE	"25Mar2009"
 
 /* PHY CHIP Address */
 #define PHY1_ADDR	1	/* For MAC1 */
