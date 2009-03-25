e42d1fe804408c57506a5326252b4db29958a7fb
Yinghai Lu <yinghai@kernel.org>
2009-03-25 13:26:13 -0700

    x86/PCI: make pci=lastbus=255 work when acpi is on
    Impact: scan more peer root buses even acpi is used
    
    Move pci_bios_fixup_peer_bridges out of pci_legacy_init and into
    pci_subsys_init.  This allows pci_bios_fixup_peer_bridges to be called
    even pci_apci_init is driving PCI initialization.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- arch/x86/pci/legacy.c
+++ arch/x86/pci/legacy.c
@@ -50,8 +50,6 @@ static int __init pci_legacy_init(void)
 	if (pci_root_bus)
 		pci_bus_add_devices(pci_root_bus);
 
-	pcibios_fixup_peer_bridges();
-
 	return 0;
 }
 
@@ -67,6 +65,7 @@ int __init pci_subsys_init(void)
 	pci_visws_init();
 #endif
 	pci_legacy_init();
+	pcibios_fixup_peer_bridges();
 	pcibios_irq_init();
 	pcibios_init();
 
