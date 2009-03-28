269647dc8f3b557d0f9ec59533a5e651cc109efb
Mike Frysinger <vapier.adi@gmail.com>
2009-03-28 20:32:57 +0800

    Blackfin arch: convert BF5{18,27,48}_FAMILY to CONFIG_BF{51,52,54}x
    convert BF5{18,27,48}_FAMILY to CONFIG_BF{51,52,54}x as the defines
    are redundant in these cases
    
    Signed-off-by: Mike Frysinger <vapier.adi@gmail.com>
    Signed-off-by: Bryan Wu <cooloney@kernel.org>

--- arch/blackfin/include/asm/gpio.h
+++ arch/blackfin/include/asm/gpio.h
@@ -110,7 +110,7 @@
 * MODIFICATION HISTORY :
 **************************************************************/
 
-#ifndef BF548_FAMILY
+#ifndef CONFIG_BF54x
 void set_gpio_dir(unsigned, unsigned short);
 void set_gpio_inen(unsigned, unsigned short);
 void set_gpio_polar(unsigned, unsigned short);
