e1312bfcc25f9301f997bf1ce84355a0f1167847
Michael Hennerich <michael.hennerich@analog.com>
2009-03-28 21:18:45 +0800

    Blackfin arch: add a check to make sure only Blackfin GPIOs may generate IRQs
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    Signed-off-by: Bryan Wu <cooloney@kernel.org>

--- arch/blackfin/include/asm/gpio.h
+++ arch/blackfin/include/asm/gpio.h
@@ -303,7 +303,10 @@ static inline void gpio_set_value(unsigned gpio, int value)
 
 static inline int gpio_to_irq(unsigned gpio)
 {
-	return (gpio + GPIO_IRQ_BASE);
+	if (likely(gpio < MAX_BLACKFIN_GPIOS))
+		return gpio + GPIO_IRQ_BASE;
+
+	return -EINVAL;
 }
 
 static inline int irq_to_gpio(unsigned irq)
