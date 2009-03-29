503e57630309643562c12f09d4c8a96eb629ee33
Ralf Baechle <ralf@linux-mips.org>
2009-03-29 12:59:50 +0200

    Fix build error in <linux/irq.h>
    <linux/irq.h> relies on <linux/gfp.h> and <linux/topology.h> having been
    included previous.  If not, the errors like below will result.
    
        CC      arch/mips/mti-malta/malta-int.o
      In file included from arch/mips/mti-malta/malta-int.c:25:
      include/linux/irq.h: In function ‘init_alloc_desc_masks’:
      include/linux/irq.h:444: error: implicit declaration of function ‘cpu_to_node’
      include/linux/irq.h:446: error: ‘GFP_ATOMIC’ undeclared (first use in this function)
      include/linux/irq.h:446: error: (Each undeclared identifier is reported only once
      include/linux/irq.h:446: error: for each function it appears in.)
      make[3]: *** [arch/mips/mti-malta/malta-int.o] Error 1
      make[2]: *** [arch/mips/mti-malta] Error 2
      make[1]: *** [sub-make] Error 2
    
    Fixed by including the two missing headers.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -17,9 +17,11 @@
 #include <linux/cache.h>
 #include <linux/spinlock.h>
 #include <linux/cpumask.h>
+#include <linux/gfp.h>
 #include <linux/irqreturn.h>
 #include <linux/irqnr.h>
 #include <linux/errno.h>
+#include <linux/topology.h>
 
 #include <asm/irq.h>
 #include <asm/ptrace.h>
