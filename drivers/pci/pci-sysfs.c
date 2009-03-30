c2ac7cdc6707a8a3fc5cdaebb65cb724ba8ecdef
Alex Chiang <achiang@hp.com>
2009-03-30 10:50:09 -0600

    PCI: allow PCI core hotplug to remove PCI root bus
    There is no reason to prevent removal of root bus devices. A subsequent
    rescan will find them just fine.
    
    Reviewed-by: Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
    Signed-off-by: Alex Chiang <achiang@hp.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/pci-sysfs.c
+++ drivers/pci/pci-sysfs.c
@@ -277,13 +277,9 @@ remove_store(struct device *dev, struct device_attribute *dummy,
 {
 	int ret = 0;
 	unsigned long val;
-	struct pci_dev *pdev = to_pci_dev(dev);
 
 	if (strict_strtoul(buf, 0, &val) < 0)
 		return -EINVAL;
-
-	if (pci_is_root_bus(pdev->bus))
-		return -EBUSY;
 
 	/* An attribute cannot be unregistered by one of its own methods,
 	 * so we have to use this roundabout approach.
