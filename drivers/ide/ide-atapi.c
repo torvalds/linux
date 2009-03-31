2eba08270990b99fb5429b76ee97184ddd272f7f
Borislav Petkov <petkovbb@gmail.com>
2009-03-31 20:14:58 +0200

    ide-atapi: start DMA after issuing a packet command
    Apparently¹, some ATAPI devices want to see the packet command first
    before enabling DMA otherwise they simply hang indefinitely. Reorder the
    two steps and start DMA only after having issued the command first.
    
    [1] http://marc.info/?l=linux-kernel&m=123835520317235&w=2
    
    Signed-off-by: Borislav Petkov <petkovbb@gmail.com>
    Reported-by: Michael Roth <mroth@nessie.de>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/ide-atapi.c
+++ drivers/ide/ide-atapi.c
@@ -613,6 +613,10 @@ static ide_startstop_t ide_transfer_pc(ide_drive_t *drive)
 					     : ide_pc_intr),
 			timeout);
 
+	/* Send the actual packet */
+	if ((drive->atapi_flags & IDE_AFLAG_ZIP_DRIVE) == 0)
+		hwif->tp_ops->output_data(drive, NULL, rq->cmd, cmd_len);
+
 	/* Begin DMA, if necessary */
 	if (dev_is_idecd(drive)) {
 		if (drive->dma)
@@ -623,10 +627,6 @@ static ide_startstop_t ide_transfer_pc(ide_drive_t *drive)
 			hwif->dma_ops->dma_start(drive);
 		}
 	}
-
-	/* Send the actual packet */
-	if ((drive->atapi_flags & IDE_AFLAG_ZIP_DRIVE) == 0)
-		hwif->tp_ops->output_data(drive, NULL, rq->cmd, cmd_len);
 
 	return ide_started;
 }
