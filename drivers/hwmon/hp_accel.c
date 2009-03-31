12a324b6a3758f04a952b99d75a625732d767585
Luca Cappa <lcappa@gmail.com>
2009-03-31 15:24:31 -0700

    hp_accel: axis conversion for hp compaq 8710w
    I have a laptop HP Compaq 8710W, I compiled into my kernel the LIS3LV02DL
    and HP_ACCEL module drivers.  While loading it cannot recognize the laptop
    model, so i am sending the necessary information to update the database of
    axis orientations.
    
    >When the laptop is horizontal the position reported is about 0 for X and Y
    >and a positive value for Z
    Yes, it is about 0,0,1000, the actual reading says: (-17,-26,1018);
    
    > If the left side is elevated, X increases (becomes positive)
    Yes, X goes toward to positive 1000.
    
    >If the front side (where the touchpad is) is elevated, Y decreases (becomes negative)
    No, Y goes toward to positive 1000.
    
    >If the laptop is put upside-down, Z becomes negative
    Yes, the laptop on a table Z gives 1000, and if upsidedown the Z reads
    -1000.
    
    So in few words the Y axis is inverted.
    
    Cc: Eric Piel <eric.piel@tremplin-utc.net>
    Signed-off-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/hwmon/hp_accel.c
+++ drivers/hwmon/hp_accel.c
@@ -203,6 +203,7 @@ static struct dmi_system_id lis3lv02d_dmi_ids[] = {
 			BOARD_NAME, "3600",
 			y_inverted),
 	AXIS_DMI_MATCH("DV7", "HP Pavilion dv7", x_inverted),
+	AXIS_DMI_MATCH("HP8710", "HP Compaq 8710", y_inverted),
 	{ NULL, }
 /* Laptop models without axis info (yet):
  * "NC6910" "HP Compaq 6910"
