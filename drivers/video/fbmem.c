6a7f2829b5f8be124e168265f176dbbbea8861a0
Andrew Morton <akpm@linux-foundation.org>
2009-03-31 15:25:19 -0700

    fbdev: uninline lock_fb_info()
    Before:
    
       text    data     bss     dec     hex filename
       3648    2910      32    6590    19be drivers/video/backlight/backlight.o
       3226    2812      32    6070    17b6 drivers/video/backlight/lcd.o
      30990   16688    8480   56158    db5e drivers/video/console/fbcon.o
      15488    8400      24   23912    5d68 drivers/video/fbmem.o
    
    After:
    
       text    data     bss     dec     hex filename
       3537    2870      32    6439    1927 drivers/video/backlight/backlight.o
       3131    2772      32    5935    172f drivers/video/backlight/lcd.o
      30876   16648    8480   56004    dac4 drivers/video/console/fbcon.o
      15506    8400      24   23930    5d7a drivers/video/fbmem.o
    
    Cc: Andrea Righi <righi.andrea@gmail.com>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Krzysztof Helt <krzysztof.h1@poczta.fm>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/fbmem.c
+++ drivers/video/fbmem.c
@@ -46,6 +46,17 @@
 struct fb_info *registered_fb[FB_MAX] __read_mostly;
 int num_registered_fb __read_mostly;
 
+int lock_fb_info(struct fb_info *info)
+{
+	mutex_lock(&info->lock);
+	if (!info->fbops) {
+		mutex_unlock(&info->lock);
+		return 0;
+	}
+	return 1;
+}
+EXPORT_SYMBOL(lock_fb_info);
+
 /*
  * Helpers
  */
--- include/linux/fb.h
+++ include/linux/fb.h
@@ -961,15 +961,7 @@ extern struct fb_info *registered_fb[FB_MAX];
 extern int num_registered_fb;
 extern struct class *fb_class;
 
-static inline int lock_fb_info(struct fb_info *info)
-{
-	mutex_lock(&info->lock);
-	if (!info->fbops) {
-		mutex_unlock(&info->lock);
-		return 0;
-	}
-	return 1;
-}
+extern int lock_fb_info(struct fb_info *info);
 
 static inline void unlock_fb_info(struct fb_info *info)
 {
