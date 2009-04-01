6e7f7b37e719746da67ecfc54a264783d473ca05
Janne Grunau <j@jannau.net>
2009-04-01 08:41:13 -0300

    V4L/DVB (11353): cx231xx: remove explicitly set v4l2_device.name
    Signed-off-by: Janne Grunau <j@jannau.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx231xx/cx231xx-cards.c
+++ drivers/media/video/cx231xx/cx231xx-cards.c
@@ -672,8 +672,6 @@ static int cx231xx_usb_probe(struct usb_interface *interface,
 	 */
 
 	/* Create v4l2 device */
-	snprintf(dev->v4l2_dev.name, sizeof(dev->v4l2_dev.name),
-					"%s-%03d", "cx231xx", nr);
 	retval = v4l2_device_register(&interface->dev, &dev->v4l2_dev);
 	if (retval) {
 		cx231xx_errdev("v4l2_device_register failed\n");
