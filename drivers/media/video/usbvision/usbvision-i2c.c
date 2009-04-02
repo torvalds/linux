f2caedd919bfef6abe9ad99cf3e210615f6a692d
Huang Weiyi <weiyi.huang@gmail.com>
2009-04-02 12:30:19 -0300

    V4L/DVB: usbvision: remove unused #include <version.h>
    Remove unused #include <version.h> in
    drivers/media/video/usbvision/usbvision-i2c.c.
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/usbvision/usbvision-i2c.c
+++ drivers/media/video/usbvision/usbvision-i2c.c
@@ -28,7 +28,6 @@
 #include <linux/module.h>
 #include <linux/delay.h>
 #include <linux/slab.h>
-#include <linux/version.h>
 #include <linux/utsname.h>
 #include <linux/init.h>
 #include <asm/uaccess.h>
