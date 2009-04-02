039fd8ce6258e01ec29f1637f9bf1868dd877c55
Cyrus Massoumi <cyrusm@gmx.net>
2009-04-02 16:57:12 -0700

    ext3: remove the BKL in ext3/ioctl.c
    Reformat ext3/ioctl.c to make it look more like ext4/ioctl.c and remove
    the BKL around ext3_ioctl().
    
    Signed-off-by: Cyrus Massoumi <cyrusm@gmx.net>
    Cc: <linux-ext4@vger.kernel.org>
    Acked-by: Jan Kara <jack@ucw.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/ext3/file.c
+++ fs/ext3/file.c
@@ -112,7 +112,7 @@ const struct file_operations ext3_file_operations = {
 	.write		= do_sync_write,
 	.aio_read	= generic_file_aio_read,
 	.aio_write	= ext3_file_write,
-	.ioctl		= ext3_ioctl,
+	.unlocked_ioctl	= ext3_ioctl,
 #ifdef CONFIG_COMPAT
 	.compat_ioctl	= ext3_compat_ioctl,
 #endif
