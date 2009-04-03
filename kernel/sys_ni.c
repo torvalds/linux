942405f36038b8f930ab67e24aa1ad72bee96a25
Eric W. Biederman <ebiederm@xmission.com>
2009-04-03 01:01:43 -0700

    sysctl: Remove the cond_syscall entry for sys32_sysctl
    Now that all architechtures are use compat_sys_sysctl and sys32_sysctl
    does not exist there is not point in retaining a cond_syscall
    entry for it.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>

--- kernel/sys_ni.c
+++ kernel/sys_ni.c
@@ -139,7 +139,6 @@ cond_syscall(sys_pciconfig_read);
 cond_syscall(sys_pciconfig_write);
 cond_syscall(sys_pciconfig_iobase);
 cond_syscall(sys32_ipc);
-cond_syscall(sys32_sysctl);
 cond_syscall(ppc_rtas);
 cond_syscall(sys_spu_run);
 cond_syscall(sys_spu_create);
