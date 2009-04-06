f4952f6cbeb025bc90906b740dc495f4817266d6
Michael Ellerman <michael@ellerman.id.au>
2009-04-06 04:40:45 +0000

    powerpc/ftrace: Fix #if that should be #ifdef
    Commit bb7253403f7a4670a128e4c080fd8ea1bd4d5029 ("powerpc64,
    ftrace: save toc only on modules for function graph"), added an
    #if CONFIG_PPC64.  This changes it to #ifdef.
    
    Fixes the following warning on 32-bit builds:
     arch/powerpc/kernel/ftrace.c:562:5: error: "CONFIG_PPC64" is not defined
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Acked-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kernel/ftrace.c
+++ arch/powerpc/kernel/ftrace.c
@@ -564,7 +564,7 @@ void prepare_ftrace_return(unsigned long *parent, unsigned long self_addr)
 	if (unlikely(atomic_read(&current->tracing_graph_pause)))
 		return;
 
-#if CONFIG_PPC64
+#ifdef CONFIG_PPC64
 	/* non core kernel code needs to save and restore the TOC */
 	if (REGION_ID(self_addr) != KERNEL_REGION_ID)
 		return_hooker = (unsigned long)&mod_return_to_handler;
