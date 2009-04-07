9c83633ad38138855181af6936e8ac570ef7e2cb
Dan Carpenter <error27@gmail.com>
2009-04-07 14:48:16 +0300

    missing unlock in jfs_quota_write()
    We should unlock &inode->i_mutex on the error path.  This bug was
    in ext2_quota_write().  I sent a patch to them today as well.
    
    Found by smatch (http://repo.or.cz/w/smatch.git).  Compile tested.
    
    regards,
    dan carpenter
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Signed-off-by: Dave Kleikamp <shaggy@linux.vnet.ibm.com>

--- fs/jfs/super.c
+++ fs/jfs/super.c
@@ -720,8 +720,10 @@ static ssize_t jfs_quota_write(struct super_block *sb, int type,
 		blk++;
 	}
 out:
-	if (len == towrite)
+	if (len == towrite) {
+		mutex_unlock(&inode->i_mutex);
 		return err;
+	}
 	if (inode->i_size < off+len-towrite)
 		i_size_write(inode, off+len-towrite);
 	inode->i_version++;
