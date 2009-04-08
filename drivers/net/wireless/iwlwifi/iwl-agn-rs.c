ddfcf999274838a8dfb0ccf8e69fc1e50eea3341
Daniel C Halperin <daniel.c.halperin@intel.com>
2009-04-08 11:39:28 -0700

    iwlwifi: do not set dual_stream_ant_msk for 3 streams
    This patch fixes a bug introduced by commit "iwlwifi: adding MIMO3 support
    in rate scaling". We should not set the dual_stream_ant_msk (for 2x2 MIMO)
    when using a 3x3 rate, this will cause a SYSASSERT in uCode.
    
    Note: there is no triple_stream_ant_msk, because all three antennas are used.
    
    Signed-off-by: Daniel C Halperin <daniel.c.halperin@intel.com>
    Acked-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-agn-rs.c
+++ drivers/net/wireless/iwlwifi/iwl-agn-rs.c
@@ -2666,9 +2666,6 @@ static void rs_fill_link_cmd(const struct iwl_priv *priv,
 	} else if (num_of_ant(tbl_type.ant_type) == 2) {
 		lq_cmd->general_params.dual_stream_ant_msk =
 						tbl_type.ant_type;
-	} else if (num_of_ant(tbl_type.ant_type) == 3) {
-		lq_cmd->general_params.dual_stream_ant_msk =
-						tbl_type.ant_type;
 	} /* otherwise we don't modify the existing value */
 
 	index++;
