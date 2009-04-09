3121bfe7631126d1b13064855ac2cfa164381bb0
Miklos Szeredi <mszeredi@suse.cz>
2009-04-09 17:37:53 +0200

    fuse: fix "direct_io" private mmap
    MAP_PRIVATE mmap could return stale data from the cache for
    "direct_io" files.  Fix this by flushing the cache on mmap. 
    
    Found with a slightly modified fsx-linux.
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1298,6 +1298,8 @@ static int fuse_direct_mmap(struct file *file, struct vm_area_struct *vma)
 	if (vma->vm_flags & VM_MAYSHARE)
 		return -ENODEV;
 
+	invalidate_inode_pages2(file->f_mapping);
+
 	return generic_file_mmap(file, vma);
 }
 
