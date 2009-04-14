7852fd08fdc78bf43150137bdbfdfdccdefffe0f
Takashi Iwai <tiwai@suse.de>
2009-04-14 12:25:52 +0200

    ALSA: lx6464es - Use snd_card_create()
    Use snd_card_create() instead of the obsoleted snd_card_new().
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/lx6464es/lx6464es.c
+++ sound/pci/lx6464es/lx6464es.c
@@ -1091,9 +1091,9 @@ static int __devinit snd_lx6464es_probe(struct pci_dev *pci,
 		return -ENOENT;
 	}
 
-	card = snd_card_new(index[dev], id[dev], THIS_MODULE, 0);
-	if (card == NULL)
-		return -ENOMEM;
+	err = snd_card_create(index[dev], id[dev], THIS_MODULE, 0, &card);
+	if (err < 0)
+		return err;
 
 	err = snd_lx6464es_create(card, pci, &chip);
 	if (err < 0) {
