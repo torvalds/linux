f69955855eac55a048d26a1618f50dfaa160a006
Chris Mason <chris.mason@oracle.com>
2009-04-15 13:22:37 -0400

    Export filemap_write_and_wait_range
    This wasn't exported before and is useful (used by the experimental ext3
    data=guarded code)
    
    Signed-off-by: Chris Mason <chris.mason@oracle.com>
    Acked-by: Theodore Tso <tytso@mit.edu>
    Acked-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/filemap.c
+++ mm/filemap.c
@@ -441,6 +441,7 @@ int filemap_write_and_wait_range(struct address_space *mapping,
 	}
 	return err;
 }
+EXPORT_SYMBOL(filemap_write_and_wait_range);
 
 /**
  * add_to_page_cache_locked - add a locked page to the pagecache
