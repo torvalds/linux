dfbc4752eab33e66f113f9daa2effbe241cd661d
Nick Piggin <npiggin@suse.de>
2009-04-15 10:27:07 +0200

    brd: support barriers
    brd is always ordered (not that it matters, as it is defined not to
    survive when the system goes down). So tell the block layer it is
    ordered, which might be of help with testing filesystems.
    
    Signed-off-by: Nick Piggin <npiggin@suse.de>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- drivers/block/brd.c
+++ drivers/block/brd.c
@@ -436,6 +436,7 @@ static struct brd_device *brd_alloc(int i)
 	if (!brd->brd_queue)
 		goto out_free_dev;
 	blk_queue_make_request(brd->brd_queue, brd_make_request);
+	blk_queue_ordered(brd->brd_queue, QUEUE_ORDERED_TAG, NULL);
 	blk_queue_max_sectors(brd->brd_queue, 1024);
 	blk_queue_bounce_limit(brd->brd_queue, BLK_BOUNCE_ANY);
 
