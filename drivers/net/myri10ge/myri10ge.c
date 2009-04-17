6498be3fba7b102132241be5763263e8e8bf6f9e
Brice Goglin <brice@myri.com>
2009-04-16 17:56:57 -0700

    myri10ge: fix tx ring size in ethtool -g
    Fix tx ring size reported via ethtool -g.
    
    Signed-off-by: Brice Goglin <brice@myri.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/myri10ge/myri10ge.c
+++ drivers/net/myri10ge/myri10ge.c
@@ -1696,7 +1696,7 @@ myri10ge_get_ringparam(struct net_device *netdev,
 	ring->rx_mini_max_pending = mgp->ss[0].rx_small.mask + 1;
 	ring->rx_max_pending = mgp->ss[0].rx_big.mask + 1;
 	ring->rx_jumbo_max_pending = 0;
-	ring->tx_max_pending = mgp->ss[0].rx_small.mask + 1;
+	ring->tx_max_pending = mgp->ss[0].tx.mask + 1;
 	ring->rx_mini_pending = ring->rx_mini_max_pending;
 	ring->rx_pending = ring->rx_max_pending;
 	ring->rx_jumbo_pending = ring->rx_jumbo_max_pending;
