dfed0ef9b3ff9e37903920b6938ed33344ad0b3d
Takashi Iwai <tiwai@suse.de>
2009-04-21 18:33:12 +0200

    ALSA: hda - Fix a typo in debug print for realtek auto-detection
    The NID and ASS numbers were swapped...
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -1026,7 +1026,7 @@ static void alc_subsystem_id(struct hda_codec *codec,
 	ass = snd_hda_codec_get_pincfg(codec, nid);
 	snd_printd("realtek: No valid SSID, "
 		   "checking pincfg 0x%08x for NID 0x%x\n",
-		   nid, ass);
+		   ass, nid);
 	if (!(ass & 1) && !(ass & 0x100000))
 		return;
 	if ((ass >> 30) != 1)	/* no physical connection */
