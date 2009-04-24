9c935386512a3faa1be1c3d81cba38b7259a43f5
Daniel Mack <daniel@caiaq.de>
2009-04-24 15:00:25 +0200

    ASoC: cs4270: fix Master Capture Switch polarity
    The control modifies the MUTE register, hence the polarity must be
    inverted.
    
    Signed-off-by: Daniel Mack <daniel@caiaq.de>
    Acked-By: Timur Tabi <timur@freescale.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/cs4270.c
+++ sound/soc/codecs/cs4270.c
@@ -486,7 +486,7 @@ static const struct snd_kcontrol_new cs4270_snd_controls[] = {
 	SOC_SINGLE("Zero Cross Switch", CS4270_TRANS, 5, 1, 0),
 	SOC_SINGLE("Popguard Switch", CS4270_MODE, 0, 1, 1),
 	SOC_SINGLE("Auto-Mute Switch", CS4270_MUTE, 5, 1, 0),
-	SOC_DOUBLE("Master Capture Switch", CS4270_MUTE, 3, 4, 1, 0)
+	SOC_DOUBLE("Master Capture Switch", CS4270_MUTE, 3, 4, 1, 1)
 };
 
 /*
