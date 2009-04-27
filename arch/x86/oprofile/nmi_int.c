6adf406f0a0eaf37251018d15f51e93f5b538ee6
Andi Kleen <andi@firstfloor.org>
2009-04-27 17:44:13 +0200

    oprofile: add support for Core i7 and Atom
    The registers are about the same as other Family 6 CPUs
    so we only need to add detection.
    
    I'm not completely happy with calling Nehalem Core i7 because
    there will be undoubtedly other Nehalem based CPUs
    in the future with different marketing names, but it's
    the best we got for now.
    
    Requires updated oprofile userland for the new event files.
    
    If you don't want to update right now you can also use
    oprofile.force_arch_perfmon=1 (added in the next patch) with 0.9.4
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Robert Richter <robert.richter@amd.com>

--- arch/x86/oprofile/nmi_int.c
+++ arch/x86/oprofile/nmi_int.c
@@ -417,6 +417,13 @@ static int __init ppro_init(char **cpu_type)
 	case 15: case 23:
 		*cpu_type = "i386/core_2";
 		break;
+	case 26:
+		arch_perfmon_setup_counters();
+		*cpu_type = "i386/core_i7";
+		break;
+	case 28:
+		*cpu_type = "i386/atom";
+		break;
 	default:
 		/* Unknown */
 		return 0;
