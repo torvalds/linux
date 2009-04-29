6e85c5ba73c07b990798087e9b858c065db2b234
H Hartley Sweeten <hartleys@visionengravers.com>
2009-04-29 19:14:32 -0400

    kernel/posix-cpu-timers.c: fix sparse warning
    Sparse reports the following in kernel/posix-cpu-timers.c:
    
      warning: symbol 'firing' shadows an earlier one
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Subrata Modak <subrata@linux.vnet.ibm.com>
    LKML-Reference: <BD79186B4FD85F4B8E60E381CAEE1909016C1AFE@mi8nycmail19.Mi8.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/posix-cpu-timers.c
+++ kernel/posix-cpu-timers.c
@@ -1420,19 +1420,19 @@ void run_posix_cpu_timers(struct task_struct *tsk)
 	 * timer call will interfere.
 	 */
 	list_for_each_entry_safe(timer, next, &firing, it.cpu.entry) {
-		int firing;
+		int cpu_firing;
+
 		spin_lock(&timer->it_lock);
 		list_del_init(&timer->it.cpu.entry);
-		firing = timer->it.cpu.firing;
+		cpu_firing = timer->it.cpu.firing;
 		timer->it.cpu.firing = 0;
 		/*
 		 * The firing flag is -1 if we collided with a reset
 		 * of the timer, which already reported this
 		 * almost-firing as an overrun.  So don't generate an event.
 		 */
-		if (likely(firing >= 0)) {
+		if (likely(cpu_firing >= 0))
 			cpu_timer_fire(timer);
-		}
 		spin_unlock(&timer->it_lock);
 	}
 }
