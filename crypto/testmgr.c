29ecd4ab3d3aa8bb231361937165dfbbbc534e9a
Jarod Wilson <jarod@redhat.com>
2009-05-04 19:51:17 +0800

    crypto: testmgr - Print self-test pass notices in fips mode
    According to our FIPS CAVS testing lab guru, when we're in fips mode,
    we must print out notices of successful self-test completion for
    every alg to be compliant.
    
    New and improved v2, without strncmp crap. Doesn't need to touch a flag
    though, due to not moving the notest label around anymore.
    
    Applies atop '[PATCH v2] crypto: catch base cipher self-test failures
    in fips mode'.
    
    Personally, I wouldn't mind seeing this info printed out regardless of
    whether or not we're in fips mode, I think its useful info, but will
    stick with only in fips mode for now.
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -2234,6 +2234,10 @@ test_done:
 	if (fips_enabled && rc)
 		panic("%s: %s alg self test failed in fips mode!\n", driver, alg);
 
+	if (fips_enabled && !rc)
+		printk(KERN_INFO "alg: self-tests for %s (%s) passed\n",
+		       driver, alg);
+
 	return rc;
 
 notest:
