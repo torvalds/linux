226c7ffe74474257b4b87bd38ae8ba0030cf65e2
Joe Eykholt <jeykholt@cisco.com>
2009-05-06 10:52:51 -0700

    [SCSI] net, libfcoe: Add the FCoE Initialization Protocol ethertype
    FIP is the FCoE Initialization Protocol and this patch
    adds the protocol ethertype to the kernel's list of
    ethertypes.
    
    Signed-off-by: Joe Eykholt <jeykholt@cisco.com>
    Signed-off-by: Robert Love <robert.w.love@intel.com>
    Acked-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- include/linux/if_ether.h
+++ include/linux/if_ether.h
@@ -79,6 +79,7 @@
 #define ETH_P_AOE	0x88A2		/* ATA over Ethernet		*/
 #define ETH_P_TIPC	0x88CA		/* TIPC 			*/
 #define ETH_P_FCOE	0x8906		/* Fibre Channel over Ethernet  */
+#define ETH_P_FIP	0x8914		/* FCoE Initialization Protocol */
 #define ETH_P_EDSA	0xDADA		/* Ethertype DSA [ NOT AN OFFICIALLY REGISTERED ID ] */
 
 /*
