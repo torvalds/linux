dbdb1c32784d0145fe545410e9771070e5dc702b
Chia-chi Yeh <chiachi@android.com>
2009-05-08 04:02:40 +0800

    net: add PPP on L2TP Access Concentrator (PPPoLAC) driver.
    Signed-off-by: Chia-chi Yeh <chiachi@android.com>

--- include/linux/if_pppox.h
+++ include/linux/if_pppox.h
@@ -27,6 +27,7 @@
 #include <linux/ppp_channel.h>
 #endif /* __KERNEL__ */
 #include <linux/if_pppol2tp.h>
+#include <linux/if_pppolac.h>
 
 /* For user-space programs to pick up these definitions
  * which they wouldn't get otherwise without defining __KERNEL__
@@ -51,7 +52,8 @@ struct pppoe_addr{
  */ 
 #define PX_PROTO_OE    0 /* Currently just PPPoE */
 #define PX_PROTO_OL2TP 1 /* Now L2TP also */
-#define PX_MAX_PROTO   2
+#define PX_PROTO_OLAC  2
+#define PX_MAX_PROTO   3
 
 struct sockaddr_pppox { 
        sa_family_t     sa_family;            /* address family, AF_PPPOX */ 
@@ -141,6 +143,13 @@ struct pppoe_opt {
 					     relayed to (PPPoE relaying) */
 };
 
+struct pppolac_opt {
+	__u32	local;
+	__u32	remote;
+	__u16	sequence;
+	__u8	sequencing;
+};
+
 #include <net/sock.h>
 
 struct pppox_sock {
@@ -150,6 +159,7 @@ struct pppox_sock {
 	struct pppox_sock	*next;	  /* for hash table */
 	union {
 		struct pppoe_opt pppoe;
+		struct pppolac_opt lac;
 	} proto;
 	__be16			num;
 };
