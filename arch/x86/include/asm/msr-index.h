bf8b9a63c18a1a7777571650de0c9f4fd4368ca0
Jaswinder Singh Rajput <jaswinder@kernel.org>
2009-05-08 20:53:58 +0530

    x86: msr-index.h remove duplicate MSR C001_0015 declaration
    MSRC001_0015 Hardware Configuration Register (HWCR) is already defined
    as MSR_K7_HWCR.
    
    And HWCR is available for >= K7.
    
    So MSR_K8_HWCR is not required and no-one is using it.
    
    [ Impact: cleanup, no object code change ]
    
    Signed-off-by: Jaswinder Singh Rajput <jaswinderrajput@gmail.com>

--- arch/x86/include/asm/msr-index.h
+++ arch/x86/include/asm/msr-index.h
@@ -121,7 +121,6 @@
 #define MSR_K8_TOP_MEM1			0xc001001a
 #define MSR_K8_TOP_MEM2			0xc001001d
 #define MSR_K8_SYSCFG			0xc0010010
-#define MSR_K8_HWCR			0xc0010015
 #define MSR_K8_INT_PENDING_MSG		0xc0010055
 /* C1E active bits in int pending message */
 #define K8_INTP_C1E_ACTIVE_MASK		0x18000000
