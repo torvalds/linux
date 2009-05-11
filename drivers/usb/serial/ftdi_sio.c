87c1edd217a6742e48028db6664d7763de0449f6
Jason Wessel <jason.wessel@windriver.com>
2009-05-11 15:24:08 -0500

    USB: serial: ftd_sio usb: move status check
    Alan Stern commented that the private driver counts must be updated
    regard less of the status return on the urb when the write call back
    is executed.
    
    This patch alters the behavior to update the private driver counts by
    simply moving the status check to after the driver count update.
    
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio.c
+++ drivers/usb/serial/ftdi_sio.c
@@ -1884,11 +1884,6 @@ static void ftdi_write_bulk_callback(struct urb *urb)
 
 	dbg("%s - port %d", __func__, port->number);
 
-	if (status) {
-		dbg("nonzero write bulk status received: %d", status);
-		return;
-	}
-
 	priv = usb_get_serial_port_data(port);
 	if (!priv) {
 		dbg("%s - bad port private data pointer - exiting", __func__);
@@ -1905,6 +1900,11 @@ static void ftdi_write_bulk_callback(struct urb *urb)
 	--priv->tx_outstanding_urbs;
 	priv->tx_outstanding_bytes -= countback;
 	spin_unlock_irqrestore(&priv->tx_lock, flags);
+
+	if (status) {
+		dbg("nonzero write bulk status received: %d", status);
+		return;
+	}
 
 	usb_serial_port_softint(port);
 } /* ftdi_write_bulk_callback */
