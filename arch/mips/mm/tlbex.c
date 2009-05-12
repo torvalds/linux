faed5288af0f05aa61ac1e8d47306d855a2868f0
David Daney <ddaney@caviumnetworks.com>
2009-05-12 12:41:55 -0700

    MIPS: Remove dead case label.
    CPU_CAVIUM_OCTEON is mips_r2 which is handled before the switch.  This
    label in the switch statement is dead code, so we remove it.
    
    Signed-off-by: David Daney <ddaney@caviumnetworks.com>
    Reviewed by: David VomLehn <dvomlehn@cisco.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mm/tlbex.c
+++ arch/mips/mm/tlbex.c
@@ -312,7 +312,6 @@ static void __cpuinit build_tlb_write_entry(u32 **p, struct uasm_label **l,
 	case CPU_BCM3302:
 	case CPU_BCM4710:
 	case CPU_LOONGSON2:
-	case CPU_CAVIUM_OCTEON:
 	case CPU_R5500:
 		if (m4kc_tlbp_war())
 			uasm_i_nop(p);
