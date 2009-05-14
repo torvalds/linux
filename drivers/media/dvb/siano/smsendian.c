2c5582e58ce2982fd64da21907dfad4061fc1f71
Mauro Carvalho Chehab <mchehab@redhat.com>
2009-05-14 09:35:26 -0300

    V4L/DVB (11780): Siano: fix compilation error due to the lack of EXTERNAL_SYMBOL
    Cc: Uri Shkolnik <urishk@yahoo.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/siano/smsendian.c
+++ drivers/media/dvb/siano/smsendian.c
@@ -49,6 +49,7 @@ void smsendian_handle_tx_message(void *buffer)
 	}
 #endif /* __BIG_ENDIAN */
 }
+EXPORT_SYMBOL_GPL(smsendian_handle_tx_message);
 
 void smsendian_handle_rx_message(void *buffer)
 {
@@ -86,6 +87,7 @@ void smsendian_handle_rx_message(void *buffer)
 	}
 #endif /* __BIG_ENDIAN */
 }
+EXPORT_SYMBOL_GPL(smsendian_handle_rx_message);
 
 void smsendian_handle_message_header(void *msg)
 {
@@ -97,4 +99,4 @@ void smsendian_handle_message_header(void *msg)
 	phdr->msgFlags = le16_to_cpu(phdr->msgFlags);
 #endif /* __BIG_ENDIAN */
 }
-
+EXPORT_SYMBOL_GPL(smsendian_handle_message_header);
