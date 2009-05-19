d95e9883ce28c23cb852d599bf0a0889527c1d09
Uri Shkolnik <uris@siano-ms.com>
2009-05-19 12:56:16 -0300

    V4L/DVB (11892): Siano: smsendian - declare function as extern
    Declare the object function as 'extern'
    
    Signed-off-by: Uri Shkolnik <uris@siano-ms.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/siano/smsendian.h
+++ drivers/media/dvb/siano/smsendian.h
@@ -24,9 +24,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <asm/byteorder.h>
 
-void smsendian_handle_tx_message(void *buffer);
-void smsendian_handle_rx_message(void *buffer);
-void smsendian_handle_message_header(void *msg);
+extern void smsendian_handle_tx_message(void *buffer);
+extern void smsendian_handle_rx_message(void *buffer);
+extern void smsendian_handle_message_header(void *msg);
 
 #endif /* __SMS_ENDIAN_H__ */
 
