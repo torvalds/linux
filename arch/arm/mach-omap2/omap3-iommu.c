5c651ffaee6f07aa1f5e6d0763845a2ee606b6bd
Hiroshi DOYU <Hiroshi.DOYU@nokia.com>
2009-05-19 09:07:55 +0300

    omap iommu: add MPU_BRIDGE_IOMMU for tidspbridge migration
    Currently "tidspbridge" driver uses its own mmu implementation and
    will migrate to use this "omap iommu" eventually. This config is
    provided to make this migration happen smoothly.
    
    Signed-off-by: Hiroshi DOYU <Hiroshi.DOYU@nokia.com>

--- arch/arm/mach-omap2/omap3-iommu.c
+++ arch/arm/mach-omap2/omap3-iommu.c
@@ -36,11 +36,13 @@ static const struct iommu_platform_data omap3_iommu_pdata[] __initconst = {
 		.nr_tlb_entries = 8,
 		.clk_name = "cam_ick",
 	},
+#if defined(CONFIG_MPU_BRIDGE_IOMMU)
 	{
 		.name = "iva2",
 		.nr_tlb_entries = 32,
 		.clk_name = "iva2_ck",
 	},
+#endif
 };
 #define NR_IOMMU_DEVICES ARRAY_SIZE(omap3_iommu_pdata)
 
