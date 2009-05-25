759d427aa5a9d88a81afd11817cdeb40aea85234
Theodore Ts'o <tytso@mit.edu>
2009-05-25 11:51:00 -0400

    ext4: remove unused function __ext4_write_dirty_metadata
    The __ext4_write_dirty_metadata() function was introduced by commit
    0390131b, "ext4: Allow ext4 to run without a journal", but nothing
    ever used the function, either then or since.  So let's remove it and
    save a bit of space.
    
    Cc: Frank Mayhar <fmayhar@google.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -4790,25 +4790,6 @@ int ext4_write_inode(struct inode *inode, int wait)
 	return ext4_force_commit(inode->i_sb);
 }
 
-int __ext4_write_dirty_metadata(struct inode *inode, struct buffer_head *bh)
-{
-	int err = 0;
-
-	mark_buffer_dirty(bh);
-	if (inode && inode_needs_sync(inode)) {
-		sync_dirty_buffer(bh);
-		if (buffer_req(bh) && !buffer_uptodate(bh)) {
-			ext4_error(inode->i_sb, __func__,
-				   "IO error syncing inode, "
-				   "inode=%lu, block=%llu",
-				   inode->i_ino,
-				   (unsigned long long)bh->b_blocknr);
-			err = -EIO;
-		}
-	}
-	return err;
-}
-
 /*
  * ext4_setattr()
  *
