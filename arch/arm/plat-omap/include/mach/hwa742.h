088962c243db42b9c608f30be3e3a05a5b696895
Andrew de Quincey <adq_dvb@lidskialf.net>
2009-05-28 14:03:31 -0700

    ARM: OMAP1: Make 770 LCD work
    Make 770 LCD work by adding clk_add_alias().
    Also remove the old unused functions.
    
    Note that the clk_add_alias() could probably be moved
    to arch/arm/clkdev.c later on.
    
    Cc: linux-fbdev-devel@lists.sourceforge.net
    Signed-off-by: Andrew de Quincey <adq_dvb@lidskialf.net>
    Signed-off-by: Imre Deak <imre.deak@nokia.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com

--- arch/arm/plat-omap/include/mach/hwa742.h
+++ arch/arm/plat-omap/include/mach/hwa742.h
@@ -2,10 +2,6 @@
 #define _HWA742_H
 
 struct hwa742_platform_data {
-	void		(*power_up)(struct device *dev);
-	void		(*power_down)(struct device *dev);
-	unsigned long	(*get_clock_rate)(struct device *dev);
-
 	unsigned	te_connected:1;
 };
 
