c722c625dbe2758d53365c0ed7d401b0e286f2cf
Herbert Xu <herbert@gondor.apana.org.au>
2009-06-03 21:45:55 -0700

    tun: Only wake up writers
    When I added socket accounting to tun I inadvertently introduced
    spurious wake-up events that kills qemu performance.  The problem
    occurs when qemu polls on the tun fd for read, and then transmits
    packets.  For each packet transmitted, we will wake up qemu even
    if it only cares about read events.
    
    Now this affects all sockets, but it is only a new problem for
    tun.  So this patch tries to fix it for tun first and we can then
    look at the problem in general.
     
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -844,11 +844,11 @@ static void tun_sock_write_space(struct sock *sk)
 	if (!sock_writeable(sk))
 		return;
 
-	if (sk->sk_sleep && waitqueue_active(sk->sk_sleep))
-		wake_up_interruptible_sync(sk->sk_sleep);
-
 	if (!test_and_clear_bit(SOCK_ASYNC_NOSPACE, &sk->sk_socket->flags))
 		return;
+
+	if (sk->sk_sleep && waitqueue_active(sk->sk_sleep))
+		wake_up_interruptible_sync(sk->sk_sleep);
 
 	tun = container_of(sk, struct tun_sock, sk)->tun;
 	kill_fasync(&tun->fasync, SIGIO, POLL_OUT);
