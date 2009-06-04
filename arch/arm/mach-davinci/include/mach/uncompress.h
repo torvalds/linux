8593790d604c29a717d5914159ce78c7bb9899fb
Mark A. Greer <mgreer@mvista.com>
2009-06-03 18:41:53 -0700

    davinci: da8xx: Add support for DA830/OMAP-L137 EVM board
    Add support for the DA830/OMAP-L137 Evaluation Module (EVM)
    from TI.  The EVM has User Interface (UI) and Audio cards
    that can be connected which contain various devices.
    Support for those devices and ones on the EVM will be
    added in subsequent patches.
    
    Additional generalizations for future SoCs in da8xx family done by
    Sudhakar Rajashekhara and Sekhar Nori.
    
    Signed-off-by: Steve Chen <schen@mvista.com>
    Signed-off-by: Mark A. Greer <mgreer@mvista.com>
    Cc: Sudhakar Rajashekhara <sudhakar.raj@ti.com>
    Cc: Sekhar Nori <nsekhar@ti.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-davinci/include/mach/uncompress.h
+++ arch/arm/mach-davinci/include/mach/uncompress.h
@@ -21,8 +21,10 @@ static u32 *uart;
 
 static u32 *get_uart_base(void)
 {
-	/* Add logic here for new platforms, using __macine_arch_type */
-	return (u32 *)DAVINCI_UART0_BASE;
+	if (__machine_arch_type == MACH_TYPE_DAVINCI_DA8XX_EVM)
+		return (u32 *)DA8XX_UART2_BASE;
+	else
+		return (u32 *)DAVINCI_UART0_BASE;
 }
 
 /* PORT_16C550A, in polled non-fifo mode */
