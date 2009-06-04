add85a418f8e224d2b09d4a8d14c6c060ba3a79c
Simon POLETTE <spolette@adetelgroup.com>
2009-06-04 12:09:01 +0200

    PATCH] mx27pdk: rename mxc_map_io to mx27_map_io
    Hi,
    Fixed issue in the mxc-master head :
    
    Signed-off-by: Simon POLETTE <spolette@adnlysd018.(none)>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-mx2/mx27pdk.c
+++ arch/arm/mach-mx2/mx27pdk.c
@@ -88,7 +88,7 @@ MACHINE_START(MX27_3DS, "Freescale MX27PDK")
 	.phys_io        = AIPI_BASE_ADDR,
 	.io_pg_offst    = ((AIPI_BASE_ADDR_VIRT) >> 18) & 0xfffc,
 	.boot_params    = PHYS_OFFSET + 0x100,
-	.map_io         = mxc_map_io,
+	.map_io         = mx27_map_io,
 	.init_irq       = mxc_init_irq,
 	.init_machine   = mx27pdk_init,
 	.timer          = &mx27pdk_timer,
