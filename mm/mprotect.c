dab5855b12411334355ba21349a06700e4ae7a3b
Peter Zijlstra <a.p.zijlstra@chello.nl>
2009-06-08 21:11:57 +0300

    perf_counter: Add mmap event hooks to mprotect()
    Some JIT compilers allocate memory for generated code with
    posix_memalign() + mprotect() so we need to hook into mprotect()
    to make sure 'perf' is aware that we're executing code in
    anonymous memory.
    
    [ penberg@cs.helsinki.fi: move the hook to sys_mprotect() ]
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>
    LKML-Reference: <Pine.LNX.4.64.0906082111030.12407@melkki.cs.Helsinki.FI>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- mm/mprotect.c
+++ mm/mprotect.c
@@ -23,6 +23,7 @@
 #include <linux/swapops.h>
 #include <linux/mmu_notifier.h>
 #include <linux/migrate.h>
+#include <linux/perf_counter.h>
 #include <asm/uaccess.h>
 #include <asm/pgtable.h>
 #include <asm/cacheflush.h>
@@ -299,6 +300,7 @@ SYSCALL_DEFINE3(mprotect, unsigned long, start, size_t, len,
 		error = mprotect_fixup(vma, &prev, nstart, tmp, newflags);
 		if (error)
 			goto out;
+		perf_counter_mmap(vma);
 		nstart = tmp;
 
 		if (nstart < prev->vm_end)
