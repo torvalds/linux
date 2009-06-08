c4865679df5453d1bc94c534f4d94a364c0546df
Takashi Iwai <tiwai@suse.de>
2009-06-08 12:57:17 +0200

    ALSA: ca0106 - Fix master volume scale
    The master volume dB scale was wrongly defined as 0.50dB setp while
    it must be 0.25dB step.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/ca0106/ca0106_mixer.c
+++ sound/pci/ca0106/ca0106_mixer.c
@@ -739,7 +739,7 @@ static int __devinit rename_ctl(struct snd_card *card, const char *src, const ch
 	} while (0)
 
 static __devinitdata
-DECLARE_TLV_DB_SCALE(snd_ca0106_master_db_scale, -6375, 50, 1);
+DECLARE_TLV_DB_SCALE(snd_ca0106_master_db_scale, -6375, 25, 1);
 
 static char *slave_vols[] __devinitdata = {
 	"Analog Front Playback Volume",
