2f0accc13520b2644b85f80aedce10d10d88b0ca
Johannes Berg <johannes@sipsolutions.net>
2009-06-10 16:50:29 +0200

    cfg80211: fix rfkill locking problem
    rfkill currently requires a global lock within the
    rfkill_register() function, and holds that lock over
    calls to the set_block() methods. This means that we
    cannot hold a lock around rfkill_register() that we
    also require in set_block(), directly or indirectly.
    Fix cfg80211 to register rfkill outside the block
    locked by its global lock. Much of what cfg80211 does
    in the locked block doesn't need to be locked anyway.
    
    Reported-by: Vasanthakumar Thiagarajan <vasanth@atheros.com>
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/wireless/core.c
+++ net/wireless/core.c
@@ -395,20 +395,22 @@ int wiphy_register(struct wiphy *wiphy)
 	/* check and set up bitrates */
 	ieee80211_set_bitrate_flags(wiphy);
 
-	mutex_lock(&cfg80211_mutex);
-
-	/* set up regulatory info */
-	wiphy_update_regulatory(wiphy, NL80211_REGDOM_SET_BY_CORE);
-
 	res = device_add(&drv->wiphy.dev);
 	if (res)
-		goto out_unlock;
+		return res;
 
 	res = rfkill_register(drv->rfkill);
 	if (res)
 		goto out_rm_dev;
 
+	mutex_lock(&cfg80211_mutex);
+
+	/* set up regulatory info */
+	wiphy_update_regulatory(wiphy, NL80211_REGDOM_SET_BY_CORE);
+
 	list_add(&drv->list, &cfg80211_drv_list);
+
+	mutex_unlock(&cfg80211_mutex);
 
 	/* add to debugfs */
 	drv->wiphy.debugfsdir =
@@ -430,13 +432,10 @@ int wiphy_register(struct wiphy *wiphy)
 
 	cfg80211_debugfs_drv_add(drv);
 
-	res = 0;
-	goto out_unlock;
+	return 0;
 
  out_rm_dev:
 	device_del(&drv->wiphy.dev);
- out_unlock:
-	mutex_unlock(&cfg80211_mutex);
 	return res;
 }
 EXPORT_SYMBOL(wiphy_register);
