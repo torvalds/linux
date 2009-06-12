fc39453debac927a3ba477bafbc6c18c7166ae78
Heiko Carstens <heiko.carstens@de.ibm.com>
2009-06-12 10:26:48 +0200

    [S390] wire up sys_rt_tgsigqueueinfo
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/unistd.h
+++ arch/s390/include/asm/unistd.h
@@ -267,7 +267,8 @@
 #define __NR_epoll_create1	327
 #define	__NR_preadv		328
 #define	__NR_pwritev		329
-#define NR_syscalls 330
+#define __NR_rt_tgsigqueueinfo	330
+#define NR_syscalls 331
 
 /* 
  * There are some system calls that are not present on 64 bit, some
