094167be1fe24a65ef1b4647b1ad22b3b7898621
Mike Frysinger <vapier@gentoo.org>
2009-06-13 10:54:56 -0400

    Blackfin: convert simple headers to asm-generic
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- arch/blackfin/include/asm/bugs.h
+++ arch/blackfin/include/asm/bugs.h
@@ -1,16 +1,1 @@
-/*
- *  include/asm-blackfin/bugs.h
- *
- *  Copyright (C) 1994  Linus Torvalds
- */
-
-/*
- * This is included by init/main.c to check for architecture-dependent bugs.
- *
- * Needs:
- *	void check_bugs(void);
- */
-
-static void check_bugs(void)
-{
-}
+#include <asm-generic/bugs.h>
--- arch/blackfin/include/asm/device.h
+++ arch/blackfin/include/asm/device.h
@@ -1,7 +1,1 @@
-/*
- * Arch specific extensions to struct device
- *
- * This file is released under the GPLv2
- */
 #include <asm-generic/device.h>
-
