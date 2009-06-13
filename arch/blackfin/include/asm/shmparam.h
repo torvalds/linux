71b844f163f8ab66227563c74ef012b4ea5049c8
Mike Frysinger <vapier@gentoo.org>
2009-06-13 10:40:15 -0400

    Blackfin: convert shm/sysv/ipc to asm-generic
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- arch/blackfin/include/asm/shmparam.h
+++ arch/blackfin/include/asm/shmparam.h
@@ -1,6 +1,1 @@
-#ifndef _BFIN_SHMPARAM_H
-#define _BFIN_SHMPARAM_H
-
-#define	SHMLBA PAGE_SIZE	/* attach addr a multiple of this */
-
-#endif				/* _BFIN_SHMPARAM_H */
+#include <asm-generic/shmparam.h>
