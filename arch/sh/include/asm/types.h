a8bd4683134affa76f2edd3a6b8b526832722242
Paul Mundt <lethal@linux-sh.org>
2009-06-14 23:25:27 +0900

    sh: Convert to asm-generic/types.h.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/types.h
+++ arch/sh/include/asm/types.h
@@ -1,26 +1,13 @@
 #ifndef __ASM_SH_TYPES_H
 #define __ASM_SH_TYPES_H
 
-#include <asm-generic/int-ll64.h>
-
-#ifndef __ASSEMBLY__
-
-typedef unsigned short umode_t;
-
-#endif /* __ASSEMBLY__ */
+#include <asm-generic/types.h>
 
 /*
  * These aren't exported outside the kernel to avoid name space clashes
  */
 #ifdef __KERNEL__
-
-#define BITS_PER_LONG 32
-
 #ifndef __ASSEMBLY__
-
-/* Dma addresses are 32-bits wide.  */
-
-typedef u32 dma_addr_t;
 
 #ifdef CONFIG_SUPERH32
 typedef u16 insn_size_t;
@@ -29,7 +16,6 @@ typedef u32 insn_size_t;
 #endif
 
 #endif /* __ASSEMBLY__ */
-
 #endif /* __KERNEL__ */
 
 #endif /* __ASM_SH_TYPES_H */
