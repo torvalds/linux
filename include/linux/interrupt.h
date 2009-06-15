144e2ce6115c0a1ee4cb5c935360ea4e2966b0ce
Nobuhiro Iwamatsu <iwamatsu.nobuhiro@renesas.com>
2009-06-15 12:16:54 +0900

    cpumask: Remove mask field from comments
    By 7be23e278f, mask field was deleted by irqaction. However, it was not
    deleted from comment.
    
    Signed-off-by: Nobuhiro Iwamatsu <iwamatsu.nobuhiro@renesas.com>
    CC: Rusty Russell <rusty@rustcorp.com.au>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -84,7 +84,6 @@ typedef irqreturn_t (*irq_handler_t)(int, void *);
  * struct irqaction - per interrupt action descriptor
  * @handler:	interrupt handler function
  * @flags:	flags (see IRQF_* above)
- * @mask:	no comment as it is useless and about to be removed
  * @name:	name of the device
  * @dev_id:	cookie to identify the device
  * @next:	pointer to the next irqaction for shared interrupts
