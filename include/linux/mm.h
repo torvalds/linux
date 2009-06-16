168f5ac668f63dfb64439766e3ef9e866b83719d
Sergei Trofimovich <slyfox@inbox.ru>
2009-06-16 15:33:02 -0700

    mm cleanup: shmem_file_setup: 'char *' -> 'const char *' for name argument
    As function shmem_file_setup does not modify/allocate/free/pass given
    filename - mark it as const.
    
    Signed-off-by: Sergei Trofimovich <slyfox@inbox.ru>
    Cc: Hugh Dickins <hugh.dickins@tiscali.co.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -724,7 +724,7 @@ static inline int shmem_lock(struct file *file, int lock,
 	return 0;
 }
 #endif
-struct file *shmem_file_setup(char *name, loff_t size, unsigned long flags);
+struct file *shmem_file_setup(const char *name, loff_t size, unsigned long flags);
 
 int shmem_zero_setup(struct vm_area_struct *);
 
