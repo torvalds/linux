4bf259e3ae5015e73282ba66716c4a917e1264ac
James Morris <jmorris@namei.org>
2009-06-17 18:02:14 -0700

    nfs: remove unnecessary NFS_INO_INVALID_ACL checks
    Unless I'm mistaken, NFS_INO_INVALID_ACL is being checked twice during
    getacl calls (i.e. first via nfs_revalidate_inode() and then by each all
    site).
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -2824,8 +2824,6 @@ static ssize_t nfs4_proc_get_acl(struct inode *inode, void *buf, size_t buflen)
 	ret = nfs_revalidate_inode(server, inode);
 	if (ret < 0)
 		return ret;
-	if (NFS_I(inode)->cache_validity & NFS_INO_INVALID_ACL)
-		nfs_zap_acl_cache(inode);
 	ret = nfs4_read_cached_acl(inode, buf, buflen);
 	if (ret != -ENOENT)
 		return ret;
