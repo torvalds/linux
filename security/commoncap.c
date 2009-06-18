775d748ba7a4c68d02c1410c39bce8750394344c
Chia-chi Yeh <chiachi@android.com>
2009-06-19 07:15:05 +0800

    security: Add AID_NET_RAW and AID_NET_ADMIN capability check in cap_capable().
    Signed-off-by: Chia-chi Yeh <chiachi@android.com>

--- security/commoncap.c
+++ security/commoncap.c
@@ -31,6 +31,10 @@
 #include <linux/binfmts.h>
 #include <linux/personality.h>
 
+#ifdef CONFIG_ANDROID_PARANOID_NETWORK
+#include <linux/android_aid.h>
+#endif
+
 /*
  * If a non-root user executes a setuid-root binary in
  * !secure(SECURE_NOROOT) mode, then we raise capabilities.
@@ -77,6 +81,11 @@ int cap_capable(const struct cred *cred, struct user_namespace *targ_ns,
 		int cap, int audit)
 {
 	struct user_namespace *ns = targ_ns;
+
+	if (cap == CAP_NET_RAW && in_egroup_p(AID_NET_RAW))
+		return 0;
+	if (cap == CAP_NET_ADMIN && in_egroup_p(AID_NET_ADMIN))
+		return 0;
 
 	/* See if cred has the capability in the target user namespace
 	 * by examining the target user namespace and all of the target
