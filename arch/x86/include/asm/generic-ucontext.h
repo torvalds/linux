4adc667593f83a18a8e54ce94f250fd166a272ac
Arnd Bergmann <arnd@arndb.de>
2009-06-18 21:48:16 +0200

    x86: add copies of some headers to convert to asm-generic
    Just an intermediate step to make reviewing easier.
    These files are identical copies of the existing headers.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    LKML-Reference: <cover.1245354003.git.arnd@arndb.de>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

---
+++ arch/x86/include/asm/generic-ucontext.h
@@ -0,0 +1,18 @@
+#ifndef _ASM_X86_UCONTEXT_H
+#define _ASM_X86_UCONTEXT_H
+
+#define UC_FP_XSTATE	0x1	/* indicates the presence of extended state
+				 * information in the memory layout pointed
+				 * by the fpstate pointer in the ucontext's
+				 * sigcontext struct (uc_mcontext).
+				 */
+
+struct ucontext {
+	unsigned long	  uc_flags;
+	struct ucontext  *uc_link;
+	stack_t		  uc_stack;
+	struct sigcontext uc_mcontext;
+	sigset_t	  uc_sigmask;	/* mask last for extensibility */
+};
+
+#endif /* _ASM_X86_UCONTEXT_H */
