2af5066f664cb011cf17d2e4414491fe24597e07
Hidetoshi Seto <seto.hidetoshi@jp.fujitsu.com>
2009-06-18 19:20:26 -0700

    PCI: make msi_free_irqs() to use msix_mask_irq() instead of open coded write
    Use msix_mask_irq() instead of direct use of writel, so as not to clear
    preserved bits in the Vector Control register [31:1].
    
    Signed-off-by: Hidetoshi Seto <seto.hidetoshi@jp.fujitsu.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/msi.c
+++ drivers/pci/msi.c
@@ -653,10 +653,7 @@ static int msi_free_irqs(struct pci_dev* dev)
 
 	list_for_each_entry_safe(entry, tmp, &dev->msi_list, list) {
 		if (entry->msi_attrib.is_msix) {
-			writel(1, entry->mask_base + entry->msi_attrib.entry_nr
-				  * PCI_MSIX_ENTRY_SIZE
-				  + PCI_MSIX_ENTRY_VECTOR_CTRL_OFFSET);
-
+			msix_mask_irq(entry, 1);
 			if (list_is_last(&entry->list, &dev->msi_list))
 				iounmap(entry->mask_base);
 		}
