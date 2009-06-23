af054ed0018f0a69f8ea6f7546cbf34385edf13b
David S. Miller <davem@davemloft.net>
2009-06-23 16:01:06 -0700

    ide-cd: Don't warn on bogus block size unless it actually matters.
    Frans Pop reported that his CDROM drive reports a blocksize of 2352,
    and this causes new warnings due to commit
    e8e7b9eb11c34ee18bde8b7011af41938d1ad667 ("ide-cd: fix oops when using
    growisofs").
    
    What we're trying to do is make sure that "blocklen >> SECTOR_BITS"
    is something the block layer won't choke on.
    
    And for Frans' case "2352 >> SECTOR_BITS" is equal to
    "2048 >> SECTOR_BITS", and thats "4".
    
    So warning in this case gives no real benefit.
    
    Reported-by: Frans Pop <elendil@planet.nl>
    Tested-by: Frans Pop <elendil@planet.nl>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/ide/ide-cd.c
+++ drivers/ide/ide-cd.c
@@ -876,9 +876,12 @@ static int cdrom_read_capacity(ide_drive_t *drive, unsigned long *capacity,
 		return stat;
 
 	/*
-	 * Sanity check the given block size
+	 * Sanity check the given block size, in so far as making
+	 * sure the sectors_per_frame we give to the caller won't
+	 * end up being bogus.
 	 */
 	blocklen = be32_to_cpu(capbuf.blocklen);
+	blocklen = (blocklen >> SECTOR_BITS) << SECTOR_BITS;
 	switch (blocklen) {
 	case 512:
 	case 1024:
