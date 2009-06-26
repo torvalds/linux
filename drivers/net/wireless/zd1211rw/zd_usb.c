8b339d05805fb91cc0b5179af5b4d05d9f8b949c
Hin-Tak Leung <hintak.leung@gmail.com>
2009-06-26 05:28:15 +0100

    zd1211rw: adding SONY IFU-WLM2 (054c:0257) as a zd1211b device
    Yevgen Kotikov reported success on the sourceforge zd1211-devs list
    with the following details:
    
    Brand/retail: SONY IFU-WLM2
    USB-IDs: Vendor: 0x054C Device: 0x0257
    chip ID: zd1211b chip 054c:0257 v4802 high 00-0b-6b AL2230_RF pa0 -----
    FCC ID: unknown
    
    Signed-off-by: Hin-Tak Leung <htl10@users.sourceforge.net>
    Tested-by: Yevgen Kotikov <yevgen.kotikov@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/zd1211rw/zd_usb.c
+++ drivers/net/wireless/zd1211rw/zd_usb.c
@@ -60,6 +60,7 @@ static struct usb_device_id usb_ids[] = {
 	{ USB_DEVICE(0x157e, 0x300a), .driver_info = DEVICE_ZD1211 },
 	{ USB_DEVICE(0x0105, 0x145f), .driver_info = DEVICE_ZD1211 },
 	/* ZD1211B */
+	{ USB_DEVICE(0x054c, 0x0257), .driver_info = DEVICE_ZD1211B },
 	{ USB_DEVICE(0x0ace, 0x1215), .driver_info = DEVICE_ZD1211B },
 	{ USB_DEVICE(0x0ace, 0xb215), .driver_info = DEVICE_ZD1211B },
 	{ USB_DEVICE(0x157e, 0x300d), .driver_info = DEVICE_ZD1211B },
