f5812a7a336fb952d819e4427b9a2dce02368e82
Arnaldo Carvalho de Melo <acme@redhat.com>
2009-06-30 11:43:17 -0300

    perf_counter tools: Adjust only prelinked symbol's addresses
    I.e. we can't handle these two kinds of files in the same way:
    
    1) prelinked system library:
    
    [acme@doppio pahole]$ readelf -s /usr/lib64/libdw-0.141.so | egrep 'FUNC.+GLOBAL.+dwfl_report_elf'
       278: 00000030450105a0   261 FUNC    GLOBAL DEFAULT   12 dwfl_report_elf@@ELFUTILS_0.122
    
    2) not prelinked library with debug information from a -debuginfo package:
    
    [acme@doppio pahole]$ readelf -s /usr/lib/debug/usr/lib64/libdw-0.141.so.debug | egrep 'FUNC.+GLOBAL.+dwfl_report_elf'
       629: 00000000000105a0   261 FUNC    GLOBAL DEFAULT   12 dwfl_report_elf
    [acme@doppio pahole]$
    
    Now the numbers I got for a pahole perf run are in line with
    the numbers I get from oprofile.
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    LKML-Reference: <20090630144317.GB12663@ghostprotocols.net>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- tools/perf/util/symbol.h
+++ tools/perf/util/symbol.h
@@ -20,8 +20,9 @@ struct symbol {
 struct dso {
 	struct list_head node;
 	struct rb_root	 syms;
-	unsigned int	 sym_priv_size;
 	struct symbol    *(*find_symbol)(struct dso *, u64 ip);
+	unsigned int	 sym_priv_size;
+	unsigned char	 prelinked;
 	char		 name[0];
 };
 
