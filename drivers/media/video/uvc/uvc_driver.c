5bdf1377587bbde8ef0893cf726de116c2250208
Laurent Pinchart <laurent.pinchart@skynet.be>
2009-07-03 12:32:46 -0300

    V4L/DVB (12188): uvcvideo: Set PROBE_MINMAX quirk for Aveo Technology webcams
    Some Aveo Technology USB 2.0 Camera (1871:0306) revisions seem to require the
    PROBE_MINMAX quirk. As the camera supports uncompressed YUYV data only, it's
    safe to set the quirk even if not strictly required for all models. Update the
    device entry in the device IDs list.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@skynet.be>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/uvc/uvc_driver.c
+++ drivers/media/video/uvc/uvc_driver.c
@@ -1947,7 +1947,8 @@ static struct usb_device_id uvc_ids[] = {
 	  .bInterfaceClass	= USB_CLASS_VIDEO,
 	  .bInterfaceSubClass	= 1,
 	  .bInterfaceProtocol	= 0,
-	  .driver_info		= UVC_QUIRK_PROBE_EXTRAFIELDS },
+	  .driver_info		= UVC_QUIRK_PROBE_MINMAX
+				| UVC_QUIRK_PROBE_EXTRAFIELDS },
 	/* Ecamm Pico iMage */
 	{ .match_flags		= USB_DEVICE_ID_MATCH_DEVICE
 				| USB_DEVICE_ID_MATCH_INT_INFO,
