5ca1ea23c4581f961afaddf1599970e6f05d02dc
Greg Ungerer <gerg@snapgear.com>
2009-07-06 15:23:34 +0000

    fec: fix definition of 5272 version of FEC_X_DES_ACTIVE register
    fec: fix definition of 5272 version of FEC_X_DES_ACTIVE register
    
    The ColdFire 5272 FEC driver has a different register address map
    than other users of the FEC driver. And its definition of the
    FEC_X_DES_ACTIVE register is incorrect, it should be 0x14.
    The fec interface cannot transmit data with the old value.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>
    ----
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/fec.h
+++ drivers/net/fec.h
@@ -51,7 +51,7 @@
 #define FEC_IMASK		0x008 /* Interrupt mask reg */
 #define FEC_IVEC		0x00c /* Interrupt vec status reg */
 #define FEC_R_DES_ACTIVE	0x010 /* Receive descriptor reg */
-#define FEC_X_DES_ACTIVE	0x01c /* Transmit descriptor reg */
+#define FEC_X_DES_ACTIVE	0x014 /* Transmit descriptor reg */
 #define FEC_MII_DATA		0x040 /* MII manage frame reg */
 #define FEC_MII_SPEED		0x044 /* MII speed control reg */
 #define FEC_R_BOUND		0x08c /* FIFO receive bound reg */
