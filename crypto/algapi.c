7ede5a5ba55a696a6e1d8456526e44635e966a81
Herbert Xu <herbert@gondor.apana.org.au>
2009-07-09 11:34:06 +0800

    crypto: api - Fix crypto_drop_spawn crash on blank spawns
    This patch allows crypto_drop_spawn to be called on spawns that
    have not been initialised or have failed initialisation.  This
    fixes potential crashes during initialisation without adding
    special case code.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/algapi.c
+++ crypto/algapi.c
@@ -507,6 +507,9 @@ EXPORT_SYMBOL_GPL(crypto_init_spawn2);
 
 void crypto_drop_spawn(struct crypto_spawn *spawn)
 {
+	if (!spawn->alg)
+		return;
+
 	down_write(&crypto_alg_sem);
 	list_del(&spawn->list);
 	up_write(&crypto_alg_sem);
