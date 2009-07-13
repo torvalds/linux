793f96b58571ec09b2a332c52f5218240eddab0c
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2009-07-13 20:14:08 +0200

    Staging: rtl8192su/ieee80211: remove superfluous container_of definition
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8192su/ieee80211/ieee80211.h
+++ drivers/staging/rtl8192su/ieee80211/ieee80211.h
@@ -47,20 +47,6 @@
 #define IWEVCUSTOM 0x8c02
 #endif
 
-#ifndef container_of
-/**
- * container_of - cast a member of a structure out to the containing structure
- *
- * @ptr:        the pointer to the member.
- * @type:       the type of the container struct this is embedded in.
- * @member:     the name of the member within the struct.
- *
- */
-#define container_of(ptr, type, member) ({                      \
-        const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
-        (type *)( (char *)__mptr - offsetof(type,member) );})
-#endif
-
 #define KEY_TYPE_NA		0x0
 #define KEY_TYPE_WEP40 		0x1
 #define KEY_TYPE_TKIP		0x2
