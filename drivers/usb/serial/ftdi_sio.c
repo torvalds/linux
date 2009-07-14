fca4404c55ab44c7413769f1d9c66451103711a5
Ville Sundberg <vsundber@gmail.com>
2009-07-15 00:27:28 +0300

    USB: ftdi_sio: Add support for GN Otometrics Aurical USB Audiometer
    The patch adds support for the GN Otometrics Aurical USB Audiometer
    (FT232BM-based).
    A new VID and a new PID is added.
    
    Signed-off-by: Ville Sundberg <vsundber@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio.c
+++ drivers/usb/serial/ftdi_sio.c
@@ -698,6 +698,7 @@ static struct usb_device_id id_table_combined [] = {
 	{ USB_DEVICE(MARVELL_VID, MARVELL_SHEEVAPLUG_PID),
 		.driver_info = (kernel_ulong_t)&ftdi_jtag_quirk },
 	{ USB_DEVICE(LARSENBRUSGAARD_VID, LB_ALTITRACK_PID) },
+	{ USB_DEVICE(GN_OTOMETRICS_VID, AURICAL_USB_PID) },
 	{ },					/* Optional parameter entry */
 	{ }					/* Terminating entry */
 };
