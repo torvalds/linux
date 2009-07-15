b5eb0589937eae2d58fca17fa45ed44152e772ed
Johannes Berg <johannes@sipsolutions.net>
2009-07-15 05:23:23 +0000

    net: deprecate print_mac
    We've had %pM for long enough now, time to deprecate
    print_mac() and remove the __maybe_unused attribute
    from DECLARE_MAC_BUF so that variables declared with
    that can be found and removed. Otherwise people are
    putting in new users of print_mac().
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/if_ether.h
+++ include/linux/if_ether.h
@@ -139,10 +139,10 @@ extern ssize_t sysfs_format_mac(char *buf, const unsigned char *addr, int len);
 /*
  *	Display a 6 byte device address (MAC) in a readable format.
  */
-extern char *print_mac(char *buf, const unsigned char *addr);
+extern char *print_mac(char *buf, const unsigned char *addr) __deprecated;
 #define MAC_FMT "%02x:%02x:%02x:%02x:%02x:%02x"
 #define MAC_BUF_SIZE	18
-#define DECLARE_MAC_BUF(var) char var[MAC_BUF_SIZE] __maybe_unused
+#define DECLARE_MAC_BUF(var) char var[MAC_BUF_SIZE]
 
 #endif
 
