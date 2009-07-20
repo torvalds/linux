676cb02dc32adef13d9efb5ea52079e4ede1e3ec
Thomas Gleixner <tglx@linutronix.de>
2009-07-20 23:33:49 +0200

    softirqs: Make wakeup_softirqd static
    No users outside of kernel/softirq.c
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -410,7 +410,6 @@ extern void softirq_init(void);
 #define __raise_softirq_irqoff(nr) do { or_softirq_pending(1UL << (nr)); } while (0)
 extern void raise_softirq_irqoff(unsigned int nr);
 extern void raise_softirq(unsigned int nr);
-extern void wakeup_softirqd(void);
 
 /* This is the worklist that queues up per-cpu softirq work.
  *
