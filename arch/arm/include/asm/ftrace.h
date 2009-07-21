4bf1fa5a34aa2dd0d2cc58f0fc213a2e22d007a4
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2009-07-21 09:56:27 +0100

    [ARM] 5613/1: implement CALLER_ADDRESSx
    From: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    
    As __builtin_return_address(n) doesn't work for ARM with n > 0, the
    kernel needs its own implementation.
    
    This fixes many warnings saying:
    
    	warning: unsupported argument to '__builtin_return_address'
    
    The new methods and walk_stackframe must not be instrumented because
    CALLER_ADDRESSx is used in the various tracers and tracing the tracer is
    a bad idea.
    
    What's currently missing is an implementation using unwind tables.  This
    is not fatal though, it's just that the tracers don't get enough
    information to be really useful.
    
    Note that if both ARM_UNWIND and FRAME_POINTER are enabled,
    walk_stackframe uses unwind information.  So in this case the same
    implementation is used as when FRAME_POINTER is disabled.
    
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/ftrace.h
+++ arch/arm/include/asm/ftrace.h
@@ -11,4 +11,38 @@ extern void mcount(void);
 
 #endif
 
+#ifndef __ASSEMBLY__
+
+#if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND)
+/*
+ * return_address uses walk_stackframe to do it's work.  If both
+ * CONFIG_FRAME_POINTER=y and CONFIG_ARM_UNWIND=y walk_stackframe uses unwind
+ * information.  For this to work in the function tracer many functions would
+ * have to be marked with __notrace.  So for now just depend on
+ * !CONFIG_ARM_UNWIND.
+ */
+
+void *return_address(unsigned int);
+
+#else
+
+extern inline void *return_address(unsigned int level)
+{
+	return NULL;
+}
+
+#endif
+
+#define HAVE_ARCH_CALLER_ADDR
+
+#define CALLER_ADDR0 ((unsigned long)__builtin_return_address(0))
+#define CALLER_ADDR1 ((unsigned long)return_address(1))
+#define CALLER_ADDR2 ((unsigned long)return_address(2))
+#define CALLER_ADDR3 ((unsigned long)return_address(3))
+#define CALLER_ADDR4 ((unsigned long)return_address(4))
+#define CALLER_ADDR5 ((unsigned long)return_address(5))
+#define CALLER_ADDR6 ((unsigned long)return_address(6))
+
+#endif /* ifndef __ASSEMBLY__ */
+
 #endif /* _ASM_ARM_FTRACE */
