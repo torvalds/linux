78ed73e84d132e2d556eda90e5b76620f4390ff2
Janusz Krzysztofik <jkrzyszt@tis.icnet.pl>
2009-07-22 05:22:37 +0200

    TTY: Add definition of a new line discipline required by Amstrad E3 (Delta) ASoC driver
    This patch adds new line discipline name an number to include/linux/tty.h. The
    line discipline will be used by the Amstrad E3 (Delta) sound driver that will
    come next in this series of patches.
    
    Created against linux-2.6.31-rc3.
    Applies to linux-omap-2.6 commit 7c5cb7862d32cb344be7831d466535d5255e35ac as
    well.
    
    Signed-off-by: Janusz Krzysztofik <jkrzyszt@tis.icnet.pl>
    Acked-by: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- include/linux/tty.h
+++ include/linux/tty.h
@@ -23,7 +23,7 @@
  */
 #define NR_UNIX98_PTY_DEFAULT	4096      /* Default maximum for Unix98 ptys */
 #define NR_UNIX98_PTY_MAX	(1 << MINORBITS) /* Absolute limit */
-#define NR_LDISCS		19
+#define NR_LDISCS		20
 
 /* line disciplines */
 #define N_TTY		0
@@ -46,6 +46,8 @@
 #define N_GIGASET_M101	16	/* Siemens Gigaset M101 serial DECT adapter */
 #define N_SLCAN		17	/* Serial / USB serial CAN Adaptors */
 #define N_PPS		18	/* Pulse per Second */
+
+#define N_AMSDELTA	19	/* codec control over modem, board specific */
 
 /*
  * This character is the same as _POSIX_VDISABLE: it cannot be used as
