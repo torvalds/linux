69885683d22d8c05910fd808c01fdce1322739b4
Mikulas Patocka <mpatocka@redhat.com>
2009-07-23 20:30:37 +0100

    dm raid1: wake kmirrord when requeueing delayed bios after remote recovery
    The recent commit 7513c2a761d69d2a93f17146b3563527d3618ba0 (dm raid1:
    add is_remote_recovering hook for clusters) changed do_writes() to
    update the ms->writes list but forgot to wake up kmirrord to process it.
    
    The rule is that when anything is being added on ms->reads, ms->writes
    or ms->failures and the list was empty before we must call
    wakeup_mirrord (for immediate processing) or delayed_wake (for delayed
    processing).  Otherwise the bios could sit on the list indefinitely.
    
    Signed-off-by: Mikulas Patocka <mpatocka@redhat.com>
    CC: stable@kernel.org
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>

--- drivers/md/dm-raid1.c
+++ drivers/md/dm-raid1.c
@@ -638,6 +638,7 @@ static void do_writes(struct mirror_set *ms, struct bio_list *writes)
 		spin_lock_irq(&ms->lock);
 		bio_list_merge(&ms->writes, &requeue);
 		spin_unlock_irq(&ms->lock);
+		delayed_wake(ms);
 	}
 
 	/*
