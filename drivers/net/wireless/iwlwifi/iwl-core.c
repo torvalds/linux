6686d17e161dcd0dc6801bcde9e397020bf9edf7
Reinette Chatre <reinette.chatre@intel.com>
2009-07-24 11:13:13 -0700

    iwlagn: fix sparse warning when compiling without debug
    C [M]  drivers/net/wireless/iwlwifi/iwl-core.o
    drivers/net/wireless/iwlwifi/iwl-core.c:1341: warning:
    ‘iwl_dump_nic_error_log’ defined but not used
    
    Reported-by: Luis R. Rodriguez <lrodriguez@atheros.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-core.c
+++ drivers/net/wireless/iwlwifi/iwl-core.c
@@ -1291,7 +1291,6 @@ static void iwl_print_rx_config_cmd(struct iwl_priv *priv)
 	IWL_DEBUG_RADIO(priv, "u8[6] bssid_addr: %pM\n", rxon->bssid_addr);
 	IWL_DEBUG_RADIO(priv, "u16 assoc_id: 0x%x\n", le16_to_cpu(rxon->assoc_id));
 }
-#endif
 
 static const char *desc_lookup_text[] = {
 	"OK",
@@ -1496,6 +1495,7 @@ void iwl_dump_nic_event_log(struct iwl_priv *priv)
 	iwl_print_event_log(priv, 0, next_entry, mode);
 
 }
+#endif
 /**
  * iwl_irq_handle_error - called for HW or SW error interrupt from card
  */
