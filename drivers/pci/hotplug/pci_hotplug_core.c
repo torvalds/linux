6ed6a8dc83dc37d30c8a7ff2eae2ed054e34c91b
Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
2009-07-29 14:39:07 +0900

    PCI hotplug: fix typo in pcie link speed info
    Fix typo in PCI-E link speed.
    
    Reviewed-by: Matthew Wilcox <matthew@wil.cx>
    Signed-off-by: Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/hotplug/pci_hotplug_core.c
+++ drivers/pci/hotplug/pci_hotplug_core.c
@@ -86,7 +86,7 @@ static char *pci_bus_speed_strings[] = {
 	"66 MHz PCIX 533",	/* 0x11 */
 	"100 MHz PCIX 533",	/* 0x12 */
 	"133 MHz PCIX 533",	/* 0x13 */
-	"25 GBps PCI-E",	/* 0x14 */
+	"2.5 GT/s PCI-E",	/* 0x14 */
 };
 
 #ifdef CONFIG_HOTPLUG_PCI_CPCI
