005fb4f0330afb2916ddc3f3c4c70e4555d64758
Dean Nelson <dnelson@redhat.com>
2009-07-31 09:13:02 +0000

    atl1c: return PCI_ERS_RESULT_DISCONNECT on permanent failure
    PCI drivers that implement the struct pci_error_handlers' error_detected
    callback should return PCI_ERS_RESULT_DISCONNECT if the state passed in is
    pci_channel_io_perm_failure. This patch fixes the issue for atl1c.
    
    Signed-off-by: Dean Nelson <dnelson@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/atl1c/atl1c_main.c
+++ drivers/net/atl1c/atl1c_main.c
@@ -2678,6 +2678,9 @@ static pci_ers_result_t atl1c_io_error_detected(struct pci_dev *pdev,
 
 	netif_device_detach(netdev);
 
+	if (state == pci_channel_io_perm_failure)
+		return PCI_ERS_RESULT_DISCONNECT;
+
 	if (netif_running(netdev))
 		atl1c_down(adapter);
 
