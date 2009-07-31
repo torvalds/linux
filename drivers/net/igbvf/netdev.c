c06c430dedf71a0e9180d673191b9c9368aeaa65
Dean Nelson <dnelson@redhat.com>
2009-07-31 09:13:33 +0000

    igbvf: return PCI_ERS_RESULT_DISCONNECT on permanent failure
    PCI drivers that implement the struct pci_error_handlers' error_detected
    callback should return PCI_ERS_RESULT_DISCONNECT if the state passed in is
    pci_channel_io_perm_failure. This patch fixes the issue for igbvf.
    
    Signed-off-by: Dean Nelson <dnelson@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/igbvf/netdev.c
+++ drivers/net/igbvf/netdev.c
@@ -2511,6 +2511,9 @@ static pci_ers_result_t igbvf_io_error_detected(struct pci_dev *pdev,
 
 	netif_device_detach(netdev);
 
+	if (state == pci_channel_io_perm_failure)
+		return PCI_ERS_RESULT_DISCONNECT;
+
 	if (netif_running(netdev))
 		igbvf_down(adapter);
 	pci_disable_device(pdev);
