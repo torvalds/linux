738f0f4301587ad09b58651390b122205086b484
Gábor Stefanik <netrolller.3d@gmail.com>
2009-08-03 01:28:12 +0200

    b43: implement baseband init for LP-PHY <= rev1
    Implement baseband init for rev.0 and rev.1 LP PHYs. Convert boardflags_hi values to defines.
    Implement b43_phy_copy for easier copying between registers, as needed by LP-PHY init.
    
    Signed-off-by: Gábor Stefanik<netrolller.3d@gmail.com>
    Cc: Michael Buesch<mb@bu3sch.de>
    Cc: Larry Finger<larry.finger@lwfinger.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/b43/phy_common.c
+++ drivers/net/wireless/b43/phy_common.c
@@ -240,6 +240,13 @@ void b43_phy_write(struct b43_wldev *dev, u16 reg, u16 value)
 	dev->phy.ops->phy_write(dev, reg, value);
 }
 
+void b43_phy_copy(struct b43_wldev *dev, u16 destreg, u16 srcreg)
+{
+	assert_mac_suspended(dev);
+	dev->phy.ops->phy_write(dev, destreg,
+		dev->phy.ops->phy_read(dev, srcreg));
+}
+
 void b43_phy_mask(struct b43_wldev *dev, u16 offset, u16 mask)
 {
 	b43_phy_write(dev, offset,
