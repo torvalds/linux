200e861c511ded56783a3032f9d41fc6f1474e7a
James A Webb <jamesawebb@gmail.com>
2009-08-04 02:38:05 -0300

    V4L/DVB (12584): Support for Kaiser Baas ExpressCard Dual HD Tuner
    The card is reported as YUAN High-Tech Development Co., Ltd STK7700D
    
    [mchehab@redhat.com: fix merge conflicts and properly name USB PID]
    Cc: Patrick Boettcher <patrick.boettcher@desy.de>
    Signed-off-by: James A Webb <jamesawebb@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/dvb-usb/dib0700_devices.c
+++ drivers/media/dvb/dvb-usb/dib0700_devices.c
@@ -1498,6 +1498,7 @@ struct usb_device_id dib0700_usb_id_table[] = {
 	{ USB_DEVICE(USB_VID_TERRATEC,	USB_PID_TERRATEC_T3) },
 	{ USB_DEVICE(USB_VID_TERRATEC,	USB_PID_TERRATEC_T5) },
 	{ USB_DEVICE(USB_VID_YUAN,      USB_PID_YUAN_STK7700D) },
+	{ USB_DEVICE(USB_VID_YUAN,	USB_PID_YUAN_STK7700D_2) },
 	{ 0 }		/* Terminating entry */
 };
 MODULE_DEVICE_TABLE(usb, dib0700_usb_id_table);
@@ -1625,7 +1626,7 @@ struct dvb_usb_device_properties dib0700_devices[] = {
 			}
 		},
 
-		.num_device_descs = 4,
+		.num_device_descs = 5,
 		.devices = {
 			{   "Pinnacle PCTV 2000e",
 				{ &dib0700_usb_id_table[11], NULL },
@@ -1641,6 +1642,10 @@ struct dvb_usb_device_properties dib0700_devices[] = {
 			},
 			{   "DiBcom STK7700D reference design",
 				{ &dib0700_usb_id_table[14], NULL },
+				{ NULL },
+			},
+			{   "YUAN High-Tech DiBcom STK7700D",
+				{ &dib0700_usb_id_table[55], NULL },
 				{ NULL },
 			},
 
