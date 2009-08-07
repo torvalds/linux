7ae5945f0cacdfa30ed3257892dbea75bc959a28
Chaithrika U S <chaithrika@ti.com>
2009-08-07 10:07:51 -0400

    ASoC: DaVinci: Support Audio on DA830 EVM
    Add support for audio on DA830 EVM- here McASP1 is interfaced to
    TLV320AIC3106 codec.
    
    Signed-off-by: Chaithrika U S <chaithrika@ti.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/davinci/davinci-evm.c
+++ sound/soc/davinci/davinci-evm.c
@@ -54,6 +54,9 @@ static int evm_hw_params(struct snd_pcm_substream *substream,
 	else if (machine_is_davinci_evm())
 		sysclk = 12288000;
 
+	else if (machine_is_davinci_da830_evm())
+		sysclk = 24576000;
+
 	else
 		return -EINVAL;
 
@@ -162,6 +165,14 @@ static struct snd_soc_dai_link dm6467_evm_dai[] = {
 		.ops = &evm_ops,
 	},
 };
+static struct snd_soc_dai_link da830_evm_dai = {
+	.name = "TLV320AIC3X",
+	.stream_name = "AIC3X",
+	.cpu_dai = &davinci_mcasp_dai[DAVINCI_MCASP_I2S_DAI],
+	.codec_dai = &aic3x_dai,
+	.init = evm_aic3x_init,
+	.ops = &evm_ops,
+};
 
 /* davinci-evm audio machine driver */
 static struct snd_soc_card snd_soc_card_evm = {
@@ -179,6 +190,13 @@ static struct snd_soc_card dm6467_snd_soc_card_evm = {
 	.num_links = ARRAY_SIZE(dm6467_evm_dai),
 };
 
+static struct snd_soc_card da830_snd_soc_card = {
+	.name = "DA830 EVM",
+	.dai_link = &da830_evm_dai,
+	.platform = &davinci_soc_platform,
+	.num_links = 1,
+};
+
 /* evm audio private data */
 static struct aic3x_setup_data evm_aic3x_setup = {
 	.i2c_bus = 1,
@@ -189,6 +207,11 @@ static struct aic3x_setup_data evm_aic3x_setup = {
 static struct aic3x_setup_data dm6467_evm_aic3x_setup = {
        .i2c_bus = 1,
        .i2c_address = 0x18,
+};
+
+static struct aic3x_setup_data da830_evm_aic3x_setup = {
+	.i2c_bus = 1,
+	.i2c_address = 0x18,
 };
 
 /* evm audio subsystem */
@@ -203,6 +226,13 @@ static struct snd_soc_device dm6467_evm_snd_devdata = {
 	.card = &dm6467_snd_soc_card_evm,
 	.codec_dev = &soc_codec_dev_aic3x,
 	.codec_data = &dm6467_evm_aic3x_setup,
+};
+
+/* evm audio subsystem */
+static struct snd_soc_device da830_evm_snd_devdata = {
+	.card = &da830_snd_soc_card,
+	.codec_dev = &soc_codec_dev_aic3x,
+	.codec_data = &da830_evm_aic3x_setup,
 };
 
 static struct platform_device *evm_snd_device;
@@ -222,6 +252,9 @@ static int __init evm_init(void)
 	} else if (machine_is_davinci_dm6467_evm()) {
 		evm_snd_dev_data = &dm6467_evm_snd_devdata;
 		index = 0;
+	} else if (machine_is_davinci_da830_evm()) {
+		evm_snd_dev_data = &da830_evm_snd_devdata;
+		index = 1;
 	} else
 		return -EINVAL;
 
