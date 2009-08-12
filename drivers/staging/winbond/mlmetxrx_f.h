ef3bf091f33ea0358323da5a163e98e9c78bed32
Pekka Enberg <penberg@cs.helsinki.fi>
2009-08-12 11:03:35 +0300

    Staging: w35und: Cleanup the mlmetxrx_f.h header file
    This patch removes the unused MLMEProcThread and MLMEReturnPacket
    declarations and indents the header file.
    
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/winbond/mlmetxrx_f.h
+++ drivers/staging/winbond/mlmetxrx_f.h
@@ -10,28 +10,16 @@
 
 #include "core.h"
 
-void
-MLMEProcThread(
-     struct wbsoft_priv *    adapter
-	);
-
-void MLME_GetNextPacket(  struct wbsoft_priv * adapter,  PDESCRIPTOR pDes );
-u8 MLMESendFrame( struct wbsoft_priv * adapter,
-					u8	*pMMPDU,
-					u16	len,
-					 u8	DataType);
+void MLME_GetNextPacket(struct wbsoft_priv *adapter, PDESCRIPTOR pDes);
+u8 MLMESendFrame(struct wbsoft_priv *adapter,
+		 u8 * pMMPDU, u16 len, u8 DataType);
 
 void
-MLME_SendComplete(  struct wbsoft_priv * adapter,  u8 PacketID,  unsigned char SendOK );
+MLME_SendComplete(struct wbsoft_priv *adapter, u8 PacketID,
+		  unsigned char SendOK);
 
-void
-MLMEReturnPacket(
-     struct wbsoft_priv *    adapter,
-     u8 *          pRxBufer
-   );
 #ifdef _IBSS_BEACON_SEQ_STICK_
-s8 SendBCNullData(struct wbsoft_priv * adapter, u16 wIdx);
+s8 SendBCNullData(struct wbsoft_priv *adapter, u16 wIdx);
 #endif
 
 #endif
-
