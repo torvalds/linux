63b5ac286d5d7f668da537cc53a552578f7674a2
Herbert Xu <herbert@gondor.apana.org.au>
2009-08-14 22:55:35 +1000

    crypto: blkcipher - Do not use eseqiv on stream ciphers
    Recently we switched to using eseqiv on SMP machines in preference
    over chainiv.  However, eseqiv does not support stream ciphers so
    they should still default to chainiv.
    
    This patch applies the same check as done by eseqiv to weed out
    the stream ciphers.  In particular, all algorithms where the IV
    size is not equal to the block size will now default to chainiv.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/ablkcipher.c
+++ crypto/ablkcipher.c
@@ -183,6 +183,12 @@ EXPORT_SYMBOL_GPL(crypto_givcipher_type);
 
 const char *crypto_default_geniv(const struct crypto_alg *alg)
 {
+	if (((alg->cra_flags & CRYPTO_ALG_TYPE_MASK) ==
+	     CRYPTO_ALG_TYPE_BLKCIPHER ? alg->cra_blkcipher.ivsize :
+					 alg->cra_ablkcipher.ivsize) !=
+	    alg->cra_blocksize)
+		return "chainiv";
+
 	return alg->cra_flags & CRYPTO_ALG_ASYNC ?
 	       "eseqiv" : skcipher_default_geniv;
 }
