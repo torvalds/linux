9b115b7555de4c16beee8fb7d8b4a084ac52bde1
Miguel Boton <mboton@gmail.com>
2009-08-17 19:09:51 +0000

    Missing logo declaration.

--- include/linux/linux_logo.h
+++ include/linux/linux_logo.h
@@ -49,6 +49,7 @@ extern const struct linux_logo logo_superh_mono;
 extern const struct linux_logo logo_superh_vga16;
 extern const struct linux_logo logo_superh_clut224;
 extern const struct linux_logo logo_m32r_clut224;
+extern const struct linux_logo logo_spe_clut224;
 
 extern const struct linux_logo *fb_find_logo(int depth);
 #ifdef CONFIG_FB_LOGO_EXTRA
