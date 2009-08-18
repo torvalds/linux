1f18345bdfd489fde1085bc85839d5d3645cf511
Ingo Molnar <mingo@elte.hu>
2009-08-18 10:59:47 +0200

    perf tools: Remove obsolete defines
    The _XOPEN_SOURCE* defines are not really needed on Linux and
    it's not like we'll port this to AIX ;-)
    
    The define also broke the build with gcc 4.4.1:
    
     CC util/trace-event-parse.o
     In file included from util/trace-event-parse.c:32:
     util/util.h:43:1: error: "_XOPEN_SOURCE" redefined
    
    So remove them.
    
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    LKML-Reference: <new-submission>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- tools/perf/util/util.h
+++ tools/perf/util/util.h
@@ -39,10 +39,6 @@
 /* Approximation of the length of the decimal representation of this type. */
 #define decimal_length(x)	((int)(sizeof(x) * 2.56 + 0.5) + 1)
 
-#if !defined(__APPLE__) && !defined(__FreeBSD__)  && !defined(__USLC__) && !defined(_M_UNIX)
-#define _XOPEN_SOURCE 600 /* glibc2 and AIX 5.3L need 500, OpenBSD needs 600 for S_ISLNK() */
-#define _XOPEN_SOURCE_EXTENDED 1 /* AIX 5.3L needs this */
-#endif
 #define _ALL_SOURCE 1
 #define _GNU_SOURCE 1
 #define _BSD_SOURCE 1
