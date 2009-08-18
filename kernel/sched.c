cde7e5ca4e329a157108769d1f752d191cbb71c6
Hiroshi Shimamoto <h-shimamoto@ct.jp.nec.com>
2009-08-18 13:01:01 +0900

    sched: Use for_each_class macro in move_one_task()
    Replace for loop with the macro for_each_class to cleanup.
    
    Signed-off-by: Hiroshi Shimamoto <h-shimamoto@ct.jp.nec.com>
    LKML-Reference: <4A8A277D.4090304@ct.jp.nec.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched.c
+++ kernel/sched.c
@@ -3461,9 +3461,10 @@ static int move_one_task(struct rq *this_rq, int this_cpu, struct rq *busiest,
 {
 	const struct sched_class *class;
 
-	for (class = sched_class_highest; class; class = class->next)
+	for_each_class(class) {
 		if (class->move_one_task(this_rq, this_cpu, busiest, sd, idle))
 			return 1;
+	}
 
 	return 0;
 }
