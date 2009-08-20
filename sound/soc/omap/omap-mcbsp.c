c721bbdad71d2928e8b5015e9b462fbeb35427c6
Eero Nurkkala <ext-eero.nurkkala@nokia.com>
2009-08-20 16:18:23 +0300

    ASoC: Add runtime check for RFIG and XFIG
    This is, no RFIG or XFIG (Not defined in 34xx), correct
    initiliazation of rccr and xccr.
    
    Signed-off-by: Eero Nurkkala <ext-eero.nurkkala@nokia.com>
    Acked-by: Jarkko Nikula <jhnikula@gmail.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/omap/omap-mcbsp.c
+++ sound/soc/omap/omap-mcbsp.c
@@ -321,8 +321,11 @@ static int omap_mcbsp_dai_set_dai_fmt(struct snd_soc_dai *cpu_dai,
 	/* Generic McBSP register settings */
 	regs->spcr2	|= XINTM(3) | FREE;
 	regs->spcr1	|= RINTM(3);
-	regs->rcr2	|= RFIG;
-	regs->xcr2	|= XFIG;
+	/* RFIG and XFIG are not defined in 34xx */
+	if (!cpu_is_omap34xx()) {
+		regs->rcr2	|= RFIG;
+		regs->xcr2	|= XFIG;
+	}
 	if (cpu_is_omap2430() || cpu_is_omap34xx()) {
 		regs->xccr = DXENDLY(1) | XDMAEN;
 		regs->rccr = RFULL_CYCLE | RDMAEN;
