9599d485cb0b5ea093414654d723f3ab03c95038
Eduardo Valentin <eduardo.valentin@nokia.com>
2009-08-20 16:18:21 +0300

    ASoC: OMAP: Enable DMA burst mode
    Improve DMA transfers by enabling Burst transaction.
    
    Signed-off-by: Eduardo Valentin <eduardo.valentin@nokia.com>
    Acked-by: Jarkko Nikula <jhnikula@gmail.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/omap/omap-pcm.c
+++ sound/soc/omap/omap-pcm.c
@@ -195,6 +195,9 @@ static int omap_pcm_prepare(struct snd_pcm_substream *substream)
 	else
 		omap_enable_dma_irq(prtd->dma_ch, OMAP_DMA_FRAME_IRQ);
 
+	omap_set_dma_src_burst_mode(prtd->dma_ch, OMAP_DMA_DATA_BURST_16);
+	omap_set_dma_dest_burst_mode(prtd->dma_ch, OMAP_DMA_DATA_BURST_16);
+
 	return 0;
 }
 
