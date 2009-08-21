337c1db645bce3f5a832129c4a803dc157ac7e9a
Haojian Zhuang <haojian.zhuang@marvell.com>
2009-08-21 16:10:41 +0800

    [ARM] pxa: update cpu_is_xsc3() to include Marvell CPUID
    CPU id is changed in Marvell chip. So update the code in cpu_is_xsc3().
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@marvell.com>
    Signed-off-by: Eric Miao <eric.y.miao@gmail.com>

--- arch/arm/include/asm/cputype.h
+++ arch/arm/include/asm/cputype.h
@@ -73,7 +73,10 @@ static inline unsigned int __attribute_const__ read_cpuid_cachetype(void)
 #else
 static inline int cpu_is_xsc3(void)
 {
-	if ((read_cpuid_id() & 0xffffe000) == 0x69056000)
+	unsigned int id;
+	id = read_cpuid_id() & 0xffffe000;
+	/* It covers both Intel ID and Marvell ID */
+	if ((id == 0x69056000) || (id == 0x56056000))
 		return 1;
 
 	return 0;
