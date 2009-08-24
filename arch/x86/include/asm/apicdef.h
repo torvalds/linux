8f3e1df48baf728bbb0f242c9dff9c9d7108218a
Cyrill Gorcunov <gorcunov@openvz.org>
2009-08-24 21:53:36 +0400

    x86, ioapic: Define IO_APIC_DEFAULT_PHYS_BASE constant
    We already have APIC_DEFAULT_PHYS_BASE so just to be
    consistent.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    LKML-Reference: <20090824175550.927946757@openvz.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/apicdef.h
+++ arch/x86/include/asm/apicdef.h
@@ -8,7 +8,8 @@
  * Ingo Molnar <mingo@redhat.com>, 1999, 2000
  */
 
-#define	APIC_DEFAULT_PHYS_BASE	0xfee00000
+#define IO_APIC_DEFAULT_PHYS_BASE	0xfec00000
+#define	APIC_DEFAULT_PHYS_BASE		0xfee00000
 
 #define	APIC_ID		0x20
 
