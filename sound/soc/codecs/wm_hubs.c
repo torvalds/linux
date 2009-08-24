c6ea2af76ab79b82a0bfed313e4c2b68b10c2bc1
Takashi Iwai <tiwai@suse.de>
2009-08-24 09:41:32 +0200

    ASoC: Remove unneeded inclusion of linux/regulator/consumer.h
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/soc/codecs/wm_hubs.c
+++ sound/soc/codecs/wm_hubs.c
@@ -18,7 +18,6 @@
 #include <linux/pm.h>
 #include <linux/i2c.h>
 #include <linux/platform_device.h>
-#include <linux/regulator/consumer.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
