0d788c7d285a91ef72a7b436dbe753d117856b3d
Klaus-Dieter Wacker <kdwacker@de.ibm.com>
2009-08-26 02:01:09 +0000

    qeth: display service_level info only when valid.
    qeth displayed an entry in /proc/service_level even when no valid
    MCL-string was available (the MCL info is blank). The change is to
    create an entry in /proc/service_level only when MCL-string is
    non-zero.
    
    Signed-off-by: Klaus-Dieter Wacker <kdwacker@de.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/s390/net/qeth_core_main.c
+++ drivers/s390/net/qeth_core_main.c
@@ -1160,8 +1160,9 @@ static void qeth_core_sl_print(struct seq_file *m, struct service_level *slr)
 {
 	struct qeth_card *card = container_of(slr, struct qeth_card,
 					qeth_service_level);
-	seq_printf(m, "qeth: %s firmware level %s\n", CARD_BUS_ID(card),
-			card->info.mcl_level);
+	if (card->info.mcl_level[0])
+		seq_printf(m, "qeth: %s firmware level %s\n",
+			CARD_BUS_ID(card), card->info.mcl_level);
 }
 
 static struct qeth_card *qeth_alloc_card(void)
