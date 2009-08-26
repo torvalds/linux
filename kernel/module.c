7d1d16e416e61aeef8655d542f8e4a4fc6e808e4
Rusty Russell <rusty@rustcorp.com.au>
2009-08-26 22:02:54 +0930

    module: fix BUG_ON() for powerpc (and other function descriptor archs)
    The rarely-used symbol_put_addr() needs to use dereference_function_descriptor
    on powerpc.
    
    Reported-by: Paul Mackerras <paulus@samba.org>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au.
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/module.c
+++ kernel/module.c
@@ -909,16 +909,18 @@ void __symbol_put(const char *symbol)
 }
 EXPORT_SYMBOL(__symbol_put);
 
+/* Note this assumes addr is a function, which it currently always is. */
 void symbol_put_addr(void *addr)
 {
 	struct module *modaddr;
+	unsigned long a = (unsigned long)dereference_function_descriptor(addr);
 
-	if (core_kernel_text((unsigned long)addr))
+	if (core_kernel_text(a))
 		return;
 
 	/* module_text_address is safe here: we're supposed to have reference
 	 * to module from symbol_get, so it can't go away. */
-	modaddr = __module_text_address((unsigned long)addr);
+	modaddr = __module_text_address(a);
 	BUG_ON(!modaddr);
 	module_put(modaddr);
 }
