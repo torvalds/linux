fc57e515a2c02599b00d252545521288dfc0158a
Matt Carlson <mcarlson@broadcom.com>
2009-08-28 14:03:44 +0000

    tg3: Update version to 3.101
    This patch updates the tg3 version to 3.101.
    
    Signed-off-by: Matt Carlson <mcarlson@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/tg3.c
+++ drivers/net/tg3.c
@@ -68,8 +68,8 @@
 
 #define DRV_MODULE_NAME		"tg3"
 #define PFX DRV_MODULE_NAME	": "
-#define DRV_MODULE_VERSION	"3.100"
-#define DRV_MODULE_RELDATE	"August 25, 2009"
+#define DRV_MODULE_VERSION	"3.101"
+#define DRV_MODULE_RELDATE	"August 28, 2009"
 
 #define TG3_DEF_MAC_MODE	0
 #define TG3_DEF_RX_MODE		0
