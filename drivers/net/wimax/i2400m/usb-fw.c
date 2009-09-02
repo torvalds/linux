59bdc4be0b819173a8f840fc11ccb82d6f2ca64b
Inaky Perez-Gonzalez <inaky@linux.intel.com>
2009-09-02 15:41:23 -0700

    wimax/i2400m: workaround not-so-working %zd printf format
    The kernel's %zd modifier does not really work. Use %ld (has to cast
    ssize_t to long).
    
    Signed-off-by: Inaky Perez-Gonzalez <inaky@linux.intel.com>

--- drivers/net/wimax/i2400m/usb-fw.c
+++ drivers/net/wimax/i2400m/usb-fw.c
@@ -330,8 +330,8 @@ error_dev_gone:
 out:
 	if (do_autopm)
 		usb_autopm_put_interface(i2400mu->usb_iface);
-	d_fnend(8, dev, "(i2400m %p ack %p size %zu) = %zd\n",
-		i2400m, ack, ack_size, result);
+	d_fnend(8, dev, "(i2400m %p ack %p size %zu) = %ld\n",
+		i2400m, ack, ack_size, (long) result);
 	return result;
 
 error_exceeded:
