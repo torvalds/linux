988abe4075e5748d9f7c79d9dfffa0cf5291611b
Alex Elder <aelder@sgi.com>
2009-09-02 17:02:24 -0500

    xfs: xfs_showargs() reports group *and* project quotas enabled
    If you enable group or project quotas on an XFS file system, then the
    mount table presented through /proc/self/mounts erroneously shows
    that both options are in effect for the file system.  The root of
    the problem is some bad logic in the xfs_showargs() function, which
    is used to format the file system type-specific options in effect
    for a file system.
    
    The problem originated in this GIT commit:
        Move platform specific mount option parse out of core XFS code
        Date: 11/22/07
        Author: Dave Chinner
        SHA1 ID: a67d7c5f5d25d0b13a4dfb182697135b014fa478
    
    For XFS quotas, project and group quota management are mutually
    exclusive--only one can be in effect at a time.  There are two
    parts to managing quotas:  aggregating usage information; and
    enforcing limits.  It is possible to have a quota in effect
    (aggregating usage) but not enforced.
    
    These features are recorded on an XFS mount point using these flags:
        XFS_PQUOTA_ACCT - Project quotas are aggregated
        XFS_GQUOTA_ACCT - Group quotas are aggregated
        XFS_OQUOTA_ENFD - Project/group quotas are enforced
    
    The code in error is in fs/xfs/linux-2.6/xfs_super.c:
    
            if (mp->m_qflags & (XFS_PQUOTA_ACCT|XFS_OQUOTA_ENFD))
                    seq_puts(m, "," MNTOPT_PRJQUOTA);
            else if (mp->m_qflags & XFS_PQUOTA_ACCT)
                    seq_puts(m, "," MNTOPT_PQUOTANOENF);
    
            if (mp->m_qflags & (XFS_GQUOTA_ACCT|XFS_OQUOTA_ENFD))
                    seq_puts(m, "," MNTOPT_GRPQUOTA);
            else if (mp->m_qflags & XFS_GQUOTA_ACCT)
                    seq_puts(m, "," MNTOPT_GQUOTANOENF);
    
    The problem is that XFS_OQUOTA_ENFD will be set in mp->m_qflags
    if either group or project quotas are enforced, and as a result
    both MNTOPT_PRJQUOTA and MNTOPT_GRPQUOTA will be shown as mount
    options.
    
    Signed-off-by: Alex Elder <aelder@sgi.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Felix Blyakher <felixb@sgi.com>

--- fs/xfs/linux-2.6/xfs_super.c
+++ fs/xfs/linux-2.6/xfs_super.c
@@ -579,15 +579,19 @@ xfs_showargs(
 	else if (mp->m_qflags & XFS_UQUOTA_ACCT)
 		seq_puts(m, "," MNTOPT_UQUOTANOENF);
 
-	if (mp->m_qflags & (XFS_PQUOTA_ACCT|XFS_OQUOTA_ENFD))
-		seq_puts(m, "," MNTOPT_PRJQUOTA);
-	else if (mp->m_qflags & XFS_PQUOTA_ACCT)
-		seq_puts(m, "," MNTOPT_PQUOTANOENF);
+	/* Either project or group quotas can be active, not both */
 
-	if (mp->m_qflags & (XFS_GQUOTA_ACCT|XFS_OQUOTA_ENFD))
-		seq_puts(m, "," MNTOPT_GRPQUOTA);
-	else if (mp->m_qflags & XFS_GQUOTA_ACCT)
-		seq_puts(m, "," MNTOPT_GQUOTANOENF);
+	if (mp->m_qflags & XFS_PQUOTA_ACCT) {
+		if (mp->m_qflags & XFS_OQUOTA_ENFD)
+			seq_puts(m, "," MNTOPT_PRJQUOTA);
+		else
+			seq_puts(m, "," MNTOPT_PQUOTANOENF);
+	} else if (mp->m_qflags & XFS_GQUOTA_ACCT) {
+		if (mp->m_qflags & XFS_OQUOTA_ENFD)
+			seq_puts(m, "," MNTOPT_GRPQUOTA);
+		else
+			seq_puts(m, "," MNTOPT_GQUOTANOENF);
+	}
 
 	if (!(mp->m_qflags & XFS_ALL_QUOTA_ACCT))
 		seq_puts(m, "," MNTOPT_NOQUOTA);
