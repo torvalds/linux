0961284c7208dfd907456fa73823f5967331b957
Axel K <rain_maker@root-forum.org>
2009-09-03 21:53:37 +0200

    Staging: rt3090: rename device from raX to wlanX
    This patch sets "wlan" as the default suffix for naming the device, a
    change which has also been previously applied to rt2860/rt2870 in
    staging.
    
    Signed-off-by: Axel Koellhofer <rain_maker@root-forum.org>
    Acked-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rt3090/rtmp_def.h
+++ drivers/staging/rt3090/rtmp_def.h
@@ -1414,8 +1414,8 @@
 #define INT_APCLI			0x0400
 #define INT_MESH			0x0500
 
-#define INF_MAIN_DEV_NAME		"ra"
-#define INF_MBSSID_DEV_NAME		"ra"
+#define INF_MAIN_DEV_NAME		"wlan"
+#define INF_MBSSID_DEV_NAME		"wlan"
 #define INF_WDS_DEV_NAME		"wds"
 #define INF_APCLI_DEV_NAME		"apcli"
 #define INF_MESH_DEV_NAME		"mesh"
