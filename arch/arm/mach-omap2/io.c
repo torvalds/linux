c0407a96d04794be586eab4a412320079446cf93
Paul Walmsley <paul@pwsan.com>
2009-09-03 20:14:01 +0300

    OMAP2/3 PM: create the OMAP PM interface and add a default OMAP PM no-op layer
    The interface provides device drivers, CPUFreq, and DSPBridge with a
    means of controlling OMAP power management parameters that are not yet
    supported by the Linux PM PMQoS interface.  Copious documentation is
    in the patch in Documentation/arm/OMAP/omap_pm and the interface
    header file, arch/arm/plat-omap/include/mach/omap-pm.h.
    
    Thanks to Rajendra Nayak <rnayak@ti.com> for adding CORE (VDD2) OPP
    support and moving the OPP table initialization earlier in the event
    that the clock code needs them.  Thanks to Tero Kristo
    <tero.kristo@nokia.com> for fixing the parameter check in
    omap_pm_set_min_bus_tput().  Jouni signed off on Tero's patch.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>
    Signed-off-by: Tero Kristo <tero.kristo@nokia.com>
    Signed-off-by: Jouni Högander <jouni.hogander@nokia.com>
    Cc: Tony Lindgren <tony@atomide.com>
    Cc: Igor Stoppa <igor.stoppa@nokia.com>
    Cc: Richard Woodruff <r-woodruff2@ti.com>
    Cc: Anand Sawant <sawant@ti.com>
    Cc: Sakari Poussa <sakari.poussa@nokia.com>
    Cc: Veeramanikandan Raju <veera@ti.com>
    Cc: Karthik Dasu <karthik-dp@ti.com>

--- arch/arm/mach-omap2/io.c
+++ arch/arm/mach-omap2/io.c
@@ -36,6 +36,7 @@
 #ifndef CONFIG_ARCH_OMAP4	/* FIXME: Remove this once clkdev is ready */
 #include "clock.h"
 
+#include <mach/omap-pm.h>
 #include <mach/powerdomain.h>
 
 #include "powerdomains.h"
@@ -281,9 +282,12 @@ void __init omap2_init_common_hw(struct omap_sdrc_params *sdrc_cs0,
 {
 	omap2_mux_init();
 #ifndef CONFIG_ARCH_OMAP4 /* FIXME: Remove this once the clkdev is ready */
+	/* The OPP tables have to be registered before a clk init */
+	omap_pm_if_early_init(mpu_opps, dsp_opps, l3_opps);
 	pwrdm_init(powerdomains_omap);
 	clkdm_init(clockdomains_omap, clkdm_pwrdm_autodeps);
 	omap2_clk_init();
+	omap_pm_if_init();
 	omap2_sdrc_init(sdrc_cs0, sdrc_cs1);
 	_omap2_init_reprogram_sdrc();
 #endif
