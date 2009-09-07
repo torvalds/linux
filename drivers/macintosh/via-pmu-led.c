db3f520738a8c5bf593e13d4ac71f8da9ffcb964
Olaf Hering <olh@suse.de>
2009-09-07 14:37:27 +0100

    leds: Fix LED names
    This is needed to get kde-powersave to work properly on some g4
    powerbooks.
    
    From: Olaf Hering <olh@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    Signed-off-by: Richard Purdie <rpurdie@linux.intel.com>

--- drivers/macintosh/via-pmu-led.c
+++ drivers/macintosh/via-pmu-led.c
@@ -72,7 +72,7 @@ static void pmu_led_set(struct led_classdev *led_cdev,
 }
 
 static struct led_classdev pmu_led = {
-	.name = "pmu-front-led",
+	.name = "pmu-led::front",
 #ifdef CONFIG_ADB_PMU_LED_IDE
 	.default_trigger = "ide-disk",
 #endif
