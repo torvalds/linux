48118b53c6ba0c48440ba552cd7b82c9a9cf530d
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2009-09-08 22:35:41 +0200

    Staging: rt3090: enable NATIVE_WPA_SUPPLICANT_SUPPORT option
    Similarly as it has been done in other in-kernel Ralink drivers
    and in openSUSE's rt3090sta package.
    
    Cc: Axel Koellhofer <rain_maker@root-forum.org>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rt3090/config.mk
+++ drivers/staging/rt3090/config.mk
@@ -20,7 +20,7 @@ HAS_APCLI=n
 HAS_WPA_SUPPLICANT=y
 
 # Support Native WpaSupplicant for Network Maganger
-HAS_NATIVE_WPA_SUPPLICANT_SUPPORT=n
+HAS_NATIVE_WPA_SUPPLICANT_SUPPORT=y
 
 #Support Net interface block while Tx-Sw queue full
 HAS_BLOCK_NET_IF=n
