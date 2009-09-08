4f7454a9970fa0f3e9f1a68201520e3df1bb5224
Takashi Iwai <tiwai@suse.de>
2009-09-08 14:29:58 +0200

    ALSA: Add const prefix to proc helper functions
    Add appropriate const prefix to char * arguments in proc helper functions.
    Also fixed the caller side to be proper const pointers.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/info.c
+++ sound/core/info.c
@@ -108,7 +108,7 @@ static int resize_info_buffer(struct snd_info_buffer *buffer,
  *
  * Returns the size of output string.
  */
-int snd_iprintf(struct snd_info_buffer *buffer, char *fmt,...)
+int snd_iprintf(struct snd_info_buffer *buffer, const char *fmt, ...)
 {
 	va_list args;
 	int len, res;
@@ -727,7 +727,7 @@ EXPORT_SYMBOL(snd_info_get_line);
  * Returns the updated pointer of the original string so that
  * it can be used for the next call.
  */
-char *snd_info_get_str(char *dest, char *src, int len)
+const char *snd_info_get_str(char *dest, const char *src, int len)
 {
 	int c;
 
