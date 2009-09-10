5686f9c3d67d5a20108fa26105c98b042df13123
David S. Miller <davem@davemloft.net>
2009-09-10 05:59:24 -0700

    sparc64: Implement a real set_perf_counter_pending().
    When the perf counter subsystem needs to reschedule work out
    from an NMI, it invokes set_perf_counter_pending().
    
    This triggers a non-NMI irq which should invoke
    perf_counter_do_pending().
    
    Currently this won't trigger because sparc64 won't trigger
    the perf counter subsystem from NMIs, but when the HW counter
    support is added it will.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/asm/perf_counter.h
+++ arch/sparc/include/asm/perf_counter.h
@@ -1,7 +1,7 @@
 #ifndef __ASM_SPARC_PERF_COUNTER_H
 #define __ASM_SPARC_PERF_COUNTER_H
 
-static inline void set_perf_counter_pending(void) { }
+extern void set_perf_counter_pending(void);
 
 #define	PERF_COUNTER_INDEX_OFFSET	0
 
