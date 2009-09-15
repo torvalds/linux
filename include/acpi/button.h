1a133e0c9dabda23e6693cabfdc1d5106dca5fc2
Jesse Barnes <jbarnes@virtuousgeek.org>
2009-09-15 16:57:24 -0700

    ACPI: make ACPI button funcs no-ops if not built in
    Yakui pointed out that we don't properly no-op the ACPI button routines
    if the button driver isn't built in.  This will cause problems if ACPI
    is disabled, so provide stub functions in that case.
    
    Reported-by: ykzhao <yakui.zhao@intel.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- include/acpi/button.h
+++ include/acpi/button.h
@@ -3,8 +3,23 @@
 
 #include <linux/notifier.h>
 
+#if defined(CONFIG_ACPI_BUTTON) || defined(CONFIG_ACPI_BUTTON_MODULE)
 extern int acpi_lid_notifier_register(struct notifier_block *nb);
 extern int acpi_lid_notifier_unregister(struct notifier_block *nb);
 extern int acpi_lid_open(void);
+#else
+static inline int acpi_lid_notifier_register(struct notifier_block *nb)
+{
+	return 0;
+}
+static inline int acpi_lid_notifier_unregister(struct notifier_block *nb)
+{
+	return 0;
+}
+static inline int acpi_lid_open(void)
+{
+	return 1;
+}
+#endif /* defined(CONFIG_ACPI_BUTTON) || defined(CONFIG_ACPI_BUTTON_MODULE) */
 
 #endif /* ACPI_BUTTON_H */
