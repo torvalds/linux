894572a363539dc2e8ddde83056bd22fadb30748
David Woodhouse <David.Woodhouse@intel.com>
2009-09-19 16:07:34 -0700

    mtd: sh_flctl: register sh_flctl using platform_driver_probe()
    As with orion_nand in commit f33dabbe79fdf7a8568c65faa1db7794c87ac4d3
    ("register orion_nand using platform_driver_probe()"), avoid .init.text
    problems by using platform_device_probe(). This isn't going to be
    hotplugged anyway.
    
    Reported-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/nand/sh_flctl.c
+++ drivers/mtd/nand/sh_flctl.c
@@ -857,7 +857,6 @@ static int __exit flctl_remove(struct platform_device *pdev)
 }
 
 static struct platform_driver flctl_driver = {
-	.probe		= flctl_probe,
 	.remove		= flctl_remove,
 	.driver = {
 		.name	= "sh_flctl",
@@ -867,7 +866,7 @@ static struct platform_driver flctl_driver = {
 
 static int __init flctl_nand_init(void)
 {
-	return platform_driver_register(&flctl_driver);
+	return platform_driver_probe(&flctl_driver, flctl_probe);
 }
 
 static void __exit flctl_nand_cleanup(void)
