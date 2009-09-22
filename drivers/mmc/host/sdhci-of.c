ad1e597d4199ffcdee04b9fb402e45c5be6a5052
Anton Vorontsov <avorontsov@ru.mvista.com>
2009-09-22 16:45:16 -0700

    sdhci-of: cleanup eSDHC's set_clock() a little bit
    - Get rid of incomprehensible "if { for { if } }" construction for the
      exponential divisor calculation. The first if statement isn't correct
      at all, since it should check for "host->max_clk / pre_div / 16 >
      clock". The error doesn't cause any bugs because the check in the for
      loop does the right thing, and so the outer check becomes useless;
    
    - For the linear divisor do the same: a single while statement is more
      readable than for + if construction;
    
    - Add dev_dbg() that prints desired and actual clock frequency.
    
    Signed-off-by: Anton Vorontsov <avorontsov@ru.mvista.com>
    Cc: Pierre Ossman <pierre@ossman.eu>
    Cc: Kumar Gala <galak@kernel.crashing.org>
    Cc: David Vrabel <david.vrabel@csr.com>
    Cc: Ben Dooks <ben@fluff.org>
    Cc: Sascha Hauer <s.hauer@pengutronix.de>
    Cc: <linux-mmc@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/mmc/host/sdhci-of.c
+++ drivers/mmc/host/sdhci-of.c
@@ -121,8 +121,8 @@ static void esdhc_writeb(struct sdhci_host *host, u8 val, int reg)
 
 static void esdhc_set_clock(struct sdhci_host *host, unsigned int clock)
 {
-	int div;
 	int pre_div = 2;
+	int div = 1;
 
 	clrbits32(host->ioaddr + ESDHC_SYSTEM_CONTROL, ESDHC_CLOCK_IPGEN |
 		  ESDHC_CLOCK_HCKEN | ESDHC_CLOCK_PEREN | ESDHC_CLOCK_MASK);
@@ -130,17 +130,14 @@ static void esdhc_set_clock(struct sdhci_host *host, unsigned int clock)
 	if (clock == 0)
 		goto out;
 
-	if (host->max_clk / 16 > clock) {
-		for (; pre_div < 256; pre_div *= 2) {
-			if (host->max_clk / pre_div < clock * 16)
-				break;
-		}
-	}
+	while (host->max_clk / pre_div / 16 > clock && pre_div < 256)
+		pre_div *= 2;
 
-	for (div = 1; div <= 16; div++) {
-		if (host->max_clk / (div * pre_div) <= clock)
-			break;
-	}
+	while (host->max_clk / pre_div / div > clock && div < 16)
+		div++;
+
+	dev_dbg(mmc_dev(host->mmc), "desired SD clock: %d, actual: %d\n",
+		clock, host->max_clk / pre_div / div);
 
 	pre_div >>= 1;
 	div--;
