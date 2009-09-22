8873c33483e62988ed886230aab71ef4c678f710
Daniel Mack <daniel@caiaq.de>
2009-09-21 17:04:43 -0700

    lis3: add free-fall/wakeup function via platform_data
    This offers a way for platforms to define flags and thresholds for the
    free-fall/wakeup functions of the lis302d chips.
    
    More registers needed to be seperated as they are specific to the
    
    Signed-off-by: Daniel Mack <daniel@caiaq.de>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Cc: Eric Piel <eric.piel@tremplin-utc.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/lis3lv02d.h
+++ include/linux/lis3lv02d.h
@@ -34,6 +34,15 @@ struct lis3lv02d_platform_data {
 #define LIS3_IRQ_OPEN_DRAIN	(1 << 6)
 #define LIS3_IRQ_ACTIVE_LOW	(1 << 7)
 	unsigned char irq_cfg;
+
+#define LIS3_WAKEUP_X_LO	(1 << 0)
+#define LIS3_WAKEUP_X_HI	(1 << 1)
+#define LIS3_WAKEUP_Y_LO	(1 << 2)
+#define LIS3_WAKEUP_Y_HI	(1 << 3)
+#define LIS3_WAKEUP_Z_LO	(1 << 4)
+#define LIS3_WAKEUP_Z_HI	(1 << 5)
+	unsigned char wakeup_flags;
+	unsigned char wakeup_thresh;
 };
 
 #endif /* __LIS3LV02D_H_ */
