7043078630bbc9eb908a56e08077c5abe7f2d311
David Howells <dhowells@redhat.com>
2009-09-23 10:40:24 +0100

    MN10300: Handle removal of struct uart_info
    Commit ebd2c8f6d2ec4012c267ecb95e72a57b8355a705 removed struct uart_info and
    commit bdc04e3174e18f475289fa8f4144f66686326b7e further moved delta_msr_wait.
    Fix up the MN10300 on-chip serial port drivers to comply with this.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/mn10300/kernel/asm-offsets.c
+++ arch/mn10300/kernel/asm-offsets.c
@@ -85,7 +85,7 @@ void foo(void)
 	OFFSET(__rx_buffer,		mn10300_serial_port, rx_buffer);
 	OFFSET(__rx_inp,		mn10300_serial_port, rx_inp);
 	OFFSET(__rx_outp,		mn10300_serial_port, rx_outp);
-	OFFSET(__tx_info_buffer,	mn10300_serial_port, uart.info);
+	OFFSET(__uart_state,		mn10300_serial_port, uart.state);
 	OFFSET(__tx_xchar,		mn10300_serial_port, tx_xchar);
 	OFFSET(__tx_break,		mn10300_serial_port, tx_break);
 	OFFSET(__intr_flags,		mn10300_serial_port, intr_flags);
