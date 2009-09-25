9bddbab36d736c181678e07515aaa63045bdcea7
Johannes Berg <johannes@sipsolutions.net>
2009-09-25 14:24:24 -0700

    iwlwifi: fix a typo
    We never have four chains, but let's fix the typo
    while we noticed it. You count 0, 1, 2, 3, not
    0, 1, 2, 4 :)
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-core.c
+++ drivers/net/wireless/iwlwifi/iwl-core.c
@@ -984,7 +984,7 @@ static u8 iwl_count_chain_bitmap(u32 chain_bitmap)
 	res = (chain_bitmap & BIT(0)) >> 0;
 	res += (chain_bitmap & BIT(1)) >> 1;
 	res += (chain_bitmap & BIT(2)) >> 2;
-	res += (chain_bitmap & BIT(4)) >> 4;
+	res += (chain_bitmap & BIT(3)) >> 3;
 	return res;
 }
 
