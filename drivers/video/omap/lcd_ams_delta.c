b89ea90d402c13c8dd4d5dfc8d4a0b6efc5492df
Peter Huewe <peterhuewe@gmx.de>
2009-09-29 03:18:06 +0200

    add __init/__exit macros to omap video drivers
    Trivial patch which adds the __init/__exit macros to the module_init/
    module_exit functions of the following drivers in media/omap:
    drivers/video/omap/lcd_ams_delta.c
    drivers/video/omap/lcd_mipid.c
    
    Signed-off-by: Peter Huewe <peterhuewe@gmx.de>
    Acked-by: Imre Deak <imre.deak@nokia.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/video/omap/lcd_ams_delta.c
+++ drivers/video/omap/lcd_ams_delta.c
@@ -123,12 +123,12 @@ struct platform_driver ams_delta_panel_driver = {
 	},
 };
 
-static int ams_delta_panel_drv_init(void)
+static int __init ams_delta_panel_drv_init(void)
 {
 	return platform_driver_register(&ams_delta_panel_driver);
 }
 
-static void ams_delta_panel_drv_cleanup(void)
+static void __exit ams_delta_panel_drv_cleanup(void)
 {
 	platform_driver_unregister(&ams_delta_panel_driver);
 }
