24836479a126e02be691e073c2b6cad7e7ab836a
Philipp Reisner <philipp.reisner@linbit.com>
2009-10-02 02:40:08 +0000

    dm/connector: Only process connector packages from privileged processes
    Signed-off-by: Philipp Reisner <philipp.reisner@linbit.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/md/dm-log-userspace-transfer.c
+++ drivers/md/dm-log-userspace-transfer.c
@@ -133,6 +133,9 @@ static void cn_ulog_callback(struct cn_msg *msg, struct netlink_skb_parms *nsp)
 {
 	struct dm_ulog_request *tfr = (struct dm_ulog_request *)(msg + 1);
 
+	if (!cap_raised(nsp->eff_cap, CAP_SYS_ADMIN))
+		return;
+
 	spin_lock(&receiving_list_lock);
 	if (msg->len == 0)
 		fill_pkg(msg, NULL);
