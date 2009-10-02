12cda817779ce5381a9a4ba8d464abe17c50a9e2
Tejun Heo <tj@kernel.org>
2009-10-02 13:28:56 +0900

    ia64: initialize cpu maps early
    All information necessary to initialize cpu possible and present maps
    are available once early_acpi_boot_init() is complete.  Reorganize
    setup_arch() and acpi init functions such that,
    
    * CPU information is printed after LAPIC entries are parsed in
      early_acpi_boot_init().
    
    * smp_build_cpu_map() is called by setup_arch() instead of acpi
      functions.
    
    * smp_build_cpu_map() is called once all CPU related information is
      available before memory is initialized.
    
    This is primarily to allow find_memory() to use cpu maps but is also a
    general cleanup.  Please note that with this change, the somewhat
    ad-hoc early_cpu_possible_map defined and used for NUMA configurations
    is probably unnecessary.  Something to clean up another day.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Acked-by: Tony Luck <tony.luck@intel.com>
    Cc: Fenghua Yu <fenghua.yu@intel.com>
    Cc: linux-ia64 <linux-ia64@vger.kernel.org>

--- arch/ia64/kernel/setup.c
+++ arch/ia64/kernel/setup.c
@@ -566,19 +566,18 @@ setup_arch (char **cmdline_p)
 	early_acpi_boot_init();
 # ifdef CONFIG_ACPI_NUMA
 	acpi_numa_init();
-#ifdef CONFIG_ACPI_HOTPLUG_CPU
+#  ifdef CONFIG_ACPI_HOTPLUG_CPU
 	prefill_possible_map();
-#endif
+#  endif
 	per_cpu_scan_finalize((cpus_weight(early_cpu_possible_map) == 0 ?
 		32 : cpus_weight(early_cpu_possible_map)),
 		additional_cpus > 0 ? additional_cpus : 0);
 # endif
-#else
-# ifdef CONFIG_SMP
-	smp_build_cpu_map();	/* happens, e.g., with the Ski simulator */
-# endif
 #endif /* CONFIG_APCI_BOOT */
 
+#ifdef CONFIG_SMP
+	smp_build_cpu_map();
+#endif
 	find_memory();
 
 	/* process SAL system table: */
