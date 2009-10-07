a82ac21efcefabb4879eb5b4e7a2c7a142026bdb
John W. Linville <linville@tuxdriver.com>
2009-10-07 10:49:33 -0400

    net/wireless/ethtool.h: drop unnecessary include of linux/ethtool.h
    Everything including this header includes net/cfg80211.h, which
    includes linux/netdevice.h, which includes linux/ethtool.h already.  Why
    slow-down the build, even a little bit?
    
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/wireless/ethtool.h
+++ net/wireless/ethtool.h
@@ -1,8 +1,6 @@
 #ifndef __CFG80211_ETHTOOL__
 #define __CFG80211_ETHTOOL__
 
-#include <linux/ethtool.h>
-
 extern const struct ethtool_ops cfg80211_ethtool_ops;
 
 #endif /* __CFG80211_ETHTOOL__ */
