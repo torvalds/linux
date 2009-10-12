2624167cd8b4965d0ef548e84767a30214edd8d4
John Kacur <jkacur@redhat.com>
2009-10-12 23:09:24 +0200

    m68k: Remove the BKL from sys_execve
    This seems like a copy-and-paste from code that no-longer needs the
    BKL Just remove it.
    
    Signed-off-by: John Kacur <jkacur@redhat.com>
    Reviewed-by: Frederic Weisbecker <fweisbec@gmail.com>
    LKML-Reference: <alpine.LFD.2.00.0910130011180.3658@localhost.localdomain>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/m68k/kernel/process.c
+++ arch/m68k/kernel/process.c
@@ -317,15 +317,12 @@ asmlinkage int sys_execve(char __user *name, char __user * __user *argv, char __
 	char * filename;
 	struct pt_regs *regs = (struct pt_regs *) &name;
 
-	lock_kernel();
 	filename = getname(name);
 	error = PTR_ERR(filename);
 	if (IS_ERR(filename))
-		goto out;
+		return error;
 	error = do_execve(filename, argv, envp, regs);
 	putname(filename);
-out:
-	unlock_kernel();
 	return error;
 }
 
