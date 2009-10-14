298ebc3ef2a6c569b3eb51651f04e26aecbf8a1d
Steven Rostedt <srostedt@redhat.com>
2009-10-14 15:43:34 -0400

    perf tools: Handle trace parsing of < and >
    The code to handle the '<' and '>' ops was all in place, but
    they were not in the switch statement to consider them as valid
    ops.
    
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    LKML-Reference: <20091014194357.807434040@goodmis.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- tools/perf/util/trace-event-parse.c
+++ tools/perf/util/trace-event-parse.c
@@ -1170,6 +1170,8 @@ process_op(struct event *event, struct print_arg *arg, char **tok)
 		   strcmp(token, "*") == 0 ||
 		   strcmp(token, "^") == 0 ||
 		   strcmp(token, "/") == 0 ||
+		   strcmp(token, "<") == 0 ||
+		   strcmp(token, ">") == 0 ||
 		   strcmp(token, "==") == 0 ||
 		   strcmp(token, "!=") == 0) {
 
