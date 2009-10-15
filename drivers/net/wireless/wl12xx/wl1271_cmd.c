7a38079e0da19447ab1c41e42094b311c6e945e4
Juuso Oikarinen <juuso.oikarinen@nokia.com>
2009-10-15 10:33:26 +0300

    wl1271: Set IEEE80211_FCTL_TODS in the null data template
    Set the IEEE80211_FCTL_TODS bit in the FC of the null data template.
    This is a mandatory requirement in specification.
    
    Signed-off-by: Juuso Oikarinen <juuso.oikarinen@nokia.com>
    Cc: Vidhya Govindan <vidhya.govindan@nokia.com>
    Cc: Janne Ylälehto <janne.ylalehto@nokia.com>
    Reviewed-by: Janne Ylalehto <janne.ylalehto@nokia.com>
    Signed-off-by: Luciano Coelho <luciano.coelho@nokia.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/wl1271_cmd.c
+++ drivers/net/wireless/wl12xx/wl1271_cmd.c
@@ -769,7 +769,8 @@ int wl1271_cmd_build_null_data(struct wl1271 *wl)
 
 	memcpy(template.header.sa, wl->mac_addr, ETH_ALEN);
 	template.header.frame_ctl = cpu_to_le16(IEEE80211_FTYPE_DATA |
-						IEEE80211_STYPE_NULLFUNC);
+						IEEE80211_STYPE_NULLFUNC |
+						IEEE80211_FCTL_TODS);
 
 	return wl1271_cmd_template_set(wl, CMD_TEMPL_NULL_DATA, &template,
 				       sizeof(template));
