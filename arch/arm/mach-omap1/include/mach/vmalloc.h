72464dbae2749dd57bc2b3cd57d4fc6ba7abca37
Tony Lindgren <tony@atomide.com>
2009-10-19 17:26:29 -0700

    omap: Split vmalloc.h for mach-omap1 and mach-omap2
    Earlier patch "omap: Remap L3, L4 to get more kernel io address space"
    changed the VMALLOC_END.
    
    However, this change causes problems on mach-omap1:
    
    BUG: mapping for 0xe0000000 at 0xe0000000 overlaps vmalloc space
    BUG: mapping for 0xe1000000 at 0xe1000000 overlaps vmalloc space
    
    Fix this by creating separate vmalloc.h files for mach-omap1
    and mach-omap2.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap1/include/mach/vmalloc.h
+++ arch/arm/mach-omap1/include/mach/vmalloc.h
@@ -1,5 +1,5 @@
 /*
- *  arch/arm/plat-omap/include/mach/vmalloc.h
+ *  arch/arm/mach-omap1/include/mach/vmalloc.h
  *
  *  Copyright (C) 2000 Russell King.
  *
@@ -17,8 +17,4 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-#ifdef CONFIG_ARCH_OMAP1
 #define VMALLOC_END	(PAGE_OFFSET + 0x18000000)
-#else
-#define VMALLOC_END	(PAGE_OFFSET + 0x38000000)
-#endif
