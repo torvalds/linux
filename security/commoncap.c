3e1c2515acf70448cad1ae3ab835ca80be043d33
James Morris <jmorris@namei.org>
2009-10-20 13:48:33 +0900

    security: remove root_plug
        Remove the root_plug example LSM code.  It's unmaintained and
        increasingly broken in various ways.
    
        Made at the 2009 Kernel Summit in Tokyo!
    
        Acked-by: Greg Kroah-Hartman <gregkh@suse.de>
        Signed-off-by: James Morris <jmorris@namei.org>

--- security/commoncap.c
+++ security/commoncap.c
@@ -1,4 +1,4 @@
-/* Common capabilities, needed by capability.o and root_plug.o
+/* Common capabilities, needed by capability.o.
  *
  *	This program is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
