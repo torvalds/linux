a7149f9a26eb44a5658d56335c23104ba529e9f6
Dominik Brodowski <linux@dominikbrodowski.net>
2009-10-24 18:07:16 +0200

    pcmcia: use dev_dbg and dev_print in pd6729.c
    As suggested by Wolfram Sang <w.sang@pengutronix.de>, use dev_dbg(),
    and dev_{err,warn,info}() in pd6729.c, and add some "\n" suggested by
    Komuro <komurojun-mbn@nifty.com>. In the ISR, use pr_devel() and
    dev_vdbg() as they are only compiled if DEBUG (or, for dev_vdbg(),
    VERBOSE_DEBUG) are set explicitly.
    
    CC: Komuro <komurojun-mbn@nifty.com>
    Acked-by: Wolfram Sang <w.sang@pengutronix.de>
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- drivers/pcmcia/pd6729.h
+++ drivers/pcmcia/pd6729.h
@@ -1,13 +1,6 @@
 #ifndef _INCLUDE_GUARD_PD6729_H_
 #define _INCLUDE_GUARD_PD6729_H_
 
-/* Debuging defines */
-#ifdef NOTRACE
-#define dprintk(fmt, args...) printk(fmt , ## args)
-#else
-#define dprintk(fmt, args...) do {} while (0)
-#endif
-
 /* Flags for I365_GENCTL */
 #define I365_DF_VS1		0x40	/* DF-step Voltage Sense */
 #define I365_DF_VS2		0x80
