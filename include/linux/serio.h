a5f523bc0cdee2a163a034344ebf1163799b3c5d
Tias Guns <tias@ulyssis.org>
2009-10-25 12:13:58 -0700

    Input: add driver for Dynapro serial touchscreen
    This is a driver for Dynapro serial touchscreen, which used to be
    supported in Xorg. The driver needs updated inputattach utility to
    initialize serial port and create proper serio device before the
    driver will be bound to it.
    
    Signed-off-by: Tias Guns <tias@ulyssis.org>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- include/linux/serio.h
+++ include/linux/serio.h
@@ -215,5 +215,6 @@ static inline void serio_unpin_driver(struct serio *serio)
 #define SERIO_INEXIO	0x37
 #define SERIO_TOUCHIT213	0x38
 #define SERIO_W8001	0x39
+#define SERIO_DYNAPRO	0x3a
 
 #endif
