d6ba452128178091dab7a04d54f7e66fdc32fb39
Mimi Zohar <zohar@linux.vnet.ibm.com>
2009-10-26 09:26:18 -0400

    tpm add default function definitions
    Add default tpm_pcr_read/extend function definitions required
    by IMA/Kconfig changes.
    
    Signed-off-by: Mimi Zohar <zohar@us.ibm.com>
    Reviewed-by: Eric Paris <eparis@redhat.com>
    Signed-off-by: James Morris <jmorris@namei.org>

--- include/linux/tpm.h
+++ include/linux/tpm.h
@@ -31,5 +31,12 @@
 
 extern int tpm_pcr_read(u32 chip_num, int pcr_idx, u8 *res_buf);
 extern int tpm_pcr_extend(u32 chip_num, int pcr_idx, const u8 *hash);
+#else
+static inline int tpm_pcr_read(u32 chip_num, int pcr_idx, u8 *res_buf) {
+	return -ENODEV;
+}
+static inline int tpm_pcr_extend(u32 chip_num, int pcr_idx, const u8 *hash) {
+	return -ENODEV;
+}
 #endif
 #endif
