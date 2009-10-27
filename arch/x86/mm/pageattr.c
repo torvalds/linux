883242dd0e5faaba041528a9a99f483f2a656c83
Steven Rostedt <srostedt@redhat.com>
2009-10-27 13:15:11 -0400

    tracing: allow to change permissions for text with dynamic ftrace enabled
    The commit 74e081797bd9d2a7d8005fe519e719df343a2ba8
    x86-64: align RODATA kernel section to 2MB with CONFIG_DEBUG_RODATA
    prevents text sections from becoming read/write using set_memory_rw.
    
    The dynamic ftrace changes all text pages to read/write just before
    converting the calls to tracing to nops, and vice versa.
    
    I orginally just added a flag to allow this transaction when ftrace
    did the change, but I also found that when the CPA testing was running
    it would remove the read/write as well, and ftrace does not do the text
    conversion on boot up, and the CPA changes caused the dynamic tracer
    to fail on self tests.
    
    The current solution I have is to simply not to prevent
    change_page_attr from setting the RW bit for kernel text pages.
    
    Reported-by: Ingo Molnar <mingo@elte.hu>
    Cc: Suresh Siddha <suresh.b.siddha@intel.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- arch/x86/mm/pageattr.c
+++ arch/x86/mm/pageattr.c
@@ -279,7 +279,8 @@ static inline pgprot_t static_protections(pgprot_t prot, unsigned long address,
 		   __pa((unsigned long)__end_rodata) >> PAGE_SHIFT))
 		pgprot_val(forbidden) |= _PAGE_RW;
 
-#if defined(CONFIG_X86_64) && defined(CONFIG_DEBUG_RODATA)
+#if defined(CONFIG_X86_64) && defined(CONFIG_DEBUG_RODATA) && \
+	!defined(CONFIG_DYNAMIC_FTRACE)
 	/*
 	 * Kernel text mappings for the large page aligned .rodata section
 	 * will be read-only. For the kernel identity mappings covering
