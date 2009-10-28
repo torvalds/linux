ff76ec18cabb12a6c8f3c65bd1d23f1a770fe908
Randy Dunlap <randy.dunlap@oracle.com>
2009-10-28 12:26:39 -0700

    tpm: fix header for modular build
    Fix build for TCG_TPM=m.  Header file doesn't handle this
    and incorrectly builds stubs.
    
    drivers/char/tpm/tpm.c:720: error: redefinition of 'tpm_pcr_read'
    include/linux/tpm.h:35: error:previous definition of 'tpm_pcr_read' was here
    drivers/char/tpm/tpm.c:752: error: redefinition of 'tpm_pcr_extend'
    include/linux/tpm.h:38: error:previous definition of 'tpm_pcr_extend' was here
    
    Repairs linux-next's
    
    commit d6ba452128178091dab7a04d54f7e66fdc32fb39
    Author: Mimi Zohar <zohar@linux.vnet.ibm.com>
    Date:   Mon Oct 26 09:26:18 2009 -0400
    
        tpm add default function definitions
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Cc: Rajiv Andrade <srajiv@linux.vnet.ibm.com>
    Cc: Mimi Zohar <zohar@us.ibm.com>
    Cc: James Morris <jmorris@namei.org>
    Cc: Eric Paris <eparis@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: James Morris <jmorris@namei.org>

--- include/linux/tpm.h
+++ include/linux/tpm.h
@@ -27,7 +27,7 @@
  */
 #define	TPM_ANY_NUM 0xFFFF
 
-#if defined(CONFIG_TCG_TPM)
+#if defined(CONFIG_TCG_TPM) || defined(CONFIG_TCG_TPM_MODULE)
 
 extern int tpm_pcr_read(u32 chip_num, int pcr_idx, u8 *res_buf);
 extern int tpm_pcr_extend(u32 chip_num, int pcr_idx, const u8 *hash);
