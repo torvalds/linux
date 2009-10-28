f8d61c26afb6b6a47f1a774e3f25ca7ea49b0053
Mike Chan <mike@android.com>
2009-10-27 17:37:44 -0700

    input: evdev: Use unique wakelock names
    wakelock names are now contain evdev->name and pid
    
    Signed-off-by: Mike Chan <mike@android.com>

--- drivers/input/evdev.c
+++ drivers/input/evdev.c
@@ -45,6 +45,7 @@ struct evdev_client {
 	struct evdev *evdev;
 	struct list_head node;
 	struct wake_lock wake_lock;
+	char name[28];
 };
 
 static struct evdev *evdev_table[EVDEV_MINORS];
@@ -276,7 +277,9 @@ static int evdev_open(struct inode *inode, struct file *file)
 	}
 
 	spin_lock_init(&client->buffer_lock);
-	wake_lock_init(&client->wake_lock, WAKE_LOCK_SUSPEND, "evdev");
+	snprintf(client->name, sizeof(client->name), "%s-%d",
+			dev_name(&evdev->dev), task_tgid_vnr(current));
+	wake_lock_init(&client->wake_lock, WAKE_LOCK_SUSPEND, client->name);
 	client->evdev = evdev;
 	evdev_attach_client(evdev, client);
 
