a1bf808849f25a4d668f81415ecebb2da9fecf8e
Daniel T Chen <crimsun@ubuntu.com>
2009-11-01 18:32:29 -0500

    ALSA: hda: Use quirk mask for Dell Inspiron Mini9/Vostro A90 using ALC268
    BugLink: https://bugs.launchpad.net/bugs/368629
    
    We should use a quirk mask for these Dell Inspiron Mini9s and Vostro
    A90s, as the model=dell quirk appears to enable audio on them.
    
    Signed-off-by: Daniel T Chen <crimsun@ubuntu.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -12602,7 +12602,8 @@ static struct snd_pci_quirk alc268_cfg_tbl[] = {
 	SND_PCI_QUIRK(0x1025, 0x015b, "Acer Aspire One",
 						ALC268_ACER_ASPIRE_ONE),
 	SND_PCI_QUIRK(0x1028, 0x0253, "Dell OEM", ALC268_DELL),
-	SND_PCI_QUIRK(0x1028, 0x02b0, "Dell Inspiron Mini9", ALC268_DELL),
+	SND_PCI_QUIRK_MASK(0x1028, 0xfff0, 0x02b0,
+			"Dell Inspiron Mini9/Vostro A90", ALC268_DELL),
 	/* almost compatible with toshiba but with optional digital outs;
 	 * auto-probing seems working fine
 	 */
