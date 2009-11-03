a6c0f6eca1173e9d44f1f0fb9ecc832abd7d77d6
Sekhar Nori <nsekhar@ti.com>
2009-11-03 15:14:13 +0530

    davinci: add CPU idle driver
    The patch adds support for DaVinci cpu idle driver.
    
    Two idle states are defined:
    1. Wait for interrupt
    2. Wait for interrupt and DDR self-refresh (or power down)
    
    Some DaVinci SoCs support putting DDR in self-refresh (eg Dm644x, DM6467)
    while others support putting DDR in self-refresh and power down (eg DM35x,
    DA8xx).
    
    Putting DDR (or mDDR) in power down saves more power than self-refresh.
    
    The patch has been tested on DA850/OMAP-L138 EVM.
    
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

---
+++ arch/arm/mach-davinci/include/mach/cpuidle.h
@@ -0,0 +1,17 @@
+/*
+ * TI DaVinci cpuidle platform support
+ *
+ * 2009 (C) Texas Instruments, Inc. http://www.ti.com/
+ *
+ * This file is licensed under the terms of the GNU General Public License
+ * version 2. This program is licensed "as is" without any warranty of any
+ * kind, whether express or implied.
+ */
+#ifndef _MACH_DAVINCI_CPUIDLE_H
+#define _MACH_DAVINCI_CPUIDLE_H
+
+struct davinci_cpuidle_config {
+	u32 ddr2_pdown;
+};
+
+#endif
