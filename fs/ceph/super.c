6a18be16f7513ea8a4923c161ce073987932cbdb
Sage Weil <sage@newdream.net>
2009-11-04 11:40:05 -0800

    ceph: fix sparse endian warning
    Use the __le macro, even though for -1 it doesn't matter.
    
    Signed-off-by: Sage Weil <sage@newdream.net>

--- fs/ceph/super.c
+++ fs/ceph/super.c
@@ -804,7 +804,7 @@ static int ceph_get_sb(struct file_system_type *fs_type,
 	struct ceph_client *client;
 	int err;
 	int (*compare_super)(struct super_block *, void *) = ceph_compare_super;
-	const char *path = 0;
+	const char *path = NULL;
 	struct ceph_mount_args *args;
 
 	dout("ceph_get_sb\n");
