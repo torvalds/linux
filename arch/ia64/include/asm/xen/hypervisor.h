5b5d94487d934be6b0aa966c9acbdf15b07ef627
Isaku Yamahata <yamahata@valinux.co.jp>
2009-11-06 11:11:43 +0900

    ia64/xen: compilation fix
    This patch fixes the following compilation error introduced by a PCI related
    features.
    
    The change set of 5dd1af9f84c79bedd589db89e71ca733f3bf0ebd moves some
    xen related definitions from the arch header file
    (x86/include/asm/xen/hypervisor.h) to the common header file
    (include/xen/xen.h).  So ia64/xen also follows it.
    
    In file included from linux-next/include/xen/grant_table.h:41,
                     from linux-next/drivers/block/xen-blkfront.c:48:
    linux-next/arch/ia64/include/asm/xen/hypervisor.h:43: error: nested redefinition of 'enum xen_domain_type'
    linux-next/arch/ia64/include/asm/xen/hypervisor.h:43: error: redeclaration of 'enum xen_domain_type'
    linux-next/arch/ia64/include/asm/xen/hypervisor.h:44: error: redeclaration of enumerator 'XEN_NATIVE'
    linux-next/include/xen/xen.h:5: error: previous definition of 'XEN_NATIVE' was here
    linux-next/arch/ia64/include/asm/xen/hypervisor.h:45: error: redeclaration of enumerator 'XEN_PV_DOMAIN'
    linux-next/include/xen/xen.h:6: error: previous definition of 'XEN_PV_DOMAIN' was here
    linux-next/arch/ia64/include/asm/xen/hypervisor.h:46: error: redeclaration of enumerator 'XEN_HVM_DOMAIN'
    linux-next/include/xen/xen.h:7: error: previous definition of 'XEN_HVM_DOMAIN' was here
    
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- arch/ia64/include/asm/xen/hypervisor.h
+++ arch/ia64/include/asm/xen/hypervisor.h
@@ -37,34 +37,8 @@
 #include <xen/interface/xen.h>
 #include <xen/interface/version.h>	/* to compile feature.c */
 #include <xen/features.h>		/* to comiple xen-netfront.c */
+#include <xen/xen.h>
 #include <asm/xen/hypercall.h>
-
-/* xen_domain_type is set before executing any C code by early_xen_setup */
-enum xen_domain_type {
-	XEN_NATIVE,	/* running on bare hardware */
-	XEN_PV_DOMAIN,	/* running in a PV domain */
-	XEN_HVM_DOMAIN,	/* running in a Xen hvm domain*/
-};
-
-#ifdef CONFIG_XEN
-extern enum xen_domain_type xen_domain_type;
-#else
-#define xen_domain_type		XEN_NATIVE
-#endif
-
-#define xen_domain()		(xen_domain_type != XEN_NATIVE)
-#define xen_pv_domain()		(xen_domain() &&			\
-				 xen_domain_type == XEN_PV_DOMAIN)
-#define xen_hvm_domain()	(xen_domain() &&			\
-				 xen_domain_type == XEN_HVM_DOMAIN)
-
-#ifdef CONFIG_XEN_DOM0
-#define xen_initial_domain()	(xen_pv_domain() &&			\
-				 (xen_start_info->flags & SIF_INITDOMAIN))
-#else
-#define xen_initial_domain()	(0)
-#endif
-
 
 #ifdef CONFIG_XEN
 extern struct shared_info *HYPERVISOR_shared_info;
