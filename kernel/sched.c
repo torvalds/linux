ffd44db5f02af32bcc25a8eb5981bf02a141cdab
Peter Zijlstra <peterz@infradead.org>
2009-11-10 20:12:01 +0100

    sched: Make sure task has correct sched_class after policy change
    From the code in rt_mutex_setprio(), it is evident that the
    intention is that task's with a RT 'prio' value as a consequence
    of receiving a PI boost also have their 'sched_class' field set
    to '&rt_sched_class'.
    
    However, Peter noticed that the code in __setscheduler() could
    result in this intention being frustrated. Fix it.
    
    Reported-by: Peter Williams <pwil3058@bigpond.net.au>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Mike Galbraith <efault@gmx.de>
    LKML-Reference: <1257880321.4108.457.camel@laptop>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched.c
+++ kernel/sched.c
@@ -6159,22 +6159,14 @@ __setscheduler(struct rq *rq, struct task_struct *p, int policy, int prio)
 	BUG_ON(p->se.on_rq);
 
 	p->policy = policy;
-	switch (p->policy) {
-	case SCHED_NORMAL:
-	case SCHED_BATCH:
-	case SCHED_IDLE:
-		p->sched_class = &fair_sched_class;
-		break;
-	case SCHED_FIFO:
-	case SCHED_RR:
-		p->sched_class = &rt_sched_class;
-		break;
-	}
-
 	p->rt_priority = prio;
 	p->normal_prio = normal_prio(p);
 	/* we are holding p->pi_lock already */
 	p->prio = rt_mutex_getprio(p);
+	if (rt_prio(p->prio))
+		p->sched_class = &rt_sched_class;
+	else
+		p->sched_class = &fair_sched_class;
 	set_load_weight(p);
 }
 
