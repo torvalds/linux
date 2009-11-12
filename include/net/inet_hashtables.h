5256f2ef3a40d784b8369035bff3f4dc637a9801
Lucian Adrian Grijincu <lgrijincu@ixiacom.com>
2009-11-12 05:07:26 +0000

    inet: fix inet_bind_bucket_for_each
    The first "node" is supposed to be the cursor used in the for_each.
    
    The second "node" is ment literally and should not be macro expanded:
    it's the name of the hlist_node field from the inet_bind_bucket.
    
    This currently works because when inet_bind_bucket_for_each is called
    it's argument is still "node".
    
    Signed-off-by: Lucian Adrian Grijincu <lgrijincu@ixiacom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/inet_hashtables.h
+++ include/net/inet_hashtables.h
@@ -92,8 +92,8 @@ static inline struct net *ib_net(struct inet_bind_bucket *ib)
 	return read_pnet(&ib->ib_net);
 }
 
-#define inet_bind_bucket_for_each(tb, node, head) \
-	hlist_for_each_entry(tb, node, head, node)
+#define inet_bind_bucket_for_each(tb, pos, head) \
+	hlist_for_each_entry(tb, pos, head, node)
 
 struct inet_bind_hashbucket {
 	spinlock_t		lock;
