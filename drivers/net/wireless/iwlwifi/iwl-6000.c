85bb174a386850c8a43e8d107af47de3c910be03
Wey-Yi Guy <wey-yi.w.guy@intel.com>
2009-11-13 11:56:24 -0800

    iwlwifi: disable coex until implementation ready for 6x50
    Temporary disable the coex function for wifi/wimax for 6x50
    series until the full implementation ready.
    
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-6000.c
+++ drivers/net/wireless/iwlwifi/iwl-6000.c
@@ -395,7 +395,6 @@ struct iwl_cfg iwl6050_2agn_cfg = {
 	.adv_thermal_throttle = true,
 	.support_ct_kill_exit = true,
 	.support_sm_ps = true,
-	.support_wimax_coexist = true,
 };
 
 struct iwl_cfg iwl6050_2abg_cfg = {
@@ -425,7 +424,6 @@ struct iwl_cfg iwl6050_2abg_cfg = {
 	.supports_idle = true,
 	.adv_thermal_throttle = true,
 	.support_ct_kill_exit = true,
-	.support_wimax_coexist = true,
 };
 
 struct iwl_cfg iwl6000_3agn_cfg = {
