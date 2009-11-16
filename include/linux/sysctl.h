60a0a52df149286a25fddf9b2d0cfe77cf0bc516
Eric W. Biederman <ebiederm@xmission.com>
2009-11-16 02:30:16 -0800

    sysctl: kill dead ctl_handler definitions.
    When removing the sysctl strategy routines I overlooked their definitions
    in sysctl.h.    So remove those unnecessary definitions now.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>

--- include/linux/sysctl.h
+++ include/linux/sysctl.h
@@ -972,10 +972,6 @@ extern int sysctl_perm(struct ctl_table_root *root,
 
 typedef struct ctl_table ctl_table;
 
-typedef int ctl_handler (struct ctl_table *table,
-			 void __user *oldval, size_t __user *oldlenp,
-			 void __user *newval, size_t newlen);
-
 typedef int proc_handler (struct ctl_table *ctl, int write,
 			  void __user *buffer, size_t *lenp, loff_t *ppos);
 
@@ -995,13 +991,6 @@ extern int proc_doulongvec_minmax(struct ctl_table *, int,
 				  void __user *, size_t *, loff_t *);
 extern int proc_doulongvec_ms_jiffies_minmax(struct ctl_table *table, int,
 				      void __user *, size_t *, loff_t *);
-
-extern ctl_handler sysctl_data;
-extern ctl_handler sysctl_string;
-extern ctl_handler sysctl_intvec;
-extern ctl_handler sysctl_jiffies;
-extern ctl_handler sysctl_ms_jiffies;
-
 
 /*
  * Register a set of sysctl names by calling register_sysctl_table
