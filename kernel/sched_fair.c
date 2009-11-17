a65ac745e47e91f9d98dbf07f22ed0492e34d998
Jupyung Lee <jupyung@gmail.com>
2009-11-17 18:51:40 +0900

    sched: Move update_curr() in check_preempt_wakeup() to avoid redundant call
    If a RT task is woken up while a non-RT task is running,
    check_preempt_wakeup() is called to check whether the new task can
    preempt the old task. The function returns quickly without going deeper
    because it is apparent that a RT task can always preempt a non-RT task.
    
    In this situation, check_preempt_wakeup() always calls update_curr() to
    update vruntime value of the currently running task. However, the
    function call is unnecessary and redundant at that moment because (1) a
    non-RT task can always be preempted by a RT task regardless of its
    vruntime value, and (2) update_curr() will be called shortly when the
    context switch between two occurs.
    
    By moving update_curr() in check_preempt_wakeup(), we can avoid
    redundant call to update_curr(), slightly reducing the time taken to
    wake up RT tasks.
    
    Signed-off-by: Jupyung Lee <jupyung@gmail.com>
    [ Place update_curr() right before the wake_preempt_entity() call, which
      is the only thing that relies on the updated vruntime ]
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    LKML-Reference: <1258451500-6714-1-git-send-email-jupyung@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -1651,8 +1651,6 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_
 	int sync = wake_flags & WF_SYNC;
 	int scale = cfs_rq->nr_running >= sched_nr_latency;
 
-	update_curr(cfs_rq);
-
 	if (unlikely(rt_prio(p->prio))) {
 		resched_task(curr);
 		return;
@@ -1709,6 +1707,8 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_
 	find_matching_se(&se, &pse);
 
 	BUG_ON(!pse);
+
+	update_curr(cfs_rq);
 
 	if (wakeup_preempt_entity(se, pse) == 1) {
 		resched_task(curr);
