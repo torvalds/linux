63ae93a19094d88c8ca62543586b20e3a7ff7637
Ron Mercer <ron.mercer@qlogic.com>
2009-11-17 11:10:40 +0000

    qlge: Bonding fix for mode 6.
    Allow MAC address to be changed even if device is not up.
    
    Signed-off-by: Ron Mercer <ron.mercer@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/qlge/qlge_main.c
+++ drivers/net/qlge/qlge_main.c
@@ -3951,9 +3951,6 @@ static int qlge_set_mac_address(struct net_device *ndev, void *p)
 	struct sockaddr *addr = p;
 	int status;
 
-	if (netif_running(ndev))
-		return -EBUSY;
-
 	if (!is_valid_ether_addr(addr->sa_data))
 		return -EADDRNOTAVAIL;
 	memcpy(ndev->dev_addr, addr->sa_data, ndev->addr_len);
