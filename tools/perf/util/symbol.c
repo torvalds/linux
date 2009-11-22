e57cfcdac6badd846a1cd831de54a1359c2d1eea
Pekka Enberg <penberg@cs.helsinki.fi>
2009-11-22 12:29:44 +0200

    perf symbols: Fix ELF header errors during "perf kmem record"
    The write_event() function in builtin-record.c writes out all
    mmap()'d DSOs including non-ELF files like GNOME resource files
    and such.
    
    Therefore, check for ELF_K_ELF in filename__read_build_id()
    before attempting to read the ELF header with gelf_getehdr().
    
    Fixes the following error messages when running "perf kmem
    record":
    
      penberg@penberg-laptop:~/src/linux/tools/perf$ perf kmem record
      ^C[ perf record: Woken up 2 times to write data ]
      [ perf record: Captured and wrote 0.753 MB perf.data (~32885 samples) ]
      filename__read_build_id: cannot get elf header.
      filename__read_build_id: cannot get elf header.
      filename__read_build_id: cannot get elf header.
      filename__read_build_id: cannot get elf header.
      filename__read_build_id: cannot get elf header.
      filename__read_build_id: cannot get elf header.
      filename__read_build_id: cannot get elf header.
      filename__read_build_id: cannot get elf header.
      filename__read_build_id: cannot get elf header.
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Li Zefan <lizf@cn.fujitsu.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    LKML-Reference: <1258885784-11709-1-git-send-email-penberg@cs.helsinki.fi>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- tools/perf/util/symbol.c
+++ tools/perf/util/symbol.c
@@ -921,6 +921,7 @@ int filename__read_build_id(const char *filename, void *bf, size_t size)
 	GElf_Shdr shdr;
 	Elf_Data *data;
 	Elf_Scn *sec;
+	Elf_Kind ek;
 	void *ptr;
 	Elf *elf;
 
@@ -936,6 +937,10 @@ int filename__read_build_id(const char *filename, void *bf, size_t size)
 		pr_debug2("%s: cannot read %s ELF file.\n", __func__, filename);
 		goto out_close;
 	}
+
+	ek = elf_kind(elf);
+	if (ek != ELF_K_ELF)
+		goto out_elf_end;
 
 	if (gelf_getehdr(elf, &ehdr) == NULL) {
 		pr_err("%s: cannot get elf header.\n", __func__);
