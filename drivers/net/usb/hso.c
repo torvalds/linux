dcfcb256cc23c4436691b0fe677275306699d6a1
Antti Kaijanmäki <antti.kaijanmaki@nomovok.com>
2009-11-23 10:54:47 -0800

    hso: fix soft-lockup
    Fix soft-lockup in hso.c which is triggered on SMP machine when
    modem is removed while file descriptor(s) under /dev are still open:
    
      old version called kref_put() too early which resulted in destroying
      hso_serial and hso_device objects which were still used later on.
    
    Signed-off-by: Antti Kaijanmäki <antti.kaijanmaki@nomovok.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/hso.c
+++ drivers/net/usb/hso.c
@@ -1363,7 +1363,7 @@ static void hso_serial_close(struct tty_struct *tty, struct file *filp)
 	/* reset the rts and dtr */
 	/* do the actual close */
 	serial->open_count--;
-	kref_put(&serial->parent->ref, hso_serial_ref_free);
+
 	if (serial->open_count <= 0) {
 		serial->open_count = 0;
 		spin_lock_irq(&serial->serial_lock);
@@ -1383,6 +1383,8 @@ static void hso_serial_close(struct tty_struct *tty, struct file *filp)
 		usb_autopm_put_interface(serial->parent->interface);
 
 	mutex_unlock(&serial->parent->mutex);
+
+	kref_put(&serial->parent->ref, hso_serial_ref_free);
 }
 
 /* close the requested serial port */
