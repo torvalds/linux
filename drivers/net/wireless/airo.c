1f351e3840dcf25aaddec2d908c3ab06ae105ee6
Michael Buesch <mb@bu3sch.de>
2009-11-25 22:55:11 +0100

    airo: Fix integer overflow warning
    On BigEndian gcc complains:
    
    drivers/net/wireless/airo.c: In function ‘sniffing_mode’:
    drivers/net/wireless/airo.c:4809: warning: integer overflow in expression
    
    Fix this by doing the bitwise AND on the host-endian value.
    
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/airo.c
+++ drivers/net/wireless/airo.c
@@ -4806,7 +4806,7 @@ static int airo_config_commit(struct net_device *dev,
 
 static inline int sniffing_mode(struct airo_info *ai)
 {
-	return le16_to_cpu(ai->config.rmode & RXMODE_MASK) >=
+	return (le16_to_cpu(ai->config.rmode) & le16_to_cpu(RXMODE_MASK)) >=
 		le16_to_cpu(RXMODE_RFMON);
 }
 
