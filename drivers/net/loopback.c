999b6d39abb4fc446f3465ca4e0a7ac747f49aec
Eric W. Biederman <ebiederm@xmission.com>
2009-11-29 22:25:29 +0000

    net: Simplify loopback and improve batching.
    Defer calling unregister_netdevice_queue to cleanup_net.  It's simpler
    and it allows the loopback device to land in the same batch as other
    network devices.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/loopback.c
+++ drivers/net/loopback.c
@@ -212,15 +212,7 @@ out:
 	return err;
 }
 
-static __net_exit void loopback_net_exit(struct net *net)
-{
-	struct net_device *dev = net->loopback_dev;
-
-	unregister_netdev(dev);
-}
-
 /* Registered in net/core/dev.c */
 struct pernet_operations __net_initdata loopback_net_ops = {
        .init = loopback_net_init,
-       .exit = loopback_net_exit,
 };
