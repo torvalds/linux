102123644979069c61d694375991aa6793e4da57
Ian Abbott <abbotti@mev.co.uk>
2009-11-30 10:59:27 +0000

    Staging: comedi: jr3_pci: Don't ioremap too much space. Check result.
    commit fa5c5f4ce0c9ba03a670c640cad17e14cb35678b upstream.
    
    For the JR3/PCI cards, the size of the PCIBAR0 region depends on the
    number of channels.  Don't try and ioremap space for 4 channels if the
    card has fewer channels.  Also check for ioremap failure.
    
    Thanks to Anders Blomdell for input and Sami Hussein for testing.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/drivers/jr3_pci.c
+++ drivers/staging/comedi/drivers/jr3_pci.c
@@ -856,8 +856,11 @@ static int jr3_pci_attach(struct comedi_device *dev,
 	}
 
 	devpriv->pci_enabled = 1;
-	devpriv->iobase =
-	    ioremap(pci_resource_start(card, 0), sizeof(struct jr3_t));
+	devpriv->iobase = ioremap(pci_resource_start(card, 0),
+			offsetof(struct jr3_t, channel[devpriv->n_channels]));
+	if (!devpriv->iobase)
+		return -ENOMEM;
+
 	result = alloc_subdevices(dev, devpriv->n_channels);
 	if (result < 0)
 		goto out;
