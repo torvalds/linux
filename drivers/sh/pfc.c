fd2cb0ce74e07babaf8c7bf96ef03c25d194e463
Paul Mundt <lethal@linux-sh.org>
2009-11-30 12:15:04 +0900

    sh: pfc: pr_info() -> pr_debug() cleanups.
    For some reason this was using pr_info() nested under an ifdef DEBUG.
    While this is appealing in that it circumvents the effort necessary to
    change ones loglevel, it's not terribly practical. So, convert it over
    to pr_debug().
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- drivers/sh/pfc.c
+++ drivers/sh/pfc.c
@@ -7,7 +7,6 @@
  * License.  See the file "COPYING" in the main directory of this archive
  * for more details.
  */
-
 #include <linux/errno.h>
 #include <linux/kernel.h>
 #include <linux/list.h>
@@ -72,11 +71,9 @@ static void gpio_write_bit(struct pinmux_data_reg *dr,
 
 	pos = dr->reg_width - (in_pos + 1);
 
-#ifdef DEBUG
-	pr_info("write_bit addr = %lx, value = %ld, pos = %ld, "
-		"r_width = %ld\n",
-		dr->reg, !!value, pos, dr->reg_width);
-#endif
+	pr_debug("write_bit addr = %lx, value = %ld, pos = %ld, "
+		 "r_width = %ld\n",
+		 dr->reg, !!value, pos, dr->reg_width);
 
 	if (value)
 		set_bit(pos, &dr->reg_shadow);
@@ -95,11 +92,9 @@ static int gpio_read_reg(unsigned long reg, unsigned long reg_width,
 	mask = (1 << field_width) - 1;
 	pos = reg_width - ((in_pos + 1) * field_width);
 
-#ifdef DEBUG
-	pr_info("read_reg: addr = %lx, pos = %ld, "
-		"r_width = %ld, f_width = %ld\n",
-		reg, pos, reg_width, field_width);
-#endif
+	pr_debug("read_reg: addr = %lx, pos = %ld, "
+		 "r_width = %ld, f_width = %ld\n",
+		 reg, pos, reg_width, field_width);
 
 	data = gpio_read_raw_reg(reg, reg_width);
 	return (data >> pos) & mask;
@@ -114,11 +109,9 @@ static void gpio_write_reg(unsigned long reg, unsigned long reg_width,
 	mask = (1 << field_width) - 1;
 	pos = reg_width - ((in_pos + 1) * field_width);
 
-#ifdef DEBUG
-	pr_info("write_reg addr = %lx, value = %ld, pos = %ld, "
-		"r_width = %ld, f_width = %ld\n",
-		reg, value, pos, reg_width, field_width);
-#endif
+	pr_debug("write_reg addr = %lx, value = %ld, pos = %ld, "
+		 "r_width = %ld, f_width = %ld\n",
+		 reg, value, pos, reg_width, field_width);
 
 	mask = ~(mask << pos);
 	value = value << pos;
