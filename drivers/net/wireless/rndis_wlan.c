6e850af58f93be8f1b4b79eb9c79d06788f38e78
Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
2009-11-30 21:50:13 +0200

    rndis_wlan: copy module parameters for bcm4320a devices too
    rndis_wlan didn't copy module parameters for bcm4320a to private structure.
    
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rndis_wlan.c
+++ drivers/net/wireless/rndis_wlan.c
@@ -2642,6 +2642,11 @@ static void rndis_copy_module_params(struct usbnet *usbdev)
 
 static int bcm4320a_early_init(struct usbnet *usbdev)
 {
+	/* copy module parameters for bcm4320a so that iwconfig reports txpower
+	 * and workaround parameter is copied to private structure correctly.
+	 */
+	rndis_copy_module_params(usbdev);
+
 	/* bcm4320a doesn't handle configuration parameters well. Try
 	 * set any and you get partially zeroed mac and broken device.
 	 */
