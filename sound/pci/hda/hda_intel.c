2f703e7a2ea5f6d5ea14a7b2cd0d31be07839ac6
Takashi Iwai <tiwai@suse.de>
2009-12-01 14:17:37 +0100

    ALSA: hda - Add position_fix quirk for HP dv3
    HP dv3 requires position_fix=1.
    
    Reference: Novell bnc#555935
    	https://bugzilla.novell.com/show_bug.cgi?id=555935
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -2233,6 +2233,7 @@ static int azx_dev_free(struct snd_device *device)
 static struct snd_pci_quirk position_fix_list[] __devinitdata = {
 	SND_PCI_QUIRK(0x1028, 0x01cc, "Dell D820", POS_FIX_LPIB),
 	SND_PCI_QUIRK(0x1028, 0x01de, "Dell Precision 390", POS_FIX_LPIB),
+	SND_PCI_QUIRK(0x103c, 0x306d, "HP dv3", POS_FIX_LPIB),
 	SND_PCI_QUIRK(0x1043, 0x813d, "ASUS P5AD2", POS_FIX_LPIB),
 	SND_PCI_QUIRK(0x1462, 0x1002, "MSI Wind U115", POS_FIX_LPIB),
 	{}
