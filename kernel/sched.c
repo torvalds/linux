c08f782985eed9959438368e84ce1d7f2ed03d95
Frederic Weisbecker <fweisbec@gmail.com>
2009-12-02 20:49:17 +0100

    mutex: Fix missing conditions to build mutex_spin_on_owner()
    We don't need to build mutex_spin_on_owner() if we have
    CONFIG_DEBUG_MUTEXES or CONFIG_HAVE_DEFAULT_NO_SPIN_MUTEXES as
    it won't be used under such configs.
    
    Use CONFIG_MUTEX_SPIN_ON_OWNER as it gathers all the necessary
    checks before building it.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    LKML-Reference: <1259783357-8542-2-git-send-regression-fweisbec@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Cc: Peter Zijlstra <peterz@infradead.org>

--- kernel/sched.c
+++ kernel/sched.c
@@ -5481,7 +5481,7 @@ need_resched_nonpreemptible:
 }
 EXPORT_SYMBOL(schedule);
 
-#ifdef CONFIG_SMP
+#ifdef CONFIG_MUTEX_SPIN_ON_OWNER
 /*
  * Look out! "owner" is an entirely speculative pointer
  * access and not reliable.
