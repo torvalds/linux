01e13543e486f1007ba5656e30c5f0932f4187b0
Arve Hjønnevåg <arve@android.com>
2009-12-01 20:54:37 -0800

    PM: wakelock: Don't dump unfrozen task list when aborting try_to_freeze_tasks after less than one second
    Change-Id: Ib2976e5b97a5ee4ec9abd4d4443584d9257d0941
    Signed-off-by: Arve Hjønnevåg <arve@android.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -130,7 +130,8 @@ static int try_to_freeze_tasks(bool sig_only)
 		read_lock(&tasklist_lock);
 		do_each_thread(g, p) {
 			task_lock(p);
-			if (!wakeup && freezing(p) && !freezer_should_skip(p))
+			if (freezing(p) && !freezer_should_skip(p) &&
+				elapsed_csecs > 100)
 				sched_show_task(p);
 			cancel_freezing(p);
 			task_unlock(p);
