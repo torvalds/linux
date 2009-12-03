8c0414cd524e9f1c483ffb3ff1c2d860f5c567c8
Sunil Mushran <sunil.mushran@oracle.com>
2009-12-03 12:46:51 -0800

    fiemap: Add new extent flag FIEMAP_EXTENT_SHARED
    Some filesystems may allow multiple files to point to a particular
    extent.  This patch adds flag FIEMAP_EXTENT_SHARED to denote extents
    that are shared with other inodes.
    
    Signed-off-by: Sunil Mushran <sunil.mushran@oracle.com>
    Acked-by: Mark Fasheh <mfasheh@suse.com>
    Signed-off-by: Joel Becker <joel.becker@oracle.com>

--- include/linux/fiemap.h
+++ include/linux/fiemap.h
@@ -62,5 +62,7 @@ struct fiemap {
 #define FIEMAP_EXTENT_MERGED		0x00001000 /* File does not natively
 						    * support extents. Result
 						    * merged for efficiency. */
+#define FIEMAP_EXTENT_SHARED		0x00002000 /* Space shared with other
+						    * files. */
 
 #endif /* _LINUX_FIEMAP_H */
