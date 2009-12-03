8818a9d884e3a589899be3303958fff182e98e55
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2009-12-02 22:24:02 -0800

    tcp: clear hints to avoid a stale one (nfs only affected?)
    Eric Dumazet mentioned in a context of another problem:
    
    "Well, it seems NFS reuses its socket, so maybe we miss some
    cleaning as spotted in this old patch"
    
    I've not check under which conditions that actually happens but
    if true, we need to make sure we don't accidently leave stale
    hints behind when the write queue had to be purged (whether reusing
    with NFS can actually happen if purging took place is something I'm
    not sure of).
    
    ...At least it compiles.
    
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -1229,6 +1229,7 @@ static inline void tcp_write_queue_purge(struct sock *sk)
 	while ((skb = __skb_dequeue(&sk->sk_write_queue)) != NULL)
 		sk_wmem_free_skb(sk, skb);
 	sk_mem_reclaim(sk);
+	tcp_clear_all_retrans_hints(tcp_sk(sk));
 }
 
 static inline struct sk_buff *tcp_write_queue_head(struct sock *sk)
