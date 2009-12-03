cd8ad40de36c2fe75f3b731bd70198b385895246
Peter Zijlstra <a.p.zijlstra@chello.nl>
2009-12-03 18:00:07 +0100

    sched: cgroup: Implement different treatment for idle shares
    When setting the weight for a per-cpu task-group, we have to put in a
    phantom weight when there is no work on that cpu, otherwise we'll not
    service that cpu when new work gets placed there until we again update
    the per-cpu weights.
    
    We used to add these phantom weights to the total, so that the idle
    per-cpu shares don't get inflated, this however causes the non-idle
    parts to get deflated, causing unexpected weight distibutions.
    
    Reverse this, so that the non-idle shares are correct but the idle
    shares are inflated.
    
    Reported-by: Yasunori Goto <y-goto@jp.fujitsu.com>
    Tested-by: Yasunori Goto <y-goto@jp.fujitsu.com>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    LKML-Reference: <1257934048.23203.76.camel@twins>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched.c
+++ kernel/sched.c
@@ -1614,7 +1614,7 @@ static void update_group_shares_cpu(struct task_group *tg, int cpu,
  */
 static int tg_shares_up(struct task_group *tg, void *data)
 {
-	unsigned long weight, rq_weight = 0, shares = 0;
+	unsigned long weight, rq_weight = 0, sum_weight = 0, shares = 0;
 	unsigned long *usd_rq_weight;
 	struct sched_domain *sd = data;
 	unsigned long flags;
@@ -1630,6 +1630,7 @@ static int tg_shares_up(struct task_group *tg, void *data)
 		weight = tg->cfs_rq[i]->load.weight;
 		usd_rq_weight[i] = weight;
 
+		rq_weight += weight;
 		/*
 		 * If there are currently no tasks on the cpu pretend there
 		 * is one of average load so that when a new task gets to
@@ -1638,9 +1639,12 @@ static int tg_shares_up(struct task_group *tg, void *data)
 		if (!weight)
 			weight = NICE_0_LOAD;
 
-		rq_weight += weight;
+		sum_weight += weight;
 		shares += tg->cfs_rq[i]->shares;
 	}
+
+	if (!rq_weight)
+		rq_weight = sum_weight;
 
 	if ((!shares && rq_weight) || shares > tg->shares)
 		shares = tg->shares;
