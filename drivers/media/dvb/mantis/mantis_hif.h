9ce39460837202920fb9bad88eeec13fc75e7a13
Manu Abraham <abraham.manu@gmail.com>
2009-12-04 04:52:23 -0300

    V4L/DVB (13728): [Mantis] Add in some Host Interface definitions
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

---
+++ drivers/media/dvb/mantis/mantis_hif.h
@@ -0,0 +1,9 @@
+#ifndef __MANTIS_HIF_H
+#define __MANTIS_HIF_H
+
+#define MANTIS_HIF_MEMRD		1
+#define MANTIS_HIF_MEMWR		2
+#define MANTIS_HIF_IOMRD		3
+#define MANTIS_HIF_IOMWR		4
+
+#endif // __MANTIS_HIF_H
