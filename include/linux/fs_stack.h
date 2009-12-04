1b8ab8159ef8f818f870a1d2e3b6953d80eefd3f
Erez Zadok <ezk@cs.sunysb.edu>
2009-12-03 21:56:09 -0500

    VFS/fsstack: handle 32-bit smp + preempt + large files in fsstack_copy_inode_size
    Copy the inode size and blocks from one inode to another correctly on 32-bit
    systems with CONFIG_SMP, CONFIG_PREEMPT, or CONFIG_LBDAF.  Use proper inode
    spinlocks only when i_size/i_blocks cannot fit in one 32-bit word.
    
    Signed-off-by: Hugh Dickins <hugh.dickins@tiscali.co.uk>
    Signed-off-by: Erez Zadok <ezk@cs.sunysb.edu>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/fs_stack.h
+++ include/linux/fs_stack.h
@@ -9,7 +9,7 @@
 
 /* externs for fs/stack.c */
 extern void fsstack_copy_attr_all(struct inode *dest, const struct inode *src);
-extern void fsstack_copy_inode_size(struct inode *dst, const struct inode *src);
+extern void fsstack_copy_inode_size(struct inode *dst, struct inode *src);
 
 /* inlines */
 static inline void fsstack_copy_attr_atime(struct inode *dest,
