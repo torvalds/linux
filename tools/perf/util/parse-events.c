180570fdb7a3c404b599f0a318c2ccf86e4827ed
Ulrich Drepper <drepper@redhat.com>
2009-12-06 13:25:30 -0500

    perf tools: Optimize parse_subsystem_tracepoint_event()
    Uses of strcat are almost always signs that someone is too lazy
    to think about the code a bit more carefully.  One always has to
    know about the lengths of the strings involved to avoid buffer
    overflows.
    
    This is one case where the size of the object code for me is
    reduced by 38 bytes.  The code should also be faster, especially
    if flags is non-NULL.
    
    Signed-off-by: Ulrich Drepper <drepper@redhat.com>
    Cc: a.p.zijlstra@chello.nl
    Cc: fweisbec@gmail.com
    Cc: jaswinderrajput@gmail.com
    Cc: paulus@samba.org
    LKML-Reference: <200912061825.nB6IPUa1023306@hs20-bc2-1.build.redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- tools/perf/util/parse-events.c
+++ tools/perf/util/parse-events.c
@@ -467,7 +467,6 @@ parse_subsystem_tracepoint_event(char *sys_name, char *flags)
 	while ((evt_ent = readdir(evt_dir))) {
 		char event_opt[MAX_EVOPT_LEN + 1];
 		int len;
-		unsigned int rem = MAX_EVOPT_LEN;
 
 		if (!strcmp(evt_ent->d_name, ".")
 		    || !strcmp(evt_ent->d_name, "..")
@@ -475,19 +474,11 @@ parse_subsystem_tracepoint_event(char *sys_name, char *flags)
 		    || !strcmp(evt_ent->d_name, "filter"))
 			continue;
 
-		len = snprintf(event_opt, MAX_EVOPT_LEN, "%s:%s", sys_name,
-			       evt_ent->d_name);
+		len = snprintf(event_opt, MAX_EVOPT_LEN, "%s:%s%s%s", sys_name,
+			       evt_ent->d_name, flags ? ":" : "",
+			       flags ?: "");
 		if (len < 0)
 			return EVT_FAILED;
-
-		rem -= len;
-		if (flags) {
-			if (rem < strlen(flags) + 1)
-				return EVT_FAILED;
-
-			strcat(event_opt, ":");
-			strcat(event_opt, flags);
-		}
 
 		if (parse_events(NULL, event_opt, 0))
 			return EVT_FAILED;
