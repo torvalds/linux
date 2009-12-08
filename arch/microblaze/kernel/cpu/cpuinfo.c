81ab0dfd9444eabdfd64fb007b0f81cde6b153da
Michal Simek <monstr@monstr.eu>
2009-12-08 17:51:06 +0100

    microblaze: Remove ancient and fake microblaze version from cpu_ver table
    We need to continue with next microblaze PVR version that's why
    I have to remove that ancient version. These version strings not match
    any versions. From Microblaze v5.00.a is possible to use this style.
    I believe that none use ancients versions. If yes they will be just
    labeled as unknown version.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/kernel/cpu/cpuinfo.c
+++ arch/microblaze/kernel/cpu/cpuinfo.c
@@ -30,11 +30,6 @@ const struct cpu_ver_key cpu_ver_lookup[] = {
 	{"7.20.b", 0x0d},
 	{"7.20.c", 0x0e},
 	{"7.20.d", 0x0f},
-	/* FIXME There is no keycode defined in MBV for these versions */
-	{"2.10.a", 0x10},
-	{"3.00.a", 0x20},
-	{"4.00.a", 0x30},
-	{"4.00.b", 0x40},
 	{NULL, 0},
 };
 
