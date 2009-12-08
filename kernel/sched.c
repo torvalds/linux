7272e504cb1f35861e59fade3628f047f897afff
Thomas Gleixner <tglx@linutronix.de>
2009-12-08 20:24:16 +0000

    sched: Protect task->cpus_allowed access in sched_getaffinity()
    commit 3160568371da441b7f2fb57f2f1225404207e8f2 upstream
    
    sched_getaffinity() is not protected against a concurrent
    modification of the tasks affinity.
    
    Serialize the access with task_rq_lock(task).
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    LKML-Reference: <20091208202026.769251187@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Mike Galbraith <efault@gmx.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- kernel/sched.c
+++ kernel/sched.c
@@ -6686,6 +6686,8 @@ SYSCALL_DEFINE3(sched_setaffinity, pid_t, pid, unsigned int, len,
 long sched_getaffinity(pid_t pid, struct cpumask *mask)
 {
 	struct task_struct *p;
+	unsigned long flags;
+	struct rq *rq;
 	int retval;
 
 	get_online_cpus();
@@ -6700,7 +6702,9 @@ long sched_getaffinity(pid_t pid, struct cpumask *mask)
 	if (retval)
 		goto out_unlock;
 
+	rq = task_rq_lock(p, &flags);
 	cpumask_and(mask, &p->cpus_allowed, cpu_online_mask);
+	task_rq_unlock(rq, &flags);
 
 out_unlock:
 	read_unlock(&tasklist_lock);
