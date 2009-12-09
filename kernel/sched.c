1a551ae715825bb2a2107a2dd68de024a1fa4e32
Thomas Gleixner <tglx@linutronix.de>
2009-12-09 10:15:11 +0000

    sched: Use rcu in sched_get_rr_param()
    read_lock(&tasklist_lock) does not protect
    sys_sched_get_rr_param() against a concurrent update of the
    policy or scheduler parameters as do_sched_scheduler() does not
    take the tasklist_lock.
    
    The access to task->sched_class->get_rr_interval is protected by
    task_rq_lock(task).
    
    Use rcu_read_lock() to protect find_task_by_vpid() and prevent
    the task struct from going away.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    LKML-Reference: <20091209100706.862897167@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched.c
+++ kernel/sched.c
@@ -6873,7 +6873,7 @@ SYSCALL_DEFINE2(sched_rr_get_interval, pid_t, pid,
 		return -EINVAL;
 
 	retval = -ESRCH;
-	read_lock(&tasklist_lock);
+	rcu_read_lock();
 	p = find_process_by_pid(pid);
 	if (!p)
 		goto out_unlock;
@@ -6886,13 +6886,13 @@ SYSCALL_DEFINE2(sched_rr_get_interval, pid_t, pid,
 	time_slice = p->sched_class->get_rr_interval(rq, p);
 	task_rq_unlock(rq, &flags);
 
-	read_unlock(&tasklist_lock);
+	rcu_read_unlock();
 	jiffies_to_timespec(time_slice, &t);
 	retval = copy_to_user(interval, &t, sizeof(t)) ? -EFAULT : 0;
 	return retval;
 
 out_unlock:
-	read_unlock(&tasklist_lock);
+	rcu_read_unlock();
 	return retval;
 }
 
