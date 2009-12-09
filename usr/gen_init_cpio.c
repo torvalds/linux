6d87fea4dd7152df4a4605a3846c3bf10f869e0c
Mike Frysinger <vapier@gentoo.org>
2009-12-09 06:55:19 -0500

    gen_init_cpio: fixed fwrite warning
    On compilers with security warnings enabled by default, we get:
    
    usr/gen_init_cpio.c: In function ‘cpio_mkfile’:
    usr/gen_init_cpio.c:357: warning: ignoring return value of ‘fwrite’,
                                      declared with attribute warn_unused_result
    
    So check the return value and handle errors accordingly.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- usr/gen_init_cpio.c
+++ usr/gen_init_cpio.c
@@ -354,7 +354,10 @@ static int cpio_mkfile(const char *name, const char *location,
 		push_pad();
 
 		if (size) {
-			fwrite(filebuf, size, 1, stdout);
+			if (fwrite(filebuf, size, 1, stdout) != 1) {
+				fprintf(stderr, "writing filebuf failed\n");
+				goto error;
+			}
 			offset += size;
 			push_pad();
 		}
