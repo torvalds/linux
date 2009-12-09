fd0775bfc9feb036e6efb669133d644ae29e12b8
Mike Frysinger <vapier@gentoo.org>
2009-12-09 03:40:04 +0000

    smc91x: fix unused flags warnings on UP systems
    Local flags variables will be declared whenever these functions get used,
    but obviously on UP systems the flags parameter won't be touched.  So add
    some dummy ops that get optimized away anyways to satisfy gcc's warnings.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>
    Acked-by: Nicolas Pitre <nico@fluxnic.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/smc91x.c
+++ drivers/net/smc91x.c
@@ -534,9 +534,9 @@ static inline void  smc_rcv(struct net_device *dev)
 #define smc_special_lock(lock, flags)		spin_lock_irqsave(lock, flags)
 #define smc_special_unlock(lock, flags) 	spin_unlock_irqrestore(lock, flags)
 #else
-#define smc_special_trylock(lock, flags)	(1)
-#define smc_special_lock(lock, flags)   	do { } while (0)
-#define smc_special_unlock(lock, flags)	do { } while (0)
+#define smc_special_trylock(lock, flags)	(flags == flags)
+#define smc_special_lock(lock, flags)   	do { flags = 0; } while (0)
+#define smc_special_unlock(lock, flags)	do { flags = 0; } while (0)
 #endif
 
 /*
