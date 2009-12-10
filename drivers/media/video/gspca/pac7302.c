37b372e5ebe187037e01885151a0afb546a38520
MÃ¡rton NÃ©meth <nm127@freemail.hu>
2009-12-10 11:31:09 -0300

    V4L/DVB (13621): gspca - some subdrivers: Make device_table[]s constant.
    Signed-off-by: MÃ¡rton NÃ©meth <nm127@freemail.hu>
    Signed-off-by: Jean-Francois Moine <moinejf@free.fr>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/gspca/pac7302.c
+++ drivers/media/video/gspca/pac7302.c
@@ -1229,7 +1229,7 @@ static struct sd_desc sd_desc = {
 };
 
 /* -- module initialisation -- */
-static __devinitdata struct usb_device_id device_table[] = {
+static const struct usb_device_id device_table[] __devinitconst = {
 	{USB_DEVICE(0x06f8, 0x3009)},
 	{USB_DEVICE(0x093a, 0x2620)},
 	{USB_DEVICE(0x093a, 0x2621)},
@@ -1245,7 +1245,7 @@ static __devinitdata struct usb_device_id device_table[] = {
 MODULE_DEVICE_TABLE(usb, device_table);
 
 /* -- device connect -- */
-static int sd_probe(struct usb_interface *intf,
+static int __devinit sd_probe(struct usb_interface *intf,
 			const struct usb_device_id *id)
 {
 	return gspca_dev_probe(intf, id, &sd_desc, sizeof(struct sd),
