26a63f6af58144818d4979cce1cbb8e49329f732
Juuso Oikarinen <juuso.oikarinen@nokia.com>
2009-12-11 15:41:03 +0200

    wl1271: Remove beacon-loss-ind from PSM entry failure handling
    Remove the beacon-loss indication to stack from PSM entry failure handling -
    this will cause more problems than it will solve due to the design of the
    mac80211.
    
    Signed-off-by: Juuso Oikarinen <juuso.oikarinen@nokia.com>
    Reviewed-by: Luciano Coelho <luciano.coelho@nokia.com>
    Signed-off-by: Luciano Coelho <luciano.coelho@nokia.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/wl1271_event.c
+++ drivers/net/wireless/wl12xx/wl1271_event.c
@@ -89,7 +89,6 @@ static int wl1271_event_ps_report(struct wl1271 *wl,
 		} else {
 			wl1271_error("PSM entry failed, giving up.\n");
 			wl->psm_entry_retry = 0;
-			*beacon_loss = true;
 		}
 		break;
 	case EVENT_ENTER_POWER_SAVE_SUCCESS:
