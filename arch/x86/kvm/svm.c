953899b659adce62cbe83d6a7527550ab8797c48
Joerg Roedel <joerg.roedel@amd.com>
2009-12-14 12:22:20 +0100

    KVM: SVM: Adjust tsc_offset only if tsc_unstable
    The tsc_offset adjustment in svm_vcpu_load is executed
    unconditionally even if Linux considers the host tsc as
    stable. This causes a Linux guest detecting an unstable tsc
    in any case.
    This patch removes the tsc_offset adjustment if the host tsc
    is stable. The guest will now get the benefit of a stable
    tsc too.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -765,14 +765,16 @@ static void svm_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 	if (unlikely(cpu != vcpu->cpu)) {
 		u64 delta;
 
-		/*
-		 * Make sure that the guest sees a monotonically
-		 * increasing TSC.
-		 */
-		delta = vcpu->arch.host_tsc - native_read_tsc();
-		svm->vmcb->control.tsc_offset += delta;
-		if (is_nested(svm))
-			svm->nested.hsave->control.tsc_offset += delta;
+		if (check_tsc_unstable()) {
+			/*
+			 * Make sure that the guest sees a monotonically
+			 * increasing TSC.
+			 */
+			delta = vcpu->arch.host_tsc - native_read_tsc();
+			svm->vmcb->control.tsc_offset += delta;
+			if (is_nested(svm))
+				svm->nested.hsave->control.tsc_offset += delta;
+		}
 		vcpu->cpu = cpu;
 		kvm_migrate_timers(vcpu);
 		svm->asid_generation = 0;
