4614a696bd1c3a9af3a08f0e5874830a85b889d4
john stultz <johnstul@us.ibm.com>
2009-12-14 18:00:05 -0800

    procfs: allow threads to rename siblings via /proc/pid/tasks/tid/comm
    Setting a thread's comm to be something unique is a very useful ability
    and is helpful for debugging complicated threaded applications.  However
    currently the only way to set a thread name is for the thread to name
    itself via the PR_SET_NAME prctl.
    
    However, there may be situations where it would be advantageous for a
    thread dispatcher to be naming the threads its managing, rather then
    having the threads self-describe themselves.  This sort of behavior is
    available on other systems via the pthread_setname_np() interface.
    
    This patch exports a task's comm via proc/pid/comm and
    proc/pid/task/tid/comm interfaces, and allows thread siblings to write to
    these values.
    
    [akpm@linux-foundation.org: cleanups]
    Signed-off-by: John Stultz <johnstul@us.ibm.com>
    Cc: Andi Kleen <andi@firstfloor.org>
    Cc: Arjan van de Ven <arjan@infradead.org>
    Cc: Mike Fulton <fultonm@ca.ibm.com>
    Cc: Sean Foley <Sean_Foley@ca.ibm.com>
    Cc: Darren Hart <dvhltc@us.ibm.com>
    Cc: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/exec.c
+++ fs/exec.c
@@ -923,6 +923,15 @@ char *get_task_comm(char *buf, struct task_struct *tsk)
 void set_task_comm(struct task_struct *tsk, char *buf)
 {
 	task_lock(tsk);
+
+	/*
+	 * Threads may access current->comm without holding
+	 * the task lock, so write the string carefully.
+	 * Readers without a lock may see incomplete new
+	 * names but are safe from non-terminating string reads.
+	 */
+	memset(tsk->comm, 0, TASK_COMM_LEN);
+	wmb();
 	strlcpy(tsk->comm, buf, sizeof(tsk->comm));
 	task_unlock(tsk);
 	perf_event_comm(tsk);
