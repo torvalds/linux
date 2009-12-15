28fddb7cd5b7a12b81c8ec1a358749e60760e741
Manu Abraham <abraham.manu@gmail.com>
2009-12-15 09:01:27 -0300

    V4L/DVB (13810): [MB86A16] Use DVB_* macros
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/mb86a16.h
+++ drivers/media/dvb/frontends/mb86a16.h
@@ -31,8 +31,22 @@ struct mb86a16_config {
 	int (*set_voltage)(struct dvb_frontend *fe, fe_sec_voltage_t voltage);
 };
 
+
+
+#if defined(CONFIG_DVB_MB86A16) || (defined(CONFIG_DVB_MB86A16_MODULE) && defined(MODULE))
+
 extern struct dvb_frontend *mb86a16_attach(const struct mb86a16_config *config,
 					   struct i2c_adapter *i2c_adap);
 
+#else
+
+static inline struct dvb_frontend *mb86a16_attach(const struct mb86a16_config *config,
+					   struct i2c_adapter *i2c_adap)
+{
+	printk(KERN_WARNING "%s: Driver disabled by Kconfig\n", __func__);
+	return NULL;
+}
+
+#endif /* CONFIG_DVB_MB86A16 */
 
 #endif /* __MB86A16_H */
