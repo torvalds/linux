3e26120cc7c819c97bc07281ca1fb9017cfe9a39
WANG Cong <amwang@redhat.com>
2009-12-17 15:27:05 -0800

    kernel/sysctl.c: fix the incomplete part of sysctl_max_map_count-should-be-non-negative.patch
    It is a mistake that we used 'proc_dointvec', it should be
    'proc_dointvec_minmax', as in the original patch.
    
    Signed-off-by: WANG Cong <amwang@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1131,7 +1131,7 @@ static struct ctl_table vm_table[] = {
 		.data		= &sysctl_max_map_count,
 		.maxlen		= sizeof(sysctl_max_map_count),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec,
+		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &zero,
 	},
 #else
