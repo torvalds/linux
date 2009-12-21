e93166f10c741f247c7e172936811bad558b4135
Joerg Roedel <joerg.roedel@amd.com>
2009-12-21 15:51:23 +0100

    x86/amd-iommu: Fix initialization failure panic
    commit 0f764806438d5576ac58898332e5dcf30bb8a679 upstream.
    
    The assumption that acpi_table_parse passes the return value
    of the hanlder function to the caller proved wrong
    recently. The return value of the handler function is
    totally ignored. This makes the initialization code for AMD
    IOMMU buggy in a way that could cause a kernel panic on
    initialization. This patch fixes the issue in the AMD IOMMU
    driver.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- arch/x86/kernel/amd_iommu_init.c
+++ arch/x86/kernel/amd_iommu_init.c
@@ -136,6 +136,11 @@ LIST_HEAD(amd_iommu_list);		/* list of all AMD IOMMUs in the
 					   system */
 
 /*
+ * Set to true if ACPI table parsing and hardware intialization went properly
+ */
+static bool amd_iommu_initialized;
+
+/*
  * Pointer to the device table which is shared by all AMD IOMMUs
  * it is indexed by the PCI device id or the HT unit id and contains
  * information about the domain the device belongs to as well as the
@@ -913,6 +918,8 @@ static int __init init_iommu_all(struct acpi_table_header *table)
 	}
 	WARN_ON(p != end);
 
+	amd_iommu_initialized = true;
+
 	return 0;
 }
 
@@ -1261,6 +1268,9 @@ int __init amd_iommu_init(void)
 	 */
 	ret = -ENODEV;
 	if (acpi_table_parse("IVRS", init_iommu_all) != 0)
+		goto free;
+
+	if (!amd_iommu_initialized)
 		goto free;
 
 	if (acpi_table_parse("IVRS", init_memory_definitions) != 0)
