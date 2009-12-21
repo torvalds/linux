3a8a3b1cee6c525661732b8bcf29ac63d42945ed
Bryan Wu <cooloney@kernel.org>
2009-12-21 10:43:06 -0500

    USB: gadget: use ep5 for bulk-in and ep6 for bulk-out for Blackfin MUSB
    Normally, the musb uses ep1 as the bidirectional bulk endpoint.  This won't
    work on the Blackfin musb as all endpoints (except ep0) are unidirectional.
    Further, ep1-ep4 have a small 128 byte FIFO which makes them undesirable
    for bulk endpoints (which need more like a 512 byte FIFO).  This leaves us
    with ep5-ep7 which have 1024 byte FIFOs and can be configured as either
    in/out and bulk/interrupt/iso on the fly.
    
    Signed-off-by: Bryan Wu <cooloney@kernel.org>
    Signed-off-by: Cliff Cai <cliff.cai@analog.com>
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/epautoconf.c
+++ drivers/usb/gadget/epautoconf.c
@@ -275,6 +275,20 @@ struct usb_ep * __init usb_ep_autoconfig (
 		ep = find_ep (gadget, "ep1-bulk");
 		if (ep && ep_matches (gadget, ep, desc))
 			return ep;
+
+#ifdef CONFIG_BLACKFIN
+	} else if (gadget_is_musbhsfc(gadget) || gadget_is_musbhdrc(gadget)) {
+		if ((USB_ENDPOINT_XFER_BULK == type) ||
+		    (USB_ENDPOINT_XFER_ISOC == type)) {
+			if (USB_DIR_IN & desc->bEndpointAddress)
+				ep = find_ep (gadget, "ep5in");
+			else
+				ep = find_ep (gadget, "ep6out");
+		} else
+			ep = NULL;
+		if (ep && ep_matches (gadget, ep, desc))
+			return ep;
+#endif
 	}
 
 	/* Second, look at endpoints until an unclaimed one looks usable */
