99e9e4b6bc9ea7e1324064b18530ffe8002dfec5
Gertjan van Wingerde <gwingerde@gmail.com>
2009-12-30 11:36:30 +0100

    rt2x00: Properly request tx headroom for alignment operations.
    commit 7a4a77b7771164d61ce702a588067d1e1d66db7c upstream.
    
    Current rt2x00 drivers may result in a "ieee80211_tx_status: headroom too
    small" error message when a frame needs to be properly aligned before
    transmitting it.
    This is because the space needed to ensure proper alignment isn't
    requested from mac80211.
    Fix this by adding sufficient amount of alignment space to the amount
    of headroom requested for TX frames.
    
    Reported-by: David Ellingsworth <david@identd.dyndns.org>
    Signed-off-by: Gertjan van Wingerde <gwingerde@gmail.com>
    Acked-by: Ivo van Doorn <ivdoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/net/wireless/rt2x00/rt2x00dev.c
+++ drivers/net/wireless/rt2x00/rt2x00dev.c
@@ -686,7 +686,17 @@ static int rt2x00lib_probe_hw(struct rt2x00_dev *rt2x00dev)
 	/*
 	 * Initialize extra TX headroom required.
 	 */
-	rt2x00dev->hw->extra_tx_headroom = rt2x00dev->ops->extra_tx_headroom;
+	rt2x00dev->hw->extra_tx_headroom =
+		max_t(unsigned int, IEEE80211_TX_STATUS_HEADROOM,
+		      rt2x00dev->ops->extra_tx_headroom);
+
+	/*
+	 * Take TX headroom required for alignment into account.
+	 */
+	if (test_bit(DRIVER_REQUIRE_L2PAD, &rt2x00dev->flags))
+		rt2x00dev->hw->extra_tx_headroom += RT2X00_L2PAD_SIZE;
+	else if (test_bit(DRIVER_REQUIRE_DMA, &rt2x00dev->flags))
+		rt2x00dev->hw->extra_tx_headroom += RT2X00_ALIGN_SIZE;
 
 	/*
 	 * Register HW.
