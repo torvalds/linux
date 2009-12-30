fc5f75773c0b3c5b44785e4efcc54c5f496211a9
Kalle Valo <kalle.valo@nokia.com>
2009-12-30 15:54:03 +0200

    mac80211: fix ieee80211_change_mac() to use struct sockaddr
    Setting the mac address from user space was buggy. For example, when
    executing this command:
    
    ip link set wlan0 address 00:1f:df:88:cd:55
    
    mac80211 used the address 01:00:00:1f:df:88 instead. It was shifted two
    bytes.
    
    The reason was that the addr (type of void *) provided to
    ieee80211_change_mac() is actually of type struct sockaddr, not just the
    mac address array. Also the call to eth_mac_addr() expects the address to
    be struct sockaddr.
    
    Signed-off-by: Kalle Valo <kalle.valo@nokia.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/iface.c
+++ net/mac80211/iface.c
@@ -63,15 +63,16 @@ static int ieee80211_change_mtu(struct net_device *dev, int new_mtu)
 static int ieee80211_change_mac(struct net_device *dev, void *addr)
 {
 	struct ieee80211_sub_if_data *sdata = IEEE80211_DEV_TO_SUB_IF(dev);
+	struct sockaddr *sa = addr;
 	int ret;
 
 	if (ieee80211_sdata_running(sdata))
 		return -EBUSY;
 
-	ret = eth_mac_addr(dev, addr);
+	ret = eth_mac_addr(dev, sa);
 
 	if (ret == 0)
-		memcpy(sdata->vif.addr, addr, ETH_ALEN);
+		memcpy(sdata->vif.addr, sa->sa_data, ETH_ALEN);
 
 	return ret;
 }
