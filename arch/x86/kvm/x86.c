f9a48e6a18c210c4bf34769fa05ede250575c6a1
Avi Kivity <avi@redhat.com>
2010-01-06 19:10:22 +0200

    KVM: Set cr0.et when the guest writes cr0
    Follow the hardware.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -428,6 +428,8 @@ out:
 
 void kvm_set_cr0(struct kvm_vcpu *vcpu, unsigned long cr0)
 {
+	cr0 |= X86_CR0_ET;
+
 	if (cr0 & CR0_RESERVED_BITS) {
 		printk(KERN_DEBUG "set_cr0: 0x%lx #GP, reserved bits 0x%lx\n",
 		       cr0, kvm_read_cr0(vcpu));
