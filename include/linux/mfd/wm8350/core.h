38f6ce45f0bca04ac653c57cacd375c469995321
Mark Brown <broonie@opensource.wolfsonmicro.com>
2010-01-07 16:10:08 +0000

    gpiolib: Add support for WM8350 GPIO controller
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- include/linux/mfd/wm8350/core.h
+++ include/linux/mfd/wm8350/core.h
@@ -645,11 +645,13 @@ struct wm8350 {
  *        used by the platform to configure GPIO functions and similar.
  * @irq_high: Set if WM8350 IRQ is active high.
  * @irq_base: Base IRQ for genirq (not currently used).
+ * @gpio_base: Base for gpiolib.
  */
 struct wm8350_platform_data {
 	int (*init)(struct wm8350 *wm8350);
 	int irq_high;
 	int irq_base;
+	int gpio_base;
 };
 
 
