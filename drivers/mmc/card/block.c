7d92df692994472cab6045bbd9d0e2c4afa4365f
Anna Lemehova <EXT-Anna.Lemehova@nokia.com>
2010-01-08 14:42:58 -0800

    mmc_block: add dev_t initialization check
    When a card is removed before mmc_blk_probe() has called add_disk(), then
    the minor field is uninitialized and has value 0.  This caused
    mmc_blk_put() to always release devidx 0 even if 0 was still in use.  Then
    the next mmc_blk_probe() used the first free idx of 0, which oopses in
    sysfs, since it is used by another card.
    
    Signed-off-by: Anna Lemehova <EXT-Anna.Lemehova@nokia.com>
    Signed-off-by: Adrian Hunter <adrian.hunter@nokia.com>
    Cc: <linux-mmc@vger.kernel.org>
    Cc: <stable@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -85,7 +85,12 @@ static void mmc_blk_put(struct mmc_blk_data *md)
 	mutex_lock(&open_lock);
 	md->usage--;
 	if (md->usage == 0) {
+		int devmaj = MAJOR(disk_devt(md->disk));
 		int devidx = MINOR(disk_devt(md->disk)) >> MMC_SHIFT;
+
+		if (!devmaj)
+			devidx = md->disk->first_minor >> MMC_SHIFT;
+
 		__clear_bit(devidx, dev_use);
 
 		put_disk(md->disk);
