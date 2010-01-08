3e3ee1560daaf09bb5dbe3afd35ca2f75e2e116a
Tuukka Toivonen <tuukka.o.toivonen@nokia.com>
2010-01-08 15:23:08 -0700

    OMAP3 clock: Add capability to change rate of dpll4_m5_ck
    Add necessary definitions to clock framework to allow changing
    dpll4_m5_ck rate.  This is used by the camera code.
    
    Signed-off-by: Jouni Högander <jouni.hogander@nokia.com>
    Signed-off-by: Tuukka Toivonen <tuukka.o.toivonen@nokia.com>
    Signed-off-by: Paul Walmsley <paul@pwsan.com>

--- arch/arm/mach-omap2/clock34xx_data.c
+++ arch/arm/mach-omap2/clock34xx_data.c
@@ -776,6 +776,8 @@ static struct clk dpll4_m5_ck = {
 	.clksel_mask	= OMAP3430_CLKSEL_CAM_MASK,
 	.clksel		= div16_dpll4_clksel,
 	.clkdm_name	= "dpll4_clkdm",
+	.set_rate	= &omap2_clksel_set_rate,
+	.round_rate	= &omap2_clksel_round_rate,
 	.recalc		= &omap2_clksel_recalc,
 };
 
