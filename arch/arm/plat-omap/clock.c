4e37c10d8a721b19933491df7af296aac9281004
Paul Walmsley <paul@pwsan.com>
2010-01-08 15:23:16 -0700

    OMAP clock/CPUFreq: add clk_exit_cpufreq_table()
    A subsequent patch adds code on OMAP2xxx to dynamically allocate the
    CPUFreq frequency table in clk_init_cpufreq_table(), so for it to
    avoid a leak, it will need a corresponding function to free the
    memory.  This patch adds clk_exit_cpufreq_table() with generic
    code to call a chip-specific variant inside the clockfw_lock spinlock via
    struct clk_functions.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>

--- arch/arm/plat-omap/clock.c
+++ arch/arm/plat-omap/clock.c
@@ -329,6 +329,16 @@ void clk_init_cpufreq_table(struct cpufreq_frequency_table **table)
 		arch_clock->clk_init_cpufreq_table(table);
 	spin_unlock_irqrestore(&clockfw_lock, flags);
 }
+
+void clk_exit_cpufreq_table(struct cpufreq_frequency_table **table)
+{
+	unsigned long flags;
+
+	spin_lock_irqsave(&clockfw_lock, flags);
+	if (arch_clock->clk_exit_cpufreq_table)
+		arch_clock->clk_exit_cpufreq_table(table);
+	spin_unlock_irqrestore(&clockfw_lock, flags);
+}
 #endif
 
 /*-------------------------------------------------------------------------*/
