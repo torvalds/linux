e00d9d4b1796dae3adf6e39c9a766e760dd1c796
Mikael Pettersson <mikpe@it.uu.se>
2010-01-09 13:03:59 +0100

    ARM: 5869/1: ixp4xx: implement sched_clock()
    Add a better sched_clock() to the ixp4xx platform,
    implemented via its clocksource support.
    
    This is based on the sched_clock() I implemented for
    the IOP platform. Tested on a ds101 ixp420 machine.
    
    Signed-off-by: Mikael Pettersson <mikpe@it.uu.se>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-ixp4xx/common.c
+++ arch/arm/mach-ixp4xx/common.c
@@ -427,6 +427,17 @@ static void __init ixp4xx_clocksource_init(void)
 }
 
 /*
+ * sched_clock()
+ */
+unsigned long long sched_clock(void)
+{
+	cycle_t cyc = ixp4xx_get_cycles(NULL);
+	struct clocksource *cs = &clocksource_ixp4xx;
+
+	return clocksource_cyc2ns(cyc, cs->mult, cs->shift);
+}
+
+/*
  * clockevents
  */
 static int ixp4xx_set_next_event(unsigned long evt,
