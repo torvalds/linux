722b3654852e48b93367a63f8ada9ee1cd43f2d3
Suresh Siddha <suresh.b.siddha@intel.com>
2010-01-13 16:19:10 -0800

    x86, vmi: Fix vmi_get_timer_vector() to use IRQ0_VECTOR
    FIRST_DEVICE_VECTOR is going away and it looks like a bad hack to steal
    FIRST_DEVICE_VECTOR / FIRST_EXTERNAL_VECTOR, when it looks like it needs
    IRQ0_VECTOR.
    
    Fix vmi_get_timer_vector() to use IRQ0_VECTOR.
    
    Signed-off-by: Suresh Siddha <suresh.b.siddha@intel.com>
    LKML-Reference: <20100114002118.436172066@sbs-t61.sc.intel.com>
    Cc: Alok N Kataria <akataria@vmware.com>
    Cc: Zach Amsden <zach@vmware.com>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/kernel/vmiclock_32.c
+++ arch/x86/kernel/vmiclock_32.c
@@ -79,11 +79,7 @@ unsigned long vmi_tsc_khz(void)
 
 static inline unsigned int vmi_get_timer_vector(void)
 {
-#ifdef CONFIG_X86_IO_APIC
-	return FIRST_DEVICE_VECTOR;
-#else
-	return FIRST_EXTERNAL_VECTOR;
-#endif
+	return IRQ0_VECTOR;
 }
 
 /** vmi clockchip */
@@ -239,8 +235,6 @@ void __init vmi_time_init(void)
 
 	vmi_time_init_clockevent();
 	setup_irq(0, &vmi_clock_action);
-	for_each_possible_cpu(cpu)
-		per_cpu(vector_irq, cpu)[vmi_get_timer_vector()] = 0;
 }
 
 #ifdef CONFIG_X86_LOCAL_APIC
