7a15cbd40108a670baae71cbd3cec62b65891fa6
Dave Airlie <airlied@redhat.com>
2010-01-14 11:42:17 +1000

    drm/radeon/kms: fix displayport->dvi connector DDC.
    It appears that attempting AUXCH DDC breaks the subsequent attempt
    to do DDC over the i2c lines, so use the sink type to determine
    if we should be doing AUXCH or i2c DDC.
    
    This fixes my DVI monitor plugged into DP->DVI convertor.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/radeon_display.c
+++ drivers/gpu/drm/radeon/radeon_display.c
@@ -357,7 +357,8 @@ int radeon_ddc_get_modes(struct radeon_connector *radeon_connector)
 	if ((radeon_connector->base.connector_type == DRM_MODE_CONNECTOR_DisplayPort) ||
 	    (radeon_connector->base.connector_type == DRM_MODE_CONNECTOR_eDP)) {
 		struct radeon_connector_atom_dig *dig = radeon_connector->con_priv;
-		if (dig->dp_i2c_bus)
+		if ((dig->dp_sink_type == CONNECTOR_OBJECT_ID_DISPLAYPORT ||
+		     dig->dp_sink_type == CONNECTOR_OBJECT_ID_eDP) && dig->dp_i2c_bus)
 			radeon_connector->edid = drm_get_edid(&radeon_connector->base, &dig->dp_i2c_bus->adapter);
 	}
 	if (!radeon_connector->ddc_bus)
