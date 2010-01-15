d5e528136cda31a32ff7d1eaa8d06220eb443781
Alexander Graf <agraf@suse.de>
2010-01-15 14:49:10 +0100

    KVM: PPC: Add helper functions to call real mode loaders
    Linux contains quite some bits of code to load FPU, Altivec and VSX lazily for
    a task. It calls those bits in real mode, coming from an interrupt handler.
    
    For KVM we better reuse those, so let's wrap a bit of trampoline magic around
    them and then we can call them from normal module code.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/powerpc/include/asm/kvm_book3s.h
+++ arch/powerpc/include/asm/kvm_book3s.h
@@ -124,6 +124,9 @@ extern void kvmppc_set_bat(struct kvm_vcpu *vcpu, struct kvmppc_bat *bat,
 extern u32 kvmppc_trampoline_lowmem;
 extern u32 kvmppc_trampoline_enter;
 extern void kvmppc_rmcall(ulong srr0, ulong srr1);
+extern void kvmppc_load_up_fpu(void);
+extern void kvmppc_load_up_altivec(void);
+extern void kvmppc_load_up_vsx(void);
 
 static inline struct kvmppc_vcpu_book3s *to_book3s(struct kvm_vcpu *vcpu)
 {
