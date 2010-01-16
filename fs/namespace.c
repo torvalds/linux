27d55f1f4c190b14092fcca3069c7d15df83514f
Al Viro <viro@zeniv.linux.org.uk>
2010-01-16 13:07:36 -0500

    do_add_mount() should sanitize mnt_flags
    MNT_WRITE_HOLD shouldn't leak into new vfsmount and neither
    should MNT_SHARED (the latter will be set properly, along with
    the rest of shared-subtree data structures)
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1671,6 +1671,8 @@ int do_add_mount(struct vfsmount *newmnt, struct path *path,
 {
 	int err;
 
+	mnt_flags &= ~(MNT_SHARED | MNT_WRITE_HOLD);
+
 	down_write(&namespace_sem);
 	/* Something was mounted here while we slept */
 	while (d_mountpoint(path->dentry) &&
