7d60a044c8226d86fa93c2cf90ccdb97e3ba28fa
Russell King <rmk+kernel@arm.linux.org.uk>
2010-01-17 20:23:57 +0000

    ARM: Integrator: allow IO_ADDRESS() to be used for register addresses
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-integrator/include/mach/hardware.h
+++ arch/arm/mach-integrator/include/mach/hardware.h
@@ -35,17 +35,17 @@
 #define PCIO_BASE		PCI_IO_VADDR
 #define PCIMEM_BASE		PCI_MEMORY_VADDR
 
-#ifdef CONFIG_MMU
-/* macro to get at IO space when running virtually */
-#define IO_ADDRESS(x) (((x) >> 4) + IO_BASE) 
-#else
-#define IO_ADDRESS(x) (x)
-#endif
-
 #define pcibios_assign_all_busses()	1
 
 #define PCIBIOS_MIN_IO		0x6000
 #define PCIBIOS_MIN_MEM 	0x00100000
+
+/* macro to get at IO space when running virtually */
+#ifdef CONFIG_MMU
+#define IO_ADDRESS(x)	(((x) & 0x000fffff) | (((x) >> 4) & 0x0ff00000) | IO_BASE)
+#else
+#define IO_ADDRESS(x)	(x)
+#endif
 
 #define __io_address(n)		((void __iomem *)IO_ADDRESS(n))
 
