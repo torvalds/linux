659431fcafd820cc426afedadcc4548933224985
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-01-18 16:02:48 +0100

    fix typos "precidence" -> "precedence" in comments
    This patch was generated by
    
    	git grep -E -i -l 'precidence' | xargs -r perl -p -i -e 's/precidence/precedence/'
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- arch/arm/common/clkdev.c
+++ arch/arm/common/clkdev.c
@@ -32,7 +32,7 @@ static DEFINE_MUTEX(clocks_mutex);
  *  If an entry has a device ID, it must match
  *  If an entry has a connection ID, it must match
  * Then we take the most specific entry - with the following
- * order of precidence: dev+con > dev only > con only.
+ * order of precedence: dev+con > dev only > con only.
  */
 static struct clk *clk_find(const char *dev_id, const char *con_id)
 {
