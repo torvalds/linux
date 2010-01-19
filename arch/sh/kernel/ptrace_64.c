d6db8888c8957fbdcd611e1321a6f6a0d6fb7e15
Paul Mundt <lethal@linux-sh.org>
2010-01-19 15:55:27 +0900

    sh64: Use the shared FPU state restorer.
    This kills off the sh64-specific state restorer and switches over to
    the generic one.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/ptrace_64.c
+++ arch/sh/kernel/ptrace_64.c
@@ -114,8 +114,7 @@ put_fpu_long(struct task_struct *task, unsigned long addr, unsigned long data)
 	regs = (struct pt_regs*)((unsigned char *)task + THREAD_SIZE) - 1;
 
 	if (!tsk_used_math(task)) {
-		fpinit(&task->thread.xstate->hardfpu);
-		set_stopped_child_used_math(task);
+		init_fpu(task);
 	} else if (last_task_used_math == task) {
 		enable_fpu();
 		save_fpu(task);
