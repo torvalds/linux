c5f800656bc985b448b1d848d309648826536543
Erik Gilling <konkers@android.com>
2010-01-21 16:53:02 -0800

    [ARM] tegra: initial tegra support
    v2: Fixes from Mike Rapoport
    	- remove unused header files (mach/dma.h and mach/nand.h)
    	- remove tegra 1 references from Makefile.boot
    
    v2: fixes from Russell King
    	- remove mach/io.h include from mach/iomap.h
    	- fix whitespace in Kconfig
    
    v2: from Colin Cross
    	- fix invalid immediate in debug-macro.S
    
    v3:
    	- allow selection of multiple boards
    
    Signed-off-by: Colin Cross <ccross@android.com>
    Signed-off-by: Erik Gilling <konkers@android.com>

---
+++ arch/arm/mach-tegra/common.c
@@ -0,0 +1,44 @@
+/*
+ * arch/arm/mach-tegra/board-harmony.c
+ *
+ * Copyright (C) 2010 Google, Inc.
+ *
+ * Author:
+ *	Colin Cross <ccross@android.com>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <linux/init.h>
+#include <linux/io.h>
+
+#include <asm/hardware/cache-l2x0.h>
+
+#include <mach/iomap.h>
+
+#include "board.h"
+
+void __init tegra_init_cache(void)
+{
+#ifdef CONFIG_CACHE_L2X0
+	void __iomem *p = IO_ADDRESS(TEGRA_ARM_PERIF_BASE) + 0x3000;
+
+	writel(0x331, p + L2X0_TAG_LATENCY_CTRL);
+	writel(0x441, p + L2X0_DATA_LATENCY_CTRL);
+
+	l2x0_init(p, 0x6C080001, 0x8200c3fe);
+#endif
+}
+
+void __init tegra_common_init(void)
+{
+	tegra_init_cache();
+}
