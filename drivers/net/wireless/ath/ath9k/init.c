658965107e2a45a1a4297cb494adc55797dd9ad1
Felix Fietkau <nbd@openwrt.org>
2010-01-24 03:26:11 +0100

    ath9k: improve max rate retry handling
    ath9k currently forces hw->max_rate_tries to 4 to work around rate
    control inefficiencies. This has some negative side effects, such as
    rate_control_send_low also using a maximum of 4 tries, which could
    negatively affect reliability of unicast management frames.
    This patch pushes the retry limit to the rate control instead, and
    allows it to use more tries on the last stage to prevent unnecessary
    packet loss.
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/init.c
+++ drivers/net/wireless/ath/ath9k/init.c
@@ -640,8 +640,7 @@ void ath9k_set_hw_capab(struct ath_softc *sc, struct ieee80211_hw *hw)
 	hw->max_rates = 4;
 	hw->channel_change_time = 5000;
 	hw->max_listen_interval = 10;
-	/* Hardware supports 10 but we use 4 */
-	hw->max_rate_tries = 4;
+	hw->max_rate_tries = 10;
 	hw->sta_data_size = sizeof(struct ath_node);
 	hw->vif_data_size = sizeof(struct ath_vif);
 
