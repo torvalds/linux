5b520259ab6d661b8d5eb39dd17cc5e4e4553c62
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2010-01-25 11:07:19 +0900

    USB: s3c-hsotg: replace deprecated dma_sync_single()
    This replaces deprecated dma_sync_single() with dma_sync_single_for_cpu().
    
    There is no functional change because dma_sync_single() simply calls
    dma_sync_single_for_cpu():
    
    static inline void __deprecated dma_sync_single(struct device *dev,
           	      	   				dma_addr_t addr, size_t size,
    						enum dma_data_direction dir)
    {
    	dma_sync_single_for_cpu(dev, addr, size, dir);
    }
    
    This fixes the following compile warnings:
    
    drivers/usb/gadget/s3c-hsotg.c: In function 's3c_hsotg_unmap_dma':
    drivers/usb/gadget/s3c-hsotg.c:376: warning: 'dma_sync_single' is deprecated (declared at /home/fujita/git/linux-2.6/include/linux/dma-mapping.h:109)
    drivers/usb/gadget/s3c-hsotg.c: In function 's3c_hsotg_map_dma':
    drivers/usb/gadget/s3c-hsotg.c:758: warning: 'dma_sync_single' is deprecated (declared at /home/fujita/git/linux-2.6/include/linux/dma-mapping.h:109)
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Acked-by: Ben Dooks <ben@simtec.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/s3c-hsotg.c
+++ drivers/usb/gadget/s3c-hsotg.c
@@ -374,7 +374,7 @@ static void s3c_hsotg_unmap_dma(struct s3c_hsotg *hsotg,
 		req->dma = DMA_ADDR_INVALID;
 		hs_req->mapped = 0;
 	} else {
-		dma_sync_single(hsotg->dev, req->dma, req->length, dir);
+		dma_sync_single_for_cpu(hsotg->dev, req->dma, req->length, dir);
 	}
 }
 
@@ -756,7 +756,7 @@ static int s3c_hsotg_map_dma(struct s3c_hsotg *hsotg,
 		hs_req->mapped = 1;
 		req->dma = dma;
 	} else {
-		dma_sync_single(hsotg->dev, req->dma, req->length, dir);
+		dma_sync_single_for_cpu(hsotg->dev, req->dma, req->length, dir);
 		hs_req->mapped = 0;
 	}
 
