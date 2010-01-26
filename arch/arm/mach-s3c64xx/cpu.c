6ce8fde296e56cda9d7416e015ed2fe495c9c48c
Ben Dooks <ben-linux@fluff.org>
2010-01-26 14:32:09 +0900

    ARM: S3C64XX: Merge s3c6400-init.c into cpu.c
    Since this file is small, and is compiled for both systems in this
    architecture merge it into the cpu support file and remove the original
    instead of moving it.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/mach-s3c64xx/cpu.c
+++ arch/arm/mach-s3c64xx/cpu.c
@@ -129,6 +129,12 @@ static struct sys_device s3c64xx_sysdev = {
 	.cls	= &s3c64xx_sysclass,
 };
 
+/* uart registration process */
+
+void __init s3c6400_common_init_uarts(struct s3c2410_uartcfg *cfg, int no)
+{
+	s3c24xx_init_uartdevs("s3c6400-uart", s3c64xx_uart_resources, cfg, no);
+}
 
 /* read cpu identification code */
 
