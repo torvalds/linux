1eaa4f2762899d2f3c54ba7a4fee564e255ad0a5
Baruch Siach <baruch@tkos.co.il>
2010-01-27 15:00:49 +0200

    mx25pdk: platform code for the DryIce RTC module
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-mx25/mx25pdk.c
+++ arch/arm/mach-mx25/mx25pdk.c
@@ -91,6 +91,7 @@ static void __init mx25pdk_init(void)
 	mxc_register_device(&mxc_uart_device0, &uart_pdata);
 	mxc_register_device(&mxc_usbh2, NULL);
 	mxc_register_device(&mxc_nand_device, &mx25pdk_nand_board_info);
+	mxc_register_device(&mx25_rtc_device, NULL);
 
 	mx25pdk_fec_reset();
 	mxc_register_device(&mx25_fec_device, &mx25_fec_pdata);
