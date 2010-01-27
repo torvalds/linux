fa55e30bc35bf4ec5a7304a537c0ce5438e908e2
Alexander Schmidt <alexs@linux.vnet.ibm.com>
2010-01-27 17:03:08 +0000

    IB/ehca: Allow access for ib_query_qp()
    The max_dest_rd_atomic and max_qp_rd_atomic values are properly
    returned by query_qp(), so there should not be an error returned when
    they are queried.
    
    Signed-off-by: Alexander Schmidt <alexs@linux.vnet.ibm.com>
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/hw/ehca/ehca_qp.c
+++ drivers/infiniband/hw/ehca/ehca_qp.c
@@ -55,9 +55,7 @@ static struct kmem_cache *qp_cache;
 /*
  * attributes not supported by query qp
  */
-#define QP_ATTR_QUERY_NOT_SUPPORTED (IB_QP_MAX_DEST_RD_ATOMIC | \
-				     IB_QP_MAX_QP_RD_ATOMIC   | \
-				     IB_QP_ACCESS_FLAGS       | \
+#define QP_ATTR_QUERY_NOT_SUPPORTED (IB_QP_ACCESS_FLAGS       | \
 				     IB_QP_EN_SQD_ASYNC_NOTIFY)
 
 /*
