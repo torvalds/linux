d1a31038aadf0fb13befb444e60d96f3618766d0
David Härdeman <david@hardeman.nu>
2010-01-28 21:02:54 +0100

    x86: Add quirk for Intel DG45FC board to avoid low memory corruption
    commit 7c099ce1575126395f186ecf58b51a60d5c3be7d upstream.
    
    Commit 6aa542a694dc9ea4344a8a590d2628c33d1b9431 added a quirk for the
    Intel DG45ID board due to low memory corruption. The Intel DG45FC
    shares the same BIOS (and the same bug) as noted in:
    
      http://bugzilla.kernel.org/show_bug.cgi?id=13736
    
    Signed-off-by: David Härdeman <david@hardeman.nu>
    LKML-Reference: <20100128200254.GA9134@hardeman.nu>
    Cc: Alexey Fisher <bug-track@fisher-privat.net>
    Cc: ykzhao <yakui.zhao@intel.com>
    Cc: Tony Bones <aabonesml@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -667,17 +667,25 @@ static struct dmi_system_id __initdata bad_bios_dmi_table[] = {
 			DMI_MATCH(DMI_BIOS_VENDOR, "Phoenix/MSC"),
 		},
 	},
-	{
 	/*
-	 * AMI BIOS with low memory corruption was found on Intel DG45ID board.
-	 * It hase different DMI_BIOS_VENDOR = "Intel Corp.", for now we will
+	 * AMI BIOS with low memory corruption was found on Intel DG45ID and
+	 * DG45FC boards.
+	 * It has a different DMI_BIOS_VENDOR = "Intel Corp.", for now we will
 	 * match only DMI_BOARD_NAME and see if there is more bad products
 	 * with this vendor.
 	 */
+	{
 		.callback = dmi_low_memory_corruption,
 		.ident = "AMI BIOS",
 		.matches = {
 			DMI_MATCH(DMI_BOARD_NAME, "DG45ID"),
+		},
+	},
+	{
+		.callback = dmi_low_memory_corruption,
+		.ident = "AMI BIOS",
+		.matches = {
+			DMI_MATCH(DMI_BOARD_NAME, "DG45FC"),
 		},
 	},
 #endif
