9538ca636f2fa28ae1514327328e2869f0215981
Maxime Bizon <mbizon@freebox.fr>
2010-01-30 18:34:58 +0100

    MIPS: BCM63xx: Initialize gpio_out_low & out_high to current value at boot.
    To avoid a glitch during GPIO initialisation read GPIO output register
    values left by the firmware.
    
    Signed-off-by: Maxime Bizon <mbizon@freebox.fr>
    To: linux-mips@linux-mips.org
    Patchwork: http://patchwork.linux-mips.org/patch/903/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/bcm63xx/gpio.c
+++ arch/mips/bcm63xx/gpio.c
@@ -125,6 +125,8 @@ static struct gpio_chip bcm63xx_gpio_chip = {
 
 int __init bcm63xx_gpio_init(void)
 {
+	gpio_out_low = bcm_gpio_readl(GPIO_DATA_LO_REG);
+	gpio_out_high = bcm_gpio_readl(GPIO_DATA_HI_REG);
 	bcm63xx_gpio_chip.ngpio = bcm63xx_gpio_count();
 	pr_info("registering %d GPIOs\n", bcm63xx_gpio_chip.ngpio);
 
