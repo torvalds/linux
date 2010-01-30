55461f4c584e812bb8e19c3fb1924edd512ea74c
Jean-François Moine <moinejf@free.fr>
2010-01-30 16:03:02 -0300

    V4L/DVB: gspca - main: Change version to 2.9.0.
    Signed-off-by: Jean-François Moine <moinejf@free.fr>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/gspca/gspca.c
+++ drivers/media/video/gspca/gspca.c
@@ -53,7 +53,7 @@ MODULE_AUTHOR("Jean-Francois Moine <http://moinejf.free.fr>");
 MODULE_DESCRIPTION("GSPCA USB Camera Driver");
 MODULE_LICENSE("GPL");
 
-#define DRIVER_VERSION_NUMBER	KERNEL_VERSION(2, 8, 0)
+#define DRIVER_VERSION_NUMBER	KERNEL_VERSION(2, 9, 0)
 
 #ifdef GSPCA_DEBUG
 int gspca_debug = D_ERR | D_PROBE;
