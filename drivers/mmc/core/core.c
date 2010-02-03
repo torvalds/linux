1cb89ed9ee6172fea7d5884385187119e99aa3d0
Dmitry Shmidt <dimitrysh@google.com>
2010-02-03 12:44:21 -0800

    drivers: mmc: Fix minimal frequency assignment
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -910,12 +910,7 @@ static void mmc_power_up(struct mmc_host *host)
 	 */
 	mmc_delay(10);
 
-	if (host->f_min > 400000) {
-		pr_warning("%s: Minimum clock frequency too high for "
-				"identification mode\n", mmc_hostname(host));
-		host->ios.clock = host->f_min;
-	} else
-		host->ios.clock = 400000;
+	host->ios.clock = host->f_min;
 
 	host->ios.power_mode = MMC_POWER_ON;
 	mmc_set_ios(host);
