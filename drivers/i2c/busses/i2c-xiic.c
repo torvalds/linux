02ca6c407e0d43e6de5d646d26d87fc2eaa7a98b
Randy Dunlap <randy.dunlap@oracle.com>
2010-02-04 12:11:09 -0800

    Add include to i2c-xii.c to fix build error
    drivers/i2c/busses/i2c-xiic.c:493: error: implicit declaration of function 'mdelay'
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Cc: "Richard Röjfors" <richard.rojfors@pelagicore.com>
    Cc: "Ben Dooks (embedded platforms)" <ben-linux@fluff.org>
    Cc: linux-i2c@vger.kernel.org
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/i2c/busses/i2c-xiic.c
+++ drivers/i2c/busses/i2c-xiic.c
@@ -32,6 +32,7 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/errno.h>
+#include <linux/delay.h>
 #include <linux/platform_device.h>
 #include <linux/i2c.h>
 #include <linux/interrupt.h>
