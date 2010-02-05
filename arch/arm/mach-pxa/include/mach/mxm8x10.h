1f3b536be7bfb3930cf7dc6d995d82877f25d6cf
Edwin Peer <epeer@tmtservices.co.za>
2010-02-05 18:31:48 +0200

    [ARM] pxa: add support for Embedian MXM-8x10
    Signed-off-by: Edwin Peer <epeer@tmtservices.co.za>
    Signed-off-by: Eric Miao <eric.y.miao@gmail.com>

---
+++ arch/arm/mach-pxa/include/mach/mxm8x10.h
@@ -0,0 +1,21 @@
+#ifndef __MACH_MXM_8X10_H
+#define __MACH_MXM_8X10_H
+
+#define MXM_8X10_ETH_PHYS	0x13000000
+
+#if defined(CONFIG_MMC)
+
+#define MXM_8X10_SD_nCD (72)
+#define MXM_8x10_SD_WP (84)
+
+extern void mxm_8x10_mmc_init(void);
+#else
+static inline void mxm_8x10_mmc_init(void) {}
+#endif
+
+extern void mxm_8x10_usb_host_init(void);
+extern void mxm_8x10_ac97_init(void);
+
+extern void mxm_8x10_barebones_init(void);
+
+#endif /* __MACH_MXM_8X10_H */
