cb6643e1c38b6bd5c1594f0a45d8cf6943a6f934
Christoph Egger <siccegge@stud.informatik.uni-erlangen.de>
2010-02-05 16:26:35 +0100

    [libata] pata_marvell: CONFIG_AHCI is really CONFIG_SATA_AHCI
    The marvell driver comtains a fallback to ahci for the sata ports
    which is incorrectly checked as CONFIG_AHCI while the only AHCI config
    item is actually called SATA_AHCI (which also sounds sensible
    considering it's a fallback for the sata ports).
    
    Signed-off-by: Christoph Egger <siccegge@stud.informatik.uni-erlangen.de>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/ata/pata_marvell.c
+++ drivers/ata/pata_marvell.c
@@ -147,7 +147,7 @@ static int marvell_init_one (struct pci_dev *pdev, const struct pci_device_id *i
 	if (pdev->device == 0x6101)
 		ppi[1] = &ata_dummy_port_info;
 
-#if defined(CONFIG_AHCI) || defined(CONFIG_AHCI_MODULE)
+#if defined(CONFIG_SATA_AHCI) || defined(CONFIG_SATA_AHCI_MODULE)
 	if (!marvell_pata_active(pdev)) {
 		printk(KERN_INFO DRV_NAME ": PATA port not active, deferring to AHCI driver.\n");
 		return -ENODEV;
