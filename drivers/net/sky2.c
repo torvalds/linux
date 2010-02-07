a0db28b898266265c1cc2ff84f041a561a265504
stephen hemminger <shemminger@vyatta.com>
2010-02-07 06:23:53 +0000

    sky2: resume clocks
    Change the resume path to use pci write config for a couple of reasons:
    1. pci_write_config_dword() allows for more error
       checking of PCI health after resume.
    
    2. better to toggle this register on all chip types, since that
       is what vendor driver does.
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/sky2.c
+++ drivers/net/sky2.c
@@ -4859,10 +4859,11 @@ static int sky2_resume(struct pci_dev *pdev)
 	pci_enable_wake(pdev, PCI_D0, 0);
 
 	/* Re-enable all clocks */
-	if (hw->chip_id == CHIP_ID_YUKON_EX ||
-	    hw->chip_id == CHIP_ID_YUKON_EC_U ||
-	    hw->chip_id == CHIP_ID_YUKON_FE_P)
-		sky2_pci_write32(hw, PCI_DEV_REG3, 0);
+	err = pci_write_config_dword(pdev, PCI_DEV_REG3, 0);
+	if (err) {
+		dev_err(&pdev->dev, "PCI write config failed\n");
+		goto out;
+	}
 
 	sky2_reset(hw);
 	sky2_write32(hw, B0_IMSK, Y2_IS_BASE);
