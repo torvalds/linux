d9d9736709a7fbe2ecee46e4775c825263545ad4
Vaidyanathan Srinivasan <svaidy@linux.vnet.ibm.com>
2010-02-08 15:35:55 +0530

    sched: Fix sched_mv_power_savings for !SMT
    commit 28f5318167adf23b16c844b9c2253f355cb21796 upstream.
    
    Fix for sched_mc_powersavigs for pre-Nehalem platforms.
    Child sched domain should clear SD_PREFER_SIBLING if parent will have
    SD_POWERSAVINGS_BALANCE because they are contradicting.
    
    Sets the flags correctly based on sched_mc_power_savings.
    
    Signed-off-by: Vaidyanathan Srinivasan <svaidy@linux.vnet.ibm.com>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    LKML-Reference: <20100208100555.GD2931@dirshya.in.ibm.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -864,7 +864,10 @@ static inline int sd_balance_for_mc_power(void)
 	if (sched_smt_power_savings)
 		return SD_POWERSAVINGS_BALANCE;
 
-	return SD_PREFER_SIBLING;
+	if (!sched_mc_power_savings)
+		return SD_PREFER_SIBLING;
+
+	return 0;
 }
 
 static inline int sd_balance_for_package_power(void)
