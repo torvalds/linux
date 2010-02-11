3b077241e02041b1f03fee912896b8de1d7ac096
Nick Pelly <npelly@google.com>
2010-02-11 11:54:28 -0800

    Bluetooth: Allow SCO/eSCO packet type selection for outgoing SCO connections.
    __u16 sco_pkt_type is introduced to struct sockaddr_sco. It allows bitwise
    selection of SCO/eSCO packet types. Currently those bits are:
    
    0x0001 HV1 may be used.
    0x0002 HV2 may be used.
    0x0004 HV3 may be used.
    0x0008 EV3 may be used.
    0x0010 EV4 may be used.
    0x0020 EV5 may be used.
    0x0040 2-EV3 may be used.
    0x0080 3-EV3 may be used.
    0x0100 2-EV5 may be used.
    0x0200 3-EV5 may be used.
    
    This is similar to the Packet Type parameter in the HCI Setup Synchronous
    Connection Command, except that we are not reversing the logic on the EDR bits.
    This makes the use of sco_pkt_tpye forward portable for the use case of
    white-listing packet types, which we expect will be the primary use case.
    
    If sco_pkt_type is zero, or userspace uses the old struct sockaddr_sco,
    then the default behavior is to allow all packet types.
    
    Packet type selection is just a request made to the Bluetooth chipset, and
    it is up to the link manager on the chipset to negiotiate and decide on the
    actual packet types used. Furthermore, when a SCO/eSCO connection is eventually
    made there is no way for the host stack to determine which packet type was used
    (however it is possible to get the link type of SCO or eSCO).
    
    sco_pkt_type is ignored for incoming SCO connections. It is possible
    to add this in the future as a parameter to the Accept Synchronous Connection
    Command, however its a little trickier because the kernel does not
    currently preserve sockaddr_sco data between userspace calls to accept().
    
    The most common use for sco_pkt_type will be to white-list only SCO packets,
    which can be done with the hci.h constant SCO_ESCO_MASK.
    
    This patch is motivated by broken Bluetooth carkits such as the Motorolo
    HF850 (it claims to support eSCO, but will actually reject eSCO connections
    after 5 seconds) and the 2007/2008 Infiniti G35/37 (fails to route audio
    if a 2-EV5 packet type is negiotiated). With this patch userspace can maintain
    a list of compatible packet types to workaround remote devices such as these.
    
    Based on a patch by Marcel Holtmann.
    
    Change-Id: I304d8fda5b4145254820a3003820163bf53de5a5
    Signed-off-by: Nick Pelly <npelly@google.com>

--- net/bluetooth/hci_conn.c
+++ net/bluetooth/hci_conn.c
@@ -196,7 +196,8 @@ static void hci_conn_idle(unsigned long arg)
 	hci_conn_enter_sniff_mode(conn);
 }
 
-struct hci_conn *hci_conn_add(struct hci_dev *hdev, int type, bdaddr_t *dst)
+struct hci_conn *hci_conn_add(struct hci_dev *hdev, int type,
+					__u16 pkt_type, bdaddr_t *dst)
 {
 	struct hci_conn *conn;
 
@@ -221,14 +222,22 @@ struct hci_conn *hci_conn_add(struct hci_dev *hdev, int type, bdaddr_t *dst)
 		conn->pkt_type = hdev->pkt_type & ACL_PTYPE_MASK;
 		break;
 	case SCO_LINK:
-		if (lmp_esco_capable(hdev))
-			conn->pkt_type = (hdev->esco_type & SCO_ESCO_MASK) |
-					(hdev->esco_type & EDR_ESCO_MASK);
-		else
-			conn->pkt_type = hdev->pkt_type & SCO_PTYPE_MASK;
-		break;
+		if (!pkt_type)
+			pkt_type = SCO_ESCO_MASK;
 	case ESCO_LINK:
-		conn->pkt_type = hdev->esco_type & ~EDR_ESCO_MASK;
+		if (!pkt_type)
+			pkt_type = ALL_ESCO_MASK;
+		if (lmp_esco_capable(hdev)) {
+			/* HCI Setup Synchronous Connection Command uses
+			   reverse logic on the EDR_ESCO_MASK bits */
+			conn->pkt_type = (pkt_type ^ EDR_ESCO_MASK) &
+					hdev->esco_type;
+		} else {
+			/* Legacy HCI Add Sco Connection Command uses a
+			   shifted bitmask */
+			conn->pkt_type = (pkt_type << 5) & hdev->pkt_type &
+					SCO_PTYPE_MASK;
+		}
 		break;
 	}
 
@@ -340,7 +349,9 @@ EXPORT_SYMBOL(hci_get_route);
 
 /* Create SCO or ACL connection.
  * Device _must_ be locked */
-struct hci_conn *hci_connect(struct hci_dev *hdev, int type, bdaddr_t *dst, __u8 sec_level, __u8 auth_type)
+struct hci_conn *hci_connect(struct hci_dev *hdev, int type,
+					__u16 pkt_type, bdaddr_t *dst,
+					__u8 sec_level, __u8 auth_type)
 {
 	struct hci_conn *acl;
 	struct hci_conn *sco;
@@ -348,7 +359,7 @@ struct hci_conn *hci_connect(struct hci_dev *hdev, int type, bdaddr_t *dst, __u8
 	BT_DBG("%s dst %s", hdev->name, batostr(dst));
 
 	if (!(acl = hci_conn_hash_lookup_ba(hdev, ACL_LINK, dst))) {
-		if (!(acl = hci_conn_add(hdev, ACL_LINK, dst)))
+		if (!(acl = hci_conn_add(hdev, ACL_LINK, 0, dst)))
 			return NULL;
 	}
 
@@ -364,7 +375,7 @@ struct hci_conn *hci_connect(struct hci_dev *hdev, int type, bdaddr_t *dst, __u8
 		return acl;
 
 	if (!(sco = hci_conn_hash_lookup_ba(hdev, type, dst))) {
-		if (!(sco = hci_conn_add(hdev, type, dst))) {
+		if (!(sco = hci_conn_add(hdev, type, pkt_type, dst))) {
 			hci_conn_put(acl);
 			return NULL;
 		}
