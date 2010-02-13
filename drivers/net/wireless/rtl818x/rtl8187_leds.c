634c8d2587abbd680603e016ae1060129abdf46f
Hauke Mehrtens <hauke@hauke-m.de>
2010-02-13 18:10:53 +0100

    rtl8187: fix typo in ifdef comment
    Cc: Herton Ronaldo Krzesinski <herton@mandriva.com.br>
    Cc: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    Acked-by: Hin-Tak Leung <htl10@users.sourceforge.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rtl818x/rtl8187_leds.c
+++ drivers/net/wireless/rtl818x/rtl8187_leds.c
@@ -241,5 +241,5 @@ void rtl8187_leds_exit(struct ieee80211_hw *dev)
 	cancel_delayed_work_sync(&priv->led_off);
 	cancel_delayed_work_sync(&priv->led_on);
 }
-#endif /* def CONFIG_RTL8187_LED */
+#endif /* def CONFIG_RTL8187_LEDS */
 
