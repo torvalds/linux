94429bb6c8343722544e282d89dc4638672e49b4
Jerome Glisse <jglisse@redhat.com>
2010-02-15 21:36:33 +0100

    drm/radeon/kms: fix bo's fence association
    Previous code did associate fence to bo before the fence was emited
    and it also didn't lock protected access to ttm sync_obj member.
    Both of this flaw leads to possible race between different code
    path. This patch fix this by associating fence only once the fence
    is emitted and properly lock protect access to sync_obj member of
    ttm.
    
    Fix:
    https://bugs.freedesktop.org/show_bug.cgi?id=26438
    and likely similar others bugs
    Signed-off-by: Jerome Glisse <jglisse@redhat.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/radeon_object.h
+++ drivers/gpu/drm/radeon/radeon_object.h
@@ -156,8 +156,8 @@ extern void radeon_bo_list_add_object(struct radeon_bo_list *lobj,
 				struct list_head *head);
 extern int radeon_bo_list_reserve(struct list_head *head);
 extern void radeon_bo_list_unreserve(struct list_head *head);
-extern int radeon_bo_list_validate(struct list_head *head, void *fence);
-extern void radeon_bo_list_unvalidate(struct list_head *head, void *fence);
+extern int radeon_bo_list_validate(struct list_head *head);
+extern void radeon_bo_list_fence(struct list_head *head, void *fence);
 extern int radeon_bo_fbdev_mmap(struct radeon_bo *bo,
 				struct vm_area_struct *vma);
 extern int radeon_bo_set_tiling_flags(struct radeon_bo *bo,
