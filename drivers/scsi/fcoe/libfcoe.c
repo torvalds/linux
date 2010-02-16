03bf7a5af58e07aa13ea61993f5892f733b6f46d
Bhanu Prakash Gollapudi <bprakash@broadcom.com>
2010-02-16 12:15:34 -0800

    [SCSI] libfcoe: Send port LKA every FIP_VN_KA_PERIOD secs.
    libfcoe module doesnt send port keep alive every
    FIP_VN_KA_PERIOD due to improper assignment of timeout value.
    Update the port_ka_time appropriately by incrementing it by
    FIP_VN_KA_PERIOD in fcoe_ctlr_timeout(), so that the link_work
    is scheduled to send the port LKA.
    
    Signed-off-by: Bhanu Gollapudi <bprakash@broadcom.com>
    Acked-by: Joe Eykholt <jeykholt@cisco.com>
    Signed-off-by: Robert Love <robert.w.love@intel.com>
    Signed-off-by: James Bottomley <James.Bottomley@suse.de>

--- drivers/scsi/fcoe/libfcoe.c
+++ drivers/scsi/fcoe/libfcoe.c
@@ -1187,7 +1187,7 @@ static void fcoe_ctlr_timeout(unsigned long arg)
 			next_timer = fip->ctlr_ka_time;
 
 		if (time_after_eq(jiffies, fip->port_ka_time)) {
-			fip->port_ka_time += jiffies +
+			fip->port_ka_time = jiffies +
 				msecs_to_jiffies(FIP_VN_KA_PERIOD);
 			fip->send_port_ka = 1;
 		}
