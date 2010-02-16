9874c41cd5e70bfc97bcd52a8b6c98c2a6ba7299
Joe Perches <joe@perches.com>
2010-02-16 18:40:04 +0000

    ipv6.h: reassembly: replace calculated magic number with multiplication
    On Tue, 2010-02-16 at 16:47 +0100, Patrick McHardy wrote:
    > Joe Perches wrote:
    > >> @@ -246,6 +246,8 @@ extern int ipv6_opt_accepted(struct sock *sk, struct sk_buff *skb);
    > >>  int ip6_frag_nqueues(struct net *net);
    > >>  int ip6_frag_mem(struct net *net);
    > >>
    > >> +#define IPV6_FRAG_HIGH_THRESH	262144		/* == 256*1024 */
    > >> +#define IPV6_FRAG_LOW_THRESH	196608		/* == 192*1024 */
    > >>  #define IPV6_FRAG_TIMEOUT	(60*HZ)		/* 60 seconds */
    > >
    > > 196608 isn't a number I want to remember.
    > > Is this better as:
    > >
    > > #define IPV6_FRAG_HIGH_THRESH	(256 * 1024)	/* 262144 */
    > > #define IPV6_FRAG_LOW_THRESH	(192 * 1024)	/* 196608 */
    >
    > Please send a patch, I'll apply it once these patches are in Dave's
    > tree.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/ipv6.h
+++ include/net/ipv6.h
@@ -246,9 +246,9 @@ extern int ipv6_opt_accepted(struct sock *sk, struct sk_buff *skb);
 int ip6_frag_nqueues(struct net *net);
 int ip6_frag_mem(struct net *net);
 
-#define IPV6_FRAG_HIGH_THRESH	262144		/* == 256*1024 */
-#define IPV6_FRAG_LOW_THRESH	196608		/* == 192*1024 */
-#define IPV6_FRAG_TIMEOUT	(60*HZ)		/* 60 seconds */
+#define IPV6_FRAG_HIGH_THRESH	(256 * 1024)	/* 262144 */
+#define IPV6_FRAG_LOW_THRESH	(192 * 1024)	/* 196608 */
+#define IPV6_FRAG_TIMEOUT	(60 * HZ)	/* 60 seconds */
 
 extern int __ipv6_addr_type(const struct in6_addr *addr);
 static inline int ipv6_addr_type(const struct in6_addr *addr)
