8e3a6e7fa739d021324d813cd6fdd1c5c7998774
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-02-17 21:05:27 +0100

    arm/imx: choose sane CONSISTENT_DMA_SIZE if video is enabled for both mx1 and mx3
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- arch/arm/plat-mxc/include/mach/memory.h
+++ arch/arm/plat-mxc/include/mach/memory.h
@@ -34,20 +34,19 @@
 # endif
 #endif
 
-#if defined(CONFIG_MX1_VIDEO)
-/*
- * Increase size of DMA-consistent memory region.
- * This is required for i.MX camera driver to capture at least four VGA frames.
- */
-#define CONSISTENT_DMA_SIZE SZ_4M
-#endif /* CONFIG_MX1_VIDEO */
-
 #if defined(CONFIG_MX3_VIDEO)
 /*
  * Increase size of DMA-consistent memory region.
  * This is required for mx3 camera driver to capture at least two QXGA frames.
  */
 #define CONSISTENT_DMA_SIZE SZ_8M
-#endif /* CONFIG_MX3_VIDEO */
+
+#elif defined(CONFIG_MX1_VIDEO)
+/*
+ * Increase size of DMA-consistent memory region.
+ * This is required for i.MX camera driver to capture at least four VGA frames.
+ */
+#define CONSISTENT_DMA_SIZE SZ_4M
+#endif /* CONFIG_MX1_VIDEO */
 
 #endif /* __ASM_ARCH_MXC_MEMORY_H__ */
