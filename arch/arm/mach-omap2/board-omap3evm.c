aa6912d8850a555dccfab21f496e84aab7ededb6
Sanjeev Premi <premi@ti.com>
2010-02-17 14:09:23 -0800

    omap3evm: Add mux settings for keypad
    This patch sets SYS_NIRQ for keypad input. It also
    sets the same as a wakeup event from OFF mode.
    
    Signed-off-by: Sanjeev Premi <premi@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-omap3evm.c
+++ arch/arm/mach-omap2/board-omap3evm.c
@@ -647,6 +647,10 @@ static struct ehci_hcd_omap_platform_data ehci_pdata __initconst = {
 
 #ifdef CONFIG_OMAP_MUX
 static struct omap_board_mux board_mux[] __initdata = {
+	OMAP3_MUX(SYS_NIRQ, OMAP_MUX_MODE0 | OMAP_PIN_INPUT_PULLUP |
+				OMAP_PIN_OFF_INPUT_PULLUP |
+				OMAP_PIN_OFF_WAKEUPENABLE),
+
 	{ .reg_offset = OMAP_MUX_TERMINATOR },
 };
 #else
