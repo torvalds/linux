2ee78f7b1d8ada2615ecbcd9fea70580008bd6ce
Yinghai Lu <yinghai@kernel.org>
2010-02-17 11:29:49 -0800

    x86: Fix non-bootmem compilation on PowerPC
    These build errors on some non-x86 platforms (PowerPC for example):
    
     mm/page_alloc.c: In function '__alloc_memory_core_early':
       mm/page_alloc.c:3468: error: implicit declaration of function 'find_early_area'
       mm/page_alloc.c:3483: error: implicit declaration of function 'reserve_early_without_check'
    
    The function is only needed on CONFIG_NO_BOOTMEM.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Johannes Weiner <hannes@saeurebad.de>
    Cc: Mel Gorman <mel@csn.ul.ie>
    LKML-Reference: <4B747239.4070907@kernel.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3450,6 +3450,7 @@ int __init add_from_early_node_map(struct range *range, int az,
 	return nr_range;
 }
 
+#ifdef CONFIG_NO_BOOTMEM
 void * __init __alloc_memory_core_early(int nid, u64 size, u64 align,
 					u64 goal, u64 limit)
 {
@@ -3486,6 +3487,7 @@ void * __init __alloc_memory_core_early(int nid, u64 size, u64 align,
 
 	return NULL;
 }
+#endif
 
 
 void __init work_with_active_regions(int nid, work_fn_t work_fn, void *data)
