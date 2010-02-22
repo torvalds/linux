6f6b5d49105eba1be0b2ef0c04a4571f20793778
Juuso Oikarinen <juuso.oikarinen@nokia.com>
2010-02-22 08:38:42 +0200

    wl1271: Remove tx-power level workaround
    This patch removes the workaround limiting chipset TX power to 12dB, instead
    using the value configured by the mac80211.
    
    Signed-off-by: Juuso Oikarinen <juuso.oikarinen@nokia.com>
    Reviewed-by: Teemu Paasikivi <ext-teemu.3.paasikivi@nokia.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/wl1271_acx.c
+++ drivers/net/wireless/wl12xx/wl1271_acx.c
@@ -135,12 +135,7 @@ int wl1271_acx_tx_power(struct wl1271 *wl, int power)
 		goto out;
 	}
 
-	/*
-	 * FIXME: This is a workaround needed while we don't the correct
-	 * calibration, to avoid distortions
-	 */
-	/* acx->current_tx_power = power * 10; */
-	acx->current_tx_power = 120;
+	acx->current_tx_power = power * 10;
 
 	ret = wl1271_cmd_configure(wl, DOT11_CUR_TX_PWR, acx, sizeof(*acx));
 	if (ret < 0) {
