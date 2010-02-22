46b72d78cb022714c89a9ebc00b9581b550cfca7
Daniel Sangorrin <daniel.sangorrin@gmail.com>
2010-02-22 11:03:11 +0900

    USB: serial: ftdi: add CONTEC vendor and product id
    This is a patch to ftdi_sio_ids.h and ftdi_sio.c that adds
    identifiers for CONTEC USB serial converter. I tested it
    with the device COM-1(USB)H
    
    Signed-off-by: Daniel Sangorrin <daniel.sangorrin@gmail.com>
    Cc: Andreas Mohr <andi@lisas.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio_ids.h
+++ drivers/usb/serial/ftdi_sio_ids.h
@@ -494,6 +494,13 @@
 #define RATOC_PRODUCT_ID_USB60F	0xb020
 
 /*
+ * Contec products (http://www.contec.com)
+ * Submitted by Daniel Sangorrin
+ */
+#define CONTEC_VID		0x06CE	/* Vendor ID */
+#define CONTEC_COM1USBH_PID	0x8311	/* COM-1(USB)H */
+
+/*
  * Definitions for B&B Electronics products.
  */
 #define BANDB_VID		0x0856	/* B&B Electronics Vendor ID */
