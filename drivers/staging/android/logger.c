ae991396e9c66ca7bb73fceb62e632ff4ed6c4e0
San Mehat <san@google.com>
2010-02-23 16:09:47 -0800

    android: logger: Add new system log for framework/system log messages
    Signed-off-by: San Mehat <san@google.com>

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -557,6 +557,7 @@ static struct logger_log VAR = { \
 DEFINE_LOGGER_DEVICE(log_main, LOGGER_LOG_MAIN, 64*1024)
 DEFINE_LOGGER_DEVICE(log_events, LOGGER_LOG_EVENTS, 256*1024)
 DEFINE_LOGGER_DEVICE(log_radio, LOGGER_LOG_RADIO, 64*1024)
+DEFINE_LOGGER_DEVICE(log_system, LOGGER_LOG_SYSTEM, 64*1024)
 
 static struct logger_log *get_log_from_minor(int minor)
 {
@@ -566,6 +567,8 @@ static struct logger_log *get_log_from_minor(int minor)
 		return &log_events;
 	if (log_radio.misc.minor == minor)
 		return &log_radio;
+	if (log_system.misc.minor == minor)
+		return &log_system;
 	return NULL;
 }
 
@@ -599,6 +602,10 @@ static int __init logger_init(void)
 		goto out;
 
 	ret = init_log(&log_radio);
+	if (unlikely(ret))
+		goto out;
+
+	ret = init_log(&log_system);
 	if (unlikely(ret))
 		goto out;
 
