2c798557b9e0961e545e526197654d9437a6330e
Kukjin Kim <kgene.kim@samsung.com>
2010-02-24 16:40:40 +0900

    ARM: S5PV210: Add IRQ support
    This patch adds IRQ support for S5PV210. This patch adds interrupt
    register definitions, IRQ definitions for various interrupt sources
    and new VIC base for VIC2 and VIC3 in plat-s5p common irq code.
    
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/plat-s5p/include/plat/irqs.h
+++ arch/arm/plat-s5p/include/plat/irqs.h
@@ -29,6 +29,7 @@
 #define S5P_VIC0_BASE		S5P_IRQ(0)
 #define S5P_VIC1_BASE		S5P_IRQ(32)
 #define S5P_VIC2_BASE		S5P_IRQ(64)
+#define S5P_VIC3_BASE		S5P_IRQ(96)
 
 #define VIC_BASE(x)		(S5P_VIC0_BASE + ((x)*32))
 
@@ -76,6 +77,7 @@
 #define S5P_IRQ_VIC0(x)		(S5P_VIC0_BASE + (x))
 #define S5P_IRQ_VIC1(x)		(S5P_VIC1_BASE + (x))
 #define S5P_IRQ_VIC2(x)		(S5P_VIC2_BASE + (x))
+#define S5P_IRQ_VIC3(x)		(S5P_VIC3_BASE + (x))
 
 #define S5P_TIMER_IRQ(x)	S5P_IRQ(11 + (x))
 
