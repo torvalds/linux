914c8ad2d18b62ad1420f518c0cab0b0b90ab308
Jiri Pirko <jpirko@redhat.com>
2010-02-24 23:57:04 +0000

    af_packet: do not accept mc address smaller then dev->addr_len in packet_mc_add()
    There is no point of accepting an address of smaller length than dev->addr_len
    here. Therefore change this for stonger check.
    
    Signed-off-by: Jiri Pirko <jpirko@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -1734,7 +1734,7 @@ static int packet_mc_add(struct sock *sk, struct packet_mreq_max *mreq)
 		goto done;
 
 	err = -EINVAL;
-	if (mreq->mr_alen > dev->addr_len)
+	if (mreq->mr_alen != dev->addr_len)
 		goto done;
 
 	err = -ENOBUFS;
