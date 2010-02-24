1d9cfc4e354cd619d92bb938657dec3c533e6929
Michael Chan <mchan@broadcom.com>
2010-02-24 14:42:09 +0000

    cnic: Update version to 2.1.1.
    And update copyright to 2010.
    
    Signed-off-by: Michael Chan <mchan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/cnic.c
+++ drivers/net/cnic.c
@@ -1,6 +1,6 @@
 /* cnic.c: Broadcom CNIC core network driver.
  *
- * Copyright (c) 2006-2009 Broadcom Corporation
+ * Copyright (c) 2006-2010 Broadcom Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
