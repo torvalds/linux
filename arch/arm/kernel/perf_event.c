ddee87f208b6229d2910dd5930c87089dc56c87e
Will Deacon <will.deacon@arm.com>
2010-02-25 15:04:14 +0100

    ARM: 5959/1: ARM: perf-events: request PMU interrupts with IRQF_NOBALANCING
    If IRQ balancing is used on a multicore ARM system, PMU interrupt
    lines may be relocated onto CPUs other than the one causing the
    counter overflow. This can result in misattribution of events to
    the wrong core and, in the case that the CPU handling the interrupt
    has not experience counter overflow, the interrupt can be disabled
    because the handler returns IRQ_NONE.
    
    This patch adds the IRQF_NOBALANCING flag to the request_irq call
    in perf_events.c.
    
    Acked-by: Jamie Iles <jamie.iles@picochip.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/perf_event.c
+++ arch/arm/kernel/perf_event.c
@@ -332,7 +332,8 @@ armpmu_reserve_hardware(void)
 
 	for (i = 0; i < pmu_irqs->num_irqs; ++i) {
 		err = request_irq(pmu_irqs->irqs[i], armpmu->handle_irq,
-				  IRQF_DISABLED, "armpmu", NULL);
+				  IRQF_DISABLED | IRQF_NOBALANCING,
+				  "armpmu", NULL);
 		if (err) {
 			pr_warning("unable to request IRQ%d for ARM "
 				   "perf counters\n", pmu_irqs->irqs[i]);
