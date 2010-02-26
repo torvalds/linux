0f63be217580f90d85affb7c14921b73a540aa47
Mike Lockwood <lockwood@android.com>
2010-02-26 09:34:19 -0500

    USB: gadget: composite: Don't increment interface number for alt settings.
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -326,8 +326,13 @@ static int config_buf(struct usb_configuration *config,
 		dest = next;
 		while ((descriptor = *descriptors++) != NULL) {
 			intf = (struct usb_interface_descriptor *)dest;
-			if (intf->bDescriptorType == USB_DT_INTERFACE)
-				intf->bInterfaceNumber = interfaceCount++;
+			if (intf->bDescriptorType == USB_DT_INTERFACE) {
+				/* don't increment bInterfaceNumber for alternate settings */
+				if (intf->bAlternateSetting == 0)
+					intf->bInterfaceNumber = interfaceCount++;
+				else
+					intf->bInterfaceNumber = interfaceCount - 1;
+			}
 			dest += intf->bLength;
 		}
 
