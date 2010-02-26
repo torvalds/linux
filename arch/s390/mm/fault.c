22e0a0467292222214d1974d9bc2664a6c05980d
Heiko Carstens <heiko.carstens@de.ibm.com>
2010-02-26 22:37:45 +0100

    [S390] use kprobes_built_in() in mm/fault code
    Use kprobes_built_in() to avoid ifdefs like most other architectures do.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/mm/fault.c
+++ arch/s390/mm/fault.c
@@ -60,15 +60,13 @@ static inline int notify_page_fault(struct pt_regs *regs)
 {
 	int ret = 0;
 
-#ifdef CONFIG_KPROBES
 	/* kprobe_running() needs smp_processor_id() */
-	if (!user_mode(regs)) {
+	if (kprobes_built_in() && !user_mode(regs)) {
 		preempt_disable();
 		if (kprobe_running() && kprobe_fault_handler(regs, 14))
 			ret = 1;
 		preempt_enable();
 	}
-#endif
 	return ret;
 }
 
