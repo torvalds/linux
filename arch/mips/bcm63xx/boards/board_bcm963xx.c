f29b7cac19ef5aa093fc9403d10735fa72cabf99
Florian Fainelli <florian@openwrt.org>
2010-03-01 23:36:27 +0100

    MIPS: BCM63xx: Add DWVS0 board
    The DWVS0 board is a BCM6358-based board with an on-board OHCI controler.
    
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    To: linux-mips@linux-mips.org
    Patchwork: http://patchwork.linux-mips.org/patch/1015/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/bcm63xx/boards/board_bcm963xx.c
+++ arch/mips/bcm63xx/boards/board_bcm963xx.c
@@ -550,6 +550,27 @@ static struct board_info __initdata board_AGPFS0 = {
 	.has_ohci0 = 1,
 	.has_ehci0 = 1,
 };
+
+static struct board_info __initdata board_DWVS0 = {
+	.name				= "DWV-S0",
+	.expected_cpu_id		= 0x6358,
+
+	.has_enet0			= 1,
+	.has_enet1			= 1,
+	.has_pci			= 1,
+
+	.enet0 = {
+		.has_phy		= 1,
+		.use_internal_phy	= 1,
+	},
+
+	.enet1 = {
+		.force_speed_100	= 1,
+		.force_duplex_full	= 1,
+	},
+
+	.has_ohci0			= 1,
+};
 #endif
 
 /*
@@ -578,6 +599,7 @@ static const struct board_info __initdata *bcm963xx_boards[] = {
 	&board_96358vw,
 	&board_96358vw2,
 	&board_AGPFS0,
+	&board_DWVS0,
 #endif
 };
 
