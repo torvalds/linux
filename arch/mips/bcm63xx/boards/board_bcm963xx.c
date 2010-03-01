2e6ad9a9585b5520cddda4743bfbfdf7f11c5a50
Florian Fainelli <florian@openwrt.org>
2010-03-01 23:36:22 +0100

    MIPS: BCM63xx: Add the RTA1025W-16 BCM6348-based board to suppported boards.
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    To: linux-mips@linux-mips.org
    Patchwork: http://patchwork.linux-mips.org/patch/1014/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/bcm63xx/boards/board_bcm963xx.c
+++ arch/mips/bcm63xx/boards/board_bcm963xx.c
@@ -368,6 +368,25 @@ static struct board_info __initdata board_FAST2404 = {
 	.has_ehci0			= 1,
 };
 
+static struct board_info __initdata board_rta1025w_16 = {
+	.name				= "RTA1025W_16",
+	.expected_cpu_id		= 0x6348,
+
+	.has_enet0			= 1,
+	.has_enet1			= 1,
+	.has_pci			= 1,
+
+	.enet0 = {
+		.has_phy		= 1,
+		.use_internal_phy	= 1,
+	},
+	.enet1 = {
+		.force_speed_100	= 1,
+		.force_duplex_full	= 1,
+	},
+};
+
+
 static struct board_info __initdata board_DV201AMR = {
 	.name				= "DV201AMR",
 	.expected_cpu_id		= 0x6348,
@@ -552,6 +571,7 @@ static const struct board_info __initdata *bcm963xx_boards[] = {
 	&board_FAST2404,
 	&board_DV201AMR,
 	&board_96348gw_a,
+	&board_rta1025w_16,
 #endif
 
 #ifdef CONFIG_BCM63XX_CPU_6358
