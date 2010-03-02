d640f45411308c70e205d04938dfda94152738fb
Mel Gorman <mel@csn.ul.ie>
2010-03-02 22:24:19 +0000

    tty: Take a 256 byte padding into account when buffering below sub-page units
    commit 352fa6ad16b89f8ffd1a93b4419b1a8f2259feab upstream.
    
    The TTY layer takes some care to ensure that only sub-page allocations
    are made with interrupts disabled. It does this by setting a goal of
    "TTY_BUFFER_PAGE" to allocate. Unfortunately, while TTY_BUFFER_PAGE takes the
    size of tty_buffer into account, it fails to account that tty_buffer_find()
    rounds the buffer size out to the next 256 byte boundary before adding on
    the size of the tty_buffer.
    
    This patch adjusts the TTY_BUFFER_PAGE calculation to take into account the
    size of the tty_buffer and the padding. Once applied, tty_buffer_alloc()
    should not require high-order allocations.
    
    Signed-off-by: Mel Gorman <mel@csn.ul.ie>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/tty.h
+++ include/linux/tty.h
@@ -70,12 +70,13 @@ struct tty_buffer {
 
 /*
  * We default to dicing tty buffer allocations to this many characters
- * in order to avoid multiple page allocations. We assume tty_buffer itself
- * is under 256 bytes. See tty_buffer_find for the allocation logic this
- * must match
+ * in order to avoid multiple page allocations. We know the size of
+ * tty_buffer itself but it must also be taken into account that the
+ * the buffer is 256 byte aligned. See tty_buffer_find for the allocation
+ * logic this must match
  */
 
-#define TTY_BUFFER_PAGE		((PAGE_SIZE  - 256) / 2)
+#define TTY_BUFFER_PAGE	(((PAGE_SIZE - sizeof(struct tty_buffer)) / 2) & ~0xFF)
 
 
 struct tty_bufhead {
