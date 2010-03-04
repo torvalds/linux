69fef0d2e2c2c049ef4207a52e78b50d527bd85a
Peter Zijlstra <a.p.zijlstra@chello.nl>
2010-03-04 13:57:24 +0100

    perf: Add attr->precise support to raw event parsing
    Minimal userspace interface to the new 'precise' events flag.
    
    Can be used like "perf top -e r00c0p" which will use PEBS to sample
    retired instructions.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Arnaldo Carvalho de Melo <acme@infradead.org>
    Cc: paulus@samba.org
    Cc: eranian@google.com
    Cc: robert.richter@amd.com
    Cc: fweisbec@gmail.com
    LKML-Reference: <20100304140100.468665803@chello.nl>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- tools/perf/util/parse-events.c
+++ tools/perf/util/parse-events.c
@@ -656,6 +656,10 @@ parse_raw_event(const char **strp, struct perf_event_attr *attr)
 		return EVT_FAILED;
 	n = hex2u64(str + 1, &config);
 	if (n > 0) {
+		if (str[n+1] == 'p') {
+			attr->precise = 1;
+			n++;
+		}
 		*strp = str + n + 1;
 		attr->type = PERF_TYPE_RAW;
 		attr->config = config;
