63e690caf24e8f43ba019fe1107669746b072d80
Andrea Gelmini <andrea.gelmini@gelma.net>
2010-03-08 13:13:07 +0100

    netfilter: include/linux/netfilter/nf_conntrack_tuple_common.h: Checkpatch cleanup
    include/linux/netfilter/nf_conntrack_tuple_common.h:5: ERROR: open brace '{' following enum go on the same line
    
    Signed-off-by: Andrea Gelmini <andrea.gelmini@gelma.net>
    Acked-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- include/linux/netfilter/nf_conntrack_tuple_common.h
+++ include/linux/netfilter/nf_conntrack_tuple_common.h
@@ -1,8 +1,7 @@
 #ifndef _NF_CONNTRACK_TUPLE_COMMON_H
 #define _NF_CONNTRACK_TUPLE_COMMON_H
 
-enum ip_conntrack_dir
-{
+enum ip_conntrack_dir {
 	IP_CT_DIR_ORIGINAL,
 	IP_CT_DIR_REPLY,
 	IP_CT_DIR_MAX
