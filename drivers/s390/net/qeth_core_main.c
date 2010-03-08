78cb27939ff4fd66d7f76cfe7c59c0fdf1b29ed8
Frank Blaschka <frank.blaschka@de.ibm.com>
2010-03-08 20:36:57 +0000

    qeth: change checksumming default for HiperSockets
    Deactivate inbound checksumming on HiperSocket is a valid but
    dangerous optimization in case the frame is routed from an OSA
    network to an HiperSockets network. To go for sure we change the
    default to software checksumming.
    
    Signed-off-by: Frank Blaschka <frank.blaschka@de.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/s390/net/qeth_core_main.c
+++ drivers/s390/net/qeth_core_main.c
@@ -1114,8 +1114,6 @@ static int qeth_setup_card(struct qeth_card *card)
 	card->ipato.enabled = 0;
 	card->ipato.invert4 = 0;
 	card->ipato.invert6 = 0;
-	if (card->info.type == QETH_CARD_TYPE_IQD)
-		card->options.checksum_type = NO_CHECKSUMMING;
 	/* init QDIO stuff */
 	qeth_init_qdio_info(card);
 	return 0;
