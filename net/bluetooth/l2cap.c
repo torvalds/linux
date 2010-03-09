a573f82deb59d887f256beb482f224071fcf9a2e
Nick Pelly <npelly@google.com>
2010-03-09 12:08:00 -0800

    Revert "Bluetooth: Introduce L2CAP_LM_FLUSHABLE to allow flushing of ACL packets."
    This reverts commit d7897fd1e9fb3a5df0740dc2dc45ec94ca0965f2.
    
    Change-Id: I3401550b6dc97b683104e9fdac30a617a2db8c8e
    Signed-off-by: Nick Pelly <npelly@google.com>

--- net/bluetooth/l2cap.c
+++ net/bluetooth/l2cap.c
@@ -360,7 +360,7 @@ static inline void l2cap_send_cmd(struct l2cap_conn *conn, u8 ident, u8 code, u1
 	if (!skb)
 		return;
 
-	hci_send_acl(conn->hcon, skb, ACL_START);
+	hci_send_acl(conn->hcon, skb, 0);
 }
 
 static inline void l2cap_send_sframe(struct l2cap_pinfo *pi, u16 control)
@@ -900,7 +900,6 @@ static void l2cap_sock_init(struct sock *sk, struct sock *parent)
 		pi->sec_level = l2cap_pi(parent)->sec_level;
 		pi->role_switch = l2cap_pi(parent)->role_switch;
 		pi->force_reliable = l2cap_pi(parent)->force_reliable;
-		pi->flushable = l2cap_pi(parent)->flushable;
 	} else {
 		pi->imtu = L2CAP_DEFAULT_MTU;
 		pi->omtu = 0;
@@ -916,7 +915,6 @@ static void l2cap_sock_init(struct sock *sk, struct sock *parent)
 		pi->sec_level = BT_SECURITY_LOW;
 		pi->role_switch = 0;
 		pi->force_reliable = 0;
-		pi->flushable = 0;
 	}
 
 	/* Default config options */
@@ -1435,16 +1433,10 @@ static void l2cap_drop_acked_frames(struct sock *sk)
 static inline void l2cap_do_send(struct sock *sk, struct sk_buff *skb)
 {
 	struct l2cap_pinfo *pi = l2cap_pi(sk);
-	u16 flags;
 
 	BT_DBG("sk %p, skb %p len %d", sk, skb, skb->len);
 
-	if (pi->flushable)
-		flags = ACL_START_FLUSHABLE;
-	else
-		flags = ACL_START;
-
-	hci_send_acl(pi->conn->hcon, skb, flags);
+	hci_send_acl(pi->conn->hcon, skb, 0);
 }
 
 static void l2cap_streaming_send(struct sock *sk)
@@ -2024,7 +2016,6 @@ static int l2cap_sock_setsockopt_old(struct socket *sock, int optname, char __us
 
 		l2cap_pi(sk)->role_switch    = (opt & L2CAP_LM_MASTER);
 		l2cap_pi(sk)->force_reliable = (opt & L2CAP_LM_RELIABLE);
-		l2cap_pi(sk)->flushable = (opt & L2CAP_LM_FLUSHABLE);
 		break;
 
 	default:
@@ -2154,9 +2145,6 @@ static int l2cap_sock_getsockopt_old(struct socket *sock, int optname, char __us
 
 		if (l2cap_pi(sk)->force_reliable)
 			opt |= L2CAP_LM_RELIABLE;
-
-		if (l2cap_pi(sk)->flushable)
-			opt |= L2CAP_LM_FLUSHABLE;
 
 		if (put_user(opt, (u32 __user *) optval))
 			err = -EFAULT;
@@ -4657,7 +4645,7 @@ static int l2cap_recv_acldata(struct hci_conn *hcon, struct sk_buff *skb, u16 fl
 
 	BT_DBG("conn %p len %d flags 0x%x", conn, skb->len, flags);
 
-	if (!(flags & ACL_CONT)) {
+	if (flags & ACL_START) {
 		struct l2cap_hdr *hdr;
 		int len;
 
