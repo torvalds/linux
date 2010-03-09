d2197e1e6038bb01568367a220e4a4e040f8337d
Tony Lindgren <tony@atomide.com>
2010-03-09 14:59:48 -0800

    omap3: Fix compile for Touch Book early_param
    Commit 2b0d8c251b8876d530a6bf671eb5425838fa698a changed ARM to use
    the common early_param code. Fix compile for Touch Book accordingly.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-omap3touchbook.c
+++ arch/arm/mach-omap2/board-omap3touchbook.c
@@ -518,14 +518,14 @@ static void omap3_touchbook_poweroff(void)
 	gpio_direction_output(TB_KILL_POWER_GPIO, 0);
 }
 
-static void __init early_touchbook_revision(char **p)
+static int __init early_touchbook_revision(char *p)
 {
-	if (!*p)
-		return;
+	if (!p)
+		return 0;
 
-	strict_strtoul(*p, 10, &touchbook_revision);
+	return strict_strtoul(p, 10, &touchbook_revision);
 }
-__early_param("tbr=", early_touchbook_revision);
+early_param("tbr", early_touchbook_revision);
 
 static struct omap_musb_board_data musb_board_data = {
 	.interface_type		= MUSB_INTERFACE_ULPI,
