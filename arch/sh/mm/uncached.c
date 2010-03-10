5b34d1ee1e51d61e779a25d28808e8ad824cea3d
Paul Mundt <lethal@linux-sh.org>
2010-03-10 16:46:58 +0900

    sh: Export uncached helper symbols.
    oprofile and others need to get at these, so provide symbol exports.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/mm/uncached.c
+++ arch/sh/mm/uncached.c
@@ -1,4 +1,5 @@
 #include <linux/init.h>
+#include <linux/module.h>
 #include <asm/sizes.h>
 #include <asm/page.h>
 #include <asm/addrspace.h>
@@ -16,11 +17,14 @@
 unsigned long cached_to_uncached = SZ_512M;
 unsigned long uncached_size = SZ_512M;
 unsigned long uncached_start, uncached_end;
+EXPORT_SYMBOL(uncached_start);
+EXPORT_SYMBOL(uncached_end);
 
 int virt_addr_uncached(unsigned long kaddr)
 {
 	return (kaddr >= uncached_start) && (kaddr < uncached_end);
 }
+EXPORT_SYMBOL(virt_addr_uncached);
 
 void __init uncached_init(void)
 {
