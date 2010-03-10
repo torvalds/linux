7dc693330c1fda407643b95c876fc072845c0593
Mike Lockwood <lockwood@android.com>
2010-03-10 17:05:03 -0500

    USB: gadget: f_rndis: Add platform data for RNDIS vendor ID and MAC address.
    This fixes a kernel panic in rndis.c when receiving the
    OID_GEN_VENDOR_DESCRIPTION command.
    
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- include/linux/usb/android_composite.h
+++ include/linux/usb/android_composite.h
@@ -18,6 +18,7 @@
 #define	__LINUX_USB_ANDROID_H
 
 #include <linux/usb/composite.h>
+#include <linux/if_ether.h>
 
 struct android_usb_function {
 	struct list_head	list;
@@ -78,6 +79,13 @@ struct usb_mass_storage_platform_data {
 
 	/* number of LUNS */
 	int nluns;
+};
+
+/* Platform data for USB ethernet driver. */
+struct usb_ether_platform_data {
+	u8	ethaddr[ETH_ALEN];
+	u32	vendorID;
+	const char *vendorDescr;
 };
 
 extern void android_usb_set_connected(int on);
