8d43d1bc81190ee8e4e694d805afa586339b0afc
Chaithrika U S <chaithrika@ti.com>
2010-03-10 14:48:33 +0530

    ASoC: DaVinci: Add hw_param callback for S/PDIF DIT link
    On TI DM6467 EVM, S/PDIF DIT codec fails to open as it is unable to install
    hardware params. This dummy codec has no set_fmt and set_sysclk implementations
    and calls from the application to these functions cause errors. This patch adds
    a new hardware params callback function for S/PDIF transciever codec.
    
    Signed-off-by: Chaithrika U S <chaithrika@ti.com>
    Tested-by: Anuj Aggarwal <anuj.aggarwal@ti.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/davinci/davinci-evm.c
+++ sound/soc/davinci/davinci-evm.c
@@ -81,8 +81,22 @@ static int evm_hw_params(struct snd_pcm_substream *substream,
 	return 0;
 }
 
+static int evm_spdif_hw_params(struct snd_pcm_substream *substream,
+				struct snd_pcm_hw_params *params)
+{
+	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_dai *cpu_dai = rtd->dai->cpu_dai;
+
+	/* set cpu DAI configuration */
+	return snd_soc_dai_set_fmt(cpu_dai, AUDIO_FORMAT);
+}
+
 static struct snd_soc_ops evm_ops = {
 	.hw_params = evm_hw_params,
+};
+
+static struct snd_soc_ops evm_spdif_ops = {
+	.hw_params = evm_spdif_hw_params,
 };
 
 /* davinci-evm machine dapm widgets */
@@ -165,7 +179,7 @@ static struct snd_soc_dai_link dm6467_evm_dai[] = {
 		.stream_name = "spdif",
 		.cpu_dai = &davinci_mcasp_dai[DAVINCI_MCASP_DIT_DAI],
 		.codec_dai = &dit_stub_dai,
-		.ops = &evm_ops,
+		.ops = &evm_spdif_ops,
 	},
 };
 static struct snd_soc_dai_link da8xx_evm_dai = {
