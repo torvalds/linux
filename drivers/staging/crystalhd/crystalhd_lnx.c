15df6385d940ad5486b4c2de8ab45979b13349fe
Jani Nikula <ext-jani.1.nikula@nokia.com>
2010-03-11 18:22:51 +0200

    Staging: crystalhd: fix device_create() return value check
    Use IS_ERR() instead of comparing to NULL.
    
    Signed-off-by: Jani Nikula <ext-jani.1.nikula@nokia.com>
    Cc: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/crystalhd/crystalhd_lnx.c
+++ drivers/staging/crystalhd/crystalhd_lnx.c
@@ -376,7 +376,7 @@ static int __devinit chd_dec_init_chdev(struct crystalhd_adp *adp)
 
 	dev = device_create(crystalhd_class, NULL, MKDEV(adp->chd_dec_major, 0),
 			    NULL, "crystalhd");
-	if (!dev) {
+	if (IS_ERR(dev)) {
 		BCMLOG_ERR("failed to create device\n");
 		goto device_create_fail;
 	}
