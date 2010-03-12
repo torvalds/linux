132631d07fb7ba4bac964cbcc16a50ea77dd5f56
Michael Krufky <mkrufky@kernellabs.com>
2010-03-12 00:56:32 -0300

    V4L/DVB: lgdt3305: Jarod Wilson gets the credit for LGDT3304 support
    Signed-off-by: Michael Krufky <mkrufky@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/lgdt3305.c
+++ drivers/media/dvb/frontends/lgdt3305.c
@@ -3,6 +3,8 @@
  *
  *    Copyright (C) 2008, 2009 Michael Krufky <mkrufky@linuxtv.org>
  *
+ *    LGDT3304 support by Jarod Wilson <jarod@redhat.com>
+ *
  *    This program is free software; you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
  *    the Free Software Foundation; either version 2 of the License, or
