566c0284389aa5d097356ae016cf04d3aa837195
Jan Beulich <JBeulich@novell.com>
2010-03-15 10:11:15 +0000

    x86: Fix placement of FIX_OHCI1394_BASE
    commit ff30a0543e9a6cd732582063e7cae951cdb7acf2 upstream.
    
    Ever for 32-bit with sufficiently high NR_CPUS, and starting
    with commit 789d03f584484af85dbdc64935270c8e45f36ef7 also for
    64-bit, the statically allocated early fixmap page tables were
    not covering FIX_OHCI1394_BASE, leading to a boot time crash
    when "ohci1394_dma=early" was used. Despite this entry not being
    a permanently used one, it needs to be moved into the permanent
    range since it has to be close to FIX_DBGP_BASE and
    FIX_EARLYCON_MEM_BASE.
    
    Reported-bisected-and-tested-by: Justin P. Mattock <justinmattock@gmail.com>
    Fixes-bug: http://bugzilla.kernel.org/show_bug.cgi?id=14487
    Signed-off-by: Jan Beulich <jbeulich@novell.com>
    LKML-Reference: <4B9E15D30200007800034D23@vpn.id2.novell.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- arch/x86/include/asm/fixmap.h
+++ arch/x86/include/asm/fixmap.h
@@ -82,6 +82,9 @@ enum fixed_addresses {
 #endif
 	FIX_DBGP_BASE,
 	FIX_EARLYCON_MEM_BASE,
+#ifdef CONFIG_PROVIDE_OHCI1394_DMA_INIT
+	FIX_OHCI1394_BASE,
+#endif
 #ifdef CONFIG_X86_LOCAL_APIC
 	FIX_APIC_BASE,	/* local (CPU) APIC) -- required for SMP or not */
 #endif
@@ -126,9 +129,6 @@ enum fixed_addresses {
 	FIX_BTMAP_END = __end_of_permanent_fixed_addresses + 256 -
 			(__end_of_permanent_fixed_addresses & 255),
 	FIX_BTMAP_BEGIN = FIX_BTMAP_END + NR_FIX_BTMAPS*FIX_BTMAPS_SLOTS - 1,
-#ifdef CONFIG_PROVIDE_OHCI1394_DMA_INIT
-	FIX_OHCI1394_BASE,
-#endif
 #ifdef CONFIG_X86_32
 	FIX_WP_TEST,
 #endif
