1d1c397db95f1c0cd95f6fa633c1e68acfaacec3
Jan Engelhardt <jengelh@medozas.de>
2010-03-16 21:09:04 +0100

    netfilter: xtables: clean up xt_mac match routine
    Signed-off-by: Jan Engelhardt <jengelh@medozas.de>

--- net/netfilter/xt_mac.c
+++ net/netfilter/xt_mac.c
@@ -26,14 +26,16 @@ MODULE_ALIAS("ip6t_mac");
 
 static bool mac_mt(const struct sk_buff *skb, const struct xt_match_param *par)
 {
-    const struct xt_mac_info *info = par->matchinfo;
+	const struct xt_mac_info *info = par->matchinfo;
+	bool ret;
 
-    /* Is mac pointer valid? */
-    return skb_mac_header(skb) >= skb->head &&
-	   skb_mac_header(skb) + ETH_HLEN <= skb->data
-	   /* If so, compare... */
-	   && ((!compare_ether_addr(eth_hdr(skb)->h_source, info->srcaddr))
-		^ info->invert);
+	if (skb_mac_header(skb) < skb->head)
+		return false;
+	if (skb_mac_header(skb) + ETH_HLEN > skb->data)
+		return false;
+	ret  = compare_ether_addr(eth_hdr(skb)->h_source, info->srcaddr) == 0;
+	ret ^= info->invert;
+	return ret;
 }
 
 static struct xt_match mac_mt_reg __read_mostly = {
