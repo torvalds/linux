275f83d836c2bbb13288eaa822c2e833e0e02e38
Dmitry Shmidt <dimitrysh@google.com>
2010-03-18 16:04:18 -0700

    wlan: Extract generic wlan platform data from tiwlan specific header
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- include/linux/wlan_plat.h
+++ include/linux/wlan_plat.h
@@ -1,6 +1,6 @@
-/* include/linux/wifi_tiwlan.h
+/* include/linux/wlan_plat.h
  *
- * Copyright (C) 2008 Google, Inc.
+ * Copyright (C) 2010 Google, Inc.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -12,16 +12,14 @@
  * GNU General Public License for more details.
  *
  */
-#ifndef _LINUX_WIFI_TIWLAN_H_
-#define _LINUX_WIFI_TIWLAN_H_
+#ifndef _LINUX_WLAN_PLAT_H_
+#define _LINUX_WLAN_PLAT_H_
 
-#include <linux/wlan_plat.h>
-
-#define WMPA_NUMBER_OF_SECTIONS	3
-#define WMPA_NUMBER_OF_BUFFERS	160
-#define WMPA_SECTION_HEADER	24
-#define WMPA_SECTION_SIZE_0	(WMPA_NUMBER_OF_BUFFERS * 64)
-#define WMPA_SECTION_SIZE_1	(WMPA_NUMBER_OF_BUFFERS * 256)
-#define WMPA_SECTION_SIZE_2	(WMPA_NUMBER_OF_BUFFERS * 2048)
+struct wifi_platform_data {
+	int (*set_power)(int val);
+	int (*set_reset)(int val);
+	int (*set_carddetect)(int val);
+	void *(*mem_prealloc)(int section, unsigned long size);
+};
 
 #endif
