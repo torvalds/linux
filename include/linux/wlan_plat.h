31401634fa02629ed95c0dc004367d954772e6b1
Dmitry Shmidt <dimitrysh@google.com>
2010-03-18 16:04:18 -0700

    ANDROID: wlan: Create generic wlan platform data header
    Change-Id: I233559218cc40acb28b57315ea25c08a9c866725
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

---
+++ include/linux/wlan_plat.h
@@ -0,0 +1,25 @@
+/* include/linux/wlan_plat.h
+ *
+ * Copyright (C) 2010 Google, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#ifndef _LINUX_WLAN_PLAT_H_
+#define _LINUX_WLAN_PLAT_H_
+
+struct wifi_platform_data {
+	int (*set_power)(int val);
+	int (*set_reset)(int val);
+	int (*set_carddetect)(int val);
+	void *(*mem_prealloc)(int section, unsigned long size);
+};
+
+#endif
