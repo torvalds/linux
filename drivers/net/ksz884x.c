ba7e94a44afbbf4c494e00b844f7bbfb144e7f6d
Huang Weiyi <weiyi.huang@gmail.com>
2010-03-18 20:24:51 -0700

    net: remove unused #include <linux/version.h>
    Remove unused #include <linux/version.h>('s) in
      drivers/net/ksz884x.c
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ksz884x.c
+++ drivers/net/ksz884x.c
@@ -19,7 +19,6 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/version.h>
 #include <linux/ioport.h>
 #include <linux/pci.h>
 #include <linux/proc_fs.h>
