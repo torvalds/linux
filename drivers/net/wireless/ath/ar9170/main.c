1d3ee54a9a486cb279a61590f3d84e878aeb88c0
Christian Lamparter <chunkeey@googlemail.com>
2010-03-23 21:51:14 +0100

    ar9170usb: fix panic triggered by undersized rxstream buffer
    commit 879999cec9489f8942ebce3ec1b5f23ef948dda7 upstream.
    
    While ar9170's USB transport packet size is currently set to 8KiB,
    the PHY is capable of receiving AMPDUs with up to 64KiB.
    Such a large frame will be split over several rx URBs and
    exceed the previously allocated space for rx stream reconstruction.
    
    This patch increases the buffer size to 64KiB which is
    in fact the phy & rx stream designed size limit.
    
    Bugzilla: https://bugzilla.kernel.org/show_bug.cgi?id=15591
    Reported-by: Christian Mehlis <mehlis@inf.fu-berlin.de>
    Signed-off-by: Christian Lamparter <chunkeey@googlemail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/net/wireless/ath/ar9170/main.c
+++ drivers/net/wireless/ath/ar9170/main.c
@@ -2526,7 +2526,7 @@ void *ar9170_alloc(size_t priv_size)
 	 * tends to split the streams into seperate rx descriptors.
 	 */
 
-	skb = __dev_alloc_skb(AR9170_MAX_RX_BUFFER_SIZE, GFP_KERNEL);
+	skb = __dev_alloc_skb(AR9170_RX_STREAM_MAX_SIZE, GFP_KERNEL);
 	if (!skb)
 		goto err_nomem;
 
