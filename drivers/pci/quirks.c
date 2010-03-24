134b345081534235dbf228b1005c14590e0570ba
Matthew Wilcox <matthew@wil.cx>
2010-03-24 07:11:01 -0600

    PCI quirk: Disable MSI on VIA K8T890 systems
    Bugzilla 15287 indicates that there's a problem with Message Signalled
    Interrupts on VIA K8T890 systems.  Add a quirk to disable MSI on these
    systems.
    
    Signed-off-by: Matthew Wilcox <willy@linux.intel.com>
    Tested-by: Jan Kreuzer <kontrollator@gmx.de>
    Tested-by: lh <jarryson@gmail.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -2122,6 +2122,7 @@ static void __devinit quirk_disable_msi(struct pci_dev *dev)
 	}
 }
 DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_AMD, PCI_DEVICE_ID_AMD_8131_BRIDGE, quirk_disable_msi);
+DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_VIA, 0xa238, quirk_disable_msi);
 
 /* Go through the list of Hypertransport capabilities and
  * return 1 if a HT MSI capability is found and enabled */
