c9fbf7e070bbf9cc7adc1420df87706c62cb04ed
Mark Brown <broonie@opensource.wolfsonmicro.com>
2010-03-26 16:49:15 +0000

    mfd: Add WM8994 interrupt controller support
    The WM8994 has an interrupt controller which supports interrupts for
    both CODEC and GPIO portions of the chip. Support this using genirq,
    while allowing for systems that do not have an interrupt hooked up.
    
    Wrapper functions are provided for the IRQ request and free to simplify
    the code in consumer drivers when handling cases where IRQs are not
    set up.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Samuel Ortiz <sameo@linux.intel.com>

--- include/linux/mfd/wm8994/pdata.h
+++ include/linux/mfd/wm8994/pdata.h
@@ -70,6 +70,7 @@ struct wm8994_pdata {
 
 	struct wm8994_ldo_pdata ldo[WM8994_NUM_LDO];
 
+	int irq_base;  /** Base IRQ number for WM8994, required for IRQs */
 
         int num_drc_cfgs;
         struct wm8994_drc_cfg *drc_cfgs;
