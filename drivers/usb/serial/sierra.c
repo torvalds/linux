a559bbbda22d8905fe71cbcb9f1ea11b8a9668a7
William Lightning <kassah@gmail.com>
2010-03-26 10:51:20 -0700

    USB: Add id for HP ev2210 a.k.a Sierra MC5725 miniPCI-e Cell Modem.
    commit cfbaa39347b34837f26e01fe8f4f8dbbae60b520 upstream.
    
    Signed-off-by: William Lightning <kassah@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/sierra.c
+++ drivers/usb/serial/sierra.c
@@ -195,6 +195,7 @@ static const struct sierra_iface_info direct_ip_interface_blacklist = {
 static struct usb_device_id id_table [] = {
 	{ USB_DEVICE(0x0F3D, 0x0112) }, /* Airprime/Sierra PC 5220 */
 	{ USB_DEVICE(0x03F0, 0x1B1D) },	/* HP ev2200 a.k.a MC5720 */
+	{ USB_DEVICE(0x03F0, 0x211D) }, /* HP ev2210 a.k.a MC5725 */
 	{ USB_DEVICE(0x03F0, 0x1E1D) },	/* HP hs2300 a.k.a MC8775 */
 
 	{ USB_DEVICE(0x1199, 0x0017) },	/* Sierra Wireless EM5625 */
