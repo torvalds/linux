1b132ea03e169ce5eca75b00224a78a6782740e8
Stephen Rothwell <sfr@canb.auug.org.au>
2010-03-29 15:32:18 +1100

    ASoC: update for removeal of slab.h from percpu.h
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/soc/codecs/ad193x.c
+++ sound/soc/codecs/ad193x.c
@@ -12,6 +12,7 @@
 #include <linux/device.h>
 #include <linux/i2c.h>
 #include <linux/spi/spi.h>
+#include <linux/slab.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
