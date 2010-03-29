4d7a5644e4adfafe76c2bd8ee168e3f3b5dae3a8
Dean Nelson <dnelson@redhat.com>
2010-03-29 22:03:00 +0200

    hwmon: (coretemp) Add missing newline to dev_warn() message
    Add missing newline to dev_warn() message string. This is more of an issue
    with older kernels that don't automatically add a newline if it was missing
    from the end of the previous line.
    
    Signed-off-by: Dean Nelson <dnelson@redhat.com>
    Cc: stable@kernel.org
    Signed-off-by: Jean Delvare <khali@linux-fr.org>

--- drivers/hwmon/coretemp.c
+++ drivers/hwmon/coretemp.c
@@ -228,7 +228,7 @@ static int __devinit adjust_tjmax(struct cpuinfo_x86 *c, u32 id, struct device *
 		if (err) {
 			dev_warn(dev,
 				 "Unable to access MSR 0xEE, for Tjmax, left"
-				 " at default");
+				 " at default\n");
 		} else if (eax & 0x40000000) {
 			tjmax = tjmax_ee;
 		}
