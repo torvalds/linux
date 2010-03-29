2f2efe38d1da236b3509aa4af0bdc6fdfb22bf0b
Stefan Bader <stefan.bader@canonical.com>
2010-03-29 17:53:12 +0200

    drm/i915: Add no_lvds entry for the Clientron U800
    commit 9875557ee8247c3f7390d378c027b45c7535a224 upstream.
    
    BugLink: http://bugs.launchpad.net/ubuntu/bugs/544671
    
    This system claims to have a LVDS but has not.
    
    Signed-off-by: Stephane Graber <stgraber@ubuntu.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/gpu/drm/i915/intel_lvds.c
+++ drivers/gpu/drm/i915/intel_lvds.c
@@ -885,6 +885,14 @@ static const struct dmi_system_id intel_no_lvds[] = {
 			DMI_MATCH(DMI_PRODUCT_VERSION, "AO00001JW"),
 		},
 	},
+	{
+		.callback = intel_no_lvds_dmi_callback,
+		.ident = "Clientron U800",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Clientron"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "U800"),
+		},
+	},
 
 	{ }	/* terminating entry */
 };
