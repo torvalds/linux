5acbbd428db47b12f137a8a2aa96b3c0a96b744e
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2010-03-30 22:35:50 +0000

    net: change illegal_highdma to use dma_mask
    Robert Hancock pointed out two problems about NETIF_F_HIGHDMA:
    
    -Many drivers only set the flag when they detect they can use 64-bit DMA,
    since otherwise they could receive DMA addresses that they can't handle
    (which on platforms without IOMMU/SWIOTLB support is fatal). This means that if
    64-bit support isn't available, even buffers located below 4GB will get copied
    unnecessarily.
    
    -Some drivers set the flag even though they can't actually handle 64-bit DMA,
    which would mean that on platforms without IOMMU/SWIOTLB they would get a DMA
    mapping error if the memory they received happened to be located above 4GB.
    
    http://lkml.org/lkml/2010/3/3/530
    
    We can use the dma_mask if we need bouncing or not here. Then we can
    safely fix drivers that misuse NETIF_F_HIGHDMA.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -129,6 +129,7 @@
 #include <linux/jhash.h>
 #include <linux/random.h>
 #include <trace/events/napi.h>
+#include <linux/pci.h>
 
 #include "net-sysfs.h"
 
@@ -1804,14 +1805,21 @@ static inline int illegal_highdma(struct net_device *dev, struct sk_buff *skb)
 {
 #ifdef CONFIG_HIGHMEM
 	int i;
+	if (!(dev->features & NETIF_F_HIGHDMA)) {
+		for (i = 0; i < skb_shinfo(skb)->nr_frags; i++)
+			if (PageHighMem(skb_shinfo(skb)->frags[i].page))
+				return 1;
+	}
 
-	if (dev->features & NETIF_F_HIGHDMA)
-		return 0;
+	if (PCI_DMA_BUS_IS_PHYS) {
+		struct device *pdev = dev->dev.parent;
 
-	for (i = 0; i < skb_shinfo(skb)->nr_frags; i++)
-		if (PageHighMem(skb_shinfo(skb)->frags[i].page))
-			return 1;
-
+		for (i = 0; i < skb_shinfo(skb)->nr_frags; i++) {
+			dma_addr_t addr = page_to_phys(skb_shinfo(skb)->frags[i].page);
+			if (!pdev->dma_mask || addr + PAGE_SIZE - 1 > *pdev->dma_mask)
+				return 1;
+		}
+	}
 #endif
 	return 0;
 }
