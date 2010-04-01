8c0236fc465c71d98203bcf5609db01b9cf5f70f
viresh kumar <viresh.kumar@st.com>
2010-04-01 12:30:46 +0100

    ARM: 6014/1: ST SPEAr: Added clock framework for SPEAr platform and machines
    Clock framework for SPEAr is based upon clkdev framework for ARM
    
    Reviewed-by: Linus Walleij <linux.walleij@stericsson.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@st.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

---
+++ arch/arm/mach-spear3xx/include/mach/clkdev.h
@@ -0,0 +1,19 @@
+/*
+ * arch/arm/mach-spear3xx/include/mach/clkdev.h
+ *
+ * Clock Dev framework definitions for SPEAr3xx machine family
+ *
+ * Copyright (C) 2009 ST Microelectronics
+ * Viresh Kumar<viresh.kumar@st.com>
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2. This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#ifndef __MACH_CLKDEV_H
+#define __MACH_CLKDEV_H
+
+#include <plat/clkdev.h>
+
+#endif /* __MACH_CLKDEV_H */
