69b7f6ff855e32ec353475ce7b7246d293816078
Gregory Bean <gbean@codeaurora.org>
2010-04-04 22:29:02 -0700

    msm: add MSM8x60 FFA support
    The MSM8X60 FFA contains different components than the MSM8X60 SURF,
    and therefore requires a different ARCH type and machine ID.
    
    Signed-off-by: Gregory Bean <gbean@codeaurora.org>
    Signed-off-by: Daniel Walker <dwalker@codeaurora.org>

--- arch/arm/mach-msm/board-msm8x60.c
+++ arch/arm/mach-msm/board-msm8x60.c
@@ -91,3 +91,10 @@ MACHINE_START(MSM8X60_SIM, "QCT MSM8X60 SIMULATOR")
 	.init_machine = msm8x60_init,
 	.timer = &msm_timer,
 MACHINE_END
+
+MACHINE_START(MSM8X60_FFA, "QCT MSM8X60 FFA")
+	.map_io = msm8x60_map_io,
+	.init_irq = msm8x60_init_irq,
+	.init_machine = msm8x60_init,
+	.timer = &msm_timer,
+MACHINE_END
