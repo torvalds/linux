6549981bc54777c37eccf987e227aff47022ab7c
Stephane Chatty <chatty@lii-enac.fr>
2010-04-06 22:22:58 +0200

    HID: fix N-trig touch panel with recent firmware
    Added an init message that avoids device freeze with recent firmware.
    
    Signed-off-by: Stephane Chatty <chatty@enac.fr>
    Tested-by: Rafi Rubin <rafi@seas.upenn.edu>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-ntrig.c
+++ drivers/hid/hid-ntrig.c
@@ -1,8 +1,8 @@
 /*
  *  HID driver for N-Trig touchscreens
  *
- *  Copyright (c) 2008 Rafi Rubin
- *  Copyright (c) 2009 Stephane Chatty
+ *  Copyright (c) 2008-2010 Rafi Rubin
+ *  Copyright (c) 2009-2010 Stephane Chatty
  *
  */
 
@@ -15,6 +15,8 @@
 
 #include <linux/device.h>
 #include <linux/hid.h>
+#include <linux/usb.h>
+#include "usbhid/usbhid.h"
 #include <linux/module.h>
 #include <linux/slab.h>
 
@@ -286,6 +288,7 @@ static int ntrig_probe(struct hid_device *hdev, const struct hid_device_id *id)
 	struct ntrig_data *nd;
 	struct hid_input *hidinput;
 	struct input_dev *input;
+	struct hid_report *report;
 
 	if (id->driver_data)
 		hdev->quirks |= HID_QUIRK_MULTI_INPUT;
@@ -348,6 +351,11 @@ static int ntrig_probe(struct hid_device *hdev, const struct hid_device_id *id)
 			break;
 		}
 	}
+
+	report = hdev->report_enum[HID_FEATURE_REPORT].report_id_hash[0x0a]; 
+	if (report)
+		usbhid_submit_report(hdev, report, USB_DIR_OUT);
+
 
 	return 0;
 err_free:
