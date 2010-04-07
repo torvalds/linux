2b080c5df00e431baec09222aa8d623f0cd77314
Dan Carpenter <error27@gmail.com>
2010-04-07 06:21:05 -0300

    V4L/DVB: davinci: don't return under lock on error path
    If the kmalloc() failed for "ccdc_cfg = kmalloc(...);" then we would exit
    with the lock held.  I moved the mutex_lock() below the allocation
    because it isn't protecting anything in that block and allocations are
    allocations are sometimes slow.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Acked-by: Vaibhav Hiremath <hvaibhav@ti.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/davinci/vpfe_capture.c
+++ drivers/media/video/davinci/vpfe_capture.c
@@ -1861,7 +1861,6 @@ static __init int vpfe_probe(struct platform_device *pdev)
 		goto probe_free_dev_mem;
 	}
 
-	mutex_lock(&ccdc_lock);
 	/* Allocate memory for ccdc configuration */
 	ccdc_cfg = kmalloc(sizeof(struct ccdc_config), GFP_KERNEL);
 	if (NULL == ccdc_cfg) {
@@ -1869,6 +1868,8 @@ static __init int vpfe_probe(struct platform_device *pdev)
 			 "Memory allocation failed for ccdc_cfg\n");
 		goto probe_free_lock;
 	}
+
+	mutex_lock(&ccdc_lock);
 
 	strncpy(ccdc_cfg->name, vpfe_cfg->ccdc, 32);
 	/* Get VINT0 irq resource */
