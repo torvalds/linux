f7a711971edd952352a89698db1d36f469e25f77
Avi Kivity <avi@redhat.com>
2010-04-11 15:33:32 +0300

    KVM: Fix MAXPHYADDR calculation when cpuid does not support it
    MAXPHYADDR is derived from cpuid 0x80000008, but when that isn't present, we
    get some random value.
    
    Fix by checking first that cpuid 0x80000008 is supported.
    
    Acked-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Signed-off-by: Avi Kivity <avi@redhat.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4171,9 +4171,13 @@ int cpuid_maxphyaddr(struct kvm_vcpu *vcpu)
 {
 	struct kvm_cpuid_entry2 *best;
 
+	best = kvm_find_cpuid_entry(vcpu, 0x80000000, 0);
+	if (!best || best->eax < 0x80000008)
+		goto not_found;
 	best = kvm_find_cpuid_entry(vcpu, 0x80000008, 0);
 	if (best)
 		return best->eax & 0xff;
+not_found:
 	return 36;
 }
 
