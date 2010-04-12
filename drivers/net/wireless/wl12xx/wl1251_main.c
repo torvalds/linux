afa5ec27f4d69952d4ae11f182258bf53d85fcf5
Grazvydas Ignotas <notasas@gmail.com>
2010-04-13 01:21:40 +0300

    wl1251: don't require NVS data when EEPROM is used
    If EEPROM is used, NVS data is now loaded but ignored.
    Stop loading it to avoid need of dummy NVS file for modules with EEPROM.
    
    Signed-off-by: Grazvydas Ignotas <notasas@gmail.com>
    Acked-by: Kalle Valo <kvalo@adurom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/wl1251_main.c
+++ drivers/net/wireless/wl12xx/wl1251_main.c
@@ -201,8 +201,8 @@ static int wl1251_chip_wakeup(struct wl1251 *wl)
 			goto out;
 	}
 
-	/* No NVS from netlink, try to get it from the filesystem */
-	if (wl->nvs == NULL) {
+	if (wl->nvs == NULL && !wl->use_eeprom) {
+		/* No NVS from netlink, try to get it from the filesystem */
 		ret = wl1251_fetch_nvs(wl);
 		if (ret < 0)
 			goto out;
