aec9562f3db8ece218125042e0b2b7ac5b7091e7
Anders Larsen <al@alarsen.net>
2010-04-13 12:05:32 +0100

    ARM: 6053/1: AT91: Save power by disabling the processor clock when CPU is idle
    Disable the processor clock when the CPU is idle.
    
    This saves much more power than merely entering 'Wait for Interrupt' mode.
    Since JTAG-debugging doesn't work when the processor clock is switched off,
    make it conditional on CONFIG_DEBUG_KERNEL.
    
    Signed-off-by: Anders Larsen <al@alarsen.net>
    Acked-by: Andrew Victor <avictor.za@gmail.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-at91/include/mach/system.h
+++ arch/arm/mach-at91/include/mach/system.h
@@ -24,21 +24,24 @@
 #include <mach/hardware.h>
 #include <mach/at91_st.h>
 #include <mach/at91_dbgu.h>
+#include <mach/at91_pmc.h>
 
 static inline void arch_idle(void)
 {
+#ifndef CONFIG_DEBUG_KERNEL
 	/*
 	 * Disable the processor clock.  The processor will be automatically
 	 * re-enabled by an interrupt or by a reset.
 	 */
-//	at91_sys_write(AT91_PMC_SCDR, AT91_PMC_PCK);
-
+	at91_sys_write(AT91_PMC_SCDR, AT91_PMC_PCK);
+#else
 	/*
 	 * Set the processor (CP15) into 'Wait for Interrupt' mode.
 	 * Unlike disabling the processor clock via the PMC (above)
 	 *  this allows the processor to be woken via JTAG.
 	 */
 	cpu_do_idle();
+#endif
 }
 
 void (*at91_arch_reset)(void);
