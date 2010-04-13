63f1032b97638fc2b023fa968b7703c8ebe6d6d9
Michal Simek <monstr@monstr.eu>
2010-04-13 08:59:37 +0200

    microblaze: Quiet section mismatch warnings for MMU version
    Remove section mismatch - based on ppc aproach.
    
    WARNING: vmlinux.o(.text+0x64834): Section mismatch in reference
    from the function __pte_alloc_kernel() to the function .init.text:early_get_page()
    The function __pte_alloc_kernel() references
    the function __init early_get_page().
    This is often because __pte_alloc_kernel lacks a __init
    annotation or the annotation of early_get_page is wrong.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/mm/pgtable.c
+++ arch/microblaze/mm/pgtable.c
@@ -274,3 +274,18 @@ unsigned long iopa(unsigned long addr)
 
 	return pa;
 }
+
+__init_refok pte_t *pte_alloc_one_kernel(struct mm_struct *mm,
+		unsigned long address)
+{
+	pte_t *pte;
+	if (mem_init_done) {
+		pte = (pte_t *)__get_free_page(GFP_KERNEL |
+					__GFP_REPEAT | __GFP_ZERO);
+	} else {
+		pte = (pte_t *)early_get_page();
+		if (pte)
+			clear_page(pte);
+	}
+	return pte;
+}
