5cac7861b2de95a1f714ebdc652813abd0afcc73
Maciej Trela <Maciej.Trela@intel.com>
2010-04-14 17:17:39 +1000

    md: notify level changes through sysfs.
    Level changes can be very significant, so make sure
    to notify them via sysfs.
    
    Signed-off-by: Maciej Trela <maciej.trela@intel.com>
    Signed-off-by: NeilBrown <neilb@suse.de>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -3059,6 +3059,7 @@ level_store(mddev_t *mddev, const char *buf, size_t len)
 	set_bit(MD_CHANGE_DEVS, &mddev->flags);
 	set_bit(MD_RECOVERY_NEEDED, &mddev->recovery);
 	md_wakeup_thread(mddev->thread);
+	sysfs_notify(&mddev->kobj, NULL, "level");
 	return rv;
 }
 
