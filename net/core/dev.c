ab9304717f7624c41927f442e6b6d418b2d8b3e4
Jiri Pirko <jpirko@redhat.com>
2010-04-20 01:45:37 -0700

    net: emphasize rtnl lock required in call_netdevice_notifiers
    Since netdev_chain is guarded by rtnl_lock, ASSERT_RTNL should be
    present here to make sure that all callers of call_netdevice_notifiers
    does the locking properly.
    
    Signed-off-by: Jiri Pirko <jpirko@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -1435,6 +1435,7 @@ EXPORT_SYMBOL(unregister_netdevice_notifier);
 
 int call_netdevice_notifiers(unsigned long val, struct net_device *dev)
 {
+	ASSERT_RTNL();
 	return raw_notifier_call_chain(&netdev_chain, val, dev);
 }
 
