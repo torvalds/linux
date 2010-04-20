6b72e3934b42930fd40fc42fe762d21be413301c
Thomas Renninger <trenn@suse.de>
2010-04-20 13:17:35 +0200

    [CPUFREQ] acpi-cpufreq: Fix CPU_ANY CPUFREQ_{PRE,POST}CHANGE notification
    Signed-off-by: Thomas Renninger <trenn@suse.de>
    CC: venki@google.com
    CC: davej@redhat.com
    CC: arjan@infradead.org
    CC: linux-kernel@vger.kernel.org
    Signed-off-by: Dave Jones <davej@redhat.com>

--- arch/x86/kernel/cpu/cpufreq/acpi-cpufreq.c
+++ arch/x86/kernel/cpu/cpufreq/acpi-cpufreq.c
@@ -351,7 +351,7 @@ static int acpi_cpufreq_target(struct cpufreq_policy *policy,
 
 	freqs.old = perf->states[perf->state].core_frequency * 1000;
 	freqs.new = data->freq_table[next_state].frequency;
-	for_each_cpu(i, cmd.mask) {
+	for_each_cpu(i, policy->cpus) {
 		freqs.cpu = i;
 		cpufreq_notify_transition(&freqs, CPUFREQ_PRECHANGE);
 	}
@@ -367,7 +367,7 @@ static int acpi_cpufreq_target(struct cpufreq_policy *policy,
 		}
 	}
 
-	for_each_cpu(i, cmd.mask) {
+	for_each_cpu(i, policy->cpus) {
 		freqs.cpu = i;
 		cpufreq_notify_transition(&freqs, CPUFREQ_POSTCHANGE);
 	}
