fed3c6551a8993a38fffd17eed4a6f15ee4fad21
Daniel T Chen <crimsun@ubuntu.com>
2010-04-21 19:55:43 -0400

    ALSA: hda: Use LPIB quirk for DG965OT board version AAD63733-203
    commit 0e0280dc2b0c7395a880d25544b47f3e3e3f79db upstream.
    
    BugLink: https://launchpad.net/bugs/459083
    
    The OR has verified with 2.6.32.11 and the latest alsa-driver stable
    daily snapshot that position_fix=1 is necessary for the external mic
    to work and for PulseAudio not to crash constantly.
    
    This patch is necessary also for 2.6.32.11 and 2.6.33.2.
    
    Reported-by: <imwithid@yahoo.com>
    Tested-by: <imwithid@yahoo.com>
    Signed-off-by: Daniel T Chen <crimsun@ubuntu.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -2236,6 +2236,7 @@ static struct snd_pci_quirk position_fix_list[] __devinitdata = {
 	SND_PCI_QUIRK(0x1462, 0x1002, "MSI Wind U115", POS_FIX_LPIB),
 	SND_PCI_QUIRK(0x1565, 0x820f, "Biostar Microtech", POS_FIX_LPIB),
 	SND_PCI_QUIRK(0x1565, 0x8218, "Biostar Microtech", POS_FIX_LPIB),
+	SND_PCI_QUIRK(0x8086, 0x2503, "DG965OT AAD63733-203", POS_FIX_LPIB),
 	SND_PCI_QUIRK(0x8086, 0xd601, "eMachines T5212", POS_FIX_LPIB),
 	{}
 };
