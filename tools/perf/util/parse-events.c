5710fcad7c367adefe5634dc998f1f88780a8457
Stephane Eranian <eranian@google.com>
2010-04-21 18:06:01 +0200

    perf: Fix initialization bug in parse_single_tracepoint_event()
    The parse_single_tracepoint_event() was setting some attributes
    before it validated the event was indeed a tracepoint event. This
    caused problems with other initialization routines like in the
    builtin-top.c module whereby sample_period is not set if not 0.
    
    Signed-off-by: Stephane Eranian <eranian@google.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Ingo Molnar <mingo@elte.hu>
    LKML-Reference: <4bcf232b.698fd80a.6fbe.ffffb737@mx.google.com>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>

--- tools/perf/util/parse-events.c
+++ tools/perf/util/parse-events.c
@@ -418,12 +418,6 @@ parse_single_tracepoint_event(char *sys_name,
 	u64 id;
 	int fd;
 
-	attr->sample_type |= PERF_SAMPLE_RAW;
-	attr->sample_type |= PERF_SAMPLE_TIME;
-	attr->sample_type |= PERF_SAMPLE_CPU;
-
-	attr->sample_period = 1;
-
 	snprintf(evt_path, MAXPATHLEN, "%s/%s/%s/id", debugfs_path,
 		 sys_name, evt_name);
 
@@ -441,6 +435,13 @@ parse_single_tracepoint_event(char *sys_name,
 	attr->config = id;
 	attr->type = PERF_TYPE_TRACEPOINT;
 	*strp = evt_name + evt_length;
+
+	attr->sample_type |= PERF_SAMPLE_RAW;
+	attr->sample_type |= PERF_SAMPLE_TIME;
+	attr->sample_type |= PERF_SAMPLE_CPU;
+
+	attr->sample_period = 1;
+
 
 	return EVT_HANDLED;
 }
