1fafc7a9353ef68e1b8d4bb130cb6402cf7dfd5a
YOSHIFUJI Hideaki / 吉藤英明 <yoshfuji@linux-ipv6.org>
2010-04-25 08:06:40 +0000

    bridge br_multicast: Ensure to initialize BR_INPUT_SKB_CB(skb)->mrouters_only.
    Even with commit 32dec5dd0233ebffa9cae25ce7ba6daeb7df4467 ("bridge
    br_multicast: Don't refer to BR_INPUT_SKB_CB(skb)->mrouters_only
    without IGMP snooping."), BR_INPUT_SKB_CB(skb)->mrouters_only is
    not appropriately initialized if IGMP/MLD snooping support is
    compiled and disabled, so we can see garbage.
    
    Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bridge/br_multicast.c
+++ net/bridge/br_multicast.c
@@ -1340,9 +1340,6 @@ static int br_multicast_ipv4_rcv(struct net_bridge *br,
 	unsigned offset;
 	int err;
 
-	BR_INPUT_SKB_CB(skb)->igmp = 0;
-	BR_INPUT_SKB_CB(skb)->mrouters_only = 0;
-
 	/* We treat OOM as packet loss for now. */
 	if (!pskb_may_pull(skb, sizeof(*iph)))
 		return -EINVAL;
@@ -1439,9 +1436,6 @@ static int br_multicast_ipv6_rcv(struct net_bridge *br,
 	unsigned len;
 	unsigned offset;
 	int err;
-
-	BR_INPUT_SKB_CB(skb)->igmp = 0;
-	BR_INPUT_SKB_CB(skb)->mrouters_only = 0;
 
 	if (!pskb_may_pull(skb, sizeof(*ip6h)))
 		return -EINVAL;
@@ -1550,6 +1544,9 @@ out:
 int br_multicast_rcv(struct net_bridge *br, struct net_bridge_port *port,
 		     struct sk_buff *skb)
 {
+	BR_INPUT_SKB_CB(skb)->igmp = 0;
+	BR_INPUT_SKB_CB(skb)->mrouters_only = 0;
+
 	if (br->multicast_disabled)
 		return 0;
 
