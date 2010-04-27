af56b1c27b909e06318d26416d7b21fe6483224a
Jassi Brar <jassi.brar@samsung.com>
2010-04-27 15:57:11 +0900

    ASoC: S3C64XX: IISv4: Add CPU driver
    Add the CPU driver for the IISv4 block found on S3C6410.
    For now, the driver is almost a copy of s3c64xx-i2s.c but
    it should diverge as more IISv4 specific stuff is added.
    
    Signed-off-by: Jassi Brar <jassi.brar@samsung.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/s3c24xx/s3c64xx-i2s.h
+++ sound/soc/s3c24xx/s3c64xx-i2s.h
@@ -37,5 +37,6 @@ struct clk;
 	 SNDRV_PCM_FMTBIT_S24_LE)
 
 extern struct snd_soc_dai s3c64xx_i2s_dai[];
+extern struct snd_soc_dai s3c64xx_i2s_v4_dai;
 
 #endif /* __SND_SOC_S3C24XX_S3C64XX_I2S_H */
