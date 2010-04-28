9414e99672271adcc661f3c160a30b374179b92f
Phil Carmody <ext-phil.2.carmody@nokia.com>
2010-04-28 12:09:16 -0500

    oprofile: protect from not being in an IRQ context
    http://lkml.org/lkml/2010/4/27/285
    
    Protect against dereferencing regs when it's NULL, and
    force a magic number into pc to prevent too deep processing.
    This approach permits the dropped samples to be tallied as
    invalid Instruction Pointer events.
    
    e.g. output from about 15mins at 10kHz sample rate:
    Nr. samples received: 2565380
    Nr. samples lost invalid pc: 4
    
    Signed-off-by: Phil Carmody <ext-phil.2.carmody@nokia.com>
    Signed-off-by: Robert Richter <robert.richter@amd.com>

--- drivers/oprofile/cpu_buffer.c
+++ drivers/oprofile/cpu_buffer.c
@@ -319,8 +319,16 @@ void oprofile_add_ext_sample(unsigned long pc, struct pt_regs * const regs,
 
 void oprofile_add_sample(struct pt_regs * const regs, unsigned long event)
 {
-	int is_kernel = !user_mode(regs);
-	unsigned long pc = profile_pc(regs);
+	int is_kernel;
+	unsigned long pc;
+
+	if (likely(regs)) {
+		is_kernel = !user_mode(regs);
+		pc = profile_pc(regs);
+	} else {
+		is_kernel = 0;    /* This value will not be used */
+		pc = ESCAPE_CODE; /* as this causes an early return. */
+	}
 
 	__oprofile_add_ext_sample(pc, regs, event, is_kernel);
 }
