7bc14acfbb86b00a93ddb1c40d9d2050cd7188fe
Dima Zavin <dima@android.com>
2010-04-27 20:57:04 -0700

    [ARM] Do not call flush_cache_user_range with mmap_sem held
    We can't be holding the mmap_sem while calling flush_cache_user_range
    because the flush can fault. If we fault on a user address, the
    page fault handler will try to take mmap_sem again. Since both places
    acquire the read lock, most of the time it succeeds. However, if another
    thread tries to acquire the write lock on the mmap_sem (e.g. mmap) in
    between the call to flush_cache_user_range and the fault, the down_read
    in do_page_fault will deadlock.
    
    Also, since we really can't be holding the mmap_sem while calling
    flush_cache_user_range AND vma is actually unused by the flush itself,
    get rid of vma as an argument.
    
    Change-Id: If55409bde41ad1060fa4fe7cbd4ac530d4d9a106
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/include/asm/cacheflush.h
+++ arch/arm/include/asm/cacheflush.h
@@ -381,7 +381,7 @@ extern void flush_ptrace_access(struct vm_area_struct *vma, struct page *page,
  * Harvard caches are synchronised for the user space address range.
  * This is used for the ARM private sys_cacheflush system call.
  */
-#define flush_cache_user_range(vma,start,end) \
+#define flush_cache_user_range(start,end) \
 	__cpuc_coherent_user_range((start) & PAGE_MASK, PAGE_ALIGN(end))
 
 /*
