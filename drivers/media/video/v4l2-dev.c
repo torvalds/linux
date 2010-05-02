dd0daf2a6fb6bec436a3ef68bd585ea09a2a54b7
Hans Verkuil <hverkuil@xs4all.nl>
2010-05-02 05:11:32 -0300

    V4L/DVB: v4l2-dev: remove unnecessary lock around atomic clear_bit
    No need to lock when unregistering the device: clear_bit is already an
    atomic operation.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/v4l2-dev.c
+++ drivers/media/video/v4l2-dev.c
@@ -600,9 +600,7 @@ void video_unregister_device(struct video_device *vdev)
 	if (!vdev || !video_is_registered(vdev))
 		return;
 
-	mutex_lock(&videodev_lock);
 	clear_bit(V4L2_FL_REGISTERED, &vdev->flags);
-	mutex_unlock(&videodev_lock);
 	device_unregister(&vdev->dev);
 }
 EXPORT_SYMBOL(video_unregister_device);
