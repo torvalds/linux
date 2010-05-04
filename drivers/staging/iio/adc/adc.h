1e3864e63576e33dd1b26ab750ffaab3066aaba9
Barry Song <Barry.Song@analog.com>
2010-05-04 14:43:14 +0100

    staging:iio:imu ADIS16300 driver
    Signed-off-by: Barry Song <Barry.Song@analog.com>
    Acked-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/adc/adc.h
+++ drivers/staging/iio/adc/adc.h
@@ -16,6 +16,9 @@
 #define IIO_DEV_ATTR_IN_RAW(_num, _show, _addr)				\
 	IIO_DEVICE_ATTR(in##_num##_raw, S_IRUGO, _show, NULL, _addr)
 
+#define IIO_DEV_ATTR_IN_NAMED_RAW(_name, _show, _addr)			\
+	IIO_DEVICE_ATTR(in_##_name##_raw, S_IRUGO, _show, NULL, _addr)
+
 #define IIO_DEV_ATTR_IN_DIFF_RAW(_nump, _numn, _show, _addr)		\
 	IIO_DEVICE_ATTR_NAMED(in##_nump##min##_numn##_raw,		\
 			      in##_nump-in##_numn##_raw,		\
