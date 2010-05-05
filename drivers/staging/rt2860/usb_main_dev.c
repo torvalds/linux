de37cd49b5a54facef174cf34496919857436e8f
Nobuhiro KUSUNO <n-kusuno@fc4.so-net.ne.jp>
2010-05-06 05:23:28 +0900

    Staging: rt2870: add device ID of MelCo.,Inc. WLI-UC-G301N
    My wireless LAN module 'MelCo.,Inc. WLI-UC-G301N' works fine,
    if the following line is added into 2870_main_dev.c.
    
    Signed-off-by: Nobhiro KUSUNO <n-kusuno@fc4.so-net.ne.jp>
    Cc: stable <stable@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rt2860/usb_main_dev.c
+++ drivers/staging/rt2860/usb_main_dev.c
@@ -97,6 +97,7 @@ struct usb_device_id rtusb_usb_id[] = {
 	{USB_DEVICE(0x5A57, 0x0282)},	/* Zinwell */
 	{USB_DEVICE(0x7392, 0x7718)},
 	{USB_DEVICE(0x7392, 0x7717)},
+	{USB_DEVICE(0x0411, 0x016f)},	/* MelCo.,Inc. WLI-UC-G301N */
 	{USB_DEVICE(0x1737, 0x0070)},	/* Linksys WUSB100 */
 	{USB_DEVICE(0x1737, 0x0071)},	/* Linksys WUSB600N */
 	{USB_DEVICE(0x0411, 0x00e8)},	/* Buffalo WLI-UC-G300N */
