41f04b17b6409026fda01942628496ec7dca2f06
Daniel Walker <dwalker@codeaurora.org>
2010-05-06 14:18:06 -0700

    msm: physical offset for MSM7X30
    The MSM7x30 does not have a separate bank of memory for shared
    memory communication with the radio CPU.  Set the kernel base
    address 2MB in, to use this first 2MB for this purpose.
    
    Signed-off-by: Daniel Walker <dwalker@codeaurora.org>
    Signed-off-by: David Brown <davidb@codeaurora.org>
    Signed-off-by: Gregory Bean <gbean@codeaurora.org>
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>

--- arch/arm/mach-msm/include/mach/memory.h
+++ arch/arm/mach-msm/include/mach/memory.h
@@ -21,6 +21,8 @@
 #define PHYS_OFFSET		UL(0x00000000)
 #elif defined(CONFIG_ARCH_QSD8X50)
 #define PHYS_OFFSET		UL(0x20000000)
+#elif defined(CONFIG_ARCH_MSM7X30)
+#define PHYS_OFFSET		UL(0x00200000)
 #else
 #define PHYS_OFFSET		UL(0x10000000)
 #endif
