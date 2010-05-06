81d658ac64000758c198c6b31fed505ec284e165
Daniel Walker <dwalker@codeaurora.org>
2010-05-06 14:16:04 -0700

    msm: physical offset for QSD8x50
    Support different RAM base addresses used by Qualcomm SOCs, with
    QSD8x50 as the first addtional one.
    
    Signed-off-by: Daniel Walker <dwalker@codeaurora.org>
    Signed-off-by: David Brown <davidb@codeaurora.org>
    Signed-off-by: Gregory Bean <gbean@codeaurora.org>
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>

--- arch/arm/mach-msm/include/mach/memory.h
+++ arch/arm/mach-msm/include/mach/memory.h
@@ -17,7 +17,13 @@
 #define __ASM_ARCH_MEMORY_H
 
 /* physical offset of RAM */
+#if defined(CONFIG_ARCH_QSD8X50) && defined(CONFIG_MSM_SOC_REV_A)
+#define PHYS_OFFSET		UL(0x00000000)
+#elif defined(CONFIG_ARCH_QSD8X50)
+#define PHYS_OFFSET		UL(0x20000000)
+#else
 #define PHYS_OFFSET		UL(0x10000000)
+#endif
 
 #endif
 
