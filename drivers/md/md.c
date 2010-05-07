fdb07a230a9f146bad017b8ec577a25c29ca1856
NeilBrown <neilb@suse.de>
2010-05-07 19:44:26 +1000

    md: restore ability of spare drives to spin down.
    commit 1176568de7e066c0be9e46c37503b9fd4730edcf upstream.
    
    Some time ago we stopped the clean/active metadata updates
    from being written to a 'spare' device in most cases so that
    it could spin down and say spun down.  Device failure/removal
    etc are still recorded on spares.
    
    However commit 51d5668cb2e3fd1827a55 broke this 50% of the time,
    depending on whether the event count is even or odd.
    The change log entry said:
    
       This means that the alignment between 'odd/even' and
        'clean/dirty' might take a little longer to attain,
    
    how ever the code makes no attempt to create that alignment, so it
    could take arbitrarily long.
    
    So when we find that clean/dirty is not aligned with odd/even,
    force a second metadata-update immediately.  There are already cases
    where a second metadata-update is needed immediately (e.g. when a
    device fails during the metadata update).  We just piggy-back on that.
    
    Reported-by: Joe Bryant <tenminjoe@yahoo.com>
    Signed-off-by: NeilBrown <neilb@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -2011,12 +2011,18 @@ repeat:
 		if (!mddev->in_sync || mddev->recovery_cp != MaxSector) { /* not clean */
 			/* .. if the array isn't clean, an 'even' event must also go
 			 * to spares. */
-			if ((mddev->events&1)==0)
+			if ((mddev->events&1)==0) {
 				nospares = 0;
+				sync_req = 2; /* force a second update to get the
+					       * even/odd in sync */
+			}
 		} else {
 			/* otherwise an 'odd' event must go to spares */
-			if ((mddev->events&1))
+			if ((mddev->events&1)) {
 				nospares = 0;
+				sync_req = 2; /* force a second update to get the
+					       * even/odd in sync */
+			}
 		}
 	}
 
