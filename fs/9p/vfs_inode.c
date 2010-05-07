fe5bd0736bcdf35bd6cc300211a97c2fef8bd83e
Dan Carpenter <error27@gmail.com>
2010-05-07 08:26:23 +0000

    9p: cleanup: remove unneeded assignment
    We never use "v9ses" and so we can remove it.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Signed-off-by: Eric Van Hensbergen <ericvh@gmail.com>

--- fs/9p/vfs_inode.c
+++ fs/9p/vfs_inode.c
@@ -456,14 +456,12 @@ static int v9fs_remove(struct inode *dir, struct dentry *file, int rmdir)
 {
 	int retval;
 	struct inode *file_inode;
-	struct v9fs_session_info *v9ses;
 	struct p9_fid *v9fid;
 
 	P9_DPRINTK(P9_DEBUG_VFS, "inode: %p dentry: %p rmdir: %d\n", dir, file,
 		rmdir);
 
 	file_inode = file->d_inode;
-	v9ses = v9fs_inode2v9ses(file_inode);
 	v9fid = v9fs_fid_clone(file);
 	if (IS_ERR(v9fid))
 		return PTR_ERR(v9fid);
