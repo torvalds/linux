1f976996c7413b5b48d35b195cc2d7adcd3f68d7
Axel Lin <axel.lin@gmail.com>
2010-05-10 09:29:58 +0800

    backlight: 88pm860x_bl: fix error handling in pm860x_backlight_probe
    This patch  properly unregisters a previously registered backlight device object
    in error handling
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Signed-off-by: Richard Purdie <rpurdie@linux.intel.com>

--- drivers/video/backlight/88pm860x_bl.c
+++ drivers/video/backlight/88pm860x_bl.c
@@ -267,6 +267,7 @@ static int pm860x_backlight_probe(struct platform_device *pdev)
 	backlight_update_status(bl);
 	return 0;
 out:
+	backlight_device_unregister(bl);
 	kfree(data);
 	return ret;
 }
