0581b52e898f3f5472820940537d91d80895db5a
Roger Quadros <roger.quadros@nokia.com>
2010-05-12 11:48:47 +0300

    OMAP: RX51: Add "vdds_sdi" supply voltage for SDI
    The SDI Display subsystem needs access to the vdds_sdi supply
    regulator. This is TWL4030's VAUX1 supply on RX-51.
    
    Signed-off-by: Roger Quadros <roger.quadros@nokia.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@nokia.com>

--- arch/arm/mach-omap2/board-rx51-peripherals.c
+++ arch/arm/mach-omap2/board-rx51-peripherals.c
@@ -316,6 +316,19 @@ static struct regulator_consumer_supply rx51_vio_supplies[] = {
 	},
 };
 
+#if defined(CONFIG_FB_OMAP2) || defined(CONFIG_FB_OMAP2_MODULE)
+extern struct platform_device rx51_display_device;
+#endif
+
+static struct regulator_consumer_supply rx51_vaux1_consumers[] = {
+#if defined(CONFIG_FB_OMAP2) || defined(CONFIG_FB_OMAP2_MODULE)
+	{
+		.supply	= "vdds_sdi",
+		.dev	= &rx51_display_device.dev,
+	},
+#endif
+};
+
 static struct regulator_init_data rx51_vaux1 = {
 	.constraints = {
 		.name			= "V28",
@@ -326,6 +339,8 @@ static struct regulator_init_data rx51_vaux1 = {
 		.valid_ops_mask		= REGULATOR_CHANGE_MODE
 					| REGULATOR_CHANGE_STATUS,
 	},
+	.num_consumer_supplies	= ARRAY_SIZE(rx51_vaux1_consumers),
+	.consumer_supplies	= rx51_vaux1_consumers,
 };
 
 static struct regulator_init_data rx51_vaux2 = {
