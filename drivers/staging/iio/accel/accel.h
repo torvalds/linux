06f1962ab475bdee3ae17afbaecee5b23f3cd5f0
Barry Song <Barry.Song@analog.com>
2010-05-16 21:11:37 +0100

    Staging: iio: adis16220 vibration sensor driver
    Signed-off-by: Barry Song <Barry.Song@analog.com>
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/accel/accel.h
+++ drivers/staging/iio/accel/accel.h
@@ -2,6 +2,9 @@
 #include "../sysfs.h"
 
 /* Accelerometer types of attribute */
+#define IIO_DEV_ATTR_ACCEL_OFFSET(_mode, _show, _store, _addr)	\
+	IIO_DEVICE_ATTR(accel_offset, _mode, _show, _store, _addr)
+
 #define IIO_DEV_ATTR_ACCEL_X_OFFSET(_mode, _show, _store, _addr)	\
 	IIO_DEVICE_ATTR(accel_x_offset, _mode, _show, _store, _addr)
 
@@ -19,6 +22,9 @@
 
 #define IIO_DEV_ATTR_ACCEL_Z_GAIN(_mode, _show, _store, _addr)		\
 	IIO_DEVICE_ATTR(accel_z_gain, _mode, _show, _store, _addr)
+
+#define IIO_DEV_ATTR_ACCEL(_show, _addr)			\
+	IIO_DEVICE_ATTR(accel_raw, S_IRUGO, _show, NULL, _addr)
 
 #define IIO_DEV_ATTR_ACCEL_X(_show, _addr)			\
 	IIO_DEVICE_ATTR(accel_x_raw, S_IRUGO, _show, NULL, _addr)
