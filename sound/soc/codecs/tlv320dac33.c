2d4cdd6fc974716555fcbaf7ec1d4dda22784c1b
Peter Ujfalusi <peter.ujfalusi@nokia.com>
2010-05-17 14:21:46 +0300

    ASoC: tlv320dac33: Avoid powering off while in BIAS_OFF
    Avoid calling the dac33_hard_power when the codec was
    already in BIAS_OFF state.
    This could happen in device suspend and module removal
    time.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@nokia.com>
    Acked-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- sound/soc/codecs/tlv320dac33.c
+++ sound/soc/codecs/tlv320dac33.c
@@ -589,6 +589,9 @@ static int dac33_set_bias_level(struct snd_soc_codec *codec,
 		}
 		break;
 	case SND_SOC_BIAS_OFF:
+		/* Do not power off, when the codec is already off */
+		if (codec->bias_level == SND_SOC_BIAS_OFF)
+			return 0;
 		ret = dac33_hard_power(codec, 0);
 		if (ret != 0)
 			return ret;
