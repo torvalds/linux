40cb989fcf9f0caf6a116562132c2fffed38d261
Atsushi Nemoto <anemo@mba.ocn.ne.jp>
2010-05-18 00:38:05 +0900

    MIPS: TX49xx: Define ARCH_KMALLOC_MINALIGN
    With SLAB, it works without ARCH_KMALLOC_MINALIGN, but with SLOB/SLUB,
    ARCH_KMALLOC_MINALIGN is required to ensure alignment of kmalloced
    buffer.
    
    Signed-off-by: Atsushi Nemoto <anemo@mba.ocn.ne.jp>
    To: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/1248/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/mach-tx49xx/kmalloc.h
+++ arch/mips/include/asm/mach-tx49xx/kmalloc.h
@@ -1,8 +1,6 @@
 #ifndef __ASM_MACH_TX49XX_KMALLOC_H
 #define __ASM_MACH_TX49XX_KMALLOC_H
 
-/*
- * All happy, no need to define ARCH_KMALLOC_MINALIGN
- */
+#define ARCH_KMALLOC_MINALIGN	L1_CACHE_BYTES
 
 #endif /* __ASM_MACH_TX49XX_KMALLOC_H */
