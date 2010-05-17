552b1439945f590f8e525d13ffbd286d0ec6a475
罗伟 <lw@rock-chips.com>
2010-05-17 09:09:37 +0000

    modify rk2818 adc

--- arch/arm/mach-rk2818/rk2818_adc.c
+++ arch/arm/mach-rk2818/rk2818_adc.c
@@ -1,4 +1,4 @@
-/* arch/staging/rk2818/rk2818_adc.c
+/* drivers/staging/rk2818/rk2818_adc.c
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
