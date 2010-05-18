9e4ed5c394e64a40adc55797e885dae571e32800
Jassi Brar <jassi.brar@samsung.com>
2010-05-18 16:02:39 +0900

    ARM: S5PC100: Add audio platform devices
    Define platform devices for all audio devices found on S5PC100
    
    Signed-off-by: Jassi Brar <jassi.brar@samsung.com>
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/plat-samsung/include/plat/devs.h
+++ arch/arm/plat-samsung/include/plat/devs.h
@@ -80,6 +80,13 @@ extern struct platform_device s5p6442_device_iis1;
 extern struct platform_device s5p6440_device_pcm;
 extern struct platform_device s5p6440_device_iis;
 
+extern struct platform_device s5pc100_device_ac97;
+extern struct platform_device s5pc100_device_pcm0;
+extern struct platform_device s5pc100_device_pcm1;
+extern struct platform_device s5pc100_device_iis0;
+extern struct platform_device s5pc100_device_iis1;
+extern struct platform_device s5pc100_device_iis2;
+
 /* s3c2440 specific devices */
 
 #ifdef CONFIG_CPU_S3C2440
