3839f7ce6dc749e3170e1bfa656cfac748528117
Wey-Yi Guy <wey-yi.w.guy@intel.com>
2010-05-18 09:18:06 -0700

    iwlwifi: do not use huge command buffer for channel switch
    Channel switch host command do not need to allocate huge command buffer
    since its size is already included in the iwl_device_cmd structure.
    
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-5000.c
+++ drivers/net/wireless/iwlwifi/iwl-5000.c
@@ -285,7 +285,7 @@ static int iwl5000_hw_channel_switch(struct iwl_priv *priv,
 	struct iwl_host_cmd hcmd = {
 		.id = REPLY_CHANNEL_SWITCH,
 		.len = sizeof(cmd),
-		.flags = CMD_SIZE_HUGE,
+		.flags = CMD_SYNC,
 		.data = &cmd,
 	};
 
