b1e89955be882bab65997c2227872211f805be80
Alberto Panizzo <maramaopercheseimorto@gmail.com>
2010-05-19 11:33:43 +0200

    MX3X: Add Keypad device definition for MX3X arch
    This patch adds also the pin definition helpers in iomux-mx3.h
    
    Signed-off-by: Alberto Panizzo <maramaopercheseimorto@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-mx3/devices.h
+++ arch/arm/mach-mx3/devices.h
@@ -28,3 +28,4 @@ extern struct platform_device imx_ssi_device1;
 extern struct platform_device imx_ssi_device1;
 extern struct platform_device imx_wdt_device0;
 extern struct platform_device imx_rtc_device0;
+extern struct platform_device imx_kpp_device;
