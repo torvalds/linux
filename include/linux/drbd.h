3d42b3612891baecf709d93f28655a6882a65d41
Philipp Reisner <philipp.reisner@linbit.com>
2010-05-20 12:14:54 +0200

    drbd: This is now equivalent to drbd release 8.3.8rc1
    Signed-off-by: Philipp Reisner <philipp.reisner@linbit.com>
    Signed-off-by: Lars Ellenberg <lars.ellenberg@linbit.com>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- include/linux/drbd.h
+++ include/linux/drbd.h
@@ -53,10 +53,10 @@
 
 
 extern const char *drbd_buildtag(void);
-#define REL_VERSION "8.3.7"
+#define REL_VERSION "8.3.8rc1"
 #define API_VERSION 88
 #define PRO_VERSION_MIN 86
-#define PRO_VERSION_MAX 93
+#define PRO_VERSION_MAX 94
 
 
 enum drbd_io_error_p {
