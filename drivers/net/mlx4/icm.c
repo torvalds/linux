c0dc72bad9cf21071f5e4005de46f7c8b67a138a
Sebastien Dugue <sebastien.dugue@bull.net>
2010-05-20 15:58:22 -0700

    mlx4_core: Fix possible chunk sg list overflow in mlx4_alloc_icm()
    If the number of sg entries in the ICM chunk reaches MLX4_ICM_CHUNK_LEN,
    we must set chunk to NULL even for coherent mappings so that the next
    time through the loop will allocate another chunk.  Otherwise we'll
    overflow the sg list the next time through the loop.  This will lead to
    memory corruption if this case is hit.
    
    mthca does not have this bug.
    
    Signed-off-by: Sebastien Dugue <sebastien.dugue@bull.net>
    Cc: <stable@kernel.org>
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/net/mlx4/icm.c
+++ drivers/net/mlx4/icm.c
@@ -175,9 +175,10 @@ struct mlx4_icm *mlx4_alloc_icm(struct mlx4_dev *dev, int npages,
 
 				if (chunk->nsg <= 0)
 					goto fail;
-
-				chunk = NULL;
 			}
+
+			if (chunk->npages == MLX4_ICM_CHUNK_LEN)
+				chunk = NULL;
 
 			npages -= 1 << cur_order;
 		} else {
