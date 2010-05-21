bc57117856cf1e581135810b37d3b75f9d1749f5
Borislav Petkov <borislav.petkov@amd.com>
2010-05-21 21:25:03 +0200

    amd64_edac: Correct scrub rate setting
    Exit early when setting scrub rate on unknown/unsupported families.
    
    Cc: <stable@kernel.org> # 32.x 33.x 34.x
    Signed-off-by: Borislav Petkov <borislav.petkov@amd.com>
    Acked-by: Doug Thompson <dougthompson@xmission.com>

--- drivers/edac/amd64_edac.c
+++ drivers/edac/amd64_edac.c
@@ -178,7 +178,7 @@ static int amd64_set_scrub_rate(struct mem_ctl_info *mci, u32 *bandwidth)
 
 	default:
 		amd64_printk(KERN_ERR, "Unsupported family!\n");
-		break;
+		return -EINVAL;
 	}
 	return amd64_search_set_scrub_rate(pvt->misc_f3_ctl, *bandwidth,
 			min_scrubrate);
