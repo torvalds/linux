0d9ec762af297f1ef38114f9498322d994063802
Michal Simek <monstr@monstr.eu>
2010-05-25 13:44:38 +0200

    microblaze: Trace hardirqs
    Add trace_hardirqs_off and trace_hardirqs_on to do_IRQ function.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/kernel/irq.c
+++ arch/microblaze/kernel/irq.c
@@ -37,6 +37,7 @@ void __irq_entry do_IRQ(struct pt_regs *regs)
 {
 	unsigned int irq;
 	struct pt_regs *old_regs = set_irq_regs(regs);
+	trace_hardirqs_off();
 
 	irq_enter();
 	irq = get_irq(regs);
@@ -53,6 +54,7 @@ next_irq:
 
 	irq_exit();
 	set_irq_regs(old_regs);
+	trace_hardirqs_on();
 }
 
 int show_interrupts(struct seq_file *p, void *v)
