c743f38013aeff58ef6252601e397b5ba281c633
Nicolas Pitre <nico@fluxnic.net>
2010-05-24 23:55:42 -0400

    ARM: initial stack protector (-fstack-protector) support
    This is the very basic stuff without the changing canary upon
    task switch yet.  Just the Kconfig option and a constant canary
    value initialized at boot time.
    
    Signed-off-by: Nicolas Pitre <nicolas.pitre@linaro.org>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -37,6 +37,12 @@
 #include <asm/stacktrace.h>
 #include <asm/mach/time.h>
 
+#ifdef CONFIG_CC_STACKPROTECTOR
+#include <linux/stackprotector.h>
+unsigned long __stack_chk_guard __read_mostly;
+EXPORT_SYMBOL(__stack_chk_guard);
+#endif
+
 static const char *processor_modes[] = {
   "USER_26", "FIQ_26" , "IRQ_26" , "SVC_26" , "UK4_26" , "UK5_26" , "UK6_26" , "UK7_26" ,
   "UK8_26" , "UK9_26" , "UK10_26", "UK11_26", "UK12_26", "UK13_26", "UK14_26", "UK15_26",
