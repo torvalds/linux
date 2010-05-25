556ae19110f2de5ace4733e0c19e5fa01fad08b3
Sarveshwar Bandi <sarveshwarb@serverengines.com>
2010-05-24 18:38:25 -0700

    be2net: Bug fix in init code in probe
    PCI function reset needs to invoked after fw init ioctl is issued.
    
    Signed-off-by: Sarveshwar Bandi <sarveshwarb@serverengines.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/benet/be_main.c
+++ drivers/net/benet/be_main.c
@@ -2487,16 +2487,18 @@ static int __devinit be_probe(struct pci_dev *pdev,
 		status = be_cmd_POST(adapter);
 		if (status)
 			goto ctrl_clean;
-
-		status = be_cmd_reset_function(adapter);
-		if (status)
-			goto ctrl_clean;
 	}
 
 	/* tell fw we're ready to fire cmds */
 	status = be_cmd_fw_init(adapter);
 	if (status)
 		goto ctrl_clean;
+
+	if (be_physfn(adapter)) {
+		status = be_cmd_reset_function(adapter);
+		if (status)
+			goto ctrl_clean;
+	}
 
 	status = be_stats_init(adapter);
 	if (status)
