6e1be45aa6ba6a36c0312f65ecf311135c73001d
Oleg Nesterov <oleg@redhat.com>
2010-05-26 14:43:21 -0700

    check_unshare_flags: kill the bogus CLONE_SIGHAND/sig->count check
    check_unshare_flags(CLONE_SIGHAND) adds CLONE_THREAD to *flags_ptr if the
    task is multithreaded to ensure unshare_thread() will fail.
    
    Not only this is a bit strange way to return the error, this is absolutely
    meaningless.  If signal->count > 1 then sighand->count must be also > 1,
    and unshare_sighand() will fail anyway.
    
    In fact, all CLONE_THREAD/SIGHAND/VM checks inside sys_unshare() do not
    look right.  Fortunately this code doesn't really work anyway.
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Cc: Balbir Singh <balbir@linux.vnet.ibm.com>
    Acked-by: Roland McGrath <roland@redhat.com>
    Cc: Veaceslav Falico <vfalico@redhat.com>
    Cc: Stanislaw Gruszka <sgruszka@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1518,14 +1518,6 @@ static void check_unshare_flags(unsigned long *flags_ptr)
 		*flags_ptr |= CLONE_SIGHAND;
 
 	/*
-	 * If unsharing signal handlers and the task was created
-	 * using CLONE_THREAD, then must unshare the thread
-	 */
-	if ((*flags_ptr & CLONE_SIGHAND) &&
-	    (atomic_read(&current->signal->count) > 1))
-		*flags_ptr |= CLONE_THREAD;
-
-	/*
 	 * If unsharing namespace, must also unshare filesystem information.
 	 */
 	if (*flags_ptr & CLONE_NEWNS)
