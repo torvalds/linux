5369fba13611118bc380674a410bede0863566f2
Mike Frysinger <vapier@gentoo.org>
2010-05-27 22:40:26 +0000

    Blackfin: merge anomalies 475 and 220 to follow official lists
    Design found that these anomalies had the same root issue, so they've
    merged 475 into 220.  We need to do the same to update to the latest
    anomaly sheets.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- arch/blackfin/mach-common/arch_checks.c
+++ arch/blackfin/mach-common/arch_checks.c
@@ -57,12 +57,6 @@
 
 /* if 220 exists, can not set External Memory WB and L2 not_cached, either External Memory not_cached and L2 WB */
 #if ANOMALY_05000220 && \
-	((defined(CONFIG_BFIN_EXTMEM_WRITEBACK) && !defined(CONFIG_BFIN_L2_DCACHEABLE)) || \
-	 (!defined(CONFIG_BFIN_EXTMEM_DCACHEABLE) && defined(CONFIG_BFIN_L2_WRITEBACK)))
-# error You are exposing Anomaly 220 in this config, either config L2 as Write Through, or make External Memory WB.
-#endif
-
-#if ANOMALY_05000475 && \
 	(defined(CONFIG_BFIN_EXTMEM_WRITEBACK) || defined(CONFIG_BFIN_L2_WRITEBACK))
-# error "Anomaly 475 does not allow you to use Write Back cache with L2 or External Memory"
+# error "Anomaly 05000220 does not allow you to use Write Back cache with L2 or External Memory"
 #endif
