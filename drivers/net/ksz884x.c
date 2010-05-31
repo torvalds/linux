96ed741e15896eea43f7203523db88bc8105c359
Denis Kirjanov <dkirjanov@hera.kernel.org>
2010-05-31 00:26:21 +0000

    ksz884x: Add missing validate_addr hook
    Add missing validate_addr hook
    
    Signed-off-by: Denis Kirjanov <dkirjanov@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ksz884x.c
+++ drivers/net/ksz884x.c
@@ -6863,6 +6863,7 @@ static const struct net_device_ops netdev_ops = {
 	.ndo_tx_timeout		= netdev_tx_timeout,
 	.ndo_change_mtu		= netdev_change_mtu,
 	.ndo_set_mac_address	= netdev_set_mac_address,
+	.ndo_validate_addr	= eth_validate_addr,
 	.ndo_do_ioctl		= netdev_ioctl,
 	.ndo_set_rx_mode	= netdev_set_rx_mode,
 #ifdef CONFIG_NET_POLL_CONTROLLER
