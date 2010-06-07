aa1df0f229829109e49d1dc493252fd94a7af2a1
Alex Deucher <alexdeucher@gmail.com>
2010-06-07 11:35:53 -0400

    drm/radeon/kms/pm: Disable voltage adjust on RS780/RS880
    The vddc value in the power tables is not an actual voltage
    like on discrete r6xx/r7xx/evergreen systems, but instead has
    a symbolic meaning (e.g., NONE, LOW, HIGH, etc.).  See atombios.h
    
    Most RS780/RS880 vbioses don't have a SetVoltage table anyway,
    so it shouldn't be doing anything to the hardware at the moment.
    
    I need to figure out how voltage is supposed to work on the newer
    IGPs; until then, disable it.
    
    Signed-off-by: Alex Deucher <alexdeucher@gmail.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/radeon_atombios.c
+++ drivers/gpu/drm/radeon/radeon_atombios.c
@@ -1833,10 +1833,7 @@ void radeon_atombios_get_power_modes(struct radeon_device *rdev)
 						/* skip invalid modes */
 						if (rdev->pm.power_state[state_index].clock_info[mode_index].sclk == 0)
 							continue;
-						rdev->pm.power_state[state_index].clock_info[mode_index].voltage.type =
-							VOLTAGE_SW;
-						rdev->pm.power_state[state_index].clock_info[mode_index].voltage.voltage =
-							clock_info->usVDDC;
+						/* voltage works differently on IGPs */
 						mode_index++;
 					} else if (ASIC_IS_DCE4(rdev)) {
 						struct _ATOM_PPLIB_EVERGREEN_CLOCK_INFO *clock_info =
