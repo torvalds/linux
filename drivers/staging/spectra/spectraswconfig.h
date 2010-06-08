bf46b9a92e45044d0b41beed61ae118e560dd3e4
David Woodhouse <David.Woodhouse@intel.com>
2010-06-08 18:27:31 +0100

    spectra: Rename config options
    This is the Spectra driver, so use CONFIG_SPECTRA.
    
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/staging/spectra/spectraswconfig.h
+++ drivers/staging/spectra/spectraswconfig.h
@@ -45,10 +45,10 @@
 #define DEBUG_BNDRY             0
 
 /***** Product Feature Support *****/
-#define FLASH_EMU               defined(CONFIG_MRST_NAND_EMU)
-#define FLASH_NAND              defined(CONFIG_MRST_NAND_HW)
-#define FLASH_MTD               defined(CONFIG_MRST_NAND_MTD)
-#define CMD_DMA                 defined(CONFIG_MRST_NAND_HW_DMA)
+#define FLASH_EMU               defined(CONFIG_SPECTRA_EMU)
+#define FLASH_NAND              defined(CONFIG_SPECTRA_MRST_HW)
+#define FLASH_MTD               defined(CONFIG_SPECTRA_MTD)
+#define CMD_DMA                 defined(CONFIG_SPECTRA_MRST_HW_DMA)
 
 #define SPECTRA_PARTITION_ID    0
 
