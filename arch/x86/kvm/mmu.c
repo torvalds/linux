4f78fd08e91c52f097d64a42d903b76fe52a3a0f
Xiao Guangrong <xiaoguangrong@cn.fujitsu.com>
2010-06-08 20:05:05 +0800

    KVM: MMU: remove unnecessary remote tlb flush
    This remote tlb flush is no necessary since we have synced while
    sp is zapped
    
    Signed-off-by: Xiao Guangrong <xiaoguangrong@cn.fujitsu.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/mmu.c
+++ arch/x86/kvm/mmu.c
@@ -3040,8 +3040,6 @@ restart:
 
 	kvm_mmu_commit_zap_page(kvm, &invalid_list);
 	spin_unlock(&kvm->mmu_lock);
-
-	kvm_flush_remote_tlbs(kvm);
 }
 
 static int kvm_mmu_remove_some_alloc_mmu_pages(struct kvm *kvm,
