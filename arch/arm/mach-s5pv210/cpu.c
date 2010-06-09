50e9769ad5cfdcfefcfdf99236a3c962e82ebc09
Naveen Krishna Ch <ch.naveen@samsung.com>
2010-06-09 15:25:19 +0530

    ARM: SAMSUNG: Implement set_name function for ADC devices
    This patch implements s3c_adc_setname() for Samsung SoCs.
    Also updates its usage in S3C64XX, S5P6440, and S5PV210.
    
    Signed-off-by: Naveen Krishna Ch <ch.naveen@samsung.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s5pv210/cpu.c
+++ arch/arm/mach-s5pv210/cpu.c
@@ -32,6 +32,7 @@
 #include <plat/devs.h>
 #include <plat/clock.h>
 #include <plat/s5pv210.h>
+#include <plat/adc-core.h>
 #include <plat/iic-core.h>
 #include <plat/sdhci.h>
 
@@ -76,16 +77,14 @@ static void s5pv210_idle(void)
 
 void __init s5pv210_map_io(void)
 {
-#ifdef CONFIG_S3C_DEV_ADC
-	s3c_device_adc.name	= "s3c64xx-adc";
-#endif
-
 	iotable_init(s5pv210_iodesc, ARRAY_SIZE(s5pv210_iodesc));
 
 	/* initialise device information early */
 	s5pv210_default_sdhci0();
 	s5pv210_default_sdhci1();
 	s5pv210_default_sdhci2();
+
+	s3c_adc_setname("s3c64xx-adc");
 
 	/* the i2c devices are directly compatible with s3c2440 */
 	s3c_i2c0_setname("s3c2440-i2c");
