fa0d11873943b29d6c8f57a7d54f77d0439acfd9
Florent AUDEBERT <florent.audebert@anevia.com>
2010-06-11 12:01:25 -0300

    [media] stb0899: Removed an extra byte sent at init on DiSEqC bus
    I noticed a stray 0x00 at init on DiSEqC bus (KNC1 DVB-S2) with a DiSEqC
    tool analyzer.
    
    I removed the register from initialization table and all seem to go well
    (at least for my KNC board).
    
    Signed-off-by: Florent Audebert <florent.audebert@anevia.com>
    Acked-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/mantis/mantis_vp1041.c
+++ drivers/media/dvb/mantis/mantis_vp1041.c
@@ -51,7 +51,6 @@ static const struct stb0899_s1_reg vp1041_stb0899_s1_init_1[] = {
 	{ STB0899_DISRX_ST0     	, 0x04 },
 	{ STB0899_DISRX_ST1     	, 0x00 },
 	{ STB0899_DISPARITY     	, 0x00 },
-	{ STB0899_DISFIFO       	, 0x00 },
 	{ STB0899_DISSTATUS		, 0x20 },
 	{ STB0899_DISF22        	, 0x99 },
 	{ STB0899_DISF22RX      	, 0xa8 },
