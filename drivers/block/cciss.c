b14aa6dcd083ad00fb416a93f76131734e6c3c17
Mike Miller <mike.miller@hp.com>
2010-06-11 13:13:35 +0200

    cciss: fix call to put_controller_in_performant_mode
    call to put_controller_in_performant_mode was in the wrong place
    The call inadvertently ended up in an error path.
    
    Signed-off-by: Mike Miller <mike.miller@hp.com>
    Cc: Stephen M. Cameron <scameron@beardog.cce.hp.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

--- drivers/block/cciss.c
+++ drivers/block/cciss.c
@@ -4150,6 +4150,7 @@ static int __devinit cciss_pci_init(ctlr_info_t *c, struct pci_dev *pdev)
 #ifdef CCISS_DEBUG
 	printk(KERN_WARNING "Trying to put board into Performant mode\n");
 #endif				/* CCISS_DEBUG */
+	cciss_put_controller_into_performant_mode(c);
 	return 0;
 
 err_out_free_res:
@@ -4158,7 +4159,6 @@ err_out_free_res:
 	 * Smart Array controllers that pci_enable_device does not undo
 	 */
 	pci_release_regions(pdev);
-	cciss_put_controller_into_performant_mode(c);
 	return err;
 }
 
