812e876e842488221aa54cb4587a8a33445cfa9e
Eric W. Biederman <ebiederm@xmission.com>
2010-06-13 03:27:04 +0000

    scm: Reorder scm_cookie.
    Reorder the fields in scm_cookie so they pack better on 64bit.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Acked-by: Pavel Emelyanov <xemul@openvz.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/scm.h
+++ include/net/scm.h
@@ -19,8 +19,8 @@ struct scm_fp_list {
 };
 
 struct scm_cookie {
-	struct ucred		creds;		/* Skb credentials	*/
 	struct scm_fp_list	*fp;		/* Passed files		*/
+	struct ucred		creds;		/* Skb credentials	*/
 #ifdef CONFIG_SECURITY_NETWORK
 	u32			secid;		/* Passed security ID 	*/
 #endif
