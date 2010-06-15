1c48bc5f71cd7783e19fb8d9462be53f829be177
Dave Airlie <airlied@redhat.com>
2010-06-15 11:02:05 +1000

    agp: add no warn since we have a fallback to vmalloc paths
    also drop the NORETRY we can probably nearly always satisfy order 1 allocs now,
    and again the vmalloc path is there.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/char/agp/generic.c
+++ drivers/char/agp/generic.c
@@ -99,7 +99,7 @@ void agp_alloc_page_array(size_t size, struct agp_memory *mem)
 	mem->pages = NULL;
 
 	if (size <= 2*PAGE_SIZE)
-		mem->pages = kmalloc(size, GFP_KERNEL | __GFP_NORETRY);
+		mem->pages = kmalloc(size, GFP_KERNEL | __GFP_NOWARN);
 	if (mem->pages == NULL) {
 		mem->pages = vmalloc(size);
 	}
