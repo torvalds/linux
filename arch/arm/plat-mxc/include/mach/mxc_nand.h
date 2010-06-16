cd57c77772b699b1e69d69510ce01109c46a6f0b
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-06-16 06:45:15 +0200

    MTD: mxc_nand: make bit fields unsigned to please sparse
    This fixes:
    
    	arch/arm/plat-mxc/include/mach/mxc_nand.h:25:14: error: dubious one-bit signed bitfield
    	arch/arm/plat-mxc/include/mach/mxc_nand.h:26:17: error: dubious one-bit signed bitfield
    
    While at it make width unsigned, too.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- arch/arm/plat-mxc/include/mach/mxc_nand.h
+++ arch/arm/plat-mxc/include/mach/mxc_nand.h
@@ -21,8 +21,8 @@
 #define __ASM_ARCH_NAND_H
 
 struct mxc_nand_platform_data {
-	int width;	/* data bus width in bytes */
-	int hw_ecc:1;	/* 0 if supress hardware ECC */
-	int flash_bbt:1; /* set to 1 to use a flash based bbt */
+	unsigned int width;	/* data bus width in bytes */
+	unsigned int hw_ecc:1;	/* 0 if supress hardware ECC */
+	unsigned int flash_bbt:1; /* set to 1 to use a flash based bbt */
 };
 #endif /* __ASM_ARCH_NAND_H */
