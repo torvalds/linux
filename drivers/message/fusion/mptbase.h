c817ce842a539d2e536b328b9689e836d48b20e9
Kashyap, Desai <kashyap.desai@lsi.com>
2010-06-17 14:44:00 +0530

    [SCSI] mptfusion: Bump version 03.04.16
    Upgrade driver version to 3.4.16
    
    Signed-off-by: Kashyap Desai <kashyap.desai@lsi.com>
    Signed-off-by: James Bottomley <James.Bottomley@suse.de>

--- drivers/message/fusion/mptbase.h
+++ drivers/message/fusion/mptbase.h
@@ -76,8 +76,8 @@
 #define COPYRIGHT	"Copyright (c) 1999-2008 " MODULEAUTHOR
 #endif
 
-#define MPT_LINUX_VERSION_COMMON	"3.04.15"
-#define MPT_LINUX_PACKAGE_NAME		"@(#)mptlinux-3.04.15"
+#define MPT_LINUX_VERSION_COMMON	"3.04.16"
+#define MPT_LINUX_PACKAGE_NAME		"@(#)mptlinux-3.04.16"
 #define WHAT_MAGIC_STRING		"@" "(" "#" ")"
 
 #define show_mptmod_ver(s,ver)  \
