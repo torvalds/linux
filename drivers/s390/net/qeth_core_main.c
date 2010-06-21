43a65303fe530afe4daf1c0fd6875fdba7090f91
Carsten Otte <cotte@de.ibm.com>
2010-06-21 22:57:09 +0000

    qeth: fix use after free for qeths debug area
    The function qeth_free_buffer_pool is called _after_ the per-card
    debug area has been released. This debug message is not all that
    usefull anyway, and thus gets removed.
    
    Signed-off-by: Carsten Otte <cotte@de.ibm.com>
    Signed-off-by: Frank Blaschka <frank.blaschka@de.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/s390/net/qeth_core_main.c
+++ drivers/s390/net/qeth_core_main.c
@@ -973,7 +973,6 @@ static void qeth_free_buffer_pool(struct qeth_card *card)
 {
 	struct qeth_buffer_pool_entry *pool_entry, *tmp;
 	int i = 0;
-	QETH_CARD_TEXT(card, 5, "freepool");
 	list_for_each_entry_safe(pool_entry, tmp,
 				 &card->qdio.init_pool.entry_list, init_list){
 		for (i = 0; i < QETH_MAX_BUFFER_ELEMENTS(card); ++i)
