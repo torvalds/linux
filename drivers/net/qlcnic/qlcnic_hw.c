42f65cbad4168958dff8a307bfe4b528409951d3
Amit Kumar Salecha <amit.salecha@qlogic.com>
2010-06-22 03:19:00 +0000

    qlcnic: fix mac address mgmt
    We first add mac address in driver local list and then send command to
    fw to add same. There are checks in driver to ensure send command doesn't fail
    before adding mac address in local list.
    
    But instead fix should be:
    Add mac address in fw and if it succeeds, add it in driver local list.
    
    Signed-off-by: Amit Kumar Salecha <amit.salecha@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/qlcnic/qlcnic_hw.c
+++ drivers/net/qlcnic/qlcnic_hw.c
@@ -413,10 +413,15 @@ static int qlcnic_nic_add_mac(struct qlcnic_adapter *adapter, u8 *addr)
 		return -ENOMEM;
 	}
 	memcpy(cur->mac_addr, addr, ETH_ALEN);
-	list_add_tail(&cur->list, &adapter->mac_list);
 
-	return qlcnic_sre_macaddr_change(adapter,
-				cur->mac_addr, QLCNIC_MAC_ADD);
+	if (qlcnic_sre_macaddr_change(adapter,
+				cur->mac_addr, QLCNIC_MAC_ADD)) {
+		kfree(cur);
+		return -EIO;
+	}
+
+	list_add_tail(&cur->list, &adapter->mac_list);
+	return 0;
 }
 
 void qlcnic_set_multi(struct net_device *netdev)
