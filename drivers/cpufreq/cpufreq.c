9c36f746d7e191ad6f44f01859af843f0c4d3c5d
Neal Buckendahl <nealb001@gmail.com>
2010-06-22 22:02:44 -0500

    [CPUFREQ] fix brace coding style issue.
    This patch fixes up a brace warning found by the checkpatch.pl tool
    
    Signed-off-by: Neal Buckendahl <nealb001@tbcnet.com>
    Signed-off-by: Dave Jones <davej@redhat.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1876,8 +1876,7 @@ static int __cpuinit cpufreq_cpu_callback(struct notifier_block *nfb,
 	return NOTIFY_OK;
 }
 
-static struct notifier_block __refdata cpufreq_cpu_notifier =
-{
+static struct notifier_block __refdata cpufreq_cpu_notifier = {
     .notifier_call = cpufreq_cpu_callback,
 };
 
