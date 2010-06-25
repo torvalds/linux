088bcc2aff25f31f1497f8196fa02c8cd2a43f68
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2010-06-25 07:19:39 +0000

    sh: remove bogus highest / lowest logic from clock rate rounding
    The use of highest and lowest in clk_rate_table_round() is completely bogus
    and superfluous. Remove it.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- drivers/sh/clk.c
+++ drivers/sh/clk.c
@@ -73,21 +73,13 @@ long clk_rate_table_round(struct clk *clk,
 {
 	unsigned long rate_error, rate_error_prev = ~0UL;
 	unsigned long rate_best_fit = rate;
-	unsigned long highest, lowest;
 	int i;
-
-	highest = lowest = 0;
 
 	for (i = 0; freq_table[i].frequency != CPUFREQ_TABLE_END; i++) {
 		unsigned long freq = freq_table[i].frequency;
 
 		if (freq == CPUFREQ_ENTRY_INVALID)
 			continue;
-
-		if (freq > highest)
-			highest = freq;
-		if (freq < lowest)
-			lowest = freq;
 
 		rate_error = abs(freq - rate);
 		if (rate_error < rate_error_prev) {
@@ -98,11 +90,6 @@ long clk_rate_table_round(struct clk *clk,
 		if (rate_error == 0)
 			break;
 	}
-
-	if (rate >= highest)
-		rate_best_fit = highest;
-	if (rate <= lowest)
-		rate_best_fit = lowest;
 
 	return rate_best_fit;
 }
