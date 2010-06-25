1704d74894912b8ecc3e95cecd7bde336a0b1bf2
Casey Leedom <leedom@chelsio.com>
2010-06-25 12:09:38 +0000

    cxgb4vf: small changes to message processing structures/macros
    Split cpl_tx_pkt_lso into core message structure and encapsulated message,
    make RSPD_LEN macro match other response descriptor macros.
    
    Signed-off-by: Casey Leedom
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/cxgb4/t4_hw.h
+++ drivers/net/cxgb4/t4_hw.h
@@ -88,11 +88,13 @@ struct rsp_ctrl {
 };
 
 #define RSPD_NEWBUF 0x80000000U
-#define RSPD_LEN    0x7fffffffU
+#define RSPD_LEN(x) (((x) >> 0) & 0x7fffffffU)
+#define RSPD_QID(x) RSPD_LEN(x)
 
 #define RSPD_GEN(x)  ((x) >> 7)
 #define RSPD_TYPE(x) (((x) >> 4) & 3)
 
 #define QINTR_CNT_EN       0x1
 #define QINTR_TIMER_IDX(x) ((x) << 1)
+#define QINTR_TIMER_IDX_GET(x) (((x) << 1) & 0x7)
 #endif /* __T4_HW_H */
