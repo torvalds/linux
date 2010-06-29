2cb4b05e7647891b46b91c07c9a60304803d1688
Changli Gao <xiaosuo@gmail.com>
2010-06-29 13:09:18 +0200

    splice: direct_splice_actor() should not use pos in sd
    direct_splice_actor() shouldn't use sd->pos, as sd->pos is for file reading,
    file->f_pos should be used instead.
    
    Signed-off-by: Changli Gao <xiaosuo@gmail.com>
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    ----
     fs/splice.c |    3 ++-
     1 file changed, 2 insertions(+), 1 deletion(-)
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

--- fs/splice.c
+++ fs/splice.c
@@ -1282,7 +1282,8 @@ static int direct_splice_actor(struct pipe_inode_info *pipe,
 {
 	struct file *file = sd->u.file;
 
-	return do_splice_from(pipe, file, &sd->pos, sd->total_len, sd->flags);
+	return do_splice_from(pipe, file, &file->f_pos, sd->total_len,
+			      sd->flags);
 }
 
 /**
