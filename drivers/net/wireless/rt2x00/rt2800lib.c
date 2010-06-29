4e9e58c6bf6512a1812556188b67bada6a09c0e8
Ivo van Doorn <IvDoorn@gmail.com>
2010-06-29 21:49:50 +0200

    rt2x00: Fix compile warning when debug disabled
    CC [M]  drivers/net/wireless/rt2x00/rt2800lib.o
    drivers/net/wireless/rt2x00/rt2800lib.c: In function 'rt2800_ampdu_action':
    drivers/net/wireless/rt2x00/rt2800lib.c:2821: warning: unused variable 'rt2x00dev'
    
    Signed-off-by: Ivo van Doorn <IvDoorn@gmail.com>
    Acked-by: Helmut Schaa <helmut.schaa@googlemail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt2800lib.c
+++ drivers/net/wireless/rt2x00/rt2800lib.c
@@ -2769,7 +2769,6 @@ static int rt2800_ampdu_action(struct ieee80211_hw *hw,
 			       struct ieee80211_sta *sta,
 			       u16 tid, u16 *ssn)
 {
-	struct rt2x00_dev *rt2x00dev = hw->priv;
 	int ret = 0;
 
 	switch (action) {
@@ -2787,7 +2786,7 @@ static int rt2800_ampdu_action(struct ieee80211_hw *hw,
 	case IEEE80211_AMPDU_TX_OPERATIONAL:
 		break;
 	default:
-		WARNING(rt2x00dev, "Unknown AMPDU action\n");
+		WARNING((struct rt2x00_dev *)hw->priv, "Unknown AMPDU action\n");
 	}
 
 	return ret;
