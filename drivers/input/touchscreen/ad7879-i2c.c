16ea10a7d557a0177cbbd716b4a06e5373d513ba
Michael Hennerich <michael.hennerich@analog.com>
2010-06-30 14:51:09 -0700

    Input: ad7879 - use i2c_smbus_read_i2c_block_data() to lower overhead
    Avoid additional addressing overhead incurred by word_data transfers.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/touchscreen/ad7879-i2c.c
+++ drivers/input/touchscreen/ad7879-i2c.c
@@ -51,10 +51,13 @@ static int ad7879_i2c_read(struct device *dev, u8 reg)
 static int ad7879_i2c_multi_read(struct device *dev,
 				 u8 first_reg, u8 count, u16 *buf)
 {
+	struct i2c_client *client = to_i2c_client(dev);
 	u8 idx;
 
+	i2c_smbus_read_i2c_block_data(client, first_reg, count * 2, (u8 *)buf);
+
 	for (idx = 0; idx < count; ++idx)
-		buf[idx] = ad7879_i2c_read(dev, first_reg + idx);
+		buf[idx] = swab16(buf[idx]);
 
 	return 0;
 }
