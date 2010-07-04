49da8be59b19c270a5b71b20a6537776760e0dc6
Jarod Wilson <jarod@redhat.com>
2010-07-03 22:48:04 -0300

    V4L/DVB: IR/imon: auto-configure another 0xffdc device variant
    Per Pieter Hoekstra:
    
    I have a Antec Fusion with a iMON Lcd and I get the following error:
    imon 6-1:1.0: Unknown 0xffdc device, defaulting to VFD and iMON IR (id
    0x9e)
    
    The driver is functional if I load it like this: (I do not use a remote for it)
    modprobe imon display_type=1 (On Mythbuntu 10.04/2.6.32)
    
    This device is a lcd-type with support for a MCE remote. Looking at
    the source code, this device (0x9e) is the same as id 0x9f.
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/IR/imon.c
+++ drivers/media/IR/imon.c
@@ -2067,6 +2067,7 @@ static void imon_get_ffdc_type(struct imon_context *ictx)
 		detected_display_type = IMON_DISPLAY_TYPE_VFD;
 		break;
 	/* iMON LCD, MCE IR */
+	case 0x9e:
 	case 0x9f:
 		dev_info(ictx->dev, "0xffdc iMON LCD, MCE IR");
 		detected_display_type = IMON_DISPLAY_TYPE_LCD;
