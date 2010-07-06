49085bd7d498c47d635851cfda22627b085cd9af
George Kadianakis <desnacked@gmail.com>
2010-07-06 11:44:12 +0000

    net/ipv4/ip_output.c: Removal of unused variable in ip_fragment()
    Removal of unused integer variable in ip_fragment().
    
    Signed-off-by: George Kadianakis <desnacked@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/ip_output.c
+++ net/ipv4/ip_output.c
@@ -442,7 +442,6 @@ static void ip_copy_metadata(struct sk_buff *to, struct sk_buff *from)
 int ip_fragment(struct sk_buff *skb, int (*output)(struct sk_buff *))
 {
 	struct iphdr *iph;
-	int raw = 0;
 	int ptr;
 	struct net_device *dev;
 	struct sk_buff *skb2;
@@ -580,7 +579,7 @@ int ip_fragment(struct sk_buff *skb, int (*output)(struct sk_buff *))
 
 slow_path:
 	left = skb->len - hlen;		/* Space per frame */
-	ptr = raw + hlen;		/* Where to start from */
+	ptr = hlen;		/* Where to start from */
 
 	/* for bridged IP traffic encapsulated inside f.e. a vlan header,
 	 * we need to make room for the encapsulating header
