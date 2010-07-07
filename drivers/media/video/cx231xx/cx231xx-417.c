dd067a8de79fcc75b3fbd84aba398c9296b47cb2
Devin Heitmueller <dheitmueller@hauppauge.com>
2010-07-07 19:28:23 -0300

    [media] cx231xx: fix format string warning
    Change a %x to a %p since the variable is a pointer
    
    Signed-off-by: Devin Heitmueller <dheitmueller@hauppauge.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx231xx/cx231xx-417.c
+++ drivers/media/video/cx231xx/cx231xx-417.c
@@ -2010,7 +2010,7 @@ static int mpeg_release(struct file *file)
 	struct cx231xx_fh  *fh  = file->private_data;
 	struct cx231xx *dev = fh->dev;
 
-	dprintk(3, "mpeg_release()! dev=0x%x\n", dev);
+	dprintk(3, "mpeg_release()! dev=0x%p\n", dev);
 
 	if (!dev) {
 		dprintk(3, "abort!!!\n");
