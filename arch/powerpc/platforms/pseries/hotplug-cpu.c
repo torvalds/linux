32d8ad4e621d6620e925cf540ef1d35aa6fa5a7b
Brian King <brking@linux.vnet.ibm.com>
2010-07-07 12:31:02 +0000

    powerpc/pseries: Partition hibernation support
    Enables support for HMC initiated partition hibernation. This is
    a firmware assisted hibernation, since the firmware handles writing
    the memory out to disk, along with other partition information,
    so we just mimic suspend to ram.
    
    Signed-off-by: Brian King <brking@linux.vnet.ibm.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/platforms/pseries/hotplug-cpu.c
+++ arch/powerpc/platforms/pseries/hotplug-cpu.c
@@ -116,6 +116,9 @@ static void pseries_mach_cpu_die(void)
 
 	if (get_preferred_offline_state(cpu) == CPU_STATE_INACTIVE) {
 		set_cpu_current_state(cpu, CPU_STATE_INACTIVE);
+		if (ppc_md.suspend_disable_cpu)
+			ppc_md.suspend_disable_cpu();
+
 		cede_latency_hint = 2;
 
 		get_lppaca()->idle = 1;
