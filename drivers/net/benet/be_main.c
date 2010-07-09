344dbf1073d1cea179ed0831547cab2b7ea4ea27
Sarveshwar Bandi <sarveshwarb@serverengines.com>
2010-07-09 01:43:55 +0000

    be2net: Patch to determine if function is VF while running in guest OS.
    When driver is loaded in guest OS, the pci variables is_virtfn and is_physfn are
    both set to 0. This change uses registers in controller to determine the same.
    
    Signed-off-by: Sarveshwar Bandi <sarveshwarb@serverengines.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/benet/be_main.c
+++ drivers/net/benet/be_main.c
@@ -1631,6 +1631,7 @@ static void be_sriov_enable(struct be_adapter *adapter)
 {
 #ifdef CONFIG_PCI_IOV
 	int status;
+	be_check_sriov_fn_type(adapter);
 	if (be_physfn(adapter) && num_vfs) {
 		status = pci_enable_sriov(adapter->pdev, num_vfs);
 		adapter->sriov_enabled = status ? false : true;
