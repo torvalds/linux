5e03f73fe8c01e301fdf38954297a1237be1ab26
Andres More <more.andres@gmail.com>
2010-07-12 18:17:08 -0300

    staging: vt6656: replaced MIN_PACKET_LEN with ETH_ZLEN
    Removed custom macro with one from include/linux/if_ether.h.
    Also removed MIN_DATA_LEN as it is not used elsewhere.
    
    Signed-off-by: Andres More <more.andres@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/vt6656/tether.h
+++ drivers/staging/vt6656/tether.h
@@ -39,11 +39,6 @@
 #define U_ETHER_ADDR_STR_LEN (ETH_ALEN * 2 + 1)
                                         // Ethernet address string length
 
-#define MIN_DATA_LEN        46          // min data length
-
-#define MIN_PACKET_LEN      (MIN_DATA_LEN + ETH_HLEN)
-                                        // 60
-                                        // min total packet length (tx)
 #define MAX_PACKET_LEN      (ETH_DATA_LEN + ETH_HLEN)
                                         // 1514
                                         // max total packet length (tx)
