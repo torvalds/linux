b88ba6193d5f2469b2b29b52e8cca78a4caa47e0
Devin Heitmueller <dheitmueller@hauppauge.com>
2010-07-12 15:37:00 -0300

    [media] cx231xx: Set the power mode instead of using the digital mux GPIOs
    The Exeter hardware design does not use GPIOs to manage whether its in
    digital mode or analog mode, but we need to setup the power control properly.
    
    For that board, setup power control and remove the mux select call.  It is
    highly likely that this change could be used by other boards as well, which
    would make power management cleaner (fixing known issues transitioning between
    analog and digital mode).
    
    Signed-off-by: Devin Heitmueller <dheitmueller@hauppauge.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx231xx/cx231xx-core.c
+++ drivers/media/video/cx231xx/cx231xx-core.c
@@ -746,8 +746,11 @@ int cx231xx_set_mode(struct cx231xx *dev, enum cx231xx_mode set_mode)
 			break;
 		case CX231XX_BOARD_CNXT_RDE_253S:
 		case CX231XX_BOARD_CNXT_RDU_253S:
+			errCode = cx231xx_set_agc_analog_digital_mux_select(dev, 1);
+			break;
 		case CX231XX_BOARD_HAUPPAUGE_EXETER:
-		errCode = cx231xx_set_agc_analog_digital_mux_select(dev, 1);
+			errCode = cx231xx_set_power_mode(dev,
+						POLARIS_AVMODE_DIGITAL);
 			break;
 		default:
 			break;
