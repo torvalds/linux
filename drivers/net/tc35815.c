1e2cfeef060fa0270f9a2d66b1218c12c05062e0
David S. Miller <davem@davemloft.net>
2010-07-13 14:20:58 -0700

    Revert "tc35815: fix iomap leak"
    This reverts commit b31fb86815153be3bc94e8ffb9dbf6e9d7694b2d.
    
    pcim_*() managed drivers do not need explicit resource
    releasing like this.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/tc35815.c
+++ drivers/net/tc35815.c
@@ -854,7 +854,7 @@ static int __devinit tc35815_init_one(struct pci_dev *pdev,
 
 	rc = register_netdev(dev);
 	if (rc)
-		goto err_out_iounmap;
+		goto err_out;
 
 	memcpy(dev->perm_addr, dev->dev_addr, dev->addr_len);
 	printk(KERN_INFO "%s: %s at 0x%lx, %pM, IRQ %d\n",
@@ -872,8 +872,6 @@ static int __devinit tc35815_init_one(struct pci_dev *pdev,
 
 err_out_unregister:
 	unregister_netdev(dev);
-err_out_iounmap:
-	pcim_iounmap_regions(pdev, 1 << 1);
 err_out:
 	free_netdev(dev);
 	return rc;
