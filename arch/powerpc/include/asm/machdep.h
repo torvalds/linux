34d97e07cc81ab6f1e63696127cc7a5d2c4fce4b
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2010-07-14 14:12:16 +1000

    powerpc/book3e: Add generic 64-bit idle powersave support
    We use a similar technique to ppc32: We set a thread local flag
    to indicate that we are about to enter or have entered the stop
    state, and have fixup code in the async interrupt entry code that
    reacts to this flag to make us return to a different location
    (sets NIP to LINK in our case).
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    --
    v2. Fix lockdep bug
        Re-mask interrupts when coming back from idle

--- arch/powerpc/include/asm/machdep.h
+++ arch/powerpc/include/asm/machdep.h
@@ -278,6 +278,7 @@ extern void e500_idle(void);
 extern void power4_idle(void);
 extern void power4_cpu_offline_powersave(void);
 extern void ppc6xx_idle(void);
+extern void book3e_idle(void);
 
 /*
  * ppc_md contains a copy of the machine description structure for the
