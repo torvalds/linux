524cce03124112240120f823b87b7137629531d2
Oliver Neukum <oliver@neukum.org>
2010-07-14 18:26:22 +0200

    USB: add quirk for Broadcom BT dongle
    commit 63ab71deae67b031045bb28bf8cff45180089f8f upstream.
    
    This device needs to be reset when resuming
    
    Signed-off-by: Oliver Neukum <oneukum@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -68,6 +68,9 @@ static const struct usb_device_id usb_quirk_list[] = {
 	/* X-Rite/Gretag-Macbeth Eye-One Pro display colorimeter */
 	{ USB_DEVICE(0x0971, 0x2000), .driver_info = USB_QUIRK_NO_SET_INTF },
 
+	/* Broadcom BCM92035DGROM BT dongle */
+	{ USB_DEVICE(0x0a5c, 0x2021), .driver_info = USB_QUIRK_RESET_RESUME },
+
 	/* Action Semiconductor flash disk */
 	{ USB_DEVICE(0x10d6, 0x2200), .driver_info =
 			USB_QUIRK_STRING_FETCH_255 },
