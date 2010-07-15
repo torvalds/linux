ae42f234470662aefe65ab59a0ef228f1f6f9c77
Matthew Garrett <mjg@redhat.com>
2010-07-15 14:23:42 -0400

    toshiba-acpi: Add an extra couple of keys
    Thomas Bächler reports that his machine generates two keycodes for zooming
    in and out. Add these to the default keymap.
    
    Signed-off-by: Matthew Garrett <mjg@redhat.com>
    Cc: Thomas Bächler <thomas@archlinux.org>

--- drivers/platform/x86/toshiba_acpi.c
+++ drivers/platform/x86/toshiba_acpi.c
@@ -129,6 +129,8 @@ enum {KE_KEY, KE_END};
 
 static struct key_entry toshiba_acpi_keymap[]  = {
 	{KE_KEY, 0x101, KEY_MUTE},
+	{KE_KEY, 0x102, KEY_ZOOMOUT},
+	{KE_KEY, 0x103, KEY_ZOOMIN},
 	{KE_KEY, 0x13b, KEY_COFFEE},
 	{KE_KEY, 0x13c, KEY_BATTERY},
 	{KE_KEY, 0x13d, KEY_SLEEP},
