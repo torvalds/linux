66c524210ab9217528b01b63c43903545e03a58c
Johannes Berg <johannes.berg@intel.com>
2010-07-22 13:58:51 +0200

    mac80211: remove bogus rcu_read_lock()
    Another remnant of the previous key locking scheme
    needs to be removed -- this causes a warning
    otherwise as ieee80211_set_default_mgmt_key will
    acquire a mutex.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/cfg.c
+++ net/mac80211/cfg.c
@@ -324,14 +324,9 @@ static int ieee80211_config_default_mgmt_key(struct wiphy *wiphy,
 					     struct net_device *dev,
 					     u8 key_idx)
 {
-	struct ieee80211_sub_if_data *sdata;
+	struct ieee80211_sub_if_data *sdata = IEEE80211_DEV_TO_SUB_IF(dev);
 
-	rcu_read_lock();
-
-	sdata = IEEE80211_DEV_TO_SUB_IF(dev);
 	ieee80211_set_default_mgmt_key(sdata, key_idx);
-
-	rcu_read_unlock();
 
 	return 0;
 }
