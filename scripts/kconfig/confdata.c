1244b41d00eb60cb3d05220383bc9d15b9045fb4
Peter Korsgaard <jacmet@sunsite.dk>
2010-07-22 14:24:57 +0200

    kconfig: make randconfig fair for booleans
    Give boolean symbols a 50% chance of getting enabled, rather than 67%.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>
    Acked-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/kconfig/confdata.c
+++ scripts/kconfig/confdata.c
@@ -862,7 +862,8 @@ void conf_set_all_new_symbols(enum conf_def_mode mode)
 				sym->def[S_DEF_USER].tri = no;
 				break;
 			case def_random:
-				sym->def[S_DEF_USER].tri = (tristate)(rand() % 3);
+				cnt = sym_get_type(sym) == S_TRISTATE ? 3 : 2;
+				sym->def[S_DEF_USER].tri = (tristate)(rand() % cnt);
 				break;
 			default:
 				continue;
