5620ba4680db0ba25b89952bdda41a9c8fd11f50
Ben Skeggs <bskeggs@redhat.com>
2010-07-23 10:00:12 +1000

    drm/nouveau: fix build without CONFIG_ACPI
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nouveau_drv.h
+++ drivers/gpu/drm/nouveau/nouveau_drv.h
@@ -833,7 +833,7 @@ static inline void nouveau_register_dsm_handler(void) {}
 static inline void nouveau_unregister_dsm_handler(void) {}
 static inline bool nouveau_acpi_rom_supported(struct pci_dev *pdev) { return false; }
 static inline int nouveau_acpi_get_bios_chunk(uint8_t *bios, int offset, int len) { return -EINVAL; }
-static inline int nouveau_acpi_edid(struct drm_device *, struct drm_connector *) { return -EINVAL; }
+static inline int nouveau_acpi_edid(struct drm_device *dev, struct drm_connector *connector) { return -EINVAL; }
 #endif
 
 /* nouveau_backlight.c */
