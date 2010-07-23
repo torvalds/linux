14e303d98bcfe4a6075407b531b1df2f237deb28
Andrew Vasquez <andrew.vasquez@qlogic.com>
2010-07-23 15:28:29 +0500

    [SCSI] qla2xxx: Stop firmware before doing init firmware.
    If BIOS is enabled then drivers init firmware fails since
    BIOS has done the init once.
    
    Signed-off-by: Giridhar Malavali <giridhar.malavali@qlogic.com>
    Signed-off-by: James Bottomley <James.Bottomley@suse.de>

--- drivers/scsi/qla2xxx/qla_init.c
+++ drivers/scsi/qla2xxx/qla_init.c
@@ -1402,9 +1402,10 @@ qla2x00_setup_chip(scsi_qla_host_t *vha)
 
 	if (IS_QLA82XX(ha)) {
 		rval = ha->isp_ops->load_risc(vha, &srisc_address);
-		if (rval == QLA_SUCCESS)
+		if (rval == QLA_SUCCESS) {
+			qla2x00_stop_firmware(vha);
 			goto enable_82xx_npiv;
-		else
+		} else
 			goto failed;
 	}
 
