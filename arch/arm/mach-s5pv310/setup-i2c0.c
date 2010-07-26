2b12b5c4ff9e0f1c5f4e5d5bde57b919fe522df2
Changhwan Youn <chaos.youn@samsung.com>
2010-07-26 21:08:52 +0900

    ARM: S5PV310: Add new CPU initialization support
    This patch adds Samsung S5PV310/S5PC210 CPU support.
    The S5PV310/S5PC210 integrates a ARM Cortex A9 multi-core.
    
    Signed-off-by: Changhwan Youn <chaos.youn@samsung.com>
    Signed-off-by: Jongpill Lee <boyko.lee@samsung.com>
    Signed-off-by: Jiseong Oh <jiseong.oh@samsung.com>
    [kgene.kim@samsung.com: fix build errors]
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

---
+++ arch/arm/mach-s5pv310/setup-i2c0.c
@@ -0,0 +1,20 @@
+/* linux/arch/arm/mach-s5pv210/setup-i2c0.c
+ *
+ * Copyright (c) 2009-2010 Samsung Electronics Co., Ltd.
+ *		http://www.samsung.com/
+ *
+ * I2C0 GPIO configuration.
+ *
+ * Based on plat-s3c64xx/setup-i2c0.c
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/
+
+struct platform_device; /* don't need the contents */
+
+void s3c_i2c0_cfg_gpio(struct platform_device *dev)
+{
+/* will be implemented later */
+}
