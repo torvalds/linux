f874e1ac21d7708464dc656a10312542c54719f1
Eric Paris <eparis@redhat.com>
2010-07-28 10:18:37 -0400

    inotify: force inotify and fsnotify use same bits
    inotify uses bits called IN_* and fsnotify uses bits called FS_*.  These
    need to line up.  This patch adds build time checks to make sure noone can
    change these bits so they are not the same.
    
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- include/linux/inotify.h
+++ include/linux/inotify.h
@@ -73,6 +73,15 @@ struct inotify_event {
 #ifdef __KERNEL__
 #include <linux/sysctl.h>
 extern struct ctl_table inotify_table[]; /* for sysctl */
+
+#define ALL_INOTIFY_BITS (IN_ACCESS | IN_MODIFY | IN_ATTRIB | IN_CLOSE_WRITE | \
+			  IN_CLOSE_NOWRITE | IN_OPEN | IN_MOVED_FROM | \
+			  IN_MOVED_TO | IN_CREATE | IN_DELETE | \
+			  IN_DELETE_SELF | IN_MOVE_SELF | IN_UNMOUNT | \
+			  IN_Q_OVERFLOW | IN_IGNORED | IN_ONLYDIR | \
+			  IN_DONT_FOLLOW | IN_EXCL_UNLINK | IN_MASK_ADD | \
+			  IN_ISDIR | IN_ONESHOT)
+
 #endif
 
 #endif	/* _LINUX_INOTIFY_H */
