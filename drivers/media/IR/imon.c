da48db669f50d47c208da3648dca7cac7ea6dcca
Jarod Wilson <jarod@redhat.com>
2010-07-27 22:29:44 -0300

    V4L/DVB: IR/imon: remove bad ir_input_dev use
    The ir_input_dev gets filled in by __ir_input_register, the one
    allocated in imon_init_idev was being overwritten by the correct one
    shortly after it was initialized (ultimately resulting in a memory
    leak). Additionally, there was an ill-advised memcpy into that
    extraneous ir_input_dev which gets fixed by this.
    
    Ill-advised memcpy pointed out by Dmitry Torokhov, bad usage of
    ir_input_dev pointed out by Maxim Levitsky.
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/IR/imon.c
+++ drivers/media/IR/imon.c
@@ -87,7 +87,6 @@ static ssize_t lcd_write(struct file *file, const char *buf,
 struct imon_context {
 	struct device *dev;
 	struct ir_dev_props *props;
-	struct ir_input_dev *ir;
 	/* Newer devices have two interfaces */
 	struct usb_device *usbdev_intf0;
 	struct usb_device *usbdev_intf1;
@@ -1656,7 +1655,6 @@ static struct input_dev *imon_init_idev(struct imon_context *ictx)
 {
 	struct input_dev *idev;
 	struct ir_dev_props *props;
-	struct ir_input_dev *ir;
 	int ret, i;
 
 	idev = input_allocate_device();
@@ -1669,12 +1667,6 @@ static struct input_dev *imon_init_idev(struct imon_context *ictx)
 	if (!props) {
 		dev_err(ictx->dev, "remote ir dev props allocation failed\n");
 		goto props_alloc_failed;
-	}
-
-	ir = kzalloc(sizeof(struct ir_input_dev), GFP_KERNEL);
-	if (!ir) {
-		dev_err(ictx->dev, "remote ir input dev allocation failed\n");
-		goto ir_dev_alloc_failed;
 	}
 
 	snprintf(ictx->name_idev, sizeof(ictx->name_idev),
@@ -1706,13 +1698,8 @@ static struct input_dev *imon_init_idev(struct imon_context *ictx)
 	props->change_protocol = imon_ir_change_protocol;
 	ictx->props = props;
 
-	ictx->ir = ir;
-	memcpy(&ir->dev, ictx->dev, sizeof(struct device));
-
 	usb_to_input_id(ictx->usbdev_intf0, &idev->id);
 	idev->dev.parent = ictx->dev;
-
-	input_set_drvdata(idev, ir);
 
 	ret = ir_input_register(idev, RC_MAP_IMON_PAD, props, MOD_NAME);
 	if (ret < 0) {
@@ -1723,8 +1710,6 @@ static struct input_dev *imon_init_idev(struct imon_context *ictx)
 	return idev;
 
 idev_register_failed:
-	kfree(ir);
-ir_dev_alloc_failed:
 	kfree(props);
 props_alloc_failed:
 	input_free_device(idev);
