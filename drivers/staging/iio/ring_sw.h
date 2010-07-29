34f57cc7894b7ba124006149a0c7b5f58496b25f
Jonathan Cameron <jic23@cam.ac.uk>
2010-07-29 17:50:46 +0100

    staging: iio: iio_sw_ring_helper_state - add dummy case for no buffer builds.
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Acked-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/ring_sw.h
+++ drivers/staging/iio/ring_sw.h
@@ -222,5 +222,8 @@ void iio_sw_trigger_bh_to_ring(struct work_struct *work_s);
 #else /* CONFIG_IIO_RING_BUFFER*/
 static inline void iio_ring_sw_register_funcs(struct iio_ring_access_funcs *ra)
 {};
+struct iio_sw_ring_helper_state {
+	struct iio_dev			*indio_dev;
+};
 #endif /* !CONFIG_IIO_RING_BUFFER */
 #endif /* _IIO_RING_SW_H_ */
