1dabc19e7f8a49cc2cb9dc3989b707470a0c9d90
Mike Lockwood <lockwood@android.com>
2010-07-29 14:42:24 -0400

    USB: g_mass_storage: Always allow disabling mass storage by writing to lun file
    For android builds we disable the check for curlun->prevent_medium_removal.
    Instead we let the framework manage unmounting policy, as we sometimes need
    to unmount after the media has been removed.
    This also helps support hosts that do not inform the device when the media
    has been unmounted.
    
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- drivers/usb/gadget/storage_common.c
+++ drivers/usb/gadget/storage_common.c
@@ -763,10 +763,16 @@ static ssize_t fsg_store_file(struct device *dev, struct device_attribute *attr,
 	struct rw_semaphore	*filesem = dev_get_drvdata(dev);
 	int		rc = 0;
 
+
+#ifndef CONFIG_USB_ANDROID_MASS_STORAGE
+	/* disabled in android because we need to allow closing the backing file
+	 * if the media was removed
+	 */
 	if (curlun->prevent_medium_removal && fsg_lun_is_open(curlun)) {
 		LDBG(curlun, "eject attempt prevented\n");
 		return -EBUSY;				/* "Door is locked" */
 	}
+#endif
 
 	/* Remove a trailing newline */
 	if (count > 0 && buf[count-1] == '\n')
