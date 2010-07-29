82ce67bf262b3f47ecb5a0ca31cace8ac72b7c98
Jarod Wilson <jarod@redhat.com>
2010-07-29 18:20:44 -0300

    V4L/DVB: staging/lirc: fix non-CONFIG_MODULES build horkage
    Fix when CONFIG_MODULES is not enabled:
    
    drivers/staging/lirc/lirc_parallel.c:243: error: implicit declaration of function 'module_refcount'
    drivers/staging/lirc/lirc_it87.c:150: error: implicit declaration of function 'module_refcount'
    drivers/built-in.o: In function `it87_probe':
    lirc_it87.c:(.text+0x4079b0): undefined reference to `init_chrdev'
    lirc_it87.c:(.text+0x4079cc): undefined reference to `drop_chrdev'
    drivers/built-in.o: In function `lirc_it87_exit':
    lirc_it87.c:(.exit.text+0x38a5): undefined reference to `drop_chrdev'
    
    Its a quick hack and untested beyond building, since I don't have the
    hardware, but it should do the trick.
    
    Acked-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/staging/lirc/lirc_parallel.c
+++ drivers/staging/lirc/lirc_parallel.c
@@ -240,7 +240,7 @@ static void irq_handler(void *blah)
 	unsigned int level, newlevel;
 	unsigned int timeout;
 
-	if (!module_refcount(THIS_MODULE))
+	if (!is_open)
 		return;
 
 	if (!is_claimed)
@@ -515,7 +515,7 @@ static long lirc_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
 
 static int lirc_open(struct inode *node, struct file *filep)
 {
-	if (module_refcount(THIS_MODULE) || !lirc_claim())
+	if (is_open || !lirc_claim())
 		return -EBUSY;
 
 	parport_enable_irq(pport);
