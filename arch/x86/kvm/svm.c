dbe7758482a870f30a86bdeefebf4fc260afef11
Joerg Roedel <joerg.roedel@amd.com>
2010-08-02 16:46:45 +0200

    KVM: SVM: Check for asid != 0 on nested vmrun
    This patch lets a nested vmrun fail if the L1 hypervisor
    left the asid zero. This fixes the asid_zero unit test.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -2019,6 +2019,9 @@ static bool nested_vmcb_checks(struct vmcb *vmcb)
 	if ((vmcb->control.intercept & (1ULL << INTERCEPT_VMRUN)) == 0)
 		return false;
 
+	if (vmcb->control.asid == 0)
+		return false;
+
 	return true;
 }
 
