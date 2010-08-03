c66bf6747f1516cb39f17815c8cf5841afd9a5e9
Michael Neuling <mikey@neuling.org>
2010-08-04 08:32:13 +1000

    CIFS: Fix compile error with __init in cifs_init_dns_resolver() definition
    An allmodconfig compile on ppc64 with 2.6.32.17 currently gives this error
    
    fs/cifs/dns_resolve.h:27: error: expected '=', ',', ';', 'asm' or '__attribute__' before 'cifs_init_dns_resolver'
    
    This adds the correct header file to fix this.
    
    Signed-off-by: Michael Neuling <mikey@neuling.org>
    Cc: David Howells <dhowells@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- fs/cifs/dns_resolve.h
+++ fs/cifs/dns_resolve.h
@@ -24,6 +24,8 @@
 #define _DNS_RESOLVE_H
 
 #ifdef __KERNEL__
+#include <linux/module.h>
+
 extern int __init cifs_init_dns_resolver(void);
 extern void cifs_exit_dns_resolver(void);
 extern int dns_resolve_server_name_to_ip(const char *unc, char **ip_addr);
