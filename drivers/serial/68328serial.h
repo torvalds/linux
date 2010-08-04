713e919e09492342eb8cd56f5aa7e3b33f672968
Christian Dietrich <qy03fugy@stud.informatik.uni-erlangen.de>
2010-08-04 14:42:43 +0200

    arch/m68knommu: Removing dead 68328_SERIAL_UART2 config option
    CONFIG_68328_SERIAL_UART2 doesn't exist in Kconfig, therefore removing
    all references to it from the source.
    
    Signed-off-by: Christian Dietrich <qy03fugy@stud.informatik.uni-erlangen.de>
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- drivers/serial/68328serial.h
+++ drivers/serial/68328serial.h
@@ -181,13 +181,8 @@ struct m68k_serial {
 /* 
  * Define the number of ports supported and their irqs.
  */
-#ifndef CONFIG_68328_SERIAL_UART2
 #define NR_PORTS 1
 #define UART_IRQ_DEFNS {UART_IRQ_NUM}
-#else
-#define NR_PORTS 2
-#define UART_IRQ_DEFNS {UART1_IRQ_NUM, UART2_IRQ_NUM}
-#endif
 
 #endif /* __KERNEL__ */
 #endif /* !(_MC683XX_SERIAL_H) */
