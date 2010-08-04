dbe3039e64b1dd4cf26f782d45b524f85b444ad4
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2010-08-04 14:09:23 +1000

    memblock/arm: Use memblock_region_is_memory() for omap fb
    Instead of the deprecated memblock_find()
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- drivers/video/omap2/vram.c
+++ drivers/video/omap2/vram.c
@@ -554,12 +554,8 @@ void __init omap_vram_reserve_sdram_memblock(void)
 	size = PAGE_ALIGN(size);
 
 	if (paddr) {
-		struct memblock_region res;
-
-		res.base = paddr;
-		res.size = size;
-		if ((paddr & ~PAGE_MASK) || memblock_find(&res) ||
-		    res.base != paddr || res.size != size) {
+		if ((paddr & ~PAGE_MASK) ||
+		    !memblock_region_is_memory(paddr, size)) {
 			pr_err("Illegal SDRAM region for VRAM\n");
 			return;
 		}
