9261ec1a8d7b17e2540bef7cad3470870d13b61e
Jason Wessel <jason.wessel@windriver.com>
2010-08-06 15:36:47 -0500

    console: Fix compilation regression
    A regression of building without CONFIG_HW_CONSOLE was introduced with
    commit b45cfba4e9005d64d419718e7ff7f7cab44c1994 (vt,console,kdb:
    implement atomic console enter/leave functions).
    
    ERROR: "con_debug_enter" [drivers/serial/kgdboc.ko] undefined!
    ERROR: "vc_cons" [drivers/serial/kgdboc.ko] undefined!
    ERROR: "fg_console" [drivers/serial/kgdboc.ko] undefined!
    ERROR: "con_debug_leave" [drivers/serial/kgdboc.ko] undefined!
    
    When there is no HW console the con_debug_enter and con_debug_leave
    functions should have no code.
    
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>
    CC: Jesse Barnes <jbarnes@virtuousgeek.org>
    Reported-by: Randy Dunlap <randy.dunlap@oracle.com>

--- include/linux/console.h
+++ include/linux/console.h
@@ -79,8 +79,13 @@ int register_con_driver(const struct consw *csw, int first, int last);
 int unregister_con_driver(const struct consw *csw);
 int take_over_console(const struct consw *sw, int first, int last, int deflt);
 void give_up_console(const struct consw *sw);
+#ifdef CONFIG_HW_CONSOLE
 int con_debug_enter(struct vc_data *vc);
 int con_debug_leave(void);
+#else
+#define con_debug_enter(vc) (0)
+#define con_debug_leave() (0)
+#endif
 
 /* scroll */
 #define SM_UP       (1)
