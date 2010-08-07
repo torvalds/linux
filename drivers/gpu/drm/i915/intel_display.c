592de9cc7e8c4886d05139771e748da2e9f13f4b
Chris Wilson <chris@chris-wilson.co.uk>
2010-08-07 11:01:36 +0100

    drm/i915/edp: Flush the write before waiting for PLLs
    commit 5ddb954b9ee50824977d2931e0ff58b3050b337d upstream.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -1402,6 +1402,7 @@ static void igdng_enable_pll_edp (struct drm_crtc *crtc)
 	dpa_ctl = I915_READ(DP_A);
 	dpa_ctl |= DP_PLL_ENABLE;
 	I915_WRITE(DP_A, dpa_ctl);
+	POSTING_READ(DP_A);
 	udelay(200);
 }
 
