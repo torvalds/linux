4b2f75922f29eecde0d590c450cab997e031db7f
Rebecca Schultz Zavin <rebecca@android.com>
2010-08-10 13:02:19 -0700

    [ARM] tegra: Modify board-stingray to use correct name for tegra_camera device
    Change-Id: I2dfd8100b264ecbfb43eed669881ec535c8a7bcd
    Signed-off-by: Rebecca Schultz Zavin <rebecca@android.com>

--- arch/arm/mach-tegra/board-stingray.c
+++ arch/arm/mach-tegra/board-stingray.c
@@ -489,8 +489,8 @@ static struct platform_device bcm4329_rfkill = {
 	.id = -1,
 };
 
-static struct platform_device tegra_isp = {
-	.name = "tegra_isp",
+static struct platform_device tegra_camera = {
+	.name = "tegra_camera",
 	.id = -1,
 };
 
@@ -546,7 +546,7 @@ static struct platform_device *stingray_devices[] __initdata = {
 	&tegra_spi_device4,
 	&tegra_gart_dev,
 	&ram_console_device,
-	&tegra_isp,
+	&tegra_camera,
 	&tegra_i2s_device1,
 };
 
