515033f97c0b5a1bce13fa93e09704d95b44f376
Anton Vorontsov <avorontsov@mvista.com>
2010-08-10 18:01:47 -0700

    sdhci-pltfm: switch to module device table matching
    Sometimes want to place SoC-specific parts alongside with the generic
    driver, and to do so, we have to switch the driver over to the module
    device table matching.
    
    Note that drivers/mmc/host/sdhci-pltfm.h is so far empty, but it'll hold
    SoC-specific driver data handlers soon.
    
    Signed-off-by: Anton Vorontsov <avorontsov@mvista.com>
    Cc: Ben Dooks <ben@simtec.co.uk>
    Cc: Richard R?jfors <richard.rojfors@pelagicore.com>
    Cc: <linux-mmc@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

---
+++ drivers/mmc/host/sdhci-pltfm.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2010 MontaVista Software, LLC.
+ *
+ * Author: Anton Vorontsov <avorontsov@ru.mvista.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _DRIVERS_MMC_SDHCI_PLTFM_H
+#define _DRIVERS_MMC_SDHCI_PLTFM_H
+
+#endif /* _DRIVERS_MMC_SDHCI_PLTFM_H */
