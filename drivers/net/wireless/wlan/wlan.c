1ced81423b43c5188087648b18f1ea7ddb777e8a
root <root@lbt-desktop.(none)>
2010-08-12 09:10:31 +0800

    remove compile warnings

--- drivers/net/wireless/wlan/wlan.c
+++ drivers/net/wireless/wlan/wlan.c
@@ -2,23 +2,21 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
-extern void if_sdio_init_module2(void);
+extern int if_sdio_init_module2(void);
 extern void if_sdio_exit_module(void);
 
 static int wlan_init_module(void)
 {
 	printk("Loading wlan driver..........\n");
-	if_sdio_init_module2();
-
-	return 0;
+	return if_sdio_init_module2();
 }
 
-static int wlan_exit_module(void)
+static void wlan_exit_module(void)
 {
 	printk("move wlan driver..........\n");
 	if_sdio_exit_module();
-
-	return 0;
 }
 module_init(wlan_init_module);
 module_exit(wlan_exit_module);
+
+MODULE_LICENSE("GPL V2");
