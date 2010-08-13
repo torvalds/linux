c7dcf87a6881bf796faee83003163eb3de41a309
John Stultz <johnstul@us.ibm.com>
2010-08-13 11:30:58 -0700

    time: Workaround gcc loop optimization that causes 64bit div errors
    Early 4.3 versions of gcc apparently aggressively optimize the raw
    time accumulation loop, replacing it with a divide.
    
    On 32bit systems, this causes the following link errors:
    	undefined reference to `__umoddi3'
    	undefined reference to `__udivdi3'
    
    The gcc issue has been fixed in 4.4 and greater.
    
    This patch replaces the accumulation loop with a do_div, as suggested
    by Linus.
    
    Signed-off-by: John Stultz <johnstul@us.ibm.com>
    CC: Jason Wessel <jason.wessel@windriver.com>
    CC: Larry Finger <Larry.Finger@lwfinger.net>
    CC: Ingo Molnar <mingo@elte.hu>
    CC: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -710,9 +710,10 @@ static cycle_t logarithmic_accumulation(cycle_t offset, int shift)
 	/* Accumulate raw time */
 	raw_nsecs = timekeeper.raw_interval << shift;
 	raw_nsecs += raw_time.tv_nsec;
-	while (raw_nsecs >= NSEC_PER_SEC) {
-		raw_nsecs -= NSEC_PER_SEC;
-		raw_time.tv_sec++;
+	if (raw_nsecs >= NSEC_PER_SEC) {
+		u64 raw_secs = raw_nsecs;
+		raw_nsecs = do_div(raw_secs, NSEC_PER_SEC);
+		raw_time.tv_sec += raw_secs;
 	}
 	raw_time.tv_nsec = raw_nsecs;
 
