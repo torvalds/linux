ec8f12533b4a439a8feb0d1a3bf15516781804be
Linus Walleij <linus.walleij@linaro.org>
2010-08-13 11:31:59 +0200

    mach-u300: config U300 PL180 PL011 PL022 for DMA
    This will configure the platform data for the PL180, PL011 and
    PL022 PrimeCells found in the U300 to use DMA with the generic
    PrimeCell DMA engine for COH 901 318.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-u300/mmc.c
+++ arch/arm/mach-u300/mmc.c
@@ -3,19 +3,22 @@
  * arch/arm/mach-u300/mmc.c
  *
  *
- * Copyright (C) 2009 ST-Ericsson AB
+ * Copyright (C) 2009 ST-Ericsson SA
  * License terms: GNU General Public License (GPL) version 2
  *
  * Author: Linus Walleij <linus.walleij@stericsson.com>
- * Author: Johan Lundin <johan.lundin@stericsson.com>
+ * Author: Johan Lundin
  * Author: Jonas Aaberg <jonas.aberg@stericsson.com>
  */
 #include <linux/device.h>
 #include <linux/amba/bus.h>
 #include <linux/mmc/host.h>
 #include <linux/gpio.h>
+#include <linux/dmaengine.h>
 #include <linux/amba/mmci.h>
 #include <linux/slab.h>
+#include <mach/coh901318.h>
+#include <mach/dma_channels.h>
 
 #include "mmc.h"
 #include "padmux.h"
@@ -32,6 +35,11 @@ static struct mmci_platform_data mmc0_plat_data = {
 	.cd_invert = true,
 	.capabilities = MMC_CAP_MMC_HIGHSPEED |
 	MMC_CAP_SD_HIGHSPEED | MMC_CAP_4_BIT_DATA | MMC_CAP_8_BIT_DATA,
+#ifdef CONFIG_COH901318
+	.dma_filter = coh901318_filter_id,
+	.dma_rx_param = (void *) U300_DMA_MMCSD_RX_TX,
+	/* Don't specify a TX channel, this RX channel is bidirectional */
+#endif
 };
 
 int __devinit mmc_init(struct amba_device *adev)
