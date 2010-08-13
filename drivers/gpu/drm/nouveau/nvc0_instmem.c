46d4cae20038bcb2511cba0c86f0be2d11520369
Ben Skeggs <bskeggs@redhat.com>
2010-08-13 10:22:41 +1000

    drm/nv50-nvc0: ramht_size is meant to be in bytes, not entries
    Fixes an infinite loop that can happen in RAMHT lookup.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvc0_instmem.c
+++ drivers/gpu/drm/nouveau/nvc0_instmem.c
@@ -221,7 +221,7 @@ nvc0_instmem_init(struct drm_device *dev)
 	/*XXX: incorrect, but needed to make hash func "work" */
 	dev_priv->ramht_offset = 0x10000;
 	dev_priv->ramht_bits   = 9;
-	dev_priv->ramht_size   = (1 << dev_priv->ramht_bits);
+	dev_priv->ramht_size   = (1 << dev_priv->ramht_bits) * 8;
 	return 0;
 }
 
