8539992f6091eb8206c781421312157d0c282e6e
Michal Simek <monstr@monstr.eu>
2010-08-18 00:26:34 +0000

    ll_temac: Fix poll implementation
    Functions ll_temac_rx_irq and ll_temac_tx_irq
    have pointer to net_device as second parameter not
    pointer to temac_local.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ll_temac_main.c
+++ drivers/net/ll_temac_main.c
@@ -902,8 +902,8 @@ temac_poll_controller(struct net_device *ndev)
 	disable_irq(lp->tx_irq);
 	disable_irq(lp->rx_irq);
 
-	ll_temac_rx_irq(lp->tx_irq, lp);
-	ll_temac_tx_irq(lp->rx_irq, lp);
+	ll_temac_rx_irq(lp->tx_irq, ndev);
+	ll_temac_tx_irq(lp->rx_irq, ndev);
 
 	enable_irq(lp->tx_irq);
 	enable_irq(lp->rx_irq);
