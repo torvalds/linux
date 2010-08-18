3b6036d148bad5bb7928b021a49bb9e395361084
Al Viro <viro@zeniv.linux.org.uk>
2010-08-18 06:21:10 -0400

    hostfs ->follow_link() braino
    we want the assignment to err done inside the if () to be
    visible after it, so (re)declaring err inside if () body
    is wrong.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/hostfs/hostfs_kern.c
+++ fs/hostfs/hostfs_kern.c
@@ -876,7 +876,7 @@ static void *hostfs_follow_link(struct dentry *dentry, struct nameidata *nd)
 		char *path = dentry_name(dentry);
 		int err = -ENOMEM;
 		if (path) {
-			int err = hostfs_do_readlink(path, link, PATH_MAX);
+			err = hostfs_do_readlink(path, link, PATH_MAX);
 			if (err == PATH_MAX)
 				err = -E2BIG;
 			__putname(path);
