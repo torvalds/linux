8d6c83f0ba5e1bd1e8bb2e3c7de4c276dc247f99
KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
2010-08-19 14:13:39 -0700

    oom: __task_cred() need rcu_read_lock()
    dump_tasks() needs to hold the RCU read lock around its access of the
    target task's UID.  To this end it should use task_uid() as it only needs
    that one thing from the creds.
    
    The fact that dump_tasks() holds tasklist_lock is insufficient to prevent the
    target process replacing its credentials on another CPU.
    
    Then, this patch change to call rcu_read_lock() explicitly.
    
    	===================================================
    	[ INFO: suspicious rcu_dereference_check() usage. ]
    	---------------------------------------------------
    	mm/oom_kill.c:410 invoked rcu_dereference_check() without protection!
    
    	other info that might help us debug this:
    
    	rcu_scheduler_active = 1, debug_locks = 1
    	4 locks held by kworker/1:2/651:
    	 #0:  (events){+.+.+.}, at: [<ffffffff8106aae7>]
    	process_one_work+0x137/0x4a0
    	 #1:  (moom_work){+.+...}, at: [<ffffffff8106aae7>]
    	process_one_work+0x137/0x4a0
    	 #2:  (tasklist_lock){.+.+..}, at: [<ffffffff810fafd4>]
    	out_of_memory+0x164/0x3f0
    	 #3:  (&(&p->alloc_lock)->rlock){+.+...}, at: [<ffffffff810fa48e>]
    	find_lock_task_mm+0x2e/0x70
    
    Signed-off-by: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Signed-off-by: David Howells <dhowells@redhat.com>
    Acked-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -372,7 +372,7 @@ static void dump_tasks(const struct mem_cgroup *mem)
 		}
 
 		pr_info("[%5d] %5d %5d %8lu %8lu %3u     %3d         %5d %s\n",
-			task->pid, __task_cred(task)->uid, task->tgid,
+			task->pid, task_uid(task), task->tgid,
 			task->mm->total_vm, get_mm_rss(task->mm),
 			task_cpu(task), task->signal->oom_adj,
 			task->signal->oom_score_adj, task->comm);
