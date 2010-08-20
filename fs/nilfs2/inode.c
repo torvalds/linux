518d1a6a1d6610a014875a2fe3252e89b6260d2f
Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>
2010-08-20 23:40:54 +0900

    nilfs2: allow nilfs_clear_inode to clear metadata file inodes
    Allows clear inode function (nilfs_clear_inode) to handle metadata
    files that uses bitmap-based object alloctor.  DAT and ifile
    correspond to this.
    
    Signed-off-by: Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>

--- fs/nilfs2/inode.c
+++ fs/nilfs2/inode.c
@@ -674,6 +674,7 @@ void nilfs_truncate(struct inode *inode)
 static void nilfs_clear_inode(struct inode *inode)
 {
 	struct nilfs_inode_info *ii = NILFS_I(inode);
+	struct nilfs_mdt_info *mdi = NILFS_MDT(inode);
 
 	/*
 	 * Free resources allocated in nilfs_read_inode(), here.
@@ -681,6 +682,9 @@ static void nilfs_clear_inode(struct inode *inode)
 	BUG_ON(!list_empty(&ii->i_dirty));
 	brelse(ii->i_bh);
 	ii->i_bh = NULL;
+
+	if (mdi && mdi->mi_palloc_cache)
+		nilfs_palloc_destroy_cache(inode);
 
 	if (test_bit(NILFS_I_BMAP, &ii->i_state))
 		nilfs_bmap_clear(ii->i_bmap);
