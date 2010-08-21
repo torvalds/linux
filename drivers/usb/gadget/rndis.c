2115157f0c9dec388cd19d06bd0384318bf23b36
Maxim Osipov <maxim.osipov@gmail.com>
2010-08-21 14:54:06 +0400

    USB: Fix kernel oops with g_ether and Windows
    commit 037d3656adbd7e8cb848f01cf5dec423ed76bbe7 upstream.
    
    Please find attached patch for
    https://bugzilla.kernel.org/show_bug.cgi?id=16023 problem.
    
    
    Signed-off-by: Maxim Osipov <maxim.osipov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/rndis.c
+++ drivers/usb/gadget/rndis.c
@@ -291,9 +291,13 @@ gen_ndis_query_resp (int configNr, u32 OID, u8 *buf, unsigned buf_len,
 	/* mandatory */
 	case OID_GEN_VENDOR_DESCRIPTION:
 		pr_debug("%s: OID_GEN_VENDOR_DESCRIPTION\n", __func__);
-		length = strlen (rndis_per_dev_params [configNr].vendorDescr);
-		memcpy (outbuf,
-			rndis_per_dev_params [configNr].vendorDescr, length);
+		if ( rndis_per_dev_params [configNr].vendorDescr ) {
+			length = strlen (rndis_per_dev_params [configNr].vendorDescr);
+			memcpy (outbuf,
+				rndis_per_dev_params [configNr].vendorDescr, length);
+		} else {
+			outbuf[0] = 0;
+		}
 		retval = 0;
 		break;
 
