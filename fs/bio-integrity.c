220eb7fd984bfc7e6b4005fdf32efe9cd8af7cf2
Andrew Morton <akpm@linux-foundation.org>
2010-08-23 13:36:20 +0200

    fs/bio-integrity.c: return -ENOMEM on kmalloc failure
    Cc: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

--- fs/bio-integrity.c
+++ fs/bio-integrity.c
@@ -416,7 +416,7 @@ int bio_integrity_prep(struct bio *bio)
 	buf = kmalloc(len, GFP_NOIO | q->bounce_gfp);
 	if (unlikely(buf == NULL)) {
 		printk(KERN_ERR "could not allocate integrity buffer\n");
-		return -EIO;
+		return -ENOMEM;
 	}
 
 	end = (((unsigned long) buf) + len + PAGE_SIZE - 1) >> PAGE_SHIFT;
