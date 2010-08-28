1963a2afc81afe6d85e7a12538b74a9919d958ae
Gregory Bean <gbean@codeaurora.org>
2010-08-28 10:05:44 -0700

    msm: add gpiomux api for gpio multiplex & configuration.
    Add the 'gpiomux' api, which addresses the following shortcomings
    of existing tlmm api:
    
    - gpio power-collapse, which is managed by a peripheral processor on
      other targets, must be managed by the application processor on the 8x60.
    - The enable/disable flag of the legacy gpio_tlmm_config api
      is not applicable on the 8x60, and causes confusion.
    - The gpio 'direction' bits are meaningless for all func_sel
      configurations except for generic-gpio mode (func_sel 0), in which
      case the gpio_direction_* functions should be used.  Having these
      bits in the tlmm api leads to confusion and misuse of the gpiolib
      api, and they have been removed in gpiomux.
    - The functional api of the legacy system ran contrary to the typical
      use-case, which is a single massive configuration at boot.  Rather
      than forcing hundreds of 'config' function calls, the new api
      allows data to be configured with a single table.
    
    gpiomux_get and gpiomux_put are meant to be called automatically
    when gpio_request and gpio_free are called, giving automatic
    gpiomux/tlmm control to those drivers/lines with simple
    power profiles - in the simplest cases, an entry in the gpiomux table
    and the correct usage of gpiolib is all that is required to get proper
    gpio power control.
    
    Signed-off-by: Gregory Bean <gbean@codeaurora.org>
    Signed-off-by: Daniel Walker <dwalker@codeaurora.org>

--- arch/arm/mach-msm/include/mach/msm_iomap-8x60.h
+++ arch/arm/mach-msm/include/mach/msm_iomap-8x60.h
@@ -20,10 +20,8 @@
  *
  */
 
-#ifndef __ASM_ARCH_MSM_IOMAP_H
-#define __ASM_ARCH_MSM_IOMAP_H
-
-#include <asm/sizes.h>
+#ifndef __ASM_ARCH_MSM_IOMAP_8X60_H
+#define __ASM_ARCH_MSM_IOMAP_8X60_H
 
 /* Physical base address and size of peripherals.
  * Ordered by the virtual base addresses they will be mapped at.
@@ -37,20 +35,8 @@
  *
  */
 
-#ifdef __ASSEMBLY__
-#define IOMEM(x)	x
-#else
-#define IOMEM(x)	((void __force __iomem *)(x))
-#endif
-
-#if defined(CONFIG_ARCH_MSM7X30)
-#include "msm_iomap-7x30.h"
-#elif defined(CONFIG_ARCH_QSD8X50)
-#include "msm_iomap-8x50.h"
-#elif defined(CONFIG_ARCH_MSM8X60)
-#include "msm_iomap-8x60.h"
-#else
-#include "msm_iomap-7x00.h"
-#endif
+#define MSM_TLMM_BASE		IOMEM(0xF0004000)
+#define MSM_TLMM_PHYS		0x00800000
+#define MSM_TLMM_SIZE		SZ_16K
 
 #endif
