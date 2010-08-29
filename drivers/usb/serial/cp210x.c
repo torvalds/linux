5b22a32e76defeb573991b301a27d299472c5714
A E Lawrence <lawrence_a_e@ntlworld.com>
2010-08-29 21:51:52 +0100

    USB: cp210x usb driver: add USB_DEVICE for Pirelli DP-L10 mobile.
    The Pirelli DP-L10 mobile is sold under various brand names. One, already
    supported by cp210x, is the T-COM TC300. Here is the lsusb for that version:
    -------------------------------------------------------------------
    Bus 001 Device 002: ID 0489:e000 Foxconn / Hon Hai T-Com TC 300
    Device Descriptor:
      bLength                18
      bDescriptorType         1
      bcdUSB               1.10
      bDeviceClass            0 (Defined at Interface level)
      bDeviceSubClass         0 
      bDeviceProtocol         0 
      bMaxPacketSize0        64
      idVendor           0x0489 Foxconn / Hon Hai
      idProduct          0xe000 T-Com TC 300
      bcdDevice            1.00
      iManufacturer           1 Silicon Labs
      iProduct                2 TC 300
      iSerial                 3 0001
        [snip]
    ---------------------------------------------------------------------------
    
    However the native Pirelli DP-L10 is not supported:
    ------------------------------------------------------------------
    Bus 001 Device 003: ID 0489:e003 Foxconn / Hon Hai Pirelli DP-L10
    Device Descriptor:
      bLength                18
      bDescriptorType         1
      bcdUSB               1.10
      bDeviceClass            0 (Defined at Interface level)
      bDeviceSubClass         0 
      bDeviceProtocol         0 
      bMaxPacketSize0        64
      idVendor           0x0489 Foxconn / Hon Hai
      idProduct          0xe003 Pirelli DP-L10
      bcdDevice            1.00
      iManufacturer           1 Silicon Labs
      iProduct                2 DP-L10
      iSerial                 3 0001
        [snip]
    -------------------------------------------------------------------------
    
    All that is required is an extra USB_DEVICE entry:
    
    { USB_DEVICE(0x0489, 0xE003) }, /* Pirelli Broadband S.p.A, DP-L10 SIP/GSM
    +Mobile */
    
    The patch adds that entry. Tested under 2.6.36-rc2 from git.
    
    Signed-off-by: A E Lawrence <lawrence_a_e@ntlworld.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/cp210x.c
+++ drivers/usb/serial/cp210x.c
@@ -56,6 +56,7 @@ static int debug;
 static const struct usb_device_id id_table[] = {
 	{ USB_DEVICE(0x0471, 0x066A) }, /* AKTAKOM ACE-1001 cable */
 	{ USB_DEVICE(0x0489, 0xE000) }, /* Pirelli Broadband S.p.A, DP-L10 SIP/GSM Mobile */
+	{ USB_DEVICE(0x0489, 0xE003) }, /* Pirelli Broadband S.p.A, DP-L10 SIP/GSM Mobile */
 	{ USB_DEVICE(0x0745, 0x1000) }, /* CipherLab USB CCD Barcode Scanner 1000 */
 	{ USB_DEVICE(0x08e6, 0x5501) }, /* Gemalto Prox-PU/CU contactless smartcard reader */
 	{ USB_DEVICE(0x08FD, 0x000A) }, /* Digianswer A/S , ZigBee/802.15.4 MAC Device */
