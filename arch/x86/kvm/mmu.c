f87f928882d080eaec8b0d76aecff003d664697d
Joerg Roedel <joerg.roedel@amd.com>
2010-09-02 17:29:45 +0200

    KVM: MMU: Fix 32 bit legacy paging with NPT
    This patch fixes 32 bit legacy paging with NPT enabled. The
    mmu_check_root call on the top-level of the loop causes
    root_gfn to take values (in the tdp_enabled path) which are
    outside of guest memory. So the mmu_check_root call fails at
    some point in the loop interation causing the guest to
    tiple-fault.
    This patch changes the mmu_check_root calls to the places
    where they are really necessary. As a side-effect it
    introduces a check for the root of a pae page table too.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/x86/kvm/mmu.c
+++ arch/x86/kvm/mmu.c
@@ -2387,6 +2387,10 @@ static int mmu_alloc_roots(struct kvm_vcpu *vcpu)
 		return 0;
 	}
 	direct = !is_paging(vcpu);
+
+	if (mmu_check_root(vcpu, root_gfn))
+		return 1;
+
 	for (i = 0; i < 4; ++i) {
 		hpa_t root = vcpu->arch.mmu.pae_root[i];
 
@@ -2398,10 +2402,10 @@ static int mmu_alloc_roots(struct kvm_vcpu *vcpu)
 				continue;
 			}
 			root_gfn = pdptr >> PAGE_SHIFT;
+			if (mmu_check_root(vcpu, root_gfn))
+				return 1;
 		} else if (vcpu->arch.mmu.root_level == 0)
 			root_gfn = 0;
-		if (mmu_check_root(vcpu, root_gfn))
-			return 1;
 		if (tdp_enabled) {
 			direct = 1;
 			root_gfn = i << 30;
