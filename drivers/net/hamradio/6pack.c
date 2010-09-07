89d9f10d0b21268d3dba33db984ab03092861700
Thomas Gleixner <tglx@linutronix.de>
2010-09-07 14:32:14 +0000

    hamradio: 6pack: semaphore cleanup
    Get rid of init_MUTEX[_LOCKED]() and use sema_init() instead.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Christoph Hellwig <hch@infradead.org>
    Acked-by: David Miller <davem@davemloft.net>
    LKML-Reference: <20100907125055.269142443@linutronix.de>

--- drivers/net/hamradio/6pack.c
+++ drivers/net/hamradio/6pack.c
@@ -608,7 +608,7 @@ static int sixpack_open(struct tty_struct *tty)
 
 	spin_lock_init(&sp->lock);
 	atomic_set(&sp->refcnt, 1);
-	init_MUTEX_LOCKED(&sp->dead_sem);
+	sema_init(&sp->dead_sem, 0);
 
 	/* !!! length of the buffers. MTU is IP MTU, not PACLEN!  */
 
