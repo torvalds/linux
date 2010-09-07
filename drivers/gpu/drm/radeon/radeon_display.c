ff32a59daea4f3eae980ae8d0932b135c633ec5d
Alex Deucher <alexdeucher@gmail.com>
2010-09-07 13:26:39 -0400

    drm/radeon/kms: fix regression in RMX code (v2)
    caused by d65d65b175a29bd7ea2bb69c046419329c4a5db7
    
    need to update the radeon crtc priv native mode before using it.
    
    Fixes:
    https://bugs.freedesktop.org/show_bug.cgi?id=30049
    
    v2: integrate v/h copy paste typo
    
    Signed-off-by: Alex Deucher <alexdeucher@gmail.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/radeon_display.c
+++ drivers/gpu/drm/radeon/radeon_display.c
@@ -1140,14 +1140,14 @@ bool radeon_crtc_scaling_mode_fixup(struct drm_crtc *crtc,
 				radeon_crtc->rmx_type = radeon_encoder->rmx_type;
 			else
 				radeon_crtc->rmx_type = RMX_OFF;
-			src_v = crtc->mode.vdisplay;
-			dst_v = radeon_crtc->native_mode.vdisplay;
-			src_h = crtc->mode.hdisplay;
-			dst_h = radeon_crtc->native_mode.vdisplay;
 			/* copy native mode */
 			memcpy(&radeon_crtc->native_mode,
 			       &radeon_encoder->native_mode,
 				sizeof(struct drm_display_mode));
+			src_v = crtc->mode.vdisplay;
+			dst_v = radeon_crtc->native_mode.vdisplay;
+			src_h = crtc->mode.hdisplay;
+			dst_h = radeon_crtc->native_mode.hdisplay;
 
 			/* fix up for overscan on hdmi */
 			if (ASIC_IS_AVIVO(rdev) &&
