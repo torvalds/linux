51ae5d558f6a136d2e83fa5ac5fe78ad7afcd7be
Mike McCormack <mikem@ring3k.org>
2010-09-08 22:04:45 +0900

    Staging: rtl8192e: Remove extern and redundant declarations
    Signed-off-by: Mike McCormack <mikem@ring3k.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8192e/r8192E_wx.h
+++ drivers/staging/rtl8192e/r8192E_wx.h
@@ -17,5 +17,5 @@
 //#include <linux/wireless.h>
 extern struct iw_handler_def r8192_wx_handlers_def;
 /* Enable  the rtl819x_core.c to share this function, david 2008.9.22 */
-extern struct iw_statistics *r8192_get_wireless_stats(struct net_device *dev);
+struct iw_statistics *r8192_get_wireless_stats(struct net_device *dev);
 #endif
