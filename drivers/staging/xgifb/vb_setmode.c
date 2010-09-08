51973d7fa0a2f2087e640200fda23f6bac71fae2
Greg Kroah-Hartman <gregkh@suse.de>
2010-09-08 01:49:45 -0700

    Staging: xgifb: revert a warning fix and resolve it properly
    Jiri pointed out that the previous compiler warning fix was incorrect as
    the compiler was properly saying that the variable was not being set.
    This was due to a #define commenting out all of the logic in the
    function.
    
    This patch reverts Javier's previous one, and properly fixes the issue
    by making the function actually doing something.
    
    Reported-by: Jiri Slaby <jirislaby@gmail.com>
    Cc: Javier Martinez Canillas <martinez.javier@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/xgifb/vb_setmode.c
+++ drivers/staging/xgifb/vb_setmode.c
@@ -376,7 +376,7 @@ void InitTo330Pointer(unsigned char ChipType, struct vb_device_info *pVBInfo)
 unsigned char XGISetModeNew(struct xgi_hw_device_info *HwDeviceExtension,
 			unsigned short ModeNo)
 {
-	unsigned short ModeIdIndex = 0;
+	unsigned short ModeIdIndex;
 	/* unsigned char *pVBInfo->FBAddr = HwDeviceExtension->pjVideoMemoryAddress; */
 	struct vb_device_info VBINF;
 	struct vb_device_info *pVBInfo = &VBINF;
@@ -3512,9 +3512,6 @@ unsigned char XGI_GetLCDInfo(unsigned short ModeNo, unsigned short ModeIdIndex,
 unsigned char XGI_SearchModeID(unsigned short ModeNo,
 		unsigned short *ModeIdIndex, struct vb_device_info *pVBInfo)
 {
-
-#ifdef LINUX /* chiawen for linux solution */
-
 	if (ModeNo <= 5)
 		ModeNo |= 1;
 	if (ModeNo <= 0x13) {
@@ -3540,8 +3537,6 @@ unsigned char XGI_SearchModeID(unsigned short ModeNo,
 				return 0;
 		}
 	}
-
-#endif
 
 	return 1;
 }
@@ -3834,7 +3829,7 @@ unsigned char XGI_SetCRT2Group301(unsigned short ModeNo,
 		struct xgi_hw_device_info *HwDeviceExtension,
 		struct vb_device_info *pVBInfo)
 {
-	unsigned short tempbx, ModeIdIndex = 0, RefreshRateTableIndex;
+	unsigned short tempbx, ModeIdIndex, RefreshRateTableIndex;
 
 	tempbx = pVBInfo->VBInfo;
 	pVBInfo->SetFlag |= ProgrammingCRT2;
