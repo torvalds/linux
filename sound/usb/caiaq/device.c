15c5ab607045e278ebf4d2ca4aea2250617d50ca
Daniel Mack <daniel@caiaq.de>
2010-09-10 17:04:57 +0800

    ALSA: snd-usb-caiaq: Add support for Traktor Kontrol S4
    This patch adds support for the new Traktor Kontrol S4 by Native
    Instruments. It features a new audio data streaming model, MIDI
    in and out ports, a huge number of 174 dimmable LEDs, 96 buttons
    and 46 absolute encoder axis, including some rotary encoders.
    
    All features are supported by the driver now.
    
    Did some code refactoring along the way.
    
    Signed-off-by: Daniel Mack <daniel@caiaq.de>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/usb/caiaq/device.c
+++ sound/usb/caiaq/device.c
@@ -48,7 +48,8 @@ MODULE_SUPPORTED_DEVICE("{{Native Instruments, RigKontrol2},"
 			 "{Native Instruments, Audio 8 DJ},"
 			 "{Native Instruments, Session I/O},"
 			 "{Native Instruments, GuitarRig mobile}"
-			 "{Native Instruments, Traktor Kontrol X1}");
+			 "{Native Instruments, Traktor Kontrol X1}"
+			 "{Native Instruments, Traktor Kontrol S4}");
 
 static int index[SNDRV_CARDS] = SNDRV_DEFAULT_IDX; /* Index 0-max */
 static char* id[SNDRV_CARDS] = SNDRV_DEFAULT_STR; /* Id for this card */
@@ -133,6 +134,11 @@ static struct usb_device_id snd_usb_id_table[] = {
 		.match_flags =  USB_DEVICE_ID_MATCH_DEVICE,
 		.idVendor =     USB_VID_NATIVEINSTRUMENTS,
 		.idProduct =    USB_PID_TRAKTORKONTROLX1
+	},
+	{
+		.match_flags =  USB_DEVICE_ID_MATCH_DEVICE,
+		.idVendor =     USB_VID_NATIVEINSTRUMENTS,
+		.idProduct =    USB_PID_TRAKTORKONTROLS4
 	},
 	{ /* terminator */ }
 };
