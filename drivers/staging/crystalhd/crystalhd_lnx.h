cae16a1742ad3cc19f15e78b821b94448e330ad9
Vasiliy Kulikov <segooon@gmail.com>
2010-09-12 22:56:51 +0400

    staging: crystalhd: fix signed bug
    chd_dec_major is unsigned, so check chd_dec_major < 0 doesn't make sense.
    Since it is used as signed, declare it as int.
    
    Signed-off-by: Vasiliy Kulikov <segooon@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/crystalhd/crystalhd_lnx.h
+++ drivers/staging/crystalhd/crystalhd_lnx.h
@@ -76,7 +76,7 @@ struct crystalhd_adp {
 	spinlock_t		lock;
 
 	/* API Related */
-	unsigned int		chd_dec_major;
+	int		chd_dec_major;
 	unsigned int		cfg_users;
 
 	struct crystalhd_ioctl_data	*idata_free_head;	/* ioctl data pool */
