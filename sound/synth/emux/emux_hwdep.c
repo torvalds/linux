a254dba37c5a372fc8b44ba29509ba052d4e859d
Ben Hutchings <ben@decadent.org.uk>
2010-09-12 02:41:47 +0100

    ALSA: emux: Add trivial compat ioctl handler
    Reported-by: Carmen Cru <carmen.cru@belgacom.net>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/synth/emux/emux_hwdep.c
+++ sound/synth/emux/emux_hwdep.c
@@ -128,6 +128,9 @@ snd_emux_init_hwdep(struct snd_emux *emu)
 	strcpy(hw->name, SNDRV_EMUX_HWDEP_NAME);
 	hw->iface = SNDRV_HWDEP_IFACE_EMUX_WAVETABLE;
 	hw->ops.ioctl = snd_emux_hwdep_ioctl;
+	/* The ioctl parameter types are compatible between 32- and
+	 * 64-bit architectures, so use the same function. */
+	hw->ops.ioctl_compat = snd_emux_hwdep_ioctl;
 	hw->exclusive = 1;
 	hw->private_data = emu;
 	if ((err = snd_card_register(emu->card)) < 0)
