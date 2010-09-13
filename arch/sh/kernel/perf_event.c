64a48d90149bb7007275fac13eaa4cb8d8ce12ea
Matt Fleming <matt@console-pimps.org>
2010-09-13 20:17:29 +0100

    perf, sh: Fix patch merge foul-up
    3f6da390 ("perf: Rework and fix the arch CPU-hotplug hooks") introduced
    this breakage. sh_pmu_setup() is missing an opening curly brace.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    LKML-Reference: <20100913191729.GA6440@console-pimps.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/sh/kernel/perf_event.c
+++ arch/sh/kernel/perf_event.c
@@ -338,7 +338,7 @@ static struct pmu pmu = {
 };
 
 static void sh_pmu_setup(int cpu)
-
+{
 	struct cpu_hw_events *cpuhw = &per_cpu(cpu_hw_events, cpu);
 
 	memset(cpuhw, 0, sizeof(struct cpu_hw_events));
