632cf92a72fecda096d0f4608eaefb2c7392b744
Thomas Gleixner <tglx@linutronix.de>
2010-09-14 07:12:35 -0400

    mmc: Convert "mutex" to semaphore
    Get rid of init_MUTEX[_LOCKED]() and use sema_init() instead.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-mmc@vger.kernel.org
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/card/queue.c
+++ drivers/mmc/card/queue.c
@@ -208,7 +208,7 @@ int mmc_init_queue(struct mmc_queue *mq, struct mmc_card *card, spinlock_t *lock
 		sg_init_table(mq->sg, host->max_segs);
 	}
 
-	init_MUTEX(&mq->thread_sem);
+	sema_init(&mq->thread_sem, 1);
 
 	mq->thread = kthread_run(mmc_queue_thread, mq, "mmcqd");
 	if (IS_ERR(mq->thread)) {
