8b8e2ec1eeca7f6941bc81cefc9663018d6ceb57
Peter Zijlstra <a.p.zijlstra@chello.nl>
2010-09-16 19:21:28 +0200

    percpu: Add {get,put}_cpu_ptr
    These are similar to {get,put}_cpu_var() except for dynamically
    allocated per-cpu memory.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Acked-by: Tejun Heo <tj@kernel.org>
    LKML-Reference: <20100917093009.252867712@chello.nl>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/percpu.h
+++ include/linux/percpu.h
@@ -39,6 +39,15 @@
 	preempt_enable();				\
 } while (0)
 
+#define get_cpu_ptr(var) ({				\
+	preempt_disable();				\
+	this_cpu_ptr(var); })
+
+#define put_cpu_ptr(var) do {				\
+	(void)(var);					\
+	preempt_enable();				\
+} while (0)
+
 #ifdef CONFIG_SMP
 
 /* minimum unit size, also is the maximum supported allocation size */
