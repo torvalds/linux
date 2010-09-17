8a41fa3b3c89e5bd3c69219ddeee268bdcce886c
Hans J. Koch <hjk@linutronix.de>
2010-09-17 18:18:57 +0200

    ARM: Basic IO mappings for mach-tcc8k
    Map the IO ranges of TCC8xxx peripherals.
    
    Signed-off-by: "Hans J. Koch" <hjk@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/arm/mach-tcc8k/common.h
+++ arch/arm/mach-tcc8k/common.h
@@ -6,5 +6,6 @@ struct clk;
 extern void tcc_clocks_init(unsigned long xi_freq, unsigned long xti_freq);
 extern void tcc8k_timer_init(struct clk *clock, void __iomem *base, int irq);
 extern void tcc8k_init_irq(void);
+extern void tcc8k_map_common_io(void);
 
 #endif
