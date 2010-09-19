3c093865aecc7addd34a56b5cf4b2ee9223d9b2d
Pekka Enberg <penberg@kernel.org>
2010-09-19 12:28:41 +0300

    staging: w35und: Inline mlme_s.h to core.h
    The mlme_s.h header is included in one place. As the header is very small, just
    inline it to core.h.
    
    Cc: Lars Lindley <lindley@coyote.org>
    Cc: Pavel Machek <pavel@ucw.cz>
    Cc: Ruslan Pisarev <ruslan@rpisarev.org.ua>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/winbond/core.h
+++ drivers/staging/winbond/core.h
@@ -1,11 +1,44 @@
 #ifndef __WINBOND_CORE_H
 #define __WINBOND_CORE_H
 
+#include <linux/spinlock.h>
 #include <linux/wireless.h>
+#include <linux/types.h>
 
-#include "mlme_s.h"
 #include "wbhal_s.h"
 #include "mto.h"
+
+#include "mac_structures.h"
+#include "mds_s.h"
+
+#define MAX_NUM_TX_MMPDU		2
+#define MAX_MMPDU_SIZE			1512
+#define MAX_NUM_RX_MMPDU		6
+
+struct mlme_frame {
+	s8		*pMMPDU;
+	u16		len;
+	u8		DataType;
+	u8		IsInUsed;
+
+	spinlock_t	MLMESpinLock;
+
+	u8		TxMMPDU[MAX_NUM_TX_MMPDU][MAX_MMPDU_SIZE];
+	u8		TxMMPDUInUse[(MAX_NUM_TX_MMPDU + 3) & ~0x03];
+
+	u16		wNumTxMMPDU;
+	u16		wNumTxMMPDUDiscarded;
+
+	u8		RxMMPDU[MAX_NUM_RX_MMPDU][MAX_MMPDU_SIZE];
+	u8		SaveRxBufSlotInUse[(MAX_NUM_RX_MMPDU + 3) & ~0x03];
+
+	u16		wNumRxMMPDU;
+	u16		wNumRxMMPDUDiscarded;
+
+	u16		wNumRxMMPDUInMLME;	/* Number of the Rx MMPDU */
+	u16		reserved_1;		/*  in MLME. */
+						/*  excluding the discarded */
+};
 
 #define WBLINUX_PACKET_ARRAY_SIZE (ETHERNET_TX_DESCRIPTORS*4)
 
