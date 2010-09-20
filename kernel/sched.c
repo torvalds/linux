a8027073eb127cd207070891374b5c54c2ce3d23
Steven Rostedt <srostedt@redhat.com>
2010-09-20 15:13:34 -0400

    tracing/sched: Add sched_pi_setprio tracepoint
    Add a tracepoint that shows the priority of a task being boosted
    via priority inheritance.
    
    Cc: Gregory Haskins <ghaskins@novell.com>
    Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/sched.c
+++ kernel/sched.c
@@ -4355,6 +4355,7 @@ void rt_mutex_setprio(struct task_struct *p, int prio)
 
 	rq = task_rq_lock(p, &flags);
 
+	trace_sched_pi_setprio(p, prio);
 	oldprio = p->prio;
 	prev_class = p->sched_class;
 	on_rq = p->se.on_rq;
