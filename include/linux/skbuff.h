cb4dfe562cac6fcb544df752e40c1d78000d0712
Eric Dumazet <eric.dumazet@gmail.com>
2010-09-23 05:06:54 +0000

    net: skb_frag_t can be smaller on small arches
    On 32bit arches, if PAGE_SIZE is smaller than 65536, we can use 16bit
    offset and size fields. This patch saves 72 bytes per skb on i386, or
    128 bytes after rounding.
    
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -129,8 +129,13 @@ typedef struct skb_frag_struct skb_frag_t;
 
 struct skb_frag_struct {
 	struct page *page;
+#if (BITS_PER_LONG > 32) || (PAGE_SIZE >= 65536)
 	__u32 page_offset;
 	__u32 size;
+#else
+	__u16 page_offset;
+	__u16 size;
+#endif
 };
 
 #define HAVE_HW_TIME_STAMP
