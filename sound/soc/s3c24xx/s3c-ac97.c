8d85d7414a681ad17f6bfc7564e310caee2c868d
Mark Brown <broonie@opensource.wolfsonmicro.com>
2010-09-23 17:41:46 +0100

    ASoC: Add debug logging for s3c-ac97 resets
    Helps tracing errors further up the stack.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Jassi Brar <jassi.brar@samsung.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- sound/soc/s3c24xx/s3c-ac97.c
+++ sound/soc/s3c24xx/s3c-ac97.c
@@ -163,6 +163,7 @@ static void s3c_ac97_write(struct snd_ac97 *ac97, unsigned short reg,
 
 static void s3c_ac97_cold_reset(struct snd_ac97 *ac97)
 {
+	pr_debug("AC97: Cold reset\n");
 	writel(S3C_AC97_GLBCTRL_COLDRESET,
 			s3c_ac97.regs + S3C_AC97_GLBCTRL);
 	msleep(1);
@@ -178,6 +179,8 @@ static void s3c_ac97_warm_reset(struct snd_ac97 *ac97)
 	stat = readl(s3c_ac97.regs + S3C_AC97_GLBSTAT) & 0x7;
 	if (stat == S3C_AC97_GLBSTAT_MAINSTATE_ACTIVE)
 		return; /* Return if already active */
+
+	pr_debug("AC97: Warm reset\n");
 
 	writel(S3C_AC97_GLBCTRL_WARMRESET, s3c_ac97.regs + S3C_AC97_GLBCTRL);
 	msleep(1);
