bfc3d57ff75a6ac34cc4b88a4abd6d6d9ef32e73
Mauro Carvalho Chehab <mchehab@redhat.com>
2010-09-26 00:39:26 -0300

    V4L/DVB: tda18271: Add some hint about what tda18217 reg ID returned
    Instead of doing:
    
    [   82.581639] tda18271 4-0060: creating new instance
    [   82.588411] Unknown device detected @ 4-0060, device not supported.
    [   82.594695] tda18271_attach: [4-0060|M] error -22 on line 1272
    [   82.600530] tda18271 4-0060: destroying instance
    
    Print:
    [  468.740392] Unknown device (0) detected @ 4-0060, device not supported.
    
    for the error message, to help detecting what's going wrong with the
    device.
    
    This helps to detect when the driver is using the wrong I2C bus (or have
    the i2g gate switch pointing to the wrong place), on devices like cx231xx
    that just return 0 on reads to a non-existent i2c device.
    
    Reviewed-by: Michael Krufky <mkrufky@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/common/tuners/tda18271-fe.c
+++ drivers/media/common/tuners/tda18271-fe.c
@@ -1156,7 +1156,6 @@ static int tda18271_get_id(struct dvb_frontend *fe)
 	struct tda18271_priv *priv = fe->tuner_priv;
 	unsigned char *regs = priv->tda18271_regs;
 	char *name;
-	int ret = 0;
 
 	mutex_lock(&priv->lock);
 	tda18271_read_regs(fe);
@@ -1172,17 +1171,16 @@ static int tda18271_get_id(struct dvb_frontend *fe)
 		priv->id = TDA18271HDC2;
 		break;
 	default:
-		name = "Unknown device";
-		ret = -EINVAL;
-		break;
+		tda_info("Unknown device (%i) detected @ %d-%04x, device not supported.\n",
+			 regs[R_ID], i2c_adapter_id(priv->i2c_props.adap),
+			 priv->i2c_props.addr);
+		return -EINVAL;
 	}
 
-	tda_info("%s detected @ %d-%04x%s\n", name,
-		 i2c_adapter_id(priv->i2c_props.adap),
-		 priv->i2c_props.addr,
-		 (0 == ret) ? "" : ", device not supported.");
+	tda_info("%s detected @ %d-%04x\n", name,
+		 i2c_adapter_id(priv->i2c_props.adap), priv->i2c_props.addr);
 
-	return ret;
+	return 0;
 }
 
 static int tda18271_setup_configuration(struct dvb_frontend *fe,
