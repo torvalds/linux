77ffc1465cec32489889d6bc9c288b7b0d2ce9fb
Mike Rapoport <mike@compulab.co.il>
2010-09-27 11:26:33 +0200

    tegra: add PCI Express support
    Change-Id: Ibd0bcd46895eb88952b9db29e1f68572d39aae01
    Signed-off-by: Mike Rapoport <mike@compulab.co.il>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    CC: Russell King <linux@arm.linux.org.uk>
    CC: Gary King <GKing@nvidia.com>
    Signed-off-by: Colin Cross <ccross@android.com>

--- arch/arm/mach-tegra/include/mach/io.h
+++ arch/arm/mach-tegra/include/mach/io.h
@@ -21,7 +21,7 @@
 #ifndef __MACH_TEGRA_IO_H
 #define __MACH_TEGRA_IO_H
 
-#define IO_SPACE_LIMIT 0xffffffff
+#define IO_SPACE_LIMIT 0xffff
 
 /* On TEGRA, many peripherals are very closely packed in
  * two 256MB io windows (that actually only use about 64KB
@@ -73,10 +73,20 @@ void tegra_iounmap(volatile void __iomem *addr);
 
 #define IO_ADDRESS(n) ((void __iomem *) IO_TO_VIRT(n))
 
+#ifdef CONFIG_TEGRA_PCI
+extern void __iomem *tegra_pcie_io_base;
+
+static inline void __iomem *__io(unsigned long addr)
+{
+	return tegra_pcie_io_base + (addr & IO_SPACE_LIMIT);
+}
+#else
 static inline void __iomem *__io(unsigned long addr)
 {
 	return (void __iomem *)addr;
 }
+#endif
+
 #define __io(a)         __io(a)
 #define __mem_pci(a)    (a)
 
