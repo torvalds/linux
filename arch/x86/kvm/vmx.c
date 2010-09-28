07d6f555d536aad1d74bb8b41dae9385007ecc26
Jan Kiszka <jan.kiszka@siemens.com>
2010-09-28 16:37:42 +0200

    KVM: VMX: Add AX to list of registers clobbered by guest switch
    By chance this caused no harm so far. We overwrite AX during switch
    to/from guest context, so we must declare this.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -4007,7 +4007,7 @@ static void vmx_vcpu_run(struct kvm_vcpu *vcpu)
 #endif
 		[cr2]"i"(offsetof(struct vcpu_vmx, vcpu.arch.cr2))
 	      : "cc", "memory"
-		, R"bx", R"di", R"si"
+		, R"ax", R"bx", R"di", R"si"
 #ifdef CONFIG_X86_64
 		, "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"
 #endif
