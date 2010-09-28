47de918e751ae19c78d923b92566072d70f9f3ae
Len Brown <len.brown@intel.com>
2010-09-28 17:20:20 -0400

    ACPI: delete ZEPTO idle=nomwait DMI quirk
    commit 64a32307b710c100beb101e9c78f8022f0e8ba61 upstream.
    
    per comments in the bug report, this entry
    seems to hurt at much as it helps.
    
    https://bugzilla.kernel.org/show_bug.cgi?id=10807
    
    Signed-off-by: Len Brown <len.brown@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/acpi/processor_core.c
+++ drivers/acpi/processor_core.c
@@ -134,12 +134,6 @@ static int set_no_mwait(const struct dmi_system_id *id)
 
 static struct dmi_system_id __cpuinitdata processor_idle_dmi_table[] = {
 	{
-	set_no_mwait, "IFL91 board", {
-	DMI_MATCH(DMI_BIOS_VENDOR, "COMPAL"),
-	DMI_MATCH(DMI_SYS_VENDOR, "ZEPTO"),
-	DMI_MATCH(DMI_PRODUCT_VERSION, "3215W"),
-	DMI_MATCH(DMI_BOARD_NAME, "IFL91") }, NULL},
-	{
 	set_no_mwait, "Extensa 5220", {
 	DMI_MATCH(DMI_BIOS_VENDOR, "Phoenix Technologies LTD"),
 	DMI_MATCH(DMI_SYS_VENDOR, "Acer"),
