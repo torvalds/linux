f6dedecc37164a58bb80ae2ed9d204669ffc4850
Jiri Olsa <jolsa@redhat.com>
2010-09-29 10:46:47 -0400

    oprofile, x86: Adding backtrace dump for 32bit process in compat mode
    This patch implements the oprofile backtrace  generation for 32 bit
    applications running in the 64bit environment (compat mode).
    
    With this change it's possible to get backtrace for 32bits applications
    under the 64bits environment using oprofile's callgraph options.
    
    opcontrol --setup -c ...
    opreport -l -cg ...
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Signed-off-by: Robert Richter <robert.richter@amd.com>

--- arch/x86/oprofile/backtrace.c
+++ arch/x86/oprofile/backtrace.c
@@ -14,6 +14,7 @@
 #include <asm/ptrace.h>
 #include <asm/uaccess.h>
 #include <asm/stacktrace.h>
+#include <linux/compat.h>
 
 static void backtrace_warning_symbol(void *data, char *msg,
 				     unsigned long symbol)
@@ -48,6 +49,55 @@ static struct stacktrace_ops backtrace_ops = {
 	.walk_stack	= print_context_stack,
 };
 
+#ifdef CONFIG_COMPAT
+static struct stack_frame_ia32 *
+dump_user_backtrace_32(struct stack_frame_ia32 *head)
+{
+	struct stack_frame_ia32 bufhead[2];
+	struct stack_frame_ia32 *fp;
+
+	/* Also check accessibility of one struct frame_head beyond */
+	if (!access_ok(VERIFY_READ, head, sizeof(bufhead)))
+		return NULL;
+	if (__copy_from_user_inatomic(bufhead, head, sizeof(bufhead)))
+		return NULL;
+
+	fp = (struct stack_frame_ia32 *) compat_ptr(bufhead[0].next_frame);
+
+	oprofile_add_trace(bufhead[0].return_address);
+
+	/* frame pointers should strictly progress back up the stack
+	* (towards higher addresses) */
+	if (head >= fp)
+		return NULL;
+
+	return fp;
+}
+
+static inline int
+x86_backtrace_32(struct pt_regs * const regs, unsigned int depth)
+{
+	struct stack_frame_ia32 *head;
+
+	/* User process is 32-bit */
+	if (!current || !test_thread_flag(TIF_IA32))
+		return 0;
+
+	head = (struct stack_frame_ia32 *) regs->bp;
+	while (depth-- && head)
+		head = dump_user_backtrace_32(head);
+
+	return 1;
+}
+
+#else
+static inline int
+x86_backtrace_32(struct pt_regs * const regs, unsigned int depth)
+{
+	return 0;
+}
+#endif /* CONFIG_COMPAT */
+
 static struct stack_frame *dump_user_backtrace(struct stack_frame *head)
 {
 	struct stack_frame bufhead[2];
@@ -80,6 +130,9 @@ x86_backtrace(struct pt_regs * const regs, unsigned int depth)
 				   &backtrace_ops, &depth);
 		return;
 	}
+
+	if (x86_backtrace_32(regs, depth))
+		return;
 
 	while (depth-- && head)
 		head = dump_user_backtrace(head);
