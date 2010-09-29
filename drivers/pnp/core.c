c1f3f2819667a238585c65bba96c8b16af39a442
Bjorn Helgaas <bjorn.helgaas@hp.com>
2010-09-29 12:24:23 -0600

    PNP: log PNP resources, as we do for PCI
    ACPI devices are often involved in address space conflicts with PCI devices,
    so I think it's worth logging the resources they use.  Otherwise we have to
    depend on lspnp or groping around in sysfs to find them.
    
    Signed-off-by: Bjorn Helgaas <bjorn.helgaas@hp.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/pnp/core.c
+++ drivers/pnp/core.c
@@ -194,8 +194,9 @@ int pnp_add_device(struct pnp_dev *dev)
 	for (id = dev->id; id; id = id->next)
 		len += scnprintf(buf + len, sizeof(buf) - len, " %s", id->id);
 
-	pnp_dbg(&dev->dev, "%s device, IDs%s (%s)\n",
-		dev->protocol->name, buf, dev->active ? "active" : "disabled");
+	dev_printk(KERN_DEBUG, &dev->dev, "%s device, IDs%s (%s)\n",
+		   dev->protocol->name, buf,
+		   dev->active ? "active" : "disabled");
 	return 0;
 }
 
