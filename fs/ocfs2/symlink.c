9921a24c5d59ca4e16364d9973b90a68e6642732
Joel Becker <joel.becker@oracle.com>
2010-09-29 17:33:05 -0700

    ocfs2: Don't walk off the end of fast symlinks.
    commit 1fc8a117865b54590acd773a55fbac9221b018f0 upstream.
    
    ocfs2 fast symlinks are NUL terminated strings stored inline in the
    inode data area.  However, disk corruption or a local attacker could, in
    theory, remove that NUL.  Because we're using strlen() (my fault,
    introduced in a731d1 when removing vfs_follow_link()), we could walk off
    the end of that string.
    
    Signed-off-by: Joel Becker <joel.becker@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- fs/ocfs2/symlink.c
+++ fs/ocfs2/symlink.c
@@ -128,7 +128,7 @@ static void *ocfs2_fast_follow_link(struct dentry *dentry,
 	}
 
 	/* Fast symlinks can't be large */
-	len = strlen(target);
+	len = strnlen(target, ocfs2_fast_symlink_chars(inode->i_sb));
 	link = kzalloc(len + 1, GFP_NOFS);
 	if (!link) {
 		status = -ENOMEM;
