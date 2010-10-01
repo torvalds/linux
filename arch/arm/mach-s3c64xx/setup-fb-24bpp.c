dab30d7f80d7a81ac736c590a81249d2fd323c62
Ben Dooks <ben-linux@fluff.org>
2010-10-01 16:39:15 +0900

    ARM: S3C64XX: Change to using s3c_gpio_cfgall_range()
    Change the code setting a range of GPIO pins' configuration and
    pull state to use the recently introduced s3c_gpio_cfgall_range().
    
    Mop up a few missed s3c_gpio_cfgpin_range() changes.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>
    [kgene.kim@samsung.com: Fix small comments]
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s3c64xx/setup-fb-24bpp.c
+++ arch/arm/mach-s3c64xx/setup-fb-24bpp.c
@@ -23,15 +23,9 @@
 
 extern void s3c64xx_fb_gpio_setup_24bpp(void)
 {
-	unsigned int gpio;
+	s3c_gpio_cfgall_range(S3C64XX_GPI(0), 16,
+			      S3C_GPIO_SFN(2), S3C_GPIO_PULL_NONE);
 
-	for (gpio = S3C64XX_GPI(0); gpio <= S3C64XX_GPI(15); gpio++) {
-		s3c_gpio_cfgpin(gpio, S3C_GPIO_SFN(2));
-		s3c_gpio_setpull(gpio, S3C_GPIO_PULL_NONE);
-	}
-
-	for (gpio = S3C64XX_GPJ(0); gpio <= S3C64XX_GPJ(11); gpio++) {
-		s3c_gpio_cfgpin(gpio, S3C_GPIO_SFN(2));
-		s3c_gpio_setpull(gpio, S3C_GPIO_PULL_NONE);
-	}
+	s3c_gpio_cfgall_range(S3C64XX_GPJ(0), 12,
+			      S3C_GPIO_SFN(2), S3C_GPIO_PULL_NONE);
 }
