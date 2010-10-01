e753a62156e952fd5a3c64f98454d9aeee3a2546
Christoph Hellwig <hch@lst.de>
2010-10-01 05:41:53 +0200

    hfsplus: remove BKL from hfsplus_put_super
    Except for ->put_super the BKL is now gone from HFS, which means it's
    superflous there too as ->put_super is serialized by the VFS.
    
    Signed-off-by: Christoph Hellwig <hch@tuxera.com>

--- fs/hfsplus/super.c
+++ fs/hfsplus/super.c
@@ -12,7 +12,6 @@
 #include <linux/pagemap.h>
 #include <linux/fs.h>
 #include <linux/slab.h>
-#include <linux/smp_lock.h>
 #include <linux/vfs.h>
 #include <linux/nls.h>
 
@@ -213,8 +212,6 @@ static void hfsplus_put_super(struct super_block *sb)
 	if (!sb->s_fs_info)
 		return;
 
-	lock_kernel();
-
 	if (sb->s_dirt)
 		hfsplus_write_super(sb);
 	if (!(sb->s_flags & MS_RDONLY) && HFSPLUS_SB(sb).s_vhdr) {
@@ -235,8 +232,6 @@ static void hfsplus_put_super(struct super_block *sb)
 	unload_nls(HFSPLUS_SB(sb).nls);
 	kfree(sb->s_fs_info);
 	sb->s_fs_info = NULL;
-
-	unlock_kernel();
 }
 
 static int hfsplus_statfs(struct dentry *dentry, struct kstatfs *buf)
