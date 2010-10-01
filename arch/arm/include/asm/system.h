c7b0aff44a0740eedd31b759fd08d9e25672fa76
Kevin Hilman <khilman@deeprootsystems.com>
2010-10-01 22:13:47 +0100

    ARM: 6428/1: add cpu_idle_wait() to support CPUidle on SMP systems.
    In order for CPUidle to work on SMP systems, an implementation of
    cpu_idle_wait() is needed.
    
    This patch duplicates the x86 implementation of cpu_idle_wait() for
    ARM.
    
    Tested-by: Colin Cross <ccross@android.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/system.h
+++ arch/arm/include/asm/system.h
@@ -325,6 +325,8 @@ static inline unsigned long __xchg(unsigned long x, volatile void *ptr, int size
 extern void disable_hlt(void);
 extern void enable_hlt(void);
 
+void cpu_idle_wait(void);
+
 #include <asm-generic/cmpxchg-local.h>
 
 #if __LINUX_ARM_ARCH__ < 6
