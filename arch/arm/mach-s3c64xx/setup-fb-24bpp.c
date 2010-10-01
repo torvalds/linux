3e9b7261502e78e351fc6a61a9b7241433c779c4
Kukjin Kim <kgene.kim@samsung.com>
2010-10-01 20:20:37 +0900

    ARM: S3C64XX: 2nd Change to using s3c_gpio_cfgrange_nopull()
    This patch changes code setting special-function and no pull-up
    to use the s3c_gpio_cfgrange_nopull() wrapper.
    NOTE: This is for missed things from the previous patch.
    
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s3c64xx/setup-fb-24bpp.c
+++ arch/arm/mach-s3c64xx/setup-fb-24bpp.c
@@ -23,9 +23,6 @@
 
 extern void s3c64xx_fb_gpio_setup_24bpp(void)
 {
-	s3c_gpio_cfgall_range(S3C64XX_GPI(0), 16,
-			      S3C_GPIO_SFN(2), S3C_GPIO_PULL_NONE);
-
-	s3c_gpio_cfgall_range(S3C64XX_GPJ(0), 12,
-			      S3C_GPIO_SFN(2), S3C_GPIO_PULL_NONE);
+	s3c_gpio_cfgrange_nopull(S3C64XX_GPI(0), 16, S3C_GPIO_SFN(2));
+	s3c_gpio_cfgrange_nopull(S3C64XX_GPJ(0), 12, S3C_GPIO_SFN(2));
 }
