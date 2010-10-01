8d3ba815d67c4ace1b27e007c371fea35bda5435
Paul Fox <pgf@laptop.org>
2010-10-01 18:17:19 +0100

    x86, olpc: Don't retry EC commands forever
    commit 286e5b97eb22baab9d9a41ca76c6b933a484252c upstream.
    
    Avoids a potential infinite loop.
    
    It was observed once, during an EC hacking/debugging
    session - not in regular operation.
    
    Signed-off-by: Daniel Drake <dsd@laptop.org>
    Cc: dilinger@queued.net
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- arch/x86/kernel/olpc.c
+++ arch/x86/kernel/olpc.c
@@ -114,6 +114,7 @@ int olpc_ec_cmd(unsigned char cmd, unsigned char *inbuf, size_t inlen,
 	unsigned long flags;
 	int ret = -EIO;
 	int i;
+	int restarts = 0;
 
 	spin_lock_irqsave(&ec_lock, flags);
 
@@ -169,7 +170,9 @@ restart:
 			if (wait_on_obf(0x6c, 1)) {
 				printk(KERN_ERR "olpc-ec:  timeout waiting for"
 						" EC to provide data!\n");
-				goto restart;
+				if (restarts++ < 10)
+					goto restart;
+				goto err;
 			}
 			outbuf[i] = inb(0x68);
 			pr_devel("olpc-ec:  received 0x%x\n", outbuf[i]);
