550591143eead286f2cd9d124a55d4a99ed024de
Paul Mundt <lethal@linux-sh.org>
2010-10-04 02:59:29 +0900

    sh: mach-x3proto: Support for baseboard GPIOs.
    This adds trivial support for the GPIOs implemented through the baseboard
    CPLD, used for driving the button matrix.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

---
+++ arch/sh/include/mach-x3proto/mach/hardware.h
@@ -0,0 +1,12 @@
+#ifndef __MACH_X3PROTO_HARDWARE_H
+#define __MACH_X3PROTO_HARDWARE_H
+
+struct gpio_chip;
+
+/* arch/sh/boards/mach-x3proto/gpio.c */
+int x3proto_gpio_setup(void);
+extern struct gpio_chip x3proto_gpio_chip;
+
+#define NR_BASEBOARD_GPIOS	16
+
+#endif /* __MACH_X3PROTO_HARDWARE_H */
