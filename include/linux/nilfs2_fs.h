c486f3895d6dc751f7c0f04f0fa67390ce4d168e
Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>
2010-10-03 17:44:03 +0900

    nilfs2: change license of exported header file
    This allows other projects to carry copies of the header file related
    to ABI and disk format (i.e. "nilfs2_fs.h") without it or distributors
    having to worry about effects on the project's overall license terms.
    It's also desired for switching the license of nilfs library to LGPL.
    
    Jiro SEKIBA pointed out these license issues (Message-ID:
    <87tylo7msw.wl%jir@sekiba.com>), and he suggested switching license of
    the library and nilfs2_fs.h to GNU Lesser General Public License.  We
    take in his suggestion to avoid the license issues.
    
    Signed-off-by: Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>
    Cc: Jiro SEKIBA <jir@unicus.jp>
    Cc: linux-nilfs <linux-nilfs@vger.kernel.org>

--- include/linux/nilfs2_fs.h
+++ include/linux/nilfs2_fs.h
@@ -4,16 +4,16 @@
  * Copyright (C) 2005-2008 Nippon Telegraph and Telephone Corporation.
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
