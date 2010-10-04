ca8e7c6ccdfb9a77de9b9719e6ef768373fb607c
Ben Skeggs <bskeggs@redhat.com>
2010-10-04 15:27:58 +1000

    drm/nouveau: parse voltage from perf 0x40 entires
    This was disabled previously because of some uncertainty that +2 was
    indeed the voltage.  It appears it is, checked on a NVA8 and a NVA3M.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nouveau_perf.c
+++ drivers/gpu/drm/nouveau/nouveau_perf.c
@@ -173,7 +173,7 @@ nouveau_perf_init(struct drm_device *dev)
 		case 0x40:
 #define subent(n) entry[perf[2] + ((n) * perf[3])]
 			perflvl->fanspeed = 0; /*XXX*/
-			perflvl->voltage = 0; /*XXX: entry[2] */;
+			perflvl->voltage = entry[2];
 			perflvl->core = (ROM16(subent(0)) & 0xfff) * 1000;
 			perflvl->shader = (ROM16(subent(1)) & 0xfff) * 1000;
 			perflvl->memory = (ROM16(subent(2)) & 0xfff) * 1000;
