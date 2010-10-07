95fa148bfbc413b6a49851f5cf5033c5acb89f18
Dmitry Shmidt <dimitrysh@google.com>
2010-10-07 14:39:16 -0700

    mmc: Add "ignore mmc pm notify" functionality
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -533,7 +533,8 @@ int mmc_add_host(struct mmc_host *host)
 	mmc_host_clk_sysfs_init(host);
 
 	mmc_start_host(host);
-	register_pm_notifier(&host->pm_notify);
+	if (!(host->pm_flags & MMC_PM_IGNORE_PM_NOTIFY))
+		register_pm_notifier(&host->pm_notify);
 
 	return 0;
 }
@@ -550,7 +551,9 @@ EXPORT_SYMBOL(mmc_add_host);
  */
 void mmc_remove_host(struct mmc_host *host)
 {
-	unregister_pm_notifier(&host->pm_notify);
+	if (!(host->pm_flags & MMC_PM_IGNORE_PM_NOTIFY))
+		unregister_pm_notifier(&host->pm_notify);
+
 	mmc_stop_host(host);
 
 #ifdef CONFIG_DEBUG_FS
