7e2004a986d4e3d876aceb60dac4f85f5648ee72
Dmitry Shmidt <dimitrysh@google.com>
2010-10-07 14:39:16 -0700

    ANDROID: mmc: core: Add "ignore mmc pm notify" functionality
    Used to prevent remounting MMC on suspend/resume.
    
    Bug: 120441127
    Change-Id: I20821a82831b07ca037973d5d92e832372c6b583
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -441,7 +441,8 @@ int mmc_add_host(struct mmc_host *host)
 #endif
 
 	mmc_start_host(host);
-	mmc_register_pm_notifier(host);
+	if (!(host->pm_flags & MMC_PM_IGNORE_PM_NOTIFY))
+		mmc_register_pm_notifier(host);
 
 	return 0;
 }
@@ -458,7 +459,8 @@ EXPORT_SYMBOL(mmc_add_host);
  */
 void mmc_remove_host(struct mmc_host *host)
 {
-	mmc_unregister_pm_notifier(host);
+	if (!(host->pm_flags & MMC_PM_IGNORE_PM_NOTIFY))
+		mmc_unregister_pm_notifier(host);
 	mmc_stop_host(host);
 
 #ifdef CONFIG_DEBUG_FS
