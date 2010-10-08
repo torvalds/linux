e844b1da87270d96aef3fc79763ecc2c6541a71b
Enric Balletbo i Serra <eballetbo@gmail.com>
2010-10-08 10:22:11 -0700

    omap3: Add minimal OMAP3 IGEP module support
    The OMAP3 IGEP module is a low-power, high performance production-ready
    system-on-module (SOM) based on TI's OMAP3 family. More about this
    board at www.igep.es.
    
    Signed-off-by: Enric Balletbo i Serra <eballetbo@gmail.com>
    [tony@atomide.com: updated for the mmc changes and to be selected by default]
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/plat/uncompress.h
+++ arch/arm/plat-omap/include/plat/uncompress.h
@@ -146,6 +146,7 @@ static inline void __arch_decomp_setup(unsigned long arch_id)
 		DEBUG_LL_OMAP3(3, cm_t35);
 		DEBUG_LL_OMAP3(3, cm_t3517);
 		DEBUG_LL_OMAP3(3, igep0020);
+		DEBUG_LL_OMAP3(3, igep0030);
 		DEBUG_LL_OMAP3(3, nokia_rx51);
 		DEBUG_LL_OMAP3(3, omap3517evm);
 		DEBUG_LL_OMAP3(3, omap3_beagle);
