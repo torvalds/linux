3ed02ada2a5e695e2fbb5e4a0008cfcb0f50feaa
John Johansen <john.johansen@canonical.com>
2010-10-09 00:47:53 -0700

    AppArmor: Ensure the size of the copy is < the buffer allocated to hold it
    Actually I think in this case the appropriate thing to do is to BUG as there
    is currently a case (remove) where the alloc_size needs to be larger than
    the copy_size, and if copy_size is ever greater than alloc_size there is
    a mistake in the caller code.
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Kees Cook <kees.cook@canonical.com>
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/apparmor/apparmorfs.c
+++ security/apparmor/apparmorfs.c
@@ -29,7 +29,7 @@
  * aa_simple_write_to_buffer - common routine for getting policy from user
  * @op: operation doing the user buffer copy
  * @userbuf: user buffer to copy data from  (NOT NULL)
- * @alloc_size: size of user buffer
+ * @alloc_size: size of user buffer (REQUIRES: @alloc_size >= @copy_size)
  * @copy_size: size of data to copy from user buffer
  * @pos: position write is at in the file (NOT NULL)
  *
@@ -41,6 +41,8 @@ static char *aa_simple_write_to_buffer(int op, const char __user *userbuf,
 				       loff_t *pos)
 {
 	char *data;
+
+	BUG_ON(copy_size > alloc_size);
 
 	if (*pos != 0)
 		/* only writes from pos 0, that is complete writes */
