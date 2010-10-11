a3d34e6ac362dafdb7aa0c3438b105c7822cdcd1
Antti Palosaari <crope@iki.fi>
2010-10-11 07:36:54 -0300

    [media] TrekStor DVB-T USB Stick remote controller
    Imported from af9015.h.
    Initial keytable was from Marc Schneider <macke@macke.org>
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/rc-map.h
+++ include/media/rc-map.h
@@ -125,6 +125,7 @@ void rc_map_init(void);
 #define RC_MAP_TERRATEC_CINERGY_XS       "rc-terratec-cinergy-xs"
 #define RC_MAP_TERRATEC_SLIM             "rc-terratec-slim"
 #define RC_MAP_TEVII_NEC                 "rc-tevii-nec"
+#define RC_MAP_TREKSTOR                  "rc-trekstor"
 #define RC_MAP_TT_1500                   "rc-tt-1500"
 #define RC_MAP_VIDEOMATE_S350            "rc-videomate-s350"
 #define RC_MAP_VIDEOMATE_TV_PVR          "rc-videomate-tv-pvr"
