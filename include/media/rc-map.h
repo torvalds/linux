ff533801bd9fa91fc61053900fd40b10132427c0
Antti Palosaari <crope@iki.fi>
2010-10-11 08:24:00 -0300

    [media] Digittrade DVB-T USB Stick remote controller keytable
    Digittrade DVB-T USB Stick remote controller.
    Imported from af9015.h. Initial keytable was from Alain Kalker <miki@dds.nl>
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/rc-map.h
+++ include/media/rc-map.h
@@ -72,6 +72,7 @@ void rc_map_init(void);
 #define RC_MAP_CINERGY                   "rc-cinergy"
 #define RC_MAP_DIB0700_NEC_TABLE         "rc-dib0700-nec"
 #define RC_MAP_DIB0700_RC5_TABLE         "rc-dib0700-rc5"
+#define RC_MAP_DIGITTRADE                "rc-digittrade"
 #define RC_MAP_DM1105_NEC                "rc-dm1105-nec"
 #define RC_MAP_DNTV_LIVE_DVBT_PRO        "rc-dntv-live-dvbt-pro"
 #define RC_MAP_DNTV_LIVE_DVB_T           "rc-dntv-live-dvb-t"
