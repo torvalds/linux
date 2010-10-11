6e144f28e583ad5ff907fed9a55ee1955e4b0cd7
Todd Poynor <toddpoynor@google.com>
2010-10-11 16:51:28 -0700

    sdhci: Always pass clock request value zero to set_clock host op
    To allow the set_clock host op to disable the SDCLK source when not
    needed, always call the host op when the requested clock speed is
    zero.  Do this even if host->clock already equals zero, because
    the SDHCI driver may set that value (without calling the host op)
    to force an update at the next (non-zero-speed) call.
    
    Change-Id: If99230d76138679b5767f77cb925f15408ae518e
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1044,7 +1044,7 @@ static void sdhci_set_clock(struct sdhci_host *host, unsigned int clock)
 	u16 clk = 0;
 	unsigned long timeout;
 
-	if (clock == host->clock)
+	if (clock && clock == host->clock)
 		return;
 
 	if (host->ops->set_clock) {
