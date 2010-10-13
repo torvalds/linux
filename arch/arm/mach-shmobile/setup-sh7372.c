934e40781aa92dae0b9d753d7de4a743c96b9bd2
Magnus Damm <damm@opensource.se>
2010-10-13 07:22:11 +0000

    ARM: shmobile: break out sh7372 platform devices
    This patch introduces sh7372_late_devices which
    should include all regular processor specific
    platform devices.
    
    Early platform devices for console and timers
    should be put on the sh7372_early_devices list,
    all other devices should be on sh7372_late_devices.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/arm/mach-shmobile/setup-sh7372.c
+++ arch/arm/mach-shmobile/setup-sh7372.c
@@ -526,6 +526,9 @@ static struct platform_device *sh7372_early_devices[] __initdata = {
 	&scif5_device,
 	&scif6_device,
 	&cmt10_device,
+};
+
+static struct platform_device *sh7372_late_devices[] __initdata = {
 	&iic0_device,
 	&iic1_device,
 	&dma0_device,
@@ -537,6 +540,9 @@ void __init sh7372_add_standard_devices(void)
 {
 	platform_add_devices(sh7372_early_devices,
 			    ARRAY_SIZE(sh7372_early_devices));
+
+	platform_add_devices(sh7372_late_devices,
+			    ARRAY_SIZE(sh7372_late_devices));
 }
 
 void __init sh7372_add_early_devices(void)
