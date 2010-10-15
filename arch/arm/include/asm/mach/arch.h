d71e3eb5894dbb626dc7e28cfd4f33ebee9a739a
Nicolas Pitre <nicolas.pitre@linaro.org>
2010-10-14 22:37:52 -0400

    ARM: make struct machine_desc definition coherent with its comment
    As mentioned in the comment right at the top, the first four fields
    are directly accessed by assembly code in head.S.  Move nr_irqs so the
    comment is true again.
    
    Signed-off-by: Nicolas Pitre <nicolas.pitre@linaro.org>

--- arch/arm/include/asm/mach/arch.h
+++ arch/arm/include/asm/mach/arch.h
@@ -20,13 +20,14 @@ struct machine_desc {
 	 * by assembler code in head.S, head-common.S
 	 */
 	unsigned int		nr;		/* architecture number	*/
-	unsigned int		nr_irqs;	/* number of IRQs */
 	unsigned int		phys_io;	/* start of physical io	*/
 	unsigned int		io_pg_offst;	/* byte offset for io 
 						 * page tabe entry	*/
 
 	const char		*name;		/* architecture name	*/
 	unsigned long		boot_params;	/* tagged list		*/
+
+	unsigned int		nr_irqs;	/* number of IRQs */
 
 	unsigned int		video_start;	/* start of video RAM	*/
 	unsigned int		video_end;	/* end of video RAM	*/
