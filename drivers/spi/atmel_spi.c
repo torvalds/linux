ee2007d299ad4020115b193858817e6c57e95db5
Matthias Brugger <matthias.bgg@googlemail.com>
2010-10-16 01:39:49 +0200

    spi/atmel: typo in debug message
    This patches a typo in the debug message.
    
    Signed-off-by: Matthias Brugger <mensch0815@gmail.com>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- drivers/spi/atmel_spi.c
+++ drivers/spi/atmel_spi.c
@@ -679,7 +679,7 @@ static int atmel_spi_transfer(struct spi_device *spi, struct spi_message *msg)
 			bits = (asd->csr >> 4) & 0xf;
 			if (bits != xfer->bits_per_word - 8) {
 				dev_dbg(&spi->dev, "you can't yet change "
-					 "bit_per_word in transfers\n");
+					 "bits_per_word in transfers\n");
 				return -ENOPROTOOPT;
 			}
 		}
