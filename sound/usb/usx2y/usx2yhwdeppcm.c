fa2eb005ebcbac89745a1f9a9f0c8678ba63f61a
Andrea Gelmini <andrea.gelmini@gelma.net>
2010-10-16 15:19:20 +0200

    sound: fixed typos
    Signed-off-by: Andrea Gelmini <andrea.gelmini@gelma.net>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/usb/usx2y/usx2yhwdeppcm.c
+++ sound/usb/usx2y/usx2yhwdeppcm.c
@@ -36,9 +36,9 @@
          plain usx2y alsa mode is able to achieve 64frames, 4periods, but only at the
          cost of easier triggered i.e. aeolus xruns (128 or 256frames,
          2periods works but is useless cause of crackling).
- 
+
  This is a first "proof of concept" implementation.
- Later, funcionalities should migrate to more apropriate places:
+ Later, functionalities should migrate to more apropriate places:
  Userland:
  - The jackd could mmap its float-pcm buffers directly from alsa-lib.
  - alsa-lib could provide power of 2 period sized shaping combined with int/float
