f444fce3c0aa199facc8c2be7635a4a82b84b71e
Linus Walleij <linus.walleij@stericsson.com>
2010-10-18 09:03:03 +0100

    ARM: 6445/1: fixup TCM memory types
    After Santosh's fixup of the generic MT_MEMORY and
    MT_MEMORY_NONCACHED I add this fix to the TCM memory types.
    The main change is that the ITCM memory is L_PTE_WRITE and
    DOMAIN_KERNEL which works just fine. The changed to the DTCM
    is just cosmetic to fit with surrounding code.
    
    Cc: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Cc: Rickard Andersson <rickard.andersson@stericsson.com>
    Signed-off-by: Linus Walleij <linus.walleij@stericsson.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -265,17 +265,17 @@ static struct mem_type mem_types[] = {
 		.domain    = DOMAIN_KERNEL,
 	},
 	[MT_MEMORY_DTCM] = {
-		.prot_pte	= L_PTE_PRESENT | L_PTE_YOUNG |
-		                  L_PTE_DIRTY | L_PTE_WRITE,
-		.prot_l1	= PMD_TYPE_TABLE,
-		.prot_sect	= PMD_TYPE_SECT | PMD_SECT_XN,
-		.domain		= DOMAIN_KERNEL,
+		.prot_pte  = L_PTE_PRESENT | L_PTE_YOUNG | L_PTE_DIRTY |
+				L_PTE_WRITE,
+		.prot_l1   = PMD_TYPE_TABLE,
+		.prot_sect = PMD_TYPE_SECT | PMD_SECT_XN,
+		.domain    = DOMAIN_KERNEL,
 	},
 	[MT_MEMORY_ITCM] = {
 		.prot_pte  = L_PTE_PRESENT | L_PTE_YOUNG | L_PTE_DIRTY |
-				L_PTE_USER | L_PTE_EXEC,
+				L_PTE_WRITE | L_PTE_EXEC,
 		.prot_l1   = PMD_TYPE_TABLE,
-		.domain    = DOMAIN_IO,
+		.domain    = DOMAIN_KERNEL,
 	},
 };
 
