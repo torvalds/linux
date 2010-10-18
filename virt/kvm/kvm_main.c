2a31339aa014c0d0b97c57d3ebc997732f8f47fc
Jan Kiszka <jan.kiszka@siemens.com>
2010-10-18 15:38:40 +0200

    KVM: Drop CONFIG_DMAR dependency around kvm_iommu_map_pages
    We also have to call kvm_iommu_map_pages for CONFIG_AMD_IOMMU. So drop
    the dependency on Intel IOMMU, kvm_iommu_map_pages will be a nop anyway
    if CONFIG_IOMMU_API is not defined.
    
    KVM-Stable-Tag.
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -705,14 +705,12 @@ skip_lpage:
 	if (r)
 		goto out_free;
 
-#ifdef CONFIG_DMAR
 	/* map the pages in iommu page table */
 	if (npages) {
 		r = kvm_iommu_map_pages(kvm, &new);
 		if (r)
 			goto out_free;
 	}
-#endif
 
 	r = -ENOMEM;
 	slots = kzalloc(sizeof(struct kvm_memslots), GFP_KERNEL);
