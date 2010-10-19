dc7ed6438a618f99357f906862dafbe9db9794d1
Dmitry Shmidt <dimitrysh@google.com>
2010-10-19 16:06:36 -0700

    mmc: sdhci: Enable MMC_CAP_ERASE
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1871,6 +1871,8 @@ int sdhci_add_host(struct sdhci_host *host)
 	if (host->quirks & SDHCI_QUIRK_BROKEN_CARD_DETECTION)
 		mmc->caps |= MMC_CAP_NEEDS_POLL;
 
+	mmc->caps |= MMC_CAP_ERASE;
+
 	mmc->ocr_avail = 0;
 	if (caps & SDHCI_CAN_VDD_330)
 		mmc->ocr_avail |= MMC_VDD_32_33|MMC_VDD_33_34;
