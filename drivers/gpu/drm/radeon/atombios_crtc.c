5480f727dc4c049eb46b191bfaeb034067aa6835
Dave Airlie <airlied@redhat.com>
2010-10-19 10:36:47 +1000

    Revert "drm/radeon/kms: remove some pll algo flags"
    This reverts commit f28488c282d8916b9b6190cc41714815bbaf97d5.
    
    On my rv610 test machine the monitor failed to light up after this.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/atombios_crtc.c
+++ drivers/gpu/drm/radeon/atombios_crtc.c
@@ -501,8 +501,20 @@ static u32 atombios_adjust_pll(struct drm_crtc *crtc,
 		    (rdev->family == CHIP_RS740))
 			pll->flags |= (/*RADEON_PLL_USE_FRAC_FB_DIV |*/
 				       RADEON_PLL_PREFER_CLOSEST_LOWER);
-	} else
+
+		if (ASIC_IS_DCE32(rdev) && mode->clock > 200000)	/* range limits??? */
+			pll->flags |= RADEON_PLL_PREFER_HIGH_FB_DIV;
+		else
+			pll->flags |= RADEON_PLL_PREFER_LOW_REF_DIV;
+	} else {
 		pll->flags |= RADEON_PLL_LEGACY;
+
+		if (mode->clock > 200000)	/* range limits??? */
+			pll->flags |= RADEON_PLL_PREFER_HIGH_FB_DIV;
+		else
+			pll->flags |= RADEON_PLL_PREFER_LOW_REF_DIV;
+
+	}
 
 	list_for_each_entry(encoder, &dev->mode_config.encoder_list, head) {
 		if (encoder->crtc == crtc) {
