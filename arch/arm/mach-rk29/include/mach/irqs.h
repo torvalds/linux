e2f0909ced4d740a0c1402439df13847e2789254
lhh <lhh@rock-chips.com>
2010-10-20 17:31:21 +0800

    add rk29 phys

--- arch/arm/mach-rk29/include/mach/irqs.h
+++ arch/arm/mach-rk29/include/mach/irqs.h
@@ -17,10 +17,76 @@
 #ifndef __ARCH_ARM_MACH_RK29_IRQS_H
 #define __ARCH_ARM_MACH_RK29_IRQS_H
 
-#define	NR_IRQS		64  
-#define IRQ_NR_TIMER0                15
-#define IRQ_NR_TIMER1                16
-#define IRQ_NR_TIMER2                17
-#define IRQ_NR_TIMER3                18
+#define	NR_IRQS		68  
+/*irq number*/
+#define IRQ_NR_DMAC00				0
+#define IRQ_NR_DMAC01				1
+#define IRQ_NR_DMAC000				2
+#define IRQ_NR_DMAC010				3
+#define IRQ_NR_DMAC20				4
+#define IRQ_NR_DMAC21				5
+#define IRQ_NR_DMAC22				6
+#define IRQ_NR_DMAC23				7
+#define IRQ_NR_DMAC24				8
+#define IRQ_NR_GPU					9
+#define IRQ_NR_VEPU					10
+#define IRQ_NR_VDPU					11
+#define IRQ_NR_VIP					12
+#define IRQ_NR_LCDC					13
+#define IRQ_NR_IPP					14
+#define IRQ_NR_EBC					15
+#define IRQ_NR_USBOTG0				16
+#define IRQ_NR_USBOTG1				17
+#define IRQ_NR_USBHOST				18
+#define IRQ_NR_MAC					19
+#define IRQ_NR_HIF0					20
+#define IRQ_NR_HIF1					21
+#define IRQ_NR_HSADC_TS1			22
+#define IRQ_NR_SDMMC				23
+#define IRQ_NR_SDIO					24
+#define IRQ_NR_EMMC					25
+#define IRQ_NR_SARADC				26
+#define IRQ_NR_NANDC				27
+#define IRQ_NR_NANDCRDY				28
+#define IRQ_NR_SMC					29
+#define IRQ_NR_PID_FILTER			30
+#define IRQ_NR_I2SPCM8				31
+#define IRQ_NR_I2SPCM2				32
+#define IRQ_NR_SPDIF				33
+#define IRQ_NR_UART0				34
+#define IRQ_NR_UART1				35
+#define IRQ_NR_UART2				36
+#define IRQ_NR_UART3				37
+#define IRQ_NR_SPI0					38
+#define IRQ_NR_SPI1					39
+#define IRQ_NR_I2C0					40
+#define IRQ_NR_I2C1					41
+#define IRQ_NR_I2C2					42
+#define IRQ_NR_I2C3					43
+#define IRQ_NR_TIMER0				44
+#define IRQ_NR_TIMER1				45
+#define IRQ_NR_TIMER2				46
+#define IRQ_NR_TIMER3				47
+#define IRQ_NR_PWM0					48
+#define IRQ_NR_PWM1					49
+#define IRQ_NR_PWM2					50
+#define IRQ_NR_PWM3					51
+#define IRQ_NR_WDT					52
+#define IRQ_NR_RTC					53
+#define IRQ_NR_PMU					54
+#define IRQ_NR_GPIO0				55
+#define IRQ_NR_GPIO1				56
+#define IRQ_NR_GPIO2				57
+#define IRQ_NR_GPIO3				58
+#define IRQ_NR_GPIO4				59
+#define IRQ_NR_GPIO5				60
+#define IRQ_NR_GPIO6				61
+#define IRQ_NR_USB_AHB_ARB			62
+#define IRQ_NR_PERI_AHB_ARB			63
+#define IRQ_NR_A8IRQ0				64
+#define IRQ_NR_A8IRQ1				65
+#define IRQ_NR_A8IRQ2				66
+#define IRQ_NR_A8IRQ3				67
+
 
 #endif
