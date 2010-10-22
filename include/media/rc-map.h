64fb58092e025235dc7fdcebeffee1516c79464d
Antti Palosaari <crope@iki.fi>
2010-10-22 20:23:31 -0300

    [media] DigitalNow TinyTwin remote controller
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Cc: Renura Enterprises Pty Ltd <renura@digitalnow.com.au>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/rc-map.h
+++ include/media/rc-map.h
@@ -76,6 +76,7 @@ void rc_map_init(void);
 #define RC_MAP_CINERGY                   "rc-cinergy"
 #define RC_MAP_DIB0700_NEC_TABLE         "rc-dib0700-nec"
 #define RC_MAP_DIB0700_RC5_TABLE         "rc-dib0700-rc5"
+#define RC_MAP_DIGITALNOW_TINYTWIN       "rc-digitalnow-tinytwin"
 #define RC_MAP_DIGITTRADE                "rc-digittrade"
 #define RC_MAP_DM1105_NEC                "rc-dm1105-nec"
 #define RC_MAP_DNTV_LIVE_DVBT_PRO        "rc-dntv-live-dvbt-pro"
