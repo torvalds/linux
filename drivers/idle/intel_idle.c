c45649ade8780f796eaf23f1cf94ea2253f66d93
Len Brown <len.brown@intel.com>
2010-10-23 23:25:53 -0400

    intel_idle: do not use the LAPIC timer for ATOM C2
    commit c25d29952b2a8c9aaf00e081c9162a0e383030cd upstream.
    
    If we use the LAPIC timer during ATOM C2 on
    some nvidia chisets, the system stalls.
    
    https://bugzilla.kernel.org/show_bug.cgi?id=21032
    
    Signed-off-by: Len Brown <len.brown@intel.com>
    Cc: Tom Gundersen <teg@jklm.no>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/idle/intel_idle.c
+++ drivers/idle/intel_idle.c
@@ -276,7 +276,7 @@ static int intel_idle_probe(void)
 
 	case 0x1C:	/* 28 - Atom Processor */
 	case 0x26:	/* 38 - Lincroft Atom Processor */
-		lapic_timer_reliable_states = (1 << 2) | (1 << 1); /* C2, C1 */
+		lapic_timer_reliable_states = (1 << 1); /* C1 */
 		cpuidle_state_table = atom_cstates;
 		break;
 #ifdef FUTURE_USE
