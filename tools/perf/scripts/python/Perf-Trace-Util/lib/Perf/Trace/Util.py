6cc7361440e499abb3a30cdbcfedad03e43c92ae
Arnaldo Carvalho de Melo <acme@redhat.com>
2010-10-25 15:15:10 -0200

    perf python scripting: Improve the failed-syscalls-by-pid script
    . Print message at script start telling how to get te summary
    . Print the syscall name using the audit-lib-python package, if
      installed
    . Print the errno string
    . Accept both pid (if numeric) or COMM name
    
    Now it looks like this:
    
    [root@emilia ~]# perf trace failed-syscalls-by-pid
    Press control+C to stop and show the summary
    ^C
    syscall errors:
    
    comm [pid]                           count
    ------------------------------  ----------
    
    automount [1670]
      syscall: futex
        err = ETIMEDOUT                     39
    
    irqbalance [1462]
      syscall: openat
        err = ENOENT                         4
    
    perf [7888]
      syscall: lseek
        err = ESPIPE                         1
      syscall: open
        err = ENOENT                        24
    
    perf [7889]
      syscall: ioctl
        err = EINVAL                         1
      syscall: readlink
        err = EINVAL                         2
      syscall: open
        err = ENOENT                       389
      syscall: stat
        err = ENOENT                       141
      syscall: lseek
        err = ESPIPE                         3
    [root@emilia ~]#
    
    [root@emilia ~]# perf trace failed-syscalls-by-pid 1670
    Press control+C to stop and show the summary
    ^C
    syscall errors:
    
    comm [pid]                           count
    ------------------------------  ----------
    
    automount [1670]
      syscall: futex
        err = ETIMEDOUT                      2
    [root@emilia ~]#
    [root@emilia ~]#
    [root@emilia ~]#
    [root@emilia ~]# perf trace failed-syscalls-by-pid automount
    Press control+C to stop and show the summary
    ^C
    syscall errors for automount:
    
    comm [pid]                           count
    ------------------------------  ----------
    
    automount [1669]
      syscall: futex
        err = ETIMEDOUT                      1
    
    automount [1670]
      syscall: futex
        err = ETIMEDOUT                      5
    [root@emilia ~]#
    
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Tom Zanussi <tzanussi@gmail.com>
    LKML-Reference: <new-submission>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/scripts/python/Perf-Trace-Util/lib/Perf/Trace/Util.py
+++ tools/perf/scripts/python/Perf-Trace-Util/lib/Perf/Trace/Util.py
@@ -6,6 +6,8 @@
 # Public License ("GPL") version 2 as published by the Free Software
 # Foundation.
 
+import errno, os
+
 NSECS_PER_SEC    = 1000000000
 
 def avg(total, n):
@@ -26,3 +28,38 @@ def nsecs_str(nsecs):
 
 def clear_term():
     print("\x1b[H\x1b[2J")
+
+audit_package_warned = False
+
+try:
+	import audit
+	machine_to_id = {
+		'x86_64': audit.MACH_86_64,
+		'alpha'	: audit.MACH_ALPHA,
+		'armeb'	: audit.MACH_ARMEB,
+		'ia64'	: audit.MACH_IA64,
+		'ppc'	: audit.MACH_PPC,
+		'ppc64'	: audit.MACH_PPC64,
+		's390'	: audit.MACH_S390,
+		's390x'	: audit.MACH_S390X,
+		'i386'	: audit.MACH_X86,
+		'i586'	: audit.MACH_X86,
+		'i686'	: audit.MACH_X86,
+	}
+	machine_id = machine_to_id[os.uname()[4]]
+except:
+	if not audit_package_warned:
+		audit_package_warned = True
+		print "Install the audit-libs-python package to get syscall names"
+
+def syscall_name(id):
+	try:
+		return audit.audit_syscall_to_name(id, machine_id)
+	except:
+		return str(id)
+
+def strerror(nr):
+	try:
+		return errno.errorcode[abs(nr)]
+	except:
+		return "Unknown %d errno" % nr
