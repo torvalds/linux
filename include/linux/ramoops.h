c3b92ce9e75f6353104fc7f8e32fb9fdb2550ad0
Kyungmin Park <kyungmin.park@samsung.com>
2010-10-27 15:34:52 -0700

    ramoops: use the platform data structure instead of module params
    As each board and system has different memory for ramoops.  It's better to
    define the platform data instead of module params.
    
    [akpm@linux-foundation.org: fix ramoops_remove() return type]
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Cc: Marco Stornelli <marco.stornelli@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

---
+++ include/linux/ramoops.h
@@ -0,0 +1,15 @@
+#ifndef __RAMOOPS_H
+#define __RAMOOPS_H
+
+/*
+ * Ramoops platform data
+ * @mem_size	memory size for ramoops
+ * @mem_address	physical memory address to contain ramoops
+ */
+
+struct ramoops_platform_data {
+	unsigned long	mem_size;
+	unsigned long	mem_address;
+};
+
+#endif
