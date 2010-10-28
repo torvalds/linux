5af634789c93b97cfb314a102436716be8fbc577
Greg Kroah-Hartman <gregkh@suse.de>
2010-10-28 09:55:53 -0700

    Staging: brcm80211: fix usage of roundup in structures
    Now that the roundup macro is sane, it can't be used in structure
    definitions, or the build breaks..  For now, create a "broken_roundup()"
    macro to get everything building again, but in the end, fixing these
    structures to use a proper size value is the correct thing to do.
    
    Cc: Brett Rudley <brudley@broadcom.com>
    Cc: Henry Ptasinski <henryp@broadcom.com>
    Cc: Nohee Ko <noheek@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/sys/wlc_event.c
+++ drivers/staging/brcm80211/sys/wlc_event.c
@@ -47,7 +47,7 @@ struct wlc_eventq {
 	bool workpending;
 	struct wl_timer *timer;
 	wlc_eventq_cb_t cb;
-	u8 event_inds_mask[roundup(WLC_E_LAST, NBBY) / NBBY];
+	u8 event_inds_mask[broken_roundup(WLC_E_LAST, NBBY) / NBBY];
 };
 
 /*
