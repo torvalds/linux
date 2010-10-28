705059a670f3af2b37695e82de0ee58e75e656ed
Larry Finger <Larry.Finger@lwfinger.net>
2010-10-28 08:47:13 -0500

    staging: rt2870: Add new USB ID for Belkin F6D4050 v1
    Add new USB ID for FT2870 for Belkin F6D4050 v1
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Reported- and Tested-by: James Long <crogonint@yahoo.com>
    Cc: Stable <stable@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rt2860/usb_main_dev.c
+++ drivers/staging/rt2860/usb_main_dev.c
@@ -65,6 +65,7 @@ struct usb_device_id rtusb_usb_id[] = {
 	{USB_DEVICE(0x14B2, 0x3C07)},	/* AL */
 	{USB_DEVICE(0x050D, 0x8053)},	/* Belkin */
 	{USB_DEVICE(0x050D, 0x825B)},	/* Belkin */
+	{USB_DEVICE(0x050D, 0x935A)},	/* Belkin F6D4050 v1 */
 	{USB_DEVICE(0x050D, 0x935B)},	/* Belkin F6D4050 v2 */
 	{USB_DEVICE(0x14B2, 0x3C23)},	/* Airlink */
 	{USB_DEVICE(0x14B2, 0x3C27)},	/* Airlink */
