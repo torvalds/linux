6bff7eccb0d9bdef4123aad5399e73cbc26683a6
Lino Sanfilippo <LinoSanfilippo@gmx.de>
2010-10-29 12:02:17 +0200

    Ensure FMODE_NONOTIFY is not set by userspace
        In fsnotify_open() ensure that FMODE_NONOTIFY is never set by userspace.
        Also always call fsnotify_parent and fsnotify.
    
    Signed-off-by: Lino Sanfilippo <LinoSanfilippo@gmx.de>
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- include/linux/fsnotify.h
+++ include/linux/fsnotify.h
@@ -235,10 +235,11 @@ static inline void fsnotify_open(struct file *file)
 	if (S_ISDIR(inode->i_mode))
 		mask |= FS_ISDIR;
 
-	if (!(file->f_mode & FMODE_NONOTIFY)) {
-		fsnotify_parent(path, NULL, mask);
-		fsnotify(inode, mask, path, FSNOTIFY_EVENT_PATH, NULL, 0);
-	}
+	/* FMODE_NONOTIFY must never be set from user */
+	file->f_mode &= ~FMODE_NONOTIFY;
+
+	fsnotify_parent(path, NULL, mask);
+	fsnotify(inode, mask, path, FSNOTIFY_EVENT_PATH, NULL, 0);
 }
 
 /*
