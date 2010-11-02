897ef192514a6b0fc10a0ce3fe7e7aa0de09bc52
Zhenyu Wang <zhenyuw@linux.intel.com>
2010-11-02 17:30:47 +0800

    agp/intel: restore cache behavior on sandybridge
    This restores cache behavior for default AGP_USER_MEMORY as
    uncached, and leave default AGP_USER_CACHED_MEMORY as LLC only.
    I've seen different cache behavior on one sandybridge desktop CPU vs.
    another mobile CPU. Until we figure out how to detect the real cache
    config, restore back to the original behavior now.
    
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/char/agp/intel-gtt.c
+++ drivers/char/agp/intel-gtt.c
@@ -1288,7 +1288,7 @@ static void gen6_write_entry(dma_addr_t addr, unsigned int entry,
 	unsigned int gfdt = flags & AGP_USER_CACHED_MEMORY_GFDT;
 	u32 pte_flags;
 
-	if (type_mask == AGP_USER_UNCACHED_MEMORY)
+	if (type_mask == AGP_USER_MEMORY)
 		pte_flags = GEN6_PTE_UNCACHED | I810_PTE_VALID;
 	else if (type_mask == AGP_USER_CACHED_MEMORY_LLC_MLC) {
 		pte_flags = GEN6_PTE_LLC_MLC | I810_PTE_VALID;
