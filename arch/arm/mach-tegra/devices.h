62224124202314c340d47ee950adc8c16fbd2332
Dima Zavin <dima@android.com>
2010-11-03 19:08:48 -0700

    ARM: tegra: add missing spdif device declaration in header
    Change-Id: If447711b095ec7025f3bbe232e22444839054927
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/mach-tegra/devices.h
+++ arch/arm/mach-tegra/devices.h
@@ -57,5 +57,6 @@ extern struct platform_device tegra_uart3_device;
 extern struct platform_device tegra_uart4_device;
 extern struct platform_device tegra_spdif_device;
 extern struct platform_device tegra_grhost_device;
+extern struct platform_device tegra_spdif_device;
 
 #endif
