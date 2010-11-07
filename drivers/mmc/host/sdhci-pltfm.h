985b1aa05bc47794780ac92eebdaf006a5187afb
Mike Rapoport <mike@compulab.co.il>
2010-11-07 16:57:12 -0500

    mmc: Add support for the Marvell Dove SDHCI controller
    Implement an sdhci-pltfm driver for the controller found in the
    Marvell Dove SoC.
    
    Signed-off-by: Mike Rapoport <mike@compulab.co.il>
    CC: Saeed Bishara <saeed@marvell.com>
    Acked-by: Wolfram Sang <w.sang@pengutronix.de>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/sdhci-pltfm.h
+++ drivers/mmc/host/sdhci-pltfm.h
@@ -22,5 +22,6 @@ struct sdhci_pltfm_host {
 
 extern struct sdhci_pltfm_data sdhci_cns3xxx_pdata;
 extern struct sdhci_pltfm_data sdhci_esdhc_imx_pdata;
+extern struct sdhci_pltfm_data sdhci_dove_pdata;
 
 #endif /* _DRIVERS_MMC_SDHCI_PLTFM_H */
