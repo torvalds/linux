8477cedb49c69ce64b603d76c7776811eec0f9d7
Lars-Peter Clausen <lars@metafoo.de>
2010-11-11 19:00:53 +0100

    jz4740-battery: Check if platform_data is supplied
    Currently platform_data is dereferenced without checking whether it
    is actually set, which can lead to kernel crashes.
    This patch adds a check which will abort the drivers probe function
    gracefully if no platform_data is supplied.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Anton Vorontsov <cbouatmailru@gmail.com>

--- drivers/power/jz4740-battery.c
+++ drivers/power/jz4740-battery.c
@@ -246,6 +246,11 @@ static int __devinit jz_battery_probe(struct platform_device *pdev)
 	struct jz_battery *jz_battery;
 	struct power_supply *battery;
 
+	if (!pdata) {
+		dev_err(&pdev->dev, "No platform_data supplied\n");
+		return -ENXIO;
+	}
+
 	jz_battery = kzalloc(sizeof(*jz_battery), GFP_KERNEL);
 	if (!jz_battery) {
 		dev_err(&pdev->dev, "Failed to allocate driver structure\n");
