e54b673081d12c46b47fdfe1772656cb2b43721d
Sonic Zhang <sonic.zhang@analog.com>
2010-11-12 02:45:38 +0000

    Blackfin: sport_uart resources: remove unused secondary RX/TX pins
    The SPORT/UART driver doesn't use the secondary channel pins, so don't
    try and request them thus keeping other drivers from using them.
    
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- arch/blackfin/mach-bf537/boards/minotaur.c
+++ arch/blackfin/mach-bf537/boards/minotaur.c
@@ -421,7 +421,7 @@ static struct resource bfin_sport0_uart_resources[] = {
 
 unsigned short bfin_sport0_peripherals[] = {
 	P_SPORT0_TFS, P_SPORT0_DTPRI, P_SPORT0_TSCLK, P_SPORT0_RFS,
-	P_SPORT0_DRPRI, P_SPORT0_RSCLK, P_SPORT0_DRSEC, P_SPORT0_DTSEC, 0
+	P_SPORT0_DRPRI, P_SPORT0_RSCLK, 0
 };
 
 static struct platform_device bfin_sport0_uart_device = {
@@ -455,7 +455,7 @@ static struct resource bfin_sport1_uart_resources[] = {
 
 unsigned short bfin_sport1_peripherals[] = {
 	P_SPORT1_TFS, P_SPORT1_DTPRI, P_SPORT1_TSCLK, P_SPORT1_RFS,
-	P_SPORT1_DRPRI, P_SPORT1_RSCLK, P_SPORT1_DRSEC, P_SPORT1_DTSEC, 0
+	P_SPORT1_DRPRI, P_SPORT1_RSCLK, 0
 };
 
 static struct platform_device bfin_sport1_uart_device = {
