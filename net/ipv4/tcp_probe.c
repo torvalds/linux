dda0b38692a7298f433b92b1329867b1ecabb4bb
Vasiliy Kulikov <segoon@openwall.com>
2010-11-14 07:06:08 +0000

    net: ipv4: tcp_probe: cleanup snprintf() use
    snprintf() returns number of bytes that were copied if there is no overflow.
    This code uses return value as number of copied bytes.  Theoretically format
    string '%lu.%09lu %pI4:%u %pI4:%u %d %#x %#x %u %u %u %u\n' may be expanded
    up to 163 bytes.  In reality tv.tv_sec is just few bytes instead of 20, 2 ports
    are just 5 bytes each instead of 10, length is 5 bytes instead of 10.  The rest
    is an unstrusted input.  Theoretically if tv_sec is big then copy_to_user() would
    overflow tbuf.
    
    tbuf was increased to fit in 163 bytes.  snprintf() is used to follow return
    value semantic.
    
    Signed-off-by: Vasiliy Kulikov <segoon@openwall.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_probe.c
+++ net/ipv4/tcp_probe.c
@@ -154,7 +154,7 @@ static int tcpprobe_sprint(char *tbuf, int n)
 	struct timespec tv
 		= ktime_to_timespec(ktime_sub(p->tstamp, tcp_probe.start));
 
-	return snprintf(tbuf, n,
+	return scnprintf(tbuf, n,
 			"%lu.%09lu %pI4:%u %pI4:%u %d %#x %#x %u %u %u %u\n",
 			(unsigned long) tv.tv_sec,
 			(unsigned long) tv.tv_nsec,
@@ -174,7 +174,7 @@ static ssize_t tcpprobe_read(struct file *file, char __user *buf,
 		return -EINVAL;
 
 	while (cnt < len) {
-		char tbuf[128];
+		char tbuf[164];
 		int width;
 
 		/* Wait for data in buffer */
