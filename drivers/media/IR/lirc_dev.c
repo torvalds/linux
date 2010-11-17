5c769a68beaee924e1dc90bf06e1b087b1d46237
Dan Carpenter <error27@gmail.com>
2010-11-17 02:12:23 -0300

    [media] lirc_dev: stray unlock in lirc_dev_fop_poll()
    We shouldn't unlock here.  I think this was a cut and paste error.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Acked-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/IR/lirc_dev.c
+++ drivers/media/IR/lirc_dev.c
@@ -522,10 +522,8 @@ unsigned int lirc_dev_fop_poll(struct file *file, poll_table *wait)
 
 	dev_dbg(ir->d.dev, LOGHEAD "poll called\n", ir->d.name, ir->d.minor);
 
-	if (!ir->attached) {
-		mutex_unlock(&ir->irctl_lock);
+	if (!ir->attached)
 		return POLLERR;
-	}
 
 	poll_wait(file, &ir->buf->wait_poll, wait);
 
