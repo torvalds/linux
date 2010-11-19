1cdf370244d5f0a4cf5ed672967cc2e16235908d
Takashi YOSHII <takashi.yoshii.zj@renesas.com>
2010-11-19 16:47:16 +0900

    ARM: mach-shmobile: Add sh73a0 pinmux support
    PFC definitions for sh73a0 to support GPIO and pinmux handling.
    
    Signed-off-by: Takashi YOSHII <takashi.yoshii.zj@renesas.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/arm/mach-shmobile/include/mach/common.h
+++ arch/arm/mach-shmobile/include/mach/common.h
@@ -33,5 +33,6 @@ extern struct clk sh7372_extal2_clk;
 extern void sh73a0_add_early_devices(void);
 extern void sh73a0_add_standard_devices(void);
 extern void sh73a0_clock_init(void);
+extern void sh73a0_pinmux_init(void);
 
 #endif /* __ARCH_MACH_COMMON_H */
