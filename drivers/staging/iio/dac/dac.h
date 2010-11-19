5a7f4fddae8a50d076b5714f47936ea78e0c0858
Michael Hennerich <michael.hennerich@analog.com>
2010-11-19 13:32:38 +0100

    staging: iio: dac: more consistent DAC sysfs attributes naming
    Align DAC sysfs attributes naming with the convention used by ADC
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    Acked-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/dac/dac.h
+++ drivers/staging/iio/dac/dac.h
@@ -2,5 +2,9 @@
  * dac.h - sysfs attributes associated with DACs
  */
 
+/* Deprecated */
 #define IIO_DEV_ATTR_DAC(_num, _store, _addr)			\
 	IIO_DEVICE_ATTR(dac_##_num, S_IWUSR, NULL, _store, _addr)
+
+#define IIO_DEV_ATTR_OUT_RAW(_num, _store, _addr)				\
+	IIO_DEVICE_ATTR(out##_num##_raw, S_IWUSR, NULL, _store, _addr)
