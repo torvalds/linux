f24d86f1a49505cdea56728b853a5d0a3f8e3d11
Alex Deucher <alexdeucher@gmail.com>
2010-11-19 23:27:04 +0000

    drm/radeon/kms: fix resume regression for some r5xx laptops
    I had removed this when I switched the atom indirect io methods
    to use the io bar rather than the mmio bar, but it appears it's
    still needed.
    
    Reported-by: Mark Lord <kernel@teksavvy.com>
    Signed-off-by: Alex Deucher <alexdeucher@gmail.com>
    Cc: stable@kernel.org
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/atom.c
+++ drivers/gpu/drm/radeon/atom.c
@@ -112,6 +112,7 @@ static uint32_t atom_iio_execute(struct atom_context *ctx, int base,
 			base += 3;
 			break;
 		case ATOM_IIO_WRITE:
+			(void)ctx->card->ioreg_read(ctx->card, CU16(base + 1));
 			ctx->card->ioreg_write(ctx->card, CU16(base + 1), temp);
 			base += 3;
 			break;
