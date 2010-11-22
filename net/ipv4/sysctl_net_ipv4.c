0147fc058d11bd4009b126d09974d2c8f48fef15
Alexey Dobriyan <adobriyan@gmail.com>
2010-11-22 12:54:21 +0000

    tcp: restrict net.ipv4.tcp_adv_win_scale (#20312)
    tcp_win_from_space() does the following:
    
          if (sysctl_tcp_adv_win_scale <= 0)
                  return space >> (-sysctl_tcp_adv_win_scale);
          else
                  return space - (space >> sysctl_tcp_adv_win_scale);
    
    "space" is int.
    
    As per C99 6.5.7 (3) shifting int for 32 or more bits is
    undefined behaviour.
    
    Indeed, if sysctl_tcp_adv_win_scale is exactly 32,
    space >> 32 equals space and function returns 0.
    
    Which means we busyloop in tcp_fixup_rcvbuf().
    
    Restrict net.ipv4.tcp_adv_win_scale to [-31, 31].
    
    Fix https://bugzilla.kernel.org/show_bug.cgi?id=20312
    
    Steps to reproduce:
    
          echo 32 >/proc/sys/net/ipv4/tcp_adv_win_scale
          wget www.kernel.org
          [softlockup]
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -26,6 +26,8 @@ static int zero;
 static int tcp_retr1_max = 255;
 static int ip_local_port_range_min[] = { 1, 1 };
 static int ip_local_port_range_max[] = { 65535, 65535 };
+static int tcp_adv_win_scale_min = -31;
+static int tcp_adv_win_scale_max = 31;
 
 /* Update system visible IP port range */
 static void set_local_port_range(int range[2])
@@ -426,7 +428,9 @@ static struct ctl_table ipv4_table[] = {
 		.data		= &sysctl_tcp_adv_win_scale,
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= &tcp_adv_win_scale_min,
+		.extra2		= &tcp_adv_win_scale_max,
 	},
 	{
 		.procname	= "tcp_tw_reuse",
