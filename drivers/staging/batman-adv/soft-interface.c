b6faaae1a15a352d68b3e3cd8b840e56709820bf
Marek Lindner <lindner_marek@yahoo.de>
2010-11-22 12:34:49 +0100

    Staging: batman-adv: ensure that eth_type_trans gets linear memory
    eth_type_trans tries to pull data with the length of the ethernet header
    from the skb. We only ensured that enough data for the first ethernet
    header and the batman header is available in non-paged memory of the skb
    and not for the ethernet after the batman header.
    
    eth_type_trans would fail sometimes with drivers which don't ensure that
    all there data is perfectly linearised.
    
    The failure was noticed through a kernel bug Oops generated by the
    skb_pull inside eth_type_trans.
    
    Reported-by: Rafal Lesniak <lesniak@eresi-project.org>
    Signed-off-by: Marek Lindner <lindner_marek@yahoo.de>
    Signed-off-by: Sven Eckelmann <sven.eckelmann@gmx.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/batman-adv/soft-interface.c
+++ drivers/staging/batman-adv/soft-interface.c
@@ -194,14 +194,15 @@ void interface_rx(struct net_device *soft_iface,
 	struct bat_priv *priv = netdev_priv(soft_iface);
 
 	/* check if enough space is available for pulling, and pull */
-	if (!pskb_may_pull(skb, hdr_size)) {
-		kfree_skb(skb);
-		return;
-	}
+	if (!pskb_may_pull(skb, hdr_size))
+		goto dropped;
+
 	skb_pull_rcsum(skb, hdr_size);
 /*	skb_set_mac_header(skb, -sizeof(struct ethhdr));*/
 
 	/* skb->dev & skb->pkt_type are set here */
+	if (unlikely(!pskb_may_pull(skb, ETH_HLEN)))
+		goto dropped;
 	skb->protocol = eth_type_trans(skb, soft_iface);
 
 	/* should not be neccesary anymore as we use skb_pull_rcsum()
@@ -216,6 +217,11 @@ void interface_rx(struct net_device *soft_iface,
 	soft_iface->last_rx = jiffies;
 
 	netif_rx(skb);
+	return;
+
+dropped:
+	kfree_skb(skb);
+	return;
 }
 
 #ifdef HAVE_NET_DEVICE_OPS
