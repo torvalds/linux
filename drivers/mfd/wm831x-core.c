798e6e321f807c46d81be1572118e031577ea9ab
Mark Brown <broonie@opensource.wolfsonmicro.com>
2010-11-24 18:01:40 +0000

    mfd: Simplify WM832x subdevice instantiation
    All the current WM832x devices have the same set of subdevices so can
    just use multiple case statements with a single body.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/wm831x-core.c
+++ drivers/mfd/wm831x-core.c
@@ -1610,17 +1610,7 @@ int wm831x_device_init(struct wm831x *wm831x, unsigned long id, int irq)
 		break;
 
 	case WM8320:
-		ret = mfd_add_devices(wm831x->dev, -1,
-				      wm8320_devs, ARRAY_SIZE(wm8320_devs),
-				      NULL, 0);
-		break;
-
 	case WM8321:
-		ret = mfd_add_devices(wm831x->dev, -1,
-				      wm8320_devs, ARRAY_SIZE(wm8320_devs),
-				      NULL, 0);
-		break;
-
 	case WM8325:
 		ret = mfd_add_devices(wm831x->dev, -1,
 				      wm8320_devs, ARRAY_SIZE(wm8320_devs),
