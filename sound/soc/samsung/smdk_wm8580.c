38533698dc14e739eefb22cf66c048e3c9264313
Mark Brown <broonie@opensource.wolfsonmicro.com>
2010-11-24 17:18:45 +0000

    ASoC: Make SMDK WM8580 driver unloadable
    Supply a module exit function so that the driver can be unloaded.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Jassi Brar <jassi.brar@samsung.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- sound/soc/samsung/smdk_wm8580.c
+++ sound/soc/samsung/smdk_wm8580.c
@@ -280,6 +280,12 @@ static int __init smdk_audio_init(void)
 }
 module_init(smdk_audio_init);
 
+static void __exit smdk_audio_exit(void)
+{
+	platform_device_unregister(smdk_snd_device);
+}
+module_exit(smdk_audio_exit);
+
 MODULE_AUTHOR("Jaswinder Singh, jassi.brar@samsung.com");
 MODULE_DESCRIPTION("ALSA SoC SMDK WM8580");
 MODULE_LICENSE("GPL");
