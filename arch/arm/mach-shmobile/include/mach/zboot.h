9a4af112bd252be801a433fc3bef793b7b487c3c
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2010-12-01 07:45:00 +0100

    ARM: 6515/1: Add zboot support for SuperH Mobile ARM
    When CONFIG_ZBOOT_ROM is selected, the resulting zImage file will be small
    boot loader and may be burned to rom or flash.
    
    This is the non-board-specific framework portion of this patch-set.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Simon Horman <horms@verge.net.au>
    Acked-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

---
+++ arch/arm/mach-shmobile/include/mach/zboot.h
@@ -0,0 +1,12 @@
+#ifndef ZBOOT_H
+#define ZBOOT_H
+
+/**************************************************
+ *
+ *		board specific settings
+ *
+ **************************************************/
+
+#error "unsupported board."
+
+#endif /* ZBOOT_H */
