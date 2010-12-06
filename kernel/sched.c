63f01241176d7cbc976385aec32f0a209b0bc36a
Steven Rostedt <srostedt@redhat.com>
2010-12-06 14:48:10 -0500

    sched: Remove unlikely() from rt_policy() in sched.c
    The rt_policy() has an unlikely() that the policy it is checking is
    of RT priority (SCHED_FIFO or SCHED_RR).
    
    According to the annotate branch profiler it is incorrect most of the time:
    
     correct incorrect  %        Function                  File              Line
     ------- ---------  -        --------                  ----              ----
       36667   654674  94 rt_policy                      sched.c              126
    
    This makes sense because the rt_policy() is used by the sched_set_scheduler()
    and nice(). Although users may use sys_nice a bit, all RT users use
    the sched_set_scheduler() to set their RT priority, including kernel
    threads.
    
    The above numbers were from a normal desktop computer running
    firefox, evolution, xchat and was part of a distcc compile farm.
    
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/sched.c
+++ kernel/sched.c
@@ -123,7 +123,7 @@
 
 static inline int rt_policy(int policy)
 {
-	if (unlikely(policy == SCHED_FIFO || policy == SCHED_RR))
+	if (policy == SCHED_FIFO || policy == SCHED_RR)
 		return 1;
 	return 0;
 }
