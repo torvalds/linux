0e4934748948b5886c965fe56207b41d3abd58e7
Roland Vossen <rvossen@broadcom.com>
2010-12-07 17:45:39 +0100

    staging: brcm80211: replaced typedef wlc_ap_info_t by struct wlc_ap_info
    Code cleanup.
    
    Signed-off-by: Roland Vossen <rvossen@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/sys/wlc_mac80211.h
+++ drivers/staging/brcm80211/sys/wlc_mac80211.h
@@ -749,7 +749,7 @@ struct wlc_info {
 	ac_bitmap_t apsd_trigger_ac;	/* Permissible Acess Category in which APSD Null
 					 * Trigger frames can be send
 					 */
-	wlc_ap_info_t *ap;
+	struct wlc_ap_info *ap;
 
 	u8 htphy_membership;	/* HT PHY membership */
 
