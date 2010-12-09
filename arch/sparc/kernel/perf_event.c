efc70d241f5c7fc0a9f1c2a01781ed946d9dbe21
Ingo Molnar <mingo@elte.hu>
2010-12-10 00:27:23 +0100

    perf, sparc: Fix CONFIG_PERF_EVENTS=y build error
    Fix a typo in:
    
      004417a6d468: perf, arch: Cleanup perf-pmu init vs lockup-detector
    
    Which caused a build failure on Sparc, reported by Stephen Rothwell.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: David S. Miller <davem@davemloft.net>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/sparc/kernel/perf_event.c
+++ arch/sparc/kernel/perf_event.c
@@ -1323,7 +1323,7 @@ int __init init_hw_perf_events(void)
 
 	return 0;
 }
-early_initcall(init_hw_perf_event);
+early_initcall(init_hw_perf_events);
 
 void perf_callchain_kernel(struct perf_callchain_entry *entry,
 			   struct pt_regs *regs)
