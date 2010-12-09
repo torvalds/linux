747fecab32e47180e2668c2b22b25752d371e636
Jesper Juhl <jj@chaosbits.net>
2010-12-09 23:25:09 +0100

    coda: kill redundant cast in coda_alloc_inode()
    kmem_cache_alloc() returns a void pointer which there is no need to cast.
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- fs/coda/inode.c
+++ fs/coda/inode.c
@@ -45,7 +45,7 @@ static struct kmem_cache * coda_inode_cachep;
 static struct inode *coda_alloc_inode(struct super_block *sb)
 {
 	struct coda_inode_info *ei;
-	ei = (struct coda_inode_info *)kmem_cache_alloc(coda_inode_cachep, GFP_KERNEL);
+	ei = kmem_cache_alloc(coda_inode_cachep, GFP_KERNEL);
 	if (!ei)
 		return NULL;
 	memset(&ei->c_fid, 0, sizeof(struct CodaFid));
