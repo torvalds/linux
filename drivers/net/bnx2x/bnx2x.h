986d730a696277520084180071ac4873aac63905
Vladislav Zolotarov <vladz@broadcom.com>
2010-12-13 05:44:50 +0000

    bnx2x: update version to 1.62.00-2
    Signed-off-by: Vladislav Zolotarov <vladz@broadcom.com>
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2x/bnx2x.h
+++ drivers/net/bnx2x/bnx2x.h
@@ -22,8 +22,8 @@
  * (you will need to reboot afterwards) */
 /* #define BNX2X_STOP_ON_ERROR */
 
-#define DRV_MODULE_VERSION      "1.60.00-7"
-#define DRV_MODULE_RELDATE      "2010/12/08"
+#define DRV_MODULE_VERSION      "1.62.00-2"
+#define DRV_MODULE_RELDATE      "2010/12/13"
 #define BNX2X_BC_VER            0x040200
 
 #define BNX2X_MULTI_QUEUE
