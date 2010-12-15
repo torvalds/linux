2b7e6bce41b6b944bd4302cf0914e2a6f4bc704b
Mohammed Shafi Shajakhan <mshajakhan@atheros.com>
2010-12-15 13:02:46 +0530

    ath9k: Add comments for making pm-qos as modparam
    PM-QOS value can be user specified via module parameter.
    This patch adds few comments regarding this in the driver
    code.
    
    Signed-off-by: Mohammed Shafi Shajakhan <mshajakhan@atheros.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/main.c
+++ drivers/net/wireless/ath/ath9k/main.c
@@ -1196,6 +1196,10 @@ static int ath9k_start(struct ieee80211_hw *hw)
 			ath9k_btcoex_timer_resume(sc);
 	}
 
+	/* User has the option to provide pm-qos value as a module
+	 * parameter rather than using the default value of
+	 * 'ATH9K_PM_QOS_DEFAULT_VALUE'.
+	 */
 	pm_qos_update_request(&sc->pm_qos_req, ath9k_pm_qos_value);
 
 	if (ah->caps.pcie_lcr_extsync_en && common->bus_ops->extn_synch_en)
