78a8e9426d1ffaa19c29e92a8c4a726d2364c10e
Russell King <rmk+kernel@arm.linux.org.uk>
2010-12-15 19:23:07 +0000

    ARM: sched_clock: provide common infrastructure for sched_clock()
    Provide common sched_clock() infrastructure for platforms to use to
    create a 64-bit ns based sched_clock() implementation from a counter
    running at a non-variable clock rate.
    
    This implementation is based upon maintaining an epoch for the counter
    and an epoch for the nanosecond time.  When we desire a sched_clock()
    time, we calculate the number of counter ticks since the last epoch
    update, convert this to nanoseconds and add to the epoch nanoseconds.
    
    We regularly refresh these epochs within the counter wrap interval.
    We perform a similar calculation as above, and store the new epochs.
    
    We read and write the epochs in such a way that sched_clock() can easily
    (and locklessly) detect when an update is in progress, and repeat the
    loading of these constants when they're known not to be stable.  The
    one caveat is that sched_clock() is not called in the middle of an
    update.  We achieve that by disabling IRQs.
    
    Finally, if the clock rate is known at compile time, the counter to ns
    conversion factors can be specified, allowing sched_clock() to be tightly
    optimized.  We ensure that these factors are correct by providing an
    initialization function which performs a run-time check.
    
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Tested-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Tested-by: Will Deacon <will.deacon@arm.com>
    Tested-by: Mikael Pettersson <mikpe@it.uu.se>
    Tested-by: Eric Miao <eric.y.miao@gmail.com>
    Tested-by: Olof Johansson <olof@lixom.net>
    Tested-by: Jamie Iles <jamie@jamieiles.com>
    Reviewed-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

---
+++ arch/arm/kernel/sched_clock.c
@@ -0,0 +1,69 @@
+/*
+ * sched_clock.c: support for extending counters to full 64-bit ns counter
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <linux/clocksource.h>
+#include <linux/init.h>
+#include <linux/jiffies.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/timer.h>
+
+#include <asm/sched_clock.h>
+
+static void sched_clock_poll(unsigned long wrap_ticks);
+static DEFINE_TIMER(sched_clock_timer, sched_clock_poll, 0, 0);
+static void (*sched_clock_update_fn)(void);
+
+static void sched_clock_poll(unsigned long wrap_ticks)
+{
+	mod_timer(&sched_clock_timer, round_jiffies(jiffies + wrap_ticks));
+	sched_clock_update_fn();
+}
+
+void __init init_sched_clock(struct clock_data *cd, void (*update)(void),
+	unsigned int clock_bits, unsigned long rate)
+{
+	unsigned long r, w;
+	u64 res, wrap;
+	char r_unit;
+
+	sched_clock_update_fn = update;
+
+	/* calculate the mult/shift to convert counter ticks to ns. */
+	clocks_calc_mult_shift(&cd->mult, &cd->shift, rate, NSEC_PER_SEC, 60);
+
+	r = rate;
+	if (r >= 4000000) {
+		r /= 1000000;
+		r_unit = 'M';
+	} else {
+		r /= 1000;
+		r_unit = 'k';
+	}
+
+	/* calculate how many ns until we wrap */
+	wrap = cyc_to_ns((1ULL << clock_bits) - 1, cd->mult, cd->shift);
+	do_div(wrap, NSEC_PER_MSEC);
+	w = wrap;
+
+	/* calculate the ns resolution of this counter */
+	res = cyc_to_ns(1ULL, cd->mult, cd->shift);
+	pr_info("sched_clock: %u bits at %lu%cHz, resolution %lluns, wraps every %lums\n",
+		clock_bits, r, r_unit, res, w);
+
+	/*
+	 * Start the timer to keep sched_clock() properly updated and
+	 * sets the initial epoch.
+	 */
+	sched_clock_timer.data = msecs_to_jiffies(w - (w / 10));
+	sched_clock_poll(sched_clock_timer.data);
+
+	/*
+	 * Ensure that sched_clock() starts off at 0ns
+	 */
+	cd->epoch_ns = 0;
+}
