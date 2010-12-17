b9f985b6e05ebd7af2aaef0eb3ae369390ef191f
Yang Zhang <kthreadd@gmail.com>
2010-12-17 08:58:36 +0100

    block: convert !IS_ERR(p) && p to !IS_ERR_NOR_NULL(p)
    Signed-off-by: Yang Zhang <kthreadd@gmail.com>
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

--- block/genhd.c
+++ block/genhd.c
@@ -735,7 +735,7 @@ static void *show_partition_start(struct seq_file *seqf, loff_t *pos)
 	static void *p;
 
 	p = disk_seqf_start(seqf, pos);
-	if (!IS_ERR(p) && p && !*pos)
+	if (!IS_ERR_OR_NULL(p) && !*pos)
 		seq_puts(seqf, "major minor  #blocks  name\n\n");
 	return p;
 }
