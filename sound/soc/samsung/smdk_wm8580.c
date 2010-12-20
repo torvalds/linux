40d2482993d39f3b99973b6e94b358fa1e1f0679
Jassi Brar <jassi.brar@samsung.com>
2010-12-20 11:05:55 +0900

    ASoC: SMDKC110: Enable I2S device
    Signed-off-by: Jassi Brar <jassi.brar@samsung.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/samsung/smdk_wm8580.c
+++ sound/soc/samsung/smdk_wm8580.c
@@ -255,7 +255,7 @@ static int __init smdk_audio_init(void)
 	char *str;
 
 	if (machine_is_smdkc100() || machine_is_smdk6442()
-			|| machine_is_smdkv210()) {
+			|| machine_is_smdkv210() || machine_is_smdkc110()) {
 		smdk.num_links = 3;
 		/* Secondary is at offset SAMSUNG_I2S_SECOFF from Primary */
 		str = (char *)smdk_dai[SEC_PLAYBACK].cpu_dai_name;
