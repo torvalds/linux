5b59cc2fc2ff90369d6aa7862caaba3cf45b5075
Raghuveer Murthy <raghuveer.murthy@ti.com>
2010-12-21 14:14:34 +0000

    OMAP4: Pandaboard: Fix MMC card detect gpio line
    commit bf56f0a6668cd (2.6.37-rc1), from Nishanth Menon attempted
    to fix card detection for PandaBoard, unfortunately, the fix missed
    to initialize .gpio_cd member of omap2_hsmmc_info. This results
    in a default value of '0', which is a valid GPIO line.
    On PandaBoard, the side effect of this is that GPIO line 0 controls
    the powering TFP410 DVI chip, and without the fix DVI chip is
    inadvertently powered.
    
    Tested-by: David Anders <x0132446@ti.com>
    Acked-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Kishore Kadiyala <kishore.kadiyala@ti.com>
    Signed-off-by: Raghuveer Murthy <raghuveer.murthy@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-omap4panda.c
+++ arch/arm/mach-omap2/board-omap4panda.c
@@ -142,6 +142,7 @@ static struct omap2_hsmmc_info mmc[] = {
 		.mmc		= 1,
 		.caps		= MMC_CAP_4_BIT_DATA | MMC_CAP_8_BIT_DATA,
 		.gpio_wp	= -EINVAL,
+		.gpio_cd	= -EINVAL,
 	},
 	{}	/* Terminator */
 };
