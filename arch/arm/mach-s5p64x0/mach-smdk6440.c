149bb5bcc3b1c5f4044e0ad088afdc8a12b51c64
Jassi Brar <jassi.brar@samsung.com>
2010-12-21 09:46:29 +0900

    ARM: S5P6440: Enable I2S device to work on SMDK6440
    Add missing virtual ASoC DMA device and WM8580 as I2C slave,
    so that the I2S can work on SMDK6440.
    
    [kgene.kim@samsung.com: removed virtual ASoC DMA device
    to avoid build error and it will be added next time]
    
    Signed-off-by: Jassi Brar <jassi.brar@samsung.com>
    Acked-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    [kgene.kim@samsung.com: minor changed title]
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s5p64x0/mach-smdk6440.c
+++ arch/arm/mach-s5p64x0/mach-smdk6440.c
@@ -117,6 +117,7 @@ static struct s3c2410_platform_i2c s5p6440_i2c1_data __initdata = {
 
 static struct i2c_board_info smdk6440_i2c_devs0[] __initdata = {
 	{ I2C_BOARD_INFO("24c08", 0x50), },
+	{ I2C_BOARD_INFO("wm8580", 0x1b), },
 };
 
 static struct i2c_board_info smdk6440_i2c_devs1[] __initdata = {
