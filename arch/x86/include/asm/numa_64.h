34dc9e74968ef4b582c3815b0124d88a68dfabef
David Rientjes <rientjes@google.com>
2010-12-22 17:23:40 -0800

    x86, numa: Reduce minimum fake node size to 32M
    This patch changes the minimum fake node size from 64MB to 32MB so it is
    possible to test NUMA code at a greater scale on smaller machines
    (64 nodes on a 2G machine, 1024 nodes on 32G machine with
    CONFIG_NODES_SHIFT=10).
    
    Signed-off-by: David Rientjes <rientjes@google.com>
    LKML-Reference: <alpine.DEB.2.00.1012221700590.3701@chino.kir.corp.google.com>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/include/asm/numa_64.h
+++ arch/x86/include/asm/numa_64.h
@@ -38,7 +38,7 @@ extern void __cpuinit numa_add_cpu(int cpu);
 extern void __cpuinit numa_remove_cpu(int cpu);
 
 #ifdef CONFIG_NUMA_EMU
-#define FAKE_NODE_MIN_SIZE	((u64)64 << 20)
+#define FAKE_NODE_MIN_SIZE	((u64)32 << 20)
 #define FAKE_NODE_MIN_HASH_MASK	(~(FAKE_NODE_MIN_SIZE - 1UL))
 #endif /* CONFIG_NUMA_EMU */
 #else
