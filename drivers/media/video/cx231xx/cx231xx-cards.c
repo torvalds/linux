707bcf326bd50c875d82bd2e7c31dcfb92b7e813
Tejun Heo <tj@kernel.org>
2010-12-24 16:14:20 +0100

    media/video: explicitly flush request_module work
    Video drivers request submodules using a work during probe and calls
    flush_scheduled_work() on exit to make sure the work is complete
    before being unloaded.  This patch makes these drivers flush the work
    directly instead of using flush_scheduled_work().
    
    While at it, relocate request_submodules() call in saa7134_initdev()
    right right before successful return as in other drivers to avoid
    failing after the work is scheduled and returning failure without the
    work still active.
    
    This is in preparation for the deprecation of flush_scheduled_work().
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/cx231xx/cx231xx-cards.c
+++ drivers/media/video/cx231xx/cx231xx-cards.c
@@ -762,8 +762,14 @@ static void request_modules(struct cx231xx *dev)
 	INIT_WORK(&dev->request_module_wk, request_module_async);
 	schedule_work(&dev->request_module_wk);
 }
+
+static void flush_request_modules(struct cx231xx *dev)
+{
+	flush_work_sync(&dev->request_module_wk);
+}
 #else
 #define request_modules(dev)
+#define flush_request_modules(dev)
 #endif /* CONFIG_MODULES */
 
 /*
@@ -1095,6 +1101,8 @@ static void cx231xx_usb_disconnect(struct usb_interface *interface)
 
 	if (!dev->udev)
 		return;
+
+	flush_request_modules(dev);
 
 	/* delete v4l2 device */
 	v4l2_device_unregister(&dev->v4l2_dev);
