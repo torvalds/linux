c506818b8ed538b8022cba852189e442fb55f031
hxy <hxy@rock-chips.com>
2010-12-24 17:47:50 +0800

    add rk29 aigo board modified

--- drivers/video/display/screen/lcd_hsd100pxn.c
+++ drivers/video/display/screen/lcd_hsd100pxn.c
@@ -9,7 +9,12 @@
 
 /* Base */
 #define OUT_TYPE		SCREEN_RGB
+
+#if defined(CONFIG_MACH_RK29SDK)
 #define OUT_FACE		OUT_D888_P666
+#elif defined(CONFIG_MACH_RK29_AIGO)
+#define OUT_FACE		OUT_P888  
+#endif
 #define OUT_CLK			 65000000
 #define LCDC_ACLK        312000000           //29 lcdc axi DMA ÆµÂÊ
 
@@ -28,8 +33,11 @@
 #define LCD_HEIGHT      152
 /* Other */
 #define DCLK_POL		0
+#if defined(CONFIG_MACH_RK29SDK)
 #define SWAP_RB			0
-
+#elif defined(CONFIG_MACH_RK29_AIGO)
+#define SWAP_RB			1
+#endif
 
 void set_lcd_info(struct rk29fb_screen *screen, struct rk29lcd_info *lcd_info )
 {
