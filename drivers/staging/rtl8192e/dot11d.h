dd1847332a5e5c6ac945ad12db0bc49871481d3a
Mike McCormack <mikem@ring3k.org>
2010-12-27 21:49:49 +0900

    Staging: rtl8192e: Convert cpMacAddr macro to inline function
    Signed-off-by: Mike McCormack <mikem@ring3k.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8192e/dot11d.h
+++ drivers/staging/rtl8192e/dot11d.h
@@ -41,9 +41,10 @@ typedef struct _RT_DOT11D_INFO {
 	DOT11D_STATE State;
 } RT_DOT11D_INFO, *PRT_DOT11D_INFO;
 
-#define cpMacAddr(des, src) ((des)[0] = (src)[0], (des)[1] = (src)[1], \
-			(des)[2] = (src)[2], (des)[3] = (src)[3], \
-			(des)[4] = (src)[4], (des)[5] = (src)[5])
+static inline void cpMacAddr(unsigned char *des, unsigned char *src)
+{
+	memcpy(des, src, 6);
+}
 
 #define GET_DOT11D_INFO(__pIeeeDev) ((PRT_DOT11D_INFO) \
 			((__pIeeeDev)->pDot11dInfo))
