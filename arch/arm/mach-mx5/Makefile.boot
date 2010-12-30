3d5a44be308203a9a192a77037fb613c2f82ed85
Richard Zhao <richard.zhao@freescale.com>
2010-12-30 19:25:05 +0800

    arm: mx50: add core functions support except clock
    Add core definitions and memory map, gpio, irq, iomux, uart device
    support.
    
    Signed-off-by: Richard Zhao <richard.zhao@freescale.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-mx5/Makefile.boot
+++ arch/arm/mach-mx5/Makefile.boot
@@ -1,3 +1,6 @@
+   zreladdr-$(CONFIG_ARCH_MX50)	:= 0x70008000
+params_phys-$(CONFIG_ARCH_MX50)	:= 0x70000100
+initrd_phys-$(CONFIG_ARCH_MX50)	:= 0x70800000
    zreladdr-$(CONFIG_ARCH_MX51)	:= 0x90008000
 params_phys-$(CONFIG_ARCH_MX51)	:= 0x90000100
 initrd_phys-$(CONFIG_ARCH_MX51)	:= 0x90800000
--- arch/arm/plat-mxc/include/mach/irqs.h
+++ arch/arm/plat-mxc/include/mach/irqs.h
@@ -25,6 +25,8 @@
 /* these are ordered by size to support multi-SoC kernels */
 #if defined CONFIG_ARCH_MX2
 #define MXC_GPIO_IRQS		(32 * 6)
+#elif defined CONFIG_ARCH_MX50
+#define MXC_GPIO_IRQS		(32 * 6)
 #elif defined CONFIG_ARCH_MX1
 #define MXC_GPIO_IRQS		(32 * 4)
 #elif defined CONFIG_ARCH_MX25
