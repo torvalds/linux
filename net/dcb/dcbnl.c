2a8fe003741aa90b6b9453e90af4bbb7bc42918c
Dan Carpenter <error27@gmail.com>
2011-01-04 21:03:44 +0000

    dcb: use after free in dcb_flushapp()
    The original code has a use after free bug because it's not using the
    _safe() version of the list_for_each_entry() macro.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dcb/dcbnl.c
+++ net/dcb/dcbnl.c
@@ -1643,9 +1643,10 @@ EXPORT_SYMBOL(dcb_setapp);
 static void dcb_flushapp(void)
 {
 	struct dcb_app_type *app;
+	struct dcb_app_type *tmp;
 
 	spin_lock(&dcb_lock);
-	list_for_each_entry(app, &dcb_app_list, list) {
+	list_for_each_entry_safe(app, tmp, &dcb_app_list, list) {
 		list_del(&app->list);
 		kfree(app);
 	}
