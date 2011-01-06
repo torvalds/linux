706c57d802394e2fe720ebc929234a678f94e716
Jarod Wilson <jarod@redhat.com>
2011-01-06 13:23:13 -0300

    [media] rc/mceusb: timeout should be in ns, not us
    Fixes an egregious bug in mceusb driver, where the receiver was being
    put into idle mode far sooner than it should have, thanks to storing a
    timeout value that in us where it should be ns. Basically, the receiver
    kept going into idle mode before a trailing space had been fully
    received, which was causing problems for some protocols, most notably
    manifesting as lirc userspace never receiving a trailing space for any
    rc5 signals.
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/rc/mceusb.c
+++ drivers/media/rc/mceusb.c
@@ -816,7 +816,7 @@ static void mceusb_handle_command(struct mceusb_dev *ir, int index)
 	switch (ir->buf_in[index]) {
 	/* 2-byte return value commands */
 	case MCE_CMD_S_TIMEOUT:
-		ir->rc->timeout = MS_TO_US((hi << 8 | lo) / 2);
+		ir->rc->timeout = MS_TO_NS((hi << 8 | lo) / 2);
 		break;
 
 	/* 1-byte return value commands */
@@ -1060,7 +1060,7 @@ static struct rc_dev *mceusb_init_rc_dev(struct mceusb_dev *ir)
 	rc->priv = ir;
 	rc->driver_type = RC_DRIVER_IR_RAW;
 	rc->allowed_protos = RC_TYPE_ALL;
-	rc->timeout = MS_TO_US(1000);
+	rc->timeout = MS_TO_NS(1000);
 	if (!ir->flags.no_tx) {
 		rc->s_tx_mask = mceusb_set_tx_mask;
 		rc->s_tx_carrier = mceusb_set_tx_carrier;
