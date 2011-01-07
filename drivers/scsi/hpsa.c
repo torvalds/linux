1d5e2ed0805bf426226b7daa54b3e60af78baa10
Stephen M. Cameron <StephenM.Cameron>
2011-01-07 10:55:48 -0600

    [SCSI] hpsa: Fix problem that CMD_UNABORTABLE command status was treated as unknown
    Signed-off-by: Stephen M. Cameron <scameron@beardog.cce.hp.com>
    Signed-off-by: James Bottomley <James.Bottomley@suse.de>

--- drivers/scsi/hpsa.c
+++ drivers/scsi/hpsa.c
@@ -1152,6 +1152,10 @@ static void complete_scsi_command(struct CommandList *cp,
 		cmd->result = DID_TIME_OUT << 16;
 		dev_warn(&h->pdev->dev, "cp %p timedout\n", cp);
 		break;
+	case CMD_UNABORTABLE:
+		cmd->result = DID_ERROR << 16;
+		dev_warn(&h->pdev->dev, "Command unabortable\n");
+		break;
 	default:
 		cmd->result = DID_ERROR << 16;
 		dev_warn(&h->pdev->dev, "cp %p returned unknown status %x\n",
@@ -1316,6 +1320,9 @@ static void hpsa_scsi_interpret_error(struct CommandList *cp)
 		break;
 	case CMD_TIMEOUT:
 		dev_warn(d, "cp %p timed out\n", cp);
+		break;
+	case CMD_UNABORTABLE:
+		dev_warn(d, "Command unabortable\n");
 		break;
 	default:
 		dev_warn(d, "cp %p returned unknown status %x\n", cp,
