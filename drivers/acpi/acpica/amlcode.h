b4e104eaeb8cd4329a23e0e4ebf166681b1d182d
Bob Moore <robert.moore@intel.com>
2011-01-17 11:05:40 +0800

    ACPICA: Update all ACPICA copyrights and signons to 2011
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lin Ming <ming.m.lin@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/acpica/amlcode.h
+++ drivers/acpi/acpica/amlcode.h
@@ -7,7 +7,7 @@
  *****************************************************************************/
 
 /*
- * Copyright (C) 2000 - 2010, Intel Corp.
+ * Copyright (C) 2000 - 2011, Intel Corp.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
