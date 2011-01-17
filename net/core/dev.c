6ee400aafb60289b78fcde5ebccd8c4973fc53f4
Jesse Gross <jesse@nicira.com>
2011-01-17 20:46:00 +0000

    net offloading: Do not mask out NETIF_F_HW_VLAN_TX for vlan.
    In netif_skb_features() we return only the features that are valid for vlans
    if we have a vlan packet.  However, we should not mask out NETIF_F_HW_VLAN_TX
    since it enables transmission of vlan tags and is obviously valid.
    
    Reported-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: Jesse Gross <jesse@nicira.com>
    Acked-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -2023,13 +2023,13 @@ int netif_skb_features(struct sk_buff *skb)
 		return harmonize_features(skb, protocol, features);
 	}
 
-	features &= skb->dev->vlan_features;
+	features &= (skb->dev->vlan_features | NETIF_F_HW_VLAN_TX);
 
 	if (protocol != htons(ETH_P_8021Q)) {
 		return harmonize_features(skb, protocol, features);
 	} else {
 		features &= NETIF_F_SG | NETIF_F_HIGHDMA | NETIF_F_FRAGLIST |
-				NETIF_F_GEN_CSUM;
+				NETIF_F_GEN_CSUM | NETIF_F_HW_VLAN_TX;
 		return harmonize_features(skb, protocol, features);
 	}
 }
