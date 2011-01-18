6c89b7b2f856a2b67f53313ddfa3af4ab52bae28
Arik Nemtsov <arik@wizery.com>
2011-01-18 20:39:52 +0100

    wl12xx: Add channel 14 to list of supported 2ghz channels
    Channel 14 is only supported in Japan (JP country code in regdb).  The
    FW limits tranmissions to CCK only on this channel.
    
    Tested in both STA and AP modes to work correctly.
    
    Signed-off-by: Arik Nemtsov <arik@wizery.com>
    Reviewed-by: Luciano Coelho <coelho@ti.com>
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/wl12xx/main.c
+++ drivers/net/wireless/wl12xx/main.c
@@ -2778,6 +2778,7 @@ static struct ieee80211_channel wl1271_channels[] = {
 	{ .hw_value = 11, .center_freq = 2462, .max_power = 25 },
 	{ .hw_value = 12, .center_freq = 2467, .max_power = 25 },
 	{ .hw_value = 13, .center_freq = 2472, .max_power = 25 },
+	{ .hw_value = 14, .center_freq = 2484, .max_power = 25 },
 };
 
 /* mapping to indexes for wl1271_rates */
