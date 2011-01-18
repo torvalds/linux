dbf05b279b8ed398a8e92b0c1187882f6df2bf7f
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-01-17 22:18:02 -0200

    [media] ngene: Fix compilation when cxd2099 is not enabled
    drivers/media/dvb/ngene/ngene-i2c.o: In function `cxd2099_attach':
    ngene-i2c.c:(.text+0x195): multiple definition of `cxd2099_attach'
    drivers/media/dvb/ngene/ngene-core.o:ngene-core.c:(.text+0x100): first defined here
    drivers/media/dvb/ngene/ngene-cards.o: In function `cxd2099_attach':
    ngene-cards.c:(.text+0x116): multiple definition of `cxd2099_attach'
    drivers/media/dvb/ngene/ngene-core.o:ngene-core.c:(.text+0x100): first defined here
    drivers/media/dvb/ngene/ngene-dvb.o: In function `cxd2099_attach':
    ngene-dvb.c:(.text+0x7ad): multiple definition of `cxd2099_attach'
    drivers/media/dvb/ngene/ngene-core.o:ngene-core.c:(.text+0x100): first defined here
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/staging/cxd2099/cxd2099.h
+++ drivers/staging/cxd2099/cxd2099.h
@@ -31,7 +31,7 @@
         (defined(CONFIG_DVB_CXD2099_MODULE) && defined(MODULE))
 struct dvb_ca_en50221 *cxd2099_attach(u8 adr, void *priv, struct i2c_adapter *i2c);
 #else
-struct dvb_ca_en50221 *cxd2099_attach(u8 adr, void *priv, struct i2c_adapter *i2c)
+static inline struct dvb_ca_en50221 *cxd2099_attach(u8 adr, void *priv, struct i2c_adapter *i2c)
 {
 	printk(KERN_WARNING "%s: driver disabled by Kconfig\n", __func__);
 	return NULL;
