d402786ea4f8433774a812d6b8635e737425cddd
Eric Dumazet <eric.dumazet@gmail.com>
2011-01-19 00:51:36 +0000

    net: fix can_checksum_protocol() arguments swap
    commit 0363466866d901fbc (net offloading: Convert checksums to use
    centrally computed features.) mistakenly swapped can_checksum_protocol()
    arguments.
    
    This broke IPv6 on bnx2 for instance, on NIC without TCPv6 checksum
    offloads.
    
    Reported-by: Hans de Bruin <jmdebruin@xmsnet.nl>
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    Acked-by: Jesse Gross <jesse@nicira.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -2001,7 +2001,7 @@ static bool can_checksum_protocol(unsigned long features, __be16 protocol)
 
 static int harmonize_features(struct sk_buff *skb, __be16 protocol, int features)
 {
-	if (!can_checksum_protocol(protocol, features)) {
+	if (!can_checksum_protocol(features, protocol)) {
 		features &= ~NETIF_F_ALL_CSUM;
 		features &= ~NETIF_F_SG;
 	} else if (illegal_highdma(skb->dev, skb)) {
