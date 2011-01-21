6f4810101a629b31b5427872a09ea092cfc5c4bd
Luis R. Rodriguez <lrodriguez@atheros.com>
2011-01-20 17:47:39 -0800

    ath9k_hw: disabled PAPRD for AR9003
    AR9003's PAPRD was enabled prematurely, and is causing some
    large discrepancies on throughput and network connectivity.
    For example downlink (RX) throughput against an AR9280 AP
    can vary widlely from 43-73 Mbit/s while disabling this
    gets AR9382 (2x2) up to around 93 Mbit/s in a 2.4 GHz HT20 setup.
    
    Cc: stable@kernel.org
    Cc: Paul Shaw <paul.shaw@atheros.com>
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/hw.c
+++ drivers/net/wireless/ath/ath9k/hw.c
@@ -369,6 +369,9 @@ static void ath9k_hw_init_config(struct ath_hw *ah)
 	else
 		ah->config.ht_enable = 0;
 
+	/* PAPRD needs some more work to be enabled */
+	ah->config.paprd_disable = 1;
+
 	ah->config.rx_intr_mitigation = true;
 	ah->config.pcieSerDesWrite = true;
 
@@ -1933,7 +1936,8 @@ int ath9k_hw_fill_cap_info(struct ath_hw *ah)
 		pCap->rx_status_len = sizeof(struct ar9003_rxs);
 		pCap->tx_desc_len = sizeof(struct ar9003_txc);
 		pCap->txs_len = sizeof(struct ar9003_txs);
-		if (ah->eep_ops->get_eeprom(ah, EEP_PAPRD))
+		if (!ah->config.paprd_disable &&
+		    ah->eep_ops->get_eeprom(ah, EEP_PAPRD))
 			pCap->hw_caps |= ATH9K_HW_CAP_PAPRD;
 	} else {
 		pCap->tx_desc_len = sizeof(struct ath_desc);
