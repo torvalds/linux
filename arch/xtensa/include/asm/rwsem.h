235454c99851ba21038061b9acf38d1a636068c5
Thomas Gleixner <tglx@linutronix.de>
2011-01-23 15:23:14 +0100

    xtensa: Replace deprecated spinlock initialization
    SPIN_LOCK_UNLOCK is deprecated. Use the lockdep capable variant
    instead.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Chris Zankel <chris@zankel.net>

--- arch/xtensa/include/asm/rwsem.h
+++ arch/xtensa/include/asm/rwsem.h
@@ -38,7 +38,7 @@ struct rw_semaphore {
 };
 
 #define __RWSEM_INITIALIZER(name) \
-	{ RWSEM_UNLOCKED_VALUE, SPIN_LOCK_UNLOCKED, \
+	{ RWSEM_UNLOCKED_VALUE, __SPIN_LOCK_UNLOCKED((name).wait_lock),	\
 	  LIST_HEAD_INIT((name).wait_list) }
 
 #define DECLARE_RWSEM(name)		\
