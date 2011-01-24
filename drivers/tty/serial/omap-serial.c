78841462d72fe7038cb7ea48adecc6fc395f2dc5
Jarkko Nikula <jhnikula@gmail.com>
2011-01-24 17:51:22 +0200

    serial: omap-serial: Enable the UART wake-up bits always
    OMAP can do also dynamic idling so wake-up enable register should be set
    also while system is running. If UART_OMAP_WER is not set, then for instance
    the RX activity cannot wake up the UART port that is sleeping.
    
    This RX wake-up feature was working when the 8250 driver was used instead
    of omap-serial. Reason for this is that the 8250 doesn't set the
    UART_OMAP_WER and then arch/arm/mach-omap2/pm34xx.c ends up saving and
    restoring the reset default which is the same than value
    OMAP_UART_WER_MOD_WKUP here.
    
    Fix this by moving the conditional UART_OMAP_WER write from serial_omap_pm
    into serial_omap_startup where wake-up bits are set unconditionally.
    
    Signed-off-by: Jarkko Nikula <jhnikula@gmail.com>
    Cc: Govindraj.R <govindraj.raja@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/tty/serial/omap-serial.c
+++ drivers/tty/serial/omap-serial.c
@@ -520,6 +520,9 @@ static int serial_omap_startup(struct uart_port *port)
 	up->ier = UART_IER_RLSI | UART_IER_RDI;
 	serial_out(up, UART_IER, up->ier);
 
+	/* Enable module level wake up */
+	serial_out(up, UART_OMAP_WER, OMAP_UART_WER_MOD_WKUP);
+
 	up->port_activity = jiffies;
 	return 0;
 }
@@ -827,9 +830,6 @@ serial_omap_pm(struct uart_port *port, unsigned int state,
 	serial_out(up, UART_LCR, UART_LCR_CONF_MODE_B);
 	serial_out(up, UART_EFR, efr);
 	serial_out(up, UART_LCR, 0);
-	/* Enable module level wake up */
-	serial_out(up, UART_OMAP_WER,
-		(state != 0) ? OMAP_UART_WER_MOD_WKUP : 0);
 }
 
 static void serial_omap_release_port(struct uart_port *port)
