db3f94c5a44ebe356c3850bf2c04ea0bd5d2443e
Arend van Spriel <arend@broadcom.com>
2011-01-25 16:53:40 +0100

    staging: brcm80211: align common dirver code
    Remove differences in util sources for the two supported drivers.
    
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/util/hndpmu.c
+++ drivers/staging/brcm80211/util/hndpmu.c
@@ -32,6 +32,10 @@
 
 #ifdef BCMDBG
 #define	PMU_MSG(args)	printf args
+
+/* debug-only definitions */
+/* #define BCMDBG_FORCEHT */
+/* #define CHIPC_UART_ALWAYS_ON */
 #else
 #define	PMU_MSG(args)
 #endif				/* BCMDBG */
@@ -2504,12 +2508,7 @@ bool si_pmu_is_otp_powered(si_t *sih, struct osl_info *osh)
 	return st;
 }
 
-void
-#if defined(BCMDBG)
-si_pmu_sprom_enable(si_t *sih, struct osl_info *osh, bool enable)
-#else
-si_pmu_sprom_enable(si_t *sih, struct osl_info *osh, bool enable)
-#endif
+void si_pmu_sprom_enable(si_t *sih, struct osl_info *osh, bool enable)
 {
 	chipcregs_t *cc;
 	uint origidx;
