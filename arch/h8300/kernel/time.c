daad8b581e7f5e21a2f79e49d57d4f6a73b26510
Torben Hohn <torbenh@gmx.de>
2011-01-27 15:59:51 +0100

    h8300: Switch do_timer() to xtime_update()
    xtime_update() takes the xtime_lock itself.
    
    Signed-off-by: Torben Hohn <torbenh@gmx.de>
    Cc: Yoshinori Sato <ysato@users.sourceforge.jp>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: johnstul@us.ibm.com
    Cc: hch@infradead.org
    Cc: yong.zhang0@gmail.com
    LKML-Reference: <20110127145951.23248.92727.stgit@localhost>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/h8300/kernel/time.c
+++ arch/h8300/kernel/time.c
@@ -35,9 +35,7 @@ void h8300_timer_tick(void)
 {
 	if (current->pid)
 		profile_tick(CPU_PROFILING);
-	write_seqlock(&xtime_lock);
-	do_timer(1);
-	write_sequnlock(&xtime_lock);
+	xtime_update(1);
 	update_process_times(user_mode(get_irq_regs()));
 }
 
