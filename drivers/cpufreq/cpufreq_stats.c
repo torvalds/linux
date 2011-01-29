1aaf1b56b91ab0ea6160972f0250211419e8df94
Colin Cross <ccross@android.com>
2011-01-28 19:32:31 -0800

    cpufreq: Prevent memory leak in cpufreq_stats on hotplug
    Ensures that cpufreq_stats_free_table is called before
    __cpufreq_remove_dev on cpu hotplug (which also occurs during
    suspend on SMP systems) to make sure that sysfs_remove_group
    can get called before the cpufreq kobj is freed.  Otherwise,
    the sysfs file structures are leaked.
    
    Change-Id: I87e55277272f5cfad47e9e7c92630e990bb90069
    Signed-off-by: Colin Cross <ccross@android.com>

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -316,6 +316,27 @@ static int cpufreq_stat_notifier_trans(struct notifier_block *nb,
 	return 0;
 }
 
+static int cpufreq_stats_create_table_cpu(unsigned int cpu)
+{
+	struct cpufreq_policy *policy;
+	struct cpufreq_frequency_table *table;
+	int ret = -ENODEV;
+
+	policy = cpufreq_cpu_get(cpu);
+	if (!policy)
+		return -ENODEV;
+
+	table = cpufreq_frequency_get_table(cpu);
+	if (!table)
+		goto out;
+
+	ret = cpufreq_stats_create_table(policy, table);
+
+out:
+	cpufreq_cpu_put(policy);
+	return ret;
+}
+
 static int __cpuinit cpufreq_stat_cpu_callback(struct notifier_block *nfb,
 					       unsigned long action,
 					       void *hcpu)
@@ -333,6 +354,10 @@ static int __cpuinit cpufreq_stat_cpu_callback(struct notifier_block *nfb,
 	case CPU_DEAD:
 	case CPU_DEAD_FROZEN:
 		cpufreq_stats_free_table(cpu);
+		break;
+	case CPU_DOWN_FAILED:
+	case CPU_DOWN_FAILED_FROZEN:
+		cpufreq_stats_create_table_cpu(cpu);
 		break;
 	}
 	return NOTIFY_OK;
