429f4d8d20b91e4a6c239f951c06a56a6ac22957
Anton Blanchard <anton@samba.org>
2011-01-29 12:37:16 +0000

    powerpc/numa: Fix bug in unmap_cpu_from_node
    When converting to the new cpumask code I screwed up:
    
    -       if (cpu_isset(cpu, numa_cpumask_lookup_table[node])) {
    -               cpu_clear(cpu, numa_cpumask_lookup_table[node]);
    +       if (cpumask_test_cpu(cpu, node_to_cpumask_map[node])) {
    +               cpumask_set_cpu(cpu, node_to_cpumask_map[node]);
    
    This was introduced in commit 25863de07af9 (powerpc/cpumask: Convert NUMA code
    to new cpumask API)
    
    Fix it.
    
    Signed-off-by: Anton Blanchard <anton@samba.org>
    Cc: <stable@kernel.org>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/mm/numa.c
+++ arch/powerpc/mm/numa.c
@@ -186,7 +186,7 @@ static void unmap_cpu_from_node(unsigned long cpu)
 	dbg("removing cpu %lu from node %d\n", cpu, node);
 
 	if (cpumask_test_cpu(cpu, node_to_cpumask_map[node])) {
-		cpumask_set_cpu(cpu, node_to_cpumask_map[node]);
+		cpumask_clear_cpu(cpu, node_to_cpumask_map[node]);
 	} else {
 		printk(KERN_ERR "WARNING: cpu %lu not found in node %d\n",
 		       cpu, node);
