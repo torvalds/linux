a0aff623ca8bc8779e6c3a394772d70d8a65dee9
Gertjan van Wingerde <gwingerde@gmail.com>
2011-01-30 13:23:22 +0100

    rt2x00: Fix FIXME comments in rt61pci and rt73usb on Michael MIC.
    Both rt61pci and rt73usb check the Michael MIC in hardware and strip the
    Michael MIC from received frames. This is perfectly allowed by mac80211 as
    long as this is properly reported to mac80211.
    Both these drivers reported the Michael MIC handling properly to mac80211, but
    still contained a FIXME comment on this, which is not needed to be handled, since
    mac80211 doesn't really need the Michael MIC in this case.
    
    Signed-off-by: Gertjan van Wingerde <gwingerde@gmail.com>
    Signed-off-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt73usb.c
+++ drivers/net/wireless/rt2x00/rt73usb.c
@@ -1709,9 +1709,8 @@ static void rt73usb_fill_rxdone(struct queue_entry *entry,
 		rxdesc->flags |= RX_FLAG_IV_STRIPPED;
 
 		/*
-		 * FIXME: Legacy driver indicates that the frame does
-		 * contain the Michael Mic. Unfortunately, in rt2x00
-		 * the MIC seems to be missing completely...
+		 * The hardware has already checked the Michael Mic and has
+		 * stripped it from the frame. Signal this to mac80211.
 		 */
 		rxdesc->flags |= RX_FLAG_MMIC_STRIPPED;
 
