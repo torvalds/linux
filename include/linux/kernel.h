ffbbf2da9e578dc7b7ae4f945412c4b74f54b20e
Randy Dunlap <randy.dunlap@oracle.com>
2011-01-31 09:29:14 -0800

    kernel.h: fix kernel-doc warning
    Fix kernel-doc warning in kernel.h from commit 7ef88ad56145
    ("BUILD_BUG_ON: make it handle more cases"):
    
      Warning(include/linux/kernel.h:605): No description found for parameter 'condition'
      Warning(include/linux/kernel.h:605): Excess function parameter 'cond' description in 'BUILD_BUG_ON'
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -588,7 +588,7 @@ struct sysinfo {
 
 /**
  * BUILD_BUG_ON - break compile if a condition is true.
- * @cond: the condition which the compiler should know is false.
+ * @condition: the condition which the compiler should know is false.
  *
  * If you have some code which relies on certain constants being equal, or
  * other compile-time-evaluated condition, you should use BUILD_BUG_ON to
