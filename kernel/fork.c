77c100c83e84316ced2507c5799f79c2c80bc6b9
Rik van Riel <riel@redhat.com>
2011-02-01 09:51:46 -0500

    export pid symbols needed for kvm_vcpu_on_spin
    Export the symbols required for a race-free kvm_vcpu_on_spin.
    
    Signed-off-by: Rik van Riel <riel@redhat.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -193,6 +193,7 @@ void __put_task_struct(struct task_struct *tsk)
 	if (!profile_handoff_task(tsk))
 		free_task(tsk);
 }
+EXPORT_SYMBOL_GPL(__put_task_struct);
 
 /*
  * macro override instead of weak attribute alias, to workaround
