cdf2a4eeded69f66ec8c5098c375fe49e0820127
Naga Chumbalkar <nagananda.chumbalkar@hp.com>
2011-02-02 23:27:04 +0000

    ACPI: minor printk format change in acpi_pad
    Minor format change.
    
    Signed-off-by: Naga Chumbalkar <nagananda.chumbalkar@hp.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/acpi_pad.c
+++ drivers/acpi/acpi_pad.c
@@ -456,7 +456,7 @@ static void acpi_pad_notify(acpi_handle handle, u32 event,
 			dev_name(&device->dev), event, 0);
 		break;
 	default:
-		printk(KERN_WARNING"Unsupported event [0x%x]\n", event);
+		printk(KERN_WARNING "Unsupported event [0x%x]\n", event);
 		break;
 	}
 }
