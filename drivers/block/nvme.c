db5d0c198d673b6a932b449d4db95a2ad50c755e
Matthew Wilcox <matthew.r.wilcox@intel.com>
2011-02-03 14:36:07 -0500

    NVMe: Release 0.2
    Signed-off-by: Matthew Wilcox <matthew.r.wilcox@intel.com>

--- drivers/block/nvme.c
+++ drivers/block/nvme.c
@@ -1272,6 +1272,6 @@ static void __exit nvme_exit(void)
 
 MODULE_AUTHOR("Matthew Wilcox <willy@linux.intel.com>");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("0.1");
+MODULE_VERSION("0.2");
 module_init(nvme_init);
 module_exit(nvme_exit);
