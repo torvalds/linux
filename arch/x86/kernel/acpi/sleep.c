d344e38b2c151ca5e5e39f562017127e93912528
H. Peter Anvin <hpa@zytor.com>
2011-02-06 21:16:09 -0800

    x86, nx: Mark the ACPI resume trampoline code as +x
    We reserve lowmem for the things that need it, like the ACPI
    wakeup code, way early to guarantee availability.  This happens
    before we set up the proper pagetables, so set_memory_x() has no
    effect.
    
    Until we have a better solution, use an initcall to mark the
    wakeup code executable.
    
    Originally-by: Matthieu Castet <castet.matthieu@free.fr>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>
    Cc: Matthias Hopf <mhopf@suse.de>
    Cc: rjw@sisk.pl
    Cc: Suresh Siddha <suresh.b.siddha@intel.com>
    LKML-Reference: <4D4F8019.2090104@zytor.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/acpi/sleep.c
+++ arch/x86/kernel/acpi/sleep.c
@@ -12,10 +12,8 @@
 #include <linux/cpumask.h>
 #include <asm/segment.h>
 #include <asm/desc.h>
-
-#ifdef CONFIG_X86_32
 #include <asm/pgtable.h>
-#endif
+#include <asm/cacheflush.h>
 
 #include "realmode/wakeup.h"
 #include "sleep.h"
@@ -148,6 +146,15 @@ void __init acpi_reserve_wakeup_memory(void)
 	acpi_wakeup_address = mem;
 	memblock_x86_reserve_range(mem, mem + WAKEUP_SIZE, "ACPI WAKEUP");
 }
+
+int __init acpi_configure_wakeup_memory(void)
+{
+	if (acpi_realmode)
+		set_memory_x(acpi_realmode, WAKEUP_SIZE >> PAGE_SHIFT);
+
+	return 0;
+}
+arch_initcall(acpi_configure_wakeup_memory);
 
 
 static int __init acpi_sleep_setup(char *str)
