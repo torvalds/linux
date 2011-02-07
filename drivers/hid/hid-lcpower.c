8ef395310852710bb4237d918bbac53b22622ef0
Chris Schlund <chrisschlund@gmx.de>
2011-02-07 17:53:10 +0100

    HID: hid-lcpower: fix key mapping
    I already got some feedback for this module, telling me red and blue keys are
    exchanged.  I checked it, and they are right. Due to my incorrect userspace
    settings I missed this during my testing.
    
    Signed-off-by: Chris Schlund <chrisschlund@gmx.de>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-lcpower.c
+++ drivers/hid/hid-lcpower.c
@@ -30,8 +30,8 @@ static int ts_input_mapping(struct hid_device *hdev, struct hid_input *hi,
 	switch (usage->hid & HID_USAGE) {
         case 0x046: ts_map_key_clear(KEY_YELLOW);         break;
         case 0x047: ts_map_key_clear(KEY_GREEN);          break;
-        case 0x049: ts_map_key_clear(KEY_RED);            break;
-        case 0x04a: ts_map_key_clear(KEY_BLUE);           break;
+        case 0x049: ts_map_key_clear(KEY_BLUE);           break;
+        case 0x04a: ts_map_key_clear(KEY_RED);		  break;
         case 0x00d: ts_map_key_clear(KEY_HOME);           break;
         case 0x025: ts_map_key_clear(KEY_TV);             break;
         case 0x048: ts_map_key_clear(KEY_VCR);            break;
