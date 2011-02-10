2243c4d0727ad85aff3f54be9d178632cc9234b2
Clemens Ladisch <clemens@ladisch.de>
2011-02-10 16:16:32 +0100

    ALSA: hrtimer: remove superfluous tasklet invocation
    Commit bb758e9637e5ddc removed snd_hrtimer_callback() from the hardware
    interrupt handler, thus moving it into a tasklet, but did not tell the
    ALSA timer framework about this, so the timer handling would now be done
    in the ALSA timer tasklet scheduled from another tasklet.
    
    To fix this, add the flag to tell the ALSA timer framework that the
    timer handler is already being invoked in a tasklet.
    
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/hrtimer.c
+++ sound/core/hrtimer.c
@@ -105,7 +105,7 @@ static int snd_hrtimer_stop(struct snd_timer *t)
 }
 
 static struct snd_timer_hardware hrtimer_hw = {
-	.flags =	SNDRV_TIMER_HW_AUTO,
+	.flags =	SNDRV_TIMER_HW_AUTO | SNDRV_TIMER_HW_TASKLET,
 	.open =		snd_hrtimer_open,
 	.close =	snd_hrtimer_close,
 	.start =	snd_hrtimer_start,
