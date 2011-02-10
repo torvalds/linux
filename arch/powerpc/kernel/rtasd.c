9ff0c61d08ac4defa5ad6c65935a67643b8f4ce3
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2011-02-10 11:57:27 +0000

    powerpc: Mask smp_processor_id() false positive
    The rtas_event_scan() function uses smp_processor_id() to select a
    starting point in cpu_online_mask, and does so under the protection
    of get_online_cpus().  This might not select the current processor
    in any case, so switch to raw_smp_processor_id().
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/kernel/rtasd.c
+++ arch/powerpc/kernel/rtasd.c
@@ -412,7 +412,8 @@ static void rtas_event_scan(struct work_struct *w)
 
 	get_online_cpus();
 
-	cpu = cpumask_next(smp_processor_id(), cpu_online_mask);
+	/* raw_ OK because just using CPU as starting point. */
+	cpu = cpumask_next(raw_smp_processor_id(), cpu_online_mask);
         if (cpu >= nr_cpu_ids) {
 		cpu = cpumask_first(cpu_online_mask);
 
