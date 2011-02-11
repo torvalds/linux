e057a4b6e0eb6701f6ec923be2075d4984cef51a
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2011-02-11 17:55:13 +0000

    xen: fix compile issue if XEN is enabled but XEN_PVHVM is disabled
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

--- arch/x86/xen/suspend.c
+++ arch/x86/xen/suspend.c
@@ -28,6 +28,7 @@ void xen_pre_suspend(void)
 
 void xen_hvm_post_suspend(int suspend_cancelled)
 {
+#ifdef CONFIG_XEN_PVHVM
 	int cpu;
 	xen_hvm_init_shared_info();
 	xen_callback_vector();
@@ -37,6 +38,7 @@ void xen_hvm_post_suspend(int suspend_cancelled)
 			xen_setup_runstate_info(cpu);
 		}
 	}
+#endif
 }
 
 void xen_post_suspend(int suspend_cancelled)
