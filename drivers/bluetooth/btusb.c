e9036e336a8e5640871e0006ea4a89982b25046f
Cho, Yu-Chen <acho@novell.com>
2011-02-15 10:20:07 +0800

    Bluetooth: Add Atheros BT AR5BBU12 fw supported
    Add the btusb.c blacklist [0489:e02c] for Atheros AR5BBU12 BT
    and add to ath3k.c supported this device.
    
    Signed-off-by: Cho, Yu-Chen <acho@novell.com>
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -105,6 +105,9 @@ static struct usb_device_id blacklist_table[] = {
 	/* Atheros AR9285 Malbec with sflash firmware */
 	{ USB_DEVICE(0x03f0, 0x311d), .driver_info = BTUSB_IGNORE },
 
+	/* Atheros AR5BBU12 with sflash firmware */
+	{ USB_DEVICE(0x0489, 0xe02c), .driver_info = BTUSB_IGNORE },
+
 	/* Broadcom BCM2035 */
 	{ USB_DEVICE(0x0a5c, 0x2035), .driver_info = BTUSB_WRONG_SCO_MTU },
 	{ USB_DEVICE(0x0a5c, 0x200a), .driver_info = BTUSB_WRONG_SCO_MTU },
