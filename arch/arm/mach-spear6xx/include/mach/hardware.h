f9324a85c10ee109022672ec72db9e2eb37050ee
viresh kumar <viresh.kumar@st.com>
2011-02-16 07:40:28 +0100

    ARM: 6677/1: SPEAr: add IOMEM(x) definition and update declaration of MISC_BASE
    Add IOMEM(x) definition, and use it with MISC_BASE for SPEAr platform.
    With this there is no need to typecast misc macros to (unsigned int *).
    
    Reviewed-by: Stanley Miao <stanley.miao@windriver.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@st.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-spear6xx/include/mach/hardware.h
+++ arch/arm/mach-spear6xx/include/mach/hardware.h
@@ -14,8 +14,9 @@
 #ifndef __MACH_HARDWARE_H
 #define __MACH_HARDWARE_H
 
+#include <plat/hardware.h>
+
 /* Vitual to physical translation of statically mapped space */
 #define IO_ADDRESS(x)		(x | 0xF0000000)
 
 #endif /* __MACH_HARDWARE_H */
-
