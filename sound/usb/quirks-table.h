e19869204fca4ae28b6e4d8f5e20849e9f7b18bd
Clemens Ladisch <clemens@ladisch.de>
2011-02-17 14:26:51 +0100

    ALSA: usb-audio: add Cakewalk UM-1G support
    Add a quirk for the Cakewalk UM-1G USB MIDI interface in
    "advanced driver" mode.  (It already works in standard mode.)
    
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/usb/quirks-table.h
+++ sound/usb/quirks-table.h
@@ -1568,6 +1568,19 @@ YAMAHA_DEVICE(0x7010, "UB99"),
 	}
 },
 {
+	USB_DEVICE_VENDOR_SPEC(0x0582, 0x0104),
+	.driver_info = (unsigned long) & (const struct snd_usb_audio_quirk) {
+		/* .vendor_name = "Roland", */
+		/* .product_name = "UM-1G", */
+		.ifnum = 0,
+		.type = QUIRK_MIDI_FIXED_ENDPOINT,
+		.data = & (const struct snd_usb_midi_endpoint_info) {
+			.out_cables = 0x0001,
+			.in_cables  = 0x0001
+		}
+	}
+},
+{
 	/* has ID 0x0110 when not in Advanced Driver mode */
 	USB_DEVICE_VENDOR_SPEC(0x0582, 0x010f),
 	.driver_info = (unsigned long) & (const struct snd_usb_audio_quirk) {
