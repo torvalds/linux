db28569adc692d9fb8a2d2d8e7ebab7fd5481f10
Vivek Natarajan <vnatarajan@atheros.com>
2011-02-18 17:18:03 +0530

    mac80211: Clear PS related flag on disabling power save.
    Clear IEEE80211_STA_NULLFUNC_ACKED flag on disabling power
    save. Without this fix, there is a chance of setting CONF_PS
    before sending nullfunc frame.
    
    Signed-off-by: Vivek Natarajan <vnatarajan@atheros.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/tx.c
+++ net/mac80211/tx.c
@@ -236,6 +236,7 @@ ieee80211_tx_h_dynamic_ps(struct ieee80211_tx_data *tx)
 	if (local->hw.conf.flags & IEEE80211_CONF_PS) {
 		ieee80211_stop_queues_by_reason(&local->hw,
 						IEEE80211_QUEUE_STOP_REASON_PS);
+		ifmgd->flags &= ~IEEE80211_STA_NULLFUNC_ACKED;
 		ieee80211_queue_work(&local->hw,
 				     &local->dynamic_ps_disable_work);
 	}
