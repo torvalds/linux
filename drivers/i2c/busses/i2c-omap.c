a5a595cc36bbbe16f6a3f0e6968f94a0604bfd28
Ben Dooks <ben-linux@fluff.org>
2011-02-23 00:43:55 +0000

    i2c-omap: fixup commit cb527ede1bf6ff2008a025606f25344b8ed7b4ac whitespace
    Fixup the whitespace error noticed in cb527ede1bf6ff2008a025606f25344b8ed7b4ac
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- drivers/i2c/busses/i2c-omap.c
+++ drivers/i2c/busses/i2c-omap.c
@@ -847,9 +847,9 @@ complete:
 			dev_err(dev->dev, "Arbitration lost\n");
 			err |= OMAP_I2C_STAT_AL;
 		}
-		/* 
+		/*
 		 * ProDB0017052: Clear ARDY bit twice
-		 */ 
+		 */
 		if (stat & (OMAP_I2C_STAT_ARDY | OMAP_I2C_STAT_NACK |
 					OMAP_I2C_STAT_AL)) {
 			omap_i2c_ack_stat(dev, stat &
