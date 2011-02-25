4e59df8f25950520cd6a8b16a39d48b5fa5aeeda
Igor M. Liplianin <liplianin@me.by>
2011-02-25 18:41:23 -0300

    [media] dw2102: Prof 7500 remote fix
    Signed-off-by: Igor M. Liplianin <liplianin@me.by>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/dvb-usb/dw2102.c
+++ drivers/media/dvb/dvb-usb/dw2102.c
@@ -1118,11 +1118,16 @@ static int ds3000_frontend_attach(struct dvb_usb_adapter *d)
 
 static int prof_7500_frontend_attach(struct dvb_usb_adapter *d)
 {
+	u8 obuf[] = {7, 1};
+
 	d->fe = dvb_attach(stv0900_attach, &prof_7500_stv0900_config,
 					&d->dev->i2c_adap, 0);
 	if (d->fe == NULL)
 		return -EIO;
+
 	d->fe->ops.set_voltage = dw210x_set_voltage;
+
+	dw210x_op_rw(d->dev->udev, 0x8a, 0, 0, obuf, 2, DW210X_WRITE_MSG);
 
 	info("Attached STV0900+STB6100A!\n");
 
