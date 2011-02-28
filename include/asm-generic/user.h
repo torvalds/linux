9b4f06fae48765650f3ac9c876bc629d804bd763
Tobias Klauser <tklauser@distanz.ch>
2011-02-28 10:32:22 +0100

    asm-generic/user.h: Fix spelling in comment
    Fix two misspellings.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- include/asm-generic/user.h
+++ include/asm-generic/user.h
@@ -1,8 +1,8 @@
 #ifndef __ASM_GENERIC_USER_H
 #define __ASM_GENERIC_USER_H
 /*
- * This file may define a 'struct user' structure. However, it it only
- * used for a.out file, which are not supported on new architectures.
+ * This file may define a 'struct user' structure. However, it is only
+ * used for a.out files, which are not supported on new architectures.
  */
 
 #endif	/* __ASM_GENERIC_USER_H */
