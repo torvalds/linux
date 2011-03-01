e8b2c3c47a53348aebbbeb5322e32937df958793
Chris Wilson <chris@chris-wilson.co.uk>
2011-03-01 19:22:52 +0000

    drm/i915: Re-enable GPU semaphores for SandyBridge mobile
    This seems to be running stably on my test laptop, so hopefully the
    reported hangs where just symptoms of other bugs.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/i915_gem_execbuffer.c
+++ drivers/gpu/drm/i915/i915_gem_execbuffer.c
@@ -744,8 +744,7 @@ i915_gem_execbuffer_sync_rings(struct drm_i915_gem_object *obj,
 	if (from == NULL || to == from)
 		return 0;
 
-	/* XXX gpu semaphores are currently causing hard hangs on SNB mobile */
-	if (INTEL_INFO(obj->base.dev)->gen < 6 || IS_MOBILE(obj->base.dev))
+	if (INTEL_INFO(obj->base.dev)->gen < 6)
 		return i915_gem_object_wait_rendering(obj);
 
 	idx = intel_ring_sync_index(from, to);
