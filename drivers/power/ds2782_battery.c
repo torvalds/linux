84ab16f54bbde67c8c2aabc36b034c91044935cb
Axel Lin <axel.lin@gmail.com>
2011-03-01 17:23:34 +0800

    ds2782_battery: Add MODULE_DEVICE_TABLE
    The device table is required to load modules based on modaliases.
    After adding the MODULE_DEVICE_TABLE, below entries will be added to
    modules.alias:
    alias i2c:ds2786 ds2782_battery
    alias i2c:ds2782 ds2782_battery
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Signed-off-by: Anton Vorontsov <cbouatmailru@gmail.com>

--- drivers/power/ds2782_battery.c
+++ drivers/power/ds2782_battery.c
@@ -393,6 +393,7 @@ static const struct i2c_device_id ds278x_id[] = {
 	{"ds2786", DS2786},
 	{},
 };
+MODULE_DEVICE_TABLE(i2c, ds278x_id);
 
 static struct i2c_driver ds278x_battery_driver = {
 	.driver 	= {
