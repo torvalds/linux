ba5c312980bfa127ceb5d913ab31e4bfdc364e08
Greg Kroah-Hartman <gregkh@suse.de>
2011-03-03 08:23:19 -0800

    Revert "swiotlb: fix wrong panic"
    This reverts commit 484d82b6e2e4239ba7a722e0c532e9aff64be51a.
    
    It caused build problems on some architectures and was already asked to
    be removed from the queue.  It was my fault for incorrectly applying it.
    
    Reported-by: Shawn Bohrer <shawn.bohrer@gmail.co
    Reported-by: Stefan Bader <stefan.bader@canonical.com>
    Reported-by: David Engel <david@istwok.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- lib/swiotlb.c
+++ lib/swiotlb.c
@@ -631,10 +631,8 @@ dma_addr_t swiotlb_map_page(struct device *dev, struct page *page,
 	/*
 	 * Ensure that the address returned is DMA'ble
 	 */
-	if (!dma_capable(dev, dev_addr, size)) {
-		swiotlb_tbl_unmap_single(dev, map, size, dir);
-		dev_addr = swiotlb_virt_to_bus(dev, io_tlb_overflow_buffer);
-	}
+	if (!dma_capable(dev, dev_addr, size))
+		panic("map_single: bounce buffer is not DMA'ble");
 
 	return dev_addr;
 }
