9b9f93da1fdb133a2de88c22670d6c39b3634eb5
Greg Kroah-Hartman <gregkh@suse.de>
2011-03-02 17:49:30 -0800

    Staging: hv: hv_mouse: fix up copyright and license header
    Use the proper license header from the other hv drivers and remove the
    nonsense about derivative works, as it's rubbish.
    
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Cc: K. Y. Srinivasan <kys@microsoft.com>
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/hv_mouse.c
+++ drivers/staging/hv/hv_mouse.c
@@ -1,27 +1,16 @@
 /*
- *  Copyright 2009 Citrix Systems, Inc.
+ *  Copyright (c) 2009, Citrix Systems, Inc.
+ *  Copyright (c) 2010, Microsoft Corporation.
+ *  Copyright (c) 2011, Novell Inc.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms and conditions of the GNU General Public License,
+ *  version 2, as published by the Free Software Foundation.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- *  For clarity, the licensor of this program does not intend that a
- *  "derivative work" include code which compiles header information from
- *  this program.
- *
- *  This code has been modified from its original by
- *  Hank Janssen <hjanssen@microsoft.com>
- *
+ *  This program is distributed in the hope it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ *  more details.
  */
 #include <linux/init.h>
 #include <linux/module.h>
