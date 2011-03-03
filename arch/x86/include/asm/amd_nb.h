84fd1d35cc868a4f7590b6dbdae2d7761287b97a
Borislav Petkov <bp@amd64.org>
2011-03-03 12:59:32 +0100

    x86, amd-nb: Misc cleanliness fixes
    Make functions used strictly in bool context return bool. Also,
    fixup used types and comments, and make a local function static,
    while at it.
    
    Signed-off-by: Borislav Petkov <borislav.petkov@amd.com>
    Cc: Borislav Petkov <bp@amd64.org>
    LKML-Reference: <20110303115932.GA8603@aftab>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/amd_nb.h
+++ arch/x86/include/asm/amd_nb.h
@@ -13,7 +13,7 @@ extern const struct pci_device_id amd_nb_misc_ids[];
 extern const struct amd_nb_bus_dev_range amd_nb_bus_dev_ranges[];
 struct bootnode;
 
-extern int early_is_amd_nb(u32 value);
+extern bool early_is_amd_nb(u32 value);
 extern int amd_cache_northbridges(void);
 extern void amd_flush_garts(void);
 extern int amd_numa_init(unsigned long start_pfn, unsigned long end_pfn);
@@ -38,18 +38,18 @@ struct amd_northbridge_info {
 };
 extern struct amd_northbridge_info amd_northbridges;
 
-#define AMD_NB_GART			0x1
-#define AMD_NB_L3_INDEX_DISABLE		0x2
-#define AMD_NB_L3_PARTITIONING		0x4
+#define AMD_NB_GART			BIT(0)
+#define AMD_NB_L3_INDEX_DISABLE		BIT(1)
+#define AMD_NB_L3_PARTITIONING		BIT(2)
 
 #ifdef CONFIG_AMD_NB
 
-static inline int amd_nb_num(void)
+static inline u16 amd_nb_num(void)
 {
 	return amd_northbridges.num;
 }
 
-static inline int amd_nb_has_feature(int feature)
+static inline bool amd_nb_has_feature(unsigned feature)
 {
 	return ((amd_northbridges.flags & feature) == feature);
 }
