ae6b585eeb74670a2dec1fe4394bdfbdb9395cc2
Oleg Nesterov <oleg@redhat.com>
2011-03-06 18:03:11 +0100

    exec: document acct_arg_size()
    Add the comment to explain acct_arg_size().
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Reviewed-by: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>

--- fs/exec.c
+++ fs/exec.c
@@ -167,7 +167,12 @@ out:
 }
 
 #ifdef CONFIG_MMU
-
+/*
+ * The nascent bprm->mm is not visible until exec_mmap() but it can
+ * use a lot of memory, account these pages in current->mm temporary
+ * for oom_badness()->get_mm_rss(). Once exec succeeds or fails, we
+ * change the counter back via acct_arg_size(0).
+ */
 static void acct_arg_size(struct linux_binprm *bprm, unsigned long pages)
 {
 	struct mm_struct *mm = current->mm;
