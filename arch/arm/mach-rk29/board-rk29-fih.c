c36bc66d8a5c14e8c7dc3649e4458990ea63ffd8
ddl <ddl@rockchip.com>
2011-03-07 17:57:13 +0800

    camera: support front sensor

--- arch/arm/mach-rk29/board-rk29-fih.c
+++ arch/arm/mach-rk29/board-rk29-fih.c
@@ -1029,7 +1029,7 @@ static struct i2c_board_info __initdata board_i2c3_devices[] = {
  *****************************************************************************************/
 #ifdef CONFIG_VIDEO_RK29
 #define SENSOR_NAME_0 RK29_CAM_SENSOR_NAME_MT9P111         /* back camera sensor */
-#define SENSOR_IIC_ADDR_0 	    0x78
+#define SENSOR_IIC_ADDR_0 	    0x00
 #define SENSOR_IIC_ADAPTER_ID_0    1
 #define SENSOR_POWER_PIN_0         RK29_PIN5_PD7
 #define SENSOR_RESET_PIN_0         INVALID_GPIO
@@ -1041,10 +1041,10 @@ static struct i2c_board_info __initdata board_i2c3_devices[] = {
 #define SENSOR_FLASHACTIVE_LEVEL_0 RK29_CAM_FLASHACTIVE_L
 
 #define SENSOR_NAME_1 RK29_CAM_SENSOR_NAME_S5K6AA          /* front camera sensor */
-#define SENSOR_IIC_ADDR_1 	    0x00
+#define SENSOR_IIC_ADDR_1 	    0x78
 #define SENSOR_IIC_ADAPTER_ID_1    1
 #define SENSOR_POWER_PIN_1         INVALID_GPIO
-#define SENSOR_RESET_PIN_1         INVALID_GPIO
+#define SENSOR_RESET_PIN_1         RK29_PIN1_PB3
 #define SENSOR_POWERDN_PIN_1       RK29_PIN6_PB7
 #define SENSOR_FALSH_PIN_1         INVALID_GPIO
 #define SENSOR_POWERACTIVE_LEVEL_1 RK29_CAM_POWERACTIVE_L
