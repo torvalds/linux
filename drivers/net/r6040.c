86f99fffffc631b8fa03d07219dc0c67bb65893d
Florian Fainelli <florian@openwrt.org>
2011-03-07 00:09:42 +0000

    r6040: bump to version 0.27 and date 23Feb2011
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/r6040.c
+++ drivers/net/r6040.c
@@ -49,8 +49,8 @@
 #include <asm/processor.h>
 
 #define DRV_NAME	"r6040"
-#define DRV_VERSION	"0.26"
-#define DRV_RELDATE	"30May2010"
+#define DRV_VERSION	"0.27"
+#define DRV_RELDATE	"23Feb2011"
 
 /* PHY CHIP Address */
 #define PHY1_ADDR	1	/* For MAC1 */
