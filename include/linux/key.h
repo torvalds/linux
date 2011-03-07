633e804e89464d3875e59de1959a53f9041d3094
David Howells <dhowells@redhat.com>
2011-03-07 15:05:51 +0000

    KEYS: Add an RCU payload dereference macro
    Add an RCU payload dereference macro as this seems to be a common piece of code
    amongst key types that use RCU referenced payloads.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Mimi Zohar <zohar@us.ibm.com>
    Signed-off-by: James Morris <jmorris@namei.org>

--- include/linux/key.h
+++ include/linux/key.h
@@ -275,6 +275,10 @@ static inline key_serial_t key_serial(struct key *key)
 	return key ? key->serial : 0;
 }
 
+#define rcu_dereference_key(KEY)					\
+	(rcu_dereference_protected((KEY)->payload.rcudata,		\
+				   rwsem_is_locked(&((struct key *)(KEY))->sem)))
+
 #ifdef CONFIG_SYSCTL
 extern ctl_table key_sysctls[];
 #endif
