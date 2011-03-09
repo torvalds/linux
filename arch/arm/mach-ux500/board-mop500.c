dfa3a824de631e6833ffa9a7befc08186b027799
Bengt Jonsson <bengt.g.jonsson@stericsson.com>
2011-03-09 13:34:17 +0100

    mach-ux500: provide ab8500 init vector
    This adds an ab8500 regulator initialization vector for the
    HREF/MOP500 series of boards. This also sets the display
    regulator to be on at boot so we don't loose our splash
    screen when the board comes up.
    
    Signed-off-by: Bengt Jonsson <bengt.g.jonsson@stericsson.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/board-mop500.c
+++ arch/arm/mach-ux500/board-mop500.c
@@ -67,6 +67,8 @@ static struct ab8500_gpio_platform_data ab8500_gpio_pdata = {
 
 static struct ab8500_platform_data ab8500_platdata = {
 	.irq_base	= MOP500_AB8500_IRQ_BASE,
+	.regulator_reg_init = ab8500_regulator_reg_init,
+	.num_regulator_reg_init	= ARRAY_SIZE(ab8500_regulator_reg_init),
 	.regulator	= ab8500_regulators,
 	.num_regulator	= ARRAY_SIZE(ab8500_regulators),
 	.gpio		= &ab8500_gpio_pdata,
