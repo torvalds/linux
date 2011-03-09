0a33443b38746f35fc4acc8a5af6c7099e03ea40
Steven Whitehouse <swhiteho@redhat.com>
2011-03-09 11:14:32 +0000

    GFS2: Remove potential race in flock code
    This patch ensures that we always wait for glock demotion when
    dropping flocks on a file in order to prevent any race
    conditions associated with further flock calls or closing
    the file.
    
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/file.c
+++ fs/gfs2/file.c
@@ -981,8 +981,10 @@ static void do_unflock(struct file *file, struct file_lock *fl)
 
 	mutex_lock(&fp->f_fl_mutex);
 	flock_lock_file_wait(file, fl);
-	if (fl_gh->gh_gl)
-		gfs2_glock_dq_uninit(fl_gh);
+	if (fl_gh->gh_gl) {
+		gfs2_glock_dq_wait(fl_gh);
+		gfs2_holder_uninit(fl_gh);
+	}
 	mutex_unlock(&fp->f_fl_mutex);
 }
 
