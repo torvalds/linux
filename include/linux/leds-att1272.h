63415cbc223d11e56b9431fa3ece38dd87d0f60f
ddl <ddl@rockchip.com>
2011-03-11 23:52:26 +0800

    leds : add support att1272 white led driver for camera flash led

---
+++ include/linux/leds-att1272.h
@@ -0,0 +1,23 @@
+/*
+ * leds-att1272.c -  LED Driver
+ *
+ * Copyright (C) 2011 Rockchips
+ * deng dalong <ddl@rock-chips.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Datasheet: http://www.rohm.com/products/databook/driver/pdf/att1272gu-e.pdf
+ *
+ */
+#ifndef _LEDS_ATT1272_H_
+#define _LEDS_ATT1272_H_
+
+struct att1272_led_platform_data {
+	const char		*name;
+	int	en_gpio;
+	int flen_gpio;
+};
+
+#endif /* _LEDS_ATT1272_H_ */
