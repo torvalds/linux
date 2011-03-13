dd12cd10cf74fe41ecd2a683b6c90cbc4f528884
Jiri Slaby <jslaby@suse.cz>
2011-03-13 06:54:31 +0000

    NET: cdc-phonet, handle empty phonet header
    commit 468c3f924f043cad7a04f4f4d5224a2c9bc886c1 upstream.
    
    Currently, for N 5800 XM I get:
    cdc_phonet: probe of 1-6:1.10 failed with error -22
    
    It's because phonet_header is empty. Extra altsetting looks like
    there:
    E 05 24 00 01 10 03 24 ab 05 24 06 0a 0b 04 24 fd  .$....$..$....$.
    E 00                                               .
    
    I don't see the header used anywhere so just check if the phonet
    descriptor is there, not the structure itself.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Cc: Rémi Denis-Courmont <remi.denis-courmont@nokia.com>
    Cc: David S. Miller <davem@davemloft.net>
    Acked-by: Rémi Denis-Courmont <remi.denis-courmont@nokia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/net/usb/cdc-phonet.c
+++ drivers/net/usb/cdc-phonet.c
@@ -325,13 +325,13 @@ int usbpn_probe(struct usb_interface *intf, const struct usb_device_id *id)
 {
 	static const char ifname[] = "usbpn%d";
 	const struct usb_cdc_union_desc *union_header = NULL;
-	const struct usb_cdc_header_desc *phonet_header = NULL;
 	const struct usb_host_interface *data_desc;
 	struct usb_interface *data_intf;
 	struct usb_device *usbdev = interface_to_usbdev(intf);
 	struct net_device *dev;
 	struct usbpn_dev *pnd;
 	u8 *data;
+	int phonet = 0;
 	int len, err;
 
 	data = intf->altsetting->extra;
@@ -352,10 +352,7 @@ int usbpn_probe(struct usb_interface *intf, const struct usb_device_id *id)
 					(struct usb_cdc_union_desc *)data;
 				break;
 			case 0xAB:
-				if (phonet_header || dlen < 5)
-					break;
-				phonet_header =
-					(struct usb_cdc_header_desc *)data;
+				phonet = 1;
 				break;
 			}
 		}
@@ -363,7 +360,7 @@ int usbpn_probe(struct usb_interface *intf, const struct usb_device_id *id)
 		len -= dlen;
 	}
 
-	if (!union_header || !phonet_header)
+	if (!union_header || !phonet)
 		return -EINVAL;
 
 	data_intf = usb_ifnum_to_if(usbdev, union_header->bSlaveInterface0);
