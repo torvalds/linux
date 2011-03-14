ce5b2acce60405b938d1f1f994024cde4e2cdd7e
Jean-François Moine <moinejf@free.fr>
2011-03-14 08:49:28 -0300

    [media] gspca - nw80x: New subdriver for Divio based webcams
    [mchehab@redhat.com: Fix a few CodingStyle issues]
    Tested-by: Kjell Claesson <kjell.claesson@epost.tidanet.se>
    Tested-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Jean-François Moine <moinejf@free.fr>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/linux/videodev2.h
+++ include/linux/videodev2.h
@@ -396,6 +396,7 @@ struct v4l2_pix_format {
 #define V4L2_PIX_FMT_TM6000   v4l2_fourcc('T', 'M', '6', '0') /* tm5600/tm60x0 */
 #define V4L2_PIX_FMT_CIT_YYVYUY v4l2_fourcc('C', 'I', 'T', 'V') /* one line of Y then 1 line of VYUY */
 #define V4L2_PIX_FMT_KONICA420  v4l2_fourcc('K', 'O', 'N', 'I') /* YUV420 planar in blocks of 256 pixels */
+#define V4L2_PIX_FMT_JPGL	v4l2_fourcc('J', 'P', 'G', 'L') /* JPEG-Lite */
 
 /*
  *	F O R M A T   E N U M E R A T I O N
