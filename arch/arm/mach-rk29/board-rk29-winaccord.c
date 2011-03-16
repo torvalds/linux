00b5e193e5ae8c3ebbf9996341794ea6924d338f
黄涛 <huangtao@rock-chips.com>
2011-03-16 16:57:05 +0800

    rk29: board-* call rk29_setup_early_printk

--- arch/arm/mach-rk29/board-rk29-winaccord.c
+++ arch/arm/mach-rk29/board-rk29-winaccord.c
@@ -1720,6 +1720,7 @@ static void __init machine_rk29_fixup(struct machine_desc *desc, struct tag *tag
 static void __init machine_rk29_mapio(void)
 {
 	rk29_map_common_io();
+	rk29_setup_early_printk();
 	rk29_sram_init();
 	rk29_clock_init();
 	rk29_iomux_init();
