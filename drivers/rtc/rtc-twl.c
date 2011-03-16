dec35d19c4ec65b94df3b27b6e373f0d48c9cd32
Ilkka Koskinen <ilkka.koskinen@nokia.com>
2011-03-16 06:07:14 +0000

    rtc: rtc-twl: Switch to using threaded irq
    The driver is accessing to i2c bus in interrupt handler.
    Therefore, it should use threaded irq.
    
    Signed-off-by: Ilkka Koskinen <ilkka.koskinen@nokia.com>
    Acked-by: Balaji T K <balajitk@ti.com>
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- drivers/rtc/rtc-twl.c
+++ drivers/rtc/rtc-twl.c
@@ -462,7 +462,7 @@ static int __devinit twl_rtc_probe(struct platform_device *pdev)
 	if (ret < 0)
 		goto out1;
 
-	ret = request_irq(irq, twl_rtc_interrupt,
+	ret = request_threaded_irq(irq, NULL, twl_rtc_interrupt,
 				IRQF_TRIGGER_RISING,
 				dev_name(&rtc->dev), rtc);
 	if (ret < 0) {
