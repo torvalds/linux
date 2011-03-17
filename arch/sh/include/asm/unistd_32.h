a88403335a9ffc66a0a1b46b6d303512eddde846
Paul Mundt <lethal@linux-sh.org>
2011-03-17 17:01:24 +0900

    sh: Wire up new fhandle and clock_adjtime syscalls.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/unistd_32.h
+++ arch/sh/include/asm/unistd_32.h
@@ -369,8 +369,11 @@
 #define __NR_recvmsg		356
 #define __NR_recvmmsg		357
 #define __NR_accept4		358
+#define __NR_name_to_handle_at	359
+#define __NR_open_by_handle_at	360
+#define __NR_clock_adjtime	361
 
-#define NR_syscalls 359
+#define NR_syscalls 362
 
 #ifdef __KERNEL__
 
