29096202176ceaa5016a17ea2dd1aea19a4e90e2
Steven Rostedt <srostedt@redhat.com>
2011-03-17 15:21:07 -0400

    futex: Fix WARN_ON() test for UP
    An update of the futex code had a
    
    	WARN_ON(!spin_is_locked(q->lock_ptr))
    
    But on UP, spin_is_locked() is always false, and will
    trigger this warning, and even worse, it will exit the function
    without doing the necessary work.
    
    Converting this to a WARN_ON_SMP() fixes the problem.
    
    Reported-by: Richard Weinberger <richard@nod.at>
    Tested-by: Richard Weinberger <richard@nod.at>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Acked-by: Darren Hart <dvhart@linux.intel.com>
    Cc: Lai Jiangshan <laijs@cn.fujitsu.com>
    LKML-Reference: <20110317192208.682654502@goodmis.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/futex.c
+++ kernel/futex.c
@@ -782,8 +782,8 @@ static void __unqueue_futex(struct futex_q *q)
 {
 	struct futex_hash_bucket *hb;
 
-	if (WARN_ON(!q->lock_ptr || !spin_is_locked(q->lock_ptr)
-			|| plist_node_empty(&q->list)))
+	if (WARN_ON_SMP(!q->lock_ptr || !spin_is_locked(q->lock_ptr))
+	    || WARN_ON(plist_node_empty(&q->list)))
 		return;
 
 	hb = container_of(q->lock_ptr, struct futex_hash_bucket, lock);
