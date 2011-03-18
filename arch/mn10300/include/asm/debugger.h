792576b1c5a4da16700f1003b1c3fc1e22cd9e07
David Howells <dhowells@redhat.com>
2011-03-18 16:54:30 +0000

    MN10300: Introduce a general config option for kernel debugger hooks
    Introduce a general config option for kernel debugger hooks so that both
    gdbstub and kgdb can use it and add a header file for both debuggers to use.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

---
+++ arch/mn10300/include/asm/debugger.h
@@ -0,0 +1,20 @@
+/* Kernel debugger for MN10300
+ *
+ * Copyright (C) 2011 Red Hat, Inc. All Rights Reserved.
+ * Written by David Howells (dhowells@redhat.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public Licence
+ * as published by the Free Software Foundation; either version
+ * 2 of the Licence, or (at your option) any later version.
+ */
+
+#ifndef _ASM_DEBUGGER_H
+#define _ASM_DEBUGGER_H
+
+#if defined(CONFIG_KERNEL_DEBUGGER)
+
+#else /* CONFIG_KERNEL_DEBUGGER */
+
+#endif /* CONFIG_KERNEL_DEBUGGER */
+#endif /* _ASM_DEBUGGER_H */
