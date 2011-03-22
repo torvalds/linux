c7a1fcd8e6e0c3c8f4f8f74fc926ff04da3bf7a7
Andrew Morton <akpm@linux-foundation.org>
2011-03-22 16:30:07 -0700

    include/asm-generic/unistd.h: fix syncfs syscall number
    syncfs() is duplicating name_to_handle_at() due to a merging mistake.
    
    Cc: Sage Weil <sage@newdream.net>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-generic/unistd.h
+++ include/asm-generic/unistd.h
@@ -652,7 +652,7 @@ __SYSCALL(__NR_name_to_handle_at, sys_name_to_handle_at)
 __SYSCALL(__NR_open_by_handle_at, sys_open_by_handle_at)
 #define __NR_clock_adjtime 266
 __SYSCALL(__NR_clock_adjtime, sys_clock_adjtime)
-#define __NR_syncfs 264
+#define __NR_syncfs 267
 __SYSCALL(__NR_syncfs, sys_syncfs)
 
 #undef __NR_syscalls
