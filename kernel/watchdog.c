fef2c9bc1b54c0261324a96e948c0b849796e896
Don Zickus <dzickus@redhat.com>
2011-03-22 16:34:16 -0700

    kernel/watchdog.c: allow hardlockup to panic by default
    When a cpu is considered stuck, instead of limping along and just printing
    a warning, it is sometimes preferred to just panic, let kdump capture the
    vmcore and reboot.  This gets the machine back into a stable state quickly
    while saving the info that got it into a stuck state to begin with.
    
    Add a Kconfig option to allow users to set the hardlockup to panic
    by default.  Also add in a 'nmi_watchdog=nopanic' to override this.
    
    [akpm@linux-foundation.org: fix strncmp length]
    Signed-off-by: Don Zickus <dzickus@redhat.com>
    Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Reviewed-by: WANG Cong <xiyou.wangcong@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/watchdog.c
+++ kernel/watchdog.c
@@ -48,12 +48,15 @@ static DEFINE_PER_CPU(struct perf_event *, watchdog_ev);
  * Should we panic when a soft-lockup or hard-lockup occurs:
  */
 #ifdef CONFIG_HARDLOCKUP_DETECTOR
-static int hardlockup_panic;
+static int hardlockup_panic =
+			CONFIG_BOOTPARAM_HARDLOCKUP_PANIC_VALUE;
 
 static int __init hardlockup_panic_setup(char *str)
 {
 	if (!strncmp(str, "panic", 5))
 		hardlockup_panic = 1;
+	else if (!strncmp(str, "nopanic", 7))
+		hardlockup_panic = 0;
 	else if (!strncmp(str, "0", 1))
 		watchdog_enabled = 0;
 	return 1;
