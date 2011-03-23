f2ee4ae87d7781ddb5bf8da25591b9b79966f8ea
Jiri Slaby <jslaby@suse.cz>
2011-03-23 09:49:57 +0100

    TTY: VT, remove unused variables
    drivers/tty/vt/vt.c:892:2: warning: Value stored to 'old_screen_size' is never read
            old_screen_size = vc->vc_screenbuf_size;
            ^                 ~~~~~~~~~~~~~~~~~~~~~
    drivers/tty/vt/vt.c:890:2: warning: Value stored to 'old_cols' is never read
    	old_cols = vc->vc_cols;
    	^          ~~~~~~~~~~~
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/tty/vt/vt.c
+++ drivers/tty/vt/vt.c
@@ -858,7 +858,7 @@ static int vc_do_resize(struct tty_struct *tty, struct vc_data *vc,
 {
 	unsigned long old_origin, new_origin, new_scr_end, rlth, rrem, err = 0;
 	unsigned long end;
-	unsigned int old_cols, old_rows, old_row_size, old_screen_size;
+	unsigned int old_rows, old_row_size;
 	unsigned int new_cols, new_rows, new_row_size, new_screen_size;
 	unsigned int user;
 	unsigned short *newscreen;
@@ -887,9 +887,7 @@ static int vc_do_resize(struct tty_struct *tty, struct vc_data *vc,
 		return -ENOMEM;
 
 	old_rows = vc->vc_rows;
-	old_cols = vc->vc_cols;
 	old_row_size = vc->vc_size_row;
-	old_screen_size = vc->vc_screenbuf_size;
 
 	err = resize_screen(vc, new_cols, new_rows, user);
 	if (err) {
