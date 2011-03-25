ef550f6f4f6c9345a27ec85d98f4f7de1adce79c
Sage Weil <sage@newdream.net>
2011-03-25 13:27:48 -0700

    ceph: flush msgr_wq during mds_client shutdown
    The release method for mds connections uses a backpointer to the
    mds_client, so we need to flush the workqueue of any pending work (and
    ceph_connection references) prior to freeing the mds_client.  This fixes
    an oops easily triggered under UML by
    
     while true ; do mount ... ; umount ... ; done
    
    Also fix an outdated comment: the flush in ceph_destroy_client only flushes
    OSD connections out.  This bug is basically an artifact of the ceph ->
    ceph+libceph conversion.
    
    Signed-off-by: Sage Weil <sage@newdream.net>

--- net/ceph/ceph_common.c
+++ net/ceph/ceph_common.c
@@ -394,8 +394,8 @@ void ceph_destroy_client(struct ceph_client *client)
 	ceph_osdc_stop(&client->osdc);
 
 	/*
-	 * make sure mds and osd connections close out before destroying
-	 * the auth module, which is needed to free those connections'
+	 * make sure osd connections close out before destroying the
+	 * auth module, which is needed to free those connections'
 	 * ceph_authorizers.
 	 */
 	ceph_msgr_flush();
