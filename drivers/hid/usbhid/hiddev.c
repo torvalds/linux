ac065bf214bb6a7fb7536f2dde686d4694342801
Dan Carpenter <error27@gmail.com>
2011-03-26 04:47:35 +0300

    HID: hiddev: fix brace indent
    There was an extra tab so the close curly brace didn't match up with
    the right if statement.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/usbhid/hiddev.c
+++ drivers/hid/usbhid/hiddev.c
@@ -509,7 +509,7 @@ static noinline int hiddev_ioctl_usage(struct hiddev *hiddev, unsigned int cmd, 
 				 (uref_multi->num_values > HID_MAX_MULTI_USAGES ||
 				  uref->usage_index + uref_multi->num_values > field->report_count))
 				goto inval;
-			}
+		}
 
 		switch (cmd) {
 		case HIDIOCGUSAGE:
