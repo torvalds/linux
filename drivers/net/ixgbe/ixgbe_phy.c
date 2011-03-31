a59e8a1a72806057084adc2d321fc2a7cbce9579
Emil Tantilov <emil.s.tantilov@intel.com>
2011-03-31 09:36:12 +0000

    ixgbe: explicitly disable 100H for x540
    100H is not supported on this HW, but the bit is set on the PHY.
    This can result in link at 100F when advertising only 1000F.
    
    Signed-off-by: Emil Tantilov <emil.s.tantilov@intel.com>
    Tested-by: Evan Swanson <evan.swanson@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ixgbe/ixgbe_phy.c
+++ drivers/net/ixgbe/ixgbe_phy.c
@@ -449,7 +449,8 @@ s32 ixgbe_setup_phy_link_generic(struct ixgbe_hw *hw)
 				     MDIO_MMD_AN,
 				     &autoneg_reg);
 
-		autoneg_reg &= ~ADVERTISE_100FULL;
+		autoneg_reg &= ~(ADVERTISE_100FULL |
+				 ADVERTISE_100HALF);
 		if (hw->phy.autoneg_advertised & IXGBE_LINK_SPEED_100_FULL)
 			autoneg_reg |= ADVERTISE_100FULL;
 
