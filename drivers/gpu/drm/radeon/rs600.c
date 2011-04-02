fb9674bd2cd3c18f6c756ee123967e43be501263
Alex Deucher <alexdeucher@gmail.com>
2011-04-02 09:15:50 -0400

    drm/radeon/kms: pageflipping cleanup for avivo+
    Avoid touching the flip setup regs while
    acceleration is running.  Set them at modeset
    rather than during pageflip.  Touching these
    regs while acceleration is active caused hangs
    on pre-avivo chips.  These chips do not seem
    to be affected, but better safe than sorry,
    plus it avoids repeatedly reprogramming the
    regs every flip.
    
    Signed-off-by: Alex Deucher <alexdeucher@gmail.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/rs600.c
+++ drivers/gpu/drm/radeon/rs600.c
@@ -48,17 +48,6 @@ int rs600_mc_wait_for_idle(struct radeon_device *rdev);
 
 void rs600_pre_page_flip(struct radeon_device *rdev, int crtc)
 {
-	struct radeon_crtc *radeon_crtc = rdev->mode_info.crtcs[crtc];
-	u32 tmp;
-
-	/* make sure flip is at vb rather than hb */
-	tmp = RREG32(AVIVO_D1GRPH_FLIP_CONTROL + radeon_crtc->crtc_offset);
-	tmp &= ~AVIVO_D1GRPH_SURFACE_UPDATE_H_RETRACE_EN;
-	WREG32(AVIVO_D1GRPH_FLIP_CONTROL + radeon_crtc->crtc_offset, tmp);
-
-	/* set pageflip to happen anywhere in vblank interval */
-	WREG32(AVIVO_D1MODE_MASTER_UPDATE_MODE + radeon_crtc->crtc_offset, 0);
-
 	/* enable the pflip int */
 	radeon_irq_kms_pflip_irq_get(rdev, crtc);
 }
