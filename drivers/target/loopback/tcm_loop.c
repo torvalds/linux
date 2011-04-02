12f09ccb4612734a53e47ed5302e0479c10a50f8
Dan Carpenter <error27@gmail.com>
2011-04-02 14:32:47 -0700

    loopback: off by one in tcm_loop_make_naa_tpg()
    This is an off by one 'tgpt' check in tcm_loop_make_naa_tpg() that could result
    in memory corruption.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Signed-off-by: Nicholas A. Bellinger <nab@linux-iscsi.org>

--- drivers/target/loopback/tcm_loop.c
+++ drivers/target/loopback/tcm_loop.c
@@ -1205,7 +1205,7 @@ struct se_portal_group *tcm_loop_make_naa_tpg(
 	tpgt_str += 5; /* Skip ahead of "tpgt_" */
 	tpgt = (unsigned short int) simple_strtoul(tpgt_str, &end_ptr, 0);
 
-	if (tpgt > TL_TPGS_PER_HBA) {
+	if (tpgt >= TL_TPGS_PER_HBA) {
 		printk(KERN_ERR "Passed tpgt: %hu exceeds TL_TPGS_PER_HBA:"
 				" %u\n", tpgt, TL_TPGS_PER_HBA);
 		return ERR_PTR(-EINVAL);
