fc3e5941248be00996150965a469d38c92913ac2
Ian Campbell <ian.campbell@citrix.com>
2011-04-04 11:07:57 -0700

    xen: netfront: assume all hw features are available until backend connection setup
    We need to assume that all features will be available when registering the
    netdev otherwise they are ommitted from the initial set of
    dev->wanted_features. When we connect to the backed we reduce the set as
    necessary due to the call to netdev_update_features() in xennet_connect().
    
    Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/xen-netfront.c
+++ drivers/net/xen-netfront.c
@@ -1251,6 +1251,14 @@ static struct net_device * __devinit xennet_create_dev(struct xenbus_device *dev
 				  NETIF_F_GSO_ROBUST;
 	netdev->hw_features	= NETIF_F_IP_CSUM | NETIF_F_SG | NETIF_F_TSO;
 
+	/*
+         * Assume that all hw features are available for now. This set
+         * will be adjusted by the call to netdev_update_features() in
+         * xennet_connect() which is the earliest point where we can
+         * negotiate with the backend regarding supported features.
+         */
+	netdev->features |= netdev->hw_features;
+
 	SET_ETHTOOL_OPS(netdev, &xennet_ethtool_ops);
 	SET_NETDEV_DEV(netdev, &dev->dev);
 
