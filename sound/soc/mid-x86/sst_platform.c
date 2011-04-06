0d1d7ce95156e0b040f1a4029613716aafd791b1
Lu Guanqun <guanqun.lu@intel.com>
2011-04-06 10:20:26 +0800

    ASoC: sst_platform: initialize module_name properly
    module_name will be checked in register_sst_card.
    It will fail to register sst card if it's not initialized.
    
    Signed-off-by: Lu Guanqun <guanqun.lu@intel.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/mid-x86/sst_platform.c
+++ sound/soc/mid-x86/sst_platform.c
@@ -247,6 +247,7 @@ static int sst_platform_open(struct snd_pcm_substream *substream)
 		return -ENOMEM;
 	}
 	stream->sstdrv_ops->vendor_id = MSIC_VENDOR_ID;
+	stream->sstdrv_ops->module_name = SST_CARD_NAMES;
 	/* registering with SST driver to get access to SST APIs to use */
 	ret_val = register_sst_card(stream->sstdrv_ops);
 	if (ret_val) {
