8bb36c2139f7bcea32a78472272f1d0de3b00f7b
Antonio Ospite <ospite@studenti.unina.it>
2011-04-07 12:45:51 -0300

    [media] Add Y10B, a 10 bpp bit-packed greyscale format
    Add a 10 bits per pixel greyscale format in a bit-packed array representation,
    naming it Y10B. Such pixel format is supplied for instance by the Kinect
    sensor device.
    
    Signed-off-by: Antonio Ospite <ospite@studenti.unina.it>
    Signed-off-by: Jean-François Moine <moinejf@free.fr>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/linux/videodev2.h
+++ include/linux/videodev2.h
@@ -311,6 +311,9 @@ struct v4l2_pix_format {
 #define V4L2_PIX_FMT_Y12     v4l2_fourcc('Y', '1', '2', ' ') /* 12  Greyscale     */
 #define V4L2_PIX_FMT_Y16     v4l2_fourcc('Y', '1', '6', ' ') /* 16  Greyscale     */
 
+/* Grey bit-packed formats */
+#define V4L2_PIX_FMT_Y10BPACK    v4l2_fourcc('Y', '1', '0', 'B') /* 10  Greyscale bit-packed */
+
 /* Palette formats */
 #define V4L2_PIX_FMT_PAL8    v4l2_fourcc('P', 'A', 'L', '8') /*  8  8-bit palette */
 
