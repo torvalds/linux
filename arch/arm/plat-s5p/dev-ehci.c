8f1d169f999fea892c3fcbf5a79ae8525a477572
Joonyoung Shim <jy0922.shim@samsung.com>
2011-04-08 13:22:10 +0900

    ARM: EXYNOS4: Add usb host phy control
    EXYNOS4 has 2 phys for usb host and usb device. This patch supports to
    control usb host phy of EXYNOS4.
    
    Signed-off-by: Joonyoung Shim <jy0922.shim@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- arch/arm/plat-s5p/dev-ehci.c
+++ arch/arm/plat-s5p/dev-ehci.c
@@ -45,6 +45,13 @@ struct platform_device s5p_device_ehci = {
 
 void __init s5p_ehci_set_platdata(struct s5p_ehci_platdata *pd)
 {
-	s3c_set_platdata(pd, sizeof(struct s5p_ehci_platdata),
+	struct s5p_ehci_platdata *npd;
+
+	npd = s3c_set_platdata(pd, sizeof(struct s5p_ehci_platdata),
 			&s5p_device_ehci);
+
+	if (!npd->phy_init)
+		npd->phy_init = s5p_usb_phy_init;
+	if (!npd->phy_exit)
+		npd->phy_exit = s5p_usb_phy_exit;
 }
