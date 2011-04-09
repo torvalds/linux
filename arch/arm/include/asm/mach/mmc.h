5b42ae3edab6c39c1337d36881d29350bb36dcff
Colin Cross <ccross@android.com>
2011-04-08 22:20:53 -0700

    Recreate asm/mach/mmc.h include file
    Change-Id: I9f10244b0603f7842b8504a16124d40dc4a71ed2
    Signed-off-by: Colin Cross <ccross@android.com>

---
+++ arch/arm/include/asm/mach/mmc.h
@@ -0,0 +1,27 @@
+/*
+ *  arch/arm/include/asm/mach/mmc.h
+ */
+#ifndef ASMARM_MACH_MMC_H
+#define ASMARM_MACH_MMC_H
+
+#include <linux/mmc/host.h>
+#include <linux/mmc/card.h>
+#include <linux/mmc/sdio_func.h>
+
+struct embedded_sdio_data {
+        struct sdio_cis cis;
+        struct sdio_cccr cccr;
+        struct sdio_embedded_func *funcs;
+        int num_funcs;
+};
+
+struct mmc_platform_data {
+	unsigned int ocr_mask;			/* available voltages */
+	int built_in;				/* built-in device flag */
+	u32 (*translate_vdd)(struct device *, unsigned int);
+	unsigned int (*status)(struct device *);
+	struct embedded_sdio_data *embedded_sdio;
+	int (*register_status_notify)(void (*callback)(int card_present, void *dev_id), void *dev_id);
+};
+
+#endif
