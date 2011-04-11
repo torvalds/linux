eeaaf817c7202b71a6e1aab87ebd538d1b5e5a42
MÃƒÂ¡rcio Alves <froooozen@gmail.com>
2011-04-11 19:57:15 -0300

    [media] cx231xx: add support for Kworld
    [mchehab@redhat.com: avoided board renumberation, removed an unused #define
     and re-used the existing mb86a20s dvb attach code]
    Signed-off-by: Márcio A Alves <froooozen@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx231xx/cx231xx.h
+++ drivers/media/video/cx231xx/cx231xx.h
@@ -65,6 +65,7 @@
 #define CX231XX_BOARD_HAUPPAUGE_USBLIVE2 9
 #define CX231XX_BOARD_PV_PLAYTV_USB_HYBRID 10
 #define CX231XX_BOARD_PV_XCAPTURE_USB 11
+#define CX231XX_BOARD_KWORLD_UB430_USB_HYBRID 12
 
 /* Limits minimum and default number of buffers */
 #define CX231XX_MIN_BUF                 4
