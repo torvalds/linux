4b38a6db01b09198f4045661815a0039c3d80660
Trond Myklebust <Trond.Myklebust@netapp.com>
2011-04-11 11:56:23 -0400

    NFS: Eliminate duplicate call to nfs_mark_request_dirty
    We only need to call nfs_mark_request_dirty() once in nfs_writepage_setup().
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/write.c
+++ fs/nfs/write.c
@@ -680,7 +680,6 @@ static int nfs_writepage_setup(struct nfs_open_context *ctx, struct page *page,
 	req = nfs_setup_write_request(ctx, page, offset, count);
 	if (IS_ERR(req))
 		return PTR_ERR(req);
-	nfs_mark_request_dirty(req);
 	/* Update file length */
 	nfs_grow_file(page, offset, count);
 	nfs_mark_uptodate(page, req->wb_pgbase, req->wb_bytes);
