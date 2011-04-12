89ea42d716e1ee94f643ecdc516d90a4111ec135
Mathew McKernan <matmckernan@rauland.com.au>
2011-04-12 06:51:38 +0100

    drm/i915/tv: Only poll for TV connections
    As a probe for a TV connection modifies the TV_CTL register, it causes a
    loss of sync and a regular glitch on the output. This is highly
    undesirable when using the TV, so only poll for TV connections and wait
    for an explicit query for detecting the disconnection event.
    
    Reported-by: Mathew McKernan <matmckernan@rauland.com.au>
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=35977
    Signed-off-by: Mathew McKernan <matmckernan@rauland.com.au>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- drivers/gpu/drm/i915/intel_tv.c
+++ drivers/gpu/drm/i915/intel_tv.c
@@ -1672,8 +1672,7 @@ intel_tv_init(struct drm_device *dev)
 	 *
 	 * More recent chipsets favour HDMI rather than integrated S-Video.
 	 */
-	connector->polled =
-		DRM_CONNECTOR_POLL_CONNECT | DRM_CONNECTOR_POLL_DISCONNECT;
+	connector->polled = DRM_CONNECTOR_POLL_CONNECT;
 
 	drm_connector_init(dev, connector, &intel_tv_connector_funcs,
 			   DRM_MODE_CONNECTOR_SVIDEO);
