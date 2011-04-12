0768daf80263e11d34b65f88cb3b1c56cf10b21a
黄涛 <huangtao@rock-chips.com>
2011-04-12 17:36:55 +0800

    rk29: remove board aigo

--- pack-kernel.sh
+++ pack-kernel.sh
@@ -37,10 +37,6 @@ arch/arm/configs/rk29_malata_defconfig
 arch/arm/mach-rk29/board-rk29-winaccord.c
 arch/arm/configs/rk29_Winaccord_defconfig
 
-arch/arm/mach-rk29/board-rk29-aigo.c
-arch/arm/mach-rk29/board-rk29aigo-key.c
-arch/arm/configs/rk29_aigo_defconfig
-
 arch/arm/mach-rk29/board-rk29-a22*
 arch/arm/configs/rk29_a22_defconfig
 
