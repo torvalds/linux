0b2e9a8e10ad2d191e5c37e77f1ce23e148e7a0b
Michael Ellerman <michael@ellerman.id.au>
2011-04-14 22:31:57 +0000

    of: Export of_irq_find_parent()
    We have platform code that needs to find a node's interrupt parent, so
    export of_irq_find_parent() so we can use it.
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- include/linux/of_irq.h
+++ include/linux/of_irq.h
@@ -68,6 +68,7 @@ extern int of_irq_to_resource(struct device_node *dev, int index,
 extern int of_irq_count(struct device_node *dev);
 extern int of_irq_to_resource_table(struct device_node *dev,
 		struct resource *res, int nr_irqs);
+extern struct device_node *of_irq_find_parent(struct device_node *child);
 
 #endif /* CONFIG_OF_IRQ */
 #endif /* CONFIG_OF */
