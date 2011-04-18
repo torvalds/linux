6f8efcfb3dc88e4c626765278afc40ed4bfc18e2
Patrice Chotard <patrice.chotard@sfr.fr>
2011-04-18 17:39:38 -0300

    [media] gspca - jeilinj: add 640*480 resolution support
    Signed-off-by: Patrice CHOTARD <patricechotard@free.fr>
    Signed-off-by: Jean-François Moine <moinejf@free.fr>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/gspca/jeilinj.c
+++ drivers/media/video/gspca/jeilinj.c
@@ -62,6 +62,11 @@ static struct v4l2_pix_format jlj_mode[] = {
 		.bytesperline = 320,
 		.sizeimage = 320 * 240,
 		.colorspace = V4L2_COLORSPACE_JPEG,
+		.priv = 0},
+	{ 640, 480, V4L2_PIX_FMT_JPEG, V4L2_FIELD_NONE,
+		.bytesperline = 640,
+		.sizeimage = 640 * 480,
+		.colorspace = V4L2_COLORSPACE_JPEG,
 		.priv = 0}
 };
 
@@ -207,7 +212,7 @@ static int sd_config(struct gspca_dev *gspca_dev,
 		"JEILINJ camera detected"
 		" (vid/pid 0x%04X:0x%04X)", id->idVendor, id->idProduct);
 	cam->cam_mode = jlj_mode;
-	cam->nmodes = 1;
+	cam->nmodes = ARRAY_SIZE(jlj_mode);
 	cam->bulk = 1;
 	cam->bulk_nurbs = 1;
 	cam->bulk_size = JEILINJ_MAX_TRANSFER;
@@ -264,7 +269,8 @@ static int sd_start(struct gspca_dev *gspca_dev)
 	jpeg_define(dev->jpeg_hdr, gspca_dev->height, gspca_dev->width,
 			0x21);          /* JPEG 422 */
 	jpeg_set_qual(dev->jpeg_hdr, dev->quality);
-	PDEBUG(D_STREAM, "Start streaming at 320x240");
+	PDEBUG(D_STREAM, "Start streaming at %dx%d",
+		gspca_dev->height, gspca_dev->width);
 	jlj_start(gspca_dev);
 	return gspca_dev->usb_err;
 }
