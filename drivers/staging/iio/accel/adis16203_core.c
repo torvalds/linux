9e558ff5448715a8cca1439e3188611b31ea72ec
Jonathan Cameron <jic23@cam.ac.uk>
2011-04-18 12:59:01 +0100

    staging:iio:adis16203 replace unnecessary event line registration.
    Whilst the adis16203 does indeed support events, currently the driver
    does not.  The trigger code should never use that infrastructure.
    
    Untested, but identical in form to those that have been so should be fine.
    
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Acked-by: Michael Hennerich <michael.hennerich@analog.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/accel/adis16203_core.c
+++ drivers/staging/iio/accel/adis16203_core.c
@@ -394,14 +394,6 @@ static IIO_DEVICE_ATTR(reset, S_IWUSR, NULL, adis16203_write_reset, 0);
 
 static IIO_CONST_ATTR(name, "adis16203");
 
-static struct attribute *adis16203_event_attributes[] = {
-	NULL
-};
-
-static struct attribute_group adis16203_event_attribute_group = {
-	.attrs = adis16203_event_attributes,
-};
-
 static struct attribute *adis16203_attributes[] = {
 	&iio_dev_attr_in0_supply_raw.dev_attr.attr,
 	&iio_const_attr_in0_supply_scale.dev_attr.attr,
@@ -455,8 +447,6 @@ static int __devinit adis16203_probe(struct spi_device *spi)
 	}
 
 	st->indio_dev->dev.parent = &spi->dev;
-	st->indio_dev->num_interrupt_lines = 1;
-	st->indio_dev->event_attrs = &adis16203_event_attribute_group;
 	st->indio_dev->attrs = &adis16203_attribute_group;
 	st->indio_dev->dev_data = (void *)(st);
 	st->indio_dev->driver_module = THIS_MODULE;
@@ -478,17 +468,9 @@ static int __devinit adis16203_probe(struct spi_device *spi)
 	}
 
 	if (spi->irq) {
-		ret = iio_register_interrupt_line(spi->irq,
-				st->indio_dev,
-				0,
-				IRQF_TRIGGER_RISING,
-				"adis16203");
-		if (ret)
-			goto error_uninitialize_ring;
-
 		ret = adis16203_probe_trigger(st->indio_dev);
 		if (ret)
-			goto error_unregister_line;
+			goto error_uninitialize_ring;
 	}
 
 	/* Get the device into a sane initial state */
@@ -499,9 +481,6 @@ static int __devinit adis16203_probe(struct spi_device *spi)
 
 error_remove_trigger:
 	adis16203_remove_trigger(st->indio_dev);
-error_unregister_line:
-	if (spi->irq)
-		iio_unregister_interrupt_line(st->indio_dev, 0);
 error_uninitialize_ring:
 	adis16203_uninitialize_ring(st->indio_dev->ring);
 error_unreg_ring_funcs:
@@ -529,9 +508,6 @@ static int adis16203_remove(struct spi_device *spi)
 	flush_scheduled_work();
 
 	adis16203_remove_trigger(indio_dev);
-	if (spi->irq)
-		iio_unregister_interrupt_line(indio_dev, 0);
-
 	adis16203_uninitialize_ring(indio_dev->ring);
 	iio_device_unregister(indio_dev);
 	adis16203_unconfigure_ring(indio_dev);
