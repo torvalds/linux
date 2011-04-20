251e031d132ea3d03e0a32f2240c67f449979c5d
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2011-04-20 09:44:43 +0200

    driver core/platform_device_add_data: free platform data before overwriting
    Reviewed-by: Viresh Kumar <viresh.kumar@st.com>
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/base/platform.c
+++ drivers/base/platform.c
@@ -228,6 +228,7 @@ int platform_device_add_data(struct platform_device *pdev, const void *data,
 			return -ENOMEM;
 	}
 
+	kfree(pdev->dev.platform_data);
 	pdev->dev.platform_data = d;
 	return 0;
 }
