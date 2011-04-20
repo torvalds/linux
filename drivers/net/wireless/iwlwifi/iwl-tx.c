51028920a9424e2437d6acc3bbc0946d6d71a7aa
Stanislaw Gruszka <sgruszka@redhat.com>
2011-04-20 15:57:14 +0200

    iwlwifi: fix skb usage after free
    commit b25026981aecde3685dd0e45ad980fff9f528daa upstream.
    
    Since
    
    commit a120e912eb51e347f36c71b60a1d13af74d30e83
    Author: Stanislaw Gruszka <sgruszka@redhat.com>
    Date:   Fri Feb 19 15:47:33 2010 -0800
    
        iwlwifi: sanity check before counting number of tfds can be free
    
    we use skb->data after calling ieee80211_tx_status_irqsafe(), which
    could free skb instantly.
    
    On current kernels I do not observe practical problems related with
    bug, but on 2.6.35.y it cause random system hangs when stressing
    wireless link.
    
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>
    Acked-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/net/wireless/iwlwifi/iwl-tx.c
+++ drivers/net/wireless/iwlwifi/iwl-tx.c
@@ -1088,11 +1088,15 @@ int iwl_tx_queue_reclaim(struct iwl_priv *priv, int txq_id, int index)
 	     q->read_ptr = iwl_queue_inc_wrap(q->read_ptr, q->n_bd)) {
 
 		tx_info = &txq->txb[txq->q.read_ptr];
-		ieee80211_tx_status_irqsafe(priv->hw, tx_info->skb[0]);
+
+		if (WARN_ON_ONCE(tx_info->skb == NULL))
+			continue;
 
 		hdr = (struct ieee80211_hdr *)tx_info->skb[0]->data;
-		if (hdr && ieee80211_is_data_qos(hdr->frame_control))
+		if (ieee80211_is_data_qos(hdr->frame_control))
 			nfreed++;
+
+		ieee80211_tx_status_irqsafe(priv->hw, tx_info->skb[0]);
 		tx_info->skb[0] = NULL;
 
 		if (priv->cfg->ops->lib->txq_inval_byte_cnt_tbl)
