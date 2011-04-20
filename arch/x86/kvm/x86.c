5197b808a7f459f9c7436573c7785ff3c1324c08
Avi Kivity <avi@redhat.com>
2011-04-20 15:55:40 +0300

    KVM: Avoid using x86_emulate_ctxt.vcpu
    We can use container_of() instead.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4366,7 +4366,7 @@ static void emulator_halt(struct x86_emulate_ctxt *ctxt)
 static void emulator_get_fpu(struct x86_emulate_ctxt *ctxt)
 {
 	preempt_disable();
-	kvm_load_guest_fpu(ctxt->vcpu);
+	kvm_load_guest_fpu(emul_to_vcpu(ctxt));
 	/*
 	 * CR0.TS may reference the host fpu state, not the guest fpu state,
 	 * so it may be clear at this point.
