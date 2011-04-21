d42244499f171a499aa6748bc52304bb40e68ecc
Randy Dunlap <randy.dunlap@oracle.com>
2011-04-21 09:09:22 -0700

    KVM: x86 emulator: fix const value warning on i386 in svm insn RAX check
    arch/x86/kvm/emulate.c:2598: warning: integer constant is too large for 'long' type
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/emulate.c
+++ arch/x86/kvm/emulate.c
@@ -2738,7 +2738,7 @@ static int check_svme_pa(struct x86_emulate_ctxt *ctxt)
 	u64 rax = ctxt->decode.regs[VCPU_REGS_RAX];
 
 	/* Valid physical address? */
-	if (rax & 0xffff000000000000)
+	if (rax & 0xffff000000000000ULL)
 		return emulate_gp(ctxt, 0);
 
 	return check_svme(ctxt);
