fa7b69475a6c192853949ba496dd9c37b497b548
Justin P. Mattock <justinmattock@gmail.com>
2011-04-22 10:08:52 -0700

    perf events, x86, P4: Fix typo in comment
    Signed-off-by: Justin P. Mattock <justinmattock@gmail.com>
    Acked-by: Cyrill Gorcunov <gorcunov@gmail.com>
    Cc: trivial@kernel.org
    Link: http://lkml.kernel.org/r/1303492132-3004-1-git-send-email-justinmattock@gmail.com
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/cpu/perf_event_p4.c
+++ arch/x86/kernel/cpu/perf_event_p4.c
@@ -1187,7 +1187,7 @@ static __init int p4_pmu_init(void)
 {
 	unsigned int low, high;
 
-	/* If we get stripped -- indexig fails */
+	/* If we get stripped -- indexing fails */
 	BUILD_BUG_ON(ARCH_P4_MAX_CCCR > X86_PMC_MAX_GENERIC);
 
 	rdmsr(MSR_IA32_MISC_ENABLE, low, high);
