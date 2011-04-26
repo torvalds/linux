0bf6e2eca2a32fc6ea69eb499313234476a526d7
Aaro Koskinen <aaro.koskinen@nokia.com>
2011-04-26 02:25:10 -0700

    OMAP3+: smartreflex: fix sr_late_init() error path in probe
    sr_late_init() will take care of freeing the resources.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@nokia.com>
    Signed-off-by: Kevin Hilman <khilman@ti.com>

--- arch/arm/mach-omap2/smartreflex.c
+++ arch/arm/mach-omap2/smartreflex.c
@@ -883,7 +883,7 @@ static int __init omap_sr_probe(struct platform_device *pdev)
 		ret = sr_late_init(sr_info);
 		if (ret) {
 			pr_warning("%s: Error in SR late init\n", __func__);
-			goto err_release_region;
+			return ret;
 		}
 	}
 
