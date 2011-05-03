6435909199c2d1b0aad3ebbfa01f641aaa24fa2a
Jeff Kirsher <jeffrey.t.kirsher@intel.com>
2011-05-03 05:26:13 +0000

    e1000: convert to set_phys_id
    Based on the original patch from Stephen Hemminger.
    Convert to new LED control infrastucture and remove no longer
    necessary bits.
    
    CC: Stephen Hemminger <shemminger@vyatta.com>
    Tested-by: Jeff Pieper <jeffrey.e.pieper@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/e1000/e1000.h
+++ drivers/net/e1000/e1000.h
@@ -238,9 +238,6 @@ struct e1000_adapter {
 	struct work_struct reset_task;
 	u8 fc_autoneg;
 
-	struct timer_list blink_timer;
-	unsigned long led_status;
-
 	/* TX */
 	struct e1000_tx_ring *tx_ring;      /* One per active queue */
 	unsigned int restart_queue;
