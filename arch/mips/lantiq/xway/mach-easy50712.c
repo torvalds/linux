f1f0ceaada9d040a41023017c87abb1d651b44af
John Crispin <blogic@openwrt.org>
2011-05-06 00:10:02 +0200

    MIPS: Lantiq: Add etop board support
    Register the etop platform device inside the machtype specific init code.
    
    Signed-off-by: John Crispin <blogic@openwrt.org>
    Signed-off-by: Ralph Hempel <ralph.hempel@lantiq.com>
    Signed-off-by: David Daney <ddaney@caviumnetworks.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/2356/
    Patchwork: https://patchwork.linux-mips.org/patch/2370/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/lantiq/xway/mach-easy50712.c
+++ arch/mips/lantiq/xway/mach-easy50712.c
@@ -12,6 +12,7 @@
 #include <linux/mtd/partitions.h>
 #include <linux/mtd/physmap.h>
 #include <linux/input.h>
+#include <linux/phy.h>
 
 #include <lantiq_soc.h>
 #include <irq.h>
@@ -55,11 +56,16 @@ static struct ltq_pci_data ltq_pci_data = {
 	},
 };
 
+static struct ltq_eth_data ltq_eth_data = {
+	.mii_mode = PHY_INTERFACE_MODE_MII,
+};
+
 static void __init easy50712_init(void)
 {
 	ltq_register_gpio_stp();
 	ltq_register_nor(&easy50712_flash_data);
 	ltq_register_pci(&ltq_pci_data);
+	ltq_register_etop(&ltq_eth_data);
 }
 
 MIPS_MACHINE(LTQ_MACH_EASY50712,
