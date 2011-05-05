9cc7c80b57c66d08e67f297169783d1f07288066
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2011-05-04 20:54:27 -0700

    Input: sh_keysc - only compile PM code if PM is enabled
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/keyboard/sh_keysc.c
+++ drivers/input/keyboard/sh_keysc.c
@@ -291,6 +291,7 @@ static int __devexit sh_keysc_remove(struct platform_device *pdev)
 	return 0;
 }
 
+#if CONFIG_PM_SLEEP
 static int sh_keysc_suspend(struct device *dev)
 {
 	struct platform_device *pdev = to_platform_device(dev);
@@ -323,11 +324,10 @@ static int sh_keysc_resume(struct device *dev)
 
 	return 0;
 }
+#endif
 
-static const struct dev_pm_ops sh_keysc_dev_pm_ops = {
-	.suspend = sh_keysc_suspend,
-	.resume = sh_keysc_resume,
-};
+static SIMPLE_DEV_PM_OPS(sh_keysc_dev_pm_ops,
+			 sh_keysc_suspend, sh_keysc_resume);
 
 static struct platform_driver sh_keysc_device_driver = {
 	.probe		= sh_keysc_probe,
