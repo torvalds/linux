addd0a09fc06179f2e02b4221775d9ab265c9fc7
Tao Ma <boyu.mt@taobao.com>
2011-05-05 15:10:05 -0600

    block: Remove 'plug/unplug' comment in blk_execute_rq_nowait
    unplug is replaced with blk_run_queue now in blk_execute_rq_nowait,
    so change the comment accordingly.
    
    Signed-off-by: Tao Ma <boyu.mt@taobao.com>
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

--- block/blk-exec.c
+++ block/blk-exec.c
@@ -56,7 +56,7 @@ void blk_execute_rq_nowait(struct request_queue *q, struct gendisk *bd_disk,
 	spin_lock_irq(q->queue_lock);
 	__elv_add_request(q, rq, where);
 	__blk_run_queue(q);
-	/* the queue is stopped so it won't be plugged+unplugged */
+	/* the queue is stopped so it won't be run */
 	if (rq->cmd_type == REQ_TYPE_PM_RESUME)
 		q->request_fn(q);
 	spin_unlock_irq(q->queue_lock);
