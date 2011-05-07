4a98f590659113f72c021201781d5b83bc743a7c
Thomas Abraham <thomas.ab@samsung.com>
2011-05-07 22:26:53 +0200

    ARM: S3C24XX: Add plaform device definition for USB High-Speed gadget controller.
    S3C2416, S3C2443 and S3C2450 includes a USB High-Speed Gadget controller module.
    This patch adds the following for supporting this controller.
    
    1. Definition for USB High-Speed controller base address.
    2. Platform device instantiation.
    3. Declaration for platform data structure.
    4. Functionality to setup platform data for the controller.
    
    Signed-off-by: Thomas Abraham <thomas.ab@samsung.com>
    Signed-off-by: Sangbeom Kim <sbkim73@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- arch/arm/plat-samsung/include/plat/devs.h
+++ arch/arm/plat-samsung/include/plat/devs.h
@@ -88,6 +88,7 @@ extern struct platform_device s3c64xx_device_onenand1;
 extern struct platform_device s5p_device_onenand;
 
 extern struct platform_device s3c_device_usbgadget;
+extern struct platform_device s3c_device_usb_hsudc;
 extern struct platform_device s3c_device_usb_hsotg;
 
 extern struct platform_device s5pv210_device_ac97;
