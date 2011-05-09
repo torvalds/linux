902ebd3e0de618b6d39004edac00b2cc36362065
David S. Miller <davem@davemloft.net>
2011-05-09 14:49:13 -0700

    sctp: Remove rt->rt_src usage in sctp_v4_get_saddr()
    Flow key is available, so fetch it from there.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sctp/protocol.c
+++ net/sctp/protocol.c
@@ -566,7 +566,7 @@ static void sctp_v4_get_saddr(struct sctp_sock *sk,
 
 	if (rt) {
 		saddr->v4.sin_family = AF_INET;
-		saddr->v4.sin_addr.s_addr = rt->rt_src;
+		saddr->v4.sin_addr.s_addr = fl->u.ip4.saddr;
 	}
 }
 
