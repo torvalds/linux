93d29cc63f1832dbbbab721cce1b9f845becf4d7
Joe Carnuccio <joe.carnuccio@qlogic.com>
2011-05-10 11:30:07 -0700

    [SCSI] qla2xxx: Correct buffer start in edc sysfs debug print.
    The debug print prints the first byte of the buffer which is buf[8].
    
    Signed-off-by: Joe Carnuccio <joe.carnuccio@qlogic.com>
    Signed-off-by: Madhuranath Iyengar <Madhu.Iyengar@qlogic.com>
    Signed-off-by: James Bottomley <jbottomley@parallels.com>
    Signed-off-by: James Bottomley <James.Bottomley@suse.de>

--- drivers/scsi/qla2xxx/qla_attr.c
+++ drivers/scsi/qla2xxx/qla_attr.c
@@ -633,7 +633,7 @@ qla2x00_sysfs_write_edc(struct file *filp, struct kobject *kobj,
 	if (rval != QLA_SUCCESS) {
 		DEBUG2(qla_printk(KERN_INFO, ha,
 		    "Unable to write EDC (%x) %02x:%02x:%04x:%02x:%02x.\n",
-		    rval, dev, adr, opt, len, *buf));
+		    rval, dev, adr, opt, len, buf[8]));
 		return 0;
 	}
 
