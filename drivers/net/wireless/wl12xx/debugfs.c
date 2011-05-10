d3eff81de6048d8af8f95f52f0f06625980f2efb
Luciano Coelho <coelho@ti.com>
2011-05-10 14:47:45 +0300

    wl12xx: export scheduled scan state in debugfs
    Add the sched_scanning value to the driver_status debugfs entry.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/wl12xx/debugfs.c
+++ drivers/net/wireless/wl12xx/debugfs.c
@@ -377,6 +377,7 @@ static ssize_t driver_state_read(struct file *file, char __user *user_buf,
 	DRIVER_STATE_PRINT_HEX(platform_quirks);
 	DRIVER_STATE_PRINT_HEX(chip.id);
 	DRIVER_STATE_PRINT_STR(chip.fw_ver_str);
+	DRIVER_STATE_PRINT_INT(sched_scanning);
 
 #undef DRIVER_STATE_PRINT_INT
 #undef DRIVER_STATE_PRINT_LONG
