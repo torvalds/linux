a73ddc61e8f14ddc7ab6e6a11dc882ef6a697114
Kukjin Kim <kgene.kim@samsung.com>
2011-05-11 16:27:51 +0900

    ARM: S5P6442: Removing ARCH_S5P6442
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/plat-samsung/include/plat/devs.h
+++ arch/arm/plat-samsung/include/plat/devs.h
@@ -110,12 +110,6 @@ extern struct platform_device exynos4_device_spdif;
 extern struct platform_device exynos4_device_pd[];
 extern struct platform_device exynos4_device_ahci;
 
-extern struct platform_device s5p6442_device_pcm0;
-extern struct platform_device s5p6442_device_pcm1;
-extern struct platform_device s5p6442_device_iis0;
-extern struct platform_device s5p6442_device_iis1;
-extern struct platform_device s5p6442_device_spi;
-
 extern struct platform_device s5p6440_device_pcm;
 extern struct platform_device s5p6440_device_iis;
 
