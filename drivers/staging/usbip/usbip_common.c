353be243d55875ae80d8074cd3489f5597550030
David Chang <dchang@novell.com>
2011-05-12 18:31:11 +0800

    staging: usbip: fix wrong endian conversion
    commit cacd18a8476ce145ca5dcd46dc5b75585fd1289c upstream.
    
    Fix number_of_packets wrong endian conversion in function
    correct_endian_ret_submit()
    
    Signed-off-by: David Chang <dchang@novell.com>
    Acked-by: Arjan Mels <arjan.mels@gmx.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/usbip/usbip_common.c
+++ drivers/staging/usbip/usbip_common.c
@@ -770,7 +770,7 @@ static void correct_endian_ret_submit(struct usbip_header_ret_submit *pdu,
 		be32_to_cpus(&pdu->status);
 		be32_to_cpus(&pdu->actual_length);
 		be32_to_cpus(&pdu->start_frame);
-		cpu_to_be32s(&pdu->number_of_packets);
+		be32_to_cpus(&pdu->number_of_packets);
 		be32_to_cpus(&pdu->error_count);
 	}
 }
