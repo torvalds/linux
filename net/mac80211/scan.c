c29acf201007a6d73223f864f52406eb5ba19933
Rajkumar Manoharan <rmanoharan@atheros.com>
2011-05-14 09:43:28 +0530

    mac80211: abort scan_work immediately when the device goes down
    As long as no delay is required b/w channel change, scan work
    is proceeding without scheduling a new work. In such case, we
    can not abort scan work when the card was unplugged. This patch
    completes the scanning immediately whenever the device goes down.
    
    Reviewed-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: Rajkumar Manoharan <rmanoharan@atheros.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/scan.c
+++ net/mac80211/scan.c
@@ -719,6 +719,11 @@ void ieee80211_scan_work(struct work_struct *work)
 	 * without scheduling a new work
 	 */
 	do {
+		if (!ieee80211_sdata_running(sdata)) {
+			aborted = true;
+			goto out_complete;
+		}
+
 		switch (local->next_scan_state) {
 		case SCAN_DECISION:
 			/* if no more bands/channels left, complete scan */
