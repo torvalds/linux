f7242d30034da3f2937fbe9eb3d17355bbff58d1
Jean-François Moine <moinejf@free.fr>
2011-05-17 03:56:09 -0300

    [media] gspca - kinect: Remove __devinitdata
    __devinit* must not be used in USB drivers.
    
    Signed-off-by: Jean-François Moine <moinejf@free.fr>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/gspca/kinect.c
+++ drivers/media/video/gspca/kinect.c
@@ -389,7 +389,7 @@ static const struct sd_desc sd_desc = {
 };
 
 /* -- module initialisation -- */
-static const __devinitdata struct usb_device_id device_table[] = {
+static const struct usb_device_id device_table[] = {
 	{USB_DEVICE(0x045e, 0x02ae)},
 	{}
 };
