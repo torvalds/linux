9edc2cc0fbfc94fddeaaf20a47e1b4c8e7d424f4
Dmitry ORNATSKYY <cdo021c@motorola.com>
2011-05-18 13:58:44 -0700

    net: wireless: bcm4329: Update example locale table
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/net/wireless/bcm4329/dhd_custom_gpio.c
+++ drivers/net/wireless/bcm4329/dhd_custom_gpio.c
@@ -183,22 +183,48 @@ dhd_custom_get_mac_address(unsigned char *buf)
 const struct cntry_locales_custom translate_custom_table[] = {
 /* Table should be filled out based on custom platform regulatory requirement */
 #ifdef EXAMPLE_TABLE
-	{"",   "XY",  4}  /* universal */
+	{"",   "XY", 4},  /* universal */
 	{"US", "US", 69}, /* input ISO "US" to : US regrev 69 */
 	{"CA", "US", 69}, /* input ISO "CA" to : US regrev 69 */
-	{"EU", "EU",  5}, /* input ISO "EU" to : EU regrev 05 */
-	{"FR", "EU",  5},
-	{"DE", "EU",  5},
-	{"IR", "EU",  5},
-	{"UK", "EU",  5}, /* input ISO "UK" to : EU regrev 05 */
-	{"KR", "XY",  3},
-	{"AU", "XY",  3},
-	{"CN", "XY",  3}, /* input ISO "CN" to : XY regrev 03 */
-	{"HK", "XY",  3},
-	{"TW", "XY",  3},
-	{"BR", "XY",  3},
-	{"MX", "XY",  3},
-	{"AR", "XY",  3}
+	{"EU", "EU", 5},  /* European union countries */
+	{"AT", "EU", 5},
+	{"BE", "EU", 5},
+	{"BG", "EU", 5},
+	{"CY", "EU", 5},
+	{"CZ", "EU", 5},
+	{"DK", "EU", 5},
+	{"EE", "EU", 5},
+	{"FI", "EU", 5},
+	{"FR", "EU", 5},
+	{"DE", "EU", 5},
+	{"GR", "EU", 5},
+	{"HU", "EU", 5},
+	{"IE", "EU", 5},
+	{"IT", "EU", 5},
+	{"LV", "EU", 5},
+	{"LI", "EU", 5},
+	{"LT", "EU", 5},
+	{"LU", "EU", 5},
+	{"MT", "EU", 5},
+	{"NL", "EU", 5},
+	{"PL", "EU", 5},
+	{"PT", "EU", 5},
+	{"RO", "EU", 5},
+	{"SK", "EU", 5},
+	{"SI", "EU", 5},
+	{"ES", "EU", 5},
+	{"SE", "EU", 5},
+	{"GB", "EU", 5},  /* input ISO "GB" to : EU regrev 05 */
+	{"IL", "IL", 0},
+	{"CH", "CH", 0},
+	{"TR", "TR", 0},
+	{"NO", "NO", 0},
+	{"KR", "XY", 3},
+	{"AU", "XY", 3},
+	{"CN", "XY", 3},  /* input ISO "CN" to : XY regrev 03 */
+	{"TW", "XY", 3},
+	{"AR", "XY", 3},
+	{"MX", "XY", 3}
 #endif /* EXAMPLE_TABLE */
 };
 
