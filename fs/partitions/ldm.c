cae13fe4cc3f24820ffb990c09110626837e85d4
Timo Warns <Warns@pre-sense.de>
2011-05-19 09:24:17 +0200

    Fix for buffer overflow in ldm_frag_add not sufficient
    As Ben Hutchings discovered [1], the patch for CVE-2011-1017 (buffer
    overflow in ldm_frag_add) is not sufficient.  The original patch in
    commit c340b1d64000 ("fs/partitions/ldm.c: fix oops caused by corrupted
    partition table") does not consider that, for subsequent fragments,
    previously allocated memory is used.
    
    [1] http://lkml.org/lkml/2011/5/6/407
    
    Reported-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Timo Warns <warns@pre-sense.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/partitions/ldm.c
+++ fs/partitions/ldm.c
@@ -1335,6 +1335,11 @@ static bool ldm_frag_add (const u8 *data, int size, struct list_head *frags)
 
 	list_add_tail (&f->list, frags);
 found:
+	if (rec >= f->num) {
+		ldm_error("REC value (%d) exceeds NUM value (%d)", rec, f->num);
+		return false;
+	}
+
 	if (f->map & (1 << rec)) {
 		ldm_error ("Duplicate VBLK, part %d.", rec);
 		f->map &= 0x7F;			/* Mark the group as broken */
