d1402307c23cfeafce2313d936a4a990eb8783f7
Stéphane Elmaleh <stephane.elmaleh@laposte.net>
2011-05-21 07:33:38 -0300

    [media] support for medion dvb stick 1660:1921
    [mchehab@redhat.com: The same patch were sent by Alf and by Randoslaw.
    I've applied the oldest version, just fixing the entry index]
    
    Cc: Patrick Boettcher <pboettcher@kernellabs.com>
    Tested-by: James Huk <huk256@gmail.com>
    Tested-by: Alf Fahland <alf-f@gmx.de>
    Signed-off-by: Radoslaw Warowny <radoslaww@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/dvb-usb/dib0700_devices.c
+++ drivers/media/dvb/dvb-usb/dib0700_devices.c
@@ -2801,6 +2801,7 @@ struct usb_device_id dib0700_usb_id_table[] = {
 	{ USB_DEVICE(USB_VID_DIBCOM,    USB_PID_DIBCOM_NIM7090) },
 	{ USB_DEVICE(USB_VID_DIBCOM,    USB_PID_DIBCOM_TFE7090PVR) },
 	{ USB_DEVICE(USB_VID_TECHNISAT, USB_PID_TECHNISAT_AIRSTAR_TELESTICK_2) },
+/* 75 */{ USB_DEVICE(USB_VID_MEDION,    USB_PID_CREATIX_CTX1921) },
 	{ 0 }		/* Terminating entry */
 };
 MODULE_DEVICE_TABLE(usb, dib0700_usb_id_table);
@@ -3410,7 +3411,7 @@ struct dvb_usb_device_properties dib0700_devices[] = {
 			},
 		},
 
-		.num_device_descs = 3,
+		.num_device_descs = 4,
 		.devices = {
 			{   "DiBcom STK7770P reference design",
 				{ &dib0700_usb_id_table[59], NULL },
@@ -3424,6 +3425,10 @@ struct dvb_usb_device_properties dib0700_devices[] = {
 			},
 			{   "TechniSat AirStar TeleStick 2",
 				{ &dib0700_usb_id_table[74], NULL },
+				{ NULL },
+			},
+			{   "Medion CTX1921 DVB-T USB",
+				{ &dib0700_usb_id_table[75], NULL },
 				{ NULL },
 			},
 		},
