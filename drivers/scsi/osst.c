a061f57d662ac2b19a1ff1955349adce7a282a5d
Roel Kluin <roel.kluin@gmail.com>
2011-05-23 15:29:12 -0700

    [SCSI] osst: wrong index used in inner loop
    Index i was already used in the outer loop.  Fixes a potentially-infinite
    loop.
    
    [akpm@linux-foundation.org: coding-style fixes]
    Signed-off-by: Roel Kluin <roel.kluin@gmail.com>
    Cc: Willem Riede <osst@riede.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: James Bottomley <jbottomley@parallels.com>

--- drivers/scsi/osst.c
+++ drivers/scsi/osst.c
@@ -4702,8 +4702,9 @@ static int __os_scsi_tape_open(struct inode * inode, struct file * filp)
 				STp->partition = STp->new_partition = 0;
 				if (STp->can_partitions)
 					STp->nbr_partitions = 1;  /* This guess will be updated later if necessary */
-				for (i=0; i < ST_NBR_PARTITIONS; i++) {
-					STps = &(STp->ps[i]);
+				int j;
+				for (j = 0; j < ST_NBR_PARTITIONS; j++) {
+					STps = &(STp->ps[j]);
 					STps->rw = ST_IDLE;
 					STps->eof = ST_NOEOF;
 					STps->at_sm = 0;
