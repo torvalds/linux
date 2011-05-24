af6a25f0e1ec0265c267e6ee4513925eaba6d0ed
Richard Kennedy <richard@rsk.demon.co.uk>
2011-05-24 14:49:59 +0100

    x86: Reorder mm_context_t to remove x86_64 alignment padding and thus shrink mm_struct
    Reorder mm_context_t to remove alignment padding on 64 bit
    builds shrinking its size from 64 to 56 bytes.
    
    This allows mm_struct to shrink from 840 to 832 bytes, so using
    one fewer cache lines, and getting more objects per slab when
    using slub.
    
    slabinfo mm_struct reports
    before :-
    
        Sizes (bytes)     Slabs
        -----------------------------------
        Object :     840  Total  :       7
        SlabObj:     896  Full   :       1
        SlabSiz:   16384  Partial:       4
        Loss   :      56  CpuSlab:       2
        Align  :      64  Objects:      18
    
    after :-
    
        Sizes (bytes)     Slabs
        ----------------------------------
        Object :     832  Total  :       7
        SlabObj:     832  Full   :       1
        SlabSiz:   16384  Partial:       4
        Loss   :       0  CpuSlab:       2
        Align  :      64  Objects:      19
    
    Signed-off-by: Richard Kennedy <richard@rsk.demon.co.uk>
    Cc: wilsons@start.ca
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Pekka Enberg <penberg@kernel.org>
    Link: http://lkml.kernel.org/r/1306244999.1999.5.camel@castor.rsk
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/mmu.h
+++ arch/x86/include/asm/mmu.h
@@ -11,14 +11,14 @@
 typedef struct {
 	void *ldt;
 	int size;
-	struct mutex lock;
-	void *vdso;
 
 #ifdef CONFIG_X86_64
 	/* True if mm supports a task running in 32 bit compatibility mode. */
 	unsigned short ia32_compat;
 #endif
 
+	struct mutex lock;
+	void *vdso;
 } mm_context_t;
 
 #ifdef CONFIG_SMP
