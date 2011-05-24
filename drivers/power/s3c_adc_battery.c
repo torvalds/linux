f704d45ede331204b6f393ba7cf49353fe7936a7
Mark Brown <broonie@opensource.wolfsonmicro.com>
2011-05-24 18:46:02 +0800

    s3c_adc_battery: Fix annotation for s3c_adc_battery_probe()
    A probe() function is used at device init time rather than system init
    time.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Vasily Khoruzhick <anarsoul@gmail.com>
    Signed-off-by: Anton Vorontsov <cbouatmailru@gmail.com>

--- drivers/power/s3c_adc_battery.c
+++ drivers/power/s3c_adc_battery.c
@@ -266,7 +266,7 @@ static irqreturn_t s3c_adc_bat_charged(int irq, void *dev_id)
 	return IRQ_HANDLED;
 }
 
-static int __init s3c_adc_bat_probe(struct platform_device *pdev)
+static int __devinit s3c_adc_bat_probe(struct platform_device *pdev)
 {
 	struct s3c_adc_client	*client;
 	struct s3c_adc_bat_pdata *pdata = pdev->dev.platform_data;
