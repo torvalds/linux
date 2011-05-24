40ebc0af58a88652c9a9c1e9126c8fcf9fce1f6e
Sage Weil <sage@newdream.net>
2011-05-24 13:06:16 -0700

    ext4: remove unnecessary dentry_unhash on rmdir/rename_dir
    ext4 has no problems with lingering references to unlinked directory
    inodes.
    
    CC: "Theodore Ts'o" <tytso@mit.edu>
    CC: Andreas Dilger <adilger.kernel@dilger.ca>
    CC: linux-ext4@vger.kernel.org
    Signed-off-by: Sage Weil <sage@newdream.net>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -2123,8 +2123,6 @@ static int ext4_rmdir(struct inode *dir, struct dentry *dentry)
 	struct ext4_dir_entry_2 *de;
 	handle_t *handle;
 
-	dentry_unhash(dentry);
-
 	/* Initialize quotas before so that eventual writes go in
 	 * separate transaction */
 	dquot_initialize(dir);
@@ -2351,9 +2349,6 @@ static int ext4_rename(struct inode *old_dir, struct dentry *old_dentry,
 	struct buffer_head *old_bh, *new_bh, *dir_bh;
 	struct ext4_dir_entry_2 *old_de, *new_de;
 	int retval, force_da_alloc = 0;
-
-	if (new_dentry->d_inode && S_ISDIR(new_dentry->d_inode->i_mode))
-		dentry_unhash(new_dentry);
 
 	dquot_initialize(old_dir);
 	dquot_initialize(new_dir);
