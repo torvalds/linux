93628ffb9ba67c154849ac6c387f98f5e3198b84
Jan Kara <jack@suse.cz>
2011-05-24 12:00:54 -0400

    ext4: fix waiting and sending of a barrier in ext4_sync_file()
    jbd2_log_start_commit() returns 1 only when we really start a
    transaction.  But we also need to wait for a transaction when the
    commit is already running.  Fix this problem by waiting for
    transaction commit unconditionally (which is just a quick check if the
    transaction is already committed).
    
    Also we have to be more careful with sending of a barrier because when
    transaction is being committed in parallel to ext4_sync_file()
    running, we cannot be sure that the barrier the journalling code sends
    happens after we wrote all the data for fsync (note that not every
    data writeout needs to trigger metadata changes thus commit of some
    metadata changes can be running while other data is still written
    out). So use jbd2_will_send_data_barrier() helper to detect the common
    cases when we can be sure barrier will be issued by the commit code
    and issue the barrier ourselves in the remaining cases.
    
    Reported-by: Edward Goggin <egoggin@vmware.com>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/fsync.c
+++ fs/ext4/fsync.c
@@ -172,6 +172,7 @@ int ext4_sync_file(struct file *file, int datasync)
 	journal_t *journal = EXT4_SB(inode->i_sb)->s_journal;
 	int ret;
 	tid_t commit_tid;
+	bool needs_barrier = false;
 
 	J_ASSERT(ext4_journal_current_handle() == NULL);
 
@@ -211,22 +212,12 @@ int ext4_sync_file(struct file *file, int datasync)
 	}
 
 	commit_tid = datasync ? ei->i_datasync_tid : ei->i_sync_tid;
-	if (jbd2_log_start_commit(journal, commit_tid)) {
-		/*
-		 * When the journal is on a different device than the
-		 * fs data disk, we need to issue the barrier in
-		 * writeback mode.  (In ordered mode, the jbd2 layer
-		 * will take care of issuing the barrier.  In
-		 * data=journal, all of the data blocks are written to
-		 * the journal device.)
-		 */
-		if (ext4_should_writeback_data(inode) &&
-		    (journal->j_fs_dev != journal->j_dev) &&
-		    (journal->j_flags & JBD2_BARRIER))
-			blkdev_issue_flush(inode->i_sb->s_bdev, GFP_KERNEL,
-					NULL);
-		ret = jbd2_log_wait_commit(journal, commit_tid);
-	} else if (journal->j_flags & JBD2_BARRIER)
+	if (journal->j_flags & JBD2_BARRIER &&
+	    !jbd2_trans_will_send_data_barrier(journal, commit_tid))
+		needs_barrier = true;
+	jbd2_log_start_commit(journal, commit_tid);
+	ret = jbd2_log_wait_commit(journal, commit_tid);
+	if (needs_barrier)
 		blkdev_issue_flush(inode->i_sb->s_bdev, GFP_KERNEL, NULL);
  out:
 	trace_ext4_sync_file_exit(inode, ret);
