e1c78de9a462f45dacdc8c27baf77aa680bcf8a4
Roland Vossen <rvossen@broadcom.com>
2011-05-24 13:35:21 +0200

    staging: brcm80211: fix for 'multiple definition of wl_msg_level' build err
    Reported-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Roland Vossen <rvossen@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmfmac/wl_iw.c
+++ drivers/staging/brcm80211/brcmfmac/wl_iw.c
@@ -64,8 +64,6 @@ wl_iw_extra_params_t g_wl_iw_params;
 extern bool wl_iw_conn_status_str(u32 event_type, u32 status,
 				  u32 reason, char *stringBuf, uint buflen);
 
-uint wl_msg_level = WL_ERROR_VAL;
-
 #define MAX_WLIW_IOCTL_LEN 1024
 
 #ifdef CONFIG_WIRELESS_EXT
