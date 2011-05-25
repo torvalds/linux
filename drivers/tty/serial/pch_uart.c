023bc8e75f8cf9e4da8411154be22a4f809d3314
Andrew Morton <akpm@linux-foundation.org>
2011-05-24 17:13:44 -0700

    drivers/tty/serial/pch_uart.c needs slab.h
    alpha allmodconfig:
    
      drivers/tty/serial/pch_uart.c: In function 'dma_handle_tx':
      drivers/tty/serial/pch_uart.c:873: error: implicit declaration of function 'kzalloc'
      drivers/tty/serial/pch_uart.c:873: warning: assignment makes pointer from integer without a cast
      drivers/tty/serial/pch_uart.c: In function 'pch_uart_init_port':
      drivers/tty/serial/pch_uart.c:1403: warning: assignment makes pointer from integer without a cast
    
    Cc: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Cc: Greg KH <greg@kroah.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/tty/serial/pch_uart.c
+++ drivers/tty/serial/pch_uart.c
@@ -15,6 +15,7 @@
  *Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
  */
 #include <linux/serial_reg.h>
+#include <linux/slab.h>
 #include <linux/module.h>
 #include <linux/pci.h>
 #include <linux/serial_core.h>
