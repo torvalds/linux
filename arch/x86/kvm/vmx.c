7b8050f570a03718d21fc8662c54586192ea2dac
Nadav Har'El <nyh@il.ibm.com>
2011-05-25 23:16:10 +0300

    KVM: nVMX: Add VMX to list of supported cpuid features
    If the "nested" module option is enabled, add the "VMX" CPU feature to the
    list of CPU features KVM advertises with the KVM_GET_SUPPORTED_CPUID ioctl.
    
    Qemu uses this ioctl, and intersects KVM's list with its own list of desired
    cpu features (depending on the -cpu option given to qemu) to determine the
    final list of features presented to the guest.
    
    Signed-off-by: Nadav Har'El <nyh@il.ibm.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -6299,6 +6299,8 @@ static void vmx_cpuid_update(struct kvm_vcpu *vcpu)
 
 static void vmx_set_supported_cpuid(u32 func, struct kvm_cpuid_entry2 *entry)
 {
+	if (func == 1 && nested)
+		entry->ecx |= bit(X86_FEATURE_VMX);
 }
 
 /*
