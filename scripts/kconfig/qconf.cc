bac6aa865b3dc98e9fbc17f11d4d513d6b0bc435
Michal Marek <mmarek@suse.cz>
2011-05-25 15:10:25 +0200

    xconfig: Abort close if configuration cannot be saved
    Give the user an opportunity to fix the error or save the configuration
    under a different path.
    
    Reported-by: Hiromu Yakura <hiromu1996@gmail.com>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/kconfig/qconf.cc
+++ scripts/kconfig/qconf.cc
@@ -1478,10 +1478,13 @@ void ConfigMainWindow::loadConfig(void)
 	ConfigView::updateListAll();
 }
 
-void ConfigMainWindow::saveConfig(void)
+bool ConfigMainWindow::saveConfig(void)
 {
-	if (conf_write(NULL))
+	if (conf_write(NULL)) {
 		QMessageBox::information(this, "qconf", _("Unable to save configuration!"));
+		return false;
+	}
+	return true;
 }
 
 void ConfigMainWindow::saveConfigAs(void)
@@ -1642,7 +1645,11 @@ void ConfigMainWindow::closeEvent(QCloseEvent* e)
 	mb.setButtonText(QMessageBox::Cancel, _("Cancel Exit"));
 	switch (mb.exec()) {
 	case QMessageBox::Yes:
-		saveConfig();
+		if (saveConfig())
+			e->accept();
+		else
+			e->ignore();
+		break;
 	case QMessageBox::No:
 		e->accept();
 		break;
