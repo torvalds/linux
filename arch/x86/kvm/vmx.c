99e65e805dea4df061aa4038211112aa96416412
Nadav Har'El <nyh@il.ibm.com>
2011-05-25 23:11:03 +0300

    KVM: nVMX: No need for handle_vmx_insn function any more
    Before nested VMX support, the exit handler for a guest executing a VMX
    instruction (vmclear, vmlaunch, vmptrld, vmptrst, vmread, vmread, vmresume,
    vmwrite, vmon, vmoff), was handle_vmx_insn(). This handler simply threw a #UD
    exception. Now that all these exit reasons are properly handled (and emulate
    the respective VMX instruction), nothing calls this dummy handler and it can
    be removed.
    
    Signed-off-by: Nadav Har'El <nyh@il.ibm.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -4309,12 +4309,6 @@ static int handle_vmcall(struct kvm_vcpu *vcpu)
 	return 1;
 }
 
-static int handle_vmx_insn(struct kvm_vcpu *vcpu)
-{
-	kvm_queue_exception(vcpu, UD_VECTOR);
-	return 1;
-}
-
 static int handle_invd(struct kvm_vcpu *vcpu)
 {
 	return emulate_instruction(vcpu, 0) == EMULATE_DONE;
