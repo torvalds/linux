2f7d63f909900c555baf36a4c6a11e9bf8e1af18
Mike Frysinger <vapier@gentoo.org>
2011-05-26 04:03:10 -0400

    Blackfin: boards: clean up redundant/dead spi resources
    The default for the Blackfin SPI driver is 8 bits and dma disabled,
    so many of the bfin5xx_spi_chip resources are redundant.  So punt
    those parts.
    
    Further, drivers should themselves be declaring 16 bit transfers,
    so for those that do, and for the ones which no longer do 16 bit
    transfers, drop the bfin5xx_spi_chip resources.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- arch/blackfin/mach-bf561/boards/ezkit.c
+++ arch/blackfin/mach-bf561/boards/ezkit.c
@@ -286,21 +286,6 @@ static struct platform_device ezkit_flash_device = {
 };
 #endif
 
-#if defined(CONFIG_SND_BF5XX_SOC_AD183X) \
-	|| defined(CONFIG_SND_BF5XX_SOC_AD183X_MODULE)
-static struct bfin5xx_spi_chip ad1836_spi_chip_info = {
-	.enable_dma = 0,
-	.bits_per_word = 16,
-};
-#endif
-
-#if defined(CONFIG_SPI_SPIDEV) || defined(CONFIG_SPI_SPIDEV_MODULE)
-static struct bfin5xx_spi_chip spidev_chip_info = {
-	.enable_dma = 0,
-	.bits_per_word = 8,
-};
-#endif
-
 #if defined(CONFIG_SPI_BFIN) || defined(CONFIG_SPI_BFIN_MODULE)
 /* SPI (0) */
 static struct resource bfin_spi0_resource[] = {
@@ -348,7 +333,6 @@ static struct spi_board_info bfin_spi_board_info[] __initdata = {
 		.bus_num = 0,
 		.chip_select = 4,
 		.platform_data = "ad1836", /* only includes chip name for the moment */
-		.controller_data = &ad1836_spi_chip_info,
 		.mode = SPI_MODE_3,
 	},
 #endif
@@ -358,7 +342,6 @@ static struct spi_board_info bfin_spi_board_info[] __initdata = {
 		.max_speed_hz = 3125000,     /* max spi clock (SCK) speed in HZ */
 		.bus_num = 0,
 		.chip_select = 1,
-		.controller_data = &spidev_chip_info,
 	},
 #endif
 };
