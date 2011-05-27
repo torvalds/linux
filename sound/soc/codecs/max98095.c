74ab24af4fe165de5af01d0507250dd099f096b0
Axel Lin <axel.lin@gmail.com>
2011-05-27 23:30:53 +0800

    ASoC: Remove redundant freq assignment for max98095->sysclk/max98088->sysclk
    Current implementation set max98095->sysclk/max98088->sysclk to freq twice.
    Set it once is enough, this patch removes the first assignment in case
    we may set invalid clock frequency to max98095->sysclk/max98088->sysclk.
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Acked-by: Peter Hsiang <peter.hsiang@maxim-ic.com>
    Acked-by: Liam Girdwood <lrg@ti.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/max98095.c
+++ sound/soc/codecs/max98095.c
@@ -1517,8 +1517,6 @@ static int max98095_dai_set_sysclk(struct snd_soc_dai *dai,
 	if (freq == max98095->sysclk)
 		return 0;
 
-	max98095->sysclk = freq; /* remember current sysclk */
-
 	/* Setup clocks for slave mode, and using the PLL
 	 * PSCLK = 0x01 (when master clk is 10MHz to 20MHz)
 	 *         0x02 (when master clk is 20MHz to 40MHz)..
