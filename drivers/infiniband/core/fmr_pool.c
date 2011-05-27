b108d9764cff25262bf764542ed1998d3e568962
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-05-27 15:29:33 -0400

    infiniband: add in export.h for files using EXPORT_SYMBOL/THIS_MODULE
    These were getting it implicitly via device.h --> module.h but
    we are going to stop that when we clean up the headers.
    
    Fix these in advance so the tree remains biscect-clean.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/infiniband/core/fmr_pool.c
+++ drivers/infiniband/core/fmr_pool.c
@@ -33,6 +33,7 @@
 
 #include <linux/errno.h>
 #include <linux/spinlock.h>
+#include <linux/export.h>
 #include <linux/slab.h>
 #include <linux/jhash.h>
 #include <linux/kthread.h>
--- drivers/infiniband/hw/mthca/mthca_provider.c
+++ drivers/infiniband/hw/mthca/mthca_provider.c
@@ -42,6 +42,7 @@
 #include <linux/slab.h>
 #include <linux/stat.h>
 #include <linux/mm.h>
+#include <linux/export.h>
 
 #include "mthca_dev.h"
 #include "mthca_cmd.h"
