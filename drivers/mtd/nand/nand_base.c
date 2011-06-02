5507766b639e53b9230484588eba8b4e18d0563b
Brian Norris <computersforpeace@gmail.com>
2011-06-02 11:38:26 -0700

    mtd: nand: remove unnecessary TODO
    I believe this TODO was unnecessary back when it was introduced:
    	commit d1e1f4e42b5df063712ca2926e50c07b95c96b96
    	mtd: nand: add support for reading ONFI parameters...
    
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- drivers/mtd/nand/nand_base.c
+++ drivers/mtd/nand/nand_base.c
@@ -3135,7 +3135,6 @@ ident_done:
 	if (mtd->writesize > 512 && chip->cmdfunc == nand_command)
 		chip->cmdfunc = nand_command_lp;
 
-	/* TODO onfi flash name */
 	printk(KERN_INFO "NAND device: Manufacturer ID:"
 		" 0x%02x, Chip ID: 0x%02x (%s %s)\n", *maf_id, *dev_id,
 		nand_manuf_ids[maf_idx].name,
