bf5140817b2d65faac9b32fc9057a097044ac35b
Peter Wippich <pewi@gw-instruments.de>
2011-06-06 15:50:58 +0200

    mtd: mtdchar: add missing initializer on raw write
    On writes in MODE_RAW the mtd_oob_ops struct is not sufficiently
    initialized which may cause nandwrite to fail. With this patch
    it is possible to write raw nand/oob data without additional ECC
    (either for testing or when some sectors need different oob layout
    e.g. bootloader) like
    nandwrite  -n -r -o  /dev/mtd0 <myfile>
    
    Signed-off-by: Peter Wippich <pewi@gw-instruments.de>
    Cc: stable@kernel.org
    Tested-by: Ricard Wanderlof <ricardw@axis.com>
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- drivers/mtd/mtdchar.c
+++ drivers/mtd/mtdchar.c
@@ -320,6 +320,7 @@ static ssize_t mtd_write(struct file *file, const char __user *buf, size_t count
 			ops.mode = MTD_OOB_RAW;
 			ops.datbuf = kbuf;
 			ops.oobbuf = NULL;
+			ops.ooboffs = 0;
 			ops.len = len;
 
 			ret = mtd->write_oob(mtd, *ppos, &ops);
