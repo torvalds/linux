c162755491f7ca9853cb9f2aaa3ff3677c1bda78
Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>
2011-06-06 18:19:20 +0200

    avr32: make intc_resume() return void to conform to syscore_ops
    This patch removes the unneeded, and now wrong, return 0 from intc_resume() and
    lets the function return void instead. This matches the resume callback in
    struct syscore_ops.
    
    Signed-off-by: Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>

--- arch/avr32/mach-at32ap/intc.c
+++ arch/avr32/mach-at32ap/intc.c
@@ -167,14 +167,12 @@ static int intc_suspend(void)
 	return 0;
 }
 
-static int intc_resume(void)
+static void intc_resume(void)
 {
 	int i;
 
 	for (i = 0; i < 64; i++)
 		intc_writel(&intc0, INTPR0 + 4 * i, intc0.saved_ipr[i]);
-
-	return 0;
 }
 #else
 #define intc_suspend	NULL
