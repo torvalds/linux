2669069aacc914821b45fe14a49e7fe0ba5d10bd
Richard Cochran <richardcochran@gmail.com>
2011-06-12 02:19:02 +0000

    tg3: enable transmit time stamping.
    This patch enables software (and phy device) transmit time stamping
    for the TIGON3 driver. Compile tested only.
    
    Cc: Matt Carlson <mcarlson@broadcom.com>
    Cc: Michael Chan <mchan@broadcom.com>
    Signed-off-by: Richard Cochran <richard.cochran@omicron.at>
    Signed-off-by: David S. Miller <davem@conan.davemloft.net>

--- drivers/net/tg3.c
+++ drivers/net/tg3.c
@@ -6092,6 +6092,8 @@ static netdev_tx_t tg3_start_xmit(struct sk_buff *skb, struct net_device *dev)
 	/* Packets are ready, update Tx producer idx local and on card. */
 	tw32_tx_mbox(tnapi->prodmbox, entry);
 
+	skb_tx_timestamp(skb);
+
 	tnapi->tx_prod = entry;
 	if (unlikely(tg3_tx_avail(tnapi) <= (MAX_SKB_FRAGS + 1))) {
 		netif_tx_stop_queue(txq);
