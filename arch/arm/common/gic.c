9c12845ee49716209cb2b087c0b47c3e37096bde
Colin Cross <ccross@android.com>
2011-06-13 00:45:59 +0000

    ARM: gic: Allow gic arch extensions to provide irqchip flags
    Tegra can benefit from the IRQCHIP_MASK_ON_SUSPEND flag, allow it
    to be passed to the gic irq chip.
    
    Signed-off-by: Colin Cross <ccross@android.com>
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Reviewed-by: Kevin Hilman <khilman@ti.com>
    Tested-and-Acked-by: Shawn Guo <shawn.guo@linaro.org>
    Tested-by: Vishwanath BS <vishwanath.bs@ti.com>

--- arch/arm/common/gic.c
+++ arch/arm/common/gic.c
@@ -544,6 +544,7 @@ void __init gic_init(unsigned int gic_nr, unsigned int irq_start,
 	if (gic_nr == 0)
 		gic_cpu_base_addr = cpu_base;
 
+	gic_chip.flags |= gic_arch_extn.flags;
 	gic_dist_init(gic, irq_start);
 	gic_cpu_init(gic);
 	gic_pm_init(gic);
