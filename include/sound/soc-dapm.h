169d5a83f687c37104d51cbaa639bdffca8a1cd3
Mark Brown <broonie@opensource.wolfsonmicro.com>
2011-06-14 09:51:50 +0100

    ASoC: Fix mismerge with release branch
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- include/sound/soc-dapm.h
+++ include/sound/soc-dapm.h
@@ -431,6 +431,7 @@ struct snd_soc_dapm_path {
 	/* status */
 	u32 connect:1;	/* source and sink widgets are connected */
 	u32 walked:1;	/* path has been walked */
+	u32 weak:1;	/* path ignored for power management */
 
 	int (*connected)(struct snd_soc_dapm_widget *source,
 			 struct snd_soc_dapm_widget *sink);
