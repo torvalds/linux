0cf9cd026adeacc3fe5e88e4d3e7f8f3aeff6790
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2011-06-14 09:25:03 +0000

    vfb: use display information in info not in var for panning
    We must not use any information in the passed var besides xoffset,
    yoffset and vmode as otherwise applications might abuse it.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- drivers/video/vfb.c
+++ drivers/video/vfb.c
@@ -395,8 +395,8 @@ static int vfb_pan_display(struct fb_var_screeninfo *var,
 		    || var->xoffset)
 			return -EINVAL;
 	} else {
-		if (var->xoffset + var->xres > info->var.xres_virtual ||
-		    var->yoffset + var->yres > info->var.yres_virtual)
+		if (var->xoffset + info->var.xres > info->var.xres_virtual ||
+		    var->yoffset + info->var.yres > info->var.yres_virtual)
 			return -EINVAL;
 	}
 	info->var.xoffset = var->xoffset;
