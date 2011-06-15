0b7287a96ba746ca04aa71afb752801a77a1cbbc
黄涛 <huangtao@rock-chips.com>
2011-06-15 16:28:38 +0800

    rk29: ddr3sdk/sdk: set gpu low clock same as general pll clock rate, save more power

--- arch/arm/mach-rk29/board-rk29sdk.c
+++ arch/arm/mach-rk29/board-rk29sdk.c
@@ -1265,7 +1265,7 @@ static struct platform_device rk29sdk_rfkill = {
 
 #ifdef CONFIG_VIVANTE
 #define GPU_HIGH_CLOCK        552
-#define GPU_LOW_CLOCK         300
+#define GPU_LOW_CLOCK         288 /* same as general pll clock rate below */
 static struct resource resources_gpu[] = {
     [0] = {
 		.name 	= "gpu_irq",
