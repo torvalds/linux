a12d59168233eb1ef96732c4ee45890e96fa5550
Todd Poynor <toddpoynor@google.com>
2011-06-15 17:21:57 -0700

    Move x86_64 idle notifiers to generic
    Move the x86_64 idle notifiers originally by Andi Kleen and Venkatesh
    Pallipadi to generic.
    
    Change-Id: Idf29cda15be151f494ff245933c12462643388d5
    Acked-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -212,4 +212,11 @@ static inline int disable_nonboot_cpus(void) { return 0; }
 static inline void enable_nonboot_cpus(void) {}
 #endif /* !CONFIG_PM_SLEEP_SMP */
 
+#define IDLE_START 1
+#define IDLE_END 2
+
+void idle_notifier_register(struct notifier_block *n);
+void idle_notifier_unregister(struct notifier_block *n);
+void idle_notifier_call_chain(unsigned long val);
+
 #endif /* _LINUX_CPU_H_ */
