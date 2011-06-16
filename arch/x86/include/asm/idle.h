9edb824ebe13a16a17b989e07495c6f2e46982fa
Todd Poynor <toddpoynor@google.com>
2011-06-15 17:21:57 -0700

    Move x86_64 idle notifiers to generic
    Move the x86_64 idle notifiers originally by Andi Kleen and Venkatesh
    Pallipadi to generic.
    
    Change-Id: Idf29cda15be151f494ff245933c12462643388d5
    Acked-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- arch/x86/include/asm/idle.h
+++ arch/x86/include/asm/idle.h
@@ -1,13 +1,6 @@
 #ifndef _ASM_X86_IDLE_H
 #define _ASM_X86_IDLE_H
 
-#define IDLE_START 1
-#define IDLE_END 2
-
-struct notifier_block;
-void idle_notifier_register(struct notifier_block *n);
-void idle_notifier_unregister(struct notifier_block *n);
-
 #ifdef CONFIG_X86_64
 void enter_idle(void);
 void exit_idle(void);
