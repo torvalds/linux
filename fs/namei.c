6291176bcd71a2766a19a10cbd9bab07d289e1d7
Al Viro <viro@zeniv.linux.org.uk>
2011-06-17 19:20:48 -0400

    kill obsolete comment for follow_down()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/namei.c
+++ fs/namei.c
@@ -1011,9 +1011,6 @@ failed:
  * Follow down to the covering mount currently visible to userspace.  At each
  * point, the filesystem owning that dentry may be queried as to whether the
  * caller is permitted to proceed or not.
- *
- * Care must be taken as namespace_sem may be held (indicated by mounting_here
- * being true).
  */
 int follow_down(struct path *path)
 {
