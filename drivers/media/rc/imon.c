372b4249243d1e7c12a0f2b67e7badc608fcff45
Jarod Wilson <jarod@redhat.com>
2011-06-20 00:07:13 -0300

    [media] imon: allow either proto on unknown 0xffdc
    While 0xffdc devices have their IR protocol hard-coded into the firmware
    of the device, we have no known way of telling what it is if we don't
    have the device's config byte already in the driver. Unknown devices
    default to the imon native protocol, but might actually be rc6, so we
    should set the driver up such that the user can load the rc6 keytable
    from userspace and still have a working device ahead of its config byte
    being added to the driver.
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/rc/imon.c
+++ drivers/media/rc/imon.c
@@ -1767,6 +1767,9 @@ static void imon_get_ffdc_type(struct imon_context *ictx)
 		dev_info(ictx->dev, "Unknown 0xffdc device, "
 			 "defaulting to VFD and iMON IR");
 		detected_display_type = IMON_DISPLAY_TYPE_VFD;
+		/* We don't know which one it is, allow user to set the
+		 * RC6 one from userspace if OTHER wasn't correct. */
+		allowed_protos |= RC_TYPE_RC6;
 		break;
 	}
 
