877ca0fe3a19ef0ceaae6e99c89860fa39119ef7
JP Abgrall <jpa@google.com>
2011-06-20 12:41:46 -0700

    netfilter: add xt_qtaguid matching module
    This module allows tracking stats at the socket level for given UIDs.
    It replaces xt_owner.
    If the --uid-owner is not specified, it will just count stats based on
    who the skb belongs to. This will even happen on incoming skbs as it
    looks into the skb via xt_socket magic to see who owns it.
    If an skb is lost, it will be assigned to uid=0.
    
    To control what sockets of what UIDs are tagged by what, one uses:
      echo t $sock_fd $accounting_tag $the_billed_uid \
         > /proc/net/xt_qtaguid/ctrl
     So whenever an skb belongs to a sock_fd, it will be accounted against
       $the_billed_uid
      and matching stats will show up under the uid with the given
       $accounting_tag.
    
    Because the number of allocations for the stats structs is not that big:
      ~500 apps * 32 per app
    we'll just do it atomic. This avoids walking lists many times, and
    the fancy worker thread handling. Slabs will grow when needed later.
    
    It use netdevice and inetaddr notifications instead of hooks in the core dev
    code to track when a device comes and goes. This removes the need for
    exposed iface_stat.h.
    
    Put procfs dirs in /proc/net/xt_qtaguid/
      ctrl
      stats
      iface_stat/<iface>/...
    The uid stats are obtainable in ./stats.
    
    Change-Id: I01af4fd91c8de651668d3decb76d9bdc1e343919
    Signed-off-by: JP Abgrall <jpa@google.com>

--- include/linux/android_aid.h
+++ include/linux/android_aid.h
@@ -22,5 +22,7 @@
 #define AID_INET         KGIDT_INIT(3003)
 #define AID_NET_RAW      KGIDT_INIT(3004)
 #define AID_NET_ADMIN    KGIDT_INIT(3005)
+#define AID_NET_BW_STATS KGIDT_INIT(3006)  /* read bandwidth statistics */
+#define AID_NET_BW_ACCT  KGIDT_INIT(3007)  /* change bandwidth statistics accounting */
 
 #endif
