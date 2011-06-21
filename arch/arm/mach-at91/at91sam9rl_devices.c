5c1f9668692061b97125e343721c7514ca05a8bb
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2011-06-21 11:24:33 +0800

    at91: fix at91_set_serial_console: use platform device id
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>

--- arch/arm/mach-at91/at91sam9rl_devices.c
+++ arch/arm/mach-at91/at91sam9rl_devices.c
@@ -1168,7 +1168,7 @@ void __init at91_set_serial_console(unsigned portnr)
 {
 	if (portnr < ATMEL_MAX_UART) {
 		atmel_default_console_device = at91_uarts[portnr];
-		at91sam9rl_set_console_clock(portnr);
+		at91sam9rl_set_console_clock(at91_uarts[portnr]->id);
 	}
 }
 
