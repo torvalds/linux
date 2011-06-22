10f046bb1df0e40bbc56579ca5ccad5f951f4047
Fabio Estevam <fabio.estevam@freescale.com>
2011-06-22 09:25:26 -0300

    ARM: mach-imx/mx27_3ds: Add touchscreen support
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-imx/mach-mx27_3ds.c
+++ arch/arm/mach-imx/mach-mx27_3ds.c
@@ -293,7 +293,7 @@ static struct mc13xxx_platform_data mc13783_pdata = {
 		.num_regulators = ARRAY_SIZE(mx27_3ds_regulators),
 
 	},
-	.flags  = MC13783_USE_REGULATOR,
+	.flags  = MC13783_USE_REGULATOR | MC13783_USE_TOUCHSCREEN,
 };
 
 /* SPI */
