0bb04bf3dfdfe1c981087cdfb0d9d772c3a0ba55
William Douglas <william.douglas@intel.com>
2011-06-23 13:38:36 +0100

    mrst_max3110: Change max missing message priority.
    Change print message to notice instead of error to clean up non critical
    messages showing on startup.  The MAX3111 not being present is a normal
    path for end user systems.
    
    Signed-off-by: William Douglas <william.douglas@intel.com>
    [rebased on 3.0, switched to dev_dbg()]
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/tty/serial/mrst_max3110.c
+++ drivers/tty/serial/mrst_max3110.c
@@ -823,7 +823,7 @@ static int __devinit serial_m3110_probe(struct spi_device *spi)
 	res = RC_TAG;
 	ret = max3110_write_then_read(max, (u8 *)&res, (u8 *)&res, 2, 0);
 	if (ret < 0 || res == 0 || res == 0xffff) {
-		printk(KERN_ERR "MAX3111 deemed not present (conf reg %04x)",
+		dev_dbg(&spi->dev, "MAX3111 deemed not present (conf reg %04x)",
 									res);
 		ret = -ENODEV;
 		goto err_get_page;
