589327905cf0ce4402f7fb1ed29682f7ae68a82e
Ben Hutchings <bhutchings@solarflare.com>
2011-06-25 00:22:08 +0100

    sfc: Fix assertions in efx_filter_rfs()
    This function is intended to assert (when DEBUG is defined) that the
    skb header area includes the header fields it's looking at, which RFS
    should already have pulled.  But it uses pskb_may_pull(), which will
    attempt to pull more data if necesary.  It must instead compare
    skb_headlen() with the required length.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>

--- drivers/net/sfc/filter.c
+++ drivers/net/sfc/filter.c
@@ -657,11 +657,11 @@ int efx_filter_rfs(struct net_device *net_dev, const struct sk_buff *skb,
 		return -EPROTONOSUPPORT;
 
 	/* RFS must validate the IP header length before calling us */
-	EFX_BUG_ON_PARANOID(!pskb_may_pull(skb, nhoff + sizeof(*ip)));
+	EFX_BUG_ON_PARANOID(skb_headlen(skb) < nhoff + sizeof(*ip));
 	ip = (const struct iphdr *)(skb->data + nhoff);
 	if (ip_is_fragment(ip))
 		return -EPROTONOSUPPORT;
-	EFX_BUG_ON_PARANOID(!pskb_may_pull(skb, nhoff + 4 * ip->ihl + 4));
+	EFX_BUG_ON_PARANOID(skb_headlen(skb) < nhoff + 4 * ip->ihl + 4);
 	ports = (const __be16 *)(skb->data + nhoff + 4 * ip->ihl);
 
 	efx_filter_init_rx(&spec, EFX_FILTER_PRI_HINT, 0, rxq_index);
