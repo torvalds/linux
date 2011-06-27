7df5659eefad9b6d457ccdee016bd78bd064cfc0
Arnd Bergmann <arnd@arndb.de>
2011-06-27 11:45:16 +0000

    serial/8250: Move UPIO_TSI to powerpc
    This iotype is only used by the legacy_serial code in powerpc, so the
    code should live there, rather than be compiled in for every 8250
    driver.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: linuxppc-dev@lists.ozlabs.org
    Cc: Greg Kroah-Hartman <gregkh@suse.de>
    Cc: linux-serial@vger.kernel.org
    Acked-by: David Daney <david.daney@cavium.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- drivers/tty/serial/8250.c
+++ drivers/tty/serial/8250.c
@@ -443,24 +443,6 @@ static void au_serial_out(struct uart_port *p, int offset, int value)
 	__raw_writel(value, p->membase + offset);
 }
 
-static unsigned int tsi_serial_in(struct uart_port *p, int offset)
-{
-	unsigned int tmp;
-	offset = map_8250_in_reg(p, offset) << p->regshift;
-	if (offset == UART_IIR) {
-		tmp = readl(p->membase + (UART_IIR & ~3));
-		return (tmp >> 16) & 0xff; /* UART_IIR % 4 == 2 */
-	} else
-		return readb(p->membase + offset);
-}
-
-static void tsi_serial_out(struct uart_port *p, int offset, int value)
-{
-	offset = map_8250_out_reg(p, offset) << p->regshift;
-	if (!((offset == UART_IER) && (value & UART_IER_UUE)))
-		writeb(value, p->membase + offset);
-}
-
 /* Save the LCR value so it can be re-written when a Busy Detect IRQ occurs. */
 static inline void dwapb_save_out_value(struct uart_port *p, int offset,
 					int value)
@@ -533,11 +515,6 @@ static void set_io_from_upio(struct uart_port *p)
 	case UPIO_AU:
 		p->serial_in = au_serial_in;
 		p->serial_out = au_serial_out;
-		break;
-
-	case UPIO_TSI:
-		p->serial_in = tsi_serial_in;
-		p->serial_out = tsi_serial_out;
 		break;
 
 	case UPIO_DWAPB:
