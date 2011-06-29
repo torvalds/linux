65d13516b2358c38ac56a5f83e989a6837dcf825
Wonil Choi <wonil22.choi@samsung.com>
2011-06-29 11:38:38 +0900

    mmc: sdhci-s3c: Fix return value in sdhci_s3c_suspend/resume()
    Signed-off-by: Wonil Choi <wonil22.choi@samsung.com>
    Signed-off-by: Minho Ban <mhban@samsung.com>
    Cc: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/sdhci-s3c.c
+++ drivers/mmc/host/sdhci-s3c.c
@@ -612,16 +612,14 @@ static int sdhci_s3c_suspend(struct platform_device *dev, pm_message_t pm)
 {
 	struct sdhci_host *host = platform_get_drvdata(dev);
 
-	sdhci_suspend_host(host, pm);
-	return 0;
+	return sdhci_suspend_host(host, pm);
 }
 
 static int sdhci_s3c_resume(struct platform_device *dev)
 {
 	struct sdhci_host *host = platform_get_drvdata(dev);
 
-	sdhci_resume_host(host);
-	return 0;
+	return sdhci_resume_host(host);
 }
 
 #else
