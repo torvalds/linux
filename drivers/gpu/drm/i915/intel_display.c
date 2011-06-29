987ccebc15b3f1256dbcdfbb81ad542d1e2ad6a8
Jesse Barnes <jbarnes@virtuousgeek.org>
2011-06-29 13:34:36 -0700

    drm/i915: enable ring freq scaling, RC6 and graphics turbo on Ivy Bridge v3
    commit 1c70c0cebd1295a42fec75045b8a6b4419cedef3 upstream.
    
    They use the same register interfaces, so we can simply enable the
    existing code on IVB.
    
    v2:
      - resolve conflict with ring freq scaling, we can enable it too
    v3:
      - resolve conflict again, this time on drm-intel-next
    
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Keith Packard <keithp@keithp.com>
    Signed-off-by: Robert Hooker <robert.hooker@canonical.com>
    Acked-by: Leann Ogasawara <leann.ogasawara@canonical.com>
    Acked-by: Herton Krzesinski <herton.krzesinski@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -7943,7 +7943,7 @@ void intel_modeset_init(struct drm_device *dev)
 		intel_init_emon(dev);
 	}
 
-	if (IS_GEN6(dev))
+	if (IS_GEN6(dev) || IS_GEN7(dev))
 		gen6_enable_rps(dev_priv);
 
 	INIT_WORK(&dev_priv->idle_work, intel_idle_update);
@@ -7985,7 +7985,7 @@ void intel_modeset_cleanup(struct drm_device *dev)
 
 	if (IS_IRONLAKE_M(dev))
 		ironlake_disable_drps(dev);
-	if (IS_GEN6(dev))
+	if (IS_GEN6(dev) || IS_GEN7(dev))
 		gen6_disable_rps(dev);
 
 	if (IS_IRONLAKE_M(dev))
