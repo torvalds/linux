e3e0109138376bb262b8ecf33bad0586fa131925
Rafael J. Wysocki <rjw@sisk.pl>
2011-07-01 22:13:56 +0200

    ARM / shmobile: Support for I/O power domains for SH7372 (v9)
    Use the generic power domains support introduced by the previous
    patch to implement support for power domains on SH7372.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Acked-by: Paul Mundt <lethal@linux-sh.org>

--- arch/arm/mach-shmobile/board-mackerel.c
+++ arch/arm/mach-shmobile/board-mackerel.c
@@ -1582,6 +1582,10 @@ static void __init mackerel_init(void)
 
 	platform_add_devices(mackerel_devices, ARRAY_SIZE(mackerel_devices));
 
+	sh7372_init_pm_domain(&sh7372_a4lc);
+	sh7372_add_device_to_domain(&sh7372_a4lc, &lcdc_device);
+	sh7372_add_device_to_domain(&sh7372_a4lc, &hdmi_lcdc_device);
+
 	hdmi_init_pm_clock();
 	sh7372_pm_init();
 }
