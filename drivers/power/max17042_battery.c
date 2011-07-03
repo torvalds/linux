7e6d62db5efecded2dd4606ff2ff3726b9c52505
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-03 15:28:29 -0400

    drivers/power: Add module.h to the actual modules in drivers/power
    The module.h header is no longer going to be implicitly present
    everywhere.  So real modular users need to call out its use
    explicitly in advance.
    
    [v2: add new users reported by Randy Dunlap <rdunlap@xenotime.net>]
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/power/max17042_battery.c
+++ drivers/power/max17042_battery.c
@@ -23,6 +23,7 @@
  */
 
 #include <linux/init.h>
+#include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/i2c.h>
 #include <linux/mod_devicetable.h>
--- drivers/power/max8998_charger.c
+++ drivers/power/max8998_charger.c
@@ -19,6 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <linux/module.h>
 #include <linux/err.h>
 #include <linux/module.h>
 #include <linux/slab.h>
