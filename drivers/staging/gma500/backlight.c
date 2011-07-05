6a8ca6f5ffbca4ef92da34bd28465539c511b97e
Alan Cox <alan@linux.intel.com>
2011-07-05 15:44:34 +0100

    gma500: Fix backlight crash
    We need to check the NULL case earlier.
    
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/gma500/backlight.c
+++ drivers/staging/gma500/backlight.c
@@ -38,9 +38,10 @@ void gma_backlight_exit(struct drm_device *dev)
 {
 #ifdef CONFIG_BACKLIGHT_CLASS_DEVICE
 	struct drm_psb_private *dev_priv = dev->dev_private;
-	dev_priv->backlight_device->props.brightness = 0;
-	backlight_update_status(dev_priv->backlight_device);
-	if (dev_priv->backlight_device)
+	if (dev_priv->backlight_device) {
+		dev_priv->backlight_device->props.brightness = 0;
+		backlight_update_status(dev_priv->backlight_device);
 		backlight_device_unregister(dev_priv->backlight_device);
+	}
 #endif
 }
