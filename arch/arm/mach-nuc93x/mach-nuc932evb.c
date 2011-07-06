03150fb6a5eb66d1fd99286991dbbecf2b28fc3e
Nicolas Pitre <nicolas.pitre@linaro.org>
2011-07-05 22:38:14 -0400

    ARM: mach-nuc93x: convert boot_params to atag_offset
    Signed-off-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-nuc93x/mach-nuc932evb.c
+++ arch/arm/mach-nuc93x/mach-nuc932evb.c
@@ -35,7 +35,6 @@ static void __init nuc932evb_init(void)
 
 MACHINE_START(NUC932EVB, "NUC932EVB")
 	/* Maintainer: Wan ZongShun */
-	.boot_params	= 0,
 	.map_io		= nuc932evb_map_io,
 	.init_irq	= nuc93x_init_irq,
 	.init_machine	= nuc932evb_init,
