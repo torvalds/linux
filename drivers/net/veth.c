81b16ba2f1cc93a1ee1dda48be2ea2d91a0cb72e
Eric Dumazet <eric.dumazet@gmail.com>
2011-07-06 01:51:04 -0700

    veth: Kill unused tx_dropped
    Followup to commit f82528bc13a (Exclude duplicated checking for
    iface-up) : We no longer need percpu tx_dropped field.
    
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/veth.c
+++ drivers/net/veth.c
@@ -30,7 +30,6 @@ struct veth_net_stats {
 	u64			rx_bytes;
 	u64			tx_bytes;
 	u64			rx_dropped;
-	u64			tx_dropped;
 	struct u64_stats_sync	syncp;
 };
 
@@ -168,7 +167,7 @@ static struct rtnl_link_stats64 *veth_get_stats64(struct net_device *dev,
 	for_each_possible_cpu(cpu) {
 		struct veth_net_stats *stats = per_cpu_ptr(priv->stats, cpu);
 		u64 rx_packets, rx_bytes, rx_dropped;
-		u64 tx_packets, tx_bytes, tx_dropped;
+		u64 tx_packets, tx_bytes;
 		unsigned int start;
 
 		do {
@@ -178,14 +177,12 @@ static struct rtnl_link_stats64 *veth_get_stats64(struct net_device *dev,
 			rx_bytes = stats->rx_bytes;
 			tx_bytes = stats->tx_bytes;
 			rx_dropped = stats->rx_dropped;
-			tx_dropped = stats->tx_dropped;
 		} while (u64_stats_fetch_retry_bh(&stats->syncp, start));
 		tot->rx_packets += rx_packets;
 		tot->tx_packets += tx_packets;
 		tot->rx_bytes   += rx_bytes;
 		tot->tx_bytes   += tx_bytes;
 		tot->rx_dropped += rx_dropped;
-		tot->tx_dropped += tx_dropped;
 	}
 
 	return tot;
