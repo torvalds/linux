af6871683e25100813d35b3d802f1ed7b90b9f3a
Nicolas Pitre <nicolas.pitre@linaro.org>
2011-07-05 22:38:19 -0400

    ARM: remove boot_params from struct machine_desc
    Now that there is no more users, we can remove it from the kernel.
    
    Signed-off-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -821,25 +821,6 @@ static struct machine_desc * __init setup_machine_tags(unsigned int nr)
 		tags = phys_to_virt(__atags_pointer);
 	else if (mdesc->atag_offset)
 		tags = (void *)(PAGE_OFFSET + mdesc->atag_offset);
-	else if (mdesc->boot_params) {
-#ifdef CONFIG_MMU
-		/*
-		 * We still are executing with a minimal MMU mapping created
-		 * with the presumption that the machine default for this
-		 * is located in the first MB of RAM.  Anything else will
-		 * fault and silently hang the kernel at this point.
-		 */
-		if (mdesc->boot_params < PHYS_OFFSET ||
-		    mdesc->boot_params >= PHYS_OFFSET + SZ_1M) {
-			printk(KERN_WARNING
-			       "Default boot params at physical 0x%08lx out of reach\n",
-			       mdesc->boot_params);
-		} else
-#endif
-		{
-			tags = phys_to_virt(mdesc->boot_params);
-		}
-	}
 
 #if defined(CONFIG_DEPRECATED_PARAM_STRUCT)
 	/*
