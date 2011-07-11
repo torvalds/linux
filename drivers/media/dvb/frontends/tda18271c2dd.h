ea4d03d138d1febab586299e1a3bbc0df585f4b7
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-07-11 15:28:12 -0300

    [media] tda18271c2dd.h: Don't add the same symbol twice
    Should be folded with the next patch when sending upstream
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/tda18271c2dd.h
+++ drivers/media/dvb/frontends/tda18271c2dd.h
@@ -1,8 +1,5 @@
 #ifndef _TDA18271C2DD_H_
 #define _TDA18271C2DD_H_
-struct dvb_frontend *tda18271c2dd_attach(struct dvb_frontend *fe,
-					 struct i2c_adapter *i2c, u8 adr);
-
 #if defined(CONFIG_DVB_TDA18271C2DD) || (defined(CONFIG_DVB_TDA18271C2DD_MODULE) \
         && defined(MODULE))
 struct dvb_frontend *tda18271c2dd_attach(struct dvb_frontend *fe,
