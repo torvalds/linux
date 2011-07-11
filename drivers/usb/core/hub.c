316541bf70281b6c3b501b442b6994c1839776ad
Jesper Juhl <jj@chaosbits.net>
2011-07-11 22:59:36 +0200

    USB: Remove test for NULL that'll never happen in usb_disconnect()
    In drivers/usb/core/hub.c::usb_disconnect(), 'udev' will never be
    NULL, so remove the test and printing of debug message.
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1636,11 +1636,6 @@ void usb_disconnect(struct usb_device **pdev)
 	int			i;
 	struct usb_hcd		*hcd = bus_to_hcd(udev->bus);
 
-	if (!udev) {
-		pr_debug ("%s nodev\n", __func__);
-		return;
-	}
-
 	/* mark the device as inactive, so any further urb submissions for
 	 * this device (and any of its children) will fail immediately.
 	 * this quiesces everything except pending urbs.
