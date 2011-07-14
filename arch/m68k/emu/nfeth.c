512e4002ab9ac54a2d8f7f198a9cd503dcc3c161
Michał Mirosław <mirq-linux@rere.qmqm.pl>
2011-07-14 14:38:17 -0700

    net: m68k/nfeth: Remove wrong usage of dev->flags
    Remove wrong setting of dev->flags. NETIF_F_NO_CSUM maps to IFF_DEBUG
    there, so looks like a mistake.
    
    Signed-off-by: Michał Mirosław <mirq-linux@rere.qmqm.pl>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/m68k/emu/nfeth.c
+++ arch/m68k/emu/nfeth.c
@@ -205,7 +205,6 @@ static struct net_device * __init nfeth_probe(int unit)
 	dev->irq = nfEtherIRQ;
 	dev->netdev_ops = &nfeth_netdev_ops;
 
-	dev->flags |= NETIF_F_NO_CSUM;
 	memcpy(dev->dev_addr, mac, ETH_ALEN);
 
 	priv = netdev_priv(dev);
