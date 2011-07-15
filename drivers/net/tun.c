c001b9448b3f5dc0fcec1aaa545524057afcaa1e
Chia-chi Yeh <chiachi@android.com>
2011-07-15 15:32:57 -0700

    net: Only NET_ADMIN is allowed to fully control TUN interfaces.
    Signed-off-by: Chia-chi Yeh <chiachi@android.com>

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -1889,6 +1889,12 @@ static long __tun_chr_ioctl(struct file *file, unsigned int cmd,
 	unsigned int ifindex;
 	int ret;
 
+#ifdef CONFIG_ANDROID_PARANOID_NETWORK
+	if (cmd != TUNGETIFF && !capable(CAP_NET_ADMIN)) {
+		return -EPERM;
+	}
+#endif
+
 	if (cmd == TUNSETIFF || cmd == TUNSETQUEUE || _IOC_TYPE(cmd) == 0x89) {
 		if (copy_from_user(&ifr, argp, ifreq_len))
 			return -EFAULT;
