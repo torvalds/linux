31a1a1526d5b6f45aaed3e225a4751dce930736f
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-18 16:05:10 -0400

    drivers/hwmon: add module.h to ultra45_env.c
    This file needs the full module.h header and up to now was just
    implicitly capitalizing on it being present already.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/hwmon/ultra45_env.c
+++ drivers/hwmon/ultra45_env.c
@@ -6,6 +6,7 @@
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/slab.h>
+#include <linux/module.h>
 #include <linux/of_device.h>
 #include <linux/io.h>
 #include <linux/hwmon.h>
