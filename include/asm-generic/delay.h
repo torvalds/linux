a87e553fabe8ceadc6f90889066559234cf194c7
Andrew Morton <akpm@linux-foundation.org>
2011-07-18 15:28:20 +0200

    asm-generic: delay.h fix udelay and ndelay for 8 bit args
    With a non-constant 8-bit argument, a call to udelay() generates a warning:
    
    drivers/gpu/drm/radeon/atom.c: In function 'atom_op_delay':
    drivers/gpu/drm/radeon/atom.c:654: warning: comparison is always false due to limited range of data type
    
    The code looks like it works OK with an 8-bit arg, and the calling code is
    doing nothing wrong, so udelay() needs fixing.
    
    Fixing it was rather tricky.  Simply typecasting `n' in the comparison with
    20000 didn't change anything.  Hence the divide-by-20000 trick.
    
    Using a do{}while loop didn't work because udelay() is used in ?: statements,
    hence the ({...}) construct.
    
    While I was there I replaced the brain-bending ?:?:?: mess with nice if/else
    code.
    
    Probably other architectures are generating the same warning and can use a
    similar change.
    
    [Taken from the x86 tree and moved to asm-generic by Jonas Bonn]
    
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: <linux-arch@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Jonas Bonn <jonas@southpole.se>

--- include/asm-generic/delay.h
+++ include/asm-generic/delay.h
@@ -10,14 +10,35 @@ extern void __ndelay(unsigned long nsecs);
 extern void __const_udelay(unsigned long xloops);
 extern void __delay(unsigned long loops);
 
+/*
+ * The weird n/20000 thing suppresses a "comparison is always false due to
+ * limited range of data type" warning with non-const 8-bit arguments.
+ */
+
 /* 0x10c7 is 2**32 / 1000000 (rounded up) */
-#define udelay(n) (__builtin_constant_p(n) ? \
-	((n) > 20000 ? __bad_udelay() : __const_udelay((n) * 0x10c7ul)) : \
-	__udelay(n))
+#define udelay(n)							\
+	({								\
+		if (__builtin_constant_p(n)) {				\
+			if ((n) / 20000 >= 1)				\
+				 __bad_udelay();			\
+			else						\
+				__const_udelay((n) * 0x10c7ul);		\
+		} else {						\
+			__udelay(n);					\
+		}							\
+	})
 
 /* 0x5 is 2**32 / 1000000000 (rounded up) */
-#define ndelay(n) (__builtin_constant_p(n) ? \
-	((n) > 20000 ? __bad_ndelay() : __const_udelay((n) * 5ul)) : \
-	__ndelay(n))
+#define ndelay(n)							\
+	({								\
+		if (__builtin_constant_p(n)) {				\
+			if ((n) / 20000 >= 1)				\
+				__bad_ndelay();				\
+			else						\
+				__const_udelay((n) * 5ul);		\
+		} else {						\
+			__ndelay(n);					\
+		}							\
+	})
 
 #endif /* __ASM_GENERIC_DELAY_H */
