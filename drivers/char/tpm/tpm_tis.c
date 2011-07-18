6eb77b214985f8c2568f1f20f941790fbf8bf97b
Stefan Berger <stefanb@linux.vnet.ibm.com>
2011-07-18 09:11:55 -0400

    tpm: Fix compilation warning when CONFIG_PNP is not defined
    The is_itpm() function is only accessed from a block surrounded by
    #ifdef CONFIG_PNP. Therefore, also surround it with #ifdef CONFIG_PNP
    and remove the #else branch causing the warning.
    
    http://lxr.linux.no/#linux+v2.6.39/drivers/char/tpm/tpm_tis.c#L622
    
    v2:
     - fixes a previous typo
    
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>
    Reported-by: Randy Dunlap <rdunlap@xenotime.net>
    Signed-off-by: James Morris <jmorris@namei.org>

--- drivers/char/tpm/tpm_tis.c
+++ drivers/char/tpm/tpm_tis.c
@@ -80,7 +80,7 @@ enum tis_defaults {
 static LIST_HEAD(tis_chips);
 static DEFINE_SPINLOCK(tis_lock);
 
-#ifdef CONFIG_ACPI
+#ifdef CONFIG_PNP
 static int is_itpm(struct pnp_dev *dev)
 {
 	struct acpi_device *acpi = pnp_acpi_device(dev);
@@ -91,11 +91,6 @@ static int is_itpm(struct pnp_dev *dev)
 			return 1;
 	}
 
-	return 0;
-}
-#else
-static int is_itpm(struct pnp_dev *dev)
-{
 	return 0;
 }
 #endif
