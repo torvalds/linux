b9094677f7a1a4b6df741e59fb7f55eaa5f5836c
CMY <cmy@rock-chips.com>
2011-07-18 17:59:11 +0800

    新的modem驱动

--- drivers/misc/rk29_modem/modem_rockchip_demo.c
+++ drivers/misc/rk29_modem/modem_rockchip_demo.c
@@ -5,142 +5,70 @@
 #include <linux/device.h>
 #include <linux/errno.h>
 #include <linux/types.h>
-#include <linux/stat.h>	 /* permission constants */
+#include <linux/stat.h>
 #include <linux/io.h>
 #include <linux/vmalloc.h>
 #include <asm/io.h>
 #include <asm/sizes.h>
 #include <mach/iomux.h>
 #include <mach/gpio.h>
-//#include <asm/arch/iomux.h>
 #include <linux/delay.h>
+
+#include <linux/wakelock.h>
+#include <linux/workqueue.h>
+#include <linux/interrupt.h>
+#include <linux/gpio.h>
+#include <mach/board.h>
+
+#include <linux/platform_device.h>
 
 #include "rk29_modem.h"
 
-/****************************************************************
-	 huawei-em660/em660c/em770
-	 zte-ad3812/mf210/mu301
-	 thinkwill-me800								
-
-*****************************************************************/
-
-static int modem_enable(void){
-	printk("%s[%d]: %s\n", __FILE__, __LINE__, __FUNCTION__);
-	// PG1: 3G reset
-         /*arch/arm/mach-rockchip/iomux.c*/
-//	rockchip_mux_api_set(G3_RESET_IOMUX_NAME, G3_RESET_IOMUX_MODE);
-	// PB0: 3G poweron
-//	rockchip_mux_api_set(G3_POWER_ON_IOMUX_NAME, G3_POWER_ON_IOMUX_MODE);
-	// PG0: 3G Radio On/Off
-//	rockchip_mux_api_set(G3_RADIO_ON_OFF_IOMUX_NAME, G3_RADIO_ON_OFF_IOMUX_MODE);
-//	msleep(10);
-	
-	/*3G Modem Power On*/
-        int ret = gpio_request(G3_POWER_ON, NULL);
-        if(ret != 0)
-        {
-            gpio_free(G3_POWER_ON);
-            printk(">>>>>> G3_POWER_ON gpio_request err \n ");
-    	    return ret;
-        }
-        gpio_direction_output(G3_POWER_ON, G3_POWER_ENABLE);
-//        gpio_set_value(FB_LCD_CABC_EN_PIN, GPIO_LOW);
-
-//	GPIOSetPinDirection(G3_POWER_ON, GPIO_OUT);
-//	GPIOSetPinLevel(G3_POWER_ON, G3_POWER_ENABLE);
-	msleep(100);
-	gpio_free(G3_POWER_ON);
-	
-	/*3G Modem Radio On*/
-//	GPIOSetPinDirection(G3_RADIO_ON_OFF, GPIO_OUT);
-//	GPIOSetPinLevel(G3_RADIO_ON_OFF, G3_RADIO_ENABLE);
-//	msleep(100);
-
-	/*3G Modem Reset Controll if needed*/
-//	if(G3_RESET){
-//		GPIOSetPinDirection(G3_RESET, GPIO_OUT);
-//		GPIOSetPinLevel(G3_RESET, G3_RESET_ENABLE);
-//		msleep(120);
-//		GPIOSetPinLevel(G3_RESET, G3_RESET_DISABLE);
-//	}
-	return 0;
-}
-
-static int modem_disable(void){
-	printk("%s[%d]: %s\n", __FILE__, __LINE__, __FUNCTION__);
-	// PG1: 3G reset
-//	rockchip_mux_api_set(G3_RESET_IOMUX_NAME, G3_RESET_IOMUX_MODE);
-	// PB0: 3G poweron
-//	rockchip_mux_api_set(G3_POWER_ON_IOMUX_NAME, G3_POWER_ON_IOMUX_MODE);
-	// PG0: 3G On/Off
-//	rockchip_mux_api_set(G3_RADIO_ON_OFF_IOMUX_NAME, G3_RADIO_ON_OFF_IOMUX_MODE);
-//	msleep(10);
-
-#if 1
-        int ret = gpio_request(G3_POWER_ON, NULL);
-        if(ret != 0)
-        {
-            gpio_free(G3_POWER_ON);
-            printk(">>>>>> G3_POWER_ON gpio_request err \n ");
-	    return ret;
-        }
-        gpio_direction_output(G3_POWER_ON, G3_POWER_DISABLE);
-#else	
-	/*3G Modem Power off*/
-	GPIOSetPinDirection(G3_POWER_ON, GPIO_OUT);
-	GPIOSetPinLevel(G3_POWER_ON, G3_POWER_DISABLE);
-#endif
-	msleep(100);
-	gpio_free(G3_POWER_ON);
-
-	/*3G Modem  Radio off*/
-//	GPIOSetPinDirection(G3_RADIO_ON_OFF, GPIO_OUT);
-//	GPIOSetPinLevel(G3_RADIO_ON_OFF, G3_RADIO_DISABLE);
-//	msleep(10);
-
-	/*3G Modem Reset enable if needed*/
-//	if(G3_RESET){
-//		GPIOSetPinDirection(G3_RESET, GPIO_OUT);
-//		GPIOSetPinLevel(G3_RESET, G3_RESET_ENABLE);
-//	}
-	return 0;
-}
-
-static int modem_sleep(void){
-	printk("%s[%d]: %s\n", __FILE__, __LINE__, __FUNCTION__);
-	
-	return 0;
-}
-
-static int modem_init(void){
-    return modem_disable();
-}
-
-struct rk29_modem_t rk29_modem = {
-	.name    = "ThinkWill_ME800",
-	.enable  = modem_enable,
-	.disable = modem_disable,
-	.sleep   = modem_sleep,
-	.init    = modem_init,
+static struct rk29_io_t demo_io_power = {
+    .io_addr    = RK29_PIN6_PB1,
+    .enable     = GPIO_HIGH,
+    .disable    = GPIO_LOW,
 };
 
-static int __init rk29_modem_init(void)
+static struct platform_driver demo_platform_driver = {
+	.driver		= {
+		.name		= "rk29_demo",
+	},
+};
+
+static struct rk29_modem_t demo_driver = {
+    .driver         = &demo_platform_driver,
+    .modem_power    = &demo_io_power,
+    .ap_ready       = NULL,
+    .bp_wakeup_ap   = NULL,
+    .status         = MODEM_ENABLE,
+    .dev_init       = NULL,
+    .dev_uninit     = NULL,
+    .irq_handler    = NULL,
+    
+    .enable         = NULL,
+    .disable        = NULL,
+    .sleep          = NULL,
+    .wakeup         = NULL,
+};
+
+static int __init demo_init(void)
 {
-	return rk29_modem_register(&rk29_modem);
+    printk("%s[%d]: %s\n", __FILE__, __LINE__, __FUNCTION__);
+
+    return rk29_modem_init(&demo_driver);
 }
 
-static void __exit rk29_modem_exit(void)
+static void __exit demo_exit(void)
 {
-	rk29_modem_unregister(&rk29_modem);
+    printk("%s[%d]: %s\n", __FILE__, __LINE__, __FUNCTION__);
+    rk29_modem_exit();
 }
 
-
-late_initcall(rk29_modem_init);
-//module_init(rk29_modem_init);
-module_exit(rk29_modem_exit);
+module_init(demo_init);
+module_exit(demo_exit);
 
 MODULE_AUTHOR("lintao lintao@rock-chips.com");
 MODULE_DESCRIPTION("ROCKCHIP modem driver");
 MODULE_LICENSE("GPL");
-
 
--- drivers/misc/rk29_modem/rk29_modem.c
+++ drivers/misc/rk29_modem/rk29_modem.c
@@ -1,145 +1,331 @@
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/moduleparam.h>
-#include <linux/init.h>
-#include <linux/delay.h>
-#include <linux/device.h>
-#include <linux/errno.h>
-#include <linux/types.h>
-#include <linux/stat.h>	 /* permission constants */
-
+#include <linux/vmalloc.h>
 #include <asm/io.h>
 #include <asm/sizes.h>
+#include <mach/iomux.h>
+#include <mach/gpio.h>
+#include <linux/delay.h>
+
+#include <linux/wakelock.h>
+#include <linux/workqueue.h>
+#include <linux/interrupt.h>
+#include <linux/gpio.h>
+#include <mach/board.h>
 
 #include "rk29_modem.h"
 
-struct rk29_modem_t *g_rk29_modem = NULL;
+struct rk29_modem_t *g_rk29_modem;
+//static struct wake_lock wakelock_bbwakeupap;
+static struct class *rk29_modem_class = NULL;
+static void rk29_modem_turnon(struct rk29_io_t *modem_power, int onoff);
 
-extern void rk29_host11_driver_enable(void);
-extern void rk29_host11_driver_disable(void);
+int rk29_modem_change_status(struct rk29_modem_t *rk29_modem, int status)
+{
+    int ret = 0;
+    switch(status)
+    {
+    case MODEM_DISABLE:
+        rk29_modem_turnon(rk29_modem->modem_power, 0);
+        break;
+    case MODEM_ENABLE :
+        rk29_modem_turnon(rk29_modem->modem_power, 1);
+        break;
+    case MODEM_SLEEP:
+        ret = -1;
+    case MODEM_WAKEUP:
+        ret = -1;
+        break;
+    }
+    
+    return ret;
+}
 
 static ssize_t modem_status_write(struct class *cls, const char *_buf, size_t _count)
 {
-	struct rk29_modem_t *rk29_modem = g_rk29_modem;
+    struct rk29_modem_t *rk29_modem = g_rk29_modem;
+    int ret = 0;
+    int new_state = simple_strtoul(_buf, NULL, 16);
 
-    int new_mode = simple_strtoul(_buf, NULL, 16);
-    printk("[%s] new_mode: %s\n", __func__, _buf);
-    
     if(rk29_modem == NULL){
-    	printk("!!!! g_rk29_modem is NULL !!!!\n");
-    	return _count;
-    }
-    
-    if(new_mode == rk29_modem->cur_mode){
-    	printk("[%s] current already in %d mode\n", __func__, new_mode);
+        printk("!!!! g_rk29_modem is NULL !!!!\n");
         return _count;
     }
     
-    switch(new_mode){
+    printk("[%s] statue change: %d -> %d\n", __func__, rk29_modem->status, new_state);
+
+    if(new_state == rk29_modem->status) return _count;
+
+
+    switch(new_state)
+    {
     case MODEM_DISABLE:
-    	if(rk29_modem->disable){
-    		printk("modem disable!\n");
-//    		rk29_host11_driver_disable();
-  //  		mdelay(10);
-    		rk29_modem->disable();
-    		rk29_modem->cur_mode = new_mode;
-    	}
-    	break;
-    	
+        if(rk29_modem->disable)
+            ret = rk29_modem->disable(rk29_modem);
+        else
+            ret = rk29_modem_change_status(rk29_modem, new_state);
+        break;
     case MODEM_ENABLE :
-    	if(rk29_modem->enable){
-    		printk("modem enable!\n");
-//    		rk29_host11_driver_enable();
-//    		mdelay(100);
-    		rk29_modem->enable();
-    		rk29_modem->cur_mode = new_mode;
-    	}
-    	break;
-    	
+        if(rk29_modem->enable)
+            ret = rk29_modem->enable(rk29_modem);
+        else
+            ret = rk29_modem_change_status(rk29_modem, new_state);
+        break;
     case MODEM_SLEEP:
-    	if(rk29_modem->sleep){
-    		printk("modem sleep!\n");
-    		rk29_modem->sleep();
-    		rk29_modem->cur_mode = new_mode;
-    	}
-    	break;
-    	
+        if(rk29_modem->sleep)
+            ret = rk29_modem->sleep(rk29_modem);
+        else
+            ret = rk29_modem_change_status(rk29_modem, new_state);
+        break;
+    case MODEM_WAKEUP:
+        if(rk29_modem->wakeup)
+            ret = rk29_modem->wakeup(rk29_modem);
+        else
+            ret = rk29_modem_change_status(rk29_modem, new_state);
+        break;
     default:
-    	printk("[%s] invalid new mode: %d\n", __func__, new_mode);
-    	break;
+        ret = -1;
+        printk("[%s] Invalid new status: %d\n", __func__, new_state);
+        break;
     }
+
+    if( !ret ) rk29_modem->status = new_state;
     
-	return _count;
+    return _count;
 }
 
 static ssize_t modem_status_read(struct class *cls, char *_buf)
 {
-	struct rk29_modem_t *rk29_modem = g_rk29_modem;
-	
-//	printk("Modem type: %s, cur_mode = %d\n", rk29_modem->name, rk29_modem->cur_mode);
-	
-	return sprintf(_buf, "%d\n", rk29_modem->cur_mode);
-}
+    struct rk29_modem_t *rk29_modem = g_rk29_modem;
 
-static struct class *rk29_modem_class = NULL;
+    return sprintf(_buf, "%d\n", rk29_modem->status);
+}
 static CLASS_ATTR(modem_status, 0666, modem_status_read, modem_status_write);
 
-int modem_is_turn_on()
+int __devinit rk29_modem_suspend(struct platform_device *pdev, pm_message_t state)
 {
-    return (g_rk29_modem->cur_mode != 0);
-}
+#ifdef CONFIG_PM
+    printk("%s[%d]: %s\n", __FILE__, __LINE__, __FUNCTION__);
 
-void turn_off_modem()
-{
-    modem_status_write( NULL, "0", sizeof("0") );
-}
-
-void turn_on_modem()
-{
-    modem_status_write( NULL, "1", sizeof("1") );
-}
-
-int rk29_modem_register(struct rk29_modem_t *rk29_modem){
-	int ret = -1;
-	
-	if(rk29_modem == NULL)
-		return -1;
-
-#if 1
-	if(rk29_modem->enable) rk29_modem->enable();
-#else
-	if(rk29_modem->disable) rk29_modem->disable();
-#endif	
-	rk29_modem_class = class_create(THIS_MODULE, "rk291x_modem");
-	if(rk29_modem_class == NULL){
-		printk("create class rk291x_modem failed!\n");
-		goto err1;
-	}
-	
-	ret = class_create_file(rk29_modem_class, &class_attr_modem_status);
-	if(ret != 0){
-		printk("create rk291x_modem class file failed!\n");
-		goto err2;
-	}
-	
-	g_rk29_modem = rk29_modem;
-	
+    if( g_rk29_modem->suspend )
+        g_rk29_modem->suspend(pdev, state);
+    else
+    {
+        if(g_rk29_modem->ap_ready) // 标志AP已挂起
+            gpio_direction_output(g_rk29_modem->ap_ready->io_addr, g_rk29_modem->ap_ready->disable);
+    }
+#endif
 	return 0;
-	
-err2:
-	class_destroy(rk29_modem_class);
-err1:
-	return ret;	
 }
 
-void rk29_modem_unregister(struct rk29_modem_t *rk29_modem){
-	/* disable 3G modem */		
-	if(rk29_modem->disable)
-		rk29_modem->disable();
-		
-	class_remove_file(rk29_modem_class, &class_attr_modem_status);
-	class_destroy(rk29_modem_class);
-	
-	rk29_modem_class = NULL;
+int __devinit rk29_modem_resume(struct platform_device *pdev)
+{
+#ifdef CONFIG_PM
+    printk("%s[%d]: %s\n", __FILE__, __LINE__, __FUNCTION__);
+    if( g_rk29_modem->resume )
+        g_rk29_modem->resume(pdev);
+    else
+    {
+        if(g_rk29_modem->ap_ready) // 标志AP已恢复
+            gpio_direction_output(g_rk29_modem->ap_ready->io_addr, g_rk29_modem->ap_ready->enable);
+    }
+#endif
+	return 0;
+}
+
+static irqreturn_t irq_bbwakeupap_handler(int irq, void *dev_id)
+{
+    printk("%s[%d]: %s\n", __FILE__, __LINE__, __FUNCTION__);
+
+    irqreturn_t irqret = IRQ_NONE;
+    if( g_rk29_modem->irq_handler )
+    {
+        irqret = g_rk29_modem->irq_handler(irq, dev_id);
+        if( irqret != IRQ_NONE ) return irqret;
+    }
+
+    // 具体设备没有 irq_handler 函数，或者它的irq_handler函数没有处理该 irq，则使用
+    // 以下的公共irq处理.
+
+    return IRQ_HANDLED;
+}
+
+static void uninstall_irq(struct rk29_irq_t *irq)
+{
+    gpio_free(irq->irq_addr);
+}
+
+static int install_irq(struct rk29_irq_t *rk29_irq, const char* label)
+{
+	int ret;
+    int irq;
+    
+    printk("%s[%d]: %s\n", __FILE__, __LINE__, __FUNCTION__);
+
+	irq = gpio_to_irq(rk29_irq->irq_addr);
+    printk("%s: %d ==> %d\n", __func__, rk29_irq->irq_addr, irq);
+
+	ret = gpio_request(rk29_irq->irq_addr, label);
+	if (ret < 0) {
+		pr_err("%s: gpio_request(%d) failed\n", __func__, rk29_irq->irq_addr);
+		return ret;
+	}
+
+	gpio_direction_input(rk29_irq->irq_addr);
+    gpio_pull_updown(rk29_irq->irq_addr, 0);
+	ret = request_irq(irq, irq_bbwakeupap_handler, rk29_irq->irq_trigger, label, NULL);
+	if (ret < 0) {
+		pr_err("%s: request_irq(%d) failed\n", __func__, irq);
+		gpio_free(rk29_irq->irq_addr);
+		return ret;
+	}
+
+	enable_irq_wake(irq);
+	return 0;
+}
+
+// 给modem上电
+static void rk29_modem_turnon(struct rk29_io_t *modem_power, int onoff)
+{
+    if( modem_power )
+        gpio_direction_output(modem_power->io_addr, onoff?modem_power->enable:modem_power->disable);
+}
+
+/*
+    RK29 modem 通用的设备初始化函数
+    主要设置各个GPIO以及IRQ的申请等
+ */
+static int rk29_modem_dev_init(struct rk29_modem_t *rk29_modem)
+{
+    int ret=0;
+    
+    printk("%s[%d]: %s\n", __FILE__, __LINE__, __FUNCTION__);
+
+// 申请控制modem电源的GPIO
+    if( rk29_modem->modem_power )
+    {
+        ret = gpio_request(rk29_modem->modem_power->io_addr, "modem_power");
+        if(ret != 0)
+        {
+            gpio_free(rk29_modem->modem_power->io_addr);
+            printk(">>>>>> Modem power io request failed!\n");
+            return ret;
+        }
+        
+        // 设置初始的modem状态
+        rk29_modem_change_status(rk29_modem, rk29_modem->status);
+    }
+
+// 申请控制AP就绪状态的GPIO
+    if( rk29_modem->ap_ready )
+    {
+        ret = gpio_request(rk29_modem->ap_ready->io_addr, "ap_ready");
+        if(ret != 0)
+        {
+            gpio_free(rk29_modem->ap_ready->io_addr);
+            printk(">>>>>> AP ready io request failed!\n");
+            return ret;
+        }
+
+        // 默认 AP is ready
+        gpio_direction_output(rk29_modem->ap_ready->io_addr, rk29_modem->ap_ready->enable);
+    }
+
+// 设置 bb_wakeup_ap 的irq，用于ap挂起之后，由bb来唤醒ap
+    if( rk29_modem->bp_wakeup_ap )
+    {
+        ret = install_irq(rk29_modem->bp_wakeup_ap, "bb_wakeup_ap");
+    }
+    
+    wake_lock_init(&rk29_modem->wakelock_bbwakeupap, WAKE_LOCK_SUSPEND, "bb_wakeup_ap");
+
+    return ret;
+}
+
+/*
+    modem下电、释放gpio wait_lock 等
+ */
+static void rk29_modem_dev_uninit(struct rk29_modem_t *rk29_modem)
+{
+    printk("%s[%d]: %s\n", __FILE__, __LINE__, __FUNCTION__);
+    
+// 给modem下电
+    if( rk29_modem->modem_power )
+        rk29_modem_turnon(rk29_modem->modem_power, 0);
+
+// 释放GPIO
+    if( rk29_modem->modem_power )
+        gpio_free(rk29_modem->modem_power->io_addr);
+
+    if(rk29_modem->ap_ready)
+        gpio_free(rk29_modem->ap_ready->io_addr);
+
+    if(rk29_modem->bp_wakeup_ap)
+        uninstall_irq(rk29_modem->bp_wakeup_ap);
+
+// 释放 wake lock
+    wake_lock_destroy(&rk29_modem->wakelock_bbwakeupap);
+}
+
+/*
+    驱动加载
+ */
+int rk29_modem_init(struct rk29_modem_t *rk29_modem)
+{
+    int retval = 0;
+    printk("%s[%d]: %s\n", __FILE__, __LINE__, __FUNCTION__);
+
+    retval = platform_driver_register(rk29_modem->driver);
+    if( retval )
+    {
+        printk("could not register rk29_modem!\n");
+        return retval;
+    }
+
+	rk29_modem_class = class_create(THIS_MODULE, "rk291x_modem");
+    if(rk29_modem_class == NULL){
+        printk("create class rk291x_modem failed!\n");
+        return -1;
+    }
+    retval = class_create_file(rk29_modem_class, &class_attr_modem_status);
+    if(retval != 0){
+        printk("create rk291x_modem class file failed!\n");
+        goto failed_create_class;
+    }
+    
+// 对modem初始化
+    if( rk29_modem->dev_init )
+        retval = rk29_modem->dev_init(rk29_modem);
+    else
+        retval = rk29_modem_dev_init(rk29_modem);
+        
+    if( retval )
+        goto failed_device_init;
+
+    g_rk29_modem = rk29_modem;
+
+    return 0;
+
+failed_device_init:
+    platform_driver_unregister(rk29_modem->driver);
+
+failed_create_class:
+    class_destroy(rk29_modem_class);
+    
+    return retval;
+}
+
+/*
+    退出驱动时调用
+    目前driver是build-in，不存在退出的情况，不会调用到这边
+ */
+void rk29_modem_exit(void)
+{
+    printk("%s[%d]: %s\n", __FILE__, __LINE__, __FUNCTION__);
+    platform_driver_unregister(g_rk29_modem->driver);
+
+// 对modem反初始化
+    if( g_rk29_modem->dev_uninit )
+        g_rk29_modem->dev_uninit(g_rk29_modem);
+    else
+        rk29_modem_dev_uninit(g_rk29_modem);
 }
 
