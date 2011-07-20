78a7539b881eb557494a7c810625c0307b27296c
Thomas Courbon <thcourbon@gmail.com>
2011-07-20 22:57:44 +0200

    Platform: fix samsung-laptop DMI identification for N150/N210/220/N230
    Some samsung latop of the N150/N2{10,20,30} serie are badly detected by the samsung-laptop platform driver, see bug # 36082.
    It appears that N230 identifies itself as N150/N210/N220/N230 whereas the other identify themselves as N150/N210/220.
    This patch attemtp fix #36082 allowing correct identification for all the said netbook model.
    
    Reported-by: Daniel Eklöf <daniel@ekloef.se>
    Signed-off-by: Thomas Courbon <thcourbon@gmail.com>
    Signed-off-by: Matthew Garrett <mjg@redhat.com>

--- drivers/platform/x86/samsung-laptop.c
+++ drivers/platform/x86/samsung-laptop.c
@@ -611,6 +611,16 @@ static struct dmi_system_id __initdata samsung_dmi_table[] = {
 		.callback = dmi_check_cb,
 	},
 	{
+		.ident = "N150/N210/N220",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR,
+					"SAMSUNG ELECTRONICS CO., LTD."),
+			DMI_MATCH(DMI_PRODUCT_NAME, "N150/N210/N220"),
+			DMI_MATCH(DMI_BOARD_NAME, "N150/N210/N220"),
+		},
+		.callback = dmi_check_cb,
+	},
+	{
 		.ident = "N150/N210/N220/N230",
 		.matches = {
 			DMI_MATCH(DMI_SYS_VENDOR,
