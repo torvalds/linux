917e3e65c35459d52f0d0b890aa5df0cad07a051
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2011-07-22 12:18:43 -0400

    xen-pcifront: Update warning comment to use 'e820_host' option.
    With Xen changeset 23428 "libxl: Add 'e820_host' option to config file"
    the E820 as seen from the host can now be passed into the guest.
    This means that a PV guest can now:
     - Use the correct PCI I/O gap. Before these patches, Linux guest would
       boot up and would tell:
       [    0.000000] Allocating PCI resources starting at 40000000 (gap: 40000000:c0000000)
       while in actuality the PCI I/O gap should have been:
       [    0.000000] Allocating PCI resources starting at b0000000 (gap: b0000000:4c000000)
    
     - The PV domain with PCI devices was limited to 3GB. It now can be booted
       with 4GB, 8GB, or whatever number you want. The PCI devices will now _not_ conflict
       with System RAM. Meaning the drivers can load.
    
    CC: Jesse Barnes <jbarnes@virtuousgeek.org>
    CC: linux-pci@vger.kernel.org
    CC: stable@kernel.org
    [v2: Made the string less broken up. Suggested by Joe Perches]
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/pci/xen-pcifront.c
+++ drivers/pci/xen-pcifront.c
@@ -400,9 +400,8 @@ static int pcifront_claim_resource(struct pci_dev *dev, void *data)
 			dev_info(&pdev->xdev->dev, "claiming resource %s/%d\n",
 				pci_name(dev), i);
 			if (pci_claim_resource(dev, i)) {
-				dev_err(&pdev->xdev->dev, "Could not claim "
-					"resource %s/%d! Device offline. Try "
-					"giving less than 4GB to domain.\n",
+				dev_err(&pdev->xdev->dev, "Could not claim resource %s/%d! "
+					"Device offline. Try using e820_host=1 in the guest config.\n",
 					pci_name(dev), i);
 			}
 		}
