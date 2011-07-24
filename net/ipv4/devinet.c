b76d0789c92a816a5539dc14232a700b8d62a53a
Zoltan Kiss <schaman@sch.bme.hu>
2011-07-24 13:09:30 +0000

    IPv4: Send gratuitous ARP for secondary IP addresses also
    If a device event generates gratuitous ARP messages, only primary
    address is used for sending. This patch iterates through the whole
    list. Tested with 2 IP addresses configuration on bonding interface.
    
    Signed-off-by: Zoltan Kiss <schaman@sch.bme.hu>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/devinet.c
+++ net/ipv4/devinet.c
@@ -1134,15 +1134,15 @@ static void inetdev_send_gratuitous_arp(struct net_device *dev,
 					struct in_device *in_dev)
 
 {
-	struct in_ifaddr *ifa = in_dev->ifa_list;
+	struct in_ifaddr *ifa;
 
-	if (!ifa)
-		return;
-
-	arp_send(ARPOP_REQUEST, ETH_P_ARP,
-		 ifa->ifa_local, dev,
-		 ifa->ifa_local, NULL,
-		 dev->dev_addr, NULL);
+	for (ifa = in_dev->ifa_list; ifa;
+	     ifa = ifa->ifa_next) {
+		arp_send(ARPOP_REQUEST, ETH_P_ARP,
+			 ifa->ifa_local, dev,
+			 ifa->ifa_local, NULL,
+			 dev->dev_addr, NULL);
+	}
 }
 
 /* Called only under RTNL semaphore */
