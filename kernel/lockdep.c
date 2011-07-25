70a0686a72c7a7e554b404ca11406ceec709d425
Peter Zijlstra <a.p.zijlstra@chello.nl>
2011-07-25 12:09:59 +0200

    lockdep: Fix up warning
    On Sun, 2011-07-24 at 21:06 -0400, Arnaud Lacombe wrote:
    
    > /src/linux/linux/kernel/lockdep.c: In function 'mark_held_locks':
    > /src/linux/linux/kernel/lockdep.c:2471:31: warning: comparison of
    > distinct pointer types lacks a cast
    
    The warning is harmless in this case, but the below makes it go away.
    
    Reported-by: Arnaud Lacombe <lacombar@gmail.com>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1311588599.2617.56.camel@laptop
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/lockdep.c
+++ kernel/lockdep.c
@@ -2468,7 +2468,7 @@ mark_held_locks(struct task_struct *curr, enum mark_type mark)
 
 		BUG_ON(usage_bit >= LOCK_USAGE_STATES);
 
-		if (hlock_class(hlock)->key == &__lockdep_no_validate__)
+		if (hlock_class(hlock)->key == __lockdep_no_validate__.subkeys)
 			continue;
 
 		if (!mark_lock(curr, hlock, usage_bit))
