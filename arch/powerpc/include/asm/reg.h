501d238633a3f9869f4e777b3b281ca7660b7156
Peter Zijlstra <peterz@infradead.org>
2011-07-27 07:27:21 +0000

    ppc: Remove duplicate definition of PV_POWER7
    One definition of PV_POWER7 seems enough to me.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/reg.h
+++ arch/powerpc/include/asm/reg.h
@@ -1003,7 +1003,6 @@
 #define PV_970		0x0039
 #define PV_POWER5	0x003A
 #define PV_POWER5p	0x003B
-#define PV_POWER7	0x003F
 #define PV_970FX	0x003C
 #define PV_POWER6	0x003E
 #define PV_POWER7	0x003F
