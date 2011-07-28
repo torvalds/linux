b12362bdb61a230a67daa77bcd2a11e59b2802e1
Eric Dumazet <eric.dumazet@gmail.com>
2011-07-28 06:11:47 +0200

    vfs: conditionally call inode_wb_list_del()
    Some inodes (pipes, sockets, ...) are not in bdi writeback list.
    
    evict() can avoid calling inode_wb_list_del() and its expensive spinlock
    by checking inode i_wb_list being empty or not.
    
    At this point, no other cpu/user can concurrently manipulate this inode
    i_wb_list
    
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/inode.c
+++ fs/inode.c
@@ -454,7 +454,9 @@ static void evict(struct inode *inode)
 	BUG_ON(!(inode->i_state & I_FREEING));
 	BUG_ON(!list_empty(&inode->i_lru));
 
-	inode_wb_list_del(inode);
+	if (!list_empty(&inode->i_wb_list))
+		inode_wb_list_del(inode);
+
 	inode_sb_list_del(inode);
 
 	if (op->evict_inode) {
