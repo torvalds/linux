2102a65e69eac8d77dd71b4991b395e825087ba8
Dave Martin <dave.martin@linaro.org>
2011-07-28 14:29:40 +0100

    ARM: 7008/1: alignment: Make SIGBUS sent to userspace POSIXly correct
    With the UM_SIGNAL alignment fault mode, no siginfo structure is
    passed to userspace.
    
    POSIX specifies how siginfo_t should be populated for alignment
    faults, so this patch does just that:
    
      * si_signo = SIGBUS
      * si_code = BUS_ADRALN
      * si_addr = misaligned data address at which access was attempted
    
    Signed-off-by: Dave Martin <dave.martin@linaro.org>
    Acked-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    Acked-by: Kirill A. Shutemov <kirill@shutemov.name>
    Reviewed-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mm/alignment.c
+++ arch/arm/mm/alignment.c
@@ -22,6 +22,7 @@
 #include <linux/sched.h>
 #include <linux/uaccess.h>
 
+#include <asm/system.h>
 #include <asm/unaligned.h>
 
 #include "fault.h"
@@ -913,9 +914,16 @@ do_alignment(unsigned long addr, unsigned int fsr, struct pt_regs *regs)
 	if (ai_usermode & UM_FIXUP)
 		goto fixup;
 
-	if (ai_usermode & UM_SIGNAL)
-		force_sig(SIGBUS, current);
-	else {
+	if (ai_usermode & UM_SIGNAL) {
+		siginfo_t si;
+
+		si.si_signo = SIGBUS;
+		si.si_errno = 0;
+		si.si_code = BUS_ADRALN;
+		si.si_addr = (void __user *)addr;
+
+		force_sig_info(si.si_signo, &si, current);
+	} else {
 		/*
 		 * We're about to disable the alignment trap and return to
 		 * user space.  But if an interrupt occurs before actually
