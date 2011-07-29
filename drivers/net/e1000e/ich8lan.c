c6e7f51e73c1bc6044bce989ec503ef2e4758d55
Bruce Allan <bruce.w.allan@intel.com>
2011-07-29 05:53:02 +0000

    e1000e: workaround invalid Tx/Rx tail descriptor register write
    When the Manageability Engine (ME) is enabled on 82579, it periodically
    accesses some MAC CSR registers.  There is an arbiter in hardware which
    prevents simultaneous access of these registers by the host software, i.e.
    the driver.  There is a hardware bug in the aribter that signals a host
    access of the registers later than it actually happens.  A write of the
    Transmit or Receive Descriptor Tail register could result in an incorrect
    value if the driver and ME perform simultaneous accesses which could result
    in an access to an invalid memory address.  This would return an
    Unsupported Request which could hang the hardware.  Workaround the issue by
    checking the FWSM register bit24 which is set by ME before it accesses the
    MAC CSR registers.
    
    Signed-off-by: Bruce Allan <bruce.w.allan@intel.com>
    Tested-by: Jeff Pieper <jeffrey.e.pieper@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/e1000e/ich8lan.c
+++ drivers/net/e1000e/ich8lan.c
@@ -814,6 +814,11 @@ static s32 e1000_get_variants_ich8lan(struct e1000_adapter *adapter)
 	    (adapter->hw.phy.type == e1000_phy_igp_3))
 		adapter->flags |= FLAG_LSC_GIG_SPEED_DROP;
 
+	/* Enable workaround for 82579 w/ ME enabled */
+	if ((adapter->hw.mac.type == e1000_pch2lan) &&
+	    (er32(FWSM) & E1000_ICH_FWSM_FW_VALID))
+		adapter->flags2 |= FLAG2_PCIM2PCI_ARBITER_WA;
+
 	/* Disable EEE by default until IEEE802.3az spec is finalized */
 	if (adapter->flags2 & FLAG2_HAS_EEE)
 		adapter->hw.dev_spec.ich8lan.eee_disable = true;
