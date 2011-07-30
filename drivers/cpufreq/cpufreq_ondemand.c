a21445e557042def4c868b296d64a11e91d19c6a
黄涛 <huangtao@rock-chips.com>
2011-07-30 22:27:06 +0800

    Revert "cpufreq: ondemand: Don't synchronize sample rate unless mulitple cpus present"
    This reverts commit beb070f3d41fb82c69c6dcec1293817c7c788d10.

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -585,9 +585,7 @@ static void do_dbs_timer(struct work_struct *work)
 	/* We want all CPUs to do sampling nearly on same jiffy */
 	int delay = usecs_to_jiffies(dbs_tuners_ins.sampling_rate);
 
-	if (num_online_cpus() > 1)
-		delay -= jiffies % delay;
-
+	delay -= jiffies % delay;
 	mutex_lock(&dbs_info->timer_mutex);
 
 	/* Common NORMAL_SAMPLE setup */
