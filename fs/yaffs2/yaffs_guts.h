08fb5eb5c65f369560355db544eaa17c376096bc
黄涛 <huangtao@rock-chips.com>
2011-07-30 22:45:05 +0800

    Revert "yaffs: Fix readdir hang."
    This reverts commit ef330d560d9c3bb66ab1031af243b3171bba219f.

--- fs/yaffs2/yaffs_guts.h
+++ fs/yaffs2/yaffs_guts.h
@@ -594,9 +594,8 @@ struct yaffs_DeviceStruct {
 	int isYaffs2;
 
 	/* The removeObjectCallback function must be supplied by OS flavours that
-	 * need it.
-         * yaffs direct uses it to implement the faster readdir.
-         * Linux uses it to protect the directory during unlocking.
+	 * need it. The Linux kernel does not use this, but yaffs direct does use
+	 * it to implement the faster readdir
 	 */
 	void (*removeObjectCallback)(struct yaffs_ObjectStruct *obj);
 
@@ -636,14 +635,10 @@ struct yaffs_DeviceStruct {
 
 	struct semaphore sem;	/* Semaphore for waiting on erasure.*/
 	struct semaphore grossLock;	/* Gross locking semaphore */
-	struct rw_semaphore dirLock; /* Lock the directory structure */
 	__u8 *spareBuffer;	/* For mtdif2 use. Don't know the size of the buffer
 				 * at compile time so we have to allocate it.
-
 				 */
 	void (*putSuperFunc) (struct super_block *sb);
-        struct ylist_head searchContexts;
-
 #endif
 
 	int isMounted;
