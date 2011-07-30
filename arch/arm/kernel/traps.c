f911f45786b0ad2b3ce9780c819eb2a2599d5a33
黄涛 <huangtao@rock-chips.com>
2011-07-30 22:16:37 +0800

    Revert "[ARM] Do not call flush_cache_user_range with mmap_sem held"
    This reverts commit 7bc14acfbb86b00a93ddb1c40d9d2050cd7188fe.

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -438,9 +438,7 @@ do_cache_op(unsigned long start, unsigned long end, int flags)
 		if (end > vma->vm_end)
 			end = vma->vm_end;
 
-		up_read(&mm->mmap_sem);
-		flush_cache_user_range(start, end);
-		return;
+		flush_cache_user_range(vma, start, end);
 	}
 	up_read(&mm->mmap_sem);
 }
