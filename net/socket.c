ea32bd9ebc4a04178c8ba357ad2178de244b2a96
黄涛 <huangtao@rock-chips.com>
2011-07-30 22:57:02 +0800

    Revert "uidstat: Adding uid stat driver to collect network statistics."
    This reverts commit ab6c8fc0decb0b39f8cff97d4889fef5c3a50aa7.
    
    Conflicts:
    
    	drivers/misc/Kconfig

--- net/socket.c
+++ net/socket.c
@@ -97,10 +97,6 @@
 #include <net/sock.h>
 #include <linux/netfilter.h>
 
-#ifdef CONFIG_UID_STAT
-#include <linux/uid_stat.h>
-#endif
-
 static int sock_no_open(struct inode *irrelevant, struct file *dontcare);
 static ssize_t sock_aio_read(struct kiocb *iocb, const struct iovec *iov,
 			 unsigned long nr_segs, loff_t pos);
@@ -574,12 +570,7 @@ static inline int __sock_sendmsg(struct kiocb *iocb, struct socket *sock,
 	if (err)
 		return err;
 
-	err = sock->ops->sendmsg(iocb, sock, msg, size);
-#ifdef CONFIG_UID_STAT
-	if (err > 0)
-		update_tcp_snd(current_uid(), err);
-#endif
-	return err;
+	return sock->ops->sendmsg(iocb, sock, msg, size);
 }
 
 int sock_sendmsg(struct socket *sock, struct msghdr *msg, size_t size)
@@ -693,12 +684,7 @@ static inline int __sock_recvmsg(struct kiocb *iocb, struct socket *sock,
 	if (err)
 		return err;
 
-	err = sock->ops->recvmsg(iocb, sock, msg, size, flags);
-#ifdef CONFIG_UID_STAT
-	if (err > 0)
-		update_tcp_rcv(current_uid(), err);
-#endif
-	return err;
+	return sock->ops->recvmsg(iocb, sock, msg, size, flags);
 }
 
 int sock_recvmsg(struct socket *sock, struct msghdr *msg,
