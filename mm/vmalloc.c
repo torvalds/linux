993b15ca042f746abaa211c6ec726860647728bd
黄涛 <huangtao@rock-chips.com>
2011-07-30 22:26:39 +0800

    Revert "vmalloc: remove BUG_ON due to racy counting of VM_LAZY_FREE"
    This reverts commit 21ba0cac4b63286aaf41befb3b93c8ffb5036306.

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -555,8 +555,10 @@ static void __purge_vmap_area_lazy(unsigned long *start, unsigned long *end,
 	}
 	rcu_read_unlock();
 
-	if (nr)
+	if (nr) {
+		BUG_ON(nr > atomic_read(&vmap_lazy_nr));
 		atomic_sub(nr, &vmap_lazy_nr);
+	}
 
 	if (nr || force_flush)
 		flush_tlb_kernel_range(*start, *end);
