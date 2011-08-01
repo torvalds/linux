33154ffcfa51bdc99511cb637e9978802de1dd1f
黄涛 <huangtao@rock-chips.com>
2011-08-01 11:26:57 +0800

    dwc_otg_hcd.h: fix for v2.6.36

--- drivers/usb/dwc_otg/dwc_otg_hcd.h
+++ drivers/usb/dwc_otg/dwc_otg_hcd.h
@@ -36,7 +36,12 @@
 
 #include <linux/list.h>
 #include <linux/usb.h>
+#include <linux/version.h>
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 35))
+#include <linux/usb/hcd.h>
+#else
 #include <../drivers/usb/core/hcd.h>
+#endif
 
 struct dwc_otg_device;
 
