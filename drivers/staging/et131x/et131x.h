5cf51e8aadfc81b9cec827565a6eaa4a0ad8dd5b
Mark Einon <mark.einon@gmail.com>
2011-08-02 20:17:50 +0100

    staging: et131x: Rename et131x_config_global_regs > et131x_configure_global_regs
    Typo in header file. Also renaming a few adapter references to fit in with the rest of the file.
    
    Signed-off-by: Mark Einon <mark.einon@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/et131x/et131x.h
+++ drivers/staging/et131x/et131x.h
@@ -49,10 +49,10 @@
  */
 
 /* et131x_eeprom.c */
-int et131x_init_eeprom(struct et131x_adapter *etdev);
+int et131x_init_eeprom(struct et131x_adapter *adapter);
 
 /* et131x_initpci.c */
-void et131x_config_global_regs(struct et131x_adapter *pAdapter);
+void et131x_configure_global_regs(struct et131x_adapter *adapter);
 void et131x_enable_interrupts(struct et131x_adapter *adapter);
 void et131x_disable_interrupts(struct et131x_adapter *adapter);
 void et131x_align_allocated_memory(struct et131x_adapter *adapter,
