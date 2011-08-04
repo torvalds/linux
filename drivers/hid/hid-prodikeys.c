00b15628b0aa73199f74bd6c988b8d2b55f51190
Bojan Prtvar <prtvar.b@gmail.com>
2011-08-04 23:46:48 +0200

    HID: prodikeys: kfree() NULL pointer cleanup
    No need for freeing pm in case when it's not allocated.
    
    Reported-by: Julia Lawall <julia@diku.dk>
    Signed-off-by: Bojan Prtvar <prtvar.b@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-prodikeys.c
+++ drivers/hid/hid-prodikeys.c
@@ -816,7 +816,7 @@ static int pk_probe(struct hid_device *hdev, const struct hid_device_id *id)
 	if (pm == NULL) {
 		hid_err(hdev, "can't alloc descriptor\n");
 		ret = -ENOMEM;
-		goto err_free;
+		goto err_free_pk;
 	}
 
 	pm->pk = pk;
@@ -849,10 +849,10 @@ static int pk_probe(struct hid_device *hdev, const struct hid_device_id *id)
 err_stop:
 	hid_hw_stop(hdev);
 err_free:
-	if (pm != NULL)
-		kfree(pm);
-
+	kfree(pm);
+err_free_pk:
 	kfree(pk);
+
 	return ret;
 }
 
