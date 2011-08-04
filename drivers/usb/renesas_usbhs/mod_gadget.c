d128a259ea4ef7cab39f14b681ee466a7dc6153c
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2011-08-03 21:41:26 -0700

    usb: renesas_usbhs: fix DMA build by including dma-mapping.h
    Include dma-mapping.h to fix build of the renesas_usbhs driver
    
    CC      drivers/usb/renesas_usbhs/mod_gadget.o
    drivers/usb/renesas_usbhs/mod_gadget.c: In function 'usbhsg_dma_map':
    drivers/usb/renesas_usbhs/mod_gadget.c:190: error: implicit declaration of function 'dma_map_single'
    drivers/usb/renesas_usbhs/mod_gadget.c:192: error: implicit declaration of function 'dma_sync_single_for_device'
    drivers/usb/renesas_usbhs/mod_gadget.c:196: error: implicit declaration of function 'dma_mapping_error'
    drivers/usb/renesas_usbhs/mod_gadget.c: In function 'usbhsg_dma_unmap':
    drivers/usb/renesas_usbhs/mod_gadget.c:217: error: implicit declaration of function 'dma_unmap_single'
    drivers/usb/renesas_usbhs/mod_gadget.c:219: error: implicit declaration of function 'dma_sync_single_for_cpu'
    make[5]: *** [drivers/usb/renesas_usbhs/mod_gadget.o] Error 1
    make[4]: *** [drivers/usb/renesas_usbhs] Error 2
    
    Reported-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/renesas_usbhs/mod_gadget.c
+++ drivers/usb/renesas_usbhs/mod_gadget.c
@@ -14,6 +14,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
+#include <linux/dma-mapping.h>
 #include <linux/io.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
