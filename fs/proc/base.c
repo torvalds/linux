c21427043dec93d40e3a1af970831d1f5f15ce5d
Linus Torvalds <torvalds@linux-foundation.org>
2011-08-06 11:43:08 -0700

    oom_ajd: don't use WARN_ONCE, just use printk_once
    WARN_ONCE() is very annoying, in that it shows the stack trace that we
    don't care about at all, and also triggers various user-level "kernel
    oopsed" logic that we really don't care about.  And it's not like the
    user can do anything about the applications (sshd) in question, it's a
    distro issue.
    
    Requested-by: Andi Kleen <andi@firstfloor.org> (and many others)
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -1118,7 +1118,7 @@ static ssize_t oom_adjust_write(struct file *file, const char __user *buf,
 	 * Warn that /proc/pid/oom_adj is deprecated, see
 	 * Documentation/feature-removal-schedule.txt.
 	 */
-	WARN_ONCE(1, "%s (%d): /proc/%d/oom_adj is deprecated, please use /proc/%d/oom_score_adj instead.\n",
+	printk_once(KERN_WARNING "%s (%d): /proc/%d/oom_adj is deprecated, please use /proc/%d/oom_score_adj instead.\n",
 		  current->comm, task_pid_nr(current), task_pid_nr(task),
 		  task_pid_nr(task));
 	task->signal->oom_adj = oom_adjust;
