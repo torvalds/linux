fa1bf42ff9296ac4cf211b0a1b450a6071d26a95
Jeff Moyer <jmoyer@redhat.com>
2011-08-09 20:32:09 +0200

    allow blk_flush_policy to return REQ_FSEQ_DATA independent of *FLUSH
    blk_insert_flush has the following check:
    
    	/*
    	 * If there's data but flush is not necessary, the request can be
    	 * processed directly without going through flush machinery.  Queue
    	 * for normal execution.
    	 */
    	if ((policy & REQ_FSEQ_DATA) &&
    	    !(policy & (REQ_FSEQ_PREFLUSH | REQ_FSEQ_POSTFLUSH))) {
    		list_add_tail(&rq->queuelist, &q->queue_head);
    		return;
    	}
    
    However, blk_flush_policy will not return with policy set to only
    REQ_FSEQ_DATA:
    
    static unsigned int blk_flush_policy(unsigned int fflags, struct request *rq)
    {
    	unsigned int policy = 0;
    
    	if (fflags & REQ_FLUSH) {
    		if (rq->cmd_flags & REQ_FLUSH)
    			policy |= REQ_FSEQ_PREFLUSH;
    		if (blk_rq_sectors(rq))
    			policy |= REQ_FSEQ_DATA;
    		if (!(fflags & REQ_FUA) && (rq->cmd_flags & REQ_FUA))
    			policy |= REQ_FSEQ_POSTFLUSH;
    	}
    	return policy;
    }
    
    Notice that REQ_FSEQ_DATA is only set if REQ_FLUSH is set.  Fix this
    mismatch by moving the setting of REQ_FSEQ_DATA outside of the REQ_FLUSH
    check.
    
    Tejun notes:
    
      Hmmm... yes, this can become a correctness issue if (and only if)
      blk_queue_flush() is called to change q->flush_flags while requests
      are in-flight; otherwise, requests wouldn't reach the function at all.
      Also, I think it would be a generally good idea to always set
      FSEQ_DATA if the request has data.
    
    Cheers,
    Jeff
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

--- block/blk-flush.c
+++ block/blk-flush.c
@@ -95,11 +95,12 @@ static unsigned int blk_flush_policy(unsigned int fflags, struct request *rq)
 {
 	unsigned int policy = 0;
 
+	if (blk_rq_sectors(rq))
+		policy |= REQ_FSEQ_DATA;
+
 	if (fflags & REQ_FLUSH) {
 		if (rq->cmd_flags & REQ_FLUSH)
 			policy |= REQ_FSEQ_PREFLUSH;
-		if (blk_rq_sectors(rq))
-			policy |= REQ_FSEQ_DATA;
 		if (!(fflags & REQ_FUA) && (rq->cmd_flags & REQ_FUA))
 			policy |= REQ_FSEQ_POSTFLUSH;
 	}
