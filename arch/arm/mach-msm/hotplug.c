1d3cfb34f9605578b4f733acba4affa751b2e4a2
Will Deacon <will.deacon@arm.com>
2011-08-09 12:02:27 +0100

    ARM: msm: convert logical CPU numbers to physical numbers
    This patch uses the new cpu_logical_map() macro for converting logical
    CPU numbers into physical numbers when dealing with the pen_release
    variable in the SMP boot and CPU hotplug paths.
    
    Tested-and-acked-by: David Brown <davidb@codeaurora.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm/mach-msm/hotplug.c
+++ arch/arm/mach-msm/hotplug.c
@@ -37,7 +37,7 @@ static inline void platform_do_lowpower(unsigned int cpu)
 		    :
 		    : "memory", "cc");
 
-		if (pen_release == cpu) {
+		if (pen_release == cpu_logical_map(cpu)) {
 			/*
 			 * OK, proper wakeup, we're done
 			 */
