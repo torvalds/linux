ca123102f69fb260221502ade9bbc069290fae84
Rafael J. Wysocki <rjw@sisk.pl>
2011-08-11 22:38:12 +0200

    PM: Fix build issue in main.c for CONFIG_PM_SLEEP unset
    Suspend statistics should depend on CONFIG_PM_SLEEP, so make that
    happen.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -133,8 +133,6 @@ static ssize_t pm_test_store(struct kobject *kobj, struct kobj_attribute *attr,
 power_attr(pm_test);
 #endif /* CONFIG_PM_DEBUG */
 
-#endif /* CONFIG_PM_SLEEP */
-
 #ifdef CONFIG_DEBUG_FS
 static char *suspend_step_name(enum suspend_stat_step step)
 {
@@ -229,6 +227,8 @@ static int __init pm_debugfs_init(void)
 
 late_initcall(pm_debugfs_init);
 #endif /* CONFIG_DEBUG_FS */
+
+#endif /* CONFIG_PM_SLEEP */
 
 struct kobject *power_kobj;
 
