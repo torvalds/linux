63f409a444fa295247363a28381314683d6f4596
Chris Rankin <rankincj@yahoo.com>
2011-08-14 09:18:46 -0300

    [media] Add missing OK key to PCTV IR keymap
    Hi,
    
    The following patch adds the IR code for the missing "OK" key to the Pinnacle
    PCTV HD map. This map is now used by the PCTV 290e DVB-T2 device, whose remote
    control has 26 buttons.
    
    Signed-off-by: Chris Rankin <rankincj@yahoo.com>
    Reviewed-by: Devin Heitmueller <dheitmueller@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/rc/keymaps/rc-pinnacle-pctv-hd.c
+++ drivers/media/rc/keymaps/rc-pinnacle-pctv-hd.c
@@ -20,6 +20,7 @@ static struct rc_map_table pinnacle_pctv_hd[] = {
 	{ 0x0701, KEY_MENU }, /* Pinnacle logo */
 	{ 0x0739, KEY_POWER },
 	{ 0x0703, KEY_VOLUMEUP },
+	{ 0x0705, KEY_OK },
 	{ 0x0709, KEY_VOLUMEDOWN },
 	{ 0x0706, KEY_CHANNELUP },
 	{ 0x070c, KEY_CHANNELDOWN },
