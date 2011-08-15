35c3ae5eef3fc641c75c5e1e4307835c8efa5f6b
Mark Brown <broonie@opensource.wolfsonmicro.com>
2011-08-16 08:37:42 +0900

    wm831x_power: Only register WM831x battery charger if enabled
    Don't tell the system about the battery charger if it's not enabled, we
    can reasonably assume that if the charger is not enabled then no battery
    will ever be connected so reporting state is at best going to waste time
    and at worst going to cause confusing information in the UI.
    
    For simplicity and robustness we continue to register and handle interrupts
    from the charger.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Anton Vorontsov <cbouatmailru@gmail.com>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -77,8 +77,8 @@ static ssize_t power_supply_show_property(struct device *dev,
 			dev_dbg(dev, "driver has no data for `%s' property\n",
 				attr->attr.name);
 		else if (ret != -ENODEV)
-			dev_err(dev, "driver failed to report `%s' property\n",
-				attr->attr.name);
+			dev_err(dev, "driver failed to report `%s' property: %d\n",
+				attr->attr.name, ret);
 		return ret;
 	}
 
