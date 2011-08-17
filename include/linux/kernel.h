b7d739c58e38e3430b216c789e2a61effc5d10e9
Nishanth Menon <nm@ti.com>
2011-08-17 18:31:58 -0500

    panic: Add board ID to panic output
    At times, it is necessary for boards to provide some additional information
    as part of panic logs. Provide information on the board hardware as part
    of panic logs.
    
    It is safer to print this information at the very end in case something
    bad happens as part of the information retrieval itself.
    
    To use this, set global mach_panic_string to an appropriate string in the
    board file.
    
    Change-Id: Id12cdda87b0cd2940dd01d52db97e6162f671b4d
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -705,6 +705,9 @@ static inline void ftrace_dump(enum ftrace_dump_mode oops_dump_mode) { }
 
 extern int do_sysinfo(struct sysinfo *info);
 
+/* To identify board information in panic logs, set this */
+extern char *mach_panic_string;
+
 #endif /* __KERNEL__ */
 
 #endif
