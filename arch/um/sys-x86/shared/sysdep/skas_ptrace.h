c75361bddb21761cabddde95dcae2059b021ea5d
Al Viro <viro@ftp.linux.org.uk>
2011-08-18 20:05:49 +0100

    um: merge skas_ptrace.h 32/64bit variants
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/sys-x86/shared/sysdep/skas_ptrace.h
+++ arch/um/sys-x86/shared/sysdep/skas_ptrace.h
@@ -1,5 +1,22 @@
-#ifdef __i386__
-#include "skas_ptrace_32.h"
-#else
-#include "skas_ptrace_64.h"
+/*
+ * Copyright (C) 2000, 2001, 2002 Jeff Dike (jdike@karaya.com)
+ * Licensed under the GPL
+ */
+
+#ifndef __SYSDEP_X86_SKAS_PTRACE_H
+#define __SYSDEP_X86_SKAS_PTRACE_H
+
+struct ptrace_faultinfo {
+        int is_write;
+        unsigned long addr;
+};
+
+struct ptrace_ldt {
+        int func;
+        void *ptr;
+        unsigned long bytecount;
+};
+
+#define PTRACE_LDT 54
+
 #endif
