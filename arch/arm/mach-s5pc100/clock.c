d20cc4c1b660d59a25f8bebef5d55c1872b9aa76
Vladimir Zapolskiy <vz@mleia.com>
2011-08-18 19:24:55 +0900

    ARM: S5PC100: use dma-pl330 device name for clock
    This change replaces s3c-pl330.x clock device names with dma-pl330.x,
    otherwise there won't be a correspondence between clock device name
    and amba device name, thus clocks can't be enabled.
    
    Fixes runtime errors on clk_get() from drivers/dma/pl330.c:
      dma-pl330 dma-pl330.0: Cannot get operation clock.
      dma-pl330: probe of dma-pl330.0 failed with error -22
    
    Signed-off-by: Vladimir Zapolskiy <vz@mleia.com>
    Acked-by: Boojin Kim <boojin.kim@samsung.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s5pc100/clock.c
+++ arch/arm/mach-s5pc100/clock.c
@@ -455,13 +455,13 @@ static struct clk init_clocks_off[] = {
 		.ctrlbit	= (1 << 2),
 	}, {
 		.name		= "pdma",
-		.devname	= "s3c-pl330.1",
+		.devname	= "dma-pl330.1",
 		.parent		= &clk_div_d1_bus.clk,
 		.enable		= s5pc100_d1_0_ctrl,
 		.ctrlbit	= (1 << 1),
 	}, {
 		.name		= "pdma",
-		.devname	= "s3c-pl330.0",
+		.devname	= "dma-pl330.0",
 		.parent		= &clk_div_d1_bus.clk,
 		.enable		= s5pc100_d1_0_ctrl,
 		.ctrlbit	= (1 << 0),
