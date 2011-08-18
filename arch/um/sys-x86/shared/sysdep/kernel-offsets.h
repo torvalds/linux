709585063f3d3da68f29b3d706fddb74de5e33f4
Al Viro <viro@ftp.linux.org.uk>
2011-08-18 20:05:59 +0100

    um: merge kernel_offsets_*.h
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/sys-x86/shared/sysdep/kernel-offsets.h
+++ arch/um/sys-x86/shared/sysdep/kernel-offsets.h
@@ -1,5 +1,21 @@
-#ifdef __i386__
-#include "kernel-offsets_32.h"
-#else
-#include "kernel-offsets_64.h"
-#endif
+#include <linux/stddef.h>
+#include <linux/sched.h>
+#include <linux/elf.h>
+#include <linux/crypto.h>
+#include <asm/mman.h>
+
+#define DEFINE(sym, val) \
+	asm volatile("\n->" #sym " %0 " #val : : "i" (val))
+
+#define STR(x) #x
+#define DEFINE_STR(sym, val) asm volatile("\n->" #sym " " STR(val) " " #val: : )
+
+#define BLANK() asm volatile("\n->" : : )
+
+#define OFFSET(sym, str, mem) \
+	DEFINE(sym, offsetof(struct str, mem));
+
+void foo(void)
+{
+#include <common-offsets.h>
+}
