6e5474ed24258324609c01ea7a652e309021b3c3
黄涛 <huangtao@rock-chips.com>
2011-08-19 10:06:21 +0800

    arm: add MT_DEVICE_STRONGLY_ORDERED mem type

--- arch/arm/include/asm/mach/map.h
+++ arch/arm/include/asm/mach/map.h
@@ -27,6 +27,7 @@ struct map_desc {
 #define MT_MEMORY		9
 #define MT_ROM			10
 #define MT_MEMORY_NONCACHED	11
+#define MT_DEVICE_STRONGLY_ORDERED	12
 
 #ifdef CONFIG_MMU
 extern void iotable_init(struct map_desc *, int);
