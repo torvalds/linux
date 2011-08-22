389d12cc7f9715771f6db64b4be1fc3d25576a57
Jon Medhurst <tixy@yxit.co.uk>
2011-08-22 10:47:56 +0100

    ARM: mxc: Remove setting of consistent dma size
    All in-tree MX boards using video use memblock_* functions to get their
    coherent dma space for the camera. So there is no need to increase
    CONSISTENT_DMA_SIZE beyond the default 2MB and we can simply remove the
    defines which do this.
    
    Signed-off-by: Jon Medhurst <tixy@yxit.co.uk>
    Acked-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/plat-mxc/include/mach/memory.h
+++ arch/arm/plat-mxc/include/mach/memory.h
@@ -40,19 +40,4 @@
 # endif
 #endif
 
-#if defined(CONFIG_MX3_VIDEO)
-/*
- * Increase size of DMA-consistent memory region.
- * This is required for mx3 camera driver to capture at least two QXGA frames.
- */
-#define CONSISTENT_DMA_SIZE SZ_8M
-
-#elif defined(CONFIG_MX1_VIDEO) || defined(CONFIG_VIDEO_MX2_HOSTSUPPORT)
-/*
- * Increase size of DMA-consistent memory region.
- * This is required for i.MX camera driver to capture at least four VGA frames.
- */
-#define CONSISTENT_DMA_SIZE SZ_4M
-#endif /* CONFIG_MX1_VIDEO || CONFIG_VIDEO_MX2_HOSTSUPPORT */
-
 #endif /* __ASM_ARCH_MXC_MEMORY_H__ */
