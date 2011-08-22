9d08d5d77a355510c2f5657c86b0a4b25acfe72c
Linus Walleij <linus.walleij@linaro.org>
2011-08-22 08:49:34 +0100

    ARM: 7050/1: mach-sa1100: delete irq_to_gpio() function
    This function is not used in the assabet build, and on the whole the
    call is hard to consolidate so get rid of it from this machine.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-sa1100/include/mach/gpio.h
+++ arch/arm/mach-sa1100/include/mach/gpio.h
@@ -53,7 +53,5 @@ static inline void gpio_set_value(unsigned gpio, int value)
 
 #define gpio_to_irq(gpio)	((gpio < 11) ? (IRQ_GPIO0 + gpio) : \
 					(IRQ_GPIO11 - 11 + gpio))
-#define irq_to_gpio(irq)	((irq < IRQ_GPIO11_27) ? (irq - IRQ_GPIO0) : \
-					(irq - IRQ_GPIO11 + 11))
 
 #endif
