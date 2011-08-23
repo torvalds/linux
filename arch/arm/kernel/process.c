f833f2c25f471bc999b64522d4f32de7eff4455b
Dima Zavin <dima@android.com>
2011-08-23 15:56:50 -0700

    ARM: add option to flush console before reboot
    If the console_lock was held while the system was rebooted, the messages
    in the temporary logbuffer would not have propogated to all the console
    drivers.
    
    This force releases the console lock if it failed to be acquired.
    
    Change-Id: I193dcf7b968be17966833e50b8b8bc70d5d9fe89
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -32,6 +32,7 @@
 #include <linux/hw_breakpoint.h>
 #include <linux/cpuidle.h>
 #include <linux/leds.h>
+#include <linux/console.h>
 
 #include <asm/cacheflush.h>
 #include <asm/idmap.h>
@@ -59,6 +60,31 @@ static const char *isa_modes[] = {
 
 extern void call_with_stack(void (*fn)(void *), void *arg, void *sp);
 typedef void (*phys_reset_t)(unsigned long);
+
+#ifdef CONFIG_ARM_FLUSH_CONSOLE_ON_RESTART
+void arm_machine_flush_console(void)
+{
+	printk("\n");
+	pr_emerg("Restarting %s\n", linux_banner);
+	if (console_trylock()) {
+		console_unlock();
+		return;
+	}
+
+	mdelay(50);
+
+	local_irq_disable();
+	if (!console_trylock())
+		pr_emerg("arm_restart: Console was locked! Busting\n");
+	else
+		pr_emerg("arm_restart: Console was locked!\n");
+	console_unlock();
+}
+#else
+void arm_machine_flush_console(void)
+{
+}
+#endif
 
 /*
  * A temporary stack to use for CPU reset. This is static so that we
@@ -239,6 +265,10 @@ void machine_power_off(void)
 void machine_restart(char *cmd)
 {
 	smp_send_stop();
+
+	/* Flush the console to make sure all the relevant messages make it
+	 * out to the console drivers */
+	arm_machine_flush_console();
 
 	arm_pm_restart(reboot_mode, cmd);
 
