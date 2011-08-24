7e49b1c8c6d64f55ac83e1f5901b22fa9e51ab80
Andy Lutomirski <luto@mit.edu>
2011-08-24 10:19:44 -0400

    x86-64, unistd: Remove bogus __IGNORE_getcpu
    The change:
    
    commit fce8dc06423d6fb2709469dc5c55b04e09c1d126
    Author: Andy Lutomirski <luto@mit.edu>
    Date:   Wed Aug 10 11:15:31 2011 -0400
    
        x86-64: Wire up getcpu syscall
    
    added getcpu as a real syscall, so we shouldn't ignore it any more.
    
    Signed-off-by: Andy Lutomirski <luto@mit.edu>
    Link: http://lkml.kernel.org/r/b4cb60ef45db3a675a0e2b9d51bcb022b0a9ab9c.1314195481.git.luto@mit.edu
    Reported-by: H.J. Lu <hjl.tools@gmail.com>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/include/asm/unistd_64.h
+++ arch/x86/include/asm/unistd_64.h
@@ -624,7 +624,6 @@ __SYSCALL(__NR_vmsplice, sys_vmsplice)
 __SYSCALL(__NR_move_pages, sys_move_pages)
 #define __NR_utimensat				280
 __SYSCALL(__NR_utimensat, sys_utimensat)
-#define __IGNORE_getcpu		/* implemented as a vsyscall */
 #define __NR_epoll_pwait			281
 __SYSCALL(__NR_epoll_pwait, sys_epoll_pwait)
 #define __NR_signalfd				282
