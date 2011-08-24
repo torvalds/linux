5c3f96b20954fd6932bcfb1a860fa1d8b5b22ab0
Magnus Damm <damm@opensource.se>
2011-08-24 22:38:43 +0200

    ARM: mach-shmobile: sh7372 LCDC1 suspend fix
    Associate the HDMI clock together with LCDC1 on sh7372.
    
    Without this patch Suspend-to-RAM hangs on the boards
    AP4EVB and Mackerel. The code hangs in the LCDC driver
    where the software is waiting forever for the hardware to
    power down. By explicitly associating the HDMI clock with
    LCDC1 we can make sure the HDMI clock is enabled using
    Runtime PM whenever the driver is accessing the hardware.
    
    This HDMI and LCDC1 dependency is documented in the sh7372
    data sheet. Older kernels did work as expected but the
    recently merged (3.1-rc)
    
     794d78f drivers: sh: late disabling of clocks V2
    
    introduced code to turn off clocks lacking software reference
    which happens to include the HDMI clock that is needed by
    LCDC1 to operate as expected.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- arch/arm/mach-shmobile/clock-sh7372.c
+++ arch/arm/mach-shmobile/clock-sh7372.c
@@ -655,6 +655,8 @@ static struct clk_lookup lookups[] = {
 	CLKDEV_DEV_ID("renesas_usbhs.1", &mstp_clks[MSTP406]), /* USB1 */
 	CLKDEV_DEV_ID("sh_keysc.0", &mstp_clks[MSTP403]), /* KEYSC */
 
+	CLKDEV_ICK_ID("hdmi", "sh_mobile_lcdc_fb.1",
+		      &div6_reparent_clks[DIV6_HDMI]),
 	CLKDEV_ICK_ID("ick", "sh-mobile-hdmi", &div6_reparent_clks[DIV6_HDMI]),
 	CLKDEV_ICK_ID("icka", "sh_fsi2", &div6_reparent_clks[DIV6_FSIA]),
 	CLKDEV_ICK_ID("ickb", "sh_fsi2", &div6_reparent_clks[DIV6_FSIB]),
