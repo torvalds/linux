8af3d8d5bc75914a01fe41839474902f2c6b79b9
Ferenc Wagner <wferi@niif.hu>
2011-08-25 14:44:57 +0200

    [SCSI] fusion: ensure NUL-termination of MptCallbacksName elements
    Using strlcpy instead of memcpy makes the strlen() calls superfluous
    and also ensures zero-termination of the copied string.
    At the same time get rid of the magic constant 50.
    
    Actually, I can't see why copying the callback name is necessary
    instead of simply storing a pointer to it (just like to the callback
    function itself), but that goes beyond the scope of this fix.
    
    Signed-off-by: Ferenc Wagner <wferi@niif.hu>
    Acked-by: "Nandigama, Nagalakshmi" <Nagalakshmi.Nandigama@lsi.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/message/fusion/mptbase.h
+++ drivers/message/fusion/mptbase.h
@@ -89,6 +89,7 @@
  */
 #define MPT_MAX_ADAPTERS		18
 #define MPT_MAX_PROTOCOL_DRIVERS	16
+#define MPT_MAX_CALLBACKNAME_LEN	49
 #define MPT_MAX_BUS			1	/* Do not change */
 #define MPT_MAX_FC_DEVICES		255
 #define MPT_MAX_SCSI_DEVICES		16
