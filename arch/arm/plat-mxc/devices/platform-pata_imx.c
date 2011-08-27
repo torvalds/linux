318007e9001349db9b4fcd49e9e79a7636dba7a9
Arnaud Patard (Rtp) <arnaud.patard@rtp-net.org>
2011-08-27 15:19:00 +0200

    Fix pata imx resource
    Commit 236c4e8be436380b5354fe0d7facf94688e024ec
    "ARM: imx: Add PATA resources for other i.MX processors"
    has broken resource declaration of pata_imx device by using iobase instead of
    iosize.
    
    Signed-off-by: Arnaud Patard <arnaud.patard@rtp-net.org>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/plat-mxc/devices/platform-pata_imx.c
+++ arch/arm/plat-mxc/devices/platform-pata_imx.c
@@ -44,7 +44,7 @@ struct platform_device *__init imx_add_pata_imx(
 	struct resource res[] = {
 		{
 			.start = data->iobase,
-			.end = data->iobase + data->iobase - 1,
+			.end = data->iobase + data->iosize - 1,
 			.flags = IORESOURCE_MEM,
 		},
 		{
