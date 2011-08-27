f198ead21bcb7b03d7bb2cba7ba0f5ad615a3862
Felipe Balbi <balbi@ti.com>
2011-08-27 15:10:09 +0300

    usb: dwc3: gadget: set request dma to invalid when unmapping
    if we don't set DMA address to invalid when unmapping,
    we might fall in a situation where request buffer
    can't be mapped to DMA again.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -83,6 +83,7 @@ void dwc3_unmap_buffer_from_dma(struct dwc3_request *req)
 				req->request.length, req->direction
 				? DMA_TO_DEVICE : DMA_FROM_DEVICE);
 		req->mapped = 0;
+		req->request.dma = DMA_ADDR_INVALID;
 	} else {
 		dma_sync_single_for_cpu(dwc->dev, req->request.dma,
 				req->request.length, req->direction
