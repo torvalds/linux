70e522a02838e52ca007d3f183171fd9324eceb4
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-08-29 17:52:39 -0400

    cpuidle: ladder.c needs module.h and not just moduleparam.h
    This file has module_init/exit and MODULE_LICENSE, and so it
    needs the full module.h header.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/cpuidle/governors/ladder.c
+++ drivers/cpuidle/governors/ladder.c
@@ -15,7 +15,7 @@
 #include <linux/kernel.h>
 #include <linux/cpuidle.h>
 #include <linux/pm_qos.h>
-#include <linux/moduleparam.h>
+#include <linux/module.h>
 #include <linux/jiffies.h>
 
 #include <asm/io.h>
