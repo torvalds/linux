95cf7a199cf8bcd6647c3b90efb78614157bf766
Ming Lei <ming.lei@canonical.com>
2011-08-30 16:03:13 +0000

    usb: ehci: make HC see up-to-date qh/qtd descriptor ASAP
    This patch introduces the helper of ehci_sync_mem to flush
    qtd/qh into memory immediately on some ARM, so that HC can
    see the up-to-date qtd/qh descriptor asap.
    
    This patch fixs one performance bug on ARM Cortex A9 dual core
    platform, which has been reported on quite a few ARM machines
    (OMAP4, Tegra 2, snowball...), see details from link of
    https://bugs.launchpad.net/bugs/709245.
    
    The patch has been tested ok on OMAP4 panda A1 board, and the
    performance of 'dd' over usb mass storage can be increased from
    4~5MB/sec to 14~16MB/sec after applying this patch.
    
    Change-Id: I7994c58a1001c7f46f13e09420328a3916bbfcef
    Cc: Alan Stern <stern@rowland.harvard.edu>
    Cc: Russell King <linux@arm.linux.org.uk>
    Signed-off-by: Ming Lei <ming.lei@canonical.com>
    Signed-off-by: Alan Stern <stern@rowland.harvard.edu>

--- drivers/usb/host/ehci.h
+++ drivers/usb/host/ehci.h
@@ -736,6 +736,23 @@ static inline u32 hc32_to_cpup (const struct ehci_hcd *ehci, const __hc32 *x)
 
 #endif
 
+/*
+ * Writing to dma coherent memory on ARM may be delayed via L2
+ * writing buffer, so introduce the helper which can flush L2 writing
+ * buffer into memory immediately, especially used to flush ehci
+ * descriptor to memory.
+ * */
+#ifdef	CONFIG_ARM_DMA_MEM_BUFFERABLE
+static inline void ehci_sync_mem()
+{
+	mb();
+}
+#else
+static inline void ehci_sync_mem()
+{
+}
+#endif
+
 /*-------------------------------------------------------------------------*/
 
 #ifndef DEBUG
