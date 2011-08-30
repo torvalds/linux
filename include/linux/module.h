639938eb606e94af498c589feae2f0b8a5c285d1
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-08-30 11:24:44 -0400

    module.h: relocate MODULE_PARM_DESC into moduleparam.h
    There are files which use module_param and MODULE_PARM_DESC
    back to back.  They only include moduleparam.h which makes sense,
    but the implicit presence of module.h everywhere hid the fact
    that MODULE_PARM_DESC wasn't in moduleparam.h at all.  Relocate
    the macro to moduleparam.h so that the moduleparam infrastructure
    can be used independently of module.h
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- include/linux/module.h
+++ include/linux/module.h
@@ -135,11 +135,6 @@ extern const struct gtype##_id __mod_##gtype##_table		\
 /* What your module does. */
 #define MODULE_DESCRIPTION(_description) MODULE_INFO(description, _description)
 
-/* One for each parameter, describing how to use it.  Some files do
-   multiple of these per line, so can't just use MODULE_INFO. */
-#define MODULE_PARM_DESC(_parm, desc) \
-	__MODULE_INFO(parm, _parm, #_parm ":" desc)
-
 #define MODULE_DEVICE_TABLE(type,name)		\
   MODULE_GENERIC_TABLE(type##_device,name)
 
