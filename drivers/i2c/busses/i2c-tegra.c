048e29cff95168ea3a9f176e84cc0bae54d0ae64
Mike Rapoport <mike@compulab.co.il>
2011-08-30 11:46:09 -0600

    i2c-tegra: add I2C_FUNC_SMBUS_EMUL
    Signed-off-by: Mike Rapoport <mike@compulab.co.il>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- drivers/i2c/busses/i2c-tegra.c
+++ drivers/i2c/busses/i2c-tegra.c
@@ -531,7 +531,7 @@ static int tegra_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[],
 
 static u32 tegra_i2c_func(struct i2c_adapter *adap)
 {
-	return I2C_FUNC_I2C;
+	return I2C_FUNC_I2C | I2C_FUNC_SMBUS_EMUL;
 }
 
 static const struct i2c_algorithm tegra_i2c_algo = {
