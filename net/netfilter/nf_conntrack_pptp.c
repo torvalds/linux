4c6e4209662b2a4147cde16c2144a253a7430a49
Sanket Shah <sanket.shah@elitecore.com>
2011-08-30 15:23:03 +0200

    netfilter: nf_ct_pptp: fix DNATed PPTP connection address translation
    When both the server and the client are NATed, the set-link-info control
    packet containing the peer's call-id field is not properly translated.
    
    I have verified that it was working in 2.6.16.13 kernel previously but
    due to rewrite, this scenario stopped working (Not knowing exact version
    when it stopped working).
    
    Signed-off-by: Sanket Shah <sanket.shah@elitecore.com>
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- net/netfilter/nf_conntrack_pptp.c
+++ net/netfilter/nf_conntrack_pptp.c
@@ -364,6 +364,7 @@ pptp_inbound_pkt(struct sk_buff *skb,
 		break;
 
 	case PPTP_WAN_ERROR_NOTIFY:
+	case PPTP_SET_LINK_INFO:
 	case PPTP_ECHO_REQUEST:
 	case PPTP_ECHO_REPLY:
 		/* I don't have to explain these ;) */
