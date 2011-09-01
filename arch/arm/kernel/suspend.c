8e6f83bbdf770014c070c5a41c8e89617cb2a66b
Russell King <rmk+kernel@arm.linux.org.uk>
2011-09-01 11:57:59 +0100

    ARM: pm: add L2 cache cleaning for suspend
    We need to ensure that state is pushed out from the L2 cache when
    suspending so that the resume paths can access their data before the
    MMU and caches have been re-initialized.  Add the necessary calls to
    __cpu_suspend_save().
    
    Tested-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Tested-by: Shawn Guo <shawn.guo@linaro.org>
    Tested-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/suspend.c
+++ arch/arm/kernel/suspend.c
@@ -28,6 +28,9 @@ void __cpu_suspend_save(u32 *ptr, u32 ptrsz, u32 sp, u32 *save_ptr)
 	cpu_do_suspend(ptr);
 
 	flush_cache_all();
+	outer_clean_range(*save_ptr, *save_ptr + ptrsz);
+	outer_clean_range(virt_to_phys(save_ptr),
+			  virt_to_phys(save_ptr) + sizeof(*save_ptr));
 }
 
 /*
