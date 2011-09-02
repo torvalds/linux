cd8a3666987ba20908bbeb7c78ed9ba82b365643
Aaron Lu <aaron.lu@amd.com>
2011-09-02 16:06:08 +0800

    mmc: core: add sd uhs string for mmc_ios_show
    This is a minor fix. It makes mmc_ios_show print proper string when the
    host's timing is one of the newly added UHS-I modes.
    
    Signed-off-by: Aaron Lu <aaron.lu@amd.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/core/debugfs.c
+++ drivers/mmc/core/debugfs.c
@@ -114,6 +114,15 @@ static int mmc_ios_show(struct seq_file *s, void *data)
 	case MMC_TIMING_SD_HS:
 		str = "sd high-speed";
 		break;
+	case MMC_TIMING_UHS_SDR50:
+		str = "sd uhs SDR50";
+		break;
+	case MMC_TIMING_UHS_SDR104:
+		str = "sd uhs SDR104";
+		break;
+	case MMC_TIMING_UHS_DDR50:
+		str = "sd uhs DDR50";
+		break;
 	default:
 		str = "invalid";
 		break;
