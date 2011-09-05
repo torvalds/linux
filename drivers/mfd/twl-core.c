11b8fc6ae54bf18a48c94e181c37ca135b858b42
Thomas Weber <weber@corscience.de>
2011-09-05 11:26:33 +0200

    mfd: Fix twl4030 dependencies for audio codec
    commit f09ee0451a44a4e913a7c3cec3805508f7de6c54 upstream.
    
    The codec for Devkit8000 (TWL4030)  was not detected except
    when build with CONFIG_SND_SOC_ALL_CODECS.
    
    twl-core.c still uses the CONFIG_TWL4030_CODEC for
    twl_has_codec().
    
    In commit 57fe7251f5bfc4332f24479376de48a1e8ca6211
    the CONFIG_TWL4030_CODEC was renamed
    into CONFIG_MFD_TWL4030_AUDIO, thatswhy the codec
    was not detected.
    
    This patch renames the CONFIG_ TWL4030_CODEC into
    CONFIG_MFD_TWL4030_AUDIO in twl-core.c.
    
    Signed-off-by: Thomas Weber <weber@corscience.de>
    Acked-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>
    Cc: Jarkko Nikula <jarkko.nikula@bitmer.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/mfd/twl-core.c
+++ drivers/mfd/twl-core.c
@@ -109,7 +109,7 @@
 #define twl_has_watchdog()        false
 #endif
 
-#if defined(CONFIG_TWL4030_CODEC) || defined(CONFIG_TWL4030_CODEC_MODULE) ||\
+#if defined(CONFIG_MFD_TWL4030_AUDIO) || defined(CONFIG_MFD_TWL4030_AUDIO_MODULE) ||\
 	defined(CONFIG_SND_SOC_TWL6040) || defined(CONFIG_SND_SOC_TWL6040_MODULE)
 #define twl_has_codec()	true
 #else
