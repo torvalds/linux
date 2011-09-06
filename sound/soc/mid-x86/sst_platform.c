22be504aaa4a3133d81e3fb0c4287960aea19c37
Lu Guanqun <guanqun.lu@intel.com>
2011-09-06 15:21:38 +0800

    ASoC: sst_platform: using builtin function
    Use the builtin snd_soc_set_runtime_hwparams() instead of assigning it by
    myself.
    
    Signed-off-by: Lu Guanqun <guanqun.lu@intel.com>
    Acked-by: Vinod Koul <vinod.koul@intel.com>
    Acked-by: Liam Girdwood <lrg@ti.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/mid-x86/sst_platform.c
+++ sound/soc/mid-x86/sst_platform.c
@@ -226,13 +226,14 @@ static int sst_platform_init_stream(struct snd_pcm_substream *substream)
 
 static int sst_platform_open(struct snd_pcm_substream *substream)
 {
-	struct snd_pcm_runtime *runtime;
+	struct snd_pcm_runtime *runtime = substream->runtime;
 	struct sst_runtime_stream *stream;
 	int ret_val = 0;
 
 	pr_debug("sst_platform_open called\n");
-	runtime = substream->runtime;
-	runtime->hw = sst_platform_pcm_hw;
+
+	snd_soc_set_runtime_hwparams(substream, &sst_platform_pcm_hw);
+
 	stream = kzalloc(sizeof(*stream), GFP_KERNEL);
 	if (!stream)
 		return -ENOMEM;
