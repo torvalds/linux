9efb4a1bb9e46d26a15116e3c72b1b81c62d8337
Ohad Ben-Cohen <ohad@wizery.com>
2011-09-11 18:54:50 +0300

    hwspinlock/omap: omap_hwspinlock_remove should be __devexit
    Mark omap_hwspinlock_remove with __devexit (and use __devexit_p
    appropriately) so the function can be discarded when the conditions are met.
    
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>

--- drivers/hwspinlock/omap_hwspinlock.c
+++ drivers/hwspinlock/omap_hwspinlock.c
@@ -142,7 +142,7 @@ iounmap_base:
 	return ret;
 }
 
-static int omap_hwspinlock_remove(struct platform_device *pdev)
+static int __devexit omap_hwspinlock_remove(struct platform_device *pdev)
 {
 	struct hwspinlock_device *bank = platform_get_drvdata(pdev);
 	void __iomem *io_base = bank->lock[0].priv - LOCK_BASE_OFFSET;
@@ -163,7 +163,7 @@ static int omap_hwspinlock_remove(struct platform_device *pdev)
 
 static struct platform_driver omap_hwspinlock_driver = {
 	.probe		= omap_hwspinlock_probe,
-	.remove		= omap_hwspinlock_remove,
+	.remove		= __devexit_p(omap_hwspinlock_remove),
 	.driver		= {
 		.name	= "omap_hwspinlock",
 		.owner	= THIS_MODULE,
