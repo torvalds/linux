f38c2b0920fb22f6a2841f362b66360a04dc974e
Peter Zijlstra <a.p.zijlstra@chello.nl>
2011-09-12 13:12:28 +0200

    llist: Add llist_next()
    So we don't have to expose the struct list_node member.
    
    Cc: Huang Ying <ying.huang@intel.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1315836348.26517.41.camel@twins
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/llist.h
+++ include/linux/llist.h
@@ -138,6 +138,11 @@ static inline bool llist_empty(const struct llist_head *head)
 	return ACCESS_ONCE(head->first) == NULL;
 }
 
+static inline struct llist_node *llist_next(struct llist_node *node)
+{
+	return node->next;
+}
+
 /**
  * llist_add - add a new entry
  * @new:	new entry to be added
