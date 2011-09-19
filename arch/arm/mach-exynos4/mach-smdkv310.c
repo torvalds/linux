1abd328ee5e2e870fa035e7882e7a278127b4ccf
Tushar Behera <tushar.behera@linaro.org>
2011-09-19 20:09:01 +0900

    ARM: EXYNOS4: convert boot_params to atag_offset
    Based on "ARM: introduce atag_offset to replace boot_params"
    by Nicolas Pitre (2bb9839e312ed55a6d5824ffa6077ce3d7d63b1e).
    
    Since boot_params variable is deleted from machine_desc, the variable
    is modified in the newer board files.
    
    Cc: Nicolas Pitre <nicolas.pitre@linaro.org>
    Signed-off-by: Tushar Behera <tushar.behera@linaro.org>
    [kgene.kim@samsung.com: added fixing for smdkv310]
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-exynos4/mach-smdkv310.c
+++ arch/arm/mach-exynos4/mach-smdkv310.c
@@ -324,7 +324,7 @@ static void __init smdkv310_machine_init(void)
 MACHINE_START(SMDKV310, "SMDKV310")
 	/* Maintainer: Kukjin Kim <kgene.kim@samsung.com> */
 	/* Maintainer: Changhwan Youn <chaos.youn@samsung.com> */
-	.boot_params	= S5P_PA_SDRAM + 0x100,
+	.atag_offset	= 0x100,
 	.init_irq	= exynos4_init_irq,
 	.map_io		= smdkv310_map_io,
 	.init_machine	= smdkv310_machine_init,
@@ -333,7 +333,7 @@ MACHINE_END
 
 MACHINE_START(SMDKC210, "SMDKC210")
 	/* Maintainer: Kukjin Kim <kgene.kim@samsung.com> */
-	.boot_params	= S5P_PA_SDRAM + 0x100,
+	.atag_offset	= 0x100,
 	.init_irq	= exynos4_init_irq,
 	.map_io		= smdkv310_map_io,
 	.init_machine	= smdkv310_machine_init,
