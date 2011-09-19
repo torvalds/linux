011a36e1193c02abcdc4853be09275a0fe9d1a32
Vivek Natarajan <nataraja@qca.qualcomm.com>
2011-09-19 13:29:16 +0530

    ath6kl: Indicate the roaming capability of the firmware
    When the rssi of the current AP drops, both wpa_supplicant and the
    firmware may do a background scan to find a better AP and try to
    associate. This might lead to a race condition where both may try
    to connect to some AP based on their scan results.
    
    Since the firmware is capable of handling roaming, let
    wpa_supplicant know about this capability so that it will back off
    from bgscan based roaming.
    
    Signed-off-by: Vivek Natarajan <nataraja@qca.qualcomm.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/init.c
+++ drivers/net/wireless/ath/ath6kl/init.c
@@ -1487,6 +1487,8 @@ static int ath6kl_init(struct net_device *dev)
 	ar->conf_flags = ATH6KL_CONF_IGNORE_ERP_BARKER |
 			 ATH6KL_CONF_ENABLE_11N | ATH6KL_CONF_ENABLE_TX_BURST;
 
+	ar->wdev->wiphy->flags |= WIPHY_FLAG_SUPPORTS_FW_ROAM;
+
 	status = ath6kl_target_config_wlan_params(ar);
 	if (!status)
 		goto ath6kl_init_done;
