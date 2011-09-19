5d42940c25ac69c4f5240392cf5e26bf08029e7a
Axel Lin <axel.lin@gmail.com>
2011-09-19 16:34:28 +0800

    ASoC: sn95031: Staticize sn95031_pcm_hw_params
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Acked-by: Vinod Koul <vinod.koul@linux.intel.com>
    Acked-by: Liam Girdwood <lrg@ti.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/sn95031.c
+++ sound/soc/codecs/sn95031.c
@@ -658,7 +658,7 @@ static int sn95031_pcm_spkr_mute(struct snd_soc_dai *dai, int mute)
 	return 0;
 }
 
-int sn95031_pcm_hw_params(struct snd_pcm_substream *substream,
+static int sn95031_pcm_hw_params(struct snd_pcm_substream *substream,
 		struct snd_pcm_hw_params *params, struct snd_soc_dai *dai)
 {
 	unsigned int format, rate;
