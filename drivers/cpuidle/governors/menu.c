30977e1244781c68f592f212cd4c3291fae6e5ab
Colin Cross <ccross@android.com>
2011-09-19 16:42:44 -0700

    cpuidle: governor: menu: don't use loadavg
    get_loadavg doesn't work as intended.  According to the comments, it
    should be returning an average over a few seconds, but it is actually
    reading the instantaneous load.  It is almost always returning 0, but
    can sometimes, depending on workload, spike very high into the hundreds
    even when the average cpu load is under 10%.  Disable it for now.
    
    Change-Id: I63ed100af1cf9463549939b8113ed83676db5f86
    Signed-off-by: Colin Cross <ccross@android.com>

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -182,7 +182,12 @@ static inline int performance_multiplier(void)
 
 	/* for higher loadavg, we are more reluctant */
 
-	mult += 2 * get_loadavg();
+	/*
+	 * this doesn't work as intended - it is almost always 0, but can
+	 * sometimes, depending on workload, spike very high into the hundreds
+	 * even when the average cpu load is under 10%.
+	 */
+	/* mult += 2 * get_loadavg(); */
 
 	/* for IO wait tasks (per cpu!) we add 5x each */
 	mult += 10 * nr_iowait_cpu(smp_processor_id());
