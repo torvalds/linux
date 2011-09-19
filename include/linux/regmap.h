39a58439d6006c48941511276c0041f56352c529
Mark Brown <broonie@opensource.wolfsonmicro.com>
2011-09-19 18:21:49 +0100

    regmap: Prototype regcache_sync()
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- include/linux/regmap.h
+++ include/linux/regmap.h
@@ -140,4 +140,5 @@ int regmap_bulk_read(struct regmap *map, unsigned int reg, void *val,
 int regmap_update_bits(struct regmap *map, unsigned int reg,
 		       unsigned int mask, unsigned int val);
 
+int regcache_sync(struct regmap *map);
 #endif
