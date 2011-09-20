5ba755e4043bbf6be5816b7cce5baae89c074e9c
Jaikumar Ganesh <jaikumarg@android.com>
2011-09-20 16:40:43 -0700

    HID: Add input_register callback.
    Add input_register callback which gets called after
    hid_configure_usage is called for all the reports
    and before the input device is registered. This allows
    individual drivers to do extra work like input mapping just
    before device registration.
    
    Based on discussions with David Herrmann <dh.herrmann@googlemail.com>
    
    Change-Id: Idab6fb4f7b1e5e569bd0410967288717e9d34c98
    Signed-off-by: Jaikumar Ganesh <jaikumarg@android.com>

--- include/linux/hid.h
+++ include/linux/hid.h
@@ -618,6 +618,8 @@ struct hid_usage_id {
  * @input_mapping: invoked on input registering before mapping an usage
  * @input_mapped: invoked on input registering after mapping an usage
  * @feature_mapping: invoked on feature registering
+ * @input_register: called just before input device is registered after reports
+ * 		    are parsed.
  * @suspend: invoked on suspend (NULL means nop)
  * @resume: invoked on resume if device was not reset (NULL means nop)
  * @reset_resume: invoked on resume if device was reset (NULL means nop)
@@ -664,6 +666,8 @@ struct hid_driver {
 	void (*feature_mapping)(struct hid_device *hdev,
 			struct hid_field *field,
 			struct hid_usage *usage);
+	int (*input_register)(struct hid_device *hdev, struct hid_input
+			*hidinput);
 #ifdef CONFIG_PM
 	int (*suspend)(struct hid_device *hdev, pm_message_t message);
 	int (*resume)(struct hid_device *hdev);
