b78db0895fff3d3da005f8e03f700b9a559c1065
Stefan Beller <stefanbeller@googlemail.com>
2011-09-20 09:16:08 -0700

    platform: samsung_laptop: add dmi information for Samsung R700 laptops
    commit f87d02996f05ec1789ceecce9ec839f629b7aa80 upstream.
    
    My DMI model is this:
    >dmesg |grep DMI
    [    0.000000] DMI present.
    [    0.000000] DMI: SAMSUNG ELECTRONICS CO., LTD. SR700/SR700, BIOS
    04SR 02/20/2008
    
    adding dmi information of Samsung R700 laptops
    This adds the dmi information of Samsungs R700 laptops.
    
    Signed-off-by: Stefan Beller <stefanbeller@googlemail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    Signed-off-by: Matthew Garrett <mjg@redhat.com>

--- drivers/platform/x86/samsung-laptop.c
+++ drivers/platform/x86/samsung-laptop.c
@@ -631,6 +631,15 @@ static struct dmi_system_id __initdata samsung_dmi_table[] = {
 		.callback = dmi_check_cb,
 	},
 	{
+		.ident = "R700",
+		.matches = {
+		      DMI_MATCH(DMI_SYS_VENDOR, "SAMSUNG ELECTRONICS CO., LTD."),
+		      DMI_MATCH(DMI_PRODUCT_NAME, "SR700"),
+		      DMI_MATCH(DMI_BOARD_NAME, "SR700"),
+		},
+		.callback = dmi_check_cb,
+	},
+	{
 		.ident = "R530/R730",
 		.matches = {
 		      DMI_MATCH(DMI_SYS_VENDOR, "SAMSUNG ELECTRONICS CO., LTD."),
