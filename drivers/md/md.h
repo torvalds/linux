7e841526263b3e0042a423513147dfd06c8e998d
Wang Sheng-Hui <shhuiw@gmail.com>
2011-09-21 15:37:46 +1000

    trival: md_k.h should be md.h in the beginning comment of file md.h
    Signed-off-by: Wang Sheng-Hui <shhuiw@gmail.com>
    Signed-off-by: NeilBrown <neilb@suse.de>

--- drivers/md/md.h
+++ drivers/md/md.h
@@ -1,5 +1,5 @@
 /*
-   md_k.h : kernel internal structure of the Linux MD driver
+   md.h : kernel internal structure of the Linux MD driver
           Copyright (C) 1996-98 Ingo Molnar, Gadi Oxman
 	  
    This program is free software; you can redistribute it and/or modify
