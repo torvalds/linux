547d42eac107f027e6101b828f9e4075f2fbcc7c
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-09-23 15:33:27 -0300

    [media] rc tables: include linux/module.h
    Prevents errors when merging with -next:
    
    drivers/media/rc/keymaps/rc-snapstream-firefly.c:105:16: error: expected declaration specifiers or ‘...’ before string constant
    drivers/media/rc/keymaps/rc-snapstream-firefly.c:106:15: error: expected declaration specifiers or ‘...’ before string constant
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/rc/keymaps/rc-ati-x10.c
+++ drivers/media/rc/keymaps/rc-ati-x10.c
@@ -23,6 +23,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#include <linux/module.h>
 #include <media/rc-map.h>
 
 static struct rc_map_table ati_x10[] = {
--- drivers/media/rc/keymaps/rc-medion-x10.c
+++ drivers/media/rc/keymaps/rc-medion-x10.c
@@ -21,6 +21,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#include <linux/module.h>
 #include <media/rc-map.h>
 
 static struct rc_map_table medion_x10[] = {
