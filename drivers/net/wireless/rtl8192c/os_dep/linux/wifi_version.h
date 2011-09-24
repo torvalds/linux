20e6fe9c5fb4b3e678b2a749bd6734a20455d349
高伟龙 <gwl@rock-chips.com>
2011-09-24 21:18:49 +0800

    Promote the capacity of WiFi connect AP.(V3.20)

---
+++ drivers/net/wireless/rtl8192c/os_dep/linux/wifi_version.h
@@ -0,0 +1,13 @@
+/*
+ * Yongle Lai @ Rockchip
+ */
+#ifndef WIFI_BCM4319_VERSION_H
+#define WIFI_BCM4319_VERSION_H
+
+/*
+ * Broadcom BCM4319 driver version.
+ */
+#define RTL8192_DRV_VERSION "3.20"
+
+#endif /* WIFI_BCM4319_VERSION_H */
+
