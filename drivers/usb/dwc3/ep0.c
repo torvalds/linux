b147f3572db230522d158312055f3d28f96fbc92
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2011-09-30 10:58:40 +0300

    usb: dwc3: ep0: fix debug output
    Use "ep0in" and "ep0out" instead "ep1in" and "ep0out" which is confusing
    and not consistent with the remaining output.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/dwc3/ep0.c
+++ drivers/usb/dwc3/ep0.c
@@ -785,7 +785,7 @@ void dwc3_ep0_interrupt(struct dwc3 *dwc,
 
 	dev_dbg(dwc->dev, "%s while ep%d%s in state '%s'\n",
 			dwc3_ep_event_string(event->endpoint_event),
-			epnum, (epnum & 1) ? "in" : "out",
+			epnum >> 1, (epnum & 1) ? "in" : "out",
 			dwc3_ep0_state_string(dwc->ep0state));
 
 	switch (event->endpoint_event) {
