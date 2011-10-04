81ec98898188639ac53413605681b3e3bb0a2ff1
Mark Salter <msalter@redhat.com>
2011-10-04 11:10:50 -0400

    C6X: clocks
    The C6X SoCs contain several PLL controllers each with up to 16 clock outputs
    feeding into the cores or peripheral clock domains. The hardware is very similar
    to arm/mach-davinci clocks. This is still a work in progress which needs to be
    updated once device tree clock binding changes shake out.
    
    Signed-off-by: Mark Salter <msalter@redhat.com>
    Signed-off-by: Aurelien Jacquiot <a-jacquiot@ti.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

---
+++ arch/c6x/include/asm/clkdev.h
@@ -0,0 +1,22 @@
+#ifndef _ASM_CLKDEV_H
+#define _ASM_CLKDEV_H
+
+#include <linux/slab.h>
+
+struct clk;
+
+static inline int __clk_get(struct clk *clk)
+{
+	return 1;
+}
+
+static inline void __clk_put(struct clk *clk)
+{
+}
+
+static inline struct clk_lookup_alloc *__clkdev_alloc(size_t size)
+{
+	return kzalloc(size, GFP_KERNEL);
+}
+
+#endif /* _ASM_CLKDEV_H */
