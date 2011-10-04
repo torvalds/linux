3e28189038bb831512cf4f8313e1aead97c3e63f
Tomi Valkeinen <tomi.valkeinen@ti.com>
2011-10-04 11:39:38 +0300

    OMAPDSS: picodlp: add missing #include <linux/module.h>
    Compiling panel-picodlp.c failed with:
    
    drivers/video/omap2/displays/panel-picodlp.c:560:12: error:
    'THIS_MODULE' undeclared here (not in a function)
    
    Add #include <linux/module.h> to get THIS_MODULE definition.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/displays/panel-picodlp.c
+++ drivers/video/omap2/displays/panel-picodlp.c
@@ -19,6 +19,7 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <linux/module.h>
 #include <linux/input.h>
 #include <linux/platform_device.h>
 #include <linux/interrupt.h>
