731b25a4ad3c27b44f3447382da18b59167eb7a1
Bogdan Radulescu <bogdan@nimblex.net>
2011-10-06 20:35:12 +0200

    PM / ACPI: Blacklist Vaio VGN-FW520F machine known to require acpi_sleep=nonvs
    Sony Vaio VGN-FW520F does not resume correctly without
    acpi_sleep=nonvs, so add it to the ACPI sleep blacklist.
    
    References: https://bugzilla.kernel.org/show_bug.cgi?id=16396#c86
    Signed-off-by: Bogdan Radulescu <bogdan@nimblex.net>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- drivers/acpi/sleep.c
+++ drivers/acpi/sleep.c
@@ -452,6 +452,14 @@ static struct dmi_system_id __initdata acpisleep_dmi_table[] = {
 		DMI_MATCH(DMI_PRODUCT_NAME, "VGN-SR26GN_P"),
 		},
 	},
+	{
+	.callback = init_nvs_nosave,
+	.ident = "Sony Vaio VGN-FW520F",
+	.matches = {
+		DMI_MATCH(DMI_SYS_VENDOR, "Sony Corporation"),
+		DMI_MATCH(DMI_PRODUCT_NAME, "VGN-FW520F"),
+		},
+	},
 	{},
 };
 #endif /* CONFIG_SUSPEND */
