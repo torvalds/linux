bbc1505482e5bb16fa956e23f22dae6cd25172e6
Seth Forshee <seth.forshee@canonical.com>
2011-10-06 15:01:55 -0500

    acer-wmi: Add wireless quirk for Lenovo 3000 N200
    commit be3128b107e36271f7973ef213ccde603a494fe8 upstream.
    
    This quirk fixes the wlan rfkill status on this machine. Without
    it, wlan is permanently soft blocked whenever acer-wmi is loaded.
    
    BugLink: https://bugs.launchpad.net/bugs/857297
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>
    Reviewed-by: Lee, Chun-Yi <jlee@novell.com>
    Signed-off-by: Matthew Garrett <mjg@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/platform/x86/acer-wmi.c
+++ drivers/platform/x86/acer-wmi.c
@@ -463,6 +463,15 @@ static struct dmi_system_id acer_quirks[] = {
 		},
 		.driver_data = &quirk_lenovo_ideapad_s205,
 	},
+	{
+		.callback = dmi_matched,
+		.ident = "Lenovo 3000 N200",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "0687A31"),
+		},
+		.driver_data = &quirk_fujitsu_amilo_li_1718,
+	},
 	{}
 };
 
