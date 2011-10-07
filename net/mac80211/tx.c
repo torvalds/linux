d5294971f11fc2b150437e43a4057c867c2bf413
Johannes Berg <johannes.berg@intel.com>
2011-10-07 14:55:40 +0200

    mac80211: dont orphan TX skb
    This was another workaround for truesize "bugs".
    The reason we did this was that when we orphaned
    the SKB it wouldn't be truesize-checked later.
    Now that the check is gone (and we just charge
    the former smaller size to the socket) there's
    no longer a reason to orphan the skb here.
    
    Keep the skb charged to the socket until it is
    really freed (or orphaned in TX status). This
    helps flow control and allows us to get at the
    socket later for other purposes.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/tx.c
+++ net/mac80211/tx.c
@@ -1384,11 +1384,6 @@ static int ieee80211_skb_resize(struct ieee80211_sub_if_data *sdata,
 		tail_need = max_t(int, tail_need, 0);
 	}
 
-	if (head_need || tail_need) {
-		/* Sorry. Can't account for this any more */
-		skb_orphan(skb);
-	}
-
 	if (skb_cloned(skb))
 		I802_DEBUG_INC(local->tx_expand_skb_head_cloned);
 	else if (head_need || tail_need)
