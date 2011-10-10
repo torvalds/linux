95f5f803b3897f622c4b5f72d554874faf74df12
danborkmann@iogearbox.net <danborkmann@iogearbox.net>
2011-10-10 06:52:46 +0000

    af_packet: remove unnecessary BUG_ON() in tpacket_destruct_skb
    If skb is NULL, then stack trace is thrown anyway on dereference.
    Therefore, the stack trace triggered by BUG_ON is duplicate.
    
    Signed-off-by: Daniel Borkmann <danborkmann@googlemail.com>
    Cc: Eric Dumazet <eric.dumazet@gmail.com>
    Acked-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -1968,8 +1968,6 @@ static void tpacket_destruct_skb(struct sk_buff *skb)
 	struct packet_sock *po = pkt_sk(skb->sk);
 	void *ph;
 
-	BUG_ON(skb == NULL);
-
 	if (likely(po->tx_ring.pg_vec)) {
 		ph = skb_shinfo(skb)->destructor_arg;
 		BUG_ON(__packet_get_status(po, ph) != TP_STATUS_SENDING);
