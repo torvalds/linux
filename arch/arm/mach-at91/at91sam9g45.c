49fe2ba3138ca60422fd90ed76c1918be1c30fc0
Nicolas Ferre <nicolas.ferre@atmel.com>
2011-10-10 18:29:24 +0200

    ARM: at91: dt: at91sam9g45 family and board device tree files
    Create a new device tree source file for Atmel at91sam9g45 SoC family.
    The Evaluation Kit at91sam9m10g45ek includes it.
    This first basic support will be populated as drivers and boards will be
    converted to device tree.
    Contains serial, dma and interrupt controllers.
    
    The generic board file still takes advantage of platform data for early serial
    init. As we need a storage media and the NAND flash driver is not converted to
    DT yet, we keep old initialization for it.
    
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Reviewed-by: Rob Herring <rob.herring@calxeda.com>

--- arch/arm/mach-at91/at91sam9g45.c
+++ arch/arm/mach-at91/at91sam9g45.c
@@ -215,6 +215,12 @@ static struct clk_lookup periph_clocks_lookups[] = {
 	CLKDEV_CON_DEV_ID("t0_clk", "atmel_tcb.1", &tcb0_clk),
 	CLKDEV_CON_DEV_ID("pclk", "ssc.0", &ssc0_clk),
 	CLKDEV_CON_DEV_ID("pclk", "ssc.1", &ssc1_clk),
+	/* more usart lookup table for DT entries */
+	CLKDEV_CON_DEV_ID("usart", "ffffee00.serial", &mck),
+	CLKDEV_CON_DEV_ID("usart", "fff8c000.serial", &usart0_clk),
+	CLKDEV_CON_DEV_ID("usart", "fff90000.serial", &usart1_clk),
+	CLKDEV_CON_DEV_ID("usart", "fff94000.serial", &usart2_clk),
+	CLKDEV_CON_DEV_ID("usart", "fff98000.serial", &usart3_clk),
 };
 
 static struct clk_lookup usart_clocks_lookups[] = {
