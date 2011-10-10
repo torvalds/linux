a461e0ad3d27b6342140566909a80db30d151a91
Steven Toth <stoth@kernellabs.com>
2011-10-10 11:09:56 -0300

    [media] cx23885: Stop the risc video fifo before reconfiguring it
    Safety reasons. We shouldn't be trying to reconfigure a risc
    processor instruction queue unless it's stopped.
    
    Signed-off-by: Steven Toth <stoth@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx23885/cx23885-video.c
+++ drivers/media/video/cx23885/cx23885-video.c
@@ -549,6 +549,9 @@ static int cx23885_start_video_dma(struct cx23885_dev *dev,
 {
 	dprintk(1, "%s()\n", __func__);
 
+	/* Stop the dma/fifo before we tamper with it's risc programs */
+	cx_clear(VID_A_DMA_CTL, 0x11);
+
 	/* setup fifo + format */
 	cx23885_sram_channel_setup(dev, &dev->sram_channels[SRAM_CH01],
 				buf->bpl, buf->risc.dma);
