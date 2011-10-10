0dc8985b7f2da02180135396871d396f33a39b43
J. Bruce Fields <bfields@redhat.com>
2011-10-10 17:34:31 -0400

    nfsd4: ignore WANT bits in open downgrade
    commit c30e92df30d7d5fe65262fbce5d1b7de675fe34e upstream.
    
    We don't use WANT bits yet--and sending them can probably trigger a
    BUG() further down.
    
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -3421,6 +3421,8 @@ nfsd4_open_downgrade(struct svc_rqst *rqstp,
 	if (!access_valid(od->od_share_access, cstate->minorversion)
 			|| !deny_valid(od->od_share_deny))
 		return nfserr_inval;
+	/* We don't yet support WANT bits: */
+	od->od_share_access &= NFS4_SHARE_ACCESS_MASK;
 
 	nfs4_lock_state();
 	if ((status = nfs4_preprocess_seqid_op(cstate,
