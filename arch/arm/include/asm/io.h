6c5482d53f195d3ca61c9ec1be25b0f4a92575fe
Tony Lindgren <tony@atomide.com>
2011-10-12 01:02:50 +0100

    ARM: 7129/1: Add __arm_ioremap_exec for mapping external memory as MT_MEMORY
    This allows mapping external memory such as SRAM for use.
    
    This is needed for some small chunks of code, such as reprogramming
    SDRAM memory source clocks that can't be executed in SDRAM. Other
    use cases include some PM related code.
    
    Acked-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    Acked-by: Andres Salomon <dilinger@queued.net>
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/io.h
+++ arch/arm/include/asm/io.h
@@ -80,6 +80,7 @@ extern void __iomem *__arm_ioremap_caller(unsigned long, size_t, unsigned int,
 
 extern void __iomem *__arm_ioremap_pfn(unsigned long, unsigned long, size_t, unsigned int);
 extern void __iomem *__arm_ioremap(unsigned long, size_t, unsigned int);
+extern void __iomem *__arm_ioremap_exec(unsigned long, size_t, bool cached);
 extern void __iounmap(volatile void __iomem *addr);
 
 /*
