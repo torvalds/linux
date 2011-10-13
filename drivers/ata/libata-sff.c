418fae2751b0d1a362ad9e0b45d446dc92e9157f
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2011-10-13 15:04:43 +0200

    libata: make ata_sff_data_xfer_noirq() work with 32-bit PIO
    Always use ata_sff_data_xfer32() in ata_sff_data_xfer_noirq()
    so the latter can be also used for host controllers supporting
    32-bit PIO operations.
    
    It is a completely safe thing to do because if 32-bit PIO is
    not supported or enabled ata_sff_data_xfer32() will fallback
    to a standard method.
    
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/ata/libata-sff.c
+++ drivers/ata/libata-sff.c
@@ -678,7 +678,7 @@ unsigned int ata_sff_data_xfer_noirq(struct ata_device *dev, unsigned char *buf,
 	unsigned int consumed;
 
 	local_irq_save(flags);
-	consumed = ata_sff_data_xfer(dev, buf, buflen, rw);
+	consumed = ata_sff_data_xfer32(dev, buf, buflen, rw);
 	local_irq_restore(flags);
 
 	return consumed;
