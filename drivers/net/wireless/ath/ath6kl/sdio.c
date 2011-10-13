17380859a8fae40f7420d8fcc4be7a041a370659
Sam Leffler <sleffler@chromium.org>
2011-10-13 13:20:32 +0300

    ath6kl: unbreak suspend
    Add missing {}'s that caused ath6kl_sdio_suspend to always return -EINVAL
    causing suspend to be aborted.
    
    kvalo: I broke this in commit f7325b85e ("ath6kl: add sdio debug messages")
    
    Signed-off-by: Sam Leffler <sleffler@chromium.org>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/sdio.c
+++ drivers/net/wireless/ath/ath6kl/sdio.c
@@ -724,12 +724,13 @@ static int ath6kl_sdio_suspend(struct ath6kl *ar)
 
 	flags = sdio_get_host_pm_caps(func);
 
-	if (!(flags & MMC_PM_KEEP_POWER))
+	if (!(flags & MMC_PM_KEEP_POWER)) {
 		/* as host doesn't support keep power we need to bail out */
 		ath6kl_dbg(ATH6KL_DBG_SDIO,
 			   "func %d doesn't support MMC_PM_KEEP_POWER\n",
 			   func->num);
 		return -EINVAL;
+	}
 
 	ret = sdio_set_host_pm_flags(func, MMC_PM_KEEP_POWER);
 	if (ret) {
