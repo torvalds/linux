cfb470b336f8bb69150697bde1316cf5fdad08fc
Ohad Ben-Cohen <ohad@wizery.com>
2011-10-13 12:53:18 +0100

    ARM: 7130/1: dev_archdata: add private iommu extension
    Add a private iommu pointer to the ARM-specific arch data in the
    device struct, which will be used to attach iommu-specific data
    to devices which require iommu support.
    
    Different iommu implementations (on different platforms) will attach
    different types of data to this pointer, so 'void *' is currently used
    (the downside is reduced typesafety).
    
    Note: ia64, x86 and sparc have this exact iommu extension as well, and
    if others are likely to adopt it too, we might want to consider
    adding this to the device struct itself directly.
    
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Grant Likely <grant.likely@secretlab.ca>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/device.h
+++ arch/arm/include/asm/device.h
@@ -10,6 +10,9 @@ struct dev_archdata {
 #ifdef CONFIG_DMABOUNCE
 	struct dmabounce_device_info *dmabounce;
 #endif
+#ifdef CONFIG_IOMMU_API
+	void *iommu; /* private IOMMU data */
+#endif
 };
 
 struct pdev_archdata {
