06a59ecb921de1d44efcf2cdf543bc689fe2e0d8
Eric Dumazet <eric.dumazet@gmail.com>
2011-10-13 18:24:42 +0000

    tcp: use TCP_INIT_CWND in tcp_fixup_sndbuf()
    Initial cwnd being 10 (TCP_INIT_CWND) instead of 3, change
    tcp_fixup_sndbuf() to get more than 16384 bytes (sysctl_tcp_wmem[1]) in
    initial sk_sndbuf
    
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -267,11 +267,9 @@ static void tcp_fixup_sndbuf(struct sock *sk)
 {
 	int sndmem = SKB_TRUESIZE(tcp_sk(sk)->rx_opt.mss_clamp + MAX_TCP_HEADER);
 
-	if (sk->sk_sndbuf < 3 * sndmem) {
-		sk->sk_sndbuf = 3 * sndmem;
-		if (sk->sk_sndbuf > sysctl_tcp_wmem[2])
-			sk->sk_sndbuf = sysctl_tcp_wmem[2];
-	}
+	sndmem *= TCP_INIT_CWND;
+	if (sk->sk_sndbuf < sndmem)
+		sk->sk_sndbuf = min(sndmem, sysctl_tcp_wmem[2]);
 }
 
 /* 2. Tuning advertised window (window_clamp, rcv_ssthresh)
