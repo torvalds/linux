2cc01eb245d167c4769dd7de0250ec8e8d3bff98
Todd Poynor <toddpoynor@google.com>
2011-10-16 22:40:29 -0700

    cpufreq: interactive governor: default timer 10ms, maxspeed load 95%
    Modify default timer from 30ms to 10ms, sampling 2 jiffies after
    idle exit on ARM as in Honeycomb.
    
    Modify default go_maxspeed_load from 85% loaded to 95% loaded, for
    use in phones where power savings is more important (tablets may be
    best served overriding this).
    
    Change-Id: I3361a6279979bfae1df5262666a2e30ea7a44328
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -59,7 +59,7 @@ static spinlock_t down_cpumask_lock;
 static struct mutex set_speed_lock;
 
 /* Go to max speed when CPU load at or above this value. */
-#define DEFAULT_GO_MAXSPEED_LOAD 85
+#define DEFAULT_GO_MAXSPEED_LOAD 95
 static unsigned long go_maxspeed_load;
 
 /*
@@ -71,7 +71,7 @@ static unsigned long min_sample_time;
 /*
  * The sample rate of the timer used to increase frequency
  */
-#define DEFAULT_TIMER_RATE 30000;
+#define DEFAULT_TIMER_RATE 10000;
 static unsigned long timer_rate;
 
 static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
