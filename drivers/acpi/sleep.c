d11c78e97e1d46a93eb468794da82a090143a72e
Dave Jones <davej@redhat.com>
2011-10-19 23:15:11 +0200

    ACPI / PM: Add Sony VGN-FW21E to nonvs blacklist.
    As noted by a user in https://bugzilla.redhat.com/show_bug.cgi?id=641789
    The Sony VGN-FW21E also needs the nonvs by default workaround added.
    
    Signed-off-by: Dave Jones <davej@redhat.com>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- drivers/acpi/sleep.c
+++ drivers/acpi/sleep.c
@@ -390,6 +390,14 @@ static struct dmi_system_id __initdata acpisleep_dmi_table[] = {
 	},
 	{
 	.callback = init_nvs_nosave,
+	.ident = "Sony Vaio VGN-FW21E",
+	.matches = {
+		DMI_MATCH(DMI_SYS_VENDOR, "Sony Corporation"),
+		DMI_MATCH(DMI_PRODUCT_NAME, "VGN-FW21E"),
+		},
+	},
+	{
+	.callback = init_nvs_nosave,
 	.ident = "Sony Vaio VGN-SR11M",
 	.matches = {
 		DMI_MATCH(DMI_SYS_VENDOR, "Sony Corporation"),
