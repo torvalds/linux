c54367f9d6be8e19c6e98ae7038438f0377ee138
Steven Rostedt <srostedt@redhat.com>
2011-10-20 09:56:41 -0400

    ktest: Do not opencode reboot in grub setting
    When setting the next kernel to boot to with grub, do not opencode
    the reboot operation.  The normal reboot operation can be modified by
    config options (namely POWERCYCLE_AFTER_REBOOT). This needs to affect
    all reboots. Remove the opencoded reboot to make sure that any changes
    to the reboot code also affect all reboots.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- tools/testing/ktest/ktest.pl
+++ tools/testing/ktest/ktest.pl
@@ -1150,7 +1150,8 @@ sub wait_for_input
 
 sub reboot_to {
     if ($reboot_type eq "grub") {
-	run_ssh "'(echo \"savedefault --default=$grub_number --once\" | grub --batch && reboot)'";
+	run_ssh "'(echo \"savedefault --default=$grub_number --once\" | grub --batch)'";
+	reboot;
 	return;
     }
 
