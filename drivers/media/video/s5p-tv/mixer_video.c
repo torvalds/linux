c1bf9c654a752b68be1b4408b33443a17515a671
Marek Szyprowski <m.szyprowski@samsung.com>
2011-10-21 04:56:12 -0300

    [media] v4l: s5p-tv: fix plane size calculation
    Fix plane size calculation.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/s5p-tv/mixer_video.c
+++ drivers/media/video/s5p-tv/mixer_video.c
@@ -853,7 +853,7 @@ static int queue_setup(struct vb2_queue *vq, const struct v4l2_format *pfmt,
 	*nplanes = fmt->num_subframes;
 	for (i = 0; i < fmt->num_subframes; ++i) {
 		alloc_ctxs[i] = layer->mdev->alloc_ctx;
-		sizes[i] = PAGE_ALIGN(planes[i].sizeimage);
+		sizes[i] = planes[i].sizeimage;
 		mxr_dbg(mdev, "size[%d] = %08lx\n", i, sizes[i]);
 	}
 
