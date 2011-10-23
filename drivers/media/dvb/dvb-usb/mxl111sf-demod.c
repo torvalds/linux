539b469518b45c0b5915bec3e258e21e03667084
Michael Krufky <mkrufky@kernellabs.com>
2011-10-23 10:46:13 +0100

    [media] mxl111sf: update demod_ops.info.name to "MaxLinear MxL111SF DVB-T demodulator"
    Signed-off-by: Michael Krufky <mkrufky@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/dvb-usb/mxl111sf-demod.c
+++ drivers/media/dvb/dvb-usb/mxl111sf-demod.c
@@ -553,11 +553,11 @@ static void mxl111sf_demod_release(struct dvb_frontend *fe)
 static struct dvb_frontend_ops mxl111sf_demod_ops = {
 
 	.info = {
-		.name                   = "MxL111SF DVB-T",
-		.type                   = FE_OFDM,
-		.frequency_min          = 177000000,
-		.frequency_max          = 858000000,
-		.frequency_stepsize     = 166666,
+		.name               = "MaxLinear MxL111SF DVB-T demodulator",
+		.type               = FE_OFDM,
+		.frequency_min      = 177000000,
+		.frequency_max      = 858000000,
+		.frequency_stepsize = 166666,
 		.caps = FE_CAN_FEC_1_2 | FE_CAN_FEC_2_3 | FE_CAN_FEC_3_4 |
 			FE_CAN_FEC_5_6 | FE_CAN_FEC_7_8 | FE_CAN_FEC_AUTO |
 			FE_CAN_QPSK | FE_CAN_QAM_16 | FE_CAN_QAM_64 |
