909a4cf1ffe4b875c87abf38239a9bfd25167e0c
Andreas Dilger <adilger@whamcloud.com>
2011-10-26 03:22:31 -0400

    ext4: avoid setting directory i_nlink to zero
    If a directory with more than EXT4_LINK_MAX subdirectories, the nlink
    count is set to 1.  Subsequently, if any subdirectories are deleted,
    ext4_dec_count() decrements the i_nlink count, which may go to 0
    temporarily before being incremented back to 1.
    
    While this is done under i_mutex, which prevents races for directory
    and inode operations that check i_nlink, the temporary i_nlink == 0
    case is exposed to userspace via stat() and similar calls that do not
    hold i_mutex.
    
    Instead, change the code to not decrement i_nlink count for any
    directories that do not already have i_nlink larger than 2.
    
    Reported-by: Cliff White <cliffw@whamcloud.com>
    Reviewed-by: Johann Lombardi <johann@whamcloud.com>
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -1706,9 +1706,8 @@ static void ext4_inc_count(handle_t *handle, struct inode *inode)
  */
 static void ext4_dec_count(handle_t *handle, struct inode *inode)
 {
-	drop_nlink(inode);
-	if (S_ISDIR(inode->i_mode) && inode->i_nlink == 0)
-		inc_nlink(inode);
+	if (!S_ISDIR(inode->i_mode) || inode->i_nlink > 2)
+		drop_nlink(inode);
 }
 
 
