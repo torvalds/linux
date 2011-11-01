d43a87e68e9e71d2987a29cc239acec4e8f410c9
Kyungmin Park <kyungmin.park@samsung.com>
2011-10-31 17:09:08 -0700

    mm: compaction: make compact_zone_order() static
    There's no compact_zone_order() user outside file scope, so make it static.
    
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Reviewed-by: Minchan Kim <minchan.kim@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/compaction.c
+++ mm/compaction.c
@@ -582,7 +582,7 @@ out:
 	return ret;
 }
 
-unsigned long compact_zone_order(struct zone *zone,
+static unsigned long compact_zone_order(struct zone *zone,
 				 int order, gfp_t gfp_mask,
 				 bool sync)
 {
