2967b0ad3302d8627c0d99bfed4b8f167e21fdee
Wolfram Sang <w.sang@pengutronix.de>
2011-10-31 17:11:44 -0700

    video/backlight: remove obsolete cleanup for clientdata
    A few new i2c-drivers came into the kernel which clear the
    clientdata-pointer on exit or error.  This is obsolete meanwhile, the core
    will do it.
    
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>
    Cc: Richard Purdie <rpurdie@rpsys.net>
    Cc: Paul Mundt <lethal@linux-sh.org>
    Acked-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/backlight/adp8870_bl.c
+++ drivers/video/backlight/adp8870_bl.c
@@ -931,7 +931,6 @@ out:
 out1:
 	backlight_device_unregister(bl);
 out2:
-	i2c_set_clientdata(client, NULL);
 	kfree(data);
 
 	return ret;
@@ -951,7 +950,6 @@ static int __devexit adp8870_remove(struct i2c_client *client)
 			&adp8870_bl_attr_group);
 
 	backlight_device_unregister(data->bl);
-	i2c_set_clientdata(client, NULL);
 	kfree(data);
 
 	return 0;
