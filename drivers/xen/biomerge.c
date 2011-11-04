0b934ccd707ff33a87f15a35a9916d1d8e85d30e
Matthew Wilcox <matthew.r.wilcox@intel.com>
2011-11-04 15:41:27 -0400

    Xen: Export xen_biovec_phys_mergeable
    When Xen is enabled, using BIOVEC_PHYS_MERGEABLE in a module
    causes xen_biovec_phys_mergeable to be referenced, so it needs
    to be exported.
    
    Acked-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Matthew Wilcox <matthew.r.wilcox@intel.com>

--- drivers/xen/biomerge.c
+++ drivers/xen/biomerge.c
@@ -11,3 +11,4 @@ bool xen_biovec_phys_mergeable(const struct bio_vec *vec1,
 	return __BIOVEC_PHYS_MERGEABLE(vec1, vec2) &&
 		((mfn1 == mfn2) || ((mfn1+1) == mfn2));
 }
+EXPORT_SYMBOL(xen_biovec_phys_mergeable);
