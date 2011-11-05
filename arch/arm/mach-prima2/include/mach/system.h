125c4033f0187d7264f08d7cf1474252df4f4c17
Russell King <rmk+kernel@arm.linux.org.uk>
2011-11-05 10:23:27 +0000

    ARM: restart: prima2: use new restart hook
    Hook these platforms restart code into the new restart hook rather
    than using arch_reset().
    
    Reviewed-by: Barry Song <Baohua.Song@csr.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-prima2/include/mach/system.h
+++ arch/arm/mach-prima2/include/mach/system.h
@@ -9,13 +9,6 @@
 #ifndef __MACH_SYSTEM_H__
 #define __MACH_SYSTEM_H__
 
-#include <linux/bitops.h>
-#include <mach/hardware.h>
-
-#define SIRFSOC_SYS_RST_BIT  BIT(31)
-
-extern void __iomem *sirfsoc_rstc_base;
-
 static inline void arch_idle(void)
 {
 	cpu_do_idle();
@@ -23,7 +16,6 @@ static inline void arch_idle(void)
 
 static inline void arch_reset(char mode, const char *cmd)
 {
-	writel(SIRFSOC_SYS_RST_BIT, sirfsoc_rstc_base);
 }
 
 #endif
