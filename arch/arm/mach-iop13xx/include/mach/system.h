00aa78eea20645a751db45ae4a46dae15a4576cf
Russell King <rmk+kernel@arm.linux.org.uk>
2011-11-06 10:49:50 +0000

    ARM: restart: iop13xx: use new restart hook
    Hook these platforms restart code into the new restart hook rather
    than using arch_reset().
    
    Acked-by: Lennert Buytenhek <buytenh@wantstofly.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-iop13xx/include/mach/system.h
+++ arch/arm/mach-iop13xx/include/mach/system.h
@@ -7,7 +7,6 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
-#include <mach/iop13xx.h>
 static inline void arch_idle(void)
 {
 	cpu_do_idle();
@@ -15,13 +14,4 @@ static inline void arch_idle(void)
 
 static inline void arch_reset(char mode, const char *cmd)
 {
-	/*
-	 * Reset the internal bus (warning both cores are reset)
-	 */
-	write_wdtcr(IOP_WDTCR_EN_ARM);
-	write_wdtcr(IOP_WDTCR_EN);
-	write_wdtsr(IOP13XX_WDTSR_WRITE_EN | IOP13XX_WDTCR_IB_RESET);
-	write_wdtcr(0x1000);
-
-	for(;;);
 }
