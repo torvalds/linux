774c8018d2566207c92f47b6421dad7d88bbfd49
Nikolaus Voss <n.voss@weinmann.de>
2011-11-08 12:11:03 +0100

    arm: at91: G45 TWI: remove open drain setting for twi function gpios
    The G45 datasheets explicitly states that setting the open drain property
    on peripheral function gpios is not allowed. (How about other A91 chips?)
    
    Signed-off-by: Nikolaus Voss <n.voss@weinmann.de>
    Tested-by: Ludovic Desroches <ludovic.desroches@atmel.com>
    Reviewed-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>

--- arch/arm/mach-at91/at91sam9g45_devices.c
+++ arch/arm/mach-at91/at91sam9g45_devices.c
@@ -686,18 +686,12 @@ void __init at91_add_device_i2c(short i2c_id, struct i2c_board_info *devices, in
 	/* pins used for TWI interface */
 	if (i2c_id == 0) {
 		at91_set_A_periph(AT91_PIN_PA20, 0);		/* TWD */
-		at91_set_multi_drive(AT91_PIN_PA20, 1);
-
 		at91_set_A_periph(AT91_PIN_PA21, 0);		/* TWCK */
-		at91_set_multi_drive(AT91_PIN_PA21, 1);
 
 		platform_device_register(&at91sam9g45_twi0_device);
 	} else {
 		at91_set_A_periph(AT91_PIN_PB10, 0);		/* TWD */
-		at91_set_multi_drive(AT91_PIN_PB10, 1);
-
 		at91_set_A_periph(AT91_PIN_PB11, 0);		/* TWCK */
-		at91_set_multi_drive(AT91_PIN_PB11, 1);
 
 		platform_device_register(&at91sam9g45_twi1_device);
 	}
