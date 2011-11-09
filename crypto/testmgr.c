937c30d7f560210b0163035edd42b2aef78fed9e
Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
2011-11-09 16:26:25 +0200

    crypto: serpent - add 8-way parallel x86_64/SSE2 assembler implementation
    Patch adds x86_64/SSE2 assembler implementation of serpent cipher. Assembler
    functions crypt data in eigth block chunks (two 4 block chunk SSE2 operations
    in parallel to improve performance on out-of-order CPUs). Glue code is based
    on one from AES-NI implementation, so requests from irq context are redirected
    to cryptd.
    
    v2:
     - add missing include of linux/module.h
       (appearently crypto.h used to include module.h, which changed for 3.2 by
        commit 7c926402a7e8c9b279968fd94efec8700ba3859e)
    
    Patch has been tested with tcrypt and automated filesystem tests.
    
    Tcrypt benchmarks results (serpent-sse2/serpent_generic speed ratios):
    
    AMD Phenom II 1055T (fam:16, model:10):
    
    size    ecb-enc ecb-dec cbc-enc cbc-dec ctr-enc ctr-dec
    16B     1.03x   1.01x   1.03x   1.05x   1.00x   0.99x
    64B     1.00x   1.01x   1.02x   1.04x   1.02x   1.01x
    256B    2.34x   2.41x   0.99x   2.43x   2.39x   2.40x
    1024B   2.51x   2.57x   1.00x   2.59x   2.56x   2.56x
    8192B   2.50x   2.54x   1.00x   2.55x   2.57x   2.57x
    
    Intel Celeron T1600 (fam:6, model:15, step:13):
    
    size    ecb-enc ecb-dec cbc-enc cbc-dec ctr-enc ctr-dec
    16B     0.97x   0.97x   1.01x   1.01x   1.01x   1.02x
    64B     1.00x   1.00x   1.00x   1.02x   1.01x   1.01x
    256B    3.41x   3.35x   1.00x   3.39x   3.42x   3.44x
    1024B   3.75x   3.72x   0.99x   3.74x   3.75x   3.75x
    8192B   3.70x   3.68x   0.99x   3.68x   3.69x   3.69x
    
    Full output:
     http://koti.mbnet.fi/axh/kernel/crypto/phenom-ii-1055t/serpent-generic.txt
     http://koti.mbnet.fi/axh/kernel/crypto/phenom-ii-1055t/serpent-sse2.txt
     http://koti.mbnet.fi/axh/kernel/crypto/celeron-t1600/serpent-generic.txt
     http://koti.mbnet.fi/axh/kernel/crypto/celeron-t1600/serpent-sse2.txt
    
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -1534,6 +1534,21 @@ static int alg_test_null(const struct alg_test_desc *desc,
 /* Please keep this list sorted by algorithm name. */
 static const struct alg_test_desc alg_test_descs[] = {
 	{
+		.alg = "__cbc-serpent-sse2",
+		.test = alg_test_null,
+		.suite = {
+			.cipher = {
+				.enc = {
+					.vecs = NULL,
+					.count = 0
+				},
+				.dec = {
+					.vecs = NULL,
+					.count = 0
+				}
+			}
+		}
+	}, {
 		.alg = "__driver-cbc-aes-aesni",
 		.test = alg_test_null,
 		.suite = {
@@ -1549,7 +1564,37 @@ static const struct alg_test_desc alg_test_descs[] = {
 			}
 		}
 	}, {
+		.alg = "__driver-cbc-serpent-sse2",
+		.test = alg_test_null,
+		.suite = {
+			.cipher = {
+				.enc = {
+					.vecs = NULL,
+					.count = 0
+				},
+				.dec = {
+					.vecs = NULL,
+					.count = 0
+				}
+			}
+		}
+	}, {
 		.alg = "__driver-ecb-aes-aesni",
+		.test = alg_test_null,
+		.suite = {
+			.cipher = {
+				.enc = {
+					.vecs = NULL,
+					.count = 0
+				},
+				.dec = {
+					.vecs = NULL,
+					.count = 0
+				}
+			}
+		}
+	}, {
+		.alg = "__driver-ecb-serpent-sse2",
 		.test = alg_test_null,
 		.suite = {
 			.cipher = {
@@ -1732,6 +1777,21 @@ static const struct alg_test_desc alg_test_descs[] = {
 		}
 	}, {
 		.alg = "cryptd(__driver-ecb-aes-aesni)",
+		.test = alg_test_null,
+		.suite = {
+			.cipher = {
+				.enc = {
+					.vecs = NULL,
+					.count = 0
+				},
+				.dec = {
+					.vecs = NULL,
+					.count = 0
+				}
+			}
+		}
+	}, {
+		.alg = "cryptd(__driver-ecb-serpent-sse2)",
 		.test = alg_test_null,
 		.suite = {
 			.cipher = {
