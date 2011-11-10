ef5e0d8237287db3a12d84f08fb2483d7a30a943
Andrey Vagin <avagin@openvz.org>
2011-11-10 05:48:03 +0000

    bridge: Fix potential deadlock on br->multicast_lock
    multicast_lock is taken in softirq context, so we should use
    spin_lock_bh() in userspace.
    
    call-chain in softirq context:
    run_timer_softirq()
    	br_multicast_query_expired()
    
    call-chain in userspace:
    sysfs_write_file()
    	store_multicast_snooping()
    		br_multicast_toggle()
    
    Signed-off-by: Andrew Vagin <avagin@openvz.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bridge/br_multicast.c
+++ net/bridge/br_multicast.c
@@ -1770,7 +1770,7 @@ int br_multicast_toggle(struct net_bridge *br, unsigned long val)
 	int err = 0;
 	struct net_bridge_mdb_htable *mdb;
 
-	spin_lock(&br->multicast_lock);
+	spin_lock_bh(&br->multicast_lock);
 	if (br->multicast_disabled == !val)
 		goto unlock;
 
@@ -1806,7 +1806,7 @@ rollback:
 	}
 
 unlock:
-	spin_unlock(&br->multicast_lock);
+	spin_unlock_bh(&br->multicast_lock);
 
 	return err;
 }
