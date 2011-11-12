f1c289115a12c75d5444c1589a2e1cb00a766bd4
Aaro Koskinen <aaro.koskinen@iki.fi>
2011-11-13 00:09:23 +0200

    staging: xgifb: delete XGI_GetSenseStatus()
    Delete unused function XGI_GetSenseStatus().
    
    Static functions XGINew_Is301B(), XGINew_Sense(), XGINew_GetPanelID(),
    XGINew_BridgeIsEnable() and XGINew_SenseHiTV() can be deleted as well,
    since they are not used anymore.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/xgifb/vb_ext.h
+++ drivers/staging/xgifb/vb_ext.h
@@ -1,8 +1,6 @@
 #ifndef _VBEXT_
 #define _VBEXT_
 
-extern void XGI_GetSenseStatus(struct xgi_hw_device_info *HwDeviceExtension,
-			       struct vb_device_info *pVBInfo);
 extern unsigned short XGINew_SenseLCD(struct xgi_hw_device_info *,
 				      struct vb_device_info *pVBInfo);
 
