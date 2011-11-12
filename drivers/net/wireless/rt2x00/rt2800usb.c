498950663547d6c020edd12d7c2fef8cad4872b8
Gertjan van Wingerde <gwingerde@gmail.com>
2011-11-12 19:10:43 +0100

    rt2x00: Add USB device ID of Buffalo WLI-UC-GNHP.
    commit c18b7806e4f3373b2f296a65fb19251a3b49a532 upstream.
    
    This is reported to be an RT3070 based device.
    
    Reported-by: Teika Kazura <teika@lavabit.com>
    Signed-off-by: Gertjan van Wingerde <gwingerde@gmail.com>
    Acked-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    Cc: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/wireless/rt2x00/rt2800usb.c
+++ drivers/net/wireless/rt2x00/rt2800usb.c
@@ -824,6 +824,7 @@ static struct usb_device_id rt2800usb_device_table[] = {
 	{ USB_DEVICE(0x050d, 0x935b) },
 	/* Buffalo */
 	{ USB_DEVICE(0x0411, 0x00e8) },
+	{ USB_DEVICE(0x0411, 0x0158) },
 	{ USB_DEVICE(0x0411, 0x016f) },
 	{ USB_DEVICE(0x0411, 0x01a2) },
 	/* Corega */
