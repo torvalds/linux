bb05e4ba452ada7966fbced4e829aa029f546445
Matt Fleming <matt.fleming@intel.com>
2011-11-15 12:57:03 +0000

    efi.h: Add allocation types for boottime->allocate_pages()
    Add the allocation types detailed in section 6.2 - "AllocatePages()"
    of the UEFI 2.3 specification. These definitions will be used by the
    x86 EFI boot stub which needs to allocate memory during boot.
    
    Cc: Matthew Garrett <mjg@redhat.com>
    Signed-off-by: Matt Fleming <matt.fleming@intel.com>
    Link: http://lkml.kernel.org/r/1318848017-12301-1-git-send-email-matt@console-pimps.org
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- include/linux/efi.h
+++ include/linux/efi.h
@@ -109,6 +109,14 @@ typedef struct {
 	u32 imagesize;
 } efi_capsule_header_t;
 
+/*
+ * Allocation types for calls to boottime->allocate_pages.
+ */
+#define EFI_ALLOCATE_ANY_PAGES		0
+#define EFI_ALLOCATE_MAX_ADDRESS	1
+#define EFI_ALLOCATE_ADDRESS		2
+#define EFI_MAX_ALLOCATE_TYPE		3
+
 typedef int (*efi_freemem_callback_t) (u64 start, u64 end, void *arg);
 
 /*
