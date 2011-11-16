b4f575ecf63b4274d5ecec5fb082363644a5b9bd
huangxin <huangxin@LServer.(none)>
2011-11-16 11:10:09 +0800

    [sun4i]:rtc,ace.use the code style to modify the code

--- drivers/rtc/rtc-sun4i.c
+++ drivers/rtc/rtc-sun4i.c
@@ -1,3 +1,17 @@
+/*
+ * drivers\rtc\rtc-sun4i.c
+ * (C) Copyright 2007-2011
+ * Allwinner Technology Co., Ltd. <www.allwinnertech.com>
+ * huangxin <huangxin@allwinnertech.com>
+ *
+ * some simple description for this code
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ */
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/string.h>
