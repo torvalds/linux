fcac4fb00eaefea375db9745464cc182389a1751
Felix Fietkau <nbd@openwrt.org>
2011-11-16 13:34:55 +0100

    mac80211: call ieee80211_recalc_idle() after sending packets
    Some drivers (e.g. ath9k) assume that it's safe to go into low-power mode
    immediately after the idle state changes. To support that, mac80211 even
    calls drv_flush() before that happens.
    In some instances, mac80211 sent a packet right after recalculating the
    idle state, this patch fixes that.
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/offchannel.c
+++ net/mac80211/offchannel.c
@@ -211,8 +211,6 @@ static void ieee80211_hw_roc_start(struct work_struct *work)
 		return;
 	}
 
-	ieee80211_recalc_idle(local);
-
 	if (local->hw_roc_skb) {
 		sdata = IEEE80211_DEV_TO_SUB_IF(local->hw_roc_dev);
 		ieee80211_tx_skb(sdata, local->hw_roc_skb);
@@ -225,6 +223,8 @@ static void ieee80211_hw_roc_start(struct work_struct *work)
 					  local->hw_roc_duration,
 					  GFP_KERNEL);
 	}
+
+	ieee80211_recalc_idle(local);
 
 	mutex_unlock(&local->mtx);
 }
