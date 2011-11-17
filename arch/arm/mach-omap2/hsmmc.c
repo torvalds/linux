a15164f13fb3f0f70849c2a099ff0af7b2924ba1
Thomas Weber <weber@corscience.de>
2011-11-17 22:39:40 +0100

    ARM: OMAP: hsmmc: Add support for non-OMAP pins
    The Devkit8000 uses a TWL4030 pin for card detection.
    Thats why the error:
    _omap_mux_init_gpio: Could not set gpio192
    occurs.
    
    This patch checks that the pin is on OMAP before
    calling omap_mux_init_gpio.
    
    Signed-off-by: Thomas Weber <weber@corscience.de>
    [tony@atomide.com: updated comments]
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/hsmmc.c
+++ arch/arm/mach-omap2/hsmmc.c
@@ -214,10 +214,12 @@ static int nop_mmc_set_power(struct device *dev, int slot, int power_on,
 static inline void omap_hsmmc_mux(struct omap_mmc_platform_data *mmc_controller,
 			int controller_nr)
 {
-	if (gpio_is_valid(mmc_controller->slots[0].switch_pin))
+	if (gpio_is_valid(mmc_controller->slots[0].switch_pin) &&
+		(mmc_controller->slots[0].switch_pin < OMAP_MAX_GPIO_LINES))
 		omap_mux_init_gpio(mmc_controller->slots[0].switch_pin,
 					OMAP_PIN_INPUT_PULLUP);
-	if (gpio_is_valid(mmc_controller->slots[0].gpio_wp))
+	if (gpio_is_valid(mmc_controller->slots[0].gpio_wp) &&
+		(mmc_controller->slots[0].gpio_wp < OMAP_MAX_GPIO_LINES))
 		omap_mux_init_gpio(mmc_controller->slots[0].gpio_wp,
 					OMAP_PIN_INPUT_PULLUP);
 	if (cpu_is_omap34xx()) {
