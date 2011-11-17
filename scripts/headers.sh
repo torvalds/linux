052ad274964e613a12bf8ce7c5631f70d0bc82cc
H. Peter Anvin <hpa@linux.intel.com>
2011-11-17 13:17:35 -0800

    kbuild: Add support for an "archheaders" target
    Add support for an "archheaders" target.  This target can generate
    files that need to be installed for user space by "make
    headers_install" or "make headers_install_all".
    
    In order to support "make headers_install_all", it must be able to run
    without the tree having to be configured first.
    
    Cc: David Woodhouse <dwmw2@infradead.org>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Cc: Michal Marek <mmarek@suse.cz>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- scripts/headers.sh
+++ scripts/headers.sh
@@ -7,6 +7,7 @@ set -e
 do_command()
 {
 	if [ -f ${srctree}/arch/$2/include/asm/Kbuild ]; then
+		make ARCH=$2 KBUILD_HEADERS=$1 archheaders
 		make ARCH=$2 KBUILD_HEADERS=$1 headers_$1
 	else
 		printf "Ignoring arch: %s\n" ${arch}
