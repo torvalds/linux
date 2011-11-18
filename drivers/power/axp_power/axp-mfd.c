f26984df94be2d697716f87237de67460dd5abc2
kevin <kevin@allwinnertech.com>
2011-11-18 14:59:11 +0800

    update axp driver, support irq wakeup

--- drivers/power/axp_power/axp-mfd.c
+++ drivers/power/axp_power/axp-mfd.c
@@ -243,13 +243,18 @@ static void axp_power_off(void)
     mdelay(20);
 	axp_read(&axp->dev, POWER20_STATUS, &val);
 	if(val & 0xF0){
-        printk("[axp] set flag!\n");
-	    axp_write(&axp->dev, POWER20_DATA_BUFFERC, 0x0f);
-        mdelay(20);
-		printk("[axp] reboot!\n");
-		arch_reset(0,NULL);
-		printk("[axp] warning!!! arch can't ,reboot, maybe some error happend!\n");
+	    axp_read(&axp->dev, POWER20_MODE_CHGSTATUS, &val);
+	    if(val & 0x20){
+            printk("[axp] set flag!\n");
+	        axp_write(&axp->dev, POWER20_DATA_BUFFERC, 0x0f);
+            mdelay(20);
+		    printk("[axp] reboot!\n");
+		    arch_reset(0,NULL);
+		    printk("[axp] warning!!! arch can't ,reboot, maybe some error happend!\n");
+	    }
 	}
+    axp_write(&axp->dev, POWER20_DATA_BUFFERC, 0x00);
+    mdelay(20);
 	axp_set_bits(&axp->dev, POWER20_OFF_CTL, 0x80);
     mdelay(20);
     printk("[axp] warning!!! axp can't power-off, maybe some error happend!\n");
