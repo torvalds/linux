10a340e6415d938939724743a4da71fd63f5a95c
Chad Dupuis <chad.dupuis@qlogic.com>
2011-11-18 09:02:16 -0800

    [SCSI] qla2xxx: Display IPE error message for ISP82xx.
    [jejb: fixup checkpatch error]
    Signed-off-by: Chad Dupuis <chad.dupuis@qlogic.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/qla2xxx/qla_nx.c
+++ drivers/scsi/qla2xxx/qla_nx.c
@@ -3858,6 +3858,11 @@ void qla82xx_watchdog(scsi_qla_host_t *vha)
 					    QLA82XX_CRB_PEG_NET_3 + 0x3c),
 				    qla82xx_rd_32(ha,
 					    QLA82XX_CRB_PEG_NET_4 + 0x3c));
+				if (LSW(MSB(halt_status)) == 0x67)
+					ql_log(ql_log_warn, vha, 0xb052,
+					    "Firmware aborted with "
+					    "error code 0x00006700. Device is "
+					    "being reset.\n");
 				if (halt_status & HALT_STATUS_UNRECOVERABLE) {
 					set_bit(ISP_UNRECOVERABLE,
 					    &vha->dpc_flags);
