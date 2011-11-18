6c1c3d83ef8497b3e87651a1976813739bf5b67e
Tom Cubie <tangliang@allwinnertech.com>
2011-11-18 17:12:37 +0800

    build: add crane build and default config

---
+++ scripts/build_sun4i_crane.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+set -e
+#########################################################################
+#
+#          Simple build scripts to build krenel(with rootfs)  -- by Benn
+#
+#########################################################################
+
+
+#Setup common variables
+export ARCH=arm
+export CROSS_COMPILE=arm-none-linux-gnueabi-
+export AS=${CROSS_COMPILE}as
+export LD=${CROSS_COMPILE}ld
+export CC=${CROSS_COMPILE}gcc
+export AR=${CROSS_COMPILE}ar
+export NM=${CROSS_COMPILE}nm
+export STRIP=${CROSS_COMPILE}strip
+export OBJCOPY=${CROSS_COMPILE}objcopy
+export OBJDUMP=${CROSS_COMPILE}objdump
+
+KERNEL_VERSION="3.0"
+LICHEE_KDIR=`pwd`
+LICHEE_MOD_DIR==${LICHEE_KDIR}/output/lib/modules/${KERNEL_VERSION}
+
+CONFIG_CHIP_ID=1123
+
+update_kern_ver()
+{
+	if [ -r include/generated/utsrelease.h ]; then
+		KERNEL_VERSION=`cat include/generated/utsrelease.h |awk -F\" '{print $2}'`
+	fi
+	LICHEE_MOD_DIR=${LICHEE_KDIR}/output/lib/modules/${KERNEL_VERSION}
+}
+
+show_help()
+{
+	printf "
+Build script for Lichee platform
+
+Invalid Options:
+
+	help         - show this help
+	kernel       - build kernel
+	modules      - build kernel module in modules dir
+	clean        - clean kernel and modules
+
+"
+}
+
+build_standby()
+{
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} KDIR=${LICHEE_KDIR} \
+		-C ${LICHEE_KDIR}/arch/arm/mach-sun4i/pm/standby all
+}
+
+build_kernel()
+{
+	if [ ! -e .config ]; then
+		echo -e "\n\t\tUsing default config... ...!\n"
+		cp arch/arm/configs/sun4i_crane_defconfig .config
+	fi
+
+	build_standby
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} -j8 uImage modules
+
+	update_kern_ver
+
+	if [ -d output ]; then
+		rm -rf output
+	fi
+	mkdir -p $LICHEE_MOD_DIR
+
+	${OBJCOPY} -R .note.gnu.build-id -S -O binary vmlinux output/bImage
+	cp -vf arch/arm/boot/[zu]Image output/
+	cp .config output/
+
+
+	for file in $(find drivers sound crypto block fs security net -name "*.ko"); do
+		cp $file ${LICHEE_MOD_DIR}
+	done
+	cp -f Module.symvers ${LICHEE_MOD_DIR}
+	#cp -f modules.* ${LICHEE_MOD_DIR}
+}
+
+build_modules()
+{
+	echo "Building modules"
+
+	if [ ! -f include/generated/utsrelease.h ]; then
+		printf "Please build kernel first!\n"
+		exit 1
+	fi
+
+	update_kern_ver
+
+	make -C modules/example LICHEE_MOD_DIR=${LICHEE_MOD_DIR} LICHEE_KDIR=${LICHEE_KDIR} \
+		CONFIG_CHIP_ID=${CONFIG_CHIP_ID} install
+
+	make -C modules/mali LICHEE_MOD_DIR=${LICHEE_MOD_DIR} LICHEE_KDIR=${LICHEE_KDIR} \
+		CONFIG_CHIP_ID=${CONFIG_CHIP_ID} install
+}
+
+clean_kernel()
+{
+	make clean
+	rm -rf output/*
+}
+
+clean_modules()
+{
+	echo "Cleaning modules"
+	make -C modules/example LICHEE_MOD_DIR=${LICHEE_MOD_DIR} LICHEE_KDIR=${LICHEE_KDIR} clean
+	make -C modules/mali LICHEE_MOD_DIR=${LICHEE_MOD_DIR} LICHEE_KDIR=${LICHEE_KDIR} clean
+}
+
+#####################################################################
+#
+#                      Main Runtine
+#
+#####################################################################
+
+LICHEE_ROOT=`(cd ${LICHEE_KDIR}/..; pwd)`
+export PATH=${LICHEE_ROOT}/buildroot/output/external-toolchain/bin:$PATH
+
+case "$1" in
+kernel)
+	build_kernel
+	;;
+modules)
+	build_modules
+	;;
+clean)
+	clean_kernel
+	clean_modules
+	;;
+all)
+	build_kernel
+	build_modules
+	;;
+*)
+	show_help
+	;;
+esac
+
