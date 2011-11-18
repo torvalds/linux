68380876d674e8e0a810128971772e38201491ba
Felipe Balbi <balbi@ti.com>
2011-11-18 21:31:14 +0200

    usb: dwc3: omap: move to module_platform_driver
    the new module_platform_driver macro is a helper
    for modules which just register and unregister the
    platform_driver. It allows us to delete a few
    duplicated lines.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/dwc3/dwc3-omap.c
+++ drivers/usb/dwc3/dwc3-omap.c
@@ -399,14 +399,4 @@ MODULE_AUTHOR("Felipe Balbi <balbi@ti.com>");
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("DesignWare USB3 OMAP Glue Layer");
 
-static int __devinit dwc3_omap_init(void)
-{
-	return platform_driver_register(&dwc3_omap_driver);
-}
-module_init(dwc3_omap_init);
-
-static void __exit dwc3_omap_exit(void)
-{
-	platform_driver_unregister(&dwc3_omap_driver);
-}
-module_exit(dwc3_omap_exit);
+module_platform_driver(dwc3_omap_driver);
