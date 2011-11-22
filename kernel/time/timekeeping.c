452d440ab295475afb04dee90879f5fb70254659
Arve Hjønnevåg <arve@android.com>
2011-11-22 15:28:27 -0800

    Fix "time: Catch invalid timespec sleep values in __timekeeping_inject_sleeptime" to compile on 3.0
    Change-Id: I1225f279cda04dedbfb7f853f6b58f1032bd6d2b

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -605,7 +605,7 @@ static struct timespec timekeeping_suspend_time;
 static void __timekeeping_inject_sleeptime(struct timespec *delta)
 {
 	if (!timespec_valid(delta)) {
-		printk(KERN_WARN "__timekeeping_inject_sleeptime: Invalid "
+		printk(KERN_WARNING "__timekeeping_inject_sleeptime: Invalid "
 					"sleep delta value!\n");
 		return;
 	}
