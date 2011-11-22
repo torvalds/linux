b712721eb6ca3b226722a956a25eb504b43ad478
Inderpal Singh <inderpal.singh@linaro.org>
2011-11-22 12:15:38 +0530

    ARM: EXYNOS: Fix for suspend/resume hang
    While resuming from suspend, the system hangs due to some spurious
    uart interrupts. This patch temporarily masks uart interrupts during
    reset_port to solve this problem.
    
    Signed-off-by: Inderpal Singh <inderpal.singh@linaro.org>
    Signed-off-by: Tushar Behera <tushar.behera@linaro.org>

--- drivers/tty/serial/samsung.c
+++ drivers/tty/serial/samsung.c
@@ -1030,6 +1030,9 @@ static void s3c24xx_serial_resetport(struct uart_port *port,
 	wr_regl(port, S3C2410_UFCON, cfg->ufcon | S3C2410_UFCON_RESETBOTH);
 	wr_regl(port, S3C2410_UFCON, cfg->ufcon);
 
+	wr_regl(port, S3C64XX_UINTM, 0xf);
+	wr_regl(port, S3C64XX_UINTP, 0xf);
+
 	/* some delay is required after fifo reset */
 	udelay(1);
 }
