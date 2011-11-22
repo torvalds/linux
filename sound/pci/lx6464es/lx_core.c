afd00d7235c1989d06d75cf8ac3d7722fcf2f394
Tim Blechmann <tim@klingt.org>
2011-11-22 11:15:44 +0100

    ALSA: lx6464es - command buffer API cleanup
    the command buffer is only accessed from one file, so we can declare the
    specific functions as static in that file
    
    Signed-off-by: Tim Blechmann <tim@klingt.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/lx6464es/lx_core.c
+++ sound/pci/lx6464es/lx_core.c
@@ -78,7 +78,8 @@ unsigned long lx_dsp_reg_read(struct lx6464es *chip, int port)
 	return ioread32(address);
 }
 
-void lx_dsp_reg_readbuf(struct lx6464es *chip, int port, u32 *data, u32 len)
+static void lx_dsp_reg_readbuf(struct lx6464es *chip, int port, u32 *data,
+			       u32 len)
 {
 	void __iomem *address = lx_dsp_register(chip, port);
 	memcpy_fromio(data, address, len*sizeof(u32));
@@ -91,8 +92,8 @@ void lx_dsp_reg_write(struct lx6464es *chip, int port, unsigned data)
 	iowrite32(data, address);
 }
 
-void lx_dsp_reg_writebuf(struct lx6464es *chip, int port, const u32 *data,
-			 u32 len)
+static void lx_dsp_reg_writebuf(struct lx6464es *chip, int port,
+				const u32 *data, u32 len)
 {
 	void __iomem *address = lx_dsp_register(chip, port);
 	memcpy_toio(address, data, len*sizeof(u32));
