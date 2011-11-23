68597986f7db9abbe380af3db6a34b6c83cdf302
Jonghwan Choi <jhbird.choi@samsung.com>
2011-11-23 13:40:05 +0900

    max8997_charger: Remove duplicate module.h
    module.h was included twice.
    
    Signed-off-by: Jonghwan Choi <jhbird.choi@samsung.com>
    Acked-by: MyungJoo Ham <myungjoo.ham@samsung.com>
    Signed-off-by: Anton Vorontsov <cbouatmailru@gmail.com>

--- drivers/power/max8997_charger.c
+++ drivers/power/max8997_charger.c
@@ -19,7 +19,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <linux/module.h>
 #include <linux/err.h>
 #include <linux/module.h>
 #include <linux/slab.h>
