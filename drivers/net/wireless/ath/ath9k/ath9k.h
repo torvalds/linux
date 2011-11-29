1a68abb0c85b5eb490971b622ff36168c64416a7
Mohammed Shafi Shajakhan <mohammed@qca.qualcomm.com>
2011-11-29 20:06:15 +0530

    ath9k: Fix LED GPIO pin for AR9462
    GPIO pin 4 is assigned AR9462 chipsets LED.
    while GPIO pin 0 worked for obselete AR9462 chipsets though
    they are meant for EEPROM as per Russell
    
    Cc: Senthil Balasubramanian <senthilb@qca.qualcomm.com>
    Signed-off-by: Russell Hu <rhu@qca.qualcomm.com>
    Signed-off-by: Mohammed Shafi Shajakhan <mohammed@qca.qualcomm.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/ath9k.h
+++ drivers/net/wireless/ath/ath9k/ath9k.h
@@ -462,7 +462,7 @@ void ath9k_btcoex_timer_pause(struct ath_softc *sc);
 #define ATH_LED_PIN_9287		8
 #define ATH_LED_PIN_9300		10
 #define ATH_LED_PIN_9485		6
-#define ATH_LED_PIN_9462		0
+#define ATH_LED_PIN_9462		4
 
 #ifdef CONFIG_MAC80211_LEDS
 void ath_init_leds(struct ath_softc *sc);
