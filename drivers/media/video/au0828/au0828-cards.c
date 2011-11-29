23bbba3411715736ad1942f3bdbb18e1bbe6d45f
Michael Krufky <mkrufky@linuxtv.org>
2011-11-29 18:21:41 -0300

    [media] au0828: add missing USB ID 2040:7260
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/au0828/au0828-cards.c
+++ drivers/media/video/au0828/au0828-cards.c
@@ -324,6 +324,8 @@ struct usb_device_id au0828_usb_id_table[] = {
 		.driver_info = AU0828_BOARD_HAUPPAUGE_HVR950Q_MXL },
 	{ USB_DEVICE(0x2040, 0x8200),
 		.driver_info = AU0828_BOARD_HAUPPAUGE_WOODBURY },
+	{ USB_DEVICE(0x2040, 0x7260),
+		.driver_info = AU0828_BOARD_HAUPPAUGE_HVR950Q },
 	{ },
 };
 
