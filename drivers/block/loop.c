dfaf3c036cd46a73f4ef3e4b75c1f647e503d4e1
Lukas Czerner <lczerner@redhat.com>
2011-12-02 14:47:03 +0100

    loop: Fix discard_alignment default setting
    discard_alignment is not relevant to the loop driver since it is
    supposed to be set as a workaround for the old sector 63 alignments.
    So set it to zero rather than block size.
    
    Signed-off-by: Lukas Czerner <lczerner@redhat.com>
    Reported-by: Milan Broz <mbroz@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -797,7 +797,7 @@ static void loop_config_discard(struct loop_device *lo)
 	}
 
 	q->limits.discard_granularity = inode->i_sb->s_blocksize;
-	q->limits.discard_alignment = inode->i_sb->s_blocksize;
+	q->limits.discard_alignment = 0;
 	q->limits.max_discard_sectors = UINT_MAX >> 9;
 	q->limits.discard_zeroes_data = 1;
 	queue_flag_set_unlocked(QUEUE_FLAG_DISCARD, q);
