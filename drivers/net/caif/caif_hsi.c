1e226773c7e79051aa8a48258fe6ae61d20332bf
Christian Auby <christian.auby@stericsson.com>
2011-12-06 12:15:41 +0000

    caif-hsi: Remove wake line modification when flushing FIFO
    Raising wake before flushing FIFO and lowering it after caused a
    spike on AC wake that were sometimes detected and acted upon by the
    modem. Fixed this by remove wake line modification when flushing FIFO.
    
    Signed-off-by: Sjur Brændeland <sjur.brandeland@stericsson.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/caif/caif_hsi.c
+++ drivers/net/caif/caif_hsi.c
@@ -117,15 +117,6 @@ static int cfhsi_flush_fifo(struct cfhsi *cfhsi)
 	dev_dbg(&cfhsi->ndev->dev, "%s.\n",
 		__func__);
 
-
-	ret = cfhsi->dev->cfhsi_wake_up(cfhsi->dev);
-	if (ret) {
-		dev_warn(&cfhsi->ndev->dev,
-			"%s: can't wake up HSI interface: %d.\n",
-			__func__, ret);
-		return ret;
-	}
-
 	do {
 		ret = cfhsi->dev->cfhsi_fifo_occupancy(cfhsi->dev,
 				&fifo_occupancy);
@@ -167,8 +158,6 @@ static int cfhsi_flush_fifo(struct cfhsi *cfhsi)
 			break;
 		}
 	} while (1);
-
-	cfhsi->dev->cfhsi_wake_down(cfhsi->dev);
 
 	return ret;
 }
