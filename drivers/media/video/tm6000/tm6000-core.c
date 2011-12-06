fcd44b9efb0b33208a8575e5e776f4319d3b9698
Thierry Reding <thierry.reding@avionic-design.de>
2011-12-06 09:05:03 -0300

    [media] tm6000: Fix fast USB access quirk
    The original patch used the fast USB quirk to enable fast access to
    registers in the tm6000_read_write_usb(). The applied patch moved the
    check to the tm6000_reset(), probably due to some merge conflicts.
    
    Signed-off-by: Thierry Reding <thierry.reding@avionic-design.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/tm6000/tm6000-core.c
+++ drivers/media/video/tm6000/tm6000-core.c
@@ -88,7 +88,9 @@ int tm6000_read_write_usb(struct tm6000_core *dev, u8 req_type, u8 req,
 	}
 
 	kfree(data);
-	msleep(5);
+
+	if ((dev->quirks & TM6000_QUIRK_NO_USB_DELAY) == 0)
+		msleep(5);
 
 	mutex_unlock(&dev->usb_lock);
 	return ret;
