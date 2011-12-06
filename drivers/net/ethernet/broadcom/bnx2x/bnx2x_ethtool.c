73dbb5e1627a35c8ab81f3813c096e9e7aaabaaa
Dmitry Kravkov <dmitry@broadcom.com>
2011-12-06 02:05:12 +0000

    bnx2x: fix crash while ethtool -t
    commit 2df1a70aaf70e8dff11b89b938a5f317556ee640  "bnx2x: Support
    for byte queue limits" has introduced an asymmetry in usage of
    netdev_tx_completed_queue and netdev_tx_sent_queue. Missing
    call to netdev_tx_sent_queue causes the crash during ethtool -t.
    
    The patch adds the missing call.
    
    Signed-off-by: Dmitry Kravkov <dmitry@broadcom.com>
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/bnx2x/bnx2x_ethtool.c
+++ drivers/net/ethernet/broadcom/bnx2x/bnx2x_ethtool.c
@@ -1745,6 +1745,7 @@ static int bnx2x_run_loopback(struct bnx2x *bp, int loopback_mode)
 	u16 len;
 	int rc = -ENODEV;
 	u8 *data;
+	struct netdev_queue *txq = netdev_get_tx_queue(bp->dev, txdata->txq_index);
 
 	/* check the loopback mode */
 	switch (loopback_mode) {
@@ -1798,6 +1799,8 @@ static int bnx2x_run_loopback(struct bnx2x *bp, int loopback_mode)
 	num_pkts = 0;
 	tx_start_idx = le16_to_cpu(*txdata->tx_cons_sb);
 	rx_start_idx = le16_to_cpu(*fp_rx->rx_cons_sb);
+
+	netdev_tx_sent_queue(txq, skb->len);
 
 	pkt_prod = txdata->tx_pkt_prod++;
 	tx_buf = &txdata->tx_buf_ring[TX_BD(pkt_prod)];
