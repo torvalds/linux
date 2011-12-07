586187e22ddc87e95f8ac93f1a1b2a629c983aaf
Stephen Warren <swarren@nvidia.com>
2011-12-07 15:13:42 -0700

    arm/tegra: board-dt: Enable audio-related clocks
    Certain clocks are required for core audio functionality. Set up the
    appropriate parenting relationships, and enable clocks that must be
    on permanently.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- arch/arm/mach-tegra/board-dt.c
+++ arch/arm/mach-tegra/board-dt.c
@@ -78,6 +78,11 @@ static __initdata struct tegra_clk_init_table tegra_dt_clk_init_table[] = {
 	{ "usbd",	"clk_m",	12000000,	false },
 	{ "usb2",	"clk_m",	12000000,	false },
 	{ "usb3",	"clk_m",	12000000,	false },
+	{ "pll_a",      "pll_p_out1",   56448000,       true },
+	{ "pll_a_out0", "pll_a",        11289600,       true },
+	{ "cdev1",      NULL,           0,              true },
+	{ "i2s1",       "pll_a_out0",   11289600,       false},
+	{ "i2s2",       "pll_a_out0",   11289600,       false},
 	{ NULL,		NULL,		0,		0},
 };
 
