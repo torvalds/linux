11534ec5b6cea13ae38d31799d2a5290c5d724af
Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
2011-12-10 19:29:25 +0530

    sched: Remove cfs bandwidth period check in tg_set_cfs_period()
    Remove cfs bandwidth period check from tg_set_cfs_period.
    Invalid bandwidth period's lower/upper limits are denoted
    by min_cfs_quota_period/max_cfs_quota_period repsectively,
    and are checked against valid period in tg_set_cfs_bandwidth().
    
    As pjt pointed out, negative input will result in very large unsigned
    numbers and will be caught by the max allowed period test.
    
    Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
    Acked-by: Paul Turner <pjt@google.com>
    [ammended changelog to mention negative values]
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/20111210135925.GA14593@linux.vnet.ibm.com
    --
     kernel/sched/core.c |    3 ---
     1 file changed, 3 deletions(-)
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7714,9 +7714,6 @@ int tg_set_cfs_period(struct task_group *tg, long cfs_period_us)
 	period = (u64)cfs_period_us * NSEC_PER_USEC;
 	quota = tg->cfs_bandwidth.quota;
 
-	if (period <= 0)
-		return -EINVAL;
-
 	return tg_set_cfs_bandwidth(tg, period, quota);
 }
 
