92b62f28d02d3aafd824062f0ea57cc36fbc59d6
Johannes Berg <johannes.berg@intel.com>
2011-12-14 12:20:28 +0100

    mac80211: remove duplicate TDLS peer verification
    This is already checked in cfg80211, so no need
    to repeat the checks here.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/cfg.c
+++ net/mac80211/cfg.c
@@ -896,12 +896,6 @@ static int ieee80211_add_station(struct wiphy *wiphy, struct net_device *dev,
 	if (is_multicast_ether_addr(mac))
 		return -EINVAL;
 
-	/* Only TDLS-supporting stations can add TDLS peers */
-	if ((params->sta_flags_set & BIT(NL80211_STA_FLAG_TDLS_PEER)) &&
-	    !((wiphy->flags & WIPHY_FLAG_SUPPORTS_TDLS) &&
-	      sdata->vif.type == NL80211_IFTYPE_STATION))
-		return -ENOTSUPP;
-
 	sta = sta_info_alloc(sdata, mac, GFP_KERNEL);
 	if (!sta)
 		return -ENOMEM;
@@ -966,14 +960,6 @@ static int ieee80211_change_station(struct wiphy *wiphy,
 	if (!sta) {
 		rcu_read_unlock();
 		return -ENOENT;
-	}
-
-	/* The TDLS bit cannot be toggled after the STA was added */
-	if ((params->sta_flags_mask & BIT(NL80211_STA_FLAG_TDLS_PEER)) &&
-	    !!(params->sta_flags_set & BIT(NL80211_STA_FLAG_TDLS_PEER)) !=
-	    !!test_sta_flag(sta, WLAN_STA_TDLS_PEER)) {
-		rcu_read_unlock();
-		return -EINVAL;
 	}
 
 	/* in station mode, supported rates are only valid with TDLS */
