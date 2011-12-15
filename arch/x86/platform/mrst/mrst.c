d79a8869d8a4b565b12a88faeff834b09a36368c
Michael Demeter <michael.demeter@intel.com>
2011-12-15 22:31:23 +0000

    x86/mrst: Add additional debug prints for pb_keys
    Added additional debug output that we always seem to add
    during power ons to validate firmware operation.
    
    Signed-off-by: Michael Demeter <michael.demeter@intel.com>
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Link: http://lkml.kernel.org/r/20111215223116.10166.50803.stgit@bob.linux.org.uk
    [ fixed line breaks, formatting and commit title. ]
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/platform/mrst/mrst.c
+++ arch/x86/platform/mrst/mrst.c
@@ -983,6 +983,7 @@ static int __init pb_keys_init(void)
 	num = sizeof(gpio_button) / sizeof(struct gpio_keys_button);
 	for (i = 0; i < num; i++) {
 		gb[i].gpio = get_gpio_by_name(gb[i].desc);
+		pr_debug("info[%2d]: name = %s, gpio = %d\n", i, gb[i].desc, gb[i].gpio);
 		if (gb[i].gpio == -1)
 			continue;
 
