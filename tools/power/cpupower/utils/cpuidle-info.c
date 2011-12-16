0b37ee65e5d5e626b50852718ed72979160f7be0
Thomas Renninger <trenn@suse.de>
2011-12-16 15:35:53 +0100

    cpupower: Fix number of idle states
    The number of idle states was wrong.
    The POLL idle state (on X86) was missed out:
    Number of idle states: 4
    Available idle states: C1-NHM C3-NHM C6-NHM
    
    While the POLL is not a real idle state, its
    statistics should still be shown. It's now also
    explained in a detailed manpage.
    This should fix a bug of missing the first idle
    state on other archs.
    
    Signed-off-by: Thomas Renninger <trenn@suse.de>
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- tools/power/cpupower/utils/cpuidle-info.c
+++ tools/power/cpupower/utils/cpuidle-info.c
@@ -43,9 +43,8 @@ static void cpuidle_cpu_output(unsigned int cpu, int verbose)
 	}
 
 	printf(_("Number of idle states: %d\n"), idlestates);
-
 	printf(_("Available idle states:"));
-	for (idlestate = 1; idlestate < idlestates; idlestate++) {
+	for (idlestate = 0; idlestate < idlestates; idlestate++) {
 		tmp = sysfs_get_idlestate_name(cpu, idlestate);
 		if (!tmp)
 			continue;
@@ -57,7 +56,7 @@ static void cpuidle_cpu_output(unsigned int cpu, int verbose)
 	if (!verbose)
 		return;
 
-	for (idlestate = 1; idlestate < idlestates; idlestate++) {
+	for (idlestate = 0; idlestate < idlestates; idlestate++) {
 		tmp = sysfs_get_idlestate_name(cpu, idlestate);
 		if (!tmp)
 			continue;
