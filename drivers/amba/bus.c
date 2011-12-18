2eac58d5026e4ec8b17ff8b62877fea9e1d2f1b3
Russell King <rmk+kernel@arm.linux.org.uk>
2011-12-18 11:43:56 +0000

    ARM: amba: make use of -1 IRQs warn
    Make the core warn about the use of -1 (NO_IRQ)
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/amba/bus.c
+++ drivers/amba/bus.c
@@ -511,6 +511,9 @@ int amba_device_add(struct amba_device *dev, struct resource *parent)
 	void __iomem *tmp;
 	int i, ret;
 
+	WARN_ON(dev->irq[0] == (unsigned int)-1);
+	WARN_ON(dev->irq[1] == (unsigned int)-1);
+
 	ret = request_resource(parent, &dev->res);
 	if (ret)
 		goto err_out;
