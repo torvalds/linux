a7250db36308424ae040f1b2eeb5bfd0cbee0b0d
Yu Xu <yuxu@marvell.com>
2011-12-19 17:33:03 +0800

    usb: gadget: enlarge maxburst bit width.
    For super speed bulk transfer, the max burst size
    is 16, so that 4 bits of maxburst cannot store it.
    
    Signed-off-by: Yu Xu <yuxu@marvell.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- include/linux/usb/gadget.h
+++ include/linux/usb/gadget.h
@@ -164,7 +164,7 @@ struct usb_ep {
 	unsigned		maxpacket:16;
 	unsigned		max_streams:16;
 	unsigned		mult:2;
-	unsigned		maxburst:4;
+	unsigned		maxburst:5;
 	u8			address;
 	const struct usb_endpoint_descriptor	*desc;
 	const struct usb_ss_ep_comp_descriptor	*comp_desc;
