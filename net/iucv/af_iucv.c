42bd48e0145567acf7b3d2ae48bea765315bdd89
Ursula Braun <ursula.braun@de.ibm.com>
2011-12-19 22:56:28 +0000

    af_iucv: accelerate close for HS transport
    Closing an af_iucv socket may wait for confirmation of outstanding
    send requests. This patch adds confirmation code for the new
    HiperSockets transport.
    
    Signed-off-by: Ursula Braun <ursula.braun@de.ibm.com>
    Signed-off-by: Frank Blaschka <frank.blaschka@de.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/iucv/af_iucv.c
+++ net/iucv/af_iucv.c
@@ -2293,6 +2293,13 @@ static void afiucv_hs_callback_txnotify(struct sk_buff *skb,
 	}
 	spin_unlock_irqrestore(&list->lock, flags);
 
+	if (sk->sk_state == IUCV_CLOSING) {
+		if (skb_queue_empty(&iucv_sk(sk)->send_skb_q)) {
+			sk->sk_state = IUCV_CLOSED;
+			sk->sk_state_change(sk);
+		}
+	}
+
 out_unlock:
 	bh_unlock_sock(sk);
 }
