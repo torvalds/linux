4f3f25821cc2d75e65046949200c514ddd00c6a8
Peter De Schrijver (NVIDIA) <pdeschrijver@nvidia.com>
2011-12-21 10:50:42 +0100

    ARM: 7241/1: mach-ux500 Use CONFIG_ARCH_NR_GPIO
    Add default value for CONFIG_ARCH_NR_GPIO to Kconfig and remove the
    definition in gpio.h. We can't remove gpio.h yet as asm/gpio.h still
    includes it.
    
    Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-ux500/include/mach/gpio.h
+++ arch/arm/mach-ux500/include/mach/gpio.h
@@ -1,10 +1,5 @@
 #ifndef __ASM_ARCH_GPIO_H
 #define __ASM_ARCH_GPIO_H
 
-/*
- * 288 (#267 is the highest one actually hooked up) onchip GPIOs, plus enough
- * room for a couple of GPIO expanders.
- */
-#define ARCH_NR_GPIOS	350
 
 #endif /* __ASM_ARCH_GPIO_H */
