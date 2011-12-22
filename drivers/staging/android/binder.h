29b858471b266be7e56b69cfcee7ba94d9427dd3
Marco Navarra <fromenglish@gmail.com>
2011-12-22 13:27:07 +0100

    Staging: android: fixed a space warning in binder.h
    This patch fixes a simple tab-space warning in binder.h found by checkpatch tool
    
    Signed-off-by: Marco Navarra <fromenglish@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/android/binder.h
+++ drivers/staging/android/binder.h
@@ -84,7 +84,7 @@ struct binder_version {
 /* This is the current protocol version. */
 #define BINDER_CURRENT_PROTOCOL_VERSION 7
 
-#define BINDER_WRITE_READ   		_IOWR('b', 1, struct binder_write_read)
+#define BINDER_WRITE_READ		_IOWR('b', 1, struct binder_write_read)
 #define	BINDER_SET_IDLE_TIMEOUT		_IOW('b', 3, int64_t)
 #define	BINDER_SET_MAX_THREADS		_IOW('b', 5, size_t)
 #define	BINDER_SET_IDLE_PRIORITY	_IOW('b', 6, int)
