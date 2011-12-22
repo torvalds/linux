b014f14c81bc5db4e40e2a4db10998fd79c1c64c
NeilBrown <neilb@suse.de>
2011-12-23 10:17:56 +1100

    md/raid1: Allow a failed replacement device to be removed.
    Replacement devices are stored at a different offset, so look
    there too.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- drivers/md/raid1.c
+++ drivers/md/raid1.c
@@ -1336,6 +1336,9 @@ static int raid1_remove_disk(struct mddev *mddev, struct md_rdev *rdev)
 	int number = rdev->raid_disk;
 	struct mirror_info *p = conf->mirrors+ number;
 
+	if (rdev != p->rdev)
+		p = conf->mirrors + conf->raid_disks + number;
+
 	print_conf(conf);
 	if (rdev == p->rdev) {
 		if (test_bit(In_sync, &rdev->flags) ||
@@ -1359,6 +1362,9 @@ static int raid1_remove_disk(struct mddev *mddev, struct md_rdev *rdev)
 			err = -EBUSY;
 			p->rdev = rdev;
 			goto abort;
+		} else {
+			clear_bit(Replacement, &rdev->flags);
+			clear_bit(WantReplacement, &rdev->flags);
 		}
 		err = md_integrity_register(mddev);
 	}
