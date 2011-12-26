244e209cbfb8fef744f47f4a9fe0b8c036772d8f
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2011-12-26 08:47:33 +0200

    iwlwifi: update SCD BC table for all SCD queues
    commit 96f1f05af76b601ab21a7dc603ae0a1cea4efc3d upstream.
    
    Since we configure all the queues as CHAINABLE, we need to update the
    byte count for all the queues, not only the AGGREGATABLE ones.
    
    Not doing so can confuse the SCD and make the fw assert.
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-agn-tx.c
+++ drivers/net/wireless/iwlwifi/iwl-agn-tx.c
@@ -778,10 +778,7 @@ int iwlagn_tx_skb(struct iwl_priv *priv, struct sk_buff *skb)
 	iwl_print_hex_dump(priv, IWL_DL_TX, (u8 *)tx_cmd, sizeof(*tx_cmd));
 	iwl_print_hex_dump(priv, IWL_DL_TX, (u8 *)tx_cmd->hdr, hdr_len);
 
-	/* Set up entry for this TFD in Tx byte-count array */
-	if (info->flags & IEEE80211_TX_CTL_AMPDU)
-		iwlagn_txq_update_byte_cnt_tbl(priv, txq,
-					       le16_to_cpu(tx_cmd->len));
+	iwlagn_txq_update_byte_cnt_tbl(priv, txq, le16_to_cpu(tx_cmd->len));
 
 	pci_dma_sync_single_for_device(priv->pci_dev, txcmd_phys,
 				       firstlen, PCI_DMA_BIDIRECTIONAL);
