4ccf2f1349e681401b5fae73efc87b8d2d70ce0e
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2011-12-30 15:48:55 +0200

    jffs: remove custom mtd_fake_writev function
    Instead, use 'default_mtd_writev()' function which MTD provides.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- fs/jffs2/writev.c
+++ fs/jffs2/writev.c
@@ -13,31 +13,6 @@
 #include <linux/mtd/mtd.h>
 #include "nodelist.h"
 
-/* This ought to be in core MTD code. All registered MTD devices
-   without writev should have this put in place. Bug the MTD
-   maintainer */
-static inline int mtd_fake_writev(struct mtd_info *mtd, const struct kvec *vecs,
-				  unsigned long count, loff_t to, size_t *retlen)
-{
-	unsigned long i;
-	size_t totlen = 0, thislen;
-	int ret = 0;
-
-	for (i=0; i<count; i++) {
-		if (!vecs[i].iov_len)
-			continue;
-		ret = mtd_write(mtd, to, vecs[i].iov_len, &thislen,
-				vecs[i].iov_base);
-		totlen += thislen;
-		if (ret || thislen != vecs[i].iov_len)
-			break;
-		to += vecs[i].iov_len;
-	}
-	if (retlen)
-		*retlen = totlen;
-	return ret;
-}
-
 int jffs2_flash_direct_writev(struct jffs2_sb_info *c, const struct kvec *vecs,
 			      unsigned long count, loff_t to, size_t *retlen)
 {
@@ -54,7 +29,7 @@ int jffs2_flash_direct_writev(struct jffs2_sb_info *c, const struct kvec *vecs,
 	if (c->mtd->writev)
 		return mtd_writev(c->mtd, vecs, count, to, retlen);
 	else {
-		return mtd_fake_writev(c->mtd, vecs, count, to, retlen);
+		return default_mtd_writev(c->mtd, vecs, count, to, retlen);
 	}
 }
 
