1e73fa5d56333230854ae9460579eb2fcee8af02
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-12-31 17:24:19 -0200

    [media] stb6100: Properly retrieve symbol rate
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/stb6100.c
+++ drivers/media/dvb/frontends/stb6100.c
@@ -327,7 +327,7 @@ static int stb6100_set_frequency(struct dvb_frontend *fe, u32 frequency)
 	int rc;
 	const struct stb6100_lkup *ptr;
 	struct stb6100_state *state = fe->tuner_priv;
-	struct dtv_frontend_properties p;
+	struct dtv_frontend_properties *p = &fe->dtv_property_cache;
 
 	u32 srate = 0, fvco, nint, nfrac;
 	u8 regs[STB6100_NUMREGS];
@@ -339,7 +339,7 @@ static int stb6100_set_frequency(struct dvb_frontend *fe, u32 frequency)
 		dprintk(verbose, FE_DEBUG, 1, "Get frontend parameters");
 		fe->ops.get_frontend(fe);
 	}
-	srate = p.symbol_rate;
+	srate = p->symbol_rate;
 
 	/* Set up tuner cleanly, LPF calibration on */
 	rc = stb6100_write_reg(state, STB6100_FCCK, 0x4d | STB6100_FCCK_FCCK);
