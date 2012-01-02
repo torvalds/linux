b2296bd43e781976743354c668a356b0df98e1da
Laxman Dewangan <ldewangan@nvidia.com>
2012-01-02 13:08:45 +0530

    regulator: Enable supply regulator if child rail is enabled.
    During regulator_register, the rail is set on the provided
    machine constraints and if it is enabled then it is also
    require to enable the supply regulator. This will make sure
    that:
       1. Proper reference count for supply regulator to be maintain.
       2. Supply regulator should be enable when given rail is enabled.
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -2802,6 +2802,14 @@ struct regulator_dev *regulator_register(struct regulator_desc *regulator_desc,
 		ret = set_supply(rdev, r);
 		if (ret < 0)
 			goto scrub;
+
+		/* Enable supply if rail is enabled */
+		if (rdev->desc->ops->is_enabled &&
+				rdev->desc->ops->is_enabled(rdev)) {
+			ret = regulator_enable(rdev->supply);
+			if (ret < 0)
+				goto scrub;
+		}
 	}
 
 	/* add consumers devices */
