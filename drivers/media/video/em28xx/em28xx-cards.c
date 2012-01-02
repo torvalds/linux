74e07f3e309599864656fb4560c498478d22d43c
Don Kramer <dgkramer@comcast.net>
2012-01-02 16:15:44 -0300

    [media] [resend] em28xx: Add Plextor ConvertX PX-AV100U to em28xx-cards.c
    Adds support for the Plextor ConvertX PX-AV100U, which uses the
    eMPIA EM2820 chip.  The device has a device_id of '0x093b, 0xa003'.  I
    am using the existing EM2820_BOARD_PINNACLE_DVC_90 board profile, as
    the Pinnacle Dazzle DVC 90/100/101/107, Kaiser Baas Video to DVD
    maker, and Kworld DVD Maker 2 were already mapped to it.  Some more
    background on the device and my testing can be found at
    http://www.donkramer.net/plextor_122710.pdf
    
    Signed-off-by: Don Kramer <dgkramer@comcast.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/em28xx/em28xx-cards.c
+++ drivers/media/video/em28xx/em28xx-cards.c
@@ -1270,7 +1270,7 @@ struct em28xx_board em28xx_boards[] = {
 	},
 	[EM2820_BOARD_PINNACLE_DVC_90] = {
 		.name         = "Pinnacle Dazzle DVC 90/100/101/107 / Kaiser Baas Video to DVD maker "
-				"/ Kworld DVD Maker 2",
+			       "/ Kworld DVD Maker 2 / Plextor ConvertX PX-AV100U",
 		.tuner_type   = TUNER_ABSENT, /* capture only board */
 		.decoder      = EM28XX_SAA711X,
 		.input        = { {
@@ -2039,6 +2039,8 @@ struct usb_device_id em28xx_id_table[] = {
 			.driver_info = EM2880_BOARD_PINNACLE_PCTV_HD_PRO },
 	{ USB_DEVICE(0x0413, 0x6023),
 			.driver_info = EM2800_BOARD_LEADTEK_WINFAST_USBII },
+	{ USB_DEVICE(0x093b, 0xa003),
+		       .driver_info = EM2820_BOARD_PINNACLE_DVC_90 },
 	{ USB_DEVICE(0x093b, 0xa005),
 			.driver_info = EM2861_BOARD_PLEXTOR_PX_TV100U },
 	{ USB_DEVICE(0x04bb, 0x0515),
