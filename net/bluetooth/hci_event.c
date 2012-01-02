c1b9b4f45f014093231c568be9b5c2e392943696
Andre Guedes <andre.guedes@openbossa.org>
2012-01-02 19:18:10 -0300

    Bluetooth: Fix adv_work delay
    This patch fixes the adv_work delay. It should be set to ADV_
    CLEAR_TIMEOUT instead of (jiffies + ADV_CLEAR_TIMEOUT) since
    queue_delayed_work() receives the number of jiffies to wait
    before queueing. It also removes the unnecessary cancel_delayed_
    work_sync() call in case LE scan has been disabled.
    
    Signed-off-by: Andre Guedes <andre.guedes@openbossa.org>
    Acked-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -1054,9 +1054,8 @@ static void hci_cc_le_set_scan_enable(struct hci_dev *hdev,
 	case LE_SCANNING_DISABLED:
 		clear_bit(HCI_LE_SCAN, &hdev->dev_flags);
 
-		cancel_delayed_work_sync(&hdev->adv_work);
 		queue_delayed_work(hdev->workqueue, &hdev->adv_work,
-						 jiffies + ADV_CLEAR_TIMEOUT);
+							ADV_CLEAR_TIMEOUT);
 		break;
 
 	default:
