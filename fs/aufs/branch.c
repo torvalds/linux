817a9796b7a6c2d72033ff09c9b604cc5761f969
J. R. Okajima <hooanon05@yahoo.co.jp>
2012-01-03 13:07:48 +0900

    aufs: tiny, update the year
    Signed-off-by: J. R. Okajima <hooanon05@yahoo.co.jp>

--- fs/aufs/branch.c
+++ fs/aufs/branch.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2011 Junjiro R. Okajima
+ * Copyright (C) 2005-2012 Junjiro R. Okajima
  *
  * This program, aufs is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
