38e3d7ce41cff58bacebb2bcecf7d386c60b954b
Steven Toth <stoth@kernellabs.com>
2012-01-04 10:54:32 -0300

    [media] cx23885: Ensure the MPEG encoder height is configured from the norm
    Bugfix: The height was not always correctly configured if switching between
    different video standards. Change the encode height based on the standard.
    
    Signed-off-by: Steven Toth <stoth@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx23885/cx23885-417.c
+++ drivers/media/video/cx23885/cx23885-417.c
@@ -1023,6 +1023,12 @@ static void cx23885_codec_settings(struct cx23885_dev *dev)
 {
 	dprintk(1, "%s()\n", __func__);
 
+	/* Dynamically change the height based on video standard */
+	if (dev->encodernorm.id & V4L2_STD_525_60)
+		dev->ts1.height = 480;
+	else
+		dev->ts1.height = 576;
+
 	/* assign frame size */
 	cx23885_api_cmd(dev, CX2341X_ENC_SET_FRAME_SIZE, 2, 0,
 				dev->ts1.height, dev->ts1.width);
