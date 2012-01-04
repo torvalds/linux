f423fc627b05f47bc9305f9661630fce30f208f9
Linus Torvalds <torvalds@linux-foundation.org>
2012-01-04 07:57:22 -0800

    Revert "rtc: Expire alarms after the time is set."
    This reverts commit 93b2ec0128c431148b216b8f7337c1a52131ef03.
    
    The call to "schedule_work()" in rtc_initialize_alarm() happens too
    early, and can cause oopses at bootup
    
    Neil Brown explains why we do it:
    
      "If you set an alarm in the future, then shutdown and boot again after
       that time, then you will end up with a timer_queue node which is in
       the past.
    
       When this happens the queue gets stuck.  That entry-in-the-past won't
       get removed until and interrupt happens and an interrupt won't happen
       because the RTC only triggers an interrupt when the alarm is "now".
    
       So you'll find that e.g.  "hwclock" will always tell you that
       'select' timed out.
    
       So we force the interrupt work to happen at the start just in case."
    
    and has a patch that convert it to do things in-process rather than with
    the worker thread, but right now it's too late to play around with this,
    so we just revert the patch that caused problems for now.
    
    Reported-by: Sander Eikelenboom <linux@eikelenboom.it>
    Requested-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Requested-by: John Stultz <john.stultz@linaro.org>
    Cc: Neil Brown <neilb@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/rtc/interface.c
+++ drivers/rtc/interface.c
@@ -73,8 +73,6 @@ int rtc_set_time(struct rtc_device *rtc, struct rtc_time *tm)
 		err = -EINVAL;
 
 	mutex_unlock(&rtc->ops_lock);
-	/* A timer might have just expired */
-	schedule_work(&rtc->irqwork);
 	return err;
 }
 EXPORT_SYMBOL_GPL(rtc_set_time);
@@ -114,8 +112,6 @@ int rtc_set_mmss(struct rtc_device *rtc, unsigned long secs)
 		err = -EINVAL;
 
 	mutex_unlock(&rtc->ops_lock);
-	/* A timer might have just expired */
-	schedule_work(&rtc->irqwork);
 
 	return err;
 }
@@ -400,8 +396,6 @@ int rtc_initialize_alarm(struct rtc_device *rtc, struct rtc_wkalrm *alarm)
 		timerqueue_add(&rtc->timerqueue, &rtc->aie_timer.node);
 	}
 	mutex_unlock(&rtc->ops_lock);
-	/* maybe that was in the past.*/
-	schedule_work(&rtc->irqwork);
 	return err;
 }
 EXPORT_SYMBOL_GPL(rtc_initialize_alarm);
