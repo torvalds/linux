0963119fca6ad250146fd7d27eb90f205e188beb
Samuel Rakitnican <samuel.rakitnican@gmail.com>
2012-01-06 17:34:53 -0300

    [media] rc-videomate-m1f.c Rename to match remote controler name
    This remote was added with support for card Compro VideoMate M1F.
    
    This remote is shipped with various Compro cards, not this one only.
    
    Furthermore this remote can be bought separately under name Compro
    VideoMate K100.
        http://compro.com.tw/en/product/k100/k100.html
    
    So give it a proper name.
    
    [mchehab@redhat.com: Fix the Makefile]
    Signed-off-by: Samuel Rakitničan <samuel.rakitnican@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/saa7134/saa7134-input.c
+++ drivers/media/video/saa7134/saa7134-input.c
@@ -755,7 +755,7 @@ int saa7134_input_init1(struct saa7134_dev *dev)
 		polling      = 50; /* ms */
 		break;
 	case SAA7134_BOARD_VIDEOMATE_M1F:
-		ir_codes     = RC_MAP_VIDEOMATE_M1F;
+		ir_codes     = RC_MAP_VIDEOMATE_K100;
 		mask_keycode = 0x0ff00;
 		mask_keyup   = 0x040000;
 		break;
