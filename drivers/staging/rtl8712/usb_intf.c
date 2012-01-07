fc2286972bd426b03fe64d79d39d9ff9ac1c4cc3
Larry Finger <Larry.Finger@lwfinger.net>
2012-01-07 10:07:03 -0600

    staging: r8712u: Add new Sitecom UsB ID
    commit 1793bf1deddc8ce25dc41925d5dbe64536c841b6 upstream.
    
    Add USB ID for SITECOM WLA-1000 V1 001 WLAN
    
    Reported-and-tested-by: Roland Gruber <post@rolandgruber.de>
    Reported-and-tested-by: Dario Lucia <dario.lucia@gmail.com>
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8712/usb_intf.c
+++ drivers/staging/rtl8712/usb_intf.c
@@ -86,6 +86,7 @@ static struct usb_device_id rtl871x_usb_id_tbl[] = {
 	{USB_DEVICE(0x0DF6, 0x0045)},
 	{USB_DEVICE(0x0DF6, 0x0059)}, /* 11n mode disable */
 	{USB_DEVICE(0x0DF6, 0x004B)},
+	{USB_DEVICE(0x0DF6, 0x005B)},
 	{USB_DEVICE(0x0DF6, 0x005D)},
 	{USB_DEVICE(0x0DF6, 0x0063)},
 	/* Sweex */
