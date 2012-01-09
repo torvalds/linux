e7a2577a410a066ae409f805618f7e84748a537d
Julia Lawall <julia.lawall@lip6.fr>
2012-01-09 10:40:49 +0100

    crypto: ixp4xx - convert GFP_KERNEL to GFP_ATOMIC
    The function is called with locks held and thus should not use GFP_KERNEL.
    
    The semantic patch that makes this report is available
    in scripts/coccinelle/locks/call_kern.cocci.
    
    More information about semantic patching is available at
    http://coccinelle.lip6.fr/
    
    Signed-off-by: Julia Lawall <julia.lawall@lip6.fr>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/ixp4xx_crypto.c
+++ drivers/crypto/ixp4xx_crypto.c
@@ -265,7 +265,7 @@ static int setup_crypt_desc(void)
 	BUILD_BUG_ON(sizeof(struct crypt_ctl) != 64);
 	crypt_virt = dma_alloc_coherent(dev,
 			NPE_QLEN * sizeof(struct crypt_ctl),
-			&crypt_phys, GFP_KERNEL);
+			&crypt_phys, GFP_ATOMIC);
 	if (!crypt_virt)
 		return -ENOMEM;
 	memset(crypt_virt, 0, NPE_QLEN * sizeof(struct crypt_ctl));
