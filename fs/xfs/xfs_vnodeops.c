2fbe11fed22ed2751aaf4b4926f27730aaac5470
Jan Kara <jack@suse.cz>
2012-01-11 18:52:10 +0000

    xfs: Fix missing xfs_iunlock() on error recovery path in xfs_readlink()
    commit 9b025eb3a89e041bab6698e3858706be2385d692 upstream.
    
    Commit b52a360b forgot to call xfs_iunlock() when it detected corrupted
    symplink and bailed out. Fix it by jumping to 'out' instead of doing return.
    
    CC: Carlos Maiolino <cmaiolino@redhat.com>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Alex Elder <elder@kernel.org>
    Reviewed-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Ben Myers <bpm@sgi.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/xfs/xfs_vnodeops.c
+++ fs/xfs/xfs_vnodeops.c
@@ -554,7 +554,8 @@ xfs_readlink(
 			 __func__, (unsigned long long) ip->i_ino,
 			 (long long) pathlen);
 		ASSERT(0);
-		return XFS_ERROR(EFSCORRUPTED);
+		error = XFS_ERROR(EFSCORRUPTED);
+		goto out;
 	}
 
 
