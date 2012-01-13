52f1a845e25dc0c6435153aca1a170e20b282429
Javier Martin <javier.martin@vista-silicon.com>
2012-01-13 06:31:57 -0300

    [media] MX2: Add platform definitions for eMMa-PrP device
    eMMa-PrP device included in Freescale i.MX2 chips can also
    be used separately to process memory buffers. This patch
    provides arch glue code for the driver which provides this
    functionality.
    
    Acked-by: Sascha Hauer <s.hauer@pengutronix.de>
    Signed-off-by: Javier Martin <javier.martin@vista-silicon.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- arch/arm/mach-imx/devices-imx27.h
+++ arch/arm/mach-imx/devices-imx27.h
@@ -50,6 +50,8 @@ extern const struct imx_imx_uart_1irq_data imx27_imx_uart_data[];
 extern const struct imx_mx2_camera_data imx27_mx2_camera_data;
 #define imx27_add_mx2_camera(pdata)	\
 	imx_add_mx2_camera(&imx27_mx2_camera_data, pdata)
+#define imx27_add_mx2_emmaprp(pdata)	\
+	imx_add_mx2_emmaprp(&imx27_mx2_camera_data)
 
 extern const struct imx_mxc_ehci_data imx27_mxc_ehci_otg_data;
 #define imx27_add_mxc_ehci_otg(pdata)	\
