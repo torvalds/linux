24dd0dd74ec8dc4abada132e380dc179459b0f77
Antonio Quartulli <ordex@autistici.org>
2012-01-18 00:10:43 +0100

    mac80211: add a 2-way Authentication challenge to IBSS mode
    In IBSS mode, whenever a new station is added a 2-way authentication challenge
    is performed. Actually this event can be used to recognise a new station joining
    the cell even if its sta_info entry is already in the list.
    
    Signed-off-by: Antonio Quartulli <ordex@autistici.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/ibss.c
+++ net/mac80211/ibss.c
@@ -298,6 +298,13 @@ static struct sta_info *ieee80211_ibss_finish_sta(struct sta_info *sta)
 	/* If it fails, maybe we raced another insertion? */
 	if (sta_info_insert_rcu(sta))
 		return sta_info_get(sdata, addr);
+#ifdef CONFIG_MAC80211_IBSS_DEBUG
+	printk(KERN_DEBUG "TX Auth SA=%pM DA=%pM BSSID=%pM"
+	       "(auth_transaction=1)\n", sdata->vif.addr,
+	       sdata->u.ibss.bssid, addr);
+#endif
+	ieee80211_send_auth(sdata, 1, WLAN_AUTH_OPEN, NULL, 0,
+			    addr, sdata->u.ibss.bssid, NULL, 0, 0);
 	return sta;
 }
 
