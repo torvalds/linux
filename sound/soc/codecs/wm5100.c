e53e417331c57b9b97e3f8be870214a02c99265c
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-01-18 20:02:38 +0000

    ASoC: Mark WM5100 register map cache only when going into BIAS_OFF
    Writing to the registers won't work if we do actually manage to hit a fully
    powered off state.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Cc: stable@kernel.org

--- sound/soc/codecs/wm5100.c
+++ sound/soc/codecs/wm5100.c
@@ -1402,6 +1402,7 @@ static int wm5100_set_bias_level(struct snd_soc_codec *codec,
 		break;
 
 	case SND_SOC_BIAS_OFF:
+		regcache_cache_only(wm5100->regmap, true);
 		if (wm5100->pdata.ldo_ena)
 			gpio_set_value_cansleep(wm5100->pdata.ldo_ena, 0);
 		regulator_bulk_disable(ARRAY_SIZE(wm5100->core_supplies),
