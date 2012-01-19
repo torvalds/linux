90a4c0f51e8e44111a926be6f4c87af3938a79c3
Linus Torvalds <torvalds@linux-foundation.org>
2012-01-18 19:26:11 -0800

    uml: fix compile for x86-64
    Randy Dunlap reports that we get
    
      arch/x86/um/shared/sysdep/ptrace.h:7:20: error: redefinition of 'regs_return_value'
      arch/x86/um/shared/sysdep/ptrace.h:7:20: note: previous definition of 'regs_return_value' was here
    
    when compiling UML for x86-64.
    
    Stephen Rothwell root-caused it and says:
    
     "Caused by commit d7e7528bcd45 ("Audit: push audit success and retcode
      into arch ptrace.h") (another patch that was never in linux-next :-().
    
      This file now needs protection against double inclusion."
    
    so let's do as the man says.
    
    Reported-by: Randy Dunlap <rdunlap@xenotime.net>
    Analyzed-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/x86/um/shared/sysdep/ptrace.h
+++ arch/x86/um/shared/sysdep/ptrace.h
@@ -1,3 +1,6 @@
+#ifndef __SYSDEP_X86_PTRACE_H
+#define __SYSDEP_X86_PTRACE_H
+
 #ifdef __i386__
 #include "ptrace_32.h"
 #else
@@ -8,3 +11,5 @@ static inline long regs_return_value(struct uml_pt_regs *regs)
 {
 	return UPT_SYSCALL_RET(regs);
 }
+
+#endif /* __SYSDEP_X86_PTRACE_H */
