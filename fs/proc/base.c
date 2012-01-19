dac306d896642f3dfcd878ab6a0a5b8c123722f3
Dima Zavin <dima@android.com>
2012-01-19 09:51:07 -0800

    Revert "proc: enable writing to /proc/pid/mem"
    This reverts commit 198214a7ee50375fa71a65e518341980cfd4b2f0.

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -891,6 +891,10 @@ out_no_task:
 	return ret;
 }
 
+#define mem_write NULL
+
+#ifndef mem_write
+/* This is a security hazard */
 static ssize_t mem_write(struct file * file, const char __user *buf,
 			 size_t count, loff_t *ppos)
 {
@@ -949,6 +953,7 @@ out_task:
 out_no_task:
 	return copied;
 }
+#endif
 
 loff_t mem_lseek(struct file *file, loff_t offset, int orig)
 {
