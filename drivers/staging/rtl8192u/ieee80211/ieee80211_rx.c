04a5602db4a07ef3cad3eff5c757384096ab8758
Masanari Iida <standby24x7@gmail.com>
2012-01-25 00:35:41 +0900

    staging: Fix typo in ieee80211_rx.c
    Correct spelling "suppported" to "supported" in
    drivers/staging/rtl8192u/ieee80211/ieee80211_rx.c
    
    Signed-off-by: Masanari iida<standby24x7@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/staging/rtl8192u/ieee80211/ieee80211_rx.c
+++ drivers/staging/rtl8192u/ieee80211/ieee80211_rx.c
@@ -237,7 +237,7 @@ ieee80211_rx_frame_mgmt(struct ieee80211_device *ieee, struct sk_buff *skb,
 
 	#ifdef NOT_YET
 	if (ieee->iw_mode == IW_MODE_MASTER) {
-		printk(KERN_DEBUG "%s: Master mode not yet suppported.\n",
+		printk(KERN_DEBUG "%s: Master mode not yet supported.\n",
 		       ieee->dev->name);
 		return 0;
 /*
