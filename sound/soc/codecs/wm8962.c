182c51ce7944a214dd77a0b5c0462241e49dd418
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-01-24 21:07:55 +0000

    ASoC: wm8962: Optimise power consumption for IN4 DC measurement usage
    When the hardware is configured with one or both of the IN4 inputs used
    for DC measurement (with no DC blocking capacitor connected) then we can
    improve power consumption slightly in idle modes by applying a register
    write sequence. Provide platform data to enable this, implemented using
    a regmap patch.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm8962.c
+++ sound/soc/codecs/wm8962.c
@@ -3638,6 +3638,13 @@ static struct snd_soc_codec_driver soc_codec_dev_wm8962 = {
 	.volatile_register = wm8962_soc_volatile,
 };
 
+/* Improve power consumption for IN4 DC measurement mode */
+static const struct reg_default wm8962_dc_measure[] = {
+	{ 0xfd, 0x1 },
+	{ 0xcc, 0x40 },
+	{ 0xfd, 0 },
+};
+
 static const struct regmap_config wm8962_regmap = {
 	.reg_bits = 16,
 	.val_bits = 16,
@@ -3653,6 +3660,7 @@ static const struct regmap_config wm8962_regmap = {
 static __devinit int wm8962_i2c_probe(struct i2c_client *i2c,
 				      const struct i2c_device_id *id)
 {
+	struct wm8962_pdata *pdata = dev_get_platdata(&i2c->dev);
 	struct wm8962_priv *wm8962;
 	unsigned int reg;
 	int ret, i;
@@ -3729,6 +3737,16 @@ static __devinit int wm8962_i2c_probe(struct i2c_client *i2c,
 	if (ret < 0) {
 		dev_err(&i2c->dev, "Failed to issue reset\n");
 		goto err_regmap;
+	}
+
+	if (pdata && pdata->in4_dc_measure) {
+		ret = regmap_register_patch(wm8962->regmap,
+					    wm8962_dc_measure,
+					    ARRAY_SIZE(wm8962_dc_measure));
+		if (ret != 0)
+			dev_err(&i2c->dev,
+				"Failed to configure for DC mesurement: %d\n",
+				ret);
 	}
 
 	regcache_cache_only(wm8962->regmap, true);
