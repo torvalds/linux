94ac27a54be6a14948f0a9b3f542b4ff1faac232
Philippe Rétornaz <philippe.retornaz@epfl.ch>
2012-01-24 14:22:01 +0100

    i.MX SDMA: Fix burstsize settings
    Commit 6584cb88 (ARM i.MX dma: Fix burstsize settings) fixed
    the mxcmmc driver but forgot to fix the SDMA driver to handle the
    correct burtsize.
    This make the SD card access works again with DMA on i.MX31 boards.
    
    Signed-off-by: Philippe Rétornaz <philippe.retornaz@epfl.ch>
    Tested-by: Sascha Hauer <s.hauer@pengutronix.de>
    Acked-by: Sascha Hauer <s.hauer@pengutronix.de>
    Signed-off-by: Vinod Koul <vinod.koul@linux.intel.com>

--- drivers/dma/imx-sdma.c
+++ drivers/dma/imx-sdma.c
@@ -1102,11 +1102,13 @@ static int sdma_control(struct dma_chan *chan, enum dma_ctrl_cmd cmd,
 	case DMA_SLAVE_CONFIG:
 		if (dmaengine_cfg->direction == DMA_DEV_TO_MEM) {
 			sdmac->per_address = dmaengine_cfg->src_addr;
-			sdmac->watermark_level = dmaengine_cfg->src_maxburst;
+			sdmac->watermark_level = dmaengine_cfg->src_maxburst *
+						dmaengine_cfg->src_addr_width;
 			sdmac->word_size = dmaengine_cfg->src_addr_width;
 		} else {
 			sdmac->per_address = dmaengine_cfg->dst_addr;
-			sdmac->watermark_level = dmaengine_cfg->dst_maxburst;
+			sdmac->watermark_level = dmaengine_cfg->dst_maxburst *
+						dmaengine_cfg->dst_addr_width;
 			sdmac->word_size = dmaengine_cfg->dst_addr_width;
 		}
 		sdmac->direction = dmaengine_cfg->direction;
