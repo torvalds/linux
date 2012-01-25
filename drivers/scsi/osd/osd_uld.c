41f8ad76362e7aefe3a03949c43e23102dae6e0b
Boaz Harrosh <bharrosh@panasas.com>
2012-01-25 21:42:58 +0200

    [SCSI] osd_uld: Bump MAX_OSD_DEVICES from 64 to 1,048,576
    It used to be that minors where 8 bit. But now they
    are actually 20 bit. So the fix is simplicity itself.
    
    I've tested with 300 devices and all user-mode utils
    work just fine. I have also mechanically added 10,000
    to the ida (so devices are /dev/osd10000, /dev/osd10001 ...)
    and was able to mkfs an exofs filesystem and access osds
    from user-mode.
    
    All the open-osd user-mode code uses the same library
    to access devices through their symbolic names in
    /dev/osdX so I'd say it's pretty safe. (Well tested)
    
    This patch is very important because some of the systems
    that will be deploying the 3.2 pnfs-objects code are larger
    than 64 OSDs and will stop to work properly when reaching
    that number.
    
    CC: Stable <stable@vger.kernel.org>
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/osd/osd_uld.c
+++ drivers/scsi/osd/osd_uld.c
@@ -69,10 +69,10 @@
 #ifndef SCSI_OSD_MAJOR
 #  define SCSI_OSD_MAJOR 260
 #endif
-#define SCSI_OSD_MAX_MINOR 64
+#define SCSI_OSD_MAX_MINOR MINORMASK
 
 static const char osd_name[] = "osd";
-static const char *osd_version_string = "open-osd 0.2.0";
+static const char *osd_version_string = "open-osd 0.2.1";
 
 MODULE_AUTHOR("Boaz Harrosh <bharrosh@panasas.com>");
 MODULE_DESCRIPTION("open-osd Upper-Layer-Driver osd.ko");
