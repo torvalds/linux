d976f7626627117c211137cd2aaf8c1656e6627d
Jim Cromie <jim.cromie@gmail.com>
2012-01-26 15:52:15 -0800

    clocksource: scx200_hrt:  use pr_<level> instead of printk
    Switch from printk to using pr_<level>.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    [added commit msg & tweaked subject -jstultz]
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- drivers/clocksource/scx200_hrt.c
+++ drivers/clocksource/scx200_hrt.c
@@ -71,7 +71,7 @@ static int __init init_hrt_clocksource(void)
 	if (!request_region(scx200_cb_base + SCx200_TIMER_OFFSET,
 			    SCx200_TIMER_SIZE,
 			    "NatSemi SCx200 High-Resolution Timer")) {
-		printk(KERN_WARNING NAME ": unable to lock timer region\n");
+		pr_warn("unable to lock timer region\n");
 		return -ENODEV;
 	}
 
@@ -88,7 +88,7 @@ static int __init init_hrt_clocksource(void)
 		cs_hrt.mult = clocksource_hz2mult(HRT_FREQ + ppm,
 						  cs_hrt.shift);
 	}
-	printk(KERN_INFO "enabling scx200 high-res timer (%s MHz +%d ppm)\n",
+	pr_info("enabling scx200 high-res timer (%s MHz +%d ppm)\n",
 		mhz27 ? "27":"1", ppm);
 
 	return clocksource_register(&cs_hrt);
