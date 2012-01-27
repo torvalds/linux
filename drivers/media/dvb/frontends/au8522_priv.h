b31506c47c5ae67a82fa72e6d763a8f34413aac8
Michael Krufky <mkrufky@kernellabs.com>
2012-01-27 13:18:29 -0300

    [media] au8522: build ATV/DTV demodulators as separate modules
    au8522_dig.o and au8522_decoder.o function independentantly
    of each other, each for a different hardware function using
    a different software subsystem api, each with its own set
    of subsystem module dependencies.
    
    Since these drivers do not depend on each other, and it is
    in fact possible to build hardware designs using one function
    and not the other, lets split this module into two, allowing
    system integrators to enable the hardware without dragging in
    undesired dependencies.
    
    Signed-off-by: Michael Krufky <mkrufky@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/au8522_priv.h
+++ drivers/media/dvb/frontends/au8522_priv.h
@@ -81,6 +81,8 @@ int au8522_sleep(struct dvb_frontend *fe);
 int au8522_get_state(struct au8522_state **state, struct i2c_adapter *i2c,
 		     u8 client_address);
 void au8522_release_state(struct au8522_state *state);
+int au8522_i2c_gate_ctrl(struct dvb_frontend *fe, int enable);
+int au8522_led_ctrl(struct au8522_state *state, int led);
 
 /* REGISTERS */
 #define AU8522_INPUT_CONTROL_REG081H			0x081
