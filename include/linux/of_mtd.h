770d7c39af940da24dd4c2c048576d778ac0abd4
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2012-01-28 12:12:36 +0800

    of/mtd/nand: add generic bindings and helpers
    - nand-ecc-mode : String, operation mode of the NAND ecc mode.
      Supported values are: "none", "soft", "hw", "hw_syndrome", "hw_oob_first",
      "soft_bch".
    - nand-bus-width : 8 or 16 bus width if not present 8
    - nand-on-flash-bbt: boolean to enable on flash bbt option if not present false
    
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Acked-by: Grant Likely <grant.likely@secretlab.ca>
    Acked-by: Stefan Roese <sr@denx.de>

---
+++ include/linux/of_mtd.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2012 Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
+ *
+ * OF helpers for mtd.
+ *
+ * This file is released under the GPLv2
+ */
+
+#ifndef __LINUX_OF_MTD_H
+#define __LINUX_OF_NET_H
+
+#ifdef CONFIG_OF_MTD
+#include <linux/of.h>
+extern const int of_get_nand_ecc_mode(struct device_node *np);
+int of_get_nand_bus_width(struct device_node *np);
+bool of_get_nand_on_flash_bbt(struct device_node *np);
+#endif
+
+#endif /* __LINUX_OF_MTD_H */
