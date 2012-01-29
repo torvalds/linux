fbae0f8912dc12b284433c05417ea76311205bbf
Russell King <rmk+kernel@arm.linux.org.uk>
2012-01-29 11:23:33 +0000

    ARM: sa11x0: neponset: don't static map neponset registers
    Now that we ioremap() the neponset register space, there's no need
    to static map the neponset registers.  Get rid of this static mapping.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-sa1100/neponset.c
+++ arch/arm/mach-sa1100/neponset.c
@@ -434,12 +434,7 @@ static int __init neponset_init(void)
 subsys_initcall(neponset_init);
 
 static struct map_desc neponset_io_desc[] __initdata = {
-	{	/* System Registers */
-		.virtual	=  0xf3000000,
-		.pfn		= __phys_to_pfn(0x10000000),
-		.length		= SZ_1M,
-		.type		= MT_DEVICE
-	}, {	/* SA-1111 */
+	{	/* SA-1111 */
 		.virtual	=  0xf4000000,
 		.pfn		= __phys_to_pfn(0x40000000),
 		.length		= SZ_1M,
