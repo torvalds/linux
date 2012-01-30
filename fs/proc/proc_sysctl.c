1347440db6f76ec5ae0af8d8558387f571a5e1dd
Dan Carpenter <dan.carpenter@oracle.com>
2012-01-30 16:40:29 +0300

    sysctl: fix memset parameters in setup_sysctl_set()
    The current code is a nop.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>

--- fs/proc/proc_sysctl.c
+++ fs/proc/proc_sysctl.c
@@ -1547,7 +1547,7 @@ void setup_sysctl_set(struct ctl_table_set *set,
 	struct ctl_table_root *root,
 	int (*is_seen)(struct ctl_table_set *))
 {
-	memset(set, sizeof(*set), 0);
+	memset(set, 0, sizeof(*set));
 	set->is_seen = is_seen;
 	init_header(&set->dir.header, root, set, NULL, root_table);
 }
