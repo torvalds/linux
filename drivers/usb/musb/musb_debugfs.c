6c2abcdd4f8e21ce686a26697de0ce198a16eaed
Felipe Balbi <balbi@ti.com>
2012-01-31 14:19:00 +0200

    usb: musb: debugfs: fix error check
    debugfs will return NULL on failure, so
    we must check for !ptr instead of IS_ERR(ptr).
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/musb/musb_debugfs.c
+++ drivers/usb/musb/musb_debugfs.c
@@ -242,22 +242,22 @@ int __devinit musb_init_debugfs(struct musb *musb)
 	int			ret;
 
 	root = debugfs_create_dir("musb", NULL);
-	if (IS_ERR(root)) {
-		ret = PTR_ERR(root);
+	if (!root) {
+		ret = -ENOMEM;
 		goto err0;
 	}
 
 	file = debugfs_create_file("regdump", S_IRUGO, root, musb,
 			&musb_regdump_fops);
-	if (IS_ERR(file)) {
-		ret = PTR_ERR(file);
+	if (!file) {
+		ret = -ENOMEM;
 		goto err1;
 	}
 
 	file = debugfs_create_file("testmode", S_IRUGO | S_IWUSR,
 			root, musb, &musb_test_mode_fops);
-	if (IS_ERR(file)) {
-		ret = PTR_ERR(file);
+	if (!file) {
+		ret = -ENOMEM;
 		goto err1;
 	}
 
