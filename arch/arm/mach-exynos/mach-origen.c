26e1451464c3b56fb57342e724f22d45c33292cf
Sachin Kamat <sachin.kamat@linaro.org>
2012-01-31 13:28:59 +0900

    ARM: EXYNOS: Add missing FIMC media device to Origen
    FIMC media device (md) registers all required video nodes.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-exynos/mach-origen.c
+++ arch/arm/mach-exynos/mach-origen.c
@@ -634,6 +634,7 @@ static struct platform_device *origen_devices[] __initdata = {
 	&s5p_device_fimc1,
 	&s5p_device_fimc2,
 	&s5p_device_fimc3,
+	&s5p_device_fimc_md,
 	&s5p_device_fimd0,
 	&s5p_device_g2d,
 	&s5p_device_hdmi,
