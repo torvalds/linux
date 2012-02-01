7414a1b8b2b99093d6a271a1220557e87dc1ae94
Rajeev KUMAR <rajeev-dlh.kumar@st.com>
2012-02-01 16:12:17 +0530

    dmaengine/dw_dmac: Hibernation support in dw_dmac
    The suspend and resume implementation is through dev_pm_ops in dmac. So
    in order to support hibernation, freeze, thaw, restore and poweroff
    features are required.
    
    Signed-off-by: Rajeev Kumar <rajeev-dlh.kumar@st.com>
    Acked-by: Viresh Kumar <viresh.kumar@st.com>
    Signed-off-by: Vinod Koul <vinod.koul@linux.intel.com>

--- drivers/dma/dw_dmac.c
+++ drivers/dma/dw_dmac.c
@@ -1562,6 +1562,10 @@ static int dw_resume_noirq(struct device *dev)
 static const struct dev_pm_ops dw_dev_pm_ops = {
 	.suspend_noirq = dw_suspend_noirq,
 	.resume_noirq = dw_resume_noirq,
+	.freeze_noirq = dw_suspend_noirq,
+	.thaw_noirq = dw_resume_noirq,
+	.restore_noirq = dw_resume_noirq,
+	.poweroff_noirq = dw_suspend_noirq,
 };
 
 static struct platform_driver dw_driver = {
