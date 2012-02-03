261cc5aa9e54aac633dcc3b6469739cb6e23e21a
Vinicius Costa Gomes <vinicius.gomes@openbossa.org>
2012-02-02 21:08:05 -0300

    Bluetooth: Add support for notifying userspace of new LTK's
    If we want to have proper pairing support over LE we need to
    inform userspace that a new LTK is available, so userspace
    can store that key permanently.
    
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@openbossa.org>
    Acked-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -1338,6 +1338,9 @@ int hci_add_ltk(struct hci_dev *hdev, bdaddr_t *bdaddr, u8 addr_type, u8 type,
 	if (!new_key)
 		return 0;
 
+	if (type & HCI_SMP_LTK)
+		mgmt_new_ltk(hdev, key, 1);
+
 	return 0;
 }
 
