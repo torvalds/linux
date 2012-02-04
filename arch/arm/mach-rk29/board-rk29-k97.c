9d1c572a1cb6e2c602e6c66c9a6e0037ea5563e5
yangyun <yangyun@rock-chips.com>
2012-02-04 15:45:10 +0800

    k97:alter gsensor orientation

--- arch/arm/mach-rk29/board-rk29-k97.c
+++ arch/arm/mach-rk29/board-rk29-k97.c
@@ -1021,7 +1021,7 @@ static struct mma8452_platform_data mma8452_info = {
   .model= 8452,
   .swap_xyz= 1,
   .swap_xy=0,
-  .orientation ={1,0,0,0,0,1,0,-1,0},
+  .orientation ={1,0,0,0,0,-1,0,-1,0},
   .init_platform_hw= mma8452_init_platform_hw,
 
 };
