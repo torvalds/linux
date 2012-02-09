733ba91cc0d5b0a3cc012431b8c5b354697b57c1
Tomas Winkler <tomas.winkler@intel.com>
2012-02-09 19:25:53 +0200

    staging/mei: update copyright year to 2012
    1. Update Copyright to 2012
    2. Also fix mei.h copyright format
       checkpaatch complained:
       WARNING: please, no spaces at the start of a line
    
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/mei/interrupt.c
+++ drivers/staging/mei/interrupt.c
@@ -1,7 +1,7 @@
 /*
  *
  * Intel Management Engine Interface (Intel MEI) Linux driver
- * Copyright (c) 2003-2011, Intel Corporation.
+ * Copyright (c) 2003-2012, Intel Corporation.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
