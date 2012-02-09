8cdde126aa60ced0d63ff137378e09dd01dfadda
Myron Stowe <mstowe@redhat.com>
2012-02-09 09:36:41 -0700

    ACPI: Fix logic for removing mappings in 'acpi_unmap'
    Make sure the removal of mappings uses the same logic that put the
    mappings in place.
    
    Signed-off-by: Myron Stowe <myron.stowe@redhat.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/osl.c
+++ drivers/acpi/osl.c
@@ -347,7 +347,7 @@ static void acpi_unmap(acpi_physical_address pg_off, void __iomem *vaddr)
 	unsigned long pfn;
 
 	pfn = pg_off >> PAGE_SHIFT;
-	if (page_is_ram(pfn))
+	if (should_use_kmap(pfn))
 		kunmap(pfn_to_page(pfn));
 	else
 		iounmap(vaddr);
