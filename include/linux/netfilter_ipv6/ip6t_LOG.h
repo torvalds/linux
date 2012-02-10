6939c33a757bd006c5e0b8b5fd429fc587a4d0f4
Richard Weinberger <richard@nod.at>
2012-02-10 23:10:52 +0100

    netfilter: merge ipt_LOG and ip6_LOG into xt_LOG
    ipt_LOG and ip6_LOG have a lot of common code, merge them
    to reduce duplicate code.
    
    Signed-off-by: Richard Weinberger <richard@nod.at>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/linux/netfilter_ipv6/ip6t_LOG.h
+++ include/linux/netfilter_ipv6/ip6t_LOG.h
@@ -1,6 +1,8 @@
 #ifndef _IP6T_LOG_H
 #define _IP6T_LOG_H
 
+#warning "Please update iptables, this file will be removed soon!"
+
 /* make sure not to change this without changing netfilter.h:NF_LOG_* (!) */
 #define IP6T_LOG_TCPSEQ		0x01	/* Log TCP sequence numbers */
 #define IP6T_LOG_TCPOPT		0x02	/* Log TCP options */
