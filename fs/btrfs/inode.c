12fc9d0923ca70ae8960bccebac09d5c12f8c4d4
Florian Albrechtskirchinger <falbrechtskirchinger@gmail.com>
2012-02-10 22:15:54 +0100

    btrfs: honor umask when creating subvol root
    Set the subvol root inode permissions based on the current umask.

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -6706,8 +6706,10 @@ int btrfs_create_subvol_root(struct btrfs_trans_handle *trans,
 	int err;
 	u64 index = 0;
 
-	inode = btrfs_new_inode(trans, new_root, NULL, "..", 2, new_dirid,
-				new_dirid, S_IFDIR | 0700, &index);
+	inode = btrfs_new_inode(trans, new_root, NULL, "..", 2,
+				new_dirid, new_dirid,
+				S_IFDIR | (~current_umask() & S_IRWXUGO),
+				&index);
 	if (IS_ERR(inode))
 		return PTR_ERR(inode);
 	inode->i_op = &btrfs_dir_inode_operations;
