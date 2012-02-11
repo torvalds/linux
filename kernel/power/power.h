6f585f750d792652f33b6e85b1ee205be4b5e572
Rafael J. Wysocki <rjw@sisk.pl>
2012-02-11 22:40:23 +0100

    PM / Sleep: Remove unnecessary label from suspend_freeze_processes()
    The Finish label in suspend_freeze_processes() is in fact unnecessary
    and makes the function look more complicated than it really is, so
    remove that label (along with a few empty lines).
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Acked-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>

--- kernel/power/power.h
+++ kernel/power/power.h
@@ -234,16 +234,14 @@ static inline int suspend_freeze_processes(void)
 	int error;
 
 	error = freeze_processes();
-
 	/*
 	 * freeze_processes() automatically thaws every task if freezing
 	 * fails. So we need not do anything extra upon error.
 	 */
 	if (error)
-		goto Finish;
+		return error;
 
 	error = freeze_kernel_threads();
-
 	/*
 	 * freeze_kernel_threads() thaws only kernel threads upon freezing
 	 * failure. So we have to thaw the userspace tasks ourselves.
@@ -251,7 +249,6 @@ static inline int suspend_freeze_processes(void)
 	if (error)
 		thaw_processes();
 
- Finish:
 	return error;
 }
 
