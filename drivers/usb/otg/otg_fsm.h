7e062c0f8888001d28cf74da0c3d16a651bd7489
Heikki Krogerus <heikki.krogerus@linux.intel.com>
2012-02-13 13:24:06 +0200

    usb: otg: fsl: Start using struct usb_otg
    Use struct usb_otg members with OTG specific functions instead
    of usb_phy members.
    
    Signed-off-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Acked-by: Li Yang <leoli@freescale.com>
    Reviewed-by: Marek Vasut <marek.vasut@gmail.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/otg/otg_fsm.h
+++ drivers/usb/otg/otg_fsm.h
@@ -82,7 +82,7 @@ struct otg_fsm {
 	int loc_sof;
 
 	struct otg_fsm_ops *ops;
-	struct usb_phy *transceiver;
+	struct usb_otg *otg;
 
 	/* Current usb protocol used: 0:undefine; 1:host; 2:client */
 	int protocol;
