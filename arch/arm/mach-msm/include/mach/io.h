b12e9ba59c83f7df846602b201b64e4ddf28ccee
Rob Herring <rob.herring@calxeda.com>
2012-02-13 13:27:24 -0600

    ARM: msm: use runtime ioremap hook
    Convert msm platforms to use run-time ioremap hook instead of the compile
    time hook.
    
    According to David Brown, only the msm7201 needed the ioremap hook.
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Tested-by: David Brown <davidb@codeaurora.org>
    Acked-by: David Brown <davidb@codeaurora.org>
    Cc: Daniel Walker <dwalker@fifo99.com>
    Cc: Bryan Huntsman <bryanh@codeaurora.org>

--- arch/arm/mach-msm/include/mach/io.h
+++ arch/arm/mach-msm/include/mach/io.h
@@ -18,11 +18,6 @@
 
 #define IO_SPACE_LIMIT 0xffffffff
 
-#define __arch_ioremap __msm_ioremap
-#define __arch_iounmap __iounmap
-
-void __iomem *__msm_ioremap(unsigned long phys_addr, size_t size, unsigned int mtype);
-
 #define __io(a)		__typesafe_io(a)
 #define __mem_pci(a)    (a)
 
