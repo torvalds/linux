c177aa98e5a7bbf71bc28baf0516896e3bb13f6e
Rob Herring <rob.herring@calxeda.com>
2012-02-13 13:24:15 -0600

    ARM: imx: convert to common runtime ioremap hook
    Convert i.MX platforms to use the common run-time ioremap hook instead of
    the imx specific hook.
    
    Also, move addr_in_module out of io.h.
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Cc: Sascha Hauer <kernel@pengutronix.de>

--- arch/arm/plat-mxc/include/mach/io.h
+++ arch/arm/plat-mxc/include/mach/io.h
@@ -14,23 +14,6 @@
 /* Allow IO space to be anywhere in the memory */
 #define IO_SPACE_LIMIT 0xffffffff
 
-#define __arch_ioremap __imx_ioremap
-#define __arch_iounmap __iounmap
-
-#define addr_in_module(addr, mod) \
-	((unsigned long)(addr) - mod ## _BASE_ADDR < mod ## _SIZE)
-
-extern void __iomem *(*imx_ioremap)(unsigned long, size_t, unsigned int);
-
-static inline void __iomem *
-__imx_ioremap(unsigned long phys_addr, size_t size, unsigned int mtype)
-{
-	if (imx_ioremap != NULL)
-		return imx_ioremap(phys_addr, size, mtype);
-	else
-		return __arm_ioremap(phys_addr, size, mtype);
-}
-
 /* io address mapping macro */
 #define __io(a)		__typesafe_io(a)
 
