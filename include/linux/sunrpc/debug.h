dbb9c2a22d32492544765e798386daa2d9da27d2
Chuck Lever <chuck.lever@oracle.com>
2012-02-15 16:35:08 -0500

    SUNRPC: Use KERN_DEFAULT for debugging printk's
    Our dprintk() debugging facility doesn't specify any verbosity level
    for it's printk() calls, but it should.
    
    The default verbosity for printk's is KERN_DEFAULT.  You might argue
    that these are debugging printk's and thus the verbosity should be
    KERN_DEBUG.  That would mean that to see NFS and SUNRPC debugging
    output an admin would also have to boost the syslog verbosity, which
    would be insufferably noisy.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- include/linux/sunrpc/debug.h
+++ include/linux/sunrpc/debug.h
@@ -54,7 +54,11 @@ extern unsigned int		nlm_debug;
 #undef ifdebug
 #ifdef RPC_DEBUG			
 # define ifdebug(fac)		if (unlikely(rpc_debug & RPCDBG_##fac))
-# define dfprintk(fac, args...)	do { ifdebug(fac) printk(args); } while(0)
+# define dfprintk(fac, args...)	\
+	do { \
+		ifdebug(fac) \
+			printk(KERN_DEFAULT args); \
+	} while (0)
 # define RPC_IFDEBUG(x)		x
 #else
 # define ifdebug(fac)		if (0)
