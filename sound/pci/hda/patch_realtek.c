ef8d60fb79614a86a82720dc2402631dbcafb315
Takashi Iwai <tiwai@suse.de>
2012-02-17 10:12:38 +0100

    ALSA: hda/realtek - Fix surround output regression on Acer Aspire 5935
    The previous fix for the speaker on Acer Aspire 59135 introduced
    another problem for surround outputs.  It changed the connections on
    the line-in/mic pins for limiting the routes, but it left the modified
    connections.  Thus wrong connection indices were written when set to
    4ch or 6ch mode.
    
    This patch fixes it by restoring the right connections just after
    parsing the tree but before the initialization.
    
    Bugzilla: https://bugzilla.kernel.org/show_bug.cgi?id=42740
    
    Cc: <stable@kernel.org> [v3.2+]
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -4441,12 +4441,20 @@ static void alc889_fixup_dac_route(struct hda_codec *codec,
 				   const struct alc_fixup *fix, int action)
 {
 	if (action == ALC_FIXUP_ACT_PRE_PROBE) {
+		/* fake the connections during parsing the tree */
 		hda_nid_t conn1[2] = { 0x0c, 0x0d };
 		hda_nid_t conn2[2] = { 0x0e, 0x0f };
 		snd_hda_override_conn_list(codec, 0x14, 2, conn1);
 		snd_hda_override_conn_list(codec, 0x15, 2, conn1);
 		snd_hda_override_conn_list(codec, 0x18, 2, conn2);
 		snd_hda_override_conn_list(codec, 0x1a, 2, conn2);
+	} else if (action == ALC_FIXUP_ACT_PROBE) {
+		/* restore the connections */
+		hda_nid_t conn[5] = { 0x0c, 0x0d, 0x0e, 0x0f, 0x26 };
+		snd_hda_override_conn_list(codec, 0x14, 5, conn);
+		snd_hda_override_conn_list(codec, 0x15, 5, conn);
+		snd_hda_override_conn_list(codec, 0x18, 5, conn);
+		snd_hda_override_conn_list(codec, 0x1a, 5, conn);
 	}
 }
 
