690fb11be34cc908ef895d16c6c1673df1b4667a
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-02-17 16:23:29 -0800

    spi: Mark spi_register_board_info() __devinit
    Some systems have SPI devices located on plugin modules which are
    enumerated at runtime as devices. The drivers for these plugin modules
    need to register their SPI devices at probe() time so want to be able
    to call spi_register_board_info() but that function is currently marked
    as __init rather than __devinit so this usage isn't legal. Change the
    annotation to __devinit to handle this.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -484,7 +484,7 @@ static void spi_match_master_to_boardinfo(struct spi_master *master,
  * The board info passed can safely be __initdata ... but be careful of
  * any embedded pointers (platform_data, etc), they're copied as-is.
  */
-int __init
+int __devinit
 spi_register_board_info(struct spi_board_info const *info, unsigned n)
 {
 	struct boardinfo *bi;
