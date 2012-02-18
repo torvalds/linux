09f05d8529ff4aa92311c1a55ce35ac98cb59b8c
Rabin Vincent <rabin@rab.in>
2012-02-18 17:52:41 +0100

    ARM: 7334/1: add jump label support
    Add the arch-specific code to support jump labels for ARM and Thumb-2.
    
    This code will only be activated on compilers that are capable of
    building it.  It has been tested with GCC 4.6 patched with the patch
    from GCC bug 48637.
    
    Cc: Jason Baron <jbaron@redhat.com>
    Signed-off-by: Rabin Vincent <rabin@rab.in>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/insn.h
+++ arch/arm/kernel/insn.h
@@ -1,6 +1,16 @@
 #ifndef __ASM_ARM_INSN_H
 #define __ASM_ARM_INSN_H
 
+static inline unsigned long
+arm_gen_nop(void)
+{
+#ifdef CONFIG_THUMB2_KERNEL
+	return 0xf3af8000; /* nop.w */
+#else
+	return 0xe1a00000; /* mov r0, r0 */
+#endif
+}
+
 unsigned long
 __arm_gen_branch(unsigned long pc, unsigned long addr, bool link);
 
