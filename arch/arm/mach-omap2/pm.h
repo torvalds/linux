7cb034f8179dfc2889c1f2341566ee68f648a324
NeilBrown <neilb@suse.de>
2012-02-19 13:29:31 +1100

    ARM: OMAP2+: remove some orphan function declarations.
    commit 2fd149645eb (ARM: OMAP2+: UART: Remove omap_uart_can_sleep and
    add pm_qos) removed omap_uart_can_sleep and omap3_can_sleep, but not
    their declarations.  So remove those now.
    
    Signed-off-by: NeilBrown <neilb@suse.de>
    Reviewed-by: Shubhrajyoti D <shubhrajyoti@ti.com>
    [khilman@ti.com: minor changelog edits]
    Signed-off-by: Kevin Hilman <khilman@ti.com>

--- arch/arm/mach-omap2/pm.h
+++ arch/arm/mach-omap2/pm.h
@@ -18,7 +18,6 @@
 extern void *omap3_secure_ram_storage;
 extern void omap3_pm_off_mode_enable(int);
 extern void omap_sram_idle(void);
-extern int omap3_can_sleep(void);
 extern int omap_set_pwrdm_state(struct powerdomain *pwrdm, u32 state);
 extern int omap3_idle_init(void);
 extern int omap4_idle_init(void);
