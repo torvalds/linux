645e5333ec9088da08a1db619561de9c93c70634
Greg Ungerer <gerg@uclinux.org>
2012-02-19 16:34:58 +1000

    m68knommu: make 528x CPU reset register addressing consistent
    If we make all MCF_RCR (CPU reset register) addressing consistent across all
    ColdFire CPU family members that use it then we will be able to remove the
    duplicated copies of the code that use it.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/platform/528x/config.c
+++ arch/m68k/platform/528x/config.c
@@ -63,7 +63,7 @@ static void __init m528x_fec_init(void)
 static void m528x_cpu_reset(void)
 {
 	local_irq_disable();
-	__raw_writeb(MCF_RCR_SWRESET, MCF_IPSBAR + MCF_RCR);
+	__raw_writeb(MCF_RCR_SWRESET, MCF_RCR);
 }
 
 /***************************************************************************/
