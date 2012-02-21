ae0e6544347d6e114b6fa7eea66ebd062e4d8858
Axel Lin <axel.lin@gmail.com>
2012-02-21 10:14:55 +0800

    regulator: Fix module desciption for tps65910 regulator
    Fix the module desciption and also update Kconfig to include supporting
    tps65911 chip.
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Acked-by: Laxman Dewangan <ldewangan@nvidia.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- drivers/regulator/tps65910-regulator.c
+++ drivers/regulator/tps65910-regulator.c
@@ -1235,6 +1235,6 @@ static void __exit tps65910_cleanup(void)
 module_exit(tps65910_cleanup);
 
 MODULE_AUTHOR("Graeme Gregory <gg@slimlogic.co.uk>");
-MODULE_DESCRIPTION("TPS6507x voltage regulator driver");
+MODULE_DESCRIPTION("TPS65910/TPS65911 voltage regulator driver");
 MODULE_LICENSE("GPL v2");
 MODULE_ALIAS("platform:tps65910-pmic");
