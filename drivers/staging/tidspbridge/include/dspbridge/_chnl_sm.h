18db4fe09886691a781f3d1e4589ede0239d8ee2
Omar Ramirez Luna <omar.ramirez@ti.com>
2012-02-21 20:46:20 -0600

    staging: tidspbridge: always compile dsp wdt code
    In order to detect it at runtime, we need the code handling wdt
    clock available at runtime to decide whether to enable or disable
    based on the baseimage symbols. Default timeout has been set to 5
    seconds.
    
    Downside is that we will lose the option to set a custom timeout
    for overflow, but than can be added (if needed) as part of debugfs.
    
    Signed-off-by: Omar Ramirez <omar.ramirez@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/tidspbridge/include/dspbridge/_chnl_sm.h
+++ drivers/staging/tidspbridge/include/dspbridge/_chnl_sm.h
@@ -99,14 +99,10 @@ struct shm {
 	struct opp_rqst_struct opp_request;
 	/* load monitor information structure */
 	struct load_mon_struct load_mon_info;
-#ifdef CONFIG_TIDSPBRIDGE_WDT3
 	/* Flag for WDT enable/disable F/I clocks */
 	u32 wdt_setclocks;
 	u32 wdt_overflow;	/* WDT overflow time */
 	char dummy[176];	/* padding to 256 byte boundary */
-#else
-	char dummy[184];	/* padding to 256 byte boundary */
-#endif
 	u32 shm_dbg_var[64];	/* shared memory debug variables */
 };
 
