2e5b5b3a1b7768c89fbfeca18e75f8ee377e924c
Hiroshi Shimamoto <h-shimamoto@ct.jp.nec.com>
2012-02-23 17:41:27 +0900

    sched: Clean up parameter passing of proc_sched_autogroup_set_nice()
    Pass nice as a value to proc_sched_autogroup_set_nice().
    
    No side effect is expected, and the variable err will be overwritten with
    the return value.
    
    Signed-off-by: Hiroshi Shimamoto <h-shimamoto@ct.jp.nec.com>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/4F45FBB7.5090607@ct.jp.nec.com
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2065,7 +2065,7 @@ extern void sched_autogroup_fork(struct signal_struct *sig);
 extern void sched_autogroup_exit(struct signal_struct *sig);
 #ifdef CONFIG_PROC_FS
 extern void proc_sched_autogroup_show_task(struct task_struct *p, struct seq_file *m);
-extern int proc_sched_autogroup_set_nice(struct task_struct *p, int *nice);
+extern int proc_sched_autogroup_set_nice(struct task_struct *p, int nice);
 #endif
 #else
 static inline void sched_autogroup_create_attach(struct task_struct *p) { }
