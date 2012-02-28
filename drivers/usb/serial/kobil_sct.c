8a9040b123b9fd325c38a985c109289d04349873
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-02-28 13:12:02 -0800

    USB: serial: kobil_sct.c: use module_usb_serial_driver
    This converts the kobil_sct.c driver to use the module_usb_serial_driver() call
    instead of having to have a module_init/module_exit function, saving a lot
    of duplicated code.
    
    CC: Johan Hovold <jhovold@gmail.com>
    CC: Jiri Slaby <jslaby@suse.cz>
    CC: Alan Stern <stern@rowland.harvard.edu>
    CC: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/serial/kobil_sct.c
+++ drivers/usb/serial/kobil_sct.c
@@ -683,25 +683,7 @@ static int kobil_ioctl(struct tty_struct *tty,
 	}
 }
 
-static int __init kobil_init(void)
-{
-	int retval;
-
-	retval = usb_serial_register_drivers(&kobil_driver, serial_drivers);
-	if (retval == 0)
-		printk(KERN_INFO KBUILD_MODNAME ": " DRIVER_VERSION ":"
-			       DRIVER_DESC "\n");
-	return retval;
-}
-
-
-static void __exit kobil_exit(void)
-{
-	usb_serial_deregister_drivers(&kobil_driver, serial_drivers);
-}
-
-module_init(kobil_init);
-module_exit(kobil_exit);
+module_usb_serial_driver(kobil_driver, serial_drivers);
 
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
