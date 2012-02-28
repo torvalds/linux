d2daea74cca96a9af28f55e361ae359e7027d26f
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-02-28 13:11:35 -0800

    USB: serial: belkin_sa.c: use module_usb_serial_driver
    This converts the belkin_sa.c driver to use the module_usb_serial_driver() call
    instead of having to have a module_init/module_exit function, saving a lot
    of duplicated code.
    
    CC: William Greathouse <wgreathouse@smva.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/serial/belkin_sa.c
+++ drivers/usb/serial/belkin_sa.c
@@ -524,26 +524,7 @@ exit:
 	return retval;
 }
 
-
-static int __init belkin_sa_init(void)
-{
-	int retval;
-
-	retval = usb_serial_register_drivers(&belkin_driver, serial_drivers);
-	if (retval == 0)
-		printk(KERN_INFO KBUILD_MODNAME ": " DRIVER_VERSION ":"
-			       DRIVER_DESC "\n");
-	return retval;
-}
-
-static void __exit belkin_sa_exit (void)
-{
-	usb_serial_deregister_drivers(&belkin_driver, serial_drivers);
-}
-
-
-module_init(belkin_sa_init);
-module_exit(belkin_sa_exit);
+module_usb_serial_driver(belkin_driver, serial_drivers);
 
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
