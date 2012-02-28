ef3d23de05254179b7f9e718eae276e5b67e1ba6
Andreas Regel <andreas.regel@gmx.de>
2012-02-28 14:40:44 -0300

    [media] stb0899: fixed reading of IF_AGC_GAIN register
    When reading IF_AGC_GAIN register a wrong value for the base address
    register was used (STB0899_DEMOD instead of STB0899_S2DEMOD). That
    lead to a wrong signal strength value on DVB-S2 transponders.
    
    Signed-off-by: Andreas Regel <andreas.regel@gmx.de>
    Acked-by: Klaus Schmidinger <Klaus.Schmidinger@tvdr.de>
    Cc: Manu Abraham <abraham.manu@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/stb0899_drv.c
+++ drivers/media/dvb/frontends/stb0899_drv.c
@@ -983,7 +983,7 @@ static int stb0899_read_signal_strength(struct dvb_frontend *fe, u16 *strength)
 		break;
 	case SYS_DVBS2:
 		if (internal->lock) {
-			reg = STB0899_READ_S2REG(STB0899_DEMOD, IF_AGC_GAIN);
+			reg = STB0899_READ_S2REG(STB0899_S2DEMOD, IF_AGC_GAIN);
 			val = STB0899_GETFIELD(IF_AGC_GAIN, reg);
 
 			*strength = stb0899_table_lookup(stb0899_dvbs2rf_tab, ARRAY_SIZE(stb0899_dvbs2rf_tab) - 1, val);
