b1ada6010e234d6c53bfcd5a617610c9284846b8
Anton Blanchard <anton@samba.org>
2012-02-29 21:09:53 +0000

    atomic: Allow atomic_inc_not_zero to be overridden
    We want to implement a ppc64 specific version of atomic_inc_not_zero
    so wrap it in an ifdef to allow it to be overridden.
    
    Signed-off-by: Anton Blanchard <anton@samba.org>
    Acked-by: Mike Frysinger <vapier@gentoo.org>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- include/linux/atomic.h
+++ include/linux/atomic.h
@@ -24,7 +24,9 @@ static inline int atomic_add_unless(atomic_t *v, int a, int u)
  * Atomically increments @v by 1, so long as @v is non-zero.
  * Returns non-zero if @v was non-zero, and zero otherwise.
  */
+#ifndef atomic_inc_not_zero
 #define atomic_inc_not_zero(v)		atomic_add_unless((v), 1, 0)
+#endif
 
 /**
  * atomic_inc_not_zero_hint - increment if not null
