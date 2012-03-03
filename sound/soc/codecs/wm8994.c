28e33269a71cb4104c2e0629b6a3ef7344436f93
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-03-03 00:10:02 +0000

    ASoC: wm8994: Don't bother updating the jackdet mode needlessly
    If we're not doing jackdet it's not needed.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm8994.c
+++ sound/soc/codecs/wm8994.c
@@ -686,6 +686,9 @@ static void wm1811_jackdet_set_mode(struct snd_soc_codec *codec, u16 mode)
 {
 	struct wm8994_priv *wm8994 = snd_soc_codec_get_drvdata(codec);
 
+	if (!wm8994->jackdet || !wm8994->jack_cb)
+		return;
+
 	if (wm8994->active_refcount)
 		mode = WM1811_JACKDET_MODE_AUDIO;
 
