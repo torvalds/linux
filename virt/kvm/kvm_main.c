bec87d6e34de459a559fc326aa4e56bee4ba7423
Alex Shi <alex.shi@intel.com>
2012-03-04 13:13:38 +0800

    KVM: use correct tlbs dirty type in cmpxchg
    Using 'int' type is not suitable for a 'long' object. So, correct it.
    
    Signed-off-by: Alex Shi <alex.shi@intel.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -203,7 +203,7 @@ static bool make_all_cpus_request(struct kvm *kvm, unsigned int req)
 
 void kvm_flush_remote_tlbs(struct kvm *kvm)
 {
-	int dirty_count = kvm->tlbs_dirty;
+	long dirty_count = kvm->tlbs_dirty;
 
 	smp_mb();
 	if (make_all_cpus_request(kvm, KVM_REQ_TLB_FLUSH))
