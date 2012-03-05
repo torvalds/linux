634830704d80ee0f544c16ea3c19dd2e819d2256
Mika Westerberg <mika.westerberg@linux.intel.com>
2012-03-05 15:01:03 -0800

    x86/mid/thermal: Add msic_thermal alias
    This device is called "msic_thermal" instead of "msic_sensor" on
    actual boards so rename it.
    
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    [ Updated to rename rather than add an entry as er discussion with Mika & Durgadoss R]
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Acked-by: H. Peter Anvin <hpa@zytor.com>
    Cc: Matthew Garrett <mjg@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Link: http://lkml.kernel.org/n/tip-gyrbptvkozsbp2yk3ssu084o@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- drivers/platform/x86/intel_mid_thermal.c
+++ drivers/platform/x86/intel_mid_thermal.c
@@ -543,7 +543,7 @@ static int mid_thermal_remove(struct platform_device *pdev)
 	return configure_adc(0);
 }
 
-#define DRIVER_NAME "msic_sensor"
+#define DRIVER_NAME "msic_thermal"
 
 static const struct platform_device_id therm_id_table[] = {
 	{ DRIVER_NAME, 1 },
