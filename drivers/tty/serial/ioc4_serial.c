4da2405606d47ca767e0c6ba556f127cfa2181d0
Jiri Slaby <jslaby@suse.cz>
2012-03-05 14:52:08 +0100

    TTY: serial, use atomic_inc_return in ioc4_serial
    We want to know the value of the atomic variable in intr_connect after
    the increment. But atomic_inc doesn't, per definition, return the
    value.  It is just a pure coincidence that ia64 defines atomic_inc as
    atomic_inc_return.
    
    So fix this mistake by using atomic_inc_return properly.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/ioc4_serial.c
+++ drivers/tty/serial/ioc4_serial.c
@@ -975,7 +975,7 @@ intr_connect(struct ioc4_soft *soft, int type,
 	BUG_ON(!((type == IOC4_SIO_INTR_TYPE)
 	       || (type == IOC4_OTHER_INTR_TYPE)));
 
-	i = atomic_inc(&soft-> is_intr_type[type].is_num_intrs) - 1;
+	i = atomic_inc_return(&soft-> is_intr_type[type].is_num_intrs) - 1;
 	BUG_ON(!(i < MAX_IOC4_INTR_ENTS || (printk("i %d\n", i), 0)));
 
 	/* Save off the lower level interrupt handler */
