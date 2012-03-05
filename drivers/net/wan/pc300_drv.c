ac4e016df4bf57cdb100170c5cc057a15a9c96bd
Jiri Slaby <jslaby@suse.cz>
2012-03-05 14:52:04 +0100

    NET: pc300, show version info from module init
    Again, no need to do that from the pci probe function.
    
    Hmm, I noticed this driver is marked as BROKEN. Won't touch it more,
    it has to be converted to dynamic tty driver allocation first.
    
    Perhaps it is time to move it to staging?
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Cc: netdev@vger.kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/wan/pc300_drv.c
+++ drivers/net/wan/pc300_drv.c
@@ -3231,7 +3231,7 @@ static void plx_init(pc300_t * card)
 
 }
 
-static inline void show_version(void)
+static void show_version(void)
 {
 	char *rcsvers, *rcsdate, *tmp;
 
@@ -3412,15 +3412,9 @@ static void cpc_init_card(pc300_t * card)
 static int __devinit
 cpc_init_one(struct pci_dev *pdev, const struct pci_device_id *ent)
 {
-	static int first_time = 1;
 	int err, eeprom_outdated = 0;
 	u16 device_id;
 	pc300_t *card;
-
-	if (first_time) {
-		first_time = 0;
-		show_version();
-	}
 
 	if ((err = pci_enable_device(pdev)) < 0)
 		return err;
@@ -3657,6 +3651,7 @@ static struct pci_driver cpc_driver = {
 
 static int __init cpc_init(void)
 {
+	show_version();
 	return pci_register_driver(&cpc_driver);
 }
 
