9a9a232a9295deb3b6b5f4ce4290a7d05ff061fa
Yevgeny Petrilin <yevgenyp@mellanox.co.il>
2012-03-06 04:04:47 +0000

    net/mlx4: fixing sparse warnings for not declared, functions
    The SET_PORT functions are implemented in port.c, which is part
    of mlx4_core, these functions are exported. The functions are in use by
    the mlx4_en module (were originally part of mlx4_en).
    Their declaration remained in mlx4_en module, moving the declaration to the right location.
    
    Signed-off-by: Yevgeny Petrilin <yevgenyp@mellanox.co.il>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlx4/mlx4_en.h
+++ drivers/net/ethernet/mellanox/mlx4/mlx4_en.h
@@ -553,10 +553,6 @@ void mlx4_en_rx_irq(struct mlx4_cq *mcq);
 
 int mlx4_SET_MCAST_FLTR(struct mlx4_dev *dev, u8 port, u64 mac, u64 clear, u8 mode);
 int mlx4_SET_VLAN_FLTR(struct mlx4_dev *dev, struct mlx4_en_priv *priv);
-int mlx4_SET_PORT_general(struct mlx4_dev *dev, u8 port, int mtu,
-			  u8 pptx, u8 pfctx, u8 pprx, u8 pfcrx);
-int mlx4_SET_PORT_qpn_calc(struct mlx4_dev *dev, u8 port, u32 base_qpn,
-			   u8 promisc);
 
 int mlx4_en_DUMP_ETH_STATS(struct mlx4_en_dev *mdev, u8 port, u8 reset);
 int mlx4_en_QUERY_PORT(struct mlx4_en_dev *mdev, u8 port);
