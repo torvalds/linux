bc3dd33cef001e61da0f71b5d8f918b3dbaa4a94
Andre Guedes <andre.guedes@openbossa.org>
2012-03-06 19:37:06 -0300

    Bluetooth: Check FINDING state in interleaved discovery
    In order to do interleaved discovery we should be in DISCOVERY_
    FINDING state. Otherwise, discovery should be stopped.
    
    Signed-off-by: Andre Guedes <andre.guedes@openbossa.org>
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -1098,7 +1098,8 @@ static void hci_cc_le_set_scan_enable(struct hci_dev *hdev,
 
 		schedule_delayed_work(&hdev->adv_work, ADV_CLEAR_TIMEOUT);
 
-		if (hdev->discovery.type == DISCOV_TYPE_INTERLEAVED) {
+		if (hdev->discovery.type == DISCOV_TYPE_INTERLEAVED &&
+		    hdev->discovery.state == DISCOVERY_FINDING) {
 			mgmt_interleaved_discovery(hdev);
 		} else {
 			hci_dev_lock(hdev);
