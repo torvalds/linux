9587190107d0c0cbaccbf7bf6b0245d29095a9ae
Nadav Har'El <nyh@math.technion.ac.il>
2012-03-06 16:39:22 +0200

    KVM: nVMX: Fix erroneous exception bitmap check
    The code which checks whether to inject a pagefault to L1 or L2 (in
    nested VMX) was wrong, incorrect in how it checked the PF_VECTOR bit.
    Thanks to Dan Carpenter for spotting this.
    
    Signed-off-by: Nadav Har'El <nyh@il.ibm.com>
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -1664,7 +1664,7 @@ static int nested_pf_handled(struct kvm_vcpu *vcpu)
 	struct vmcs12 *vmcs12 = get_vmcs12(vcpu);
 
 	/* TODO: also check PFEC_MATCH/MASK, not just EB.PF. */
-	if (!(vmcs12->exception_bitmap & PF_VECTOR))
+	if (!(vmcs12->exception_bitmap & (1u << PF_VECTOR)))
 		return 0;
 
 	nested_vmx_vmexit(vcpu);
