8d2a5e6ee37f205b3d76c223d4f3f88ba1d06220
Dave Chinner <dchinner@redhat.com>
2012-03-07 04:50:19 +0000

    xfs: clean up minor sparse warnings
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Ben Myers <bpm@sgi.com>

--- fs/xfs/xfs_iops.c
+++ fs/xfs/xfs_iops.c
@@ -50,12 +50,15 @@
 #include <linux/fiemap.h>
 #include <linux/slab.h>
 
-int xfs_initxattrs(struct inode *inode, const struct xattr *xattr_array,
-		   void *fs_info)
+static int
+xfs_initxattrs(
+	struct inode		*inode,
+	const struct xattr	*xattr_array,
+	void			*fs_info)
 {
-	const struct xattr *xattr;
-	struct xfs_inode *ip = XFS_I(inode);
-	int error = 0;
+	const struct xattr	*xattr;
+	struct xfs_inode	*ip = XFS_I(inode);
+	int			error = 0;
 
 	for (xattr = xattr_array; xattr->name != NULL; xattr++) {
 		error = xfs_attr_set(ip, xattr->name, xattr->value,
