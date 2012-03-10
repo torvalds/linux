310fa7a36722017088af123043ebd231cd6bc559
Al Viro <viro@zeniv.linux.org.uk>
2012-03-10 17:07:28 -0500

    restore smp_mb() in unlock_new_inode()
    wait_on_inode() doesn't have ->i_lock
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/inode.c
+++ fs/inode.c
@@ -965,6 +965,7 @@ void unlock_new_inode(struct inode *inode)
 	spin_lock(&inode->i_lock);
 	WARN_ON(!(inode->i_state & I_NEW));
 	inode->i_state &= ~I_NEW;
+	smp_mb();
 	wake_up_bit(&inode->i_state, __I_NEW);
 	spin_unlock(&inode->i_lock);
 }
