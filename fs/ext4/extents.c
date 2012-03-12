471320c7569290aeed04f9c730d92b67b9acb53d
Theodore Ts'o <tytso@mit.edu>
2012-03-11 23:30:16 -0400

    ext4: check for zero length extent
    commit 31d4f3a2f3c73f279ff96a7135d7202ef6833f12 upstream.
    
    Explicitly test for an extent whose length is zero, and flag that as a
    corrupted extent.
    
    This avoids a kernel BUG_ON assertion failure.
    
    Tested: Without this patch, the file system image found in
    tests/f_ext_zero_len/image.gz in the latest e2fsprogs sources causes a
    kernel panic.  With this patch, an ext4 file system error is noted
    instead, and the file system is marked as being corrupted.
    
    https://bugzilla.kernel.org/show_bug.cgi?id=42859
    
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -341,6 +341,8 @@ static int ext4_valid_extent(struct inode *inode, struct ext4_extent *ext)
 	ext4_fsblk_t block = ext4_ext_pblock(ext);
 	int len = ext4_ext_get_actual_len(ext);
 
+	if (len == 0)
+		return 0;
 	return ext4_data_block_valid(EXT4_SB(inode->i_sb), block, len);
 }
 
