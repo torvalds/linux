bb6fa8b275e132b1e9319dbab94211543a0b7bd3
H. Peter Anvin <hpa@zytor.com>
2012-03-13 22:44:41 -0700

    x32: Fix stupid ia32/x32 inversion in the siginfo format
    Fix a stray ! which flipped the sense if we were generating a signal
    frame for ia32 vs. x32.
    
    Introduced in:
    
    e7084fd5 x32: Switch to a 64-bit clock_t
    
    Reported-by: H. J. Lu <hjl.tools@gmail.com>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>
    Cc: Gregory M. Lueck <gregory.m.lueck@intel.com>
    Link: http://lkml.kernel.org/r/1329696488-16970-1-git-send-email-hpa@zytor.com

--- arch/x86/ia32/ia32_signal.c
+++ arch/x86/ia32/ia32_signal.c
@@ -37,7 +37,7 @@
 int copy_siginfo_to_user32(compat_siginfo_t __user *to, siginfo_t *from)
 {
 	int err = 0;
-	bool ia32 = !is_ia32_task();
+	bool ia32 = is_ia32_task();
 
 	if (!access_ok(VERIFY_WRITE, to, sizeof(compat_siginfo_t)))
 		return -EFAULT;
