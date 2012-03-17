14b5842ebf552d242b0238f70d0730a564065fb0
Tobias Klauser <tklauser@distanz.ch>
2012-03-16 23:31:25 -0700

    Input: altera_ps2 - use of_match_ptr()
    Instead of having to define the match table to NULL if CONFIG_OF isn't
    set, use the of_match_ptr() macro which will do this for us.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/serio/altera_ps2.c
+++ drivers/input/serio/altera_ps2.c
@@ -180,8 +180,6 @@ static const struct of_device_id altera_ps2_match[] = {
 	{},
 };
 MODULE_DEVICE_TABLE(of, altera_ps2_match);
-#else /* CONFIG_OF */
-#define altera_ps2_match NULL
 #endif /* CONFIG_OF */
 
 /*
@@ -193,7 +191,7 @@ static struct platform_driver altera_ps2_driver = {
 	.driver	= {
 		.name	= DRV_NAME,
 		.owner	= THIS_MODULE,
-		.of_match_table = altera_ps2_match,
+		.of_match_table = of_match_ptr(altera_ps2_match),
 	},
 };
 module_platform_driver(altera_ps2_driver);
