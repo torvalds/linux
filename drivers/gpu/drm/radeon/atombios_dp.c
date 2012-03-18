abc8113fe56948858ff44f4b1795875be0cb5e99
Dave Airlie <airlied@redhat.com>
2012-03-18 10:10:50 +0000

    drm/radeon: dp only enable enhanced framing if sink supports.
    DisplayPort spec v1.1a, Table 2-52.
    
    Same commit to Intel by ajax in 9962c9252e46eda7058067cbe73bdf1ed74b0d37
    
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/atombios_dp.c
+++ drivers/gpu/drm/radeon/atombios_dp.c
@@ -746,7 +746,8 @@ static int radeon_dp_link_train_init(struct radeon_dp_link_train_info *dp_info)
 
 	/* set the lane count on the sink */
 	tmp = dp_info->dp_lane_count;
-	if (dp_info->dpcd[0] >= 0x11)
+	if (dp_info->dpcd[DP_DPCD_REV] >= 0x11 &&
+	    dp_info->dpcd[DP_MAX_LANE_COUNT] & DP_ENHANCED_FRAME_CAP)
 		tmp |= DP_LANE_COUNT_ENHANCED_FRAME_EN;
 	radeon_write_dpcd_reg(dp_info->radeon_connector, DP_LANE_COUNT_SET, tmp);
 
