8c3ec84102d171a24f050a086bfc546e9de93f9f
Stephen Warren <swarren@wwwdotorg.org>
2012-03-19 13:57:13 -0600

    ARM: tegra: Fix device tree AUXDATA for USB/EHCI
    Commit 4a53f4e "USB: ehci-tegra: add probing through device tree" added
    AUXDATA for Tegra's USB/EHCI controller. However, it pointed the platform
    data at a location containing the address of the intended platform data,
    rather than the platform data itself. This change fixes that.
    
    Signed-off-by: Stephen Warren <swarren@wwwdotorg.org>
    Cc: <stable@vger.kernel.org> # 3.3

--- arch/arm/mach-tegra/devices.c
+++ arch/arm/mach-tegra/devices.c
@@ -23,7 +23,6 @@
 #include <linux/fsl_devices.h>
 #include <linux/serial_8250.h>
 #include <linux/i2c-tegra.h>
-#include <linux/platform_data/tegra_usb.h>
 #include <asm/pmu.h>
 #include <mach/irqs.h>
 #include <mach/iomap.h>
@@ -446,18 +445,18 @@ static struct tegra_ulpi_config tegra_ehci2_ulpi_phy_config = {
 	.clk = "cdev2",
 };
 
-static struct tegra_ehci_platform_data tegra_ehci1_pdata = {
+struct tegra_ehci_platform_data tegra_ehci1_pdata = {
 	.operating_mode = TEGRA_USB_OTG,
 	.power_down_on_bus_suspend = 1,
 };
 
-static struct tegra_ehci_platform_data tegra_ehci2_pdata = {
+struct tegra_ehci_platform_data tegra_ehci2_pdata = {
 	.phy_config = &tegra_ehci2_ulpi_phy_config,
 	.operating_mode = TEGRA_USB_HOST,
 	.power_down_on_bus_suspend = 1,
 };
 
-static struct tegra_ehci_platform_data tegra_ehci3_pdata = {
+struct tegra_ehci_platform_data tegra_ehci3_pdata = {
 	.operating_mode = TEGRA_USB_HOST,
 	.power_down_on_bus_suspend = 1,
 };
