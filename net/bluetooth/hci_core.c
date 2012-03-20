7537e5c3064c5a5751a7b26aba82ad83c5e89190
Andre Guedes <aguedespe@gmail.com>
2012-03-20 00:13:38 -0300

    Bluetooth: Replace EPERM by EALREADY in hci_cancel_inquiry
    We should return -EALREADY in hci_cancel_inquiry since it is more
    suitable than -EPERM error code.
    
    Signed-off-by: Andre Guedes <aguedespe@gmail.com>
    Signed-off-by: Gustavo Padovan <gustavo@padovan.org>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -2956,7 +2956,7 @@ int hci_cancel_inquiry(struct hci_dev *hdev)
 	BT_DBG("%s", hdev->name);
 
 	if (!test_bit(HCI_INQUIRY, &hdev->flags))
-		return -EPERM;
+		return -EALREADY;
 
 	return hci_send_cmd(hdev, HCI_OP_INQUIRY_CANCEL, 0, NULL);
 }
