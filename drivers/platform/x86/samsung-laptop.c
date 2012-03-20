8522944085ffd83af129ffce06e3a9f34635391c
David Rientjes <rientjes@google.com>
2012-03-20 09:53:05 +0100

    drivers, samsung-laptop: fix initialization of sabi_data in sabi_set_commandb
    Fields d0, d1, d2, and d3 are members of an anonymous struct inside an
    anonymous union inside struct sabi_data.  Initialization must be done by
    wrapping the anonymous union and structs with brackets to avoid a build
    error:
    
    drivers/platform/x86/samsung-laptop.c: In function ‘sabi_set_commandb’:
    drivers/platform/x86/samsung-laptop.c:433: error: unknown field ‘d0’ specified in initializer
    drivers/platform/x86/samsung-laptop.c:433: warning: missing braces around initializer
    drivers/platform/x86/samsung-laptop.c:433: warning: (near initialization for ‘in.<anonymous>’)
    ...
    
    Signed-off-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Corentin Chary <corentincj@iksaif.net>
    Signed-off-by: Matthew Garrett <mjg@redhat.com>

--- drivers/platform/x86/samsung-laptop.c
+++ drivers/platform/x86/samsung-laptop.c
@@ -430,7 +430,7 @@ exit:
 static int sabi_set_commandb(struct samsung_laptop *samsung,
 			     u16 command, u8 data)
 {
-	struct sabi_data in = { .d0 = 0, .d1 = 0, .d2 = 0, .d3 = 0 };
+	struct sabi_data in = { { { .d0 = 0, .d1 = 0, .d2 = 0, .d3 = 0 } } };
 
 	in.data[0] = data;
 	return sabi_command(samsung, command, &in, NULL);
