397a21f24d455982a8a6f9bc11b5f3326ce3c6ef
Denys Vlasenko <vda.linux@googlemail.com>
2012-03-23 15:01:54 -0700

    kernel/exit.c: if init dies, log a signal which killed it, if any
    I just received another user's pleas for help when their init
    mysteriously died.  I again explained that they need to check whether it
    died because of bad instruction, a segv, or something else.  Which was
    an annoying detour into writing a trivial C program to spawn his init
    and print its exit code:
    
      http://lists.busybox.net/pipermail/busybox/2012-January/077172.html
    
    I hear you saying "just test it under /bin/sh".  Well, the crashing init
    _was_ /bin/sh.
    
    Which prompted me to make kernel do this first step automatically.  We can
    print exit code, which makes it possible to see that death was from e.g.
    SIGILL without writing test programs.
    
    [akpm@linux-foundation.org: add 0x to hex number output]
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>
    Acked-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/exit.c
+++ kernel/exit.c
@@ -711,8 +711,11 @@ static struct task_struct *find_new_reaper(struct task_struct *father)
 
 	if (unlikely(pid_ns->child_reaper == father)) {
 		write_unlock_irq(&tasklist_lock);
-		if (unlikely(pid_ns == &init_pid_ns))
-			panic("Attempted to kill init!");
+		if (unlikely(pid_ns == &init_pid_ns)) {
+			panic("Attempted to kill init! exitcode=0x%08x\n",
+				father->signal->group_exit_code ?:
+					father->exit_code);
+		}
 
 		zap_pid_ns_processes(pid_ns);
 		write_lock_irq(&tasklist_lock);
