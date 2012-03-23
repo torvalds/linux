a7103b99e4cc00b9d40f6bad77389f9e2341820a
Jeff Layton <jlayton@redhat.com>
2012-03-23 14:40:56 -0400

    cifs: fix up get_numpages
    Use DIV_ROUND_UP. Also, PAGE_SIZE is more appropriate here since these
    aren't pagecache pages.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Reviewed-by: Pavel Shilovsky <piastry@etersoft.ru>

--- fs/cifs/file.c
+++ fs/cifs/file.c
@@ -2071,9 +2071,7 @@ size_t get_numpages(const size_t wsize, const size_t len, size_t *cur_len)
 	size_t clen;
 
 	clen = min_t(const size_t, len, wsize);
-	num_pages = clen / PAGE_CACHE_SIZE;
-	if (clen % PAGE_CACHE_SIZE)
-		num_pages++;
+	num_pages = DIV_ROUND_UP(clen, PAGE_SIZE);
 
 	if (cur_len)
 		*cur_len = clen;
