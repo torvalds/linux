db70b04407a63668e5fee773f76f90367492fd25
Daniel Lezcano <daniel.lezcano@linaro.org>
2012-03-26 14:51:27 +0200

    cpuidle: remove useless array definition in cpuidle_structure
    All the modules name are ro-data, it is never copied to the array.
    
    eg.
    
    static struct cpuidle_driver intel_idle_driver = {
            .name = "intel_idle",
            .owner = THIS_MODULE,
    };
    
    It safe to assign the pointer of this ro-data to a const char *.
    By this way we save 12 bytes.
    
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Acked-by: Deepthi Dharwar <deepthi@linux.vnet.ibm.com>
    Tested-by: Deepthi Dharwar <deepthi@linux.vnet.ibm.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -120,7 +120,7 @@ static inline int cpuidle_get_last_residency(struct cpuidle_device *dev)
  ****************************/
 
 struct cpuidle_driver {
-	char			name[CPUIDLE_NAME_LEN];
+	const char		*name;
 	struct module 		*owner;
 
 	unsigned int		power_specified:1;
