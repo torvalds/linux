e3176ca2769e420f64eba4b093bbddea6d7a89c3
Ilya Dryomov <idryomov@gmail.com>
2012-03-27 17:09:16 +0300

    Btrfs: stop silently switching single chunks to raid0 on balance
    This has been causing a lot of confusion for quite a while now and a lot
    of users were surprised by this (some of them were even stuck in a
    ENOSPC situation which they couldn't easily get out of).  The addition
    of restriper gives users a clear choice between raid0 and drive concat
    setup so there's absolutely no excuse for us to keep doing this.
    
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- fs/btrfs/extent-tree.c
+++ fs/btrfs/extent-tree.c
@@ -6941,7 +6941,6 @@ static u64 update_block_group_flags(struct btrfs_root *root, u64 flags)
 		if (flags & (BTRFS_BLOCK_GROUP_RAID1 |
 			     BTRFS_BLOCK_GROUP_RAID10))
 			return stripped | BTRFS_BLOCK_GROUP_DUP;
-		return flags;
 	} else {
 		/* they already had raid on here, just return */
 		if (flags & stripped)
@@ -6954,9 +6953,9 @@ static u64 update_block_group_flags(struct btrfs_root *root, u64 flags)
 		if (flags & BTRFS_BLOCK_GROUP_DUP)
 			return stripped | BTRFS_BLOCK_GROUP_RAID1;
 
-		/* turn single device chunks into raid0 */
-		return stripped | BTRFS_BLOCK_GROUP_RAID0;
+		/* this is drive concat, leave it alone */
 	}
+
 	return flags;
 }
 
