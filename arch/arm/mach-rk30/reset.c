414554aa70c96258ef6ab0e5c015ce94dc73bc18
kfx <kfx@rock-chips.com>
2012-03-28 17:10:04 +0800

    rk30: reboot: parameter(loader,recovery)

--- arch/arm/mach-rk30/reset.c
+++ arch/arm/mach-rk30/reset.c
@@ -1,11 +1,23 @@
 #include <linux/io.h>
 #include <linux/kernel.h>
 #include <mach/system.h>
+#include <linux/string.h>
 #include <mach/cru.h>
 #include <mach/iomux.h>
+#include <mach/loader.h>
 
 static void rk30_arch_reset(char mode, const char *cmd)
 {
+	unsigned int reason = 0;
+
+	if(cmd) {
+		if (!strcmp(cmd, "loader") || !strcmp(cmd, "bootloader")) 
+			reason = SYS_LOADER_REBOOT_FLAG + BOOT_LOADER;
+		else if(!strcmp(cmd, "recovery"))
+			reason = SYS_LOADER_REBOOT_FLAG + BOOT_RECOVER;
+	}
+	writel_relaxed(reason, RK30_PMU_PHYS + 0x40);  //PMU_OS_REG0
+	dsb();
 	/* disable remap */
 	writel_relaxed(1 << (12 + 16), RK30_GRF_BASE + GRF_SOC_CON0);
 	dsb();
