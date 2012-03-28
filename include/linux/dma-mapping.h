613c4578d4079a14dbee76ef7e0c80f635522fe3
Marek Szyprowski <m.szyprowski@samsung.com>
2012-03-28 16:36:27 +0200

    common: dma-mapping: introduce generic alloc() and free() methods
    Introduce new generic alloc and free methods with attributes argument.
    
    Existing alloc_coherent and free_coherent can be implemented on top of the
    new calls with NULL attributes argument. Later also dma_alloc_non_coherent
    can be implemented using DMA_ATTR_NONCOHERENT attribute as well as
    dma_alloc_writecombine with separate DMA_ATTR_WRITECOMBINE attribute.
    
    This way the drivers will get more generic, platform independent way of
    allocating dma buffers with specific parameters.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Acked-by: Kyungmin Park <kyungmin.park@samsung.com>
    Reviewed-by: David Gibson <david@gibson.dropbear.ud.au>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -13,6 +13,12 @@ struct dma_map_ops {
 				dma_addr_t *dma_handle, gfp_t gfp);
 	void (*free_coherent)(struct device *dev, size_t size,
 			      void *vaddr, dma_addr_t dma_handle);
+	void* (*alloc)(struct device *dev, size_t size,
+				dma_addr_t *dma_handle, gfp_t gfp,
+				struct dma_attrs *attrs);
+	void (*free)(struct device *dev, size_t size,
+			      void *vaddr, dma_addr_t dma_handle,
+			      struct dma_attrs *attrs);
 	dma_addr_t (*map_page)(struct device *dev, struct page *page,
 			       unsigned long offset, size_t size,
 			       enum dma_data_direction dir,
