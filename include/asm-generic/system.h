5d1250660a476c588361fb5cb9cbfa52768540c2
David Howells <dhowells@redhat.com>
2012-03-28 18:30:03 +0100

    Split arch_align_stack() out from asm-generic/system.h
    Split arch_align_stack() out from asm-generic/system.h into its own header of
    asm-generic/exec.h as part of the asm/system.h disintegration.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

--- include/asm-generic/system.h
+++ include/asm-generic/system.h
@@ -1,23 +1,5 @@
-/* Generic system definitions, based on MN10300 definitions.
- *
- * It should be possible to use these on really simple architectures,
- * but it serves more as a starting point for new ports.
- *
- * Copyright (C) 2007 Red Hat, Inc. All Rights Reserved.
- * Written by David Howells (dhowells@redhat.com)
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public Licence
- * as published by the Free Software Foundation; either version
- * 2 of the Licence, or (at your option) any later version.
- */
-#ifndef __ASM_GENERIC_SYSTEM_H
-#define __ASM_GENERIC_SYSTEM_H
-
+/* FILE TO BE DELETED. DO NOT ADD STUFF HERE! */
 #include <asm/barrier.h>
 #include <asm/cmpxchg.h>
+#include <asm/exec.h>
 #include <asm/switch_to.h>
-
-#define arch_align_stack(x) (x)
-
-#endif /* __ASM_GENERIC_SYSTEM_H */
