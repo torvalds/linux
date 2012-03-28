158bc507c2f53109bed51c806acf5bfd48d95f1a
David Howells <dhowells@redhat.com>
2012-03-28 18:30:03 +0100

    Split the switch_to() wrapper out of asm-generic/system.h
    Split the switch_to() wrapper out of asm-generic/system.h into its own
    asm-generic/system.h as part of the asm/system.h disintegration.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

--- include/asm-generic/switch_to.h
+++ include/asm-generic/switch_to.h
@@ -1,4 +1,4 @@
-/* Generic system definitions, based on MN10300 definitions.
+/* Generic task switch macro wrapper, based on MN10300 definitions.
  *
  * It should be possible to use these on really simple architectures,
  * but it serves more as a starting point for new ports.
@@ -11,13 +11,20 @@
  * as published by the Free Software Foundation; either version
  * 2 of the Licence, or (at your option) any later version.
  */
-#ifndef __ASM_GENERIC_SYSTEM_H
-#define __ASM_GENERIC_SYSTEM_H
+#ifndef __ASM_GENERIC_SWITCH_TO_H
+#define __ASM_GENERIC_SWITCH_TO_H
 
-#include <asm/barrier.h>
-#include <asm/cmpxchg.h>
-#include <asm/switch_to.h>
+#include <linux/thread_info.h>
 
-#define arch_align_stack(x) (x)
+/*
+ * Context switching is now performed out-of-line in switch_to.S
+ */
+extern struct task_struct *__switch_to(struct task_struct *,
+				       struct task_struct *);
 
-#endif /* __ASM_GENERIC_SYSTEM_H */
+#define switch_to(prev, next, last)					\
+	do {								\
+		((last) = __switch_to((prev), (next)));			\
+	} while (0)
+
+#endif /* __ASM_GENERIC_SWITCH_TO_H */
