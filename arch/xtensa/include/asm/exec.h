f9aa7e1882f3ceec919b30f64a2ce6e66a2571b1
David Howells <dhowells@redhat.com>
2012-03-28 18:30:03 +0100

    Disintegrate asm/system.h for Xtensa
    Disintegrate asm/system.h for Xtensa.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: Chris Zankel <chris@zankel.net>

--- arch/xtensa/include/asm/exec.h
+++ arch/xtensa/include/asm/exec.h
@@ -1,6 +1,4 @@
 /*
- * include/asm-xtensa/setup.h
- *
  * This file is subject to the terms and conditions of the GNU General Public
  * License.  See the file "COPYING" in the main directory of this archive
  * for more details.
@@ -8,11 +6,9 @@
  * Copyright (C) 2001 - 2005 Tensilica Inc.
  */
 
-#ifndef _XTENSA_SETUP_H
-#define _XTENSA_SETUP_H
+#ifndef _XTENSA_EXEC_H
+#define _XTENSA_EXEC_H
 
-#define COMMAND_LINE_SIZE	256
+#define arch_align_stack(x) (x)
 
-extern void set_except_vector(int n, void *addr);
-
-#endif
+#endif /* _XTENSA_EXEC_H */
