081b9f69e5a88cff1dd5fe1cbcf77f8017961797
黄涛 <huangtao@rock-chips.com>
2012-03-28 09:53:21 +0800

    rk30: reboot use the second global software reset

--- arch/arm/mach-rk30/reset.c
+++ arch/arm/mach-rk30/reset.c
@@ -1,7 +1,16 @@
+#include <linux/io.h>
+#include <linux/kernel.h>
 #include <mach/system.h>
+#include <mach/cru.h>
+#include <mach/iomux.h>
 
 static void rk30_arch_reset(char mode, const char *cmd)
 {
+	/* disable remap */
+	writel_relaxed(1 << (12 + 16), RK30_GRF_BASE + GRF_SOC_CON0);
+	dsb();
+	writel_relaxed(0xeca8, RK30_CRU_BASE + CRU_GLB_SRST_SND);
+	dsb();
 }
 
 void (*arch_reset)(char, const char *) = rk30_arch_reset;
