f5dcca1cf47a3a3167a9083788f109de680ea5c1
yzq <yzq@rock-chip.com>
2012-04-01 01:46:27 -0700

    rk610 lvds screen 1024x768 1280x800 update

--- drivers/video/display/screen/lcd_hdmi_1280x800.c
+++ drivers/video/display/screen/lcd_hdmi_1280x800.c
@@ -8,121 +8,127 @@
 #include "../../rk29_fb.h"
 #include "../lcd/rk610_lcd.h"
 
+
 /* Base */
 #define OUT_TYPE		SCREEN_LVDS
 
 #define OUT_FORMAT      LVDS_8BIT_2
+
 #define OUT_FACE		OUT_D888_P666  
 #define OUT_CLK			65000000
 #define LCDC_ACLK        500000000//312000000           //29 lcdc axi DMA ÆµÂÊ
 
+
 /* Timing */
 #define H_PW			10
-#define H_BP			100
-#define H_VD			1024
-#define H_FP			210
+#define H_BP			10
+#define H_VD			1280
+#define H_FP			20
 
 #define V_PW			10
 #define V_BP			10
-#define V_VD			768
-#define V_FP			18
+#define V_VD			800
+#define V_FP			13
 
 #define LCD_WIDTH       202
 #define LCD_HEIGHT      152
 
 /* scaler Timing    */
 //1920*1080*60
+
 #define S_OUT_CLK		SCALE_RATE(148500000,66000000) //m=16 n=9 no=4
-#define S_H_PW			100
-#define S_H_BP			100
-#define S_H_VD			1024
-#define S_H_FP			151
+#define S_H_PW			10
+#define S_H_BP			10
+#define S_H_VD			1280
+#define S_H_FP			20
 
-#define S_V_PW			5
-#define S_V_BP			15
-#define S_V_VD			768
-#define S_V_FP			12
+#define S_V_PW			10
+#define S_V_BP			10
+#define S_V_VD			800
+#define S_V_FP			13
 
-#define S_H_ST			1757
-#define S_V_ST			14
+#define S_H_ST			440
+#define S_V_ST			13
 
 //1920*1080*50
-#define S1_OUT_CLK		SCALE_RATE(148500000,54000000)  //m=16 n=11 no=4 
-#define S1_H_PW			100
-#define S1_H_BP			100
-#define S1_H_VD			1024
-#define S1_H_FP			126
+#define S1_OUT_CLK		SCALE_RATE(148500000,57375000)  //m=17 n=11 no=4 
+#define S1_H_PW			10
+#define S1_H_BP			10
+#define S1_H_VD			1280
+#define S1_H_FP			77
 
-#define S1_V_PW			5
-#define S1_V_BP			15
-#define S1_V_VD			768
-#define S1_V_FP			12
+#define S1_V_PW			10
+#define S1_V_BP			10
+#define S1_V_VD			800
+#define S1_V_FP			13
 
-#define S1_H_ST			1757
-#define S1_V_ST			14
+#define S1_H_ST			459
+#define S1_V_ST			13
 
 //1280*720*60
 #define S2_OUT_CLK		SCALE_RATE(74250000,66000000)  //m=32 n=9 no=4
-#define S2_H_PW			100
-#define S2_H_BP			100
-#define S2_H_VD			1024
-#define S2_H_FP			151
+#define S2_H_PW			10
+#define S2_H_BP			10
+#define S2_H_VD			1280
+#define S2_H_FP			20
 
-#define S2_V_PW			5
-#define S2_V_BP			15
-#define S2_V_VD			768
-#define S2_V_FP			12
+#define S2_V_PW			10
+#define S2_V_BP			10
+#define S2_V_VD			800
+#define S2_V_FP			13
 
-#define S2_H_ST			0
-#define S2_V_ST			12
+#define S2_H_ST			440
+#define S2_V_ST			13
+
 //1280*720*50
 
-#define S3_OUT_CLK		SCALE_RATE(74250000,54000000)   // m=32 n=11 no=4 
-#define S3_H_PW			100
-#define S3_H_BP			100
-#define S3_H_VD			1024
-#define S3_H_FP			151
+#define S3_OUT_CLK		SCALE_RATE(74250000,57375000)   // m=34 n=11 no=4
+#define S3_H_PW			10
+#define S3_H_BP			10
+#define S3_H_VD			1280
+#define S3_H_FP			77
 
-#define S3_V_PW			5
-#define S3_V_BP			15
-#define S3_V_VD			768
-#define S3_V_FP			12
+#define S3_V_PW			10
+#define S3_V_BP			10
+#define S3_V_VD			800
+#define S3_V_FP			13
 
-#define S3_H_ST			0
-#define S3_V_ST			12
+#define S3_H_ST			459
+#define S3_V_ST			13
 
 //720*576*50
-#define S4_OUT_CLK		SCALE_RATE(27000000,54375000)  //m=145 n=9 no=8 
-#define S4_H_PW			100
-#define S4_H_BP			100
-#define S4_H_VD			1024
-#define S4_H_FP			81
+#define S4_OUT_CLK		SCALE_RATE(27000000,63281250)  //m=75 n=4 no=8
+#define S4_H_PW			10
+#define S4_H_BP			10
+#define S4_H_VD			1280
+#define S4_H_FP			185
 
-#define S4_V_PW			5
-#define S4_V_BP			15
-#define S4_V_VD			768
-#define S4_V_FP			45
+#define S4_V_PW			10
+#define S4_V_BP			10
+#define S4_V_VD			800
+#define S4_V_FP			48
 
+#define S4_H_ST			81
+#define S4_V_ST			48
 
-#define S4_H_ST			435
-#define S4_V_ST			45
 //720*480*60
-#define S5_OUT_CLK		SCALE_RATE(27000000,72000000)  //m=32 n=3 no=4 
-#define S5_H_PW			100
-#define S5_H_BP			100
-#define S5_H_VD			1024
-#define S5_H_FP			81
+#define S5_OUT_CLK		SCALE_RATE(27000000,75000000)  //m=100 n=9 no=4
+#define S5_H_PW			10
+#define S5_H_BP			10
+#define S5_H_VD			1280
+#define S5_H_FP			130
 
-#define S5_V_PW			5
-#define S5_V_BP			15
-#define S5_V_VD			768
-#define S5_V_FP			51
+#define S5_V_PW			10
+#define S5_V_BP			10
+#define S5_V_VD			800
+#define S5_V_FP			54
 
-#define S5_H_ST			858
-#define S5_V_ST			45
+#define S5_H_ST			476
+#define S5_V_ST			48
 /* Other */
 #define DCLK_POL		0
 #define SWAP_RB			0 
+
 #ifdef  CONFIG_HDMI_DUAL_DISP
 static int set_scaler_info(struct rk29fb_screen *screen, u8 hdmi_resolution)
 {
@@ -224,7 +230,6 @@ static int set_scaler_info(struct rk29fb_screen *screen, u8 hdmi_resolution)
 #else
 static int set_scaler_info(struct rk29fb_screen *screen, u8 hdmi_resolution){}
 #endif
-
 void set_lcd_info(struct rk29fb_screen *screen,  struct rk29lcd_info *lcd_info )
 {
     /* screen type & face */
@@ -265,7 +270,8 @@ void set_lcd_info(struct rk29fb_screen *screen,  struct rk29lcd_info *lcd_info )
     screen->init = NULL;
     screen->standby = NULL;
     screen->sscreen_get = set_scaler_info;
-#ifdef CONFIG_RK610_LCD
     screen->sscreen_set = rk610_lcd_scaler_set_param;
-#endif
 }
+
+
+
