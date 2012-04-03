bc4854bc91c9a7f117437215cd8b16a0a5671d93
Cong Wang <xiyou.wangcong@gmail.com>
2012-04-03 14:13:36 +0800

    drbd: check MODULE for THIS_MODULE
    THIS_MODULE is NULL only when drbd is compiled as built-in,
    so the #ifdef CONFIG_MODULES should be #ifdef MODULE instead.
    
    This fixes the warning:
    
    drivers/block/drbd/drbd_main.c: In function ‘drbd_buildtag’:
    drivers/block/drbd/drbd_main.c:4187:24: warning: the comparison will always evaluate as ‘true’ for the address of ‘__this_module’ will never be NULL [-Waddress]
    
    Signed-off-by: WANG Cong <xiyou.wangcong@gmail.com>
    Signed-off-by: Philipp Reisner <philipp.reisner@linbit.com>
    Signed-off-by: Lars Ellenberg <lars.ellenberg@linbit.com>

--- drivers/block/drbd/drbd_main.c
+++ drivers/block/drbd/drbd_main.c
@@ -4365,12 +4365,11 @@ const char *drbd_buildtag(void)
 	static char buildtag[38] = "\0uilt-in";
 
 	if (buildtag[0] == 0) {
-#ifdef CONFIG_MODULES
-		if (THIS_MODULE != NULL)
-			sprintf(buildtag, "srcversion: %-24s", THIS_MODULE->srcversion);
-		else
+#ifdef MODULE
+		sprintf(buildtag, "srcversion: %-24s", THIS_MODULE->srcversion);
+#else
+		buildtag[0] = 'b';
 #endif
-			buildtag[0] = 'b';
 	}
 
 	return buildtag;
