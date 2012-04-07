1c41e01a514bc6b5136fd9040bed5137cfd712a8
Todd Poynor <toddpoynor@google.com>
2012-04-06 19:50:12 -0700

    cpufreq: interactive: set at least hispeed when above hispeed load
    If load is above go_hispeed_load, always go to at least hispeed_freq,
    even when reducing speed from a higher speed, not just when jumping
    up from minimum speed.  Avoids running at a lower than intended
    speed after a burst of even higher load.
    
    Change-Id: I5b9d2a15ba25ce609b21bac7c724265cf6838dee
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -167,10 +167,14 @@ static void cpufreq_interactive_timer(unsigned long data)
 		cpu_load = load_since_change;
 
 	if (cpu_load >= go_hispeed_load) {
-		if (pcpu->policy->cur == pcpu->policy->min)
+		if (pcpu->policy->cur == pcpu->policy->min) {
 			new_freq = hispeed_freq;
-		else
+		} else {
 			new_freq = pcpu->policy->max * cpu_load / 100;
+
+			if (new_freq < hispeed_freq)
+				new_freq = hispeed_freq;
+		}
 	} else {
 		new_freq = pcpu->policy->max * cpu_load / 100;
 	}
