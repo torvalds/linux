8b7730ddff5affd623bed2affa0d0fa47ebbad3b
Rob Herring <rob.herring@calxeda.com>
2012-04-09 15:24:59 -0500

    clk: remove trailing whitespace from clk.h
    Remove trailing whitespace from 2 lines.
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>

--- include/linux/clk.h
+++ include/linux/clk.h
@@ -220,7 +220,7 @@ void clk_put(struct clk *clk);
  * Returns rounded clock rate in Hz, or negative errno.
  */
 long clk_round_rate(struct clk *clk, unsigned long rate);
- 
+
 /**
  * clk_set_rate - set the clock rate for a clock source
  * @clk: clock source
@@ -229,7 +229,7 @@ long clk_round_rate(struct clk *clk, unsigned long rate);
  * Returns success (0) or negative errno.
  */
 int clk_set_rate(struct clk *clk, unsigned long rate);
- 
+
 /**
  * clk_set_parent - set the parent clock source for this clock
  * @clk: clock source
