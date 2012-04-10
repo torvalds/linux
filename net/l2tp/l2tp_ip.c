d1f224ae186b834af647661ffaf403a817c050ce
James Chapman <jchapman@katalix.com>
2012-04-10 00:10:42 +0000

    l2tp: fix refcount leak in l2tp_ip sockets
    The l2tp_ip socket close handler does not update the module refcount
    correctly which prevents module unload after the first bind() call on
    an L2TPv3 IP encapulation socket.
    
    Signed-off-by: James Chapman <jchapman@katalix.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/l2tp/l2tp_ip.c
+++ net/l2tp/l2tp_ip.c
@@ -232,7 +232,7 @@ static void l2tp_ip_close(struct sock *sk, long timeout)
 {
 	write_lock_bh(&l2tp_ip_lock);
 	hlist_del_init(&sk->sk_bind_node);
-	hlist_del_init(&sk->sk_node);
+	sk_del_node_init(sk);
 	write_unlock_bh(&l2tp_ip_lock);
 	sk_common_release(sk);
 }
