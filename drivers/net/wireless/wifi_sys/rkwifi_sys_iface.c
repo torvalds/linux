881609e873a83fd65906106be37e7feba7fdc987
duke <root@duke-desktop.(none)>
2012-04-10 13:04:13 +0800

    update rkwifi driver

--- drivers/net/wireless/wifi_sys/rkwifi_sys_iface.c
+++ drivers/net/wireless/wifi_sys/rkwifi_sys_iface.c
@@ -11,7 +11,7 @@ static ssize_t wifi_chip_read(struct class *cls, struct class_attribute *attr, c
 static ssize_t wifi_chip_read(struct class *cls, char *_buf)
 #endif
 {
-    int count;
+    int count = 0;
 
 #ifdef CONFIG_BCM4329
     count = sprintf(_buf, "%s", "BCM4329");
@@ -21,21 +21,6 @@ static ssize_t wifi_chip_read(struct class *cls, char *_buf)
 #ifdef CONFIG_RTL8192CU
     count = sprintf(_buf, "%s", "RTL8188");
     printk("Current WiFi chip is RTL8188.\n");
-#endif
-
-#ifdef CONFIG_BCM4330
-    count = sprintf(_buf, "%s", "BCM4330");
-    printk("Current WiFi chip is BCM4330.\n");
-#endif
-
-#ifdef CONFIG_RK901
-    count = sprintf(_buf, "%s", "RK901");
-    printk("Current WiFi chip is RK901.\n");
-#endif
-
-#ifdef CONFIG_RK903
-    count = sprintf(_buf, "%s", "RK903");
-    printk("Current WiFi chip is RK903.\n");
 #endif
     
     return count;
@@ -69,8 +54,40 @@ static ssize_t wifi_channel_write(struct class *cls, const char *_buf, size_t _c
 }
 */
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 37))
+static ssize_t wifi_p2p_read(struct class *cls, struct class_attribute *attr, char *_buf)
+#else
+static ssize_t wifi_p2p_read(struct class *cls, char *_buf)
+#endif
+{
+	int count = 0;
+
+#ifdef CONFIG_BCM4329
+    count = sprintf(_buf, "%s", "false");
+	printk("Current WiFi chip BCM4329 doesn't support direct.(%s)\n", _buf);
+#endif
+
+#ifdef CONFIG_RTL8192CU
+    count = sprintf(_buf, "%s", "false");
+	printk("Current WiFi chip RTL8188 support direct.(%s)\n", _buf);
+#endif
+
+#ifdef CONFIG_RK903
+    count = sprintf(_buf, "%s", "true");
+	    printk("Current WiFi chip RK903 support direct.(%s)\n", _buf);
+#endif
+
+#ifdef CONFIG_BCM4330
+    count = sprintf(_buf, "%s", "true");
+	printk("Current WiFi chip BCM4330 support direct.(%s)\n", _buf);
+#endif
+
+	return count;
+}
+
 static struct class *rkwifi_class = NULL;
 static CLASS_ATTR(chip, 0664, wifi_chip_read, NULL);
+static CLASS_ATTR(p2p, 0664, wifi_p2p_read, NULL);
 
 int rkwifi_sysif_init(void)
 {
@@ -88,6 +105,7 @@ int rkwifi_sysif_init(void)
     }
     
     ret =  class_create_file(rkwifi_class, &class_attr_chip);
+    ret =  class_create_file(rkwifi_class, &class_attr_p2p);
     
     return 0;
 }
