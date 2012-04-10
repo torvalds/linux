7a0a289c5f4aa7547e4b2630c7b18da8b0fb8b4f
Fabio Estevam <festevam@gmail.com>
2012-04-10 19:38:23 -0300

    ASoC: ac97: Fix build due to removal of 'runtime' definition
    Fix the following build error:
    
    sound/soc/codecs/ac97.c: In function 'ac97_prepare':
    sound/soc/codecs/ac97.c:33: error: 'runtime' undeclared (first use in this function)
    
    This was caused by commit e6968a (ASoC: codecs: Remove rtd->codec usage from CODEC drivers),
    which removed the 'struct snd_pcm_runtime *runtime = substream->runtime' definition.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/ac97.c
+++ sound/soc/codecs/ac97.c
@@ -30,7 +30,7 @@ static int ac97_prepare(struct snd_pcm_substream *substream,
 
 	int reg = (substream->stream == SNDRV_PCM_STREAM_PLAYBACK) ?
 		  AC97_PCM_FRONT_DAC_RATE : AC97_PCM_LR_ADC_RATE;
-	return snd_ac97_set_rate(codec->ac97, reg, runtime->rate);
+	return snd_ac97_set_rate(codec->ac97, reg, substream->runtime->rate);
 }
 
 #define STD_AC97_RATES (SNDRV_PCM_RATE_8000 | SNDRV_PCM_RATE_11025 |\
