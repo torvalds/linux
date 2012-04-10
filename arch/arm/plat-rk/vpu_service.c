e8d8e5ee346f2debf48eb263a61e3f55b0a918c6
陈恒明 <chm@rock-chips.com>
2012-04-10 18:11:28 +0800

    rk30: vpu_service: final solution of vpu hardware bug

--- arch/arm/plat-rk/vpu_service.c
+++ arch/arm/plat-rk/vpu_service.c
@@ -449,9 +449,10 @@ void reg_copy_to_hw(vpu_reg *reg)
 	case VPU_ENC : {
 		u32 *dst = (u32 *)enc_dev.hwregs;
 #if defined(CONFIG_ARCH_RK30)
-		writel_relaxed(0, dec_dev.hwregs + DEC_INTERRUPT_REGISTER);
+		cru_set_soft_reset(SOFT_RST_CPU_VCODEC, true);
 		cru_set_soft_reset(SOFT_RST_VCODEC_AHB, true);
 		cru_set_soft_reset(SOFT_RST_VCODEC_AHB, false);
+		cru_set_soft_reset(SOFT_RST_CPU_VCODEC, false);
 #endif
 		service.reg_codec = reg;
 
