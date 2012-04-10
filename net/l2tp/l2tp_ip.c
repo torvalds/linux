c9be48dc8bb22f1f6e6ff1560b2b28e925a0b815
James Chapman <jchapman@katalix.com>
2012-04-10 00:10:43 +0000

    l2tp: don't overwrite source address in l2tp_ip_bind()
    Applications using L2TP/IP sockets want to be able to bind() an L2TP/IP
    socket to set the local tunnel id while leaving the auto-assigned source
    address alone. So if no source address is supplied, don't overwrite
    the address already stored in the socket.
    
    Signed-off-by: James Chapman <jchapman@katalix.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/l2tp/l2tp_ip.c
+++ net/l2tp/l2tp_ip.c
@@ -271,7 +271,8 @@ static int l2tp_ip_bind(struct sock *sk, struct sockaddr *uaddr, int addr_len)
 	    chk_addr_ret != RTN_MULTICAST && chk_addr_ret != RTN_BROADCAST)
 		goto out;
 
-	inet->inet_rcv_saddr = inet->inet_saddr = addr->l2tp_addr.s_addr;
+	if (addr->l2tp_addr.s_addr)
+		inet->inet_rcv_saddr = inet->inet_saddr = addr->l2tp_addr.s_addr;
 	if (chk_addr_ret == RTN_MULTICAST || chk_addr_ret == RTN_BROADCAST)
 		inet->inet_saddr = 0;  /* Use device */
 	sk_dst_reset(sk);
