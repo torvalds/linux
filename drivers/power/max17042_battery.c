a2ebfe2fc6e088a70d06cd15a5bc9bcb621cc195
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2012-04-10 16:21:20 +0530

    power_supply: Add voltage_ocv property and use it for max17042 driver
    This adds a new sysfs file called 'voltage_ocv' which gives the
    Open Circuit Voltage of the battery.
    
    This property can be used for platform shutdown policies and
    can be useful for initial capacity estimations.
    
    Note: This patch is generated against linux-next branch.
    
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>
    Signed-off-by: Anton Vorontsov <anton.vorontsov@linaro.org>

--- drivers/power/max17042_battery.c
+++ drivers/power/max17042_battery.c
@@ -106,6 +106,7 @@ static enum power_supply_property max17042_battery_props[] = {
 	POWER_SUPPLY_PROP_VOLTAGE_MIN_DESIGN,
 	POWER_SUPPLY_PROP_VOLTAGE_NOW,
 	POWER_SUPPLY_PROP_VOLTAGE_AVG,
+	POWER_SUPPLY_PROP_VOLTAGE_OCV,
 	POWER_SUPPLY_PROP_CAPACITY,
 	POWER_SUPPLY_PROP_CHARGE_FULL,
 	POWER_SUPPLY_PROP_TEMP,
@@ -167,6 +168,13 @@ static int max17042_get_property(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_VOLTAGE_AVG:
 		ret = max17042_read_reg(chip->client, MAX17042_AvgVCELL);
+		if (ret < 0)
+			return ret;
+
+		val->intval = ret * 625 / 8;
+		break;
+	case POWER_SUPPLY_PROP_VOLTAGE_OCV:
+		ret = max17042_read_reg(chip->client, MAX17042_OCVInternal);
 		if (ret < 0)
 			return ret;
 
