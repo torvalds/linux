7a47462902d03c6e4d3412a5b703069f4dd13c44
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-04-12 17:29:36 +0100

    ASoC: dapm: Ensure power gets managed for line widgets
    commit 7e1f7c8a6e517900cd84da1b8ae020f08f286c3b upstream.
    
    Line widgets had not been included in either the power up or power down
    sequences so if a widget had an event associated with it that event would
    never be run. Fix this minimally by adding them to the sequences, we
    should probably be doing away with the specific widget types as they all
    have the same priority anyway.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -67,6 +67,7 @@ static int dapm_up_seq[] = {
 	[snd_soc_dapm_out_drv] = 10,
 	[snd_soc_dapm_hp] = 10,
 	[snd_soc_dapm_spk] = 10,
+	[snd_soc_dapm_line] = 10,
 	[snd_soc_dapm_post] = 11,
 };
 
@@ -75,6 +76,7 @@ static int dapm_down_seq[] = {
 	[snd_soc_dapm_adc] = 1,
 	[snd_soc_dapm_hp] = 2,
 	[snd_soc_dapm_spk] = 2,
+	[snd_soc_dapm_line] = 2,
 	[snd_soc_dapm_out_drv] = 2,
 	[snd_soc_dapm_pga] = 4,
 	[snd_soc_dapm_mixer_named_ctl] = 5,
