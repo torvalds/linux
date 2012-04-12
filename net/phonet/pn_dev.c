3c490a87f6f6ce5b002c42f9c00c9136ddbd7248
Rémi Denis-Courmont <remi.denis-courmont@nokia.com>
2012-04-12 03:39:16 +0000

    Phonet: phonet_net_id can be static (sparse)
    Signed-off-by: Rémi Denis-Courmont <remi.denis-courmont@nokia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/phonet/pn_dev.c
+++ net/phonet/pn_dev.c
@@ -44,7 +44,7 @@ struct phonet_net {
 	struct phonet_routes routes;
 };
 
-int phonet_net_id __read_mostly;
+static int phonet_net_id __read_mostly;
 
 static struct phonet_net *phonet_pernet(struct net *net)
 {
