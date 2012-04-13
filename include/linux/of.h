e05e5070f0ec2557d2d2ff3655ba03f29e297151
Thierry Reding <thierry.reding@avionic-design.de>
2012-04-13 16:19:21 +0200

    dt: Add empty of_parse_phandle_with_args() function
    This commit adds an empty of_parse_phandle_with_args() function for
    !CONFIG_OF builds.
    
    Acked-by: Rob Herring <rob.herring@calxeda.com>
    Signed-off-by: Thierry Reding <thierry.reding@avionic-design.de>

--- include/linux/of.h
+++ include/linux/of.h
@@ -391,6 +391,15 @@ static inline struct device_node *of_parse_phandle(struct device_node *np,
 	return NULL;
 }
 
+static inline int of_parse_phandle_with_args(struct device_node *np,
+					     const char *list_name,
+					     const char *cells_name,
+					     int index,
+					     struct of_phandle_args *out_args)
+{
+	return -ENOSYS;
+}
+
 static inline int of_alias_get_id(struct device_node *np, const char *stem)
 {
 	return -ENOSYS;
