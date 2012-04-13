3f24f5ada638dd07705bd83ebcc80044d587f374
Axel Lin <axel.lin@gmail.com>
2012-04-13 21:35:05 +0800

    regulator: core: Fix getting input_uV when supplied by another regulator
    When supplied by another regulator, returns the supply regulator's output
    voltage for inpu_uV.
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -649,7 +649,7 @@ static void drms_uA_update(struct regulator_dev *rdev)
 	/* get input voltage */
 	input_uV = 0;
 	if (rdev->supply)
-		input_uV = _regulator_get_voltage(rdev);
+		input_uV = regulator_get_voltage(rdev->supply);
 	if (input_uV <= 0)
 		input_uV = rdev->constraints->input_uV;
 	if (input_uV <= 0)
