b41b6f1d1cb6a16c737c4699d2815112ec21faa6
Venkatraman S <svenkatr@ti.com>
2012-04-13 17:54:13 +0530

    mmc: queue: remove redundant memsets
    Not needed to memset, as they are pointers and are assigned
    to proper values in the next line anyway.
    
    Signed-off-by: Venkatraman S <svenkatr@ti.com>
    Reviewed-by: Namjae Jeon <linkinjeon@gmail.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/card/queue.c
+++ drivers/mmc/card/queue.c
@@ -175,8 +175,6 @@ int mmc_init_queue(struct mmc_queue *mq, struct mmc_card *card,
 	if (!mq->queue)
 		return -ENOMEM;
 
-	memset(&mq->mqrq_cur, 0, sizeof(mq->mqrq_cur));
-	memset(&mq->mqrq_prev, 0, sizeof(mq->mqrq_prev));
 	mq->mqrq_cur = mqrq_cur;
 	mq->mqrq_prev = mqrq_prev;
 	mq->queue->queuedata = mq;
