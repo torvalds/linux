7563bbf89d065a2c3f05059ecbcc805645edcc62
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-04-15 10:52:54 +0100

    gpiolib/arches: Centralise bolierplate asm/gpio.h
    Rather than requiring architectures that use gpiolib but don't have any
    need to define anything custom to copy an asm/gpio.h provide a Kconfig
    symbol which architectures must select in order to include gpio.h and
    for other architectures just provide the trivial implementation directly.
    
    This makes it much easier to do gpiolib updates and is also a step towards
    making gpiolib APIs available on every architecture.
    
    For architectures with existing boilerplate code leave a stub header in
    place which warns on direct inclusion of asm/gpio.h and includes
    linux/gpio.h to catch code that's doing this.  Direct inclusion of
    asm/gpio.h has long been deprecated.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Jonas Bonn <jonas@southpole.se>
    Acked-by: Tony Luck <tony.luck@intel.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- include/linux/gpio.h
+++ include/linux/gpio.h
@@ -1,6 +1,8 @@
 #ifndef __LINUX_GPIO_H
 #define __LINUX_GPIO_H
 
+#include <linux/errno.h>
+
 /* see Documentation/gpio.txt */
 
 /* make these flag values available regardless of GPIO kconfig options */
@@ -38,7 +40,39 @@ struct gpio {
 };
 
 #ifdef CONFIG_GENERIC_GPIO
+
+#ifdef CONFIG_ARCH_HAVE_CUSTOM_GPIO_H
 #include <asm/gpio.h>
+#else
+
+#include <asm-generic/gpio.h>
+
+static inline int gpio_get_value(unsigned int gpio)
+{
+	return __gpio_get_value(gpio);
+}
+
+static inline void gpio_set_value(unsigned int gpio, int value)
+{
+	__gpio_set_value(gpio, value);
+}
+
+static inline int gpio_cansleep(unsigned int gpio)
+{
+	return __gpio_cansleep(gpio);
+}
+
+static inline int gpio_to_irq(unsigned int gpio)
+{
+	return __gpio_to_irq(gpio);
+}
+
+static inline int irq_to_gpio(unsigned int irq)
+{
+	return -EINVAL;
+}
+
+#endif
 
 #else
 
