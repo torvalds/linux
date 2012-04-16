df1a000e767f54a539f4d2d663130d063f5208ea
Greg Ungerer <gerg@uclinux.org>
2012-04-16 17:01:31 +1000

    m68knommu: switch to GPIO init macros in ColdFire 5307 init code
    Modify the GPIO setup table to use the mcfgpio.h macros for table init.
    Simplifies code and reduces line count significantly.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>
    Acked-by: Steven King <sfking@fdwdc.com>

--- arch/m68k/platform/5307/gpio.c
+++ arch/m68k/platform/5307/gpio.c
@@ -21,21 +21,7 @@
 #include <asm/mcfgpio.h>
 
 static struct mcf_gpio_chip mcf_gpio_chips[] = {
-	{
-		.gpio_chip			= {
-			.label			= "PP",
-			.request		= mcf_gpio_request,
-			.free			= mcf_gpio_free,
-			.direction_input	= mcf_gpio_direction_input,
-			.direction_output	= mcf_gpio_direction_output,
-			.get			= mcf_gpio_get_value,
-			.set			= mcf_gpio_set_value,
-			.ngpio			= 16,
-		},
-		.pddr				= (void __iomem *) MCFSIM_PADDR,
-		.podr				= (void __iomem *) MCFSIM_PADAT,
-		.ppdr				= (void __iomem *) MCFSIM_PADAT,
-	},
+	MCFGPS(PP, 0, 16, MCFSIM_PADDR, MCFSIM_PADAT, MCFSIM_PADAT),
 };
 
 static int __init mcf_gpio_init(void)
