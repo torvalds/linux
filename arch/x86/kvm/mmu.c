f71fa31f9f7ac33cba12b8897983f950ad2c7a5b
Davidlohr Bueso <dave@gnu.org>
2012-04-18 12:24:29 +0200

    KVM: MMU: use page table level macro
    Its much cleaner to use PT_PAGE_TABLE_LEVEL than its numeric value.
    
    Signed-off-by: Davidlohr Bueso <dave@gnu.org>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/x86/kvm/mmu.c
+++ arch/x86/kvm/mmu.c
@@ -3618,7 +3618,7 @@ static bool detect_write_flooding(struct kvm_mmu_page *sp)
 	 * Skip write-flooding detected for the sp whose level is 1, because
 	 * it can become unsync, then the guest page is not write-protected.
 	 */
-	if (sp->role.level == 1)
+	if (sp->role.level == PT_PAGE_TABLE_LEVEL)
 		return false;
 
 	return ++sp->write_flooding_count >= 3;
