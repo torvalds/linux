53c50ef508cc7b47c8b912549d4e2593fd0e5049
Haojian Zhuang <haojian.zhuang@gmail.com>
2012-04-19 18:22:16 +0800

    ARM: mmp: fix build issue on mmp with device tree
    Since irq_domain_add_simple() is removed, remove it in mmp-dt.c also.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@gmail.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-mmp/mmp-dt.c
+++ arch/arm/mach-mmp/mmp-dt.c
@@ -32,30 +32,8 @@ static const struct of_dev_auxdata mmp_auxdata_lookup[] __initconst = {
 	{}
 };
 
-static int __init mmp_intc_add_irq_domain(struct device_node *np,
-					   struct device_node *parent)
-{
-	irq_domain_add_simple(np, 0);
-	return 0;
-}
-
-static int __init mmp_gpio_add_irq_domain(struct device_node *np,
-					   struct device_node *parent)
-{
-	irq_domain_add_simple(np, IRQ_GPIO_START);
-	return 0;
-}
-
-static const struct of_device_id mmp_irq_match[] __initconst = {
-	{ .compatible = "mrvl,mmp-intc", .data = mmp_intc_add_irq_domain, },
-	{ .compatible = "mrvl,mmp-gpio", .data = mmp_gpio_add_irq_domain, },
-	{}
-};
-
 static void __init mmp_dt_init(void)
 {
-
-	of_irq_init(mmp_irq_match);
 
 	of_platform_populate(NULL, of_default_bus_match_table,
 			     mmp_auxdata_lookup, NULL);
