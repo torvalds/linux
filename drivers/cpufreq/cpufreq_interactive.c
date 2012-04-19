b5ba81900f59d03d6c976d316843f9b705cc2fe5
Todd Poynor <toddpoynor@google.com>
2012-04-19 12:52:48 -0700

    cpufreq: interactive: base hispeed bump on target freq, not actual
    For systems that set a common speed for all CPUs, checking current
    speed here could bypass the intermediate hispeed bump decision for
    this CPU when another CPU was already at hispeed.  This could
    result in an overly high setting (for all CPUs) in situations
    where all CPUs were about to drop to load levels that map to
    hispeed or below.
    
    Change-Id: I186f23dcfc5e2b6336cab8b0327f0c8a9a4482bc
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -174,7 +174,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 		cpu_load = load_since_change;
 
 	if (cpu_load >= go_hispeed_load) {
-		if (pcpu->policy->cur == pcpu->policy->min) {
+		if (pcpu->target_freq <= pcpu->policy->min) {
 			new_freq = hispeed_freq;
 		} else {
 			new_freq = pcpu->policy->max * cpu_load / 100;
