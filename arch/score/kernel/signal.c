cf4a0ce45c06f79f800f6d8747618901e3a6a8ad
Al Viro <viro@zeniv.linux.org.uk>
2012-04-21 21:39:54 -0400

    score: don't open-code force_sigsegv()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/score/kernel/signal.c
+++ arch/score/kernel/signal.c
@@ -236,9 +236,7 @@ static int setup_rt_frame(struct k_sigaction *ka, struct pt_regs *regs,
 	return 0;
 
 give_sigsegv:
-	if (signr == SIGSEGV)
-		ka->sa.sa_handler = SIG_DFL;
-	force_sig(SIGSEGV, current);
+	force_sigsegv(signr, current);
 	return -EFAULT;
 }
 
