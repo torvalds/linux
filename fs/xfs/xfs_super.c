7582df516c93046b8d2111a780c69de77f9882fb
Shaohua Li <shli@kernel.org>
2012-04-24 21:23:46 +0800

    xfs: using GFP_NOFS for blkdev_issue_flush
    Issuing a block device flush request in transaction context using GFP_KERNEL
    directly can cause deadlocks due to memory reclaim recursion. Use GFP_NOFS to
    avoid recursion from reclaim context.
    
    Signed-off-by: Shaohua Li <shli@fusionio.com>
    Reviewed-by: Mark Tinguely <tinguely@sgi.com>
    Signed-off-by: Ben Myers <bpm@sgi.com>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -622,7 +622,7 @@ void
 xfs_blkdev_issue_flush(
 	xfs_buftarg_t		*buftarg)
 {
-	blkdev_issue_flush(buftarg->bt_bdev, GFP_KERNEL, NULL);
+	blkdev_issue_flush(buftarg->bt_bdev, GFP_NOFS, NULL);
 }
 
 STATIC void
