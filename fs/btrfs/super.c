996d282c7ff470f150a467eb4815b90159d04c47
Josef Bacik <josef@redhat.com>
2012-04-23 20:35:03 -0400

    Btrfs: do not start delalloc inodes during sync
    btrfs_start_delalloc_inodes will just walk the list of delalloc inodes and
    start writing them out, but it doesn't splice the list or anything so as
    long as somebody is doing work on the box you could end up in this section
    _forever_.  So just remove it, it's not needed anyway since sync will start
    writeback on all inodes anyway, all we need to do is wait for ordered
    extents and then we can commit the transaction.  In my horrible torture test
    sync goes from taking 4 minutes to about 1.5 minutes.  Thanks,
    
    Signed-off-by: Josef Bacik <josef@redhat.com>
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -819,7 +819,6 @@ int btrfs_sync_fs(struct super_block *sb, int wait)
 		return 0;
 	}
 
-	btrfs_start_delalloc_inodes(root, 0);
 	btrfs_wait_ordered_extents(root, 0, 0);
 
 	trans = btrfs_start_transaction(root, 0);
