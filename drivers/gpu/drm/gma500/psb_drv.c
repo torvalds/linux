5f68127d9cf4b358060ce5f9906246262a56f179
Alan Cox <alan@linux.intel.com>
2012-04-25 14:34:04 +0100

    gma500: don't register the ACPI video bus
    commit 155689defc782b486a7e6776a57ecc4ebb37ed52 upstream.
    
    We are not yet ready for this and it makes a mess on some devices.
    
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/gma500/psb_drv.c
+++ drivers/gpu/drm/gma500/psb_drv.c
@@ -349,7 +349,7 @@ static int psb_driver_load(struct drm_device *dev, unsigned long chipset)
 	PSB_WSGX32(0x30000000, PSB_CR_BIF_3D_REQ_BASE);
 
 /*	igd_opregion_init(&dev_priv->opregion_dev); */
-	acpi_video_register();
+/*	acpi_video_register(); */
 	if (dev_priv->lid_state)
 		psb_lid_timer_init(dev_priv);
 
