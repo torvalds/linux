922ee08baad2052d0759f100e026d49798c51fef
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2012-04-25 20:50:53 +0200

    dmaengine: Fixup dmaengine_prep_slave_single() to be actually useful
    dmaengine_prep_slave_single() is a helper function which is supposed to be used
    to prepare a transfer of a single contingous buffer. Currently the function
    takes a pointer to such a buffer from which it builds a scatterlist and passes
    it on to device_prep_slave_sg. The dmaengine framework requires that any
    scatterlist that is passed to device_prep_slave_sg is mapped and it may not be
    unmapped until the DMA operation has completed. This is not the here and any use
    of dmaengine_prep_slave_single() will lead to undefined behaviour (Most likely a
    system crash).
    
    This patch changes dmaengine_prep_slave_single() to take a dma_addr_t instead of
    a pointer to a buffer and moves the responsibility of mapping and unmapping the
    buffer up to the caller.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Vinod Koul <vinod.koul@linux.intel.com>

--- include/linux/dmaengine.h
+++ include/linux/dmaengine.h
@@ -615,11 +615,13 @@ static inline int dmaengine_slave_config(struct dma_chan *chan,
 }
 
 static inline struct dma_async_tx_descriptor *dmaengine_prep_slave_single(
-	struct dma_chan *chan, void *buf, size_t len,
+	struct dma_chan *chan, dma_addr_t buf, size_t len,
 	enum dma_transfer_direction dir, unsigned long flags)
 {
 	struct scatterlist sg;
-	sg_init_one(&sg, buf, len);
+	sg_init_table(&sg, 1);
+	sg_dma_address(&sg) = buf;
+	sg_dma_len(&sg) = len;
 
 	return chan->device->device_prep_slave_sg(chan, &sg, 1,
 						  dir, flags, NULL);
