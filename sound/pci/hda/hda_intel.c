f3af90517d87bf8f4b21c2e68c8a15d9b7fd516e
Jaroslav Kysela <perex@perex.cz>
2012-04-26 17:52:35 +0200

    ALSA: hda - add probe_mask=0x101 automatically for WinFast VP200 H
    This patch just sets the codec probe_mask=0x101 value for the WinFast VP200 H
    PCoIP card based on Teradici hardware matching the PCI subsystem vendor/device
    IDs 3a21:040d. The user reported no codec detection issues without this
    explicit codec configuration.
    
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -2551,6 +2551,8 @@ static struct snd_pci_quirk probe_mask_list[] __devinitdata = {
 	/* forced codec slots */
 	SND_PCI_QUIRK(0x1043, 0x1262, "ASUS W5Fm", 0x103),
 	SND_PCI_QUIRK(0x1046, 0x1262, "ASUS W5F", 0x103),
+	/* WinFast VP200 H (Teradici) user reported broken communication */
+	SND_PCI_QUIRK(0x3a21, 0x040d, "WinFast VP200 H", 0x101),
 	{}
 };
 
