7654b72417e10e294563496e25211200f9b8b6d3
Daniel J Blueman <daniel@quora.org>
2012-04-27 12:41:46 -0400

    Btrfs: Fix space checking during fs resize
    Fix out-of-space checking, addressing a warning and potential resource
    leak when resizing the filesystem down while allocating blocks.
    
    Signed-off-by: Daniel J Blueman <daniel@quora.org>
    Reviewed-by: Josef Bacik <josef@redhat.com>
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/relocation.c
+++ fs/btrfs/relocation.c
@@ -3813,7 +3813,7 @@ restart:
 
 		ret = btrfs_block_rsv_check(rc->extent_root, rc->block_rsv, 5);
 		if (ret < 0) {
-			if (ret != -EAGAIN) {
+			if (ret != -ENOSPC) {
 				err = ret;
 				WARN_ON(1);
 				break;
