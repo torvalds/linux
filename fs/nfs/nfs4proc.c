9e907fec6ef7705ba07e22f034dacf102d29a538
Trond Myklebust <Trond.Myklebust@netapp.com>
2012-04-27 13:48:17 -0400

    NFSv4: Delegreturn only needs the cache consistency bitmask
    In order to do close-to-open cache consistency checking after
    a delegreturn, we don't need to retrieve the full set of
    attributes.
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -4117,7 +4117,7 @@ static int _nfs4_proc_delegreturn(struct inode *inode, struct rpc_cred *cred, co
 	nfs41_init_sequence(&data->args.seq_args, &data->res.seq_res, 1);
 	data->args.fhandle = &data->fh;
 	data->args.stateid = &data->stateid;
-	data->args.bitmask = server->attr_bitmask;
+	data->args.bitmask = server->cache_consistency_bitmask;
 	nfs_copy_fh(&data->fh, NFS_FH(inode));
 	nfs4_stateid_copy(&data->stateid, stateid);
 	data->res.fattr = &data->fattr;
