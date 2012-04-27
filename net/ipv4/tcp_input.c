651913ce9de2bbcedef608c5d6cf39c244248509
Neal Cardwell <ncardwell@google.com>
2012-04-27 11:29:37 -0400

    tcp: clean up use of jiffies in tcp_rcv_rtt_measure()
    Clean up a reference to jiffies in tcp_rcv_rtt_measure() that should
    instead reference tcp_time_stamp. Since the result of the subtraction
    is passed into a function taking u32, this should not change any
    behavior (and indeed the generated assembly does not change on
    x86_64). However, it seems worth cleaning this up for consistency and
    clarity (and perhaps to avoid bugs if this is copied and pasted
    somewhere else).
    
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -495,7 +495,7 @@ static inline void tcp_rcv_rtt_measure(struct tcp_sock *tp)
 		goto new_measure;
 	if (before(tp->rcv_nxt, tp->rcv_rtt_est.seq))
 		return;
-	tcp_rcv_rtt_update(tp, jiffies - tp->rcv_rtt_est.time, 1);
+	tcp_rcv_rtt_update(tp, tcp_time_stamp - tp->rcv_rtt_est.time, 1);
 
 new_measure:
 	tp->rcv_rtt_est.seq = tp->rcv_nxt + tp->rcv_wnd;
