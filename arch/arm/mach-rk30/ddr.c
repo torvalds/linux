4d2fbd0f746ee92bd63aaaf7bacfdd21b917981d
hecanyang <hcy@ubuntu-fs.(none)>
2012-04-28 15:18:14 +0800

    DDR3 ODT reduce from 120ohm to 60ohm

--- arch/arm/mach-rk30/ddr.c
+++ arch/arm/mach-rk30/ddr.c
@@ -1284,6 +1284,9 @@ static __sramfunc void ddr_adjust_config(uint32_t dram_type)
     idle_port();
     move_to_Config_state();
 
+    //extend capability for debug
+    pGRF_Reg->GRF_SOC_CON[2] = rank_to_row15_en;
+
     //set data training address
     pPHY_Reg->DTAR = value;
 
@@ -1293,7 +1296,7 @@ static __sramfunc void ddr_adjust_config(uint32_t dram_type)
     //adjust DRV and ODT
     if(dram_type == DDR3)
     {
-        pPHY_Reg->ZQ0CR[1] = 0x1B;  //DS=40ohm,ODT=120ohm
+        pPHY_Reg->ZQ0CR[1] = 0x5B;  //DS=40ohm,ODT=60ohm
         pPHY_Reg->ZQ0CR[0] |= (1<<30);  //trigger
     }
     if (dram_type == DDR2)
