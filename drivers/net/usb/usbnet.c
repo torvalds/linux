a4723848d05dd31d298c551fb77ad28481309999
tom.leiming@gmail.com <tom.leiming@gmail.com>
2012-04-29 22:51:03 +0000

    usbnet: fix failure handling in usbnet_probe
    If register_netdev returns failure, the dev->interrupt and
    its transfer buffer should be released, so just fix it.
    
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/usbnet.c
+++ drivers/net/usb/usbnet.c
@@ -1444,7 +1444,7 @@ usbnet_probe (struct usb_interface *udev, const struct usb_device_id *prod)
 
 	status = register_netdev (net);
 	if (status)
-		goto out3;
+		goto out4;
 	netif_info(dev, probe, dev->net,
 		   "register '%s' at usb-%s-%s, %s, %pM\n",
 		   udev->dev.driver->name,
@@ -1462,6 +1462,8 @@ usbnet_probe (struct usb_interface *udev, const struct usb_device_id *prod)
 
 	return 0;
 
+out4:
+	usb_free_urb(dev->interrupt);
 out3:
 	if (info->unbind)
 		info->unbind (dev, udev);
