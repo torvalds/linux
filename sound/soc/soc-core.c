cd0f8911c5d0cc04763f87cf118c66b867e2e6d2
Liam Girdwood <lrg@ti.com>
2012-04-30 11:05:30 +0100

    ASoC: core: Fix dai_link dereference.
    We should check dailess before dereferencing.
    
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Liam Girdwood <lrg@ti.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1218,7 +1218,7 @@ static int soc_post_component_init(struct snd_soc_card *card,
 
 #ifdef CONFIG_DEBUG_FS
 	/* add DPCM sysfs entries */
-	if (!dai_link->dynamic)
+	if (!dailess && !dai_link->dynamic)
 		goto out;
 
 	ret = soc_dpcm_debugfs_add(rtd);
