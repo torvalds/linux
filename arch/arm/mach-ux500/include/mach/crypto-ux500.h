8a63b1994c500d4825ee73dc71502deffe5b135b
Andreas Westin <andreas.westin@stericsson.com>
2012-04-30 10:11:18 +0200

    crypto: ux500 - Add driver for HASH hardware
    This adds a driver for the ST-Ericsson ux500 hash hardware
    module. The driver implements support for SHA-1 and SHA-2.
    
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Andreas Westin <andreas.westin@stericsson.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- arch/arm/mach-ux500/include/mach/crypto-ux500.h
+++ arch/arm/mach-ux500/include/mach/crypto-ux500.h
@@ -8,6 +8,11 @@
 #include <linux/dmaengine.h>
 #include <plat/ste_dma40.h>
 
+struct hash_platform_data {
+	void *mem_to_engine;
+	bool (*dma_filter)(struct dma_chan *chan, void *filter_param);
+};
+
 struct cryp_platform_data {
 	struct stedma40_chan_cfg mem_to_engine;
 	struct stedma40_chan_cfg engine_to_mem;
