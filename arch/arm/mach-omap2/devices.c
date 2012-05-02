259bd6ce0200c4b9692fcbfa9cbc29d818aa2116
Peter Ujfalusi <peter.ujfalusi@ti.com>
2012-05-02 15:23:18 +0300

    OMAP4: devices: Do not create dmic device if the dtb has been provided
    If dtb is provided the needed device will be created dynamically.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -722,12 +722,13 @@ static int __init omap2_init_devices(void)
 	 */
 	omap_init_audio();
 	omap_init_mcpdm();
-	omap_init_dmic();
 	omap_init_camera();
 	omap_init_mbox();
 	/* If dtb is there, the devices will be created dynamically */
-	if (!of_have_populated_dt())
+	if (!of_have_populated_dt()) {
+		omap_init_dmic();
 		omap_init_mcspi();
+	}
 	omap_init_pmu();
 	omap_hdq_init();
 	omap_init_sti();
