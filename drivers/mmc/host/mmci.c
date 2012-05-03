dfb85185bda373a70409c70c2632b02fb0f8f449
Russell King <rmk+kernel@arm.linux.org.uk>
2012-05-03 11:33:15 +0100

    AMBA: get rid of last two uses of NO_IRQ
    This gets rid of the last two users of NO_IRQ in AMBA primecell
    drivers.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/mmc/host/mmci.c
+++ drivers/mmc/host/mmci.c
@@ -1397,7 +1397,7 @@ static int __devinit mmci_probe(struct amba_device *dev,
 	if (ret)
 		goto unmap;
 
-	if (dev->irq[1] == NO_IRQ || !dev->irq[1])
+	if (!dev->irq[1])
 		host->singleirq = true;
 	else {
 		ret = request_irq(dev->irq[1], mmci_pio_irq, IRQF_SHARED,
