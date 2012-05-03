e5846fc665d1c3dd32d877febe7402ccd583b8a1
Chris Mason <chris.mason@oracle.com>
2012-05-03 12:08:48 -0400

    Btrfs: Add properly locking around add_root_to_dirty_list
    add_root_to_dirty_list happens once at the very beginning of the
    transaction, but it is still racey.
    
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/ctree.c
+++ fs/btrfs/ctree.c
@@ -220,10 +220,12 @@ struct extent_buffer *btrfs_read_lock_root_node(struct btrfs_root *root)
  */
 static void add_root_to_dirty_list(struct btrfs_root *root)
 {
+	spin_lock(&root->fs_info->trans_lock);
 	if (root->track_dirty && list_empty(&root->dirty_list)) {
 		list_add(&root->dirty_list,
 			 &root->fs_info->dirty_cowonly_roots);
 	}
+	spin_unlock(&root->fs_info->trans_lock);
 }
 
 /*
