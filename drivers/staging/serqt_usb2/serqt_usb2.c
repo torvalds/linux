3cccc292524dfd5d77f38f52116b1da4864e3789
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-05-04 16:17:51 -0700

    USB: serqt_usb2.c: remove dbg() tracing calls
    dbg() was used a lot a long time ago to trace code flow.  Now that we have
    ftrace, this isn't needed at all, so remove these calls.
    
    CC: Bill Pemberton <wfp5p@virginia.edu>
    CC: Rusty Russell <rusty@rustcorp.com.au>
    CC: Alan Stern <stern@rowland.harvard.edu>
    CC: Mauro Carvalho Chehab <mchehab@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/serqt_usb2/serqt_usb2.c
+++ drivers/staging/serqt_usb2/serqt_usb2.c
@@ -304,8 +304,6 @@ static void qt_write_bulk_callback(struct urb *urb)
 
 	quatech_port = urb->context;
 
-	dbg("%s - port %d\n", __func__, quatech_port->port_num);
-
 	tty = tty_port_tty_get(&quatech_port->port->port);
 
 	if (tty)
@@ -351,7 +349,6 @@ static void qt_read_bulk_callback(struct urb *urb)
 	/* index = MINOR(port->tty->device) - serial->minor; */
 	index = tty->index - serial->minor;
 
-	dbg("%s - port %d\n", __func__, port->number);
 	dbg("%s - port->RxHolding = %d\n", __func__, qt_port->RxHolding);
 
 	if (port_paranoia_check(port, __func__) != 0) {
@@ -726,8 +723,6 @@ static int qt_startup(struct usb_serial *serial)
 	int i;
 	int status;
 
-	dbg("enterting %s", __func__);
-
 	/* Now setup per port private data */
 	for (i = 0; i < serial->num_ports; i++) {
 		port = serial->port[i];
@@ -855,8 +850,6 @@ static void qt_release(struct usb_serial *serial)
 	struct quatech_port *qt_port;
 	int i;
 
-	dbg("enterting %s", __func__);
-
 	for (i = 0; i < serial->num_ports; i++) {
 		port = serial->port[i];
 		if (!port)
@@ -881,8 +874,6 @@ static int qt_open(struct tty_struct *tty,
 
 	if (port_paranoia_check(port, __func__))
 		return -ENODEV;
-
-	dbg("%s - port %d\n", __func__, port->number);
 
 	serial = port->serial;
 
@@ -1006,8 +997,6 @@ static int qt_chars_in_buffer(struct tty_struct *tty)
 
 	serial = get_usb_serial(port, __func__);
 
-	dbg("%s - port %d\n", __func__, port->number);
-
 	if (serial->num_bulk_out) {
 		if (port->write_urb->status == -EINPROGRESS)
 			chars = port->write_urb->transfer_buffer_length;
@@ -1053,8 +1042,6 @@ static void qt_close(struct usb_serial_port *port)
 	int status;
 	unsigned int index;
 	status = 0;
-
-	dbg("%s - port %d\n", __func__, port->number);
 
 	tty = tty_port_tty_get(&port->port);
 	index = tty->index - serial->minor;
@@ -1108,8 +1095,6 @@ static int qt_write(struct tty_struct *tty, struct usb_serial_port *port,
 
 	if (serial == NULL)
 		return -ENODEV;
-
-	dbg("%s - port %d\n", __func__, port->number);
 
 	if (count == 0) {
 		dbg("%s - write request of 0 bytes\n", __func__);
@@ -1172,8 +1157,6 @@ static int qt_write_room(struct tty_struct *tty)
 	qt_port = qt_get_port_private(port);
 
 	mutex_lock(&qt_port->lock);
-
-	dbg("%s - port %d\n", __func__, port->number);
 
 	if (serial->num_bulk_out) {
 		if (port->write_urb->status != -EINPROGRESS)
@@ -1240,8 +1223,6 @@ static void qt_set_termios(struct tty_struct *tty,
 	unsigned int index;
 	int baud, divisor, remainder;
 	int status;
-
-	dbg("%s", __func__);
 
 	index = tty->index - port->serial->minor;
 
@@ -1365,8 +1346,6 @@ static void qt_break(struct tty_struct *tty, int break_state)
 
 	mutex_lock(&qt_port->lock);
 
-	dbg("%s - port %d\n", __func__, port->number);
-
 	result =
 	    usb_control_msg(serial->dev, usb_sndctrlpipe(serial->dev, 0),
 			    QT_BREAK_CONTROL, 0x40, onoff, index, NULL, 0, 300);
@@ -1384,8 +1363,6 @@ static inline int qt_real_tiocmget(struct tty_struct *tty,
 	unsigned int result = 0;
 	int status;
 	unsigned int index;
-
-	dbg("%s - port %d, tty =0x%p\n", __func__, port->number, tty);
 
 	index = tty->index - serial->minor;
 	status =
@@ -1426,8 +1403,6 @@ static inline int qt_real_tiocmset(struct tty_struct *tty,
 	int status;
 	unsigned int index;
 
-	dbg("%s - port %d\n", __func__, port->number);
-
 	index = tty->index - serial->minor;
 	status =
 	    BoxGetRegister(port->serial, index, MODEM_CONTROL_REGISTER, &mcr);
@@ -1461,18 +1436,11 @@ static int qt_tiocmget(struct tty_struct *tty)
 	struct quatech_port *qt_port = qt_get_port_private(port);
 	int retval = -ENODEV;
 
-	dbg("In %s\n", __func__);
-
 	if (!serial)
 		return -ENODEV;
 
 	mutex_lock(&qt_port->lock);
-
-	dbg("%s - port %d\n", __func__, port->number);
-	dbg("%s - port->RxHolding = %d\n", __func__, qt_port->RxHolding);
-
 	retval = qt_real_tiocmget(tty, port, serial);
-
 	mutex_unlock(&qt_port->lock);
 	return retval;
 }
@@ -1486,18 +1454,11 @@ static int qt_tiocmset(struct tty_struct *tty,
 	struct quatech_port *qt_port = qt_get_port_private(port);
 	int retval = -ENODEV;
 
-	dbg("In %s\n", __func__);
-
 	if (!serial)
 		return -ENODEV;
 
 	mutex_lock(&qt_port->lock);
-
-	dbg("%s - port %d\n", __func__, port->number);
-	dbg("%s - qt_port->RxHolding = %d\n", __func__, qt_port->RxHolding);
-
 	retval = qt_real_tiocmset(tty, port, serial, set);
-
 	mutex_unlock(&qt_port->lock);
 	return retval;
 }
@@ -1508,8 +1469,6 @@ static void qt_throttle(struct tty_struct *tty)
 	struct usb_serial *serial = get_usb_serial(port, __func__);
 	struct quatech_port *qt_port;
 
-	dbg("%s - port %d\n", __func__, port->number);
-
 	if (!serial)
 		return;
 
@@ -1519,7 +1478,6 @@ static void qt_throttle(struct tty_struct *tty)
 
 	/* pass on to the driver specific version of this function */
 	qt_port->RxHolding = 1;
-	dbg("%s - port->RxHolding = 1\n", __func__);
 
 	mutex_unlock(&qt_port->lock);
 	return;
@@ -1538,8 +1496,6 @@ static void qt_unthrottle(struct tty_struct *tty)
 	qt_port = qt_get_port_private(port);
 
 	mutex_lock(&qt_port->lock);
-
-	dbg("%s - port %d\n", __func__, port->number);
 
 	if (qt_port->RxHolding == 1) {
 		dbg("%s -qt_port->RxHolding == 1\n", __func__);
