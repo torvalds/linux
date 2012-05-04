43a18b1e588d1b6a993eedd44dd3154590d9bebd
Thomas Gleixner <tglx@linutronix.de>
2012-05-04 12:52:25 +0200

    smp: Fix idle_thread_init() inline stub
    idle_thread_init() does not have arguments.
    
    Reported-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/smpboot.h
+++ kernel/smpboot.h
@@ -12,7 +12,7 @@ void idle_threads_init(void);
 #else
 static inline struct task_struct *idle_thread_get(unsigned int cpu) { return NULL; }
 static inline void idle_thread_set_boot_cpu(void) { }
-static inline void idle_threads_init(unsigned int cpu) { }
+static inline void idle_threads_init(void) { }
 #endif
 
 #endif
