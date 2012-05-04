ada3a3d0cb75f4cb015a2521badcffcf6d16ba45
Russ Dill <Russ.Dill@ti.com>
2012-05-04 04:24:47 -0700

    USB: EHCI: OMAP: Finish ehci omap phy reset cycle before adding hcd.
    commit 3aa2ae74ba630ec9b98736d64aea8e4cb490861d upstream.
    
    'ARM: OMAP3: USB: Fix the EHCI ULPI PHY reset issue' (1fcb57d0f) created a regression
    with Beagleboard xM if booting the kernel after running 'usb start' under u-boot.
    
    Finishing the reset before calling 'usb_add_hcd' fixes the regression. This is most likely due to
    usb_add_hcd calling the driver's reset and init functions which expect the hardware to be
    up and running.
    
    Signed-off-by: Russ Dill <Russ.Dill@ti.com>
    Acked-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/ehci-omap.c
+++ drivers/usb/host/ehci-omap.c
@@ -242,15 +242,6 @@ static int ehci_hcd_omap_probe(struct platform_device *pdev)
 
 	ehci_reset(omap_ehci);
 
-	ret = usb_add_hcd(hcd, irq, IRQF_SHARED);
-	if (ret) {
-		dev_err(dev, "failed to add hcd with err %d\n", ret);
-		goto err_add_hcd;
-	}
-
-	/* root ports should always stay powered */
-	ehci_port_power(omap_ehci, 1);
-
 	if (pdata->phy_reset) {
 		/* Hold the PHY in RESET for enough time till
 		 * PHY is settled and ready
@@ -263,6 +254,15 @@ static int ehci_hcd_omap_probe(struct platform_device *pdev)
 		if (gpio_is_valid(pdata->reset_gpio_port[1]))
 			gpio_set_value(pdata->reset_gpio_port[1], 1);
 	}
+
+	ret = usb_add_hcd(hcd, irq, IRQF_SHARED);
+	if (ret) {
+		dev_err(dev, "failed to add hcd with err %d\n", ret);
+		goto err_add_hcd;
+	}
+
+	/* root ports should always stay powered */
+	ehci_port_power(omap_ehci, 1);
 
 	return 0;
 
