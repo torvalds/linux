16b79bb8ec4f20527525f8c063ae3e4d77388e97
Jiri Kosina <jkosina@suse.cz>
2012-05-05 23:32:54 +0200

    HID: multitouch: MT interface matching for Baanto
    Baanto device entry needs to be altered in the same way other device
    entries have been changed in 2c2110e90b5 ("HID: hid-multitouch: Only match
    MT interfaces").
    
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -762,7 +762,7 @@ static const struct hid_device_id mt_devices[] = {
 
 	/* Baanto multitouch devices */
 	{ .driver_data = MT_CLS_DEFAULT,
-		HID_USB_DEVICE(USB_VENDOR_ID_BAANTO,
+		MT_USB_DEVICE(USB_VENDOR_ID_BAANTO,
 			USB_DEVICE_ID_BAANTO_MT_190W2) },
 	/* Cando panels */
 	{ .driver_data = MT_CLS_DUAL_INRANGE_CONTACTNUMBER,
