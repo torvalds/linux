8e8a507c2427c960c5d1fe58a250635b35f3acdc
Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
2012-05-07 15:57:23 +0800

    regulator: da9052: fix bug in device tree iteration loop
    The driver of da9052 is buggy due to the iteration loop of device tree.
    This patch fix the loop condition to make the driver work with device tree.
    
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- drivers/regulator/da9052-regulator.c
+++ drivers/regulator/da9052-regulator.c
@@ -444,7 +444,7 @@ static int __devinit da9052_regulator_probe(struct platform_device *pdev)
 		if (!nproot)
 			return -ENODEV;
 
-		for (np = of_get_next_child(nproot, NULL); !np;
+		for (np = of_get_next_child(nproot, NULL); np;
 		     np = of_get_next_child(nproot, np)) {
 			if (!of_node_cmp(np->name,
 					 regulator->info->reg_desc.name)) {
