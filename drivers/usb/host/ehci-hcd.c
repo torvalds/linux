47fc28bff82a4dd5f6b41c97e335d10fc78a8e9a
Chris Metcalf <cmetcalf@tilera.com>
2012-05-09 13:58:14 -0400

    usb: add host support for the tilegx architecture
    This change adds OHCI and EHCI support for the tilegx's on-chip
    USB hardware.
    
    Signed-off-by: Chris Metcalf <cmetcalf@tilera.com>

--- drivers/usb/host/ehci-hcd.c
+++ drivers/usb/host/ehci-hcd.c
@@ -1349,6 +1349,11 @@ MODULE_LICENSE ("GPL");
 #define PLATFORM_DRIVER		ehci_msm_driver
 #endif
 
+#ifdef CONFIG_TILE_USB
+#include "ehci-tilegx.c"
+#define	PLATFORM_DRIVER		ehci_hcd_tilegx_driver
+#endif
+
 #ifdef CONFIG_USB_EHCI_HCD_PMC_MSP
 #include "ehci-pmcmsp.c"
 #define	PLATFORM_DRIVER		ehci_hcd_msp_driver
