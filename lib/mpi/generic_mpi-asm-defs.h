9e235dcaf4f63d88a7e9ce5735ba5c2eb2719603
Dmitry Kasatkin <dmitry.kasatkin@intel.com>
2012-05-09 17:37:55 +0300

    Revert "crypto: GnuPG based MPI lib - additional sources (part 4)"
    This reverts commit 7e8dec918ef8e0f68b4937c3c50fa57002077a4d.
    
    RSA verification implementation does not use this code.
    James Morris has asked to remove that.
    
    Signed-off-by: Dmitry Kasatkin <dmitry.kasatkin@intel.com>
    Requested-by: James Morris <james.l.morris@oracle.com>
    Signed-off-by: James Morris <james.l.morris@oracle.com>

--- lib/mpi/generic_mpi-asm-defs.h
+++
@@ -1,4 +0,0 @@
-/* This file defines some basic constants for the MPI machinery.  We
- * need to define the types on a per-CPU basis, so it is done with
- * this file here.  */
-#define BYTES_PER_MPI_LIMB  (SIZEOF_UNSIGNED_LONG)
