3507efa08417f416a8b8a64ab701ad5df37ae077
Luciano Coelho <coelho@ti.com>
2012-05-10 12:14:21 +0300

    wl18xx: increase tx_ba_win_size to 64
    Now the firmware can support TX block ack sessions with 64 frames.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>
    Signed-off-by: Arik Nemtsov <arik@wizery.com>

--- drivers/net/wireless/ti/wl18xx/main.c
+++ drivers/net/wireless/ti/wl18xx/main.c
@@ -408,7 +408,7 @@ static struct wlcore_conf wl18xx_conf = {
 	},
 	.ht = {
 		.rx_ba_win_size = 10,
-		.tx_ba_win_size = 10,
+		.tx_ba_win_size = 64,
 		.inactivity_timeout = 10000,
 		.tx_ba_tid_bitmap = CONF_TX_BA_ENABLED_TID_BITMAP,
 	},
