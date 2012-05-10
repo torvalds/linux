0b25b7c8cba83fa8c02fbf38eb905996f2455dd0
Phil Edworthy <phil.edworthy@renesas.com>
2012-05-10 09:26:52 +0100

    sh: Add sh7269 device
    This is an sh2a device (max 266MHz) with FPU, video display
    controller (VDC), 8 serial ports, 4 I2C channels, 3 CAN ports,
    SD and on-chip USB.
    
    Signed-off-by: Phil Edworthy <phil.edworthy@renesas.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/cpu/sh2a/probe.c
+++ arch/sh/kernel/cpu/sh2a/probe.c
@@ -32,6 +32,9 @@ void __cpuinit cpu_probe(void)
 #elif defined(CONFIG_CPU_SUBTYPE_SH7264)
 	boot_cpu_data.type			= CPU_SH7264;
 	boot_cpu_data.flags			|= CPU_HAS_FPU;
+#elif defined(CONFIG_CPU_SUBTYPE_SH7269)
+	boot_cpu_data.type			= CPU_SH7269;
+	boot_cpu_data.flags			|= CPU_HAS_FPU;
 #elif defined(CONFIG_CPU_SUBTYPE_SH7206)
 	boot_cpu_data.type			= CPU_SH7206;
 	boot_cpu_data.flags			|= CPU_HAS_DSP;
