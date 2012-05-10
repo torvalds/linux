93278814d3590eba0ee360b8d69a35c7f2203ea8
Sasha Levin <levinsasha928@gmail.com>
2012-05-10 13:01:44 -0700

    mm: fix division by 0 in percpu_pagelist_fraction()
    percpu_pagelist_fraction_sysctl_handler() has only considered -EINVAL as
    a possible error from proc_dointvec_minmax().
    
    If any other error is returned, it would proceed to divide by zero since
    percpu_pagelist_fraction wasn't getting initialized at any point.  For
    example, writing 0 bytes into the proc file would trigger the issue.
    
    Signed-off-by: Sasha Levin <levinsasha928@gmail.com>
    Reviewed-by: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -105,7 +105,7 @@ unsigned long totalreserve_pages __read_mostly;
  */
 unsigned long dirty_balance_reserve __read_mostly;
 
-int percpu_pagelist_fraction;
+int percpu_pagelist_fraction = 8;
 gfp_t gfp_allowed_mask __read_mostly = GFP_BOOT_MASK;
 
 #ifdef CONFIG_PM_SLEEP
@@ -5203,7 +5203,7 @@ int percpu_pagelist_fraction_sysctl_handler(ctl_table *table, int write,
 	int ret;
 
 	ret = proc_dointvec_minmax(table, write, buffer, length, ppos);
-	if (!write || (ret == -EINVAL))
+	if (!write || (ret < 0))
 		return ret;
 	for_each_populated_zone(zone) {
 		for_each_possible_cpu(cpu) {
