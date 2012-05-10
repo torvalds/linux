10b1e8a27a45559df7d02c864081e929369d83bf
Luciano Coelho <coelho@ti.com>
2012-05-10 12:14:08 +0300

    wl12xx: implement fw status debugfs entries
    Implement the operations that are necessary to fetch the
    wl12xx-specific FW statistics.  Re-add some of the code removed from
    wlcore.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>
    Signed-off-by: Arik Nemtsov <arik@wizery.com>

--- drivers/net/wireless/ti/wl12xx/main.c
+++ drivers/net/wireless/ti/wl12xx/main.c
@@ -39,6 +39,7 @@
 #include "reg.h"
 #include "cmd.h"
 #include "acx.h"
+#include "debugfs.h"
 
 static char *fref_param;
 static char *tcxo_param;
@@ -1359,6 +1360,11 @@ out:
 	return ret;
 }
 
+static int wl12xx_debugfs_init(struct wl1271 *wl, struct dentry *rootdir)
+{
+	return wl12xx_debugfs_add_files(wl, rootdir);
+}
+
 static struct wlcore_ops wl12xx_ops = {
 	.identify_chip		= wl12xx_identify_chip,
 	.identify_fw		= wl12xx_identify_fw,
@@ -1381,6 +1387,7 @@ static struct wlcore_ops wl12xx_ops = {
 	.set_tx_desc_csum	= wl12xx_set_tx_desc_csum,
 	.set_rx_csum		= NULL,
 	.ap_get_mimo_wide_rate_mask = NULL,
+	.debugfs_init		= wl12xx_debugfs_init,
 };
 
 static struct ieee80211_sta_ht_cap wl12xx_ht_cap = {
@@ -1422,6 +1429,7 @@ static int __devinit wl12xx_probe(struct platform_device *pdev)
 	wl->hw_tx_rate_tbl_size = WL12XX_CONF_HW_RXTX_RATE_MAX;
 	wl->hw_min_ht_rate = WL12XX_CONF_HW_RXTX_RATE_MCS0;
 	wl->fw_status_priv_len = 0;
+	wl->stats.fw_stats_len = sizeof(struct wl12xx_acx_statistics);
 	memcpy(&wl->ht_cap, &wl12xx_ht_cap, sizeof(wl12xx_ht_cap));
 	wl12xx_conf_init(wl);
 
