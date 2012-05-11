416289b14a8ab1a0fea7b7ee03724c4820578427
Samuel Iglesias Gonsalvez <siglesias@igalia.com>
2012-05-11 10:17:13 +0200

    Staging: ipack: change the licence to explicitly GPLv2
    Change the licence to explicitly GPLv2 to avoid possible conflicts in the
    future.
    
    Signed-off-by: Samuel Iglesias Gonsalvez <siglesias@igalia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ipack/ipack.c
+++ drivers/staging/ipack/ipack.c
@@ -6,8 +6,7 @@
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
+ * Software Foundation; version 2 of the License.
  */
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
