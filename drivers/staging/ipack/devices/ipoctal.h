32254363c2bb1a5384e8919ea706cf0355ecc2eb
Samuel Iglesias Gonsalvez <siglesias@igalia.com>
2012-05-11 10:17:15 +0200

    Staging: ipack/devices/ipoctal: change the licence to explicitly GPLv2
    Change the licence to explicitly GPLv2 to avoid possible conflicts in the
    future.
    
    Signed-off-by: Samuel Iglesias Gonsalvez <siglesias@igalia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ipack/devices/ipoctal.h
+++ drivers/staging/ipack/devices/ipoctal.h
@@ -8,8 +8,7 @@
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
+ * Software Foundation; version 2 of the License.
  */
 
 #ifndef _IPOCTAL_H
