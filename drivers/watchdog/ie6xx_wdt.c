491d9e2a5f0e845a91ef53065afb4805a668d80d
Randy Dunlap <rdunlap@xenotime.net>
2012-05-14 13:14:29 -0700

    watchdog: ie6xx_wdt needs io.h
    Fix build error by including <linux/io.h>:
    
    drivers/watchdog/ie6xx_wdt.c:97:2: error: implicit declaration of function 'outb'
    drivers/watchdog/ie6xx_wdt.c:133:2: error: implicit declaration of function 'outl'
    drivers/watchdog/ie6xx_wdt.c:161:2: error: implicit declaration of function 'inb'
    drivers/watchdog/ie6xx_wdt.c:199:3: error: implicit declaration of function 'inl'
    drivers/watchdog/ie6xx_wdt.c:203:3: error: implicit declaration of function 'inw'
    
    Signed-off-by: Randy Dunlap <rdunlap@xenotime.net>
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/watchdog/ie6xx_wdt.c
+++ drivers/watchdog/ie6xx_wdt.c
@@ -24,6 +24,7 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/platform_device.h>
+#include <linux/io.h>
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/watchdog.h>
