d4d434a82421008a452acd022cd6730feb20851b
Dave Martin <dave.martin@linaro.org>
2012-05-14 17:40:07 +0100

    ARM: bL_switcher: Basic trace events support
    This patch adds simple trace events to the b.L switcher code
    to allow tracing of CPU migration events.
    
    To make use of the trace events, you will need:
    
    CONFIG_FTRACE=y
    CONFIG_ENABLE_DEFAULT_TRACERS=y
    
    The following events are added:
      * power:cpu_migrate_begin
      * power:cpu_migrate_finish
    
    each with the following data:
        u64     timestamp;
        u32     cpu_hwid;
    
    power:cpu_migrate_begin occurs immediately before the
    switcher-specific migration operations start.
    power:cpu_migrate_finish occurs immediately when migration is
    completed.
    
    The cpu_hwid field contains the ID fields of the MPIDR.
    
    * For power:cpu_migrate_begin, cpu_hwid is the ID of the outbound
      physical CPU (equivalent to (from_phys_cpu,from_phys_cluster)).
    
    * For power:cpu_migrate_finish, cpu_hwid is the ID of the inbound
      physical CPU (equivalent to (to_phys_cpu,to_phys_cluster)).
    
    By design, the cpu_hwid field is masked in the same way as the
    device tree cpu node reg property, allowing direct correlation to
    the DT description of the hardware.
    
    The timestamp is added in order to minimise timing noise.  An
    accurate system-wide clock should be used for generating this
    (hopefully getnstimeofday is appropriate, but it could be changed).
    It could be any monotonic shared clock, since the aim is to allow
    accurate deltas to be computed.  We don't necessarily care about
    accurate synchronisation with wall clock time.
    
    In practice, each switch takes place on a single logical CPU,
    and the trace infrastructure should guarantee that events are
    well-ordered with respect to a single logical CPU.
    
    Signed-off-by: Dave Martin <dave.martin@linaro.org>
    Signed-off-by: Nicolas Pitre <nico@linaro.org>

--- arch/arm/common/bL_switcher.c
+++ arch/arm/common/bL_switcher.c
@@ -20,6 +20,7 @@
 #include <linux/cpumask.h>
 #include <linux/kthread.h>
 #include <linux/wait.h>
+#include <linux/time.h>
 #include <linux/clockchips.h>
 #include <linux/hrtimer.h>
 #include <linux/tick.h>
@@ -34,9 +35,13 @@
 
 #include <asm/smp_plat.h>
 #include <asm/cacheflush.h>
+#include <asm/cputype.h>
 #include <asm/suspend.h>
 #include <asm/mcpm.h>
 #include <asm/bL_switcher.h>
+
+#define CREATE_TRACE_POINTS
+#include <trace/events/power_cpu_migrate.h>
 
 
 /*
@@ -50,6 +55,16 @@ static int read_mpidr(void)
 	unsigned int id;
 	asm volatile ("mrc\tp15, 0, %0, c0, c0, 5" : "=r" (id));
 	return id & MPIDR_HWID_BITMASK;
+}
+
+/*
+ * Get a global nanosecond time stamp for tracing.
+ */
+static s64 get_ns(void)
+{
+	struct timespec ts;
+	getnstimeofday(&ts);
+	return timespec_to_ns(&ts);
 }
 
 /*
@@ -209,6 +224,7 @@ static int bL_switch_to(unsigned int new_cluster_id)
 	 */
 	local_irq_disable();
 	local_fiq_disable();
+	trace_cpu_migrate_begin(get_ns(), ob_mpidr);
 
 	/* redirect GIC's SGIs to our counterpart */
 	gic_migrate_target(bL_gic_id[ib_cpu][ib_cluster]);
@@ -256,6 +272,7 @@ static int bL_switch_to(unsigned int new_cluster_id)
 					  tdev->evtdev->next_event, 1);
 	}
 
+	trace_cpu_migrate_finish(get_ns(), ib_mpidr);
 	local_fiq_enable();
 	local_irq_enable();
 
