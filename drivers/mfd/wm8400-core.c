ebd29c6cc0b29b4bb041441fc251e0f400eea2cf
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-05-14 10:13:16 +0100

    mfd: Export wm8400_block_read()
    Used by the regulator driver.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/wm8400-core.c
+++ drivers/mfd/wm8400-core.c
@@ -60,6 +60,7 @@ int wm8400_block_read(struct wm8400 *wm8400, u8 reg, int count, u16 *data)
 {
 	return regmap_bulk_read(wm8400->regmap, reg, data, count);
 }
+EXPORT_SYMBOL_GPL(wm8400_block_read);
 
 static int wm8400_register_codec(struct wm8400 *wm8400)
 {
