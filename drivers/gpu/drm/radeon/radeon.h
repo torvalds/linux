6823d74003abedd688a3f535aefe6ce0e06444fd
Christian Koenig <christian.koenig@amd.com>
2012-05-16 20:24:36 +0200

    drm/radeon: remove some unneeded structure members
    Signed-off-by: Christian Koenig <christian.koenig@amd.com>
    Reviewed-by: Jerome Glisse <jglisse@redhat.com>

--- drivers/gpu/drm/radeon/radeon.h
+++ drivers/gpu/drm/radeon/radeon.h
@@ -738,9 +738,7 @@ struct r600_ih {
 	struct radeon_bo	*ring_obj;
 	volatile uint32_t	*ring;
 	unsigned		rptr;
-	unsigned		rptr_offs;
 	unsigned		wptr;
-	unsigned		wptr_old;
 	unsigned		ring_size;
 	uint64_t		gpu_addr;
 	uint32_t		ptr_mask;
