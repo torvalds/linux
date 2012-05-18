450878759aa10c6d172a0cafd5922b23a3b4547a
Yinghai Lu <yinghai@kernel.org>
2012-05-17 18:51:13 -0700

    PCI: cpqhp: register busn_res
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/hotplug/cpqphp_pci.c
+++ drivers/pci/hotplug/cpqphp_pci.c
@@ -106,9 +106,11 @@ int cpqhp_configure_device (struct controller* ctrl, struct pci_func* func)
 	}
 
 	if (func->pci_dev->hdr_type == PCI_HEADER_TYPE_BRIDGE) {
+		int max;
 		pci_read_config_byte(func->pci_dev, PCI_SECONDARY_BUS, &bus);
 		child = (struct pci_bus*) pci_add_new_bus(func->pci_dev->bus, (func->pci_dev), bus);
-		pci_do_scan_bus(child);
+		max = pci_do_scan_bus(child);
+		pci_bus_update_busn_res_end(child, max);
 	}
 
 	pci_dev_put(func->pci_dev);
