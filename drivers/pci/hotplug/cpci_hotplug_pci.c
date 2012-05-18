f406384628e97618955e17e8d61e59d5ecdc9ca0
Yinghai Lu <yinghai@kernel.org>
2012-05-17 18:51:13 -0700

    PCI: cpci_hotplug: register busn_res
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/hotplug/cpci_hotplug_pci.c
+++ drivers/pci/hotplug/cpci_hotplug_pci.c
@@ -294,6 +294,7 @@ int __ref cpci_configure_slot(struct slot *slot)
 			struct pci_bus *child;
 			unsigned char busnr, start = parent->busn_res.start;
 			unsigned char end = parent->busn_res.end;
+			int max;
 
 			for (busnr = start; busnr <= end; busnr++) {
 				if (!pci_find_bus(pci_domain_nr(parent),
@@ -312,7 +313,8 @@ int __ref cpci_configure_slot(struct slot *slot)
 				pci_dev_put(dev);
 				continue;
 			}
-			child->busn_res.end = pci_do_scan_bus(child);
+			max = pci_do_scan_bus(child);
+			pci_bus_update_busn_res_end(child, max);
 			pci_bus_size_bridges(child);
 		}
 		pci_dev_put(dev);
