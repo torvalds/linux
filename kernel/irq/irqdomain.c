a87487e687ceafdc696b8cb1fb27e37cc463586b
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-05-19 12:15:35 +0100

    irqdomain: Document size parameter of irq_domain_add_linear()
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- kernel/irq/irqdomain.c
+++ kernel/irq/irqdomain.c
@@ -217,6 +217,7 @@ EXPORT_SYMBOL_GPL(irq_domain_add_legacy);
 /**
  * irq_domain_add_linear() - Allocate and register a legacy revmap irq_domain.
  * @of_node: pointer to interrupt controller's device tree node.
+ * @size: Number of interrupts in the domain.
  * @ops: map/unmap domain callbacks
  * @host_data: Controller private data pointer
  */
