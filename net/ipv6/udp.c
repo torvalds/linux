9a52e97e24d6da744e6c3332b8dd478a4974983e
Jeffrin Jose <ahiliation@yahoo.co.in>
2012-05-19 01:45:21 +0000

    net:ipv6:fixed a trailing white space issue.
    Fixed a trailing white space issue found by
    checkpatch.pl tool in net/ipv6/udp.c
    
    Signed-off-by: Jeffrin Jose <ahiliation@yahoo.co.in>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/udp.c
+++ net/ipv6/udp.c
@@ -103,7 +103,7 @@ int udp_v6_get_port(struct sock *sk, unsigned short snum)
 {
 	unsigned int hash2_nulladdr =
 		udp6_portaddr_hash(sock_net(sk), &in6addr_any, snum);
-	unsigned int hash2_partial = 
+	unsigned int hash2_partial =
 		udp6_portaddr_hash(sock_net(sk), &inet6_sk(sk)->rcv_saddr, 0);
 
 	/* precompute partial secondary hash */
