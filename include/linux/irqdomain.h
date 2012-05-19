cb5557bec9f14d05204a9014ae1b23aca8b04f1d
Paul Mundt <lethal@linux-sh.org>
2012-05-19 15:11:45 +0900

    irqdomain: Kill off duplicate definitions.
    Presently irqdomain.h has duplicate definitions for irq_find_host() and
    irq_set_default_host(), presumably from merge damage. Kill off the
    duplicates.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- include/linux/irqdomain.h
+++ include/linux/irqdomain.h
@@ -144,9 +144,6 @@ static inline struct irq_domain *irq_domain_add_legacy_isa(
 
 extern void irq_domain_remove(struct irq_domain *host);
 
-extern struct irq_domain *irq_find_host(struct device_node *node);
-extern void irq_set_default_host(struct irq_domain *host);
-
 extern unsigned int irq_create_mapping(struct irq_domain *host,
 				       irq_hw_number_t hwirq);
 extern void irq_dispose_mapping(unsigned int virq);
