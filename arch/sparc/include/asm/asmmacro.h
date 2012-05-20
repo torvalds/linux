0bfcee9ad369aca590211a4a8fffd28f1ed39a82
David S. Miller <davem@davemloft.net>
2012-05-19 21:22:11 -0700

    sparc32: Move GET_PROCESSOR*_ID() out of asm/asmmacro.h
    GET_PROCESSOR4D_ID is completely unused, so delete it.
    
    Move GET_PROCESSOR4M_ID to the sun4m specific trap code
    which uses it.
    
    We now no longer need to include asm/asi.h
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/asm/asmmacro.h
+++ arch/sparc/include/asm/asmmacro.h
@@ -6,16 +6,6 @@
 #ifndef _SPARC_ASMMACRO_H
 #define _SPARC_ASMMACRO_H
 
-#include <asm/asi.h>
-
-#define GET_PROCESSOR4M_ID(reg) \
-	rd	%tbr, %reg; \
-	srl	%reg, 12, %reg; \
-	and	%reg, 3, %reg;
-
-#define GET_PROCESSOR4D_ID(reg) \
-	lda	[%g0] ASI_M_VIKING_TMP1, %reg;
-
 /* All trap entry points _must_ begin with this macro or else you
  * lose.  It makes sure the kernel has a proper window so that
  * c-code can be called.
