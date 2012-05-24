4b8a147428a99a312492e10fbc16756c6a6c99ff
Alejandro Mery <amery@geeks.cl>
2012-05-24 11:11:17 +0200

    arm: sun5i: standby: use linux/types.h

--- arch/arm/mach-sun5i/pm/standby/common.h
+++ arch/arm/mach-sun5i/pm/standby/common.h
@@ -18,15 +18,7 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-typedef signed char         __s8;
-typedef unsigned char       __u8;
-typedef signed short        __s16;
-typedef unsigned short      __u16;
-typedef signed int          __s32;
-typedef unsigned int        __u32;
-typedef signed long long    __s64;
-typedef unsigned long long  __u64;
-
+#include <linux/types.h>
 
 static inline __u64 standby_uldiv(__u64 dividend, __u32 divisior)
 {
