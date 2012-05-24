b60762f1472cb68a8cd8897a35f6e940ea1c242b
lw <lw@rock-chips.com>
2012-05-24 18:05:18 +0800

    rk30_phone_loquat:modify sensors orientation

--- arch/arm/mach-rk30/board-rk30-phone-loquat.c
+++ arch/arm/mach-rk30/board-rk30-phone-loquat.c
@@ -880,8 +880,8 @@ static struct akm8975_platform_data akm8975_info =
 	.m_layout = 
 	{
 		{
-			{1, 0, 0},
 			{0, 0, 1},
+			{1, 0, 0},
 			{0, 1, 0},
 		},
 
@@ -920,7 +920,7 @@ static int l3g4200d_init_platform_hw(void)
 }
 
 static struct l3g4200d_platform_data l3g4200d_info = {
-	.orientation = {0, 1, 0, -1, 0, 0, 0, 0, 1},
+	.orientation = {1, 0, 0, 0, 1, 0, 0, 0, 1},
 	.init = l3g4200d_init_platform_hw,
 	.x_min = 40,//x_min,y_min,z_min = (0-100) according to hardware
 	.y_min = 40,
@@ -1586,7 +1586,7 @@ static struct i2c_board_info __initdata i2c0_info[] = {
 #if defined (CONFIG_INPUT_LPSENSOR_AL3006)
 	{
 		.type           = "al3006",
-		.addr           = 0x1d,             //sel = 0; if sel =1, then addr = 0x1D
+		.addr           = 0x1c,             //sel = 0; if sel =1, then addr = 0x1D
 		.flags          = 0,
 		.irq            = RK30_PIN6_PA2,
 	},
