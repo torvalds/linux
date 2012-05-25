4a43faf54e9173b6acce37cf7f053fc9515a2cdf
Sascha Hauer <s.hauer@pengutronix.de>
2012-05-25 16:22:42 +0200

    mtd: mxc_nand: move ecc strengh setup before nand_scan_tail
    Since commit 6a918bade9dab40aaef80559bd1169c69e8d69cb, the mxc_nand driver
    fails with:
    
    Driver must set ecc.strength when using hardware ECC
    
    This is because nand_scan_tail checks for correct ecc strength
    settings, so we must set them up before nand_scan_tail.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>
    Cc: stable@vger.kernel.org [3.4+]
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/nand/mxc_nand.c
+++ drivers/mtd/nand/mxc_nand.c
@@ -1465,17 +1465,17 @@ static int __init mxcnd_probe(struct platform_device *pdev)
 	else if (mtd->writesize == 4096)
 		this->ecc.layout = host->devtype_data->ecclayout_4k;
 
-	/* second phase scan */
-	if (nand_scan_tail(mtd)) {
-		err = -ENXIO;
-		goto escan;
-	}
-
 	if (this->ecc.mode == NAND_ECC_HW) {
 		if (nfc_is_v1())
 			this->ecc.strength = 1;
 		else
 			this->ecc.strength = (host->eccsize == 4) ? 4 : 8;
+	}
+
+	/* second phase scan */
+	if (nand_scan_tail(mtd)) {
+		err = -ENXIO;
+		goto escan;
 	}
 
 	/* Register the partitions */
