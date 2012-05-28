359d7d1c976851c658aa7085761015812ed3b56f
Trond Myklebust <Trond.Myklebust@netapp.com>
2012-05-28 10:01:34 -0400

    NFSv4: update_changeattr does not need to set NFS_INO_REVAL_PAGECACHE
    We're already invalidating the data cache, and setting the new change
    attribute. Since directories don't care about the i_size field, there
    is no need to be forcing any extra revalidation of the page cache.
    
    We do keep the NFS_INO_INVALID_ATTR flag, in order to force an
    attribute cache revalidation on stat() calls since we do not
    update the mtime and ctime fields.
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -774,7 +774,7 @@ static void update_changeattr(struct inode *dir, struct nfs4_change_info *cinfo)
 	struct nfs_inode *nfsi = NFS_I(dir);
 
 	spin_lock(&dir->i_lock);
-	nfsi->cache_validity |= NFS_INO_INVALID_ATTR|NFS_INO_REVAL_PAGECACHE|NFS_INO_INVALID_DATA;
+	nfsi->cache_validity |= NFS_INO_INVALID_ATTR|NFS_INO_INVALID_DATA;
 	if (!cinfo->atomic || cinfo->before != dir->i_version)
 		nfs_force_lookup_revalidate(dir);
 	dir->i_version = cinfo->after;
