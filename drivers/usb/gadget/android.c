c8479b8fffbab3130d96dcb2363691bf0c8cc5c1
Benoit Goby <benoit@android.com>
2012-05-29 13:57:27 -0700

    usb: gadget: android: Fix product name
    Product names may contain spaces and scanf %s only matches the 1st word.
    Use strlcpy instead.
    
    Change-Id: Ie8703fea9775f7fc17fe615a42597ca3816d36b0
    Signed-off-by: Benoit Goby <benoit@android.com>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -1053,10 +1053,7 @@ field ## _store(struct device *dev, struct device_attribute *attr,	\
 {									\
 	if (size >= sizeof(buffer))					\
 		return -EINVAL;						\
-	if (sscanf(buf, "%s", buffer) == 1) {				\
-		return size;						\
-	}								\
-	return -1;							\
+	return strlcpy(buffer, buf, sizeof(buffer));			\
 }									\
 static DEVICE_ATTR(field, S_IRUGO | S_IWUSR, field ## _show, field ## _store);
 
