74416e1e07660798379ce10a210bf4fd35b84f9f
Alex Williamson <alex.williamson@redhat.com>
2012-05-30 14:18:41 -0600

    driver core: Add iommu_group tracking to struct device
    IOMMU groups allow IOMMU drivers to represent DMA visibility
    and isolation of devices.  Multiple devices may be grouped
    together for the purposes of DMA.  Placing a pointer on
    struct device enable easy access for things like streaming
    DMA programming and drivers like VFIO.
    
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- include/linux/device.h
+++ include/linux/device.h
@@ -36,6 +36,7 @@ struct subsys_private;
 struct bus_type;
 struct device_node;
 struct iommu_ops;
+struct iommu_group;
 
 struct bus_attribute {
 	struct attribute	attr;
@@ -687,6 +688,7 @@ struct device {
 	const struct attribute_group **groups;	/* optional groups */
 
 	void	(*release)(struct device *dev);
+	struct iommu_group	*iommu_group;
 };
 
 /* Get the wakeup routines, which depend on struct device */
