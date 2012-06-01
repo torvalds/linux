31af49db7b2d700fdaf26ab55ca96428bc2e8b06
Gregory CLEMENT <gregory.clement@free-electrons.com>
2012-06-01 18:21:46 +0200

    arm: mach-mvebu: add source files
    [ben.dooks@codethink.co.uk: fixup style error in system-controller]
    [ben.dooks@codethink.co.uk: check result of of_match_node()]
    
    Signed-off-by: Gregory CLEMENT <gregory.clement@free-electrons.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Lior Amsalem <alior@marvell.com>
    Acked-by: Andrew Lunn <andrew@lunn.ch>
    Tested-by: Yehuda Yitschak <yehuday@marvell.com>
    Tested-by: Lior Amsalem <alior@marvell.com>
    Tested-by: Andrew Lunn <andrew@lunn.ch>

---
+++ arch/arm/mach-mvebu/common.h
@@ -0,0 +1,20 @@
+/*
+ * Core functions for Marvell System On Chip
+ *
+ * Copyright (C) 2012 Marvell
+ *
+ * Lior Amsalem <alior@marvell.com>
+ * Gregory CLEMENT <gregory.clement@free-electrons.com>
+ * Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2.  This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#ifndef __ARCH_MVEBU_COMMON_H
+#define __ARCH_MVEBU_COMMON_H
+
+void mvebu_restart(char mode, const char *cmd);
+
+#endif
