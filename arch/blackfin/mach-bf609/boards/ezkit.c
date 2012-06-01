335dd559b5c870b4ca3111b46f3358389c1ead3b
Scott Jiang <scott.jiang.linux@gmail.com>
2012-06-01 18:12:52 -0400

    bf609: add ssm2602 support on bf609 platform
    Signed-off-by: Scott Jiang <scott.jiang.linux@gmail.com>
    Signed-off-by: Bob Liu <lliubbo@gmail.com>

--- arch/blackfin/mach-bf609/boards/ezkit.c
+++ arch/blackfin/mach-bf609/boards/ezkit.c
@@ -1278,6 +1278,11 @@ static struct i2c_board_info __initdata bfin_i2c_board_info0[] = {
 		.platform_data = (void *)&adau1761_info
 	},
 #endif
+#if defined(CONFIG_SND_SOC_SSM2602) || defined(CONFIG_SND_SOC_SSM2602_MODULE)
+	{
+		I2C_BOARD_INFO("ssm2602", 0x1b),
+	},
+#endif
 };
 
 static struct i2c_board_info __initdata bfin_i2c_board_info1[] = {
