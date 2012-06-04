b7884eb45ec98c0d34c7f49005ae9d4b4b4e38f6
Daniel Vetter <daniel.vetter@ffwll.ch>
2012-06-04 11:18:15 +0200

    drm/i915: hold forcewake around ring hw init
    Empirical evidence suggests that we need to: On at least one ivb
    machine when running the hangman i-g-t test, the rings don't properly
    initialize properly - the RING_START registers seems to be stuck at
    all zeros.
    
    Holding forcewake around this register init sequences makes chip reset
    reliable again. Note that this is not the first such issue:
    
    commit f01db988ef6f6c70a6cc36ee71e4a98a68901229
    Author: Sean Paul <seanpaul@chromium.org>
    Date:   Fri Mar 16 12:43:22 2012 -0400
    
        drm/i915: Add wait_for in init_ring_common
    
    added delay loops to make RING_START and RING_CTL initialization
    reliable on the blt ring at boot-up. So I guess it won't hurt if we do
    this unconditionally for all force_wake needing gpus.
    
    To avoid copy&pasting of the HAS_FORCE_WAKE check I've added a new
    intel_info bit for that.
    
    v2: Fixup missing commas in static struct and properly handling the
    error case in init_ring_common, both noticed by Jani Nikula.
    
    Cc: stable@vger.kernel.org
    Reported-and-tested-by: Yang Guang <guang.a.yang@intel.com>
    Reviewed-by: Eugeni Dodonov <eugeni.dodonov@intel.com>
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=50522
    Signed-Off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_ringbuffer.c
+++ drivers/gpu/drm/i915/intel_ringbuffer.c
@@ -266,9 +266,14 @@ u32 intel_ring_get_active_head(struct intel_ring_buffer *ring)
 
 static int init_ring_common(struct intel_ring_buffer *ring)
 {
-	drm_i915_private_t *dev_priv = ring->dev->dev_private;
+	struct drm_device *dev = ring->dev;
+	drm_i915_private_t *dev_priv = dev->dev_private;
 	struct drm_i915_gem_object *obj = ring->obj;
+	int ret = 0;
 	u32 head;
+
+	if (HAS_FORCE_WAKE(dev))
+		gen6_gt_force_wake_get(dev_priv);
 
 	/* Stop the ring if it's running. */
 	I915_WRITE_CTL(ring, 0);
@@ -317,7 +322,8 @@ static int init_ring_common(struct intel_ring_buffer *ring)
 				I915_READ_HEAD(ring),
 				I915_READ_TAIL(ring),
 				I915_READ_START(ring));
-		return -EIO;
+		ret = -EIO;
+		goto out;
 	}
 
 	if (!drm_core_check_feature(ring->dev, DRIVER_MODESET))
@@ -329,7 +335,11 @@ static int init_ring_common(struct intel_ring_buffer *ring)
 		ring->last_retired_head = -1;
 	}
 
-	return 0;
+out:
+	if (HAS_FORCE_WAKE(dev))
+		gen6_gt_force_wake_put(dev_priv);
+
+	return ret;
 }
 
 static int
