cd3d67609055fb0fbd3ccfee9b1a90d4898959be
Tom Cubie <tangliang@allwinnertech.com>
2012-06-04 16:25:37 +0800

    ARM: sun5i: clean the license problem of mach-sun5i

--- arch/arm/mach-sun5i/dma/dma.c
+++ arch/arm/mach-sun5i/dma/dma.c
@@ -1,18 +1,25 @@
 /*
- * arch\arm\mach-sun5i\dma\dma.c
- * (C) Copyright 2007-2011
+ * arch/arm/mach-sun5i/dma/dma.c
+ *
+ * (C) Copyright 2007-2012
  * Allwinner Technology Co., Ltd. <www.allwinnertech.com>
  * huangxin <huangxin@allwinnertech.com>
- *
- * some simple description for this code
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
  */
-//#define DEBUG
 
 #include <linux/module.h>
 #include <linux/init.h>
@@ -34,6 +41,7 @@
 #include <mach/dma.h>
 #include "dma_regs.h"
 
+#undef DEBUG
 
 /* io map for dma */
 static void __iomem *dma_base;
