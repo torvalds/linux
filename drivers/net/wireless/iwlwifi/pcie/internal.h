1ce8658c08f1c1baa72e3cdea43a9d715dad08a2
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2012-06-04 16:48:17 +0300

    iwlwifi: don't configure a txq that is being disabled
    This is not needed, we just need to tell the SCD not to use
    that queue. We will reconfigure that queue when we will use
    it again.
    
    Clean up a bit the code on the way.
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/iwlwifi/pcie/internal.h
+++ drivers/net/wireless/iwlwifi/pcie/internal.h
@@ -340,10 +340,6 @@ void iwl_trans_txq_update_byte_cnt_tbl(struct iwl_trans *trans,
 				       struct iwl_tx_queue *txq,
 				       u16 byte_cnt);
 void iwl_trans_pcie_txq_disable(struct iwl_trans *trans, int queue);
-void iwl_trans_set_wr_ptrs(struct iwl_trans *trans, int txq_id, u32 index);
-void iwl_trans_tx_queue_set_status(struct iwl_trans *trans,
-				   struct iwl_tx_queue *txq,
-				   int tx_fifo_id, bool active);
 void __iwl_trans_pcie_txq_enable(struct iwl_trans *trans, int txq_id,
 					int fifo, int sta_id, int tid,
 					int frame_limit, u16 ssn);
