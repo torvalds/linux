ae1aaa9c369478ec61380220aa29d904a8f99960
Tom Cubie <tangliang@allwinnertech.com>
2012-06-04 16:44:03 +0800

    ARM: sunxi: clean sata, cpufreq, mmc driver license problem

--- drivers/mmc/sunxi-host/host_op.h
+++ drivers/mmc/sunxi-host/host_op.h
@@ -4,14 +4,13 @@
  * Allwinner Technology Co., Ltd. <www.allwinnertech.com>
  * Aaron.Maoye <leafy.myeh@allwinnertech.com>
  *
- * description for this code
- *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
  *
  */
+
 #ifndef _SW_HOST_OP_H_
 #define _SW_HOST_OP_H_ "host_op.h"
 
