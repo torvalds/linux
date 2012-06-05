c2238f10e0c34a85a2a555c8a197316d1ca3fb7e
Chen Gong <gong.chen@linux.intel.com>
2012-06-05 10:35:02 +0800

    x86/mce: Fix the MCE poll timer logic
    In commit 82f7af09 (x86/mce: Cleanup timer mess), Thomas just forgot
    the "/ 2" there while cleaning up.
    
    Signed-off-by: Chen Gong <gong.chen@linux.intel.com>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/x86/kernel/cpu/mcheck/mce.c
+++ arch/x86/kernel/cpu/mcheck/mce.c
@@ -1266,7 +1266,7 @@ static void mce_timer_fn(unsigned long data)
 	 */
 	iv = __this_cpu_read(mce_next_interval);
 	if (mce_notify_irq())
-		iv = max(iv, (unsigned long) HZ/100);
+		iv = max(iv / 2, (unsigned long) HZ/100);
 	else
 		iv = min(iv * 2, round_jiffies_relative(check_interval * HZ));
 	__this_cpu_write(mce_next_interval, iv);
