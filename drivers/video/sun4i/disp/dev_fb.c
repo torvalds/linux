0caab988c9da8e6a56e76259b3fb794ef3a3ddde
Tom Cubie <tangliang@allwinnertech.com>
2012-06-05 15:36:35 +0800

    disp: sun4i: clean license problem

--- drivers/video/sun4i/disp/dev_fb.c
+++ drivers/video/sun4i/disp/dev_fb.c
@@ -1,3 +1,26 @@
+/*
+ * drivers/video/sun4i/disp/dev_fb.c
+ *
+ * (C) Copyright 2007-2012
+ * Allwinner Technology Co., Ltd. <www.allwinnertech.com>
+ * Danling <danliang@allwinnertech.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
 #include <linux/module.h>
 #include "drv_disp_i.h"
 #include "dev_disp.h"
@@ -8,7 +31,6 @@ extern fb_info_t g_fbi;
 
 #define FBHANDTOID(handle)  ((handle) - 100)
 #define FBIDTOHAND(ID)  ((ID) + 100)
-
 
 //              0:ARGB    1:BRGA    2:ABGR    3:RGBA
 //seq           ARGB        BRGA       ARGB       BRGA
