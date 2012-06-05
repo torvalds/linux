e4aafc1846a7653123f83f4a407fe2613d1e76ba
Tom Cubie <tangliang@allwinnertech.com>
2012-06-05 15:10:49 +0800

    disp: sun3i: clean license problem

--- drivers/video/sun3i/disp/OSAL/csp/csp_ccm_para.h
+++ drivers/video/sun3i/disp/OSAL/csp/csp_ccm_para.h
@@ -1,22 +1,25 @@
 /*
-*******************************************************************************
-*           				eBase
-*                 the Abstract of Hardware
-*
-*
-*              (c) Copyright 2006-2010, ALL WINNER TECH.
-*           								All Rights Reserved
-*
-* File     :  D:\winners\eBase\eBSP\CSP\sun_20\HW_CCMU\ccm_para.h
-* Date     :  2010/11/27 12:19
-* By       :  Sam.Wu
-* Version  :  V1.00
-* Description :
-* Update   :  date      author      version
-*
-* notes    :
-*******************************************************************************
-*/
+ * drivers/video/sun3i/disp/OSAL/csp/csp_ccm_para.h
+ *
+ * (C) Copyright 2007-2012
+ * Allwinner Technology Co., Ltd. <www.allwinnertech.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
 #ifndef _CSP_CCM_PARA_H_
 #define _CSP_CCM_PARA_H_
 
--- drivers/video/sun3i/disp/include/eBSP_basetype/ebase_const.h
+++ drivers/video/sun3i/disp/include/eBSP_basetype/ebase_const.h
@@ -1,18 +1,26 @@
 /*
-*********************************************************************************************************
-*											        eBase
-*
-*
-*
-*						        (c) Copyright 2006-2010, holigun China
-*											All	Rights Reserved
-*
-* File    	: 	ebase_const.h
-* Date	:	2010-09-25
-* By      	: 	holigun
-* Version 	: 	V1.00
-*********************************************************************************************************
-*/
+ * drivers/video/sun3i/disp/include/eBSP_basetype/ebase_const.h
+ *
+ * (C) Copyright 2007-2012
+ * Allwinner Technology Co., Ltd. <www.allwinnertech.com>
+ * holigun <holigun@allwinnertech.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
 
 
 #ifndef _EBASE_CONST_H_
