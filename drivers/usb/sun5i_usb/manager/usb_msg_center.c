c49797391d437ba93fc025d7f2a3f5d911bda5d7
Tom Cubie <tangliang@allwinnertech.com>
2012-06-05 11:16:33 +0800

    usb: clean the sun3i,sun4i,sun5i usb license problem

--- drivers/usb/sun5i_usb/manager/usb_msg_center.c
+++ drivers/usb/sun5i_usb/manager/usb_msg_center.c
@@ -1,23 +1,26 @@
 /*
-*************************************************************************************
-*                         			      Linux
-*					           USB Host Controller Driver
-*
-*				        (c) Copyright 2006-2010, All winners Co,Ld.
-*							       All Rights Reserved
-*
-* File Name 	: usb_msg_center.c
-*
-* Author 		: javen
-*
-* Description 	: USB 消息分发
-*
-* History 		:
-*      <author>    		<time>       	<version >    		<desc>
-*       javen     	  2011-4-14            1.0          create this file
-*
-*************************************************************************************
-*/
+ * drivers/usb/sun5i_usb/manager/usb_msg_center.c
+ *
+ * (C) Copyright 2007-2012
+ * Allwinner Technology Co., Ltd. <www.allwinnertech.com>
+ * javen <javen@allwinnertech.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
 
 #include <linux/module.h>
 #include <linux/kernel.h>
--- drivers/usb/sun5i_usb/misc/sw_usb_debug.c
+++ drivers/usb/sun5i_usb/misc/sw_usb_debug.c
@@ -1,23 +1,26 @@
 /*
-*************************************************************************************
-*                         			      Linux
-*					                 USB Host Driver
-*
-*				        (c) Copyright 2006-2010, All winners Co,Ld.
-*							       All Rights Reserved
-*
-* File Name 	: sw_usb_debug.c
-*
-* Author 		: javen
-*
-* Description 	:
-*
-* History 		:
-*      <author>    		<time>       	<version >    		<desc>
-*       javen     	  2010-12-20           1.0          create this file
-*
-*************************************************************************************
-*/
+ * drivers/usb/sun5i_usb/misc/sw_usb_debug.c
+ *
+ * (C) Copyright 2007-2012
+ * Allwinner Technology Co., Ltd. <www.allwinnertech.com>
+ * javen <javen@allwinnertech.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
 
 #include  "../include/sw_usb_config.h"
 
