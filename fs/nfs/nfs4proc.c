08106ac7c892cad769c5026cb9dd877a39aed603
Trond Myklebust <Trond.Myklebust@netapp.com>
2012-06-05 10:08:24 -0400

    NFSv4.1: Convert a trivial printk into a dprintk
    There is no need to bug the user about the server returning an error
    on destroy_session. The error will be handled by the state manager,
    without any need for further input from anyone else.
    So convert that printk into a debugging dprintk.
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -5766,8 +5766,7 @@ int nfs4_proc_destroy_session(struct nfs4_session *session,
 	status = rpc_call_sync(session->clp->cl_rpcclient, &msg, RPC_TASK_TIMEOUT);
 
 	if (status)
-		printk(KERN_WARNING
-			"NFS: Got error %d from the server on DESTROY_SESSION. "
+		dprintk("NFS: Got error %d from the server on DESTROY_SESSION. "
 			"Session has been destroyed regardless...\n", status);
 
 	dprintk("<-- nfs4_proc_destroy_session\n");
