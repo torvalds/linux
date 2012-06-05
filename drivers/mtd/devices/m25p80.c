9d6367f4f7835131b2b3987d134fd4c44636fa8d
ing. Federico Fuga <fuga@studiofuga.com>
2012-06-05 17:37:01 +0200

    mtd: add JEDEC ID for w25q32dw to chip table
    Adds JEDEC ID for the 1.8V version of WinBond w25q32.
    
    Signed-off-by: Federico Fuga <fuga@studiofuga.com>
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/devices/m25p80.c
+++ drivers/mtd/devices/m25p80.c
@@ -730,6 +730,7 @@ static const struct spi_device_id m25p_ids[] = {
 	{ "w25x16", INFO(0xef3015, 0, 64 * 1024,  32, SECT_4K) },
 	{ "w25x32", INFO(0xef3016, 0, 64 * 1024,  64, SECT_4K) },
 	{ "w25q32", INFO(0xef4016, 0, 64 * 1024,  64, SECT_4K) },
+	{ "w25q32dw", INFO(0xef6016, 0, 64 * 1024,  64, SECT_4K) },
 	{ "w25x64", INFO(0xef3017, 0, 64 * 1024, 128, SECT_4K) },
 	{ "w25q64", INFO(0xef4017, 0, 64 * 1024, 128, SECT_4K) },
 	{ "w25q80", INFO(0xef5014, 0, 64 * 1024,  16, SECT_4K) },
