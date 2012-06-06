f2f12b6fc032c7b1419fd6db84e2868b5f05a878
Shuah Khan <shuah.khan@hp.com>
2012-06-06 10:50:06 -0600

    iommu/amd: Fix missing iommu_shutdown initialization in passthrough mode
    The iommu_shutdown callback is not initialized when the AMD
    IOMMU driver runs in passthrough mode. Fix that by moving
    the callback initialization before the check for
    passthrough mode.
    
    Signed-off-by: Shuah Khan <shuah.khan@hp.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- drivers/iommu/amd_iommu_init.c
+++ drivers/iommu/amd_iommu_init.c
@@ -1641,6 +1641,8 @@ static int __init amd_iommu_init(void)
 
 	amd_iommu_init_api();
 
+	x86_platform.iommu_shutdown = disable_iommus;
+
 	if (iommu_pass_through)
 		goto out;
 
@@ -1648,8 +1650,6 @@ static int __init amd_iommu_init(void)
 		printk(KERN_INFO "AMD-Vi: IO/TLB flush on unmap enabled\n");
 	else
 		printk(KERN_INFO "AMD-Vi: Lazy IO/TLB flushing enabled\n");
-
-	x86_platform.iommu_shutdown = disable_iommus;
 
 out:
 	return ret;
