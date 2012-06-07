e076eb5c952c0f724980b44a77902a2ff93d098c
Wang Xingchao <xingchao.wang@intel.com>
2012-06-07 16:52:07 +0800

    ALSA: hda - check proper return value
    snd_hda_param_read() return value -1 means error, others are responses
    
    Signed-off-by: Wang Xingchao <xingchao.wang@intel.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_proc.c
+++ sound/pci/hda/hda_proc.c
@@ -451,7 +451,7 @@ static void print_power_state(struct snd_info_buffer *buffer,
 	int sup = snd_hda_param_read(codec, nid, AC_PAR_POWER_STATE);
 	int pwr = snd_hda_codec_read(codec, nid, 0,
 				     AC_VERB_GET_POWER_STATE, 0);
-	if (sup)
+	if (sup != -1)
 		snd_iprintf(buffer, "  Power states: %s\n",
 			    bits_names(sup, names, ARRAY_SIZE(names)));
 
