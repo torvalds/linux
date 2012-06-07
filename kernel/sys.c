300f786b2683f8bb1ec0afb6e1851183a479c86d
Cyrill Gorcunov <gorcunov@openvz.org>
2012-06-07 14:21:12 -0700

    c/r: prctl: add ability to get clear_tid_address
    Zero is written at clear_tid_address when the process exits.  This
    functionality is used by pthread_join().
    
    We already have sys_set_tid_address() to change this address for the
    current task but there is no way to obtain it from user space.
    
    Without the ability to find this address and dump it we can't restore
    pthread'ed apps which call pthread_join() once they have been restored.
    
    This patch introduces the PR_GET_TID_ADDRESS prctl option which allows
    the current process to obtain own clear_tid_address.
    
    This feature is available iif CONFIG_CHECKPOINT_RESTORE is set.
    
    [akpm@linux-foundation.org: fix prctl numbering]
    Signed-off-by: Andrew Vagin <avagin@openvz.org>
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Cc: Pedro Alves <palves@redhat.com>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Pavel Emelyanov <xemul@parallels.com>
    Cc: Tejun Heo <tj@kernel.org>
    Acked-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1988,9 +1988,19 @@ out:
 	up_read(&mm->mmap_sem);
 	return error;
 }
+
+static int prctl_get_tid_address(struct task_struct *me, int __user **tid_addr)
+{
+	return put_user(me->clear_child_tid, tid_addr);
+}
+
 #else /* CONFIG_CHECKPOINT_RESTORE */
 static int prctl_set_mm(int opt, unsigned long addr,
 			unsigned long arg4, unsigned long arg5)
+{
+	return -EINVAL;
+}
+static int prctl_get_tid_address(struct task_struct *me, int __user **tid_addr)
 {
 	return -EINVAL;
 }
@@ -2131,6 +2141,9 @@ SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 				else
 					return -EINVAL;
 				break;
+		case PR_GET_TID_ADDRESS:
+			error = prctl_get_tid_address(me, (int __user **)arg2);
+			break;
 			default:
 				return -EINVAL;
 			}
