a8fb688e1d0cfffe715ada2d1af33af82b647922
Russell King <rmk+kernel@arm.linux.org.uk>
2012-06-08 09:46:23 +0100

    ARM: spear: update for PL08x dma engine changes
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/plat-spear/include/plat/pl080.h
+++ arch/arm/plat-spear/include/plat/pl080.h
@@ -14,8 +14,8 @@
 #ifndef __PLAT_PL080_H
 #define __PLAT_PL080_H
 
-struct pl08x_dma_chan;
-int pl080_get_signal(struct pl08x_dma_chan *ch);
-void pl080_put_signal(struct pl08x_dma_chan *ch);
+struct pl08x_channel_data;
+int pl080_get_signal(const struct pl08x_channel_data *cd);
+void pl080_put_signal(const struct pl08x_channel_data *cd, int signal);
 
 #endif /* __PLAT_PL080_H */
