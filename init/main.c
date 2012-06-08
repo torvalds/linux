3410afedcda2f504e8fbe02a7f4c49912ce688c8
Rusty Russell <rusty@rustcorp.com.au>
2012-06-08 14:58:13 +0930

    module_param: stop double-calling parameters.
    commit ae82fdb1406ad41d68f07027fe31f2d35ba22a90 upstream.
    
    Commit 026cee0086fe1df4cf74691cf273062cc769617d "params:
    <level>_initcall-like kernel parameters" set old-style module
    parameters to level 0.  And we call those level 0 calls where we used
    to, early in start_kernel().
    
    We also loop through the initcall levels and call the levelled
    module_params before the corresponding initcall.  Unfortunately level
    0 is early_init(), so we call the standard module_param calls twice.
    
    (Turns out most things don't care, but at least ubi.mtd does).
    
    Change the level to -1 for standard module_param calls.
    
    Reported-by: Benoît Thébaudeau <benoit.thebaudeau@advansee.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- init/main.c
+++ init/main.c
@@ -508,7 +508,7 @@ asmlinkage void __init start_kernel(void)
 	parse_early_param();
 	parse_args("Booting kernel", static_command_line, __start___param,
 		   __stop___param - __start___param,
-		   0, 0, &unknown_bootoption);
+		   -1, -1, &unknown_bootoption);
 
 	jump_label_init();
 
