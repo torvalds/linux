98d9452448122486f81030c6c70f29471f65e1ce
Trond Myklebust <Trond.Myklebust@netapp.com>
2012-06-08 12:01:14 -0400

    NFSv4: Decode getdevicelist should use nfs4_verifier
    The verifier returned by the GETDEVICELIST operation is not a write
    verifier, but a nfs4_verifier.
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/nfs4xdr.c
+++ fs/nfs/nfs4xdr.c
@@ -5579,7 +5579,7 @@ static int decode_getdevicelist(struct xdr_stream *xdr,
 {
 	__be32 *p;
 	int status, i;
-	struct nfs_writeverf verftemp;
+	nfs4_verifier verftemp;
 
 	status = decode_op_hdr(xdr, OP_GETDEVICELIST);
 	if (status)
@@ -5593,7 +5593,7 @@ static int decode_getdevicelist(struct xdr_stream *xdr,
 	p += 2;
 
 	/* Read verifier */
-	p = xdr_decode_opaque_fixed(p, verftemp.verifier, NFS4_VERIFIER_SIZE);
+	p = xdr_decode_opaque_fixed(p, verftemp.data, NFS4_VERIFIER_SIZE);
 
 	res->num_devs = be32_to_cpup(p);
 
