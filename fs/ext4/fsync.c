9f713878f22e0b2d34d62df0ca55f65166375634
Al Viro <viro@zeniv.linux.org.uk>
2012-06-09 13:19:12 -0400

    ext4: get rid of open-coded d_find_any_alias()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/ext4/fsync.c
+++ fs/ext4/fsync.c
@@ -135,14 +135,7 @@ static int ext4_sync_parent(struct inode *inode)
 	inode = igrab(inode);
 	while (ext4_test_inode_state(inode, EXT4_STATE_NEWENTRY)) {
 		ext4_clear_inode_state(inode, EXT4_STATE_NEWENTRY);
-		dentry = NULL;
-		spin_lock(&inode->i_lock);
-		if (!list_empty(&inode->i_dentry)) {
-			dentry = list_first_entry(&inode->i_dentry,
-						  struct dentry, d_alias);
-			dget(dentry);
-		}
-		spin_unlock(&inode->i_lock);
+		dentry = d_find_any_alias(inode);
 		if (!dentry)
 			break;
 		next = igrab(dentry->d_parent->d_inode);
