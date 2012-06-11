b871a42b6091b720e82ddff237659534c525c25b
Thomas Gleixner <tglx@linutronix.de>
2012-06-11 15:07:08 +0200

    smpboot: Remove leftover declaration
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/smpboot.h
+++ kernel/smpboot.h
@@ -3,8 +3,6 @@
 
 struct task_struct;
 
-int smpboot_prepare(unsigned int cpu);
-
 #ifdef CONFIG_GENERIC_SMP_IDLE_THREAD
 struct task_struct *idle_thread_get(unsigned int cpu);
 void idle_thread_set_boot_cpu(void);
