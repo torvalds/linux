535588e61a007416f46cf08b4ccb6cc73b3f6fb0
Avinash Patil <patila@marvell.com>
2012-06-11 18:14:16 -0700

    ieee80211: definitions for Microsoft Vendor OUI and WPA OUI type
    Reference: http://standards.ieee.org/develop/regauth/oui/oui.txt
    
    Signed-off-by: Avinash Patil <patila@marvell.com>
    Signed-off-by: Bing Zhao <bzhao@marvell.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- include/linux/ieee80211.h
+++ include/linux/ieee80211.h
@@ -1589,6 +1589,8 @@ enum ieee80211_sa_query_action {
 
 #define WLAN_OUI_WFA			0x506f9a
 #define WLAN_OUI_TYPE_WFA_P2P		9
+#define WLAN_OUI_MICROSOFT		0x0050f2
+#define WLAN_OUI_TYPE_MICROSOFT_WPA	1
 
 /*
  * WMM/802.11e Tspec Element
