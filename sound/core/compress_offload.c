6b18f79399f25a0f8e2b915b2dcb8bf5c7aa470d
Vinod Koul <vinod.koul@linux.intel.com>
2012-06-12 16:16:17 +0530

    ALSA: compress_core: don't wake up on pause
    during pause the core should maintain the status-quo on the device and pointers
    and not wake up. If app needs it should call DROP explcitly.
    
    Signed-off-by: Namarta Kohli <namartax.kohli@intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@linux.intel.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/compress_offload.c
+++ sound/core/compress_offload.c
@@ -502,10 +502,8 @@ static int snd_compr_pause(struct snd_compr_stream *stream)
 	if (stream->runtime->state != SNDRV_PCM_STATE_RUNNING)
 		return -EPERM;
 	retval = stream->ops->trigger(stream, SNDRV_PCM_TRIGGER_PAUSE_PUSH);
-	if (!retval) {
+	if (!retval)
 		stream->runtime->state = SNDRV_PCM_STATE_PAUSED;
-		wake_up(&stream->runtime->sleep);
-	}
 	return retval;
 }
 
