351cfc34db8decb0c5cc1aac7cf1780a0e45c8b1
Daniel Vetter <daniel.vetter@ffwll.ch>
2012-06-12 13:20:47 +0200

    drm/i915: eDP aux needs vdd
    The new oui probe has been missing these.
    
    This issue has been introduce in
    
    commit 0d198328538276c4459ef5de081e68ae60e6c4c2
    Author: Adam Jackson <ajax@redhat.com>
    Date:   Mon May 14 16:05:47 2012 -0400
    
        drm/i915/dp: Probe branch/sink OUIs
    
    v2: Do the eDP vdd dance of simply not probing the OUI on eDP panels
    as suggested by Chris Wilson.
    
    v3: Fix up the error path fail - I suck.
    
    Cc: Adam Jackson <ajax@redhat.com>
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=50808
    Reported-by: Linus Torvalds <torvalds@linux-foundation.org>
    Bugreport: http://permalink.gmane.org/gmane.comp.video.dri.devel/69695
    Tested-by: Yang Guang <guang.a.yang@intel.com>
    Reviewed-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-Off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -1973,6 +1973,8 @@ intel_dp_probe_oui(struct intel_dp *intel_dp)
 	if (!(intel_dp->dpcd[DP_DOWN_STREAM_PORT_COUNT] & DP_OUI_SUPPORT))
 		return;
 
+	ironlake_edp_panel_vdd_on(intel_dp);
+
 	if (intel_dp_aux_native_read_retry(intel_dp, DP_SINK_OUI, buf, 3))
 		DRM_DEBUG_KMS("Sink OUI: %02hx%02hx%02hx\n",
 			      buf[0], buf[1], buf[2]);
@@ -1980,6 +1982,8 @@ intel_dp_probe_oui(struct intel_dp *intel_dp)
 	if (intel_dp_aux_native_read_retry(intel_dp, DP_BRANCH_OUI, buf, 3))
 		DRM_DEBUG_KMS("Branch OUI: %02hx%02hx%02hx\n",
 			      buf[0], buf[1], buf[2]);
+
+	ironlake_edp_panel_vdd_off(intel_dp, false);
 }
 
 static bool
