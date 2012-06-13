6fe9cbd1119b3cd2595dabaf25a400eed95b170c
Gregory CLEMENT <gregory.clement@free-electrons.com>
2012-06-13 18:58:09 +0200

    clocksource: time-armada-370-xp: Marvell Armada 370/XP SoC timer driver
    Timer 0 is used as free-running clocksource, while timer 1 is used as
    clock_event_device.
    
    Signed-off-by: Gregory CLEMENT <gregory.clement@free-electrons.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Lior Amsalem <alior@marvell.com>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Yehuda Yitschak <yehuday@marvell.com>
    Tested-by: Lior Amsalem <alior@marvell.com>
    Acked-by: Andrew Lunn <andrew@lunn.ch>
    CC: Thomas Gleixner <tglx@linutronix.de>
    CC: John Stultz <johnstul@us.ibm.com>

---
+++ include/linux/time-armada-370-xp.h
@@ -0,0 +1,18 @@
+/*
+ * Marvell Armada 370/XP SoC timer handling.
+ *
+ * Copyright (C) 2012 Marvell
+ *
+ * Lior Amsalem <alior@marvell.com>
+ * Gregory CLEMENT <gregory.clement@free-electrons.com>
+ * Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
+ *
+ */
+#ifndef __TIME_ARMADA_370_XPPRCMU_H
+#define __TIME_ARMADA_370_XPPRCMU_H
+
+#include <linux/init.h>
+
+void __init armada_370_xp_timer_init(void);
+
+#endif
