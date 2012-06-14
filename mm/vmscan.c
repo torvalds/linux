be7bd59db71dfb6dc011c9880fec5a659430003a
Wanpeng Li <liwp@linux.vnet.ibm.com>
2012-06-14 20:41:02 +0800

    mm: fix page reclaim comment error
    Since there are five lists in LRU cache, the array nr in get_scan_count
    should be:
    
    nr[0] = anon inactive pages to scan; nr[1] = anon active pages to scan
    nr[2] = file inactive pages to scan; nr[3] = file active pages to scan
    
    Signed-off-by: Wanpeng Li <liwp.linux@gmail.com>
    Reviewed-by: Rik van Riel <riel@redhat.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Acked-by: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Acked-by: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1894,7 +1894,8 @@ static int vmscan_swappiness(struct mem_cgroup_zone *mz,
  * by looking at the fraction of the pages scanned we did rotate back
  * onto the active list instead of evict.
  *
- * nr[0] = anon pages to scan; nr[1] = file pages to scan
+ * nr[0] = anon inactive pages to scan; nr[1] = anon active pages to scan
+ * nr[2] = file inactive pages to scan; nr[3] = file active pages to scan
  */
 static void get_scan_count(struct mem_cgroup_zone *mz, struct scan_control *sc,
 			   unsigned long *nr, int priority)
