bc7ff6fe2a3eddb83f06955017a96165589cb107
Rebecca Schultz Zavin <rebecca@android.com>
2012-06-14 14:52:17 -0700

    gpu: ion: Fix bug in heap type enum
    Change-Id: I8bf1345896774f02bd25c8d25a05d2c1eae262ef
    Signed-off-by: Rebecca Schultz Zavin <rebecca@android.com>

--- include/linux/ion.h
+++ include/linux/ion.h
@@ -27,7 +27,9 @@ struct ion_handle;
  * @ION_HEAP_TYPE_CARVEOUT:	 memory allocated from a prereserved
  * 				 carveout heap, allocations are physically
  * 				 contiguous
- * @ION_HEAP_END:		 helper for iterating over heaps
+ * @ION_NUM_HEAPS:		 helper for iterating over heaps, a bit mask
+ * 				 is used to identify the heaps, so only 32
+ * 				 total heap types are supported
  */
 enum ion_heap_type {
 	ION_HEAP_TYPE_SYSTEM,
@@ -35,7 +37,7 @@ enum ion_heap_type {
 	ION_HEAP_TYPE_CARVEOUT,
 	ION_HEAP_TYPE_CUSTOM, /* must be last so device specific heaps always
 				 are at the end of this enum */
-	ION_NUM_HEAPS,
+	ION_NUM_HEAPS = 32,
 };
 
 #define ION_HEAP_SYSTEM_MASK		(1 << ION_HEAP_TYPE_SYSTEM)
