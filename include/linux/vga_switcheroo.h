d3decf3a0c1d28c80c76be170373f4c7a7217f09
Ozan Çağlayan <ozancag@gmail.com>
2012-06-14 15:02:35 +0300

    vga_switcheroo: Add include guard
    Guard vga_switcheroo.h against multiple inclusion.
    
    Signed-off-by: Ozan Çağlayan <ozancag@gmail.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- include/linux/vga_switcheroo.h
+++ include/linux/vga_switcheroo.h
@@ -7,6 +7,9 @@
  * vga_switcheroo.h - Support for laptop with dual GPU using one set of outputs
  */
 
+#ifndef _LINUX_VGA_SWITCHEROO_H_
+#define _LINUX_VGA_SWITCHEROO_H_
+
 #include <linux/fb.h>
 
 struct pci_dev;
@@ -73,3 +76,4 @@ static inline int vga_switcheroo_get_client_state(struct pci_dev *dev) { return 
 
 
 #endif
+#endif /* _LINUX_VGA_SWITCHEROO_H_ */
