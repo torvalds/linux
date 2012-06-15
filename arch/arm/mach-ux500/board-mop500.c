c57920e6c23350b08c1b05606aafe356ebc6d8cc
Lee Jones <lee.jones@linaro.org>
2012-06-15 09:27:54 +0100

    ARM: ux500: Fix build errors/warnings when MACH_UX500_DT is not set
    When MACH_UX500_DT and all related Device Tree configurations are forced
    off the warning and error below prevent the kernel from compiling. This
    simple patch fixes both issues and allows for full build and boot of
    ST-Ericsson's low-cost development board, Snowball.
    
    Warnings fixed:
      arch/arm/mach-ux500/board-mop500.c:680:32: warning: ‘snowball_of_platform_devs’ defined but not used
    
    Errors fixed:
      arch/arm/mach-ux500/timer.c: In function ‘ux500_timer_init’:
      arch/arm/mach-ux500/timer.c:66:3: error: implicit declaration of function ‘of_find_matching_node’
      arch/arm/mach-ux500/timer.c:66:6: warning: assignment makes pointer from integer without a cast
    
    Cc: stable@vger.kernel.org
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- arch/arm/mach-ux500/board-mop500.c
+++ arch/arm/mach-ux500/board-mop500.c
@@ -625,11 +625,6 @@ static struct platform_device *snowball_platform_devs[] __initdata = {
 	&ab8500_device,
 };
 
-static struct platform_device *snowball_of_platform_devs[] __initdata = {
-	&snowball_led_dev,
-	&snowball_key_dev,
-};
-
 static void __init mop500_init_machine(void)
 {
 	struct device *parent = NULL;
@@ -768,6 +763,11 @@ MACHINE_START(SNOWBALL, "Calao Systems Snowball platform")
 MACHINE_END
 
 #ifdef CONFIG_MACH_UX500_DT
+
+static struct platform_device *snowball_of_platform_devs[] __initdata = {
+	&snowball_led_dev,
+	&snowball_key_dev,
+};
 
 struct of_dev_auxdata u8500_auxdata_lookup[] __initdata = {
 	/* Requires DMA and call-back bindings. */
