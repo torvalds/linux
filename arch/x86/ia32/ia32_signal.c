0b91f45b23cb73ce11acdc3cf4c6efd4441e3b3e
Suresh Siddha <suresh.b.siddha@intel.com>
2012-06-14 18:07:15 -0700

    x86, compat: Use test_thread_flag(TIF_IA32) in compat signal delivery
    Signal delivery compat path may not have the 'TS_COMPAT' flag (that
    flag indicates how we entered the kernel).  So use
    test_thread_flag(TIF_IA32) instead of is_ia32_task(): one of the
    functions of TIF_IA32 is just what kind of signal frame we want.
    
    Signed-off-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Link: http://lkml.kernel.org/r/1339722435.3475.57.camel@sbsiddha-desk.sc.intel.com
    Cc: stable@kernel.org	# v3.4
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/ia32/ia32_signal.c
+++ arch/x86/ia32/ia32_signal.c
@@ -38,7 +38,7 @@
 int copy_siginfo_to_user32(compat_siginfo_t __user *to, siginfo_t *from)
 {
 	int err = 0;
-	bool ia32 = is_ia32_task();
+	bool ia32 = test_thread_flag(TIF_IA32);
 
 	if (!access_ok(VERIFY_WRITE, to, sizeof(compat_siginfo_t)))
 		return -EFAULT;
