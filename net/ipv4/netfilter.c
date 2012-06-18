ce9f3f31efb88841e4df98794b13dbac8c4901da
Jan Beulich <JBeulich@suse.com>
2012-06-18 00:18:31 +0000

    netfilter: properly annotate ipv4_netfilter_{init,fini}()
    Despite being just a few bytes of code, they should still have proper
    annotations.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/ipv4/netfilter.c
+++ net/ipv4/netfilter.c
@@ -188,12 +188,12 @@ static const struct nf_afinfo nf_ip_afinfo = {
 	.route_key_size		= sizeof(struct ip_rt_info),
 };
 
-static int ipv4_netfilter_init(void)
+static int __init ipv4_netfilter_init(void)
 {
 	return nf_register_afinfo(&nf_ip_afinfo);
 }
 
-static void ipv4_netfilter_fini(void)
+static void __exit ipv4_netfilter_fini(void)
 {
 	nf_unregister_afinfo(&nf_ip_afinfo);
 }
