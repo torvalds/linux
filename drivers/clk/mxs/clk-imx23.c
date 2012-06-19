8c6facab8e99f7f90eaa1edbaf4baf217ac5ea5f
Fabio Estevam <fabio.estevam@freescale.com>
2012-06-19 17:27:32 -0300

    clk: mxs: Fix the GPMI clock name
    The correct name for the GPMI clock is 'gpmi-nand'.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Acked-by: Marek Vasut <marex@denx.de>
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- drivers/clk/mxs/clk-imx23.c
+++ drivers/clk/mxs/clk-imx23.c
@@ -106,7 +106,7 @@ static struct clk_lookup lcdif_lookups[] __initdata = {
 
 static struct clk_lookup gpmi_lookups[] __initdata = {
 	{ .dev_id = "imx23-gpmi-nand", },
-	{ .dev_id = "8000c000.gpmi", },
+	{ .dev_id = "8000c000.gpmi-nand", },
 };
 
 static const char *sel_pll[]  __initconst = { "pll", "ref_xtal", };
