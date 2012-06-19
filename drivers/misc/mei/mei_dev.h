21c66d1be7c586b56c2902ada4ba030a12b00063
Tomas Winkler <tomas.winkler@intel.com>
2012-06-19 09:13:37 +0300

    mei: group wd_interface_reg with watchdog variables within struct mei_device
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/mei/mei_dev.h
+++ drivers/misc/mei/mei_dev.h
@@ -237,13 +237,13 @@ struct mei_device {
 	bool mei_host_buffer_is_empty;
 
 	struct mei_cl wd_cl;
+	bool wd_interface_reg;
 	bool wd_pending;
 	bool wd_stopped;
 	bool wd_bypass;	/* if false, don't refresh watchdog ME client */
 	u16 wd_timeout;	/* seconds ((wd_data[1] << 8) + wd_data[0]) */
 	u16 wd_due_counter;
 	unsigned char wd_data[MEI_START_WD_DATA_SIZE];
-
 
 
 	struct file *iamthif_file_object;
@@ -259,8 +259,6 @@ struct mei_device {
 	bool iamthif_flow_control_pending;
 	bool iamthif_ioctl;
 	bool iamthif_canceled;
-
-	bool wd_interface_reg;
 };
 
 
