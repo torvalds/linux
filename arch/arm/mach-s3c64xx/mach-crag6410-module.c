1521109318466c2e064dcbd732aa64f99a62852f
Dimitris Papastamos <dp@opensource.wolfsonmicro.com>
2012-06-21 03:43:57 +0900

    ARM: S3C64XX: Add IRQ mapping for wm0010 on Cragganmore
    Signed-off-by: Dimitris Papastamos <dp@opensource.wolfsonmicro.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s3c64xx/mach-crag6410-module.c
+++ arch/arm/mach-s3c64xx/mach-crag6410-module.c
@@ -39,6 +39,7 @@ static struct spi_board_info wm1253_devs[] = {
 		.bus_num	= 0,
 		.chip_select	= 0,
 		.mode		= SPI_MODE_0,
+		.irq		= S3C_EINT(5),
 		.controller_data = &wm0010_spi_csinfo,
 	},
 };
