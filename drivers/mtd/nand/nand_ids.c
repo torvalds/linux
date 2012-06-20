9d9a8811622e36611e8eeb2401e4e9805d4727f3
Brian Norris <computersforpeace@gmail.com>
2012-06-20 16:14:02 -0700

    mtd: nand: change "AMD" manuf. ID to "AMD/Spansion"
    This manufacturer ID is used under the name Spansion.
    
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/nand/nand_ids.c
+++ drivers/mtd/nand/nand_ids.c
@@ -174,7 +174,7 @@ struct nand_manufacturers nand_manuf_ids[] = {
 	{NAND_MFR_STMICRO, "ST Micro"},
 	{NAND_MFR_HYNIX, "Hynix"},
 	{NAND_MFR_MICRON, "Micron"},
-	{NAND_MFR_AMD, "AMD"},
+	{NAND_MFR_AMD, "AMD/Spansion"},
 	{NAND_MFR_MACRONIX, "Macronix"},
 	{NAND_MFR_EON, "Eon"},
 	{0x0, "Unknown"}
