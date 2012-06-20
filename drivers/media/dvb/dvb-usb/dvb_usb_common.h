41a0abf7100928b412be4d35d62fd77c83064a9b
Antti Palosaari <crope@iki.fi>
2012-06-19 22:21:34 -0300

    [media] dvb_usb_v2: merge files dvb_usb_init.c and dvb_usb_dvb.c
    Merge files dvb_usb_init.c and dvb_usb_dvb.c to dvb_usb_core.c.
    It is still under 1000 lines of code after all the optimization
    so put it one file.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/dvb-usb/dvb_usb_common.h
+++ drivers/media/dvb/dvb-usb/dvb_usb_common.h
@@ -11,24 +11,12 @@
 
 #include "dvb_usb.h"
 
-extern int dvb_usbv2_disable_rc_polling;
-
 /* commonly used  methods */
-extern int dvb_usbv2_device_power_ctrl(struct dvb_usb_device *d, int onoff);
-
 extern int usb_urb_initv2(struct usb_data_stream *stream,
 		const struct usb_data_stream_properties *props);
 extern int usb_urb_exitv2(struct usb_data_stream *stream);
 extern int usb_urb_submitv2(struct usb_data_stream *stream,
 		struct usb_data_stream_properties *props);
 extern int usb_urb_killv2(struct usb_data_stream *stream);
-
-extern int dvb_usbv2_adapter_stream_init(struct dvb_usb_adapter *adap);
-extern int dvb_usbv2_adapter_stream_exit(struct dvb_usb_adapter *adap);
-
-extern int dvb_usbv2_adapter_dvb_init(struct dvb_usb_adapter *adap);
-extern int dvb_usbv2_adapter_dvb_exit(struct dvb_usb_adapter *adap);
-extern int dvb_usbv2_adapter_frontend_init(struct dvb_usb_adapter *adap);
-extern int dvb_usbv2_adapter_frontend_exit(struct dvb_usb_adapter *adap);
 
 #endif
