44b99462d9d776522e174d6c531ce5ccef309e26
Vaibhav Nagarnaik <vnagarnaik@google.com>
2012-06-22 11:50:05 -0700

    ring-buffer: Fix crash due to uninitialized new_pages list head
    The new_pages list head in the cpu_buffer is not initialized. When
    adding pages to the ring buffer, if the memory allocation fails in
    ring_buffer_resize, the clean up handler tries to free up the allocated
    pages from all the cpu buffers. The panic is caused by referencing the
    uninitialized new_pages list head.
    
    Initializing the new_pages list head in rb_allocate_cpu_buffer fixes
    this.
    
    Link: http://lkml.kernel.org/r/1340391005-10880-1-git-send-email-vnagarnaik@google.com
    
    Cc: Justin Teravest <teravest@google.com>
    Cc: David Sharp <dhsharp@google.com>
    Signed-off-by: Vaibhav Nagarnaik <vnagarnaik@google.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/trace/ring_buffer.c
+++ kernel/trace/ring_buffer.c
@@ -1075,6 +1075,7 @@ rb_allocate_cpu_buffer(struct ring_buffer *buffer, int nr_pages, int cpu)
 	rb_init_page(bpage->page);
 
 	INIT_LIST_HEAD(&cpu_buffer->reader_page->list);
+	INIT_LIST_HEAD(&cpu_buffer->new_pages);
 
 	ret = rb_allocate_pages(cpu_buffer, nr_pages);
 	if (ret < 0)
