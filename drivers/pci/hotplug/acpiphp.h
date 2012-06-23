2ac45f28b8eeef576c48579833b40b572f3f9790
Yinghai Lu <yinghai@kernel.org>
2012-06-23 00:42:03 -0700

    PCI: acpiphp: merge acpiphp_debug and debug
    Should not have two, just remove debug, and use module_param_named
    instead.
    
    Also change acpiphp_debug to bool.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/hotplug/acpiphp.h
+++ drivers/pci/hotplug/acpiphp.h
@@ -205,6 +205,6 @@ extern u8 acpiphp_get_latch_status (struct acpiphp_slot *slot);
 extern u8 acpiphp_get_adapter_status (struct acpiphp_slot *slot);
 
 /* variables */
-extern int acpiphp_debug;
+extern bool acpiphp_debug;
 
 #endif /* _ACPIPHP_H */
