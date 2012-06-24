d0a69d6321ca759bb8d47803d06ba8571ab42d07
Michael S. Tsirkin <mst@redhat.com>
2012-06-24 19:24:42 +0300

    x86, bitops: note on __test_and_clear_bit atomicity
    __test_and_clear_bit is actually atomic with respect
    to the local CPU. Add a note saying that KVM on x86
    relies on this behaviour so people don't accidentaly break it.
    Also warn not to rely on this in portable code.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/include/asm/bitops.h
+++ arch/x86/include/asm/bitops.h
@@ -264,6 +264,13 @@ static inline int test_and_clear_bit(int nr, volatile unsigned long *addr)
  * This operation is non-atomic and can be reordered.
  * If two examples of this operation race, one can appear to succeed
  * but actually fail.  You must protect multiple accesses with a lock.
+ *
+ * Note: the operation is performed atomically with respect to
+ * the local CPU, but not other CPUs. Portable code should not
+ * rely on this behaviour.
+ * KVM relies on this behaviour on x86 for modifying memory that is also
+ * accessed from a hypervisor on the same CPU if running in a VM: don't change
+ * this without also updating arch/x86/kernel/kvm.c
  */
 static inline int __test_and_clear_bit(int nr, volatile unsigned long *addr)
 {
