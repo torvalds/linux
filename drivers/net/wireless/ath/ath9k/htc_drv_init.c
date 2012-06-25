fb1c078edb50376c14e049b22b10768b8245428a
Sujith Manoharan <c_manoha@qca.qualcomm.com>
2012-06-25 13:54:30 +0530

    ath9k_htc: Change default listen interval to 1
    Signed-off-by: Sujith Manoharan <c_manoha@qca.qualcomm.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/htc_drv_init.c
+++ drivers/net/wireless/ath/ath9k/htc_drv_init.c
@@ -718,7 +718,7 @@ static void ath9k_set_hw_capab(struct ath9k_htc_priv *priv,
 
 	hw->queues = 4;
 	hw->channel_change_time = 5000;
-	hw->max_listen_interval = 10;
+	hw->max_listen_interval = 1;
 
 	hw->vif_data_size = sizeof(struct ath9k_htc_vif);
 	hw->sta_data_size = sizeof(struct ath9k_htc_sta);
