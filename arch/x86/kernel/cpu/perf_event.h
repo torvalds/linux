3e0091e2b6f8cd59e567f247e345a3a6ad1f6e7e
Peter Zijlstra <a.p.zijlstra@chello.nl>
2012-06-26 23:38:39 +0200

    perf/x86: Save a few bytes in 'struct x86_pmu'
    All these are basically boolean flags, use a bitfield to save a few
    bytes.
    
    Suggested-by: Borislav Petkov <bp@amd64.org>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/n/tip-vsevd5g8lhcn129n3s7trl7r@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/kernel/cpu/perf_event.h
+++ arch/x86/kernel/cpu/perf_event.h
@@ -374,8 +374,11 @@ struct x86_pmu {
 	/*
 	 * Intel DebugStore bits
 	 */
-	int		bts, pebs;
-	int		bts_active, pebs_active, pebs_broken;
+	int		bts		:1,
+			bts_active	:1,
+			pebs		:1,
+			pebs_active	:1,
+			pebs_broken	:1;
 	int		pebs_record_size;
 	void		(*drain_pebs)(struct pt_regs *regs);
 	struct event_constraint *pebs_constraints;
