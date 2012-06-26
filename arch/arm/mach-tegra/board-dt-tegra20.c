bd976e0307fbb3c715df944ded6b70a9fa8d6bb1
Laxman Dewangan <ldewangan@nvidia.com>
2012-06-26 12:48:31 +0530

    ARM: tegra: add device tree AUXDATA for APBDMA
    Add DT AUXDATA for Tegra's APBDMA controller driver.
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/board-dt-tegra20.c
+++ arch/arm/mach-tegra/board-dt-tegra20.c
@@ -64,6 +64,7 @@ struct of_dev_auxdata tegra20_auxdata_lookup[] __initdata = {
 		       &tegra_ehci2_pdata),
 	OF_DEV_AUXDATA("nvidia,tegra20-ehci", TEGRA_USB3_BASE, "tegra-ehci.2",
 		       &tegra_ehci3_pdata),
+	OF_DEV_AUXDATA("nvidia,tegra20-apbdma", 0x6000a000, "tegra-apbdma", NULL),
 	{}
 };
 
