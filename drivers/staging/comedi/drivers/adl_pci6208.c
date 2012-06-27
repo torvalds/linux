a28c98524c2634a5d84151de253ee7e99e38564d
H Hartley Sweeten <hartleys@visionengravers.com>
2012-06-27 14:57:22 -0700

    staging: comedi: adl_pci6208: cleanup pci6208_pci_setup()
    The "local configuration register" stuff is not required. The
    io base/range are only queried for use in a kernel message
    that it just added noise. Similar with the io range for the
    actual io base used by the driver.
    
    Remove both printk's and all the unnecessary code.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/adl_pci6208.c
+++ drivers/staging/comedi/drivers/adl_pci6208.c
@@ -229,7 +229,7 @@ static int
 pci6208_pci_setup(struct pci_dev *pci_dev, unsigned long *io_base_ptr,
 		  int dev_minor)
 {
-	unsigned long io_base, io_range, lcr_io_base, lcr_io_range;
+	unsigned long io_base;
 
 	/*  Enable PCI device and request regions */
 	if (comedi_pci_enable(pci_dev, "adl_pci6208") < 0) {
@@ -238,28 +238,11 @@ pci6208_pci_setup(struct pci_dev *pci_dev, unsigned long *io_base_ptr,
 			dev_minor);
 		return -EIO;
 	}
-	/* Read local configuration register
-	 * base address [PCI_BASE_ADDRESS #1].
-	 */
-	lcr_io_base = pci_resource_start(pci_dev, 1);
-	lcr_io_range = pci_resource_len(pci_dev, 1);
-
-	printk(KERN_INFO "comedi%d: local config registers at address"
-			" 0x%4lx [0x%4lx]\n",
-			dev_minor, lcr_io_base, lcr_io_range);
 
 	/*  Read PCI6208 register base address [PCI_BASE_ADDRESS #2]. */
 	io_base = pci_resource_start(pci_dev, 2);
-	io_range = pci_resource_end(pci_dev, 2) - io_base + 1;
-
-	printk("comedi%d: 6208 registers at address 0x%4lx [0x%4lx]\n",
-	       dev_minor, io_base, io_range);
 
 	*io_base_ptr = io_base;
-	/* devpriv->io_range = io_range; */
-	/* devpriv->is_valid=0; */
-	/* devpriv->lcr_io_base=lcr_io_base; */
-	/* devpriv->lcr_io_range=lcr_io_range; */
 
 	return 0;
 }
