5ff8407798f1d8c44f3165bc535676a2a897666b
Sam Leffler <sleffler@chromium.org>
2012-06-27 12:55:56 -0700

    cpufreq: interactive: keep freezer happy when not current governor
    Fix a problem where the hung task mechanism was deeming the interactive
    clock boost thread as hung.  This was because the thread is created at
    module init but never run/woken up until needed.  If the governor is not
    being used this can be forever.  To workaround this explicitly wake up
    the thread once all the necessary data structures are initialized.  The
    latter required some minor code shuffle.
    
    Signed-off-by: Sam Leffler <sleffler@chromium.org>
    Change-Id: Ie2c058dd75dcb6460ea10e7ac997e46baf66b1fe

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -952,6 +952,10 @@ static int __init cpufreq_interactive_init(void)
 		pcpu->cpu_timer.data = i;
 	}
 
+	spin_lock_init(&up_cpumask_lock);
+	spin_lock_init(&down_cpumask_lock);
+	mutex_init(&set_speed_lock);
+
 	up_task = kthread_create(cpufreq_interactive_up_task, NULL,
 				 "kinteractiveup");
 	if (IS_ERR(up_task))
@@ -967,14 +971,12 @@ static int __init cpufreq_interactive_init(void)
 	if (!down_wq)
 		goto err_freeuptask;
 
-	INIT_WORK(&freq_scale_down_work,
-		  cpufreq_interactive_freq_down);
-
-	spin_lock_init(&up_cpumask_lock);
-	spin_lock_init(&down_cpumask_lock);
-	mutex_init(&set_speed_lock);
-
+	INIT_WORK(&freq_scale_down_work, cpufreq_interactive_freq_down);
 	INIT_WORK(&inputopen.inputopen_work, cpufreq_interactive_input_open);
+
+	/* NB: wake up so the thread does not look hung to the freezer */
+	wake_up_process(up_task);
+
 	return cpufreq_register_governor(&cpufreq_gov_interactive);
 
 err_freeuptask:
