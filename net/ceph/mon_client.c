a5b0662bb814837139bb73249463e199528101b5
Sage Weil <sage@inktank.com>
2012-06-27 12:24:34 -0700

    libceph: initialize mon_client con only once
    (cherry picked from commit 735a72ef952d42a256f79ae3e6dc1c17a45c041b)
    
    Do not re-initialize the con on every connection attempt.  When we
    ceph_con_close, there may still be work queued on the socket (e.g., to
    close it), and re-initializing will clobber the work_struct state.
    
    Signed-off-by: Sage Weil <sage@inktank.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/ceph/mon_client.c
+++ net/ceph/mon_client.c
@@ -119,7 +119,6 @@ static void __close_session(struct ceph_mon_client *monc)
 	dout("__close_session closing mon%d\n", monc->cur_mon);
 	ceph_msg_revoke(monc->m_auth);
 	ceph_con_close(&monc->con);
-	monc->con.private = NULL;
 	monc->cur_mon = -1;
 	monc->pending_auth = 0;
 	ceph_auth_reset(monc->auth);
@@ -141,9 +140,6 @@ static int __open_session(struct ceph_mon_client *monc)
 		monc->sub_sent = 0;
 		monc->sub_renew_after = jiffies;  /* i.e., expired */
 		monc->want_next_osdmap = !!monc->want_next_osdmap;
-
-		ceph_con_init(&monc->con, monc, &mon_con_ops,
-			&monc->client->msgr);
 
 		dout("open_session mon%d opening\n", monc->cur_mon);
 		ceph_con_open(&monc->con,
@@ -797,6 +793,9 @@ int ceph_monc_init(struct ceph_mon_client *monc, struct ceph_client *cl)
 	monc->pending_auth = 0;
 	if (!monc->m_auth)
 		goto out_auth_reply;
+
+	ceph_con_init(&monc->con, monc, &mon_con_ops,
+		      &monc->client->msgr);
 
 	monc->cur_mon = -1;
 	monc->hunting = true;
