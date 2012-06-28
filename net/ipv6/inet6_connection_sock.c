9247869ee661b046510c19a36cf0d91d9c2639d3
Neal Cardwell <ncardwell@google.com>
2012-06-28 12:34:18 +0000

    tcp: fix inet6_csk_route_req() for link-local addresses
    Fix inet6_csk_route_req() to use as the flowi6_oif the treq->iif,
    which is correctly fixed up in tcp_v6_conn_request() to handle the
    case of link-local addresses. This brings it in line with the
    tcp_v6_send_synack() code, which is already correctly using the
    treq->iif in this way.
    
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/inet6_connection_sock.c
+++ net/ipv6/inet6_connection_sock.c
@@ -68,7 +68,7 @@ struct dst_entry *inet6_csk_route_req(struct sock *sk,
 	fl6.daddr = treq->rmt_addr;
 	final_p = fl6_update_dst(&fl6, np->opt, &final);
 	fl6.saddr = treq->loc_addr;
-	fl6.flowi6_oif = sk->sk_bound_dev_if;
+	fl6.flowi6_oif = treq->iif;
 	fl6.flowi6_mark = sk->sk_mark;
 	fl6.fl6_dport = inet_rsk(req)->rmt_port;
 	fl6.fl6_sport = inet_rsk(req)->loc_port;
