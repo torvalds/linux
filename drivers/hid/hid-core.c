b94e3c94aae04a911d61f620f4ff5b575fc196ad
Matthieu CASTET <matthieu.castet@parrot.com>
2012-06-28 16:53:11 +0200

    HID: hid-core: optimize in case of hidraw
    When using hidraw, hid buffer can be big and take lot's of
    time to process (interrupt) kernel context.
    Don't try to parse report if we are only interrested in hidraw.
    
    Also don't prepare data for debug stuff if no debugfs file
    are opened.
    
    Signed-off-by: Matthieu CASTET <matthieu.castet@parrot.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -1194,8 +1194,10 @@ int hid_report_raw_event(struct hid_device *hid, int type, u8 *data, int size,
 			goto out;
 	}
 
-	for (a = 0; a < report->maxfield; a++)
-		hid_input_field(hid, report->field[a], cdata, interrupt);
+	if (hid->claimed != HID_CLAIMED_HIDRAW) {
+		for (a = 0; a < report->maxfield; a++)
+			hid_input_field(hid, report->field[a], cdata, interrupt);
+	}
 
 	if (hid->claimed & HID_CLAIMED_INPUT)
 		hidinput_report_event(hid, report);
@@ -1242,6 +1244,10 @@ int hid_input_report(struct hid_device *hid, int type, u8 *data, int size, int i
 		ret = -1;
 		goto unlock;
 	}
+
+	/* Avoid unnecessary overhead if debugfs is disabled */
+	if (list_empty(&hid->debug_list))
+		goto nomem;
 
 	buf = kmalloc(sizeof(char) * HID_DEBUG_BUFSIZE, GFP_ATOMIC);
 
--- drivers/hid/hid-picolcd.c
+++ drivers/hid/hid-picolcd.c
@@ -1846,7 +1846,7 @@ static void picolcd_debug_out_report(struct picolcd_data *data,
 #define BUFF_SZ 256
 
 	/* Avoid unnecessary overhead if debugfs is disabled */
-	if (!hdev->debug_events)
+	if (list_empty(&hdev->debug_list))
 		return;
 
 	buff = kmalloc(BUFF_SZ, GFP_ATOMIC);
