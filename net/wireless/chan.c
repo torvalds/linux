4f03c1ed8901a01ad4abcef95c02c007a2d481c2
Michal Kazior <michal.kazior@tieto.com>
2012-06-29 12:47:03 +0200

    cfg80211: refuse to .set_monitor_channel when non-monitors are present
    Having .set_monitor_channel work with non-monitor
    interfaces running would make interface
    combinations accounting ambiguous.
    
    Signed-off-by: Michal Kazior <michal.kazior@tieto.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/wireless/chan.c
+++ net/wireless/chan.c
@@ -85,6 +85,8 @@ int cfg80211_set_monitor_channel(struct cfg80211_registered_device *rdev,
 
 	if (!rdev->ops->set_monitor_channel)
 		return -EOPNOTSUPP;
+	if (!cfg80211_has_monitors_only(rdev))
+		return -EBUSY;
 
 	chan = rdev_freq_to_chan(rdev, freq, chantype);
 	if (!chan)
