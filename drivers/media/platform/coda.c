df1e74cc04dbc0d2fada237333b91e167b09dd30
Philipp Zabel <p.zabel@pengutronix.de>
2012-07-02 06:07:10 -0300

    [media] media: coda: add i.MX53 / CODA7541 platform support
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
    Tested-by: Javier Martin <javier.martin@vista-silicon.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/coda.c
+++ drivers/media/platform/coda.c
@@ -84,6 +84,7 @@ enum coda_inst_type {
 
 enum coda_product {
 	CODA_DX6 = 0xf001,
+	CODA_7541 = 0xf012,
 };
 
 struct coda_fmt {
@@ -244,6 +245,24 @@ static struct coda_q_data *get_q_data(struct coda_ctx *ctx,
  *  i.MX6  -> coda960
  */
 static struct coda_fmt codadx6_formats[] = {
+	{
+		.name = "YUV 4:2:0 Planar",
+		.fourcc = V4L2_PIX_FMT_YUV420,
+		.type = CODA_FMT_RAW,
+	},
+	{
+		.name = "H264 Encoded Stream",
+		.fourcc = V4L2_PIX_FMT_H264,
+		.type = CODA_FMT_ENC,
+	},
+	{
+		.name = "MPEG4 Encoded Stream",
+		.fourcc = V4L2_PIX_FMT_MPEG4,
+		.type = CODA_FMT_ENC,
+	},
+};
+
+static struct coda_fmt coda7_formats[] = {
 	{
 		.name = "YUV 4:2:0 Planar",
 		.fourcc = V4L2_PIX_FMT_YUV420,
@@ -1488,6 +1507,7 @@ static irqreturn_t coda_irq_handler(int irq, void *data)
 
 static u32 coda_supported_firmwares[] = {
 	CODA_FIRMWARE_VERNUM(CODA_DX6, 2, 2, 5),
+	CODA_FIRMWARE_VERNUM(CODA_7541, 13, 4, 29),
 };
 
 static bool coda_firmware_supported(u32 vernum)
@@ -1507,6 +1527,8 @@ static char *coda_product_name(int product)
 	switch (product) {
 	case CODA_DX6:
 		return "CodaDx6";
+	case CODA_7541:
+		return "CODA7541";
 	default:
 		snprintf(buf, sizeof(buf), "(0x%04x)", product);
 		return buf;
@@ -1699,6 +1721,7 @@ static int coda_firmware_request(struct coda_dev *dev)
 
 enum coda_platform {
 	CODA_IMX27,
+	CODA_IMX53,
 };
 
 static const struct coda_devtype coda_devdata[] = {
@@ -1708,10 +1731,17 @@ static const struct coda_devtype coda_devdata[] = {
 		.formats     = codadx6_formats,
 		.num_formats = ARRAY_SIZE(codadx6_formats),
 	},
+	[CODA_IMX53] = {
+		.firmware    = "v4l-coda7541-imx53.bin",
+		.product     = CODA_7541,
+		.formats     = coda7_formats,
+		.num_formats = ARRAY_SIZE(coda7_formats),
+	},
 };
 
 static struct platform_device_id coda_platform_ids[] = {
 	{ .name = "coda-imx27", .driver_data = CODA_IMX27 },
+	{ .name = "coda-imx53", .driver_data = CODA_7541 },
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(platform, coda_platform_ids);
@@ -1719,6 +1749,7 @@ MODULE_DEVICE_TABLE(platform, coda_platform_ids);
 #ifdef CONFIG_OF
 static const struct of_device_id coda_dt_ids[] = {
 	{ .compatible = "fsl,imx27-vpu", .data = &coda_platform_ids[CODA_IMX27] },
+	{ .compatible = "fsl,imx53-vpu", .data = &coda_devdata[CODA_IMX53] },
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, coda_dt_ids);
