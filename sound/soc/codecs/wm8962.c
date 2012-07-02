081413f206876e9d3755e1673828c7742fd00184
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-07-02 18:19:58 +0100

    ASoC: wm8962: Log AIF configuration requested by hw_params()
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm8962.c
+++ sound/soc/codecs/wm8962.c
@@ -2580,6 +2580,9 @@ static int wm8962_hw_params(struct snd_pcm_substream *substream,
 			    WM8962_SAMPLE_RATE_INT_MODE |
 			    WM8962_SAMPLE_RATE_MASK, adctl3);
 
+	dev_dbg(codec->dev, "hw_params set BCLK %dHz LRCLK %dHz\n",
+		wm8962->bclk, wm8962->lrclk);
+
 	if (codec->dapm.bias_level == SND_SOC_BIAS_ON)
 		wm8962_configure_bclk(codec);
 
