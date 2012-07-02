0bdbf4ccef4f87016e2fa0c0b34f7a025f364c3d
Hiroshi DOYU <hdoyu@nvidia.com>
2012-07-02 14:26:37 +0300

    iommu/tegra: smmu: Remove unnecessary sanity check at alloc_pdir()
    alloc_pdir() is called with smmu->as[?].pdir_page == NULL. No need to
    check pdir_page again inside alloc_pdir().
    
    Signed-off-by: Hiroshi DOYU <hdoyu@nvidia.com>
    Acked-by: Stephen Warren <swarren@wwwdotorg.org>
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- drivers/iommu/tegra-smmu.c
+++ drivers/iommu/tegra-smmu.c
@@ -562,9 +562,6 @@ static int alloc_pdir(struct smmu_as *as)
 	u32 val;
 	struct smmu_device *smmu = as->smmu;
 
-	if (as->pdir_page)
-		return 0;
-
 	as->pte_count = devm_kzalloc(smmu->dev,
 		     sizeof(as->pte_count[0]) * SMMU_PDIR_COUNT, GFP_KERNEL);
 	if (!as->pte_count) {
