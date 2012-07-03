333872c371cdbe37c3fdd0862716fdfe77cf9d6a
黄涛 <huangtao@rock-chips.com>
2012-07-03 10:20:19 +0800

    rk: plat: add memory.h

---
+++ arch/arm/plat-rk/include/plat/memory.h
@@ -0,0 +1,16 @@
+#ifndef __PLAT_MEMORY_H
+#define __PLAT_MEMORY_H
+
+#include <linux/version.h>
+
+/*
+ * Physical DRAM offset.
+ */
+#define PLAT_PHYS_OFFSET	UL(0x60000000)
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 34))
+#define dmac_clean_range(start, end)	dmac_map_area(start, end - start, DMA_TO_DEVICE)
+#define dmac_inv_range(start, end)	dmac_unmap_area(start, end - start, DMA_FROM_DEVICE)
+#endif
+
+#endif
