a44982e45922096726ed9376ef9bc40d8e2bb954
chenxing <chenxing@rock-chips.com>
2012-07-04 17:23:30 +0800

    rk30:sdk: add GPU frequency limit between 133MHz to 400MHz

--- arch/arm/mach-rk30/cpufreq.c
+++ arch/arm/mach-rk30/cpufreq.c
@@ -256,6 +256,9 @@ static int rk30_cpu_init(struct cpufreq_policy *policy)
 		}
 		clk_enable_dvfs(cpu_clk);
 
+		/* Limit gpu frequency between 133M to 400M */
+		dvfs_clk_enable_limit(gpu_clk, 133000000, 400000000);
+
 		freq_wq = create_singlethread_workqueue("rk30_cpufreqd");
 #ifdef CONFIG_RK30_CPU_FREQ_LIMIT_BY_TEMP
 		if (rk30_cpufreq_is_ondemand_policy(policy)) {
