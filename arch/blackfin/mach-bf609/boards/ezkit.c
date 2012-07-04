00afdbbfaf07baf62a31920862128e55842f07dc
Vivi Li <vivi.li@analog.com>
2012-07-04 14:20:33 +0800

    bf60x: vs6624 pin update
    Change ce pin to PE4 according to new adaptor board and re-define ppi for vs6624
    to avoid gpio conflict.
    
    Signed-off-by: Vivi Li <vivi.li@analog.com>
    Signed-off-by: Bob Liu <lliubbo@gmail.com>

--- arch/blackfin/mach-bf609/boards/ezkit.c
+++ arch/blackfin/mach-bf609/boards/ezkit.c
@@ -830,8 +830,10 @@ static const unsigned short ppi_req[] = {
 	P_PPI0_D4, P_PPI0_D5, P_PPI0_D6, P_PPI0_D7,
 	P_PPI0_D8, P_PPI0_D9, P_PPI0_D10, P_PPI0_D11,
 	P_PPI0_D12, P_PPI0_D13, P_PPI0_D14, P_PPI0_D15,
+#if !defined(CONFIG_VIDEO_VS6624) && !defined(CONFIG_VIDEO_VS6624_MODULE)
 	P_PPI0_D16, P_PPI0_D17, P_PPI0_D18, P_PPI0_D19,
 	P_PPI0_D20, P_PPI0_D21, P_PPI0_D22, P_PPI0_D23,
+#endif
 	P_PPI0_CLK, P_PPI0_FS1, P_PPI0_FS2,
 	0,
 };
@@ -862,7 +864,7 @@ static struct bcap_route vs6624_routes[] = {
 	},
 };
 
-static const unsigned vs6624_ce_pin = GPIO_PD1;
+static const unsigned vs6624_ce_pin = GPIO_PE4;
 
 static struct bfin_capture_config bfin_capture_data = {
 	.card_name = "BF609",
