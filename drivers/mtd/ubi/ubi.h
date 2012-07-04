87e773c95eb0b363f2efcc7aff8a347dc18925d0
Shmulik Ladkani <shmulik.ladkani@gmail.com>
2012-07-04 11:06:04 +0300

    UBI: harmonize the update of ubi->beb_rsvd_pebs
    Currently, there are several locations where an attempt to reserve more
    PEBs for bad PEB handling is made, with the same code being duplicated.
    
    Harmonize it by introducing 'ubi_update_reserved()'.
    
    Also, improve the debug message issued, making it more descriptive.
    
    Artem: amended the patch a little.
    
    Signed-off-by: Shmulik Ladkani <shmulik.ladkani@gmail.com>
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@linux.intel.com>

--- drivers/mtd/ubi/ubi.h
+++ drivers/mtd/ubi/ubi.h
@@ -647,6 +647,7 @@ int ubi_more_leb_change_data(struct ubi_device *ubi, struct ubi_volume *vol,
 int ubi_calc_data_len(const struct ubi_device *ubi, const void *buf,
 		      int length);
 int ubi_check_volume(struct ubi_device *ubi, int vol_id);
+void ubi_update_reserved(struct ubi_device *ubi);
 void ubi_calculate_reserved(struct ubi_device *ubi);
 int ubi_check_pattern(const void *buf, uint8_t patt, int size);
 
