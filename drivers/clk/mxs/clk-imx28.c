64e2bc41304726a5ca8a4d37754db69b88841ebf
Lauri Hintsala <lauri.hintsala@bluegiga.com>
2012-07-04 13:49:54 +0300

    clk: mxs: imx28: decrease the frequency of ref_io1 for SSP2 and SSP3
    SSP0 and SSP1 use ref_io0 which has decreased frequency. Expand
    the frequency fix for ref_io1 to get SSP2 and SSP3 to work.
    
    Signed-off-by: Lauri Hintsala <lauri.hintsala@bluegiga.com>
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- drivers/clk/mxs/clk-imx28.c
+++ drivers/clk/mxs/clk-imx28.c
@@ -112,11 +112,11 @@ static void __init clk_misc_init(void)
 
 	/*
 	 * 480 MHz seems too high to be ssp clock source directly,
-	 * so set frac0 to get a 288 MHz ref_io0.
+	 * so set frac0 to get a 288 MHz ref_io0 and ref_io1.
 	 */
 	val = readl_relaxed(FRAC0);
-	val &= ~(0x3f << BP_FRAC0_IO0FRAC);
-	val |= 30 << BP_FRAC0_IO0FRAC;
+	val &= ~((0x3f << BP_FRAC0_IO0FRAC) | (0x3f << BP_FRAC0_IO1FRAC));
+	val |= (30 << BP_FRAC0_IO0FRAC) | (30 << BP_FRAC0_IO1FRAC);
 	writel_relaxed(val, FRAC0);
 }
 
