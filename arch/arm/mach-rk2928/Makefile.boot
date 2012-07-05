64e1e8cff85ddcfe97b9d5525b879ffb5f51f0f9
黄涛 <huangtao@rock-chips.com>
2012-07-05 18:22:44 +0800

    rk2928: add initial support

---
+++ arch/arm/mach-rk2928/Makefile.boot
@@ -0,0 +1,3 @@
+   zreladdr-y		:= 0x60408000
+params_phys-y		:= 0x60088000
+initrd_phys-y		:= 0x60800000
---
+++ arch/arm/mach-rk2928/include/mach/sram.h
@@ -0,0 +1,13 @@
+#ifndef __MACH_SRAM_H
+#define __MACH_SRAM_H
+
+#include <plat/sram.h>
+
+#define SRAM_LOOPS_PER_USEC	24
+#define SRAM_LOOP(loops)	do { unsigned int i = (loops); if (i < 7) i = 7; barrier(); while (--i) barrier(); } while (0)
+/* delay on slow mode */
+#define sram_udelay(usecs)	SRAM_LOOP((usecs)*SRAM_LOOPS_PER_USEC)
+/* delay on deep slow mode */
+#define sram_32k_udelay(usecs)	SRAM_LOOP(((usecs)*SRAM_LOOPS_PER_USEC)/(24000000/32768))
+
+#endif
