d9c2ede63c74048dfddbb129c59ac01176b0ab71
Eldad Zack <eldad@fogrefinery.com>
2012-07-06 21:31:56 +0200

    sunrpc/cache.h: fix coding style
    Neaten code style in get_int().
    Also use sizeof() instead of hard coded number as suggested by
    Joe Perches <joe@perches.com>.
    
    Cc: Joe Perches <joe@perches.com>
    Signed-off-by: Eldad Zack <eldad@fogrefinery.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- include/linux/sunrpc/cache.h
+++ include/linux/sunrpc/cache.h
@@ -219,11 +219,17 @@ static inline int get_int(char **bpp, int *anint)
 	char buf[50];
 	char *ep;
 	int rv;
-	int len = qword_get(bpp, buf, 50);
-	if (len < 0) return -EINVAL;
-	if (len ==0) return -ENOENT;
+	int len = qword_get(bpp, buf, sizeof(buf));
+
+	if (len < 0)
+		return -EINVAL;
+	if (len == 0)
+		return -ENOENT;
+
 	rv = simple_strtol(buf, &ep, 0);
-	if (*ep) return -EINVAL;
+	if (*ep)
+		return -EINVAL;
+
 	*anint = rv;
 	return 0;
 }
