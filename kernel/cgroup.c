7db5b3ca0ecdb2e8fad52a4770e4e320e61c77a6
Tejun Heo <tj@kernel.org>
2012-07-07 15:55:47 -0700

    Revert "cgroup: superblock can't be released with active dentries"
    This reverts commit fa980ca87d15bb8a1317853f257a505990f3ffde.  The
    commit was an attempt to fix a race condition where a cgroup hierarchy
    may be unmounted with positive dentry reference on root cgroup.  While
    the commit made the race condition slightly more difficult to trigger,
    the race was still there and could be reliably triggered using a
    different test case.
    
    Revert the incorrect fix.  The next commit will describe the race and
    fix it correctly.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    LKML-Reference: <4FEEA5CB.8070809@huawei.com>
    Reported-by: shyju pv <shyju.pv@huawei.com>
    Cc: Sasha Levin <levinsasha928@gmail.com>
    Acked-by: Li Zefan <lizefan@huawei.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -901,13 +901,10 @@ static void cgroup_diput(struct dentry *dentry, struct inode *inode)
 		mutex_unlock(&cgroup_mutex);
 
 		/*
-		 * We want to drop the active superblock reference from the
-		 * cgroup creation after all the dentry refs are gone -
-		 * kill_sb gets mighty unhappy otherwise.  Mark
-		 * dentry->d_fsdata with cgroup_diput() to tell
-		 * cgroup_d_release() to call deactivate_super().
+		 * Drop the active superblock reference that we took when we
+		 * created the cgroup
 		 */
-		dentry->d_fsdata = cgroup_diput;
+		deactivate_super(cgrp->root->sb);
 
 		/*
 		 * if we're getting rid of the cgroup, refcount should ensure
@@ -931,13 +928,6 @@ static void cgroup_diput(struct dentry *dentry, struct inode *inode)
 static int cgroup_delete(const struct dentry *d)
 {
 	return 1;
-}
-
-static void cgroup_d_release(struct dentry *dentry)
-{
-	/* did cgroup_diput() tell me to deactivate super? */
-	if (dentry->d_fsdata == cgroup_diput)
-		deactivate_super(dentry->d_sb);
 }
 
 static void remove_dir(struct dentry *d)
@@ -1547,7 +1537,6 @@ static int cgroup_get_rootdir(struct super_block *sb)
 	static const struct dentry_operations cgroup_dops = {
 		.d_iput = cgroup_diput,
 		.d_delete = cgroup_delete,
-		.d_release = cgroup_d_release,
 	};
 
 	struct inode *inode =
