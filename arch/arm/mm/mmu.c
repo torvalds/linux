20d6956d8cd2452cec0889ff040f18afc03c2e6b
Vitaly Andrianov <vitalya@ti.com>
2012-07-10 14:41:17 -0400

    ARM: LPAE: use phys_addr_t in alloc_init_pud()
    This patch fixes the alloc_init_pud() function to use phys_addr_t instead of
    unsigned long when passing in the phys argument.
    
    This is an extension to commit 97092e0c56830457af0639f6bd904537a150ea4a (ARM:
    pgtable: use phys_addr_t for physical addresses), which applied similar changes
    elsewhere in the ARM memory management code.
    
    Signed-off-by: Vitaly Andrianov <vitalya@ti.com>
    Signed-off-by: Cyril Chemparathy <cyril@ti.com>
    Acked-by: Nicolas Pitre <nico@linaro.org>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Tested-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Tested-by: Subash Patel <subash.rp@samsung.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -673,7 +673,8 @@ static void __init alloc_init_pmd(pud_t *pud, unsigned long addr,
 }
 
 static void __init alloc_init_pud(pgd_t *pgd, unsigned long addr,
-	unsigned long end, unsigned long phys, const struct mem_type *type)
+				  unsigned long end, phys_addr_t phys,
+				  const struct mem_type *type)
 {
 	pud_t *pud = pud_offset(pgd, addr);
 	unsigned long next;
