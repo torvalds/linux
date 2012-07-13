19e2e172f0ed2552793ecef506b00c3fa3421845
Ralf Baechle <ralf@linux-mips.org>
2012-07-13 23:38:17 +0200

    MIPS: Provide arch_syscall_addr.
    The generic version is wrong for MIPS.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

---
+++ arch/mips/include/asm/syscall.h
@@ -0,0 +1,16 @@
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2012 Ralf Baechle <ralf@linux-mips.org>
+ */
+
+#ifndef __ASM_MIPS_SYSCALL_H
+#define __ASM_MIPS_SYSCALL_H
+
+extern const unsigned long sys_call_table[];
+extern const unsigned long sys32_call_table[];
+extern const unsigned long sysn32_call_table[];
+
+#endif	/* __ASM_MIPS_SYSCALL_H */
