474239158e69343ce641538719563f9e42037ceb
Todd Poynor <toddpoynor@google.com>
2012-07-13 13:30:04 -0700

    power: power_supply: add POWER_SUPPLY_PROP_USB_OTG
    Change-Id: Idfc6ef2e37d62aad6f26cc8eafa53db642cd352b
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -144,6 +144,7 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_SCOPE,
 	/* Local extensions */
 	POWER_SUPPLY_PROP_USB_HC,
+	POWER_SUPPLY_PROP_USB_OTG,
 	/* Properties of type `const char *' */
 	POWER_SUPPLY_PROP_MODEL_NAME,
 	POWER_SUPPLY_PROP_MANUFACTURER,
