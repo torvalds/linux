a98cdb85b990765dbe80a215367ae007320bfeea
Stefan Behrens <sbehrens@giantdisaster.de>
2012-07-17 09:02:11 -0600

    Btrfs: suppress printk() if all device I/O stats are zero
    Code is added to suppress the I/O stats printing at mount time if all
    statistic values are zero.
    
    Signed-off-by: Stefan Behrens <sbehrens@giantdisaster.de>

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -4884,6 +4884,14 @@ void btrfs_dev_stat_print_on_error(struct btrfs_device *dev)
 
 static void btrfs_dev_stat_print_on_load(struct btrfs_device *dev)
 {
+	int i;
+
+	for (i = 0; i < BTRFS_DEV_STAT_VALUES_MAX; i++)
+		if (btrfs_dev_stat_read(dev, i) != 0)
+			break;
+	if (i == BTRFS_DEV_STAT_VALUES_MAX)
+		return; /* all values == 0, suppress message */
+
 	printk_in_rcu(KERN_INFO "btrfs: bdev %s errs: wr %u, rd %u, flush %u, corrupt %u, gen %u\n",
 	       rcu_str_deref(dev->name),
 	       btrfs_dev_stat_read(dev, BTRFS_DEV_STAT_WRITE_ERRS),
