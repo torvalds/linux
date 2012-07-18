778435045a416da71f1757a80e37200a5ea5af70
Andrew Lunn <andrew@lunn.ch>
2012-07-18 19:22:54 +0200

    ARM: Kirkwood: Replace mrvl with marvell
    It has been decided to use marvell, not mrvl, in the compatibility
    property. Search & replace.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>

--- drivers/mtd/nand/orion_nand.c
+++ drivers/mtd/nand/orion_nand.c
@@ -218,7 +218,7 @@ static int __devexit orion_nand_remove(struct platform_device *pdev)
 
 #ifdef CONFIG_OF
 static struct of_device_id orion_nand_of_match_table[] = {
-	{ .compatible = "mrvl,orion-nand", },
+	{ .compatible = "marvell,orion-nand", },
 	{},
 };
 #endif
