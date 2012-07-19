caeb2490e046909fcb2f8209b88458a147011819
H Hartley Sweeten <hartleys@visionengravers.com>
2012-07-18 18:25:49 -0700

    staging: comedi: adl_pci7296: store the pci_dev in the comedi_device
    Use the hw_dev pointer in the comedi_device struct to hold the
    pci_dev instead of carrying it in the private data.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/adl_pci7296.c
+++ drivers/staging/comedi/drivers/adl_pci7296.c
@@ -49,7 +49,6 @@ Configuration Options:
 
 struct adl_pci7296_private {
 	int data;
-	struct pci_dev *pci_dev;
 };
 
 #define devpriv ((struct adl_pci7296_private *)dev->private)
@@ -82,6 +81,7 @@ static struct pci_dev *adl_pci7296_find_pci(struct comedi_device *dev,
 static int adl_pci7296_attach(struct comedi_device *dev,
 			      struct comedi_devconfig *it)
 {
+	struct pci_dev *pcidev;
 	struct comedi_subdevice *s;
 	int ret;
 
@@ -96,18 +96,19 @@ static int adl_pci7296_attach(struct comedi_device *dev,
 	if (ret)
 		return ret;
 
-	devpriv->pci_dev = adl_pci7296_find_pci(dev, it);
-	if (!devpriv->pci_dev)
+	pcidev = adl_pci7296_find_pci(dev, it);
+	if (!pcidev)
 		return -EIO;
+	comedi_set_hw_dev(dev, &pcidev->dev);
 
-	if (comedi_pci_enable(devpriv->pci_dev, "adl_pci7296") < 0) {
+	if (comedi_pci_enable(pcidev, "adl_pci7296") < 0) {
 		printk(KERN_ERR
 			"comedi%d: Failed to enable PCI device and request regions\n",
 			dev->minor);
 		return -EIO;
 	}
 
-	dev->iobase = pci_resource_start(devpriv->pci_dev, 2);
+	dev->iobase = pci_resource_start(pcidev, 2);
 	printk(KERN_INFO "comedi: base addr %4lx\n", dev->iobase);
 
 	/*  four 8255 digital io subdevices */
@@ -139,10 +140,12 @@ static int adl_pci7296_attach(struct comedi_device *dev,
 
 static void adl_pci7296_detach(struct comedi_device *dev)
 {
-	if (devpriv && devpriv->pci_dev) {
+	struct pci_dev *pcidev = comedi_to_pci_dev(dev);
+
+	if (pcidev) {
 		if (dev->iobase)
-			comedi_pci_disable(devpriv->pci_dev);
-		pci_dev_put(devpriv->pci_dev);
+			comedi_pci_disable(pcidev);
+		pci_dev_put(pcidev);
 	}
 	if (dev->subdevices) {
 		subdev_8255_cleanup(dev, dev->subdevices + 0);
