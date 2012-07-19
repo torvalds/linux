e6c1b59aaa759ae5374dedf8508ee14aff2f967f
Joerg Roedel <joerg.roedel@amd.com>
2012-07-19 10:56:10 +0200

    iommu/amd: Add missing spin_lock initialization
    commit 2c13d47a1a7ee8808796016c617aef25fd1d1925 upstream.
    
    Add missing spin_lock initialization in
    amd_iommu_bind_pasid() function and make lockdep happy
    again.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/iommu/amd_iommu_v2.c
+++ drivers/iommu/amd_iommu_v2.c
@@ -681,6 +681,8 @@ int amd_iommu_bind_pasid(struct pci_dev *pdev, int pasid,
 
 	atomic_set(&pasid_state->count, 1);
 	init_waitqueue_head(&pasid_state->wq);
+	spin_lock_init(&pasid_state->lock);
+
 	pasid_state->task         = task;
 	pasid_state->mm           = get_task_mm(task);
 	pasid_state->device_state = dev_state;
