ca6d25029a5ecc9f1f3497537eafa8557e9cfdab
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-07-20 11:25:50 -0700

    staging: csr: clean up csr_types.h a bit
    Remove the floating point variables and the sections that are not being
    built if the code isn't in the kernel.
    
    Cc: Mikko Virkkilä <mikko.virkkila@bluegiga.com>
    Cc: Lauri Hintsala <Lauri.Hintsala@bluegiga.com>
    Cc: Riku Mettälä <riku.mettala@bluegiga.com>
    Cc: Veli-Pekka Peltola <veli-pekka.peltola@bluegiga.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/csr/csr_types.h
+++ drivers/staging/csr/csr_types.h
@@ -10,19 +10,11 @@
 
 *****************************************************************************/
 
-#ifdef __KERNEL__
 #include <linux/stddef.h>
 #include <linux/kernel.h>
 #include <linux/types.h>
-#include <asm/byteorder.h>
 #include <linux/string.h>
-#else
-#include <stdint.h>
-#include <stddef.h>
-#include <sys/types.h>
-#include <stdarg.h>
-#include <string.h>
-#endif
+#include <asm/byteorder.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -38,11 +30,7 @@ extern "C" {
 typedef size_t CsrSize;         /* Return type of sizeof (ISO/IEC 9899:1990 7.1.6) */
 typedef ptrdiff_t CsrPtrdiff;   /* Type of the result of subtracting two pointers (ISO/IEC 9899:1990 7.1.6) */
 typedef uintptr_t CsrUintptr;   /* Unsigned integer large enough to hold any pointer (ISO/IEC 9899:1999 7.18.1.4) */
-#ifdef __KERNEL__
 typedef ptrdiff_t CsrIntptr;    /* intptr_t is not defined in kernel. Use the equivalent ptrdiff_t. */
-#else
-typedef intptr_t CsrIntptr;     /* Signed integer large enough to hold any pointer (ISO/IEC 9899:1999 7.18.1.4) */
-#endif
 
 /* Unsigned fixed width types */
 typedef uint8_t CsrUint8;
@@ -75,17 +63,6 @@ typedef CsrUint32 CsrUint24;
 typedef uint64_t CsrUint64;
 typedef int64_t CsrInt64;
 
-/*
- * Floating point
- *
- * Note: If a given compiler does not support floating point, it is
- * OK to omit these definitions;  alternative versions of the code using
- * these types may be available.  Consult the relevant documentation
- * or the customer support group for information on this.
- */
-#define CSR_HAVE_FLOATING_POINT
-typedef float CsrFloat;
-typedef double CsrDouble;
 
 #ifdef __cplusplus
 }
