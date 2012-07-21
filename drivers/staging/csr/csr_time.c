02c4be05d9718d5d0f45db39cbef8a927ff4ebb8
Devendra Naga <develkernel412222@gmail.com>
2012-07-21 13:05:32 +0545

    staging/csr: coding style fixes in CsrTimeGet
    the following warnings were fixed:
    1. removed all spaces before the start of a line and used tabs
    2. removed the braces around the if as it encloses only a single statement
    
    Signed-off-by: Devendra Naga <develkernel412222@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/csr/csr_time.c
+++ drivers/staging/csr/csr_time.c
@@ -24,20 +24,18 @@
 
 CsrTime CsrTimeGet(CsrTime *high)
 {
-    struct timespec ts;
-    u64 time;
-    CsrTime low;
+	struct timespec ts;
+	u64 time;
+	CsrTime low;
 
-    ts = current_kernel_time();
-    time = (u64) ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
+	ts = current_kernel_time();
+	time = (u64) ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
 
-    if (high != NULL)
-    {
-        *high = (CsrTime) ((time >> 32) & 0xFFFFFFFF);
-    }
+	if (high != NULL)
+		*high = (CsrTime) ((time >> 32) & 0xFFFFFFFF);
 
-    low = (CsrTime) (time & 0xFFFFFFFF);
+	low = (CsrTime) (time & 0xFFFFFFFF);
 
-    return low;
+	return low;
 }
 EXPORT_SYMBOL_GPL(CsrTimeGet);
