163049aefdc04323a2d17ec9f2862027b43b0502
Stefan Hajnoczi <stefanha@gmail.com>
2012-07-21 06:55:37 +0000

    vhost: make vhost work queue visible
    The vhost work queue allows processing to be done in vhost worker thread
    context, which uses the owner process mm.  Access to the vring and guest
    memory is typically only possible from vhost worker context so it is
    useful to allow work to be queued directly by users.
    
    Currently vhost_net only uses the poll wrappers which do not expose the
    work queue functions.  However, for tcm_vhost (vhost_scsi) it will be
    necessary to queue custom work.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
    Cc: Zhi Yong Wu <wuzhy@cn.ibm.com>
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- drivers/vhost/vhost.h
+++ drivers/vhost/vhost.h
@@ -43,6 +43,9 @@ struct vhost_poll {
 	struct vhost_dev	 *dev;
 };
 
+void vhost_work_init(struct vhost_work *work, vhost_work_fn_t fn);
+void vhost_work_queue(struct vhost_dev *dev, struct vhost_work *work);
+
 void vhost_poll_init(struct vhost_poll *poll, vhost_work_fn_t fn,
 		     unsigned long mask, struct vhost_dev *dev);
 void vhost_poll_start(struct vhost_poll *poll, struct file *file);
