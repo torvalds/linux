0b5306b329a773b9fdd1d53bec6d08ce122d7b65
Al Viro <viro@zeniv.linux.org.uk>
2012-07-22 21:15:37 +0400

    brcm80211: pointless current->files passed to filp_close()
    ... only needed if it's been in descriptor table
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/net/wireless/brcm80211/brcmfmac/dhd_linux.c
+++ drivers/net/wireless/brcm80211/brcmfmac/dhd_linux.c
@@ -1182,7 +1182,7 @@ exit:
 	kfree(buf);
 	/* close file before return */
 	if (fp)
-		filp_close(fp, current->files);
+		filp_close(fp, NULL);
 	/* restore previous address limit */
 	set_fs(old_fs);
 
