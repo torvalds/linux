d95e933730b3eb7b06bd778dc1d8f0ab3702b607
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-07-26 15:25:48 +0100

    ASoC: ab8500: Remove pointless cast
    There's never any need to cast away from void.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Lee Jones <lee.jones@linaro.org>

--- sound/soc/codecs/ab8500-codec.c
+++ sound/soc/codecs/ab8500-codec.c
@@ -2404,11 +2404,11 @@ static int ab8500_codec_probe(struct snd_soc_codec *codec)
 
 	dev_dbg(dev, "%s: Enter.\n", __func__);
 
-	/* Setup AB8500 according to board-settings */
-	pdata = (struct ab8500_platform_data *)dev_get_platdata(dev->parent);
-
 	/* Inform SoC Core that we have our own I/O arrangements. */
 	codec->control_data = (void *)true;
+
+	/* Setup AB8500 according to board-settings */
+	pdata = dev_get_platdata(dev->parent);
 
 	status = ab8500_audio_setup_mics(codec, &pdata->codec->amics);
 	if (status < 0) {
