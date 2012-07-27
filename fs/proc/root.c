4f326c0064b20b78b8041f4d2f6fe188a1129f18
Eric W. Biederman <ebiederm@xmission.com>
2012-07-27 05:56:48 -0700

    userns: Allow unprivilged mounts of proc and sysfs
    - The context in which proc and sysfs are mounted have no
      effect on the the uid/gid of their files so no conversion is
      needed except allowing the mount.
    
    Acked-by: Serge Hallyn <serge.hallyn@canonical.com>
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- fs/proc/root.c
+++ fs/proc/root.c
@@ -145,6 +145,7 @@ static struct file_system_type proc_fs_type = {
 	.name		= "proc",
 	.mount		= proc_mount,
 	.kill_sb	= proc_kill_sb,
+	.fs_flags	= FS_USERNS_MOUNT,
 };
 
 void __init proc_root_init(void)
