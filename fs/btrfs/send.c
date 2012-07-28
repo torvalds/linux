d27aed5e24f8e7bb2b0d11e7a579f2bbdebafc2f
Alexander Block <ablock84@googlemail.com>
2012-07-28 12:04:16 +0200

    Btrfs: remove unused use_list from send/receive code
    use_list is a leftover and unused.
    
    Signed-off-by: Alexander Block <ablock84@googlemail.com>

--- fs/btrfs/send.c
+++ fs/btrfs/send.c
@@ -125,7 +125,6 @@ struct send_ctx {
 
 struct name_cache_entry {
 	struct list_head list;
-	struct list_head use_list;
 	u64 ino;
 	u64 gen;
 	u64 parent_ino;
@@ -1906,7 +1905,6 @@ out_cache:
 	nce->name_len = fs_path_len(dest);
 	nce->ret = ret;
 	strcpy(nce->name, dest->start);
-	memset(&nce->use_list, 0, sizeof(nce->use_list));
 
 	if (ino < sctx->send_progress)
 		nce->need_later_update = 0;
