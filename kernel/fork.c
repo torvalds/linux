b2412b7fa7a3816fa8633dc2ff19f1a90aabe423
Huang Shijie <shijie8@gmail.com>
2012-07-30 14:42:30 -0700

    fork: use vma_pages() to simplify the code
    The current code can be replaced by vma_pages().  So use it to simplify
    the code.
    
    [akpm@linux-foundation.org: initialise `len' at its definition site]
    Signed-off-by: Huang Shijie <shijie8@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -391,8 +391,8 @@ static int dup_mmap(struct mm_struct *mm, struct mm_struct *oldmm)
 		}
 		charge = 0;
 		if (mpnt->vm_flags & VM_ACCOUNT) {
-			unsigned long len;
-			len = (mpnt->vm_end - mpnt->vm_start) >> PAGE_SHIFT;
+			unsigned long len = vma_pages(mpnt);
+
 			if (security_vm_enough_memory_mm(oldmm, len)) /* sic */
 				goto fail_nomem;
 			charge = len;
