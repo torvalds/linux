6f1a656325ea01638de44f8d40bb77c73181b275
Lars Ellenberg <lars.ellenberg@linbit.com>
2012-07-30 09:11:01 +0200

    drbd: call local-io-error handler early
    In case we want to hard-reset from the local-io-error handler,
    we need to call it before notifying the peer or aborting local IO.
    Otherwise the peer will advance its data generation UUIDs even
    if secondary.
    
    This way, local io error looks like a "regular" node crash,
    which reduces the number of different failure cases.
    This may be useful in a bigger picture where crashed or otherwise
    "misbehaving" nodes are automatically re-deployed.
    
    Signed-off-by: Philipp Reisner <philipp.reisner@linbit.com>
    Signed-off-by: Lars Ellenberg <lars.ellenberg@linbit.com>

--- drivers/block/drbd/drbd_state.c
+++ drivers/block/drbd/drbd_state.c
@@ -1342,6 +1342,9 @@ static void after_state_ch(struct drbd_conf *mdev, union drbd_state os,
 
 			was_io_error = test_and_clear_bit(WAS_IO_ERROR, &mdev->flags);
 
+			if (was_io_error && eh == EP_CALL_HELPER)
+				drbd_khelper(mdev, "local-io-error");
+
 			/* Immediately allow completion of all application IO,
 			 * that waits for completion from the local disk,
 			 * if this was a force-detach due to disk_timeout
@@ -1377,9 +1380,6 @@ static void after_state_ch(struct drbd_conf *mdev, union drbd_state os,
 			drbd_md_sync(mdev);
 		}
 		put_ldev(mdev);
-
-		if (was_io_error && eh == EP_CALL_HELPER)
-			drbd_khelper(mdev, "local-io-error");
 	}
 
         /* second half of local IO error, failure to attach,
