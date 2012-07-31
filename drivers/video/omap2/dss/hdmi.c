d3b4aa519c2c916c76ce7d83fa7288a6fd9ad9ac
Ricardo Neri <ricardo.neri@ti.com>
2012-07-30 19:12:02 -0500

    OMAPDSS: HDMI: Disable PLL properly in case of error at power_on
    Small patch to disable the PLL appropriately before runtime_put in case
    an error occurs while enabling the PHY.
    
    Signed-off-by: Ricardo Neri <ricardo.neri@ti.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/hdmi.c
+++ drivers/video/omap2/dss/hdmi.c
@@ -501,7 +501,7 @@ static int hdmi_power_on(struct omap_dss_device *dssdev)
 	r = hdmi.ip_data.ops->phy_enable(&hdmi.ip_data);
 	if (r) {
 		DSSDBG("Failed to start PHY\n");
-		goto err;
+		goto err_phy_enable;
 	}
 
 	hdmi.ip_data.ops->video_configure(&hdmi.ip_data);
@@ -537,6 +537,7 @@ err_mgr_enable:
 	hdmi.ip_data.ops->video_disable(&hdmi.ip_data);
 err_vid_enable:
 	hdmi.ip_data.ops->phy_disable(&hdmi.ip_data);
+err_phy_enable:
 	hdmi.ip_data.ops->pll_disable(&hdmi.ip_data);
 err:
 	hdmi_runtime_put();
