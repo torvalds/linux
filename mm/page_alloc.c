8783b6e2b2cb726f2734cf208d101f73ac1ba616
Linus Torvalds <torvalds@linux-foundation.org>
2012-08-02 10:37:03 -0700

    mm: remove node_start_pfn checking in new WARN_ON for now
    Borislav Petkov reports that the new warning added in commit
    88fdf75d1bb5 ("mm: warn if pg_data_t isn't initialized with zero")
    triggers for him, and it is the node_start_pfn field that has already
    been initialized once.
    
    The call trace looks like this:
    
      x86_64_start_kernel ->
        x86_64_start_reservations ->
        start_kernel ->
        setup_arch ->
        paging_init ->
        zone_sizes_init ->
        free_area_init_nodes ->
        free_area_init_node
    
    and (with the warning replaced by debug output), Borislav sees
    
      On node 0 totalpages: 4193848
        DMA zone: 64 pages used for memmap
        DMA zone: 6 pages reserved
        DMA zone: 3890 pages, LIFO batch:0
        DMA32 zone: 16320 pages used for memmap
        DMA32 zone: 798464 pages, LIFO batch:31
        Normal zone: 52736 pages used for memmap
        Normal zone: 3322368 pages, LIFO batch:31
      free_area_init_node: pgdat->node_start_pfn: 4423680      <----
      On node 1 totalpages: 4194304
        Normal zone: 65536 pages used for memmap
        Normal zone: 4128768 pages, LIFO batch:31
      free_area_init_node: pgdat->node_start_pfn: 8617984      <----
      On node 2 totalpages: 4194304
        Normal zone: 65536 pages used for memmap
        Normal zone: 4128768 pages, LIFO batch:31
      free_area_init_node: pgdat->node_start_pfn: 12812288     <----
      On node 3 totalpages: 4194304
        Normal zone: 65536 pages used for memmap
        Normal zone: 4128768 pages, LIFO batch:31
    
    so remove the bogus warning for now to avoid annoying people.  Minchan
    Kim is looking at it.
    
    Reported-by: Borislav Petkov <bp@amd64.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4511,7 +4511,7 @@ void __paginginit free_area_init_node(int nid, unsigned long *zones_size,
 	pg_data_t *pgdat = NODE_DATA(nid);
 
 	/* pg_data_t should be reset to zero when it's allocated */
-	WARN_ON(pgdat->nr_zones || pgdat->node_start_pfn || pgdat->classzone_idx);
+	WARN_ON(pgdat->nr_zones || pgdat->classzone_idx);
 
 	pgdat->node_id = nid;
 	pgdat->node_start_pfn = node_start_pfn;
