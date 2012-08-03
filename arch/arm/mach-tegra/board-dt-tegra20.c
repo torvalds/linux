be6a9194f158482347c430c04ac9ea64644dda67
Stephen Warren <swarren@nvidia.com>
2012-08-03 14:55:36 -0600

    ARM: tegra: remove board (but not DT) support for TrimSlice
    TrimSlice can be booted using device tree with equal functionality as
    when booted using a board file. Remove the board file since it's no
    longer needed.
    
    One special-case is still left in board-dt-tegra20.c, since the Tegra
    PCIe driver doesn't support device tree yet. This logic is now enabled
    by CONFIG_TEGRA_PCI rather than via CONFIG_MACH_TRIMSLICE. The extra
    cases where it's enabled (.configs which did not enable TrimSlice
    support) shouldn't impact much since the amount of code is tiny.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/board-dt-tegra20.c
+++ arch/arm/mach-tegra/board-dt-tegra20.c
@@ -95,16 +95,16 @@ static void __init tegra_dt_init(void)
 				tegra20_auxdata_lookup, NULL);
 }
 
-#ifdef CONFIG_MACH_TRIMSLICE
 static void __init trimslice_init(void)
 {
+#ifdef CONFIG_TEGRA_PCI
 	int ret;
 
 	ret = tegra_pcie_init(true, true);
 	if (ret)
 		pr_err("tegra_pci_init() failed: %d\n", ret);
-}
 #endif
+}
 
 #ifdef CONFIG_MACH_HARMONY
 static void __init harmony_init(void)
@@ -134,9 +134,7 @@ static struct {
 	char *machine;
 	void (*init)(void);
 } board_init_funcs[] = {
-#ifdef CONFIG_MACH_TRIMSLICE
 	{ "compulab,trimslice", trimslice_init },
-#endif
 #ifdef CONFIG_MACH_HARMONY
 	{ "nvidia,harmony", harmony_init },
 #endif
