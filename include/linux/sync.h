8b79db57632b7e9d45af8c0d4fda1db7f116b535
Erik Gilling <konkers@android.com>
2012-08-02 17:26:45 -0700

    sync: add reference counting to timelines
    If a timeline is destroyed while fences still hold pts on it, the reworked
    fence release handler can cause the timeline to be freed before all it's points
    are freed.
    
    Change-Id: I1cd8ddb638eded7db9db446ff6b37f3dd165d6c4
    Signed-off-by: Erik Gilling <konkers@android.com>

--- include/linux/sync.h
+++ include/linux/sync.h
@@ -80,6 +80,7 @@ struct sync_timeline_ops {
 
 /**
  * struct sync_timeline - sync object
+ * @kref:		reference count on fence.
  * @ops:		ops that define the implementaiton of the sync_timeline
  * @name:		name of the sync_timeline. Useful for debugging
  * @destoryed:		set when sync_timeline is destroyed
@@ -90,6 +91,7 @@ struct sync_timeline_ops {
  * @sync_timeline_list:	membership in global sync_timeline_list
  */
 struct sync_timeline {
+	struct kref		kref;
 	const struct sync_timeline_ops	*ops;
 	char			name[32];
 
