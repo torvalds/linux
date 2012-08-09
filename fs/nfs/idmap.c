12dfd080556124088ed61a292184947711b46cbe
Bryan Schumaker <bjschuma@netapp.com>
2012-08-09 14:05:50 -0400

    NFS: return -ENOKEY when the upcall fails to map the name
    This allows the normal error-paths to handle the error, rather than
    making a special call to complete_request_key() just for this instance.
    
    Signed-off-by: Bryan Schumaker <bjschuma@netapp.com>
    Tested-by: William Dauchy <wdauchy@gmail.com>
    Cc: stable@vger.kernel.org [>= 3.4]
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/idmap.c
+++ fs/nfs/idmap.c
@@ -760,9 +760,8 @@ idmap_pipe_downcall(struct file *filp, const char __user *src, size_t mlen)
 	}
 
 	if (!(im.im_status & IDMAP_STATUS_SUCCESS)) {
-		ret = mlen;
-		complete_request_key(cons, -ENOKEY);
-		goto out_incomplete;
+		ret = -ENOKEY;
+		goto out;
 	}
 
 	namelen_in = strnlen(im.im_name, IDMAP_NAMESZ);
@@ -779,7 +778,6 @@ idmap_pipe_downcall(struct file *filp, const char __user *src, size_t mlen)
 
 out:
 	complete_request_key(cons, ret);
-out_incomplete:
 	return ret;
 }
 
