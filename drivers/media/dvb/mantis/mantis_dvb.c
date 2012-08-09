136eafad6722b0957cf77f40b187c4f0d97f9f22
Alan Cox <alan@linux.intel.com>
2012-08-09 12:33:52 -0300

    media: mantis: fix silly crash case
    commit e1d45ae10aea8e8a403e5d96bf5902ee670007ff upstream.
    
    If we set mantis->fe to NULL on an error its not a good idea to then try
    passing NULL to the unregister paths and oopsing really.
    
    Resolves-bug: https://bugzilla.kernel.org/show_bug.cgi?id=16473
    
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>
    Cc: Bjørn Mork <bjorn@mork.no>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/media/dvb/mantis/mantis_dvb.c
+++ drivers/media/dvb/mantis/mantis_dvb.c
@@ -248,8 +248,10 @@ int __devinit mantis_dvb_init(struct mantis_pci *mantis)
 err5:
 	tasklet_kill(&mantis->tasklet);
 	dvb_net_release(&mantis->dvbnet);
-	dvb_unregister_frontend(mantis->fe);
-	dvb_frontend_detach(mantis->fe);
+	if (mantis->fe) {
+		dvb_unregister_frontend(mantis->fe);
+		dvb_frontend_detach(mantis->fe);
+	}
 err4:
 	mantis->demux.dmx.remove_frontend(&mantis->demux.dmx, &mantis->fe_mem);
 
