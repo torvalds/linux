e1bbb55d11622012197a654af874f9f485ddbc47
Linus Walleij <linus.walleij@linaro.org>
2012-08-09 17:10:36 +0200

    ARM: ux500: reform Ux500 family names
    Counting the U9540 and the new U8540 as a U8500 family member
    does not work. Instead, split the function in two:
    
    cpu_is_u8500_family() covering U8500 and U8520
    cpu_is_ux540_family() covering U9540 and U8540
    
    This works much better in practice. Update users to keep the
    same behaviour.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/cache-l2x0.c
+++ arch/arm/mach-ux500/cache-l2x0.c
@@ -38,7 +38,7 @@ static int __init ux500_l2x0_init(void)
 {
 	u32 aux_val = 0x3e000000;
 
-	if (cpu_is_u8500_family())
+	if (cpu_is_u8500_family() || cpu_is_ux540_family())
 		l2x0_base = __io_address(U8500_L2CC_BASE);
 	else
 		ux500_unknown_soc();
--- arch/arm/mach-ux500/include/mach/id.h
+++ arch/arm/mach-ux500/include/mach/id.h
@@ -46,14 +46,24 @@ static inline bool __attribute_const__ cpu_is_u8520(void)
 	return dbx500_partnumber() == 0x8520;
 }
 
+static inline bool cpu_is_u8500_family(void)
+{
+	return cpu_is_u8500() || cpu_is_u8520();
+}
+
 static inline bool __attribute_const__ cpu_is_u9540(void)
 {
 	return dbx500_partnumber() == 0x9540;
 }
 
-static inline bool cpu_is_u8500_family(void)
+static inline bool __attribute_const__ cpu_is_u8540(void)
 {
-	return cpu_is_u8500() || cpu_is_u9540();
+	return dbx500_partnumber() == 0x8540;
+}
+
+static inline bool cpu_is_ux540_family(void)
+{
+	return cpu_is_u9540() || cpu_is_u8540();
 }
 
 /*
@@ -97,12 +107,7 @@ static inline bool cpu_is_u8500v21(void)
 
 static inline bool cpu_is_u8500v20_or_later(void)
 {
-	/*
-	 * U9540 has so much in common with U8500 that is is considered a
-	 * U8500 variant.
-	 */
-	return cpu_is_u9540() ||
-		(cpu_is_u8500() && !cpu_is_u8500v10() && !cpu_is_u8500v11());
+	return (cpu_is_u8500() && !cpu_is_u8500v10() && !cpu_is_u8500v11());
 }
 
 static inline bool ux500_is_svp(void)
