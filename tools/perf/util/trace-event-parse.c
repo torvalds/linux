d0d3913895e5b50623bd4dafd876e41c1169031d
Namhyung Kim <namhyung.kim@lge.com>
2012-08-14 10:57:03 +0900

    perf script: Fix a NULL pointer dereference
    If 'perf script --gen-script' was called with a perf.data which contains
    no tracepoint event, it'd segfault due to NULL pevent pointer. Fix it.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Feng Tang <feng.tang@intel.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1344909423-26384-1-git-send-email-namhyung@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/trace-event-parse.c
+++ tools/perf/util/trace-event-parse.c
@@ -293,7 +293,7 @@ struct event_format *trace_find_next_event(struct pevent *pevent,
 {
 	static int idx;
 
-	if (!pevent->events)
+	if (!pevent || !pevent->events)
 		return NULL;
 
 	if (!event) {
