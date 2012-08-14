f026cfa82f628db24b8cea41b9d6202af104cecb
H. Peter Anvin <hpa@zytor.com>
2012-08-14 09:53:38 -0700

    Revert "x86-64/efi: Use EFI to deal with platform wall clock"
    This reverts commit bacef661acdb634170a8faddbc1cf28e8f8b9eee.
    
    This commit has been found to cause serious regressions on a number of
    ASUS machines at the least.  We probably need to provide a 1:1 map in
    addition to the EFI virtual memory map in order for this to work.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>
    Reported-and-bisected-by: Jérôme Carretero <cJ-ko@zougloub.eu>
    Cc: Jan Beulich <jbeulich@suse.com>
    Cc: Matt Fleming <matt.fleming@intel.com>
    Cc: Matthew Garrett <mjg@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/20120805172903.5f8bb24c@zougloub.eu

--- init/main.c
+++ init/main.c
@@ -461,10 +461,6 @@ static void __init mm_init(void)
 	percpu_init_late();
 	pgtable_cache_init();
 	vmalloc_init();
-#ifdef CONFIG_X86
-	if (efi_enabled)
-		efi_enter_virtual_mode();
-#endif
 }
 
 asmlinkage void __init start_kernel(void)
@@ -606,6 +602,10 @@ asmlinkage void __init start_kernel(void)
 	calibrate_delay();
 	pidmap_init();
 	anon_vma_init();
+#ifdef CONFIG_X86
+	if (efi_enabled)
+		efi_enter_virtual_mode();
+#endif
 	thread_info_cache_init();
 	cred_init();
 	fork_init(totalram_pages);
