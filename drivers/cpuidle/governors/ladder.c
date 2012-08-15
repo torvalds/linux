66804c13f7b79fb26cf4848ebac1e865b9aff65c
Rafael J. Wysocki <rjw@sisk.pl>
2012-08-15 20:28:52 +0200

    PM / cpuidle: Make ladder governor use the "disabled" state flag
    For the mechanism introduced by commit cbc9ef0 (PM / Domains: Add
    preliminary support for cpuidle, v2) to work with the ladder
    governor, that governor should respect the "disabled" state flag
    added by that commit.  Change the ladder governor accordingly.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- drivers/cpuidle/governors/ladder.c
+++ drivers/cpuidle/governors/ladder.c
@@ -88,6 +88,7 @@ static int ladder_select_state(struct cpuidle_driver *drv,
 
 	/* consider promotion */
 	if (last_idx < drv->state_count - 1 &&
+	    !drv->states[last_idx + 1].disabled &&
 	    !dev->states_usage[last_idx + 1].disable &&
 	    last_residency > last_state->threshold.promotion_time &&
 	    drv->states[last_idx + 1].exit_latency <= latency_req) {
@@ -101,7 +102,8 @@ static int ladder_select_state(struct cpuidle_driver *drv,
 
 	/* consider demotion */
 	if (last_idx > CPUIDLE_DRIVER_STATE_START &&
-	    (dev->states_usage[last_idx].disable ||
+	    (drv->states[last_idx].disabled ||
+	    dev->states_usage[last_idx].disable ||
 	    drv->states[last_idx].exit_latency > latency_req)) {
 		int i;
 
