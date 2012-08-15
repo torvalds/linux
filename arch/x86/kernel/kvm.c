8fbe6a541f50eeec5e3e49bd92db23ade9496673
Florian Westphal <fw@strlen.de>
2012-08-15 16:00:40 +0200

    KVM guest: disable stealtime on reboot to avoid mem corruption
    else, host continues to update stealtime after reboot,
    which can corrupt e.g. initramfs area.
    found when tracking down initramfs unpack error on initial reboot
    (with qemu-kvm -smp 2, no problem with single-core).
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/x86/kernel/kvm.c
+++ arch/x86/kernel/kvm.c
@@ -354,6 +354,7 @@ static void kvm_pv_guest_cpu_reboot(void *unused)
 	if (kvm_para_has_feature(KVM_FEATURE_PV_EOI))
 		wrmsrl(MSR_KVM_PV_EOI_EN, 0);
 	kvm_pv_disable_apf();
+	kvm_disable_steal_time();
 }
 
 static int kvm_pv_reboot_notify(struct notifier_block *nb,
