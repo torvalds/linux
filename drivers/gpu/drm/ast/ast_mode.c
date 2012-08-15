993dcb05e47e357ade19c41f60b39085eeba8787
Jani Nikula <jani.nikula@intel.com>
2012-08-15 09:32:37 +0000

    drm/ast: fix EDID memory leak
    The EDID returned by drm_get_edid() was never freed.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/ast/ast_mode.c
+++ drivers/gpu/drm/ast/ast_mode.c
@@ -737,6 +737,7 @@ static int ast_get_modes(struct drm_connector *connector)
 	if (edid) {
 		drm_mode_connector_update_edid_property(&ast_connector->base, edid);
 		ret = drm_add_edid_modes(connector, edid);
+		kfree(edid);
 		return ret;
 	} else
 		drm_mode_connector_update_edid_property(&ast_connector->base, NULL);
