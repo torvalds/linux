5c13f8067745efc15f6ad0158b58d57c44104c25
Daniel De Graaf <dgdegra@tycho.nsa.gov>
2012-08-16 16:40:26 -0400

    xen/sysfs: Use XENVER_guest_handle to query UUID
    This hypercall has been present since Xen 3.1, and is the preferred
    method for a domain to obtain its UUID. Fall back to the xenstore method
    if using an older version of Xen (which returns -ENOSYS).
    
    Signed-off-by: Daniel De Graaf <dgdegra@tycho.nsa.gov>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- include/xen/interface/version.h
+++ include/xen/interface/version.h
@@ -60,4 +60,7 @@ struct xen_feature_info {
 /* arg == NULL; returns host memory page size. */
 #define XENVER_pagesize 7
 
+/* arg == xen_domain_handle_t. */
+#define XENVER_guest_handle 8
+
 #endif /* __XEN_PUBLIC_VERSION_H__ */
