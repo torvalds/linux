7da5907c84f8b092dc42b47b5c19684c1b3b4c98
Inki Dae <inki.dae@samsung.com>
2012-08-17 15:24:03 +0900

    drm/exynos: fixed page align bug.
    do not align in page unit at dumb creation. the align is done
    by exynos_drm_gem_create() to be called commonly.
    
    Signed-off-by: Inki Dae <inki.dae@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_gem.c
+++ drivers/gpu/drm/exynos/exynos_drm_gem.c
@@ -662,7 +662,7 @@ int exynos_drm_gem_dumb_create(struct drm_file *file_priv,
 	 */
 
 	args->pitch = args->width * ((args->bpp + 7) / 8);
-	args->size = PAGE_ALIGN(args->pitch * args->height);
+	args->size = args->pitch * args->height;
 
 	exynos_gem_obj = exynos_drm_gem_create(dev, args->flags, args->size);
 	if (IS_ERR(exynos_gem_obj))
