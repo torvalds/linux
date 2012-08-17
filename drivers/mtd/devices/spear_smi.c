036a1ac1f4ac8cdfed4574738c63aba2e81a13e4
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2012-08-17 13:05:41 +0300

    mtd: spear_smi: fix compilation warning
    drivers/mtd/devices/spear_smi.c: In function 'spear_smi_probe':
    drivers/mtd/devices/spear_smi.c:984:2: warning: comparison of unsigned expression < 0 is always false [-Wtype-limits]
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/devices/spear_smi.c
+++ drivers/mtd/devices/spear_smi.c
@@ -981,7 +981,7 @@ static int __devinit spear_smi_probe(struct platform_device *pdev)
 	dev->pdev = pdev;
 	dev->clk_rate = pdata->clk_rate;
 
-	if (dev->clk_rate < 0 || dev->clk_rate > SMI_MAX_CLOCK_FREQ)
+	if (dev->clk_rate > SMI_MAX_CLOCK_FREQ)
 		dev->clk_rate = SMI_MAX_CLOCK_FREQ;
 
 	dev->num_flashes = pdata->num_flashes;
