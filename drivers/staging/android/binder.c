efde99cd281a3f0d3562bb2fa7e7ef60ad32fe8d
Sachin Kamat <sachin.kamat@linaro.org>
2012-08-17 16:39:36 +0530

    Staging: android: binder: Make task_get_unused_fd_flags function static
    Silence the following warning:
    drivers/staging/android/binder.c:368:5: warning:
    symbol 'task_get_unused_fd_flags' was not declared. Should it be static?
    
    Cc: Arve Hjønnevåg <arve@android.com>
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -365,7 +365,7 @@ binder_defer_work(struct binder_proc *proc, enum binder_deferred_state defer);
 /*
  * copied from get_unused_fd_flags
  */
-int task_get_unused_fd_flags(struct binder_proc *proc, int flags)
+static int task_get_unused_fd_flags(struct binder_proc *proc, int flags)
 {
 	struct files_struct *files = proc->files;
 	int fd, error;
