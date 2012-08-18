3de7a37b02f607716753dc669c1dca4f71db08fc
Randy Dunlap <rdunlap@xenotime.net>
2012-08-18 14:36:44 +0000

    net/core/dev.c: fix kernel-doc warning
    Fix kernel-doc warning:
    
    Warning(net/core/dev.c:5745): No description found for parameter 'dev'
    
    Signed-off-by: Randy Dunlap <rdunlap@xenotime.net>
    Cc:	"David S. Miller" <davem@davemloft.net>
    Cc:	netdev@vger.kernel.org
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -5744,6 +5744,7 @@ EXPORT_SYMBOL(netdev_refcnt_read);
 
 /**
  * netdev_wait_allrefs - wait until all references are gone.
+ * @dev: target net_device
  *
  * This is called when unregistering network devices.
  *
