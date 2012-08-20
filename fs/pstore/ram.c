b4a871bce619dc5ca03cc6c78e1c467ceacb8e7e
Jovi Zhang <bookjovi@gmail.com>
2012-08-20 14:58:26 +0800

    pstore/ram: Add missing platform_device_unregister
    We need to unregister platform device when module exit, this commit fixes
    the issue.
    
    Signed-off-by: Jovi Zhang <bookjovi@gmail.com>
    Acked-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Anton Vorontsov <anton.vorontsov@linaro.org>

--- fs/pstore/ram.c
+++ fs/pstore/ram.c
@@ -538,6 +538,7 @@ postcore_initcall(ramoops_init);
 static void __exit ramoops_exit(void)
 {
 	platform_driver_unregister(&ramoops_driver);
+	platform_device_unregister(dummy);
 	kfree(dummy_data);
 }
 module_exit(ramoops_exit);
