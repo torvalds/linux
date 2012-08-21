a81aba14dc0ea499f4c218b5db0303b2ea8151d3
Avi Kivity <avi@redhat.com>
2012-08-21 17:07:10 +0300

    KVM: VMX: Ignore segment G and D bits when considering whether we can virtualize
    We will enter the guest with G and D cleared; as real hardware ignores D in
    real mode, and G is taken care of by the limit test, we allow more code to
    run in vm86 mode.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -3320,7 +3320,7 @@ static bool rmode_segment_valid(struct kvm_vcpu *vcpu, int seg)
 		return false;
 	if (var.limit < 0xffff)
 		return false;
-	if ((ar | (3 << AR_DPL_SHIFT)) != 0xf3)
+	if (((ar | (3 << AR_DPL_SHIFT)) & ~(AR_G_MASK | AR_DB_MASK)) != 0xf3)
 		return false;
 
 	return true;
