0b8623caddde9a30c129eb8b721f8d25eebf4711
Antti Palosaari <crope@iki.fi>
2012-08-21 09:08:51 -0300

    [media] gl861: reset_resume support
    It survives now on reset_resume.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/usb/dvb-usb-v2/gl861.c
+++ drivers/media/usb/dvb-usb-v2/gl861.c
@@ -163,6 +163,7 @@ static struct usb_driver gl861_usb_driver = {
 	.disconnect = dvb_usbv2_disconnect,
 	.suspend = dvb_usbv2_suspend,
 	.resume = dvb_usbv2_resume,
+	.reset_resume = dvb_usbv2_reset_resume,
 	.no_dynamic_id = 1,
 	.soft_unbind = 1,
 };
