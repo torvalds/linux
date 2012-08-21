70cf058aa900394abba126d5950712e5c107768f
Antti Palosaari <crope@iki.fi>
2012-08-21 10:18:59 -0300

    [media] qt1010: remove debug register dump
    I didn't found easy way to handle register dump only when needed so
    remove it totally. It is quite useless and trivial function, every
    developer could write new one in few minutes when needed.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/tuners/qt1010.c
+++ drivers/media/tuners/qt1010.c
@@ -54,27 +54,6 @@ static int qt1010_writereg(struct qt1010_priv *priv, u8 reg, u8 val)
 	return 0;
 }
 
-/* dump all registers */
-static void qt1010_dump_regs(struct qt1010_priv *priv)
-{
-	u8 reg, val;
-
-	for (reg = 0; ; reg++) {
-		if (reg % 16 == 0) {
-			if (reg)
-				printk(KERN_CONT "\n");
-			printk(KERN_DEBUG "%02x:", reg);
-		}
-		if (qt1010_readreg(priv, reg, &val) == 0)
-			printk(KERN_CONT " %02x", val);
-		else
-			printk(KERN_CONT " --");
-		if (reg == 0x2f)
-			break;
-	}
-	printk(KERN_CONT "\n");
-}
-
 static int qt1010_set_params(struct dvb_frontend *fe)
 {
 	struct dtv_frontend_properties *c = &fe->dtv_property_cache;
@@ -239,8 +218,6 @@ static int qt1010_set_params(struct dvb_frontend *fe)
 		}
 		if (err) return err;
 	}
-
-	qt1010_dump_regs(priv);
 
 	if (fe->ops.i2c_gate_ctrl)
 		fe->ops.i2c_gate_ctrl(fe, 0); /* close i2c_gate */
