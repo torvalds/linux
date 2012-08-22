5f42f7bc9638a0bc69dcb8b8b6da542b72113fc2
Giuseppe CAVALLARO <peppe.cavallaro@st.com>
2012-08-22 00:26:15 +0000

    stmmac: fix GMAC syn ID
    Erroneously the DWMAC_CORE_3_40 was set to 34 instead of 0x34.
    This can generate problems when run on old chips because
    the driver assumes that there are the extra 16 regs available
    for perfect filtering.
    
    Signed-off-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>
    Cc: Gianni Antoniazzi <gianni.antoniazzi-ext@st.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/dwmac1000.h
+++ drivers/net/ethernet/stmicro/stmmac/dwmac1000.h
@@ -229,6 +229,6 @@ enum rtc_control {
 #define GMAC_MMC_RX_CSUM_OFFLOAD   0x208
 
 /* Synopsys Core versions */
-#define	DWMAC_CORE_3_40	34
+#define	DWMAC_CORE_3_40	0x34
 
 extern const struct stmmac_dma_ops dwmac1000_dma_ops;
