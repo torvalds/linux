e92347742704342ce1ce88e4a0d46b4d6897c629
Ezequiel García <elezegarcia@gmail.com>
2012-08-23 09:08:24 -0300

    [media] vivi: Remove unneeded struct vb2_queue clearing
    struct vb2_queue is allocated through kzalloc as part of a larger struct,
    there's no need to clear it.
    
    Signed-off-by: Ezequiel Garcia <elezegarcia@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/vivi.c
+++ drivers/media/platform/vivi.c
@@ -1306,7 +1306,6 @@ static int __init vivi_create_instance(int inst)
 
 	/* initialize queue */
 	q = &dev->vb_vidq;
-	memset(q, 0, sizeof(dev->vb_vidq));
 	q->type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 	q->io_modes = VB2_MMAP | VB2_USERPTR | VB2_READ;
 	q->drv_priv = dev;
