b9d29abd98a2bbeb3a6c49c1607348c92bc80105
Benjamin Poirier <bpoirier@suse.de>
2012-08-23 14:55:03 -0400

    menuconfig: Remove superfluous conditionnal
    Because end_reached is set to 0 before the loop, the test "!end_reached" is
    always true and can be removed. This structure was perhaps copied from the
    similar one in back_lines().
    
    Signed-off-by: Benjamin Poirier <bpoirier@suse.de>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/kconfig/lxdialog/textbox.c
+++ scripts/kconfig/lxdialog/textbox.c
@@ -357,10 +357,8 @@ static char *get_line(void)
 	end_reached = 0;
 	while (*page != '\n') {
 		if (*page == '\0') {
-			if (!end_reached) {
-				end_reached = 1;
-				break;
-			}
+			end_reached = 1;
+			break;
 		} else if (i < MAX_LEN)
 			line[i++] = *(page++);
 		else {
@@ -373,7 +371,7 @@ static char *get_line(void)
 	if (i <= MAX_LEN)
 		line[i] = '\0';
 	if (!end_reached)
-		page++;		/* move pass '\n' */
+		page++;		/* move past '\n' */
 
 	return line;
 }
