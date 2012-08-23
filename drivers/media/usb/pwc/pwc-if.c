4a7ec2db262628f8b13af76b09bb147943264529
Ezequiel Garcia <elezegarcia@gmail.com>
2012-08-23 10:08:23 -0300

    [media] pwc: Remove unneeded struct vb2_queue clearing
    struct vb2_queue is allocated through kzalloc as part of a larger struct,
    there's no need to clear it.
    
    Cc: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Ezequiel Garcia <elezegarcia@gmail.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/usb/pwc/pwc-if.c
+++ drivers/media/usb/pwc/pwc-if.c
@@ -994,7 +994,6 @@ static int usb_pwc_probe(struct usb_interface *intf, const struct usb_device_id 
 	pdev->power_save = my_power_save;
 
 	/* Init videobuf2 queue structure */
-	memset(&pdev->vb_queue, 0, sizeof(pdev->vb_queue));
 	pdev->vb_queue.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 	pdev->vb_queue.io_modes = VB2_MMAP | VB2_USERPTR | VB2_READ;
 	pdev->vb_queue.drv_priv = pdev;
