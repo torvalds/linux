f819bacd68cb99883563ed9d70944fa9b4b02505
Erik Gilling <konkers@android.com>
2012-08-24 13:48:34 -0700

    sync: clean up compiler warnings
    Change-Id: I8a2ec5db652c61fd04571402067b37273b91e78f
    Signed-off-by: Erik Gilling <konkers@android.com>

--- drivers/base/sync.c
+++ drivers/base/sync.c
@@ -87,9 +87,6 @@ static void sync_timeline_free(struct kref *kref)
 
 void sync_timeline_destroy(struct sync_timeline *obj)
 {
-	unsigned long flags;
-	bool needs_freeing;
-
 	obj->destroyed = true;
 
 	/*
@@ -117,7 +114,6 @@ static void sync_timeline_remove_pt(struct sync_pt *pt)
 {
 	struct sync_timeline *obj = pt->parent;
 	unsigned long flags;
-	bool needs_freeing = false;
 
 	spin_lock_irqsave(&obj->active_list_lock, flags);
 	if (!list_empty(&pt->active_list))
