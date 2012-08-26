ee6eb96673704225164f0ba7462e1973ce10885c
Patrick McHardy <kaber@trash.net>
2012-08-26 19:14:22 +0200

    netfilter: nf_nat: support IPv6 in amanda NAT helper
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- net/netfilter/nf_conntrack_amanda.c
+++ net/netfilter/nf_conntrack_amanda.c
@@ -155,8 +155,7 @@ static int amanda_help(struct sk_buff *skb,
 				  IPPROTO_TCP, NULL, &port);
 
 		nf_nat_amanda = rcu_dereference(nf_nat_amanda_hook);
-		if (nf_nat_amanda && nf_ct_l3num(ct) == NFPROTO_IPV4 &&
-		    ct->status & IPS_NAT_MASK)
+		if (nf_nat_amanda && ct->status & IPS_NAT_MASK)
 			ret = nf_nat_amanda(skb, ctinfo, protoff,
 					    off - dataoff, len, exp);
 		else if (nf_ct_expect_related(exp) != 0)
