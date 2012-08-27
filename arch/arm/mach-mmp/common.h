9e73d6982314a903beebb2e47e585a9804f237cf
Chao Xie <chao.xie@marvell.com>
2012-08-27 10:54:01 +0800

    arm: mmp: move pxa910 clock definition to separated file
    move pxa910 clock definition to another file. Then pxa910 can
    choose common clock framework or private clock framework.
    
    Signed-off-by: Chao Xie <xiechao.mail@gmail.com>
    Signed-off-by: Haojian Zhuang <haojian.zhuang@gmail.com>

--- arch/arm/mach-mmp/common.h
+++ arch/arm/mach-mmp/common.h
@@ -8,3 +8,4 @@ extern void __init icu_init_irq(void);
 extern void __init mmp_map_io(void);
 extern void mmp_restart(char, const char *);
 extern void __init pxa168_clk_init(void);
+extern void __init pxa910_clk_init(void);
