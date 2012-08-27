50d0e24499787a39e5fa585a770591648a9760fd
Chao Xie <chao.xie@marvell.com>
2012-08-27 10:54:00 +0800

    arm: mmp: move pxa168 clock definition to separated file
    move pxa168 clock definition to another file. Then pxa168 can
    choose common clock framework or private clock framework.
    
    Signed-off-by: Chao Xie <xiechao.mail@gmail.com>
    Signed-off-by: Haojian Zhuang <haojian.zhuang@gmail.com>

--- arch/arm/mach-mmp/common.h
+++ arch/arm/mach-mmp/common.h
@@ -7,3 +7,4 @@ extern void timer_init(int irq);
 extern void __init icu_init_irq(void);
 extern void __init mmp_map_io(void);
 extern void mmp_restart(char, const char *);
+extern void __init pxa168_clk_init(void);
