8430305dc3f3a286a337f1b4419c04afe55a2583
Chao Xie <chao.xie@marvell.com>
2012-08-27 10:54:02 +0800

    ARM: mmp: move mmp2 clock definition to separated file
    move mmp2 clock definition to another file. Then mmp2 can
    choose common clock framework or private clock framework.
    
    Signed-off-by: Chao Xie <xiechao.mail@gmail.com>
    Signed-off-by: Haojian Zhuang <haojian.zhuang@gmail.com>

--- arch/arm/mach-mmp/common.h
+++ arch/arm/mach-mmp/common.h
@@ -9,3 +9,4 @@ extern void __init mmp_map_io(void);
 extern void mmp_restart(char, const char *);
 extern void __init pxa168_clk_init(void);
 extern void __init pxa910_clk_init(void);
+extern void __init mmp2_clk_init(void);
