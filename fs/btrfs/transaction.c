e209db7ace281ca347b1ac699bf1fb222eac03fe
Sage Weil <sage@inktank.com>
2012-08-30 16:26:16 -0600

    Btrfs: set journal_info in async trans commit worker
    We expect current->journal_info to point to the trans handle we are
    committing.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- fs/btrfs/transaction.c
+++ fs/btrfs/transaction.c
@@ -1237,6 +1237,8 @@ static void do_async_commit(struct work_struct *work)
 		&ac->root->fs_info->sb->s_writers.lock_map[SB_FREEZE_FS-1],
 		0, 1, _THIS_IP_);
 
+	current->journal_info = ac->newtrans;
+
 	btrfs_commit_transaction(ac->newtrans, ac->root);
 	kfree(ac);
 }
