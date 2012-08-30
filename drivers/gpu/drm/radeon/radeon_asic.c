e737a14cd1f30a0bca753ae32ebada10adf93c3b
Alex Deucher <alexander.deucher@amd.com>
2012-08-30 14:00:03 -0400

    drm/radeon: radeon-asic updates for Oland
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/radeon_asic.c
+++ drivers/gpu/drm/radeon/radeon_asic.c
@@ -1944,9 +1944,13 @@ int radeon_asic_init(struct radeon_device *rdev)
 	case CHIP_TAHITI:
 	case CHIP_PITCAIRN:
 	case CHIP_VERDE:
+	case CHIP_OLAND:
 		rdev->asic = &si_asic;
 		/* set num crtcs */
-		rdev->num_crtc = 6;
+		if (rdev->family == CHIP_OLAND)
+			rdev->num_crtc = 2;
+		else
+			rdev->num_crtc = 6;
 		break;
 	default:
 		/* FIXME: not supported yet */
