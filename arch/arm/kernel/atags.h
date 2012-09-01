aa783b6fd60b3844e199b1c2d2f4068f3caa1358
Nicolas Pitre <nicolas.pitre@linaro.org>
2012-09-01 03:00:15 +0100

    ARM: 7505/1: split out ATAGS parsing
    Make ATAGS parsing into a source file of its own, namely atags_parse.c.
    Also rename compat.c to atags_compat.c to make it clearer what it is
    about.  Same for atags.c which is now atags_proc.c. Gather all the atags
    function declarations into a common atags.h.
    
    Signed-off-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    Tested-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/atags.h
+++ arch/arm/kernel/atags.h
@@ -3,3 +3,6 @@ extern void save_atags(struct tag *tags);
 #else
 static inline void save_atags(struct tag *tags) { }
 #endif
+
+void convert_to_tag_list(struct tag *tags);
+struct machine_desc *setup_machine_tags(phys_addr_t __atags_pointer, unsigned int machine_nr);
