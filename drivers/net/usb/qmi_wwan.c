3ee2403739db7ec8b683e6451c3401ad716ad5a2
Bjørn Mork <bjorn@mork.no>
2012-09-07 07:36:06 +0000

    net: qmi_wwan: increase max QMI message size to 4096
    QMI requests exceeding 1500 bytes are possible and
    device firmware does not handle fragmented messages
    very well.  It is therefore necessary to increase
    the maximum message size from the current 512 bytes.
    
    The protocol message size limit is not documented
    in any publicly known source, but the out of tree
    driver from CodeAurora use 4 kB.  This is therefore
    chosen as the new arbitrary default until the real
    limit is known.
    
    This should allow any QMI message to be transmitted
    without fragmentation, fixing known issues with GPS
    assistance data upload.
    
    Signed-off-by: Bjørn Mork <bjorn@mork.no>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/qmi_wwan.c
+++ drivers/net/usb/qmi_wwan.c
@@ -108,7 +108,7 @@ static int qmi_wwan_register_subdriver(struct usbnet *dev)
 	atomic_set(&info->pmcount, 0);
 
 	/* register subdriver */
-	subdriver = usb_cdc_wdm_register(info->control, &dev->status->desc, 512, &qmi_wwan_cdc_wdm_manage_power);
+	subdriver = usb_cdc_wdm_register(info->control, &dev->status->desc, 4096, &qmi_wwan_cdc_wdm_manage_power);
 	if (IS_ERR(subdriver)) {
 		dev_err(&info->control->dev, "subdriver registration failed\n");
 		rv = PTR_ERR(subdriver);
