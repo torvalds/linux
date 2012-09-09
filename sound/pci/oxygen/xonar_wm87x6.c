4492363251235c4499a2d073c5f09121ea23d39d
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2012-09-09 05:14:15 -0400

    ALSA: virtuoso: Xonar DSX support
    This patch adds support for ASUS - Xonar DSX sound cards. Tested on
    openSUSE 12.2 with kernel:
    Linux 3.4.6-2.10-desktop #1 SMP PREEMPT Thu Jul 26 09:36:26 UTC 2012 (641c197) x86_64 x86_64 x86_64 GNU/Linux
    Works:
     - play sounds
     - adjust volume on master channel.
     - mute .
    
    Since Xonar DS uses the same chip, everything that works for DS should
    work for DSX as well.
    
    Signed-off-by: Sergiu Giurgiu <sgiurgiu11@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/oxygen/xonar_wm87x6.c
+++ sound/pci/oxygen/xonar_wm87x6.c
@@ -1255,7 +1255,6 @@ static void dump_wm87x6_registers(struct oxygen *chip,
 }
 
 static const struct oxygen_model model_xonar_ds = {
-	.shortname = "Xonar DS",
 	.longname = "Asus Virtuoso 66",
 	.chip = "AV200",
 	.init = xonar_ds_init,
@@ -1327,6 +1326,11 @@ int __devinit get_xonar_wm87x6_model(struct oxygen *chip,
 	switch (id->subdevice) {
 	case 0x838e:
 		chip->model = model_xonar_ds;
+		chip->model.shortname = "Xonar DS";
+		break;
+	case 0x8522:
+		chip->model = model_xonar_ds;
+		chip->model.shortname = "Xonar DSX";
 		break;
 	case 0x835e:
 		chip->model = model_xonar_hdav_slim;
