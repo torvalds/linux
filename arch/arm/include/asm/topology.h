e1103d96f28905cb0143dd289d2bc8605bebfe8a
Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
2012-09-10 16:06:30 +0100

    ARM: kernel: provide cluster to logical cpu mask mapping API
    Some device drivers like PMU require to retrieve the logical cpu mask
    that corresponds to a given cluster id. This patch provides a hook in
    the topology code that, given an existing cluster id as input,
    initializes the corresponding cpumask passed as a pointer, reusing all
    existing topology information required by sched domains in the kernel.
    
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>

--- arch/arm/include/asm/topology.h
+++ arch/arm/include/asm/topology.h
@@ -26,11 +26,14 @@ extern struct cputopo_arm cpu_topology[NR_CPUS];
 void init_cpu_topology(void);
 void store_cpu_topology(unsigned int cpuid);
 const struct cpumask *cpu_coregroup_mask(int cpu);
+int cluster_to_logical_mask(unsigned int socket_id, cpumask_t *cluster_mask);
 
 #else
 
 static inline void init_cpu_topology(void) { }
 static inline void store_cpu_topology(unsigned int cpuid) { }
+static inline int cluster_to_logical_mask(unsigned int socket_id,
+	cpumask_t *cluster_mask) { return -EINVAL; }
 
 #endif
 
