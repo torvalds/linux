1c463e57b32e3b6a3250fe75d1d56cb598d664e6
Randy Dunlap <rdunlap@xenotime.net>
2012-09-10 09:13:07 -0700

    net: fix net/core/sock.c build error
    Fix net/core/sock.c build error when CONFIG_INET is not enabled:
    
    net/built-in.o: In function `sock_edemux':
    (.text+0xd396): undefined reference to `inet_twsk_put'
    
    Signed-off-by: Randy Dunlap <rdunlap@xenotime.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/sock.c
+++ net/core/sock.c
@@ -1525,9 +1525,11 @@ void sock_edemux(struct sk_buff *skb)
 {
 	struct sock *sk = skb->sk;
 
+#ifdef CONFIG_INET
 	if (sk->sk_state == TCP_TIME_WAIT)
 		inet_twsk_put(inet_twsk(sk));
 	else
+#endif
 		sock_put(sk);
 }
 EXPORT_SYMBOL(sock_edemux);
