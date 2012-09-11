765a1939a364d121e70b452c3df34c2af446f47a
Eduardo Valentin <eduardo.valentin@ti.com>
2012-09-11 19:06:54 +0300

    staging: omap-thermal: fix polling period settings
    While registering the omap thermal zones we need to
    properly specify TC1 and TC2, as long as the proper
    passive polling period and monitor period.
    
    This patch fixes the parameters passed while registering
    the thermal zone.
    
    Signed-off-by: Eduardo Valentin <eduardo.valentin@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/omap-thermal/omap-thermal-common.c
+++ drivers/staging/omap-thermal/omap-thermal-common.c
@@ -246,7 +246,9 @@ int omap_thermal_expose_sensor(struct omap_bandgap *bg_ptr, int id,
 	/* Create thermal zone */
 	data->omap_thermal = thermal_zone_device_register(domain,
 				OMAP_TRIP_NUMBER, 0, data, &omap_thermal_ops,
-				0, FAST_TEMP_MONITORING_RATE, 0, 0);
+				1, 2, /*TODO: remove this when FW allows */
+				FAST_TEMP_MONITORING_RATE,
+				FAST_TEMP_MONITORING_RATE);
 	if (IS_ERR_OR_NULL(data->omap_thermal)) {
 		dev_err(bg_ptr->dev, "thermal zone device is NULL\n");
 		return PTR_ERR(data->omap_thermal);
