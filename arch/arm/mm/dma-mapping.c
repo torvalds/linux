75c5971614932ca53009cbbdfd6c8f96eab9e46f
Hiroshi Doyu <hdoyu@nvidia.com>
2012-09-11 07:39:48 +0200

    ARM: dma-mapping: Refrain noisy console message
    With many IOMMU'able devices, console gets noisy.
    
    Tegra30 has a few dozen of IOMMU'able devices.
    
    Signed-off-by: Hiroshi Doyu <hdoyu@nvidia.com>
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -1666,7 +1666,7 @@ int arm_iommu_attach_device(struct device *dev,
 	dev->archdata.mapping = mapping;
 	set_dma_ops(dev, &iommu_ops);
 
-	pr_info("Attached IOMMU controller to %s device.\n", dev_name(dev));
+	pr_debug("Attached IOMMU controller to %s device.\n", dev_name(dev));
 	return 0;
 }
 
