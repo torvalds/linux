048fa2df92c325cb4425dccd66c8922c8342b507
Catalin Marinas <catalin.marinas@arm.com>
2012-09-12 22:00:53 +0100

    generic: Implement generic ffs/fls using __builtin_* functions
    This patch implements ffs, __ffs, fls, __fls using __builtin_* gcc
    functions. These header files can be used by other architectures that
    rely on the gcc builtins.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

---
+++ include/asm-generic/bitops/builtin-__fls.h
@@ -0,0 +1,15 @@
+#ifndef _ASM_GENERIC_BITOPS_BUILTIN___FLS_H_
+#define _ASM_GENERIC_BITOPS_BUILTIN___FLS_H_
+
+/**
+ * __fls - find last (most-significant) set bit in a long word
+ * @word: the word to search
+ *
+ * Undefined if no set bit exists, so code should check against 0 first.
+ */
+static __always_inline unsigned long __fls(unsigned long word)
+{
+	return (sizeof(word) * 8) - 1 - __builtin_clzl(word);
+}
+
+#endif
