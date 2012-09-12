d530d6df96ee28902486f8e11815ef9ad3a1cd1b
Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
2012-09-12 20:32:25 +0000

    netprio_cgroup: Remove update_netdev_tables() since it is unnecessary
    The update_netdev_tables() function appears to be unnecessary, since the
    write_update_netdev_table() function will adjust the priomaps as and when
    required anyway. So drop the usage of update_netdev_tables() entirely.
    
    Signed-off-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/netprio_cgroup.c
+++ net/core/netprio_cgroup.c
@@ -109,32 +109,6 @@ static int write_update_netdev_table(struct net_device *dev)
 	return ret;
 }
 
-static int update_netdev_tables(void)
-{
-	int ret = 0;
-	struct net_device *dev;
-	u32 max_len;
-	struct netprio_map *map;
-
-	rtnl_lock();
-	max_len = atomic_read(&max_prioidx) + 1;
-	for_each_netdev(&init_net, dev) {
-		map = rtnl_dereference(dev->priomap);
-		/*
-		 * don't allocate priomap if we didn't
-		 * change net_prio.ifpriomap (map == NULL),
-		 * this will speed up skb_update_prio.
-		 */
-		if (map && map->priomap_len < max_len) {
-			ret = extend_netdev_table(dev, max_len);
-			if (ret < 0)
-				break;
-		}
-	}
-	rtnl_unlock();
-	return ret;
-}
-
 static struct cgroup_subsys_state *cgrp_create(struct cgroup *cgrp)
 {
 	struct cgroup_netprio_state *cs;
@@ -150,12 +124,6 @@ static struct cgroup_subsys_state *cgrp_create(struct cgroup *cgrp)
 	ret = get_prioidx(&cs->prioidx);
 	if (ret < 0) {
 		pr_warn("No space in priority index array\n");
-		goto out;
-	}
-
-	ret = update_netdev_tables();
-	if (ret < 0) {
-		put_prioidx(cs->prioidx);
 		goto out;
 	}
 
