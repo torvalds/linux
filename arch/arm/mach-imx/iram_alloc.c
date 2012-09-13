c045e3f137362fa34b337cdca66d3f6000a3b094
Shawn Guo <shawn.guo@linaro.org>
2012-09-13 21:22:55 +0800

    ARM: imx: include iram.h rather than mach/iram.h
    Rename mach-imx/include/mach/iram.h to mach-imx/iram.h, and update users
    to include iram.h rather than mach/iram.h.
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>
    Acked-by: Sascha Hauer <s.hauer@pengutronix.de>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-imx/iram_alloc.c
+++ arch/arm/mach-imx/iram_alloc.c
@@ -22,7 +22,8 @@
 #include <linux/module.h>
 #include <linux/spinlock.h>
 #include <linux/genalloc.h>
-#include <mach/iram.h>
+
+#include "iram.h"
 
 static unsigned long iram_phys_base;
 static void __iomem *iram_virt_base;
