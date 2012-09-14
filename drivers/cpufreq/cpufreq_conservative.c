2d8fced75cfa8c513ba1cbe682cb30207d941f2b
Michal Pecio <mpecio@nvidia.com>
2012-09-14 21:07:48 +0200

    cpufreq: conservative: update frequency when limits are relaxed
    Reevaluate CPU load and update frequency immediately whenever limits
    are changed. Currently conservative doesn't do that when limits are
    relaxed, wasting power on systems with relatively low sampling rate.
    
    Signed-off-by: Michal Pecio <mpecio@nvidia.com>
    Reviewed-by: Thomas Renninger <trenn@suse.de>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- drivers/cpufreq/cpufreq_conservative.c
+++ drivers/cpufreq/cpufreq_conservative.c
@@ -584,6 +584,7 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			__cpufreq_driver_target(
 					this_dbs_info->cur_policy,
 					policy->min, CPUFREQ_RELATION_L);
+		dbs_check_cpu(this_dbs_info);
 		mutex_unlock(&this_dbs_info->timer_mutex);
 
 		break;
