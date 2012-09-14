db023ea595015058270be6a62fe60a7b6b5c50d7
Oleg Nesterov <oleg@redhat.com>
2012-09-14 19:05:46 +0200

    uprobes: Move clear_thread_flag(TIF_UPROBE) to uprobe_notify_resume()
    Move clear_thread_flag(TIF_UPROBE) from do_notify_resume() to
    uprobe_notify_resume() for !CONFIG_UPROBES case.
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Acked-by: Srikar Dronamraju <srikar@linux.vnet.ibm.com>

--- kernel/events/uprobes.c
+++ kernel/events/uprobes.c
@@ -1558,6 +1558,8 @@ void uprobe_notify_resume(struct pt_regs *regs)
 {
 	struct uprobe_task *utask;
 
+	clear_thread_flag(TIF_UPROBE);
+
 	utask = current->utask;
 	if (utask && utask->active_uprobe)
 		handle_singlestep(utask, regs);
