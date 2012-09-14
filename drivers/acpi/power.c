f25b70613c048ceb1df052576fda03321ebf41cf
Aaron Lu <aaron.lu@intel.com>
2012-09-14 20:54:44 +0200

    ACPI / PM: Use KERN_DEBUG when no power resources are found
    commit a606dac368eed5696fb38e16b1394f1d049c09e9 adds support to link
    devices which have _PRx, if a device does not have _PRx, a warning
    message will be printed.
    
    This commit is for ZPODD on Intel ZPODD capable platforms, on other
    platforms, it has no problem if there is no power resource for this
    device, so a warning here is not appropriate, change it to debug.
    
    Reported-by: Borislav Petkov <bp@amd64.org>
    Signed-off-by: Aaron Lu <aaron.lu@intel.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- drivers/acpi/power.c
+++ drivers/acpi/power.c
@@ -473,7 +473,7 @@ int acpi_power_resource_register_device(struct device *dev, acpi_handle handle)
 	return ret;
 
 no_power_resource:
-	printk(KERN_WARNING PREFIX "Invalid Power Resource to register!");
+	printk(KERN_DEBUG PREFIX "Invalid Power Resource to register!");
 	return -ENODEV;
 }
 EXPORT_SYMBOL_GPL(acpi_power_resource_register_device);
