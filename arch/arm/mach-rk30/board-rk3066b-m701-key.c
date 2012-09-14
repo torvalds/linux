d696603f366a6e56b98b1d1fafe162d451dede07
hxy <hxy@rock-chips.com>
2012-09-14 16:28:19 +0800

    rk3066b:adjust key value for M701

--- arch/arm/mach-rk30/board-rk3066b-m701-key.c
+++ arch/arm/mach-rk30/board-rk3066b-m701-key.c
@@ -11,8 +11,7 @@ static struct rk29_keys_button key_button[] = {
 	{
 		.desc	= "vol-",
 		.code	= KEY_VOLUMEDOWN,
-		.adc_value	= 1,
-		.gpio = INVALID_GPIO,
+		.gpio = RK30_PIN0_PB5,
 		.active_low = PRESS_LEV_LOW,
 	},
 	{
@@ -25,35 +24,7 @@ static struct rk29_keys_button key_button[] = {
 	{
 		.desc	= "vol+",
 		.code	= KEY_VOLUMEUP,
-		.adc_value	= 630,
-		.gpio = INVALID_GPIO,
-		.active_low = PRESS_LEV_LOW,
-	},
-	{
-		.desc	= "menu",
-		.code	= EV_MENU,
-		.adc_value	= 155,
-		.gpio = INVALID_GPIO,
-		.active_low = PRESS_LEV_LOW,
-	},
-	{
-		.desc	= "home",
-		.code	= KEY_HOME,
-		.adc_value	= 550,
-		.gpio = INVALID_GPIO,
-		.active_low = PRESS_LEV_LOW,
-	},
-	{
-		.desc	= "esc",
-		.code	= KEY_BACK,
-		.adc_value	= 386,
-		.gpio = INVALID_GPIO,
-		.active_low = PRESS_LEV_LOW,
-	},
-	{
-		.desc	= "camera",
-		.code	= KEY_CAMERA,
-		.adc_value	= 827,
+		.adc_value	= 1,
 		.gpio = INVALID_GPIO,
 		.active_low = PRESS_LEV_LOW,
 	},
