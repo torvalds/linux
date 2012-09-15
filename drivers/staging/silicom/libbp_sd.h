f8402b6311db6d2789e1d08af1cc0cd4953c404b
Daniel Cotey <puff65537@bansheeslibrary.com>
2012-09-15 06:09:00 -0700

    Staging: silicom: checkpatch cleanup: header file whitespace
    Whack all the line ending spaces so checkpatch.pl is happy.
    
    Signed-off-by: Daniel Cotey <puff65537@bansheeslibrary.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/silicom/libbp_sd.h
+++ drivers/staging/silicom/libbp_sd.h
@@ -118,12 +118,12 @@ int get_bypass_slave_sd(int if_index);
  *
  *13	WD_CTL_CAP	            The interface has watchdog capabilities to turn to Bypass mode when not reset
  *                              for defined period of time.
- * 
+ *
  *14	WD_STATUS_CAP	        The interface can report on the watchdog status (Active/inactive)
- * 
+ *
  *15	WD_TIMEOUT_CAP	        The interface can report the time left till watchdog triggers to Bypass mode.
- * 
- *16-31 RESERVED	
+ *
+ *16-31 RESERVED
  *
  * **/
 int get_bypass_caps_sd(int if_index);
@@ -133,34 +133,34 @@ int get_bypass_caps_sd(int if_index);
  * @if_index: network device index
  *
  * Output:
- * 
- * Set of numbers defining the various parameters of the watchdog capable 
+ *
+ * Set of numbers defining the various parameters of the watchdog capable
  * to be set to as described bellow.
  * -1 - on failure (device not support Bypass or it's a slave device)
- * 
+ *
  * Bit	feature	        description
- * 
+ *
  * 0-3	WD_MIN_TIME	    The interface WD minimal time period  in 100mS units
- * 
- * 4	WD_STEP_TIME	The steps of the WD timer in 
+ *
+ * 4	WD_STEP_TIME	The steps of the WD timer in
  *                      0 - for linear steps (WD_MIN_TIME * X)
  *                      1 - for multiply by 2 from previous step (WD_MIN_TIME * 2^X)
- * 
- * 5-8	WD_STEP_COUNT	Number of steps the WD timer supports in 2^X 
+ *
+ * 5-8	WD_STEP_COUNT	Number of steps the WD timer supports in 2^X
  *                      (X bit available for defining the value)
- * 
- * 
- * 
+ *
+ *
+ *
  **/
 int get_wd_set_caps_sd(int if_index);
 
 /**
  * set_bypass - set Bypass state
  * @if_index: network device index of the controlling device
- * @bypass_mode:  bypass mode (1=on, 0=off) 
+ * @bypass_mode:  bypass mode (1=on, 0=off)
  * Output:
  *  0 - on success
- * -1 - on failure (device not support Bypass or it's a slave device) 
+ * -1 - on failure (device not support Bypass or it's a slave device)
  **/
 int set_bypass_sd(int if_index, int bypass_mode);
 
@@ -169,7 +169,7 @@ int set_bypass_sd(int if_index, int bypass_mode);
  * @if_index: network device index of the controlling device
  * Output:
  *  0/1 - (off/on) on success
- * -1 - on failure (device not support Bypass or it's a slave device) 
+ * -1 - on failure (device not support Bypass or it's a slave device)
  **/
 int get_bypass_sd(int if_index);
 
@@ -178,7 +178,7 @@ int get_bypass_sd(int if_index);
  * @if_index: network device index of the controlling device
  * Output:
  *  0/1 - (off/on) on success
- * -1 - on failure (device not support Bypass or it's a slave device) 
+ * -1 - on failure (device not support Bypass or it's a slave device)
  **/
 int get_bypass_change_sd(int if_index);
 
@@ -188,8 +188,8 @@ int get_bypass_change_sd(int if_index);
  * @dis_bypass: disable bypass(1=dis, 0=en)
  * Output:
  *  0 - on success
- * -1 - on failure (device is not capable of the operation ordevice not support Bypass 
- *                  or it's a slave device) 
+ * -1 - on failure (device is not capable of the operation ordevice not support Bypass
+ *                  or it's a slave device)
  **/
 int set_dis_bypass_sd(int if_index, int dis_bypass);
 
@@ -198,8 +198,8 @@ int set_dis_bypass_sd(int if_index, int dis_bypass);
  * @if_index: network device index of the controlling device
  * Output:
  *  0/1 - on success (normal Bypass mode/ Disable bypass)
- * -1 - on failure (device is not capable of the operation ordevice not support Bypass 
- *                  or it's a slave device) 
+ * -1 - on failure (device is not capable of the operation ordevice not support Bypass
+ *                  or it's a slave device)
  **/
 int get_dis_bypass_sd(int if_index);
 
@@ -208,9 +208,9 @@ int get_dis_bypass_sd(int if_index);
  * @if_index: network device index of the controlling device
  * @bypass_mode: bypass mode setting at power off state (1=BP en, 0=BP Dis)
  * Output:
- *  0 - on success 
- * -1 - on failure (device is not capable of the operation ordevice not support Bypass 
- *                  or it's a slave device) 
+ *  0 - on success
+ * -1 - on failure (device is not capable of the operation ordevice not support Bypass
+ *                  or it's a slave device)
  **/
 int set_bypass_pwoff_sd(int if_index, int bypass_mode);
 
@@ -219,8 +219,8 @@ int set_bypass_pwoff_sd(int if_index, int bypass_mode);
  * @if_index: network device index of the controlling device
  * Output:
  *  0/1 - on success (Disable bypass at power off state / normal Bypass mode)
- * -1 - on failure (device is not capable of the operation ordevice not support Bypass 
- *                  or it's a slave device) 
+ * -1 - on failure (device is not capable of the operation ordevice not support Bypass
+ *                  or it's a slave device)
  **/
 int get_bypass_pwoff_sd(int if_index);
 
@@ -229,9 +229,9 @@ int get_bypass_pwoff_sd(int if_index);
  * @if_index: network device index of the controlling device
  * @bypass_mode: bypass mode setting at power up state (1=BP en, 0=BP Dis)
  * Output:
- *  0 - on success 
- * -1 - on failure (device is not capable of the operation ordevice not support Bypass 
- *                  or it's a slave device) 
+ *  0 - on success
+ * -1 - on failure (device is not capable of the operation ordevice not support Bypass
+ *                  or it's a slave device)
  **/
 int set_bypass_pwup_sd(int if_index, int bypass_mode);
 
@@ -240,8 +240,8 @@ int set_bypass_pwup_sd(int if_index, int bypass_mode);
  * @if_index: network device index of the controlling device
  * Output:
  *  0/1 - on success (Disable bypass at power up state / normal Bypass mode)
- * -1 - on failure (device is not capable of the operation ordevice not support Bypass 
- *                  or it's a slave device) 
+ * -1 - on failure (device is not capable of the operation ordevice not support Bypass
+ *                  or it's a slave device)
  **/
 int get_bypass_pwup_sd(int if_index);
 
@@ -249,50 +249,50 @@ int get_bypass_pwup_sd(int if_index);
  * set_bypass_wd - Set watchdog state
  * @if_index: network device index of the controlling device
  * @ms_timeout: requested timeout (in ms units), 0 for disabling the watchdog timer
- * @ms_timeout_set(output): requested timeout (in ms units), 
+ * @ms_timeout_set(output): requested timeout (in ms units),
  *                          that the adapter supports and will be used by the watchdog
  * Output:
- * 0  - on success 
- * -1 - on failure (device is not capable of the operation ordevice not support Bypass 
- *                  or it's a slave device) 
+ * 0  - on success
+ * -1 - on failure (device is not capable of the operation ordevice not support Bypass
+ *                  or it's a slave device)
  **/
 int set_bypass_wd_sd(int if_index, int ms_timeout, int *ms_timeout_set);
 
 /**
  * get_bypass_wd - Get watchdog state
  * @if_index: network device index of the controlling device
- * @ms_timeout (output): WDT timeout (in ms units), 
+ * @ms_timeout (output): WDT timeout (in ms units),
  *                       -1 for unknown wdt status
  *                        0 if WDT is disabled
  * Output:
  * 0  - on success
- * -1 - on failure (device is not capable of the operation ordevice not support Bypass 
- *                  or it's a slave device) 
+ * -1 - on failure (device is not capable of the operation ordevice not support Bypass
+ *                  or it's a slave device)
  **/
 int get_bypass_wd_sd(int if_index, int *ms_timeout_set);
 
 /**
  * get_wd_expire_time - Get watchdog expire
  * @if_index: network device index of the controlling device
- * @ms_time_left (output): time left till watchdog time expire, 
+ * @ms_time_left (output): time left till watchdog time expire,
  *                       -1 if WDT has expired
  *                       0  if WDT is disabled
  * Output:
  * 0  - on success
- * -1 - on failure (device is not capable of the operation ordevice not support Bypass 
- *                  or it's a slave device or unknown wdt status) 
+ * -1 - on failure (device is not capable of the operation ordevice not support Bypass
+ *                  or it's a slave device or unknown wdt status)
  **/
 int get_wd_expire_time_sd(int if_index, int *ms_time_left);
 
 /**
  * reset_bypass_wd_timer - Reset watchdog timer
  * @if_index: network device index of the controlling device
- * 
+ *
  * Output:
  * 1  - on success
  * 0 - watchdog is not configured
- * -1 - on failure (device is not capable of the operation ordevice not support Bypass 
- *                  or it's a slave device or unknown wdt status) 
+ * -1 - on failure (device is not capable of the operation ordevice not support Bypass
+ *                  or it's a slave device or unknown wdt status)
  **/
 int reset_bypass_wd_timer_sd(int if_index);
 
@@ -300,64 +300,64 @@ int reset_bypass_wd_timer_sd(int if_index);
  * set_std_nic - Standard NIC mode of operation
  * @if_index: network device index of the controlling device
  * @nic_mode: 0/1 (Default Bypass mode / Standard NIC mode)
- * 
+ *
  * Output:
  * 0  - on success
- * -1 - on failure (device is not capable of the operation ordevice not support Bypass 
- *                  or it's a slave device) 
+ * -1 - on failure (device is not capable of the operation ordevice not support Bypass
+ *                  or it's a slave device)
  **/
 int set_std_nic_sd(int if_index, int nic_mode);
 
 /**
  * get_std_nic - Get Standard NIC mode setting
  * @if_index: network device index of the controlling device
- * 
+ *
  * Output:
  * 0/1 (Default Bypass mode / Standard NIC mode) on success
- * -1 - on failure (device is not capable of the operation ordevice not support Bypass 
- *                  or it's a slave device) 
+ * -1 - on failure (device is not capable of the operation ordevice not support Bypass
+ *                  or it's a slave device)
  **/
 int get_std_nic_sd(int if_index);
 
 /**
- * set_tx - set transmitter enable/disable 
+ * set_tx - set transmitter enable/disable
  * @if_index: network device index of the controlling device
  * @tx_state: 0/1 (Transmit Disable / Transmit Enable)
- * 
+ *
  * Output:
  * 0  - on success
- * -1 - on failure (device is not capable of the operation ) 
+ * -1 - on failure (device is not capable of the operation )
  **/
 int set_tx_sd(int if_index, int tx_state);
 
 /**
  * get_tx - get transmitter state (disable / enable)
  * @if_index: network device index of the controlling device
- * 
+ *
  * Output:
  * 0/1 (ransmit Disable / Transmit Enable) on success
- * -1 - on failure (device is not capable of the operation ordevice not support Bypass) 
+ * -1 - on failure (device is not capable of the operation ordevice not support Bypass)
  **/
 int get_tx_sd(int if_index);
 
 /**
- * set_tpl - set TPL enable/disable 
+ * set_tpl - set TPL enable/disable
  * @if_index: network device index of the controlling device
  * @tx_state: 0/1 (TPL Disable / TPL Enable)
- * 
+ *
  * Output:
  * 0  - on success
- * -1 - on failure (device is not capable of the operation ) 
+ * -1 - on failure (device is not capable of the operation )
  **/
 int set_tpl_sd(int if_index, int tpl_state);
 
 /**
  * get_tpl - get TPL state (disable / enable)
  * @if_index: network device index of the controlling device
- * 
+ *
  * Output:
  * 0/1 (TPL Disable / TPL Enable) on success
- * -1 - on failure (device is not capable of the operation) 
+ * -1 - on failure (device is not capable of the operation)
  **/
 int get_tpl_sd(int if_index);
 
@@ -368,10 +368,10 @@ int set_bp_hw_reset_sd(int if_index, int status);
 /**
  * set_tap - set TAP state
  * @if_index: network device index of the controlling device
- * @tap_mode: 1 tap mode , 0 normal nic mode 
+ * @tap_mode: 1 tap mode , 0 normal nic mode
  * Output:
  *  0 - on success
- * -1 - on failure (device not support TAP or it's a slave device) 
+ * -1 - on failure (device not support TAP or it's a slave device)
  **/
 int set_tap_sd(int if_index, int tap_mode);
 
@@ -380,7 +380,7 @@ int set_tap_sd(int if_index, int tap_mode);
  * @if_index: network device index of the controlling device
  * Output:
  *  0/1 - (off/on) on success
- * -1 - on failure (device not support TAP or it's a slave device) 
+ * -1 - on failure (device not support TAP or it's a slave device)
  **/
 int get_tap_sd(int if_index);
 
@@ -389,7 +389,7 @@ int get_tap_sd(int if_index);
  * @if_index: network device index of the controlling device
  * Output:
  *  0/1 - (off/on) on success
- * -1 - on failure (device not support TAP or it's a slave device) 
+ * -1 - on failure (device not support TAP or it's a slave device)
  **/
 int get_tap_change_sd(int if_index);
 
@@ -399,8 +399,8 @@ int get_tap_change_sd(int if_index);
  * @dis_tap: disable tap(1=dis, 0=en)
  * Output:
  *  0 - on success
- * -1 - on failure (device is not capable of the operation ordevice not support TAP 
- *                  or it's a slave device) 
+ * -1 - on failure (device is not capable of the operation ordevice not support TAP
+ *                  or it's a slave device)
  **/
 int set_dis_tap_sd(int if_index, int dis_tap);
 
@@ -409,8 +409,8 @@ int set_dis_tap_sd(int if_index, int dis_tap);
  * @if_index: network device index of the controlling device
  * Output:
  *  0/1 - on success (normal TAP mode/ Disable TAP)
- * -1 - on failure (device is not capable of the operation ordevice not support TAP 
- *                  or it's a slave device) 
+ * -1 - on failure (device is not capable of the operation ordevice not support TAP
+ *                  or it's a slave device)
  **/
 int get_dis_tap_sd(int if_index);
 
@@ -419,7 +419,7 @@ int get_dis_tap_sd(int if_index);
  * @if_index: network device index of the controlling device
  * @bypass_mode: tap mode setting at power up state (1=TAP en, 0=TAP Dis)
  * Output:
- *  0 - on success 
+ *  0 - on success
  * -1 - on failure (device is not capable of the operation ordevice not support TAP 
  *                  or it's a slave device) 
  **/
