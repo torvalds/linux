533e6903bea0440816a0f517b0845ccea4cc7917
Al Viro <viro@zeniv.linux.org.uk>
2012-09-16 12:05:09 -0400

    m68k: split ret_from_fork(), simplify kernel_thread()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/m68k/include/asm/processor.h
+++ arch/m68k/include/asm/processor.h
@@ -154,8 +154,6 @@ static inline void release_thread(struct task_struct *dead_task)
 {
 }
 
-extern int kernel_thread(int (*fn)(void *), void * arg, unsigned long flags);
-
 /*
  * Free current thread data structures etc..
  */
