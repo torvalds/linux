78a320542e6cdb2800cd736b2d136e4261d34f43
Oleg Nesterov <oleg@redhat.com>
2012-09-16 19:07:41 +0200

    uprobes: Change valid_vma() to demand VM_MAYEXEC rather than VM_EXEC
    uprobe_register() or uprobe_mmap() requires VM_READ | VM_EXEC, this
    is not right. An apllication can do mprotect(PROT_EXEC) later and
    execute this code.
    
    Change valid_vma(is_register => true) to check VM_MAYEXEC instead.
    No need to check VM_MAYREAD, it is always set.
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Acked-by: Srikar Dronamraju <srikar@linux.vnet.ibm.com>

--- kernel/events/uprobes.c
+++ kernel/events/uprobes.c
@@ -106,8 +106,8 @@ static bool valid_vma(struct vm_area_struct *vma, bool is_register)
 	if (!is_register)
 		return true;
 
-	if ((vma->vm_flags & (VM_HUGETLB|VM_READ|VM_WRITE|VM_EXEC|VM_SHARED))
-				== (VM_READ|VM_EXEC))
+	if ((vma->vm_flags & (VM_HUGETLB | VM_WRITE | VM_MAYEXEC | VM_SHARED))
+				== VM_MAYEXEC)
 		return true;
 
 	return false;
