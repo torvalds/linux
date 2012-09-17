2ca9bc2e69b66f98ff41c06700333cb544f91211
H Hartley Sweeten <hartleys@visionengravers.com>
2012-09-17 13:14:08 -0700

    staging: comedi: jr3_pci: local functions should not be exported
    The function read_idm_word() is only referenced in this file.
    Make it static.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/jr3_pci.c
+++ drivers/staging/comedi/drivers/jr3_pci.c
@@ -362,7 +362,8 @@ static int jr3_pci_open(struct comedi_device *dev)
 	return 0;
 }
 
-int read_idm_word(const u8 * data, size_t size, int *pos, unsigned int *val)
+static int read_idm_word(const u8 *data, size_t size, int *pos,
+			 unsigned int *val)
 {
 	int result = 0;
 	if (pos != 0 && val != 0) {
