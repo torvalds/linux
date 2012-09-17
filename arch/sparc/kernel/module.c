dfa520af2c20aa3c75f0497d49165561212a1c6f
Andreas Larsson <andreas@gaisler.com>
2012-09-17 19:59:52 +0000

    sparc32: Enable the relocation target R_SPARC_DISP32 for sparc32
    GNU Binutils 2.20.1 generates .eh_frame sections that uses R_SPARC_DISP32.
    
    Signed-off-by: Andreas Larsson <andreas@gaisler.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/kernel/module.c
+++ arch/sparc/kernel/module.c
@@ -116,6 +116,10 @@ int apply_relocate_add(Elf_Shdr *sechdrs,
 		v = sym->st_value + rel[i].r_addend;
 
 		switch (ELF_R_TYPE(rel[i].r_info) & 0xff) {
+		case R_SPARC_DISP32:
+			v -= (Elf_Addr) location;
+			*loc32 = v;
+			break;
 #ifdef CONFIG_SPARC64
 		case R_SPARC_64:
 			location[0] = v >> 56;
@@ -126,11 +130,6 @@ int apply_relocate_add(Elf_Shdr *sechdrs,
 			location[5] = v >> 16;
 			location[6] = v >>  8;
 			location[7] = v >>  0;
-			break;
-
-		case R_SPARC_DISP32:
-			v -= (Elf_Addr) location;
-			*loc32 = v;
 			break;
 
 		case R_SPARC_WDISP19:
