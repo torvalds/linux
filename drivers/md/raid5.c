9fd0132a29b4e40e5cf5ff82a02227b7f603bba7
NeilBrown <neilb@suse.de>
2012-09-19 12:52:30 +1000

    md/raid5: fix calculate of 'degraded' when a replacement becomes active.
    commit e5c86471f933608db5d43679f84cb4346c32033e upstream.
    
    When a replacement device becomes active, we mark the device that it
    replaces as 'faulty' so that it can subsequently get removed.
    However 'calc_degraded' only pays attention to the primary device, not
    the replacement, so the array appears to become degraded, which is
    wrong.
    
    So teach 'calc_degraded' to consider any replacement if a primary
    device is faulty.
    
    This is suitable for -stable as an incorrect 'degraded' value can
    confuse md and could lead to data corruption.
    This is only relevant for 3.3 and later.
    
    Reported-by: Robin Hill <robin@robinhill.me.uk>
    Reported-by: John Drescher <drescherjm@gmail.com>
    Signed-off-by: NeilBrown <neilb@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/md/raid5.c
+++ drivers/md/raid5.c
@@ -380,6 +380,8 @@ static int calc_degraded(struct r5conf *conf)
 	degraded = 0;
 	for (i = 0; i < conf->previous_raid_disks; i++) {
 		struct md_rdev *rdev = rcu_dereference(conf->disks[i].rdev);
+		if (rdev && test_bit(Faulty, &rdev->flags))
+			rdev = rcu_dereference(conf->disks[i].replacement);
 		if (!rdev || test_bit(Faulty, &rdev->flags))
 			degraded++;
 		else if (test_bit(In_sync, &rdev->flags))
@@ -404,6 +406,8 @@ static int calc_degraded(struct r5conf *conf)
 	degraded2 = 0;
 	for (i = 0; i < conf->raid_disks; i++) {
 		struct md_rdev *rdev = rcu_dereference(conf->disks[i].rdev);
+		if (rdev && test_bit(Faulty, &rdev->flags))
+			rdev = rcu_dereference(conf->disks[i].replacement);
 		if (!rdev || test_bit(Faulty, &rdev->flags))
 			degraded2++;
 		else if (test_bit(In_sync, &rdev->flags))
