829ce5906f6c714293d2f62165abecc24c61eba9
Tony Lindgren <tony@atomide.com>
2012-09-20 11:41:06 -0700

    ARM: OMAP2+: Make ctrl_module_wkup_44xx.h local
    This can be local to mach-omap2.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/control.h
+++ arch/arm/mach-omap2/control.h
@@ -17,7 +17,7 @@
 #define __ARCH_ARM_MACH_OMAP2_CONTROL_H
 
 #include "ctrl_module_core_44xx.h"
-#include <mach/ctrl_module_wkup_44xx.h>
+#include "ctrl_module_wkup_44xx.h"
 #include "ctrl_module_pad_core_44xx.h"
 #include "ctrl_module_pad_wkup_44xx.h"
 
