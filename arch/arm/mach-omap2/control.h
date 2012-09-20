91e80aecff77349312f032cf8792885871fa6477
Tony Lindgren <tony@atomide.com>
2012-09-20 11:40:59 -0700

    ARM: OMAP2+: Make ctrl_module_core_44xx.h local
    This can be local to mach-omap2.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/control.h
+++ arch/arm/mach-omap2/control.h
@@ -16,7 +16,7 @@
 #ifndef __ARCH_ARM_MACH_OMAP2_CONTROL_H
 #define __ARCH_ARM_MACH_OMAP2_CONTROL_H
 
-#include <mach/ctrl_module_core_44xx.h>
+#include "ctrl_module_core_44xx.h"
 #include <mach/ctrl_module_wkup_44xx.h>
 #include <mach/ctrl_module_pad_core_44xx.h>
 #include <mach/ctrl_module_pad_wkup_44xx.h>
