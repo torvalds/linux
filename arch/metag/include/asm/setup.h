85d9d7a9206631abd1030cd69ebb1c45f4a01037
James Hogan <james.hogan@imgtec.com>
2012-09-21 17:38:15 +0100

    metag: Boot
    Add boot code for metag. Due to the multi-threaded nature of Meta it is
    not uncommon for an RTOS or bare metal application to be started on
    other hardware threads by the bootloader. Since there is a single MMU
    switch which affects all threads, the MMU is traditionally configured by
    the bootloader prior to starting Linux. The bootloader passes a
    structure to Linux which among other things contains information about
    memory regions which have been mapped. Linux then assumes control of the
    local heap memory region.
    
    A kernel arguments string pointer or a flattened device tree pointer can
    be provided in the third argument.
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>

---
+++ arch/metag/include/asm/setup.h
@@ -0,0 +1,8 @@
+#ifndef _ASM_METAG_SETUP_H
+#define _ASM_METAG_SETUP_H
+
+#include <uapi/asm/setup.h>
+
+void per_cpu_trap_init(unsigned long);
+extern void __init dump_machine_table(void);
+#endif /* _ASM_METAG_SETUP_H */
