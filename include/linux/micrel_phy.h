69190e67d4fac49e2f6ea22f46780d0c712214b8
Marek Vasut <marex@denx.de>
2012-09-23 16:58:51 +0000

    phy/micrel: Add missing header to micrel_phy.h
    The license header was missing in micrel_phy.h . This patch adds
    one.
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    Cc: David J. Choi <david.choi@micrel.com>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Nobuhiro Iwamatsu <nobuhiro.iwamatsu.yj@renesas.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/micrel_phy.h
+++ include/linux/micrel_phy.h
@@ -1,3 +1,15 @@
+/*
+ * include/linux/micrel_phy.h
+ *
+ * Micrel PHY IDs
+ *
+ * This program is free software; you can redistribute  it and/or modify it
+ * under  the terms of  the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
+ *
+ */
+
 #ifndef _MICREL_PHY_H
 #define _MICREL_PHY_H
 
