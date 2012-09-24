686f871b7109e7e253a7a1cef542c00d0ed1a323
Ulf Hansson <ulf.hansson@linaro.org>
2012-09-24 16:43:17 +0200

    mfd: dbx500: Export prmcu_request_ape_opp_100_voltage
    This function needs to be exported to let clients be able to
    request the ape opp 100 voltage.
    
    Cc: Samuel Ortiz <sameo@linux.intel.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Mike Turquette <mturquette@linaro.org>

--- include/linux/mfd/dbx500-prcmu.h
+++ include/linux/mfd/dbx500-prcmu.h
@@ -336,6 +336,11 @@ static inline int prcmu_get_ape_opp(void)
 	return db8500_prcmu_get_ape_opp();
 }
 
+static inline int prcmu_request_ape_opp_100_voltage(bool enable)
+{
+	return db8500_prcmu_request_ape_opp_100_voltage(enable);
+}
+
 static inline void prcmu_system_reset(u16 reset_code)
 {
 	return db8500_prcmu_system_reset(reset_code);
@@ -505,6 +510,11 @@ static inline int prcmu_set_ape_opp(u8 opp)
 static inline int prcmu_get_ape_opp(void)
 {
 	return APE_100_OPP;
+}
+
+static inline int prcmu_request_ape_opp_100_voltage(bool enable)
+{
+	return 0;
 }
 
 static inline int prcmu_set_arm_opp(u8 opp)
