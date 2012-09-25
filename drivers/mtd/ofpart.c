ecbcbc7b75bb1b7596fca2d588d2f7539dc4e581
Sachin Kamat <sachin.kamat@linaro.org>
2012-09-25 15:27:13 +0530

    mtd: ofpart: Fix incorrect NULL check in parse_ofoldpart_partitions()
    The pointer returned by kzalloc should be tested for NULL
    to avoid potential NULL pointer dereference later. Incorrect
    pointer was being tested for NULL. Bug introduced by commit fbcf62a3
    (mtd: physmap_of: move parse_obsolete_partitions to become separate
    parser).
    This patch fixes this bug.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Cc: stable@vger.kernel.org [3.2+]
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- drivers/mtd/ofpart.c
+++ drivers/mtd/ofpart.c
@@ -121,7 +121,7 @@ static int parse_ofoldpart_partitions(struct mtd_info *master,
 	nr_parts = plen / sizeof(part[0]);
 
 	*pparts = kzalloc(nr_parts * sizeof(*(*pparts)), GFP_KERNEL);
-	if (!pparts)
+	if (!*pparts)
 		return -ENOMEM;
 
 	names = of_get_property(dp, "partition-names", &plen);
