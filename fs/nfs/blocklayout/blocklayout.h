af283885b70248268617955a5ea5476647bd556b
Peng Tao <bergwolf@gmail.com>
2012-09-25 14:55:57 +0800

    pnfsblock: cleanup nfs4_blkdev_get
    It is not needed at all and it is messing with return values...
    
    Reported-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Peng Tao <tao.peng@emc.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/blocklayout/blocklayout.h
+++ fs/nfs/blocklayout/blocklayout.h
@@ -173,7 +173,6 @@ struct bl_msg_hdr {
 /* blocklayoutdev.c */
 ssize_t bl_pipe_downcall(struct file *, const char __user *, size_t);
 void bl_pipe_destroy_msg(struct rpc_pipe_msg *);
-struct block_device *nfs4_blkdev_get(dev_t dev);
 int nfs4_blkdev_put(struct block_device *bdev);
 struct pnfs_block_dev *nfs4_blk_decode_device(struct nfs_server *server,
 						struct pnfs_device *dev);
