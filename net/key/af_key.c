f674e72ff1aad23a99c7c205473cf02c85c2ac33
Dan Carpenter <dan.carpenter@oracle.com>
2012-09-27 22:21:19 +0000

    net/key/af_key.c: add range checks on ->sadb_x_policy_len
    Because sizeof() is size_t then if "len" is negative, it counts as a
    large positive value.
    
    The call tree looks like:
    pfkey_sendmsg()
    -> pfkey_process()
       -> pfkey_spdadd()
          -> parse_ipsecrequests()
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/key/af_key.c
+++ net/key/af_key.c
@@ -1923,6 +1923,9 @@ parse_ipsecrequests(struct xfrm_policy *xp, struct sadb_x_policy *pol)
 	int len = pol->sadb_x_policy_len*8 - sizeof(struct sadb_x_policy);
 	struct sadb_x_ipsecrequest *rq = (void*)(pol+1);
 
+	if (pol->sadb_x_policy_len * 8 < sizeof(struct sadb_x_policy))
+		return -EINVAL;
+
 	while (len >= sizeof(struct sadb_x_ipsecrequest)) {
 		if ((err = parse_ipsecrequest(xp, rq)) < 0)
 			return err;
