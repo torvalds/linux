bbdd68086ca4a8976226e23efd08e2058d34dd81
Theodore Ts'o <tytso@mit.edu>
2012-09-27 09:29:33 -0400

    fs: reserve fallocate flag codepoint
    As discussed at the Plumber's Conference, reserve the bit 0x04 in
    fallocate() to prevent collisions with a commonly used out-of-tree
    patch which implements the no-hide-stale feature.
    
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- include/linux/falloc.h
+++ include/linux/falloc.h
@@ -3,6 +3,7 @@
 
 #define FALLOC_FL_KEEP_SIZE	0x01 /* default is extend size */
 #define FALLOC_FL_PUNCH_HOLE	0x02 /* de-allocates range */
+#define FALLOC_FL_NO_HIDE_STALE	0x04 /* reserved codepoint */
 
 #ifdef __KERNEL__
 
