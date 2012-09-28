d676188e44680c2f2eb114a24b3b32e56165f079
Seungwon Jeon <tgih.jun@samsung.com>
2012-09-28 14:21:59 +0900

    mmc: dw_mmc: convert the variable type of irq
    Even though platform_get_irq returns error, 'host->irq'
    always has an unsigned value. Less-than-zero comparison
    of an unsigned value is never true. Type of 'unsigned int'
    will be changed for 'int'.
    
    Signed-off-by: Seungwon Jeon <tgih.jun@samsung.com>
    Acked-by: Will Newton <will.newton@imgtec.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- include/linux/mmc/dw_mmc.h
+++ include/linux/mmc/dw_mmc.h
@@ -186,7 +186,7 @@ struct dw_mci {
 
 	struct regulator	*vmmc;	/* Power regulator */
 	unsigned long		irq_flags; /* IRQ flags */
-	unsigned int		irq;
+	int			irq;
 };
 
 /* DMA ops for Internal/External DMAC interface */
