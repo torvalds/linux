4ed5978bdd99114db7773cb3d78f2998bd17f694
Stefano Stabellini <Stefano.Stabellini@eu.citrix.com>
2012-10-03 18:08:52 +0100

    xen/xen_initial_domain: check that xen_start_info is initialized
    Since commit commit 4c071ee5268f7234c3d084b6093bebccc28cdcba ("arm:
    initial Xen support") PV on HVM guests can be xen_initial_domain.
    However PV on HVM guests might have an unitialized xen_start_info, so
    check before accessing its fields.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Acked-by: Ian Campbell <Ian.Campbell@citrix.com>
    Reported-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- include/xen/xen.h
+++ include/xen/xen.h
@@ -24,7 +24,7 @@ extern enum xen_domain_type xen_domain_type;
 #include <asm/xen/hypervisor.h>
 
 #define xen_initial_domain()	(xen_domain() && \
-				 xen_start_info->flags & SIF_INITDOMAIN)
+				 xen_start_info && xen_start_info->flags & SIF_INITDOMAIN)
 #else  /* !CONFIG_XEN_DOM0 */
 #define xen_initial_domain()	(0)
 #endif	/* CONFIG_XEN_DOM0 */
