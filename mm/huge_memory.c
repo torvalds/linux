1ba6e0b50b479cbadb8f05ebde3020da9ac87201
Andrea Arcangeli <aarcange@redhat.com>
2012-10-04 01:51:06 +0200

    mm: numa: split_huge_page: transfer the NUMA type from the pmd to the pte
    When we split a transparent hugepage, transfer the NUMA type from the
    pmd to the pte if needed.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Signed-off-by: Mel Gorman <mgorman@suse.de>
    Reviewed-by: Rik van Riel <riel@redhat.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -1364,6 +1364,8 @@ static int __split_huge_page_map(struct page *page,
 				BUG_ON(page_mapcount(page) != 1);
 			if (!pmd_young(*pmd))
 				entry = pte_mkold(entry);
+			if (pmd_numa(*pmd))
+				entry = pte_mknuma(entry);
 			pte = pte_offset_map(&_pmd, haddr);
 			BUG_ON(!pte_none(*pte));
 			set_pte_at(mm, haddr, pte, entry);
