8b09b87ff6d33d23effabc70b934cacb988a8357
张晴 <zhangqing@rock-chips.com>
2012-10-07 17:10:26 +0800

    rk2926:m713:slove wakeup failed

--- arch/arm/mach-rk2928/board-rk2928-a720.c
+++ arch/arm/mach-rk2928/board-rk2928-a720.c
@@ -732,8 +732,8 @@ void __sramfunc rk30_pwm_logic_resume_voltage(void)
 #ifdef CONFIG_RK30_PWM_REGULATOR
 #ifdef CONFIG_MACH_RK2926_M713
 	writel_relaxed((1<<22)|gpio0d3_iomux, GRF_GPIO0D_IOMUX);
-	gpio_writel(gpio0d3_dir, GPIO_SWPORTA_DDR);
-	gpio_writel(gpio0d3_do, GPIO_SWPORTA_DR);
+//	gpio_writel(gpio0d3_dir, GPIO_SWPORTA_DDR);
+//	gpio_writel(gpio0d3_do, GPIO_SWPORTA_DR);
 	sram_udelay(10000);
 #else
 	writel_relaxed((1<<24)|gpio0d4_iomux, GRF_GPIO0D_IOMUX);
