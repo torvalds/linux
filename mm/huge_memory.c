227e4047488d3ee0173a914275a7fd207ad51e5b
Xiao Guangrong <xiaoguangrong@linux.vnet.ibm.com>
2012-10-08 16:29:59 -0700

    thp: remove unnecessary set_recommended_min_free_kbytes
    Since it is called in start_khugepaged
    
    Signed-off-by: Xiao Guangrong <xiaoguangrong@linux.vnet.ibm.com>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -225,9 +225,6 @@ static ssize_t enabled_store(struct kobject *kobj,
 			ret = err;
 	}
 
-	if (ret > 0 && khugepaged_enabled())
-		set_recommended_min_free_kbytes();
-
 	return ret;
 }
 static struct kobj_attribute enabled_attr =
@@ -561,8 +558,6 @@ static int __init hugepage_init(void)
 		transparent_hugepage_flags = 0;
 
 	start_khugepaged();
-
-	set_recommended_min_free_kbytes();
 
 	return 0;
 out:
