692373e128f16da708ec6ddf80ee5b5bb3761ef9
Srinivas Kandagatla <srinivas.kandagatla@st.com>
2012-10-10 19:36:52 +0100

    usb: musb: blackfin: use module_platform_driver macro
    This patch removes some code duplication by using
    module_platform_driver.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@st.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/musb/blackfin.c
+++ drivers/usb/musb/blackfin.c
@@ -585,15 +585,4 @@ static struct platform_driver bfin_driver = {
 MODULE_DESCRIPTION("Blackfin MUSB Glue Layer");
 MODULE_AUTHOR("Bryan Wy <cooloney@kernel.org>");
 MODULE_LICENSE("GPL v2");
-
-static int __init bfin_init(void)
-{
-	return platform_driver_register(&bfin_driver);
-}
-module_init(bfin_init);
-
-static void __exit bfin_exit(void)
-{
-	platform_driver_unregister(&bfin_driver);
-}
-module_exit(bfin_exit);
+module_platform_driver(bfin_driver);
