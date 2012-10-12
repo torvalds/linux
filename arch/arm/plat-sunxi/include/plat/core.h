02bf6b90d3ab7cbcdf4ec670b751b5f326f61537
Alejandro Mery <amery@geeks.cl>
2012-10-12 02:16:40 +0200

    sunxi: move new fixup code and pr_brom to plat/

---
+++ arch/arm/plat-sunxi/include/plat/core.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2012  Alejandro Mery <amery@geeks.cl>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _SUNXI_CORE_H
+#define _SUNXI_CORE_H
+
+#define pr_reserve_info(L, START, SIZE) \
+	pr_info("\t" L " : 0x%08x - 0x%08x  (%4d %s)\n", \
+		(u32)(START), (u32)((START) + (SIZE) - 1), \
+		(u32)((SIZE) < SZ_1M ? (SIZE) / SZ_1K : (SIZE) / SZ_1M), \
+		(SIZE) < SZ_1M ? "kB" : "MB")
+
+void sunxi_pr_brom(void *base);
+
+void __init sunxi_core_fixup(struct machine_desc *desc,
+			     struct tag *t, char **cmdline,
+			     struct meminfo *mi);
+
+#endif
