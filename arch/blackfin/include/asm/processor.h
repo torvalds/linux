ee1e17c69eb3c843d283eb3fbed1616ed9a122eb
Al Viro <viro@zeniv.linux.org.uk>
2012-10-13 03:22:53 -0400

    blackfin: convert kernel_thread() and kernel_execve() to generic ones
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/blackfin/include/asm/processor.h
+++ arch/blackfin/include/asm/processor.h
@@ -75,8 +75,6 @@ static inline void release_thread(struct task_struct *dead_task)
 {
 }
 
-extern int kernel_thread(int (*fn) (void *), void *arg, unsigned long flags);
-
 /*
  * Free current thread data structures etc..
  */
