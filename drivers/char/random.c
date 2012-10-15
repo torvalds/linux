be5b779ae9ce64ede0a8f4939360b0320bb257e2
Jiri Kosina <jkosina@suse.cz>
2012-10-15 23:42:55 +0200

    random: make it possible to enable debugging without rebuild
    The module parameter that turns debugging mode (which basically means
    printing a few extra lines during runtime) is in '#if 0' block. Forcing
    everyone who would like to see how entropy is behaving on his system to
    rebuild seems to be a little bit too harsh.
    
    If we were concerned about speed, we could potentially turn 'debug' into a
    static key, but I don't think it's necessary.
    
    Drop the '#if 0' block to allow using the 'debug' parameter without rebuilding.
    
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -399,7 +399,6 @@ static DECLARE_WAIT_QUEUE_HEAD(random_read_wait);
 static DECLARE_WAIT_QUEUE_HEAD(random_write_wait);
 static struct fasync_struct *fasync;
 
-#if 0
 static bool debug;
 module_param(debug, bool, 0644);
 #define DEBUG_ENT(fmt, arg...) do { \
@@ -410,9 +409,6 @@ module_param(debug, bool, 0644);
 		blocking_pool.entropy_count,\
 		nonblocking_pool.entropy_count,\
 		## arg); } while (0)
-#else
-#define DEBUG_ENT(fmt, arg...) do {} while (0)
-#endif
 
 /**********************************************************************
  *
