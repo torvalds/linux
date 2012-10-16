177d2bf5c7d3ab41bfb4ce2597dde668225958dd
Viresh Kumar <viresh.kumar@linaro.org>
2012-10-16 09:49:16 +0530

    dmaengine: dw_dmac: Update documentation style comments for dw_dma_platform_data
    Documentation style comments were missing for few fields in struct
    dw_dma_platform_data. Add these.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@linux.intel.com>

--- include/linux/dw_dmac.h
+++ include/linux/dw_dmac.h
@@ -19,6 +19,8 @@
  * @nr_channels: Number of channels supported by hardware (max 8)
  * @is_private: The device channels should be marked as private and not for
  *	by the general purpose DMA channel allocator.
+ * @chan_allocation_order: Allocate channels starting from 0 or 7
+ * @chan_priority: Set channel priority increasing from 0 to 7 or 7 to 0.
  * @block_size: Maximum block size supported by the controller
  * @nr_masters: Number of AHB masters supported by the controller
  * @data_width: Maximum data width supported by hardware per AHB master
