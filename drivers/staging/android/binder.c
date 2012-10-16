585650dcec88e704a19bb226a34b6a7166111623
Arve Hjønnevåg <arve@android.com>
2012-10-16 15:29:55 -0700

    Staging: android: binder: Allow using highmem for binder buffers
    The default kernel mapping for the pages allocated for the binder
    buffers is never used. Set the __GFP_HIGHMEM flag when allocating
    these pages so we don't needlessly use low memory pages that may
    be required elsewhere.
    
    Signed-off-by: Arve Hjønnevåg <arve@android.com>
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -567,7 +567,7 @@ static int binder_update_page_range(struct binder_proc *proc, int allocate,
 		page = &proc->pages[(page_addr - proc->buffer) / PAGE_SIZE];
 
 		BUG_ON(*page);
-		*page = alloc_page(GFP_KERNEL | __GFP_ZERO);
+		*page = alloc_page(GFP_KERNEL | __GFP_HIGHMEM | __GFP_ZERO);
 		if (*page == NULL) {
 			pr_err("binder: %d: binder_alloc_buf failed "
 			       "for page at %p\n", proc->pid, page_addr);
