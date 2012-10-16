cf174b0ef52ad8184779e1da4132e2d9d17247e5
SeongJae Park <sj38.park@gmail.com>
2012-10-16 16:47:50 +0900

    staging: csr: csr_framework_ext_types.h: fix coding style
    Fix coding style of csr_framework_ext_types.h
        
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/csr/csr_framework_ext_types.h
+++ drivers/staging/csr/csr_framework_ext_types.h
@@ -2,11 +2,11 @@
 #define CSR_FRAMEWORK_EXT_TYPES_H__
 /*****************************************************************************
 
-            (c) Cambridge Silicon Radio Limited 2010
-            All rights reserved and confidential information of CSR
+	(c) Cambridge Silicon Radio Limited 2010
+	All rights reserved and confidential information of CSR
 
-            Refer to LICENSE.txt included with this source for details
-            on the license terms.
+	Refer to LICENSE.txt included with this source for details
+	on the license terms.
 
 *****************************************************************************/
 
@@ -24,17 +24,15 @@ extern "C" {
 
 #ifdef __KERNEL__
 
-struct CsrThread
-{
-    struct task_struct *thread_task;
-    char                name[16];
+struct CsrThread {
+	struct task_struct *thread_task;
+	char                name[16];
 };
 
-struct CsrEvent
-{
-    /* wait_queue for waking the kernel thread */
-    wait_queue_head_t wakeup_q;
-    unsigned int      wakeup_flag;
+struct CsrEvent {
+	/* wait_queue for waking the kernel thread */
+	wait_queue_head_t wakeup_q;
+	unsigned int      wakeup_flag;
 };
 
 typedef struct CsrEvent CsrEventHandle;
@@ -43,11 +41,10 @@ typedef struct CsrThread CsrThreadHandle;
 
 #else /* __KERNEL __ */
 
-struct CsrEvent
-{
-    pthread_cond_t  event;
-    pthread_mutex_t mutex;
-    u32       eventBits;
+struct CsrEvent {
+	pthread_cond_t  event;
+	pthread_mutex_t mutex;
+	u32       eventBits;
 };
 
 typedef struct CsrEvent CsrEventHandle;
