7ffba5a22183e2d2d4198b29ebffa0b340821a49
Luc Verhaegen <libv@codethink.co.uk>
2012-10-17 13:13:27 +0200

    include:linux: chmod and remove paths from sunxi disp files
    Signed-off-by: Luc Verhaegen <libv@codethink.co.uk>

--- include/linux/drv_display.h
+++ include/linux/drv_display.h
@@ -1,6 +1,4 @@
 /*
- * include/linux/drv_display.h
- *
  * (C) Copyright 2007-2012
  * Allwinner Technology Co., Ltd. <www.allwinnertech.com>
  *
--- include/linux/drv_hdmi.h
+++ include/linux/drv_hdmi.h
@@ -1,6 +1,4 @@
 /*
- * include/linux/drv_hdmi.h
- *
  * (C) Copyright 2007-2012
  * Allwinner Technology Co., Ltd. <www.allwinnertech.com>
  *
