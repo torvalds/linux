248ada294174583d918564de33bc5504311c69bb
Pritesh Raithatha <praithatha@nvidia.com>
2012-10-17 17:09:36 +0530

    pinctrl: tegra: set low power mode bank width to 2
    commit 154f3ebf53edcfbe28728452b4ab37a118581125 upstream.
    
    Signed-off-by: Pritesh Raithatha <praithatha@nvidia.com>
    Acked-by: Stephen Warren <swarren@nvidia.com>
    Tested-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/pinctrl/pinctrl-tegra.c
+++ drivers/pinctrl/pinctrl-tegra.c
@@ -259,7 +259,7 @@ static int tegra_pinconf_reg(struct tegra_pmx *pmx,
 		*bank = g->drv_bank;
 		*reg = g->drv_reg;
 		*bit = g->lpmd_bit;
-		*width = 1;
+		*width = 2;
 		break;
 	case TEGRA_PINCONF_PARAM_DRIVE_DOWN_STRENGTH:
 		*bank = g->drv_bank;
