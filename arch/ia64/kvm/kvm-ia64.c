787c57c0fb393fe8a3974d300ddcfe30373386fe
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2012-10-17 23:03:42 +0800

    KVM: ia64: remove unused variable in kvm_release_vm_pages()
    The variable base_gfn is initialized but never used
    otherwise, so remove the unused variable.
    
    dpatch engine is used to auto generate this patch.
    (https://github.com/weiyj/dpatch)
    
    Acked-by: Xiantao Zhang <xiantao.zhang@intel.com>
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/ia64/kvm/kvm-ia64.c
+++ arch/ia64/kvm/kvm-ia64.c
@@ -1362,11 +1362,9 @@ static void kvm_release_vm_pages(struct kvm *kvm)
 	struct kvm_memslots *slots;
 	struct kvm_memory_slot *memslot;
 	int j;
-	unsigned long base_gfn;
 
 	slots = kvm_memslots(kvm);
 	kvm_for_each_memslot(memslot, slots) {
-		base_gfn = memslot->base_gfn;
 		for (j = 0; j < memslot->npages; j++) {
 			if (memslot->rmap[j])
 				put_page((struct page *)memslot->rmap[j]);
