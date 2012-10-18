5d8c61bc283826827e1f06816c146bfc507d3834
Johan Hovold <jhovold@gmail.com>
2012-10-18 11:43:28 +0200

    USB: io_ti: fix sysfs-attribute creation
    Make sure port data is initialised before creating sysfs attributes to
    avoid a race.
    
    A recent patch ("USB: io_ti: fix port-data memory leak") got the
    sysfs-attribute creation and port-data initialisation ordering wrong.
    
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Johan Hovold <jhovold@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/serial/io_ti.c
+++ drivers/usb/serial/io_ti.c
@@ -2578,19 +2578,19 @@ static int edge_port_probe(struct usb_serial_port *port)
 		return -ENOMEM;
 	}
 
-	ret = edge_create_sysfs_attrs(port);
-	if (ret) {
-		kfifo_free(&edge_port->write_fifo);
-		kfree(edge_port);
-		return ret;
-	}
-
 	spin_lock_init(&edge_port->ep_lock);
 	edge_port->port = port;
 	edge_port->edge_serial = usb_get_serial_data(port->serial);
 	edge_port->bUartMode = default_uart_mode;
 
 	usb_set_serial_port_data(port, edge_port);
+
+	ret = edge_create_sysfs_attrs(port);
+	if (ret) {
+		kfifo_free(&edge_port->write_fifo);
+		kfree(edge_port);
+		return ret;
+	}
 
 	return 0;
 }
