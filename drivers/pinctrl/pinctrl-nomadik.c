38843e29217bc6b6fce80a5bf19d1636ee337c77
Linus Walleij <linus.walleij@linaro.org>
2012-10-23 11:44:42 +0200

    pinctrl/nomadik: pass DT node to the irqdomain
    When creating the simple irqdomain, pass the DT node pointer along,
    as is apropriate.
    
    Acked-by: Lee Jones <lee.jones@linaro.org>
    Reported-by: Gabriel Fernandez <gabriel.fernandez@stericsson.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-nomadik.c
+++ drivers/pinctrl/pinctrl-nomadik.c
@@ -1387,7 +1387,7 @@ static int __devinit nmk_gpio_probe(struct platform_device *dev)
 
 	if (!np)
 		irq_start = NOMADIK_GPIO_TO_IRQ(pdata->first_gpio);
-	nmk_chip->domain = irq_domain_add_simple(NULL,
+	nmk_chip->domain = irq_domain_add_simple(np,
 				NMK_GPIO_PER_CHIP, irq_start,
 				&nmk_gpio_irq_simple_ops, nmk_chip);
 	if (!nmk_chip->domain) {
