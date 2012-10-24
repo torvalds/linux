224db157431299582a65fb9d4c9228363146f8a9
Bart Van Assche <bvanassche@acm.org>
2012-10-24 15:05:37 +0200

    IB/srp: Simplify SCSI error handling
    Since scsi_remove_host() has been modified so that SCSI error handling
    functions will no longer be invoked after scsi_remove_host() returns,
    the test at the start of srp_send_tsk_mgmt() is now superfluous.
    
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Acked-by: David Dillow <dillowda@ornl.gov>
    Signed-off-by: Roland Dreier <roland@purestorage.com>

--- drivers/infiniband/ulp/srp/ib_srp.c
+++ drivers/infiniband/ulp/srp/ib_srp.c
@@ -1661,10 +1661,6 @@ static int srp_send_tsk_mgmt(struct srp_target_port *target,
 	struct srp_iu *iu;
 	struct srp_tsk_mgmt *tsk_mgmt;
 
-	if (target->state == SRP_TARGET_DEAD ||
-	    target->state == SRP_TARGET_REMOVED)
-		return -1;
-
 	init_completion(&target->tsk_mgmt_done);
 
 	spin_lock_irq(&target->lock);
