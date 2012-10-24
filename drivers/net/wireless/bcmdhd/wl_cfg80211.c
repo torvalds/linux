6bec03d6183a26e531e386d5816cf33fa96f67a0
Dmitry Shmidt <dimitrysh@google.com>
2012-10-24 10:05:23 -0700

    net: wireless: bcmdhd: Fix compilation without SUPPORT_PM2_ONLY
    Change-Id: I9a95a81ec713824b3d6bfba86f62e420523ce0e4
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/net/wireless/bcmdhd/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -3436,6 +3436,9 @@ wl_cfg80211_set_power_mgmt(struct wiphy *wiphy, struct net_device *dev,
 	s32 err = 0;
 	struct wl_priv *wl = wiphy_priv(wiphy);
 	struct net_info *_net_info = wl_get_netinfo_by_netdev(wl, dev);
+#ifndef SUPPORT_PM2_ONLY
+	dhd_pub_t *dhd = (dhd_pub_t *)(wl->pub);
+#endif
 
 	CHECK_SYS_UP(wl);
 
