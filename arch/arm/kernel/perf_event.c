2ac29a14a8b6b4a37c09c50db88dc893e6e7fc75
Jon Hunter <jon-hunter@ti.com>
2012-10-25 21:23:18 +0100

    ARM: PMU: fix runtime PM enable
    Commit 7be2958 (ARM: PMU: Add runtime PM Support) updated the ARM PMU code to
    use runtime PM which was prototyped and validated on the OMAP devices. In this
    commit, there is no call pm_runtime_enable() and for OMAP devices
    pm_runtime_enable() is currently being called from the OMAP PMU code when the
    PMU device is created. However, there are two problems with this:
    
    1. For any other ARM device wishing to use runtime PM for PMU they will need
       to call pm_runtime_enable() for runtime PM to work.
    2. When booting with device-tree and using device-tree to create the PMU
       device, pm_runtime_enable() needs to be called from within the ARM PERF
       driver as we are no longer calling any device specific code to create the
       device. Hence, PMU does not work on OMAP devices that use the runtime PM
       callbacks when using device-tree to create the PMU device.
    
    Therefore,  call pm_runtime_enable() directly from the ARM PMU driver when
    registering the device. For platforms that do not use runtime PM,
    pm_runtime_enable() does nothing and for platforms that do use runtime PM but
    may not require it specifically for PMU, this will just add a little overhead
    when initialising and uninitialising the PMU device.
    
    Tested with PERF on OMAP2420, OMAP3430 and OMAP4460.
    
    Acked-by: Kevin Hilman <khilman@ti.com>
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Jon Hunter <jon-hunter@ti.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm/kernel/perf_event.c
+++ arch/arm/kernel/perf_event.c
@@ -514,6 +514,7 @@ static void __init armpmu_init(struct arm_pmu *armpmu)
 int armpmu_register(struct arm_pmu *armpmu, int type)
 {
 	armpmu_init(armpmu);
+	pm_runtime_enable(&armpmu->plat_device->dev);
 	pr_info("enabled with %s PMU driver, %d counters available\n",
 			armpmu->name, armpmu->num_events);
 	return perf_pmu_register(&armpmu->pmu, armpmu->name, type);
