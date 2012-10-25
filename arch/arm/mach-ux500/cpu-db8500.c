f48283367fd2679906e06bdd9e886eec1ee59eb1
Jonas Aaberg <jonas.aberg@stericsson.com>
2012-10-25 08:46:53 +0200

    ARM: ux500: add PRCM register base for pinctrl
    This adds the PRCM register range base as a resource to
    the pinctrl driver do we can break the dependency to the
    PRCMU driver and handle these registers in the driver
    alone.
    
    Cc: arm@kernel.org
    Signed-off-by: Jonas Aaberg <jonas.aberg@stericsson.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/cpu-db8500.c
+++ arch/arm/mach-ux500/cpu-db8500.c
@@ -158,7 +158,7 @@ static void __init db8500_add_gpios(struct device *parent)
 
 	dbx500_add_gpios(parent, ARRAY_AND_SIZE(db8500_gpio_base),
 			 IRQ_DB8500_GPIO0, &pdata);
-	dbx500_add_pinctrl(parent, "pinctrl-db8500");
+	dbx500_add_pinctrl(parent, "pinctrl-db8500", U8500_PRCMU_BASE);
 }
 
 static int usb_db8500_rx_dma_cfg[] = {
