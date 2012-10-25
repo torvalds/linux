b40a79591ca918e7b91b0d9b6abd5d00f2e88c19
Oleg Nesterov <oleg@redhat.com>
2012-10-25 22:28:12 +0200

    freezer: exec should clear PF_NOFREEZE along with PF_KTHREAD
    flush_old_exec() clears PF_KTHREAD but forgets about PF_NOFREEZE.
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Cc: stable@vger.kernel.org
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1083,7 +1083,8 @@ int flush_old_exec(struct linux_binprm * bprm)
 	bprm->mm = NULL;		/* We're using it now */
 
 	set_fs(USER_DS);
-	current->flags &= ~(PF_RANDOMIZE | PF_FORKNOEXEC | PF_KTHREAD);
+	current->flags &=
+		~(PF_RANDOMIZE | PF_FORKNOEXEC | PF_KTHREAD | PF_NOFREEZE);
 	flush_thread();
 	current->personality &= ~bprm->per_clear;
 
