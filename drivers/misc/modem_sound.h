d8c1ed909ad400df8bccb2c9eee224d7559b4485
邱建斌 <qjb@rock-chips.com>
2012-10-25 19:00:45 +0800

    rk2928phonepad  modem_sound : 	add set volume ioctl cmd

--- drivers/misc/modem_sound.h
+++ drivers/misc/modem_sound.h
@@ -9,7 +9,13 @@
 #define IOCTL_MODEM_SPK_PHONE      	        _IO(MODEM_SOUND, 0x02) 
 #define IOCTL_MODEM_HP_PHONE      	        _IO(MODEM_SOUND, 0x03)
 #define IOCTL_MODEM_BT_PHONE      	        _IO(MODEM_SOUND, 0x04)
-#define IOCTL_MODEM_STOP_PHONE      	        _IO(MODEM_SOUND, 0x05) 
+#define IOCTL_MODEM_STOP_PHONE      	    _IO(MODEM_SOUND, 0x05) 
+
+#define IOCTL_SET_EAR_VALUME      	    _IO(MODEM_SOUND, 0x11) 
+#define IOCTL_SET_SPK_VALUME      	    _IO(MODEM_SOUND, 0x12) 
+#define IOCTL_SET_HP_VALUME      	    _IO(MODEM_SOUND, 0x13) 
+#define IOCTL_SET_BT_VALUME      	    _IO(MODEM_SOUND, 0x14) 
+ 
 
 struct modem_sound_data {
 	int spkctl_io;
