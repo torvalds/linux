3657600040a7279e52252af3f9d7e253f4f49ef0
Joonsoo Kim <js1304@gmail.com>
2012-10-26 23:03:49 +0900

    workqueue: add WARN_ON_ONCE() on CPU number to wq_worker_waking_up()
    Recently, workqueue code has gone through some changes and we found
    some bugs related to concurrency management operations happening on
    the wrong CPU.  When a worker is concurrency managed
    (!WORKER_NOT_RUNNIG), it should be bound to its associated cpu and
    woken up to that cpu.  Add WARN_ON_ONCE() to verify this.
    
    Signed-off-by: Joonsoo Kim <js1304@gmail.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -739,8 +739,10 @@ void wq_worker_waking_up(struct task_struct *task, unsigned int cpu)
 {
 	struct worker *worker = kthread_data(task);
 
-	if (!(worker->flags & WORKER_NOT_RUNNING))
+	if (!(worker->flags & WORKER_NOT_RUNNING)) {
+		WARN_ON_ONCE(worker->pool->gcwq->cpu != cpu);
 		atomic_inc(get_pool_nr_running(worker->pool));
+	}
 }
 
 /**
