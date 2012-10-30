5bcfb7c663a916c9025dc0cc95043a2ddf3edec5
Alejandro Mery <amery@geeks.cl>
2012-10-30 19:20:29 +0000

    input:ts:gt811: adapted for 3.4

--- drivers/input/touchscreen/gt811_ts.h
+++ drivers/input/touchscreen/gt811_ts.h
@@ -19,7 +19,9 @@
 #ifndef 	_LINUX_GT811_H
 #define		_LINUX_GT811_H
 
+#ifdef CONFIG_HAS_EARLYSUSPEND
 #include <linux/earlysuspend.h>
+#endif
 #include <linux/hrtimer.h>
 #include <linux/i2c.h>
 #include <linux/input.h>
@@ -123,7 +125,9 @@ struct goodix_ts_data {
        unsigned int version;
 /////////////////////////////// UPDATE STEP 6 END /////////////////////////////////////////////////////////////////
 
+#ifdef CONFIG_HAS_EARLYSUSPEND
 	struct early_suspend early_suspend;
+#endif
 	int (*power)(struct goodix_ts_data * ts, int on);
 };
 
