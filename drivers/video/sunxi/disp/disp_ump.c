554ca19c83a91fd99f062b8e93c709274f0a818f
Luc Verhaegen <libv@skynet.be>
2012-10-30 15:42:22 +0100

    video:sunxi: fix up missing license statements
    And while we are at it, beautify the copyright statements.
    
    Signed-off-by: Luc Verhaegen <libv@skynet.be>

--- drivers/video/sunxi/disp/disp_ump.c
+++ drivers/video/sunxi/disp/disp_ump.c
@@ -1,6 +1,5 @@
 /*
- * (C) Copyright 2012
- * Henrik Nordstrom <henrik@henriknordstrom.net>
+ * copyright (c) 2012 Henrik Nordstrom <henrik@henriknordstrom.net>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
