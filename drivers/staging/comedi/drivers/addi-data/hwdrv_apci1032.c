618a97e8a2fe6a36259d90560f0ea5f7976a505f
H Hartley Sweeten <hartleys@visionengravers.com>
2012-10-30 16:55:45 -0700

    staging: comedi: hwrdv_apci1032: absorb private header
    The header file hwdrv_apci1032.h is only included by the source
    file hwrdv_apci1032.c. Absorb the header into the source and
    delete the header.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/addi-data/hwdrv_apci1032.c
+++ drivers/staging/comedi/drivers/addi-data/hwdrv_apci1032.c
@@ -46,13 +46,30 @@ You should also find the complete GPL in the COPYING file accompanying this sour
   +----------+-----------+------------------------------------------------+
 */
 
-/*
-+----------------------------------------------------------------------------+
-|                               Included files                               |
-+----------------------------------------------------------------------------+
-*/
-#include "hwdrv_apci1032.h"
-#include <linux/delay.h>
+/*********      Definitions for APCI-1032 card  *****/
+
+#define APCI1032_ADDRESS_RANGE			20
+/* DIGITAL INPUT DEFINE */
+
+#define APCI1032_DIGITAL_IP			0
+#define APCI1032_DIGITAL_IP_INTERRUPT_MODE1	4
+#define APCI1032_DIGITAL_IP_INTERRUPT_MODE2	8
+#define APCI1032_DIGITAL_IP_IRQ			16
+
+/* Digital Input IRQ Function Selection */
+#define ADDIDATA_OR				0
+#define ADDIDATA_AND				1
+
+/* Digital Input Interrupt Status */
+#define APCI1032_DIGITAL_IP_INTERRUPT_STATUS	12
+
+/* Digital Input Interrupt Enable Disable. */
+#define APCI1032_DIGITAL_IP_INTERRUPT_ENABLE	0x4
+#define APCI1032_DIGITAL_IP_INTERRUPT_DISABLE	0xfffffffb
+
+/* ADDIDATA Enable Disable */
+#define ADDIDATA_ENABLE				1
+#define ADDIDATA_DISABLE			0
 
 static unsigned int ui_InterruptStatus;
 
