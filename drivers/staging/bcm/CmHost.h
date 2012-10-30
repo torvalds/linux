ac56d89aced80133ab9ca783930602c3be4e03c8
Kevin McKinney <klmckinney1@gmail.com>
2012-10-29 23:08:46 -0400

    Staging: bcm: Correctly format comments in CmHost.h
    This patch correctly formats comments
    in CmHost.h as reported by checkpatch.pl.
    
    Signed-off-by: Kevin McKinney <klmckinney1@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/CmHost.h
+++ drivers/staging/bcm/CmHost.h
@@ -1,72 +1,72 @@
-/// **************************************************************************
-/// (c) Beceem Communications Inc.
-///     All Rights Reserved
-///
-///   \file        : CmHost.h
-///   \author      : Rajeev Tirumala
-///   \date        : September 8 , 2006
-///   \brief       : Definitions for Connection Management Requests structure
-///                  which we will use to setup our connection structures.Its high
-///                  time we had a header file for CmHost.cpp to isolate the way
-///                  f/w sends DSx messages and the way we interpret them in code.
-///                              Revision History
-///
-///    Date       Author   Version   Description
-///  08-Sep-06    Rajeev       0.1      Created
-/// **************************************************************************
+/***************************************************************************
+ * (c) Beceem Communications Inc.
+ * All Rights Reserved
+ *
+ * file  : CmHost.h
+ * author: Rajeev Tirumala
+ * date  : September 8 , 2006
+ * brief : Definitions for Connection Management Requests structure
+ *          which we will use to setup our connection structures.Its high
+ *          time we had a header file for CmHost.cpp to isolate the way
+ *          f/w sends DSx messages and the way we interpret them in code.
+ *          Revision History
+ *
+ *   Date       Author   Version   Description
+ *   08-Sep-06    Rajeev       0.1      Created
+ ***************************************************************************/
 #ifndef _CM_HOST_H
 #define _CM_HOST_H
 
 #pragma once
 #pragma pack (push,4)
 
-#define  DSX_MESSAGE_EXCHANGE_BUFFER        0xBF60AC84 // This contains the pointer
-#define  DSX_MESSAGE_EXCHANGE_BUFFER_SIZE   72000 // 24 K Bytes
+#define  DSX_MESSAGE_EXCHANGE_BUFFER        0xBF60AC84 /* This contains the pointer */
+#define  DSX_MESSAGE_EXCHANGE_BUFFER_SIZE   72000 /* 24 K Bytes */
 
-/// \brief structure stLocalSFAddRequest
+/* brief structure stLocalSFAddRequest */
 typedef struct stLocalSFAddRequestAlt{
 	B_UINT8                         u8Type;
 	B_UINT8      u8Direction;
 
 	B_UINT16                        u16TID;
-   /// \brief 16bitCID
+	/* brief 16bitCID */
     B_UINT16                        u16CID;
-    /// \brief 16bitVCID
+	/* brief 16bitVCID */
     B_UINT16                        u16VCID;
 
 
 	struct bcm_connect_mgr_params sfParameterSet;
 
-    //USE_MEMORY_MANAGER();
+	/* USE_MEMORY_MANAGER(); */
 }stLocalSFAddRequestAlt;
 
-/// \brief structure stLocalSFAddIndication
+/* brief structure stLocalSFAddIndication */
 typedef struct stLocalSFAddIndicationAlt{
     B_UINT8                         u8Type;
 	B_UINT8      u8Direction;
 	B_UINT16                         u16TID;
-    /// \brief 16bitCID
+/* brief 16bitCID */
     B_UINT16                        u16CID;
-    /// \brief 16bitVCID
+	/* brief 16bitVCID */
     B_UINT16                        u16VCID;
 	struct bcm_connect_mgr_params sfAuthorizedSet;
 	struct bcm_connect_mgr_params sfAdmittedSet;
 	struct bcm_connect_mgr_params sfActiveSet;
 
-	B_UINT8 						u8CC;	/**<  Confirmation Code*/
-	B_UINT8 						u8Padd; 	/**<  8-bit Padding */
-	B_UINT16						u16Padd;	/**< 16 bit Padding */
-//    USE_MEMORY_MANAGER();
+	B_UINT8 						u8CC;    /* < Confirmation Code */
+	B_UINT8 						u8Padd;  /* < 8-bit Padding */
+	B_UINT16						u16Padd; /* < 16 bit Padding */
+/* USE_MEMORY_MANAGER(); */
 }stLocalSFAddIndicationAlt;
 
-/// \brief structure stLocalSFAddConfirmation
+/* brief structure stLocalSFAddConfirmation */
 typedef struct stLocalSFAddConfirmationAlt{
 	B_UINT8                     u8Type;
 	B_UINT8      				u8Direction;
 	B_UINT16					u16TID;
-    /// \brief 16bitCID
+	/* brief 16bitCID */
     B_UINT16                        u16CID;
-    /// \brief 16bitVCID
+	/* brief 16bitVCID */
     B_UINT16                        u16VCID;
 	struct bcm_connect_mgr_params sfAuthorizedSet;
 	struct bcm_connect_mgr_params sfAdmittedSet;
@@ -74,38 +74,38 @@ typedef struct stLocalSFAddConfirmationAlt{
 }stLocalSFAddConfirmationAlt;
 
 
-/// \brief structure stLocalSFChangeRequest
+/* brief structure stLocalSFChangeRequest */
 typedef struct stLocalSFChangeRequestAlt{
     B_UINT8                         u8Type;
 	B_UINT8      u8Direction;
 	B_UINT16					u16TID;
-    /// \brief 16bitCID
+	/* brief 16bitCID */
     B_UINT16                        u16CID;
-    /// \brief 16bitVCID
+	/* brief 16bitVCID */
     B_UINT16                        u16VCID;
 	/*
-	//Pointer location at which following connection manager param Structure can be read
-	//from the target. We only get the address location and we need to read out the
-	//entire connection manager param structure at the given location on target
-	*/
+	 * Pointer location at which following connection manager param Structure can be read
+	 * from the target. We only get the address location and we need to read out the
+	 * entire connection manager param structure at the given location on target
+	 */
 	struct bcm_connect_mgr_params sfAuthorizedSet;
 	struct bcm_connect_mgr_params sfAdmittedSet;
 	struct bcm_connect_mgr_params sfActiveSet;
 
-	B_UINT8 						u8CC;	/**<  Confirmation Code*/
-	B_UINT8 						u8Padd; 	/**<  8-bit Padding */
-	B_UINT16						u16Padd;	/**< 16 bit */
+	B_UINT8 						u8CC;	 /* < Confirmation Code */
+	B_UINT8 						u8Padd;  /* < 8-bit Padding */
+	B_UINT16						u16Padd; /* < 16 bit */
 
 }stLocalSFChangeRequestAlt;
 
-/// \brief structure stLocalSFChangeConfirmation
+/* brief structure stLocalSFChangeConfirmation */
 typedef struct stLocalSFChangeConfirmationAlt{
 	B_UINT8                         u8Type;
 	B_UINT8      					u8Direction;
 	B_UINT16						u16TID;
-    /// \brief 16bitCID
+	/* brief 16bitCID */
     B_UINT16                        u16CID;
-    /// \brief 16bitVCID
+	/* brief 16bitVCID */
     B_UINT16                        u16VCID;
 	struct bcm_connect_mgr_params sfAuthorizedSet;
 	struct bcm_connect_mgr_params sfAdmittedSet;
@@ -113,22 +113,22 @@ typedef struct stLocalSFChangeConfirmationAlt{
 
 }stLocalSFChangeConfirmationAlt;
 
-/// \brief structure stLocalSFChangeIndication
+/* brief structure stLocalSFChangeIndication */
 typedef struct stLocalSFChangeIndicationAlt{
 	B_UINT8                         u8Type;
 		B_UINT8      u8Direction;
 	B_UINT16						u16TID;
-    /// \brief 16bitCID
+	/* brief 16bitCID */
     B_UINT16                        u16CID;
-    /// \brief 16bitVCID
+	/* brief 16bitVCID */
     B_UINT16                        u16VCID;
 	struct bcm_connect_mgr_params sfAuthorizedSet;
 	struct bcm_connect_mgr_params sfAdmittedSet;
 	struct bcm_connect_mgr_params sfActiveSet;
 
-	B_UINT8 						u8CC;	/**<  Confirmation Code*/
-	B_UINT8 						u8Padd; 	/**<  8-bit Padding */
-	B_UINT16						u16Padd;	/**< 16 bit */
+	B_UINT8 						u8CC;    /* < Confirmation Code */
+	B_UINT8 						u8Padd;  /* < 8-bit Padding */
+	B_UINT16						u16Padd; /* < 16 bit */
 
 }stLocalSFChangeIndicationAlt;
 
