014c9a80c2facff48b6ddda9fae2b7fb7c32bb8d
Adrien Tétar <adri.du.59@gmail.com>
2012-10-31 14:11:03 +0100

    video/sunxi: correct licence statements
    Commit 554ca19c83a91fd99f062b8e93c709274f0a818f follow-up.
    
    Cc: Luc Verhaegen <libv@skynet.be>
    Signed-off-by: Adrien T?tar <adri-from-59@hotmail.fr>

--- drivers/video/sunxi/disp/OSAL_Pin.c
+++ drivers/video/sunxi/disp/OSAL_Pin.c
@@ -1,5 +1,5 @@
 /*
- * copyright (c) 2007-2012 Allwinner Technology Co., Ltd.
+ * Copyright (C) 2007-2012 Allwinner Technology Co., Ltd.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
--- drivers/video/sunxi/disp/disp_ump.c
+++ drivers/video/sunxi/disp/disp_ump.c
@@ -1,5 +1,5 @@
 /*
- * copyright (c) 2012 Henrik Nordstrom <henrik@henriknordstrom.net>
+ * Copyright (C) 2012 Henrik Nordstrom <henrik@henriknordstrom.net>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
