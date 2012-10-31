571e41214e988bc38c99d804e6d8e1ea1d016342
Alan Stern <stern@rowland.harvard.edu>
2012-10-31 13:09:07 -0400

    USB: remove iteration limit in hub_tt_work()
    This patch (as1621) removes the limit on the number of loops allowed
    in hub_tt_work().  The value is arbitrary, and it's silly to have a
    limit in the first place -- anything beyond the limit would not get
    handled.
    
    Besides, it's most unlikely that we'll ever need to clear more than a
    couple of TT buffers at any time.
    
    Signed-off-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -741,7 +741,6 @@ static void hub_tt_work(struct work_struct *work)
 	struct usb_hub		*hub =
 		container_of(work, struct usb_hub, tt.clear_work);
 	unsigned long		flags;
-	int			limit = 100;
 
 	spin_lock_irqsave (&hub->tt.lock, flags);
 	while (!list_empty(&hub->tt.clear_list)) {
@@ -750,9 +749,6 @@ static void hub_tt_work(struct work_struct *work)
 		struct usb_device	*hdev = hub->hdev;
 		const struct hc_driver	*drv;
 		int			status;
-
-		if (!hub->quiescing && --limit < 0)
-			break;
 
 		next = hub->tt.clear_list.next;
 		clear = list_entry (next, struct usb_tt_clear, clear_list);
