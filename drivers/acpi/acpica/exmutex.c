2489ef01849d3d7f62f53b47c245017406f02d32
Bob Moore <robert.moore@intel.com>
2012-10-31 02:27:24 +0000

    ACPICA: Add debug print message for mutex objects that are force-released
    At control method termination, any currently acquired mutex
    objects are force-released. Add a new message for each one that
    is released.
    
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpica/exmutex.c
+++ drivers/acpi/acpica/exmutex.c
@@ -461,7 +461,7 @@ void acpi_ex_release_all_mutexes(struct acpi_thread_state *thread)
 	union acpi_operand_object *next = thread->acquired_mutex_list;
 	union acpi_operand_object *obj_desc;
 
-	ACPI_FUNCTION_ENTRY();
+	ACPI_FUNCTION_NAME(ex_release_all_mutexes);
 
 	/* Traverse the list of owned mutexes, releasing each one */
 
@@ -472,6 +472,10 @@ void acpi_ex_release_all_mutexes(struct acpi_thread_state *thread)
 		obj_desc->mutex.prev = NULL;
 		obj_desc->mutex.next = NULL;
 		obj_desc->mutex.acquisition_depth = 0;
+
+		ACPI_DEBUG_PRINT((ACPI_DB_EXEC,
+				  "Force-releasing held mutex: %p\n",
+				  obj_desc));
 
 		/* Release the mutex, special case for Global Lock */
 
