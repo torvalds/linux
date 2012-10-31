984a4a0378f609fc943e77d1beda900e8a6abaaf
Xiaotian Feng <xtfeng@gmail.com>
2012-10-31 18:56:48 +0800

    staging: ozwpan: use tasklet_kill in device remove/release process
    Some driver uses tasklet_disable in device remove/release process,
    tasklet_disable will inc tasklet->count and return. If the tasklet
    is not handled yet under some softirq pressure, the tasklet will be
    placed on the tasklet_vec, never have a chance to be excuted. This might
    lead to a heavy loaded ksoftirqd, wakeup with pending_softirq, but
    tasklet is disabled. tasklet_kill should be used in this case.
    
    Signed-off-by: Xiaotian Feng <dannyfeng@tencent.com>
    Cc: Rupesh Gujare <rgujare@ozmodevices.com>
    Cc: Chris Kelly <ckelly@ozmodevices.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ozwpan/ozhcd.c
+++ drivers/staging/ozwpan/ozhcd.c
@@ -2304,8 +2304,8 @@ error:
  */
 void oz_hcd_term(void)
 {
-	tasklet_disable(&g_urb_process_tasklet);
-	tasklet_disable(&g_urb_cancel_tasklet);
+	tasklet_kill(&g_urb_process_tasklet);
+	tasklet_kill(&g_urb_cancel_tasklet);
 	platform_device_unregister(g_plat_dev);
 	platform_driver_unregister(&g_oz_plat_drv);
 	oz_trace("Pending urbs:%d\n", atomic_read(&g_pending_urbs));
