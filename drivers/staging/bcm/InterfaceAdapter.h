5c032faa82ff8a9416fc567f4e79dd626e945cec
Kevin McKinney <klmckinney1@gmail.com>
2012-11-01 23:42:13 -0400

    Staging: bcm: Fix all white space issues in InterfaceAdapter.h
    This patch fixes all white space issues
    in InterfaceAdapter.h as reported by checkpatch.pl.
    
    Signed-off-by: Kevin McKinney <klmckinney1@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/InterfaceAdapter.h
+++ drivers/staging/bcm/InterfaceAdapter.h
@@ -3,56 +3,54 @@
 
 typedef struct _BULK_ENDP_IN
 {
-    PCHAR		    bulk_in_buffer;
-    size_t          bulk_in_size;
-    UCHAR			bulk_in_endpointAddr;
-    UINT bulk_in_pipe;
-}BULK_ENDP_IN, *PBULK_ENDP_IN;
+	PCHAR	bulk_in_buffer;
+	size_t	bulk_in_size;
+	UCHAR	bulk_in_endpointAddr;
+	UINT	bulk_in_pipe;
+} BULK_ENDP_IN, *PBULK_ENDP_IN;
 
 
 typedef struct _BULK_ENDP_OUT
 {
-    UCHAR			bulk_out_buffer;
-    size_t          bulk_out_size;
-    UCHAR			bulk_out_endpointAddr;
-    UINT bulk_out_pipe;
-    //this is used when int out endpoint is used as bulk out end point
-	UCHAR			int_out_interval;
-}BULK_ENDP_OUT, *PBULK_ENDP_OUT;
+	UCHAR	bulk_out_buffer;
+	size_t	bulk_out_size;
+	UCHAR	bulk_out_endpointAddr;
+	UINT	bulk_out_pipe;
+	//this is used when int out endpoint is used as bulk out end point
+	UCHAR	int_out_interval;
+} BULK_ENDP_OUT, *PBULK_ENDP_OUT;
 
 typedef struct _INTR_ENDP_IN
 {
-    PCHAR		    int_in_buffer;
-    size_t          int_in_size;
-    UCHAR			int_in_endpointAddr;
-    UCHAR			int_in_interval;
-    UINT int_in_pipe;
-}INTR_ENDP_IN, *PINTR_ENDP_IN;
+	PCHAR	int_in_buffer;
+	size_t	int_in_size;
+	UCHAR	int_in_endpointAddr;
+	UCHAR	int_in_interval;
+	UINT	int_in_pipe;
+} INTR_ENDP_IN, *PINTR_ENDP_IN;
 
 typedef struct _INTR_ENDP_OUT
 {
-    PCHAR		    int_out_buffer;
-    size_t          int_out_size;
-    UCHAR			int_out_endpointAddr;
-    UCHAR			int_out_interval;
-    UINT int_out_pipe;
-}INTR_ENDP_OUT, *PINTR_ENDP_OUT;
-
+	PCHAR	int_out_buffer;
+	size_t	int_out_size;
+	UCHAR	int_out_endpointAddr;
+	UCHAR	int_out_interval;
+	UINT	int_out_pipe;
+} INTR_ENDP_OUT, *PINTR_ENDP_OUT;
 
 typedef struct _USB_TCB
 {
 	struct urb *urb;
 	PVOID psIntfAdapter;
 	BOOLEAN bUsed;
-}USB_TCB, *PUSB_TCB;
-
+} USB_TCB, *PUSB_TCB;
 
 typedef struct _USB_RCB
 {
 	struct urb *urb;
 	PVOID psIntfAdapter;
 	BOOLEAN bUsed;
-}USB_RCB, *PUSB_RCB;
+} USB_RCB, *PUSB_RCB;
 
 /*
 //This is the interface specific Sub-Adapter
@@ -60,9 +58,8 @@ typedef struct _USB_RCB
 */
 typedef struct _S_INTERFACE_ADAPTER
 {
-    struct usb_device * udev;
-    struct usb_interface *  interface;
-
+	struct usb_device *udev;
+	struct usb_interface *interface;
 	/* Bulk endpoint in info */
 	BULK_ENDP_IN	sBulkIn;
 	/* Bulk endpoint out info */
@@ -71,27 +68,20 @@ typedef struct _S_INTERFACE_ADAPTER
 	INTR_ENDP_IN	sIntrIn;
 	/* Interrupt endpoint out info */
 	INTR_ENDP_OUT	sIntrOut;
-
-
-
-	ULONG ulInterruptData[2];
-
+	ULONG		ulInterruptData[2];
 	struct urb *psInterruptUrb;
-
-	USB_TCB			asUsbTcb[MAXIMUM_USB_TCB];
-	USB_RCB			asUsbRcb[MAXIMUM_USB_RCB];
-	atomic_t	  	uNumTcbUsed;
-	atomic_t		uCurrTcb;
-	atomic_t		uNumRcbUsed;
-	atomic_t		uCurrRcb;
-
+	USB_TCB		asUsbTcb[MAXIMUM_USB_TCB];
+	USB_RCB		asUsbRcb[MAXIMUM_USB_RCB];
+	atomic_t	uNumTcbUsed;
+	atomic_t	uCurrTcb;
+	atomic_t	uNumRcbUsed;
+	atomic_t	uCurrRcb;
 	struct bcm_mini_adapter *psAdapter;
-	BOOLEAN                 bFlashBoot;
-	BOOLEAN 		bHighSpeedDevice ;
-
-	BOOLEAN 		bSuspended;
-	BOOLEAN 		bPreparingForBusSuspend;
+	BOOLEAN		bFlashBoot;
+	BOOLEAN		bHighSpeedDevice;
+	BOOLEAN		bSuspended;
+	BOOLEAN		bPreparingForBusSuspend;
 	struct work_struct usbSuspendWork;
-}S_INTERFACE_ADAPTER,*PS_INTERFACE_ADAPTER;
+} S_INTERFACE_ADAPTER, *PS_INTERFACE_ADAPTER;
 
 #endif
