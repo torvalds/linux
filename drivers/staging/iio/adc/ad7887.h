5daa751f89eb3e73424d0cb5c204f9e0bc14e25c
Lars-Peter Clausen <lars@metafoo.de>
2012-11-05 09:56:00 +0000

    staging:iio:ad7887: Use proper kernel doc
    Use proper kernel doc to document the platform data struct.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/adc/ad7887.h
+++ drivers/staging/iio/adc/ad7887.h
@@ -12,21 +12,20 @@
  * TODO: struct ad7887_platform_data needs to go into include/linux/iio
  */
 
+
+/**
+ * struct ad7887_platform_data - AD7887 ADC driver platform data
+ * @en_dual: Whether to use dual channel mode. If set to true AIN1 becomes the
+ *	second input channel, and Vref is internally connected to Vdd. If set to
+ *	false the device is used in single channel mode and AIN1/Vref is used as
+ *	VREF input.
+ * @use_onchip_ref: Whether to use the onchip reference. If set to true the
+ *	internal 2.5V reference is used. If set to false a external reference is
+ *	used.
+ */
 struct ad7887_platform_data {
-	/*
-	 * AD7887:
-	 * In single channel mode en_dual = flase, AIN1/Vref pins assumes its
-	 * Vref function. In dual channel mode en_dual = true, AIN1 becomes the
-	 * second input channel, and Vref is internally connected to Vdd.
-	 */
-	bool				en_dual;
-	/*
-	 * AD7887:
-	 * use_onchip_ref = true, the Vref is internally connected to the 2.500V
-	 * Voltage reference. If use_onchip_ref = false, the reference voltage
-	 * is supplied by AIN1/Vref
-	 */
-	bool				use_onchip_ref;
+	bool en_dual;
+	bool use_onchip_ref;
 };
 
 #endif /* IIO_ADC_AD7887_H_ */
