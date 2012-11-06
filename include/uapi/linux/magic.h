cee7e443344a3845e5b9111614b41e0b1afb60ce
Jarkko Sakkinen <jarkko.sakkinen@iki.fi>
2012-11-06 10:17:49 +0200

    smack: SMACK_MAGIC to include/uapi/linux/magic.h
    SMACK_MAGIC moved to a proper place for easy user space access
    (i.e. libsmack).
    
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@iki.fi>

--- include/uapi/linux/magic.h
+++ include/uapi/linux/magic.h
@@ -11,6 +11,7 @@
 #define DEBUGFS_MAGIC          0x64626720
 #define SECURITYFS_MAGIC	0x73636673
 #define SELINUX_MAGIC		0xf97cff8c
+#define SMACK_MAGIC		0x43415d53	/* "SMAC" */
 #define RAMFS_MAGIC		0x858458f6	/* some random number */
 #define TMPFS_MAGIC		0x01021994
 #define HUGETLBFS_MAGIC 	0x958458f6	/* some random number */
