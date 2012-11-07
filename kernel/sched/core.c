4e79752c25ec221ac1e28f8875b539ed7631a0db
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2012-11-07 13:35:32 -0800

    sched: Mark RCU reader in sched_show_task()
    When sched_show_task() is invoked from try_to_freeze_tasks(), there is
    no RCU read-side critical section, resulting in the following splat:
    
    [  125.780730] ===============================
    [  125.780766] [ INFO: suspicious RCU usage. ]
    [  125.780804] 3.7.0-rc3+ #988 Not tainted
    [  125.780838] -------------------------------
    [  125.780875] /home/rafael/src/linux/kernel/sched/core.c:4497 suspicious rcu_dereference_check() usage!
    [  125.780946]
    [  125.780946] other info that might help us debug this:
    [  125.780946]
    [  125.781031]
    [  125.781031] rcu_scheduler_active = 1, debug_locks = 0
    [  125.781087] 4 locks held by s2ram/4211:
    [  125.781120]  #0:  (&buffer->mutex){+.+.+.}, at: [<ffffffff811e2acf>] sysfs_write_file+0x3f/0x160
    [  125.781233]  #1:  (s_active#94){.+.+.+}, at: [<ffffffff811e2b58>] sysfs_write_file+0xc8/0x160
    [  125.781339]  #2:  (pm_mutex){+.+.+.}, at: [<ffffffff81090a81>] pm_suspend+0x81/0x230
    [  125.781439]  #3:  (tasklist_lock){.?.?..}, at: [<ffffffff8108feed>] try_to_freeze_tasks+0x2cd/0x3f0
    [  125.781543]
    [  125.781543] stack backtrace:
    [  125.781584] Pid: 4211, comm: s2ram Not tainted 3.7.0-rc3+ #988
    [  125.781632] Call Trace:
    [  125.781662]  [<ffffffff810a3c73>] lockdep_rcu_suspicious+0x103/0x140
    [  125.781719]  [<ffffffff8107cf21>] sched_show_task+0x121/0x180
    [  125.781770]  [<ffffffff8108ffb4>] try_to_freeze_tasks+0x394/0x3f0
    [  125.781823]  [<ffffffff810903b5>] freeze_kernel_threads+0x25/0x80
    [  125.781876]  [<ffffffff81090b65>] pm_suspend+0x165/0x230
    [  125.781924]  [<ffffffff8108fa29>] state_store+0x99/0x100
    [  125.781975]  [<ffffffff812f5867>] kobj_attr_store+0x17/0x20
    [  125.782038]  [<ffffffff811e2b71>] sysfs_write_file+0xe1/0x160
    [  125.782091]  [<ffffffff811667a6>] vfs_write+0xc6/0x180
    [  125.782138]  [<ffffffff81166ada>] sys_write+0x5a/0xa0
    [  125.782185]  [<ffffffff812ff6ae>] ? trace_hardirqs_on_thunk+0x3a/0x3f
    [  125.782242]  [<ffffffff81669dd2>] system_call_fastpath+0x16/0x1b
    
    This commit therefore adds the needed RCU read-side critical section.
    
    Reported-by: "Rafael J. Wysocki" <rjw@sisk.pl>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4474,6 +4474,7 @@ static const char stat_nam[] = TASK_STATE_TO_CHAR_STR;
 void sched_show_task(struct task_struct *p)
 {
 	unsigned long free = 0;
+	int ppid;
 	unsigned state;
 
 	state = p->state ? __ffs(p->state) + 1 : 0;
@@ -4493,8 +4494,11 @@ void sched_show_task(struct task_struct *p)
 #ifdef CONFIG_DEBUG_STACK_USAGE
 	free = stack_not_used(p);
 #endif
+	rcu_read_lock();
+	ppid = task_pid_nr(rcu_dereference(p->real_parent));
+	rcu_read_unlock();
 	printk(KERN_CONT "%5lu %5d %6d 0x%08lx\n", free,
-		task_pid_nr(p), task_pid_nr(rcu_dereference(p->real_parent)),
+		task_pid_nr(p), ppid,
 		(unsigned long)task_thread_info(p)->flags);
 
 	show_stack(p, NULL);
