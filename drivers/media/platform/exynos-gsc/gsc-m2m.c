24fc681a8ea6efa6bd7d6f366824a31addad2203
Sylwester Nawrocki <sylvester.nawrocki@gmail.com>
2012-11-10 19:57:56 -0300

    [media] exynos-gsc: Add missing video device vfl_dir flag initialization
    vfl_dir should be set to VFL_DIR_M2M so valid ioctls for this
    mem-to-mem device can be properly determined in the v4l2 core.
    
    Signed-off-by: Sylwester Nawrocki <sylvester.nawrocki@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/exynos-gsc/gsc-m2m.c
+++ drivers/media/platform/exynos-gsc/gsc-m2m.c
@@ -731,6 +731,7 @@ int gsc_register_m2m_device(struct gsc_dev *gsc)
 	gsc->vdev.ioctl_ops	= &gsc_m2m_ioctl_ops;
 	gsc->vdev.release	= video_device_release_empty;
 	gsc->vdev.lock		= &gsc->lock;
+	gsc->vdev.vfl_dir	= VFL_DIR_M2M;
 	snprintf(gsc->vdev.name, sizeof(gsc->vdev.name), "%s.%d:m2m",
 					GSC_MODULE_NAME, gsc->id);
 
