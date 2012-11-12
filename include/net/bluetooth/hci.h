d1244adc4281eb983a7f41b753164233a7b16b55
Szymon Janc <szymon.janc@tieto.com>
2012-11-12 11:01:05 +0100

    Bluetooth: Increase HCI command tx timeout
    Read Local OOB Data command can take more than 1 second on some chips.
    e.g. on CSR 0a12:0001 first call to Read Local OOB Data after reset
    takes about 1300ms resulting in tx timeout error.
    
    [27698.368655] Bluetooth: hci0 command 0x0c57 tx timeout
    
    2012-10-31 15:53:36.178585 < HCI Command: Read Local OOB Data (0x03|0x0057) plen 0
    2012-10-31 15:53:37.496996 > HCI Event: Command Complete (0x0e) plen 36
        Read Local OOB Data (0x03|0x0057) ncmd 1
        status 0x00
        hash 0x92219d9b447f2aa9dc12dda2ae7bae6a
        randomizer 0xb1948d0febe4ea38ce85c4e66313beba
    
    Signed-off-by: Szymon Janc <szymon.janc@tieto.com>
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>

--- include/net/bluetooth/hci.h
+++ include/net/bluetooth/hci.h
@@ -154,7 +154,7 @@ enum {
 #define HCI_DISCONN_TIMEOUT	msecs_to_jiffies(2000)	/* 2 seconds */
 #define HCI_PAIRING_TIMEOUT	msecs_to_jiffies(60000)	/* 60 seconds */
 #define HCI_INIT_TIMEOUT	msecs_to_jiffies(10000)	/* 10 seconds */
-#define HCI_CMD_TIMEOUT		msecs_to_jiffies(1000)	/* 1 second */
+#define HCI_CMD_TIMEOUT		msecs_to_jiffies(2000)	/* 2 seconds */
 #define HCI_ACL_TX_TIMEOUT	msecs_to_jiffies(45000)	/* 45 seconds */
 #define HCI_AUTO_OFF_TIMEOUT	msecs_to_jiffies(2000)	/* 2 seconds */
 
