e3fef09dda9ebf113a9a861260eff6f223808043
Jean Delvare <khali@linux-fr.org>
2012-11-12 14:18:02 +0100

    drm/i915: Optimize DIV_ROUND_CLOSEST call
    DIV_ROUND_CLOSEST is faster if the compiler knows it will only be
    dealing with unsigned dividends. This optimization rips 32 bytes of
    binary code on x86_64.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Cc: Guenter Roeck <linux@roeck-us.net>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: David Airlie <airlied@linux.ie>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -2552,7 +2552,8 @@ static void gen6_update_ring_freq(struct drm_device *dev)
 {
 	struct drm_i915_private *dev_priv = dev->dev_private;
 	int min_freq = 15;
-	int gpu_freq, ia_freq, max_ia_freq;
+	int gpu_freq;
+	unsigned int ia_freq, max_ia_freq;
 	int scaling_factor = 180;
 
 	WARN_ON(!mutex_is_locked(&dev_priv->rps.hw_lock));
