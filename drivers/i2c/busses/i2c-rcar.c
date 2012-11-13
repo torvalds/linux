45fd5e4ad2052101b4ceda5fdf4b003c428ebdfc
Wolfram Sang <w.sang@pengutronix.de>
2012-11-13 11:24:15 +0100

    i2c: rcar: fix section mismatch
    Give the driver struct a name according to the 'standard' to fix:
    
    WARNING: vmlinux.o(.data+0x11798): Section mismatch in reference from the variable rcar_i2c_drv to the function .devinit.text:rcar_i2c_probe()
    ...
    WARNING: vmlinux.o(.data+0x1179c): Section mismatch in reference from the variable rcar_i2c_drv to the function .devexit.text:rcar_i2c_remove()
    
    Reported-by: Simon Horman <horms@verge.net.au>
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>
    Cc: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- drivers/i2c/busses/i2c-rcar.c
+++ drivers/i2c/busses/i2c-rcar.c
@@ -693,7 +693,7 @@ static int __devexit rcar_i2c_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static struct platform_driver rcar_i2c_drv = {
+static struct platform_driver rcar_i2c_driver = {
 	.driver	= {
 		.name	= "i2c-rcar",
 		.owner	= THIS_MODULE,
@@ -702,7 +702,7 @@ static struct platform_driver rcar_i2c_drv = {
 	.remove		= __devexit_p(rcar_i2c_remove),
 };
 
-module_platform_driver(rcar_i2c_drv);
+module_platform_driver(rcar_i2c_driver);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Renesas R-Car I2C bus driver");
