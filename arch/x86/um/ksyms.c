b894633e7b00bcdb3e320b7ff0b995a08749441b
Jan-Simon Möller <dl9pf@gmx.de>
2012-11-14 12:45:08 +0100

    x86: LLVMLinux: Add option for clang in export of memcpy.
    Along with gcc >= 4.3, clang also needs memcpy exported.
    
    Signed-off-by: Jan-Simon Möller <dl9pf@gmx.de>
    Cc: PaX Team <pageexec@freemail.hu>

--- arch/x86/um/ksyms.c
+++ arch/x86/um/ksyms.c
@@ -4,7 +4,7 @@
 
 #ifndef CONFIG_X86_32
 /*XXX: we need them because they would be exported by x86_64 */
-#if (__GNUC__ == 4 && __GNUC_MINOR__ >= 3) || __GNUC__ > 4
+#if (__GNUC__ == 4 && __GNUC_MINOR__ >= 3) || __GNUC__ > 4 || defined(__clang__)
 EXPORT_SYMBOL(memcpy);
 #else
 EXPORT_SYMBOL(__memcpy);
