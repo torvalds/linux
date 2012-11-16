cff109768b2d9c03095848f4cd4b0754117262aa
Eric W. Biederman <ebiederm@xmission.com>
2012-11-16 03:03:01 +0000

    net: Update the per network namespace sysctls to be available to the network namespace owner
    - Allow anyone with CAP_NET_ADMIN rights in the user namespace of the
      the netowrk namespace to change sysctls.
    - Allow anyone the uid of the user namespace root the same
      permissions over the network namespace sysctls as the global root.
    - Allow anyone with gid of the user namespace root group the same
      permissions over the network namespace sysctl as the global root group.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sysctl_net.c
+++ net/sysctl_net.c
@@ -41,10 +41,20 @@ static int is_seen(struct ctl_table_set *set)
 static int net_ctl_permissions(struct ctl_table_header *head,
 			       struct ctl_table *table)
 {
+	struct net *net = container_of(head->set, struct net, sysctls);
+	kuid_t root_uid = make_kuid(net->user_ns, 0);
+	kgid_t root_gid = make_kgid(net->user_ns, 0);
+
 	/* Allow network administrator to have same access as root. */
-	if (capable(CAP_NET_ADMIN)) {
+	if (ns_capable(net->user_ns, CAP_NET_ADMIN) ||
+	    uid_eq(root_uid, current_uid())) {
 		int mode = (table->mode >> 6) & 7;
 		return (mode << 6) | (mode << 3) | mode;
+	}
+	/* Allow netns root group to have the same assess as the root group */
+	if (gid_eq(root_gid, current_gid())) {
+		int mode = (table->mode >> 3) & 7;
+		return (mode << 3) | (mode << 3) | mode;
 	}
 	return table->mode;
 }
