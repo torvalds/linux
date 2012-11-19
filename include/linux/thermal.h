d069015e268bcac6c8bd8997b3235f5f977d3ab6
Zhang Rui <rui.zhang@intel.com>
2012-11-19 15:33:51 +0800

    Introduce THERMAL_TREND_RAISE_FULL and THERMAL_TREND_DROP_FULL
    These two new thermal_trend types are used to tell the governor
    that the temeprature is raising/dropping quickly.
    
    Thermal cooling governors should handle this situation and make
    proper decisions, e.g. set cooling state to upper/lower limit directly
    instead of one step each time.
    
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>

--- include/linux/thermal.h
+++ include/linux/thermal.h
@@ -74,6 +74,8 @@ enum thermal_trend {
 	THERMAL_TREND_STABLE, /* temperature is stable */
 	THERMAL_TREND_RAISING, /* temperature is raising */
 	THERMAL_TREND_DROPPING, /* temperature is dropping */
+	THERMAL_TREND_RAISE_FULL, /* apply highest cooling action */
+	THERMAL_TREND_DROP_FULL, /* apply lowest cooling action */
 };
 
 /* Events supported by Thermal Netlink */
