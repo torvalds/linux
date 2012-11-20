98319002a2bb8e1d2d59a863bc852fbd87cf6ad8
YAMANE Toshiaki <yamanetoshi@gmail.com>
2012-11-20 21:13:32 +0900

    staging/rtl8187se: Fix spacing coding style in ieee80211/ieee80211_crypt_wep.c
    The following errors fixed.
    -ERROR: "foo * bar" should be "foo *bar"
    
    Signed-off-by: YAMANE Toshiaki <yamanetoshi@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8187se/ieee80211/ieee80211_crypt_wep.c
+++ drivers/staging/rtl8187se/ieee80211/ieee80211_crypt_wep.c
@@ -40,7 +40,7 @@ struct prism2_wep_data {
 };
 
 
-static void * prism2_wep_init(int keyidx)
+static void *prism2_wep_init(int keyidx)
 {
 	struct prism2_wep_data *priv;
 
@@ -248,7 +248,7 @@ static int prism2_wep_get_key(void *key, int len, u8 *seq, void *priv)
 }
 
 
-static char * prism2_wep_print_stats(char *p, void *priv)
+static char *prism2_wep_print_stats(char *p, void *priv)
 {
 	struct prism2_wep_data *wep = priv;
 	p += sprintf(p, "key[%d] alg=WEP len=%d\n",
