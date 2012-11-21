681e014b20d1af6851c5461dd9e6323999dd273a
Saurav Kashyap <saurav.kashyap@qlogic.com>
2012-11-21 02:40:28 -0500

    [SCSI] qla2xxx: Honor status value of 2 for report-id acquisition.
    The value of 2 is informational message and it means that port id has
    changed. The driver should honor the value and continue its normal course.
    
    Signed-off-by: Giridhar Malavali <giridhar.malavali@qlogic.com>
    Signed-off-by: Saurav Kashyap <saurav.kashyap@qlogic.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/qla2xxx/qla_mbx.c
+++ drivers/scsi/qla2xxx/qla_mbx.c
@@ -3122,7 +3122,7 @@ qla24xx_report_id_acquisition(scsi_qla_host_t *vha,
 		if (vp_idx == 0 && (MSB(stat) != 1))
 			goto reg_needed;
 
-		if (MSB(stat) != 0) {
+		if (MSB(stat) != 0 && MSB(stat) != 2) {
 			ql_dbg(ql_dbg_mbx, vha, 0x10ba,
 			    "Could not acquire ID for VP[%d].\n", vp_idx);
 			return;
