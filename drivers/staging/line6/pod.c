439f21fa2212fb839c5324c7fdfe6b04e9760e00
Stefan Hajnoczi <stefanha@gmail.com>
2012-11-22 20:48:51 +0100

    staging: line6: drop retrieve_channel sysfs attr
    Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/line6/pod.c
+++ drivers/staging/line6/pod.c
@@ -608,16 +608,6 @@ static ssize_t pod_set_store_amp_setup(struct device *dev,
 }
 
 /*
-	"write" request on "retrieve_channel" special file.
-*/
-static ssize_t pod_set_retrieve_channel(struct device *dev,
-					struct device_attribute *attr,
-					const char *buf, size_t count)
-{
-	return pod_send_retrieve_command(dev, buf, count, 0x0000, 0x00c0);
-}
-
-/*
 	"write" request on "retrieve_effects_setup" special file.
 */
 static ssize_t pod_set_retrieve_effects_setup(struct device *dev,
@@ -800,8 +790,6 @@ static DEVICE_ATTR(firmware_version, S_IRUGO, pod_get_firmware_version,
 		   line6_nop_write);
 static DEVICE_ATTR(midi_postprocess, S_IWUSR | S_IRUGO,
 		   pod_get_midi_postprocess, pod_set_midi_postprocess);
-static DEVICE_ATTR(retrieve_channel, S_IWUSR, line6_nop_read,
-		   pod_set_retrieve_channel);
 static DEVICE_ATTR(retrieve_effects_setup, S_IWUSR, line6_nop_read,
 		   pod_set_retrieve_effects_setup);
 static DEVICE_ATTR(routing, S_IWUSR | S_IRUGO, pod_get_routing,
@@ -902,7 +890,6 @@ static int pod_create_files2(struct device *dev)
 	CHECK_RETURN(device_create_file(dev, &dev_attr_finish));
 	CHECK_RETURN(device_create_file(dev, &dev_attr_firmware_version));
 	CHECK_RETURN(device_create_file(dev, &dev_attr_midi_postprocess));
-	CHECK_RETURN(device_create_file(dev, &dev_attr_retrieve_channel));
 	CHECK_RETURN(device_create_file(dev, &dev_attr_retrieve_effects_setup));
 	CHECK_RETURN(device_create_file(dev, &dev_attr_routing));
 	CHECK_RETURN(device_create_file(dev, &dev_attr_serial_number));
@@ -1034,7 +1021,6 @@ void line6_pod_disconnect(struct usb_interface *interface)
 			device_remove_file(dev, &dev_attr_finish);
 			device_remove_file(dev, &dev_attr_firmware_version);
 			device_remove_file(dev, &dev_attr_midi_postprocess);
-			device_remove_file(dev, &dev_attr_retrieve_channel);
 			device_remove_file(dev,
 					   &dev_attr_retrieve_effects_setup);
 			device_remove_file(dev, &dev_attr_routing);
