ea4f3111ef0daffa1d11fded6f375227febca458
Steffen Trumtrar <s.trumtrar@pengutronix.de>
2012-11-22 13:11:32 +0100

    viafb: rename display_timing to via_display_timing
    The struct display_timing is specific to the via subsystem. The naming leads to
    collisions with the new struct display_timing, which is supposed to be a shared
    struct between different subsystems.
    To clean this up, prepend the existing struct with the subsystem it is specific
    to.
    
    Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>

--- drivers/video/via/via_modesetting.h
+++ drivers/video/via/via_modesetting.h
@@ -33,7 +33,7 @@
 #define VIA_PITCH_MAX	0x3FF8
 
 
-struct display_timing {
+struct via_display_timing {
 	u16 hor_total;
 	u16 hor_addr;
 	u16 hor_blank_start;
@@ -49,8 +49,8 @@ struct display_timing {
 };
 
 
-void via_set_primary_timing(const struct display_timing *timing);
-void via_set_secondary_timing(const struct display_timing *timing);
+void via_set_primary_timing(const struct via_display_timing *timing);
+void via_set_secondary_timing(const struct via_display_timing *timing);
 void via_set_primary_address(u32 addr);
 void via_set_secondary_address(u32 addr);
 void via_set_primary_pitch(u32 pitch);
