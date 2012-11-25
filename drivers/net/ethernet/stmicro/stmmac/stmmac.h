cfd1979e81c72386cb4dad51a957d6f154f23525
Giuseppe CAVALLARO <peppe.cavallaro@st.com>
2012-11-25 23:10:46 +0000

    stmmac: update the driver version to Nov_2012
    Many new feauture have been introduced in the driver:
    ethtool coalesce options, Rx HW watchdog... so this patch updates the
    driver's version.
    
    Signed-off-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/stmmac.h
+++ drivers/net/ethernet/stmicro/stmmac/stmmac.h
@@ -24,7 +24,7 @@
 #define __STMMAC_H__
 
 #define STMMAC_RESOURCE_NAME   "stmmaceth"
-#define DRV_MODULE_VERSION	"March_2012"
+#define DRV_MODULE_VERSION	"Nov_2012"
 
 #include <linux/clk.h>
 #include <linux/stmmac.h>
