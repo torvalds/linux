06b7bcd8cbd7eb1af331e437ec3d8f5182ae1b7e
Oleg Nesterov <oleg@redhat.com>
2012-11-25 22:01:42 +0100

    uprobes: Introduce uprobe_is_active()
    The lifetime of uprobe->rb_node and uprobe->inode is not refcounted,
    delete_uprobe() is called when we detect that uprobe has no consumers,
    and it would be deadly wrong to do this twice.
    
    Change delete_uprobe() to WARN() if it was already called. We use
    RB_CLEAR_NODE() to mark uprobe "inactive", then RB_EMPTY_NODE() can
    be used to detect this case.
    
    RB_EMPTY_NODE() is not used directly, we add the trivial helper for
    the next change.
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Acked-by: Anton Arapov <anton@redhat.com>
    Acked-by: Srikar Dronamraju <srikar@linux.vnet.ibm.com>

--- kernel/events/uprobes.c
+++ kernel/events/uprobes.c
@@ -669,6 +669,10 @@ remove_breakpoint(struct uprobe *uprobe, struct mm_struct *mm, unsigned long vad
 	return set_orig_insn(&uprobe->arch, mm, vaddr);
 }
 
+static inline bool uprobe_is_active(struct uprobe *uprobe)
+{
+	return !RB_EMPTY_NODE(&uprobe->rb_node);
+}
 /*
  * There could be threads that have already hit the breakpoint. They
  * will recheck the current insn and restart if find_uprobe() fails.
@@ -676,9 +680,13 @@ remove_breakpoint(struct uprobe *uprobe, struct mm_struct *mm, unsigned long vad
  */
 static void delete_uprobe(struct uprobe *uprobe)
 {
+	if (WARN_ON(!uprobe_is_active(uprobe)))
+		return;
+
 	spin_lock(&uprobes_treelock);
 	rb_erase(&uprobe->rb_node, &uprobes_tree);
 	spin_unlock(&uprobes_treelock);
+	RB_CLEAR_NODE(&uprobe->rb_node); /* for uprobe_is_active() */
 	iput(uprobe->inode);
 	put_uprobe(uprobe);
 }
