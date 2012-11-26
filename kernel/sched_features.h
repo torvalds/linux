e82a284a854a95fbb1d685f49e78b9128d37e0d4
sarcastillo <sar.castillo@gmail.com>
2012-11-26 16:01:53 +0800

    sched_features:set NO_GENTLE_FAIR_SLEEPERS in the kernel instead of via the init script we've been using since RedPill for the S2 as it's shown stable enough
    Signed-off-by: sarcastillo <sar.castillo@gmail.com>

--- kernel/sched_features.h
+++ kernel/sched_features.h
@@ -3,7 +3,7 @@
  * them to run sooner, but does not allow tons of sleepers to
  * rip the spread apart.
  */
-SCHED_FEAT(GENTLE_FAIR_SLEEPERS, 1)
+SCHED_FEAT(GENTLE_FAIR_SLEEPERS, 0)
 
 /*
  * Place new tasks ahead so that they do not starve already running
