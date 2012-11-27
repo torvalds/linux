bac0382c6ad764156025978845147e5a6eccca09
Hillf Danton <dhillf@gmail.com>
2012-11-27 14:46:24 +0000

    mm: numa: migrate: Set last_nid on newly allocated page
    Pass last_nid from misplaced page to newly allocated migration target page.
    
    Signed-off-by: Hillf Danton <dhillf@gmail.com>
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- mm/migrate.c
+++ mm/migrate.c
@@ -1457,6 +1457,9 @@ static struct page *alloc_misplaced_dst_page(struct page *page,
 					  __GFP_NOMEMALLOC | __GFP_NORETRY |
 					  __GFP_NOWARN) &
 					 ~GFP_IOFS, 0);
+	if (newpage)
+		page_xchg_last_nid(newpage, page_last_nid(page));
+
 	return newpage;
 }
 
