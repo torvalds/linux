044c4f8fbab513af399ff8ce7a2af2d303d85849
Arnaldo Carvalho de Melo <acme@redhat.com>
2012-11-29 16:51:01 -0300

    perf tools: Fix TUI helpline output
    In commit e2f4351 "perf ui/helpline: Introduce ui_helpline__vshow()" the
    test for the browser used made ui_helpline__vshow() to be called only
    for the GTK browser.
    
    The TUI one then was not used and vfprintf(stderr, ...) was used
    instead, making the TUI scroll the screen instead of just printing on
    the last line.
    
    Fix it by doing the proper check, that is to call ui_helpline__vshow to
    be called for both the TUI and GTK browsers.
    
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Namhyung Kim <namhyung@gmail.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lkml.kernel.org/n/tip-iad0nw09x4orhmn0uzz4ljx3@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/debug.c
+++ tools/perf/util/debug.c
@@ -23,7 +23,7 @@ int eprintf(int level, const char *fmt, ...)
 
 	if (verbose >= level) {
 		va_start(args, fmt);
-		if (use_browser > 1)
+		if (use_browser >= 1)
 			ui_helpline__vshow(fmt, args);
 		else
 			ret = vfprintf(stderr, fmt, args);
