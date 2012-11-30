ed3125c82a9d02b023b82d6b8a41e9d33c03b96b
Johan Meiring <johanmeiring@gmail.com>
2012-11-30 18:58:28 +0200

    staging: wlags49_h2: ap_h25: corrects parentheses styling issue
    This commit corrects incorrect spaces around parentheses.
    
    Signed-off-by: Johan Meiring <johanmeiring@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlags49_h2/ap_h25.c
+++ drivers/staging/wlags49_h2/ap_h25.c
@@ -3996,7 +3996,7 @@ static const hcf_8 fw_image_4_data[] = {
 
 static const CFG_IDENTITY_STRCT fw_image_infoidentity[] = {
 	{
-		sizeof( CFG_IDENTITY_STRCT ) / sizeof(hcf_16) - 1,
+		sizeof(CFG_IDENTITY_STRCT) / sizeof(hcf_16) - 1,
 		CFG_FW_IDENTITY,
 		COMP_ID_FW_AP,
 		3,							//Variant
