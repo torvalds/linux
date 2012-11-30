95820245edf93c9cd464004db8b52a4b690637a3
Lee Jones <lee.jones@linaro.org>
2012-11-30 10:48:30 +0000

    ab8500_btemp: Reorder obtainment of platform specific battery management data
    Now that we always pass platform specific battery management data
    through platform_data instead of obtaining it via different means
    depending the way be boot the system (DT or ATAGs); we need to
    re-jiggle the way we acquire it in the driver start-up functions.
    Now it is wrong for it to be missing, but we still allow Device
    Tree code to fiddle with it once we've confirmed it's there.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/power/ab8500_btemp.c
+++ drivers/power/ab8500_btemp.c
@@ -976,6 +976,7 @@ static char *supply_interface[] = {
 static int __devinit ab8500_btemp_probe(struct platform_device *pdev)
 {
 	struct device_node *np = pdev->dev.of_node;
+	struct abx500_bm_data *plat = pdev->dev.platform_data;
 	struct ab8500_btemp *di;
 	int irq, i, ret = 0;
 	u8 val;
@@ -985,21 +986,19 @@ static int __devinit ab8500_btemp_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "%s no mem for ab8500_btemp\n", __func__);
 		return -ENOMEM;
 	}
-	di->bm = pdev->mfd_cell->platform_data;
-	if (!di->bm) {
-		if (np) {
-			ret = ab8500_bm_of_probe(&pdev->dev, np, di->bm);
-			if (ret) {
-				dev_err(&pdev->dev,
-					"failed to get battery information\n");
-				return ret;
-			}
-		} else {
-			dev_err(&pdev->dev, "missing dt node for ab8500_btemp\n");
-			return -EINVAL;
+
+	if (!plat) {
+		dev_err(&pdev->dev, "no battery management data supplied\n");
+		return -EINVAL;
+	}
+	di->bm = plat;
+
+	if (np) {
+		ret = ab8500_bm_of_probe(&pdev->dev, np, di->bm);
+		if (ret) {
+			dev_err(&pdev->dev, "failed to get battery information\n");
+			return ret;
 		}
-	} else {
-		dev_info(&pdev->dev, "falling back to legacy platform data\n");
 	}
 
 	/* get parent data */
