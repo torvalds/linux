9d2951bcd96ff2d5a88022ad3b4aebd6357d65d7
Lars-Peter Clausen <lars@metafoo.de>
2012-12-01 14:24:41 +0100

    asm-generic/mmu.h: Add support for FDPIC
    No-MMU architectures often have support for FDPIC binaries. FDPIC support
    requires two additional fields in the mm_context_t struct. This patch adds these
    fields to the generic mm_context_t definition if support for FDPIC binaries is
    enabled. This allows to use the generic mmu.h for a few more architectures.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/asm-generic/mmu.h
+++ include/asm-generic/mmu.h
@@ -8,6 +8,11 @@
 #ifndef __ASSEMBLY__
 typedef struct {
 	unsigned long		end_brk;
+
+#ifdef CONFIG_BINFMT_ELF_FDPIC
+	unsigned long		exec_fdpic_loadmap;
+	unsigned long		interp_fdpic_loadmap;
+#endif
 } mm_context_t;
 #endif
 
