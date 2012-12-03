85f395c5b0a26b3a80f9e2d35333981a2a75c0ae
Suzuki K. Poulose <suzuki@in.ibm.com>
2012-12-03 20:37:42 +0530

    powerpc/kprobes: Do not disable External interrupts during single step
    External/Decrement exceptions have lower priority than the Debug Exception.
    So, we don't have to disable the External interrupts before a single step.
    However, on BookE, Critical Input Exception(CE) has higher priority than a
    Debug Exception. Hence we mask them.
    
    Signed-off-by: 	Suzuki K. Poulose <suzuki@in.ibm.com>
    Cc:		Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Cc:		Ananth N Mavinakaynahalli <ananth@in.ibm.com>
    Cc:		Kumar Gala <galak@kernel.crashing.org>
    Cc:		linuxppc-dev@ozlabs.org
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/kernel/kprobes.c
+++ arch/powerpc/kernel/kprobes.c
@@ -104,13 +104,13 @@ void __kprobes arch_remove_kprobe(struct kprobe *p)
 
 static void __kprobes prepare_singlestep(struct kprobe *p, struct pt_regs *regs)
 {
-	/* We turn off async exceptions to ensure that the single step will
-	 * be for the instruction we have the kprobe on, if we dont its
-	 * possible we'd get the single step reported for an exception handler
-	 * like Decrementer or External Interrupt */
-	regs->msr &= ~MSR_EE;
 	regs->msr |= MSR_SINGLESTEP;
 #ifdef CONFIG_PPC_ADV_DEBUG_REGS
+	/*
+	 * We turn off Critical Input Exception(CE) to ensure that the single
+	 * step will be for the instruction we have the probe on; if we don't,
+	 * it is possible we'd get the single step reported for CE.
+	 */
 	regs->msr &= ~MSR_CE;
 	mtspr(SPRN_DBCR0, mfspr(SPRN_DBCR0) | DBCR0_IC | DBCR0_IDM);
 #ifdef CONFIG_PPC_47x
