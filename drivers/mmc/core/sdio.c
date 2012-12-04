51aa66a58494f869f491eedda86c409c50536c14
Subhash Jadavani <subhashj@codeaurora.org>
2012-12-04 17:06:18 +0530

    mmc: sdio: fix resume failure due to lack of CMD52 reset
    If SDIO keep power flag (MMC_PM_KEEP_POWER) is not set, card would
    be reinitialized during resume but as we are not resetting
    (CMD52 reset) the SDIO card during this reinitialization, card may
    fail to respond back to subsequent commands (CMD5 etc...).
    
    This change resets the card before the reinitialization of card
    during resume.
    
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    Acked-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/core/sdio.c
+++ drivers/mmc/core/sdio.c
@@ -937,10 +937,12 @@ static int mmc_sdio_resume(struct mmc_host *host)
 	mmc_claim_host(host);
 
 	/* No need to reinitialize powered-resumed nonremovable cards */
-	if (mmc_card_is_removable(host) || !mmc_card_keep_power(host))
+	if (mmc_card_is_removable(host) || !mmc_card_keep_power(host)) {
+		sdio_reset(host);
+		mmc_go_idle(host);
 		err = mmc_sdio_init_card(host, host->ocr, host->card,
 					mmc_card_keep_power(host));
-	else if (mmc_card_keep_power(host) && mmc_card_wake_sdio_irq(host)) {
+	} else if (mmc_card_keep_power(host) && mmc_card_wake_sdio_irq(host)) {
 		/* We may have switched to 1-bit mode during suspend */
 		err = sdio_enable_4bit_bus(host->card);
 		if (err > 0) {
