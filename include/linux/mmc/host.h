9f1fb60a2338aa2202ca94d67f84c582f31dbf5a
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2012-12-04 16:51:32 +0100

    mmc: add a card-event host operation
    Some hosts need to perform additional actions upon card insertion or
    ejection. Add a host operation to be called from card detection handlers.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Reviewed-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -136,6 +136,7 @@ struct mmc_host_ops {
 	void	(*enable_preset_value)(struct mmc_host *host, bool enable);
 	int	(*select_drive_strength)(unsigned int max_dtr, int host_drv, int card_drv);
 	void	(*hw_reset)(struct mmc_host *host);
+	void	(*card_event)(struct mmc_host *host);
 };
 
 struct mmc_card;
