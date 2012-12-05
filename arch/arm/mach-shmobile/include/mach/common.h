13baf88bd69ed3cf7e2374eec4a7128f62ae9c1f
Bastian Hecht <hechtb@gmail.com>
2012-12-05 12:13:06 +0000

    ARM: shmobile: sh73a0: Add CPU sleep suspend
    Add the lighest possible sleep mode on Cortex-A9 cores: CPU sleep. It is
    entered by a simple dsb and wfi instruction via cpu_do_idle(). As just
    clocks are stopped there is no need to save or restore any state of the
    system.
    
    Signed-off-by: Bastian Hecht <hechtb+renesas@gmail.com>
    Acked-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/include/mach/common.h
+++ arch/arm/mach-shmobile/include/mach/common.h
@@ -40,6 +40,7 @@ extern void sh73a0_add_early_devices(void);
 extern void sh73a0_add_standard_devices(void);
 extern void sh73a0_clock_init(void);
 extern void sh73a0_pinmux_init(void);
+extern void sh73a0_pm_init(void);
 extern struct clk sh73a0_extal1_clk;
 extern struct clk sh73a0_extal2_clk;
 extern struct clk sh73a0_extcki_clk;
