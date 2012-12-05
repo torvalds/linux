fd3065b25b69ce345073bbd294a73343a608fd8b
Jan Glauber <jang@linux.vnet.ibm.com>
2012-12-05 23:20:14 +0000

    chelsio: remove get_clock and use ktime_get
    The get_clock() of the chelsio driver clashes with the s390 one.
    The chelsio helper reads a timespec via ktime just to convert it
    back to ktime. I can see no different outcome from calling
    ktime_get directly.
    
    Remove the get_clock and use ktime_get directly.
    
    Signed-off-by: Jan Glauber <jang@linux.vnet.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/chelsio/cxgb/sge.c
+++ drivers/net/ethernet/chelsio/cxgb/sge.c
@@ -367,18 +367,6 @@ void t1_sched_set_drain_bits_per_us(struct sge *sge, unsigned int port,
 
 #endif  /*  0  */
 
-
-/*
- * get_clock() implements a ns clock (see ktime_get)
- */
-static inline ktime_t get_clock(void)
-{
-	struct timespec ts;
-
-	ktime_get_ts(&ts);
-	return timespec_to_ktime(ts);
-}
-
 /*
  * tx_sched_init() allocates resources and does basic initialization.
  */
@@ -411,7 +399,7 @@ static int tx_sched_init(struct sge *sge)
 static inline int sched_update_avail(struct sge *sge)
 {
 	struct sched *s = sge->tx_sched;
-	ktime_t now = get_clock();
+	ktime_t now = ktime_get();
 	unsigned int i;
 	long long delta_time_ns;
 
