a3adb1432d7a3ad86bb17a1638e44414537e4118
Lars-Peter Clausen <lars@metafoo.de>
2012-12-07 18:30:51 +0100

    ASoC: sigmadsp: Fix endianness conversion issue
    The 'addr' field of the sigma_action struct is stored as big endian in the
    firmware file.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Cc: stable@vger.kernel.org

--- sound/soc/codecs/sigmadsp.c
+++ sound/soc/codecs/sigmadsp.c
@@ -225,7 +225,7 @@ EXPORT_SYMBOL(process_sigma_firmware);
 static int sigma_action_write_regmap(void *control_data,
 	const struct sigma_action *sa, size_t len)
 {
-	return regmap_raw_write(control_data, le16_to_cpu(sa->addr),
+	return regmap_raw_write(control_data, be16_to_cpu(sa->addr),
 		sa->payload, len - 2);
 }
 
