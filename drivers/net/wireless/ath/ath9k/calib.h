5004c75870b05d2ffd5974d1fd681a5a1b0d9f4c
Felix Fietkau <nbd@openwrt.org>
2012-12-10 14:03:17 +0100

    ath9k_hw: Fix signal strength / channel noise reporting
    commit b7c0c238898d200e80487516e2b67aba2a522cc0 upstream.
    
    While AR_PHY_CCA_NOM_VAL_* does contain the expected internal noise floor
    for a chip measured in clean air, it refers to the lowest expected reading.
    
    Depending on the frequency, this measurement can vary by about 6db, thus
    causing a higher reported channel noise and signal strength.
    
    Factor in the 6db offset when converting internal noisefloor to channel noise.
    
    This patch makes the reported values more accurate for all chips without
    affecting NF calibration behavior.
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/wireless/ath/ath9k/calib.h
+++ drivers/net/wireless/ath/ath9k/calib.h
@@ -21,6 +21,9 @@
 
 #define AR_PHY_CCA_FILTERWINDOW_LENGTH          5
 
+/* Internal noise floor can vary by about 6db depending on the frequency */
+#define ATH9K_NF_CAL_NOISE_THRESH		6
+
 #define NUM_NF_READINGS       6
 #define ATH9K_NF_CAL_HIST_MAX 5
 
