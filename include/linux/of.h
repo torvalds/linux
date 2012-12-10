93c667ca2598bd84f1bd3f2fa176af69707699fe
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2012-12-10 20:41:30 +0100

    of: *node argument to of_parse_phandle_with_args should be const
    The "struct device_node *" argument of of_parse_phandle_with_args() can
    be const. Making this change makes it explicit that the function will
    not modify a node.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    [grant.likely: Resolved conflict with previous patch modifying of_parse_phandle()]
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- include/linux/of.h
+++ include/linux/of.h
@@ -273,7 +273,7 @@ extern int of_modalias_node(struct device_node *node, char *modalias, int len);
 extern struct device_node *of_parse_phandle(const struct device_node *np,
 					    const char *phandle_name,
 					    int index);
-extern int of_parse_phandle_with_args(struct device_node *np,
+extern int of_parse_phandle_with_args(const struct device_node *np,
 	const char *list_name, const char *cells_name, int index,
 	struct of_phandle_args *out_args);
 
