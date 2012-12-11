5cc3dead2a38ed465c99e6052e4756b4c2602f8b
Kevin McKinney <klmckinney1@gmail.com>
2012-12-10 21:54:55 -0500

    Staging: bcm: Properly format and remove comments as needed in IPv6ProtocolHdr.h
    This patch formats all comments and removes
    them as needed.
    
    Signed-off-by: Kevin McKinney <klmckinney1@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/IPv6ProtocolHdr.h
+++ drivers/staging/bcm/IPv6ProtocolHdr.h
@@ -16,14 +16,10 @@
 
 #define IPV6_MAX_CHAINEDHDR_BUFFBYTES 0x64
 /*
-// Size of Dest Options field of Destinations Options Header
-// in bytes.
-*/
+ * Size of Dest Options field of Destinations Options Header
+ * in bytes.
+ */
 #define IPV6_DESTOPTS_HDR_OPTIONSIZE 0x8
-
-//typedef  unsigned char UCHAR;
-//typedef  unsigned short USHORT;
-//typedef  unsigned long int ULONG;
 
 typedef struct IPV6HeaderFormatTag
 {
@@ -43,8 +39,6 @@ typedef struct IPV6RoutingHeaderFormatTag
 	UCHAR ucNumAddresses;
 	UCHAR ucNextAddress;
 	ULONG ulReserved;
-	//UCHAR aucAddressList[0];
-
 } IPV6RoutingHeader;
 
 typedef struct IPV6FragmentHeaderFormatTag
@@ -60,7 +54,6 @@ typedef struct IPV6DestOptionsHeaderFormatTag
 	UCHAR ucNextHeader;
 	UCHAR ucHdrExtLen;
 	UCHAR ucDestOptions[6];
-	//UCHAR udExtDestOptions[0];
 } IPV6DestOptionsHeader;
 
 typedef struct IPV6HopByHopOptionsHeaderFormatTag
@@ -76,7 +69,6 @@ typedef struct IPV6AuthenticationHeaderFormatTag
 	UCHAR  ucLength;
 	USHORT usReserved;
 	ULONG  ulSecurityParametersIndex;
-	//UCHAR  ucAuthenticationData[0];
 } IPV6AuthenticationHeader;
 
 typedef struct IPV6IcmpHeaderFormatTag
@@ -84,7 +76,6 @@ typedef struct IPV6IcmpHeaderFormatTag
 	UCHAR  ucType;
 	UCHAR  ucCode;
 	USHORT usChecksum;
-	//UCHAR  ucIcmpMsg[0];
 } IPV6IcmpHeader;
 
 typedef enum _E_IPADDR_CONTEXT
@@ -93,10 +84,10 @@ typedef enum _E_IPADDR_CONTEXT
 	eDestIpAddress
 } E_IPADDR_CONTEXT;
 
-//Function Prototypes
+/* Function Prototypes */
 
-USHORT IpVersion6(struct bcm_mini_adapter *Adapter, /**< Pointer to the driver control structure */
-					PVOID pcIpHeader, /**<Pointer to the IP Hdr of the packet*/
+USHORT IpVersion6(struct bcm_mini_adapter *Adapter, /* < Pointer to the driver control structure */
+					PVOID pcIpHeader, /* <Pointer to the IP Hdr of the packet */
 					struct bcm_classifier_rule *pstClassifierRule);
 
 VOID DumpIpv6Address(ULONG *puIpv6Address);
