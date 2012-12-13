8fa45a70badf6ce2c57421c17e86e8967ce0d478
Ang Way Chuang <wcang@sfc.wide.ad.jp>
2012-12-13 23:08:39 +0000

    bridge: remove temporary variable for MLDv2 maximum response code computation
    As suggested by Stephen Hemminger, this remove the temporary variable
    introduced in commit eca2a43bb0d2c6ebd528be6acb30a88435abe307
    ("bridge: fix icmpv6 endian bug and other sparse warnings")
    
    Signed-off-by: Ang Way Chuang <wcang@sfc.wide.ad.jp>
    Acked-by: Stephen Hemminger <shemminger@vyatta.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bridge/br_multicast.c
+++ net/bridge/br_multicast.c
@@ -1165,7 +1165,6 @@ static int br_ip6_multicast_query(struct net_bridge *br,
 		if (max_delay)
 			group = &mld->mld_mca;
 	} else if (skb->len >= sizeof(*mld2q)) {
-		u16 mrc;
 		if (!pskb_may_pull(skb, sizeof(*mld2q))) {
 			err = -EINVAL;
 			goto out;
@@ -1173,8 +1172,7 @@ static int br_ip6_multicast_query(struct net_bridge *br,
 		mld2q = (struct mld2_query *)icmp6_hdr(skb);
 		if (!mld2q->mld2q_nsrcs)
 			group = &mld2q->mld2q_mca;
-		mrc = ntohs(mld2q->mld2q_mrc);
-		max_delay = mrc ? MLDV2_MRC(mrc) : 1;
+		max_delay = mld2q->mld2q_mrc ? MLDV2_MRC(ntohs(mld2q->mld2q_mrc)) : 1;
 	}
 
 	if (!group)
