5946e7bb4401b2b8b8803c9193c569613ab1faa2
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2012-12-15 23:51:06 +0100

    sh: sh7264: Register PFC platform device
    Add arch code to register the PFC platform device instead of calling the
    driver directly. Platform device registration in the sh-pfc driver will
    be removed.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/sh/kernel/cpu/sh2a/pinmux-sh7264.c
+++ arch/sh/kernel/cpu/sh2a/pinmux-sh7264.c
@@ -11,6 +11,7 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/gpio.h>
+#include <cpu/pfc.h>
 #include <cpu/sh7264.h>
 
 enum {
@@ -2131,6 +2132,6 @@ static struct pinmux_info sh7264_pinmux_info = {
 
 static int __init plat_pinmux_setup(void)
 {
-	return register_pinmux(&sh7264_pinmux_info);
+	return sh_pfc_register_info(NULL, NULL, 0, &sh7264_pinmux_info);
 }
 arch_initcall(plat_pinmux_setup);
