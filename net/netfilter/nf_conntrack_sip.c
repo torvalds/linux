7266507d89991fa1e989283e4e032c6d9357fe26
Kevin Cernekee <cernekee@gmail.com>
2012-12-17 18:33:58 +0000

    netfilter: nf_ct_sip: support Cisco 7941/7945 IP phones
    Most SIP devices use a source port of 5060/udp on SIP requests, so the
    response automatically comes back to port 5060:
    
        phone_ip:5060 -> proxy_ip:5060   REGISTER
        proxy_ip:5060 -> phone_ip:5060   100 Trying
    
    The newer Cisco IP phones, however, use a randomly chosen high source
    port for the SIP request but expect the response on port 5060:
    
        phone_ip:49173 -> proxy_ip:5060  REGISTER
        proxy_ip:5060 -> phone_ip:5060   100 Trying
    
    Standard Linux NAT, with or without nf_nat_sip, will send the reply back
    to port 49173, not 5060:
    
        phone_ip:49173 -> proxy_ip:5060  REGISTER
        proxy_ip:5060 -> phone_ip:49173  100 Trying
    
    But the phone is not listening on 49173, so it will never see the reply.
    
    This patch modifies nf_*_sip to work around this quirk by extracting
    the SIP response port from the Via: header, iff the source IP in the
    packet header matches the source IP in the SIP request.
    
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>
    Acked-by: Eric Dumazet <eric.dumazet@gmail.com>
    Cc: Patrick McHardy <kaber@trash.net>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nf_conntrack_sip.c
+++ net/netfilter/nf_conntrack_sip.c
@@ -1440,8 +1440,25 @@ static int process_sip_request(struct sk_buff *skb, unsigned int protoff,
 {
 	enum ip_conntrack_info ctinfo;
 	struct nf_conn *ct = nf_ct_get(skb, &ctinfo);
+	struct nf_ct_sip_master *ct_sip_info = nfct_help_data(ct);
+	enum ip_conntrack_dir dir = CTINFO2DIR(ctinfo);
 	unsigned int matchoff, matchlen;
 	unsigned int cseq, i;
+	union nf_inet_addr addr;
+	__be16 port;
+
+	/* Many Cisco IP phones use a high source port for SIP requests, but
+	 * listen for the response on port 5060.  If we are the local
+	 * router for one of these phones, save the port number from the
+	 * Via: header so that nf_nat_sip can redirect the responses to
+	 * the correct port.
+	 */
+	if (ct_sip_parse_header_uri(ct, *dptr, NULL, *datalen,
+				    SIP_HDR_VIA_UDP, NULL, &matchoff,
+				    &matchlen, &addr, &port) > 0 &&
+	    port != ct->tuplehash[dir].tuple.src.u.udp.port &&
+	    nf_inet_addr_cmp(&addr, &ct->tuplehash[dir].tuple.src.u3))
+		ct_sip_info->forced_dport = port;
 
 	for (i = 0; i < ARRAY_SIZE(sip_handlers); i++) {
 		const struct sip_handler *handler;
