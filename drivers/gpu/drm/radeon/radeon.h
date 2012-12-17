5f8f635edd8ad5a6416bff4c5ff486500357f473
Jerome Glisse <jglisse@redhat.com>
2012-12-17 11:04:32 -0500

    drm/radeon: avoid deadlock in pm path when waiting for fence
    radeon_fence_wait_empty_locked should not trigger GPU reset as no
    place where it's call from would benefit from such thing and it
    actually lead to a kernel deadlock in case the reset is triggered
    from pm codepath. Instead force ring completion in place where it
    makes sense or return early in others.
    
    Signed-off-by: Jerome Glisse <jglisse@redhat.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: stable@vger.kernel.org

--- drivers/gpu/drm/radeon/radeon.h
+++ drivers/gpu/drm/radeon/radeon.h
@@ -231,7 +231,7 @@ void radeon_fence_process(struct radeon_device *rdev, int ring);
 bool radeon_fence_signaled(struct radeon_fence *fence);
 int radeon_fence_wait(struct radeon_fence *fence, bool interruptible);
 int radeon_fence_wait_next_locked(struct radeon_device *rdev, int ring);
-void radeon_fence_wait_empty_locked(struct radeon_device *rdev, int ring);
+int radeon_fence_wait_empty_locked(struct radeon_device *rdev, int ring);
 int radeon_fence_wait_any(struct radeon_device *rdev,
 			  struct radeon_fence **fences,
 			  bool intr);
