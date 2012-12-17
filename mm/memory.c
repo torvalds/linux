b3dd20709db2cab0da0ade1f246fd6e6ab1396eb
Andrew Morton <akpm@linux-foundation.org>
2012-12-17 15:59:24 -0800

    mm/memory.c: suppress warning
    gcc-4.4.4 screws this up.
    
      mm/memory.c: In function 'do_pmd_numa_page':
      mm/memory.c:3594: warning: no return statement in function returning non-void
    
    Cc: Mel Gorman <mgorman@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/memory.c
+++ mm/memory.c
@@ -3590,6 +3590,7 @@ static int do_pmd_numa_page(struct mm_struct *mm, struct vm_area_struct *vma,
 		     unsigned long addr, pmd_t *pmdp)
 {
 	BUG();
+	return 0;
 }
 #endif /* CONFIG_NUMA_BALANCING */
 
