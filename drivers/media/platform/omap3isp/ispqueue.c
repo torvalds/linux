e19bc863f1f45223a85935f070177eda6b422f8f
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2012-12-17 04:52:48 -0300

    [media] omap3isp: ispqueue: Fix uninitialized variable compiler warnings
    drivers/media/platform/omap3isp/ispqueue.c:399:18: warning: 'pa' may be
    used uninitialized in this function [-Wuninitialized]
    This is a false positive but the compiler has no way to know about it,
    so initialize the variable to 0.
    drivers/media/platform/omap3isp/ispqueue.c:445:6: warning:
    'vm_page_prot' may be used uninitialized in this function
    [-Wuninitialized]
    This is a false positive and the compiler should know better. Use
    uninitialized_var().
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/omap3isp/ispqueue.c
+++ drivers/media/platform/omap3isp/ispqueue.c
@@ -366,7 +366,7 @@ static int isp_video_buffer_prepare_pfnmap(struct isp_video_buffer *buf)
 	unsigned long this_pfn;
 	unsigned long start;
 	unsigned long end;
-	dma_addr_t pa;
+	dma_addr_t pa = 0;
 	int ret = -EFAULT;
 
 	start = buf->vbuf.m.userptr;
@@ -419,7 +419,7 @@ done:
 static int isp_video_buffer_prepare_vm_flags(struct isp_video_buffer *buf)
 {
 	struct vm_area_struct *vma;
-	pgprot_t vm_page_prot;
+	pgprot_t uninitialized_var(vm_page_prot);
 	unsigned long start;
 	unsigned long end;
 	int ret = -EFAULT;
