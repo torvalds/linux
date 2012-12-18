f562146a3daf6aa0bbf2a1bc4b6b7da031ed5dcd
Namjae Jeon <linkinjeon@gmail.com>
2012-12-17 16:02:56 -0800

    fat: notify when discard is not supported
    Change fatfs so that a warning is emitted when an attempt is made to mount
    a filesystem with the unsupported `discard' option.
    
    ext4 aready does this: http://patchwork.ozlabs.org/patch/192668/
    
    Signed-off-by: Namjae Jeon <linkinjeon@gmail.com>
    Signed-off-by: Amit Sahrawat <amit.sahrawat83@gmail.com>
    Acked-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/fat/inode.c
+++ fs/fat/inode.c
@@ -26,6 +26,7 @@
 #include <linux/writeback.h>
 #include <linux/log2.h>
 #include <linux/hash.h>
+#include <linux/blkdev.h>
 #include <asm/unaligned.h>
 #include "fat.h"
 
@@ -1429,6 +1430,14 @@ int fat_fill_super(struct super_block *sb, void *data, int silent, int isvfat,
 	if (!sb->s_root) {
 		fat_msg(sb, KERN_ERR, "get root inode failed");
 		goto out_fail;
+	}
+
+	if (sbi->options.discard) {
+		struct request_queue *q = bdev_get_queue(sb->s_bdev);
+		if (!blk_queue_discard(q))
+			fat_msg(sb, KERN_WARNING,
+					"mounting with \"discard\" option, but "
+					"the device does not support discard");
 	}
 
 	return 0;
