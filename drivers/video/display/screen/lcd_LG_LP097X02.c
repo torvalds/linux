4bddb7c26d978f4fe2e7b20df00855461b8dd410
hhb <hhb@rock-chips.com>
2012-12-18 10:45:56 +0800

    modify lcd_LG_LP097X02 driver

--- drivers/video/display/screen/lcd_LG_LP097X02.c
+++ drivers/video/display/screen/lcd_LG_LP097X02.c
@@ -12,20 +12,20 @@
 #define OUT_CLK		 100000000
 #define LCDC_ACLK           500000000
 /* Timing */
-#define H_PW			380
-#define H_BP			540
+#define H_PW			320
+#define H_BP			480
 #define H_VD			1024
-#define H_FP			300
+#define H_FP			260
 
-#define V_PW			15
-#define V_BP			10
+#define V_PW			10
+#define V_BP			6
 #define V_VD			768
-#define V_FP			30
+#define V_FP			16
 
 #define LCD_WIDTH   196// 142  // 202
 #define LCD_HEIGHT  147 //106//  152
 /* Other */
-#define DCLK_POL		0 // 
+#define DCLK_POL		1 // 
 #define SWAP_RB			0
 
 void set_lcd_info(struct rk29fb_screen *screen, struct rk29lcd_info *lcd_info )
