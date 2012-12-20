eca15296a9c2a5d5d7d8281a710ba4bd0c2e7cd3
Ben Skeggs <bskeggs@redhat.com>
2012-12-20 12:52:06 +1000

    drm/nve0/graph: fix fuc, and enable acceleration on all known chipsets
    Also adds GK106 to chipsets known by ucode.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/engine/graph/nve0.c
+++ drivers/gpu/drm/nouveau/core/engine/graph/nve0.c
@@ -203,7 +203,7 @@ nve0_graph_ctor(struct nouveau_object *parent, struct nouveau_object *engine,
 	struct nvc0_graph_priv *priv;
 	int ret, i;
 
-	ret = nouveau_graph_create(parent, engine, oclass, false, &priv);
+	ret = nouveau_graph_create(parent, engine, oclass, true, &priv);
 	*pobject = nv_object(priv);
 	if (ret)
 		return ret;
