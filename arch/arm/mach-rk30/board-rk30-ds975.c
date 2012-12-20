966f8f27c5d21efc92b9d71f78ce5fa6efc90227
hhb <hhb@rock-chips.com>
2012-12-20 10:05:11 +0800

    DS975: change BATTERY_RK30_ADC to BATTERY_RK30_ADC_FAC

--- arch/arm/mach-rk30/board-rk30-ds975.c
+++ arch/arm/mach-rk30/board-rk30-ds975.c
@@ -1609,7 +1609,7 @@ struct rk29_sdmmc_platform_data default_sdmmc1_data = {
  * the end of setting for SDMMC devices
 **************************************************************************************************/
 
-#ifdef CONFIG_BATTERY_RK30_ADC
+#ifdef CONFIG_BATTERY_RK30_ADC_FAC
 static struct rk30_adc_battery_platform_data rk30_adc_battery_platdata = {
         .dc_det_pin      = RK30_PIN6_PA5,
         .batt_low_pin    = RK30_PIN6_PA0,
@@ -1739,7 +1739,7 @@ static struct platform_device *devices[] __initdata = {
 #ifdef CONFIG_RFKILL_RK
 	&device_rfkill_rk,
 #endif
-#ifdef CONFIG_BATTERY_RK30_ADC
+#ifdef CONFIG_BATTERY_RK30_ADC_FAC
  	&rk30_device_adc_battery,
 #endif
 #ifdef CONFIG_BATTERY_RK29_ADC
