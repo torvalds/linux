b391b0ef0f9399dcba8fa025ab5e5d4229847467
Mauro Carvalho Chehab <mchehab@redhat.com>
2012-12-20 15:21:23 -0200

    [media] tm6000-video.c: warning fix
    drivers/media/usb/tm6000/tm6000-video.c: In function '__check_keep_urb':
    drivers/media/usb/tm6000/tm6000-video.c:1926:1: warning: return from incompatible pointer type [enabled by default]
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/usb/tm6000/tm6000-video.c
+++ drivers/media/usb/tm6000/tm6000-video.c
@@ -57,7 +57,7 @@
 static unsigned int vid_limit = 16;	/* Video memory limit, in Mb */
 static int video_nr = -1;		/* /dev/videoN, -1 for autodetect */
 static int radio_nr = -1;		/* /dev/radioN, -1 for autodetect */
-static int keep_urb;			/* keep urb buffers allocated */
+static bool keep_urb;			/* keep urb buffers allocated */
 
 /* Debug level */
 int tm6000_debug;
