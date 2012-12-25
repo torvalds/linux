7b83d1a297ac1afda2bb9a9979ca73173148623f
Al Viro <viro@zeniv.linux.org.uk>
2012-12-25 15:26:55 -0500

    x86: switch to generic compat rt_sigqueueinfo()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/x86/include/asm/sys_ia32.h
+++ arch/x86/include/asm/sys_ia32.h
@@ -45,7 +45,6 @@ asmlinkage long sys32_sysfs(int, u32, u32);
 
 asmlinkage long sys32_sched_rr_get_interval(compat_pid_t,
 					    struct compat_timespec __user *);
-asmlinkage long sys32_rt_sigqueueinfo(int, int, compat_siginfo_t __user *);
 
 asmlinkage long sys32_pread(unsigned int, char __user *, u32, u32, u32);
 asmlinkage long sys32_pwrite(unsigned int, const char __user *, u32, u32, u32);
