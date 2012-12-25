c86b53c82ce4b6572f020d71c4d11a7a81686570
sarcastillo <sar.castillo@gmail.com>
2012-12-25 17:28:01 +0800

    block:sio tweaks for flash based devices

--- block/sio-iosched.c
+++ block/sio-iosched.c
@@ -26,12 +26,12 @@ enum { ASYNC, SYNC };
 static const int sync_read_expire  = HZ / 2;	/* max time before a sync read is submitted. */
 static const int sync_write_expire = 2 * HZ;	/* max time before a sync write is submitted. */
 
-static const int async_read_expire  =  4 * HZ;	/* ditto for async, these limits are SOFT! */
-static const int async_write_expire = 16 * HZ;	/* ditto for async, these limits are SOFT! */
+static const int async_read_expire  =  2 * HZ;	/* ditto for async, these limits are SOFT! */
+static const int async_write_expire =  2 * HZ;	/* ditto for async, these limits are SOFT! */
 
-static const int writes_starved = 1;		/* max times reads can starve a write */
-static const int fifo_batch     = 1;		/* # of sequential requests treated as one
-						   by the above parameters. For throughput. */
+static const int writes_starved = 2;		/* max times reads can starve a write */
+static const int fifo_batch     = 2;		/* # of sequential requests treated as one
+											   by the above parameters. For throughput. */
 
 /* Elevator data */
 struct sio_data {
