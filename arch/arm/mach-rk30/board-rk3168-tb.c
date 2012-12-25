3b07097f9f85001a17024007c68884b8dc5098c8
hxy <hxy@rock-chips.com>
2012-12-25 17:39:56 +0800

    rk3168: modidied key map in rk3168 top board

--- arch/arm/mach-rk30/board-rk3168-tb.c
+++ arch/arm/mach-rk30/board-rk3168-tb.c
@@ -83,7 +83,6 @@
 #endif
 
 #include "board-rk3168-tb-camera.c"
-#include "board-rk3066b-sdk-key.c"
 
 #if defined(CONFIG_TOUCHSCREEN_GT8XX)
 #define TOUCH_RESET_PIN  RK30_PIN0_PB6
@@ -1825,6 +1824,77 @@ static void __init rk30_i2c_register_board_info(void)
 #endif
 }
 //end of i2c
+
+// ========== Begin of rk3168 top board keypad defination ============
+
+#include <mach/gpio.h>
+#include <plat/key.h>
+
+#define EV_ENCALL                               KEY_F4
+#define EV_MENU                                 KEY_F1
+
+#define PRESS_LEV_LOW                   1
+#define PRESS_LEV_HIGH                  0
+
+static struct rk29_keys_button key_button[] = {
+        {
+                .desc   = "vol-",
+                .code   = KEY_VOLUMEDOWN,
+		.adc_value      = 900,
+                .gpio   = INVALID_GPIO,
+                .active_low = PRESS_LEV_LOW,
+        },
+        {
+                .desc   = "play",
+                .code   = KEY_POWER,
+                .gpio   = RK30_PIN0_PA4,
+                .active_low = PRESS_LEV_LOW,
+                .wakeup = 1,
+        },
+        {
+                .desc   = "vol+",
+                .code   = KEY_VOLUMEUP,
+                .adc_value      = 1,
+                .gpio = INVALID_GPIO,
+                .active_low = PRESS_LEV_LOW,
+        },
+	{
+                .desc   = "menu",
+                .code   = EV_MENU,
+                .adc_value      = 133,
+                .gpio = INVALID_GPIO,
+                .active_low = PRESS_LEV_LOW,
+        },
+        {
+                .desc   = "home",
+                .code   = KEY_HOME,
+                .adc_value      = 550,
+                .gpio = INVALID_GPIO,
+                .active_low = PRESS_LEV_LOW,
+        },
+        {
+                .desc   = "esc",
+                .code   = KEY_BACK,
+                .adc_value      = 333,
+		.gpio = INVALID_GPIO,
+		.active_low = PRESS_LEV_LOW,
+	},
+	{
+		.desc	= "camera",
+		.code	= KEY_CAMERA,
+		.adc_value	= 742,
+		.gpio = INVALID_GPIO,
+		.active_low = PRESS_LEV_LOW,
+	},
+};
+struct rk29_keys_platform_data rk29_keys_pdata = {
+	.buttons	= key_button,
+	.nbuttons	= ARRAY_SIZE(key_button),
+	.chn	= 1,  //chn: 0-7, if do not use ADC,set 'chn' -1
+};
+
+// =========== End of rk3168 top board keypad defination  =============
+
 
 #define POWER_ON_PIN RK30_PIN0_PA0   //power_hold
 static void rk30_pm_power_off(void)
