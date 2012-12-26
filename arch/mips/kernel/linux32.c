5e392b8db7471c15f587e5f206b7ca98282697e8
Al Viro <viro@zeniv.linux.org.uk>
2012-12-25 23:22:15 -0500

    mips: don't bother with compat_sys_futex() wrappers
    ... it's COMPAT_SYSCALL_DEFINE now
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/mips/kernel/linux32.c
+++ arch/mips/kernel/linux32.c
@@ -312,10 +312,3 @@ SYSCALL_DEFINE6(32_fanotify_mark, int, fanotify_fd, unsigned int, flags,
 	return sys_fanotify_mark(fanotify_fd, flags, merge_64(a3, a4),
 				 dfd, pathname);
 }
-
-SYSCALL_DEFINE6(32_futex, u32 __user *, uaddr, int, op, u32, val,
-		struct compat_timespec __user *, utime, u32 __user *, uaddr2,
-		u32, val3)
-{
-	return compat_sys_futex(uaddr, op, val, utime, uaddr2, val3);
-}
