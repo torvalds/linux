102c9543ff368709980c6d68b7a75f840edda6b5
Andrew Lunn <andrew@lunn.ch>
2012-12-28 15:14:10 +0100

    ARM: Kirkwood: Convert NS2 to gpio-poweroff.
    Remove C code and add a Device Tree node in its place.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Tested-by: Simon Guinot <simon.guinot@sequanux.org>
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- arch/arm/mach-kirkwood/board-ns2.c
+++ arch/arm/mach-kirkwood/board-ns2.c
@@ -15,20 +15,12 @@
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/mv643xx_eth.h>
-#include <linux/gpio.h>
 #include <linux/of.h>
 #include "common.h"
 
 static struct mv643xx_eth_platform_data ns2_ge00_data = {
 	.phy_addr	= MV643XX_ETH_PHY_ADDR(8),
 };
-
-#define NS2_GPIO_POWER_OFF	31
-
-static void ns2_power_off(void)
-{
-	gpio_set_value(NS2_GPIO_POWER_OFF, 1);
-}
 
 void __init ns2_init(void)
 {
@@ -39,10 +31,4 @@ void __init ns2_init(void)
 	    of_machine_is_compatible("lacie,netspace_mini_v2"))
 		ns2_ge00_data.phy_addr = MV643XX_ETH_PHY_ADDR(0);
 	kirkwood_ge00_init(&ns2_ge00_data);
-
-	if (gpio_request(NS2_GPIO_POWER_OFF, "power-off") == 0 &&
-	    gpio_direction_output(NS2_GPIO_POWER_OFF, 0) == 0)
-		pm_power_off = ns2_power_off;
-	else
-		pr_err("ns2: failed to configure power-off GPIO\n");
 }
