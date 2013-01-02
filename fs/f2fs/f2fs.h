d66d1f76878fcb1e78592fe8aecd13f438d6c0d7
Jaegeuk Kim <jaegeuk.kim@samsung.com>
2013-01-03 08:57:21 +0900

    f2fs: initialize newly allocated dnode structure
    This patch resolves Coverity #753112.
    
    In practical, the existing code flow does not fall into the reported errorneous
    path. But, anyway, let's avoid this for future.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk.kim@samsung.com>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -211,11 +211,11 @@ struct dnode_of_data {
 static inline void set_new_dnode(struct dnode_of_data *dn, struct inode *inode,
 		struct page *ipage, struct page *npage, nid_t nid)
 {
+	memset(dn, 0, sizeof(*dn));
 	dn->inode = inode;
 	dn->inode_page = ipage;
 	dn->node_page = npage;
 	dn->nid = nid;
-	dn->inode_page_locked = 0;
 }
 
 /*
