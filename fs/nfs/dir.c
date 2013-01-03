f8d9a897d4384b77f13781ea813156568f68b83e
Weston Andros Adamson <dros@netapp.com>
2013-01-03 16:42:29 -0500

    NFS: Fix access to suid/sgid executables
    nfs_open_permission_mask() should only check MAY_EXEC for files that
    are opened with __FMODE_EXEC.
    
    Also fix NFSv4 access-in-open path in a similar way -- openflags must be
    used because fmode will not always have FMODE_EXEC set.
    
    This patch fixes https://bugzilla.kernel.org/show_bug.cgi?id=49101
    
    Signed-off-by: Weston Andros Adamson <dros@netapp.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    Cc: stable@vger.kernel.org

--- fs/nfs/dir.c
+++ fs/nfs/dir.c
@@ -2153,12 +2153,16 @@ static int nfs_open_permission_mask(int openflags)
 {
 	int mask = 0;
 
-	if ((openflags & O_ACCMODE) != O_WRONLY)
-		mask |= MAY_READ;
-	if ((openflags & O_ACCMODE) != O_RDONLY)
-		mask |= MAY_WRITE;
-	if (openflags & __FMODE_EXEC)
-		mask |= MAY_EXEC;
+	if (openflags & __FMODE_EXEC) {
+		/* ONLY check exec rights */
+		mask = MAY_EXEC;
+	} else {
+		if ((openflags & O_ACCMODE) != O_WRONLY)
+			mask |= MAY_READ;
+		if ((openflags & O_ACCMODE) != O_RDONLY)
+			mask |= MAY_WRITE;
+	}
+
 	return mask;
 }
 
