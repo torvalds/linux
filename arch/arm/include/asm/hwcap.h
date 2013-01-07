91b575ccfa029c823dc51eec8dd9dbacb4377005
Laurent Coustet <ed@zehome.com>
2013-01-07 23:55:44 +0100

    Error in conflict merge from 3.0.57

--- arch/arm/include/asm/hwcap.h
+++ arch/arm/include/asm/hwcap.h
@@ -23,6 +23,7 @@
 #define HWCAP_VFPv4	(1 << 16)
 #define HWCAP_IDIVA	(1 << 17)
 #define HWCAP_IDIVT	(1 << 18)
+#define HWCAP_VFPD32    (1 << 19)   /* set if VFP has 32 regs (not 16) */
 #define HWCAP_IDIV	(HWCAP_IDIVA | HWCAP_IDIVT)
 
 #if defined(__KERNEL__) && !defined(__ASSEMBLY__)
