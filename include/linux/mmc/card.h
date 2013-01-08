c148e9ff4bd45f26d3f0253c20efc497672c3c84
Zhang, YiX X <yix.x.zhang@intel.com>
2013-01-08 06:07:39 +0000

    mmc: correct the EXCEPTION_EVENTS_STATUS value in comment
    The right value is 54 according to eMMC 4.5 specification.
    
    Signed-off-by: ZhangYi <yix.x.zhang@intel.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -83,7 +83,7 @@ struct mmc_ext_csd {
 	unsigned int            data_tag_unit_size;     /* DATA TAG UNIT size */
 	unsigned int		boot_ro_lock;		/* ro lock support */
 	bool			boot_ro_lockable;
-	u8			raw_exception_status;	/* 53 */
+	u8			raw_exception_status;	/* 54 */
 	u8			raw_partition_support;	/* 160 */
 	u8			raw_rpmb_size_mult;	/* 168 */
 	u8			raw_erased_mem_count;	/* 181 */
