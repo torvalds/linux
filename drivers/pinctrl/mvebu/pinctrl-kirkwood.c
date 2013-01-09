16fa36be89305f06a217509fcce4f4aed5df0627
Andrew Lunn <andrew@lunn.ch>
2013-01-09 19:28:09 +0100

    pinctrl: mvebu: Fix compiler warnings
    match->data is const void * where as dev.platform_data is just void *.
    Add a cast to remove the const, which is causing the compiler warning:
    
    drivers/pinctrl/mvebu/pinctrl-kirkwood.c:461:26: warning: assignment
    discards 'const' qualifier from pointer target type
    
    Dove has the exact same warning, so gets the same cast.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Acked-by: Jason Cooper <jason@lakedaemon.net>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/mvebu/pinctrl-kirkwood.c
+++ drivers/pinctrl/mvebu/pinctrl-kirkwood.c
@@ -458,7 +458,7 @@ static int kirkwood_pinctrl_probe(struct platform_device *pdev)
 {
 	const struct of_device_id *match =
 		of_match_device(kirkwood_pinctrl_of_match, &pdev->dev);
-	pdev->dev.platform_data = match->data;
+	pdev->dev.platform_data = (void *)match->data;
 	return mvebu_pinctrl_probe(pdev);
 }
 
