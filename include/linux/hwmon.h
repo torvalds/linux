c25fb816298138a4b12c606f0aaa018bdd3cc09c
Guenter Roeck <linux@roeck-us.net>
2013-01-09 08:12:46 -0800

    hwmon: Retire SENSORS_LIMIT
    SENSORS_LIMIT and clamp_val have the same functionality, so retire SENSORS_LIMIT
    as it is no longer needed.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Acked-by: Jean Delvare <khali@linux-fr.org>

--- include/linux/hwmon.h
+++ include/linux/hwmon.h
@@ -20,16 +20,4 @@ struct device *hwmon_device_register(struct device *dev);
 
 void hwmon_device_unregister(struct device *dev);
 
-/* Scale user input to sensible values */
-static inline int SENSORS_LIMIT(long value, long low, long high)
-{
-	if (value < low)
-		return low;
-	else if (value > high)
-		return high;
-	else
-		return value;
-}
-
 #endif
-
