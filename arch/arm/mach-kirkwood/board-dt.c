970a43c76ffb85fbb3d215e5c2b91074f5e33768
Andrew Lunn <andrew@lunn.ch>
2013-01-10 22:01:09 +0100

    ARM: Kirkwood: Remove redundent SDIO clock alias
    Now that SDIO is instantiated via DT, and the SDIO DT node has a clocks
    property, we no longer need a C coded clock alias. Remove it.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- arch/arm/mach-kirkwood/board-dt.c
+++ arch/arm/mach-kirkwood/board-dt.c
@@ -62,11 +62,6 @@ static void __init kirkwood_legacy_clk_init(void)
 	clkspec.args[0] = CGC_BIT_GE1;
 	orion_clkdev_add(NULL, "mv643xx_eth_port.1",
 			 of_clk_get_from_provider(&clkspec));
-
-	clkspec.args[0] = CGC_BIT_SDIO;
-	orion_clkdev_add(NULL, "mvsdio",
-			 of_clk_get_from_provider(&clkspec));
-
 }
 
 static void __init kirkwood_of_clk_init(void)
