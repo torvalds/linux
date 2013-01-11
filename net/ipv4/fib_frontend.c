28a28283f8220e0b397a18f624fbcbbe046b063c
Rami Rosen <ramirose@gmail.com>
2013-01-11 09:59:20 +0000

    ipv4: fib: fix a comment.
    In fib_frontend.c, there is a confusing comment; NETLINK_CB(skb).portid does not
    refer to a pid of sending process, but rather to a netlink portid.
    
    Signed-off-by: Rami Rosen <ramirose@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/fib_frontend.c
+++ net/ipv4/fib_frontend.c
@@ -974,7 +974,7 @@ static void nl_fib_input(struct sk_buff *skb)
 
 	nl_fib_lookup(frn, tb);
 
-	portid = NETLINK_CB(skb).portid;      /* pid of sending process */
+	portid = NETLINK_CB(skb).portid;      /* netlink portid */
 	NETLINK_CB(skb).portid = 0;        /* from kernel */
 	NETLINK_CB(skb).dst_group = 0;  /* unicast */
 	netlink_unicast(net->ipv4.fibnl, skb, portid, MSG_DONTWAIT);
