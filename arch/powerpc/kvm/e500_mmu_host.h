b71c9e2fb72cf538aadbc59ea719639a1e2191fa
Alexander Graf <agraf@suse.de>
2013-01-11 15:22:45 +0100

    KVM: PPC: E500: Split host and guest MMU parts
    This patch splits the file e500_tlb.c into e500_mmu.c (guest TLB handling)
    and e500_mmu_host.c (host TLB handling).
    
    The main benefit of this split is readability and maintainability. It's
    just a lot harder to write dirty code :).
    
    Signed-off-by: Alexander Graf <agraf@suse.de>

---
+++ arch/powerpc/kvm/e500_mmu_host.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2008-2013 Freescale Semiconductor, Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef KVM_E500_MMU_HOST_H
+#define KVM_E500_MMU_HOST_H
+
+void inval_gtlbe_on_host(struct kvmppc_vcpu_e500 *vcpu_e500, int tlbsel,
+			 int esel);
+
+void clear_tlb1_bitmap(struct kvmppc_vcpu_e500 *vcpu_e500);
+void clear_tlb_refs(struct kvmppc_vcpu_e500 *vcpu_e500);
+int e500_mmu_host_init(struct kvmppc_vcpu_e500 *vcpu_e500);
+void e500_mmu_host_uninit(struct kvmppc_vcpu_e500 *vcpu_e500);
+
+#endif /* KVM_E500_MMU_HOST_H */
