87696f9234cb681ded2697b0f0fd01bb15a2aff2
Alexander Duyck <alexander.h.duyck@intel.com>
2013-01-11 10:38:42 -0800

    net: Export __netdev_pick_tx so that it can be used in modules
    When testing with FCoE enabled we discovered that I had not exported
    __netdev_pick_tx.  As a result ixgbe doesn't build with the RFC patches
    applied because ixgbe_select_queue was calling the function.  This change
    corrects that build issue by correctly exporting __netdev_pick_tx so it
    can be used by modules.
    
    Signed-off-by: Alexander Duyck <alexander.h.duyck@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -2746,6 +2746,7 @@ u16 __netdev_pick_tx(struct net_device *dev, struct sk_buff *skb)
 
 	return queue_index;
 }
+EXPORT_SYMBOL(__netdev_pick_tx);
 
 struct netdev_queue *netdev_pick_tx(struct net_device *dev,
 				    struct sk_buff *skb)
