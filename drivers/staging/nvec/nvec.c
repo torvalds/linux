1e54f0a21e7debcd8acb0fe39adc6f81751e1b57
Prashant Gaikwad <pgaikwad@nvidia.com>
2013-01-11 13:31:24 +0530

    staging: nvec: remove use of clk_get_sys
    As clock information is added to device tree clock can be looked up
    using clk_get. Remove use of clk_get_sys.
    
    Signed-off-by: Prashant Gaikwad <pgaikwad@nvidia.com>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Acked-by: Marc Dietrich <marvin24@gmx.de>
    Acked-by: Julian Andres Klode <jak@jak-linux.org>
    [swarren: updated TODO file to remove entry that requested this change]
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- drivers/staging/nvec/nvec.c
+++ drivers/staging/nvec/nvec.c
@@ -770,7 +770,7 @@ static int tegra_nvec_probe(struct platform_device *pdev)
 		return -ENODEV;
 	}
 
-	i2c_clk = clk_get_sys("tegra-i2c.2", "div-clk");
+	i2c_clk = clk_get(&pdev->dev, "div-clk");
 	if (IS_ERR(i2c_clk)) {
 		dev_err(nvec->dev, "failed to get controller clock\n");
 		return -ENODEV;
