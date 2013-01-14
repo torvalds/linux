bb37f1223a7ba9e0abf21d5392d0bb4aedce78f5
Paul Gortmaker <paul.gortmaker@windriver.com>
2013-01-13 19:41:14 -0500

    drivers/net: delete old fujitsu based eth16i driver
    This is another driver for relatively rare 10Mbit hardware that
    originated in the early 1990's.  So we select it for removal at
    this point in time as well.
    
    Cc: Mika Kuoppala <miku@iki.fi>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/net/Space.c
+++ drivers/net/Space.c
@@ -45,7 +45,6 @@ extern struct net_device *ne_probe(int unit);
 extern struct net_device *hp_probe(int unit);
 extern struct net_device *hp_plus_probe(int unit);
 extern struct net_device *fmv18x_probe(int unit);
-extern struct net_device *eth16i_probe(int unit);
 extern struct net_device *i82596_probe(int unit);
 extern struct net_device *ewrk3_probe(int unit);
 extern struct net_device *e2100_probe(int unit);
@@ -138,9 +137,6 @@ static struct devprobe2 isa_probes[] __initdata = {
 #ifndef CONFIG_CS89x0_PLATFORM
  	{cs89x0_probe, 0},
 #endif
-#endif
-#ifdef CONFIG_ETH16I
-	{eth16i_probe, 0},	/* ICL EtherTeam 16i/32 */
 #endif
 #ifdef CONFIG_EWRK3             /* DEC EtherWORKS 3 */
     	{ewrk3_probe, 0},
