e43b3cec711a61edf047adf6204d542f3a659ef8
H. Peter Anvin <hpa@linux.intel.com>
2013-01-13 20:56:41 -0800

    x86/Sandy Bridge: Sandy Bridge workaround depends on CONFIG_PCI
    early_pci_allowed() and read_pci_config_16() are only available if
    CONFIG_PCI is defined.
    
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>
    Cc: Jesse Barnes <jbarnes@virtuousgeek.org>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -612,6 +612,7 @@ static unsigned reserve_low = CONFIG_X86_RESERVE_LOW << 10;
 
 static bool __init snb_gfx_workaround_needed(void)
 {
+#ifdef CONFIG_PCI
 	int i;
 	u16 vendor, devid;
 	static const __initconst u16 snb_ids[] = {
@@ -636,6 +637,7 @@ static bool __init snb_gfx_workaround_needed(void)
 	for (i = 0; i < ARRAY_SIZE(snb_ids); i++)
 		if (devid == snb_ids[i])
 			return true;
+#endif
 
 	return false;
 }
