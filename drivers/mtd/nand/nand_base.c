0ce82b7f7b7373b16ecf7b5725e21e2975204500
Matthieu CASTET <matthieu.castet@parrot.com>
2013-01-16 15:25:45 +0100

    mtd: nand: onfi don't WARN if we are in 16 bits mode
    Commit ff3206b2450499203532af2505a7f6f8413e92c0 ('mtd: nand: onfi need 
    to be probed in 8 bits mode') adds a WARN if the onfi probe is in 16 
    bits mode. This allows to detect driver that need to be fixed, but this 
    is a bit noisy¹. Transform the WARN in a pr_err.
    
    ¹ http://article.gmane.org/gmane.linux.ports.arm.omap/91317
    
    Signed-off-by: Matthieu CASTET <matthieu.castet@parrot.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/nand/nand_base.c
+++ drivers/mtd/nand/nand_base.c
@@ -2857,8 +2857,11 @@ static int nand_flash_detect_onfi(struct mtd_info *mtd, struct nand_chip *chip,
 	int i;
 	int val;
 
-	/* ONFI need to be probed in 8 bits mode */
-	WARN_ON(chip->options & NAND_BUSWIDTH_16);
+	/* ONFI need to be probed in 8 bits mode, and 16 bits should be selected with NAND_BUSWIDTH_AUTO */
+	if (chip->options & NAND_BUSWIDTH_16) {
+		pr_err("Trying ONFI probe in 16 bits mode, aborting !\n");
+		return 0;
+	}
 	/* Try ONFI for unknown chip or LP */
 	chip->cmdfunc(mtd, NAND_CMD_READID, 0x20, -1);
 	if (chip->read_byte(mtd) != 'O' || chip->read_byte(mtd) != 'N' ||
