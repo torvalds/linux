85eb7ec1565e9a57342bee4fb687b16137f3b3a0
sarcastillo <sar.castillo@gmail.com>
2013-01-16 15:46:07 +0800

    kernel/timer: Optimise apply_slack() for size and speed.
    To apply proper slack, the original algorithm used to prepare a mask and
    then apply the mask to obtain the appropriately rounded-off absolute
    time the timer expires.
    
    This patch modifies the masking logic to a bit-shift logic, therby
    reducing the complexity and number of operations. Thus obtaining a minor
    speed-up.
    
    Signed-off-by: Chinmay V S <chinmay.v.s@pathpartnertech.com>
    Signed-off-by: sarcastillo <sar.castillo@gmail.com>

--- kernel/timer.c
+++ kernel/timer.c
@@ -741,9 +741,7 @@ EXPORT_SYMBOL(mod_timer_pending);
  * Algorithm:
  *   1) calculate the maximum (absolute) time
  *   2) calculate the highest bit where the expires and new max are different
- *   3) use this bit to make a mask
- *   4) use the bitmask to round down the maximum time, so that all last
- *      bits are zeros
+ *   3) round down the maximum time, so that all the lower bits are zeros
  */
 static inline
 unsigned long apply_slack(struct timer_list *timer, unsigned long expires)
@@ -767,9 +765,7 @@ unsigned long apply_slack(struct timer_list *timer, unsigned long expires)
 
 	bit = find_last_bit(&mask, BITS_PER_LONG);
 
-	mask = (1 << bit) - 1;
-
-	expires_limit = expires_limit & ~(mask);
+	expires_limit = (expires_limit >> bit) << bit;
 
 	return expires_limit;
 }
