fa1e492aa3cbafba9f8fc6d05e5b08a3091daf4a
Steffen Klassert <steffen.klassert@secunet.com>
2013-01-16 20:58:10 +0000

    ipv4: Don't update the pmtu on mtu locked routes
    Routes with locked mtu should not use learned pmtu informations,
    so do not update the pmtu on these routes.
    
    Reported-by: Julian Anastasov <ja@ssi.bg>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -912,6 +912,9 @@ static void __ip_rt_update_pmtu(struct rtable *rt, struct flowi4 *fl4, u32 mtu)
 	struct dst_entry *dst = &rt->dst;
 	struct fib_result res;
 
+	if (dst_metric_locked(dst, RTAX_MTU))
+		return;
+
 	if (dst->dev->mtu < mtu)
 		return;
 
