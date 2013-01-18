f494d4ebdf61c973b267c1636d4c5abeeeefce16
张晴 <zhangqing@rock-chips.com>
2013-01-18 16:09:26 +0800

    rk3168_tb:support rtc_hym8563

--- arch/arm/mach-rk30/board-rk3168-tb.c
+++ arch/arm/mach-rk30/board-rk3168-tb.c
@@ -1649,6 +1649,15 @@ static struct i2c_board_info __initdata i2c1_info[] = {
 		.platform_data=&act8846_data,
 	},
 #endif
+#if defined (CONFIG_RTC_HYM8563)
+	{
+		.type                   = "rtc_hym8563",
+		.addr           = 0x51,
+		.flags                  = 0,
+		.irq            = RK30_PIN1_PA4,
+	},
+#endif
+
 };
 #endif
 
