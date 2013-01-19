26365625947fb7d6501065272a1fd59460c0f4ed
John Crispin <blogic@openwrt.org>
2013-01-19 08:54:27 +0000

    MIPS: lantiq: rework external irq code
    This code makes the irqs used by the EIU loadable from the DT. Additionally we
    add a helper that allows the pinctrl layer to map external irqs to real irq
    numbers.
    
    Signed-off-by: John Crispin <blogic@openwrt.org>
    Patchwork: http://patchwork.linux-mips.org/patch/4818/

--- arch/mips/include/asm/mach-lantiq/lantiq.h
+++ arch/mips/include/asm/mach-lantiq/lantiq.h
@@ -34,6 +34,7 @@ extern spinlock_t ebu_lock;
 extern void ltq_disable_irq(struct irq_data *data);
 extern void ltq_mask_and_ack_irq(struct irq_data *data);
 extern void ltq_enable_irq(struct irq_data *data);
+extern int ltq_eiu_get_irq(int exin);
 
 /* clock handling */
 extern int clk_activate(struct clk *clk);
