65dcf729491948eab570d5537b0867f6180bf601
黄涛 <huangtao@rock-chips.com>
2013-01-21 10:13:40 +0800

    rk: cpu.h: fix gcc warning for cpu_is_rk3066b

--- arch/arm/plat-rk/include/plat/cpu.h
+++ arch/arm/plat-rk/include/plat/cpu.h
@@ -41,14 +41,14 @@ static inline bool cpu_is_rk30xx(void)
 
 static inline bool cpu_is_rk3066b(void)
 {
-	return readl_relaxed(RK30_ROM_BASE + 0x27f0) == 0x33303041
+	return(readl_relaxed(RK30_ROM_BASE + 0x27f0) == 0x33303041
 	    && readl_relaxed(RK30_ROM_BASE + 0x27f4) == 0x32303131
 	    && readl_relaxed(RK30_ROM_BASE + 0x27f8) == 0x31313131
-	    && readl_relaxed(RK30_ROM_BASE + 0x27fc) == 0x56313030
-	    || readl_relaxed(RK30_ROM_BASE + 0x27f0) == 0x33303042
+	    && readl_relaxed(RK30_ROM_BASE + 0x27fc) == 0x56313030)
+	    ||(readl_relaxed(RK30_ROM_BASE + 0x27f0) == 0x33303042
 	    && readl_relaxed(RK30_ROM_BASE + 0x27f4) == 0x32303132
 	    && readl_relaxed(RK30_ROM_BASE + 0x27f8) == 0x31303031
-	    && readl_relaxed(RK30_ROM_BASE + 0x27fc) == 0x56313030;
+	    && readl_relaxed(RK30_ROM_BASE + 0x27fc) == 0x56313030);
 }
 
 static inline bool cpu_is_rk3188(void)
