8bb43d5d42b268d4c5421c0b555e00a1090c2788
Mauro Ribeiro <git@mdrjr.net>
2013-01-21 13:41:03 -0200

    HardkernelUMP: Moved the ioctl's addresses. Those are already in use by s3c-mem

--- include/video/hardkernel_ump.h
+++ include/video/hardkernel_ump.h
@@ -18,8 +18,8 @@
  */
 
 
-#define GET_UMP_SECURE_ID_BUF1   _IOWR('m', 311, unsigned int)
-#define GET_UMP_SECURE_ID_BUF2   _IOWR('m', 312, unsigned int)
+#define GET_UMP_SECURE_ID_BUF1   _IOWR('m', 331, unsigned int)
+#define GET_UMP_SECURE_ID_BUF2   _IOWR('m', 332, unsigned int)
 
 extern int (*disp_get_ump_secure_id) (struct fb_info *info, unsigned long arg, int buf);
 
