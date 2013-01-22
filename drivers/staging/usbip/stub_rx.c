dcf14779280215d1765141ce04c675df3e4301c5
Harvey Yang <harvey.huawei.yang@gmail.com>
2013-01-22 13:31:30 +0800

    staging: usbip: use interrupt safe spinlock to avoid potential deadlock.
    The function 'usbip_event_add()' may be called in interrupt context on
    the stub side:
    'stub_complete'->'stub_enqueue_ret_unlink'->'usbip_event_add'.
    In this function it tries to get the lock 'ud->lock', so we should
    disable irq when we get this lock in process context.
    
    Signed-off-by: Harvey Yang <harvey.huawei.yang@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/usbip/stub_rx.c
+++ drivers/staging/usbip/stub_rx.c
@@ -307,12 +307,12 @@ static int valid_request(struct stub_device *sdev, struct usbip_header *pdu)
 	int valid = 0;
 
 	if (pdu->base.devid == sdev->devid) {
-		spin_lock(&ud->lock);
+		spin_lock_irq(&ud->lock);
 		if (ud->status == SDEV_ST_USED) {
 			/* A request is valid. */
 			valid = 1;
 		}
-		spin_unlock(&ud->lock);
+		spin_unlock_irq(&ud->lock);
 	}
 
 	return valid;
