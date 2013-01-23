fbadc58dd3a52c330c8f3926aa93011bf9d91fa0
ShuoX Liu <shuox.liu@intel.com>
2013-01-23 21:49:37 +0100

    PM / Runtime: Add new helper function: pm_runtime_active()
    This boolean function simply returns whether or not the runtime
    status of the device is 'active'. The typical scenario is driver
    calls pm_runtime_get firstly, then check pm_runtime_active in
    atomic environment.
    
    Also add entry to Documentation/power/runtime.txt
    
    Signed-off-by: Yanmin Zhang <yanmin.zhang@intel.com>
    Signed-off-by: ShuoX Liu <shuox.liu@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/linux/pm_runtime.h
+++ include/linux/pm_runtime.h
@@ -80,6 +80,12 @@ static inline bool pm_runtime_suspended(struct device *dev)
 		&& !dev->power.disable_depth;
 }
 
+static inline bool pm_runtime_active(struct device *dev)
+{
+	return dev->power.runtime_status == RPM_ACTIVE
+		|| dev->power.disable_depth;
+}
+
 static inline bool pm_runtime_status_suspended(struct device *dev)
 {
 	return dev->power.runtime_status == RPM_SUSPENDED;
@@ -132,6 +138,7 @@ static inline void pm_runtime_put_noidle(struct device *dev) {}
 static inline bool device_run_wake(struct device *dev) { return false; }
 static inline void device_set_run_wake(struct device *dev, bool enable) {}
 static inline bool pm_runtime_suspended(struct device *dev) { return false; }
+static inline bool pm_runtime_active(struct device *dev) { return true; }
 static inline bool pm_runtime_status_suspended(struct device *dev) { return false; }
 static inline bool pm_runtime_enabled(struct device *dev) { return false; }
 
