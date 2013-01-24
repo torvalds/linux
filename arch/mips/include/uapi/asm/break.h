5b003119ab6a12f03ab6ec43a1c921c9469945a4
Ralf Baechle <ralf@linux-mips.org>
2013-01-24 22:01:48 +0100

    MIPS: Cleanup break and trap codes.
    Very ancient out-of-tree KDB versions were using BRK_KDB code but it's
    unused in modern kernels since a long time.  Delete it.
    
    The microMIPS encoding only reserves 4 bits for a trap code so it's time
    for further weedkilling.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/uapi/asm/break.h
+++ arch/mips/include/uapi/asm/break.h
@@ -16,19 +16,11 @@
  * non-Linux/MIPS object files or make use of them in the future.
  */
 #define BRK_USERBP	0	/* User bp (used by debuggers) */
-#define BRK_KERNELBP	1	/* Break in the kernel */
-#define BRK_ABORT	2	/* Sometimes used by abort(3) to SIGIOT */
-#define BRK_BD_TAKEN	3	/* For bd slot emulation - not implemented */
-#define BRK_BD_NOTTAKEN 4	/* For bd slot emulation - not implemented */
 #define BRK_SSTEPBP	5	/* User bp (used by debuggers) */
 #define BRK_OVERFLOW	6	/* Overflow check */
 #define BRK_DIVZERO	7	/* Divide by zero check */
 #define BRK_RANGE	8	/* Range error check */
-#define BRK_STACKOVERFLOW 9	/* For Ada stackchecking */
-#define BRK_NORLD	10	/* No rld found - not used by Linux/MIPS */
-#define _BRK_THREADBP	11	/* For threads, user bp (used by debuggers) */
 #define BRK_BUG		12	/* Used by BUG() */
-#define BRK_KDB		513	/* Used in KDB_ENTER() */
 #define BRK_MEMU	514	/* Used by FPU emulator */
 #define BRK_KPROBE_BP	515	/* Kprobe break */
 #define BRK_KPROBE_SSTEPBP 516	/* Kprobe single step software implementation */
