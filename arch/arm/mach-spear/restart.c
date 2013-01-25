af63275bc4da161f4c78c8ddcddc9f90c2d2d194
Arnd Bergmann <arnd@arndb.de>
2013-01-25 23:09:57 +0000

    ARM: spear: fix build error in restart.c
    We can now enable mach-spear without selecting any of the
    machines in a multiplatform configuration.
    Doing so causes a build error that is trivial to fix by
    making both the spear13xx and the spear3xx/6xx portion of
    this file conditional rather than alternatives.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>

--- arch/arm/mach-spear/restart.c
+++ arch/arm/mach-spear/restart.c
@@ -26,7 +26,8 @@ void spear_restart(char mode, const char *cmd)
 		/* hardware reset, Use on-chip reset capability */
 #ifdef CONFIG_ARCH_SPEAR13XX
 		writel_relaxed(0x01, SPEAR13XX_SYS_SW_RES);
-#else
+#endif
+#if defined(CONFIG_ARCH_SPEAR3XX) || defined(CONFIG_ARCH_SPEAR6XX)
 		sysctl_soft_reset((void __iomem *)VA_SPEAR_SYS_CTRL_BASE);
 #endif
 	}
