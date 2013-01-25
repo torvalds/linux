9d5f78fbbf924492001ac7b2915537968906e517
Ville Syrjälä <ville.syrjala@linux.intel.com>
2013-01-25 21:44:43 +0200

    drm/i915: VLV doesn't have SDVO
    Don't call intel_sdvo_init() for VLV.
    
    Preserve the same behaviour as when intel_sdvo_init() would
    have returned false.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -8227,18 +8227,13 @@ static void intel_setup_outputs(struct drm_device *dev)
 		if (I915_READ(PCH_DP_D) & DP_DETECTED)
 			intel_dp_init(dev, PCH_DP_D, PORT_D);
 	} else if (IS_VALLEYVIEW(dev)) {
-		int found;
-
 		/* Check for built-in panel first. Shares lanes with HDMI on SDVOC */
 		if (I915_READ(DP_C) & DP_DETECTED)
 			intel_dp_init(dev, DP_C, PORT_C);
 
 		if (I915_READ(SDVOB) & PORT_DETECTED) {
-			/* SDVOB multiplex with HDMIB */
-			found = intel_sdvo_init(dev, SDVOB, true);
-			if (!found)
-				intel_hdmi_init(dev, SDVOB, PORT_B);
-			if (!found && (I915_READ(DP_B) & DP_DETECTED))
+			intel_hdmi_init(dev, SDVOB, PORT_B);
+			if (I915_READ(DP_B) & DP_DETECTED)
 				intel_dp_init(dev, DP_B, PORT_B);
 		}
 
