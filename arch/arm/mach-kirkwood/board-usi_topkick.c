0dbe6ce0373394ff79744684b78e37bc4430daac
Jason Cooper <jason@lakedaemon.net>
2013-01-26 20:50:10 +0000

    ARM: kirkwood: topkick: init mvsdio via DT
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>
    Tested-by: Andrew Lunn <andrew@lunn.ch>

--- arch/arm/mach-kirkwood/board-usi_topkick.c
+++ arch/arm/mach-kirkwood/board-usi_topkick.c
@@ -14,15 +14,10 @@
 #include <linux/init.h>
 #include <linux/mv643xx_eth.h>
 #include <linux/gpio.h>
-#include <linux/platform_data/mmc-mvsdio.h>
 #include "common.h"
 
 static struct mv643xx_eth_platform_data topkick_ge00_data = {
 	.phy_addr	= MV643XX_ETH_PHY_ADDR(0),
-};
-
-static struct mvsdio_platform_data topkick_mvsdio_data = {
-	/* unfortunately the CD signal has not been connected */
 };
 
 void __init usi_topkick_init(void)
@@ -31,5 +26,4 @@ void __init usi_topkick_init(void)
 	 * Basic setup. Needs to be called early.
 	 */
 	kirkwood_ge00_init(&topkick_ge00_data);
-	kirkwood_sdio_init(&topkick_mvsdio_data);
 }
