0cc9d08d3dca174ee712044e299732f26fdab22d
Mark Brown <broonie@opensource.wolfsonmicro.com>
2013-01-27 12:07:23 +0800

    mfd: wm5102: Mark DSP memory regions as volatile and readable
    We can cache some of them but this is simpler for now.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/wm5102-tables.c
+++ drivers/mfd/wm5102-tables.c
@@ -1826,7 +1826,13 @@ static bool wm5102_readable_register(struct device *dev, unsigned int reg)
 	case ARIZONA_DSP1_STATUS_3:
 		return true;
 	default:
-		return false;
+		if ((reg >= 0x100000 && reg < 0x106000) ||
+		    (reg >= 0x180000 && reg < 0x180800) ||
+		    (reg >= 0x190000 && reg < 0x194800) ||
+		    (reg >= 0x1a8000 && reg < 0x1a9800))
+			return true;
+		else
+			return false;
 	}
 }
 
