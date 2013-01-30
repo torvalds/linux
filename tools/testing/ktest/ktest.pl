35275685bf6123529e67c1dc91b8c05e479124e8
Steven Rostedt (Red Hat) <rostedt@goodmis.org>
2013-01-30 12:28:15 -0500

    ktest: Strip off '\n' when reading which files were modified
    The patchcheck test looks at what files are modified for each patch it
    checks and makes sure that those files do not produce any warnings.
    
    Unfortunately, when it read the diffstat, the newlines were added on the
    files and this made compares miss warnings, and commits that should not
    have passed, ktest let pass.
    
    Fix this by using the perl command "chomp" that strips off whitespace at
    the end of lines.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- tools/testing/ktest/ktest.pl
+++ tools/testing/ktest/ktest.pl
@@ -1925,6 +1925,10 @@ sub check_buildlog {
 
     my @files = `git show $patch | diffstat -l`;
 
+    foreach my $file (@files) {
+	chomp $file;
+    }
+
     open(IN, "git show $patch |") or
 	dodie "failed to show $patch";
     while (<IN>) {
