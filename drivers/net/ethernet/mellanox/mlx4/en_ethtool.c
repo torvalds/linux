69d7126b7fef09beaa4b7c103ec2a5bd8c8b2666
Hadar Hen Zion <hadarh@mellanox.com>
2013-01-30 23:07:05 +0000

    net/mlx4_en: Validate VLAN IDs provided in ethtool flow steering rules
    When attaching flow steering rules via Ethtool accept only valid vlans IDs e.g
    in the range: [0,4095].
    
    Signed-off-by: Hadar Hen Zion <hadarh@mellanox.com>
    Signed-off-by: Amir Vadai <amirv@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlx4/en_ethtool.c
+++ drivers/net/ethernet/mellanox/mlx4/en_ethtool.c
@@ -669,6 +669,10 @@ static int mlx4_en_validate_flow(struct net_device *dev,
 		    !(cmd->fs.m_ext.vlan_tci == 0 ||
 		      cmd->fs.m_ext.vlan_tci == cpu_to_be16(0xfff)))
 			return -EINVAL;
+		if (cmd->fs.m_ext.vlan_tci) {
+			if (be16_to_cpu(cmd->fs.h_ext.vlan_tci) >= VLAN_N_VID)
+				return -EINVAL;
+		}
 	}
 
 	return 0;
