a3f4002870df77f827f839fa85bab0534287a651
黄涛 <huangtao@rock-chips.com>
2013-01-31 11:12:06 +0800

    rk3188: no use Thumb instruction set to build pm.c and ddr.c

--- arch/arm/mach-rk3188/include/mach/memory.h
+++ arch/arm/mach-rk3188/include/mach/memory.h
@@ -10,6 +10,6 @@
 #define SRAM_CODE_OFFSET        (RK30_IMEM_BASE + 0x0100)
 #define SRAM_CODE_END           (RK30_IMEM_BASE + 0x0FFF)
 #define SRAM_DATA_OFFSET        (RK30_IMEM_BASE + 0x1000)
-#define SRAM_DATA_END           (RK30_IMEM_BASE + 0x1FFF - 64)
+#define SRAM_DATA_END           (RK30_IMEM_BASE + 0x2FFF - 64)
 
 #endif
