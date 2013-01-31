4f1d8cb8f13b1698fbc6d4a2e8b46a13973b1b6d
wuhao <wuhao@wuhao@rock-chips.com>
2013-01-31 12:11:19 +0800

    rk3188 ds1006h: fix l3g4200d & ak8963

--- arch/arm/mach-rk3188/board-rk3188-ds1006h.c
+++ arch/arm/mach-rk3188/board-rk3188-ds1006h.c
@@ -315,8 +315,8 @@ static int l3g4200d_init_platform_hw(void)
 static struct sensor_platform_data l3g4200d_info = {
 	.type = SENSOR_TYPE_GYROSCOPE,
 	.irq_enable = 1,
-	.poll_delay_ms = 30,
-	.orientation = {0, 1, 0, -1, 0, 0, 0, 0, 1},
+	.poll_delay_ms = 0,
+	.orientation = {1, 0, 0 , -1, 0, 0, 0, 0, -1},
 	.init_platform_hw = l3g4200d_init_platform_hw,
 	.x_min = 40,//x_min,y_min,z_min = (0-100) according to hardware
 	.y_min = 40,
