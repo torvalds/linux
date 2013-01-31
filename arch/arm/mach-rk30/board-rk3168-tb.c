cdb009261f917c9012ec9cecd4d437f3e453d4e7
黄涛 <huangtao@rock-chips.com>
2013-01-31 13:15:04 +0800

    rk3168: tb: enable ddr freq on video and suspend

--- arch/arm/mach-rk30/board-rk3168-tb.c
+++ arch/arm/mach-rk30/board-rk3168-tb.c
@@ -2018,8 +2018,8 @@ static struct cpufreq_frequency_table dvfs_gpu_table[] = {
 };
 
 static struct cpufreq_frequency_table dvfs_ddr_table[] = {
-//	{.frequency = 200 * 1000 + DDR_FREQ_SUSPEND,    .index = 950 * 1000},
-//	{.frequency = 300 * 1000 + DDR_FREQ_VIDEO,      .index = 1000 * 1000},
+	{.frequency = 200 * 1000 + DDR_FREQ_SUSPEND,    .index = 950 * 1000},
+	{.frequency = 300 * 1000 + DDR_FREQ_VIDEO,      .index = 1000 * 1000},
 	{.frequency = 396 * 1000 + DDR_FREQ_NORMAL,     .index = 1100 * 1000},
 	{.frequency = CPUFREQ_TABLE_END},
 };
