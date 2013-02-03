39bb356ee60581c3d28d6a636e5f221006d95f0c
Ruslan Bilovol <ruslan.bilovol@ti.com>
2013-02-03 02:15:10 +0200

    ARM: OMAP2+: mux: correct wrong error messages
    This is needed because the omap_mux_get_by_name()
    function calls the _omap_mux_get_by_name subfunction
    for each mux partition until needed mux is not found.
    As a result, we get messages like
    "Could not find signal XXX" for each partition
    where this mux name does not exist.
    
    This patch fixes wrong error message in
    the _omap_mux_get_by_name() function moving it
    to the omap_mux_get_by_name() one and as result
    reduces noise in the kernel log.
    
    My kernel log without this patch:
    [...]
    [    0.221801] omap_mux_init: Add partition: #2: wkup, flags: 3
    [    0.222045] _omap_mux_get_by_name: Could not find signal fref_clk0_out.sys_drm_msecure
    [    0.222137] _omap_mux_get_by_name: Could not find signal sys_nirq
    [    0.222167] _omap_mux_get_by_name: Could not find signal sys_nirq
    [    0.225006] _omap_mux_get_by_name: Could not find signal uart1_rx.uart1_rx
    [    0.225006] _omap_mux_get_by_name: Could not find signal uart1_rx.uart1_rx
    [    0.270111] _omap_mux_get_by_name: Could not find signal fref_clk4_out.fref_clk4_out
    [    0.273406] twl: not initialized
    
    [...]
    
    My kernel log with this patch:
    [...]
    [    0.221771] omap_mux_init: Add partition: #2: wkup, flags: 3
    [    0.222106] omap_mux_get_by_name: Could not find signal sys_nirq
    [    0.224945] omap_mux_get_by_name: Could not find signal uart1_rx.uart1_rx
    [    0.274536] twl: not initialized
    [...]
    
    Signed-off-by: Ruslan Bilovol <ruslan.bilovol@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/mux.c
+++ arch/arm/mach-omap2/mux.c
@@ -211,8 +211,6 @@ static int __init _omap_mux_get_by_name(struct omap_mux_partition *partition,
 		return -EINVAL;
 	}
 
-	pr_err("%s: Could not find signal %s\n", __func__, muxname);
-
 	return -ENODEV;
 }
 
@@ -233,6 +231,8 @@ int __init omap_mux_get_by_name(const char *muxname,
 
 		return mux_mode;
 	}
+
+	pr_err("%s: Could not find signal %s\n", __func__, muxname);
 
 	return -ENODEV;
 }
