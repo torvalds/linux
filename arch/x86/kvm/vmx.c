c08800a56cb8622bb61577abb4a120c6fdc4b9be
Dongxiao Xu <dongxiao.xu@intel.com>
2013-02-04 11:50:43 +0800

    KVM: VMX: disable SMEP feature when guest is in non-paging mode
    SMEP is disabled if CPU is in non-paging mode in hardware.
    However KVM always uses paging mode to emulate guest non-paging
    mode with TDP. To emulate this behavior, SMEP needs to be manually
    disabled when guest switches to non-paging mode.
    
    We met an issue that, SMP Linux guest with recent kernel (enable
    SMEP support, for example, 3.5.3) would crash with triple fault if
    setting unrestricted_guest=0. This is because KVM uses an identity
    mapping page table to emulate the non-paging mode, where the page
    table is set with USER flag. If SMEP is still enabled in this case,
    guest will meet unhandlable page fault and then crash.
    
    Reviewed-by: Gleb Natapov <gleb@redhat.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Dongxiao Xu <dongxiao.xu@intel.com>
    Signed-off-by: Xiantao Zhang <xiantao.zhang@intel.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -3227,6 +3227,14 @@ static int vmx_set_cr4(struct kvm_vcpu *vcpu, unsigned long cr4)
 		if (!is_paging(vcpu)) {
 			hw_cr4 &= ~X86_CR4_PAE;
 			hw_cr4 |= X86_CR4_PSE;
+			/*
+			 * SMEP is disabled if CPU is in non-paging mode in
+			 * hardware. However KVM always uses paging mode to
+			 * emulate guest non-paging mode with TDP.
+			 * To emulate this behavior, SMEP needs to be manually
+			 * disabled when guest switches to non-paging mode.
+			 */
+			hw_cr4 &= ~X86_CR4_SMEP;
 		} else if (!(cr4 & X86_CR4_PAE)) {
 			hw_cr4 &= ~X86_CR4_PAE;
 		}
