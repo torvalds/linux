61bc65599f8118fc53975670f19c37fe2be92a3d
Tobias Klauser <tklauser@distanz.ch>
2013-02-05 18:07:42 +0100

    tty: serial: altera_uart: Simplify altera_uart_init()
    No need for two separate return statements, consolidate them.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/altera_uart.c
+++ drivers/tty/serial/altera_uart.c
@@ -637,11 +637,9 @@ static int __init altera_uart_init(void)
 	if (rc)
 		return rc;
 	rc = platform_driver_register(&altera_uart_platform_driver);
-	if (rc) {
+	if (rc)
 		uart_unregister_driver(&altera_uart_driver);
-		return rc;
-	}
-	return 0;
+	return rc;
 }
 
 static void __exit altera_uart_exit(void)
