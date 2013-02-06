518f97dbb8d047f54c9f9f9b970fb42e1a6c8184
Kumar, Anil <anilkumar.v@ti.com>
2013-02-06 09:30:03 +0530

    ARM: davinci: da850: add wdt DT node
    Add da850 wdt DT node.
    Add OF_DEV_AUXDATA for wdt driver to use wdt clock.
    
    Signed-off-by: Kumar, Anil <anilkumar.v@ti.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- arch/arm/mach-davinci/da8xx-dt.c
+++ arch/arm/mach-davinci/da8xx-dt.c
@@ -39,6 +39,7 @@ static void __init da8xx_init_irq(void)
 
 struct of_dev_auxdata da850_auxdata_lookup[] __initdata = {
 	OF_DEV_AUXDATA("ti,davinci-i2c", 0x01c22000, "i2c_davinci.1", NULL),
+	OF_DEV_AUXDATA("ti,davinci-wdt", 0x01c21000, "watchdog", NULL),
 	{}
 };
 
