a865c50a64f1c0c27fc0be0652b6a352f537cb6f
Saurav Kashyap <saurav.kashyap@qlogic.com>
2013-02-08 01:57:43 -0500

    [SCSI] qla2xxx: Get VPD information from common location for CNA.
    Signed-off-by: Giridhar Malavali <giridhar.malavali@qlogic.com>
    Signed-off-by: Saurav Kashyap <saurav.kashyap@qlogic.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/qla2xxx/qla_fw.h
+++ drivers/scsi/qla2xxx/qla_fw.h
@@ -1388,9 +1388,7 @@ struct qla_flt_header {
 #define FLT_REG_FCP_PRIO_0	0x87
 #define FLT_REG_FCP_PRIO_1	0x88
 #define FLT_REG_FCOE_FW		0xA4
-#define FLT_REG_FCOE_VPD_0	0xA9
 #define FLT_REG_FCOE_NVRAM_0	0xAA
-#define FLT_REG_FCOE_VPD_1	0xAB
 #define FLT_REG_FCOE_NVRAM_1	0xAC
 
 struct qla_flt_region {
