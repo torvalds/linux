c07f670041058eff04bbceeea854f4b0fea55bda
Matthew Gerlach <mgerlach@altera.com>
2013-02-12 09:39:27 -0800

    FogBugz #99886: Clk name and DT node name conflict with DTG
    Change name of clock and device tree node from stmmac to ethernet
    
    Signed-off-by: Matthew Gerlach <mgerlach@altera.com>

--- drivers/clk/socfpga/clk.c
+++ drivers/clk/socfpga/clk.c
@@ -156,12 +156,12 @@ static __init struct clk *socfpga_clk_init(struct device_node *node,
 	clk = clk_register_gate(NULL, "gmac0_clk", "per_pll_clk", 0,
 			clk_mgr_base_addr + CLKMGR_PERPLLGRP_EN,
 			CLKMGR_EMAC0_CLK_EN, 0, &_lock);
-	clk_register_clkdev(clk, NULL, "ff700000.stmmac");
+	clk_register_clkdev(clk, NULL, "ff700000.ethernet");
 
 	clk = clk_register_gate(NULL, "gmac1_clk", "per_pll_clk", 0,
 			clk_mgr_base_addr + CLKMGR_PERPLLGRP_EN,
 			CLKMGR_EMAC1_CLK_EN, 0, &_lock);
-	clk_register_clkdev(clk, NULL, "ff702000.stmmac");
+	clk_register_clkdev(clk, NULL, "ff702000.ethernet");
 
 	clk = clk_register_gate(NULL, "spi0_clk", "per_pll_clk", 0,
 			clk_mgr_base_addr + CLKMGR_PERPLLGRP_EN,
