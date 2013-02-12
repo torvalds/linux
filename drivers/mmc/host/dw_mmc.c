774018803414a1cf6f2a84e00bc9dee51502e0d6
Dinh Nguyen <dinguyen@altera.com>
2013-02-12 15:20:24 -0600

    FogBugz #91445: Fix intermittent data starvation for DMA on SD/MMC
    This issue is related to FB #13909. We can also safely ignore the
    FIFO Under/Overrun error condition when the driver is using IDMAC.
    
    Signed-off-by: Dinh Nguyen <dinguyen@altera.com>

--- drivers/mmc/host/dw_mmc.c
+++ drivers/mmc/host/dw_mmc.c
@@ -42,17 +42,10 @@
 
 /* According to Synopsys, the data starvation interrupt (HTO) should not treat
  * as error. Software should continue the data transfer. We have verified this
- * in Virtual Target. Remove this fogbugz after verify this in actual
- * hardware.
+ * in Virtual Target. The same is applied to FIFO under/overrun (FRUN) as well.
  */
-#ifdef FOGBUGZ_13909
- #define DW_MCI_DATA_ERROR_FLAGS	(SDMMC_INT_DTO | SDMMC_INT_DCRC | \
- 				 SDMMC_INT_HTO | SDMMC_INT_SBE  | \
- 				 SDMMC_INT_EBE)
-#else
 #define DW_MCI_DATA_ERROR_FLAGS (SDMMC_INT_DTO | SDMMC_INT_DCRC | \
-				 SDMMC_INT_SBE | SDMMC_INT_EBE)
-#endif
+				 SDMMC_INT_HTO | SDMMC_INT_FRUN | SDMMC_INT_SBE | SDMMC_INT_EBE)
 
 #define DW_MCI_CMD_ERROR_FLAGS	(SDMMC_INT_RTO | SDMMC_INT_RCRC | \
 				 SDMMC_INT_RESP_ERR)
