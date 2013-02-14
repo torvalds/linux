01ffe957e2f0340a13fd40a6577d029f252ad7c7
Arnd Bergmann <arnd@arndb.de>
2013-02-14 23:11:33 +0100

    [media] s5p-fimc: fix s5pv210 build
    56bc911 "[media] s5p-fimc: Redefine platform data structure for fimc-is"
    changed the bus_type member of struct fimc_source_info treewide, but
    got one instance wrong in mach-s5pv210, which was evidently not
    even build tested.
    
    This adds the missing change to get s5pv210_defconfig to build again.
    Applies on the Mauro's media tree.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Cc: Kyungmin Park <kyungmin.park@samsung.com>
    Cc: Kukjin Kim <kgene.kim@samsung.com>
    Cc: Mauro Carvalho Chehab <mchehab@redhat.com>

--- arch/arm/mach-s5pv210/mach-goni.c
+++ arch/arm/mach-s5pv210/mach-goni.c
@@ -845,7 +845,7 @@ static struct fimc_source_info goni_camera_sensors[] = {
 		.mux_id		= 0,
 		.flags		= V4L2_MBUS_PCLK_SAMPLE_FALLING |
 				  V4L2_MBUS_VSYNC_ACTIVE_LOW,
-		.bus_type	= FIMC_BUS_TYPE_ITU_601,
+		.fimc_bus_type	= FIMC_BUS_TYPE_ITU_601,
 		.board_info	= &noon010pc30_board_info,
 		.i2c_bus_num	= 0,
 		.clk_frequency	= 16000000UL,
