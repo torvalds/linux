39b70ee05199f9bea50641df104aee4dbd913d1d
Haojian Zhuang <haojian.zhuang@linaro.org>
2013-02-17 19:42:51 +0800

    gpio: pl061: bind pinctrl by gpio request
    Add the pl061_gpio_request() to request pinctrl. Create the logic
    between pl061 gpio driver and pinctrl (pinctrl-single) driver.
    
    While a gpio pin is requested, it will request pinctrl driver to
    set that pin with gpio function mode. So pinctrl driver should
    append .gpio_request_enable() in pinmux_ops.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-pl061.c
+++ drivers/gpio/gpio-pl061.c
@@ -23,6 +23,7 @@
 #include <linux/amba/bus.h>
 #include <linux/amba/pl061.h>
 #include <linux/slab.h>
+#include <linux/pinctrl/consumer.h>
 #include <linux/pm.h>
 #include <asm/mach/irq.h>
 
@@ -59,6 +60,17 @@ struct pl061_gpio {
 	struct pl061_context_save_regs csave_regs;
 #endif
 };
+
+static int pl061_gpio_request(struct gpio_chip *chip, unsigned offset)
+{
+	/*
+	 * Map back to global GPIO space and request muxing, the direction
+	 * parameter does not matter for this controller.
+	 */
+	int gpio = chip->base + offset;
+
+	return pinctrl_request_gpio(gpio);
+}
 
 static int pl061_direction_input(struct gpio_chip *gc, unsigned offset)
 {
@@ -274,6 +286,7 @@ static int pl061_probe(struct amba_device *adev, const struct amba_id *id)
 
 	spin_lock_init(&chip->lock);
 
+	chip->gc.request = pl061_gpio_request;
 	chip->gc.direction_input = pl061_direction_input;
 	chip->gc.direction_output = pl061_direction_output;
 	chip->gc.get = pl061_get_value;
