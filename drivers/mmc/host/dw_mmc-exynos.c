20183d509cadfaee3b2b3ebe368431d566403c3d
Sachin Kamat <sachin.kamat@linaro.org>
2013-02-18 14:23:09 +0530

    mmc: dw_mmc: exynos: Remove unnecessary use of of_match_ptr()
    'dw_mci_exynos_match' is always compiled in. Hence of_match_ptr
    is not required.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Acked-by: Jaehoon Chung <jh80.chung@samsung.com>
    Acked-by: Seungwon Jeon <tgih.jun@samsung.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/dw_mmc-exynos.c
+++ drivers/mmc/host/dw_mmc-exynos.c
@@ -230,7 +230,7 @@ static struct platform_driver dw_mci_exynos_pltfm_driver = {
 	.remove		= __exit_p(dw_mci_pltfm_remove),
 	.driver		= {
 		.name		= "dwmmc_exynos",
-		.of_match_table	= of_match_ptr(dw_mci_exynos_match),
+		.of_match_table	= dw_mci_exynos_match,
 		.pm		= &dw_mci_pltfm_pmops,
 	},
 };
