d37204566a61d5116d385ae909db8e14a734b30f
Andrew Jones <drjones@redhat.com>
2013-02-18 10:29:20 +0000

    xen: netback: remove redundant xenvif_put
    netbk_fatal_tx_err() calls xenvif_carrier_off(), which does
    a xenvif_put(). As callers of netbk_fatal_tx_err should only
    have one reference to the vif at this time, then the xenvif_put
    in netbk_fatal_tx_err is one too many.
    
    Signed-off-by: Andrew Jones <drjones@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/xen-netback/netback.c
+++ drivers/net/xen-netback/netback.c
@@ -893,7 +893,6 @@ static void netbk_fatal_tx_err(struct xenvif *vif)
 {
 	netdev_err(vif->dev, "fatal error; disabling device\n");
 	xenvif_carrier_off(vif);
-	xenvif_put(vif);
 }
 
 static int netbk_count_requests(struct xenvif *vif,
