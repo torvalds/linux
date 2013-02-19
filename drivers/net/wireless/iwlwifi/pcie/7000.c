f4a7dfa3f21afc1b2362f19cebc79d3d7d5a9aee
Beni Lev <beni.lev@intel.com>
2013-02-19 14:30:16 +0200

    iwlwifi: 7000: disable HT greenfield support
    The 7000 series devices don't support HT greenfield mode
    so don't advertise or use it.
    
    Signed-off-by: Beni Lev <beni.lev@intel.com>
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/iwlwifi/pcie/7000.c
+++ drivers/net/wireless/iwlwifi/pcie/7000.c
@@ -107,7 +107,6 @@ static const struct iwl_base_params iwl7000_base_params = {
 };
 
 static const struct iwl_ht_params iwl7000_ht_params = {
-	.ht_greenfield_support = true,
 	.use_rts_for_aggregation = true, /* use rts/cts protection */
 	.ht40_bands = BIT(IEEE80211_BAND_2GHZ) | BIT(IEEE80211_BAND_5GHZ),
 };
