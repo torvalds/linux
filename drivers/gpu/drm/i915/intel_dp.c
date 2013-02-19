af13188a1a6623fc8b4b6c42178046fb80f8b1d0
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-02-19 17:45:00 +0100

    drm/i915: force bpp for eDP panels
    We've had our fair share of woes already which showed that we can't
    rely on the bpc limits in the EDID for eDP panels without risking
    black screens. So now we limit the depth by what the BIOS recommends
    in the VBT:
    
    commit 2f4f649a69a9eb51f6e98130e19dd90a260a4145
    Author: Jani Nikula <jani.nikula@intel.com>
    Date:   Mon Nov 12 14:33:44 2012 +0200
    
        drm/i915: do not ignore eDP bpc settings from vbt
    
    But that's not enough, since at least the panel on my ASUS Zenbook
    Prime here is also unhappy if the bpc is too low. Hence just take the
    firmware value and dither to get what flimsy panels want.
    
    Like before we ensure that we don't change the bpp if the firmware
    doesn't provide a value, see
    
    commit 9a30a61f3516871c5c638fd7c025fbaa11ddf7fe
    Author: Jani Nikula <jani.nikula@intel.com>
    Date:   Mon Nov 12 14:33:45 2012 +0200
    
        drm/i915: do not default to 18 bpp for eDP if missing from VBT
    
    v2: Apparently there are some horribly broken eDP panels around which
    only work if the DP link is set up as if we want to driver a 24bpp
    mode, but still only work if the data is feed at 18bpp. See
    
    commit 57c219633275c7e7413f8bc7be250dc092887458
    Author: Daniel Vetter <daniel.vetter@ffwll.ch>
    Date:   Thu Apr 4 17:19:37 2013 +0200
    
        drm/i915: revert eDP bpp clamping code changes
    
    for the gory details.
    
    Adjust the patch accordingly and update all the relevant comments.
    
    v3: Give up on the cargo-culting v2 attempt and just enfore the edp
    bpp value if it's there. Broken panels be damned!
    
    Cc: Jani Nikula <jani.nikula@intel.com>
    Cc: Paulo Zanoni <przanoni@gmail.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Tested-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -748,6 +748,18 @@ intel_dp_compute_config(struct intel_encoder *encoder,
 	/* Walk through all bpp values. Luckily they're all nicely spaced with 2
 	 * bpc in between. */
 	bpp = min_t(int, 8*3, pipe_config->pipe_bpp);
+
+	/*
+	 * eDP panels are really fickle, try to enfore the bpp the firmware
+	 * recomments. This means we'll up-dither 16bpp framebuffers on
+	 * high-depth panels.
+	 */
+	if (is_edp(intel_dp) && dev_priv->edp.bpp) {
+		DRM_DEBUG_KMS("forcing bpp for eDP panel to BIOS-provided %i\n",
+			      dev_priv->edp.bpp);
+		bpp = dev_priv->edp.bpp;
+	}
+
 	for (; bpp >= 6*3; bpp -= 2*3) {
 		mode_rate = intel_dp_link_required(target_clock, bpp);
 
@@ -797,18 +809,6 @@ found:
 			       target_clock, adjusted_mode->clock,
 			       &pipe_config->dp_m_n);
 
-	/*
-	 * XXX: We have a strange regression where using the vbt edp bpp value
-	 * for the link bw computation results in black screens, the panel only
-	 * works when we do the computation at the usual 24bpp (but still
-	 * requires us to use 18bpp). Until that's fully debugged, stay
-	 * bug-for-bug compatible with the old code.
-	 */
-	if (is_edp(intel_dp) && dev_priv->edp.bpp) {
-		DRM_DEBUG_KMS("clamping display bpc (was %d) to eDP (%d)\n",
-			      bpp, dev_priv->edp.bpp);
-		bpp = min_t(int, bpp, dev_priv->edp.bpp);
-	}
 	pipe_config->pipe_bpp = bpp;
 
 	intel_dp_set_clock(encoder, pipe_config, intel_dp->link_bw);
