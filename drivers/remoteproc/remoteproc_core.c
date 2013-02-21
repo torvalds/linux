cde42e076c1cdd69a1f955d66ff6596bad3d2105
Sjur Brændeland <sjur.brandeland@stericsson.com>
2013-02-21 18:15:32 +0100

    remoteproc: fix error path of handle_vdev
    Remove the vdev entry from the list before freeing it,
    otherwise rproc->vdevs will explode.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Sjur Brændeland <sjur.brandeland@stericsson.com>
    Acked-by: Ido Yariv <ido@wizery.com>
    [edit subject, minor commit log edit, cc stable]
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -366,10 +366,12 @@ static int rproc_handle_vdev(struct rproc *rproc, struct fw_rsc_vdev *rsc,
 	/* it is now safe to add the virtio device */
 	ret = rproc_add_virtio_dev(rvdev, rsc->id);
 	if (ret)
-		goto free_rvdev;
+		goto remove_rvdev;
 
 	return 0;
 
+remove_rvdev:
+	list_del(&rvdev->node);
 free_rvdev:
 	kfree(rvdev);
 	return ret;
