be9f97f04565a6c438b7521ad679870d25645475
David Herrmann <dh.herrmann@gmail.com>
2013-02-24 19:36:52 +0100

    Bluetooth: change bt_sock_unregister() to return void
    There is no reason a caller ever wants to check the return type of this
    call. _Iff_ a user successfully called bt_sock_register(), they're allowed
    to call bt_sock_unregister().
    All other calls in the kernel (device_del, device_unregister, kfree(), ..)
    that are logically equivalent return void. Lets not make callers think
    they have to check the return type of this call and instead simply return
    void.
    
    We guarantee that after bt_sock_unregister() is called, the socket type
    _is_ unregistered. If that is not what the caller wants, they're using the
    wrong function, anyway.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>

--- include/net/bluetooth/bluetooth.h
+++ include/net/bluetooth/bluetooth.h
@@ -232,7 +232,7 @@ struct bt_sock_list {
 };
 
 int  bt_sock_register(int proto, const struct net_proto_family *ops);
-int  bt_sock_unregister(int proto);
+void bt_sock_unregister(int proto);
 void bt_sock_link(struct bt_sock_list *l, struct sock *s);
 void bt_sock_unlink(struct bt_sock_list *l, struct sock *s);
 int  bt_sock_recvmsg(struct kiocb *iocb, struct socket *sock,
