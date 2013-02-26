98091839ad5b72f8aa2c9b22ed65cec6fa3bbf6f
Johannes Berg <johannes.berg@intel.com>
2013-02-26 22:37:57 +0100

    mac80211: always unblock CSA queue stop when disconnecting
    Commit 5b36ebd8249f403c7edf7cf68d68e9a0d0f55243 upstream.
    
    In some cases when disconnecting after (or during?) CSA
    the queues might not recover, and then the only way to
    recover is reloading the module.
    
    Fix this by always unblocking the queue CSA reason when
    
    disconnecting.
    
    Reported-by: Jan-Michael Brummer <jan.brummer@tabos.org>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -1812,6 +1812,8 @@ static void __ieee80211_disconnect(struct ieee80211_sub_if_data *sdata,
 			       WLAN_REASON_DISASSOC_DUE_TO_INACTIVITY,
 			       transmit_frame, frame_buf);
 	ifmgd->flags &= ~IEEE80211_STA_CSA_RECEIVED;
+	ieee80211_wake_queues_by_reason(&sdata->local->hw,
+					IEEE80211_QUEUE_STOP_REASON_CSA);
 	mutex_unlock(&ifmgd->mtx);
 
 	/*
@@ -1856,8 +1858,6 @@ static void ieee80211_csa_connection_drop_work(struct work_struct *work)
 		container_of(work, struct ieee80211_sub_if_data,
 			     u.mgd.csa_connection_drop_work);
 
-	ieee80211_wake_queues_by_reason(&sdata->local->hw,
-					IEEE80211_QUEUE_STOP_REASON_CSA);
 	__ieee80211_disconnect(sdata, true);
 }
 
