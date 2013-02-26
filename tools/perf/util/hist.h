1e825efb571a87d039cb3fe7ca833a25f11c7cb9
Michael Ellerman <michael@ellerman.id.au>
2013-02-26 16:02:02 +1100

    perf annotate: Fix build with NO_NEWT=1
    Commit 18c9e5c "Make it to be able to skip unannotatable symbols" broke
    the build with NO_NEWT=1:
    
       CC builtin-annotate.o
    builtin-annotate.c: In function 'hists__find_annotations':
    builtin-annotate.c:161:4: error: duplicate case value
    builtin-annotate.c:154:4: error: previously used here
    make: *** [builtin-annotate.o] Error 1
    
    This is because without NEWT support K_LEFT is #defined to -1 in
    utils/hist.h
    
    Fix it by shifting the K_LEFT/K_RIGHT #defines out of the likely range
    of error values.
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Cc: Feng Tang <feng.tang@intel.com>
    Cc: Namhyung Kim <namhyung.kim@lge.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Link: http://lkml.kernel.org/r/1361854923-1814-1-git-send-email-michael@ellerman.id.au
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/hist.h
+++ tools/perf/util/hist.h
@@ -208,8 +208,8 @@ static inline int script_browse(const char *script_opt __maybe_unused)
 	return 0;
 }
 
-#define K_LEFT -1
-#define K_RIGHT -2
+#define K_LEFT  -1000
+#define K_RIGHT -2000
 #endif
 
 #ifdef GTK2_SUPPORT
