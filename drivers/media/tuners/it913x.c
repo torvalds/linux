8c1285a28084b5fa07a7842dd72a561f5c3b913e
Antti Palosaari <crope@iki.fi>
2013-02-26 20:42:21 -0300

    [media] it913x: merge it913x_fe_suspend() to it913x_fe_sleep()
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/tuners/it913x.c
+++ drivers/media/tuners/it913x.c
@@ -401,36 +401,6 @@ static int it9137_set_tuner(struct dvb_frontend *fe)
 	return (ret < 0) ? -ENODEV : 0;
 }
 
-static int it913x_fe_suspend(struct it913x_fe_state *state)
-{
-	int ret = 0;
-	return 0;
-#if 0
-	int ret, i;
-	u8 b;
-
-	ret = it913x_write_reg(state, PRO_DMOD, SUSPEND_FLAG, 0x1);
-
-	ret |= it913x_write_reg(state, PRO_DMOD, TRIGGER_OFSM, 0x0);
-
-	for (i = 0; i < 128; i++) {
-		ret = it913x_read_reg(state, SUSPEND_FLAG, &b, 1);
-		if (ret < 0)
-			return -ENODEV;
-		if (b == 0)
-			break;
-
-	}
-
-	ret |= it913x_write_reg(state, PRO_DMOD, AFE_MEM0, 0x8);
-	/* Turn LED off */
-	ret |= it913x_write_reg(state, PRO_LINK, GPIOH3_O, 0x0);
-#endif
-	ret |= it913x_fe_script_loader(state, it9137_tuner_off);
-
-	return (ret < 0) ? -ENODEV : 0;
-}
-
 /* Power sequence */
 /* Power Up	Tuner on -> Frontend suspend off -> Tuner clk on */
 /* Power Down	Frontend suspend on -> Tuner clk off -> Tuner off */
@@ -438,7 +408,7 @@ static int it913x_fe_suspend(struct it913x_fe_state *state)
 static int it913x_fe_sleep(struct dvb_frontend *fe)
 {
 	struct it913x_fe_state *state = fe->tuner_priv;
-	return it913x_fe_suspend(state);
+	return it913x_fe_script_loader(state, it9137_tuner_off);
 }
 
 static int it913x_release(struct dvb_frontend *fe)
