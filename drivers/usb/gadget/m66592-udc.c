4b282fbe97412cc06fd9f173b4318e69a90b3442
Felipe Balbi <balbi@ti.com>
2013-02-26 15:15:26 +0200

    usb: gadget: m66592-udc: don't assign gadget.dev.release directly
    udc-core provides a better way to handle release
    methods, let's use it.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/m66592-udc.c
+++ drivers/usb/gadget/m66592-udc.c
@@ -1606,7 +1606,6 @@ static int __init m66592_probe(struct platform_device *pdev)
 
 	m66592->gadget.ops = &m66592_gadget_ops;
 	m66592->gadget.max_speed = USB_SPEED_HIGH;
-	m66592->gadget.dev.release = pdev->dev.release;
 	m66592->gadget.name = udc_name;
 
 	init_timer(&m66592->timer);
