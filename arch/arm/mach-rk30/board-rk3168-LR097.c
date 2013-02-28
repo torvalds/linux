f60b4cf0c4a99e3e0cb27cf1cbd814922774a4a6
yxj <yxj@rock-chips.com>
2013-02-28 17:58:46 +0800

    LR097:compatile with gsensor lis3dh

--- arch/arm/mach-rk30/board-rk3168-LR097.c
+++ arch/arm/mach-rk30/board-rk3168-LR097.c
@@ -415,7 +415,7 @@ static struct sensor_platform_data lis3dh_info = {
 	.irq_enable = 1,
 	.poll_delay_ms = 30,
         .init_platform_hw = lis3dh_init_platform_hw,
-	.orientation = {-1, 0, 0, 0, 0, 1, 0, -1, 0},
+	.orientation = {1, 0, 0, 0, 0, -1, 0, -1, 0},
 };
 #endif
 #if defined(CONFIG_CHARGER_CW2015)
