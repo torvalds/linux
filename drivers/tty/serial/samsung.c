a0428578db4ff28a86caec98629f2cc884f497d3
JP Abgrall <jpa@google.com>
2013-03-01 18:28:21 -0800

    serial: samsung: fix leaked requested irq
    The s3c64xx_serial_startup() requests an IRQ, but the common
    s3c24xx_serial_shutdown() would not free it.
    
    s3c64xx_serial_startup() is used when
    s3c24xx_serial_has_interrupt_mask(port) is true, at which point
      ourport->rx_claimed = ourport->tx_claimed = 1
    so we only need to free in one of the claimed cases.
    
    Change-Id: I8e17ce52cf48dbf3faa26ac24c8a71f016771f32
    Signed-off-by: JP Abgrall <jpa@google.com>

--- drivers/tty/serial/samsung.c
+++ drivers/tty/serial/samsung.c
@@ -427,6 +427,8 @@ static void s3c24xx_serial_shutdown(struct uart_port *port)
 	if (ourport->tx_claimed) {
 		if (!s3c24xx_serial_has_interrupt_mask(port))
 			free_irq(ourport->tx_irq, ourport);
+		else
+			free_irq(port->irq, ourport);
 		tx_enabled(port) = 0;
 		ourport->tx_claimed = 0;
 	}
@@ -434,6 +436,8 @@ static void s3c24xx_serial_shutdown(struct uart_port *port)
 	if (ourport->rx_claimed) {
 		if (!s3c24xx_serial_has_interrupt_mask(port))
 			free_irq(ourport->rx_irq, ourport);
+		/* else already freed above as the s3c64xx_serial_startup()
+		 * will have set both tx_claimed and rx_claimed */
 		ourport->rx_claimed = 0;
 		rx_enabled(port) = 0;
 	}
