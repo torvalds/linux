badd9b71c5651a687d042966a5fa8d195e9cffe4
Gustavo Maciel Dias Vieira <gustavo@sagui.org>
2013-03-04 15:23:37 +0000

    ACPI video: ignore BIOS backlight value for HP dm4
    commit 771d09b3c4c45d4d534a83a68e6331b97fd82e15 upstream.
    
    On a HP Pavilion dm4 laptop the BIOS sets minimum backlight on boot,
    completely dimming the screen. Ignore this initial value for this
    machine.
    
    Signed-off-by: Gustavo Maciel Dias Vieira <gustavo@sagui.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    [wyj: Backported to 3.4: adjust context]
    Signed-off-by: Yijing Wang <wangyijing@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/acpi/video.c
+++ drivers/acpi/video.c
@@ -471,6 +471,14 @@ static struct dmi_system_id video_dmi_table[] __initdata = {
 		DMI_MATCH(DMI_PRODUCT_NAME, "HP 1000 Notebook PC"),
 		},
 	},
+	{
+	 .callback = video_ignore_initial_backlight,
+	 .ident = "HP Pavilion dm4",
+	 .matches = {
+		DMI_MATCH(DMI_BOARD_VENDOR, "Hewlett-Packard"),
+		DMI_MATCH(DMI_PRODUCT_NAME, "HP Pavilion dm4 Notebook PC"),
+		},
+	},
 	{}
 };
 
