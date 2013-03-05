d3929b832833db870af72479666fa4e4d043e02e
Dirk Brandewie <dirk.brandewie@gmail.com>
2013-03-05 14:15:26 -0800

    cpufreq / intel_pstate: Do not load on VM that does not report max P state.
    It seems some VMs support the P state MSRs but return zeros. Fail
    gracefully if we are running in this environment.
    
    References: https://bugzilla.redhat.com/show_bug.cgi?id=916833
    Reported-by: Josh Boyer <jwboyer@redhat.com>
    Signed-off-by: Dirk Brandewie <dirk.j.brandewie@intel.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -662,6 +662,9 @@ static int intel_pstate_set_policy(struct cpufreq_policy *policy)
 
 	cpu = all_cpu_data[policy->cpu];
 
+	if (!policy->cpuinfo.max_freq)
+		return -ENODEV;
+
 	intel_pstate_get_min_max(cpu, &min, &max);
 
 	limits.min_perf_pct = (policy->min * 100) / policy->cpuinfo.max_freq;
