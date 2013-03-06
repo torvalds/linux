3c4aff6b9a183b4f24eb7b8dd6c8a92cdba3bc75
Peter Jones <pjones@redhat.com>
2013-03-06 13:00:23 -0500

    x86, doc: Be explicit about what the x86 struct boot_params requires
    If the sentinel triggers, we do not want the boot loader authors to
    just poke it and make the error go away, we want them to actually fix
    the problem.
    
    This should help avoid making the incorrect change in non-compliant
    bootloaders.
    
    [ hpa: dropped the Documentation/x86/boot.txt hunk pending
      clarifications ]
    
    Signed-off-by: Peter Jones <pjones@redhat.com>
    Link: http://lkml.kernel.org/r/1362592823-28967-1-git-send-email-pjones@redhat.com
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/include/asm/bootparam_utils.h
+++ arch/x86/include/asm/bootparam_utils.h
@@ -19,8 +19,22 @@
  */
 static void sanitize_boot_params(struct boot_params *boot_params)
 {
+	/* 
+	 * IMPORTANT NOTE TO BOOTLOADER AUTHORS: do not simply clear
+	 * this field.  The purpose of this field is to guarantee
+	 * compliance with the x86 boot spec located in
+	 * Documentation/x86/boot.txt .  That spec says that the
+	 * *whole* structure should be cleared, after which only the
+	 * portion defined by struct setup_header (boot_params->hdr)
+	 * should be copied in.
+	 *
+	 * If you're having an issue because the sentinel is set, you
+	 * need to change the whole structure to be cleared, not this
+	 * (or any other) individual field, or you will soon have
+	 * problems again.
+	 */
 	if (boot_params->sentinel) {
-		/*fields in boot_params are not valid, clear them */
+		/* fields in boot_params are left uninitialized, clear them */
 		memset(&boot_params->olpc_ofw_header, 0,
 		       (char *)&boot_params->efi_info -
 			(char *)&boot_params->olpc_ofw_header);
