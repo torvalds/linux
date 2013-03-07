85faa17c6abc754dac48a82f3d9bc70dd1c7453b
Ley Foon Tan <lftan@altera.com>
2013-03-07 10:28:37 +0800

    tty/serial: Add support for Altera serial port
    commit e06c93cacb82dd147266fd1bdb2d0a0bd45ff2c1 upstream.
    
    Add support for Altera 8250/16550 compatible serial port.
    
    Signed-off-by: Ley Foon Tan <lftan@altera.com>
    [xr: Backported to 3.4: adjust filenames, context]
    Signed-off-by: Rui Xiang <rui.xiang@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/serial_core.h
+++ include/linux/serial_core.h
@@ -48,7 +48,10 @@
 #define PORT_TEGRA	20	/* NVIDIA Tegra internal UART */
 #define PORT_XR17D15X	21	/* Exar XR17D15x UART */
 #define PORT_BRCM_TRUMANAGE	25
-#define PORT_MAX_8250	25	/* max port ID */
+#define PORT_ALTR_16550_F32 26 /* Altera 16550 UART with 32 FIFOs */
+#define PORT_ALTR_16550_F64 27 /* Altera 16550 UART with 64 FIFOs */
+#define PORT_ALTR_16550_F128 28 /* Altera 16550 UART with 128 FIFOs */
+#define PORT_MAX_8250	28	/* max port ID */
 
 /*
  * ARM specific type numbers.  These are not currently guaranteed
