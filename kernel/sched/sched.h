b719203b846284e77f5c50fca04b458b6484aeae
Li Zefan <lizefan@huawei.com>
2013-03-07 10:00:26 +0800

    sched: Fix update_group_power() prototype placement to fix build warning when !CONFIG_SMP
    All warnings:
    
       In file included from kernel/sched/core.c:85:0:
       kernel/sched/sched.h:1036:39: warning: 'struct sched_domain' declared inside parameter list
       kernel/sched/sched.h:1036:39: warning: its scope is only this definition or declaration, which is probably not what you want
    
    It's because struct sched_domain is defined inside #if CONFIG_SMP,
    while update_group_power() is declared unconditionally.
    
    Fix this warning by declaring update_group_power() only if
    CONFIG_SMP=n.
    
    Build tested with CONFIG_SMP enabled and then disabled.
    
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Li Zefan <lizefan@huawei.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/5137F4BA.2060101@huawei.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1026,6 +1026,8 @@ extern const struct sched_class idle_sched_class;
 
 #ifdef CONFIG_SMP
 
+extern void update_group_power(struct sched_domain *sd, int cpu);
+
 extern void trigger_load_balance(struct rq *rq, int cpu);
 extern void idle_balance(int this_cpu, struct rq *this_rq);
 
@@ -1040,7 +1042,6 @@ static inline void idle_balance(int cpu, struct rq *rq)
 extern void sysrq_sched_debug_show(void);
 extern void sched_init_granularity(void);
 extern void update_max_interval(void);
-extern void update_group_power(struct sched_domain *sd, int cpu);
 extern int update_runtime(struct notifier_block *nfb, unsigned long action, void *hcpu);
 extern void init_sched_rt_class(void);
 extern void init_sched_fair_class(void);
