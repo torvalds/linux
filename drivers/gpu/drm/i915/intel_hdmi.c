7637bfdb54c11f3471bac7ebe4fdca46970e72af
Jesse Barnes <jbarnes@virtuousgeek.org>
2013-03-08 10:46:01 -0800

    drm/i915: use VLV DIP routines on VLV v2
    This fixes up broken logic introduced in
    
    commit 90b107c8f7ea75ef55db4e0515dda86b245f8978
    Author: Shobhit Kumar <shobhit.kumar@intel.com>
    Date:   Wed Mar 28 13:39:32 2012 -0700
    
        drm/i915: Enable HDMI on ValleyView
    
    That one was probably a rebase fail along the way.
    
    v2: clean up init ordering (Daniel)
    
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    [danvet: Pimp commit message a bit.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_hdmi.c
+++ drivers/gpu/drm/i915/intel_hdmi.c
@@ -1004,12 +1004,12 @@ void intel_hdmi_init_connector(struct intel_digital_port *intel_dig_port,
 		BUG();
 	}
 
-	if (!HAS_PCH_SPLIT(dev)) {
-		intel_hdmi->write_infoframe = g4x_write_infoframe;
-		intel_hdmi->set_infoframes = g4x_set_infoframes;
-	} else if (IS_VALLEYVIEW(dev)) {
+	if (IS_VALLEYVIEW(dev)) {
 		intel_hdmi->write_infoframe = vlv_write_infoframe;
 		intel_hdmi->set_infoframes = vlv_set_infoframes;
+	} else if (!HAS_PCH_SPLIT(dev)) {
+		intel_hdmi->write_infoframe = g4x_write_infoframe;
+		intel_hdmi->set_infoframes = g4x_set_infoframes;
 	} else if (HAS_DDI(dev)) {
 		intel_hdmi->write_infoframe = hsw_write_infoframe;
 		intel_hdmi->set_infoframes = hsw_set_infoframes;
