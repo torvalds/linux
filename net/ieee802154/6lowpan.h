9026c4927254f5bea695cc3ef2e255280e6a3011
YOSHIFUJI Hideaki / 吉藤英明 <yoshfuji@linux-ipv6.org>
2013-03-09 09:11:57 +0000

    6lowpan: Fix endianness issue in is_addr_link_local().
    Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ieee802154/6lowpan.h
+++ net/ieee802154/6lowpan.h
@@ -84,7 +84,7 @@
 	(memcmp(addr1, addr2, length >> 3) == 0)
 
 /* local link, i.e. FE80::/10 */
-#define is_addr_link_local(a) (((a)->s6_addr16[0]) == 0x80FE)
+#define is_addr_link_local(a) (((a)->s6_addr16[0]) == htons(0xFE80))
 
 /*
  * check whether we can compress the IID to 16 bits,
