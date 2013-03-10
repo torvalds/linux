81e59494a56cb14f559886c345c4a93fb576bbab
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2013-03-10 15:44:52 -0700

    rcu: Tone down debugging during boot-up and shutdown.
    In some situations, randomly delaying RCU grace-period initialization
    can cause more trouble than help.  This commit therefore restricts this
    type of RCU self-torture to runtime, giving it a rest during boot and
    shutdown.
    
    Reported-by: Sasha Levin <sasha.levin@oracle.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/rcutree.c
+++ kernel/rcutree.c
@@ -1319,7 +1319,8 @@ static int rcu_gp_init(struct rcu_state *rsp)
 					    rnp->grphi, rnp->qsmask);
 		raw_spin_unlock_irq(&rnp->lock);
 #ifdef CONFIG_PROVE_RCU_DELAY
-		if ((random32() % (rcu_num_nodes * 8)) == 0)
+		if ((random32() % (rcu_num_nodes * 8)) == 0 &&
+		    system_state == SYSTEM_RUNNING)
 			schedule_timeout_uninterruptible(2);
 #endif /* #ifdef CONFIG_PROVE_RCU_DELAY */
 		cond_resched();
