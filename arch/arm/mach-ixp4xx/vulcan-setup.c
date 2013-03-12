2d798a3f20ae992b0b69a2b68c04ada397c32ed4
Johan Hovold <jhovold@gmail.com>
2013-03-12 20:21:34 +0100

    ARM: w1-gpio: fix erroneous gpio requests
    Fix regression introduced by commit d2323cf773 ("onewire: w1-gpio: add
    ext_pullup_enable pin in platform data") which added a gpio entry to the
    platform data, but did not add the required initialisers to the board
    files using it. Consequently, the driver would request gpio 0 at probe,
    which could break other uses of the corresponding pin.
    
    On AT91 requesting gpio 0 changes the pin muxing for PIOA0, which, for
    instance, breaks SPI0 on at91sam9g20.
    
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Johan Hovold <jhovold@gmail.com>
    Acked-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm/mach-ixp4xx/vulcan-setup.c
+++ arch/arm/mach-ixp4xx/vulcan-setup.c
@@ -163,6 +163,7 @@ static struct platform_device vulcan_max6369 = {
 
 static struct w1_gpio_platform_data vulcan_w1_gpio_pdata = {
 	.pin			= 14,
+	.ext_pullup_enable_pin	= -EINVAL,
 };
 
 static struct platform_device vulcan_w1_gpio = {
