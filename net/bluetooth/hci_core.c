f9f85279fd3a3284023231c7f0796f98c417e7cd
Johan Hedberg <johan.hedberg@intel.com>
2013-03-15 17:06:58 -0500

    Bluetooth: Clear non-persistent flags when closing HCI device
    When hci_dev_do_close() is called we should make sure to clear all
    non-persistent flags in hci->dev_flags.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Acked-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -1139,6 +1139,7 @@ static int hci_dev_do_close(struct hci_dev *hdev)
 
 	/* Clear flags */
 	hdev->flags = 0;
+	hdev->dev_flags &= ~HCI_PERSISTENT_MASK;
 
 	/* Controller radio is available but is currently powered down */
 	hdev->amp_status = 0;
