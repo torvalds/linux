22c5291e70ba66880c6a6acffbd8200a623c4556
Joe Perches <joe@perches.com>
2013-03-18 10:44:48 -0700

    staging:vt6655:dpc: Whitespace cleanups
    Neatening only.
    git diff -w shows no differences.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/dpc.h
+++ drivers/staging/vt6655/dpc.h
@@ -42,10 +42,10 @@
 /*---------------------  Export Functions  --------------------------*/
 
 bool
-device_receive_frame (
-    PSDevice pDevice,
-    PSRxDesc pCurrRD
-    );
+device_receive_frame(
+	PSDevice pDevice,
+	PSRxDesc pCurrRD
+);
 
 void	MngWorkItem(void *Context);
 
