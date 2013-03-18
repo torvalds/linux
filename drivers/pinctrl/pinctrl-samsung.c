d5517bec272ce4732f7e86a72a1bb815834c912c
Tomasz Figa <tomasz.figa@gmail.com>
2013-03-18 22:31:51 +0100

    pinctrl: samsung: Include pinctrl-exynos driver data conditionally
    Since pinctrl-samsung is a common part of the pin control support for
    several Samsung SoCs, it can be compiled without Exynos support enabled.
    
    This patch surrounds Exynos-specific driver data with ifdefs to include
    them only when support for Exynos is enabled.
    
    Signed-off-by: Tomasz Figa <tomasz.figa@gmail.com>
    [Fixed up EXYNOS4->EXYNOS]
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-samsung.c
+++ drivers/pinctrl/pinctrl-samsung.c
@@ -968,10 +968,12 @@ static int samsung_pinctrl_probe(struct platform_device *pdev)
 }
 
 static const struct of_device_id samsung_pinctrl_dt_match[] = {
+#ifdef CONFIG_PINCTRL_EXYNOS
 	{ .compatible = "samsung,exynos4210-pinctrl",
 		.data = (void *)exynos4210_pin_ctrl },
 	{ .compatible = "samsung,exynos4x12-pinctrl",
 		.data = (void *)exynos4x12_pin_ctrl },
+#endif
 	{},
 };
 MODULE_DEVICE_TABLE(of, samsung_pinctrl_dt_match);
