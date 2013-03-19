3d5a96582303e28c48699f3faaf920ef7d43e6f2
Arnd Bergmann <arnd@arndb.de>
2013-03-19 15:38:50 +0100

    clocksource: make CLOCKSOURCE_OF_DECLARE type safe
    This ensures that a function pointer passed into CLOCKSOURCE_OF_DECLARE
    takes the same arguments that we use for calling that function later.
    
    Also fix the extraneous semicolon at end of the CLOCKSOURCE_OF_DECLARE
    definition.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Rob Herring <rob.herring@calxeda.com>

--- drivers/clocksource/clksrc-of.c
+++ drivers/clocksource/clksrc-of.c
@@ -16,6 +16,7 @@
 
 #include <linux/init.h>
 #include <linux/of.h>
+#include <linux/clocksource.h>
 
 extern struct of_device_id __clksrc_of_table[];
 
@@ -26,7 +27,7 @@ void __init clocksource_of_init(void)
 {
 	struct device_node *np;
 	const struct of_device_id *match;
-	void (*init_func)(struct device_node *);
+	clocksource_of_init_fn init_func;
 
 	for_each_matching_node_and_match(np, __clksrc_of_table, &match) {
 		init_func = match->data;
