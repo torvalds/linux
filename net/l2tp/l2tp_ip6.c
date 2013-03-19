936063175afd895913a5e9db77e1a0ef43ea44ea
Tom Parkin <tparkin@katalix.com>
2013-03-19 06:11:15 +0000

    l2tp: close sessions in ip socket destroy callback
    l2tp_core hooks UDP's .destroy handler to gain advance warning of a tunnel
    socket being closed from userspace.  We need to do the same thing for
    IP-encapsulation sockets.
    
    Signed-off-by: Tom Parkin <tparkin@katalix.com>
    Signed-off-by: James Chapman <jchapman@katalix.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/l2tp/l2tp_ip6.c
+++ net/l2tp/l2tp_ip6.c
@@ -241,9 +241,16 @@ static void l2tp_ip6_close(struct sock *sk, long timeout)
 
 static void l2tp_ip6_destroy_sock(struct sock *sk)
 {
+	struct l2tp_tunnel *tunnel = l2tp_sock_to_tunnel(sk);
+
 	lock_sock(sk);
 	ip6_flush_pending_frames(sk);
 	release_sock(sk);
+
+	if (tunnel) {
+		l2tp_tunnel_closeall(tunnel);
+		sock_put(sk);
+	}
 
 	inet6_destroy_sock(sk);
 }
