d7f08452ffd3abbc2dbbf763cdec05496923f3b7
Johan Hovold <jhovold@gmail.com>
2013-03-21 12:36:33 +0100

    USB: keyspan_pda: remove bogus disconnect test from dtr_rts
    Remove bogus (and unnecessary) test for serial->dev being NULL in
    dtr_rts.
    
    The device is never cleared, and disconnect is handled for dtr_rts in
    usb-serial core anyway.
    
    Signed-off-by: Johan Hovold <jhovold@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/serial/keyspan_pda.c
+++ drivers/usb/serial/keyspan_pda.c
@@ -595,12 +595,10 @@ static void keyspan_pda_dtr_rts(struct usb_serial_port *port, int on)
 {
 	struct usb_serial *serial = port->serial;
 
-	if (serial->dev) {
-		if (on)
-			keyspan_pda_set_modem_info(serial, (1<<7) | (1<< 2));
-		else
-			keyspan_pda_set_modem_info(serial, 0);
-	}
+	if (on)
+		keyspan_pda_set_modem_info(serial, (1 << 7) | (1 << 2));
+	else
+		keyspan_pda_set_modem_info(serial, 0);
 }
 
 
