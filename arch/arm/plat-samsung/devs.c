e636602ac2613da8c1777cb42443223994be4107
Heiko Stübner <heiko@sntech.de>
2013-03-21 04:09:25 +0000

    i2c: s3c2410: move mach/regs-iic.h into i2c-s3c2410 device driver
    The register definitions are only used in the driver itself.
    This also removes the last dependency on plat/ includes from the
    i2c driver.
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Acked-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- arch/arm/plat-samsung/devs.c
+++ arch/arm/plat-samsung/devs.c
@@ -62,7 +62,6 @@
 #include <linux/platform_data/usb-s3c2410_udc.h>
 #include <linux/platform_data/usb-ohci-s3c2410.h>
 #include <plat/usb-phy.h>
-#include <plat/regs-iic.h>
 #include <plat/regs-serial.h>
 #include <plat/regs-spi.h>
 #include <linux/platform_data/spi-s3c64xx.h>
