0bb6ee4f4587d219cfbb328e8a14f608405b52b9
Rebecca Schultz Zavin <rebecca@android.com>
2013-03-22 13:18:36 -0700

    gpu: ion: Modify ION_NUM_HEAPS to reflect actual max
    ION_NUM_HEAPS is meant to represent the actual max number
    of heap types in the system, but currently doesn't reflect
    any custom heaps.  This patch changes it to 16.
    
    Signed-off-by: Rebecca Schultz Zavin <rebecca@android.com>

--- include/linux/ion.h
+++ include/linux/ion.h
@@ -35,7 +35,7 @@ enum ion_heap_type {
 	ION_HEAP_TYPE_CARVEOUT,
 	ION_HEAP_TYPE_CUSTOM, /* must be last so device specific heaps always
 				 are at the end of this enum */
-	ION_NUM_HEAPS,
+	ION_NUM_HEAPS = 16,
 };
 
 #define ION_HEAP_SYSTEM_MASK		(1 << ION_HEAP_TYPE_SYSTEM)
