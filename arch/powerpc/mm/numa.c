55671f3cc29c31681278b7782de4f6a4edb97a7e
Stephen Rothwell <sfr@canb.auug.org.au>
2013-03-25 18:44:44 +0000

    powerpc: fix annotation of fake_numa_create_new_node()
    This function has always been marked as __cpuinit, but is only called
    from functions marked as __init and references an __initdata variable.
    So change its annotation to __init.
    
    Fixes this build warning:
    
    WARNING: arch/powerpc/mm/built-in.o(.cpuinit.text+0x86): Section mismatch in reference from the function .fake_numa_create_new_node() to the variable .init.data:cmdline
    The function __cpuinit .fake_numa_create_new_node() references
    a variable __initdata cmdline.
    If cmdline is only used by .fake_numa_create_new_node then
    annotate cmdline with a matching annotation.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>

--- arch/powerpc/mm/numa.c
+++ arch/powerpc/mm/numa.c
@@ -79,7 +79,7 @@ static void __init setup_node_to_cpumask_map(void)
 	dbg("Node to cpumask map for %d nodes\n", nr_node_ids);
 }
 
-static int __cpuinit fake_numa_create_new_node(unsigned long end_pfn,
+static int __init fake_numa_create_new_node(unsigned long end_pfn,
 						unsigned int *nid)
 {
 	unsigned long long mem;
