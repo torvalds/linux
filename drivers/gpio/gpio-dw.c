e90ab742d185acafcac04ff5addc85c1896d2f17
Alan Tull <atull@altera.com>
2013-03-27 16:28:10 -0500

    FogBugz #110526: adjust license header to GPLv2
    The Designware GPIO Linux driver use GPLv3 header.
    This change fixes the license header to GPLv2.
    
    Signed-off-by: Alan Tull <atull@altera.com>

--- drivers/gpio/gpio-dw.c
+++ drivers/gpio/gpio-dw.c
@@ -3,9 +3,9 @@
  *
  * Copyright (C) 2012 Altera
  *
- * This program is free software: you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <linux/gpio.h>
