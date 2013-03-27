b91af27a3ca8d22e78789db1192a8b66d2985e99
Markus Pargmann <mpa@pengutronix.de>
2013-03-27 14:01:32 +0100

    ARM: imx27 DT init cpufreq-cpu0 device
    Adds cpufreq-cpu0 platform device for imx27 DT init and adds a clock
    registration for cpufreq-cpu0 device.
    
    Signed-off-by: Markus Pargmann <mpa@pengutronix.de>
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- arch/arm/mach-imx/imx27-dt.c
+++ arch/arm/mach-imx/imx27-dt.c
@@ -20,7 +20,11 @@
 
 static void __init imx27_dt_init(void)
 {
+	struct platform_device_info devinfo = { .name = "cpufreq-cpu0", };
+
 	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
+
+	platform_device_register_full(&devinfo);
 }
 
 static const char * const imx27_dt_board_compat[] __initconst = {
