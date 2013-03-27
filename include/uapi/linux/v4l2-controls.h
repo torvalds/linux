33a80fc2eb43afbc25f04cc1d5ed899da213c21f
Andrey Smirnov <andrew.smirnov@gmail.com>
2013-03-26 22:47:25 -0300

    [media] v4l2: Add private controls base for SI476X
    Add a base to be used for allocation of all the SI476X specific
    controls in the corresponding driver.
    
    Acked-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/uapi/linux/v4l2-controls.h
+++ include/uapi/linux/v4l2-controls.h
@@ -156,6 +156,10 @@ enum v4l2_colorfx {
  * We reserve 8 controls for this driver. */
 #define V4L2_CID_USER_S2255_BASE		(V4L2_CID_USER_BASE + 0x1010)
 
+/* The base for the si476x driver controls. See include/media/si476x.h for the list
+ * of controls. Total of 16 controls is reserved for that driver */
+#define V4L2_CID_USER_SI476X_BASE		(V4L2_CID_USER_BASE + 0x1010)
+
 /* MPEG-class control IDs */
 
 #define V4L2_CID_MPEG_BASE 			(V4L2_CTRL_CLASS_MPEG | 0x900)
