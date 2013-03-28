7a80c2c9a957b1ab056fac235140ebd6c43d9831
Alex Deucher <alexander.deucher@amd.com>
2013-03-27 20:34:19 -0400

    drm/radeon: fix typo in atom voltage table handling (6xx-ni)
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/radeon_atombios.c
+++ drivers/gpu/drm/radeon/radeon_atombios.c
@@ -3150,7 +3150,7 @@ radeon_atom_is_voltage_gpio(struct radeon_device *rdev,
 				break;
 			case 2:
 				num_indices = (size - sizeof(ATOM_COMMON_TABLE_HEADER)) /
-					sizeof(ATOM_VOLTAGE_OBJECT_INFO_V2);
+					sizeof(ATOM_VOLTAGE_OBJECT_V2);
 
 				for (i = 0; i < num_indices; i++) {
 					if ((voltage_info->v2.asVoltageObj[i].ucVoltageType == voltage_type) &&
@@ -3231,7 +3231,7 @@ int radeon_atom_get_max_voltage(struct radeon_device *rdev,
 			break;
 		case 2:
 			num_indices = (size - sizeof(ATOM_COMMON_TABLE_HEADER)) /
-				sizeof(ATOM_VOLTAGE_OBJECT_INFO_V2);
+				sizeof(ATOM_VOLTAGE_OBJECT_V2);
 
 			for (i = 0; i < num_indices; i++) {
 				if (voltage_info->v2.asVoltageObj[i].ucVoltageType == voltage_type) {
@@ -3287,7 +3287,7 @@ int radeon_atom_get_min_voltage(struct radeon_device *rdev,
 			break;
 		case 2:
 			num_indices = (size - sizeof(ATOM_COMMON_TABLE_HEADER)) /
-				sizeof(ATOM_VOLTAGE_OBJECT_INFO_V2);
+				sizeof(ATOM_VOLTAGE_OBJECT_V2);
 
 			for (i = 0; i < num_indices; i++) {
 				if (voltage_info->v2.asVoltageObj[i].ucVoltageType == voltage_type) {
@@ -3406,7 +3406,7 @@ int radeon_atom_get_voltage_table(struct radeon_device *rdev,
 				return -EINVAL;
 			case 2:
 				num_indices = (size - sizeof(ATOM_COMMON_TABLE_HEADER)) /
-					sizeof(ATOM_VOLTAGE_OBJECT_INFO_V2);
+					sizeof(ATOM_VOLTAGE_OBJECT_V2);
 
 				for (i = 0; i < num_indices; i++) {
 					if (voltage_info->v2.asVoltageObj[i].ucVoltageType == voltage_type) {
