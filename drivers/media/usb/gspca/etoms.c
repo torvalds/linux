471c4dd38642eb881c4da8372d69e8d11e450a79
Paul Bolle <pebolle@tiscali.nl>
2013-03-28 17:33:57 -0300

    [media] gspca: remove obsolete Kconfig macros
    The et61x251 driver was removed in v3.5. Remove the last references to
    its Kconfig macro now.
    
    Signed-off-by: Paul Bolle <pebolle@tiscali.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/usb/gspca/etoms.c
+++ drivers/media/usb/gspca/etoms.c
@@ -766,9 +766,7 @@ static const struct sd_desc sd_desc = {
 /* -- module initialisation -- */
 static const struct usb_device_id device_table[] = {
 	{USB_DEVICE(0x102c, 0x6151), .driver_info = SENSOR_PAS106},
-#if !defined CONFIG_USB_ET61X251 && !defined CONFIG_USB_ET61X251_MODULE
 	{USB_DEVICE(0x102c, 0x6251), .driver_info = SENSOR_TAS5130CXX},
-#endif
 	{}
 };
 
