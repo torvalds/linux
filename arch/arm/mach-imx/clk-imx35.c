cab1e0a36c9dd0b0671fb84197ed294513f5adc1
Markus Pargmann <mpa@pengutronix.de>
2013-03-29 16:20:09 +0100

    ARM: clk-imx35: Bugfix iomux clock
    This patch enables iomuxc_gate clock. It is necessary to be able to
    reconfigure iomux pads. Without this clock enabled, the
    clk_disable_unused function will disable this clock and the iomux pads
    are not configurable anymore. This happens at every boot. After a reboot
    (watchdog system reset) the clock is not enabled again, so all iomux pad
    reconfigurations in boot code are without effect.
    
    The iomux pads should be always configurable, so this patch always
    enables it.
    
    Signed-off-by: Markus Pargmann <mpa@pengutronix.de>
    Cc: stable@vger.kernel.org
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- arch/arm/mach-imx/clk-imx35.c
+++ arch/arm/mach-imx/clk-imx35.c
@@ -265,6 +265,7 @@ int __init mx35_clocks_init(void)
 	clk_prepare_enable(clk[iim_gate]);
 	clk_prepare_enable(clk[emi_gate]);
 	clk_prepare_enable(clk[max_gate]);
+	clk_prepare_enable(clk[iomuxc_gate]);
 
 	/*
 	 * SCC is needed to boot via mmc after a watchdog reset. The clock code
