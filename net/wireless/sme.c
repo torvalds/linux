90e0970f8788cef2c8f5183af5a98f4f94600faf
Artem Savkov <artem.savkov@gmail.com>
2013-03-30 21:20:14 +0400

    cfg80211: sched_scan_mtx lock in cfg80211_conn_work()
    Introduced in f9f475292dbb0e7035fb6661d1524761ea0888d9
    ("cfg80211: always check for scan end on P2P device")
    
    cfg80211_conn_scan() which requires sched_scan_mtx to be held can be called
    from cfg80211_conn_work(). Without this we are hitting multiple warnings like
    the following:
    
    WARNING: at net/wireless/sme.c:88 cfg80211_conn_scan+0x1dc/0x3a0 [cfg80211]()
    Hardware name: 0578A21
    Modules linked in: ...
    Pid: 620, comm: kworker/3:1 Not tainted 3.9.0-rc4-next-20130328+ #326
    Call Trace:
     [<c1036992>] warn_slowpath_common+0x72/0xa0
     [<c10369e2>] warn_slowpath_null+0x22/0x30
     [<faa4b0ec>] cfg80211_conn_scan+0x1dc/0x3a0 [cfg80211]
     [<faa4b344>] cfg80211_conn_do_work+0x94/0x380 [cfg80211]
     [<faa4c3b2>] cfg80211_conn_work+0xa2/0x130 [cfg80211]
     [<c1051858>] process_one_work+0x198/0x450
    
    Signed-off-by: Artem Savkov <artem.savkov@gmail.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/wireless/sme.c
+++ net/wireless/sme.c
@@ -224,6 +224,7 @@ void cfg80211_conn_work(struct work_struct *work)
 	rtnl_lock();
 	cfg80211_lock_rdev(rdev);
 	mutex_lock(&rdev->devlist_mtx);
+	mutex_lock(&rdev->sched_scan_mtx);
 
 	list_for_each_entry(wdev, &rdev->wdev_list, list) {
 		wdev_lock(wdev);
@@ -248,6 +249,7 @@ void cfg80211_conn_work(struct work_struct *work)
 		wdev_unlock(wdev);
 	}
 
+	mutex_unlock(&rdev->sched_scan_mtx);
 	mutex_unlock(&rdev->devlist_mtx);
 	cfg80211_unlock_rdev(rdev);
 	rtnl_unlock();
