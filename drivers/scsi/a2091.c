408bb25ba50c6abaf516d844fd62556ec89a0af2
Al Viro <viro@zeniv.linux.org.uk>
2013-03-31 00:30:35 -0400

    switch wd33c93 to ->show_info()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/scsi/a2091.c
+++ drivers/scsi/a2091.c
@@ -166,7 +166,8 @@ static int a2091_bus_reset(struct scsi_cmnd *cmd)
 static struct scsi_host_template a2091_scsi_template = {
 	.module			= THIS_MODULE,
 	.name			= "Commodore A2091/A590 SCSI",
-	.proc_info		= wd33c93_proc_info,
+	.show_info		= wd33c93_show_info,
+	.write_info		= wd33c93_write_info,
 	.proc_name		= "A2901",
 	.queuecommand		= wd33c93_queuecommand,
 	.eh_abort_handler	= wd33c93_abort,
