16b39738f9ae2acc50dc82d5259be37d575eb3c2
J Keerthy <j-keerthy@ti.com>
2013-04-01 12:04:41 -0400

    staging: ti-soc-thermal: Enable COUNTER_DELAY feature for OMAP5
    Enable COUNTER_DELAY feature for OMAP5.
    
    Signed-off-by: J Keerthy <j-keerthy@ti.com>
    Signed-off-by: Eduardo Valentin <eduardo.valentin@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ti-soc-thermal/omap5-thermal-data.c
+++ drivers/staging/ti-soc-thermal/omap5-thermal-data.c
@@ -318,7 +318,8 @@ omap5430_adc_to_temp[
 const struct ti_bandgap_data omap5430_data = {
 	.features = TI_BANDGAP_FEATURE_TSHUT_CONFIG |
 			TI_BANDGAP_FEATURE_FREEZE_BIT |
-			TI_BANDGAP_FEATURE_TALERT,
+			TI_BANDGAP_FEATURE_TALERT |
+			TI_BANDGAP_FEATURE_COUNTER_DELAY,
 	.fclock_name = "l3instr_ts_gclk_div",
 	.div_ck_name = "l3instr_ts_gclk_div",
 	.conv_table = omap5430_adc_to_temp,
