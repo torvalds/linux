064f370c5fd982e1264c03f5b704e00f5e41eb36
Thierry Escande <thierry.escande@linux.intel.com>
2013-04-02 10:25:16 +0200

    NFC: llcp: Add support in getsockopt for RW, LTO, and MIU remote parameters
    Useful for LLCP validation tests.
    
    Signed-off-by: Thierry Escande <thierry.escande@linux.intel.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- net/nfc/llcp/sock.c
+++ net/nfc/llcp/sock.c
@@ -303,7 +303,7 @@ static int nfc_llcp_getsockopt(struct socket *sock, int level, int optname,
 	struct sock *sk = sock->sk;
 	struct nfc_llcp_sock *llcp_sock = nfc_llcp_sock(sk);
 	int len, err = 0;
-	u16 miux;
+	u16 miux, remote_miu;
 	u8 rw;
 
 	pr_debug("%p optname %d\n", sk, optname);
@@ -335,6 +335,27 @@ static int nfc_llcp_getsockopt(struct socket *sock, int level, int optname,
 			be16_to_cpu(local->miux) : be16_to_cpu(llcp_sock->miux);
 
 		if (put_user(miux, (u32 __user *) optval))
+			err = -EFAULT;
+
+		break;
+
+	case NFC_LLCP_REMOTE_MIU:
+		remote_miu = llcp_sock->remote_miu > LLCP_MAX_MIU ?
+				local->remote_miu : llcp_sock->remote_miu;
+
+		if (put_user(remote_miu, (u32 __user *) optval))
+			err = -EFAULT;
+
+		break;
+
+	case NFC_LLCP_REMOTE_LTO:
+		if (put_user(local->remote_lto / 10, (u32 __user *) optval))
+			err = -EFAULT;
+
+		break;
+
+	case NFC_LLCP_REMOTE_RW:
+		if (put_user(llcp_sock->remote_rw, (u32 __user *) optval))
 			err = -EFAULT;
 
 		break;
