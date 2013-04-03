5b82d03b74cadb681377e1c2494c477185bf6619
Ulf Hansson <ulf.hansson@linaro.org>
2013-04-03 14:26:57 +0200

    clk: ux500: Add support for sysctrl clocks
    The abx500 sysctrl clocks are using the ab8500 sysctrl driver to
    modify the clock hardware. Sysctrl clocks are represented by a
    ab8500 sysctrl register and with a corresponding bitmask.
    
    The sysctrl clocks are slow path clocks, which means clk_prepare
    and clk_unprepare will be used to gate|ungate these clocks.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Mike Turquette <mturquette@linaro.org>

--- drivers/clk/ux500/clk.h
+++ drivers/clk/ux500/clk.h
@@ -11,6 +11,7 @@
 #define __UX500_CLK_H
 
 #include <linux/clk.h>
+#include <linux/device.h>
 
 struct clk *clk_reg_prcc_pclk(const char *name,
 			      const char *parent_name,
@@ -56,5 +57,33 @@ struct clk *clk_reg_prcmu_opp_volt_scalable(const char *name,
 					    u8 cg_sel,
 					    unsigned long rate,
 					    unsigned long flags);
+
+struct clk *clk_reg_sysctrl_gate(struct device *dev,
+				 const char *name,
+				 const char *parent_name,
+				 u16 reg_sel,
+				 u8 reg_mask,
+				 u8 reg_bits,
+				 unsigned long enable_delay_us,
+				 unsigned long flags);
+
+struct clk *clk_reg_sysctrl_gate_fixed_rate(struct device *dev,
+					    const char *name,
+					    const char *parent_name,
+					    u16 reg_sel,
+					    u8 reg_mask,
+					    u8 reg_bits,
+					    unsigned long rate,
+					    unsigned long enable_delay_us,
+					    unsigned long flags);
+
+struct clk *clk_reg_sysctrl_set_parent(struct device *dev,
+				       const char *name,
+				       const char **parent_names,
+				       u8 num_parents,
+				       u16 *reg_sel,
+				       u8 *reg_mask,
+				       u8 *reg_bits,
+				       unsigned long flags);
 
 #endif /* __UX500_CLK_H */
