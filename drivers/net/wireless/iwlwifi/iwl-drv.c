1618b2b02a3a0ee7a6863fed4b0d22e697e7e97c
Johannes Berg <johannes.berg@intel.com>
2013-04-04 10:35:23 +0200

    iwlwifi: print warning on request_module failure
    If request_module() failed then we didn't have the correct
    opmode module that the driver needs to function, so print
    a warning in this case to make it more obvious what could
    be wrong. This still won't catch the case where the module
    simply doesn't exist because it wasn't compiled though.
    
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-drv.c
+++ drivers/net/wireless/iwlwifi/iwl-drv.c
@@ -998,8 +998,13 @@ static void iwl_req_fw_callback(const struct firmware *ucode_raw, void *context)
 	 * else from proceeding if the module fails to load
 	 * or hangs loading.
 	 */
-	if (load_module)
-		request_module("%s", op->name);
+	if (load_module) {
+		err = request_module("%s", op->name);
+		if (err)
+			IWL_ERR(drv,
+				"failed to load module %s (error %d), is dynamic loading enabled?\n",
+				op->name, err);
+	}
 	return;
 
  try_again:
