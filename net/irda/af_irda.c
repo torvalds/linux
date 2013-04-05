e1e3c806dab8bdf5d748653edc67b2d97804a666
Mathias Krause <minipli@googlemail.com>
2013-04-05 10:41:28 +0000

    irda: use GFP_KERNEL in irda_connect_response()
    The only call site of irda_connect_response() is irda_accept() -- a
    function called from user context only. Therefore it has no need for
    GFP_ATOMIC.
    
    Signed-off-by: Mathias Krause <minipli@googlemail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/irda/af_irda.c
+++ net/irda/af_irda.c
@@ -305,8 +305,7 @@ static void irda_connect_response(struct irda_sock *self)
 
 	IRDA_DEBUG(2, "%s()\n", __func__);
 
-	skb = alloc_skb(TTP_MAX_HEADER + TTP_SAR_HEADER,
-			GFP_ATOMIC);
+	skb = alloc_skb(TTP_MAX_HEADER + TTP_SAR_HEADER, GFP_KERNEL);
 	if (skb == NULL) {
 		IRDA_DEBUG(0, "%s() Unable to allocate sk_buff!\n",
 			   __func__);
