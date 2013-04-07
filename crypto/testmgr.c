e448370d7377f064c2fef55f72e9b45184bf0926
Jussi Kivilinna <jussi.kivilinna@iki.fi>
2013-04-07 16:43:56 +0300

    crypto: testmgr - add empty test vectors for null ciphers
    Without these, kernel log shows:
    [    5.984881] alg: No test for cipher_null (cipher_null-generic)
    [    5.985096] alg: No test for ecb(cipher_null) (ecb-cipher_null)
    [    5.985170] alg: No test for compress_null (compress_null-generic)
    [    5.985297] alg: No test for digest_null (digest_null-generic)
    
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@iki.fi>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -1913,6 +1913,9 @@ static const struct alg_test_desc alg_test_descs[] = {
 			}
 		}
 	}, {
+		.alg = "compress_null",
+		.test = alg_test_null,
+	}, {
 		.alg = "crc32c",
 		.test = alg_test_crc32c,
 		.fips_allowed = 1,
@@ -2127,6 +2130,9 @@ static const struct alg_test_desc alg_test_descs[] = {
 			}
 		}
 	}, {
+		.alg = "digest_null",
+		.test = alg_test_null,
+	}, {
 		.alg = "ecb(__aes-aesni)",
 		.test = alg_test_null,
 		.fips_allowed = 1,
@@ -2236,6 +2242,9 @@ static const struct alg_test_desc alg_test_descs[] = {
 				}
 			}
 		}
+	}, {
+		.alg = "ecb(cipher_null)",
+		.test = alg_test_null,
 	}, {
 		.alg = "ecb(des)",
 		.test = alg_test_skcipher,
