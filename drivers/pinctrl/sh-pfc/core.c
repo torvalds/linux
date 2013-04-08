58c229e18b7754dfe505f3bc1688feb28c84f42a
Koji Matsuoka <koji.matsuoka.xm@rms.renesas.com>
2013-04-08 11:08:53 +0900

    sh-pfc: Initial r8a7790 PFC support
    Add initial PFC support for the r8a7790 SoC.
    
    At this point only GPIO interface is supported, move to
    newer interfaces planned as incremental changes.
    
    Original authors is Koji Matsuoka-san, thanks for him
    and his team for the heavy lifting. Adjusted by Magnus
    to work together with updated code in drivers/pinctrl.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@rms.renesas.com>
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- drivers/pinctrl/sh-pfc/core.c
+++ drivers/pinctrl/sh-pfc/core.c
@@ -427,6 +427,9 @@ static const struct platform_device_id sh_pfc_id_table[] = {
 #ifdef CONFIG_PINCTRL_PFC_R8A7779
 	{ "pfc-r8a7779", (kernel_ulong_t)&r8a7779_pinmux_info },
 #endif
+#ifdef CONFIG_PINCTRL_PFC_R8A7790
+	{ "pfc-r8a7790", (kernel_ulong_t)&r8a7790_pinmux_info },
+#endif
 #ifdef CONFIG_PINCTRL_PFC_SH7203
 	{ "pfc-sh7203", (kernel_ulong_t)&sh7203_pinmux_info },
 #endif
--- drivers/pinctrl/sh-pfc/core.h
+++ drivers/pinctrl/sh-pfc/core.h
@@ -57,6 +57,7 @@ int sh_pfc_config_mux(struct sh_pfc *pfc, unsigned mark, int pinmux_type);
 extern const struct sh_pfc_soc_info r8a73a4_pinmux_info;
 extern const struct sh_pfc_soc_info r8a7740_pinmux_info;
 extern const struct sh_pfc_soc_info r8a7779_pinmux_info;
+extern const struct sh_pfc_soc_info r8a7790_pinmux_info;
 extern const struct sh_pfc_soc_info sh7203_pinmux_info;
 extern const struct sh_pfc_soc_info sh7264_pinmux_info;
 extern const struct sh_pfc_soc_info sh7269_pinmux_info;
