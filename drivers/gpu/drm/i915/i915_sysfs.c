182642b09311c94898acbc01cc9bac8e02f63da6
Mika Kuoppala <mika.kuoppala@linux.intel.com>
2013-04-11 15:07:59 +0300

    drm/i915: Return stored value from max freq sysfs entry
    commit 4f9b2fe0441d4bdf5666a306156b5d6755de2584
    Author: Ben Widawsky <ben@bwidawsk.net>
    Date:   Fri Apr 5 14:29:22 2013 -0700
    
        drm/i915: Better overclock support
    
    changed the sysfs read semantics for 'gt_max_freq_mhz'. By
    always returning overclock max instead of stored value.
    
    Fix this by returning the stored value. Separate sysfs entry
    should be considered for overclocking max freq.
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=63415
    Cc: Ben Widawsky <ben@bwidawsk.net>
    Signed-off-by: Mika Kuoppala <mika.kuoppala@intel.com>
    Reviewed-by: Ben Widawsky <ben@bwidawsk.net>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_sysfs.c
+++ drivers/gpu/drm/i915/i915_sysfs.c
@@ -226,7 +226,7 @@ static ssize_t gt_max_freq_mhz_show(struct device *kdev, struct device_attribute
 	int ret;
 
 	mutex_lock(&dev_priv->rps.hw_lock);
-	ret = dev_priv->rps.hw_max * GT_FREQUENCY_MULTIPLIER;
+	ret = dev_priv->rps.max_delay * GT_FREQUENCY_MULTIPLIER;
 	mutex_unlock(&dev_priv->rps.hw_lock);
 
 	return snprintf(buf, PAGE_SIZE, "%d\n", ret);
