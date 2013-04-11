f3bff6318fa0f54956b02ed451d9b120441006ea
Yang Zhang <yang.z.zhang@Intel.com>
2013-04-11 19:21:39 +0800

    KVM: Force vmexit with virtual interrupt delivery
    Need the EOI to track interrupt deliver status, so force vmexit
    on EOI for rtc interrupt when enabling virtual interrupt delivery.
    
    Signed-off-by: Yang Zhang <yang.z.zhang@Intel.com>
    Reviewed-by: Gleb Natapov <gleb@redhat.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- virt/kvm/ioapic.c
+++ virt/kvm/ioapic.c
@@ -189,7 +189,7 @@ void kvm_ioapic_calculate_eoi_exitmap(struct kvm_vcpu *vcpu,
 		if (!e->fields.mask &&
 			(e->fields.trig_mode == IOAPIC_LEVEL_TRIG ||
 			 kvm_irq_has_notifier(ioapic->kvm, KVM_IRQCHIP_IOAPIC,
-				 index))) {
+				 index) || index == RTC_GSI)) {
 			if (kvm_apic_match_dest(vcpu, NULL, 0,
 				e->fields.dest_id, e->fields.dest_mode))
 				__set_bit(e->fields.vector, (unsigned long *)eoi_exit_bitmap);
