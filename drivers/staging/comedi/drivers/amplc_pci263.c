22691aec19371c0817565c3af54e276884e908ee
Ian Abbott <abbotti@mev.co.uk>
2013-04-12 17:01:54 +0100

    staging: comedi: amplc_pc263: split out PCI support
    The "amplc_pc263" module is a hybrid driver for Amplicon PC263 (ISA) and
    PCI263 (PCI) and uses conditional compilation (and compiler
    optimization) to compile in the support for the different bus types.
    
    Split out support for the PCI263 into a new module "amplc_pci263",
    retaining support for the PC263 in the existing module "amplc_pc263".
    
    Don't bother supporting the legacy attach mechanism for PCI board in the
    new module as that is no longer in vogue for the comedi drivers and the
    PCI263 board has no special configuration requirements.
    
    Although the code to handle the single subdevice of each board is the
    same for both drivers, this is only a small amount of code and I don't
    think it's worth creating a common module to handle it.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/amplc_pci263.c
+++ drivers/staging/comedi/drivers/amplc_pci263.c
@@ -1,6 +1,6 @@
 /*
-    comedi/drivers/amplc_pc263.c
-    Driver for Amplicon PC263 and PCI263 relay boards.
+    comedi/drivers/amplc_pci263.c
+    Driver for Amplicon PCI263 relay board.
 
     Copyright (C) 2002 MEV Ltd. <http://www.mev.co.uk/>
 
@@ -23,45 +23,32 @@
 
 */
 /*
-Driver: amplc_pc263
-Description: Amplicon PC263
+Driver: amplc_pci263
+Description: Amplicon PCI263
 Author: Ian Abbott <abbotti@mev.co.uk>
-Devices: [Amplicon] PC263 (pc263)
+Devices: [Amplicon] PCI263 (amplc_pci263)
 Updated: Fri, 12 Apr 2013 15:19:36 +0100
 Status: works
 
-Configuration options:
-  [0] - I/O port base address
+Configuration options: not applicable, uses PCI auto config
 
 The board appears as one subdevice, with 16 digital outputs, each
 connected to a reed-relay. Relay contacts are closed when output is 1.
 The state of the outputs can be read.
 */
 
+#include <linux/pci.h>
+
 #include "../comedidev.h"
 
-#define PC263_DRIVER_NAME	"amplc_pc263"
+#define PCI263_DRIVER_NAME	"amplc_pci263"
 
-/* PC263 registers */
-#define PC263_IO_SIZE	2
+/* PCI263 PCI configuration register information */
+#define PCI_DEVICE_ID_AMPLICON_PCI263 0x000c
 
-/*
- * Board descriptions for Amplicon PC263.
- */
-
-struct pc263_board {
-	const char *name;
-};
-
-static const struct pc263_board pc263_boards[] = {
-	{
-		.name = "pc263",
-	},
-};
-
-static int pc263_do_insn_bits(struct comedi_device *dev,
-			      struct comedi_subdevice *s,
-			      struct comedi_insn *insn, unsigned int *data)
+static int pci263_do_insn_bits(struct comedi_device *dev,
+			       struct comedi_subdevice *s,
+			       struct comedi_insn *insn, unsigned int *data)
 {
 	/* The insn data is a mask in data[0] and the new data
 	 * in data[1], each channel cooresponding to a bit. */
@@ -75,15 +62,18 @@ static int pc263_do_insn_bits(struct comedi_device *dev,
 	return insn->n;
 }
 
-static int pc263_attach(struct comedi_device *dev, struct comedi_devconfig *it)
+static int pci263_auto_attach(struct comedi_device *dev,
+			      unsigned long context_unused)
 {
+	struct pci_dev *pci_dev = comedi_to_pci_dev(dev);
 	struct comedi_subdevice *s;
 	int ret;
 
-	ret = comedi_request_region(dev, it->options[0], PC263_IO_SIZE);
+	ret = comedi_pci_enable(dev);
 	if (ret)
 		return ret;
 
+	dev->iobase = pci_resource_start(pci_dev, 2);
 	ret = comedi_alloc_subdevices(dev, 1);
 	if (ret)
 		return ret;
@@ -95,33 +85,43 @@ static int pc263_attach(struct comedi_device *dev, struct comedi_devconfig *it)
 	s->n_chan = 16;
 	s->maxdata = 1;
 	s->range_table = &range_digital;
-	s->insn_bits = pc263_do_insn_bits;
+	s->insn_bits = pci263_do_insn_bits;
 	/* read initial relay state */
 	s->state = inb(dev->iobase) | (inb(dev->iobase + 1) << 8);
 
-	dev_info(dev->class_dev, "%s (base %#lx) attached\n", dev->board_name,
-		 dev->iobase);
+	dev_info(dev->class_dev, "%s (pci %s) attached\n", dev->board_name,
+		 pci_name(pci_dev));
 	return 0;
 }
 
-static void pc263_detach(struct comedi_device *dev)
-{
-	if (dev->iobase)
-		release_region(dev->iobase, PC263_IO_SIZE);
-}
-
-static struct comedi_driver amplc_pc263_driver = {
-	.driver_name = PC263_DRIVER_NAME,
-	.module = THIS_MODULE,
-	.attach = pc263_attach,
-	.detach = pc263_detach,
-	.board_name = &pc263_boards[0].name,
-	.offset = sizeof(struct pc263_board),
-	.num_names = ARRAY_SIZE(pc263_boards),
+static struct comedi_driver amplc_pci263_driver = {
+	.driver_name	= PCI263_DRIVER_NAME,
+	.module		= THIS_MODULE,
+	.auto_attach	= pci263_auto_attach,
+	.detach		= comedi_pci_disable,
 };
 
-module_comedi_driver(amplc_pc263_driver);
+static DEFINE_PCI_DEVICE_TABLE(pci263_pci_table) = {
+	{ PCI_DEVICE(PCI_VENDOR_ID_AMPLICON, PCI_DEVICE_ID_AMPLICON_PCI263) },
+	{0}
+};
+MODULE_DEVICE_TABLE(pci, pci263_pci_table);
+
+static int amplc_pci263_pci_probe(struct pci_dev *dev,
+				  const struct pci_device_id *id)
+{
+	return comedi_pci_auto_config(dev, &amplc_pci263_driver,
+				      id->driver_data);
+}
+
+static struct pci_driver amplc_pci263_pci_driver = {
+	.name		= PCI263_DRIVER_NAME,
+	.id_table	= pci263_pci_table,
+	.probe		= &amplc_pci263_pci_probe,
+	.remove		= comedi_pci_auto_unconfig,
+};
+module_comedi_pci_driver(amplc_pci263_driver, amplc_pci263_pci_driver);
 
 MODULE_AUTHOR("Comedi http://www.comedi.org");
-MODULE_DESCRIPTION("Comedi driver for Amplicon PC263 relay board");
+MODULE_DESCRIPTION("Comedi driver for Amplicon PCI263 relay board");
 MODULE_LICENSE("GPL");
