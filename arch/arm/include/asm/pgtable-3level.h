865499ea90d399e0682bcce3ae7af24277633699
Marc Zyngier <marc.zyngier@arm.com>
2013-04-12 14:00:16 +0100

    ARM: KVM: fix L_PTE_S2_RDWR to actually be Read/Write
    Looks like our L_PTE_S2_RDWR definition is slightly wrong,
    and is actually write only (see ARM ARM Table B3-9, Stage 2 control
    of access permissions). Didn't make a difference for normal pages,
    as we OR the flags together, but I'm still wondering how it worked
    for Stage-2 mapped devices, such as the GIC.
    
    Brown paper bag time, again.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoffer Dall <cdall@cs.columbia.edu>

--- arch/arm/include/asm/pgtable-3level.h
+++ arch/arm/include/asm/pgtable-3level.h
@@ -111,7 +111,7 @@
 #define L_PTE_S2_MT_WRITETHROUGH (_AT(pteval_t, 0xa) << 2) /* MemAttr[3:0] */
 #define L_PTE_S2_MT_WRITEBACK	 (_AT(pteval_t, 0xf) << 2) /* MemAttr[3:0] */
 #define L_PTE_S2_RDONLY		 (_AT(pteval_t, 1) << 6)   /* HAP[1]   */
-#define L_PTE_S2_RDWR		 (_AT(pteval_t, 2) << 6)   /* HAP[2:1] */
+#define L_PTE_S2_RDWR		 (_AT(pteval_t, 3) << 6)   /* HAP[2:1] */
 
 /*
  * Hyp-mode PL2 PTE definitions for LPAE.
