841670351e8e5acbe9f7367f7df661a5d0cfef79
Ulf Hansson <ulf.hansson@linaro.org>
2013-04-12 09:41:44 +0000

    PM / Runtime: Improve prepare handling at system suspend for genpd
    When genpd prepares for a system suspend it will fetch a runtime
    reference for the device. When returning it we now use the
    asyncronous runtime PM API. Thus we don't have to wait for the
    device to become idle|suspended before we move on and handle the
    next device in queue.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/base/power/domain.c
+++ drivers/base/power/domain.c
@@ -920,7 +920,7 @@ static int pm_genpd_prepare(struct device *dev)
 		pm_wakeup_event(dev, 0);
 
 	if (pm_wakeup_pending()) {
-		pm_runtime_put_sync(dev);
+		pm_runtime_put(dev);
 		return -EBUSY;
 	}
 
@@ -961,7 +961,7 @@ static int pm_genpd_prepare(struct device *dev)
 		pm_runtime_enable(dev);
 	}
 
-	pm_runtime_put_sync(dev);
+	pm_runtime_put(dev);
 	return ret;
 }
 
