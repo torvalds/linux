adaa3229fbb88532e0d460afad779efbfb92ffeb
Lars-Peter Clausen <lars@metafoo.de>
2013-04-15 19:19:59 +0200

    ASoC: imx: Use generic dmaengine PCM
    Use the generic dmaengine PCM driver instead of a custom implementation.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Tested-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/fsl/imx-pcm.h
+++ sound/soc/fsl/imx-pcm.h
@@ -39,10 +39,15 @@ void imx_pcm_free(struct snd_pcm *pcm);
 
 #ifdef CONFIG_SND_SOC_IMX_PCM_DMA
 int imx_pcm_dma_init(struct platform_device *pdev);
+void imx_pcm_dma_exit(struct platform_device *pdev);
 #else
 static inline int imx_pcm_dma_init(struct platform_device *pdev)
 {
 	return -ENODEV;
+}
+
+static inline void imx_pcm_dma_exit(struct platform_device *pdev)
+{
 }
 #endif
 
