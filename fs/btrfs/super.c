62dbd7176e196cd042c5542696981b268264fe92
Josef Bacik <jbacik@fusionio.com>
2013-04-17 12:16:59 -0400

    Btrfs: fix lockdep warning
    The locking order for stuff is
    
    __sb_start_write
    ordered_mutex
    
    but with sync() we don't do __sb_start_write for some strange reason, which
    means that our iput in wait_ordered_extents could start a transaction which does
    the __sb_start_write while we're holding the ordered_mutex.  Fix this by using
    delayed iput in sync.  Thanks,
    
    Reported-by: David Sterba <dsterba@suse.cz>
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -866,7 +866,7 @@ int btrfs_sync_fs(struct super_block *sb, int wait)
 		return 0;
 	}
 
-	btrfs_wait_ordered_extents(root, 0);
+	btrfs_wait_ordered_extents(root, 1);
 
 	trans = btrfs_attach_transaction_barrier(root);
 	if (IS_ERR(trans)) {
