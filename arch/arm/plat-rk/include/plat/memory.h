1a2a5961257aa52e95548a887fdc85dd2cdba043
黄涛 <huangtao@rock-chips.com>
2013-04-18 17:05:22 +0800

    rk: set CONSISTENT_DMA_SIZE to 8M

--- arch/arm/plat-rk/include/plat/memory.h
+++ arch/arm/plat-rk/include/plat/memory.h
@@ -8,6 +8,8 @@
  */
 #define PLAT_PHYS_OFFSET	UL(0x60000000)
 
+#define CONSISTENT_DMA_SIZE	SZ_8M
+
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 34))
 #define dmac_clean_range(start, end)	dmac_map_area(start, end - start, DMA_TO_DEVICE)
 #define dmac_inv_range(start, end)	dmac_unmap_area(start, end - start, DMA_FROM_DEVICE)
