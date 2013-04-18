bc6edd95c5da928ef337b7efb95745ac34228f3c
Simon Horman <horms+renesas@verge.net.au>
2013-04-18 21:21:09 +0900

    ARM: shmobile: marzen: Use INTC External IRQ pin driver for SMSC
    Update the marzen board to use the INTC External IRQ pin driver for SMSC.
    
    This code was originally posted by Magnus Damm as part of
    "ARM: shmobile: INTC External IRQ pin driver on r8a7779"
    but somehow omitted when I applied that patch.
    
    Cc: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    (cherry picked from commit 6e267030252ab5309e074c3a19c92ceb7a01fc8b)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/board-marzen.c
+++ arch/arm/mach-shmobile/board-marzen.c
@@ -69,7 +69,7 @@ static struct resource smsc911x_resources[] = {
 		.flags		= IORESOURCE_MEM,
 	},
 	[1] = {
-		.start		= gic_iid(0x3c), /* IRQ 1 */
+		.start		= irq_pin(1), /* IRQ 1 */
 		.flags		= IORESOURCE_IRQ,
 	},
 };
@@ -405,6 +405,7 @@ static void __init marzen_init(void)
 	pinctrl_register_mappings(marzen_pinctrl_map,
 				  ARRAY_SIZE(marzen_pinctrl_map));
 	r8a7779_pinmux_init();
+	r8a7779_init_irq_extpin(1); /* IRQ1 as individual interrupt */
 
 	r8a7779_add_standard_devices();
 	platform_add_devices(marzen_devices, ARRAY_SIZE(marzen_devices));
