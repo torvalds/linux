2a7aceecf15a463ba6bfa83b6579e75bb4703cd9
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-04-19 11:24:39 +0200

    drm/i915: Fixup non-24bpp support for VGA screens on Haswell
    The LPT PCH only supports 8bpc, so we need to force the pipe bpp
    to the right value.
    
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_crt.c
+++ drivers/gpu/drm/i915/intel_crt.c
@@ -214,6 +214,10 @@ static bool intel_crt_compute_config(struct intel_encoder *encoder,
 	if (HAS_PCH_SPLIT(dev))
 		pipe_config->has_pch_encoder = true;
 
+	/* LPT FDI RX only supports 8bpc. */
+	if (HAS_PCH_LPT(dev))
+		pipe_config->pipe_bpp = 24;
+
 	return true;
 }
 
