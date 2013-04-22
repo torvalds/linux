5ffedc6ed3d066f5c0d2c2106f9081170b3d24fa
Dan Carpenter <dan.carpenter@oracle.com>
2013-04-22 20:24:52 +0000

    NFC: llcp: two bugs in ->getname()
    The sockaddr_nfc_llcp struct has as hole between ->sa_family and
    ->dev_idx so I've added a memset() to clear it and prevent an
    information leak.
    
    Also the ->nfc_protocol element wasn't set so I've added that.
    
    "uaddr->sa_family" and "llcp_addr->sa_family" are the same thing but
    it's less confusing to use llcp_addr consistently throughout.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/nfc/llcp/sock.c
+++ net/nfc/llcp/sock.c
@@ -358,12 +358,13 @@ static int llcp_sock_getname(struct socket *sock, struct sockaddr *uaddr,
 	pr_debug("%p %d %d %d\n", sk, llcp_sock->target_idx,
 		 llcp_sock->dsap, llcp_sock->ssap);
 
-	uaddr->sa_family = AF_NFC;
-
+	memset(llcp_addr, 0, sizeof(*llcp_addr));
 	*len = sizeof(struct sockaddr_nfc_llcp);
 
+	llcp_addr->sa_family = AF_NFC;
 	llcp_addr->dev_idx = llcp_sock->dev->idx;
 	llcp_addr->target_idx = llcp_sock->target_idx;
+	llcp_addr->nfc_protocol = llcp_sock->nfc_protocol;
 	llcp_addr->dsap = llcp_sock->dsap;
 	llcp_addr->ssap = llcp_sock->ssap;
 	llcp_addr->service_name_len = llcp_sock->service_name_len;
