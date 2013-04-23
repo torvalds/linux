1d5085cbab8c9fba727567102b104f06f2064b36
Stephen Rothwell <sfr@canb.auug.org.au>
2013-04-23 17:40:35 +1000

    netlink: fix typo in net/netlink/af_netlink.c
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netlink/af_netlink.c
+++ net/netlink/af_netlink.c
@@ -381,7 +381,7 @@ static void netlink_frame_flush_dcache(const struct nl_mmap_hdr *hdr)
 
 	/* First page is flushed through netlink_{get,set}_status */
 	p_start = pgvec_to_page(hdr + PAGE_SIZE);
-	p_end   = pgvec_to_page((void *)hdr + NL_MMAP_MSG_HDRLEN + hdr->nm_len - 1);
+	p_end   = pgvec_to_page((void *)hdr + NL_MMAP_HDRLEN + hdr->nm_len - 1);
 	while (p_start <= p_end) {
 		flush_dcache_page(p_start);
 		p_start++;
