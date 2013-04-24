5edf5ee42a4dfbc500e9b9531335305d4c81a129
Olaf Hering <olaf@aepfle.de>
2013-04-24 07:48:49 -0700

    tools: hv: fix checks for origin of netlink message in hv_vss_daemon
    Similar to what commit 95a69adab9acfc3981c504737a2b6578e4d846ef ("tools:
    hv: Netlink source address validation allows DoS") does in
    hv_kvp_daemon, improve checks for origin of netlink connector message.
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- tools/hv/hv_vss_daemon.c
+++ tools/hv/hv_vss_daemon.c
@@ -186,11 +186,17 @@ int main(void)
 		len = recvfrom(fd, vss_recv_buffer, sizeof(vss_recv_buffer), 0,
 				addr_p, &addr_l);
 
-		if (len < 0 || addr.nl_pid) {
+		if (len < 0) {
 			syslog(LOG_ERR, "recvfrom failed; pid:%u error:%d %s",
 					addr.nl_pid, errno, strerror(errno));
 			close(fd);
 			return -1;
+		}
+
+		if (addr.nl_pid) {
+			syslog(LOG_WARNING, "Received packet from untrusted pid:%u",
+					addr.nl_pid);
+			continue;
 		}
 
 		incoming_msg = (struct nlmsghdr *)vss_recv_buffer;
