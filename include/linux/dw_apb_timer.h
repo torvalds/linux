fc1f7d5606487ae28d6c84e95401952927d7379e
Thomas Gleixner <tglx@linutronix.de>
2013-04-25 20:31:43 +0000

    clocksource: apb_timer: Remove unsused function
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: John Stultz <john.stultz@linaro.org>
    Cc: Magnus Damm <magnus.damm@gmail.com>
    Acked-by: Jamie Iles <jamie@jamieiles.com>
    Link: http://lkml.kernel.org/r/20130425143435.558006195@linutronix.de
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/dw_apb_timer.h
+++ include/linux/dw_apb_timer.h
@@ -51,7 +51,6 @@ dw_apb_clocksource_init(unsigned rating, const char *name, void __iomem *base,
 void dw_apb_clocksource_register(struct dw_apb_clocksource *dw_cs);
 void dw_apb_clocksource_start(struct dw_apb_clocksource *dw_cs);
 cycle_t dw_apb_clocksource_read(struct dw_apb_clocksource *dw_cs);
-void dw_apb_clocksource_unregister(struct dw_apb_clocksource *dw_cs);
 
 extern void dw_apb_timer_init(void);
 #endif /* __DW_APB_TIMER_H__ */
