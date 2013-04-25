03e0221444b7833c9c938a9ca66167aa1d4b1305
Trulan Martin <trulanm@gmail.com>
2013-04-24 21:19:21 -0400

    ALSA: usb-audio: USB quirk for Yamaha THR10C
    This patch adds a USB quirk for the Yamaha THR10C amp.
    
    Signed-off-by: Trulan Martin <trulanm@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/usb/quirks-table.h
+++ sound/usb/quirks-table.h
@@ -405,6 +405,32 @@ YAMAHA_DEVICE(0x105d, NULL),
 		}
 	}
 },
+{
+	USB_DEVICE(0x0499, 0x150c),
+	.driver_info = (unsigned long) & (const struct snd_usb_audio_quirk) {
+		/* .vendor_name = "Yamaha", */
+		/* .product_name = "THR10C", */
+		.ifnum = QUIRK_ANY_INTERFACE,
+		.type = QUIRK_COMPOSITE,
+		.data = (const struct snd_usb_audio_quirk[]) {
+			{
+				.ifnum = 1,
+				.type = QUIRK_AUDIO_STANDARD_INTERFACE
+			},
+			{
+				.ifnum = 2,
+				.type = QUIRK_AUDIO_STANDARD_INTERFACE
+			},
+			{
+				.ifnum = 3,
+				.type = QUIRK_MIDI_YAMAHA
+			},
+			{
+				.ifnum = -1
+			}
+		}
+	}
+},
 YAMAHA_DEVICE(0x2000, "DGP-7"),
 YAMAHA_DEVICE(0x2001, "DGP-5"),
 YAMAHA_DEVICE(0x2002, NULL),
