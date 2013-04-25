6f7ee06f4ec40d2bfc91ec67ee6e40e5fd2f2e59
Mauro Carvalho Chehab <mchehab@redhat.com>
2013-04-25 10:36:56 -0300

    [media] dib8000: warning fix: declare internal functions as static
    drivers/media/dvb-frontends/dib8000.c:2412:5: warning: no previous prototype for 'dib8000_wait_lock' [-Wmissing-prototypes]
    drivers/media/dvb-frontends/dib8000.c:2688:5: warning: no previous prototype for 'dib8000_get_symbol_duration' [-Wmissing-prototypes]
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb-frontends/dib8000.c
+++ drivers/media/dvb-frontends/dib8000.c
@@ -2409,7 +2409,8 @@ static void dib8000_set_isdbt_common_channel(struct dib8000_state *state, u8 seq
 	state->isdbt_cfg_loaded = 0;
 }
 
-u32 dib8000_wait_lock(struct dib8000_state *state, u32 internal, u32 wait0_ms, u32 wait1_ms, u32 wait2_ms)
+static u32 dib8000_wait_lock(struct dib8000_state *state, u32 internal,
+			     u32 wait0_ms, u32 wait1_ms, u32 wait2_ms)
 {
 	u32 value;
 	u16 reg = 11; /* P_search_end0 start addr */
@@ -2685,7 +2686,8 @@ static void dib8000_set_frequency_offset(struct dib8000_state *state)
 }
 
 static u16 LUT_isdbt_symbol_duration[4] = { 26, 101, 63 };
-u32 dib8000_get_symbol_duration(struct dib8000_state *state)
+
+static u32 dib8000_get_symbol_duration(struct dib8000_state *state)
 {
 	u16 i;
 
