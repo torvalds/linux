845a16ab4d6066d763874bc6bf8d641e444934e3
yxj <yxj@rock-chips.com>
2013-04-28 12:18:56 +0800

    lcdc:rename CONFIG_RK616_LVDS to CONFIG_RK616_VIF

--- drivers/video/rockchip/lcdc/rk3188_lcdc.c
+++ drivers/video/rockchip/lcdc/rk3188_lcdc.c
@@ -1394,7 +1394,7 @@ static int __devinit rk3188_lcdc_probe(struct platform_device *pdev)
 	}
 	screen->lcdc_id = lcdc_dev->id;
 	screen->screen_id = 0;
-#if defined(CONFIG_ONE_LCDC_DUAL_OUTPUT_INF)&&  (defined(CONFIG_RK610_LVDS) || defined(CONFIG_RK616_LVDS))
+#if defined(CONFIG_ONE_LCDC_DUAL_OUTPUT_INF)&&  (defined(CONFIG_RK610_LVDS) || defined(CONFIG_RK616_VIF))
 	screen1 =  kzalloc(sizeof(rk_screen), GFP_KERNEL);
 	if(!screen1)
 	{
@@ -1444,7 +1444,7 @@ static int __devinit rk3188_lcdc_probe(struct platform_device *pdev)
 	printk("lcdc%d:reg_phy_base = 0x%08x,reg_vir_base:0x%p\n",pdev->id,lcdc_dev->reg_phy_base, lcdc_dev->regs);
 	lcdc_dev->driver.dev = dev;
 	lcdc_dev->driver.screen0 = screen;
-#if defined(CONFIG_ONE_LCDC_DUAL_OUTPUT_INF)&& (defined(CONFIG_RK610_LVDS) || defined(CONFIG_RK616_LVDS))
+#if defined(CONFIG_ONE_LCDC_DUAL_OUTPUT_INF)&& (defined(CONFIG_RK610_LVDS) || defined(CONFIG_RK616_VIF))
 	lcdc_dev->driver.screen1 = screen1;
 #endif
 	lcdc_dev->driver.cur_screen = screen;
