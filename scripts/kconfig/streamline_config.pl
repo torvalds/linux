bc20d12eca43028548e5a4f655c2070e57bede1c
Steven Rostedt <rostedt@goodmis.org>
2013-04-29 15:17:40 -0400

    localmodconfig: Add debug prints for dependencies of module configs
    When a config for a module is added to the list to save in the final
    config file, add a print to show what dependencies are used. This is
    useful to debug when a config is disabled by the make oldconfig after
    localmodconfig is finished.
    
    This print only appears if the environment variable LOCALMODCONFIG_DEBUG
    is defined.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- scripts/kconfig/streamline_config.pl
+++ scripts/kconfig/streamline_config.pl
@@ -396,6 +396,15 @@ foreach my $module (keys(%modules)) {
 	foreach my $conf (@arr) {
 	    $configs{$conf} = $module;
 	    dprint "$conf added by direct ($module)\n";
+	    if ($debugprint) {
+		my $c=$conf;
+		$c =~ s/^CONFIG_//;
+		if (defined($depends{$c})) {
+		    dprint " deps = $depends{$c}\n";
+		} else {
+		    dprint " no deps\n";
+		}
+	    }
 	}
     } else {
 	# Most likely, someone has a custom (binary?) module loaded.
