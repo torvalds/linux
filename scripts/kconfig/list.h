21ca352b71ca252e1933b1538fe89da8a04395c3
Yann E. MORIN <yann.morin.1998@free.fr>
2013-04-29 19:50:32 +0200

    kconfig: fix lists definition for C++
    The C++ compiler is more strict in that it refuses to assign
    a void* to a struct list_head*.
    
    Fix that by explicitly casting the poisonning constants.
    
    (Tested with all 5 frontends, now.)
    
    Reported-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Benjamin Poirier <bpoirier@suse.de>

--- scripts/kconfig/list.h
+++ scripts/kconfig/list.h
@@ -125,7 +125,7 @@ static inline void __list_del(struct list_head *prev, struct list_head *next)
 static inline void list_del(struct list_head *entry)
 {
 	__list_del(entry->prev, entry->next);
-	entry->next = LIST_POISON1;
-	entry->prev = LIST_POISON2;
+	entry->next = (struct list_head*)LIST_POISON1;
+	entry->prev = (struct list_head*)LIST_POISON2;
 }
 #endif
