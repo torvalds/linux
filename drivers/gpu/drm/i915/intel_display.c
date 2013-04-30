fec32900cc8f4ec8ad6b3007d2035e598f128f24
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-04-30 14:01:43 +0200

    drm/i915: rip out TV-out lore ...
    This seems to be an impressive piece of copy&pasta lore. I've
    checked all docs and on most platforms these bits are all MBZ, with
    the exception of the SDVO pixel multiplier on gen3. On gen4 that
    moved to a special DPLL_MD registers.
    
    No indication whatsoever that we actually need this for native
    TV-out support. I suspect this started as a hack when we didn't
    yet have proper pixel multiplier support in place for SDVO TV, but
    then got stuck in a life of its own.
    
    Just rip it out.
    
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -4589,10 +4589,6 @@ static void i9xx_update_pll(struct intel_crtc *crtc,
 
 	if (is_sdvo && intel_pipe_has_type(&crtc->base, INTEL_OUTPUT_TVOUT))
 		dpll |= PLL_REF_INPUT_TVCLKINBC;
-	else if (intel_pipe_has_type(&crtc->base, INTEL_OUTPUT_TVOUT))
-		/* XXX: just matching BIOS for now */
-		/*	dpll |= PLL_REF_INPUT_TVCLKINBC; */
-		dpll |= 3;
 	else if (intel_pipe_has_type(&crtc->base, INTEL_OUTPUT_LVDS) &&
 		 intel_panel_use_ssc(dev_priv) && num_connectors < 2)
 		dpll |= PLLB_REF_INPUT_SPREADSPECTRUMIN;
