cc8a8d05fdd028e3b4da01680bc8c583a0aa1ed0
Viresh Kumar <viresh.kumar@linaro.org>
2013-05-01 16:31:01 +0530

    cpufreq: interactive: Remove unnecessary cpu_online() check
    Cpufreq no longer calls governor callback for offlined cpus. i.e. All
    policy->cpus are guaranteed to be online. Hence we don't need explicit check to
    see if cpu is online or not.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -1035,9 +1035,6 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 
 	switch (event) {
 	case CPUFREQ_GOV_START:
-		if (!cpu_online(policy->cpu))
-			return -EINVAL;
-
 		mutex_lock(&gov_lock);
 
 		freq_table =
