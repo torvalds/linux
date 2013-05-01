b11b88ef0e07a1ea9a3df6666ba8e15833facc67
Dave Airlie <airlied@redhat.com>
2013-05-01 14:23:41 +1000

    drm/i915: fix dmabuf vmap support
    Sometimes that extra semicolon can really be hard to spot.
    
    Acked-by: Imre Deak <imre.deak@intel.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/i915/i915_gem_dmabuf.c
+++ drivers/gpu/drm/i915/i915_gem_dmabuf.c
@@ -129,7 +129,7 @@ static void *i915_gem_dmabuf_vmap(struct dma_buf *dma_buf)
 		goto error;
 
 	i = 0;
-	for_each_sg_page(obj->pages->sgl, &sg_iter, obj->pages->nents, 0);
+	for_each_sg_page(obj->pages->sgl, &sg_iter, obj->pages->nents, 0)
 		pages[i++] = sg_page_iter_page(&sg_iter);
 
 	obj->dma_buf_vmapping = vmap(pages, i, 0, PAGE_KERNEL);
