fc31d6f55908759530462998d0464a9f124b1c32
Nishanth Menon <nm@ti.com>
2013-05-01 13:38:12 +0000

    cpufreq: cpufreq-cpu0: defer probe when regulator is not ready
    With commit 1e4b545, regulator_get will now return -EPROBE_DEFER
    when the cpu0-supply node is present, but the regulator is not yet
    registered.
    
    It is possible for this to occur when the regulator registration
    by itself might be defered due to some dependent interface not yet
    instantiated. For example: an regulator which uses I2C and GPIO might
    need both systems available before proceeding, in this case, the
    regulator might defer it's registration.
    
    However, the cpufreq-cpu0 driver assumes that any un-successful
    return result is equivalent of failure.
    
    When the regulator_get returns failure other than -EPROBE_DEFER, it
    makes sense to assume that supply node is not present and proceed
    with the assumption that only clock control is necessary in the
    platform.
    
    With this change, we can now handle the following conditions:
     a) cpu0-supply binding is not present, regulator_get will return
        appropriate error result, resulting in cpufreq-cpu0 driver
        controlling just the clock.
     b) cpu0-supply binding is present, regulator_get returns
        -EPROBE_DEFER, we retry resulting in cpufreq-cpu0 driver
        registering later once the regulator is available.
     c) cpu0-supply binding is present, regulator_get returns
        -EPROBE_DEFER, however, regulator never registers, we retry until
        cpufreq-cpu0 driver fails to register pointing at device tree
        information bug. However, in this case, the fact that
        cpufreq-cpu0 operates with clock only when the DT binding clearly
        indicates need of a supply is a bug of it's own.
     d) cpu0-supply gets an regulator at probe - cpufreq-cpu0 driver
        controls both the clock and regulator
    
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Acked-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/cpufreq-cpu0.c
+++ drivers/cpufreq/cpufreq-cpu0.c
@@ -195,17 +195,27 @@ static int cpu0_cpufreq_probe(struct platform_device *pdev)
 	cpu_dev = &pdev->dev;
 	cpu_dev->of_node = np;
 
+	cpu_reg = devm_regulator_get(cpu_dev, "cpu0");
+	if (IS_ERR(cpu_reg)) {
+		/*
+		 * If cpu0 regulator supply node is present, but regulator is
+		 * not yet registered, we should try defering probe.
+		 */
+		if (PTR_ERR(cpu_reg) == -EPROBE_DEFER) {
+			dev_err(cpu_dev, "cpu0 regulator not ready, retry\n");
+			ret = -EPROBE_DEFER;
+			goto out_put_node;
+		}
+		pr_warn("failed to get cpu0 regulator: %ld\n",
+			PTR_ERR(cpu_reg));
+		cpu_reg = NULL;
+	}
+
 	cpu_clk = devm_clk_get(cpu_dev, NULL);
 	if (IS_ERR(cpu_clk)) {
 		ret = PTR_ERR(cpu_clk);
 		pr_err("failed to get cpu0 clock: %d\n", ret);
 		goto out_put_node;
-	}
-
-	cpu_reg = devm_regulator_get(cpu_dev, "cpu0");
-	if (IS_ERR(cpu_reg)) {
-		pr_warn("failed to get cpu0 regulator\n");
-		cpu_reg = NULL;
 	}
 
 	ret = of_init_opp_table(cpu_dev);
