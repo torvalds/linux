9b3eb5edf33897dc9128aa27300066153d4f8b9c
Pravin B Shelar <pshelar@nicira.com>
2013-05-02 16:14:19 +0000

    gre: Fix GREv4 TCPv6 segmentation.
    For ipv6 traffic, GRE can generate packet with strange GSO
    bits, e.g. ipv4 packet with SKB_GSO_TCPV6 flag set.  Therefore
    following patch relaxes check in inet gso handler to allow
    such packet for segmentation.
    This patch also fixes wrong skb->protocol set that was done in
    gre_gso_segment() handler.
    
    Reported-by: Steinar H. Gunderson <sesse@google.com>
    CC: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: Pravin B Shelar <pshelar@nicira.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/gre.c
+++ net/ipv4/gre.c
@@ -121,6 +121,7 @@ static struct sk_buff *gre_gso_segment(struct sk_buff *skb,
 	int ghl = GRE_HEADER_SECTION;
 	struct gre_base_hdr *greh;
 	int mac_len = skb->mac_len;
+	__be16 protocol = skb->protocol;
 	int tnl_hlen;
 	bool csum;
 
@@ -150,7 +151,7 @@ static struct sk_buff *gre_gso_segment(struct sk_buff *skb,
 
 	/* setup inner skb. */
 	if (greh->protocol == htons(ETH_P_TEB)) {
-		struct ethhdr *eth = eth_hdr(skb);
+		struct ethhdr *eth = (struct ethhdr *)skb_inner_mac_header(skb);
 		skb->protocol = eth->h_proto;
 	} else {
 		skb->protocol = greh->protocol;
@@ -199,6 +200,7 @@ static struct sk_buff *gre_gso_segment(struct sk_buff *skb,
 		skb_reset_mac_header(skb);
 		skb_set_network_header(skb, mac_len);
 		skb->mac_len = mac_len;
+		skb->protocol = protocol;
 	} while ((skb = skb->next));
 out:
 	return segs;
