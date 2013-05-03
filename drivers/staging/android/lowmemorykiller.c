fd7509d9e36b2deb2ff4428d8189d6320a29d19a
Colin Cross <ccross@android.com>
2013-05-03 14:57:29 -0700

    lowmemorykiller: make default lowmemorykiller debug message useful
    lowmemorykiller debug messages are inscrutable and mostly useful
    for debugging the lowmemorykiller, not explaining why a process
    was killed.  Make the messages more useful by prefixing them
    with "lowmemorykiller: " and explaining in more readable terms
    what was killed, who it was killed for, and why it was killed.
    
    The messages now look like:
    [   76.997631] lowmemorykiller: Killing 'droid.gallery3d' (2172), adj 1000,
    [   76.997635]    to free 27436kB on behalf of 'kswapd0' (29) because
    [   76.997638]    cache 122624kB is below limit 122880kB for oom_score_adj 1000
    [   76.997641]    Free memory is -53356kB above reserved
    
    A negative number for free memory above reserved means some of the
    reserved memory has been used and is being regenerated by kswapd,
    which is likely what called the shrinkers.
    
    Change-Id: I1fe983381e73e124b90aa5d91cb66e55eaca390f
    Signed-off-by: Colin Cross <ccross@android.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -82,6 +82,7 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	int tasksize;
 	int i;
 	short min_score_adj = OOM_SCORE_ADJ_MAX + 1;
+	int minfree = 0;
 	int selected_tasksize = 0;
 	short selected_oom_score_adj;
 	int array_size = ARRAY_SIZE(lowmem_adj);
@@ -94,8 +95,8 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	if (lowmem_minfree_size < array_size)
 		array_size = lowmem_minfree_size;
 	for (i = 0; i < array_size; i++) {
-		if (other_free < lowmem_minfree[i] &&
-		    other_file < lowmem_minfree[i]) {
+		minfree = lowmem_minfree[i];
+		if (other_free < minfree && other_file < minfree) {
 			min_score_adj = lowmem_adj[i];
 			break;
 		}
@@ -150,13 +151,22 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		selected = p;
 		selected_tasksize = tasksize;
 		selected_oom_score_adj = oom_score_adj;
-		lowmem_print(2, "select %d (%s), adj %hd, size %d, to kill\n",
-			     p->pid, p->comm, oom_score_adj, tasksize);
+		lowmem_print(2, "select '%s' (%d), adj %hd, size %d, to kill\n",
+			     p->comm, p->pid, oom_score_adj, tasksize);
 	}
 	if (selected) {
-		lowmem_print(1, "send sigkill to %d (%s), adj %hd, size %d\n",
-			     selected->pid, selected->comm,
-			     selected_oom_score_adj, selected_tasksize);
+		lowmem_print(1, "Killing '%s' (%d), adj %hd,\n" \
+				"   to free %ldkB on behalf of '%s' (%d) because\n" \
+				"   cache %ldkB is below limit %ldkB for oom_score_adj %hd\n" \
+				"   Free memory is %ldkB above reserved\n",
+			     selected->comm, selected->pid,
+			     selected_oom_score_adj,
+			     selected_tasksize * (long)(PAGE_SIZE / 1024),
+			     current->comm, current->pid,
+			     other_file * (long)(PAGE_SIZE / 1024),
+			     minfree * (long)(PAGE_SIZE / 1024),
+			     min_score_adj,
+			     other_free * (long)(PAGE_SIZE / 1024));
 		lowmem_deathpending_timeout = jiffies + HZ;
 		send_sig(SIGKILL, selected, 0);
 		set_tsk_thread_flag(selected, TIF_MEMDIE);
