c8712a53671aef12a8b6b3ec4e942ef4f92bc17e
Sachin Kamat <sachin.kamat@linaro.org>
2013-05-06 07:35:41 -0700

    Input: pxa27x_keypad - remove redundant platform_set_drvdata()
    Commit 0998d06310 (device-core: Ensure drvdata = NULL when no
    driver is bound) removes the need to set driver data field to
    NULL.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/keyboard/pxa27x_keypad.c
+++ drivers/input/keyboard/pxa27x_keypad.c
@@ -609,7 +609,6 @@ static int pxa27x_keypad_remove(struct platform_device *pdev)
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	release_mem_region(res->start, resource_size(res));
 
-	platform_set_drvdata(pdev, NULL);
 	kfree(keypad);
 
 	return 0;
