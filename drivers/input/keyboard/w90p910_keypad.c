d52014556d8a1f3be57db4f04d6fdce06af87493
Sachin Kamat <sachin.kamat@linaro.org>
2013-05-06 07:39:36 -0700

    Input: w90p910_keypad - remove redundant platform_set_drvdata()
    Commit 0998d06310 (device-core: Ensure drvdata = NULL when no
    driver is bound) removes the need to set driver data field to
    NULL.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Cc: Wan ZongShun <mcuos.com@gmail.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/keyboard/w90p910_keypad.c
+++ drivers/input/keyboard/w90p910_keypad.c
@@ -249,7 +249,6 @@ static int w90p910_keypad_remove(struct platform_device *pdev)
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	release_mem_region(res->start, resource_size(res));
 
-	platform_set_drvdata(pdev, NULL);
 	kfree(keypad);
 
 	return 0;
