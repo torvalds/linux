1e2b3bdaa770fa2d6fe8435e57cd2452d91cd18b
Geert Uytterhoeven <geert@linux-m68k.org>
2013-05-07 10:27:53 +0200

    h8300: Fix <asm/tlb.h>
    Ten years ago, a mismerge happened, concatenating two slightly different
    versions of the same file. As a consequence, <asm-generic/tlb.h> was never
    included, leading to a build failure only now:
    
    kernel/cpu/idle.c: In function 'cpu_idle_loop':
    kernel/cpu/idle.c:70:4: error: implicit declaration of function 'check_pgt_cache' [-Werror=implicit-function-declaration]
    
    Remove the duplicates, and the header comment with the no longer correct
    file name.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Acked-by: Yoshinori Sato <ysato@users.sourceforge.jp>

--- arch/h8300/include/asm/tlb.h
+++ arch/h8300/include/asm/tlb.h
@@ -1,23 +1,8 @@
-/* 
-  include/asm-h8300/tlb.h 
-*/
-
-#ifndef __H8300_TLB_H__
-#define __H8300_TLB_H__
-
-#define tlb_flush(tlb)	do { } while(0)
-
-/* 
-  include/asm-h8300/tlb.h 
-*/
-
 #ifndef __H8300_TLB_H__
 #define __H8300_TLB_H__
 
 #define tlb_flush(tlb)	do { } while(0)
 
 #include <asm-generic/tlb.h>
-
-#endif
 
 #endif
