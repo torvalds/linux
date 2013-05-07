33dee0532c7652d6759949f562857733f13b8de6
许盛飞 <xsf@rock-chips.com>
2013-05-07 16:42:36 +0800

    charging-displasy: when mode_normal and mode_charge

--- drivers/power/rk29_charger_display.c
+++ drivers/power/rk29_charger_display.c
@@ -166,7 +166,7 @@ static int  __init start_charge_logo_display(void)
 
 	if(val_status.intval == POWER_SUPPLY_STATUS_CHARGING)
 	{
-		if ((board_boot_mode() != BOOT_MODE_REBOOT) || (val_capacity.intval <= pwr_on_thrsd))  //do not enter power on charge mode when soft  reset
+		if ((board_boot_mode() == BOOT_MODE_NORMAL) ||(board_boot_mode() == BOOT_MODE_CHARGE)|| (val_capacity.intval <= pwr_on_thrsd))  //do not enter power on charge mode when soft  reset
 	    {			
 			add_bootmode_charger_to_cmdline();
 			//boot_mode_init("charge");
