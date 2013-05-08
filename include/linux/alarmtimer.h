7b71ddcebb443992703f409994bebb3a3121bfc8
Todd Poynor <toddpoynor@google.com>
2013-05-07 20:43:29 -0700

    alarmtimer: add alarm_expires_remaining
    Similar to hrtimer_expires_remaining, return the amount of time
    remaining until alarm expiry.
    
    Change-Id: I8c57512d619ac66bcdaf2d9ccdf0d7f74af2ff66
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- include/linux/alarmtimer.h
+++ include/linux/alarmtimer.h
@@ -51,6 +51,7 @@ int alarm_cancel(struct alarm *alarm);
 
 u64 alarm_forward(struct alarm *alarm, ktime_t now, ktime_t interval);
 u64 alarm_forward_now(struct alarm *alarm, ktime_t interval);
+ktime_t alarm_expires_remaining(const struct alarm *alarm);
 
 /* Provide way to access the rtc device being used by alarmtimers */
 struct rtc_device *alarmtimer_get_rtcdev(void);
