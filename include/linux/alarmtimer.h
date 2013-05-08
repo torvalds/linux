48f6005e9393c9cd0ec2c84361e0fa93c3966401
Todd Poynor <toddpoynor@google.com>
2013-05-07 20:43:29 -0700

    alarmtimer: add alarm_expires_remaining
    Similar to hrtimer_expires_remaining, return the amount of time
    remaining until alarm expiry.
    
    Change-Id: I8c57512d619ac66bcdaf2d9ccdf0d7f74af2ff66
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- include/linux/alarmtimer.h
+++ include/linux/alarmtimer.h
@@ -52,6 +52,7 @@ int alarm_cancel(struct alarm *alarm);
 
 u64 alarm_forward(struct alarm *alarm, ktime_t now, ktime_t interval);
 u64 alarm_forward_now(struct alarm *alarm, ktime_t interval);
+ktime_t alarm_expires_remaining(const struct alarm *alarm);
 
 /*
  * A alarmtimer is active, when it is enqueued into timerqueue or the
--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -199,6 +199,12 @@ static enum hrtimer_restart alarmtimer_fired(struct hrtimer *timer)
 
 }
 
+ktime_t alarm_expires_remaining(const struct alarm *alarm)
+{
+	struct alarm_base *base = &alarm_bases[alarm->type];
+	return ktime_sub(alarm->node.expires, base->gettime());
+}
+
 #ifdef CONFIG_RTC_CLASS
 /**
  * alarmtimer_suspend - Suspend time callback
