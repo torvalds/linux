ec66fa45a23d3317a73e6dc0fb14e84dc3d3fbd8
Todd Poynor <toddpoynor@google.com>
2013-05-08 15:49:18 -0700

    alarmtimer: add alarm_restart
    Analogous to hrtimer_restart, restart an alarmtimer after the expires
    time has already been updated (as with alarm_forward).
    
    Change-Id: Ia2613bbb467404cb2c35c11efa772bc56294963a
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -324,6 +324,18 @@ int alarm_start(struct alarm *alarm, ktime_t start)
 	return ret;
 }
 
+void alarm_restart(struct alarm *alarm)
+{
+	struct alarm_base *base = &alarm_bases[alarm->type];
+	unsigned long flags;
+
+	spin_lock_irqsave(&base->lock, flags);
+	hrtimer_set_expires(&alarm->timer, alarm->node.expires);
+	hrtimer_restart(&alarm->timer);
+	alarmtimer_enqueue(base, alarm);
+	spin_unlock_irqrestore(&base->lock, flags);
+}
+
 /**
  * alarm_try_to_cancel - Tries to cancel an alarm timer
  * @alarm: ptr to alarm to be canceled
