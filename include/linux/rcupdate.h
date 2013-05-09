feed66ed26a53e700ca02ce1744fed7d0c647292
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2013-05-09 08:55:54 -0700

    rcu: Eliminate unused APIs intended for adaptive ticks
    The rcu_user_enter_after_irq() and rcu_user_exit_after_irq()
    functions were intended for use by adaptive ticks, but changes
    in implementation have rendered them unnecessary.  This commit
    therefore removes them.
    
    Reported-by: Frederic Weisbecker <fweisbec@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -229,13 +229,9 @@ extern void rcu_irq_exit(void);
 #ifdef CONFIG_RCU_USER_QS
 extern void rcu_user_enter(void);
 extern void rcu_user_exit(void);
-extern void rcu_user_enter_after_irq(void);
-extern void rcu_user_exit_after_irq(void);
 #else
 static inline void rcu_user_enter(void) { }
 static inline void rcu_user_exit(void) { }
-static inline void rcu_user_enter_after_irq(void) { }
-static inline void rcu_user_exit_after_irq(void) { }
 static inline void rcu_user_hooks_switch(struct task_struct *prev,
 					 struct task_struct *next) { }
 #endif /* CONFIG_RCU_USER_QS */
