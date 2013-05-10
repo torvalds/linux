486dd6d305ddf6229011e23226416b179425ca23
Sachin Kamat <sachin.kamat@linaro.org>
2013-05-10 17:25:08 +0530

    video: imxfb: Make local symbols static
    These symbols are used only in this file.
    Make them static.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/imxfb.c
+++ drivers/video/imxfb.c
@@ -957,7 +957,7 @@ static int imxfb_remove(struct platform_device *pdev)
 	return 0;
 }
 
-void  imxfb_shutdown(struct platform_device * dev)
+static void imxfb_shutdown(struct platform_device *dev)
 {
 	struct fb_info *info = platform_get_drvdata(dev);
 	struct imxfb_info *fbi = info->par;
@@ -996,7 +996,7 @@ static int imxfb_setup(void)
 	return 0;
 }
 
-int __init imxfb_init(void)
+static int __init imxfb_init(void)
 {
 	int ret = imxfb_setup();
 
