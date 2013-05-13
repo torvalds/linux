4fa94e224b84be7b2522a0f5ce5b64124f146fac
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-05-13 01:57:37 -0300

    [media] vpif_capture: fix error return code in vpif_probe()
    Fix to return -ENODEV in the subdevice register error handling
    case instead of 0, as done elsewhere in this function.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Acked-by: Lad, Prabhakar <prabhakar.csengg@gmail.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/davinci/vpif_capture.c
+++ drivers/media/platform/davinci/vpif_capture.c
@@ -2159,6 +2159,7 @@ static __init int vpif_probe(struct platform_device *pdev)
 
 		if (!vpif_obj.sd[i]) {
 			vpif_err("Error registering v4l2 subdevice\n");
+			err = -ENODEV;
 			goto probe_subdev_out;
 		}
 		v4l2_info(&vpif_obj.v4l2_dev, "registered sub device %s\n",
