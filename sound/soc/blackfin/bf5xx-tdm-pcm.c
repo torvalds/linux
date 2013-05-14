19b6317b8c78a51c41b3d7abf14fdbb0df8f41b7
Lars-Peter Clausen <lars@metafoo.de>
2013-05-14 22:19:53 +0200

    ASoC: bf5xx-tdm-pcm: Staticize non exported struct
    The bf5xx_tdm_pcm_ops struct is only used in bf5xx-tdm-pcm.c, so make it static.
    
    Cc: Scott Jiang <scott.jiang.linux@gmail.com>
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/blackfin/bf5xx-tdm-pcm.c
+++ sound/soc/blackfin/bf5xx-tdm-pcm.c
@@ -229,8 +229,7 @@ static int bf5xx_pcm_silence(struct snd_pcm_substream *substream,
 	return 0;
 }
 
-
-struct snd_pcm_ops bf5xx_pcm_tdm_ops = {
+static struct snd_pcm_ops bf5xx_pcm_tdm_ops = {
 	.open           = bf5xx_pcm_open,
 	.ioctl          = snd_pcm_lib_ioctl,
 	.hw_params      = bf5xx_pcm_hw_params,
