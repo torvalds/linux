c21bec86b6e36143a1fc0be60bbd9dfaebcb88a0
Zhang Rui <rui.zhang@intel.com>
2013-05-16 02:16:21 +0000

    Thermal: don't check resource with devm_ioremap_resource
    devm_ioremap_resource does sanity checks on the given resource.
    No need to duplicate this in the driver.
    
    CC: Ezequiel Garcia <ezequiel.garcia@free-electrons.com>
    CC: Vincenzo Frascino <vincenzo.frascino@st.com>
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    Acked-by: Ezequiel Garcia <ezequiel.garcia@free-electrons.com>

--- drivers/thermal/spear_thermal.c
+++ drivers/thermal/spear_thermal.c
@@ -118,18 +118,11 @@ static int spear_thermal_probe(struct platform_device *pdev)
 		return -ENOMEM;
 	}
 
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	if (!res) {
-		dev_err(&pdev->dev, "memory resource missing\n");
-		return -ENODEV;
-	}
-
 	/* Enable thermal sensor */
-	stdev->thermal_base = devm_ioremap_resource(dev, res);
-	if (IS_ERR(stdev->thermal_base)) {
-		dev_err(&pdev->dev, "ioremap failed\n");
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	stdev->thermal_base = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(stdev->thermal_base))
 		return PTR_ERR(stdev->thermal_base);
-	}
 
 	stdev->clk = devm_clk_get(&pdev->dev, NULL);
 	if (IS_ERR(stdev->clk)) {
