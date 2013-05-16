2b52d1e3488970a12101d0c98a072249f4bb36b5
Elena Ufimtseva <ufimtseva@gmail.com>
2013-05-16 12:47:07 -0400

    Staging: ced1401: Patch removes unused typedefs.
    Patch removes unused typedefs BOOLEAN, *LPCSTR, *LPCSTR, UCHAR, *LPBYTE.
    
    Signed-off-by: Elena Ufimtseva <ufimtseva@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ced1401/machine.h
+++ drivers/staging/ced1401/machine.h
@@ -80,13 +80,8 @@
 	#define FAR
 
 	typedef int BOOL;       /*  To match Windows */
-	typedef char *LPSTR;
-	typedef const char *LPCSTR;
 	typedef unsigned char  BYTE;
-	typedef BYTE  BOOLEAN;
-	typedef unsigned char UCHAR;
 	#define __packed __attribute__((packed))
-	typedef BYTE *LPBYTE;
 	#define HIWORD(x) (unsigned short)(((x)>>16) & 0xffff)
 	#define LOWORD(x) (unsigned short)((x) & 0xffff)
 #endif
