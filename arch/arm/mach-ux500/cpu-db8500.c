946cc7dd7d43a2c8bb1c5c2c19f35b8e28142044
Lee Jones <lee.jones@linaro.org>
2013-05-16 12:27:23 +0100

    ARM: ux500: Provide an AUXDATA entry for ux500-crypt
    This provides a device name which is required by the common clk API.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    [Edited patch subject]
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/cpu-db8500.c
+++ arch/arm/mach-ux500/cpu-db8500.c
@@ -277,6 +277,7 @@ static struct of_dev_auxdata u8500_auxdata_lookup[] __initdata = {
 	OF_DEV_AUXDATA("stericsson,db8500-prcmu", 0x80157000, "db8500-prcmu",
 			&db8500_prcmu_pdata),
 	OF_DEV_AUXDATA("smsc,lan9115", 0x50000000, "smsc911x.0", NULL),
+	OF_DEV_AUXDATA("stericsson,ux500-cryp", 0xa03cb000, "cryp1", NULL),
 	/* Requires device name bindings. */
 	OF_DEV_AUXDATA("stericsson,nmk-pinctrl", U8500_PRCMU_BASE,
 		"pinctrl-db8500", NULL),
