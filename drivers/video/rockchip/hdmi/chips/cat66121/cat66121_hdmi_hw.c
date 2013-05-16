966580fc852e1020767c699b23388d31c51214ff
yzq <yzq@rock-chips.com>
2013-05-16 10:44:23 +0800

    it66121 hdmi: remove deviceId check because it maybe be changed when the chip upgraded

--- drivers/video/rockchip/hdmi/chips/cat66121/cat66121_hdmi_hw.c
+++ drivers/video/rockchip/hdmi/chips/cat66121/cat66121_hdmi_hw.c
@@ -174,12 +174,12 @@ int cat66121_detect_device(void)
 	VendorID1 = HDMITX_ReadI2C_Byte(REG_TX_VENDOR_ID1);
 	DeviceID0 = HDMITX_ReadI2C_Byte(REG_TX_DEVICE_ID0);
 	DeviceID1 = HDMITX_ReadI2C_Byte(REG_TX_DEVICE_ID1);
-	if( (VendorID0 == 0x54) && (VendorID1 == 0x49) &&
-			(DeviceID0 == 0x12) && (DeviceID1 == 0x16) )
-		return 1;
-
 	printk("CAT66121: Reg[0-3] = 0x[%02x].[%02x].[%02x].[%02x]\n",
 			VendorID0, VendorID1, DeviceID0, DeviceID1);
+	if( (VendorID0 == 0x54) && (VendorID1 == 0x49))
+	   //    	&&(DeviceID0 == 0x12) && (DeviceID1 == 0x16) )
+		return 1;
+
 	printk("[CAT66121] Device not found!\n");
 
 	return 0;
