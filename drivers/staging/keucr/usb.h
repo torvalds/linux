c41d74c31e36d218b81c5bb6c6b62f0ada69f2db
Amarjargal Gundjalam <amarjargal16@gmail.com>
2013-05-17 01:03:41 -0700

    keucr: fixes open brace go on the next line error
    This patch fixes the following checkpatch error,
    ERROR: open brace '{' following function declarations go on the next
    line
    
    Signed-off-by: Amarjargal Gundjalam <amarjargal16@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/keucr/usb.h
+++ drivers/staging/keucr/usb.h
@@ -218,10 +218,12 @@ struct us_data {
 };
 
 /* Convert between us_data and the corresponding Scsi_Host */
-static inline struct Scsi_Host *us_to_host(struct us_data *us) {
+static inline struct Scsi_Host *us_to_host(struct us_data *us)
+{
 	return container_of((void *) us, struct Scsi_Host, hostdata);
 }
-static inline struct us_data *host_to_us(struct Scsi_Host *host) {
+static inline struct us_data *host_to_us(struct Scsi_Host *host)
+{
 	return (struct us_data *) host->hostdata;
 }
 
