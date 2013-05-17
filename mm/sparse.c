9d1936cf86be8dc0cc27365bd8f1efdf23941961
Zhang Yanfei <zhangyanfei@cn.fujitsu.com>
2013-05-17 22:10:38 +0800

    mm/sparse: Remove unused ret in sparse_index_init
    The ret variable is not used in the function, so remove it and
    directly return 0 at the end of the function.
    
    Signed-off-by: Zhang Yanfei <zhangyanfei@cn.fujitsu.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- mm/sparse.c
+++ mm/sparse.c
@@ -79,7 +79,6 @@ static int __meminit sparse_index_init(unsigned long section_nr, int nid)
 {
 	unsigned long root = SECTION_NR_TO_ROOT(section_nr);
 	struct mem_section *section;
-	int ret = 0;
 
 	if (mem_section[root])
 		return -EEXIST;
@@ -90,7 +89,7 @@ static int __meminit sparse_index_init(unsigned long section_nr, int nid)
 
 	mem_section[root] = section;
 
-	return ret;
+	return 0;
 }
 #else /* !SPARSEMEM_EXTREME */
 static inline int sparse_index_init(unsigned long section_nr, int nid)
