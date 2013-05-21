502f769662978a2fe99d0caed5e53e3006107381
Shuah Khan <shuah.kh@samsung.com>
2013-05-21 09:32:06 -0600

    Bluetooth: Add missing reset_resume dev_pm_ops
    Add missing reset_resume dev_pm_ops. Missing reset_resume results in the
    following message after power management device test. This change sets
    reset_resume to btusb_resume().
    
    [ 2506.936134] btusb 1-1.5:1.0: no reset_resume for driver btusb?
    [ 2506.936137] btusb 1-1.5:1.1: no reset_resume for driver btusb?
    
    Signed-off-by: Shuah Khan <shuah.kh@samsung.com>
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -1619,6 +1619,7 @@ static struct usb_driver btusb_driver = {
 #ifdef CONFIG_PM
 	.suspend	= btusb_suspend,
 	.resume		= btusb_resume,
+	.reset_resume	= btusb_resume,
 #endif
 	.id_table	= btusb_table,
 	.supports_autosuspend = 1,
