735ae5392a622e88ebe9dda1f66d80850580239d
Severin Gsponer <svgsponer@gmail.com>
2013-05-22 01:46:02 +0200

    Staging: bcm: fix checkpatch errors and warnings in Version.h
    This patch change the comment style to C89 and removes a unnecessary line.
    
    Signed-off-by: Severin Gsponer <svgsponer@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/Version.h
+++ drivers/staging/bcm/Version.h
@@ -1,4 +1,3 @@
-
 /*Copyright (c) 2005 Beceem Communications Inc.
 
 Module Name:
@@ -17,7 +16,6 @@ Abstract:
 #define VER_FILETYPE                VFT_DRV
 #define VER_FILESUBTYPE             VFT2_DRV_NETWORK
 
-
 #define VER_FILEVERSION             5.2.45
 #define VER_FILEVERSION_STR         "5.2.45"
 
@@ -28,8 +26,4 @@ Abstract:
 #define VER_PRODUCTVERSION_STR      VER_FILEVERSION_STR
 
 
-
-
-//#include "common.ver"
-
-#endif 	//VERSION_H
+#endif /* VERSION_H */
