70c40a3a834d58a96a02777e379ddc31b4d66cc1
Mika Westerberg <mika.westerberg@linux.intel.com>
2013-05-22 13:03:11 +0300

    i2c: designware: prevent signals from aborting I2C transfers
    If a process receives signal while it is waiting for I2C transfer to
    complete, an error is returned to the caller and the transfer is aborted.
    This can cause the driver to fail subsequent transfers. Also according to
    commit d295a86eab2 (i2c: mv64xxx: work around signals causing I2C
    transactions to be aborted) I2C drivers aren't supposed to abort
    transactions on signals.
    
    To prevent this switch to use wait_for_completion_timeout() instead of
    wait_for_completion_interruptible_timeout() in the designware I2C driver.
    
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Reviewed-by: Christian Ruppert <christian.ruppert@abilis.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>
    (cherry picked from commit e42dba569fceca5d59a88571370785e9ce9775b8)
    Signed-off-by: Darren Hart <dvhart@linux.intel.com>

--- drivers/i2c/busses/i2c-designware-core.c
+++ drivers/i2c/busses/i2c-designware-core.c
@@ -580,14 +580,13 @@ i2c_dw_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 	i2c_dw_xfer_init(dev);
 
 	/* wait for tx to complete */
-	ret = wait_for_completion_interruptible_timeout(&dev->cmd_complete, HZ);
+	ret = wait_for_completion_timeout(&dev->cmd_complete, HZ);
 	if (ret == 0) {
 		dev_err(dev->dev, "controller timed out\n");
 		i2c_dw_init(dev);
 		ret = -ETIMEDOUT;
 		goto done;
-	} else if (ret < 0)
-		goto done;
+	}
 
 	if (dev->msg_err) {
 		ret = dev->msg_err;
