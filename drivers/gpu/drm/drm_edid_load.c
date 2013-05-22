8d06cd0af6c50a26d87ce78310ad38b9b40fa795
Thomas Meyer <thomas@m3y3r.de>
2013-05-22 21:06:30 +0000

    drm: Cocci spatch "memdup.spatch"
    Signed-off-by: Thomas Meyer <thomas@m3y3r.de>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/drm_edid_load.c
+++ drivers/gpu/drm/drm_edid_load.c
@@ -186,12 +186,11 @@ static u8 *edid_load(struct drm_connector *connector, char *name,
 		goto relfw_out;
 	}
 
-	edid = kmalloc(fwsize, GFP_KERNEL);
+	edid = kmemdup(fwdata, fwsize, GFP_KERNEL);
 	if (edid == NULL) {
 		err = -ENOMEM;
 		goto relfw_out;
 	}
-	memcpy(edid, fwdata, fwsize);
 
 	if (!drm_edid_block_valid(edid, 0, print_bad_edid)) {
 		connector->bad_edid_counter++;
