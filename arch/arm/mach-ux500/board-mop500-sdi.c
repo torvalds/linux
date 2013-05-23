4fa0614f40bfe8434d843da82664a6da06915bc1
Ulf Hansson <ulf.hansson@linaro.org>
2013-05-23 15:13:46 +0200

    ARM: ux500: Don't set plf ocr mask for SD/MMC device
    The mmci host driver overrides platform ocr mask when a vmmc regulator
    can be used. This is the case for sdi0(SD-card) and for sdi4(eMMC).
    Thus it is pointless of specifing these mask and why we do remove them.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/board-mop500-sdi.c
+++ arch/arm/mach-ux500/board-mop500-sdi.c
@@ -52,7 +52,6 @@ static struct stedma40_chan_cfg mop500_sdi0_dma_cfg_tx = {
 #endif
 
 struct mmci_platform_data mop500_sdi0_data = {
-	.ocr_mask	= MMC_VDD_29_30,
 	.f_max		= 100000000,
 	.capabilities	= MMC_CAP_4_BIT_DATA |
 				MMC_CAP_SD_HIGHSPEED |
@@ -180,7 +179,6 @@ static struct stedma40_chan_cfg mop500_sdi4_dma_cfg_tx = {
 #endif
 
 struct mmci_platform_data mop500_sdi4_data = {
-	.ocr_mask	= MMC_VDD_29_30,
 	.f_max		= 100000000,
 	.capabilities	= MMC_CAP_4_BIT_DATA | MMC_CAP_8_BIT_DATA |
 			  MMC_CAP_MMC_HIGHSPEED,
