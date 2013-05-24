d211c6e82435dfa4ae9f4d80bb2ee75930bde5d4
Axel Lin <axel.lin@ingics.com>
2013-05-25 01:41:44 +0800

    generic-adc-battery: Fix checking if none of the channels are supported
    If none of the channels are supported, index is 0. Also ensure to return
    error code instead of 0 in goto second_mem_fail path.
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Acked-by: anish kumar <anish198519851985@gmail.com>
    Signed-off-by: Anton Vorontsov <anton@enomsg.org>

--- drivers/power/generic-adc-battery.c
+++ drivers/power/generic-adc-battery.c
@@ -299,8 +299,10 @@ static int gab_probe(struct platform_device *pdev)
 	}
 
 	/* none of the channels are supported so let's bail out */
-	if (index == ARRAY_SIZE(gab_chan_name))
+	if (index == 0) {
+		ret = -ENODEV;
 		goto second_mem_fail;
+	}
 
 	/*
 	 * Total number of properties is equal to static properties
