9ae6a79ed21c686aedbcef04cf76833f33ea1d2a
James Hogan <james.hogan@imgtec.com>
2013-05-24 17:21:11 +0100

    pinconf-generic: add drive strength to debugfs output
    Add the drive strength pinconf to debugfs output (with the unit "mA").
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>
    Cc: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    (cherry picked from commit 73ae368cd309dae277b66444d471ac62825ee407)
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/pinctrl/pinconf-generic.c
+++ drivers/pinctrl/pinconf-generic.c
@@ -42,6 +42,7 @@ static struct pin_config_item conf_items[] = {
 	PCONFDUMP(PIN_CONFIG_DRIVE_PUSH_PULL, "output drive push pull", NULL),
 	PCONFDUMP(PIN_CONFIG_DRIVE_OPEN_DRAIN, "output drive open drain", NULL),
 	PCONFDUMP(PIN_CONFIG_DRIVE_OPEN_SOURCE, "output drive open source", NULL),
+	PCONFDUMP(PIN_CONFIG_DRIVE_STRENGTH, "output drive strength", "mA"),
 	PCONFDUMP(PIN_CONFIG_INPUT_SCHMITT_ENABLE, "input schmitt enabled", NULL),
 	PCONFDUMP(PIN_CONFIG_INPUT_SCHMITT, "input schmitt trigger", NULL),
 	PCONFDUMP(PIN_CONFIG_INPUT_DEBOUNCE, "input debounce", "time units"),
