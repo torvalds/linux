24b1944fc6b7be857c81f0cb0ff264c57b85ee29
Chen Gang <gang.chen@asianux.com>
2013-05-26 07:01:46 +0000

    MIPS: Ralink: Use strlcpy() instead of strncpy().
    'compatible' is used by strlen() in __of_device_is_compatible().
    
    Ensure strings are always '\0' terminated.
    
    'of_ids is not a structure in "include/uapi/*", so no need to initialize
    it completly; using strlcpy() instead of strncpy() will do.
    
    Signed-off-by: Chen Gang <gang.chen@asianux.com>
    Acked-by: John Crispin <blogic@openwrt.org>
    Cc: juhosg@openwrt.org
    Cc: linux-mips@linux-mips.org
    Cc: Linux-Arch <linux-arch@vger.kernel.org>
    Patchwork: https://patchwork.linux-mips.org/patch/5330/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/ralink/of.c
+++ arch/mips/ralink/of.c
@@ -104,7 +104,7 @@ static int __init plat_of_setup(void)
 	if (!of_have_populated_dt())
 		panic("device tree not present");
 
-	strncpy(of_ids[0].compatible, soc_info.compatible, len);
+	strlcpy(of_ids[0].compatible, soc_info.compatible, len);
 	strncpy(of_ids[1].compatible, "palmbus", len);
 
 	if (of_platform_populate(NULL, of_ids, NULL, NULL))
