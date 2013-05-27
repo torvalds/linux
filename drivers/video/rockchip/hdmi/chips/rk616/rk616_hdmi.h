e556297119603e5d4d6f80d599c341b41d6f7592
xuhuicong <xhc@rock-chips.com>
2013-05-27 15:12:08 +0800

    rk616: fix rk616 hdmi no sound, select i2s source default

--- drivers/video/rockchip/hdmi/chips/rk616/rk616_hdmi.h
+++ drivers/video/rockchip/hdmi/chips/rk616/rk616_hdmi.h
@@ -13,7 +13,7 @@ enum{
 	INPUT_IIS,
 	INPUT_SPDIF
 };
-#define HDMI_CODEC_SOURCE_SELECT INPUT_SPDIF
+#define HDMI_CODEC_SOURCE_SELECT INPUT_IIS
 
 extern void rk616_hdmi_control_output(int enable);
 extern int rk616_hdmi_register_hdcp_callbacks(void (*hdcp_cb)(void),
