eee06c56784496805b198964c59fa8cd7c00bf48
Darrick J. Wong <darrick.wong@oracle.com>
2013-05-28 07:31:59 -0400

    jbd2: fix block tag checksum verification brokenness
    Al Viro complained of a ton of bogosity with regards to the jbd2 block
    tag header checksum.  This one checksum is 16 bits, so cut off the
    upper 16 bits and treat it as a 16-bit value and don't mess around
    with be32* conversions.  Fortunately metadata checksumming is still
    "experimental" and not in a shipping e2fsprogs, so there should be few
    users affected by this.
    
    Reported-by: Al Viro <viro@ZenIV.linux.org.uk>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/jbd2/commit.c
+++ fs/jbd2/commit.c
@@ -343,20 +343,21 @@ static void jbd2_block_tag_csum_set(journal_t *j, journal_block_tag_t *tag,
 {
 	struct page *page = bh->b_page;
 	__u8 *addr;
-	__u32 csum;
+	__u32 csum32;
 
 	if (!JBD2_HAS_INCOMPAT_FEATURE(j, JBD2_FEATURE_INCOMPAT_CSUM_V2))
 		return;
 
 	sequence = cpu_to_be32(sequence);
 	addr = kmap_atomic(page);
-	csum = jbd2_chksum(j, j->j_csum_seed, (__u8 *)&sequence,
-			  sizeof(sequence));
-	csum = jbd2_chksum(j, csum, addr + offset_in_page(bh->b_data),
-			  bh->b_size);
+	csum32 = jbd2_chksum(j, j->j_csum_seed, (__u8 *)&sequence,
+			     sizeof(sequence));
+	csum32 = jbd2_chksum(j, csum32, addr + offset_in_page(bh->b_data),
+			     bh->b_size);
 	kunmap_atomic(addr);
 
-	tag->t_checksum = cpu_to_be32(csum);
+	/* We only have space to store the lower 16 bits of the crc32c. */
+	tag->t_checksum = cpu_to_be16(csum32);
 }
 /*
  * jbd2_journal_commit_transaction
