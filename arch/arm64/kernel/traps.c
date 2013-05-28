9955ac47f4ba1c95ecb6092aeaefb40a22e99268
Mark Rutland <mark.rutland@arm.com>
2013-05-28 15:54:15 +0100

    arm64: don't kill the kernel on a bad esr from el0
    Rather than completely killing the kernel if we receive an esr value we
    can't deal with in the el0 handlers, send the process a SIGILL and log
    the esr value in the hope that we can debug it. If we receive a bad esr
    from el1, we'll die() as before.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Cc: stable@vger.kernel.org

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -311,14 +311,20 @@ asmlinkage long do_ni_syscall(struct pt_regs *regs)
  */
 asmlinkage void bad_mode(struct pt_regs *regs, int reason, unsigned int esr)
 {
+	siginfo_t info;
+	void __user *pc = (void __user *)instruction_pointer(regs);
 	console_verbose();
 
 	pr_crit("Bad mode in %s handler detected, code 0x%08x\n",
 		handler[reason], esr);
+	__show_regs(regs);
 
-	die("Oops - bad mode", regs, 0);
-	local_irq_disable();
-	panic("bad mode");
+	info.si_signo = SIGILL;
+	info.si_errno = 0;
+	info.si_code  = ILL_ILLOPC;
+	info.si_addr  = pc;
+
+	arm64_notify_die("Oops - bad mode", regs, &info, 0);
 }
 
 void __pte_error(const char *file, int line, unsigned long val)
