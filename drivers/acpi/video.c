fedbe9bc6fd3e14b1ffbb3dac407777ac4a3650c
Alex Hung <alex.hung@canonical.com>
2013-05-28 02:05:09 +0000

    ACPI / video: ignore BIOS initial backlight value for HP m4
    On HP m4 lapops, BIOS reports minimum backlight on boot and
    causes backlight to dim completely. This ignores the initial backlight
    values and set to max brightness.
    
    References: https://bugs.launchpad.net/bugs/1184501
    Cc: All <stable@vger.kernel.org>
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/video.c
+++ drivers/acpi/video.c
@@ -464,6 +464,14 @@ static struct dmi_system_id video_dmi_table[] __initdata = {
 		DMI_MATCH(DMI_PRODUCT_NAME, "HP 1000 Notebook PC"),
 		},
 	},
+	{
+	 .callback = video_ignore_initial_backlight,
+	 .ident = "HP Pavilion m4",
+	 .matches = {
+		DMI_MATCH(DMI_BOARD_VENDOR, "Hewlett-Packard"),
+		DMI_MATCH(DMI_PRODUCT_NAME, "HP Pavilion m4 Notebook PC"),
+		},
+	},
 	{}
 };
 
