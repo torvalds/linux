0bde6c3e4c4d594eb66200e37d9aec6ed45a8543
Hans Verkuil <hans.verkuil@cisco.com>
2013-05-29 10:19:02 -0300

    [media] bttv: fix querystd
    AND the standard mask with the detected standards.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/pci/bt8xx/bttv-driver.c
+++ drivers/media/pci/bt8xx/bttv-driver.c
@@ -1760,9 +1760,9 @@ static int bttv_querystd(struct file *file, void *f, v4l2_std_id *id)
 	struct bttv *btv = fh->btv;
 
 	if (btread(BT848_DSTATUS) & BT848_DSTATUS_NUML)
-		*id = V4L2_STD_625_50;
+		*id &= V4L2_STD_625_50;
 	else
-		*id = V4L2_STD_525_60;
+		*id &= V4L2_STD_525_60;
 	return 0;
 }
 
