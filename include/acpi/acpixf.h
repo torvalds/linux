67a9277496d8e674f27bb2a62c995eb79801d555
Bob Moore <robert.moore@intel.com>
2013-05-30 10:01:19 +0800

    ACPICA: Update version to 20130418
    Version 20130418.
    
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Acked-by: Len Brown <len.brown@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/acpixf.h
+++ include/acpi/acpixf.h
@@ -46,7 +46,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20130328
+#define ACPI_CA_VERSION                 0x20130418
 
 #include <acpi/acconfig.h>
 #include <acpi/actypes.h>
