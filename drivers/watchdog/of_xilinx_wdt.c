0c7788b07834d6738eed1bad700afb4b61a056e0
Michal Simek <michal.simek@xilinx.com>
2013-05-31 07:56:34 +0200

    watchdog: xilinx: Setup the origin compatible string
    Watchdog 1.01.a is also compatible with 1.00.a.
    Add the origin version to compatible list.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>
    (cherry picked from commit 8fce9b367d672332d2d101175b10737ee5c18b59)
    Signed-off-by: Daniel Sangorrin <daniel.sangorrin@toshiba.co.jp>
    Signed-off-by: Yoshitake Kobayashi <yoshitake.kobayashi@toshiba.co.jp>

--- drivers/watchdog/of_xilinx_wdt.c
+++ drivers/watchdog/of_xilinx_wdt.c
@@ -384,6 +384,7 @@ static int xwdt_remove(struct platform_device *dev)
 
 /* Match table for of_platform binding */
 static struct of_device_id xwdt_of_match[] = {
+	{ .compatible = "xlnx,xps-timebase-wdt-1.00.a", },
 	{ .compatible = "xlnx,xps-timebase-wdt-1.01.a", },
 	{},
 };
