72f2a7686cd37324ba5b5111ab294d307f1df50d
Sylwester Nawrocki <s.nawrocki@samsung.com>
2013-05-31 10:36:19 -0300

    [media] exynos4-is: Change fimc-is firmware file names
    This patch changes the firmware file names of the FIMC-IS subsystem.
    It is needed since there are different firmwares used across various
    SoC series, e.g. Exynos4 and Exynos5.
    Also the sensor specific "setfile" name is changed, to account for
    it depends on an image sensor and is also specific to the FIMC-IS
    and the SoC.
    This is a change for a driver merged in 3.10.
    
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/exynos4-is/fimc-is.h
+++ drivers/media/platform/exynos4-is/fimc-is.h
@@ -33,8 +33,8 @@
 
 #define FIMC_IS_DRV_NAME		"exynos4-fimc-is"
 
-#define FIMC_IS_FW_FILENAME		"fimc_is_fw.bin"
-#define FIMC_IS_SETFILE_6A3		"setfile.bin"
+#define FIMC_IS_FW_FILENAME		"exynos4_fimc_is_fw.bin"
+#define FIMC_IS_SETFILE_6A3		"exynos4_s5k6a3_setfile.bin"
 
 #define FIMC_IS_FW_LOAD_TIMEOUT		1000 /* ms */
 #define FIMC_IS_POWER_ON_TIMEOUT	1000 /* us */
