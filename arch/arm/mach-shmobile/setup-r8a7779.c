a07a0ba911ea0183a1d65b54fe2e3216e2517936
Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
2013-06-02 01:50:25 +0400

    phy-rcar-usb: correct base address
    The memory region that is used by the driver overlaps EHCI and OHCI  register
    regions for absolutely no reason now  -- fix it  by adding offset of 0x800 to
    the base address, changing the register #define's accordingly. This has extra
    positive effect that we now can use devm_ioremap_resource()...
    
    Note that the driver and the SoC code have to be in one patch to keep the code
    bisectable...
    
    The patch has been tested on the Marzen board.
    
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Acked-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Acked-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    (cherry picked from commit 725bf9dcafe16aa69c8ab34c63ba36c6eb4492f2)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    
    Conflicts:
    	drivers/usb/phy/phy-rcar-usb.c

--- arch/arm/mach-shmobile/setup-r8a7779.c
+++ arch/arm/mach-shmobile/setup-r8a7779.c
@@ -391,7 +391,7 @@ static struct platform_device sata_device = {
 /* USB PHY */
 static struct resource usb_phy_resources[] = {
 	[0] = {
-		.start		= 0xffe70000,
+		.start		= 0xffe70800,
 		.end		= 0xffe70900 - 1,
 		.flags		= IORESOURCE_MEM,
 	},
