cc5943a7816ba6c00639837a62131386619548dc
Tejun Heo <tj@kernel.org>
2013-06-03 19:13:55 -0700

    cgroup: mark "notify_on_release" and "release_agent" cgroup files insane
    The empty cgroup notification mechanism currently implemented in
    cgroup is tragically outdated.  Forking and execing userland process
    stopped being a viable notification mechanism more than a decade ago.
    We're gonna have a saner mechanism.  Let's make it clear that this
    abomination is going away.
    
    Mark "notify_on_release" and "release_agent" with CFTYPE_INSANE.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Acked-by: Li Zefan <lizefan@huawei.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -4052,6 +4052,7 @@ static struct cftype files[] = {
 	},
 	{
 		.name = "notify_on_release",
+		.flags = CFTYPE_INSANE,
 		.read_u64 = cgroup_read_notify_on_release,
 		.write_u64 = cgroup_write_notify_on_release,
 	},
@@ -4073,7 +4074,7 @@ static struct cftype files[] = {
 	},
 	{
 		.name = "release_agent",
-		.flags = CFTYPE_ONLY_ON_ROOT,
+		.flags = CFTYPE_INSANE | CFTYPE_ONLY_ON_ROOT,
 		.read_seq_string = cgroup_release_agent_show,
 		.write_string = cgroup_release_agent_write,
 		.max_write_len = PATH_MAX,
