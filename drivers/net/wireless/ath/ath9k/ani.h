057c1dd6cd5041e3b6a6fb65708ac5a4d9c12b36
Sujith Manoharan <c_manoha@qca.qualcomm.com>
2013-06-04 15:41:35 +0530

    ath9k: Remove unused ANI macros
    Signed-off-by: Sujith Manoharan <c_manoha@qca.qualcomm.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/ani.h
+++ drivers/net/wireless/ath/ath9k/ani.h
@@ -21,17 +21,10 @@
 
 /* units are errors per second */
 #define ATH9K_ANI_OFDM_TRIG_HIGH          3500
-
-/* units are errors per second */
 #define ATH9K_ANI_OFDM_TRIG_LOW           400
-
-/* units are errors per second */
 #define ATH9K_ANI_CCK_TRIG_HIGH           600
-
-/* units are errors per second */
 #define ATH9K_ANI_CCK_TRIG_LOW            300
 
-#define ATH9K_ANI_NOISE_IMMUNE_LVL        4
 #define ATH9K_ANI_SPUR_IMMUNE_LVL         3
 #define ATH9K_ANI_FIRSTEP_LVL             2
 
@@ -42,10 +35,6 @@
 
 /* in ms */
 #define ATH9K_ANI_POLLINTERVAL            1000
-
-#define HAL_NOISE_IMMUNE_MAX              4
-#define HAL_SPUR_IMMUNE_MAX               7
-#define HAL_FIRST_STEP_MAX                2
 
 #define ATH9K_SIG_FIRSTEP_SETTING_MIN     0
 #define ATH9K_SIG_FIRSTEP_SETTING_MAX     20
