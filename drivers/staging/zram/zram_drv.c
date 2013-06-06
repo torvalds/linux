f7b071d02492426c5b5c1086fe082ba7a0de6de8
Jiang Liu <liuj97@gmail.com>
2013-06-07 00:07:25 +0800

    zram: avoid double free in function zram_bvec_write()
    commit 65c484609a3b25c35e4edcd5f2c38f98f5226093 upstream.
    
    When doing a patial write and the whole page is filled with zero,
    zram_bvec_write() will free uncmem twice.
    
    Signed-off-by: Jiang Liu <jiang.liu@huawei.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -272,8 +272,6 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 
 	if (page_zero_filled(uncmem)) {
 		kunmap_atomic(user_mem);
-		if (is_partial_io(bvec))
-			kfree(uncmem);
 		zram->stats.pages_zero++;
 		zram_set_flag(meta, index, ZRAM_ZERO);
 		ret = 0;
