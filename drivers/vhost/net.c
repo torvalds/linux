05c05351943cc03bf5c77e86953b24ae6fb21368
Michael S. Tsirkin <mst@redhat.com>
2013-06-06 15:20:39 +0300

    vhost: check owner before we overwrite ubuf_info
    If device has an owner, we shouldn't touch ubuf_info
    since it might be in use.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/vhost/net.c
+++ drivers/vhost/net.c
@@ -1054,6 +1054,10 @@ static long vhost_net_set_owner(struct vhost_net *n)
 	int r;
 
 	mutex_lock(&n->dev.mutex);
+	if (vhost_dev_has_owner(&n->dev)) {
+		r = -EBUSY;
+		goto out;
+	}
 	r = vhost_net_set_ubuf_info(n);
 	if (r)
 		goto out;
