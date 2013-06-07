4b80e971a8685ffda26181ba6b8c5de226a39a7c
Davis Abubakr-Sadik Nii Nai <dwa2pac@gmail.com>
2013-06-07 09:43:11 +0000

    staging: csr: fix indentation coding style issue in csr_framework_ext.h
    This is a patch to csr_framework_ext.h that fixes indentation coding style
    issues found by the checkpatch.pl tool.
    
    Signed-off-by: Davis Abubakr-Sadik Nii Nai <dwa2pac@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/csr/csr_framework_ext.h
+++ drivers/staging/csr/csr_framework_ext.h
@@ -2,11 +2,11 @@
 #define CSR_FRAMEWORK_EXT_H__
 /*****************************************************************************
 
-            (c) Cambridge Silicon Radio Limited 2010
-            All rights reserved and confidential information of CSR
+		(c) Cambridge Silicon Radio Limited 2010
+	All rights reserved and confidential information of CSR
 
-            Refer to LICENSE.txt included with this source for details
-            on the license terms.
+		Refer to LICENSE.txt included with this source for details
+		on the license terms.
 
 *****************************************************************************/
 
