589ebdef7e3107401bf96a9c660753d397329ee9
Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
2013-06-07 14:05:59 +0000

    sh_eth: get R8A777x support out of #ifdef
    Get the R-Car code/data in the driver out of #ifdef by adding "r8a777x-ether" to
    the platfrom driver's  ID table; since it's the last #ifdef, we remove CARDNAME
    from the  ID table and no longer check  the driver data before  assigning it to
    'mdp->cd'...
    Change the Ether platform device's name in the ARM platform code accordingly.
    
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/arm/mach-shmobile/clock-r8a7778.c
+++ arch/arm/mach-shmobile/clock-r8a7778.c
@@ -77,7 +77,7 @@ static struct clk mstp_clks[MSTP_NR] = {
 
 static struct clk_lookup lookups[] = {
 	/* MSTP32 clocks */
-	CLKDEV_DEV_ID("sh-eth",	&mstp_clks[MSTP114]), /* Ether */
+	CLKDEV_DEV_ID("r8a777x-ether", &mstp_clks[MSTP114]), /* Ether */
 	CLKDEV_DEV_ID("sh-sci.0", &mstp_clks[MSTP026]), /* SCIF0 */
 	CLKDEV_DEV_ID("sh-sci.1", &mstp_clks[MSTP025]), /* SCIF1 */
 	CLKDEV_DEV_ID("sh-sci.2", &mstp_clks[MSTP024]), /* SCIF2 */
