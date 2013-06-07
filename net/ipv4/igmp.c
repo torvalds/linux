946d3bd7231be3b6202759ea0bea59989ae28c4a
Shawn Bohrer <sbohrer@rgmadvisors.com>
2013-06-07 12:34:43 -0500

    igmp: remove unnecessary in_device member zeroing
    ip_mc_init_dev() is passed a freshly kzalloc'd in_device so it is
    unnecessary to explicitly zero out the members.
    
    Signed-off-by: Shawn Bohrer <sbohrer@rgmadvisors.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/igmp.c
+++ net/ipv4/igmp.c
@@ -1435,13 +1435,9 @@ void ip_mc_init_dev(struct in_device *in_dev)
 {
 	ASSERT_RTNL();
 
-	in_dev->mc_tomb = NULL;
 #ifdef CONFIG_IP_MULTICAST
-	in_dev->mr_gq_running = 0;
 	setup_timer(&in_dev->mr_gq_timer, igmp_gq_timer_expire,
 			(unsigned long)in_dev);
-	in_dev->mr_ifc_count = 0;
-	in_dev->mc_count     = 0;
 	setup_timer(&in_dev->mr_ifc_timer, igmp_ifc_timer_expire,
 			(unsigned long)in_dev);
 	in_dev->mr_qrv = IGMP_Unsolicited_Report_Count;
