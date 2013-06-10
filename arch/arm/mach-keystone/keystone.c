828989ad87af15b555f783a70efa2cc526b35b3f
Santosh Shilimkar <santosh.shilimkar@ti.com>
2013-06-10 11:27:13 -0400

    ARM: keystone: Add minimal TI Keystone platform support
    Texas Instruments Keystone family of multi-core devices are
    based on ARM Cortex A15. Patch adds basic definitions for a
    new Keystone sub-architecture in ARM.
    
    The TCI66xxK2H Communications Infrastructure Keystone SoCs
    are member of the C66x family based on TI's new KeyStone 2
    multi-core SoC Architecture designed specifically for high
    performance wireless and networking infrastructure applications.
    The SOCs contains many subsystems like Cortex A15 ARM CorePacs,
    C66XX DSP CorePacs, MSMC memory controller, Tera Net bus,
    IP Network, Navigator, Hyperlink, 1G/10G Ethernet, Radio layers
    and queue based communication systems.
    
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: arm@kernel.org
    
    Acked-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>

---
+++ arch/arm/mach-keystone/keystone.c
@@ -0,0 +1,71 @@
+/*
+ * Keystone2 based boards and SOC related code.
+ *
+ * Copyright 2013 Texas Instruments, Inc.
+ *	Cyril Chemparathy <cyril@ti.com>
+ *	Santosh Shilimkar <santosh.shillimkar@ti.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ */
+#include <linux/io.h>
+#include <linux/of.h>
+#include <linux/init.h>
+#include <linux/of_platform.h>
+#include <linux/of_address.h>
+
+#include <asm/setup.h>
+#include <asm/mach/map.h>
+#include <asm/mach/arch.h>
+#include <asm/mach/time.h>
+
+#define PLL_RESET_WRITE_KEY_MASK		0xffff0000
+#define PLL_RESET_WRITE_KEY			0x5a69
+#define PLL_RESET				BIT(16)
+
+static void __iomem *keystone_rstctrl;
+
+static void __init keystone_init(void)
+{
+	struct device_node *node;
+
+	node = of_find_compatible_node(NULL, NULL, "ti,keystone-reset");
+	if (WARN_ON(!node))
+		pr_warn("ti,keystone-reset node undefined\n");
+
+	keystone_rstctrl = of_iomap(node, 0);
+	if (WARN_ON(!keystone_rstctrl))
+		pr_warn("ti,keystone-reset iomap error\n");
+
+	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
+}
+
+static const char *keystone_match[] __initconst = {
+	"ti,keystone-evm",
+	NULL,
+};
+
+void keystone_restart(char mode, const char *cmd)
+{
+	u32 val;
+
+	BUG_ON(!keystone_rstctrl);
+
+	/* Enable write access to RSTCTRL */
+	val = readl(keystone_rstctrl);
+	val &= PLL_RESET_WRITE_KEY_MASK;
+	val |= PLL_RESET_WRITE_KEY;
+	writel(val, keystone_rstctrl);
+
+	/* Reset the SOC */
+	val = readl(keystone_rstctrl);
+	val &= ~PLL_RESET;
+	writel(val, keystone_rstctrl);
+}
+
+DT_MACHINE_START(KEYSTONE, "Keystone")
+	.init_machine	= keystone_init,
+	.dt_compat	= keystone_match,
+	.restart	= keystone_restart,
+MACHINE_END
