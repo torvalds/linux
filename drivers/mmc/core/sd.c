5992e78697484d8f58b749f87a06f89f4ba40a47
Ulf Hansson <ulf.hansson@linaro.org>
2013-06-10 17:03:42 +0200

    mmc: core: Add shutdown callback for SD bus_ops
    For the SD .shutdown callback we re-use the SD suspend function since
    it performs the relevant actions.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -1170,6 +1170,7 @@ static const struct mmc_bus_ops mmc_sd_ops = {
 	.resume = NULL,
 	.power_restore = mmc_sd_power_restore,
 	.alive = mmc_sd_alive,
+	.shutdown = mmc_sd_suspend,
 };
 
 static const struct mmc_bus_ops mmc_sd_ops_unsafe = {
@@ -1181,6 +1182,7 @@ static const struct mmc_bus_ops mmc_sd_ops_unsafe = {
 	.resume = mmc_sd_resume,
 	.power_restore = mmc_sd_power_restore,
 	.alive = mmc_sd_alive,
+	.shutdown = mmc_sd_suspend,
 };
 
 static void mmc_sd_attach_bus_ops(struct mmc_host *host)
