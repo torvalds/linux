4f675e8a6b338ff6ac510595649c647d4888576e
Ralf Baechle <ralf@linux-mips.org>
2013-06-13 14:34:50 +0200

    MIPS: IP27: Fix build error if CONFIG_PCI=y and CONFIG_NUMA disabled.
    Then <asm-generic/topology.h> will define cpp macro as default definition
    for pcibus_to_node resulting in:
    
      CC      arch/mips/pci/pci-ip27.o
    arch/mips/pci/pci-ip27.c:220:7: error: expected identifier or ‘(’ before ‘void’
    arch/mips/pci/pci-ip27.c:220:12: error: expected ‘)’ before ‘(’ token
    make[1]: *** [arch/mips/pci/pci-ip27.o] Error 1
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/pci/pci-ip27.c
+++ arch/mips/pci/pci-ip27.c
@@ -217,6 +217,7 @@ static void pci_fixup_ioc3(struct pci_dev *d)
 	pci_disable_swapping(d);
 }
 
+#ifdef CONFIG_NUMA
 int pcibus_to_node(struct pci_bus *bus)
 {
 	struct bridge_controller *bc = BRIDGE_CONTROLLER(bus);
@@ -224,6 +225,7 @@ int pcibus_to_node(struct pci_bus *bus)
 	return bc->nasid;
 }
 EXPORT_SYMBOL(pcibus_to_node);
+#endif /* CONFIG_NUMA */
 
 DECLARE_PCI_FIXUP_HEADER(PCI_VENDOR_ID_SGI, PCI_DEVICE_ID_SGI_IOC3,
 	pci_fixup_ioc3);
