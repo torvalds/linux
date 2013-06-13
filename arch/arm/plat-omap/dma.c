86d4d78c320d335c2699d4897720b8eb1b934e47
Rajendra Nayak <rnayak@ti.com>
2013-06-13 19:47:11 +0530

    ARM: OMAP: dma: Remove the erroneous freeing of platform data
    Given p = pdev->dev.platform_data; and
          d = p->dma_attr;
    the freeing of either one of these by the driver
    seems just plain wrong.
    
    Get rid of them in the .probe failure path as well as the
    .remove.
    
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/dma.c
+++ arch/arm/plat-omap/dma.c
@@ -2111,8 +2111,6 @@ exit_dma_irq_fail:
 	}
 
 exit_dma_lch_fail:
-	kfree(p);
-	kfree(d);
 	kfree(dma_chan);
 	return ret;
 }
@@ -2133,8 +2131,6 @@ static int omap_system_dma_remove(struct platform_device *pdev)
 			free_irq(dma_irq, (void *)(irq_rel + 1));
 		}
 	}
-	kfree(p);
-	kfree(d);
 	kfree(dma_chan);
 	return 0;
 }
