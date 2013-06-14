46223a19ad566dee955969c9a2cea55ef59f21d8
Chao Xie <chao.xie@marvell.com>
2013-06-14 01:21:46 -0400

    mfd: 88pm80x: Fix driver name for 88pm800 and 88pm805
    88pm800 and 88pm805 shouldnot have the same driver name.
    
    Signed-off-by: Chao Xie <chao.xie@marvell.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/88pm800.c
+++ drivers/mfd/88pm800.c
@@ -569,7 +569,7 @@ static int pm800_remove(struct i2c_client *client)
 
 static struct i2c_driver pm800_driver = {
 	.driver = {
-		.name = "88PM80X",
+		.name = "88PM800",
 		.owner = THIS_MODULE,
 		.pm = &pm80x_pm_ops,
 		},
