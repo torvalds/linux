bf2920aa57f339b3901172ca35fbbf303acf8e16
Jarkko Nikula <jarkko.nikula@bitmer.com>
2013-06-15 11:31:07 +0300

    ARM: OMAP1: Remove duplicated DMA channel definitions
    Similarly than with OMAP2 there are many DMA channel definitions that have
    been moved or redefined in drivers using them and we can remove them from
    dma.h.
    
    There is exception with MMC that arch/arm/mach-omap1/devices.c is using
    MMC DMA channel definitions for setting platform data but those can be well
    replaced with numeric values.
    
    Remove dma.h include from arch/arm/mach-omap1/devices.c and use a script
    below for dropping duplicated definitions and for replacing definitions
    with DMA channel numbers.
    
    grep '#define OMAP_DMA' arch/arm/mach-omap1/dma.h | while read -r i; do \
    		DDEF=`echo $i |cut -d ' ' -f 1-2`; \
    		DEF=`echo $DDEF |cut -d ' ' -f 2`; \
    		CH=`echo $i |cut -d ' ' -f 3`; \
    		if [ `git grep -c "$DDEF" |wc -l` -gt 1 ]; then \
    			echo "removing" $DEF; \
    			sed -i "s/${DEF}/${CH}/" arch/arm/mach-omap1/devices.c; \
    			sed -i "/${DDEF}/d" arch/arm/mach-omap1/dma.h; \
    		fi; \
    	done
    
    Signed-off-by: Jarkko Nikula <jarkko.nikula@bitmer.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap1/dma.h
+++ arch/arm/mach-omap1/dma.h
@@ -20,17 +20,9 @@
 #define __OMAP1_DMA_CHANNEL_H
 
 /* DMA channels for omap1 */
-#define OMAP_DMA_NO_DEVICE		0
 #define OMAP_DMA_UART3_TX		18
 #define OMAP_DMA_UART3_RX		19
-#define OMAP_DMA_CAMERA_IF_RX		20
-#define OMAP_DMA_MMC_TX			21
-#define OMAP_DMA_MMC_RX			22
-#define OMAP_DMA_USB_W2FC_RX0		26
-#define OMAP_DMA_USB_W2FC_TX0		29
 
 /* These are only for 1610 */
-#define OMAP_DMA_MMC2_TX		54
-#define OMAP_DMA_MMC2_RX		55
 
 #endif /* __OMAP1_DMA_CHANNEL_H */
