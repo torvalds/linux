27ec8da4ace46900a71b8462157aa2bc88ff5d2c
Laura Abbott <lauraa@codeaurora.org>
2013-06-17 10:29:12 -0700

    ARM: add definitions for pte_mkexec/pte_mknexec
    Other architectures define pte_mkexec to mark a pte as executable.
    Add pte_mkexec for ARM to get the same functionality. Although no
    other architectures currently define it, also add pte_mknexec to
    explicitly allow a pte to be marked as non executable.
    
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/pgtable.h
+++ arch/arm/include/asm/pgtable.h
@@ -254,6 +254,8 @@ PTE_BIT_FUNC(mkclean,   &= ~L_PTE_DIRTY);
 PTE_BIT_FUNC(mkdirty,   |= L_PTE_DIRTY);
 PTE_BIT_FUNC(mkold,     &= ~L_PTE_YOUNG);
 PTE_BIT_FUNC(mkyoung,   |= L_PTE_YOUNG);
+PTE_BIT_FUNC(mkexec,   &= ~L_PTE_XN);
+PTE_BIT_FUNC(mknexec,   |= L_PTE_XN);
 
 static inline pte_t pte_mkspecial(pte_t pte) { return pte; }
 
