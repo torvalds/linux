a688393bd3fb27690a77f7ae8607b4969039bac5
Jiri Kosina <jkosina@suse.cz>
2013-06-19 23:52:11 +0200

    HID: explain out-of-range check better
    Extend the comment explaining the condition for discarding
    out-of-range values to clarify the cases in which devices don't
    provide any logical min/max.
    
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-input.c
+++ drivers/hid/hid-input.c
@@ -1042,7 +1042,11 @@ void hidinput_hid_event(struct hid_device *hid, struct hid_field *field, struct 
 
 	/*
 	 * Ignore out-of-range values as per HID specification,
-	 * section 5.10 and 6.2.25
+	 * section 5.10 and 6.2.25.
+	 *
+	 * The logical_minimum < logical_maximum check is done so that we
+	 * don't unintentionally discard values sent by devices which
+	 * don't specify logical min and max.
 	 */
 	if ((field->flags & HID_MAIN_ITEM_VARIABLE) &&
 	    (field->logical_minimum < field->logical_maximum) &&
