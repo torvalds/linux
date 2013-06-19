0686ab7a7d05cda3373e311a49b2478bd4f03c5d
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-06-19 10:42:35 +0800

    vme: vme_tsi148.c: fix error return code in tsi148_probe()
    Fix to return a negative error code in the tsi148_crcsr_init() error
    handling case instead of 0, as done elsewhere in this function.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/vme/bridges/vme_tsi148.c
+++ drivers/vme/bridges/vme_tsi148.c
@@ -2582,7 +2582,8 @@ static int tsi148_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	dev_info(&pdev->dev, "VME Write and flush and error check is %s\n",
 		err_chk ? "enabled" : "disabled");
 
-	if (tsi148_crcsr_init(tsi148_bridge, pdev)) {
+	retval = tsi148_crcsr_init(tsi148_bridge, pdev);
+	if (retval) {
 		dev_err(&pdev->dev, "CR/CSR configuration failed.\n");
 		goto err_crcsr;
 	}
