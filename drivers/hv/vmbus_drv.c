2608fb653103419ac163206ff6d51b7b6528e2d9
Jason Wang <jasowang@redhat.com>
2013-06-19 11:28:10 +0800

    drivers: hv: allocate synic structures before hv_synic_init()
    We currently allocate synic structures in hv_sync_init(), but there's no way for
    the driver to know about the allocation failure and it may continue to use the
    uninitialized pointers. Solve this by introducing helpers for allocating and
    freeing and doing the allocation before the on_each_cpu() call in
    vmbus_bus_init().
    
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hv/vmbus_drv.c
+++ drivers/hv/vmbus_drv.c
@@ -563,6 +563,9 @@ static int vmbus_bus_init(int irq)
 	 */
 	hv_register_vmbus_handler(irq, vmbus_isr);
 
+	ret = hv_synic_alloc();
+	if (ret)
+		goto err_alloc;
 	/*
 	 * Initialize the per-cpu interrupt state and
 	 * connect to the host.
@@ -570,13 +573,14 @@ static int vmbus_bus_init(int irq)
 	on_each_cpu(hv_synic_init, NULL, 1);
 	ret = vmbus_connect();
 	if (ret)
-		goto err_irq;
+		goto err_alloc;
 
 	vmbus_request_offers();
 
 	return 0;
 
-err_irq:
+err_alloc:
+	hv_synic_free();
 	free_irq(irq, hv_acpi_dev);
 
 err_unregister:
