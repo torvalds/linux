90f8908127b55aa22f5a995880d5ac8cd23fbe1e
Ming Lei <ming.lei@canonical.com>
2013-06-20 12:30:16 +0800

    firmware loader: fix compile warning with PM_SLEEP set
    This patch fixes the below compile warning:
    
    drivers/base/firmware_class.c:1254:12: warning: 'cache_firmware' defined
    but not used [-Wunused-function]
     static int cache_firmware(const char *fw_name)
                ^
    drivers/base/firmware_class.c:1281:12: warning: 'uncache_firmware'
    defined but not used [-Wunused-function]
     static int uncache_firmware(const char *fw_name)
                ^
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Ming Lei <ming.lei@canonical.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -1247,6 +1247,9 @@ request_firmware_nowait(
 }
 EXPORT_SYMBOL(request_firmware_nowait);
 
+#ifdef CONFIG_PM_SLEEP
+static ASYNC_DOMAIN_EXCLUSIVE(fw_cache_domain);
+
 /**
  * cache_firmware - cache one firmware image in kernel memory space
  * @fw_name: the firmware image name
@@ -1306,9 +1309,6 @@ static int uncache_firmware(const char *fw_name)
 
 	return -EINVAL;
 }
-
-#ifdef CONFIG_PM_SLEEP
-static ASYNC_DOMAIN_EXCLUSIVE(fw_cache_domain);
 
 static struct fw_cache_entry *alloc_fw_cache_entry(const char *name)
 {
