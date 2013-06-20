c364796a473db467b9201ea31a096bc0cf23547a
Markus Pargmann <mpa@pengutronix.de>
2013-06-20 15:20:20 +0200

    ASoC: imx-pcm-dma: DT support
    This patch removes the NO_DT flag. The pdev pointer may have a proper
    of_node with the dmas property, so we can use it to request DMA
    channels.
    
    Signed-off-by: Markus Pargmann <mpa@pengutronix.de>
    Tested-by: Shawn Guo <shawn.guo@linaro.org>
    Acked-by: Timur Tabi <timur@tabi.org>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/fsl/imx-pcm-dma.c
+++ sound/soc/fsl/imx-pcm-dma.c
@@ -64,7 +64,6 @@ int imx_pcm_dma_init(struct platform_device *pdev)
 {
 	return snd_dmaengine_pcm_register(&pdev->dev, &imx_dmaengine_pcm_config,
 		SND_DMAENGINE_PCM_FLAG_NO_RESIDUE |
-		SND_DMAENGINE_PCM_FLAG_NO_DT |
 		SND_DMAENGINE_PCM_FLAG_COMPAT);
 }
 EXPORT_SYMBOL_GPL(imx_pcm_dma_init);
