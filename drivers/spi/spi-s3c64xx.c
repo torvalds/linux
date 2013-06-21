bff82038ca3e2976ad0b51d32c7a8c9ed31065f7
Girish K S <girishks2000@gmail.com>
2013-06-21 11:26:13 +0530

    spi: s3c64xx: Added support for exynos5440 spi
    This patch adds support for the exynos5440 spi controller.
    The integration of the spi IP in exynos5440 is different from
    other SoC's. The I/O pins are no more configured via gpio, they
    have dedicated pins.
    
    Signed-off-by: Girish K S <ks.giri@samsung.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi-s3c64xx.c
+++ drivers/spi/spi-s3c64xx.c
@@ -1609,6 +1609,15 @@ static struct s3c64xx_spi_port_config exynos4_spi_port_config = {
 	.clk_from_cmu	= true,
 };
 
+static struct s3c64xx_spi_port_config exynos5440_spi_port_config = {
+	.fifo_lvl_mask	= { 0x1ff },
+	.rx_lvl_offset	= 15,
+	.tx_st_done	= 25,
+	.high_speed	= true,
+	.clk_from_cmu	= true,
+	.quirks		= S3C64XX_SPI_QUIRK_POLL,
+};
+
 static struct platform_device_id s3c64xx_spi_driver_ids[] = {
 	{
 		.name		= "s3c2443-spi",
@@ -1636,6 +1645,9 @@ static struct platform_device_id s3c64xx_spi_driver_ids[] = {
 static const struct of_device_id s3c64xx_spi_dt_match[] = {
 	{ .compatible = "samsung,exynos4210-spi",
 			.data = (void *)&exynos4_spi_port_config,
+	},
+	{ .compatible = "samsung,exynos5440-spi",
+			.data = (void *)&exynos5440_spi_port_config,
 	},
 	{ },
 };
