c6a8af50b89e78f7f429f6189d9db82215cc8873
Alex Williamson <alex.williamson@redhat.com>
2013-06-21 09:33:50 -0600

    iommu: Fix compiler warning on pr_debug
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>
    Signed-off-by: Joerg Roedel <joro@8bytes.org>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -820,7 +820,7 @@ int iommu_map(struct iommu_domain *domain, unsigned long iova,
 		size_t pgsize = iommu_pgsize(domain, iova | paddr, size);
 
 		pr_debug("mapping: iova 0x%lx pa 0x%lx pgsize %lu\n", iova,
-					(unsigned long)paddr, pgsize);
+			 (unsigned long)paddr, (unsigned long)pgsize);
 
 		ret = domain->ops->map(domain, iova, paddr, pgsize, prot);
 		if (ret)
