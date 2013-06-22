8ed7b7e9d2a4e208d3c94af3ddbc628317b30409
Paul Mackerras <paulus@samba.org>
2013-06-22 17:13:32 +1000

    KVM: PPC: Book3S PR: Fix proto-VSID calculations
    This makes sure the calculation of the proto-VSIDs used by PR KVM
    is done with 64-bit arithmetic.  Since vcpu3s->context_id[] is int,
    when we do vcpu3s->context_id[0] << ESID_BITS the shift will be done
    with 32-bit instructions, possibly leading to significant bits
    getting lost, as the context id can be up to 524283 and ESID_BITS is
    18.  To fix this we cast the context id to u64 before shifting.
    
    Signed-off-by: Paul Mackerras <paulus@samba.org>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- arch/powerpc/kvm/book3s_64_mmu_host.c
+++ arch/powerpc/kvm/book3s_64_mmu_host.c
@@ -325,9 +325,9 @@ int kvmppc_mmu_init(struct kvm_vcpu *vcpu)
 		return -1;
 	vcpu3s->context_id[0] = err;
 
-	vcpu3s->proto_vsid_max = ((vcpu3s->context_id[0] + 1)
+	vcpu3s->proto_vsid_max = ((u64)(vcpu3s->context_id[0] + 1)
 				  << ESID_BITS) - 1;
-	vcpu3s->proto_vsid_first = vcpu3s->context_id[0] << ESID_BITS;
+	vcpu3s->proto_vsid_first = (u64)vcpu3s->context_id[0] << ESID_BITS;
 	vcpu3s->proto_vsid_next = vcpu3s->proto_vsid_first;
 
 	kvmppc_mmu_hpte_init(vcpu);
