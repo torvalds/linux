490f16171119a16e05d670306c105f3b45c38837
Yann E. MORIN <yann.morin.1998@free.fr>
2013-06-25 23:37:44 +0200

    Revert "kconfig: fix randomising choice entries in presence of KCONFIG_ALLCONFIG"
    This reverts commit 8357b48549e17b3e4e402c7f977b65708922e60f.
    
    It breaks more stuff than it fixes.
    
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Reported-by: Sedat Dilek <sedat.dilek@gmail.com>
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Fengguang Wu <fengguang.wu@intel.com>
    Cc: Sedat Dilek <sedat.dilek@gmail.com>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: Alexandre Bounine <alexandre.bounine@idt.com>
    Cc: Matt Porter <mporter@kernel.crashing.org>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/kconfig/confdata.c
+++ scripts/kconfig/confdata.c
@@ -288,6 +288,8 @@ load:
 	for_all_symbols(i, sym) {
 		sym->flags |= SYMBOL_CHANGED;
 		sym->flags &= ~(def_flags|SYMBOL_VALID);
+		if (sym_is_choice(sym))
+			sym->flags |= def_flags;
 		switch (sym->type) {
 		case S_INT:
 		case S_HEX:
@@ -377,13 +379,13 @@ setsym:
 			case mod:
 				if (cs->def[def].tri == yes) {
 					conf_warning("%s creates inconsistent choice state", sym->name);
+					cs->flags &= ~def_flags;
 				}
 				break;
 			case yes:
 				if (cs->def[def].tri != no)
 					conf_warning("override: %s changes choice state", sym->name);
 				cs->def[def].val = sym;
-				cs->flags |= def_flags;
 				break;
 			}
 			cs->def[def].tri = EXPR_OR(cs->def[def].tri, sym->def[def].tri);
@@ -788,8 +790,6 @@ int conf_write(const char *name)
 		} else if (!(sym->flags & SYMBOL_CHOICE)) {
 			sym_calc_value(sym);
 			if (!(sym->flags & SYMBOL_WRITE))
-				goto next;
-			if (sym_is_choice_value(sym) && !menu_is_visible(menu->parent))
 				goto next;
 			sym->flags &= ~SYMBOL_WRITE;
 
