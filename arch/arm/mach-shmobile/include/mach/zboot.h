2c408d149299e99c89fc4be80fb4fe00a7016f02
Magnus Damm <damm@opensource.se>
2013-06-27 08:48:07 +0900

    ARM: shmobile: Update romImage to relocate appended DTB
    Instead of relying of MACH_TYPE for board identification,
    update the romImage code to relocate an appended DTB to
    the beginning of RAM. This implementation is independent
    of ARM_APPENDED_DTB, this because it is necessary to copy
    the DTB to memory so the kernel can access it.
    
    Without this patch Mackerel does not boot via the Mask ROM
    over USB (r_usb_boot) - this since non-DT boot was broken
    ages ago in commit:
    
    0ce53cd ARM: mach-shmobile: Use DT_MACHINE for mackerel
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/include/mach/zboot.h
+++ arch/arm/mach-shmobile/include/mach/zboot.h
@@ -1,7 +1,6 @@
 #ifndef ZBOOT_H
 #define ZBOOT_H
 
-#include <asm/mach-types.h>
 #include <mach/zboot_macros.h>
 
 /**************************************************
@@ -11,7 +10,6 @@
  **************************************************/
 
 #ifdef CONFIG_MACH_MACKEREL
-#define MACH_TYPE	MACH_TYPE_MACKEREL
 #define MEMORY_START	0x40000000
 #include "mach/head-mackerel.txt"
 #else
