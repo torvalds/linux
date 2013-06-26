3948ff8bba6c09baf1a4722d4021139a5044ae45
Matthias Schid <aircrach115@gmail.com>
2013-06-26 16:25:53 +0200

    scsi/megaraid fixed several typos in comments
    Fixed several typos in comments in megaraid_mbox.c, megaraid_mm.c
    and megaraid_sas_fusion.h.
    
    Signed-off-by: Matthias Schid <aircrach115@gmail.com>
    Signed-off-by: Stefan Huber <steffhip@gmail.com>
    Signed-off-by: Simon Puels <simon.puels@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/scsi/megaraid/megaraid_sas_fusion.h
+++ drivers/scsi/megaraid/megaraid_sas_fusion.h
@@ -88,7 +88,7 @@ enum MR_RAID_FLAGS_IO_SUB_TYPE {
 #define MEGASAS_FUSION_IN_RESET 0
 
 /*
- * Raid Context structure which describes MegaRAID specific IO Paramenters
+ * Raid Context structure which describes MegaRAID specific IO Parameters
  * This resides at offset 0x60 where the SGL normally starts in MPT IO Frames
  */
 
