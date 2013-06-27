035b214e57d7d575c30d3db33671c478e89bb927
Winston Hudson (b45308) <winston.h.hudson@freescale.com>
2013-06-26 21:08:30 -0700

    MLK-9769-18 Add 3DES-ECB-EDE support for CAAM in i.MX6 family
    Adds 3DES-ECB-EDE Mode support to the CAAM crypto accelerator core in
    the i.MX6 family of SoC devices.
    
    Note that CAAM also goes by sec-4.0 or sec-5.0 in other product families (such as QorIQ).
    Thus the property names are often tied to the sec-4.0+ nomenclature.
    
    Signed-off-by: Winston Hudson (b45308) <winston.h.hudson@freescale.com>
    Signed-off-by: Dan Douglass <dan.douglass@freescale.com>

--- drivers/crypto/caam/caamalg.c
+++ drivers/crypto/caam/caamalg.c
@@ -4115,6 +4115,22 @@ static struct caam_alg_template driver_algs[] = {
 		.class1_alg_type = OP_ALG_ALGSEL_AES | OP_ALG_AAI_CBC,
 	},
 	{
+		.name = "ecb(des3_ede)",
+		.driver_name = "ecb-des3-caam",
+		.blocksize = DES3_EDE_BLOCK_SIZE,
+		.type = CRYPTO_ALG_TYPE_ABLKCIPHER,
+		.template_ablkcipher = {
+			.setkey = ablkcipher_setkey,
+			.encrypt = ablkcipher_encrypt,
+			.decrypt = ablkcipher_decrypt,
+			.geniv = "eseqiv",
+			.min_keysize = DES3_EDE_KEY_SIZE,
+			.max_keysize = DES3_EDE_KEY_SIZE,
+			.ivsize = DES3_EDE_BLOCK_SIZE,
+			},
+		.class1_alg_type = OP_ALG_ALGSEL_3DES | OP_ALG_AAI_ECB,
+	},
+	{
 		.name = "cbc(des3_ede)",
 		.driver_name = "cbc-3des-caam",
 		.blocksize = DES3_EDE_BLOCK_SIZE,
