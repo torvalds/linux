8f484d8767d3d80bc20bb341a4b3e35894ef704b
Hans Verkuil <hans.verkuil@cisco.com>
2013-06-27 02:44:04 -0300

    [media] mem2mem: set missing v4l2_dev pointer
    The m2m-deinterlace, mem2mem_testdev and mx2_emmaprp drivers didn't set
    the v4l2_dev pointer in struct video_device, even though a v4l2_device was
    registered correctly. These days this v4l2_dev pointer must be set correctly,
    so this patch adds that for these three drivers.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/m2m-deinterlace.c
+++ drivers/media/platform/m2m-deinterlace.c
@@ -1033,6 +1033,7 @@ static int deinterlace_probe(struct platform_device *pdev)
 
 	*vfd = deinterlace_videodev;
 	vfd->lock = &pcdev->dev_mutex;
+	vfd->v4l2_dev = &pcdev->v4l2_dev;
 
 	ret = video_register_device(vfd, VFL_TYPE_GRABBER, 0);
 	if (ret) {
