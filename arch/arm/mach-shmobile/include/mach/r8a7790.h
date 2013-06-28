8333d8c995169e489a81241bd632a4b9370ed7c3
Magnus Damm <damm@opensource.se>
2013-06-28 20:27:13 +0900

    ARM: shmobile: Make r8a7790 Arch timer optional
    Update the r8a7790 code to allow using other
    timers than Arch timer for clock events.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/include/mach/r8a7790.h
+++ arch/arm/mach-shmobile/include/mach/r8a7790.h
@@ -4,6 +4,7 @@
 void r8a7790_add_standard_devices(void);
 void r8a7790_clock_init(void);
 void r8a7790_pinmux_init(void);
+void r8a7790_init_delay(void);
 void r8a7790_timer_init(void);
 
 #endif /* __ASM_R8A7790_H__ */
