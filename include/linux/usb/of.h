1494a1f62bf7cf57345e9282c8189fe2a21fab64
Felipe Balbi <balbi@ti.com>
2013-06-30 13:56:45 +0300

    usb: common: introduce of_usb_get_maximum_speed()
    this helper will be used for controllers which
    want to work at a lower speed even though they
    support higher USB transfer rates.
    
    One such case is Texas Instruments' AM437x
    SoC where it uses a USB3 controller without
    a USB3 PHY, rendering the controller USB2-only.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- include/linux/usb/of.h
+++ include/linux/usb/of.h
@@ -7,15 +7,23 @@
 #ifndef __LINUX_USB_OF_H
 #define __LINUX_USB_OF_H
 
+#include <linux/usb/ch9.h>
 #include <linux/usb/otg.h>
 #include <linux/usb/phy.h>
 
 #if IS_ENABLED(CONFIG_OF)
 enum usb_dr_mode of_usb_get_dr_mode(struct device_node *np);
+enum usb_device_speed of_usb_get_maximum_speed(struct device_node *np);
 #else
 static inline enum usb_dr_mode of_usb_get_dr_mode(struct device_node *np)
 {
 	return USB_DR_MODE_UNKNOWN;
+}
+
+static inline enum usb_device_speed
+of_usb_get_maximum_speed(struct device_node *np)
+{
+	return USB_SPEED_UNKNOWN;
 }
 #endif
 
