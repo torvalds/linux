5da6e2db35d02c41470757ccf747b889f806e0bc
Magnus Damm <damm@opensource.se>
2013-07-01 15:01:31 +0900

    ARM: shmobile: lager: Remove init_irq declaration in machine description
    Remove redundant irqchip_init() callback. The default case
    of NULL will result in invoking irqchip_init() anyway.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    [ horms+renesas@verge.net.au: Trimmed patch to remove portion
      that updates the r8a7790 SoC and altered the subject to
      use the same format as the patch that updates the r8a7790. ]
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    
    (cherry picked from commit 5f85052c606e38c98de28170a2df72f78931aa30)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/board-lager.c
+++ arch/arm/mach-shmobile/board-lager.c
@@ -22,7 +22,6 @@
 #include <linux/gpio_keys.h>
 #include <linux/input.h>
 #include <linux/interrupt.h>
-#include <linux/irqchip.h>
 #include <linux/kernel.h>
 #include <linux/leds.h>
 #include <linux/pinctrl/machine.h>
@@ -103,7 +102,6 @@ static const char *lager_boards_compat_dt[] __initdata = {
 };
 
 DT_MACHINE_START(LAGER_DT, "lager")
-	.init_irq	= irqchip_init,
 	.init_time	= r8a7790_timer_init,
 	.init_machine	= lager_add_standard_devices,
 	.dt_compat	= lager_boards_compat_dt,
