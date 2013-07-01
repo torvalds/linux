18700d7b0c6964a0df4e771fcde38102b77dccce
Jesse Barnes <jbarnes@virtuousgeek.org>
2013-07-01 15:50:17 -0700

    drm/i915: split encoder get_config calls from crtc get_clock calls
    This should help on HSW, where we don't currently have a get_clock call.
    
    Reported-by: Paulo Zanoni <przanoni@gmail.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit 510d5f2f6b97eccbfa08234e21b0577c1748807d)
    (cherry picked from drm-intel-next-queued)
    Signed-off-by: James Ausmus <james.ausmus@intel.com>
    
    Conflicts:
    	drivers/gpu/drm/i915/intel_display.c
    	(context changes)
    Signed-off-by: Darren Hart <dvhart@linux.intel.com>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -8380,13 +8380,12 @@ check_crtc_state(struct drm_device *dev)
 			if (encoder->base.crtc != &crtc->base)
 				continue;
 			if (encoder->get_config &&
-			    encoder->get_hw_state(encoder, &pipe) &&
-			    dev_priv->display.get_clock) {
+			    encoder->get_hw_state(encoder, &pipe))
 				encoder->get_config(encoder, &pipe_config);
-				dev_priv->display.get_clock(crtc,
-							    &pipe_config);
-			}
 		}
+
+		if (dev_priv->display.get_clock)
+			dev_priv->display.get_clock(crtc, &pipe_config);
 
 		WARN(crtc->active != active,
 		     "crtc active state doesn't match with hw state "
@@ -9980,12 +9979,8 @@ static void intel_modeset_readout_hw_state(struct drm_device *dev)
 		if (encoder->get_hw_state(encoder, &pipe)) {
 			crtc = to_intel_crtc(dev_priv->pipe_to_crtc_mapping[pipe]);
 			encoder->base.crtc = &crtc->base;
-			if (encoder->get_config &&
-			    dev_priv->display.get_clock) {
+			if (encoder->get_config)
 				encoder->get_config(encoder, &crtc->config);
-				dev_priv->display.get_clock(crtc,
-							    &crtc->config);
-			}
 		} else {
 			encoder->base.crtc = NULL;
 		}
@@ -9996,6 +9991,15 @@ static void intel_modeset_readout_hw_state(struct drm_device *dev)
 			      drm_get_encoder_name(&encoder->base),
 			      encoder->base.crtc ? "enabled" : "disabled",
 			      pipe);
+	}
+
+	list_for_each_entry(crtc, &dev->mode_config.crtc_list,
+			    base.head) {
+		if (!crtc->active)
+			continue;
+		if (dev_priv->display.get_clock)
+			dev_priv->display.get_clock(crtc,
+						    &crtc->config);
 	}
 
 	list_for_each_entry(connector, &dev->mode_config.connector_list,
