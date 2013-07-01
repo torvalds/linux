b7f15519edb2e3c3d7d07d6a0780a4386ef23085
Alexander Z Lam <azl@google.com>
2013-07-01 15:31:24 -0700

    tracing: Make trace_marker use the correct per-instance buffer
    commit 2d71619c59fac95a5415a326162fa046161b938c upstream.
    
    The trace_marker file was present for each new instance created, but it
    added the trace mark to the global trace buffer instead of to
    the instance's buffer.
    
    Link: http://lkml.kernel.org/r/1372717885-4543-2-git-send-email-azl@google.com
    
    Signed-off-by: Alexander Z Lam <azl@google.com>
    Cc: David Sharp <dhsharp@google.com>
    Cc: Vaibhav Nagarnaik <vnagarnaik@google.com>
    Cc: Alexander Z Lam <lambchop468@gmail.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -4328,6 +4328,7 @@ tracing_mark_write(struct file *filp, const char __user *ubuf,
 					size_t cnt, loff_t *fpos)
 {
 	unsigned long addr = (unsigned long)ubuf;
+	struct trace_array *tr = filp->private_data;
 	struct ring_buffer_event *event;
 	struct ring_buffer *buffer;
 	struct print_entry *entry;
@@ -4387,7 +4388,7 @@ tracing_mark_write(struct file *filp, const char __user *ubuf,
 
 	local_save_flags(irq_flags);
 	size = sizeof(*entry) + cnt + 2; /* possible \n added */
-	buffer = global_trace.trace_buffer.buffer;
+	buffer = tr->trace_buffer.buffer;
 	event = trace_buffer_lock_reserve(buffer, TRACE_PRINT, size,
 					  irq_flags, preempt_count());
 	if (!event) {
