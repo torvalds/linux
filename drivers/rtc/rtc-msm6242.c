e7d5a628f2e6c8153922b20099cba0b7c35ac2ae
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-03 15:06:49 -0700

    drivers/rtc/rtc-msm6242.c: remove empty function
    After the switch to devm_ functions and the removal of
    rtc_device_unregister(), the 'remove' function does not do anything.
    Delete it.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/rtc/rtc-msm6242.c
+++ drivers/rtc/rtc-msm6242.c
@@ -222,17 +222,11 @@ static int __init msm6242_rtc_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static int __exit msm6242_rtc_remove(struct platform_device *pdev)
-{
-	return 0;
-}
-
 static struct platform_driver msm6242_rtc_driver = {
 	.driver	= {
 		.name	= "rtc-msm6242",
 		.owner	= THIS_MODULE,
 	},
-	.remove	= __exit_p(msm6242_rtc_remove),
 };
 
 module_platform_driver_probe(msm6242_rtc_driver, msm6242_rtc_probe);
