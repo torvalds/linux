581183939c410202dcc5bd46440f90e0844e707a
Jani Nikula <jani.nikula@intel.com>
2013-07-03 15:05:05 -0700

    drm/i915: quirk away phantom LVDS on Intel's D525MW mainboard
    This replaceable mainboard only has a VGA-out, yet it claims to also have
    a connected LVDS header.
    
    Addresses https://bugs.freedesktop.org/show_bug.cgi?id=65256
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Reported-by: Cornel Panceac <cpanceac@gmail.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: <annndddrr@gmail.com>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Greg KH <greg@kroah.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit dcf6d294830d46b0e6901477fb4bf455281d90c8)
    Signed-off-by: Darren Hart <dvhart@linux.intel.com>

--- drivers/gpu/drm/i915/intel_lvds.c
+++ drivers/gpu/drm/i915/intel_lvds.c
@@ -893,6 +893,14 @@ static const struct dmi_system_id intel_no_lvds[] = {
 			DMI_EXACT_MATCH(DMI_BOARD_NAME, "D510MO"),
 		},
 	},
+	{
+		.callback = intel_no_lvds_dmi_callback,
+		.ident = "Intel D525MW",
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "Intel"),
+			DMI_EXACT_MATCH(DMI_BOARD_NAME, "D525MW"),
+		},
+	},
 
 	{ }	/* terminating entry */
 };
