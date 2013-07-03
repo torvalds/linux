fccc998771043db1613509b26c18b3d7f071e668
Jiang Liu <liuj97@gmail.com>
2013-07-03 15:04:23 -0700

    mm: introduce helper function set_max_mapnr()
    Introduce a helper function set_max_mapnr() to set global variable
    max_mapnr.
    
    Also unify condition compilation for max_mapnr with
    CONFIG_NEED_MULTIPLE_NODES instead of CONFIG_DISCONTIGMEM.
    
    Signed-off-by: Jiang Liu <jiang.liu@huawei.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Mauro Carvalho Chehab <mchehab@redhat.com>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -25,8 +25,15 @@ struct file_ra_state;
 struct user_struct;
 struct writeback_control;
 
-#ifndef CONFIG_DISCONTIGMEM          /* Don't use mapnrs, do it properly */
+#ifndef CONFIG_NEED_MULTIPLE_NODES	/* Don't use mapnrs, do it properly */
 extern unsigned long max_mapnr;
+
+static inline void set_max_mapnr(unsigned long limit)
+{
+	max_mapnr = limit;
+}
+#else
+static inline void set_max_mapnr(unsigned long limit) { }
 #endif
 
 extern unsigned long totalram_pages;
