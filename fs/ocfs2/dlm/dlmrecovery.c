40c7f2eaf59230135fd91a398924bdbf873378ec
Xue jiufei <xuejiufei@huawei.com>
2013-07-03 15:00:50 -0700

    ocfs2: add missing dlm_put() in dlm_begin_reco_handler()
    dlm_begin_reco_handler() returns without putting dlm when dlm recovery
    state is DLM_RECO_STATE_FINALIZE.
    
    Signed-off-by: joyce <xuejiufei@huawei.com>
    Reviewed-by: Jie Liu <jeff.liu@oracle.com>
    Acked-by: Joel Becker <jlbec@evilplan.org>
    Cc: Mark Fasheh <mfasheh@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/ocfs2/dlm/dlmrecovery.c
+++ fs/ocfs2/dlm/dlmrecovery.c
@@ -2692,6 +2692,7 @@ int dlm_begin_reco_handler(struct o2net_msg *msg, u32 len, void *data,
 		     dlm->name, br->node_idx, br->dead_node,
 		     dlm->reco.dead_node, dlm->reco.new_master);
 		spin_unlock(&dlm->spinlock);
+		dlm_put(dlm);
 		return -EAGAIN;
 	}
 	spin_unlock(&dlm->spinlock);
