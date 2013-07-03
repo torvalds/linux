5898d863f236b4850ff564cca8e128369655a3f5
Kees Cook <keescook@chromium.org>
2013-07-03 15:01:15 -0700

    crypto: sanitize argument for format string
    commit 1c8fca1d92e14859159a82b8a380d220139b7344 upstream.
    
    The template lookup interface does not provide a way to use format
    strings, so make sure that the interface cannot be abused accidentally.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Cc: Herbert Xu <herbert@gondor.apana.org.au>
    Cc: "David S. Miller" <davem@davemloft.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- crypto/algapi.c
+++ crypto/algapi.c
@@ -512,7 +512,8 @@ static struct crypto_template *__crypto_lookup_template(const char *name)
 
 struct crypto_template *crypto_lookup_template(const char *name)
 {
-	return try_then_request_module(__crypto_lookup_template(name), name);
+	return try_then_request_module(__crypto_lookup_template(name), "%s",
+				       name);
 }
 EXPORT_SYMBOL_GPL(crypto_lookup_template);
 
