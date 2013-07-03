ef9e78fd2753213ea01d77f7a76a9cb6ad0f50a7
HATAYAMA Daisuke <d.hatayama@jp.fujitsu.com>
2013-07-03 15:02:21 -0700

    vmcore: allow user process to remap ELF note segment buffer
    Now ELF note segment has been copied in the buffer on vmalloc memory.
    To allow user process to remap the ELF note segment buffer with
    remap_vmalloc_page, the corresponding VM area object has to have
    VM_USERMAP flag set.
    
    [akpm@linux-foundation.org: use the conventional comment layout]
    Signed-off-by: HATAYAMA Daisuke <d.hatayama@jp.fujitsu.com>
    Acked-by: Vivek Goyal <vgoyal@redhat.com>
    Cc: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Cc: Atsushi Kumagai <kumagai-atsushi@mxc.nes.nec.co.jp>
    Cc: Lisa Mitchell <lisa.mitchell@hp.com>
    Cc: Zhang Yanfei <zhangyanfei@cn.fujitsu.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/proc/vmcore.c
+++ fs/proc/vmcore.c
@@ -369,6 +369,7 @@ static int __init merge_note_headers_elf64(char *elfptr, size_t *elfsz,
 	Elf64_Ehdr *ehdr_ptr;
 	Elf64_Phdr phdr;
 	u64 phdr_sz = 0, note_off;
+	struct vm_struct *vm;
 
 	ehdr_ptr = (Elf64_Ehdr *)elfptr;
 
@@ -384,6 +385,14 @@ static int __init merge_note_headers_elf64(char *elfptr, size_t *elfsz,
 	*notes_buf = vzalloc(*notes_sz);
 	if (!*notes_buf)
 		return -ENOMEM;
+
+	/*
+	 * Allow users to remap ELF note segment buffer on vmalloc memory using
+	 * remap_vmalloc_range.()
+	 */
+	vm = find_vm_area(*notes_buf);
+	BUG_ON(!vm);
+	vm->flags |= VM_USERMAP;
 
 	rc = copy_notes_elf64(ehdr_ptr, *notes_buf);
 	if (rc < 0)
@@ -548,6 +557,7 @@ static int __init merge_note_headers_elf32(char *elfptr, size_t *elfsz,
 	Elf32_Ehdr *ehdr_ptr;
 	Elf32_Phdr phdr;
 	u64 phdr_sz = 0, note_off;
+	struct vm_struct *vm;
 
 	ehdr_ptr = (Elf32_Ehdr *)elfptr;
 
@@ -563,6 +573,14 @@ static int __init merge_note_headers_elf32(char *elfptr, size_t *elfsz,
 	*notes_buf = vzalloc(*notes_sz);
 	if (!*notes_buf)
 		return -ENOMEM;
+
+	/*
+	 * Allow users to remap ELF note segment buffer on vmalloc memory using
+	 * remap_vmalloc_range()
+	 */
+	vm = find_vm_area(*notes_buf);
+	BUG_ON(!vm);
+	vm->flags |= VM_USERMAP;
 
 	rc = copy_notes_elf32(ehdr_ptr, *notes_buf);
 	if (rc < 0)
