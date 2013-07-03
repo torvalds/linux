ccea4bc654a9d5330c4488acadc3abfa4ea7ebbf
Joseph Lo <josephl@nvidia.com>
2013-07-03 17:50:40 +0800

    ARM: tegra: add low level code for Tegra114 cluster power down
    When the CPU cluster power down, the vGIC is powered down too. The
    flow controller needs to monitor the legacy interrupt controller to
    wake up CPU. So setting up the appropriate wake up event in flow
    controller.
    
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/flowctrl.h
+++ arch/arm/mach-tegra/flowctrl.h
@@ -28,6 +28,8 @@
 #define FLOW_CTRL_SCLK_RESUME		(1 << 27)
 #define FLOW_CTRL_HALT_CPU_IRQ		(1 << 10)
 #define	FLOW_CTRL_HALT_CPU_FIQ		(1 << 8)
+#define FLOW_CTRL_HALT_LIC_IRQ		(1 << 11)
+#define FLOW_CTRL_HALT_LIC_FIQ		(1 << 10)
 #define FLOW_CTRL_HALT_GIC_IRQ		(1 << 9)
 #define FLOW_CTRL_HALT_GIC_FIQ		(1 << 8)
 #define FLOW_CTRL_CPU0_CSR		0x8
