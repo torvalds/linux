dcb6b45254e2281b6f99ea7f2d51343954aa3ba8
Alex Thorlton <athorlton@sgi.com>
2013-07-08 16:00:42 -0700

    panic: add cpu/pid to warn_slowpath_common in WARNING printk()s
    Add the cpu/pid that called WARN() so that the stack traces can be
    matched up with the WARNING messages.
    
    [akpm@linux-foundation.org: remove stray quote]
    Signed-off-by: Alex Thorlton <athorlton@sgi.com>
    Reviewed-by: Robin Holt <holt@sgi.com>
    Cc: Stephen Boyd <sboyd@codeaurora.org>
    Cc: Vikram Mulukutla <markivx@codeaurora.org>
    Cc: Rusty Russell <rusty@rustcorp.com.au>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/panic.c
+++ kernel/panic.c
@@ -399,8 +399,9 @@ struct slowpath_args {
 static void warn_slowpath_common(const char *file, int line, void *caller,
 				 unsigned taint, struct slowpath_args *args)
 {
-	printk(KERN_WARNING "------------[ cut here ]------------\n");
-	printk(KERN_WARNING "WARNING: at %s:%d %pS()\n", file, line, caller);
+	pr_warn("------------[ cut here ]------------\n");
+	pr_warn("WARNING: CPU: %d PID: %d at %s:%d %pS()\n",
+		raw_smp_processor_id(), current->pid, file, line, caller);
 
 	if (args)
 		vprintk(args->fmt, args->args);
