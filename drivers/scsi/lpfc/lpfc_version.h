810d22f75326298dc0ff1369ddcc3974381a151d
James Smart <james.smart@emulex.com>
2013-07-15 18:35:55 -0400

    [SCSI] lpfc 8.3.41: Update lpfc version to driver version 8.3.41
    Signed-off-by: James Smart <james.smart@emulex.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/lpfc/lpfc_version.h
+++ drivers/scsi/lpfc/lpfc_version.h
@@ -18,7 +18,7 @@
  * included with this package.                                     *
  *******************************************************************/
 
-#define LPFC_DRIVER_VERSION "8.3.40"
+#define LPFC_DRIVER_VERSION "8.3.41"
 #define LPFC_DRIVER_NAME		"lpfc"
 
 /* Used for SLI 2/3 */
