f6eb6e46f766f4e2eb64d2d467ef7efff084c47c
Yann E. MORIN <yann.morin.1998@free.fr>
2013-07-16 20:24:09 +0200

    kconfig/[mn]conf: shorten title in search-box
    No need to repeat the 'CONFIG_' string in the title,
    once is explicit enough.
    
    Reported-by: Jean Delvare <jdelvare@suse.de>
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Jean Delvare <jdelvare@suse.de>

--- scripts/kconfig/nconf.c
+++ scripts/kconfig/nconf.c
@@ -695,8 +695,8 @@ static void search_conf(void)
 	int dres;
 
 	title = str_new();
-	str_printf( &title, _("Enter %s (sub)string or regexp to search for "
-			      "(with or without \"%s\")"), CONFIG_, CONFIG_);
+	str_printf( &title, _("Enter (sub)string or regexp to search for "
+			      "(with or without \"%s\")"), CONFIG_);
 
 again:
 	dres = dialog_inputbox(main_window,
