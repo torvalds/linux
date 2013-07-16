cbaa56896146cbb5ab54bd65f98d020af282e6c6
Mark Brown <broonie@linaro.org>
2013-07-16 13:39:52 +0100

    ASoC: tlv320aic3x: List tlv320aic3106 as a supported device
    Currently there is no specific handling for it but the tlv320aic3106 is
    supported using this driver.
    
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/codecs/tlv320aic3x.c
+++ sound/soc/codecs/tlv320aic3x.c
@@ -1492,6 +1492,7 @@ static const struct i2c_device_id aic3x_i2c_id[] = {
 	{ "tlv320aic3x", AIC3X_MODEL_3X },
 	{ "tlv320aic33", AIC3X_MODEL_33 },
 	{ "tlv320aic3007", AIC3X_MODEL_3007 },
+	{ "tlv320aic3106", AIC3X_MODEL_3X },
 	{ }
 };
 MODULE_DEVICE_TABLE(i2c, aic3x_i2c_id);
@@ -1584,6 +1585,7 @@ static const struct of_device_id tlv320aic3x_of_match[] = {
 	{ .compatible = "ti,tlv320aic3x", },
 	{ .compatible = "ti,tlv320aic33" },
 	{ .compatible = "ti,tlv320aic3007" },
+	{ .compatible = "ti,tlv320aic3106" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, tlv320aic3x_of_match);
