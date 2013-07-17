908fd7e566b4c12e36e4487d2d5946de5e5ea30f
Ruchika Kharwar <ruchika@ti.com>
2013-07-17 16:29:13 +0300

    serial: omap: Fix IRQ handling return value
    Ensure the Interrupt handling routine return IRQ_HANDLED vs
    IRQ_NONE.
    
    Signed-off-by: Ruchika Kharwar <ruchika@ti.com>
    Signed-off-by: Alexander Savchenko <oleksandr.savchenko@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/omap-serial.c
+++ drivers/tty/serial/omap-serial.c
@@ -485,7 +485,6 @@ static irqreturn_t serial_omap_irq(int irq, void *dev_id)
 	struct uart_omap_port *up = dev_id;
 	unsigned int iir, lsr;
 	unsigned int type;
-	irqreturn_t ret = IRQ_NONE;
 	int max_count = 256;
 
 	spin_lock(&up->port.lock);
@@ -496,7 +495,6 @@ static irqreturn_t serial_omap_irq(int irq, void *dev_id)
 		if (iir & UART_IIR_NO_INT)
 			break;
 
-		ret = IRQ_HANDLED;
 		lsr = serial_in(up, UART_LSR);
 
 		/* extract IRQ type from IIR register */
@@ -535,7 +533,7 @@ static irqreturn_t serial_omap_irq(int irq, void *dev_id)
 	pm_runtime_put_autosuspend(up->dev);
 	up->port_activity = jiffies;
 
-	return ret;
+	return IRQ_HANDLED;
 }
 
 static unsigned int serial_omap_tx_empty(struct uart_port *port)
