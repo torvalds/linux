86bad320d900111dbb3c070d71db143a3f19e662
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2013-07-17 10:22:50 +0900

    mmc: SDHI: add DT compatibility strings for further SoCs
    Add further OF compatibility strings to the SDHI driver to be able to
    precisely control driver's behaviour on each of them.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski+renesas@gmail.com>
    Acked-by: Magnus Damm <damm@opensource.se>
    Acked-by: Simon Horman <horms@verge.net.au>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    (cherry picked from commit af99106ee9231b5382ee8c9a38899c5758bd6298)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- drivers/mmc/host/sh_mobile_sdhi.c
+++ drivers/mmc/host/sh_mobile_sdhi.c
@@ -115,7 +115,12 @@ static const struct sh_mobile_sdhi_ops sdhi_ops = {
 static const struct of_device_id sh_mobile_sdhi_of_match[] = {
 	{ .compatible = "renesas,shmobile-sdhi" },
 	{ .compatible = "renesas,sh7372-sdhi" },
+	{ .compatible = "renesas,sh73a0-sdhi", .data = &sh_mobile_sdhi_of_cfg[0], },
+	{ .compatible = "renesas,r8a73a4-sdhi", .data = &sh_mobile_sdhi_of_cfg[0], },
 	{ .compatible = "renesas,r8a7740-sdhi", .data = &sh_mobile_sdhi_of_cfg[0], },
+	{ .compatible = "renesas,r8a7778-sdhi", .data = &sh_mobile_sdhi_of_cfg[0], },
+	{ .compatible = "renesas,r8a7779-sdhi", .data = &sh_mobile_sdhi_of_cfg[0], },
+	{ .compatible = "renesas,r8a7790-sdhi", .data = &sh_mobile_sdhi_of_cfg[0], },
 	{},
 };
 MODULE_DEVICE_TABLE(of, sh_mobile_sdhi_of_match);
