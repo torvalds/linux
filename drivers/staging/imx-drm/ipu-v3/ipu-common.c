10f2268df80d127d0777dbf5986b58e3956ddb0f
Fabio Estevam <fabio.estevam@freescale.com>
2013-07-20 18:22:11 -0300

    imx-drm: ipu-common: Add MODULE_ALIAS
    Add MODULE_ALIAS, so that auto module loading can work.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Acked-by: Sascha Hauer <s.hauer@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/imx-drm/ipu-v3/ipu-common.c
+++ drivers/staging/imx-drm/ipu-v3/ipu-common.c
@@ -1163,6 +1163,7 @@ static struct platform_driver imx_ipu_driver = {
 
 module_platform_driver(imx_ipu_driver);
 
+MODULE_ALIAS("platform:imx-ipuv3");
 MODULE_DESCRIPTION("i.MX IPU v3 driver");
 MODULE_AUTHOR("Sascha Hauer <s.hauer@pengutronix.de>");
 MODULE_LICENSE("GPL");
