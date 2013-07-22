18f92b19b4101dbe9fef3923028cf66b27847778
Fabio Estevam <fabio.estevam@freescale.com>
2013-07-22 18:17:52 -0300

    gpio: gpio-mxc: Include "<linux/err.h>"
    Commit 8cd73e4e (gpio: gpio-mxc: Use devm functions) causes the following build
    error on imx_v4_v7_defconfig:
    
    drivers/gpio/gpio-mxc.c:414:2: error: implicit declaration of function 'IS_ERR' [-Werror=implicit-function-declaration]
    drivers/gpio/gpio-mxc.c:415:3: error: implicit declaration of function 'PTR_ERR' [-Werror=implicit-function-declaration]
    
    Note: imx_v6_v7_defconfig does not give this build error, probably due to some
    indirect header file inclusion.
    
    Reported-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-mxc.c
+++ drivers/gpio/gpio-mxc.c
@@ -19,6 +19,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+#include <linux/err.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/io.h>
