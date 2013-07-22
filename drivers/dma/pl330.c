aefb50879b71d6be6d139df869cf4920a2bb3bc2
graham <grmoore@altera.com>
2013-07-22 15:43:01 -0500

    FogBugz #139359: Code changes for PL330 DMA driver to support socfpga CycloneV
    Increased number of amba device irqs to support 8 DMA channels.
    Fixed NULL pointer error.
    Added check for aborted DMA transactions.
    Added code to read copy-align parameter from device tree.
    
    Signed-off-by: graham <grmoore@altera.com>
    
    V2:
    
    Add #ifdef CONFIG_ARCH_SOCFPGA to include/linux/amba/bus.h so that
    these changes do not affect other platforms
    
    We tried integrating Lee Booi's code for 8-1 irq mapping, but were unable
    to get it working in time for the code freeze.  We will file a FogBugz
    immediately which flags this commit as an incorrect implemention.  Then we
    will get Lee Booi's code working.
    
    V3:
    
    Added comment at line 1752 in pl330.c to explain need for id == -1 check.

--- drivers/dma/pl330.c
+++ drivers/dma/pl330.c
@@ -1749,6 +1749,10 @@ static int pl330_update(const struct pl330_info *pi)
 			ret = 1;
 
 			id = pl330->events[ev];
+			/* If error occurs when running dmatest, then id is -1,
+				which causes crash below, so skip to next event */
+			if (id == -1)
+				continue;
 
 			thrd = &pl330->channels[id];
 
@@ -2951,13 +2955,17 @@ pl330_probe(struct amba_device *adev, const struct amba_id *id)
 
 	amba_set_drvdata(adev, pdmac);
 
-	irq = adev->irq[0];
-	ret = request_irq(irq, pl330_irq_handler, 0,
-			dev_name(&adev->dev), pi);
-	if (ret)
-		return ret;
+	for (i = 0; i < AMBA_NR_IRQS; i++) {
+		irq = adev->irq[i];
+		if (irq == 0)
+			break;
+		ret = request_irq(irq, pl330_irq_handler, 0,
+				dev_name(&adev->dev), pi);
+		if (ret)
+			goto probe_err1;
+	}
 
-	if (pdat->init) {
+	if (pdat && pdat->init) {
 		ret = pdat->init(adev);
 		if (ret)
 			goto probe_err3;
@@ -3028,6 +3036,13 @@ pl330_probe(struct amba_device *adev, const struct amba_id *id)
 	pd->device_control = pl330_control;
 	pd->device_issue_pending = pl330_issue_pending;
 
+	if (adev->dev.of_node) {
+		u32 val;
+		if (!of_property_read_u32(adev->dev.of_node,
+			"copy-align", &val))
+			pd->copy_align = val;
+	}
+
 	ret = dma_async_device_register(pd);
 	if (ret) {
 		dev_err(&adev->dev, "unable to register DMAC\n");
@@ -3069,7 +3084,12 @@ probe_err3:
 probe_err2:
 	pl330_del(pi);
 probe_err1:
-	free_irq(irq, pi);
+	for (i = 0; i < AMBA_NR_IRQS; i++) {
+		irq = adev->irq[i];
+		if (irq == 0)
+			break;
+		free_irq(irq, pi);
+	}
 
 	return ret;
 }
@@ -3077,10 +3097,10 @@ probe_err1:
 static int pl330_remove(struct amba_device *adev)
 {
 	struct dma_pl330_dmac *pdmac = amba_get_drvdata(adev);
-	struct dma_pl330_platdata *pdat = adev->dev.platform_data;
 	struct dma_pl330_chan *pch, *_p;
 	struct pl330_info *pi;
 	int irq;
+	int i;
 
 	if (!pdmac)
 		return 0;
@@ -3107,8 +3127,12 @@ static int pl330_remove(struct amba_device *adev)
 
 	pl330_del(pi);
 
-	irq = adev->irq[0];
-	free_irq(irq, pi);
+	for (i = 0; i < AMBA_NR_IRQS; i++) {
+		irq = adev->irq[i];
+		if (irq == 0)
+			break;
+		free_irq(irq, pi);
+	}
 
 	return 0;
 }
