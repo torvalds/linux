5bc08e1921e46101457d3be09835697490177fdd
Luis Alves <ljalvs@gmail.com>
2013-07-24 09:06:01 -0300

    [media] cx23885[v4]: Fix interrupt storm when enabling IR receiver
    Apparently the Flatiron genereates an interrupt after the built-in self
    test for each of its left and right channels has completed.
    
    Apparently Conexant wire-OR'ed the Flatiron's interrupt output with the
    interrupt output of the CX23885 A/V core.
    
    Those interrupts need to be handled, otherwise, they generate an
    interrrupt request storm.
    
    So:
    
    - Add flatiron readreg and writereg functions prototypes
      on a new header file;
    - Modify the av interrupt handler to cleanup flatiron IRQs if no other
      interrupt handling happens.
    
    Signed-off-by: Luis Alves <ljalvs@gmail.com>
    Acked-by: Andy Walls <awalls@md.metrocast.net>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/pci/cx23885/cx23885-video.c
+++ drivers/media/pci/cx23885/cx23885-video.c
@@ -417,7 +417,7 @@ static void res_free(struct cx23885_dev *dev, struct cx23885_fh *fh,
 	mutex_unlock(&dev->lock);
 }
 
-static int cx23885_flatiron_write(struct cx23885_dev *dev, u8 reg, u8 data)
+int cx23885_flatiron_write(struct cx23885_dev *dev, u8 reg, u8 data)
 {
 	/* 8 bit registers, 8 bit values */
 	u8 buf[] = { reg, data };
@@ -428,7 +428,7 @@ static int cx23885_flatiron_write(struct cx23885_dev *dev, u8 reg, u8 data)
 	return i2c_transfer(&dev->i2c_bus[2].i2c_adap, &msg, 1);
 }
 
-static u8 cx23885_flatiron_read(struct cx23885_dev *dev, u8 reg)
+u8 cx23885_flatiron_read(struct cx23885_dev *dev, u8 reg)
 {
 	/* 8 bit registers, 8 bit values */
 	int ret;
--- drivers/media/pci/cx23885/cx23885-video.h
+++ drivers/media/pci/cx23885/cx23885-video.h
@@ -1,8 +1,6 @@
 /*
  *  Driver for the Conexant CX23885/7/8 PCIe bridge
  *
- *  AV device support routines - non-input, non-vl42_subdev routines
- *
  *  Copyright (C) 2010  Andy Walls <awalls@md.metrocast.net>
  *
  *  This program is free software; you can redistribute it and/or
@@ -21,29 +19,8 @@
  *  02110-1301, USA.
  */
 
-#include "cx23885.h"
-#include "cx23885-av.h"
-#include "cx23885-video.h"
-
-void cx23885_av_work_handler(struct work_struct *work)
-{
-	struct cx23885_dev *dev =
-			   container_of(work, struct cx23885_dev, cx25840_work);
-	bool handled;
-
-	v4l2_subdev_call(dev->sd_cx25840, core, interrupt_service_routine,
-			 PCI_MSK_AV_CORE, &handled);
-
-	/* Getting here with the interrupt not handled
-	   then probbaly flatiron does have pending interrupts.
-	*/
-	if (!handled) {
-		/* clear left and right adc channel interrupt request flag */
-		cx23885_flatiron_write(dev, 0x1f,
-			cx23885_flatiron_read(dev, 0x1f) | 0x80);
-		cx23885_flatiron_write(dev, 0x23,
-			cx23885_flatiron_read(dev, 0x23) | 0x80);
-	}
-
-	cx23885_irq_enable(dev, PCI_MSK_AV_CORE);
-}
+#ifndef _CX23885_VIDEO_H_
+#define _CX23885_VIDEO_H_
+int cx23885_flatiron_write(struct cx23885_dev *dev, u8 reg, u8 data);
+u8 cx23885_flatiron_read(struct cx23885_dev *dev, u8 reg);
+#endif
