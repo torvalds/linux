731e152446281638e2d18b414ece9921e20b9713
Steve Cornelius <steve.cornelius@freescale.com>
2013-07-23 20:49:29 -0700

    MLK-9710-11 Add internal key cover and external blob export/import to prototype SM-API
    Extended/amended the prototype SM-API with the following functions:
    - Added key covering (blackening) function in-place to a keyslot
    - Added export operation to encapsulate data to external memory as a
      secure memory blob (including descriptor capable of secure memory or
      general memory blob generation)
    - Removed in-place blob encapsulation
    - Added import operation to decapsulate a blob from external memory into
      secure memory (including descriptor capable of general memory or secure
      memory content decapsulation)
    - Removed in-place blob decapsulation
    
    [<vicki.milhoan@freescale.com>: Edited to apply to 3.10]
    Signed-off-by: Steve Cornelius <steve.cornelius@freescale.com>
    Signed-off-by: Victoria Milhoan <vicki.milhoan@freescale.com>
    
    (cherry picked from commit c577769ed0347bb4e3428b5696fb7f209af0a7ad)
    Signed-off-by: Dan Douglass <dan.douglass@freescale.com>

--- drivers/crypto/caam/sm.h
+++ drivers/crypto/caam/sm.h
@@ -13,6 +13,25 @@
 #define SM_PERM_WRITE 0x02
 #define SM_PERM_BLOB 0x03
 
+/* Define treatment of secure memory vs. general memory blobs */
+#define SM_SECMEM 0
+#define SM_GENMEM 1
+
+/* Define treatment of red/black keys */
+#define RED_KEY 0
+#define BLACK_KEY 1
+
+/* Define key encryption/covering options */
+#define KEY_COVER_ECB 0	/* cover key in AES-ECB */
+#define KEY_COVER_CCM 1 /* cover key with AES-CCM */
+
+/*
+ * Round a key size up to an AES blocksize boundary so to allow for
+ * padding out to a full block
+ */
+#define AES_BLOCK_PAD(x) ((x % 16) ? ((x >> 4) + 1) << 4 : x)
+
+#define BLOB_OVERHEAD (16 + 8) /* BKEK + MAC tag storage in any blob */
 
 /* Keystore maintenance functions */
 void sm_init_keystore(struct device *dev);
@@ -30,6 +49,16 @@ extern int sm_keystore_slot_load(struct device *dev, u32 unit, u32 slot,
 				 const u8 *key_data, u32 key_length);
 extern int sm_keystore_slot_read(struct device *dev, u32 unit, u32 slot,
 				 u32 key_length, u8 *key_data);
+extern int sm_keystore_cover_key(struct device *dev, u32 unit, u32 slot,
+				 u16 key_length, u8 keyauth);
+extern int sm_keystore_slot_export(struct device *dev, u32 unit, u32 slot,
+				   u8 keycolor, u8 keyauth, u8 *outbuf,
+				   u16 keylen, u8 *keymod);
+extern int sm_keystore_slot_import(struct device *dev, u32 unit, u32 slot,
+				   u8 keycolor, u8 keyauth, u8 *inbuf,
+				   u16 keylen, u8 *keymod);
+
+/* Prior functions from legacy API, deprecated */
 extern int sm_keystore_slot_encapsulate(struct device *dev, u32 unit,
 					u32 inslot, u32 outslot, u16 secretlen,
 					u8 *keymod, u16 keymodlen);
@@ -45,7 +74,8 @@ struct keystore_data_slot_info {
 
 /* Data structure to hold keystore information */
 struct keystore_data {
-	void	*base_address;	/* Base of the Secure Partition */
+	void	*base_address;	/* Virtual base of secure memory pages */
+	void	*phys_address;	/* Physical base of secure memory pages */
 	u32	slot_count;	/* Number of slots in the keystore */
 	struct keystore_data_slot_info *slot; /* Per-slot information */
 };
@@ -55,6 +85,7 @@ struct sm_page_descriptor {
 	u16 phys_pagenum;	/* may be discontiguous */
 	u16 own_part;		/* Owning partition */
 	void *pg_base;		/* Calculated virtual address */
+	void *pg_phys;		/* Calculated physical address */
 	struct keystore_data *ksdata;
 };
 
@@ -80,6 +111,7 @@ struct caam_drv_private_sm {
 	int (*slot_alloc)(struct device *dev, u32 unit, u32 size, u32 *slot);
 	int (*slot_dealloc)(struct device *dev, u32 unit, u32 slot);
 	void *(*slot_get_address)(struct device *dev, u32 unit, u32 handle);
+	void *(*slot_get_physical)(struct device *dev, u32 unit, u32 handle);
 	u32 (*slot_get_base)(struct device *dev, u32 unit, u32 handle);
 	u32 (*slot_get_offset)(struct device *dev, u32 unit, u32 handle);
 	u32 (*slot_get_slot_size)(struct device *dev, u32 unit, u32 handle);
