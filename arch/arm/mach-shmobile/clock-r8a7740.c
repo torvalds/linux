eb98cb13bedecb8e42c02172f50d8b821b9c2535
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2013-07-26 00:50:59 +0200

    ARM: shmobile: r8a7740: Add TPU clock entry for DT platforms
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    (cherry picked from commit d687f4d1993fa046171dbb1ca1970a3172ce11b9)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/clock-r8a7740.c
+++ arch/arm/mach-shmobile/clock-r8a7740.c
@@ -597,6 +597,7 @@ static struct clk_lookup lookups[] = {
 	CLKDEV_DEV_ID("sh-eth",			&mstp_clks[MSTP309]),
 	CLKDEV_DEV_ID("e9a00000.sh-eth",	&mstp_clks[MSTP309]),
 	CLKDEV_DEV_ID("renesas-tpu-pwm",	&mstp_clks[MSTP304]),
+	CLKDEV_DEV_ID("e6600000.pwm",		&mstp_clks[MSTP304]),
 
 	CLKDEV_DEV_ID("sh_mobile_sdhi.2",	&mstp_clks[MSTP415]),
 	CLKDEV_DEV_ID("e6870000.sdhi",          &mstp_clks[MSTP415]),
