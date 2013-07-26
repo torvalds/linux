9504a923924d663e1953f872f0a828e6454a6cfc
Hans Verkuil <hverkuil@xs4all.nl>
2013-07-26 18:43:45 +0200

    [SCSI] pm80xx: fix Adaptec 71605H hang
    The IO command size is 128 bytes for these new controllers as opposed to 64
    for the old 8001 controller.
    
    The Adaptec out-of-tree driver did this correctly. After comparing the two
    this turned out to be the crucial difference.
    
    So don't hardcode the IO command size, instead use pm8001_ha->iomb_size as
    that is the correct value for both old and new controllers.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Acked-by: Anand Kumar Santhanam <AnandKumar.Santhanam@pmcs.com>
    Acked-by: Jack Wang <xjtuwjp@gmail.com>
    Cc: stable@vger.kernel.org      # for v3.10 and up
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/pm8001/pm80xx_hwi.c
+++ drivers/scsi/pm8001/pm80xx_hwi.c
@@ -275,7 +275,7 @@ static void init_default_table_values(struct pm8001_hba_info *pm8001_ha)
 
 	for (i = 0; i < PM8001_MAX_SPCV_INB_NUM; i++) {
 		pm8001_ha->inbnd_q_tbl[i].element_pri_size_cnt	=
-			PM8001_MPI_QUEUE | (64 << 16) | (0x00<<30);
+			PM8001_MPI_QUEUE | (pm8001_ha->iomb_size << 16) | (0x00<<30);
 		pm8001_ha->inbnd_q_tbl[i].upper_base_addr	=
 			pm8001_ha->memoryMap.region[IB + i].phys_addr_hi;
 		pm8001_ha->inbnd_q_tbl[i].lower_base_addr	=
@@ -301,7 +301,7 @@ static void init_default_table_values(struct pm8001_hba_info *pm8001_ha)
 	}
 	for (i = 0; i < PM8001_MAX_SPCV_OUTB_NUM; i++) {
 		pm8001_ha->outbnd_q_tbl[i].element_size_cnt	=
-			PM8001_MPI_QUEUE | (64 << 16) | (0x01<<30);
+			PM8001_MPI_QUEUE | (pm8001_ha->iomb_size << 16) | (0x01<<30);
 		pm8001_ha->outbnd_q_tbl[i].upper_base_addr	=
 			pm8001_ha->memoryMap.region[OB + i].phys_addr_hi;
 		pm8001_ha->outbnd_q_tbl[i].lower_base_addr	=
