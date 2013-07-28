fafb6ebe384e62e68519ae8f5ae9b2cb578d7dde
David S. Miller <davem@davemloft.net>
2013-07-27 20:00:44 -0700

    Revert "atl1c: Fix misuse of netdev_alloc_skb in refilling rx ring"
    This reverts commit ebe7fdbaf3e90ea22feade6c9f5e50f42b23b6d8.
    
    This change is not correct.  GFP_DMA is not necessary for
    this device.
    
    There is some other problem causing this bug.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/atheros/atl1c/atl1c_main.c
+++ drivers/net/ethernet/atheros/atl1c/atl1c_main.c
@@ -1660,7 +1660,7 @@ static int atl1c_alloc_rx_buffer(struct atl1c_adapter *adapter)
 	while (next_info->flags & ATL1C_BUFFER_FREE) {
 		rfd_desc = ATL1C_RFD_DESC(rfd_ring, rfd_next_to_use);
 
-		skb = __netdev_alloc_skb(adapter->netdev, adapter->rx_buffer_len, GFP_ATOMIC|GFP_DMA);
+		skb = netdev_alloc_skb(adapter->netdev, adapter->rx_buffer_len);
 		if (unlikely(!skb)) {
 			if (netif_msg_rx_err(adapter))
 				dev_warn(&pdev->dev, "alloc rx buffer failed\n");
