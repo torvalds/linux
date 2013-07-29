4bdfb2729c3a396fe7400c9332c49aee2b971bd8
Mark Brown <broonie@linaro.org>
2013-07-29 21:00:53 +0100

    regulator: core: Add missing stub for regulator_get_exclusive()
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- include/linux/regulator/consumer.h
+++ include/linux/regulator/consumer.h
@@ -212,6 +212,13 @@ static inline struct regulator *__must_check regulator_get(struct device *dev,
 }
 
 static inline struct regulator *__must_check
+regulator_get_exclusive(struct device *dev, const char *id)
+{
+	return NULL;
+}
+
+
+static inline struct regulator *__must_check
 devm_regulator_get(struct device *dev, const char *id)
 {
 	return NULL;
