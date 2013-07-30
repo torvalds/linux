c34a761231b56dea4bd205cb9c29ffe37475d232
Eric Dumazet <edumazet@google.com>
2013-07-30 16:11:15 -0700

    net: skb_orphan() changes
    It is illegal to set skb->sk without corresponding destructor.
    
    Its therefore safe for skb_orphan() to not clear skb->sk if
    skb->destructor is not set.
    
    Also avoid clearing skb->destructor if already NULL.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -1805,10 +1805,11 @@ static inline void pskb_trim_unique(struct sk_buff *skb, unsigned int len)
  */
 static inline void skb_orphan(struct sk_buff *skb)
 {
-	if (skb->destructor)
+	if (skb->destructor) {
 		skb->destructor(skb);
-	skb->destructor = NULL;
-	skb->sk		= NULL;
+		skb->destructor = NULL;
+		skb->sk		= NULL;
+	}
 }
 
 /**
