6d0bbd3f05a67ce09983199044f8a0d39b711450
Magnus Damm <damm@opensource.se>
2013-08-01 04:02:51 +0900

    ARM: shmobile: Use default ->init_time() on KZM9G DT ref
    Leave ->init_time() set to NULL to use the default ARM behaviour.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    (cherry picked from commit f6110210347fea5599223fba6ecce14f1683f510)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/board-kzm9g-reference.c
+++ arch/arm/mach-shmobile/board-kzm9g-reference.c
@@ -52,6 +52,5 @@ DT_MACHINE_START(KZM9G_DT, "kzm9g-reference")
 	.init_early	= sh73a0_init_delay,
 	.nr_irqs	= NR_IRQS_LEGACY,
 	.init_machine	= kzm_init,
-	.init_time	= shmobile_timer_init,
 	.dt_compat	= kzm9g_boards_compat_dt,
 MACHINE_END
