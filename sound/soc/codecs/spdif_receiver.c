db5ff9541b61ef8394bad0fb05508921b8c5b17b
Nicolin Chen <b42378@freescale.com>
2013-07-31 20:07:05 +0800

    ASoC: spdif: Add S20_3LE and S24_LE support for dummy codec drivers
    Generally, S/PDIF supports 20bit and optional 24bit samples. Thus add these
    two formats for the dummy codec drivers.
    
    If one S/PDIF controller has its own limitation, its CPU DAI driver should
    set the supported format by its own circumstance, since the soc-pcm driver
    will use the intersection of cpu_dai's formats and codec_dai's formats.
    
    Signed-off-by: Nicolin Chen <b42378@freescale.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/codecs/spdif_receiver.c
+++ sound/soc/codecs/spdif_receiver.c
@@ -25,6 +25,8 @@
 
 #define STUB_RATES	SNDRV_PCM_RATE_8000_192000
 #define STUB_FORMATS	(SNDRV_PCM_FMTBIT_S16_LE | \
+			SNDRV_PCM_FMTBIT_S20_3LE | \
+			SNDRV_PCM_FMTBIT_S24_LE | \
 			SNDRV_PCM_FMTBIT_IEC958_SUBFRAME_LE)
 
 static struct snd_soc_codec_driver soc_codec_spdif_dir;
--- sound/soc/codecs/spdif_transmitter.c
+++ sound/soc/codecs/spdif_transmitter.c
@@ -25,8 +25,9 @@
 #define DRV_NAME "spdif-dit"
 
 #define STUB_RATES	SNDRV_PCM_RATE_8000_96000
-#define STUB_FORMATS	SNDRV_PCM_FMTBIT_S16_LE
-
+#define STUB_FORMATS	(SNDRV_PCM_FMTBIT_S16_LE | \
+			SNDRV_PCM_FMTBIT_S20_3LE | \
+			SNDRV_PCM_FMTBIT_S24_LE)
 
 static struct snd_soc_codec_driver soc_codec_spdif_dit;
 
