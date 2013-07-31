c4d17d776c872d66b49a97b4fbef700e4a69771e
黄涛 <huangtao@rock-chips.com>
2013-07-31 14:47:57 +0800

    rk3026: cpufreq: suspend voltage set to 1.1v

--- arch/arm/mach-rk3188/cpufreq.c
+++ arch/arm/mach-rk3188/cpufreq.c
@@ -67,7 +67,11 @@ static struct cpufreq_frequency_table *freq_table = default_freq_table;
 #define CPUFREQ_PRIVATE                 0x100
 static int no_cpufreq_access;
 static unsigned int suspend_freq = 816 * 1000;
+#if defined(CONFIG_ARCH_RK3026)
+static unsigned int suspend_volt = 1100000; // 1.1V
+#else
 static unsigned int suspend_volt = 1000000; // 1V
+#endif
 static unsigned int low_battery_freq = 600 * 1000;
 static unsigned int low_battery_capacity = 5; // 5%
 static bool is_booting = true;
