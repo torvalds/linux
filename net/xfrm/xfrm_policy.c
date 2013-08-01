0659eea912cf2ce65298784b918904ba0f91f5e1
Fan Du <fan.du@windriver.com>
2013-08-01 18:08:36 +0800

    xfrm: Delete hold_timer when destroy policy
    Both policy timer and hold_timer need to be deleted when destroy policy
    
    Signed-off-by: Fan Du <fan.du@windriver.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- net/xfrm/xfrm_policy.c
+++ net/xfrm/xfrm_policy.c
@@ -308,7 +308,7 @@ void xfrm_policy_destroy(struct xfrm_policy *policy)
 {
 	BUG_ON(!policy->walk.dead);
 
-	if (del_timer(&policy->timer))
+	if (del_timer(&policy->timer) || del_timer(&policy->polq.hold_timer))
 		BUG();
 
 	security_xfrm_policy_free(policy->security);
