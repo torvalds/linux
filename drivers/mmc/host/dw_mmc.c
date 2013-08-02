34349d7ca59d78069edb430d460fe203c5c93126
Dinh Nguyen <dinguyen@altera.com>
2013-08-01 22:21:55 -0500

    FogBugz #142858: Fix intermittent SD/MMC RFS from not mounting
    This bug was uncovered during a kernel upgrade to v3.10. The SD/MMC
    Hardware Locked Write Error(HLE) was intermittently getting triggered.
    The driver does not have any mechanism for handling this interrupt.
    
    Clear the HLE interrupt when it gets triggered.
    
    Signed-off-by: Dinh Nguyen <dinguyen@altera.com>

--- drivers/mmc/host/dw_mmc.c
+++ drivers/mmc/host/dw_mmc.c
@@ -1659,6 +1659,9 @@ static irqreturn_t dw_mci_interrupt(int irq, void *dev_id)
 			queue_work(host->card_workqueue, &host->card_work);
 		}
 
+		if (pending & SDMMC_INT_HLE)
+			mci_writel(host, RINTSTS, SDMMC_INT_HLE);
+
 		/* Handle SDIO Interrupts */
 		for (i = 0; i < host->num_slots; i++) {
 			struct dw_mci_slot *slot = host->slot[i];
