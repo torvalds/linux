15645581a9401cf2370d2c975e2673d289d5e561
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2013-08-01 23:39:13 -0700

    ARM: shmobile: armadillo800eva: Use DT for GIC
    Current Armadillo800eva is using DT booting,
    and r8a7740.dtsi already has GIC settings.
    
    So, we can remove the C version of the GIC setup code,
    instead rely on GIC information provided by DT.
    
    This patch removes r8a7740_init_irq() which has
    no user any more
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Acked-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/intc-r8a7740.c
+++ arch/arm/mach-shmobile/intc-r8a7740.c
@@ -23,11 +23,13 @@
 #include <linux/irqchip.h>
 #include <linux/irqchip/arm-gic.h>
 
-static void __init r8a7740_init_irq_common(void)
+void __init r8a7740_init_irq_of(void)
 {
 	void __iomem *intc_prio_base = ioremap_nocache(0xe6900010, 0x10);
 	void __iomem *intc_msk_base = ioremap_nocache(0xe6900040, 0x10);
 	void __iomem *pfc_inta_ctrl = ioremap_nocache(0xe605807c, 0x4);
+
+	irqchip_init();
 
 	/* route signals to GIC */
 	iowrite32(0x0, pfc_inta_ctrl);
@@ -49,20 +51,4 @@ static void __init r8a7740_init_irq_common(void)
 	iounmap(intc_prio_base);
 	iounmap(intc_msk_base);
 	iounmap(pfc_inta_ctrl);
-}
-
-void __init r8a7740_init_irq_of(void)
-{
-	irqchip_init();
-	r8a7740_init_irq_common();
-}
-
-void __init r8a7740_init_irq(void)
-{
-	void __iomem *gic_dist_base = ioremap_nocache(0xc2800000, 0x1000);
-	void __iomem *gic_cpu_base = ioremap_nocache(0xc2000000, 0x1000);
-
-	/* initialize the Generic Interrupt Controller PL390 r0p0 */
-	gic_init(0, 29, gic_dist_base, gic_cpu_base);
-	r8a7740_init_irq_common();
 }
