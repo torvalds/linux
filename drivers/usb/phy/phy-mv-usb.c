d07f4a8200cfc7ff2ba23ef87dd6267a4d70fb10
Jingoo Han <jg1.han@samsung.com>
2013-08-05 14:17:53 +0900

    usb: phy: mv-usb: remove incorrect __exit_p annotation
    When platform_driver_probe() is not used, bind/unbind via sysfs
    is enabled.  Thus, __exit_p annotation should be removed from
    remove().
    
    Also, mv_otg_remove() is staticized, because this function is
    used only in this file. Fix the following sparse warning:
    
    drivers/usb/phy/phy-mv-usb.c:656:5: warning: symbol 'mv_otg_remove' was not declared. Should it be static?
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/phy/phy-mv-usb.c
+++ drivers/usb/phy/phy-mv-usb.c
@@ -653,7 +653,7 @@ static struct attribute_group inputs_attr_group = {
 	.attrs = inputs_attrs,
 };
 
-int mv_otg_remove(struct platform_device *pdev)
+static int mv_otg_remove(struct platform_device *pdev)
 {
 	struct mv_otg *mvotg = platform_get_drvdata(pdev);
 
@@ -893,7 +893,7 @@ static int mv_otg_resume(struct platform_device *pdev)
 
 static struct platform_driver mv_otg_driver = {
 	.probe = mv_otg_probe,
-	.remove = __exit_p(mv_otg_remove),
+	.remove = mv_otg_remove,
 	.driver = {
 		   .owner = THIS_MODULE,
 		   .name = driver_name,
