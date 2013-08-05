a1ed4ddfb780910c1bb1e9df9cfc87454b607489
Andi Kleen <ak@linux.intel.com>
2013-08-05 15:02:38 -0700

    x86, asmlinkage: Make _*_start_kernel visible
    Obviously these functions have to be visible, otherwise
    the whole kernel could be optimized away.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Link: http://lkml.kernel.org/r/1375740170-7446-5-git-send-email-andi@firstfloor.org
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/kernel/head64.c
+++ arch/x86/kernel/head64.c
@@ -137,7 +137,7 @@ static void __init copy_bootdata(char *real_mode_data)
 	}
 }
 
-void __init x86_64_start_kernel(char * real_mode_data)
+asmlinkage void __init x86_64_start_kernel(char * real_mode_data)
 {
 	int i;
 
