abb29c3bb13c7b747fae18b9c63b660529ccc612
Kevin Hao <haokexin@gmail.com>
2013-08-06 18:23:30 +0800

    powerpc: Move the testing of CPU_FTR_COHERENT_ICACHE into __flush_icache_range
    In function flush_icache_range(), we use cpu_has_feature() to test
    the feature bit of CPU_FTR_COHERENT_ICACHE. But this seems not optimal
    for two reasons:
     a) For ppc32, the function __flush_icache_range() already do this
        check with the macro END_FTR_SECTION_IFSET.
     b) Compare with the cpu_has_feature(), the method of using macro
        END_FTR_SECTION_IFSET will not introduce any runtime overhead.
    
    [And while at it, add the missing required isync] -- BenH
    
    Signed-off-by: Kevin Hao <haokexin@gmail.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/cacheflush.h
+++ arch/powerpc/include/asm/cacheflush.h
@@ -35,8 +35,7 @@ extern void __flush_disable_L1(void);
 extern void __flush_icache_range(unsigned long, unsigned long);
 static inline void flush_icache_range(unsigned long start, unsigned long stop)
 {
-	if (!cpu_has_feature(CPU_FTR_COHERENT_ICACHE))
-		__flush_icache_range(start, stop);
+	__flush_icache_range(start, stop);
 }
 
 extern void flush_icache_user_range(struct vm_area_struct *vma,
