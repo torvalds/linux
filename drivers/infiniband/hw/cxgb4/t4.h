a2de1499b349b9c7d0d6defcf3e97c5289324044
Steve Wise <swise@opengridcomputing.com>
2013-08-06 21:04:39 +0530

    RDMA/cxgb4: Advertise ~0ULL as max MR size
    Lustre uses a advertised max MR size of ~0ULL to indicate it should
    use a dma_mr.  Hence advertise max MR size as ~0ULL.
    
    Signed-off-by: Steve Wise <swise@opengridcomputing.com>
    Signed-off-by: Vipul Pandya <vipul@chelsio.com>
    Signed-off-by: Roland Dreier <roland@purestorage.com>

--- drivers/infiniband/hw/cxgb4/t4.h
+++ drivers/infiniband/hw/cxgb4/t4.h
@@ -47,7 +47,7 @@
 #define T4_MAX_QP_DEPTH (T4_MAX_RQ_SIZE - 1)
 #define T4_MAX_CQ_DEPTH (T4_MAX_IQ_SIZE - 1)
 #define T4_MAX_NUM_STAG (1<<15)
-#define T4_MAX_MR_SIZE (~0ULL - 1)
+#define T4_MAX_MR_SIZE (~0ULL)
 #define T4_PAGESIZE_MASK 0xffff000  /* 4KB-128MB */
 #define T4_STAG_UNSET 0xffffffff
 #define T4_FW_MAJ 0
