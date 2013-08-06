b298881fcf52b04f073d64e4c584ac050506d7b9
Steve Wise <swise@opengridcomputing.com>
2013-08-06 21:04:38 +0530

    RDMA/cxgb4: Always do GTS write if cidx_inc == CIDXINC_MASK
    When polling, we do a GTS update if the accumulated cidx_inc == the CQ
    depth / 16.  However, if the CQ is large enough, Cq depth / 16 exceeds
    the size of the field in the GTS word.  So we also need to update if
    cidx_inc hits CIDXINC_MASK to avoid overflowing the field.
    
    Signed-off-by: Steve Wise <swise@opengridcomputing.com>
    Signed-off-by: Vipul Pandya <vipul@chelsio.com>
    Signed-off-by: Roland Dreier <roland@purestorage.com>

--- drivers/infiniband/hw/cxgb4/t4.h
+++ drivers/infiniband/hw/cxgb4/t4.h
@@ -531,7 +531,7 @@ static inline void t4_swcq_consume(struct t4_cq *cq)
 static inline void t4_hwcq_consume(struct t4_cq *cq)
 {
 	cq->bits_type_ts = cq->queue[cq->cidx].bits_type_ts;
-	if (++cq->cidx_inc == (cq->size >> 4)) {
+	if (++cq->cidx_inc == (cq->size >> 4) || cq->cidx_inc == CIDXINC_MASK) {
 		u32 val;
 
 		val = SEINTARM(0) | CIDXINC(cq->cidx_inc) | TIMERREG(7) |
