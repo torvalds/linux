a1bfacf4006a3bb410b0fa85e203f9249d2d35e9
Vikas Sajjan <vikas.sajjan@linaro.org>
2013-08-06 17:22:04 +0530

    drm/exynos: Add fallback option to get non physically contiguous memory for fb
    While trying to get boot-logo up on exynos5420 SMDK which has eDP panel
    connected with resolution 2560x1600, following error occured even with
    IOMMU enabled:
    [0.880000] [drm:lowlevel_buffer_allocate] *ERROR* failed to allocate buffer.
    [0.890000] [drm] Initialized exynos 1.0.0 20110530 on minor 0
    
    To address the cases where physically contiguous memory MAY NOT be a
    mandatory requirement for fb, the patch adds a feature to get non physically
    contiguous memory for fb if physically contiguous memory allocation fails
    and if IOMMU is supported.
    
    Signed-off-by: Vikas Sajjan <vikas.sajjan@linaro.org>
    Signed-off-by: Arun Kumar <arun.kk@samsung.com>
    Reviewed-by: Rob Clark <robdclark@gmail.com>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_fbdev.c
+++ drivers/gpu/drm/exynos/exynos_drm_fbdev.c
@@ -16,6 +16,7 @@
 #include <drm/drm_crtc.h>
 #include <drm/drm_fb_helper.h>
 #include <drm/drm_crtc_helper.h>
+#include <drm/exynos_drm.h>
 
 #include "exynos_drm_drv.h"
 #include "exynos_drm_fb.h"
@@ -165,8 +166,18 @@ static int exynos_drm_fbdev_create(struct drm_fb_helper *helper,
 
 	size = mode_cmd.pitches[0] * mode_cmd.height;
 
-	/* 0 means to allocate physically continuous memory */
-	exynos_gem_obj = exynos_drm_gem_create(dev, 0, size);
+	exynos_gem_obj = exynos_drm_gem_create(dev, EXYNOS_BO_CONTIG, size);
+	/*
+	 * If physically contiguous memory allocation fails and if IOMMU is
+	 * supported then try to get buffer from non physically contiguous
+	 * memory area.
+	 */
+	if (IS_ERR(exynos_gem_obj) && is_drm_iommu_supported(dev)) {
+		dev_warn(&pdev->dev, "contiguous FB allocation failed, falling back to non-contiguous\n");
+		exynos_gem_obj = exynos_drm_gem_create(dev, EXYNOS_BO_NONCONTIG,
+							size);
+	}
+
 	if (IS_ERR(exynos_gem_obj)) {
 		ret = PTR_ERR(exynos_gem_obj);
 		goto err_release_framebuffer;
