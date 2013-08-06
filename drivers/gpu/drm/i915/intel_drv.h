ddfe15677d9c47f2491e401cd773b45e1aac74bf
Chris Wilson <chris@chris-wilson.co.uk>
2013-08-06 17:43:07 +0100

    drm/i915: Export intel_framebuffer_fini
    Rather than open-code the teardown of a framebuffer, export the routine
    from intel_display.c. This then make intel_fbdev symmetric in its use of
    the common intel_framebuffer routines to initialise and clean up the
    struct intel_framebuffer. (And new features need only be added in one
    location!)
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -720,6 +720,7 @@ extern int intel_framebuffer_init(struct drm_device *dev,
 				  struct intel_framebuffer *ifb,
 				  struct drm_mode_fb_cmd2 *mode_cmd,
 				  struct drm_i915_gem_object *obj);
+extern void intel_framebuffer_fini(struct intel_framebuffer *fb);
 extern int intel_fbdev_init(struct drm_device *dev);
 extern void intel_fbdev_initial_config(struct drm_device *dev);
 extern void intel_fbdev_fini(struct drm_device *dev);
