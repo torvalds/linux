4fa1e6de7cbd27b87a7c3e56fbf72758d4884119
Ley Foon Tan <lftan@altera.com>
2013-08-07 10:04:23 +0800

    FogBugz #144109: Remove extra lines from license header
    Follow approved GPLv2 header standard.
    
    Signed-off-by: Ley Foon Tan <lftan@altera.com>

--- drivers/mailbox/mailbox-altera.c
+++ drivers/mailbox/mailbox-altera.c
@@ -12,8 +12,6 @@
  *
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <http://www.gnu.org/licenses/>.
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
  */
 
 #include <linux/kernel.h>
