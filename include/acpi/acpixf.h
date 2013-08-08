4ef17507834d19ba4fc24c4c1c61e8f68df356a6
Bob Moore <robert.moore@intel.com>
2013-08-08 15:30:05 +0800

    ACPICA: Update names for walk_namespace callbacks to clarify usage.
    Use of "preorder" and "postorder" was incorrect. The callbacks are
    simply invoked during tree ascent and descent during the
    depth-first walk.
    
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/acpixf.h
+++ include/acpi/acpixf.h
@@ -212,8 +212,8 @@ acpi_status
 acpi_walk_namespace(acpi_object_type type,
 		    acpi_handle start_object,
 		    u32 max_depth,
-		    acpi_walk_callback pre_order_visit,
-		    acpi_walk_callback post_order_visit,
+		    acpi_walk_callback descending_callback,
+		    acpi_walk_callback ascending_callback,
 		    void *context, void **return_value);
 
 acpi_status
