3d24cfe485e2750cc209a77dd62fa1fe004fc6c7
Mark Brown <broonie@linaro.org>
2013-08-09 18:12:29 +0100

    ASoC: compress: Use power efficient workqueue
    There is no need for the power down work to be done on a per CPU workqueue
    especially considering the fairly long delay before powerdown.
    
    Signed-off-by: Mark Brown <broonie@linaro.org>
    Acked-by: Vinod Koul <vinod.koul@intel.com>

--- sound/soc/soc-compress.c
+++ sound/soc/soc-compress.c
@@ -149,8 +149,9 @@ static int soc_compr_free(struct snd_compr_stream *cstream)
 					SND_SOC_DAPM_STREAM_STOP);
 		} else {
 			rtd->pop_wait = 1;
-			schedule_delayed_work(&rtd->delayed_work,
-				msecs_to_jiffies(rtd->pmdown_time));
+			queue_delayed_work(system_power_efficient_wq,
+					   &rtd->delayed_work,
+					   msecs_to_jiffies(rtd->pmdown_time));
 		}
 	} else {
 		/* capture streams can be powered down now */
