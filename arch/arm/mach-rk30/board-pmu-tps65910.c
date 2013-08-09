b584bd22d73c1a943027674cac59506f297c3b46
张晴 <zhangqing@rock-chips.com>
2013-08-09 09:40:33 +0800

    rk3028a:tps65910:support pmu tps65910 for 3028a_86v

--- arch/arm/mach-rk30/board-pmu-tps65910.c
+++ arch/arm/mach-rk30/board-pmu-tps65910.c
@@ -307,7 +307,8 @@ static struct regulator_consumer_supply tps65910_smps1_supply[] = {
 	{
 		.supply = "vdd1",
 	},
-	#if defined(CONFIG_SOC_RK3168) || defined(CONFIG_ARCH_RK3188) || defined(CONFIG_SOC_RK3028)
+	#if defined(CONFIG_SOC_RK3168) || defined(CONFIG_ARCH_RK3188) || defined(CONFIG_SOC_RK3028) ||defined(CONFIG_MACH_RK3028A_86V)
+
 	{
 		.supply = "vdd_core",
 	},
@@ -321,7 +322,8 @@ static struct regulator_consumer_supply tps65910_smps2_supply[] = {
 	{
 		.supply = "vdd2",
 	},
-	#if defined(CONFIG_MACH_RK3168_86V) || defined(CONFIG_SOC_RK3028)||defined(CONFIG_MACH_RK_FAC)
+	#if defined(CONFIG_MACH_RK3168_86V) || defined(CONFIG_SOC_RK3028)||defined(CONFIG_MACH_RK_FAC) ||defined(CONFIG_MACH_RK3028A_86V)
+
 	{
                 .supply = "vdd_cpu",
         },
