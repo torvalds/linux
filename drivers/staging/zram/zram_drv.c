66db73c14bd16d8ad05ecb80e6bf6050d8828467
Minchan Kim <minchan@kernel.org>
2013-08-12 15:13:56 +0900

    zram: don't grab mutex in zram_slot_free_noity
    [1] introduced down_write in zram_slot_free_notify to prevent race
    between zram_slot_free_notify and zram_bvec_[read|write]. The race
    could happen if somebody who has right permission to open swap device
    is reading swap device while it is used by swap in parallel.
    
    However, zram_slot_free_notify is called with holding spin_lock of
    swap layer so we shouldn't avoid holing mutex. Otherwise, lockdep
    warns it.
    
    This patch adds new list to handle free slot and workqueue
    so zram_slot_free_notify just registers slot index to be freed and
    registers the request to workqueue. If workqueue is expired,
    it holds mutex_lock so there is no problem any more.
    
    If any I/O is issued, zram handles pending slot-free request
    caused by zram_slot_free_notify right before handling issued
    request because workqueue wouldn't be expired yet so zram I/O
    request handling function can miss it.
    
    Lastly, when zram is reset, flush_work could handle all of pending
    free request so we shouldn't have memory leak.
    
    NOTE: If zram_slot_free_notify's kmalloc with GFP_ATOMIC would be
    failed, the slot will be freed when next write I/O write the slot.
    
    [1] [57ab0485, zram: use zram->lock to protect zram_free_page()
        in swap free notify path]
    
    * from v2
      * refactoring
    
    * from v1
      * totally redesign
    
    Cc: Nitin Gupta <ngupta@vflare.org>
    Cc: Jiang Liu <jiang.liu@huawei.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit a0c516cbfc7452c8cbd564525fef66d9f20b46d1)
    
    Conflicts:
    	drivers/staging/zram/zram_drv.c

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -278,6 +278,14 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 		goto out;
 	}
 
+	/*
+	 * zram_slot_free_notify could miss free so that let's
+	 * double check.
+	 */
+	if (unlikely(meta->table[index].handle ||
+			zram_test_flag(meta, index, ZRAM_ZERO)))
+		zram_free_page(zram, index);
+
 	ret = lzo1x_1_compress(uncmem, PAGE_SIZE, src, &clen,
 			       meta->compress_workmem);
 
@@ -335,6 +343,20 @@ out:
 	return ret;
 }
 
+static void handle_pending_slot_free(struct zram *zram)
+{
+	struct zram_slot_free *free_rq;
+
+	spin_lock(&zram->slot_free_lock);
+	while (zram->slot_free_rq) {
+		free_rq = zram->slot_free_rq;
+		zram->slot_free_rq = free_rq->next;
+		zram_free_page(zram, free_rq->index);
+		kfree(free_rq);
+	}
+	spin_unlock(&zram->slot_free_lock);
+}
+
 static int zram_bvec_rw(struct zram *zram, struct bio_vec *bvec, u32 index,
 			int offset, struct bio *bio, int rw)
 {
@@ -342,10 +364,12 @@ static int zram_bvec_rw(struct zram *zram, struct bio_vec *bvec, u32 index,
 
 	if (rw == READ) {
 		down_read(&zram->lock);
+		handle_pending_slot_free(zram);
 		ret = zram_bvec_read(zram, bvec, index, offset, bio);
 		up_read(&zram->lock);
 	} else {
 		down_write(&zram->lock);
+		handle_pending_slot_free(zram);
 		ret = zram_bvec_write(zram, bvec, index, offset);
 		up_write(&zram->lock);
 	}
@@ -496,6 +520,8 @@ static void __zram_reset_device(struct zram *zram)
 
 void zram_reset_device(struct zram *zram)
 {
+	flush_work(&zram->free_work);
+
 	down_write(&zram->init_lock);
 	__zram_reset_device(zram);
 	up_write(&zram->init_lock);
@@ -581,16 +607,40 @@ void zram_init_device(struct zram *zram, struct zram_meta *meta)
 	pr_debug("Initialization done!\n");
 }
 
+static void zram_slot_free(struct work_struct *work)
+{
+	struct zram *zram;
+
+	zram = container_of(work, struct zram, free_work);
+	down_write(&zram->lock);
+	handle_pending_slot_free(zram);
+	up_write(&zram->lock);
+}
+
+static void add_slot_free(struct zram *zram, struct zram_slot_free *free_rq)
+{
+	spin_lock(&zram->slot_free_lock);
+	free_rq->next = zram->slot_free_rq;
+	zram->slot_free_rq = free_rq;
+	spin_unlock(&zram->slot_free_lock);
+}
+
 static void zram_slot_free_notify(struct block_device *bdev,
 				unsigned long index)
 {
 	struct zram *zram;
+	struct zram_slot_free *free_rq;
 
 	zram = bdev->bd_disk->private_data;
-	down_write(&zram->lock);
-	zram_free_page(zram, index);
-	up_write(&zram->lock);
 	zram_stat64_inc(zram, &zram->stats.notify_free);
+
+	free_rq = kmalloc(sizeof(struct zram_slot_free), GFP_ATOMIC);
+	if (!free_rq)
+		return;
+
+	free_rq->index = index;
+	add_slot_free(zram, free_rq);
+	schedule_work(&zram->free_work);
 }
 
 static const struct block_device_operations zram_devops = {
@@ -605,6 +655,10 @@ static int create_device(struct zram *zram, int device_id)
 	init_rwsem(&zram->lock);
 	init_rwsem(&zram->init_lock);
 	spin_lock_init(&zram->stat64_lock);
+
+	INIT_WORK(&zram->free_work, zram_slot_free);
+	spin_lock_init(&zram->slot_free_lock);
+	zram->slot_free_rq = NULL;
 
 	zram->queue = blk_alloc_queue(GFP_KERNEL);
 	if (!zram->queue) {
