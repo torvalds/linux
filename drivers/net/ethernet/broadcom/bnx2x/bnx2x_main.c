6ef5a92cdf5ee65578a287b9a030964b03a538ed
Yuval Mintz <yuvalmin@broadcom.com>
2013-08-13 02:25:03 +0300

    bnx2x: prevent crash in shutdown flow with CNIC
    There might be a crash as during shutdown flow CNIC might try
    to access resources already freed by bnx2x.
    Change bnx2x_close() into dev_close() in __bnx2x_remove (shutdown flow)
    to guarantee CNIC is notified of the device's change of status.
    
    Signed-off-by: Yuval Mintz <yuvalmin@broadcom.com>
    Signed-off-by: Dmitry Kravkov <dmitry@broadcom.com>
    Signed-off-by: Ariel Elior <ariele@broadcom.com>
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/bnx2x/bnx2x_main.c
+++ drivers/net/ethernet/broadcom/bnx2x/bnx2x_main.c
@@ -12838,8 +12838,7 @@ static void __bnx2x_remove(struct pci_dev *pdev,
 		unregister_netdev(dev);
 	} else {
 		rtnl_lock();
-		if (netif_running(dev))
-			bnx2x_close(dev);
+		dev_close(dev);
 		rtnl_unlock();
 	}
 
