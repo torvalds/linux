ffb8ea8af20d88ed8b314be62a77b992055d10c8
Emil Velikov <emil.l.velikov@gmail.com>
2013-08-11 22:48:48 -0400

    drm/nouveau/therm: Set the correct pwm_mode upon resume
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Signed-off-by: Martin Peres <martin.peres@labri.fr>
    Tested-by: Martin Peres <martin.peres@labri.fr>
    Tested-by: Dash Four <mr.dash.four@googlemail.com>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/subdev/therm/base.c
+++ drivers/gpu/drm/nouveau/core/subdev/therm/base.c
@@ -268,7 +268,7 @@ _nouveau_therm_init(struct nouveau_object *object)
 		return ret;
 
 	if (priv->suspend >= 0)
-		nouveau_therm_fan_mode(therm, priv->mode);
+		nouveau_therm_fan_mode(therm, priv->suspend);
 	priv->sensor.program_alarms(therm);
 	return 0;
 }
