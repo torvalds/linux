3673af00afe200fd7ae6f58153acadded900a798
Ley Foon Tan <lftan@altera.com>
2013-08-12 11:10:00 +0800

    FogBugz #143478: arch/nios2: Move sysid from arch to drivers
    Removed sysid from arch/arm and use default soc_id/revision.
    
    Signed-off-by: Ley Foon Tan <lftan@altera.com>

--- arch/nios2/platform/platform.c
+++ arch/nios2/platform/platform.c
@@ -16,11 +16,8 @@
 #include <linux/sys_soc.h>
 #include <linux/io.h>
 
-#define NIOS2_SYSID_DEFAULT		(0x1)
+#define NIOS2_ID_DEFAULT		(0x1)
 #define NIOS2_REVISION_DEFAULT		(0x1)
-
-/* Sysid register map */
-#define SYSID_ID_REG			(0x0)
 
 static struct of_device_id altera_of_bus_ids[] __initdata = {
 	{ .compatible = "simple-bus", },
@@ -31,12 +28,9 @@ static struct of_device_id altera_of_bus_ids[] __initdata = {
 static void __init nios2_soc_device_init(void)
 {
 	struct device_node *root;
-	struct device_node *sysid_node;
 	struct soc_device *soc_dev;
 	struct soc_device_attribute *soc_dev_attr;
-	void __iomem *sysid_base;
 	const char *machine;
-	u32 id = NIOS2_SYSID_DEFAULT;
 	int err;
 
 	root = of_find_node_by_path("/");
@@ -47,24 +41,13 @@ static void __init nios2_soc_device_init(void)
 	if (err)
 		return;
 
+	of_node_put(root);
+
 	soc_dev_attr = kzalloc(sizeof(*soc_dev_attr), GFP_KERNEL);
 	if (!soc_dev_attr)
 		return;
 
-	sysid_node = of_find_compatible_node(root, NULL, "ALTR,sysid-1.0");
-	if (sysid_node) {
-		sysid_base = of_iomap(sysid_node, 0);
-		if (sysid_base) {
-			/* Use id from Sysid hardware. */
-			id = readl(sysid_base + SYSID_ID_REG);
-			iounmap(sysid_base);
-		}
-		of_node_put(sysid_node);
-	}
-
-	of_node_put(root);
-
-	soc_dev_attr->soc_id = kasprintf(GFP_KERNEL, "%u", id);
+	soc_dev_attr->soc_id = kasprintf(GFP_KERNEL, "%u", NIOS2_ID_DEFAULT);
 	soc_dev_attr->revision = kasprintf(GFP_KERNEL, "%d",
 		NIOS2_REVISION_DEFAULT);
 	soc_dev_attr->machine = kasprintf(GFP_KERNEL, "%s", machine);
