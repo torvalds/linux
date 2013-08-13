072b2064b88f709c53a83c6ec1f1cb17bf7c0abf
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2013-08-13 16:57:06 +0000

    xen: fix ARM build after 6efa20e4
    The following commit:
    
    commit 6efa20e49b9cb1db1ab66870cc37323474a75a13
    Author: Konrad Rzeszutek Wilk <konrad@kernel.org>
    Date:   Fri Jul 19 11:51:31 2013 -0400
    
        xen: Support 64-bit PV guest receiving NMIs
    
    breaks the Xen ARM build:
    
    CC      drivers/xen/events.o
    drivers/xen/events.c: In function 'xen_send_IPI_one':
    drivers/xen/events.c:1218:6: error: 'XEN_NMI_VECTOR' undeclared (first use in this function)
    
    Simply ifdef the undeclared symbol in the code.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

--- drivers/xen/events.c
+++ drivers/xen/events.c
@@ -1215,12 +1215,14 @@ void xen_send_IPI_one(unsigned int cpu, enum ipi_vector vector)
 {
 	int irq;
 
+#ifdef CONFIG_X86
 	if (unlikely(vector == XEN_NMI_VECTOR)) {
 		int rc =  HYPERVISOR_vcpu_op(VCPUOP_send_nmi, cpu, NULL);
 		if (rc < 0)
 			printk(KERN_WARNING "Sending nmi to CPU%d failed (rc:%d)\n", cpu, rc);
 		return;
 	}
+#endif
 	irq = per_cpu(ipi_to_irq, cpu)[vector];
 	BUG_ON(irq < 0);
 	notify_remote_via_irq(irq);
