1c40313ecb2a70a779127ad67f72ac180336083d
lyz <lyz@rock-chips.com>
2013-08-13 11:32:15 +0800

    rk3026: V86 not need OTG_DRV_VBUS mux GPIO3_C1 to GPIO

--- drivers/usb/dwc_otg/usbdev_rk3026.c
+++ drivers/usb/dwc_otg/usbdev_rk3026.c
@@ -114,7 +114,7 @@ void usb20otg_hw_init(void)
 #ifdef CONFIG_RK_CONFIG
     otg_drv_init(0);
 #else
-#ifdef CONFIG_ARCH_RK3026_86V
+#ifdef CONFIG_MACH_RK3026_86V
 
 #else
     iomux_set(OTG_DRV_VBUS);
