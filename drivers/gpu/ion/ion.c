a3041cac9fc117df010e66ce40091fa2c4f646f8
Colin Cross <ccross@android.com>
2013-08-14 14:10:19 -0700

    ion: add free list size to heap debug files
    Change-Id: I3c6309afdbd661a2f870fd1ba3fea9543e229882
    Signed-off-by: Colin Cross <ccross@android.com>

--- drivers/gpu/ion/ion.c
+++ drivers/gpu/ion/ion.c
@@ -1286,6 +1286,9 @@ static int ion_debug_heap_show(struct seq_file *s, void *unused)
 	seq_printf(s, "%16.s %16u\n", "total orphaned",
 		   total_orphaned_size);
 	seq_printf(s, "%16.s %16u\n", "total ", total_size);
+	if (heap->flags & ION_HEAP_FLAG_DEFER_FREE)
+		seq_printf(s, "%16.s %16u\n", "deferred free",
+				heap->free_list_size);
 	seq_printf(s, "----------------------------------------------------\n");
 
 	if (heap->debug_show)
