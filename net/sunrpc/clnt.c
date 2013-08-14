acf9ebdebcb3f39a41cf5fead7c11c32372dbf6c
Andy Adamson <andros@netapp.com>
2013-08-14 11:59:13 -0400

    SUNRPC: don't map EKEYEXPIRED to EACCES in call_refreshresult
    commit f1ff0c27fd9987c59d707cd1a6b6c1fc3ae0a250 upstream.
    
    The NFS layer needs to know when a key has expired.
    This change also returns -EKEYEXPIRED to the application, and the informative
    "Key has expired" error message is displayed. The user then knows that
    credential renewal is required.
    
    Signed-off-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/sunrpc/clnt.c
+++ net/sunrpc/clnt.c
@@ -1336,9 +1336,9 @@ call_refreshresult(struct rpc_task *task)
 		return;
 	case -ETIMEDOUT:
 		rpc_delay(task, 3*HZ);
-	case -EKEYEXPIRED:
 	case -EAGAIN:
 		status = -EACCES;
+	case -EKEYEXPIRED:
 		if (!task->tk_cred_retry)
 			break;
 		task->tk_cred_retry--;
