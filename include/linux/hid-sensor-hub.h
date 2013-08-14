15261f6d8d032b30f6eb7dbf1dbb9e4095df84c0
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2013-08-14 11:07:08 +0300

    HID: hid-sensor-hub: fix style of comments
    This patch fixes the style of the comments to be like following
    	/* The commentary */
    
    There is no functional change.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- include/linux/hid-sensor-hub.h
+++ include/linux/hid-sensor-hub.h
@@ -172,7 +172,7 @@ struct hid_sensor_common {
 	struct hid_sensor_hub_attribute_info sensitivity;
 };
 
-/*Convert from hid unit expo to regular exponent*/
+/* Convert from hid unit expo to regular exponent */
 static inline int hid_sensor_convert_exponent(int unit_expo)
 {
 	if (unit_expo < 0x08)
