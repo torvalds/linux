84f1f787421cd83bb7dfb34d584586f6a5fe7baa
John Johansen <john.johansen@canonical.com>
2013-08-14 11:27:32 -0700

    apparmor: export set of capabilities supported by the apparmor module
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Seth Arnold <seth.arnold@canonical.com>

--- security/apparmor/apparmorfs.c
+++ security/apparmor/apparmorfs.c
@@ -773,6 +773,7 @@ static struct aa_fs_entry aa_fs_entry_features[] = {
 	AA_FS_DIR("file",			aa_fs_entry_file),
 	AA_FS_FILE_U64("capability",		VFS_CAP_FLAGS_MASK),
 	AA_FS_DIR("rlimit",			aa_fs_entry_rlimit),
+	AA_FS_DIR("caps",			aa_fs_entry_caps),
 	{ }
 };
 
