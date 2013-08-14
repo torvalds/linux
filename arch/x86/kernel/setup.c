2449f343e4adc778de1c3d45b5aa14fe788663f5
Tang Chen <tangchen@cn.fujitsu.com>
2013-08-14 11:44:04 +0800

    x86: Use memblock_set_current_limit() to set limit for memblock.
    In setup_arch() of x86, it set memblock.current_limit directly.
    We should use memblock_set_current_limit(). If the implementation
    is changed, it is easy to maintain.
    
    Signed-off-by: Tang Chen <tangchen@cn.fujitsu.com>
    Link: http://lkml.kernel.org/r/1376451844-15682-1-git-send-email-tangchen@cn.fujitsu.com
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -1070,7 +1070,7 @@ void __init setup_arch(char **cmdline_p)
 
 	cleanup_highmap();
 
-	memblock.current_limit = ISA_END_ADDRESS;
+	memblock_set_current_limit(ISA_END_ADDRESS);
 	memblock_x86_fill();
 
 	/*
@@ -1103,7 +1103,7 @@ void __init setup_arch(char **cmdline_p)
 
 	setup_real_mode();
 
-	memblock.current_limit = get_max_mapped();
+	memblock_set_current_limit(get_max_mapped());
 	dma_contiguous_reserve(0);
 
 	/*
