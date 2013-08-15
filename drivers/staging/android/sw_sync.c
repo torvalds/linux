45acea57335ece628bdf96dc35d2ebdefd1efb9e
Yann Droneaud <ydroneaud@opteya.com>
2013-08-15 15:10:53 +0200

    android/sw_sync: use get_unused_fd_flags(O_CLOEXEC) instead of get_unused_fd()
    Macro get_unused_fd() is used to allocate a file descriptor with
    default flags. Those default flags (0) can be "unsafe":
    O_CLOEXEC must be used by default to not leak file descriptor
    across exec().
    
    Instead of macro get_unused_fd(), functions anon_inode_getfd()
    or get_unused_fd_flags() should be used with flags given by userspace.
    If not possible, flags should be set to O_CLOEXEC to provide userspace
    with a default safe behavor.
    
    In a further patch, get_unused_fd() will be removed so that
    new code start using anon_inode_getfd() or get_unused_fd_flags()
    with correct flags.
    
    This patch replaces calls to get_unused_fd() with call to
    get_unused_fd_flags(O_CLOEXEC) following advice from Erik Gilling.
    
    Signed-off-by: Yann Droneaud <ydroneaud@opteya.com>
    Cc: Erik Gilling <konkers@android.com>
    Cc: Colin Cross <ccross@google.com>
    Link: http://lkml.kernel.org/r/CACSP8SjZcpcpEtQHzcGYhf-MP7QGo0XpN7-uN7rmD=vNtopG=w@mail.gmail.com
    Link: http://lkml.kernel.org/r/cover.1376327678.git.ydroneaud@opteya.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/sw_sync.c
+++ drivers/staging/android/sw_sync.c
@@ -163,7 +163,7 @@ static int sw_sync_release(struct inode *inode, struct file *file)
 static long sw_sync_ioctl_create_fence(struct sw_sync_timeline *obj,
 				       unsigned long arg)
 {
-	int fd = get_unused_fd();
+	int fd = get_unused_fd_flags(O_CLOEXEC);
 	int err;
 	struct sync_pt *pt;
 	struct sync_fence *fence;
