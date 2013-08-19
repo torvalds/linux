37e6071787908fa9009cbd002c86402720becc5f
Mark Brown <broonie@linaro.org>
2013-08-19 20:33:20 +0100

    ASoC: samsung: Check to see if we managed to allocate a channel
    Signed-off-by: Mark Brown <broonie@linaro.org>
    Acked-by: Sangbeom Kim <sbkim73@samsung.com>

--- sound/soc/samsung/dma.c
+++ sound/soc/samsung/dma.c
@@ -176,6 +176,10 @@ static int dma_hw_params(struct snd_pcm_substream *substream,
 		prtd->params->ch = prtd->params->ops->request(
 				prtd->params->channel, &req, rtd->cpu_dai->dev,
 				prtd->params->ch_name);
+		if (!prtd->params->ch) {
+			pr_err("Failed to allocate DMA channel\n");
+			return -ENXIO;
+		}
 		prtd->params->ops->config(prtd->params->ch, &config);
 	}
 
