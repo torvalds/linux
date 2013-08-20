975a3cd4cc0dff06a635cb16b8fb25fd3ae88234
Will Deacon <will.deacon@arm.com>
2013-08-20 11:47:39 +0100

    arm64: perf: fix array out of bounds access in armpmu_map_hw_event()
    commit 868f6fea8fa63f09acbfa93256d0d2abdcabff79 upstream.
    
    This is a port of d9f966357b14 ("ARM: 7810/1: perf: Fix array out of
    bounds access in armpmu_map_hw_event()") to arm64, which fixes an oops
    in the arm64 perf backend found as a result of Vince's fuzzing tool.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/kernel/perf_event.c
+++ arch/arm64/kernel/perf_event.c
@@ -107,7 +107,12 @@ armpmu_map_cache_event(const unsigned (*cache_map)
 static int
 armpmu_map_event(const unsigned (*event_map)[PERF_COUNT_HW_MAX], u64 config)
 {
-	int mapping = (*event_map)[config];
+	int mapping;
+
+	if (config >= PERF_COUNT_HW_MAX)
+		return -EINVAL;
+
+	mapping = (*event_map)[config];
 	return mapping == HW_OP_UNSUPPORTED ? -ENOENT : mapping;
 }
 
