0ce814096f388f6801587f01c1c5ee1d04e746b3
Steven Rostedt <rostedt@goodmis.org>
2013-08-20 15:33:19 +0930

    module: Add NOARG flag for ops with param_set_bool_enable_only() set function
    The ops that uses param_set_bool_enable_only() as its set function can
    easily handle being used without an argument. There's no reason to
    fail the loading of the module if it does not have one.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- kernel/module.c
+++ kernel/module.c
@@ -136,6 +136,7 @@ static int param_set_bool_enable_only(const char *val,
 }
 
 static const struct kernel_param_ops param_ops_bool_enable_only = {
+	.flags = KERNEL_PARAM_FL_NOARG,
 	.set = param_set_bool_enable_only,
 	.get = param_get_bool,
 };
