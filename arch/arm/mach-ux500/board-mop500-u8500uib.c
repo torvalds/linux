f368a0874b314f53d8cd2739731a66c961db6d48
Lee Jones <lee.jones@linaro.org>
2013-08-23 09:24:38 +0100

    ARM: ux500: Don't register Synaptics RMI4 TS when booting with ATAGs
    It's time to remove all ATAG support from ux500 and rely solely on
    Device Tree booting. This patch is part of that endeavour.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/board-mop500-u8500uib.c
+++ arch/arm/mach-ux500/board-mop500-u8500uib.c
@@ -4,25 +4,11 @@
  * Board data for the U8500 UIB, also known as the New UIB
  * License terms: GNU General Public License (GPL), version 2
  */
-#include <linux/gpio.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/i2c.h>
-#include <linux/interrupt.h>
-
-#include "irqs.h"
 
 #include "board-mop500.h"
 
-static struct i2c_board_info __initdata mop500_i2c3_devices_u8500[] = {
-	{
-		I2C_BOARD_INFO("synaptics_rmi4_i2c", 0x4B),
-		.irq = NOMADIK_GPIO_TO_IRQ(84),
-	},
-};
-
 void __init mop500_u8500uib_init(void)
 {
-	mop500_uib_i2c_add(3, mop500_i2c3_devices_u8500,
-			ARRAY_SIZE(mop500_i2c3_devices_u8500));
 }
