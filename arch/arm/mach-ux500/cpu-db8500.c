2120c3bfc5c5fbd12a851d5823c8e1e01deccfe7
Lee Jones <lee.jones@linaro.org>
2013-08-23 10:29:22 +0100

    ARM: ux500: Stop registering Pinctrl when booting with ATAG support
    It's time to remove all ATAG support from ux500 and rely solely on
    Device Tree booting. This patch is part of that endeavour.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/cpu-db8500.c
+++ arch/arm/mach-ux500/cpu-db8500.c
@@ -159,7 +159,6 @@ static void __init db8500_add_gpios(struct device *parent)
 	dbx500_add_gpios(parent, db8500_gpio_base,
 			 ARRAY_SIZE(db8500_gpio_base),
 			 IRQ_DB8500_GPIO0, &pdata);
-	dbx500_add_pinctrl(parent, "pinctrl-db8500", U8500_PRCMU_BASE);
 }
 
 static const char *db8500_read_soc_id(void)
