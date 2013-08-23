25aa2957973d361081ac6c8b6e5a0d9d7a83fef6
Liu Ping Fan <kernelfans@gmail.com>
2013-08-23 16:58:47 +0800

    x86/ioapic: Check attr against the previous setting when programmed more than once
    When programming ioapic pinX more than once, current code
    does not check whether the later attr (trigger & polarity) is the
    same as the former or not.
    
    This causes broken semantics which can be observed in a qemu q35
    machine, where ioapic's ioredtbl[x] can never be set as low-active,
    even if the hpet driver registered it.
    
    And hpet driver may share a high-level active IRQ line with other
    devices. So in qemu, when hpet-dev asserts low-level as kernel
    expects, the kernel has no response.
    
    With this patch, we can observe an ioredtbl[x] set as low-active
    for hpet.
    
    Fix it by reporting -EBUSY to the caller, when attr is different.
    
    Signed-off-by: Liu Ping Fan <pingfank@linux.vnet.ibm.com>
    Cc: Kevin Hao <haokexin@gmail.com>
    Cc: Len Brown <len.brown@intel.com>
    Cc: Yinghai Lu <yinghai@kernel.org>
    Link: http://lkml.kernel.org/r/1377248327-19633-1-git-send-email-pingfank@linux.vnet.ibm.com
    [ Made small readability edits to both the changelog and the code. ]
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/kernel/apic/io_apic.c
+++ arch/x86/kernel/apic/io_apic.c
@@ -3380,12 +3380,15 @@ int io_apic_setup_irq_pin_once(unsigned int irq, int node,
 {
 	unsigned int ioapic_idx = attr->ioapic, pin = attr->ioapic_pin;
 	int ret;
+	struct IO_APIC_route_entry orig_entry;
 
 	/* Avoid redundant programming */
 	if (test_bit(pin, ioapics[ioapic_idx].pin_programmed)) {
-		pr_debug("Pin %d-%d already programmed\n",
-			 mpc_ioapic_id(ioapic_idx), pin);
-		return 0;
+		pr_debug("Pin %d-%d already programmed\n", mpc_ioapic_id(ioapic_idx), pin);
+		orig_entry = ioapic_read_entry(attr->ioapic, pin);
+		if (attr->trigger == orig_entry.trigger && attr->polarity == orig_entry.polarity)
+			return 0;
+		return -EBUSY;
 	}
 	ret = io_apic_setup_irq_pin(irq, node, attr);
 	if (!ret)
