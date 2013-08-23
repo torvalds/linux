8212db5775902438ae8875e223738fc931325ce4
Dan Carpenter <dan.carpenter@oracle.com>
2013-08-23 06:54:44 -0300

    media: sh_vou: almost forever loop in sh_vou_try_fmt_vid_out()
    commit 47c32ec9392a1fc7dec9d7cfde084e1432fcee82 upstream.
    
    The "i < " part of the "i < ARRAY_SIZE()" condition was missing.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    [g.liakhovetski@gmx.de: remove unrelated superfluous braces]
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/media/platform/sh_vou.c
+++ drivers/media/platform/sh_vou.c
@@ -776,7 +776,7 @@ static int sh_vou_try_fmt_vid_out(struct file *file, void *priv,
 	v4l_bound_align_image(&pix->width, 0, VOU_MAX_IMAGE_WIDTH, 1,
 			      &pix->height, 0, VOU_MAX_IMAGE_HEIGHT, 1, 0);
 
-	for (i = 0; ARRAY_SIZE(vou_fmt); i++)
+	for (i = 0; i < ARRAY_SIZE(vou_fmt); i++)
 		if (vou_fmt[i].pfmt == pix->pixelformat)
 			return 0;
 
