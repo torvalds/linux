3b321a1cf1625ebd633012c7211026fb381aa105
Kristian Høgsberg <krh@bitplanet.net>
2013-08-25 18:29:01 +0200

    drm/i915: Support render nodes
    Enable support for drm render nodes for i915 by flagging the ioctls that
    are safe and just needed for rendering.
    
    v2: mark reg_read, set_caching and get_caching (ickle, danvet)
    
    Signed-off-by: Kristian Høgsberg <krh@bitplanet.net>
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    (cherry picked from commit 10ba50129ab0bdbc0ee712e50913d1c8db88c5f0)
    Signed-off-by: Darren Hart <dvhart@linux.intel.com>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -1022,7 +1022,8 @@ static struct drm_driver driver = {
 	 */
 	.driver_features =
 	    DRIVER_USE_AGP | DRIVER_REQUIRE_AGP |
-	    DRIVER_HAVE_IRQ | DRIVER_IRQ_SHARED | DRIVER_GEM | DRIVER_PRIME,
+	    DRIVER_HAVE_IRQ | DRIVER_IRQ_SHARED | DRIVER_GEM | DRIVER_PRIME |
+	    DRIVER_RENDER,
 	.load = i915_driver_load,
 	.unload = i915_driver_unload,
 	.open = i915_driver_open,
