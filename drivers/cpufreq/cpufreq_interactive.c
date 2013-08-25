d2589aba9c19fe6ceb7c3f59b07a8cd473a48e65
Minsung Kim <ms925.kim@samsung.com>
2013-08-25 19:23:34 +0900

    cpufreq: interactive: fix show_target_loads and show_above_hispeed_delay
    Remove a trailing whitespace from target_loads and above_hispeed_delay. Problem
    happens when user-space program tried to restore parameters that saved before
    changing parameters. In this case was returned error(EINVAL).
    
    Change-Id: I5a74e3824602cd6f2b74651adda5ec1b627e61e9
    Signed-off-by: Minsung Kim <ms925.kim@samsung.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -742,7 +742,7 @@ static ssize_t show_target_loads(
 		ret += sprintf(buf + ret, "%u%s", target_loads[i],
 			       i & 0x1 ? ":" : " ");
 
-	ret += sprintf(buf + ret, "\n");
+	ret += sprintf(buf + --ret, "\n");
 	spin_unlock_irqrestore(&target_loads_lock, flags);
 	return ret;
 }
@@ -785,7 +785,7 @@ static ssize_t show_above_hispeed_delay(
 		ret += sprintf(buf + ret, "%u%s", above_hispeed_delay[i],
 			       i & 0x1 ? ":" : " ");
 
-	ret += sprintf(buf + ret, "\n");
+	ret += sprintf(buf + --ret, "\n");
 	spin_unlock_irqrestore(&above_hispeed_delay_lock, flags);
 	return ret;
 }
