a7c12d016aa50762e4816308f46e2572ff9b5a01
Huang Shijie <b32955@freescale.com>
2013-08-27 17:29:05 +0800

    mtd: gpmi: use DMA channel 0 for all the nand chips
    We only have one DMA channel : the channel 0.
    Use DMA channel 0 to access all the nand chips.
    
    Signed-off-by: Huang Shijie <b32955@freescale.com>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/nand/gpmi-nand/gpmi-nand.c
+++ drivers/mtd/nand/gpmi-nand/gpmi-nand.c
@@ -357,9 +357,8 @@ int common_nfc_set_geometry(struct gpmi_nand_data *this)
 
 struct dma_chan *get_dma_chan(struct gpmi_nand_data *this)
 {
-	int chipnr = this->current_chip;
-
-	return this->dma_chans[chipnr];
+	/* We use the DMA channel 0 to access all the nand chips. */
+	return this->dma_chans[0];
 }
 
 /* Can we use the upper's buffer directly for DMA? */
