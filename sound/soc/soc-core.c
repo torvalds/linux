8c193b8dce4f2a2474dc2bc39ec972454df9d439
Lars-Peter Clausen <lars@metafoo.de>
2013-08-27 15:51:00 +0200

    ASoC: Move call to snd_soc_dapm_new_widgets() after snd_soc_dapm_auto_nc_codec_pins()
    Call snd_soc_dapm_new_widgets() before the auto non-connected pins have been
    marked as not connected will power the system under the assumption that those
    pins are connected. Once the pins have been marked as disconnected the system
    there will be an additional power run. This can cause unnecessary power
    transitions. Calling snd_soc_dapm_new_widgets() only after the pins have been
    marked as non-connected avoids this.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1803,11 +1803,11 @@ static int snd_soc_instantiate_card(struct snd_soc_card *card)
 		}
 	}
 
-	snd_soc_dapm_new_widgets(&card->dapm);
-
 	if (card->fully_routed)
 		list_for_each_entry(codec, &card->codec_dev_list, card_list)
 			snd_soc_dapm_auto_nc_codec_pins(codec);
+
+	snd_soc_dapm_new_widgets(&card->dapm);
 
 	ret = snd_card_register(card->snd_card);
 	if (ret < 0) {
