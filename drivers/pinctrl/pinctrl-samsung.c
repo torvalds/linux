608a26a7bc04a39cfd7041f31ca2b2100113d14e
Mateusz Krawczuk <m.krawczuk@partner.samsung.com>
2013-08-27 15:08:10 +0200

    pinctrl: Add s5pv210 support to pinctrl-exynos
    This patch implements pinctrl support and adds device tree bindings
    for s5pv210.
    
    Signed-off-by: Mateusz Krawczuk <m.krawczuk@partner.samsung.com>
    Acked-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-samsung.c
+++ drivers/pinctrl/pinctrl-samsung.c
@@ -1122,6 +1122,8 @@ static const struct of_device_id samsung_pinctrl_dt_match[] = {
 		.data = (void *)exynos5250_pin_ctrl },
 	{ .compatible = "samsung,exynos5420-pinctrl",
 		.data = (void *)exynos5420_pin_ctrl },
+	{ .compatible = "samsung,s5pv210-pinctrl",
+		.data = (void *)s5pv210_pin_ctrl },
 #endif
 #ifdef CONFIG_PINCTRL_S3C64XX
 	{ .compatible = "samsung,s3c64xx-pinctrl",
