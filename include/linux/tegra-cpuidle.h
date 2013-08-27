f97c43bbdf8a1ea42477b1a804a48e7e368cb13c
Kyle McMartin <kyle@redhat.com>
2013-08-27 12:52:34 -0600

    tegra-cpuidle: provide stub when !CONFIG_CPU_IDLE
    While poking at something using the for-3.12/* trees, I hit the
    following compile error:
    drivers/built-in.o: In function `tegra_pcie_map_irq':
    /builddir/build/BUILD/kernel-3.10.fc20/linux-3.11.0-0.rc6.git4.1.fc20.armv7hl/drivers/pci/host/pci-tegra.c:640:
    undefined reference to `tegra_cpuidle_pcie_irqs_in_use'
    drivers/built-in.o: In function `tegra_msi_map':
    /builddir/build/BUILD/kernel-3.10.fc20/linux-3.11.0-0.rc6.git4.1.fc20.armv7hl/drivers/pci/host/pci-tegra.c:1227:
    undefined reference to `tegra_cpuidle_pcie_irqs_in_use'
    make: *** [vmlinux] Error 1
    
    Since our .config had CONFIG_CPU_IDLE off. We should probably provide
    an empty function to handle this to avoid cluttering up pci-tegra.c
    with conditionals.
    
    Signed-off-by: Kyle McMartin <kyle@redhat.com>
    Reviewed-by: Thierry Reding <treding@nvidia.com>
    [swarren, removed unnecessary return statement]
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- include/linux/tegra-cpuidle.h
+++ include/linux/tegra-cpuidle.h
@@ -14,6 +14,12 @@
 #ifndef __LINUX_TEGRA_CPUIDLE_H__
 #define __LINUX_TEGRA_CPUIDLE_H__
 
+#ifdef CONFIG_CPU_IDLE
 void tegra_cpuidle_pcie_irqs_in_use(void);
+#else
+static inline void tegra_cpuidle_pcie_irqs_in_use(void)
+{
+}
+#endif
 
 #endif
