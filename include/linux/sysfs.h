f799878000c5a9a1e6a311dfd4faa50601dcb1f8
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-08-27 17:24:49 -0700

    sysfs: add sysfs_create/remove_groups for when SYSFS is not enabled
    We need these functions for when CONFIG_SYSFS=n.
    
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/sysfs.h
+++ include/linux/sysfs.h
@@ -350,6 +350,11 @@ static inline int sysfs_create_group(struct kobject *kobj,
 	return 0;
 }
 
+static inline int sysfs_create_groups(struct kobject *kobj,
+				      const struct attribute_group **groups)
+{
+}
+
 static inline int sysfs_update_group(struct kobject *kobj,
 				const struct attribute_group *grp)
 {
@@ -358,6 +363,11 @@ static inline int sysfs_update_group(struct kobject *kobj,
 
 static inline void sysfs_remove_group(struct kobject *kobj,
 				      const struct attribute_group *grp)
+{
+}
+
+static inline void sysfs_remove_groups(struct kobject *kobj,
+				       const struct attribute_group **groups)
 {
 }
 
