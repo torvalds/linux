e2a240c7d3bcebf90936cc7c22c2729b3a4cec1f
Sonic Zhang <sonic.zhang@analog.com>
2013-08-28 18:55:39 +0800

    driver:net:stmmac: Disable DMA store and forward mode if platform data force_thresh_dma_mode is set.
    Some synopsys ip implementation doesn't support DMA store and forward mode,
    such as BF60x. So, set force_thresh_dma_mode to use DMA thresholds only.
    Update document and devicetree as well.
    
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>
    Acked-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
@@ -79,6 +79,11 @@ static int stmmac_probe_config_dt(struct platform_device *pdev,
 	of_property_read_u32(np, "snps,pbl", &dma_cfg->pbl);
 	dma_cfg->fixed_burst = of_property_read_bool(np, "snps,fixed-burst");
 	dma_cfg->mixed_burst = of_property_read_bool(np, "snps,mixed-burst");
+	plat->force_thresh_dma_mode = of_property_read_bool(np, "snps,force_thresh_dma_mode");
+	if (plat->force_thresh_dma_mode) {
+		plat->force_sf_dma_mode = 0;
+		pr_warn("force_sf_dma_mode is ignored if force_thresh_dma_mode is set.");
+	}
 
 	return 0;
 }
