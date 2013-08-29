6b367cf0567b4fee2b8e7d5535c211e24dcdd302
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-08-29 19:50:31 +0200

    drm/i915: Fix list corruption in vma_unbind
    The saga around the breadcrumb vmas used by execbuf continues ...
    
    This time around we've managed to unconditionally move the object to
    the unbound list on the last vma unbind even though it might never
    have been on either the bound or unbound list. Hilarity ensued.
    
    Chris Wilson tracked this one down but compared to his patches I've
    simply opted to completely separate the unbound case for not-yet bound
    vmas. Otherwise we imo end up with semantically hard to parse checks
    around the list_move_tail(global_list, ...).
    
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Ben Widawsky <ben@bwidawsk.net>
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=68462
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit 0ff501cbb5d825557da7b9a0226ef031344df87d)
    Signed-off-by: Darren Hart <dvhart@linux.intel.com>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -2610,8 +2610,11 @@ int i915_vma_unbind(struct i915_vma *vma)
 	if (list_empty(&vma->vma_link))
 		return 0;
 
-	if (!drm_mm_node_allocated(&vma->node))
-		goto destroy;
+	if (!drm_mm_node_allocated(&vma->node)) {
+		i915_gem_vma_destroy(vma);
+
+		return 0;
+	}
 
 	if (obj->pin_count)
 		return -EBUSY;
@@ -2651,7 +2654,6 @@ int i915_vma_unbind(struct i915_vma *vma)
 
 	drm_mm_remove_node(&vma->node);
 
-destroy:
 	i915_gem_vma_destroy(vma);
 
 	/* Since the unbound list is global, only move to that list if
