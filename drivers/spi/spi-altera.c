f073d37de0468d517b1fb682b5bea5bfa9ce55c9
Axel Lin <axel.lin@ingics.com>
2013-08-29 23:41:20 +0800

    spi: altera: Use DIV_ROUND_UP to calculate hw->bytes_per_word
    The Altera SPI hardware can be configured to support data width from 1 to 32
    since Quartus II 8.1. To avoid truncation by integer division, use DIV_ROUND_UP
    to calculate hw->bytes_per_word.
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Acked-by: Thomas Chou <thomas@wytron.com.tw>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi-altera.c
+++ drivers/spi/spi-altera.c
@@ -134,7 +134,7 @@ static int altera_spi_txrx(struct spi_device *spi, struct spi_transfer *t)
 	hw->tx = t->tx_buf;
 	hw->rx = t->rx_buf;
 	hw->count = 0;
-	hw->bytes_per_word = t->bits_per_word / 8;
+	hw->bytes_per_word = DIV_ROUND_UP(t->bits_per_word, 8);
 	hw->len = t->len / hw->bytes_per_word;
 
 	if (hw->irq >= 0) {
