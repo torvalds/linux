6a3808b8233eb91b57c230cf1161ac116a189ffd
Christian König <christian.koenig@amd.com>
2013-08-30 11:10:33 +0200

    drm/radeon: enable UVD interrupts on CIK
    The same as on evergreen.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reported-by: FrankR Huang <FrankR.Huang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: stable@vger.kernel.org

--- drivers/gpu/drm/radeon/cik.c
+++ drivers/gpu/drm/radeon/cik.c
@@ -6733,6 +6733,10 @@ restart_ih:
 				break;
 			}
 			break;
+		case 124: /* UVD */
+			DRM_DEBUG("IH: UVD int: 0x%08x\n", src_data);
+			radeon_fence_process(rdev, R600_RING_TYPE_UVD_INDEX);
+			break;
 		case 146:
 		case 147:
 			addr = RREG32(VM_CONTEXT1_PROTECTION_FAULT_ADDR);
