326b16db9f69fd0d279be873c6c00f88c0a4aad5
Will Deacon <will.deacon@arm.com>
2013-08-30 18:06:48 +0100

    arm64: delay: don't bother reporting bogomips in /proc/cpuinfo
    We always use a timer-backed delay loop for arm64, so don't bother
    reporting a bogomips value which appears to confuse some people.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -328,9 +328,6 @@ static int c_show(struct seq_file *m, void *v)
 #ifdef CONFIG_SMP
 		seq_printf(m, "processor\t: %d\n", i);
 #endif
-		seq_printf(m, "BogoMIPS\t: %lu.%02lu\n\n",
-			   loops_per_jiffy / (500000UL/HZ),
-			   loops_per_jiffy / (5000UL/HZ) % 100);
 	}
 
 	/* dump out the processor features */
