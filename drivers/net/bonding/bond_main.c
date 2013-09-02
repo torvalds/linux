ee8487c0e1aed52b534f9bf31d3934af4c50bf33
nikolay@redhat.com <nikolay@redhat.com>
2013-09-02 13:51:39 +0200

    bonding: trivial: remove outdated comment and braces
    We don't have to release all slaves when closing the bond dev, so remove
    the outdated comment and the braces around the left single statement.
    
    Signed-off-by: Nikolay Aleksandrov <nikolay@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -3210,12 +3210,8 @@ static int bond_close(struct net_device *bond_dev)
 
 	bond_work_cancel_all(bond);
 	bond->send_peer_notif = 0;
-	if (bond_is_lb(bond)) {
-		/* Must be called only after all
-		 * slaves have been released
-		 */
+	if (bond_is_lb(bond))
 		bond_alb_deinitialize(bond);
-	}
 	bond->recv_probe = NULL;
 
 	return 0;
