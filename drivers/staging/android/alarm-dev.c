b59f105c7e26e30f9c549b9c08251c662ed0cfb8
Cruz Julian Bishop <cruzjbishop@gmail.com>
2013-09-03 22:05:09 +1000

    staging/android: Add kerneldoc to one function in alarm-dev.c
    Sorry. I thought that this would be a nice easy class to document fully.
    Turns out I was very wrong - I will have to research the Linux alarm and
    timer subsystem one day next week and try again.
    
    Here is what I started out with, anyway. It's not much, but it's better
    than nothing!
    
    Signed-off-by: Cruz Julian Bishop <cruzjbishop@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/alarm-dev.c
+++ drivers/staging/android/alarm-dev.c
@@ -60,7 +60,12 @@ struct devalarm {
 
 static struct devalarm alarms[ANDROID_ALARM_TYPE_COUNT];
 
-
+/**
+ * is_wakeup() - Checks to see if this alarm can wake the device
+ * @type:	 The type of alarm being checked
+ *
+ * Return: 1 if this is a wakeup alarm, otherwise 0
+ */
 static int is_wakeup(enum android_alarm_type type)
 {
 	return (type == ANDROID_ALARM_RTC_WAKEUP ||
@@ -75,7 +80,6 @@ static void devalarm_start(struct devalarm *alrm, ktime_t exp)
 	else
 		hrtimer_start(&alrm->u.hrt, exp, HRTIMER_MODE_ABS);
 }
-
 
 static int devalarm_try_to_cancel(struct devalarm *alrm)
 {
