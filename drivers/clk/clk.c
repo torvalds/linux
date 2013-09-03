05ce4e7d0bff01176a33f3a71cbb8cd6cb5465f4
Thierry Reding <thierry.reding@gmail.com>
2013-09-03 09:43:51 +0200

    clk: Move symbol export to proper location
    The __clk_get_flags() symbol is exported immediately following the
    clk_unprepare_unused_subtree() function. This is unusual, since a symbol
    export typically follows body of the function that it exports.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Mike Turquette <mturquette@linaro.org>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -458,7 +458,6 @@ static void clk_unprepare_unused_subtree(struct clk *clk)
 			clk->ops->unprepare(clk->hw);
 	}
 }
-EXPORT_SYMBOL_GPL(__clk_get_flags);
 
 /* caller must hold prepare_lock */
 static void clk_disable_unused_subtree(struct clk *clk)
@@ -607,6 +606,7 @@ unsigned long __clk_get_flags(struct clk *clk)
 {
 	return !clk ? 0 : clk->flags;
 }
+EXPORT_SYMBOL_GPL(__clk_get_flags);
 
 bool __clk_is_prepared(struct clk *clk)
 {
