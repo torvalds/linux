32c4cd54df834d9266db78ca03a126c5ca55ec63
J. R. Okajima <hooanon05@yahoo.co.jp>
2013-09-04 12:28:13 +0900

    aufs: refine XINO truncation
    Now the truncation is triggered by the free space of the filesystem
    where the XINO file exists.
    With introducing a short lived kstatfs cache which is to stop calling
    vfs_statfs() so frequently.
    
    Signed-off-by: J. R. Okajima <hooanon05@yahoo.co.jp>

--- fs/aufs/sbinfo.c
+++ fs/aufs/sbinfo.c
@@ -100,6 +100,9 @@ int au_si_alloc(struct super_block *sb)
 
 	sbinfo->si_mntflags = au_opts_plink(AuOpt_Def);
 
+	sbinfo->si_xino_jiffy = jiffies;
+	sbinfo->si_xino_expire
+		= msecs_to_jiffies(AUFS_XINO_DEF_SEC * MSEC_PER_SEC);
 	mutex_init(&sbinfo->si_xib_mtx);
 	sbinfo->si_xino_brid = -1;
 	/* leave si_xib_last_pindex and si_xib_next_bit */
