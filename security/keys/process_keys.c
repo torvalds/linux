c124bde28bce41f9e46e32d03d134a81e116d38c
Mimi Zohar <zohar@linux.vnet.ibm.com>
2013-09-04 13:26:22 +0100

    KEYS: initialize root uid and session keyrings early
    In order to create the integrity keyrings (eg. _evm, _ima), root's
    uid and session keyrings need to be initialized early.
    
    Signed-off-by: Mimi Zohar <zohar@us.ibm.com>
    Signed-off-by: David Howells <dhowells@redhat.com>

--- security/keys/process_keys.c
+++ security/keys/process_keys.c
@@ -857,3 +857,13 @@ void key_change_session_keyring(struct callback_head *twork)
 
 	commit_creds(new);
 }
+
+/*
+ * Make sure that root's user and user-session keyrings exist.
+ */
+static int __init init_root_keyring(void)
+{
+	return install_user_keyrings();
+}
+
+late_initcall(init_root_keyring);
