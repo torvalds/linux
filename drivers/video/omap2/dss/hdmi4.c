c8a0d959b776f6799b995941ee0c2f8c67110fe8
Archit Taneja <archit@ti.com>
2013-09-06 18:35:44 +0530

    omapdss: OMAP4: HDMI: remove unnecessary edid macros
    The hdmi4 driver has edid macros that aren't used at all. Remove them.
    
    Signed-off-by: Archit Taneja <archit@ti.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/hdmi4.c
+++ drivers/video/omap2/dss/hdmi4.c
@@ -38,14 +38,6 @@
 #include "dss.h"
 #include "dss_features.h"
 
-/* HDMI EDID Length move this */
-#define HDMI_EDID_MAX_LENGTH			256
-#define EDID_TIMING_DESCRIPTOR_SIZE		0x12
-#define EDID_DESCRIPTOR_BLOCK0_ADDRESS		0x36
-#define EDID_DESCRIPTOR_BLOCK1_ADDRESS		0x80
-#define EDID_SIZE_BLOCK0_TIMING_DESCRIPTOR	4
-#define EDID_SIZE_BLOCK1_TIMING_DESCRIPTOR	4
-
 static struct {
 	struct mutex lock;
 	struct platform_device *pdev;
