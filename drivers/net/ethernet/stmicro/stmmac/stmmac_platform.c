356f9e74ffaafd11741589a9aa21d6c9d2721417
Olof Johansson <olof@lixom.net>
2013-09-05 18:01:41 -0700

    net: stmmac: fix bad merge conflict resolution
    Merge commit 06c54055bebf did a bad conflict resolution accidentally
    leaving out a closing brace.  Add it back.
    
    This breaks a handful of defconfigs on ARM, so it'd be good to see it
    applied pretty quickly.
    
    Signed-off-by: Olof Johansson <olof@lixom.net>
    Cc: David S. Miller <davem@davemloft.net>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
@@ -87,6 +87,7 @@ static int stmmac_probe_config_dt(struct platform_device *pdev,
 	if (plat->force_thresh_dma_mode) {
 		plat->force_sf_dma_mode = 0;
 		pr_warn("force_sf_dma_mode is ignored if force_thresh_dma_mode is set.");
+	}
 
 	return 0;
 }
