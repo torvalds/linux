a0fb05d1aef0f5df936f80b726d1b3bfd4275f95
Daniel Borkmann <dborkman@redhat.com>
2013-09-07 16:44:59 +0200

    net: sctp: fix bug in sctp_poll for SOCK_SELECT_ERR_QUEUE
    If we do not add braces around ...
    
      mask |= POLLERR |
              sock_flag(sk, SOCK_SELECT_ERR_QUEUE) ? POLLPRI : 0;
    
    ... then this condition always evaluates to true as POLLERR is
    defined as 8 and binary or'd with whatever result comes out of
    sock_flag(). Hence instead of (X | Y) ? A : B, transform it into
    X | (Y ? A : B). Unfortunatelty, commit 8facd5fb73 ("net: fix
    smatch warnings inside datagram_poll") forgot about SCTP. :-(
    
    Introduced by 7d4c04fc170 ("net: add option to enable error queue
    packets waking select").
    
    Signed-off-by: Daniel Borkmann <dborkman@redhat.com>
    Cc: Jacob Keller <jacob.e.keller@intel.com>
    Acked-by: Neil Horman <nhorman@tuxdriver.com>
    Acked-by: Vlad Yasevich <vyasevich@gmail.com>
    Acked-by: Jacob Keller <jacob.e.keller@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sctp/socket.c
+++ net/sctp/socket.c
@@ -6176,7 +6176,7 @@ unsigned int sctp_poll(struct file *file, struct socket *sock, poll_table *wait)
 	/* Is there any exceptional events?  */
 	if (sk->sk_err || !skb_queue_empty(&sk->sk_error_queue))
 		mask |= POLLERR |
-			sock_flag(sk, SOCK_SELECT_ERR_QUEUE) ? POLLPRI : 0;
+			(sock_flag(sk, SOCK_SELECT_ERR_QUEUE) ? POLLPRI : 0);
 	if (sk->sk_shutdown & RCV_SHUTDOWN)
 		mask |= POLLRDHUP | POLLIN | POLLRDNORM;
 	if (sk->sk_shutdown == SHUTDOWN_MASK)
