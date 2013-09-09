3cea430d80e24b994349af29649f0e124767bbb0
Shridhar Rasal <srasal@nvidia.com>
2013-09-09 19:17:14 +0530

    cpufreq: interactive: delete timers for GOV_START
    Make sure that timers cpu_timer and cpu_slack_timer
    deactivated before addition of new.
    
    Change-Id: If31c4049606871df6f00efdc24b1d713c86a6f69
    Signed-off-by: Shridhar Rasal <srasal@nvidia.com>
    Signed-off-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -1247,6 +1247,8 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 			pcpu->hispeed_validate_time =
 				pcpu->floor_validate_time;
 			down_write(&pcpu->enable_sem);
+			del_timer_sync(&pcpu->cpu_timer);
+			del_timer_sync(&pcpu->cpu_slack_timer);
 			cpufreq_interactive_timer_start(tunables, j);
 			pcpu->governor_enabled = 1;
 			up_write(&pcpu->enable_sem);
