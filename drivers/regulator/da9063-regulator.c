0fdfd40aca7046eb0b770d64dfe526a03ed5d092
Geert Uytterhoeven <geert@linux-m68k.org>
2013-09-09 13:49:27 +0200

    regulator: da9063: Add missing initialization of da9063_reg_matches
    With gcc 4.1.2:
    
    drivers/regulator/da9063-regulator.c: In function ‘da9063_regulator_probe’:
    drivers/regulator/da9063-regulator.c:847: warning: ‘da9063_reg_matches’ is used
    uninitialized in this function
    
    If the parent device already has platform data, da9063_reg_matches will
    not be initialized.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/regulator/da9063-regulator.c
+++ drivers/regulator/da9063-regulator.c
@@ -717,7 +717,7 @@ static int da9063_regulator_probe(struct platform_device *pdev)
 {
 	struct da9063 *da9063 = dev_get_drvdata(pdev->dev.parent);
 	struct da9063_pdata *da9063_pdata = dev_get_platdata(da9063->dev);
-	struct of_regulator_match *da9063_reg_matches;
+	struct of_regulator_match *da9063_reg_matches = NULL;
 	struct da9063_regulators_pdata *regl_pdata;
 	const struct da9063_dev_model *model;
 	struct da9063_regulators *regulators;
