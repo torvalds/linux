e84cc14213e2c81ae5a2da341a9da0d58a1dbfad
Filipe David Borba Manana <fdmanana@gmail.com>
2013-09-09 19:49:43 +0100

    Btrfs: don't leak block group on error
    In extent-tree.c:btrfs_write_dirty_block_groups(), if the call to
    write_one_cache_group() failed, we would return without putting
    the block group first.
    
    Signed-off-by: Filipe David Borba Manana <fdmanana@gmail.com>
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>
    Signed-off-by: Chris Mason <chris.mason@fusionio.com>

--- fs/btrfs/extent-tree.c
+++ fs/btrfs/extent-tree.c
@@ -3317,10 +3317,9 @@ again:
 		last = cache->key.objectid + cache->key.offset;
 
 		err = write_one_cache_group(trans, root, path, cache);
+		btrfs_put_block_group(cache);
 		if (err) /* File system offline */
 			goto out;
-
-		btrfs_put_block_group(cache);
 	}
 
 	while (1) {
