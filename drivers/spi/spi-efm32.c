e6f7563b7354c2eb26d89b8622b3582a4c6510d8
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2013-09-10 09:51:30 +0200

    spi: efm32: add spi_bitbang_stop to device remove callback
    This call is needed to cleanup the resources requested by
    spi_bitbang_start in the probe callback.
    
    Noticed-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi-efm32.c
+++ drivers/spi/spi-efm32.c
@@ -477,6 +477,8 @@ static int efm32_spi_remove(struct platform_device *pdev)
 	struct spi_master *master = platform_get_drvdata(pdev);
 	struct efm32_spi_ddata *ddata = spi_master_get_devdata(master);
 
+	spi_bitbang_stop(&ddata->bitbang);
+
 	efm32_spi_write32(ddata, 0, REG_IEN);
 
 	free_irq(ddata->txirq, ddata);
