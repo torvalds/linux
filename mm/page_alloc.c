6decef03e12ed91d6f29a298784b3d09ec0770d4
Chen Gang <gang.chen@asianux.com>
2013-09-11 14:20:27 -0700

    mm/page_alloc.c: use '__paginginit' instead of '__init'
    set_pageblock_order() may be called when memory hotplug, so need use
    '__paginginit' instead of '__init'.
    
    The related warning:
    
      The function __meminit .free_area_init_node() references
      a function __init .set_pageblock_order().
      If .set_pageblock_order is only used by .free_area_init_node then
      annotate .set_pageblock_order with a matching annotation.
    
    Signed-off-by: Chen Gang <gang.chen@asianux.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 15ca220e1a63af06e000691e4ae1beaba5430c32)

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4551,7 +4551,7 @@ static inline void setup_usemap(struct pglist_data *pgdat, struct zone *zone,
 #ifdef CONFIG_HUGETLB_PAGE_SIZE_VARIABLE
 
 /* Initialise the number of pages represented by NR_PAGEBLOCK_BITS */
-void __init set_pageblock_order(void)
+void __paginginit set_pageblock_order(void)
 {
 	unsigned int order;
 
@@ -4579,7 +4579,7 @@ void __init set_pageblock_order(void)
  * include/linux/pageblock-flags.h for the values of pageblock_order based on
  * the kernel config
  */
-void __init set_pageblock_order(void)
+void __paginginit set_pageblock_order(void)
 {
 }
 
