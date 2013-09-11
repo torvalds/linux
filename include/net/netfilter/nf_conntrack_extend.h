78ab8cbc11ee095ec27d7f6edde91e2c3af1540d
Michal Kubecek <mkubecek@suse.cz>
2013-09-11 10:17:27 +0200

    netfilter: nf_conntrack: use RCU safe kfree for conntrack extensions
    commit c13a84a830a208fb3443628773c8ca0557773cc7 upstream.
    
    Commit 68b80f11 (netfilter: nf_nat: fix RCU races) introduced
    RCU protection for freeing extension data when reallocation
    moves them to a new location. We need the same protection when
    freeing them in nf_ct_ext_free() in order to prevent a
    use-after-free by other threads referencing a NAT extension data
    via bysource list.
    
    Signed-off-by: Michal Kubecek <mkubecek@suse.cz>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/net/netfilter/nf_conntrack_extend.h
+++ include/net/netfilter/nf_conntrack_extend.h
@@ -80,7 +80,7 @@ static inline void nf_ct_ext_destroy(struct nf_conn *ct)
 static inline void nf_ct_ext_free(struct nf_conn *ct)
 {
 	if (ct->ext)
-		kfree(ct->ext);
+		kfree_rcu(ct->ext, rcu);
 }
 
 /* Add this type, returns pointer to data or NULL. */
