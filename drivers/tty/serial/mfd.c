f8bf876209a9828a456a9ac09ef6369a007dda2d
Jingoo Han <jg1.han@samsung.com>
2013-09-12 15:31:06 +0900

    serial: mfd: remove unnecessary pci_set_drvdata()
    The driver core clears the driver data to NULL after device_release
    or on probe failure. Thus, it is not needed to manually clear the
    device driver data to NULL.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/mfd.c
+++ drivers/tty/serial/mfd.c
@@ -1451,7 +1451,6 @@ static void serial_hsu_remove(struct pci_dev *pdev)
 		uart_remove_one_port(&serial_hsu_reg, &up->port);
 	}
 
-	pci_set_drvdata(pdev, NULL);
 	free_irq(pdev->irq, priv);
 	pci_disable_device(pdev);
 }
