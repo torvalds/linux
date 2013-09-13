01a74c11d3ab3adb842e168ee0c2ac1efe35d71a
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2013-09-13 17:43:17 +0100

    ALSA: compress: Fix compress device unregister.
    commit 4028b6c4c03f213260e9290ff3a6b5439aad07ce upstream.
    
    snd_unregister_device() should return the device type and not stream
    direction.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>
    Acked-by: Vinod Koul <vinod.koul@intel.com>
    Tested-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- sound/core/compress_offload.c
+++ sound/core/compress_offload.c
@@ -663,7 +663,8 @@ static int snd_compress_dev_disconnect(struct snd_device *device)
 	struct snd_compr *compr;
 
 	compr = device->device_data;
-	snd_unregister_device(compr->direction, compr->card, compr->device);
+	snd_unregister_device(SNDRV_DEVICE_TYPE_COMPRESS, compr->card,
+		compr->device);
 	return 0;
 }
 
