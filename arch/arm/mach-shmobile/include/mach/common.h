6b30e97ec4f17ba0575d51d39e366232f9e27f7b
Magnus Damm <damm@opensource.se>
2013-09-14 22:47:02 +0900

    ARM: shmobile: Remove shmobile_smp_scu_boot_secondary()
    Remove shmobile_smp_scu_boot_secondary() since
    it is no longer used. CPU boot vector setup is
    instead handled by CPU notifiers.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    (cherry picked from commit e4550216ef8245b1c9892239a2121a7b15ac2e91)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/include/mach/common.h
+++ arch/arm/mach-shmobile/include/mach/common.h
@@ -18,8 +18,6 @@ extern int shmobile_smp_cpu_disable(unsigned int cpu);
 extern void shmobile_invalidate_start(void);
 extern void shmobile_boot_scu(void);
 extern void shmobile_smp_scu_prepare_cpus(unsigned int max_cpus);
-extern int shmobile_smp_scu_boot_secondary(unsigned int cpu,
-					   struct task_struct *idle);
 extern void shmobile_smp_scu_cpu_die(unsigned int cpu);
 extern int shmobile_smp_scu_cpu_kill(unsigned int cpu);
 extern void shmobile_smp_apmu_prepare_cpus(unsigned int max_cpus);
