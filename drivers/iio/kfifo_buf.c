623c334c1a40ae84a19ab8ef77924e9f0f2d4bcd
Lars-Peter Clausen <lars@metafoo.de>
2013-09-15 16:31:00 +0100

    iio:kfifo_buf: Use wake_up_interruptible_poll()
    Use wake_up_interruptible_poll() instead of wake_up_interruptible() to only wake
    up those threads that listen for input poll notifications.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/kfifo_buf.c
+++ drivers/iio/kfifo_buf.c
@@ -7,6 +7,7 @@
 #include <linux/mutex.h>
 #include <linux/iio/kfifo_buf.h>
 #include <linux/sched.h>
+#include <linux/poll.h>
 
 struct iio_kfifo {
 	struct iio_buffer buffer;
@@ -102,7 +103,7 @@ static int iio_store_to_kfifo(struct iio_buffer *r,
 	if (ret != 1)
 		return -EBUSY;
 	r->stufftoread = true;
-	wake_up_interruptible(&r->pollq);
+	wake_up_interruptible_poll(&r->pollq, POLLIN | POLLRDNORM);
 
 	return 0;
 }
