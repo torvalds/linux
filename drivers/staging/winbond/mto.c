7a0b6c62441e951af7f356d1ef34edb95c61e23b
Iker Pedrosa <ikerpedrosam@gmail.com>
2013-09-16 15:43:21 +0200

    Staging: winbond: mto: removed function declaration
    The following function declarations have been removed because they aren't implemented.
    
    Signed-off-by: Iker Pedrosa <ikerpedrosam@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/winbond/mto.c
+++ drivers/staging/winbond/mto.c
@@ -46,10 +46,7 @@ static int retryrate_rec[MTO_MAX_DATA_RATE_LEVELS];
 static u8 boSparseTxTraffic;
 
 void MTO_Init(struct wbsoft_priv *adapter);
-void TxRateReductionCtrl(struct wbsoft_priv *adapter);
 void MTO_SetTxCount(struct wbsoft_priv *adapter, u8 t0, u8 index);
-void MTO_TxFailed(struct wbsoft_priv *adapter);
-void hal_get_dto_para(struct wbsoft_priv *adapter, char *buffer);
 
 /*
  * ===========================================================================
