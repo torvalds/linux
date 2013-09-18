00c877c69ba315d6c565a4df51c71b11e82cdeb8
Laxman Dewangan <ldewangan@nvidia.com>
2013-09-18 18:18:02 +0530

    regulator: core: add support for configuring turn-on time through constraints
    The turn-on time of the regulator depends on the regulator device's
    electrical characteristics. Sometimes regulator turn-on time also
    depends on the capacitive load on the given platform and it can be
    more than the datasheet value.
    
    The driver provides the enable-time as per datasheet.
    
    Add support for configure the enable ramp time through regulator
    constraints so that regulator core can take this value for enable
    time for that regulator.
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Acked-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -1186,6 +1186,8 @@ overflow_err:
 
 static int _regulator_get_enable_time(struct regulator_dev *rdev)
 {
+	if (rdev->constraints && rdev->constraints->enable_time)
+		return rdev->constraints->enable_time;
 	if (!rdev->desc->ops->enable_time)
 		return rdev->desc->enable_time;
 	return rdev->desc->ops->enable_time(rdev);
