832ae42a43dd7ea2a39d7cc0687363d0039da850
Ansis Atteka <aatteka@nicira.com>
2013-09-18 15:29:52 -0700

    ip: use ip_hdr() in __ip_make_skb() to retrieve IP header
    [ Upstream commit 749154aa56b57652a282cbde57a57abc278d1205 ]
    
    skb->data already points to IP header, but for the sake of
    consistency we can also use ip_hdr() to retrieve it.
    
    Signed-off-by: Ansis Atteka <aatteka@nicira.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/ipv4/ip_output.c
+++ net/ipv4/ip_output.c
@@ -1342,7 +1342,7 @@ struct sk_buff *__ip_make_skb(struct sock *sk,
 	else
 		ttl = ip_select_ttl(inet, &rt->dst);
 
-	iph = (struct iphdr *)skb->data;
+	iph = ip_hdr(skb);
 	iph->version = 4;
 	iph->ihl = 5;
 	iph->tos = inet->tos;
