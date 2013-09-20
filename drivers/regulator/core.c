f8c1700dd7d2ce9b2238b20d364317b2968ac76b
Laxman Dewangan <ldewangan@nvidia.com>
2013-09-20 13:13:02 +0530

    regulator: core: set current constraints while setting machine constraints
    Machine constraints is configured during regulator register. If current
    constraints are provided through machine constraints then it is observed
    that sometime the current configured on rail is out of range what machine
    constraint has.
    
    Set the current constraints when setting machine constraints to make
    sure that rail's current is within the range of given machine constraints.
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -923,6 +923,36 @@ static int machine_constraints_voltage(struct regulator_dev *rdev,
 	return 0;
 }
 
+static int machine_constraints_current(struct regulator_dev *rdev,
+	struct regulation_constraints *constraints)
+{
+	struct regulator_ops *ops = rdev->desc->ops;
+	int ret;
+
+	if (!constraints->min_uA && !constraints->max_uA)
+		return 0;
+
+	if (constraints->min_uA > constraints->max_uA) {
+		rdev_err(rdev, "Invalid current constraints\n");
+		return -EINVAL;
+	}
+
+	if (!ops->set_current_limit || !ops->get_current_limit) {
+		rdev_warn(rdev, "Operation of current configuration missing\n");
+		return 0;
+	}
+
+	/* Set regulator current in constraints range */
+	ret = ops->set_current_limit(rdev, constraints->min_uA,
+			constraints->max_uA);
+	if (ret < 0) {
+		rdev_err(rdev, "Failed to set current constraint, %d\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
 /**
  * set_machine_constraints - sets regulator constraints
  * @rdev: regulator source
@@ -950,6 +980,10 @@ static int set_machine_constraints(struct regulator_dev *rdev,
 		return -ENOMEM;
 
 	ret = machine_constraints_voltage(rdev, rdev->constraints);
+	if (ret != 0)
+		goto out;
+
+	ret = machine_constraints_current(rdev, rdev->constraints);
 	if (ret != 0)
 		goto out;
 
