ffab239956613be7b81d00999128fd05fd096762
Tero Kristo <t-kristo@ti.com>
2013-09-20 17:02:40 +0300

    CLK: TI: add am43xx clock init file
    clk-43xx.c now contains the clock init functionality for am43xx, including
    DT clock registration and adding of static clkdev entries.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>
    Tested-by: Nishanth Menon <nm@ti.com>
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Mike Turquette <mturquette@linaro.org>

--- include/linux/clk/ti.h
+++ include/linux/clk/ti.h
@@ -273,6 +273,7 @@ int omap4xxx_dt_clk_init(void);
 int omap5xxx_dt_clk_init(void);
 int dra7xx_dt_clk_init(void);
 int am33xx_dt_clk_init(void);
+int am43xx_dt_clk_init(void);
 
 #ifdef CONFIG_OF
 void of_ti_clk_allow_autoidle_all(void);
