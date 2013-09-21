18d8519d35cb0b794d9b9c6f4ff7b97940a95bda
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-09-21 04:10:54 -0400

    OMAP/serial: Fix Mode13 vs Mode16 priority
    Make Mode16 more preferred than Mode13, to match TRM baudrates table.
    
    Signed-off-by: Alexey Pelykh <alexey.pelykh@gmail.com>
    Cc: Tony Lindgren <tony@atomide.com>
    Cc: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/omap-serial.c
+++ drivers/tty/serial/omap-serial.c
@@ -247,7 +247,7 @@ serial_omap_baud_is_mode16(struct uart_port *port, unsigned int baud)
 	if(baudAbsDiff16 < 0)
 		baudAbsDiff16 = -baudAbsDiff16;
 
-	return (baudAbsDiff13 > baudAbsDiff16);
+	return (baudAbsDiff13 >= baudAbsDiff16);
 }
 
 /*
