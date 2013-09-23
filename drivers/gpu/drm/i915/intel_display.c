773ae0346576a19b6a5e10adb0f63deca603ee00
Ville Syrjälä <ville.syrjala@linux.intel.com>
2013-09-23 17:48:20 +0300

    drm/i915: Fix intel_crtc_mode_get() mode clock
    i9xx_crtc_clock_get() no longer populates adjusted_mode.clock, so we
    must get the pixel clock from port_clock in intel_crtc_mode_get().
    
    This bug caused Chris's 845g machine to lockup during boot, and it
    was introduced in:
    
     commit 18442d08786472c63a0a80c27f92b033dffc26de
     Author: Ville Syrjälä <ville.syrjala@linux.intel.com>
     Date:   Fri Sep 13 16:00:08 2013 +0300
    
        drm/i915: Fix port_clock and adjusted_mode.clock readout all over
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=69713
    Tested-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -7543,7 +7543,7 @@ struct drm_display_mode *intel_crtc_mode_get(struct drm_device *dev,
 	pipe_config.dpll_hw_state.fp1 = I915_READ(FP1(pipe));
 	i9xx_crtc_clock_get(intel_crtc, &pipe_config);
 
-	mode->clock = pipe_config.adjusted_mode.clock;
+	mode->clock = pipe_config.port_clock / pipe_config.pixel_multiplier;
 	mode->hdisplay = (htot & 0xffff) + 1;
 	mode->htotal = ((htot & 0xffff0000) >> 16) + 1;
 	mode->hsync_start = (hsync & 0xffff) + 1;
