8ecf542b84b713cbe4e96552cbfe7eca80572315
Chris Wilson <chris@chris-wilson.co.uk>
2013-09-25 10:23:19 +0100

    drm/i915: Show WT caching in debugfs
    Add the missing cache-level to the describe_obj() function for debug and
    error reporting.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit f56383cb9fc8d7165bd7d2136b4c4af98c41177c)
    Signed-off-by: Darren Hart <dvhart@linux.intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -324,7 +324,7 @@ struct drm_i915_error_state {
 		u32 dirty:1;
 		u32 purgeable:1;
 		s32 ring:4;
-		u32 cache_level:2;
+		u32 cache_level:3;
 	} **active_bo, **pinned_bo;
 	u32 *active_bo_count, *pinned_bo_count;
 	struct intel_overlay_error_state *overlay;
