3764eb72a514cad27ab09fa0b8e591b2b8b7a70b
Larry Finger <Larry.Finger@lwfinger.net>
2013-09-26 13:25:29 -0500

    rtlwifi: rtl8192ce: Convert driver to use new rtl_phy_scan_operation_backup() routine
    Now that rtl_phy_scan_operation_backup() exists, convert rtl8192ce to use it.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rtlwifi/rtl8192ce/phy.h
+++ drivers/net/wireless/rtlwifi/rtl8192ce/phy.h
@@ -207,8 +207,6 @@ void rtl92c_phy_get_txpower_level(struct ieee80211_hw *hw,
 void rtl92c_phy_set_txpower_level(struct ieee80211_hw *hw, u8 channel);
 bool rtl92c_phy_update_txpower_dbm(struct ieee80211_hw *hw,
 					  long power_indbm);
-void rtl92c_phy_scan_operation_backup(struct ieee80211_hw *hw,
-					     u8 operation);
 void rtl92c_phy_set_bw_mode(struct ieee80211_hw *hw,
 				   enum nl80211_channel_type ch_type);
 void rtl92c_phy_sw_chnl_callback(struct ieee80211_hw *hw);
