05071aa864e84000759191438a4a9ff7ba2c360e
Lars-Peter Clausen <lars@metafoo.de>
2013-09-27 16:34:27 +0200

    spi: Add a spi_w8r16be() helper
    This patch adds a new spi_w8r16be() helper, which is similar to spi_w8r16()
    except that it converts the read data word from big endian to native endianness
    before returning it. The reason for introducing this new helper is that for SPI
    slave devices it is quite common that the read 16 bit data word is in big
    endian. So users of spi_w8r16() have to convert the result to native endianness
    manually. A second reason is that in this case the endianness of the return
    value of spi_w8r16() depends on its sign. If it is negative (i.e. a error code)
    it is already in native endianness, if it is positive it is in big endian. The
    sparse code checker doesn't like this kind of mixed endianness and special
    annotations are necessary to keep it quiet (E.g. casting to be16 using __force).
    Doing the conversion to native endianness in the helper function does not
    require such annotations since we are not mixing different endiannesses in the
    same variable.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- include/linux/spi/spi.h
+++ include/linux/spi/spi.h
@@ -823,6 +823,33 @@ static inline ssize_t spi_w8r16(struct spi_device *spi, u8 cmd)
 	return (status < 0) ? status : result;
 }
 
+/**
+ * spi_w8r16be - SPI synchronous 8 bit write followed by 16 bit big-endian read
+ * @spi: device with which data will be exchanged
+ * @cmd: command to be written before data is read back
+ * Context: can sleep
+ *
+ * This returns the (unsigned) sixteen bit number returned by the device in cpu
+ * endianness, or else a negative error code. Callable only from contexts that
+ * can sleep.
+ *
+ * This function is similar to spi_w8r16, with the exception that it will
+ * convert the read 16 bit data word from big-endian to native endianness.
+ *
+ */
+static inline ssize_t spi_w8r16be(struct spi_device *spi, u8 cmd)
+
+{
+	ssize_t status;
+	__be16 result;
+
+	status = spi_write_then_read(spi, &cmd, 1, &result, 2);
+	if (status < 0)
+		return status;
+
+	return be16_to_cpu(result);
+}
+
 /*---------------------------------------------------------------------------*/
 
 /*
