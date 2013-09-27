22d7dc8aba3bf34fdf324f43718c238075cc7cf7
Lee Jones <lee.jones@linaro.org>
2013-09-27 14:25:55 +0100

    mfd: arizona: Only attempt to parse DT if platform data was not passed
    If platform data is passed when probing the device then it should take
    precedence over Device Tree. This patch saves cycles in the pdata case
    and prevents error messages when DT is not passed.
    
    Reported-by: Mark Brown <broonie@linaro.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/arizona-core.c
+++ drivers/mfd/arizona-core.c
@@ -607,11 +607,11 @@ int arizona_dev_init(struct arizona *arizona)
 	dev_set_drvdata(arizona->dev, arizona);
 	mutex_init(&arizona->clk_lock);
 
-	arizona_of_get_core_pdata(arizona);
-
 	if (dev_get_platdata(arizona->dev))
 		memcpy(&arizona->pdata, dev_get_platdata(arizona->dev),
 		       sizeof(arizona->pdata));
+	else
+		arizona_of_get_core_pdata(arizona);
 
 	regcache_cache_only(arizona->regmap, true);
 
