59197e447108220bc0b442d4e6749e24bb3d8fef
Antony Pavlov <antonynpavlov@gmail.com>
2013-09-28 19:49:34 +0400

    MIPS: JZ4740: reuse UART0 address macro for vmlinuz debug port
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>
    Acked-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: John Crispin <blogic@openwrt.org>
    Patchwork: http://patchwork.linux-mips.org/patch/5927/

--- arch/mips/boot/compressed/uart-16550.c
+++ arch/mips/boot/compressed/uart-16550.c
@@ -19,8 +19,8 @@
 #endif
 
 #ifdef CONFIG_MACH_JZ4740
-#define UART0_BASE  0xB0030000
-#define PORT(offset) (UART0_BASE + (4 * offset))
+#include <asm/mach-jz4740/base.h>
+#define PORT(offset) (CKSEG1ADDR(JZ4740_UART0_BASE_ADDR) + (4 * offset))
 #endif
 
 #ifdef CONFIG_CPU_XLR
