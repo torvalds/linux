f69727fd78fa761dc49ee3091c432a8c6ab81292
Felix Fietkau <nbd@openwrt.org>
2013-09-29 13:06:31 +0200

    ath9k: fix powersave response handling for BA session packets
    When a packet is passed from mac80211 to the driver with the
    IEEE80211_TX_CTL_PS_RESPONSE flag set, it bypasses the normal driver
    internal queueing and goes directly to the UAPSD queue.
    
    When that happens, packets that are part of a BlockAck session still
    need to be tracked as such inside the driver, otherwise it will create
    discrepancies in the receiver BA reorder window, causing traffic stalls.
    This only happens in AP mode with powersave-enabled clients.
    
    This patch fixes the regression introduced in the commit
    "ath9k: use software queues for un-aggregated data packets"
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/xmit.c
+++ drivers/net/wireless/ath/ath9k/xmit.c
@@ -1969,15 +1969,18 @@ static void ath_tx_txqaddbuf(struct ath_softc *sc, struct ath_txq *txq,
 static void ath_tx_send_normal(struct ath_softc *sc, struct ath_txq *txq,
 			       struct ath_atx_tid *tid, struct sk_buff *skb)
 {
+	struct ieee80211_tx_info *tx_info = IEEE80211_SKB_CB(skb);
 	struct ath_frame_info *fi = get_frame_info(skb);
 	struct list_head bf_head;
-	struct ath_buf *bf;
-
-	bf = fi->bf;
+	struct ath_buf *bf = fi->bf;
 
 	INIT_LIST_HEAD(&bf_head);
 	list_add_tail(&bf->list, &bf_head);
 	bf->bf_state.bf_type = 0;
+	if (tid && (tx_info->flags & IEEE80211_TX_CTL_AMPDU)) {
+		bf->bf_state.bf_type = BUF_AMPDU;
+		ath_tx_addto_baw(sc, tid, bf);
+	}
 
 	bf->bf_next = NULL;
 	bf->bf_lastbf = bf;
