ff0359af10e1a4a36c66ed9deaf979d0890fa980
hjc <hjc@rock-chips.com>
2013-09-30 18:30:06 +0800

    rk3026_86v: set ddr 300 default voltage

--- arch/arm/mach-rk3026/board-rk3026-86v.c
+++ arch/arm/mach-rk3026/board-rk3026-86v.c
@@ -1764,7 +1764,7 @@ static struct cpufreq_frequency_table dvfs_gpu_table[] = {
 
 static struct cpufreq_frequency_table dvfs_ddr_table[] = {
 	{.frequency = 200 * 1000 + DDR_FREQ_SUSPEND,    .index = 1200 * 1000},
-	//{.frequency = 300 * 1000 + DDR_FREQ_VIDEO,      .index = 1200 * 1000},
+	{.frequency = 300 * 1000,                       .index = 1200 * 1000},
 	{.frequency = 400 * 1000 + DDR_FREQ_NORMAL,     .index = 1200 * 1000},
 	{.frequency = CPUFREQ_TABLE_END},
 };
