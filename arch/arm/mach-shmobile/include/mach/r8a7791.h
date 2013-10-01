ddbb0f9bea337a8a6d09e100d8192e0a8439106f
Magnus Damm <damm@opensource.se>
2013-10-01 17:11:53 +0900

    ARM: shmobile: Introduce r8a7791_add_standard_devices()
    Introduce the function r8a7791_add_standard_devices() that
    follows the same style as other mach-shmobile SoC code and
    allows C version of board code to add on-chip devices.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    (cherry picked from commit 44a268e2a652eab3c21e282b6418a8c9ea279626)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/include/mach/r8a7791.h
+++ arch/arm/mach-shmobile/include/mach/r8a7791.h
@@ -1,6 +1,7 @@
 #ifndef __ASM_R8A7791_H__
 #define __ASM_R8A7791_H__
 
+void r8a7791_add_standard_devices(void);
 void r8a7791_add_dt_devices(void);
 void r8a7791_clock_init(void);
 void r8a7791_init_early(void);
