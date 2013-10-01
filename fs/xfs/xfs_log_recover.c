b2a42f78ab475f4730300b0e9568bc3b2587d112
Thierry Reding <thierry.reding@gmail.com>
2013-10-01 16:47:53 +0200

    xfs: Use kmem_free() instead of free()
    This fixes a build failure caused by calling the free() function which
    does not exist in the Linux kernel.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Reviewed-by: Mark Tinguely <tinguely@sgi.com>
    Signed-off-by: Ben Myers <bpm@sgi.com>
    
    (cherry picked from commit aaaae98022efa4f3c31042f1fdf9e7a0c5f04663)

--- fs/xfs/xfs_log_recover.c
+++ fs/xfs/xfs_log_recover.c
@@ -1585,7 +1585,7 @@ xlog_recover_add_to_trans(
 		"bad number of regions (%d) in inode log format",
 				  in_f->ilf_size);
 			ASSERT(0);
-			free(ptr);
+			kmem_free(ptr);
 			return XFS_ERROR(EIO);
 		}
 
