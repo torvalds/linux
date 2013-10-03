eb4c7df6c20b407ecbf1a985edc33d967371c2e8
Shirish Pargaonkar <shirishpargaonkar@gmail.com>
2013-10-03 05:44:45 -0500

    cifs: Avoid umount hangs with smb2 when server is unresponsive
    Do not send SMB2 Logoff command when reconnecting, the way smb1
    code base works.
    
    Also, no need to wait for a credit for an echo command when one is already
    in flight.
    
    Without these changes, umount command hangs if the server is unresponsive
    e.g. hibernating.
    
    Signed-off-by: Shirish Pargaonkar <shirishpargaonkar@gmail.com>
    Acked-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Steve French <smfrench@us.ibm.com>

--- fs/cifs/smb2pdu.c
+++ fs/cifs/smb2pdu.c
@@ -687,6 +687,10 @@ SMB2_logoff(const unsigned int xid, struct cifs_ses *ses)
 	else
 		return -EIO;
 
+	/* no need to send SMB logoff if uid already closed due to reconnect */
+	if (ses->need_reconnect)
+		goto smb2_session_already_dead;
+
 	rc = small_smb2_init(SMB2_LOGOFF, NULL, (void **) &req);
 	if (rc)
 		return rc;
@@ -701,6 +705,8 @@ SMB2_logoff(const unsigned int xid, struct cifs_ses *ses)
 	 * No tcon so can't do
 	 * cifs_stats_inc(&tcon->stats.smb2_stats.smb2_com_fail[SMB2...]);
 	 */
+
+smb2_session_already_dead:
 	return rc;
 }
 
