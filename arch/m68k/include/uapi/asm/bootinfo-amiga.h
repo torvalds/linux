7bc449688bf73891625616f3f2e93e359603016b
Geert Uytterhoeven <geert@linux-m68k.org>
2013-10-03 11:26:22 +0200

    m68k/UAPI: Move Amiga model/chipset definitions to <asm/bootinfo-amiga.h>
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- arch/m68k/include/uapi/asm/bootinfo-amiga.h
+++ arch/m68k/include/uapi/asm/bootinfo-amiga.h
@@ -22,6 +22,38 @@
 
 
     /*
+     *  Amiga models (BI_AMIGA_MODEL)
+     */
+
+#define AMI_UNKNOWN		0
+#define AMI_500			1
+#define AMI_500PLUS		2
+#define AMI_600			3
+#define AMI_1000		4
+#define AMI_1200		5
+#define AMI_2000		6
+#define AMI_2500		7
+#define AMI_3000		8
+#define AMI_3000T		9
+#define AMI_3000PLUS		10
+#define AMI_4000		11
+#define AMI_4000T		12
+#define AMI_CDTV		13
+#define AMI_CD32		14
+#define AMI_DRACO		15
+
+
+    /*
+     *  Amiga chipsets (BI_AMIGA_CHIPSET)
+     */
+
+#define CS_STONEAGE		0
+#define CS_OCS			1
+#define CS_ECS			2
+#define CS_AGA			3
+
+
+    /*
      *  Latest Amiga bootinfo version
      */
 
