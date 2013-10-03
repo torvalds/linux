7678e77d2a2fb2dd74a27b8283e030c580294a62
Geert Uytterhoeven <geert@linux-m68k.org>
2013-10-03 11:31:08 +0200

    m68k/UAPI: Move Apollo model definitions to <asm/bootinfo-apollo.h>
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- arch/m68k/include/uapi/asm/bootinfo-apollo.h
+++ arch/m68k/include/uapi/asm/bootinfo-apollo.h
@@ -13,4 +13,16 @@
 #define BI_APOLLO_MODEL		0x8000	/* model (u_long) */
 
 
+    /*
+     *  Apollo models (BI_APOLLO_MODEL)
+     */
+
+#define APOLLO_UNKNOWN		0
+#define APOLLO_DN3000		1
+#define APOLLO_DN3010		2
+#define APOLLO_DN3500		3
+#define APOLLO_DN4000		4
+#define APOLLO_DN4500		5
+
+
 #endif /* _UAPI_ASM_M68K_BOOTINFO_APOLLO_H */
