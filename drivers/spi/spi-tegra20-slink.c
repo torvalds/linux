fb30a1819a4315091cb731403ebf74ff1a490f43
Mark Brown <broonie@linaro.org>
2013-10-04 16:39:22 +0100

    spi/tegra20-slink: Remove unused is_single_xfer check
    Currently transfer_one_message() checks to see if the message consists of
    a single spi_transfer and tells _start_transfer_one() but it just ignores
    this. Don't bother.
    
    Signed-off-by: Mark Brown <broonie@linaro.org>
    Acked-by: Laxman Dewangan <ldewangan@nvidia.com>

--- drivers/spi/spi-tegra20-slink.c
+++ drivers/spi/spi-tegra20-slink.c
@@ -707,8 +707,7 @@ static void tegra_slink_deinit_dma_param(struct tegra_slink_data *tspi,
 }
 
 static int tegra_slink_start_transfer_one(struct spi_device *spi,
-		struct spi_transfer *t, bool is_first_of_msg,
-		bool is_single_xfer)
+		struct spi_transfer *t, bool is_first_of_msg)
 {
 	struct tegra_slink_data *tspi = spi_master_get_devdata(spi->master);
 	u32 speed;
@@ -828,7 +827,6 @@ static int tegra_slink_transfer_one_message(struct spi_master *master,
 			struct spi_message *msg)
 {
 	bool is_first_msg = true;
-	int single_xfer;
 	struct tegra_slink_data *tspi = spi_master_get_devdata(master);
 	struct spi_transfer *xfer;
 	struct spi_device *spi = msg->spi;
@@ -837,11 +835,9 @@ static int tegra_slink_transfer_one_message(struct spi_master *master,
 	msg->status = 0;
 	msg->actual_length = 0;
 
-	single_xfer = list_is_singular(&msg->transfers);
 	list_for_each_entry(xfer, &msg->transfers, transfer_list) {
 		INIT_COMPLETION(tspi->xfer_completion);
-		ret = tegra_slink_start_transfer_one(spi, xfer,
-					is_first_msg, single_xfer);
+		ret = tegra_slink_start_transfer_one(spi, xfer, is_first_msg);
 		if (ret < 0) {
 			dev_err(tspi->dev,
 				"spi can not start transfer, err %d\n", ret);
