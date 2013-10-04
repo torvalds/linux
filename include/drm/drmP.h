2a3ca34aa531332c20b342615049df988dfe40df
Ville Syrjälä <ville.syrjala@linux.intel.com>
2013-10-04 14:53:34 +0300

    drm: Make vblank_inmodeset unsigned
    vblank_inmodeset is a bitmask, with only two bits mind you, but better
    make it unsigned anyway.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    (cherry picked from commit 54edf9aec79779f85d49674580b7ccab4d6f3c4a)
    Signed-off-by: Darren Hart <dvhart@linux.intel.com>

--- include/drm/drmP.h
+++ include/drm/drmP.h
@@ -1167,7 +1167,7 @@ struct drm_device {
 					/* for wraparound handling */
 	bool *vblank_enabled;           /* so we don't call enable more than
 					   once per disable */
-	int *vblank_inmodeset;          /* Display driver is setting mode */
+	unsigned int *vblank_inmodeset; /* Display driver is setting mode */
 	u32 *last_vblank_wait;		/* Last vblank seqno waited per CRTC */
 	struct timer_list vblank_disable_timer;
 
