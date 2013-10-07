8eaede49dfdc1ff1d727f9c913665b8009945191
Ben Hutchings <ben@decadent.org.uk>
2013-10-07 01:05:46 +0100

    sysrq: Allow magic SysRq key functions to be disabled through Kconfig
    Turn the initial value of sysctl kernel.sysrq (SYSRQ_DEFAULT_ENABLE)
    into a Kconfig variable.
    
    Original version by Bastian Blank <waldi@debian.org>.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -190,7 +190,7 @@ static int proc_dostring_coredump(struct ctl_table *table, int write,
 
 #ifdef CONFIG_MAGIC_SYSRQ
 /* Note: sysrq code uses it's own private copy */
-static int __sysrq_enabled = SYSRQ_DEFAULT_ENABLE;
+static int __sysrq_enabled = CONFIG_MAGIC_SYSRQ_DEFAULT_ENABLE;
 
 static int sysrq_sysctl_handler(ctl_table *table, int write,
 				void __user *buffer, size_t *lenp,
