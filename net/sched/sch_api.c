2c8c8e6f9d534a77f97df4d7148561cb902c0f83
Eric Dumazet <edumazet@google.com>
2013-10-07 08:32:32 -0700

    net_sched: increment drop counters in qdisc_tree_decrease_qlen()
    qdisc_tree_decrease_qlen() is called when some packets are dropped
    on a qdisc, and we want to notify parents of qlen changes.
    
    We also can increment parents qdisc qstats drop counters.
    
    This permits more accurate drop counters up to root qdisc.
    
    For example a graft operation typically resets a qdisc
    (drops all packets) and call qdisc_tree_decrease_qlen()
    
    Note that callers are responsible for their drop counters.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/sch_api.c
+++ net/sched/sch_api.c
@@ -737,9 +737,11 @@ void qdisc_tree_decrease_qlen(struct Qdisc *sch, unsigned int n)
 	const struct Qdisc_class_ops *cops;
 	unsigned long cl;
 	u32 parentid;
+	int drops;
 
 	if (n == 0)
 		return;
+	drops = max_t(int, n, 0);
 	while ((parentid = sch->parent)) {
 		if (TC_H_MAJ(parentid) == TC_H_MAJ(TC_H_INGRESS))
 			return;
@@ -756,6 +758,7 @@ void qdisc_tree_decrease_qlen(struct Qdisc *sch, unsigned int n)
 			cops->put(sch, cl);
 		}
 		sch->q.qlen -= n;
+		sch->qstats.drops += drops;
 	}
 }
 EXPORT_SYMBOL(qdisc_tree_decrease_qlen);
