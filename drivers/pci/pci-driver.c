84822b158fd3fc7d9f9c67f1b26a1e007880e13c
Liu Chuansheng <chuansheng.liu@intel.com>
2013-10-07 15:29:27 -0600

    PCI/PM: Remove pci_pm_complete()
    88d26136 ("PM: Prevent runtime suspend during system resume") removed the
    pm_runtime_put_sync() from pci_pm_complete() to PM core code
    device_complete().
    
    Here the pci_pm_complete() is doing the same work which can be done in
    device_complete(), so we can remove it directly.
    
    Signed-off-by: Liu Chuansheng <chuansheng.liu@intel.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/pci/pci-driver.c
+++ drivers/pci/pci-driver.c
@@ -599,18 +599,10 @@ static int pci_pm_prepare(struct device *dev)
 	return error;
 }
 
-static void pci_pm_complete(struct device *dev)
-{
-	struct device_driver *drv = dev->driver;
-
-	if (drv && drv->pm && drv->pm->complete)
-		drv->pm->complete(dev);
-}
 
 #else /* !CONFIG_PM_SLEEP */
 
 #define pci_pm_prepare	NULL
-#define pci_pm_complete	NULL
 
 #endif /* !CONFIG_PM_SLEEP */
 
@@ -1123,7 +1115,6 @@ static int pci_pm_runtime_idle(struct device *dev)
 
 static const struct dev_pm_ops pci_dev_pm_ops = {
 	.prepare = pci_pm_prepare,
-	.complete = pci_pm_complete,
 	.suspend = pci_pm_suspend,
 	.resume = pci_pm_resume,
 	.freeze = pci_pm_freeze,
