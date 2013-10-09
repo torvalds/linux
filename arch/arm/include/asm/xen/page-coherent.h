d6fe76c58c358498b91d21f0ca8054f6aa6e672d
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2013-10-09 17:18:14 +0000

    xen: introduce xen_alloc/free_coherent_pages
    xen_swiotlb_alloc_coherent needs to allocate a coherent buffer for cpu
    and devices. On native x86 is sufficient to call __get_free_pages in
    order to get a coherent buffer, while on ARM (and potentially ARM64) we
    need to call the native dma_ops->alloc implementation.
    
    Introduce xen_alloc_coherent_pages to abstract the arch specific buffer
    allocation.
    
    Similarly introduce xen_free_coherent_pages to free a coherent buffer:
    on x86 is simply a call to free_pages while on ARM and ARM64 is
    arm_dma_ops.free.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    
    
    Changes in v7:
    - rename __get_dma_ops to __generic_dma_ops;
    - call __generic_dma_ops(hwdev)->alloc/free on arm64 too.
    
    Changes in v6:
    - call __get_dma_ops to get the native dma_ops pointer on arm.

---
+++ arch/arm/include/asm/xen/page-coherent.h
@@ -0,0 +1,22 @@
+#ifndef _ASM_ARM_XEN_PAGE_COHERENT_H
+#define _ASM_ARM_XEN_PAGE_COHERENT_H
+
+#include <asm/page.h>
+#include <linux/dma-attrs.h>
+#include <linux/dma-mapping.h>
+
+static inline void *xen_alloc_coherent_pages(struct device *hwdev, size_t size,
+		dma_addr_t *dma_handle, gfp_t flags,
+		struct dma_attrs *attrs)
+{
+	return __generic_dma_ops(hwdev)->alloc(hwdev, size, dma_handle, flags, attrs);
+}
+
+static inline void xen_free_coherent_pages(struct device *hwdev, size_t size,
+		void *cpu_addr, dma_addr_t dma_handle,
+		struct dma_attrs *attrs)
+{
+	__generic_dma_ops(hwdev)->free(hwdev, size, cpu_addr, dma_handle, attrs);
+}
+
+#endif /* _ASM_ARM_XEN_PAGE_COHERENT_H */
---
+++ arch/x86/include/asm/xen/page-coherent.h
@@ -0,0 +1,24 @@
+#ifndef _ASM_X86_XEN_PAGE_COHERENT_H
+#define _ASM_X86_XEN_PAGE_COHERENT_H
+
+#include <asm/page.h>
+#include <linux/dma-attrs.h>
+#include <linux/dma-mapping.h>
+
+static inline void *xen_alloc_coherent_pages(struct device *hwdev, size_t size,
+		dma_addr_t *dma_handle, gfp_t flags,
+		struct dma_attrs *attrs)
+{
+	void *vstart = (void*)__get_free_pages(flags, get_order(size));
+	*dma_handle = virt_to_phys(vstart);
+	return vstart;
+}
+
+static inline void xen_free_coherent_pages(struct device *hwdev, size_t size,
+		void *cpu_addr, dma_addr_t dma_handle,
+		struct dma_attrs *attrs)
+{
+	free_pages((unsigned long) cpu_addr, get_order(size));
+}
+
+#endif /* _ASM_X86_XEN_PAGE_COHERENT_H */
