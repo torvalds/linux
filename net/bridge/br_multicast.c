9e393b6d30a4f56850fd506c9802da8fc633ac11
Vlad Yasevich <vyasevic@redhat.com>
2013-10-10 15:57:59 -0400

    bridge: update mdb expiration timer upon reports.
    [ Upstream commit f144febd93d5ee534fdf23505ab091b2b9088edc ]
    
    commit 9f00b2e7cf241fa389733d41b615efdaa2cb0f5b
    	bridge: only expire the mdb entry when query is received
    changed the mdb expiration timer to be armed only when QUERY is
    received.  Howerver, this causes issues in an environment where
    the multicast server socket comes and goes very fast while a client
    is trying to send traffic to it.
    
    The root cause is a race where a sequence of LEAVE followed by REPORT
    messages can race against QUERY messages generated in response to LEAVE.
    The QUERY ends up starting the expiration timer, and that timer can
    potentially expire after the new REPORT message has been received signaling
    the new join operation.  This leads to a significant drop in multicast
    traffic and possible complete stall.
    
    The solution is to have REPORT messages update the expiration timer
    on entries that already exist.
    
    Signed-off-by: Vlad Yasevich <vyasevic@redhat.com>
    CC: Cong Wang <xiyou.wangcong@gmail.com>
    CC: Herbert Xu <herbert@gondor.apana.org.au>
    CC: Stephen Hemminger <stephen@networkplumber.org>
    Acked-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/bridge/br_multicast.c
+++ net/bridge/br_multicast.c
@@ -610,6 +610,9 @@ rehash:
 		break;
 
 	default:
+		/* If we have an existing entry, update it's expire timer */
+		mod_timer(&mp->timer,
+			  jiffies + br->multicast_membership_interval);
 		goto out;
 	}
 
@@ -679,8 +682,12 @@ static int br_multicast_add_group(struct net_bridge *br,
 	for (pp = &mp->ports;
 	     (p = mlock_dereference(*pp, br)) != NULL;
 	     pp = &p->next) {
-		if (p->port == port)
+		if (p->port == port) {
+			/* We already have a portgroup, update the timer.  */
+			mod_timer(&p->timer,
+				  jiffies + br->multicast_membership_interval);
 			goto out;
+		}
 		if ((unsigned long)p->port < (unsigned long)port)
 			break;
 	}
