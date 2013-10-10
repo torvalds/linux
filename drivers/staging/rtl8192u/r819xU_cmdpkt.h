acd537427c448422a9efdabe28d881d7df00bff1
Xenia Ragiadakou <burzalodowa@gmail.com>
2013-10-10 10:43:52 +0300

    staging: rtl8192u: fix space after ( in r819xU_cmdpkt.h
    This patch removes prohibited space after ( in r819xU_cmdpkt.h according to
    the linux kernel coding style convention.
    
    Signed-off-by: Xenia Ragiadakou <burzalodowa@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r819xU_cmdpkt.h
+++ drivers/staging/rtl8192u/r819xU_cmdpkt.h
@@ -185,7 +185,7 @@ typedef enum _rt_status{
 
 extern  u32 cmpk_message_handle_rx(struct net_device *dev,
 		struct ieee80211_rx_stats *pstats);
-extern rt_status SendTxCommandPacket( struct net_device *dev,
+extern rt_status SendTxCommandPacket(struct net_device *dev,
 		void *pData, u32 DataLen);
 
 
