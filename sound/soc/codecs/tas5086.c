285d00c11b0a8d0ef63c176f88caab5071c9e80d
Sachin Kamat <sachin.kamat@linaro.org>
2013-10-11 17:23:59 +0530

    ASoC: tas5086: Include linux/of.h header
    'of_match_ptr' is defined in linux/of.h. Include it explicitly.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/codecs/tas5086.c
+++ sound/soc/codecs/tas5086.c
@@ -37,6 +37,7 @@
 #include <linux/i2c.h>
 #include <linux/regmap.h>
 #include <linux/spi/spi.h>
+#include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/of_gpio.h>
 #include <sound/pcm.h>
