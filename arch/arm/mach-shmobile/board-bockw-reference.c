efdcab70a193b1a784ca194e2b472974b42349bf
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2013-10-10 23:35:34 -0700

    ARM: shmobile: bockw: add pin pull-up setting for SDHI
    SDHI CD/WP pin needs pull-up
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    (cherry picked from commit 79990c164dcc7514398ca824a609c74cb5f563da)
    (Queued by Simon Horman for v3.14 but not yet in Linus's tree)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/board-bockw-reference.c
+++ arch/arm/mach-shmobile/board-bockw-reference.c
@@ -30,9 +30,13 @@
 #define FPGA	0x18200000
 #define IRQ0MR	0x30
 #define COMCTLR	0x101c
+
+#define PFC	0xfffc0000
+#define PUPR4	0x110
 static void __init bockw_init(void)
 {
 	void __iomem *fpga;
+	void __iomem *pfc;
 
 	r8a7778_clock_init();
 	r8a7778_init_irq_extpin_dt(1);
@@ -52,6 +56,17 @@ static void __init bockw_init(void)
 		iowrite16(val, fpga + IRQ0MR);
 
 		iounmap(fpga);
+	}
+
+	pfc = ioremap_nocache(PFC, 0x200);
+	if (pfc) {
+		/*
+		 * FIXME
+		 *
+		 * SDHI CD/WP pin needs pull-up
+		 */
+		iowrite32(ioread32(pfc + PUPR4) | (3 << 26), pfc + PUPR4);
+		iounmap(pfc);
 	}
 
 	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
