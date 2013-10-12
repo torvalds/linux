8b414521bc5375ae8ba18c083af95d44b8da0d04
Marcelo Tosatti <mtosatti@redhat.com>
2013-10-11 21:39:26 -0300

    hung_task: add method to reset detector
    In certain occasions it is possible for a hung task detector
    positive to be false: continuation from a paused VM, for example.
    
    Add a method to reset detection, similar as is done
    with other kernel watchdogs.
    
    Acked-by: Don Zickus <dzickus@redhat.com>
    Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>
    Signed-off-by: Gleb Natapov <gleb@redhat.com>

--- arch/x86/kernel/pvclock.c
+++ arch/x86/kernel/pvclock.c
@@ -48,6 +48,7 @@ void pvclock_touch_watchdogs(void)
 	touch_softlockup_watchdog_sync();
 	clocksource_touch_watchdog();
 	rcu_cpu_stall_reset();
+	reset_hung_task_detector();
 }
 
 static atomic64_t last_value = ATOMIC64_INIT(0);
