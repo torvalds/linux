45cf70c0dc4ae63756ba5107060e51f3674c90fd
Bo Shen <voice.shen@atmel.com>
2013-10-14 13:38:29 +0800

    ARM: at91: add at91sam9n12 ssc clock in look up table
    Add at91sam9n12 ssc clock in look up table
    
    Signed-off-by: Bo Shen <voice.shen@atmel.com>
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/at91sam9n12.c
+++ arch/arm/mach-at91/at91sam9n12.c
@@ -169,6 +169,7 @@ static struct clk_lookup periph_clocks_lookups[] = {
 	CLKDEV_CON_DEV_ID("t0_clk", "f8008000.timer", &tcb_clk),
 	CLKDEV_CON_DEV_ID("t0_clk", "f800c000.timer", &tcb_clk),
 	CLKDEV_CON_DEV_ID("mci_clk", "f0008000.mmc", &mmc_clk),
+	CLKDEV_CON_DEV_ID(NULL, "f0010000.ssc", &ssc_clk),
 	CLKDEV_CON_DEV_ID("dma_clk", "ffffec00.dma-controller", &dma_clk),
 	CLKDEV_CON_DEV_ID(NULL, "f8010000.i2c", &twi0_clk),
 	CLKDEV_CON_DEV_ID(NULL, "f8014000.i2c", &twi1_clk),
