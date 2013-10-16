adfedd9a32e4e3490c0060576fd824881572b72a
Vinod Koul <vinod.koul@intel.com>
2013-10-16 13:29:02 +0530

    dmaengine: use DMA_COMPLETE for dma completion status
    the DMA_SUCCESS is a misnomer as dmaengine indicates the transfer is complete and
    gives no guarantee of the transfer success. Hence we should use DMA_COMPLTE
    instead of DMA_SUCCESS
    
    Acked-by: Dan Williams <dan.j.williams@intel.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/dmaengine.c
+++ drivers/dma/dmaengine.c
@@ -1062,7 +1062,7 @@ dma_wait_for_async_tx(struct dma_async_tx_descriptor *tx)
 	unsigned long dma_sync_wait_timeout = jiffies + msecs_to_jiffies(5000);
 
 	if (!tx)
-		return DMA_SUCCESS;
+		return DMA_COMPLETE;
 
 	while (tx->cookie == -EBUSY) {
 		if (time_after_eq(jiffies, dma_sync_wait_timeout)) {
