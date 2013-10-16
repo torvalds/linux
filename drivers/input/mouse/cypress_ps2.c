c2e609599a57926f20b324d05b164aabe9e372ed
Geyslan G. Bem <geyslan@gmail.com>
2013-10-16 16:52:14 -0700

    Input: cypress_ps2 - remove useless cast
    Get rid of unnecessary (void *) casting in 'cypress_init' function.
    
    Signed-off-by: Geyslan G. Bem <geyslan@gmail.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/mouse/cypress_ps2.c
+++ drivers/input/mouse/cypress_ps2.c
@@ -680,13 +680,13 @@ int cypress_init(struct psmouse *psmouse)
 	struct cytp_data *cytp;
 
 	cytp = kzalloc(sizeof(struct cytp_data), GFP_KERNEL);
-	psmouse->private = (void *)cytp;
-	if (cytp == NULL)
+	if (!cytp)
 		return -ENOMEM;
 
-	cypress_reset(psmouse);
-
+	psmouse->private = cytp;
 	psmouse->pktsize = 8;
+
+	cypress_reset(psmouse);
 
 	if (cypress_query_hardware(psmouse)) {
 		psmouse_err(psmouse, "Unable to query Trackpad hardware.\n");
