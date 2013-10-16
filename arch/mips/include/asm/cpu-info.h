d6d3c9afaab47418ab2d7f874fb8aeac1f067104
Ralf Baechle <ralf@linux-mips.org>
2013-10-16 17:10:07 +0200

    MIPS: MT: proc: Add support for printing VPE and TC ids
    And there are more CPUs or configurations that want to provide special
    per-CPU information in /proc/cpuinfo.  So I think there needs to be a
    hook mechanism, such as a notifier.
    
    This is a first cut only; I need to think about what sort of looking
    the notifier needs to have.  But I'd appreciate testing on MT hardware!
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Cc: Markos Chandras <markos.chandras@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/6066/

--- arch/mips/include/asm/cpu-info.h
+++ arch/mips/include/asm/cpu-info.h
@@ -96,6 +96,27 @@ extern void cpu_report(void);
 extern const char *__cpu_name[];
 #define cpu_name_string()	__cpu_name[smp_processor_id()]
 
+struct seq_file;
+struct notifier_block;
+
+extern int register_proc_cpuinfo_notifier(struct notifier_block *nb);
+extern int proc_cpuinfo_notifier_call_chain(unsigned long val, void *v);
+
+#define proc_cpuinfo_notifier(fn, pri)					\
+({									\
+	static struct notifier_block fn##_nb = {			\
+		.notifier_call = fn,					\
+		.priority = pri						\
+	};								\
+									\
+	register_proc_cpuinfo_notifier(&fn##_nb);			\
+})
+
+struct proc_cpuinfo_notifier_args {
+	struct seq_file *m;
+	unsigned long n;
+};
+
 #if defined(CONFIG_MIPS_MT_SMP) || defined(CONFIG_MIPS_MT_SMTC)
 # define cpu_vpe_id(cpuinfo)	((cpuinfo)->vpe_id)
 #else
