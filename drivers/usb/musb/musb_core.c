8d1aad7485e653c2c5fd985b326096f680f7d918
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2013-10-16 12:50:07 +0200

    usb: musb: core: Call dma_controller_destroy() in error path only once.
    In commit f3ce4d5 ("usb: musb: core: call dma_controller_destroy() in the err path")
    I erroneously assumed that the dma controller is not removed in the
    error patch. This was wrong because it happens later via musb_free().
    That means the original commit can be reverted because it is wrong or we
    do this, so it is more obvious.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -1764,8 +1764,6 @@ static void musb_free(struct musb *musb)
 		free_irq(musb->nIrq, musb);
 	}
 	cancel_work_sync(&musb->irq_work);
-	if (musb->dma_controller)
-		dma_controller_destroy(musb->dma_controller);
 
 	musb_host_free(musb);
 }
@@ -1989,6 +1987,9 @@ static int musb_remove(struct platform_device *pdev)
 	 */
 	musb_exit_debugfs(musb);
 	musb_shutdown(pdev);
+
+	if (musb->dma_controller)
+		dma_controller_destroy(musb->dma_controller);
 
 	musb_free(musb);
 	device_init_wakeup(dev, 0);
