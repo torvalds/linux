58c8b26909f287c72a7a39d26f800ffe61e01bf3
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2013-10-18 16:01:32 +0000

    arm64/xen: get_dma_ops: return xen_dma_ops if we are running as xen_initial_domain
    We can't simply override arm_dma_ops with xen_dma_ops because devices
    are allowed to have their own dma_ops and they take precedence over
    arm_dma_ops. When running on Xen as initial domain, we always want
    xen_dma_ops to be the one in use.
    
    We introduce __generic_dma_ops to allow xen_dma_ops functions to call
    back to the native implementation.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    CC: will.deacon@arm.com
    
    Changes in v7:
    - return xen_dma_ops only if we are the initial domain;
    - rename __get_dma_ops to __generic_dma_ops.

--- arch/arm64/include/asm/dma-mapping.h
+++ arch/arm64/include/asm/dma-mapping.h
@@ -23,17 +23,28 @@
 
 #include <asm-generic/dma-coherent.h>
 
+#include <xen/xen.h>
+#include <asm/xen/hypervisor.h>
+
 #define ARCH_HAS_DMA_GET_REQUIRED_MASK
 
 #define DMA_ERROR_CODE	(~(dma_addr_t)0)
 extern struct dma_map_ops *dma_ops;
 
-static inline struct dma_map_ops *get_dma_ops(struct device *dev)
+static inline struct dma_map_ops *__generic_dma_ops(struct device *dev)
 {
 	if (unlikely(!dev) || !dev->archdata.dma_ops)
 		return dma_ops;
 	else
 		return dev->archdata.dma_ops;
+}
+
+static inline struct dma_map_ops *get_dma_ops(struct device *dev)
+{
+	if (xen_initial_domain())
+		return xen_dma_ops;
+	else
+		return __generic_dma_ops(dev);
 }
 
 #include <asm-generic/dma-mapping-common.h>
