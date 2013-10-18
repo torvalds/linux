1bda19eb73d68b304148e67253e47cef049a419d
Josef Bacik <jbacik@fusionio.com>
2013-10-18 12:10:36 -0400

    Btrfs: release path before starting transaction in can_nocow_extent
    We can't be holding tree locks while we try to start a transaction, we will
    deadlock.  Thanks,
    
    Reported-by: Sage Weil <sage@inktank.com>
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>
    Signed-off-by: Chris Mason <chris.mason@fusionio.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -6437,6 +6437,7 @@ noinline int can_nocow_extent(struct inode *inode, u64 offset, u64 *len,
 
 	if (btrfs_extent_readonly(root, disk_bytenr))
 		goto out;
+	btrfs_release_path(path);
 
 	/*
 	 * look for other files referencing this extent, if we
