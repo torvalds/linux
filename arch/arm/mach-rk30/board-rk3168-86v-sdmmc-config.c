ae6918710275a6cb17b65a61b3d87469850466b1
gwl <gwl@rock-chips.com>
2013-10-19 16:57:56 +0800

    Consummate bcm wifi oob on all platform(Wake gpio define).

--- arch/arm/mach-rk30/board-rk3168-86v-sdmmc-config.c
+++ arch/arm/mach-rk30/board-rk3168-86v-sdmmc-config.c
@@ -60,6 +60,10 @@ int rk31sdk_get_sdmmc0_pin_io_voltage(void)
 #define RK29SDK_SD_CARD_DETECT_N                RK30_PIN3_PB0  //According to your own project to set the value of card-detect-pin.
 #define RK29SDK_SD_CARD_INSERT_LEVEL            GPIO_LOW       // set the voltage of insert-card. Please pay attention to the default setting.
 
+//wake up host gpio
+#define RK30SDK_WIFI_GPIO_WIFI_INT_B                RK30_PIN3_PD2
+#define RK30SDK_WIFI_GPIO_WIFI_INT_B_ENABLE_VALUE   GPIO_HIGH
+
 /*
 * Define wifi module's power and reset gpio, and gpio sensitive level.
 * Please set the value according to your own project.
