f233ca04fa09cf510e60c5ced95595355a961d25
Dmitry Shmidt <dimitrysh@google.com>
2013-10-21 19:01:31 -0700

    net: wireless: bcmdhd: Fix country checking
    Bug: 11319610
    
    Change-Id: I5c52ed594614d8039f167de3c98bd9f802b521df
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/net/wireless/bcmdhd/wldev_common.c
+++ drivers/net/wireless/bcmdhd/wldev_common.c
@@ -354,7 +354,7 @@ int wldev_set_country(
 	}
 
 	if ((error < 0) ||
-	    (strncmp(country_code, cspec.ccode, WLC_CNTRY_BUF_SZ) != 0)) {
+	    (strncmp(country_code, cspec.country_abbrev, WLC_CNTRY_BUF_SZ) != 0)) {
 
 		if (user_enforced) {
 			bzero(&scbval, sizeof(scb_val_t));
