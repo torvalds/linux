b2f974d6af9accfec11e69cc76d2ab9f0c7359e0
Cody P Schafer <cody@linux.vnet.ibm.com>
2013-10-23 11:49:57 -0700

    tracing: Open tracer when ftrace_dump_on_oops is used
    With ftrace_dump_on_oops, we previously did not open the tracer in
    question, sometimes causing the trace output to be useless.
    
    For example, the function_graph tracer with tracing_thresh set dumped via
    ftrace_dump_on_oops would show a series of '}' indented at different levels,
    but no function names.
    
    call trace->open() (and do a few other fixups copied from the normal dump
    path) to make the output more intelligible.
    
    Link: http://lkml.kernel.org/r/1382554197-16961-1-git-send-email-cody@linux.vnet.ibm.com
    
    Signed-off-by: Cody P Schafer <cody@linux.vnet.ibm.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -6276,6 +6276,17 @@ void trace_init_global_iter(struct trace_iterator *iter)
 	iter->trace = iter->tr->current_trace;
 	iter->cpu_file = RING_BUFFER_ALL_CPUS;
 	iter->trace_buffer = &global_trace.trace_buffer;
+
+	if (iter->trace && iter->trace->open)
+		iter->trace->open(iter);
+
+	/* Annotate start of buffers if we had overruns */
+	if (ring_buffer_overruns(iter->trace_buffer->buffer))
+		iter->iter_flags |= TRACE_FILE_ANNOTATE;
+
+	/* Output in nanoseconds only if we are using a clock in nanoseconds. */
+	if (trace_clocks[iter->tr->clock_id].in_ns)
+		iter->iter_flags |= TRACE_FILE_TIME_IN_NS;
 }
 
 void ftrace_dump(enum ftrace_dump_mode oops_dump_mode)
