e7b519ba55aeb675daee1d304e80d752c385f7f0
Hannes Frederic Sowa <hannes@stressinduktion.org>
2013-10-23 11:06:55 +0200

    ipv4: initialize ip4_frags hash secret as late as possible
    Defer the generation of the first hash secret for the ipv4 fragmentation
    cache as late as possible.
    
    ip4_frags.rnd gets initial seeded by inet_frags_init and regulary
    reseeded by inet_frag_secret_rebuild. Either we call ipqhashfn directly
    from ip_fragment.c in which case we initialize the secret directly.
    
    If we first get called by inet_frag_secret_rebuild we install a new secret
    by a manual call to get_random_bytes. This secret will be overwritten
    as soon as the first call to ipqhashfn happens. This is safe because we
    won't race while publishing the new secrets with anyone else.
    
    Cc: Eric Dumazet <edumazet@google.com>
    Cc: "David S. Miller" <davem@davemloft.net>
    Signed-off-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/ip_fragment.c
+++ net/ipv4/ip_fragment.c
@@ -106,6 +106,7 @@ struct ip4_create_arg {
 
 static unsigned int ipqhashfn(__be16 id, __be32 saddr, __be32 daddr, u8 prot)
 {
+	net_get_random_once(&ip4_frags.rnd, sizeof(ip4_frags.rnd));
 	return jhash_3words((__force u32)id << 16 | prot,
 			    (__force u32)saddr, (__force u32)daddr,
 			    ip4_frags.rnd) & (INETFRAGS_HASHSZ - 1);
