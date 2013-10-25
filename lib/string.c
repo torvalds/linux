927a23b199cd6147c53bf7cbd92b12de2b7531c7
Miao Xie <miaox@cn.fujitsu.com>
2013-10-25 06:02:21 +0000

    lib: improve the performance of memcpy and memmove of the general version the performance of memcpy and memmove of the general version is very inefficient, this patch improved them.
    Signed-off-by: Miao Xie <miaox@cn.fujitsu.com>

--- lib/string.c
+++ lib/string.c
@@ -26,6 +26,7 @@
 #include <linux/export.h>
 #include <linux/bug.h>
 #include <linux/errno.h>
+#include <linux/memcopy.h>
 
 #ifndef __HAVE_ARCH_STRNICMP
 /**
@@ -598,11 +599,12 @@ EXPORT_SYMBOL(memset);
  */
 void *memcpy(void *dest, const void *src, size_t count)
 {
-	char *tmp = dest;
-	const char *s = src;
+	unsigned long dstp = (unsigned long)dest;
+	unsigned long srcp = (unsigned long)src;
 
-	while (count--)
-		*tmp++ = *s++;
+	/* Copy from the beginning to the end */
+	mem_copy_fwd(dstp, srcp, count);
+
 	return dest;
 }
 EXPORT_SYMBOL(memcpy);
@@ -619,21 +621,15 @@ EXPORT_SYMBOL(memcpy);
  */
 void *memmove(void *dest, const void *src, size_t count)
 {
-	char *tmp;
-	const char *s;
+	unsigned long dstp = (unsigned long)dest;
+	unsigned long srcp = (unsigned long)src;
 
-	if (dest <= src) {
-		tmp = dest;
-		s = src;
-		while (count--)
-			*tmp++ = *s++;
+	if (dest - src >= count) {
+		/* Copy from the beginning to the end */
+		mem_copy_fwd(dstp, srcp, count);
 	} else {
-		tmp = dest;
-		tmp += count;
-		s = src;
-		s += count;
-		while (count--)
-			*--tmp = *--s;
+		/* Copy from the end to the beginning */
+		mem_copy_bwd(dstp, srcp, count);
 	}
 	return dest;
 }
