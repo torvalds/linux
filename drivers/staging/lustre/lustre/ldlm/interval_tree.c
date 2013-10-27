16fcfa17d52f9f9e4e2f02a5b078b078eb5eabf3
Rashika Kheria <rashika.kheria@gmail.com>
2013-10-27 18:49:31 +0530

    Staging: lustre: Fix no use of assignment in if condition
    This patch fixes the following checkpatch.pl error in lustre/ldlm/interval_tree.c-
    ERROR: do not use assignment in if condition
    
    Signed-off-by: Rashika Kheria <rashika.kheria@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ldlm/interval_tree.c
+++ drivers/staging/lustre/lustre/ldlm/interval_tree.c
@@ -427,7 +427,8 @@ static void interval_erase_color(struct interval_node *node,
 			} else {
 				if (node_is_black_or_0(tmp->in_right)) {
 					struct interval_node *o_left;
-					if ((o_left = tmp->in_left))
+					o_left = tmp->in_left;
+					if (o_left)
 						o_left->in_color = INTERVAL_BLACK;
 					tmp->in_color = INTERVAL_RED;
 					__rotate_right(tmp, root);
@@ -457,7 +458,8 @@ static void interval_erase_color(struct interval_node *node,
 			} else {
 				if (node_is_black_or_0(tmp->in_left)) {
 					struct interval_node *o_right;
-					if ((o_right = tmp->in_right))
+					o_right = tmp->in_right;
+					if (o_right)
 						o_right->in_color = INTERVAL_BLACK;
 					tmp->in_color = INTERVAL_RED;
 					__rotate_left(tmp, root);
