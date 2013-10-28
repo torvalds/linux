53c18c990644174935ca93e50d7ebd05e04cb9ee
Heinz Graalfs <graalfs@linux.vnet.ibm.com>
2013-10-29 09:40:11 +1030

    virtio_test: verify if virtqueue_kick() succeeded
    Verify if a host kick succeeded by checking return value of virtqueue_kick().
    
    Signed-off-by: Heinz Graalfs <graalfs@linux.vnet.ibm.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tools/virtio/virtio_test.c
+++ tools/virtio/virtio_test.c
@@ -172,7 +172,8 @@ static void run_test(struct vdev_info *dev, struct vq_info *vq,
 							 GFP_ATOMIC);
 				if (likely(r == 0)) {
 					++started;
-					virtqueue_kick(vq->vq);
+					if (unlikely(!virtqueue_kick(vq->vq))
+						r = -1;
 				}
 			} else
 				r = -1;
