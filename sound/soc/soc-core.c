a19685cb72bb6a80ac453e76b3ab3bb7770e1742
Takashi Iwai <tiwai@suse.de>
2013-10-28 14:21:46 +0100

    ASoC: Use strlcpy() for copying in snd_soc_info_enum_double()
    The provided texts aren't guaranteed to be in the fixed size.
    Spotted by coverity CID 139318.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -2552,8 +2552,9 @@ int snd_soc_info_enum_double(struct snd_kcontrol *kcontrol,
 
 	if (uinfo->value.enumerated.item > e->max - 1)
 		uinfo->value.enumerated.item = e->max - 1;
-	strcpy(uinfo->value.enumerated.name,
-		e->texts[uinfo->value.enumerated.item]);
+	strlcpy(uinfo->value.enumerated.name,
+		e->texts[uinfo->value.enumerated.item],
+		sizeof(uinfo->value.enumerated.name));
 	return 0;
 }
 EXPORT_SYMBOL_GPL(snd_soc_info_enum_double);
