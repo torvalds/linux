02a9ae6e31975e97777a4e3db6e33e059f256c05
Atul Deshmukh <atul.deshmukh@qlogic.com>
2013-10-30 03:38:20 -0400

    [SCSI] qla2xxx: Clear RISC INT reg only for an event and not always while polling.
    Signed-off-by: Atul Deshmukh <atul.deshmukh@qlogic.com>
    Signed-off-by: Saurav Kashyap <saurav.kashyap@qlogic.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/qla2xxx/qla_nx.c
+++ drivers/scsi/qla2xxx/qla_nx.c
@@ -2237,8 +2237,8 @@ qla82xx_poll(int irq, void *dev_id)
 			    stat * 0xff);
 			break;
 		}
+		WRT_REG_DWORD(&reg->host_int, 0);
 	}
-	WRT_REG_DWORD(&reg->host_int, 0);
 out:
 	spin_unlock_irqrestore(&ha->hardware_lock, flags);
 }
