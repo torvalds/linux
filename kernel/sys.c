d67a07b6ec961b4e14356d545040769be95ce178
Colin Cross <ccross@android.com>
2013-10-30 13:17:34 -0700

    anonymous vma names: fix build with !MMU
    Disable PR_SET_VMA when building with !MMU
    
    Change-Id: I896b6979b99aa61df85caf4c3ec22eb8a8204e64
    Signed-off-by: Colin Cross <ccross@android.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -2101,7 +2101,7 @@ static int prctl_get_tid_address(struct task_struct *me, int __user **tid_addr)
 }
 #endif
 
-
+#ifdef CONFIG_MMU
 static int prctl_update_vma_anon_name(struct vm_area_struct *vma,
 		struct vm_area_struct **prev,
 		unsigned long start, unsigned long end,
@@ -2240,6 +2240,13 @@ static int prctl_set_vma(unsigned long opt, unsigned long start,
 
 	return error;
 }
+#else /* CONFIG_MMU */
+static int prctl_set_vma(unsigned long opt, unsigned long start,
+		unsigned long len_in, unsigned long arg)
+{
+	return -EINVAL;
+}
+#endif
 
 SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 		unsigned long, arg4, unsigned long, arg5)
