f692d2d8bebddeb8c8faedfc41e4732f3dc23e5b
Jakob Bornecrantz <jakob@vmware.com>
2013-10-30 02:46:56 -0700

    drm/ttm: Handle in-memory region copies
    commit 9a0599ddeae012a771bba5e23393fc52d8a59d89 upstream.
    
    Fix the case where the ttm pointer may be NULL causing
    a NULL pointer dereference.
    
    Signed-off-by: Jakob Bornecrantz <jakob@vmware.com>
    Signed-off-by: Thomas Hellström <thellstrom@vmware.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/ttm/ttm_bo_util.c
+++ drivers/gpu/drm/ttm/ttm_bo_util.c
@@ -347,7 +347,9 @@ int ttm_bo_move_memcpy(struct ttm_buffer_object *bo,
 	if (old_iomap == NULL && ttm == NULL)
 		goto out2;
 
-	if (ttm->state == tt_unpopulated) {
+	/* TTM might be null for moves within the same region.
+	 */
+	if (ttm && ttm->state == tt_unpopulated) {
 		ret = ttm->bdev->driver->ttm_tt_populate(ttm);
 		if (ret) {
 			/* if we fail here don't nuke the mm node
