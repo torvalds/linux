ca0e8bd68bae3d0bad758b5a82dbf9327b75325c
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-10-30 13:31:30 +0800

    netfilter: nf_tables: remove duplicated include from nf_tables_ipv4.c
    Remove duplicated include.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/ipv4/netfilter/nf_tables_ipv4.c
+++ net/ipv4/netfilter/nf_tables_ipv4.c
@@ -16,7 +16,6 @@
 #include <net/netfilter/nf_tables.h>
 #include <net/net_namespace.h>
 #include <net/ip.h>
-#include <net/net_namespace.h>
 #include <net/netfilter/nf_tables_ipv4.h>
 
 static unsigned int nft_ipv4_output(const struct nf_hook_ops *ops,
