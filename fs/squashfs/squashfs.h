4ff4dc4d1508e64cb642dc1d1380085af47d1d6e
Phillip Lougher <phillip@squashfs.org.uk>
2013-10-31 19:24:27 +0000

    Squashfs: Restructure squashfs_readpage()
    Restructure squashfs_readpage() splitting it into separate
    functions for datablocks, fragments and sparse blocks.
    
    Move the memcpying (from squashfs cache entry) implementation of
    squashfs_readpage_block into file_cache.c
    
    This allows different implementations to be supported.
    
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>
    Reviewed-by: Minchan Kim <minchan@kernel.org>

--- fs/squashfs/squashfs.h
+++ fs/squashfs/squashfs.h
@@ -66,6 +66,13 @@ extern int squashfs_frag_lookup(struct super_block *, unsigned int, u64 *);
 extern __le64 *squashfs_read_fragment_index_table(struct super_block *,
 				u64, u64, unsigned int);
 
+/* file.c */
+void squashfs_copy_cache(struct page *, struct squashfs_cache_entry *, int,
+				int);
+
+/* file_xxx.c */
+extern int squashfs_readpage_block(struct page *, u64, int);
+
 /* id.c */
 extern int squashfs_get_id(struct super_block *, unsigned int, unsigned int *);
 extern __le64 *squashfs_read_id_index_table(struct super_block *, u64, u64,
