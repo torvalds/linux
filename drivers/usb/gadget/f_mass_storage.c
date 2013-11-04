655403c21236af7b121c8c44618e60ff747acc87
Pratyush Anand <pratyush.anand@st.com>
2013-11-04 22:13:54 +0530

    usb: gadget: mass storage: fix return of delayed status
    Mass storage gadget returns DELAYED_STATUS in stead of
    USB_GADGET_DELAYED_STATUS while handling bulk reset request. Since
    peripheral driver uses USB_GADGET_DELAYED_STATUS for delayed status
    handling, therefore replace DELAYED_STATUS by USB_GADGET_DELAYED_STATUS
    in mass storage driver.
    
    Since, DELAYED_STATUS and hence EP0_BUFSIZE will no longer be used now,
    so remove them.
    
    Signed-off-by: Pratyush Anand <pratyush.anand@st.com>
    Cc: Paul Zimmerman <Paul.Zimmerman@synopsys.com>
    Cc: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/f_mass_storage.c
+++ drivers/usb/gadget/f_mass_storage.c
@@ -523,7 +523,7 @@ static int fsg_setup(struct usb_function *f,
 		 */
 		DBG(fsg, "bulk reset request\n");
 		raise_exception(fsg->common, FSG_STATE_RESET);
-		return DELAYED_STATUS;
+		return USB_GADGET_DELAYED_STATUS;
 
 	case US_BULK_GET_MAX_LUN:
 		if (ctrl->bRequestType !=
