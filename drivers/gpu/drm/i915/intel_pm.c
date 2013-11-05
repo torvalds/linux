f64a28a7c5ab2fc342326de9e126acf3cc0f91d6
Jesse Barnes <jbarnes@virtuousgeek.org>
2013-11-04 16:07:00 -0800

    drm/i915/vlv: fixup DDR freq detection per Punit spec
    Either the docs were wrong or the values have changed since the old days
    before we had wheels.
    
    Reported-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -5319,14 +5319,21 @@ static void valleyview_init_clock_gating(struct drm_device *dev)
 	mutex_unlock(&dev_priv->rps.hw_lock);
 	switch ((val >> 6) & 3) {
 	case 0:
-	case 1:
 		dev_priv->mem_freq = 800;
 		break;
-	case 2:
+	case 1:
 		dev_priv->mem_freq = 1066;
 		break;
-	case 3:
+	case 2:
 		dev_priv->mem_freq = 1333;
+		break;
+	case 3:
+		/*
+		 * Probably a BIOS/Punit bug, or a new platform we don't
+		 * support yet.
+		 */
+		WARN(1, "invalid DDR freq detected, assuming 800MHz\n");
+		dev_priv->mem_freq = 800;
 		break;
 	}
 	DRM_DEBUG_DRIVER("DDR speed: %d MHz", dev_priv->mem_freq);
