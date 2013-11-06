5b74ff92d35e0b634d9e3ca0e698cbb568e68901
Himangi Saraogi <himangi774@gmail.com>
2013-11-06 11:10:24 +0530

    staging:rtl8187se: Use preferred library linux/uaccess.h over asm/uaccess.h
    This patch fixes the checkpatch.pl warning "Use #include
    <linux/uaccess.h> instead of <asm/uaccess.h>" in
    ieee80211/ieee80211_softmac.c .
    
    Signed-off-by: Himangi Saraogi <himangi774@gmail.com>
    Reviewed-by: Lisa Nguyen <lisa@xenapiadmin.com>
    Reviewed-by: Peter P Waskiewicz Jr <peter.p.waskiewicz.jr@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8187se/ieee80211/ieee80211_softmac.c
+++ drivers/staging/rtl8187se/ieee80211/ieee80211_softmac.c
@@ -20,7 +20,7 @@
 #include <linux/delay.h>
 #include <linux/slab.h>
 #include <linux/interrupt.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <linux/etherdevice.h>
 
 #include "dot11d.h"
