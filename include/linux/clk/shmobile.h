6b54db7c3d38df37f37dc32f45a603992edaed0f
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2013-11-06 13:14:19 +0100

    clk: shmobile: Add R-Car Gen2 clocks support
    The R-Car Gen2 SoCs (R8A7790 and R8A7791) have several clocks that are
    too custom to be supported in a generic driver. Those clocks can be
    divided in two categories:
    
    - Fixed rate clocks with multiplier and divisor set according to boot
      mode configuration
    
    - Custom divider clocks with SoC-specific divider values
    
    This driver supports both.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Acked-by: Kumar Gala <galak@codeaurora.org>
    Signed-off-by: Mike Turquette <mturquette@linaro.org>
    (cherry picked from commit 10cdfe9f327ab8d120cf6957e58c6203e3a53847)
    (Queued by Mike Turquette for v3.14 but not yet in Linus's tree)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    
    Conflicts:
    	drivers/clk/Makefile

---
+++ include/linux/clk/shmobile.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2013 Ideas On Board SPRL
+ *
+ * Contact: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef __LINUX_CLK_SHMOBILE_H_
+#define __LINUX_CLK_SHMOBILE_H_
+
+#include <linux/types.h>
+
+void rcar_gen2_clocks_init(u32 mode);
+
+#endif
