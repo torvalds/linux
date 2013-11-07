c91e8c503c720f120b91b1f7dab504b92c373800
Colin Cross <ccross@android.com>
2013-11-07 13:19:34 -0800

    net: move PPPoLAC and PPPoPNS headers to uapi
    Move the entire contents of the linux/if_pppolac.h and
    linux/if_pppopns.h headers to uapi, they only contain userspace
    interfaces.
    
    Change-Id: I3cfed7f2ae400b53269a1f59144aa3dbc30ae0b5
    Signed-off-by: Colin Cross <ccross@android.com>

--- include/linux/if_pppolac.h
+++ include/linux/if_pppolac.h
@@ -18,16 +18,6 @@
 #ifndef __LINUX_IF_PPPOLAC_H
 #define __LINUX_IF_PPPOLAC_H
 
-#include <linux/socket.h>
-#include <linux/types.h>
-
-struct sockaddr_pppolac {
-	sa_family_t	sa_family;	/* AF_PPPOX */
-	unsigned int	sa_protocol;	/* PX_PROTO_OLAC */
-	int		udp_socket;
-	struct __attribute__((packed)) {
-		__u16	tunnel, session;
-	} local, remote;
-} __attribute__((packed));
+#include <uapi/linux/if_pppolac.h>
 
 #endif /* __LINUX_IF_PPPOLAC_H */
