f2027c99d19fd8f88f7305ad8c04b3bfb1523621
Colin Cross <ccross@android.com>
2013-11-07 12:46:33 -0800

    input: misc: keychord: move header to uapi
    Move the entire contents of linux/keychord.h header to uapi, it only
    contains a userspace interface.
    
    Change-Id: If94f83328b19efb58c66391dce3bd8e927788d8d
    Signed-off-by: Colin Cross <ccross@android.com>

--- include/linux/keychord.h
+++ include/linux/keychord.h
@@ -18,35 +18,6 @@
 #ifndef __LINUX_KEYCHORD_H_
 #define __LINUX_KEYCHORD_H_
 
-#include <linux/input.h>
-
-#define KEYCHORD_VERSION		1
-
-/*
- * One or more input_keychord structs are written to /dev/keychord
- * at once to specify the list of keychords to monitor.
- * Reading /dev/keychord returns the id of a keychord when the
- * keychord combination is pressed.  A keychord is signalled when
- * all of the keys in the keycode list are in the pressed state.
- * The order in which the keys are pressed does not matter.
- * The keychord will not be signalled if keys not in the keycode
- * list are pressed.
- * Keychords will not be signalled on key release events.
- */
-struct input_keychord {
-	/* should be KEYCHORD_VERSION */
-	__u16 version;
-	/*
-	 * client specified ID, returned from read()
-	 * when this keychord is pressed.
-	 */
-	__u16 id;
-
-	/* number of keycodes in this keychord */
-	__u16 count;
-
-	/* variable length array of keycodes */
-	__u16 keycodes[];
-};
+#include <uapi/linux/keychord.h>
 
 #endif	/* __LINUX_KEYCHORD_H_ */
