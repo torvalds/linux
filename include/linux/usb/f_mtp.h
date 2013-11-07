577c291b2cc0d74049100d614fdcee267d6e1c43
Colin Cross <ccross@android.com>
2013-11-07 13:08:15 -0800

    usb: gadget: f_mtp: move userspace interface to uapi
    Move the most of linux/usb/f_mtp.h header to uapi.  Move the only
    remaining structure definition into f_mtp.c, the only place that
    uses it.
    
    Change-Id: I952c1a9dc15c36bf295a0eb4d74b6b1ad912ed03
    Signed-off-by: Colin Cross <ccross@android.com>

--- include/linux/usb/f_mtp.h
+++ include/linux/usb/f_mtp.h
@@ -18,58 +18,6 @@
 #ifndef __LINUX_USB_F_MTP_H
 #define __LINUX_USB_F_MTP_H
 
-#include <linux/ioctl.h>
-
-#ifdef __KERNEL__
-
-struct mtp_data_header {
-	/* length of packet, including this header */
-	uint32_t	length;
-	/* container type (2 for data packet) */
-	uint16_t	type;
-	/* MTP command code */
-	uint16_t    command;
-	/* MTP transaction ID */
-	uint32_t	transaction_id;
-};
-
-#endif /* __KERNEL__ */
-
-struct mtp_file_range {
-	/* file descriptor for file to transfer */
-	int			fd;
-	/* offset in file for start of transfer */
-	loff_t		offset;
-	/* number of bytes to transfer */
-	int64_t		length;
-	/* MTP command ID for data header,
-	 * used only for MTP_SEND_FILE_WITH_HEADER
-	 */
-	uint16_t	command;
-	/* MTP transaction ID for data header,
-	 * used only for MTP_SEND_FILE_WITH_HEADER
-	 */
-	uint32_t	transaction_id;
-};
-
-struct mtp_event {
-	/* size of the event */
-	size_t		length;
-	/* event data to send */
-	void		*data;
-};
-
-/* Sends the specified file range to the host */
-#define MTP_SEND_FILE              _IOW('M', 0, struct mtp_file_range)
-/* Receives data from the host and writes it to a file.
- * The file is created if it does not exist.
- */
-#define MTP_RECEIVE_FILE           _IOW('M', 1, struct mtp_file_range)
-/* Sends an event to the host via the interrupt endpoint */
-#define MTP_SEND_EVENT             _IOW('M', 3, struct mtp_event)
-/* Sends the specified file range to the host,
- * with a 12 byte MTP data packet header at the beginning.
- */
-#define MTP_SEND_FILE_WITH_HEADER  _IOW('M', 4, struct mtp_file_range)
+#include <uapi/linux/usb/f_mtp.h>
 
 #endif /* __LINUX_USB_F_MTP_H */
