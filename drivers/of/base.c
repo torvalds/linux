e3963fd60a83967573f6330c9db134bd581da746
Pantelis Antoniou <panto@antoniou-consulting.com>
2013-11-08 17:03:57 +0200

    of: Clear detach flag on attach
    When attaching a node always clear the detach flag. Without this change
    the sequence detach, attach fails.
    
    Signed-off-by: Pantelis Antoniou <panto@antoniou-consulting.com>
    Signed-off-by: Grant Likely <grant.likely@linaro.org>

--- drivers/of/base.c
+++ drivers/of/base.c
@@ -1692,6 +1692,7 @@ int of_attach_node(struct device_node *np)
 	np->allnext = of_allnodes;
 	np->parent->child = np;
 	of_allnodes = np;
+	of_node_clear_flag(np, OF_DETACHED);
 	raw_spin_unlock_irqrestore(&devtree_lock, flags);
 
 	of_add_proc_dt_entry(np);
