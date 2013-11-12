910bffe0861c956096c31ad7e887cc23169955ed
Joel Becker <jlbec@evilplan.org>
2013-11-12 15:06:57 -0800

    ocfs2: don't spam on -EDQUOT
    -EDQUOT is a user-visible error, not a logic problem.  Teach mlog_errno()
    to ignore it like it ignores -ENOSPC, etc.
    
    Signed-off-by: Joel Becker <jlbec@evilplan.org>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Reported-by: Marek Królikowski <admin@wset.edu.pl>
    Cc: Joel Becker <jlbec@evilplan.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/ocfs2/cluster/masklog.h
+++ fs/ocfs2/cluster/masklog.h
@@ -199,7 +199,8 @@ extern struct mlog_bits mlog_and_bits, mlog_not_bits;
 #define mlog_errno(st) do {						\
 	int _st = (st);							\
 	if (_st != -ERESTARTSYS && _st != -EINTR &&			\
-	    _st != AOP_TRUNCATED_PAGE && _st != -ENOSPC)		\
+	    _st != AOP_TRUNCATED_PAGE && _st != -ENOSPC &&		\
+	    _st != -EDQUOT)						\
 		mlog(ML_ERROR, "status = %lld\n", (long long)_st);	\
 } while (0)
 
