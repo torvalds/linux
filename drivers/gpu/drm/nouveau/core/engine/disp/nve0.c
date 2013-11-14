d2fa7d32ea7b54d910637e50df4972e3540722d4
Ben Skeggs <bskeggs@redhat.com>
2013-11-14 13:37:48 +1000

    drm/nouveau/disp: add a method to fetch info needed by drm vblank timestamping
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/engine/disp/nve0.c
+++ drivers/gpu/drm/nouveau/core/engine/disp/nve0.c
@@ -41,7 +41,7 @@ nve0_disp_sclass[] = {
 
 static struct nouveau_oclass
 nve0_disp_base_oclass[] = {
-	{ NVE0_DISP_CLASS, &nvd0_disp_base_ofuncs, nva3_disp_base_omthds },
+	{ NVE0_DISP_CLASS, &nvd0_disp_base_ofuncs, nvd0_disp_base_omthds },
 	{}
 };
 
