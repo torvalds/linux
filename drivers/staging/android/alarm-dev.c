027c440971f9b3dccc05da5bf626cb129f8daf58
SeongJae Park <sj38.park@gmail.com>
2013-11-14 21:15:11 +0900

    staging: alarm-dev: Remove unnecessary blank lines
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/alarm-dev.c
+++ drivers/staging/android/alarm-dev.c
@@ -72,7 +72,6 @@ static int is_wakeup(enum android_alarm_type type)
 		type == ANDROID_ALARM_ELAPSED_REALTIME_WAKEUP;
 }
 
-
 static void devalarm_start(struct devalarm *alrm, ktime_t exp)
 {
 	if (is_wakeup(alrm->type))
@@ -111,7 +110,6 @@ static void alarm_clear(enum android_alarm_type alarm_type)
 	}
 	alarm_enabled &= ~alarm_type_mask;
 	spin_unlock_irqrestore(&alarm_slock, flags);
-
 }
 
 static void alarm_set(enum android_alarm_type alarm_type,
@@ -370,7 +368,6 @@ static void devalarm_triggered(struct devalarm *alarm)
 	}
 	spin_unlock_irqrestore(&alarm_slock, flags);
 }
-
 
 static enum hrtimer_restart devalarm_hrthandler(struct hrtimer *hrt)
 {
