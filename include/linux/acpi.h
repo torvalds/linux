45c42a7f582cc07259bc78910de8b0b1c94094d5
Jarkko Nikula <jarkko.nikula@linux.intel.com>
2013-11-14 14:03:51 +0200

    ACPI: Provide acpi_dev_name accessor for struct acpi_device device name
    struct acpi_device fields are only available when CONFIG_ACPI is set. We may
    find use for dev_name(&adev->dev) in generic code that is build also without
    CONFIG_ACPI is set but currently this requires #ifdef CONFIG_ACPI churn.
    
    Provide here an accessor that returns dev_name of embedded struct device dev
    in struct acpi_device or NULL depending on CONFIG_ACPI setting.
    
    Signed-off-by: Jarkko Nikula <jarkko.nikula@linux.intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -53,6 +53,11 @@ static inline acpi_handle acpi_device_handle(struct acpi_device *adev)
 #define ACPI_COMPANION_SET(dev, adev)	ACPI_COMPANION(dev) = (adev)
 #define ACPI_HANDLE(dev)		acpi_device_handle(ACPI_COMPANION(dev))
 
+static inline const char *acpi_dev_name(struct acpi_device *adev)
+{
+	return dev_name(&adev->dev);
+}
+
 enum acpi_irq_model_id {
 	ACPI_IRQ_MODEL_PIC = 0,
 	ACPI_IRQ_MODEL_IOAPIC,
@@ -413,6 +418,11 @@ static inline bool acpi_driver_match_device(struct device *dev,
 #define ACPI_COMPANION(dev)		(NULL)
 #define ACPI_COMPANION_SET(dev, adev)	do { } while (0)
 #define ACPI_HANDLE(dev)		(NULL)
+
+static inline const char *acpi_dev_name(struct acpi_device *adev)
+{
+	return NULL;
+}
 
 static inline void acpi_early_init(void) { }
 
