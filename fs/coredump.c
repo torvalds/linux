db51242d89b3059a46a3cf2f3339f8cd975cb954
Al Viro <viro@zeniv.linux.org.uk>
2013-11-15 21:55:52 -0500

    dump_align(): fix the dumb braino
    Mea culpa - original variant used 64-by-32-bit division,
    which got caught very late.  Getting rid of that wasn't
    hard, but I'd managed to botch the calling conventions
    in process ;-/
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/coredump.c
+++ fs/coredump.c
@@ -733,7 +733,7 @@ int dump_align(struct coredump_params *cprm, int align)
 {
 	unsigned mod = cprm->written & (align - 1);
 	if (align & (align - 1))
-		return -EINVAL;
-	return mod ? dump_skip(cprm, align - mod) : 0;
+		return 0;
+	return mod ? dump_skip(cprm, align - mod) : 1;
 }
 EXPORT_SYMBOL(dump_align);
