9fee8240cfd37081c88e2d84852c497ba18baeac
Adam Cozzette <acozzette@gmail.com>
2013-11-17 13:51:23 -0800

    HID: usbkbd: fix inconsistent debugging output
    It looks like this typo was introduced by a mistake in a copy-and-paste in
    commit ddbe32491951.
    
    Signed-off-by: Adam Cozzette <acozzette@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/usbhid/usbkbd.c
+++ drivers/hid/usbhid/usbkbd.c
@@ -146,7 +146,7 @@ static void usb_kbd_irq(struct urb *urb)
 				input_report_key(kbd->dev, usb_kbd_keycode[kbd->new[i]], 1);
 			else
 				hid_info(urb->dev,
-					 "Unknown key (scancode %#x) released.\n",
+					 "Unknown key (scancode %#x) pressed.\n",
 					 kbd->new[i]);
 		}
 	}
