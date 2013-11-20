6e43860dd7ad8c2687a5987e0ae197da5c0a4b3a
Magnus Damm <damm@opensource.se>
2013-11-20 09:23:35 +0900

    gpio: em: Use lazy disable
    Set the ->irq_enable() and ->irq_disable() methods to NULL
    to enable lazy disable of interrupts. This by itself provides
    some level of optimization, but is mainly enabled as ground
    work for future Suspend-to-RAM wake up support.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    (cherry picked from commit 664734c012e6f3a88b2da4c586002cd62a277003)
    (Queued by Linus Walleij for v3.14 but not yet in Linus's tree)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- drivers/gpio/gpio-em.c
+++ drivers/gpio/gpio-em.c
@@ -336,8 +336,6 @@ static int em_gio_probe(struct platform_device *pdev)
 	irq_chip->name = name;
 	irq_chip->irq_mask = em_gio_irq_disable;
 	irq_chip->irq_unmask = em_gio_irq_enable;
-	irq_chip->irq_enable = em_gio_irq_enable;
-	irq_chip->irq_disable = em_gio_irq_disable;
 	irq_chip->irq_set_type = em_gio_irq_set_type;
 	irq_chip->flags	= IRQCHIP_SKIP_SET_WAKE;
 
