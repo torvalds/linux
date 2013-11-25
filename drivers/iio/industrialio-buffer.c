647cc7b9be861e01723a3183f5d06729a1613a97
Lars-Peter Clausen <lars@metafoo.de>
2013-11-25 14:56:00 +0000

    iio: Add data_available callback for buffers
    This patch adds a new data_available() callback to the iio_buffer_access_funcs
    struct. The callback is used to indicate whether data is available in the buffer
    for reading. It is meant to replace the stufftoread flag from the iio_buffer
    struct. The reasoning for this is that the buffer implementation usually can
    determine whether data is available rather easily based on its state, on the
    other hand it can be rather tricky to update the stufftoread flag in a race free
    way.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/industrialio-buffer.c
+++ drivers/iio/industrialio-buffer.c
@@ -37,6 +37,14 @@ static bool iio_buffer_is_active(struct iio_buffer *buf)
 	return !list_empty(&buf->buffer_list);
 }
 
+static bool iio_buffer_data_available(struct iio_buffer *buf)
+{
+	if (buf->access->data_available)
+		return buf->access->data_available(buf);
+
+	return buf->stufftoread;
+}
+
 /**
  * iio_buffer_read_first_n_outer() - chrdev read for buffer access
  *
@@ -70,7 +78,7 @@ unsigned int iio_buffer_poll(struct file *filp,
 		return -ENODEV;
 
 	poll_wait(filp, &rb->pollq, wait);
-	if (rb->stufftoread)
+	if (iio_buffer_data_available(rb))
 		return POLLIN | POLLRDNORM;
 	/* need a way of knowing if there may be enough data... */
 	return 0;
