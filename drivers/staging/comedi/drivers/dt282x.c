e0bee2de37da868c858891dd1c5e2fd93ec15297
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-11-26 16:42:05 -0700

    staging: comedi: dt282x: convert dt282x_ai_dma_interrupt() messages
    Convert the messages in this function to dev_{level}().
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/dt282x.c
+++ drivers/staging/comedi/drivers/dt282x.c
@@ -348,7 +348,7 @@ static void dt282x_ai_dma_interrupt(struct comedi_device *dev)
 	outw(devpriv->supcsr | DT2821_CLRDMADNE, dev->iobase + DT2821_SUPCSR);
 
 	if (!s->async->prealloc_buf) {
-		printk(KERN_ERR "async->data disappeared.  dang!\n");
+		dev_err(dev->class_dev, "no buffer in %s\n", __func__);
 		return;
 	}
 
@@ -369,7 +369,7 @@ static void dt282x_ai_dma_interrupt(struct comedi_device *dev)
 	devpriv->nread -= size / 2;
 
 	if (devpriv->nread < 0) {
-		printk(KERN_INFO "dt282x: off by one\n");
+		dev_info(dev->class_dev, "nread off by one\n");
 		devpriv->nread = 0;
 	}
 	if (!devpriv->nread) {
