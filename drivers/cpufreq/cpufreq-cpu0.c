2209b0c964c7c402b07d0df6fcfbecf8ff53db30
Paul Walmsley <pwalmsley@nvidia.com>
2013-11-25 18:01:18 -0800

    cpufreq: cpufreq-cpu0: clk_round_rate() can return a zero upon error
    Treat both negative and zero return values from clk_round_rate()
    as errors.  This is needed since subsequent patches will convert
    clk_round_rate()'s return value to be an unsigned type, rather
    than a signed type, since some clock sources can generate rates
    higher than (2^31)-1 Hz.
    
    Eventually, when calling clk_round_rate(), only a return value of
    zero will be considered a error.  All other values will be
    considered valid rates.  The comparison against values less than
    0 is kept to preserve the correct behavior in the meantime.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/cpufreq-cpu0.c
+++ drivers/cpufreq/cpufreq-cpu0.c
@@ -44,7 +44,7 @@ static int cpu0_set_target(struct cpufreq_policy *policy, unsigned int index)
 	int ret;
 
 	freq_Hz = clk_round_rate(cpu_clk, freq_table[index].frequency * 1000);
-	if (freq_Hz < 0)
+	if (freq_Hz <= 0)
 		freq_Hz = freq_table[index].frequency * 1000;
 
 	freq_exact = freq_Hz;
