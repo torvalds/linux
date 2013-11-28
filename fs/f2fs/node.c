01d2d1aa0648192fd1d49f7d74d7e8b85b1c585a
Chao Yu <chao2.yu@samsung.com>
2013-11-28 15:43:07 +0800

    f2fs: use true and false for boolean variable
    The inode_page_locked should be a boolean variable.
    
    struct dnode_of_data {
    	struct inode *inode;            /* vfs inode pointer */
    	struct page *inode_page;        /* its inode page, NULL is possible */
    	struct page *node_page;         /* cached direct node page */
    	nid_t nid;                      /* node id of the direct node block */
    	unsigned int ofs_in_node;       /* data offset in the node page */
    ==>	bool inode_page_locked;         /* inode page is locked or not */
    	block_t data_blkaddr;           /* block address of the node block */
    };
    
    Signed-off-by: Chao Yu <chao2.yu@samsung.com>
    [Jaegeuk Kim: add description]
    Signed-off-by: Jaegeuk Kim <jaegeuk.kim@samsung.com>

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -791,7 +791,7 @@ int truncate_xattr_node(struct inode *inode, struct page *page)
 	set_new_dnode(&dn, inode, page, npage, nid);
 
 	if (page)
-		dn.inode_page_locked = 1;
+		dn.inode_page_locked = true;
 	truncate_node(&dn);
 	return 0;
 }
