6dd500751ded6e7b5af455a63d8afef1a581868a
Jingoo Han <jg1.han@samsung.com>
2013-12-02 09:24:29 +0900

    serial: use platform_{get,set}_drvdata()
        Use the wrapper functions for getting and setting the driver data using
        platform_device instead of using dev_{get,set}_drvdata() with &pdev->dev,
        so we can directly pass a struct platform_device.
    
        Also, unnecessary dev_set_drvdata() is removed, because the driver core
        clears the driver data to NULL after device_release or on probe failure.
    
        Signed-off-by: Jingoo Han <jg1.han@samsung.com>
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
        (cherry picked from commit 696faedd616e202f5c510cd03dcc8853c11ca6db)
        Signed-off-by: Daniel Sangorrin <daniel.sangorrin@toshiba.co.jp>
    
    Signed-off-by: Yoshitake Kobayashi <yoshitake.kobayashi@toshiba.co.jp>

--- drivers/tty/serial/xilinx_uartps.c
+++ drivers/tty/serial/xilinx_uartps.c
@@ -1006,12 +1006,11 @@ static int xuartps_probe(struct platform_device *pdev)
 		port->dev = &pdev->dev;
 		port->uartclk = clk_get_rate(xuartps_data->refclk);
 		port->private_data = xuartps_data;
-		dev_set_drvdata(&pdev->dev, port);
+                platform_set_drvdata(pdev, port);
 		rc = uart_add_one_port(&xuartps_uart_driver, port);
 		if (rc) {
 			dev_err(&pdev->dev,
 				"uart_add_one_port() failed; err=%i\n", rc);
-			dev_set_drvdata(&pdev->dev, NULL);
 			goto err_out_clk_disable;
 		}
 		return 0;
@@ -1039,13 +1038,12 @@ err_out_free:
  **/
 static int xuartps_remove(struct platform_device *pdev)
 {
-	struct uart_port *port = dev_get_drvdata(&pdev->dev);
+	struct uart_port *port = platform_get_drvdata(pdev);
 	struct xuartps *xuartps_data = port->private_data;
 	int rc;
 
 	/* Remove the xuartps port from the serial core */
 	rc = uart_remove_one_port(&xuartps_uart_driver, port);
-	dev_set_drvdata(&pdev->dev, NULL);
 	port->mapbase = 0;
 	clk_disable_unprepare(xuartps_data->refclk);
 	clk_disable_unprepare(xuartps_data->aperclk);
