f00367d82281548528b3ee60a65dee7cc9f78c8d
luowei <lw@rock-chips.com>
2013-12-03 11:48:30 +0800

    add pinctrl driver support

--- arch/arm/mach-rockchip/rk3188.c
+++ arch/arm/mach-rockchip/rk3188.c
@@ -49,6 +49,7 @@ static const char * const rk3188_dt_compat[] = {
 };
 
 DT_MACHINE_START(RK3188_DT, "Rockchip RK3188 (Flattened Device Tree)")
+	.nr_irqs        = 32*10,
 	.smp		= smp_ops(rockchip_smp_ops),
 	.map_io		= rk3188_dt_map_io,
 	.init_machine	= rk3188_dt_init,
