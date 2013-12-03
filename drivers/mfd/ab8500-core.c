ed83d301eac2ca7c1315d0e283e1c14c07a53e16
Linus Walleij <linus.walleij@linaro.org>
2013-12-03 15:11:54 +0100

    mfd: ab8500: Use irqdomain to map interrupts
    All subdrivers use the irqdomain to demux AB8500 IRQs but here
    in the hierarchical path we find a leftover instance using the
    hard-coded IRQ base. Convert it to use irqdomain with a
    oneliner.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/ab8500-core.c
+++ drivers/mfd/ab8500-core.c
@@ -491,7 +491,7 @@ static int ab8500_handle_hierarchical_line(struct ab8500 *ab8500,
 		if (line == AB8540_INT_GPIO43F || line == AB8540_INT_GPIO44F)
 			line += 1;
 
-		handle_nested_irq(ab8500->irq_base + line);
+		handle_nested_irq(irq_create_mapping(ab8500->domain, line));
 	}
 
 	return 0;
