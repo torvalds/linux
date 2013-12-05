3b5a7ab40c2a1bfd89dd5b4bbfe98f8692d5fd62
Ping Cheng <pinglinux@gmail.com>
2013-12-05 12:42:09 -0800

    Input: add SW_MUTE_DEVICE switch definition
    Some devices, such as new Intuos series tablets, have a hardware switch to
    turn touch data on/off. To report the state, SW_MUTE_DEVICE is added
    in include/uapi/linux/input.h.
    
    Reviewed_by: Chris Bagwell <chris@cnpbagwell.com>
    Acked-by: Peter Hutterer <peter.hutterer@who-t.net>
    Tested-by: Jason Gerecke <killertofu@gmail.com>
    Signed-off-by: Ping Cheng <pingc@wacom.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- include/uapi/linux/input.h
+++ include/uapi/linux/input.h
@@ -858,6 +858,7 @@ struct input_keymap_entry {
 #define SW_FRONT_PROXIMITY	0x0b  /* set = front proximity sensor active */
 #define SW_ROTATE_LOCK		0x0c  /* set = rotate locked/disabled */
 #define SW_LINEIN_INSERT	0x0d  /* set = inserted */
+#define SW_MUTE_DEVICE		0x0e  /* set = device disabled */
 #define SW_MAX			0x0f
 #define SW_CNT			(SW_MAX+1)
 
