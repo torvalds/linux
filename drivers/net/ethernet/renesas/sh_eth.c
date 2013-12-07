cc01e018e04e8ee4549dc6f788d8ccf85c675363
Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
2013-12-08 02:59:18 +0300

    sh_eth: add R8A7791 support
    Add support for yet another ARM member of the R-Car family, R-Car M2, also known
    as R8A7791 -- it will share the code and data with previously added R8A7790.
    Despite the Ether devices in these SoCs are indistinguishable at least from the
    driver's point of view, we do introduce a new platform device ID "r8a7791-ether"
    unlike the wildcard ID used for R8A7778/9 SoCs, due to newly established policy
    for the Renesas SoCs.
    
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 94a12b15e4c575e0aa0ba5e24a4f213163a823d0)
    (Queued by David Miller for v3.14 but not yet in Linus's tree)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- drivers/net/ethernet/renesas/sh_eth.c
+++ drivers/net/ethernet/renesas/sh_eth.c
@@ -395,8 +395,8 @@ static struct sh_eth_cpu_data r8a777x_data = {
 	.hw_swap	= 1,
 };
 
-/* R8A7790 */
-static struct sh_eth_cpu_data r8a7790_data = {
+/* R8A7790/1 */
+static struct sh_eth_cpu_data r8a779x_data = {
 	.set_duplex	= sh_eth_set_duplex,
 	.set_rate	= sh_eth_set_rate_r8a777x,
 
@@ -2811,7 +2811,8 @@ static struct platform_device_id sh_eth_id_table[] = {
 	{ "sh7763-gether", (kernel_ulong_t)&sh7763_data },
 	{ "r8a7740-gether", (kernel_ulong_t)&r8a7740_data },
 	{ "r8a777x-ether", (kernel_ulong_t)&r8a777x_data },
-	{ "r8a7790-ether", (kernel_ulong_t)&r8a7790_data },
+	{ "r8a7790-ether", (kernel_ulong_t)&r8a779x_data },
+	{ "r8a7791-ether", (kernel_ulong_t)&r8a779x_data },
 	{ }
 };
 MODULE_DEVICE_TABLE(platform, sh_eth_id_table);
