68536053600425c24aba031c45f053d447eedd9c
Florent Fourcot <florent.fourcot@enst-bretagne.fr>
2013-12-12 17:07:58 +0100

    ipv6: fix incorrect type in declaration
    Introduced by 1397ed35f22d7c30d0b89ba74b6b7829220dfcfd
      "ipv6: add flowinfo for tcp6 pkt_options for all cases"
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    
    V2: fix the title, add empty line after the declaration (Sergei Shtylyov
    feedbacks)
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/ipv6_sockglue.c
+++ net/ipv6/ipv6_sockglue.c
@@ -1035,7 +1035,8 @@ static int do_ipv6_getsockopt(struct sock *sk, int level, int optname,
 				put_cmsg(&msg, SOL_IPV6, IPV6_2292HOPLIMIT, sizeof(hlim), &hlim);
 			}
 			if (np->rxopt.bits.rxflow) {
-				int flowinfo = np->rcv_flowinfo;
+				__be32 flowinfo = np->rcv_flowinfo;
+
 				put_cmsg(&msg, SOL_IPV6, IPV6_FLOWINFO, sizeof(flowinfo), &flowinfo);
 			}
 		}
