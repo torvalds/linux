cd174e67a6b312fce9bab502ba2b0583e11f537f
Hannes Frederic Sowa <hannes@stressinduktion.org>
2013-12-14 05:13:45 +0100

    ipv4: new ip_no_pmtu_disc mode to always discard incoming frag needed msgs
    This new mode discards all incoming fragmentation-needed notifications
    as I guess was originally intended with this knob. To not break backward
    compatibility too much, I only added a special case for mode 2 in the
    receiving path.
    
    Signed-off-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/icmp.c
+++ net/ipv4/icmp.c
@@ -705,7 +705,9 @@ static void icmp_unreach(struct sk_buff *skb)
 		case ICMP_PORT_UNREACH:
 			break;
 		case ICMP_FRAG_NEEDED:
-			if (net->ipv4.sysctl_ip_no_pmtu_disc) {
+			if (net->ipv4.sysctl_ip_no_pmtu_disc == 2) {
+				goto out;
+			} else if (net->ipv4.sysctl_ip_no_pmtu_disc) {
 				LIMIT_NETDEBUG(KERN_INFO pr_fmt("%pI4: fragmentation needed and DF set\n"),
 					       &iph->daddr);
 			} else {
