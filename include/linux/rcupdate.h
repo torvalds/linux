88c1863066ccfa456797e12c5d8b4631aa1ad0d0
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2013-12-16 13:24:32 -0800

    rcu: Define rcu_assign_pointer() in terms of smp_store_release()
    The new smp_store_release() function provides better guarantees than did
    rcu_assign_pointer(), and potentially less overhead on some architectures.
    The guarantee that smp_store_release() provides that rcu_assign_pointer()
    does that is obscure, but its lack could cause considerable confusion.
    This guarantee is illustrated by the following code fragment:
    
    	struct foo {
    		int a;
    		int b;
    		int c;
    		struct foo *next;
    	};
    	struct foo foo1;
    	struct foo foo2;
    	struct foo __rcu *foop;
    
    	...
    
    	foo2.a = 1;
    	foo2.b = 2;
    	BUG_ON(foo2.c);
    	rcu_assign_pointer(foop, &foo);
    
    	...
    
    	fp = rcu_dereference(foop);
    	fp.c = 3;
    
    The current rcu_assign_pointer() semantics permit the BUG_ON() to
    trigger because rcu_assign_pointer()'s smp_wmb() is not guaranteed to
    order prior reads against later writes.  This commit therefore upgrades
    rcu_assign_pointer() from smp_wmb() to smp_store_release() to avoid this
    counter-intuitive outcome.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -44,6 +44,7 @@
 #include <linux/debugobjects.h>
 #include <linux/bug.h>
 #include <linux/compiler.h>
+#include <asm/barrier.h>
 
 #ifdef CONFIG_RCU_TORTURE_TEST
 extern int rcutorture_runnable; /* for sysctl */
@@ -580,12 +581,7 @@ static inline void rcu_preempt_sleep_check(void)
  * please be careful when making changes to rcu_assign_pointer() and the
  * other macros that it invokes.
  */
-#define rcu_assign_pointer(p, v) \
-	do { \
-		smp_wmb(); \
-		ACCESS_ONCE(p) = RCU_INITIALIZER(v); \
-	} while (0)
-
+#define rcu_assign_pointer(p, v) smp_store_release(&p, RCU_INITIALIZER(v))
 
 /**
  * rcu_access_pointer() - fetch RCU pointer with no dereferencing
