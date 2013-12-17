e77a715ac3af14d16c027caebdb0ce5412976b9e
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-12-17 09:03:05 -0800

    Input: pmic8xxx-pwrkey - pass correct device identity to free_irq()
    free_irq() in the error handling case is missing when change
    pass input device directly to interrupt.
    
    Fixes: b27f8fee4965('Input: pmic8xxx-pwrkey - pass input device directly to interrupt')
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/misc/pmic8xxx-pwrkey.c
+++ drivers/input/misc/pmic8xxx-pwrkey.c
@@ -182,7 +182,7 @@ static int pmic8xxx_pwrkey_probe(struct platform_device *pdev)
 	return 0;
 
 free_press_irq:
-	free_irq(key_press_irq, pwrkey);
+	free_irq(key_press_irq, pwr);
 unreg_input_dev:
 	input_unregister_device(pwr);
 	pwr = NULL;
