a97bfc1d1f2b4496d01fda34fc546451f1c02bf5
tanxiaojun <tanxiaojun@huawei.com>
2013-12-19 13:28:11 +0800

    bridge: remove unnecessary parentheses
    Return is not a function, parentheses are not required.
    
    Signed-off-by: Tan Xiaojun <tanxiaojun@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bridge/netfilter/ebtables.c
+++ net/bridge/netfilter/ebtables.c
@@ -118,7 +118,7 @@ ebt_dev_check(const char *entry, const struct net_device *device)
 	/* 1 is the wildcard token */
 	while (entry[i] != '\0' && entry[i] != 1 && entry[i] == devname[i])
 		i++;
-	return (devname[i] != entry[i] && entry[i] != 1);
+	return devname[i] != entry[i] && entry[i] != 1;
 }
 
 #define FWINV2(bool,invflg) ((bool) ^ !!(e->invflags & invflg))
