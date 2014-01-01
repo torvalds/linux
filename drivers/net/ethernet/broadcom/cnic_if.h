d15e2a92c4c263fd44a0b41e637a032bd618e6c9
Eddie Wai <eddie.wai@broadcom.com>
2013-12-31 23:18:34 -0800

    cnic: Add a signature to indicate valid doorbell offset.
    The buffer that is used to pass doorbell offset to the userspace UIO
    driver may contain nonzero value in older versions of bnx2x driver.
    Userspace cannot easily tell whether it contains a valid doorbell
    offset or not.  With the added signature, userspace will only use
    the doorbell offset if the signature is present.
    
    Update version to 2.5.19.
    
    Signed-off-by: Eddie Wai <eddie.wai@broadcom.com>
    Signed-off-by: Michael Chan <mchan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/cnic_if.h
+++ drivers/net/ethernet/broadcom/cnic_if.h
@@ -14,8 +14,8 @@
 
 #include "bnx2x/bnx2x_mfw_req.h"
 
-#define CNIC_MODULE_VERSION	"2.5.18"
-#define CNIC_MODULE_RELDATE	"Sept 01, 2013"
+#define CNIC_MODULE_VERSION	"2.5.19"
+#define CNIC_MODULE_RELDATE	"December 19, 2013"
 
 #define CNIC_ULP_RDMA		0
 #define CNIC_ULP_ISCSI		1
