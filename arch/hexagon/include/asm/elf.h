a9a44fdf329668125042055c569656aa83d8cccb
Richard Kuo <rkuo@codeaurora.org>
2014-01-02 17:17:17 -0600

    Hexagon: update CR year for elf.h
    Signed-off-by: Richard Kuo <rkuo@codeaurora.org>

--- arch/hexagon/include/asm/elf.h
+++ arch/hexagon/include/asm/elf.h
@@ -1,7 +1,7 @@
 /*
  * ELF definitions for the Hexagon architecture
  *
- * Copyright (c) 2010-2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2010-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
