26009d653408c3dd058c3b64fd01e2020cc052f2
Russell King <rmk+kernel@arm.linux.org.uk>
2014-01-03 15:01:39 +0000

    ARM: fix "bad mode in ... handler" message for undefined instructions
    commit 29c350bf28da333e41e30497b649fe335712a2ab upstream.
    
    The array was missing the final entry for the undefined instruction
    exception handler; this commit adds it.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -35,7 +35,13 @@
 #include <asm/tls.h>
 #include <asm/system_misc.h>
 
-static const char *handler[]= { "prefetch abort", "data abort", "address exception", "interrupt" };
+static const char *handler[]= {
+	"prefetch abort",
+	"data abort",
+	"address exception",
+	"interrupt",
+	"undefined instruction",
+};
 
 void *vectors_page;
 
