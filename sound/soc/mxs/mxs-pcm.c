96ae0f08ac574f3dac17cff9afdeee5562f61cbb
Lars-Peter Clausen <lars@metafoo.de>
2014-01-06 17:19:22 +0100

    ASoC: mxs: Remove SND_DMAENGINE_PCM_FLAG_NO_RESIDUE flag
    Since commit 7b11304 ("dma: mxs-dma: Report correct residue for cyclic DMA")
    the mxs dmaengine driver has support for residue reporting. So there is no need
    to specify the SND_DMAENGINE_PCM_FLAG_NO_RESIDUE flag anymore. This allows a
    finer grained resolution for the PCM pointer as well as avoids the race
    condition that can occur with the period counting that is used when the
    dmaengine driver does not support residue reporting.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Tested-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/mxs/mxs-pcm.c
+++ sound/soc/mxs/mxs-pcm.c
@@ -52,7 +52,6 @@ static const struct snd_dmaengine_pcm_config mxs_dmaengine_pcm_config = {
 int mxs_pcm_platform_register(struct device *dev)
 {
 	return devm_snd_dmaengine_pcm_register(dev, &mxs_dmaengine_pcm_config,
-		SND_DMAENGINE_PCM_FLAG_NO_RESIDUE |
 		SND_DMAENGINE_PCM_FLAG_HALF_DUPLEX);
 }
 EXPORT_SYMBOL_GPL(mxs_pcm_platform_register);
