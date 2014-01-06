65eddb56f465f314502679ceade6fc5848a53a50
Yongqiang Yang <xiaoqiangnk@gmail.com>
2014-01-06 14:06:18 -0500

    ext4: ext4_inode_is_fast_symlink should use EXT4_CLUSTER_SIZE
    Can be reproduced by xfstests 62 with bigalloc and 128bit size inode.
    
    Signed-off-by: Yongqiang Yang <yangyongqiang01@baidu.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
    Reviewed-by: Carlos Maiolino <cmaiolino@redhat.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -144,8 +144,8 @@ static int ext4_meta_trans_blocks(struct inode *inode, int lblocks,
  */
 static int ext4_inode_is_fast_symlink(struct inode *inode)
 {
-	int ea_blocks = EXT4_I(inode)->i_file_acl ?
-		(inode->i_sb->s_blocksize >> 9) : 0;
+        int ea_blocks = EXT4_I(inode)->i_file_acl ?
+		EXT4_CLUSTER_SIZE(inode->i_sb) >> 9 : 0;
 
 	return (S_ISLNK(inode->i_mode) && inode->i_blocks - ea_blocks == 0);
 }
