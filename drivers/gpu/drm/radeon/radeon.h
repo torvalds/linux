363eb0b4b787ea6ebb7a58d15df032126c111770
Alex Deucher <alexander.deucher@amd.com>
2014-01-08 17:55:08 -0500

    drm/radeon: add hard_reset module parameter
    Enabling this parameter enables pci config reset,
    aka hard reset, which is a bus level chip reset.
    In some cases this works more reliably than a soft
    reset.  Disabled by default.
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/radeon.h
+++ drivers/gpu/drm/radeon/radeon.h
@@ -99,6 +99,7 @@ extern int radeon_fastfb;
 extern int radeon_dpm;
 extern int radeon_aspm;
 extern int radeon_runtime_pm;
+extern int radeon_hard_reset;
 
 /*
  * Copy from radeon_drv.h so we don't have to include both and have conflicting
