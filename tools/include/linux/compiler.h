835d44b9041e578e3e553a57dfffc7003605c93b
Namhyung Kim <namhyung@kernel.org>
2014-01-09 23:00:53 +0900

    tools include: Define likely/unlikely in linux/compiler.h
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Namhyung Kim <namhyung.kim@lge.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Link: http://lkml.kernel.org/r/1389276059-8829-3-git-send-email-namhyung@kernel.org
    [ Added the new header to tools/perf/MANIFEST ]
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/include/linux/compiler.h
+++ tools/include/linux/compiler.h
@@ -27,4 +27,12 @@
 # define __weak			__attribute__((weak))
 #endif
 
+#ifndef likely
+# define likely(x)		__builtin_expect(!!(x), 1)
+#endif
+
+#ifndef unlikely
+# define unlikely(x)		__builtin_expect(!!(x), 0)
+#endif
+
 #endif /* _TOOLS_LINUX_COMPILER_H */
