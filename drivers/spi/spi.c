b6fb8d3a1f156c50a35f88b9b55f404034493938
Mika Westerberg <mika.westerberg@linux.intel.com>
2014-01-09 15:23:55 +0200

    spi: Check conflicting CS based on spi->chip_select instead of device name
    Commit e13ac47bec20 (spi: Use stable dev_name for ACPI enumerated SPI
    slaves) changed the SPI device naming to be based on ACPI device name
    instead of carrying bus number and chip select for devices enumerated
    from ACPI namespace.
    
    In case of a buggy BIOS that lists multiple SPI devices sharing the same
    chip select (even though they should use different) the current code fails
    to detect that and allows the devices to be added to the bus.
    
    Fix this by walking through the bus and comparing spi->chip_select instead
    of device name. This should work regardless what the device name will be in
    future.
    
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Acked-by: Jarkko Nikula <jarkko.nikula@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -370,6 +370,17 @@ static void spi_dev_set_name(struct spi_device *spi)
 		     spi->chip_select);
 }
 
+static int spi_dev_check(struct device *dev, void *data)
+{
+	struct spi_device *spi = to_spi_device(dev);
+	struct spi_device *new_spi = data;
+
+	if (spi->master == new_spi->master &&
+	    spi->chip_select == new_spi->chip_select)
+		return -EBUSY;
+	return 0;
+}
+
 /**
  * spi_add_device - Add spi_device allocated with spi_alloc_device
  * @spi: spi_device to register
@@ -384,7 +395,6 @@ int spi_add_device(struct spi_device *spi)
 	static DEFINE_MUTEX(spi_add_lock);
 	struct spi_master *master = spi->master;
 	struct device *dev = master->dev.parent;
-	struct device *d;
 	int status;
 
 	/* Chipselects are numbered 0..max; validate. */
@@ -404,12 +414,10 @@ int spi_add_device(struct spi_device *spi)
 	 */
 	mutex_lock(&spi_add_lock);
 
-	d = bus_find_device_by_name(&spi_bus_type, NULL, dev_name(&spi->dev));
-	if (d != NULL) {
+	status = bus_for_each_dev(&spi_bus_type, NULL, spi, spi_dev_check);
+	if (status) {
 		dev_err(dev, "chipselect %d already in use\n",
 				spi->chip_select);
-		put_device(d);
-		status = -EBUSY;
 		goto done;
 	}
 
