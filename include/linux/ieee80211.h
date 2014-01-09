852c0153df5082311f50c062275813905f39f56e
Avinash Patil <patila@marvell.com>
2014-01-09 15:22:57 -0800

    ieee80211: add definition for TDLS wide band extended capability
    Seventh bit of 8th byte of extended capabilities specifies wide
    bandwidth support for TDLS links. Add this definition to ieee80211.
    
    Signed-off-by: Avinash Patil <patila@marvell.com>
    Signed-off-by: Bing Zhao <bzhao@marvell.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- include/linux/ieee80211.h
+++ include/linux/ieee80211.h
@@ -1902,6 +1902,7 @@ enum ieee80211_tdls_actioncode {
 #define WLAN_EXT_CAPA5_TDLS_PROHIBITED	BIT(6)
 
 #define WLAN_EXT_CAPA8_OPMODE_NOTIF	BIT(6)
+#define WLAN_EXT_CAPA8_TDLS_WIDE_BW_ENABLED	BIT(7)
 
 /* TDLS specific payload type in the LLC/SNAP header */
 #define WLAN_TDLS_SNAP_RFTYPE	0x2
