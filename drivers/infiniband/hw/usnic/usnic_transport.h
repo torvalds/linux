6214105460842759020bdd7f4dbb50afa1be9d17
Upinder Malhi <umalhi@cisco.com>
2014-01-09 14:48:16 -0800

    IB:usnic: Add UDP support to usnic_transport.[hc]
    This patch provides API for rest of usNIC code to increment or decrement
    socket's reference count. Auxiliary socket APIs are also provided.
    
    Signed-off-by: Upinder Malhi <umalhi@cisco.com>
    Signed-off-by: Roland Dreier <roland@purestorage.com>

--- drivers/infiniband/hw/usnic/usnic_transport.h
+++ drivers/infiniband/hw/usnic/usnic_transport.h
@@ -21,8 +21,27 @@
 
 #include "usnic_abi.h"
 
+const char *usnic_transport_to_str(enum usnic_transport_type trans_type);
+/*
+ * Returns number of bytes written, excluding null terminator. If
+ * nothing was written, the function returns 0.
+ */
+int usnic_transport_sock_to_str(char *buf, int buf_sz,
+					struct socket *sock);
 u16 usnic_transport_rsrv_port(enum usnic_transport_type type, u16 port_num);
 void usnic_transport_unrsrv_port(enum usnic_transport_type type, u16 port_num);
+/*
+ * Do a fget on the socket refered to by sock_fd and returns the socket.
+ * Socket will not be destroyed before usnic_transport_put_socket has
+ * been called.
+ */
+struct socket *usnic_transport_get_socket(int sock_fd);
+void usnic_transport_put_socket(struct socket *sock);
+/*
+ * Call usnic_transport_get_socket before calling *_sock_get_addr
+ */
+int usnic_transport_sock_get_addr(struct socket *sock, int *proto,
+					uint32_t *addr, uint16_t *port);
 int usnic_transport_init(void);
 void usnic_transport_fini(void);
 #endif /* !USNIC_TRANSPORT_H */
