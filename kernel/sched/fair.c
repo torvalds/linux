257e5075a1433513bb354f202adcd2dea8a8dc08
Chris Redpath <chris.redpath@arm.com>
2014-01-09 10:38:54 +0000

    sched: hmp: Make idle balance behaviour normal when packing disabled
    The presence of packing permanently changed the idle balance
    behaviour. Do not restrict idle balance on the smallest CPUs when
    packing is present but disabled.
    
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6350,6 +6350,10 @@ static int hmp_packing_ilb_needed(int cpu)
 	if (!hmp_cpu_is_slowest(cpu))
 		return 1;
 
+	/* if disabled, use normal ILB behaviour */
+	if (!hmp_packing_enabled)
+		return 1;
+
 	hmp = hmp_cpu_domain(cpu);
 	for_each_cpu_and(cpu, &hmp->cpus, nohz.idle_cpus_mask) {
 		/* only idle balance if a CPU is loaded over threshold */
