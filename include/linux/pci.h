4ab44676064baeb6c7d807c0f627e07d29ce48e0
Stephen Hemminger <stephen@networkplumber.org>
2014-01-10 15:41:35 -0700

    PCI: Remove unused pci_renumber_slot()
    My philosophy is unused code is dead code.  And dead code is subject to bit
    rot and is a likely source of bugs.  Use it or lose it.
    
    This reverts part of f46753c5e354 ("PCI: introduce pci_slot") and
    d25b7c8d6ba2 ("PCI: rename pci_update_slot_number to pci_renumber_slot"),
    removing this interface:
    
        pci_renumber_slot()
    
    [bhelgaas: split to separate patch, add historical link from Alex]
    Link: http://lkml.kernel.org/r/20081009043140.8678.44164.stgit@bob.kio
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Alex Chiang <achiang@canonical.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -763,7 +763,6 @@ struct pci_slot *pci_create_slot(struct pci_bus *parent, int slot_nr,
 				 const char *name,
 				 struct hotplug_slot *hotplug);
 void pci_destroy_slot(struct pci_slot *slot);
-void pci_renumber_slot(struct pci_slot *slot, int slot_nr);
 int pci_scan_slot(struct pci_bus *bus, int devfn);
 struct pci_dev *pci_scan_single_device(struct pci_bus *bus, int devfn);
 void pci_device_add(struct pci_dev *dev, struct pci_bus *bus);
