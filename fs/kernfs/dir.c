88533f990c616cf50c2fe585ea03f75c806a293d
Tejun Heo <tj@kernel.org>
2014-01-11 18:23:23 -0500

    kernfs: remove unnecessary NULL check in __kernfs_remove()
    895a068a524e ("kernfs: make kernfs_get_active() block if the node is
    deactivated but not removed") added "struct kernfs_root *root =
    kernfs_root(kn);" at the head of the function; however, the parameter
    @kn is checked for later implying that the function may be called with
    NULL.  This means that we may end up invoking kernfs_root() with NULL
    which will oops.  None of the existing users invokes removal with NULL
    @kn, so this bug doesn't actually trigger.
    
    We can relocate kernfs_root() invocation after NULL check; however,
    allowing NULL param tends to cause more confusion than actually
    helping anything.  As there's no existing user, let's remove the
    spurious NULL check.
    
    This bug was detected by smatch.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/kernfs/dir.c
+++ fs/kernfs/dir.c
@@ -920,9 +920,6 @@ static void __kernfs_remove(struct kernfs_node *kn)
 
 	lockdep_assert_held(&kernfs_mutex);
 
-	if (!kn)
-		return;
-
 	pr_debug("kernfs %s: removing\n", kn->name);
 
 	__kernfs_deactivate(kn);
