84bb9cdd035ee6ed63371ae926ed225ba51770f2
Peter Meerwald <pmeerw@pmeerw.net>
2014-01-13 21:28:00 +0000

    staging:iio:accel:sca3000: Event_attribute_group seems to be missing for _info_with_temp
    issue introduced here 6fe8135f:
    staging:iio: implement an iio_info structure to take some of the constant elements out of iio_dev
    
    Signed-off-by: Peter Meerwald <pmeerw@pmeerw.net>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/accel/sca3000_core.c
+++ drivers/staging/iio/accel/sca3000_core.c
@@ -1141,6 +1141,7 @@ static const struct iio_info sca3000_info = {
 static const struct iio_info sca3000_info_with_temp = {
 	.attrs = &sca3000_attribute_group_with_temp,
 	.read_raw = &sca3000_read_raw,
+	.event_attrs = &sca3000_event_attribute_group,
 	.read_event_value = &sca3000_read_thresh,
 	.write_event_value = &sca3000_write_thresh,
 	.read_event_config = &sca3000_read_event_config,
