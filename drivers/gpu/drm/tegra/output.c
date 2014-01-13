acde541324dcfe443791da2f3926614d35429222
Thierry Reding <treding@nvidia.com>
2014-01-13 12:56:19 +0100

    drm/tegra: Clarify how panel modes override others
    When a panel advertises one or more modes, they are used exclusively.
    Other methods for obtaining the mode, such as DDC as used for HDMI or
    binary EDID blobs embedded in the DT, are ignored. The panel drivers
    should be providing this functionality if they want to expose it as
    well.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/output.c
+++ drivers/gpu/drm/tegra/output.c
@@ -18,6 +18,10 @@ static int tegra_connector_get_modes(struct drm_connector *connector)
 	struct edid *edid = NULL;
 	int err = 0;
 
+	/*
+	 * If the panel provides one or more modes, use them exclusively and
+	 * ignore any other means of obtaining a mode.
+	 */
 	if (output->panel) {
 		err = output->panel->funcs->get_modes(output->panel);
 		if (err > 0)
