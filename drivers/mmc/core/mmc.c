57de31f6351ecad9212a26d2112b546e4c8b4a5b
Ulf Hansson <ulf.hansson@linaro.org>
2014-01-14 17:36:21 +0100

    mmc: core: Use generic CMD6 time while switching to eMMC HS200 mode
    Conform to the eMMC spec and use the CMD6 generic timeout from the
    EXT_CSD register, when switching to HS200 mode.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Chris Ball <chris@printf.net>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -857,7 +857,9 @@ static int mmc_select_hs200(struct mmc_card *card)
 	/* switch to HS200 mode if bus width set successfully */
 	if (!err)
 		err = __mmc_switch(card, EXT_CSD_CMD_SET_NORMAL,
-				EXT_CSD_HS_TIMING, 2, 0, true, true, true);
+				EXT_CSD_HS_TIMING, 2,
+				card->ext_csd.generic_cmd6_time,
+				true, true, true);
 err:
 	return err;
 }
