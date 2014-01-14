3c06b12b046e426200d016dbdb1e3e81ffb1c185
Hauke Mehrtens <hauke@hauke-m.de>
2014-01-14 19:36:55 +0100

    MIPS: BCM47XX: fix position of cpu_wait disabling
    The disabling of cpu_wait was done too early, before the detection was
    done. This moves the code to a position where it actually works.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    Acked-by: Rafał Miłecki <zajec5@gmail.com>
    Signed-off-by: John Crispin <blogic@openwrt.org>
    Patchwork: http://patchwork.linux-mips.org/patch/6352/

--- arch/mips/bcm47xx/setup.c
+++ arch/mips/bcm47xx/setup.c
@@ -202,15 +202,6 @@ static void __init bcm47xx_register_bcma(void)
 		panic("Failed to initialize BCMA bus (err %d)", err);
 
 	bcm47xx_fill_bcma_boardinfo(&bcm47xx_bus.bcma.bus.boardinfo, NULL);
-
-	/* The BCM4706 has a problem with the CPU wait instruction.
-	 * When r4k_wait or r4k_wait_irqoff is used will just hang and
-	 * not return from a msleep(). Removing the cpu_wait
-	 * functionality is a workaround for this problem. The BCM4716
-	 * does not have this problem.
-	 */
-	if (bcm47xx_bus.bcma.bus.chipinfo.id == BCMA_CHIP_ID_BCM4706)
-		cpu_wait = NULL;
 }
 #endif
 
@@ -240,6 +231,31 @@ void __init plat_mem_setup(void)
 	bcm47xx_board_detect();
 	mips_set_machine_name(bcm47xx_board_get_name());
 }
+
+static int __init bcm47xx_cpu_fixes(void)
+{
+	switch (bcm47xx_bus_type) {
+#ifdef CONFIG_BCM47XX_SSB
+	case BCM47XX_BUS_TYPE_SSB:
+		/* Nothing to do */
+		break;
+#endif
+#ifdef CONFIG_BCM47XX_BCMA
+	case BCM47XX_BUS_TYPE_BCMA:
+		/* The BCM4706 has a problem with the CPU wait instruction.
+		 * When r4k_wait or r4k_wait_irqoff is used will just hang and
+		 * not return from a msleep(). Removing the cpu_wait
+		 * functionality is a workaround for this problem. The BCM4716
+		 * does not have this problem.
+		 */
+		if (bcm47xx_bus.bcma.bus.chipinfo.id == BCMA_CHIP_ID_BCM4706)
+			cpu_wait = NULL;
+		break;
+#endif
+	}
+	return 0;
+}
+arch_initcall(bcm47xx_cpu_fixes);
 
 static int __init bcm47xx_register_bus_complete(void)
 {
