15ddb513f2f9494e8dec8ba2462f288a324b0e04
Monam Agarwal <monamagarwal123@gmail.com>
2014-01-14 09:02:42 -0300

    [media] Staging: media: Fix line length exceeding 80 characters in as102_fe.c
    This patch fixes the following checkpatch.pl issues in
    as102/as102_fe.c
    WARNING: line over 80 characters
    
    Signed-off-by: Monam Agarwal <monamagarwal123@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/staging/media/as102/as102_fe.c
+++ drivers/staging/media/as102/as102_fe.c
@@ -238,7 +238,8 @@ static int as102_fe_ts_bus_ctrl(struct dvb_frontend *fe, int acquire)
 
 	if (acquire) {
 		if (elna_enable)
-			as10x_cmd_set_context(&dev->bus_adap, CONTEXT_LNA, dev->elna_cfg);
+			as10x_cmd_set_context(&dev->bus_adap,
+					      CONTEXT_LNA, dev->elna_cfg);
 
 		ret = as10x_cmd_turn_on(&dev->bus_adap);
 	} else {
