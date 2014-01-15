62b389161fea824b6c87614daa6483ef7632f786
Fabio Estevam <fabio.estevam@freescale.com>
2014-01-15 00:52:45 -0200

    regulator: pfuze100-regulator: Fix some checkpatch complaints
    Fix the following checkpatch error and warning:
    
    ERROR: switch and case should be at the same indent
    #311: FILE: drivers/regulator/pfuze100-regulator.c:311:
    +	switch (value & 0x0f) {
    [...]
    +		case 0x8:
    [...]
    +		case 0x0:
    [...]
    +		default:
    
    WARNING: line over 80 characters
    #312: FILE: drivers/regulator/pfuze100-regulator.c:312:
    +		/* Freescale misprogrammed 1-3% of parts prior to week 8 of 2013 as ID=8 */
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/regulator/pfuze100-regulator.c
+++ drivers/regulator/pfuze100-regulator.c
@@ -309,14 +309,17 @@ static int pfuze_identify(struct pfuze_chip *pfuze_chip)
 		return ret;
 
 	switch (value & 0x0f) {
-		/* Freescale misprogrammed 1-3% of parts prior to week 8 of 2013 as ID=8 */
-		case 0x8:
-			dev_info(pfuze_chip->dev, "Assuming misprogrammed ID=0x8");
-		case 0x0:
-			break;
-		default:
-			dev_warn(pfuze_chip->dev, "Illegal ID: %x\n", value);
-			return -ENODEV;
+	/*
+	 * Freescale misprogrammed 1-3% of parts prior to week 8 of 2013
+	 * as ID=8
+	 */
+	case 0x8:
+		dev_info(pfuze_chip->dev, "Assuming misprogrammed ID=0x8");
+	case 0x0:
+		break;
+	default:
+		dev_warn(pfuze_chip->dev, "Illegal ID: %x\n", value);
+		return -ENODEV;
 	}
 
 	ret = regmap_read(pfuze_chip->regmap, PFUZE100_REVID, &value);
