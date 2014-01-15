1104a9c822f0e9f5e57a236f20a142166dd8f91e
Mark Brown <broonie@linaro.org>
2014-01-15 19:04:19 +0000

    ASoC: core: Return -ENOTSUPP from set_sysclk() if no operation provided
    Make it easier for generic code to work with set_sysclk() by distinguishing
    between the operation not being supported and an error as is done for
    other operations like set_dai_fmt()
    
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -3484,7 +3484,7 @@ int snd_soc_dai_set_sysclk(struct snd_soc_dai *dai, int clk_id,
 		return dai->codec->driver->set_sysclk(dai->codec, clk_id, 0,
 						      freq, dir);
 	else
-		return -EINVAL;
+		return -ENOTSUPP;
 }
 EXPORT_SYMBOL_GPL(snd_soc_dai_set_sysclk);
 
@@ -3505,7 +3505,7 @@ int snd_soc_codec_set_sysclk(struct snd_soc_codec *codec, int clk_id,
 		return codec->driver->set_sysclk(codec, clk_id, source,
 						 freq, dir);
 	else
-		return -EINVAL;
+		return -ENOTSUPP;
 }
 EXPORT_SYMBOL_GPL(snd_soc_codec_set_sysclk);
 
