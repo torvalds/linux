590eca1e9f10b7ddf97718c6c4c12c73d0fd0083
Xiubo Li <Li.Xiubo@freescale.com>
2014-01-15 11:16:35 +0800

    misc: sram: cleanup the code
    Since the devm_gen_pool_create() is used, so the gen_pool_destroy()
    here is redundant.
    
    Signed-off-by: Xiubo Li <Li.Xiubo@freescale.com>
    Acked-by: Philipp Zabel <p.zabel@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/sram.c
+++ drivers/misc/sram.c
@@ -87,8 +87,6 @@ static int sram_remove(struct platform_device *pdev)
 	if (gen_pool_avail(sram->pool) < gen_pool_size(sram->pool))
 		dev_dbg(&pdev->dev, "removed while SRAM allocated\n");
 
-	gen_pool_destroy(sram->pool);
-
 	if (sram->clk)
 		clk_disable_unprepare(sram->clk);
 
