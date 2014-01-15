fff421580f512fc044cc7421fdff31a7a6997350
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2014-01-14 20:20:43 -0800

    timers: Track total number of timers in list
    Currently, the tvec_base structure's ->active_timers field tracks only
    the non-deferrable timers, which means that even if ->active_timers is
    zero, there might well be deferrable timers in the list.  This commit
    therefore adds an ->all_timers field to track all the timers, whether
    deferrable or not.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Reviewed-by: Oleg Nesterov <oleg@redhat.com>
    Reviewed-by: Steven Rostedt <rostedt@goodmis.org>
    Tested-by: Mike Galbraith <bitbucket@online.de>

--- kernel/timer.c
+++ kernel/timer.c
@@ -81,6 +81,7 @@ struct tvec_base {
 	unsigned long timer_jiffies;
 	unsigned long next_timer;
 	unsigned long active_timers;
+	unsigned long all_timers;
 	struct tvec_root tv1;
 	struct tvec tv2;
 	struct tvec tv3;
@@ -392,6 +393,7 @@ static void internal_add_timer(struct tvec_base *base, struct timer_list *timer)
 			base->next_timer = timer->expires;
 		base->active_timers++;
 	}
+	base->all_timers++;
 }
 
 #ifdef CONFIG_TIMER_STATS
@@ -671,6 +673,7 @@ detach_expired_timer(struct timer_list *timer, struct tvec_base *base)
 	detach_timer(timer, true);
 	if (!tbase_get_deferrable(timer->base))
 		base->active_timers--;
+	base->all_timers--;
 }
 
 static int detach_if_pending(struct timer_list *timer, struct tvec_base *base,
@@ -685,6 +688,7 @@ static int detach_if_pending(struct timer_list *timer, struct tvec_base *base,
 		if (timer->expires == base->next_timer)
 			base->next_timer = base->timer_jiffies;
 	}
+	base->all_timers--;
 	return 1;
 }
 
@@ -1559,6 +1563,7 @@ static int init_timers_cpu(int cpu)
 	base->timer_jiffies = jiffies;
 	base->next_timer = base->timer_jiffies;
 	base->active_timers = 0;
+	base->all_timers = 0;
 	return 0;
 }
 
