7179ba52889bef7e5e23f72908270e1ab2b7fc6f
Michael Ellerman <mpe@ellerman.id.au>
2014-01-15 18:14:29 +1100

    powerpc: Implement arch_spin_is_locked() using arch_spin_value_unlocked()
    At a glance these are just the inverse of each other. The one subtlety
    is that arch_spin_value_unlocked() takes the lock by value, rather than
    as a pointer, which is important for the lockref code.
    
    On the other hand arch_spin_is_locked() doesn't really care, so
    implement it in terms of arch_spin_value_unlocked().
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/spinlock.h
+++ arch/powerpc/include/asm/spinlock.h
@@ -28,8 +28,6 @@
 #include <asm/synch.h>
 #include <asm/ppc-opcode.h>
 
-#define arch_spin_is_locked(x)		((x)->slock != 0)
-
 #ifdef CONFIG_PPC64
 /* use 0x800000yy when locked, where yy == CPU number */
 #ifdef __BIG_ENDIAN__
@@ -57,6 +55,11 @@
 static __always_inline int arch_spin_value_unlocked(arch_spinlock_t lock)
 {
 	return lock.slock == 0;
+}
+
+static inline int arch_spin_is_locked(arch_spinlock_t *lock)
+{
+	return !arch_spin_value_unlocked(*lock);
 }
 
 /*
