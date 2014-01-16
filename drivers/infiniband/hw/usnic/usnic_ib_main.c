61f78268936e781a104b4ac06b7e47d760800c40
Upinder Malhi <umalhi@cisco.com>
2014-01-15 17:02:37 -0800

    IB/usnic: Advertise usNIC devices as RDMA_NODE_USNIC_UDP
    usNIC default transport is UDP.  Hence, advertise RDMA_NODE_USNIC_UDP
    by default for usNIC devices.
    
    Signed-off-by: Upinder Malhi <umalhi@cisco.com>
    Signed-off-by: Roland Dreier <roland@purestorage.com>

--- drivers/infiniband/hw/usnic/usnic_ib_main.c
+++ drivers/infiniband/hw/usnic/usnic_ib_main.c
@@ -332,7 +332,7 @@ static void *usnic_ib_device_add(struct pci_dev *dev)
 	us_ibdev->pdev = dev;
 	us_ibdev->netdev = pci_get_drvdata(dev);
 	us_ibdev->ib_dev.owner = THIS_MODULE;
-	us_ibdev->ib_dev.node_type = RDMA_NODE_USNIC;
+	us_ibdev->ib_dev.node_type = RDMA_NODE_USNIC_UDP;
 	us_ibdev->ib_dev.phys_port_cnt = USNIC_IB_PORT_CNT;
 	us_ibdev->ib_dev.num_comp_vectors = USNIC_IB_NUM_COMP_VECTORS;
 	us_ibdev->ib_dev.dma_device = &dev->dev;
