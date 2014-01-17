f303b4ce8b386558b2b92aeb0c6af96685fcd4b2
Radim Krčmář <rkrcmar@redhat.com>
2014-01-17 20:52:42 +0100

    KVM: SVM: fix NMI window after iret
    We should open NMI window right after an iret, but SVM exits before it.
    We wanted to single step using the trap flag and then open it.
    (or we could emulate the iret instead)
    We don't do it since commit 3842d135ff2 (likely), because the iret exit
    handler does not request an event, so NMI window remains closed until
    the next exit.
    
    Fix this by making KVM_REQ_EVENT request in the iret handler.
    
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -2842,6 +2842,7 @@ static int iret_interception(struct vcpu_svm *svm)
 	clr_intercept(svm, INTERCEPT_IRET);
 	svm->vcpu.arch.hflags |= HF_IRET_MASK;
 	svm->nmi_iret_rip = kvm_rip_read(&svm->vcpu);
+	kvm_make_request(KVM_REQ_EVENT, &svm->vcpu);
 	return 1;
 }
 
