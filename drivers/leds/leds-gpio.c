4270a78d23eece0b25a13bff1e71d114ec547de4
Robin Gong <b38343@freescale.com>
2014-01-20 03:41:26 -0800

    leds: leds-gpio: add retain-state-suspended property
    Some gpio-leds need retain the state even in suspend, such as charger led.
    But this property missed in devicetree, add it.
    
    (cooloney@gmail.com: fold DT binding updates into this patch)
    
    Signed-off-by: Robin Gong <b38343@freescale.com>
    Signed-off-by: Bryan Wu <cooloney@gmail.com>

--- drivers/leds/leds-gpio.c
+++ drivers/leds/leds-gpio.c
@@ -204,6 +204,9 @@ static struct gpio_leds_priv *gpio_leds_create_of(struct platform_device *pdev)
 				led.default_state = LEDS_GPIO_DEFSTATE_OFF;
 		}
 
+		if (of_get_property(child, "retain-state-suspended", NULL))
+			led.retain_state_suspended = 1;
+
 		ret = create_gpio_led(&led, &priv->leds[priv->num_leds++],
 				      &pdev->dev, NULL);
 		if (ret < 0) {
