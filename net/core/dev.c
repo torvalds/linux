e27a2f839598e48bb345f9fc86d4d54c3944db50
Or Gerlitz <ogerlitz@mellanox.com>
2014-01-20 13:59:20 +0200

    net: Export gro_find_by_type helpers
    Export the gro_find_receive/complete_by_type helpers to they can be invoked
    by the gro callbacks of encapsulation protocols such as vxlan.
    
    Signed-off-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -3984,6 +3984,7 @@ struct packet_offload *gro_find_receive_by_type(__be16 type)
 	}
 	return NULL;
 }
+EXPORT_SYMBOL(gro_find_receive_by_type);
 
 struct packet_offload *gro_find_complete_by_type(__be16 type)
 {
@@ -3997,6 +3998,7 @@ struct packet_offload *gro_find_complete_by_type(__be16 type)
 	}
 	return NULL;
 }
+EXPORT_SYMBOL(gro_find_complete_by_type);
 
 static gro_result_t napi_skb_finish(gro_result_t ret, struct sk_buff *skb)
 {
