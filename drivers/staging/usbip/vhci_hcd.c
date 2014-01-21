3b92e8ae303f8df6a30690358876692d979bd6dc
Shuah Khan <shuah.kh@samsung.com>
2014-01-21 12:18:34 -0700

    staging/usbip: remove vhci_hcd vhci_hub_status change message
    When vhci_hcd is enabled, the following message floods the dmesg buffer.
    This message doesn't provide any useful information other than cluttering
    the dmesg buffer. Fix it by removing the message. There is another debug
    message in this routine that dumps detailed port status change information.
    
    [ 4062.716662] vhci_hcd: changed 0
    
    Signed-off-by: Shuah Khan <shuah.kh@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/usbip/vhci_hcd.c
+++ drivers/staging/usbip/vhci_hcd.c
@@ -205,8 +205,6 @@ static int vhci_hub_status(struct usb_hcd *hcd, char *buf)
 		}
 	}
 
-	pr_info("changed %d\n", changed);
-
 	if ((hcd->state == HC_STATE_SUSPENDED) && (changed == 1))
 		usb_hcd_resume_root_hub(hcd);
 
