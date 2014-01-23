813f020c5d16878486b9a103bd59831846652247
Jiri Pirko <jiri@resnulli.us>
2014-01-23 19:19:21 +0100

    rtnetlink: remove check for fill_slave_info in rtnl_have_link_slave_info
    This check is not needed because the same check is done before
    fill_slave_info is used in rtnl_link_slave_info_fill.
    Also, by removing this check, kernel will fillup IFLA_INFO_SLAVE_KIND
    even for slaves of masters which does not implement fill_slave_info.
    
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -482,8 +482,7 @@ static bool rtnl_have_link_slave_info(const struct net_device *dev)
 	struct net_device *master_dev;
 
 	master_dev = netdev_master_upper_dev_get((struct net_device *) dev);
-	if (master_dev && master_dev->rtnl_link_ops &&
-	    master_dev->rtnl_link_ops->fill_slave_info)
+	if (master_dev && master_dev->rtnl_link_ops)
 		return true;
 	return false;
 }
