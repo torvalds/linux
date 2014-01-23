45fb9c35b27c9982e9a55d04ed0a5230a2d0b306
Daniele Di Proietto <daniele.di.proietto@gmail.com>
2014-01-23 10:47:35 -0800

    openvswitch: Fix ovs_dp_cmd_msg_size()
    commit 43d4be9cb55f3bac5253e9289996fd9d735531db (openvswitch: Allow user space
    to announce ability to accept unaligned Netlink messages) introduced
    OVS_DP_ATTR_USER_FEATURES netlink attribute in datapath responses,
    but the attribute size was not taken into account in ovs_dp_cmd_msg_size().
    
    Signed-off-by: Daniele Di Proietto <daniele.di.proietto@gmail.com>
    Signed-off-by: Jesse Gross <jesse@nicira.com>

--- net/openvswitch/datapath.c
+++ net/openvswitch/datapath.c
@@ -1087,6 +1087,7 @@ static size_t ovs_dp_cmd_msg_size(void)
 	msgsize += nla_total_size(IFNAMSIZ);
 	msgsize += nla_total_size(sizeof(struct ovs_dp_stats));
 	msgsize += nla_total_size(sizeof(struct ovs_dp_megaflow_stats));
+	msgsize += nla_total_size(sizeof(u32)); /* OVS_DP_ATTR_USER_FEATURES */
 
 	return msgsize;
 }
