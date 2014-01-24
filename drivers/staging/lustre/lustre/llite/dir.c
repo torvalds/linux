8b9e418c013e8b671fc10108ab14243f0657bffd
Cédric Dufour - Idiap Research Institute <cedric.dufour@idiap.ch>
2014-01-24 20:57:05 +0100

    staging: lustre: fix quotactl permission denied (LU-4530)
    The changes introduced in commit 4b1a25f06b30b203 ("fix build when
    CONFIG_UIDGID_STRICT_TYPE_CHECKS is on") got the UID check the wrong way
    around, leading to "Permission denied" when a regular user attempts to
    retrieve his quota (lfs quota -u ...) but allowing him to retrieve other
    users quota.
    
    Full details at: https://jira.hpdd.intel.com/browse/LU-4530
    
    Cc: Peng Tao <tao.peng@emc.com>
    Cc: <stable@vger.kernel.org> # 3.12.x
    Cc: <stable@vger.kernel.org> # 3.13.x
    Signed-off-by: Cédric Dufour <cedric.dufour@idiap.ch>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/dir.c
+++ drivers/staging/lustre/lustre/llite/dir.c
@@ -1086,7 +1086,7 @@ static int quotactl_ioctl(struct ll_sb_info *sbi, struct if_quotactl *qctl)
 		break;
 	case Q_GETQUOTA:
 		if (((type == USRQUOTA &&
-		      uid_eq(current_euid(), make_kuid(&init_user_ns, id))) ||
+		      !uid_eq(current_euid(), make_kuid(&init_user_ns, id))) ||
 		     (type == GRPQUOTA &&
 		      !in_egroup_p(make_kgid(&init_user_ns, id)))) &&
 		    (!cfs_capable(CFS_CAP_SYS_ADMIN) ||
