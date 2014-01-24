5b60053cae8faa211c5e407ede1a1ee9110f3f26
Mauro Carvalho Chehab <m.chehab@samsung.com>
2014-01-24 06:31:03 -0300

    [media] drx-j: Avoid any regressions by preserving old behavior
    The version is initialized with zero at drx_driver.c. Keep it,
    in order to avoid the risk of causing any regression.
    
    While here, remove the drx_driver.h from drxj, as this is not
    required there.
    
    Acked-by: Devin Heitmueller <dheitmueller@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/dvb-frontends/drx39xyj/drxj.c
+++ drivers/media/dvb-frontends/drx39xyj/drxj.c
@@ -39,7 +39,6 @@ INCLUDE FILES
 
 #include "drxj.h"
 #include "drxj_map.h"
-#include "drx_driver.h"
 
 /*============================================================================*/
 /*=== DEFINES ================================================================*/
@@ -20982,7 +20981,7 @@ static int drx_ctrl_version(struct drx_demod_instance *demod,
 {
 	static char drx_driver_core_module_name[] = "Core driver";
 	static char drx_driver_core_version_text[] =
-	    DRX_VERSIONSTRING(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
+	    DRX_VERSIONSTRING(0, 0, 0);
 
 	static struct drx_version drx_driver_core_version;
 	static struct drx_version_list drx_driver_core_version_list;
@@ -21003,9 +21002,9 @@ static int drx_ctrl_version(struct drx_demod_instance *demod,
 	/* Always fill in the information of the driver SW . */
 	drx_driver_core_version.module_type = DRX_MODULE_DRIVERCORE;
 	drx_driver_core_version.module_name = drx_driver_core_module_name;
-	drx_driver_core_version.v_major = VERSION_MAJOR;
-	drx_driver_core_version.v_minor = VERSION_MINOR;
-	drx_driver_core_version.v_patch = VERSION_PATCH;
+	drx_driver_core_version.v_major = 0;
+	drx_driver_core_version.v_minor = 0;
+	drx_driver_core_version.v_patch = 0;
 	drx_driver_core_version.v_string = drx_driver_core_version_text;
 
 	drx_driver_core_version_list.version = &drx_driver_core_version;
