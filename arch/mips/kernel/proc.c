91119686f33928e2ced93eaedf4191cedbd0e827
Markos Chandras <markos.chandras@imgtec.com>
2014-01-27 15:10:40 +0000

    MIPS: kernel: proc: Add EVA to the list of CPU features
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>

--- arch/mips/kernel/proc.c
+++ arch/mips/kernel/proc.c
@@ -96,6 +96,7 @@ static int show_cpuinfo(struct seq_file *m, void *v)
 	if (cpu_has_mmips)	seq_printf(m, "%s", " micromips");
 	if (cpu_has_vz)		seq_printf(m, "%s", " vz");
 	if (cpu_has_msa)	seq_printf(m, "%s", " msa");
+	if (cpu_has_eva)	seq_printf(m, "%s", " eva");
 	seq_printf(m, "\n");
 
 	if (cpu_has_mmips) {
