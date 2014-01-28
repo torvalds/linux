b783fbc3a55691f978b9f78d552a0d7e7d2705ad
Johan Hedberg <johan.hedberg@intel.com>
2014-01-28 15:16:49 -0800

    Bluetooth: Refuse peer L2CAP address reading when not connected
    When we're not connected the peer address information is undefined. This
    patch fixes the remote address getting to return a proper error in case
    the state is anything else than BT_CONNECTED.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/l2cap_sock.c
+++ net/bluetooth/l2cap_sock.c
@@ -366,6 +366,9 @@ static int l2cap_sock_getname(struct socket *sock, struct sockaddr *addr,
 
 	BT_DBG("sock %p, sk %p", sock, sk);
 
+	if (peer && sk->sk_state != BT_CONNECTED)
+		return -ENOTCONN;
+
 	memset(la, 0, sizeof(struct sockaddr_l2));
 	addr->sa_family = AF_BLUETOOTH;
 	*len = sizeof(struct sockaddr_l2);
