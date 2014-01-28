6e4ae926d86530ec2a8bc462b97023022c8dbfc5
Li Zhong <zhong@linux.vnet.ibm.com>
2014-01-28 17:52:42 +0530

    powerpc/mm: Fix compile error of pgtable-ppc64.h
    commit fd120dc2e205d2318a8b47d6d8098b789e3af67d upstream.
    
    It seems that forward declaration couldn't work well with typedef, use
    struct spinlock directly to avoiding following build errors:
    
    In file included from include/linux/spinlock.h:81,
                     from include/linux/seqlock.h:35,
                     from include/linux/time.h:5,
                     from include/uapi/linux/timex.h:56,
                     from include/linux/timex.h:56,
                     from include/linux/sched.h:17,
                     from arch/powerpc/kernel/asm-offsets.c:17:
    include/linux/spinlock_types.h:76: error: redefinition of typedef 'spinlock_t'
    /root/linux-next/arch/powerpc/include/asm/pgtable-ppc64.h:563: note: previous declaration of 'spinlock_t' was here
    
    Signed-off-by: Li Zhong <zhong@linux.vnet.ibm.com>
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/powerpc/include/asm/pgtable-ppc64.h
+++ arch/powerpc/include/asm/pgtable-ppc64.h
@@ -560,9 +560,9 @@ extern void pmdp_invalidate(struct vm_area_struct *vma, unsigned long address,
 			    pmd_t *pmdp);
 
 #define pmd_move_must_withdraw pmd_move_must_withdraw
-typedef struct spinlock spinlock_t;
-static inline int pmd_move_must_withdraw(spinlock_t *new_pmd_ptl,
-					 spinlock_t *old_pmd_ptl)
+struct spinlock;
+static inline int pmd_move_must_withdraw(struct spinlock *new_pmd_ptl,
+					 struct spinlock *old_pmd_ptl)
 {
 	/*
 	 * Archs like ppc64 use pgtable to store per pmd
