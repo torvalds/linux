84db564aad45774ab64375ee019d5e7a42675b1f
Richard Guy Briggs <rgb@redhat.com>
2014-01-29 16:17:58 -0500

    audit: add arch field to seccomp event log
    The AUDIT_SECCOMP record looks something like this:
    
    type=SECCOMP msg=audit(1373478171.953:32775): auid=4325 uid=4325 gid=4325 ses=1 subj=unconfined_u:unconfined_r:unconfined_t:s0 pid=12381 comm="test" sig=31 syscall=231 compat=0 ip=0x39ea8bca89 code=0x0
    
    In order to determine what syscall 231 maps to, we need to have the arch= field right before it.
    
    To see the event, compile this test.c program:
    
    =====
    int main(void)
    {
            return seccomp_load(seccomp_init(SCMP_ACT_KILL));
    }
    =====
    
    gcc -g test.c -o test -lseccomp
    
    After running the program, find the record by:  ausearch --start recent -m SECCOMP -i
    
    Signed-off-by: Richard Guy Briggs <rgb@redhat.com>
    signed-off-by: Eric Paris <eparis@redhat.com>

--- kernel/auditsc.c
+++ kernel/auditsc.c
@@ -67,6 +67,7 @@
 #include <linux/binfmts.h>
 #include <linux/highmem.h>
 #include <linux/syscalls.h>
+#include <asm/syscall.h>
 #include <linux/capability.h>
 #include <linux/fs_struct.h>
 #include <linux/compat.h>
@@ -2488,11 +2489,9 @@ void __audit_seccomp(unsigned long syscall, long signr, int code)
 	if (unlikely(!ab))
 		return;
 	audit_log_task(ab);
-	audit_log_format(ab, " sig=%ld", signr);
-	audit_log_format(ab, " syscall=%ld", syscall);
-	audit_log_format(ab, " compat=%d", is_compat_task());
-	audit_log_format(ab, " ip=0x%lx", KSTK_EIP(current));
-	audit_log_format(ab, " code=0x%x", code);
+	audit_log_format(ab, " sig=%ld arch=%x syscall=%ld compat=%d ip=0x%lx code=0x%x",
+			 signr, syscall_get_arch(), syscall, is_compat_task(),
+			 KSTK_EIP(current), code);
 	audit_log_end(ab);
 }
 
