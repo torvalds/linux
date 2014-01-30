00cfab35e838986cf72222f981444dab704db687
Minchan Kim <minchan@kernel.org>
2014-01-30 15:45:55 -0800

    zsmalloc: add copyright
    Add my copyright to the zsmalloc source code which I maintain.
    
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 31fc00bb788ffde7d8d861d8b2bba798ab445992)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>

--- include/linux/zsmalloc.h
+++ include/linux/zsmalloc.h
@@ -2,6 +2,7 @@
  * zsmalloc memory allocator
  *
  * Copyright (C) 2011  Nitin Gupta
+ * Copyright (C) 2012, 2013 Minchan Kim
  *
  * This code is released using a dual license strategy: BSD/GPL
  * You can choose the license that better fits your requirements.
