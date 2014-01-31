ca2ed12f163bf430a425ec725ef76df5254b9f45
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2014-01-31 12:04:10 -0800

    HID: hid-sensor-hub: Processing for duplicate physical ids
    In HID sensor hub, HID physical ids are used to represent different sensors.
    For example physical id of 0x73 in usage page = 0x20, represents an
    accelerometer. The HID sensor hub driver uses this physical ids to create
    platform devices using MFD. There is 1:1 correspondence between an phy id and a
    client driver.
    
    But in some cases these physical ids are reused. There is a phy id 0xe1, which
    specifies a custom sensor, which can exist multiple times to represent various
    custom sensors. In this case there can be multiple instances of client MFD
    drivers, processing specific custom sensor. In this case when client driver
    looks for report id or a field index, it should still get the report id
    specific to its own type. This is also true for reports, they should be
    directed towards correct instance.  This change introduce a way to parse and
    tie physical devices to their correct instance.
    
    Summary of changes:
    - To get physical ids, use collections. If a collection of type=physical
      exist then use usage id as in the name of platform device name
    - As part of the platform data, we assign a hdsev instance, which has
      start and end of collection indexes. Using these indexes attributes
      can be tied to correct MFD client instances
    - When a report is received, call callback with correct hsdev instance.
      In this way using its private data stored as part of its registry, it
      can distinguish different sensors even when they have same physical and
      logical ids.
    
      This patch is co-authored with Archana Patni <archna.patni@intel.com>.
    
    Reported-by: Archana Patni <archana.patni@intel.com>
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Signed-off-by: Archana Patni <archana.patni@intel.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- include/linux/hid-sensor-hub.h
+++ include/linux/hid-sensor-hub.h
@@ -51,13 +51,15 @@ struct hid_sensor_hub_attribute_info {
  * @hdev:		Stores the hid instance.
  * @vendor_id:		Vendor id of hub device.
  * @product_id:		Product id of hub device.
- * @ref_cnt:		Number of MFD clients have opened this device
+ * @start_collection_index: Starting index for a phy type collection
+ * @end_collection_index: Last index for a phy type collection
  */
 struct hid_sensor_hub_device {
 	struct hid_device *hdev;
 	u32 vendor_id;
 	u32 product_id;
-	int ref_cnt;
+	int start_collection_index;
+	int end_collection_index;
 };
 
 /**
