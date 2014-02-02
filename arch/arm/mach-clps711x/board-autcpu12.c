6c41a9979c3f2d5c9cf3458dda3fdb6542535df8
Alexander Shiyan <shc_work@mail.ru>
2014-02-02 12:09:01 +0400

    ARM: clps711x: Migrate CLPS711X subarch to the new irqchip driver
    This patch remove old code and migrate Cirrus Logic CLPS711X subarch
    to the new irqchip driver.
    
    Signed-off-by: Alexander Shiyan <shc_work@mail.ru>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-clps711x/board-autcpu12.c
+++ arch/arm/mach-clps711x/board-autcpu12.c
@@ -265,14 +265,12 @@ static void __init autcpu12_init_late(void)
 MACHINE_START(AUTCPU12, "autronix autcpu12")
 	/* Maintainer: Thomas Gleixner */
 	.atag_offset	= 0x20000,
-	.nr_irqs	= CLPS711X_NR_IRQS,
 	.map_io		= clps711x_map_io,
 	.init_early	= clps711x_init_early,
 	.init_irq	= clps711x_init_irq,
 	.init_time	= clps711x_timer_init,
 	.init_machine	= autcpu12_init,
 	.init_late	= autcpu12_init_late,
-	.handle_irq	= clps711x_handle_irq,
 	.restart	= clps711x_restart,
 MACHINE_END
 
