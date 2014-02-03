3067bab1cb6e45063c602a731b9341651c4f2ddb
Geert Uytterhoeven <geert@linux-m68k.org>
2014-02-03 10:38:39 +0100

    m68k: Switch to asm-generic/barrier.h
    The generic nop() implementation is fine for m68k.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Acked-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/include/asm/barrier.h
+++
@@ -1,8 +0,0 @@
-#ifndef _M68K_BARRIER_H
-#define _M68K_BARRIER_H
-
-#define nop()		do { asm volatile ("nop"); barrier(); } while (0)
-
-#include <asm-generic/barrier.h>
-
-#endif /* _M68K_BARRIER_H */
