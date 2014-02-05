f3846266f593595632a07242fcbc6c24bc2ade68
Rajat Jain <rajatxjain@gmail.com>
2014-02-05 15:24:57 -0800

    fuse: fix "uninitialized variable" warning
    Fix the following warning:
    
    In file included from include/linux/fs.h:16:0,
                     from fs/fuse/fuse_i.h:13,
                     from fs/fuse/file.c:9:
    fs/fuse/file.c: In function 'fuse_file_poll':
    include/linux/rbtree.h:82:28: warning: 'parent' may be used
    uninitialized in this function [-Wmaybe-uninitialized]
    fs/fuse/file.c:2592:27: note: 'parent' was declared here
    
    Signed-off-by: Rajat Jain <rajatxjain@gmail.com>
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -2772,7 +2772,7 @@ static void fuse_register_polled_file(struct fuse_conn *fc,
 {
 	spin_lock(&fc->lock);
 	if (RB_EMPTY_NODE(&ff->polled_node)) {
-		struct rb_node **link, *parent;
+		struct rb_node **link, *uninitialized_var(parent);
 
 		link = fuse_find_polled_node(fc, ff->kh, &parent);
 		BUG_ON(*link);
