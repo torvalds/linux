1e700846d6facd4e42a56978a2f3d9379ec788d0
Maxime Ripard <maxime.ripard@free-electrons.com>
2014-02-07 22:20:39 +0100

    misc: eeprom: sunxi: Change compatibles
    The Allwinner A10 compatibles were following a slightly different compatible
    patterns than the rest of the SoCs for historical reasons. Change the compatibles
    to match the other pattern in the SID driver for consistency.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/eeprom/sunxi_sid.c
+++ drivers/misc/eeprom/sunxi_sid.c
@@ -95,7 +95,7 @@ static int sunxi_sid_remove(struct platform_device *pdev)
 }
 
 static const struct of_device_id sunxi_sid_of_match[] = {
-	{ .compatible = "allwinner,sun4i-sid", .data = (void *)16},
+	{ .compatible = "allwinner,sun4i-a10-sid", .data = (void *)16},
 	{ .compatible = "allwinner,sun7i-a20-sid", .data = (void *)512},
 	{/* sentinel */},
 };
