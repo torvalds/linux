90db6919f5f1614d1b7a92052445506bc6c564d2
Kent Overstreet <kmo@daterainc.com>
2014-02-10 17:26:40 -0800

    bcache: Fix discard granularity
    blk_stack_limits() doesn't like a discard granularity of 0.
    
    Signed-off-by: Kent Overstreet <kmo@daterainc.com>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -843,6 +843,7 @@ static int bcache_device_init(struct bcache_device *d, unsigned block_size,
 	q->limits.max_segment_size	= UINT_MAX;
 	q->limits.max_segments		= BIO_MAX_PAGES;
 	q->limits.max_discard_sectors	= UINT_MAX;
+	q->limits.discard_granularity	= 512;
 	q->limits.io_min		= block_size;
 	q->limits.logical_block_size	= block_size;
 	q->limits.physical_block_size	= block_size;
