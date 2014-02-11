867d49e286e632491b3d29b0a4ef0e9441c4fc88
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2014-02-11 16:57:10 +0100

    usb3503: write to PDS and CFG1 register instead of updating

--- drivers/usb/misc/usb3503.c
+++ drivers/usb/misc/usb3503.c
@@ -126,9 +126,7 @@ static int usb3503_connect(struct usb3503 *hub)
 
 		/* PDS : Set the ports which are disabled in self-powered mode. */
 		if (hub->port_off_mask) {
-			err = regmap_update_bits(hub->regmap, USB3503_PDS,
-					hub->port_off_mask,
-					hub->port_off_mask);
+			err = regmap_write(hub->regmap, USB3503_PDS, hub->port_off_mask);
 			if (err < 0) {
 				dev_err(dev, "PDS failed (%d)\n", err);
 				return err;
@@ -136,9 +134,7 @@ static int usb3503_connect(struct usb3503 *hub)
 		}
 
 		/* CFG1 : Set SELF_BUS_PWR, this enables self-powered operation. */
-		err = regmap_update_bits(hub->regmap, USB3503_CFG1,
-					 USB3503_SELF_BUS_PWR,
-					 USB3503_SELF_BUS_PWR);
+		err = regmap_write(hub->regmap, USB3503_CFG1, USB3503_SELF_BUS_PWR);
 		if (err < 0) {
 			dev_err(dev, "CFG1 failed (%d)\n", err);
 			return err;
