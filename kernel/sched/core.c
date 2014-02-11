e9e7cb38c21c80c82af4b16608bb4c8c5ec6a28e
Juri Lelli <juri.lelli@gmail.com>
2014-02-11 09:24:26 +0100

    sched/core: Fix sched_rt_global_validate
    Don't compare sysctl_sched_rt_runtime against sysctl_sched_rt_period if
    the former is equal to RUNTIME_INF, otherwise disabling -rt bandwidth
    management (with CONFIG_RT_GROUP_SCHED=n) fails.
    
    Cc: Ingo Molnar <mingo@redhat.com>
    Signed-off-by: Juri Lelli <juri.lelli@gmail.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1392107067-19907-2-git-send-email-juri.lelli@gmail.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7475,7 +7475,8 @@ static int sched_rt_global_validate(void)
 	if (sysctl_sched_rt_period <= 0)
 		return -EINVAL;
 
-	if (sysctl_sched_rt_runtime > sysctl_sched_rt_period)
+	if ((sysctl_sched_rt_runtime != RUNTIME_INF) &&
+		(sysctl_sched_rt_runtime > sysctl_sched_rt_period))
 		return -EINVAL;
 
 	return 0;
