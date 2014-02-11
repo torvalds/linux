a5934804a834f525c9e6289935ceef65b952b101
Peter Hurley <peter@hurleysoftware.com>
2014-02-11 11:49:58 -0500

    n_tty: Fix poll() when TIME_CHAR and MIN_CHAR == 0
    Commit eafbe67f84761d787802e5113d895a316b6292fe,
      n_tty: Refactor input_available_p() by call site
    broke poll() when TIME_CHAR(tty) and MIN_CHAR(tty) are both 0.
    
    When TIME_CHAR and MIN_CHAR are both 0, input is available if the
    read_cnt is 1 (not 0).
    
    Reported-by: Eric Dumazet <edumazet@google.com>
    Tested-by: Eric Dumazet <edumazet@google.com>
    Reported-by: Stephane Eranian <eranian@google.com>
    Tested-by: David Ahern <dsahern@gmail.com>
    Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: Peter Hurley <peter@hurleysoftware.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/n_tty.c
+++ drivers/tty/n_tty.c
@@ -1896,7 +1896,7 @@ err:
 static inline int input_available_p(struct tty_struct *tty, int poll)
 {
 	struct n_tty_data *ldata = tty->disc_data;
-	int amt = poll && !TIME_CHAR(tty) ? MIN_CHAR(tty) : 1;
+	int amt = poll && !TIME_CHAR(tty) && MIN_CHAR(tty) ? MIN_CHAR(tty) : 1;
 
 	if (ldata->icanon && !L_EXTPROC(tty)) {
 		if (ldata->canon_head != ldata->read_tail)
