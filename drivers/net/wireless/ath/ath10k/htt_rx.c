716ae53c56cf51a64a56aa94d60240b9dff4a112
Janusz Dziedzic <janusz.dziedzic@tieto.com>
2014-02-13 17:50:00 +0200

    ath10k: pass frames with invalid peer status to upper layer
    Pass frames with invalid peer status to upper layer.
    Next mac80211 will validate frames and drop if required.
    This is required to detect spurious frames and pass
    this info to user mode (detect CLASS2 CLASS3 frames from
    nonauthenticated/nonassociated stations).
    
    Signed-off-by: Janusz Dziedzic <janusz.dziedzic@tieto.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath10k/htt_rx.c
+++ drivers/net/wireless/ath/ath10k/htt_rx.c
@@ -948,6 +948,7 @@ static void ath10k_htt_rx_handler(struct ath10k_htt *htt,
 
 			if (status != HTT_RX_IND_MPDU_STATUS_OK &&
 			    status != HTT_RX_IND_MPDU_STATUS_TKIP_MIC_ERR &&
+			    status != HTT_RX_IND_MPDU_STATUS_ERR_INV_PEER &&
 			    !htt->ar->monitor_enabled) {
 				ath10k_dbg(ATH10K_DBG_HTT,
 					   "htt rx ignoring frame w/ status %d\n",
