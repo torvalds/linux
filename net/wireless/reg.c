fb5c96368fa306dae0f79d0078d2d4e505278204
Janusz Dziedzic <janusz.dziedzic@tieto.com>
2014-02-14 08:54:01 +0100

    cfg80211: regulatory: allow user to set world regdomain
    Allow to set world regulatory domain in case of user
    request (iw reg set 00).
    
    Signed-off-by: Janusz Dziedzic <janusz.dziedzic@tieto.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/wireless/reg.c
+++ net/wireless/reg.c
@@ -2362,9 +2362,6 @@ static int reg_set_rd_user(const struct ieee80211_regdomain *rd,
 {
 	const struct ieee80211_regdomain *intersected_rd = NULL;
 
-	if (is_world_regdom(rd->alpha2))
-		return -EINVAL;
-
 	if (!regdom_changes(rd->alpha2))
 		return -EALREADY;
 
