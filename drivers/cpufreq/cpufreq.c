1c0ca90207d61e4868043b5bbbbd7cc0bb1ac974
Viresh Kumar <viresh.kumar@linaro.org>
2014-02-14 16:30:41 +0530

    cpufreq: don't call cpufreq_update_policy() on CPU addition
    cpufreq_update_policy() is called from two places currently. From a
    workqueue handled queued from cpufreq_bp_resume() for boot CPU and
    from cpufreq_cpu_callback() whenever a CPU is added.
    
    The first one makes sure that boot CPU is running on the frequency
    present in policy->cpu. But we don't really need a call from
    cpufreq_cpu_callback(), because we always call cpufreq_driver->init()
    (which will set policy->cur correctly) whenever first CPU of any
    policy is added back. And so every policy structure is guaranteed to
    have the right frequency in policy->cur.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2175,7 +2175,6 @@ static int cpufreq_cpu_callback(struct notifier_block *nfb,
 		switch (action & ~CPU_TASKS_FROZEN) {
 		case CPU_ONLINE:
 			__cpufreq_add_dev(dev, NULL, frozen);
-			cpufreq_update_policy(cpu);
 			break;
 
 		case CPU_DOWN_PREPARE:
