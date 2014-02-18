2ee881b74e1fa96bfe0c7d2be11f6de69bc145e9
Arnd Bergmann <arnd@arndb.de>
2014-02-18 16:14:20 +0100

    serial: pxa: fix build with !SERIAL_PXA_CONSOLE
    When CONFIG_SERIAL_PXA_CONSOLE is disabled, the serial_pxa_get_poll_char
    and serial_pxa_put_poll_char functions are not defined, and we can't
    reference them.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/pxa.c
+++ drivers/tty/serial/pxa.c
@@ -778,7 +778,7 @@ static struct uart_ops serial_pxa_pops = {
 	.request_port	= serial_pxa_request_port,
 	.config_port	= serial_pxa_config_port,
 	.verify_port	= serial_pxa_verify_port,
-#ifdef CONFIG_CONSOLE_POLL
+#if defined(CONFIG_CONSOLE_POLL) && defined(CONFIG_SERIAL_PXA_CONSOLE)
 	.poll_get_char = serial_pxa_get_poll_char,
 	.poll_put_char = serial_pxa_put_poll_char,
 #endif
