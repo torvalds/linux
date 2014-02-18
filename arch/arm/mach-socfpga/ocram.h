a4043370d4d113c273c68df82ad67b753ae31264
Thor Thayer <tthayer@altera.com>
2014-02-18 16:34:13 -0600

    FogBugz #184650: Cleanup Altera license headers.
    A few files have incorrect headers and some files do not have
    any headers. The biggest issue is that some files say "GPLv2 or
    later". The "or later" part is the problem.
    
    V2:
    Remove asterisks from date.
    Correct lower portion of header
    
    Signed-off-by: Thor Thayer <tthayer@altera.com>

--- arch/arm/mach-socfpga/ocram.h
+++ arch/arm/mach-socfpga/ocram.h
@@ -1,10 +1,8 @@
 /*
- *  arch/arm/mach-socfpga/ocram.h
+ * Copyright Altera Corporation (C) 2014. All rights reserved.
  *
- * Copyright (C) 2014 Altera Corporation
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms and conditions of the GNU General Public License,
  * version 2, as published by the Free Software Foundation.
  *
  * This program is distributed in the hope it will be useful, but WITHOUT
