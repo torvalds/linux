2c6a92b0097464e08caaa1caeb8baa9d470ab990
Justin Maggard <jmaggard10@gmail.com>
2014-02-20 08:48:07 -0800

    btrfs: wake up transaction thread upon remount
    Now that we can adjust the commit interval with a remount, we need
    to wake up the transaction thread or else he will continue to sleep
    until the previous transaction interval has elapsed before waking
    up.  So, if we go from a large commit interval to something smaller,
    the transaction thread will not wake up until the large interval has
    expired.  This also causes the cleaner thread to stay sleeping, since
    it gets woken up by the transaction thread.
    
    Fix it by simply waking up the transaction thread during a remount.
    
    Signed-off-by: Justin Maggard <jmaggard10@gmail.com>
    Signed-off-by: Josef Bacik <jbacik@fb.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -1479,6 +1479,7 @@ static int btrfs_remount(struct super_block *sb, int *flags, char *data)
 		sb->s_flags &= ~MS_RDONLY;
 	}
 out:
+	wake_up_process(fs_info->transaction_kthread);
 	btrfs_remount_cleanup(fs_info, old_opts);
 	return 0;
 
