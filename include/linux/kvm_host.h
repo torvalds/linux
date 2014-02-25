f3f710bc64e121c10c67ce58c893d3bc8c72abe4
Cornelia Huck <cornelia.huck@de.ibm.com>
2014-02-25 12:48:01 +0100

    KVM: Bump KVM_MAX_IRQ_ROUTES for s390
    The maximum number for irq routes is currently 1024, which is a bit on
    the small size for s390: We support up to 4 x 64k virtual devices with
    up to 64 queues, and we need one route for each of the queues if we want
    to operate it via irqfd.
    
    Let's bump this to 4k on s390 for now, as this at least covers the saner
    setups.
    
    We need to find a more general solution, though, as we can't just grow
    the routing table indefinitly.
    
    Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Cornelia Huck <cornelia.huck@de.ibm.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -922,7 +922,11 @@ static inline int mmu_notifier_retry(struct kvm *kvm, unsigned long mmu_seq)
 
 #ifdef CONFIG_HAVE_KVM_IRQ_ROUTING
 
+#ifdef CONFIG_S390
+#define KVM_MAX_IRQ_ROUTES 4096 //FIXME: we can have more than that...
+#else
 #define KVM_MAX_IRQ_ROUTES 1024
+#endif
 
 int kvm_setup_default_irq_routing(struct kvm *kvm);
 int kvm_set_irq_routing(struct kvm *kvm,
