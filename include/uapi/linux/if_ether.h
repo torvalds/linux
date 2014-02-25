2ebe21fdde1c92ef1654f23d29194145be0e1ddc
Neil Jerram <Neil.Jerram@metaswitch.com>
2014-02-25 11:17:25 +0000

    net: order MPLS ethertypes numerically
    All ethertypes other than ETH_P_MPLS_UC, ETH_P_MPLS_MC and
    ETH_P_ATMMPOA were already ordered numerically.  This commit moves
    those three ETH_P_... values into correct numerical order too.
    
    Signed-off-by: Neil Jerram <Neil.Jerram@metaswitch.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/uapi/linux/if_ether.h
+++ include/uapi/linux/if_ether.h
@@ -68,11 +68,11 @@
 #define ETH_P_SLOW	0x8809		/* Slow Protocol. See 802.3ad 43B */
 #define ETH_P_WCCP	0x883E		/* Web-cache coordination protocol
 					 * defined in draft-wilson-wrec-wccp-v2-00.txt */
-#define ETH_P_PPP_DISC	0x8863		/* PPPoE discovery messages     */
-#define ETH_P_PPP_SES	0x8864		/* PPPoE session messages	*/
 #define ETH_P_MPLS_UC	0x8847		/* MPLS Unicast traffic		*/
 #define ETH_P_MPLS_MC	0x8848		/* MPLS Multicast traffic	*/
 #define ETH_P_ATMMPOA	0x884c		/* MultiProtocol Over ATM	*/
+#define ETH_P_PPP_DISC	0x8863		/* PPPoE discovery messages     */
+#define ETH_P_PPP_SES	0x8864		/* PPPoE session messages	*/
 #define ETH_P_LINK_CTL	0x886c		/* HPNA, wlan link local tunnel */
 #define ETH_P_ATMFATE	0x8884		/* Frame-based ATM Transport
 					 * over Ethernet
