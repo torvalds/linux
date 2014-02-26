2a0c772f1967565d5b71a98012e74d5a7bddc1c4
Ross Zwisler <ross.zwisler@linux.intel.com>
2014-02-26 12:06:51 -0700

    x86: Use clflushopt in drm_clflush_page
    If clflushopt is available on the system, use it instead of clflush in
    drm_clflush_page.
    
    Signed-off-by: Ross Zwisler <ross.zwisler@linux.intel.com>
    Link: http://lkml.kernel.org/r/1393441612-19729-4-git-send-email-ross.zwisler@linux.intel.com
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- drivers/gpu/drm/drm_cache.c
+++ drivers/gpu/drm/drm_cache.c
@@ -32,6 +32,12 @@
 #include <drm/drmP.h>
 
 #if defined(CONFIG_X86)
+
+/*
+ * clflushopt is an unordered instruction which needs fencing with mfence or
+ * sfence to avoid ordering issues.  For drm_clflush_page this fencing happens
+ * in the caller.
+ */
 static void
 drm_clflush_page(struct page *page)
 {
@@ -44,7 +50,7 @@ drm_clflush_page(struct page *page)
 
 	page_virtual = kmap_atomic(page);
 	for (i = 0; i < PAGE_SIZE; i += size)
-		clflush(page_virtual + i);
+		clflushopt(page_virtual + i);
 	kunmap_atomic(page_virtual);
 }
 
