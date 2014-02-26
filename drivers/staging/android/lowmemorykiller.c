058dbde928597e7a8bd04e28e77e5cfc4270591d
Vinayak Menon <vinayakm.list@gmail.com>
2014-02-27 00:36:22 +0530

    staging: android: lowmemorykiller: neglect swap cached pages in other_file
    With ZRAM enabled it is observed that lowmemory killer
    doesn't trigger properly. swap cached pages are
    accounted in NR_FILE, and lowmemorykiller considers
    this as reclaimable and adds to other_file. But these
    pages can't be reclaimed unless lowmemorykiller triggers.
    So subtract swap pages from other_file.
    
    Signed-off-by: Vinayak Menon <vinayakm.list@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -88,7 +88,8 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	int array_size = ARRAY_SIZE(lowmem_adj);
 	int other_free = global_page_state(NR_FREE_PAGES) - totalreserve_pages;
 	int other_file = global_page_state(NR_FILE_PAGES) -
-						global_page_state(NR_SHMEM);
+						global_page_state(NR_SHMEM) -
+						total_swapcache_pages();
 
 	if (lowmem_adj_size < array_size)
 		array_size = lowmem_adj_size;
