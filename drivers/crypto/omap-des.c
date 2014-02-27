e78f91932d6005adc41e4244294fca66cdbf4d90
Jingoo Han <jg1.han@samsung.com>
2014-02-27 20:32:35 +0900

    crypto: omap-des - Use SIMPLE_DEV_PM_OPS macro
    Use SIMPLE_DEV_PM_OPS macro in order to make the code simpler.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/omap-des.c
+++ drivers/crypto/omap-des.c
@@ -1196,9 +1196,7 @@ static int omap_des_resume(struct device *dev)
 }
 #endif
 
-static const struct dev_pm_ops omap_des_pm_ops = {
-	SET_SYSTEM_SLEEP_PM_OPS(omap_des_suspend, omap_des_resume)
-};
+static SIMPLE_DEV_PM_OPS(omap_des_pm_ops, omap_des_suspend, omap_des_resume);
 
 static struct platform_driver omap_des_driver = {
 	.probe	= omap_des_probe,
