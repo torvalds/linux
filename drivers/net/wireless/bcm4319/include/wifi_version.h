724bdb33d60f9db4df1d02d082fa535683e189a2
gwl <gwl@rock-chips.com>
2014-02-27 17:00:53 +0800

    wifi: remove unused wifi drivers we add before.

--- drivers/net/wireless/bcm4319/include/wifi_version.h
+++
@@ -1,9 +0,0 @@
-#ifndef WIFI_VERSION_H
-#define WIFI_VERSION_H
-
-/*
- * Broadcom BCM4319 driver version.
- */
-#define BCM4319_DRV_VERSION "2.00"
-
-#endif /* WIFI_BCM4319_VERSION_H */
--- drivers/net/wireless/bcm4329/include/wifi_version.h
+++
@@ -1,9 +0,0 @@
-#ifndef WIFI_VERSION_H
-#define WIFI_VERSION_H
-
-/*
- * Broadcom BCM4329 driver version.
- */
-#define BCM4329_DRV_VERSION "2.00"
-
-#endif /* WIFI_BCM4329_VERSION_H */
