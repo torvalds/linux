23c058201fe36adf1a225281739b3ec31ec4e858
Bjørn Mork <bjorn@mork.no>
2014-02-27 14:23:55 +0100

    usb: hub: usb_ext_cap_descriptor.bmAttributes is le32
    Better check the correct bit on big endian systems too. Shuts
    up the following sparse __CHECK_ENDIAN__ warning:
    
      .../hub.c:3965:32: warning: restricted __le32 degrades to integer
    
    Signed-off-by: Bjørn Mork <bjorn@mork.no>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -3962,7 +3962,7 @@ static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 	connect_type = usb_get_hub_port_connect_type(udev->parent,
 			udev->portnum);
 
-	if ((udev->bos->ext_cap->bmAttributes & USB_BESL_SUPPORT) ||
+	if ((udev->bos->ext_cap->bmAttributes & cpu_to_le32(USB_BESL_SUPPORT)) ||
 			connect_type == USB_PORT_CONNECT_TYPE_HARD_WIRED) {
 		udev->usb2_hw_lpm_allowed = 1;
 		usb_set_usb2_hardware_lpm(udev, 1);
