92cb6f3c5533832aa70a7dc9326ef21270169583
Paolo Bonzini <pbonzini@redhat.com>
2014-02-27 22:54:11 +0100

    kvm, vmx: Really fix lazy FPU on nested guest
    commit 1b385cbdd74aa803e966e01e5fe49490d6044e30 upstream.
    
    Commit e504c9098ed6 (kvm, vmx: Fix lazy FPU on nested guest, 2013-11-13)
    highlighted a real problem, but the fix was subtly wrong.
    
    nested_read_cr0 is the CR0 as read by L2, but here we want to look at
    the CR0 value reflecting L1's setup.  In other words, L2 might think
    that TS=0 (so nested_read_cr0 has the bit clear); but if L1 is actually
    running it with TS=1, we should inject the fault into L1.
    
    The effective value of CR0 in L2 is contained in vmcs12->guest_cr0, use
    it.
    
    Fixes: e504c9098ed6acd9e1079c5e10e4910724ad429f
    Reported-by: Kashyap Chamarty <kchamart@redhat.com>
    Reported-by: Stefan Bader <stefan.bader@canonical.com>
    Tested-by: Kashyap Chamarty <kchamart@redhat.com>
    Tested-by: Anthoine Bourgeois <bourgeois@bertin.fr>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -6644,7 +6644,7 @@ static bool nested_vmx_exit_handled(struct kvm_vcpu *vcpu)
 		else if (is_page_fault(intr_info))
 			return enable_ept;
 		else if (is_no_device(intr_info) &&
-			 !(nested_read_cr0(vmcs12) & X86_CR0_TS))
+			 !(vmcs12->guest_cr0 & X86_CR0_TS))
 			return 0;
 		return vmcs12->exception_bitmap &
 				(1u << (intr_info & INTR_INFO_VECTOR_MASK));
