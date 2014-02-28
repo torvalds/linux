8303d769ea9e9626c4f0c3bd13e35e904a1253ab
Lars-Peter Clausen <lars@metafoo.de>
2014-02-28 08:31:02 +0100

    ASoC: Remove unused 'reg2' field from soc_enum struct
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- include/sound/soc.h
+++ include/sound/soc.h
@@ -1101,7 +1101,6 @@ struct soc_mreg_control {
 /* enumerated kcontrol */
 struct soc_enum {
 	unsigned short reg;
-	unsigned short reg2;
 	unsigned char shift_l;
 	unsigned char shift_r;
 	unsigned int items;
