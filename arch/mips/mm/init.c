e2a9e5ad719fb424ab3c30520733aa0e8fbcf1ce
Paul Burton <paul.burton@imgtec.com>
2014-03-03 12:08:40 +0000

    MIPS: add kmap_noncoherent to wire a cached non-coherent TLB entry
    This is identical to kmap_coherent apart from the cache coherency
    attribute used for the TLB entry, so kmap_coherent is abstracted to
    kmap_prot which is then called for both kmap_coherent &
    kmap_noncoherent. This will be used by a subsequent patch.
    
    Suggested-by: Leonid Yegoshin <leonid.yegoshin@imgtec.com>
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>

--- arch/mips/mm/init.c
+++ arch/mips/mm/init.c
@@ -114,7 +114,7 @@ static void __init kmap_coherent_init(void)
 static inline void kmap_coherent_init(void) {}
 #endif
 
-void *kmap_coherent(struct page *page, unsigned long addr)
+static void *__kmap_pgprot(struct page *page, unsigned long addr, pgprot_t prot)
 {
 	enum fixed_addresses idx;
 	unsigned long vaddr, flags, entrylo;
@@ -133,7 +133,7 @@ void *kmap_coherent(struct page *page, unsigned long addr)
 	idx += in_interrupt() ? FIX_N_COLOURS : 0;
 #endif
 	vaddr = __fix_to_virt(FIX_CMAP_END - idx);
-	pte = mk_pte(page, PAGE_KERNEL);
+	pte = mk_pte(page, prot);
 #if defined(CONFIG_64BIT_PHYS_ADDR) && defined(CONFIG_CPU_MIPS32)
 	entrylo = pte.pte_high;
 #else
@@ -169,6 +169,16 @@ void *kmap_coherent(struct page *page, unsigned long addr)
 	EXIT_CRITICAL(flags);
 
 	return (void*) vaddr;
+}
+
+void *kmap_coherent(struct page *page, unsigned long addr)
+{
+	return __kmap_pgprot(page, addr, PAGE_KERNEL);
+}
+
+void *kmap_noncoherent(struct page *page, unsigned long addr)
+{
+	return __kmap_pgprot(page, addr, PAGE_KERNEL_NC);
 }
 
 void kunmap_coherent(void)
