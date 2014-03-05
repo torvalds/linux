073d8224d299528778e90773becd1e890953443c
Frederic Weisbecker <fweisbec@gmail.com>
2014-03-05 17:24:12 +0100

    arch: Remove stub cputime.h headers
    Many architectures have a stub cputime.h that only include the default
    cputime.h
    
    Lets remove the useless headers, we only need to mention that we want
    the default headers on the Kbuild files.
    
    Cc: Archs <linux-arch@vger.kernel.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Marcelo Tosatti <mtosatti@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Rik van Riel <riel@redhat.com>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>

--- arch/alpha/include/asm/cputime.h
+++
@@ -1,6 +0,0 @@
-#ifndef __ALPHA_CPUTIME_H
-#define __ALPHA_CPUTIME_H
-
-#include <asm-generic/cputime.h>
-
-#endif /* __ALPHA_CPUTIME_H */
--- arch/m32r/include/asm/cputime.h
+++
@@ -1,6 +0,0 @@
-#ifndef __M32R_CPUTIME_H
-#define __M32R_CPUTIME_H
-
-#include <asm-generic/cputime.h>
-
-#endif /* __M32R_CPUTIME_H */
