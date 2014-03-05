6aced33f4974ab240971fdb5e42d7bebacd8ee45
Josh Cartwright <joshc@codeaurora.org>
2014-03-05 13:33:08 -0600

    pinctrl: msm: drop wake_irqs bitmap
    Currently, the wake_irqs bitmap is used to track whether there are any
    gpio's which are configured as wake irqs, and uses this to determine
    whether or not to call enable_irq_wake()/disable_irq_wake() on the
    summary interrupt.
    
    However, the genirq core already handles this case, by maintaining a
    'wake_count' per irq_desc, and only calling into the controlling
    irq_chip when wake_count transitions 0 <-> 1.
    
    Drop this bitmap, and unconditionally call irq_set_irq_wake() on the
    summary interrupt.
    
    Signed-off-by: Josh Cartwright <joshc@codeaurora.org>
    Acked-by: Bjorn Andersson <bjorn.andersson@sonymobile.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-msm.c
+++ drivers/pinctrl/pinctrl-msm.c
@@ -50,7 +50,6 @@
  * @enabled_irqs:   Bitmap of currently enabled irqs.
  * @dual_edge_irqs: Bitmap of irqs that need sw emulated dual edge
  *                  detection.
- * @wake_irqs:      Bitmap of irqs with requested as wakeup source.
  * @soc;            Reference to soc_data of platform specific data.
  * @regs:           Base address for the TLMM register map.
  */
@@ -65,7 +64,6 @@ struct msm_pinctrl {
 
 	DECLARE_BITMAP(dual_edge_irqs, MAX_NR_GPIO);
 	DECLARE_BITMAP(enabled_irqs, MAX_NR_GPIO);
-	DECLARE_BITMAP(wake_irqs, MAX_NR_GPIO);
 
 	const struct msm_pinctrl_soc_data *soc;
 	void __iomem *regs;
@@ -810,22 +808,12 @@ static int msm_gpio_irq_set_wake(struct irq_data *d, unsigned int on)
 {
 	struct msm_pinctrl *pctrl;
 	unsigned long flags;
-	unsigned ngpio;
 
 	pctrl = irq_data_get_irq_chip_data(d);
-	ngpio = pctrl->chip.ngpio;
 
 	spin_lock_irqsave(&pctrl->lock, flags);
 
-	if (on) {
-		if (bitmap_empty(pctrl->wake_irqs, ngpio))
-			enable_irq_wake(pctrl->irq);
-		set_bit(d->hwirq, pctrl->wake_irqs);
-	} else {
-		clear_bit(d->hwirq, pctrl->wake_irqs);
-		if (bitmap_empty(pctrl->wake_irqs, ngpio))
-			disable_irq_wake(pctrl->irq);
-	}
+	irq_set_irq_wake(pctrl->irq, on);
 
 	spin_unlock_irqrestore(&pctrl->lock, flags);
 
