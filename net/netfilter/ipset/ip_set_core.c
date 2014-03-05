6843bc3c568128e8771ba35cfefe95b7ec1c93a8
Ilia Mirkin <imirkin@alum.mit.edu>
2014-03-05 07:55:10 +0100

    netfilter: ipset: move registration message to init from net_init
    Commit 1785e8f473 ("netfiler: ipset: Add net namespace for ipset") moved
    the initialization print into net_init, which can get called a lot due
    to namespaces. Move it back into init, reduce to pr_info.
    
    Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Signed-off-by: Jozsef Kadlecsik <kadlec@blackhole.kfki.hu>

--- net/netfilter/ipset/ip_set_core.c
+++ net/netfilter/ipset/ip_set_core.c
@@ -1945,7 +1945,6 @@ ip_set_net_init(struct net *net)
 		return -ENOMEM;
 	inst->is_deleted = 0;
 	rcu_assign_pointer(inst->ip_set_list, list);
-	pr_notice("ip_set: protocol %u\n", IPSET_PROTOCOL);
 	return 0;
 }
 
@@ -1996,6 +1995,7 @@ ip_set_init(void)
 		nfnetlink_subsys_unregister(&ip_set_netlink_subsys);
 		return ret;
 	}
+	pr_info("ip_set: protocol %u\n", IPSET_PROTOCOL);
 	return 0;
 }
 
