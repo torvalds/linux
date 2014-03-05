2cee2121db44cfeee206d0854bedd52344eea444
Axel Lin <axel.lin@ingics.com>
2014-03-05 18:02:43 +0800

    regulator: pfuze100: Add PFUZE200 support to Kconfig and module description
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Acked-by: Robin Gong <b38343@freescale.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/regulator/pfuze100-regulator.c
+++ drivers/regulator/pfuze100-regulator.c
@@ -527,6 +527,6 @@ static struct i2c_driver pfuze_driver = {
 module_i2c_driver(pfuze_driver);
 
 MODULE_AUTHOR("Robin Gong <b38343@freescale.com>");
-MODULE_DESCRIPTION("Regulator Driver for Freescale PFUZE100 PMIC");
+MODULE_DESCRIPTION("Regulator Driver for Freescale PFUZE100/PFUZE200 PMIC");
 MODULE_LICENSE("GPL v2");
 MODULE_ALIAS("i2c:pfuze100-regulator");
