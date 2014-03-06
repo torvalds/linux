d9a83d62b326574fb4831b64317a82a42642a9a2
David Howells <dhowells@redhat.com>
2014-03-06 13:35:59 +0000

    i2c: Add message transfer tracepoints for I2C
    Add tracepoints into the I2C message transfer function to retrieve the message
    sent or received.  The following config options must be turned on to make use
    of the facility:
    
    	CONFIG_FTRACE
    	CONFIG_ENABLE_DEFAULT_TRACERS
    
    The I2C tracepoint can be enabled thusly:
    
    	echo 1 >/sys/kernel/debug/tracing/events/i2c/enable
    
    and will dump messages that can be viewed in /sys/kernel/debug/tracing/trace
    that look like:
    
    	... i2c_write: i2c-5 #0 a=044 f=0000 l=2 [02-14]
    	... i2c_read: i2c-5 #1 a=044 f=0001 l=4
    	... i2c_reply: i2c-5 #1 a=044 f=0001 l=4 [33-00-00-00]
    	... i2c_result: i2c-5 n=2 ret=2
    
    formatted as:
    
    	i2c-<adapter-nr>
    	#<message-array-index>
    	a=<addr>
    	f=<flags>
    	l=<datalen>
    	n=<message-array-size>
    	ret=<result>
    	[<data>]
    
    The operation is done between the i2c_write/i2c_read lines and the i2c_reply
    and i2c_result lines so that if the hardware hangs, the trace buffer can be
    consulted to determine the problematic operation.
    
    The adapters to be traced can be selected by something like:
    
    	echo adapter_nr==1 >/sys/kernel/debug/tracing/events/i2c/filter
    
    These changes are based on code from Steven Rostedt.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: David Howells <dhowells@redhat.com>
    Reviewed-by: Steven Rostedt <rostedt@goodmis.org>
    [wsa: adapted path for 'enable' in the commit msg]
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/i2c-core.c
+++ drivers/i2c/i2c-core.c
@@ -48,10 +48,13 @@
 #include <linux/rwsem.h>
 #include <linux/pm_runtime.h>
 #include <linux/acpi.h>
+#include <linux/jump_label.h>
 #include <asm/uaccess.h>
 
 #include "i2c-core.h"
 
+#define CREATE_TRACE_POINTS
+#include <trace/events/i2c.h>
 
 /* core_lock protects i2c_adapter_idr, and guarantees
    that device detection, deletion of detected devices, and attach_adapter
@@ -61,6 +64,18 @@ static DEFINE_IDR(i2c_adapter_idr);
 
 static struct device_type i2c_client_type;
 static int i2c_detect(struct i2c_adapter *adapter, struct i2c_driver *driver);
+
+static struct static_key i2c_trace_msg = STATIC_KEY_INIT_FALSE;
+
+void i2c_transfer_trace_reg(void)
+{
+	static_key_slow_inc(&i2c_trace_msg);
+}
+
+void i2c_transfer_trace_unreg(void)
+{
+	static_key_slow_dec(&i2c_trace_msg);
+}
 
 /* ------------------------------------------------------------------------- */
 
@@ -1686,6 +1701,7 @@ static void __exit i2c_exit(void)
 	class_compat_unregister(i2c_adapter_compat_class);
 #endif
 	bus_unregister(&i2c_bus_type);
+	tracepoint_synchronize_unregister();
 }
 
 /* We must initialize early, because some subsystems register i2c drivers
@@ -1716,6 +1732,19 @@ int __i2c_transfer(struct i2c_adapter *adap, struct i2c_msg *msgs, int num)
 	unsigned long orig_jiffies;
 	int ret, try;
 
+	/* i2c_trace_msg gets enabled when tracepoint i2c_transfer gets
+	 * enabled.  This is an efficient way of keeping the for-loop from
+	 * being executed when not needed.
+	 */
+	if (static_key_false(&i2c_trace_msg)) {
+		int i;
+		for (i = 0; i < num; i++)
+			if (msgs[i].flags & I2C_M_RD)
+				trace_i2c_read(adap, &msgs[i], i);
+			else
+				trace_i2c_write(adap, &msgs[i], i);
+	}
+
 	/* Retry automatically on arbitration loss */
 	orig_jiffies = jiffies;
 	for (ret = 0, try = 0; try <= adap->retries; try++) {
@@ -1724,6 +1753,14 @@ int __i2c_transfer(struct i2c_adapter *adap, struct i2c_msg *msgs, int num)
 			break;
 		if (time_after(jiffies, orig_jiffies + adap->timeout))
 			break;
+	}
+
+	if (static_key_false(&i2c_trace_msg)) {
+		int i;
+		for (i = 0; i < ret; i++)
+			if (msgs[i].flags & I2C_M_RD)
+				trace_i2c_reply(adap, &msgs[i], i);
+		trace_i2c_result(adap, i, ret);
 	}
 
 	return ret;
