ebade5e833eda30f648d9bf4954f7d47a920b60f
Ingo Molnar <mingo@elte.hu>
2014-03-07 14:58:39 +0100

    serial: 8250: Clean up the locking for -rt
    In -RT the spin_lock_irqsave() does not spin but sleep if the lock is
    taken. Before that, local_irq_save() is invoked which disables
    interrupts even on -RT. Therefore local_irq_save() + spin_lock() does not
    work.
    In the ->sysrq and oops_in_progress case it is save to trylock the lock
    i.e. this is what we do now anyway except for ->sysrq where we assume
    that the lock is already taken.
    
    The spin_lock_irqsave() grabs the lock and disables the interrupts on
    vanilla (the same behavior) and on -RT it won't disable interrupts.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    [bigeasy: add a patch description]
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/8250/8250_core.c
+++ drivers/tty/serial/8250/8250_core.c
@@ -2886,14 +2886,10 @@ serial8250_console_write(struct console *co, const char *s, unsigned int count)
 
 	touch_nmi_watchdog();
 
-	local_irq_save(flags);
-	if (port->sysrq) {
-		/* serial8250_handle_irq() already took the lock */
-		locked = 0;
-	} else if (oops_in_progress) {
-		locked = spin_trylock(&port->lock);
-	} else
-		spin_lock(&port->lock);
+	if (port->sysrq || oops_in_progress)
+		locked = spin_trylock_irqsave(&port->lock, flags);
+	else
+		spin_lock_irqsave(&port->lock, flags);
 
 	/*
 	 *	First save the IER then disable the interrupts
@@ -2925,8 +2921,7 @@ serial8250_console_write(struct console *co, const char *s, unsigned int count)
 		serial8250_modem_status(up);
 
 	if (locked)
-		spin_unlock(&port->lock);
-	local_irq_restore(flags);
+		spin_unlock_irqrestore(&port->lock, flags);
 }
 
 static int __init serial8250_console_setup(struct console *co, char *options)
