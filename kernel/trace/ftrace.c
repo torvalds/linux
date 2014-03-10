db0fbadcbd0c288525ea9f76488b324642a78c7f
Jiri Slaby <jslaby@suse.cz>
2014-03-10 21:42:11 +0100

    ftrace: Fix compilation warning about control_ops_free
    With CONFIG_DYNAMIC_FTRACE=n, I see a warning:
    kernel/trace/ftrace.c:240:13: warning: 'control_ops_free' defined but not used
     static void control_ops_free(struct ftrace_ops *ops)
                 ^
    Move that function around to an already existing #ifdef
    CONFIG_DYNAMIC_FTRACE block as the function is used solely from the
    dynamic function tracing functions.
    
    Link: http://lkml.kernel.org/r/1394484131-5107-1-git-send-email-jslaby@suse.cz
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/trace/ftrace.c
+++ kernel/trace/ftrace.c
@@ -237,11 +237,6 @@ static int control_ops_alloc(struct ftrace_ops *ops)
 	return 0;
 }
 
-static void control_ops_free(struct ftrace_ops *ops)
-{
-	free_percpu(ops->disabled);
-}
-
 static void update_global_ops(void)
 {
 	ftrace_func_t func = ftrace_global_list_func;
@@ -2099,6 +2094,11 @@ static void ftrace_run_update_code(int command)
 static ftrace_func_t saved_ftrace_func;
 static int ftrace_start_up;
 static int global_start_up;
+
+static void control_ops_free(struct ftrace_ops *ops)
+{
+	free_percpu(ops->disabled);
+}
 
 static void ftrace_startup_enable(int command)
 {
