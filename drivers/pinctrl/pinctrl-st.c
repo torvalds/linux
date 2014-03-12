bcca9220b211b9668c23c9d8ccb0a3d29716482b
Srinivas Kandagatla <srinivas.kandagatla@st.com>
2014-03-12 13:35:05 +0000

    pinctrl: st: Fix error check for of_irq_to_resource usage
    This patch fixes an error check while using of_irq_to_resource.
    of_irq_to_resource returns non-zero interrupt number on success and zero
    on error. The driver was using error check is wrong way.
    
    Without this patch the driver will configure interrupt zero if there is
    no interrupt specified in the node.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@st.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-st.c
+++ drivers/pinctrl/pinctrl-st.c
@@ -1569,7 +1569,7 @@ static int st_gpiolib_register_bank(struct st_pinctrl *info,
 	 *	[irqN]----> [gpio-bank (n)]
 	 */
 
-	if (!of_irq_to_resource(np, 0, &irq_res)) {
+	if (of_irq_to_resource(np, 0, &irq_res)) {
 		gpio_irq = irq_res.start;
 		irq_set_chained_handler(gpio_irq, st_gpio_irq_handler);
 		irq_set_handler_data(gpio_irq, bank);
