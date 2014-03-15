ff29a86ec9143ff1aa1901ae60f06f4980981df4
Jakub Kicinski <kubakici@wp.pl>
2014-03-15 14:55:16 +0000

    ixgbe: never generate both software and hardware timestamps
    skb_tx_timestamp() does not report software time stamp
    if SKBTX_IN_PROGRESS is set. According to timestamping.txt
    software time stamps are a fallback and should not be
    generated if hardware time stamp is provided.
    
    Move call to skb_tx_timestamp() after setting
    SKBTX_IN_PROGRESS.
    
    Signed-off-by: Jakub Kicinski <kubakici@wp.pl>
    Tested-by: Phil Schmitt <phillip.j.schmitt@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -7042,8 +7042,6 @@ netdev_tx_t ixgbe_xmit_frame_ring(struct sk_buff *skb,
 		tx_flags |= IXGBE_TX_FLAGS_SW_VLAN;
 	}
 
-	skb_tx_timestamp(skb);
-
 	if (unlikely(skb_shinfo(skb)->tx_flags & SKBTX_HW_TSTAMP)) {
 		skb_shinfo(skb)->tx_flags |= SKBTX_IN_PROGRESS;
 		tx_flags |= IXGBE_TX_FLAGS_TSTAMP;
@@ -7053,6 +7051,8 @@ netdev_tx_t ixgbe_xmit_frame_ring(struct sk_buff *skb,
 		adapter->ptp_tx_start = jiffies;
 		schedule_work(&adapter->ptp_tx_work);
 	}
+
+	skb_tx_timestamp(skb);
 
 #ifdef CONFIG_PCI_IOV
 	/*
