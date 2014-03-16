3dde47c63188ca20ec4c2a3540b9d0847ff72c75
Ana Rey <anarey@gmail.com>
2014-03-16 20:44:25 +0100

    staging: rtl8187se: Remove unused functions 'rsn_authen_cipher_suite' and 'ext_ieee80211_send_beacon_wq'
    Remove unused functions rsn_authen_cipher_suite() and
    ext_ieee80211_send_beacon_wq() from ieee80211_softmac.c.
    
    Fix sparse warnings:
    drivers/staging/rtl8187se/ieee80211/ieee80211_softmac.c:27:11: warning: ‘rsn_authen_cipher_suite’ defined but not used [-Wunused-variable]
    drivers/staging/rtl8187se/ieee80211/ieee80211_softmac.c:327:13: warning: ‘ext_ieee80211_send_beacon_wq’ defined but not used [-Wunused-function]
    
    Signed-off-by: Ana Rey <anarey@gmail.com>
    Signed-off-by: Peter P Waskiewicz Jr <peter.p.waskiewicz.jr@intel.com>

--- drivers/staging/rtl8187se/ieee80211/ieee80211_softmac.c
+++ drivers/staging/rtl8187se/ieee80211/ieee80211_softmac.c
@@ -315,25 +315,6 @@ inline struct sk_buff *ieee80211_probe_req(struct ieee80211_device *ieee)
 
 struct sk_buff *ieee80211_get_beacon_(struct ieee80211_device *ieee);
 
-static void ext_ieee80211_send_beacon_wq(struct ieee80211_device *ieee)
-{
-	struct sk_buff *skb;
-
-	skb = ieee80211_get_beacon_(ieee);
-
-	if (skb){
-		softmac_mgmt_xmit(skb, ieee);
-		ieee->softmac_stats.tx_beacons++;
-		dev_kfree_skb_any(skb);
-	}
-
-	ieee->beacon_timer.expires = jiffies +
-		(MSECS( ieee->current_network.beacon_interval -5));
-
-	if(ieee->beacon_txing)
-		add_timer(&ieee->beacon_timer);
-}
-
 static void ieee80211_send_beacon(struct ieee80211_device *ieee)
 {
 	struct sk_buff *skb;
