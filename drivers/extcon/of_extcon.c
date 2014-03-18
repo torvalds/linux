ca48824117b3ceaa4e35a7c5d651b95c288308e6
Chanwoo Choi <cw00.choi@samsung.com>
2014-03-18 19:09:41 +0900

    extcon: of: Remove unnecessary function call by using the name of device_node
    This patch remove unnecessary function call in of_extcon_get_extcon_dev()
    by using the name of device_node structure.
    
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- drivers/extcon/of_extcon.c
+++ drivers/extcon/of_extcon.c
@@ -32,7 +32,6 @@ struct extcon_dev *of_extcon_get_extcon_dev(struct device *dev, int index)
 {
 	struct device_node *node;
 	struct extcon_dev *edev;
-	struct platform_device *extcon_parent_dev;
 
 	if (!dev->of_node) {
 		dev_dbg(dev, "device does not have a device node entry\n");
@@ -46,16 +45,9 @@ struct extcon_dev *of_extcon_get_extcon_dev(struct device *dev, int index)
 		return ERR_PTR(-ENODEV);
 	}
 
-	extcon_parent_dev = of_find_device_by_node(node);
-	if (!extcon_parent_dev) {
-		dev_dbg(dev, "unable to find device by node\n");
-		return ERR_PTR(-EPROBE_DEFER);
-	}
-
-	edev = extcon_get_extcon_dev(dev_name(&extcon_parent_dev->dev));
+	edev = extcon_get_extcon_dev(node->name);
 	if (!edev) {
-		dev_dbg(dev, "unable to get extcon device : %s\n",
-				dev_name(&extcon_parent_dev->dev));
+		dev_dbg(dev, "unable to get extcon device : %s\n", node->name);
 		return ERR_PTR(-ENODEV);
 	}
 
