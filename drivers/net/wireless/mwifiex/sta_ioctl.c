52250cbee7f62400140295a632e0ffbbe5b083ca
Amitkumar Karwar <akarwar@marvell.com>
2014-03-20 16:23:50 -0700

    mwifiex: use timeout variant for wait_event_interruptible
    It has been observed that system hangs during suspend, if host
    sleep activation fails due to a missing interrupt from firmware.
    Use timeout variant, so that the thread will be woken up when
    timer expires.
    
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Bing Zhao <bzhao@marvell.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/mwifiex/sta_ioctl.c
+++ drivers/net/wireless/mwifiex/sta_ioctl.c
@@ -520,8 +520,9 @@ int mwifiex_enable_hs(struct mwifiex_adapter *adapter)
 		return false;
 	}
 
-	if (wait_event_interruptible(adapter->hs_activate_wait_q,
-				     adapter->hs_activate_wait_q_woken)) {
+	if (wait_event_interruptible_timeout(adapter->hs_activate_wait_q,
+					     adapter->hs_activate_wait_q_woken,
+					     (10 * HZ)) <= 0) {
 		dev_err(adapter->dev, "hs_activate_wait_q terminated\n");
 		return false;
 	}
