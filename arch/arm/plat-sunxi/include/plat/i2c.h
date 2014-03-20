b443d507c409ff8e2c303b019d1294cc5848a44f
Jonathan Liu <net147@gmail.com>
2014-03-20 23:53:06 +1100

    i2c-sunxi: use standard mode I2C speed of 100 kbit/s for I2C[1-4]
    Standard mode I2C speed is 100 kbit/s and should be used instead of
    200 kbit/s which is non-standard.
    
    Signed-off-by: Jonathan Liu <net147@gmail.com>

--- arch/arm/plat-sunxi/include/plat/i2c.h
+++ arch/arm/plat-sunxi/include/plat/i2c.h
@@ -194,10 +194,10 @@ Foscl is clock SCL;standard mode:100KHz or fast mode:400KHz
 #define TWI4_BASE_ADDR_END    (TWI4_BASE_ADDR_START + AW_TWI_ADDR_SIZE)
 
 #define I2C0_TRANSFER_SPEED     (400000)
-#define I2C1_TRANSFER_SPEED     (200000)
-#define I2C2_TRANSFER_SPEED     (200000)
-#define I2C3_TRANSFER_SPEED     (200000)
-#define I2C4_TRANSFER_SPEED     (200000)
+#define I2C1_TRANSFER_SPEED     (100000)
+#define I2C2_TRANSFER_SPEED     (100000)
+#define I2C3_TRANSFER_SPEED     (100000)
+#define I2C4_TRANSFER_SPEED     (100000)
 
 struct sunxi_i2c_platform_data {
 	int 		 bus_num;
