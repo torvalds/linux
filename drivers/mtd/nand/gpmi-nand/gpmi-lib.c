6189cccbe8d16d0ef175bd0dca18e3824ca01174
Huang Shijie <b32955@freescale.com>
2014-03-21 18:19:39 +0800

    mtd: gpmi: add gpmi_devdata{} to simplify the code
    More and more chips use the GPMI controller, but these chips may use different
    version of the IPs for GPMI and BCH. Different IPs have
     different features, such as the BCH's maximum ECC strength:
    
         imx23/imx28 -- the BCH's maximum ECC strength is 20
         imx6q       -- the BCH's maximum ECC strength is 40
         imx6sx      -- the BCH's maximum ECC strength is 62
    
    This patch does the following things:
    
      [1] add a new data structure, gpmi_devdata{}, to store the information for
          each IP. Besides the IP version, we store the following information:
             <1> BCH's maximum ECC strength.
             <2> the maximum chain delay in ns used by the EDO mode.
    
          but we may add more information in future.
    
      [2] add the gpmi_devdata_imx{23|28|6q} to replace the gpmi_ids.
    
      [3] simplify the code by using the ECC strength from gpmi_devdata, such as
          gpmi_check_ecc() and legacy_set_geometry();
    
      [4] use the maximum chain delay to initialize the EDO mode,
          see gpmi_compute_edo_timing().
    
      [5] rewrite the macros, such GPMI_IS_MX{23|28|6Q}.
    
    Signed-off-by: Huang Shijie <b32955@freescale.com>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/nand/gpmi-nand/gpmi-lib.c
+++ drivers/mtd/nand/gpmi-nand/gpmi-lib.c
@@ -861,7 +861,7 @@ static void gpmi_compute_edo_timing(struct gpmi_nand_data *this,
 	struct resources *r = &this->resources;
 	unsigned long rate = clk_get_rate(r->clock[0]);
 	int mode = this->timing_mode;
-	int dll_threshold = 16; /* in ns */
+	int dll_threshold = this->devdata->max_chain_delay;
 	unsigned long delay;
 	unsigned long clk_period;
 	int t_rea;
@@ -885,9 +885,6 @@ static void gpmi_compute_edo_timing(struct gpmi_nand_data *this,
 
 	/* [3] for GPMI_HW_GPMI_CTRL1 */
 	hw->wrn_dly_sel = BV_GPMI_CTRL1_WRN_DLY_SEL_NO_DELAY;
-
-	if (GPMI_IS_MX6Q(this))
-		dll_threshold = 12;
 
 	/*
 	 * Enlarge 10 times for the numerator and denominator in {3}.
