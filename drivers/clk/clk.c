c646cbf10fb3347ecda290dfce96b813a423ca07
Alex Elder <elder@linaro.org>
2014-03-21 06:43:56 -0500

    clk: support hardware-specific debugfs entries
    Add a new clk_ops->debug_init method to allow a clock hardware
    driver to populate the clock's debugfs directory with entries
    beyond those common for every clock.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Mike Turquette <mturquette@linaro.org>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -277,6 +277,10 @@ static int clk_debug_create_one(struct clk *clk, struct dentry *pdentry)
 	if (!d)
 		goto err_out;
 
+	if (clk->ops->debug_init)
+		if (clk->ops->debug_init(clk->hw, clk->dentry))
+			goto err_out;
+
 	ret = 0;
 	goto out;
 
