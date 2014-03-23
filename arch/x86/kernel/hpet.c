b712c8dae05931a76b6c17a4254f403798e6caef
Thomas Gleixner <tglx@linutronix.de>
2014-03-23 14:20:45 +0000

    x86: hpet: Use proper destructor for delayed work
    destroy_timer_on_stack() is hardly the right thing for a delayed
    work. We leak a tracking object for the work itself when DEBUG_OBJECTS
    is enabled.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vince Weaver <vincent.weaver@maine.edu>
    Cc: x86@kernel.org
    Link: http://lkml.kernel.org/r/20140323141940.034005322@linutronix.de
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/hpet.c
+++ arch/x86/kernel/hpet.c
@@ -699,7 +699,7 @@ static int hpet_cpuhp_notify(struct notifier_block *n,
 		/* FIXME: add schedule_work_on() */
 		schedule_delayed_work_on(cpu, &work.work, 0);
 		wait_for_completion(&work.complete);
-		destroy_timer_on_stack(&work.work.timer);
+		destroy_delayed_work_on_stack(&work.work);
 		break;
 	case CPU_DEAD:
 		if (hdev) {
