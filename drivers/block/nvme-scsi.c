ddcb776286c091189a7b928188112470ec7e9efc
Keith Busch <keith.busch@intel.com>
2014-03-24 10:03:56 -0400

    NVMe: Fix divide-by-zero in nvme_trans_io_get_num_cmds
    dev->max_hw_sectors may be zero to indicate the device has no limit on
    the number of sectors.  nvme_trans_do_nvme_io() should use the software
    limit, since this is guaranteed to be non-zero.
    
    Reported-by: Mundu <mundu2510@gmail.com>
    Signed-off-by: Keith Busch <keith.busch@intel.com>
    Signed-off-by: Matthew Wilcox <matthew.r.wilcox@intel.com>

--- drivers/block/nvme-scsi.c
+++ drivers/block/nvme-scsi.c
@@ -2044,7 +2044,7 @@ static int nvme_trans_do_nvme_io(struct nvme_ns *ns, struct sg_io_hdr *hdr,
 	struct nvme_command c;
 	u8 opcode = (is_write ? nvme_cmd_write : nvme_cmd_read);
 	u16 control;
-	u32 max_blocks = nvme_block_nr(ns, dev->max_hw_sectors);
+	u32 max_blocks = queue_max_hw_sectors(ns->queue);
 
 	num_cmds = nvme_trans_io_get_num_cmds(hdr, cdb_info, max_blocks);
 
