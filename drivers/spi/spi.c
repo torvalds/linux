1e25cd4729bd76662d02b142a6a7f8504bb6aea7
Mark Brown <broonie@linaro.org>
2014-03-25 19:28:22 +0000

    spi: Do not require a completion
    There is no real reason why we require transfers to have a completion and
    the only user of the completion now checks to see if one has been provided
    before using it so stop enforcing this. This makes it more convenient for
    drivers to chain multiple asynchronous transfers together.
    
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -1621,8 +1621,6 @@ static int __spi_validate(struct spi_device *spi, struct spi_message *message)
 
 	if (list_empty(&message->transfers))
 		return -EINVAL;
-	if (!message->complete)
-		return -EINVAL;
 
 	/* Half-duplex links include original MicroWire, and ones with
 	 * only one data pin like SPI_3WIRE (switches direction) or where
