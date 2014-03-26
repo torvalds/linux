566ef8924d815cb49fce18683efa09cad66640d8
Tushar Behera <tushar.behera@linaro.org>
2014-03-26 15:27:05 +0530

    tcp: Fix build error if IPV6 is not selected
    If CONFIG_IPV6=m is selected, we are getting following build errors.
    
    net/built-in.o: In function `tcp_is_local6':
    net/ipv4/tcp.c:3261: undefined reference to `rt6_lookup'
    
    Making the code conditional upon only CONFIG_IPV6=y fixes this issue.
    
    Signed-off-by: Tushar Behera <tushar.behera@linaro.org>
    CC: John Stultz <john.stultz@linaro.org>
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -3252,7 +3252,7 @@ static int tcp_is_local(struct net *net, __be32 addr) {
 	return rt->dst.dev && (rt->dst.dev->flags & IFF_LOOPBACK);
 }
 
-#if defined(CONFIG_IPV6) || defined(CONFIG_IPV6_MODULE)
+#if defined(CONFIG_IPV6)
 static int tcp_is_local6(struct net *net, struct in6_addr *addr) {
 	struct rt6_info *rt6 = rt6_lookup(net, addr, addr, 0, 0);
 	return rt6 && rt6->dst.dev && (rt6->dst.dev->flags & IFF_LOOPBACK);
@@ -3309,7 +3309,7 @@ restart:
 					continue;
 			}
 
-#if defined(CONFIG_IPV6) || defined(CONFIG_IPV6_MODULE)
+#if defined(CONFIG_IPV6)
 			if (family == AF_INET6) {
 				struct in6_addr *s6;
 				if (!inet->pinet6)
