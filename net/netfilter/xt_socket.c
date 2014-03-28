bd1bca4efb8e5146ebe5f486cc78092da8a18827
John Stultz <john.stultz@linaro.org>
2014-03-28 16:23:48 -0700

    ANDROID: netfilter: Build fixups - kuid/kguid changes & xt_socket_get/put_sk
    Fix up build kuid/kguid build issues in netfilter code.
    
    Also re-add the xt_socket_get/put_sk interfaces needed by xt_qtaguid.
    
    Change-Id: I7027fb840e109785bddffe8ea717b8d018b26d82
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- net/netfilter/xt_socket.c
+++ net/netfilter/xt_socket.c
@@ -35,6 +35,16 @@
 #include <net/netfilter/nf_conntrack.h>
 #endif
 
+void
+xt_socket_put_sk(struct sock *sk)
+{
+	if (sk->sk_state == TCP_TIME_WAIT)
+		inet_twsk_put(inet_twsk(sk));
+	else
+		sock_put(sk);
+}
+EXPORT_SYMBOL(xt_socket_put_sk);
+
 static int
 extract_icmp4_fields(const struct sk_buff *skb,
 		    u8 *protocol,
@@ -144,7 +154,7 @@ static bool xt_socket_sk_is_transparent(struct sock *sk)
 	}
 }
 
-static struct sock *xt_socket_lookup_slow_v4(struct net *net,
+struct sock *xt_socket_lookup_slow_v4(struct net *net,
 					     const struct sk_buff *skb,
 					     const struct net_device *indev)
 {
@@ -208,6 +218,7 @@ static struct sock *xt_socket_lookup_slow_v4(struct net *net,
 	return xt_socket_get_sock_v4(net, data_skb, doff, protocol, saddr,
 				     daddr, sport, dport, indev);
 }
+EXPORT_SYMBOL(xt_socket_lookup_slow_v4);
 
 static bool
 socket_match(const struct sk_buff *skb, struct xt_action_param *par,
@@ -344,7 +355,7 @@ xt_socket_get_sock_v6(struct net *net, struct sk_buff *skb, int doff,
 	return NULL;
 }
 
-static struct sock *xt_socket_lookup_slow_v6(struct net *net,
+struct sock *xt_socket_lookup_slow_v6(struct net *net,
 					     const struct sk_buff *skb,
 					     const struct net_device *indev)
 {
@@ -390,6 +401,7 @@ static struct sock *xt_socket_lookup_slow_v6(struct net *net,
 	return xt_socket_get_sock_v6(net, data_skb, doff, tproto, saddr, daddr,
 				     sport, dport, indev);
 }
+EXPORT_SYMBOL(xt_socket_lookup_slow_v6);
 
 static bool
 socket_mt6_v1_v2_v3(const struct sk_buff *skb, struct xt_action_param *par)
