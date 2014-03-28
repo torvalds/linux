aabcaa8b4994b935ae54a902afc7b0c3d89098a6
Andrea Merello <andrea.merello@gmail.com>
2014-03-28 18:12:08 +0100

    rtl8187: fix compile warning
    ANAPARAM3 register, defined in the rtl818x common register
    struct, is accessed as 16bit by rtl8187se and as 8bit by rtl8187b.
    Since I have no documentation about this, I can only stick to
    the reference code and to what is known to work.
    
    This issue has been addressed by a patch from Larry Finger
    that introduces an "union", in the register struct.
    In my last patch-set I applied it on the register struct, but
    I forget to update rtl8187 driver too.
    This patch does it.
    
    Suggested-by: Larry Finger <Larry.Finger@lwfinger.net> [ Original patch ]
    Signed-off-by: Andrea Merello <andrea.merello@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rtl818x/rtl8187/dev.c
+++ drivers/net/wireless/rtl818x/rtl8187/dev.c
@@ -592,7 +592,7 @@ static void rtl8187_set_anaparam(struct rtl8187_priv *priv, bool rfon)
 	rtl818x_iowrite32(priv, &priv->map->ANAPARAM, anaparam);
 	rtl818x_iowrite32(priv, &priv->map->ANAPARAM2, anaparam2);
 	if (priv->is_rtl8187b)
-		rtl818x_iowrite8(priv, &priv->map->ANAPARAM3, anaparam3);
+		rtl818x_iowrite8(priv, &priv->map->ANAPARAM3A, anaparam3);
 	reg &= ~RTL818X_CONFIG3_ANAPARAM_WRITE;
 	rtl818x_iowrite8(priv, &priv->map->CONFIG3, reg);
 	rtl818x_iowrite8(priv, &priv->map->EEPROM_CMD,
