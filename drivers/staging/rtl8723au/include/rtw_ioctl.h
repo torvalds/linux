364e30ebd2dbaccba430c603da03e68746eb932a
Larry Finger <Larry.Finger@lwfinger.net>
2014-03-28 21:37:41 -0500

    staging: r8723au: Add source files for new driver - part 4
    The Realtek USB device RTL8723AU is found in Lenovo Yoga 13 tablets.
    A driver for it has been available in a GitHub repo for several months.
    This commit contains the fourth part of source files. The source
    is arbitrarily split to avoid E-mail files that are too large.
    
    Jes Sorensen at RedHat has made many improvements to the vendor code,
    and he has been doing the testing. I do not have access to this device.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Cc: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

---
+++ drivers/staging/rtl8723au/include/rtw_ioctl.h
@@ -0,0 +1,26 @@
+/******************************************************************************
+ *
+ * Copyright(c) 2007 - 2011 Realtek Corporation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ ******************************************************************************/
+#ifndef _RTW_IOCTL_H_
+#define _RTW_IOCTL_H_
+
+#include <osdep_service.h>
+#include <drv_types.h>
+
+
+#if defined(CONFIG_WIRELESS_EXT)
+extern struct iw_handler_def  rtw_handlers_def;
+#endif
+
+#endif /*  #ifndef __INC_CEINFO_ */
