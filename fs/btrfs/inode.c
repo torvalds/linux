68bb462d42a963169bf7acbe106aae08c17129a5
Wang Shilong <wangsl.fnst@cn.fujitsu.com>
2014-04-01 18:01:42 +0800

    Btrfs: don't compress for a small write
    To compress a small file range(<=blocksize) that is not
    an inline extent can not save disk space at all. skip it can
    save us some cpu time.
    
    This patch can also fix wrong setting nocompression flag for
    inode, say a case when @total_in is 4096, and then we get
    @total_compressed 52,because we do aligment to page cache size
    firstly, and then we get into conclusion @total_in=@total_compressed
    thus we will clear this inode's compression flag.
    
    An exception comes from inserting inline extent failure but we
    still have @total_compressed < @total_in,so we will still reset
    inode's flag, this is ok, because we don't have good compression
    effect.
    
    Signed-off-by: Wang Shilong <wangsl.fnst@cn.fujitsu.com>
    Signed-off-by: Chris Mason <clm@fb.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -394,6 +394,14 @@ static noinline int compress_file_range(struct inode *inode,
 	    (start > 0 || end + 1 < BTRFS_I(inode)->disk_i_size))
 		btrfs_add_inode_defrag(NULL, inode);
 
+	/*
+	 * skip compression for a small file range(<=blocksize) that
+	 * isn't an inline extent, since it dosen't save disk space at all.
+	 */
+	if ((end - start + 1) <= blocksize &&
+	    (start > 0 || end + 1 < BTRFS_I(inode)->disk_i_size))
+		goto cleanup_and_bail_uncompressed;
+
 	actual_end = min_t(u64, isize, end + 1);
 again:
 	will_compress = 0;
