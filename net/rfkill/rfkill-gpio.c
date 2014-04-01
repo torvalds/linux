41d09df1e08bf18316d08cb73c5d08b5346c0c5d
Heikki Krogerus <heikki.krogerus@linux.intel.com>
2014-04-01 17:02:55 +0300

    net: rfkill: gpio: add ACPI IDs for a Broadcom bluetooth chip
    This adds ACPI IDs for Broadcom bluetooth chip BCM43241 used
    on various Baytrail based boards such as Lenovo Miix 2 and
    Asus Transformer Book T100TA.
    
    Acked-by: Alexandre Courbot <acourbot@nvidia.com>
    Signed-off-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/rfkill/rfkill-gpio.c
+++ net/rfkill/rfkill-gpio.c
@@ -171,11 +171,16 @@ static int rfkill_gpio_remove(struct platform_device *pdev)
 	return 0;
 }
 
+#ifdef CONFIG_ACPI
 static const struct acpi_device_id rfkill_acpi_match[] = {
+	{ "BCM2E1A", RFKILL_TYPE_BLUETOOTH },
+	{ "BCM2E39", RFKILL_TYPE_BLUETOOTH },
+	{ "BCM2E3D", RFKILL_TYPE_BLUETOOTH },
 	{ "BCM4752", RFKILL_TYPE_GPS },
 	{ "LNV4752", RFKILL_TYPE_GPS },
 	{ },
 };
+#endif
 
 static struct platform_driver rfkill_gpio_driver = {
 	.probe = rfkill_gpio_probe,
