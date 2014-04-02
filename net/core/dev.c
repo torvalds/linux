d0290214de712150b118a532ded378a29255893b
Jiri Pirko <jiri@resnulli.us>
2014-04-02 23:09:31 +0200

    net: add busy_poll device feature
    Currently there is no way how to find out if a device supports busy
    polling. So add a feature and make it dependent on ndo_busy_poll
    existence.
    
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -5696,6 +5696,13 @@ static netdev_features_t netdev_fix_features(struct net_device *dev,
 		}
 	}
 
+#ifdef CONFIG_NET_RX_BUSY_POLL
+	if (dev->netdev_ops->ndo_busy_poll)
+		features |= NETIF_F_BUSY_POLL;
+	else
+#endif
+		features &= ~NETIF_F_BUSY_POLL;
+
 	return features;
 }
 
