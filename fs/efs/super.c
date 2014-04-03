7a42d4b6a10c93ff15cff4f09c5a2d65877fa6ba
Fabian Frederick <fabf@skynet.be>
2014-04-03 14:49:33 -0700

    fs/efs/super.c: add __init to init_inodecache()
    init_inodecache is only called by __init init_efs_fs.
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/efs/super.c
+++ fs/efs/super.c
@@ -91,7 +91,7 @@ static void init_once(void *foo)
 	inode_init_once(&ei->vfs_inode);
 }
 
-static int init_inodecache(void)
+static int __init init_inodecache(void)
 {
 	efs_inode_cachep = kmem_cache_create("efs_inode_cache",
 				sizeof(struct efs_inode_info),
