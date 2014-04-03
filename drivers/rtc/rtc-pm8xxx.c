fda9909ddaab13fb7b35b94f204623c1f9571fc7
Josh Cartwright <joshc@codeaurora.org>
2014-04-03 14:50:14 -0700

    rtc: pm8xxx: move device_init_wakeup() before rtc_register
    Setup wakeup capability before rtc_register to ensure the rtc class core
    properly sets up our 'wakealarm' sysfs attribute.
    
    Signed-off-by: Josh Cartwright <joshc@codeaurora.org>
    Reviewed-by: Stephen Boyd <sboyd@codeaurora.org>
    Cc: Alessandro Zummo <a.zummo@towertech.it>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/rtc/rtc-pm8xxx.c
+++ drivers/rtc/rtc-pm8xxx.c
@@ -429,6 +429,8 @@ static int pm8xxx_rtc_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, rtc_dd);
 
+	device_init_wakeup(&pdev->dev, 1);
+
 	/* Register the RTC device */
 	rtc_dd->rtc = devm_rtc_device_register(&pdev->dev, "pm8xxx_rtc",
 					       &pm8xxx_rtc_ops, THIS_MODULE);
@@ -447,8 +449,6 @@ static int pm8xxx_rtc_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "Request IRQ failed (%d)\n", rc);
 		return rc;
 	}
-
-	device_init_wakeup(&pdev->dev, 1);
 
 	dev_dbg(&pdev->dev, "Probe success !!\n");
 
