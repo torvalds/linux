b21cc2f5fdfe22429501cd7040db0b2b2a9b29de
Xiubo Li <Li.Xiubo@freescale.com>
2014-04-04 15:10:28 +0800

    ASoC: esai: Add VF610+ compatibles support.
    Signed-off-by: Xiubo Li <Li.Xiubo@freescale.com>
    Acked-by: Nicolin Chen <Guangyu.Chen@freescale.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/fsl/fsl_esai.c
+++ sound/soc/fsl/fsl_esai.c
@@ -818,6 +818,7 @@ static int fsl_esai_probe(struct platform_device *pdev)
 
 static const struct of_device_id fsl_esai_dt_ids[] = {
 	{ .compatible = "fsl,imx35-esai", },
+	{ .compatible = "fsl,vf610-esai", },
 	{}
 };
 MODULE_DEVICE_TABLE(of, fsl_esai_dt_ids);
