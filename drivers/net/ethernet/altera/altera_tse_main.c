6b0f64fd19faf2c7174e810cf913f1dd142d342f
Vince Bridgers <vbridger@altera.com>
2014-04-08 20:04:48 -0500

    FogBugz #196682: Change ether_addr_copy(a,b) to memcpy(a,b,6)
    Upstreamed driver required use of ether_addr_copy(a,b) per
    community comments, backporting requires reversion to
    memcpy(a,b,6)
    
    Signed-off-by: Vince Bridgers <vbridger@altera.com>

--- drivers/net/ethernet/altera/altera_tse_main.c
+++ drivers/net/ethernet/altera/altera_tse_main.c
@@ -1375,7 +1375,7 @@ static int altera_tse_probe(struct platform_device *pdev)
 	/* get default MAC address from device tree */
 	macaddr = of_get_mac_address(pdev->dev.of_node);
 	if (macaddr)
-		ether_addr_copy(ndev->dev_addr, macaddr);
+		memcpy(ndev->dev_addr, macaddr, 6);
 	else
 		eth_hw_addr_random(ndev);
 
