8757ad65d30f009fe0beeb2d70d3cd834cb998f2
Matthew Wilcox <matthew.r.wilcox@intel.com>
2014-04-11 10:37:39 -0400

    NVMe: Update copyright headers
    Make the copyright dates accurate and remove the final paragraph that
    includes the address of the FSF.
    
    Signed-off-by: Matthew Wilcox <matthew.r.wilcox@intel.com>

--- include/uapi/linux/nvme.h
+++ include/uapi/linux/nvme.h
@@ -1,6 +1,6 @@
 /*
  * Definitions for the NVM Express interface
- * Copyright (c) 2011-2013, Intel Corporation.
+ * Copyright (c) 2011-2014, Intel Corporation.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -10,10 +10,6 @@
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 
- * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #ifndef _UAPI_LINUX_NVME_H
