b30ca4bdde773534daa5e04da946a00cabe44bb5
Peter Ujfalusi <peter.ujfalusi@ti.com>
2014-04-16 15:46:29 +0300

    ASoC: omap-hdmi-card: Use the same name for platform as the cpu_dai
    Now that the platform driver is registered with the cpu_dai's device we
    can use the same name for it.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/omap/omap-hdmi-card.c
+++ sound/soc/omap/omap-hdmi-card.c
@@ -33,7 +33,7 @@ static struct snd_soc_dai_link omap_hdmi_dai = {
 	.name = "HDMI",
 	.stream_name = "HDMI",
 	.cpu_dai_name = "omap-hdmi-audio-dai",
-	.platform_name = "omap-pcm-audio",
+	.platform_name = "omap-hdmi-audio-dai",
 	.codec_name = "hdmi-audio-codec",
 	.codec_dai_name = "hdmi-hifi",
 };
