12de375ec493ab1767d4a07dde823e63ae5edc21
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2014-04-17 09:33:19 -0700

    Revert "serial: 8250, disable "too much work" messages"
    This reverts commit f4f653e9875e573860e783fecbebde284a8626f5.
    
    Jiri writes:
    	No, please drop this one. We need a better solution as it turned
    	out that some boxes need 16k loops and it will increase with new
    	processors :(.
    
    Cc: Jiri Slaby <jslaby@suse.cz>
    Cc: Martin Pluskal <mpluskal@suse.com>
    Cc: Takashi Iwai <tiwai@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/8250/8250_core.c
+++ drivers/tty/serial/8250/8250_core.c
@@ -1601,7 +1601,8 @@ static irqreturn_t serial8250_interrupt(int irq, void *dev_id)
 		l = l->next;
 
 		if (l == i->head && pass_counter++ > PASS_LIMIT) {
-			pr_debug_ratelimited(
+			/* If we hit this, we're dead. */
+			printk_ratelimited(KERN_ERR
 				"serial8250: too much work for irq%d\n", irq);
 			break;
 		}
