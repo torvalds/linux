ff3c664505bf8a8334bca5045e87b85cfe4d2277
Neil Greatorex <neil@fatboyfat.co.uk>
2014-04-18 14:19:49 +0200

    irqchip: armada-370-xp: Fix releasing of MSIs
    Store the value of d->hwirq in a local variable as the real value is wiped out
    by calling irq_dispose_mapping. Without this patch, the armada_370_xp_free_msi
    function would always free MSI#0, no matter what was passed to it.
    
    Fixes: 31f614edb726fcc4d5aa0f2895fbdec9b04a3ca4 ('irqchip: armada-370-xp: implement MSI support')
    Cc: <stable@vger.kernel.org> # v3.13+
    Signed-off-by: Neil Greatorex <neil@fatboyfat.co.uk>
    Link: https://lkml.kernel.org/r/1397823593-1932-4-git-send-email-thomas.petazzoni@free-electrons.com
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Link: https://lkml.kernel.org/r/1397823593-1932-4-git-send-email-thomas.petazzoni@free-electrons.com
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- drivers/irqchip/irq-armada-370-xp.c
+++ drivers/irqchip/irq-armada-370-xp.c
@@ -158,8 +158,10 @@ static void armada_370_xp_teardown_msi_irq(struct msi_chip *chip,
 					   unsigned int irq)
 {
 	struct irq_data *d = irq_get_irq_data(irq);
+	unsigned long hwirq = d->hwirq;
+
 	irq_dispose_mapping(irq);
-	armada_370_xp_free_msi(d->hwirq);
+	armada_370_xp_free_msi(hwirq);
 }
 
 static int armada_370_xp_check_msi_device(struct msi_chip *chip, struct pci_dev *dev,
