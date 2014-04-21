a2ea6ed6a631e24d315b5f2bd468e6f4c7ff2e48
Christian Engelmayer <cengelma@gmx.at>
2014-04-21 20:46:40 +0200

    crypto: tcrypt - Fix potential leak in test_aead_speed() if crypto_alloc_aead() fails
    Fix a potential memory leak in the error handling of test_aead_speed(). In case
    crypto_alloc_aead() fails, the function returns without going through the
    centralized cleanup path. Reported by Coverity - CID 1163870.
    
    Signed-off-by: Christian Engelmayer <cengelma@gmx.at>
    Reviewed-by: Marek Vasut <marex@denx.de>
    Acked-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/tcrypt.c
+++ crypto/tcrypt.c
@@ -313,7 +313,7 @@ static void test_aead_speed(const char *algo, int enc, unsigned int sec,
 	if (IS_ERR(tfm)) {
 		pr_err("alg: aead: Failed to load transform for %s: %ld\n", algo,
 		       PTR_ERR(tfm));
-		return;
+		goto out_notfm;
 	}
 
 	req = aead_request_alloc(tfm, GFP_KERNEL);
@@ -391,6 +391,7 @@ static void test_aead_speed(const char *algo, int enc, unsigned int sec,
 
 out:
 	crypto_free_aead(tfm);
+out_notfm:
 	kfree(sg);
 out_nosg:
 	testmgr_free_buf(xoutbuf);
