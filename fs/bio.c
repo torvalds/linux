a6c39cb4f71e61aff19d07e2d0b26bb6e3548fae
Fabian Frederick <fabf@skynet.be>
2014-04-22 15:09:05 -0600

    fs/bio: remove bs paramater in biovec_create_pool
    bs is no longer used in biovec_create_pool since 9f060e2231ca96 ("block:
    Convert integrity to bvec_alloc_bs()")
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Cc: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- fs/bio.c
+++ fs/bio.c
@@ -1861,7 +1861,7 @@ EXPORT_SYMBOL_GPL(bio_trim);
  * create memory pools for biovec's in a bio_set.
  * use the global biovec slabs created for general use.
  */
-mempool_t *biovec_create_pool(struct bio_set *bs, int pool_entries)
+mempool_t *biovec_create_pool(int pool_entries)
 {
 	struct biovec_slab *bp = bvec_slabs + BIOVEC_MAX_IDX;
 
@@ -1924,7 +1924,7 @@ struct bio_set *bioset_create(unsigned int pool_size, unsigned int front_pad)
 	if (!bs->bio_pool)
 		goto bad;
 
-	bs->bvec_pool = biovec_create_pool(bs, pool_size);
+	bs->bvec_pool = biovec_create_pool(pool_size);
 	if (!bs->bvec_pool)
 		goto bad;
 
