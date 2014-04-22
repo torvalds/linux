b8eade24c9891b8f153c40cf310ef4696c873af9
Jingoo Han <jg1.han@samsung.com>
2014-04-22 14:45:32 +0900

    drm/exynos: use %pad for dma_addr_t
    Use %pad for dma_addr_t, because a dma_addr_t type can vary
    based on build options. So, it prevents possible build warnings
    in printks.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/exynos/exynos_drm_dmabuf.c
+++ drivers/gpu/drm/exynos/exynos_drm_dmabuf.c
@@ -263,7 +263,7 @@ struct drm_gem_object *exynos_dmabuf_prime_import(struct drm_device *drm_dev,
 	buffer->sgt = sgt;
 	exynos_gem_obj->base.import_attach = attach;
 
-	DRM_DEBUG_PRIME("dma_addr = 0x%x, size = 0x%lx\n", buffer->dma_addr,
+	DRM_DEBUG_PRIME("dma_addr = %pad, size = 0x%lx\n", &buffer->dma_addr,
 								buffer->size);
 
 	return &exynos_gem_obj->base;
