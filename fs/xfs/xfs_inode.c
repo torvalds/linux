2cd2ef6a300b1ac912bb515b75451585c3d33ea9
Christoph Hellwig <hch@lst.de>
2014-04-23 07:11:51 +1000

    xfs: rewrite the filestream allocator using the dentry cache
    In Linux we will always be able to find a parent inode for file that are
    undergoing I/O.  Use this to simply the file stream allocator by only
    keeping track of parent inodes.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -846,9 +846,9 @@ xfs_ialloc(
 
 	/* now we have set up the vfs inode we can associate the filestream */
 	if (filestreams) {
-		error = xfs_filestream_associate(pip, ip);
-		if (error < 0)
-			return -error;
+		error = xfs_filestream_associate(pip);
+		if (error)
+			return error;
 	}
 
 	*ipp = ip;
@@ -1694,16 +1694,6 @@ xfs_release(
 
 	if (!XFS_FORCED_SHUTDOWN(mp)) {
 		int truncated;
-
-		/*
-		 * If we are using filestreams, and we have an unlinked
-		 * file that we are processing the last close on, then nothing
-		 * will be able to reopen and write to this file. Purge this
-		 * inode from the filestreams cache so that it doesn't delay
-		 * teardown of the inode.
-		 */
-		if ((ip->i_d.di_nlink == 0) && xfs_inode_is_filestream(ip))
-			xfs_filestream_deassociate(ip);
 
 		/*
 		 * If we previously truncated this file and removed old data
@@ -2661,13 +2651,7 @@ xfs_remove(
 	if (error)
 		goto std_return;
 
-	/*
-	 * If we are using filestreams, kill the stream association.
-	 * If the file is still open it may get a new one but that
-	 * will get killed on last close in xfs_close() so we don't
-	 * have to worry about that.
-	 */
-	if (!is_dir && link_zero && xfs_inode_is_filestream(ip))
+	if (is_dir && xfs_inode_is_filestream(ip))
 		xfs_filestream_deassociate(ip);
 
 	return 0;
