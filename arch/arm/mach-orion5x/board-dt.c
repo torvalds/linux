ab5ab9dbe6bd480d17f23b3a33510ae33e6ab280
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-04-22 23:26:27 +0200

    ARM: orion5x: switch to DT interrupts and timer
    This commit switches the Orion5x platforms described through DT to use
    a DT-defined interrupt controller and timer.
    
    This involves:
    
     * Describing in the DT the bridge interrupt controller, which is a
       child interrupt controller to the main one, which is used for timer
       and watchdog interrupts.
    
     * Describing in the DT the timer.
    
     * Adding in the DT the interrupt specifications for the watchdog.
    
     * Selecting the ORION_IRQCHIP and ORION_TIMER drivers to be compiled.
    
     * Change board-dt.c to no longer have an ->init_time() callback,
       since the default callback will work fine: it calls
       clocksource_of_init() and of_clk_init(), as needed.
    
     * Implement a multi-IRQ handler for non-DT platforms in
       mach-orion5x/irq.c.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
    Link: https://lkml.kernel.org/r/1398202002-28530-24-git-send-email-thomas.petazzoni@free-electrons.com
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- arch/arm/mach-orion5x/board-dt.c
+++ arch/arm/mach-orion5x/board-dt.c
@@ -17,6 +17,7 @@
 #include <linux/cpu.h>
 #include <linux/mbus.h>
 #include <linux/clk-provider.h>
+#include <linux/clocksource.h>
 #include <asm/system_misc.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
@@ -35,17 +36,6 @@ static struct of_dev_auxdata orion5x_auxdata_lookup[] __initdata = {
 	OF_DEV_AUXDATA("marvell,orion-crypto", 0xf1090000, "mv_crypto", NULL),
 	{},
 };
-
-static void orion5x_dt_init_early(void)
-{
-	orion_time_set_base(TIMER_VIRT_BASE);
-}
-
-static void orion5x_dt_init_time(void)
-{
-	orion5x_timer_init();
-	of_clk_init(NULL);
-}
 
 static void __init orion5x_dt_init(void)
 {
@@ -86,9 +76,6 @@ static const char *orion5x_dt_compat[] = {
 DT_MACHINE_START(ORION5X_DT, "Marvell Orion5x (Flattened Device Tree)")
 	/* Maintainer: Thomas Petazzoni <thomas.petazzoni@free-electrons.com> */
 	.map_io		= orion5x_map_io,
-	.init_early	= orion5x_dt_init_early,
-	.init_irq	= orion_dt_init_irq,
-	.init_time	= orion5x_dt_init_time,
 	.init_machine	= orion5x_dt_init,
 	.restart	= orion5x_restart,
 	.dt_compat	= orion5x_dt_compat,
