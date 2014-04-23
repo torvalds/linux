7710cd2e240f507ec70897258b945c30e2ccb83a
Michal Kazior <michal.kazior@tieto.com>
2014-04-23 19:30:04 +0300

    ath10k: don't wait for device init if it crashed
    When warm resetting it's possible for device to
    crash during initialization. Instead of waiting 3
    seconds just return failure as soon as
    FW_IND_EVENT_PENDING is set.
    
    This speeds up device bootup and recovery in some
    cases.
    
    Signed-off-by: Michal Kazior <michal.kazior@tieto.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath10k/pci.c
+++ drivers/net/wireless/ath/ath10k/pci.c
@@ -2452,6 +2452,10 @@ static int ath10k_pci_wait_for_target_init(struct ath10k *ar)
 		if (val == 0xffffffff)
 			continue;
 
+		/* the device has crashed so don't bother trying anymore */
+		if (val & FW_IND_EVENT_PENDING)
+			break;
+
 		if (val & FW_IND_INITIALIZED)
 			break;
 
@@ -2467,6 +2471,12 @@ static int ath10k_pci_wait_for_target_init(struct ath10k *ar)
 	if (val == 0xffffffff) {
 		ath10k_err("failed to read device register, device is gone\n");
 		ret = -EIO;
+		goto out;
+	}
+
+	if (val & FW_IND_EVENT_PENDING) {
+		ath10k_warn("device has crashed during init\n");
+		ret = -ECOMM;
 		goto out;
 	}
 
