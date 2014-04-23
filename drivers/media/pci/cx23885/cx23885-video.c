200e0841ee4a05d06041fb25a8931a5dfb15c767
Hans Verkuil <hans.verkuil@cisco.com>
2014-04-23 07:18:12 -0300

    [media] cx23885: map invalid fields to a valid field
    If field format is not valid, map it as V4L2_FIELD_INTERLACED,
    instead of pretending to support an invalid format.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/pci/cx23885/cx23885-video.c
+++ drivers/media/pci/cx23885/cx23885-video.c
@@ -948,7 +948,8 @@ static int vidioc_try_fmt_vid_cap(struct file *file, void *priv,
 	case V4L2_FIELD_INTERLACED:
 		break;
 	default:
-		return -EINVAL;
+		field = V4L2_FIELD_INTERLACED;
+		break;
 	}
 
 	f->fmt.pix.field = field;
