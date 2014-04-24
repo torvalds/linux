ab0615e2d6fb074764a3e4d05f1326fa2fdb4627
Rabin Vincent <rabin@rab.in>
2014-04-24 23:28:57 +0200

    arm: use fixmap for text patching when text is RO
    Use fixmaps for text patching when the kernel text is read-only,
    inspired by x86.  This makes jump labels and kprobes work with the
    currently available CONFIG_DEBUG_SET_MODULE_RONX and the upcoming
    CONFIG_DEBUG_RODATA options.
    
    Signed-off-by: Rabin Vincent <rabin@rab.in>
    [kees: fixed up for merge with "arm: use generic fixmap.h"]
    [kees: added parse acquire/release annotations to pass C=1 builds]
    [kees: always use stop_machine to keep TLB flushing local]
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Acked-by: Nicolas Pitre <nico@linaro.org>

--- arch/arm/include/asm/fixmap.h
+++ arch/arm/include/asm/fixmap.h
@@ -11,6 +11,10 @@ enum fixed_addresses {
 	FIX_KMAP_BEGIN,
 	FIX_KMAP_END = FIX_KMAP_BEGIN + (KM_TYPE_NR * NR_CPUS) - 1,
 
+	/* Support writing RO kernel text via kprobes, jump labels, etc. */
+	FIX_TEXT_POKE0,
+	FIX_TEXT_POKE1,
+
 	__end_of_fixed_addresses
 };
 
