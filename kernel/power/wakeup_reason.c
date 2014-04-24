b4e6247778b0ba611812776821ad14e3e7d4aa10
Ruchi Kandoi <kandoiruchi@google.com>
2014-04-24 14:31:57 -0700

    ANDROID: Power: Changes the permission to read only for sysfs file /sys/kernel/wakeup_reasons/last_resume_reason
    Change-Id: I8ac568a7cb58c31decd379195de517ff3c6f9c65
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>

--- kernel/power/wakeup_reason.c
+++ kernel/power/wakeup_reason.c
@@ -34,7 +34,7 @@ static int irqcount;
 static struct kobject *wakeup_reason;
 static spinlock_t resume_reason_lock;
 
-static ssize_t reason_show(struct kobject *kobj, struct kobj_attribute *attr,
+static ssize_t last_resume_reason_show(struct kobject *kobj, struct kobj_attribute *attr,
 		char *buf)
 {
 	int irq_no, buf_offset = 0;
@@ -53,8 +53,7 @@ static ssize_t reason_show(struct kobject *kobj, struct kobj_attribute *attr,
 	return buf_offset;
 }
 
-static struct kobj_attribute resume_reason = __ATTR(last_resume_reason, 0666,
-		reason_show, NULL);
+static struct kobj_attribute resume_reason = __ATTR_RO(last_resume_reason);
 
 static struct attribute *attrs[] = {
 	&resume_reason.attr,
