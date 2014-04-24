8ac33ed3dc60126a1cb41c11bdd78d0371351d25
Daniel Vetter <daniel.vetter@ffwll.ch>
2014-04-24 23:54:54 +0200

    drm/i915/dp: Remove ->mode_set callback
    With all the preceding refactoring the dp mode_set callback only
    computes a bit of state (all derived from the pipe config) and also
    writes the eld. As long as we do that before we enable the audio bit
    or depend upon the correct value in intel_dp->DP we'll be fine.
    
    No other hw state is touched.
    
    We therefore only need to check that clearing intel_dp->DP is save.
    Which it is since when we re-enable we already mask out all the bits
    the link training code sets. And we need to keep on doing that so that
    the re-train loop walking over pre-emph/voltage-swing values still
    works properly.
    
    Reviewed-by: Naresh Kumar Kachhi <naresh.kumar.kachhi@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -925,7 +925,7 @@ static void ironlake_set_pll_cpu_edp(struct intel_dp *intel_dp)
 	udelay(500);
 }
 
-static void intel_dp_mode_set(struct intel_encoder *encoder)
+static void intel_dp_prepare(struct intel_encoder *encoder)
 {
 	struct drm_device *dev = encoder->base.dev;
 	struct drm_i915_private *dev_priv = dev->dev_private;
@@ -1902,6 +1902,8 @@ static void g4x_pre_enable_dp(struct intel_encoder *encoder)
 	struct intel_dp *intel_dp = enc_to_intel_dp(&encoder->base);
 	struct intel_digital_port *dport = dp_to_dig_port(intel_dp);
 
+	intel_dp_prepare(encoder);
+
 	/* Only ilk+ has port A */
 	if (dport->port == PORT_A) {
 		ironlake_set_pll_cpu_edp(intel_dp);
@@ -1957,6 +1959,8 @@ static void vlv_dp_pre_pll_enable(struct intel_encoder *encoder)
 		to_intel_crtc(encoder->base.crtc);
 	enum dpio_channel port = vlv_dport_to_channel(dport);
 	int pipe = intel_crtc->pipe;
+
+	intel_dp_prepare(encoder);
 
 	/* Program Tx lane resets to default */
 	mutex_lock(&dev_priv->dpio_lock);
@@ -4224,7 +4228,6 @@ intel_dp_init(struct drm_device *dev, int output_reg, enum port port)
 			 DRM_MODE_ENCODER_TMDS);
 
 	intel_encoder->compute_config = intel_dp_compute_config;
-	intel_encoder->mode_set = intel_dp_mode_set;
 	intel_encoder->disable = intel_disable_dp;
 	intel_encoder->get_hw_state = intel_dp_get_hw_state;
 	intel_encoder->get_config = intel_dp_get_config;
