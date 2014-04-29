f9fab7e4edfb2e14b870dcb902976325b21e9f1e
Jayachandran C <jchandra@broadcom.com>
2014-04-29 20:07:41 +0530

    MIPS: Netlogic: Fix uniprocessor compilation
    The macros in topology.h need CONFIG_SMP, and the uniprocessor compilation
    fails due to this. Wrap the macros in an ifdef so that uniprocessor works.
    
    Signed-off-by: Jayachandran C <jchandra@broadcom.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: http://patchwork.linux-mips.org/patch/6863/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/mach-netlogic/topology.h
+++ arch/mips/include/asm/mach-netlogic/topology.h
@@ -10,10 +10,12 @@
 
 #include <asm/mach-netlogic/multi-node.h>
 
+#ifdef CONFIG_SMP
 #define topology_physical_package_id(cpu)	cpu_to_node(cpu)
 #define topology_core_id(cpu)	(cpu_logical_map(cpu) / NLM_THREADS_PER_CORE)
 #define topology_thread_cpumask(cpu)		(&cpu_sibling_map[cpu])
 #define topology_core_cpumask(cpu)	cpumask_of_node(cpu_to_node(cpu))
+#endif
 
 #include <asm-generic/topology.h>
 
