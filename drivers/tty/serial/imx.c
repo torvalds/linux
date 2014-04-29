03a7cd983e18ad9b3a98ec62f2b7260b0c5d0e95
Anson Huang <b20788@freescale.com>
2014-04-29 12:33:52 +0800

    MLK-11368-01 serial: imx: add UART save/restore during DSM enter/exit
    When Mega/Fast mix is off, UART controller will lost power,
    so need to add save/restore to make it work after resume.
    
    Signed-off-by: Anson Huang <b20788@freescale.com>
    Signed-off-by: Huang Shijie <b32955@freescale.com>
    Signed-off-by: Fugang Duan <B38611@freescale.com>
    (cherry picked from commit: ea086a7cac1f900b19883e89b2bbb25e5d0b3de1)

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -216,6 +216,7 @@ struct imx_port {
 	unsigned int		tx_bytes;
 	unsigned int		dma_tx_nents;
 	wait_queue_head_t	dma_wait;
+	unsigned int            saved_reg[11];
 };
 
 struct imx_port_ucrs {
@@ -1824,6 +1825,19 @@ static int serial_imx_suspend(struct platform_device *dev, pm_message_t state)
 
 	uart_suspend_port(&imx_reg, &sport->port);
 
+	/* Save necessary regs */
+	sport->saved_reg[0] = readl(sport->port.membase + UCR1);
+	sport->saved_reg[1] = readl(sport->port.membase + UCR2);
+	sport->saved_reg[2] = readl(sport->port.membase + UCR3);
+	sport->saved_reg[3] = readl(sport->port.membase + UCR4);
+	sport->saved_reg[4] = readl(sport->port.membase + UFCR);
+	sport->saved_reg[5] = readl(sport->port.membase + UESC);
+	sport->saved_reg[6] = readl(sport->port.membase + UTIM);
+	sport->saved_reg[7] = readl(sport->port.membase + UBIR);
+	sport->saved_reg[8] = readl(sport->port.membase + UBMR);
+	sport->saved_reg[9] = readl(sport->port.membase + UBRC);
+	sport->saved_reg[10] = readl(sport->port.membase + IMX21_UTS);
+
 	return 0;
 }
 
@@ -1831,6 +1845,18 @@ static int serial_imx_resume(struct platform_device *dev)
 {
 	struct imx_port *sport = platform_get_drvdata(dev);
 	unsigned int val;
+
+	writel(sport->saved_reg[4], sport->port.membase + UFCR);
+	writel(sport->saved_reg[5], sport->port.membase + UESC);
+	writel(sport->saved_reg[6], sport->port.membase + UTIM);
+	writel(sport->saved_reg[7], sport->port.membase + UBIR);
+	writel(sport->saved_reg[8], sport->port.membase + UBMR);
+	writel(sport->saved_reg[9], sport->port.membase + UBRC);
+	writel(sport->saved_reg[10], sport->port.membase + IMX21_UTS);
+	writel(sport->saved_reg[0], sport->port.membase + UCR1);
+	writel(sport->saved_reg[1] | 0x1, sport->port.membase + UCR2);
+	writel(sport->saved_reg[2], sport->port.membase + UCR3);
+	writel(sport->saved_reg[3], sport->port.membase + UCR4);
 
 	/* disable wakeup from i.MX UART */
 	val = readl(sport->port.membase + UCR3);
