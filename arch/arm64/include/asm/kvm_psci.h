7d0f84aae9e231930985eaff63ac91b61aaa15d6
Anup Patel <anup.patel@linaro.org>
2014-04-29 11:24:16 +0530

    ARM/ARM64: KVM: Add base for PSCI v0.2 emulation
    Currently, the in-kernel PSCI emulation provides PSCI v0.1 interface to
    VCPUs. This patch extends current in-kernel PSCI emulation to provide
    PSCI v0.2 interface to VCPUs.
    
    By default, ARM/ARM64 KVM will always provide PSCI v0.1 interface for
    keeping the ABI backward-compatible.
    
    To select PSCI v0.2 interface for VCPUs, the user space (i.e. QEMU or
    KVMTOOL) will have to set KVM_ARM_VCPU_PSCI_0_2 feature when doing VCPU
    init using KVM_ARM_VCPU_INIT ioctl.
    
    Signed-off-by: Anup Patel <anup.patel@linaro.org>
    Signed-off-by: Pranavkumar Sawargaonkar <pranavkumar@linaro.org>
    Acked-by: Christoffer Dall <christoffer.dall@linaro.org>
    Acked-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- arch/arm64/include/asm/kvm_psci.h
+++ arch/arm64/include/asm/kvm_psci.h
@@ -18,6 +18,10 @@
 #ifndef __ARM64_KVM_PSCI_H__
 #define __ARM64_KVM_PSCI_H__
 
+#define KVM_ARM_PSCI_0_1	1
+#define KVM_ARM_PSCI_0_2	2
+
+int kvm_psci_version(struct kvm_vcpu *vcpu);
 bool kvm_psci_call(struct kvm_vcpu *vcpu);
 
 #endif /* __ARM64_KVM_PSCI_H__ */
