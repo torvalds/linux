8ad6b634928a25971dc42dce101808b1491f87ec
Anup Patel <anup.patel@linaro.org>
2014-04-29 11:24:19 +0530

    KVM: Add KVM_EXIT_SYSTEM_EVENT to user space API header
    Currently, we don't have an exit reason to notify user space about
    a system-level event (for e.g. system reset or shutdown) triggered
    by the VCPU. This patch adds exit reason KVM_EXIT_SYSTEM_EVENT for
    this purpose. We can also inform user space about the 'type' and
    architecture specific 'flags' of a system-level event using the
    kvm_run structure.
    
    This newly added KVM_EXIT_SYSTEM_EVENT will be used by KVM ARM/ARM64
    in-kernel PSCI v0.2 support to reset/shutdown VMs.
    
    Signed-off-by: Anup Patel <anup.patel@linaro.org>
    Signed-off-by: Pranavkumar Sawargaonkar <pranavkumar@linaro.org>
    Reviewed-by: Christoffer Dall <christoffer.dall@linaro.org>
    Reviewed-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -171,6 +171,7 @@ struct kvm_pit_config {
 #define KVM_EXIT_WATCHDOG         21
 #define KVM_EXIT_S390_TSCH        22
 #define KVM_EXIT_EPR              23
+#define KVM_EXIT_SYSTEM_EVENT     24
 
 /* For KVM_EXIT_INTERNAL_ERROR */
 /* Emulate instruction failed. */
@@ -301,6 +302,13 @@ struct kvm_run {
 		struct {
 			__u32 epr;
 		} epr;
+		/* KVM_EXIT_SYSTEM_EVENT */
+		struct {
+#define KVM_SYSTEM_EVENT_SHUTDOWN       1
+#define KVM_SYSTEM_EVENT_RESET          2
+			__u32 type;
+			__u64 flags;
+		} system_event;
 		/* Fix the size of the union. */
 		char padding[256];
 	};
