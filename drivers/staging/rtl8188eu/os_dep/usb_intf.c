599a2c02a784934b9bf0725920c1bc235b9b28ce
Kim Nylund <kim@pratsam.com>
2014-05-01 10:24:09 -0500

    staging: r8188eu: Add support for Dlink DWA-121 802.11n Wireless N 150 Pico Adapter
    This is a new device supported by this driver.
    
    Signed-off-by: Kim Nylund <kim@pratsam.com>
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/os_dep/usb_intf.c
+++ drivers/staging/rtl8188eu/os_dep/usb_intf.c
@@ -55,6 +55,7 @@ static struct usb_device_id rtw_usb_id_tbl[] = {
 	/*=== Customer ID ===*/
 	/****** 8188EUS ********/
 	{USB_DEVICE(0x07b8, 0x8179)}, /* Abocom - Abocom */
+	{USB_DEVICE(0x2001, 0x3308)}, /* DLink DWA-121 REV A1 */
 	{USB_DEVICE(0x2001, 0x330F)}, /* DLink DWA-125 REV D1 */
 	{USB_DEVICE(0x2001, 0x3310)}, /* Dlink DWA-123 REV D1 */
 	{}	/* Terminating entry */
