6e2954e801d7d93fe6f54eff988ddf25836fee0a
Ian Abbott <abbotti@mev.co.uk>
2014-05-01 17:38:24 +0100

    staging: comedi: amplc_dio200_common: correct bound on counter mode
    For the mode configured by the `INSN_CONFIG_SET_COUNTER_MODE` comedi
    instruction for the counter subdevice channels supported by this module,
    the upper bound should be `I8254_MODE5 | I8254_BCD` ((5 << 1) | 1)
    rather than `I8254_MODE5 | I8254_BINARY` ((5 << 1) | 0).  Fix it.
    
    Reported-by: Hartley Sweeten <HartleyS@visionengravers.com>
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/amplc_dio200_common.c
+++ drivers/staging/comedi/drivers/amplc_dio200_common.c
@@ -820,7 +820,7 @@ dio200_subdev_8254_config(struct comedi_device *dev, struct comedi_subdevice *s,
 	spin_lock_irqsave(&subpriv->spinlock, flags);
 	switch (data[0]) {
 	case INSN_CONFIG_SET_COUNTER_MODE:
-		if (data[1] > (I8254_MODE5 | I8254_BINARY))
+		if (data[1] > (I8254_MODE5 | I8254_BCD))
 			ret = -EINVAL;
 		else
 			dio200_subdev_8254_set_mode(dev, s, chan, data[1]);
