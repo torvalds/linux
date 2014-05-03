3e966cff406d9b43a16e07291a918cb2a098e32f
Arve Hjønnevåg <arve@android.com>
2014-05-02 20:23:21 -0700

    pstore/ram: Add ramoops_console_write_buf api
    Allow writing into the ramoops console buffer.
    
    Change-Id: Iff0d69b562e4dae33ea7f8d19412227bebb17e47
    Signed-off-by: Arve Hjønnevåg <arve@android.com>

--- fs/pstore/ram.c
+++ fs/pstore/ram.c
@@ -377,6 +377,12 @@ static int ramoops_init_prz(struct device *dev, struct ramoops_context *cxt,
 	return 0;
 }
 
+void notrace ramoops_console_write_buf(const char *buf, size_t size)
+{
+	struct ramoops_context *cxt = &oops_cxt;
+	persistent_ram_write(cxt->cprz, buf, size);
+}
+
 static int ramoops_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
