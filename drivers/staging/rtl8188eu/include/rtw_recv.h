498562560a351cd483bc5d6013a6a33c70e15043
navin patidar <navin.patidar@gmail.com>
2014-05-03 17:15:23 +0530

    staging: rtl8188eu: Remove 'dma_addr_t dma_transfer_addr' from struct recv_buf
    Remove unused variable dma_transfer_addr.
    
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/rtw_recv.h
+++ drivers/staging/rtl8188eu/include/rtw_recv.h
@@ -245,7 +245,6 @@ struct recv_buf {
 	u8	*ptail;
 	u8	*pend;
 	struct urb *purb;
-	dma_addr_t dma_transfer_addr;	/* (in) dma addr for transfer_buffer */
 	u32 alloc_sz;
 	u8  irp_pending;
 	int  transfer_len;
