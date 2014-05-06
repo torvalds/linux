7216e10227c90a388db1ee29b7b89820b81aa8eb
poma <pomidorabelisima@gmail.com>
2014-05-06 22:03:38 +0200

    WAPF 4 for ASUSTeK COMPUTER INC. X75VBP WLAN ON.
    The 'asus-nb-wmi' WAPF parameter must be set to 4, so the internal Wireless LAN device is operational.
    
    Signed-off-by: poma <pomidorabelisima@gmail.com>
    Signed-off-by: Matthew Garrett <matthew.garrett@nebula.com>

--- drivers/platform/x86/asus-nb-wmi.c
+++ drivers/platform/x86/asus-nb-wmi.c
@@ -191,6 +191,15 @@ static struct dmi_system_id asus_quirks[] = {
 	},
 	{
 		.callback = dmi_matched,
+		.ident = "ASUSTeK COMPUTER INC. X75VBP",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
+			DMI_MATCH(DMI_PRODUCT_NAME, "X75VBP"),
+		},
+		.driver_data = &quirk_asus_x401u,
+	},
+	{
+		.callback = dmi_matched,
 		.ident = "ASUSTeK COMPUTER INC. 1015E",
 		.matches = {
 			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
