be4034016c11f8913d38fccf692007fab1c50be1
Thomas Gleixner <tglx@linutronix.de>
2014-05-07 15:44:20 +0000

    s390: Avoid call to irq_reserve_irqs()
    There is no need to mark the lower interrupts as reserved in order to
    exclude them from dynamic allocation.
    
    Provide arch_dynirq_lower_bound() to exclude the lower space.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Grant Likely <grant.likely@linaro.org>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: linux390@de.ibm.com
    Link: http://lkml.kernel.org/r/20140507154339.811205235@linutronix.de
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/s390/kernel/irq.c
+++ arch/s390/kernel/irq.c
@@ -92,7 +92,6 @@ static const struct irq_class irqclass_sub_desc[NR_ARCH_IRQS] = {
 
 void __init init_IRQ(void)
 {
-	irq_reserve_irqs(0, THIN_INTERRUPT);
 	init_cio_interrupts();
 	init_airq_interrupts();
 	init_ext_interrupts();
@@ -149,6 +148,11 @@ int show_interrupts(struct seq_file *p, void *v)
 out:
 	put_online_cpus();
 	return 0;
+}
+
+unsigned int arch_dynirq_lower_bound(unsigned int from)
+{
+	return from < THIN_INTERRUPT ? THIN_INTERRUPT : from;
 }
 
 /*
