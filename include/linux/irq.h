7b6ef1262549f6afc5c881aaef80beb8fd15f908
Thomas Gleixner <tglx@linutronix.de>
2014-05-07 15:44:05 +0000

    genirq: Provide generic hwirq allocation facility
    Not really the solution to the problem, but at least it confines the
    mess in the core code and allows to get rid of the create/destroy_irq
    variants from hell, i.e. 3 implementations with different semantics
    plus the x86 specific variants __create_irqs and create_irq_nr
    which have been invented in another circle of hell.
    
    x86 : x86 should be converted to irq domains and I'm deliberately
          making it impossible to do the multi-vector MSI support by
          adding more crap to the current mess. It's not that hard to do
          and I'm really tired of the trainwrecks which have been invented
          by baindaid engineering so far. Any attempt to do multi-vector
          MSI or ioapic hotplug without converting to irq domains is NAKed
          hereby.
    
    tile: Might use irq domains as well, but it has a very limited
          interrupt space, so handling it via this functionality might be
          the right thing to do even in the long run.
    
    ia64: That's an hopeless case, as I doubt that anyone has the stomach
          to rewrite the homebrewn dynamic allocation facilities. I stared
          at it for a couple of hours and gave up. The create/destroy_irq
          mess could be made private to itanic right away if there
          wouldn't be the iommu/dmar driver being shared with x86. So to
          do that I'm going to add a separate ia64 specific implementation
          later in order not to deep-six itanic right away.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Grant Likely <grant.likely@linaro.org>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Chris Metcalf <cmetcalf@tilera.com>
    Cc: Fenghua Yu <fenghua.yu@intel.com>
    Cc: x86@kernel.org
    Link: http://lkml.kernel.org/r/20140507154334.208629358@linutronix.de
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -637,6 +637,21 @@ static inline int irq_reserve_irq(unsigned int irq)
 	return irq_reserve_irqs(irq, 1);
 }
 
+#ifdef CONFIG_GENERIC_IRQ_LEGACY_ALLOC_HWIRQ
+unsigned int irq_alloc_hwirqs(int cnt, int node);
+static inline unsigned int irq_alloc_hwirq(int node)
+{
+	return irq_alloc_hwirqs(1, node);
+}
+void irq_free_hwirqs(unsigned int from, int cnt);
+static inline void irq_free_hwirq(unsigned int irq)
+{
+	return irq_free_hwirqs(irq, 1);
+}
+int arch_setup_hwirq(unsigned int irq, int node);
+void arch_teardown_hwirq(unsigned int irq);
+#endif
+
 #ifndef irq_reg_writel
 # define irq_reg_writel(val, addr)	writel(val, addr)
 #endif
