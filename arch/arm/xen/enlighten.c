79390289cfeb6d0f4295ca32a54630c93154428e
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2014-05-08 16:54:02 +0100

    arm,arm64/xen: introduce HYPERVISOR_suspend()
    Introduce HYPERVISOR_suspend() and a few additional empty stubs for
    Xen arch specific functions called by drivers/xen/manage.c.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Signed-off-by: David Vrabel <david.vrabel@citrix.com>

--- arch/arm/xen/enlighten.c
+++ arch/arm/xen/enlighten.c
@@ -339,6 +339,14 @@ static int __init xen_pm_init(void)
 }
 late_initcall(xen_pm_init);
 
+
+/* empty stubs */
+void xen_arch_pre_suspend(void) { }
+void xen_arch_post_suspend(int suspend_cancelled) { }
+void xen_timer_resume(void) { }
+void xen_arch_resume(void) { }
+
+
 /* In the hypervisor.S file. */
 EXPORT_SYMBOL_GPL(HYPERVISOR_event_channel_op);
 EXPORT_SYMBOL_GPL(HYPERVISOR_grant_table_op);
