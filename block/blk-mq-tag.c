1f236ab22ce3bc5d4f975aa116966c0ea7ec2013
Ming Lei <tom.leiming@gmail.com>
2014-05-11 01:01:51 +0800

    blk-mq: bitmap tag: cleanup blk_mq_init_tags
    Both nr_cache and nr_tags arn't needed for bitmap tag anymore.
    
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- block/blk-mq-tag.c
+++ block/blk-mq-tag.c
@@ -417,7 +417,6 @@ enomem:
 struct blk_mq_tags *blk_mq_init_tags(unsigned int total_tags,
 				     unsigned int reserved_tags, int node)
 {
-	unsigned int nr_tags, nr_cache;
 	struct blk_mq_tags *tags;
 
 	if (total_tags > BLK_MQ_TAG_MAX) {
@@ -428,9 +427,6 @@ struct blk_mq_tags *blk_mq_init_tags(unsigned int total_tags,
 	tags = kzalloc_node(sizeof(*tags), GFP_KERNEL, node);
 	if (!tags)
 		return NULL;
-
-	nr_tags = total_tags - reserved_tags;
-	nr_cache = nr_tags / num_online_cpus();
 
 	tags->nr_tags = total_tags;
 	tags->nr_reserved_tags = reserved_tags;
