f682a21854166d66e67606283dffd8d9fe31dbee
Matthias Brugger <matthias.bgg@gmail.com>
2014-05-13 01:06:13 +0200

    arm: add basic support for Mediatek MT6589 boards
    This adds a generic devicetree board file and a dtsi for boards
    based on MT6589 SoCs from Mediatek.
    
    Apart from the generic parts (gic, clocks) the only component
    currently supported are the timers.
    
    Signed-off-by: Matthias Brugger <matthias.bgg@gmail.com>

---
+++ arch/arm/mach-mediatek/mediatek.c
@@ -0,0 +1,27 @@
+/*
+ * Device Tree support for Mediatek SoCs
+ *
+ * Copyright (c) 2014 MundoReader S.L.
+ * Author: Matthias Brugger <matthias.bgg@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#include <linux/init.h>
+#include <asm/mach/arch.h>
+
+static const char * const mediatek_board_dt_compat[] = {
+	"mediatek,mt6589",
+	NULL,
+};
+
+DT_MACHINE_START(MEDIATEK_DT, "Mediatek Cortex-A7 (Device Tree)")
+	.dt_compat	= mediatek_board_dt_compat,
+MACHINE_END
