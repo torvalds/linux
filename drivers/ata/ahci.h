888d91a08fa8e2be4cb0eef1e5736ef68b8f77f0
Kefeng Wang <kefeng.wang@linaro.org>
2014-05-14 14:13:40 +0800

    ata: ahci: append new hflag AHCI_HFLAG_NO_FBS
    Append AHCI_HFLAG_NO_FBS to force turning off FBS flag.
    
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>
    Signed-off-by: Kefeng Wang <kefeng.wang@linaro.org>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- drivers/ata/ahci.h
+++ drivers/ata/ahci.h
@@ -235,6 +235,7 @@ enum {
 						        port start (wait until
 						        error-handling stage) */
 	AHCI_HFLAG_MULTI_MSI		= (1 << 16), /* multiple PCI MSIs */
+	AHCI_HFLAG_NO_FBS		= (1 << 17), /* no FBS */
 
 	/* ap->flags bits */
 
