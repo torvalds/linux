d3af6976a24f8cebef23e72ade6d91ddd781c12b
Leela Krishna Amudala <leela.krishna@linaro.org>
2014-05-16 04:23:24 +0900

    ARM: EXYNOS: Add generic cpu power control functions for exynos SoCs
    Add generic cpu power control functions for exynos based
    SoCS for cpu power up/down and to know the cpu status.
    
    Signed-off-by: Leela Krishna Amudala <leela.krishna@linaro.org>
    Signed-off-by: Abhilash Kesavan <a.kesavan@samsung.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-exynos/common.h
+++ arch/arm/mach-exynos/common.h
@@ -63,5 +63,8 @@ struct exynos_pmu_conf {
 };
 
 extern void exynos_sys_powerdown_conf(enum sys_powerdown mode);
+extern void exynos_cpu_power_down(int cpu);
+extern void exynos_cpu_power_up(int cpu);
+extern int  exynos_cpu_power_state(int cpu);
 
 #endif /* __ARCH_ARM_MACH_EXYNOS_COMMON_H */
