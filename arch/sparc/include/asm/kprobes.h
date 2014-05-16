758a0170855b3476ad767892cf6314866958cb77
Sam Ravnborg <sam@ravnborg.org>
2014-05-16 23:26:10 +0200

    sparc64: fix sparse warning in kprobes.c
    Fix following warning:
    kprobes.c:419:27: warning: symbol 'kprobe_trap' was not declared. Should it be static?
    
    Add proper prototype
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/asm/kprobes.h
+++ arch/sparc/include/asm/kprobes.h
@@ -46,4 +46,6 @@ struct kprobe_ctlblk {
 int kprobe_exceptions_notify(struct notifier_block *self,
 			     unsigned long val, void *data);
 int kprobe_fault_handler(struct pt_regs *regs, int trapnr);
+asmlinkage void __kprobes kprobe_trap(unsigned long trap_level,
+				      struct pt_regs *regs);
 #endif /* _SPARC64_KPROBES_H */
