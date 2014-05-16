17ed9efd8afdd8c38f19ff72f08e71c1fd2376e3
Simon Horman <horms+renesas@verge.net.au>
2014-05-16 13:43:00 +0900

    ARM: shmobile: marzen-reference: Use DT CPU Frequency
    Convert the Marzen DT reference board support to use shmobile_init_delay()
    to be able to migrate away from per-SoC delay setup functions.
    
    Based on work for the Armadillo800 EVA board by Magnus Damm.
    
    Cc: Magnus Damm <damm+renesas@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/board-marzen-reference.c
+++ arch/arm/mach-shmobile/board-marzen-reference.c
@@ -66,7 +66,7 @@ static const char *marzen_boards_compat_dt[] __initdata = {
 DT_MACHINE_START(MARZEN, "marzen")
 	.smp		= smp_ops(r8a7779_smp_ops),
 	.map_io		= r8a7779_map_io,
-	.init_early	= r8a7779_init_delay,
+	.init_early	= shmobile_init_delay,
 	.init_time	= marzen_init_timer,
 	.nr_irqs	= NR_IRQS_LEGACY,
 	.init_irq	= r8a7779_init_irq_dt,
