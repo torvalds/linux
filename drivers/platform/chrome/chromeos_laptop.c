61dc1abae64854c7cef543598b9e6f04886c4ebd
Nick Dyer <nick.dyer@itdev.co.uk>
2014-05-18 23:16:49 -0700

    Input: atmel_mxt_ts - read screen config from chip
    By reading the touchscreen configuration from the settings that the
    maXTouch chip is actually using, we can remove some platform data.
    
    The matrix size is not used for anything, and results in some rather
    confusing code to re-read it because it may change when configuration
    is downloaded, so don't print it out.
    
    Signed-off-by: Nick Dyer <nick.dyer@itdev.co.uk>
    Acked-by: Benson Leung <bleung@chromium.org>
    Acked-by: Yufeng Shen <miletus@chromium.org>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/platform/chrome/chromeos_laptop.c
+++ drivers/platform/chrome/chromeos_laptop.c
@@ -94,9 +94,6 @@ static int mxt_t19_keys[] = {
 };
 
 static struct mxt_platform_data atmel_224s_tp_platform_data = {
-	.x_size			= 102*20,
-	.y_size			= 68*20,
-	.orient			= MXT_VERTICAL_FLIP,
 	.irqflags		= IRQF_TRIGGER_FALLING,
 	.t19_num_keys		= ARRAY_SIZE(mxt_t19_keys),
 	.t19_keymap		= mxt_t19_keys,
@@ -111,9 +108,6 @@ static struct i2c_board_info atmel_224s_tp_device = {
 };
 
 static struct mxt_platform_data atmel_1664s_platform_data = {
-	.x_size			= 1700,
-	.y_size			= 2560,
-	.orient			= MXT_ROTATED_90_COUNTER,
 	.irqflags		= IRQF_TRIGGER_FALLING,
 	.config			= NULL,
 	.config_length		= 0,
