c1406846e4e1ae92c4fb96fcb4532a63a2bceb21
Lars-Peter Clausen <lars@metafoo.de>
2014-05-19 08:03:04 +0200

    ASoC: rt5651: Do not use rtd->codec
    rtd->codec does not necessarily point to the CODEC instance for which the
    callback was called (e.g. for CODEC<->CODEC or multi-CODEC links). Use
    dai->codec instead.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/codecs/rt5651.c
+++ sound/soc/codecs/rt5651.c
@@ -1368,8 +1368,7 @@ static int get_clk_info(int sclk, int rate)
 static int rt5651_hw_params(struct snd_pcm_substream *substream,
 	struct snd_pcm_hw_params *params, struct snd_soc_dai *dai)
 {
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_codec *codec = rtd->codec;
+	struct snd_soc_codec *codec = dai->codec;
 	struct rt5651_priv *rt5651 = snd_soc_codec_get_drvdata(codec);
 	unsigned int val_len = 0, val_clk, mask_clk;
 	int pre_div, bclk_ms, frame_size;
