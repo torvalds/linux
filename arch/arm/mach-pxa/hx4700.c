9becf5001130bcd24f57584e48467050e85eee03
Alexandre Belloni <alexandre.belloni@free-electrons.com>
2014-05-19 22:42:40 +0200

    ARM: pxa: hx4700: use PWM_LOOKUP to initialize struct pwm_lookup
    Use the new variant of the PWM_LOOKUP macro to initialize the PWM lookup
    table.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Acked-by: Philipp Zabel <philipp.zabel@gmail.com>
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- arch/arm/mach-pxa/hx4700.c
+++ arch/arm/mach-pxa/hx4700.c
@@ -574,14 +574,8 @@ static struct platform_device backlight = {
 };
 
 static struct pwm_lookup hx4700_pwm_lookup[] = {
-	{
-		.provider = "pxa27x-pwm.1",
-		.index = 0,
-		.dev_id = "pwm-backlight",
-		.con_id = NULL,
-		.period = 30923,
-		.polarity = PWM_POLARITY_NORMAL,
-	},
+	PWM_LOOKUP("pxa27x-pwm.1", 0, "pwm-backlight", NULL,
+		   30923, PWM_POLARITY_NORMAL),
 };
 
 /*
