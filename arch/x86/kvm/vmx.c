1f854112553a1d65363ab27d4ee3dfb4b27075fb
Nadav Amit <namit@cs.technion.ac.il>
2014-05-19 09:50:50 +0300

    KVM: vmx: DR7 masking on task switch emulation is wrong
    The DR7 masking which is done on task switch emulation should be in hex format
    (clearing the local breakpoints enable bits 0,2,4 and 6).
    
    Signed-off-by: Nadav Amit <namit@cs.technion.ac.il>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -5401,7 +5401,7 @@ static int handle_task_switch(struct kvm_vcpu *vcpu)
 	}
 
 	/* clear all local breakpoint enable flags */
-	vmcs_writel(GUEST_DR7, vmcs_readl(GUEST_DR7) & ~55);
+	vmcs_writel(GUEST_DR7, vmcs_readl(GUEST_DR7) & ~0x55);
 
 	/*
 	 * TODO: What about debug traps on tss switch?
