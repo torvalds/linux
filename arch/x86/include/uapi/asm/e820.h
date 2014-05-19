448466b723cc7b44016547116aa2b55a3736f1f0
Ross Zwisler <ross.zwisler@linux.intel.com>
2014-05-19 11:50:24 -0600

    x86: Remove obsolete comment in uapi/e820.h
    A comment introduced by this old commit:
    
      028b785888c5 ("x86 boot: extend some internal memory map arrays to handle larger EFI input")
    
    had to do with some nested preprocessor directives.  The
    directives were split into separate files by this commit:
    
      af170c5061dd ("UAPI: (Scripted) Disintegrate arch/x86/include/asm")
    
    The comment explaining their interaction was retained and is now
    present in arch/x86/include/uapi/asm/e820.h.  This comment is no
    longer correct, so delete it.
    
    Signed-off-by: Ross Zwisler <ross.zwisler@linux.intel.com>
    Link: http://lkml.kernel.org/r/1400521824-21040-1-git-send-email-ross.zwisler@linux.intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/uapi/asm/e820.h
+++ arch/x86/include/uapi/asm/e820.h
@@ -21,11 +21,6 @@
  * this size.
  */
 
-/*
- * Odd: 'make headers_check' complains about numa.h if I try
- * to collapse the next two #ifdef lines to a single line:
- *	#if defined(__KERNEL__) && defined(CONFIG_EFI)
- */
 #ifndef __KERNEL__
 #define E820_X_MAX E820MAX
 #endif
