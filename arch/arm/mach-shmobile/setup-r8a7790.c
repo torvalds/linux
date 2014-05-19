ed06ecbcc66f6224a908c8abfe940b00540f0aa8
Magnus Damm <damm+renesas@opensource.se>
2014-05-20 08:37:24 +0900

    ARM: shmobile: Use r8a7790 DT CPU Frequency in common case
    Convert the common C-code-less r8a7790 DT board support
    to use shmobile_init_delay() to be able to migrate away
    from per-SoC delay setup functions.
    
    Signed-off-by: Magnus Damm <damm+renesas@opensource.se>
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/setup-r8a7790.c
+++ arch/arm/mach-shmobile/setup-r8a7790.c
@@ -324,7 +324,7 @@ static const char * const r8a7790_boards_compat_dt[] __initconst = {
 
 DT_MACHINE_START(R8A7790_DT, "Generic R8A7790 (Flattened Device Tree)")
 	.smp		= smp_ops(r8a7790_smp_ops),
-	.init_early	= r8a7790_init_early,
+	.init_early	= shmobile_init_delay,
 	.init_time	= rcar_gen2_timer_init,
 	.dt_compat	= r8a7790_boards_compat_dt,
 MACHINE_END
