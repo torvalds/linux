261cbc97abc673e575ef6a1a95ed6ada68f04e78
Paul Bolle <pebolle@tiscali.nl>
2014-05-20 13:34:36 +0200

    MIPS: Remove CONFIG_PMCTWILED completely
    Commit 8b284dbc2200 ("MIPS: PNX Removing dead CONFIG_PMCTWILED") missed
    one reference to CONFIG_PMCTWILED in the code. It also missed one
    related reference to pmctwiled_setup(). Remove these references now.
    
    Signed-off-by: Paul Bolle <pebolle@tiscali.nl>
    Cc: linux-mips@linux-mips.org
    Cc: linux-kernel@vger.kernel.org
    Patchwork: https://patchwork.linux-mips.org/patch/6957/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/pmcs-msp71xx/msp_setup.c
+++ arch/mips/pmcs-msp71xx/msp_setup.c
@@ -27,7 +27,6 @@
 #endif
 
 extern void msp_serial_setup(void);
-extern void pmctwiled_setup(void);
 
 #if defined(CONFIG_PMC_MSP7120_EVAL) || \
     defined(CONFIG_PMC_MSP7120_GW) || \
@@ -235,12 +234,4 @@ void __init prom_init(void)
 		register_smp_ops(&msp_smtc_smp_ops);
 #endif
 	}
-
-#ifdef CONFIG_PMCTWILED
-	/*
-	 * Setup LED states before the subsys_initcall loads other
-	 * dependent drivers/modules.
-	 */
-	pmctwiled_setup();
-#endif
 }
