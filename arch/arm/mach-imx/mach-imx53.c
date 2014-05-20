ff4ab2311a902e30cd579339a83679bab944e1bc
Shawn Guo <shawn.guo@freescale.com>
2014-05-20 15:34:06 +0800

    ARM: imx5: move init hooks into mach-imx5x.c
    These imx5 init_early[late] hooks are called only from mach-imx5x.c.
    Let's move them into mach-imx5x.c.
    
    While at it, replace the static mapping in imx51_ipu_mipi_setup() with
    dynamic mapping.  Also this function and imx_src_init() do not
    necessarily to be called at .init_early hook, so move them into
    .init_machine.
    
    The mxc_iomux_v3_init() is dropped from imx51_init_early() in the
    moving, since it's only needed by non-DT boot.
    
    Signed-off-by: Shawn Guo <shawn.guo@freescale.com>

--- arch/arm/mach-imx/mach-imx53.c
+++ arch/arm/mach-imx/mach-imx53.c
@@ -24,11 +24,22 @@
 #include "hardware.h"
 #include "mx53.h"
 
+static void __init imx53_init_early(void)
+{
+	mxc_set_cpu_type(MXC_CPU_MX53);
+}
+
 static void __init imx53_dt_init(void)
 {
 	mxc_arch_reset_init_dt();
+	imx_src_init();
 
 	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
+}
+
+static void __init imx53_init_late(void)
+{
+	imx53_pm_init();
 }
 
 static const char *imx53_dt_board_compat[] __initconst = {
