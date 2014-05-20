08488e20cc649be3606d4d39a605e0933dcddbf0
Gabriel FERNANDEZ <gabriel.fernandez@st.com>
2014-05-20 15:22:00 +0200

    ARM: STi: DT: STiH416: 416 DT Entry for clockgen A0/1/10/11/12
    Patch adds DT entries for clockgen A0/1/10/11/12
    
    Signed-off-by: Pankaj Dev <pankaj.dev@st.com>
    Signed-off-by: Gabriel Fernandez <gabriel.fernandez@linaro.org>
    Signed-off-by: Maxime Coquelin <maxime.coquelin@st.com>

---
+++ include/dt-bindings/clock/stih416-clks.h
@@ -0,0 +1,15 @@
+/*
+ * This header provides constants clk index STMicroelectronics
+ * STiH416 SoC.
+ */
+#ifndef _CLK_STIH416
+#define _CLK_STIH416
+
+/* CLOCKGEN A0 */
+#define CLK_ICN_REG		0
+#define CLK_ETH1_PHY		4
+
+/* CLOCKGEN A1 */
+#define CLK_GMAC0_PHY		3
+
+#endif
