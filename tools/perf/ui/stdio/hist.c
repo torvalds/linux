56772ad4750e23460a4b80f7ece5377d8c922ee1
Namhyung Kim <namhyung@kernel.org>
2014-05-23 18:31:52 +0900

    perf ui/stdio: Fix invalid percentage value of cumulated hist entries
    On stdio, there's a problem that it shows invalid values for
    callchains in cumulated hist entries.  It's because it only cares
    about the self period.  But with --children behavior, we always add
    callchain info to the cumulated entries so it should use the value in
    that case.
    
    Before:
    
      # Children      Self  Command      Shared Object            Symbol
      # ........  ........  .......  .................  ................
      #
          61.22%     0.32%  swapper  [kernel.kallsyms]      [k] cpu_idle
                        |
                        --- cpu_idle
                           |
                           |--16530.76%-- start_secondary
                           |
                           |--2758.70%-- rest_init
                           |          start_kernel
                           |          x86_64_start_reservations
                           |          x86_64_start_kernel
                            --6837850969203030.00%-- [...]
    
    After:
    
      # Children      Self  Command      Shared Object            Symbol
      # ........  ........  .......  .................  ................
      #
          61.22%     0.32%  swapper  [kernel.kallsyms]      [k] cpu_idle
                        |
                        --- cpu_idle
                           |
                           |--85.70%-- start_secondary
                           |
                            --14.30%-- rest_init
                                      start_kernel
                                      x86_64_start_reservations
                                      x86_64_start_kernel
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Arun Sharma <asharma@fb.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Link: http://lkml.kernel.org/r/1401335910-16832-24-git-send-email-namhyung@kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/ui/stdio/hist.c
+++ tools/perf/ui/stdio/hist.c
@@ -271,7 +271,9 @@ static size_t hist_entry_callchain__fprintf(struct hist_entry *he,
 {
 	switch (callchain_param.mode) {
 	case CHAIN_GRAPH_REL:
-		return callchain__fprintf_graph(fp, &he->sorted_chain, he->stat.period,
+		return callchain__fprintf_graph(fp, &he->sorted_chain,
+						symbol_conf.cumulate_callchain ?
+						he->stat_acc->period : he->stat.period,
 						left_margin);
 		break;
 	case CHAIN_GRAPH_ABS:
