13aa75858bff8ae0799f40ab2b37dfa912f8947c
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-05-25 22:43:47 +0200

    staging: rtl8723au: Remove unused struct btdata_info
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/include/rtl8723a_bt-coexist.h
+++ drivers/staging/rtl8723au/include/rtl8723a_bt-coexist.h
@@ -1086,12 +1086,6 @@ enum hci_ext_bp_operation {
 	HCI_BT_OP_MAX
 };
 
-/*	Function proto type */
-struct btdata_entry {
-	struct list_head	List;
-	void			*pDataBlock;
-};
-
 #define BTHCI_SM_WITH_INFO(_Adapter, _StateToEnter, _StateCmd, _EntryNum)	\
 {										\
 	RTPRINT(FIOCTL, IOCTL_STATE, ("[BT state change] caused by ""%s"", line =%d\n", __func__, __LINE__));							\
