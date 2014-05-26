c0a8d9b0692cced5b0701ed501012e28b224d32b
Richard Guy Briggs <rgb@redhat.com>
2014-05-26 10:59:28 -0400

    audit: reduce scope of audit_net_id
    audit_net_id isn't used outside kernel/audit.c.  Reduce its scope.
    
    Signed-off-by: Richard Guy Briggs <rgb@redhat.com>

--- kernel/audit.c
+++ kernel/audit.c
@@ -126,7 +126,7 @@ static atomic_t    audit_lost = ATOMIC_INIT(0);
 
 /* The netlink socket. */
 static struct sock *audit_sock;
-int audit_net_id;
+static int audit_net_id;
 
 /* Hash for inode-based rules */
 struct list_head audit_inode_hash[AUDIT_INODE_BUCKETS];
