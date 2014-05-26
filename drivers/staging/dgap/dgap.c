0ade4a34fd439d62df46937e8f3e584eb0879579
Daeseok Youn <daeseok.youn@gmail.com>
2014-05-26 19:24:17 +0900

    staging: dgap: remove unneeded kfree() in dgap_tty_register_ports()
    When it failed to allocate for printer_ports, serial_ports
    can be freed in dgap_tty_uninit().
    
    Signed-off-by: Daeseok Youn <daeseok.youn@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgap/dgap.c
+++ drivers/staging/dgap/dgap.c
@@ -4147,10 +4147,9 @@ static int dgap_tty_register_ports(struct board_t *brd)
 
 	brd->printer_ports = kcalloc(brd->nasync, sizeof(*brd->printer_ports),
 					GFP_KERNEL);
-	if (brd->printer_ports == NULL) {
-		kfree(brd->serial_ports);
+	if (brd->printer_ports == NULL)
 		return -ENOMEM;
-	}
+
 	for (i = 0; i < brd->nasync; i++)
 		tty_port_init(&brd->printer_ports[i]);
 
