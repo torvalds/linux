49ac3ae8d5c356131fcfba59327a91318b98289d
navin patidar <navin.patidar@gmail.com>
2014-05-26 21:35:30 +0530

    staging: rtl8188eu: Remove unused function pointer declaration.
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/odm_interface.h
+++ drivers/staging/rtl8188eu/include/odm_interface.h
@@ -58,16 +58,6 @@ enum odm_h2c_cmd {
 	ODM_MAX_H2CCMD
 };
 
-/*  2012/02/17 MH For non-MP compile pass only. Linux does not support workitem. */
-/*  Suggest HW team to use thread instead of workitem. Windows also support the feature. */
-typedef void (*RT_WORKITEM_CALL_BACK)(void *pContext);
-
-/*  =========== Extern Variable ??? It should be forbidden. */
-
-/*  =========== EXtern Function Prototype */
-
-/*  Memory Relative Function. */
-
 /*  ODM Timer relative API. */
 
 void ODM_CancelTimer(struct odm_dm_struct *pDM_Odm, struct timer_list *pTimer);
