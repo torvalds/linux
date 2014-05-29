b0156b05a688e599237dd46d2f4d5c0c60dccd03
Joachim Eastwood <manabian@gmail.com>
2014-05-29 00:29:31 -0700

    ARM: OMAP2+: remove unused omap4-keypad file and code
    This has been unused since omap4 board files went away.
    
    Signed-off-by: Joachim Eastwood <manabian@gmail.com>
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- arch/arm/mach-omap2/omap4-keypad.h
+++
@@ -1,8 +0,0 @@
-#ifndef ARCH_ARM_PLAT_OMAP4_KEYPAD_H
-#define ARCH_ARM_PLAT_OMAP4_KEYPAD_H
-
-struct omap_board_data;
-
-extern int omap4_keyboard_init(struct omap4_keypad_platform_data *,
-				struct omap_board_data *);
-#endif
