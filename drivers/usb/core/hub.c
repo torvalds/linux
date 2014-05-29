4a95b1fce97756d0333f8232eb7ed6974e93b054
Stephen Rothwell <sfr@canb.auug.org.au>
2014-05-29 18:55:06 +1000

    usb: hub_handle_remote_wakeup() only exists for CONFIG_PM=y
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -3966,6 +3966,12 @@ EXPORT_SYMBOL_GPL(usb_disable_ltm);
 void usb_enable_ltm(struct usb_device *udev) { }
 EXPORT_SYMBOL_GPL(usb_enable_ltm);
 
+static int hub_handle_remote_wakeup(struct usb_hub *hub, unsigned int port,
+		u16 portstatus, u16 portchange)
+{
+	return 0;
+}
+
 #endif	/* CONFIG_PM */
 
 
