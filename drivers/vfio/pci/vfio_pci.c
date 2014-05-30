fd49c81f080a997aad4b0e73541cd42772b6a772
Gavin Shan <gwshan@linux.vnet.ibm.com>
2014-05-30 11:35:54 -0600

    drivers/vfio/pci: Fix wrong MSI interrupt count
    According PCI local bus specification, the register of Message
    Control for MSI (offset: 2, length: 2) has bit#0 to enable or
    disable MSI logic and it shouldn't be part contributing to the
    calculation of MSI interrupt count. The patch fixes the issue.
    
    Signed-off-by: Gavin Shan <gwshan@linux.vnet.ibm.com>
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

--- drivers/vfio/pci/vfio_pci.c
+++ drivers/vfio/pci/vfio_pci.c
@@ -197,8 +197,7 @@ static int vfio_pci_get_irq_count(struct vfio_pci_device *vdev, int irq_type)
 		if (pos) {
 			pci_read_config_word(vdev->pdev,
 					     pos + PCI_MSI_FLAGS, &flags);
-
-			return 1 << (flags & PCI_MSI_FLAGS_QMASK);
+			return 1 << ((flags & PCI_MSI_FLAGS_QMASK) >> 1);
 		}
 	} else if (irq_type == VFIO_PCI_MSIX_IRQ_INDEX) {
 		u8 pos;
