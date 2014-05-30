cc2afe9fe25d99cbe19eac57c0f8f098b2710b7c
Himangi Saraogi <himangi774@gmail.com>
2014-05-30 21:16:05 +0530

    RDS/RDMA: Replace comma with semicolon
    This patch replaces a comma between expression statements by a semicolon.
    
    A simplified version of the semantic patch that performs this
    transformation is as follows:
    
    // <smpl>
    @r@
    expression e1,e2,e;
    type T;
    identifier i;
    @@
    
     e1
    -,
    +;
     e2;
    // </smpl>
    
    Signed-off-by: Himangi Saraogi <himangi774@gmail.com>
    Acked-by: Julia Lawall <julia.lawall@lip6.fr>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/rds/rdma_transport.c
+++ net/rds/rdma_transport.c
@@ -168,7 +168,7 @@ static int rds_rdma_listen_init(void)
 		return ret;
 	}
 
-	sin.sin_family = AF_INET,
+	sin.sin_family = AF_INET;
 	sin.sin_addr.s_addr = (__force u32)htonl(INADDR_ANY);
 	sin.sin_port = (__force u16)htons(RDS_PORT);
 
