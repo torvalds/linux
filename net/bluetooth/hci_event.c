7e3691e13ab51f3491e996e2edaf99b173621288
Johan Hedberg <johan.hedberg@intel.com>
2014-05-30 14:45:19 +0300

    Bluetooth: Fix authentication check for FIPS security level
    When checking whether we need to request authentication or not we should
    include HCI_SECURITY_FIPS to the levels that always need authentication.
    This patch fixes check for it in the hci_outgoing_auth_needed()
    function.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Cc: stable@vger.kernel.org

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -1453,6 +1453,7 @@ static int hci_outgoing_auth_needed(struct hci_dev *hdev,
 	 * is requested.
 	 */
 	if (!hci_conn_ssp_enabled(conn) && !(conn->auth_type & 0x01) &&
+	    conn->pending_sec_level != BT_SECURITY_FIPS &&
 	    conn->pending_sec_level != BT_SECURITY_HIGH &&
 	    conn->pending_sec_level != BT_SECURITY_MEDIUM)
 		return 0;
