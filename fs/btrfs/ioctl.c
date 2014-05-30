ff5df9b884268d319d041934c6059c09733a4be2
Filipe Manana <fdmanana@gmail.com>
2014-05-30 17:56:24 +0100

    Btrfs: ioctl, don't re-lock extent range when not necessary
    In ioctl.c:lock_extent_range(), after locking our target range, the
    ordered extent that btrfs_lookup_first_ordered_extent() returns us
    may not overlap our target range at all. In this case we would just
    unlock our target range, wait for any new ordered extents that overlap
    the range to complete, lock again the range and repeat all these steps
    until we don't get any ordered extent and the delalloc flag isn't set
    in the io tree for our target range.
    
    Therefore just stop if we get an ordered extent that doesn't overlap
    our target range and the dealalloc flag isn't set for the range in
    the inode's io tree.
    
    Signed-off-by: Filipe David Borba Manana <fdmanana@gmail.com>
    Signed-off-by: Chris Mason <clm@fb.com>

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -2700,10 +2700,15 @@ static inline void lock_extent_range(struct inode *inode, u64 off, u64 len)
 		lock_extent(&BTRFS_I(inode)->io_tree, off, off + len - 1);
 		ordered = btrfs_lookup_first_ordered_extent(inode,
 							    off + len - 1);
-		if (!ordered &&
+		if ((!ordered ||
+		     ordered->file_offset + ordered->len <= off ||
+		     ordered->file_offset >= off + len) &&
 		    !test_range_bit(&BTRFS_I(inode)->io_tree, off,
-				    off + len - 1, EXTENT_DELALLOC, 0, NULL))
+				    off + len - 1, EXTENT_DELALLOC, 0, NULL)) {
+			if (ordered)
+				btrfs_put_ordered_extent(ordered);
 			break;
+		}
 		unlock_extent(&BTRFS_I(inode)->io_tree, off, off + len - 1);
 		if (ordered)
 			btrfs_put_ordered_extent(ordered);
