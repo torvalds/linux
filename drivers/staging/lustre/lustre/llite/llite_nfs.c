5b39bd5f33bb65f60492441bf6b50d88bc843e1c
Josep Puigdemont <josep.puigdemont@gmail.com>
2014-06-01 20:23:16 +0200

    staging: lustre: Fix coding style
    The if block should be placed in a new line rather
    than after the if statement.
    Blank line required between variable declarations and code.
    
    Signed-off-by: Josep Puigdemont <josep.puigdemont@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/llite_nfs.c
+++ drivers/staging/lustre/lustre/llite/llite_nfs.c
@@ -51,7 +51,9 @@ __u32 get_uuid2int(const char *name, int len)
 	__u32 key0 = 0x12a3fe2d, key1 = 0x37abe8f9;
 	while (len--) {
 		__u32 key = key1 + (key0 ^ (*name++ * 7152373));
-		if (key & 0x80000000) key -= 0x7fffffff;
+
+		if (key & 0x80000000)
+			key -= 0x7fffffff;
 		key1 = key0;
 		key0 = key;
 	}
