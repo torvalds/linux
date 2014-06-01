701accdc48d3f5f23afc9d23cf6624695872476c
navin patidar <navin.patidar@gmail.com>
2014-06-01 17:46:36 +0530

    staging: rtl8188eu: Remove unused stucture defination struct specific_device_id
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/drv_types.h
+++ drivers/staging/rtl8188eu/include/drv_types.h
@@ -68,12 +68,6 @@ enum _NIC_VERSION {
 #define SPEC_DEV_ID_RF_CONFIG_2T2R	BIT(4)
 #define SPEC_DEV_ID_ASSIGN_IFNAME	BIT(5)
 
-struct specific_device_id {
-	u32		flags;
-	u16		idVendor;
-	u16		idProduct;
-};
-
 struct registry_priv {
 	u8	chip_version;
 	u8	rfintfs;
