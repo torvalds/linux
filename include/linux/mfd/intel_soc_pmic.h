5165238460068e53c740eaa621ebb6623dc4a50d
Zhu, Lejun <lejun.zhu@linux.intel.com>
2014-06-03 13:26:02 +0800

    mfd: intel_soc_pmic: Core driver
    This patch provides the common I2C driver code for Intel SoC PMICs.
    
    Signed-off-by: Yang, Bin <bin.yang@intel.com>
    Signed-off-by: Zhu, Lejun <lejun.zhu@linux.intel.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

---
+++ include/linux/mfd/intel_soc_pmic.h
@@ -0,0 +1,30 @@
+/*
+ * intel_soc_pmic.h - Intel SoC PMIC Driver
+ *
+ * Copyright (C) 2012-2014 Intel Corporation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Author: Yang, Bin <bin.yang@intel.com>
+ * Author: Zhu, Lejun <lejun.zhu@linux.intel.com>
+ */
+
+#ifndef __INTEL_SOC_PMIC_H__
+#define __INTEL_SOC_PMIC_H__
+
+#include <linux/regmap.h>
+
+struct intel_soc_pmic {
+	int irq;
+	struct regmap *regmap;
+	struct regmap_irq_chip_data *irq_chip_data;
+};
+
+#endif	/* __INTEL_SOC_PMIC_H__ */
