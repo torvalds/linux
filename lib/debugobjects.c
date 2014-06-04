c0f35cc0be0e1d06b89d5867a6db09eda5033189
Fabian Frederick <fabf@skynet.be>
2014-06-04 16:06:05 -0700

    lib/debugobjects.c: convert printk(KERN_DEBUG to pr_debug
    Direct conversion of one KERN_DEBUG message without DEBUG definition
    (suggested by Josh Triplett)
    
    That message will now be disabled by default.  (see
    Documentation/CodingStyle Chapter 13)
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/debugobjects.c
+++ lib/debugobjects.c
@@ -1061,8 +1061,8 @@ static int __init debug_objects_replace_static_objects(void)
 	}
 	local_irq_enable();
 
-	printk(KERN_DEBUG "ODEBUG: %d of %d active objects replaced\n", cnt,
-	       obj_pool_used);
+	pr_debug("%d of %d active objects replaced\n",
+		 cnt, obj_pool_used);
 	return 0;
 free:
 	hlist_for_each_entry_safe(obj, tmp, &objects, node) {
