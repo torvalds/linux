53c5fa16b4c843f1df91f7498e3c7bf95e0eaefa
Frederic Weisbecker <fweisbec@gmail.com>
2014-06-04 16:20:21 +0200

    nohz: Switch to nohz full remote kick on timer enqueue
    When a new timer is enqueued on a full dynticks target, that CPU must
    re-evaluate the next tick to handle the timer correctly.
    
    This is currently performed through the scheduler IPI. Meanwhile this
    happens at the cost of off-topic workarounds in that fast path to make
    it call irq_exit().
    
    As we plan to remove this hack off the scheduler IPI, lets use
    the nohz full kick instead. Pretty much any IPI fits for that job
    as long at it calls irq_exit(). The nohz full kick just happens to be
    handy and readily available here.
    
    If it happens to be too much an overkill in the future, we can still
    turn that timer kick into an empty IPI.
    
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Kevin Hilman <khilman@linaro.org>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -684,10 +684,16 @@ static void wake_up_idle_cpu(int cpu)
 
 static bool wake_up_full_nohz_cpu(int cpu)
 {
+	/*
+	 * We just need the target to call irq_exit() and re-evaluate
+	 * the next tick. The nohz full kick at least implies that.
+	 * If needed we can still optimize that later with an
+	 * empty IRQ.
+	 */
 	if (tick_nohz_full_cpu(cpu)) {
 		if (cpu != smp_processor_id() ||
 		    tick_nohz_tick_stopped())
-			smp_send_reschedule(cpu);
+			tick_nohz_full_kick_cpu(cpu);
 		return true;
 	}
 
