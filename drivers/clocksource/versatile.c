1605abfffe331b3d9f845a456e7762f1bf53d973
Thomas Gleixner <tglx@linutronix.de>
2014-06-04 12:34:15 +0200

    clocksource: versatile: Use sched_clock_register()
    The newly merged versatile sched clock support uses a deprecated
    interface.  Of course that patch got routed through the ARM tree instead
    of going through the relevant maintainer tree.
    
    Use the proper interface so we can get rid of the cruft.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/clocksource/versatile.c
+++ drivers/clocksource/versatile.c
@@ -20,7 +20,7 @@
 
 static void __iomem *versatile_sys_24mhz;
 
-static u32 notrace versatile_sys_24mhz_read(void)
+static u64 notrace versatile_sys_24mhz_read(void)
 {
 	return readl(versatile_sys_24mhz);
 }
@@ -34,7 +34,7 @@ static void __init versatile_sched_clock_init(struct device_node *node)
 
 	versatile_sys_24mhz = base + SYS_24MHZ;
 
-	setup_sched_clock(versatile_sys_24mhz_read, 32, 24000000);
+	sched_clock_register(versatile_sys_24mhz_read, 32, 24000000);
 }
 CLOCKSOURCE_OF_DECLARE(versatile, "arm,vexpress-sysreg",
-		versatile_sched_clock_init);
+		       versatile_sched_clock_init);
