1812062790ab647e85821f21f2263f56eaeffc11
Dan Streetman <ddstreet@ieee.org>
2014-06-04 16:11:49 -0700

    lib/plist.c: replace pr_debug with printk in plist_test()
    Replace pr_debug() in lib/plist.c test function plist_test() with
    printk(KERN_DEBUG ...).
    
    Without DEBUG defined, pr_debug() is complied out, but the entire
    plist_test() function is already inside CONFIG_DEBUG_PI_LIST, so printk
    should just be used directly.
    
    Signed-off-by: Dan Streetman <ddstreet@ieee.org>
    Reviewed-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/plist.c
+++ lib/plist.c
@@ -223,7 +223,7 @@ static int  __init plist_test(void)
 	int nr_expect = 0, i, loop;
 	unsigned int r = local_clock();
 
-	pr_debug("start plist test\n");
+	printk(KERN_DEBUG "start plist test\n");
 	plist_head_init(&test_head);
 	for (i = 0; i < ARRAY_SIZE(test_node); i++)
 		plist_node_init(test_node + i, 0);
@@ -255,7 +255,7 @@ static int  __init plist_test(void)
 		plist_test_check(nr_expect);
 	}
 
-	pr_debug("end plist test\n");
+	printk(KERN_DEBUG "end plist test\n");
 	return 0;
 }
 
