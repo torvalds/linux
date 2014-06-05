a5457e7bcf9a76ec5c2de5d311d9b0d3b724edc6
Paolo Bonzini <pbonzini@redhat.com>
2014-06-05 17:29:34 +0200

    KVM: emulate: POP SS triggers a MOV SS shadow too
    We did not do that when interruptibility was added to the emulator,
    because at the time pop to segment was not implemented.  Now it is,
    add it.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/emulate.c
+++ arch/x86/kvm/emulate.c
@@ -1762,6 +1762,9 @@ static int em_pop_sreg(struct x86_emulate_ctxt *ctxt)
 	if (rc != X86EMUL_CONTINUE)
 		return rc;
 
+	if (ctxt->modrm_reg == VCPU_SREG_SS)
+		ctxt->interruptibility = KVM_X86_SHADOW_INT_MOV_SS;
+
 	rc = load_segment_descriptor(ctxt, (u16)selector, seg);
 	return rc;
 }
