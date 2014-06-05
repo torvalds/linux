96d016108640bc2b7fb0ee800737f80923847294
Sam bobroff <sam.bobroff@au1.ibm.com>
2014-06-05 16:19:22 +1000

    powerpc: Correct DSCR during TM context switch
    Correct the DSCR SPR becoming temporarily corrupted if a task is
    context switched during a transaction.
    
    The problem occurs while suspending the task and is caused by saving
    the DSCR to thread.dscr after it has already been set to the CPU's
    default value:
    
    __switch_to() calls __switch_to_tm()
    	which calls tm_reclaim_task()
    	which calls tm_reclaim_thread()
    	which calls tm_reclaim()
    		where the DSCR is set to the CPU's default
    __switch_to() calls _switch()
    		where thread.dscr is set to the DSCR
    
    When the task is resumed, it's transaction will be doomed (as usual)
    and the DSCR SPR will be corrupted, although the checkpointed value
    will be correct. Therefore the DSCR will be immediately corrected by
    the transaction aborting, unless it has been suspended. In that case
    the incorrect value can be seen by the task until it resumes the
    transaction.
    
    The fix is to treat the DSCR similarly to the TAR and save it early
    in __switch_to().
    
    A program exposing the problem is added to the kernel self tests as:
    tools/testing/selftests/powerpc/tm/tm-resched-dscr.
    
    Signed-off-by: Sam Bobroff <sam.bobroff@au1.ibm.com>
    CC: <stable@vger.kernel.org> [v3.10+]
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/switch_to.h
+++ arch/powerpc/include/asm/switch_to.h
@@ -16,13 +16,15 @@ struct thread_struct;
 extern struct task_struct *_switch(struct thread_struct *prev,
 				   struct thread_struct *next);
 #ifdef CONFIG_PPC_BOOK3S_64
-static inline void save_tar(struct thread_struct *prev)
+static inline void save_early_sprs(struct thread_struct *prev)
 {
 	if (cpu_has_feature(CPU_FTR_ARCH_207S))
 		prev->tar = mfspr(SPRN_TAR);
+	if (cpu_has_feature(CPU_FTR_DSCR))
+		prev->dscr = mfspr(SPRN_DSCR);
 }
 #else
-static inline void save_tar(struct thread_struct *prev) {}
+static inline void save_early_sprs(struct thread_struct *prev) {}
 #endif
 
 extern void enable_kernel_fp(void);
