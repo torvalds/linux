36d789a4d75f3826faa6e75b018942b63ffed1a0
Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
2014-06-06 07:13:45 +0000

    perf probe: Improve error message for unknown member of data structure
    Improve the error message if we can not find given member in the given
    structure. Currently perf probe shows a wrong error message as below.
    
      -----
      # perf probe getname_flags:65 "result->BOGUS"
      result(type:filename) has no member BOGUS.
      Failed to find 'result' in this function.
        Error: Failed to add events. (-22)
      -----
    
    The first message is correct, but the second one is not, since we didn't
    fail to find a variable but fails to find the member of given variable.
    
      -----
      # perf probe getname_flags:65 "result->BOGUS"
      result(type:filename) has no member BOGUS.
        Error: Failed to add events. (-22)
      -----
    
    With this patch, the error message shows only the first one.  And if we
    really failed to find given variable, it tells us so.
    
      -----
      # perf probe getname_flags:65 "BOGUS"
      Failed to find 'BOGUS' in this function.
        Error: Failed to add events. (-2)
      -----
    
    Reported-by: Arnaldo Carvalho de Melo <acme@kernel.org>
    Signed-off-by: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/20140606071345.6788.23744.stgit@kbuild-fedora.novalocal
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/probe-finder.c
+++ tools/perf/util/probe-finder.c
@@ -573,14 +573,13 @@ static int find_variable(Dwarf_Die *sc_die, struct probe_finder *pf)
 	if (!die_find_variable_at(sc_die, pf->pvar->var, pf->addr, &vr_die)) {
 		/* Search again in global variables */
 		if (!die_find_variable_at(&pf->cu_die, pf->pvar->var, 0, &vr_die))
+			pr_warning("Failed to find '%s' in this function.\n",
+				   pf->pvar->var);
 			ret = -ENOENT;
 	}
 	if (ret >= 0)
 		ret = convert_variable(&vr_die, pf);
 
-	if (ret < 0)
-		pr_warning("Failed to find '%s' in this function.\n",
-			   pf->pvar->var);
 	return ret;
 }
 
