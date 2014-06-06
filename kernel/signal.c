650226bd95817d727c9a388752d72ef292c4a668
Oleg Nesterov <oleg@redhat.com>
2014-06-06 14:36:44 -0700

    ptrace: task_clear_jobctl_trapping()->wake_up_bit() needs mb()
    __wake_up_bit() checks waitqueue_active() and thus the caller needs mb()
    as wake_up_bit() documents, fix task_clear_jobctl_trapping().
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/signal.c
+++ kernel/signal.c
@@ -277,6 +277,7 @@ void task_clear_jobctl_trapping(struct task_struct *task)
 {
 	if (unlikely(task->jobctl & JOBCTL_TRAPPING)) {
 		task->jobctl &= ~JOBCTL_TRAPPING;
+		smp_mb();	/* advised by wake_up_bit() */
 		wake_up_bit(&task->jobctl, JOBCTL_TRAPPING_BIT);
 	}
 }
