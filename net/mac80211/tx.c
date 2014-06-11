8d7b70fb7bf8963634052f455db86074d48bab2c
Thomas Gleixner <tglx@linutronix.de>
2014-06-11 23:59:18 +0000

    net: Mac80211: Remove silly timespec dance
    Converting time from one format to another seems to give coders a warm
    and fuzzy feeling.
    
    Use the proper interfaces.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: John Stultz <john.stultz@linaro.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: John W. Linville <linville@tuxdriver.com>
    [fix compile error]
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/tx.c
+++ net/mac80211/tx.c
@@ -1770,15 +1770,12 @@ fail:
 static void ieee80211_tx_latency_start_msrmnt(struct ieee80211_local *local,
 					      struct sk_buff *skb)
 {
-	struct timespec skb_arv;
 	struct ieee80211_tx_latency_bin_ranges *tx_latency;
 
 	tx_latency = rcu_dereference(local->tx_latency);
 	if (!tx_latency)
 		return;
-
-	ktime_get_ts(&skb_arv);
-	skb->tstamp = ktime_set(skb_arv.tv_sec, skb_arv.tv_nsec);
+	skb->tstamp = ktime_get();
 }
 
 /**
