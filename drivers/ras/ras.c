d963cd95bea93b7db9390a71d1e2cabbb3b2c3ea
Chen, Gong <gong.chen@linux.intel.com>
2014-06-11 14:02:20 -0700

    RAS, debugfs: Add debugfs interface for RAS subsystem
    Implement a new debugfs interface for RAS susbsystem.
    A file named daemon_active is added there accordingly.
    This file is used to track if user space daemon accesses
    perf/trace interface or not. One can track which daemon
    opens it via "lsof /path/to/debugfs/ras/daemon_active".
    
    Signed-off-by: Chen, Gong <gong.chen@linux.intel.com>
    Link: http://lkml.kernel.org/r/1402475691-30045-5-git-send-email-gong.chen@linux.intel.com
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- drivers/ras/ras.c
+++ drivers/ras/ras.c
@@ -5,8 +5,22 @@
  *	Chen, Gong <gong.chen@linux.intel.com>
  */
 
+#include <linux/init.h>
+#include <linux/ras.h>
+
 #define CREATE_TRACE_POINTS
 #define TRACE_INCLUDE_PATH ../../include/ras
 #include <ras/ras_event.h>
+
+static int __init ras_init(void)
+{
+	int rc = 0;
+
+	ras_debugfs_init();
+	rc = ras_add_daemon_trace();
+
+	return rc;
+}
+subsys_initcall(ras_init);
 
 EXPORT_TRACEPOINT_SYMBOL_GPL(mc_event);
