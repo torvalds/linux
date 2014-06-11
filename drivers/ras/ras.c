76ac8275f296b49c58f684825543bf4eb85d43d0
Chen, Gong <gong.chen@linux.intel.com>
2014-06-11 13:54:04 -0700

    trace, RAS: Add basic RAS trace event
    To avoid confuision and conflict of usage for RAS related trace event,
    add an unified RAS trace event stub.
    
    Start a RAS subsystem menu which will be fleshed out in time, when more
    features get added to it.
    
    Signed-off-by: Chen, Gong <gong.chen@linux.intel.com>
    Link: http://lkml.kernel.org/r/1402475691-30045-2-git-send-email-gong.chen@linux.intel.com
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

---
+++ drivers/ras/ras.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2014 Intel Corporation
+ *
+ * Authors:
+ *	Chen, Gong <gong.chen@linux.intel.com>
+ */
+
+#define CREATE_TRACE_POINTS
+#define TRACE_INCLUDE_PATH ../../include/ras
+#include <ras/ras_event.h>
+
+EXPORT_TRACEPOINT_SYMBOL_GPL(mc_event);
