c51bd6acf59f24f2ec4334689329f1741f071621
Dan Carpenter <dan.carpenter@oracle.com>
2014-06-12 23:56:09 +0200

    i2c: rk3x: add NULL entry to the end of_device_id array
    drivers/i2c/busses/i2c-rk3x.c:610:69-70: rk3x_i2c_match is not NULL terminated at line 610
    
    Make sure of_device_id tables are NULL terminated
    Generated by: /kbuild/src/linux/scripts/coccinelle/misc/of_table.cocci
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/busses/i2c-rk3x.c
+++ drivers/i2c/busses/i2c-rk3x.c
@@ -608,6 +608,7 @@ static const struct of_device_id rk3x_i2c_match[] = {
 	{ .compatible = "rockchip,rk3066-i2c", .data = (void *)&soc_data[0] },
 	{ .compatible = "rockchip,rk3188-i2c", .data = (void *)&soc_data[1] },
 	{ .compatible = "rockchip,rk3288-i2c", .data = (void *)&soc_data[2] },
+	{},
 };
 
 static int rk3x_i2c_probe(struct platform_device *pdev)
