745160ee10b76ed739f78f0116ab3d17b3f77309
Oren Givon <oren.givon@intel.com>
2014-06-16 10:54:52 +0300

    iwlwifi: add max RX aggregation size
    Allow to configure the maximal Rx AMPDU size per device.
    
    Signed-off-by: Oren Givon <oren.givon@intel.com>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>

--- drivers/net/wireless/iwlwifi/mvm/ops.c
+++ drivers/net/wireless/iwlwifi/mvm/ops.c
@@ -391,6 +391,9 @@ iwl_op_mode_mvm_start(struct iwl_trans *trans, const struct iwl_cfg *cfg,
 	if (!hw)
 		return NULL;
 
+	if (cfg->max_rx_agg_size)
+		hw->max_rx_aggregation_subframes = cfg->max_rx_agg_size;
+
 	op_mode = hw->priv;
 	op_mode->ops = &iwl_mvm_ops;
 
