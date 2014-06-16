c03068852da3705d1953bcf75bfbfe322c40876d
Stephan Gabert <stephan.gabert@fau.de>
2014-06-16 16:50:53 +0200

    staging/wlags49_h2: checkpatch: line over 80 characters
    Signed-off-by: Stephan Gabert <stephan.gabert@fau.de>
    Signed-off-by: Nicolas Pfeiffer <nicolas.pfeiffer@fau.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlags49_h2/wl_cs.c
+++ drivers/staging/wlags49_h2/wl_cs.c
@@ -340,8 +340,9 @@ static const struct pcmcia_device_id wl_adapter_ids[] = {
 				0x33103a9b, 0xe175b0dd),
 #else
 	PCMCIA_DEVICE_MANF_CARD(0x0156, 0x0004),
-	PCMCIA_DEVICE_PROD_ID12("Linksys", "WCF54G_Wireless-G_CompactFlash_Card",
-				0x0733cc81, 0x98a599e1),
+	PCMCIA_DEVICE_PROD_ID12("Linksys",
+			"WCF54G_Wireless-G_CompactFlash_Card", 0x0733cc81,
+			0x98a599e1),
 #endif  /* (HCF_TYPE) & HCF_TYPE_HII5 */
 	PCMCIA_DEVICE_NULL,
 };
