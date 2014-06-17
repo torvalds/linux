16dcbdef404f4e87dab985494381939fe0a2d456
Guido Martínez <guido@vanguardiasur.com.ar>
2014-06-17 11:17:06 -0300

    drm/tilcdc: tfp410: fix dangling sysfs connector node
    Add a drm_sysfs_connector_remove call when we destroy the panel to make
    sure the connector node in sysfs gets deleted.
    
    This is required for proper unload and re-load of this driver, otherwise
    we will get a warning about a duplicate filename in sysfs.
    
    Signed-off-by: Guido Martínez <guido@vanguardiasur.com.ar>
    Tested-by: Darren Etheridge <detheridge@ti.com>
    Cc: <stable@vger.kernel.org> #v3.9+
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/tilcdc/tilcdc_tfp410.c
+++ drivers/gpu/drm/tilcdc/tilcdc_tfp410.c
@@ -167,6 +167,7 @@ struct tfp410_connector {
 static void tfp410_connector_destroy(struct drm_connector *connector)
 {
 	struct tfp410_connector *tfp410_connector = to_tfp410_connector(connector);
+	drm_sysfs_connector_remove(connector);
 	drm_connector_cleanup(connector);
 	kfree(tfp410_connector);
 }
