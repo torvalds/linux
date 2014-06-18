9a2b51b6ca93fc42e5676aa444c956f7506185db
Andrey Ryabinin <a.ryabinin@samsung.com>
2014-06-18 16:12:40 +0100

    ARM: 8078/1: get rid of hardcoded assumptions about kernel stack size
    Changing kernel stack size on arm is not as simple as it should be:
    1) THREAD_SIZE macro doesn't respect PAGE_SIZE and THREAD_SIZE_ORDER
    2) stack size is hardcoded in get_thread_info macro
    
    This patch fixes it by calculating THREAD_SIZE and thread_info address
    taking into account PAGE_SIZE and THREAD_SIZE_ORDER.
    
    Now changing stack size becomes simply changing THREAD_SIZE_ORDER.
    
    Signed-off-by: Andrey Ryabinin <a.ryabinin@samsung.com>
    Acked-by: Nicolas Pitre <nico@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/assembler.h
+++ arch/arm/include/asm/assembler.h
@@ -24,6 +24,8 @@
 #include <asm/domain.h>
 #include <asm/opcodes-virt.h>
 #include <asm/asm-offsets.h>
+#include <asm/page.h>
+#include <asm/thread_info.h>
 
 #define IOMEM(x)	(x)
 
@@ -179,10 +181,10 @@
  * Get current thread_info.
  */
 	.macro	get_thread_info, rd
- ARM(	mov	\rd, sp, lsr #13	)
+ ARM(	mov	\rd, sp, lsr #THREAD_SIZE_ORDER + PAGE_SHIFT	)
  THUMB(	mov	\rd, sp			)
- THUMB(	lsr	\rd, \rd, #13		)
-	mov	\rd, \rd, lsl #13
+ THUMB(	lsr	\rd, \rd, #THREAD_SIZE_ORDER + PAGE_SHIFT	)
+	mov	\rd, \rd, lsl #THREAD_SIZE_ORDER + PAGE_SHIFT
 	.endm
 
 /*
