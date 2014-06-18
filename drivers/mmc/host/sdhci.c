c938a53aac076df4b081ed98aee08617e06baedf
Ulf Hansson <ulf.hansson@linaro.org>
2014-06-18 09:14:08 +0200

    mmc: sdhci: Remove blank line
    While merging the sdhci patchset from Russell King, somehow a blank
    line was left behind. Let's correct the formatting.
    
    Cc: Chris Ball <chris@printf.net>
    Cc: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1534,7 +1534,6 @@ static void sdhci_do_set_ios(struct sdhci_host *host, struct mmc_ios *ios)
 			host->ops->set_clock(host, host->clock);
 		}
 
-
 		/* Reset SD Clock Enable */
 		clk = sdhci_readw(host, SDHCI_CLOCK_CONTROL);
 		clk &= ~SDHCI_CLOCK_CARD_EN;
