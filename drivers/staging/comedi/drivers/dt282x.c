7152f7d4b130a4b99c9a2dd1fc705a3b8ca8dcd5
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-06-20 13:12:58 -0700

    staging: comedi: dt282x: update the MODULE_DESCRIPTION
    Change the MODULE_DESCRIPTION to something more useful than the
    generic "Comedi low-level driver".
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/dt282x.c
+++ drivers/staging/comedi/drivers/dt282x.c
@@ -1305,5 +1305,5 @@ static struct comedi_driver dt282x_driver = {
 module_comedi_driver(dt282x_driver);
 
 MODULE_AUTHOR("Comedi http://www.comedi.org");
-MODULE_DESCRIPTION("Comedi low-level driver");
+MODULE_DESCRIPTION("Comedi driver for Data Translation DT2821 series");
 MODULE_LICENSE("GPL");
