dc87a84e316b486d7a24f744b79459c2fab6dc95
Martin Gumbrecht <martin.gumbrecht@googlemail.com>
2014-06-20 17:38:02 +0200

    Staging: bcm: Queue.h: fix checkpatch errors: whitespaces
    This patch improves coding style in Queue.h
    Whitespaces according to the coding guideline
    
    Signed-off-by: Martin Gumbrecht <martin.gumbrecht@googlemail.com>
    Signed-off-by: Christian Bay <christian.bay@studium.fau.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/Queue.h
+++ drivers/staging/bcm/Queue.h
@@ -6,24 +6,24 @@
 
 
 
-#define ENQUEUEPACKET(_Head, _Tail,_Packet)	\
+#define ENQUEUEPACKET(_Head, _Tail, _Packet)	\
 do {						\
-    if (!_Head) {                           \
-        	_Head = _Packet;                \
-	} 					\
-	else {                                  \
-        	(_Tail)->next = _Packet; 	\
-        }                                       \
-   	(_Packet)->next = NULL;  		\
-    _Tail = _Packet;                        \
-}while(0)
-#define DEQUEUEPACKET(Head, Tail )            	\
+	if (!_Head) {				\
+		_Head = _Packet;		\
+	}					\
+	else {					\
+		(_Tail)->next = _Packet;	\
+	}					\
+	(_Packet)->next = NULL;			\
+	_Tail = _Packet;			\
+} while (0)
+#define DEQUEUEPACKET(Head, Tail)		\
 do {						\
 	if (Head) {				\
-        if (!Head->next) {                      \
-        	Tail = NULL;                    \
-        }                                       \
-        Head = Head->next;                      \
-	}                \
+		if (!Head->next) {		\
+			Tail = NULL;		\
+		}				\
+		Head = Head->next;		\
+	}					\
 } while (0)
 #endif /* __QUEUE_H__ */
