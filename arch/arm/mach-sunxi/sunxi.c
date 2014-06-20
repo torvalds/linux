ac84b79fbd97117de54531efad2c526896be7d19
Chen-Yu Tsai <wens@csie.org>
2014-06-20 22:52:50 +0800

    ARM: sunxi: Introduce Allwinner A23 support
    The Allwinner A23 is a dual-core Cortex-A7-based SoC. It re-uses most of
    the IPs found in previous SoCs, notably the A31.
    
    Signed-off-by: Chen-Yu Tsai <wens@csie.org>
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- arch/arm/mach-sunxi/sunxi.c
+++ arch/arm/mach-sunxi/sunxi.c
@@ -53,3 +53,12 @@ static const char * const sun7i_board_dt_compat[] = {
 DT_MACHINE_START(SUN7I_DT, "Allwinner sun7i (A20) Family")
 	.dt_compat	= sun7i_board_dt_compat,
 MACHINE_END
+
+static const char * const sun8i_board_dt_compat[] = {
+	"allwinner,sun8i-a23",
+	NULL,
+};
+
+DT_MACHINE_START(SUN8I_DT, "Allwinner sun8i (A23) Family")
+	.dt_compat	= sun8i_board_dt_compat,
+MACHINE_END
