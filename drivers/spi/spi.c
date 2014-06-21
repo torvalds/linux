840e9c354f3492259a65ae235351cde1089a409e
Mark Brown <broonie@linaro.org>
2014-06-21 11:40:40 +0100

    spi: Remove unused variable
    Reported-by: kbuild test robot
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -345,7 +345,6 @@ static DEFINE_MUTEX(board_lock);
 struct spi_device *spi_alloc_device(struct spi_master *master)
 {
 	struct spi_device	*spi;
-	struct device		*dev = master->dev.parent;
 
 	if (!spi_master_get(master))
 		return NULL;
