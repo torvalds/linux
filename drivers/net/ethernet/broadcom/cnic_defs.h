d7afae05ad2e9c2cbf74e1419de5a0d6aa9c74f4
Jitendra Kalsaria <jitendra.kalsaria@qlogic.com>
2014-06-23 15:10:34 -0400

    cnic: Rebranding cnic driver.
    o QLogic has acquired the NetXtremeII products and drivers from Broadcom.
      This patch re-brands cnic driver as a QLogic driver
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/cnic_defs.h
+++ drivers/net/ethernet/broadcom/cnic_defs.h
@@ -1,7 +1,8 @@
 
-/* cnic.c: Broadcom CNIC core network driver.
+/* cnic.c: QLogic CNIC core network driver.
  *
  * Copyright (c) 2006-2014 Broadcom Corporation
+ * Copyright (c) 2014 QLogic Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
