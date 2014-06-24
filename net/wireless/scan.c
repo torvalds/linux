9d0c7e69e946442641bac8eb7babfc0120af7664
Dmitry Shmidt <dimitrysh@google.com>
2014-06-24 09:36:50 -0700

    ANDROID: net: wireless: Decrease scan entry expiration to avoid stall results
    Change-Id: I0e23ce45d78d7c17633670973f49943a5ed6032d
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- net/wireless/scan.c
+++ net/wireless/scan.c
@@ -70,7 +70,7 @@ module_param(bss_entries_limit, int, 0644);
 MODULE_PARM_DESC(bss_entries_limit,
                  "limit to number of scan BSS entries (per wiphy, default 1000)");
 
-#define IEEE80211_SCAN_RESULT_EXPIRE	(30 * HZ)
+#define IEEE80211_SCAN_RESULT_EXPIRE	(7 * HZ)
 
 static void bss_free(struct cfg80211_internal_bss *bss)
 {
