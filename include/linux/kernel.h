a69f5edb8ba20c87c5f7c96ec40581f9f51f2910
Joe Perches <joe@perches.com>
2014-06-24 11:20:48 -0700

    mac_pton: Use bool not int return
    Use bool instead of int as the return type.
    
    All uses are tested with !.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -501,7 +501,7 @@ static inline char * __deprecated pack_hex_byte(char *buf, u8 byte)
 extern int hex_to_bin(char ch);
 extern int __must_check hex2bin(u8 *dst, const char *src, size_t count);
 
-int mac_pton(const char *s, u8 *mac);
+bool mac_pton(const char *s, u8 *mac);
 
 /*
  * General tracing related utility functions - trace_printk(),
