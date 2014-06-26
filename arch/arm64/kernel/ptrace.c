9580f7022362bf717457b6b73de1e0ed0fede211
Mark Brown <broonie@linaro.org>
2014-06-26 10:49:57 +0100

    Revert "Merge remote-tracking branch 'lsk/v3.10/topic/arm64-fpsimd' into linux-linaro-lsk"
    This reverts commit b621c22123dc6b6facafe115f6364cf80172d551, reversing
    changes made to d7610b8eafd543c7f75568dcaeead439f31ffa53.

--- arch/arm64/kernel/ptrace.c
+++ arch/arm64/kernel/ptrace.c
@@ -521,7 +521,6 @@ static int fpr_set(struct task_struct *target, const struct user_regset *regset,
 		return ret;
 
 	target->thread.fpsimd_state.user_fpsimd = newstate;
-	fpsimd_flush_task_state(target);
 	return ret;
 }
 
@@ -769,7 +768,6 @@ static int compat_vfp_set(struct task_struct *target,
 		uregs->fpcr = fpscr & VFP_FPSCR_CTRL_MASK;
 	}
 
-	fpsimd_flush_task_state(target);
 	return ret;
 }
 
