db2fb7f202ba1d1b2c6532b359b7c763bf17fe72
Deng-Cheng Zhu <dengcheng.zhu@imgtec.com>
2014-06-26 12:11:39 -0700

    MIPS: KVM: Skip memory cleaning in kvm_mips_commpage_init()
    The commpage is allocated using kzalloc(), so there's no need of cleaning
    the memory of the kvm_mips_commpage struct and its internal mips_coproc.
    
    Reviewed-by: James Hogan <james.hogan@imgtec.com>
    Signed-off-by: Deng-Cheng Zhu <dengcheng.zhu@imgtec.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/mips/kvm/commpage.c
+++ arch/mips/kvm/commpage.c
@@ -28,9 +28,6 @@ void kvm_mips_commpage_init(struct kvm_vcpu *vcpu)
 {
 	struct kvm_mips_commpage *page = vcpu->arch.kseg0_commpage;
 
-	memset(page, 0, sizeof(struct kvm_mips_commpage));
-
 	/* Specific init values for fields */
 	vcpu->arch.cop0 = &page->cop0;
-	memset(vcpu->arch.cop0, 0, sizeof(struct mips_coproc));
 }
