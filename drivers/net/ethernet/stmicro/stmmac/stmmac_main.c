0acf16768740776feffac506ce93b1c06c059ac6
Vince Bridgers <vbridgers2013@gmail.com>
2014-06-29 20:34:51 -0500

    net: stmmac: add platform init/exit for Altera's ARM socfpga
    This patch adds platform init/exit functions and modifications to support
    suspend/resume for the Altera Cyclone 5 SOC Ethernet controller. The platform
    exit function puts the controller into reset using the socfpga reset
    controller driver. The platform init function sets up the Synopsys mac by
    first making sure the Ethernet controller is held in reset, programming the
    phy mode through external support logic, then deasserts reset through
    the socfpga reset manager driver.
    
    Signed-off-by: Vince Bridgers <vbridgers2013@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -2878,6 +2878,10 @@ int stmmac_suspend(struct net_device *ndev)
 		clk_disable_unprepare(priv->stmmac_clk);
 	}
 	spin_unlock_irqrestore(&priv->lock, flags);
+
+	priv->oldlink = 0;
+	priv->speed = 0;
+	priv->oldduplex = -1;
 	return 0;
 }
 
