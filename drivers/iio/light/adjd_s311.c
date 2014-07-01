4f38521b38e1b14621d9301198d2548873a64490
Peter Meerwald <pmeerw@pmeerw.net>
2014-07-01 22:29:00 +0100

    iio: Remove obsolete variable in adjd_s311 driver
    len variable become obsolete with
    iio_push_to_buffers_with_timestamp()
    
    Signed-off-by: Peter Meerwald <pmeerw@pmeerw.net>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/light/adjd_s311.c
+++ drivers/iio/light/adjd_s311.c
@@ -120,7 +120,6 @@ static irqreturn_t adjd_s311_trigger_handler(int irq, void *p)
 	struct iio_dev *indio_dev = pf->indio_dev;
 	struct adjd_s311_data *data = iio_priv(indio_dev);
 	s64 time_ns = iio_get_time_ns();
-	int len = 0;
 	int i, j = 0;
 
 	int ret = adjd_s311_req_data(indio_dev);
@@ -135,7 +134,6 @@ static irqreturn_t adjd_s311_trigger_handler(int irq, void *p)
 			goto done;
 
 		data->buffer[j++] = ret & ADJD_S311_DATA_MASK;
-		len += 2;
 	}
 
 	iio_push_to_buffers_with_timestamp(indio_dev, data->buffer, time_ns);
