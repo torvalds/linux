888210267e226f437abb7fd0604530032a98cafa
Minchan Kim <minchan@kernel.org>
2014-07-02 15:22:36 -0700

    zram: revalidate disk after capacity change
    Alexander reported mkswap on /dev/zram0 is failed if other process is
    opening the block device file.
    
    Step is as follows,
    
    0. Reset the unused zram device.
    1. Use a program that opens /dev/zram0 with O_RDWR and sleeps
       until killed.
    2. While that program sleeps, echo the correct value to
       /sys/block/zram0/disksize.
    3. Verify (e.g. in /proc/partitions) that the disk size is applied
       correctly. It is.
    4. While that program still sleeps, attempt to mkswap /dev/zram0.
       This fails: mkswap: error: swap area needs to be at least 40 KiB
    
    When I investigated, the size get by ioctl(fd, BLKGETSIZE64, xxx) on
    mkswap to get a size of blockdev was zero although zram0 has right size by
    2.
    
    The reason is zram didn't revalidate disk after changing capacity so that
    size of blockdev's inode is not uptodate until all of file is close.
    
    This patch should fix the BUG.
    
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Reported-by: Alexander E. Patrakov <patrakov@gmail.com>
    Tested-by: Alexander E. Patrakov <patrakov@gmail.com>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Acked-by: Jerome Marchand <jmarchan@redhat.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 2e32baea46ce542c561a519414c840295b229c8f)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -621,8 +621,10 @@ static void zram_reset_device(struct zram *zram, bool reset_capacity)
 	memset(&zram->stats, 0, sizeof(zram->stats));
 
 	zram->disksize = 0;
-	if (reset_capacity)
+	if (reset_capacity) {
 		set_capacity(zram->disk, 0);
+		revalidate_disk(zram->disk);
+	}
 	up_write(&zram->init_lock);
 }
 
@@ -663,6 +665,7 @@ static ssize_t disksize_store(struct device *dev,
 	zram->comp = comp;
 	zram->disksize = disksize;
 	set_capacity(zram->disk, zram->disksize >> SECTOR_SHIFT);
+	revalidate_disk(zram->disk);
 	up_write(&zram->init_lock);
 	return len;
 
