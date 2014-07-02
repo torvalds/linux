0e02dcae1ce238a2afda25da8cfd829c540088bb
Ian Abbott <abbotti@mev.co.uk>
2014-07-02 12:02:16 +0100

    staging: comedi: allow COMEDI_BUFINFO, COMEDI_CANCEL and COMEDI_POLL on locked subdevice
    Comedi subdevices can be locked for mostly exclusive access by a
    particular file object by means of the COMEDI_LOCK ioctl.  Some
    operations disregard the lock.  For example, the read() and write() file
    operations are allowed for the file object that set up an asynchronous
    command on the subdevice (making it "busy") regardless of whether it is
    locked.
    
    The COMEDI_BUFINFO, COMEDI_CANCEL and COMEDI_POLL ioctls are also
    associated with the file object that made the subdevice "busy" with a
    command.  They currently obey the exclusive access lock on the subdevice
    as well, but it makes more sense for them to ignore it like the read()
    and write() file operations.  Remove the exclusive subdevice access
    checks for these ioctls.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/comedi_fops.c
+++ drivers/staging/comedi/comedi_fops.c
@@ -968,9 +968,6 @@ static int do_bufinfo_ioctl(struct comedi_device *dev,
 
 	s = &dev->subdevices[bi.subdevice];
 
-	if (s->lock && s->lock != file)
-		return -EACCES;
-
 	async = s->async;
 
 	if (!async) {
@@ -1744,9 +1741,6 @@ static int do_cancel_ioctl(struct comedi_device *dev, unsigned int arg,
 	if (s->async == NULL)
 		return -EINVAL;
 
-	if (s->lock && s->lock != file)
-		return -EACCES;
-
 	if (!s->busy)
 		return 0;
 
@@ -1780,9 +1774,6 @@ static int do_poll_ioctl(struct comedi_device *dev, unsigned int arg,
 	if (arg >= dev->n_subdevices)
 		return -EINVAL;
 	s = &dev->subdevices[arg];
-
-	if (s->lock && s->lock != file)
-		return -EACCES;
 
 	if (!s->busy)
 		return 0;
