4f6ad60cf357e1663d84de052af68dfccd734ec7
Fabian Frederick <fabf@skynet.be>
2014-07-02 22:22:20 +0200

    ipconfig: add static to local variable
    ic_dev_xid is only used in ipconfig.c
    
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Alexey Kuznetsov <kuznet@ms2.inr.ac.ru>
    Cc: netdev@vger.kernel.org
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/ipconfig.c
+++ net/ipv4/ipconfig.c
@@ -143,7 +143,7 @@ __be32 ic_servaddr = NONE;	/* Boot server IP address */
 __be32 root_server_addr = NONE;	/* Address of NFS server */
 u8 root_server_path[256] = { 0, };	/* Path to mount as root */
 
-__be32 ic_dev_xid;		/* Device under configuration */
+static __be32 ic_dev_xid;		/* Device under configuration */
 
 /* vendor class identifier */
 static char vendor_class_identifier[253] __initdata;
