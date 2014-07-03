104a1c13ac66e40cf8c6ae74d76ff14ff24b9b01
Alex Williamson <alex.williamson@redhat.com>
2014-07-03 09:51:18 -0600

    iommu/core: Create central IOMMU group lookup/creation interface
    Currently each IOMMU driver that supports IOMMU groups has its own
    code for discovering the base device used in grouping.  This code
    is generally not specific to the IOMMU hardware, but to the bus of
    the devices managed by the IOMMU.  We can therefore create a common
    interface for supporting devices on different buses.
    
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -181,6 +181,7 @@ extern int iommu_group_register_notifier(struct iommu_group *group,
 extern int iommu_group_unregister_notifier(struct iommu_group *group,
 					   struct notifier_block *nb);
 extern int iommu_group_id(struct iommu_group *group);
+extern struct iommu_group *iommu_group_get_for_dev(struct device *dev);
 
 extern int iommu_domain_get_attr(struct iommu_domain *domain, enum iommu_attr,
 				 void *data);
