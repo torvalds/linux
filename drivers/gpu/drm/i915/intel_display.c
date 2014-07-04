930e8c9e79acc0e3f1702dcd2345f4584818c2e9
Paulo Zanoni <paulo.r.zanoni@intel.com>
2014-07-04 13:38:34 -0300

    drm/i915: check the power domains in ironlake_get_pipe_config()
    Just like we already do in haswell_get_pipe_config(). This should
    prevent some WARNs when we run pm_rpm on SNB.
    
    Testcase: igt/pm_rpm
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=80463
    Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Reviewed-by: Damien Lespiau <damien.lespiau@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -7240,6 +7240,10 @@ static bool ironlake_get_pipe_config(struct intel_crtc *crtc,
 	struct drm_i915_private *dev_priv = dev->dev_private;
 	uint32_t tmp;
 
+	if (!intel_display_power_enabled(dev_priv,
+					 POWER_DOMAIN_PIPE(crtc->pipe)))
+		return false;
+
 	pipe_config->cpu_transcoder = (enum transcoder) crtc->pipe;
 	pipe_config->shared_dpll = DPLL_ID_PRIVATE;
 
