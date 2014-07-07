6befda9a272b98bfb1dc772efc3564644cbfb270
Steffen Trumtrar <s.trumtrar@pengutronix.de>
2014-07-07 11:41:27 +0200

    ARM: i.MX53: globally disable supervisor protect
    Most peripherals on the i.MX53 have an
      Off-Platform Peripheral Access Control Register (OPACR)
    in which the access rights (together with the MPROT registers) can be declared.
    However, this does not seem to work for example for SSI1+SDMA, because the
    supervisor bit is not set for the SDMA unit.
    It does work for SSI2, the QSB for example uses SSI2 for its audio. But SSI2 only
    works because it does NOT have an OPACR.
    
    The right solution would be to fix the access rights for the SDMA, but the unit
    responsible for this is the Central Security Unit (CSU), which of course is NOT
    documented. So, until documentation for this is openly available, turn off the
    supervisor protection because it cripples the hardware.
    
    Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>
    Signed-off-by: Shawn Guo <shawn.guo@freescale.com>

--- arch/arm/mach-imx/mach-imx53.c
+++ arch/arm/mach-imx/mach-imx53.c
@@ -34,6 +34,8 @@ static void __init imx53_dt_init(void)
 	imx_src_init();
 
 	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
+
+	imx_aips_allow_unprivileged_access("fsl,imx53-aipstz");
 }
 
 static void __init imx53_init_late(void)
