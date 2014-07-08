bf33eb1aef23e8049cd222471d35b0988c420b18
Pranith Kumar <bobby.prani@gmail.com>
2014-07-08 18:26:10 -0400

    rcu: Fix sparse warning about rcu_batches_completed_preempt() being non-static
    fix sparse warning about rcu_batches_completed_preempt() being non-static by
    marking it as static
    
    Signed-off-by: Pranith Kumar <bobby.prani@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/rcu/tree_plugin.h
+++ kernel/rcu/tree_plugin.h
@@ -134,7 +134,7 @@ static void __init rcu_bootup_announce(void)
  * Return the number of RCU-preempt batches processed thus far
  * for debug and statistics.
  */
-long rcu_batches_completed_preempt(void)
+static long rcu_batches_completed_preempt(void)
 {
 	return rcu_preempt_state.completed;
 }
