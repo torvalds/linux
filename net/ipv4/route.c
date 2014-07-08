455b09d66a9ccfc572497ae88375ae343ff9ae66
Sreeram Ramachandran <sreeram@google.com>
2014-07-08 11:57:14 -0700

    Handle 'sk' being NULL in UID-based routing.
    Bug: 15413527
    Change-Id: Iab1fae9da6053b284591628ef1de878761b137b1
    Signed-off-by: Sreeram Ramachandran <sreeram@google.com>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -517,7 +517,7 @@ static void __build_flow_key(struct flowi4 *fl4, struct sock *sk,
 			   RT_SCOPE_UNIVERSE, prot,
 			   flow_flags,
 			   iph->daddr, iph->saddr, 0, 0,
-			   sock_i_uid(sk));
+			   sk ? sock_i_uid(sk) : 0);
 }
 
 static void build_skb_flow_key(struct flowi4 *fl4, const struct sk_buff *skb,
