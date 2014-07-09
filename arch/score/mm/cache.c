51de2f11f5d4d51ad3a77ea3ef67e7b66f6709f5
Chen Gang <gang.chen.5i5j@gmail.com>
2014-07-09 16:48:45 +0800

    arch: score: Export necessary symbols in related files
    'csum_partial_copy_from_user' and 'flush_dcache_page' are also needed by
    outside modules, so need export them in the related files.
    
    The related error (with allmodconfig under score):
    
        MODPOST 1365 modules
      ERROR: "csum_partial_copy_from_user" [net/rxrpc/af-rxrpc.ko] undefined!
      ERROR: "flush_dcache_page" [net/sunrpc/sunrpc.ko] undefined!
    
    Acked-by: Lennox Wu <lennox.wu@gmail.com>
    Signed-off-by: Chen Gang <gang.chen.5i5j@gmail.com>

--- arch/score/mm/cache.c
+++ arch/score/mm/cache.c
@@ -72,6 +72,7 @@ void flush_dcache_page(struct page *page)
 	addr = (unsigned long) page_address(page);
 	flush_data_cache_page(addr);
 }
+EXPORT_SYMBOL(flush_dcache_page);
 
 /* called by update_mmu_cache. */
 void __update_cache(struct vm_area_struct *vma, unsigned long address,
