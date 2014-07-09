5d8ba82c3a1f10b77bf39ee3e7670d6789a8d149
Li Zefan <lizefan@huawei.com>
2014-07-09 16:49:12 +0800

    cpuset: allow writing offlined masks to cpuset.cpus/mems
    As the configured masks won't be limited by its parent, and the top
    cpuset's masks won't change when hotplug happens, it's natural to
    allow writing offlined masks to the configured masks.
    
    If on default hierarchy:
    
    	# echo 0 > /sys/devices/system/cpu/cpu1/online
    	# mkdir /cpuset/sub
    	# echo 1 > /cpuset/sub/cpuset.cpus
    	# cat /cpuset/sub/cpuset.cpus
    	1
    
    If on legacy hierarchy:
    
    	# echo 0 > /sys/devices/system/cpu/cpu1/online
    	# mkdir /cpuset/sub
    	# echo 1 > /cpuset/sub/cpuset.cpus
    	-bash: echo: write error: Invalid argument
    
    Note the checks don't need to be gated by cgroup_on_dfl, because we've
    initialized top_cpuset.{cpus,mems}_allowed accordingly in cpuset_bind().
    
    Signed-off-by: Li Zefan <lizefan@huawei.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -929,7 +929,8 @@ static int update_cpumask(struct cpuset *cs, struct cpuset *trialcs,
 		if (retval < 0)
 			return retval;
 
-		if (!cpumask_subset(trialcs->cpus_allowed, cpu_active_mask))
+		if (!cpumask_subset(trialcs->cpus_allowed,
+				    top_cpuset.cpus_allowed))
 			return -EINVAL;
 	}
 
@@ -1186,8 +1187,8 @@ static int update_nodemask(struct cpuset *cs, struct cpuset *trialcs,
 			goto done;
 
 		if (!nodes_subset(trialcs->mems_allowed,
-				node_states[N_MEMORY])) {
-			retval =  -EINVAL;
+				  top_cpuset.mems_allowed)) {
+			retval = -EINVAL;
 			goto done;
 		}
 	}
