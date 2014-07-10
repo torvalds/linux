67db20f0bbdd2d649f2126a2a75cf1e3bffa1377
navin patidar <navin.patidar@gmail.com>
2014-07-10 09:12:27 +0530

    staging: rtl8188eu: Remove unused functions rtw_cbuf_[full, empty, push, pop]()
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/osdep_service.h
+++ drivers/staging/rtl8188eu/include/osdep_service.h
@@ -194,10 +194,4 @@ struct rtw_cbuf {
 	u32 size;
 	void *bufs[0];
 };
-
-bool rtw_cbuf_full(struct rtw_cbuf *cbuf);
-bool rtw_cbuf_empty(struct rtw_cbuf *cbuf);
-bool rtw_cbuf_push(struct rtw_cbuf *cbuf, void *buf);
-void *rtw_cbuf_pop(struct rtw_cbuf *cbuf);
-
 #endif
