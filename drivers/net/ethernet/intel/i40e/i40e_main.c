e8e724db0a559f666fc552e65294ce7b5b837eae
Catherine Sullivan <catherine.sullivan@intel.com>
2014-07-10 07:58:26 +0000

    i40e/i40evf: Bump i40e & i40evf version
    Bump versions for i40e to 1.0.4 and i40evf to 1.0.1.
    
    Change-ID: I960c04da2c91bdf1d02f8e5011e68c34a634122d
    Signed-off-by: Catherine Sullivan <catherine.sullivan@intel.com>
    Tested-By: Jim Young <jamesx.m.young@intel.com>
    Tested-by: Sibai Li <sibai.li@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -37,9 +37,9 @@ static const char i40e_driver_string[] =
 
 #define DRV_KERN "-k"
 
-#define DRV_VERSION_MAJOR 0
-#define DRV_VERSION_MINOR 4
-#define DRV_VERSION_BUILD 21
+#define DRV_VERSION_MAJOR 1
+#define DRV_VERSION_MINOR 0
+#define DRV_VERSION_BUILD 4
 #define DRV_VERSION __stringify(DRV_VERSION_MAJOR) "." \
 	     __stringify(DRV_VERSION_MINOR) "." \
 	     __stringify(DRV_VERSION_BUILD)    DRV_KERN
