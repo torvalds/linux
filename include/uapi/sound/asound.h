5646eda5851e6cfdfa22d41895e3f5daffa643d3
Takashi Iwai <tiwai@suse.de>
2014-07-10 09:50:19 +0200

    ALSA: pcm: Add timestamp type to sw_params
    For allowing adjusting the timestamp type on the fly, add it to
    sw_params.  The existing ioctl is still kept for compatibility.
    
    Along with this, increment the PCM protocol version.
    
    The extension was suggested by Clemens Ladisch.
    
    Acked-by: Jaroslav Kysela <perex@perex.cz>
    Reviewed-by: Mark Brown <broonie@linaro.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/uapi/sound/asound.h
+++ include/uapi/sound/asound.h
@@ -139,7 +139,7 @@ struct snd_hwdep_dsp_image {
  *                                                                           *
  *****************************************************************************/
 
-#define SNDRV_PCM_VERSION		SNDRV_PROTOCOL_VERSION(2, 0, 11)
+#define SNDRV_PCM_VERSION		SNDRV_PROTOCOL_VERSION(2, 0, 12)
 
 typedef unsigned long snd_pcm_uframes_t;
 typedef signed long snd_pcm_sframes_t;
@@ -391,7 +391,9 @@ struct snd_pcm_sw_params {
 	snd_pcm_uframes_t silence_threshold;	/* min distance from noise for silence filling */
 	snd_pcm_uframes_t silence_size;		/* silence block size */
 	snd_pcm_uframes_t boundary;		/* pointers wrap point */
-	unsigned char reserved[64];		/* reserved for future */
+	unsigned int tstamp_type;		/* timestamp type */
+	int pads;				/* alignment, reserved */
+	unsigned char reserved[56];		/* reserved for future */
 };
 
 struct snd_pcm_channel_info {
