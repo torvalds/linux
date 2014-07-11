f736d9985e69e72d9a2ebfd131a72ee8f870c6f3
Nikita Edward Baruzdin <nebaruzdin@gmail.com>
2014-07-11 16:13:21 +0400

    can: netlink: Remove space before tab
    Fixes the corresponing checkpatch.pl warning.
    
    Signed-off-by: Nikita Edward Baruzdin <nebaruzdin@gmail.com>
    Acked-by: Oliver Hartkopp <socketcan@hartkopp.net>
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- include/uapi/linux/can/netlink.h
+++ include/uapi/linux/can/netlink.h
@@ -92,7 +92,7 @@ struct can_ctrlmode {
 };
 
 #define CAN_CTRLMODE_LOOPBACK		0x01	/* Loopback mode */
-#define CAN_CTRLMODE_LISTENONLY		0x02 	/* Listen-only mode */
+#define CAN_CTRLMODE_LISTENONLY		0x02	/* Listen-only mode */
 #define CAN_CTRLMODE_3_SAMPLES		0x04	/* Triple sampling mode */
 #define CAN_CTRLMODE_ONE_SHOT		0x08	/* One-Shot mode */
 #define CAN_CTRLMODE_BERR_REPORTING	0x10	/* Bus-error reporting */
