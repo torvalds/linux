913827ee9aa6bc4731c3f954581fab067c448b89
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2014-07-12 01:09:15 -0700

    staging: lustre: fid: fid_request.c: checkpatch cleanup: align arguments to parenthesis
    whitespace changes only - git diff -w shows no difference
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/fid/fid_request.c
+++ drivers/staging/lustre/lustre/fid/fid_request.c
@@ -197,7 +197,7 @@ static int seq_client_alloc_seq(const struct lu_env *env,
 		rc = seq_client_alloc_meta(env, seq);
 		if (rc) {
 			CERROR("%s: Can't allocate new meta-sequence, rc %d\n",
-				seq->lcs_name, rc);
+			       seq->lcs_name, rc);
 			return rc;
 		} else {
 			CDEBUG(D_INFO, "%s: New range - "DRANGE"\n",
@@ -267,14 +267,14 @@ int seq_client_get_seq(const struct lu_env *env,
 	rc = seq_client_alloc_seq(env, seq, seqnr);
 	if (rc) {
 		CERROR("%s: Can't allocate new sequence, rc %d\n",
-			seq->lcs_name, rc);
+		       seq->lcs_name, rc);
 		seq_fid_alloc_fini(seq);
 		mutex_unlock(&seq->lcs_mutex);
 		return rc;
 	}
 
 	CDEBUG(D_INFO, "%s: allocate sequence [0x%16.16"LPF64"x]\n",
-			seq->lcs_name, *seqnr);
+	       seq->lcs_name, *seqnr);
 
 	/* Since the caller require the whole seq,
 	 * so marked this seq to be used */
@@ -330,14 +330,14 @@ int seq_client_alloc_fid(const struct lu_env *env,
 		rc = seq_client_alloc_seq(env, seq, &seqnr);
 		if (rc) {
 			CERROR("%s: Can't allocate new sequence, rc %d\n",
-				seq->lcs_name, rc);
+			       seq->lcs_name, rc);
 			seq_fid_alloc_fini(seq);
 			mutex_unlock(&seq->lcs_mutex);
 			return rc;
 		}
 
 		CDEBUG(D_INFO, "%s: Switch to sequence [0x%16.16"LPF64"x]\n",
-				seq->lcs_name, seqnr);
+		       seq->lcs_name, seqnr);
 
 		seq->lcs_fid.f_oid = LUSTRE_FID_INIT_OID;
 		seq->lcs_fid.f_seq = seqnr;
@@ -429,7 +429,7 @@ static int seq_client_proc_init(struct lu_client_seq *seq)
 			      seq_client_proc_list, seq);
 	if (rc) {
 		CERROR("%s: Can't init sequence manager proc, rc %d\n",
-			seq->lcs_name, rc);
+		       seq->lcs_name, rc);
 		GOTO(out_cleanup, rc);
 	}
 
