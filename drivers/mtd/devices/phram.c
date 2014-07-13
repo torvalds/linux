c95777a4bb5daca9857157cc18cb7b8d470364aa
Rob Ward <robert.ward114@googlemail.com>
2014-07-13 20:39:01 +0100

    mtd: phram: Fix whitespace issues
    Fix various whitespace issues.
    
    No functional changes.
    
    Signed-off-by: Rob Ward <robert.ward114@googlemail.com>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/devices/phram.c
+++ drivers/mtd/devices/phram.c
@@ -193,6 +193,7 @@ static int parse_name(char **pname, const char *token)
 static inline void kill_final_newline(char *str)
 {
 	char *newline = strrchr(str, '\n');
+
 	if (newline && !newline[1])
 		*newline = 0;
 }
@@ -231,7 +232,7 @@ static int phram_setup(const char *val)
 	strcpy(str, val);
 	kill_final_newline(str);
 
-	for (i=0; i<3; i++)
+	for (i = 0; i < 3; i++)
 		token[i] = strsep(&str, ",");
 
 	if (str)
