9c8958bc24e241de2c0d4740e6dea861fe47daa5
Daniel Vetter <daniel.vetter@ffwll.ch>
2014-07-14 19:35:31 +0200

    drm/i915: Track the primary plane correctly when reassigning planes
    commit 98ec77397a5c68ce753dc283aaa6f4742328bcdd
    Author: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Date:   Wed Apr 30 17:43:01 2014 +0300
    
        drm/i915: Make primary_enabled match the actual hardware state
    
    introduced more accurate tracking of the primary plane and some
    checks. It missed the plane->pipe reassignement code for gen2/3
    though, which the checks caught and resulted in WARNING backtraces.
    
    Since we only use this path if the plane is on and on the wrong pipe
    we can just always set the tracking bit to "enabled".
    
    Reported-and-tested-by: Paul Bolle <pebolle@tiscali.nl>
    Cc: Paul Bolle <pebolle@tiscali.nl>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -11914,6 +11914,7 @@ static void intel_sanitize_crtc(struct intel_crtc *crtc)
 		 * ...  */
 		plane = crtc->plane;
 		crtc->plane = !plane;
+		crtc->primary_enabled = true;
 		dev_priv->display.crtc_disable(&crtc->base);
 		crtc->plane = plane;
 
