81691503b2090e5238586224af26a26973f6c254
Steve French <smfrench@gmail.com>
2014-07-14 23:28:37 -0500

    Update cifs version
    to 2.04
    
    Signed-off-by: Steve French <smfrench@gmail.com>

--- fs/cifs/cifsfs.h
+++ fs/cifs/cifsfs.h
@@ -136,5 +136,5 @@ extern long cifs_ioctl(struct file *filep, unsigned int cmd, unsigned long arg);
 extern const struct export_operations cifs_export_ops;
 #endif /* CONFIG_CIFS_NFSD_EXPORT */
 
-#define CIFS_VERSION   "2.03"
+#define CIFS_VERSION   "2.04"
 #endif				/* _CIFSFS_H */
