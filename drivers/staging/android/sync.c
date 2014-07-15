6a44b50f9819571c2b871bffba107defec556a87
Lucas Tanure <tanure@linux.com>
2014-07-15 00:32:35 -0300

    staging: android: Clean up else statement from sync_fence_poll()
    Kernel coding style. Remove useless else statement after return.
    
    Signed-off-by: Lucas Tanure <tanure@linux.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/sync.c
+++ drivers/staging/android/sync.c
@@ -687,8 +687,7 @@ static unsigned int sync_fence_poll(struct file *file, poll_table *wait)
 		return POLLIN;
 	else if (fence->status < 0)
 		return POLLERR;
-	else
-		return 0;
+	return 0;
 }
 
 static long sync_fence_ioctl_wait(struct sync_fence *fence, unsigned long arg)
