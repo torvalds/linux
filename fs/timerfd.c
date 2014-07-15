5442e9fbd7c23172a1c9bc736629cd123a9923f0
Cyrill Gorcunov <gorcunov@openvz.org>
2014-07-16 01:54:54 +0400

    timerfd: Implement timerfd_ioctl method to restore timerfd_ctx::ticks, v3
    The read() of timerfd files allows to fetch the number of timer ticks
    while there is no way to set it back from userspace.
    
    To restore the timer's state as it was at checkpoint moment we need
    a path to bring @ticks back. Initially I thought about writing ticks
    back via write() interface but it seems such API is somehow obscure.
    
    Instead implement timerfd_ioctl() method with TFD_IOC_SET_TICKS
    command which allows to adjust @ticks into non-zero value waking
    up the waiters.
    
    I wrapped code with CONFIG_CHECKPOINT_RESTORE which can be
    dropped off if there users except c/r camp appear.
    
    v2 (by akpm@):
     - Use define timerfd_ioctl NULL for non c/r config
    
    v3:
     - Use copy_from_user for @ticks fetching since
       not all arch support get_user for 8 byte argument
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Michael Kerrisk <mtk.manpages@gmail.com>
    Cc: Andrey Vagin <avagin@openvz.org>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Christopher Covington <cov@codeaurora.org>
    Cc: Pavel Emelyanov <xemul@parallels.com>
    Cc: Vladimir Davydov <vdavydov@parallels.com>
    Link: http://lkml.kernel.org/r/20140715215703.285617923@openvz.org
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- fs/timerfd.c
+++ fs/timerfd.c
@@ -315,12 +315,49 @@ static int timerfd_show(struct seq_file *m, struct file *file)
 #define timerfd_show NULL
 #endif
 
+#ifdef CONFIG_CHECKPOINT_RESTORE
+static long timerfd_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+	struct timerfd_ctx *ctx = file->private_data;
+	int ret = 0;
+
+	switch (cmd) {
+	case TFD_IOC_SET_TICKS: {
+		u64 ticks;
+
+		if (copy_from_user(&ticks, (u64 __user *)arg, sizeof(ticks)))
+			return -EFAULT;
+		if (!ticks)
+			return -EINVAL;
+
+		spin_lock_irq(&ctx->wqh.lock);
+		if (!timerfd_canceled(ctx)) {
+			ctx->ticks = ticks;
+			if (ticks)
+				wake_up_locked(&ctx->wqh);
+		} else
+			ret = -ECANCELED;
+		spin_unlock_irq(&ctx->wqh.lock);
+		break;
+	}
+	default:
+		ret = -ENOTTY;
+		break;
+	}
+
+	return ret;
+}
+#else
+#define timerfd_ioctl NULL
+#endif
+
 static const struct file_operations timerfd_fops = {
 	.release	= timerfd_release,
 	.poll		= timerfd_poll,
 	.read		= timerfd_read,
 	.llseek		= noop_llseek,
 	.show_fdinfo	= timerfd_show,
+	.unlocked_ioctl	= timerfd_ioctl,
 };
 
 static int timerfd_fget(int fd, struct fd *p)
