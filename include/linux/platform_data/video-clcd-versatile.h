ec4c4d877becf1c224f45347f4fc0016765e00d0
Arnd Bergmann <arnd@arndb.de>
2014-07-15 09:58:06 +0200

    video: fix up versatile CLCD helper move
    commit 11c32d7b6274cb0f ("video: move Versatile CLCD helpers")
    moved files out of the plat-versatile directory but in the process
    got a few of the dependencies wrong:
    
    - If CONFIG_FB is not set, the file no longer gets built, resulting
      in a link error
    - If CONFIG_FB or CONFIG_FB_ARMCLCD are disabled, we also get a
      Kconfig warning for incorrect dependencies due to the symbol
      being 'select'ed from the platform Kconfig.
    - When the file is not built, we also get a link error for missing
      symbols.
    
    This patch should fix all three, by removing the 'select' statements,
    changing the Kconfig description of the symbol to be enabled in
    exactly the right configurations, and adding inline stub functions
    for the case when the framebuffer driver is disabled.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>

--- include/linux/platform_data/video-clcd-versatile.h
+++ include/linux/platform_data/video-clcd-versatile.h
@@ -1,9 +1,27 @@
 #ifndef PLAT_CLCD_H
 #define PLAT_CLCD_H
 
+#ifdef CONFIG_PLAT_VERSATILE_CLCD
 struct clcd_panel *versatile_clcd_get_panel(const char *);
 int versatile_clcd_setup_dma(struct clcd_fb *, unsigned long);
 int versatile_clcd_mmap_dma(struct clcd_fb *, struct vm_area_struct *);
 void versatile_clcd_remove_dma(struct clcd_fb *);
+#else
+static inline struct clcd_panel *versatile_clcd_get_panel(const char *s)
+{
+	return NULL;
+}
+static inline int versatile_clcd_setup_dma(struct clcd_fb *fb, unsigned long framesize)
+{
+	return -ENODEV;
+}
+static inline int versatile_clcd_mmap_dma(struct clcd_fb *fb, struct vm_area_struct *vm)
+{
+	return -ENODEV;
+}
+static inline void versatile_clcd_remove_dma(struct clcd_fb *fb)
+{
+}
+#endif
 
 #endif
