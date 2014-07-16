3c9c2baa8b74ea3834503ce7df899325c3bd4303
Ley Foon Tan <lftan@altera.com>
2014-07-16 10:40:06 +0800

    FogBugz #216208: nios2: fix warning from get_fb_unmapped_area
    arch/nios2/kernel/sys_nios2.c: In function 'get_fb_unmapped_area':
    
    arch/nios2/kernel/sys_nios2.c:92:2: warning: return makes integer
    from pointer without a cast [enabled by default]
    
    Signed-off-by: Ley Foon Tan <lftan@altera.com>

--- arch/nios2/kernel/sys_nios2.c
+++ arch/nios2/kernel/sys_nios2.c
@@ -89,7 +89,7 @@ unsigned long get_fb_unmapped_area(struct file *filp, unsigned long orig_addr,
 {
 
 	struct fb_info *info = filp->private_data;
-	return info->screen_base;
+	return (unsigned long)info->screen_base;
 }
 EXPORT_SYMBOL(get_fb_unmapped_area);
 #endif /* CONFIG_FB */
