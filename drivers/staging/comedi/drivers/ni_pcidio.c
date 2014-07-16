fd28703c76977d7a5937d56865dc73ab49b8b405
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-07-16 11:02:07 -0700

    staging: comedi: ni_pcidio: checkpatch.pl cleanup (missing blank line)
    Fix the checkpatch.pl warning:
    
    WARNING: Missing a blank line after declarations
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_pcidio.c
+++ drivers/staging/comedi/drivers/ni_pcidio.c
@@ -711,6 +711,7 @@ static int ni_pcidio_cmd(struct comedi_device *dev, struct comedi_subdevice *s)
 
 	{
 		int retval = setup_mite_dma(dev, s);
+
 		if (retval)
 			return retval;
 	}
@@ -853,6 +854,7 @@ static int pci_6534_load_fpga(struct comedi_device *dev,
 	}
 	for (j = 0; j + 1 < data_len;) {
 		unsigned int value = data[j++];
+
 		value |= data[j++] << 8;
 		writew(value,
 		       devpriv->mite->daq_io_addr + Firmware_Data_Register);
