06ffc7789e76a095e85814dbcf7b660344f6b679
Paulo Zanoni <paulo.r.zanoni@intel.com>
2014-07-17 17:43:46 -0300

    d rm/i915: freeze display before the interrupts and GT
    Since we started using intel_runtime_pm_disable_interrupts() at normal
    (non-runtime) suspend/resume, we had to remove a WARN from
    ironlake_disable_display_irq to avoid a case where we were doing the
    correct thing and the WARN was not really needed. The problem is that
    the WARN was useful in other cases, and its removal can hide some bugs
    that we would catch automatically.
    
    To be able to add back the WARN, we have to call intel_crtc_control()
    before interrupts are disabled, which is what this patch currently
    does.
    
    Also notice that Ville's patch from the Watermarks series "drm/i915:
    Leave interrupts enabled while disabling crtcs during suspend" also
    did a change that's equivalent to the one we're doing on this patch,
    with the exception that its original patch, when applied to the
    current tree, procduces a WARN.
    
    Related commits:
    
    commit daa390e5ee45cc051d6bf37b296901f2f92b002d
    Author: Jesse Barnes <jbarnes@virtuousgeek.org>
        drm/i915: don't warn if IRQs are disabled when shutting down display IRQs
    
    commit e11aa362308f5de467ce355a2a2471321b15a35c
    Author: Jesse Barnes <jbarnes@virtuousgeek.org>
        drm/i915: use runtime irq suspend/resume in freeze/thaw
    
    Note that the function part of this patch has already been done in
    
    commit 0e32b39ceed665bfa4a77a4bc307b6652b991632
    Author: Dave Airlie <airlied@redhat.com>
    Date:   Fri May 2 14:02:48 2014 +1000
    
        drm/i915: add DP 1.2 MST support (v0.7)
    
    with the fixup
    
    commit 09b64267c1f72f2670fcde9f11e5453ce365ca23
    Author: Dave Airlie <airlied@redhat.com>
    Date:   Wed Jul 23 14:25:24 2014 +1000
    
        drm/i915: don't suspend gt until after we disable irqs and display (v2)
    
    so all that's left from Paulo's patch is reinstating the WARNING.
    
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Cc: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Reviewed-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    [danvet: Explain conflict resolution with Dave's DP MST patches with a
    note in the commit message.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_irq.c
+++ drivers/gpu/drm/i915/i915_irq.c
@@ -151,7 +151,7 @@ ironlake_disable_display_irq(struct drm_i915_private *dev_priv, u32 mask)
 {
 	assert_spin_locked(&dev_priv->irq_lock);
 
-	if (!intel_irqs_enabled(dev_priv))
+	if (WARN_ON(!intel_irqs_enabled(dev_priv)))
 		return;
 
 	if ((dev_priv->irq_mask & mask) != mask) {
