23fa5c2caae08f919d906b1064b9fdc352b3024e
Veaceslav Falico <vfalico@gmail.com>
2014-07-17 12:04:08 +0200

    bonding: destroy proc directory only after all bonds are gone
    Currently we might arrive to bond_net_exit() with some bonds left (that
    were created while the module is unloading). We take care of that by
    destroying sysfs (the last possibility to add new bonds) and then
    destroying all the remaining bonds.
    
    However, we destroy the /proc/net/bonding directory before destroying those
    last bonds, and get a warning that we're trying to destroy a non-empty
    proc directory (containing /proc/net/bonding/bondX).
    
    Fix this by moving bond_destroy_proc_dir() after all the bonds are
    destroyed, so that we're sure that no bonds exist.
    
    CC: Jay Vosburgh <j.vosburgh@gmail.com>
    CC: Andy Gospodarek <andy@greyhouse.net>
    Signed-off-by: Veaceslav Falico <vfalico@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -4448,7 +4448,6 @@ static void __net_exit bond_net_exit(struct net *net)
 	LIST_HEAD(list);
 
 	bond_destroy_sysfs(bn);
-	bond_destroy_proc_dir(bn);
 
 	/* Kill off any bonds created after unregistering bond rtnl ops */
 	rtnl_lock();
@@ -4456,6 +4455,8 @@ static void __net_exit bond_net_exit(struct net *net)
 		unregister_netdevice_queue(bond->dev, &list);
 	unregister_netdevice_many(&list);
 	rtnl_unlock();
+
+	bond_destroy_proc_dir(bn);
 }
 
 static struct pernet_operations bond_net_ops = {
