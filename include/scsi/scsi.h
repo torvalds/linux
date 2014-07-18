7a14c1c3319608154da8712e4174d56ffb2f7b8d
Christoph Hellwig <hch@lst.de>
2014-07-18 16:51:51 +0200

    scsi: add a symbolic name for the ZBC device type
    Make sure we have a symbolic name for the ZBC type available,
    so that e.g. patch for a SATA to translate ZAC commands can
    make use of it.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Reviewed-by: Martin K. Petersen <martin.petersen@oracle.com>

--- include/scsi/scsi.h
+++ include/scsi/scsi.h
@@ -332,6 +332,7 @@ static inline int scsi_status_is_good(int status)
 #define TYPE_ENCLOSURE      0x0d    /* Enclosure Services Device */
 #define TYPE_RBC	    0x0e
 #define TYPE_OSD            0x11
+#define TYPE_ZBC            0x14
 #define TYPE_NO_LUN         0x7f
 
 /* SCSI protocols; these are taken from SPC-3 section 7.5 */
