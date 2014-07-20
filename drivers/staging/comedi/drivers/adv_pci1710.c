1d0117ff330696092ad8949d9b7736774fe191be
Sam Asadi <asadi.samuel@gmail.com>
2014-07-20 17:06:21 +0300

    Staging: comedi: adv_pci1710: a line over 80 characters fixed
    An issue in 'line over 80 characters' fixed and over-lined comment
    removed to the previous line.
    
    Signed-off-by: Sam Asadi <asadi.samuel@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/adv_pci1710.c
+++ drivers/staging/comedi/drivers/adv_pci1710.c
@@ -1058,7 +1058,8 @@ static int pci171x_reset(struct comedi_device *dev)
 	pci171x_start_pacer(dev, false);
 	devpriv->da_ranges = 0;
 	if (this_board->n_aochan) {
-		outb(devpriv->da_ranges, dev->iobase + PCI171x_DAREF);	/*  set DACs to 0..5V */
+		/* set DACs to 0..5V */
+		outb(devpriv->da_ranges, dev->iobase + PCI171x_DAREF);
 		outw(0, dev->iobase + PCI171x_DA1);	/*  set DA outputs to 0V */
 		devpriv->ao_data[0] = 0x0000;
 		if (this_board->n_aochan > 1) {
