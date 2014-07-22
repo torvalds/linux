d809b68ff7493ced086dc6cddd69e564b713f864
huang zhibao <hzb@rock-chips.com>
2014-07-22 15:40:52 +0800

    input:ir:add remotectl support

---
+++ drivers/input/remotectl/rockchip_pwm_remotectl.c
@@ -0,0 +1,453 @@
+#include <linux/clk.h>
+#include <linux/io.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/platform_device.h>
+#include <linux/input.h>
+#include <linux/workqueue.h>
+#include <linux/wakelock.h>
+#include "rockchip_pwm_remotectl.h"
+
+
+
+/*sys/module/rk_pwm_remotectl/parameters,
+modify code_print to change the value*/
+
+static int rk_remote_print_code;
+module_param_named(code_print, rk_remote_print_code, int, 0644);
+#define DBG_CODE(args...) \
+	do { \
+		if (rk_remote_print_code) { \
+			pr_info(args); \
+		} \
+	} while (0)
+
+static int rk_remote_pwm_dbg_level;
+module_param_named(dbg_level, rk_remote_pwm_dbg_level, int, 0644);
+#define DBG(args...) \
+	do { \
+		if (rk_remote_pwm_dbg_level) { \
+			pr_info(args); \
+		} \
+	} while (0)
+
+
+struct rkxx_remote_key_table {
+	int scancode;
+	int keycode;
+};
+
+struct rkxx_remotectl_button {
+	int usercode;
+	int nbuttons;
+	struct rkxx_remote_key_table *key_table;
+};
+
+struct rkxx_remotectl_drvdata {
+	void __iomem *base;
+	int state;
+	int nbuttons;
+	int result;
+	int scandata;
+	int count;
+	int keynum;
+	int keycode;
+	int press;
+	int pre_press;
+	int period;
+	int irq;
+	int wakeup;
+	struct input_dev *input;
+	struct timer_list timer;
+	struct tasklet_struct remote_tasklet;
+	struct wake_lock remotectl_wake_lock;
+};
+
+
+static struct rkxx_remote_key_table remote_key_table_meiyu_4040[] = {
+	{0xf2, KEY_REPLY},
+	{0xba, KEY_BACK},
+	{0xf4, KEY_UP},
+	{0xf1, KEY_DOWN},
+	{0xef, KEY_LEFT},
+	{0xee, KEY_RIGHT},
+	{0xbd, KEY_HOME},
+	{0xea, KEY_VOLUMEUP},
+	{0xe3, KEY_VOLUMEDOWN},
+	{0xe2, KEY_SEARCH},
+	{0xb2, KEY_POWER},
+	{0xbc, KEY_MUTE},
+	{0xec, KEY_MENU},
+/*lay pause*/
+	{0xbf, 0x190},
+/*pre*/
+	{0xe0, 0x191},
+/*next*/
+	{0xe1, 0x192},
+/*pic,rorate left*/
+	{0xe9, 183},
+/*rorate right*/
+	{0xe6, 248},
+/*zoom out*/
+	{0xe8, 185},
+/*zoom in*/
+	{0xe7, 186},
+/*mouse switch*/
+	{0xb8, 388},
+/*zoom outdisplay switch*/
+	{0xbe, 0x175},
+};
+
+
+static struct rkxx_remote_key_table remote_key_table_sunchip_ff00[] = {
+	{0xf9, KEY_HOME},
+	{0xbf, KEY_BACK},
+	{0xfb, KEY_MENU},
+	{0xaa, KEY_REPLY},
+	{0xb9, KEY_UP},
+	{0xe9, KEY_DOWN},
+	{0xb8, KEY_LEFT},
+	{0xea, KEY_RIGHT},
+	{0xeb, KEY_VOLUMEDOWN},
+	{0xef, KEY_VOLUMEUP},
+	{0xf7, KEY_MUTE},
+	{0xe7, KEY_POWER},
+	{0xfc, KEY_POWER},
+	{0xa9, KEY_VOLUMEDOWN},
+	{0xa8, KEY_VOLUMEDOWN},
+	{0xe0, KEY_VOLUMEDOWN},
+	{0xa5, KEY_VOLUMEDOWN},
+	{0xab, 183},
+	{0xb7, 388},
+	{0xf8, 184},
+	{0xaf, 185},
+	{0xed, KEY_VOLUMEDOWN},
+	{0xee, 186},
+	{0xb3, KEY_VOLUMEDOWN},
+	{0xf1, KEY_VOLUMEDOWN},
+	{0xf2, KEY_VOLUMEDOWN},
+	{0xf3, KEY_SEARCH},
+	{0xb4, KEY_VOLUMEDOWN},
+	{0xbe, KEY_SEARCH},
+};
+
+
+static struct rkxx_remotectl_button remotectl_button[] = {
+	{
+		.usercode = 0xff00,
+		.nbuttons =  29,
+		.key_table = &remote_key_table_sunchip_ff00[0],
+	},
+	{
+		.usercode = 0x4040,
+		.nbuttons =  22,
+		.key_table = &remote_key_table_meiyu_4040[0],
+	},
+};
+
+
+static int remotectl_keybd_num_lookup(struct rkxx_remotectl_drvdata *ddata)
+{
+	int i;
+	int num;
+
+	num =  sizeof(remotectl_button)/sizeof(struct rkxx_remotectl_button);
+	for (i = 0; i < num; i++) {
+		if (remotectl_button[i].usercode == (ddata->scandata&0xFFFF)) {
+			ddata->keynum = i;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+
+static int remotectl_keycode_lookup(struct rkxx_remotectl_drvdata *ddata)
+{
+	int i;
+	unsigned char keydata = (unsigned char)((ddata->scandata >> 8) & 0xff);
+
+	for (i = 0; i < remotectl_button[ddata->keynum].nbuttons; i++) {
+		if (remotectl_button[ddata->keynum].key_table[i].scancode ==
+		    keydata) {
+			ddata->keycode =
+			remotectl_button[ddata->keynum].key_table[i].keycode;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+
+static void rk_pwm_remotectl_do_something(unsigned long  data)
+{
+	struct rkxx_remotectl_drvdata *ddata;
+
+	ddata = (struct rkxx_remotectl_drvdata *)data;
+	switch (ddata->state) {
+	case RMC_IDLE: {
+		;
+		break;
+	}
+	case RMC_PRELOAD: {
+		mod_timer(&ddata->timer, jiffies + msecs_to_jiffies(130));
+		if ((RK_PWM_TIME_PRE_MIN < ddata->period) &&
+		    (ddata->period < RK_PWM_TIME_PRE_MAX)) {
+			ddata->scandata = 0;
+			ddata->count = 0;
+			ddata->state = RMC_USERCODE;
+		} else {
+			ddata->state = RMC_PRELOAD;
+		}
+		break;
+	}
+	case RMC_USERCODE: {
+		if ((RK_PWM_TIME_BIT1_MIN < ddata->period) &&
+		    (ddata->period < RK_PWM_TIME_BIT1_MAX))
+			ddata->scandata |= (0x01 << ddata->count);
+		ddata->count++;
+		if (ddata->count == 0x10) {
+			DBG_CODE("USERCODE=0x%x\n", ddata->scandata);
+			if (remotectl_keybd_num_lookup(ddata)) {
+				ddata->state = RMC_GETDATA;
+				ddata->scandata = 0;
+				ddata->count = 0;
+			} else {
+				ddata->state = RMC_PRELOAD;
+			}
+		}
+	}
+	break;
+	case RMC_GETDATA: {
+		if ((RK_PWM_TIME_BIT1_MIN < ddata->period) &&
+		    (ddata->period < RK_PWM_TIME_BIT1_MAX))
+			ddata->scandata |= (0x01<<ddata->count);
+		ddata->count++;
+		if (ddata->count < 0x10)
+			return;
+		DBG_CODE("RMC_GETDATA=%x\n", (ddata->scandata>>8));
+		if ((ddata->scandata&0x0ff) ==
+		    ((~ddata->scandata >> 8) & 0x0ff)) {
+			if (remotectl_keycode_lookup(ddata)) {
+				ddata->press = 1;
+				input_event(ddata->input, EV_KEY,
+					    ddata->keycode, 1);
+				input_sync(ddata->input);
+				ddata->state = RMC_SEQUENCE;
+			} else {
+				ddata->state = RMC_PRELOAD;
+			}
+		} else {
+			ddata->state = RMC_PRELOAD;
+		}
+	}
+	break;
+	case RMC_SEQUENCE:{
+		DBG("S=%d\n", ddata->period);
+		if ((RK_PWM_TIME_RPT_MIN < ddata->period) &&
+		    (ddata->period < RK_PWM_TIME_RPT_MAX)) {
+			DBG("S1\n");
+			mod_timer(&ddata->timer, jiffies
+				  + msecs_to_jiffies(110));
+		} else if ((RK_PWM_TIME_SEQ1_MIN < ddata->period) &&
+			   (ddata->period < RK_PWM_TIME_SEQ1_MAX)) {
+			DBG("S2\n");
+			mod_timer(&ddata->timer, jiffies
+				  + msecs_to_jiffies(110));
+		} else if ((RK_PWM_TIME_SEQ2_MIN < ddata->period) &&
+			  (ddata->period < RK_PWM_TIME_SEQ2_MAX)) {
+			DBG("S3\n");
+			mod_timer(&ddata->timer, jiffies
+				  + msecs_to_jiffies(110));
+		} else {
+			DBG("S4\n");
+			input_event(ddata->input, EV_KEY,
+				    ddata->keycode, 0);
+			input_sync(ddata->input);
+			ddata->state = RMC_PRELOAD;
+			ddata->press = 0;
+		}
+	}
+	break;
+	default:
+	break;
+	}
+}
+
+static void rk_pwm_remotectl_timer(unsigned long _data)
+{
+	struct rkxx_remotectl_drvdata *ddata;
+
+	ddata =  (struct rkxx_remotectl_drvdata *)_data;
+	if (ddata->press != ddata->pre_press) {
+		ddata->pre_press = 0;
+		ddata->press = 0;
+		input_event(ddata->input, EV_KEY, ddata->keycode, 0);
+		input_sync(ddata->input);
+	}
+	ddata->state = RMC_PRELOAD;
+}
+
+
+static irqreturn_t rockchip_pwm_irq(int irq, void *dev_id)
+{
+	struct rkxx_remotectl_drvdata *ddata;
+	int val;
+
+	ddata = (struct rkxx_remotectl_drvdata *)dev_id;
+	val = readl_relaxed(ddata->base + PWM_REG_INTSTS);
+	if (val&PWM_CH3_INT) {
+		if (val & PWM_CH3_POL) {
+			val = readl_relaxed(ddata->base + PWM_REG_HPR);
+			ddata->period = val;
+			tasklet_hi_schedule(&ddata->remote_tasklet);
+			DBG("hpr=0x%x\n", val);
+		} else {
+			val = readl_relaxed(ddata->base + PWM_REG_LPR);
+			DBG("lpr=0x%x\n", val);
+		}
+		writel_relaxed(PWM_CH3_INT, ddata->base + PWM_REG_INTSTS);
+		if (ddata->state == RMC_PRELOAD)
+			wake_lock_timeout(&ddata->remotectl_wake_lock, HZ);
+		return IRQ_HANDLED;
+	}
+	return IRQ_NONE;
+}
+
+static int rk_pwm_remotectl_hw_init(struct rkxx_remotectl_drvdata *ddata)
+{
+	int val;
+
+	val = readl_relaxed(ddata->base + PWM_REG_CTRL);
+	val = (val & 0xFFFFFFFE) | PWM_DISABLE;
+	writel_relaxed(val, ddata->base + PWM_REG_CTRL);
+	val = readl_relaxed(ddata->base + PWM_REG_CTRL);
+	val = (val & 0xFFFFFFF9) | PWM_MODE_CAPTURE;
+	writel_relaxed(val, ddata->base + PWM_REG_CTRL);
+	val = readl_relaxed(ddata->base + PWM_REG_CTRL);
+	val = (val & 0xFF008DFF) | 0x00646200;
+	writel_relaxed(val, ddata->base + PWM_REG_CTRL);
+	val = readl_relaxed(ddata->base + PWM_REG_INT_EN);
+	val = (val & 0xFFFFFFF7) | PWM_CH3_INT_ENABLE;
+	writel_relaxed(val, ddata->base + PWM_REG_INT_EN);
+	val = readl_relaxed(ddata->base + PWM_REG_CTRL);
+	val = (val & 0xFFFFFFFE) | PWM_ENABLE;
+	writel_relaxed(val, ddata->base + PWM_REG_CTRL);
+	return 0;
+}
+
+
+
+static int rk_pwm_probe(struct platform_device *pdev)
+{
+	struct rkxx_remotectl_drvdata *ddata;
+	struct resource *r;
+	struct input_dev *input;
+	struct clk *clk;
+	int num;
+	int irq;
+	int ret;
+	int i, j;
+
+	DBG(".. rk pwm remotectl v1.1 init\n");
+	r = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (!r) {
+		dev_err(&pdev->dev, "no memory resources defined\n");
+		return -ENODEV;
+	}
+	ddata = devm_kzalloc(&pdev->dev, sizeof(struct rkxx_remotectl_drvdata),
+			     GFP_KERNEL);
+	if (!ddata) {
+		dev_err(&pdev->dev, "failed to allocate memory\n");
+		return -ENOMEM;
+	}
+	ddata->state = RMC_PRELOAD;
+	ddata->base = devm_ioremap_resource(&pdev->dev, r);
+	if (IS_ERR(ddata->base))
+		return PTR_ERR(ddata->base);
+	clk = devm_clk_get(&pdev->dev, "pclk_pwm");
+	if (IS_ERR(clk))
+		return PTR_ERR(clk);
+	platform_set_drvdata(pdev, ddata);
+	input = input_allocate_device();
+	input->name = pdev->name;
+	input->phys = "gpio-keys/remotectl";
+	input->dev.parent = &pdev->dev;
+	input->id.bustype = BUS_HOST;
+	input->id.vendor = 0x0001;
+	input->id.product = 0x0001;
+	input->id.version = 0x0100;
+	ddata->input = input;
+	ddata->input = input;
+	wake_lock_init(&ddata->remotectl_wake_lock,
+		       WAKE_LOCK_SUSPEND, "rk29_pwm_remote");
+	ret = clk_prepare_enable(clk);
+	if (ret)
+		return ret;
+	irq = platform_get_irq(pdev, 0);
+	if (ret < 0) {
+		dev_err(&pdev->dev, "cannot find IRQ\n");
+		return ret;
+	}
+	ddata->irq = irq;
+	ddata->wakeup = 1;
+	tasklet_init(&ddata->remote_tasklet, rk_pwm_remotectl_do_something,
+		     (unsigned long)ddata);
+	num =  sizeof(remotectl_button)/sizeof(struct rkxx_remotectl_button);
+	for (j = 0; j < num; j++) {
+		DBG("remotectl probe j = 0x%x\n", j);
+		for (i = 0; i < remotectl_button[j].nbuttons; i++) {
+			unsigned int type = EV_KEY;
+
+			input_set_capability(input, type, remotectl_button[j].
+					     key_table[i].keycode);
+		}
+	}
+	ret = input_register_device(input);
+	if (ret)
+		pr_err("remotectl: register input device err, ret: %d\n", ret);
+	input_set_capability(input, EV_KEY, KEY_WAKEUP);
+	device_init_wakeup(&pdev->dev, 1);
+	ret = devm_request_irq(&pdev->dev, irq, rockchip_pwm_irq,
+			       0, "rk_pwm_irq", ddata);
+	if (ret) {
+		dev_err(&pdev->dev, "cannot claim IRQ %d\n", irq);
+		return ret;
+	}
+	enable_irq_wake(irq);
+	setup_timer(&ddata->timer, rk_pwm_remotectl_timer,
+		    (unsigned long)ddata);
+	mod_timer(&ddata->timer, jiffies + msecs_to_jiffies(1000));
+	rk_pwm_remotectl_hw_init(ddata);
+	return ret;
+}
+
+static int rk_pwm_remove(struct platform_device *pdev)
+{
+	return 0;
+}
+
+
+static const struct of_device_id rk_pwm_of_match[] = {
+	{ .compatible =  "rockchip,remotectl-pwm"},
+	{ }
+};
+
+MODULE_DEVICE_TABLE(of, rk_pwm_of_match);
+
+static struct platform_driver rk_pwm_driver = {
+	.driver = {
+		.name = "remotectl-pwm",
+		.of_match_table = rk_pwm_of_match,
+	},
+	.probe = rk_pwm_probe,
+	.remove = rk_pwm_remove,
+};
+
+module_platform_driver(rk_pwm_driver);
+
+MODULE_LICENSE("GPL");
---
+++ drivers/input/remotectl/rockchip_pwm_remotectl.h
@@ -0,0 +1,128 @@
+
+#ifndef __RKXX_PWM_REMOTECTL_H__
+#define __RKXX_PWM_REMOTECTL_H__
+#include <linux/input.h>
+
+/* PWM0 registers  */
+#define PWM_REG_CNTR                    0x00  /* Counter Register */
+#define PWM_REG_HPR		                  0x04  /* Period Register */
+#define PWM_REG_LPR                     0x08  /* Duty Cycle Register */
+#define PWM_REG_CTRL                    0x0c  /* Control Register */
+#define PWM_REG_INTSTS                  0x10  /* Interrupt Status Refister */
+#define PWM_REG_INT_EN                  0x14  /* Interrupt Enable Refister */
+
+
+/*REG_CTRL bits definitions*/
+#define PWM_ENABLE			            (1 << 0)
+#define PWM_DISABLE			            (0 << 0)
+
+/*operation mode*/
+#define PWM_MODE_ONESHOT			     (0x00 << 1)
+#define PWM_MODE_CONTINUMOUS 	     (0x01 << 1)
+#define PWM_MODE_CAPTURE		        (0x02 << 1)
+
+/*duty cycle output polarity*/
+#define PWM_DUTY_POSTIVE	            (0x01 << 3)
+#define PWM_DUTY_NEGATIVE	            (0x00 << 3)
+
+/*incative state output polarity*/
+#define PWM_INACTIVE_POSTIVE 		    (0x01 << 4)
+#define PWM_INACTIVE_NEGATIVE		    (0x00 << 4)
+
+/*clock source select*/
+#define PWM_CLK_SCALE		            (1 << 9)
+#define PWM_CLK_NON_SCALE 	            (0 << 9)
+
+#define PWM_CH0_INT                     (1 << 0)
+#define PWM_CH1_INT                     (1 << 1)
+#define PWM_CH2_INT                     (1 << 2)
+#define PWM_CH3_INT                     (1 << 3)
+
+#define PWM_CH0_POL                     (1 << 8)
+#define PWM_CH1_POL                     (1 << 9)
+#define PWM_CH2_POL                     (1 << 10)
+#define PWM_CH3_POL                     (1 << 11)
+
+#define PWM_CH0_INT_ENABLE              (1 << 0)
+#define PWM_CH0_INT_DISABLE             (0 << 0)
+
+#define PWM_CH1_INT_ENABLE              (1 << 0)
+#define PWM_CH1_INT_DISABLE             (0 << 1)
+
+#define PWM_CH2_INT_ENABLE              (1 << 2)
+#define PWM_CH2_INT_DISABLE             (0 << 2)
+
+#define PWM_CH3_INT_ENABLE              (1 << 3)
+#define PWM_CH3_INT_DISABLE             (0 << 3)
+
+/*prescale factor*/
+#define PWMCR_MIN_PRESCALE	            0x00
+#define PWMCR_MAX_PRESCALE	            0x07
+
+#define PWMDCR_MIN_DUTY	       	        0x0001
+#define PWMDCR_MAX_DUTY		            0xFFFF
+
+#define PWMPCR_MIN_PERIOD		        0x0001
+#define PWMPCR_MAX_PERIOD		        0xFFFF
+
+#define PWMPCR_MIN_PERIOD		        0x0001
+#define PWMPCR_MAX_PERIOD		        0xFFFF
+
+enum pwm_div {
+        PWM_DIV1                 = (0x0 << 12),
+        PWM_DIV2                 = (0x1 << 12),
+        PWM_DIV4                 = (0x2 << 12),
+        PWM_DIV8                 = (0x3 << 12),
+        PWM_DIV16                = (0x4 << 12),
+        PWM_DIV32                = (0x5 << 12),
+        PWM_DIV64                = (0x6 << 12),
+        PWM_DIV128   	         = (0x7 << 12),
+}; 
+
+
+
+
+/********************************************************************
+**                            宏定义                                *
+********************************************************************/
+#define RK_PWM_TIME_PRE_MIN      19   /*4500*/
+#define RK_PWM_TIME_PRE_MAX      30   /*5500*/           /*PreLoad 4.5+0.56 = 5.06ms*/
+
+#define RK_PWM_TIME_BIT0_MIN     1  /*Bit0  1.125ms*/
+#define RK_PWM_TIME_BIT0_MAX     5
+
+#define RK_PWM_TIME_BIT1_MIN     7  /*Bit1  2.25ms*/
+#define RK_PWM_TIME_BIT1_MAX     11
+
+#define RK_PWM_TIME_RPT_MIN      200   /*101000*/
+#define RK_PWM_TIME_RPT_MAX      250   /*103000*/         /*Repeat  105-2.81=102.19ms*/  //110-9-2.25-0.56=98.19ms
+
+#define RK_PWM_TIME_SEQ1_MIN     8   /*2650*/
+#define RK_PWM_TIME_SEQ1_MAX     12   /*3000*/           /*sequence  2.25+0.56=2.81ms*/ //11.25ms
+
+#define RK_PWM_TIME_SEQ2_MIN     450   /*101000*/
+#define RK_PWM_TIME_SEQ2_MAX     500   /*103000*/         /*Repeat  105-2.81=102.19ms*/  //110-9-2.25-0.56=98.19ms
+
+/********************************************************************
+**                          结构定义                                *
+********************************************************************/
+typedef enum _RMC_STATE
+{
+    RMC_IDLE,
+    RMC_PRELOAD,
+    RMC_USERCODE,
+    RMC_GETDATA,
+    RMC_SEQUENCE
+}eRMC_STATE;
+
+
+struct RKxx_remotectl_platform_data {
+	//struct rkxx_remotectl_button *buttons;
+	int nbuttons;
+	int rep;
+	int timer;
+	int wakeup;
+};
+
+#endif
+
