8dc4379e17cddad7b2088a3f300ded50d2a6d493
Gu Zheng <guz.fnst@cn.fujitsu.com>
2014-07-23 18:03:52 +0800

    aio: use the macro rather than the inline magic number
    Replace the inline magic number with the ready-made macro(AIO_RING_MAGIC),
    just clean up.
    
    Reviewed-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Gu Zheng <guz.fnst@cn.fujitsu.com>
    Signed-off-by: Benjamin LaHaise <bcrl@kvack.org>

--- fs/aio.c
+++ fs/aio.c
@@ -201,7 +201,7 @@ static struct dentry *aio_mount(struct file_system_type *fs_type,
 	static const struct dentry_operations ops = {
 		.d_dname	= simple_dname,
 	};
-	return mount_pseudo(fs_type, "aio:", NULL, &ops, 0xa10a10a1);
+	return mount_pseudo(fs_type, "aio:", NULL, &ops, AIO_RING_MAGIC);
 }
 
 /* aio_setup
