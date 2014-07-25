4e3a25b0274b8474f5ad46215a270785dd18265e
Pawel Moll <pawel.moll@arm.com>
2014-07-25 15:23:49 +0100

    ARM: imx: Remove references to platform_bus in mxc code
    The bus devices created to be parents for other peripherals
    were using platform_bus as a parent, not being platform
    devices themselves. Remove the references, making them
    virtual devices instead.
    
    Cc: Sascha Hauer <kernel@pengutronix.de>
    Cc: Russell King <linux@arm.linux.org.uk>
    Signed-off-by: Pawel Moll <pawel.moll@arm.com>
    Acked-by: Shawn Guo <shawn.guo@freescale.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm/mach-imx/devices/devices.c
+++ arch/arm/mach-imx/devices/devices.c
@@ -24,12 +24,10 @@
 
 struct device mxc_aips_bus = {
 	.init_name	= "mxc_aips",
-	.parent		= &platform_bus,
 };
 
 struct device mxc_ahb_bus = {
 	.init_name	= "mxc_ahb",
-	.parent		= &platform_bus,
 };
 
 int __init mxc_device_init(void)
