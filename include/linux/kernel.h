37549e94c77a94a9c32b5ae3313a3801cb66adf9
Rusty Russell <rusty@rustcorp.com.au>
2014-07-27 07:26:01 +0930

    sysfs: disallow world-writable files.
    This check was introduced in 2006 by Alexey Dobriyan (9774a1f54f173)
    for module parameters; we removed it when we unified the check into
    VERIFY_OCTAL_PERMISSIONS() as sysfs didn't have the same requirement.
    Now all those users are fixed, reintroduce it.
    
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Dave Jones <davej@redhat.com>
    Cc: Joe Perches <joe@perches.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -849,5 +849,7 @@ static inline void ftrace_dump(enum ftrace_dump_mode oops_dump_mode) { }
 	 /* User perms >= group perms >= other perms */			\
 	 BUILD_BUG_ON_ZERO(((perms) >> 6) < (((perms) >> 3) & 7)) +	\
 	 BUILD_BUG_ON_ZERO((((perms) >> 3) & 7) < ((perms) & 7)) +	\
+	 /* Other writable?  Generally considered a bad idea. */	\
+	 BUILD_BUG_ON_ZERO((perms) & 2) +				\
 	 (perms))
 #endif
