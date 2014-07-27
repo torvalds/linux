1e209c969a40c1eeb789515d01d8dd1427982a54
John Crispin <blogic@openwrt.org>
2014-07-27 10:14:30 +0100

    MIPS: ralink: add rt3883 wmac clock
    Register the wireless mac clock on rti3883. This is required by the wifi driver.
    
    Signed-off-by: John Crispin <blogic@openwrt.org>
    Patchwork: http://patchwork.linux-mips.org/patch/8007/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/ralink/rt3883.c
+++ arch/mips/ralink/rt3883.c
@@ -204,6 +204,7 @@ void __init ralink_clk_init(void)
 	ralink_clk_add("10000b00.spi", sys_rate);
 	ralink_clk_add("10000c00.uartlite", 40000000);
 	ralink_clk_add("10100000.ethernet", sys_rate);
+	ralink_clk_add("10180000.wmac", 40000000);
 }
 
 void __init ralink_of_remap(void)
