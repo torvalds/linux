5a341be27fe23c31e4849b0a0506a4469bcbc283
Christian König <christian.koenig@amd.com>
2014-07-30 21:05:16 +0200

    drm/radeon: add VM GART copy optimization to NI as well
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/ni_dma.c
+++ drivers/gpu/drm/radeon/ni_dma.c
@@ -330,7 +330,26 @@ void cayman_dma_vm_set_page(struct radeon_device *rdev,
 
 	trace_radeon_vm_set_page(pe, addr, count, incr, flags);
 
-	if ((flags & R600_PTE_SYSTEM) || (count == 1)) {
+	if ((flags & R600_PTE_GART_MASK) == R600_PTE_GART_MASK) {
+		uint64_t src = rdev->gart.table_addr + (addr >> 12) * 8;
+		while (count) {
+			ndw = count * 2;
+			if (ndw > 0xFFFFE)
+				ndw = 0xFFFFE;
+
+			ib->ptr[ib->length_dw++] = DMA_PACKET(DMA_PACKET_COPY,
+							      0, 0, ndw);
+			ib->ptr[ib->length_dw++] = lower_32_bits(pe);
+			ib->ptr[ib->length_dw++] = lower_32_bits(src);
+			ib->ptr[ib->length_dw++] = upper_32_bits(pe) & 0xff;
+			ib->ptr[ib->length_dw++] = upper_32_bits(src) & 0xff;
+
+			pe += ndw * 4;
+			src += ndw * 4;
+			count -= ndw / 2;
+		}
+
+	} else if ((flags & R600_PTE_SYSTEM) || (count == 1)) {
 		while (count) {
 			ndw = count * 2;
 			if (ndw > 0xFFFFE)
