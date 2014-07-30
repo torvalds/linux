1d8fcba1de632d7a43349788ad534c5a32c5a44c
Linus Torvalds <torvalds@linux-foundation.org>
2014-07-30 08:56:23 -0700

    Revert "cdc_subset: deal with a device that needs reset for timeout"
    This reverts commit 20fbe3ae990fd54fc7d1f889d61958bc8b38f254.
    
    As reported by Stephen Rothwell, it causes compile failures in certain
    configurations:
    
      drivers/net/usb/cdc_subset.c:360:15: error: 'dummy_prereset' undeclared here (not in a function)
        .pre_reset = dummy_prereset,
                     ^
      drivers/net/usb/cdc_subset.c:361:16: error: 'dummy_postreset' undeclared here (not in a function)
        .post_reset = dummy_postreset,
                      ^
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: David Miller <davem@davemloft.net>
    Cc: Oliver Neukum <oneukum@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/net/usb/usbnet.c
+++ drivers/net/usb/usbnet.c
@@ -1218,12 +1218,8 @@ void usbnet_tx_timeout (struct net_device *net)
 
 	unlink_urbs (dev, &dev->txq);
 	tasklet_schedule (&dev->bh);
-	/* this needs to be handled individually because the generic layer
-	 * doesn't know what is sufficient and could not restore private
-	 * information if a remedy of an unconditional reset were used.
-	 */
-	if (dev->driver_info->recover)
-		(dev->driver_info->recover)(dev);
+
+	// FIXME: device recovery -- reset?
 }
 EXPORT_SYMBOL_GPL(usbnet_tx_timeout);
 
