ade9862470dd0595d8e292ecea8445ed90b98df5
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2014-07-31 16:02:33 -0700

    rcu: Make TINY_RCU tinier by putting error checks under #ifdef
    The rcu_idle_enter_common() and rcu_idle_exit_common() functions contain
    error checks that have to the best of my knowledge have never triggered
    over the past several years.  These are nevertheless valuable when
    creating new architectures or doing other low-level changes, so the
    checks should not be deleted.  This commit instead places these checks
    under #ifdef CONFIG_RCU_TRACE so that they are executed only when
    specifically requested.
    
    The savings are significant:
    
    	Before:
    
    	   text    data     bss     dec     hex filename
    	   1749      39       0    1788     6fc /tmp/b/kernel/rcu/tiny.o
    	    632     152       0     784     310 /tmp/b/kernel/rcu/update.o
    				   ----
    				   2572
    
    	After:
    
    	   text    data     bss     dec     hex filename
    	   1281      37       0    1318     526 /tmp/b/kernel/rcu/tiny.o
    	    632     152       0     784     310 /tmp/b/kernel/rcu/update.o
    				   ----
    				   2102
    
    This amounts to 470 bytes, or 18% of the original.
    
    Switched from #ifdef to IS_ENABLED() on Josh Triplett's advice.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>

--- kernel/rcu/tiny.c
+++ kernel/rcu/tiny.c
@@ -62,7 +62,7 @@ static void rcu_idle_enter_common(long long newval)
 	}
 	RCU_TRACE(trace_rcu_dyntick(TPS("Start"),
 				    rcu_dynticks_nesting, newval));
-	if (!is_idle_task(current)) {
+	if (IS_ENABLED(CONFIG_RCU_TRACE) && !is_idle_task(current)) {
 		struct task_struct *idle __maybe_unused = idle_task(smp_processor_id());
 
 		RCU_TRACE(trace_rcu_dyntick(TPS("Entry error: not idle task"),
@@ -123,7 +123,7 @@ static void rcu_idle_exit_common(long long oldval)
 		return;
 	}
 	RCU_TRACE(trace_rcu_dyntick(TPS("End"), oldval, rcu_dynticks_nesting));
-	if (!is_idle_task(current)) {
+	if (IS_ENABLED(CONFIG_RCU_TRACE) && !is_idle_task(current)) {
 		struct task_struct *idle __maybe_unused = idle_task(smp_processor_id());
 
 		RCU_TRACE(trace_rcu_dyntick(TPS("Exit error: not idle task"),
