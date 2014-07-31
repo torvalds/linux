8c058d53f6f2eb0b9cd3bc4ce5c053a64dded671
Len Brown <len.brown@intel.com>
2014-07-31 15:21:24 -0400

    intel_idle: Disable Baytrail Core and Module C6 auto-demotion
    Power efficiency improves on Baytrail (Intel Atom Processor E3000)
    when Linux disables C6 auto-demotion.
    
    Based on work by Srinidhi Kasagar <srinidhi.kasagar@intel.com>.
    
    Signed-off-by: Len Brown <len.brown@intel.com>
    Cc: x86@kernel.org

--- drivers/idle/intel_idle.c
+++ drivers/idle/intel_idle.c
@@ -89,6 +89,7 @@ struct idle_cpu {
 	 * Indicate which enable bits to clear here.
 	 */
 	unsigned long auto_demotion_disable_flags;
+	bool byt_auto_demotion_disable_flag;
 	bool disable_promotion_to_c1e;
 };
 
@@ -613,6 +614,7 @@ static const struct idle_cpu idle_cpu_snb = {
 static const struct idle_cpu idle_cpu_byt = {
 	.state_table = byt_cstates,
 	.disable_promotion_to_c1e = true,
+	.byt_auto_demotion_disable_flag = true,
 };
 
 static const struct idle_cpu idle_cpu_ivb = {
@@ -813,6 +815,11 @@ static int __init intel_idle_cpuidle_driver_init(void)
 
 	if (icpu->auto_demotion_disable_flags)
 		on_each_cpu(auto_demotion_disable, NULL, 1);
+
+	if (icpu->byt_auto_demotion_disable_flag) {
+		wrmsrl(MSR_CC6_DEMOTION_POLICY_CONFIG, 0);
+		wrmsrl(MSR_MC6_DEMOTION_POLICY_CONFIG, 0);
+	}
 
 	if (icpu->disable_promotion_to_c1e)	/* each-cpu is redundant */
 		on_each_cpu(c1e_promotion_disable, NULL, 1);
