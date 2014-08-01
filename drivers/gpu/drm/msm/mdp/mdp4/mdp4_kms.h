3e87599b68e7929a84a32ab65ad17b79a3f271f6
Rob Clark <robdclark@gmail.com>
2014-08-01 13:08:11 -0400

    drm/msm/mdp4: add LVDS panel support
    LVDS panel support uses the LCDC (parallel) encoder.  Unlike with HDMI,
    there is not a separate LVDS block, so no need to split things into a
    bridge+connector.  Nor is there is anything re-used with mdp5.
    
    Note that there can be some regulators shared between HDMI and LVDS (in
    particular, on apq8064, ext_3v3p), so we should not use the _exclusive()
    variants of devm_regulator_get().
    
    The drm_panel framework is used for panel-specific driver.
    
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/mdp/mdp4/mdp4_kms.h
+++ drivers/gpu/drm/msm/mdp/mdp4/mdp4_kms.h
@@ -23,6 +23,8 @@
 #include "mdp/mdp_kms.h"
 #include "mdp4.xml.h"
 
+#include "drm_panel.h"
+
 struct mdp4_kms {
 	struct mdp_kms base;
 
@@ -216,6 +218,22 @@ struct drm_crtc *mdp4_crtc_init(struct drm_device *dev,
 
 long mdp4_dtv_round_pixclk(struct drm_encoder *encoder, unsigned long rate);
 struct drm_encoder *mdp4_dtv_encoder_init(struct drm_device *dev);
+
+long mdp4_lcdc_round_pixclk(struct drm_encoder *encoder, unsigned long rate);
+struct drm_encoder *mdp4_lcdc_encoder_init(struct drm_device *dev,
+		struct drm_panel *panel);
+
+struct drm_connector *mdp4_lvds_connector_init(struct drm_device *dev,
+		struct drm_panel *panel, struct drm_encoder *encoder);
+
+#ifdef CONFIG_COMMON_CLK
+struct clk *mpd4_lvds_pll_init(struct drm_device *dev);
+#else
+static inline struct clk *mpd4_lvds_pll_init(struct drm_device *dev)
+{
+	return ERR_PTR(-ENODEV);
+}
+#endif
 
 #ifdef CONFIG_MSM_BUS_SCALING
 static inline int match_dev_name(struct device *dev, void *data)
