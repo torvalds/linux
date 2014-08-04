6ccf62c7bea561cca7ffbd50839f883327080800
Nicolin Chen <nicoleotsuka@gmail.com>
2014-08-04 12:19:48 +0800

    ASoC: fsl_sarc_dma: Check pair before using it
    The patch 3117bb3109dc: "ASoC: fsl_asrc: Add ASRC ASoC CPU DAI and
    platform drivers" from Jul 29, 2014, leads to the following Smatch
    complaint:
    
    sound/soc/fsl/fsl_asrc_dma.c:304 fsl_asrc_dma_shutdown()
    warn: variable dereferenced before check 'pair' (see line 302)
    
    sound/soc/fsl/fsl_asrc_dma.c
    301          struct fsl_asrc_pair *pair = runtime->private_data;
    302          struct fsl_asrc *asrc_priv = pair->asrc_priv;
                                              ^^^^^^^^^^^^^^^
                                                Dereference.
    
    303
    304          if (pair && asrc_priv->pair[pair->index] == pair)
                     ^^^^
                    Check.
    
    305                  asrc_priv->pair[pair->index] = NULL;
    306
    
    So we just let the driver check pair before using it.
    
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Nicolin Chen <nicoleotsuka@gmail.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/fsl/fsl_asrc_dma.c
+++ sound/soc/fsl/fsl_asrc_dma.c
@@ -299,9 +299,14 @@ static int fsl_asrc_dma_shutdown(struct snd_pcm_substream *substream)
 {
 	struct snd_pcm_runtime *runtime = substream->runtime;
 	struct fsl_asrc_pair *pair = runtime->private_data;
-	struct fsl_asrc *asrc_priv = pair->asrc_priv;
+	struct fsl_asrc *asrc_priv;
 
-	if (pair && asrc_priv->pair[pair->index] == pair)
+	if (!pair)
+		return 0;
+
+	asrc_priv = pair->asrc_priv;
+
+	if (asrc_priv->pair[pair->index] == pair)
 		asrc_priv->pair[pair->index] = NULL;
 
 	kfree(pair);
