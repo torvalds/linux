a509574e5ea7b617268943526773ebf7e2d20a9b
Adam Goode <agoode@google.com>
2014-08-05 12:44:51 -0400

    ALSA: usb-audio: Whitespace cleanups for sound/usb/midi.*
    Signed-off-by: Adam Goode <agoode@google.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/usb/midi.h
+++ sound/usb/midi.h
@@ -43,8 +43,8 @@ int snd_usbmidi_create(struct snd_card *card,
 		       struct usb_interface *iface,
 		       struct list_head *midi_list,
 		       const struct snd_usb_audio_quirk *quirk);
-void snd_usbmidi_input_stop(struct list_head* p);
-void snd_usbmidi_input_start(struct list_head* p);
+void snd_usbmidi_input_stop(struct list_head *p);
+void snd_usbmidi_input_start(struct list_head *p);
 void snd_usbmidi_disconnect(struct list_head *p);
 void snd_usbmidi_suspend(struct list_head *p);
 void snd_usbmidi_resume(struct list_head *p);
