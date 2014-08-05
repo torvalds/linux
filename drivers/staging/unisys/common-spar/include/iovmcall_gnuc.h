3c1a653b90d7b3a681a7e49feef32e3e290b36f0
Benjamin Romer <benjamin.romer@unisys.com>
2014-08-05 14:57:52 -0400

    staging: unisys: fix spacing in iovmcall_gnuc.h
    There are spacing errors in the functions in iovmcall_gnuc. Fix these.
    
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/common-spar/include/iovmcall_gnuc.h
+++ drivers/staging/unisys/common-spar/include/iovmcall_gnuc.h
@@ -19,8 +19,8 @@ __unisys_vmcall_gnuc(unsigned long tuple, unsigned long reg_ebx,
 		     unsigned long reg_ecx)
 {
 	unsigned long result = 0;
-
 	unsigned int cpuid_eax, cpuid_ebx, cpuid_ecx, cpuid_edx;
+
 	cpuid(0x00000001, &cpuid_eax, &cpuid_ebx, &cpuid_ecx, &cpuid_edx);
 	if (cpuid_ecx & 0x80000000) {
 	      __asm__ __volatile__(".byte 0x00f, 0x001, 0x0c1" : "=a"(result) :
@@ -39,8 +39,8 @@ __unisys_extended_vmcall_gnuc(unsigned long long tuple,
 			      unsigned long long reg_edx)
 {
 	unsigned long result = 0;
-
 	unsigned int cpuid_eax, cpuid_ebx, cpuid_ecx, cpuid_edx;
+
 	cpuid(0x00000001, &cpuid_eax, &cpuid_ebx, &cpuid_ecx, &cpuid_edx);
 	if (cpuid_ecx & 0x80000000) {
 	      __asm__ __volatile__(".byte 0x00f, 0x001, 0x0c1" : "=a"(result) :
