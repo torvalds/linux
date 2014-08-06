61b3d6c48f059bb054b0019088736dab6c2ac0ec
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2014-08-06 16:09:44 -0700

    lib: list_sort.c: Limit number of unused cmp callbacks
    The helper merge_and_restore_back_links() makes sure to call the
    caller's cmp function during the final ->prev pointer fixup, so that the
    cmp function may call cond_resched().  However, if the cmp function does
    not call cond_resched() at all, this is entirely redundant.  If it does,
    doing at least two function calls for every two pointer assignments is a
    bit excessive.  This patch limits the calls to once for every 256
    iterations.
    
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Cc: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    Cc: Don Mullis <don.mullis@gmail.com>
    Cc: Dave Chinner <david@fromorbit.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/list_sort.c
+++ lib/list_sort.c
@@ -47,6 +47,7 @@ static void merge_and_restore_back_links(void *priv,
 				struct list_head *a, struct list_head *b)
 {
 	struct list_head *tail = head;
+	u8 count = 0;
 
 	while (a && b) {
 		/* if equal, take 'a' -- important for sort stability */
@@ -70,7 +71,8 @@ static void merge_and_restore_back_links(void *priv,
 		 * element comparison is needed, so the client's cmp()
 		 * routine can invoke cond_resched() periodically.
 		 */
-		(*cmp)(priv, tail->next, tail->next);
+		if (unlikely(!(++count)))
+			(*cmp)(priv, tail->next, tail->next);
 
 		tail->next->prev = tail;
 		tail = tail->next;
