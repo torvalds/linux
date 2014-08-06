e11cb6668f1a349250873337858a502e2ab73c8e
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2014-08-06 16:08:25 -0700

    zram: rename struct `table' to `zram_table_entry'
    Andrew Morton has recently noted that `struct table' actually represents
    table entry and, thus, should be renamed.  Rename to `zram_table_entry'.
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Cc: Weijie Yang <weijie.yang@samsung.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit cb8f2eec3c5c87e31219c5e58625b8e890004e48)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -62,7 +62,7 @@ enum zram_pageflags {
 /*-- Data structures */
 
 /* Allocated for each disk page */
-struct table {
+struct zram_table_entry {
 	unsigned long handle;
 	u16 size;	/* object size (excluding header) */
 	u8 flags;
@@ -82,7 +82,7 @@ struct zram_stats {
 
 struct zram_meta {
 	rwlock_t tb_lock;	/* protect table */
-	struct table *table;
+	struct zram_table_entry *table;
 	struct zs_pool *mem_pool;
 };
 
