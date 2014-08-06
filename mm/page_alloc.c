7be12fc9f8dcae7f4c9647d495bd64fc4ffb6836
Michal Nazarewicz <mina86@mina86.com>
2014-08-06 16:05:15 -0700

    mm: page_alloc: simplify drain_zone_pages by using min()
    Instead of open-coding getting minimal value of two, just use min macro.
    That is why it is there for.  While changing the function also change
    type of batch local variable to match type of per_cpu_pages::batch
    (which is int).
    
    Signed-off-by: Michal Nazarewicz <mina86@mina86.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1257,15 +1257,11 @@ static int rmqueue_bulk(struct zone *zone, unsigned int order,
 void drain_zone_pages(struct zone *zone, struct per_cpu_pages *pcp)
 {
 	unsigned long flags;
-	int to_drain;
-	unsigned long batch;
+	int to_drain, batch;
 
 	local_irq_save(flags);
 	batch = ACCESS_ONCE(pcp->batch);
-	if (pcp->count >= batch)
-		to_drain = batch;
-	else
-		to_drain = pcp->count;
+	to_drain = min(pcp->count, batch);
 	if (to_drain > 0) {
 		free_pcppages_bulk(zone, to_drain, pcp);
 		pcp->count -= to_drain;
