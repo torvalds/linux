1574a29f8e769998fe3e55eb6030dc61e3d09ccd
Joe Perches <joe@perches.com>
2014-08-06 16:10:50 -0700

    checkpatch: allow multiple const * types
    checkpatch's $Type variable does not match declarations of multiple
    const * types.
    
    This can produce false positives for things like:
    
      $ ./scripts/checkpatch.pl -f drivers/staging/comedi/comedidev.h
      WARNING: Missing a blank line after declarations
      #60: FILE: drivers/staging/comedi/comedidev.h:60:
      +       const struct comedi_lrange *range_table;
      +       const struct comedi_lrange *const *range_table_list;
    
    Fix the $Type variable to support matching multiple "* const" uses.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Reported-by: Hartley Sweeten <HartleyS@visionengravers.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -435,7 +435,7 @@ sub build_types {
 		  }x;
 	$Type	= qr{
 			$NonptrType
-			(?:(?:\s|\*|\[\])+\s*const|(?:\s|\*|\[\])+|(?:\s*\[\s*\])+)?
+			(?:(?:\s|\*|\[\])+\s*const|(?:\s|\*\s*(?:const\s*)?|\[\])+|(?:\s*\[\s*\])+)?
 			(?:\s+$Inline|\s+$Modifier)*
 		  }x;
 	$Declare	= qr{(?:$Storage\s+(?:$Inline\s+)?)?$Type};
