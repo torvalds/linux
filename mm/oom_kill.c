8d060bf490930f305c4efc45724e861a268f4d2f
David Rientjes <rientjes@google.com>
2014-08-06 16:07:50 -0700

    mm, oom: ensure memoryless node zonelist always includes zones
    With memoryless node support being worked on, it's possible that for
    optimizations that a node may not have a non-NULL zonelist.  When
    CONFIG_NUMA is enabled and node 0 is memoryless, this means the zonelist
    for first_online_node may become NULL.
    
    The oom killer requires a zonelist that includes all memory zones for
    the sysrq trigger and pagefault out of memory handler.
    
    Ensure that a non-NULL zonelist is always passed to the oom killer.
    
    [akpm@linux-foundation.org: fix non-numa build]
    Signed-off-by: David Rientjes <rientjes@google.com>
    Cc: "Kirill A. Shutemov" <kirill.shutemov@linux.intel.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Rik van Riel <riel@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -694,7 +694,7 @@ void pagefault_out_of_memory(void)
 	if (mem_cgroup_oom_synchronize(true))
 		return;
 
-	zonelist = node_zonelist(first_online_node, GFP_KERNEL);
+	zonelist = node_zonelist(first_memory_node, GFP_KERNEL);
 	if (try_set_zonelist_oom(zonelist, GFP_KERNEL)) {
 		out_of_memory(NULL, 0, 0, NULL, false);
 		clear_zonelist_oom(zonelist, GFP_KERNEL);
