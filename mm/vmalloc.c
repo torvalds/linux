660654f90e7f8f6d8163276d47fc1573a39c7007
Eric Dumazet <edumazet@google.com>
2014-08-06 16:06:25 -0700

    mm/vmalloc.c: add a schedule point to vmalloc()
    It is not uncommon on busy servers to get stuck hundred of ms in
    vmalloc() calls (like file descriptor expansions).
    
    Add a cond_resched() to __vmalloc_area_node() to be gentle to
    other tasks.
    
    [akpm@linux-foundation.org: only do it for __GFP_WAIT, per David]
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Hugh Dickins <hughd@google.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -1602,6 +1602,8 @@ static void *__vmalloc_area_node(struct vm_struct *area, gfp_t gfp_mask,
 			goto fail;
 		}
 		area->pages[i] = page;
+		if (gfp_mask & __GFP_WAIT)
+			cond_resched();
 	}
 
 	if (map_vm_area(area, prot, &pages))
