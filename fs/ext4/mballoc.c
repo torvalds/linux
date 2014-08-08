9197ac427c8b6831ab953bd12ef1012c298f9785
JP Abgrall <jpa@google.com>
2014-08-07 17:12:43 -0700

    ext4: Add support for FIDTRIM, a best-effort ioctl for deep discard trim
    * What
    This provides an interface for issuing an FITRIM which uses the
    secure discard instead of just a discard.
    Only the eMMC command is "secure", and not how the FS uses it:
    due to the fact that the FS might reassign a region somewhere else,
    the original deleted data will not be affected by the "trim" which only
    handles un-used regions.
    So we'll just call it "deep discard", and note that this is a
    "best effort" cleanup.
    
    * Why
    Once in a while, We want to be able to cleanup most of the unused blocks
    after erasing a bunch of files.
    We don't want to constantly secure-discard via a mount option.
    
    From an eMMC spec perspective, it tells the device to really get rid of
    all the data for the specified blocks and not just put them back into the
    pool of free ones (unlike the normal TRIM). The eMMC spec says the
    secure trim handling must make sure the data (and metadata) is not available
    anymore. A simple TRIM doesn't clear the data, it just puts blocks in the
    free pool.
    JEDEC Standard No. 84-A441
      7.6.9 Secure Erase
      7.6.10 Secure Trim
    
    From an FS perspective, it is acceptable to leave some data behind.
     - directory entries related to deleted files
     - databases entries related to deleted files
     - small-file data stored in inode extents
     - blocks held by the FS waiting to be re-used (mitigated by sync).
     - blocks reassigned by the FS prior to FIDTRIM.
    
    Change-Id: I676a1404a80130d93930c84898360f2e6fb2f81e
    Signed-off-by: Geremy Condra <gcondra@google.com>
    Signed-off-by: JP Abgrall <jpa@google.com>

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -2576,7 +2576,8 @@ int ext4_mb_release(struct super_block *sb)
 }
 
 static inline int ext4_issue_discard(struct super_block *sb,
-		ext4_group_t block_group, ext4_grpblk_t cluster, int count)
+		ext4_group_t block_group, ext4_grpblk_t cluster, int count,
+		unsigned long flags)
 {
 	ext4_fsblk_t discard_block;
 
@@ -2585,7 +2586,7 @@ static inline int ext4_issue_discard(struct super_block *sb,
 	count = EXT4_C2B(EXT4_SB(sb), count);
 	trace_ext4_discard_blocks(sb,
 			(unsigned long long) discard_block, count);
-	return sb_issue_discard(sb, discard_block, count, GFP_NOFS, 0);
+	return sb_issue_discard(sb, discard_block, count, GFP_NOFS, flags);
 }
 
 /*
@@ -2606,7 +2607,7 @@ static void ext4_free_data_callback(struct super_block *sb,
 
 	if (test_opt(sb, DISCARD))
 		ext4_issue_discard(sb, entry->efd_group,
-				   entry->efd_start_cluster, entry->efd_count);
+				   entry->efd_start_cluster, entry->efd_count, 0);
 
 	err = ext4_mb_load_buddy(sb, entry->efd_group, &e4b);
 	/* we expect to find existing buddy because it's pinned */
@@ -4850,13 +4851,15 @@ error_return:
  * @count:	number of blocks to TRIM
  * @group:	alloc. group we are working with
  * @e4b:	ext4 buddy for the group
+ * @blkdev_flags: flags for the block device
  *
  * Trim "count" blocks starting at "start" in the "group". To assure that no
  * one will allocate those blocks, mark it as used in buddy bitmap. This must
  * be called with under the group lock.
  */
 static void ext4_trim_extent(struct super_block *sb, int start, int count,
-			     ext4_group_t group, struct ext4_buddy *e4b)
+			    ext4_group_t group, struct ext4_buddy *e4b,
+			    unsigned long blkdev_flags)
 {
 	struct ext4_free_extent ex;
 
@@ -4874,7 +4877,7 @@ static void ext4_trim_extent(struct super_block *sb, int start, int count,
 	 */
 	mb_mark_used(e4b, &ex);
 	ext4_unlock_group(sb, group);
-	ext4_issue_discard(sb, group, start, count);
+	ext4_issue_discard(sb, group, start, count, blkdev_flags);
 	ext4_lock_group(sb, group);
 	mb_free_blocks(NULL, e4b, start, ex.fe_len);
 }
@@ -4886,6 +4889,7 @@ static void ext4_trim_extent(struct super_block *sb, int start, int count,
  * @start:		first group block to examine
  * @max:		last group block to examine
  * @minblocks:		minimum extent block count
+ * @blkdev_flags:	flags for the block device
  *
  * ext4_trim_all_free walks through group's buddy bitmap searching for free
  * extents. When the free block is found, ext4_trim_extent is called to TRIM
@@ -4900,7 +4904,7 @@ static void ext4_trim_extent(struct super_block *sb, int start, int count,
 static ext4_grpblk_t
 ext4_trim_all_free(struct super_block *sb, ext4_group_t group,
 		   ext4_grpblk_t start, ext4_grpblk_t max,
-		   ext4_grpblk_t minblocks)
+		   ext4_grpblk_t minblocks, unsigned long blkdev_flags)
 {
 	void *bitmap;
 	ext4_grpblk_t next, count = 0, free_count = 0;
@@ -4933,7 +4937,7 @@ ext4_trim_all_free(struct super_block *sb, ext4_group_t group,
 
 		if ((next - start) >= minblocks) {
 			ext4_trim_extent(sb, start,
-					 next - start, group, &e4b);
+					 next - start, group, &e4b, blkdev_flags);
 			count += next - start;
 		}
 		free_count += next - start;
@@ -4970,6 +4974,7 @@ out:
  * ext4_trim_fs() -- trim ioctl handle function
  * @sb:			superblock for filesystem
  * @range:		fstrim_range structure
+ * @blkdev_flags:	flags for the block device
  *
  * start:	First Byte to trim
  * len:		number of Bytes to trim from start
@@ -4978,7 +4983,8 @@ out:
  * start to start+len. For each such a group ext4_trim_all_free function
  * is invoked to trim all free space.
  */
-int ext4_trim_fs(struct super_block *sb, struct fstrim_range *range)
+int ext4_trim_fs(struct super_block *sb, struct fstrim_range *range,
+			unsigned long blkdev_flags)
 {
 	struct ext4_group_info *grp;
 	ext4_group_t group, first_group, last_group;
@@ -5033,7 +5039,7 @@ int ext4_trim_fs(struct super_block *sb, struct fstrim_range *range)
 
 		if (grp->bb_free >= minlen) {
 			cnt = ext4_trim_all_free(sb, group, first_cluster,
-						end, minlen);
+						end, minlen, blkdev_flags);
 			if (cnt < 0) {
 				ret = cnt;
 				break;
