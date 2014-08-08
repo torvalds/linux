2c67568903d6ae1b8cfa343c649029180239418e
Thierry Reding <treding@nvidia.com>
2014-08-08 13:02:36 +0200

    spi: Add missing kerneldoc bits
    These are all arguments or fields that got added without updating the
    kerneldoc comments.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -843,6 +843,7 @@ out:
 
 /**
  * spi_finalize_current_transfer - report completion of a transfer
+ * @master: the master reporting completion
  *
  * Called by SPI drivers using the core transfer_one_message()
  * implementation to notify it that the current interrupt driven
