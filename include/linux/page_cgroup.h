3cbb01871e22709fdd39478eca831de317df332f
Greg Thelen <gthelen@google.com>
2014-08-08 14:19:31 -0700

    memcg: remove lookup_cgroup_page() prototype
    Commit 6b208e3f6e35 ("mm: memcg: remove unused node/section info from
    pc->flags") deleted lookup_cgroup_page() but left a prototype for it.
    
    Kill the vestigial prototype.
    
    Signed-off-by: Greg Thelen <gthelen@google.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Michal Hocko <mhocko@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/page_cgroup.h
+++ include/linux/page_cgroup.h
@@ -40,7 +40,6 @@ static inline void page_cgroup_init(void)
 #endif
 
 struct page_cgroup *lookup_page_cgroup(struct page *page);
-struct page *lookup_cgroup_page(struct page_cgroup *pc);
 
 static inline int PageCgroupUsed(struct page_cgroup *pc)
 {
