aafec40c30e97cbcbc703ca17ab1ebf7fc06cf3a
Matthias Beyer <mail@beyermatthias.de>
2014-08-10 14:32:03 +0200

    Staging: bcm: Bcmchar.c: Renamed variable "Packet" -> "packet"
    Renamed variable "Packet" -> "packet" in bcm_char_read().
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/Bcmchar.c
+++ drivers/staging/bcm/Bcmchar.c
@@ -187,7 +187,7 @@ static ssize_t bcm_char_read(struct file *filp,
 {
 	struct bcm_tarang_data *tarang = filp->private_data;
 	struct bcm_mini_adapter *ad = tarang->Adapter;
-	struct sk_buff *Packet = NULL;
+	struct sk_buff *packet = NULL;
 	ssize_t pkt_len = 0;
 	int wait_ret_val = 0;
 	unsigned long ret = 0;
@@ -215,7 +215,7 @@ static ssize_t bcm_char_read(struct file *filp,
 	down(&ad->RxAppControlQueuelock);
 
 	if (tarang->RxAppControlHead) {
-		Packet = tarang->RxAppControlHead;
+		packet = tarang->RxAppControlHead;
 		DEQUEUEPACKET(tarang->RxAppControlHead,
 			      tarang->RxAppControlTail);
 		tarang->AppCtrlQueueLen--;
@@ -223,20 +223,20 @@ static ssize_t bcm_char_read(struct file *filp,
 
 	up(&ad->RxAppControlQueuelock);
 
-	if (Packet) {
-		pkt_len = Packet->len;
-		ret = copy_to_user(buf, Packet->data,
+	if (packet) {
+		pkt_len = packet->len;
+		ret = copy_to_user(buf, packet->data,
 				   min_t(size_t, pkt_len, size));
 		if (ret) {
-			dev_kfree_skb(Packet);
+			dev_kfree_skb(packet);
 			BCM_DEBUG_PRINT(ad, DBG_TYPE_PRINTK, 0, 0,
 					"Returning from copy to user failure\n");
 			return -EFAULT;
 		}
 		BCM_DEBUG_PRINT(ad, DBG_TYPE_OTHERS, OSAL_DBG, DBG_LVL_ALL,
 				"Read %zd Bytes From Adapter packet = %p by process %d!\n",
-				pkt_len, Packet, current->pid);
-		dev_kfree_skb(Packet);
+				pkt_len, packet, current->pid);
+		dev_kfree_skb(packet);
 	}
 
 	BCM_DEBUG_PRINT(ad, DBG_TYPE_OTHERS, OSAL_DBG, DBG_LVL_ALL, "<\n");
