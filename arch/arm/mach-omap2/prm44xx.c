2aba071c50591eaa7a2e33d3df296dd74232691e
Nishanth Menon <nm@ti.com>
2014-08-11 14:58:23 -0500

    ARM: OMAP4+: PRM: Enable wakeup capability for OMAP5, DRA7
    OMAP5 and DRA7 can now use pinctrl based I/O daisychain wakeup
    capability. So, enable the support.
    
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Acked-by: Santosh Shilimkar <santosh.shilimkar@ti.com>

--- arch/arm/mach-omap2/prm44xx.c
+++ arch/arm/mach-omap2/prm44xx.c
@@ -693,7 +693,7 @@ static struct prm_ll_data omap44xx_prm_ll_data = {
 
 int __init omap44xx_prm_init(void)
 {
-	if (cpu_is_omap44xx())
+	if (cpu_is_omap44xx() || soc_is_omap54xx() || soc_is_dra7xx())
 		prm_features |= PRM_HAS_IO_WAKEUP;
 
 	return prm_register(&omap44xx_prm_ll_data);
