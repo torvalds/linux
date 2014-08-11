8144f1f69943f447fd1bcb2d26ca011002d5df63
Jeff Layton <jlayton@primarydata.com>
2014-08-11 13:36:54 -0400

    locks: show delegations as "DELEG" in /proc/locks
    Now that they are a distinct lease type, show them as such.
    
    Cc: J. Bruce Fields <bfields@fieldses.org>
    Signed-off-by: Jeff Layton <jlayton@primarydata.com>

--- fs/locks.c
+++ fs/locks.c
@@ -2452,7 +2452,11 @@ static void lock_get_status(struct seq_file *f, struct file_lock *fl,
 			seq_puts(f, "FLOCK  ADVISORY  ");
 		}
 	} else if (IS_LEASE(fl)) {
-		seq_puts(f, "LEASE  ");
+		if (fl->fl_flags & FL_DELEG)
+			seq_puts(f, "DELEG  ");
+		else
+			seq_puts(f, "LEASE  ");
+
 		if (lease_breaking(fl))
 			seq_puts(f, "BREAKING  ");
 		else if (fl->fl_file)
