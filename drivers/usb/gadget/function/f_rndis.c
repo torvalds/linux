70d6557c5913961b590f3807302664ce36b36c6f
xerox_lin <xerox_lin@htc.com>
2014-08-14 14:48:44 +0800

    usb: Add support for rndis uplink aggregation
    RNDIS protocol supports data aggregation on uplink and can help
    reduce mips by reducing number of interrupts on device. Throughput
    also improved by 20-30%. Aggregation is disabled by setting
    aggregation packet size to 1. To help better UL throughput, set
    as ul aggregation support to 3 rndis packets by default. It can be
    configured via module parameter: rndis_ul_max_pkt_per_xfer.
    
    Change-Id: I0b62a21a5c3ceb6b04933d0d6da33301dbafe493
    Signed-off-by: Vamsi Krishna <vskrishn@codeaurora.org>
    Signed-off-by: Xerox Lin <xerox_lin@htc.com>

--- drivers/usb/gadget/function/f_rndis.c
+++ drivers/usb/gadget/function/f_rndis.c
@@ -70,6 +70,16 @@
  *   - MS-Windows drivers sometimes emit undocumented requests.
  */
 
+static bool rndis_multipacket_dl_disable;
+module_param(rndis_multipacket_dl_disable, bool, S_IRUGO|S_IWUSR);
+MODULE_PARM_DESC(rndis_multipacket_dl_disable,
+	"Disable RNDIS Multi-packet support in DownLink");
+
+static unsigned int rndis_ul_max_pkt_per_xfer = 3;
+module_param(rndis_ul_max_pkt_per_xfer, uint, S_IRUGO | S_IWUSR);
+MODULE_PARM_DESC(rndis_ul_max_pkt_per_xfer,
+       "Maximum packets per transfer for UL aggregation");
+
 struct f_rndis {
 	struct gether			port;
 	u8				ctrl_id, data_id;
@@ -792,6 +802,7 @@ rndis_bind(struct usb_configuration *c, struct usb_function *f)
 
 	rndis_set_param_medium(rndis->params, RNDIS_MEDIUM_802_3, 0);
 	rndis_set_host_mac(rndis->params, rndis->ethaddr);
+	rndis_set_max_pkt_xfer(rndis->config, rndis_ul_max_pkt_per_xfer);
 
 	if (rndis->manufacturer && rndis->vendorID &&
 			rndis_set_param_vendor(rndis->params, rndis->vendorID,
@@ -978,6 +989,7 @@ static struct usb_function *rndis_alloc(struct usb_function_instance *fi)
 	rndis->port.header_len = sizeof(struct rndis_packet_msg_type);
 	rndis->port.wrap = rndis_add_header;
 	rndis->port.unwrap = rndis_rm_hdr;
+	rndis->port.ul_max_pkts_per_xfer = rndis_ul_max_pkt_per_xfer;
 
 	rndis->port.func.name = "rndis";
 	/* descriptors are per-instance copies */
