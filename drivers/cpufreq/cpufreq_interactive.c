371db7789926170f9235c540c7cb3608c51ab9bf
Junjie Wu <junjiew@codeaurora.org>
2014-08-15 16:34:37 -0700

    cpufreq: interactive: Round up timer_rate to match jiffy
    Timers are scheduled in unit of jiffies. Round up timer_rate so that
    it matches the actual sampling period.
    
    Change-Id: I88386a5a448e40333f9a9b9f0cf72af58cb54656
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -855,12 +855,18 @@ static ssize_t store_timer_rate(struct cpufreq_interactive_tunables *tunables,
 		const char *buf, size_t count)
 {
 	int ret;
-	unsigned long val;
+	unsigned long val, val_round;
 
 	ret = strict_strtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
-	tunables->timer_rate = val;
+
+	val_round = jiffies_to_usecs(usecs_to_jiffies(val));
+	if (val != val_round)
+		pr_warn("timer_rate not aligned to jiffy. Rounded up to %lu\n",
+			val_round);
+
+	tunables->timer_rate = val_round;
 	return count;
 }
 
