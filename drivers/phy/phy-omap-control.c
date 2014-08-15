437a6bc478e6530efad9aeb04b84b2f5c4e356d5
Peter Griffin <peter.griffin@linaro.org>
2014-08-15 13:40:08 +0100

    phy: phy-omap-control: Remove unncessary site specific OOM messages
    The site specific OOM messages are unncessary, because they duplicate
    messages from the memory subsystem which include dump_stack().
    
    Removing these superflous messages makes the kernel smaller. A discussion
    here http://patchwork.ozlabs.org/patch/324158/ found that all error paths
    from kzalloc will print a error message, and that any error path which maybe
    found which doesn't would be considered a bug in kzalloc.
    
    Signed-off-by: Peter Griffin <peter.griffin@linaro.org>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- drivers/phy/phy-omap-control.c
+++ drivers/phy/phy-omap-control.c
@@ -295,10 +295,8 @@ static int omap_control_phy_probe(struct platform_device *pdev)
 
 	control_phy = devm_kzalloc(&pdev->dev, sizeof(*control_phy),
 		GFP_KERNEL);
-	if (!control_phy) {
-		dev_err(&pdev->dev, "unable to alloc memory for control phy\n");
+	if (!control_phy)
 		return -ENOMEM;
-	}
 
 	control_phy->dev = &pdev->dev;
 	control_phy->type = *(enum omap_control_phy_type *)of_id->data;
