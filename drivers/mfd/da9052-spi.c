e9e9d3973594cadd9e892bc79f914f299bb61124
Axel Lin <axel.lin@ingics.com>
2014-08-16 21:23:40 +0800

    mfd: da9052: Avoid setting read_flag_mask for da9052-i2c driver
    Current code init regmap with &da9052_regmap_config for both da9052-spi and
    da9052-i2c drivers. da9052-spi sets the read_flag_mask.
    The same setting may be applied for da9052-i2c if da9052-spi driver is loaded
    first because they actually use the same regmap_config setting.
    Fix this issue by using a local variable for regmap_config in da9052-spi driver,
    so the settings in spi driver won't impact the settings in i2c driver.
    Also makes da9052_regmap_config const to avoid similar issue.
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Acked-by: Adam Thomson <Adam.Thomson.Opensource@diasemi.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/da9052-spi.c
+++ drivers/mfd/da9052-spi.c
@@ -23,6 +23,7 @@
 
 static int da9052_spi_probe(struct spi_device *spi)
 {
+	struct regmap_config config;
 	int ret;
 	const struct spi_device_id *id = spi_get_device_id(spi);
 	struct da9052 *da9052;
@@ -40,10 +41,10 @@ static int da9052_spi_probe(struct spi_device *spi)
 
 	spi_set_drvdata(spi, da9052);
 
-	da9052_regmap_config.read_flag_mask = 1;
-	da9052_regmap_config.write_flag_mask = 0;
+	config = da9052_regmap_config;
+	config.read_flag_mask = 1;
 
-	da9052->regmap = devm_regmap_init_spi(spi, &da9052_regmap_config);
+	da9052->regmap = devm_regmap_init_spi(spi, &config);
 	if (IS_ERR(da9052->regmap)) {
 		ret = PTR_ERR(da9052->regmap);
 		dev_err(&spi->dev, "Failed to allocate register map: %d\n",
