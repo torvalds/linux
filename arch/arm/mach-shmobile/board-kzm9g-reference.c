fc35ca258c079411b64ed94e517eb04e7d89bc44
Magnus Damm <damm+renesas@opensource.se>
2014-08-20 22:03:30 +0900

    ARM: shmobile: kzm9g: Use shmobile_init_delay()
    Adjust the KZM9G board support code to use shmobile_init_delay()
    together with CPU Frequency settings from the DTS.
    
    Signed-off-by: Magnus Damm <damm+renesas@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/board-kzm9g-reference.c
+++ arch/arm/mach-shmobile/board-kzm9g-reference.c
@@ -51,7 +51,7 @@ static const char *kzm9g_boards_compat_dt[] __initdata = {
 DT_MACHINE_START(KZM9G_DT, "kzm9g-reference")
 	.smp		= smp_ops(sh73a0_smp_ops),
 	.map_io		= sh73a0_map_io,
-	.init_early	= sh73a0_init_delay,
+	.init_early	= shmobile_init_delay,
 	.init_machine	= kzm_init,
 	.init_late	= shmobile_init_late,
 	.dt_compat	= kzm9g_boards_compat_dt,
