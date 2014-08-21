8913dc0bb913ac3dc83ed5c10bac2f4e55431981
Paul Zimmerman <Paul.Zimmerman@synopsys.com>
2014-08-21 20:28:20 +0000

    usb: gadget: document a usb_ep_dequeue() requirement
    Document the requirement that the request be dequeued and its
    completion routine called before usb_ep_dequeue() returns. Also
    fix some capitalization issues in the existing text.
    
    Signed-off-by: Paul Zimmerman <paulz@synopsys.com>
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- include/linux/usb/gadget.h
+++ include/linux/usb/gadget.h
@@ -345,12 +345,13 @@ static inline int usb_ep_queue(struct usb_ep *ep,
  * @ep:the endpoint associated with the request
  * @req:the request being canceled
  *
- * if the request is still active on the endpoint, it is dequeued and its
+ * If the request is still active on the endpoint, it is dequeued and its
  * completion routine is called (with status -ECONNRESET); else a negative
- * error code is returned.
+ * error code is returned. This is guaranteed to happen before the call to
+ * usb_ep_dequeue() returns.
  *
- * note that some hardware can't clear out write fifos (to unlink the request
- * at the head of the queue) except as part of disconnecting from usb.  such
+ * Note that some hardware can't clear out write fifos (to unlink the request
+ * at the head of the queue) except as part of disconnecting from usb. Such
  * restrictions prevent drivers from supporting configuration changes,
  * even to configuration zero (a "chapter 9" requirement).
  */
