e0455e194a5e0cf49bc7596a20d4f7e47995b9c6
Andreea-Cristina Bernat <bernat.ada@gmail.com>
2014-08-22 17:15:36 +0300

    perf/callchain: Replace rcu_assign_pointer() with RCU_INIT_POINTER()
    The use of "rcu_assign_pointer()" is NULLing out the pointer.
    According to RCU_INIT_POINTER()'s block comment:
    
     "1.   This use of RCU_INIT_POINTER() is NULLing out the pointer"
    
    it is better to use it instead of rcu_assign_pointer() because it has a
    smaller overhead.
    
     The following Coccinelle semantic patch was used:
     @@
     @@
    
     - rcu_assign_pointer
     + RCU_INIT_POINTER
       (..., NULL)
    
    Signed-off-by: Andreea-Cristina Bernat <bernat.ada@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: paulmck@linux.vnet.ibm.com
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Link: http://lkml.kernel.org/r/20140822141536.GA32051@ada
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/events/callchain.c
+++ kernel/events/callchain.c
@@ -52,7 +52,7 @@ static void release_callchain_buffers(void)
 	struct callchain_cpus_entries *entries;
 
 	entries = callchain_cpus_entries;
-	rcu_assign_pointer(callchain_cpus_entries, NULL);
+	RCU_INIT_POINTER(callchain_cpus_entries, NULL);
 	call_rcu(&entries->rcu_head, release_callchain_buffers_rcu);
 }
 
