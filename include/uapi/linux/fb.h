3cbf2deaa0ce0dcf43df429832c051ab75d6728b
Jamie Nicol <jamie.nicol@arm.com>
2014-08-22 17:08:47 +0100

    FROMLIST: fb: add dma-buf support
    Add support for the dma-buf exporter role to the frame buffer API. The
    importer role isn't meaningful for frame buffer devices, as the frame
    buffer device model doesn't allow using externally allocated memory.
    
    taken from an RFC on the linaro-mm-sig mailing list:
    http://lists.linaro.org/pipermail/linaro-mm-sig/2012-June/002167.html
    
    Fixes by Mark Yao:
      add FBIOGET_DMABUF to compat_ioctl.
    
    Change-Id: I39c9bbdd6b88c6d5ba7524abfc5b560dceb4633e
    Signed-off-by: Guillaume Tucker <guillaume.tucker@arm.com>
    Signed-off-by: Mark Yao <mark.yao@rock-chips.com>
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>
    (am from https://patchwork.linuxtv.org/patch/12980)

--- include/uapi/linux/fb.h
+++ include/uapi/linux/fb.h
@@ -35,6 +35,7 @@
 #define FBIOPUT_MODEINFO        0x4617
 #define FBIOGET_DISPINFO        0x4618
 #define FBIO_WAITFORVSYNC	_IOW('F', 0x20, __u32)
+#define FBIOGET_DMABUF		_IOR('F', 0x21, struct fb_dmabuf_export)
 
 #define FB_TYPE_PACKED_PIXELS		0	/* Packed Pixels	*/
 #define FB_TYPE_PLANES			1	/* Non interleaved planes */
@@ -399,5 +400,9 @@ struct fb_cursor {
 #define FB_BACKLIGHT_MAX	0xFF
 #endif
 
+struct fb_dmabuf_export {
+	__u32 fd;
+	__u32 flags;
+};
 
 #endif /* _UAPI_LINUX_FB_H */
