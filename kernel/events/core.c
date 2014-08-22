70691d4a0bf7c871559d4ef1b0056edefbca123b
Andreea-Cristina Bernat <bernat.ada@gmail.com>
2014-08-22 16:26:05 +0300

    perf/core: Replace rcu_assign_pointer() with RCU_INIT_POINTER()
    The use of "rcu_assign_pointer()" is NULLing out the pointer.
    According to RCU_INIT_POINTER()'s block comment:
    
      "1.   This use of RCU_INIT_POINTER() is NULLing out the pointer"
    
    it is better to use it instead of rcu_assign_pointer() because it has a
    smaller overhead.
    
    The following Coccinelle semantic patch was used:
      @@
      @@
    
      - rcu_assign_pointer
      + RCU_INIT_POINTER
        (..., NULL)
    
    Signed-off-by: Andreea-Cristina Bernat <bernat.ada@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Link: http://lkml.kernel.org/r/20140822132605.GA20130@ada
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -5908,7 +5908,7 @@ static void swevent_hlist_release(struct swevent_htable *swhash)
 	if (!hlist)
 		return;
 
-	rcu_assign_pointer(swhash->swevent_hlist, NULL);
+	RCU_INIT_POINTER(swhash->swevent_hlist, NULL);
 	kfree_rcu(hlist, rcu_head);
 }
 
