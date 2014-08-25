1befe7e49f8d4e706e5ef39fb57dac1da734f163
Gabriel FERNANDEZ <gabriel.fernandez@st.com>
2014-08-25 16:44:00 +0200

    ARM: STi: DT: STiH407: 407 DT Entry for clockgen C0
    Patch adds DT entries for clockgen C0
    
    Signed-off-by: Gabriel Fernandez <gabriel.fernandez@linaro.org>
    Signed-off-by: Olivier Bideau <olivier.bideau@st.com>
    Signed-off-by: Maxime Coquelin <maxime.coquelin@st.com>

---
+++ include/dt-bindings/clock/stih407-clks.h
@@ -0,0 +1,11 @@
+/*
+ * This header provides constants clk index STMicroelectronics
+ * STiH407 SoC.
+ */
+#ifndef _DT_BINDINGS_CLK_STIH407
+#define _DT_BINDINGS_CLK_STIH407
+
+/* CLOCKGEN C0 */
+#define CLK_EXT2F_A9		13
+
+#endif
