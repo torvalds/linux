e21eba05afd288a227320f797864ddd859397eed
Hans de Goede <hdegoede@redhat.com>
2014-08-25 12:21:56 +0200

    xhci: Disable streams on Via XHCI with device-id 0x3432
    This is a bit bigger hammer then I would like to use for this, but for now
    it will have to make do. I'm working on getting my hands on one of these so
    that I can try to get streams to work (with a quirk flag if necessary) and
    then we can re-enable them.
    
    For now this at least makes uas capable disk enclosures work again by forcing
    fallback to the usb-storage driver.
    
    https://bugzilla.kernel.org/show_bug.cgi?id=79511
    
    Cc: stable@vger.kernel.org # 3.15
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Acked-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/xhci-pci.c
+++ drivers/usb/host/xhci-pci.c
@@ -155,6 +155,11 @@ static void xhci_pci_quirks(struct device *dev, struct xhci_hcd *xhci)
 	if (pdev->vendor == PCI_VENDOR_ID_VIA)
 		xhci->quirks |= XHCI_RESET_ON_RESUME;
 
+	/* See https://bugzilla.kernel.org/show_bug.cgi?id=79511 */
+	if (pdev->vendor == PCI_VENDOR_ID_VIA &&
+			pdev->device == 0x3432)
+		xhci->quirks |= XHCI_BROKEN_STREAMS;
+
 	if (xhci->quirks & XHCI_RESET_ON_RESUME)
 		xhci_dbg_trace(xhci, trace_xhci_dbg_quirks,
 				"QUIRK: Resetting on resume");
