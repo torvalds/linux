8df438571cdbd5c4fcd1b25b19eea1ad5c3cf777
Dinh Nguyen <dinguyen@opensource.altera.com>
2014-08-26 11:19:53 -0500

    usb: dwc2: move "samsung,s3c6400-hsotg" into common platform
    Move the "samsung,s3c6400-hsotg" binding as the probe function in the gadget
    driver will get removed when the dual-role driver is implemented.
    
    Signed-off-by: Dinh Nguyen <dinguyen@opensource.altera.com>
    Acked-by: Paul Zimmerman <paulz@synopsys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/dwc2/gadget.c
+++ drivers/usb/dwc2/gadget.c
@@ -3642,7 +3642,6 @@ static int s3c_hsotg_resume(struct platform_device *pdev)
 
 #ifdef CONFIG_OF
 static const struct of_device_id s3c_hsotg_of_ids[] = {
-	{ .compatible = "samsung,s3c6400-hsotg", },
 	{ .compatible = "snps,dwc2", },
 	{ /* sentinel */ }
 };
