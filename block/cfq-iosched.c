7b5af5cffce569298b1d03af1ddf1dc43570ab42
Toshiaki Makita <makita.toshiaki@lab.ntt.co.jp>
2014-08-28 17:14:58 +0900

    cfq-iosched: Add comments on update timing of weight
    Explain that weight has to be updated on activation.
    This complements previous fix e15693ef18e1 ("cfq-iosched: Fix wrong
    children_weight calculation").
    
    Signed-off-by: Toshiaki Makita <makita.toshiaki@lab.ntt.co.jp>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -1272,6 +1272,9 @@ __cfq_group_service_tree_add(struct cfq_rb_root *st, struct cfq_group *cfqg)
 	rb_insert_color(&cfqg->rb_node, &st->rb);
 }
 
+/*
+ * This has to be called only on activation of cfqg
+ */
 static void
 cfq_update_group_weight(struct cfq_group *cfqg)
 {
@@ -1303,6 +1306,11 @@ cfq_group_service_tree_add(struct cfq_rb_root *st, struct cfq_group *cfqg)
 	/* add to the service tree */
 	BUG_ON(!RB_EMPTY_NODE(&cfqg->rb_node));
 
+	/*
+	 * Update leaf_weight.  We cannot update weight at this point
+	 * because cfqg might already have been activated and is
+	 * contributing its current weight to the parent's child_weight.
+	 */
 	cfq_update_group_leaf_weight(cfqg);
 	__cfq_group_service_tree_add(st, cfqg);
 
