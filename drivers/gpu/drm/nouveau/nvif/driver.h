3cfb2face6205d30ecfc0145d68cd9e0c3dfe6f1
Al Viro <viro@zeniv.linux.org.uk>
2014-08-31 15:06:09 -0400

    nouveau: __iomem misannotations
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/gpu/drm/nouveau/nvif/driver.h
+++ drivers/gpu/drm/nouveau/nvif/driver.h
@@ -9,8 +9,8 @@ struct nvif_driver {
 	int (*suspend)(void *priv);
 	int (*resume)(void *priv);
 	int (*ioctl)(void *priv, bool super, void *data, u32 size, void **hack);
-	void *(*map)(void *priv, u64 handle, u32 size);
-	void (*unmap)(void *priv, void *ptr, u32 size);
+	void __iomem *(*map)(void *priv, u64 handle, u32 size);
+	void (*unmap)(void *priv, void __iomem *ptr, u32 size);
 	bool keep;
 };
 
