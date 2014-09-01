e35e305eff0fda4ddd5cfbd8eea06948faa503af
Ajay Kumar <ajaykumar.rs@samsung.com>
2014-09-01 15:40:02 +0530

    drm/panel: simple: Add AUO B116XW03 panel support
    The AUO B116XW03 is a 11.6" HD TFT LCD panel connecting to a LVDS
    interface and with an integrated LED backlight unit.
    
    This panel is used on the Samsung Chromebook(XE303C12).
    
    Signed-off-by: Ajay Kumar <ajaykumar.rs@samsung.com>
    [treding@nvidia.com: add missing .bpc field]
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -369,6 +369,29 @@ static const struct panel_desc auo_b101xtn01 = {
 	},
 };
 
+static const struct drm_display_mode auo_b116xw03_mode = {
+	.clock = 70589,
+	.hdisplay = 1366,
+	.hsync_start = 1366 + 40,
+	.hsync_end = 1366 + 40 + 40,
+	.htotal = 1366 + 40 + 40 + 32,
+	.vdisplay = 768,
+	.vsync_start = 768 + 10,
+	.vsync_end = 768 + 10 + 12,
+	.vtotal = 768 + 10 + 12 + 6,
+	.vrefresh = 60,
+};
+
+static const struct panel_desc auo_b116xw03 = {
+	.modes = &auo_b116xw03_mode,
+	.num_modes = 1,
+	.bpc = 6,
+	.size = {
+		.width = 256,
+		.height = 144,
+	},
+};
+
 static const struct drm_display_mode auo_b133xtn01_mode = {
 	.clock = 69500,
 	.hdisplay = 1366,
@@ -658,6 +681,9 @@ static const struct of_device_id platform_of_match[] = {
 	}, {
 		.compatible = "auo,b101xtn01",
 		.data = &auo_b101xtn01,
+	}, {
+		.compatible = "auo,b116xw03",
+		.data = &auo_b116xw03,
 	}, {
 		.compatible = "auo,b133htn01",
 		.data = &auo_b133htn01,
