1ccf58579661282083951323f08ea51441b9ad03
zsq <zsq@rock-chips.com>
2014-09-02 18:04:16 +0800

    fix rga_plus rgb2yuv reg error

--- drivers/video/rockchip/rga/rga_reg_info.c
+++ drivers/video/rockchip/rga/rga_reg_info.c
@@ -708,7 +708,7 @@ s32 RGA_set_dst(u8 *base, const struct rga_req *msg)
         case RK_FORMAT_YCbCr_422_SP :
             *bRGA_PRESCL_CB_MST = (u32)msg->dst.uv_addr + ((y_off) * stride) + ((x_off) * pw);
             *bRGA_DST_UV_MST = (u32)msg->dst.uv_addr + ((y_off) * stride) + ((x_off));
-            *bRGA_YUV_OUT_CFG |= (0 << 3) | 1 | ((msg->yuv2rgb_mode >> 4) & 2);
+            *bRGA_YUV_OUT_CFG |= (0 << 3) | 1 | (((msg->yuv2rgb_mode >> 4) & 3) << 4);
             break;
         case RK_FORMAT_YCbCr_422_P  :
             *bRGA_PRESCL_CB_MST = (u32)msg->dst.uv_addr + ((y_off) * stride) + ((x_off>>1) * pw);
@@ -717,7 +717,7 @@ s32 RGA_set_dst(u8 *base, const struct rga_req *msg)
         case RK_FORMAT_YCbCr_420_SP :
             *bRGA_PRESCL_CB_MST = (u32)msg->dst.uv_addr + ((y_off>>1) * stride) + ((x_off) * pw);
             *bRGA_DST_UV_MST = (u32)msg->dst.uv_addr + ((y_off>>1) * stride) + ((x_off));
-            *bRGA_YUV_OUT_CFG |= (0 << 3)|(1 << 1) | 1 | ((msg->yuv2rgb_mode >> 4) & 2);
+            *bRGA_YUV_OUT_CFG |= (0 << 3)|(1 << 1) | 1 | (((msg->yuv2rgb_mode >> 4) & 3) << 4);
             break;
         case RK_FORMAT_YCbCr_420_P :
             *bRGA_PRESCL_CB_MST = (u32)msg->dst.uv_addr + ((y_off>>1) * stride) + ((x_off>>1) * pw);
@@ -726,7 +726,7 @@ s32 RGA_set_dst(u8 *base, const struct rga_req *msg)
         case RK_FORMAT_YCrCb_422_SP :
             *bRGA_PRESCL_CB_MST = (u32)msg->dst.uv_addr + ((y_off) * stride) + ((x_off) * pw);
             *bRGA_DST_UV_MST = (u32)msg->dst.uv_addr + ((y_off) * stride) + ((x_off));
-            *bRGA_YUV_OUT_CFG |= (1 << 3)|(0 << 1) | 1 | ((msg->yuv2rgb_mode >> 4) & 2);
+            *bRGA_YUV_OUT_CFG |= (1 << 3)|(0 << 1) | 1 | (((msg->yuv2rgb_mode >> 4) & 3) << 4);
             break;
         case RK_FORMAT_YCrCb_422_P  :
             *bRGA_PRESCL_CB_MST = (u32)msg->dst.uv_addr + ((y_off) * stride) + ((x_off>>1) * pw);
@@ -735,7 +735,7 @@ s32 RGA_set_dst(u8 *base, const struct rga_req *msg)
         case RK_FORMAT_YCrCb_420_SP :
             *bRGA_PRESCL_CB_MST = (u32)msg->dst.uv_addr + ((y_off>>1) * stride) + ((x_off) * pw);
             *bRGA_DST_UV_MST = (u32)msg->dst.uv_addr + ((y_off>>1) * stride) + ((x_off));
-            *bRGA_YUV_OUT_CFG |= (1 << 3)|(1 << 1) | 1 | ((msg->yuv2rgb_mode >> 4) & 2);
+            *bRGA_YUV_OUT_CFG |= (1 << 3)|(1 << 1) | 1 | (((msg->yuv2rgb_mode >> 4) & 3) << 4);
             break;
         case RK_FORMAT_YCrCb_420_P :
             *bRGA_PRESCL_CB_MST = (u32)msg->dst.uv_addr + ((y_off>>1) * stride) + ((x_off>>1) * pw);
