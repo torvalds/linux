0b93a4c838fa10370d72f86fe712426ac63804de
Felipe Balbi <balbi@ti.com>
2014-09-04 10:28:10 -0500

    usb: dwc3: fix TRB completion when multiple TRBs are started
    After commit 2ec2a8be (usb: dwc3: gadget:
    always enable IOC on bulk/interrupt transfers)
    we created a situation where it was possible to
    hang a bulk/interrupt endpoint if we had more
    than one pending request in our queue and they
    were both started with a single Start Transfer
    command.
    
    The problems triggers because we had not enabled
    Transfer In Progress event for those endpoints
    and we were not able to process early giveback
    of requests completed without LST bit set.
    
    Fix the problem by finally enabling Xfer In Progress
    event for all endpoint types, except control.
    
    Fixes: 2ec2a8be (usb: dwc3: gadget: always
    	enable IOC on bulk/interrupt transfers)
    Cc: <stable@vger.kernel.org> # v3.14+
    Reported-by: Pratyush Anand <pratyush.anand@st.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -527,7 +527,7 @@ static int dwc3_gadget_set_ep_config(struct dwc3 *dwc, struct dwc3_ep *dep,
 		dep->stream_capable = true;
 	}
 
-	if (usb_endpoint_xfer_isoc(desc))
+	if (!usb_endpoint_xfer_control(desc))
 		params.param1 |= DWC3_DEPCFG_XFER_IN_PROGRESS_EN;
 
 	/*
@@ -2042,12 +2042,6 @@ static void dwc3_endpoint_interrupt(struct dwc3 *dwc,
 		dwc3_endpoint_transfer_complete(dwc, dep, event);
 		break;
 	case DWC3_DEPEVT_XFERINPROGRESS:
-		if (!usb_endpoint_xfer_isoc(dep->endpoint.desc)) {
-			dev_dbg(dwc->dev, "%s is not an Isochronous endpoint\n",
-					dep->name);
-			return;
-		}
-
 		dwc3_endpoint_transfer_complete(dwc, dep, event);
 		break;
 	case DWC3_DEPEVT_XFERNOTREADY:
