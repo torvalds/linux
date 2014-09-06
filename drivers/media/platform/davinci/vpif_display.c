815789244eda0b3be11fb1824354ca20c8ec3508
Prabhakar Lad <prabhakar.csengg@gmail.com>
2014-09-06 12:26:50 -0300

    [media] media: davinci: vpif_display: fix the check on suspend/resume callbacks
    It is possible to call STREAMON without having any buffers queued.
    So vb2_is_streaming() can return true without start_streaming()
    having been called. Only after at least one buffer has been
    queued will start_streaming be called.
    
    The check vb2_is_streaming() is incorrect as this would start
    the DMA without having proper DMA pointers set up. this patch
    uses vb2_start_streaming_called() instead to check is streaming
    was called.
    
    Signed-off-by: Lad, Prabhakar <prabhakar.csengg@gmail.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/platform/davinci/vpif_display.c
+++ drivers/media/platform/davinci/vpif_display.c
@@ -1400,7 +1400,7 @@ static int vpif_suspend(struct device *dev)
 		ch = vpif_obj.dev[i];
 		common = &ch->common[VPIF_VIDEO_INDEX];
 
-		if (!vb2_is_streaming(&common->buffer_queue))
+		if (!vb2_start_streaming_called(&common->buffer_queue))
 			continue;
 
 		mutex_lock(&common->lock);
@@ -1432,7 +1432,7 @@ static int vpif_resume(struct device *dev)
 		ch = vpif_obj.dev[i];
 		common = &ch->common[VPIF_VIDEO_INDEX];
 
-		if (!vb2_is_streaming(&common->buffer_queue))
+		if (!vb2_start_streaming_called(&common->buffer_queue))
 			continue;
 
 		mutex_lock(&common->lock);
