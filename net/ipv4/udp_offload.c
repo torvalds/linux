72bb17b37b9076e12b388feee4a52e85ef8f6620
Eric Dumazet <edumazet@google.com>
2014-09-09 08:29:12 -0700

    ipv4: udp4_gro_complete() is static
    net/ipv4/udp_offload.c:339:5: warning: symbol 'udp4_gro_complete' was
    not declared. Should it be static?
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Tom Herbert <therbert@google.com>
    Fixes: 57c67ff4bd92 ("udp: additional GRO support")
    Acked-by: Tom Herbert <therbert@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/udp_offload.c
+++ net/ipv4/udp_offload.c
@@ -336,7 +336,7 @@ int udp_gro_complete(struct sk_buff *skb, int nhoff)
 	return err;
 }
 
-int udp4_gro_complete(struct sk_buff *skb, int nhoff)
+static int udp4_gro_complete(struct sk_buff *skb, int nhoff)
 {
 	const struct iphdr *iph = ip_hdr(skb);
 	struct udphdr *uh = (struct udphdr *)(skb->data + nhoff);
