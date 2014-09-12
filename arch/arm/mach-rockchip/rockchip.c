47b06fc2d97a0d98d6053357561f2953d6547d25
Heiko Stuebner <heiko@sntech.de>
2014-09-13 00:34:31 +0200

    ARM: rockchip: add a cpufreq-cpu0 device
    Rockchip SoCs can sucessfully use the generic cpufreq-cpu0 driver to
    do frequency scaling. Therefore add a platform device in the machine
    init code.
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>

--- arch/arm/mach-rockchip/rockchip.c
+++ arch/arm/mach-rockchip/rockchip.c
@@ -24,6 +24,12 @@
 #include <asm/hardware/cache-l2x0.h>
 #include "core.h"
 
+static void __init rockchip_dt_init(void)
+{
+	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
+	platform_device_register_simple("cpufreq-cpu0", 0, NULL, 0);
+}
+
 static const char * const rockchip_board_dt_compat[] = {
 	"rockchip,rk2928",
 	"rockchip,rk3066a",
@@ -37,4 +43,5 @@ DT_MACHINE_START(ROCKCHIP_DT, "Rockchip Cortex-A9 (Device Tree)")
 	.l2c_aux_val	= 0,
 	.l2c_aux_mask	= ~0,
 	.dt_compat	= rockchip_board_dt_compat,
+	.init_machine	= rockchip_dt_init,
 MACHINE_END
