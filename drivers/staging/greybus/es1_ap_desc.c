082570b0eebdd108c6b50b6c1299cf6d02a6ad82
Greg Kroah-Hartman <greg@kroah.com>
2014-09-13 16:15:07 -0700

    greybus: es1 endpoint descriptor: minor fixes to get the config right

--- drivers/staging/greybus/es1_ap_desc.c
+++ drivers/staging/greybus/es1_ap_desc.c
@@ -40,7 +40,7 @@ static const u8 es1_config_descriptor[] = {
 	0x04,		/*  __u8  if_bDescriptorType; Interface */
 	0x00,		/*  __u8  if_bInterfaceNumber; */
 	0x00,		/*  __u8  if_bAlternateSetting; */
-	0x02,		/*  __u8  if_bNumEndpoints; */
+	0x03,		/*  __u8  if_bNumEndpoints; */
 	0xff,		/*  __u8  if_bInterfaceClass; Vendor-specific */
 	0xff,		/*  __u8  if_bInterfaceSubClass; Vendor-specific */
 	0xff,		/*  __u8  if_bInterfaceProtocol; Vendor-specific */
@@ -50,21 +50,21 @@ static const u8 es1_config_descriptor[] = {
 	0x07,		/*  __u8   ep_bLength; */
 	0x05,		/*  __u8   ep_bDescriptorType; Endpoint */
 	0x81,		/*  __u8   ep_bEndpointAddress; IN Endpoint 1 */
-	0x03,		/*  __u8   ep_bmAttributes; Bulk */
+	0x03,		/*  __u8   ep_bmAttributes; Interrupt */
 	0x00, 0x04,	/*  __le16 ep_wMaxPacketSize; 1024 */
-	0x00		/*  __u8   ep_bInterval; */
+	0x40,		/*  __u8   ep_bInterval; 64ms */
 
 	0x07,		/*  __u8   ep_bLength; */
 	0x05,		/*  __u8   ep_bDescriptorType; Endpoint */
 	0x82,		/*  __u8   ep_bEndpointAddress; IN Endpoint 2 */
 	0x02,		/*  __u8   ep_bmAttributes; Bulk */
 	0x00, 0x04,	/*  __le16 ep_wMaxPacketSize; 1024 */
-	0x00		/*  __u8   ep_bInterval; */
+	0x40		/*  __u8   ep_bInterval; */
 
 	0x07,		/*  __u8   ep_bLength; */
 	0x05,		/*  __u8   ep_bDescriptorType; Endpoint */
 	0x02,		/*  __u8   ep_bEndpointAddress; Out Endpoint 2 */
 	0x02,		/*  __u8   ep_bmAttributes; Bulk */
 	0x00, 0x04,	/*  __le16 ep_wMaxPacketSize; 1024 */
-	0x00		/*  __u8   ep_bInterval; */
+	0x40		/*  __u8   ep_bInterval; */
 };
