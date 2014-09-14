ddfc1593737f36a04ec5e23e9cb0c6b2bad3f3a6
Eyal Shapira <eyal@wizery.com>
2014-09-14 16:40:38 +0300

    iwlwifi: mvm: limit aggregation size in low latency to 6
    This is a tradeoff between enabling better throughput for
    bursty traffic and low latency. The number 6 was found to be
    a good tradeoff for the Miracast use case which is the
    major use case for low latency.
    
    Signed-off-by: Eyal Shapira <eyalx.shapira@intel.com>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>

--- drivers/net/wireless/iwlwifi/mvm/ops.c
+++ drivers/net/wireless/iwlwifi/mvm/ops.c
@@ -422,7 +422,7 @@ iwl_op_mode_mvm_start(struct iwl_trans *trans, const struct iwl_cfg *cfg,
 		mvm->first_agg_queue = 12;
 	}
 	mvm->sf_state = SF_UNINIT;
-	mvm->low_latency_agg_frame_limit = 1;
+	mvm->low_latency_agg_frame_limit = 6;
 
 	mutex_init(&mvm->mutex);
 	mutex_init(&mvm->d0i3_suspend_mutex);
