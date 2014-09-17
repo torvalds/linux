c0e7f7ee717e2b4c5791e7422424c96b5008c39e
Daniel Thompson <daniel.thompson@linaro.org>
2014-09-17 17:12:06 +0100

    ARM: 8150/3: fiq: Replace default FIQ handler
    This patch introduces a new default FIQ handler that is structured in a
    similar way to the existing ARM exception handler and result in the FIQ
    being handled by C code running on the SVC stack (despite this code run
    in the FIQ handler is subject to severe limitations with respect to
    locking making normal interaction with the kernel impossible).
    
    This default handler allows concepts that on x86 would be handled using
    NMIs to be realized on ARM.
    
    Credit:
    
        This patch is a near complete re-write of a patch originally
        provided by Anton Vorontsov. Today only a couple of small fragments
        survive, however without Anton's work to build from this patch would
        not exist. Thanks also to Russell King for spoonfeeding me a variety
        of fixes during the review cycle.
    
    Signed-off-by: Daniel Thompson <daniel.thompson@linaro.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Nicolas Pitre <nico@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/fiq.c
+++ arch/arm/kernel/fiq.c
@@ -52,7 +52,8 @@
 		(unsigned)&vector_fiq_offset;		\
 	})
 
-static unsigned long no_fiq_insn;
+static unsigned long dfl_fiq_insn;
+static struct pt_regs dfl_fiq_regs;
 
 /* Default reacquire function
  * - we always relinquish FIQ control
@@ -60,8 +61,15 @@ static unsigned long no_fiq_insn;
  */
 static int fiq_def_op(void *ref, int relinquish)
 {
-	if (!relinquish)
-		set_fiq_handler(&no_fiq_insn, sizeof(no_fiq_insn));
+	if (!relinquish) {
+		/* Restore default handler and registers */
+		local_fiq_disable();
+		set_fiq_regs(&dfl_fiq_regs);
+		set_fiq_handler(&dfl_fiq_insn, sizeof(dfl_fiq_insn));
+		local_fiq_enable();
+
+		/* FIXME: notify irq controller to standard enable FIQs */
+	}
 
 	return 0;
 }
@@ -150,6 +158,7 @@ EXPORT_SYMBOL(disable_fiq);
 void __init init_FIQ(int start)
 {
 	unsigned offset = FIQ_OFFSET;
-	no_fiq_insn = *(unsigned long *)(0xffff0000 + offset);
+	dfl_fiq_insn = *(unsigned long *)(0xffff0000 + offset);
+	get_fiq_regs(&dfl_fiq_regs);
 	fiq_start = start;
 }
