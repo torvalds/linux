e87b4c7a7ac6d895846570dec637744cf7050df3
NeilBrown <neilb@suse.de>
2014-09-18 16:09:27 +1000

    NFS: don't use STABLE writes during writeback.
    commit b31268ac793fd300da66b9c28bbf0a200339ab96
      FS: Use stable writes when not doing a bulk flush
    
    was a bit heavy handed.
    The particular problem that lead to this patch was that
    small writes to an O_SYNC file we being written as UNSTABLE writes
    followed by a commit.
    This is appropriate for large writes (which require multiple NFS
    requests) but for small writes (single NFS request), using
    NFS_FILE_SYNC is more efficient.
    
    So that patch causes the code to select between the two methods
    depending on how many nfs requests get generated.
    
    Unfortunately this ends up applying to non O_SYNC writes as well.
    In particular if you memory-map a file and update random pages, then
    when they are eventually written out by writeback they will go as
    NFS_FILE_SYNC.  This is inefficient and slows down the application.
    
    So: only set FLUSH_COND_STABLE when wbc->sync_mode is WB_SYNC_ALL.
    With this patch:
     O_SYNC writes are NFS_FILE_SYNC for single requests, and NFS_UNSTABLE
        followed by COMMIT for multiple requests
     Writing immediately before close of fsync follow the same pattern.
     Non-O_SYNC writes without an fsync of close eventually get flushed
     out as UNSTABLE and a commit follows eventually as appropriate.
    
    Signed-off-by: NeilBrown <neilb@suse.de>
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>

--- fs/nfs/write.c
+++ fs/nfs/write.c
@@ -242,11 +242,14 @@ static void nfs_mark_uptodate(struct nfs_page *req)
 
 static int wb_priority(struct writeback_control *wbc)
 {
+	int ret = 0;
 	if (wbc->for_reclaim)
 		return FLUSH_HIGHPRI | FLUSH_STABLE;
+	if (wbc->sync_mode == WB_SYNC_ALL)
+		ret = FLUSH_COND_STABLE;
 	if (wbc->for_kupdate || wbc->for_background)
-		return FLUSH_LOWPRI | FLUSH_COND_STABLE;
-	return FLUSH_COND_STABLE;
+		ret |= FLUSH_LOWPRI;
+	return ret;
 }
 
 /*
