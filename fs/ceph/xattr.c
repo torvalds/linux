0abb43dcacb52145aa265f82c914375d59dfe2da
Yan, Zheng <zyan@redhat.com>
2014-09-18 16:11:12 +0800

    ceph: fix llistxattr on symlink
    only regular file and directory have vxattrs.
    
    Signed-off-by: Yan, Zheng <zyan@redhat.com>

--- fs/ceph/xattr.c
+++ fs/ceph/xattr.c
@@ -284,8 +284,7 @@ static size_t ceph_vxattrs_name_size(struct ceph_vxattr *vxattrs)
 		return ceph_dir_vxattrs_name_size;
 	if (vxattrs == ceph_file_vxattrs)
 		return ceph_file_vxattrs_name_size;
-	BUG();
-
+	BUG_ON(vxattrs);
 	return 0;
 }
 
