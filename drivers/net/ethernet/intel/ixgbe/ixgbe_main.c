ffefa9f64ce021b26aa3fd6558f1d3b6c47b3619
Emil Tantilov <emil.s.tantilov@intel.com>
2014-09-18 08:05:02 +0000

    ixgbe: fix setting of TXDCTL.WTRHESH when ITR is set to 0 and no BQL
    This patch consolidates the logic behind dynamically setting TXDCTL.WTHRESH
    depending on interrupt throttle rate (ITR) setting regardless of BQL.
    
    Previously TXDCTL.WTHRESH was dynamically being set only with BQL being
    enabled, but we have to set it regardless of BQL when ITR is low to avoid
    Tx stalls/hangs.
    
    CC: John Greene <jogreene@redhat.com>
    Reported by: Masayuki Gouji <gouji.masayuki@jp.fujitsu.com>
    Signed-off-by: Emil Tantilov <emil.s.tantilov@intel.com>
    Tested-by: Phil Schmitt <phillip.j.schmitt@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -2982,11 +2982,7 @@ void ixgbe_configure_tx_ring(struct ixgbe_adapter *adapter,
 	 * to or less than the number of on chip descriptors, which is
 	 * currently 40.
 	 */
-#if IS_ENABLED(CONFIG_BQL)
 	if (!ring->q_vector || (ring->q_vector->itr < IXGBE_100K_ITR))
-#else
-	if (!ring->q_vector || (ring->q_vector->itr < 8))
-#endif
 		txdctl |= (1 << 16);	/* WTHRESH = 1 */
 	else
 		txdctl |= (8 << 16);	/* WTHRESH = 8 */
