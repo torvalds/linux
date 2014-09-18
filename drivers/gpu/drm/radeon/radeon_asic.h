721566768787f3b4790d22d993d70befdd440f64
Alex Deucher <alexander.deucher@amd.com>
2014-09-18 16:36:08 -0400

    drm/radeon: consolidate r600_audio.c into r600_hdmi.c
    Most of that functionality is only used by r600_hdmi.c
    and I'm planning to change that further.
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/radeon_asic.h
+++ drivers/gpu/drm/radeon/radeon_asic.h
@@ -392,7 +392,6 @@ void r600_disable_interrupts(struct radeon_device *rdev);
 void r600_rlc_stop(struct radeon_device *rdev);
 /* r600 audio */
 int r600_audio_init(struct radeon_device *rdev);
-struct r600_audio_pin r600_audio_status(struct radeon_device *rdev);
 void r600_audio_fini(struct radeon_device *rdev);
 void r600_audio_set_dto(struct drm_encoder *encoder, u32 clock);
 void r600_hdmi_update_avi_infoframe(struct drm_encoder *encoder, void *buffer,
