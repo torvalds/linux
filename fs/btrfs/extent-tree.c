f6acfd50110b335c7af636cf1fc8e55319cae5fc
Josef Bacik <jbacik@fb.com>
2014-09-18 11:27:17 -0400

    Btrfs: don't do async reclaim during log replay
    Trying to reproduce a log enospc bug I hit a panic in the async reclaim code
    during log replay.  This is because we use fs_info->fs_root as our root for
    shrinking and such.  Technically we can use whatever root we want, but let's
    just not allow async reclaim while we're doing log replay.  Thanks,
    
    Signed-off-by: Josef Bacik <jbacik@fb.com>
    Signed-off-by: Chris Mason <clm@fb.com>

--- fs/btrfs/extent-tree.c
+++ fs/btrfs/extent-tree.c
@@ -4513,7 +4513,13 @@ again:
 		space_info->flush = 1;
 	} else if (!ret && space_info->flags & BTRFS_BLOCK_GROUP_METADATA) {
 		used += orig_bytes;
-		if (need_do_async_reclaim(space_info, root->fs_info, used) &&
+		/*
+		 * We will do the space reservation dance during log replay,
+		 * which means we won't have fs_info->fs_root set, so don't do
+		 * the async reclaim as we will panic.
+		 */
+		if (!root->fs_info->log_root_recovering &&
+		    need_do_async_reclaim(space_info, root->fs_info, used) &&
 		    !work_busy(&root->fs_info->async_reclaim_work))
 			queue_work(system_unbound_wq,
 				   &root->fs_info->async_reclaim_work);
