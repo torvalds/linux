13402f7b76223e7f50ab42c82aac4788940c8277
Mahati Chamarthy <mahati.chamarthy@gmail.com>
2014-09-20 02:36:31 +0530

    Staging: rtl8192e: Fix else is not useful warning
    This fixes the following checkpatch.pl warning:
    WARNING: else is not generally useful after a break or return
    
    Signed-off-by: Mahati Chamarthy <mahati.chamarthy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl819x_TSProc.c
+++ drivers/staging/rtl8192e/rtl819x_TSProc.c
@@ -281,8 +281,7 @@ static struct ts_common_info *SearchAdmitTRStream(struct rtllib_device *ieee,
 
 	if (pRet && &pRet->List  != psearch_list)
 		return pRet ;
-	else
-		return NULL;
+	return NULL;
 }
 
 static void MakeTSEntry(struct ts_common_info *pTsCommonInfo, u8 *Addr,
