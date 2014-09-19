6c97c9c1acfce89cce2f239f0325786f95aea848
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2014-09-19 10:08:12 +0200

    i2c: cros-ec-tunnel: Add of match table
    To enable the cros-ec-tunnel driver to be auto-loaded when build as a
    module add an of match table (and export it) to match the modalias
    information passed on to userspace as the Cros EC MFD driver registers
    the MFD subdevices with an of_compatibility string.
    
    Signed-off-by: Sjoerd Simons <sjoerd.simons@collabora.co.uk>
    Reviewed-by: Javier Martinez Canillas <javier.martinez@collabora.co.uk>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/busses/i2c-cros-ec-tunnel.c
+++ drivers/i2c/busses/i2c-cros-ec-tunnel.c
@@ -310,11 +310,20 @@ static int ec_i2c_remove(struct platform_device *dev)
 	return 0;
 }
 
+#ifdef CONFIG_OF
+static const struct of_device_id cros_ec_i2c_of_match[] = {
+	{ .compatible = "google,cros-ec-i2c-tunnel" },
+	{},
+};
+MODULE_DEVICE_TABLE(of, cros_ec_i2c_of_match);
+#endif
+
 static struct platform_driver ec_i2c_tunnel_driver = {
 	.probe = ec_i2c_probe,
 	.remove = ec_i2c_remove,
 	.driver = {
 		.name = "cros-ec-i2c-tunnel",
+		.of_match_table = of_match_ptr(cros_ec_i2c_of_match),
 	},
 };
 
