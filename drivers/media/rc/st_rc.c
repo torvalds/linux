89d7ce5a0f159ffa99d0521b4336b922acdc7ac5
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2014-09-22 19:22:26 -0300

    [media] media: st-rc: move to using reset_control_get_optional
    This patch fixes a compilation error while building with the
    random kernel configuration.
    
    drivers/media/rc/st_rc.c: In function 'st_rc_probe':
    drivers/media/rc/st_rc.c:281:2: error: implicit declaration of
    function 'reset_control_get' [-Werror=implicit-function-declaration]
      rc_dev->rstc = reset_control_get(dev, NULL);
    
    drivers/media/rc/st_rc.c:281:15: warning: assignment makes pointer
    from integer without a cast [enabled by default]
      rc_dev->rstc = reset_control_get(dev, NULL);
    
    Reported-by: Jim Davis <jim.epost@gmail.com>
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Acked-by: Maxime Coquelin <maxime.coquelin@st.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/rc/st_rc.c
+++ drivers/media/rc/st_rc.c
@@ -278,7 +278,7 @@ static int st_rc_probe(struct platform_device *pdev)
 		rc_dev->rx_base = rc_dev->base;
 
 
-	rc_dev->rstc = reset_control_get(dev, NULL);
+	rc_dev->rstc = reset_control_get_optional(dev, NULL);
 	if (IS_ERR(rc_dev->rstc))
 		rc_dev->rstc = NULL;
 
