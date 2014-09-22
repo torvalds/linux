8aa6f0ebf41b5fdd186276394bf07e7bd6884d94
Kirill Tkhai <ktkhai@parallels.com>
2014-09-22 22:36:43 +0400

    sched/rt: Use resched_curr() in task_tick_rt()
    Some time ago PREEMPT_NEED_RESCHED was implemented,
    so reschedule technics is a little more difficult now.
    
    Signed-off-by: Kirill Tkhai <ktkhai@parallels.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Link: http://lkml.kernel.org/r/20140922183642.11015.66039.stgit@localhost
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -2072,7 +2072,7 @@ static void task_tick_rt(struct rq *rq, struct task_struct *p, int queued)
 	for_each_sched_rt_entity(rt_se) {
 		if (rt_se->run_list.prev != rt_se->run_list.next) {
 			requeue_task_rt(rq, p, 0);
-			set_tsk_need_resched(p);
+			resched_curr(rq);
 			return;
 		}
 	}
