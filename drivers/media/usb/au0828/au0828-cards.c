1ba3f927ad66759b0081fe1f96d77500c9a622c3
Mauro Carvalho Chehab <mchehab@osg.samsung.com>
2014-09-22 17:48:01 -0300

    [media] au0828-cards: remove a comment about i2c clock stretching
    This comment is already at the au0828-i2c where it belongs.
    So, remove it from a board's entry. It doesn't make any sense
    there, as we're setting the clock to 250kHz there, slowing it
    down only at the au0828-i2c.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/usb/au0828/au0828-cards.c
+++ drivers/media/usb/au0828/au0828-cards.c
@@ -73,12 +73,6 @@ struct au0828_board au0828_boards[] = {
 		.tuner_type = TUNER_XC5000,
 		.tuner_addr = 0x61,
 		.has_ir_i2c = 1,
-		/* The au0828 hardware i2c implementation does not properly
-		   support the xc5000's i2c clock stretching.  So we need to
-		   lower the clock frequency enough where the 15us clock
-		   stretch fits inside of a normal clock cycle, or else the
-		   au0828 fails to set the STOP bit.  A 30 KHz clock puts the
-		   clock pulse width at 18us */
 		.i2c_clk_divider = AU0828_I2C_CLK_250KHZ,
 		.input = {
 			{
