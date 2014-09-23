9fcf93b5de063e5cadb95a7bd0130bf73edcd3b5
Jason J. Herne <jjherne@linux.vnet.ibm.com>
2014-09-23 09:18:57 -0400

    KVM: S390: Create helper function get_guest_storage_key
    Define get_guest_storage_key which can be used to get the value of a guest
    storage key. This compliments the functionality provided by the helper function
    set_guest_storage_key. Both functions are needed for live migration of s390
    guests that use storage keys.
    
    Signed-off-by: Jason J. Herne <jjherne@linux.vnet.ibm.com>
    Reviewed-by: David Hildenbrand <dahi@linux.vnet.ibm.com>
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>

--- arch/s390/include/asm/pgalloc.h
+++ arch/s390/include/asm/pgalloc.h
@@ -26,6 +26,7 @@ void page_table_reset_pgste(struct mm_struct *, unsigned long, unsigned long,
 			    bool init_skey);
 int set_guest_storage_key(struct mm_struct *mm, unsigned long addr,
 			  unsigned long key, bool nq);
+unsigned long get_guest_storage_key(struct mm_struct *mm, unsigned long addr);
 
 static inline void clear_table(unsigned long *s, unsigned long val, size_t n)
 {
