2a52f6e49e5e400ed98a79503193d81207009647
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2014-09-23 17:18:15 +0300

    dmaengine: dw: export probe()/remove() and Co to users
    The driver library functions can be used directly by the compound devices such
    as ADSP or serial driver where DesignWare DMA IP is privately attached to the
    main hardware.
    
    Instead of creating a new platform device leaf they may call dw_dma_probe()
    with given struct dw_dma_chip directly and make sure that the main device is
    DMA capable.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/dw/internal.h
+++ drivers/dma/dw/internal.h
@@ -8,39 +8,16 @@
  * published by the Free Software Foundation.
  */
 
-#ifndef _DW_DMAC_INTERNAL_H
-#define _DW_DMAC_INTERNAL_H
-
-#include <linux/device.h>
+#ifndef _DMA_DW_INTERNAL_H
+#define _DMA_DW_INTERNAL_H
 
 #include <linux/dma/dw.h>
-#include <linux/platform_data/dma-dw.h>
 
 #include "regs.h"
-
-/**
- * struct dw_dma_chip - representation of DesignWare DMA controller hardware
- * @dev:		struct device of the DMA controller
- * @irq:		irq line
- * @regs:		memory mapped I/O space
- * @clk:		hclk clock
- * @dw:			struct dw_dma that is filed by dw_dma_probe()
- */
-struct dw_dma_chip {
-	struct device	*dev;
-	int		irq;
-	void __iomem	*regs;
-	struct clk	*clk;
-	struct dw_dma	*dw;
-};
-
-/* Export to the platform drivers */
-int dw_dma_probe(struct dw_dma_chip *chip, struct dw_dma_platform_data *pdata);
-int dw_dma_remove(struct dw_dma_chip *chip);
 
 int dw_dma_disable(struct dw_dma_chip *chip);
 int dw_dma_enable(struct dw_dma_chip *chip);
 
 extern bool dw_dma_filter(struct dma_chan *chan, void *param);
 
-#endif /* _DW_DMAC_INTERNAL_H */
+#endif /* _DMA_DW_INTERNAL_H */
--- include/linux/dma/dw.h
+++ include/linux/dma/dw.h
@@ -3,6 +3,7 @@
  *
  * Copyright (C) 2007 Atmel Corporation
  * Copyright (C) 2010-2011 ST Microelectronics
+ * Copyright (C) 2014 Intel Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -11,7 +12,33 @@
 #ifndef _DMA_DW_H
 #define _DMA_DW_H
 
+#include <linux/clk.h>
+#include <linux/device.h>
 #include <linux/dmaengine.h>
+
+#include <linux/platform_data/dma-dw.h>
+
+struct dw_dma;
+
+/**
+ * struct dw_dma_chip - representation of DesignWare DMA controller hardware
+ * @dev:		struct device of the DMA controller
+ * @irq:		irq line
+ * @regs:		memory mapped I/O space
+ * @clk:		hclk clock
+ * @dw:			struct dw_dma that is filed by dw_dma_probe()
+ */
+struct dw_dma_chip {
+	struct device	*dev;
+	int		irq;
+	void __iomem	*regs;
+	struct clk	*clk;
+	struct dw_dma	*dw;
+};
+
+/* Export to the platform drivers */
+int dw_dma_probe(struct dw_dma_chip *chip, struct dw_dma_platform_data *pdata);
+int dw_dma_remove(struct dw_dma_chip *chip);
 
 /* DMA API extensions */
 struct dw_desc;
