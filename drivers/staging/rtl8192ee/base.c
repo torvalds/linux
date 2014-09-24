18eaddefd4d156291b5a3dcc336778b67d98b531
Heena Sirwani <heenasirwani@gmail.com>
2014-09-24 12:07:16 +0530

    Staging: rtl8192ee: Removed unnecessary return statement.
    The following patch removes the checkpatch.pl warning:
    WARNING: void function return statements are generally not useful.
    
    Signed-off-by: Heena Sirwani <heenasirwani@gmail.com>
    Acked-by: Daniel Baluta <daniel.baluta@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192ee/base.c
+++ drivers/staging/rtl8192ee/base.c
@@ -622,8 +622,6 @@ static void _rtl_qurey_shortpreamble_mode(struct ieee80211_hw *hw,
 		return;
 	else if (rate_flag & IEEE80211_TX_RC_USE_SHORT_PREAMBLE)
 		tcb_desc->use_shortpreamble = true;
-
-	return;
 }
 
 static void _rtl_query_shortgi(struct ieee80211_hw *hw,
