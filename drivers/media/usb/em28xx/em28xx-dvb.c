1b07a77f6e1ebaebe7601fa734ff415fdf568ebb
Mauro Carvalho Chehab <mchehab@osg.samsung.com>
2014-09-28 23:23:20 -0300

    [media] em28xx-dvb: remove unused mfe_sharing
    This field is not used on this driver anymore. Remove it.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/usb/em28xx/em28xx-dvb.c
+++ drivers/media/usb/em28xx/em28xx-dvb.c
@@ -1047,7 +1047,7 @@ static void em28xx_unregister_dvb(struct em28xx_dvb *dvb)
 
 static int em28xx_dvb_init(struct em28xx *dev)
 {
-	int result = 0, mfe_shared = 0;
+	int result = 0;
 	struct em28xx_dvb *dvb;
 
 	if (dev->is_audio_only) {
@@ -1623,9 +1623,6 @@ static int em28xx_dvb_init(struct em28xx *dev)
 
 	if (result < 0)
 		goto out_free;
-
-	/* MFE lock */
-	dvb->adapter.mfe_shared = mfe_shared;
 
 	em28xx_info("DVB extension successfully initialized\n");
 
