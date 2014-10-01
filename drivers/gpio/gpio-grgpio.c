65fdc966c012b62ce4da5bf8175140a9b2ec2ee7
Pramod Gurav <pramod.gurav@smartplayin.com>
2014-10-02 00:08:04 +0530

    gpio: grgpio: remove irq_domain resources on failure
    Call irq_domain_remove when gpiochip_add fails to release irq_domain
    resources.
    
    Signed-off-by: Pramod Gurav <pramod.gurav@smartplayin.com>
    Acked-by: Alexandre Courbot <acourbot@nvidia.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-grgpio.c
+++ drivers/gpio/gpio-grgpio.c
@@ -441,6 +441,7 @@ static int grgpio_probe(struct platform_device *ofdev)
 	err = gpiochip_add(gc);
 	if (err) {
 		dev_err(&ofdev->dev, "Could not add gpiochip\n");
+		irq_domain_remove(priv->domain);
 		return err;
 	}
 
