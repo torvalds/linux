1b1bc49c0fc0501bf0d1366a2a5e5c1f8dcf9cb1
Pablo Neira Ayuso <pablo@netfilter.org>
2014-10-01 13:53:20 +0200

    netfilter: nf_tables: wait for call_rcu completion on module removal
    Make sure the objects have been released before the nf_tables modules
    is removed.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -4163,6 +4163,7 @@ static void __exit nf_tables_module_exit(void)
 {
 	unregister_pernet_subsys(&nf_tables_net_ops);
 	nfnetlink_subsys_unregister(&nf_tables_subsys);
+	rcu_barrier();
 	nf_tables_core_module_exit();
 	kfree(info);
 }
