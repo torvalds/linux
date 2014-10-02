bc394a39cbbbce3e9672303d5c5fc3d7c7b93f19
Sarah Khan <sarahjmi07@gmail.com>
2014-10-03 01:11:35 +0530

    staging: rtl8712: Fixed else not required after return
    WARNING: Else generally not required after return
    checkpatch.pl warning in hal_init.c
    Fixed by removing else
    
    Signed-off-by: Sarah Khan <sarahjmi07@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8712/hal_init.c
+++ drivers/staging/rtl8712/hal_init.c
@@ -389,10 +389,8 @@ uint rtl871x_hal_init(struct _adapter *padapter)
 	padapter->hw_init_completed = false;
 	if (padapter->halpriv.hal_bus_init == NULL)
 		return _FAIL;
-	else {
-		if (padapter->halpriv.hal_bus_init(padapter) != _SUCCESS)
-			return _FAIL;
-	}
+	if (padapter->halpriv.hal_bus_init(padapter) != _SUCCESS)
+		return _FAIL;
 	if (rtl8712_hal_init(padapter) == _SUCCESS)
 		padapter->hw_init_completed = true;
 	else {
