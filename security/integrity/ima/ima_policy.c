272a6e90ffee1dea39efd6fdf9592edc83a0738e
Dmitry Kasatkin <d.kasatkin@samsung.com>
2014-10-03 14:40:19 +0300

    ima: no need to allocate entry for comment
    If a rule is a comment, there is no need to allocate an entry.
    Move the checking for comments before allocating the entry.
    
    Signed-off-by: Dmitry Kasatkin <d.kasatkin@samsung.com>
    Signed-off-by: Mimi Zohar <zohar@linux.vnet.ibm.com>

--- security/integrity/ima/ima_policy.c
+++ security/integrity/ima/ima_policy.c
@@ -694,6 +694,12 @@ ssize_t ima_parse_add_rule(char *rule)
 		return -EACCES;
 	}
 
+	p = strsep(&rule, "\n");
+	len = strlen(p) + 1;
+
+	if (*p == '#')
+		return len;
+
 	entry = kzalloc(sizeof(*entry), GFP_KERNEL);
 	if (!entry) {
 		integrity_audit_msg(AUDIT_INTEGRITY_STATUS, NULL,
@@ -702,14 +708,6 @@ ssize_t ima_parse_add_rule(char *rule)
 	}
 
 	INIT_LIST_HEAD(&entry->list);
-
-	p = strsep(&rule, "\n");
-	len = strlen(p) + 1;
-
-	if (*p == '#') {
-		kfree(entry);
-		return len;
-	}
 
 	result = ima_parse_rule(p, entry);
 	if (result) {
