51db998fb6e04e8dfcf4e33295651367b91238a5
Tilman Schmidt <tilman@imap.cc>
2014-10-03 17:03:32 +0200

    isdn/capi: drop two dead if branches
    The last branch in command_2_index() cannot be reached since
    c==0xff is already caught by the first "if".
    The empty second branch makes no difference since no other branch
    will be taken for c<0x0f.
    
    Signed-off-by: Tilman Schmidt <tilman@imap.cc>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/isdn/capi/capiutil.c
+++ drivers/isdn/capi/capiutil.c
@@ -205,11 +205,8 @@ static unsigned command_2_index(unsigned c, unsigned sc)
 {
 	if (c & 0x80)
 		c = 0x9 + (c & 0x0f);
-	else if (c <= 0x0f);
 	else if (c == 0x41)
 		c = 0x9 + 0x1;
-	else if (c == 0xff)
-		c = 0x00;
 	return (sc & 3) * (0x9 + 0x9) + c;
 }
 
