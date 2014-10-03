2dd3887b503c1cc8a61ef4a4f24462ce03f3ada5
Tomi Valkeinen <tomi.valkeinen@ti.com>
2014-10-03 15:14:08 +0000

    drm/omap: fix plane's channel selection
    omap_plane_pre_apply() sets the plane's output channel too late, only
    after the plane has already been otherwise configured and enabled. This
    causes problems, as at the configuration stage we need to make decisions
    based on the output channel.
    
    This may lead to bad plane settings or failing to setup the plane.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/omapdrm/omap_plane.c
+++ drivers/gpu/drm/omapdrm/omap_plane.c
@@ -149,6 +149,8 @@ static void omap_plane_pre_apply(struct omap_drm_apply *apply)
 	DBG("%d,%d %pad %pad", info->pos_x, info->pos_y,
 			&info->paddr, &info->p_uv_addr);
 
+	dispc_ovl_set_channel_out(omap_plane->id, channel);
+
 	/* and finally, update omapdss: */
 	ret = dispc_ovl_setup(omap_plane->id, info, false,
 			      omap_crtc_timings(crtc), false);
@@ -158,7 +160,6 @@ static void omap_plane_pre_apply(struct omap_drm_apply *apply)
 	}
 
 	dispc_ovl_enable(omap_plane->id, true);
-	dispc_ovl_set_channel_out(omap_plane->id, channel);
 }
 
 static void omap_plane_post_apply(struct omap_drm_apply *apply)
