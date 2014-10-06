33fb0d01ce60fe4c0c12c4f0c134c5cdb818ac5a
Alexander Gordeev <agordeev@redhat.com>
2014-10-06 16:26:35 +0100

    AHCI: Do not acquire ata_host::lock from single IRQ handler
    There is no need to acquire ata_host::lock spinlock from
    hardware context single IRQ interrupt handler since the
    handler does not access host data that could be altered
    by concurrent processors.
    
    Signed-off-by: Alexander Gordeev <agordeev@redhat.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: linux-ide@vger.kernel.org

--- drivers/ata/libahci.c
+++ drivers/ata/libahci.c
@@ -1875,8 +1875,6 @@ static irqreturn_t ahci_single_irq_intr(int irq, void *dev_instance)
 
 	irq_masked = irq_stat & hpriv->port_map;
 
-	spin_lock(&host->lock);
-
 	for (i = 0; i < host->n_ports; i++) {
 		struct ata_port *ap;
 
@@ -1907,8 +1905,6 @@ static irqreturn_t ahci_single_irq_intr(int irq, void *dev_instance)
 	 * pending event on a dummy port might cause screaming IRQ.
 	 */
 	writel(irq_stat, mmio + HOST_IRQ_STAT);
-
-	spin_unlock(&host->lock);
 
 	VPRINTK("EXIT\n");
 
