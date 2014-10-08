60b388befb42d1e90a8594cdcfe80dbf57542ac0
Dan Carpenter <dan.carpenter@oracle.com>
2014-10-08 13:40:37 +0300

    usb: gadget: f_uac1: remove an unneeded NULL check
    This NULL check sets off a static checker warning because we already
    dereferenced "card" earlier in the function.  However, since "card" is
    never NULL so we can just remove the check.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/function/u_uac1.c
+++ drivers/usb/gadget/function/u_uac1.c
@@ -213,9 +213,6 @@ static int gaudio_open_snd_dev(struct gaudio *card)
 	fn_cap = opts->fn_cap;
 	fn_cntl = opts->fn_cntl;
 
-	if (!card)
-		return -ENODEV;
-
 	/* Open control device */
 	snd = &card->control;
 	snd->filp = filp_open(fn_cntl, O_RDWR, 0);
