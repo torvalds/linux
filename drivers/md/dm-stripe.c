a3f2af2547884e02f7e43f995a6c442a4e54f1ea
Pavitra Kumar <pavitrak@nvidia.com>
2014-10-10 15:19:46 +0000

    dm stripe: fix potential for leak in stripe_ctr error path
    Fix a potential struct stripe_c leak that would occur if the
    chunk_size exceeded the maximum allowed by dm_set_target_max_io_len
    (UINT_MAX).  However, in practice there is no possibility of this
    occuring given that chunk_size is of type uint32_t.  But it is good to
    fix this to future-proof in case dm_set_target_max_io_len's
    implementation were to change.
    
    Signed-off-by: Pavitra Kumar <pavitrak@nvidia.com>
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>

--- drivers/md/dm-stripe.c
+++ drivers/md/dm-stripe.c
@@ -159,8 +159,10 @@ static int stripe_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 		sc->stripes_shift = __ffs(stripes);
 
 	r = dm_set_target_max_io_len(ti, chunk_size);
-	if (r)
+	if (r) {
+		kfree(sc);
 		return r;
+	}
 
 	ti->num_flush_bios = stripes;
 	ti->num_discard_bios = stripes;
