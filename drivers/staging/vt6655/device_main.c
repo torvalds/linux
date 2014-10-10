4a2bc3bdfffcb31e7ba4d47358c0193d4fc259dc
Vladimir A. Nazarenko <naszar@ya.ru>
2014-10-10 12:27:01 +1100

    staging: vt6655: delete SndEvt_ToAPI code
    It's never enabled, so we can safely remove it.
    
    Signed-off-by: Vladimir A. Nazarenko <naszar@ya.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/device_main.c
+++ drivers/staging/vt6655/device_main.c
@@ -2911,14 +2911,8 @@ static int  device_ioctl(struct net_device *dev, struct ifreq *rq, int cmd)
 
 	case IOCTL_CMD_SET:
 
-#ifdef SndEvt_ToAPI
-		if ((((PSCmdRequest)rq)->wCmdCode != WLAN_CMD_SET_EVT) &&
-		    !(pDevice->flags & DEVICE_FLAGS_OPENED))
-#else
-			if (!(pDevice->flags & DEVICE_FLAGS_OPENED) &&
-			    (((PSCmdRequest)rq)->wCmdCode != WLAN_CMD_SET_WPA))
-#endif
-			{
+		if (!(pDevice->flags & DEVICE_FLAGS_OPENED) &&
+		    (((PSCmdRequest)rq)->wCmdCode != WLAN_CMD_SET_WPA))	{
 				rc = -EFAULT;
 				break;
 			}
