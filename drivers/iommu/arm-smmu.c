0029a8dd6c807e04b119ec9416510d30936f45e6
Antonios Motakis <a.motakis@virtualopensystems.com>
2014-10-13 14:06:18 +0100

    iommu/arm-smmu: add IOMMU_CAP_NOEXEC to the ARM SMMU driver
    The ARM SMMU supports the IOMMU_NOEXEC protection flag. Add the
    corresponding IOMMU capability.
    
    Signed-off-by: Antonios Motakis <a.motakis@virtualopensystems.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -1569,6 +1569,8 @@ static bool arm_smmu_capable(enum iommu_cap cap)
 		return true;
 	case IOMMU_CAP_INTR_REMAP:
 		return true; /* MSIs are just memory writes */
+	case IOMMU_CAP_NOEXEC:
+		return true;
 	default:
 		return false;
 	}
