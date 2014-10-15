d09e9b160fc18116942101743693f5535bc5136a
Jeff Kirsher <jeffrey.t.kirsher@intel.com>
2014-10-15 15:53:27 -0700

    staging: bcm: remove driver
    The Beceem WiMAX driver was barely function in its current state
    and was non-functional on 64 bit systems.  Based on repeated
    statements from Greg KH that he wanted the driver removed, I am
    removing the driver.
    
    CC: Matthias Beyer <mail@beyermatthias.de>
    CC: Kevin McKinney <klmckinney1@gmail.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/DDRInit.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DDR_INIT_H_
-#define _DDR_INIT_H_
-
-
-
-int ddr_init(struct bcm_mini_adapter *psAdapter);
-int download_ddr_settings(struct bcm_mini_adapter *psAdapter);
-
-#endif
--- drivers/staging/bcm/InterfaceTx.h
+++
@@ -1,7 +0,0 @@
-#ifndef _INTERFACE_TX_H
-#define _INTERFACE_TX_H
-
-INT InterfaceTransmitPacket(PVOID arg, PVOID data, UINT len);
-
-#endif
-
