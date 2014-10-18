4006e565e1500db40b8546dcc6011737bc5d986c
Daniel Glöckner <dg@emlix.com>
2014-10-19 00:46:25 +0200

    xtensa: remove s6000 variant and s6105 platform
    The Stretch s6000 family support has been merged into mainline 5 years
    ago. There appear to be no users of this code since nobody complained
    that there is a merge error preventing compilation.
    
    Apart from the s6105 IP camera reference design there are no s6000 devices
    known to ever have run Linux and as the chips are out of production there
    probably never will be. The successor s7000 no longer uses an Xtensa core
    for the OS.
    
    Let's remove the code until someone is found who actually needs it.
    
    Signed-off-by: Daniel Glöckner <dg@emlix.com>
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- arch/xtensa/platforms/s6105/include/platform/serial.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ASM_XTENSA_S6105_SERIAL_H
-#define __ASM_XTENSA_S6105_SERIAL_H
-
-#include <variant/hardware.h>
-
-#define BASE_BAUD (S6_SCLK / 16)
-
-#endif /* __ASM_XTENSA_S6105_SERIAL_H */
