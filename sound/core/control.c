df803e1389716bcdf11932fff47d7f1fc198bc8a
Takashi Iwai <tiwai@suse.de>
2014-10-20 18:07:21 +0200

    ALSA: control: Warn if too long string is passed to snd_ctl_enum_info()
    This allows us to catch the bugs in drivers easily.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/control.c
+++ sound/core/control.c
@@ -1747,6 +1747,9 @@ int snd_ctl_enum_info(struct snd_ctl_elem_info *info, unsigned int channels,
 	info->value.enumerated.items = items;
 	if (info->value.enumerated.item >= items)
 		info->value.enumerated.item = items - 1;
+	WARN(strlen(names[info->value.enumerated.item]) >= sizeof(info->value.enumerated.name),
+	     "ALSA: too long item name '%s'\n",
+	     names[info->value.enumerated.item]);
 	strlcpy(info->value.enumerated.name,
 		names[info->value.enumerated.item],
 		sizeof(info->value.enumerated.name));
