2c1bfc3be0fc525d95ff5983bf8ea3f2aff24d1b
Tapasweni Pathak <tapaswenipathak@gmail.com>
2014-10-20 16:58:10 +0530

    staging: media: lirc: Delete unused function tv2int
    The function tv2int is used to convert timeval to int.
    
    32-bit systems using 'struct timeval' will break in the year 2038,
    so we have to replace that code with more appropriate types.
    This patch changes the lirc_imon.c file of media: lirc driver.
    
    tv2int function is static and not used anywhere in this file too,
    so it is removed. I also build tested it.
    
    Signed-off-by: Tapasweni Pathak <tapaswenipathak@gmail.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/media/lirc/lirc_imon.c
+++ drivers/staging/media/lirc/lirc_imon.c
@@ -572,29 +572,6 @@ static void submit_data(struct imon_context *context)
 	wake_up(&context->driver->rbuf->wait_poll);
 }
 
-static inline int tv2int(const struct timeval *a, const struct timeval *b)
-{
-	int usecs = 0;
-	int sec   = 0;
-
-	if (b->tv_usec > a->tv_usec) {
-		usecs = 1000000;
-		sec--;
-	}
-
-	usecs += a->tv_usec - b->tv_usec;
-
-	sec += a->tv_sec - b->tv_sec;
-	sec *= 1000;
-	usecs /= 1000;
-	sec += usecs;
-
-	if (sec < 0)
-		sec = 1000;
-
-	return sec;
-}
-
 /**
  * Process the incoming packet
  */
