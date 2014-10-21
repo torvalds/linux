cc9571e85808dfc121e4fda5c75c9a3c3c75e514
Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
2014-10-21 11:22:35 +0200

    mmc: sdhci-pxav3: Move private driver data to driver source
    struct sdhci_pxa is only used in sdhci_pxa driver itself, so move it
    there.
    
    Signed-off-by: Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/sdhci-pxav3.c
+++ drivers/mmc/host/sdhci-pxav3.c
@@ -58,6 +58,11 @@
 #define SDCE_MISC_INT		(1<<2)
 #define SDCE_MISC_INT_EN	(1<<1)
 
+struct sdhci_pxa {
+	u8	clk_enable;
+	u8	power_mode;
+};
+
 /*
  * These registers are relative to the second register region, for the
  * MBus bridge.
