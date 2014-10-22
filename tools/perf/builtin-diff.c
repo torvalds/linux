9ab1f50876dbb8b962db058259be7aae920d4c25
Kan Liang <kan.liang@intel.com>
2014-10-22 15:02:41 -0400

    perf diff: Add missing hists__init() call at tool start
    It also uses hists/hist_entries, hists__init() should be called before
    creating any evsels.
    
    Otherwise no extra space will be allocated per perf_evsel nor this space
    will be initialized when allocating a new perf_evsel instance, resulting
    in reads/writes to non allocated space, oops. Fix it.
    
    Signed-off-by: Kan Liang <kan.liang@intel.com>
    Link: http://lkml.kernel.org/r/1414004561-22096-1-git-send-email-kan.liang@intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/builtin-diff.c
+++ tools/perf/builtin-diff.c
@@ -1142,6 +1142,11 @@ static int data_init(int argc, const char **argv)
 
 int cmd_diff(int argc, const char **argv, const char *prefix __maybe_unused)
 {
+	int ret = hists__init();
+
+	if (ret < 0)
+		return ret;
+
 	perf_config(perf_default_config, NULL);
 
 	argc = parse_options(argc, argv, options, diff_usage, 0);
