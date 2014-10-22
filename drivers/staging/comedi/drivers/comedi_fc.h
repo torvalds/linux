ce8bfa275a3c9fabd3bd670ea0da0b2d1d4ba8d2
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-10-22 15:36:25 -0700

    staging: comedi: comedi_fc.h: remove cfc_write_long_to_buffer()
    The inline function is not used. Remove it.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/comedi_fc.h
+++ drivers/staging/comedi/drivers/comedi_fc.h
@@ -36,12 +36,6 @@ static inline unsigned int cfc_write_to_buffer(struct comedi_subdevice *s,
 	return comedi_write_array_to_buffer(s, &data, sizeof(data));
 };
 
-static inline unsigned int cfc_write_long_to_buffer(struct comedi_subdevice *s,
-						    unsigned int data)
-{
-	return comedi_write_array_to_buffer(s, &data, sizeof(data));
-};
-
 /**
  * cfc_check_trigger_src() - trivially validate a comedi_cmd trigger source
  * @src: pointer to the trigger source to validate
