191db0902606214aed1a90396698c28044d0a753
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-10-22 15:36:57 -0700

    staging: comedi: ni_6527: use comedi_buf_write_samples()
    Use comedi_buf_write_samples() to add the sample to the async buffer.
    
    The async events COMEDI_CB_EOS will automatically be set by the core
    with the end-of-scan is detected. Remove the unnecessary event in the
    driver.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_6527.c
+++ drivers/staging/comedi/drivers/ni_6527.c
@@ -208,8 +208,7 @@ static irqreturn_t ni6527_interrupt(int irq, void *d)
 		return IRQ_NONE;
 
 	if (status & NI6527_STATUS_EDGE) {
-		comedi_buf_put(s, 0);
-		s->async->events |= COMEDI_CB_EOS;
+		comedi_buf_write_samples(s, &s->state, 1);
 		comedi_handle_events(dev, s);
 	}
 
