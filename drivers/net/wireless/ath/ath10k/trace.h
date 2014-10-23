b04e204fcae5a2c1e99e94f7e80f1e75fa95b938
Michal Kazior <michal.kazior@tieto.com>
2014-10-23 17:04:27 +0300

    ath10k: remove tsf argument from rx_desc tracing
    Fundamentally this was wrong. Tsf is only valid
    in last MPDU of a PPDU. This means tsf value was
    wrong most of the time during heavy traffic.
    
    Also I don't see much point in exposing a
    redundant (and broken) tsf value. Userspace can
    already read it from the dumped rx descriptor
    buffer.
    
    Cc: Rajkumar Manoharan <rmanohar@qti.qualcomm.com>
    Signed-off-by: Michal Kazior <michal.kazior@tieto.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath10k/trace.h
+++ drivers/net/wireless/ath/ath10k/trace.h
@@ -282,14 +282,13 @@ TRACE_EVENT(ath10k_htt_pktlog,
 );
 
 TRACE_EVENT(ath10k_htt_rx_desc,
-	    TP_PROTO(struct ath10k *ar, u32 tsf, void *rxdesc, u16 len),
+	    TP_PROTO(struct ath10k *ar, void *rxdesc, u16 len),
 
-	TP_ARGS(ar, tsf, rxdesc, len),
+	TP_ARGS(ar, rxdesc, len),
 
 	TP_STRUCT__entry(
 		__string(device, dev_name(ar->dev))
 		__string(driver, dev_driver_string(ar->dev))
-		__field(u32, tsf)
 		__field(u16, len)
 		__dynamic_array(u8, rxdesc, len)
 	),
@@ -297,16 +296,14 @@ TRACE_EVENT(ath10k_htt_rx_desc,
 	TP_fast_assign(
 		__assign_str(device, dev_name(ar->dev));
 		__assign_str(driver, dev_driver_string(ar->dev));
-		__entry->tsf = tsf;
 		__entry->len = len;
 		memcpy(__get_dynamic_array(rxdesc), rxdesc, len);
 	),
 
 	TP_printk(
-		"%s %s %u len %hu",
+		"%s %s len %hu",
 		__get_str(driver),
 		__get_str(device),
-		__entry->tsf,
 		__entry->len
 	 )
 );
