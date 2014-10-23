1f7732fe6cc0c37befc74cef1d289cd2272b7a5c
Jan Kara <jack@suse.cz>
2014-10-23 14:40:20 +0200

    f2fs: remove pointless bit testing in f2fs_delete_entry()
    There's no point in using test_and_clear_bit_le() when we don't use the
    return value of the function. Just use clear_bit_le() instead.
    
    Coverity-id: 1016434
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/dir.c
+++ fs/f2fs/dir.c
@@ -654,7 +654,7 @@ void f2fs_delete_entry(struct f2fs_dir_entry *dentry, struct page *page,
 	dentry_blk = page_address(page);
 	bit_pos = dentry - dentry_blk->dentry;
 	for (i = 0; i < slots; i++)
-		test_and_clear_bit_le(bit_pos + i, &dentry_blk->dentry_bitmap);
+		clear_bit_le(bit_pos + i, &dentry_blk->dentry_bitmap);
 
 	/* Let's check and deallocate this dentry page */
 	bit_pos = find_next_bit_le(&dentry_blk->dentry_bitmap,
