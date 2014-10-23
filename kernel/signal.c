eca1a08986f622c11b75b3b44d561a1f901c9cec
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2014-10-23 11:41:22 -0700

    signal: Exit RCU read-side critical section on each pass through loop
    The kill_pid_info() can potentially loop indefinitely if tasks are created
    and deleted sufficiently quickly, and if this happens, this function
    will remain in a single RCU read-side critical section indefinitely.
    This commit therefore exits the RCU read-side critical section on each
    pass through the loop.  Because a race must happen to retry the loop,
    this should have no performance impact in the common case.
    
    Reported-by: Dave Jones <davej@redhat.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Acked-by: Oleg Nesterov <oleg@redhat.com>
    Reviewed-by: Pranith Kumar <bobby.prani@gmail.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -1331,23 +1331,21 @@ int kill_pid_info(int sig, struct siginfo *info, struct pid *pid)
 	int error = -ESRCH;
 	struct task_struct *p;
 
-	rcu_read_lock();
-retry:
-	p = pid_task(pid, PIDTYPE_PID);
-	if (p) {
-		error = group_send_sig_info(sig, info, p);
-		if (unlikely(error == -ESRCH))
-			/*
-			 * The task was unhashed in between, try again.
-			 * If it is dead, pid_task() will return NULL,
-			 * if we race with de_thread() it will find the
-			 * new leader.
-			 */
-			goto retry;
-	}
-	rcu_read_unlock();
+	for (;;) {
+		rcu_read_lock();
+		p = pid_task(pid, PIDTYPE_PID);
+		if (p)
+			error = group_send_sig_info(sig, info, p);
+		rcu_read_unlock();
+		if (likely(!p || error != -ESRCH))
+			return error;
 
-	return error;
+		/*
+		 * The task was unhashed in between, try again.  If it
+		 * is dead, pid_task() will return NULL, if we race with
+		 * de_thread() it will find the new leader.
+		 */
+	}
 }
 
 int kill_proc_info(int sig, struct siginfo *info, pid_t pid)
