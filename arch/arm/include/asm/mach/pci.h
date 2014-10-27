6cf00af0ae158120e8149ed3d77746397b3f911f
Yijing Wang <wangyijing@huawei.com>
2014-10-27 15:48:46 +0800

    ARM/PCI: Remove unused pcibios_add_bus() and pcibios_remove_bus()
    There are no users of the struct hw_pci.add_bus() or .remove_bus() methods,
    so remove the pointers from hw_pci.  That makes pcibios_add_bus() and
    pcibios_remove_bus() themselves superfluous, so remove them as well.
    
    [bhelgaas: changelog]
    Signed-off-by: Yijing Wang <wangyijing@huawei.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- arch/arm/include/asm/mach/pci.h
+++ arch/arm/include/asm/mach/pci.h
@@ -39,8 +39,6 @@ struct hw_pci {
 					  resource_size_t start,
 					  resource_size_t size,
 					  resource_size_t align);
-	void		(*add_bus)(struct pci_bus *bus);
-	void		(*remove_bus)(struct pci_bus *bus);
 };
 
 /*
@@ -71,8 +69,6 @@ struct pci_sys_data {
 					  resource_size_t start,
 					  resource_size_t size,
 					  resource_size_t align);
-	void		(*add_bus)(struct pci_bus *bus);
-	void		(*remove_bus)(struct pci_bus *bus);
 	void		*private_data;	/* platform controller private data	*/
 };
 
