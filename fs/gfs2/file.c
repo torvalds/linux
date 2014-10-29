33ad5d54284adf110f6e78aa9c4f42d3d17d7f68
Bob Peterson <rpeterso@redhat.com>
2014-10-29 08:02:29 -0500

    GFS2: Only increase rs_sizehint
    If an application does a sequence of (1) big write, (2) little write
    we don't necessarily want to reset the size hint based on the smaller
    size. The fact that they did any big writes implies they may do more,
    and therefore we should try to allocate bigger block reservations, even
    if the last few were small writes. Therefore this patch changes function
    gfs2_size_hint so that the size hint can only grow; it cannot shrink.
    This is especially important where there are multiple writers.
    
    Signed-off-by: Bob Peterson <rpeterso@redhat.com>
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/file.c
+++ fs/gfs2/file.c
@@ -337,7 +337,8 @@ static void gfs2_size_hint(struct file *filep, loff_t offset, size_t size)
 	size_t blks = (size + sdp->sd_sb.sb_bsize - 1) >> sdp->sd_sb.sb_bsize_shift;
 	int hint = min_t(size_t, INT_MAX, blks);
 
-	atomic_set(&ip->i_res->rs_sizehint, hint);
+	if (hint > atomic_read(&ip->i_res->rs_sizehint))
+		atomic_set(&ip->i_res->rs_sizehint, hint);
 }
 
 /**
