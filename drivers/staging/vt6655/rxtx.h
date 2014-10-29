1903e6dd0552d15a39664d71e4ebb9f0769969cc
Malcolm Priestley <tvboxspy@gmail.com>
2014-10-29 17:56:03 +0000

    staging: vt6655: Move LIFETIME RES 64us macros to rxtx.h
    Used only by vnt_generate_fifo_header move these macros.
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/rxtx.h
+++ drivers/staging/vt6655/rxtx.h
@@ -32,6 +32,10 @@
 #include "ttype.h"
 #include "device.h"
 
+#define DEFAULT_MSDU_LIFETIME_RES_64us	8000 /* 64us */
+#define DEFAULT_MGN_LIFETIME_RES_64us	125  /* 64us */
+
+
 /*---------------------  Export Definitions -------------------------*/
 
 /*---------------------  Export Variables  --------------------------*/
