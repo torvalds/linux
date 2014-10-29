2716777b4f21649fb907b4a4fb96e1c8d0a5ec16
Guenter Roeck <linux@roeck-us.net>
2014-10-29 10:44:57 -0700

    net: dsa/mv88e6352: Add support for MV88E6176
    MV88E6176 is mostly compatible to MV88E6352 and is documented
    in the same functional specification. Add support for it.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/mv88e6352.c
+++ drivers/net/dsa/mv88e6352.c
@@ -73,6 +73,8 @@ static char *mv88e6352_probe(struct device *host_dev, int sw_addr)
 
 	ret = __mv88e6xxx_reg_read(bus, sw_addr, REG_PORT(0), 0x03);
 	if (ret >= 0) {
+		if ((ret & 0xfff0) == 0x1760)
+			return "Marvell 88E6176";
 		if (ret == 0x3521)
 			return "Marvell 88E6352 (A0)";
 		if (ret == 0x3522)
