3b3f7517262ba59188cfbd23d0d0c20eea2c8c84
Behan Webster <behanw@converseincode.com>
2014-10-29 15:42:21 -0700

    staging, rtl8192e, LLVMLinux: Remove unused inline prototype
    commit 62ec95f86d2850b7ce6d73fb236a6fcf48411aea upstream.
    
    rtllib_probe_req is defined as "static inline" in rtllib_softmac.c however it
    is declared differently as "extern inline" in rtllib_softmac.h. Since it isn't
    used outside of the scope of rtllib_softmac, it makes sense to remove the
    incorrect declaration.
    
    Signed-off-by: Behan Webster <behanw@converseincode.com>
    Suggested-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtllib.h
+++ drivers/staging/rtl8192e/rtllib.h
@@ -2761,7 +2761,6 @@ extern void rtllib_stop_scan(struct rtllib_device *ieee);
 extern bool rtllib_act_scanning(struct rtllib_device *ieee, bool sync_scan);
 extern void rtllib_stop_scan_syncro(struct rtllib_device *ieee);
 extern void rtllib_start_scan_syncro(struct rtllib_device *ieee, u8 is_mesh);
-extern inline struct sk_buff *rtllib_probe_req(struct rtllib_device *ieee);
 extern u8 MgntQuery_MgntFrameTxRate(struct rtllib_device *ieee);
 extern void rtllib_sta_ps_send_null_frame(struct rtllib_device *ieee,
 					  short pwr);
