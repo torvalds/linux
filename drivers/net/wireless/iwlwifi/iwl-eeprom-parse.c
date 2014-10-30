5f0d98f278f943cb6115b6fe4441f11a7015bb50
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2014-10-30 10:19:38 +0200

    iwlwifi: mvm: forbid MIMO on devices that don't support it
    There are devices that forbid MIMO by the mean of the NVM.
    Detect thoses devices and forbid MIMO otherwise the firmware
    would crash. STBC is still allowed on these devices.
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-eeprom-parse.c
+++ drivers/net/wireless/iwlwifi/iwl-eeprom-parse.c
@@ -6,6 +6,7 @@
  * GPL LICENSE SUMMARY
  *
  * Copyright(c) 2008 - 2014 Intel Corporation. All rights reserved.
+ * Copyright(c) 2015 Intel Mobile Communications GmbH
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of version 2 of the GNU General Public License as
@@ -31,6 +32,7 @@
  * BSD LICENSE
  *
  * Copyright(c) 2005 - 2014 Intel Corporation. All rights reserved.
+ * Copyright(c) 2015 Intel Mobile Communications GmbH
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -747,6 +749,9 @@ void iwl_init_ht_hw_capab(const struct iwl_cfg *cfg,
 		ht_info->ht_supported = false;
 		return;
 	}
+
+	if (data->sku_cap_mimo_disabled)
+		rx_chains = 1;
 
 	ht_info->ht_supported = true;
 	ht_info->cap = IEEE80211_HT_CAP_DSSSCCK40;
