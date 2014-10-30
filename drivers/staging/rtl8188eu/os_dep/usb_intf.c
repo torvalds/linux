04947c5be5ca3824586f94352293549b899b2d89
Gulsah Kose <gulsah.1004@gmail.com>
2014-10-30 02:28:03 +0200

    staging: rtl8188eu: os_dep: Removed unnecessary return.
    This patch fixes
    WARNING:  void function return statements are not generally useful
    checkpatch.pl warning in usb_intf.c by using this coccinelle script
    
    @r@
    identifier i;
    @@
    void i(...)
    {
    ...
    -return;
    }
    
    Signed-off-by: Gulsah Kose <gulsah.1004@gmail.com>
    Reviewed-by: Daniel Baluta <daniel.baluta@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/os_dep/usb_intf.c
+++ drivers/staging/rtl8188eu/os_dep/usb_intf.c
@@ -556,8 +556,6 @@ static void rtw_dev_remove(struct usb_interface *pusb_intf)
 
 	RT_TRACE(_module_hci_intfs_c_, _drv_err_, ("-dev_remove()\n"));
 	DBG_88E("-r871xu_dev_remove, done\n");
-
-	return;
 }
 
 static struct usb_driver rtl8188e_usb_drv = {
