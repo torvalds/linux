24119a8829cdaf299294095ec568bf49e863c5a5
Hanjun Guo <hanjun.guo@linaro.org>
2014-10-30 17:52:58 +0800

    ACPI / processor: Update the comments in processor.h
    In commit 46ba51e (ACPI / processor: Introduce ARCH_MIGHT_HAVE_ACPI_PDC),
    acpi_processor_set_pdc() was moved to processor_pdc.c, so update
    the comments accordingly.
    
    Signed-off-by: Hanjun Guo <hanjun.guo@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/processor.h
+++ include/acpi/processor.h
@@ -313,10 +313,12 @@ static inline int acpi_processor_get_bios_limit(int cpu, unsigned int *limit)
 #endif				/* CONFIG_CPU_FREQ */
 
 /* in processor_core.c */
-void acpi_processor_set_pdc(acpi_handle handle);
 int acpi_get_apicid(acpi_handle, int type, u32 acpi_id);
 int acpi_map_cpuid(int apic_id, u32 acpi_id);
 int acpi_get_cpuid(acpi_handle, int type, u32 acpi_id);
+
+/* in processor_pdc.c */
+void acpi_processor_set_pdc(acpi_handle handle);
 
 /* in processor_throttling.c */
 int acpi_processor_tstate_has_changed(struct acpi_processor *pr);
