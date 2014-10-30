891310d53dcf396d6a92987da265a5ffe6c09d71
Michele Baldessari <michele@acksyn.org>
2014-10-30 10:29:15 +0100

    sctp: add transport state in /proc/net/sctp/remaddr
    It is often quite helpful to be able to know the state of a transport
    outside of the application itself (for troubleshooting purposes or for
    monitoring purposes). Add it under /proc/net/sctp/remaddr.
    
    Signed-off-by: Michele Baldessari <michele@acksyn.org>
    Acked-by: Neil Horman <nhorman@tuxdriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sctp/proc.c
+++ net/sctp/proc.c
@@ -417,7 +417,7 @@ static void *sctp_remaddr_seq_start(struct seq_file *seq, loff_t *pos)
 
 	if (*pos == 0)
 		seq_printf(seq, "ADDR ASSOC_ID HB_ACT RTO MAX_PATH_RTX "
-				"REM_ADDR_RTX  START\n");
+				"REM_ADDR_RTX START STATE\n");
 
 	return (void *)pos;
 }
@@ -497,7 +497,13 @@ static int sctp_remaddr_seq_show(struct seq_file *seq, void *v)
 			 * currently implemented, but we can record it with a
 			 * jiffies marker in a subsequent patch
 			 */
-			seq_printf(seq, "0");
+			seq_printf(seq, "0 ");
+
+			/*
+			 * The current state of this destination. I.e.
+			 * SCTP_ACTIVE, SCTP_INACTIVE, ...
+			 */
+			seq_printf(seq, "%d", tsp->state);
 
 			seq_printf(seq, "\n");
 		}
