2fd36c55264926e268deb50f6de5f43fa5e490f7
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2014-10-30 15:59:38 +0200

    i2c: core: Map OF IRQ at probe time
    I2C clients instantiated from OF get their IRQ mapped at device
    registration time. This leads to the IRQ being silently ignored if the
    related irqchip hasn't been proved yet.
    
    Fix this by moving IRQ mapping at probe time using of_get_irq(). The
    function operates as irq_of_parse_and_map() but additionally returns
    -EPROBE_DEFER if the irqchip isn't available, allowing us to defer I2C
    client probing.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/i2c-core.c
+++ drivers/i2c/i2c-core.c
@@ -626,6 +626,15 @@ static int i2c_device_probe(struct device *dev)
 	if (!client)
 		return 0;
 
+	if (!client->irq && dev->of_node) {
+		int irq = of_irq_get(dev->of_node, 0);
+
+		if (irq < 0)
+			return irq;
+
+		client->irq = irq;
+	}
+
 	driver = to_i2c_driver(dev->driver);
 	if (!driver->probe || !driver->id_table)
 		return -ENODEV;
@@ -1407,7 +1416,6 @@ static void of_i2c_register_devices(struct i2c_adapter *adap)
 			continue;
 		}
 
-		info.irq = irq_of_parse_and_map(node, 0);
 		info.of_node = of_node_get(node);
 		info.archdata = &dev_ad;
 
@@ -1421,7 +1429,6 @@ static void of_i2c_register_devices(struct i2c_adapter *adap)
 			dev_err(&adap->dev, "of_i2c: Failure registering %s\n",
 				node->full_name);
 			of_node_put(node);
-			irq_dispose_mapping(info.irq);
 			continue;
 		}
 	}
