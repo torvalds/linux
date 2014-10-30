d73341bff0c21157ee84dcb619b2ddcd4afc1bb3
Anton Blanchard <anton@samba.org>
2014-10-30 17:27:08 -0500

    ipr: convert to generic DMA API
    Even though the ipr driver is only used on PCI, convert it
    to use the generic DMA API.
    
    Signed-off-by: Anton Blanchard <anton@samba.org>
    Signed-off-by: Brian King <brking@linux.vnet.ibm.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/scsi/ipr.h
+++ drivers/scsi/ipr.h
@@ -1549,7 +1549,7 @@ struct ipr_ioa_cfg {
 	struct ipr_misc_cbs *vpd_cbs;
 	dma_addr_t vpd_cbs_dma;
 
-	struct pci_pool *ipr_cmd_pool;
+	struct dma_pool *ipr_cmd_pool;
 
 	struct ipr_cmnd *reset_cmd;
 	int (*reset) (struct ipr_cmnd *);
