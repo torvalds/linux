54279552bd260532d90e7a59fbc931924bbb0f7b
Boris Ostrovsky <boris.ostrovsky@oracle.com>
2014-10-31 11:49:32 -0400

    x86/core, x86/xen/smp: Use 'die_complete' completion when taking CPU down
    Commit 2ed53c0d6cc9 ("x86/smpboot: Speed up suspend/resume by
    avoiding 100ms sleep for CPU offline during S3") introduced
    completions to CPU offlining process. These completions are not
    initialized on Xen kernels causing a panic in
    play_dead_common().
    
    Move handling of die_complete into common routines to make them
    available to Xen guests.
    
    Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Reviewed-by: David Vrabel <david.vrabel@citrix.com>
    Cc: tianyu.lan@intel.com
    Cc: konrad.wilk@oracle.com
    Cc: xen-devel@lists.xenproject.org
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Link: http://lkml.kernel.org/r/1414770572-7950-1-git-send-email-boris.ostrovsky@oracle.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/smp.h
+++ arch/x86/include/asm/smp.h
@@ -150,6 +150,7 @@ static inline void arch_send_call_function_ipi_mask(const struct cpumask *mask)
 }
 
 void cpu_disable_common(void);
+void cpu_die_common(unsigned int cpu);
 void native_smp_prepare_boot_cpu(void);
 void native_smp_prepare_cpus(unsigned int max_cpus);
 void native_smp_cpus_done(unsigned int max_cpus);
