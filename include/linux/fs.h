78d28e651f97866d608d9b41f8ad291e65d47dd5
Al Viro <viro@zeniv.linux.org.uk>
2014-10-31 01:22:04 -0400

    kill f_dentry macro
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -789,7 +789,6 @@ struct file {
 		struct rcu_head 	fu_rcuhead;
 	} f_u;
 	struct path		f_path;
-#define f_dentry	f_path.dentry
 	struct inode		*f_inode;	/* cached value */
 	const struct file_operations	*f_op;
 
