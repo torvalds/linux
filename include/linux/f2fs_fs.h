be138b7b0d4cbfb8a927d9bc333ceffee9908c23
Jaegeuk Kim <jaegeuk@kernel.org>
2014-10-30 19:01:10 -0700

    f2fs: remove unnecessary macro
    Let's remove unused macro.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- include/linux/f2fs_fs.h
+++ include/linux/f2fs_fs.h
@@ -177,10 +177,6 @@ struct f2fs_extent {
 #define MAX_INLINE_DATA		(sizeof(__le32) * (DEF_ADDRS_PER_INODE - \
 						F2FS_INLINE_XATTR_ADDRS - 1))
 
-#define INLINE_DATA_OFFSET	(PAGE_CACHE_SIZE - sizeof(struct node_footer) -\
-				sizeof(__le32) * (DEF_ADDRS_PER_INODE + \
-				DEF_NIDS_PER_INODE - 1))
-
 struct f2fs_inode {
 	__le16 i_mode;			/* file mode */
 	__u8 i_advise;			/* file hints */
