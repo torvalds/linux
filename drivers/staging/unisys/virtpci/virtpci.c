09b0e27621ad8eed4fd48a5fb2d177834b1cb142
Bryan Thompson <bryan.thompson@unisys.com>
2014-11-03 14:21:00 -0500

    staging: unisys: virtpci: Fix CamelCase Chipset_DriverInfo
    Rename Chipset_DriverInfo to chipset_driver_info in virtpci.c
    
    Signed-off-by: Bryan Thompson <bryan.thompson@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/virtpci/virtpci.c
+++ drivers/staging/unisys/virtpci/virtpci.c
@@ -138,7 +138,7 @@ static struct device virtpci_rootbus_device = {
 };
 
 /* filled in with info about parent chipset driver when we register with it */
-static struct ultra_vbus_deviceinfo Chipset_DriverInfo;
+static struct ultra_vbus_deviceinfo chipset_driver_info;
 
 static const struct sysfs_ops virtpci_driver_sysfs_ops = {
 	.show = virtpci_driver_attr_show,
@@ -280,7 +280,7 @@ static int add_vbus(struct add_vbus_guestpart *addparams)
 		return 0;
 	}
 	write_vbus_chpInfo(vbus->platform_data /* chanptr */ ,
-			   &Chipset_DriverInfo);
+			   &chipset_driver_info);
 	write_vbus_busInfo(vbus->platform_data /* chanptr */ , &Bus_DriverInfo);
 	LOGINF("Added vbus %d; device %s created successfully\n",
 	       addparams->bus_no, BUS_ID(vbus));
@@ -801,7 +801,7 @@ static void fix_vbus_devInfo(struct device *dev, int devNo, int devType,
 	/* Re-write bus+chipset info, because it is possible that this
 	* was previously written by our good counterpart, visorbus.
 	*/
-	write_vbus_chpInfo(pChan, &Chipset_DriverInfo);
+	write_vbus_chpInfo(pChan, &chipset_driver_info);
 	write_vbus_busInfo(pChan, &Bus_DriverInfo);
 }
 
@@ -1536,7 +1536,7 @@ static int __init virtpci_mod_init(void)
 	DBGINF("device_register successful ret:%x\n", ret);
 
 	if (!uisctrl_register_req_handler(2, (void *)&virtpci_ctrlchan_func,
-					  &Chipset_DriverInfo)) {
+					  &chipset_driver_info)) {
 		LOGERR("uisctrl_register_req_handler ****FAILED.\n");
 		POSTCODE_LINUX_2(VPCI_CREATE_FAILURE_PC, POSTCODE_SEVERITY_ERR);
 		device_unregister(&virtpci_rootbus_device);
