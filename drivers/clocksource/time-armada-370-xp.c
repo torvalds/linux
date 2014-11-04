4a22d9c93af1f2b2c40354c4bc59fd007f33f05e
Ezequiel Garcia <ezequiel.garcia@free-electrons.com>
2014-11-04 10:21:33 -0300

    clocksource: armada-370-xp: Use the reference clock on A375 SoC
    The 25 MHz reference clock has better stability so its use is preferred over the
    core clock.
    
    This commit takes advantage of the already introduced Armada 375 devicetree
    compatible string and adds a new timer initialization. If available, the timer
    will use the reference clock (named as 'fixed'). Otherwise, it falls back to the
    previous behavior.
    
    Acked-by: Jason Cooper <jason@lakedaemon.net>
    Acked-by: Gregory CLEMENT <gregory.clement@free-electrons.com>
    Acked-by: Wim Van Sebroeck <wim@iguana.be>
    Reviewed-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Tested-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Ezequiel Garcia <ezequiel.garcia@free-electrons.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- drivers/clocksource/time-armada-370-xp.c
+++ drivers/clocksource/time-armada-370-xp.c
@@ -301,6 +301,34 @@ static void __init armada_xp_timer_init(struct device_node *np)
 CLOCKSOURCE_OF_DECLARE(armada_xp, "marvell,armada-xp-timer",
 		       armada_xp_timer_init);
 
+static void __init armada_375_timer_init(struct device_node *np)
+{
+	struct clk *clk;
+
+	clk = of_clk_get_by_name(np, "fixed");
+	if (!IS_ERR(clk)) {
+		clk_prepare_enable(clk);
+		timer_clk = clk_get_rate(clk);
+	} else {
+
+		/*
+		 * This fallback is required in order to retain proper
+		 * devicetree backwards compatibility.
+		 */
+		clk = of_clk_get(np, 0);
+
+		/* Must have at least a clock */
+		BUG_ON(IS_ERR(clk));
+		clk_prepare_enable(clk);
+		timer_clk = clk_get_rate(clk) / TIMER_DIVIDER;
+		timer25Mhz = false;
+	}
+
+	armada_370_xp_timer_common_init(np);
+}
+CLOCKSOURCE_OF_DECLARE(armada_375, "marvell,armada-375-timer",
+		       armada_375_timer_init);
+
 static void __init armada_370_timer_init(struct device_node *np)
 {
 	struct clk *clk = of_clk_get(np, 0);
