1906bbbddbe085b19be2c21cd132335260f551c3
Alexander Aring <alex.aring@gmail.com>
2014-11-05 20:51:21 +0100

    ieee802154: add IEEE802154_EXTENDED_ADDR_LEN
    This patch adds a new define for getting the length of an extended
    address.
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- include/linux/ieee802154.h
+++ include/linux/ieee802154.h
@@ -29,6 +29,8 @@
 #define IEEE802154_MTU			127
 #define IEEE802154_MIN_PSDU_LEN		5
 
+#define IEEE802154_EXTENDED_ADDR_LEN	8
+
 #define IEEE802154_FC_TYPE_BEACON	0x0	/* Frame is beacon */
 #define	IEEE802154_FC_TYPE_DATA		0x1	/* Frame is data */
 #define IEEE802154_FC_TYPE_ACK		0x2	/* Frame is acknowledgment */
