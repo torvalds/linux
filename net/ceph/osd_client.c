54ff4c89a5445fa8f313a338c1cf5478317df154
Ilya Dryomov <idryomov@redhat.com>
2014-11-05 19:33:44 +0300

    libceph: change from BUG to WARN for __remove_osd() asserts
    commit cc9f1f518cec079289d11d732efa490306b1ddad upstream.
    
    No reason to use BUG_ON for osd request list assertions.
    
    Signed-off-by: Ilya Dryomov <idryomov@redhat.com>
    Reviewed-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/ceph/osd_client.c
+++ net/ceph/osd_client.c
@@ -968,8 +968,8 @@ static void put_osd(struct ceph_osd *osd)
 static void __remove_osd(struct ceph_osd_client *osdc, struct ceph_osd *osd)
 {
 	dout("__remove_osd %p\n", osd);
-	BUG_ON(!list_empty(&osd->o_requests));
-	BUG_ON(!list_empty(&osd->o_linger_requests));
+	WARN_ON(!list_empty(&osd->o_requests));
+	WARN_ON(!list_empty(&osd->o_linger_requests));
 
 	rb_erase(&osd->o_node, &osdc->osds);
 	list_del_init(&osd->o_osd_lru);
