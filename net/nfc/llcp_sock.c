413df10bbf09e8ab1e2659126af849b4c9f3a3e3
Axel Lin <axel.lin@ingics.com>
2014-11-06 18:20:41 +0800

    NFC: llcp: Use list_for_each_entry in llcp_accept_poll
    list_for_each_entry_safe() is necessary if list objects are deleted from
    the list while traversing it. Not the case here, so we can use the base
    list_for_each_entry variant.
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- net/nfc/llcp_sock.c
+++ net/nfc/llcp_sock.c
@@ -524,13 +524,13 @@ static int llcp_sock_getname(struct socket *sock, struct sockaddr *uaddr,
 
 static inline unsigned int llcp_accept_poll(struct sock *parent)
 {
-	struct nfc_llcp_sock *llcp_sock, *n, *parent_sock;
+	struct nfc_llcp_sock *llcp_sock, *parent_sock;
 	struct sock *sk;
 
 	parent_sock = nfc_llcp_sock(parent);
 
-	list_for_each_entry_safe(llcp_sock, n, &parent_sock->accept_queue,
-				 accept_queue) {
+	list_for_each_entry(llcp_sock, &parent_sock->accept_queue,
+			    accept_queue) {
 		sk = &llcp_sock->sk;
 
 		if (sk->sk_state == LLCP_CONNECTED)
