8a8ae62f8296760a2a1eee7009a1444c327603e0
Jiri Slaby <jslaby@suse.cz>
2014-11-06 16:56:33 +0100

    tty: warn on deprecated serial flags
    When somebody calls TIOCSSERIAL ioctl with serial flags to set one of
    * ASYNC_SESSION_LOCKOUT
    * ASYNC_PGRP_LOCKOUT
    * ASYNC_CALLOUT_NOHUP
    * ASYNC_AUTOPROBE
    nothing happens. We actually ignore the flags for over a decade at
    least (I checked 2.6.0).
    
    So start yelling at users who use those flags, that they shouldn't.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Cc: Peter Hurley <peter@hurleysoftware.com>
    Cc: Alan Cox <gnomes@lxorguk.ukuu.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/tty_io.c
+++ drivers/tty/tty_io.c
@@ -2760,6 +2760,24 @@ static int tty_tiocgicount(struct tty_struct *tty, void __user *arg)
 	return 0;
 }
 
+static void tty_warn_deprecated_flags(struct serial_struct __user *ss)
+{
+	static DEFINE_RATELIMIT_STATE(depr_flags,
+			DEFAULT_RATELIMIT_INTERVAL,
+			DEFAULT_RATELIMIT_BURST);
+	char comm[TASK_COMM_LEN];
+	int flags;
+
+	if (get_user(flags, &ss->flags))
+		return;
+
+	flags &= ASYNC_DEPRECATED;
+
+	if (flags && __ratelimit(&depr_flags))
+		pr_warning("%s: '%s' is using deprecated serial flags (with no effect): %.8x\n",
+				__func__, get_task_comm(comm, current), flags);
+}
+
 /*
  * if pty, return the slave side (real_tty)
  * otherwise, return self
@@ -2902,6 +2920,9 @@ long tty_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 			tty_buffer_flush(tty, NULL);
 			break;
 		}
+		break;
+	case TIOCSSERIAL:
+		tty_warn_deprecated_flags(p);
 		break;
 	}
 	if (tty->ops->ioctl) {
