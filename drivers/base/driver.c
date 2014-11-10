6386a15c445c1cd0108671a0078a4b65af89799c
Wolfram Sang <wsa@the-dreams.de>
2014-11-11 00:49:25 +0100

    Revert "core: platform: add warning if driver has no owner"
    This is too noisy at the moment, triggered by codepaths not accessed on
    our test-systems. Needs more investigation.
    
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/driver.c
+++ drivers/base/driver.c
@@ -151,9 +151,6 @@ int driver_register(struct device_driver *drv)
 
 	BUG_ON(!drv->bus->p);
 
-	if (!drv->owner)
-		printk(KERN_WARNING "Driver '%s' needs an owner", drv->name);
-
 	if ((drv->bus->probe && drv->probe) ||
 	    (drv->bus->remove && drv->remove) ||
 	    (drv->bus->shutdown && drv->shutdown))
