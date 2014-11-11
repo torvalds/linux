2a6af25befd0197e805b3aa5ebfc253079ce7136
Stephan Mueller <smueller@chronox.de>
2014-11-11 05:37:07 +0100

    crypto: algif - zeroize message digest buffer
    Zeroize the buffer holding the message digest calculated for the
    consumer before the buffer is released by the hash AF_ALG interface
    handler.
    
    Signed-off-by: Stephan Mueller <smueller@chronox.de>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/algif_hash.c
+++ crypto/algif_hash.c
@@ -258,6 +258,8 @@ static void hash_sock_destruct(struct sock *sk)
 	struct alg_sock *ask = alg_sk(sk);
 	struct hash_ctx *ctx = ask->private;
 
+	memzero_explicit(ctx->result,
+		     crypto_ahash_digestsize(crypto_ahash_reqtfm(&ctx->req)));
 	sock_kfree_s(sk, ctx->result,
 		     crypto_ahash_digestsize(crypto_ahash_reqtfm(&ctx->req)));
 	sock_kfree_s(sk, ctx, ctx->len);
