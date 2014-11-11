e1bd95bf7c25530a99ce371e0a26e4932efc021d
Stephan Mueller <smueller@chronox.de>
2014-11-11 05:37:57 +0100

    crypto: algif - zeroize IV buffer
    Zeroize the buffer holding the IV used for the completed
    cipher operation before the buffer is released by the
    skcipher AF_ALG interface handler.
    
    Signed-off-by: Stephan Mueller <smueller@chronox.de>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/algif_skcipher.c
+++ crypto/algif_skcipher.c
@@ -566,6 +566,7 @@ static void skcipher_sock_destruct(struct sock *sk)
 	struct crypto_ablkcipher *tfm = crypto_ablkcipher_reqtfm(&ctx->req);
 
 	skcipher_free_sgl(sk);
+	memzero_explicit(ctx->iv, crypto_ablkcipher_ivsize(tfm));
 	sock_kfree_s(sk, ctx->iv, crypto_ablkcipher_ivsize(tfm));
 	sock_kfree_s(sk, ctx, ctx->len);
 	af_alg_release_parent(sk);
