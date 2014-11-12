92de383154e98bc0b9c4b3364af3945076f77032
Finn Thain <fthain@telegraphics.com.au>
2014-11-12 16:12:04 +1100

    mac_scsi: Remove header
    The #defines in mac_scsi.h are intended to influence subsequent #includes in
    mac_scsi.c. IMHO, that's too convoluted.
    
    Remove mac_scsi.h by moving those macro definitions to mac_scsi.c,
    consistent with other NCR5380 drivers.
    
    Signed-off-by: Finn Thain <fthain@telegraphics.com.au>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/scsi/mac_scsi.c
+++ drivers/scsi/mac_scsi.c
@@ -31,9 +31,28 @@
 #include <asm/mac_via.h>
 
 #include <scsi/scsi_host.h>
-#include "mac_scsi.h"
+
+/* Definitions for the core NCR5380 driver. */
 
 #define PSEUDO_DMA
+
+#define NCR5380_implementation_fields   /* none */
+#define NCR5380_local_declare()         struct Scsi_Host *_instance
+#define NCR5380_setup(instance)         _instance = instance
+
+#define NCR5380_read(reg)               macscsi_read(_instance, reg)
+#define NCR5380_write(reg, value)       macscsi_write(_instance, reg, value)
+
+#define NCR5380_pread                   macscsi_pread
+#define NCR5380_pwrite                  macscsi_pwrite
+
+#define NCR5380_intr                    macscsi_intr
+#define NCR5380_queue_command           macscsi_queue_command
+#define NCR5380_abort                   macscsi_abort
+#define NCR5380_bus_reset               macscsi_bus_reset
+#define NCR5380_info                    macscsi_info
+#define NCR5380_show_info               macscsi_show_info
+#define NCR5380_write_info              macscsi_write_info
 
 #include "NCR5380.h"
 
