069fb0b63722f8c9f8b4bbce236793626c89af33
Sebastian Schmidt <yath@yath.de>
2014-11-14 10:51:45 -0800

    syslog: Provide stub check_syslog_permissions
    When building without CONFIG_PRINTK, we need to provide a stub
    check_syslog_permissions. As there is no way to turn on the
    dmesg_restrict sysctl without CONFIG_PRINTK, return success.
    
    Reported-by: Jim Davis <jim.epost@gmail.com>
    Signed-off-by: Sebastian Schmidt <yath@yath.de>
    Acked-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- include/linux/syslog.h
+++ include/linux/syslog.h
@@ -48,6 +48,14 @@
 #define SYSLOG_FROM_PROC             1
 
 int do_syslog(int type, char __user *buf, int count, bool from_file);
+
+#ifdef CONFIG_PRINTK
 int check_syslog_permissions(int type, bool from_file);
+#else
+static inline int check_syslog_permissions(int type, bool from_file)
+{
+	return 0;
+}
+#endif
 
 #endif /* _LINUX_SYSLOG_H */
