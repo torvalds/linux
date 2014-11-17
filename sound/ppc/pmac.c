1ea7a568c63a4735872fc091efbd22d2e4d9c972
Markus Elfring <elfring@users.sourceforge.net>
2014-11-17 13:35:54 +0100

    ALSA: powermac: Deletion of an unnecessary check before the function call "pci_dev_put"
    The pci_dev_put() function tests whether its argument is NULL and then
    returns immediately. Thus the test around the call is not needed.
    
    This issue was detected by using the Coccinelle software.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/ppc/pmac.c
+++ sound/ppc/pmac.c
@@ -887,8 +887,7 @@ static int snd_pmac_free(struct snd_pmac *chip)
 		}
 	}
 
-	if (chip->pdev)
-		pci_dev_put(chip->pdev);
+	pci_dev_put(chip->pdev);
 	of_node_put(chip->node);
 	kfree(chip);
 	return 0;
