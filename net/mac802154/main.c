cd11d935f2df86b6a619e8721553b1d41633f0fd
Alexander Aring <alex.aring@gmail.com>
2014-11-17 08:20:48 +0100

    mac802154: remove deprecated linux-zigbee info
    We don't and we can't name it zigbee anymore. This patch removes
    deprecated information for project website.
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/mac802154/main.c
+++ net/mac802154/main.c
@@ -4,8 +4,6 @@
  * Written by:
  * Alexander Smirnov <alex.bluesman.smirnov@gmail.com>
  *
- * Based on the code from 'linux-zigbee.sourceforge.net' project.
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2
  * as published by the Free Software Foundation.
