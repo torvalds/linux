8488d6eca9d3bfd10420d37fccf45b98cdd867ac
Tobenna P. Igwe <ptigwe@gmail.com>
2014-11-18 01:45:41 +0000

    staging:rtl8723au: Fix sparse warning cast to restricted __le16
    This patch fixes the following sparse warning:
    
    drivers/staging/rtl8723au/hal/rtl8723a_bt-coexist.c:343:36: warning:
    cast to restricted __le16
    
    by using the le16_to_cpus function.
    
    Signed-off-by: Tobenna P. Igwe <ptigwe@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/hal/rtl8723a_bt-coexist.c
+++ drivers/staging/rtl8723au/hal/rtl8723a_bt-coexist.c
@@ -340,7 +340,7 @@ static u8 bthci_GetAssocInfo(struct rtw_adapter *padapter, u8 EntryNum)
 			tempBuf, TotalLen-BaseMemoryShift);
 
 		pAmpAsoc = (struct amp_assoc_structure *)tempBuf;
-		pAmpAsoc->Length = le16_to_cpu(pAmpAsoc->Length);
+		le16_to_cpus(&pAmpAsoc->Length);
 		BaseMemoryShift += 3 + pAmpAsoc->Length;
 
 		RTPRINT(FIOCTL, IOCTL_BT_HCICMD, ("TypeID = 0x%x, ", pAmpAsoc->TypeID));
