952768c41a993dc1d813baa29d87ecb3da9b266e
Hans Verkuil <hans.verkuil@cisco.com>
2014-11-18 09:51:06 -0300

    [media] vivid: enable vb2_expbuf support
    Now that vb2 supports DMABUF export for dma-sg and vmalloc memory
    modes, we can enable the vb2_expbuf support in vivid.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Reviewed-by: Pawel Osciak <pawel@osciak.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/platform/vivid/vivid-core.c
+++ drivers/media/platform/vivid/vivid-core.c
@@ -572,7 +572,7 @@ static const struct v4l2_ioctl_ops vivid_ioctl_ops = {
 	.vidioc_querybuf		= vb2_ioctl_querybuf,
 	.vidioc_qbuf			= vb2_ioctl_qbuf,
 	.vidioc_dqbuf			= vb2_ioctl_dqbuf,
-/* Not yet	.vidioc_expbuf		= vb2_ioctl_expbuf,*/
+	.vidioc_expbuf			= vb2_ioctl_expbuf,
 	.vidioc_streamon		= vb2_ioctl_streamon,
 	.vidioc_streamoff		= vb2_ioctl_streamoff,
 
