99990f1b0b95b3c6056d7d80669ab9a8644fed34
Imre Deak <imre.deak@intel.com>
2014-11-20 17:59:22 +0200

    drm/i915: remove the IRQs enabled WARN from intel_disable_gt_powersave
    This function can be called now with i915 interrupts enabled, so the
    corresponding WARN is incorrect, remove it. I think this was spotted by
    Paulo during his review, but since I already removed the same WARN
    from intel_suspend_gt_powersave() I missed then his point.
    
    Spotted-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -6223,9 +6223,6 @@ void intel_disable_gt_powersave(struct drm_device *dev)
 {
 	struct drm_i915_private *dev_priv = dev->dev_private;
 
-	/* Interrupts should be disabled already to avoid re-arming. */
-	WARN_ON(intel_irqs_enabled(dev_priv));
-
 	if (IS_IRONLAKE_M(dev)) {
 		ironlake_disable_drps(dev);
 		ironlake_disable_rc6(dev);
