7347a6cbf1b5672a87975e58c8b99d882b96e9b9
kbuild test robot <fengguang.wu@intel.com>
2014-11-20 17:16:23 +0800

    amdkfd: pqm_get_kernel_queue() can be static
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Oded Gabbay <oded.gabbay@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_process_queue_manager.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_process_queue_manager.c
@@ -325,7 +325,8 @@ int pqm_update_queue(struct process_queue_manager *pqm, unsigned int qid,
 	return 0;
 }
 
-struct kernel_queue *pqm_get_kernel_queue(struct process_queue_manager *pqm,
+static __attribute__((unused)) struct kernel_queue *pqm_get_kernel_queue(
+					struct process_queue_manager *pqm,
 					unsigned int qid)
 {
 	struct process_queue_node *pqn;
