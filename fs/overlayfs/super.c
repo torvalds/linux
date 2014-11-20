71d509280f7e92eb60ae6b7c78c20afafff060c7
Miklos Szeredi <mszeredi@suse.cz>
2014-11-20 16:40:01 +0100

    ovl: use lockless_dereference() for upperdentry
    Don't open code lockless_dereference() in ovl_upperdentry_dereference().
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -84,12 +84,7 @@ enum ovl_path_type ovl_path_type(struct dentry *dentry)
 
 static struct dentry *ovl_upperdentry_dereference(struct ovl_entry *oe)
 {
-	struct dentry *upperdentry = ACCESS_ONCE(oe->__upperdentry);
-	/*
-	 * Make sure to order reads to upperdentry wrt ovl_dentry_update()
-	 */
-	smp_read_barrier_depends();
-	return upperdentry;
+	return lockless_dereference(oe->__upperdentry);
 }
 
 void ovl_path_upper(struct dentry *dentry, struct path *path)
