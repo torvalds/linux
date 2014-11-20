7676895f4736421ebafc48de5078e25ea69e88ee
Miklos Szeredi <mszeredi@suse.cz>
2014-11-20 16:40:02 +0100

    ovl: ovl_dir_fsync() cleanup
    Check against !OVL_PATH_LOWER instead of OVL_PATH_MERGE.  For a copied up
    directory the two are currently equivalent.
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>

--- fs/overlayfs/readdir.c
+++ fs/overlayfs/readdir.c
@@ -450,10 +450,10 @@ static int ovl_dir_fsync(struct file *file, loff_t start, loff_t end,
 	/*
 	 * Need to check if we started out being a lower dir, but got copied up
 	 */
-	if (!od->is_upper && ovl_path_type(dentry) == OVL_PATH_MERGE) {
+	if (!od->is_upper && ovl_path_type(dentry) != OVL_PATH_LOWER) {
 		struct inode *inode = file_inode(file);
 
-		realfile =lockless_dereference(od->upperfile);
+		realfile = lockless_dereference(od->upperfile);
 		if (!realfile) {
 			struct path upperpath;
 
