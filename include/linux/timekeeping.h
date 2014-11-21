21b6c0512e8aca75ce76365e1aef9fb16e007100
John Stultz <john.stultz@linaro.org>
2014-11-20 21:19:15 -0800

    time: Remove timekeeping_inject_sleeptime()
    Since all users have been converted to using the 64bit
    timekeeping_inject_sleeptime64(), remove the old y2038
    problematic timekeeping_inject_sleeptime().
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- include/linux/timekeeping.h
+++ include/linux/timekeeping.h
@@ -225,17 +225,6 @@ static inline void timekeeping_clocktai(struct timespec *ts)
  */
 extern void timekeeping_inject_sleeptime64(struct timespec64 *delta);
 
-/**
- * Deprecated. Use timekeeping_inject_sleeptime64().
- */
-static inline void timekeeping_inject_sleeptime(struct timespec *delta)
-{
-	struct timespec64 delta64;
-
-	delta64 = timespec_to_timespec64(*delta);
-	timekeeping_inject_sleeptime64(&delta64);
-}
-
 /*
  * PPS accessor
  */
