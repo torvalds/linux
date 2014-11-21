e5092d796913ad28d528b571f646143ffe1d5dcc
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-11-21 14:22:35 -0700

    staging: comedi: adv_pci1723: update the MODULE_DESCRIPTION
    Update the MODULE_DESCRIPTION to something more useful than the
    generic "Comedi low-level driver".
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/adv_pci1723.c
+++ drivers/staging/comedi/drivers/adv_pci1723.c
@@ -234,5 +234,5 @@ static struct pci_driver adv_pci1723_pci_driver = {
 module_comedi_pci_driver(adv_pci1723_driver, adv_pci1723_pci_driver);
 
 MODULE_AUTHOR("Comedi http://www.comedi.org");
-MODULE_DESCRIPTION("Comedi low-level driver");
+MODULE_DESCRIPTION("Advantech PCI-1723 Comedi driver");
 MODULE_LICENSE("GPL");
