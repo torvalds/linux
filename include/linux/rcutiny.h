c1fe9cde4ae904fffb5b4d975d0a37e99136ff50
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2014-11-21 15:45:27 -0800

    rcu: Provide rcu_batches_completed_sched() for TINY_RCU
    A bug in rcutorture has caused it to ignore completed batches.
    In preparation for fixing that bug, this commit provides TINY_RCU with
    the required rcu_batches_completed_sched().
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- include/linux/rcutiny.h
+++ include/linux/rcutiny.h
@@ -107,6 +107,14 @@ static inline unsigned long rcu_batches_completed_bh(void)
 	return 0;
 }
 
+/*
+ * Return the number of sched grace periods.
+ */
+static inline unsigned long rcu_batches_completed_sched(void)
+{
+	return 0;
+}
+
 static inline void rcu_force_quiescent_state(void)
 {
 }
