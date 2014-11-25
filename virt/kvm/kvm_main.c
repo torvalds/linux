eed6e79d7362335967def243cc891179a9c59892
David Hildenbrand <dahi@linux.vnet.ibm.com>
2014-11-25 17:04:08 +0100

    KVM: don't check for PF_VCPU when yielding
    kvm_enter_guest() has to be called with preemption disabled and will
    set PF_VCPU.  Current code takes PF_VCPU as a hint that the VCPU thread
    is running and therefore needs no yield.
    
    However, the check on PF_VCPU is wrong on s390, where preemption has
    to stay enabled in order to correctly process page faults.  Thus,
    s390 reenables preemption and starts to execute the guest.  The thread
    might be scheduled out between kvm_enter_guest() and kvm_exit_guest(),
    resulting in PF_VCPU being set but not being run.  When this happens,
    the opportunity for directed yield is missed.
    
    However, this check is done already in kvm_vcpu_on_spin before calling
    kvm_vcpu_yield_loop:
    
            if (!ACCESS_ONCE(vcpu->preempted))
                    continue;
    
    so the check on PF_VCPU is superfluous in general, and this patch
    removes it.
    
    Signed-off-by: David Hildenbrand <dahi@linux.vnet.ibm.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -1788,10 +1788,6 @@ int kvm_vcpu_yield_to(struct kvm_vcpu *target)
 	rcu_read_unlock();
 	if (!task)
 		return ret;
-	if (task->flags & PF_VCPU) {
-		put_task_struct(task);
-		return ret;
-	}
 	ret = yield_to(task, 1);
 	put_task_struct(task);
 
