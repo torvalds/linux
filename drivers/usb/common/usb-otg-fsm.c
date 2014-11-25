1f12c99f84ff70871def90d13182a3d5b1808139
Li Jun <jun.li@freescale.com>
2014-11-25 12:08:48 +0800

    MLK-9897-2 usb: otg-fsm: check B-device state before sending polling request
    While system resume, the connected udev as B-device may not has been resumed,
    if HNP polling request is sent to it, HNP polling may fail, this patch adds
    check of the udev state to make sure it finished resume and to be configured
    state before sending host request message, otherwise try next HNP polling
    request.
    
    Acked-by: Peter Chen <peter.chen@freescale.com>
    Signed-off-by: Li Jun <jun.li@freescale.com>
    (cherry picked from commit 2cff5eb7bd4feb70cd28ac4655e7433e57a17938)
    (cherry picked from commit b6eea060a3760bdb24d1514a5130a346daba81c9)

--- drivers/usb/common/usb-otg-fsm.c
+++ drivers/usb/common/usb-otg-fsm.c
@@ -438,6 +438,12 @@ int otg_hnp_polling(struct otg_fsm *fsm)
 		return -ENODEV;
 	}
 
+	if (udev->state != USB_STATE_CONFIGURED) {
+		dev_dbg(&udev->dev, "the B dev is not resumed!\n");
+		otg_add_timer(fsm, HNP_POLLING);
+		return -EPERM;
+	}
+
 	/*
 	 * Legacy otg test device does not support HNP polling,
 	 * start HNP directly for legacy otg test device.
@@ -477,7 +483,7 @@ int otg_hnp_polling(struct otg_fsm *fsm)
 			retval = -EINVAL;
 		}
 	} else {
-		dev_err(&udev->dev, "Get one byte OTG status failed\n");
+		dev_warn(&udev->dev, "Get one byte OTG status failed\n");
 		retval = -EIO;
 	}
 	return retval;
