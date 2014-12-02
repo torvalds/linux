3fe6b49e2f8adc61fef9a22ecc0c3565f9ea468d
Jiri Pirko <jiri@resnulli.us>
2014-12-02 18:00:33 +0100

    net_sched: cls_bpf: remove faulty use of list_for_each_entry_rcu
    rcu variant is not correct here. The code is called by updater (rtnl
    lock is held), not by reader (no rcu_read_lock is held).
    
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>
    ACKed-by: Jamal Hadi Salim <jhs@mojatatu.com>
    Acked-by: Daniel Borkmann <dborkman@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/cls_bpf.c
+++ net/sched/cls_bpf.c
@@ -141,7 +141,7 @@ static unsigned long cls_bpf_get(struct tcf_proto *tp, u32 handle)
 	if (head == NULL)
 		return 0UL;
 
-	list_for_each_entry_rcu(prog, &head->plist, link) {
+	list_for_each_entry(prog, &head->plist, link) {
 		if (prog->handle == handle) {
 			ret = (unsigned long) prog;
 			break;
@@ -337,7 +337,7 @@ static void cls_bpf_walk(struct tcf_proto *tp, struct tcf_walker *arg)
 	struct cls_bpf_head *head = rtnl_dereference(tp->root);
 	struct cls_bpf_prog *prog;
 
-	list_for_each_entry_rcu(prog, &head->plist, link) {
+	list_for_each_entry(prog, &head->plist, link) {
 		if (arg->count < arg->skip)
 			goto skip;
 		if (arg->fn(tp, (unsigned long) prog, arg) < 0) {
