8117bf17fe1a2a37ba33d71e59597c0f2261ad92
Arnd Bergmann <arnd@arndb.de>
2014-12-02 14:26:44 +0100

    ARM: at91: at91rm9200 ST initialization is now DT only
    As at91rm9200 is now DT only, there is no need to keep old entry point in this
    at91rm9200 System Timer (ST) driver.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    [nicolas.ferre@atmel.com: split patch]
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/board-dt-rm9200.c
+++ arch/arm/mach-at91/board-dt-rm9200.c
@@ -26,9 +26,7 @@
 
 static void __init at91rm9200_dt_timer_init(void)
 {
-#if defined(CONFIG_COMMON_CLK)
 	of_clk_init(NULL);
-#endif
 	at91rm9200_timer_init();
 }
 
--- arch/arm/mach-at91/generic.h
+++ arch/arm/mach-at91/generic.h
@@ -30,8 +30,6 @@ extern void __init at91_sysirq_mask_rtc(u32 rtc_base);
 extern void __init at91_sysirq_mask_rtt(u32 rtt_base);
 
  /* Timer */
-extern void __init at91_init_time(void);
-extern void at91rm9200_ioremap_st(u32 addr);
 extern void at91rm9200_timer_init(void);
 extern void at91sam926x_ioremap_pit(u32 addr);
 extern void at91sam926x_pit_init(int irq);
