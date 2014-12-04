b45257b10d7cc21ed0393f31e1f8ac6cdde9fa18
Viresh Kumar <viresh.kumar@linaro.org>
2014-12-04 09:41:44 +0530

    thermal: db8500: pass cpu_present_mask to cpufreq_cooling_register()
    cpufreq_cooling_register() expects mask of all the CPUs where frequency
    constraint is applicable.
    
    This platform has more than one CPU to which these constraints will apply and so
    passing mask of only CPU0 wouldn't be sufficient. Also, this platform has a
    single cluster of CPUs and the constraint applies to all CPUs.
    
    If CPU0 is hoplugged out then we may face strange BUGs as cpu_cooling framework
    isn't aware of any siblings sharing clock line.
    
    Fix it by passing cpu_present_mask to cpufreq_cooling_register().
    
    Cc: Hongbo Zhang <hongbo.zhang@linaro.com>
    Cc: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Eduardo Valentin <edubezval@gmail.com>

--- drivers/thermal/db8500_cpufreq_cooling.c
+++ drivers/thermal/db8500_cpufreq_cooling.c
@@ -27,11 +27,8 @@
 static int db8500_cpufreq_cooling_probe(struct platform_device *pdev)
 {
 	struct thermal_cooling_device *cdev;
-	struct cpumask mask_val;
 
-	cpumask_set_cpu(0, &mask_val);
-	cdev = cpufreq_cooling_register(&mask_val);
-
+	cdev = cpufreq_cooling_register(cpu_present_mask);
 	if (IS_ERR(cdev)) {
 		dev_err(&pdev->dev, "Failed to register cooling device\n");
 		return PTR_ERR(cdev);
