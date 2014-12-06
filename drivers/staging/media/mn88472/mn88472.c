92ced56d06a9c5cfa3ef3b628068c36fd3c34786
Benjamin Larsson <benjamin@southpole.se>
2014-12-06 18:57:31 -0300

    [media] mn88472: add 5MHz dvb-t2 bandwitdh support
    Signed-off-by: Benjamin Larsson <benjamin@southpole.se>
    Reviewed-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/staging/media/mn88472/mn88472.c
+++ drivers/staging/media/mn88472/mn88472.c
@@ -61,7 +61,10 @@ static int mn88472_set_frontend(struct dvb_frontend *fe)
 	switch (c->delivery_system) {
 	case SYS_DVBT:
 	case SYS_DVBT2:
-		if (c->bandwidth_hz <= 6000000) {
+		if (c->bandwidth_hz <= 5000000) {
+			memcpy(bw_val, "\xe5\x99\x9a\x1b\xa9\x1b\xa9", 7);
+			bw_val2 = 0x03;
+		} else if (c->bandwidth_hz <= 6000000) {
 			/* IF 3570000 Hz, BW 6000000 Hz */
 			memcpy(bw_val, "\xbf\x55\x55\x15\x6b\x15\x6b", 7);
 			bw_val2 = 0x02;
