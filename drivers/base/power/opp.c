7284a00f17290114d304528fc49aeee5c2b9b433
Viresh Kumar <viresh.kumar@linaro.org>
2014-12-09 11:18:51 +0530

    PM / OPP: set new_opp->dev_opp to a valid dev_opp
    We find/allocate dev_opp after using its value to fill new_opp->dev_opp right
    now. Move this to a later point where dev_opp is valid.
    
    Fixes: a7470db6fec4 (PM / OPP don't match for existing OPPs when list is empty)
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/base/power/opp.c
+++ drivers/base/power/opp.c
@@ -400,7 +400,6 @@ static int dev_pm_opp_add_dynamic(struct device *dev, unsigned long freq,
 	mutex_lock(&dev_opp_list_lock);
 
 	/* populate the opp table */
-	new_opp->dev_opp = dev_opp;
 	new_opp->rate = freq;
 	new_opp->u_volt = u_volt;
 	new_opp->available = true;
@@ -460,6 +459,7 @@ static int dev_pm_opp_add_dynamic(struct device *dev, unsigned long freq,
 	}
 
 list_add:
+	new_opp->dev_opp = dev_opp;
 	list_add_rcu(&new_opp->node, head);
 	mutex_unlock(&dev_opp_list_lock);
 
