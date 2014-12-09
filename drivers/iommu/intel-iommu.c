6d1b9cc9ee10374290583f69270e97de38128fb6
Joerg Roedel <jroedel@suse.de>
2014-12-09 13:18:22 +0100

    iommu/vt-d: Remove dead code in device_notifier
    This code only runs when action == BUS_NOTIFY_REMOVED_DEVICE,
    so it can't be BUS_NOTIFY_DEL_DEVICE.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/intel-iommu.c
+++ drivers/iommu/intel-iommu.c
@@ -4029,14 +4029,6 @@ static int device_notifier(struct notifier_block *nb,
 	if (action != BUS_NOTIFY_REMOVED_DEVICE)
 		return 0;
 
-	/*
-	 * If the device is still attached to a device driver we can't
-	 * tear down the domain yet as DMA mappings may still be in use.
-	 * Wait for the BUS_NOTIFY_UNBOUND_DRIVER event to do that.
-	 */
-	if (action == BUS_NOTIFY_DEL_DEVICE && dev->driver != NULL)
-		return 0;
-
 	domain = find_domain(dev);
 	if (!domain)
 		return 0;
