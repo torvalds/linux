304f0a98d168563b5f69c53a7544c36e6d221ac4
Alessio Igor Bogani <alessio.bogani@elettra.eu>
2014-12-09 09:40:38 +0100

    mmc: sdhci: Fix FSL ESDHC reset handling quirk
    The commit 0718e59ae259 ("mmc: sdhci: move FSL ESDHC reset handling quirk into
    esdhc code") states that Freescale esdhc is the only controller which needs
    the interrupt registers restored after a reset. So it moves
    SDHCI_QUIRK_RESTORE_IRQS_AFTER_RESET quirk handling code into the
    esdhc-imx driver only. Unfortunately the same controller is used in
    other boards which use the of-esdhc driver instead (like powerpc P2020).
    
    Restore interrupts after reset in the sdhci-of-esdhc driver also.
    
    Signed-off-by: Alessio Igor Bogani <alessio.bogani@elettra.eu>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/sdhci-of-esdhc.c
+++ drivers/mmc/host/sdhci-of-esdhc.c
@@ -276,6 +276,14 @@ static void esdhc_pltfm_set_bus_width(struct sdhci_host *host, int width)
 			ESDHC_CTRL_BUSWIDTH_MASK, ctrl);
 }
 
+static void esdhc_reset(struct sdhci_host *host, u8 mask)
+{
+	sdhci_reset(host, mask);
+
+	sdhci_writel(host, host->ier, SDHCI_INT_ENABLE);
+	sdhci_writel(host, host->ier, SDHCI_SIGNAL_ENABLE);
+}
+
 static const struct sdhci_ops sdhci_esdhc_ops = {
 	.read_l = esdhc_readl,
 	.read_w = esdhc_readw,
@@ -290,7 +298,7 @@ static const struct sdhci_ops sdhci_esdhc_ops = {
 	.platform_init = esdhc_of_platform_init,
 	.adma_workaround = esdhci_of_adma_workaround,
 	.set_bus_width = esdhc_pltfm_set_bus_width,
-	.reset = sdhci_reset,
+	.reset = esdhc_reset,
 	.set_uhs_signaling = sdhci_set_uhs_signaling,
 };
 
