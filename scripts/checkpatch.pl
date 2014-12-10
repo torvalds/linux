f512357646268451da0d7e1e0100c55df6c8cea6
Julius Werner <jwerner@chromium.org>
2014-12-10 15:51:54 -0800

    checkpatch: allow certain SI units with three characters
    Checkpatch flags CamelCase identifiers in strict mode, but it has a
    feature to ignore parts with only two characters to allow for SI units
    like mV or uA.  Unfortunately, not all SI units fit in two characters, and
    not all are lower case followed by upper case.
    
    This patch adds hardcoded detection for frequency and 1024-based size
    units (Hz/KHz/MHz/GHz/THz and KiB/MiB/GiB/TiB), since allowing any three
    character combinations might be too lenient.  The list can later be
    expanded as needed.
    
    Signed-off-by: Julius Werner <jwerner@chromium.org>
    Acked-by: Joe Perches <joe@perches.com>
    Cc: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -4104,7 +4104,9 @@ sub process {
 #Ignore Page<foo> variants
 			    $var !~ /^(?:Clear|Set|TestClear|TestSet|)Page[A-Z]/ &&
 #Ignore SI style variants like nS, mV and dB (ie: max_uV, regulator_min_uA_show)
-			    $var !~ /^(?:[a-z_]*?)_?[a-z][A-Z](?:_[a-z_]+)?$/) {
+			    $var !~ /^(?:[a-z_]*?)_?[a-z][A-Z](?:_[a-z_]+)?$/ &&
+#Ignore some three character SI units explicitly, like MiB and KHz
+			    $var !~ /^(?:[a-z_]*?)_?(?:[KMGT]iB|[KMGT]?Hz)(?:_[a-z_]+)?$/) {
 				while ($var =~ m{($Ident)}g) {
 					my $word = $1;
 					next if ($word !~ /[A-Z][a-z]|[a-z][A-Z]/);
