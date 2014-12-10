347d761c74f75d55a36350ae7505498bd56b33ec
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2014-12-11 01:26:04 +0200

    drm: rcar-du: Don't fail probe in case of partial encoder init error
    If an encoder fails to initialize the device can still be used without
    the failed encoder. Don't propagate the error out of the probe function
    but just skip the failed encoder.
    
    As a special case a deferred probe request from the encoder is still
    propagated out of the probe function in order not to break the deferred
    probing mechanism.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>

--- drivers/gpu/drm/rcar-du/rcar_du_encoder.c
+++ drivers/gpu/drm/rcar-du/rcar_du_encoder.c
@@ -193,32 +193,46 @@ int rcar_du_encoder_init(struct rcar_du_device *rcdu,
 		if (renc->lvds) {
 			dev_err(rcdu->dev,
 				"Chaining LVDS and HDMI encoders not supported\n");
-			return -EINVAL;
+			ret = -EINVAL;
+			goto done;
 		}
 
 		ret = rcar_du_hdmienc_init(rcdu, renc, enc_node);
 		if (ret < 0)
-			return ret;
+			goto done;
 	} else {
 		ret = drm_encoder_init(rcdu->ddev, encoder, &encoder_funcs,
 				       encoder_type);
 		if (ret < 0)
-			return ret;
+			goto done;
 
 		drm_encoder_helper_add(encoder, &encoder_helper_funcs);
 	}
 
 	switch (encoder_type) {
 	case DRM_MODE_ENCODER_LVDS:
-		return rcar_du_lvds_connector_init(rcdu, renc, con_node);
+		ret = rcar_du_lvds_connector_init(rcdu, renc, con_node);
+		break;
 
 	case DRM_MODE_ENCODER_DAC:
-		return rcar_du_vga_connector_init(rcdu, renc);
+		ret = rcar_du_vga_connector_init(rcdu, renc);
+		break;
 
 	case DRM_MODE_ENCODER_TMDS:
-		return rcar_du_hdmi_connector_init(rcdu, renc);
+		ret = rcar_du_hdmi_connector_init(rcdu, renc);
+		break;
 
 	default:
-		return -EINVAL;
+		ret = -EINVAL;
+		break;
 	}
+
+done:
+	if (ret < 0) {
+		if (encoder->name)
+			encoder->funcs->destroy(encoder);
+		devm_kfree(rcdu->dev, renc);
+	}
+
+	return ret;
 }
