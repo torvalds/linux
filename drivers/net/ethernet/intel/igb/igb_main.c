124b74c18e0e31b24638d256afee7122a994e1b3
Alexander Duyck <alexander.h.duyck@redhat.com>
2014-12-11 15:02:28 -0800

    fm10k/igb/ixgbe: Use dma_rmb on Rx descriptor reads
    This change makes it so that dma_rmb is used when reading the Rx
    descriptor.  The advantage of dma_rmb is that it allows for a much
    lower cost barrier on x86, powerpc, arm, and arm64 architectures than a
    traditional memory barrier when dealing with reads that only have to
    synchronize to coherent memory.
    
    In addition I have updated the code so that it just checks to see if any
    bits have been set instead of just the DD bit since the DD bit will always
    be set as a part of a descriptor write-back so we just need to check for a
    non-zero value being present at that memory location rather than just
    checking for any specific bit.  This allows the code itself to appear much
    cleaner and allows the compiler more room to optimize.
    
    Cc: Matthew Vick <matthew.vick@intel.com>
    Cc: Don Skidmore <donald.c.skidmore@intel.com>
    Acked-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Alexander Duyck <alexander.h.duyck@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/intel/igb/igb_main.c
+++ drivers/net/ethernet/intel/igb/igb_main.c
@@ -6910,14 +6910,14 @@ static bool igb_clean_rx_irq(struct igb_q_vector *q_vector, const int budget)
 
 		rx_desc = IGB_RX_DESC(rx_ring, rx_ring->next_to_clean);
 
-		if (!igb_test_staterr(rx_desc, E1000_RXD_STAT_DD))
+		if (!rx_desc->wb.upper.status_error)
 			break;
 
 		/* This memory barrier is needed to keep us from reading
 		 * any other fields out of the rx_desc until we know the
-		 * RXD_STAT_DD bit is set
+		 * descriptor has been written back
 		 */
-		rmb();
+		dma_rmb();
 
 		/* retrieve a buffer from the ring */
 		skb = igb_fetch_rx_buffer(rx_ring, rx_desc, skb);
