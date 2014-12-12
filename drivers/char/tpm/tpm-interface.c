30fc8d138e9123f374a3c3867e7c7c5cd4004941
Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
2014-12-12 11:46:39 -0800

    tpm: TPM 2.0 CRB Interface
    tpm_crb is a driver for TPM 2.0 Command Response Buffer (CRB) Interface
    as defined in PC Client Platform TPM Profile (PTP) Specification.
    
    Only polling and single locality is supported as these are the limitations
    of the available hardware, Platform Trust Techonlogy (PTT) in Haswell
    CPUs.
    
    The driver always applies CRB with ACPI start because PTT reports using
    only ACPI start as start method but as a result of my testing it requires
    also CRB start.
    
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
    Reviewed-by: Jasob Gunthorpe <jason.gunthorpe@obsidianresearch.com>
    Reviewed-by: Stefan Berger <stefanb@linux.vnet.ibm.com>
    Signed-off-by: Peter Huewe <peterhuewe@gmx.de>

--- drivers/char/tpm/tpm-interface.c
+++ drivers/char/tpm/tpm-interface.c
@@ -901,6 +901,9 @@ int tpm_pm_suspend(struct device *dev)
 	if (chip == NULL)
 		return -ENODEV;
 
+	if (chip->flags & TPM_CHIP_FLAG_TPM2)
+		return tpm2_shutdown(chip, TPM2_SU_CLEAR);
+
 	/* for buggy tpm, flush pcrs with extend to selected dummy */
 	if (tpm_suspend_pcr) {
 		cmd.header.in = pcrextend_header;
