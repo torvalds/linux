bb04159df99fa353d0fb524574aca03ce2c6515b
Kirill Tkhai <ktkhai@parallels.com>
2014-12-15 14:56:58 +0300

    sched/fair: Fix sched_entity::avg::decay_count initialization
    Child has the same decay_count as parent. If it's not zero,
    we add it to parent's cfs_rq->removed_load:
    
    wake_up_new_task()->set_task_cpu()->migrate_task_rq_fair().
    
    Child's load is a just garbade after copying of parent,
    it hasn't been on cfs_rq yet, and it must not be added to
    cfs_rq::removed_load in migrate_task_rq_fair().
    
    The patch moves sched_entity::avg::decay_count intialization
    in sched_fork(). So, migrate_task_rq_fair() does not change
    removed_load.
    
    Signed-off-by: Kirill Tkhai <ktkhai@parallels.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Ben Segall <bsegall@google.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Link: http://lkml.kernel.org/r/1418644618.6074.13.camel@tkhai
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1832,6 +1832,9 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 	p->se.prev_sum_exec_runtime	= 0;
 	p->se.nr_migrations		= 0;
 	p->se.vruntime			= 0;
+#ifdef CONFIG_SMP
+	p->se.avg.decay_count		= 0;
+#endif
 	INIT_LIST_HEAD(&p->se.group_node);
 
 #ifdef CONFIG_SCHEDSTATS
--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -676,7 +676,6 @@ void init_task_runnable_average(struct task_struct *p)
 {
 	u32 slice;
 
-	p->se.avg.decay_count = 0;
 	slice = sched_slice(task_cfs_rq(p), &p->se) >> 10;
 	p->se.avg.runnable_avg_sum = slice;
 	p->se.avg.runnable_avg_period = slice;
