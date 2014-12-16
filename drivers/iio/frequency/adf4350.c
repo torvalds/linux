d9d0ac96554c21e40fd56982bc7f8f4bc557f16a
Roberta Dobrescu <roberta.dobrescu@gmail.com>
2014-12-16 11:11:45 +0200

    iio: frequency: Remove 'out of memory' message
    This patch fixes the following checkpatch.pl warning:
    WARNING: Possible unnecessary 'out of memory' message
    
    Signed-off-by: Roberta Dobrescu <roberta.dobrescu@gmail.com>
    Acked-by: Lars-Peter Clausen <lars@metafoo.de>
    Reviewed-by: Daniel Baluta <daniel.baluta@intel.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/frequency/adf4350.c
+++ drivers/iio/frequency/adf4350.c
@@ -387,10 +387,8 @@ static struct adf4350_platform_data *adf4350_parse_dt(struct device *dev)
 	int ret;
 
 	pdata = devm_kzalloc(dev, sizeof(*pdata), GFP_KERNEL);
-	if (!pdata) {
-		dev_err(dev, "could not allocate memory for platform data\n");
+	if (!pdata)
 		return NULL;
-	}
 
 	strncpy(&pdata->name[0], np->name, SPI_NAME_SIZE - 1);
 
