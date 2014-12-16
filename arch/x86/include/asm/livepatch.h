b700e7f03df5d92f85fa5247fe1f557528d3363d
Seth Jennings <sjenning@redhat.com>
2014-12-16 11:58:19 -0600

    livepatch: kernel: add support for live patching
    This commit introduces code for the live patching core.  It implements
    an ftrace-based mechanism and kernel interface for doing live patching
    of kernel and kernel module functions.
    
    It represents the greatest common functionality set between kpatch and
    kgraft and can accept patches built using either method.
    
    This first version does not implement any consistency mechanism that
    ensures that old and new code do not run together.  In practice, ~90% of
    CVEs are safe to apply in this way, since they simply add a conditional
    check.  However, any function change that can not execute safely with
    the old version of the function can _not_ be safely applied in this
    version.
    
    [ jkosina@suse.cz: due to the number of contributions that got folded into
      this original patch from Seth Jennings, add SUSE's copyright as well, as
      discussed via e-mail ]
    
    Signed-off-by: Seth Jennings <sjenning@redhat.com>
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Reviewed-by: Miroslav Benes <mbenes@suse.cz>
    Reviewed-by: Petr Mladek <pmladek@suse.cz>
    Reviewed-by: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Signed-off-by: Miroslav Benes <mbenes@suse.cz>
    Signed-off-by: Petr Mladek <pmladek@suse.cz>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

---
+++ arch/x86/include/asm/livepatch.h
@@ -0,0 +1,37 @@
+/*
+ * livepatch.h - x86-specific Kernel Live Patching Core
+ *
+ * Copyright (C) 2014 Seth Jennings <sjenning@redhat.com>
+ * Copyright (C) 2014 SUSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _ASM_X86_LIVEPATCH_H
+#define _ASM_X86_LIVEPATCH_H
+
+#include <linux/module.h>
+
+#ifdef CONFIG_LIVE_PATCHING
+#ifndef CC_USING_FENTRY
+#error Your compiler must support -mfentry for live patching to work
+#endif
+extern int klp_write_module_reloc(struct module *mod, unsigned long type,
+				  unsigned long loc, unsigned long value);
+
+#else
+#error Live patching support is disabled; check CONFIG_LIVE_PATCHING
+#endif
+
+#endif /* _ASM_X86_LIVEPATCH_H */
