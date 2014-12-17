d034f53cc6c5c09a4e10cb9716986f3844c58514
Geert Uytterhoeven <geert+renesas@glider.be>
2014-12-17 14:40:42 +0100

    ARM: shmobile: r8a7740: Instantiate GIC from C board code in legacy builds
    As of commit 9a1091ef0017c40a ("irqchip: gic: Support hierarchy irq
    domain."), armadillo-legacy hangs during boot with:
    
         sh-tmu.0: ch0: used for clock events
         sh-tmu.0: ch0: used for periodic clock events
         sh-tmu.0: ch0: failed to request irq 230
         sh-tmu.0: ch1: used as clock source
         sh-cmt-48.1: ch0: failed to request irq 90
         sh-cmt-48.1: ch0: registration failed
        earlytimer: unable to probe sh-cmt-48 early.
        Calibrating delay loop...
    
    This happens because the IRQ numbers of the GIC are now virtual, and no
    longer match the hardcoded hardware IRQ numbers in the platform board
    code.
    To fix this, instantiate the GIC from platform board code when compiling
    a legacy kernel, like is done on sh73a0 for kzm9g-legacy.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Acked-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/setup-r8a7740.c
+++ arch/arm/mach-shmobile/setup-r8a7740.c
@@ -793,7 +793,14 @@ void __init r8a7740_init_irq_of(void)
 	void __iomem *intc_msk_base = ioremap_nocache(0xe6900040, 0x10);
 	void __iomem *pfc_inta_ctrl = ioremap_nocache(0xe605807c, 0x4);
 
+#ifdef CONFIG_ARCH_SHMOBILE_LEGACY
+	void __iomem *gic_dist_base = ioremap_nocache(0xc2800000, 0x1000);
+	void __iomem *gic_cpu_base = ioremap_nocache(0xc2000000, 0x1000);
+
+	gic_init(0, 29, gic_dist_base, gic_cpu_base);
+#else
 	irqchip_init();
+#endif
 
 	/* route signals to GIC */
 	iowrite32(0x0, pfc_inta_ctrl);
