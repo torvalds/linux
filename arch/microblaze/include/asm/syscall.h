8543e6c96762fcc930af5725088c2b9e4865c3aa
Michal Simek <michal.simek@xilinx.com>
2014-12-18 15:41:13 +0100

    microblaze: Use unsigned return type in do_syscall_trace_enter
    Registers are not signed types.
    
    The patch removes warnings:
    arch/microblaze/kernel/ptrace.c: In function 'do_syscall_trace_enter':
    arch/microblaze/kernel/ptrace.c:152:14: warning: signed and unsigned
    type in conditional expression [-Wsign-compare]
      return ret ?: regs->r12;
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/microblaze/include/asm/syscall.h
+++ arch/microblaze/include/asm/syscall.h
@@ -97,7 +97,7 @@ static inline void syscall_set_arguments(struct task_struct *task,
 		microblaze_set_syscall_arg(regs, i++, *args++);
 }
 
-asmlinkage long do_syscall_trace_enter(struct pt_regs *regs);
+asmlinkage unsigned long do_syscall_trace_enter(struct pt_regs *regs);
 asmlinkage void do_syscall_trace_leave(struct pt_regs *regs);
 
 static inline int syscall_get_arch(void)
