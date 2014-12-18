fe89707f0afa17edbb1bf8381fea1d2457aeebaa
Troy Tan <troy_tan@realsil.com.cn>
2014-12-18 03:05:25 -0600

    rtlwifi: rtl8821ae: Simplify loading of WOWLAN firmware
    The existing method for loading both normal and WOWLAN firmware for the
    device duplicates a lot of code. This solution is much cleaner.
    
    Signed-off-by: Troy Tan <troy_tan@realsil.com.cn>
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/rtlwifi/wifi.h
+++ drivers/net/wireless/rtlwifi/wifi.h
@@ -2242,6 +2242,7 @@ struct rtl_hal_cfg {
 	char *name;
 	char *fw_name;
 	char *alt_fw_name;
+	char *wowlan_fw_name;
 	struct rtl_hal_ops *ops;
 	struct rtl_mod_params *mod_params;
 	struct rtl_hal_usbint_cfg *usb_interface_cfg;
@@ -2518,8 +2519,6 @@ struct proxim {
 
 struct rtl_priv {
 	struct ieee80211_hw *hw;
-	/* Used to load a second firmware */
-	void (*rtl_fw_second_cb)(struct rtl_priv *rtlpriv);
 	struct completion firmware_loading_complete;
 	struct list_head list;
 	struct rtl_priv *buddy_priv;
