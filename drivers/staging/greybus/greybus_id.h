0e51032f381738e3e854febc627936e1c4d8deda
Greg Kroah-Hartman <greg@kroah.com>
2014-12-19 14:56:34 -0800

    greybus: greybus_id.h: minor typo fixes
    Align up the BIT() #defines and properly comment the include block
    define.
    
    Reviewed-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/greybus_id.h
+++ drivers/staging/greybus/greybus_id.h
@@ -20,7 +20,7 @@ struct greybus_interface_id {
 
 /* Used to match the greybus_interface_id */
 #define GREYBUS_ID_MATCH_VENDOR		BIT(0)
-#define GREYBUS_ID_MATCH_PRODUCT		BIT(1)
+#define GREYBUS_ID_MATCH_PRODUCT	BIT(1)
 #define GREYBUS_ID_MATCH_SERIAL		BIT(2)
 
-#endif /* __LINUX_GREYBUS_H */
+#endif /* __LINUX_GREYBUS_ID_H */
