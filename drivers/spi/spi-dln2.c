b2e5dda14fbc2ca1155c614c500c6de6f5b4ba8c
Mark Brown <broonie@kernel.org>
2014-12-23 00:27:41 +0000

    spi/dln2: Fix for PM_RUNTIME removal
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-dln2.c
+++ drivers/spi/spi-dln2.c
@@ -850,7 +850,7 @@ static int dln2_spi_resume(struct device *dev)
 }
 #endif /* CONFIG_PM_SLEEP */
 
-#ifdef CONFIG_PM_RUNTIME
+#ifdef CONFIG_PM
 static int dln2_spi_runtime_suspend(struct device *dev)
 {
 	struct spi_master *master = dev_get_drvdata(dev);
