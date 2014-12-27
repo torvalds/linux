00647dbf5150fa2f3279d9be534cb06e24d537f4
Chris Wilson <chris@chris-wilson.co.uk>
2014-12-27 09:48:28 +0000

    drm/i915: Ban Haswell from using RCS flips
    commit 48bf5b2d00bfeb681f6500c626189c7cd2c964d2 upstream.
    
    Like Ivybridge, we have reports that we get random hangs when flipping
    with multiple pipes. Extend
    
    commit 2a92d5bca1999b69c78f3c3e97b5484985b094b9
    Author: Chris Wilson <chris@chris-wilson.co.uk>
    Date:   Tue Jul 8 10:40:29 2014 +0100
    
        drm/i915: Disable RCS flips on Ivybridge
    
    to also apply to Haswell.
    
    Reported-and-tested-by: Scott Tsai <scottt.tw@gmail.com>
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=87759
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -10019,7 +10019,7 @@ static int intel_crtc_page_flip(struct drm_crtc *crtc,
 		if (obj->tiling_mode != work->old_fb_obj->tiling_mode)
 			/* vlv: DISPLAY_FLIP fails to change tiling */
 			ring = NULL;
-	} else if (IS_IVYBRIDGE(dev)) {
+	} else if (IS_IVYBRIDGE(dev) || IS_HASWELL(dev)) {
 		ring = &dev_priv->ring[BCS];
 	} else if (INTEL_INFO(dev)->gen >= 7) {
 		ring = obj->ring;
