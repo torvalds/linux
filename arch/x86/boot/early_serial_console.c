b34630014dad0ba69aadd8deb231ddc6d2efcf53
Alexander Kuleshov <kuleshovmail@gmail.com>
2014-12-31 13:12:38 +0600

    x86, early_serial_console: Remove unnecessary check
    We do this check already a couple of lines up.
    
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>
    Link: http://lkml.kernel.org/r/1420009958-4803-1-git-send-email-kuleshovmail@gmail.com
    Signed-off-by: Borislav Petkov <bp@suse.de>

--- arch/x86/boot/early_serial_console.c
+++ arch/x86/boot/early_serial_console.c
@@ -72,8 +72,8 @@ static void parse_earlyprintk(void)
 			static const int bases[] = { 0x3f8, 0x2f8 };
 			int idx = 0;
 
-			if (!strncmp(arg + pos, "ttyS", 4))
-				pos += 4;
+			/* += strlen("ttyS"); */
+			pos += 4;
 
 			if (arg[pos++] == '1')
 				idx = 1;
