03305e535cd5cdc1079b32909bf4b2dd67d46f7f
Stefan Wahren <stefan.wahren@i2se.com>
2015-01-03 20:34:12 +0000

    iio: mxs-lradc: fix iio channel map regression
    Since commit c8231a9af8147f8a ("iio: mxs-lradc: compute temperature
    from channel 8 and 9") with the removal of adc channel 9 there is
    no 1-1 mapping in the channel spec.
    
    All hwmon channel values above 9 are accessible via there index minus
    one. So add a hidden iio channel 9 to fix this issue.
    
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>
    Acked-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Reviewed-by: Marek Vasut <marex@denx.de>
    Cc: Stable@vger.kernel.org
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/adc/mxs-lradc.c
+++ drivers/staging/iio/adc/mxs-lradc.c
@@ -1397,6 +1397,13 @@ static const struct iio_chan_spec mxs_lradc_chan_spec[] = {
 		.channel = 8,
 		.scan_type = {.sign = 'u', .realbits = 18, .storagebits = 32,},
 	},
+	/* Hidden channel to keep indexes */
+	{
+		.type = IIO_TEMP,
+		.indexed = 1,
+		.scan_index = -1,
+		.channel = 9,
+	},
 	MXS_ADC_CHAN(10, IIO_VOLTAGE),	/* VDDIO */
 	MXS_ADC_CHAN(11, IIO_VOLTAGE),	/* VTH */
 	MXS_ADC_CHAN(12, IIO_VOLTAGE),	/* VDDA */
