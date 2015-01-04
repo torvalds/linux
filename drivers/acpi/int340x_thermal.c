48628e4015dbd77b676c5a9b5bd145a284f8bb93
Zhang Rui <rui.zhang@intel.com>
2015-01-04 14:24:23 +0800

    ACPI/int340x_thermal: enumerate INT340X devices even if they're not in _ART/_TRT
    For some INT340X thermal devices, even if they are not referred in
    _TRT/_ART table, they still can be used by userspace for thermal control.
    Thus change the code to enumerated all the INT340X devices,
    no matter if they're referred in _TRT/_ART or not.
    
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    Tested-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>

--- drivers/acpi/int340x_thermal.c
+++ drivers/acpi/int340x_thermal.c
@@ -14,9 +14,8 @@
 
 #include "internal.h"
 
-#define DO_ENUMERATION 0x01
 static const struct acpi_device_id int340x_thermal_device_ids[] = {
-	{"INT3400", DO_ENUMERATION },
+	{"INT3400"},
 	{"INT3401"},
 	{"INT3402"},
 	{"INT3403"},
@@ -34,8 +33,7 @@ static int int340x_thermal_handler_attach(struct acpi_device *adev,
 					const struct acpi_device_id *id)
 {
 #if defined(CONFIG_INT340X_THERMAL) || defined(CONFIG_INT340X_THERMAL_MODULE)
-	if (id->driver_data == DO_ENUMERATION)
-		acpi_create_platform_device(adev);
+	acpi_create_platform_device(adev);
 #endif
 	return 1;
 }
