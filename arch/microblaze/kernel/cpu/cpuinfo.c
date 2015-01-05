ed89466f2368fc15c72ce5344d582c640dcf53a6
Michal Simek <michal.simek@xilinx.com>
2015-01-05 12:01:17 +0100

    microblaze: Add target architecture
    Add missing target architectures - virtex7, ultrascale virtex
    and ultrascale kintex.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/microblaze/kernel/cpu/cpuinfo.c
+++ arch/microblaze/kernel/cpu/cpuinfo.c
@@ -65,11 +65,14 @@ const struct family_string_key family_string_lookup[] = {
 	{"spartan3adsp", 0xc},
 	{"spartan6", 0xd},
 	{"virtex6", 0xe},
+	{"virtex7", 0xf},
 	/* FIXME There is no key code defined for spartan2 */
 	{"spartan2", 0xf0},
 	{"kintex7", 0x10},
 	{"artix7", 0x11},
 	{"zynq7000", 0x12},
+	{"UltraScale Virtex", 0x13},
+	{"UltraScale Kintex", 0x14},
 	{NULL, 0},
 };
 
