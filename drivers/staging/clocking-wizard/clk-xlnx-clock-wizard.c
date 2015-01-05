19f15a3000a6ede8c3fcd2735225b894002a7cc1
Soren Brinkmann <soren.brinkmann@xilinx.com>
2015-01-05 09:21:29 -0800

    staging: clocking-wizard: Handle invalid clk in notifier
    Even though it should never happen, handle the case that the clock
    notifier is called with an unexpected clock handle. This avoids the
    compiler warning:
       drivers/staging/clocking-wizard/clk-xlnx-clock-wizard.c: In function 'clk_wzrd_clk_notifier':
    >> drivers/staging/clocking-wizard/clk-xlnx-clock-wizard.c:99:6: warning: 'max' may be used uninitialized in this function [-Wmaybe-uninitialized]
          if (ndata->new_rate > max)
             ^
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/clocking-wizard/clk-xlnx-clock-wizard.c
+++ drivers/staging/clocking-wizard/clk-xlnx-clock-wizard.c
@@ -91,8 +91,10 @@ static int clk_wzrd_clk_notifier(struct notifier_block *nb, unsigned long event,
 
 	if (ndata->clk == clk_wzrd->clk_in1)
 		max = clk_wzrd_max_freq[clk_wzrd->speed_grade - 1];
-	if (ndata->clk == clk_wzrd->axi_clk)
+	else if (ndata->clk == clk_wzrd->axi_clk)
 		max = WZRD_ACLK_MAX_FREQ;
+	else
+		return NOTIFY_DONE;	/* should never happen */
 
 	switch (event) {
 	case PRE_RATE_CHANGE:
