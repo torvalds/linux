c960f9b22d63934401949fb20d10ec4e69b535b2
Viresh Kumar <viresh.kumar@linaro.org>
2015-01-06 21:09:12 +0530

    cpufreq: stats: time_in_state can't be NULL in cpufreq_stats_update()
    'time_in_state' can't be NULL if 'stats' is valid. These are allocated together
    and only if time_in_state is allocated successfully, we update policy->stats.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -35,9 +35,7 @@ static int cpufreq_stats_update(struct cpufreq_stats *stats)
 	unsigned long long cur_time = get_jiffies_64();
 
 	spin_lock(&cpufreq_stats_lock);
-	if (stats->time_in_state)
-		stats->time_in_state[stats->last_index] +=
-			cur_time - stats->last_time;
+	stats->time_in_state[stats->last_index] += cur_time - stats->last_time;
 	stats->last_time = cur_time;
 	spin_unlock(&cpufreq_stats_lock);
 	return 0;
