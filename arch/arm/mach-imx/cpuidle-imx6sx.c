c8aeb7dfe6e815906c3d9c23ce17a00e8d01b3d5
Shawn Guo <shawn.guo@linaro.org>
2015-01-06 20:06:16 +0800

    ARM: imx: drop CPUIDLE_FLAG_TIME_VALID from cpuidle-imx6sx
    As the result of commit b82b6cca4880 ("cpuidle: Invert
    CPUIDLE_FLAG_TIME_VALID logic"), the flag gets removed and hence we see
    the compile error below.
    
      CC      arch/arm/mach-imx/cpuidle-imx6sx.o
    arch/arm/mach-imx/cpuidle-imx6sx.c:69:13: error: ‘CPUIDLE_FLAG_TIME_VALID’ undeclared here (not in a function)
    
    Since the behavior of the original flag has been the default, we can
    simply drop the flag now.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- arch/arm/mach-imx/cpuidle-imx6sx.c
+++ arch/arm/mach-imx/cpuidle-imx6sx.c
@@ -66,8 +66,7 @@ static struct cpuidle_driver imx6sx_cpuidle_driver = {
 		{
 			.exit_latency = 50,
 			.target_residency = 75,
-			.flags = CPUIDLE_FLAG_TIME_VALID |
-				CPUIDLE_FLAG_TIMER_STOP,
+			.flags = CPUIDLE_FLAG_TIMER_STOP,
 			.enter = imx6sx_enter_wait,
 			.name = "WAIT",
 			.desc = "Clock off",
@@ -81,7 +80,6 @@ static struct cpuidle_driver imx6sx_cpuidle_driver = {
 			 */
 			.exit_latency = 300,
 			.target_residency = 500,
-			.flags = CPUIDLE_FLAG_TIME_VALID,
 			.enter = imx6sx_enter_wait,
 			.name = "LOW-POWER-IDLE",
 			.desc = "ARM power off",
