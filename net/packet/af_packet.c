eee2f04b801ebc95da16f91fef029a083ba1a216
Willem de Bruijn <willemb@google.com>
2015-01-08 11:29:18 -0500

    packet: make packet too small warning match condition
    The expression in ll_header_truncated() tests less than or equal, but
    the warning prints less than. Update the warning.
    
    Reported-by: Jouni Malinen <jkmalinen@gmail.com>
    Signed-off-by: Willem de Bruijn <willemb@google.com>
    Acked-by: Daniel Borkmann <dborkman@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -2102,7 +2102,7 @@ static bool ll_header_truncated(const struct net_device *dev, int len)
 {
 	/* net device doesn't like empty head */
 	if (unlikely(len <= dev->hard_header_len)) {
-		net_warn_ratelimited("%s: packet size is too short (%d < %d)\n",
+		net_warn_ratelimited("%s: packet size is too short (%d <= %d)\n",
 				     current->comm, len, dev->hard_header_len);
 		return true;
 	}
