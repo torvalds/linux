eb130dbfc40eabcd4e10797310bda6b9f6dd7e76
Keith Busch <keith.busch@intel.com>
2015-01-08 08:59:53 -0700

    blk-mq: End unstarted requests on a dying queue
    Requests that haven't been started prior to a queue dying can be ended
    in error without waiting for them to start and time out.
    
    Signed-off-by: Keith Busch <keith.busch@intel.com>
    
    Added code comment to explain why this is done.
    
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -625,8 +625,17 @@ static void blk_mq_check_expired(struct blk_mq_hw_ctx *hctx,
 {
 	struct blk_mq_timeout_data *data = priv;
 
-	if (!test_bit(REQ_ATOM_STARTED, &rq->atomic_flags))
+	if (!test_bit(REQ_ATOM_STARTED, &rq->atomic_flags)) {
+		/*
+		 * If a request wasn't started before the queue was
+		 * marked dying, kill it here or it'll go unnoticed.
+		 */
+		if (unlikely(blk_queue_dying(rq->q))) {
+			rq->errors = -EIO;
+			blk_mq_complete_request(rq);
+		}
 		return;
+	}
 	if (rq->cmd_flags & REQ_NO_TIMEOUT)
 		return;
 
