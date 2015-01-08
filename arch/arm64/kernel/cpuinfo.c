5d425c18653731af62831d30a4fa023d532657a9
Sudeep Holla <sudeep.holla@arm.com>
2015-01-08 10:42:34 +0000

    arm64: kernel: add support for cpu cache information
    This patch adds support for cacheinfo on ARM64.
    
    On ARMv8, the cache hierarchy can be identified through Cache Level ID
    (CLIDR) register while the cache geometry is provided by Cache Size ID
    (CCSIDR) register.
    
    Since the architecture doesn't provide any way of detecting the cpus
    sharing particular cache, device tree is used for the same purpose.
    
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/kernel/cpuinfo.c
+++ arch/arm64/kernel/cpuinfo.c
@@ -231,15 +231,3 @@ void __init cpuinfo_store_boot_cpu(void)
 
 	boot_cpu_data = *info;
 }
-
-u64 __attribute_const__ icache_get_ccsidr(void)
-{
-	u64 ccsidr;
-
-	WARN_ON(preemptible());
-
-	/* Select L1 I-cache and read its size ID register */
-	asm("msr csselr_el1, %1; isb; mrs %0, ccsidr_el1"
-	    : "=r"(ccsidr) : "r"(1L));
-	return ccsidr;
-}
