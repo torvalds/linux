5479eb3e05609733bb34446a6f381deae7b7dac5
xxh <xxh@rock-chips.com>
2015-01-09 17:38:08 +0800

    3.10 kernel support more Huawei 3G dongles

--- drivers/usb/storage/initializers.h
+++ drivers/usb/storage/initializers.h
@@ -48,3 +48,5 @@ int usb_stor_ucr61s2b_init(struct us_data *us);
 
 /* This places the HUAWEI E220 devices in multi-port mode */
 int usb_stor_huawei_e220_init(struct us_data *us);
+
+int usb_stor_huawei_init(struct us_data *us);
--- drivers/usb/storage/unusual_devs.h
+++ drivers/usb/storage/unusual_devs.h
@@ -1908,6 +1908,12 @@ UNUSUAL_DEV(  0x12d1, 0x143F, 0x0000, 0x0000,
 		USB_SC_DEVICE, USB_PR_DEVICE, usb_stor_huawei_e220_init,
 		0),
 
+UNUSUAL_VENDOR_INTF(0x12d1, 0x08, 0x06, 0x50,
+                 "HUAWEI MOBILE",
+                 "Mass Storage",
+                 USB_SC_DEVICE, USB_PR_DEVICE, usb_stor_huawei_init,
+                 0),
+
 /* Reported by Vilius Bilinkevicius <vilisas AT xxx DOT lt) */
 UNUSUAL_DEV(  0x132b, 0x000b, 0x0001, 0x0001,
 		"Minolta",
