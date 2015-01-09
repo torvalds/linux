04bf1c7f76b05be8134a833ec023f1c96f81b8a1
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2015-01-09 09:27:57 +0100

    PM / OPP: Assert RCU lock in exported functions
    Add lockdep asserts for holding the RCU lock when calling
    dev_pm_opp_get_freq() and dev_pm_opp_get_voltage() to aid in detecting
    RCU misuses.
    
    These are called often after dev_pm_opp_find_freq_ceil/exact() which
    already asserts for RCU lock. However one could make an error by
    releasing lock too early - just after dev_pm_opp_find_freq_ceil().
    
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/base/power/opp.c
+++ drivers/base/power/opp.c
@@ -169,6 +169,8 @@ unsigned long dev_pm_opp_get_voltage(struct dev_pm_opp *opp)
 	struct dev_pm_opp *tmp_opp;
 	unsigned long v = 0;
 
+	opp_rcu_lockdep_assert();
+
 	tmp_opp = rcu_dereference(opp);
 	if (unlikely(IS_ERR_OR_NULL(tmp_opp)) || !tmp_opp->available)
 		pr_err("%s: Invalid parameters\n", __func__);
@@ -198,6 +200,8 @@ unsigned long dev_pm_opp_get_freq(struct dev_pm_opp *opp)
 {
 	struct dev_pm_opp *tmp_opp;
 	unsigned long f = 0;
+
+	opp_rcu_lockdep_assert();
 
 	tmp_opp = rcu_dereference(opp);
 	if (unlikely(IS_ERR_OR_NULL(tmp_opp)) || !tmp_opp->available)
