12c243b833c2c441f46dc592ab410fdccb5e056b
Andy Shevchenko <andy.shevchenko@gmail.com>
2015-01-10 16:57:45 +0200

    ALSA: fm801: remove FSF address
    The FSF address is subject to change. So, we remove it from the header.
    
    While here, remove a line with my old and non-functional email from the
    header. Moreover, nowadays we have a Git VCS that contains a history of
    changes.
    
    Signed-off-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/fm801.c
+++ sound/pci/fm801.c
@@ -2,8 +2,6 @@
  *  The driver for the ForteMedia FM801 based soundcards
  *  Copyright (c) by Jaroslav Kysela <perex@perex.cz>
  *
- *  Support FM only card by Andy Shevchenko <andy@smile.org.ua>
- *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 2 of the License, or
@@ -13,10 +11,6 @@
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
  */
 
