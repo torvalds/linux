487d60cc09237d24f93ec2892cd8437ae689d7e0
Peter Chen <peter.chen@freescale.com>
2015-01-12 16:37:57 +0800

    usb: gadget: mv_udc_core: delete pullup operation at .udc_start
    UDC core has already done it after .udc_start.
    
    Signed-off-by: Peter Chen <peter.chen@freescale.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/udc/mv_udc_core.c
+++ drivers/usb/gadget/udc/mv_udc_core.c
@@ -1378,9 +1378,6 @@ static int mv_udc_start(struct usb_gadget *gadget,
 		}
 	}
 
-	/* pullup is always on */
-	mv_udc_pullup(&udc->gadget, 1);
-
 	/* When boot with cable attached, there will be no vbus irq occurred */
 	if (udc->qwork)
 		queue_work(udc->qwork, &udc->vbus_work);
