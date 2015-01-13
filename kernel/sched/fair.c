ccfcc4ed08c51b800a829053567af1ab661f4a07
Morten Rasmussen <morten.rasmussen@arm.com>
2015-01-13 14:11:28 +0000

    sched: Compute cpu capacity available at current frequency
    capacity_orig_of() returns the max available compute capacity of a cpu.
    For scale-invariant utilization tracking and energy-aware scheduling
    decisions it is useful to know the compute capacity available at the
    current OPP of a cpu.
    
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4672,6 +4672,17 @@ static long effective_load(struct task_group *tg, int cpu, long wl, long wg)
 
 #endif
 
+/*
+ * Returns the current capacity of cpu after applying both
+ * cpu and freq scaling.
+ */
+static unsigned long capacity_curr_of(int cpu)
+{
+	return cpu_rq(cpu)->cpu_capacity_orig *
+	       arch_scale_freq_capacity(NULL, cpu)
+	       >> SCHED_CAPACITY_SHIFT;
+}
+
 static inline bool energy_aware(void)
 {
 	return sched_feat(ENERGY_AWARE);
