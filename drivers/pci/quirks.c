6a3763d1734bf133330dc8e246bf794b9e360e8a
Vasundhara Volam <vasundhara.volam@emulex.com>
2015-01-13 01:22:23 -0500

    PCI: Add ACS quirk for Emulex NICs
    As Skyhawk and BE3-R (both multi-function devices) don't advertise the
    PCI-ACS capability, the vfio driver places all the functions of these
    devices in a single IOMMU group.  Attaching (via PCI-passthru) two
    different Skyhawk/BE3-R partitions (nPAR, Flex, etc. PFs) using vfio, to
    different guests doesn't work as vfio only allows functions in *different*
    IOMMU groups to be assigned to different guests.
    
    As peer-to-peer access between PFs in Skyhawk/BE3-R is not possible, we can
    treat them as "fully isolated" even though the device doesn't advertise
    ACS.  Add a PCI quirk for Skyhawk and BE3-R chips to fix this problem.
    
    Signed-off-by: Vasundhara Volam <vasundhara.volam@emulex.com>
    Signed-off-by: Sathya Perla <sathya.perla@emulex.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Alex Williamson <alex.williamson@redhat.com>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -3748,6 +3748,8 @@ static const struct pci_dev_acs_enabled {
 	{ PCI_VENDOR_ID_INTEL, 0x1551, pci_quirk_mf_endpoint_acs },
 	{ PCI_VENDOR_ID_INTEL, 0x1558, pci_quirk_mf_endpoint_acs },
 	{ PCI_VENDOR_ID_INTEL, PCI_ANY_ID, pci_quirk_intel_pch_acs },
+	{ 0x19a2, 0x710, pci_quirk_mf_endpoint_acs }, /* Emulex BE3-R */
+	{ 0x10df, 0x720, pci_quirk_mf_endpoint_acs }, /* Emulex Skyhawk-R */
 	{ 0 }
 };
 
