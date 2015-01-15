6321b54a43e2dd3e2635f4a7cb7e4d83fd68dfe1
Michael Schmitz <schmitzmic@gmail.com>
2015-01-15 14:06:15 +0100

    net: smc91x: Add Atari EtherNAT support
    Add Atari specific code to the smc91x Ethernet driver. This code is used
    on the EtherNAT adapter card for the Atari Falcon extension port.
    
    Signed-off-by: Michael Schmitz <schmitz@debian.org>
    Tested-by: Christian Steigies <cts@debian.org>
    [geert: Sort Kconfig entries, split in hard and soft dependencies]
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Acked-by: Nicolas Pitre <nico@fluxnic.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/smsc/smc91x.h
+++ drivers/net/ethernet/smsc/smc91x.h
@@ -216,6 +216,27 @@ SMC_outw(u16 val, void __iomem *ioaddr, int reg)
 
 #include <unit/smc91111.h>
 
+#elif defined(CONFIG_ATARI)
+
+#define SMC_CAN_USE_8BIT        1
+#define SMC_CAN_USE_16BIT       1
+#define SMC_CAN_USE_32BIT       1
+#define SMC_NOWAIT              1
+
+#define SMC_inb(a, r)           readb((a) + (r))
+#define SMC_inw(a, r)           readw((a) + (r))
+#define SMC_inl(a, r)           readl((a) + (r))
+#define SMC_outb(v, a, r)       writeb(v, (a) + (r))
+#define SMC_outw(v, a, r)       writew(v, (a) + (r))
+#define SMC_outl(v, a, r)       writel(v, (a) + (r))
+#define SMC_insw(a, r, p, l)    readsw((a) + (r), p, l)
+#define SMC_outsw(a, r, p, l)   writesw((a) + (r), p, l)
+#define SMC_insl(a, r, p, l)    readsl((a) + (r), p, l)
+#define SMC_outsl(a, r, p, l)   writesl((a) + (r), p, l)
+
+#define RPC_LSA_DEFAULT         RPC_LED_100_10
+#define RPC_LSB_DEFAULT         RPC_LED_TX_RX
+
 #elif defined(CONFIG_ARCH_MSM)
 
 #define SMC_CAN_USE_8BIT	0
