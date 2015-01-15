2179aabe0122b476a3bd9f5e0716d86829f71638
Roberta Dobrescu <roberta.dobrescu@gmail.com>
2015-01-16 00:24:14 +0200

    iio: industrialio-event: Fix typo 's/dynically/dynamically/'
    Signed-off-by: Roberta Dobrescu <roberta.dobrescu@gmail.com>
    Reviewed-by: Daniel Baluta <daniel.baluta@intel.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/industrialio-event.c
+++ drivers/iio/industrialio-event.c
@@ -411,7 +411,7 @@ static inline int __iio_add_event_config_attrs(struct iio_dev *indio_dev)
 {
 	int j, ret, attrcount = 0;
 
-	/* Dynically created from the channels array */
+	/* Dynamically created from the channels array */
 	for (j = 0; j < indio_dev->num_channels; j++) {
 		ret = iio_device_add_event_sysfs(indio_dev,
 						 &indio_dev->channels[j]);
