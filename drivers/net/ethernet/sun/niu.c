ff8b335610cac5509156f28e03355c895bcc94f5
Eric Dumazet <edumazet@google.com>
2015-01-16 05:39:30 -0800

    niu: remove one compound_head() call
    After a "page = alloc_page(mask);", we do not need to use
    compound_head() : page already points to the right place.
    
    This would be true even if using alloc_pages().
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/sun/niu.c
+++ drivers/net/ethernet/sun/niu.c
@@ -3341,8 +3341,7 @@ static int niu_rbr_add_page(struct niu *np, struct rx_ring_info *rp,
 
 	niu_hash_page(rp, page, addr);
 	if (rp->rbr_blocks_per_page > 1)
-		atomic_add(rp->rbr_blocks_per_page - 1,
-			   &compound_head(page)->_count);
+		atomic_add(rp->rbr_blocks_per_page - 1, &page->_count);
 
 	for (i = 0; i < rp->rbr_blocks_per_page; i++) {
 		__le32 *rbr = &rp->rbr[start_index + i];
