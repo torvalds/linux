52d50ca4efe09f5bbbdf48f65da637cdb6585497
Triton64 <ianstevenson15@gmail.com>
2015-01-16 19:36:58 -0700

    Moving index in a more precise scope.

--- sound/last.c
+++ sound/last.c
@@ -24,10 +24,10 @@
 
 static int __init alsa_sound_last_init(void)
 {
-	int idx, ok = 0;
+	int ok = 0;
 	
 	printk(KERN_INFO "ALSA device list:\n");
-	for (idx = 0; idx < SNDRV_CARDS; idx++)
+	for (int idx = 0; idx < SNDRV_CARDS; idx++)
 		if (snd_cards[idx] != NULL) {
 			printk(KERN_INFO "  #%i: %s\n", idx, snd_cards[idx]->longname);
 			ok++;
