528012c1f4379738c6307b273d20cc3caa7d08af
Lukasz Majewski <l.majewski@samsung.com>
2015-01-19 12:44:04 +0100

    thermal: of: Enable thermal_zoneX when sensor is correctly added
    Up till now the thermal_zone mode was by default "disabled". With this
    patch the default behavior was changed to "enable".
    
    One can read the mode at:
    /sys/class/thermal/thermal_zone0/mode
    
    Tested-by: Javi Merino <javi.merino@arm.com>
    Reported-by: Abhilash Kesavan <a.kesavan@samsung.com>
    Signed-off-by: Lukasz Majewski <l.majewski@samsung.com>
    Signed-off-by: Eduardo Valentin <edubezval@gmail.com>

--- drivers/thermal/of-thermal.c
+++ drivers/thermal/of-thermal.c
@@ -497,6 +497,9 @@ thermal_zone_of_sensor_register(struct device *dev, int sensor_id, void *data,
 		if (sensor_specs.np == sensor_np && id == sensor_id) {
 			tzd = thermal_zone_of_add_sensor(child, sensor_np,
 							 data, ops);
+			if (!IS_ERR(tzd))
+				tzd->ops->set_mode(tzd, THERMAL_DEVICE_ENABLED);
+
 			of_node_put(sensor_specs.np);
 			of_node_put(child);
 			goto exit;
