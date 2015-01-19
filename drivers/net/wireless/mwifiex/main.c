ea2325b89d16c0803714d48e28b116aec2ef5026
Johannes Berg <johannes@sipsolutions.net>
2015-01-19 15:54:36 +0530

    mwifiex: set netif carrier off in ndo_open
    This patch adds fix to set carrier state off during ndo_open.
    Carrier should be set to ON when device is ready to send data.
    In case of station/adhoc interface device is able to transfer
    data after successful association/join operation.
    For AP this would be after bss_active event.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: Avinash Patil <patila@marvell.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/mwifiex/main.c
+++ drivers/net/wireless/mwifiex/main.c
@@ -562,7 +562,8 @@ static int mwifiex_init_hw_fw(struct mwifiex_adapter *adapter)
 static int
 mwifiex_open(struct net_device *dev)
 {
-	netif_tx_start_all_queues(dev);
+	netif_carrier_off(dev);
+
 	return 0;
 }
 
