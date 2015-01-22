b4810773754fe8371d17b1be49b1c15e8d9d4547
Dan Carpenter <dan.carpenter@oracle.com>
2015-01-22 08:20:16 -0800

    Input: cyapa - off by one in cyapa_update_fw_store()
    If "(count == NAME_MAX)" then we could end up putting the NUL terminator
    one space beyond the end of the fw_name[] array.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/mouse/cyapa.c
+++ drivers/input/mouse/cyapa.c
@@ -1013,7 +1013,7 @@ static ssize_t cyapa_update_fw_store(struct device *dev,
 	char fw_name[NAME_MAX];
 	int ret, error;
 
-	if (count > NAME_MAX) {
+	if (count >= NAME_MAX) {
 		dev_err(dev, "File name too long\n");
 		return -EINVAL;
 	}
