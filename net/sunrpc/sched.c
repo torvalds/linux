c4a7ca774949960064dac11b326908f28407e8c3
Trond Myklebust <trond.myklebust@primarydata.com>
2015-01-23 14:50:56 -0500

    SUNRPC: Allow waiting on memory allocation
    We should be safe now, as long as we don't do GFP_IO or higher allocations
    
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>

--- net/sunrpc/sched.c
+++ net/sunrpc/sched.c
@@ -844,10 +844,10 @@ static void rpc_async_schedule(struct work_struct *work)
 void *rpc_malloc(struct rpc_task *task, size_t size)
 {
 	struct rpc_buffer *buf;
-	gfp_t gfp = GFP_NOWAIT | __GFP_NOWARN;
+	gfp_t gfp = GFP_NOIO | __GFP_NOWARN;
 
 	if (RPC_IS_SWAPPER(task))
-		gfp |= __GFP_MEMALLOC;
+		gfp = __GFP_MEMALLOC | GFP_NOWAIT | __GFP_NOWARN;
 
 	size += sizeof(struct rpc_buffer);
 	if (size <= RPC_BUFFER_MAXSIZE)
