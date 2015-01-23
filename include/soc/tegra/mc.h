d1313e7896e932a92e21912850ef034e58571b66
Thierry Reding <treding@nvidia.com>
2015-01-23 09:49:25 +0100

    iommu/tegra-smmu: Add debugfs support
    Provide clients and swgroups files in debugfs. These files show for
    which clients IOMMU translation is enabled and which ASID is associated
    with each SWGROUP.
    
    Cc: Hiroshi Doyu <hdoyu@nvidia.com>
    Acked-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- include/soc/tegra/mc.h
+++ include/soc/tegra/mc.h
@@ -72,12 +72,17 @@ struct tegra_smmu;
 struct tegra_smmu *tegra_smmu_probe(struct device *dev,
 				    const struct tegra_smmu_soc *soc,
 				    struct tegra_mc *mc);
+void tegra_smmu_remove(struct tegra_smmu *smmu);
 #else
 static inline struct tegra_smmu *
 tegra_smmu_probe(struct device *dev, const struct tegra_smmu_soc *soc,
 		 struct tegra_mc *mc)
 {
 	return NULL;
+}
+
+static inline void tegra_smmu_remove(struct tegra_smmu *smmu)
+{
 }
 #endif
 
