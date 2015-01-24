d44758c0dfc5993a4b9952935a7eae4c91ebb6b4
Nikolay Nikolaev <n.nikolaev@virtualopensystems.com>
2015-01-24 12:00:02 +0000

    KVM: arm/arm64: enable KVM_CAP_IOEVENTFD
    As the infrastructure for eventfd has now been merged, report the
    ioeventfd capability as being supported.
    
    Signed-off-by: Nikolay Nikolaev <n.nikolaev@virtualopensystems.com>
    [maz: grouped the case entry with the others, fixed commit log]
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/kvm/arm.c
+++ arch/arm/kvm/arm.c
@@ -172,6 +172,7 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 	switch (ext) {
 	case KVM_CAP_IRQCHIP:
 	case KVM_CAP_IRQFD:
+	case KVM_CAP_IOEVENTFD:
 	case KVM_CAP_DEVICE_CTRL:
 	case KVM_CAP_USER_MEMORY:
 	case KVM_CAP_SYNC_MMU:
