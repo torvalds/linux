b8d48ce9519b21021e5875dce1a69f2941b17d1e
Jean Delvare <jdelvare@suse.de>
2015-01-24 14:16:21 +0100

    hwmon: (i5500_temp) Convert to devm_hwmon_device_register_with_groups
    Use devm_hwmon_device_register_with_groups() to simplify the code a
    bit.
    
    Signed-off-by: Jean Delvare <jdelvare@suse.de>
    Cc: Romain Dolbeau <romain@dolbeau.org>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/i5500_temp.c
+++ drivers/hwmon/i5500_temp.c
@@ -18,6 +18,7 @@
 #include <linux/init.h>
 #include <linux/slab.h>
 #include <linux/jiffies.h>
+#include <linux/device.h>
 #include <linux/pci.h>
 #include <linux/hwmon.h>
 #include <linux/hwmon-sysfs.h>
@@ -37,11 +38,6 @@
 #define REG_CTCTRL	0xF7
 #define REG_TSTIMER	0xF8
 
-struct i5500_temp_data {
-	struct device *hwmon_dev;
-	const char *name;
-};
-
 /*
  * Sysfs stuff
  */
@@ -50,7 +46,7 @@ struct i5500_temp_data {
 static ssize_t show_temp(struct device *dev,
 			 struct device_attribute *devattr, char *buf)
 {
-	struct pci_dev *pdev = to_pci_dev(dev);
+	struct pci_dev *pdev = to_pci_dev(dev->parent);
 	long temp;
 	u16 tsthrhi;
 	s8 tsfsc;
@@ -65,7 +61,7 @@ static ssize_t show_temp(struct device *dev,
 static ssize_t show_thresh(struct device *dev,
 			   struct device_attribute *devattr, char *buf)
 {
-	struct pci_dev *pdev = to_pci_dev(dev);
+	struct pci_dev *pdev = to_pci_dev(dev->parent);
 	int reg = to_sensor_dev_attr(devattr)->index;
 	long temp;
 	u16 tsthr;
@@ -79,20 +75,12 @@ static ssize_t show_thresh(struct device *dev,
 static ssize_t show_alarm(struct device *dev,
 			  struct device_attribute *devattr, char *buf)
 {
-	struct pci_dev *pdev = to_pci_dev(dev);
+	struct pci_dev *pdev = to_pci_dev(dev->parent);
 	int nr = to_sensor_dev_attr(devattr)->index;
 	u8 ctsts;
 
 	pci_read_config_byte(pdev, REG_CTSTS, &ctsts);
 	return sprintf(buf, "%u\n", (unsigned int)ctsts & (1 << nr));
-}
-
-static ssize_t show_name(struct device *dev, struct device_attribute
-			 *devattr, char *buf)
-{
-	struct i5500_temp_data *data = dev_get_drvdata(dev);
-
-	return sprintf(buf, "%s\n", data->name);
 }
 
 static DEVICE_ATTR(temp1_input, S_IRUGO, show_temp, NULL);
@@ -101,7 +89,6 @@ static SENSOR_DEVICE_ATTR(temp1_max_hyst, S_IRUGO, show_thresh, NULL, 0xEC);
 static SENSOR_DEVICE_ATTR(temp1_max, S_IRUGO, show_thresh, NULL, 0xEE);
 static SENSOR_DEVICE_ATTR(temp1_crit_alarm, S_IRUGO, show_alarm, NULL, 0);
 static SENSOR_DEVICE_ATTR(temp1_max_alarm, S_IRUGO, show_alarm, NULL, 1);
-static DEVICE_ATTR(name, S_IRUGO, show_name, NULL);
 
 static struct attribute *i5500_temp_attributes[] = {
 	&dev_attr_temp1_input.attr,
@@ -110,12 +97,16 @@ static struct attribute *i5500_temp_attributes[] = {
 	&sensor_dev_attr_temp1_max.dev_attr.attr,
 	&sensor_dev_attr_temp1_crit_alarm.dev_attr.attr,
 	&sensor_dev_attr_temp1_max_alarm.dev_attr.attr,
-	&dev_attr_name.attr,
 	NULL
 };
 
 static const struct attribute_group i5500_temp_group = {
 	.attrs = i5500_temp_attributes,
+};
+
+static const struct attribute_group *i5500_temp_groups[] = {
+	&i5500_temp_group,
+	NULL
 };
 
 static const struct pci_device_id i5500_temp_ids[] = {
@@ -129,58 +120,24 @@ static int i5500_temp_probe(struct pci_dev *pdev,
 			    const struct pci_device_id *id)
 {
 	int err;
-	struct i5500_temp_data *data;
-
-	data = kzalloc(sizeof(struct i5500_temp_data), GFP_KERNEL);
-	if (!data) {
-		err = -ENOMEM;
-		goto exit;
-	}
-
-	data->name = "intel5500";
-	dev_set_drvdata(&pdev->dev, data);
+	struct device *hwmon_dev;
 
 	err = pci_enable_device(pdev);
 	if (err) {
 		dev_err(&pdev->dev, "Failed to enable device\n");
-		goto exit_free;
+		return err;
 	}
 
-	/* Register sysfs hooks */
-	err = sysfs_create_group(&pdev->dev.kobj, &i5500_temp_group);
-	if (err)
-		goto exit_free;
-
-	data->hwmon_dev = hwmon_device_register(&pdev->dev);
-	if (IS_ERR(data->hwmon_dev)) {
-		err = PTR_ERR(data->hwmon_dev);
-		goto exit_remove;
-	}
-
-	return 0;
-
- exit_remove:
-	sysfs_remove_group(&pdev->dev.kobj, &i5500_temp_group);
- exit_free:
-	kfree(data);
- exit:
-	return err;
-}
-
-static void i5500_temp_remove(struct pci_dev *pdev)
-{
-	struct i5500_temp_data *data = dev_get_drvdata(&pdev->dev);
-
-	hwmon_device_unregister(data->hwmon_dev);
-	sysfs_remove_group(&pdev->dev.kobj, &i5500_temp_group);
-	kfree(data);
+	hwmon_dev = devm_hwmon_device_register_with_groups(&pdev->dev,
+							   "intel5500", NULL,
+							   i5500_temp_groups);
+	return PTR_ERR_OR_ZERO(hwmon_dev);
 }
 
 static struct pci_driver i5500_temp_driver = {
 	.name = "i5500_temp",
 	.id_table = i5500_temp_ids,
 	.probe = i5500_temp_probe,
-	.remove = i5500_temp_remove,
 };
 
 static int __init i5500_temp_init(void)
