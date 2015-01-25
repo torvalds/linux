2b55aacbd93ed7117ee1fb17dba974fe685dc11e
Kristina Martšenko <kristina.martsenko@gmail.com>
2015-01-25 18:28:21 +0200

    iio: mxs-lradc: make ADC reads not unschedule touchscreen conversions
    commit 6abe0300a1d5242f4ff89257197f284679af1a06 upstream.
    
    Reading a channel through sysfs, or starting a buffered capture, can
    occasionally turn off the touchscreen.
    
    This is because the read_raw() and buffer preenable()/postdisable()
    callbacks unschedule current conversions on all channels. If a delay
    channel happens to schedule a touchscreen conversion at the same time,
    the conversion gets cancelled and the touchscreen sequence stops.
    
    This is probably related to this note from the reference manual:
    
    	"If a delay group schedules channels to be sampled and a manual
    	write to the schedule field in CTRL0 occurs while the block is
    	discarding samples, the LRADC will switch to the new schedule
    	and will not sample the channels that were previously scheduled.
    	The time window for this to happen is very small and lasts only
    	while the LRADC is discarding samples."
    
    So make the callbacks only unschedule conversions for the channels they
    use. This means channel 0 for read_raw() and channels 0-5 for the buffer
    (if the touchscreen is enabled). Since the touchscreen uses different
    channels (6 and 7), it no longer gets turned off.
    
    This is tested and fixes the issue on i.MX28, but hasn't been tested on
    i.MX23.
    
    Signed-off-by: Kristina Martšenko <kristina.martsenko@gmail.com>
    Reviewed-by: Marek Vasut <marex@denx.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/iio/adc/mxs-lradc.c
+++ drivers/staging/iio/adc/mxs-lradc.c
@@ -824,7 +824,7 @@ static int mxs_lradc_read_single(struct iio_dev *iio_dev, int chan, int *val)
 	if (lradc->soc == IMX28_LRADC)
 		mxs_lradc_reg_clear(lradc, LRADC_CTRL1_LRADC_IRQ_EN(0),
 			LRADC_CTRL1);
-	mxs_lradc_reg_clear(lradc, 0xff, LRADC_CTRL0);
+	mxs_lradc_reg_clear(lradc, 0x1, LRADC_CTRL0);
 
 	/* Enable / disable the divider per requirement */
 	if (test_bit(chan, &lradc->is_divided))
@@ -1272,7 +1272,7 @@ static int mxs_lradc_buffer_preenable(struct iio_dev *iio)
 		mxs_lradc_reg_clear(lradc,
 			lradc->buffer_vchans << LRADC_CTRL1_LRADC_IRQ_EN_OFFSET,
 			LRADC_CTRL1);
-	mxs_lradc_reg_clear(lradc, 0xff, LRADC_CTRL0);
+	mxs_lradc_reg_clear(lradc, lradc->buffer_vchans, LRADC_CTRL0);
 
 	for_each_set_bit(chan, iio->active_scan_mask, LRADC_MAX_TOTAL_CHANS) {
 		ctrl4_set |= chan << LRADC_CTRL4_LRADCSELECT_OFFSET(ofs);
@@ -1305,7 +1305,7 @@ static int mxs_lradc_buffer_postdisable(struct iio_dev *iio)
 	mxs_lradc_reg_clear(lradc, LRADC_DELAY_TRIGGER_LRADCS_MASK |
 					LRADC_DELAY_KICK, LRADC_DELAY(0));
 
-	mxs_lradc_reg_clear(lradc, 0xff, LRADC_CTRL0);
+	mxs_lradc_reg_clear(lradc, lradc->buffer_vchans, LRADC_CTRL0);
 	if (lradc->soc == IMX28_LRADC)
 		mxs_lradc_reg_clear(lradc,
 			lradc->buffer_vchans << LRADC_CTRL1_LRADC_IRQ_EN_OFFSET,
