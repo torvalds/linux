0346dadbf041a2606bcb5bd27828b0d105897f4a
Greg Thelen <gthelen@google.com>
2015-01-26 12:58:38 -0800

    memcg: remove extra newlines from memcg oom kill log
    Commit e61734c55c24 ("cgroup: remove cgroup->name") added two extra
    newlines to memcg oom kill log messages.  This makes dmesg hard to read
    and parse.  The issue affects 3.15+.
    
    Example:
    
      Task in /t                          <<< extra #1
       killed as a result of limit of /t
                                          <<< extra #2
      memory: usage 102400kB, limit 102400kB, failcnt 274712
    
    Remove the extra newlines from memcg oom kill messages, so the messages
    look like:
    
      Task in /t killed as a result of limit of /t
      memory: usage 102400kB, limit 102400kB, failcnt 240649
    
    Fixes: e61734c55c24 ("cgroup: remove cgroup->name")
    Signed-off-by: Greg Thelen <gthelen@google.com>
    Acked-by: Michal Hocko <mhocko@suse.cz>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1477,9 +1477,9 @@ void mem_cgroup_print_oom_info(struct mem_cgroup *memcg, struct task_struct *p)
 
 	pr_info("Task in ");
 	pr_cont_cgroup_path(task_cgroup(p, memory_cgrp_id));
-	pr_info(" killed as a result of limit of ");
+	pr_cont(" killed as a result of limit of ");
 	pr_cont_cgroup_path(memcg->css.cgroup);
-	pr_info("\n");
+	pr_cont("\n");
 
 	rcu_read_unlock();
 
