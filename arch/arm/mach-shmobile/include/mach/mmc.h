040d64d6e0ef6b583ec6572d2266b0077c6184cf
Magnus Damm <damm+renesas@opensource.se>
2015-01-26 15:17:58 +0900

    ARM: shmobile: mackerel: Remove ZBOOT code
    Remove ZBOOT support code for Mackerel. With this patch in place
    it is no longer possible to boot a self-contained kernel directly
    from the reset vector. ZBOOT is still supported on kzm9g.
    
    Signed-off-by: Magnus Damm <damm+renesas@opensource.se>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Acked-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/include/mach/mmc.h
+++ arch/arm/mach-shmobile/include/mach/mmc.h
@@ -7,10 +7,5 @@
  *
  **************************************************/
 
-#ifdef CONFIG_MACH_MACKEREL
-#include "mach/mmc-mackerel.h"
-#else
 #error "unsupported board."
-#endif
-
 #endif /* MMC_H */
