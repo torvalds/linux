5d4549ccb80a3abdc9437980284fc0a136560b37
Li Jun <jun.li@freescale.com>
2015-01-27 17:11:14 +0800

    MLK-10101-4 usb: add otg_fsm pointer in usb_bus
    Add otg_fsm pointer in struct of usb_bus for access otg_fsm via bus.
    Original way was to put it in usb_otg, then usb host can access otg_fsm
    via hcd->usb_phy->otg->fsm, since usb_phy will not be the future direction,
    instead phy is preferred, so this way may not work. It's more direct and
    simple to put it in usb_bus.
    
    Signed-off-by: Li Jun <jun.li@freescale.com>
    (cherry picked from commit c118afccbbb1c78938c649ee72758213c22cf489)

--- include/linux/usb.h
+++ include/linux/usb.h
@@ -354,6 +354,7 @@ struct usb_bus {
 					 * Does the host controller use PIO
 					 * for control transfers?
 					 */
+	struct otg_fsm *otg_fsm;	/* usb otg finite state machine */
 	u8 otg_port;			/* 0, or number of OTG/HNP port */
 	unsigned is_b_host:1;		/* true during some HNP roleswitches */
 	unsigned b_hnp_enable:1;	/* OTG: did A-Host enable HNP? */
