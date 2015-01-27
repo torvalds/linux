d1a6b736284cb0f618ff82abb79c2dd7fcad33dd
Ian Abbott <abbotti@mev.co.uk>
2015-01-27 17:49:01 +0000

    staging: comedi: add comedi_usb.h
    Add a new header that Comedi USB drivers can include instead of
    "comedidev.h".  Currently, it just pulls in <linux/usb.h> and
    "comedidev.h", but the plan is to migrate the USB-specific stuff from
    "comedidev.h" here.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

---
+++ drivers/staging/comedi/comedi_usb.h
@@ -0,0 +1,30 @@
+/*
+ * comedi_usb.h
+ * header file for USB Comedi drivers
+ *
+ * COMEDI - Linux Control and Measurement Device Interface
+ * Copyright (C) 1997-2000 David A. Schleef <ds@schleef.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _COMEDI_USB_H
+#define _COMEDI_USB_H
+
+#include <linux/usb.h>
+
+#include "comedidev.h"
+
+/*
+ * TODO: Move USB-specific stuff into here from "comedidev.h".
+ */
+
+#endif /* _COMEDI_USB_H */
