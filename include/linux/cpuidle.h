3e63fb2c9136fe919893b3ac3622b61b4eb5c836
Morten Rasmussen <morten.rasmussen@arm.com>
2015-01-27 13:48:07 +0000

    ANDROID: sched, cpuidle: Track cpuidle state index in the scheduler
    The idle-state of each cpu is currently pointed to by rq->idle_state but
    there isn't any information in the struct cpuidle_state that can used to
    look up the idle-state energy model data stored in struct
    sched_group_energy. For this purpose is necessary to store the idle
    state index as well. Ideally, the idle-state data should be unified.
    
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
    Change-Id: Ib3d1178512735b0e314881f73fb8ccff5a69319f
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    (cherry picked from commit a732c97420e109956c20f34c70b91e6d06f5df31)
    [ Fixed trivial cherry-pick conflict in kernel/sched/sched.h ]
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -219,7 +219,7 @@ static inline void cpuidle_use_deepest_state(bool enable)
 #endif
 
 /* kernel/sched/idle.c */
-extern void sched_idle_set_state(struct cpuidle_state *idle_state);
+extern void sched_idle_set_state(struct cpuidle_state *idle_state, int index);
 extern void default_idle_call(void);
 
 #ifdef CONFIG_ARCH_NEEDS_CPU_IDLE_COUPLED
