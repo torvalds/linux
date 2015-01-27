2706f607bfd0aac647ba45a97f3dcfff821cc04a
Morten Rasmussen <morten.rasmussen@arm.com>
2015-01-27 13:48:07 +0000

    sched, cpuidle: Track cpuidle state index in the scheduler
    The idle-state of each cpu is currently pointed to by rq->idle_state but
    there isn't any information in the struct cpuidle_state that can used to
    look up the idle-state energy model data stored in struct
    sched_group_energy. For this purpose is necessary to store the idle
    state index as well. Ideally, the idle-state data should be unified.
    
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -204,7 +204,7 @@ static inline int cpuidle_enter_freeze(struct cpuidle_driver *drv,
 #endif
 
 /* kernel/sched/idle.c */
-extern void sched_idle_set_state(struct cpuidle_state *idle_state);
+extern void sched_idle_set_state(struct cpuidle_state *idle_state, int index);
 extern void default_idle_call(void);
 
 #ifdef CONFIG_ARCH_NEEDS_CPU_IDLE_COUPLED
--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -19,9 +19,10 @@
  * sched_idle_set_state - Record idle state for the current CPU.
  * @idle_state: State to record.
  */
-void sched_idle_set_state(struct cpuidle_state *idle_state)
+void sched_idle_set_state(struct cpuidle_state *idle_state, int index)
 {
 	idle_set_state(this_rq(), idle_state);
+	idle_set_state_idx(this_rq(), index);
 }
 
 static int __read_mostly cpu_idle_force_poll;
