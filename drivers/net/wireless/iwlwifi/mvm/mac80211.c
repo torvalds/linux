fce2d025479af5e1fa6717480c7853cdfb8b71aa
Luciano Coelho <luciano.coelho@intel.com>
2015-01-27 15:06:57 +0200

    iwlwifi: mvm: fix failure path when power_update fails in add_interface
    commit fd66fc1cafd72ddf27dbec3a5e29e99839d1bc84 upstream.
    
    When iwl_mvm_power_update_mac() is called, we have already added the
    mac context, so if this call fails we should remove the mac.
    
    Fixes: commit e5e7aa8e2561 ('iwlwifi: mvm: refactor power code')
    Signed-off-by: Luciano Coelho <luciano.coelho@intel.com>
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/wireless/iwlwifi/mvm/mac80211.c
+++ drivers/net/wireless/iwlwifi/mvm/mac80211.c
@@ -544,7 +544,7 @@ static int iwl_mvm_mac_add_interface(struct ieee80211_hw *hw,
 
 	ret = iwl_mvm_mac_ctxt_add(mvm, vif);
 	if (ret)
-		goto out_release;
+		goto out_remove_mac;
 
 	/*
 	 * Update power state on the new interface. Admittedly, based on
