09271f4eea9cd11543ae91db02727a3b20b29d03
Felipe Balbi <balbi@ti.com>
2015-01-29 10:29:18 -0600

    usb: dwc3: gadget: add missing spin_lock()
    commit 5c7b3b02de766a8634708953e805399e3544a290 upstream.
    
    commit 8e74475b0e0a (usb: dwc3: gadget: use udc-core's
    reset notifier) added support for the new UDC core's
    reset notifier to dwc3 but while at it, it removed
    a spin_lock() from dwc3_reset_gadget() which might
    cause an unbalanced spin_unlock() further down the line
    
    Fixes: 8e74475b0e0a (usb: dwc3: gadget: use udc-core's reset notifier)
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -2043,6 +2043,7 @@ static void dwc3_resume_gadget(struct dwc3 *dwc)
 	if (dwc->gadget_driver && dwc->gadget_driver->resume) {
 		spin_unlock(&dwc->lock);
 		dwc->gadget_driver->resume(&dwc->gadget);
+		spin_lock(&dwc->lock);
 	}
 }
 
