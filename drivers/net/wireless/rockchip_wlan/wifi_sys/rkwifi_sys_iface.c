2644e8a0c9d2c7ca429d92a5ac4de317dc1b27d0
li bing <libing@rock-chips.com>
2015-01-29 15:01:37 +0800

    Wifi->rtl8812au: add chip name /sys/class/rkwifi/chip for RTL8812BU

--- drivers/net/wireless/rockchip_wlan/wifi_sys/rkwifi_sys_iface.c
+++ drivers/net/wireless/rockchip_wlan/wifi_sys/rkwifi_sys_iface.c
@@ -57,6 +57,13 @@ if(type == WIFI_RTKWIFI) {
     count = sprintf(_buf, "%s", "RTL8189ES");
     printk("Current WiFi chip is RTL8189ES.\n");
 #endif
+
+#ifdef CONFIG_RTL8812AU
+    count = sprintf(_buf, "%s", "RTL8812AU");
+    printk("Current WiFi chip is RTL8812AU.\n");
+#endif
+
+
 }
 
 if(type == WIFI_BCMWIFI) {
