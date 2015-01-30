00f457b5755824cb5fc91584644c1831eb6d536e
Ian Abbott <abbotti@mev.co.uk>
2015-01-30 09:57:17 +0000

    staging: comedi: add comedi_pcmcia.h
    Add a new header that Comedi PCMCIA drivers can include instead of
    "comedidev.h".  Currently, it just pulls in <pcmcia/cistpl.h>,
    <pcmcia/ds.h> and "comedidev.h", but the plan is to migrate the
    PCMCIA-specific stuff from "comedidev.h" here.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

---
+++ drivers/staging/comedi/comedi_pcmcia.h
@@ -0,0 +1,31 @@
+/*
+ * comedi_pcmcia.h
+ * header file for Comedi PCMCIA drivers
+ *
+ * COMEDI - Linux Control and Measurement Device Interface
+ * Copyright (C) 1997-2000 David A. Schleef <ds@schleef.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _COMEDI_PCMCIA_H
+#define _COMEDI_PCMCIA_H
+
+#include <pcmcia/cistpl.h>
+#include <pcmcia/ds.h>
+
+#include "comedidev.h"
+
+/*
+ * TODO: Move PCMCIA-specific stuff into here from "comedidev.h".
+ */
+
+#endif /* _COMEDI_PCMCIA_H */
