bc6563cbd3e2a9c1feeab5aa2ca62d9a11cec2dd
Guodong Xu <guodong.xu@linaro.org>
2015-01-30 11:20:41 +0800

    misc: hi6220: enable PMIC clock output CK32B
    CK32B is a 32K clock output from PMIC Hi6553. This clock is used by
    TI WL1835MOD on HiKey board.
    
    Signed-off-by: Bintian Wang <bintian.wang@huawei.com>
    Signed-off-by: Guodong Xu <guodong.xu@linaro.org>

--- drivers/misc/hi6220-sysconfig.c
+++ drivers/misc/hi6220-sysconfig.c
@@ -7,24 +7,37 @@
  *
  */
 
+#include <linux/io.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
 
 #define reset_offset 0x334
 #define pclk_offset 0x230
+#define PMUSSI_REG_EX(pmu_base, reg_addr) (((reg_addr) << 2) + (char *)pmu_base)
 
 static int __init hi6220_sysconf(void)
 {
-        static void __iomem *base = NULL;
-        struct device_node *node;
+        static void __iomem *base = NULL, *base1 = NULL;
+        struct device_node *node, *node1;
+	unsigned char ret;
 
-        node = of_find_compatible_node(NULL, NULL, "hisilicon,sysctrl");
+        node = of_find_compatible_node(NULL, NULL, "hisilicon,hi6220-sysctrl");
         if (!node)
                 return -ENOENT;
 
         base = of_iomap(node, 0);
         if (base == NULL) {
                 printk(KERN_ERR "hi6220: sysctrl reg iomap failed!\n");
+                return -ENOMEM;
+        }
+
+        node1 = of_find_compatible_node(NULL, NULL, "hisilicon,hi655x-pmic-driver");
+        if (!node1)
+                return -ENOENT;
+
+        base1 = of_iomap(node1, 0);
+        if (base1 == NULL) {
+                printk(KERN_ERR "hi6220: pmic reg iomap failed!\n");
                 return -ENOMEM;
         }
 
@@ -47,7 +60,13 @@ static int __init hi6220_sysconf(void)
 	/*unreset microSD*/
 	writel(readl(base+0x304) | 0x6, base + 0x304);
 
+	/*enable clk for BT/WIFI*/
+	ret = *(volatile unsigned char*)PMUSSI_REG_EX(base1, 0x1c);
+	ret |= 0x40;
+	*(volatile unsigned char*)PMUSSI_REG_EX(base1, 0x1c) = ret;
+
         iounmap(base);
+        iounmap(base1);
 
         return 0;
 }
