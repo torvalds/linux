cecc58962014995db02dbfa9778523177b14fd6e
Antti Palosaari <crope@iki.fi>
2015-02-02 14:35:48 -0300

    [media] rtl2832_sdr: add kernel-doc comments for platform_data
    Add kernel-doc comments for platform_data configuration structure.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/dvb-frontends/rtl2832_sdr.h
+++ drivers/media/dvb-frontends/rtl2832_sdr.h
@@ -29,16 +29,22 @@
 #include <media/v4l2-subdev.h>
 #include "dvb_frontend.h"
 
-struct rtl2832_sdr_platform_data {
-	/*
-	 * Clock frequency.
-	 * Hz
-	 * 4000000, 16000000, 25000000, 28800000
-	 */
-	u32 clk;
+/**
+ * struct rtl2832_sdr_platform_data - Platform data for the rtl2832_sdr driver
+ * @clk: Clock frequency (4000000, 16000000, 25000000, 28800000).
+ * @tuner: Used tuner model.
+ * @i2c_client: rtl2832 demod driver I2C client.
+ * @bulk_read: rtl2832 driver private I/O interface.
+ * @bulk_write: rtl2832 driver private I/O interface.
+ * @update_bits: rtl2832 driver private I/O interface.
+ * @dvb_frontend: rtl2832 DVB frontend.
+ * @v4l2_subdev: Tuner v4l2 controls.
+ * @dvb_usb_device: DVB USB interface for USB streaming.
+ */
 
+struct rtl2832_sdr_platform_data {
+	u32 clk;
 	/*
-	 * Tuner.
 	 * XXX: This list must be kept sync with dvb_usb_rtl28xxu USB IF driver.
 	 */
 #define RTL2832_SDR_TUNER_TUA9001   0x24
