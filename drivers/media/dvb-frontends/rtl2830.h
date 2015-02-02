61c309ffcdc09c5fc7a13371fcf178a334eb36b7
Antti Palosaari <crope@iki.fi>
2015-02-02 13:53:38 -0300

    [media] rtl2830: add kernel-doc comments for platform_data
    Add kernel-doc comments for platform_data configuration structure.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/dvb-frontends/rtl2830.h
+++ drivers/media/dvb-frontends/rtl2830.h
@@ -20,33 +20,26 @@
 
 #include <linux/dvb/frontend.h>
 
+/**
+ * struct rtl2830_platform_data - Platform data for the rtl2830 driver
+ * @clk: Clock frequency (4000000, 16000000, 25000000, 28800000).
+ * @spec_inv: Spectrum inversion.
+ * @vtop: AGC take-over point.
+ * @krf: AGC ratio.
+ * @agc_targ_val: AGC.
+ * @get_dvb_frontend: Get DVB frontend.
+ * @get_i2c_adapter: Get I2C adapter.
+ * @pid_filter: Set PID to PID filter.
+ * @pid_filter_ctrl: Control PID filter.
+ */
+
 struct rtl2830_platform_data {
-	/*
-	 * Clock frequency.
-	 * Hz
-	 * 4000000, 16000000, 25000000, 28800000
-	 */
 	u32 clk;
-
-	/*
-	 * Spectrum inversion.
-	 */
 	bool spec_inv;
-
-	/*
-	 */
 	u8 vtop;
-
-	/*
-	 */
 	u8 krf;
-
-	/*
-	 */
 	u8 agc_targ_val;
 
-	/*
-	 */
 	struct dvb_frontend* (*get_dvb_frontend)(struct i2c_client *);
 	struct i2c_adapter* (*get_i2c_adapter)(struct i2c_client *);
 	int (*pid_filter)(struct dvb_frontend *, u8, u16, int);
