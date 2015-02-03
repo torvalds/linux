0ee291a9b487015768465cfbf90366a43941a685
Robin Gong <b38343@freescale.com>
2015-02-03 10:25:53 +0800

    spi: imx: use pio mode for i.mx6dl
    commit a02bb401f8ae264be782ee57d98bdd99f14c8022 upstream.
    
    For TKT238285 hardware issue which may cause txfifo store data twice can only
    be caught on i.mx6dl, we use pio mode instead of DMA mode on i.mx6dl.
    
    Fixes: f62caccd12c17e4 (spi: spi-imx: add DMA support)
    Signed-off-by: Robin Gong <b38343@freescale.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/spi/spi-imx.c
+++ drivers/spi/spi-imx.c
@@ -823,6 +823,10 @@ static int spi_imx_sdma_init(struct device *dev, struct spi_imx_data *spi_imx,
 	struct dma_slave_config slave_config = {};
 	int ret;
 
+	/* use pio mode for i.mx6dl chip TKT238285 */
+	if (of_machine_is_compatible("fsl,imx6dl"))
+		return 0;
+
 	/* Prepare for TX DMA: */
 	master->dma_tx = dma_request_slave_channel(dev, "tx");
 	if (!master->dma_tx) {
