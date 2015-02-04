e948262ad01fc683d893abe10d1161231b2a6457
Kevin Strasser <kevin.strasser@linux.intel.com>
2015-02-04 11:35:07 -0800

    ASoC: Intel: fix sst firmware path
    All sst firmware is provided under the intel directory of the linux-firmware
    tree. By default this directory structure is kept when installing on a target
    system. Change the path to expect a default linux-firmware installation.
    
    Signed-off-by: Kevin Strasser <kevin.strasser@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/sst/sst_acpi.c
+++ sound/soc/intel/sst/sst_acpi.c
@@ -343,16 +343,16 @@ static int sst_acpi_remove(struct platform_device *pdev)
 }
 
 static struct sst_machines sst_acpi_bytcr[] = {
-	{"10EC5640", "T100", "bytt100_rt5640", NULL, "fw_sst_0f28.bin",
+	{"10EC5640", "T100", "bytt100_rt5640", NULL, "intel/fw_sst_0f28.bin",
 						&byt_rvp_platform_data },
 	{},
 };
 
 /* Cherryview-based platforms: CherryTrail and Braswell */
 static struct sst_machines sst_acpi_chv[] = {
-	{"10EC5670", "cht-bsw", "cht-bsw-rt5672", NULL, "fw_sst_22a8.bin",
+	{"10EC5670", "cht-bsw", "cht-bsw-rt5672", NULL, "intel/fw_sst_22a8.bin",
 						&chv_platform_data },
-	{"10EC5645", "cht-bsw", "cht-bsw-rt5645", NULL, "fw_sst_22a8.bin",
+	{"10EC5645", "cht-bsw", "cht-bsw-rt5645", NULL, "intel/fw_sst_22a8.bin",
 						&chv_platform_data },
 	{},
 };
