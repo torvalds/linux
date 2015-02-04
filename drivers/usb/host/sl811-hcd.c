1dc6120ef7f003305d99ef12f598a6b05eacc38c
Lad, Prabhakar <prabhakar.csengg@gmail.com>
2015-02-04 18:05:13 +0000

    usb: host/sl811-hcd: fix sparse warning
    this patch fixes following sparse warning:
    sl811-hcd.c:1804:24: warning: symbol 'sl811h_driver' was not declared. Should it be static?
    
    Signed-off-by: Lad, Prabhakar <prabhakar.csengg@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/sl811-hcd.c
+++ drivers/usb/host/sl811-hcd.c
@@ -1801,7 +1801,7 @@ sl811h_resume(struct platform_device *dev)
 
 
 /* this driver is exported so sl811_cs can depend on it */
-struct platform_driver sl811h_driver = {
+static struct platform_driver sl811h_driver = {
 	.probe =	sl811h_probe,
 	.remove =	sl811h_remove,
 
