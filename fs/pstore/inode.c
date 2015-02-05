ae011d2e48d2e6c2ae29fd8aab439caf2fbfb5a8
Hari Bathini <hbathini@linux.vnet.ibm.com>
2015-02-06 01:06:28 +0530

    pstore: Add pstore type id for PPC64 opal nvram partition
    This patch adds a new PPC64 partition type to be used for opal
    specific nvram partition. A new partition type is needed as none
    of the existing type matches this partition type.
    
    Signed-off-by: Hari Bathini <hbathini@linux.vnet.ibm.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- fs/pstore/inode.c
+++ fs/pstore/inode.c
@@ -364,6 +364,9 @@ int pstore_mkfile(enum pstore_type_id type, char *psname, u64 id, int count,
 	case PSTORE_TYPE_PMSG:
 		scnprintf(name, sizeof(name), "pmsg-%s-%lld", psname, id);
 		break;
+	case PSTORE_TYPE_PPC_OPAL:
+		sprintf(name, "powerpc-opal-%s-%lld", psname, id);
+		break;
 	case PSTORE_TYPE_UNKNOWN:
 		scnprintf(name, sizeof(name), "unknown-%s-%lld", psname, id);
 		break;
