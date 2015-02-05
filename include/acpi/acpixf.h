121b7d91e902f19d21a669618f6b8966180e5131
David E. Box <david.e.box@linux.intel.com>
2015-02-05 15:20:54 +0800

    ACPICA: Update version to 20150204
    ACPICA commit e06b1624b02dc8317d144e9a6fe9d684c5fa2f90
    
    Version 20150204.
    
    Link: https://github.com/acpica/acpica/commit/e06b1624
    Signed-off-by: David E. Box <david.e.box@linux.intel.com>
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/acpixf.h
+++ include/acpi/acpixf.h
@@ -46,7 +46,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20141107
+#define ACPI_CA_VERSION                 0x20150204
 
 #include <acpi/acconfig.h>
 #include <acpi/actypes.h>
