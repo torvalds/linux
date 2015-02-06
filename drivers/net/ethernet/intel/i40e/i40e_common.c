3b38cd17f8a022fdcc99342301bce52bf79a87a4
Kevin Scott <kevin.c.scott@intel.com>
2015-02-06 08:52:18 +0000

    i40e: Set FLAG_RD when sending buffer FW must read
    Set FLAG_RD for send_driver_version AQ command.
    
    Change-ID: I8253051eff85a1d4b5a4e12ce0395b65ceb91e62
    Signed-off-by: Kevin Scott <kevin.c.scott@intel.com>
    Tested-by: Jim Young <james.m.young@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_common.c
+++ drivers/net/ethernet/intel/i40e/i40e_common.c
@@ -1794,7 +1794,7 @@ i40e_status i40e_aq_send_driver_version(struct i40e_hw *hw,
 
 	i40e_fill_default_direct_cmd_desc(&desc, i40e_aqc_opc_driver_version);
 
-	desc.flags |= cpu_to_le16(I40E_AQ_FLAG_BUF | I40E_AQ_FLAG_SI);
+	desc.flags |= cpu_to_le16(I40E_AQ_FLAG_BUF | I40E_AQ_FLAG_RD);
 	cmd->driver_major_ver = dv->major_version;
 	cmd->driver_minor_ver = dv->minor_version;
 	cmd->driver_build_ver = dv->build_version;
