d857a1e253498feb231173218df26f5562c70f09
Olof Kindgren <olof.kindgren@gmail.com>
2015-02-06 13:41:51 +0100

    openrisc: Add optimized memset
    This adds a hand-optimized assembler version of memset and sets
    __HAVE_ARCH_MEMSET to use this version instead of the generic C
    routine
    
    Signed-off-by: Olof Kindgren <olof.kindgren@gmail.com>
    Signed-off-by: Stafford Horne <shorne@gmail.com>

---
+++ arch/openrisc/include/asm/string.h
@@ -0,0 +1,7 @@
+#ifndef __ASM_OPENRISC_STRING_H
+#define __ASM_OPENRISC_STRING_H
+
+#define __HAVE_ARCH_MEMSET
+extern void *memset(void *s, int c, __kernel_size_t n);
+
+#endif /* __ASM_OPENRISC_STRING_H */
