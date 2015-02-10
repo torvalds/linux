bea2b592fd18eb8ffa3fc4ad380610632d03a38f
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2015-02-10 11:12:21 +0100

    lib/lz4: Pull out constant tables
    There's no reason to allocate the dec{32,64}table on the stack; it
    just wastes a bunch of instructions setting them up and, of course,
    also consumes quite a bit of stack. Using size_t for such small
    integers is a little excessive.
    
    $ scripts/bloat-o-meter /tmp/built-in.o lib/built-in.o
    add/remove: 2/2 grow/shrink: 2/0 up/down: 1304/-1548 (-244)
    function                                     old     new   delta
    lz4_decompress_unknownoutputsize              55     718    +663
    lz4_decompress                                55     632    +577
    dec64table                                     -      32     +32
    dec32table                                     -      32     +32
    lz4_uncompress                               747       -    -747
    lz4_uncompress_unknownoutputsize             801       -    -801
    
    The now inlined lz4_uncompress functions used to have a stack
    footprint of 176 bytes (according to -fstack-usage); their inlinees
    have increased their stack use from 32 bytes to 48 and 80 bytes,
    respectively.
    
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- lib/lz4/lz4_decompress.c
+++ lib/lz4/lz4_decompress.c
@@ -47,6 +47,11 @@
 
 #include "lz4defs.h"
 
+static const int dec32table[] = {0, 3, 2, 3, 0, 0, 0, 0};
+#if LZ4_ARCH64
+static const int dec64table[] = {0, 0, 0, -1, 0, 1, 2, 3};
+#endif
+
 static int lz4_uncompress(const char *source, char *dest, int osize)
 {
 	const BYTE *ip = (const BYTE *) source;
@@ -56,10 +61,6 @@ static int lz4_uncompress(const char *source, char *dest, int osize)
 	BYTE *cpy;
 	unsigned token;
 	size_t length;
-	size_t dec32table[] = {0, 3, 2, 3, 0, 0, 0, 0};
-#if LZ4_ARCH64
-	size_t dec64table[] = {0, 0, 0, -1, 0, 1, 2, 3};
-#endif
 
 	while (1) {
 
@@ -116,7 +117,7 @@ static int lz4_uncompress(const char *source, char *dest, int osize)
 		/* copy repeated sequence */
 		if (unlikely((op - ref) < STEPSIZE)) {
 #if LZ4_ARCH64
-			size_t dec64 = dec64table[op - ref];
+			int dec64 = dec64table[op - ref];
 #else
 			const int dec64 = 0;
 #endif
@@ -176,11 +177,6 @@ static int lz4_uncompress_unknownoutputsize(const char *source, char *dest,
 	BYTE *op = (BYTE *) dest;
 	BYTE * const oend = op + maxoutputsize;
 	BYTE *cpy;
-
-	size_t dec32table[] = {0, 3, 2, 3, 0, 0, 0, 0};
-#if LZ4_ARCH64
-	size_t dec64table[] = {0, 0, 0, -1, 0, 1, 2, 3};
-#endif
 
 	/* Main Loop */
 	while (ip < iend) {
@@ -249,7 +245,7 @@ static int lz4_uncompress_unknownoutputsize(const char *source, char *dest,
 		/* copy repeated sequence */
 		if (unlikely((op - ref) < STEPSIZE)) {
 #if LZ4_ARCH64
-			size_t dec64 = dec64table[op - ref];
+			int dec64 = dec64table[op - ref];
 #else
 			const int dec64 = 0;
 #endif
