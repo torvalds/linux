1dfeb768475dfded66bba03a1744c2e8141d3429
alex chen <alex.chen@huawei.com>
2015-02-10 14:09:04 -0800

    ocfs2: add a mount option journal_async_commit on ocfs2 filesystem
    Add a mount option to support JBD2 feature:
    
    JBD2_FEATURE_INCOMPAT_ASYNC_COMMIT.  When this feature is opened, journal
    commit block can be written to disk without waiting for descriptor blocks,
    which can improve journal commit performance.  This option will enable
    'journal_checksum' internally.
    
    Using the fs_mark benchmark, using journal_async_commit shows a 50%
    improvement, the files per second go up from 215.2 to 317.5.
    
    test script:
    fs_mark  -d  /mnt/ocfs2/  -s  10240  -n  1000
    
    default:
    FSUse%        Count         Size    Files/sec     App Overhead
         0         1000        10240        215.2            17878
    
    with journal_async_commit option:
    FSUse%        Count         Size    Files/sec     App Overhead
         0         1000        10240        317.5            17881
    
    Signed-off-by: Alex Chen <alex.chen@huawei.com>
    Signed-off-by: Weiwei Wang <wangww631@huawei.comm>
    Reviewed-by: Joseph Qi <joseph.qi@huawei.com>
    Reviewed-by: Mark Fasheh <mfasheh@suse.de>
    Cc: Joel Becker <jlbec@evilplan.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/ocfs2/super.c
+++ fs/ocfs2/super.c
@@ -191,6 +191,7 @@ enum {
 	Opt_coherency_full,
 	Opt_resv_level,
 	Opt_dir_resv_level,
+	Opt_journal_async_commit,
 	Opt_err,
 };
 
@@ -222,6 +223,7 @@ static const match_table_t tokens = {
 	{Opt_coherency_full, "coherency=full"},
 	{Opt_resv_level, "resv_level=%u"},
 	{Opt_dir_resv_level, "dir_resv_level=%u"},
+	{Opt_journal_async_commit, "journal_async_commit"},
 	{Opt_err, NULL}
 };
 
@@ -1500,6 +1502,9 @@ static int ocfs2_parse_options(struct super_block *sb,
 			    option < OCFS2_MAX_RESV_LEVEL)
 				mopt->dir_resv_level = option;
 			break;
+		case Opt_journal_async_commit:
+			mopt->mount_opt |= OCFS2_MOUNT_JOURNAL_ASYNC_COMMIT;
+			break;
 		default:
 			mlog(ML_ERROR,
 			     "Unrecognized mount option \"%s\" "
@@ -1605,6 +1610,9 @@ static int ocfs2_show_options(struct seq_file *s, struct dentry *root)
 
 	if (osb->osb_dir_resv_level != osb->osb_resv_level)
 		seq_printf(s, ",dir_resv_level=%d", osb->osb_resv_level);
+
+	if (opts & OCFS2_MOUNT_JOURNAL_ASYNC_COMMIT)
+		seq_printf(s, ",journal_async_commit");
 
 	return 0;
 }
@@ -2474,6 +2482,15 @@ static int ocfs2_check_volume(struct ocfs2_super *osb)
 		mlog(ML_ERROR, "ocfs2 journal load failed! %d\n", status);
 		goto finally;
 	}
+
+	if (osb->s_mount_opt & OCFS2_MOUNT_JOURNAL_ASYNC_COMMIT)
+		jbd2_journal_set_features(osb->journal->j_journal,
+				JBD2_FEATURE_COMPAT_CHECKSUM, 0,
+				JBD2_FEATURE_INCOMPAT_ASYNC_COMMIT);
+	else
+		jbd2_journal_clear_features(osb->journal->j_journal,
+				JBD2_FEATURE_COMPAT_CHECKSUM, 0,
+				JBD2_FEATURE_INCOMPAT_ASYNC_COMMIT);
 
 	if (dirty) {
 		/* recover my local alloc if we didn't unmount cleanly. */
