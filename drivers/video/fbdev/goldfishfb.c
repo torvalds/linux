f85111e72da98ad3d8d4da51440dd74eadcb1167
Yu Ning <yu.ning@intel.com>
2015-02-12 11:44:40 +0800

    ANDROID: goldfish: Enable ACPI-based enumeration for goldfish framebuffer
    Follow the same way in which ACPI was enabled for goldfish battery. See
    commit d3be10e for details.
    
    Note that this patch also depends on commit af33cac.
    
    Change-Id: Ic63b6e7e0a4b9896ef9a9d0ed135a7796a4c1fdb
    Signed-off-by: Yu Ning <yu.ning@intel.com>

--- drivers/video/fbdev/goldfishfb.c
+++ drivers/video/fbdev/goldfishfb.c
@@ -26,6 +26,7 @@
 #include <linux/interrupt.h>
 #include <linux/ioport.h>
 #include <linux/platform_device.h>
+#include <linux/acpi.h>
 
 enum {
 	FB_GET_WIDTH        = 0x00,
@@ -310,6 +311,12 @@ static const struct of_device_id goldfish_fb_of_match[] = {
 };
 MODULE_DEVICE_TABLE(of, goldfish_fb_of_match);
 
+static const struct acpi_device_id goldfish_fb_acpi_match[] = {
+	{ "GFSH0004", 0 },
+	{ },
+};
+MODULE_DEVICE_TABLE(acpi, goldfish_fb_acpi_match);
+
 static struct platform_driver goldfish_fb_driver = {
 	.probe		= goldfish_fb_probe,
 	.remove		= goldfish_fb_remove,
@@ -317,6 +324,7 @@ static struct platform_driver goldfish_fb_driver = {
 		.name = "goldfish_fb",
 		.owner = THIS_MODULE,
 		.of_match_table = goldfish_fb_of_match,
+		.acpi_match_table = ACPI_PTR(goldfish_fb_acpi_match),
 	}
 };
 
