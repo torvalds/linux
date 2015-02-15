f9be10850f533895c360e2692a34a5be1d21cd8d
Tolga Ceylan <tolga.ceylan@gmail.com>
2015-02-14 20:32:45 -0800

    Staging: iio: meter: ade7854-i2c: code style improvements
    Code reformatting based on checkpatch.pl with --strict:
    Comparison to NULL rewritten as !indio_dev
    
    Signed-off-by: Tolga Ceylan <tolga.ceylan@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/iio/meter/ade7854-i2c.c
+++ drivers/staging/iio/meter/ade7854-i2c.c
@@ -210,7 +210,7 @@ static int ade7854_i2c_probe(struct i2c_client *client,
 	struct iio_dev *indio_dev;
 
 	indio_dev = devm_iio_device_alloc(&client->dev, sizeof(*st));
-	if (indio_dev == NULL)
+	if (!indio_dev)
 		return -ENOMEM;
 	st = iio_priv(indio_dev);
 	i2c_set_clientdata(client, indio_dev);
