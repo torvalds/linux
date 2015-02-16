1e78cdbd9b2266503339accafe0ebdd99b93a531
Rik van Riel <riel@redhat.com>
2015-02-16 15:23:49 -0500

    sched/rt/nohz: Stop scheduler tick if running realtime task
    If the CPU is running a realtime task that does not round-robin
    with another realtime task of equal priority, there is no point
    in keeping the scheduler tick going. After all, whenever the
    scheduler tick runs, the kernel will just decide not to
    reschedule.
    
    Extend sched_can_stop_tick() to recognize these situations, and
    inform the rest of the kernel that the scheduler tick can be
    stopped.
    
    Tested-by: Luiz Capitulino <lcapitulino@redhat.com>
    Signed-off-by: Rik van Riel <riel@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: fweisbec@redhat.com
    Cc: mtosatti@redhat.com
    Link: http://lkml.kernel.org/r/20150216152349.6a8ed824@annuminas.surriel.com
    [ Small cleanliness tweak. ]
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -690,6 +690,23 @@ static inline bool got_nohz_idle_kick(void)
 bool sched_can_stop_tick(void)
 {
 	/*
+	 * FIFO realtime policy runs the highest priority task. Other runnable
+	 * tasks are of a lower priority. The scheduler tick does nothing.
+	 */
+	if (current->policy == SCHED_FIFO)
+		return true;
+
+	/*
+	 * Round-robin realtime tasks time slice with other tasks at the same
+	 * realtime priority. Is this task the only one at this priority?
+	 */
+	if (current->policy == SCHED_RR) {
+		struct sched_rt_entity *rt_se = &current->rt;
+
+		return rt_se->run_list.prev == rt_se->run_list.next;
+	}
+
+	/*
 	 * More than one running task need preemption.
 	 * nr_running update is assumed to be visible
 	 * after IPI is sent from wakers.
