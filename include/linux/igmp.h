959d10f6bbf6ab5b8813c4e37540a2e43ca2ae96
Eric Dumazet <edumazet@google.com>
2015-02-17 03:19:24 -0800

    igmp: add __ip_mc_{join|leave}_group()
    There is a need to perform igmp join/leave operations while RTNL is
    held.
    
    Make ip_mc_{join|leave}_group() wrappers around
    __ip_mc_{join|leave}_group() to avoid the proliferation of work queues.
    
    For example, vxlan_igmp_join() could possibly be removed.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/igmp.h
+++ include/linux/igmp.h
@@ -111,7 +111,9 @@ struct ip_mc_list {
 
 extern int ip_check_mc_rcu(struct in_device *dev, __be32 mc_addr, __be32 src_addr, u16 proto);
 extern int igmp_rcv(struct sk_buff *);
+extern int __ip_mc_join_group(struct sock *sk, struct ip_mreqn *imr);
 extern int ip_mc_join_group(struct sock *sk, struct ip_mreqn *imr);
+extern int __ip_mc_leave_group(struct sock *sk, struct ip_mreqn *imr);
 extern int ip_mc_leave_group(struct sock *sk, struct ip_mreqn *imr);
 extern void ip_mc_drop_socket(struct sock *sk);
 extern int ip_mc_source(int add, int omode, struct sock *sk,
