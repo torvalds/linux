230f259ffe4ad6d334bb100ce1c0c6a73ff18989
Geert Uytterhoeven <geert+renesas@glider.be>
2015-02-17 16:43:03 +0100

    drivers: sh: Disable PM runtime for multi-platform r8a73a4 with genpd
    If the default PM domain using PM_CLK is used for PM runtime, the real PM
    domain(s) cannot be registered from DT later.
    
    Hence do not enable it when running a multi-platform kernel with genpd
    support on an r8a73a4. The R-Mobile PM domain driver will take care of
    PM runtime management of the module clocks.
    
    The default PM domain is still needed for:
      - platforms without genpd support,
      - the legacy (non-DT) case, where genpd may take over later, except
        for the C5 "always on" PM domain.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- drivers/sh/pm_runtime.c
+++ drivers/sh/pm_runtime.c
@@ -80,8 +80,8 @@ static int __init sh_pm_runtime_init(void)
 	if (IS_ENABLED(CONFIG_ARCH_SHMOBILE_MULTI)) {
 		if (!of_machine_is_compatible("renesas,emev2") &&
 		    !of_machine_is_compatible("renesas,r7s72100") &&
-		    !of_machine_is_compatible("renesas,r8a73a4") &&
 #ifndef CONFIG_PM_GENERIC_DOMAINS_OF
+		    !of_machine_is_compatible("renesas,r8a73a4") &&
 		    !of_machine_is_compatible("renesas,r8a7740") &&
 		    !of_machine_is_compatible("renesas,sh73a0") &&
 #endif
