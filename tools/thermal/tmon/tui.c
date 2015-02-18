a90b6b006c616f1a33f8ffb6939e31c8d66926a4
Brian Norris <computersforpeace@gmail.com>
2015-02-17 18:18:30 -0800

    tools/thermal: tmon: add min/max macros
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>
    Acked-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>

--- tools/thermal/tmon/tui.c
+++ tools/thermal/tmon/tui.c
@@ -30,6 +30,18 @@
 
 #include "tmon.h"
 
+#define min(x, y) ({				\
+	typeof(x) _min1 = (x);			\
+	typeof(y) _min2 = (y);			\
+	(void) (&_min1 == &_min2);		\
+	_min1 < _min2 ? _min1 : _min2; })
+
+#define max(x, y) ({				\
+	typeof(x) _max1 = (x);			\
+	typeof(y) _max2 = (y);			\
+	(void) (&_max1 == &_max2);		\
+	_max1 > _max2 ? _max1 : _max2; })
+
 static PANEL *data_panel;
 static PANEL *dialogue_panel;
 static PANEL *top;
