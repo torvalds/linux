7e9f07388779ccc5067f206357d9791aeef38864
Trond Myklebust <trond.myklebust@primarydata.com>
2015-02-18 12:07:19 -0800

    NFSv4.1: Always set up a forward channel when binding the session
    Currently, the client requests a back channel or a bidirectional
    connection when binding a new TCP channel to an existing session.
    Fix that to ask for a forward channel or bidirectional.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>

--- fs/nfs/nfs4xdr.c
+++ fs/nfs/nfs4xdr.c
@@ -1724,7 +1724,7 @@ static void encode_bind_conn_to_session(struct xdr_stream *xdr,
 		decode_bind_conn_to_session_maxsz, hdr);
 	encode_opaque_fixed(xdr, session->sess_id.data, NFS4_MAX_SESSIONID_LEN);
 	p = xdr_reserve_space(xdr, 8);
-	*p++ = cpu_to_be32(NFS4_CDFC4_BACK_OR_BOTH);
+	*p++ = cpu_to_be32(NFS4_CDFC4_FORE_OR_BOTH);
 	*p = 0;	/* use_conn_in_rdma_mode = False */
 }
 
