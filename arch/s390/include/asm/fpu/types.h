04864808029e59ea1bf075c756a0f35c8398fc11
Hendrik Brueckner <brueckner@linux.vnet.ibm.com>
2015-02-18 14:46:00 +0100

    s390/vx: add support functions for in-kernel FPU use
    Introduce the kernel_fpu_begin() and kernel_fpu_end() function
    to enclose any in-kernel use of FPU instructions and registers.
    In enclosed sections, you can perform floating-point or vector
    (SIMD) computations.  The functions take care of saving and
    restoring FPU register contents and controls.
    
    For usage details, see the guidelines in arch/s390/include/asm/fpu/api.h
    
    Signed-off-by: Hendrik Brueckner <brueckner@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/fpu/types.h
+++ arch/s390/include/asm/fpu/types.h
@@ -24,4 +24,14 @@ struct fpu {
 /* VX array structure for address operand constraints in inline assemblies */
 struct vx_array { __vector128 _[__NUM_VXRS]; };
 
+/* In-kernel FPU state structure */
+struct kernel_fpu {
+	u32	    mask;
+	u32	    fpc;
+	union {
+		freg_t fprs[__NUM_FPRS];
+		__vector128 vxrs[__NUM_VXRS];
+	};
+};
+
 #endif /* _ASM_S390_FPU_TYPES_H */
