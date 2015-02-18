b07da53c79ba6480759c1ad352a96b96c7b97c7a
Thomas Daniel <thomas.daniel@intel.com>
2015-02-18 11:48:21 +0000

    drm/i915: Shift driver's HWSP usage out of reserved range
    As of Gen6, the general purpose area of the hardware status page has shrunk and
    now begins at dword 0x30.  i915 driver uses dword 0x20 to store the seqno which
    is now reserved.  So shift our HWSP dwords up into the general purpose range
    before this bites us.
    
    Note that all available documentation just says this is reserved
    without going into details about what it's used for.
    
    Signed-off-by: Thomas Daniel <thomas.daniel@intel.com>
    Reviewed-by: Dave Gordon <david.s.gordon@intel.com>
    [danvet: Add clarification from Thomas that unfortunately Bspec is
    silent on what "reserverd" precisely means.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_ringbuffer.h
+++ drivers/gpu/drm/i915/intel_ringbuffer.h
@@ -373,11 +373,12 @@ intel_write_status_page(struct intel_engine_cs *ring,
  * 0x06: ring 2 head pointer (915-class)
  * 0x10-0x1b: Context status DWords (GM45)
  * 0x1f: Last written status offset. (GM45)
+ * 0x20-0x2f: Reserved (Gen6+)
  *
- * The area from dword 0x20 to 0x3ff is available for driver usage.
+ * The area from dword 0x30 to 0x3ff is available for driver usage.
  */
-#define I915_GEM_HWS_INDEX		0x20
-#define I915_GEM_HWS_SCRATCH_INDEX	0x30
+#define I915_GEM_HWS_INDEX		0x30
+#define I915_GEM_HWS_SCRATCH_INDEX	0x40
 #define I915_GEM_HWS_SCRATCH_ADDR (I915_GEM_HWS_SCRATCH_INDEX << MI_STORE_DWORD_INDEX_SHIFT)
 
 void intel_unpin_ringbuffer_obj(struct intel_ringbuffer *ringbuf);
