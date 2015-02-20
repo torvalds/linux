737eb0301f296d55c22350c6968ff1ef51bacb5f
Mark Rutland <mark.rutland@arm.com>
2015-02-20 14:53:46 +0000

    genirq / PM: better describe IRQF_NO_SUSPEND semantics
    The IRQF_NO_SUSPEND flag is intended to be used for interrupts required
    to be enabled during the suspend-resume cycle. This mostly consists of
    IPIs and timer interrupts, potentially including chained irqchip
    interrupts if these are necessary to handle timers or IPIs. If an
    interrupt does not fall into one of the aforementioned categories,
    requesting it with IRQF_NO_SUSPEND is likely incorrect.
    
    Using IRQF_NO_SUSPEND does not guarantee that the interrupt can wake the
    system from a suspended state. For an interrupt to be able to trigger a
    wakeup, it may be necessary to program various components of the system.
    In these cases it is necessary to use {enable,disabled}_irq_wake.
    
    Unfortunately, several drivers assume that IRQF_NO_SUSPEND ensures that
    an IRQ can wake up the system, and the documentation can be read
    ambiguously w.r.t. this property.
    
    This patch updates the documentation regarding IRQF_NO_SUSPEND to make
    this caveat explicit, hopefully making future misuse rarer. Cleanup of
    existing misuse will occur as part of later patch series.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -52,7 +52,9 @@
  * IRQF_ONESHOT - Interrupt is not reenabled after the hardirq handler finished.
  *                Used by threaded interrupts which need to keep the
  *                irq line disabled until the threaded handler has been run.
- * IRQF_NO_SUSPEND - Do not disable this IRQ during suspend
+ * IRQF_NO_SUSPEND - Do not disable this IRQ during suspend.  Does not guarantee
+ *                   that this interrupt will wake the system from a suspended
+ *                   state.  See Documentation/power/suspend-and-interrupts.txt
  * IRQF_FORCE_RESUME - Force enable it on resume even if IRQF_NO_SUSPEND is set
  * IRQF_NO_THREAD - Interrupt cannot be threaded
  * IRQF_EARLY_RESUME - Resume IRQ early during syscore instead of at device
