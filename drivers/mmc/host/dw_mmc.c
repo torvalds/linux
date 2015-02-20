bdb9a90b3d123d9d421dd840c0f7fba1d1dfeba2
addy ke <addy.ke@rock-chips.com>
2015-02-20 10:55:25 +0800

    mmc: dw_mmc: fix mmc_test by not sending abort for DRTO/EBE errors
    The STOP command can terminate a data transfer between a memory card and
    mmc controller.
    
    As show in Synopsys DesignWare Cores Mobile Storage Host Databook:
    Data timeout and Data end-bit error will terminate further data transfer
    by mmc controller. So we should not send abort command to terminate a
    data transfer again if we got DRTO and EBE interrupt.
    
    After this patch, all mmc_test cases can pass on RK3288-Pink2 board.
    
    Signed-off-by: Addy Ke <addy.ke@rock-chips.com>
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    Tested-by: Doug Anderson <dianders@chromium.org>
    Tested-by: Javier Martinez Canillas <javier.martinez@collabora.co.uk>
    Signed-off-by: Jaehoon Chung <jh80.chung@samsung.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/dw_mmc.c
+++ drivers/mmc/host/dw_mmc.c
@@ -1534,7 +1534,10 @@ static void dw_mci_tasklet_func(unsigned long priv)
 			if (test_and_clear_bit(EVENT_DATA_ERROR,
 					       &host->pending_events)) {
 				dw_mci_stop_dma(host);
-				send_stop_abort(host, data);
+				if (data->stop ||
+				    !(host->data_status & (SDMMC_INT_DRTO |
+							   SDMMC_INT_EBE)))
+					send_stop_abort(host, data);
 				state = STATE_DATA_ERROR;
 				break;
 			}
@@ -1561,7 +1564,10 @@ static void dw_mci_tasklet_func(unsigned long priv)
 			if (test_and_clear_bit(EVENT_DATA_ERROR,
 					       &host->pending_events)) {
 				dw_mci_stop_dma(host);
-				send_stop_abort(host, data);
+				if (data->stop ||
+				    !(host->data_status & (SDMMC_INT_DRTO |
+							   SDMMC_INT_EBE)))
+					send_stop_abort(host, data);
 				state = STATE_DATA_ERROR;
 				break;
 			}
