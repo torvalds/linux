68c1b89cf5653acd1107253e146b332420a1f4a7
Fenghua Yu <fenghua.yu@intel.com>
2015-02-21 13:07:27 -0800

    iommu/vt-d: Print x2apic opt out info instead of printing a warning
    BIOS can set up x2apic_opt_out bit on some platforms, for various misguided
    reasons like insane SMM code with weird assumptions about what descriptors
    look like, or wanting Windows not to enable the IOMMU so that the graphics
    driver will take it over for SVM in "driver mode".
    
    A user can either disable the x2apic_opt_out bit in BIOS or by kernel
    parameter "no_x2apic_optout". Instead of printing a warning, we just
    print information of x2apic opt out.
    
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/iommu/intel_irq_remapping.c
+++ drivers/iommu/intel_irq_remapping.c
@@ -615,10 +615,7 @@ static int __init intel_enable_irq_remapping(void)
 
 		eim = !dmar_x2apic_optout();
 		if (!eim)
-			printk(KERN_WARNING
-				"Your BIOS is broken and requested that x2apic be disabled.\n"
-				"This will slightly decrease performance.\n"
-				"Use 'intremap=no_x2apic_optout' to override BIOS request.\n");
+			pr_info("x2apic is disabled because BIOS sets x2apic opt out bit. You can use 'intremap=no_x2apic_optout' to override the BIOS setting.\n");
 	}
 
 	for_each_iommu(iommu, drhd) {
