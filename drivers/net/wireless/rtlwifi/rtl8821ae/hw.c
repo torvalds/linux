df905570bbf18139c0a437ec2f63bb33a03e9703
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2015-02-23 13:05:59 +0100

    rtlwifi: rtl8821ae: Remove duplicate hex prefixes
    The # flag in %X means print a 0X prefix. Remove the extra 0x prefix.
    
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/rtlwifi/rtl8821ae/hw.c
+++ drivers/net/wireless/rtlwifi/rtl8821ae/hw.c
@@ -1515,7 +1515,7 @@ static bool _rtl8821ae_dynamic_rqpn(struct ieee80211_hw *hw, u32 boundary,
 				      (u8 *)(&support_remote_wakeup));
 
 	RT_TRACE(rtlpriv, COMP_INIT, DBG_LOUD,
-		 "boundary=0x%#X, NPQ_RQPNValue=0x%#X, RQPNValue=0x%#X\n",
+		 "boundary=%#X, NPQ_RQPNValue=%#X, RQPNValue=%#X\n",
 		  boundary, npq_rqpn_value, rqpn_val);
 
 	/* stop PCIe DMA
