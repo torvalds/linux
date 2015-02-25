1e0e76508c55eac8475735b1ae79d41288edda94
aybuke ozdemir <aybuke.147@gmail.com>
2015-02-26 00:10:21 +0200

    Staging: i2o: Remove unnecessary braces
    Brackets were removed from the expression that containing single
    statement. Removed following checkpatch.pl warnings:
    WARNING: braces {} are not necessary for single statement blocks
    
    Signed-off-by: aybuke ozdemir <aybuke.147@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/i2o/device.c
+++ drivers/staging/i2o/device.c
@@ -565,9 +565,8 @@ int i2o_parm_table_get(struct i2o_device *dev, int oper, int group,
 		size += 4 - size % 4;
 
 	opblk = kmalloc(size, GFP_KERNEL);
-	if (opblk == NULL) {
+	if (opblk == NULL)
 		return -ENOMEM;
-	}
 
 	opblk[0] = 1;		/* operation count */
 	opblk[1] = 0;		/* pad */
