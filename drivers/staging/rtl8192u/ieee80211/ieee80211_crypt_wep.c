6518c10be952ef0afb95e5ea5d277458e273b662
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2015-02-27 00:03:02 +0100

    Staging: rtl8192u: Replace printk() with pr_debug()
    For dynamic debugging netdev_dbg(), dev_dbg() or  pr_debug() macro is
    preferred over printk(), which is the raw way to print something.
    Network system has it's own printk format, netdev_dbg, but in this case
    function's argument list has no  pointer to a struct netdevice so
    pr_debug is used instead.
    Issue found by checkpatch.pl.
    
    Signed-off-by: Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/ieee80211/ieee80211_crypt_wep.c
+++ drivers/staging/rtl8192u/ieee80211/ieee80211_crypt_wep.c
@@ -48,14 +48,14 @@ static void *prism2_wep_init(int keyidx)
 
 	priv->tx_tfm = crypto_alloc_blkcipher("ecb(arc4)", 0, CRYPTO_ALG_ASYNC);
 	if (IS_ERR(priv->tx_tfm)) {
-		printk(KERN_DEBUG "ieee80211_crypt_wep: could not allocate "
+		pr_debug("ieee80211_crypt_wep: could not allocate "
 		       "crypto API arc4\n");
 		priv->tx_tfm = NULL;
 		goto fail;
 	}
 	priv->rx_tfm = crypto_alloc_blkcipher("ecb(arc4)", 0, CRYPTO_ALG_ASYNC);
 	if (IS_ERR(priv->rx_tfm)) {
-		printk(KERN_DEBUG "ieee80211_crypt_wep: could not allocate "
+		pr_debug("ieee80211_crypt_wep: could not allocate "
 		       "crypto API arc4\n");
 		priv->rx_tfm = NULL;
 		goto fail;
