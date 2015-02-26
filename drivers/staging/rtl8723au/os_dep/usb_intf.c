ab4f931f3173dc1250c29a1e0367f92dfbe88a27
Melike Yurtoglu <aysemelikeyurtoglu@gmail.com>
2015-02-26 09:52:07 +0200

    Staging: rtl8723au: Remove return in void function
    WARNING: void function return statements are not generally useful
    Remove return in void function. That was found by running checkpatch.
    
    Signed-off-by: Melike Yurtoglu <aysemelikeyurtoglu@gmail.com>
    Reviewed-by: Daniel Baluta <daniel.baluta@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/os_dep/usb_intf.c
+++ drivers/staging/rtl8723au/os_dep/usb_intf.c
@@ -599,8 +599,6 @@ static void rtw_disconnect(struct usb_interface *pusb_intf)
 
 	RT_TRACE(_module_hci_intfs_c_, _drv_err_, ("-dev_remove()\n"));
 	DBG_8723A("-r871xu_dev_remove, done\n");
-
-	return;
 }
 
 static int __init rtw_drv_entry(void)
