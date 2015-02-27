92d64e47b67b5e7fe1b5358402ab222a32ec3479
Trond Myklebust <trond.myklebust@primarydata.com>
2015-02-26 19:48:26 -0500

    NFS: Fix nfs_post_op_update_inode() to set an attribute barrier
    nfs_post_op_update_inode() is called after a self-induced attribute
    update. Ensure that it also sets the barrier.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>
    Tested-by: Chuck Lever <chuck.lever@oracle.com>

--- fs/nfs/inode.c
+++ fs/nfs/inode.c
@@ -1475,6 +1475,7 @@ int nfs_post_op_update_inode(struct inode *inode, struct nfs_fattr *fattr)
 	int status;
 
 	spin_lock(&inode->i_lock);
+	nfs_fattr_set_barrier(fattr);
 	status = nfs_post_op_update_inode_locked(inode, fattr);
 	spin_unlock(&inode->i_lock);
 
