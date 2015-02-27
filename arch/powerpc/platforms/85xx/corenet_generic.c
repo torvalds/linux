2e6e99666de1005fb2d201320082bd89442b7b14
Shruti Kanetkar <Kanetkar.Shruti@gmail.com>
2015-02-27 09:59:22 -0600

    powerpc/corenet: Enable muxing MDIO buses via FPGA
    Signed-off-by: Andy Fleming <afleming@gmail.com>
    Signed-off-by: Shaohui Xie <Shaohui.Xie@freescale.com>
    Signed-off-by: Shruti Kanetkar <Kanetkar.Shruti@gmail.com>
    Signed-off-by: Emil Medve <Emilian.Medve@Freescale.com>
    Signed-off-by: Scott Wood <scottwood@freescale.com>

--- arch/powerpc/platforms/85xx/corenet_generic.c
+++ arch/powerpc/platforms/85xx/corenet_generic.c
@@ -91,6 +91,12 @@ static const struct of_device_id of_device_ids[] = {
 		.compatible	= "mdio-mux-gpio"
 	},
 	{
+		.compatible	= "fsl,fpga-ngpixis"
+	},
+	{
+		.compatible	= "fsl,fpga-qixis"
+	},
+	{
 		.compatible	= "fsl,srio",
 	},
 	{
