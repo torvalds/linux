8bb6f4b9c50eef838931e0e667f1a451f34a76ae
Luciano Coelho <luciano.coelho@intel.com>
2015-03-01 09:10:07 +0200

    mac80211: remove useless double check for open_count in __ieee80211_suspend()
    We check local->open_count at the top of the __ieee80211_suspend(), so
    there's no need to check for it again.  open_count is protected by the
    rtnl, so there's no chance for it to have change between the two
    calls.
    
    Signed-off-by: Luciano Coelho <luciano.coelho@intel.com>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/pm.c
+++ net/mac80211/pm.c
@@ -59,7 +59,7 @@ int __ieee80211_suspend(struct ieee80211_hw *hw, struct cfg80211_wowlan *wowlan)
 	cancel_work_sync(&local->dynamic_ps_enable_work);
 	del_timer_sync(&local->dynamic_ps_timer);
 
-	local->wowlan = wowlan && local->open_count;
+	local->wowlan = wowlan;
 	if (local->wowlan) {
 		int err;
 
