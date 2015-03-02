1d93b850296339082b7ace26ed20385d3bc5e2c6
Valentin Rothberg <Valentin.Rothberg@lip6.fr>
2015-03-02 15:32:48 +0100

    power/smb347-charger.c: set IRQF_ONESHOT flag to ensure IRQ request
    Since commit 1c6c69525b40eb76de8adf039409722015927dc3 ("genirq: Reject
    bogus threaded irq requests") threaded IRQs without a primary handler
    need to be requested with IRQF_ONESHOT, otherwise the request may fail.
    
    Generated by: scripts/coccinelle/misc/irqf_oneshot.cocci
    
    Signed-off-by: Valentin Rothberg <Valentin.Rothberg@lip6.fr>
    Signed-off-by: Sebastian Reichel <sre@kernel.org>

--- drivers/power/smb347-charger.c
+++ drivers/power/smb347-charger.c
@@ -842,7 +842,8 @@ static int smb347_irq_init(struct smb347_charger *smb,
 		goto fail;
 
 	ret = request_threaded_irq(irq, NULL, smb347_interrupt,
-				   IRQF_TRIGGER_FALLING, client->name, smb);
+				   IRQF_TRIGGER_FALLING | IRQF_ONESHOT,
+				   client->name, smb);
 	if (ret < 0)
 		goto fail_gpio;
 
