d3a891652adb82e1973348c703a597cb54e41dea
Fabio Estevam <fabio.estevam@freescale.com>
2015-03-03 10:04:45 -0300

    of/overlay: Remove unused variable
    Commit 3e7f7626fd49a ("of/overlay: Do not generate duplicate nodes") removed
    the only use of the 'grandchild' variable, which leads to the following build
    warning:
    
    drivers/of/overlay.c: In function 'of_overlay_apply_single_device_node':
    drivers/of/overlay.c:89:31: warning: unused variable 'grandchild' [-Wunused-variable]
      struct device_node *tchild, *grandchild;
                                   ^
    
    Remove this unused variable.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- drivers/of/overlay.c
+++ drivers/of/overlay.c
@@ -86,7 +86,7 @@ static int of_overlay_apply_single_device_node(struct of_overlay *ov,
 		struct device_node *target, struct device_node *child)
 {
 	const char *cname;
-	struct device_node *tchild, *grandchild;
+	struct device_node *tchild;
 	int ret = 0;
 
 	cname = kbasename(child->full_name);
