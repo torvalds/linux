f4f8e73850589008095b1da3c8c17cf68bd1c62a
Joe Stringer <joestringer@nicira.com>
2015-03-02 18:49:56 -0800

    openvswitch: Fix serialization of non-masked set actions.
    Set actions consist of a regular OVS_KEY_ATTR_* attribute nested inside
    of a OVS_ACTION_ATTR_SET action attribute. When converting masked actions
    back to regular set actions, the inner attribute length was not changed,
    ie, double the length being serialized. This patch fixes the bug.
    
    Fixes: 83d2b9b ("net: openvswitch: Support masked set actions.")
    Signed-off-by: Joe Stringer <joestringer@nicira.com>
    Acked-by: Jarno Rajahalme <jrajahalme@nicira.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/openvswitch/flow_netlink.c
+++ net/openvswitch/flow_netlink.c
@@ -2253,14 +2253,20 @@ static int masked_set_action_to_set_action_attr(const struct nlattr *a,
 						struct sk_buff *skb)
 {
 	const struct nlattr *ovs_key = nla_data(a);
+	struct nlattr *nla;
 	size_t key_len = nla_len(ovs_key) / 2;
 
 	/* Revert the conversion we did from a non-masked set action to
 	 * masked set action.
 	 */
-	if (nla_put(skb, OVS_ACTION_ATTR_SET, nla_len(a) - key_len, ovs_key))
+	nla = nla_nest_start(skb, OVS_ACTION_ATTR_SET);
+	if (!nla)
 		return -EMSGSIZE;
 
+	if (nla_put(skb, nla_type(ovs_key), key_len, nla_data(ovs_key)))
+		return -EMSGSIZE;
+
+	nla_nest_end(skb, nla);
 	return 0;
 }
 
