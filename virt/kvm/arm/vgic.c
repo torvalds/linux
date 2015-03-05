0f37247574b3ef5b130116bbf7c0f9eb8a4c78c2
Mark Rutland <mark.rutland@arm.com>
2015-03-05 14:47:44 +0000

    KVM: vgic: add virt-capable compatible strings
    Several dts only list "arm,cortex-a7-gic" or "arm,gic-400" in their GIC
    compatible list, and while this is correct (and supported by the GIC
    driver), KVM will fail to detect that it can support these cases.
    
    This patch adds the missing strings to the VGIC code. The of_device_id
    entries are padded to keep the probe function data aligned.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Andre Przywara <andre.przywara@arm.com>
    Cc: Christoffer Dall <christoffer.dall@linaro.org>
    Cc: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Michal Simek <monstr@monstr.eu>
    Acked-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- virt/kvm/arm/vgic.c
+++ virt/kvm/arm/vgic.c
@@ -1865,8 +1865,10 @@ static struct notifier_block vgic_cpu_nb = {
 };
 
 static const struct of_device_id vgic_ids[] = {
-	{ .compatible = "arm,cortex-a15-gic", .data = vgic_v2_probe, },
-	{ .compatible = "arm,gic-v3", .data = vgic_v3_probe, },
+	{ .compatible = "arm,cortex-a15-gic",	.data = vgic_v2_probe, },
+	{ .compatible = "arm,cortex-a7-gic",	.data = vgic_v2_probe, },
+	{ .compatible = "arm,gic-400",		.data = vgic_v2_probe, },
+	{ .compatible = "arm,gic-v3",		.data = vgic_v3_probe, },
 	{},
 };
 
