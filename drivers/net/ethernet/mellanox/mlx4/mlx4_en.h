708b869bf56e58b0c41460ba7bf363bf50f330c2
Shani Michaeli <shanim@mellanox.com>
2015-03-05 20:16:13 +0200

    net/mlx4_en: Add QCN parameters and statistics handling
    Implement the IEEE DCB handlers for set/get QCN parameters and
    statistics reading per TC.
    
    Signed-off-by: Shani Michaeli <shanim@mellanox.com>
    Signed-off-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlx4/mlx4_en.h
+++ drivers/net/ethernet/mellanox/mlx4/mlx4_en.h
@@ -608,6 +608,7 @@ struct mlx4_en_priv {
 #ifdef CONFIG_MLX4_EN_DCB
 	struct ieee_ets ets;
 	u16 maxrate[IEEE_8021QAZ_MAX_TCS];
+	enum dcbnl_cndd_states cndd_state[IEEE_8021QAZ_MAX_TCS];
 #endif
 #ifdef CONFIG_RFS_ACCEL
 	spinlock_t filters_lock;
