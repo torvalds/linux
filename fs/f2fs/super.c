fdf6c8be337af72b066548f325abcbcdf626dea8
Wanpeng Li <wanpeng.li@linux.intel.com>
2015-03-06 15:00:54 +0800

    f2fs: fix extent cache memory leak
    extent tree/node slab cache is created during f2fs insmod,
    how, it isn't destroyed during f2fs rmmod, this patch fix
    it by destroy extent tree/node slab cache once rmmod f2fs.
    
    Signed-off-by: Wanpeng Li <wanpeng.li@linux.intel.com>
    Reviewed-by: Chao Yu <chao2.yu@samsung.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1323,6 +1323,7 @@ static void __exit exit_f2fs_fs(void)
 	remove_proc_entry("fs/f2fs", NULL);
 	f2fs_destroy_root_stats();
 	unregister_filesystem(&f2fs_fs_type);
+	destroy_extent_cache();
 	destroy_checkpoint_caches();
 	destroy_segment_manager_caches();
 	destroy_node_manager_caches();
