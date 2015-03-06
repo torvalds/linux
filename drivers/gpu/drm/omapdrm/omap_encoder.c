68dc0390d4e9d3f73fa6a7756a948ff97a40eccb
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2015-03-07 00:22:39 +0200

    drm: omapdrm: Implement encoder .disable() and .enable() operations
    The operations are required by the atomic helpers, implement them.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/omapdrm/omap_encoder.c
+++ drivers/gpu/drm/omapdrm/omap_encoder.c
@@ -124,12 +124,22 @@ static void omap_encoder_commit(struct drm_encoder *encoder)
 {
 }
 
+static void omap_encoder_disable(struct drm_encoder *encoder)
+{
+}
+
+static void omap_encoder_enable(struct drm_encoder *encoder)
+{
+}
+
 static const struct drm_encoder_helper_funcs omap_encoder_helper_funcs = {
 	.dpms = omap_encoder_dpms,
 	.mode_fixup = omap_encoder_mode_fixup,
 	.mode_set = omap_encoder_mode_set,
 	.prepare = omap_encoder_prepare,
 	.commit = omap_encoder_commit,
+	.disable = omap_encoder_disable,
+	.enable = omap_encoder_enable,
 };
 
 /*
