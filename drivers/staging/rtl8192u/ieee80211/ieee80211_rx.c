ca67dfefd852c5fba5aac232e20a0e52bfadf693
Dilek Uzulmez <dilekuzulmez@gmail.com>
2015-03-06 16:08:10 +0200

    Staging: rtl8192u: Added #include <linux/uaccess.h> instead of <asm/uaccess.h>
    The following patch fixes the checkpatch.pl warning:
    WARNING: Use #include <linux/uaccess.h> instead of <asm/uaccess.h>
    
    Signed-off-by: Dilek Uzulmez <dilekuzulmez@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/ieee80211/ieee80211_rx.c
+++ drivers/staging/rtl8192u/ieee80211/ieee80211_rx.c
@@ -38,7 +38,7 @@
 #include <linux/types.h>
 #include <linux/wireless.h>
 #include <linux/etherdevice.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <linux/ctype.h>
 
 #include "ieee80211.h"
