421d9df0628be16e55705573ab49d8ddb6a1d68c
Cyrille Pitchen <cyrille.pitchen@atmel.com>
2015-03-07 07:23:32 +0100

    net/macb: merge at91_ether driver into macb driver
    macb and at91_ether drivers can be compiled as modules, but the at91_ether
    driver use some functions and variables defined in the macb one, thus
    creating a dependency on the macb driver.
    
    Since these drivers are sharing the same logic we can easily merge
    at91_ether into macb.
    
    In order to factorize common probing logic we've added an ->init() function
    to struct macb_config (the structure associated with the compatible
    string), and moved macb specific init code from macb_probe to macb_init.
    
    Signed-off-by: Cyrille Pitchen <cyrille.pitchen@atmel.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>
    Tested-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/cadence/macb.h
+++ drivers/net/ethernet/cadence/macb.h
@@ -754,6 +754,7 @@ struct macb_or_gem_ops {
 struct macb_config {
 	u32			caps;
 	unsigned int		dma_burst_length;
+	int	(*init)(struct platform_device *pdev);
 };
 
 struct macb_queue {
@@ -823,15 +824,6 @@ struct macb {
 
 	u64			ethtool_stats[GEM_STATS_LEN];
 };
-
-extern const struct ethtool_ops macb_ethtool_ops;
-
-int macb_mii_init(struct macb *bp);
-int macb_ioctl(struct net_device *dev, struct ifreq *rq, int cmd);
-struct net_device_stats *macb_get_stats(struct net_device *dev);
-void macb_set_rx_mode(struct net_device *dev);
-void macb_set_hwaddr(struct macb *bp);
-void macb_get_hwaddr(struct macb *bp);
 
 static inline bool macb_is_gem(struct macb *bp)
 {
