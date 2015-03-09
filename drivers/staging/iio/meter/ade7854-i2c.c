07f83131e5b5d309dc8dd41911b66d3ce4c99323
Tina Johnson <tinajohnson.1234@gmail.com>
2015-03-09 16:11:19 +0530

    drivers: staging: iio: meter: Removed unnecessary variable
    Variable ret is used only to store the return value. Hence ret is
    removed and the return statement modified. Coccinelle was used to
    detect such removable variables:
    
    @rule1@
    identifier ret;
    expression e;
    @@
    
    -ret =
    +return
               e;
    -return ret;
    
    Signed-off-by: Tina Johnson <tinajohnson.1234@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/iio/meter/ade7854-i2c.c
+++ drivers/staging/iio/meter/ade7854-i2c.c
@@ -205,7 +205,6 @@ out:
 static int ade7854_i2c_probe(struct i2c_client *client,
 			     const struct i2c_device_id *id)
 {
-	int ret;
 	struct ade7854_state *st;
 	struct iio_dev *indio_dev;
 
@@ -225,9 +224,7 @@ static int ade7854_i2c_probe(struct i2c_client *client,
 	st->i2c = client;
 	st->irq = client->irq;
 
-	ret = ade7854_probe(indio_dev, &client->dev);
-
-	return ret;
+	return ade7854_probe(indio_dev, &client->dev);
 }
 
 static int ade7854_i2c_remove(struct i2c_client *client)
