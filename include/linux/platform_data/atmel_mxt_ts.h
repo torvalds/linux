8c3f7d9d66ccd405c7ba818c49fe7ca82e254372
Nick Dyer <nick.dyer@itdev.co.uk>
2015-03-10 15:19:49 +0000

    Input: atmel_mxt_ts - add suspend modes

--- include/linux/platform_data/atmel_mxt_ts.h
+++ include/linux/platform_data/atmel_mxt_ts.h
@@ -10,16 +10,18 @@
  * option) any later version.
  */
 
-#ifndef __LINUX_ATMEL_MXT_TS_H
-#define __LINUX_ATMEL_MXT_TS_H
+#ifndef __LINUX_PLATFORM_DATA_ATMEL_MXT_TS_H
+#define __LINUX_PLATFORM_DATA_ATMEL_MXT_TS_H
 
 #include <linux/types.h>
+#include <dt-bindings/input/atmel_mxt_ts.h>
 
 /* The platform data for the Atmel maXTouch touchscreen driver */
 struct mxt_platform_data {
 	unsigned long irqflags;
 	u8 t19_num_keys;
 	const unsigned int *t19_keymap;
+	enum mxt_suspend_mode suspend_mode;
 	int t15_num_keys;
 	const unsigned int *t15_keymap;
 	unsigned long gpio_reset;
@@ -27,4 +29,4 @@ struct mxt_platform_data {
 	const char *input_name;
 };
 
-#endif /* __LINUX_ATMEL_MXT_TS_H */
+#endif /* __LINUX_PLATFORM_DATA_ATMEL_MXT_TS_H */
