2a12d9681b39fd9a221199e6ebb143ae61efc412
Ian Abbott <abbotti@mev.co.uk>
2015-03-10 16:10:38 +0000

    staging: comedi: adv_pci1710: include new "comedi_pci.h" header
    Include the new "../comedi_pci.h" header instead of <linux/pci.h> and
    "../comedidev.h", which will now get included indirectly.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/adv_pci1710.c
+++ drivers/staging/comedi/drivers/adv_pci1710.c
@@ -42,10 +42,9 @@ Configuration options:
 */
 
 #include <linux/module.h>
-#include <linux/pci.h>
 #include <linux/interrupt.h>
 
-#include "../comedidev.h"
+#include "../comedi_pci.h"
 
 #include "comedi_fc.h"
 #include "comedi_8254.h"
