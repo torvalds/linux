dc0961bc816b9365cb37a68b34e84b625f7c7f0f
Julia Lawall <Julia.Lawall@lip6.fr>
2015-03-11 17:56:36 +0100

    ARM: cns3xxx: don't export static symbol
    The semantic patch that fixes this problem is as follows:
    (http://coccinelle.lip6.fr/)
    
    // <smpl>
    @r@
    type T;
    identifier f;
    @@
    
    static T f (...) { ... }
    
    @@
    identifier r.f;
    declarer name EXPORT_SYMBOL;
    @@
    
    -EXPORT_SYMBOL(f);
    // </smpl>
    
    Signed-off-by: Julia Lawall <Julia.Lawall@lip6.fr>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Krzysztof Halasa <khalasa@piap.pl>

--- arch/arm/mach-cns3xxx/pm.c
+++ arch/arm/mach-cns3xxx/pm.c
@@ -73,7 +73,6 @@ static void cns3xxx_pwr_soft_rst_force(unsigned int block)
 
 	__raw_writel(reg, PM_SOFT_RST_REG);
 }
-EXPORT_SYMBOL(cns3xxx_pwr_soft_rst_force);
 
 void cns3xxx_pwr_soft_rst(unsigned int block)
 {
