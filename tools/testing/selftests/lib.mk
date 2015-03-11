32dcfba6f8df667e4b915e0542b33ccbc8e76fa8
Michael Ellerman <mpe@ellerman.id.au>
2015-03-11 15:06:00 +1100

    selftests: Add install target
    This adds make install support to selftests. The basic usage is:
    
    $ cd tools/testing/selftests
    $ make install
    
    That installs into tools/testing/selftests/install, which can then be
    copied where ever necessary.
    
    The install destination is also configurable using eg:
    
    $ INSTALL_PATH=/mnt/selftests make install
    
    The implementation uses two targets in the child makefiles. The first
    "install" is expected to install all files into $(INSTALL_PATH).
    
    The second, "emit_tests", is expected to emit the test instructions (ie.
    bash script) on stdout. Separating this from install means the child
    makefiles need no knowledge of the location of the test script.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Signed-off-by: Shuah Khan <shuahkh@osg.samsung.com>

--- tools/testing/selftests/lib.mk
+++ tools/testing/selftests/lib.mk
@@ -7,4 +7,25 @@ endef
 run_tests: all
 	$(RUN_TESTS)
 
-.PHONY: run_tests all clean
+define INSTALL_RULE
+	mkdir -p $(INSTALL_PATH)
+	install -t $(INSTALL_PATH) $(TEST_PROGS) $(TEST_FILES)
+endef
+
+install: all
+ifdef INSTALL_PATH
+	$(INSTALL_RULE)
+else
+	$(error Error: set INSTALL_PATH to use install)
+endif
+
+define EMIT_TESTS
+	@for TEST in $(TEST_PROGS); do \
+		echo "(./$$TEST && echo \"selftests: $$TEST [PASS]\") || echo \"selftests: $$TEST [FAIL]\""; \
+	done;
+endef
+
+emit_tests:
+	$(EMIT_TESTS)
+
+.PHONY: run_tests all clean install emit_tests
