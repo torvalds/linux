8f4d81863ba4e8dfee93bd50840f1099a296251f
Oleg Nesterov <oleg@redhat.com>
2015-03-11 18:34:29 +0100

    x86/fpu: Introduce restore_init_xstate()
    Extract the "use_eager_fpu()" code from drop_init_fpu() into a new,
    simple helper restore_init_xstate(). The next patch adds another user.
    
    - It is not clear why we do not check use_fxsr() like fpu_restore_checking()
      does. eager_fpu_init_bp() calls setup_init_fpu_buf() too, and we have the
      "eagerfpu=on" kernel option.
    
    - Ignoring the fact that init_xstate_buf is "struct xsave_struct *", not
      "union thread_xstate *", it is not clear why we can not simply use
      fpu_restore_checking() and avoid the code duplication.
    
    - It is not clear why we can't call setup_init_fpu_buf() unconditionally
      to always create init_xstate_buf(). Then do_device_not_available() path
      (at least) could use restore_init_xstate() too. It doesn't need to init
      fpu->state, its content doesn't matter until unlazy_fpu()/__switch_to()/etc
      which overwrites this memory anyway.
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: Fenghua Yu <fenghua.yu@intel.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Pekka Riikonen <priikone@iki.fi>
    Cc: Quentin Casasnovas <quentin.casasnovas@oracle.com>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Suresh Siddha <sbsiddha@gmail.com>
    Link: http://lkml.kernel.org/r/20150311173429.GD5032@redhat.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/fpu-internal.h
+++ arch/x86/include/asm/fpu-internal.h
@@ -401,16 +401,20 @@ static inline void drop_fpu(struct task_struct *tsk)
 	preempt_enable();
 }
 
+static inline void restore_init_xstate(void)
+{
+	if (use_xsave())
+		xrstor_state(init_xstate_buf, -1);
+	else
+		fxrstor_checking(&init_xstate_buf->i387);
+}
+
 static inline void drop_init_fpu(struct task_struct *tsk)
 {
 	if (!use_eager_fpu())
 		drop_fpu(tsk);
-	else {
-		if (use_xsave())
-			xrstor_state(init_xstate_buf, -1);
-		else
-			fxrstor_checking(&init_xstate_buf->i387);
-	}
+	else
+		restore_init_xstate();
 }
 
 /*
