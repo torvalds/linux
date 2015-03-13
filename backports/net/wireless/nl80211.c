16e525801dc8b51f5d95bc925080f5fb77872546
Mauro Ribeiro <mauro.ribeiro@hardkernel.com>
2015-03-12 22:43:10 -0300

    apply backport patch collateral-evolutions/network/0002-disable-dump-adjust-on-old-kernels.patch
    Change-Id: I4357d96455e073d51bf76a0d9df31045099efa97

--- backports/net/wireless/nl80211.c
+++ backports/net/wireless/nl80211.c
@@ -1802,6 +1802,7 @@ static int nl80211_dump_wiphy(struct sk_buff *skb, struct netlink_callback *cb)
 						 cb->nlh->nlmsg_seq,
 						 NLM_F_MULTI, state);
 			if (ret < 0) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,1,0)
 				/*
 				 * If sending the wiphy data didn't fit (ENOBUFS
 				 * or EMSGSIZE returned), this SKB is still
@@ -1823,6 +1824,7 @@ static int nl80211_dump_wiphy(struct sk_buff *skb, struct netlink_callback *cb)
 					rtnl_unlock();
 					return 1;
 				}
+#endif
 				idx--;
 				break;
 			}
