4040835afa11579029d2b1bec09757a49d9ef3d7
Mauro Ribeiro <mauro.ribeiro@hardkernel.com>
2015-03-12 22:43:13 -0300

    apply backport patch collateral-evolutions/network/0012-driver-quirks/drivers_net_wireless_cw1200_sdio.patch
    Change-Id: Id001d11336f62ea800f865456c9178af717857f4

--- backports/drivers/net/wireless/cw1200/cw1200_sdio.c
+++ backports/drivers/net/wireless/cw1200/cw1200_sdio.c
@@ -253,6 +253,12 @@ static size_t cw1200_sdio_align_size(struct hwbus_priv *self, size_t size)
 	else
 		size = sdio_align_size(self->func, size);
 
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3, 2, 0))
+	/* A quirk to handle this was committed in 3.2-rc */
+	if (size == SDIO_BLOCK_SIZE)
+		size += SDIO_BLOCK_SIZE;  /* HW bug; force use of block mode */
+#endif
+
 	return size;
 }
 
