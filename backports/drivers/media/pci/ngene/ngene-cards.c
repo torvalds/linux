ad3a4ec3cce09f9d28e9f08d8c09db86dff261e7
Mauro Ribeiro <mauro.ribeiro@hardkernel.com>
2015-03-12 22:43:38 -0300

    apply backport patch collateral-evolutions/network/0042-pci_error_handlers/ngene.patch
    Change-Id: I36504954cbee84305aaa14cd84230fcdf13283f8

--- backports/drivers/media/pci/ngene/ngene-cards.c
+++ backports/drivers/media/pci/ngene/ngene-cards.c
@@ -798,7 +798,11 @@ static void ngene_resume(struct pci_dev *dev)
 	printk(KERN_INFO DEVICE_NAME ": resume\n");
 }
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,7,0)
 static const struct pci_error_handlers ngene_errors = {
+#else
+static struct pci_error_handlers ngene_errors = {
+#endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(3,7,0) */
 	.error_detected = ngene_error_detected,
 	.link_reset = ngene_link_reset,
 	.slot_reset = ngene_slot_reset,
