2fd80ce63f5d77e5a8e894c25a00ed1d25cd7d9c
Mauro Ribeiro <mauro.ribeiro@hardkernel.com>
2015-03-12 22:43:16 -0300

    apply backport patch collateral-evolutions/network/0017-get_ts_info/drivers_net_usb_usbnet.patch
    Change-Id: I36fc45db7c6016a0e3ce8356a463bb98b29de119

--- backports/drivers/net/usb/usbnet.c
+++ backports/drivers/net/usb/usbnet.c
@@ -1022,7 +1022,9 @@ static const struct ethtool_ops usbnet_ethtool_ops = {
 	.get_drvinfo		= usbnet_get_drvinfo,
 	.get_msglevel		= usbnet_get_msglevel,
 	.set_msglevel		= usbnet_set_msglevel,
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,5,0))
 	.get_ts_info		= ethtool_op_get_ts_info,
+#endif
 };
 
 /*-------------------------------------------------------------------------*/
