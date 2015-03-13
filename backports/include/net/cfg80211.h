80386d6a60214e1d0b9a5701a0bdbf7fb8e25f4d
Mauro Ribeiro <mauro.ribeiro@hardkernel.com>
2015-03-12 22:43:10 -0300

    apply backport patch collateral-evolutions/network/0003-cfg80211-wext-padding/include_net_cfg80211.patch
    Change-Id: I84890ed9a9608ec713b9e0c471b3ab5e943ffe90

--- backports/include/net/cfg80211.h
+++ backports/include/net/cfg80211.h
@@ -3081,6 +3081,9 @@ struct wiphy_vendor_command {
 struct wiphy {
 	/* assign these fields before you register the wiphy */
 
+#define WIPHY_COMPAT_PAD_SIZE	2048
+	u8 padding[WIPHY_COMPAT_PAD_SIZE];
+
 	/* permanent MAC address(es) */
 	u8 perm_addr[ETH_ALEN];
 	u8 addr_mask[ETH_ALEN];
