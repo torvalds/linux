ae67bf0188cbb9d1786bdfcca9e1976cb36ee327
Florian Fainelli <f.fainelli@gmail.com>
2015-03-13 12:11:06 -0700

    net: bcmgenet: update ring producer index and buffer count in xmit
    There is no need to have both bcmgenet_xmit_single() and
    bcmgenet_xmit_frag() perform a free_bds decrement and a prod_index
    increment by one. In case one of these functions fails to map a SKB or
    fragment for transmit, we will return and exit bcmgenet_xmit() with an
    error.
    
    We can therefore safely use our local copy of nr_frags to know by how
    much we should decrement the number of free buffers available, and by
    how much the producer count must be incremented and do this in the tail
    of bcmgenet_xmit().
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Acked-by: Petri Gynther <pgynther@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/genet/bcmgenet.c
+++ drivers/net/ethernet/broadcom/genet/bcmgenet.c
@@ -1130,11 +1130,6 @@ static int bcmgenet_xmit_single(struct net_device *dev,
 
 	dmadesc_set(priv, tx_cb_ptr->bd_addr, mapping, length_status);
 
-	/* Decrement total BD count and advance our write pointer */
-	ring->free_bds -= 1;
-	ring->prod_index += 1;
-	ring->prod_index &= DMA_P_INDEX_MASK;
-
 	return 0;
 }
 
@@ -1172,11 +1167,6 @@ static int bcmgenet_xmit_frag(struct net_device *dev,
 	dmadesc_set(priv, tx_cb_ptr->bd_addr, mapping,
 		    (frag->size << DMA_BUFLENGTH_SHIFT) | dma_desc_flags |
 		    (priv->hw_params->qtag_mask << DMA_TX_QTAG_SHIFT));
-
-
-	ring->free_bds -= 1;
-	ring->prod_index += 1;
-	ring->prod_index &= DMA_P_INDEX_MASK;
 
 	return 0;
 }
@@ -1321,9 +1311,11 @@ static netdev_tx_t bcmgenet_xmit(struct sk_buff *skb, struct net_device *dev)
 
 	skb_tx_timestamp(skb);
 
-	/* we kept a software copy of how much we should advance the TDMA
-	 * producer index, now write it down to the hardware
-	 */
+	/* Decrement total BD count and advance our write pointer */
+	ring->free_bds -= nr_frags + 1;
+	ring->prod_index += nr_frags + 1;
+	ring->prod_index &= DMA_P_INDEX_MASK;
+
 	bcmgenet_tdma_ring_writel(priv, ring->index,
 				  ring->prod_index, TDMA_PROD_INDEX);
 
