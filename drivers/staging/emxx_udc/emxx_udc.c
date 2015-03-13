ca3d253eb9676ffc1bbe616b485826f337c7a616
Somya Anand <somyaanand214@gmail.com>
2015-03-14 01:03:08 +0530

    Staging: emxx_udc: Iterate list using list_for_each_entry
    Code using doubly linked list is iterated generally  using list_empty and
    list_entry functions, but it can be better written using list_for_each_entry
    macro.
    
    This patch replaces the while loop containing list_empty and list_entry with
    list_for_each_entry and list_for_each_entry_safe. list_for_each_entry is a
    macro which is used to iterate over a list of given type. So while loop used to
    iterate over a list can be replaced with list_for_each_entry macro. However, if
    list_del is used in the loop, then list_for_each_entry_safe is a better choice.
    This transformation is done by using the following coccinelle script.
    
    @ rule1 @
    expression E1;
    identifier I1, I2;
    type T;
    iterator name list_for_each_entry;
    @@
    
    - while (list_empty(&E1) == 0)
    + list_for_each_entry (I1, &E1, I2)
     {
    	...when != T *I1;
    - 	I1 = list_entry(E1.next, T, I2);
        ...when != list_del(...);
           when != list_del_init(...);
     }
    
    @ rule2  @
    expression E1;
    identifier I1, I2;
    type T;
    iterator name list_for_each_entry_safe;
    @@
       T *I1;
    +  T *tmp;
      ...
    - while (list_empty(&E1) == 0)
    + list_for_each_entry_safe (I1, tmp, &E1, I2)
     {
    	...when != T *I1;
    - 	I1 = list_entry(E1.next, T, I2);
        ...
     }
    
    Signed-off-by: Somya Anand <somyaanand214@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/emxx_udc/emxx_udc.c
+++ drivers/staging/emxx_udc/emxx_udc.c
@@ -2229,8 +2229,7 @@ static int _nbu2ss_nuke(struct nbu2ss_udc *udc,
 		return 0;
 
 	/* called with irqs blocked */
-	while (!list_empty(&ep->queue)) {
-		req = list_entry(ep->queue.next, struct nbu2ss_req, queue);
+	list_for_each_entry(req, &ep->queue, queue) {
 		_nbu2ss_ep_done(ep, req, status);
 	}
 
