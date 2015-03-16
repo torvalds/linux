34fff6db29eac1f0c48ff3ab95db9b24ffd6c51a
Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
2015-03-17 00:00:56 +0100

    staging: rtl8192e: rtllib.h: remove duplicated container_of
    This macro is already defined in kernel.h - there is no need to redefine it.
    
    Signed-off-by: Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtllib.h
+++ drivers/staging/rtl8192e/rtllib.h
@@ -66,20 +66,6 @@
 #define IW_CUSTOM_MAX	256	/* In bytes */
 #endif
 
-#ifndef container_of
-/**
- * container_of - cast a member of a structure out to the containing structure
- *
- * @ptr:	the pointer to the member.
- * @type:       the type of the container struct this is embedded in.
- * @member:     the name of the member within the struct.
- *
- */
-#define container_of(ptr, type, member) ({		      \
-	const typeof(((type *)0)->member)*__mptr = (ptr);    \
-	(type *)((char *)__mptr - offsetof(type, member)); })
-#endif
-
 #define skb_tail_pointer_rsl(skb) skb_tail_pointer(skb)
 
 #define EXPORT_SYMBOL_RSL(x) EXPORT_SYMBOL(x)
