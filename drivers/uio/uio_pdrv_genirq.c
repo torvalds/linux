4d8beff2ae07fad85d723b4cdf704b05f0ed4794
Fabian Frederick <fabf@skynet.be>
2015-03-16 20:54:38 +0100

    uio: constify of_device_id array
    of_device_id is always used as const.
    (See driver.of_match_table and open firmware functions)
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/uio/uio_pdrv_genirq.c
+++ drivers/uio/uio_pdrv_genirq.c
@@ -252,7 +252,7 @@ static const struct dev_pm_ops uio_pdrv_genirq_dev_pm_ops = {
 };
 
 #ifdef CONFIG_OF
-static struct of_device_id uio_of_genirq_match[] = {
+static const struct of_device_id uio_of_genirq_match[] = {
 	{ /* This is filled with module_parm */ },
 	{ /* Sentinel */ },
 };
