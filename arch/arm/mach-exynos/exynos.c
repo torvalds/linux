cfdda3535f87e752780ca18a57d13db58f6a6913
Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
2015-03-18 03:26:11 +0900

    ARM: EXYNOS: Fix build breakage cpuidle on !SMP
    The Exynos cpuidle driver has coupled cpuidle built-in so it cannot be
    built without SMP:
    
    arch/arm/mach-exynos/pm.c: In function 'exynos_cpu0_enter_aftr':
    arch/arm/mach-exynos/pm.c:246:4: error: implicit declaration of function 'arch_send_wakeup_ipi_mask' [-Werror=implicit-function-declaration]
    arch/arm/mach-exynos/built-in.o: In function 'exynos_pre_enter_aftr':
    ../arch/arm/mach-exynos/pm.c:300: undefined reference to 'cpu_boot_reg_base'
    arch/arm/mach-exynos/built-in.o: In function 'exynos_cpu1_powerdown':
    ../arch/arm/mach-exynos/pm.c:282: undefined reference to 'exynos_cpu_power_down'
    
    Fix it by adding missing checks for SMP.
    
    Reported-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Signed-off-by: Kukjin Kim <kgene@kernel.org>

--- arch/arm/mach-exynos/exynos.c
+++ arch/arm/mach-exynos/exynos.c
@@ -211,7 +211,7 @@ static void __init exynos_dt_machine_init(void)
 	if (!IS_ENABLED(CONFIG_SMP))
 		exynos_sysram_init();
 
-#ifdef CONFIG_ARM_EXYNOS_CPUIDLE
+#if defined(CONFIG_SMP) && defined(CONFIG_ARM_EXYNOS_CPUIDLE)
 	if (of_machine_is_compatible("samsung,exynos4210"))
 		exynos_cpuidle.dev.platform_data = &cpuidle_coupled_exynos_data;
 #endif
