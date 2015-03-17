b41caa99a25f62326be451592c493de29347d142
Johan Hovold <johan@hovoldconsulting.com>
2015-03-17 18:24:29 +0100

    greybus: pwm: fix config-request alignment
    Fix alignment of the duty and period-fields in the config request,
    which should follow the which-field without any inserted padding.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/pwm.c
+++ drivers/staging/greybus/pwm.c
@@ -56,8 +56,8 @@ struct gb_pwm_deactivate_request {
 
 struct gb_pwm_config_request {
 	__u8	which;
-	__le32	duty;
-	__le32	period;
+	__le32	duty __packed;
+	__le32	period __packed;
 };
 
 struct gb_pwm_polarity_request {
