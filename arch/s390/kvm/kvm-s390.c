a3ed8dae6e3db479ca275883ba7fe994170b0ae6
Christian Borntraeger <borntraeger@de.ibm.com>
2015-03-18 13:54:31 +0100

    KVM: s390: enable more features that need no hypervisor changes
    After some review about what these facilities do, the following
    facilities will work under KVM and can, therefore, be reported
    to the guest if the cpu model and the host cpu provide this bit.
    
    There are plans underway to make the whole bit thing more readable,
    but its not yet finished. So here are some last bit changes and
    we enhance the KVM mask with:
    
    9 The sense-running-status facility is installed in the
      z/Architecture architectural mode.
      ---> handled by SIE or KVM
    
    10 The conditional-SSKE facility is installed in the
       z/Architecture architectural mode.
      ---> handled by SIE. KVM will retry SIE
    
    13 The IPTE-range facility is installed in the
       z/Architecture architectural mode.
      ---> handled by SIE. KVM will retry SIE
    
    36 The enhanced-monitor facility is installed in the
       z/Architecture architectural mode.
      ---> handled by SIE
    
    47 The CMPSC-enhancement facility is installed in the
       z/Architecture architectural mode.
      ---> handled by SIE
    
    48 The decimal-floating-point zoned-conversion facility
       is installed in the z/Architecture architectural mode.
      ---> handled by SIE
    
    49 The execution-hint, load-and-trap, miscellaneous-
       instruction-extensions and processor-assist
      ---> handled by SIE
    
    51 The local-TLB-clearing facility is installed in the
       z/Architecture architectural mode.
      ---> handled by SIE
    
    52 The interlocked-access facility 2 is installed.
      ---> handled by SIE
    
    53 The load/store-on-condition facility 2 and load-and-
       zero-rightmost-byte facility are installed in the
       z/Architecture architectural mode.
      ---> handled by SIE
    
    57 The message-security-assist-extension-5 facility is
      installed in the z/Architecture architectural mode.
      ---> handled by SIE
    
    66 The reset-reference-bits-multiple facility is installed
      in the z/Architecture architectural mode.
      ---> handled by SIE. KVM will retry SIE
    
    80 The decimal-floating-point packed-conversion
       facility is installed in the z/Architecture architectural
       mode.
      ---> handled by SIE
    
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Tested-by: Michael Mueller <mimu@linux.vnet.ibm.com>
    Acked-by: Cornelia Huck <cornelia.huck@de.ibm.com>

--- arch/s390/kvm/kvm-s390.c
+++ arch/s390/kvm/kvm-s390.c
@@ -105,8 +105,8 @@ struct kvm_stats_debugfs_item debugfs_entries[] = {
 
 /* upper facilities limit for kvm */
 unsigned long kvm_s390_fac_list_mask[] = {
-	0xff82fffbf4fc2000UL,
-	0x005c000000000000UL,
+	0xffe6fffbfcfdfc40UL,
+	0x205c800000000000UL,
 };
 
 unsigned long kvm_s390_fac_list_mask_size(void)
