715a03d2848275269bd4014a49212cc83452f32d
Kinglong Mee <kinglongmee@gmail.com>
2015-03-20 15:56:40 +0800

    NFSD: restore trace event lost in mismerge
    31ef83dc05 "nfsd: add trace events" had a typo that dropped a trace
    event and replaced it by an incorrect recursive call to
    nfsd4_cb_layout_fail.  133d558216d9 "Subject: nfsd: don't recursively
    call nfsd4_cb_layout_fail" fixed the crash, this restores the
    tracepoint.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Kinglong Mee <kinglongmee@gmail.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/nfs4layouts.c
+++ fs/nfsd/nfs4layouts.c
@@ -587,6 +587,8 @@ nfsd4_cb_layout_fail(struct nfs4_layout_stateid *ls)
 
 	rpc_ntop((struct sockaddr *)&clp->cl_addr, addr_str, sizeof(addr_str));
 
+	trace_layout_recall_fail(&ls->ls_stid.sc_stateid);
+
 	printk(KERN_WARNING
 		"nfsd: client %s failed to respond to layout recall. "
 		"  Fencing..\n", addr_str);
