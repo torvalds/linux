b6e5b8f1a90230f922de8af59cdaf1ad83e1ac1e
Ricardo Ribalda <ricardo.ribalda@gmail.com>
2015-03-20 10:45:43 -0300

    [media] media: New flag V4L2_CTRL_FLAG_EXECUTE_ON_WRITE
    Create a new flag that represent controls which its value needs to be
    passed to the driver even if it has not changed.
    
    They typically represent actions, like triggering a flash or clearing an
    error flag. So writing to such a control means some action is executed.
    
    Signed-off-by: Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -1457,6 +1457,7 @@ struct v4l2_querymenu {
 #define V4L2_CTRL_FLAG_WRITE_ONLY 	0x0040
 #define V4L2_CTRL_FLAG_VOLATILE		0x0080
 #define V4L2_CTRL_FLAG_HAS_PAYLOAD	0x0100
+#define V4L2_CTRL_FLAG_EXECUTE_ON_WRITE	0x0200
 
 /*  Query flags, to be ORed with the control ID */
 #define V4L2_CTRL_FLAG_NEXT_CTRL	0x80000000
