381ca677af0a898af0e19002ea445055e6f0923a
Haneen Mohammed <hamohammed.sa@gmail.com>
2015-03-21 08:51:35 +0300

    Staging: lustre: remove extern in .c file
    This patch removes extern from .c file; for the variable is not used
    anywhere else in the file.
    In addition, it mark it static in the one file it is in
    "ldlm_resource.c".
    
    Signed-off-by: Haneen Mohammed <hamohammed.sa@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ldlm/ldlm_pool.c
+++ drivers/staging/lustre/lustre/ldlm/ldlm_pool.c
@@ -141,8 +141,6 @@
  */
 #define LDLM_POOL_SLV_SHIFT (10)
 
-extern struct proc_dir_entry *ldlm_ns_proc_dir;
-
 static inline __u64 dru(__u64 val, __u32 shift, int round_up)
 {
 	return (val + (round_up ? (1 << shift) - 1 : 0)) >> shift;
