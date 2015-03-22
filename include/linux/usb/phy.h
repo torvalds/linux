e842b84c8e7221c45c8dbd7de09185c6149e1cf9
NeilBrown <neilb@suse.de>
2015-03-23 09:52:48 +1100

    usb: phy: Add interface to get phy give of device_node.
    Split the "get phy from device_node" functionality out of
    "get phy by phandle" so it can be used directly.
    
    This is useful when a battery-charger is intimately associated with a
    particular phy but handled by a separate driver.  The charger
    can find the device_node based on sibling relationships
    without the need for a redundant declaration in the devicetree
    description.
    
    As a peripheral that gets a phy will often want to register a
    notifier block, and de-register it later, that functionality
    is included so the de-registration is automatic.
    
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: NeilBrown <neilb@suse.de>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- include/linux/usb/phy.h
+++ include/linux/usb/phy.h
@@ -205,6 +205,8 @@ extern struct usb_phy *usb_get_phy_dev(struct device *dev, u8 index);
 extern struct usb_phy *devm_usb_get_phy_dev(struct device *dev, u8 index);
 extern struct usb_phy *devm_usb_get_phy_by_phandle(struct device *dev,
 	const char *phandle, u8 index);
+extern struct usb_phy *devm_usb_get_phy_by_node(struct device *dev,
+	struct device_node *node, struct notifier_block *nb);
 extern void usb_put_phy(struct usb_phy *);
 extern void devm_usb_put_phy(struct device *dev, struct usb_phy *x);
 extern int usb_bind_phy(const char *dev_name, u8 index,
