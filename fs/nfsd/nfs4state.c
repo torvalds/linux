fc26c3860a6a4a9396f401024f1cc91a95a59143
Jeff Layton <jlayton@poochiereds.net>
2015-03-23 10:53:43 -0400

    nfsd: remove bogus setting of status in nfsd4_process_open2
    status is always reset after this (and it doesn't make much sense there
    anyway).
    
    Signed-off-by: Jeff Layton <jeff.layton@primarydata.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -4049,7 +4049,6 @@ nfsd4_process_open2(struct svc_rqst *rqstp, struct svc_fh *current_fh, struct nf
 		status = nfserr_bad_stateid;
 		if (nfsd4_is_deleg_cur(open))
 			goto out;
-		status = nfserr_jukebox;
 	}
 
 	/*
