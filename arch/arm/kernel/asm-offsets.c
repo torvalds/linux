8512287a8165592466cb9cb347ba94892e9c56a5
Nathan Lynch <nathan_lynch@mentor.com>
2015-03-25 19:14:22 +0100

    ARM: 8330/1: add VDSO user-space code
    Place VDSO-related user-space code in arch/arm/kernel/vdso/.
    
    It is almost completely written in C with some assembly helpers to
    load the data page address, sample the counter, and fall back to
    system calls when necessary.
    
    The VDSO can service gettimeofday and clock_gettime when
    CONFIG_ARM_ARCH_TIMER is enabled and the architected timer is present
    (and correctly configured).  It reads the CP15-based virtual counter
    to compute high-resolution timestamps.
    
    Of particular note is that a post-processing step ("vdsomunge") is
    necessary to produce a shared object which is architecturally allowed
    to be used by both soft- and hard-float EABI programs.
    
    The 2012 edition of the ARM ABI defines Tag_ABI_VFP_args = 3 "Code is
    compatible with both the base and VFP variants; the user did not
    permit non-variadic functions to pass FP parameters/results."
    Unfortunately current toolchains do not support this tag, which is
    ideally what we would use.
    
    The best available option is to ensure that both EF_ARM_ABI_FLOAT_SOFT
    and EF_ARM_ABI_FLOAT_HARD are unset in the ELF header's e_flags,
    indicating that the shared object is "old" and should be accepted for
    backward compatibility's sake.  While binutils < 2.24 appear to
    produce a vdso.so with both flags clear, 2.24 always sets
    EF_ARM_ABI_FLOAT_SOFT, with no way to inhibit this behavior.  So we
    have to fix things up with a custom post-processing step.
    
    In fact, the VDSO code in glibc does much less validation (including
    checking these flags) than the code for handling conventional
    file-backed shared libraries, so this is a bit moot unless glibc's
    VDSO code becomes more strict.
    
    Signed-off-by: Nathan Lynch <nathan_lynch@mentor.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/asm-offsets.c
+++ arch/arm/kernel/asm-offsets.c
@@ -25,6 +25,7 @@
 #include <asm/memory.h>
 #include <asm/procinfo.h>
 #include <asm/suspend.h>
+#include <asm/vdso_datapage.h>
 #include <asm/hardware/cache-l2x0.h>
 #include <linux/kbuild.h>
 
@@ -209,6 +210,10 @@ int main(void)
   DEFINE(KVM_VGIC_VCTRL,	offsetof(struct kvm, arch.vgic.vctrl_base));
 #endif
   DEFINE(KVM_VTTBR,		offsetof(struct kvm, arch.vttbr));
+#endif
+  BLANK();
+#ifdef CONFIG_VDSO
+  DEFINE(VDSO_DATA_SIZE,	sizeof(union vdso_data_store));
 #endif
   return 0; 
 }
