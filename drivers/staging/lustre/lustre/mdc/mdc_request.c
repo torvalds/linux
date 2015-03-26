f42894004727ffdfb3b0826b0047eaf44cbdece6
Christopher J. Morrone <morrone2@llnl.gov>
2015-03-25 21:53:18 -0400

    staging/lustre/mdc: Handle empty but non-zero acl xattr
    We have found that posix_acl_access can have a value
    of \002\000\000\000.  In that case body->aclsize is
    non-zero, but the there are no actuall acls stored
    in the xattr.
    
    In mdc_unpack_acl(), it only checks IS_ERR() on the
    pointer returned by posix_acl_from_xattr(), it does not
    check for NULL.  Because of the above situation, the
    xattr aclsize can be non-zero, but posic_acl_from_xattr()
    still returns NULL.  Passing NULL to posix_acl_valid()
    crashes the kernel.
    
    We add a check to properly handle the NULL return value.
    
    Signed-off-by: Christopher J. Morrone <morrone2@llnl.gov>
    Reviewed-on: http://review.whamcloud.com/11989
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-5150
    Reviewed-by: Fan Yong <fan.yong@intel.com>
    Reviewed-by: Bob Glossman <bob.glossman@intel.com>
    Signed-off-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/mdc/mdc_request.c
+++ drivers/staging/lustre/lustre/mdc/mdc_request.c
@@ -481,6 +481,9 @@ static int mdc_unpack_acl(struct ptlrpc_request *req, struct lustre_md *md)
 		return -EPROTO;
 
 	acl = posix_acl_from_xattr(&init_user_ns, buf, body->aclsize);
+	if (acl == NULL)
+		return 0;
+
 	if (IS_ERR(acl)) {
 		rc = PTR_ERR(acl);
 		CERROR("convert xattr to acl: %d\n", rc);
