bd50752034e4c4847d413e812f554f1bde1e668c
Tadeusz Struk <tadeusz.struk@intel.com>
2015-03-30 14:25:43 -0700

    crypto: af_alg - make exports consistant
    Use EXPORT_SYMBOL_GPL instead of EXPORT_SYMBOL.
    
    Signed-off-by: Tadeusz Struk <tadeusz.struk@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- crypto/af_alg.c
+++ crypto/af_alg.c
@@ -381,7 +381,7 @@ void af_alg_link_sg(struct af_alg_sgl *sgl_prev, struct af_alg_sgl *sgl_new)
 	sg_unmark_end(sgl_prev->sg + sgl_prev->npages - 1);
 	sg_chain(sgl_prev->sg, sgl_prev->npages + 1, sgl_new->sg);
 }
-EXPORT_SYMBOL(af_alg_link_sg);
+EXPORT_SYMBOL_GPL(af_alg_link_sg);
 
 void af_alg_free_sg(struct af_alg_sgl *sgl)
 {
