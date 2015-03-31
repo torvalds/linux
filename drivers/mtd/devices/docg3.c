45fd357a491ecc56c0f9bb110bd05d9caf28889f
Brian Norris <computersforpeace@gmail.com>
2015-03-31 09:53:37 -0700

    mtd: docg3: remove invalid __exit annotations
    The .remove callback may be used when detaching a device via sysfs, so
    we can't expect to free up this memory.
    
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>
    Acked-by: Robert Jarzmik <robert.jarzmik@free.fr>

--- drivers/mtd/devices/docg3.c
+++ drivers/mtd/devices/docg3.c
@@ -1805,7 +1805,7 @@ static int __init doc_dbg_register(struct docg3 *docg3)
 	}
 }
 
-static void __exit doc_dbg_unregister(struct docg3 *docg3)
+static void doc_dbg_unregister(struct docg3 *docg3)
 {
 	debugfs_remove_recursive(docg3->debugfs_root);
 }
@@ -2100,7 +2100,7 @@ err_probe:
  *
  * Returns 0
  */
-static int __exit docg3_release(struct platform_device *pdev)
+static int docg3_release(struct platform_device *pdev)
 {
 	struct docg3_cascade *cascade = platform_get_drvdata(pdev);
 	struct docg3 *docg3 = cascade->floors[0]->priv;
@@ -2131,7 +2131,7 @@ static struct platform_driver g3_driver = {
 	},
 	.suspend	= docg3_suspend,
 	.resume		= docg3_resume,
-	.remove		= __exit_p(docg3_release),
+	.remove		= docg3_release,
 };
 
 module_platform_driver_probe(g3_driver, docg3_probe);
