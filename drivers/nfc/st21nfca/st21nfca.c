a8f686ec1041bd704748a2d919cea9652e8e943e
Christophe Ricard <christophe.ricard@gmail.com>
2015-03-31 08:02:17 +0200

    NFC: st21nfca: Change st21nfca_get_iso14443_3_uid parameter name
    st21nfca_get_iso14443_3_uid gate parameter name is incorrect
    and should be uid.
    
    Signed-off-by: Christophe Ricard <christophe-h.ricard@st.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/st21nfca/st21nfca.c
+++ drivers/nfc/st21nfca/st21nfca.c
@@ -572,7 +572,7 @@ exit:
 	return r;
 }
 
-static int st21nfca_get_iso14443_3_uid(struct nfc_hci_dev *hdev, u8 *gate,
+static int st21nfca_get_iso14443_3_uid(struct nfc_hci_dev *hdev, u8 *uid,
 				       int *len)
 {
 	int r;
@@ -588,7 +588,7 @@ static int st21nfca_get_iso14443_3_uid(struct nfc_hci_dev *hdev, u8 *gate,
 		goto exit;
 	}
 
-	memcpy(gate, uid_skb->data, uid_skb->len);
+	memcpy(uid, uid_skb->data, uid_skb->len);
 	*len = uid_skb->len;
 exit:
 	kfree_skb(uid_skb);
