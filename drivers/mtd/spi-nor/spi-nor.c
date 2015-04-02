e20621ab54aa03441916ecbaaa0036a8f123e050
Han Xu <b45815@freescale.com>
2015-04-02 15:01:41 -0500

    MLK-10565-2: mtd: qspi: Support QSPI on i.MX7D SDB
    Add a new entry for MX25L51245G QSPI NOR chip.
    
    Signed-off-by: Han Xu <b45815@freescale.com>
    (cherry picked from commit 9f3f15ad1b0461d6b638c34599dd74d9c43fa01f)

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -587,6 +587,7 @@ static const struct spi_device_id spi_nor_ids[] = {
 	{ "mx25l12855e", INFO(0xc22618, 0, 64 * 1024, 256, 0) },
 	{ "mx25l25635e", INFO(0xc22019, 0, 64 * 1024, 512, 0) },
 	{ "mx25l25655e", INFO(0xc22619, 0, 64 * 1024, 512, 0) },
+	{ "mx25l51245g", INFO(0xc2201a, 0, 64 * 1024, 1024, SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ | SPI_NOR_DDR_QUAD_READ) },
 	{ "mx66l51235l", INFO(0xc2201a, 0, 64 * 1024, 1024, SPI_NOR_QUAD_READ) },
 	{ "mx66l1g55g",  INFO(0xc2261b, 0, 64 * 1024, 2048, SPI_NOR_QUAD_READ) },
 
