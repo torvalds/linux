9fb186cf6907ba4e873d8396d7e5abfa4f22ca4e
Georges-Axel Jaloyan <georges-axel.jaloyan@ens.fr>
2015-04-02 16:10:31 +0200

    Staging: Lustre: rw26.c: include according to checkpatch.pl
    Signed-off-by: Georges-Axel Jaloyan <georges-axel.jaloyan@ens.fr>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/rw26.c
+++ drivers/staging/lustre/lustre/llite/rw26.c
@@ -44,7 +44,7 @@
 #include <linux/stat.h>
 #include <linux/errno.h>
 #include <linux/unistd.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 
 #include <linux/migrate.h>
 #include <linux/fs.h>
