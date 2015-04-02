d06df60b9460838553d37d01049d04c43a36f396
Markus Pargmann <mpa@pengutronix.de>
2015-04-02 10:11:36 +0200

    nbd: Replace kthread_create with kthread_run
    kthread_run includes the wake_up_process() call, so instead of
    kthread_create() followed by wake_up_process() we can use this macro.
    
    Signed-off-by: Markus Pargmann <mpa@pengutronix.de>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- drivers/block/nbd.c
+++ drivers/block/nbd.c
@@ -728,13 +728,13 @@ static int __nbd_ioctl(struct block_device *bdev, struct nbd_device *nbd,
 		else
 			blk_queue_flush(nbd->disk->queue, 0);
 
-		thread = kthread_create(nbd_thread, nbd, "%s",
-					nbd->disk->disk_name);
+		thread = kthread_run(nbd_thread, nbd, "%s",
+				     nbd->disk->disk_name);
 		if (IS_ERR(thread)) {
 			mutex_lock(&nbd->tx_lock);
 			return PTR_ERR(thread);
 		}
-		wake_up_process(thread);
+
 		error = nbd_do_it(nbd);
 		kthread_stop(thread);
 
