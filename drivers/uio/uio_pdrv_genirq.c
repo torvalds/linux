16c9c8e1ae228e89b66cbc03ec6c753ee44d39bc
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2015-04-03 16:04:21 +0200

    Revert "uio: constify of_device_id array"
    This reverts commit 4d8beff2ae07fad85d723b4cdf704b05f0ed4794.
    
    It causes build warnings, and it's incorrect as we do write to this
    structure.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/uio/uio_pdrv_genirq.c
+++ drivers/uio/uio_pdrv_genirq.c
@@ -252,7 +252,7 @@ static const struct dev_pm_ops uio_pdrv_genirq_dev_pm_ops = {
 };
 
 #ifdef CONFIG_OF
-static const struct of_device_id uio_of_genirq_match[] = {
+static struct of_device_id uio_of_genirq_match[] = {
 	{ /* This is filled with module_parm */ },
 	{ /* Sentinel */ },
 };
