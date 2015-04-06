f34c4bc7e599bb895f77381c4d91ccc77635d68f
kbuild test robot <fengguang.wu@intel.com>
2015-04-07 03:06:06 +0800

    ASoC: Intel: read_shim_data() can be static
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/atom/sst/sst_pvt.c
+++ sound/soc/intel/atom/sst/sst_pvt.c
@@ -111,7 +111,7 @@ int sst_wait_interruptible(struct intel_sst_drv *sst_drv_ctx,
 
 }
 
-unsigned long long read_shim_data(struct intel_sst_drv *sst, int addr)
+static unsigned long long read_shim_data(struct intel_sst_drv *sst, int addr)
 {
 	unsigned long long val = 0;
 
@@ -124,7 +124,7 @@ unsigned long long read_shim_data(struct intel_sst_drv *sst, int addr)
 	return val;
 }
 
-void write_shim_data(struct intel_sst_drv *sst, int addr,
+static void write_shim_data(struct intel_sst_drv *sst, int addr,
 				unsigned long long data)
 {
 	switch (sst->dev_id) {
