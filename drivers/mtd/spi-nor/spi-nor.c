b79c332fb283c101abb5d8570dea2d29f3871802
Gabor Juhos <juhosg@openwrt.org>
2015-04-07 19:35:02 +0200

    mtd: spi-nor: add support for the ISSI SI25CD512 SPI flash
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -560,6 +560,9 @@ static const struct spi_device_id spi_nor_ids[] = {
 	{ "320s33b",  INFO(0x898912, 0, 64 * 1024,  64, 0) },
 	{ "640s33b",  INFO(0x898913, 0, 64 * 1024, 128, 0) },
 
+	/* ISSI */
+	{ "is25cd512", INFO(0x7f9d20, 0, 32 * 1024,   2, SECT_4K) },
+
 	/* Macronix */
 	{ "mx25l512e",   INFO(0xc22010, 0, 64 * 1024,   1, SECT_4K) },
 	{ "mx25l2005a",  INFO(0xc22012, 0, 64 * 1024,   4, SECT_4K) },
