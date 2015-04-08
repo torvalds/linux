b211a6388ca279a16a6627da2f3681a2cab18444
Michal Malý <madcatxster@devoid-pointer.net>
2015-04-08 22:56:47 +0200

    HID: hid-lg4ff: Stop the hid device from lg4ff
    Stop the hid device from lg4ff. Originally hid-lg was responsible for
    stopping of the device.
    
    Signed-off-by: Michal Malý <madcatxster@devoid-pointer.net>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-lg.c
+++ drivers/hid/hid-lg.c
@@ -735,8 +735,8 @@ static void lg_remove(struct hid_device *hdev)
 	struct lg_drv_data *drv_data = hid_get_drvdata(hdev);
 	if (drv_data->quirks & LG_FF4)
 		lg4ff_deinit(hdev);
-
-	hid_hw_stop(hdev);
+	else
+		hid_hw_stop(hdev);
 	kfree(drv_data);
 }
 
--- drivers/hid/hid-lg4ff.c
+++ drivers/hid/hid-lg4ff.c
@@ -1213,6 +1213,8 @@ int lg4ff_deinit(struct hid_device *hid)
 		}
 	}
 #endif
+	hid_hw_stop(hid);
+	drv_data->device_props = NULL;
 
 	kfree(entry);
 out:
