09bf4a858d72a632dd12a61bda1d5272d67b2ce9
Mauro Carvalho Chehab <mchehab@osg.samsung.com>
2015-04-08 13:16:57 -0300

    Revert "[media] v4l: vb2-memops: use vma slab when vma allocation"
    Please revert this patch: vm_area_cachep is not exported, so you cannot use
    this in a module.
    
    This reverts commit 5ed1c328ea077d70e1ebcd5188dc77cdc754df3a.
    
    Reported-by: Hans Verkuil <hverkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/v4l2-core/videobuf2-memops.c
+++ drivers/media/v4l2-core/videobuf2-memops.c
@@ -37,7 +37,7 @@ struct vm_area_struct *vb2_get_vma(struct vm_area_struct *vma)
 {
 	struct vm_area_struct *vma_copy;
 
-	vma_copy = kmem_cache_alloc(vm_area_cachep, GFP_KERNEL);
+	vma_copy = kmalloc(sizeof(*vma_copy), GFP_KERNEL);
 	if (vma_copy == NULL)
 		return NULL;
 
@@ -75,7 +75,7 @@ void vb2_put_vma(struct vm_area_struct *vma)
 	if (vma->vm_file)
 		fput(vma->vm_file);
 
-	kmem_cache_free(vm_area_cachep, vma);
+	kfree(vma);
 }
 EXPORT_SYMBOL_GPL(vb2_put_vma);
 
