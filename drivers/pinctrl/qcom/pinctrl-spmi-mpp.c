4e637ac212b63f4b5dd1da626aca34ffcbfd5daa
Ivan T. Ivanov <ivan.ivanov@linaro.org>
2015-04-09 18:18:37 +0300

    pinctrl: qcom-spmi-mpp: Fix input value report
    Fix interpretation of the pmic_mpp_read() return code,
    negative value means an error.
    
    Signed-off-by: Ivan T. Ivanov <ivan.ivanov@linaro.org>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@sonymobile.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/qcom/pinctrl-spmi-mpp.c
+++ drivers/pinctrl/qcom/pinctrl-spmi-mpp.c
@@ -577,10 +577,11 @@ static void pmic_mpp_config_dbg_show(struct pinctrl_dev *pctldev,
 
 		if (pad->input_enabled) {
 			ret = pmic_mpp_read(state, pad, PMIC_MPP_REG_RT_STS);
-			if (!ret) {
-				ret &= PMIC_MPP_REG_RT_STS_VAL_MASK;
-				pad->out_value = ret;
-			}
+			if (ret < 0)
+				return;
+
+			ret &= PMIC_MPP_REG_RT_STS_VAL_MASK;
+			pad->out_value = ret;
 		}
 
 		seq_printf(s, " %-4s", pad->output_enabled ? "out" : "in");
