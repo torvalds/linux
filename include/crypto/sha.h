ca142584bc8ebc8e15b317680771ade58ca96315
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2015-04-09 12:55:38 +0200

    crypto: sha512-generic - move to generic glue implementation
    This updated the generic SHA-512 implementation to use the
    generic shared SHA-512 glue code.
    
    It also implements a .finup hook crypto_sha512_finup() and exports
    it to other modules. The import and export() functions and the
    .statesize member are dropped, since the default implementation
    is perfectly suitable for this module.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/sha.h
+++ include/crypto/sha.h
@@ -98,4 +98,7 @@ extern int crypto_sha256_finup(struct shash_desc *desc, const u8 *data,
 
 extern int crypto_sha512_update(struct shash_desc *desc, const u8 *data,
 			      unsigned int len);
+
+extern int crypto_sha512_finup(struct shash_desc *desc, const u8 *data,
+			       unsigned int len, u8 *hash);
 #endif
