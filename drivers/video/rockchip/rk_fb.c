bd0229c230e7ce936612e36e1cb8915faef90018
hjc <hjc@rock-chips.com>
2015-04-09 17:37:15 +0800

    rk fb: hdmi 24hz frame time need 41ms
    Signed-off-by: hjc <hjc@rock-chips.com>

--- drivers/video/rockchip/rk_fb.c
+++ drivers/video/rockchip/rk_fb.c
@@ -1770,7 +1770,7 @@ static void rk_fb_update_reg(struct rk_lcdc_driver *dev_drv,
 		timestamp = dev_drv->vsync_info.timestamp;
 		timeout = wait_event_interruptible_timeout(dev_drv->vsync_info.wait,
 				ktime_compare(dev_drv->vsync_info.timestamp, timestamp) > 0,
-				msecs_to_jiffies(25));
+				msecs_to_jiffies(50));
 
 		wait_for_vsync = rk_fb_reg_effect(dev_drv, regs, count);
 	} while (wait_for_vsync && count--);
