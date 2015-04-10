5af7a6f3e2d015dcaaeffa48c6d47238415cbe66
Michael Ellerman <mpe@ellerman.id.au>
2015-04-10 11:52:06 +1000

    powerpc/pasemi: Only the build the pasemi MSI code for PASEMI=y
    The pasemi MSI code is currently always built when MPIC=y && PCI_MSI=y.
    It should not have any effect on other platforms, because it immediately
    checks the MPIC's compatible property for "pasemi,pwrficient-openpic".
    
    However it's odd that it's still built even when PASEMI=n. It also
    needn't be in sysdev, as it's only used by pasemi. So move it into
    platforms/pasemi, whereby it will only be built for PASEMI=y.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/platforms/pasemi/msi.c
+++ arch/powerpc/platforms/pasemi/msi.c
@@ -13,8 +13,6 @@
  *
  */
 
-#undef DEBUG
-
 #include <linux/irq.h>
 #include <linux/msi.h>
 #include <asm/mpic.h>
@@ -23,7 +21,7 @@
 #include <asm/ppc-pci.h>
 #include <asm/msi_bitmap.h>
 
-#include "mpic.h"
+#include <sysdev/mpic.h>
 
 /* Allocate 16 interrupts per device, to give an alignment of 16,
  * since that's the size of the grouping w.r.t. affinity. If someone
