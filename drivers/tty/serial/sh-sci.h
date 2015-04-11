bb5f633ab8c6cd14411ce57273533d26c35e8f85
Valentin Rothberg <valentinrothberg@gmail.com>
2015-04-11 12:26:19 +0200

    drivers/tty/serial/sh-sci.h: remove dead reference on ARCH_SH7372
    The Kconfig option ARCH_SH7372 has been removed by commit 59b89af1d555
    ("ARM: shmobile: sh7372: Remove Legacy C SoC code").  This patch removes
    the last reference on this Kconfig option.
    
    Signed-off-by: Valentin Rothberg <valentinrothberg@gmail.com>
    Acked-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/sh-sci.h
+++ drivers/tty/serial/sh-sci.h
@@ -15,7 +15,6 @@
     defined(CONFIG_CPU_SUBTYPE_SH7720) || \
     defined(CONFIG_CPU_SUBTYPE_SH7721) || \
     defined(CONFIG_ARCH_SH73A0) || \
-    defined(CONFIG_ARCH_SH7372) || \
     defined(CONFIG_ARCH_R8A7740)
 
 # define SCxSR_RDxF_CLEAR(port)	 (serial_port_in(port, SCxSR) & 0xfffc)
