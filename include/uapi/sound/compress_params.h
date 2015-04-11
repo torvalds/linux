03333fc86cac02ed1375caa9ac055b96f68d7fb7
Banajit Goswami <bgoswami@codeaurora.org>
2015-04-10 18:04:08 -0700

    ANDROID: GKI: ASoC: msm: qdsp6v2: Add FLAC in compress offload path
    Add FLAC format in compress offload driver, and asm
    module.
    
    Change-Id: I818ace8397e761b1acff7f9b2eab6e0103ed78c8
    Signed-off-by: Apurupa Pattapu <apurupa@codeaurora.org>
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>
    Signed-off-by: Meng Wang <mwang@codeaurora.org>
    Bug: 151372815
    (cherry picked from commit 02c08155fd0fd57e94b14fc49979d21d4361a067)
    Signed-off-by: Saravana Kannan <saravanak@google.com>

--- include/uapi/sound/compress_params.h
+++ include/uapi/sound/compress_params.h
@@ -335,12 +335,23 @@ struct snd_enc_generic {
 	__s32 reserved[15];	/* Can be used for SND_AUDIOCODEC_BESPOKE */
 } __attribute__((packed, aligned(4)));
 
+struct snd_dec_flac {
+	__u16 sample_size;
+	__u16 min_blk_size;
+	__u16 max_blk_size;
+	__u16 min_frame_size;
+	__u16 max_frame_size;
+} __attribute__((packed, aligned(4)));
+
+#define SND_DEC_FLAC_SUPPORTED
+
 union snd_codec_options {
 	struct snd_enc_wma wma;
 	struct snd_enc_vorbis vorbis;
 	struct snd_enc_real real;
 	struct snd_enc_flac flac;
 	struct snd_enc_generic generic;
+	struct snd_dec_flac flac_dec;
 } __attribute__((packed, aligned(4)));
 
 /** struct snd_codec_desc - description of codec capabilities
