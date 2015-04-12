6b46f7b7e97e58a23fd96c3bfc1efee9da3d1a3d
Richard Weinberger <richard@nod.at>
2015-04-13 00:52:38 +0200

    netfilter: Fix format string of nfnetlink_queue proc file
    The printed values are all of type unsigned integer, therefore use
    %u instead of %d. Otherwise an user can face negative values.
    
    Fixes:
    $ cat /proc/net/netfilter/nfnetlink_queue
        0  29508   278 2 65531     0 2004213241 -2129885586  1
        1 -27747     0 2 65531     0     0        0  1
        2 -27748     0 2 65531     0     0        0  1
    
    Signed-off-by: Richard Weinberger <richard@nod.at>
    Acked-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netfilter/nfnetlink_queue_core.c
+++ net/netfilter/nfnetlink_queue_core.c
@@ -1251,7 +1251,7 @@ static int seq_show(struct seq_file *s, void *v)
 {
 	const struct nfqnl_instance *inst = v;
 
-	seq_printf(s, "%5d %6d %5d %1d %5d %5d %5d %8d %2d\n",
+	seq_printf(s, "%5u %6u %5u %1u %5u %5u %5u %8u %2d\n",
 		   inst->queue_num,
 		   inst->peer_portid, inst->queue_total,
 		   inst->copy_mode, inst->copy_range,
