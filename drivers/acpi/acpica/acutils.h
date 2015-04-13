9bd4ce36dcbc8e3b2218e84cdfd1dff6d5671f3c
Rickard Strandqvist <rickard_strandqvist@spectrumdigital.se>
2015-04-13 11:49:24 +0800

    ACPICA: Utilities: Remove unused acpi_ut_create_pkg_state_and_push().
    ACPICA commit 2a9ebd974aee41391f4b0edcd4f0cc5ee23ec2f8
    
    Remove the function acpi_ut_create_pkg_state_and_push() that is not used anywhere.
    This was partially found by using a static code analysis program called cppcheck.
    
    Link: https://github.com/acpica/acpica/commit/2a9ebd97
    Signed-off-by: Rickard Strandqvist <rickard_strandqvist@spectrumdigital.se>
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpica/acutils.h
+++ drivers/acpi/acpica/acutils.h
@@ -539,12 +539,6 @@ acpi_ut_create_update_state_and_push(union acpi_operand_object *object,
 				     u16 action,
 				     union acpi_generic_state **state_list);
 
-acpi_status
-acpi_ut_create_pkg_state_and_push(void *internal_object,
-				  void *external_object,
-				  u16 index,
-				  union acpi_generic_state **state_list);
-
 union acpi_generic_state *acpi_ut_create_control_state(void);
 
 void acpi_ut_delete_generic_state(union acpi_generic_state *state);
