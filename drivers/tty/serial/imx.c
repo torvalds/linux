53794183225f25830ab40e1c83ae885f11501784
Jiada Wang <jiada_wang@mentor.com>
2015-04-13 18:31:43 +0900

    serial: imx: protect Soft Reset of port with lock
    Previously Soft Reset (clear of SRST bit in UCR2 register)
    of UART in startup is not protected by lock, which may have race
    with console_write, as console_write may occur at anytime even
    when UART port is shutdown.
    
    To avoid this race, protect Soft reset of UART port with spin_lock.
    
    Signed-off-by: Jiada Wang <jiada_wang@mentor.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -1114,6 +1114,12 @@ static int imx_startup(struct uart_port *port)
 
 	writel(temp & ~UCR4_DREN, sport->port.membase + UCR4);
 
+	/* Can we enable the DMA support? */
+	if (is_imx6q_uart(sport) && !uart_console(port) &&
+	    !sport->dma_is_inited)
+		imx_uart_dma_init(sport);
+
+	spin_lock_irqsave(&sport->port.lock, flags);
 	/* Reset fifo's and state machines */
 	i = 100;
 
@@ -1123,13 +1129,6 @@ static int imx_startup(struct uart_port *port)
 
 	while (!(readl(sport->port.membase + UCR2) & UCR2_SRST) && (--i > 0))
 		udelay(1);
-
-	/* Can we enable the DMA support? */
-	if (is_imx6q_uart(sport) && !uart_console(port) &&
-	    !sport->dma_is_inited)
-		imx_uart_dma_init(sport);
-
-	spin_lock_irqsave(&sport->port.lock, flags);
 
 	/*
 	 * Finally, clear and enable interrupts
