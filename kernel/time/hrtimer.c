19d9f4225dd6a47fca430f15eeae345ceb95c301
Thomas Gleixner <tglx@linutronix.de>
2015-04-14 21:09:25 +0000

    hrtimer: Avoid locking in hrtimer_cancel() if timer not active
    We can do a lockless check for hrtimer_active before actually taking
    the lock in hrtimer[_try_to]_cancel. This is useful for hotpath users
    like nanosleep as they avoid the lock dance when the timer has
    expired.
    
    This is safe because active is true when the timer is enqueued or the
    callback is running. Taking the hrtimer base lock does not protect
    against concurrent hrtimer_start calls, the callsite has to do the
    proper serialization itself.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Cc: Preeti U Murthy <preeti@linux.vnet.ibm.com>
    Cc: Viresh Kumar <viresh.kumar@linaro.org>
    Cc: Marcelo Tosatti <mtosatti@redhat.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Link: http://lkml.kernel.org/r/20150414203503.580273114@linutronix.de
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -991,6 +991,15 @@ int hrtimer_try_to_cancel(struct hrtimer *timer)
 	unsigned long flags;
 	int ret = -1;
 
+	/*
+	 * Check lockless first. If the timer is not active (neither
+	 * enqueued nor running the callback, nothing to do here.  The
+	 * base lock does not serialize against a concurrent enqueue,
+	 * so we can avoid taking it.
+	 */
+	if (!hrtimer_active(timer))
+		return 0;
+
 	base = lock_hrtimer_base(timer, &flags);
 
 	if (!hrtimer_callback_running(timer))
