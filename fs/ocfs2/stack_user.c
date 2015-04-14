43ee9cad8a81954eea893b52e08d0c00ca9baccc
Markus Elfring <elfring@users.sourceforge.net>
2015-04-14 15:42:59 -0700

    ocfs2: one function call less in user_cluster_connect() after error detection
    kfree() was called by user_cluster_connect() even if a previous call of
    the kzalloc() function failed.
    
    Return from this implementation directly after failure detection.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Cc: Mark Fasheh <mfasheh@suse.com>
    Cc: Joel Becker <jlbec@evilplan.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/ocfs2/stack_user.c
+++ fs/ocfs2/stack_user.c
@@ -1004,10 +1004,8 @@ static int user_cluster_connect(struct ocfs2_cluster_connection *conn)
 	BUG_ON(conn == NULL);
 
 	lc = kzalloc(sizeof(struct ocfs2_live_connection), GFP_KERNEL);
-	if (!lc) {
-		rc = -ENOMEM;
-		goto out;
-	}
+	if (!lc)
+		return -ENOMEM;
 
 	init_waitqueue_head(&lc->oc_wait);
 	init_completion(&lc->oc_sync_wait);
