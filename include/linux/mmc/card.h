96541bac0b4e62efa42e7900d9b32e6baa9a214c
Ulf Hansson <ulf.hansson@linaro.org>
2015-04-14 13:06:12 +0200

    Revert "mmc: core: Convert mmc_driver to device_driver"
    This reverts commit 6685ac62b2f0 ("mmc: core: Convert mmc_driver to
    device_driver")
    
    The reverted commit went too far in simplifing the device driver parts
    for mmc.
    
    Let's restore the old mmc_driver to enable driver core to sooner
    or later to remove the ->probe(), ->remove() and ->shutdown() callbacks
    from the struct device_driver.
    
    Note that, the old ->suspend|resume() callbacks in the struct
    mmc_driver don't need to be restored, since the mmc block layer has
    converted to the modern system PM ops.
    
    Fixes: 6685ac62b2f0 ("mmc: core: Convert mmc_driver to device_driver")
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Acked-by: Jaehoon Chung <jh80.chung@samsung.com>

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -512,8 +512,18 @@ static inline int mmc_card_broken_irq_polling(const struct mmc_card *c)
 
 #define mmc_dev_to_card(d)	container_of(d, struct mmc_card, dev)
 
-extern int mmc_register_driver(struct device_driver *);
-extern void mmc_unregister_driver(struct device_driver *);
+/*
+ * MMC device driver (e.g., Flash card, I/O card...)
+ */
+struct mmc_driver {
+	struct device_driver drv;
+	int (*probe)(struct mmc_card *);
+	void (*remove)(struct mmc_card *);
+	void (*shutdown)(struct mmc_card *);
+};
+
+extern int mmc_register_driver(struct mmc_driver *);
+extern void mmc_unregister_driver(struct mmc_driver *);
 
 extern void mmc_fixup_device(struct mmc_card *card,
 			     const struct mmc_fixup *table);
