b0c24c3bdf237182b2f043bfee68bf886b0c9ad3
Al Viro <viro@zeniv.linux.org.uk>
2015-04-18 19:10:36 -0400

    link_path_walk: handle get_link() returning ERR_PTR() immediately
    If we get ERR_PTR() from get_link(), we are guaranteed to get err != 0
    when we break out of do-while, so we are going to hit if (err) return err;
    shortly after it.  Pull that into the if (IS_ERR(s)) body.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/namei.c
+++ fs/namei.c
@@ -1792,7 +1792,9 @@ static int link_path_walk(const char *name, struct nameidata *nd)
 
 				if (unlikely(IS_ERR(s))) {
 					err = PTR_ERR(s);
-					break;
+					current->link_count--;
+					nd->depth--;
+					return err;
 				}
 				err = 0;
 				if (likely(s)) {
