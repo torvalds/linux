e4e8ccccf314eacc935e699073575de7816338e4
Fabio Estevam <fabio.estevam@freescale.com>
2015-04-20 16:19:01 -0300

    regulator: max77686: Fix typo in email address
    Fix typo in the samsung email address.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/max77686.c
+++ drivers/regulator/max77686.c
@@ -2,7 +2,7 @@
  * max77686.c - Regulator driver for the Maxim 77686
  *
  * Copyright (C) 2012 Samsung Electronics
- * Chiwoong Byun <woong.byun@smasung.com>
+ * Chiwoong Byun <woong.byun@samsung.com>
  * Jonghwa Lee <jonghwa3.lee@samsung.com>
  *
  * This program is free software; you can redistribute it and/or modify
