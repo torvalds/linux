3e19acdc5bdd9709bfd89cc14cbcd1cb90b44965
Alban Bedel <albeu@free.fr>
2015-04-24 16:19:23 +0200

    spi: spi-ath79: Use clk_prepare_enable and clk_disable_unprepare
    Clocks should be prepared and unprepared, fix this by using
    clk_prepare_enable() and clk_disable_unprepare() instead of
    clk_enable() and clk_disable().
    
    Signed-off-by: Alban Bedel <albeu@free.fr>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-ath79.c
+++ drivers/spi/spi-ath79.c
@@ -249,7 +249,7 @@ static int ath79_spi_probe(struct platform_device *pdev)
 		goto err_put_master;
 	}
 
-	ret = clk_enable(sp->clk);
+	ret = clk_prepare_enable(sp->clk);
 	if (ret)
 		goto err_put_master;
 
@@ -273,7 +273,7 @@ static int ath79_spi_probe(struct platform_device *pdev)
 err_disable:
 	ath79_spi_disable(sp);
 err_clk_disable:
-	clk_disable(sp->clk);
+	clk_disable_unprepare(sp->clk);
 err_put_master:
 	spi_master_put(sp->bitbang.master);
 
@@ -286,7 +286,7 @@ static int ath79_spi_remove(struct platform_device *pdev)
 
 	spi_bitbang_stop(&sp->bitbang);
 	ath79_spi_disable(sp);
-	clk_disable(sp->clk);
+	clk_disable_unprepare(sp->clk);
 	spi_master_put(sp->bitbang.master);
 
 	return 0;
