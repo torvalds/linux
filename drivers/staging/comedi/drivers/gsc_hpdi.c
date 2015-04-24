cf376ec0ee5c2078d99a60aadd742232f725b451
Gujulan Elango, Hari Prasath (H.) <hgujulan@visteon.com>
2015-04-24 06:58:11 +0000

    staging: gsc_hdpi: Remove dead code
    This patch removes commented code from this driver.
    
    Signed-off-by: Hari Prasath Gujulan Elango <hgujulan@visteon.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/gsc_hpdi.c
+++ drivers/staging/comedi/drivers/gsc_hpdi.c
@@ -135,13 +135,6 @@ static const struct hpdi_board hpdi_boards[] = {
 		.device_id	= PCI_DEVICE_ID_PLX_9080,
 		.subdevice_id	= 0x2400,
 	 },
-#if 0
-	{
-		.name		= "pxi-hpdi32",
-		.device_id	= 0x9656,
-		.subdevice_id	= 0x2705,
-	 },
-#endif
 };
 
 struct hpdi_private {
