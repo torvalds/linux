38d23a6cc16c02f7b0c920266053f340b5601735
Andreas Sandberg <andreas.sandberg@arm.com>
2015-04-24 13:06:05 +0000

    tick: hrtimer-broadcast: Prevent endless restarting when broadcast device is unused
    The hrtimer callback in the hrtimer's tick broadcast code sometimes
    incorrectly ends up scheduling events at the current tick causing the
    kernel to hang servicing the same hrtimer forever. This typically
    happens when a device is swapped out by
    tick_install_broadcast_device(), which replaces the event handler with
    clock_events_handle_noop() and sets the device mode to
    CLOCK_EVT_MODE_UNUSED. If the timer is scheduled when this happens,
    the next_event field will not be updated and the hrtimer ends up being
    restarted at the current tick. To prevent this from happening, only
    try to restart the hrtimer if the broadcast clock event device is in
    one of the active modes and try to cancel the timer when entering the
    CLOCK_EVT_MODE_UNUSED mode.
    
    Signed-off-by: Andreas Sandberg <andreas.sandberg@arm.com>
    Tested-by: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Reviewed-by: Preeti U Murthy <preeti@linux.vnet.ibm.com>
    Link: http://lkml.kernel.org/r/1429880765-5558-1-git-send-email-andreas.sandberg@arm.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/time/tick-broadcast-hrtimer.c
+++ kernel/time/tick-broadcast-hrtimer.c
@@ -22,6 +22,7 @@ static void bc_set_mode(enum clock_event_mode mode,
 			struct clock_event_device *bc)
 {
 	switch (mode) {
+	case CLOCK_EVT_MODE_UNUSED:
 	case CLOCK_EVT_MODE_SHUTDOWN:
 		/*
 		 * Note, we cannot cancel the timer here as we might
@@ -101,10 +102,13 @@ static enum hrtimer_restart bc_handler(struct hrtimer *t)
 {
 	ce_broadcast_hrtimer.event_handler(&ce_broadcast_hrtimer);
 
-	if (ce_broadcast_hrtimer.next_event.tv64 == KTIME_MAX)
+	switch (ce_broadcast_hrtimer.mode) {
+	case CLOCK_EVT_MODE_ONESHOT:
+		if (ce_broadcast_hrtimer.next_event.tv64 != KTIME_MAX)
+			return HRTIMER_RESTART;
+	default:
 		return HRTIMER_NORESTART;
-
-	return HRTIMER_RESTART;
+	}
 }
 
 void tick_setup_hrtimer_broadcast(void)
