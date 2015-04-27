4545c89880138b30a868159bc1b209867b8a5f32
Jiri Kosina <jkosina@suse.cz>
2015-04-27 13:17:19 +0200

    x86: introduce kaslr_offset()
    Offset that has been chosen for kaslr during kernel decompression can be
    easily computed as a difference between _text and __START_KERNEL. We are
    already making use of this in dump_kernel_offset() notifier and in
    arch_crash_save_vmcoreinfo().
    
    Introduce kaslr_offset() that makes this computation instead of hard-coding
    it, so that other kernel code (such as live patching) can make use of it.
    Also convert existing users to make use of it.
    
    This patch is equivalent transofrmation without any effects on the resulting
    code:
    
    	$ diff -u vmlinux.old.asm vmlinux.new.asm
    	--- vmlinux.old.asm     2015-04-28 17:55:19.520983368 +0200
    	+++ vmlinux.new.asm     2015-04-28 17:55:24.141206072 +0200
    	@@ -1,5 +1,5 @@
    
    	-vmlinux.old:     file format elf64-x86-64
    	+vmlinux.new:     file format elf64-x86-64
    
    	Disassembly of section .text:
    	$
    
    Acked-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- arch/x86/include/asm/setup.h
+++ arch/x86/include/asm/setup.h
@@ -65,10 +65,16 @@ static inline void x86_ce4100_early_setup(void) { }
  * This is set up by the setup-routine at boot-time
  */
 extern struct boot_params boot_params;
+extern char _text[];
 
 static inline bool kaslr_enabled(void)
 {
 	return !!(boot_params.hdr.loadflags & KASLR_FLAG);
+}
+
+static inline unsigned long kaslr_offset(void)
+{
+	return (unsigned long)&_text - __START_KERNEL;
 }
 
 /*
