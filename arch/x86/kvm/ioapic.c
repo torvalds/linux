d90e3a35e90af7f8beceebefc90e62eae237c5ed
Julia Lawall <Julia.Lawall@lip6.fr>
2015-04-27 22:35:34 +0200

    KVM: x86: drop unneeded null test
    If the null test is needed, the call to cancel_delayed_work_sync would have
    already crashed.  Normally, the destroy function should only be called
    if the init function has succeeded, in which case ioapic is not null.
    
    Problem found using Coccinelle.
    
    Suggested-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Julia Lawall <Julia.Lawall@lip6.fr>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/ioapic.c
+++ arch/x86/kvm/ioapic.c
@@ -638,11 +638,9 @@ void kvm_ioapic_destroy(struct kvm *kvm)
 	struct kvm_ioapic *ioapic = kvm->arch.vioapic;
 
 	cancel_delayed_work_sync(&ioapic->eoi_inject);
-	if (ioapic) {
-		kvm_io_bus_unregister_dev(kvm, KVM_MMIO_BUS, &ioapic->dev);
-		kvm->arch.vioapic = NULL;
-		kfree(ioapic);
-	}
+	kvm_io_bus_unregister_dev(kvm, KVM_MMIO_BUS, &ioapic->dev);
+	kvm->arch.vioapic = NULL;
+	kfree(ioapic);
 }
 
 int kvm_get_ioapic(struct kvm *kvm, struct kvm_ioapic_state *state)
