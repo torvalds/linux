dcd93e8303cbd0ef3dba165daa0633ebd05b3e17
Allan, Bruce W <bruce.w.allan@intel.com>
2015-04-27 13:58:27 -0700

    crypto: qat - do not duplicate string containing firmware name
    Use ADF_DH895XCC_FW instead of duplicating the string "qat_895xcc.bin"
    when referring to the DH895xCC firmware.
    
    Signed-off-by: Bruce Allan <bruce.w.allan@intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/qat/qat_dh895xcc/adf_drv.c
+++ drivers/crypto/qat/qat_dh895xcc/adf_drv.c
@@ -417,5 +417,5 @@ module_exit(adfdrv_release);
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Intel");
-MODULE_FIRMWARE("qat_895xcc.bin");
+MODULE_FIRMWARE(ADF_DH895XCC_FW);
 MODULE_DESCRIPTION("Intel(R) QuickAssist Technology");
