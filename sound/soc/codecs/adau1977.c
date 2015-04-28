fb75ee66c2707f56397eb29c01decf36254e3d46
kbuild test robot <fengguang.wu@intel.com>
2015-04-28 12:20:32 +0800

    ASoC: adau1977: fix simple_return.cocci warnings
    sound/soc/codecs/adau1977.c:496:5-8: WARNING: end returns can be simpified
    
     Simplify a trivial if-return sequence.  Possibly combine with a
     preceding function call.
    Generated by: scripts/coccinelle/misc/simple_return.cocci
    
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/adau1977.c
+++ sound/soc/codecs/adau1977.c
@@ -493,10 +493,7 @@ static int adau1977_set_bias_level(struct snd_soc_codec *codec,
 		break;
 	}
 
-	if (ret)
-		return ret;
-
-	return 0;
+	return ret;
 }
 
 static int adau1977_set_tdm_slot(struct snd_soc_dai *dai, unsigned int tx_mask,
