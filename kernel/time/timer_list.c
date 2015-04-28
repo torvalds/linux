1ef09cd713c90781b683a0b4e0a874803c172b1d
Preeti U Murthy <preeti@linux.vnet.ibm.com>
2015-04-28 14:15:20 +0530

    tick-broadcast: Fix the printing of broadcast masks
    Today the number of bits of the broadcast masks that is output into
    /proc/timer_list is sizeof(unsigned long). This means that on machines
    with a larger number of CPUs, the bitmasks of CPUs beyond this range do
    not appear.
    
    Fix this by using bitmap printing through "%*pb" instead, so as to
    output the broadcast masks for the range of nr_cpu_ids into
    /proc/timer_list.
    
    Signed-off-by: Preeti U Murthy <preeti@linux.vnet.ibm.com>
    Cc: peterz@infradead.org
    Cc: linuxppc-dev@ozlabs.org
    Cc: john.stultz@linaro.org
    Link: http://lkml.kernel.org/r/20150428084520.3314.62668.stgit@preeti.in.ibm.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/time/timer_list.c
+++ kernel/time/timer_list.c
@@ -276,11 +276,11 @@ static void timer_list_show_tickdevices_header(struct seq_file *m)
 {
 #ifdef CONFIG_GENERIC_CLOCKEVENTS_BROADCAST
 	print_tickdevice(m, tick_get_broadcast_device(), -1);
-	SEQ_printf(m, "tick_broadcast_mask: %08lx\n",
-		   cpumask_bits(tick_get_broadcast_mask())[0]);
+	SEQ_printf(m, "tick_broadcast_mask: %*pb\n",
+		   cpumask_pr_args(tick_get_broadcast_mask()));
 #ifdef CONFIG_TICK_ONESHOT
-	SEQ_printf(m, "tick_broadcast_oneshot_mask: %08lx\n",
-		   cpumask_bits(tick_get_broadcast_oneshot_mask())[0]);
+	SEQ_printf(m, "tick_broadcast_oneshot_mask: %*pb\n",
+		   cpumask_pr_args(tick_get_broadcast_oneshot_mask()));
 #endif
 	SEQ_printf(m, "\n");
 #endif
