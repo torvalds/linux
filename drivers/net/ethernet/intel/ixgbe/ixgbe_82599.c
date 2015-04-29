3b7884f75f3d3f0a0191c00a083535a1dd01a5a4
Fan Du <fan.du@intel.com>
2015-04-29 10:57:41 +0800

    ixgbe: Don't report flow director filter's status
    For two reasons I want to disable this:
    1. Not any part actually check the report status(Alexander Duyck)
    2. To report hash value of a packet to stack,
       RSS -> 32bits hash value
       Perfect match fdir filter -> 13bits hash value
       Hashed-based fdir filter -> 31bits hash value
    
       fdir filter might hash on masked tuples for IP address,
       so it's still not desirable for usage.
    
    So for now, just stick to RSS 32bits hash value.
    
    Signed-off-by: Fan Du <fan.du@intel.com>
    Suggested-by: Alexander Duyck <alexander.h.duyck@redhat.com>
    Tested-by: Phil Schmitt <phillip.j.schmitt@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_82599.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_82599.c
@@ -1394,14 +1394,12 @@ s32 ixgbe_init_fdir_perfect_82599(struct ixgbe_hw *hw, u32 fdirctrl)
 	/*
 	 * Continue setup of fdirctrl register bits:
 	 *  Turn perfect match filtering on
-	 *  Report hash in RSS field of Rx wb descriptor
 	 *  Initialize the drop queue
 	 *  Move the flexible bytes to use the ethertype - shift 6 words
 	 *  Set the maximum length per hash bucket to 0xA filters
 	 *  Send interrupt when 64 (0x4 * 16) filters are left
 	 */
 	fdirctrl |= IXGBE_FDIRCTRL_PERFECT_MATCH |
-		    IXGBE_FDIRCTRL_REPORT_STATUS |
 		    (IXGBE_FDIR_DROP_QUEUE << IXGBE_FDIRCTRL_DROP_Q_SHIFT) |
 		    (0x6 << IXGBE_FDIRCTRL_FLEX_SHIFT) |
 		    (0xA << IXGBE_FDIRCTRL_MAX_LENGTH_SHIFT) |
