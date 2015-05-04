b6bbf59899f91fe38d36c20defcee84dd5c19195
Eyal Reizer <eyalreizer@gmail.com>
2015-05-04 15:24:24 +0300

    ARM: dts: am437x-gp-evm: add wilink8 support
    enable mmc3 used for wlan and uart3 used for bluetooth
    
    configure the gpios used for wlan and bluetooth controls
    
    add fixed voltage regulator used for wlan power control
    
    Signed-off-by: Eyal Reizer <eyalr@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- include/dt-bindings/pinctrl/am43xx.h
+++ include/dt-bindings/pinctrl/am43xx.h
@@ -21,6 +21,7 @@
 #define SLEWCTRL_SLOW		(1 << 19)
 #define SLEWCTRL_FAST		0
 #define DS0_PULL_UP_DOWN_EN	(1 << 27)
+#define WAKEUP_ENABLE		(1 << 29)
 
 #define PIN_OUTPUT		(PULL_DISABLE)
 #define PIN_OUTPUT_PULLUP	(PULL_UP)
