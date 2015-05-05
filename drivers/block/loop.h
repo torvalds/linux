7c9da37f27a40fabe91dae40efb6117e97469326
Ming Lei <ming.lei@canonical.com>
2015-05-05 19:49:54 +0800

    block: loop: convert to per-device workqueue
    commit f4aa4c7bbac6c4afdd4adccf90898c1a3685396d upstream.
    
    Documentation/workqueue.txt:
    	If there is dependency among multiple work items used
    	during memory reclaim, they should be queued to separate
    	wq each with WQ_MEM_RECLAIM.
    
    Loop devices can be stacked, so we have to convert to per-device
    workqueue. One example is Fedora live CD.
    
    Fixes: b5dd2f6047ca108001328aac0e8588edd15f1778
    Cc: Justin M. Forbes <jforbes@fedoraproject.org>
    Signed-off-by: Ming Lei <ming.lei@canonical.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/block/loop.h
+++ drivers/block/loop.h
@@ -54,6 +54,7 @@ struct loop_device {
 	gfp_t		old_gfp_mask;
 
 	spinlock_t		lo_lock;
+	struct workqueue_struct *wq;
 	struct list_head	write_cmd_head;
 	struct work_struct	write_work;
 	bool			write_started;
