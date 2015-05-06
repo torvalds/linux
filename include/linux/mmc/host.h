9f6e0bff2afb52a4c29f5ca8a4db01810357974e
Lars-Peter Clausen <lars@metafoo.de>
2015-05-06 20:31:19 +0200

    mmc: Add support for disabling write-protect detection
    It is not uncommon to see systems where there is no physical write-protect
    signal (e.g. when using eMMC or microSD card slots). For some controllers,
    which have a dedicated write-protection detection logic (like SDHCI
    controllers), the get_ro() callback can return bogus data in such a case.
    
    Instead of handling this on a per controller basis this patch adds a new
    capability flag to the MMC core that can be set to specify that the result
    of get_ro() is invalid. When the flag is set the core will not call
    get_ro() and assume that the card is always read-write.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -286,6 +286,7 @@ struct mmc_host {
 				 MMC_CAP2_HS400_1_2V)
 #define MMC_CAP2_HSX00_1_2V	(MMC_CAP2_HS200_1_2V_SDR | MMC_CAP2_HS400_1_2V)
 #define MMC_CAP2_SDIO_IRQ_NOTHREAD (1 << 17)
+#define MMC_CAP2_NO_WRITE_PROTECT (1 << 18)	/* No physical write protect pin, assume that card is always read-write */
 
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
 
