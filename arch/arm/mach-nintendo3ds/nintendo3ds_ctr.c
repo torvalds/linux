a53589e72cde275e404688d9a1eea0a9640986d9
Sergi Granell <xerpi.g.12@gmail.com>
2015-05-07 17:10:46 +0200

    Updated stuff (printks)

--- arch/arm/mach-nintendo3ds/nintendo3ds_ctr.c
+++ arch/arm/mach-nintendo3ds/nintendo3ds_ctr.c
@@ -19,12 +19,6 @@
 #include <mach/hardware.h>
 #include <mach/platform.h>
 
-extern void shared_3ds_printf(const char *str, ...);
-
-/*****************************************************************************
- * I/O Address Mapping
- ****************************************************************************/
-
 static struct map_desc nintendo3ds_ctr_io_desc[] __initdata = {
 	{
 		.virtual	= IO_ADDRESS(NINTENDO3DS_CTR_PRIV_MEM_BASE),
@@ -35,37 +29,12 @@ static struct map_desc nintendo3ds_ctr_io_desc[] __initdata = {
 };
 
 
-#ifdef CONFIG_HAVE_ARM_TWD
-static DEFINE_TWD_LOCAL_TIMER(twd_local_timer,
-			      NINTENDO3DS_CTR_CPU_TIMER_WATCHDOG_BASE,
-			      NINTENDO3DS_CTR_GIC_TIMER_ID);
-
-static void __init nintendo3ds_ctr_twd_init(void)
-{
-	int err = twd_local_timer_register(&twd_local_timer);
-	if (err)
-		pr_err("twd_local_timer_register failed %d\n", err);
-}
-#else
-#define nintendo3ds_ctr_twd_init()	do {} while(0)
-#endif
-
-static void __init nintendo3ds_ctr_map_io(void)
-{
-	shared_3ds_printf("nintendo3ds_ctr_map_io\n");
-	//iotable_init(nintendo3ds_ctr_io_desc, ARRAY_SIZE(nintendo3ds_ctr_io_desc));
-}
-
-static void __init nintendo3ds_ctr_restart(enum reboot_mode mode, const char *cmd)
-{
-	shared_3ds_printf("nintendo3ds_ctr_restart\n");
-}
-
+/*
 static void __init nintendo3ds_ctr_init_irq(void)
 {
 	int i;
 
-	shared_3ds_printf("nintendo3ds_ctr_init_irq\n");
+	printk("nintendo3ds_ctr_init_irq\n");
 
 	// Enable clear all the GID interrupts
 	// Pending clear all the GID interrupts
@@ -77,36 +46,37 @@ static void __init nintendo3ds_ctr_init_irq(void)
 	gic_init(0, 29, __io_address(NINTENDO3DS_CTR_GIC_BASE),
 		__io_address(NINTENDO3DS_CTR_CPU_INT_INTERFACE_BASE));
 
-	//shared_3ds_printf("nintendo3ds_ctr_init_irq DONE\n");
-	
+	//printk("nintendo3ds_ctr_init_irq DONE\n");
+
 	//gic_init(unsigned int nr, int start, void __iomem *dist , void __iomem *cpu)
-	
+
 	// Enable CPU Interrupt Interface
 	//writel(1, __io_address(NINTENDO3DS_CTR_CPU_INT_INTERFACE_CONTROL));
 
 	// Enable Global Interrupt Distributor
 	//writel(1, __io_address(NINTENDO3DS_CTR_GID_IDCR));
-}
+}*/
 
-static void __init nintendo3ds_ctr_handle_irq(struct pt_regs *regs)
+static void __init nintendo3ds_ctr_map_io(void)
 {
-
+	printk("nintendo3ds_ctr_map_io\n");
+	iotable_init(nintendo3ds_ctr_io_desc, ARRAY_SIZE(nintendo3ds_ctr_io_desc));
 }
 
-static void __init nintendo3ds_ctr_dt_init_time(void)
+static void __init nintendo3ds_ctr_init_early(void)
 {
-	shared_3ds_printf("nintendo3ds_ctr_dt_init_time\n");
-	nintendo3ds_ctr_twd_init();
 }
-
-
 
 static void __init nintendo3ds_ctr_dt_init_machine(void)
 {
-	shared_3ds_printf("nintendo3ds_ctr_dt_init_machine\n");
-	of_platform_populate(NULL, NULL, NULL, NULL);
+	printk("nintendo3ds_ctr_dt_init_machine\n");
+	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
 }
 
+static void __init nintendo3ds_ctr_restart(enum reboot_mode mode, const char *cmd)
+{
+	printk("nintendo3ds_ctr_restart\n");
+}
 
 
 static const char *nintendo3ds_ctr_dt_platform_compat[] __initconst = {
@@ -115,11 +85,9 @@ static const char *nintendo3ds_ctr_dt_platform_compat[] __initconst = {
 };
 
 DT_MACHINE_START(NINTENDO3DS_DT, "Nintendo 3DS (CTR) (Device Tree)")
-	.init_irq     = nintendo3ds_ctr_init_irq,
-	.map_io		  = nintendo3ds_ctr_map_io,
-	.handle_irq   = nintendo3ds_ctr_handle_irq,
-	.init_time    = nintendo3ds_ctr_dt_init_time,
-	.init_machine = nintendo3ds_ctr_dt_init_machine,
-	.dt_compat	  = nintendo3ds_ctr_dt_platform_compat,
-	.restart      = nintendo3ds_ctr_restart,
+	.map_io		= nintendo3ds_ctr_map_io,
+	.init_early	= nintendo3ds_ctr_init_early,
+	.init_machine	= nintendo3ds_ctr_dt_init_machine,
+	.dt_compat	= nintendo3ds_ctr_dt_platform_compat,
+	.restart	= nintendo3ds_ctr_restart,
 MACHINE_END
--- copy_3ds.sh
+++ copy_3ds.sh
@@ -2,7 +2,7 @@
 
 make ARCH=arm -j3
 make ARCH=arm nintendo3ds_ctr.dtb
-cp ./arch/arm/boot/zImage "/run/media/$USER/GATEWAYNAND/"
-cp ./arch/arm/boot/dts/nintendo3ds_ctr.dtb "/run/media/$USER/GATEWAYNAND/"
+cp ./arch/arm/boot/zImage "/mnt/GATEWAYNAND/"
+cp ./arch/arm/boot/dts/nintendo3ds_ctr.dtb "/mnt/GATEWAYNAND/"
 sync
 echo "Copied!"
