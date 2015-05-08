6656bde5ec868d89cc803539f9edf85a89497b6a
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2015-05-08 22:41:05 +0200

    ACPI / PM: Drop stale comment from acpi_power_transition()
    An old comment in acpi_power_transition() indicates that support
    for ordering power resources needs to be added, but the current
    code handles that already.
    
    Drop the comment to avoid confusion.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/power.c
+++ drivers/acpi/power.c
@@ -710,8 +710,6 @@ int acpi_power_transition(struct acpi_device *device, int state)
 	    || (device->power.state > ACPI_STATE_D3_COLD))
 		return -ENODEV;
 
-	/* TBD: Resources must be ordered. */
-
 	/*
 	 * First we reference all power resources required in the target list
 	 * (e.g. so the device doesn't lose power while transitioning).  Then,
