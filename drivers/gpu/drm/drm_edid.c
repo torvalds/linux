4d53dc0c02c1ad74dbeda89a9245f7fe15d45d02
Ville Syrjälä <ville.syrjala@linux.intel.com>
2015-05-08 17:45:07 +0300

    drm/edid: Add CEA modes before inferred modes
    Currently we're adding CEA modes after the inferred modes, which means
    we might get multiple modes that are very close to each other, but
    slightly different, which seems a bit silly. That's because duplicate
    mode check that occurs when adding inferred modes would not consider
    CEA modes as potential duplicates. Reverse the order so that CEA
    modes get added before inferred modes, and are thus considered potential
    duplicates.
    
    Or as ajax put it on irc:
    "< ajax> the point of the "pick a timing formula" heuristic was to
    generate something the sink could _likely_ sink.  if it tells us
    timings it can sink explicitly then second-guessing seems dumb."
    
    Cc: Adam Jackson <ajax@redhat.com>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/drm_edid.c
+++ drivers/gpu/drm/drm_edid.c
@@ -3744,10 +3744,10 @@ int drm_add_edid_modes(struct drm_connector *connector, struct edid *edid)
 	num_modes += add_cvt_modes(connector, edid);
 	num_modes += add_standard_modes(connector, edid);
 	num_modes += add_established_modes(connector, edid);
-	if (edid->features & DRM_EDID_FEATURE_DEFAULT_GTF)
-		num_modes += add_inferred_modes(connector, edid);
 	num_modes += add_cea_modes(connector, edid);
 	num_modes += add_alternate_cea_modes(connector, edid);
+	if (edid->features & DRM_EDID_FEATURE_DEFAULT_GTF)
+		num_modes += add_inferred_modes(connector, edid);
 
 	if (quirks & (EDID_QUIRK_PREFER_LARGE_60 | EDID_QUIRK_PREFER_LARGE_75))
 		edid_fixup_preferred(connector, quirks);
