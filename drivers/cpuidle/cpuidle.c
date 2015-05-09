7312280bd2ad9df1bcca236c5614091a0bd1504c
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2015-05-09 21:50:32 +0200

    cpuidle: Fix the kerneldoc comment for cpuidle_enter_state()
    The kerneldoc comment for cpuidle_enter_state() doesn't match the
    function's header any more, so fix it.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -150,7 +150,7 @@ int cpuidle_enter_freeze(struct cpuidle_driver *drv, struct cpuidle_device *dev)
  * cpuidle_enter_state - enter the state and update stats
  * @dev: cpuidle device for this cpu
  * @drv: cpuidle driver for this cpu
- * @next_state: index into drv->states of the state to enter
+ * @index: index into the states table in @drv of the state to enter
  */
 int cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_driver *drv,
 			int index)
