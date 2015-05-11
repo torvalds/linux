d0634868d30ed2c511b45c63efe8eeb85c62c7f6
Florian Fainelli <f.fainelli@gmail.com>
2015-05-11 15:12:42 -0700

    net: systemport: Implement RX coalescing control knobs
    Similarly to the TX path, allow the RX path to be configured with both
    'rx-frames' and 'rx-usecs' coalescing parameters.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/bcmsysport.c
+++ drivers/net/ethernet/broadcom/bcmsysport.c
@@ -467,6 +467,11 @@ static int bcm_sysport_get_coalesce(struct net_device *dev,
 	ec->tx_coalesce_usecs = (reg >> RING_TIMEOUT_SHIFT) * 8192 / 1000;
 	ec->tx_max_coalesced_frames = reg & RING_INTR_THRESH_MASK;
 
+	reg = rdma_readl(priv, RDMA_MBDONE_INTR);
+
+	ec->rx_coalesce_usecs = (reg >> RDMA_TIMEOUT_SHIFT) * 8192 / 1000;
+	ec->rx_max_coalesced_frames = reg & RDMA_INTR_THRESH_MASK;
+
 	return 0;
 }
 
@@ -477,16 +482,18 @@ static int bcm_sysport_set_coalesce(struct net_device *dev,
 	unsigned int i;
 	u32 reg;
 
-	/* Base system clock is 125Mhz, TDMA timeout is this reference clock
-	 * divided by 1024, which yield roughly 8.192 us, our maximum value
-	 * has to fit in the RING_TIMEOUT_MASK (16 bits).
+	/* Base system clock is 125Mhz, DMA timeout is this reference clock
+	 * divided by 1024, which yield roughly 8.192 us, our maximum value has
+	 * to fit in the RING_TIMEOUT_MASK (16 bits).
 	 */
 	if (ec->tx_max_coalesced_frames > RING_INTR_THRESH_MASK ||
-	    ec->tx_coalesce_usecs > (RING_TIMEOUT_MASK * 8) + 1)
+	    ec->tx_coalesce_usecs > (RING_TIMEOUT_MASK * 8) + 1 ||
+	    ec->rx_max_coalesced_frames > RDMA_INTR_THRESH_MASK ||
+	    ec->rx_coalesce_usecs > (RDMA_TIMEOUT_MASK * 8) + 1)
 		return -EINVAL;
 
-	if (ec->tx_coalesce_usecs == 0 &&
-	    ec->tx_max_coalesced_frames == 0)
+	if ((ec->tx_coalesce_usecs == 0 && ec->tx_max_coalesced_frames == 0) ||
+	    (ec->rx_coalesce_usecs == 0 && ec->rx_max_coalesced_frames == 0))
 		return -EINVAL;
 
 	for (i = 0; i < dev->num_tx_queues; i++) {
@@ -498,6 +505,14 @@ static int bcm_sysport_set_coalesce(struct net_device *dev,
 			 RING_TIMEOUT_SHIFT;
 		tdma_writel(priv, reg, TDMA_DESC_RING_INTR_CONTROL(i));
 	}
+
+	reg = rdma_readl(priv, RDMA_MBDONE_INTR);
+	reg &= ~(RDMA_INTR_THRESH_MASK |
+		 RDMA_TIMEOUT_MASK << RDMA_TIMEOUT_SHIFT);
+	reg |= ec->rx_max_coalesced_frames;
+	reg |= DIV_ROUND_UP(ec->rx_coalesce_usecs * 1000, 8192) <<
+			    RDMA_TIMEOUT_SHIFT;
+	rdma_writel(priv, reg, RDMA_MBDONE_INTR);
 
 	return 0;
 }
