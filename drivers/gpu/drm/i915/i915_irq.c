4bca26d0a6518d51a9abe64fbde4b12f04c74053
Ville Syrjälä <ville.syrjala@linux.intel.com>
2015-05-11 20:49:10 +0300

    drm/i915: Use HOTPLUG_INT_STATUS_G4X on VLV/CHV
    Use HOTPLUG_INT_STATUS_G4X instead of HOTPLUG_INT_STATUS_I915 on VLV/CHV
    so that we don't confuse the AUX status bits with SDVO status bits.
    
    Avoid pointless log spam as below while handling AUX interrupts:
    [drm:intel_hpd_irq_handler] hotplug event received, stat 0x00000040, dig 0x00000000
    [drm:intel_hpd_irq_handler] hotplug event received, stat 0x00000040, dig 0x00000000
    [drm:intel_hpd_irq_handler] hotplug event received, stat 0x00000040, dig 0x00000000
    [drm:intel_hpd_irq_handler] hotplug event received, stat 0x00000040, dig 0x00000000
    [drm:intel_hpd_irq_handler] hotplug event received, stat 0x00000040, dig 0x00000000
    [drm:intel_dp_aux_ch] dp_aux_ch timeout status 0x71450064
    
    Note that there's no functional issue, it's just that the sdvo bits
    overlap with the dp aux bits. Hence every time we receive an aux
    interrupt we also think there's an sdvo hpd interrupt, but due to lack
    of any sdvo encoders nothing ever happens because of that.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    [danvet: Add Ville's explanation why nothing functional really
    changes.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_irq.c
+++ drivers/gpu/drm/i915/i915_irq.c
@@ -79,7 +79,7 @@ static const u32 hpd_status_g4x[HPD_NUM_PINS] = {
 	[HPD_PORT_D] = PORTD_HOTPLUG_INT_STATUS
 };
 
-static const u32 hpd_status_i915[HPD_NUM_PINS] = { /* i915 and valleyview are the same */
+static const u32 hpd_status_i915[HPD_NUM_PINS] = {
 	[HPD_CRT] = CRT_HOTPLUG_INT_STATUS,
 	[HPD_SDVO_B] = SDVOB_HOTPLUG_INT_STATUS_I915,
 	[HPD_SDVO_C] = SDVOC_HOTPLUG_INT_STATUS_I915,
@@ -1743,7 +1743,7 @@ static void i9xx_hpd_irq_handler(struct drm_device *dev)
 		 */
 		POSTING_READ(PORT_HOTPLUG_STAT);
 
-		if (IS_G4X(dev)) {
+		if (IS_G4X(dev) || IS_VALLEYVIEW(dev)) {
 			u32 hotplug_trigger = hotplug_status & HOTPLUG_INT_STATUS_G4X;
 
 			intel_hpd_irq_handler(dev, hotplug_trigger, 0, hpd_status_g4x);
