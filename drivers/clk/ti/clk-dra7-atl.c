e0cdcda508f110b7ec190dc7c5eb2869ba73a535
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2015-05-13 15:54:40 +0900

    clk: ti: dra7-atl-clock: Fix possible ERR_PTR dereference
    of_clk_get_from_provider() returns ERR_PTR on failure. The
    dra7-atl-clock driver was not checking its return value and
    immediately used it in __clk_get_hw().  __clk_get_hw()
    dereferences supplied clock, if it is not NULL, so in that case
    it would dereference an ERR_PTR.
    
    Fixes: 9ac33b0ce81f ("CLK: TI: Driver for DRA7 ATL (Audio Tracking Logic)")
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/ti/clk-dra7-atl.c
+++ drivers/clk/ti/clk-dra7-atl.c
@@ -252,6 +252,11 @@ static int of_dra7_atl_clk_probe(struct platform_device *pdev)
 		}
 
 		clk = of_clk_get_from_provider(&clkspec);
+		if (IS_ERR(clk)) {
+			pr_err("%s: failed to get atl clock %d from provider\n",
+			       __func__, i);
+			return PTR_ERR(clk);
+		}
 
 		cdesc = to_atl_desc(__clk_get_hw(clk));
 		cdesc->cinfo = cinfo;
