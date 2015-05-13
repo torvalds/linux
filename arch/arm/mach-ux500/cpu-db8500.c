951b833ee88c2b6df4346414dc9d88a96076ff2d
Linus Walleij <linus.walleij@linaro.org>
2015-05-13 15:01:48 +0200

    ARM: ux500: delete UART static map
    Delete the static maps for the Ux500 UARTs and rely
    on debug_ll_io_init() to do the trick.
    commit f87b95dd2e4a8832b1d6034f15a5324db42608a0
    "ARM: ux500: move debugmacro to debug includes"
    defined the necessary addruart macro two years ago.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/cpu-db8500.c
+++ arch/arm/mach-ux500/cpu-db8500.c
@@ -43,12 +43,6 @@ static struct prcmu_pdata db8500_prcmu_pdata = {
 	.legacy_offset	= DB8500_PRCMU_LEGACY_OFFSET,
 };
 
-/* minimum static i/o mapping required to boot U8500 platforms */
-static struct map_desc u8500_uart_io_desc[] __initdata = {
-	__IO_DEV_DESC(U8500_UART0_BASE, SZ_4K),
-	__IO_DEV_DESC(U8500_UART2_BASE, SZ_4K),
-};
-
 /*  U8500 and U9540 common io_desc */
 static struct map_desc u8500_common_io_desc[] __initdata = {
 	/* SCU base also covers GIC CPU BASE and TWD with its 4K page */
@@ -73,11 +67,7 @@ static struct map_desc u9540_io_desc[] __initdata = {
 
 static void __init u8500_map_io(void)
 {
-	/*
-	 * Map the UARTs early so that the DEBUG_LL stuff continues to work.
-	 */
-	iotable_init(u8500_uart_io_desc, ARRAY_SIZE(u8500_uart_io_desc));
-
+	debug_ll_io_init();
 	ux500_map_io();
 
 	iotable_init(u8500_common_io_desc, ARRAY_SIZE(u8500_common_io_desc));
