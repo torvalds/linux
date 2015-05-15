1e321fa917fb2d30d39ff1c6ea89d6f1cf4f34a5
Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
2015-05-14 18:34:43 -0700

    MIPS64: Support of at least 48 bits of SEGBITS
    SEGBITS is 40 bits or more, depending on CPU type.  Introduces optional
    support for 48 bits of application virtual address space.  Only 16K and
    64K pages are supported.
    
    Enabling will result in a memory overhead of a small number of pages for
    small applications.  For 64K pages a 3rd level of page tables is required
    which has some impact during software TLB refill.
    
    [ralf@linux-mips.org: Fixed things raised in the review of the version
    posted and changed kconfig to be a bit more userfriendly.]
    
    Signed-off-by: Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
    Cc: aleksey.makarov@auriga.com
    Cc: james.hogan@imgtec.com
    Cc: paul.burton@imgtec.com
    Cc: david.daney@cavium.com
    Cc: peterz@infradead.org
    Cc: linux-mips@linux-mips.org
    Cc: linux-kernel@vger.kernel.org
    Cc: davidlohr@hp.com
    Cc: kirill@shutemov.name
    Cc: akpm@linux-foundation.org
    Cc: mingo@kernel.org
    Patchwork: https://patchwork.linux-mips.org/patch/10051/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/processor.h
+++ arch/mips/include/asm/processor.h
@@ -63,7 +63,11 @@ extern unsigned int vced_count, vcei_count;
  * 8192EB ...
  */
 #define TASK_SIZE32	0x7fff8000UL
-#define TASK_SIZE64	0x10000000000UL
+#ifdef CONFIG_MIPS_VA_BITS_48
+#define TASK_SIZE64     (0x1UL << ((cpu_data[0].vmbits>48)?48:cpu_data[0].vmbits))
+#else
+#define TASK_SIZE64     0x10000000000UL
+#endif
 #define TASK_SIZE (test_thread_flag(TIF_32BIT_ADDR) ? TASK_SIZE32 : TASK_SIZE64)
 #define STACK_TOP_MAX	TASK_SIZE64
 
