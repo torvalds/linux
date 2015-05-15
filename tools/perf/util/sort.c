2f15bd8c6c6e80f15c899ea84b53fa607a2f08b2
Jiri Olsa <jolsa@kernel.org>
2015-05-15 17:54:28 +0200

    perf tools: Fix "Command" sort_entry's cmp and collapse function
    Currently the se_cmp and se_collapse use pointer comparison,
    which is ok for for testing equality of strings. It's not ok
    as comparing function for rbtree insertion, because it gives
    different results based on current pointer values.
    
    We saw test 32 (hists cumulation test) failing based on different
    environment setup. Having all sort functions straightened fix the
    test for us.
    
    Reported-by: Jan Stancek <jstancek@redhat.com>
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jan Stancek <jstancek@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/sort.c
+++ tools/perf/util/sort.c
@@ -89,14 +89,14 @@ static int64_t
 sort__comm_cmp(struct hist_entry *left, struct hist_entry *right)
 {
 	/* Compare the addr that should be unique among comm */
-	return comm__str(right->comm) - comm__str(left->comm);
+	return strcmp(comm__str(right->comm), comm__str(left->comm));
 }
 
 static int64_t
 sort__comm_collapse(struct hist_entry *left, struct hist_entry *right)
 {
 	/* Compare the addr that should be unique among comm */
-	return comm__str(right->comm) - comm__str(left->comm);
+	return strcmp(comm__str(right->comm), comm__str(left->comm));
 }
 
 static int64_t
