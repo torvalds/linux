744b058827b3db9a4f6027522dd9c73a208c2d31
Gustavo Padovan <gustavo.padovan@collabora.co.uk>
2015-05-15 16:12:27 -0300

    drm/atomic: remove duplicated assignment of old_plane_state
    old_plane_state is already assigned to old_state->plane_states[i] inside
    for_each_plane_in_state(). Here we remove an the extra assignment.
    
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/drm_atomic_helper.c
+++ drivers/gpu/drm/drm_atomic_helper.c
@@ -1151,8 +1151,6 @@ void drm_atomic_helper_commit_planes(struct drm_device *dev,
 		if (!funcs)
 			continue;
 
-		old_plane_state = old_state->plane_states[i];
-
 		/*
 		 * Special-case disabling the plane if drivers support it.
 		 */
