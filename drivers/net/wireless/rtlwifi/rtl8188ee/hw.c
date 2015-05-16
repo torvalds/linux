1277fa2ab2f9a624a4b0177119ca13b5fd65edd0
Vincent Fann <vincent_fann@realtek.com>
2015-05-15 21:29:27 -0500

    rtlwifi: Remove the clear interrupt routine from all drivers
    Several of these drivers have there TX randomly blocked for 3~5 seconds while
    measuring tx throughput (iperf). The root couse happens in rtl_pci_flush().
    The function uses a while-loop to wait for TX queue length to decrease to 0.
    The TX queue length counts the number of packets that are queued in the driver.
    The driver relys on the TX OK interrupt to return skb and reduce TX queue length.
    
    The interrupt subroutine disables interupts, reads the interrupt registers, and
    then clears the registers in the beginning of _rtl_pci_interrupt(). After all
    interupts process are finished, the driver invokes enable_interrupt() to enable
    interupts. This behavior is normal for an interrupt subroutine.
    
    But enable_interrupt() invokes clear_interrupt() again. This unexpected interrupt
    clearing may cleari me fresh TX OK interrupts. These missing interrupts cause TX
    queue length to never reduce to 0i, which causes rtl_pci_flush() to be stuck in
    unterminated while-loop.
    
    This patch removes clear_interrupt() in enable_interrupt() to avoid this behavior.
    
    Signed-off-by: Vincent Fann <vincent_fann@realtek.com>
    Signed-off-by: Shao Fu <shaofu@realtek.com>
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Cc: Stable <stable@vger.kernel.org> [3.18+]
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/rtlwifi/rtl8188ee/hw.c
+++ drivers/net/wireless/rtlwifi/rtl8188ee/hw.c
@@ -1354,27 +1354,11 @@ void rtl88ee_set_qos(struct ieee80211_hw *hw, int aci)
 	}
 }
 
-static void rtl88ee_clear_interrupt(struct ieee80211_hw *hw)
-{
-	struct rtl_priv *rtlpriv = rtl_priv(hw);
-	u32 tmp;
-
-	tmp = rtl_read_dword(rtlpriv, REG_HISR);
-	rtl_write_dword(rtlpriv, REG_HISR, tmp);
-
-	tmp = rtl_read_dword(rtlpriv, REG_HISRE);
-	rtl_write_dword(rtlpriv, REG_HISRE, tmp);
-
-	tmp = rtl_read_dword(rtlpriv, REG_HSISR);
-	rtl_write_dword(rtlpriv, REG_HSISR, tmp);
-}
-
 void rtl88ee_enable_interrupt(struct ieee80211_hw *hw)
 {
 	struct rtl_priv *rtlpriv = rtl_priv(hw);
 	struct rtl_pci *rtlpci = rtl_pcidev(rtl_pcipriv(hw));
 
-	rtl88ee_clear_interrupt(hw);/*clear it here first*/
 	rtl_write_dword(rtlpriv, REG_HIMR,
 			rtlpci->irq_mask[0] & 0xFFFFFFFF);
 	rtl_write_dword(rtlpriv, REG_HIMRE,
