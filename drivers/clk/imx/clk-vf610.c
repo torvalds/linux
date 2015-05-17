d930d56825e934dc464cdad3f909333f994a89f3
Stefan Agner <stefan@agner.ch>
2015-05-18 00:13:33 +0200

    ARM: imx: clk-vf610: enable debug access port by default
    Enabled DAP (debug access port) by default. This enables the hw-
    breakpoint framework to make use of the breakpoints and watchpoints
    supported by hardware.
    
    [    0.215805] hw-breakpoint: found 2 (+1 reserved) breakpoint and 1 watchpoint registers.
    [    0.224624] hw-breakpoint: maximum watchpoint size is 4 bytes.
    
    Without this clock, the hw-breakpoint driver claims an undefined
    instruction during initialization:
    [    0.227380] hw-breakpoint: Debug register access (0xee003e17) caused undefined instruction on CPU 0
    [    0.227519] hw-breakpoint: CPU 0 failed to disable vector catch
    
    Signed-off-by: Stefan Agner <stefan@agner.ch>
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- drivers/clk/imx/clk-vf610.c
+++ drivers/clk/imx/clk-vf610.c
@@ -118,6 +118,7 @@ static struct clk_onecell_data clk_data;
 static unsigned int const clks_init_on[] __initconst = {
 	VF610_CLK_SYS_BUS,
 	VF610_CLK_DDR_SEL,
+	VF610_CLK_DAP,
 };
 
 static struct clk * __init vf610_get_fixed_clock(
@@ -383,6 +384,7 @@ static void __init vf610_clocks_init(struct device_node *ccm_node)
 	clk[VF610_CLK_DMAMUX3] = imx_clk_gate2("dmamux3", "platform_bus", CCM_CCGR6, CCM_CCGRx_CGn(2));
 
 	clk[VF610_CLK_SNVS] = imx_clk_gate2("snvs-rtc", "ipg_bus", CCM_CCGR6, CCM_CCGRx_CGn(7));
+	clk[VF610_CLK_DAP] = imx_clk_gate("dap", "platform_bus", CCM_CCSR, 24);
 
 	imx_check_clocks(clk, ARRAY_SIZE(clk));
 
