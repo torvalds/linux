d0e72cc0ac3dcebf0de179ba1dd33a276642c5bb
Subhransu S. Prusty <subhransu.s.prusty@intel.com>
2015-05-19 15:00:39 +0530

    ASoC: Intel: Initialize max mailbox size for haswell
    Signed-off-by: Subhransu S. Prusty <subhransu.s.prusty@intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Acked-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/haswell/sst-haswell-ipc.c
+++ sound/soc/intel/haswell/sst-haswell-ipc.c
@@ -2127,6 +2127,9 @@ int sst_hsw_dsp_init(struct device *dev, struct sst_pdata *pdata)
 	ipc->ops.reply_msg_match = hsw_reply_msg_match;
 	ipc->ops.is_dsp_busy = hsw_is_dsp_busy;
 
+	ipc->tx_data_max_size = IPC_MAX_MAILBOX_BYTES;
+	ipc->rx_data_max_size = IPC_MAX_MAILBOX_BYTES;
+
 	ret = sst_ipc_init(ipc);
 	if (ret != 0)
 		goto ipc_init_err;
