efa7d1c733d1d2c1a468b85126d70bad9fdf6ba8
Tejun Heo <tj@kernel.org>
2015-05-22 17:13:18 -0400

    update !CONFIG_BLK_CGROUP dummies in include/linux/blk-cgroup.h
    The header file will be used more widely with the pending cgroup
    writeback support and the current set of dummy declarations aren't
    enough to handle different config combinations.  Update as follows.
    
    * Drop the struct cgroup declaration.  None of the dummy defs need it.
    
    * Define blkcg as an empty struct instead of just declaring it.
    
    * Wrap dummy function defs in CONFIG_BLOCK.  Some functions use block
      data types and none of them are to be used w/o block enabled.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- include/linux/blk-cgroup.h
+++ include/linux/blk-cgroup.h
@@ -558,8 +558,8 @@ static inline void blkg_rwstat_merge(struct blkg_rwstat *to,
 
 #else	/* CONFIG_BLK_CGROUP */
 
-struct cgroup;
-struct blkcg;
+struct blkcg {
+};
 
 struct blkg_policy_data {
 };
@@ -569,6 +569,8 @@ struct blkcg_gq {
 
 struct blkcg_policy {
 };
+
+#ifdef CONFIG_BLOCK
 
 static inline struct blkcg_gq *blkg_lookup(struct blkcg *blkcg, void *key) { return NULL; }
 static inline int blkcg_init_queue(struct request_queue *q) { return 0; }
@@ -599,5 +601,6 @@ static inline struct request_list *blk_rq_rl(struct request *rq) { return &rq->q
 #define blk_queue_for_each_rl(rl, q)	\
 	for ((rl) = &(q)->root_rl; (rl); (rl) = NULL)
 
+#endif	/* CONFIG_BLOCK */
 #endif	/* CONFIG_BLK_CGROUP */
 #endif	/* _BLK_CGROUP_H */
