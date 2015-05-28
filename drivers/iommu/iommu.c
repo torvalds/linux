92e7066fde31d5ac48a9bccc12d3063d251dd079
Joerg Roedel <jroedel@suse.de>
2015-05-28 18:41:24 +0200

    iommu: Remove function name from pr_fmt()
    Including the function name is only useful for debugging
    messages. They don't belong into other messages from the
    iommu core.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  */
 
-#define pr_fmt(fmt)    "%s: " fmt, __func__
+#define pr_fmt(fmt)    "iommu: " fmt
 
 #include <linux/device.h>
 #include <linux/kernel.h>
