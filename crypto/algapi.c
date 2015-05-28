c2110f28341cec2ecfd9474db15ac090bce1234c
Herbert Xu <herbert@gondor.apana.org.au>
2015-05-28 22:07:56 +0800

    crypto: api - Include alignment in crypto_alg_extsize
    This patch ensures that the tfm context always has enough extra
    memory to ensure that it is aligned according to cra_alignment.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/algapi.c
+++ crypto/algapi.c
@@ -980,7 +980,8 @@ EXPORT_SYMBOL_GPL(crypto_xor);
 
 unsigned int crypto_alg_extsize(struct crypto_alg *alg)
 {
-	return alg->cra_ctxsize;
+	return alg->cra_ctxsize +
+	       (alg->cra_alignmask & ~(crypto_tfm_ctx_alignment() - 1));
 }
 EXPORT_SYMBOL_GPL(crypto_alg_extsize);
 
