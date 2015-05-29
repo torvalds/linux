a3fbe05a611811ad8413130f63aaa79428b00377
John Harrison <John.C.Harrison@Intel.com>
2015-05-29 17:43:38 +0100

    drm/i915: Don't tag kernel batches as user batches
    The render state initialisation code does an explicit i915_add_request() call to
    commit the init commands. It was passing in the initialisation batch buffer to
    add_request() as the batch object parameter. However, the batch object entry in
    the request structure (which is all that parameter is used for) is meant for
    keeping track of user generated batch buffers for blame tagging during GPU
    hangs.
    
    This patch clears the batch object parameter so that kernel generated batch
    buffers are not tagged as being user generated.
    
    For: VIZ-5115
    Signed-off-by: John Harrison <John.C.Harrison@Intel.com>
    Reviewed-by: Tomas Elf <tomas.elf@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_gem_render_state.c
+++ drivers/gpu/drm/i915/i915_gem_render_state.c
@@ -173,7 +173,7 @@ int i915_gem_render_state_init(struct intel_engine_cs *ring)
 
 	i915_vma_move_to_active(i915_gem_obj_to_ggtt(so.obj), ring);
 
-	__i915_add_request(ring, NULL, so.obj, true);
+	__i915_add_request(ring, NULL, NULL, true);
 	/* __i915_add_request moves object to inactive if it fails */
 out:
 	i915_gem_render_state_fini(&so);
