5e9b4e31916c52499a08db505c59cfa29d472972
Olliver Schinagl <o.schinagl@ultimaker.com>
2015-05-29 16:09:43 +0200

    leds: pca963x: alphabetize headers
    Re-order headers so they are in alphabetical order.
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

--- drivers/leds/leds-pca963x.c
+++ drivers/leds/leds-pca963x.c
@@ -26,16 +26,16 @@
  */
 
 #include <linux/acpi.h>
-#include <linux/module.h>
-#include <linux/delay.h>
-#include <linux/string.h>
 #include <linux/ctype.h>
-#include <linux/leds.h>
+#include <linux/delay.h>
 #include <linux/err.h>
 #include <linux/i2c.h>
-#include <linux/slab.h>
+#include <linux/leds.h>
+#include <linux/module.h>
 #include <linux/of.h>
 #include <linux/platform_data/leds-pca963x.h>
+#include <linux/slab.h>
+#include <linux/string.h>
 
 /* LED select registers determine the source that drives LED outputs */
 #define PCA963X_LED_OFF		0x0	/* LED driver off */
