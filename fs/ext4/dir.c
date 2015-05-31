6bc445e0ff44c7e83225124c214d350407e68ccf
Theodore Ts'o <tytso@mit.edu>
2015-05-31 13:34:57 -0400

    ext4 crypto: make sure the encryption info is initialized on opendir(2)
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/dir.c
+++ fs/ext4/dir.c
@@ -593,6 +593,13 @@ finished:
 	return 0;
 }
 
+static int ext4_dir_open(struct inode * inode, struct file * filp)
+{
+	if (ext4_encrypted_inode(inode))
+		return ext4_get_encryption_info(inode) ? -EACCES : 0;
+	return 0;
+}
+
 static int ext4_release_dir(struct inode *inode, struct file *filp)
 {
 	if (filp->private_data)
@@ -635,5 +642,6 @@ const struct file_operations ext4_dir_operations = {
 	.compat_ioctl	= ext4_compat_ioctl,
 #endif
 	.fsync		= ext4_sync_file,
+	.open		= ext4_dir_open,
 	.release	= ext4_release_dir,
 };
