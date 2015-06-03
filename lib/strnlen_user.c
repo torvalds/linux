226a07ef0a5a2dfad4cce1a5c226c4cb7370d41f
Jan Kara <jack@suse.cz>
2015-06-03 15:50:35 +0200

    lib: Clarify the return value of strnlen_user()
    strnlen_user() can return a number in a range 0 to count +
    sizeof(unsigned long) - 1. Clarify the comment at the top of the
    function so that users don't think the function returns at most count+1.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    [ Also added commentary about preferably not using this function ]
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/strnlen_user.c
+++ lib/strnlen_user.c
@@ -90,8 +90,15 @@ static inline long do_strnlen_user(const char __user *src, unsigned long count, 
  * Get the size of a NUL-terminated string in user space.
  *
  * Returns the size of the string INCLUDING the terminating NUL.
- * If the string is too long, returns 'count+1'.
+ * If the string is too long, returns a number larger than @count. User
+ * has to check the return value against "> count".
  * On exception (or invalid count), returns 0.
+ *
+ * NOTE! You should basically never use this function. There is
+ * almost never any valid case for using the length of a user space
+ * string, since the string can be changed at any time by other
+ * threads. Use "strncpy_from_user()" instead to get a stable copy
+ * of the string.
  */
 long strnlen_user(const char __user *str, long count)
 {
