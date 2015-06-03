1af21985473d72965807ef5e5cc02528aa8c01e4
Marek Szyprowski <m.szyprowski@samsung.com>
2015-06-03 07:36:23 -0300

    [media] s5p-mfc: add additional check for incorrect memory configuration
    MFC hardware is known to trash random memory if one tries to use a
    buffer which has lower DMA addresses than the configured DMA base
    address. This patch adds a check for this case and proper error
    handling.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Acked-by: Kamil Debski <kamil@wypas.org>
    [s.nawrocki@samsung.com: fixed typo (addres -> address]
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/platform/s5p-mfc/s5p_mfc_opr.h
+++ drivers/media/platform/s5p-mfc/s5p_mfc_opr.h
@@ -334,7 +334,7 @@ struct s5p_mfc_hw_ops {
 
 void s5p_mfc_init_hw_ops(struct s5p_mfc_dev *dev);
 void s5p_mfc_init_regs(struct s5p_mfc_dev *dev);
-int s5p_mfc_alloc_priv_buf(struct device *dev,
+int s5p_mfc_alloc_priv_buf(struct device *dev, dma_addr_t base,
 					struct s5p_mfc_priv_buf *b);
 void s5p_mfc_release_priv_buf(struct device *dev,
 					struct s5p_mfc_priv_buf *b);
