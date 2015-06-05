57334a30c8da966e4470572d3096873e26c66076
Ian Abbott <abbotti@mev.co.uk>
2015-06-05 18:30:35 +0100

    staging: comedi: das08: use better MODULE_DESCRIPTION()
    Replace the boiler-plate Comedi module description string with something
    more specific.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/das08.c
+++ drivers/staging/comedi/drivers/das08.c
@@ -476,5 +476,5 @@ static void __exit das08_exit(void)
 module_exit(das08_exit);
 
 MODULE_AUTHOR("Comedi http://www.comedi.org");
-MODULE_DESCRIPTION("Comedi low-level driver");
+MODULE_DESCRIPTION("Comedi common DAS08 support module");
 MODULE_LICENSE("GPL");
