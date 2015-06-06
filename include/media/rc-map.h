4862690d82eec2d530bf78803009a1e3bb82be9a
Jan Klötzke <jan@kloetzke.net>
2015-06-06 16:58:10 -0300

    [media] rc/keymaps: add keytable for Terratec Cinergy C PCI
    This RC map was taken from Christoph Pinkl's patch
    (http://patchwork.linuxtv.org/patch/7217/). It is used solely by the respective
    mantis based card because the encoding is not known.
    
    Signed-off-by: Jan Klötzke <jan@kloetzke.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- include/media/rc-map.h
+++ include/media/rc-map.h
@@ -196,6 +196,7 @@ void rc_map_init(void);
 #define RC_MAP_TBS_NEC                   "rc-tbs-nec"
 #define RC_MAP_TECHNISAT_TS35            "rc-technisat-ts35"
 #define RC_MAP_TECHNISAT_USB2            "rc-technisat-usb2"
+#define RC_MAP_TERRATEC_CINERGY_C_PCI    "rc-terratec-cinergy-c-pci"
 #define RC_MAP_TERRATEC_CINERGY_XS       "rc-terratec-cinergy-xs"
 #define RC_MAP_TERRATEC_SLIM             "rc-terratec-slim"
 #define RC_MAP_TERRATEC_SLIM_2           "rc-terratec-slim-2"
