97ac46a5087eaf87fd76ff7bb31ec9c896010442
Dave Hansen <dave.hansen@linux.intel.com>
2015-06-07 11:37:06 -0700

    x86/mpx: Allow 32-bit binaries on 64-bit kernels again
    Now that the bugs in mixed mode MPX handling are fixed, re-allow
    32-bit binaries on 64-bit kernels again.
    
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Dave Hansen <dave@sr71.net>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/20150607183706.70277DAD@viggo.jf.intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/mm/mpx.c
+++ arch/x86/mm/mpx.c
@@ -367,12 +367,6 @@ static __user void *mpx_get_bounds_dir(void)
 		return MPX_INVALID_BOUNDS_DIR;
 
 	/*
-	 * 32-bit binaries on 64-bit kernels are currently
-	 * unsupported.
-	 */
-	if (IS_ENABLED(CONFIG_X86_64) && test_thread_flag(TIF_IA32))
-		return MPX_INVALID_BOUNDS_DIR;
-	/*
 	 * The bounds directory pointer is stored in a register
 	 * only accessible if we first do an xsave.
 	 */
