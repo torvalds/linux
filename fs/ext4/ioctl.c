b4ab9e29820bf2e8842281a6b5e645e59c9992a5
Fabian Frederick <fabf@skynet.be>
2015-06-08 12:23:21 -0400

    ext4 crypto: fix sparse warnings in fs/ext4/ioctl.c
    [ Added another sparse fix for EXT4_IOC_GET_ENCRYPTION_POLICY while
      we're at it. --tytso ]
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/ioctl.c
+++ fs/ext4/ioctl.c
@@ -675,8 +675,8 @@ encryption_policy_out:
 			if (err)
 				return err;
 		}
-		if (copy_to_user((void *) arg, sbi->s_es->s_encrypt_pw_salt,
-				 16))
+		if (copy_to_user((void __user *) arg,
+				 sbi->s_es->s_encrypt_pw_salt, 16))
 			return -EFAULT;
 		return 0;
 	}
@@ -690,7 +690,7 @@ encryption_policy_out:
 		err = ext4_get_policy(inode, &policy);
 		if (err)
 			return err;
-		if (copy_to_user((void *)arg, &policy, sizeof(policy)))
+		if (copy_to_user((void __user *)arg, &policy, sizeof(policy)))
 			return -EFAULT;
 		return 0;
 #else
