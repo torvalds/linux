c4263e319ef923eab9041174dde248484ed71771
Sunghoon Cho <ywhsbliss@gmail.com>
2015-06-15 19:42:31 +0900

    staging: wilc1000: remove multiple blank lines.
    Remove the warnings for multiple blank lines reported by checkpatch.pl.
    
    Signed-off-by: Sunghoon Cho <ywhsbliss@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/coreconfigsimulator.h
+++ drivers/staging/wilc1000/coreconfigsimulator.h
@@ -8,13 +8,10 @@
  *  @version	1.0
  */
 
-
 #ifndef CORECONFIGSIMULATOR_H
 #define CORECONFIGSIMULATOR_H
 
-
 extern s32 CoreConfigSimulatorInit (void);
 extern s32 CoreConfigSimulatorDeInit (void);
-
 
 #endif
