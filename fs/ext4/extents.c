4134f5c88dcd5b00e4a5f37c3842b2b831a61ee1
Lukas Czerner <lczerner@redhat.com>
2015-06-15 00:20:46 -0400

    ext4: recalculate journal credits as inode depth changes
    Currently in ext4_alloc_file_blocks() the number of credits is
    calculated only once before we enter the allocation loop. However within
    the allocation loop the extent tree depth can change, hence the number
    of credits needed can increase potentially exceeding the number of credits
    reserved in the handle which can cause journal failures.
    
    Fix this by recalculating number of credits when the inode depth
    changes. Note that even though ext4_alloc_file_blocks() is only
    currently used by extent base inodes we will avoid recalculating number
    of credits unnecessarily in the case of indirect based inodes.
    
    Signed-off-by: Lukas Czerner <lczerner@redhat.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -4663,6 +4663,7 @@ static int ext4_alloc_file_blocks(struct file *file, ext4_lblk_t offset,
 	int ret = 0;
 	int ret2 = 0;
 	int retries = 0;
+	int depth = 0;
 	struct ext4_map_blocks map;
 	unsigned int credits;
 	loff_t epos;
@@ -4681,9 +4682,24 @@ static int ext4_alloc_file_blocks(struct file *file, ext4_lblk_t offset,
 	 * credits to insert 1 extent into extent tree
 	 */
 	credits = ext4_chunk_trans_blocks(inode, len);
+	/*
+	 * We can only call ext_depth() on extent based inodes
+	 */
+	if (ext4_test_inode_flag(inode, EXT4_INODE_EXTENTS))
+		depth = ext_depth(inode);
+	else
+		depth = -1;
 
 retry:
 	while (ret >= 0 && len) {
+		/*
+		 * Recalculate credits when extent tree depth changes.
+		 */
+		if (depth >= 0 && depth != ext_depth(inode)) {
+			credits = ext4_chunk_trans_blocks(inode, len);
+			depth = ext_depth(inode);
+		}
+
 		handle = ext4_journal_start(inode, EXT4_HT_MAP_BLOCKS,
 					    credits);
 		if (IS_ERR(handle)) {
