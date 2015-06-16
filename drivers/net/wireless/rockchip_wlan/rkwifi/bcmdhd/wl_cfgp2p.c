c983156c6489e98e4b6986c9636d97e659d0ac11
hwg <hwg@rock-chips.com>
2015-06-16 11:02:56 +0800

    bcmdhd wifi: after android stop; start, wpa_supplicant start fail because of Failed to create a P2P Device interface p2p-dev-wlan0

--- drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/wl_cfgp2p.c
+++ drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/wl_cfgp2p.c
@@ -2795,7 +2795,8 @@ wl_cfgp2p_add_p2p_disc_if(struct bcm_cfg80211 *cfg)
 
 	if (cfg->p2p_wdev) {
 		CFGP2P_ERR(("p2p_wdev defined already.\n"));
-#if (defined(CUSTOMER_HW10) && defined(CONFIG_ARCH_ODIN))
+//#if (defined(CUSTOMER_HW10) && defined(CONFIG_ARCH_ODIN))
+#if 1 // after android stop; start, wpa_supplicant start fail because of "Failed to create a P2P Device interface p2p-dev-wlan0", huweiguo
 		wl_cfgp2p_del_p2p_disc_if(cfg->p2p_wdev, cfg);
 		CFGP2P_ERR(("p2p_wdev deleted.\n"));
 #else
