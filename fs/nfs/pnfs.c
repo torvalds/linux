759367b8eaa40930166671679a00f5ec97e2c5fa
Trond Myklebust <trond.myklebust@primarydata.com>
2015-06-17 19:41:51 -0400

    pNFS: Fix a memory leak when attempted pnfs fails
    commit 1ca018d28d96d07788474abf66a5f3e9594841f5 upstream.
    
    pnfs_do_write() expects the call to pnfs_write_through_mds() to free the
    pgio header and to release the layout segment before exiting. The problem
    is that nfs_pgio_data_destroy() doesn't actually do this; it only frees
    the memory allocated by nfs_generic_pgio().
    
    Ditto for pnfs_do_read()...
    
    Fix in both cases is to add a call to hdr->release(hdr).
    
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/nfs/pnfs.c
+++ fs/nfs/pnfs.c
@@ -1865,6 +1865,7 @@ pnfs_write_through_mds(struct nfs_pageio_descriptor *desc,
 		mirror->pg_recoalesce = 1;
 	}
 	nfs_pgio_data_destroy(hdr);
+	hdr->release(hdr);
 }
 
 static enum pnfs_try_status
@@ -1979,6 +1980,7 @@ pnfs_read_through_mds(struct nfs_pageio_descriptor *desc,
 		mirror->pg_recoalesce = 1;
 	}
 	nfs_pgio_data_destroy(hdr);
+	hdr->release(hdr);
 }
 
 /*
