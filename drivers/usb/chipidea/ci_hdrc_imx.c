e765bfb73ff711b1d139a4438af9ecedabb07d34
Peter Chen <peter.chen@freescale.com>
2015-06-18 11:43:34 +0800

    usb: chipidea: imx: enable CI_HDRC_SET_NON_ZERO_TTHA
    For i.mx platform, set ttctrl.ttha with non-zero value only affects
    sitd, and ehci core makes sure the schedule is not full when accepts
    new request, so it will not occur the transaction which will acorss
    the SoF.
    
    Signed-off-by: Peter Chen <peter.chen@freescale.com>

--- drivers/usb/chipidea/ci_hdrc_imx.c
+++ drivers/usb/chipidea/ci_hdrc_imx.c
@@ -126,7 +126,8 @@ static int ci_hdrc_imx_probe(struct platform_device *pdev)
 	struct ci_hdrc_platform_data pdata = {
 		.name		= dev_name(&pdev->dev),
 		.capoffset	= DEF_CAPOFFSET,
-		.flags		= CI_HDRC_DISABLE_STREAMING,
+		.flags		= CI_HDRC_DISABLE_STREAMING |
+					CI_HDRC_SET_NON_ZERO_TTHA,
 	};
 	int ret;
 	const struct of_device_id *of_id =
