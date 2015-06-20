a614a610ac9b28f195d790d25be72d26f345c53a
Thomas Gleixner <tglx@linutronix.de>
2015-06-20 12:05:40 +0200

    genirq: Remove bogus restriction in irq_move_mask_irq()
    If an interrupt is marked with the no balancing flag, we still allow
    setting the affinity for such an interrupt from the kernel itself, but
    for interrupts which move the affinity from interrupt context via
    irq_move_mask_irq() this runs into a check for the no balancing flag,
    which in turn ends up with an endless storm of stack dumps because the
    move pending flag is not reset.
    
    Allow the move for interrupts which have the no balancing flag set and
    clear the move pending bit before checking for interrupts with the per
    cpu flag set.
    
    Reported-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Jiang Liu <jiang.liu@linux.intel.com>
    Link: http://lkml.kernel.org/r/alpine.DEB.2.11.1506201002570.4107@nanos
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/irq/migration.c
+++ kernel/irq/migration.c
@@ -12,15 +12,15 @@ void irq_move_masked_irq(struct irq_data *idata)
 	if (likely(!irqd_is_setaffinity_pending(&desc->irq_data)))
 		return;
 
+	irqd_clr_move_pending(&desc->irq_data);
+
 	/*
 	 * Paranoia: cpu-local interrupts shouldn't be calling in here anyway.
 	 */
-	if (!irqd_can_balance(&desc->irq_data)) {
+	if (irqd_is_per_cpu(&desc->irq_data)) {
 		WARN_ON(1);
 		return;
 	}
-
-	irqd_clr_move_pending(&desc->irq_data);
 
 	if (unlikely(cpumask_empty(desc->pending_mask)))
 		return;
