41bdbd2887591a3fa586e71cd2b462c3525845a3
Wolfram Sang <wsa+renesas@sang-engineering.com>
2015-06-20 21:03:21 +0200

    i2c: sh_mobile: use adapter default for timeout
    5 seconds is a very large timeout, and it is hardcoded. Use the default
    timeout from 'struct adapter' which is 1 second. It can also be modified
    from userspace for specific workloads via i2c-dev.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/busses/i2c-sh_mobile.c
+++ drivers/i2c/busses/i2c-sh_mobile.c
@@ -751,7 +751,7 @@ static int sh_mobile_i2c_xfer(struct i2c_adapter *adapter,
 		/* The interrupt handler takes care of the rest... */
 		k = wait_event_timeout(pd->wait,
 				       pd->sr & (ICSR_TACK | SW_DONE),
-				       5 * HZ);
+				       adapter->timeout);
 		if (!k) {
 			dev_err(pd->dev, "Transfer request timed out\n");
 			if (pd->dma_direction != DMA_NONE)
