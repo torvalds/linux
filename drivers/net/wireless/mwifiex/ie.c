8b2c621c9f6f6240edaf4359c3148068fff8b1c7
John Linville <linville@tuxdriver.com>
2015-06-23 15:04:03 -0400

    mwifiex: do not short circuit exit from mwifiex_set_mgmt_ies
    Without this change, the code simply exits after calling
    mwifiex_uap_set_head_tail_ies, leving the call to
    mwifiex_set_mgmt_beacon_data_ies as dead code.
    
    Coverity CID #1271292
    
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    Acked-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/mwifiex/ie.c
+++ drivers/net/wireless/mwifiex/ie.c
@@ -409,6 +409,8 @@ int mwifiex_set_mgmt_ies(struct mwifiex_private *priv,
 	int ret;
 
 	ret = mwifiex_uap_parse_tail_ies(priv, info);
+
+	if (ret)
 		return ret;
 
 	return mwifiex_set_mgmt_beacon_data_ies(priv, info);
