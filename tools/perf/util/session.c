fe692ac86adbbbbf9745909aafe0c87fd7fc6368
Adrian Hunter <adrian.hunter@intel.com>
2015-06-23 10:52:49 +0300

    perf tools: Print a newline before dumping Aggregated stats
    When dumping events with 'perf report -D' the event print always starts
    with a newline (see dump_event()).
    
    Do the same with the "Aggregated stats" print so that it is not jammed
    up against the last event print.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Link: http://lkml.kernel.org/r/1435045969-15999-2-git-send-email-adrian.hunter@intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/session.c
+++ tools/perf/util/session.c
@@ -1728,7 +1728,7 @@ size_t perf_session__fprintf_nr_events(struct perf_session *session, FILE *fp)
 	if (perf_header__has_feat(&session->header, HEADER_AUXTRACE))
 		msg = " (excludes AUX area (e.g. instruction trace) decoded / synthesized events)";
 
-	ret = fprintf(fp, "Aggregated stats:%s\n", msg);
+	ret = fprintf(fp, "\nAggregated stats:%s\n", msg);
 
 	ret += events_stats__fprintf(&session->evlist->stats, fp);
 	return ret;
