69916d96094e1e16567c5f25515a13ed2896c730
Ray Jui <rjui@broadcom.com>
2015-06-29 14:30:10 -0700

    clk: iproc: fix bit manipulation arithmetic
    A 32-bit variable should be type casted to 64-bit before arithmetic
    operation and assigning it to a 64-bit variable
    
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Ray Jui <rjui@broadcom.com>
    Fixes: 5fe225c105fd ("clk: iproc: add initial common clock support")
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/bcm/clk-iproc-pll.c
+++ drivers/clk/bcm/clk-iproc-pll.c
@@ -366,7 +366,7 @@ static unsigned long iproc_pll_recalc_rate(struct clk_hw *hw,
 	val = readl(pll->pll_base + ctrl->ndiv_int.offset);
 	ndiv_int = (val >> ctrl->ndiv_int.shift) &
 		bit_mask(ctrl->ndiv_int.width);
-	ndiv = ndiv_int << ctrl->ndiv_int.shift;
+	ndiv = (u64)ndiv_int << ctrl->ndiv_int.shift;
 
 	if (ctrl->flags & IPROC_CLK_PLL_HAS_NDIV_FRAC) {
 		val = readl(pll->pll_base + ctrl->ndiv_frac.offset);
@@ -374,7 +374,8 @@ static unsigned long iproc_pll_recalc_rate(struct clk_hw *hw,
 			bit_mask(ctrl->ndiv_frac.width);
 
 		if (ndiv_frac != 0)
-			ndiv = (ndiv_int << ctrl->ndiv_int.shift) | ndiv_frac;
+			ndiv = ((u64)ndiv_int << ctrl->ndiv_int.shift) |
+				ndiv_frac;
 	}
 
 	val = readl(pll->pll_base + ctrl->pdiv.offset);
