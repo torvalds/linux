4b01d9785bb0adb3ddea2624c8411cca0865b772
Michael Turquette <mturquette@baylibre.com>
2015-06-30 12:45:48 +0100

    sched: scheduler-driven cpu frequency selection
    Scheduler-driven CPU frequency selection hopes to exploit both
    per-task and global information in the scheduler to improve frequency
    selection policy, achieving lower power consumption, improved
    responsiveness/performance, and less reliance on heuristics and
    tunables. For further discussion on the motivation of this integration
    see [0].
    
    This patch implements a shim layer between the Linux scheduler and the
    cpufreq subsystem. The interface accepts capacity requests from the
    CFS, RT and deadline sched classes. The requests from each sched class
    are summed on each CPU with a margin applied to the CFS and RT
    capacity requests to provide some headroom. Deadline requests are
    expected to be precise enough given their nature to not require
    headroom. The maximum total capacity request for a CPU in a frequency
    domain drives the requested frequency for that domain.
    
    Policy is determined by both the sched classes and this shim layer.
    
    Note that this algorithm is event-driven. There is no polling loop to
    check cpu idle time nor any other method which is unsynchronized with
    the scheduler, aside from a throttling mechanism to ensure frequency
    changes are not attempted faster than the hardware can accommodate them.
    
    Thanks to Juri Lelli <juri.lelli@arm.com> for contributing design ideas,
    code and test results, and to Ricky Liang <jcliang@chromium.org>
    for initialization and static key inc/dec fixes.
    
    [0] http://article.gmane.org/gmane.linux.kernel/1499836
    
    [smuckle@linaro.org: various additions and fixes, revised commit text]
    
    CC: Ricky Liang <jcliang@chromium.org>
    Signed-off-by: Michael Turquette <mturquette@baylibre.com>
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    Signed-off-by: Steve Muckle <smuckle@linaro.org>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -496,6 +496,9 @@ extern struct cpufreq_governor cpufreq_gov_ondemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
+extern struct cpufreq_governor cpufreq_gov_sched;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
 #endif
 
 /*********************************************************************
