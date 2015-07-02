0f0ff9a9f3fa2ec6f427603fd521d5f3a0b076d1
Theodore Ts'o <tytso@mit.edu>
2015-07-01 23:37:46 -0400

    ext4: fix fencepost error in lazytime optimization
    Commit 8f4d8558391: "ext4: fix lazytime optimization" was not a
    complete fix.  In the case where the inode number is a multiple of 16,
    and we could still end up updating an inode with dirty timestamps
    written to the wrong inode on disk.  Oops.
    
    This can be easily reproduced by using generic/005 with a file system
    with metadata_csum and lazytime enabled.
    
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Cc: stable@vger.kernel.org

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -4348,7 +4348,12 @@ static void ext4_update_other_inodes_time(struct super_block *sb,
 	int inode_size = EXT4_INODE_SIZE(sb);
 
 	oi.orig_ino = orig_ino;
-	ino = (orig_ino & ~(inodes_per_block - 1)) + 1;
+	/*
+	 * Calculate the first inode in the inode table block.  Inode
+	 * numbers are one-based.  That is, the first inode in a block
+	 * (assuming 4k blocks and 256 byte inodes) is (n*16 + 1).
+	 */
+	ino = ((orig_ino - 1) & ~(inodes_per_block - 1)) + 1;
 	for (i = 0; i < inodes_per_block; i++, ino++, buf += inode_size) {
 		if (ino == orig_ino)
 			continue;
