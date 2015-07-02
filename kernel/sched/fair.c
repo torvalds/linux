65b8ddef4ef06f78b29207ef5ec508fb58ad807f
Morten Rasmussen <morten.rasmussen@arm.com>
2015-07-02 17:16:34 +0100

    ANDROID: sched: Prevent unnecessary active balance of single task in sched group
    Scenarios with the busiest group having just one task and the local
    being idle on topologies with sched groups with different numbers of
    cpus manage to dodge all load-balance bailout conditions resulting the
    nr_balance_failed counter to be incremented. This eventually causes a
    pointless active migration of the task. This patch prevents this by not
    incrementing the counter when the busiest group only has one task.
    ASYM_PACKING migrations and migrations due to reduced capacity should
    still take place as these are explicitly captured by
    need_active_balance().
    
    A better solution would be to not attempt the load-balance in the first
    place, but that requires significant changes to the order of bailout
    conditions and statistics gathering.
    
    Change-Id: I28f69c72febe0211decbe77b7bc3e48839d3d7b3
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7112,6 +7112,7 @@ struct lb_env {
 	int			new_dst_cpu;
 	enum cpu_idle_type	idle;
 	long			imbalance;
+	unsigned int		src_grp_nr_running;
 	/* The set of CPUs under consideration for load-balancing */
 	struct cpumask		*cpus;
 
@@ -8294,6 +8295,8 @@ next_group:
 	if (env->sd->flags & SD_NUMA)
 		env->fbq_type = fbq_classify_group(&sds->busiest_stat);
 
+	env->src_grp_nr_running = sds->busiest_stat.sum_nr_running;
+
 	if (!env->sd->parent) {
 		struct root_domain *rd = env->dst_rq->rd;
 
@@ -8988,7 +8991,8 @@ more_balance:
 		 * excessive cache_hot migrations and active balances.
 		 */
 		if (idle != CPU_NEWLY_IDLE)
-			sd->nr_balance_failed++;
+			if (env.src_grp_nr_running > 1)
+				sd->nr_balance_failed++;
 
 		if (need_active_balance(&env)) {
 			unsigned long flags;
