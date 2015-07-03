876f9a348779b348f44736254365bb574fea74eb
Richard Weinberger <richard@nod.at>
2015-07-03 10:36:14 +0200

    UBI: Fastmap: Simplify expression
    There is no need to compute pnum again.
    
    Signed-off-by: Richard Weinberger <richard@nod.at>
    Acked-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/ubi/fastmap.c
+++ drivers/mtd/ubi/fastmap.c
@@ -775,7 +775,7 @@ static int ubi_attach_fastmap(struct ubi_device *ubi,
 		for (j = 0; j < be32_to_cpu(fm_eba->reserved_pebs); j++) {
 			int pnum = be32_to_cpu(fm_eba->pnum[j]);
 
-			if ((int)be32_to_cpu(fm_eba->pnum[j]) < 0)
+			if (pnum < 0)
 				continue;
 
 			aeb = NULL;
