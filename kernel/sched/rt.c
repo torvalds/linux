8fd373548eed815858d7b5534971bdc64b4d8d2c
Xunlei Pang <pang.xunlei@linaro.org>
2015-07-04 15:39:22 +0800

    sched/rt: Remove a redundant condition from task_woken_rt()
    'p' has been already queued at this point, so "!task_running(rq, p)"
    and "p->nr_cpus_allowed > 1" imply that "has_pushable_tasks(rq)" is
    true, so it can be removed.
    
    Signed-off-by: Xunlei Pang <pang.xunlei@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Steven Rostedt <rostedt@goodmis.org>
    Cc: Juri Lelli <juri.lelli@gmail.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1435995563-3723-1-git-send-email-xlpang@126.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -2069,7 +2069,6 @@ static void task_woken_rt(struct rq *rq, struct task_struct *p)
 {
 	if (!task_running(rq, p) &&
 	    !test_tsk_need_resched(rq->curr) &&
-	    has_pushable_tasks(rq) &&
 	    p->nr_cpus_allowed > 1 &&
 	    (dl_task(rq->curr) || rt_task(rq->curr)) &&
 	    (rq->curr->nr_cpus_allowed < 2 ||
