05f5cf60a4696fe63510581f530c8e8f74c9327b
Rom Lemarchand <romlem@android.com>
2015-07-06 16:50:33 -0700

    initramfs: Add skip_initramfs command line option
    Add a skip_initramfs option to allow choosing whether to boot using
    the initramfs or not at runtime.
    
    Change-Id: If30428fa748c1d4d3d7b9d97c1f781de5e4558c3
    Signed-off-by: Rom Lemarchand <romlem@google.com>

--- init/noinitramfs.c
+++ init/noinitramfs.c
@@ -21,11 +21,16 @@
 #include <linux/stat.h>
 #include <linux/kdev_t.h>
 #include <linux/syscalls.h>
+#include <linux/kconfig.h>
+#include <linux/initramfs.h>
 
 /*
  * Create a simple rootfs that is similar to the default initramfs
  */
-static int __init default_rootfs(void)
+#if !IS_BUILTIN(CONFIG_BLK_DEV_INITRD)
+static
+#endif
+int __init default_rootfs(void)
 {
 	int err;
 
@@ -49,4 +54,6 @@ out:
 	printk(KERN_WARNING "Failed to create a rootfs\n");
 	return err;
 }
+#if !IS_BUILTIN(CONFIG_BLK_DEV_INITRD)
 rootfs_initcall(default_rootfs);
+#endif
