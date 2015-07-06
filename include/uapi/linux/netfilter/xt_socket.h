6e2b405681d25dd5352637373659aa1f39211fa8
Amit Pundir <amit.pundir@linaro.org>
2015-07-07 00:28:49 +0530

    ANDROID: netfilter: xt_qtaguid: xt_socket: build fixes
    Add missing header <linux/miscdevice.h> and use
    xt_socket_lookup_slow_v* instead of xt_socket_get*_sk
    in xt_qtaguid.c.
    
    Fix xt_socket_lookup_slow_v* functions in xt_socket.c
    and declare them in xt_socket.h
    
    Change-Id: I55819b2d4ffa82a2be20995c87d28fb5cc77b5ba
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- include/uapi/linux/netfilter/xt_socket.h
+++ include/uapi/linux/netfilter/xt_socket.h
@@ -26,9 +26,9 @@ struct xt_socket_mtinfo3 {
 			   | XT_SOCKET_NOWILDCARD \
 			   | XT_SOCKET_RESTORESKMARK)
 
-struct sock *xt_socket_get4_sk(const struct sk_buff *skb,
-			       struct xt_action_param *par);
-struct sock *xt_socket_get6_sk(const struct sk_buff *skb,
-			       struct xt_action_param *par);
+struct sock *xt_socket_lookup_slow_v4(const struct sk_buff *skb,
+				const struct net_device *indev);
+struct sock *xt_socket_lookup_slow_v6(const struct sk_buff *skb,
+				const struct net_device *indev);
 
 #endif /* _XT_SOCKET_H */
