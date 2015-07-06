79f33269441c847004b5961e13012769a5a6766a
Rom Lemarchand <romlem@android.com>
2015-07-06 16:50:33 -0700

    ANDROID: initramfs: Add skip_initramfs command line option
    Add a skip_initramfs option to allow choosing whether to boot using
    the initramfs or not at runtime.
    
    Change-Id: If30428fa748c1d4d3d7b9d97c1f781de5e4558c3
    Signed-off-by: Rom Lemarchand <romlem@google.com>
    Signed-off-by: Dayao Ji <jdy@rock-chips.com>

--- init/initramfs.c
+++ init/initramfs.c
@@ -10,6 +10,7 @@
 #include <linux/syscalls.h>
 #include <linux/utime.h>
 #include <linux/file.h>
+#include <linux/initramfs.h>
 
 static ssize_t __init xwrite(int fd, const char *p, size_t count)
 {
@@ -615,10 +616,26 @@ static void __init clean_rootfs(void)
 }
 #endif
 
+static int __initdata do_skip_initramfs;
+
+static int __init skip_initramfs_param(char *str)
+{
+	if (*str)
+		return 0;
+	do_skip_initramfs = 1;
+	return 1;
+}
+__setup("skip_initramfs", skip_initramfs_param);
+
 static int __init populate_rootfs(void)
 {
+	char *err;
+
+	if (do_skip_initramfs)
+		return default_rootfs();
+
 	/* Load the built in initramfs */
-	char *err = unpack_to_rootfs(__initramfs_start, __initramfs_size);
+	err = unpack_to_rootfs(__initramfs_start, __initramfs_size);
 	if (err)
 		panic("%s", err); /* Failed to decompress INTERNAL initramfs */
 	/* If available load the bootloader supplied initrd */
