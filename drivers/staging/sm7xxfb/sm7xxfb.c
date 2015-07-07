eb90d44de7e6c7498a21e8de2935123395bbc31b
Sudip Mukherjee <sudipm.mukherjee@gmail.com>
2015-07-07 13:44:35 +0530

    staging: sm7xxfb: use kernel commandline
    We were only using the kernel commandline to set the mode if this driver
    is builtin, but when it is built as a module we were not having any way
    to set the mode. Start using commandline even if it is built as a
    module.
    
    Signed-off-by: Sudip Mukherjee <sudip@vectorindia.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm7xxfb/sm7xxfb.c
+++ drivers/staging/sm7xxfb/sm7xxfb.c
@@ -1660,14 +1660,12 @@ static struct pci_driver smtcfb_driver = {
 
 static int __init sm712fb_init(void)
 {
-#ifndef MODULE
 	char *option = NULL;
 
 	if (fb_get_options("sm712fb", &option))
 		return -ENODEV;
 	if (option && *option)
 		mode_option = option;
-#endif
 	sm7xx_vga_setup(mode_option);
 
 	return pci_register_driver(&smtcfb_driver);
