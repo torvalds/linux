0bf4fb5a1f25649d467869c4fac8d4c927a423f6
Sugar Zhang <sugar.zhang@rock-chips.com>
2015-07-09 16:17:50 +0800

    ASoC: hdmi_spdif: support format SNDRV_PCM_FMTBIT_S24_LE
    because of DMA_SLAVE_BUSWIDTH_4_BYTES, we support SNDRV_PCM_FMTBIT_S24_LE
    which store data in low three bytes. not support SNDRV_PCM_FMTBIT_S24_3LE
    
    Signed-off-by: Sugar Zhang <sugar.zhang@rock-chips.com>

--- sound/soc/codecs/hdmi_spdif.c
+++ sound/soc/codecs/hdmi_spdif.c
@@ -26,7 +26,7 @@
 #define DRV_NAME "spdif-dit"
 
 #define STUB_RATES	SNDRV_PCM_RATE_8000_192000
-#define STUB_FORMATS	SNDRV_PCM_FMTBIT_S16_LE
+#define STUB_FORMATS	(SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S24_LE)
 
 
 static struct snd_soc_codec_driver soc_codec_spdif_dit;
