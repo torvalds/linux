af5554f957f8a7e28b9826a9c4ad2a7dedd15a78
Peter Hurley <peter@hurleysoftware.com>
2015-07-12 20:50:50 -0400

    tty: buffers: Move hidden buffer index advance into outer loop
    The advance of the 'read' buffer index belongs in the outer
    flip buffer consume loop, with the other buffer index arithmetic.
    
    No functional change.
    
    Signed-off-by: Peter Hurley <peter@hurleysoftware.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/tty_buffer.c
+++ drivers/tty/tty_buffer.c
@@ -444,7 +444,6 @@ receive_buf(struct tty_struct *tty, struct tty_buffer *head, int count)
 		if (count)
 			disc->ops->receive_buf(tty, p, f, count);
 	}
-	head->read += count;
 	return count;
 }
 
@@ -506,6 +505,7 @@ static void flush_to_ldisc(struct work_struct *work)
 		count = receive_buf(tty, head, count);
 		if (!count)
 			break;
+		head->read += count;
 	}
 
 	mutex_unlock(&buf->lock);
