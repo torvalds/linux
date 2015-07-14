2b16e01ae6b5c0c5e1f382fcad51a3053270374a
Takashi Iwai <tiwai@suse.de>
2015-07-14 15:19:37 +0200

    ALSA: line6: Fix -EBUSY error during active monitoring
    commit 4d0e677523a999e1dec28e55cc314c47ba09ca12 upstream.
    
    When a monitor stream is active, the next PCM stream access results in
    EBUSY error because of the check in line6_stream_start().  Fix this by
    just skipping the submission of pending URBs when the stream is
    already running instead.
    
    Bugzilla: https://bugzilla.kernel.org/show_bug.cgi?id=101431
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- sound/usb/line6/pcm.c
+++ sound/usb/line6/pcm.c
@@ -186,12 +186,8 @@ static int line6_stream_start(struct snd_line6_pcm *line6pcm, int direction,
 	int ret = 0;
 
 	spin_lock_irqsave(&pstr->lock, flags);
-	if (!test_and_set_bit(type, &pstr->running)) {
-		if (pstr->active_urbs || pstr->unlink_urbs) {
-			ret = -EBUSY;
-			goto error;
-		}
-
+	if (!test_and_set_bit(type, &pstr->running) &&
+	    !(pstr->active_urbs || pstr->unlink_urbs)) {
 		pstr->count = 0;
 		/* Submit all currently available URBs */
 		if (direction == SNDRV_PCM_STREAM_PLAYBACK)
@@ -199,7 +195,6 @@ static int line6_stream_start(struct snd_line6_pcm *line6pcm, int direction,
 		else
 			ret = line6_submit_audio_in_all_urbs(line6pcm);
 	}
- error:
 	if (ret < 0)
 		clear_bit(type, &pstr->running);
 	spin_unlock_irqrestore(&pstr->lock, flags);
