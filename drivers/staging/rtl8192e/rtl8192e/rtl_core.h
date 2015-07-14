1e16cb17d59c9aafe8cc3dfc912050b1c4dddca7
Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
2015-07-14 22:04:13 +0200

    staging: rtl8192e: Remove read|write_nic_io_(d)word|byte()
    None of this functions was used.
    Also remove PlatformIOCheckPageLegalAndGetRegMask() used by them.
    
    Signed-off-by: Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/rtl_core.h
+++ drivers/staging/rtl8192e/rtl8192e/rtl_core.h
@@ -581,13 +581,6 @@ extern const struct ethtool_ops rtl819x_ethtool_ops;
 void rtl8192_tx_cmd(struct net_device *dev, struct sk_buff *skb);
 short rtl8192_tx(struct net_device *dev, struct sk_buff *skb);
 
-u8 read_nic_io_byte(struct net_device *dev, int x);
-u32 read_nic_io_dword(struct net_device *dev, int x);
-u16 read_nic_io_word(struct net_device *dev, int x);
-void write_nic_io_byte(struct net_device *dev, int x, u8 y);
-void write_nic_io_word(struct net_device *dev, int x, u16 y);
-void write_nic_io_dword(struct net_device *dev, int x, u32 y);
-
 u8 read_nic_byte(struct net_device *dev, int x);
 u32 read_nic_dword(struct net_device *dev, int x);
 u16 read_nic_word(struct net_device *dev, int x);
