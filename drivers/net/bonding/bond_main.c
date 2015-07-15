22f94e625635fb8555f5e7361745ea780a3853e4
Nikolay Aleksandrov <nikolay@cumulusnetworks.com>
2015-07-15 16:31:09 +0200

    bonding: trivial: remove unused variables
    Get rid of these:
    drivers/net/bonding//bond_main.c: In function ‘bond_update_slave_arr’:
    drivers/net/bonding//bond_main.c:3754:6: warning: variable
    ‘slaves_in_agg’ set but not used [-Wunused-but-set-variable]
      int slaves_in_agg;
          ^
      CC [M]  drivers/net/bonding//bond_3ad.o
    drivers/net/bonding//bond_3ad.c: In function
    ‘ad_marker_response_received’:
    drivers/net/bonding//bond_3ad.c:1870:61: warning: parameter ‘marker’
    set but not used [-Wunused-but-set-parameter]
     static void ad_marker_response_received(struct bond_marker *marker,
                                                                 ^
    drivers/net/bonding//bond_3ad.c:1871:19: warning: parameter ‘port’ set
    but not used [-Wunused-but-set-parameter]
          struct port *port)
                       ^
    
    Signed-off-by: Nikolay Aleksandrov <nikolay@cumulusnetworks.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -3751,7 +3751,6 @@ int bond_update_slave_arr(struct bonding *bond, struct slave *skipslave)
 	struct slave *slave;
 	struct list_head *iter;
 	struct bond_up_slave *new_arr, *old_arr;
-	int slaves_in_agg;
 	int agg_id = 0;
 	int ret = 0;
 
@@ -3782,7 +3781,6 @@ int bond_update_slave_arr(struct bonding *bond, struct slave *skipslave)
 			}
 			goto out;
 		}
-		slaves_in_agg = ad_info.ports;
 		agg_id = ad_info.aggregator_id;
 	}
 	bond_for_each_slave(bond, slave, iter) {
