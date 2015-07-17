3c765399524308ab36777a443ce77e19810a97d7
Bjorn Helgaas <bhelgaas@google.com>
2015-07-17 15:30:26 -0500

    PCI: Rationalize pci_ats_queue_depth() error checking
    We previously returned -ENODEV for devices that don't support ATS (except
    that we always returned 0 for VFs, whether or not they support ATS).
    
    For consistency, always return -EINVAL (not -ENODEV) if the device doesn't
    support ATS.  Return zero for VFs that support ATS.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Joerg Roedel <jroedel@suse.de>

--- drivers/pci/ats.c
+++ drivers/pci/ats.c
@@ -136,13 +136,13 @@ EXPORT_SYMBOL_GPL(pci_restore_ats_state);
  */
 int pci_ats_queue_depth(struct pci_dev *dev)
 {
+	if (!dev->ats_cap)
+		return -EINVAL;
+
 	if (dev->is_virtfn)
 		return 0;
 
-	if (dev->ats_cap)
-		return dev->ats_qdep;
-
-	return -ENODEV;
+	return dev->ats_qdep;
 }
 EXPORT_SYMBOL_GPL(pci_ats_queue_depth);
 
