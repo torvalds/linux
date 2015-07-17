091571d071a435a517aec42c4f8c56883c5dc531
akpm@linux-foundation.org <akpm@linux-foundation.org>
2015-07-17 15:43:46 -0700

    ASoC: cs4349: include gpio/consumer.h
    s390 allmodconfig:
    
    sound/soc/codecs/cs4349.c: In function 'cs4349_i2c_probe':
    sound/soc/codecs/cs4349.c:300: error: implicit declaration of function 'devm_gpiod_get_optional'
    sound/soc/codecs/cs4349.c:301: error: 'GPIOD_OUT_LOW' undeclared (first use in this function)
    sound/soc/codecs/cs4349.c:301: error: (Each undeclared identifier is reported only once
    sound/soc/codecs/cs4349.c:301: error: for each function it appears in.)
    sound/soc/codecs/cs4349.c:306: error: implicit declaration of function 'gpiod_set_value_cansleep'
    
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/cs4349.c
+++ sound/soc/codecs/cs4349.c
@@ -16,6 +16,7 @@
 #include <linux/init.h>
 #include <linux/delay.h>
 #include <linux/gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/platform_device.h>
 #include <linux/pm.h>
 #include <linux/i2c.h>
