dedae86d4da6c88ec2105e0bd038acc57c203bca
Lars-Peter Clausen <lars@metafoo.de>
2015-07-19 12:15:18 +0200

    ASoC: cs4349: Don't use rtd->codec
    rtd->codec does not necessarily point to the CODEC instance for which the
    callback was called (e.g. for CODEC<->CODEC or multi-CODEC links). Use
    dai->codec instead.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/cs4349.c
+++ sound/soc/codecs/cs4349.c
@@ -94,8 +94,7 @@ static int cs4349_pcm_hw_params(struct snd_pcm_substream *substream,
 			    struct snd_pcm_hw_params *params,
 			    struct snd_soc_dai *dai)
 {
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_codec *codec = rtd->codec;
+	struct snd_soc_codec *codec = dai->codec;
 	struct cs4349_private *cs4349 = snd_soc_codec_get_drvdata(codec);
 	int mode, fmt, ret;
 
