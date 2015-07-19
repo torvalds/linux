9a2477c2cabddf6fa52eda49502017191b34acb6
Kolbeinn Karlsson <kk752@cornell.edu>
2015-07-19 16:23:19 -0400

    staging: lustre: make functions only used locally static
    Add a static modifier to two functions that have no
    separate declaration and are only used within the file they are
    defined in. This problem was reported by sparse.
    
    Signed-off-by: Kolbeinn Karlsson <kk752@cornell.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ptlrpc/events.c
+++ drivers/staging/lustre/lustre/ptlrpc/events.c
@@ -485,7 +485,7 @@ int ptlrpc_uuid_to_peer(struct obd_uuid *uuid,
 	return rc;
 }
 
-void ptlrpc_ni_fini(void)
+static void ptlrpc_ni_fini(void)
 {
 	wait_queue_head_t waitq;
 	struct l_wait_info lwi;
@@ -529,7 +529,7 @@ lnet_pid_t ptl_get_pid(void)
 	return pid;
 }
 
-int ptlrpc_ni_init(void)
+static int ptlrpc_ni_init(void)
 {
 	int rc;
 	lnet_pid_t pid;
