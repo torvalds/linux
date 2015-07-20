6ccfe64c770139675a080ee5029ded7d89d9ea0d
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2015-07-20 09:56:18 -0700

    Input: elan_i2c - enable ELAN0600 acpi panels
    ELAN0600 seems to work just fine in mouse emulation mode through i2c-hid,
    but to have full raw touch support we need to register it in elan_i2c.ko
    
    Reported-and-tested-by: Alessio Treglia <alessio@debian.org>
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Acked-by: Jiri Kosina <jkosina@suse.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/mouse/elan_i2c_core.c
+++ drivers/input/mouse/elan_i2c_core.c
@@ -1167,6 +1167,7 @@ MODULE_DEVICE_TABLE(i2c, elan_id);
 #ifdef CONFIG_ACPI
 static const struct acpi_device_id elan_acpi_id[] = {
 	{ "ELAN0000", 0 },
+	{ "ELAN0600", 0 },
 	{ }
 };
 MODULE_DEVICE_TABLE(acpi, elan_acpi_id);
