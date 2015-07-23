24b4d2abd0bd628f396dada3e915d395cbf459eb
Alex Gartrell <agartrell@fb.com>
2015-07-23 14:24:40 -0700

    ebpf: Allow dereferences of PTR_TO_STACK registers
    mov %rsp, %r1           ; r1 = rsp
            add $-8, %r1            ; r1 = rsp - 8
            store_q $123, -8(%rsp)  ; *(u64*)r1 = 123  <- valid
            store_q $123, (%r1)     ; *(u64*)r1 = 123  <- previously invalid
            mov $0, %r0
            exit                    ; Always need to exit
    
    And we'd get the following error:
    
    	0: (bf) r1 = r10
    	1: (07) r1 += -8
    	2: (7a) *(u64 *)(r10 -8) = 999
    	3: (7a) *(u64 *)(r1 +0) = 999
    	R1 invalid mem access 'fp'
    
    	Unable to load program
    
    We already know that a register is a stack address and the appropriate
    offset, so we should be able to validate those references as well.
    
    Signed-off-by: Alex Gartrell <agartrell@fb.com>
    Acked-by: Alexei Starovoitov <ast@plumgrid.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -648,6 +648,9 @@ static int check_mem_access(struct verifier_env *env, u32 regno, int off,
 	struct verifier_state *state = &env->cur_state;
 	int size, err = 0;
 
+	if (state->regs[regno].type == PTR_TO_STACK)
+		off += state->regs[regno].imm;
+
 	size = bpf_size_to_bytes(bpf_size);
 	if (size < 0)
 		return size;
@@ -667,7 +670,8 @@ static int check_mem_access(struct verifier_env *env, u32 regno, int off,
 		if (!err && t == BPF_READ && value_regno >= 0)
 			mark_reg_unknown_value(state->regs, value_regno);
 
-	} else if (state->regs[regno].type == FRAME_PTR) {
+	} else if (state->regs[regno].type == FRAME_PTR ||
+		   state->regs[regno].type == PTR_TO_STACK) {
 		if (off >= 0 || off < -MAX_BPF_STACK) {
 			verbose("invalid stack off=%d size=%d\n", off, size);
 			return -EACCES;
