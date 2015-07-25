f8163c8673efaabb361281a2759b3167d181ecf6
Axel Lin <axel.lin@ingics.com>
2015-07-25 10:23:40 +0800

    ASoC: rt5677: Return error if devm_gpiod_get_optional return ERR_PTR
    If devm_gpiod_get_optional() return ERR_PTR, it means something wrong
    so request gpio fails. We had better return error in such case.
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/rt5677.c
+++ sound/soc/codecs/rt5677.c
@@ -5133,14 +5133,14 @@ static int rt5677_i2c_probe(struct i2c_client *i2c,
 	if (IS_ERR(rt5677->pow_ldo2)) {
 		ret = PTR_ERR(rt5677->pow_ldo2);
 		dev_err(&i2c->dev, "Failed to request POW_LDO2: %d\n", ret);
-		rt5677->pow_ldo2 = 0;
+		return ret;
 	}
 	rt5677->reset_pin = devm_gpiod_get_optional(&i2c->dev,
 			"realtek,reset", GPIOD_OUT_HIGH);
 	if (IS_ERR(rt5677->reset_pin)) {
 		ret = PTR_ERR(rt5677->reset_pin);
 		dev_err(&i2c->dev, "Failed to request RESET: %d\n", ret);
-		rt5677->reset_pin = 0;
+		return ret;
 	}
 
 	if (rt5677->pow_ldo2 || rt5677->reset_pin) {
