48f8bd57756573b08520629a2413227fcdf058f5
Vladimir Murzin <Vladimir.Murzin@arm.com>
2015-07-28 10:42:23 +0100

    arm64: KVM: remove remaining reference to vgic_sr_vectors
    Since commit 8a14849 (arm64: KVM: Switch vgic save/restore to
    alternative_insn) vgic_sr_vectors is not used anymore, so remove
    remaining leftovers and kill the structure.
    
    Signed-off-by: Vladimir Murzin <vladimir.murzin@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -246,11 +246,6 @@ static inline void __cpu_init_hyp_mode(phys_addr_t boot_pgd_ptr,
 		     hyp_stack_ptr, vector_ptr);
 }
 
-struct vgic_sr_vectors {
-	void	*save_vgic;
-	void	*restore_vgic;
-};
-
 static inline void kvm_arch_hardware_disable(void) {}
 static inline void kvm_arch_hardware_unsetup(void) {}
 static inline void kvm_arch_sync_events(struct kvm *kvm) {}
