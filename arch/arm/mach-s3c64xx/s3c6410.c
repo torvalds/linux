fe31d04416e8a019a4ce8dca49aef80f10397aaf
Kukjin Kim <kgene@kernel.org>
2015-07-30 02:00:47 +0900

    ARM: SAMSUNG: local onenand-core header in mach-s3c64xx
    This patch moves onenand-core header file into mach-s3c64xx.
    Because it is not used for others except mach-s3c64xx.
    
    Cc: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Kukjin Kim <kgene@kernel.org>

--- arch/arm/mach-s3c64xx/s3c6410.c
+++ arch/arm/mach-s3c64xx/s3c6410.c
@@ -43,10 +43,10 @@
 #include <plat/sdhci.h>
 #include <plat/adc-core.h>
 #include <plat/iic-core.h>
-#include <plat/onenand-core.h>
 
 #include "ata-core.h"
 #include "common.h"
+#include "onenand-core.h"
 
 void __init s3c6410_map_io(void)
 {
