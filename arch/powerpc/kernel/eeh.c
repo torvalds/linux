39bfd715b4837433a86c168c610880e9ae2185ae
Gavin Shan <gwshan@linux.vnet.ibm.com>
2015-07-30 09:26:51 +1000

    powerpc/eeh: Disable automatically blocked PCI config
    pcibios_set_pcie_reset_state() could be called to complete
    reset request when passing through PCI device, flag
    EEH_PE_ISOLATED is set before saving the PCI config sapce.
    On some Broadcom adapters, EEH_PE_CFG_BLOCKED is automatically
    set when the flag EEH_PE_ISOLATED is marked. It caused bogus
    data saved from the PCI config space, which will be restored
    to the PCI adapter after the reset. Eventually, the hardware
    can't work with corrupted data in PCI config space.
    
    The patch fixes the issue with eeh_pe_state_mark_no_cfg(), which
    doesn't set EEH_PE_CFG_BLOCKED when seeing EEH_PE_ISOLATED on the
    PE, in order to avoid the bogus data saved and restored to the PCI
    config space.
    
    Reported-by: Rajanikanth H. Adaveeshaiah <rajanikanth.ha@in.ibm.com>
    Signed-off-by: Gavin Shan <gwshan@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/kernel/eeh.c
+++ arch/powerpc/kernel/eeh.c
@@ -750,14 +750,14 @@ int pcibios_set_pcie_reset_state(struct pci_dev *dev, enum pcie_reset_state stat
 		eeh_pe_state_clear(pe, EEH_PE_ISOLATED);
 		break;
 	case pcie_hot_reset:
-		eeh_pe_state_mark(pe, EEH_PE_ISOLATED);
+		eeh_pe_state_mark_with_cfg(pe, EEH_PE_ISOLATED);
 		eeh_ops->set_option(pe, EEH_OPT_FREEZE_PE);
 		eeh_pe_dev_traverse(pe, eeh_disable_and_save_dev_state, dev);
 		eeh_pe_state_mark(pe, EEH_PE_CFG_BLOCKED);
 		eeh_ops->reset(pe, EEH_RESET_HOT);
 		break;
 	case pcie_warm_reset:
-		eeh_pe_state_mark(pe, EEH_PE_ISOLATED);
+		eeh_pe_state_mark_with_cfg(pe, EEH_PE_ISOLATED);
 		eeh_ops->set_option(pe, EEH_OPT_FREEZE_PE);
 		eeh_pe_dev_traverse(pe, eeh_disable_and_save_dev_state, dev);
 		eeh_pe_state_mark(pe, EEH_PE_CFG_BLOCKED);
