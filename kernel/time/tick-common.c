d74892c5b291c0010295d26d6b1e11cd70451722
Luiz Capitulino <lcapitulino@redhat.com>
2015-07-29 15:14:17 -0400

    clockevents: Drop redundant cpumask check in tick_check_new_device()
    The same check is performed by tick_check_percpu().
    
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>
    Link: http://lkml.kernel.org/r/20150729151417.069d1bb0@redhat.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/time/tick-common.c
+++ kernel/time/tick-common.c
@@ -304,9 +304,6 @@ void tick_check_new_device(struct clock_event_device *newdev)
 	int cpu;
 
 	cpu = smp_processor_id();
-	if (!cpumask_test_cpu(cpu, newdev->cpumask))
-		goto out_bc;
-
 	td = &per_cpu(tick_cpu_device, cpu);
 	curdev = td->evtdev;
 
