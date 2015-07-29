c847fe8895fded71d0b9a0ba58d3828320fd1824
Paolo Bonzini <pbonzini@redhat.com>
2015-07-29 11:06:34 +0200

    KVM: x86: remove unnecessary memory barriers for shared MSRs
    There is no smp_rmb matching the smp_wmb.  shared_msr_update is called from
    hardware_enable, which in turn is called via on_each_cpu.  on_each_cpu
    and must imply a read memory barrier (on x86 the rmb is achieved simply
    through asm volatile in native_apic_mem_write).
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -222,11 +222,9 @@ static void shared_msr_update(unsigned slot, u32 msr)
 void kvm_define_shared_msr(unsigned slot, u32 msr)
 {
 	BUG_ON(slot >= KVM_NR_SHARED_MSRS);
+	shared_msrs_global.msrs[slot] = msr;
 	if (slot >= shared_msrs_global.nr)
 		shared_msrs_global.nr = slot + 1;
-	shared_msrs_global.msrs[slot] = msr;
-	/* we need ensured the shared_msr_global have been updated */
-	smp_wmb();
 }
 EXPORT_SYMBOL_GPL(kvm_define_shared_msr);
 
