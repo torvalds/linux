be26849bfb6fcee4123c687fc39bd6da1b3be328
Tom Herbert <tom@herbertland.com>
2015-07-31 16:52:13 -0700

    ipv6: Disable flowlabel state ranges by default
    Per RFC6437 stateful flow labels (e.g. labels set by flow label manager)
    cannot "disturb" nodes taking part in stateless flow labels. While the
    ranges only reduce the flow label entropy by one bit, it is conceivable
    that this might bias the algorithm on some routers causing a load
    imbalance. For best results on the Internet we really need the full
    20 bits.
    
    Signed-off-by: Tom Herbert <tom@herbertland.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/af_inet6.c
+++ net/ipv6/af_inet6.c
@@ -771,7 +771,7 @@ static int __net_init inet6_net_init(struct net *net)
 	net->ipv6.sysctl.auto_flowlabels = IP6_DEFAULT_AUTO_FLOW_LABELS;
 	net->ipv6.sysctl.idgen_retries = 3;
 	net->ipv6.sysctl.idgen_delay = 1 * HZ;
-	net->ipv6.sysctl.flowlabel_state_ranges = 1;
+	net->ipv6.sysctl.flowlabel_state_ranges = 0;
 	atomic_set(&net->ipv6.fib6_sernum, 1);
 
 	err = ipv6_init_mibs(net);
