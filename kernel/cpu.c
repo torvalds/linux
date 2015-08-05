32145c4677d2c46b9d877a33ae82c6fcacd002f9
Vitaly Kuznetsov <vkuznets@redhat.com>
2015-08-05 00:52:47 -0700

    cpu-hotplug: export cpu_hotplug_enable/cpu_hotplug_disable
    Hyper-V module needs to disable cpu hotplug (offlining) as there is no
    support from hypervisor side to reassign already opened event channels
    to a different CPU. Currently it is been done by altering
    smp_ops.cpu_disable but it is hackish.
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -194,6 +194,7 @@ void cpu_hotplug_disable(void)
 	cpu_hotplug_disabled++;
 	cpu_maps_update_done();
 }
+EXPORT_SYMBOL_GPL(cpu_hotplug_disable);
 
 void cpu_hotplug_enable(void)
 {
@@ -201,7 +202,7 @@ void cpu_hotplug_enable(void)
 	WARN_ON(--cpu_hotplug_disabled < 0);
 	cpu_maps_update_done();
 }
-
+EXPORT_SYMBOL_GPL(cpu_hotplug_enable);
 #endif	/* CONFIG_HOTPLUG_CPU */
 
 /* Need to know about CPUs going up/down? */
