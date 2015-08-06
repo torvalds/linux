cb9d61e5304e2880180e4ccf685a298c868094e0
Aniket Nagarnaik <aniketn@marvell.com>
2015-08-06 00:39:16 -0700

    mwifiex: claim sdio bus while downloading the firmware
    Our 8887 A2 chip can have separate firmware images for
    bluetooth and WLAN. We observed an issue during parallel
    downloading of these images by btmrvl and mwifiex drivers.
    
    This patch claims sdio bus before starting of the firmware
    download in mwifiex and releases it after completion to
    fix the problem.
    
    Signed-off-by: Aniket Nagarnaik <aniketn@marvell.com>
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/mwifiex/sdio.c
+++ drivers/net/wireless/mwifiex/sdio.c
@@ -920,6 +920,8 @@ static int mwifiex_prog_fw_w_helper(struct mwifiex_adapter *adapter,
 	if (!fwbuf)
 		return -ENOMEM;
 
+	sdio_claim_host(card->func);
+
 	/* Perform firmware data transfer */
 	do {
 		/* The host polls for the DN_LD_CARD_RDY and CARD_IO_READY
@@ -1023,6 +1025,8 @@ static int mwifiex_prog_fw_w_helper(struct mwifiex_adapter *adapter,
 
 		offset += txlen;
 	} while (true);
+
+	sdio_release_host(card->func);
 
 	mwifiex_dbg(adapter, MSG,
 		    "info: FW download over, size %d bytes\n", offset);
