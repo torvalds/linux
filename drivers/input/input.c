00159f19a5057cb779146afce1cceede692af346
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2015-08-06 19:15:30 -0700

    Input: do not emit unneeded EV_SYN when suspending
    Do not emit EV_SYN/SYN_REPORT on suspend if there were no keys that are
    still pressed as we are suspending the device (and in all other cases when
    input core is forcibly releasing keys via input_dev_release_keys() call).
    
    Reviewed-by: Benson Leung <bleung@chromium.org>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -674,13 +674,19 @@ EXPORT_SYMBOL(input_close_device);
  */
 static void input_dev_release_keys(struct input_dev *dev)
 {
+	bool need_sync = false;
 	int code;
 
 	if (is_event_supported(EV_KEY, dev->evbit, EV_MAX)) {
-		for_each_set_bit(code, dev->key, KEY_CNT)
+		for_each_set_bit(code, dev->key, KEY_CNT) {
 			input_pass_event(dev, EV_KEY, code, 0);
+			need_sync = true;
+		}
+
+		if (need_sync)
+			input_pass_event(dev, EV_SYN, SYN_REPORT, 1);
+
 		memset(dev->key, 0, sizeof(dev->key));
-		input_pass_event(dev, EV_SYN, SYN_REPORT, 1);
 	}
 }
 
