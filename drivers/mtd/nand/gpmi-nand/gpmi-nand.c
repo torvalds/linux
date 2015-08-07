00ca888481c436e1bebe4dd57af993363b8907ad
Han Xu <b45815@freescale.com>
2015-08-07 10:37:50 -0500

    MLK-11336: mtd: nand: enable LPSR for GPMI NAND
    The LPSR turns off the power for IOMUX when suspending so restore the
    IOMUX when resuming in GPMI NAND driver.
    
    The function was not tested yet since NAND only supported on 19x19
    LPDDR board.
    
    Signed-off-by: Han Xu <b45815@freescale.com>
    Signed-off-by: Fugang Duan <B38611@freescale.com>
    (cherry picked from commit: b0375f42a27044667082e53449e534b265d7a029)

--- drivers/mtd/nand/gpmi-nand/gpmi-nand.c
+++ drivers/mtd/nand/gpmi-nand/gpmi-nand.c
@@ -2117,6 +2117,7 @@ static int gpmi_pm_suspend(struct device *dev)
 	struct gpmi_nand_data *this = dev_get_drvdata(dev);
 
 	release_dma_channels(this);
+	pinctrl_pm_select_sleep_state(dev);
 	return 0;
 }
 
@@ -2124,6 +2125,8 @@ static int gpmi_pm_resume(struct device *dev)
 {
 	struct gpmi_nand_data *this = dev_get_drvdata(dev);
 	int ret;
+
+	pinctrl_pm_select_default_state(dev);
 
 	ret = acquire_dma_channels(this);
 	if (ret < 0)
