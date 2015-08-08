bbd80cc166dc841f6bab7fe721da157c6371e84b
Viresh Kumar <viresh.kumar@linaro.org>
2015-08-08 10:25:36 +0530

    greybus: i2c: Use (already defined) major/minor macros
    We already have macros for these, use them instead of writing fixed
    values.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/i2c.c
+++ drivers/staging/greybus/i2c.c
@@ -349,8 +349,8 @@ static void gb_i2c_connection_exit(struct gb_connection *connection)
 static struct gb_protocol i2c_protocol = {
 	.name			= "i2c",
 	.id			= GREYBUS_PROTOCOL_I2C,
-	.major			= 0,
-	.minor			= 1,
+	.major			= GB_I2C_VERSION_MAJOR,
+	.minor			= GB_I2C_VERSION_MINOR,
 	.connection_init	= gb_i2c_connection_init,
 	.connection_exit	= gb_i2c_connection_exit,
 	.request_recv		= NULL,	/* no incoming requests */
