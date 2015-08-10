1955f213a68323f7348fc06461017c7675efe6c1
Alexandre Belloni <alexandre.belloni@free-electrons.com>
2015-08-10 16:33:39 +0200

    rtc: at91sam9: include linux/of.h
    This driver is using device tree but is not including of.h
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>

--- drivers/rtc/rtc-at91sam9.c
+++ drivers/rtc/rtc-at91sam9.c
@@ -18,6 +18,7 @@
 #include <linux/kernel.h>
 #include <linux/mfd/syscon.h>
 #include <linux/module.h>
+#include <linux/of.h>
 #include <linux/platform_data/atmel.h>
 #include <linux/platform_device.h>
 #include <linux/regmap.h>
