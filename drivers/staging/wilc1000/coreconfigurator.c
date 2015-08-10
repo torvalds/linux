85b509f18a7f27d6cc296d62e96e4632c83161fd
Chaehyun Lim <chaehyun.lim@gmail.com>
2015-08-10 11:33:15 +0900

    staging: wilc1000: Use strncpy instead of WILC_strncpy
    Use strncpy instead of WILC_strncpy that is a custom function
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/coreconfigurator.c
+++ drivers/staging/wilc1000/coreconfigurator.c
@@ -1577,7 +1577,7 @@ s32 further_process_response(u8 *resp,
 	case WID_ADR:
 		create_mac_addr(cfg_str, resp + idx);
 
-		WILC_strncpy(pstrWIDresult->ps8WidVal, cfg_str, strlen(cfg_str));
+		strncpy(pstrWIDresult->ps8WidVal, cfg_str, strlen(cfg_str));
 		pstrWIDresult->ps8WidVal[strlen(cfg_str)] = '\0';
 		break;
 
