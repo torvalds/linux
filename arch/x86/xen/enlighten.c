3375d8284dfb7866f261ec008d15d30999ff273b
Boris Ostrovsky <boris.ostrovsky@oracle.com>
2015-08-10 16:34:38 -0400

    xen/x86: Don't try to set PCE bit in CR4
    Since VPMU code emulates RDPMC instruction with RDMSR and because hypervisor
    does not emulate it there is no reason to try setting CR4's PCE bit (and the
    hypervisor will warn on seeing it set).
    
    Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Signed-off-by: David Vrabel <david.vrabel@citrix.com>

--- arch/x86/xen/enlighten.c
+++ arch/x86/xen/enlighten.c
@@ -1011,8 +1011,7 @@ static void xen_write_cr0(unsigned long cr0)
 
 static void xen_write_cr4(unsigned long cr4)
 {
-	cr4 &= ~X86_CR4_PGE;
-	cr4 &= ~X86_CR4_PSE;
+	cr4 &= ~(X86_CR4_PGE | X86_CR4_PSE | X86_CR4_PCE);
 
 	native_write_cr4(cr4);
 }
