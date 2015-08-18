98d25afa970d134024d8652360569e3bd74782b3
Dan Carpenter <dan.carpenter@oracle.com>
2015-08-18 12:22:10 +0300

    IB/core: missing curly braces in ib_find_gid()
    Smatch says that, based on the indenting, we should probably add curly
    braces here.
    
    Fixes: 03db3a2d81e6 ('IB/core: Add RoCE GID table management')
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/core/device.c
+++ drivers/infiniband/core/device.c
@@ -835,9 +835,10 @@ int ib_find_gid(struct ib_device *device, union ib_gid *gid,
 	for (port = rdma_start_port(device); port <= rdma_end_port(device); ++port) {
 		if (rdma_cap_roce_gid_table(device, port)) {
 			if (!ib_cache_gid_find_by_port(device, gid, port,
-						       NULL, index))
+						       NULL, index)) {
 				*port_num = port;
 				return 0;
+			}
 		}
 
 		for (i = 0; i < device->port_immutable[port].gid_tbl_len; ++i) {
