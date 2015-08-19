8f183751a8604be5aaf0ad6dedac4890bb6fa0d5
Stephan Mueller <smueller@chronox.de>
2015-08-19 08:42:07 +0200

    crypto: cmac - allow usage in FIPS mode
    CMAC is an approved cipher in FIPS 140-2. The patch allows the use
    of CMAC with TDES and AES in FIPS mode.
    
    Signed-off-by: Stephan Mueller <smueller@chronox.de>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -2476,6 +2476,7 @@ static const struct alg_test_desc alg_test_descs[] = {
 		}
 	}, {
 		.alg = "cmac(aes)",
+		.fips_allowed = 1,
 		.test = alg_test_hash,
 		.suite = {
 			.hash = {
@@ -2485,6 +2486,7 @@ static const struct alg_test_desc alg_test_descs[] = {
 		}
 	}, {
 		.alg = "cmac(des3_ede)",
+		.fips_allowed = 1,
 		.test = alg_test_hash,
 		.suite = {
 			.hash = {
