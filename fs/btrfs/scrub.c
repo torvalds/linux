4734b7ed79e1230602927a38bc4d04dbe9dd09c1
Zhao Lei <zhaolei@cn.fujitsu.com>
2015-08-19 22:39:18 +0800

    btrfs: scrub: setup all fields for sblock_to_check
    scrub_setup_recheck_block() isn't setup all necessary fields for
    sblock_to_check because history reason.
    
    So current code need more arguments in severial functions,
    and more local variables, just to passing these lacked values to
    necessary place.
    
    This patch setup above fields to sblock_to_check in
    scrub_setup_recheck_block(), for:
    1: more cleanup for function arg, local variable
    2: to make sblock_to_check complete, then we can use sblock_to_check
       without concern about some uninitialized member.
    
    Signed-off-by: Zhao Lei <zhaolei@cn.fujitsu.com>
    Signed-off-by: Chris Mason <clm@fb.com>

--- fs/btrfs/scrub.c
+++ fs/btrfs/scrub.c
@@ -1318,6 +1318,9 @@ static int scrub_setup_recheck_block(struct scrub_block *original_sblock,
 	struct btrfs_fs_info *fs_info = sctx->dev_root->fs_info;
 	u64 length = original_sblock->page_count * PAGE_SIZE;
 	u64 logical = original_sblock->pagev[0]->logical;
+	u64 generation = original_sblock->pagev[0]->generation;
+	u64 flags = original_sblock->pagev[0]->flags;
+	u64 have_csum = original_sblock->pagev[0]->have_csum;
 	struct scrub_recover *recover;
 	struct btrfs_bio *bbio;
 	u64 sublen;
@@ -1372,6 +1375,7 @@ static int scrub_setup_recheck_block(struct scrub_block *original_sblock,
 
 			sblock = sblocks_for_recheck + mirror_index;
 			sblock->sctx = sctx;
+
 			page = kzalloc(sizeof(*page), GFP_NOFS);
 			if (!page) {
 leave_nomem:
@@ -1383,7 +1387,15 @@ leave_nomem:
 			}
 			scrub_page_get(page);
 			sblock->pagev[page_index] = page;
+			page->sblock = sblock;
+			page->flags = flags;
+			page->generation = generation;
 			page->logical = logical;
+			page->have_csum = have_csum;
+			if (have_csum)
+				memcpy(page->csum,
+				       original_sblock->pagev[0]->csum,
+				       sctx->csum_size);
 
 			scrub_stripe_index_and_offset(logical,
 						      bbio->map_type,
