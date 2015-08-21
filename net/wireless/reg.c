8de1c63ba1ccfa8225505e60b405537c2c72673c
Johannes Berg <johannes.berg@intel.com>
2015-08-21 14:13:06 +0200

    wireless: make __freq_reg_info static
    As pointed out by sparse, this symbol should be static, make it so.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/wireless/reg.c
+++ net/wireless/reg.c
@@ -1040,8 +1040,8 @@ freq_reg_info_regd(struct wiphy *wiphy, u32 center_freq,
 	return ERR_PTR(-EINVAL);
 }
 
-const struct ieee80211_reg_rule *__freq_reg_info(struct wiphy *wiphy,
-						 u32 center_freq, u32 min_bw)
+static const struct ieee80211_reg_rule *
+__freq_reg_info(struct wiphy *wiphy, u32 center_freq, u32 min_bw)
 {
 	const struct ieee80211_regdomain *regd = reg_get_regdomain(wiphy);
 	const struct ieee80211_reg_rule *reg_rule = NULL;
