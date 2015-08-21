e000f4e8d5ffaff5915c84e031536a8bc5a03eed
Banajit Goswami <bgoswami@codeaurora.org>
2015-08-20 18:52:39 -0700

    ANDROID: GKI: ASoC: soc-core: export function to find components
    Drivers may need to use the ASoC core function to
    find out whether a particular component is already
    registered with ALSA core or not.
    Export the function so that drivers can use it outside
    of the file.
    
    Bug: 151372815
    Test: build
    Change-Id: I13e4a053de085974b0b53c392a9453e46f1aa66d
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>
    (cherry picked from commit fbcf71fe2dadcfbfe6b60f49534dd0312fd3b9d6)
    Signed-off-by: Hridya Valsaraju <hridya@google.com>

--- include/sound/soc.h
+++ include/sound/soc.h
@@ -1354,6 +1354,8 @@ int snd_soc_component_update_bits_async(struct snd_soc_component *component,
 void snd_soc_component_async_complete(struct snd_soc_component *component);
 int snd_soc_component_test_bits(struct snd_soc_component *component,
 	unsigned int reg, unsigned int mask, unsigned int value);
+struct snd_soc_component *soc_find_component(
+	const struct device_node *of_node, const char *name);
 
 /* component wide operations */
 int snd_soc_component_set_sysclk(struct snd_soc_component *component,
