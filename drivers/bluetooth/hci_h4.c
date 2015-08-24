4da2289bb7f0e53cfa4ef1b8899e4f1dafd19e2d
Loic Poulain <loic.poulain@intel.com>
2015-08-24 18:57:57 +0200

    Bluetooth: hci_uart: Fix zero len data packet reception issue
    Packets with a variable length value equal to zero were not received.
    
    Since no more data expected (and input buffer entirely consumed), we
    need to complete/forward the packet immediately instead of waiting for
    more data.
    
    Signed-off-by: Loic Poulain <loic.poulain@intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Fugang Duan <B38611@freescale.com>

--- drivers/bluetooth/hci_h4.c
+++ drivers/bluetooth/hci_h4.c
@@ -223,8 +223,7 @@ struct sk_buff *h4_recv_buf(struct hci_dev *hdev, struct sk_buff *skb,
 			switch ((&pkts[i])->lsize) {
 			case 0:
 				/* No variable data length */
-				(&pkts[i])->recv(hdev, skb);
-				skb = NULL;
+				dlen = 0;
 				break;
 			case 1:
 				/* Single octet variable length */
@@ -251,6 +250,12 @@ struct sk_buff *h4_recv_buf(struct hci_dev *hdev, struct sk_buff *skb,
 				/* Unsupported variable length */
 				kfree_skb(skb);
 				return ERR_PTR(-EILSEQ);
+			}
+
+			if (!dlen) {
+				/* No more data, complete frame */
+				(&pkts[i])->recv(hdev, skb);
+				skb = NULL;
 			}
 		} else {
 			/* Complete frame */
