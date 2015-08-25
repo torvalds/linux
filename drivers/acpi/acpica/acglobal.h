1bdc63bf05096afac4b1e5361417fb730d77dfc7
Lv Zheng <lv.zheng@intel.com>
2015-08-25 10:30:27 +0800

    ACPICA: Disassembler: Cleanup acpi_gbl_db_opt_disasm
    ACPICA commit 969989cf7f85e2a2a0cd048cd25fc706246a48a2
    
    This patch cleans up the following global variable - acpi_gbl_db_opt_disasm:
    The setting is used to control the full disassembly feature for iasl. ACPI
    debugger (acpiexec) shall have nothing to do with it. Actually, acpiexec
    never links to ad_aml_disassemble().
    
    This patch thus renames this global option to acpi_gbl_dm_opt_disasm and
    removes all acpiexec and debugger references on it. Lv Zheng.
    
    This patch doesn't affect Linux kernel.
    
    Link: https://github.com/acpica/acpica/commit/969989cf
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpica/acglobal.h
+++ drivers/acpi/acpica/acglobal.h
@@ -313,7 +313,7 @@ ACPI_INIT_GLOBAL(u8, acpi_gbl_ignore_noop_operator, FALSE);
 ACPI_INIT_GLOBAL(u8, acpi_gbl_cstyle_disassembly, TRUE);
 ACPI_INIT_GLOBAL(u8, acpi_gbl_force_aml_disassembly, FALSE);
 
-ACPI_GLOBAL(u8, acpi_gbl_db_opt_disasm);
+ACPI_GLOBAL(u8, acpi_gbl_dm_opt_disasm);
 ACPI_GLOBAL(u8, acpi_gbl_dm_opt_listing);
 ACPI_GLOBAL(u8, acpi_gbl_db_opt_verbose);
 ACPI_GLOBAL(u8, acpi_gbl_num_external_methods);
