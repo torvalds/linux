9dcc144b1f91c4fe33422518e401e9c624e2c64f
Takashi Iwai <tiwai@suse.de>
2015-08-25 08:52:55 +0200

    ALSA: hda - Refresh sysfs at snd_hda_codec_update_widgets()
    In the commit [fa4f18b4f402: ALSA: hda - Refresh widgets sysfs at
    probing Haswell+ HDMI codecs], snd_hdac_refresh_widget_sysfs() is
    explicitly called in the codec driver.  But this results in refreshing
    twice, as snd_hdac_refresh_widget_sysfs() itself calls
    snd_hdac_refresh_widgets() function.
    
    Instead, we can replace the call in snd_hda_codec_update_widgets()
    with snd_hdac_refresh_widget_sysfs().  This also fixes the missing
    sysfs update for ca0132, too.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_codec.c
+++ sound/pci/hda/hda_codec.c
@@ -955,7 +955,7 @@ int snd_hda_codec_update_widgets(struct hda_codec *codec)
 	hda_nid_t fg;
 	int err;
 
-	err = snd_hdac_refresh_widgets(&codec->core);
+	err = snd_hdac_refresh_widget_sysfs(&codec->core);
 	if (err < 0)
 		return err;
 
