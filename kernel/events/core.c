516792e67c39d31701641ab355acdb9cbfec0643
Kirill Tkhai <ktkhai@odin.com>
2015-08-31 15:12:56 +0300

    perf/core: Delete PF_EXITING checks from perf_cgroup_exit() callback
    cgroup_exit() is not called from copy_process() after commit:
    
      e8604cb43690 ("cgroup: fix spurious lockdep warning in cgroup_exit()")
    
    from do_exit(). So this check is useless and the comment is obsolete.
    
    Signed-off-by: Kirill Tkhai <ktkhai@odin.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vince Weaver <vincent.weaver@maine.edu>
    Link: http://lkml.kernel.org/r/55E444C8.3020402@odin.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -9297,14 +9297,6 @@ static void perf_cgroup_exit(struct cgroup_subsys_state *css,
 			     struct cgroup_subsys_state *old_css,
 			     struct task_struct *task)
 {
-	/*
-	 * cgroup_exit() is called in the copy_process() failure path.
-	 * Ignore this case since the task hasn't ran yet, this avoids
-	 * trying to poke a half freed task state from generic code.
-	 */
-	if (!(task->flags & PF_EXITING))
-		return;
-
 	task_function_call(task, __perf_cgroup_move, task);
 }
 
