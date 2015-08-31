d1bfc62591a0a5144dc380976e737fbbb4f40f4f
Madalin Bucur <madalin.bucur@freescale.com>
2015-08-31 15:46:07 +0300

    ipv4: fix 32b build
    Address remaining issue after 80ec192.
    
    Signed-off-by: Madalin Bucur <madalin.bucur@freescale.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -1496,7 +1496,7 @@ u64 snmp_fold_field64(void __percpu *mib, int offt, size_t syncp_offset)
 	int cpu;
 
 	for_each_possible_cpu(cpu) {
-		res += snmp_get_cpu_field(mib, cpu, offt, syncp_offset);
+		res += snmp_get_cpu_field64(mib, cpu, offt, syncp_offset);
 	}
 	return res;
 }
