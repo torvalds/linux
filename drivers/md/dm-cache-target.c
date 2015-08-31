38c7d7770013cdbac508cf10f542b1ed52c70176
Joe Thornber <ejt@redhat.com>
2015-08-31 18:20:08 +0100

    dm cache: fix leaking of deferred bio prison cells
    commit 9153df7405ae04c1b0466de720e0a685cfea1a3a upstream.
    
    There were two cases where dm_cell_visit_release() was being called,
    which removes the cell from the prison's rbtree, but the callers didn't
    also return the cell to the mempool.  Fix this by having them call
    free_prison_cell().
    
    This leak manifested as the 'kmalloc-96' slab growing until OOM.
    
    Fixes: 651f5fa2a3 ("dm cache: defer whole cells")
    Signed-off-by: Joe Thornber <ejt@redhat.com>
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/md/dm-cache-target.c
+++ drivers/md/dm-cache-target.c
@@ -1729,6 +1729,8 @@ static void remap_cell_to_origin_clear_discard(struct cache *cache,
 		remap_to_origin(cache, bio);
 		issue(cache, bio);
 	}
+
+	free_prison_cell(cache, cell);
 }
 
 static void remap_cell_to_cache_dirty(struct cache *cache, struct dm_bio_prison_cell *cell,
@@ -1763,6 +1765,8 @@ static void remap_cell_to_cache_dirty(struct cache *cache, struct dm_bio_prison_
 		remap_to_cache(cache, bio, cblock);
 		issue(cache, bio);
 	}
+
+	free_prison_cell(cache, cell);
 }
 
 /*----------------------------------------------------------------*/
