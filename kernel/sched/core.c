84778472e1b6a27a8931712c40e8cf31143c8f6c
Davidlohr Bueso <dave@stgolabs.net>
2015-09-02 01:28:44 -0700

    sched: Export sched_setscheduler_nocheck
    The new locktorture rtmutex_lock tests exercise priority boosting, which
    means that they need to set some tasks to real-time priority.  To do this,
    they use sched_setscheduler_nocheck().  However, this is not exported to
    modules, which results in the following error when building locktorture
    as a module:
    
    ERROR: "sched_setscheduler_nocheck" [kernel/locking/locktorture.ko] undefined!
    
    This commit therefore adds an EXPORT_SYMBOL_GPL() to allow this function
    to be invoked from locktorture when built as a module.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Davidlohr Bueso <dave@stgolabs.net>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Acked-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4022,6 +4022,7 @@ int sched_setscheduler_nocheck(struct task_struct *p, int policy,
 {
 	return _sched_setscheduler(p, policy, param, false);
 }
+EXPORT_SYMBOL_GPL(sched_setscheduler_nocheck);
 
 static int
 do_sched_setscheduler(pid_t pid, int policy, struct sched_param __user *param)
