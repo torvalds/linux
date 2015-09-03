dc3a04d551b5d21f1badbb39bfe8e5bc1289b184
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2015-09-02 17:11:22 -0700

    security/device_cgroup: Fix RCU_LOCKDEP_WARN() condition
    f78f5b90c4ff ("rcu: Rename rcu_lockdep_assert() to RCU_LOCKDEP_WARN()")
    introduced a bug by incorrectly inverting the condition when moving from
    rcu_lockdep_assert() to RCU_LOCKDEP_WARN().  This commit therefore fixes
    the inversion.
    
    Reported-by: Felipe Balbi <balbi@ti.com>
    Reported-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Acked-by: Serge Hallyn <serge.hallyn@canonical.com>
    Tested-by: Josh Boyer <jwboyer@fedoraproject.org>

--- security/device_cgroup.c
+++ security/device_cgroup.c
@@ -401,7 +401,7 @@ static bool verify_new_ex(struct dev_cgroup *dev_cgroup,
 	bool match = false;
 
 	RCU_LOCKDEP_WARN(!rcu_read_lock_held() &&
-			 lockdep_is_held(&devcgroup_mutex),
+			 !lockdep_is_held(&devcgroup_mutex),
 			 "device_cgroup:verify_new_ex called without proper synchronization");
 
 	if (dev_cgroup->behavior == DEVCG_DEFAULT_ALLOW) {
