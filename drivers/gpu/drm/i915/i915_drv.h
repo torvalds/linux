f22f4cf524e66f20c86570023027504288f9bef2
Chris Wilson <chris@chris-wilson.co.uk>
2015-09-08 14:17:13 +0100

    drm/i915: Limit the number of loops for reading a split 64bit register
    commit acd29f7b22262d9e848393b9b6ae13eb42d22514 upstream.
    
    In I915_READ64_2x32 we attempt to read a 64bit register using 2 32bit
    reads. Due to the nature of the registers we try to read in this manner,
    they may increment between the two instruction (e.g. a timestamp
    counter). To keep the result accurate, we repeat the read if we detect
    an overflow (i.e. the upper value varies). However, some hardware is just
    plain flaky and may endless loop as the the upper 32bits are not stable.
    Just give up after a couple of tries and report whatever we read last.
    
    v2: Use the most recent values when erring out on an unstable register.
    
    Reported-by: russianneuromancer@ya.ru
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=91906
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Michał Winiarski <michal.winiarski@intel.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Jani Nikula <jani.nikula@linux.intel.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -3190,13 +3190,13 @@ int intel_freq_opcode(struct drm_i915_private *dev_priv, int val);
 #define I915_READ64(reg)	dev_priv->uncore.funcs.mmio_readq(dev_priv, (reg), true)
 
 #define I915_READ64_2x32(lower_reg, upper_reg) ({			\
-	u32 upper, lower, tmp;						\
-	tmp = I915_READ(upper_reg);					\
+	u32 upper, lower, old_upper, loop = 0;				\
+	upper = I915_READ(upper_reg);					\
 	do {								\
-		upper = tmp;						\
+		old_upper = upper;					\
 		lower = I915_READ(lower_reg);				\
-		tmp = I915_READ(upper_reg);				\
-	} while (upper != tmp);						\
+		upper = I915_READ(upper_reg);				\
+	} while (upper != old_upper && loop++ < 2);			\
 	(u64)upper << 32 | lower; })
 
 #define POSTING_READ(reg)	(void)I915_READ_NOTRACE(reg)
