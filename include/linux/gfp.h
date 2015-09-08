82c1fc714763b823169958a98196d9be56c63b30
Vlastimil Babka <vbabka@suse.cz>
2015-09-08 15:03:56 -0700

    mm: use numa_mem_id() in alloc_pages_node()
    alloc_pages_node() might fail when called with NUMA_NO_NODE and
    __GFP_THISNODE on a CPU belonging to a memoryless node.  To make the
    local-node fallback more robust and prevent such situations, use
    numa_mem_id(), which was introduced for similar scenarios in the slab
    context.
    
    Suggested-by: Christoph Lameter <cl@linux.com>
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    Acked-by: David Rientjes <rientjes@google.com>
    Acked-by: Mel Gorman <mgorman@techsingularity.net>
    Acked-by: Christoph Lameter <cl@linux.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/gfp.h
+++ include/linux/gfp.h
@@ -318,13 +318,14 @@ __alloc_pages_node(int nid, gfp_t gfp_mask, unsigned int order)
 
 /*
  * Allocate pages, preferring the node given as nid. When nid == NUMA_NO_NODE,
- * prefer the current CPU's node. Otherwise node must be valid and online.
+ * prefer the current CPU's closest node. Otherwise node must be valid and
+ * online.
  */
 static inline struct page *alloc_pages_node(int nid, gfp_t gfp_mask,
 						unsigned int order)
 {
 	if (nid == NUMA_NO_NODE)
-		nid = numa_node_id();
+		nid = numa_mem_id();
 
 	return __alloc_pages_node(nid, gfp_mask, order);
 }
