e4e29dc4841d21943bec1bc5378ab421d2320d83
Fabio Estevam <fabio.estevam@freescale.com>
2015-09-09 15:39:15 -0700

    lib/decompress_unlzma: Do a NULL check for pointer
    Compare pointer-typed values to NULL rather than 0.
    
    The semantic patch that makes this change is available
    in scripts/coccinelle/null/badzero.cocci.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/decompress_unlzma.c
+++ lib/decompress_unlzma.c
@@ -620,7 +620,7 @@ STATIC inline int INIT unlzma(unsigned char *buf, long in_len,
 
 	num_probs = LZMA_BASE_SIZE + (LZMA_LIT_SIZE << (lc + lp));
 	p = (uint16_t *) large_malloc(num_probs * sizeof(*p));
-	if (p == 0)
+	if (p == NULL)
 		goto exit_2;
 	num_probs = LZMA_LITERAL + (LZMA_LIT_SIZE << (lc + lp));
 	for (i = 0; i < num_probs; i++)
