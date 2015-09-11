a19a19de8310fb8ca2ca0621a9db1aab082943c5
Arnd Bergmann <arnd@arndb.de>
2015-09-11 11:33:01 +0200

    bnx2x: use ktime_get_seconds() for timestamp
    commit c48f350ff5e7 "bnx2x: Add MFW dump support" added the
    bnx2x_update_mfw_dump() function that reads the current time and stores
    it in a 32-bit field that gets passed into a buffer in a fixed format.
    
    This is potentially broken when the epoch overflows in 2038, and
    otherwise overflows in 2106. As we're trying to avoid uses of
    struct timeval for this reason, I noticed the addition of this
    function, and tried to rewrite it in a way that is more explicit
    about the overflow and that will keep working once we deprecate
    struct timeval.
    
    I assume that it is not possible to change the ABI any more, otherwise
    we should try to use a 64-bit field for the seconds right away.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Yuval Mintz <Yuval.Mintz@qlogic.com>
    Cc: Ariel Elior <Ariel.Elior@qlogic.com>
    Acked-by: Yuval Mintz <Yuval.Mintz@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/bnx2x/bnx2x_main.c
+++ drivers/net/ethernet/broadcom/bnx2x/bnx2x_main.c
@@ -3705,16 +3705,14 @@ out:
 
 void bnx2x_update_mfw_dump(struct bnx2x *bp)
 {
-	struct timeval epoc;
 	u32 drv_ver;
 	u32 valid_dump;
 
 	if (!SHMEM2_HAS(bp, drv_info))
 		return;
 
-	/* Update Driver load time */
-	do_gettimeofday(&epoc);
-	SHMEM2_WR(bp, drv_info.epoc, epoc.tv_sec);
+	/* Update Driver load time, possibly broken in y2038 */
+	SHMEM2_WR(bp, drv_info.epoc, (u32)ktime_get_real_seconds());
 
 	drv_ver = bnx2x_update_mng_version_utility(DRV_MODULE_VERSION, true);
 	SHMEM2_WR(bp, drv_info.drv_ver, drv_ver);
