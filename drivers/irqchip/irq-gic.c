8709b9eb37f07193e39ae4f8f8cb59aaed9eae2e
Geert Uytterhoeven <geert+renesas@glider.be>
2015-09-14 22:06:43 +0200

    irqchip/gic: Add arm,pl390 support
    Add support for the PrimeCell® Generic Interrupt Controller (PL390) to
    the GIC DT bindings and driver.
    
    Currently the GIC driver treats this GIC variant the same as other GIC
    variants, but there are differences in hardware topology (e.g. clock
    inputs).
    
    Sort the list of compatible values while we're at it.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Acked-by: Rob Herring <robh+dt@kernel.org>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: Pawel Moll <pawel.moll@arm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Kumar Gala <galak@codeaurora.org>
    Cc: Jason Cooper <jason@lakedaemon.net>
    Cc: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Ian Campbell <ijc+devicetree@hellion.org.uk>
    Link: http://lkml.kernel.org/r/1442261204-30931-2-git-send-email-geert%2Brenesas@glider.be
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- drivers/irqchip/irq-gic.c
+++ drivers/irqchip/irq-gic.c
@@ -1191,6 +1191,7 @@ IRQCHIP_DECLARE(cortex_a9_gic, "arm,cortex-a9-gic", gic_of_init);
 IRQCHIP_DECLARE(cortex_a7_gic, "arm,cortex-a7-gic", gic_of_init);
 IRQCHIP_DECLARE(msm_8660_qgic, "qcom,msm-8660-qgic", gic_of_init);
 IRQCHIP_DECLARE(msm_qgic2, "qcom,msm-qgic2", gic_of_init);
+IRQCHIP_DECLARE(pl390, "arm,pl390", gic_of_init);
 
 #endif
 
