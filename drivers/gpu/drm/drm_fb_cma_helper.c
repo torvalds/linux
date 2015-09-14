0ecd0dfbd409a7a7d8954e704a9152f0f24dfcbc
Xinliang Liu <xinliang.liu@linaro.org>
2015-09-14 17:55:04 +0800

    drm/cma-helper: Add multi buffer support for cma fbdev
    This patch add a config to support to create multi buffer for cma fbdev.
    Such as double buffer and triple buffer.
    
    Cma fbdev is convient to add a legency fbdev. And still many Android
    devices use fbdev now and at least double buffer is needed for these
    Android devices, so that a buffer flip can be operated. It will need some
    time for Android device vendors to abondon legency fbdev. So multi buffer
    for fbdev is needed.
    
    Signed-off-by: Xinliang Liu <xinliang.liu@linaro.org>

--- drivers/gpu/drm/drm_fb_cma_helper.c
+++ drivers/gpu/drm/drm_fb_cma_helper.c
@@ -27,6 +27,11 @@
 #include <linux/module.h>
 
 #define DEFAULT_FBDEFIO_DELAY_MS 50
+#ifdef CONFIG_DRM_CMA_FBDEV_BUFFER_NUM
+#define FBDEV_BUFFER_NUM CONFIG_DRM_CMA_FBDEV_BUFFER_NUM
+#else
+#define FBDEV_BUFFER_NUM 1
+#endif
 
 struct drm_fb_cma {
 	struct drm_framebuffer		fb;
@@ -403,7 +408,7 @@ int drm_fbdev_cma_create_with_funcs(struct drm_fb_helper *helper,
 	bytes_per_pixel = DIV_ROUND_UP(sizes->surface_bpp, 8);
 
 	mode_cmd.width = sizes->surface_width;
-	mode_cmd.height = sizes->surface_height;
+	mode_cmd.height = sizes->surface_height * FBDEV_BUFFER_NUM;
 	mode_cmd.pitches[0] = sizes->surface_width * bytes_per_pixel;
 	mode_cmd.pixel_format = drm_mode_legacy_fb_format(sizes->surface_bpp,
 		sizes->surface_depth);
