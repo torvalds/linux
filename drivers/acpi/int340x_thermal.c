cf3a51059efd653aa38667db799e85d77231af9d
Sudeep Holla <Sudeep.Holla@arm.com>
2015-09-14 16:01:54 +0100

    ACPI: int340x_thermal: add missing CONFIG_ prefix
    This patch adds the missing CONFIG_ prefix to INTEL_SOC_DTS_THERMAL
    macros.
    
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/int340x_thermal.c
+++ drivers/acpi/int340x_thermal.c
@@ -35,7 +35,7 @@ static int int340x_thermal_handler_attach(struct acpi_device *adev,
 {
 #if defined(CONFIG_INT340X_THERMAL) || defined(CONFIG_INT340X_THERMAL_MODULE)
 	acpi_create_platform_device(adev);
-#elif defined(INTEL_SOC_DTS_THERMAL) || defined(INTEL_SOC_DTS_THERMAL_MODULE)
+#elif defined(CONFIG_INTEL_SOC_DTS_THERMAL) || defined(CONFIG_INTEL_SOC_DTS_THERMAL_MODULE)
 	/* Intel SoC DTS thermal driver needs INT3401 to set IRQ descriptor */
 	if (id->driver_data == INT3401_DEVICE)
 		acpi_create_platform_device(adev);
