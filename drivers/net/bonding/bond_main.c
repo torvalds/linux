4b1b865e4e97e336316f30e32af36d71e98bdabc
Eric Dumazet <edumazet@google.com>
2015-09-15 15:24:28 -0700

    bonding: use l4 hash if available
    If skb carries a l4 hash, no need to perform a flow dissection.
    
    Performance is slightly better :
    
    lpaa5:~# ./super_netperf 200 -H lpaa6 -t TCP_RR -l 100
    2.39012e+06
    lpaa5:~# ./super_netperf 200 -H lpaa6 -t TCP_RR -l 100
    2.39393e+06
    lpaa5:~# ./super_netperf 200 -H lpaa6 -t TCP_RR -l 100
    2.39988e+06
    
    After patch :
    
    lpaa5:~# ./super_netperf 200 -H lpaa6 -t TCP_RR -l 100
    2.43579e+06
    lpaa5:~# ./super_netperf 200 -H lpaa6 -t TCP_RR -l 100
    2.44304e+06
    lpaa5:~# ./super_netperf 200 -H lpaa6 -t TCP_RR -l 100
    2.44312e+06
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Tom Herbert <tom@herbertland.com>
    Cc: Mahesh Bandewar <maheshb@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -3136,6 +3136,10 @@ u32 bond_xmit_hash(struct bonding *bond, struct sk_buff *skb)
 	struct flow_keys flow;
 	u32 hash;
 
+	if (bond->params.xmit_policy == BOND_XMIT_POLICY_ENCAP34 &&
+	    skb->l4_hash)
+		return skb->hash;
+
 	if (bond->params.xmit_policy == BOND_XMIT_POLICY_LAYER2 ||
 	    !bond_flow_dissect(bond, skb, &flow))
 		return bond_eth_hash(skb);
