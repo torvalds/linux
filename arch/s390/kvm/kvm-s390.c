9bf9fde2c98ba8362ea1d41d8bd8b32a23776e67
Jason J. Herne <jjherne@linux.vnet.ibm.com>
2015-09-16 09:13:50 -0400

    KVM: s390: Replace incorrect atomic_or with atomic_andnot
    The offending commit accidentally replaces an atomic_clear with an
    atomic_or instead of an atomic_andnot in kvm_s390_vcpu_request_handled.
    The symptom is that kvm guests on s390 hang on startup.
    This patch simply replaces the incorrect atomic_or with atomic_andnot
    
    Fixes: 805de8f43c20 (atomic: Replace atomic_{set,clear}_mask() usage)
    Signed-off-by: Jason J. Herne <jjherne@linux.vnet.ibm.com>
    Acked-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Cornelia Huck <cornelia.huck@de.ibm.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/s390/kvm/kvm-s390.c
+++ arch/s390/kvm/kvm-s390.c
@@ -1575,7 +1575,7 @@ static void kvm_s390_vcpu_request(struct kvm_vcpu *vcpu)
 
 static void kvm_s390_vcpu_request_handled(struct kvm_vcpu *vcpu)
 {
-	atomic_or(PROG_REQUEST, &vcpu->arch.sie_block->prog20);
+	atomic_andnot(PROG_REQUEST, &vcpu->arch.sie_block->prog20);
 }
 
 /*
