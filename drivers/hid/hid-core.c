0678072755b6672b19ee0fd42748a003912fca09
Daniel Martin <daniel.martin@secunet.com>
2015-09-17 17:02:22 +0200

    HID: Make I2C a known bus in hid_connect()
    Just to prettify the log message. Otherwise it would be <UNKNOWN>.
    
    Signed-off-by: Daniel Martin <consume.noise@gmail.com>
    Reviewed-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -1678,6 +1678,9 @@ int hid_connect(struct hid_device *hdev, unsigned int connect_mask)
 	case BUS_BLUETOOTH:
 		bus = "BLUETOOTH";
 		break;
+	case BUS_I2C:
+		bus = "I2C";
+		break;
 	default:
 		bus = "<UNKNOWN>";
 	}
