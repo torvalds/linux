c33be5d1187b88ea5936b01a7df3dbc71ad466b0
Dietmar Eggemann <dietmar.eggemann@arm.com>
2015-09-17 16:10:56 +0100

    ANDROID: cpufreq: Frequency invariant scheduler load-tracking support
    Implements cpufreq_scale_freq_capacity() to provide the scheduler with a
    frequency scaling correction factor for more accurate load-tracking.
    
    The factor is:
    
    	current_freq(cpu) << SCHED_CAPACITY_SHIFT / max_freq(cpu)
    
    In fact, freq_scale should be a struct cpufreq_policy data member. But
    this would require that the scheduler hot path (__update_load_avg()) would
    have to grab the cpufreq lock. This can be avoided by using per-cpu data
    initialized to SCHED_CAPACITY_SCALE for freq_scale.
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Andres Oportus <andresoportus@google.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -886,4 +886,7 @@ unsigned int cpufreq_generic_get(unsigned int cpu);
 int cpufreq_generic_init(struct cpufreq_policy *policy,
 		struct cpufreq_frequency_table *table,
 		unsigned int transition_latency);
+
+struct sched_domain;
+unsigned long cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu);
 #endif /* _LINUX_CPUFREQ_H */
