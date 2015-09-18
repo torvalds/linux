4fc2010defdc81f20ca7e8337fe6a05a13191cb9
Octavian Purdila <octavian.purdila@intel.com>
2015-09-18 13:56:48 +0300

    lkl tools: skeleton for host side library, tests and tools
    This patch adds the skeleton for the host library, tests and
    application examples.
    
    The host library is implementing the host operations needed by LKL and
    is split into host dependent (depends on a specific host, e.g. POSIX
    hosts) and host independent parts (will work on all supported hosts).
    
    Signed-off-by: Octavian Purdila <octavian.purdila@intel.com>

---
+++ tools/lkl/include/lkl.h
@@ -0,0 +1,15 @@
+#ifndef _LKL_H
+#define _LKL_H
+
+#include <lkl/asm/unistd.h>
+
+/**
+ * lkl_sys_lseek - wrapper for lkl_sys_llseek
+ */
+static inline long lkl_sys_lseek(unsigned int fd, __lkl__kernel_loff_t off,
+				 __lkl__kernel_loff_t *res, unsigned int whence)
+{
+	return lkl_sys_llseek(fd, off >> 32, off & 0xffffffff, res, whence);
+}
+
+#endif
---
+++ tools/lkl/include/lkl_host.h
@@ -0,0 +1,9 @@
+#ifndef _LKL_HOST_H
+#define _LKL_HOST_H
+
+#include <lkl/asm/host_ops.h>
+#include <lkl.h>
+
+extern struct lkl_host_operations lkl_host_ops;
+
+#endif
