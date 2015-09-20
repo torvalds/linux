b821768a77d5b43f05ad7a9316906a1c5a22cad6
Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
2015-09-20 10:18:50 +0200

    staging: rtl8192e: Rename r8192_wx_get_power
    Use naming schema found in other rtlwifi devices.
    Rename r8192_wx_get_power to _rtl92e_wx_get_power.
    
    Signed-off-by: Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/rtl_wx.c
+++ drivers/staging/rtl8192e/rtl8192e/rtl_wx.c
@@ -130,9 +130,9 @@ static int r8192_wx_set_power(struct net_device *dev,
 	return ret;
 }
 
-static int r8192_wx_get_power(struct net_device *dev,
-			     struct iw_request_info *info,
-			     union iwreq_data *wrqu, char *extra)
+static int _rtl92e_wx_get_power(struct net_device *dev,
+				struct iw_request_info *info,
+				union iwreq_data *wrqu, char *extra)
 {
 	struct r8192_priv *priv = rtllib_priv(dev);
 
@@ -1144,7 +1144,7 @@ static iw_handler r8192_wx_handlers[] = {
 	[IW_IOCTL(SIOCSIWENCODE)] = r8192_wx_set_enc,
 	[IW_IOCTL(SIOCGIWENCODE)] = _rtl92e_wx_get_enc,
 	[IW_IOCTL(SIOCSIWPOWER)] = r8192_wx_set_power,
-	[IW_IOCTL(SIOCGIWPOWER)] = r8192_wx_get_power,
+	[IW_IOCTL(SIOCGIWPOWER)] = _rtl92e_wx_get_power,
 	[IW_IOCTL(SIOCSIWGENIE)] = r8192_wx_set_gen_ie,
 	[IW_IOCTL(SIOCGIWGENIE)] = _rtl92e_wx_get_gen_ie,
 	[IW_IOCTL(SIOCSIWMLME)] = r8192_wx_set_mlme,
