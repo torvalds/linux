b4ff1de3b8381a471d1b1320be0daa3f8fc2ed85
Boris Barbulovski <bbarbulovski@gmail.com>
2015-09-22 11:36:38 -0700

    Port xconfig to Qt5 - Update copyright.
    Signed-off-by: Boris Barbulovski <bbarbulovski@gmail.com>
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>
    Signed-off-by: Michal Marek <mmarek@suse.com>

--- scripts/kconfig/qconf.cc
+++ scripts/kconfig/qconf.cc
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2002 Roman Zippel <zippel@linux-m68k.org>
+ * Copyright (C) 2015 Boris Barbulovski <bbarbulovski@gmail.com>
  * Released under the terms of the GNU GPL v2.0.
  */
 
@@ -1746,7 +1747,8 @@ void ConfigMainWindow::showIntro(void)
 
 void ConfigMainWindow::showAbout(void)
 {
-	static const QString str = _("qconf is Copyright (C) 2002 Roman Zippel <zippel@linux-m68k.org>.\n\n"
+	static const QString str = _("qconf is Copyright (C) 2002 Roman Zippel <zippel@linux-m68k.org>.\n"
+		"Copyright (C) 2015 Boris Barbulovski <bbarbulovski@gmail.com>.\n\n"
 		"Bug reports and feature request can also be entered at http://bugzilla.kernel.org/\n");
 
 	QMessageBox::information(this, "qconf", str);
