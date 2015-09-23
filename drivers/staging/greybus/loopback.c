8d8d36da750ccba4c9ba9a293b3a338afd0b75b0
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2015-09-23 09:31:33 -0700

    greybus: loopback: drop redundant endo0 string from debugfs entry name
    dev_name() will return the endo0 component of the string on it's own,
    there's no need to include it in the snprintf() when construting the
    debugfs name. This fixes 'endo0' appearing more than once in the debugfs
    name - shamefully slipped through testing cb570c93783f
    ('greybus/loopback: use dev_name to populate sysfsname').
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/loopback.c
+++ drivers/staging/greybus/loopback.c
@@ -906,7 +906,7 @@ static int gb_loopback_connection_init(struct gb_connection *connection)
 	}
 
 	/* Create per-connection sysfs and debugfs data-points */
-	snprintf(name, sizeof(name), "raw_latency_endo0:%s",
+	snprintf(name, sizeof(name), "raw_latency_%s",
 		 dev_name(&connection->dev));
 	gb->file = debugfs_create_file(name, S_IFREG | S_IRUGO, gb_dev.root, gb,
 				       &gb_loopback_debugfs_latency_ops);
