30433d3b9850140ceb801c5cc2013c7b408c33de
Johannes Berg <johannes.berg@intel.com>
2015-09-24 18:14:55 +0200

    iwlwifi: mvm: prevent multiple stations with the same address
    As the device (and parts of the driver) cannot deal with having the
    same MAC address for two stations (on two virtual interfaces), add
    some explicit code to prevent this case.
    
    Note that in practice this cannot happen since the device doesn't
    support operating with two AP/GO interfaces at the same time either,
    and other scenarios for this are, while not impossible, not going to
    happen in practice.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>

--- drivers/net/wireless/intel/iwlwifi/mvm/mac80211.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/mac80211.c
@@ -439,6 +439,7 @@ int iwl_mvm_mac_setup_register(struct iwl_mvm *mvm)
 	ieee80211_hw_set(hw, SUPPORT_FAST_XMIT);
 	ieee80211_hw_set(hw, SUPPORTS_CLONED_SKBS);
 	ieee80211_hw_set(hw, SUPPORTS_AMSDU_IN_AMPDU);
+	ieee80211_hw_set(hw, NEEDS_UNIQUE_STA_ADDR);
 
 	if (mvm->trans->max_skb_frags)
 		hw->netdev_features = NETIF_F_HIGHDMA | NETIF_F_SG;
