a1b7febd725a2cdfc8ac245b7b7437ce4b91aecb
Maxime Ripard <maxime.ripard@free-electrons.com>
2015-09-25 18:09:32 +0200

    genirq: Fix the documentation of request_percpu_irq
    The documentation of request_percpu_irq is confusing and suggest that the
    interrupt is not enabled at all, while it is actually enabled on the local
    CPU.
    
    Clarify that.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>
    Signed-off-by: Gregory CLEMENT <gregory.clement@free-electrons.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -1790,9 +1790,10 @@ int setup_percpu_irq(unsigned int irq, struct irqaction *act)
  *	@devname: An ascii name for the claiming device
  *	@dev_id: A percpu cookie passed back to the handler function
  *
- *	This call allocates interrupt resources, but doesn't
- *	automatically enable the interrupt. It has to be done on each
- *	CPU using enable_percpu_irq().
+ *	This call allocates interrupt resources and enables the
+ *	interrupt on the local CPU. If the interrupt is supposed to be
+ *	enabled on other CPUs, it has to be done on each CPU using
+ *	enable_percpu_irq().
  *
  *	Dev_id must be globally unique. It is a per-cpu variable, and
  *	the handler gets called with the interrupted CPU's instance of
