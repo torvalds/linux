777273b78ea8e63ce5bf2f90ffbc8247cba8c15c
Dietmar Eggemann <dietmar.eggemann@arm.com>
2015-09-25 17:15:11 +0100

    arm64: Enable frequency invariant scheduler load-tracking support
    Defines arch_scale_freq_capacity() to use cpufreq implementation.
    
    Including <linux/cpufreq.h> in topology.h like for the arm arch doesn't
    work because of CONFIG_COMPAT=y (Kernel support for 32-bit EL0).
    That's why cpufreq_scale_freq_capacity() has to be declared extern in
    topology.h.
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

--- arch/arm64/include/asm/topology.h
+++ arch/arm64/include/asm/topology.h
@@ -22,6 +22,12 @@ void init_cpu_topology(void);
 void store_cpu_topology(unsigned int cpuid);
 const struct cpumask *cpu_coregroup_mask(int cpu);
 
+#ifdef CONFIG_CPU_FREQ
+#define arch_scale_freq_capacity cpufreq_scale_freq_capacity
+struct sched_domain;
+extern unsigned long cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu);
+#endif
+
 #include <asm-generic/topology.h>
 
 #endif /* _ASM_ARM_TOPOLOGY_H */
