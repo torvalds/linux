9f088dba3cc267ea11ec0da318cd0175575b5f9b
Arnd Bergmann <arnd@arndb.de>
2015-09-27 16:45:01 -0400

    staging/lustre: use jiffies for lp_last_query times
    The recently introduced lnet_peer_set_alive() function uses
    get_seconds() to read the current time into a shared variable,
    but all other uses of that variable compare it to jiffies values.
    
    This changes the current use to jiffies as well for consistency.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Fixes: af3fa7c71bf ("staging/lustre/lnet: peer aliveness status and NI status")
    Cc: Liang Zhen <liang.zhen@intel.com>
    Cc: James Simmons <uja.ornl@gmail.com>
    Cc: Isaac Huang <he.huang@intel.com>
    Signed-off-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/lnet/lib-lnet.h
+++ drivers/staging/lustre/include/linux/lnet/lib-lnet.h
@@ -680,7 +680,7 @@ void lnet_debug_peer(lnet_nid_t nid);
 static inline void
 lnet_peer_set_alive(lnet_peer_t *lp)
 {
-	lp->lp_last_alive = lp->lp_last_query = get_seconds();
+	lp->lp_last_alive = lp->lp_last_query = jiffies;
 	if (!lp->lp_alive)
 		lnet_notify_locked(lp, 0, 1, lp->lp_last_alive);
 }
