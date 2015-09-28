49dfebebfb6811693a9e883423585f079b5941fa
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2015-09-28 18:57:07 +0300

    avr32: use dw_dmac autoconfiguration
    Instead of hardcoding a platform data for dw_dmac let's use it's own
    autoconfiguration feature. Thus, remove hardcoded values.
    
    Tested on ATNGW100.
    
    Acked-by: Hans-Christian Egtvedt <egtvedt@samfundet.no>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- arch/avr32/mach-at32ap/at32ap700x.c
+++ arch/avr32/mach-at32ap/at32ap700x.c
@@ -603,18 +603,11 @@ static void __init genclk_init_parent(struct clk *clk)
 	clk->parent = parent;
 }
 
-static struct dw_dma_platform_data dw_dmac0_data = {
-	.nr_channels	= 3,
-	.block_size	= 4095U,
-	.nr_masters	= 2,
-	.data_width	= { 2, 2 },
-};
-
 static struct resource dw_dmac0_resource[] = {
 	PBMEM(0xff200000),
 	IRQ(2),
 };
-DEFINE_DEV_DATA(dw_dmac, 0);
+DEFINE_DEV(dw_dmac, 0);
 DEV_CLK(hclk, dw_dmac0, hsb, 10);
 
 /* --------------------------------------------------------------------
