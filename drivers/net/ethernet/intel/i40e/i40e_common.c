8589af70d0879a68c93ef77505a6234d22b1b1a7
Carolyn Wyborny <carolyn.wyborny@intel.com>
2015-09-28 14:16:56 -0400

    i40e: fix for PHY NVM interaction problem
    This patch fixes a problem where the NVMUpdate Tool, when using the PHY
    NVM feature, gets bad data from the PHY because of contention on the
    MDIO interface from get PHY capability calls from the driver during
    regular operations.  The problem is fixed by adding a check if media
    is available before calling get PHY capability function because that
    bit is not set when device is in PHY interaction mode.
    
    Change-ID: Ib89991b0f841808dd92410f5e8683d6ee3301cd0
    Signed-off-by: Carolyn Wyborny <carolyn.wyborny@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_common.c
+++ drivers/net/ethernet/intel/i40e/i40e_common.c
@@ -2278,13 +2278,15 @@ i40e_status i40e_update_link_info(struct i40e_hw *hw)
 	if (status)
 		return status;
 
-	status = i40e_aq_get_phy_capabilities(hw, false, false, &abilities,
-					      NULL);
-	if (status)
-		return status;
+	if (hw->phy.link_info.link_info & I40E_AQ_MEDIA_AVAILABLE) {
+		status = i40e_aq_get_phy_capabilities(hw, false, false,
+						      &abilities, NULL);
+		if (status)
+			return status;
 
-	memcpy(hw->phy.link_info.module_type, &abilities.module_type,
-	       sizeof(hw->phy.link_info.module_type));
+		memcpy(hw->phy.link_info.module_type, &abilities.module_type,
+		       sizeof(hw->phy.link_info.module_type));
+	}
 
 	return status;
 }
