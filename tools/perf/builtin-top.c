4b37af595742977f1bdd8c0fd0f3e6e55b36e7b7
Namhyung Kim <namhyung@kernel.org>
2015-09-30 10:45:25 +0900

    perf top: Fix unresolved comm when -s comm is used
    The perf top uses 'dso,symbol' sort keys by default so it overlooked a
    problem in task's comm resolving.  When the sort key contains 'comm',
    some task's comm is not shown properly.  This is because the
    perf_top__mmap_read_idx() checks the cpumode value improperly.
    
    The cpumode value of non-sample events are 0 (PERF_RECORD_MISC_CPUMODE_
    UNKNOWN) so the events will be ignored by the switch statement.  This patch
    allows it for non-sample events.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1443577526-3240-2-git-send-email-namhyung@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/builtin-top.c
+++ tools/perf/builtin-top.c
@@ -857,9 +857,12 @@ static void perf_top__mmap_read_idx(struct perf_top *top, int idx)
 			 * TODO: we don't process guest user from host side
 			 * except simple counting.
 			 */
-			/* Fall thru */
-		default:
 			goto next_event;
+		default:
+			if (event->header.type == PERF_RECORD_SAMPLE)
+				goto next_event;
+			machine = &session->machines.host;
+			break;
 		}
 
 
