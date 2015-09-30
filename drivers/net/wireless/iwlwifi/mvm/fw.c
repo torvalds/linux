4508582e6a831d8256fe6305967d0a8cf13f9758
Arik Nemtsov <arik@wizery.com>
2015-09-30 11:19:55 +0300

    iwlwifi: mvm: init card correctly on ctkill exit check
    commit 1a3fe0b2b6778b7866e2b3f5c9a299d5e9bbd89c upstream.
    
    During the CT-kill exit flow, the card is powered up and partially
    initialized to check if the temperature is already low enough.
    Unfortunately the init bails early because the CT-kill flag is set.
    Make the code bail early only for HW RF-kill, as was intended by the
    author. CT-kill is self-imposed and is not really RF-kill.
    
    Fixes: 31b8b343e019 ("iwlwifi: fix RFkill while calibrating")
    Signed-off-by: Arik Nemtsov <arikx.nemtsov@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/wireless/iwlwifi/mvm/fw.c
+++ drivers/net/wireless/iwlwifi/mvm/fw.c
@@ -364,7 +364,7 @@ int iwl_run_init_mvm_ucode(struct iwl_mvm *mvm, bool read_nvm)
 	 * abort after reading the nvm in case RF Kill is on, we will complete
 	 * the init seq later when RF kill will switch to off
 	 */
-	if (iwl_mvm_is_radio_killed(mvm)) {
+	if (iwl_mvm_is_radio_hw_killed(mvm)) {
 		IWL_DEBUG_RF_KILL(mvm,
 				  "jump over all phy activities due to RF kill\n");
 		iwl_remove_notification(&mvm->notif_wait, &calib_wait);
@@ -397,7 +397,7 @@ int iwl_run_init_mvm_ucode(struct iwl_mvm *mvm, bool read_nvm)
 	ret = iwl_wait_notification(&mvm->notif_wait, &calib_wait,
 			MVM_UCODE_CALIB_TIMEOUT);
 
-	if (ret && iwl_mvm_is_radio_killed(mvm)) {
+	if (ret && iwl_mvm_is_radio_hw_killed(mvm)) {
 		IWL_DEBUG_RF_KILL(mvm, "RFKILL while calibrating.\n");
 		ret = 1;
 	}
