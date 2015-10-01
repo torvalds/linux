053118935e7a0f62e96471220acd659b83d04ef7
Shraddha Barke <shraddha.6596@gmail.com>
2015-10-01 15:07:58 +0530

    Staging: lustre: obdclass: Declare structure as static.
    Declare structure as static since it is not accessed anywhere
    apart from this particular file.
    
    Signed-off-by: Shraddha Barke <shraddha.6596@gmail.com>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/obdclass/lu_object.c
+++ drivers/staging/lustre/lustre/obdclass/lu_object.c
@@ -429,7 +429,7 @@ LU_KEY_INIT_FINI(lu_global, struct lu_cdebug_data);
  * Key, holding temporary buffer. This key is registered very early by
  * lu_global_init().
  */
-struct lu_context_key lu_global_key = {
+static struct lu_context_key lu_global_key = {
 	.lct_tags = LCT_MD_THREAD | LCT_DT_THREAD |
 		    LCT_MG_THREAD | LCT_CL_THREAD | LCT_LOCAL,
 	.lct_init = lu_global_key_init,
