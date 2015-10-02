5ccd9abd508644cefee8641ccd6157e8b55a6ee3
Javier Martinez Canillas <javier@osg.samsung.com>
2015-10-02 11:14:26 -0700

    Input: kxtj9 - remove unneeded retval variable
    The retval variable isn't needed since isn't used in the function. Remove
    the variable and just return 0 instead.
    
    This also fixes the following make coccicheck warning:
    
    drivers/input/misc/kxtj9.c:638:5-11: Unneeded variable: "retval". Return "0" on line 646
    
    Signed-off-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/misc/kxtj9.c
+++ drivers/input/misc/kxtj9.c
@@ -635,7 +635,6 @@ static int __maybe_unused kxtj9_resume(struct device *dev)
 	struct i2c_client *client = to_i2c_client(dev);
 	struct kxtj9_data *tj9 = i2c_get_clientdata(client);
 	struct input_dev *input_dev = tj9->input_dev;
-	int retval = 0;
 
 	mutex_lock(&input_dev->mutex);
 
@@ -643,7 +642,7 @@ static int __maybe_unused kxtj9_resume(struct device *dev)
 		kxtj9_enable(tj9);
 
 	mutex_unlock(&input_dev->mutex);
-	return retval;
+	return 0;
 }
 
 static SIMPLE_DEV_PM_OPS(kxtj9_pm_ops, kxtj9_suspend, kxtj9_resume);
