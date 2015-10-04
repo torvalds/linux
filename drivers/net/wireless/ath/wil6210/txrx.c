a3dcbae2476f2c32e8f123dd833d789f7f494711
Vladimir Kondratiev <QCA_vkondrat@QCA.qualcomm.com>
2015-10-04 10:23:25 +0300

    wil6210: ratelimit Tx error message
    Situations observed when IP stack schedules lots of
    frames for Tx while no connection (connection lost,
    for example). In this case, dmesg bloated with error
    message "FW not connected", printed for every frame.
    
    Ratelimit this error message to avoid dmesg pollution.
    
    Signed-off-by: Vladimir Kondratiev <qca_vkondrat@qca.qualcomm.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/wil6210/txrx.c
+++ drivers/net/wireless/ath/wil6210/txrx.c
@@ -1667,7 +1667,7 @@ netdev_tx_t wil_start_xmit(struct sk_buff *skb, struct net_device *ndev)
 		goto drop;
 	}
 	if (unlikely(!test_bit(wil_status_fwconnected, wil->status))) {
-		wil_err(wil, "FW not connected\n");
+		wil_err_ratelimited(wil, "FW not connected\n");
 		goto drop;
 	}
 	if (unlikely(wil->wdev->iftype == NL80211_IFTYPE_MONITOR)) {
