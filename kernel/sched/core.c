ce03e4137bb22fc560ad7a07cf4138ae2cd59f65
Geliang Tang <geliangtang@163.com>
2015-10-05 21:26:05 +0800

    sched/core: Drop unlikely behind BUG_ON()
    (1) For !CONFIG_BUG cases, the bug call is a no-op, so we couldn't care
        less and the change is ok.
    
    (2) PPC and MIPS, which HAVE_ARCH_BUG_ON, do not rely on branch predictions
        as it seems to be pointless [1] and thus callers should not be trying to
        push an optimization in the first place.
    
    (3) For CONFIG_BUG and !HAVE_ARCH_BUG_ON cases, BUG_ON() contains an
        unlikely compiler flag already.
    
    Hence, we can drop unlikely behind BUG_ON().
    
      [1] http://lkml.iu.edu/hypermail/linux/kernel/1101.3/02289.html
    
    Signed-off-by: Geliang Tang <geliangtang@163.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Link: http://lkml.kernel.org/r/6fa7125979f98bbeac26e268271769b6ca935c8d.1444051018.git.geliangtang@163.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2971,7 +2971,7 @@ static noinline void __schedule_bug(struct task_struct *prev)
 static inline void schedule_debug(struct task_struct *prev)
 {
 #ifdef CONFIG_SCHED_STACK_END_CHECK
-	BUG_ON(unlikely(task_stack_end_corrupted(prev)));
+	BUG_ON(task_stack_end_corrupted(prev));
 #endif
 
 	if (unlikely(in_atomic_preempt_off())) {
