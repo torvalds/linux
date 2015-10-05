0e841b04c829f59a5d5745f98d2857f48882efe9
Chen-Yu Tsai <wens@csie.org>
2015-10-06 00:42:14 +0800

    irqchip/sunxi-nmi: Switch to of_io_request_and_map() from of_iomap()
    Switch to the new of_io_request_and_map() call, so the IO resource is
    properly held, and also shows up in /proc/iomem.
    
    Signed-off-by: Chen-Yu Tsai <wens@csie.org>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: Jason Cooper <jason@lakedaemon.net>
    Cc: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Maxime Ripard <maxime.ripard@free-electrons.com>
    Link: http://lkml.kernel.org/r/1444063334-19832-3-git-send-email-wens@csie.org
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- drivers/irqchip/irq-sunxi-nmi.c
+++ drivers/irqchip/irq-sunxi-nmi.c
@@ -153,7 +153,7 @@ static int __init sunxi_sc_nmi_irq_init(struct device_node *node,
 	}
 
 	gc = irq_get_domain_generic_chip(domain, 0);
-	gc->reg_base = of_iomap(node, 0);
+	gc->reg_base = of_io_request_and_map(node, 0, of_node_full_name(node));
 	if (!gc->reg_base) {
 		pr_err("unable to map resource\n");
 		ret = -ENOMEM;
