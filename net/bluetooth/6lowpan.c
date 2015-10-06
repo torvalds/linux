5d0fd77a043504dabccb66d9b5671e682868e96d
Johan Hedberg <johan.hedberg@intel.com>
2015-10-06 13:03:20 +0300

    Bluetooth: 6lowpan: Remove redundant (and incorrect) MPS assignments
    The L2CAP core code already sets the local MPS to a sane value. The
    remote MPS value otoh comes from the remote side so there's no point
    in trying to hard-code it to any value.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Acked-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/6lowpan.c
+++ net/bluetooth/6lowpan.c
@@ -788,9 +788,6 @@ static struct l2cap_chan *chan_open(struct l2cap_chan *pchan)
 	if (!chan)
 		return NULL;
 
-	chan->remote_mps = chan->omtu;
-	chan->mps = chan->omtu;
-
 	chan->state = BT_CONNECTED;
 
 	return chan;
