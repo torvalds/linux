b299167652fe58f1ebadb3e3ac84a5a0b74e534e
Max Filippov <jcmvbkbc@gmail.com>
2015-10-07 02:45:11 +0300

    i2c: ocores: support big-endian register layout
    This allows using OpenCores I2C controller attached to its host in
    native-endian mode with bi-endian CPUs. Example of such system is Xtensa
    XTFPGA platform.
    
    Acked-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- include/linux/i2c-ocores.h
+++ include/linux/i2c-ocores.h
@@ -15,6 +15,7 @@ struct ocores_i2c_platform_data {
 	u32 reg_shift; /* register offset shift value */
 	u32 reg_io_width; /* register io read/write width */
 	u32 clock_khz; /* input clock in kHz */
+	bool big_endian; /* registers are big endian */
 	u8 num_devices; /* number of devices in the devices list */
 	struct i2c_board_info const *devices; /* devices connected to the bus */
 };
