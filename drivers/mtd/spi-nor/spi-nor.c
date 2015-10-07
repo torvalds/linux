f9bcb6dc8013d4da6660556ddf2383e4fbcbe3d7
Aurelien Chanot <chanot.a@gmail.com>
2015-10-07 12:10:08 -0700

    mtd: spi-nor: Add support for Micron n25q032a
    The N25Q032A is identical to the N25Q032 except it has a different
    supply voltage range. Therefore, it has a new JEDEC ID.
    
    Signed-off-by: Aurelien Chanot <chanot.a@gmail.com>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -615,6 +615,7 @@ static const struct flash_info spi_nor_ids[] = {
 
 	/* Micron */
 	{ "n25q032",	 INFO(0x20ba16, 0, 64 * 1024,   64, SPI_NOR_QUAD_READ) },
+	{ "n25q032a",	 INFO(0x20bb16, 0, 64 * 1024,   64, SPI_NOR_QUAD_READ) },
 	{ "n25q064",     INFO(0x20ba17, 0, 64 * 1024,  128, SECT_4K | SPI_NOR_QUAD_READ) },
 	{ "n25q064a",    INFO(0x20bb17, 0, 64 * 1024,  128, SECT_4K | SPI_NOR_QUAD_READ) },
 	{ "n25q128a11",  INFO(0x20bb18, 0, 64 * 1024,  256, SPI_NOR_QUAD_READ) },
