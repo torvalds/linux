71c01379d1c57c96d833202d6b8128ccffc42665
H Hartley Sweeten <hsweeten@visionengravers.com>
2015-10-07 15:43:44 -0700

    staging: comedi: fl512: usleep_range is preferred over udelay
    Fix checkpatch.pl issue:
    
    CHECK: usleep_range is preferred over udelay; see
           Documentation/timers/timers-howto.txt
    
    Replace the udelay() with usleep_range() with a reasonable upper limit.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/fl512.c
+++ drivers/staging/comedi/drivers/fl512.c
@@ -71,7 +71,7 @@ static int fl512_ai_insn_read(struct comedi_device *dev,
 		outb(0, dev->iobase + FL512_AI_START_CONV_REG);
 
 		/* XXX should test "done" flag instead of delay */
-		udelay(30);
+		usleep_range(30, 100);
 
 		val = inb(dev->iobase + FL512_AI_LSB_REG);
 		val |= (inb(dev->iobase + FL512_AI_MSB_REG) << 8);
