468f9d2903bcf23e50663679eab5a18da5b886f6
Javier Martinez Canillas <javier@osg.samsung.com>
2015-10-08 09:54:44 +0200

    drm/i915/irq: Fix kernel-doc warnings
    Add the dev parameter for the functions i915_enable_asle_pipestat() and
    i915_reset_and_wakeup() to the kernel-doc to fix the following warnings:
    
    .//drivers/gpu/drm/i915/i915_irq.c:586: warning: No description found for parameter 'dev'
    .//drivers/gpu/drm/i915/i915_irq.c:2400: warning: No description found for parameter 'dev'
    
    Signed-off-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_irq.c
+++ drivers/gpu/drm/i915/i915_irq.c
@@ -581,6 +581,7 @@ i915_disable_pipestat(struct drm_i915_private *dev_priv, enum pipe pipe,
 
 /**
  * i915_enable_asle_pipestat - enable ASLE pipestat for OpRegion
+ * @dev: drm device
  */
 static void i915_enable_asle_pipestat(struct drm_device *dev)
 {
@@ -2392,6 +2393,7 @@ static void i915_error_wake_up(struct drm_i915_private *dev_priv,
 
 /**
  * i915_reset_and_wakeup - do process context error handling work
+ * @dev: drm device
  *
  * Fire an error uevent so userspace can see that a hang or error
  * was detected.
