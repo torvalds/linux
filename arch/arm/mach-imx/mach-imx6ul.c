7f7308198f98700be82d123569ea59229a75d3c3
Bai Ping <b51503@freescale.com>
2015-10-09 23:35:30 +0800

    ARM: imx: add cpufreq device for imx6ul
    Add cpufreq device for i.MX6UL. Using the common
    cpufreq of i.MX6 SOC.
    
    Signed-off-by: Bai Ping <b51503@freescale.com>
    Signed-off-by: Shawn Guo <shawnguo@kernel.org>

--- arch/arm/mach-imx/mach-imx6ul.c
+++ arch/arm/mach-imx/mach-imx6ul.c
@@ -78,6 +78,12 @@ static void __init imx6ul_init_irq(void)
 	imx6_pm_ccm_init("fsl,imx6ul-ccm");
 }
 
+static void __init imx6ul_init_late(void)
+{
+	if (IS_ENABLED(CONFIG_ARM_IMX6Q_CPUFREQ))
+		platform_device_register_simple("imx6q-cpufreq", -1, NULL, 0);
+}
+
 static const char *imx6ul_dt_compat[] __initconst = {
 	"fsl,imx6ul",
 	NULL,
@@ -86,5 +92,6 @@ static const char *imx6ul_dt_compat[] __initconst = {
 DT_MACHINE_START(IMX6UL, "Freescale i.MX6 Ultralite (Device Tree)")
 	.init_irq	= imx6ul_init_irq,
 	.init_machine	= imx6ul_init_machine,
+	.init_late	= imx6ul_init_late,
 	.dt_compat	= imx6ul_dt_compat,
 MACHINE_END
