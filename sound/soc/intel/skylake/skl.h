ba40a854ea4f9d81368dc023bd25ac4073058039
Dan Williams <dan.j.williams@intel.com>
2015-10-09 18:16:36 -0400

    ASoC: Intel: switch from ioremap_cache to memremap
    In preparation for deprecating ioremap_cache() convert its usage in
    skl-nhlt to memremap.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/skylake/skl.h
+++ sound/soc/intel/skylake/skl.h
@@ -62,7 +62,7 @@ struct skl {
 	unsigned int init_failed:1; /* delayed init failed */
 	struct platform_device *dmic_dev;
 
-	void __iomem *nhlt; /* nhlt ptr */
+	void *nhlt; /* nhlt ptr */
 	struct skl_sst *skl_sst; /* sst skl ctx */
 
 	struct skl_dsp_resource resource;
@@ -83,8 +83,8 @@ struct skl_dma_params {
 int skl_platform_unregister(struct device *dev);
 int skl_platform_register(struct device *dev);
 
-void __iomem *skl_nhlt_init(struct device *dev);
-void skl_nhlt_free(void __iomem *addr);
+void *skl_nhlt_init(struct device *dev);
+void skl_nhlt_free(void *addr);
 struct nhlt_specific_cfg *skl_get_ep_blob(struct skl *skl, u32 instance,
 			u8 link_type, u8 s_fmt, u8 no_ch, u32 s_rate, u8 dirn);
 
