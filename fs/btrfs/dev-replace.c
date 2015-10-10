730d9ec36bf27e87805aee6ebf462071735e31f9
David Sterba <dsterba@suse.com>
2015-10-10 18:16:38 +0200

    btrfs: remove waitqueue_active check from btrfs_rm_dev_replace_unblocked
    Normally the waitqueue_active would need a barrier, but this is not
    necessary here because it's not a performance sensitive context and we
    can call wake_up directly.
    
    Suggested-by: Chris Mason <clm@fb.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/dev-replace.c
+++ fs/btrfs/dev-replace.c
@@ -454,8 +454,7 @@ static void btrfs_rm_dev_replace_blocked(struct btrfs_fs_info *fs_info)
 static void btrfs_rm_dev_replace_unblocked(struct btrfs_fs_info *fs_info)
 {
 	clear_bit(BTRFS_FS_STATE_DEV_REPLACING, &fs_info->fs_state);
-	if (waitqueue_active(&fs_info->replace_wait))
-		wake_up(&fs_info->replace_wait);
+	wake_up(&fs_info->replace_wait);
 }
 
 static int btrfs_dev_replace_finishing(struct btrfs_fs_info *fs_info,
