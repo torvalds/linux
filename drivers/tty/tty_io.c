4b41b9539a1e9531f942ededfcdcff372317d2e7
Peter Hurley <peter@hurleysoftware.com>
2015-10-10 20:28:44 -0400

    tty: Prevent tty teardown during tty_write_message()
    tty_write_message() allows the caller to directly write to a specific
    tty. Since the line discipline is bypassed for the direct write,
    nothing prevents the tty from being torn down after the tty count is
    checked.
    
    Hold the tty lock for the duration of the direct write.
    
    Signed-off-by: Peter Hurley <peter@hurleysoftware.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/tty_io.c
+++ drivers/tty/tty_io.c
@@ -1200,11 +1200,9 @@ void tty_write_message(struct tty_struct *tty, char *msg)
 	if (tty) {
 		mutex_lock(&tty->atomic_write_lock);
 		tty_lock(tty);
-		if (tty->ops->write && tty->count > 0) {
-			tty_unlock(tty);
+		if (tty->ops->write && tty->count > 0)
 			tty->ops->write(tty, msg, strlen(msg));
-		} else
-			tty_unlock(tty);
+		tty_unlock(tty);
 		tty_write_unlock(tty);
 	}
 	return;
