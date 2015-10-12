d4583edb221eb7297bcbc6f0febe895a96fed5f7
Aaro Koskinen <aaro.koskinen@nokia.com>
2015-10-12 16:12:33 +0300

    staging: octeon-ethernet: xaui: don't register poll function in init
    Link status poll function is already controlled by open/stop functions,
    so we don't need to do it on init.
    
    This eliminates a redundant xaui link status notification when the module
    is loaded.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@nokia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/octeon/ethernet-xaui.c
+++ drivers/staging/octeon/ethernet-xaui.c
@@ -32,11 +32,7 @@ int cvm_oct_xaui_open(struct net_device *dev)
 
 int cvm_oct_xaui_init(struct net_device *dev)
 {
-	struct octeon_ethernet *priv = netdev_priv(dev);
-
 	cvm_oct_common_init(dev);
-	if (!octeon_is_simulation() && priv->phydev == NULL)
-		priv->poll = cvm_oct_link_poll;
 
 	return 0;
 }
