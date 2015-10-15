afb2b5ddac9f3727983030bc4450e9e3b5956b2a
sumit.saxena@avagotech.com <sumit.saxena@avagotech.com>
2015-10-15 13:41:04 +0530

    megaraid_sas : Driver version upgrade
    Signed-off-by: Sumit Saxena <sumit.saxena@avagotech.com>
    Reviewed-by: Tomas Henzl <thenzl@redhat.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/megaraid/megaraid_sas.h
+++ drivers/scsi/megaraid/megaraid_sas.h
@@ -35,8 +35,8 @@
 /*
  * MegaRAID SAS Driver meta data
  */
-#define MEGASAS_VERSION				"06.808.14.00-rc1"
-#define MEGASAS_RELDATE				"Jul 31, 2015"
+#define MEGASAS_VERSION				"06.808.16.00-rc1"
+#define MEGASAS_RELDATE				"Oct. 8, 2015"
 
 /*
  * Device IDs
