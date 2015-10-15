997f69fb4a95b668bd6d04e14c7af1a218351bf4
Insu Yun <wuninsu@gmail.com>
2015-10-15 17:22:45 -0300

    [media] mxl111sf: missing return values validation
    Return values of mxl111sf_enable_usb_output and mxl1x1sf_top_master_ctrl
    are not validated.
    
    Signed-off-by: Insu Yun <wuninsu@gmail.com>
    Reviewed-by: Michael Ira Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/usb/dvb-usb-v2/mxl111sf.c
+++ drivers/media/usb/dvb-usb-v2/mxl111sf.c
@@ -288,9 +288,9 @@ static int mxl111sf_adap_fe_init(struct dvb_frontend *fe)
 	err = mxl1x1sf_set_device_mode(state, adap_state->device_mode);
 
 	mxl_fail(err);
-	mxl111sf_enable_usb_output(state);
+	err = mxl111sf_enable_usb_output(state);
 	mxl_fail(err);
-	mxl1x1sf_top_master_ctrl(state, 1);
+	err = mxl1x1sf_top_master_ctrl(state, 1);
 	mxl_fail(err);
 
 	if ((MXL111SF_GPIO_MOD_DVBT != adap_state->gpio_mode) &&
