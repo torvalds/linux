a1ccdb63b5535dc3446b0a9efc6d97aca82c72ef
Michal Marek <mmarek@suse.com>
2015-10-15 15:12:51 +0200

    tags: Drop the _PE rule
    We are not indexing the userspace tools, so the rules only match some
    false positives in the kernel code.
    
    Signed-off-by: Michal Marek <mmarek@suse.com>

--- scripts/tags.sh
+++ scripts/tags.sh
@@ -189,7 +189,6 @@ exuberant()
 	--regex-c='/__CLEARPAGEFLAG_NOOP\(([^,)]*).*/__ClearPage\1/'	\
 	--regex-c='/TESTCLEARFLAG_FALSE\(([^,)]*).*/TestClearPage\1/' \
 	--regex-c='/__TESTCLEARFLAG_FALSE\(([^,)]*).*/__TestClearPage\1/' \
-	--regex-c='/_PE\(([^,)]*).*/PEVENT_ERRNO__\1/'		\
 	--regex-c='/TASK_PFA_TEST\([^,]*,\s*([^)]*)\)/task_\1/'	\
 	--regex-c='/TASK_PFA_SET\([^,]*,\s*([^)]*)\)/task_set_\1/'	\
 	--regex-c='/TASK_PFA_CLEAR\([^,]*,\s*([^)]*)\)/task_clear_\1/'\
@@ -253,7 +252,6 @@ emacs()
 	--regex='/TASK_PFA_TEST\([^,]*,\s*([^)]*)\)/task_\1/'		\
 	--regex='/TASK_PFA_SET\([^,]*,\s*([^)]*)\)/task_set_\1/'	\
 	--regex='/TASK_PFA_CLEAR\([^,]*,\s*([^)]*)\)/task_clear_\1/'	\
-	--regex='/_PE(\([^,)]*\).*/PEVENT_ERRNO__\1/'		\
 	--regex='/PCI_OP_READ(\([a-z]*[a-z]\).*[1-4])/pci_bus_read_config_\1/' \
 	--regex='/PCI_OP_WRITE(\([a-z]*[a-z]\).*[1-4])/pci_bus_write_config_\1/'\
 	--regex='/[^#]*DEFINE_HASHTABLE(\([^,)]*\)/\1/'
