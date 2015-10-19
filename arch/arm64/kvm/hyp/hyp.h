1431af367e52b08038e78d346822966d968f1694
Marc Zyngier <marc.zyngier@arm.com>
2015-10-19 16:32:20 +0100

    arm64: KVM: Implement timer save/restore
    Implement the timer save restore as a direct translation of
    the assembly code version.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm64/kvm/hyp/hyp.h
+++ arch/arm64/kvm/hyp/hyp.h
@@ -35,5 +35,8 @@ void __vgic_v2_restore_state(struct kvm_vcpu *vcpu);
 void __vgic_v3_save_state(struct kvm_vcpu *vcpu);
 void __vgic_v3_restore_state(struct kvm_vcpu *vcpu);
 
+void __timer_save_state(struct kvm_vcpu *vcpu);
+void __timer_restore_state(struct kvm_vcpu *vcpu);
+
 #endif /* __ARM64_KVM_HYP_H__ */
 
