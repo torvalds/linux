771788078a5c20bb1979e3ad2a6ef6a78e1044f9
Leo Kim <leo.kim@atmel.com>
2015-10-19 18:26:11 +0900

    staging: wilc1000: fixes unnecessary variable replacement
    This patch removes an unnecessary variable use for the errors and
    returns errors directly without a result variable.
    
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Tony Cho <tony.cho@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.c
+++ drivers/staging/wilc1000/host_interface.c
@@ -3167,9 +3167,8 @@ s32 host_int_add_wep_key_bss_sta(struct host_if_drv *hif_drv,
 	struct host_if_msg msg;
 
 	if (!hif_drv) {
-		result = -EFAULT;
 		PRINT_ER("driver is null\n");
-		return result;
+		return -EFAULT;
 	}
 
 	memset(&msg, 0, sizeof(struct host_if_msg));
@@ -3204,9 +3203,8 @@ s32 host_int_add_wep_key_bss_ap(struct host_if_drv *hif_drv,
 	u8 i;
 
 	if (!hif_drv) {
-		result = -EFAULT;
 		PRINT_ER("driver is null\n");
-		return result;
+		return -EFAULT;
 	}
 
 	memset(&msg, 0, sizeof(struct host_if_msg));
@@ -3246,9 +3244,8 @@ s32 host_int_add_ptk(struct host_if_drv *hif_drv, const u8 *pu8Ptk,
 	u32 i;
 
 	if (!hif_drv) {
-		result = -EFAULT;
 		PRINT_ER("driver is null\n");
-		return result;
+		return -EFAULT;
 	}
 	if (pu8RxMic != NULL)
 		u8KeyLen += RX_MIC_KEY_LEN;
@@ -3311,9 +3308,8 @@ s32 host_int_add_rx_gtk(struct host_if_drv *hif_drv, const u8 *pu8RxGtk,
 	u8 u8KeyLen = u8GtkKeylen;
 
 	if (!hif_drv) {
-		result = -EFAULT;
 		PRINT_ER("driver is null\n");
-		return result;
+		return -EFAULT;
 	}
 	memset(&msg, 0, sizeof(struct host_if_msg));
 
@@ -3370,9 +3366,8 @@ s32 host_int_set_pmkid_info(struct host_if_drv *hif_drv, struct host_if_pmkid_at
 
 
 	if (!hif_drv) {
-		result = -EFAULT;
 		PRINT_ER("driver is null\n");
-		return result;
+		return -EFAULT;
 	}
 
 	memset(&msg, 0, sizeof(struct host_if_msg));
@@ -3514,9 +3509,8 @@ s32 host_int_set_join_req(struct host_if_drv *hif_drv, u8 *pu8bssid,
 	struct host_if_msg msg;
 
 	if (!hif_drv || pfConnectResult == NULL) {
-		result = -EFAULT;
 		PRINT_ER("Driver is null\n");
-		return result;
+		return -EFAULT;
 	}
 
 	if (pJoinParams == NULL) {
@@ -3575,16 +3569,12 @@ s32 host_int_flush_join_req(struct host_if_drv *hif_drv)
 	s32 result = 0;
 	struct host_if_msg msg;
 
-	if (!join_req) {
-		result = -EFAULT;
-		return result;
-	}
-
+	if (!join_req)
+		return -EFAULT;
 
 	if (!hif_drv) {
-		result = -EFAULT;
 		PRINT_ER("Driver is null\n");
-		return result;
+		return -EFAULT;
 	}
 
 	msg.id = HOST_IF_MSG_FLUSH_CONNECT;
