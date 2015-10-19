c008fa83c416bbdcca9a49fb2d6f3fbc50daf421
Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
2015-10-19 22:00:19 +0200

    staging: rtl8192e: cmdpkt: Pass data as const void*
    Signed-off-by: Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/r8192E_cmdpkt.c
+++ drivers/staging/rtl8192e/rtl8192e/r8192E_cmdpkt.c
@@ -17,7 +17,8 @@
 #include "r8192E_hw.h"
 #include "r8192E_cmdpkt.h"
 
-bool rtl92e_send_cmd_pkt(struct net_device *dev, u32 type, u8 *data, u32 len)
+bool rtl92e_send_cmd_pkt(struct net_device *dev, u32 type, const void *data,
+			 u32 len)
 {
 
 	bool				rt_status = true;
