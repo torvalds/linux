b31968828352ecae41d47aaa703e16c1ba06bfd8
Bob Moore <robert.moore@intel.com>
2015-10-19 10:26:08 +0800

    ACPICA: Update version to 20150930
    ACPICA commit e9c75ca267262326e80d49a290e8387a5963e2d2
    
    Version 20150930.
    
    Link: https://github.com/acpica/acpica/commit/e9c75ca2
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/acpixf.h
+++ include/acpi/acpixf.h
@@ -46,7 +46,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20150818
+#define ACPI_CA_VERSION                 0x20150930
 
 #include <acpi/acconfig.h>
 #include <acpi/actypes.h>
