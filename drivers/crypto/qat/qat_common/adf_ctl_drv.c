b0c8bc1b9d8027093f8506266ab06c5cf0e5199f
Tadeusz Struk <tadeusz.struk@intel.com>
2015-10-20 12:50:03 -0700

    crypto: qat - when stopping all devices make fure VF are stopped first
    When stopping all devices make sure VFs are stopped before the
    corresponding PF.
    VFs will always be after PF so just need to loop back.
    
    Signed-off-by: Tadeusz Struk <tadeusz.struk@intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/qat/qat_common/adf_ctl_drv.c
+++ drivers/crypto/qat/qat_common/adf_ctl_drv.c
@@ -278,7 +278,7 @@ static int adf_ctl_stop_devices(uint32_t id)
 	struct list_head *itr, *head = adf_devmgr_get_head();
 	int ret = 0;
 
-	list_for_each(itr, head) {
+	list_for_each_prev(itr, head) {
 		struct adf_accel_dev *accel_dev =
 				list_entry(itr, struct adf_accel_dev, list);
 		if (id == accel_dev->accel_id || id == ADF_CFG_ALL_DEVICES) {
