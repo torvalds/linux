a519435a96597d8cd96123246fea4ae5a6c90b02
Christian König <christian.koenig@amd.com>
2015-10-20 16:34:16 +0200

    dma-buf/fence: add fence_wait_any_timeout function v2
    Waiting for the first fence in an array of fences to signal.
    
    This is useful for device driver specific resource managers
    and also Vulkan needs something similar.
    
    v2: more parameter checks, handling for timeout==0,
        remove NULL entry support, better callback removal.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>

--- include/linux/fence.h
+++ include/linux/fence.h
@@ -305,7 +305,8 @@ static inline struct fence *fence_later(struct fence *f1, struct fence *f2)
 }
 
 signed long fence_wait_timeout(struct fence *, bool intr, signed long timeout);
-
+signed long fence_wait_any_timeout(struct fence **fences, uint32_t count,
+				   bool intr, signed long timeout);
 
 /**
  * fence_wait - sleep until the fence gets signaled
