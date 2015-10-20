d4182e35aa4ae358c024ca3d370fb3cc3da30eb7
Luis de Bethencourt <luisbg@osg.samsung.com>
2015-10-20 15:48:31 +0100

    drm: omapdrm: tiler: Remove unneded module alias for tiler
    omap_dmm_tiler.c can't be compiled as a module and it is built
    unconditionally as part of omapdrm. Since it can't be used as a module,
    there is no need for it to have an unused MODULE_ALIAS().
    
    Signed-off-by: Luis de Bethencourt <luisbg@osg.samsung.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/omapdrm/omap_dmm_tiler.c
+++ drivers/gpu/drm/omapdrm/omap_dmm_tiler.c
@@ -1030,4 +1030,3 @@ struct platform_driver omap_dmm_driver = {
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Andy Gross <andy.gross@ti.com>");
 MODULE_DESCRIPTION("OMAP DMM/Tiler Driver");
-MODULE_ALIAS("platform:" DMM_DRIVER_NAME);
