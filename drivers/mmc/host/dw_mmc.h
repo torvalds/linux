98daafd8a09d9a514bc2166709731d659037003b
Jaehoon Chung <jh80.chung@samsung.com>
2015-10-21 19:49:41 +0900

    mmc: dw_mmc: fix the CardThreshold boundary at CardThrCtl register
    According to DesignWare DoC file, CardThreshold bit should be
    bit[27:16].
    So it's correct to use (0xFFF << 16), not (0x1FFF << 16).
    
    Signed-off-by: Jaehoon Chung <jh80.chung@samsung.com>
    Reviewed-by: Alim Akhtar <alim.akhtar@samsung.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/dw_mmc.h
+++ drivers/mmc/host/dw_mmc.h
@@ -172,7 +172,7 @@
 /* Version ID register define */
 #define SDMMC_GET_VERID(x)		((x) & 0xFFFF)
 /* Card read threshold */
-#define SDMMC_SET_RD_THLD(v, x)		(((v) & 0x1FFF) << 16 | (x))
+#define SDMMC_SET_RD_THLD(v, x)		(((v) & 0xFFF) << 16 | (x))
 #define SDMMC_UHS_18V			BIT(0)
 /* All ctrl reset bits */
 #define SDMMC_CTRL_ALL_RESET_FLAGS \
