eb99dec6d1e3b75580d312b1d29a18c39ed1e7ad
Marc Zyngier <marc.zyngier@arm.com>
2015-10-21 10:09:49 +0100

    arm64: KVM: Add a HYP-specific header file
    In order to expose the various EL2 services that are private to
    the hypervisor, add a new hyp.h file.
    
    So far, it only contains mundane things such as section annotation
    and VA manipulation.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    (cherry picked from commit c76a0a6695c61088c8d2e731e25305502666bf7d)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>

---
+++ arch/arm64/kvm/hyp/hyp.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2015 - ARM Ltd
+ * Author: Marc Zyngier <marc.zyngier@arm.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ARM64_KVM_HYP_H__
+#define __ARM64_KVM_HYP_H__
+
+#include <linux/compiler.h>
+#include <linux/kvm_host.h>
+#include <asm/kvm_mmu.h>
+#include <asm/sysreg.h>
+
+#define __hyp_text __section(.hyp.text) notrace
+
+#define kern_hyp_va(v) (typeof(v))((unsigned long)(v) & HYP_PAGE_OFFSET_MASK)
+#define hyp_kern_va(v) (typeof(v))((unsigned long)(v) - HYP_PAGE_OFFSET \
+						      + PAGE_OFFSET)
+
+#endif /* __ARM64_KVM_HYP_H__ */
+
