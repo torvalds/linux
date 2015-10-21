1d2a168a085f1c65b895f258ee11a52813d25af6
Geliang Tang <geliangtang@163.com>
2015-10-21 17:44:27 -0400

    selinux: ioctl_has_perm should be static
    Fixes the following sparse warning:
    
     security/selinux/hooks.c:3242:5: warning: symbol 'ioctl_has_perm' was
     not declared. Should it be static?
    
    Signed-off-by: Geliang Tang <geliangtang@163.com>
    Acked-by: Jeff Vander Stoep <jeffv@google.com>
    Acked-by: Stephen Smalley <sds@tycho.nsa.gov>
    Signed-off-by: Paul Moore <pmoore@redhat.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -3235,7 +3235,7 @@ static void selinux_file_free_security(struct file *file)
  * Check whether a task has the ioctl permission and cmd
  * operation to an inode.
  */
-int ioctl_has_perm(const struct cred *cred, struct file *file,
+static int ioctl_has_perm(const struct cred *cred, struct file *file,
 		u32 requested, u16 cmd)
 {
 	struct common_audit_data ad;
