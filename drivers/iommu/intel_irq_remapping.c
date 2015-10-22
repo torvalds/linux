a13c8f27e4d79420ae7bcb48123fa13851026428
Baoquan He <bhe@redhat.com>
2015-10-22 14:00:51 +0800

    iommu/vt-d: Adjust the return value of the parse_ioapics_under_ir
    Adjust the return value of parse_ioapics_under_ir as
    negative value representing failure and "0" representing
    succcess. Just make it consistent with other function
    implementations, and we can judge if calling is successfull
    by if (!parse_ioapics_under_ir()) style.
    
    Signed-off-by: Baoquan He <bhe@redhat.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/intel_irq_remapping.c
+++ drivers/iommu/intel_irq_remapping.c
@@ -690,7 +690,7 @@ static int __init intel_prepare_irq_remapping(void)
 	if (!dmar_ir_support())
 		return -ENODEV;
 
-	if (parse_ioapics_under_ir() != 1) {
+	if (!parse_ioapics_under_ir()) {
 		pr_info("Not enabling interrupt remapping\n");
 		goto error;
 	}
@@ -943,7 +943,7 @@ static int __init parse_ioapics_under_ir(void)
 		}
 
 	if (!ir_supported)
-		return 0;
+		return -ENODEV;
 
 	for (ioapic_idx = 0; ioapic_idx < nr_ioapics; ioapic_idx++) {
 		int ioapic_id = mpc_ioapic_id(ioapic_idx);
@@ -955,7 +955,7 @@ static int __init parse_ioapics_under_ir(void)
 		}
 	}
 
-	return 1;
+	return 0;
 }
 
 static int __init ir_dev_scope_init(void)
