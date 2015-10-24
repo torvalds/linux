efb1cf7d28b8aeacec53e9ba8f3f2809c5cb9686
Chen Yu <yu.c.chen@intel.com>
2015-10-25 01:02:36 +0800

    ACPI: Using correct irq when waiting for events
    When the system is waiting for GPE/fixed event handler to finish,
    it uses acpi_gbl_FADT.sci_interrupt directly as the IRQ number.
    However, the remapped IRQ returned by acpi_gsi_to_irq() should be
    passed to synchronize_hardirq() instead of it.
    
    Cc: 3.19+ <stable@vger.kernel.org> # 3.19+
    Acked-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/osl.c
+++ drivers/acpi/osl.c
@@ -1183,8 +1183,8 @@ void acpi_os_wait_events_complete(void)
 	 * Make sure the GPE handler or the fixed event handler is not used
 	 * on another CPU after removal.
 	 */
-	if (acpi_irq_handler)
-		synchronize_hardirq(acpi_gbl_FADT.sci_interrupt);
+	if (acpi_sci_irq_valid())
+		synchronize_hardirq(acpi_sci_irq);
 	flush_workqueue(kacpid_wq);
 	flush_workqueue(kacpi_notify_wq);
 }
