e3b6e95d070cbca5e82279fea99e2dba8e38f960
Jacob Keller <jacob.e.keller@intel.com>
2015-10-26 14:38:40 -0700

    fm10k: bump driver version
    We haven't bumped the driver version in a while despite many fixes being
    pulled in from the out-of-tree Sourceforge driver. Update the version to
    match.
    
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    Tested-by: Krishneil Singh <Krishneil.k.singh@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/fm10k/fm10k_main.c
+++ drivers/net/ethernet/intel/fm10k/fm10k_main.c
@@ -28,7 +28,7 @@
 
 #include "fm10k.h"
 
-#define DRV_VERSION	"0.15.2-k"
+#define DRV_VERSION	"0.19.3-k"
 const char fm10k_driver_version[] = DRV_VERSION;
 char fm10k_driver_name[] = "fm10k";
 static const char fm10k_driver_string[] =
