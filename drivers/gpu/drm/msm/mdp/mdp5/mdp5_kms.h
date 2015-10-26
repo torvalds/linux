e2dd9f9ffa7fff1d56d331b08cce72781f65aa52
Archit Taneja <architt@codeaurora.org>
2015-10-26 14:43:57 +0530

    drm/msm/mdp5: high precision vblank timestamp support
    MDP5 has line count and frame count registers for each interface. Enable
    these counters and use them to implement the get_vblank_timestamp drm
    driver op.
    
    The line counter starts with the value 1 at the beginning of the VSYNC
    pulse and ends with value VTOTAL at the end of VFP. This value is used
    to determine whether we're in blanking period or not, and an adjusted
    value of this counter is used to get vpos as expected by
    get_scanout_position. Since there is no way to calculate hpos, we always
    set it to 0.
    
    Signed-off-by: Archit Taneja <architt@codeaurora.org>
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/mdp/mdp5/mdp5_kms.h
+++ drivers/gpu/drm/msm/mdp/mdp5/mdp5_kms.h
@@ -222,6 +222,8 @@ struct drm_encoder *mdp5_encoder_init(struct drm_device *dev,
 		struct mdp5_interface *intf, struct mdp5_ctl *ctl);
 int mdp5_encoder_set_split_display(struct drm_encoder *encoder,
 					struct drm_encoder *slave_encoder);
+int mdp5_encoder_get_linecount(struct drm_encoder *encoder);
+u32 mdp5_encoder_get_framecount(struct drm_encoder *encoder);
 
 #ifdef CONFIG_DRM_MSM_DSI
 struct drm_encoder *mdp5_cmd_encoder_init(struct drm_device *dev,
