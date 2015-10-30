c3cdad8899c6608a2e928e1ce4fcaaddbf17cc96
Shraddha Barke <shraddha.6596@gmail.com>
2015-10-30 20:59:04 +0530

    Staging: lustre: mdc: Remove unused mdc_open function from header
    Function mdc_open is declared in header file but it is not used anywhere.
    Hence drop the declaration.
    
    Signed-off-by: Shraddha Barke <shraddha.6596@gmail.com>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/mdc/mdc_internal.h
+++ drivers/staging/lustre/lustre/mdc/mdc_internal.h
@@ -97,11 +97,6 @@ int mdc_resource_get_unused(struct obd_export *exp, const struct lu_fid *fid,
 /* mdc/mdc_request.c */
 int mdc_fid_alloc(struct obd_export *exp, struct lu_fid *fid,
 		  struct md_op_data *op_data);
-
-int mdc_open(struct obd_export *exp, u64 ino, int type, int flags,
-	     struct lov_mds_md *lmm, int lmm_size, struct lustre_handle *fh,
-	     struct ptlrpc_request **);
-
 struct obd_client_handle;
 
 int mdc_set_open_replay_data(struct obd_export *exp,
