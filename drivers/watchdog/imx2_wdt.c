0b8bcbd3618bfb24f1552be7e251bef6a3eca411
Robin Gong <b38343@freescale.com>
2015-11-02 18:09:43 +0800

    MLK-11809: watchdog: imx2_wdt: correct timeout after set_timeout()
    Correct wdog->timeout after set_timeout(), otherwise, the WDIOC_GETTIMEOUT
    will return the default value '60s' always, correct it and also for the new
    wdog->pretimeout.
    
    Signed-off-by: Robin Gong <b38343@freescale.com>

--- drivers/watchdog/imx2_wdt.c
+++ drivers/watchdog/imx2_wdt.c
@@ -181,6 +181,9 @@ static int imx2_wdt_set_timeout(struct watchdog_device *wdog,
 
 	regmap_update_bits(wdev->regmap, IMX2_WDT_WCR, IMX2_WDT_WCR_WT,
 			   WDOG_SEC_TO_COUNT(new_timeout));
+
+	wdog->timeout = new_timeout;
+
 	return 0;
 }
 
@@ -240,6 +243,8 @@ static int imx2_wdt_set_pretimeout(struct watchdog_device *wdog, unsigned int ne
 		val |= IMX2_WDT_WICR_WIE;	/*enable*/
 
 	regmap_write(wdev->regmap, IMX2_WDT_WICR, val);
+
+	wdog->pretimeout = new_timeout;
 
 	return 0;
 }
