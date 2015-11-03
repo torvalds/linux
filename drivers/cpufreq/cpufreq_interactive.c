e197a21971a5bfb9d335a9baa701c70b973fdab4
Amit Pundir <amit.pundir@linaro.org>
2015-11-03 20:53:29 +0530

    cpufreq: interactive: replace strict_strtoul() with kstrtoul()
    strict_strtoul() is obsolete. Use kstrtoul() instead.
    Otherwise we run into following build error:
    
      CC      drivers/cpufreq/cpufreq_interactive.o
    drivers/cpufreq/cpufreq_interactive.c: In function ‘store_hispeed_freq’:
    drivers/cpufreq/cpufreq_interactive.c:784:2: error: implicit declaration of function ‘strict_strtoul’ [-Werror=implicit-function-declaration]
    cc1: some warnings being treated as errors
    make[2]: *** [drivers/cpufreq/cpufreq_interactive.o] Error 1
    
    Change-Id: Ib91b9df3af5fe2a244861c2f598bd20ec8115e6c
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -781,7 +781,7 @@ static ssize_t store_hispeed_freq(struct cpufreq_interactive_tunables *tunables,
 	int ret;
 	long unsigned int val;
 
-	ret = strict_strtoul(buf, 0, &val);
+	ret = kstrtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
 	tunables->hispeed_freq = val;
@@ -800,7 +800,7 @@ static ssize_t store_go_hispeed_load(struct cpufreq_interactive_tunables
 	int ret;
 	unsigned long val;
 
-	ret = strict_strtoul(buf, 0, &val);
+	ret = kstrtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
 	tunables->go_hispeed_load = val;
@@ -819,7 +819,7 @@ static ssize_t store_min_sample_time(struct cpufreq_interactive_tunables
 	int ret;
 	unsigned long val;
 
-	ret = strict_strtoul(buf, 0, &val);
+	ret = kstrtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
 	tunables->min_sample_time = val;
@@ -838,7 +838,7 @@ static ssize_t store_timer_rate(struct cpufreq_interactive_tunables *tunables,
 	int ret;
 	unsigned long val, val_round;
 
-	ret = strict_strtoul(buf, 0, &val);
+	ret = kstrtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
 
