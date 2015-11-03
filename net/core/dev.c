fd867d51f889aec11cca235ebb008578780d052d
Jarod Wilson <jarod@redhat.com>
2015-11-02 21:55:59 -0500

    net/core: generic support for disabling netdev features down stack
    There are some netdev features, which when disabled on an upper device,
    such as a bonding master or a bridge, must be disabled and cannot be
    re-enabled on underlying devices.
    
    This is a rework of an earlier more heavy-handed appraoch, which simply
    disables and prevents re-enabling of netdev features listed in a new
    define in include/net/netdev_features.h, NETIF_F_UPPER_DISABLES. Any upper
    device that disables a flag in that feature mask, the disabling will
    propagate down the stack, and any lower device that has any upper device
    with one of those flags disabled should not be able to enable said flag.
    
    Initially, only LRO is included for proof of concept, and because this
    code effectively does the same thing as dev_disable_lro(), though it will
    also activate from the ethtool path, which was one of the goals here.
    
    [root@dell-per730-01 ~]# ethtool -k bond0 |grep large
    large-receive-offload: on
    [root@dell-per730-01 ~]# ethtool -k p5p1 |grep large
    large-receive-offload: on
    [root@dell-per730-01 ~]# ethtool -K bond0 lro off
    [root@dell-per730-01 ~]# ethtool -k bond0 |grep large
    large-receive-offload: off
    [root@dell-per730-01 ~]# ethtool -k p5p1 |grep large
    large-receive-offload: off
    
    dmesg dump:
    
    [ 1033.277986] bond0: Disabling feature 0x0000000000008000 on lower dev p5p2.
    [ 1034.067949] bnx2x 0000:06:00.1 p5p2: using MSI-X  IRQs: sp 74  fp[0] 76 ... fp[7] 83
    [ 1034.753612] bond0: Disabling feature 0x0000000000008000 on lower dev p5p1.
    [ 1035.591019] bnx2x 0000:06:00.0 p5p1: using MSI-X  IRQs: sp 62  fp[0] 64 ... fp[7] 71
    
    This has been successfully tested with bnx2x, qlcnic and netxen network
    cards as slaves in a bond interface. Turning LRO on or off on the master
    also turns it on or off on each of the slaves, new slaves are added with
    LRO in the same state as the master, and LRO can't be toggled on the
    slaves.
    
    Also, this should largely remove the need for dev_disable_lro(), and most,
    if not all, of its call sites can be replaced by simply making sure
    NETIF_F_LRO isn't included in the relevant device's feature flags.
    
    Note that this patch is driven by bug reports from users saying it was
    confusing that bonds and slaves had different settings for the same
    features, and while it won't be 100% in sync if a lower device doesn't
    support a feature like LRO, I think this is a good step in the right
    direction.
    
    CC: "David S. Miller" <davem@davemloft.net>
    CC: Eric Dumazet <edumazet@google.com>
    CC: Jay Vosburgh <j.vosburgh@gmail.com>
    CC: Veaceslav Falico <vfalico@gmail.com>
    CC: Andy Gospodarek <gospo@cumulusnetworks.com>
    CC: Jiri Pirko <jiri@resnulli.us>
    CC: Nikolay Aleksandrov <razor@blackwall.org>
    CC: Michal Kubecek <mkubecek@suse.cz>
    CC: Alexander Duyck <alexander.duyck@gmail.com>
    CC: netdev@vger.kernel.org
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -6288,6 +6288,44 @@ static void rollback_registered(struct net_device *dev)
 	list_del(&single);
 }
 
+static netdev_features_t netdev_sync_upper_features(struct net_device *lower,
+	struct net_device *upper, netdev_features_t features)
+{
+	netdev_features_t upper_disables = NETIF_F_UPPER_DISABLES;
+	netdev_features_t feature;
+
+	for_each_netdev_feature(&upper_disables, feature) {
+		if (!(upper->wanted_features & feature)
+		    && (features & feature)) {
+			netdev_dbg(lower, "Dropping feature %pNF, upper dev %s has it off.\n",
+				   &feature, upper->name);
+			features &= ~feature;
+		}
+	}
+
+	return features;
+}
+
+static void netdev_sync_lower_features(struct net_device *upper,
+	struct net_device *lower, netdev_features_t features)
+{
+	netdev_features_t upper_disables = NETIF_F_UPPER_DISABLES;
+	netdev_features_t feature;
+
+	for_each_netdev_feature(&upper_disables, feature) {
+		if (!(features & feature) && (lower->features & feature)) {
+			netdev_dbg(upper, "Disabling feature %pNF on lower dev %s.\n",
+				   &feature, lower->name);
+			lower->wanted_features &= ~feature;
+			netdev_update_features(lower);
+
+			if (unlikely(lower->features & feature))
+				netdev_WARN(upper, "failed to disable %pNF on %s!\n",
+					    &feature, lower->name);
+		}
+	}
+}
+
 static netdev_features_t netdev_fix_features(struct net_device *dev,
 	netdev_features_t features)
 {
@@ -6357,7 +6395,9 @@ static netdev_features_t netdev_fix_features(struct net_device *dev,
 
 int __netdev_update_features(struct net_device *dev)
 {
+	struct net_device *upper, *lower;
 	netdev_features_t features;
+	struct list_head *iter;
 	int err = 0;
 
 	ASSERT_RTNL();
@@ -6369,6 +6409,10 @@ int __netdev_update_features(struct net_device *dev)
 
 	/* driver might be less strict about feature dependencies */
 	features = netdev_fix_features(dev, features);
+
+	/* some features can't be enabled if they're off an an upper device */
+	netdev_for_each_upper_dev_rcu(dev, upper, iter)
+		features = netdev_sync_upper_features(dev, upper, features);
 
 	if (dev->features == features)
 		return 0;
@@ -6385,6 +6429,12 @@ int __netdev_update_features(struct net_device *dev)
 			err, &features, &dev->features);
 		return -1;
 	}
+
+	/* some features must be disabled on lower devices when disabled
+	 * on an upper device (think: bonding master or bridge)
+	 */
+	netdev_for_each_lower_dev(dev, lower, iter)
+		netdev_sync_lower_features(dev, lower, features);
 
 	if (!err)
 		dev->features = features;
