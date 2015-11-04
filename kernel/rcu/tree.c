e11f13355b09df970495c45ed0eac1dc85dcf5c1
Peter Zijlstra <peterz@infradead.org>
2015-11-04 08:22:05 -0800

    rcu: Move wakeup out from under rnp->lock
    This patch removes a potential deadlock hazard by moving the
    wake_up_process() in rcu_spawn_gp_kthread() out from under rnp->lock.
    
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -4319,8 +4319,8 @@ static int __init rcu_spawn_gp_kthread(void)
 			sp.sched_priority = kthread_prio;
 			sched_setscheduler_nocheck(t, SCHED_FIFO, &sp);
 		}
-		wake_up_process(t);
 		raw_spin_unlock_irqrestore(&rnp->lock, flags);
+		wake_up_process(t);
 	}
 	rcu_spawn_nocb_kthreads();
 	rcu_spawn_boost_kthreads();
