9fcf836b215ca5685030ecab3e35ecc14ee3bcfb
Yaowei Bai <bywxiaobai@163.com>
2015-11-04 08:23:51 -0500

    audit: audit_string_contains_control can be boolean
    This patch makes audit_string_contains_control return bool to improve
    readability due to this particular function only using either one or
    zero as its return value.
    
    Signed-off-by: Yaowei Bai <bywxiaobai@163.com>
    [PM: tweaked subject line]
    Signed-off-by: Paul Moore <pmoore@redhat.com>

--- include/linux/audit.h
+++ include/linux/audit.h
@@ -457,7 +457,7 @@ extern struct audit_buffer *audit_log_start(struct audit_context *ctx, gfp_t gfp
 extern __printf(2, 3)
 void audit_log_format(struct audit_buffer *ab, const char *fmt, ...);
 extern void		    audit_log_end(struct audit_buffer *ab);
-extern int		    audit_string_contains_control(const char *string,
+extern bool		    audit_string_contains_control(const char *string,
 							  size_t len);
 extern void		    audit_log_n_hex(struct audit_buffer *ab,
 					  const unsigned char *buf,
--- kernel/audit.c
+++ kernel/audit.c
@@ -1583,14 +1583,14 @@ void audit_log_n_string(struct audit_buffer *ab, const char *string,
  * @string: string to be checked
  * @len: max length of the string to check
  */
-int audit_string_contains_control(const char *string, size_t len)
+bool audit_string_contains_control(const char *string, size_t len)
 {
 	const unsigned char *p;
 	for (p = string; p < (const unsigned char *)string + len; p++) {
 		if (*p == '"' || *p < 0x21 || *p > 0x7e)
-			return 1;
+			return true;
 	}
-	return 0;
+	return false;
 }
 
 /**
