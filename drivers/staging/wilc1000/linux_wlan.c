3d6d8cd890afa9c2bcdc19e5b77e450717f1c7e7
Leo Kim <leo.kim@atmel.com>
2015-11-05 14:36:23 +0900

    staging: wilc1000: remove do-nothing if condition case.
    This patch removes do-nothing if condition case.
    
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Glen Lee <glen.lee@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/linux_wlan.c
+++ drivers/staging/wilc1000/linux_wlan.c
@@ -326,9 +326,6 @@ void linux_wlan_mac_indicate(struct wilc *wilc, int flag)
 		} else {
 			wilc->mac_status = status;
 		}
-
-		if (wilc->mac_status == WILC_MAC_STATUS_CONNECT) {
-		}
 	} else if (flag == WILC_MAC_INDICATE_SCAN) {
 		PRINT_D(GENERIC_DBG, "Scanning ...\n");
 	}
