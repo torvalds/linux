a99ac0d9f6fd5f50ebf5cdab84ab9f855c46cdb2
Tomi Valkeinen <tomi.valkeinen@ti.com>
2015-11-05 17:19:32 +0200

    drm/omap: add omapdss-base.ko
    We are working towards enabling omapdss6, which will consists of a new
    dss, dispc and dpi drivers. omapdss6 will be a new module. The panel,
    encoder and omapdrm will need to use either the current omapdss driver
    or the new omapdss6 driver, depending on the platform.
    
    This will be implemented with a common base module and function
    pointers.
    
    This patch adds a skeleton omapdss-base.ko module, to which we'll be
    moving common dss functionality like registration of the panels.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

---
+++ drivers/gpu/drm/omapdrm/dss/base.c
@@ -0,0 +1,6 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+MODULE_AUTHOR("Tomi Valkeinen <tomi.valkeinen@ti.com>");
+MODULE_DESCRIPTION("OMAP Display Subsystem Base");
+MODULE_LICENSE("GPL v2");
