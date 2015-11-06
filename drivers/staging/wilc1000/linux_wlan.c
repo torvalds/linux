b4b87a0b1278c872b0873ce285b25e60ca5bbe42
Glen Lee <glen.lee@atmel.com>
2015-11-06 18:40:12 +0900

    staging: wilc1000: remove function pointer spi_rx of wilc_wlan_io_func_t
    This patch removes spi_rx of wilc_wlan_io_func_t and it's related codes since
    it is not used anymore.
    
    Signed-off-by: Glen Lee <glen.lee@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/linux_wlan.c
+++ drivers/staging/wilc1000/linux_wlan.c
@@ -898,7 +898,6 @@ void linux_to_wlan(wilc_wlan_inp_t *nwi, struct wilc *nic)
 	nwi->io_func.io_type = HIF_SPI;
 	nwi->io_func.io_init = linux_spi_init;
 	nwi->io_func.io_deinit = linux_spi_deinit;
-	nwi->io_func.u.spi.spi_rx = linux_spi_read;
 	nwi->io_func.u.spi.spi_trx = linux_spi_write_read;
 	nwi->io_func.u.spi.spi_max_speed = linux_spi_set_max_speed;
 #endif
