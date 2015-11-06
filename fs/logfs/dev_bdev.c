ce4f2fd7eacd84d78530fb97621621ae50d167f1
Sudip Mukherjee <sudipm.mukherjee@gmail.com>
2015-11-05 18:43:49 -0800

    logfs: fix build warning
    fs/logfs/dev_bdev.c: In function '__bdev_writeseg':
    include/linux/kernel.h:601:17: warning: comparison of distinct pointer types lacks a cast [enabled by default]
      (void) (&_min1 == &_min2);  \
    fs/logfs/dev_bdev.c:84:14: note: in  expansion of macro 'min'
      max_pages = min(nr_pages, BIO_MAX_PAGES);
    
    fs/logfs/dev_bdev.c: In function 'do_erase':
    include/linux/kernel.h:601:17: warning: comparison of distinct pointer types lacks a cast [enabled by default]
     (void) (&_min1 == &_min2);  \
    fs/logfs/dev_bdev.c:174:14: note: in expansion of macro 'min'
     max_pages = min(nr_pages, BIO_MAX_PAGES);
    
    Lets use min_t and mention the type.
    
    Signed-off-by: Sudip Mukherjee <sudip@vectorindia.org>
    Cc: Joern Engel <joern@logfs.org>
    Cc: Prasad Joshi <prasadjoshi.linux@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/logfs/dev_bdev.c
+++ fs/logfs/dev_bdev.c
@@ -81,7 +81,7 @@ static int __bdev_writeseg(struct super_block *sb, u64 ofs, pgoff_t index,
 	unsigned int max_pages;
 	int i;
 
-	max_pages = min(nr_pages, BIO_MAX_PAGES);
+	max_pages = min_t(size_t, nr_pages, BIO_MAX_PAGES);
 
 	bio = bio_alloc(GFP_NOFS, max_pages);
 	BUG_ON(!bio);
@@ -171,7 +171,7 @@ static int do_erase(struct super_block *sb, u64 ofs, pgoff_t index,
 	unsigned int max_pages;
 	int i;
 
-	max_pages = min(nr_pages, BIO_MAX_PAGES);
+	max_pages = min_t(size_t, nr_pages, BIO_MAX_PAGES);
 
 	bio = bio_alloc(GFP_NOFS, max_pages);
 	BUG_ON(!bio);
