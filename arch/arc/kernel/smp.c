71f9cf8fdd72e5f5c3a60f632bd1534fc7996b81
Noam Camus <noamc@ezchip.com>
2015-11-07 09:16:19 +0200

    ARC: Mark secondary cpu online only after all HW setup is done
    In SMP setup, master loops for each_present_cpu calling cpu_up().
    For ARC it returns as soon as new cpu's status becomes online,
    However secondary may still do HW initializing,
    machine or platform hook level.
    
    So turn secondary online only after all HW setup is done.
    Signed-off-by: Noam Camus <noamc@ezchip.com>
    Acked-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/kernel/smp.c
+++ arch/arc/kernel/smp.c
@@ -126,17 +126,17 @@ void start_kernel_secondary(void)
 	current->active_mm = mm;
 	cpumask_set_cpu(cpu, mm_cpumask(mm));
 
-	notify_cpu_starting(cpu);
-	set_cpu_online(cpu, true);
-
-	pr_info("## CPU%u LIVE ##: Executing Code...\n", cpu);
-
 	/* Some SMP H/w setup - for each cpu */
 	if (plat_smp_ops.init_per_cpu)
 		plat_smp_ops.init_per_cpu(cpu);
 
 	if (machine_desc->init_per_cpu)
 		machine_desc->init_per_cpu(cpu);
+
+	notify_cpu_starting(cpu);
+	set_cpu_online(cpu, true);
+
+	pr_info("## CPU%u LIVE ##: Executing Code...\n", cpu);
 
 	local_irq_enable();
 	preempt_disable();
