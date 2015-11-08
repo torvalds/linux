84f82ed814196acbdc892cd2b98d3e696e6dd607
Chaehyun Lim <chaehyun.lim@gmail.com>
2015-11-08 16:49:06 +0900

    staging: wilc1000: fix argument name of host_int_add_rx_gtk
    This patch changes struct host_if_drv of host_int_add_rx_gtk function
    declaration from hWFIDrv to hif_drv.
    With this change, first argument of this function declaration and
    definition has same name as hif_drv.
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.h
+++ drivers/staging/wilc1000/host_interface.h
@@ -317,7 +317,7 @@ int host_int_add_ptk(struct host_if_drv *hif_drv, const u8 *ptk,
 		     u8 mode, u8 cipher_mode, u8 index);
 s32 host_int_get_inactive_time(struct host_if_drv *hWFIDrv, const u8 *mac,
 			       u32 *pu32InactiveTime);
-int host_int_add_rx_gtk(struct host_if_drv *hWFIDrv, const u8 *pu8RxGtk,
+int host_int_add_rx_gtk(struct host_if_drv *hif_drv, const u8 *pu8RxGtk,
 			u8 u8GtkKeylen,	u8 u8KeyIdx,
 			u32 u32KeyRSClen, const u8 *KeyRSC,
 			const u8 *pu8RxMic, const u8 *pu8TxMic,
