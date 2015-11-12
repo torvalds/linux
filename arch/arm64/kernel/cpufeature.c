3d6d103538e73a5710ea66d95fa2b652bf6eab77
Arnd Bergmann <arnd@arndb.de>
2015-11-12 15:20:16 +0100

    arm64: mark cpus_have_hwcap as __maybe_unused
    cpus_have_hwcap() is defined as a 'static' function an only used in
    one place that is inside of an #ifdef, so we get a warning when
    the only user is disabled:
    
    arch/arm64/kernel/cpufeature.c:699:13: warning: 'cpus_have_hwcap' defined but not used [-Wunused-function]
    
    This marks the function as __maybe_unused, so the compiler knows that
    it can drop the function definition without warning about it.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Fixes: 37b01d53ceef ("arm64/HWCAP: Use system wide safe values")
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -679,7 +679,7 @@ static void cap_set_hwcap(const struct arm64_cpu_capabilities *cap)
 }
 
 /* Check if we have a particular HWCAP enabled */
-static bool cpus_have_hwcap(const struct arm64_cpu_capabilities *cap)
+static bool __maybe_unused cpus_have_hwcap(const struct arm64_cpu_capabilities *cap)
 {
 	bool rc;
 
