7b5585cd22de53d5961c2baaf05634d923a21795
Arnd Bergmann <arnd@arndb.de>
2015-11-12 15:14:23 +0100

    UPSTREAM: mmc: dw_mmc: use resource_size_t to store physical address
    The dw_mmc driver stores the physical address of the MMIO registers
    in a pointer, which requires the use of type casts, and is actually
    broken if anyone ever has this device on a 32-bit SoC in registers
    above 4GB. Gcc warns about this possibility when the driver is built
    with ARM LPAE enabled:
    
    mmc/host/dw_mmc.c: In function 'dw_mci_edmac_start_dma':
    mmc/host/dw_mmc.c:702:17: warning: cast from pointer to integer of different size
      cfg.dst_addr = (dma_addr_t)(host->phy_regs + fifo_offset);
                     ^
    mmc/host/dw_mmc-pltfm.c: In function 'dw_mci_pltfm_register':
    mmc/host/dw_mmc-pltfm.c:63:19: warning: cast to pointer from integer of different size
      host->phy_regs = (void *)(regs->start);
    
    This changes the code to use resource_size_t, which gets rid of the
    warning, the bug and the useless casts.
    
    Change-Id: I894c49cede8f0626efb80a9a3181a5385bbb2bcd
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Jaehoon Chung <jh80.chung@samsung.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Huang, Tao <huangtao@rock-chips.com>
    (cherry picked from commit 260b31643691e8a58683a4ccc3bdf7abfd86f54a)

--- include/linux/mmc/dw_mmc.h
+++ include/linux/mmc/dw_mmc.h
@@ -172,7 +172,7 @@ struct dw_mci {
 	/* For edmac */
 	struct dw_mci_dma_slave *dms;
 	/* Registers's physical base address */
-	void                    *phy_regs;
+	resource_size_t		phy_regs;
 
 	u32			cmd_status;
 	u32			data_status;
