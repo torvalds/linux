d95e2822f105386dd48f1b42b7d0f0c0a2f61bdb
Hajime Tazaki <thehajime@gmail.com>
2015-11-12 15:21:03 +0900

    lkl: define __IGNORE_* instead of __NR_* 0 for unused system calls
    Use __IGNORE_ to mark system calls we don't plan to export due to lack
    of support in LKL instead of defining them to 0.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>
    [Octavian: only mark those system calls we know we can't support]
    Signed-off-by: Octavian Purdila <octavian.purdila@intel.com>

--- arch/lkl/include/asm/unistd.h
+++ arch/lkl/include/asm/unistd.h
@@ -6,87 +6,87 @@
 /*
  * Unsupported system calls due to lack of support in LKL (e.g. related to
  * virtual memory, signal, user processes). We also only support 64bit version
- * of system calls where we have two version to keep the same APi across 32 and
+ * of system calls where we have two version to keep the same API across 32 and
  * 64 bit hosts.
  */
-#define __NR_restart_syscall 0
-#define __NR_exit 0
-#define __NR_fork 0
-#define __NR_execve 0
-#define __NR_ptrace 0
-#define __NR_alarm 0
-#define __NR_pause 0
-#define __NR_kill 0
-#define __NR_brk 0
-#define __NR_uselib 0
-#define __NR_swapon 0
-#define __NR_mmap 0
-#define __NR_munmap 0
-#define __NR_swapoff 0
-#define __NR_clone 0
-#define __NR_mprotect 0
-#define __NR_init_module 0
-#define __NR_quotactl 0
-#define __NR_msync 0
-#define __NR_mlock 0
-#define __NR_munlock 0
-#define __NR_mlockall 0
-#define __NR_munlockall 0
-#define __NR_mremap 0
-#define __NR_rt_sigreturn 0
-#define __NR_rt_sigaction 0
-#define __NR_rt_sigprocmask 0
-#define __NR_rt_sigpending 0
-#define __NR_rt_sigtimedwait 0
-#define __NR_rt_sigqueueinfo 0
-#define __NR_rt_sigsuspend 0
-#define __NR_sigaltstack 0
-#define __NR_vfork 0
-#define __NR_mincore 0
-#define __NR_madvise 0
-#define __NR_getdents 0 /* we use the 64 bit counter part instead */
-#define __NR_tkill 0
-#define __NR_exit_group 0
-#define __NR_remap_file_pages 0
-#define __NR_statfs 0 /* we use the 64 bit counter part instead */
-#define __NR_fstatfs 0 /* we use the 64 bit counter part instead */
-#define __NR_fstat 0 /* we use the 64 bit counter part instead */
-#define __NR_fadvise64_64 0
-#define __NR_mbind 0
-#define __NR_get_mempolicy 0
-#define __NR_set_mempolicy 0
-#define __NR_mq_open 0
-#define __NR_mq_unlink 0
-#define __NR_mq_timedsend 0
-#define __NR_mq_timedreceive 0
-#define __NR_mq_0
-#define __NR_mq_getsetattr 0
-#define __NR_kexec_load 0
-#define __NR_migrate_pages 0
-#define __NR_unshare 0
-#define __NR_set_robust_list 0
-#define __NR_get_robust_list 0
-#define __NR_sync_file_range 0
-#define __NR_vmsplice 0
-#define __NR_move_pages 0
-#define __NR_mq_notify 0
-#define __NR_umount2 0
-#define __NR_delete_module 0
-#define __NR_signalfd4 0
-#define __NR_preadv 0 /* we use the 64 bit counter part instead */
-#define __NR_pwritev 0 /* we use the 64 bit counter part instead */
-#define __NR_rt_tgsigqueueinfo 0
-#define __NR_perf_event_open 0
-#define __NR_setns 0
-#define __NR_process_vm_readv 0
-#define __NR_process_vm_writev 0
-#define __NR_kcmp 0
-#define __NR_finit_module 0
-#define __NR_seccomp 0
-#define __NR_memfd_create 0
-#define __NR_bpf 0
-#define __NR_execveat 0
-#define __NR_lseek 0 /* we use the 64 bit counter part instead */
+#define __IGNORE_restart_syscall
+#define __IGNORE_exit
+#define __IGNORE_fork
+#define __IGNORE_execve
+#define __IGNORE_ptrace
+#define __IGNORE_alarm
+#define __IGNORE_pause
+#define __IGNORE_kill
+#define __IGNORE_brk
+#define __IGNORE_uselib
+#define __IGNORE_swapon
+#define __IGNORE_mmap
+#define __IGNORE_munmap
+#define __IGNORE_swapoff
+#define __IGNORE_clone
+#define __IGNORE_mprotect
+#define __IGNORE_init_module
+#define __IGNORE_quotactl
+#define __IGNORE_msync
+#define __IGNORE_mlock
+#define __IGNORE_munlock
+#define __IGNORE_mlockall
+#define __IGNORE_munlockall
+#define __IGNORE_mremap
+#define __IGNORE_rt_sigreturn
+#define __IGNORE_rt_sigaction
+#define __IGNORE_rt_sigprocmask
+#define __IGNORE_rt_sigpending
+#define __IGNORE_rt_sigtimedwait
+#define __IGNORE_rt_sigqueueinfo
+#define __IGNORE_rt_sigsuspend
+#define __IGNORE_sigaltstack
+#define __IGNORE_vfork
+#define __IGNORE_mincore
+#define __IGNORE_madvise
+#define __IGNORE_getdents /* we use the 64 bit counter part instead */
+#define __IGNORE_tkill
+#define __IGNORE_exit_group
+#define __IGNORE_remap_file_pages
+#define __IGNORE_statfs /* we use the 64 bit counter part instead */
+#define __IGNORE_fstatfs /* we use the 64 bit counter part instead */
+#define __IGNORE_fstat /* we use the 64 bit counter part instead */
+#define __IGNORE_fadvise64_64
+#define __IGNORE_mbind
+#define __IGNORE_get_mempolicy
+#define __IGNORE_set_mempolicy
+#define __IGNORE_mq_open
+#define __IGNORE_mq_unlink
+#define __IGNORE_mq_timedsend
+#define __IGNORE_mq_timedreceive
+#define __IGNORE_mq
+#define __IGNORE_mq_getsetattr
+#define __IGNORE_kexec_load
+#define __IGNORE_migrate_pages
+#define __IGNORE_unshare
+#define __IGNORE_set_robust_list
+#define __IGNORE_get_robust_list
+#define __IGNORE_sync_file_range
+#define __IGNORE_vmsplice
+#define __IGNORE_move_pages
+#define __IGNORE_mq_notify
+#define __IGNORE_umount2
+#define __IGNORE_delete_module
+#define __IGNORE_signalfd4
+#define __IGNORE_preadv /* we use the 64 bit counter part instead */
+#define __IGNORE_pwritev /* we use the 64 bit counter part instead */
+#define __IGNORE_rt_tgsigqueueinfo
+#define __IGNORE_perf_event_open
+#define __IGNORE_setns
+#define __IGNORE_process_vm_readv
+#define __IGNORE_process_vm_writev
+#define __IGNORE_kcmp
+#define __IGNORE_finit_module
+#define __IGNORE_seccomp
+#define __IGNORE_memfd_create
+#define __IGNORE_bpf
+#define __IGNORE_execveat
+#define __IGNORE_lseek /* we use the 64 bit counter part instead */
 
 int run_syscalls(void);
 
