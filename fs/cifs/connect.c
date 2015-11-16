275516cdcfa42c603c79ea42d2a7d4288d950fea
Arnd Hannemann <arnd@arndnet.de>
2015-11-16 17:38:28 +0100

    Print IP address of unresponsive server
    Before this patch, only the hostname of the server
    is printed when it becomes unresponsive.
    This might not be helpful, if the IP-Address has
    changed since initial mount when the name was
    resolved (e.g. because the IPv6-Prefix changed).
    
    This patch adds the cached IP address of the unresponsive server,
    to the log message.
    
    Signed-off-by: Arnd Hannemann <arnd@arndnet.de>
    Signed-off-by: Steve French <sfrench@localhost.localdomain>

--- fs/cifs/connect.c
+++ fs/cifs/connect.c
@@ -488,8 +488,11 @@ server_unresponsive(struct TCP_Server_Info *server)
 	 */
 	if (server->tcpStatus == CifsGood &&
 	    time_after(jiffies, server->lstrp + 2 * SMB_ECHO_INTERVAL)) {
-		cifs_dbg(VFS, "Server %s has not responded in %d seconds. Reconnecting...\n",
-			 server->hostname, (2 * SMB_ECHO_INTERVAL) / HZ);
+		cifs_dbg(VFS, "Server %s (addr=%pISc) has not responded in "
+			 "%d seconds. Reconnecting...\n",
+			 server->hostname,
+			 (struct sockaddr *)&server->dstaddr,
+			 (2 * SMB_ECHO_INTERVAL) / HZ);
 		cifs_reconnect(server);
 		wake_up(&server->response_q);
 		return true;
