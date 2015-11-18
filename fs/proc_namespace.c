5d9f3c7b620f6d1d9555223817bdfddfbd4b93a0
Dmitry V. Levin <ldv@altlinux.org>
2015-11-19 00:57:52 +0300

    vfs: show_vfsmnt: remove redundant initialization of error code
    As err variable is now always checked right after the first assignment,
    its initialization is redundant and could be safely removed.
    
    Signed-off-by: Dmitry V. Levin <ldv@altlinux.org>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -95,9 +95,9 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 {
 	struct proc_mounts *p = m->private;
 	struct mount *r = real_mount(mnt);
-	int err = 0;
 	struct path mnt_path = { .dentry = mnt->mnt_root, .mnt = mnt };
 	struct super_block *sb = mnt_path.dentry->d_sb;
+	int err;
 
 	if (sb->s_op->show_devname) {
 		err = sb->s_op->show_devname(m, mnt_path.dentry);
