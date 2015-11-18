4e0b6ca9dace8dc0731571efd2bf1897fcb11938
Chen Gang <gang.chen.5i5j@gmail.com>
2015-11-19 03:24:42 +0800

    asm-generic: page.h: Remove useless get_user_page and free_user_page
    They are not symmetric with each other, neither are used in real world
    (can not be found by grep command in source code root directory), so
    remove them.
    
    Signed-off-by: Chen Gang <gang.chen.5i5j@gmail.com>
    Acked-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Acked-by: Greg Ungerer <gerg@uclinux.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arc/include/asm/page.h
+++ arch/arc/include/asm/page.h
@@ -13,9 +13,6 @@
 
 #ifndef __ASSEMBLY__
 
-#define get_user_page(vaddr)		__get_free_page(GFP_KERNEL)
-#define free_user_page(page, addr)	free_page(addr)
-
 #define clear_page(paddr)		memset((paddr), 0, PAGE_SIZE)
 #define copy_page(to, from)		memcpy((to), (from), PAGE_SIZE)
 
--- arch/arm/include/asm/page-nommu.h
+++ arch/arm/include/asm/page-nommu.h
@@ -17,9 +17,6 @@
 #define KTHREAD_SIZE PAGE_SIZE
 #endif
  
-#define get_user_page(vaddr)		__get_free_page(GFP_KERNEL)
-#define free_user_page(page, addr)	free_page(addr)
-
 #define clear_page(page)	memset((page), 0, PAGE_SIZE)
 #define copy_page(to,from)	memcpy((to), (from), PAGE_SIZE)
 
