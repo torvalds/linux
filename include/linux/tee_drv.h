85c9c87d872f544550c341354bfa30c74edf4a58
Jens Wiklander <jens.wiklander@linaro.org>
2015-11-19 13:29:35 +0100

    tee: add kernel internal client interface **not for mainline**
    Adds a kernel internal TEE client interface to be used by other drivers.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- include/linux/tee_drv.h
+++ include/linux/tee_drv.h
@@ -286,4 +286,24 @@ int tee_shm_get_id(struct tee_shm *shm);
  */
 struct tee_shm *tee_shm_get_from_id(struct tee_context *ctx, int id);
 
+struct tee_context *tee_client_open_context(struct tee_context *start,
+			int (*match)(struct tee_ioctl_version_data *,
+				const void *),
+			const void *data, struct tee_ioctl_version_data *vers);
+
+void tee_client_close_context(struct tee_context *ctx);
+
+void tee_client_get_version(struct tee_context *ctx,
+			struct tee_ioctl_version_data *vers);
+
+int tee_client_open_session(struct tee_context *ctx,
+			struct tee_ioctl_open_session_arg *arg,
+			struct tee_param *param);
+
+int tee_client_close_session(struct tee_context *ctx, u32 session);
+
+int tee_client_invoke_func(struct tee_context *ctx,
+			struct tee_ioctl_invoke_arg *arg,
+			struct tee_param *param);
+
 #endif /*__TEE_DRV_H*/
