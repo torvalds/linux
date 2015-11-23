4a4d045eb2c174472b68f366108bf76f1802f803
Simon Wunderlich <sw@simonwunderlich.de>
2015-11-23 20:30:15 +0100

    batman-adv: Start new development cycle
    Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>
    Signed-off-by: Antonio Quartulli <a@unstable.cc>

--- net/batman-adv/main.h
+++ net/batman-adv/main.h
@@ -24,7 +24,7 @@
 #define BATADV_DRIVER_DEVICE "batman-adv"
 
 #ifndef BATADV_SOURCE_VERSION
-#define BATADV_SOURCE_VERSION "2015.2"
+#define BATADV_SOURCE_VERSION "2016.0"
 #endif
 
 /* B.A.T.M.A.N. parameters */
