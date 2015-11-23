c4be044abc6292f392c286b599e260f0c1bbb8ba
Geert Uytterhoeven <geert+renesas@glider.be>
2015-11-23 14:56:02 +0100

    ARM: shmobile: r8a7740: Remove mapping of L2 cache controller registers
    Now all r8a7740-based platforms have been migrated to the generic l2c OF
    initialization, it's no longer needed to map the L2 cache controller
    registers from .map_io().
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/setup-r8a7740.c
+++ arch/arm/mach-shmobile/setup-r8a7740.c
@@ -37,18 +37,6 @@ static struct map_desc r8a7740_io_desc[] __initdata = {
 		.length		= 160 << 20,
 		.type		= MT_DEVICE_NONSHARED
 	},
-#ifdef CONFIG_CACHE_L2X0
-	/*
-	 * for l2x0_init()
-	 * 0xf0100000-0xf0101000 -> 0xf0002000-0xf0003000
-	 */
-	{
-		.virtual	= 0xf0002000,
-		.pfn		= __phys_to_pfn(0xf0100000),
-		.length		= PAGE_SIZE,
-		.type		= MT_DEVICE_NONSHARED
-	},
-#endif
 };
 
 static void __init r8a7740_map_io(void)
