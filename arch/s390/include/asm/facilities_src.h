185edd4431f0dfd48a465e19ccc3280b08e9fa1a
Heiko Carstens <heiko.carstens@de.ibm.com>
2015-11-24 13:11:18 +0100

    s390/facilities: remove unneeded facility bits
    The facility lists contain a lot of bits which are not necessary to
    run the kernel.  Therefore remove them and keep only those bits which
    are required for the kernel.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/facilities_src.h
+++ arch/s390/include/asm/facilities_src.h
@@ -31,33 +31,21 @@ static struct facility_def facility_defs[] = {
 #endif
 #ifdef CONFIG_HAVE_MARCH_Z9_109_FEATURES
 			7,  /* stfle */
-			16, /* extended translation facility 2 */
 			17, /* message security assist */
-			20, /* HFP-multiply-and-add */
 			21, /* extended-immediate facility */
-			22, /* extended-translation facility 3 */
-			23, /* HFP-unnormalized-extension */
-			24, /* ETF2-enhancement */
 			25, /* store clock fast */
-			30, /* ETF3-enhancement */
 #endif
 #ifdef CONFIG_HAVE_MARCH_Z10_FEATURES
-			26, /* parsing enhancement facility */
 			27, /* mvcos */
 			32, /* compare and swap and store */
 			33, /* compare and swap and store 2 */
 			34, /* general extension facility */
 			35, /* execute extensions */
-			41, /* floating point support enhancement */
-			42, /* DFP facility */
-			44, /* PFPO */
 #endif
 #ifdef CONFIG_HAVE_MARCH_Z196_FEATURES
-			37, /* floating point extension */
 			45, /* fast-BCR, etc. */
 #endif
 #ifdef CONFIG_HAVE_MARCH_ZEC12_FEATURES
-			48, /* decimal floating point zoned */
 			49, /* misc-instruction-extensions */
 			52, /* interlocked facility 2 */
 #endif
