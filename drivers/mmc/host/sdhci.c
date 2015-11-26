9d5de93f6d543b356e39e225988ef443a7bce34c
Adrian Hunter <adrian.hunter@intel.com>
2015-11-26 14:00:46 +0200

    mmc: sdhci: Do not BUG on invalid vdd
    The driver may not be able to set the power correctly but that
    is not a reason to BUG().
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1299,7 +1299,9 @@ static void sdhci_set_power(struct sdhci_host *host, unsigned char mode,
 			pwr = SDHCI_POWER_330;
 			break;
 		default:
-			BUG();
+			WARN(1, "%s: Invalid vdd %#x\n",
+			     mmc_hostname(host->mmc), vdd);
+			break;
 		}
 	}
 
