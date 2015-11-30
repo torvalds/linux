76f10cb390f01ba74acea1fd1953d6232706afb8
Mark Yao <mark.yao@rock-chips.com>
2015-11-30 18:33:40 +0800

    UPSTREAM: drm: bridge/dw_hdmi: add atomic API support
    Fill atomic needed funcs with default atomic helper library.
    
    Rockchip use dw_hdmi, and drm/rockchip will covert to atomic api,
    we need dw_hdmi support atomic funcs.
    
    Now another drm driver use dw_hdmi is imx, not yet atomic, so
    check DRIVER_ATOMIC at runtime to spilt atomic and not atomic.
    
    Signed-off-by: Mark Yao <mark.yao@rock-chips.com>
    
    Change-Id: I519527efaf88b1e5c1b30db1fd23e59d45b88d50
    Signed-off-by: Yakir Yang <ykk@rock-chips.com>
    (cherry pick from 2c5b2cccdbde278fb702893404a2ea20f81f0345)

--- drivers/gpu/drm/bridge/dw-hdmi.c
+++ drivers/gpu/drm/bridge/dw-hdmi.c
@@ -22,6 +22,7 @@
 
 #include <drm/drm_of.h>
 #include <drm/drmP.h>
+#include <drm/drm_atomic_helper.h>
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_edid.h>
 #include <drm/drm_encoder_slave.h>
@@ -1522,6 +1523,17 @@ static const struct drm_connector_funcs dw_hdmi_connector_funcs = {
 	.force = dw_hdmi_connector_force,
 };
 
+static const struct drm_connector_funcs dw_hdmi_atomic_connector_funcs = {
+	.dpms = drm_atomic_helper_connector_dpms,
+	.fill_modes = drm_helper_probe_single_connector_modes,
+	.detect = dw_hdmi_connector_detect,
+	.destroy = dw_hdmi_connector_destroy,
+	.force = dw_hdmi_connector_force,
+	.reset = drm_atomic_helper_connector_reset,
+	.atomic_duplicate_state = drm_atomic_helper_connector_duplicate_state,
+	.atomic_destroy_state = drm_atomic_helper_connector_destroy_state,
+};
+
 static const struct drm_connector_helper_funcs dw_hdmi_connector_helper_funcs = {
 	.get_modes = dw_hdmi_connector_get_modes,
 	.mode_valid = dw_hdmi_connector_mode_valid,
@@ -1645,8 +1657,15 @@ static int dw_hdmi_register(struct drm_device *drm, struct dw_hdmi *hdmi)
 
 	drm_connector_helper_add(&hdmi->connector,
 				 &dw_hdmi_connector_helper_funcs);
-	drm_connector_init(drm, &hdmi->connector, &dw_hdmi_connector_funcs,
-			   DRM_MODE_CONNECTOR_HDMIA);
+
+	if (drm_core_check_feature(drm, DRIVER_ATOMIC))
+		drm_connector_init(drm, &hdmi->connector,
+				   &dw_hdmi_atomic_connector_funcs,
+				   DRM_MODE_CONNECTOR_HDMIA);
+	else
+		drm_connector_init(drm, &hdmi->connector,
+				   &dw_hdmi_connector_funcs,
+				   DRM_MODE_CONNECTOR_HDMIA);
 
 	hdmi->connector.encoder = encoder;
 
