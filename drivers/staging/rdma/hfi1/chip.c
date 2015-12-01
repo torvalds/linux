56af5543f8cf078da18a0912b8fa04baf724b210
Dean Luick <dean.luick@intel.com>
2015-12-01 15:38:22 -0500

    staging/rdma/hfi1: remove SPC freeze error messages
    An SPC freeze is not an error.  Remove the messages.
    
    Reviewed-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Signed-off-by: Dean Luick <dean.luick@intel.com>
    Signed-off-by: Jubin John <jubin.john@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rdma/hfi1/chip.c
+++ drivers/staging/rdma/hfi1/chip.c
@@ -6487,7 +6487,6 @@ void handle_freeze(struct work_struct *work)
 	struct hfi1_devdata *dd = ppd->dd;
 
 	/* wait for freeze indicators on all affected blocks */
-	dd_dev_info(dd, "Entering SPC freeze\n");
 	wait_for_freeze_status(dd, 1);
 
 	/* SPC is now frozen */
@@ -6545,7 +6544,6 @@ void handle_freeze(struct work_struct *work)
 	wake_up(&dd->event_queue);
 
 	/* no longer frozen */
-	dd_dev_err(dd, "Exiting SPC freeze\n");
 }
 
 /*
