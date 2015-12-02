e0e827b9fc71fbed1a9cd246067c2a4dbd3ea220
Raghavendra K T <raghavendra.kt@linux.vnet.ibm.com>
2015-12-02 16:57:06 +0530

    blk-mq: Reuse hardware context cpumask for tags
    hctx->cpumask is already populated and let the tag cpumask follow that
    instead of going through a new for loop.
    
    Signed-off-by: Raghavendra K T <raghavendra.kt@linux.vnet.ibm.com>
    Reviewed-by: Sagi Grimberg <sagig@mellanox.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -1850,6 +1850,7 @@ static void blk_mq_map_swqueue(struct request_queue *q,
 		hctx->tags = set->tags[i];
 		WARN_ON(!hctx->tags);
 
+		cpumask_copy(hctx->tags->cpumask, hctx->cpumask);
 		/*
 		 * Set the map size to the number of mapped software queues.
 		 * This is more accurate and more efficient than looping
@@ -1862,14 +1863,6 @@ static void blk_mq_map_swqueue(struct request_queue *q,
 		 */
 		hctx->next_cpu = cpumask_first(hctx->cpumask);
 		hctx->next_cpu_batch = BLK_MQ_CPU_WORK_BATCH;
-	}
-
-	queue_for_each_ctx(q, ctx, i) {
-		if (!cpumask_test_cpu(i, online_mask))
-			continue;
-
-		hctx = q->mq_ops->map_queue(q, i);
-		cpumask_set_cpu(i, hctx->tags->cpumask);
 	}
 }
 
