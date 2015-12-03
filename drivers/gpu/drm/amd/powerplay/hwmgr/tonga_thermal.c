7ae0a66134c7274cd889129f67a83ac004084b3b
Eric Huang <JinHuiEric.Huang@amd.com>
2015-12-03 15:13:46 -0500

    drm/amd/powerplay: Fix a bug in fan control setting default mode for Tonga and Fiji.
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Eric Huang <JinHuiEric.Huang@amd.com>

--- drivers/gpu/drm/amd/powerplay/hwmgr/tonga_thermal.c
+++ drivers/gpu/drm/amd/powerplay/hwmgr/tonga_thermal.c
@@ -129,7 +129,7 @@ int tonga_fan_ctrl_set_static_mode(struct pp_hwmgr *hwmgr, uint32_t mode)
 */
 int tonga_fan_ctrl_set_default_mode(struct pp_hwmgr *hwmgr)
 {
-	if (hwmgr->fan_ctrl_is_in_default_mode) {
+	if (!hwmgr->fan_ctrl_is_in_default_mode) {
 		PHM_WRITE_VFPF_INDIRECT_FIELD(hwmgr->device, CGS_IND_REG__SMC, CG_FDO_CTRL2, FDO_PWM_MODE, hwmgr->fan_ctrl_default_mode);
 		PHM_WRITE_VFPF_INDIRECT_FIELD(hwmgr->device, CGS_IND_REG__SMC, CG_FDO_CTRL2, TMIN, hwmgr->tmin);
 		hwmgr->fan_ctrl_is_in_default_mode = true;
