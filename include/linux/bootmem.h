8dd3303001976aa8583bf20f6b93590c74114308
Igor Mammedov <imammedo@redhat.com>
2015-12-04 14:07:05 +0100

    x86/mm: Introduce max_possible_pfn
    max_possible_pfn will be used for tracking max possible
    PFN for memory that isn't present in E820 table and
    could be hotplugged later.
    
    By default max_possible_pfn is initialized with max_pfn,
    but later it could be updated with highest PFN of
    hotpluggable memory ranges declared in ACPI SRAT table
    if any present.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: akataria@vmware.com
    Cc: fujita.tomonori@lab.ntt.co.jp
    Cc: konrad.wilk@oracle.com
    Cc: pbonzini@redhat.com
    Cc: revers@redhat.com
    Cc: riel@redhat.com
    Link: http://lkml.kernel.org/r/1449234426-273049-2-git-send-email-imammedo@redhat.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/bootmem.h
+++ include/linux/bootmem.h
@@ -19,6 +19,10 @@ extern unsigned long min_low_pfn;
  * highest page
  */
 extern unsigned long max_pfn;
+/*
+ * highest possible page
+ */
+extern unsigned long long max_possible_pfn;
 
 #ifndef CONFIG_NO_BOOTMEM
 /*
