61b87cae6361ea6af161c1ffa549898892707b19
Stephane Eranian <eranian@google.com>
2015-12-07 20:33:25 +0100

    perf/x86: Fix filter_events() bug with event mappings
    This patch fixes a bug in the filter_events() function.
    
    The patch fixes the bug whereby if some mappings did not
    exist, e.g., STALLED_CYCLES_FRONTEND, then any event after it
    in the attrs array would disappear from the published list of
    events in /sys/devices/cpu/events. This could be verified
    easily on any system post SNB (which do not publish
    STALLED_CYCLES_FRONTEND):
    
    	$ ./perf stat -e cycles,ref-cycles true
    	Performance counter stats for 'true':
                  1,217,348      cycles
    	<not supported>      ref-cycles
    
    The problem is that in filter_events() there is an assumption
    that the argument (attrs) is organized in increasing continuous
    event indexes related to the event_map(). But if we remove the
    non-supported events by shifing the position in the array, then
    the lookup x86_pmu.event_map() needs to compensate for it, otherwise
    we are looking up the wrong index. This patch corrects this problem
    by compensating for the deleted events and with that ref-cycles
    reappears (here shown on Haswell):
    
    	$ perf stat -e ref-cycles,cycles true
    	Performance counter stats for 'true':
             4,525,910      ref-cycles
             1,064,920      cycles
           0.002943888 seconds time elapsed
    
    Signed-off-by: Stephane Eranian <eranian@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vince Weaver <vincent.weaver@maine.edu>
    Cc: jolsa@kernel.org
    Cc: kan.liang@intel.com
    Fixes: 8300daa26755 ("perf/x86: Filter out undefined events from sysfs events attribute")
    Link: http://lkml.kernel.org/r/1449516805-6637-1-git-send-email-eranian@google.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/kernel/cpu/perf_event.c
+++ arch/x86/kernel/cpu/perf_event.c
@@ -1534,6 +1534,7 @@ static void __init filter_events(struct attribute **attrs)
 {
 	struct device_attribute *d;
 	struct perf_pmu_events_attr *pmu_attr;
+	int offset = 0;
 	int i, j;
 
 	for (i = 0; attrs[i]; i++) {
@@ -1542,7 +1543,7 @@ static void __init filter_events(struct attribute **attrs)
 		/* str trumps id */
 		if (pmu_attr->event_str)
 			continue;
-		if (x86_pmu.event_map(i))
+		if (x86_pmu.event_map(i + offset))
 			continue;
 
 		for (j = i; attrs[j]; j++)
@@ -1550,6 +1551,14 @@ static void __init filter_events(struct attribute **attrs)
 
 		/* Check the shifted attr. */
 		i--;
+
+		/*
+		 * event_map() is index based, the attrs array is organized
+		 * by increasing event index. If we shift the events, then
+		 * we need to compensate for the event_map(), otherwise
+		 * we are looking up the wrong event in the map
+		 */
+		offset++;
 	}
 }
 
