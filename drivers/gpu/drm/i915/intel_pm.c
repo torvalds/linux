344df9809f4521c8c11d67c5ef18764b54358950
Mika Kuoppala <mika.kuoppala@linux.intel.com>
2015-12-07 18:29:44 +0200

    drm/i915/skl: Disable coarse power gating up until F0
    There is conflicting info between E0 and F0 steppings
    for this workarounds. Trust more authoritative source and
    be conservative and extend also for F0.
    
    This prevents numerous (>50) gpu hangs with SKL GT4e
    during piglit run.
    
    References: HSD: gen9lp/2134184
    Cc: Sagar Arun Kamble <sagar.a.kamble@intel.com>
    Signed-off-by: Mika Kuoppala <mika.kuoppala@intel.com>
    Reviewed-by: Sagar Arun Kamble <sagar.a.kamble@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1449505785-20812-1-git-send-email-mika.kuoppala@intel.com
    (cherry picked from commit 6686ece19f7446f0e29c77d9e0402e1d0ce10c48)
    Cc: stable@vger.kernel.org # v4.3+
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -4825,7 +4825,7 @@ static void gen9_enable_rc6(struct drm_device *dev)
 	 * WaRsDisableCoarsePowerGating:skl,bxt - Render/Media PG need to be disabled with RC6.
 	 */
 	if ((IS_BROXTON(dev) && (INTEL_REVID(dev) < BXT_REVID_B0)) ||
-	    ((IS_SKL_GT3(dev) || IS_SKL_GT4(dev)) && (INTEL_REVID(dev) <= SKL_REVID_E0)))
+	    ((IS_SKL_GT3(dev) || IS_SKL_GT4(dev)) && (INTEL_REVID(dev) <= SKL_REVID_F0)))
 		I915_WRITE(GEN9_PG_ENABLE, 0);
 	else
 		I915_WRITE(GEN9_PG_ENABLE, (rc6_mask & GEN6_RC_CTL_RC6_ENABLE) ?
