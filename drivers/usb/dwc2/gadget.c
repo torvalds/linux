3be99cd0e882dd2127b8cfe3942f5e464915aeba
Gregory Herrero <gregory.herrero@intel.com>
2015-12-07 12:07:31 +0100

    usb: dwc2: gadget: don't overwrite DCTL register on NAKEFF interrupts
    When receiving GINTSTS_GINNAKEFF or GINTSTS_GOUTNAKEFF interrupt,
    DCTL will be overwritten with DCTL_CGOUTNAK or DCTL_CGNPINNAK values.
    Instead of overwriting it, write only needed bits.
    
    It could cause an issue if GINTSTS_GINNAKEFF or GINTSTS_GOUTNAKEFF
    interrupt is received after dwc2 disabled pullup by writing
    DCTL_SFTDISCON bit.
    Pullup will then be re-enabled whereas it should not.
    
    Acked-by: John Youn <johnyoun@synopsys.com>
    Signed-off-by: Gregory Herrero <gregory.herrero@intel.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/dwc2/gadget.c
+++ drivers/usb/dwc2/gadget.c
@@ -2585,7 +2585,7 @@ irq_retry:
 	if (gintsts & GINTSTS_GOUTNAKEFF) {
 		dev_info(hsotg->dev, "GOUTNakEff triggered\n");
 
-		dwc2_writel(DCTL_CGOUTNAK, hsotg->regs + DCTL);
+		__orr32(hsotg->regs + DCTL, DCTL_CGOUTNAK);
 
 		dwc2_hsotg_dump(hsotg);
 	}
@@ -2593,7 +2593,7 @@ irq_retry:
 	if (gintsts & GINTSTS_GINNAKEFF) {
 		dev_info(hsotg->dev, "GINNakEff triggered\n");
 
-		dwc2_writel(DCTL_CGNPINNAK, hsotg->regs + DCTL);
+		__orr32(hsotg->regs + DCTL, DCTL_CGNPINNAK);
 
 		dwc2_hsotg_dump(hsotg);
 	}
