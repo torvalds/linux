bf9af08cd5151f1915119ea4c6a873d5a4880d74
Anton Bondarenko <anton.bondarenko.sama@gmail.com>
2015-12-08 07:43:46 +0100

    spi: imx: defer spi initialization, if DMA engine is
    If SPI device supports DMA mode, but DMA controller is not yet
    available due to e.g. a delay in the corresponding kernel module
    initialization, retry to initialize SPI driver later on instead of
    falling back into PIO only mode.
    
    Signed-off-by: Anton Bondarenko <anton.bondarenko.sama@gmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-imx.c
+++ drivers/spi/spi-imx.c
@@ -1222,6 +1222,9 @@ static int spi_imx_probe(struct platform_device *pdev)
 	 */
 	if (is_imx51_ecspi(spi_imx)) {
 		ret = spi_imx_sdma_init(&pdev->dev, spi_imx, master, res);
+		if (ret == -EPROBE_DEFER)
+			goto out_clk_put;
+
 		if (ret < 0)
 			dev_err(&pdev->dev, "dma setup error %d, use pio\n",
 				ret);
