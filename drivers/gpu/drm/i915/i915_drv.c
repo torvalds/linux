825f272895b9ec9151399b4102739c899b0b6392
Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
2015-12-09 15:56:13 +0200

    drm/i915: Cancel hangcheck before GPU is suspended
    In order to avoid accessing GPU registers while GPU is suspended cancel
    the hangcheck work before calling intel_suspend_complete which actually
    puts the GPU to suspend. Otherwise hangcheck might do MMIO reads to a
    suspended GPU.
    
    Placement before intel_guc_suspend is imitated from i915_drm_suspend
    which cancels the work at i915_gem_suspend, to keep the functions
    similar.
    
    On VLV systems, namely BYT, this was causing an error during runtime
    suspend cycle:
    
    [drm:vlv_check_no_gt_access [i915]] *ERROR* GT register access while GT waking disabled
    
    Testcase: igt/pm_rpm/basic-rte
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=93121
    
    Signed-off-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Cc: Imre Deak <imre.deak@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Link: http://patchwork.freedesktop.org/patch/msgid/1449669373-8588-1-git-send-email-joonas.lahtinen@linux.intel.com
    Reviewed-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Imre Deak <imre.deak@intel.com>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -1462,6 +1462,8 @@ static int intel_runtime_suspend(struct device *device)
 	i915_gem_release_all_mmaps(dev_priv);
 	mutex_unlock(&dev->struct_mutex);
 
+	cancel_delayed_work_sync(&dev_priv->gpu_error.hangcheck_work);
+
 	intel_guc_suspend(dev);
 
 	intel_suspend_gt_powersave(dev);
@@ -1475,7 +1477,6 @@ static int intel_runtime_suspend(struct device *device)
 		return ret;
 	}
 
-	cancel_delayed_work_sync(&dev_priv->gpu_error.hangcheck_work);
 	intel_uncore_forcewake_reset(dev, false);
 	dev_priv->pm.suspended = true;
 
