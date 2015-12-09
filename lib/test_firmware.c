47e0bbb7fa985a0f1b5794a8653fae4f8f49de77
Brian Norris <computersforpeace@gmail.com>
2015-12-09 14:50:25 -0800

    test: firmware_class: report errors properly on failure
    request_firmware() failures currently won't get reported at all (the
    error code is discarded). What's more, we get confusing messages, like:
    
        # echo -n notafile > /sys/devices/virtual/misc/test_firmware/trigger_request
        [ 8280.311856] test_firmware: loading 'notafile'
        [ 8280.317042] test_firmware: load of 'notafile' failed: -2
        [ 8280.322445] test_firmware: loaded: 0
        # echo $?
        0
    
    Report the failures via write() errors, and don't say we "loaded"
    anything.
    
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>
    Acked-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Shuah Khan <shuahkh@osg.samsung.com>

--- lib/test_firmware.c
+++ lib/test_firmware.c
@@ -65,14 +65,19 @@ static ssize_t trigger_request_store(struct device *dev,
 	release_firmware(test_firmware);
 	test_firmware = NULL;
 	rc = request_firmware(&test_firmware, name, dev);
-	if (rc)
+	if (rc) {
 		pr_info("load of '%s' failed: %d\n", name, rc);
-	pr_info("loaded: %zu\n", test_firmware ? test_firmware->size : 0);
+		goto out;
+	}
+	pr_info("loaded: %zu\n", test_firmware->size);
+	rc = count;
+
+out:
 	mutex_unlock(&test_fw_mutex);
 
 	kfree(name);
 
-	return count;
+	return rc;
 }
 static DEVICE_ATTR_WO(trigger_request);
 
