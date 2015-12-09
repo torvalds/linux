8b7663de6e2bfe3c40e1846e1c4625f33d138757
Xiubo Li <lixiubo@cmss.chinamobile.com>
2015-12-09 13:09:07 +0800

    regmap: cache: Add 64-bit mode support
    Since the mmio has support the 64-bit has been supported for the
    64-bit platform, so should the regcache core too.
    
    Signed-off-by: Xiubo Li <lixiubo@cmss.chinamobile.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/base/regmap/regcache.c
+++ drivers/base/regmap/regcache.c
@@ -551,6 +551,14 @@ bool regcache_set_val(struct regmap *map, void *base, unsigned int idx,
 		cache[idx] = val;
 		break;
 	}
+#ifdef CONFIG_64BIT
+	case 8: {
+		u64 *cache = base;
+
+		cache[idx] = val;
+		break;
+	}
+#endif
 	default:
 		BUG();
 	}
@@ -584,6 +592,13 @@ unsigned int regcache_get_val(struct regmap *map, const void *base,
 
 		return cache[idx];
 	}
+#ifdef CONFIG_64BIT
+	case 8: {
+		const u64 *cache = base;
+
+		return cache[idx];
+	}
+#endif
 	default:
 		BUG();
 	}
