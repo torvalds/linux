e7571f7fd66c77a760338340adbe41d994fe93ac
Chris Wilson <chris@chris-wilson.co.uk>
2015-12-11 11:32:57 +0000

    drm/i915: Break busywaiting for requests on pending signals
    The busywait in __i915_spin_request() does not respect pending signals
    and so may consume the entire timeslice for the task instead of
    returning to userspace to handle the signal.
    
    In the worst case this could cause a delay in signal processing of 20ms,
    which would be a noticeable jitter in cursor tracking. If a higher
    resolution signal was being used, for example to provide fairness of a
    server timeslices between clients, we could expect to detect some
    unfairness between clients (i.e. some windows not updating as fast as
    others). This issue was noticed when inspecting a report of poor
    interactivity resulting from excessively high __i915_spin_request usage.
    
    Fixes regression from
    commit 2def4ad99befa25775dd2f714fdd4d92faec6e34 [v4.2]
    Author: Chris Wilson <chris@chris-wilson.co.uk>
    Date:   Tue Apr 7 16:20:41 2015 +0100
    
         drm/i915: Optimistically spin for the request completion
    
    v2: Try to assess the impact of the bug
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@linux.intel.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc; "Rogozhkin, Dmitry V" <dmitry.v.rogozhkin@intel.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Tvrtko Ursulin <tvrtko.ursulin@linux.intel.com>
    Cc: Eero Tamminen <eero.t.tamminen@intel.com>
    Cc: "Rantala, Valtteri" <valtteri.rantala@intel.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1449833608-22125-2-git-send-email-chris@chris-wilson.co.uk
    (cherry picked from commit 91b0c352ace9afec1fb51590c7b8bd60e0eb9fbd)
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -1146,7 +1146,7 @@ static bool missed_irq(struct drm_i915_private *dev_priv,
 	return test_bit(ring->id, &dev_priv->gpu_error.missed_irq_rings);
 }
 
-static int __i915_spin_request(struct drm_i915_gem_request *req)
+static int __i915_spin_request(struct drm_i915_gem_request *req, int state)
 {
 	unsigned long timeout;
 
@@ -1157,6 +1157,9 @@ static int __i915_spin_request(struct drm_i915_gem_request *req)
 	while (!need_resched()) {
 		if (i915_gem_request_completed(req, true))
 			return 0;
+
+		if (signal_pending_state(state, current))
+			break;
 
 		if (time_after_eq(jiffies, timeout))
 			break;
@@ -1197,6 +1200,7 @@ int __i915_wait_request(struct drm_i915_gem_request *req,
 	struct drm_i915_private *dev_priv = dev->dev_private;
 	const bool irq_test_in_progress =
 		ACCESS_ONCE(dev_priv->gpu_error.test_irq_rings) & intel_ring_flag(ring);
+	int state = interruptible ? TASK_INTERRUPTIBLE : TASK_UNINTERRUPTIBLE;
 	DEFINE_WAIT(wait);
 	unsigned long timeout_expire;
 	s64 before, now;
@@ -1229,7 +1233,7 @@ int __i915_wait_request(struct drm_i915_gem_request *req,
 	before = ktime_get_raw_ns();
 
 	/* Optimistic spin for the next jiffie before touching IRQs */
-	ret = __i915_spin_request(req);
+	ret = __i915_spin_request(req, state);
 	if (ret == 0)
 		goto out;
 
@@ -1241,8 +1245,7 @@ int __i915_wait_request(struct drm_i915_gem_request *req,
 	for (;;) {
 		struct timer_list timer;
 
-		prepare_to_wait(&ring->irq_queue, &wait,
-				interruptible ? TASK_INTERRUPTIBLE : TASK_UNINTERRUPTIBLE);
+		prepare_to_wait(&ring->irq_queue, &wait, state);
 
 		/* We need to check whether any gpu reset happened in between
 		 * the caller grabbing the seqno and now ... */
@@ -1260,7 +1263,7 @@ int __i915_wait_request(struct drm_i915_gem_request *req,
 			break;
 		}
 
-		if (interruptible && signal_pending(current)) {
+		if (signal_pending_state(state, current)) {
 			ret = -ERESTARTSYS;
 			break;
 		}
