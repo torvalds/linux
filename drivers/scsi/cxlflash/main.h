8c67424e2a37005364d0aecd6541d221517a1c4f
Manoj Kumar <manoj@linux.vnet.ibm.com>
2015-12-14 15:07:43 -0600

    cxlflash: Enable device id for future IBM CXL adapter
    [ Upstream commit a2746fb16e41b7c8f02aa4d2605ecce97abbebbd ]
    
    This drop enables a future card with a device id of 0x0600 to be
    recognized by the cxlflash driver.
    
    As per the design, the Accelerator Function Unit (AFU) for this new IBM
    CXL Flash Adapter retains the same host interface as the previous
    generation. For the early prototypes of the new card, the driver with
    this change behaves exactly as the driver prior to this behaved with the
    earlier generation card. Therefore, no card specific programming has
    been added. These card specific changes can be staged in later if
    needed.
    
    Signed-off-by: Manoj N. Kumar <manoj@linux.vnet.ibm.com>
    Acked-by: Matthew R. Ochs <mrochs@linux.vnet.ibm.com>
    Reviewed-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/scsi/cxlflash/main.h
+++ drivers/scsi/cxlflash/main.h
@@ -24,8 +24,8 @@
 #define CXLFLASH_ADAPTER_NAME	"IBM POWER CXL Flash Adapter"
 #define CXLFLASH_DRIVER_DATE	"(August 13, 2015)"
 
-#define PCI_DEVICE_ID_IBM_CORSA	0x04F0
-#define CXLFLASH_SUBS_DEV_ID	0x04F0
+#define PCI_DEVICE_ID_IBM_CORSA		0x04F0
+#define PCI_DEVICE_ID_IBM_FLASH_GT	0x0600
 
 /* Since there is only one target, make it 0 */
 #define CXLFLASH_TARGET		0
