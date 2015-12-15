a06cd4b76277bd319d65646481cb61aa3fd89b3b
Álvaro Fernández Rojas <noltari@gmail.com>
2015-12-15 21:03:49 +0100

    leds: bcm6328: add little endian support
    This adds little endian support while keeping big endian support.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
    Signed-off-by: Jacek Anaszewski <j.anaszewski@samsung.com>

--- drivers/leds/leds-bcm6328.c
+++ drivers/leds/leds-bcm6328.c
@@ -76,12 +76,20 @@ struct bcm6328_led {
 
 static void bcm6328_led_write(void __iomem *reg, unsigned long data)
 {
+#ifdef CONFIG_CPU_BIG_ENDIAN
 	iowrite32be(data, reg);
+#else
+	writel(data, reg);
+#endif
 }
 
 static unsigned long bcm6328_led_read(void __iomem *reg)
 {
+#ifdef CONFIG_CPU_BIG_ENDIAN
 	return ioread32be(reg);
+#else
+	return readl(reg);
+#endif
 }
 
 /**
