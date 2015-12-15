542db3cd345c38936bb9e4bb2c8d1971e6ac6619
Imre Deak <imre.deak@intel.com>
2015-12-15 20:10:36 +0200

    drm/i915: check that we hold an RPM wakelock ref before we put it
    With this change we have the corresponding wake lock checks in both the
    rpm get and put functions.
    
    v2-v3:
    - unchanged
    v4:
    - keep the corresponding check in the get helper (Chris)
    v5:
    - add a note to the commit message that with this change we have the
      checks both in the rpm get and put functions (Joonas)
    
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1450203038-5150-9-git-send-email-imre.deak@intel.com

--- drivers/gpu/drm/i915/intel_runtime_pm.c
+++ drivers/gpu/drm/i915/intel_runtime_pm.c
@@ -2286,6 +2286,7 @@ void intel_runtime_pm_put(struct drm_i915_private *dev_priv)
 	struct drm_device *dev = dev_priv->dev;
 	struct device *device = &dev->pdev->dev;
 
+	assert_rpm_wakelock_held(dev_priv);
 	atomic_dec(&dev_priv->pm.wakeref_count);
 
 	pm_runtime_mark_last_busy(device);
