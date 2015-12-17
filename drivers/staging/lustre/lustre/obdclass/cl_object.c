9569ea54ebadbb01746b7d6a5e79a16f78eaa1ae
Shraddha Barke <shraddha.6596@gmail.com>
2015-12-18 02:40:26 +0530

    Staging: lustre: obdclass: Declare cl_env_peek as static
    Declare cl_env_peek as static since it is used only in this file.
    Also remove the EXPORT SYMBOL.
    
    Signed-off-by: Shraddha Barke <shraddha.6596@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/obdclass/cl_object.c
+++ drivers/staging/lustre/lustre/obdclass/cl_object.c
@@ -704,7 +704,7 @@ static inline struct cl_env *cl_env_container(struct lu_env *env)
 	return container_of(env, struct cl_env, ce_lu);
 }
 
-struct lu_env *cl_env_peek(int *refcheck)
+static struct lu_env *cl_env_peek(int *refcheck)
 {
 	struct lu_env *env;
 	struct cl_env *cle;
@@ -724,7 +724,6 @@ struct lu_env *cl_env_peek(int *refcheck)
 	CDEBUG(D_OTHER, "%d@%p\n", cle ? cle->ce_ref : 0, cle);
 	return env;
 }
-EXPORT_SYMBOL(cl_env_peek);
 
 /**
  * Returns lu_env: if there already is an environment associated with the
