b67b1ab417fe73a632583cdd7b710e4450c9d4ec
Ashok Kumar <ashoks@broadcom.com>
2015-12-17 01:38:32 -0800

    arm64: Use PoU cache instr for I/D coherency
    In systems with three levels of cache(PoU at L1 and PoC at L3),
    PoC cache flush instructions flushes L2 and L3 caches which could affect
    performance.
    For cache flushes for I and D coherency, PoU should suffice.
    So changing all I and D coherency related cache flushes to PoU.
    
    Introduced a new __clean_dcache_area_pou API for dcache flush till PoU
    and provided a common macro for __flush_dcache_area and
    __clean_dcache_area_pou.
    
    Also, now in __sync_icache_dcache, icache invalidation for non-aliasing
    VIPT icache is done only for that particular page instead of the earlier
    __flush_icache_all.
    
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Ashok Kumar <ashoks@broadcom.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    (cherry picked from commit 0a28714c53fd4f7aea709be7577dfbe0095c8c3e)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>
    
    Conflicts:
    	arch/arm64/mm/proc-macros.S

--- arch/arm64/include/asm/cacheflush.h
+++ arch/arm64/include/asm/cacheflush.h
@@ -68,6 +68,7 @@
 extern void flush_cache_range(struct vm_area_struct *vma, unsigned long start, unsigned long end);
 extern void flush_icache_range(unsigned long start, unsigned long end);
 extern void __flush_dcache_area(void *addr, size_t len);
+extern void __clean_dcache_area_pou(void *addr, size_t len);
 extern long __flush_cache_user_range(unsigned long start, unsigned long end);
 
 static inline void flush_cache_mm(struct mm_struct *mm)
