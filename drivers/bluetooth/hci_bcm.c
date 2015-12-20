806f50c729c2d9f80500014f1d20c0ac74e66082
Luka Karinja <luka.karinja@gmail.com>
2015-12-20 13:36:06 +0100

    Bluetooth: hci_bcm: Add BCM2E65 ACPI ID
    Add BCM2E65 device in acpi_device_id table used on Asus T100TAF.
    
    Signed-off-by: Luka Karinja <luka.karinja@gmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/hci_bcm.c
+++ drivers/bluetooth/hci_bcm.c
@@ -815,6 +815,7 @@ static const struct hci_uart_proto bcm_proto = {
 #ifdef CONFIG_ACPI
 static const struct acpi_device_id bcm_acpi_match[] = {
 	{ "BCM2E39", 0 },
+	{ "BCM2E65", 0 },
 	{ "BCM2E67", 0 },
 	{ },
 };
