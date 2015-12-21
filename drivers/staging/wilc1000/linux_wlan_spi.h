43a7622935c72a6724d62d7fb5da35fbe7328f0e
Glen Lee <glen.lee@atmel.com>
2015-12-21 14:18:33 +0900

    staging: wilc1000: linux_wlan_spi.c: move all the codes to wilc_spi.c
    This patch moves all the codes in linux_wlan_spi.c to wilc_spi.c to make
    one spi module. Make wilc_spi_tx, wilc_spi_rx and wilc_spi_tx_rx static
    functions. Remove function declaration in linux_wlan_spi.h, which is unnedded
    now. No modification has been made inside the codes.
    linux_wlan_spi.[ch] will be remove in the next patch.
    
    Signed-off-by: Glen Lee <glen.lee@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/linux_wlan_spi.h
+++ drivers/staging/wilc1000/linux_wlan_spi.h
@@ -3,8 +3,4 @@
 
 #include <linux/spi/spi.h>
 #include "wilc_wfi_netdevice.h"
-
-int wilc_spi_tx(struct wilc *wilc, u8 *b, u32 len);
-int wilc_spi_rx(struct wilc *wilc, u8 *rb, u32 rlen);
-int wilc_spi_tx_rx(struct wilc *wilc, u8 *wb, u8 *rb, u32 rlen);
 #endif
