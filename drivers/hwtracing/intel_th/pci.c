6396b912f1e97a0e9566c0d801923a0d0eb31749
Alexander Shishkin <alexander.shishkin@linux.intel.com>
2015-12-22 17:25:22 +0200

    intel_th: pci: Add Apollo Lake SOC support
    This adds Intel(R) Trace Hub PCI ID for Apollo Lake SOC.
    
    Signed-off-by: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hwtracing/intel_th/pci.c
+++ drivers/hwtracing/intel_th/pci.c
@@ -67,6 +67,11 @@ static const struct pci_device_id intel_th_pci_id_table[] = {
 		PCI_DEVICE(PCI_VENDOR_ID_INTEL, 0xa126),
 		.driver_data = (kernel_ulong_t)0,
 	},
+	{
+		/* Apollo Lake */
+		PCI_DEVICE(PCI_VENDOR_ID_INTEL, 0x5a8e),
+		.driver_data = (kernel_ulong_t)0,
+	},
 	{ 0 },
 };
 
