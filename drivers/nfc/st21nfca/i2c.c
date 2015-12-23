39db2d214898baa55db39a54f8644ffc0535fe4a
Christophe Ricard <christophe.ricard@gmail.com>
2015-12-23 23:45:06 +0100

    nfc: st21nfca: Group device table together
    Group device table at the same place in order to make the code
    easier to read and parse.
    
    Signed-off-by: Christophe Ricard <christophe-h.ricard@st.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/st21nfca/i2c.c
+++ drivers/nfc/st21nfca/i2c.c
@@ -60,13 +60,6 @@
 
 #define ST21NFCA_HCI_I2C_DRIVER_NAME "st21nfca_hci_i2c"
 
-static struct i2c_device_id st21nfca_hci_i2c_id_table[] = {
-	{ST21NFCA_HCI_DRIVER_NAME, 0},
-	{}
-};
-
-MODULE_DEVICE_TABLE(i2c, st21nfca_hci_i2c_id_table);
-
 struct st21nfca_i2c_phy {
 	struct i2c_client *i2c_dev;
 	struct nfc_hci_dev *hdev;
@@ -662,6 +655,12 @@ static int st21nfca_hci_i2c_remove(struct i2c_client *client)
 
 	return 0;
 }
+
+static struct i2c_device_id st21nfca_hci_i2c_id_table[] = {
+	{ST21NFCA_HCI_DRIVER_NAME, 0},
+	{}
+};
+MODULE_DEVICE_TABLE(i2c, st21nfca_hci_i2c_id_table);
 
 static const struct of_device_id of_st21nfca_i2c_match[] = {
 	{ .compatible = "st,st21nfca-i2c", },
