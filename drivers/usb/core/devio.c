64f10edf0751727154e843e8bcf59fb659e579a2
Chase Metzger <chasemetzger15@gmail.com>
2015-12-22 20:19:45 -0800

    usb: core: devio.c: Removed unnecessary space
    Removed an unnecessary space between a function name and arguments.
    
    Signed-off-by: Chase Metzger <chasemetzger15@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/devio.c
+++ drivers/usb/core/devio.c
@@ -1903,7 +1903,7 @@ static int proc_releaseinterface(struct usb_dev_state *ps, void __user *arg)
 	ret = releaseintf(ps, ifnum);
 	if (ret < 0)
 		return ret;
-	destroy_async_on_interface (ps, ifnum);
+	destroy_async_on_interface(ps, ifnum);
 	return 0;
 }
 
