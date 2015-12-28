e0d8fdecf3acf36eb738e42d3caf46371500881c
Sara Sharon <sara.sharon@intel.com>
2015-12-28 22:37:08 +0200

    iwlwifi: support tracing wide commands
    Current iwlwifi_trace_dev_rx prints only the cmd without the
    group, which might be misleading. Change it to print the wide
    id. While at it add the DATA_PATH group and sub commands to the
    trace of the command names, sine it is missing due to patches
    submitted in parallel.
    
    Signed-off-by: Sara Sharon <sara.sharon@intel.com>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>

--- drivers/net/wireless/intel/iwlwifi/mvm/ops.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/ops.c
@@ -398,6 +398,13 @@ static const struct iwl_hcmd_names iwl_mvm_phy_names[] = {
 /* Please keep this array *SORTED* by hex value.
  * Access is done through binary search
  */
+static const struct iwl_hcmd_names iwl_mvm_data_path_names[] = {
+	HCMD_NAME(UPDATE_MU_GROUPS_CMD),
+};
+
+/* Please keep this array *SORTED* by hex value.
+ * Access is done through binary search
+ */
 static const struct iwl_hcmd_names iwl_mvm_prot_offload_names[] = {
 	HCMD_NAME(STORED_BEACON_NTF),
 };
@@ -406,6 +413,7 @@ static const struct iwl_hcmd_arr iwl_mvm_groups[] = {
 	[LEGACY_GROUP] = HCMD_ARR(iwl_mvm_legacy_names),
 	[LONG_GROUP] = HCMD_ARR(iwl_mvm_legacy_names),
 	[PHY_OPS_GROUP] = HCMD_ARR(iwl_mvm_phy_names),
+	[DATA_PATH_GROUP] = HCMD_ARR(iwl_mvm_data_path_names),
 	[PROT_OFFLOAD_GROUP] = HCMD_ARR(iwl_mvm_prot_offload_names),
 };
 
