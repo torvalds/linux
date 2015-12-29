cc28d6d80f6ab494b10f0e2ec949eacd610f66e3
xuejiufei <xuejiufei@huawei.com>
2015-12-29 14:54:29 -0800

    ocfs2/dlm: clear migration_pending when migration target goes down
    We have found a BUG on res->migration_pending when migrating lock
    resources.  The situation is as follows.
    
    dlm_mark_lockres_migration
      res->migration_pending = 1;
      __dlm_lockres_reserve_ast
      dlm_lockres_release_ast returns with res->migration_pending remains
          because other threads reserve asts
      wait dlm_migration_can_proceed returns 1
      >>>>>>> o2hb found that target goes down and remove target
              from domain_map
      dlm_migration_can_proceed returns 1
      dlm_mark_lockres_migrating returns -ESHOTDOWN with
          res->migration_pending still remains.
    
    When reentering dlm_mark_lockres_migrating(), it will trigger the BUG_ON
    with res->migration_pending.  So clear migration_pending when target is
    down.
    
    Signed-off-by: Jiufei Xue <xuejiufei@huawei.com>
    Reviewed-by: Joseph Qi <joseph.qi@huawei.com>
    Cc: Mark Fasheh <mfasheh@suse.de>
    Cc: Joel Becker <jlbec@evilplan.org>
    Cc: Junxiao Bi <junxiao.bi@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/ocfs2/dlm/dlmmaster.c
+++ fs/ocfs2/dlm/dlmmaster.c
@@ -2843,6 +2843,8 @@ again:
 	res->state &= ~DLM_LOCK_RES_BLOCK_DIRTY;
 	if (!ret)
 		BUG_ON(!(res->state & DLM_LOCK_RES_MIGRATING));
+	else
+		res->migration_pending = 0;
 	spin_unlock(&res->spinlock);
 
 	/*
