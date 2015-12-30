1a699374533b23ec4deff885db121a5e4c42aa27
Andy Lutomirski <luto@kernel.org>
2015-12-29 20:12:18 -0800

    x86/vsdo: Fix build on PARAVIRT_CLOCK=y, KVM_GUEST=n
    commit 8705d603edd49f1cff165cd3b7998f4c7f098d27 upstream.
    
    arch/x86/built-in.o: In function `arch_setup_additional_pages':
     (.text+0x587): undefined reference to `pvclock_pvti_cpu0_va'
    
    KVM_GUEST selects PARAVIRT_CLOCK, so we can make pvclock_pvti_cpu0_va depend
    on KVM_GUEST.
    
    Signed-off-by: Andy Lutomirski <luto@kernel.org>
    Tested-by: Borislav Petkov <bp@alien8.de>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Kees Cook <keescook@chromium.org>
    Link: http://lkml.kernel.org/r/444d38a9bcba832685740ea1401b569861d09a72.1451446564.git.luto@kernel.org
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: James Dingwall <james@dingwall.me.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/include/asm/pvclock.h
+++ arch/x86/include/asm/pvclock.h
@@ -4,7 +4,7 @@
 #include <linux/clocksource.h>
 #include <asm/pvclock-abi.h>
 
-#ifdef CONFIG_PARAVIRT_CLOCK
+#ifdef CONFIG_KVM_GUEST
 extern struct pvclock_vsyscall_time_info *pvclock_pvti_cpu0_va(void);
 #else
 static inline struct pvclock_vsyscall_time_info *pvclock_pvti_cpu0_va(void)
