ea44463f376236a0541288cb8d575ab6b50370d2
Geliang Tang <geliangtang@163.com>
2016-01-01 22:06:29 +0800

    lockd: use to_delayed_work
    Use to_delayed_work() instead of open-coding it.
    
    Signed-off-by: Geliang Tang <geliangtang@163.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/lockd/svc.c
+++ fs/lockd/svc.c
@@ -94,8 +94,7 @@ static unsigned long get_lockd_grace_period(void)
 
 static void grace_ender(struct work_struct *grace)
 {
-	struct delayed_work *dwork = container_of(grace, struct delayed_work,
-						  work);
+	struct delayed_work *dwork = to_delayed_work(grace);
 	struct lockd_net *ln = container_of(dwork, struct lockd_net,
 					    grace_period_end);
 
