edb0d9d4a3fe65cb08dab750b4ad6c2ae5d65d50
Saatvik Arya <aryasaatvik@gmail.com>
2016-01-03 22:42:40 +0530

    drivers: staging: octeon-usb: octeon-hcd.c: fixed coding style related warnings
    fixed coding style warnings related to comment blocks
    
    Signed-off-by: Saatvik Arya <aryasaatvik@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/octeon-usb/octeon-hcd.c
+++ drivers/staging/octeon-usb/octeon-hcd.c
@@ -2006,7 +2006,8 @@ static void octeon_usb_urb_complete_callback(struct cvmx_usb_state *usb,
 	urb->hcpriv = NULL;
 
 	/* For Isochronous transactions we need to update the URB packet status
-	   list from data in our private copy */
+	 * list from data in our private copy
+	 */
 	if (usb_pipetype(urb->pipe) == PIPE_ISOCHRONOUS) {
 		int i;
 		/*
