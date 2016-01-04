59c34a505bcc39da6be24568c2e569a83cd62b76
Jens Wiklander <jens.wiklander@linaro.org>
2016-01-04 15:42:55 +0100

    ARM: 8479/2: add implementation for arm-smccc
    Adds implementation for arm-smccc and enables CONFIG_HAVE_SMCCC for
    architectures that may support arm-smccc. It's the responsibility of the
    caller to know if the SMC instruction is supported by the platform.
    
    Reviewed-by: Lars Persson <lars.persson@axis.com>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    (cherry picked from commit b329f95d70f3f955093e9a2b18ac1ed3587a8f73)

--- arch/arm/kernel/armksyms.c
+++ arch/arm/kernel/armksyms.c
@@ -16,6 +16,7 @@
 #include <linux/syscalls.h>
 #include <linux/uaccess.h>
 #include <linux/io.h>
+#include <linux/arm-smccc.h>
 
 #include <asm/checksum.h>
 #include <asm/ftrace.h>
@@ -168,4 +169,9 @@ EXPORT_SYMBOL(__gnu_mcount_nc);
 #ifdef CONFIG_ARM_PATCH_PHYS_VIRT
 EXPORT_SYMBOL(__pv_phys_pfn_offset);
 EXPORT_SYMBOL(__pv_offset);
+#endif
+
+#ifdef CONFIG_HAVE_ARM_SMCCC
+EXPORT_SYMBOL(arm_smccc_smc);
+EXPORT_SYMBOL(arm_smccc_hvc);
 #endif
