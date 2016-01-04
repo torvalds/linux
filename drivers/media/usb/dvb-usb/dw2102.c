93b66420a4eb846ed8859d25c0a2056486f2929d
Philipp Zabel <p.zabel@pengutronix.de>
2016-01-04 17:32:51 -0200

    [media] dw2102: Add support for Terratec Cinergy S2 USB BOX
    The Terratec Cinergy S2 USB BOX uses a Montage M88TS2022 tuner
    and a M88DS3103 demodulator, same as Technotrend TT-connect S2-4600.
    This patch adds the missing USB Product ID to make it work.
    
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/usb/dvb-usb/dw2102.c
+++ drivers/media/usb/dvb-usb/dw2102.c
@@ -1688,6 +1688,7 @@ enum dw2102_table_entry {
 	TECHNOTREND_S2_4600,
 	TEVII_S482_1,
 	TEVII_S482_2,
+	TERRATEC_CINERGY_S2_BOX,
 };
 
 static struct usb_device_id dw2102_table[] = {
@@ -1715,6 +1716,7 @@ static struct usb_device_id dw2102_table[] = {
 		USB_PID_TECHNOTREND_CONNECT_S2_4600)},
 	[TEVII_S482_1] = {USB_DEVICE(0x9022, 0xd483)},
 	[TEVII_S482_2] = {USB_DEVICE(0x9022, 0xd484)},
+	[TERRATEC_CINERGY_S2_BOX] = {USB_DEVICE(USB_VID_TERRATEC, 0x0105)},
 	{ }
 };
 
@@ -2232,7 +2234,7 @@ static struct dvb_usb_device_properties tt_s2_4600_properties = {
 		} },
 		}
 	},
-	.num_device_descs = 3,
+	.num_device_descs = 4,
 	.devices = {
 		{ "TechnoTrend TT-connect S2-4600",
 			{ &dw2102_table[TECHNOTREND_S2_4600], NULL },
@@ -2244,6 +2246,10 @@ static struct dvb_usb_device_properties tt_s2_4600_properties = {
 		},
 		{ "TeVii S482 (tuner 2)",
 			{ &dw2102_table[TEVII_S482_2], NULL },
+			{ NULL },
+		},
+		{ "Terratec Cinergy S2 USB BOX",
+			{ &dw2102_table[TERRATEC_CINERGY_S2_BOX], NULL },
 			{ NULL },
 		},
 	}
