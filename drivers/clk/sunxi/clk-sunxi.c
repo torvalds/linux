149851b015e14af11f554be955ba3944bb30ed94
Vishnu Patekar <vishnupatekar0510@gmail.com>
2016-01-06 21:11:52 +0800

    ARM: sunxi: Introduce Allwinner for A83T support
    Allwinner A83T is octa-core cortex-a7 based SoC.
    It's clock control unit and prcm, pinmux are different from previous sun8i
    series.
    Its processor cores are arragned in two clusters 4 cores each,
    similar to A80.
    
    Signed-off-by: Vishnu Patekar <vishnupatekar0510@gmail.com>
    Acked-by: Rob Herring <robh@kernel.org>
    Acked-by: Chen-Yu Tsai <wens@csie.org>
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- drivers/clk/sunxi/clk-sunxi.c
+++ drivers/clk/sunxi/clk-sunxi.c
@@ -1219,6 +1219,12 @@ CLK_OF_DECLARE(sun8i_a23_clk_init, "allwinner,sun8i-a23", sun6i_init_clocks);
 CLK_OF_DECLARE(sun8i_a33_clk_init, "allwinner,sun8i-a33", sun6i_init_clocks);
 CLK_OF_DECLARE(sun8i_h3_clk_init, "allwinner,sun8i-h3", sun6i_init_clocks);
 
+static void __init sun8i_a83t_init_clocks(struct device_node *node)
+{
+	sunxi_init_clocks(NULL, 0);
+}
+CLK_OF_DECLARE(sun8i_a83t_clk_init, "allwinner,sun8i-a83t", sun8i_a83t_init_clocks);
+
 static void __init sun9i_init_clocks(struct device_node *node)
 {
 	sunxi_init_clocks(NULL, 0);
