fdb4ea33201e6b679b6dacb0c80d94f83f7d8f04
Octavian Purdila <octavian.purdila@intel.com>
2016-01-06 16:36:05 +0200

    lkl tools: hijack lib: add support for TAP based networking
    Signed-off-by: Octavian Purdila <octavian.purdila@intel.com>

--- tools/lkl/lib/hijack/init.c
+++ tools/lkl/lib/hijack/init.c
@@ -10,7 +10,21 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <linux/if_tun.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/ioctl.h>
 
+#undef st_atime
+#undef st_mtime
+#undef st_ctime
 #include <lkl.h>
 #include <lkl_host.h>
 
@@ -19,8 +33,48 @@
 void __attribute__((constructor(102)))
 hijack_init(void)
 {
-	int ret;
-	int i, dev_null;
+	int ret, i, dev_null, nd_id = -1, nd_ifindex = -1;
+	char *tap = getenv("LKL_HIJACK_NET_TAP");
+	char *ip = getenv("LKL_HIJACK_NET_IP");
+	char *netmask_len = getenv("LKL_HIJACK_NET_NETMASK_LEN");
+	char *gateway = getenv("LKL_HIJACK_NET_GATEWAY");
+	char *debug = getenv("LKL_HIJACK_DEBUG");
+
+	if (tap) {
+		struct ifreq ifr = {
+			.ifr_flags = IFF_TAP | IFF_NO_PI,
+		};
+		union lkl_netdev nd;
+
+		strncpy(ifr.ifr_name, tap, IFNAMSIZ);
+
+		nd.fd = open("/dev/net/tun", O_RDWR|O_NONBLOCK);
+		if (nd.fd < 0) {
+			fprintf(stderr, "failed to open tap: %s\n", strerror(errno));
+			goto no_tap;
+		}
+
+		ret = ioctl(nd.fd, TUNSETIFF, &ifr);
+		if (ret < 0) {
+			fprintf(stderr, "failed to attach to %s: %s\n",
+				ifr.ifr_name, strerror(errno));
+			goto no_tap;
+		}
+
+		ret = lkl_netdev_add(nd, NULL);
+		if (ret < 0) {
+			fprintf(stderr, "failed to add netdev: %s\n",
+				lkl_strerror(ret));
+			goto no_tap;
+		}
+
+		nd_id = ret;
+	}
+
+no_tap:
+	if (!debug)
+		lkl_host_ops.print = NULL;
+
 
 	ret = lkl_start_kernel(&lkl_host_ops, 64 * 1024 * 1024, "");
 	if (ret) {
@@ -42,6 +96,37 @@ hijack_init(void)
 	/* lo iff_up */
 	lkl_if_up(1);
 
+	if (nd_id >= 0) {
+		nd_ifindex = lkl_netdev_get_ifindex(nd_id);
+		if (nd_ifindex > 0)
+			lkl_if_up(nd_ifindex);
+		else
+			fprintf(stderr, "failed to get ifindex for netdev id %d: %s\n",
+				nd_id, lkl_strerror(nd_ifindex));
+	}
+
+	if (nd_ifindex >= 0 && ip && netmask_len) {
+		unsigned int addr = inet_addr(ip);
+		int nmlen = atoi(netmask_len);
+
+		if (addr != INADDR_NONE && nmlen > 0 && nmlen < 32) {
+			ret = lkl_if_set_ipv4(nd_ifindex, addr, nmlen);
+			if (ret < 0)
+				fprintf(stderr, "failed to set IPv4 address: %s\n",
+					lkl_strerror(ret));
+		}
+	}
+
+	if (nd_ifindex >= 0 && gateway) {
+		unsigned int addr = inet_addr(gateway);
+
+		if (addr != INADDR_NONE) {
+			ret = lkl_set_ipv4_gateway(addr);
+			if (ret< 0)
+				fprintf(stderr, "failed to set IPv4 gateway: %s\n",
+					lkl_strerror(ret));
+		}
+	}
 }
 
 void __attribute__((destructor))
--- tools/lkl/tests/hijack-test.sh
+++ tools/lkl/tests/hijack-test.sh
@@ -12,3 +12,20 @@ echo "== ping test=="
 sudo ${script_dir}/../bin/${hijack_script} ping 127.0.0.1 -c 2
 echo "== ping6 test=="
 sudo ${script_dir}/../bin/${hijack_script} ping6 ::1 -c 2
+
+echo "== TAP tests =="
+if [ -c /dev/net/tun ]; then
+    sudo ip link set dev lkl_ptt0 down || true
+    sudo ip tuntap del dev lkl_ptt0 mode tap || true
+    sudo ip tuntap add dev lkl_ptt0 mode tap user $USER
+    sudo ip link set dev lkl_ptt0 up
+    sudo ip addr add dev lkl_ptt0 192.168.13.1/24
+    LKL_HIJACK_NET_TAP=lkl_ptt0 LKL_HIJACK_NET_IP=192.168.13.2 LKL_HIJACK_NET_NETMASK_LEN=24 ${hijack_script} ip link | grep eth0
+    LKL_HIJACK_NET_TAP=lkl_ptt0 LKL_HIJACK_NET_IP=192.168.13.2 LKL_HIJACK_NET_NETMASK_LEN=24 ${hijack_script} ip addr | grep 192.168.13.2
+    cp `which ping` .
+    LKL_HIJACK_NET_TAP=lkl_ptt0 LKL_HIJACK_NET_IP=192.168.13.2 LKL_HIJACK_NET_NETMASK_LEN=24 ${hijack_script} ./ping 192.168.13.1 -i 0.2 -c 65
+    rm ./ping
+    (sudo arp -d 192.168.13.2 && ping -i 0.2 -c 65 192.168.13.2 & LKL_HIJACK_NET_TAP=lkl_ptt0 LKL_HIJACK_NET_IP=192.168.13.2 LKL_HIJACK_NET_NETMASK_LEN=24 ${hijack_script} sleep 15)
+    sudo ip link set dev lkl_ptt0 down
+    sudo ip tuntap del dev lkl_ptt0 mode tap
+fi;
