819634ec6cf3543120972a715642877b30836f9f
Octavian Purdila <octavian.purdila@intel.com>
2016-01-06 16:39:15 +0200

    lkl tools: hijack lib: tests: run unpriviledged ping
    For LKL we don't need root to run ping, so copy ping to local dir and
    run it from there to make sure the suid bit is disabled.
    
    Also, make sure we don't test the hijack lib it if was not compiled for
    this targer.
    
    Signed-off-by: Octavian Purdila <octavian.purdila@intel.com>

--- tools/lkl/tests/hijack-test.sh
+++ tools/lkl/tests/hijack-test.sh
@@ -4,14 +4,25 @@ script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 hijack_script=lkl-hijack.sh
 export PATH=${script_dir}/../bin/:${PATH}
 
+if ! [ -e ${script_dir}/../lib/liblkl-hijack.so ]; then
+    exit 0;
+fi
+
 echo "== ip addr test=="
 ${hijack_script} ip addr
+
 echo "== ip route test=="
 ${hijack_script} ip route
+
 echo "== ping test=="
-sudo ${script_dir}/../bin/${hijack_script} ping 127.0.0.1 -c 2
+cp `which ping` .
+${script_dir}/../bin/${hijack_script} ./ping 127.0.0.1 -c 2
+rm ping
+
 echo "== ping6 test=="
-sudo ${script_dir}/../bin/${hijack_script} ping6 ::1 -c 2
+cp `which ping6` .
+${script_dir}/../bin/${hijack_script} ./ping6 ::1 -c 2
+rm ping6
 
 echo "== TAP tests =="
 if [ -c /dev/net/tun ]; then
