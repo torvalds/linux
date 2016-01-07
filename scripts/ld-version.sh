8083013fc3201b75535afc39338cc7d2cad32e37
Michael S. Tsirkin <mst@redhat.com>
2016-01-07 19:55:24 +0200

    ld-version: Fix it on Fedora
    On Fedora 23, ld --version outputs:
    GNU ld version 2.25-15.fc23
    
    But ld-version.sh fails to parse this, so e.g.  mips build fails to
    enable VDSO, printing a warning that binutils >= 2.24 is required.
    
    To fix, teach ld-version to parse this format.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Cc: linux-kernel@vger.kernel.org
    Cc: Michal Marek <mmarek@suse.com>
    Cc: linux-kbuild@vger.kernel.org
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/12023/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- scripts/ld-version.sh
+++ scripts/ld-version.sh
@@ -2,6 +2,8 @@
 # extract linker version number from stdin and turn into single number
 	{
 	gsub(".*)", "");
+	gsub(".*version ", "");
+	gsub("-.*", "");
 	split($1,a, ".");
 	print a[1]*100000000 + a[2]*1000000 + a[3]*10000 + a[4]*100 + a[5];
 	exit
