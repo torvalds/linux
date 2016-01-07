239849dde350c9f7356974ca4f62171f50a2c86e
Arnaldo Carvalho de Melo <acme@redhat.com>
2016-01-07 16:51:58 -0300

    perf tests: No need to set attr.sample_freq for tracking !PERF_RECORD_SAMPLE
    We were asking for a 4kHz sample_freq, making the test fail needlessly
    when the system reduced /proc/sys/kernel/perf_event_max_sample_rate
    below that.
    
    Before:
    
      # perf test -vv dummy
      23: Test using a dummy software event to keep tracking       :
      --- start ---
      test child forked, pid 32421
      ------------------------------------------------------------
      perf_event_attr:
        type                             1
        size                             112
        config                           0x9
        { sample_period, sample_freq }   4000
        sample_type                      IP|TID|ID|PERIOD
      <SNIP>
      sys_perf_event_open failed, error -22
      Unable to open dummy and cycles event
      test child finished with -2
      ---- end ----
      Test using a dummy software event to keep tracking: Skip
      #
      [root@zoo ~]# cat /proc/sys/kernel/perf_event_max_sample_rate
      1000
    
    After:
    
      [root@zoo ~]# perf test dummy
      23: Test using a dummy software event to keep tracking       : Ok
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lkml.kernel.org/n/tip-487iquegrs2379e5n0pi0tcp@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/tests/keep-tracking.c
+++ tools/perf/tests/keep-tracking.c
@@ -55,7 +55,6 @@ int test__keep_tracking(int subtest __maybe_unused)
 		.mmap_pages	     = UINT_MAX,
 		.user_freq	     = UINT_MAX,
 		.user_interval	     = ULLONG_MAX,
-		.freq		     = 4000,
 		.target		     = {
 			.uses_mmap   = true,
 		},
