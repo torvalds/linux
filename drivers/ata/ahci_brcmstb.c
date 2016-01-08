6ca92dd7aff899777a3081c5948409f4bdef7d38
Danesh Petigara <dpetigara@broadcom.com>
2016-01-07 16:03:31 -0800

    ata: ahci_brcmstb: disable DIPM support
    The Broadcom STB SATA host controller does not support device
    initiated power management. Disable support for this feature
    so the driver never sends SETFEATURES commands to the device
    to enable/disable DIPM.
    
    Signed-off-by: Danesh Petigara <dpetigara@broadcom.com>
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- drivers/ata/ahci_brcmstb.c
+++ drivers/ata/ahci_brcmstb.c
@@ -84,7 +84,7 @@ struct brcm_ahci_priv {
 };
 
 static const struct ata_port_info ahci_brcm_port_info = {
-	.flags		= AHCI_FLAG_COMMON,
+	.flags		= AHCI_FLAG_COMMON | ATA_FLAG_NO_DIPM,
 	.pio_mask	= ATA_PIO4,
 	.udma_mask	= ATA_UDMA6,
 	.port_ops	= &ahci_platform_ops,
