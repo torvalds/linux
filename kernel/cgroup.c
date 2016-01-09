2cfa2b191e56fd4c5c6ffb72e1d1e82ed225676d
Rami Rosen <rami.rosen@intel.com>
2016-01-09 23:33:06 +0200

    cgroup: fix a typo.
    This patch fixes a typo in a comment in cgroup.c.
    
    Signed-off-by: Rami Rosen <rami.rosen@intel.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -4037,7 +4037,7 @@ int cgroup_transfer_tasks(struct cgroup *to, struct cgroup *from)
 		goto out_err;
 
 	/*
-	 * Migrate tasks one-by-one until @form is empty.  This fails iff
+	 * Migrate tasks one-by-one until @from is empty.  This fails iff
 	 * ->can_attach() fails.
 	 */
 	do {
