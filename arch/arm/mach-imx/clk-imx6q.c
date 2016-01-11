5d0442d7fde05214546e8a1bed183c3384804a0c
Robin Gong <yibin.gong@nxp.com>
2016-01-11 17:25:33 +0800

    MLK-12186: ARM: imx: clk-imx6q: correct gpu2d_core_clock on i.mx6dl
    On i.mx6dl, gpu2d_core_clock share with gpu3d_shader_clock, thus, no need
    change gpu2d_core_clock anymore, since gpu3d_shader has already change.
    This patch also fix the below boot warning, since there is no pll2_pfd1_594m
    in gpu2d_core_sel:
    
    [    0.000000] failed to set parent of clk gpu2d_core_sel to pll2_pfd1_594m: -22
    
    Signed-off-by: Robin Gong <yibin.gong@nxp.com>

--- arch/arm/mach-imx/clk-imx6q.c
+++ arch/arm/mach-imx/clk-imx6q.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2015 Freescale Semiconductor, Inc.
+ * Copyright 2011-2016 Freescale Semiconductor, Inc.
  * Copyright 2011 Linaro Ltd.
  *
  * The code contained herein is licensed under the GNU General Public
@@ -836,9 +836,6 @@ static void __init imx6q_clocks_init(struct device_node *ccm_node)
 		/* for mx6dl, change gpu3d_core parent to 594_PFD*/
 		imx_clk_set_parent(clk[IMX6QDL_CLK_GPU3D_CORE_SEL], clk[IMX6QDL_CLK_PLL2_PFD1_594M]);
 		imx_clk_set_rate(clk[IMX6QDL_CLK_GPU3D_CORE], 528000000);
-		/* for mx6dl, change gpu2d_core parent to 594_PFD*/
-		imx_clk_set_parent(clk[IMX6QDL_CLK_GPU2D_CORE_SEL], clk[IMX6QDL_CLK_PLL2_PFD1_594M]);
-		imx_clk_set_rate(clk[IMX6QDL_CLK_GPU2D_CORE], 528000000);
 	} else if (cpu_is_imx6q()) {
 		if (imx_get_soc_revision() == IMX_CHIP_REVISION_2_0) {
 			imx_clk_set_parent(clk[IMX6QDL_CLK_GPU3D_SHADER_SEL], clk[IMX6QDL_CLK_PLL3_PFD0_720M]);
