b827e1137ca9b495f10f3215ed6dd6ff738f0c81
Rui Miguel Silva <rui.silva@linaro.org>
2016-01-12 14:35:48 +0000

    greybus: kernel_ver: add handle for lights in 4.5
    In v4.5 the LED API is changed related to the set brightness
    operations, set_sync is removed and the workqueue handling is now done
    by leds core, so no need for individual drivers to have one.
    This will be done by removing the SET_SYNC macro and add a new
    SET_BLOCKING.
    
    Signed-off-by: Rui Miguel Silva <rui.silva@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/kernel_ver.h
+++ drivers/staging/greybus/kernel_ver.h
@@ -267,6 +267,15 @@ static inline size_t sg_pcopy_from_buffer(struct scatterlist *sgl,
 #include <linux/led-class-flash.h>
 #endif
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 5, 0)
+/*
+ * New change in LED api, the set_sync operation was renamed to set_blocking and
+ * the workqueue is now handle by core. So, only one set operation is need.
+ */
+#undef LED_HAVE_SET_SYNC
+#define LED_HAVE_SET_BLOCKING
+#endif
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 18, 0)
 /*
  * From this version upper it was introduced the possibility to disable led
