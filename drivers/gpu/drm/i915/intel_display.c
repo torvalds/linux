741598a5bce0a7bb07006cfbcd8044a3c7b91c31
Daniel Vetter <daniel.vetter@ffwll.ch>
2016-01-13 11:55:28 +0100

    drm/i915: Init power domains early in driver load
    commit f5949141a21ee16edf1beaf95cbae7e419171ab5 upstream.
    
    Since
    
    commit ac9b8236551d1177fd07b56aef9b565d1864420d
    Author: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Date:   Fri Nov 27 18:55:26 2015 +0200
    
        drm/i915: Introduce a gmbus power domain
    
    gmbus also needs the power domain infrastructure right from the start,
    since as soon as we register the i2c controllers someone can use them.
    
    v2: Adjust cleanup paths too (Chris).
    
    v3: Rebase onto -nightly (totally bogus tree I had lying around) and
    also move dpio init head (Ville).
    
    v4: Ville instead suggested to move gmbus setup later in the sequence,
    since it's only needed by the modeset code.
    
    v5: Move even close to the actual user, right next to the comment that
    states where we really need gmbus (and interrupts!).
    
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Cc: Patrik Jakobsson <patrik.jakobsson@linux.intel.com>
    Cc: Imre Deak <imre.deak@intel.com>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Cc: Meelis Roos <mroos@linux.ee>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Fixes: ac9b8236551d ("drm/i915: Introduce a gmbus power domain")
    References: http://www.spinics.net/lists/intel-gfx/msg83075.html
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1452682528-19437-1-git-send-email-daniel.vetter@ffwll.ch
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -15565,6 +15565,8 @@ void intel_modeset_cleanup(struct drm_device *dev)
 	mutex_lock(&dev->struct_mutex);
 	intel_cleanup_gt_powersave(dev);
 	mutex_unlock(&dev->struct_mutex);
+
+	intel_teardown_gmbus(dev);
 }
 
 /*
