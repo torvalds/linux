c1cab1df9b651d999922e0f04d01dc046121a237
Miaoqing Pan <miaoqing@codeaurora.org>
2016-01-18 09:33:46 +0800

    ath9k: avoid ANI restart if no trigger
    Fixes commit 54da20d83f0e ("ath9k_hw: improve ANI processing and rx desensitizing parameters")
    
    Call ath9k_ani_restart() only when the phy error rate reach the
    ANI immunity threshold. Sync the logic with internal code base.
    
    Signed-off-by: Miaoqing Pan <miaoqing@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath9k/ani.c
+++ drivers/net/wireless/ath/ath9k/ani.c
@@ -450,7 +450,9 @@ void ath9k_hw_ani_monitor(struct ath_hw *ah, struct ath9k_channel *chan)
 		} else if (cckPhyErrRate > ah->config.cck_trig_high) {
 			ath9k_hw_ani_cck_err_trigger(ah);
 			aniState->ofdmsTurn = true;
-		}
+		} else
+			return;
+			
 		ath9k_ani_restart(ah);
 	}
 }
