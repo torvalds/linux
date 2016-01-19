04a0e4dee85642138dc7bd78f50ebee397e057a8
Tomasz Nowicki <tn@semihalf.com>
2016-01-19 14:11:18 +0100

    irqchip/gic-v3-its: Mark its_init() and its children as __init
    gicv3_init_bases() is the only caller for its_init(),
    also it is a __init function, so mark its_init() as __init too,
    then recursively mark the functions called as __init.
    
    This will help to introduce ITS initialization using ACPI tables as
    we will use acpi_table_parse_entries family functions there which
    belong to __init section as well.
    
    Acked-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Hanjun Guo <hanjun.guo@linaro.org>
    Signed-off-by: Tomasz Nowicki <tn@semihalf.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- drivers/irqchip/irq-gic-v3-its.c
+++ drivers/irqchip/irq-gic-v3-its.c
@@ -670,7 +670,7 @@ static int its_chunk_to_lpi(int chunk)
 	return (chunk << IRQS_PER_CHUNK_SHIFT) + 8192;
 }
 
-static int its_lpi_init(u32 id_bits)
+static int __init its_lpi_init(u32 id_bits)
 {
 	lpi_chunks = its_lpi_to_chunk(1UL << id_bits);
 
@@ -1429,7 +1429,8 @@ static void its_enable_quirks(struct its_node *its)
 	gic_enable_quirks(iidr, its_quirks, its);
 }
 
-static int its_probe(struct device_node *node, struct irq_domain *parent)
+static int __init its_probe(struct device_node *node,
+			    struct irq_domain *parent)
 {
 	struct resource res;
 	struct its_node *its;
@@ -1590,7 +1591,7 @@ static struct of_device_id its_device_id[] = {
 	{},
 };
 
-int its_init(struct device_node *node, struct rdists *rdists,
+int __init its_init(struct device_node *node, struct rdists *rdists,
 	     struct irq_domain *parent_domain)
 {
 	struct device_node *np;
