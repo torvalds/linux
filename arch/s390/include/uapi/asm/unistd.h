696aafd36905a90af87c64d1278f91e39e66ae1e
Heiko Carstens <heiko.carstens@de.ibm.com>
2016-01-19 10:19:02 +0100

    s390: wire up copy_file_range syscall
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/uapi/asm/unistd.h
+++ arch/s390/include/uapi/asm/unistd.h
@@ -310,7 +310,8 @@
 #define __NR_recvmsg		372
 #define __NR_shutdown		373
 #define __NR_mlock2		374
-#define NR_syscalls 375
+#define __NR_copy_file_range	375
+#define NR_syscalls 376
 
 /* 
  * There are some system calls that are not present on 64 bit, some
