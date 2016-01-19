81fba24969c0ca427a8cd3d31bea10749f15b737
Johan Hovold <johan@hovoldconsulting.com>
2016-01-19 12:51:03 +0100

    greybus: connection: always cancel operations on connection disable
    Always cancel all operations on connection disable and remove the now
    unused DESTROYING state.
    
    Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/connection.c
+++ drivers/staging/greybus/connection.c
@@ -426,11 +426,16 @@ EXPORT_SYMBOL_GPL(gb_connection_enable);
 
 void gb_connection_disable(struct gb_connection *connection)
 {
+	if (connection->state == GB_CONNECTION_STATE_DISABLED)
+		return;
+
 	gb_connection_control_disconnected(connection);
 
 	spin_lock_irq(&connection->lock);
 	connection->state = GB_CONNECTION_STATE_DISABLED;
 	spin_unlock_irq(&connection->lock);
+
+	gb_connection_cancel_operations(connection, -ESHUTDOWN);
 
 	gb_connection_svc_connection_destroy(connection);
 	gb_connection_hd_cport_disable(connection);
@@ -483,18 +488,12 @@ EXPORT_SYMBOL_GPL(gb_connection_legacy_init);
 
 void gb_connection_legacy_exit(struct gb_connection *connection)
 {
-	spin_lock_irq(&connection->lock);
-	if (connection->state != GB_CONNECTION_STATE_ENABLED) {
-		spin_unlock_irq(&connection->lock);
+	if (connection->state == GB_CONNECTION_STATE_DISABLED)
 		return;
-	}
-	connection->state = GB_CONNECTION_STATE_DESTROYING;
-	spin_unlock_irq(&connection->lock);
-
-	gb_connection_cancel_operations(connection, -ESHUTDOWN);
-	connection->protocol->connection_exit(connection);
 
 	gb_connection_disable(connection);
+
+	connection->protocol->connection_exit(connection);
 
 	gb_connection_unbind_protocol(connection);
 }
