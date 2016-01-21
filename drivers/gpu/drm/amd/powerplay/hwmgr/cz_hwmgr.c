da26bae051b8c4a13c31f2275a4ec4b560c172e6
Rex Zhu <Rex.Zhu@amd.com>
2016-01-21 19:33:56 +0800

    drm/amd/powerplay: refine the dmesg info.
    this do not mean driver error.
    
    Change-Id: If2080eb4b79fc6389280b7c75cb7998d77090739
    Signed-off-by: Rex Zhu <Rex.Zhu@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/hwmgr/cz_hwmgr.c
+++ drivers/gpu/drm/amd/powerplay/hwmgr/cz_hwmgr.c
@@ -726,8 +726,9 @@ static int cz_tf_update_sclk_limit(struct pp_hwmgr *hwmgr,
 		cz_hwmgr->sclk_dpm.soft_max_clk  = table->entries[table->count - 1].clk;
 
 	clock = hwmgr->display_config.min_core_set_clock;
+;
 	if (clock == 0)
-		printk(KERN_ERR "[ powerplay ] min_core_set_clock not set\n");
+		printk(KERN_INFO "[ powerplay ] min_core_set_clock not set\n");
 
 	if (cz_hwmgr->sclk_dpm.hard_min_clk != clock) {
 		cz_hwmgr->sclk_dpm.hard_min_clk = clock;
