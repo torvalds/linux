1b347b6891335a30131899dd16ec6ca843c578f0
Jacob Siverskog <jacob@teenage.engineering>
2016-01-22 13:39:53 +0100

    ASoC: pcm179x: Split into core and SPI parts
    The pcm179x family supports both SPI and I2C for configuration. This
    patch splits the driver into core and SPI parts, in preparation for
    I2C support.
    
    Signed-off-by: Jacob Siverskog <jacob@teenage.engineering>
    Reviewed-by: Johan Hovold <johan@kernel.org>
    Acked-by: Michael Trimarchi <michael@amarulasolutions.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/pcm179x.h
+++ sound/soc/codecs/pcm179x.h
@@ -24,4 +24,9 @@
 #define PCM1792A_FORMATS (SNDRV_PCM_FMTBIT_S32_LE | SNDRV_PCM_FMTBIT_S24_LE | \
 			  SNDRV_PCM_FMTBIT_S16_LE)
 
+extern const struct regmap_config pcm179x_regmap_config;
+
+int pcm179x_common_init(struct device *dev, struct regmap *regmap);
+int pcm179x_common_exit(struct device *dev);
+
 #endif
