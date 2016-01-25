1c17a353c5a8346b1ce128c60ffb862d156f7e7e
Julia Lawall <julia.lawall@lip6.fr>
2016-01-25 16:21:54 +0100

    usb: gadget: rndis: fix itnull.cocci warnings
    The index variable of list_for_each_entry_safe is an offset from a list
    pointer, and thus should not be NULL.
    
    Generated by: scripts/coccinelle/iterators/itnull.cocci
    
    CC: Geliang Tang <geliangtang@163.com>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Julia Lawall <julia.lawall@lip6.fr>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/gadget/function/rndis.c
+++ drivers/usb/gadget/function/rndis.c
@@ -1009,7 +1009,7 @@ void rndis_free_response(struct rndis_params *params, u8 *buf)
 	rndis_resp_t *r, *n;
 
 	list_for_each_entry_safe(r, n, &params->resp_queue, list) {
-		if (r && r->buf == buf) {
+		if (r->buf == buf) {
 			list_del(&r->list);
 			kfree(r);
 		}
