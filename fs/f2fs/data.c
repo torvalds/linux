7c506896cf933a07460dd9777ac023382446beac
Jaegeuk Kim <jaegeuk@kernel.org>
2016-01-26 11:55:35 -0800

    f2fs: use wq_has_sleeper for cp_wait wait_queue
    We need to use wq_has_sleeper including smp_mb to consider cp_wait concurrency.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -74,8 +74,7 @@ static void f2fs_write_end_io(struct bio *bio)
 		dec_page_count(sbi, F2FS_WRITEBACK);
 	}
 
-	if (!get_pages(sbi, F2FS_WRITEBACK) &&
-			!list_empty(&sbi->cp_wait.task_list))
+	if (!get_pages(sbi, F2FS_WRITEBACK) && wq_has_sleeper(&sbi->cp_wait))
 		wake_up(&sbi->cp_wait);
 
 	bio_put(bio);
