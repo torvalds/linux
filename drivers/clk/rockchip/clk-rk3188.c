e8b63288b37dbb8457b510c9d96f6006da4653f6
Alexander Kochetkov <al.kochet@gmail.com>
2016-01-26 16:34:00 +0300

    clk: rockchip: add hclk_cpubus to the list of rk3188 critical clocks
    hclk_cpubus needs to keep running because it is needed for devices like
    the rom, i2s0 or spdif to be accessible via cpu. Without that all
    accesses to devices (readl/writel) return wrong data. So add it
    to the list of critical clocks.
    
    Fixes: 78eaf6095cc763c ("clk: rockchip: disable unused clocks")
    Signed-off-by: Alexander Kochetkov <al.kochet@gmail.com>
    Cc: stable@vger.kernel.org # 4.1.x-
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>

--- drivers/clk/rockchip/clk-rk3188.c
+++ drivers/clk/rockchip/clk-rk3188.c
@@ -748,6 +748,7 @@ static const char *const rk3188_critical_clocks[] __initconst = {
 	"hclk_peri",
 	"pclk_cpu",
 	"pclk_peri",
+	"hclk_cpubus"
 };
 
 static void __init rk3188_common_clk_init(struct device_node *np)
