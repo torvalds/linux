c9f3c009185e11fcf3a025c36aba2bb3186e33c6
Fugang Duan <b38611@freescale.com>
2016-01-27 16:32:44 +0800

    MLK-12330 Bluetooth: Revert "MLK-12295 Bluetooth: add hdev check to avoid passing null pointer"
    This reverts commit dce2d1ebda17c80bdcb8c9a97a5044988c03809a.
    
    The below piece of code can guarantee that any handler doesn't set HCI_MGMT_NO_HDEV
    will not be called with NULL hdev.
    	...
            no_hdev = (handler->flags & HCI_MGMT_NO_HDEV);
            if (no_hdev != !hdev) {
                    err = mgmt_cmd_status(sk, index, opcode,
                                          MGMT_STATUS_INVALID_INDEX);
                    goto done;
            }
    	...
    
    The issue MLK-12295 is reported mistakenly by coverity CID17648.
    
    Signed-off-by: Fugang Duan <B38611@freescale.com>

--- net/bluetooth/hci_sock.c
+++ net/bluetooth/hci_sock.c
@@ -976,7 +976,7 @@ static int hci_mgmt_cmd(struct hci_mgmt_chan *chan, struct sock *sk,
 	struct hci_dev *hdev = NULL;
 	const struct hci_mgmt_handler *handler;
 	bool var_len, no_hdev;
-	int err = 0;
+	int err;
 
 	BT_DBG("got %zu bytes", msglen);
 
@@ -1060,8 +1060,6 @@ static int hci_mgmt_cmd(struct hci_mgmt_chan *chan, struct sock *sk,
 
 	if (hdev && chan->hdev_init)
 		chan->hdev_init(sk, hdev);
-	else if (!hdev)
-		goto done;
 
 	cp = buf + sizeof(*hdr);
 
