c459dadb94b704e440ab8d3ed9e3df634416088f
Imre Deak <imre.deak@intel.com>
2016-01-28 16:04:12 +0200

    drm/i915/bxt: update list of PCIIDs
    [ Upstream commit 985dd4360fdf2533fe48a33a4a2094f2e4718dc0 ]
    
    Add PCIIDs for new versions of the SOC, based on BSpec. Also add the
    name of the versions as code comment where this is available. The new
    versions don't have any changes visible to the kernel driver.
    
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1453989852-13569-1-git-send-email-imre.deak@intel.com
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/drm/i915_pciids.h
+++ include/drm/i915_pciids.h
@@ -289,6 +289,8 @@
 #define INTEL_BXT_IDS(info) \
 	INTEL_VGA_DEVICE(0x0A84, info), \
 	INTEL_VGA_DEVICE(0x1A84, info), \
-	INTEL_VGA_DEVICE(0x5A84, info)
+	INTEL_VGA_DEVICE(0x1A85, info), \
+	INTEL_VGA_DEVICE(0x5A84, info), /* APL HD Graphics 505 */ \
+	INTEL_VGA_DEVICE(0x5A85, info)  /* APL HD Graphics 500 */
 
 #endif /* _I915_PCIIDS_H */
