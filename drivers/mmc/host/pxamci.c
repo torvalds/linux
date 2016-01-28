41c89159a6ae5472d39ed8bded5b3b4e07a37944
Robert Jarzmik <robert.jarzmik@free.fr>
2016-01-29 00:21:26 +0100

    mmc: pxamci: fix again read-only gpio detection polarity
    The commit fixing the conversion of pxamci to slot-gpio API fixed the
    inverted the logic of the read-only gpio. Unfortunately, the commit was
    tested on a non-inverted gpio, and not on the inverted one. And the fix
    did work partially, by luck.
    
    This is the remaining missing part of the fix, trivial but still necessary.
    
    Fixes: Fixes: 26d49fe71953 ("mmc: pxamci: fix read-only gpio detection polarity")
    Reported-by: Andrea Adami <andrea.adami@gmail.com>
    Tested-by: Andrea Adami <andrea.adami@gmail.com>
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>
    Cc: stable@vger.kernel.org
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/pxamci.c
+++ drivers/mmc/host/pxamci.c
@@ -804,7 +804,7 @@ static int pxamci_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "Failed requesting gpio_ro %d\n", gpio_ro);
 		goto out;
 	} else {
-		mmc->caps |= host->pdata->gpio_card_ro_invert ?
+		mmc->caps2 |= host->pdata->gpio_card_ro_invert ?
 			0 : MMC_CAP2_RO_ACTIVE_HIGH;
 	}
 
