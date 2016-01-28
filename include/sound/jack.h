3dce7164d2e3405a5557bcb909a87f5a7d9ad337
Sudheer Papothi <spapothi@codeaurora.org>
2016-01-29 00:45:30 +0530

    ANDROID: GKI: ALSA: jack: update jack types
    Change updates the jack types that will be supported
    by the platforms.
    
    Test: build
    Bug: 151372815
    Change-Id: I948c6ffe4cc2cadd710242637c625006715ff7cc
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>
    Signed-off-by: Sudheer Papothi <spapothi@codeaurora.org>
    Signed-off-by: Meng Wang <mwang@codeaurora.org>
    (cherry picked from commit e7fcc54d6dd81ea5d24ac01202ace1738026a3ba)
    Signed-off-by: Hridya Valsaraju <hridya@google.com>

--- include/sound/jack.h
+++ include/sound/jack.h
@@ -58,6 +58,9 @@ enum snd_jack_types {
 	SND_JACK_VIDEOOUT	= 0x0010,
 	SND_JACK_AVOUT		= SND_JACK_LINEOUT | SND_JACK_VIDEOOUT,
 	SND_JACK_LINEIN		= 0x0020,
+	SND_JACK_OC_HPHL        = 0x0040,
+	SND_JACK_OC_HPHR        = 0x0080,
+	SND_JACK_UNSUPPORTED    = 0x0100,
 
 	/* Kept separate from switches to facilitate implementation */
 	SND_JACK_BTN_0		= 0x4000,
