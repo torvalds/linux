fae4e82609b0887d6d675170d0c20b6af45d83ba
Douglas Anderson <dianders@chromium.org>
2016-01-28 18:20:10 -0800

    usb: dwc2: host: Add dwc2_hcd_get_future_frame_number() call
    As we start getting more exact about our scheduling it's becoming more
    and more important to know exactly how far through the current frame we
    are.  This lets us make decisions about whether there's still time left
    to start a new transaction in the current frame.
    
    We'll add dwc2_hcd_get_future_frame_number() which will tell you what
    the frame number will be a certain number of microseconds (us) from
    now.  We can use this information to help decide if there's enough time
    left in the frame for a transaction that will take a certain duration.
    
    This is expected to be used by a future change ("usb: dwc2: host:
    Properly set even/odd frame").
    
    Acked-by: John Youn <johnyoun@synopsys.com>
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Tested-by: Heiko Stuebner <heiko@sntech.de>
    Tested-by: Stefan Wahren <stefan.wahren@i2se.com>
    Signed-off-by: Felipe Balbi <balbi@kernel.org>

--- drivers/usb/dwc2/core.h
+++ drivers/usb/dwc2/core.h
@@ -1235,11 +1235,15 @@ static inline int dwc2_hsotg_set_test_mode(struct dwc2_hsotg *hsotg,
 
 #if IS_ENABLED(CONFIG_USB_DWC2_HOST) || IS_ENABLED(CONFIG_USB_DWC2_DUAL_ROLE)
 extern int dwc2_hcd_get_frame_number(struct dwc2_hsotg *hsotg);
+extern int dwc2_hcd_get_future_frame_number(struct dwc2_hsotg *hsotg, int us);
 extern void dwc2_hcd_connect(struct dwc2_hsotg *hsotg);
 extern void dwc2_hcd_disconnect(struct dwc2_hsotg *hsotg, bool force);
 extern void dwc2_hcd_start(struct dwc2_hsotg *hsotg);
 #else
 static inline int dwc2_hcd_get_frame_number(struct dwc2_hsotg *hsotg)
+{ return 0; }
+static inline int dwc2_hcd_get_future_frame_number(struct dwc2_hsotg *hsotg,
+						   int us)
 { return 0; }
 static inline void dwc2_hcd_connect(struct dwc2_hsotg *hsotg) {}
 static inline void dwc2_hcd_disconnect(struct dwc2_hsotg *hsotg, bool force) {}
