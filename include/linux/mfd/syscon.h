8c037e0c8eaa7dcb3a190b9be841ee81edb865ff
Philipp Zabel <p.zabel@pengutronix.de>
2016-01-29 09:42:19 +0100

    mfd: syscon: Return ENOTSUPP instead of ENOSYS when disabled
    When CONFIG_MFD_SYSCON is disabled, have the function stubs return
    ENOTSUPP to indicate the syscon functionality is not available.
    There are currently no callers that depend on the ENOSYS return value.
    
    This patchfixes a checkpatch warning:
        WARNING: ENOSYS means 'invalid syscall nr' and nothing else
    
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/syscon.h
+++ include/linux/mfd/syscon.h
@@ -29,24 +29,24 @@ extern struct regmap *syscon_regmap_lookup_by_phandle(
 #else
 static inline struct regmap *syscon_node_to_regmap(struct device_node *np)
 {
-	return ERR_PTR(-ENOSYS);
+	return ERR_PTR(-ENOTSUPP);
 }
 
 static inline struct regmap *syscon_regmap_lookup_by_compatible(const char *s)
 {
-	return ERR_PTR(-ENOSYS);
+	return ERR_PTR(-ENOTSUPP);
 }
 
 static inline struct regmap *syscon_regmap_lookup_by_pdevname(const char *s)
 {
-	return ERR_PTR(-ENOSYS);
+	return ERR_PTR(-ENOTSUPP);
 }
 
 static inline struct regmap *syscon_regmap_lookup_by_phandle(
 					struct device_node *np,
 					const char *property)
 {
-	return ERR_PTR(-ENOSYS);
+	return ERR_PTR(-ENOTSUPP);
 }
 #endif
 
