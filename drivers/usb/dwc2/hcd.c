1062ece06ec0ed50dbd61e23cf3e2d37a2f9654a
Douglas Anderson <dianders@chromium.org>
2016-01-28 18:20:10 -0800

    UPSTREAM: usb: dwc2: host: Add dwc2_hcd_get_future_frame_number() call
    As we start getting more exact about our scheduling it's becoming more
    and more important to know exactly how far through the current frame we
    are.  This lets us make decisions about whether there's still time left
    to start a new transaction in the current frame.
    
    We'll add dwc2_hcd_get_future_frame_number() which will tell you what
    the frame number will be a certain number of microseconds (us) from
    now.  We can use this information to help decide if there's enough time
    left in the frame for a transaction that will take a certain duration.
    
    This is expected to be used by a future change ("usb: dwc2: host:
    Properly set even/odd frame").
    
    Change-Id: I3cb39be7281e537696b6378e798c36ccea254739
    Acked-by: John Youn <johnyoun@synopsys.com>
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Tested-by: Heiko Stuebner <heiko@sntech.de>
    Tested-by: Stefan Wahren <stefan.wahren@i2se.com>
    Signed-off-by: Felipe Balbi <balbi@kernel.org>
    Signed-off-by: Huang, Tao <huangtao@rock-chips.com>
    (cherry picked from commit fae4e82609b0887d6d675170d0c20b6af45d83ba)

--- drivers/usb/dwc2/hcd.c
+++ drivers/usb/dwc2/hcd.c
@@ -1947,6 +1947,35 @@ int dwc2_hcd_get_frame_number(struct dwc2_hsotg *hsotg)
 	return (hfnum & HFNUM_FRNUM_MASK) >> HFNUM_FRNUM_SHIFT;
 }
 
+int dwc2_hcd_get_future_frame_number(struct dwc2_hsotg *hsotg, int us)
+{
+	u32 hprt = dwc2_readl(hsotg->regs + HPRT0);
+	u32 hfir = dwc2_readl(hsotg->regs + HFIR);
+	u32 hfnum = dwc2_readl(hsotg->regs + HFNUM);
+	unsigned int us_per_frame;
+	unsigned int frame_number;
+	unsigned int remaining;
+	unsigned int interval;
+	unsigned int phy_clks;
+
+	/* High speed has 125 us per (micro) frame; others are 1 ms per */
+	us_per_frame = (hprt & HPRT0_SPD_MASK) ? 1000 : 125;
+
+	/* Extract fields */
+	frame_number = (hfnum & HFNUM_FRNUM_MASK) >> HFNUM_FRNUM_SHIFT;
+	remaining = (hfnum & HFNUM_FRREM_MASK) >> HFNUM_FRREM_SHIFT;
+	interval = (hfir & HFIR_FRINT_MASK) >> HFIR_FRINT_SHIFT;
+
+	/*
+	 * Number of phy clocks since the last tick of the frame number after
+	 * "us" has passed.
+	 */
+	phy_clks = (interval - remaining) +
+		   DIV_ROUND_UP(interval * us, us_per_frame);
+
+	return dwc2_frame_num_inc(frame_number, phy_clks / interval);
+}
+
 int dwc2_hcd_is_b_host(struct dwc2_hsotg *hsotg)
 {
 	return hsotg->op_state == OTG_STATE_B_HOST;
