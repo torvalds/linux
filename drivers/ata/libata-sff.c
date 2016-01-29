a588afc920bc50e894f6ae2874c4281c795e0979
Tejun Heo <tj@kernel.org>
2016-01-29 07:06:53 -0500

    libata-sff: use WARN instead of BUG on illegal host state machine state
    ata_sff_hsm_move() triggers BUG if it sees a host state machine state
    that it dind't expect.  The risk for data corruption when the
    condition occurs is low as it's highly unlikely that it would lead to
    spurious completion of commands.  The BUG occasionally triggered for
    subtle race conditions in the driver.  Let's downgrade it to WARN so
    that it doesn't kill the machine unnecessarily.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Dmitry Vyukov <dvyukov@google.com>

--- drivers/ata/libata-sff.c
+++ drivers/ata/libata-sff.c
@@ -1296,7 +1296,8 @@ fsm_start:
 		break;
 	default:
 		poll_next = 0;
-		BUG();
+		WARN(true, "ata%d: SFF host state machine in invalid state %d",
+		     ap->print_id, ap->hsm_task_state);
 	}
 
 	return poll_next;
