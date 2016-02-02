799487f51d7419e33736d81b0b9087b2aaa593af
Mika Kahola <mika.kahola@intel.com>
2016-02-02 15:16:38 +0200

    drm/i915: DisplayPort pixel clock check
    It is possible the we request to have a mode that has
    higher pixel clock than our HW can support. This patch
    checks if requested pixel clock is lower than the one
    supported by the HW. The requested mode is discarded
    if we cannot support the requested pixel clock.
    
    This patch applies to DisplayPort.
    
    V2:
    - removed computation for max DOT clock
    
    V3:
    - cleanup by removing unnecessary lines
    
    V4:
    - max_pixclk renamed as max_dotclk
    
    Signed-off-by: Mika Kahola <mika.kahola@intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1454419003-6001-2-git-send-email-mika.kahola@intel.com

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -203,6 +203,7 @@ intel_dp_mode_valid(struct drm_connector *connector,
 	struct drm_display_mode *fixed_mode = intel_connector->panel.fixed_mode;
 	int target_clock = mode->clock;
 	int max_rate, mode_rate, max_lanes, max_link_clock;
+	int max_dotclk = to_i915(connector->dev)->max_dotclk_freq;
 
 	if (is_edp(intel_dp) && fixed_mode) {
 		if (mode->hdisplay > fixed_mode->hdisplay)
@@ -220,7 +221,7 @@ intel_dp_mode_valid(struct drm_connector *connector,
 	max_rate = intel_dp_max_data_rate(max_link_clock, max_lanes);
 	mode_rate = intel_dp_link_required(target_clock, 18);
 
-	if (mode_rate > max_rate)
+	if (mode_rate > max_rate || target_clock > max_dotclk)
 		return MODE_CLOCK_HIGH;
 
 	if (mode->clock < 10000)
