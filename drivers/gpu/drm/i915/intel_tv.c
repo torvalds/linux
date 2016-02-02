54c032b354fa74bab32fc7b8c020d4e1934dc654
Mika Kahola <mika.kahola@intel.com>
2016-02-02 15:16:43 +0200

    drm/i915: TV pixel clock check
    It is possible the we request to have a mode that has
    higher pixel clock than our HW can support. This patch
    checks if requested pixel clock is lower than the one
    supported by the HW. The requested mode is discarded
    if we cannot support the requested pixel clock.
    
    This patch applies to TV.
    
    V2:
    - removed computation for max pixel clock
    
    V3:
    - cleanup by removing unnecessary lines
    
    V4:
    - max_pixclk variable renamed as max_dotclk
    
    Signed-off-by: Mika Kahola <mika.kahola@intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1454419003-6001-7-git-send-email-mika.kahola@intel.com

--- drivers/gpu/drm/i915/intel_tv.c
+++ drivers/gpu/drm/i915/intel_tv.c
@@ -897,6 +897,10 @@ intel_tv_mode_valid(struct drm_connector *connector,
 {
 	struct intel_tv *intel_tv = intel_attached_tv(connector);
 	const struct tv_mode *tv_mode = intel_tv_mode_find(intel_tv);
+	int max_dotclk = to_i915(connector->dev)->max_dotclk_freq;
+
+	if (mode->clock > max_dotclk)
+		return MODE_CLOCK_HIGH;
 
 	/* Ensure TV refresh is close to desired refresh */
 	if (tv_mode && abs(tv_mode->refresh - drm_mode_vrefresh(mode) * 1000)
