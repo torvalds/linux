67fe85dd6df202d66a46054486d2b339ebed84c4
LABBE Corentin <clabbe.montjoie@gmail.com>
2016-02-04 15:03:52 +0100

    drm: modes: add missing [drm] to message printing
    The warning message in drm_mode_parse_command_line_for_connector miss
    the [drm] at beginning.
    This patch add it and take the opportunity to convert
    printk(KERN_WARNING to pr_warn()
    
    Signed-off-by: LABBE Corentin <clabbe.montjoie@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1454594633-15100-1-git-send-email-clabbe.montjoie@gmail.com

--- drivers/gpu/drm/drm_modes.c
+++ drivers/gpu/drm/drm_modes.c
@@ -1371,8 +1371,7 @@ bool drm_mode_parse_command_line_for_connector(const char *mode_option,
 	}
 done:
 	if (i >= 0) {
-		printk(KERN_WARNING
-			"parse error at position %i in video mode '%s'\n",
+		pr_warn("[drm] parse error at position %i in video mode '%s'\n",
 			i, name);
 		mode->specified = false;
 		return false;
