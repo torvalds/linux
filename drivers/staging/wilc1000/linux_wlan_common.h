c9f75c3071a469a0a4cac0c5d32c47873bca9bf5
Chris Park <chris.park@atmel.com>
2016-02-04 18:24:29 +0900

    staging: wilc1000: remove unused debug tag
    This patch remove unused debug tag INT_DBG, LOCK_DBG, BUS_DBG,
    MEM_DBG and FIRM_DBG
    
    Signed-off-by: Chris Park <chris.park@atmel.com>
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/linux_wlan_common.h
+++ drivers/staging/wilc1000/linux_wlan_common.h
@@ -5,29 +5,14 @@ enum debug_region {
 	Generic_debug = 0,
 	Hostapd_debug,
 	CFG80211_debug,
-	Interrupt_debug,
-	Lock_debug,
-	Tcp_enhance,
-	Spin_debug,
-
 	Init_debug,
-	Bus_debug,
-	Mem_debug,
-	Firmware_debug,
 	COMP = 0xFFFFFFFF,
 };
 
 #define GENERIC_DBG             (1 << Generic_debug)
 #define HOSTAPD_DBG             (1 << Hostapd_debug)
 #define CFG80211_DBG            (1 << CFG80211_debug)
-#define INT_DBG                 (1 << Interrupt_debug)
-#define LOCK_DBG                (1 << Lock_debug)
-#define TCP_ENH                 (1 << Tcp_enhance)
-#define SPIN_DEBUG              (1 << Spin_debug)
 #define INIT_DBG                (1 << Init_debug)
-#define BUS_DBG                 (1 << Bus_debug)
-#define MEM_DBG                 (1 << Mem_debug)
-#define FIRM_DBG                (1 << Firmware_debug)
 
 #if defined (WILC_DEBUGFS)
 extern atomic_t WILC_REGION;
