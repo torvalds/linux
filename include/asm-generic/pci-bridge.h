b6b83f7fdae7cae5260b907510619a47bd29cd70
Bjorn Helgaas <bhelgaas@google.com>
2016-02-05 14:58:07 -0600

    PCI: Remove empty asm-generic/pci-bridge.h
    include/asm-generic/pci-bridge.h is empty, and nobody includes it, so
    remove it.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- include/asm-generic/pci-bridge.h
+++
@@ -1,9 +0,0 @@
-/*
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version
- * 2 of the License, or (at your option) any later version.
- */
-#ifndef _ASM_GENERIC_PCI_BRIDGE_H
-#define _ASM_GENERIC_PCI_BRIDGE_H
-#endif	/* _ASM_GENERIC_PCI_BRIDGE_H */
