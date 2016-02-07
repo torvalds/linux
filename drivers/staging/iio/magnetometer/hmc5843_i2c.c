4630adb8ec5a89dca07bf39c341256803c4f1a46
Cristina Moraru <cristina.moraru09@gmail.com>
2016-02-08 00:21:50 +0200

    iio: hmc5843: Fix comment style warnings
    Fix comment style warnings in order to comply with Coding Style
    standard provided by Documentation/CodingStyle.
    
    This patch solves following issue found with checkpatch:
    WARNING: Block comments use a trailing */ on a separate line
    
    Signed-off-by: Cristina Moraru <cristina.moraru09@gmail.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/magnetometer/hmc5843_i2c.c
+++ drivers/staging/iio/magnetometer/hmc5843_i2c.c
@@ -7,8 +7,7 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
- *
- * */
+ */
 
 #include <linux/module.h>
 #include <linux/i2c.h>
