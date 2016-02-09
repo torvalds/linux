b3232842dbef8e6356af75b3a98ea7f752879bc8
Haneen Mohammed <hamohammed.sa@gmail.com>
2016-02-09 23:06:40 +0300

    Staging: rts5208: remove unnecessary parantheses
    This patch removes unnecessary parantheses around rtsx->pci->dev.
    
    Signed-off-by: Haneen Mohammed <hamohammed.sa@gmail.com>
    Acked-by: Julia Lawall <julia.lawall@lip6.fr>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rts5208/rtsx_transport.c
+++ drivers/staging/rts5208/rtsx_transport.c
@@ -652,7 +652,7 @@ static int rtsx_transfer_buf(struct rtsx_chip *chip, u8 card, void *buf,
 	else
 		return -ENXIO;
 
-	addr = dma_map_single(&(rtsx->pci->dev), buf, len, dma_dir);
+	addr = dma_map_single(&rtsx->pci->dev, buf, len, dma_dir);
 	if (dma_mapping_error(&rtsx->pci->dev, addr))
 		return -ENOMEM;
 
