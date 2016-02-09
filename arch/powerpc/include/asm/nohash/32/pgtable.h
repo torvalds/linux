be00ed728c32ed8311d5191637067c9eaf0fca4b
Christophe Leroy <christophe.leroy@c-s.fr>
2016-02-09 17:07:56 +0100

    powerpc32: Fix pte_offset_kernel() to return NULL for bad pages
    The fixmap related functions try to map kernel pages that are
    already mapped through Large TLBs. pte_offset_kernel() has to
    return NULL for LTLBs, otherwise the caller will try to access
    level 2 table which doesn't exist
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Scott Wood <oss@buserror.net>

--- arch/powerpc/include/asm/nohash/32/pgtable.h
+++ arch/powerpc/include/asm/nohash/32/pgtable.h
@@ -309,7 +309,8 @@ static inline void __ptep_set_access_flags(pte_t *ptep, pte_t entry)
 #define pte_index(address)		\
 	(((address) >> PAGE_SHIFT) & (PTRS_PER_PTE - 1))
 #define pte_offset_kernel(dir, addr)	\
-	((pte_t *) pmd_page_vaddr(*(dir)) + pte_index(addr))
+	(pmd_bad(*(dir)) ? NULL : (pte_t *)pmd_page_vaddr(*(dir)) + \
+				  pte_index(addr))
 #define pte_offset_map(dir, addr)		\
 	((pte_t *) kmap_atomic(pmd_page(*(dir))) + pte_index(addr))
 #define pte_unmap(pte)		kunmap_atomic(pte)
