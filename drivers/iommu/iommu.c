06bfcaa91f0eff1aeab37c74056afa79fb450aea
Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
2016-02-10 10:18:04 +0900

    iommu: Fix second argument of trace_map() to report correct paddr
    Since iommu_map() code added pgsize value to the paddr, trace_map()
    used wrong paddr. So, this patch adds "orig_paddr" value in the
    iommu_map() to use for the trace_map().
    
    Signed-off-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -1314,6 +1314,7 @@ int iommu_map(struct iommu_domain *domain, unsigned long iova,
 	unsigned long orig_iova = iova;
 	unsigned int min_pagesz;
 	size_t orig_size = size;
+	phys_addr_t orig_paddr = paddr;
 	int ret = 0;
 
 	if (unlikely(domain->ops->map == NULL ||
@@ -1358,7 +1359,7 @@ int iommu_map(struct iommu_domain *domain, unsigned long iova,
 	if (ret)
 		iommu_unmap(domain, orig_iova, orig_size - size);
 	else
-		trace_map(orig_iova, paddr, orig_size);
+		trace_map(orig_iova, orig_paddr, orig_size);
 
 	return ret;
 }
