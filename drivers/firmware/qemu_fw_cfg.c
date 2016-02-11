9b3ec23ae9e4cd810f754c4cb9bff030a3a9d3ac
Valentin Rothberg <valentin.rothberg@posteo.net>
2016-02-11 12:19:03 +0100

    firmware: qemu_fw_cfg.c: fix typo FW_CFG_DATA_OFF
    s/FW_CTRL_DATA_OFF/FW_CFG_DATA_OFF/
    
    Signed-off-by: Valentin Rothberg <valentin.rothberg@posteo.net>
    Signed-off-by: Andreas Ziegler <andreas.ziegler@fau.de>
    Acked-by: Gabriel Somlo <somlo@cmu.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/firmware/qemu_fw_cfg.c
+++ drivers/firmware/qemu_fw_cfg.c
@@ -98,7 +98,7 @@ static void fw_cfg_io_cleanup(void)
 }
 
 /* arch-specific ctrl & data register offsets are not available in ACPI, DT */
-#if !(defined(FW_CFG_CTRL_OFF) && defined(FW_CTRL_DATA_OFF))
+#if !(defined(FW_CFG_CTRL_OFF) && defined(FW_CFG_DATA_OFF))
 # if (defined(CONFIG_ARM) || defined(CONFIG_ARM64))
 #  define FW_CFG_CTRL_OFF 0x08
 #  define FW_CFG_DATA_OFF 0x00
