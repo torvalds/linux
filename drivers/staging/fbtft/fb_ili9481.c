61abd03a3dba2f6a73c06930075935683b474fa8
Eva Rachel Retuya <eraretuya@gmail.com>
2016-02-12 15:39:30 +0800

    staging: fbtft: Remove the use of CamelCase
    Rename the macro defining constant ROWxCOL into ROW_X_COL to remove
    checkpatch check concerning use of CamelCase and to achieve better
    readability.
    
    CHECK: Avoid CamelCase: <ROWxCOL>
    
    Signed-off-by: Eva Rachel Retuya <eraretuya@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fbtft/fb_ili9481.c
+++ drivers/staging/fbtft/fb_ili9481.c
@@ -64,13 +64,13 @@ static void set_addr_win(struct fbtft_par *par, int xs, int ys, int xe, int ye)
 
 #define HFLIP 0x01
 #define VFLIP 0x02
-#define ROWxCOL 0x20
+#define ROW_X_COL 0x20
 static int set_var(struct fbtft_par *par)
 {
 	switch (par->info->var.rotate) {
 	case 270:
 		write_reg(par, MIPI_DCS_SET_ADDRESS_MODE,
-			  ROWxCOL | HFLIP | VFLIP | (par->bgr << 3));
+			  ROW_X_COL | HFLIP | VFLIP | (par->bgr << 3));
 		break;
 	case 180:
 		write_reg(par, MIPI_DCS_SET_ADDRESS_MODE,
@@ -78,7 +78,7 @@ static int set_var(struct fbtft_par *par)
 		break;
 	case 90:
 		write_reg(par, MIPI_DCS_SET_ADDRESS_MODE,
-			  ROWxCOL | (par->bgr << 3));
+			  ROW_X_COL | (par->bgr << 3));
 		break;
 	default:
 		write_reg(par, MIPI_DCS_SET_ADDRESS_MODE,
