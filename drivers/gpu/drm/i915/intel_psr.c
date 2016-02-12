9b58e352b463f2f096d699d47b1c4c57879b617f
Rodrigo Vivi <rodrigo.vivi@intel.com>
2016-02-12 04:08:13 -0800

    drm/i915: Enable PSR by default on Haswell and Broadwell.
    With a reliable frontbuffer tracking and all instability corner cases
    on Haswell and Broadwell solved let's re-enabled PSR by default on
    these platforms.
    
    In case a new issue is found and PSR is the main suspect, please check
    if i915.enable_psr=0 really makes your problem go away. If this is the case
    PSR is the culprit so after that please check if i915.enable_psr=2
    or i915.enable_psr=3 solves your issue and please let us know.
    There are many panels out there and not all implementations apparently
    work as we would expect.
    
    In case you needed to force it on standby or disabled or in case of any
    PSR related bug please report it at bugs.freedesktop.org.
    In a bugzilla entry for PSR is desirable:
    - dmesg (drm.debug=0xe)
    - output of /sys/kernel/debug/dri/0/i915_edp_psr_status
    - Platform information. Vendor, model, id, pci id.
    - Graphical environment: Gnome, KDE, openbox, etc...
    - Details how to reproduce.
    - Also good if you could run PSR test cases of Intel-gpu-tools
    - Please mention if forcing main link standby or main link off helps you.
    
    There are Intel-gpu-tools test cases that can be helpful to
    determine if PSR is working as expected:
    kms_psr_sink_crc and kms_psr_frontbuffer_tracking.
    
    Cc: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1455278893-1307-2-git-send-email-rodrigo.vivi@intel.com
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_psr.c
+++ drivers/gpu/drm/i915/intel_psr.c
@@ -780,7 +780,8 @@ void intel_psr_init(struct drm_device *dev)
 
 	/* Per platform default */
 	if (i915.enable_psr == -1) {
-		if (IS_VALLEYVIEW(dev) || IS_CHERRYVIEW(dev))
+		if (IS_HASWELL(dev) || IS_BROADWELL(dev) ||
+		    IS_VALLEYVIEW(dev) || IS_CHERRYVIEW(dev))
 			i915.enable_psr = 1;
 		else
 			i915.enable_psr = 0;
