fbbef866fce2d81aa1791d0c762afb07dbc4e660
Colin Ian King <colin.king@canonical.com>
2016-02-15 22:54:47 +0000

    net-sysfs: remove unused fmt_long_hex
    Ever since commit 04ed3e741d0f133e02bed7fa5c98edba128f90e7
    ("net: change netdev->features to u32") the format string
    fmt_long_hex has not been used, so we may as well remove it.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/net-sysfs.c
+++ net/core/net-sysfs.c
@@ -29,7 +29,6 @@
 
 #ifdef CONFIG_SYSFS
 static const char fmt_hex[] = "%#x\n";
-static const char fmt_long_hex[] = "%#lx\n";
 static const char fmt_dec[] = "%d\n";
 static const char fmt_ulong[] = "%lu\n";
 static const char fmt_u64[] = "%llu\n";
