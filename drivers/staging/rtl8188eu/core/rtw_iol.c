a3c0c480e8908c478385a62aa4f9e6ba706478a0
Colin Vidal <colin@cvidal.org>
2016-02-16 23:12:19 +0100

    Staging: rtl8188eu/core: Coding style fix, avoid line over 80 characters
    Jump a new line after and operator of the test. It avoids to exceed 80
    chars line, and remove a checkpatch warning.
    
    Signed-off-by: Colin Vidal <colin@cvidal.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/core/rtw_iol.c
+++ drivers/staging/rtl8188eu/core/rtw_iol.c
@@ -21,7 +21,8 @@ bool rtw_IOL_applied(struct adapter *adapter)
 	if (adapter->registrypriv.fw_iol == 1)
 		return true;
 
-	if ((adapter->registrypriv.fw_iol == 2) && (!adapter_to_dvobj(adapter)->ishighspeed))
+	if ((adapter->registrypriv.fw_iol == 2) &&
+	    (!adapter_to_dvobj(adapter)->ishighspeed))
 		return true;
 	return false;
 }
