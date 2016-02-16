369bc9abf22bf026e8645a4dd746b90649a2f6ee
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2016-02-16 13:52:33 +0100

    of/fdt: factor out assignment of initrd_start/initrd_end
    Since architectures may not yet have their linear mapping up and running
    when the initrd address is discovered from the DT, factor out the
    assignment of initrd_start and initrd_end, so that an architecture can
    override it and use the translation it needs.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Acked-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- drivers/of/fdt.c
+++ drivers/of/fdt.c
@@ -760,6 +760,16 @@ const void * __init of_flat_dt_match_machine(const void *default_match,
 }
 
 #ifdef CONFIG_BLK_DEV_INITRD
+#ifndef __early_init_dt_declare_initrd
+static void __early_init_dt_declare_initrd(unsigned long start,
+					   unsigned long end)
+{
+	initrd_start = (unsigned long)__va(start);
+	initrd_end = (unsigned long)__va(end);
+	initrd_below_start_ok = 1;
+}
+#endif
+
 /**
  * early_init_dt_check_for_initrd - Decode initrd location from flat tree
  * @node: reference to node containing initrd location ('chosen')
@@ -782,9 +792,7 @@ static void __init early_init_dt_check_for_initrd(unsigned long node)
 		return;
 	end = of_read_number(prop, len/4);
 
-	initrd_start = (unsigned long)__va(start);
-	initrd_end = (unsigned long)__va(end);
-	initrd_below_start_ok = 1;
+	__early_init_dt_declare_initrd(start, end);
 
 	pr_debug("initrd_start=0x%llx  initrd_end=0x%llx\n",
 		 (unsigned long long)start, (unsigned long long)end);
