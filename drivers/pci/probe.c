788858ebc49a07fe5f812778f245a51b0d800d82
Jake Oshins <jakeo@microsoft.com>
2016-02-16 21:56:22 +0000

    PCI: Look up IRQ domain by fwnode_handle
    If pci_host_bridge_msi_domain() can't find an IRQ domain through the OF
    tree, try to look it up directly through the fwnode_handle.
    
    [bhelgaas: changelog]
    Signed-off-by: Jake Oshins <jakeo@microsoft.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -15,6 +15,7 @@
 #include <linux/pci-aspm.h>
 #include <linux/aer.h>
 #include <linux/acpi.h>
+#include <linux/irqdomain.h>
 #include <asm-generic/pci-bridge.h>
 #include "pci.h"
 
@@ -674,6 +675,20 @@ static struct irq_domain *pci_host_bridge_msi_domain(struct pci_bus *bus)
 	d = pci_host_bridge_of_msi_domain(bus);
 	if (!d)
 		d = pci_host_bridge_acpi_msi_domain(bus);
+
+#ifdef CONFIG_PCI_MSI_IRQ_DOMAIN
+	/*
+	 * If no IRQ domain was found via the OF tree, try looking it up
+	 * directly through the fwnode_handle.
+	 */
+	if (!d) {
+		struct fwnode_handle *fwnode = pci_root_bus_fwnode(bus);
+
+		if (fwnode)
+			d = irq_find_matching_fwnode(fwnode,
+						     DOMAIN_BUS_PCI_MSI);
+	}
+#endif
 
 	return d;
 }
