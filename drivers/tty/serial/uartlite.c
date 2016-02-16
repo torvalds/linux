acf5e6c88914333f8e1b66d9968c65112a97e846
Maarten Brock <m.brock@vanmierlo.com>
2016-02-16 18:59:01 +0100

    serial-uartlite: Enlarge maximum nr of devices to 16.
    This device is ideal to use when you need a lot of uarts in your FPGA.
    Try not to force all those users to patch their kernel.
    
    Signed-off-by: Maarten Brock <m.brock@vanmierlo.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/uartlite.c
+++ drivers/tty/serial/uartlite.c
@@ -28,7 +28,7 @@
 #define ULITE_NAME		"ttyUL"
 #define ULITE_MAJOR		204
 #define ULITE_MINOR		187
-#define ULITE_NR_UARTS		4
+#define ULITE_NR_UARTS		16
 
 /* ---------------------------------------------------------------------
  * Register definitions
