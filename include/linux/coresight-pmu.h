c8ec21bd630e8a56fa811c6542c466b1bdc565ac
Mathieu Poirier <mathieu.poirier@linaro.org>
2016-02-17 17:52:02 -0700

    coresight: introducing a global trace ID function
    TraceID values have to be unique for all tracers and
    consistent between drivers and user space.  As such
    introducing a central function to be used whenever a
    traceID value is required.
    
    The patch also account for data traceIDs, which are usually
    I(N) + 1.
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 17534ceb835a1a96eb921a2a80df168723d6570a)

--- include/linux/coresight-pmu.h
+++ include/linux/coresight-pmu.h
@@ -19,9 +19,21 @@
 #define _LINUX_CORESIGHT_PMU_H
 
 #define CORESIGHT_ETM_PMU_NAME "cs_etm"
+#define CORESIGHT_ETM_PMU_SEED  0x10
 
 /* ETMv3.5/PTM's ETMCR config bit */
 #define ETM_OPT_CYCACC  12
 #define ETM_OPT_TS      28
+
+static inline int coresight_get_trace_id(int cpu)
+{
+	/*
+	 * A trace ID of value 0 is invalid, so let's start at some
+	 * random value that fits in 7 bits and go from there.  Since
+	 * the common convention is to have data trace IDs be I(N) + 1,
+	 * set instruction trace IDs as a function of the CPU number.
+	 */
+	return (CORESIGHT_ETM_PMU_SEED + (cpu * 2));
+}
 
 #endif
