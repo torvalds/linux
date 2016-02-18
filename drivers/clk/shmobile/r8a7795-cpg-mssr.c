f099aa075749c6fadb79554ce175fbdc31c45d7a
Magnus Damm <damm+renesas@opensource.se>
2016-02-18 16:14:03 +0900

    clk: shmobile: r8a7795: Add INTC-EX clock
    Add the "intc-ex" clock to the r8a7795 CPG MSSR driver.
    
    According to information from the hardware team the INTC-EX
    parent clock is CP. The next data sheet version will include
    this information.
    
    Signed-off-by: Magnus Damm <damm+renesas@opensource.se>
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- drivers/clk/shmobile/r8a7795-cpg-mssr.c
+++ drivers/clk/shmobile/r8a7795-cpg-mssr.c
@@ -138,6 +138,7 @@ static const struct mssr_mod_clk r8a7795_mod_clks[] __initconst = {
 	DEF_MOD("usb3-if0",		 328,	R8A7795_CLK_S3D1),
 	DEF_MOD("usb-dmac0",		 330,	R8A7795_CLK_S3D1),
 	DEF_MOD("usb-dmac1",		 331,	R8A7795_CLK_S3D1),
+	DEF_MOD("intc-ex",		 407,	R8A7795_CLK_CP),
 	DEF_MOD("intc-ap",		 408,	R8A7795_CLK_S3D1),
 	DEF_MOD("audmac0",		 502,	R8A7795_CLK_S3D4),
 	DEF_MOD("audmac1",		 501,	R8A7795_CLK_S3D4),
