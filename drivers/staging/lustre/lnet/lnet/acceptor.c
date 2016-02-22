0cf56a1a9926b5e78c6dce246960fda625df744c
Bruno Faccini <bruno.faccini@intel.com>
2016-02-22 17:29:21 -0500

    staging: lustre: avoid race during lnet acceptor thread termination
    This patch will avoid potential race, around socket sleepers
    wait list, during acceptor thread termination and using
    sk_callback_lock RW-Lock protection.
    
    Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-6476
    Reviewed-on: http://review.whamcloud.com/14503
    Reviewed-by: Amir Shehata <amir.shehata@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/lnet/acceptor.c
+++ drivers/staging/lustre/lnet/lnet/acceptor.c
@@ -488,11 +488,17 @@ lnet_acceptor_start(void)
 void
 lnet_acceptor_stop(void)
 {
+	struct sock *sk;
+
 	if (lnet_acceptor_state.pta_shutdown) /* not running */
 		return;
 
 	lnet_acceptor_state.pta_shutdown = 1;
-	wake_up_all(sk_sleep(lnet_acceptor_state.pta_sock->sk));
+
+	sk = lnet_acceptor_state.pta_sock->sk;
+
+	/* awake any sleepers using safe method */
+	sk->sk_state_change(sk);
 
 	/* block until acceptor signals exit */
 	wait_for_completion(&lnet_acceptor_state.pta_signal);
