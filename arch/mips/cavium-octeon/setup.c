1a1590ab1a4a0898fe0a6809e5bcd7c225e3b4d6
Aaro Koskinen <aaro.koskinen@iki.fi>
2016-02-23 00:22:57 +0200

    MIPS: Octeon: Use model string from DTB for unknown board type
    Use model string from DTB for board type if the board is unknown.
    This is more informative, e.g. with EdgeRouter Pro the /proc/cpuinfo
    will display "ubnt,e200 (CN6120p1.1-1000-NSP)" instead of misleading
    "Unsupported Board".
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Cc: David Daney <ddaney.cavm@gmail.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/12582/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/cavium-octeon/setup.c
+++ arch/mips/cavium-octeon/setup.c
@@ -469,8 +469,16 @@ static void __init init_octeon_system_type(void)
 	char const *board_type;
 
 	board_type = cvmx_board_type_to_string(octeon_bootinfo->board_type);
-	if (board_type == NULL)
-		board_type = "Unsupported Board";
+	if (board_type == NULL) {
+		struct device_node *root;
+		int ret;
+
+		root = of_find_node_by_path("/");
+		ret = of_property_read_string(root, "model", &board_type);
+		of_node_put(root);
+		if (ret)
+			board_type = "Unsupported Board";
+	}
 
 	snprintf(octeon_system_type, sizeof(octeon_system_type), "%s (%s)",
 		 board_type, octeon_model_get_string(read_c0_prid()));
