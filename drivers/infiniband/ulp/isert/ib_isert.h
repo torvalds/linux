572a143489a10360ce2aca98c64f8be43c8400a6
Sagi Grimberg <sagig@mellanox.com>
2016-02-24 19:24:09 +0200

    iser-target: Use ib_drain_qp
    Now the rdma core offers a QP draining service in v4.6-rc1,
    use it instead of our own.
    
    Signed-off-by: Sagi Grimberg <sagig@mellanox.com>
    Reported-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>

--- drivers/infiniband/ulp/isert/ib_isert.h
+++ drivers/infiniband/ulp/isert/ib_isert.h
@@ -209,14 +209,12 @@ struct isert_conn {
 	struct ib_qp		*qp;
 	struct isert_device	*device;
 	struct mutex		mutex;
-	struct completion	wait_comp_err;
 	struct kref		kref;
 	struct list_head	fr_pool;
 	int			fr_pool_size;
 	/* lock to protect fastreg pool */
 	spinlock_t		pool_lock;
 	struct work_struct	release_work;
-	struct ib_recv_wr       beacon;
 	bool                    logout_posted;
 	bool                    snd_w_inv;
 };
