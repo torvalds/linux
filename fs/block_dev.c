03cdadb04077b9311bbc67d98cc5401aff76482d
Dan Williams <dan.j.williams@intel.com>
2016-02-26 15:19:43 -0800

    block: disable block device DAX by default
    The recent *sync enabling discovered that we are inserting into the
    block_device pagecache counter to the expectations of the dirty data
    tracking for dax mappings.  This can lead to data corruption.
    
    We want to support DAX for block devices eventually, but it requires
    wider changes to properly manage the pagecache.
    
       dump_stack+0x85/0xc2
       dax_writeback_mapping_range+0x60/0xe0
       blkdev_writepages+0x3f/0x50
       do_writepages+0x21/0x30
       __filemap_fdatawrite_range+0xc6/0x100
       filemap_write_and_wait+0x4a/0xa0
       set_blocksize+0x70/0xd0
       sb_set_blocksize+0x1d/0x50
       ext4_fill_super+0x75b/0x3360
       mount_bdev+0x180/0x1b0
       ext4_mount+0x15/0x20
       mount_fs+0x38/0x170
    
    Mark the support broken so its disabled by default, but otherwise still
    available for testing.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Ross Zwisler <ross.zwisler@linux.intel.com>
    Reported-by: Ross Zwisler <ross.zwisler@linux.intel.com>
    Suggested-by: Dave Chinner <david@fromorbit.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Cc: Jens Axboe <axboe@fb.com>
    Cc: Matthew Wilcox <matthew.r.wilcox@intel.com>
    Cc: Al Viro <viro@ftp.linux.org.uk>
    Cc: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/block_dev.c
+++ fs/block_dev.c
@@ -1201,7 +1201,11 @@ static int __blkdev_get(struct block_device *bdev, fmode_t mode, int for_part)
 		bdev->bd_disk = disk;
 		bdev->bd_queue = disk->queue;
 		bdev->bd_contains = bdev;
-		bdev->bd_inode->i_flags = disk->fops->direct_access ? S_DAX : 0;
+		if (IS_ENABLED(CONFIG_BLK_DEV_DAX) && disk->fops->direct_access)
+			bdev->bd_inode->i_flags = S_DAX;
+		else
+			bdev->bd_inode->i_flags = 0;
+
 		if (!partno) {
 			ret = -ENXIO;
 			bdev->bd_part = disk_get_part(disk, partno);
