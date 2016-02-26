caa88c26575c9be856e5a801abd121d7ff0808f8
Oleg Drokin <green@linuxhacker.ru>
2016-02-26 01:49:58 -0500

    staging/lustre: F_SETLKW64 F_SETLK64 F_GETLK64 are always defined
    Remove the ifdefs for the around usage.
    
    Signed-off-by: Oleg Drokin <green@linuxhacker.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/file.c
+++ drivers/staging/lustre/lustre/llite/file.c
@@ -2746,21 +2746,15 @@ ll_file_flock(struct file *file, int cmd, struct file_lock *file_lock)
 
 	switch (cmd) {
 	case F_SETLKW:
-#ifdef F_SETLKW64
 	case F_SETLKW64:
-#endif
 		flags = 0;
 		break;
 	case F_SETLK:
-#ifdef F_SETLK64
 	case F_SETLK64:
-#endif
 		flags = LDLM_FL_BLOCK_NOWAIT;
 		break;
 	case F_GETLK:
-#ifdef F_GETLK64
 	case F_GETLK64:
-#endif
 		flags = LDLM_FL_TEST_LOCK;
 		/* Save the old mode so that if the mode in the lock changes we
 		 * can decrement the appropriate reader or writer refcount.
