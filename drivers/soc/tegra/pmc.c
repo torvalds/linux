0a2d87e0473fc5eea3f8168f5a24a35e4cf3f29c
Jon Hunter <jonathanh@nvidia.com>
2016-02-26 15:48:40 +0000

    soc/tegra: pmc: Wait for powergate state to change
    Currently, the function tegra_powergate_set() simply sets the desired
    powergate state but does not wait for the state to change. In most cases
    we should wait for the state to change before proceeding. Currently,
    there is a case for Tegra114 and Tegra124 devices where we do not wait
    when starting the secondary CPU as this is not necessary. However, this
    is only done at boot time and so waiting here will only have a small
    impact on boot time. Therefore, update tegra_powergate_set() to wait
    when setting the powergate.
    
    By adding this feature, we can also eliminate the polling loop from
    tegra30_boot_secondary().
    
    A function has been added for checking the status of the powergate and
    so update the tegra_powergate_is_powered() to use this macro as well.
    
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/soc/tegra/pmc.c
+++ drivers/soc/tegra/pmc.c
@@ -28,6 +28,7 @@
 #include <linux/export.h>
 #include <linux/init.h>
 #include <linux/io.h>
+#include <linux/iopoll.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/platform_device.h>
@@ -194,6 +195,9 @@ static inline bool tegra_powergate_is_valid(int id)
  */
 static int tegra_powergate_set(unsigned int id, bool new_state)
 {
+	bool status;
+	int err;
+
 	if (id == TEGRA_POWERGATE_3D && pmc->soc->has_gpu_clamps)
 		return -EINVAL;
 
@@ -206,9 +210,12 @@ static int tegra_powergate_set(unsigned int id, bool new_state)
 
 	tegra_pmc_writel(PWRGATE_TOGGLE_START | id, PWRGATE_TOGGLE);
 
+	err = readx_poll_timeout(tegra_powergate_state, id, status,
+				 status == new_state, 10, 100000);
+
 	mutex_unlock(&pmc->powergates_lock);
 
-	return 0;
+	return err;
 }
 
 /**
