6f4b352f004c696439c5b5d75a4edc3d0823e770
Al Viro <viro@zeniv.linux.org.uk>
2016-02-27 19:31:01 -0500

    should_follow_link(): validate ->d_seq after having decided to follow
    commit a7f775428b8f5808815c0e3004020cedb94cbe3b upstream.
    
    ... otherwise d_is_symlink() above might have nothing to do with
    the inode value we've got.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/namei.c
+++ fs/namei.c
@@ -1711,6 +1711,11 @@ static inline int should_follow_link(struct nameidata *nd, struct path *link,
 		return 0;
 	if (!follow)
 		return 0;
+	/* make sure that d_is_symlink above matches inode */
+	if (nd->flags & LOOKUP_RCU) {
+		if (read_seqcount_retry(&link->dentry->d_seq, seq))
+			return -ECHILD;
+	}
 	return pick_link(nd, link, inode, seq);
 }
 
