89913ea615754163ae9a066c9c1b95aa2a2f51b4
Alex Deucher <alexander.deucher@amd.com>
2016-02-29 16:11:07 -0500

    drm/amdgpu/cz: remove commented out call to enable vce pg
    This code path is not currently enabled now that we properly
    respect the vce pg flags, so uncomment the actual pg calls
    so the code is as it should be we are eventually able to
    enable vce pg.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Rex Zhu <Rex.Zhu@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/cz_dpm.c
+++ drivers/gpu/drm/amd/amdgpu/cz_dpm.c
@@ -2202,8 +2202,7 @@ static void cz_dpm_powergate_vce(struct amdgpu_device *adev, bool gate)
 							    AMD_PG_STATE_GATE);
 
 				cz_enable_vce_dpm(adev, false);
-				/* TODO: to figure out why vce can't be poweroff. */
-				/* cz_send_msg_to_smc(adev, PPSMC_MSG_VCEPowerOFF); */
+				cz_send_msg_to_smc(adev, PPSMC_MSG_VCEPowerOFF);
 				pi->vce_power_gated = true;
 			} else {
 				cz_send_msg_to_smc(adev, PPSMC_MSG_VCEPowerON);
