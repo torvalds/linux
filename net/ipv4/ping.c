a9d562358b5c12a3d1c343f45a3c56df47dad753
Eric Engestrom <eric.engestrom@imgtec.com>
2016-02-29 16:38:06 +0000

    net/ipv4: remove left over dead code
    8cc785f6f429c2a3fb81745dc142cbd72a462c4a ("net: ipv4: make the ping
    /proc code AF-independent") removed the code using it, but renamed this
    variable instead of removing it.
    
    Signed-off-by: Eric Engestrom <eric.engestrom@imgtec.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/ping.c
+++ net/ipv4/ping.c
@@ -1142,13 +1142,6 @@ static int ping_v4_seq_show(struct seq_file *seq, void *v)
 	return 0;
 }
 
-static const struct seq_operations ping_v4_seq_ops = {
-	.show		= ping_v4_seq_show,
-	.start		= ping_v4_seq_start,
-	.next		= ping_seq_next,
-	.stop		= ping_seq_stop,
-};
-
 static int ping_seq_open(struct inode *inode, struct file *file)
 {
 	struct ping_seq_afinfo *afinfo = PDE_DATA(inode);
