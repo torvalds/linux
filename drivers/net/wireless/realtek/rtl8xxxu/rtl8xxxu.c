7ff8c1ae610159fcb6e7188224520cf0f8b7a336
Jes Sorensen <Jes.Sorensen@redhat.com>
2016-02-29 17:04:32 -0500

    rtl8xxxu: Use correct formatting type to print sizeof()
    Usual gcc i386 issue reported by kbuildbot
    
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu.c
+++ drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu.c
@@ -5129,7 +5129,7 @@ static void rtl8xxxu_update_rate_mask(struct rtl8xxxu_priv *priv,
 	if (sgi)
 		h2c.ramask.arg |= 0x20;
 
-	dev_dbg(&priv->udev->dev, "%s: rate mask %08x, arg %02x, size %li\n",
+	dev_dbg(&priv->udev->dev, "%s: rate mask %08x, arg %02x, size %zi\n",
 		__func__, ramask, h2c.ramask.arg, sizeof(h2c.ramask));
 	rtl8723a_h2c_cmd(priv, &h2c, sizeof(h2c.ramask));
 }
