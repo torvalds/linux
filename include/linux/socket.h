a9c7e0955a71e12167d763ef286c80acb5e5fb45
Guenter Roeck <groeck@chromium.org>
2016-03-01 09:44:17 -0800

    net: pppolac/pppopns: Replace msg.msg_iov with iov_iter_kvec()
    Commit 1af89c1ef3b6 ("Hack: net: PPPoPNS and PPPoLAC build fixes for 4.1")
    fixed the build for PPPoPNS and PPPoLAC by re-introducing a field in
    struct msghdr which was removed upstream. Re-introducing the field doesn't
    get it used, so it is quite likely that the code never worked. Fix it up for
    good.
    
    Fixes: 1af89c1ef3b6 ("Hack: net: PPPoPNS and PPPoLAC build fixes for 4.1")
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- include/linux/socket.h
+++ include/linux/socket.h
@@ -47,10 +47,6 @@ struct linger {
 struct msghdr {
 	void		*msg_name;	/* ptr to socket address structure */
 	int		msg_namelen;	/* size of socket address structure */
-#if defined(CONFIG_PPPOLAC) || defined(CONFIG_PPPOPNS)
-	struct iovec	*msg_iov;	/* scatter/gather array */
-	__kernel_size_t	msg_iovlen;	/* # elements in msg_iov */
-#endif
 	struct iov_iter	msg_iter;	/* data */
 	void		*msg_control;	/* ancillary data */
 	__kernel_size_t	msg_controllen;	/* ancillary data buffer length */
