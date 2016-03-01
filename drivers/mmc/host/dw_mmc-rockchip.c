c6a9bf99dd458147241ef93817d18e92c3650530
Shawn Lin <shawn.lin@rock-chips.com>
2016-03-01 15:12:53 +0800

    mmc: dw_mmc-rockchip: fix failing to mount partition with "discard"
    Without MMC_CAP_ERASE support, we fail to mount partition
    with "discard" option since mmc_queue_setup_discard is limited
    for checking mmc_can_erase. Without doing mmc_queue_setup_discard,
    blk_queue_discard fails to test QUEUE_FLAG_DISCARD flag, so we get
    the following log from f2fs(actually similar to other file system):
    
    mounting with "discard" option, but the device does not support discard
    
    Signed-off-by: Shawn Lin <shawn.lin@rock-chips.com>
    Signed-off-by: Jaehoon Chung <jh80.chung@samsung.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/dw_mmc-rockchip.c
+++ drivers/mmc/host/dw_mmc-rockchip.c
@@ -234,11 +234,20 @@ static int dw_mci_rockchip_init(struct dw_mci *host)
 	return 0;
 }
 
+/* Common capabilities of RK3288 SoC */
+static unsigned long dw_mci_rk3288_dwmmc_caps[4] = {
+	MMC_CAP_ERASE,
+	MMC_CAP_ERASE,
+	MMC_CAP_ERASE,
+	MMC_CAP_ERASE,
+};
+
 static const struct dw_mci_drv_data rk2928_drv_data = {
 	.init			= dw_mci_rockchip_init,
 };
 
 static const struct dw_mci_drv_data rk3288_drv_data = {
+	.caps			= dw_mci_rk3288_dwmmc_caps,
 	.set_ios		= dw_mci_rk3288_set_ios,
 	.execute_tuning		= dw_mci_rk3288_execute_tuning,
 	.parse_dt		= dw_mci_rk3288_parse_dt,
