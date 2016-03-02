bcc71beb2a101699426820d5cd3f28dcb9877181
Tom St Denis <tom.stdenis@amd.com>
2016-03-02 09:10:50 -0500

    drm/amd/amdgpu: Whitespace typo fix in sw_init (DCEv11)
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/dce_v11_0.c
+++ drivers/gpu/drm/amd/amdgpu/dce_v11_0.c
@@ -2964,7 +2964,7 @@ static int dce_v11_0_sw_init(void *handle)
 	for (i = 0; i < adev->mode_info.num_crtc; i++) {
 		r = amdgpu_irq_add_id(adev, i + 1, &adev->crtc_irq);
 		if (r)
-		return r;
+			return r;
 	}
 
 	for (i = 8; i < 20; i += 2) {
@@ -2976,7 +2976,7 @@ static int dce_v11_0_sw_init(void *handle)
 	/* HPD hotplug */
 	r = amdgpu_irq_add_id(adev, 42, &adev->hpd_irq);
 	if (r)
-	return r;
+		return r;
 
 	adev->mode_info.mode_config_initialized = true;
 
