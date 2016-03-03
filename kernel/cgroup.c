2378d8b8ba3d2adffb4f98c0c60ee2f448b3be69
Tejun Heo <tj@kernel.org>
2016-03-03 09:57:57 -0500

    cgroup: re-hash init_css_set after subsystems are initialized
    css_sets are hashed by their subsys[] contents and in cgroup_init()
    init_css_set is hashed early, before subsystem inits, when all entries
    in its subsys[] are NULL, so that cgroup_dfl_root initialization can
    find and link to it.  As subsystems are initialized,
    init_css_set.subsys[] is filled up but the hashing is never updated
    making init_css_set hashed in the wrong place.  While incorrect, this
    doesn't cause a critical failure as css_set management code would
    create an identical css_set dynamically.
    
    Fix it by rehashing init_css_set after subsystems are initialized.
    While at it, drop unnecessary @key local variable.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Acked-by: Zefan Li <lizefan@huawei.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -5266,7 +5266,6 @@ static u16 cgroup_disable_mask __initdata;
 int __init cgroup_init(void)
 {
 	struct cgroup_subsys *ss;
-	unsigned long key;
 	int ssid;
 
 	BUILD_BUG_ON(CGROUP_SUBSYS_COUNT > 16);
@@ -5276,9 +5275,12 @@ int __init cgroup_init(void)
 
 	mutex_lock(&cgroup_mutex);
 
-	/* Add init_css_set to the hash table */
-	key = css_set_hash(init_css_set.subsys);
-	hash_add(css_set_table, &init_css_set.hlist, key);
+	/*
+	 * Add init_css_set to the hash table so that dfl_root can link to
+	 * it during init.
+	 */
+	hash_add(css_set_table, &init_css_set.hlist,
+		 css_set_hash(init_css_set.subsys));
 
 	BUG_ON(cgroup_setup_root(&cgrp_dfl_root, 0));
 
@@ -5330,6 +5332,11 @@ int __init cgroup_init(void)
 		if (ss->bind)
 			ss->bind(init_css_set.subsys[ssid]);
 	}
+
+	/* init_css_set.subsys[] has been updated, re-hash */
+	hash_del(&init_css_set.hlist);
+	hash_add(css_set_table, &init_css_set.hlist,
+		 css_set_hash(init_css_set.subsys));
 
 	WARN_ON(sysfs_create_mount_point(fs_kobj, "cgroup"));
 	WARN_ON(register_filesystem(&cgroup_fs_type));
