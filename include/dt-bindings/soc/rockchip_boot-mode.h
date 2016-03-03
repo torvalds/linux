7138bcb02ec313da24598e0414c328877f86c72c
Andy Yan <andy.yan@rock-chips.com>
2016-03-03 20:16:14 +0800

    FROMLIST: dt-bindings: soc: add rockchip reboot mode head file
    add macro #define for rockchip reboot mode
    
    Change-Id: I84036d43864f80623519d0df959e1de7e1a540db
    Signed-off-by: Andy Yan <andy.yan@rock-chips.com>

---
+++ include/dt-bindings/soc/rockchip_boot-mode.h
@@ -0,0 +1,14 @@
+#ifndef __ROCKCHIP_BOOT_MODE_H
+#define __ROCKCHIP_BOOT_MODE_H
+
+/*high 24 bits is tag, low 8 bits is type*/
+#define REBOOT_FLAG		0x5242C300
+/* normal boot */
+#define BOOT_NORMAL		(REBOOT_FLAG + 0)
+/* enter loader rockusb mode */
+#define BOOT_LOADER		(REBOOT_FLAG + 1)
+/* enter recovery */
+#define BOOT_RECOVERY		(REBOOT_FLAG + 3)
+#define BOOT_FASTBOOT		(REBOOT_FLAG + 9)
+
+#endif
