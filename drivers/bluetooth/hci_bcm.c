c5fd9cbb9dc40e0d273d6731030e65096adceeda
Jérôme de Bretagne <jerome.debretagne@gmail.com>
2016-03-03 01:46:28 +0100

    Bluetooth: hci_bcm: Add BCM2E55 ACPI ID used in Lenovo ThinkPad Tablet 8
    Lenovo ThinkPad Tablet 8 with BCM43241 rev B5 chipset uses the BCM2E55
    ACPI ID for Bluetooth. Add it to the list of supported devices.
    
    Signed-off-by: Jérôme de Bretagne <jerome.debretagne@gmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/hci_bcm.c
+++ drivers/bluetooth/hci_bcm.c
@@ -821,6 +821,7 @@ static const struct acpi_device_id bcm_acpi_match[] = {
 	{ "BCM2E3F", 0 },
 	{ "BCM2E40", 0 },
 	{ "BCM2E54", 0 },
+	{ "BCM2E55", 0 },
 	{ "BCM2E64", 0 },
 	{ "BCM2E65", 0 },
 	{ "BCM2E67", 0 },
