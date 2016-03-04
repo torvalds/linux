457a0b7e49c625dd87b3f9d7fd7cc4d9d59430d8
chenzhen <chenzhen@rock-chips.com>
2016-03-04 21:50:45 +0800

    MALI: rockchip: tidy 'platform specific code'.
    We use devfreq to implement DVFS of GPU, instead of 'legacy_dvfs'.
    
    Change-Id: If5c8ef05c8f37c88a5c22779468b21315d71eda0
    Signed-off-by: chenzhen <chenzhen@rock-chips.com>

--- drivers/gpu/arm/midgard/platform/rk/mali_kbase_config_platform.h
+++ drivers/gpu/arm/midgard/platform/rk/mali_kbase_config_platform.h
@@ -6,26 +6,17 @@
  * GNU General Public License version 2 as published by the Free Software
  * Foundation, and any use by you of this program is subject to the terms
  * of such GNU licence.
- *
- * A copy of the licence is included with the program, and can also be obtained
- * from Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301, USA.
- *
  */
 
 /**
  * @file mali_kbase_config_platform.h
  * 声明 platform_config_of_rk (platform_rk 的 platform_config).
- * 
- * 参见 文档 'mali_midgard_ddk_r6p0_integration_manual_DIT0023P_en' 中的 3.4.1.
  */
 
 /**
- * Maximum frequency 
- *	GPU will be clocked at. 
+ * Maximum frequency GPU will be clocked at.
  * Given in kHz.
- * This must be specified 
- *	as there is no default value.
+ * This must be specified as there is no default value.
  *
  * Attached value: number in kHz
  * Default value: NA
@@ -33,10 +24,9 @@
 #define GPU_FREQ_KHZ_MAX (5000)
 
 /**
- * Minimum frequency GPU will be clocked at. 
- * Given in kHz.  
- * This must be specified 
- *	as there is no default value.
+ * Minimum frequency GPU will be clocked at.
+ * Given in kHz.
+ * This must be specified as there is no default value.
  *
  * Attached value: number in kHz
  * Default value: NA
@@ -44,12 +34,11 @@
 #define GPU_FREQ_KHZ_MIN (5000)
 
 /**
- * CPU_SPEED_FUNC 
- *	- A pointer to a function that calculates the CPU clock
+ * CPU_SPEED_FUNC
+ * - A pointer to a function that calculates the CPU clock
  *
- * CPU clock speed of the platform is in MHz 
- * - see kbase_cpu_clk_speed_func 
- *	for the function prototype.
+ * CPU clock speed of the platform is in MHz
+ * - see kbase_cpu_clk_speed_func for the function prototype.
  *
  * Attached value: A kbase_cpu_clk_speed_func.
  * Default Value:  NA
@@ -57,11 +46,10 @@
 #define CPU_SPEED_FUNC (NULL)
 
 /**
- * GPU_SPEED_FUNC 
- * - A pointer to a function 
- *	that calculates the GPU clock
+ * GPU_SPEED_FUNC
+ * - A pointer to a function that calculates the GPU clock
  *
- * GPU clock speed of the platform in MHz 
+ * GPU clock speed of the platform in MHz
  * - see kbase_gpu_clk_speed_func for the function prototype.
  *
  * Attached value: A kbase_gpu_clk_speed_func.
@@ -72,9 +60,9 @@
 /**
  * Power management configuration
  *
- * Attached value: 
+ * Attached value:
  *	pointer to @ref kbase_pm_callback_conf
- * Default value: 
+ * Default value:
  *	See @ref kbase_pm_callback_conf
  */
 #define POWER_MANAGEMENT_CALLBACKS (&pm_callbacks)
@@ -83,20 +71,13 @@ extern struct kbase_pm_callback_conf pm_callbacks;
 /**
  * Platform specific configuration functions
  *
- * Attached value: 
+ * Attached value:
  *	pointer to @ref kbase_platform_funcs_conf
- * Default value: 
+ * Default value:
  *	See @ref kbase_platform_funcs_conf
  */
 #define PLATFORM_FUNCS (&platform_funcs)
 extern struct kbase_platform_funcs_conf platform_funcs;
-
-/** Power model for IPA
- *
- * Attached value: pointer to @ref mali_pa_model_ops
- */
-#define POWER_MODEL_CALLBACKS (NULL)
-
 
 /**
  * Secure mode switch
