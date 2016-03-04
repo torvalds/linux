2066390ad47b374f3d35075a32325b47d15bf735
Geert Uytterhoeven <geert+renesas@glider.be>
2016-03-04 17:03:46 +0100

    clk: renesas: cpg-mssr: Export cpg_mssr_{at,de}tach_dev()
    The R-Car SYSC PM Domain driver has to power manage devices in power
    areas using clocks. To reuse code and to share knowledge of clocks
    suitable for power management, this is ideally done through the existing
    cpg_mssr_attach_dev() and cpg_mssr_detach_dev() callbacks.
    
    Hence these callbacks can no longer rely on their "domain" parameter
    pointing to the CPG/MSSR Clock Domain. To handle this, keep a pointer to
    the clock domain in a static variable. cpg_mssr_attach_dev() has to
    support probe deferral, as the R-Car SYSC PM Domain may be initialized,
    and devices may be added to it, before the CPG/MSSR Clock Domain is
    initialized.
    
    Dummy callbacks are provided for the case where CPG/MSTP support is not
    included, so the rcar-sysc driver won't have to care about this.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- include/linux/clk/renesas.h
+++ include/linux/clk/renesas.h
@@ -33,4 +33,11 @@ void cpg_mstp_detach_dev(struct generic_pm_domain *unused, struct device *dev);
 #define cpg_mstp_detach_dev	NULL
 #endif
 
+#ifdef CONFIG_CLK_RENESAS_CPG_MSSR
+int cpg_mssr_attach_dev(struct generic_pm_domain *unused, struct device *dev);
+void cpg_mssr_detach_dev(struct generic_pm_domain *unused, struct device *dev);
+#else
+#define cpg_mssr_attach_dev	NULL
+#define cpg_mssr_detach_dev	NULL
+#endif
 #endif
