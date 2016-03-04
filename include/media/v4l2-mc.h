1e89f58499f3351a3b3c61dae8213fe3cd24a476
Hans Verkuil <hverkuil@xs4all.nl>
2016-03-04 05:46:58 -0300

    [media] v4l2-mc.h: fix yet more compiler errors
    The newly added functions have an extra semicolon, which
    prevents compilation, and they need to be marked inline:
    
    In file included from ../include/media/tuner.h:23:0,
                     from ../drivers/media/tuners/tuner-simple.c:10:
    ../include/media/v4l2-mc.h:233:1: error: expected identifier or '(' before '{' token
    
    Remove spurious return, remove copy-and-pasted semi-colons, add static
    inline.
    
    Fixes: a77bf7048add ("v4l2-mc.h: Add stubs for the V4L2 PM/pipeline routines")
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- include/media/v4l2-mc.h
+++ include/media/v4l2-mc.h
@@ -221,7 +221,6 @@ static inline int v4l_enable_media_source(struct video_device *vdev)
 
 static inline void v4l_disable_media_source(struct video_device *vdev)
 {
-	return;
 }
 
 static inline int v4l_vb2q_enable_media_source(struct vb2_queue *q)
@@ -229,13 +228,13 @@ static inline int v4l_vb2q_enable_media_source(struct vb2_queue *q)
 	return 0;
 }
 
-int v4l2_pipeline_pm_use(struct media_entity *entity, int use);
+static inline int v4l2_pipeline_pm_use(struct media_entity *entity, int use)
 {
 	return 0;
 }
 
-int v4l2_pipeline_link_notify(struct media_link *link, u32 flags,
-			      unsigned int notification);
+static inline int v4l2_pipeline_link_notify(struct media_link *link, u32 flags,
+					    unsigned int notification)
 {
 	return 0;
 }
