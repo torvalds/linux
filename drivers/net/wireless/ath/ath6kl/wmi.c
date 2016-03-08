c9b260a684d0493238433e08fc2ac7865a89aece
Steve deRosier <derosier@gmail.com>
2016-03-07 16:58:50 -0800

    ath6kl: ignore WMI_TXE_NOTIFY_EVENTID based on fw capability flags
    Certain 6004 firmware releases redefine the WMI_TXE_NOTIFY_EVENTID event
    number and sends the new event frequently. However it doesn't have the
    tx-err-notify feature and thus this firmware capability flag isn't set on
    the firmware package. By guarding the processing of this event by the same
    method we guard the sending of the WMI_SET_TXE_NOTIFY_CMDID command, we
    can ignore the spurious event that we don't know how to process.
    
    Without this change we call cfg80211_cqm_txe_notify() with possibly bad
    data.
    
    Signed-off-by: Steve deRosier <steve.derosier@lairdtech.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/wmi.c
+++ drivers/net/wireless/ath/ath6kl/wmi.c
@@ -1584,6 +1584,11 @@ static int ath6kl_wmi_txe_notify_event_rx(struct wmi *wmi, u8 *datap, int len,
 	if (len < sizeof(*ev))
 		return -EINVAL;
 
+	if (vif->nw_type != INFRA_NETWORK ||
+	    !test_bit(ATH6KL_FW_CAPABILITY_TX_ERR_NOTIFY,
+		      vif->ar->fw_capabilities))
+		return -EOPNOTSUPP;
+
 	if (vif->sme_state != SME_CONNECTED)
 		return -ENOTCONN;
 
