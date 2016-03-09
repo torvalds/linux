9d93efe35ed011327999675d70bb1b12ccdbd62d
Linus Walleij <linus.walleij@linaro.org>
2016-03-09 22:02:52 +0700

    gpio: tps65912: fix bad merge
    I screwed up while merging the immutable branch for TPS65912,
    so fixing it unbroken again.
    
    Cc: Lee Jones <lee.jones@linaro.org>
    Cc: Andrew F. Davis <afd@ti.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-tps65912.c
+++ drivers/gpio/gpio-tps65912.c
@@ -117,8 +117,8 @@ static int tps65912_gpio_probe(struct platform_device *pdev)
 	gpio->gpio_chip = template_chip;
 	gpio->gpio_chip.parent = tps->dev;
 
-	ret = devm_gpiochip_add_data(&pdev->dev, &tps65912_gpio->gpio_chip,
-				     tps65912_gpio);
+	ret = devm_gpiochip_add_data(&pdev->dev, &gpio->gpio_chip,
+				     gpio);
 	if (ret < 0) {
 		dev_err(&pdev->dev, "Could not register gpiochip, %d\n", ret);
 		return ret;
