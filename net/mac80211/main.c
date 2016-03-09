e03521232d2f808b1b593f44565665efb7b242b1
Sara Sharon <sara.sharon@intel.com>
2016-03-09 13:27:08 +0200

    mac80211: add NETIF_F_RXCSUM to features white list
    NETIF_F_RXCSUM is not in the white list, though some
    drivers may want to set it in order to enable seeing the
    actual RX checksum status in ethtool.
    
    Signed-off-by: Sara Sharon <sara.sharon@intel.com>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/main.c
+++ net/mac80211/main.c
@@ -856,7 +856,7 @@ int ieee80211_register_hw(struct ieee80211_hw *hw)
 	/* Only HW csum features are currently compatible with mac80211 */
 	feature_whitelist = NETIF_F_IP_CSUM | NETIF_F_IPV6_CSUM |
 			    NETIF_F_HW_CSUM | NETIF_F_SG | NETIF_F_HIGHDMA |
-			    NETIF_F_GSO_SOFTWARE;
+			    NETIF_F_GSO_SOFTWARE | NETIF_F_RXCSUM;
 	if (WARN_ON(hw->netdev_features & ~feature_whitelist))
 		return -EINVAL;
 
