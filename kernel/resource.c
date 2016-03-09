8095d0f225fe31eaac4a013177b77ed5283278f8
Toshi Kani <toshi.kani@hpe.com>
2016-03-09 12:47:05 -0700

    resource: Export insert_resource and remove_resource
    insert_resource() and remove_resouce() are called by producers
    of resources, such as FW modules and bus drivers.  These modules
    may be implemented as loadable modules.
    
    Export insert_resource() and remove_resouce() so that they can
    be called from such modules.
    
    link: https://lkml.org/lkml/2016/3/8/872
    Signed-off-by: Toshi Kani <toshi.kani@hpe.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- kernel/resource.c
+++ kernel/resource.c
@@ -866,6 +866,7 @@ int insert_resource(struct resource *parent, struct resource *new)
 	conflict = insert_resource_conflict(parent, new);
 	return conflict ? -EBUSY : 0;
 }
+EXPORT_SYMBOL_GPL(insert_resource);
 
 /**
  * insert_resource_expand_to_fit - Insert a resource into the resource tree
@@ -925,6 +926,7 @@ int remove_resource(struct resource *old)
 	write_unlock(&resource_lock);
 	return retval;
 }
+EXPORT_SYMBOL_GPL(remove_resource);
 
 static int __adjust_resource(struct resource *res, resource_size_t start,
 				resource_size_t size)
