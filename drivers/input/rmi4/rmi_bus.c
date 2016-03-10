b43d2c1e9353814ce1716fff03d9223f40fa73b5
Andrew Duggan <aduggan@synaptics.com>
2016-03-10 15:55:29 -0800

    Input: synaptics-rmi4 - add support for F12
    Function 12 implements 2D touch position sensor for newer Synaptics touch
    devices. It replaces F11 and no device will contain both functions.
    
    Signed-off-by: Andrew Duggan <aduggan@synaptics.com>
    Signed-off-by: Christopher Heiny <cheiny@synaptics.com>
    Tested-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Tested-by: Linus Walleij <linus.walleij@linaro.org>
    Tested-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/rmi4/rmi_bus.c
+++ drivers/input/rmi4/rmi_bus.c
@@ -309,6 +309,9 @@ static struct rmi_function_handler *fn_handlers[] = {
 #ifdef CONFIG_RMI4_F11
 	&rmi_f11_handler,
 #endif
+#ifdef CONFIG_RMI4_F12
+	&rmi_f12_handler,
+#endif
 };
 
 static void __rmi_unregister_function_handlers(int start_idx)
