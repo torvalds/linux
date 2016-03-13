1afe23194d0580bc332fe27c4e8717f6562348c5
Yong Wu <yong.wu@mediatek.com>
2016-03-14 06:01:10 +0800

    iommu/io-pgtable: Add MTK 4GB mode in Short-descriptor
    In MT8173, Normally the first 1GB PA is for the HW SRAM and Regs,
    so the PA will be 33bits if the dram size is 4GB. We have a
    "DRAM 4GB mode" toggle bit for this. If it's enabled, from CPU's
    point of view, the dram PA will be from 0x1_00000000~0x1_ffffffff.
    
    In short descriptor, the pagetable descriptor is always 32bit.
    Mediatek extend bit9 in the lvl1 and lvl2 pgtable descriptor
    as the 4GB mode.
    
    In the 4GB mode, the bit9 must be set, then M4U help add 0x1_00000000
    based on the PA in pagetable. Thus the M4U output address to EMI is
    always 33bits(the input address is still 32bits).
    
    We add a special quirk for this MTK-4GB mode. And in the standard
    spec, Bit9 in the lvl1 is "IMPLEMENTATION DEFINED", while it's AP[2]
    in the lvl2, therefore if this quirk is enabled, NO_PERMS is also
    expected.
    
    Signed-off-by: Yong Wu <yong.wu@mediatek.com>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/io-pgtable-arm-v7s.c
+++ drivers/iommu/io-pgtable-arm-v7s.c
@@ -121,6 +121,8 @@
 #define ARM_V7S_TEX_MASK		0x7
 #define ARM_V7S_ATTR_TEX(val)		(((val) & ARM_V7S_TEX_MASK) << ARM_V7S_TEX_SHIFT)
 
+#define ARM_V7S_ATTR_MTK_4GB		BIT(9) /* MTK extend it for 4GB mode */
+
 /* *well, except for TEX on level 2 large pages, of course :( */
 #define ARM_V7S_CONT_PAGE_TEX_SHIFT	6
 #define ARM_V7S_CONT_PAGE_TEX_MASK	(ARM_V7S_TEX_MASK << ARM_V7S_CONT_PAGE_TEX_SHIFT)
@@ -363,6 +365,9 @@ static int arm_v7s_init_pte(struct arm_v7s_io_pgtable *data,
 	pte |= ARM_V7S_PTE_TYPE_PAGE;
 	if (lvl == 1 && (cfg->quirks & IO_PGTABLE_QUIRK_ARM_NS))
 		pte |= ARM_V7S_ATTR_NS_SECTION;
+
+	if (cfg->quirks & IO_PGTABLE_QUIRK_ARM_MTK_4GB)
+		pte |= ARM_V7S_ATTR_MTK_4GB;
 
 	if (num_entries > 1)
 		pte = arm_v7s_pte_to_cont(pte, lvl);
@@ -625,8 +630,14 @@ static struct io_pgtable *arm_v7s_alloc_pgtable(struct io_pgtable_cfg *cfg,
 
 	if (cfg->quirks & ~(IO_PGTABLE_QUIRK_ARM_NS |
 			    IO_PGTABLE_QUIRK_NO_PERMS |
-			    IO_PGTABLE_QUIRK_TLBI_ON_MAP))
+			    IO_PGTABLE_QUIRK_TLBI_ON_MAP |
+			    IO_PGTABLE_QUIRK_ARM_MTK_4GB))
 		return NULL;
+
+	/* If ARM_MTK_4GB is enabled, the NO_PERMS is also expected. */
+	if (cfg->quirks & IO_PGTABLE_QUIRK_ARM_MTK_4GB &&
+	    !(cfg->quirks & IO_PGTABLE_QUIRK_NO_PERMS))
+			return NULL;
 
 	data = kmalloc(sizeof(*data), GFP_KERNEL);
 	if (!data)
