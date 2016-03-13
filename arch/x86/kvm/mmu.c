9753f529156d03112d1ff6f3758ccd6fa6faf189
Lan Tianyu <tianyu.lan@intel.com>
2016-03-13 11:10:24 +0800

    KVM: Remove redundant smp_mb() in the kvm_mmu_commit_zap_page()
    There is already a barrier inside of kvm_flush_remote_tlbs() which can
    help to make sure everyone sees our modifications to the page tables and
    see changes to vcpu->mode here. So remove the smp_mb in the
    kvm_mmu_commit_zap_page() and update the comment.
    
    Signed-off-by: Lan Tianyu <tianyu.lan@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/mmu.c
+++ arch/x86/kvm/mmu.c
@@ -2390,14 +2390,13 @@ static void kvm_mmu_commit_zap_page(struct kvm *kvm,
 		return;
 
 	/*
-	 * wmb: make sure everyone sees our modifications to the page tables
-	 * rmb: make sure we see changes to vcpu->mode
-	 */
-	smp_mb();
-
-	/*
-	 * Wait for all vcpus to exit guest mode and/or lockless shadow
-	 * page table walks.
+	 * We need to make sure everyone sees our modifications to
+	 * the page tables and see changes to vcpu->mode here. The barrier
+	 * in the kvm_flush_remote_tlbs() achieves this. This pairs
+	 * with vcpu_enter_guest and walk_shadow_page_lockless_begin/end.
+	 *
+	 * In addition, kvm_flush_remote_tlbs waits for all vcpus to exit
+	 * guest mode and/or lockless shadow page table walks.
 	 */
 	kvm_flush_remote_tlbs(kvm);
 
