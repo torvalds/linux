5c8877593a44ae914327a7fe850645766635b9b9
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2016-03-14 15:21:06 +0200

    iwlwifi: add default value to disable_11ac mod param description
    Small change to make it clear that the default value is false.
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/iwl-drv.c
+++ drivers/net/wireless/intel/iwlwifi/iwl-drv.c
@@ -1724,4 +1724,4 @@ MODULE_PARM_DESC(d0i3_timeout, "Timeout to D0i3 entry when idle (ms)");
 
 module_param_named(disable_11ac, iwlwifi_mod_params.disable_11ac, bool,
 		   S_IRUGO);
-MODULE_PARM_DESC(disable_11ac, "Disable VHT capabilities");
+MODULE_PARM_DESC(disable_11ac, "Disable VHT capabilities (default: false)");
