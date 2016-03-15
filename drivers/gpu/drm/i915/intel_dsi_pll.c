ae9ec62bdadc4cd3bf893d6baced80aa3a5dbbd6
Ville Syrjälä <ville.syrjala@linux.intel.com>
2016-03-15 16:40:05 +0200

    drm/i915: Fix CHV DSI PLL refclk during state readout
    Use the proper refclock frequency (100MHz) when reading out the
    current DSI clock on CHV.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1458052809-23426-13-git-send-email-ville.syrjala@linux.intel.com
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/intel_dsi_pll.c
+++ drivers/gpu/drm/i915/intel_dsi_pll.c
@@ -258,7 +258,7 @@ static u32 vlv_dsi_get_pclk(struct intel_encoder *encoder, int pipe_bpp)
 	u32 dsi_clock, pclk;
 	u32 pll_ctl, pll_div;
 	u32 m = 0, p = 0, n;
-	int refclk = 25000;
+	int refclk = IS_CHERRYVIEW(dev_priv) ? 100000 : 25000;
 	int i;
 
 	DRM_DEBUG_KMS("\n");
