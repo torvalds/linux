075cca50b6d3ba70ee08cc14535e1c0ba073f871
Martin Brandenburg <martin@omnibond.com>
2016-03-15 11:36:18 -0400

    orangefs: use new orangefs_inode_getattr to create new inodes
    Signed-off-by: Martin Brandenburg <martin@omnibond.com>
    Signed-off-by: Mike Marshall <hubcap@omnibond.com>

--- fs/orangefs/inode.c
+++ fs/orangefs/inode.c
@@ -402,8 +402,7 @@ struct inode *orangefs_iget(struct super_block *sb, struct orangefs_object_kref 
 	if (!inode || !(inode->i_state & I_NEW))
 		return inode;
 
-	error = orangefs_inode_old_getattr(inode,
-	    ORANGEFS_ATTR_SYS_ALL_NOHINT_NOSIZE, 0);
+	error = orangefs_inode_getattr(inode, 1, 0);
 	if (error) {
 		iget_failed(inode);
 		return ERR_PTR(error);
@@ -448,8 +447,7 @@ struct inode *orangefs_new_inode(struct super_block *sb, struct inode *dir,
 	orangefs_set_inode(inode, ref);
 	inode->i_ino = hash;	/* needed for stat etc */
 
-	error = orangefs_inode_old_getattr(inode,
-	    ORANGEFS_ATTR_SYS_ALL_NOHINT_NOSIZE, 0);
+	error = orangefs_inode_getattr(inode, 1, 0);
 	if (error)
 		goto out_iput;
 
