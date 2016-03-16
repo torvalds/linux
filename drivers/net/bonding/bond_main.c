1098cee6e1b11e4fe80b2765eb54d412d4294f03
Zhang Shengju <zhangshengju@cmss.chinamobile.com>
2016-03-16 09:59:15 +0000

    bonding: remove duplicate set of flag IFF_MULTICAST
    Remove unnecessary set of flag IFF_MULTICAST, since ether_setup
    already does this.
    
    Signed-off-by: Zhang Shengju <zhangshengju@cmss.chinamobile.com>
    Reviewed-by: Nikolay Aleksandrov <nikolay@cumulusnetworks.com>
    Signed-off-by: Andy Gospodarek <gospo@cumulusnetworks.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -4175,7 +4175,7 @@ void bond_setup(struct net_device *bond_dev)
 	SET_NETDEV_DEVTYPE(bond_dev, &bond_type);
 
 	/* Initialize the device options */
-	bond_dev->flags |= IFF_MASTER|IFF_MULTICAST;
+	bond_dev->flags |= IFF_MASTER;
 	bond_dev->priv_flags |= IFF_BONDING | IFF_UNICAST_FLT | IFF_NO_QUEUE;
 	bond_dev->priv_flags &= ~(IFF_XMIT_DST_RELEASE | IFF_TX_SKB_SHARING);
 
