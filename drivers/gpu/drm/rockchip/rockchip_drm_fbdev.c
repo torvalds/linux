f572d1c89a72eed9afd4b26874819813c03cd817
Mark Yao <mark.yao@rock-chips.com>
2016-03-18 11:53:46 +0800

    drm/rockchip: fix compile warning
    fix warning:
        warning: format '%x' expects argument of type 'unsigned int',
        but argument 2 has type 'size_t'
    
    Change-Id: Ifab0d16f0229aa3d5fc244678298fa2138bd4aa1
    Signed-off-by: Mark Yao <mark.yao@rock-chips.com>

--- drivers/gpu/drm/rockchip/rockchip_drm_fbdev.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_fbdev.c
@@ -108,7 +108,7 @@ static int rockchip_drm_fbdev_create(struct drm_fb_helper *helper,
 	fbi->screen_size = rk_obj->base.size;
 	fbi->fix.smem_len = rk_obj->base.size;
 
-	DRM_DEBUG_KMS("FB [%dx%d]-%d kvaddr=%p offset=%ld size=%d\n",
+	DRM_DEBUG_KMS("FB [%dx%d]-%d kvaddr=%p offset=%ld size=%zu\n",
 		      fb->width, fb->height, fb->depth, rk_obj->kvaddr,
 		      offset, size);
 
