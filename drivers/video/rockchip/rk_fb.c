a40e1eaedf72f8be11278ce69c724be7f6b6c3de
Huang Jiachai <hjc@rock-chips.com>
2016-03-21 15:19:59 +0800

    video: rockchip: fb: the default state of FBDC is closed
    Change-Id: I6c1a4e47daa00089bfeb7b7316dbe6bac4409a5c
    Signed-off-by: Huang Jiachai <hjc@rock-chips.com>

--- drivers/video/rockchip/rk_fb.c
+++ drivers/video/rockchip/rk_fb.c
@@ -1697,6 +1697,7 @@ static void rk_fb_update_win(struct rk_lcdc_driver *dev_drv,
 				}
 			} else {
 				win->area[i].state = 0;
+				win->area[i].fbdc_en = 0;
 				if (dev_drv->iommu_enabled) {
 					g_now_config_addr[win->id][i] = 0;
 					g_now_config_state[win->id][i] = 0;
@@ -1876,8 +1877,10 @@ static void rk_fb_update_reg(struct rk_lcdc_driver *dev_drv,
 		} else {
 			win->z_order = -1;
 			win->state = 0;
-			for (j = 0; j < 4; j++)
+			for (j = 0; j < 4; j++) {
 				win->area[j].state = 0;
+				win->area[j].fbdc_en = 0;
+			}
 			if (dev_drv->iommu_enabled) {
 				for (j = 0; j < 4; j++) {
 					g_now_config_addr[i][j] = 0;
