2d061d999424efd99e30fa4115c7dced07533223
Kieran Bingham <kieran.bingham@linaro.org>
2016-03-22 14:27:33 -0700

    scripts/gdb: add version command
    lx-version Report the Linux Version of the current kernel.
    
    Add a command to identify the version specified by the banner in the
    debugged kernel.
    
    This lets the user identify the kernel of the running kernel, and will
    let later scripts compare the banner of the attached kernel against the
    banner in the vmlinux symbols files to verify that the files are
    correct.
    
    [jan.kiszka@siemens.com: remove blank line from help output and fix pep8 warning]
    Signed-off-by: Kieran Bingham <kieran.bingham@linaro.org>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Cc: Jason Wessel <jason.wessel@windriver.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

---
+++ scripts/gdb/linux/proc.py
@@ -0,0 +1,28 @@
+#
+# gdb helper commands and functions for Linux kernel debugging
+#
+#  Kernel proc information reader
+#
+# Copyright (c) 2016 Linaro Ltd
+#
+# Authors:
+#  Kieran Bingham <kieran.bingham@linaro.org>
+#
+# This work is licensed under the terms of the GNU GPL version 2.
+#
+
+import gdb
+
+
+class LxVersion(gdb.Command):
+    """ Report the Linux Version of the current kernel.
+        Equivalent to cat /proc/version on a running target"""
+
+    def __init__(self):
+        super(LxVersion, self).__init__("lx-version", gdb.COMMAND_DATA)
+
+    def invoke(self, arg, from_tty):
+        # linux_banner should contain a newline
+        gdb.write(gdb.parse_and_eval("linux_banner").string())
+
+LxVersion()
