ed940cd27416f9887864b95e1f8f8845aa9d6391
Shuah Khan <shuahkh@osg.samsung.com>
2016-03-22 01:04:05 -0300

    [media] au0828: fix au0828_v4l2_close() dev_state race condition
    au0828_v4l2_close() check for dev_state == DEV_DISCONNECTED will fail to
    detect the device disconnected state correctly, if au0828_v4l2_open() runs
    to set the DEV_INITIALIZED bit. A loop test of bind/unbind found this bug
    by increasing the likelihood of au0828_v4l2_open() occurring while unbind
    is in progress. When au0828_v4l2_close() fails to detect that the device
    is in disconnect state, it attempts to power down the device and fails with
    the following general protection fault:
    
    [  260.992962] Call Trace:
    [  260.993008]  [<ffffffffa0f80f0f>] ? xc5000_sleep+0x8f/0xd0 [xc5000]
    [  260.993095]  [<ffffffffa0f6803c>] ? fe_standby+0x3c/0x50 [tuner]
    [  260.993186]  [<ffffffffa0ef541c>] au0828_v4l2_close+0x53c/0x620 [au0828]
    [  260.993298]  [<ffffffffa0d08ec0>] v4l2_release+0xf0/0x210 [videodev]
    [  260.993382]  [<ffffffff81570f9c>] __fput+0x1fc/0x6c0
    [  260.993449]  [<ffffffff815714ce>] ____fput+0xe/0x10
    [  260.993519]  [<ffffffff8116eb83>] task_work_run+0x133/0x1f0
    [  260.993602]  [<ffffffff810035d0>] exit_to_usermode_loop+0x140/0x170
    [  260.993681]  [<ffffffff810061ca>] syscall_return_slowpath+0x16a/0x1a0
    [  260.993754]  [<ffffffff82835fb3>] entry_SYSCALL_64_fastpath+0xa6/0xa8
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Shuah Khan <shuahkh@osg.samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/usb/au0828/au0828-video.c
+++ drivers/media/usb/au0828/au0828-video.c
@@ -1075,7 +1075,7 @@ static int au0828_v4l2_close(struct file *filp)
 		del_timer_sync(&dev->vbi_timeout);
 	}
 
-	if (dev->dev_state == DEV_DISCONNECTED)
+	if (dev->dev_state & DEV_DISCONNECTED)
 		goto end;
 
 	if (dev->users == 1) {
