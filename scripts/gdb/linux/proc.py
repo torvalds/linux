72bf92ec29ac052e950dcb2cc7f8820a2e1f0a02
Kieran Bingham <kieran.bingham@linaro.org>
2016-03-22 14:27:36 -0700

    scripts/gdb: add cmdline reader command
    lx-cmdline Report the Linux Commandline used in the current kernel
    
    [jan.kiszka@siemens.com: remove blank line from help output and fix pep8 warning]
    Signed-off-by: Kieran Bingham <kieran.bingham@linaro.org>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Cc: Jason Wessel <jason.wessel@windriver.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/gdb/linux/proc.py
+++ scripts/gdb/linux/proc.py
@@ -14,6 +14,19 @@
 import gdb
 
 
+class LxCmdLine(gdb.Command):
+    """ Report the Linux Commandline used in the current kernel.
+        Equivalent to cat /proc/cmdline on a running target"""
+
+    def __init__(self):
+        super(LxCmdLine, self).__init__("lx-cmdline", gdb.COMMAND_DATA)
+
+    def invoke(self, arg, from_tty):
+        gdb.write(gdb.parse_and_eval("saved_command_line").string() + "\n")
+
+LxCmdLine()
+
+
 class LxVersion(gdb.Command):
     """ Report the Linux Version of the current kernel.
         Equivalent to cat /proc/version on a running target"""
