42a0b5740ec7d0ad4740a64b6cff35091f23a96a
Guenter Roeck <groeck@chromium.org>
2016-03-23 08:32:23 -0700

    ANDROID: fs: sdcardfs: Declare LOOKUP_CASE_INSENSITIVE unconditionally
    Attempts to build sdcardfs as module fail with
    
    fs/sdcardfs/lookup.c: In function '__sdcardfs_lookup':
    fs/sdcardfs/lookup.c:243:5: error: 'LOOKUP_CASE_INSENSITIVE' undeclared
    
    This occurs because the define is enclosed with #ifdef
    CONFIG_SDCARD_FS_CI_SEARCH. If SDCARD_FS_CI_SEARCH is configured to be
    built as module, this does not work. Alternatives would be to use #if
    IS_ENABLED(CONFIG_SDCARD_FS_CI_SEARCH), or to declare SDCARD_FS_CI_SEARCH
    as bool, but that does not work because the define is used unconditionally
    in the source.
    
    Note that LOOKUP_CASE_INSENSITIVE is only set but not evaluated in the
    current source code, so setting the flag has no real effect.
    
    Fixes: 84a1b7d3d312 ("Included sdcardfs source code for kernel 3.0")
    Cc: Daniel Rosenberg <drosen@google.com>
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- include/linux/namei.h
+++ include/linux/namei.h
@@ -44,9 +44,7 @@ enum {LAST_NORM, LAST_ROOT, LAST_DOT, LAST_DOTDOT, LAST_BIND};
 #define LOOKUP_JUMPED		0x1000
 #define LOOKUP_ROOT		0x2000
 #define LOOKUP_EMPTY		0x4000
-#ifdef CONFIG_SDCARD_FS_CI_SEARCH
 #define LOOKUP_CASE_INSENSITIVE 0x8000
-#endif
 
 extern int path_pts(struct path *path);
 
