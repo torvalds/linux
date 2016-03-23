103626b312b383b337f9367fb764644bd44e6901
Sergi Granell <xerpi.g.12@gmail.com>
2016-03-23 23:39:24 +0100

    [Nintendo 3DS] Add stub SPI driver

---
+++ drivers/spi/spi-nintendo3ds.c
@@ -0,0 +1,11 @@
+/*
+ *  spi-nintendo3ds.c
+ *
+ *  Copyright (C) 2016 Sergi Granell (xerpi)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+/* STUBBED */
