119a0a3c13ef5ffe78dc0c1a55c5da9e377b2976
Helge Deller <deller@gmx.de>
2016-03-23 16:22:42 +0100

    parisc: Wire up preadv2 and pwritev2 syscalls
    Signed-off-by: Helge Deller <deller@gmx.de>

--- arch/parisc/include/uapi/asm/unistd.h
+++ arch/parisc/include/uapi/asm/unistd.h
@@ -362,8 +362,10 @@
 #define __NR_userfaultfd	(__NR_Linux + 344)
 #define __NR_mlock2		(__NR_Linux + 345)
 #define __NR_copy_file_range	(__NR_Linux + 346)
+#define __NR_preadv2		(__NR_Linux + 347)
+#define __NR_pwritev2		(__NR_Linux + 348)
 
-#define __NR_Linux_syscalls	(__NR_copy_file_range + 1)
+#define __NR_Linux_syscalls	(__NR_pwritev2 + 1)
 
 
 #define __IGNORE_select		/* newselect */
