ca64b65b04fe925bb84f4f33eeb0a13260bf9989
Radostan Riedel <raybuntu@googlemail.com>
2016-03-25 08:10:26 +0100

    Changing CEC from PLAYBACK_DEVICE to RECORDING_DEVICE to make more keys work on Samsung TV's

--- drivers/amlogic/hdmi/hdmi_tx/hdmi_tx_cec.c
+++ drivers/amlogic/hdmi/hdmi_tx/hdmi_tx_cec.c
@@ -183,9 +183,9 @@ void cec_node_init(hdmitx_dev_t* hdmitx_device)
     struct vendor_info_data *vend_data = NULL;
     
     int i, bool = 0;
-    const enum _cec_log_dev_addr_e player_dev[3] = {CEC_PLAYBACK_DEVICE_1_ADDR,
-                                                    CEC_PLAYBACK_DEVICE_2_ADDR,
-                                                    CEC_PLAYBACK_DEVICE_3_ADDR,
+    const enum _cec_log_dev_addr_e player_dev[3] = {CEC_RECORDING_DEVICE_1_ADDR,
+                                                    CEC_RECORDING_DEVICE_2_ADDR,
+                                                    CEC_RECORDING_DEVICE_3_ADDR,
                                                    };
 
     unsigned long cec_phy_addr;
