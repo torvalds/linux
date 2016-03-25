a49ac9f83b31e41c8311d64bd2b3f97a23dcb38d
Huang Rui <ray.huang@amd.com>
2016-03-25 11:18:25 +0800

    perf/x86: Move events_sysfs_show() outside CPU_SUP_INTEL
    randconfig builds can sometimes disable CONFIG_CPU_SUP_INTEL while
    enabling the AMD power reporting PMU driver, resulting in this
    build failure:
    
      arch/x86/kernel/cpu/perf_event.h:663:31: error: 'events_sysfs_show' undeclared here (not in a function)
    
    To fix it, move events_sysfs_show() outside of #ifdef CONFIG_CPU_SUP_INTEL.
    
    Reported-by: Randy Dunlap <rdunlap@infradead.org>
    Reported-by: build test robot <lkp@intel.com>
    Signed-off-by: Huang Rui <ray.huang@amd.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: Fengguang Wu <fengguang.wu@intel.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Sherry Hurwitz <sherry.hurwitz@amd.com>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: acme@kernel.org
    Cc: kbuild-all@01.org
    Cc: linux-next@vger.kernel.org
    Cc: spg_linux_kernel@amd.com
    Link: http://lkml.kernel.org/r/1458875905-4278-1-git-send-email-ray.huang@amd.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/events/perf_event.h
+++ arch/x86/events/perf_event.h
@@ -795,6 +795,9 @@ ssize_t intel_event_sysfs_show(char *page, u64 config);
 
 struct attribute **merge_attr(struct attribute **a, struct attribute **b);
 
+ssize_t events_sysfs_show(struct device *dev, struct device_attribute *attr,
+			  char *page);
+
 #ifdef CONFIG_CPU_SUP_AMD
 
 int amd_pmu_init(void);
@@ -924,9 +927,6 @@ int p4_pmu_init(void);
 int p6_pmu_init(void);
 
 int knc_pmu_init(void);
-
-ssize_t events_sysfs_show(struct device *dev, struct device_attribute *attr,
-			  char *page);
 
 static inline int is_ht_workaround_enabled(void)
 {
