aaf4fb712b8311d8b950e89937479d61e9c25ba8
Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
2016-03-25 14:21:53 -0700

    include/linux/oom.h: remove undefined oom_kills_count()/note_oom_kill()
    A leftover from commit c32b3cbe0d06 ("oom, PM: make OOM detection in the
    freezer path raceless").
    
    Signed-off-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/oom.h
+++ include/linux/oom.h
@@ -76,8 +76,6 @@ extern unsigned long oom_badness(struct task_struct *p,
 		struct mem_cgroup *memcg, const nodemask_t *nodemask,
 		unsigned long totalpages);
 
-extern int oom_kills_count(void);
-extern void note_oom_kill(void);
 extern void oom_kill_process(struct oom_control *oc, struct task_struct *p,
 			     unsigned int points, unsigned long totalpages,
 			     struct mem_cgroup *memcg, const char *message);
