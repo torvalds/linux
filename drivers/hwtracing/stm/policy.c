f57af6df6af23c086cfe023896822200eee48dd1
Chunyan Zhang <zhang.chunyan@linaro.org>
2016-03-28 15:55:42 +0800

    stm class: Fix integer boundary checks for master range
    Master IDs are of unsigned int type, yet in the configfs policy code
    we're validating user's input against INT_MAX. This is both pointless
    and misleading as the real limits are imposed by the stm device's
    [sw_start..sw_end] (which are also limited by the spec to be no larger
    than 2^16-1).
    
    Clean this up by getting rid of the redundant comparisons.
    
    Signed-off-by: Chunyan Zhang <zhang.chunyan@linaro.org>
    Signed-off-by: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Reviewed-by: Laurent Fert <laurent.fert@intel.com>

--- drivers/hwtracing/stm/policy.c
+++ drivers/hwtracing/stm/policy.c
@@ -107,8 +107,7 @@ stp_policy_node_masters_store(struct config_item *item, const char *page,
 		goto unlock;
 
 	/* must be within [sw_start..sw_end], which is an inclusive range */
-	if (first > INT_MAX || last > INT_MAX || first > last ||
-	    first < stm->data->sw_start ||
+	if (first > last || first < stm->data->sw_start ||
 	    last > stm->data->sw_end) {
 		ret = -ERANGE;
 		goto unlock;
