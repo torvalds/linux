e9d5c746246c88a891b0ed21980535909cb39af7
Jens Axboe <axboe@fb.com>
2016-03-30 10:17:20 -0600

    mmc/block: switch to using blk_queue_write_cache()
    Signed-off-by: Jens Axboe <axboe@fb.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -2286,7 +2286,7 @@ static struct mmc_blk_data *mmc_blk_alloc_req(struct mmc_card *card,
 	    ((card->ext_csd.rel_param & EXT_CSD_WR_REL_PARAM_EN) ||
 	     card->ext_csd.rel_sectors)) {
 		md->flags |= MMC_BLK_REL_WR;
-		blk_queue_flush(md->queue.queue, REQ_FLUSH | REQ_FUA);
+		blk_queue_write_cache(md->queue.queue, true, true);
 	}
 
 	if (mmc_card_mmc(card) &&
