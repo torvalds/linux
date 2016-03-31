3b9a1d6fea75baaa1f300d85a265c145f024ca30
John Stultz <john.stultz@linaro.org>
2016-03-31 00:35:00 -0700

    HACK: hdmi: Restrict HDMI output modes to known good hikey mode clocks
    Currently the hikey dsi logic cannot generate accurate byte clocks
    values for all pixel clock values. Thus if a mode clock is selected
    that cannot match the calculated byte clock, the device will boot
    with a blank screen.
    
    This patch enforces known good mode clocks for well known resolutions,
    which should allow the display to work from given EDID options, and
    ensures for a given resolution & refresh, the right mode clock is
    selected.
    
    4.1 had similar logic, but keyed off off resolution & refresh, rather
    then resolution & mode clock (and thus 4.1 could select bad clock
    values for a given refresh rate).
    
    Suggested-by: Xinliang Liu <xinliang.liu@linaro.org>
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
+++ drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
@@ -638,8 +638,24 @@ static int adv7511_mode_valid(struct adv7511 *adv7511,
 {
 	if (mode->clock > 165000)
 		return MODE_CLOCK_HIGH;
-
-	return MODE_OK;
+	/*
+	 * some work well modes which want to put in the front of the mode list.
+	 */
+	DRM_DEBUG("Checking mode %ix%i@%i clock: %i...",
+		  mode->hdisplay, mode->vdisplay, drm_mode_vrefresh(mode), mode->clock);
+	if ((mode->hdisplay == 1920 && mode->vdisplay == 1080 && mode->clock == 148500) ||
+	    (mode->hdisplay == 1280 && mode->vdisplay == 800 && mode->clock == 83496) ||
+	    (mode->hdisplay == 1280 && mode->vdisplay == 720 && mode->clock == 74440) ||
+	    (mode->hdisplay == 1280 && mode->vdisplay == 720 && mode->clock == 74250) ||
+	    (mode->hdisplay == 1024 && mode->vdisplay == 768 && mode->clock == 75000) ||
+	    (mode->hdisplay == 1024 && mode->vdisplay == 768 && mode->clock == 81833) ||
+	    (mode->hdisplay == 800 && mode->vdisplay == 600 && mode->clock == 40000)) {
+		mode->type |= DRM_MODE_TYPE_PREFERRED;
+		DRM_DEBUG("OK\n");
+		return MODE_OK;
+	}
+	DRM_DEBUG("BAD\n");
+	return MODE_BAD;
 }
 
 static void adv7511_mode_set(struct adv7511 *adv7511,
