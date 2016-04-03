f51bdc236b6c5835fa5e0df772acc234288b8af2
Kazuya Mizuguchi <kazuya.mizuguchi.ks@renesas.com>
2016-04-03 23:54:38 +0900

    ravb: Add dma queue interrupt support
    This patch supports the following interrupts.
    
    - One interrupt for multiple (timestamp, error, gPTP)
    - One interrupt for emac
    - Four interrupts for dma queue (best effort rx/tx, network control rx/tx)
    
    This patch improve efficiency of the interrupt handler by adding the
    interrupt handler corresponding to each interrupt source described
    above. Additionally, it reduces the number of times of the access to
    EthernetAVB IF.
    Also this patch prevent this driver depends on the whim of a boot loader.
    
    [ykaneko0929@gmail.com: define bit names of registers]
    [ykaneko0929@gmail.com: add comment for gen3 only registers]
    [ykaneko0929@gmail.com: fix coding style]
    [ykaneko0929@gmail.com: update changelog]
    [ykaneko0929@gmail.com: gen3: fix initialization of interrupts]
    [ykaneko0929@gmail.com: gen3: fix clearing interrupts]
    [ykaneko0929@gmail.com: gen3: add helper function for request_irq()]
    [ykaneko0929@gmail.com: gen3: remove IRQF_SHARED flag for request_irq()]
    [ykaneko0929@gmail.com: revert ravb_close() and ravb_ptp_stop()]
    [ykaneko0929@gmail.com: avoid calling free_irq() to non-hooked interrupts]
    [ykaneko0929@gmail.com: make NC/BE interrupt handler a function]
    [ykaneko0929@gmail.com: make timestamp interrupt handler a function]
    [ykaneko0929@gmail.com: timestamp interrupt is handled in multiple
     interrupt handler instead of dma queue interrupt handler]
    Signed-off-by: Kazuya Mizuguchi <kazuya.mizuguchi.ks@renesas.com>
    Signed-off-by: Yoshihiro Kaneko <ykaneko0929@gmail.com>
    Acked-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/renesas/ravb_ptp.c
+++ drivers/net/ethernet/renesas/ravb_ptp.c
@@ -194,7 +194,12 @@ static int ravb_ptp_extts(struct ptp_clock_info *ptp,
 	priv->ptp.extts[req->index] = on;
 
 	spin_lock_irqsave(&priv->lock, flags);
-	ravb_modify(ndev, GIC, GIC_PTCE, on ? GIC_PTCE : 0);
+	if (priv->chip_id == RCAR_GEN2)
+		ravb_modify(ndev, GIC, GIC_PTCE, on ? GIC_PTCE : 0);
+	else if (on)
+		ravb_write(ndev, GIE_PTCS, GIE);
+	else
+		ravb_write(ndev, GID_PTCD, GID);
 	mmiowb();
 	spin_unlock_irqrestore(&priv->lock, flags);
 
@@ -241,7 +246,10 @@ static int ravb_ptp_perout(struct ptp_clock_info *ptp,
 		error = ravb_ptp_update_compare(priv, (u32)start_ns);
 		if (!error) {
 			/* Unmask interrupt */
-			ravb_modify(ndev, GIC, GIC_PTME, GIC_PTME);
+			if (priv->chip_id == RCAR_GEN2)
+				ravb_modify(ndev, GIC, GIC_PTME, GIC_PTME);
+			else
+				ravb_write(ndev, GIE_PTMS0, GIE);
 		}
 	} else	{
 		spin_lock_irqsave(&priv->lock, flags);
@@ -250,7 +258,10 @@ static int ravb_ptp_perout(struct ptp_clock_info *ptp,
 		perout->period = 0;
 
 		/* Mask interrupt */
-		ravb_modify(ndev, GIC, GIC_PTME, 0);
+		if (priv->chip_id == RCAR_GEN2)
+			ravb_modify(ndev, GIC, GIC_PTME, 0);
+		else
+			ravb_write(ndev, GID_PTMD0, GID);
 	}
 	mmiowb();
 	spin_unlock_irqrestore(&priv->lock, flags);
