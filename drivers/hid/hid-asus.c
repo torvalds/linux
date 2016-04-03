b94f7d5ddf1b114e66d9bcc07d0ead080470383b
Yusuke Fujimaki <usk.fujimaki@gmail.com>
2016-04-03 23:15:16 +0900

    HID: asus: add support for VivoBook E200HA
    Asus X205TA and E200HA built-in keyboard contain wrong logical maximum value in
    report descriptor.
    
    This patch correct wrong logical maximum in report descriptor.
    
    Signed-off-by: Yusuke Fujimaki <usk.fujimaki@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-asus.c
+++ drivers/hid/hid-asus.c
@@ -1,6 +1,10 @@
 /*
- *  HID driver for Asus X205TA built-in keyboard.
+ *  HID driver for Asus notebook built-in keyboard.
  *  Fixes small logical maximum to match usage maximum.
+ *
+ *  Currently supported devices are:
+ *    EeeBook X205TA
+ *    VivoBook E200HA
  *
  *  Copyright (c) 2016 Yusuke Fujimaki <usk.fujimaki@gmail.com>
  *
@@ -25,15 +29,15 @@
 static __u8 *asus_report_fixup(struct hid_device *hdev, __u8 *rdesc,
 		unsigned int *rsize)
 {
-	if (*rsize >= 180 && rdesc[54] == 0x25 && rdesc[55] == 0x65) {
-		hid_info(hdev, "Fixing up Asus X205TA report descriptor\n");
+	if (*rsize >= 56 && rdesc[54] == 0x25 && rdesc[55] == 0x65) {
+		hid_info(hdev, "Fixing up Asus notebook report descriptor\n");
 		rdesc[55] = 0xdd;
 	}
 	return rdesc;
 }
 
 static const struct hid_device_id asus_devices[] = {
-	{ HID_I2C_DEVICE(USB_VENDOR_ID_ASUSTEK, USB_DEVICE_ID_ASUSTEK_X205TA_KEYBOARD) },
+	{ HID_I2C_DEVICE(USB_VENDOR_ID_ASUSTEK, USB_DEVICE_ID_ASUSTEK_NOTEBOOK_KEYBOARD) },
 	{ }
 };
 MODULE_DEVICE_TABLE(hid, asus_devices);
