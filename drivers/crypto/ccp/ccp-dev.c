7587c407540006e4e8fd5ed33f66ffe6158e830a
Mike Galbraith <umgwanakikbuti@gmail.com>
2016-04-05 15:03:21 +0200

    crypto: ccp - Fix RT breaking #include <linux/rwlock_types.h>
    Direct include of rwlock_types.h breaks RT, use spinlock_types.h instead.
    
    Fixes: 553d2374db0b crypto: ccp - Support for multiple CCPs
    Signed-off-by: Mike Galbraith <umgwanakikbuti@gmail.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/ccp/ccp-dev.c
+++ drivers/crypto/ccp/ccp-dev.c
@@ -16,7 +16,7 @@
 #include <linux/sched.h>
 #include <linux/interrupt.h>
 #include <linux/spinlock.h>
-#include <linux/rwlock_types.h>
+#include <linux/spinlock_types.h>
 #include <linux/types.h>
 #include <linux/mutex.h>
 #include <linux/delay.h>
