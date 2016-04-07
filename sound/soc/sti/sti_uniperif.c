8d8b1e2eddaef25ca0a18500dd9425638f1cfd02
Moise Gergaud <moise.gergaud@st.com>
2016-04-07 11:25:35 +0200

    ASoC: sti: unip player tdm mode
    here are the changes to enable player tdm mode:
    - When TDM_ENABLE is set to 1, the i2s format should be automatically
    configured. Unfortunately this is not the case (HW bug). Then, we shall
    force DATA_SIZE setting.
    - Compute the transfer size for tdm mode: transfer size = user frame size
    - Manage tdm slots configuration given in DT.
    - Don't use mclk-fs when unip in tdm mode; use tdm slot config to compute
    frame size and to set mclk rate.
    - Refine the hw param (channels & format) according to tdm slot config.
    
    Signed-off-by: Moise Gergaud <moise.gergaud@st.com>
    Acked-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sti/sti_uniperif.c
+++ sound/soc/sti/sti_uniperif.c
@@ -181,10 +181,16 @@ int sti_uniperiph_dai_hw_params(struct snd_pcm_substream *substream,
 				struct snd_pcm_hw_params *params,
 				struct snd_soc_dai *dai)
 {
+	struct sti_uniperiph_data *priv = snd_soc_dai_get_drvdata(dai);
+	struct uniperif *uni = priv->dai_data.uni;
 	struct snd_dmaengine_dai_dma_data *dma_data;
 	int transfer_size;
 
-	transfer_size = params_channels(params) * UNIPERIF_FIFO_FRAMES;
+	if (uni->info->type == SND_ST_UNIPERIF_TYPE_TDM)
+		/* transfer size = user frame size (in 32-bits FIFO cell) */
+		transfer_size = snd_soc_params_to_frame_size(params) / 32;
+	else
+		transfer_size = params_channels(params) * UNIPERIF_FIFO_FRAMES;
 
 	dma_data = snd_soc_dai_get_dma_data(dai, substream);
 	dma_data->maxburst = transfer_size;
--- sound/soc/sti/uniperif.h
+++ sound/soc/sti/uniperif.h
@@ -1389,6 +1389,17 @@ int sti_uniperiph_dai_hw_params(struct snd_pcm_substream *substream,
 				struct snd_pcm_hw_params *params,
 				struct snd_soc_dai *dai);
 
+static inline int sti_uniperiph_get_user_frame_size(
+	struct snd_pcm_runtime *runtime)
+{
+	return (runtime->channels * snd_pcm_format_width(runtime->format) / 8);
+}
+
+static inline int sti_uniperiph_get_unip_tdm_frame_size(struct uniperif *uni)
+{
+	return (uni->tdm_slot.slots * uni->tdm_slot.slot_width / 8);
+}
+
 int sti_uniperiph_set_tdm_slot(struct snd_soc_dai *dai, unsigned int tx_mask,
 			       unsigned int rx_mask, int slots,
 			       int slot_width);
