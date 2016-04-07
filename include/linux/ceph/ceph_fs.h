0cabbd94ff52c4803fc0ad9ad0ad5e43df493ab0
Yan, Zheng <zyan@redhat.com>
2016-04-07 11:34:43 +0800

    libceph: fsmap.user subscription support
    Signed-off-by: Yan, Zheng <zyan@redhat.com>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- include/linux/ceph/ceph_fs.h
+++ include/linux/ceph/ceph_fs.h
@@ -121,6 +121,7 @@ struct ceph_dir_layout {
 
 /* client <-> mds */
 #define CEPH_MSG_MDS_MAP                21
+#define CEPH_MSG_FS_MAP_USER            103
 
 #define CEPH_MSG_CLIENT_SESSION         22
 #define CEPH_MSG_CLIENT_RECONNECT       23
