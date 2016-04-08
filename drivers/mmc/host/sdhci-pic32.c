dc4c90fa6510ab8d5eb5e06fd7546c9cb00932c5
Masahiro Yamada <yamada.masahiro@socionext.com>
2016-04-08 14:22:37 +0900

    mmc: sdhci-pic32: remove owner assignment
    A platform_driver does not need to set an owner, it will be populated
    by the driver core.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/sdhci-pic32.c
+++ drivers/mmc/host/sdhci-pic32.c
@@ -243,7 +243,6 @@ MODULE_DEVICE_TABLE(of, pic32_sdhci_id_table);
 static struct platform_driver pic32_sdhci_driver = {
 	.driver = {
 		.name	= "pic32-sdhci",
-		.owner	= THIS_MODULE,
 		.of_match_table = of_match_ptr(pic32_sdhci_id_table),
 	},
 	.probe		= pic32_sdhci_probe,
