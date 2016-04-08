c1c7040e072caac6445a1e298bd1fc5e8da0a5f5
Rafał Miłecki <zajec5@gmail.com>
2016-04-08 12:23:46 +0200

    mtd: nand: mxc: set ECC algorithm explicitly
    This is part of process deprecating NAND_ECC_SOFT_BCH (and switching to
    enum nand_ecc_algo).
    
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- drivers/mtd/nand/mxc_nand.c
+++ drivers/mtd/nand/mxc_nand.c
@@ -1585,6 +1585,7 @@ static int mxcnd_probe(struct platform_device *pdev)
 		this->ecc.mode = NAND_ECC_HW;
 	} else {
 		this->ecc.mode = NAND_ECC_SOFT;
+		this->ecc.algo = NAND_ECC_HAMMING;
 	}
 
 	/* NAND bus width determines access functions used by upper layer */
