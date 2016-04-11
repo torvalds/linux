5901f4c279f7ddbd32041ce1166387ffa05b902d
Pankaj Dubey <pankaj.dubey@samsung.com>
2016-04-11 13:12:26 +0530

    ARM: EXYNOS: Remove SROM related register settings from mach-exynos
    As now we have dedicated driver for SROM controller, it will take care
    of saving register banks during S2R so we can safely remove these
    settings from mach-exynos.
    
    Signed-off-by: Pankaj Dubey <pankaj.dubey@samsung.com>
    Reviewed-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Kukjin Kim <kgene@kernel.org>
    [k.kozlowski: Need to select also SAMSUNG_MC]
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>

--- arch/arm/mach-exynos/suspend.c
+++ arch/arm/mach-exynos/suspend.c
@@ -34,10 +34,11 @@
 #include <asm/smp_scu.h>
 #include <asm/suspend.h>
 
+#include <mach/map.h>
+
 #include <plat/pm-common.h>
 
 #include "common.h"
-#include "regs-srom.h"
 
 #define REG_TABLE_END (-1U)
 
@@ -51,15 +52,6 @@
 struct exynos_wkup_irq {
 	unsigned int hwirq;
 	u32 mask;
-};
-
-static struct sleep_save exynos_core_save[] = {
-	/* SROM side */
-	SAVE_ITEM(S5P_SROM_BW),
-	SAVE_ITEM(S5P_SROM_BC0),
-	SAVE_ITEM(S5P_SROM_BC1),
-	SAVE_ITEM(S5P_SROM_BC2),
-	SAVE_ITEM(S5P_SROM_BC3),
 };
 
 struct exynos_pm_data {
@@ -343,8 +335,6 @@ static void exynos_pm_prepare(void)
 	/* Set wake-up mask registers */
 	exynos_pm_set_wakeup_mask();
 
-	s3c_pm_do_save(exynos_core_save, ARRAY_SIZE(exynos_core_save));
-
 	exynos_pm_enter_sleep_mode();
 
 	/* ensure at least INFORM0 has the resume address */
@@ -374,8 +364,6 @@ static void exynos5420_pm_prepare(void)
 
 	/* Set wake-up mask registers */
 	exynos_pm_set_wakeup_mask();
-
-	s3c_pm_do_save(exynos_core_save, ARRAY_SIZE(exynos_core_save));
 
 	exynos_pmu_spare3 = pmu_raw_readl(S5P_PMU_SPARE3);
 	/*
@@ -467,8 +455,6 @@ static void exynos_pm_resume(void)
 	/* For release retention */
 	exynos_pm_release_retention();
 
-	s3c_pm_do_restore_core(exynos_core_save, ARRAY_SIZE(exynos_core_save));
-
 	if (cpuid == ARM_CPU_PART_CORTEX_A9)
 		scu_enable(S5P_VA_SCU);
 
@@ -534,8 +520,6 @@ static void exynos5420_pm_resume(void)
 	exynos_pm_release_retention();
 
 	pmu_raw_writel(exynos_pmu_spare3, S5P_PMU_SPARE3);
-
-	s3c_pm_do_restore_core(exynos_core_save, ARRAY_SIZE(exynos_core_save));
 
 early_wakeup:
 
