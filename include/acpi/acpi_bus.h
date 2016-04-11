c7e16e5257ec46530e3e874af38191746c137c83
Jerry Hoemann <jerry.hoemann@hpe.com>
2016-04-11 15:02:26 -0700

    acpi: widen acpi_evaluate_dsm() revision and function-index arguments
    The ACPI specification states that arguments "Revision ID" and "Function
    Index" to a _DSM are type "Integer."  Type Integers are 64 bit
    quantities.
    
    The function evaluate_dsm specifies these types as simple "int" which
    are 32 bits.  Widen type passed to acpi_evaluate_dsm and its callers and
    derived callers to pass correct type.
    
    acpi_check_dsm and acpi_evaluate_dsm_typed had similar issue and were
    corrected as well.
    
    This is in preparation for libnvdimm implementing a generic _DSM
    passthrough facility to have the capacity to pass 64-bit values as the
    ACPI specification allows.
    
    [djbw: clarify the changelog, add rationale]
    Signed-off-by: Jerry Hoemann <jerry.hoemann@hpe.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- include/acpi/acpi_bus.h
+++ include/acpi/acpi_bus.h
@@ -61,12 +61,12 @@ bool acpi_ata_match(acpi_handle handle);
 bool acpi_bay_match(acpi_handle handle);
 bool acpi_dock_match(acpi_handle handle);
 
-bool acpi_check_dsm(acpi_handle handle, const u8 *uuid, int rev, u64 funcs);
+bool acpi_check_dsm(acpi_handle handle, const u8 *uuid, u64 rev, u64 funcs);
 union acpi_object *acpi_evaluate_dsm(acpi_handle handle, const u8 *uuid,
-			int rev, int func, union acpi_object *argv4);
+			u64 rev, u64 func, union acpi_object *argv4);
 
 static inline union acpi_object *
-acpi_evaluate_dsm_typed(acpi_handle handle, const u8 *uuid, int rev, int func,
+acpi_evaluate_dsm_typed(acpi_handle handle, const u8 *uuid, u64 rev, u64 func,
 			union acpi_object *argv4, acpi_object_type type)
 {
 	union acpi_object *obj;
