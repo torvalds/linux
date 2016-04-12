98781fa62bfc3b680d4be8733223f278ea315151
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2016-04-12 19:40:46 -0300

    BACKPORT: [media] v4l: Add metadata buffer type and format
    The metadata buffer type is used to transfer metadata between userspace
    and kernelspace through a V4L2 buffers queue. It comes with a new
    metadata capture capability and format description.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Tested-by: Guennadi Liakhovetski <guennadi.liakhovetski@intel.com>
    Acked-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Acked-by: Hans Verkuil <hans.verkuil@cisco.com>
    [hans.verkuil@cisco.com: removed left-over 'experimental' note]
    [hans.verkuil@cisco.com: add newline after _v4l2-meta-format label]
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>
    (cherry picked from commit fb9ffa6a7f7ef39cc0f14f417b66411be5492512)
    
     Conflicts:
    	Documentation/media/uapi/v4l/buffer.rst
    	Documentation/media/uapi/v4l/devices.rst
    	Documentation/media/uapi/v4l/vidioc-querycap.rst
    	Documentation/media/videodev2.h.rst.exceptions
    	drivers/media/v4l2-core/v4l2-dev.c
    	drivers/media/v4l2-core/videobuf2-v4l2.c
    	include/media/v4l2-ioctl.h
    
    Deleted the documentation, as 4.4 is before the conversion to rst.
    
    BUG=b:66317170
    TEST=compile
    
    Change-Id: Id43757c0a0b1f34e10d17e5345f89ded25503d13
    Signed-off-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/676767
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>

--- include/media/v4l2-ioctl.h
+++ include/media/v4l2-ioctl.h
@@ -38,6 +38,8 @@ struct v4l2_ioctl_ops {
 					    struct v4l2_fmtdesc *f);
 	int (*vidioc_enum_fmt_sdr_out)     (struct file *file, void *fh,
 					    struct v4l2_fmtdesc *f);
+	int (*vidioc_enum_fmt_meta_cap)(struct file *file, void *fh,
+					struct v4l2_fmtdesc *f);
 
 	/* VIDIOC_G_FMT handlers */
 	int (*vidioc_g_fmt_vid_cap)    (struct file *file, void *fh,
@@ -64,6 +66,8 @@ struct v4l2_ioctl_ops {
 					struct v4l2_format *f);
 	int (*vidioc_g_fmt_sdr_out)    (struct file *file, void *fh,
 					struct v4l2_format *f);
+	int (*vidioc_g_fmt_meta_cap)(struct file *file, void *fh,
+				     struct v4l2_format *f);
 
 	/* VIDIOC_S_FMT handlers */
 	int (*vidioc_s_fmt_vid_cap)    (struct file *file, void *fh,
@@ -90,6 +94,8 @@ struct v4l2_ioctl_ops {
 					struct v4l2_format *f);
 	int (*vidioc_s_fmt_sdr_out)    (struct file *file, void *fh,
 					struct v4l2_format *f);
+	int (*vidioc_s_fmt_meta_cap)(struct file *file, void *fh,
+				     struct v4l2_format *f);
 
 	/* VIDIOC_TRY_FMT handlers */
 	int (*vidioc_try_fmt_vid_cap)    (struct file *file, void *fh,
@@ -116,6 +122,8 @@ struct v4l2_ioctl_ops {
 					  struct v4l2_format *f);
 	int (*vidioc_try_fmt_sdr_out)    (struct file *file, void *fh,
 					  struct v4l2_format *f);
+	int (*vidioc_try_fmt_meta_cap)(struct file *file, void *fh,
+				       struct v4l2_format *f);
 
 	/* Buffer handlers */
 	int (*vidioc_reqbufs) (struct file *file, void *fh, struct v4l2_requestbuffers *b);
