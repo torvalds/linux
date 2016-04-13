018cfbde63a4b1cb7ca4cb0c0b1d4286f7708236
Jyri Sarha <jsarha@ti.com>
2016-04-13 19:00:51 +0300

    drm/tilcdc: tfp410: Add atomic modeset helpers to connector funcs
    Add atomic modeset helpers to tfp410 connector funcs. Property handling
    related helpers, atomic reset helper, and new dpms helper is needed in
    connector for atomic modeseting to work. The default helper functions
    are enough.
    
    Signed-off-by: Jyri Sarha <jsarha@ti.com>

--- drivers/gpu/drm/tilcdc/tilcdc_tfp410.c
+++ drivers/gpu/drm/tilcdc/tilcdc_tfp410.c
@@ -20,6 +20,7 @@
 #include <linux/of_gpio.h>
 #include <linux/pinctrl/pinmux.h>
 #include <linux/pinctrl/consumer.h>
+#include <drm/drm_atomic_helper.h>
 
 #include "tilcdc_drv.h"
 
@@ -200,9 +201,12 @@ static struct drm_encoder *tfp410_connector_best_encoder(
 
 static const struct drm_connector_funcs tfp410_connector_funcs = {
 	.destroy            = tfp410_connector_destroy,
-	.dpms               = drm_helper_connector_dpms,
+	.dpms               = drm_atomic_helper_connector_dpms,
 	.detect             = tfp410_connector_detect,
 	.fill_modes         = drm_helper_probe_single_connector_modes,
+	.reset              = drm_atomic_helper_connector_reset,
+	.atomic_duplicate_state = drm_atomic_helper_connector_duplicate_state,
+	.atomic_destroy_state = drm_atomic_helper_connector_destroy_state,
 };
 
 static const struct drm_connector_helper_funcs tfp410_connector_helper_funcs = {
