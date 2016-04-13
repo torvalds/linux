ccc5429f7bf9c78d64f1bb03c578f1641ca72fe4
Mauro Carvalho Chehab <mchehab@osg.samsung.com>
2016-04-13 17:02:20 -0300

    [media] cx231xx: return proper error codes at cx231xx-417.c
    Instead of returning -1, return valid error codes.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/usb/cx231xx/cx231xx-417.c
+++ drivers/media/usb/cx231xx/cx231xx-417.c
@@ -360,7 +360,7 @@ static int wait_for_mci_complete(struct cx231xx *dev)
 
 		if (count++ > 100) {
 			dprintk(3, "ERROR: Timeout - gpio=%x\n", gpio);
-			return -1;
+			return -EIO;
 		}
 	}
 	return 0;
@@ -856,7 +856,7 @@ static int cx231xx_find_mailbox(struct cx231xx *dev)
 		}
 	}
 	dprintk(3, "Mailbox signature values not found!\n");
-	return -1;
+	return -EIO;
 }
 
 static void mci_write_memory_to_gpio(struct cx231xx *dev, u32 address, u32 value,
@@ -960,14 +960,14 @@ static int cx231xx_load_firmware(struct cx231xx *dev)
 	p_fw = p_current_fw;
 	if (p_current_fw == NULL) {
 		dprintk(2, "FAIL!!!\n");
-		return -1;
+		return -ENOMEM;
 	}
 
 	p_buffer = vmalloc(4096);
 	if (p_buffer == NULL) {
 		dprintk(2, "FAIL!!!\n");
 		vfree(p_current_fw);
-		return -1;
+		return -ENOMEM;
 	}
 
 	dprintk(2, "%s()\n", __func__);
@@ -992,7 +992,7 @@ static int cx231xx_load_firmware(struct cx231xx *dev)
 			"%s: Error with mc417_register_write\n", __func__);
 		vfree(p_current_fw);
 		vfree(p_buffer);
-		return -1;
+		return retval;
 	}
 
 	retval = request_firmware(&firmware, CX231xx_FIRM_IMAGE_NAME,
@@ -1006,7 +1006,7 @@ static int cx231xx_load_firmware(struct cx231xx *dev)
 			"Please fix your hotplug setup, the board will not work without firmware loaded!\n");
 		vfree(p_current_fw);
 		vfree(p_buffer);
-		return -1;
+		return retval;
 	}
 
 	if (firmware->size != CX231xx_FIRM_IMAGE_SIZE) {
@@ -1016,7 +1016,7 @@ static int cx231xx_load_firmware(struct cx231xx *dev)
 		release_firmware(firmware);
 		vfree(p_current_fw);
 		vfree(p_buffer);
-		return -1;
+		return -EINVAL;
 	}
 
 	if (0 != memcmp(firmware->data, magic, 8)) {
@@ -1025,7 +1025,7 @@ static int cx231xx_load_firmware(struct cx231xx *dev)
 		release_firmware(firmware);
 		vfree(p_current_fw);
 		vfree(p_buffer);
-		return -1;
+		return -EINVAL;
 	}
 
 	initGPIO(dev);
@@ -1140,21 +1140,21 @@ static int cx231xx_initialize_codec(struct cx231xx *dev)
 		if (retval < 0) {
 			dev_err(dev->dev, "%s: mailbox < 0, error\n",
 				__func__);
-			return -1;
+			return retval;
 		}
 		dev->cx23417_mailbox = retval;
 		retval = cx231xx_api_cmd(dev, CX2341X_ENC_PING_FW, 0, 0);
 		if (retval < 0) {
 			dev_err(dev->dev,
 				"ERROR: cx23417 firmware ping failed!\n");
-			return -1;
+			return retval;
 		}
 		retval = cx231xx_api_cmd(dev, CX2341X_ENC_GET_VERSION, 0, 1,
 			&version);
 		if (retval < 0) {
 			dev_err(dev->dev,
 				"ERROR: cx23417 firmware get encoder: version failed!\n");
-			return -1;
+			return retval;
 		}
 		dprintk(1, "cx23417 firmware version is 0x%08x\n", version);
 		msleep(200);
