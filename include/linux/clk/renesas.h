12524e348bcb2189a8cf43829e90256f7e7d4f3d
Geert Uytterhoeven <geert+renesas@glider.be>
2016-04-13 11:18:06 +0200

    clk: renesas: mstp: Provide dummy attach/detach_dev callbacks
    Provide dummy cpg_mstp_{at,de}tach_dev() PM Domain callbacks if CPG/MSTP
    support is not included, so the rcar-sysc driver won't have to care
    about this.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- include/linux/clk/renesas.h
+++ include/linux/clk/renesas.h
@@ -25,7 +25,12 @@ void r8a7779_clocks_init(u32 mode);
 void rcar_gen2_clocks_init(u32 mode);
 
 void cpg_mstp_add_clk_domain(struct device_node *np);
+#ifdef CONFIG_CLK_RENESAS_CPG_MSTP
 int cpg_mstp_attach_dev(struct generic_pm_domain *unused, struct device *dev);
 void cpg_mstp_detach_dev(struct generic_pm_domain *unused, struct device *dev);
+#else
+#define cpg_mstp_attach_dev	NULL
+#define cpg_mstp_detach_dev	NULL
+#endif
 
 #endif
