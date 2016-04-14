5f575efea79caae69b81f922d99b221302a2c003
Michal Hocko <mhocko@suse.com>
2016-04-14 10:51:42 +0200

    crypto: lzo - get rid of superfluous __GFP_REPEAT
    __GFP_REPEAT has a rather weak semantic but since it has been introduced
    around 2.6.12 it has been ignored for low order allocations.
    
    lzo_init uses __GFP_REPEAT to allocate LZO1X_MEM_COMPRESS 16K. This is
    order 3 allocation request and __GFP_REPEAT is ignored for this size
    as well as all <= PAGE_ALLOC_COSTLY requests.
    
    Cc: Herbert Xu <herbert@gondor.apana.org.au>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: linux-crypto@vger.kernel.org
    Signed-off-by: Michal Hocko <mhocko@suse.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/lzo.c
+++ crypto/lzo.c
@@ -32,7 +32,7 @@ static int lzo_init(struct crypto_tfm *tfm)
 	struct lzo_ctx *ctx = crypto_tfm_ctx(tfm);
 
 	ctx->lzo_comp_mem = kmalloc(LZO1X_MEM_COMPRESS,
-				    GFP_KERNEL | __GFP_NOWARN | __GFP_REPEAT);
+				    GFP_KERNEL | __GFP_NOWARN);
 	if (!ctx->lzo_comp_mem)
 		ctx->lzo_comp_mem = vmalloc(LZO1X_MEM_COMPRESS);
 	if (!ctx->lzo_comp_mem)
