996ecb8246676100af2a4dc1cfed747712a3c85f
Vivien Didelot <vivien.didelot@savoirfairelinux.com>
2016-04-14 14:42:08 -0400

    net: dsa: mv88e6xxx: enable SA learning on DSA ports
    In multi-chip systems, DSA Tag ports must learn SA addresses in order to
    correctly switch frames between interconnected chips.
    
    This fixes cross-chip hardware bridging in a VLAN filtering aware
    system, because a bridge group gets implemented as an hardware 802.1Q
    VLAN and thus DSA and user ports share the same FDB.
    
    Fixes: 4c7ea3c0791e ("net: dsa: mv88e6xxx: disable SA learning for DSA and CPU ports")
    Signed-off-by: Vivien Didelot <vivien.didelot@savoirfairelinux.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/mv88e6xxx.c
+++ drivers/net/dsa/mv88e6xxx.c
@@ -2476,8 +2476,8 @@ static int mv88e6xxx_setup_port(struct dsa_switch *ds, int port)
 	 * the other bits clear.
 	 */
 	reg = 1 << port;
-	/* Disable learning for DSA and CPU ports */
-	if (dsa_is_cpu_port(ds, port) || dsa_is_dsa_port(ds, port))
+	/* Disable learning for CPU port */
+	if (dsa_is_cpu_port(ds, port))
 		reg = 0;
 
 	ret = _mv88e6xxx_reg_write(ds, REG_PORT(port), PORT_ASSOC_VECTOR, reg);
