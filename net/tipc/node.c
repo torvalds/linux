de7e07f9ee14f47d05aa43046404c2904f0247dc
Jon Paul Maloy <jon.maloy@ericsson.com>
2016-04-15 13:33:06 -0400

    tipc: ensure that first packets on link are sent in order
    In some link establishment scenarios we see that packet #2 may be sent
    out before packet #1, forcing the receiver to demand retransmission of
    the missing packet. This is harmless, but may cause confusion among
    people tracing the packet flow.
    
    Since this is extremely easy to fix, we do so by adding en extra send
    call to the bearer immediately after the link has come up.
    
    Acked-by: Ying Xue <ying.xue@windriver.com>
    Signed-off-by: Jon Maloy <jon.maloy@ericsson.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/tipc/node.c
+++ net/tipc/node.c
@@ -581,8 +581,12 @@ static void __tipc_node_link_up(struct tipc_node *n, int bearer_id,
 static void tipc_node_link_up(struct tipc_node *n, int bearer_id,
 			      struct sk_buff_head *xmitq)
 {
+	struct tipc_media_addr *maddr;
+
 	tipc_node_write_lock(n);
 	__tipc_node_link_up(n, bearer_id, xmitq);
+	maddr = &n->links[bearer_id].maddr;
+	tipc_bearer_xmit(n->net, bearer_id, xmitq, maddr);
 	tipc_node_write_unlock(n);
 }
 
