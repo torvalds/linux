f808f63372cc47119f81d94ec92f9d1b4e21c562
Lyude <cpaul@redhat.com>
2016-04-15 10:25:35 -0400

    drm/dp_helper: Perform throw-away read before actual read in drm_dp_dpcd_read()
    This is part of a patch series to migrate all of the workarounds for
    commonly seen behavior from bad sinks in intel_dp_dpcd_read_wake() to drm's
    DP helper.
    
    Some sinks will just return garbage for the first aux tranaction they
    receive when coming out of sleep mode, so we need to perform an additional
    read before the actual read to workaround this.
    
    			    Changes since v5
    - If the throwaway read in drm_dp_dpcd_read() fails, return the error
      from that instead of continuing. This follows the same logic we do in
      drm_dp_dpcd_access() (e.g. the error from the first transaction may
      differ from the errors that proceeding attempts might return).
    
    Signed-off-by: Lyude <cpaul@redhat.com>
    Tested-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1460730335-5012-1-git-send-email-cpaul@redhat.com

--- drivers/gpu/drm/drm_dp_helper.c
+++ drivers/gpu/drm/drm_dp_helper.c
@@ -248,6 +248,25 @@ unlock:
 ssize_t drm_dp_dpcd_read(struct drm_dp_aux *aux, unsigned int offset,
 			 void *buffer, size_t size)
 {
+	int ret;
+
+	/*
+	 * HP ZR24w corrupts the first DPCD access after entering power save
+	 * mode. Eg. on a read, the entire buffer will be filled with the same
+	 * byte. Do a throw away read to avoid corrupting anything we care
+	 * about. Afterwards things will work correctly until the monitor
+	 * gets woken up and subsequently re-enters power save mode.
+	 *
+	 * The user pressing any button on the monitor is enough to wake it
+	 * up, so there is no particularly good place to do the workaround.
+	 * We just have to do it before any DPCD access and hope that the
+	 * monitor doesn't power down exactly after the throw away read.
+	 */
+	ret = drm_dp_dpcd_access(aux, DP_AUX_NATIVE_READ, DP_DPCD_REV, buffer,
+				 1);
+	if (ret != 1)
+		return ret;
+
 	return drm_dp_dpcd_access(aux, DP_AUX_NATIVE_READ, offset, buffer,
 				  size);
 }
