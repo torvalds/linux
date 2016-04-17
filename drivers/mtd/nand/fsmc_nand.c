ef296dc947f6a9300a7fb5b696d1e1f543479e18
Rafał Miłecki <zajec5@gmail.com>
2016-04-17 22:53:04 +0200

    mtd: nand: fsmc: validate ECC setup by checking algorithm directly
    NAND core sets ECC algorithm in algo field now and it should be
    preferred over the mode field. This also prepares driver for dropping
    NAND_ECC_SOFT_BCH.
    
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- drivers/mtd/nand/fsmc_nand.c
+++ drivers/mtd/nand/fsmc_nand.c
@@ -958,9 +958,12 @@ static int __init fsmc_nand_probe(struct platform_device *pdev)
 			nand->ecc.strength = 1;
 			break;
 
+		case NAND_ECC_SOFT:
 		case NAND_ECC_SOFT_BCH:
-			dev_info(&pdev->dev, "Using 4-bit SW BCH ECC scheme\n");
-			break;
+			if (nand->ecc.algo == NAND_ECC_BCH) {
+				dev_info(&pdev->dev, "Using 4-bit SW BCH ECC scheme\n");
+				break;
+			}
 
 		default:
 			dev_err(&pdev->dev, "Unsupported ECC mode!\n");
