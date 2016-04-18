82611c14c4e479715c071fb0f44ddd72dc632037
Jan Glauber <jan.glauber@caviumnetworks.com>
2016-04-18 09:43:33 +0200

    arm64: Reduce verbosity on SMP CPU stop
    When CPUs are stopped during an abnormal operation like panic
    for each CPU a line is printed and the stack trace is dumped.
    
    This information is only interesting for the aborting CPU
    and on systems with many CPUs it only makes it harder to
    debug if after the aborting CPU the log is flooded with data
    about all other CPUs too.
    
    Therefore remove the stack dump and printk of other CPUs
    and only print a single line that the other CPUs are going to be
    stopped and, in case any CPUs remain online list them.
    
    Signed-off-by: Jan Glauber <jglauber@cavium.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -805,21 +805,11 @@ void arch_irq_work_raise(void)
 }
 #endif
 
-static DEFINE_RAW_SPINLOCK(stop_lock);
-
 /*
  * ipi_cpu_stop - handle IPI from smp_send_stop()
  */
 static void ipi_cpu_stop(unsigned int cpu)
 {
-	if (system_state == SYSTEM_BOOTING ||
-	    system_state == SYSTEM_RUNNING) {
-		raw_spin_lock(&stop_lock);
-		pr_crit("CPU%u: stopping\n", cpu);
-		dump_stack();
-		raw_spin_unlock(&stop_lock);
-	}
-
 	set_cpu_online(cpu, false);
 
 	local_irq_disable();
@@ -914,6 +904,9 @@ void smp_send_stop(void)
 		cpumask_copy(&mask, cpu_online_mask);
 		cpumask_clear_cpu(smp_processor_id(), &mask);
 
+		if (system_state == SYSTEM_BOOTING ||
+		    system_state == SYSTEM_RUNNING)
+			pr_crit("SMP: stopping secondary CPUs\n");
 		smp_cross_call(&mask, IPI_CPU_STOP);
 	}
 
@@ -923,7 +916,8 @@ void smp_send_stop(void)
 		udelay(1);
 
 	if (num_online_cpus() > 1)
-		pr_warning("SMP: failed to stop secondary CPUs\n");
+		pr_warning("SMP: failed to stop secondary CPUs %*pbl\n",
+			   cpumask_pr_args(cpu_online_mask));
 }
 
 /*
