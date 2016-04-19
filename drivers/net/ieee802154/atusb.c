151c37bc29bcbc4b34450c76a8125a5b155520e7
Stefan Schmidt <stefan@osg.samsung.com>
2016-04-19 16:28:55 +0200

    ieee802154: atusb: update my copyright years for this driver
    Signed-off-by: Stefan Schmidt <stefan@osg.samsung.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/net/ieee802154/atusb.c
+++ drivers/net/ieee802154/atusb.c
@@ -3,6 +3,8 @@
  *
  * Written 2013 by Werner Almesberger <werner@almesberger.net>
  *
+ * Copyright (c) 2015 - 2016 Stefan Schmidt <stefan@datenfreihafen.org>
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, version 2
