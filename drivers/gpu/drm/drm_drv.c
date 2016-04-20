ae88dd3f98b2ddce57b7d402760cf3c540853956
Ezequiel Garcia <ezequiel@vanguardiasur.com.ar>
2016-04-20 13:45:03 -0300

    UPSTREAM: drm: Make drm.debug parameter description more helpful
    Let's be user-friendly and print an actually helpful parameter
    description.
    
    This makes modinfo output the debug parameter like this:
    
    parm:           debug:Enable debug output, where each bit enables a debug category.
    		Bit 0 (0x01) will enable CORE messages (drm core code)
    		Bit 1 (0x02) will enable DRIVER messages (drm controller code)
    		Bit 2 (0x04) will enable KMS messages (modesetting code)
    		Bit 3 (0x08) will enable PRIME messages (prime code)
    		Bit 4 (0x10) will enable ATOMIC messages (atomic code)
    		Bit 5 (0x20) will enable VBL messages (vblank code) (int)
    
    Changes from v1:
    
      * Fixed s/PRMIE/PRIME typo.
      * Add ATOMIC and VBL debug parameter documentation.
      * Prefix the continuation lines with two tabs and
        removed the last new line.
      * Remove spurious whitespace.
    
    Signed-off-by: Ezequiel Garcia <ezequiel@vanguardiasur.com.ar>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1461170703-11216-1-git-send-email-ezequiel@vanguardiasur.com.ar
    (cherry picked from commit 6dc3e22ee197095eb6a6b3119f04f9dc6c2bbf91)
    
    Change-Id: Ic78c00e9baec0eaa63a49ddc774bcf37ee5479ce
    Signed-off-by: Mark Yao <mark.yao@rock-chips.com>

--- drivers/gpu/drm/drm_drv.c
+++ drivers/gpu/drm/drm_drv.c
@@ -38,13 +38,23 @@
 #include "drm_legacy.h"
 #include "drm_internal.h"
 
-unsigned int drm_debug = 0;	/* bitmask of DRM_UT_x */
+/*
+ * drm_debug: Enable debug output.
+ * Bitmask of DRM_UT_x. See include/drm/drmP.h for details.
+ */
+unsigned int drm_debug = 0;
 EXPORT_SYMBOL(drm_debug);
 
 MODULE_AUTHOR(CORE_AUTHOR);
 MODULE_DESCRIPTION(CORE_DESC);
 MODULE_LICENSE("GPL and additional rights");
-MODULE_PARM_DESC(debug, "Enable debug output");
+MODULE_PARM_DESC(debug, "Enable debug output, where each bit enables a debug category.\n"
+"\t\tBit 0 (0x01) will enable CORE messages (drm core code)\n"
+"\t\tBit 1 (0x02) will enable DRIVER messages (drm controller code)\n"
+"\t\tBit 2 (0x04) will enable KMS messages (modesetting code)\n"
+"\t\tBit 3 (0x08) will enable PRIME messages (prime code)\n"
+"\t\tBit 4 (0x10) will enable ATOMIC messages (atomic code)\n"
+"\t\tBit 5 (0x20) will enable VBL messages (vblank code)");
 module_param_named(debug, drm_debug, int, 0600);
 
 static DEFINE_SPINLOCK(drm_minor_lock);
