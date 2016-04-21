bde569874b2afa6571eee6c373b236a8cd292115
Hans Verkuil <hans.verkuil@cisco.com>
2016-04-21 03:23:58 -0300

    [media] tw686x-video: test for 60Hz instead of 50Hz
    When determining if the standard is 50 or 60 Hz it is standard
    practice to test for 60 Hz instead of 50 Hz.
    
    This doesn't matter normally, except if the user specifies both
    60 and 50 Hz standards.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/pci/tw686x/tw686x-video.c
+++ drivers/media/pci/tw686x/tw686x-video.c
@@ -25,7 +25,7 @@
 
 #define TW686X_INPUTS_PER_CH		4
 #define TW686X_VIDEO_WIDTH		720
-#define TW686X_VIDEO_HEIGHT(id)		((id & V4L2_STD_625_50) ? 576 : 480)
+#define TW686X_VIDEO_HEIGHT(id)		((id & V4L2_STD_525_60) ? 480 : 576)
 
 static const struct tw686x_format formats[] = {
 	{
@@ -517,10 +517,10 @@ static int tw686x_s_std(struct file *file, void *priv, v4l2_std_id id)
 	reg_write(vc->dev, SDT[vc->ch], val);
 
 	val = reg_read(vc->dev, VIDEO_CONTROL1);
-	if (id & V4L2_STD_625_50)
-		val |= (1 << (SYS_MODE_DMA_SHIFT + vc->ch));
-	else
+	if (id & V4L2_STD_525_60)
 		val &= ~(1 << (SYS_MODE_DMA_SHIFT + vc->ch));
+	else
+		val |= (1 << (SYS_MODE_DMA_SHIFT + vc->ch));
 	reg_write(vc->dev, VIDEO_CONTROL1, val);
 
 	/*
