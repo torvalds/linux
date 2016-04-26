6e97ea22575e820ef195b69567cef2427e48c9ef
Octavian Purdila <octavian.purdila@intel.com>
2016-04-26 12:01:34 +0300

    lkl: add support for noop dma mappings
    Signed-off-by: Octavian Purdila <octavian.purdila@intel.com>

--- arch/lkl/include/asm/dma-mapping.h
+++ arch/lkl/include/asm/dma-mapping.h
@@ -1,6 +1,21 @@
 #ifndef _ASM_LKL_DMA_MAPPING_H
 #define _ASM_LKL_DMA_MAPPING_H
 
-#include <asm-generic/dma-mapping-broken.h>
+static inline struct dma_map_ops *get_dma_ops(struct device *dev)
+{
+	return &dma_noop_ops;
+}
+
+static inline void dma_cache_sync(struct device *dev, void *vaddr, size_t size,
+				  enum dma_data_direction direction)
+{
+}
+
+static inline bool dma_capable(struct device *dev, dma_addr_t addr, size_t size)
+{
+	if (!dev->dma_mask)
+		return false;
+	return addr + size - 1 <= *dev->dma_mask;
+}
 
 #endif
