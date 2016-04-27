f6a4790a5471d7cba406d87f6b41323f40bb93d2
Aaron Lu <aaron.lu@intel.com>
2016-04-27 20:45:02 +0800

    video / backlight: add two APIs for drivers to use
    It is useful to get the backlight device's pointer and use it to set
    backlight in some cases(the following patch will make use of it) so add
    the two APIs and export them.
    
    Signed-off-by: Aaron Lu <aaron.lu@intel.com>
    Acked-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/linux/backlight.h
+++ include/linux/backlight.h
@@ -144,6 +144,8 @@ extern void backlight_force_update(struct backlight_device *bd,
 extern bool backlight_device_registered(enum backlight_type type);
 extern int backlight_register_notifier(struct notifier_block *nb);
 extern int backlight_unregister_notifier(struct notifier_block *nb);
+extern struct backlight_device *backlight_device_get_by_type(enum backlight_type type);
+extern int backlight_device_set_brightness(struct backlight_device *bd, unsigned long brightness);
 
 #define to_backlight_device(obj) container_of(obj, struct backlight_device, dev)
 
