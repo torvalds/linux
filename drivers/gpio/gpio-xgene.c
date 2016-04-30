0c60de3f73cddde6a83979c64f63cb1101f5326c
Duc Dang <dhdang@apm.com>
2016-04-30 13:49:27 -0700

    gpio: xgene: Enable ACPI support for X-Gene GFC GPIO driver
    This patch enables ACPI support for X-Gene GFC GPIO driver.
    
    Signed-off-by: Duc Dang <dhdang@apm.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-xgene.c
+++ drivers/gpio/gpio-xgene.c
@@ -17,6 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <linux/acpi.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/io.h>
@@ -211,10 +212,18 @@ static const struct of_device_id xgene_gpio_of_match[] = {
 	{},
 };
 
+#ifdef CONFIG_ACPI
+static const struct acpi_device_id xgene_gpio_acpi_match[] = {
+	{ "APMC0D14", 0 },
+	{ },
+};
+#endif
+
 static struct platform_driver xgene_gpio_driver = {
 	.driver = {
 		.name = "xgene-gpio",
 		.of_match_table = xgene_gpio_of_match,
+		.acpi_match_table = ACPI_PTR(xgene_gpio_acpi_match),
 		.pm     = XGENE_GPIO_PM_OPS,
 	},
 	.probe = xgene_gpio_probe,
