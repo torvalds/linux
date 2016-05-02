1d18a3f0f0809f6c71f1f6e9e268ee904ce0b588
Laxman Dewangan <ldewangan@nvidia.com>
2016-05-03 00:17:32 +0530

    pinctrl: tegra: avoid parked_reg and parked_bank
    NVIDIA's Tegra210 support the park bit to make pinmux configuration
    enable/disable. If parked bit is 1 then configuration does not apply
    and if it is 0 then pinmux configuration applies. This is to support
    to avoid any glitch in pinmux configurations.
    
    The parked bit is part of mux register and mux bank and hence it is
    not required to have member for the parked_reg and parked bank very
    similar to other bit field of the same register.
    
    Remove the need of the parked register and parked bank and get whether
    parked function supported or not by parked_bit.
    
    This is to make the parked bit handling same as other fields of mux
    registers.
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Acked-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/tegra/pinctrl-tegra114.c
+++ drivers/pinctrl/tegra/pinctrl-tegra114.c
@@ -1578,7 +1578,7 @@ static struct tegra_function tegra114_functions[] = {
 		.lock_bit = 7,						\
 		.ioreset_bit = PINGROUP_BIT_##ior(8),			\
 		.rcv_sel_bit = PINGROUP_BIT_##rcv_sel(9),		\
-		.parked_reg = -1,					\
+		.parked_bit = -1,					\
 		.drv_reg = -1,						\
 	}
 
@@ -1599,7 +1599,7 @@ static struct tegra_function tegra114_functions[] = {
 		.rcv_sel_bit = -1,					\
 		.drv_reg = DRV_PINGROUP_REG(r),				\
 		.drv_bank = 0,						\
-		.parked_reg = -1,					\
+		.parked_bit = -1,					\
 		.hsm_bit = hsm_b,					\
 		.schmitt_bit = schmitt_b,				\
 		.lpmd_bit = lpmd_b,					\
