092398dce8c2406bfb0c9eebc3e764ff2ddb62a8
Heinz Mauelshagen <heinzm@redhat.com>
2016-05-03 19:43:57 +0200

    md: md.c: fix oops in mddev_suspend for raid0
    Introduced by upstream commit 70d9798b95562abac005d4ba71d28820f9a201eb
    
    The raid0 personality does not create mddev->thread as oposed to
    other personalities leading to its unconditional access in
    mddev_suspend() causing an oops.
    
    Patch checks for mddev->thread in order to keep the
    intention of aforementioned commit.
    
    Fixes: 70d9798b9556 ("MD: warn for potential deadlock")
    Cc: stable@vger.kernel.org (4.5+)
    Signed-off-by: Heinz Mauelshagen <heinzm@redhat.com>
    Signed-off-by: Shaohua Li <shli@fb.com>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -307,7 +307,7 @@ static blk_qc_t md_make_request(struct request_queue *q, struct bio *bio)
  */
 void mddev_suspend(struct mddev *mddev)
 {
-	WARN_ON_ONCE(current == mddev->thread->tsk);
+	WARN_ON_ONCE(mddev->thread && current == mddev->thread->tsk);
 	if (mddev->suspended++)
 		return;
 	synchronize_rcu();
