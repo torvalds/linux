77a13dd0d162a0f569d16c9210b585120f870a45
Kangjie Lu <kangjielu@gmail.com>
2016-05-03 16:44:07 -0400

    ALSA: timer: Fix leak in SNDRV_TIMER_IOCTL_PARAMS
    commit cec8f96e49d9be372fdb0c3836dcf31ec71e457e upstream.
    
    The stack object âtreadâ has a total size of 32 bytes. Its field
    âeventâ and âvalâ both contain 4 bytes padding. These 8 bytes
    padding bytes are sent to user without being initialized.
    
    Signed-off-by: Kangjie Lu <kjlu@gatech.edu>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- sound/core/timer.c
+++ sound/core/timer.c
@@ -1707,6 +1707,7 @@ static int snd_timer_user_params(struct file *file,
 	if (tu->timeri->flags & SNDRV_TIMER_IFLG_EARLY_EVENT) {
 		if (tu->tread) {
 			struct snd_timer_tread tread;
+			memset(&tread, 0, sizeof(tread));
 			tread.event = SNDRV_TIMER_EVENT_EARLY;
 			tread.tstamp.tv_sec = 0;
 			tread.tstamp.tv_nsec = 0;
