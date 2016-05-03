ab7fdefba68f66c8523571c3b3a940635d781824
Kan Liang <kan.liang@intel.com>
2016-05-03 00:26:06 -0700

    perf/core: Fix implicitly enable dynamic interrupt throttle
    This patch fixes an issue which was introduced by commit:
    
      91a612eea9a3 ("perf/core: Fix dynamic interrupt throttle")
    
    ... which commit unconditionally sets the perf_sample_allowed_ns value
    to !0. But that could trigger a bug in the following corner case:
    
    The user can disable the dynamic interrupt throttle mechanism by setting
    perf_cpu_time_max_percent to 0. Then they change perf_event_max_sample_rate.
    For this case, the mechanism will be enabled implicitly, because
    perf_sample_allowed_ns becomes !0 - which is not what we want.
    
    This patch only updates perf_sample_allowed_ns when the dynamic
    interrupt throttle mechanism is enabled.
    
    Signed-off-by: Kan Liang <kan.liang@intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vince Weaver <vincent.weaver@maine.edu>
    Cc: acme@kernel.org
    Link: http://lkml.kernel.org/r/1462260366-3160-1-git-send-email-kan.liang@intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -397,6 +397,13 @@ int perf_proc_update_handler(struct ctl_table *table, int write,
 	if (ret || !write)
 		return ret;
 
+	/*
+	 * If throttling is disabled don't allow the write:
+	 */
+	if (sysctl_perf_cpu_time_max_percent == 100 ||
+	    sysctl_perf_cpu_time_max_percent == 0)
+		return -EINVAL;
+
 	max_samples_per_tick = DIV_ROUND_UP(sysctl_perf_event_sample_rate, HZ);
 	perf_sample_period_ns = NSEC_PER_SEC / sysctl_perf_event_sample_rate;
 	update_perf_cpu_limits();
