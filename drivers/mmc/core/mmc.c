437db4c6e79881d33aca521987188c728df350a8
Adrian Hunter <adrian.hunter@intel.com>
2016-05-04 10:38:21 +0300

    mmc: mmc: Attempt to flush cache before reset
    CMD0 or hardware reset may invalidate the cache, so it needs to be
    flushed before reset.
    
    In the case of recovery, we can't expect flushing the cache to work
    always, but have a go and ignore errors.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1958,6 +1958,12 @@ static int mmc_reset(struct mmc_host *host)
 {
 	struct mmc_card *card = host->card;
 
+	/*
+	 * In the case of recovery, we can't expect flushing the cache to work
+	 * always, but we have a go and ignore errors.
+	 */
+	mmc_flush_cache(host->card);
+
 	if ((host->caps & MMC_CAP_HW_RESET) && host->ops->hw_reset &&
 	     mmc_can_reset(card)) {
 		/* If the card accept RST_n signal, send it. */
