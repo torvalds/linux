4d2458507d0b465c62ae80f3e81b8c008ec96b05
Fabio Estevam <fabio.estevam@nxp.com>
2016-05-04 19:33:59 -0300

    ASoC: fsl_sai: Allow setting the SAI MCLK direction
    On mx6ul the General Purpose Register 1 (GPR1) contains the following
    bits for configuring the direction of the SAI MCLKs:
    SAI1_MCLK_DIR, SAI2_MCLK_DIR, SAI3_MCLK_DIR
    
    Introduce  the "fsl,sai-mclk-direction-output" optional property to allow
    configuring the SAI_MCLK outputs.
    
    Tested on a imx6ul-evk board.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>
    Acked-by: Nicolin Chen <nicoleotsuka@gmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/fsl/fsl_sai.c
+++ sound/soc/fsl/fsl_sai.c
@@ -21,6 +21,8 @@
 #include <sound/core.h>
 #include <sound/dmaengine_pcm.h>
 #include <sound/pcm_params.h>
+#include <linux/mfd/syscon.h>
+#include <linux/mfd/syscon/imx6q-iomuxc-gpr.h>
 
 #include "fsl_sai.h"
 #include "imx-pcm.h"
@@ -786,10 +788,12 @@ static int fsl_sai_probe(struct platform_device *pdev)
 {
 	struct device_node *np = pdev->dev.of_node;
 	struct fsl_sai *sai;
+	struct regmap *gpr;
 	struct resource *res;
 	void __iomem *base;
 	char tmp[8];
 	int irq, ret, i;
+	int index;
 
 	sai = devm_kzalloc(&pdev->dev, sizeof(*sai), GFP_KERNEL);
 	if (!sai)
@@ -876,6 +880,22 @@ static int fsl_sai_probe(struct platform_device *pdev)
 		fsl_sai_dai.symmetric_rates = 0;
 		fsl_sai_dai.symmetric_channels = 0;
 		fsl_sai_dai.symmetric_samplebits = 0;
+	}
+
+	if (of_find_property(np, "fsl,sai-mclk-direction-output", NULL) &&
+	    of_device_is_compatible(pdev->dev.of_node, "fsl,imx6ul-sai")) {
+		gpr = syscon_regmap_lookup_by_compatible("fsl,imx6ul-iomuxc-gpr");
+		if (IS_ERR(gpr)) {
+			dev_err(&pdev->dev, "cannot find iomuxc registers\n");
+			return PTR_ERR(gpr);
+		}
+
+		index = of_alias_get_id(np, "sai");
+		if (index < 0)
+			return index;
+
+		regmap_update_bits(gpr, IOMUXC_GPR1, MCLK_DIR(index),
+				   MCLK_DIR(index));
 	}
 
 	sai->dma_params_rx.addr = res->start + FSL_SAI_RDR;
