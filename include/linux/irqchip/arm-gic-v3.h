44bfc42e94cd76a0bd44f3fce98d4a7b76f31bc0
Andre Przywara <andre.przywara@arm.com>
2016-05-04 14:35:48 +0100

    KVM: arm/arm64: move GICv2 emulation defines into arm-gic-v3.h
    As (some) GICv3 hosts can emulate a GICv2, some GICv2 specific masks
    for the list register definition also apply to GICv3 LRs.
    At the moment we have those definitions in the KVM VGICv3
    implementation, so let's move them into the GICv3 header file to
    have them automatically defined.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>
    Acked-by: Marc Zyngier <marc.zyngier@arm.com>

--- include/linux/irqchip/arm-gic-v3.h
+++ include/linux/irqchip/arm-gic-v3.h
@@ -276,6 +276,11 @@
 #define ICH_LR_PHYS_ID_SHIFT		32
 #define ICH_LR_PHYS_ID_MASK		(0x3ffULL << ICH_LR_PHYS_ID_SHIFT)
 
+/* These are for GICv2 emulation only */
+#define GICH_LR_VIRTUALID		(0x3ffUL << 0)
+#define GICH_LR_PHYSID_CPUID_SHIFT	(10)
+#define GICH_LR_PHYSID_CPUID		(7UL << GICH_LR_PHYSID_CPUID_SHIFT)
+
 #define ICH_MISR_EOI			(1 << 0)
 #define ICH_MISR_U			(1 << 1)
 
