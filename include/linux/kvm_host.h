9b9e3fc4d5a31f6050508f2404369beac4356867
Greg Kurz <gkurz@linux.vnet.ibm.com>
2016-05-09 18:11:54 +0200

    KVM: remove NULL return path for vcpu ids >= KVM_MAX_VCPUS
    Commit c896939f7cff ("KVM: use heuristic for fast VCPU lookup by id") added
    a return path that prevents vcpu ids to exceed KVM_MAX_VCPUS. This is a
    problem for powerpc where vcpu ids can grow up to 8*KVM_MAX_VCPUS.
    
    This patch simply reverses the logic so that we only try fast path if the
    vcpu id can be tried as an index in kvm->vcpus[]. The slow path is not
    affected by the change.
    
    Reviewed-by: David Hildenbrand <dahi@linux.vnet.ibm.com>
    Reviewed-by: Cornelia Huck <cornelia.huck@de.ibm.com>
    Signed-off-by: Greg Kurz <gkurz@linux.vnet.ibm.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -447,12 +447,13 @@ static inline struct kvm_vcpu *kvm_get_vcpu(struct kvm *kvm, int i)
 
 static inline struct kvm_vcpu *kvm_get_vcpu_by_id(struct kvm *kvm, int id)
 {
-	struct kvm_vcpu *vcpu;
+	struct kvm_vcpu *vcpu = NULL;
 	int i;
 
-	if (id < 0 || id >= KVM_MAX_VCPUS)
+	if (id < 0)
 		return NULL;
-	vcpu = kvm_get_vcpu(kvm, id);
+	if (id < KVM_MAX_VCPUS)
+		vcpu = kvm_get_vcpu(kvm, id);
 	if (vcpu && vcpu->vcpu_id == id)
 		return vcpu;
 	kvm_for_each_vcpu(i, vcpu, kvm)
