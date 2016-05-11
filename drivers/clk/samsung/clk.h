4dbf01b8df00864787b6c7e3d3e0890d385b0ff5
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2016-05-11 14:02:14 +0200

    clk: samsung: Fully constify mux parent names
    The PNAME macro defines array of strings for names of mux parents.
    Although the strings itself were const but pointers to them were not thus
    this data resided in initdata. Make this an array of const pointers to
    const strings and move to initconst section.
    
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>

--- drivers/clk/samsung/clk.h
+++ drivers/clk/samsung/clk.h
@@ -261,7 +261,7 @@ struct samsung_gate_clock {
 #define GATE_DA(_id, dname, cname, pname, o, b, f, gf, a)	\
 	__GATE(_id, dname, cname, pname, o, b, f, gf, a)
 
-#define PNAME(x) static const char *x[] __initdata
+#define PNAME(x) static const char * const x[] __initconst
 
 /**
  * struct samsung_clk_reg_dump: register dump of clock controller registers.
