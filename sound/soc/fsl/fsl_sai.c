7c2cbb4af186a38b304faeb698dcae4e25f1907e
Shengjiu Wang <shengjiu.wang@freescale.com>
2016-05-12 17:54:08 +0800

    MLK-12786-2: ASoC: fsl_sai: correct the clock source for mclk0
    mclk0 is assigned through the device tree.
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@freescale.com>

--- sound/soc/fsl/fsl_sai.c
+++ sound/soc/fsl/fsl_sai.c
@@ -833,8 +833,7 @@ static int fsl_sai_probe(struct platform_device *pdev)
 		sai->bus_clk = NULL;
 	}
 
-	sai->mclk_clk[0] = sai->bus_clk;
-	for (i = 1; i < FSL_SAI_MCLK_MAX; i++) {
+	for (i = 0; i < FSL_SAI_MCLK_MAX; i++) {
 		sprintf(tmp, "mclk%d", i);
 		sai->mclk_clk[i] = devm_clk_get(&pdev->dev, tmp);
 		if (IS_ERR(sai->mclk_clk[i])) {
