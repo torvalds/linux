3bce6db27ec2a46dd4082f782582d8915eabac2e
Shengjiu Wang <shengjiu.wang@freescale.com>
2016-05-14 14:50:22 +0800

    MLK-12794: ASoC: fsl_asrc: fix underrun issue when convert 192k to 96kHz.
    The maximum divider of asrc clock is 1024, but there is no judgement
    for this limitaion in driver, which may cause the divider setting not
    correct.
    
    When IDEAL_RATIO_RATE 200kHZ, the cost time of conversion from 192kHz
    to 96kHz is 24ms every 1024 sample, but these sample's playback time
    is 1024/96=11ms, so there will be underrun. So need to enlarge this RATE.
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@freescale.com>

--- sound/soc/fsl/fsl_asrc.c
+++ sound/soc/fsl/fsl_asrc.c
@@ -354,6 +354,12 @@ static int fsl_asrc_config_pair(struct fsl_asrc_pair *pair, bool p2p_in, bool p2
 		return -EINVAL;
 	}
 
+	if (div[IN] > 1024)
+		div[IN] = 1024;
+
+	if (div[OUT] > 1024)
+		div[OUT] = 1024;
+
 	/* Set the channel number */
 	channels = config->channel_num;
 
