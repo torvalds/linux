aa3463d65e7b9f5ae322db4a12214c2cb041bc8e
Tom Herbert <tom@herbertland.com>
2016-05-18 09:06:18 -0700

    fou: Add encap ops for IPv6 tunnels
    This patch add a new fou6 module that provides encapsulation
    operations for IPv6.
    
    Signed-off-by: Tom Herbert <tom@herbertland.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/fou.h
+++ include/net/fou.h
@@ -9,7 +9,7 @@
 #include <net/udp.h>
 
 size_t fou_encap_hlen(struct ip_tunnel_encap *e);
-static size_t gue_encap_hlen(struct ip_tunnel_encap *e);
+size_t gue_encap_hlen(struct ip_tunnel_encap *e);
 
 int __fou_build_header(struct sk_buff *skb, struct ip_tunnel_encap *e,
 		       u8 *protocol, __be16 *sport, int type);
