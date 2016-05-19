11825b0dba78b2f26a6ff9a6acb5d5b6b186e741
Ville Syrjälä <ville.syrjala@linux.intel.com>
2016-05-19 12:14:43 +0300

    drm/i915: Enable GSE interrupt on BDW+
    We've never actually enabled or unmasked the GSE interrupt on BDW+,
    even though the interrupt handler was always prepared for it.
    Let's enable it and see what happens.
    
    Credit to Mark Kettenis who fixed this in the OpenBSD fork of the
    driver. He reports that it fixed the "ACPI _BCM/_BCQ-based
    brightness mechanism on a MacBookPro12,1 and a 3rd gen Lenovo X1
    Carbon" for them.
    
    Mark says:
    "FWIW, this *is* needed if you want ACPI-based backlight control to
     work.  On Linux you probably don't notice, since "hardware" backlight
     control is preferred over "firmware" or "platform" backlight control.
    
     It would help me if this did land in the Linux tree though, as it will
     make future imports of the i915 driver into OpenBSD easier."
    
    So even though we don't really need this, let's put it in to help Mark
    with future porting efforts. Should be harmless to have it enabled in
    any case.
    
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    References: http://lists.freedesktop.org/archives/intel-gfx/2015-December/081799.html
    Reported-by: Mark Kettenis <mark.kettenis@xs4all.nl>
    Cc: Mark Kettenis <mark.kettenis@xs4all.nl>
    Cc: Jani Nikula <jani.nikula@linux.intel.com>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1463649283-28698-1-git-send-email-ville.syrjala@linux.intel.com
    Acked-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/i915_irq.c
+++ drivers/gpu/drm/i915/i915_irq.c
@@ -3794,6 +3794,7 @@ static void gen8_de_irq_postinstall(struct drm_i915_private *dev_priv)
 	uint32_t de_pipe_enables;
 	u32 de_port_masked = GEN8_AUX_CHANNEL_A;
 	u32 de_port_enables;
+	u32 de_misc_masked = GEN8_DE_MISC_GSE;
 	enum pipe pipe;
 
 	if (INTEL_INFO(dev_priv)->gen >= 9) {
@@ -3829,6 +3830,7 @@ static void gen8_de_irq_postinstall(struct drm_i915_private *dev_priv)
 					  de_pipe_enables);
 
 	GEN5_IRQ_INIT(GEN8_DE_PORT_, ~de_port_masked, de_port_enables);
+	GEN5_IRQ_INIT(GEN8_DE_MISC_, ~de_misc_masked, de_misc_masked);
 }
 
 static int gen8_irq_postinstall(struct drm_device *dev)
