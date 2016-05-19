eac7193632e64383e35fa387e683dd317b9df047
Jubin John <jubin.john@intel.com>
2016-05-19 05:21:37 -0700

    IB/hfi1: Fix sdma_event_names[] build warning
    sdma_event_names[] is only used within CONFIG_SDMA_VERBOSITY ifdefs, so
    when CONFIG_SDMA_VERBOSITY is disabled, it results in the following
    0-day build warning:
    >> drivers/infiniband/hw/hfi1/sdma.c:137:27: warning: 'sdma_event_names'
    >> defined but not used [-Wunused-const-variable=]
        static const char * const sdma_event_names[] = {
                                  ^~~~~~~~~~~~~~~~
    This occurs on the following compiler:
    compiler: gcc-6 (Debian 6.1.1-1) 6.1.1 20160430
    
    For more information check:
    https://lists.01.org/pipermail/kbuild-all/2016-May/020060.html
    
    Fix this warning by defining sdma_event_name[] only within the
    CONFIG_SDMA_VERBOSITY ifdefs.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Reviewed-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
    Reviewed-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Signed-off-by: Jubin John <jubin.john@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/staging/rdma/hfi1/sdma.c
+++ drivers/staging/rdma/hfi1/sdma.c
@@ -134,6 +134,7 @@ static const char * const sdma_state_names[] = {
 	[sdma_state_s99_running]                = "s99_Running",
 };
 
+#ifdef CONFIG_SDMA_VERBOSITY
 static const char * const sdma_event_names[] = {
 	[sdma_event_e00_go_hw_down]   = "e00_GoHwDown",
 	[sdma_event_e10_go_hw_start]  = "e10_GoHwStart",
@@ -150,6 +151,7 @@ static const char * const sdma_event_names[] = {
 	[sdma_event_e85_link_down]    = "e85_LinkDown",
 	[sdma_event_e90_sw_halted]    = "e90_SwHalted",
 };
+#endif
 
 static const struct sdma_set_state_action sdma_action_table[] = {
 	[sdma_state_s00_hw_down] = {
