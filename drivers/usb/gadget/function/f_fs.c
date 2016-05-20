53642399aa71b7c3b15d0305dc54738c4222bb1e
Jim Lin <jilin@nvidia.com>
2016-05-20 18:13:19 +0800

    usb: gadget: f_fs: Fix wrong check on reserved1 of OS_DESC_EXT_COMPAT
    Current __ffs_data_do_os_desc() of f_fs.c will check reserved1 field
    of OS_DESC_EXT_COMPAT and return -EINVAL if it's 1.
    But MS OS 1.0 Descriptors
    http://msdn.microsoft.com/en-us/library/windows/hardware/gg463179.aspx
    defines that field to be 1.
    
    Signed-off-by: Jim Lin <jilin@nvidia.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -2051,7 +2051,7 @@ static int __ffs_data_do_os_desc(enum ffs_os_desc_type type,
 
 		if (len < sizeof(*d) ||
 		    d->bFirstInterfaceNumber >= ffs->interfaces_count ||
-		    d->Reserved1)
+		    !d->Reserved1)
 			return -EINVAL;
 		for (i = 0; i < ARRAY_SIZE(d->Reserved2); ++i)
 			if (d->Reserved2[i])
