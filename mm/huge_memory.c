340a43bed674a70308f196f2a61ec0b01f8a14d9
Yang Shi <yang.shi@linaro.org>
2016-05-20 16:57:30 -0700

    mm: thp: simplify the implementation of mk_huge_pmd()
    The implementation of mk_huge_pmd looks verbose, it could be just
    simplified to one line code.
    
    Signed-off-by: Yang Shi <yang.shi@linaro.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -764,10 +764,7 @@ pmd_t maybe_pmd_mkwrite(pmd_t pmd, struct vm_area_struct *vma)
 
 static inline pmd_t mk_huge_pmd(struct page *page, pgprot_t prot)
 {
-	pmd_t entry;
-	entry = mk_pmd(page, prot);
-	entry = pmd_mkhuge(entry);
-	return entry;
+	return pmd_mkhuge(mk_pmd(page, prot));
 }
 
 static inline struct list_head *page_deferred_list(struct page *page)
