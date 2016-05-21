f39e1769bbfc4936ff8364fb2529dc8bf6bc6888
Joe Perches <joe@perches.com>
2016-05-20 17:04:02 -0700

    checkpatch: improve CONSTANT_COMPARISON test for structure members
    A "." dereference to an all uppercase structure member can be
    incorrectly reported as a CONSTANT_COMPARISON.
    
    ie: "if (table[i].PANELID == tempdx)"
    
    Fix it by checking for "." before the constant test.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -4291,7 +4291,7 @@ sub process {
 			my $comp = $3;
 			my $to = $4;
 			my $newcomp = $comp;
-			if ($lead !~ /$Operators\s*$/ &&
+			if ($lead !~ /(?:$Operators|\.)\s*$/ &&
 			    $to !~ /^(?:Constant|[A-Z_][A-Z0-9_]*)$/ &&
 			    WARN("CONSTANT_COMPARISON",
 				 "Comparisons should place the constant on the right side of the test\n" . $herecurr) &&
