a46d8c3fc7f520632e773ff51209e98770a56846
Salah Triki <salah.triki@acm.org>
2016-05-23 16:22:55 -0700

    fs/befs/io.c:befs_bread(): remove unneeded initialization to NULL
    bh is reinitialized by sb_bread() so no need to init it
    with NULL in the beginning of befs_bread()
    
    Link: http://lkml.kernel.org/r/88481760b43226fac16adb1f1e68897e47d8235c.1462841692.git.salah.triki@acm.org
    Signed-off-by: Salah Triki <salah.triki@acm.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/befs/io.c
+++ fs/befs/io.c
@@ -63,7 +63,7 @@ befs_bread_iaddr(struct super_block *sb, befs_inode_addr iaddr)
 struct buffer_head *
 befs_bread(struct super_block *sb, befs_blocknr_t block)
 {
-	struct buffer_head *bh = NULL;
+	struct buffer_head *bh;
 
 	befs_debug(sb, "---> Enter %s %lu", __func__, (unsigned long)block);
 
