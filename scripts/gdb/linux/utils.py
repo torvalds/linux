321958d9710c33e74ec98c0f3c96aa2a5dbe3008
Dom Cote <buzdelabuz2+git@gmail.com>
2016-05-23 16:25:16 -0700

    scripts/gdb: improve types abstraction for gdb python scripts
    Change the read_u16 function so it accepts both 'str' and 'byte' as type
    for the arguments.
    
    When calling read_memory() from gdb API, depending on if it was built
    with 2.7 or 3.X, the format used to return the data will differ ( 'str'
    for 2.7, and 'byte' for 3.X ).
    
    Add a function read_memoryview() to be able to get a 'memoryview' object
    back from read_memory() both with python 2.7 and 3.X .
    
    Tested with python 3.4 and 2.7
    Tested with gdb 7.7
    
    Link: http://lkml.kernel.org/r/73621f564503137a002a639d174e4fb35f73f462.1462865983.git.jan.kiszka@siemens.com
    Signed-off-by: Dom Cote <buzdelabuz2+git@gmail.com>
    Tested-by: Kieran Bingham <kieran@bingham.xyz> (Py2.7,Py3.4,GDB10)
    Signed-off-by: Kieran Bingham <kieran@bingham.xyz>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/gdb/linux/utils.py
+++ scripts/gdb/linux/utils.py
@@ -87,11 +87,24 @@ def get_target_endianness():
     return target_endianness
 
 
+def read_memoryview(inf, start, length):
+    return memoryview(inf.read_memory(start, length))
+
+
 def read_u16(buffer):
-    if get_target_endianness() == LITTLE_ENDIAN:
-        return ord(buffer[0]) + (ord(buffer[1]) << 8)
+    value = [0, 0]
+
+    if type(buffer[0]) is str:
+        value[0] = ord(buffer[0])
+        value[1] = ord(buffer[1])
     else:
-        return ord(buffer[1]) + (ord(buffer[0]) << 8)
+        value[0] = buffer[0]
+        value[1] = buffer[1]
+
+    if get_target_endianness() == LITTLE_ENDIAN:
+        return value[0] + (value[1] << 8)
+    else:
+        return value[1] + (value[0] << 8)
 
 
 def read_u32(buffer):
