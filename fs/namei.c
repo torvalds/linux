f2a031b66ec6da1512f9db228e4174ad2c7d2688
George Spelvin <linux@sciencehorizons.net>
2016-05-29 01:26:41 -0400

    Rename other copy of hash_string to hashlen_string
    The original name was simply hash_string(), but that conflicted with a
    function with that name in drivers/base/power/trace.c, and I decided
    that calling it "hashlen_" was better anyway.
    
    But you have to do it in two places.
    
    [ This caused build errors for architectures that don't define
      CONFIG_DCACHE_WORD_ACCESS   - Linus ]
    
    Signed-off-by: George Spelvin <linux@sciencehorizons.net>
    Reported-by: Guenter Roeck <linux@roeck-us.net>
    Fixes: fcfd2fbf22d2 ("fs/namei.c: Add hashlen_string() function")
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/namei.c
+++ fs/namei.c
@@ -1962,7 +1962,7 @@ unsigned int full_name_hash(const char *name, unsigned int len)
 EXPORT_SYMBOL(full_name_hash);
 
 /* Return the "hash_len" (hash and length) of a null-terminated string */
-u64 hash_string(const char *name)
+u64 hashlen_string(const char *name)
 {
 	unsigned long hash = init_name_hash();
 	unsigned long len = 0, c;
@@ -1975,7 +1975,7 @@ u64 hash_string(const char *name)
 	} while (c);
 	return hashlen_create(end_name_hash(hash), len);
 }
-EXPORT_SYMBOL(hash_string);
+EXPORT_SYMBOL(hashlen_string);
 
 /*
  * We know there's a real path component here of at least
