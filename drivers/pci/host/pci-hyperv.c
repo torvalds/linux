60fcdac8136b4275da42d6edf9ddb10439350289
Vitaly Kuznetsov <vkuznets@redhat.com>
2016-05-30 16:17:58 +0200

    PCI: hv: Don't leak buffer in hv_pci_onchannelcallback()
    We don't free buffer on several code paths in hv_pci_onchannelcallback(),
    put kfree() to the end of the function to fix the issue.  Direct { kfree();
    return; } can now be replaced with a simple 'break';
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Jake Oshins <jakeo@microsoft.com>

--- drivers/pci/host/pci-hyperv.c
+++ drivers/pci/host/pci-hyperv.c
@@ -1661,10 +1661,8 @@ static void hv_pci_onchannelcallback(void *context)
 		 * All incoming packets must be at least as large as a
 		 * response.
 		 */
-		if (bytes_recvd <= sizeof(struct pci_response)) {
-			kfree(buffer);
-			return;
-		}
+		if (bytes_recvd <= sizeof(struct pci_response))
+			break;
 		desc = (struct vmpacket_descriptor *)buffer;
 
 		switch (desc->type) {
@@ -1679,8 +1677,7 @@ static void hv_pci_onchannelcallback(void *context)
 			comp_packet->completion_func(comp_packet->compl_ctxt,
 						     response,
 						     bytes_recvd);
-			kfree(buffer);
-			return;
+			break;
 
 		case VM_PKT_DATA_INBAND:
 
@@ -1729,6 +1726,8 @@ static void hv_pci_onchannelcallback(void *context)
 		}
 		break;
 	}
+
+	kfree(buffer);
 }
 
 /**
