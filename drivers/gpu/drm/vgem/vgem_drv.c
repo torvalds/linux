1bd816f120718d578af7e4b623b9a0dec59c028c
Daniel Vetter <daniel.vetter@ffwll.ch>
2016-05-30 19:53:08 +0200

    drm/vgem: Use lockless gem BO free callback
    No dev->struct_mutex anywhere to be seen.
    
    Cc: seanpaul@chromium.org
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1464630800-30786-17-git-send-email-daniel.vetter@ffwll.ch

--- drivers/gpu/drm/vgem/vgem_drv.c
+++ drivers/gpu/drm/vgem/vgem_drv.c
@@ -235,7 +235,7 @@ static const struct file_operations vgem_driver_fops = {
 
 static struct drm_driver vgem_driver = {
 	.driver_features		= DRIVER_GEM,
-	.gem_free_object		= vgem_gem_free_object,
+	.gem_free_object_unlocked	= vgem_gem_free_object,
 	.gem_vm_ops			= &vgem_gem_vm_ops,
 	.ioctls				= vgem_ioctls,
 	.fops				= &vgem_driver_fops,
