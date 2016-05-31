79c2df5866a14fcac4235332fddb4d5a2d99d76d
Wolfram Sang <wsa+renesas@sang-engineering.com>
2016-05-31 12:56:36 +0200

    staging: ks7010: indent michael_mic.h
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/michael_mic.h
+++ drivers/staging/ks7010/michael_mic.h
@@ -16,14 +16,14 @@
 struct michel_mic_t {
 	uint32_t K0;	// Key 
 	uint32_t K1;	// Key 
-	uint32_t L;		// Current state 
-	uint32_t R;		// Current state 
-	uint8_t M[4];		// Message accumulator (single word) 
-	int		nBytesInM; 	// # bytes in M 
-	uint8_t 	Result[8];
+	uint32_t L;	// Current state 
+	uint32_t R;	// Current state 
+	uint8_t M[4];	// Message accumulator (single word) 
+	int nBytesInM;	// # bytes in M 
+	uint8_t Result[8];
 };
 
 extern
-void MichaelMICFunction( struct michel_mic_t *Mic, uint8_t *Key, 
-			 uint8_t *Data, int Len, uint8_t priority, 
-			 uint8_t *Result );
+void MichaelMICFunction(struct michel_mic_t *Mic, uint8_t * Key,
+			uint8_t * Data, int Len, uint8_t priority,
+			uint8_t * Result);
