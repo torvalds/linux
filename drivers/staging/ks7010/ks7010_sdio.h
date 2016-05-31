4a3e75483dfadd5164f482bc1af9c4a7e3ee67b0
Wolfram Sang <wsa+renesas@sang-engineering.com>
2016-05-31 12:56:29 +0200

    staging: ks7010: indent ks7010_sdio.h
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/ks7010_sdio.h
+++ drivers/staging/ks7010/ks7010_sdio.h
@@ -21,7 +21,7 @@
 #define DEVICE_ALIGNMENT 32
 
 /*  SDIO KeyStream vendor and device */
-#define SDIO_VENDOR_ID_KS_CODE_A	0x005b 
+#define SDIO_VENDOR_ID_KS_CODE_A	0x005b
 #define SDIO_VENDOR_ID_KS_CODE_B	0x0023
 #define SDIO_DEVICE_ID_KS_7010		0x7910
 
@@ -48,8 +48,8 @@
  * for network packet (less than 2048 bytes data)
  */
 #define WSTATUS_RSIZE		0x000014
-#define WSTATUS_MASK		0x80 /* Write Status Register value */
-#define RSIZE_MASK		0x7F /* Read Data Size Register value [10:4] */
+#define WSTATUS_MASK		0x80	/* Write Status Register value */
+#define RSIZE_MASK		0x7F	/* Read Data Size Register value [10:4] */
 
 /* ARM to SD interrupt Enable */
 #define INT_ENABLE		0x000020
@@ -88,52 +88,48 @@
 
 #define KS7010_IRAM_ADDRESS	0x06000000
 
-
 /* 
  * struct define
  */
 struct hw_info_t {
-	struct ks_sdio_card *sdio_card;	
+	struct ks_sdio_card *sdio_card;
 	struct completion ks7010_sdio_wait;
 	struct workqueue_struct *ks7010sdio_wq;
 	struct delayed_work rw_wq;
-	unsigned char	*read_buf;
+	unsigned char *read_buf;
 	struct tasklet_struct rx_bh_task;
 };
 
 struct ks_sdio_packet {
-        struct ks_sdio_packet   *next;
-        u16                     nb;
-        u8                      buffer[0] __attribute__((aligned(4)));
+	struct ks_sdio_packet *next;
+	u16 nb;
+	u8 buffer[0] __attribute__ ((aligned(4)));
 };
-
 
 struct ks_sdio_card {
-	struct sdio_func	*func;
+	struct sdio_func *func;
 	struct ks_wlan_private *priv;
-	int			model;
-	const char		*firmware;
-        spinlock_t              lock;
+	int model;
+	const char *firmware;
+	spinlock_t lock;
 };
-
-
 
 /* Tx Device struct */
-#define	TX_DEVICE_BUFF_SIZE	1024 
+#define	TX_DEVICE_BUFF_SIZE	1024
 
 struct tx_device_buffer {
-        unsigned char *sendp;		/* pointer of send req data */
-        unsigned int  size;
-	void	(*complete_handler)(void *arg1, void *arg2);
-	void	*arg1;
-	void	*arg2;
+	unsigned char *sendp;	/* pointer of send req data */
+	unsigned int size;
+	void (*complete_handler) (void *arg1, void *arg2);
+	void *arg1;
+	void *arg2;
 };
 
-struct tx_device{
-        struct tx_device_buffer tx_dev_buff[TX_DEVICE_BUFF_SIZE];
-        unsigned int	qhead; /* tx buffer queue first pointer */
-        unsigned int	qtail; /* tx buffer queue last pointer */
-        spinlock_t  tx_dev_lock;
+struct tx_device {
+	struct tx_device_buffer tx_dev_buff[TX_DEVICE_BUFF_SIZE];
+	unsigned int qhead;	/* tx buffer queue first pointer */
+	unsigned int qtail;	/* tx buffer queue last pointer */
+	spinlock_t tx_dev_lock;
 };
 
 /* Rx Device struct */
@@ -141,15 +137,15 @@ struct tx_device{
 #define	RX_DEVICE_BUFF_SIZE	32
 
 struct rx_device_buffer {
-       unsigned char	data[RX_DATA_SIZE];
-       unsigned int	size;
+	unsigned char data[RX_DATA_SIZE];
+	unsigned int size;
 };
 
-struct rx_device{
-        struct rx_device_buffer rx_dev_buff[RX_DEVICE_BUFF_SIZE];
-        unsigned int	qhead; /* rx buffer queue first pointer */
-	unsigned int	qtail; /* rx buffer queue last pointer */
-	spinlock_t  rx_dev_lock;
+struct rx_device {
+	struct rx_device_buffer rx_dev_buff[RX_DEVICE_BUFF_SIZE];
+	unsigned int qhead;	/* rx buffer queue first pointer */
+	unsigned int qtail;	/* rx buffer queue last pointer */
+	spinlock_t rx_dev_lock;
 };
 #define	ROM_FILE "ks7010sd.rom"
 #define	CFG_FILE "ks79xx.cfg"
