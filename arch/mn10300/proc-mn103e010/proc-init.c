5ee98ab3a8ea94fe24e288dc02c6b489889cf06a
Arnd Bergmann <arnd@arndb.de>
2016-06-01 16:46:22 +0200

    mn10300: use RTC_DRV_CMOS instead of CONFIG_RTC
    nn10300 has a dependency on mc146818_get_time/mc146818_set_time,
    which we want to move from the mc146818rtc.h header into the
    rtc subsystem, which in turn is not usable on mn10300.
    
    This changes mn10300 to use the modern rtc-cmos driver instead
    of the old RTC driver, and that in turn lets us completely
    remove the read_persistent_clock/update_persistent_clock callbacks.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>

--- arch/mn10300/proc-mn103e010/proc-init.c
+++ arch/mn10300/proc-mn103e010/proc-init.c
@@ -9,6 +9,7 @@
  * 2 of the Licence, or (at your option) any later version.
  */
 #include <linux/kernel.h>
+#include <linux/irq.h>
 #include <asm/cacheflush.h>
 #include <asm/fpu.h>
 #include <asm/irq.h>
