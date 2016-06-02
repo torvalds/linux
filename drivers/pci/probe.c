d963f6512e15fb2c0a9e9770078e2206f55c2f7a
Mika Westerberg <mika.westerberg@linux.intel.com>
2016-06-02 11:17:13 +0300

    PCI: Power on bridges before scanning new devices
    When a PCI device is removed through sysfs interface, the upstream bridge
    (PCIe port) can be runtime suspended if it was the last device on that bus.
    Now, if the bridge is in D3 we cannot find devices below the bridge
    anymore.  For example following fails to find the removed device again:
    
      # echo 1 > /sys/bus/pci/devices/0000:00:01.0/0000:01:00.0/remove
      # echo 1 > /sys/bus/pci/devices/0000:00:01.0/rescan
    
    Where 0000:00:01.0 is the bridge device.
    
    In order to be able to rescan devices below the bridge add
    pm_runtime_get_sync()/pm_runtime_put() calls to pci_scan_bridge().  This
    should keep bridges powered on while their children devices are being
    scanned.
    
    Reported-by: Peter Wu <peter@lekensteyn.nl>
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -16,6 +16,7 @@
 #include <linux/aer.h>
 #include <linux/acpi.h>
 #include <linux/irqdomain.h>
+#include <linux/pm_runtime.h>
 #include "pci.h"
 
 #define CARDBUS_LATENCY_TIMER	176	/* secondary latency timer */
@@ -832,6 +833,12 @@ int pci_scan_bridge(struct pci_bus *bus, struct pci_dev *dev, int max, int pass)
 	u8 primary, secondary, subordinate;
 	int broken = 0;
 
+	/*
+	 * Make sure the bridge is powered on to be able to access config
+	 * space of devices below it.
+	 */
+	pm_runtime_get_sync(&dev->dev);
+
 	pci_read_config_dword(dev, PCI_PRIMARY_BUS, &buses);
 	primary = buses & 0xFF;
 	secondary = (buses >> 8) & 0xFF;
@@ -1011,6 +1018,8 @@ int pci_scan_bridge(struct pci_bus *bus, struct pci_dev *dev, int max, int pass)
 
 out:
 	pci_write_config_word(dev, PCI_BRIDGE_CONTROL, bctl);
+
+	pm_runtime_put(&dev->dev);
 
 	return max;
 }
