5151bbef1a652dda43f88d8f21c4ccef5022daf2
Jon Medhurst <tixy@linaro.org>
2016-06-02 12:18:08 +0000

    arm: Fix #if/#ifdef typo in topology.c
    Probably a typo in arch/arm/kernel/topology.c
    
    This patch fixes the warning...
    
    arch/arm/kernel/topology.c: In function 'scale_cpu_capacity':
    arch/arm/kernel/topology.c:47:5: warning: "CONFIG_CPU_FREQ" is not defined [-Wundef]
    
    Fixes: Change-Id: If5e9e0ba8ff5a5d3236b373dbce8c72ea71b5e18
           ("arm: Enable max freq invariant scheduler load-tracking and capacity support")
    Signed-off-by: Jon Medhurst <tixy@linaro.org>
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    [jstultz: Cherry-picked from android-3.18]
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- arch/arm/kernel/topology.c
+++ arch/arm/kernel/topology.c
@@ -44,7 +44,7 @@ static DEFINE_PER_CPU(unsigned long, cpu_scale);
 
 unsigned long scale_cpu_capacity(struct sched_domain *sd, int cpu)
 {
-#if CONFIG_CPU_FREQ
+#ifdef CONFIG_CPU_FREQ
 	unsigned long max_freq_scale = cpufreq_scale_max_freq_capacity(cpu);
 
 	return per_cpu(cpu_scale, cpu) * max_freq_scale >> SCHED_CAPACITY_SHIFT;
