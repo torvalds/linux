7f319975ccea80cf03377ff579b0a9e613308570
Jaegeuk Kim <jaegeuk@kernel.org>
2016-06-03 12:28:26 -0700

    f2fs: set mapping error for EIO
    If EIO occurred, we need to set all the mapping to avoid any further IOs.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1243,7 +1243,7 @@ write:
 
 	/* we should bypass data pages to proceed the kworkder jobs */
 	if (unlikely(f2fs_cp_error(sbi))) {
-		SetPageError(page);
+		mapping_set_error(page->mapping, -EIO);
 		goto out;
 	}
 
