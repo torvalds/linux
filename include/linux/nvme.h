14e974a84e831bf9a44495c7256a6846e7f77630
Christoph Hellwig <hch@lst.de>
2016-06-06 23:20:43 +0200

    nvme.h: add RTD3R, RTD3E and OAES fields
    These have been added in NVMe 1.2 and we'll need at least oaes for the
    NVMe target driver.
    
    Reviewed-by: Sagi Grimberg <sagi@grimberg.me>
    Reviewed-by: Jay Freyensee <james.p.freyensee@intel.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Keith Busch <keith.busch@intel.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- include/linux/nvme.h
+++ include/linux/nvme.h
@@ -107,7 +107,10 @@ struct nvme_id_ctrl {
 	__u8			mdts;
 	__le16			cntlid;
 	__le32			ver;
-	__u8			rsvd84[172];
+	__le32			rtd3r;
+	__le32			rtd3e;
+	__le32			oaes;
+	__u8			rsvd96[160];
 	__le16			oacs;
 	__u8			acl;
 	__u8			aerl;
