c8841c837800f1f9e94cdecffa62a8bf4dc336c8
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2016-06-06 16:53:32 +0200

    UPSTREAM: drm/rockchip: Use atomic PM helpers
    This driver was still using the old legacy helpers and that caused a few
    NULL dereferences when trying to call empty callbacks.
    
    Change-Id: I9656aed34892260dbf9b571b95befd6af4d9b70f
    Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Cc: Caesar Wang <wxt@rock-chips.com>
    Cc: Douglas Anderson <dianders@chromium.org>
    Cc: Heiko Stuebner <heiko@sntech.de>
    Cc: Yakir Yang <ykk@rock-chips.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1465224813-7359-1-git-send-email-tomeu.vizoso@collabora.com
    Signed-off-by: Mark Yao <mark.yao@rock-chips.com>
    (cherry picked from 5a5873830972e7d8983dd205b3686f728047d556)

--- drivers/gpu/drm/rockchip/rockchip_drm_drv.h
+++ drivers/gpu/drm/rockchip/rockchip_drm_drv.h
@@ -93,6 +93,7 @@ struct rockchip_drm_private {
 	struct drm_fb_helper fbdev_helper;
 	struct drm_gem_object *fbdev_bo;
 	const struct rockchip_crtc_funcs *crtc_funcs[ROCKCHIP_MAX_CRTC];
+	struct drm_atomic_state *state;
 
 	struct rockchip_atomic_commit commit;
 	struct iommu_domain *domain;
