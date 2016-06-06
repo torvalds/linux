19163ac3123e7fef8b1ecb2f1d4223f58ed5e884
Zhao Qiang <qiang.zhao@nxp.com>
2016-06-06 14:30:00 +0800

    fsl/qe: Make regs resouce_size_t
    Signed-off-by: Zhao Qiang <qiang.zhao@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/soc/fsl/qe/ucc_fast.h
+++ include/soc/fsl/qe/ucc_fast.h
@@ -123,7 +123,7 @@ struct ucc_fast_info {
 	enum qe_clock tx_clock;
 	enum qe_clock rx_sync;
 	enum qe_clock tx_sync;
-	u32 regs;
+	resource_size_t regs;
 	int irq;
 	u32 uccm_mask;
 	int bd_mem_part;
