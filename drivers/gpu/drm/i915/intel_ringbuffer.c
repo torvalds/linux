e587f6cb0af140f3c0ea794d8616eb9a29969983
Mika Kuoppala <mika.kuoppala@linux.intel.com>
2016-06-07 17:18:59 +0300

    drm/i915/kbl: Add WaEnableGapsTsvCreditFix
    We need this crucial workaround from skl also to all kbl revisions.
    Lack of it was causing system hangs on skl enabling so this is
    a must have.
    
    v2: Don't add revid checks to gen9 init workarounds (Arun)
    
    References: HSD#2126660
    Cc: Arun Siluvery <arun.siluvery@linux.intel.com>
    Signed-off-by: Mika Kuoppala <mika.kuoppala@intel.com>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1465309159-30531-8-git-send-email-mika.kuoppala@intel.com

--- drivers/gpu/drm/i915/intel_ringbuffer.c
+++ drivers/gpu/drm/i915/intel_ringbuffer.c
@@ -1188,11 +1188,16 @@ static int bxt_init_workarounds(struct intel_engine_cs *engine)
 
 static int kbl_init_workarounds(struct intel_engine_cs *engine)
 {
+	struct drm_i915_private *dev_priv = engine->i915;
 	int ret;
 
 	ret = gen9_init_workarounds(engine);
 	if (ret)
 		return ret;
+
+	/* WaEnableGapsTsvCreditFix:kbl */
+	I915_WRITE(GEN8_GARBCNTL, (I915_READ(GEN8_GARBCNTL) |
+				   GEN9_GAPS_TSV_CREDIT_DISABLE));
 
 	return 0;
 }
