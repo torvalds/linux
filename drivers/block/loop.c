7a6497378ae832daa1668a10be04ebdacd60468b
Minfei Huang <mnghuan@gmail.com>
2016-06-07 10:05:15 +0800

    loop: Make user notify for adding loop device failed
    There is no error number returned if loop driver fails in function
    alloc_disk to add new loop device. Add a correct error number to make
    user notify in this case.
    
    Signed-off-by: Minfei Huang <mnghuan@gmail.com>
    Reviewed-by: Ming Lei <tom.leiming@gmail.com>
    Reviewed-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -1765,6 +1765,7 @@ static int loop_add(struct loop_device **l, int i)
 	 */
 	queue_flag_set_unlocked(QUEUE_FLAG_NOMERGES, lo->lo_queue);
 
+	err = -ENOMEM;
 	disk = lo->lo_disk = alloc_disk(1 << part_shift);
 	if (!disk)
 		goto out_free_queue;
