452fa86e98061ebd528dd79e22befd5f87c83269
Bhaktipriya Shridhar <bhaktipriya96@gmail.com>
2016-06-08 01:38:53 +0530

    libertas_tf: Remove create_workqueue
    alloc_workqueue replaces deprecated create_workqueue().
    
    A dedicated workqueue has been used since the workitem (viz
    &priv->cmd_work per priv, which maps to lbtf_cmd_work) is involved in
    actual command processing and may be used on a memory reclaim path.
    The workitems require forward progress under memory pressure and hence,
    WQ_MEM_RECLAIM has been set. Since there are only a fixed number of work
    items, explicit concurrency limit is unnecessary here.
    
    Signed-off-by: Bhaktipriya Shridhar <bhaktipriya96@gmail.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/marvell/libertas_tf/main.c
+++ drivers/net/wireless/marvell/libertas_tf/main.c
@@ -735,7 +735,7 @@ EXPORT_SYMBOL_GPL(lbtf_bcn_sent);
 static int __init lbtf_init_module(void)
 {
 	lbtf_deb_enter(LBTF_DEB_MAIN);
-	lbtf_wq = create_workqueue("libertastf");
+	lbtf_wq = alloc_workqueue("libertastf", WQ_MEM_RECLAIM, 0);
 	if (lbtf_wq == NULL) {
 		printk(KERN_ERR "libertastf: couldn't create workqueue\n");
 		return -ENOMEM;
