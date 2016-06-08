8913f8d2930368f30998e60851259606eeed2c49
Jan Glauber <jglauber@cavium.com>
2016-06-08 08:51:19 +0200

    i2c: octeon: Avoid printk after too long SMBUS message
    Remove the warning about a too long SMBUS message because
    the ipmi_ssif driver triggers this warning too frequently so it
    spams the message log.
    
    Signed-off-by: Jan Glauber <jglauber@cavium.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/busses/i2c-octeon.c
+++ drivers/i2c/busses/i2c-octeon.c
@@ -957,12 +957,8 @@ static int octeon_i2c_read(struct octeon_i2c *i2c, int target,
 
 		data[i] = octeon_i2c_data_read(i2c);
 		if (recv_len && i == 0) {
-			if (data[i] > I2C_SMBUS_BLOCK_MAX + 1) {
-				dev_err(i2c->dev,
-					"%s: read len > I2C_SMBUS_BLOCK_MAX %d\n",
-					__func__, data[i]);
+			if (data[i] > I2C_SMBUS_BLOCK_MAX + 1)
 				return -EPROTO;
-			}
 			length += data[i];
 		}
 
