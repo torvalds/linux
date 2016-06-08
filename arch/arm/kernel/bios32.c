313cb90285d8715302d79069700d78770ee91e9a
Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
2016-06-08 12:04:50 +0100

    ARM/PCI: Remove arch-specific pcibios_enable_device()
    On systems with PCI_PROBE_ONLY set, we rely on BAR assignments from
    firmware.  Previously we did not insert those resources into the resource
    tree, so we had to skip pci_enable_resources() because it fails if
    resources are not in the resource tree.
    
    Now that we *do* insert resources even when PCI_PROBE_ONLY is set, we no
    longer need the ARM-specific pcibios_enable_device().  Remove it so we
    use the generic version.
    
    [bhelgaas: changelog]
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Will Deacon <will.deacon@arm.com>
    CC: Arnd Bergmann <arnd@arndb.de>
    CC: Russell King <linux@arm.linux.org.uk>

--- arch/arm/kernel/bios32.c
+++ arch/arm/kernel/bios32.c
@@ -588,18 +588,6 @@ resource_size_t pcibios_align_resource(void *data, const struct resource *res,
 	return start;
 }
 
-/**
- * pcibios_enable_device - Enable I/O and memory.
- * @dev: PCI device to be enabled
- */
-int pcibios_enable_device(struct pci_dev *dev, int mask)
-{
-	if (pci_has_flag(PCI_PROBE_ONLY))
-		return 0;
-
-	return pci_enable_resources(dev, mask);
-}
-
 int pci_mmap_page_range(struct pci_dev *dev, struct vm_area_struct *vma,
 			enum pci_mmap_state mmap_state, int write_combine)
 {
