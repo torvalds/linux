19de659cb7216eb1c04889bd1a248593f296e19f
Lukas Wunner <lukas@wunner.de>
2016-06-08 18:47:27 +0200

    drm/radeon: Don't leak runtime pm ref on driver unload
    radeon_driver_load_kms() calls pm_runtime_put_autosuspend() if
    radeon_is_px(dev), but radeon_driver_unload_kms() calls
    pm_runtime_get_sync() unconditionally. We therefore leak a runtime pm
    ref whenever radeon is unloaded on a non-PX machine or if runpm=0. The
    GPU will subsequently never runtime suspend after loading radeon again.
    
    Fix by taking the runtime pm ref under the same condition that it was
    released on driver load.
    
    Fixes: 10ebc0bc0934 ("drm/radeon: add runtime PM support (v2)")
    Cc: Dave Airlie <airlied@redhat.com>
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/aaf71106c042126817aeca8b8e54ed468ab61ef7.1465392124.git.lukas@wunner.de

--- drivers/gpu/drm/radeon/radeon_kms.c
+++ drivers/gpu/drm/radeon/radeon_kms.c
@@ -63,7 +63,9 @@ int radeon_driver_unload_kms(struct drm_device *dev)
 	if (rdev->rmmio == NULL)
 		goto done_free;
 
-	pm_runtime_get_sync(dev->dev);
+	if (radeon_is_px(dev)) {
+		pm_runtime_get_sync(dev->dev);
+	}
 
 	radeon_kfd_device_fini(rdev);
 
