5cce438298a0d2a7a857a4a3c3e26aeb8f77b941
Alan Stern <stern@rowland.harvard.edu>
2016-06-10 14:42:55 -0400

    USB: remove race condition in usbfs/libusb when using reap-after-disconnect
    Hans de Goede has reported a difficulty in the Linux port of libusb.
    When a device is removed, the poll() system call in usbfs starts
    returning POLLERR as soon as udev->state is set to
    USB_STATE_NOTATTACHED, but the outstanding URBs are not available for
    reaping until some time later (after usbdev_remove() has been called).
    This is awkward for libusb or other usbfs clients, although not an
    insuperable problem.
    
    At any rate, it's easy to change usbfs so that it returns POLLHUP as
    soon as the state becomes USB_STATE_NOTATTACHED but it doesn't return
    POLLERR until after the outstanding URBs have completed.  That's what
    this patch does; it uses the fact that ps->list is always on the
    dev->filelist list until usbdev_remove() takes it off, which happens
    after all the outstanding URBs have been cancelled.
    
    Signed-off-by: Alan Stern <stern@rowland.harvard.edu>
    Reported-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/devio.c
+++ drivers/usb/core/devio.c
@@ -2583,7 +2583,9 @@ static unsigned int usbdev_poll(struct file *file,
 	if (file->f_mode & FMODE_WRITE && !list_empty(&ps->async_completed))
 		mask |= POLLOUT | POLLWRNORM;
 	if (!connected(ps))
-		mask |= POLLERR | POLLHUP;
+		mask |= POLLHUP;
+	if (list_empty(&ps->list))
+		mask |= POLLERR;
 	return mask;
 }
 
