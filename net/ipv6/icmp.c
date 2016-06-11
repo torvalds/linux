a270309c905338503f94a104178a3175f75f57c6
Hannes Frederic Sowa <hannes@stressinduktion.org>
2016-06-11 20:32:06 +0200

    ipv6: fix endianness error in icmpv6_err
    IPv6 ping socket error handler doesn't correctly convert the new 32 bit
    mtu to host endianness before using.
    
    [Cherry-pick of net dcb94b88c09ce82a80e188d49bcffdc83ba215a6]
    
    Bug: 29370996
    Change-Id: Iea0ca79f16c2a1366d82b3b0a3097093d18da8b7
    Cc: Lorenzo Colitti <lorenzo@google.com>
    Fixes: 6d0bfe22611602f ("net: ipv6: Add IPv6 support to the ping socket.")
    Signed-off-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Acked-by: Lorenzo Colitti <lorenzo@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/icmp.c
+++ net/ipv6/icmp.c
@@ -98,7 +98,7 @@ static void icmpv6_err(struct sk_buff *skb, struct inet6_skb_parm *opt,
 
 	if (!(type & ICMPV6_INFOMSG_MASK))
 		if (icmp6->icmp6_type == ICMPV6_ECHO_REQUEST)
-			ping_err(skb, offset, info);
+			ping_err(skb, offset, ntohl(info));
 }
 
 static int icmpv6_rcv(struct sk_buff *skb);
