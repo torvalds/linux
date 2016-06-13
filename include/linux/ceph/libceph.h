a0f2b65275413b3438e9f55b1427273cd893c3b2
Arnd Bergmann <arnd@arndb.de>
2016-06-13 15:04:56 +0200

    ceph: fix symbol versioning for ceph_monc_do_statfs
    The genksyms helper in the kernel cannot parse a type definition
    like "typeof(((type *)0)->keyfld)" that is used in the DEFINE_RB_FUNCS
    helper, causing the following EXPORT_SYMBOL() statement to be ignored
    when computing the crcs, and triggering a warning about this:
    
    WARNING: "ceph_monc_do_statfs" [fs/ceph/ceph.ko] has no CRC
    
    To work around the problem, we can rewrite the type to reference
    an undefined 'extern' symbol instead of a NULL pointer. This is
    evidently ok for genksyms, and it no longer complains about the
    line when calling it with 'genksyms -w'.
    
    I've looked briefly into extending genksyms instead, but it seems
    really hard to do. Jan Beulich introduced basic support for 'typeof'
    a while ago in dc53324060f3 ("genksyms: fix typeof() handling"),
    but that is not sufficient for the expression we have here.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Fixes: fcd00b68bbe2 ("libceph: DEFINE_RB_FUNCS macro")
    Cc: Jan Beulich <jbeulich@suse.com>
    Cc: Michal Marek <mmarek@suse.cz>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- include/linux/ceph/libceph.h
+++ include/linux/ceph/libceph.h
@@ -215,8 +215,9 @@ static void erase_##name(struct rb_root *root, type *t)			\
 }
 
 #define DEFINE_RB_LOOKUP_FUNC(name, type, keyfld, nodefld)		\
+extern type __lookup_##name##_key;					\
 static type *lookup_##name(struct rb_root *root,			\
-			   typeof(((type *)0)->keyfld) key)		\
+			   typeof(__lookup_##name##_key.keyfld) key)	\
 {									\
 	struct rb_node *n = root->rb_node;				\
 									\
