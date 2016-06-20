da1410be21bfedd16740aee6d772e669cf4e852f
Baolin Wang <baolin.wang@linaro.org>
2016-06-20 16:19:48 +0800

    usb: dwc3: gadget: Add the suspend state checking when stopping gadget
    It will be crash to stop gadget when the dwc3 device had been into suspend
    state, thus we need to check if the dwc3 device had been into suspend state
    when UDC try to stop gadget.
    
    Signed-off-by: Baolin Wang <baolin.wang@linaro.org>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1803,6 +1803,9 @@ err0:
 
 static void __dwc3_gadget_stop(struct dwc3 *dwc)
 {
+	if (pm_runtime_suspended(dwc->dev))
+		return;
+
 	dwc3_gadget_disable_irq(dwc);
 	__dwc3_gadget_ep_disable(dwc->eps[0]);
 	__dwc3_gadget_ep_disable(dwc->eps[1]);
