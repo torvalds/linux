a3dff71c1c88fc184a1ae5e425ba621d547d16ec
Kees Cook <keescook@chromium.org>
2016-06-26 08:46:23 -0700

    lkdtm: split usercopy tests to separate file
    This splits the USERCOPY_* tests into the new lkdtm_usercopy.c file to
    help separate things better for readability.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- drivers/misc/lkdtm.h
+++ drivers/misc/lkdtm.h
@@ -1,6 +1,19 @@
 #ifndef __LKDTM_H
 #define __LKDTM_H
 
+/* lkdtm_rodata.c */
 void lkdtm_rodata_do_nothing(void);
+
+/* lkdtm_usercopy.c */
+void __init lkdtm_usercopy_init(void);
+void __exit lkdtm_usercopy_exit(void);
+void lkdtm_USERCOPY_HEAP_SIZE_TO(void);
+void lkdtm_USERCOPY_HEAP_SIZE_FROM(void);
+void lkdtm_USERCOPY_HEAP_FLAG_TO(void);
+void lkdtm_USERCOPY_HEAP_FLAG_FROM(void);
+void lkdtm_USERCOPY_STACK_FRAME_TO(void);
+void lkdtm_USERCOPY_STACK_FRAME_FROM(void);
+void lkdtm_USERCOPY_STACK_BEYOND(void);
+void lkdtm_USERCOPY_KERNEL(void);
 
 #endif
