ca857cc1d4cf17aba4bbb3b95d35454ad96924b3
Trond Myklebust <trond.myklebust@primarydata.com>
2016-06-28 13:54:09 -0400

    NFS/pnfs: Do not clobber existing pgio_done_cb in nfs4_proc_read_setup
    If a pNFS client sets hdr->pgio_done_cb, then we should not overwrite that
    in nfs4_proc_read_setup()
    
    Fixes: 75bf47ebf6b5 ("pNFS/flexfile: Fix erroneous fall back to...")
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -4392,7 +4392,8 @@ static void nfs4_proc_read_setup(struct nfs_pgio_header *hdr,
 				 struct rpc_message *msg)
 {
 	hdr->timestamp   = jiffies;
-	hdr->pgio_done_cb = nfs4_read_done_cb;
+	if (!hdr->pgio_done_cb)
+		hdr->pgio_done_cb = nfs4_read_done_cb;
 	msg->rpc_proc = &nfs4_procedures[NFSPROC4_CLNT_READ];
 	nfs4_init_sequence(&hdr->args.seq_args, &hdr->res.seq_res, 0);
 }
