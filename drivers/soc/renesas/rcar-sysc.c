1023578ec38ec1bae085917134ba985da755503e
Geert Uytterhoeven <geert+renesas@glider.be>
2016-06-28 16:10:31 +0200

    soc: renesas: rcar-sysc: Fix uninitialized error code in rcar_sysc_pd_init()
    On success, rcar_sysc_pd_init() returns an uninitialized error code.
    Use the return value of of_genpd_add_provider_onecell() to fix this.
    
    This went unnoticed, as early_initcall() doesn't care about the return
    value.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- drivers/soc/renesas/rcar-sysc.c
+++ drivers/soc/renesas/rcar-sysc.c
@@ -398,7 +398,7 @@ static int __init rcar_sysc_pd_init(void)
 		domains->domains[area->isr_bit] = &pd->genpd;
 	}
 
-	of_genpd_add_provider_onecell(np, &domains->onecell_data);
+	error = of_genpd_add_provider_onecell(np, &domains->onecell_data);
 
 out_put:
 	of_node_put(np);
