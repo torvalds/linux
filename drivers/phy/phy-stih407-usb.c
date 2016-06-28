9278e707f4e187df2b4d9eeb2bc78a1724fbe4ac
Lee Jones <lee.jones@linaro.org>
2016-06-28 09:32:12 +0100

    phy: phy-stih407-usb: Inform the reset framework that our reset line may be shared
    On the STiH410 B2120 development board the ports on the Generic PHY
    share their reset lines with each other.  New functionality in the
    reset subsystems forces consumers to be explicit when requesting
    shared/exclusive reset lines.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/phy/phy-stih407-usb.c
+++ drivers/phy/phy-stih407-usb.c
@@ -105,7 +105,7 @@ static int stih407_usb2_picophy_probe(struct platform_device *pdev)
 	phy_dev->dev = dev;
 	dev_set_drvdata(dev, phy_dev);
 
-	phy_dev->rstc = devm_reset_control_get(dev, "global");
+	phy_dev->rstc = devm_reset_control_get_shared(dev, "global");
 	if (IS_ERR(phy_dev->rstc)) {
 		dev_err(dev, "failed to ctrl picoPHY reset\n");
 		return PTR_ERR(phy_dev->rstc);
