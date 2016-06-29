a4a551b8f1d4c4ebffd0f49dfef44df3128546f8
Namhyung Kim <namhyung@kernel.org>
2016-06-29 19:56:48 +0900

    ftrace: Reduce size of function graph entries
    Currently ftrace_graph_ent{,_entry} and ftrace_graph_ret{,_entry} struct
    can have padding bytes at the end due to alignment in 64-bit data type.
    As these data are recorded so frequently, those paddings waste
    non-negligible space.  As the ring buffer maintains alignment properly
    for each architecture, just to remove the extra padding using 'packed'
    attribute.
    
      ftrace_graph_ent_entry:  24 -> 20
      ftrace_graph_ret_entry:  48 -> 44
    
    Also I moved the 'overrun' field in struct ftrace_graph_ret to minimize
    the padding in the middle.
    
    Tested on x86_64 only.
    
    Link: http://lkml.kernel.org/r/1467197808-13578-1-git-send-email-namhyung@kernel.org
    
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: linux-arch@vger.kernel.org
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/trace/trace.h
+++ kernel/trace/trace.h
@@ -80,6 +80,12 @@ enum trace_type {
 	FTRACE_ENTRY(name, struct_name, id, PARAMS(tstruct), PARAMS(print), \
 		     filter)
 
+#undef FTRACE_ENTRY_PACKED
+#define FTRACE_ENTRY_PACKED(name, struct_name, id, tstruct, print,	\
+			    filter)					\
+	FTRACE_ENTRY(name, struct_name, id, PARAMS(tstruct), PARAMS(print), \
+		     filter) __packed
+
 #include "trace_entries.h"
 
 /*
@@ -1625,6 +1631,11 @@ int set_tracer_flag(struct trace_array *tr, unsigned int mask, int enabled);
 #define FTRACE_ENTRY_DUP(call, struct_name, id, tstruct, print, filter)	\
 	FTRACE_ENTRY(call, struct_name, id, PARAMS(tstruct), PARAMS(print), \
 		     filter)
+#undef FTRACE_ENTRY_PACKED
+#define FTRACE_ENTRY_PACKED(call, struct_name, id, tstruct, print, filter) \
+	FTRACE_ENTRY(call, struct_name, id, PARAMS(tstruct), PARAMS(print), \
+		     filter)
+
 #include "trace_entries.h"
 
 #if defined(CONFIG_PERF_EVENTS) && defined(CONFIG_FUNCTION_TRACER)
