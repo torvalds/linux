b74d6e740be36d6d0c05f3386520fb44bae886b3
Amitkumar Karwar <akarwar@marvell.com>
2016-06-29 17:32:40 +0530

    mwifiex: fix scan_block flag handling
    scan_block flag is used to block scan operation when 4 way handshake
    is in progress. Sometimes it doesn't get cleared due to incomplete
    association. An example is assoc request/response is done, but add key
    operation get canceled in some corner cases. As a result, further
    association/scan operations are blocked.
    
    This patch fixes the problem by clearing scan_block flag.
    
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Xinming Hu <huxm@marvell.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/marvell/mwifiex/cfg80211.c
+++ drivers/net/wireless/marvell/mwifiex/cfg80211.c
@@ -2250,6 +2250,9 @@ mwifiex_cfg80211_connect(struct wiphy *wiphy, struct net_device *dev,
 		return -EALREADY;
 	}
 
+	if (priv->scan_block)
+		priv->scan_block = false;
+
 	if (adapter->surprise_removed || adapter->is_cmd_timedout) {
 		mwifiex_dbg(adapter, ERROR,
 			    "%s: Ignore connection.\t"
@@ -2467,6 +2470,9 @@ mwifiex_cfg80211_scan(struct wiphy *wiphy,
 			    "cmd: Scan already in process..\n");
 		return -EBUSY;
 	}
+
+	if (!priv->wdev.current_bss && priv->scan_block)
+		priv->scan_block = false;
 
 	if (!mwifiex_stop_bg_scan(priv))
 		cfg80211_sched_scan_stopped_rtnl(priv->wdev.wiphy);
