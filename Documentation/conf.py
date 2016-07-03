bc21467114b03ce47317cce9a49f0a46cdde50ca
Markus Heiser <markus.heiser@darmarIT.de>
2016-07-03 10:05:28 +0200

    doc-rst: boilerplate HTML theme customization
    Implements the minimal boilerplate for Sphinx HTML theme customization.
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarIT.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -176,7 +176,14 @@ except ImportError:
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+
+html_static_path = ['sphinx-static']
+
+html_context = {
+    'css_files': [
+        '_static/theme_overrides.css',
+    ],
+}
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
