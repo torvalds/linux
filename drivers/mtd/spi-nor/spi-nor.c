cebc1fd0690713ec86ab27e606daf9967a2833ab
P L Sai Krishna <lakshmi.sai.krishna.potthuri@xilinx.com>
2016-07-08 19:16:55 +0530

    mtd: spi-nor: Added support for n25q00a.
    Add Micron (n25q00a) 1Gbit NOR Flash in the list of supported
    devices.
    This part is different from n25q00 in Memory Type.
    Memory Type for n25q00 - BAh
    Memory Type for n25q00a - BBh
    
    Signed-off-by: P L Sai Krishna <lakshmis@xilinx.com>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -871,6 +871,7 @@ static const struct flash_info spi_nor_ids[] = {
 	{ "n25q512a",    INFO(0x20bb20, 0, 64 * 1024, 1024, SECT_4K | USE_FSR | SPI_NOR_QUAD_READ) },
 	{ "n25q512ax3",  INFO(0x20ba20, 0, 64 * 1024, 1024, SECT_4K | USE_FSR | SPI_NOR_QUAD_READ) },
 	{ "n25q00",      INFO(0x20ba21, 0, 64 * 1024, 2048, SECT_4K | USE_FSR | SPI_NOR_QUAD_READ) },
+	{ "n25q00a",     INFO(0x20bb21, 0, 64 * 1024, 2048, SECT_4K | USE_FSR | SPI_NOR_QUAD_READ) },
 
 	/* PMC */
 	{ "pm25lv512",   INFO(0,        0, 32 * 1024,    2, SECT_4K_PMC) },
