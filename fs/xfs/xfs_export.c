15d66ac20927e00ac0e65af6707eff60b92e61b8
Benjamin Coddington <bcodding@redhat.com>
2016-07-08 09:53:20 -0400

    xfs: abstract block export operations from nfsd layouts
    Instead of creeping pnfs layout configuration into filesystems, move the
    definition of block-based export operations under a more abstract
    configuration.
    
    Signed-off-by: Benjamin Coddington <bcodding@redhat.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Dave Chinner <david@fromorbit.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/xfs/xfs_export.c
+++ fs/xfs/xfs_export.c
@@ -246,7 +246,7 @@ const struct export_operations xfs_export_operations = {
 	.fh_to_parent		= xfs_fs_fh_to_parent,
 	.get_parent		= xfs_fs_get_parent,
 	.commit_metadata	= xfs_fs_nfs_commit_metadata,
-#ifdef CONFIG_NFSD_BLOCKLAYOUT
+#ifdef CONFIG_EXPORTFS_BLOCK_OPS
 	.get_uuid		= xfs_fs_get_uuid,
 	.map_blocks		= xfs_fs_map_blocks,
 	.commit_blocks		= xfs_fs_commit_blocks,
