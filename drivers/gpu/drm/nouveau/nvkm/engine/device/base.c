4cb53a5ed1d0e662e9aab73fbf2097ead5cad563
Ben Skeggs <bskeggs@redhat.com>
2016-07-09 10:41:01 +1000

    drm/nouveau/mmu/gp100: initial support
    GP100 still supports the previous generations' page table layout, which
    we will temporarily make use of here.
    
    Proper support for the new MMU layout requires some rework to the common
    MMU code, which is in progress.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
@@ -2156,6 +2156,7 @@ nv130_chipset = {
 	.fb = gp100_fb_new,
 	.imem = nv50_instmem_new,
 	.mc = gp100_mc_new,
+	.mmu = gf100_mmu_new,
 	.pci = gp100_pci_new,
 	.timer = gk20a_timer_new,
 	.top = gk104_top_new,
