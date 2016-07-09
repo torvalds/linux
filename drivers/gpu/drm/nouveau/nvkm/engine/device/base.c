13a66d2f9d86fc002322767acc4a955f8bc40a71
Ben Skeggs <bskeggs@redhat.com>
2016-07-09 10:41:01 +1000

    drm/nouveau/i2c/gp104: initial support
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
@@ -2191,6 +2191,7 @@ nv134_chipset = {
 	.fb = gp104_fb_new,
 	.fuse = gm107_fuse_new,
 	.gpio = gk104_gpio_new,
+	.i2c = gm200_i2c_new,
 	.imem = nv50_instmem_new,
 	.mc = gp100_mc_new,
 	.mmu = gf100_mmu_new,
