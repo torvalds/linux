efba142bcd980e08ed5e2b98b23b103697d9aa6c
Jon Hunter <jonathanh@nvidia.com>
2016-07-12 14:53:36 +0100

    mmc: sdhci: Request regulators before reading capabilities
    The capabilities of the SDHCI host controller are read early during the
    SDHCI host initialisation in sdhci_setup_host() and before any
    regulators for the host have been requested. This means that if the host
    supports some high-speed modes (according to its capabilities register),
    but the board cannot because the appropriate voltage regulator is not
    available, then the host cannot easily override the capabilities that
    are supported.
    
    To allow a SDHCI host controller to determine if it can support UHS high
    speed modes via the presence of the MMC regulators, request the
    regulators before reading the capabilities of the host controller. This
    will allow the SDHCI host to use the 'reset' callback to take the
    appropriate action (set flags, configure registers, etc) before the
    capabilities register(s) are read.
    
    Please note that some SDHCI hosts, such as the Tegra SDHCI host, has
    the ability to mask bits in the capabilities register to prevent
    certain capabilities from being advertised.
    
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -3027,6 +3027,16 @@ int sdhci_setup_host(struct sdhci_host *host)
 
 	mmc = host->mmc;
 
+	/*
+	 * If there are external regulators, get them. Note this must be done
+	 * early before resetting the host and reading the capabilities so that
+	 * the host can take the appropriate action if regulators are not
+	 * available.
+	 */
+	ret = mmc_regulator_get_supply(mmc);
+	if (ret == -EPROBE_DEFER)
+		return ret;
+
 	sdhci_read_caps(host);
 
 	override_timeout_clk = host->timeout_clk;
@@ -3258,11 +3268,6 @@ int sdhci_setup_host(struct sdhci_host *host)
 	    mmc_card_is_removable(mmc) &&
 	    mmc_gpio_get_cd(host->mmc) < 0)
 		mmc->caps |= MMC_CAP_NEEDS_POLL;
-
-	/* If there are external regulators, get them */
-	ret = mmc_regulator_get_supply(mmc);
-	if (ret == -EPROBE_DEFER)
-		goto undma;
 
 	/* If vqmmc regulator and no 1.8V signalling, then there's no UHS */
 	if (!IS_ERR(mmc->supply.vqmmc)) {
