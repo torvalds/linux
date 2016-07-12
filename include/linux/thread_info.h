fdb92b0de361f9043f359a1de52e2bedd9da4599
Kees Cook <keescook@chromium.org>
2016-07-12 16:19:48 -0700

    mm: Implement stack frame object validation
    This creates per-architecture function arch_within_stack_frames() that
    should validate if a given object is contained by a kernel stack frame.
    Initial implementation is on x86.
    
    This is based on code from PaX.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    (cherry picked from commit 0f60a8efe4005ab5e65ce000724b04d4ca04a199)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>
    
    Conflicts:
    	skip EBPF_JIT in arch/x86/Kconfig

--- include/linux/thread_info.h
+++ include/linux/thread_info.h
@@ -145,6 +145,15 @@ static inline bool test_and_clear_restore_sigmask(void)
 #error "no set_restore_sigmask() provided and default one won't work"
 #endif
 
+#ifndef CONFIG_HAVE_ARCH_WITHIN_STACK_FRAMES
+static inline int arch_within_stack_frames(const void * const stack,
+					   const void * const stackend,
+					   const void *obj, unsigned long len)
+{
+	return 0;
+}
+#endif
+
 #endif	/* __KERNEL__ */
 
 #endif /* _LINUX_THREAD_INFO_H */
