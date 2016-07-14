1f3b0f8243cb934307f59bd4d8e43b868e61d4d9
Gaurav Jindal <Gaurav.Jindal@spreadtrum.com>
2016-07-14 12:04:20 +0000

    tick/nohz: Optimize nohz idle enter
    tick_nohz_start_idle is called before checking whether the idle tick can be
    stopped. If the tick cannot be stopped, calling tick_nohz_start_idle() is
    pointless and just wasting CPU cycles.
    
    Only invoke tick_nohz_start_idle() when can_stop_idle_tick() returns true. A
    short one minute observation of the effect on ARM64 shows a reduction of calls
    by 1.5% thus optimizing the idle entry sequence.
    
    [tglx: Massaged changelog ]
    
    Co-developed-by: Sanjeev Yadav<sanjeev.yadav@spreadtrum.com>
    Signed-off-by: Gaurav Jindal<gaurav.jindal@spreadtrum.com>
    Link: http://lkml.kernel.org/r/20160714120416.GB21099@gaurav.jindal@spreadtrum.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -908,11 +908,10 @@ static void __tick_nohz_idle_enter(struct tick_sched *ts)
 	ktime_t now, expires;
 	int cpu = smp_processor_id();
 
-	now = tick_nohz_start_idle(ts);
-
 	if (can_stop_idle_tick(cpu, ts)) {
 		int was_stopped = ts->tick_stopped;
 
+		now = tick_nohz_start_idle(ts);
 		ts->idle_calls++;
 
 		expires = tick_nohz_stop_sched_tick(ts, now, cpu);
