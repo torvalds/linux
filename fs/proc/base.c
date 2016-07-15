f3758151ca099b3988de8c1cb12f0716a5cb99b4
John Stultz <john.stultz@linaro.org>
2016-07-14 17:22:19 -0700

    FROMLIST: proc: Add LSM hook checks to /proc/<tid>/timerslack_ns
    As requested, this patch checks the existing LSM hooks
    task_getscheduler/task_setscheduler when reading or modifying
    the task's timerslack value.
    
    Previous versions added new get/settimerslack LSM hooks, but
    since they checked the same PROCESS__SET/GETSCHED values as
    existing hooks, it was suggested we just use the existing ones.
    
    Cc: Kees Cook <keescook@chromium.org>
    Cc: "Serge E. Hallyn" <serge@hallyn.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    CC: Arjan van de Ven <arjan@linux.intel.com>
    Cc: Oren Laadan <orenl@cellrox.com>
    Cc: Ruchi Kandoi <kandoiruchi@google.com>
    Cc: Rom Lemarchand <romlem@android.com>
    Cc: Todd Kjos <tkjos@google.com>
    Cc: Colin Cross <ccross@android.com>
    Cc: Nick Kralevich <nnk@google.com>
    Cc: Dmitry Shmidt <dimitrysh@google.com>
    Cc: Elliott Hughes <enh@google.com>
    Cc: James Morris <jmorris@namei.org>
    Cc: Android Kernel Team <kernel-team@android.com>
    Cc: linux-security-module@vger.kernel.org
    Cc: selinux@tycho.nsa.gov
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    
    FROMLIST URL: https://lkml.org/lkml/2016/7/21/523
    Change-Id: Id157d10e2fe0b85f1be45035a6117358a42af028
    (Cherry picked back to common/android-4.4)
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -2266,6 +2266,12 @@ static ssize_t timerslack_ns_write(struct file *file, const char __user *buf,
 		goto out;
 	}
 
+	err = security_task_setscheduler(p);
+	if (err) {
+		count = err;
+		goto out;
+	}
+
 	task_lock(p);
 	if (slack_ns == 0)
 		p->timer_slack_ns = p->default_timer_slack_ns;
@@ -2293,6 +2299,10 @@ static int timerslack_ns_show(struct seq_file *m, void *v)
 		err = -EPERM;
 		goto out;
 	}
+
+	err = security_task_getscheduler(p);
+	if (err)
+		goto out;
 
 	task_lock(p);
 	seq_printf(m, "%llu\n", p->timer_slack_ns);
