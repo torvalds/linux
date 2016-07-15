ba3904ee86cb7072c2435883421b165dc1684bce
Kangjie Lu <kangjielu@gmail.com>
2016-07-15 15:08:18 -0400

    USB: usbfs: fix potential infoleak in devio
    commit 681fef8380eb818c0b845fca5d2ab1dcbab114ee upstream.
    
    The stack object "ci" has a total size of 8 bytes. Its last 3 bytes
    are padding bytes which are not initialized and leaked to userland
    via "copy_to_user".
    
    CVE-2016-4482
    
    Signed-off-by: Kangjie Lu <kjlu@gatech.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [ciwillia@brocade.com: backported to 3.10: adjusted context]
    Signed-off-by: Charles (Chas) Williams <ciwillia@brocade.com>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- drivers/usb/core/devio.c
+++ drivers/usb/core/devio.c
@@ -1106,10 +1106,11 @@ static int proc_getdriver(struct dev_state *ps, void __user *arg)
 
 static int proc_connectinfo(struct dev_state *ps, void __user *arg)
 {
-	struct usbdevfs_connectinfo ci = {
-		.devnum = ps->dev->devnum,
-		.slow = ps->dev->speed == USB_SPEED_LOW
-	};
+	struct usbdevfs_connectinfo ci;
+
+	memset(&ci, 0, sizeof(ci));
+	ci.devnum = ps->dev->devnum;
+	ci.slow = ps->dev->speed == USB_SPEED_LOW;
 
 	if (copy_to_user(arg, &ci, sizeof(ci)))
 		return -EFAULT;
