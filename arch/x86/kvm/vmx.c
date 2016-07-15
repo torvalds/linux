6bd2820f906f75c4a05484a5b446e50a1f1b0e34
Cao, Lei <Lei.Cao@stratus.com>
2016-07-15 13:54:04 +0000

    KVM: VMX: handle PML full VMEXIT that occurs during event delivery
    commit b244c9fc251e14a083a1cbf04bef10bd99303a76 upstream.
    
    With PML enabled, guest will shut down if a PML full VMEXIT occurs during
    event delivery. According to Intel SDM 27.2.3, PML full VMEXIT can occur when
    event is being delivered through IDT, so KVM should not exit to user space
    with error. Instead, it should let EXIT_REASON_PML_FULL go through and the
    event will be re-injected on the next VMENTRY.
    
    Signed-off-by: Lei Cao <lei.cao@stratus.com>
    Fixes: 843e4330573c ("KVM: VMX: Add PML support in VMX")
    [Shortened the summary and Cc'd stable.]
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -8124,6 +8124,7 @@ static int vmx_handle_exit(struct kvm_vcpu *vcpu)
 	if ((vectoring_info & VECTORING_INFO_VALID_MASK) &&
 			(exit_reason != EXIT_REASON_EXCEPTION_NMI &&
 			exit_reason != EXIT_REASON_EPT_VIOLATION &&
+			exit_reason != EXIT_REASON_PML_FULL &&
 			exit_reason != EXIT_REASON_TASK_SWITCH)) {
 		vcpu->run->exit_reason = KVM_EXIT_INTERNAL_ERROR;
 		vcpu->run->internal.suberror = KVM_INTERNAL_ERROR_DELIVERY_EV;
