91d231b3ca0fe884fb87567ea5710facb0e3009f
Jacob Chen <jacob2.chen@rock-chips.com>
2016-07-15 09:43:06 +0800

    UPSTREAM: ASoC: rt5616: add an of_match table
    Add a device tree match table. This serves to make the driver's support
    of device tree more explicit.
    
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    
    Bug: 25923642
    Patchset: rt5616 audio
    
    (cherry picked from broonie/sound.git#for-next e17ff2de826f8c2153cf23c8bbd9097219a84fa9)
    Signed-off-by: Kees Cook <keescook@chromium.org>
    
    Change-Id: I7ee1967a3cdf9583942c0d75b5e4d0125d31cd0c
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>

--- sound/soc/codecs/rt5616.c
+++ sound/soc/codecs/rt5616.c
@@ -1287,6 +1287,14 @@ static const struct i2c_device_id rt5616_i2c_id[] = {
 };
 MODULE_DEVICE_TABLE(i2c, rt5616_i2c_id);
 
+#if defined(CONFIG_OF)
+static const struct of_device_id rt5616_of_match[] = {
+	{ .compatible = "realtek,rt5616", },
+	{},
+};
+MODULE_DEVICE_TABLE(of, rt5616_of_match);
+#endif
+
 static int rt5616_i2c_probe(struct i2c_client *i2c,
 		    const struct i2c_device_id *id)
 {
@@ -1359,6 +1367,7 @@ static void rt5616_i2c_shutdown(struct i2c_client *client)
 static struct i2c_driver rt5616_i2c_driver = {
 	.driver = {
 		.name = "rt5616",
+		.of_match_table = of_match_ptr(rt5616_of_match),
 	},
 	.probe = rt5616_i2c_probe,
 	.remove = rt5616_i2c_remove,
