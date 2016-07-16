a37cfa8be20c5571ca82fac38441592040a2d071
Bhaktipriya Shridhar <bhaktipriya96@gmail.com>
2016-07-16 17:00:44 +0530

    drm/radeon: Remove deprecated create_singlethread_workqueue
    alloc_workqueue replaces deprecated create_singlethread_workqueue().
    
    Each hardware CRTC has a single flip work queue.
    When a radeon_flip_work_func item is queued, it needs to be executed
    ASAP because even a slight delay may cause the flip to be delayed by
    one refresh cycle.
    
    Hence, a dedicated workqueue with WQ_HIGHPRI set, has been used here
    since a delay can cause the outcome to miss the refresh cycle.
    
    Since there are only a fixed number of work items, explicit concurrency
    limit is unnecessary here.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Bhaktipriya Shridhar <bhaktipriya96@gmail.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/radeon_display.c
+++ drivers/gpu/drm/radeon/radeon_display.c
@@ -714,7 +714,7 @@ static void radeon_crtc_init(struct drm_device *dev, int index)
 
 	drm_mode_crtc_set_gamma_size(&radeon_crtc->base, 256);
 	radeon_crtc->crtc_id = index;
-	radeon_crtc->flip_queue = create_singlethread_workqueue("radeon-crtc");
+	radeon_crtc->flip_queue = alloc_workqueue("radeon-crtc", WQ_HIGHPRI, 0);
 	rdev->mode_info.crtcs[index] = radeon_crtc;
 
 	if (rdev->family >= CHIP_BONAIRE) {
