6633d80afbeecd91d5d786d6fbb32cdb8bc0a567
Johan Hovold <johan@hovoldconsulting.com>
2016-07-19 15:24:50 +0200

    greybus: module: suppress activation error message for dummy interfaces
    We currently handle dummy interfaces by deactivating them using
    activation error paths, but we don't want the corresponding
    module_inserted error message to be printed.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/module.c
+++ drivers/staging/greybus/module.c
@@ -153,8 +153,11 @@ static void gb_module_register_interface(struct gb_interface *intf)
 			break;
 	}
 	if (ret) {
-		dev_err(&module->dev, "failed to activate interface %u: %d\n",
-				intf_id, ret);
+		if (intf->type != GB_SVC_INTF_TYPE_DUMMY) {
+			dev_err(&module->dev,
+					"failed to activate interface %u: %d\n",
+					intf_id, ret);
+		}
 
 		/*
 		 * -EAGAIN indicates that the Greybus operation
