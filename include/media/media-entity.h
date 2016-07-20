5b8700e9f909d8fb2802d7ea2626460d3a233256
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2016-07-20 09:22:38 -0300

    [media] doc-rst: Fix some Sphinx warnings
    Fix all remaining media warnings with ReST that are fixable
    without changing at the Sphinx code.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- include/media/media-entity.h
+++ include/media/media-entity.h
@@ -180,8 +180,10 @@ struct media_pad {
  *			view. The media_entity_pipeline_start() function
  *			validates all links by calling this operation. Optional.
  *
- * .. note:: Those these callbacks are called with struct media_device.@graph_mutex
- * mutex held.
+ * .. note::
+ *
+ *    Those these callbacks are called with struct media_device.@graph_mutex
+ *    mutex held.
  */
 struct media_entity_operations {
 	int (*link_setup)(struct media_entity *entity,
