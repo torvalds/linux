e64c97b53bc6727aa4385535166aaa047281e02d
Marcel Holtmann <marcel@holtmann.org>
2016-07-21 14:12:41 +0200

    Bluetooth: Add combined LED trigger for controller power
    Instead of just having a LED trigger for power on a specific controller,
    this adds the LED trigger "bluetooth-power" that combines the power
    states of all controllers into a single trigger. This simplifies the
    trigger selection and also supports multiple controllers per host
    system via a single LED.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- net/bluetooth/af_bluetooth.c
+++ net/bluetooth/af_bluetooth.c
@@ -31,6 +31,7 @@
 #include <net/bluetooth/bluetooth.h>
 #include <linux/proc_fs.h>
 
+#include "leds.h"
 #include "selftest.h"
 
 /* Bluetooth sockets */
@@ -726,6 +727,8 @@ static int __init bt_init(void)
 
 	bt_debugfs = debugfs_create_dir("bluetooth", NULL);
 
+	bt_leds_init();
+
 	err = bt_sysfs_init();
 	if (err < 0)
 		return err;
@@ -784,6 +787,8 @@ static void __exit bt_exit(void)
 	sock_unregister(PF_BLUETOOTH);
 
 	bt_sysfs_cleanup();
+
+	bt_leds_cleanup();
 
 	debugfs_remove_recursive(bt_debugfs);
 }
