8a293be0d6fa0720809db6ac35a0552c51710cd2
Paul Gortmaker <paul.gortmaker@windriver.com>
2016-07-23 14:01:45 -0400

    core: migrate exception table users off module.h and onto extable.h
    These files were including module.h for exception table related
    functions.  We've now separated that content out into its own file
    "extable.h" so now move over to that and where possible, avoid all
    the extra header content in module.h that we don't really need to
    compile these non-modular files.
    
    Note:
       init/main.c still needs module.h for __init_or_module
       kernel/extable.c still needs module.h for is_module_text_address
    
    ...and so we don't get the benefit of removing module.h from the cpp
    feed for these two files, unlike the almost universal 1:1 exchange
    of module.h for extable.h we were able to do in the arch dirs.
    
    Cc: Rusty Russell <rusty@rustcorp.com.au>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Acked-by: Jessica Yu <jeyu@redhat.com>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- kernel/module.c
+++ kernel/module.c
@@ -17,6 +17,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 #include <linux/export.h>
+#include <linux/extable.h>
 #include <linux/moduleloader.h>
 #include <linux/trace_events.h>
 #include <linux/init.h>
