c0bf09d047e18e9948a431f7662b20777e540d80
Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
2016-07-25 16:19:33 -0300

    [media] rcar-vin: add R-Car gen2 fallback compatibility string
    Such fallback string is present in the 'soc_camera' version of the R-Car VIN
    driver, so need  to add it here as well...
    
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Acked-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/platform/rcar-vin/rcar-core.c
+++ drivers/media/platform/rcar-vin/rcar-core.c
@@ -231,6 +231,7 @@ static const struct of_device_id rvin_of_id_table[] = {
 	{ .compatible = "renesas,vin-r8a7790", .data = (void *)RCAR_GEN2 },
 	{ .compatible = "renesas,vin-r8a7779", .data = (void *)RCAR_H1 },
 	{ .compatible = "renesas,vin-r8a7778", .data = (void *)RCAR_M1 },
+	{ .compatible = "renesas,rcar-gen2-vin", .data = (void *)RCAR_GEN2 },
 	{ },
 };
 MODULE_DEVICE_TABLE(of, rvin_of_id_table);
