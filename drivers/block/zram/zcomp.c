4225fb587ba9b7fb73bc642a76455b17fc43fba7
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2016-07-26 15:22:56 -0700

    UPSTREAM: zram: add more compression algorithms
    Add "deflate", "lz4hc", "842" algorithms to the list of known
    compression backends.  The real availability of those algorithms,
    however, depends on the corresponding CONFIG_CRYPTO_FOO config options.
    
    [sergey.senozhatsky@gmail.com: zram-add-more-compression-algorithms-v3]
      Link: http://lkml.kernel.org/r/20160604024902.11778-7-sergey.senozhatsky@gmail.com
    Link: http://lkml.kernel.org/r/20160531122017.2878-8-sergey.senozhatsky@gmail.com
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    (cherry picked from commit eb9f56d82547db407779967a2251ea28969245b0)
    Signed-off-by: Peter Kalauskas <peskal@google.com>
    Bug: 112488418
    Change-Id: Ie46c7676363ef13c559b45dab4968e2cc48a6cbe
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -23,6 +23,15 @@ static const char * const backends[] = {
 #if IS_ENABLED(CONFIG_CRYPTO_LZ4)
 	"lz4",
 #endif
+#if IS_ENABLED(CONFIG_CRYPTO_DEFLATE)
+	"deflate",
+#endif
+#if IS_ENABLED(CONFIG_CRYPTO_LZ4HC)
+	"lz4hc",
+#endif
+#if IS_ENABLED(CONFIG_CRYPTO_842)
+	"842",
+#endif
 	NULL
 };
 
