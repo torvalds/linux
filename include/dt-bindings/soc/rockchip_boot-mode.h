bcb4ce870a54e28e1d72450f43b149c4a6df0d3a
Huang, Tao <huangtao@rock-chips.com>
2016-07-28 10:56:35 +0800

    dt-bindings: soc: rockchip_boot-mode: add charging mode support
    Change-Id: I66d5eb1813583aae37b2d19a2cf18dd9bbaf7125
    Signed-off-by: Huang, Tao <huangtao@rock-chips.com>

--- include/dt-bindings/soc/rockchip_boot-mode.h
+++ include/dt-bindings/soc/rockchip_boot-mode.h
@@ -1,7 +1,7 @@
 #ifndef __ROCKCHIP_BOOT_MODE_H
 #define __ROCKCHIP_BOOT_MODE_H
 
-/*high 24 bits is tag, low 8 bits is type*/
+/* high 24 bits is tag, low 8 bits is type */
 #define REBOOT_FLAG		0x5242C300
 /* normal boot */
 #define BOOT_NORMAL		(REBOOT_FLAG + 0)
@@ -9,6 +9,9 @@
 #define BOOT_LOADER		(REBOOT_FLAG + 1)
 /* enter recovery */
 #define BOOT_RECOVERY		(REBOOT_FLAG + 3)
+/* enter fastboot mode */
 #define BOOT_FASTBOOT		(REBOOT_FLAG + 9)
+/* enter charging mode */
+#define BOOT_CHARGING		(REBOOT_FLAG + 11)
 
 #endif
