19502b1c82f77dfadbe556b9b77d520e369e18fd
Yongji Xie <xyjxie@linux.vnet.ibm.com>
2016-07-29 18:11:53 +0800

    PCI: Ignore enforced alignment when kernel uses existing firmware setup
    PCI resources allocator will use firmware setup and not try to
    reassign resource when PCI_PROBE_ONLY or IORESOURCE_PCI_FIXED
    is set.
    
    The enforced alignment in pci_reassigndev_resource_alignment()
    should be ignored in this case. Otherwise, some PCI devices'
    resources would be released here and not re-allocated.
    
    Signed-off-by: Yongji Xie <xyjxie@linux.vnet.ibm.com>
    Signed-off-by: Paul Mackerras <paulus@ozlabs.org>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -4760,6 +4760,13 @@ static resource_size_t pci_specified_resource_alignment(struct pci_dev *dev)
 
 	spin_lock(&resource_alignment_lock);
 	p = resource_alignment_param;
+	if (pci_has_flag(PCI_PROBE_ONLY)) {
+		if (*p)
+			pr_info_once("PCI: resource_alignment ignored with PCI_PROBE_ONLY\n");
+		spin_unlock(&resource_alignment_lock);
+		return 0;
+	}
+
 	while (*p) {
 		count = 0;
 		if (sscanf(p, "%d%n", &align_order, &count) == 1 &&
@@ -4837,6 +4844,12 @@ void pci_reassigndev_resource_alignment(struct pci_dev *dev)
 		r = &dev->resource[i];
 		if (!(r->flags & IORESOURCE_MEM))
 			continue;
+		if (r->flags & IORESOURCE_PCI_FIXED) {
+			dev_info(&dev->dev, "No alignment for fixed BAR%d: %pR\n",
+				i, r);
+			continue;
+		}
+
 		size = resource_size(r);
 		if (size < align) {
 			size = align;
