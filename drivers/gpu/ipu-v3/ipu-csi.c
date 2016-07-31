ea6bead7ebd99bab1fcc5728beb4c3a791886d77
Steve Longerbeam <slongerbeam@gmail.com>
2016-07-31 12:42:21 -0700

    gpu: ipu-v3: Fix CSI data format for 16-bit media bus formats
    The CSI data format was being programmed incorrectly for the
    1x16 media bus formats. The CSI data format for 16-bit must
    be bayer/generic (CSI_SENS_CONF_DATA_FMT_BAYER).
    
    Suggested-by: Carsten Resch <Carsten.Resch@de.bosch.com>
    Signed-off-by: Steve Longerbeam <steve_longerbeam@mentor.com>
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/gpu/ipu-v3/ipu-csi.c
+++ drivers/gpu/ipu-v3/ipu-csi.c
@@ -258,12 +258,8 @@ static int mbus_code_to_bus_cfg(struct ipu_csi_bus_config *cfg, u32 mbus_code)
 		cfg->data_width = IPU_CSI_DATA_WIDTH_8;
 		break;
 	case MEDIA_BUS_FMT_UYVY8_1X16:
-		cfg->data_fmt = CSI_SENS_CONF_DATA_FMT_YUV422_UYVY;
-		cfg->mipi_dt = MIPI_DT_YUV422;
-		cfg->data_width = IPU_CSI_DATA_WIDTH_16;
-		break;
 	case MEDIA_BUS_FMT_YUYV8_1X16:
-		cfg->data_fmt = CSI_SENS_CONF_DATA_FMT_YUV422_YUYV;
+		cfg->data_fmt = CSI_SENS_CONF_DATA_FMT_BAYER;
 		cfg->mipi_dt = MIPI_DT_YUV422;
 		cfg->data_width = IPU_CSI_DATA_WIDTH_16;
 		break;
