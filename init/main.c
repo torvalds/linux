841c06d71e25a4e5fe8f7ed4ba7ba4324397f910
Prarit Bhargava <prarit@redhat.com>
2016-08-02 14:07:15 -0700

    init: allow blacklisting of module_init functions
    sprint_symbol_no_offset() returns the string "function_name
    [module_name]" where [module_name] is not printed for built in kernel
    functions.  This means that the blacklisting code will fail when
    comparing module function names with the extended string.
    
    This patch adds the functionality to block a module's module_init()
    function by finding the space in the string and truncating the
    comparison to that length.
    
    Link: http://lkml.kernel.org/r/1466124387-20446-1-git-send-email-prarit@redhat.com
    Signed-off-by: Prarit Bhargava <prarit@redhat.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Yang Shi <yang.shi@linaro.org>
    Cc: Prarit Bhargava <prarit@redhat.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Yaowei Bai <baiyaowei@cmss.chinamobile.com>
    Cc: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Cc: Rusty Russell <rusty@rustcorp.com.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- init/main.c
+++ init/main.c
@@ -716,6 +716,12 @@ static bool __init_or_module initcall_blacklisted(initcall_t fn)
 	addr = (unsigned long) dereference_function_descriptor(fn);
 	sprint_symbol_no_offset(fn_name, addr);
 
+	/*
+	 * fn will be "function_name [module_name]" where [module_name] is not
+	 * displayed for built-in init functions.  Strip off the [module_name].
+	 */
+	strreplace(fn_name, ' ', '\0');
+
 	list_for_each_entry(entry, &blacklisted_initcalls, next) {
 		if (!strcmp(fn_name, entry->buf)) {
 			pr_debug("initcall %s blacklisted\n", fn_name);
