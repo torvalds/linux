8e3562f6f8d28804b5499bf3cff520598aa25323
Luwei Kang <luwei.kang@intel.com>
2016-08-02 16:01:18 +0800

    KVM: x86: Expose more Intel AVX512 feature to guest
    Expose AVX512DQ, AVX512BW, AVX512VL feature to guest.
    Its spec can be found at:
    https://software.intel.com/sites/default/files/managed/b4/3a/319433-024.pdf
    
    Signed-off-by: Luwei Kang <luwei.kang@intel.com>
    [Resolved a trivial conflict with removed F(PCOMMIT).]
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -366,7 +366,8 @@ static inline int __do_cpuid_ent(struct kvm_cpuid_entry2 *entry, u32 function,
 		F(FSGSBASE) | F(BMI1) | F(HLE) | F(AVX2) | F(SMEP) |
 		F(BMI2) | F(ERMS) | f_invpcid | F(RTM) | f_mpx | F(RDSEED) |
 		F(ADX) | F(SMAP) | F(AVX512F) | F(AVX512PF) | F(AVX512ER) |
-		F(AVX512CD) | F(CLFLUSHOPT) | F(CLWB);
+		F(AVX512CD) | F(CLFLUSHOPT) | F(CLWB) | F(AVX512DQ) |
+		F(AVX512BW) | F(AVX512VL);
 
 	/* cpuid 0xD.1.eax */
 	const u32 kvm_cpuid_D_1_eax_x86_features =
