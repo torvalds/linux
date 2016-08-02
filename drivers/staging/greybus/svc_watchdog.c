b6fc2876a060fdd89f6dd55a4923580983092b0f
David Lin <dtwlin@google.com>
2016-08-01 20:51:38 -0700

    greybus: svc_watchdog: use schedule_delayed_work helper
    Instead of using the queue_delayed_work call for delayed work on
    system_wq, use the schedule_delayed_work helper to be more consistent on
    the style.
    
    Testing Done:
     - Check SVC watchdog is pining after the change.
    
    Signed-off-by: David Lin <dtwlin@google.com>
    Reviewed-by: Vaibhav Hiremath <vaibhav.hiremath@linaro.org>
    Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/svc_watchdog.c
+++ drivers/staging/greybus/svc_watchdog.c
@@ -90,7 +90,7 @@ static void do_work(struct work_struct *work)
 			dev_err(&svc->dev, "Resetting the greybus network, watch out!!!\n");
 
 			INIT_DELAYED_WORK(&reset_work, greybus_reset);
-			queue_delayed_work(system_wq, &reset_work, HZ / 2);
+			schedule_delayed_work(&reset_work, HZ / 2);
 
 			/*
 			 * Disable ourselves, we don't want to trip again unless
@@ -102,8 +102,7 @@ static void do_work(struct work_struct *work)
 
 	/* resubmit our work to happen again, if we are still "alive" */
 	if (watchdog->enabled)
-		queue_delayed_work(system_wq, &watchdog->work,
-				   SVC_WATCHDOG_PERIOD);
+		schedule_delayed_work(&watchdog->work, SVC_WATCHDOG_PERIOD);
 }
 
 int gb_svc_watchdog_create(struct gb_svc *svc)
@@ -178,8 +177,7 @@ int gb_svc_watchdog_enable(struct gb_svc *svc)
 		return 0;
 
 	watchdog->enabled = true;
-	queue_delayed_work(system_wq, &watchdog->work,
-			   SVC_WATCHDOG_PERIOD);
+	schedule_delayed_work(&watchdog->work, SVC_WATCHDOG_PERIOD);
 	return 0;
 }
 
