3cbf6a5deb2f4a469de7a90a3cc169e8fcba95e2
Daniel Vetter <daniel.vetter@ffwll.ch>
2016-08-03 21:11:09 +0200

    drm: Mark up legacy/dri1 drivers with DRM_LEGACY
    It's super confusing that new drivers need to be marked with
    DRIVER_MODESET when really it means DRIVER_MODERN. Much better to
    invert the meaning and rename it to something that's suitably
    off-putting.
    
    Since there's over 100 places using DRIVER_MODESET we need to roll out
    this change without a flag day.
    
    v2: Update docs.
    
    Reviewed-by: Frank Binns <frank.binns@imgtec.com>
    Reviewed-by: David Herrmann <dh.herrmann@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1470251470-30830-1-git-send-email-daniel.vetter@ffwll.ch

--- include/drm/drmP.h
+++ include/drm/drmP.h
@@ -146,6 +146,7 @@ void drm_err(const char *format, ...);
 
 /* driver capabilities and requirements mask */
 #define DRIVER_USE_AGP			0x1
+#define DRIVER_LEGACY			0x2
 #define DRIVER_PCI_DMA			0x8
 #define DRIVER_SG			0x10
 #define DRIVER_HAVE_DMA			0x20
