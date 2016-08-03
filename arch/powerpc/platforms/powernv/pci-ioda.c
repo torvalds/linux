4d9021957b5218310e28767f25551ca8f5eac797
Alexey Kardashevskiy <aik@ozlabs.ru>
2016-08-03 18:40:45 +1000

    powerpc/powernv/ioda: Fix TCE invalidate to work in real mode again
    Commit fd141d1a99a3 ("powerpc/powernv/pci: Rework accessing the TCE
    invalidate register") broke TCE invalidation on IODA2/PHB3 for real
    mode.
    
    This makes invalidate work again.
    
    Fixes: fd141d1a99a3 ("powerpc/powernv/pci: Rework accessing the TCE invalidate register")
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/platforms/powernv/pci-ioda.c
+++ arch/powerpc/platforms/powernv/pci-ioda.c
@@ -1877,7 +1877,7 @@ static void pnv_pci_phb3_tce_invalidate(struct pnv_ioda_pe *pe, bool rm,
 					unsigned shift, unsigned long index,
 					unsigned long npages)
 {
-	__be64 __iomem *invalidate = pnv_ioda_get_inval_reg(pe->phb, false);
+	__be64 __iomem *invalidate = pnv_ioda_get_inval_reg(pe->phb, rm);
 	unsigned long start, end, inc;
 
 	/* We'll invalidate DMA address in PE scope */
