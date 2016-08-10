9bcd919eb84224aee2a5ed4fa33ed80aabfe0343
Wei Yongjun <weiyj.lk@gmail.com>
2016-08-10 13:44:27 +0000

    EDAC, altera: Make a10_eccmgr_ic_ops static
    Fix the following sparse warning:
    
      drivers/edac/altera_edac.c:1649:23: warning:
       symbol 'a10_eccmgr_ic_ops' was not declared. Should it be static?
    
    Signed-off-by: Wei Yongjun <weiyj.lk@gmail.com>
    Reviewed-by: Thor Thayer <tthayer@opensource.altera.com>
    Cc: linux-edac <linux-edac@vger.kernel.org>
    Cc: lkml <linux-kernel@vger.kernel.org>
    Link: http://lkml.kernel.org/r/1470836667-11822-1-git-send-email-weiyj.lk@gmail.com
    Signed-off-by: Borislav Petkov <bp@suse.de>

--- drivers/edac/altera_edac.c
+++ drivers/edac/altera_edac.c
@@ -1831,7 +1831,7 @@ static int a10_eccmgr_irqdomain_map(struct irq_domain *d, unsigned int irq,
 	return 0;
 }
 
-struct irq_domain_ops a10_eccmgr_ic_ops = {
+static struct irq_domain_ops a10_eccmgr_ic_ops = {
 	.map = a10_eccmgr_irqdomain_map,
 	.xlate = irq_domain_xlate_twocell,
 };
