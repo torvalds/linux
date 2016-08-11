da752563e7fff00885c93e9d5c06bbbfaf34e4da
Mark Rutland <mark.rutland@arm.com>
2016-08-11 17:59:46 +0100

    arm64: remove traces of perf_ops_bp
    Even though perf_ops_bp was removed/renamed back in commit
    b0a873ebbf87bf38 ("perf: Register PMU implementations"), as part of
    v2.6.37, its definition still lives on in some arch headers.
    
    This patch removes the vestigal definition from arm64.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/hw_breakpoint.h
+++ arch/arm64/include/asm/hw_breakpoint.h
@@ -141,8 +141,6 @@ static inline void ptrace_hw_copy_thread(struct task_struct *task)
 }
 #endif
 
-extern struct pmu perf_ops_bp;
-
 /* Determine number of BRP registers available. */
 static inline int get_num_brps(void)
 {
