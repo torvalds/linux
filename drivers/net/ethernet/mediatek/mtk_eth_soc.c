d7005652cd31dfc5660e1e32bf7e53538ef14987
sean.wang@mediatek.com <sean.wang@mediatek.com>
2016-08-13 19:16:18 +0800

    net: ethernet: mediatek: fixed that initializing u64_stats_sync is missing
    To fix runtime warning with lockdep is enabled due that u64_stats_sync
    is not initialized well, so add it.
    
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mediatek/mtk_eth_soc.c
+++ drivers/net/ethernet/mediatek/mtk_eth_soc.c
@@ -1751,6 +1751,7 @@ static int mtk_add_mac(struct mtk_eth *eth, struct device_node *np)
 		goto free_netdev;
 	}
 	spin_lock_init(&mac->hw_stats->stats_lock);
+	u64_stats_init(&mac->hw_stats->syncp);
 	mac->hw_stats->reg_offset = id * MTK_STAT_OFFSET;
 
 	SET_NETDEV_DEV(eth->netdev[id], eth->dev);
