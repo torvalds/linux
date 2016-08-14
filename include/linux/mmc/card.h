5275a652d296711aaf7f2f4173c8db153e5777c3
Uri Yanai <uri.yanai@sandisk.com>
2016-08-14 11:46:36 +0300

    mmc: sd: Export SD Status via “ssr” device attribute
    The SD Status register contains several important fields related to the
    SD Card proprietary features.
    Those fields may be used by user space applications for vendor specific
    usage.
    None of those fields are exported today by the driver to user space.
    In this patch, we are reading the SD Status register and exporting
    (using MMC_DEV_ATTR) the SD Status register to the user space.
    
    Signed-off-by: Uri Yanai <uri.yanai@sandisk.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -292,6 +292,7 @@ struct mmc_card {
 	u32			raw_cid[4];	/* raw card CID */
 	u32			raw_csd[4];	/* raw card CSD */
 	u32			raw_scr[2];	/* raw card SCR */
+	u32			raw_ssr[16];	/* raw card SSR */
 	struct mmc_cid		cid;		/* card identification */
 	struct mmc_csd		csd;		/* card specific */
 	struct mmc_ext_csd	ext_csd;	/* mmc v4 extended card specific */
