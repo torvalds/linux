320b4da6d4254067372826d4fa90897b2d51e925
Luis de Bethencourt <luisbg@osg.samsung.com>
2016-08-14 18:31:38 +0100

    befs: fix style issues in header files
    Fixing checkpatch.pl issues in befs header files:
    WARNING: Missing a blank line after declarations
    +       befs_inode_addr iaddr;
    +       iaddr.allocation_group = blockno >> BEFS_SB(sb)->ag_shift;
    
    WARNING: space prohibited between function name and open parenthesis '('
    +       return BEFS_SB(sb)->block_size / sizeof (befs_disk_inode_addr);
    
    ERROR: "foo * bar" should be "foo *bar"
    +                   const char *key, befs_off_t * value);
    
    ERROR: Macros with complex values should be enclosed in parentheses
    +#define PACKED __attribute__ ((__packed__))
    
    Signed-off-by: Luis de Bethencourt <luisbg@osg.samsung.com>

--- fs/befs/inode.h
+++ fs/befs/inode.h
@@ -3,6 +3,5 @@
  * 
  */
 
-int befs_check_inode(struct super_block *sb, befs_inode * raw_inode,
+int befs_check_inode(struct super_block *sb, befs_inode *raw_inode,
 		     befs_blocknr_t inode);
-
