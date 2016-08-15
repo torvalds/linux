2c645cd7c4a0d4b35da1e43ec3a5b55a64038157
Markus Heiser <markus.heiser@darmarIT.de>
2016-08-15 16:08:25 +0200

    doc-rst:c-domain: ref-name of a function declaration
    Add option 'name' to the "c:function:" directive.  With option 'name'
    the ref-name of a function can be modified. E.g.::
    
        .. c:function:: int ioctl( int fd, int request )
           :name: VIDIOC_LOG_STATUS
    
    The func-name (e.g. ioctl) remains in the output but the ref-name
    changed from ``ioctl`` to ``VIDIOC_LOG_STATUS``. The index entry for
    this function is also changed to ``VIDIOC_LOG_STATUS`` and the function
    can now referenced by::
    
        :c:func:`VIDIOC_LOG_STATUS`
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarIT.de>
    Signed-off-by: Jonathan Corbet <corbet@lwn.net>

--- Documentation/sphinx/cdomain.py
+++ Documentation/sphinx/cdomain.py
@@ -7,7 +7,23 @@ u"""
 
     :copyright:  Copyright (C) 2016  Markus Heiser
     :license:    GPL Version 2, June 1991 see Linux/COPYING for details.
+
+    List of customizations:
+
+    * Add option 'name' to the "c:function:" directive.  With option 'name' the
+      ref-name of a function can be modified. E.g.::
+
+          .. c:function:: int ioctl( int fd, int request )
+             :name: VIDIOC_LOG_STATUS
+
+      The func-name (e.g. ioctl) remains in the output but the ref-name changed
+      from 'ioctl' to 'VIDIOC_LOG_STATUS'. The function is referenced by::
+
+          * :c:func:`VIDIOC_LOG_STATUS` or
+          * :any:`VIDIOC_LOG_STATUS` (``:any:`` needs sphinx 1.3)
 """
+
+from docutils.parsers.rst import directives
 
 from sphinx.domains.c import CObject as Base_CObject
 from sphinx.domains.c import CDomain as Base_CDomain
@@ -29,6 +45,21 @@ class CObject(Base_CObject):
     """
     Description of a C language object.
     """
+    option_spec = {
+        "name" : directives.unchanged
+    }
+
+    def handle_signature(self, sig, signode):
+        """Transform a C signature into RST nodes."""
+        fullname = super(CObject, self).handle_signature(sig, signode)
+        if "name" in self.options:
+            if self.objtype == 'function':
+                fullname = self.options["name"]
+            else:
+                # FIXME: handle :name: value of other declaration types?
+                pass
+        return fullname
+
 
 class CDomain(Base_CDomain):
 
