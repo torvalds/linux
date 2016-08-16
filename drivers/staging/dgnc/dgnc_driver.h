f56edd42ee69ea73e1ca755614ed6c0e1a6ccef8
Anson Jacob <ansonjacob.aj@gmail.com>
2016-08-16 14:56:12 -0400

    staging: dgnc: fix 'line over 80 characters'
    fix checkpatch.pl warning about 'line over 80 characters'.
    
    Signed-off-by: Anson Jacob <ansonjacob.aj@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_driver.h
+++ drivers/staging/dgnc/dgnc_driver.h
@@ -390,7 +390,7 @@ extern uint		dgnc_major;		/* Our driver/mgmt major */
 extern int		dgnc_poll_tick;		/* Poll interval - 20 ms */
 extern spinlock_t	dgnc_global_lock;	/* Driver global spinlock */
 extern spinlock_t	dgnc_poll_lock;		/* Poll scheduling lock */
-extern uint		dgnc_num_boards;		/* Total number of boards */
+extern uint		dgnc_num_boards;	/* Total number of boards */
 extern struct dgnc_board	*dgnc_board[MAXBOARDS];	/* Array of board
 							 * structs
 							 */
