d4fe7e14e4ff5d90286397180831a35df502969e
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2016-08-16 13:25:43 -0300

    docs-rst: enable the Sphinx math extension
    This extension will be used by the media books.
    
    The name of the math image extension changed on Sphinx 1.4.x,
    according with:
    	http://www.sphinx-doc.org/en/stable/ext/math.html#module-sphinx.ext.imgmath
    
    Let's autodetect, to keep building with versions < 1.4.
    
    Suggested-by: Markus Heiser <markus.heiser@darmarit.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>
    Signed-off-by: Jonathan Corbet <corbet@lwn.net>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -14,6 +14,11 @@
 
 import sys
 import os
+import sphinx
+
+# Get Sphinx version
+major, minor, patch = map(int, sphinx.__version__.split("."))
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -30,6 +35,12 @@ from load_config import loadConfig
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['kernel-doc', 'rstFlatTable', 'kernel_include']
+
+# The name of the math extension changed on Sphinx 1.4
+if minor > 3:
+    extensions.append("sphinx.ext.imgmath")
+else:
+    extensions.append("sphinx.ext.pngmath")
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
