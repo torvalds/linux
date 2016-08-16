3256aaef5e9a851f6be47656868020726e102187
Vincent Stehlé <vincent.stehle@intel.com>
2016-08-16 15:11:25 +0200

    nvmet-rdma: Fix use after free
    Avoid dereferencing the queue pointer in nvmet_rdma_release_queue_work()
    after it has been freed by nvmet_rdma_free_queue().
    
    Fixes: d8f7750a08968b10 ("nvmet-rdma: Correctly handle RDMA device hot removal")
    Signed-off-by: Vincent Stehlé <vincent.stehle@intel.com>
    Cc: Sagi Grimberg <sagi@grimberg.me>
    Cc: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>

--- drivers/nvme/target/rdma.c
+++ drivers/nvme/target/rdma.c
@@ -978,10 +978,11 @@ static void nvmet_rdma_release_queue_work(struct work_struct *w)
 		container_of(w, struct nvmet_rdma_queue, release_work);
 	struct rdma_cm_id *cm_id = queue->cm_id;
 	struct nvmet_rdma_device *dev = queue->dev;
+	enum nvmet_rdma_queue_state state = queue->state;
 
 	nvmet_rdma_free_queue(queue);
 
-	if (queue->state != NVMET_RDMA_IN_DEVICE_REMOVAL)
+	if (state != NVMET_RDMA_IN_DEVICE_REMOVAL)
 		rdma_destroy_id(cm_id);
 
 	kref_put(&dev->ref, nvmet_rdma_free_dev);
