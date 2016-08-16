4d486e0083796b54d5aeddd7a5794f897fca1008
Christophe Leroy <christophe.leroy@c-s.fr>
2016-08-16 08:26:20 +0200

    soc/fsl/qe: fix Oops on CPM1 (and likely CPM2)
    Commit 0e6e01ff694ee ("CPM/QE: use genalloc to manage CPM/QE muram")
    has changed the way muram is managed.
    genalloc uses kmalloc(), hence requires the SLAB to be up and running.
    
    On powerpc 8xx, cpm_reset() is called early during startup.
    cpm_reset() then calls cpm_muram_init() before SLAB is available,
    hence the following Oops.
    
    cpm_reset() cannot be called during initcalls because the CPM is
    needed for console.
    
    This patch removes the call to cpm_muram_init() from cpm_reset().
    cpm_muram_init() will be called from a new function called cpm_init()
    which is declared as subsys_initcall, unless cpm_muram_alloc() is
    called earlier for the serial console in which case cpm_muram_init()
    will be called from there.
    
    The reason for calling it from two places is that some drivers
    (e.g. i2c-cpm) need some of the initialisations done by
    cpm_muram_init() but don't call cpm_muram_alloc(). The console
    driver calls cpm_muram_alloc() but some platforms might not use
    the CPM serial ports for console.
    
    [    0.000000] Unable to handle kernel paging request for data at address 0x00000008
    [    0.000000] Faulting instruction address: 0xc01acce0
    [    0.000000] Oops: Kernel access of bad area, sig: 11 [#1]
    [    0.000000] PREEMPT CMPC885
    [    0.000000] CPU: 0 PID: 0 Comm: swapper Not tainted 4.4.14-g0886ed8 #5
    [    0.000000] task: c05183e0 ti: c0536000 task.ti: c0536000
    [    0.000000] NIP: c01acce0 LR: c0011068 CTR: 00000000
    [    0.000000] REGS: c0537e50 TRAP: 0300   Not tainted (4.4.14-s3k-dev-g0886ed8-svn)
    [    0.000000] MSR: 00001032 <ME,IR,DR,RI>  CR: 28044428  XER: 00000000
    [    0.000000] DAR: 00000008 DSISR: c0000000
    GPR00: c0011068 c0537f00 c05183e0 00000000 00009000 ffffffff 00000bc0 ffffffff
    GPR08: ff003000 ff00b000 ff003bbf 00000000 22044422 100d43a8 00000000 07ff94e8
    GPR16: 00000000 07bb5d70 00000000 07ff81f4 07ff81f4 07ff81f4 00000000 00000000
    GPR24: 07ffb3a0 07fe7628 c0550000 c7ffa190 c0540000 ff003bbf 00000000 00000001
    [    0.000000] NIP [c01acce0] gen_pool_add_virt+0x14/0xdc
    [    0.000000] LR [c0011068] cpm_muram_init+0xd4/0x18c
    [    0.000000] Call Trace:
    [    0.000000] [c0537f00] [00000200] 0x200 (unreliable)
    [    0.000000] [c0537f20] [c0011068] cpm_muram_init+0xd4/0x18c
    [    0.000000] [c0537f70] [c0494684] cpm_reset+0xb4/0xc8
    [    0.000000] [c0537f90] [c0494c64] cmpc885_setup_arch+0x10/0x30
    [    0.000000] [c0537fa0] [c0493cd4] setup_arch+0x130/0x168
    [    0.000000] [c0537fb0] [c04906bc] start_kernel+0x88/0x380
    [    0.000000] [c0537ff0] [c0002224] start_here+0x38/0x98
    [    0.000000] Instruction dump:
    [    0.000000] 91430010 91430014 80010014 83e1000c 7c0803a6 38210010 4e800020 7c0802a6
    [    0.000000] 9421ffe0 bf61000c 90010024 7c7e1b78 <80630008> 7c9c2378 7cc31c30 3863001f
    [    0.000000] ---[ end trace dc8fa200cb88537f ]---
    
    fixes: 0e6e01ff694ee ("CPM/QE: use genalloc to manage CPM/QE muram")
    Cc: stable@vger.linux.org
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    [scottwood: Removed some string changes unrelated to bugfix]
    Signed-off-by: Scott Wood <oss@buserror.net>

--- arch/powerpc/sysdev/cpm1.c
+++ arch/powerpc/sysdev/cpm1.c
@@ -233,8 +233,6 @@ void __init cpm_reset(void)
 	else
 		out_be32(&siu_conf->sc_sdcr, 1);
 	immr_unmap(siu_conf);
-
-	cpm_muram_init();
 }
 
 static DEFINE_SPINLOCK(cmd_lock);
--- drivers/soc/fsl/qe/qe_common.c
+++ drivers/soc/fsl/qe/qe_common.c
@@ -70,6 +70,11 @@ int cpm_muram_init(void)
 	}
 
 	muram_pool = gen_pool_create(0, -1);
+	if (!muram_pool) {
+		pr_err("Cannot allocate memory pool for CPM/QE muram");
+		ret = -ENOMEM;
+		goto out_muram;
+	}
 	muram_pbase = of_translate_address(np, zero);
 	if (muram_pbase == (phys_addr_t)OF_BAD_ADDR) {
 		pr_err("Cannot translate zero through CPM muram node");
@@ -115,6 +120,9 @@ static unsigned long cpm_muram_alloc_common(unsigned long size,
 {
 	struct muram_block *entry;
 	unsigned long start;
+
+	if (!muram_pool && cpm_muram_init())
+		goto out2;
 
 	start = gen_pool_alloc_algo(muram_pool, size, algo, data);
 	if (!start)
