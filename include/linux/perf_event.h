d48d0c64e2895ad8eb6cabfbad586f8817fa1aab
Ben Hutchings <ben@decadent.org.uk>
2016-08-16 10:27:00 -0600

    UBUNTU: SAUCE: security,perf: Allow further restriction of perf_event_open
    https://lkml.org/lkml/2016/1/11/587
    
    The GRKERNSEC_PERF_HARDEN feature extracted from grsecurity.  Adds the
    option to disable perf_event_open() entirely for unprivileged users.
    This standalone version doesn't include making the variable read-only
    (or renaming it).
    
    When kernel.perf_event_open is set to 3 (or greater), disallow all
    access to performance events by users without CAP_SYS_ADMIN.
    Add a Kconfig symbol CONFIG_SECURITY_PERF_EVENTS_RESTRICT that
    makes this value the default.
    
    This is based on a similar feature in grsecurity
    (CONFIG_GRKERNSEC_PERF_HARDEN).  This version doesn't include making
    the variable read-only.  It also allows enabling further restriction
    at run-time regardless of whether the default is changed.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -1151,6 +1151,11 @@ extern int perf_cpu_time_max_percent_handler(struct ctl_table *table, int write,
 int perf_event_max_stack_handler(struct ctl_table *table, int write,
 				 void __user *buffer, size_t *lenp, loff_t *ppos);
 
+static inline bool perf_paranoid_any(void)
+{
+	return sysctl_perf_event_paranoid > 2;
+}
+
 static inline bool perf_paranoid_tracepoint_raw(void)
 {
 	return sysctl_perf_event_paranoid > -1;
