e7deb890c5f345fb2a685de342f364d1370c28ad
wang di <di.wang@intel.com>
2016-08-16 16:18:21 -0400

    staging: lustre: lmv: remove duplicate MAX_HASH_*
    The MAX_HASH_* macros already exist in obd.h. Remove
    the duplicated defines in lustre_idl.h.
    
    Signed-off-by: wang di <di.wang@intel.com>
    Reviewed-on: http://review.whamcloud.com/7043
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-3531
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/lustre/lustre_idl.h
+++ drivers/staging/lustre/lustre/include/lustre/lustre_idl.h
@@ -2486,10 +2486,6 @@ struct lmv_desc {
 #define MEA_MAGIC_ALL_CHARS      0xb222a11c
 #define MEA_MAGIC_HASH_SEGMENT   0xb222a11b
 
-#define MAX_HASH_SIZE_32	 0x7fffffffUL
-#define MAX_HASH_SIZE	    0x7fffffffffffffffULL
-#define MAX_HASH_HIGHEST_BIT     0x1000000000000000ULL
-
 /* lmv structures */
 #define LMV_MAGIC_V1	0x0CD10CD0	/* normal stripe lmv magic */
 #define LMV_USER_MAGIC	0x0CD20CD0	/* default lmv magic*/
