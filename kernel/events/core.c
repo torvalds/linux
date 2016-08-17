29dd3288705f26cc27663e79061209dabce2d5b9
Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
2016-08-17 15:06:08 +0530

    bitmap.h, perf/core: Fix the mask in perf_output_sample_regs()
    When decoding the perf_regs mask in perf_output_sample_regs(),
    we loop through the mask using find_first_bit and find_next_bit functions.
    
    While the exisiting code works fine in most of the case, the logic
    is broken for big-endian 32-bit kernels.
    
    When reading a u64 mask using (u32 *)(&val)[0], find_*_bit() assumes
    that it gets the lower 32 bits of u64, but instead it gets the upper
    32 bits - which is wrong.
    
    The fix is to swap the words of the u64 to handle this case.
    This is _not_ a regular endianness swap.
    
    Suggested-by: Yury Norov <ynorov@caviumnetworks.com>
    Signed-off-by: Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Yury Norov <ynorov@caviumnetworks.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vince Weaver <vincent.weaver@maine.edu>
    Cc: linuxppc-dev@lists.ozlabs.org
    Link: http://lkml.kernel.org/r/1471426568-31051-2-git-send-email-maddy@linux.vnet.ibm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -5340,9 +5340,10 @@ perf_output_sample_regs(struct perf_output_handle *handle,
 			struct pt_regs *regs, u64 mask)
 {
 	int bit;
+	DECLARE_BITMAP(_mask, 64);
 
-	for_each_set_bit(bit, (const unsigned long *) &mask,
-			 sizeof(mask) * BITS_PER_BYTE) {
+	bitmap_from_u64(_mask, mask);
+	for_each_set_bit(bit, _mask, sizeof(mask) * BITS_PER_BYTE) {
 		u64 val;
 
 		val = perf_reg_value(regs, bit);
