05a76d3d6ad1ee9f9814f88949cc9305fc165460
Lyude <cpaul@redhat.com>
2016-08-17 15:55:57 -0400

    drm/i915/skl: Ensure pipes with changed wms get added to the state
    If we're enabling a pipe, we'll need to modify the watermarks on all
    active planes. Since those planes won't be added to the state on
    their own, we need to add them ourselves.
    
    Signed-off-by: Lyude <cpaul@redhat.com>
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>
    Cc: stable@vger.kernel.org
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Cc: Daniel Vetter <daniel.vetter@intel.com>
    Cc: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    Cc: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1471463761-26796-6-git-send-email-cpaul@redhat.com

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -4112,6 +4112,10 @@ skl_compute_ddb(struct drm_atomic_state *state)
 		ret = skl_allocate_pipe_ddb(cstate, ddb);
 		if (ret)
 			return ret;
+
+		ret = drm_atomic_add_affected_planes(state, &intel_crtc->base);
+		if (ret)
+			return ret;
 	}
 
 	return 0;
