60bcabd080f53561efa9288be45c128feda1a8bb
Oliver Neukum <oneukum@suse.com>
2016-08-17 15:51:55 +0200

    kaweth: fix firmware download
    This fixes the oops discovered by the Umap2 project and Alan Stern.
    The intf member needs to be set before the firmware is downloaded.
    
    Signed-off-by: Oliver Neukum <oneukum@suse.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/kaweth.c
+++ drivers/net/usb/kaweth.c
@@ -1029,6 +1029,7 @@ static int kaweth_probe(
 	kaweth = netdev_priv(netdev);
 	kaweth->dev = udev;
 	kaweth->net = netdev;
+	kaweth->intf = intf;
 
 	spin_lock_init(&kaweth->device_lock);
 	init_waitqueue_head(&kaweth->term_wait);
@@ -1138,8 +1139,6 @@ err_fw:
 	}
 
 	dev_dbg(dev, "Initializing net device.\n");
-
-	kaweth->intf = intf;
 
 	kaweth->tx_urb = usb_alloc_urb(0, GFP_KERNEL);
 	if (!kaweth->tx_urb)
