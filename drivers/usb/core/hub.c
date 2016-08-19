4e248000e0d3b406bd6612186835467f2f84486e
Yonglong Wu <yonglong.wu@mediatek.com>
2016-08-19 11:37:26 +0800

    usb: hub: change CLEAR_FEATURE to SET_FEATURE
    In USB20 specification, describes in chapter 9.4.5: The Remote Wakeup
    field can be modified by the SetFeature() and ClearFeature() requests
    using the DEVICE_REMOTE_WAKEUP feature selector.
    
    In USB30 specification, also describes in chapter 9.4.5: The Function
    Remote Wakeup field can be modified by the SetFeature() requests
    using the FUNCTION_SUSPEND feature selector. In chapter 9.4.9 Set
    Feature reference, it describes Function Remote Wake Enabled/Disabled
    at suspend options by SET_FEATURE.
    
    In USB30 specification only mentioned SetFeature(), so we need use
    SET_FEATURE replace CLEAR_FEATURE to disable USB30 function remote
    wakeup in suspend options.
    
    Signed-off-by: Yonglong Wu <yonglong.wu@mediatek.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -3104,7 +3104,7 @@ static int usb_disable_remote_wakeup(struct usb_device *udev)
 				USB_CTRL_SET_TIMEOUT);
 	else
 		return usb_control_msg(udev, usb_sndctrlpipe(udev, 0),
-				USB_REQ_CLEAR_FEATURE, USB_RECIP_INTERFACE,
+				USB_REQ_SET_FEATURE, USB_RECIP_INTERFACE,
 				USB_INTRF_FUNC_SUSPEND,	0, NULL, 0,
 				USB_CTRL_SET_TIMEOUT);
 }
