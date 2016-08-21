a769de07edfd7881d17bf6c6fa014a0e176beb8b
Jannik Becher <becher.jannik@gmail.com>
2016-08-21 15:44:20 -0300

    [media] drivers: hackrf: fixed a coding style issue
    changed 'unsigned' to 'unsigned int' to obtain the coding style.
    
    Signed-off-by: Jannik Becher <Becher.Jannik@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/usb/hackrf/hackrf.c
+++ drivers/media/usb/hackrf/hackrf.c
@@ -129,7 +129,7 @@ struct hackrf_dev {
 	struct list_head rx_buffer_list;
 	struct list_head tx_buffer_list;
 	spinlock_t buffer_list_lock; /* Protects buffer_list */
-	unsigned sequence;	     /* Buffer sequence counter */
+	unsigned int sequence;	     /* Buffer sequence counter */
 	unsigned int vb_full;        /* vb is full and packets dropped */
 	unsigned int vb_empty;       /* vb is empty and packets dropped */
 
