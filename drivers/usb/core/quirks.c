903c5a46a861244e7fc5ddf6101963b6a4c9b88f
Willy Tarreau <w@1wt.eu>
2016-08-21 10:47:12 +0200

    USB: fix up faulty backports
    Ben Hutchings reported that two patches were incorrectly backported
    to 3.10 :
    
    - ddbe1fca0bcb ("USB: Add device quirk for ASUS T100 Base Station keyboard")
    - ad87e03213b5 ("USB: add quirk for devices with broken LPM")
    
    These two patches introduce quirks which must be in usb_quirk_list and
    not in usb_interface_quirk_list. These last one must only contain the
    Logitech UVC camera.
    
    Reported-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -170,14 +170,6 @@ static const struct usb_device_id usb_quirk_list[] = {
 	/* INTEL VALUE SSD */
 	{ USB_DEVICE(0x8086, 0xf1a5), .driver_info = USB_QUIRK_RESET_RESUME },
 
-	{ }  /* terminating entry must be last */
-};
-
-static const struct usb_device_id usb_interface_quirk_list[] = {
-	/* Logitech UVC Cameras */
-	{ USB_VENDOR_AND_INTERFACE_INFO(0x046d, USB_CLASS_VIDEO, 1, 0),
-	  .driver_info = USB_QUIRK_RESET_RESUME },
-
 	/* ASUS Base Station(T100) */
 	{ USB_DEVICE(0x0b05, 0x17e0), .driver_info =
 			USB_QUIRK_IGNORE_REMOTE_WAKEUP },
@@ -187,6 +179,14 @@ static const struct usb_device_id usb_interface_quirk_list[] = {
 
 	/* Blackmagic Design UltraStudio SDI */
 	{ USB_DEVICE(0x1edb, 0xbd4f), .driver_info = USB_QUIRK_NO_LPM },
+
+	{ }  /* terminating entry must be last */
+};
+
+static const struct usb_device_id usb_interface_quirk_list[] = {
+	/* Logitech UVC Cameras */
+	{ USB_VENDOR_AND_INTERFACE_INFO(0x046d, USB_CLASS_VIDEO, 1, 0),
+	  .driver_info = USB_QUIRK_RESET_RESUME },
 
 	{ }  /* terminating entry must be last */
 };
