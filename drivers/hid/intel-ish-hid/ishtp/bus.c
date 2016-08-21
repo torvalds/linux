814cb72e25afccdd24855a26c934c1d3dfbf808e
Wei Yongjun <weiyj.lk@gmail.com>
2016-08-21 15:31:35 +0000

    HID: intel_ish-hid: Remove duplicated include from bus.c
    Remove duplicated include.
    
    Signed-off-by: Wei Yongjun <weiyj.lk@gmail.com>
    Acked-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/intel-ish-hid/ishtp/bus.c
+++ drivers/hid/intel-ish-hid/ishtp/bus.c
@@ -16,7 +16,6 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
-#include <linux/kernel.h>
 #include <linux/device.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
