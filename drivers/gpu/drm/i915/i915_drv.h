d5d0804f8f6d0c89913f6a2de5348adef8ec33e4
Daniel Vetter <daniel.vetter@ffwll.ch>
2016-08-22 08:35:48 +0200

    drm/i915: Update DRIVER_DATE to 20160822
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -70,7 +70,7 @@
 
 #define DRIVER_NAME		"i915"
 #define DRIVER_DESC		"Intel Graphics"
-#define DRIVER_DATE		"20160808"
+#define DRIVER_DATE		"20160822"
 
 #undef WARN_ON
 /* Many gcc seem to no see through this and fall over :( */
