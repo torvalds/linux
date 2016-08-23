36d050d913333ea21e9728cbbc1fc07ae5be8d6c
Greg Ungerer <gerg@linux-m68k.org>
2016-08-23 21:46:06 +1000

    m68knommu: fix IO write size in nettel pin set
    The pin write code that supports the UART signals is not using he correct
    word write IO access method. It correctly reads the correct 16 bit
    registrer, it should also write the new value back with a 16 bit write.
    Fix it to use writew().
    
    Signed-off-by: Greg Ungerer <gerg@linux-m68k.org>

--- arch/m68k/include/asm/nettel.h
+++ arch/m68k/include/asm/nettel.h
@@ -92,7 +92,7 @@ static __inline__ unsigned int mcf_getppdata(void)
 
 static __inline__ void mcf_setppdata(unsigned int mask, unsigned int bits)
 {
-	write((readw(MCFSIM_PBDAT) & ~mask) | bits, MCFSIM_PBDAT);
+	writew((readw(MCFSIM_PBDAT) & ~mask) | bits, MCFSIM_PBDAT);
 }
 #endif
 
