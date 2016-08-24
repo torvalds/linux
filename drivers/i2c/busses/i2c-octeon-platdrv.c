caa505f20df01b326ca99dcace0c4656298d94f3
Jan Glauber <jglauber@cavium.com>
2016-08-24 23:25:47 +0200

    i2c: octeon: Sort include files alphabetically
    Sort include files alphabetically to reduce probability of merge
    conflicts.
    
    Signed-off-by: Jan Glauber <jglauber@cavium.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/busses/i2c-octeon-platdrv.c
+++ drivers/i2c/busses/i2c-octeon-platdrv.c
@@ -12,16 +12,16 @@
  */
 
 #include <linux/atomic.h>
-#include <linux/platform_device.h>
+#include <linux/delay.h>
+#include <linux/i2c.h>
 #include <linux/interrupt.h>
+#include <linux/io.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/delay.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
-#include <linux/i2c.h>
-#include <linux/io.h>
-#include <linux/of.h>
 
 #include <asm/octeon/octeon.h>
 #include "i2c-octeon-core.h"
