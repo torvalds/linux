b02b573fcf4d27b98f2e867b0f9bdbd415b2e508
Ivan Safonov <insafonov@gmail.com>
2016-08-24 15:20:22 +0700

    staging: r8188eu: remove rtw_set_ie_secondary_ch_offset function
    Driver does not use this function.
    
    Signed-off-by: Ivan Safonov <insafonov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/core/rtw_ieee80211.c
+++ drivers/staging/rtl8188eu/core/rtw_ieee80211.c
@@ -166,11 +166,6 @@ inline u8 *rtw_set_ie_ch_switch(u8 *buf, u32 *buf_len, u8 ch_switch_mode,
 	return rtw_set_ie(buf, WLAN_EID_CHANNEL_SWITCH,  3, ie_data, buf_len);
 }
 
-inline u8 *rtw_set_ie_secondary_ch_offset(u8 *buf, u32 *buf_len, u8 secondary_ch_offset)
-{
-	return rtw_set_ie(buf, WLAN_EID_SECONDARY_CHANNEL_OFFSET,  1, &secondary_ch_offset, buf_len);
-}
-
 inline u8 *rtw_set_ie_mesh_ch_switch_parm(u8 *buf, u32 *buf_len, u8 ttl,
 	u8 flags, u16 reason, u16 precedence)
 {
--- drivers/staging/rtl8188eu/include/ieee80211.h
+++ drivers/staging/rtl8188eu/include/ieee80211.h
@@ -1098,8 +1098,6 @@ enum secondary_ch_offset {
 };
 u8 *rtw_set_ie_ch_switch(u8 *buf, u32 *buf_len, u8 ch_switch_mode,
 			 u8 new_ch, u8 ch_switch_cnt);
-u8 *rtw_set_ie_secondary_ch_offset(u8 *buf, u32 *buf_len,
-				   u8 secondary_ch_offset);
 u8 *rtw_set_ie_mesh_ch_switch_parm(u8 *buf, u32 *buf_len, u8 ttl,
 				   u8 flags, u16 reason, u16 precedence);
 
