4b30950252265140139ce441fd7d8743d1d6052e
Jonathan Liu <net147@gmail.com>
2016-08-30 16:55:00 +1000

    drm/sun4i: rgb: add missing calls to drm_panel_{prepare,unprepare}
    If the enable-gpios property of a simple panel in device tree is set,
    the GPIO is not toggled on/off because of missing calls to
    drm_panel_prepare and drm_panel_unprepare.
    
    Signed-off-by: Jonathan Liu <net147@gmail.com>
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- drivers/gpu/drm/sun4i/sun4i_rgb.c
+++ drivers/gpu/drm/sun4i/sun4i_rgb.c
@@ -151,8 +151,10 @@ static void sun4i_rgb_encoder_enable(struct drm_encoder *encoder)
 
 	DRM_DEBUG_DRIVER("Enabling RGB output\n");
 
-	if (!IS_ERR(tcon->panel))
+	if (!IS_ERR(tcon->panel)) {
+		drm_panel_prepare(tcon->panel);
 		drm_panel_enable(tcon->panel);
+	}
 
 	if (!IS_ERR(encoder->bridge))
 		drm_bridge_enable(encoder->bridge);
@@ -173,8 +175,10 @@ static void sun4i_rgb_encoder_disable(struct drm_encoder *encoder)
 	if (!IS_ERR(encoder->bridge))
 		drm_bridge_disable(encoder->bridge);
 
-	if (!IS_ERR(tcon->panel))
+	if (!IS_ERR(tcon->panel)) {
 		drm_panel_disable(tcon->panel);
+		drm_panel_unprepare(tcon->panel);
+	}
 }
 
 static void sun4i_rgb_encoder_mode_set(struct drm_encoder *encoder,
