ea2f7321a9029194a31ff9f798a8276d45747bcd
Baoyou Xie <baoyou.xie@linaro.org>
2016-08-31 17:30:31 +0800

    regulator: hi6421: mark hi6421_regulator_ldo_get_optimum_mode() static
    We get 1 warning when building kernel with W=1:
    drivers/regulator/hi6421-regulator.c:480:14: warning: no previous prototype for
    'hi6421_regulator_ldo_get_optimum_mode' [-Wmissing-prototypes]
    
    In fact, this function is only used in the file in which it is
    declared and don't need a declaration, but can be made static.
    So this patch marks it 'static'.
    
    Signed-off-by: Baoyou Xie <baoyou.xie@linaro.org>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/hi6421-regulator.c
+++ drivers/regulator/hi6421-regulator.c
@@ -477,7 +477,8 @@ static int hi6421_regulator_buck_set_mode(struct regulator_dev *rdev,
 	return 0;
 }
 
-unsigned int hi6421_regulator_ldo_get_optimum_mode(struct regulator_dev *rdev,
+static unsigned int
+hi6421_regulator_ldo_get_optimum_mode(struct regulator_dev *rdev,
 			int input_uV, int output_uV, int load_uA)
 {
 	struct hi6421_regulator_info *info = rdev_get_drvdata(rdev);
