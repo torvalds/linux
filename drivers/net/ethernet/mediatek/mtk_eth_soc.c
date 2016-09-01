1b43079906367d4d6e1ad00f04fff525a32e26c4
Sean Wang <sean.wang@mediatek.com>
2016-09-01 10:47:29 +0800

    net: ethernet: mediatek: fix API usage with skb_free_frag
    use skb_free_frag() instead of legacy put_page()
    
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Acked-by: John Crispin <john@phrozen.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mediatek/mtk_eth_soc.c
+++ drivers/net/ethernet/mediatek/mtk_eth_soc.c
@@ -870,7 +870,7 @@ static int mtk_poll_rx(struct napi_struct *napi, int budget,
 		/* receive data */
 		skb = build_skb(data, ring->frag_size);
 		if (unlikely(!skb)) {
-			put_page(virt_to_head_page(new_data));
+			skb_free_frag(new_data);
 			netdev->stats.rx_dropped++;
 			goto release_desc;
 		}
