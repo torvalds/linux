e994608150803215f01adc2c699dabee9d295191
Laura Abbott <labbott@redhat.com>
2016-09-01 15:40:41 -0700

    staging: android: ion: Drop heap type masks
    There is no advantage to having heap types be a mask. The ion client has
    long since dropped the mask. Drop the notion of heap type masks as well.
    
    Signed-off-by: Laura Abbott <labbott@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/uapi/ion.h
+++ drivers/staging/android/uapi/ion.h
@@ -44,13 +44,7 @@ enum ion_heap_type {
 			       * must be last so device specific heaps always
 			       * are at the end of this enum
 			       */
-	ION_NUM_HEAPS = 16,
 };
-
-#define ION_HEAP_SYSTEM_MASK		(1 << ION_HEAP_TYPE_SYSTEM)
-#define ION_HEAP_SYSTEM_CONTIG_MASK	(1 << ION_HEAP_TYPE_SYSTEM_CONTIG)
-#define ION_HEAP_CARVEOUT_MASK		(1 << ION_HEAP_TYPE_CARVEOUT)
-#define ION_HEAP_TYPE_DMA_MASK		(1 << ION_HEAP_TYPE_DMA)
 
 #define ION_NUM_HEAP_IDS		(sizeof(unsigned int) * 8)
 
