5552aad35d439ad96ae55f0e3db25b6268170259
Fabien Dessenne <fabien.dessenne@st.com>
2016-09-06 09:41:48 +0200

    drm/sti: fix atomic_disable check
    When a drm_plane is being disabled, its ->crtc member is set to NULL
    before the .atomic_disable() func is called.
    To get the crtc of the plane, read old_state->crtc instead of
    drm_plane->crtc
    
    Signed-off-by: Fabien Dessenne <fabien.dessenne@st.com>
    Acked-by: Vincent Abriou <vincent.abriou@st.com>

--- drivers/gpu/drm/sti/sti_hqvdp.c
+++ drivers/gpu/drm/sti/sti_hqvdp.c
@@ -1215,15 +1215,15 @@ static void sti_hqvdp_atomic_disable(struct drm_plane *drm_plane,
 {
 	struct sti_plane *plane = to_sti_plane(drm_plane);
 
-	if (!drm_plane->crtc) {
+	if (!oldstate->crtc) {
 		DRM_DEBUG_DRIVER("drm plane:%d not enabled\n",
 				 drm_plane->base.id);
 		return;
 	}
 
 	DRM_DEBUG_DRIVER("CRTC:%d (%s) drm plane:%d (%s)\n",
-			 drm_plane->crtc->base.id,
-			 sti_mixer_to_str(to_sti_mixer(drm_plane->crtc)),
+			 oldstate->crtc->base.id,
+			 sti_mixer_to_str(to_sti_mixer(oldstate->crtc)),
 			 drm_plane->base.id, sti_plane_to_str(plane));
 
 	plane->status = STI_PLANE_DISABLING;
