466fd24b681c734640ebefb1e611df0a5f10d011
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2016-09-06 23:38:48 +0200

    net: stmmac: update the module description of the dwmac-meson driver
    The dwmac-meson glue driver supports Meson6 and Meson8 SoCs. Newer SoCs
    are supported by the dwmac-meson8b driver.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Signed-off-by: Kevin Hilman <khilman@baylibre.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-meson.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-meson.c
@@ -1,5 +1,5 @@
 /*
- * Amlogic Meson DWMAC glue layer
+ * Amlogic Meson6 and Meson8 DWMAC glue layer
  *
  * Copyright (C) 2014 Beniamino Galvani <b.galvani@gmail.com>
  *
@@ -96,5 +96,5 @@ static struct platform_driver meson6_dwmac_driver = {
 module_platform_driver(meson6_dwmac_driver);
 
 MODULE_AUTHOR("Beniamino Galvani <b.galvani@gmail.com>");
-MODULE_DESCRIPTION("Amlogic Meson DWMAC glue layer");
+MODULE_DESCRIPTION("Amlogic Meson6 and Meson8 DWMAC glue layer");
 MODULE_LICENSE("GPL v2");
