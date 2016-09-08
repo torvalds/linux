2668c6195685f4b6f281767d10b4f4f2e32c2305
Harry Pan <harry.pan@intel.com>
2016-09-08 17:08:57 +0800

    perf/x86/rapl: Enable Apollo Lake RAPL support
    This patch enables RAPL counters (energy consumption counters)
    support for Intel Apollo Lake (Goldmont) processors (Model 92):
    
    RAPL of Goldmont, unlikes ESU increment of Silvermont/Airmont,
    it likes the Haswell microarchitecture in 1/2^ESU joules and
    supports power domains in PP0/PP1/PKG/RAM.
    
    ESU and power domains refer to Intel Software Developers' Manual,
    Vol. 3C, Order No. 325384, Table 35-12.
    
    Usage example:
    
      $ perf list
      $ perf stat -a -e power/energy-cores/,power/energy-pkg/ sleep 10
    
    Signed-off-by: Harry Pan <harry.pan@intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vince Weaver <vincent.weaver@maine.edu>
    Cc: bp@alien8.de
    Cc: gs0622@gmail.com
    Cc: hpa@zytor.com
    Cc: srinivas.pandruvada@linux.intel.com
    Link: http://lkml.kernel.org/r/1473325738-730-1-git-send-email-harry.pan@intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/events/intel/rapl.c
+++ arch/x86/events/intel/rapl.c
@@ -767,6 +767,8 @@ static const struct x86_cpu_id rapl_cpu_match[] __initconst = {
 	X86_RAPL_MODEL_MATCH(INTEL_FAM6_SKYLAKE_MOBILE,  skl_rapl_init),
 	X86_RAPL_MODEL_MATCH(INTEL_FAM6_SKYLAKE_DESKTOP, skl_rapl_init),
 	X86_RAPL_MODEL_MATCH(INTEL_FAM6_SKYLAKE_X,	 hsx_rapl_init),
+
+	X86_RAPL_MODEL_MATCH(INTEL_FAM6_ATOM_GOLDMONT, hsw_rapl_init),
 	{},
 };
 
