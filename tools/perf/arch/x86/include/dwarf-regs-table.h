7a023fd239498edd269838784f12888147d970da
Arnaldo Carvalho de Melo <acme@redhat.com>
2016-09-09 12:00:08 -0300

    perf probe: Fix dwarf regs table for x86_64
    In 293d5b439483 ("perf probe: Support probing on offline cross-arch binary")
    DWARF register tables were introduced for many architectures, with the one for
    the "dx" register being broken for x86_64, which got noticed by the 'perf test
    bpf' testcase, that has this difference from a successful run to one that
    fails, with the aforementioned patch:
    
      -Writing event: p:perf_bpf_probe/func _text+5197232 f_mode=+68(%di):x32 offset=%si:s64 orig=dx:s32
      -Failed to write event: Invalid argument
      -bpf_probe: failed to apply perf probe eventsFailed to add events selected by BPF
      +Writing event: p:perf_bpf_probe/func _text+5197232 f_mode=+68(%di):x32 offset=%si:s64 orig=%dx:s32
    
    Add the missing '%' to '%dx' to fix this.
    
    Acked-by: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Fixes: 293d5b439483 ("perf probe: Support probing on offline cross-arch binary")
    Link: https://lkml.kernel.org/r/20160909145955.GC32585@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/arch/x86/include/dwarf-regs-table.h
+++ tools/perf/arch/x86/include/dwarf-regs-table.h
@@ -7,7 +7,7 @@ static const char * const x86_32_regstr_tbl[] = {
 };
 
 static const char * const x86_64_regstr_tbl[] = {
-	"%ax", "dx", "%cx", "%bx", "%si", "%di",
+	"%ax", "%dx", "%cx", "%bx", "%si", "%di",
 	"%bp", "%sp", "%r8", "%r9", "%r10", "%r11",
 	"%r12", "%r13", "%r14", "%r15",
 };
