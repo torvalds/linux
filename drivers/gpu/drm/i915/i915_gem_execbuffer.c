851ba2d6977d6ada31366ae0b791a69132f2c8a7
Chris Wilson <chris@chris-wilson.co.uk>
2016-09-09 14:12:01 +0100

    drm/i915: Serialise execbuf operation after a dma-buf reservation object
    Now that we can wait upon fences before emitting the request, it becomes
    trivial to wait upon any implicit fence provided by the dma-buf
    reservation object.
    
    To protect against failure, we force any asynchronous waits on a foreign
    fence to timeout after 10s - so that a stall in another driver does not
    permanently cripple ourselves. Still unpleasant though!
    
    Testcase: igt/prime_vgem/fence-wait
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: John Harrison <john.c.harrison@intel.com>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20160909131201.16673-21-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_gem_execbuffer.c
+++ drivers/gpu/drm/i915/i915_gem_execbuffer.c
@@ -1131,11 +1131,22 @@ i915_gem_execbuffer_move_to_gpu(struct drm_i915_gem_request *req,
 
 	list_for_each_entry(vma, vmas, exec_list) {
 		struct drm_i915_gem_object *obj = vma->obj;
+		struct reservation_object *resv;
 
 		if (obj->flags & other_rings) {
 			ret = i915_gem_request_await_object
 				(req, obj, obj->base.pending_write_domain);
 			if (ret)
+				return ret;
+		}
+
+		resv = i915_gem_object_get_dmabuf_resv(obj);
+		if (resv) {
+			ret = i915_sw_fence_await_reservation
+				(&req->submit, resv, &i915_fence_ops,
+				 obj->base.pending_write_domain, 10*HZ,
+				 GFP_KERNEL | __GFP_NOWARN);
+			if (ret < 0)
 				return ret;
 		}
 
