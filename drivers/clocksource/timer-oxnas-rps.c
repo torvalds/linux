2ea3401e2a84eed3f5f55b2075706f88df160d85
Neil Armstrong <narmstrong@baylibre.com>
2016-09-09 15:26:08 +0200

    clocksource/drivers/oxnas: Add OX820 compatible
    In order to support the Oxford Semiconductor OX820 SoC, add new
    compatible string to rps timer driver.
    Also add new string in the dt-bindings.
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- drivers/clocksource/timer-oxnas-rps.c
+++ drivers/clocksource/timer-oxnas-rps.c
@@ -295,3 +295,5 @@ err_alloc:
 
 CLOCKSOURCE_OF_DECLARE(ox810se_rps,
 		       "oxsemi,ox810se-rps-timer", oxnas_rps_timer_init);
+CLOCKSOURCE_OF_DECLARE(ox820_rps,
+		       "oxsemi,ox820se-rps-timer", oxnas_rps_timer_init);
