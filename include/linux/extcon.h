c7914e8dfa4032d24ef7af4c86b9c841ec6b74e6
Chris Zhong <zyw@rock-chips.com>
2016-09-09 19:15:44 -0700

    extcon: Introduce EXTCON_PROP_DISP_HPD property
    EXTCON_PROP_DISP_HPD is need by display port, if the system has no hpd
    interrupt, this property can be used.
    - HPD (Hot Plug Detect) send the signal whether display device is on or off
      to source device.
    
    Signed-off-by: Chris Zhong <zyw@rock-chips.com>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    [cw00.choi: Add the description of HPD and full name of HPD]
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- include/linux/extcon.h
+++ include/linux/extcon.h
@@ -133,9 +133,20 @@
 #define EXTCON_PROP_JACK_MAX		100
 #define EXTCON_PROP_JACK_CNT (EXTCON_PROP_JACK_MAX - EXTCON_PROP_JACK_MIN + 1)
 
+/*
+ * Properties of EXTCON_TYPE_DISP.
+ *
+ * - EXTCON_PROP_DISP_HPD (Hot Plug Detect)
+ * @type:       integer (intval)
+ * @value:      0 (no hpd) or 1 (hpd)
+ * @default:    0 (no hpd)
+ *
+ */
+#define EXTCON_PROP_DISP_HPD		150
+
 /* Properties of EXTCON_TYPE_DISP. */
 #define EXTCON_PROP_DISP_MIN		150
-#define EXTCON_PROP_DISP_MAX		150
+#define EXTCON_PROP_DISP_MAX		151
 #define EXTCON_PROP_DISP_CNT (EXTCON_PROP_DISP_MAX - EXTCON_PROP_DISP_MIN + 1)
 
 /*
