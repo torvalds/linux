0152b5b2a519406e10f91cdb3f1ab7d507f49783
Anchal Jain <anchalj109@gmail.com>
2016-09-13 16:34:53 +0530

    staging: media: lirc: lirc_imon: Remove multiple blank lines
    checkpatch complains about multiple blank lines.
    
    Signed-off-by: Anchal Jain <anchalj109@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/media/lirc/lirc_imon.c
+++ drivers/staging/media/lirc/lirc_imon.c
@@ -32,7 +32,6 @@
 #include <media/lirc.h>
 #include <media/lirc_dev.h>
 
-
 #define MOD_AUTHOR	"Venky Raju <dev@venky.ws>"
 #define MOD_DESC	"Driver for SoundGraph iMON MultiMedia IR/Display"
 #define MOD_NAME	"lirc_imon"
