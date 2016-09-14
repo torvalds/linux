d769079b08da16c614849362199cc075f5a1fda2
Sandhya Bankar <bankarsandhya512@gmail.com>
2016-09-14 17:37:51 +0530

    Staging: sm750fb: Remove unnecessary blank line
    Remove unnecessary blank line.
    This patch is detected by checkpatch.pl script .
    CHECK: Blank lines aren't necessary before a close brace '}'
    
    Signed-off-by: Sandhya Bankar <bankarsandhya512@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/ddk750_display.c
+++ drivers/staging/sm750fb/ddk750_display.c
@@ -137,7 +137,6 @@ static void swPanelPowerSequence(int disp, int delay)
 	reg |= (disp ? PANEL_DISPLAY_CTRL_FPEN : 0);
 	POKE32(PANEL_DISPLAY_CTRL, reg);
 	primaryWaitVerticalSync(delay);
-
 }
 
 void ddk750_setLogicalDispOut(disp_output_t output)
@@ -162,7 +161,6 @@ void ddk750_setLogicalDispOut(disp_output_t output)
 		/*se blank off */
 		reg &= ~CRT_DISPLAY_CTRL_BLANK;
 		POKE32(CRT_DISPLAY_CTRL, reg);
-
 	}
 
 	if (output & PRI_TP_USAGE) {
