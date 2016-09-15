2835d2d9e366a2985b24051d228333bfba82f3a7
Ivan Vecera <ivecera@redhat.com>
2016-09-15 22:47:51 +0200

    bna: add missing per queue ethtool stat
    Commit ba5ca784 "bna: check for dma mapping errors" added besides other
    things a statistic that counts number of DMA buffer mapping failures
    per each Rx queue. This counter is not included in ethtool stats output.
    
    Fixes: ba5ca784 "bna: check for dma mapping errors"
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/brocade/bna/bnad_ethtool.c
+++ drivers/net/ethernet/brocade/bna/bnad_ethtool.c
@@ -31,7 +31,7 @@
 #define BNAD_NUM_TXF_COUNTERS 12
 #define BNAD_NUM_RXF_COUNTERS 10
 #define BNAD_NUM_CQ_COUNTERS (3 + 5)
-#define BNAD_NUM_RXQ_COUNTERS 6
+#define BNAD_NUM_RXQ_COUNTERS 7
 #define BNAD_NUM_TXQ_COUNTERS 5
 
 #define BNAD_ETHTOOL_STATS_NUM						\
@@ -658,6 +658,8 @@ bnad_get_strings(struct net_device *netdev, u32 stringset, u8 *string)
 				string += ETH_GSTRING_LEN;
 				sprintf(string, "rxq%d_allocbuf_failed", q_num);
 				string += ETH_GSTRING_LEN;
+				sprintf(string, "rxq%d_mapbuf_failed", q_num);
+				string += ETH_GSTRING_LEN;
 				sprintf(string, "rxq%d_producer_index", q_num);
 				string += ETH_GSTRING_LEN;
 				sprintf(string, "rxq%d_consumer_index", q_num);
@@ -677,6 +679,9 @@ bnad_get_strings(struct net_device *netdev, u32 stringset, u8 *string)
 					string += ETH_GSTRING_LEN;
 					sprintf(string, "rxq%d_allocbuf_failed",
 								q_num);
+					string += ETH_GSTRING_LEN;
+					sprintf(string, "rxq%d_mapbuf_failed",
+						q_num);
 					string += ETH_GSTRING_LEN;
 					sprintf(string, "rxq%d_producer_index",
 								q_num);
