38fdb8d95f8496f0db4fb1071998d27a35ba16a8
Vincent Abriou <vincent.abriou@st.com>
2016-09-15 17:11:07 +0200

    drm/sti: fix compositor debugfs creation
    Fix typo and issue while creating the vid and mixer debugfs entries.
    
    Signed-off-by: Vincent Abriou <vincent.abriou@st.com>
    Acked-by: Benjamin Gaignard <benjamin.gaignard@linaro.org>

--- drivers/gpu/drm/sti/sti_compositor.c
+++ drivers/gpu/drm/sti/sti_compositor.c
@@ -55,22 +55,18 @@ static const struct sti_compositor_data stih416_compositor_data = {
 	},
 };
 
-int sti_compositor_debufs_init(struct sti_compositor *compo,
-			       struct drm_minor *minor)
+int sti_compositor_debugfs_init(struct sti_compositor *compo,
+				struct drm_minor *minor)
 {
-	int ret = 0, i;
+	unsigned int i;
 
-	for (i = 0; compo->vid[i]; i++) {
-		ret = vid_debugfs_init(compo->vid[i], minor);
-		if (ret)
-			return ret;
-	}
+	for (i = 0; i < STI_MAX_VID; i++)
+		if (compo->vid[i])
+			vid_debugfs_init(compo->vid[i], minor);
 
-	for (i = 0; compo->mixer[i]; i++) {
-		ret = sti_mixer_debugfs_init(compo->mixer[i], minor);
-		if (ret)
-			return ret;
-	}
+	for (i = 0; i < STI_MAX_MIXER; i++)
+		if (compo->mixer[i])
+			sti_mixer_debugfs_init(compo->mixer[i], minor);
 
 	return 0;
 }
