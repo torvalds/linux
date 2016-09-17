27d0fc431c6b4847231c1490fa541bc3f5a7a351
David Howells <dhowells@redhat.com>
2016-09-17 10:49:13 +0100

    rxrpc: Don't transmit an ACK if there's no reason set
    Don't transmit an ACK if call->ackr_reason in unset.  There's the
    possibility of a race between recvmsg() sending an ACK and the background
    processing thread trying to send the same one.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- net/rxrpc/output.c
+++ net/rxrpc/output.c
@@ -137,6 +137,11 @@ int rxrpc_send_call_packet(struct rxrpc_call *call, u8 type)
 	switch (type) {
 	case RXRPC_PACKET_TYPE_ACK:
 		spin_lock_bh(&call->lock);
+		if (!call->ackr_reason) {
+			spin_unlock_bh(&call->lock);
+			ret = 0;
+			goto out;
+		}
 		n = rxrpc_fill_out_ack(call, pkt);
 		call->ackr_reason = 0;
 
