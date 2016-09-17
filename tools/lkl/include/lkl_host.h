6fc0b787106f2c48d74e0b56876e78f1188293f5
Octavian Purdila <tavi@cs.pub.ro>
2016-09-17 22:25:00 +0300

    lkl tools: virtio: fix kernel-doc comments
    Update a few comments to make them kernel-doc compatible.
    
    Signed-off-by: Octavian Purdila <tavi@cs.pub.ro>

--- tools/lkl/include/lkl_host.h
+++ tools/lkl/include/lkl_host.h
@@ -13,7 +13,7 @@ extern struct lkl_host_operations lkl_host_ops;
 /**
  * lkl_printf - print a message via the host print operation
  *
- * @fmt - printf like format string
+ * @fmt: printf like format string
  */
 int lkl_printf(const char *fmt, ...);
 
@@ -80,21 +80,25 @@ struct lkl_netdev {
 	uint8_t has_vnet_hdr: 1;
 };
 
+/**
+ * struct lkl_dev_net_ops - network device host operations
+ */
 struct lkl_dev_net_ops {
-	/*
-	 * Writes a L2 packet into the net device.
+	/**
+	 * @tx: writes a L2 packet into the net device
 	 *
 	 * The data buffer can only hold 0 or 1 complete packets.
 	 *
-	 * @nd - pointer to the network device
-	 * @iov - pointer to the buffer vector
+	 * @nd - pointer to the network device;
+	 * @iov - pointer to the buffer vector;
 	 * @cnt - # of vectors in iov.
+	 *
 	 * @returns number of bytes transmitted
 	 */
 	int (*tx)(struct lkl_netdev *nd, struct iovec *iov, int cnt);
 
-	/*
-	 * Reads a packet from the net device.
+	/**
+	 * @rx: reads a packet from the net device.
 	 *
 	 * It must only read one complete packet if present.
 	 *
@@ -104,6 +108,7 @@ struct lkl_dev_net_ops {
 	 * @nd - pointer to the network device
 	 * @iov - pointer to the buffer vector to store the packet
 	 * @cnt - # of vectors in iov.
+	 *
 	 * @returns number of bytes read for success or < 0 if error
 	 */
 	int (*rx)(struct lkl_netdev *nd, struct iovec *iov, int cnt);
@@ -112,28 +117,36 @@ struct lkl_dev_net_ops {
 #define LKL_DEV_NET_POLL_TX		2
 #define LKL_DEV_NET_POLL_HUP		4
 
-	/*
-	 * Polls a net device.
+	/**
+	 * @poll: polls a net device
 	 *
-	 * Supports the following events: LKL_DEV_NET_POLL_RX (readable),
-	 * LKL_DEV_NET_POLL_TX (writable) or LKL_DEV_NET_POLL_HUP (the close
-	 * operations has been issued and we need to clean up). Blocks until one
-	 * event is available.
+	 * Supports the following events: LKL_DEV_NET_POLL_RX
+	 * (readable), LKL_DEV_NET_POLL_TX (writable) or
+	 * LKL_DEV_NET_POLL_HUP (the close operations has been issued
+	 * and we need to clean up). Blocks until one event is
+	 * available.
 	 *
 	 * @nd - pointer to the network device
+	 *
+	 * @returns - LKL_DEV_NET_POLL_RX, LKL_DEV_NET_POLL_TX,
+	 * LKL_DEV_NET_POLL_HUP or a negative value for errors
 	 */
 	int (*poll)(struct lkl_netdev *nd);
 
-	/*
-	 * Make poll wakeup and return LKL_DEV_NET_POLL_HUP.
+	/**
+	 * @poll_hup: make poll wakeup and return LKL_DEV_NET_POLL_HUP
+	 *
+	 * @nd - pointer to the network device
 	 */
 	void (*poll_hup)(struct lkl_netdev *nd);
 
-	/*
-	 * Frees a network device.
+	/**
+	 * @free: frees a network device
 	 *
 	 * Implementation must release its resources and free the network device
 	 * structure.
+	 *
+	 * @nd - pointer to the network device
 	 */
 	void (*free)(struct lkl_netdev *nd);
 };
