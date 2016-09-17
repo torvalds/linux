e7c3c6462406a4de7956cfd8383eb0f2fea1bb6b
Al Viro <viro@zeniv.linux.org.uk>
2016-09-17 18:57:42 -0400

    splice_to_pipe(): don't open-code wakeup_pipe_readers()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/splice.c
+++ fs/splice.c
@@ -242,10 +242,7 @@ ssize_t splice_to_pipe(struct pipe_inode_info *pipe,
 		}
 
 		if (do_wakeup) {
-			smp_mb();
-			if (waitqueue_active(&pipe->wait))
-				wake_up_interruptible_sync(&pipe->wait);
-			kill_fasync(&pipe->fasync_readers, SIGIO, POLL_IN);
+			wakeup_pipe_readers(pipe);
 			do_wakeup = 0;
 		}
 
