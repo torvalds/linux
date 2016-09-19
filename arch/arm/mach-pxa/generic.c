f4e14edf25661f32c9e7323d8a482b8632a6f47f
Robert Jarzmik <robert.jarzmik@free.fr>
2016-09-19 21:12:13 +0200

    ARM: sa11x0/pxa: acquire timer rate from the clock rate
    As both pxa and sa1100 provide a clock to the timer, the rate can be
    inferred from the clock rather than hard encoded in a functional call.
    
    This patch changes the pxa timer to have a mandatory clock which is used
    as the timer rate.
    
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Acked-by: Russell King <rmk+kernel@armlinux.org.uk>

--- arch/arm/mach-pxa/generic.c
+++ arch/arm/mach-pxa/generic.c
@@ -69,8 +69,7 @@ void __init pxa_timer_init(void)
 		pxa27x_clocks_init();
 	if (cpu_is_pxa3xx())
 		pxa3xx_clocks_init();
-	pxa_timer_nodt_init(IRQ_OST0, io_p2v(0x40a00000),
-			    get_clock_tick_rate());
+	pxa_timer_nodt_init(IRQ_OST0, io_p2v(0x40a00000));
 }
 
 /*
