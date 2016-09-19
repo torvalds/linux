18d4689643b08c61e15b11bc78830e1d6d28df63
Torsten Polle <tpolle@de.adit-jv.com>
2016-09-19 10:05:41 +0200

    usb: gadget: u_ether: link socket buffers to the device for received packets
    Socket buffers should be linked to the (network) device that allocated
    the buffers. __netdev_alloc_skb performs this task.
    
    Signed-off-by: Torsten Polle <tpolle@de.adit-jv.com>
    Signed-off-by: Jim Baxter <jim_baxter@mentor.com>
    Signed-off-by: Harish Jenny K N <harish_kandiga@mentor.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/gadget/function/u_ether.c
+++ drivers/usb/gadget/function/u_ether.c
@@ -224,7 +224,7 @@ rx_submit(struct eth_dev *dev, struct usb_request *req, gfp_t gfp_flags)
 	if (dev->port_usb->is_fixed)
 		size = max_t(size_t, size, dev->port_usb->fixed_out_len);
 
-	skb = alloc_skb(size + NET_IP_ALIGN, gfp_flags);
+	skb = __netdev_alloc_skb(dev->net, size + NET_IP_ALIGN, gfp_flags);
 	if (skb == NULL) {
 		DBG(dev, "no rx skb\n");
 		goto enomem;
