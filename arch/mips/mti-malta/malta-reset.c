10b6ea0959de2c42b9f105c88b51a54c4dd961c2
Paul Burton <paul.burton@imgtec.com>
2016-09-19 22:21:28 +0100

    MIPS: Malta: Use syscon-reboot driver to reboot
    Make use of the generic syscon-reboot driver to reboot the Malta board,
    reducing the amount of platform code it requires.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>
    Cc: Stephan Linz <linz@li-pro.net>
    Cc: Jacek Anaszewski <j.anaszewski@samsung.com>
    Cc: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Cc: Rob Herring <robh+dt@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: linux-mips@linux-mips.org
    Cc: devicetree@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Patchwork: https://patchwork.linux-mips.org/patch/14279/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mti-malta/malta-reset.c
+++ arch/mips/mti-malta/malta-reset.c
@@ -8,20 +8,10 @@
  */
 #include <linux/io.h>
 #include <linux/pm.h>
+#include <linux/reboot.h>
 
 #include <asm/reboot.h>
 #include <asm/mach-malta/malta-pm.h>
-
-#define SOFTRES_REG	0x1f000500
-#define GORESET		0x42
-
-static void mips_machine_restart(char *command)
-{
-	unsigned int __iomem *softres_reg =
-		ioremap(SOFTRES_REG, sizeof(unsigned int));
-
-	__raw_writel(GORESET, softres_reg);
-}
 
 static void mips_machine_halt(void)
 {
@@ -33,12 +23,11 @@ static void mips_machine_power_off(void)
 	mips_pm_suspend(PIIX4_FUNC3IO_PMCNTRL_SUS_TYP_SOFF);
 
 	pr_info("Failed to power down, resetting\n");
-	mips_machine_restart(NULL);
+	machine_restart(NULL);
 }
 
 static int __init mips_reboot_setup(void)
 {
-	_machine_restart = mips_machine_restart;
 	_machine_halt = mips_machine_halt;
 	pm_power_off = mips_machine_power_off;
 
