9e86c7d3b1010a8a9bb603926a4c95e61e4bba26
Jeff Vander Stoep <jeffv@google.com>
2016-09-18 21:39:28 -0700

    ANDROID: fiq_debugger: Pass task parameter to unwind_frame()
    Fixes: fe13f95b7200 ("arm64: pass a task parameter to unwind_frame()")
    
    Bug: 30369029
    Patchset: rework-pagetable
    
    Change-Id: I9a4ab50ef61532d27282f189f063c938c196ec08
    Signed-off-by: Jeff Vander Stoep <jeffv@google.com>

--- drivers/staging/android/fiq_debugger/fiq_debugger_arm64.c
+++ drivers/staging/android/fiq_debugger/fiq_debugger_arm64.c
@@ -197,6 +197,6 @@ void fiq_debugger_dump_stacktrace(struct fiq_debugger_output *output,
 		frame.sp = regs->sp;
 		frame.pc = regs->pc;
 		output->printf(output, "\n");
-		walk_stackframe(&frame, report_trace, &sts);
+		walk_stackframe(current, &frame, report_trace, &sts);
 	}
 }
