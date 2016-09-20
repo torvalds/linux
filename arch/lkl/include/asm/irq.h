f640ba8619fec8dd12ca54217944c00dcad884bd
Octavian Purdila <tavi@cs.pub.ro>
2016-09-21 01:37:17 +0300

    lkl: increase the number of irqs
    Since we use one irq for each thread and each device, 64/32 irqs is not
    enough in some cases.
    
    To increase the number of irqs and avoid too much overhead we use an
    indirect approach: the irqs are marked using a bitmap (array of longs)
    and a summary of the modified bits is kept in a separate "index" long -
    one bit for each sizeof(long). Thus we can support 4096 irqs on 64bit
    platforms and 1024 irqs on 32bit platforms.
    
    Whenever an irq is trigger both the array and the index is updated. To
    find which irqs were triggered we first search the index and then the
    corresponding part of the arrary.
    
    This limits the number of atomic operations to 2 per irq, in the worst
    case, for both triggering and checking irqs.
    
    Signed-off-by: Octavian Purdila <tavi@cs.pub.ro>

--- arch/lkl/include/asm/irq.h
+++ arch/lkl/include/asm/irq.h
@@ -1,7 +1,8 @@
 #ifndef _ASM_LKL_IRQ_H
 #define _ASM_LKL_IRQ_H
 
-#define NR_IRQS			((int)sizeof(long) * 8)
+#define IRQ_STATUS_BITS		(sizeof(long) * 8)
+#define NR_IRQS			((int)IRQ_STATUS_BITS * IRQ_STATUS_BITS)
 
 #include <uapi/asm/irq.h>
 
