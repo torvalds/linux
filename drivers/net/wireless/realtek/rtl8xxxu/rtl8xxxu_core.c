b42fbed6b8a5942e9f76ec8c7f9c9fd798a2d3af
Larry Finger <Larry.Finger@lwfinger.net>
2016-09-20 21:19:29 -0400

    rtl8xxxu: Stop log spam from each successful interrupt
    As soon as debugging is turned on, the logs are filled with messages
    reporting the interrupt status. As this quantity is usually zero, this
    output is not needed. In fact, there will be a report if the status is
    not zero, thus the debug line in question could probably be deleted.
    Rather than taking that action, I have changed it to only be printed
    when the newly added RTL8XXXU_DEBUG_INTERRUPT bit is set in the debug
    mask.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_core.c
+++ drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_core.c
@@ -5375,7 +5375,8 @@ static void rtl8xxxu_int_complete(struct urb *urb)
 	struct device *dev = &priv->udev->dev;
 	int ret;
 
-	dev_dbg(dev, "%s: status %i\n", __func__, urb->status);
+	if (rtl8xxxu_debug & RTL8XXXU_DEBUG_INTERRUPT)
+		dev_dbg(dev, "%s: status %i\n", __func__, urb->status);
 	if (urb->status == 0) {
 		usb_anchor_urb(urb, &priv->int_anchor);
 		ret = usb_submit_urb(urb, GFP_ATOMIC);
