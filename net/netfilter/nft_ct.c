7bfdde7045ad54d9fdccac70baffd094d9de73f8
Liping Zhang <liping.zhang@spreadtrum.com>
2016-09-22 22:28:52 +0800

    netfilter: nft_ct: report error if mark and dir specified simultaneously
    NFT_CT_MARK is unrelated to direction, so if NFTA_CT_DIRECTION attr is
    specified, report EINVAL to the userspace. This validation check was
    already done at nft_ct_get_init, but we missed it in nft_ct_set_init.
    
    Signed-off-by: Liping Zhang <liping.zhang@spreadtrum.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nft_ct.c
+++ net/netfilter/nft_ct.c
@@ -364,6 +364,8 @@ static int nft_ct_set_init(const struct nft_ctx *ctx,
 	switch (priv->key) {
 #ifdef CONFIG_NF_CONNTRACK_MARK
 	case NFT_CT_MARK:
+		if (tb[NFTA_CT_DIRECTION])
+			return -EINVAL;
 		len = FIELD_SIZEOF(struct nf_conn, mark);
 		break;
 #endif
