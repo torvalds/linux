caf9e93b848d25d11fd6ae7a21b9dfca0086de06
Octavian Purdila <tavi@cs.pub.ro>
2016-09-23 23:26:35 +0300

    lkl: fix irq warning
    In file included from include/linux/printk.h:6:0,
                     from include/linux/kernel.h:13,
                     from include/linux/list.h:8,
                     from include/linux/smp.h:11,
                     from include/linux/irq.h:12,
                     from kernel/irq/irqdesc.c:10:
    kernel/irq/irqdesc.c: In function ‘early_irq_init’:
    include/linux/kern_levels.h:4:18: warning: format ‘%d’ expects argument
    of type ‘int’, but argument 2 has type ‘long unsigned int’ [-Wformat=]
                      ^
    include/linux/kern_levels.h:13:19: note: in expansion of macro
    ‘KERN_SOH’
                       ^
    kernel/irq/irqdesc.c:304:9: note: in expansion of macro ‘KERN_INFO’
    printk(KERN_INFO "NR_IRQS:%d\n", NR_IRQS);
             ^
    
    Signed-off-by: Octavian Purdila <tavi@cs.pub.ro>

--- arch/lkl/include/asm/irq.h
+++ arch/lkl/include/asm/irq.h
@@ -2,7 +2,7 @@
 #define _ASM_LKL_IRQ_H
 
 #define IRQ_STATUS_BITS		(sizeof(long) * 8)
-#define NR_IRQS			((int)IRQ_STATUS_BITS * IRQ_STATUS_BITS)
+#define NR_IRQS			((int)(IRQ_STATUS_BITS * IRQ_STATUS_BITS))
 
 #include <uapi/asm/irq.h>
 
