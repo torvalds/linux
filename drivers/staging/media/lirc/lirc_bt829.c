e2ba2cb3278c5f15240ea4c05639a318e4f90128
Namrata A Shettar <namrataashettar@gmail.com>
2016-09-24 21:30:04 +0530

    staging: media: lirc: Remove multiple blank lines
    Remove multiple blank lines to resolve checkpatch issue.
    
    Signed-off-by: Namrata A Shettar <namrataashettar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/media/lirc/lirc_bt829.c
+++ drivers/staging/media/lirc/lirc_bt829.c
@@ -163,7 +163,6 @@ err_put_dev:
 	return rc;
 }
 
-
 void cleanup_module(void)
 {
 	struct pci_dev *pdev = to_pci_dev(atir_driver.dev);
@@ -173,7 +172,6 @@ void cleanup_module(void)
 	pci_disable_device(pdev);
 	pci_dev_put(pdev);
 }
-
 
 static int atir_init_start(void)
 {
@@ -189,7 +187,6 @@ static void cycle_delay(int cycle)
 {
 	udelay(WAIT_CYCLE*cycle);
 }
-
 
 static int poll_main(void)
 {
