d9e1397783765a275c3a7930250dcdb7e9480d7d
Alexander Graf <agraf@suse.de>
2016-09-27 21:08:06 +0200

    KVM: arm/arm64: Support arch timers with a userspace gic
    If you're running with a userspace gic or other interrupt controller
    (that is no vgic in the kernel), then you have so far not been able to
    use the architected timers, because the output of the architected
    timers, which are driven inside the kernel, was a kernel-only construct
    between the arch timer code and the vgic.
    
    This patch implements the new KVM_CAP_ARM_USER_IRQ feature, where we use a
    side channel on the kvm_run structure, run->s.regs.device_irq_level, to
    always notify userspace of the timer output levels when using a userspace
    irqchip.
    
    This works by ensuring that before we enter the guest, if the timer
    output level has changed compared to what we last told userspace, we
    don't enter the guest, but instead return to userspace to notify it of
    the new level.  If we are exiting, because of an MMIO for example, and
    the level changed at the same time, the value is also updated and
    userspace can sample the line as it needs.  This is nicely achieved
    simply always updating the timer_irq_level field after the main run
    loop.
    
    Note that the kvm_timer_update_irq trace event is changed to show the
    host IRQ number for the timer instead of the guest IRQ number, because
    the kernel no longer know which IRQ userspace wires up the timer signal
    to.
    
    Also note that this patch implements all required functionality but does
    not yet advertise the capability.
    
    Reviewed-by: Alexander Graf <agraf@suse.de>
    Reviewed-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Alexander Graf <agraf@suse.de>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- arch/arm/kvm/arm.c
+++ arch/arm/kvm/arm.c
@@ -515,13 +515,7 @@ static int kvm_vcpu_first_run_init(struct kvm_vcpu *vcpu)
 			return ret;
 	}
 
-	/*
-	 * Enable the arch timers only if we have an in-kernel VGIC
-	 * and it has been properly initialized, since we cannot handle
-	 * interrupts from the virtual timer with a userspace gic.
-	 */
-	if (irqchip_in_kernel(kvm) && vgic_initialized(kvm))
-		ret = kvm_timer_enable(vcpu);
+	ret = kvm_timer_enable(vcpu);
 
 	return ret;
 }
@@ -640,9 +634,12 @@ int kvm_arch_vcpu_ioctl_run(struct kvm_vcpu *vcpu, struct kvm_run *run)
 		local_irq_disable();
 
 		/*
-		 * Re-check atomic conditions
+		 * If we have a singal pending, or need to notify a userspace
+		 * irqchip about timer level changes, then we exit (and update
+		 * the timer level state in kvm_timer_update_run below).
 		 */
-		if (signal_pending(current)) {
+		if (signal_pending(current) ||
+		    kvm_timer_should_notify_user(vcpu)) {
 			ret = -EINTR;
 			run->exit_reason = KVM_EXIT_INTR;
 		}
@@ -713,6 +710,9 @@ int kvm_arch_vcpu_ioctl_run(struct kvm_vcpu *vcpu, struct kvm_run *run)
 
 		ret = handle_exit(vcpu, run, ret);
 	}
+
+	/* Tell userspace about in-kernel device output levels */
+	kvm_timer_update_run(vcpu);
 
 	if (vcpu->sigset_active)
 		sigprocmask(SIG_SETMASK, &sigsaved, NULL);
