de95929c550ba936220048e9935e028f6e035a0d
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2016-09-28 20:19:48 +0200

    staging: wlang-ng: avoid new typedef: hfa384x_pdr_vgdac_measurements_t
    This patch fixes the following checkpatch.pl warning in hfa384x.h:
    WARNING: do not add new typedefs
    
    It applies for typedef hfa384x_pdr_vgdac_measurements_t
    
    Signed-off-by: Sergio Paracuellos <sergio.paracuellos@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/hfa384x.h
+++ drivers/staging/wlan-ng/hfa384x.h
@@ -988,9 +988,9 @@ struct hfa384x_pdr_refdac_measurements {
 	u16 value[0];
 } __packed;
 
-typedef struct hfa384x_pdr_vgdac_measurements {
+struct hfa384x_pdr_vgdac_measurements {
 	u16 value[0];
-} __packed hfa384x_pdr_vgdac_measurements_t;
+} __packed;
 
 typedef struct hfa384x_pdr_level_comp_measurements {
 	u16 value[0];
@@ -1103,7 +1103,7 @@ typedef struct hfa384x_pdrec {
 		struct hfa384x_pdr_cfisuprange cfisuprange;
 		struct hfa384x_pdr_nicid nicid;
 		struct hfa384x_pdr_refdac_measurements refdac_measurements;
-		hfa384x_pdr_vgdac_measurements_t vgdac_measurements;
+		struct hfa384x_pdr_vgdac_measurements vgdac_measurements;
 		hfa384x_pdr_level_compc_measurements_t level_compc_measurements;
 		hfa384x_pdr_mac_address_t mac_address;
 		hfa384x_pdr_mkk_callname_t mkk_callname;
