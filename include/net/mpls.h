9095e10edd28e1e4a10ba5ca61fb54d9f74f8968
Jiri Benc <jbenc@redhat.com>
2016-09-30 19:08:06 +0200

    mpls: move mpls_hdr to a common location
    This will be also used by openvswitch.
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Acked-by: David Ahern <dsa@cumulusnetworks.com>
    Acked-by: Pravin B Shelar <pshelar@ovn.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/mpls.h
+++ include/net/mpls.h
@@ -19,10 +19,19 @@
 
 #define MPLS_HLEN 4
 
+struct mpls_shim_hdr {
+	__be32 label_stack_entry;
+};
+
 static inline bool eth_p_mpls(__be16 eth_type)
 {
 	return eth_type == htons(ETH_P_MPLS_UC) ||
 		eth_type == htons(ETH_P_MPLS_MC);
+}
+
+static inline struct mpls_shim_hdr *mpls_hdr(const struct sk_buff *skb)
+{
+	return (struct mpls_shim_hdr *)skb_network_header(skb);
 }
 
 /*
