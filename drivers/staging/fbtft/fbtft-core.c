fc1e2c8ea85e109acf09e74789e9b852f6eed251
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2016-10-02 17:42:35 +0200

    Staging: fbtft: Fix bug in fbtft-core
    Commit 367e8560e8d7a62d96e9b1d644028a3816e04206 introduced a bug
    in fbtft-core where fps is always 0, this is because variable
    update_time is not assigned correctly.
    
    Signed-off-by: Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
    Fixes: 367e8560e8d7 ("Staging: fbtbt: Replace timespec with ktime_t")
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fbtft/fbtft-core.c
+++ drivers/staging/fbtft/fbtft-core.c
@@ -391,11 +391,11 @@ static void fbtft_update_display(struct fbtft_par *par, unsigned int start_line,
 
 	if (unlikely(timeit)) {
 		ts_end = ktime_get();
-		if (ktime_to_ns(par->update_time))
+		if (!ktime_to_ns(par->update_time))
 			par->update_time = ts_start;
 
-		par->update_time = ts_start;
 		fps = ktime_us_delta(ts_start, par->update_time);
+		par->update_time = ts_start;
 		fps = fps ? 1000000 / fps : 0;
 
 		throughput = ktime_us_delta(ts_end, ts_start);
