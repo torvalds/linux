8dedefbc38172f3fcb43a26b6d0e394dcb1ee562
Stefan Agner <stefan@agner.ch>
2016-10-04 16:40:14 -0700

    drm/fsl-dcu: enable TCON bypass mode by default
    Do not use encoder disable/enable callbacks to control bypass
    mode as this seems to mess with the signals not liked by
    displays. This also makes more sense since the encoder is
    already defined to be parallel RGB/LVDS at creation time.
    
    Signed-off-by: Stefan Agner <stefan@agner.ch>
    Tested-By: Meng Yi <meng.yi@nxp.com>

--- drivers/gpu/drm/fsl-dcu/fsl_dcu_drm_drv.c
+++ drivers/gpu/drm/fsl-dcu/fsl_dcu_drm_drv.c
@@ -273,6 +273,8 @@ static int fsl_dcu_drm_pm_resume(struct device *dev)
 		goto disable_dcu_clk;
 	}
 
+	if (fsl_dev->tcon)
+		fsl_tcon_bypass_enable(fsl_dev->tcon);
 	fsl_dcu_drm_init_planes(fsl_dev->drm);
 	drm_atomic_helper_resume(fsl_dev->drm, fsl_dev->state);
 
