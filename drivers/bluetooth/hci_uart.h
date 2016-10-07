ec6659a06ca2c6880eaec99ab08ae8c6d7a98341
Jacob Chen <jacob2.chen@rock-chips.com>
2016-10-07 09:10:45 +0800

    UPSTREAM: Bluetooth: hci_ldisc: Fix null pointer derefence in case of early data
    HCI_UART_PROTO_SET flag is set before hci_uart_set_proto call. If we
    receive data from tty layer during this procedure, proto pointer may
    not be assigned yet, leading to null pointer dereference in rx method
    hci_uart_tty_receive.
    
    This patch fixes this issue by introducing HCI_UART_PROTO_READY flag in
    order to avoid any proto operation before proto opening and assignment.
    
    Signed-off-by: Loic Poulain <loic.poulain@intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    
    Change-Id: Ibe366f3222cbe7a093cd08aaecbc0de1004088c8
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>
    (cherry picked from commit 84cb3df02aea4b00405521e67c4c67c2d525c364)

--- drivers/bluetooth/hci_uart.h
+++ drivers/bluetooth/hci_uart.h
@@ -94,6 +94,7 @@ struct hci_uart {
 /* HCI_UART proto flag bits */
 #define HCI_UART_PROTO_SET	0
 #define HCI_UART_REGISTERED	1
+#define HCI_UART_PROTO_READY	2
 
 /* TX states  */
 #define HCI_UART_SENDING	1
