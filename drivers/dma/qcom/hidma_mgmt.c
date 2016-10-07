9da0be80be6e7855385ec519c7c13576dfed84c0
Sinan Kaya <okaya@codeaurora.org>
2016-10-07 01:25:09 -0400

    dmaengine: qcom_hidma: configure DMA and MSI for OF
    Configure the DMA bindings for the device tree based firmware.
    
    Signed-off-by: Sinan Kaya <okaya@codeaurora.org>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/qcom/hidma_mgmt.c
+++ drivers/dma/qcom/hidma_mgmt.c
@@ -375,8 +375,15 @@ static int __init hidma_mgmt_of_populate_channels(struct device_node *np)
 			ret = PTR_ERR(new_pdev);
 			goto out;
 		}
+		of_node_get(child);
+		new_pdev->dev.of_node = child;
 		of_dma_configure(&new_pdev->dev, child);
-
+		/*
+		 * It is assumed that calling of_msi_configure is safe on
+		 * platforms with or without MSI support.
+		 */
+		of_msi_configure(&new_pdev->dev, child);
+		of_node_put(child);
 		kfree(res);
 		res = NULL;
 	}
