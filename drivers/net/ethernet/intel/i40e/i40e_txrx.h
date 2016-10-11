1dc8b538795fcf92161cc056ceebb852cf375b17
Alexander Duyck <alexander.h.duyck@intel.com>
2016-10-11 15:26:54 -0700

    i40e: Reorder logic for coalescing RS bits
    This patch reorders the logic at the end of i40e_tx_map to address the
    fact that the logic was rather convoluted and much larger than it needed
    to be.
    
    In order to try and coalesce the code paths I have updated some of the
    comments and repurposed some of the variables in order to reduce
    unnecessary overhead.
    
    This patch does the following:
    1.  Quit tracking skb->xmit_more with a flag, just max out packet_stride
    2.  Drop tail_bump and do_rs and instead just use desc_count and td_cmd
    3.  Pull comments from ixgbe that make need for wmb() more explicit.
    
    Change-ID: Ic7da85ec75043c634e87fef958109789bcc6317c
    Signed-off-by: Alexander Duyck <alexander.h.duyck@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_txrx.h
+++ drivers/net/ethernet/intel/i40e/i40e_txrx.h
@@ -313,7 +313,6 @@ struct i40e_ring {
 
 	u16 flags;
 #define I40E_TXR_FLAGS_WB_ON_ITR	BIT(0)
-#define I40E_TXR_FLAGS_LAST_XMIT_MORE_SET BIT(2)
 
 	/* stats structs */
 	struct i40e_queue_stats	stats;
