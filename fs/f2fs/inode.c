d9d712862732783e16998d11f9b9867d28e282fd
Chao Yu <yuchao0@huawei.com>
2016-10-11 22:56:59 +0800

    f2fs: clear nlink if fail to add_link
    commit a11b9f65eae766b17ec3451a6a1766f0a9d1dbff upstream.
    
    We don't need to keep incomplete created inode in cache, so if we fail to
    add link into directory during new inode creation, it's better to set
    nlink of inode to zero, then we can evict inode immediately. Otherwise
    release of nid belong to inode will be delayed until inode cache is being
    shrunk, it may cause a seemingly endless loop while allocating free nids
    in time of testing generic/269 case of fstest suit.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    [Jaegeuk Kim: add update_inode_page to fix kernel panic]
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/inode.c
+++ fs/f2fs/inode.c
@@ -387,6 +387,8 @@ retry:
 		f2fs_lock_op(sbi);
 		err = remove_inode_page(inode);
 		f2fs_unlock_op(sbi);
+		if (err == -ENOENT)
+			err = 0;
 	}
 
 	/* give more chances, if ENOMEM case */
@@ -426,6 +428,18 @@ void handle_failed_inode(struct inode *inode)
 {
 	struct f2fs_sb_info *sbi = F2FS_I_SB(inode);
 	struct node_info ni;
+
+	/*
+	 * clear nlink of inode in order to release resource of inode
+	 * immediately.
+	 */
+	clear_nlink(inode);
+
+	/*
+	 * we must call this to avoid inode being remained as dirty, resulting
+	 * in a panic when flushing dirty inodes in gdirty_list.
+	 */
+	update_inode_page(inode);
 
 	/* don't make bad inode, since it becomes a regular file. */
 	unlock_new_inode(inode);
