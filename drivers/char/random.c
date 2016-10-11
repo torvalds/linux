99fdafdeacfa99ca9047641b684fa2aaf094a661
Jason Cooper <jason@lakedaemon.net>
2016-10-11 13:53:52 -0700

    random: simplify API for random address requests
    To date, all callers of randomize_range() have set the length to 0, and
    check for a zero return value.  For the current callers, the only way to
    get zero returned is if end <= start.  Since they are all adding a
    constant to the start address, this is unnecessary.
    
    We can remove a bunch of needless checks by simplifying the API to do just
    what everyone wants, return an address between [start, start + range).
    
    While we're here, s/get_random_int/get_random_long/.  No current call site
    is adversely affected by get_random_int(), since all current range
    requests are < UINT_MAX.  However, we should match caller expectations to
    avoid coming up short (ha!) in the future.
    
    All current callers to randomize_range() chose to use the start address if
    randomize_range() failed.  Therefore, we simplify things by just returning
    the start address on error.
    
    randomize_range() will be removed once all callers have been converted
    over to randomize_addr().
    
    Link: http://lkml.kernel.org/r/20160803233913.32511-2-jason@lakedaemon.net
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>
    Acked-by: Kees Cook <keescook@chromium.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: "Roberts, William C" <william.c.roberts@intel.com>
    Cc: Yann Droneaud <ydroneaud@opteya.com>
    Cc: Russell King <linux@arm.linux.org.uk>
    Cc: "Theodore Ts'o" <tytso@mit.edu>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: "H . Peter Anvin" <hpa@zytor.com>
    Cc: Nick Kralevich <nnk@google.com>
    Cc: Jeffrey Vander Stoep <jeffv@google.com>
    Cc: Daniel Cashman <dcashman@android.com>
    Cc: Chris Metcalf <cmetcalf@mellanox.com>
    Cc: Guan Xuetao <gxt@mprc.pku.edu.cn>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -2119,6 +2119,39 @@ randomize_range(unsigned long start, unsigned long end, unsigned long len)
 	return PAGE_ALIGN(get_random_int() % range + start);
 }
 
+/**
+ * randomize_page - Generate a random, page aligned address
+ * @start:	The smallest acceptable address the caller will take.
+ * @range:	The size of the area, starting at @start, within which the
+ *		random address must fall.
+ *
+ * If @start + @range would overflow, @range is capped.
+ *
+ * NOTE: Historical use of randomize_range, which this replaces, presumed that
+ * @start was already page aligned.  We now align it regardless.
+ *
+ * Return: A page aligned address within [start, start + range).  On error,
+ * @start is returned.
+ */
+unsigned long
+randomize_page(unsigned long start, unsigned long range)
+{
+	if (!PAGE_ALIGNED(start)) {
+		range -= PAGE_ALIGN(start) - start;
+		start = PAGE_ALIGN(start);
+	}
+
+	if (start > ULONG_MAX - range)
+		range = ULONG_MAX - start;
+
+	range >>= PAGE_SHIFT;
+
+	if (range == 0)
+		return start;
+
+	return start + (get_random_long() % range << PAGE_SHIFT);
+}
+
 /* Interface for in-kernel drivers of true hardware RNGs.
  * Those devices may produce endless random bits and will be throttled
  * when our pool is full.
