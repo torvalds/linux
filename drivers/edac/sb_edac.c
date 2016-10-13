9a9260ca926c6e7b6bcfd3c93a1820d86565ff4f
Piotr Luc <piotr.luc@intel.com>
2016-10-13 17:30:59 +0200

    EDAC, sb_edac: Add Knights Mill support
    Add Knights Mill (KNM) to the list of CPU models supported by sb_edac.
    
    Signed-off-by: Piotr Luc <piotr.luc@intel.com>
    Reviewed-by: Dave Hansen <dave.hansen@intel.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: linux-edac <linux-edac@vger.kernel.org>
    Link: http://lkml.kernel.org/r/20161013153105.2517-6-piotr.luc@intel.com
    Signed-off-by: Borislav Petkov <bp@suse.de>

--- drivers/edac/sb_edac.c
+++ drivers/edac/sb_edac.c
@@ -3372,6 +3372,7 @@ static const struct x86_cpu_id sbridge_cpuids[] = {
 	ICPU(INTEL_FAM6_BROADWELL_X,	  pci_dev_descr_broadwell_table),
 	ICPU(INTEL_FAM6_BROADWELL_XEON_D, pci_dev_descr_broadwell_table),
 	ICPU(INTEL_FAM6_XEON_PHI_KNL,	  pci_dev_descr_knl_table),
+	ICPU(INTEL_FAM6_XEON_PHI_KNM,	  pci_dev_descr_knl_table),
 	{ }
 };
 MODULE_DEVICE_TABLE(x86cpu, sbridge_cpuids);
