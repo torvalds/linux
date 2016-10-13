be4dd651ff79b9fddc46ef330afeedc6560fd1ca
Dan Carpenter <dan.carpenter@oracle.com>
2016-10-13 11:55:08 +0300

    drm/i915: fix a read size argument
    We want to read 3 bytes here, but because the parenthesis are in the
    wrong place we instead read:
    
    	sizeof(intel_dp->edp_dpcd) == sizeof(intel_dp->edp_dpcd)
    
    which is one byte.
    
    Fixes: fe5a66f91c88 ("drm/i915: Read PSR caps/intermediate freqs/etc. only once on eDP")
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: <drm-intel-fixes@lists.freedesktop.org>
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/20161013085508.GJ16198@mwanda
    (cherry picked from commit f7170e2eb8f6bf7ef2032cc0659cd38740bf5b97)
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -3551,8 +3551,8 @@ intel_edp_init_dpcd(struct intel_dp *intel_dp)
 	/* Read the eDP Display control capabilities registers */
 	if ((intel_dp->dpcd[DP_EDP_CONFIGURATION_CAP] & DP_DPCD_DISPLAY_CONTROL_CAPABLE) &&
 	    drm_dp_dpcd_read(&intel_dp->aux, DP_EDP_DPCD_REV,
-			     intel_dp->edp_dpcd, sizeof(intel_dp->edp_dpcd) ==
-			     sizeof(intel_dp->edp_dpcd)))
+			     intel_dp->edp_dpcd, sizeof(intel_dp->edp_dpcd)) ==
+			     sizeof(intel_dp->edp_dpcd))
 		DRM_DEBUG_KMS("EDP DPCD : %*ph\n", (int) sizeof(intel_dp->edp_dpcd),
 			      intel_dp->edp_dpcd);
 
