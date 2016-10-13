fed9d8f3cd6b7e8dbb697377065ee3b88b6baa45
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2016-10-13 15:42:47 -0300

    [media] stv090x: get rid of continuation lines
    This driver has printk continuation lines for debugging purposes.
    Since commit 563873318d32 ("Merge branch 'printk-cleanups'")',
    this won't work as expected anymore.
    
    So, use %*ph and get rid of it.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/dvb-frontends/stv090x.c
+++ drivers/media/dvb-frontends/stv090x.c
@@ -739,14 +739,8 @@ static int stv090x_write_regs(struct stv090x_state *state, unsigned int reg, u8 
 	buf[1] = reg & 0xff;
 	memcpy(&buf[2], data, count);
 
-	if (unlikely(*state->verbose >= FE_DEBUGREG)) {
-		int i;
-
-		printk(KERN_DEBUG "%s [0x%04x]:", __func__, reg);
-		for (i = 0; i < count; i++)
-			printk(" %02x", data[i]);
-		printk("\n");
-	}
+	dprintk(FE_DEBUGREG, 1, "%s [0x%04x]: %*ph",
+		__func__, reg, count, data);
 
 	ret = i2c_transfer(state->i2c, &i2c_msg, 1);
 	if (ret != 1) {
