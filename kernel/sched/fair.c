810a5b6ce7217b7cb19713eac705cfe88d336a5b
Morten Rasmussen <morten.rasmussen@arm.com>
2016-10-14 14:41:12 +0100

    UPSTREAM: sched/fair: Fix incorrect comment for capacity_margin
    The comment for capacity_margin introduced in:
    
      3273163c6775 ("sched/fair: Let asymmetric CPU configurations balance at wake-up")
    
    ... got its usage the wrong way round - fix it.
    
    Change-Id: Ie46eac3e5ff43397b5bed61d0999d2817f1a1d96
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: dietmar.eggemann@arm.com
    Cc: freedom.tan@mediatek.com
    Cc: keita.kobayashi.ym@renesas.com
    Cc: mgalbraith@suse.de
    Cc: sgurrappadi@nvidia.com
    Cc: vincent.guittot@linaro.org
    Cc: yuyang.du@intel.com
    Link: http://lkml.kernel.org/r/1476452472-24740-7-git-send-email-morten.rasmussen@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 893c5d2279041afeb593f1fa8edd9d02edf5b7cb)
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -130,7 +130,7 @@ unsigned int sysctl_sched_cfs_bandwidth_slice = 5000UL;
 
 /*
  * The margin used when comparing utilization with CPU capacity:
- * util * 1024 < capacity * margin
+ * util * margin < capacity * 1024
  */
 unsigned int capacity_margin = 1280; /* ~20% */
 
