a335e95590acee89e75a21849d943f085c6269b5
Bob Moore <robert.moore@intel.com>
2016-10-18 03:03:39 +0800

    ACPICA: Disassembler: Fix for Divide() support, new support for test suite
    ACPICA commit 4b367408659af08fd44839866ec301285284e6f4
    
    Fixes a problem with complex expressions where an illegal mix
    of legacy ASL and ASL+ could be emitted.
    
    Adds new support for ASLTS that disables some disassembler
    optimizations could be changed during a conversion to ASL+.
    These expressions are now emitted in legacy ASL instead
    of ASL+.
    
    Link: https://github.com/acpica/acpica/commit/4b367408
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpica/acglobal.h
+++ drivers/acpi/acpica/acglobal.h
@@ -318,6 +318,7 @@ ACPI_INIT_GLOBAL(u8, acpi_gbl_cstyle_disassembly, TRUE);
 ACPI_INIT_GLOBAL(u8, acpi_gbl_force_aml_disassembly, FALSE);
 ACPI_INIT_GLOBAL(u8, acpi_gbl_dm_opt_verbose, TRUE);
 ACPI_INIT_GLOBAL(u8, acpi_gbl_dm_emit_external_opcodes, FALSE);
+ACPI_INIT_GLOBAL(u8, acpi_gbl_do_disassembler_optimizations, TRUE);
 
 ACPI_GLOBAL(u8, acpi_gbl_dm_opt_disasm);
 ACPI_GLOBAL(u8, acpi_gbl_dm_opt_listing);
