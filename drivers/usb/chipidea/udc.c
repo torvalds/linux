a09886f7892149e019b2a2184c1729dbc7d088f5
Peter Chen <peter.chen@nxp.com>
2016-10-18 16:32:04 +0800

    MLK-13308-1 usb: chipidea: udc: add USB PHY event
    Add USB PHY event for below situation:
    - vbus connect
    - vbus disconnect
    - gadget driver is enumerated
    
    USB PHY driver can get the last event after above situation
    occurs.
    
    Signed-off-by: Peter Chen <peter.chen@nxp.com>
    (cherry picked from commit 079f8daba9f52036c51b6d9b5e618e110fe86eeb)

--- drivers/usb/chipidea/udc.c
+++ drivers/usb/chipidea/udc.c
@@ -1582,6 +1582,13 @@ static int ci_udc_vbus_session(struct usb_gadget *_gadget, int is_active)
 	/* Charger Detection */
 	ci_usb_charger_connect(ci, is_active);
 
+	if (ci->usb_phy) {
+		if (is_active)
+			usb_phy_set_event(ci->usb_phy, USB_EVENT_VBUS);
+		else
+			usb_phy_set_event(ci->usb_phy, USB_EVENT_NONE);
+	}
+
 	if (gadget_ready)
 		ci_hdrc_gadget_connect(_gadget, is_active);
 
@@ -1862,6 +1869,9 @@ static irqreturn_t udc_irq(struct ci_hdrc *ci)
 		if (USBi_PCI & intr) {
 			ci->gadget.speed = hw_port_is_high_speed(ci) ?
 				USB_SPEED_HIGH : USB_SPEED_FULL;
+			if (ci->usb_phy)
+				usb_phy_set_event(ci->usb_phy,
+					USB_EVENT_ENUMERATED);
 			if (ci->suspended && ci->driver->resume) {
 				spin_unlock(&ci->lock);
 				ci->driver->resume(&ci->gadget);
