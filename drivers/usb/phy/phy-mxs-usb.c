e2368c515fc9dd50762c643f83e366b590fc56f7
Peter Chen <peter.chen@nxp.com>
2016-10-18 16:36:39 +0800

    MLK-13308-2 usb: phy: phy-mxs-usb: handle USB PHY event
    For mxs PHY, if there is a vbus but the bus is not enumerated,
    force the dp/dm as SE0 from the consider side. If not, there
    is possible USB wakeup due to unstable dp/dm, since there is
    possible no pull on dp/dm, eg, there is a USB charger on the
    port. Note, the vbus event is only occurred at device mode,
    and sent by udc driver.
    
    Signed-off-by: Peter Chen <peter.chen@nxp.com>
    (cherry picked from commit 6810a146e7b86aecaac1f19345533b3de91e5444)

--- drivers/usb/phy/phy-mxs-usb.c
+++ drivers/usb/phy/phy-mxs-usb.c
@@ -337,6 +337,7 @@ static bool mxs_phy_is_otg_host(struct mxs_phy *mxs_phy)
 static void mxs_phy_disconnect_line(struct mxs_phy *mxs_phy, bool on)
 {
 	bool vbus_is_on = false;
+	enum usb_phy_events last_event = mxs_phy->phy.last_event;
 
 	/* If the SoCs don't need to disconnect line without vbus, quit */
 	if (!(mxs_phy->data->flags & MXS_PHY_DISCONNECT_LINE_WITHOUT_VBUS))
@@ -348,7 +349,8 @@ static void mxs_phy_disconnect_line(struct mxs_phy *mxs_phy, bool on)
 
 	vbus_is_on = mxs_phy_get_vbus_status(mxs_phy);
 
-	if (on && !vbus_is_on && !mxs_phy_is_otg_host(mxs_phy))
+	if (on && ((!vbus_is_on && !mxs_phy_is_otg_host(mxs_phy)) ||
+			(last_event == USB_EVENT_VBUS)))
 		__mxs_phy_disconnect_line(mxs_phy, true);
 	else
 		__mxs_phy_disconnect_line(mxs_phy, false);
