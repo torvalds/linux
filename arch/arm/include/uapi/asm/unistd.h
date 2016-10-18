6127d124ee4eb9c39983813cc9803f3654ab7e16
Russell King <rmk+kernel@armlinux.org.uk>
2016-10-18 21:46:18 +0100

    ARM: wire up new pkey syscalls
    Wire up the new pkey syscalls for ARM.
    
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- arch/arm/include/uapi/asm/unistd.h
+++ arch/arm/include/uapi/asm/unistd.h
@@ -420,6 +420,9 @@
 #define __NR_copy_file_range		(__NR_SYSCALL_BASE+391)
 #define __NR_preadv2			(__NR_SYSCALL_BASE+392)
 #define __NR_pwritev2			(__NR_SYSCALL_BASE+393)
+#define __NR_pkey_mprotect		(__NR_SYSCALL_BASE+394)
+#define __NR_pkey_alloc			(__NR_SYSCALL_BASE+395)
+#define __NR_pkey_free			(__NR_SYSCALL_BASE+396)
 
 /*
  * The following SWIs are ARM private.
