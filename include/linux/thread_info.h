0e9d11969866474ee26584f402e4c6f688179ae7
Mark Rutland <mark.rutland@arm.com>
2016-10-19 19:28:12 +0100

    UPSTREAM: thread_info: factor out restart_block
    Since commit f56141e3e2d9aabf ("all arches, signal: move restart_block
    to struct task_struct"), thread_info and restart_block have been
    logically distinct, yet struct restart_block is still defined in
    <linux/thread_info.h>.
    
    At least one architecture (erroneously) uses restart_block as part of
    its thread_info, and thus the definition of restart_block must come
    before the include of <asm/thread_info>. Subsequent patches in this
    series need to shuffle the order of includes and definitions in
    <linux/thread_info.h>, and will make this ordering fragile.
    
    This patch moves the definition of restart_block out to its own header.
    This serves as generic cleanup, logically separating thread_info and
    restart_block, and also makes it easier to avoid fragility.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Reviewed-by: Andy Lutomirski <luto@kernel.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: Kees Cook <keescook@chromium.org>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    
    Bug: 38331309
    Change-Id: I4283c87072c092179e2b6c02cbf7248b4a1c2d22
    (cherry picked from commit 53d74d056a4e306a72b8883d325b5d853c0618e6)
    Signed-off-by: Zubin Mithra <zsm@google.com>

--- include/linux/thread_info.h
+++ include/linux/thread_info.h
@@ -9,9 +9,7 @@
 
 #include <linux/types.h>
 #include <linux/bug.h>
-
-struct timespec;
-struct compat_timespec;
+#include <linux/restart_block.h>
 
 #ifdef CONFIG_THREAD_INFO_IN_TASK
 struct thread_info {
@@ -27,43 +25,6 @@ struct thread_info {
 #ifdef CONFIG_THREAD_INFO_IN_TASK
 #define current_thread_info() ((struct thread_info *)current)
 #endif
-
-/*
- * System call restart block.
- */
-struct restart_block {
-	long (*fn)(struct restart_block *);
-	union {
-		/* For futex_wait and futex_wait_requeue_pi */
-		struct {
-			u32 __user *uaddr;
-			u32 val;
-			u32 flags;
-			u32 bitset;
-			u64 time;
-			u32 __user *uaddr2;
-		} futex;
-		/* For nanosleep */
-		struct {
-			clockid_t clockid;
-			struct timespec __user *rmtp;
-#ifdef CONFIG_COMPAT
-			struct compat_timespec __user *compat_rmtp;
-#endif
-			u64 expires;
-		} nanosleep;
-		/* For poll */
-		struct {
-			struct pollfd __user *ufds;
-			int nfds;
-			int has_timeout;
-			unsigned long tv_sec;
-			unsigned long tv_nsec;
-		} poll;
-	};
-};
-
-extern long do_no_restart_syscall(struct restart_block *parm);
 
 #include <linux/bitops.h>
 #include <asm/thread_info.h>
