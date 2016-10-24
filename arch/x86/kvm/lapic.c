a10388e11fd09b99ca1bde4b9362502f005fb6d4
Wanpeng Li <wanpeng.li@hotmail.com>
2016-10-24 18:23:10 +0800

    KVM: LAPIC: guarantee the timer is in tsc-deadline mode
    Check apic_lvtt_tscdeadline() mode directly instead of apic_lvtt_oneshot()
    and apic_lvtt_period() to guarantee the timer is in tsc-deadline mode when
    rdmsr MSR_IA32_TSCDEADLINE.
    
    Suggested-by: Radim Krčmář <rkrcmar@redhat.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Radim Krčmář <rkrcmar@redhat.com>
    Cc: Yunhong Jiang <yunhong.jiang@intel.com>
    Signed-off-by: Wanpeng Li <wanpeng.li@hotmail.com>
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>

--- arch/x86/kvm/lapic.c
+++ arch/x86/kvm/lapic.c
@@ -1711,8 +1711,8 @@ u64 kvm_get_lapic_tscdeadline_msr(struct kvm_vcpu *vcpu)
 {
 	struct kvm_lapic *apic = vcpu->arch.apic;
 
-	if (!lapic_in_kernel(vcpu) || apic_lvtt_oneshot(apic) ||
-			apic_lvtt_period(apic))
+	if (!lapic_in_kernel(vcpu) ||
+		!apic_lvtt_tscdeadline(apic))
 		return 0;
 
 	return apic->lapic_timer.tscdeadline;
