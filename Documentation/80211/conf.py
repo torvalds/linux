fdd91a3dfadfa73bbb54702f1e951e7c17a507d6
Markus Heiser <markus.heiser@darmarit.de>
2016-10-26 08:23:17 +0200

    doc-rst: build PDF of 80211 and gpu sub-project
    This allows to build PDF of only the sub-projects, which reduce the
    roundtrip compared to build the PDF from the main documentation.
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>
    Signed-off-by: Jonathan Corbet <corbet@lwn.net>

--- Documentation/80211/conf.py
+++ Documentation/80211/conf.py
@@ -3,3 +3,8 @@
 project = "Linux 802.11 Driver Developer's Guide"
 
 tags.add("subproject")
+
+latex_documents = [
+    ('index', '80211.tex', project,
+     'The kernel development community', 'manual'),
+]
