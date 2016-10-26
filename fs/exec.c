e81cea2a6f86374194031a736b7139100c0b66c2
Daniel Rosenberg <drosen@google.com>
2016-10-26 16:27:45 -0700

    ANDROID: vfs: Add permission2 for filesystems with per mount permissions
    This allows filesystems to use their mount private data to
    influence the permssions they return in permission2. It has
    been separated into a new call to avoid disrupting current
    permission users.
    
    Bug: 35848445
    Bug: 120446149
    Change-Id: I9d416e3b8b6eca84ef3e336bd2af89ddd51df6ca
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    [AmitP: Minor refactoring of original patch to align with
            changes from the following upstream commit
            4bfd054ae11e ("fs: fold __inode_permission() into inode_permission()").
            Also introduce vfs_mkobj2(), because do_create()
            moved from using vfs_create() to vfs_mkobj()
            eecec19d9e70 ("mqueue: switch to vfs_mkobj(), quit abusing ->d_fsdata")
            do_create() is dropped/cleaned-up upstream so a
            minor refactoring there as well.
            066cc813e94a ("do_mq_open(): move all work prior to dentry_open() into a helper")]
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    [astrachan: Folded the following changes into this patch:
                f46c9d62dd81 ("ANDROID: fs: Export vfs_rmdir2")
                9992eb8b9a1e ("ANDROID: xattr: Pass EOPNOTSUPP to permission2")]
    Signed-off-by: Alistair Strachan <astrachan@google.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1308,7 +1308,7 @@ EXPORT_SYMBOL(flush_old_exec);
 void would_dump(struct linux_binprm *bprm, struct file *file)
 {
 	struct inode *inode = file_inode(file);
-	if (inode_permission(inode, MAY_READ) < 0) {
+	if (inode_permission2(file->f_path.mnt, inode, MAY_READ) < 0) {
 		struct user_namespace *old, *user_ns;
 		bprm->interp_flags |= BINPRM_FLAGS_ENFORCE_NONDUMP;
 
