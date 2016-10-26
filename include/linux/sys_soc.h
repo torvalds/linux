da65a1589dacc7ec44ea0557a14d70a39d991f32
Geert Uytterhoeven <geert+renesas@glider.be>
2016-10-26 15:13:15 +0200

    base: soc: Provide a dummy implementation of soc_device_match()
    Provide a dummy implementation of soc_device_match(), to allow compiling
    drivers that may be used on SoCs both with and without CONFIG_SOC_BUS,
    and for compile testing.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- include/linux/sys_soc.h
+++ include/linux/sys_soc.h
@@ -35,6 +35,12 @@ void soc_device_unregister(struct soc_device *soc_dev);
  */
 struct device *soc_device_to_device(struct soc_device *soc);
 
+#ifdef CONFIG_SOC_BUS
 const struct soc_device_attribute *soc_device_match(
 	const struct soc_device_attribute *matches);
+#else
+static inline const struct soc_device_attribute *soc_device_match(
+	const struct soc_device_attribute *matches) { return NULL; }
+#endif
+
 #endif /* __SOC_BUS_H */
