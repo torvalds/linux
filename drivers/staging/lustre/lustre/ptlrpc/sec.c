bbadab80d2cb1908705d32b1689fe5cb5a32faab
Sebastien Buisson <sebastien.buisson@bull.net>
2016-10-27 18:12:00 -0400

    staging: lustre: ptlrpc: do not switch out-of-date context
    When trying to replace a dead sec context with a new one, we must
    ensure the new context is already up-to-date.
    If it is not the case, just return from sptlrpc_req_replace_dead_ctx()
    and come later when the new context has been updated.
    
    Signed-off-by: Sebastien Buisson <sebastien.buisson@bull.net>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-6536
    Reviewed-on: http://review.whamcloud.com/15708
    Reviewed-by: Mike Pershin <mike.pershin@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ptlrpc/sec.c
+++ drivers/staging/lustre/lustre/ptlrpc/sec.c
@@ -515,6 +515,13 @@ static int sptlrpc_req_replace_dead_ctx(struct ptlrpc_request *req)
 
 		set_current_state(TASK_INTERRUPTIBLE);
 		schedule_timeout(msecs_to_jiffies(MSEC_PER_SEC));
+	} else if (unlikely(!test_bit(PTLRPC_CTX_UPTODATE_BIT, &newctx->cc_flags))) {
+		/*
+		 * new ctx not up to date yet
+		 */
+		CDEBUG(D_SEC,
+		       "ctx (%p, fl %lx) doesn't switch, not up to date yet\n",
+		       newctx, newctx->cc_flags);
 	} else {
 		/*
 		 * it's possible newctx == oldctx if we're switching
