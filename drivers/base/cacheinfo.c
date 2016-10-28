8e1073b1ffff5a90c1af75862309c5ed87ef545c
Sudeep Holla <sudeep.holla@arm.com>
2016-10-28 09:45:30 +0100

    drivers: base: cacheinfo: add pr_fmt logging
    This cleanup patch just adds pr_fmt style logging for cacheinfo.
    
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/cacheinfo.c
+++ drivers/base/cacheinfo.c
@@ -16,6 +16,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/acpi.h>
 #include <linux/bitops.h>
 #include <linux/cacheinfo.h>
