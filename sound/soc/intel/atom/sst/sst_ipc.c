6a7f5e41128de638fae7522262d18a6ef9cf1096
Sebastien Guiriec <sebastien.guiriec@intel.com>
2016-10-28 16:32:50 +0200

    ASoC: Intel: atom: Add debug information related to FW version
    This patch is adding debug information related to SST FW version.
    
    Signed-off-by: Sebastien Guiriec <sebastien.guiriec@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/atom/sst/sst_ipc.c
+++ sound/soc/intel/atom/sst/sst_ipc.c
@@ -236,6 +236,11 @@ static void process_fw_init(struct intel_sst_drv *sst_drv_ctx,
 		retval = init->result;
 		goto ret;
 	}
+	dev_info(sst_drv_ctx->dev, "FW Version %02x.%02x.%02x.%02x\n",
+			init->fw_version.type, init->fw_version.major,
+			init->fw_version.minor, init->fw_version.build);
+	dev_dbg(sst_drv_ctx->dev, "Build date %s Time %s\n",
+			init->build_info.date, init->build_info.time);
 
 ret:
 	sst_wake_up_block(sst_drv_ctx, retval, FW_DWNL_ID, 0 , NULL, 0);
