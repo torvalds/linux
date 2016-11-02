62641014202e6681f488ffa72c2956dd97faa0f8
Ngai-Mint Kwan <ngai-mint.kwan@intel.com>
2016-11-02 16:44:46 -0700

    fm10k: request reset when mbx->state changes
    [ Upstream commit 2f3fc1e6200309ccf87f61dea56e57e563c4f800 ]
    
    Multiple IES API resets can cause a race condition where the mailbox
    interrupt request bits can be cleared before being handled. This can
    leave certain mailbox messages from the PF to be untreated and the PF
    will enter in some inactive state. If this situation occurs, the IES API
    will initiate a mailbox version reset which, then, trigger a mailbox
    state change. Once this mailbox transition occurs (from OPEN to CONNECT
    state), a request for reset will be returned.
    
    This ensures that PF will undergo a reset whenever IES API encounters an
    unknown global mailbox interrupt event or whenever the IES API
    terminates.
    
    Signed-off-by: Ngai-Mint Kwan <ngai-mint.kwan@intel.com>
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    Tested-by: Krishneil Singh <krishneil.k.singh@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/intel/fm10k/fm10k_pci.c
+++ drivers/net/ethernet/intel/fm10k/fm10k_pci.c
@@ -1072,6 +1072,7 @@ static irqreturn_t fm10k_msix_mbx_pf(int __always_unused irq, void *data)
 	struct fm10k_hw *hw = &interface->hw;
 	struct fm10k_mbx_info *mbx = &hw->mbx;
 	u32 eicr;
+	s32 err = 0;
 
 	/* unmask any set bits related to this interrupt */
 	eicr = fm10k_read_reg(hw, FM10K_EICR);
@@ -1087,11 +1088,14 @@ static irqreturn_t fm10k_msix_mbx_pf(int __always_unused irq, void *data)
 
 	/* service mailboxes */
 	if (fm10k_mbx_trylock(interface)) {
-		mbx->ops.process(hw, mbx);
+		err = mbx->ops.process(hw, mbx);
 		/* handle VFLRE events */
 		fm10k_iov_event(interface);
 		fm10k_mbx_unlock(interface);
 	}
+
+	if (err == FM10K_ERR_RESET_REQUESTED)
+		interface->flags |= FM10K_FLAG_RESET_REQUESTED;
 
 	/* if switch toggled state we should reset GLORTs */
 	if (eicr & FM10K_EICR_SWITCHNOTREADY) {
