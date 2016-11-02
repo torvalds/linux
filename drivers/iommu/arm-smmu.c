3c117b543528b0d25801bd75444037dd0fcd2b8d
Robin Murphy <robin.murphy@arm.com>
2016-11-02 17:31:32 +0000

    iommu/arm-smmu: Check that iommu_fwspecs are ours
    We seem to have forgotten to check that iommu_fwspecs actually belong to
    us before we go ahead and dereference their private data. Oops.
    
    Fixes: 021bb8420d44 ("iommu/arm-smmu: Wire up generic configuration support")
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -1400,7 +1400,7 @@ static int arm_smmu_add_device(struct device *dev)
 		fwspec = dev->iommu_fwspec;
 		if (ret)
 			goto out_free;
-	} else if (fwspec) {
+	} else if (fwspec && fwspec->ops == &arm_smmu_ops) {
 		smmu = arm_smmu_get_by_node(to_of_node(fwspec->iommu_fwnode));
 	} else {
 		return -ENODEV;
