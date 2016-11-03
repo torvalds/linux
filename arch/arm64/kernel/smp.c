0b2ea94d16356077e522cbd00726a85cdd7daea2
Mark Rutland <mark.rutland@arm.com>
2016-11-03 20:23:10 +0000

    UPSTREAM: arm64: smp: prepare for smp_processor_id() rework
    Subsequent patches will make smp_processor_id() use a percpu variable.
    This will make smp_processor_id() dependent on the percpu offset, and
    thus we cannot use smp_processor_id() to figure out what to initialise
    the offset to.
    
    Prepare for this by initialising the percpu offset based on
    current::cpu, which will work regardless of how smp_processor_id() is
    implemented. Also, make this relationship obvious by placing this code
    together at the start of secondary_start_kernel().
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Tested-by: Laura Abbott <labbott@redhat.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    
    Bug: 38331309
    Change-Id: I43304d06602216fbb5b968ff83e0face11e238f5
    (cherry picked from commit 580efaa7ccfb8c0790dce4396434f0e5ac8d86ee)
    Signed-off-by: Zubin Mithra <zsm@google.com>

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -177,7 +177,10 @@ static void smp_store_cpu_info(unsigned int cpuid)
 asmlinkage void secondary_start_kernel(void)
 {
 	struct mm_struct *mm = &init_mm;
-	unsigned int cpu = smp_processor_id();
+	unsigned int cpu;
+
+	cpu = task_cpu(current);
+	set_my_cpu_offset(per_cpu_offset(cpu));
 
 	/*
 	 * All kernel threads share the same mm context; grab a
@@ -185,8 +188,6 @@ asmlinkage void secondary_start_kernel(void)
 	 */
 	atomic_inc(&mm->mm_count);
 	current->active_mm = mm;
-
-	set_my_cpu_offset(per_cpu_offset(smp_processor_id()));
 
 	/*
 	 * TTBR0 is only used for the identity mapping at this stage. Make it
