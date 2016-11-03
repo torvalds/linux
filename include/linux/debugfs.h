68f929ff2654bced015ccb9b5555667f46f88dfa
Jakub Kicinski <jakub.kicinski@netronome.com>
2016-11-03 17:12:06 +0000

    debugfs: constify argument to debugfs_real_fops()
    seq_file users can only access const version of file pointer,
    because the ->file member of struct seq_operations is marked
    as such.  Make parameter to debugfs_real_fops() const.
    
    CC: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    CC: Nicolai Stange <nicstange@gmail.com>
    CC: Christian Lamparter <chunkeey@gmail.com>
    CC: LKML <linux-kernel@vger.kernel.org>
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/debugfs.h
+++ include/linux/debugfs.h
@@ -52,7 +52,8 @@ extern struct srcu_struct debugfs_srcu;
  * Must only be called under the protection established by
  * debugfs_use_file_start().
  */
-static inline const struct file_operations *debugfs_real_fops(struct file *filp)
+static inline const struct file_operations *
+debugfs_real_fops(const struct file *filp)
 	__must_hold(&debugfs_srcu)
 {
 	/*
