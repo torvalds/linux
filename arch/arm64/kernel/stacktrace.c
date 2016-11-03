501f11450245360a7927734b25005058c66630e4
Mark Rutland <mark.rutland@arm.com>
2016-11-03 20:23:07 +0000

    UPSTREAM: arm64: unexport walk_stackframe
    The walk_stackframe functions is architecture-specific, with a varying
    prototype, and common code should not use it directly. None of its
    current users can be built as modules. With THREAD_INFO_IN_TASK, users
    will also need to hold a stack reference before calling it.
    
    There's no reason for it to be exported, and it's very easy to misuse,
    so unexport it for now.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    
    Bug: 38331309
    Change-Id: Ibe0dca36cc7d35f92c6bc13b373755d82f0eb9ef
    (cherry picked from commit 2020a5ae7c8c2c8504565004915017507b135c63)
    Signed-off-by: Zubin Mithra <zsm@google.com>

--- arch/arm64/kernel/stacktrace.c
+++ arch/arm64/kernel/stacktrace.c
@@ -129,7 +129,6 @@ void notrace walk_stackframe(struct task_struct *tsk, struct stackframe *frame,
 			break;
 	}
 }
-EXPORT_SYMBOL(walk_stackframe);
 
 #ifdef CONFIG_STACKTRACE
 struct stack_trace_data {
