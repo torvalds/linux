bc02aa1e2246b25e1905d80a88b17c011a00207e
Mark Rutland <mark.rutland@arm.com>
2016-11-03 20:23:06 +0000

    UPSTREAM: arm64: traps: simplify die() and __die()
    In arm64's die and __die routines we pass around a thread_info, and
    subsequently use this to determine the relevant task_struct, and the end
    of the thread's stack. Subsequent patches will decouple thread_info from
    the stack, and this approach will no longer work.
    
    To figure out the end of the stack, we can use the new generic
    end_of_stack() helper. As we only call __die() from die(), and die()
    always deals with the current task, we can remove the parameter and have
    both acquire current directly, which also makes it clear that __die
    can't be called for arbitrary tasks.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Tested-by: Laura Abbott <labbott@redhat.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    
    Bug: 38331309
    Change-Id: Ie1a96a0a8e244d458a7f147001b64216403e07c4
    (cherry picked from commit 876e7a38e8788773aac768091aaa3b42e470c03b)
    Signed-off-by: Zubin Mithra <zsm@google.com>

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -235,10 +235,9 @@ void show_stack(struct task_struct *tsk, unsigned long *sp)
 #endif
 #define S_SMP " SMP"
 
-static int __die(const char *str, int err, struct thread_info *thread,
-		 struct pt_regs *regs)
+static int __die(const char *str, int err, struct pt_regs *regs)
 {
-	struct task_struct *tsk = thread->task;
+	struct task_struct *tsk = current;
 	static int die_counter;
 	int ret;
 
@@ -253,7 +252,8 @@ static int __die(const char *str, int err, struct thread_info *thread,
 	print_modules();
 	__show_regs(regs);
 	pr_emerg("Process %.*s (pid: %d, stack limit = 0x%p)\n",
-		 TASK_COMM_LEN, tsk->comm, task_pid_nr(tsk), thread + 1);
+		 TASK_COMM_LEN, tsk->comm, task_pid_nr(tsk),
+		 end_of_stack(tsk));
 
 	if (!user_mode(regs) || in_interrupt()) {
 		dump_mem(KERN_EMERG, "Stack: ", regs->sp,
@@ -273,7 +273,6 @@ static DEFINE_RAW_SPINLOCK(die_lock);
  */
 void die(const char *str, struct pt_regs *regs, int err)
 {
-	struct thread_info *thread = current_thread_info();
 	int ret;
 
 	oops_enter();
@@ -281,9 +280,9 @@ void die(const char *str, struct pt_regs *regs, int err)
 	raw_spin_lock_irq(&die_lock);
 	console_verbose();
 	bust_spinlocks(1);
-	ret = __die(str, err, thread, regs);
+	ret = __die(str, err, regs);
 
-	if (regs && kexec_should_crash(thread->task))
+	if (regs && kexec_should_crash(current))
 		crash_kexec(regs);
 
 	bust_spinlocks(0);
