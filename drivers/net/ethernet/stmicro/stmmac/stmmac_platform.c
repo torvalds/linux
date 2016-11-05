5a3c7805c444d9d55f302a4b3930e8758be13fab
Joachim Eastwood <manabian@gmail.com>
2016-11-05 14:04:52 +0100

    Revert "net: stmmac: allow to split suspend/resume from init/exit callbacks"
    Instead of adding hooks inside stmmac_platform it is better to just use
    the standard PM callbacks within the specific dwmac-driver. This only
    used by the dwmac-rk driver.
    
    This reverts commit cecbc5563a02 ("stmmac: allow to split suspend/resume
    from init/exit callbacks").
    
    Signed-off-by: Joachim Eastwood <manabian@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
@@ -417,9 +417,7 @@ static int stmmac_pltfr_suspend(struct device *dev)
 	struct platform_device *pdev = to_platform_device(dev);
 
 	ret = stmmac_suspend(dev);
-	if (priv->plat->suspend)
-		priv->plat->suspend(pdev, priv->plat->bsp_priv);
-	else if (priv->plat->exit)
+	if (priv->plat->exit)
 		priv->plat->exit(pdev, priv->plat->bsp_priv);
 
 	return ret;
@@ -438,9 +436,7 @@ static int stmmac_pltfr_resume(struct device *dev)
 	struct stmmac_priv *priv = netdev_priv(ndev);
 	struct platform_device *pdev = to_platform_device(dev);
 
-	if (priv->plat->resume)
-		priv->plat->resume(pdev, priv->plat->bsp_priv);
-	else if (priv->plat->init)
+	if (priv->plat->init)
 		priv->plat->init(pdev, priv->plat->bsp_priv);
 
 	return stmmac_resume(dev);
