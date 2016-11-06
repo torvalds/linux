e2fdf7841cb32128685ddcd6db7a51d0e3c3c739
K. Y. Srinivasan <kys@microsoft.com>
2016-11-06 13:14:17 -0800

    Drivers: hv: vmbus: On write cleanup the logic to interrupt the host
    commit 1f6ee4e7d83586c8b10bd4f2f4346353d04ce884 upstream.
    
    Signal the host when we determine the host is to be signaled.
    The currrent code determines the need to signal in the ringbuffer
    code and actually issues the signal elsewhere. This can result
    in the host viewing this interrupt as spurious since the host may also
    poll the channel. Make the necessary adjustments.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Cc: Rolf Neugebauer <rolf.neugebauer@docker.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/hyperv.h
+++ include/linux/hyperv.h
@@ -1447,6 +1447,7 @@ void hv_event_tasklet_enable(struct vmbus_channel *channel);
 
 void hv_process_channel_removal(struct vmbus_channel *channel, u32 relid);
 
+void vmbus_setevent(struct vmbus_channel *channel);
 /*
  * Negotiated version with the Host.
  */
