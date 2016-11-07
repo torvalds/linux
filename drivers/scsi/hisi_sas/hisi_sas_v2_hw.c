04b7f431da23241e3beaa61883fb6b74627bc5ac
Xiang Chen <chenxiang66@hisilicon.com>
2016-11-07 20:48:36 +0800

    scsi: hisi_sas: delete repeated configuration in free_device_v2_hw()
    Delete repeated configuration items for hisi_sas_device() when
    we free a device. These items are now only set in
    hisi_sas_dev_gone().
    
    Signed-off-by: Xiang Chen <chenxiang66@hisilicon.com>
    Signed-off-by: John Garry <john.garry@huawei.com>
    Reviewed-by: Zhangfei Gao <zhangfei.gao@linaro.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/hisi_sas/hisi_sas_v2_hw.c
+++ drivers/scsi/hisi_sas/hisi_sas_v2_hw.c
@@ -739,8 +739,6 @@ static void free_device_v2_hw(struct hisi_hba *hisi_hba,
 			qw0 &= ~(1 << ITCT_HDR_VALID_OFF);
 			hisi_sas_write32(hisi_hba, ENT_INT_SRC3,
 					 ENT_INT_SRC3_ITC_INT_MSK);
-			hisi_hba->devices[dev_id].dev_type = SAS_PHY_UNUSED;
-			hisi_hba->devices[dev_id].dev_status = HISI_SAS_DEV_NORMAL;
 
 			/* clear the itct */
 			hisi_sas_write32(hisi_hba, ITCT_CLR, 0);
