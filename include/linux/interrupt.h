20e407e195b29a4f5a18d713a61f54a75f992bd5
Christoph Hellwig <hch@lst.de>
2016-11-08 17:15:01 -0800

    genirq/affinity: Introduce struct irq_affinity
    Some drivers (various network and RDMA adapter for example) have a MSI-X
    vector layout where most of the vectors are used for I/O queues and should
    have CPU affinity assigned to them, but some (usually 1 but sometimes more)
    at the beginning or end are used for low-performance admin or configuration
    work and should not have any explicit affinity assigned to them.
    
    Add a new irq_affinity structure, which will be passed through a variant of
    pci_irq_alloc_vectors that allows to specify these requirements (and is
    extensible to any future quirks in that area) so that the core IRQ affinity
    algorithm can take this quirks into account.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
    Reviewed-by: Hannes Reinecke <hare@suse.com>
    Acked-by: Jens Axboe <axboe@kernel.dk>
    Cc: linux-block@vger.kernel.org
    Cc: linux-pci@vger.kernel.org
    Link: http://lkml.kernel.org/r/1478654107-7384-2-git-send-email-hch@lst.de
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -232,6 +232,18 @@ struct irq_affinity_notify {
 	void (*release)(struct kref *ref);
 };
 
+/**
+ * struct irq_affinity - Description for automatic irq affinity assignements
+ * @pre_vectors:	Don't apply affinity to @pre_vectors at beginning of
+ *			the MSI(-X) vector space
+ * @post_vectors:	Don't apply affinity to @post_vectors at end of
+ *			the MSI(-X) vector space
+ */
+struct irq_affinity {
+	int	pre_vectors;
+	int	post_vectors;
+};
+
 #if defined(CONFIG_SMP)
 
 extern cpumask_var_t irq_default_affinity;
