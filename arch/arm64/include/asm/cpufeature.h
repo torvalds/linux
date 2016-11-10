ecad39540a66997b642907e2f939a9f662471a9f
Alex Shi <alex.shi@linaro.org>
2016-11-10 22:03:13 +0800

    arm64:cpufeature ARM64_NCAPS is the indicator of last feature
    commit d2d693d1ba7d set the NCAPS before ARM64_WORKAROUND_CAVIUM_27456
    that would lead to this feature out of tracking. This commit fixs
    this problem
    
    Signed-off-by: Alex Shi <alex.shi@linaro.org>

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -34,8 +34,8 @@
 #define ARM64_HAS_UAO				9
 #define ARM64_ALT_PAN_NOT_UAO			10
 
-#define ARM64_NCAPS				11
-#define ARM64_WORKAROUND_CAVIUM_27456		12	
+#define ARM64_WORKAROUND_CAVIUM_27456		11
+#define ARM64_NCAPS				12
 
 
 #ifndef __ASSEMBLY__
