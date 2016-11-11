fa39baa9700c86a73b2203eab92dbba7eb2d4d69
Even Xu <even.xu@intel.com>
2016-11-11 09:40:13 +0800

    HID: intel-ish-hid: ipc: remove unused macro
    The macro timed_wait_for() in utils.h isn't used in current ipc
    driver, so remove it for avoiding confusion.
    
    Signed-off-by: Even Xu <even.xu@intel.com>
    Acked-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/intel-ish-hid/ipc/utils.h
+++ drivers/hid/intel-ish-hid/ipc/utils.h
@@ -19,27 +19,6 @@
 #define	WAIT_FOR_CONNECT_SLICE	(HZ / 10)
 
 /*
- * Waits for specified event when a thread that triggers event can't signal
- * Also, waits *at_least* `timeinc` after condition is satisfied
- */
-#define	timed_wait_for(timeinc, condition)			\
-	do {							\
-		int completed = 0;				\
-		do {						\
-			unsigned long	j;			\
-			int	done = 0;			\
-								\
-			completed = (condition);		\
-			for (j = jiffies, done = 0; !done; ) {	\
-				schedule_timeout(timeinc);	\
-				if (time_is_before_eq_jiffies(j + timeinc)) \
-					done = 1;		\
-			}					\
-		} while (!(completed));				\
-	} while (0)
-
-
-/*
  * Waits for specified event when a thread that triggers event
  * can't signal with timeout (use whenever we may hang)
  */
