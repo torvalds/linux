5bf35ddfee052d44f39ebaa395d87101c8918405
Xin Long <lucien.xin@gmail.com>
2016-11-13 21:44:37 +0800

    sctp: change sk state only when it has assocs in sctp_shutdown
    Now when users shutdown a sock with SEND_SHUTDOWN in sctp, even if
    this sock has no connection (assoc), sk state would be changed to
    SCTP_SS_CLOSING, which is not as we expect.
    
    Besides, after that if users try to listen on this sock, kernel
    could even panic when it dereference sctp_sk(sk)->bind_hash in
    sctp_inet_listen, as bind_hash is null when sock has no assoc.
    
    This patch is to move sk state change after checking sk assocs
    is not empty, and also merge these two if() conditions and reduce
    indent level.
    
    Fixes: d46e416c11c8 ("sctp: sctp should change socket state when shutdown is received")
    Reported-by: Andrey Konovalov <andreyknvl@google.com>
    Tested-by: Andrey Konovalov <andreyknvl@google.com>
    Signed-off-by: Xin Long <lucien.xin@gmail.com>
    Acked-by: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
    Acked-by: Neil Horman <nhorman@tuxdriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sctp/socket.c
+++ net/sctp/socket.c
@@ -4285,19 +4285,18 @@ static void sctp_shutdown(struct sock *sk, int how)
 {
 	struct net *net = sock_net(sk);
 	struct sctp_endpoint *ep;
-	struct sctp_association *asoc;
 
 	if (!sctp_style(sk, TCP))
 		return;
 
-	if (how & SEND_SHUTDOWN) {
+	ep = sctp_sk(sk)->ep;
+	if (how & SEND_SHUTDOWN && !list_empty(&ep->asocs)) {
+		struct sctp_association *asoc;
+
 		sk->sk_state = SCTP_SS_CLOSING;
-		ep = sctp_sk(sk)->ep;
-		if (!list_empty(&ep->asocs)) {
-			asoc = list_entry(ep->asocs.next,
-					  struct sctp_association, asocs);
-			sctp_primitive_SHUTDOWN(net, asoc, NULL);
-		}
+		asoc = list_entry(ep->asocs.next,
+				  struct sctp_association, asocs);
+		sctp_primitive_SHUTDOWN(net, asoc, NULL);
 	}
 }
 
