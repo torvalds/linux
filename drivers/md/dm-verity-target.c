eb4228c7a5edcbfe1dc50c49323af6e6861ffaf7
Keun-young Park <keunyoung@google.com>
2016-11-14 18:25:15 -0800

    ANDROID: dm verity: add minimum prefetch size
    - For device like eMMC, it gives better performance to read more hash
      blocks at a time.
    - For android, set it to default 128.
      For other devices, set it to 1 which is the same as now.
    - saved boot-up time by 300ms in tested device
    
    bug: 32246564
    
    Change-Id: Ibc0401a0cddba64b862a80445844b4e595213621
    Cc: Sami Tolvanen <samitolvanen@google.com>
    Signed-off-by: Keun-young Park <keunyoung@google.com>

--- drivers/md/dm-verity-target.c
+++ drivers/md/dm-verity-target.c
@@ -501,6 +501,7 @@ static void verity_prefetch_io(struct work_struct *work)
 		container_of(work, struct dm_verity_prefetch_work, work);
 	struct dm_verity *v = pw->v;
 	int i;
+	sector_t prefetch_size;
 
 	for (i = v->levels - 2; i >= 0; i--) {
 		sector_t hash_block_start;
@@ -523,8 +524,14 @@ static void verity_prefetch_io(struct work_struct *work)
 				hash_block_end = v->hash_blocks - 1;
 		}
 no_prefetch_cluster:
+		// for emmc, it is more efficient to send bigger read
+		prefetch_size = max((sector_t)CONFIG_DM_VERITY_HASH_PREFETCH_MIN_SIZE,
+			hash_block_end - hash_block_start + 1);
+		if ((hash_block_start + prefetch_size) >= (v->hash_start + v->hash_blocks)) {
+			prefetch_size = hash_block_end - hash_block_start + 1;
+		}
 		dm_bufio_prefetch(v->bufio, hash_block_start,
-				  hash_block_end - hash_block_start + 1);
+				  prefetch_size);
 	}
 
 	kfree(pw);
