0f8fb2ab1e093c031ff4ce91570951e82fef6ca1
Alexandre Courbot <acourbot@nvidia.com>
2016-11-15 16:30:26 +0900

    drm/nouveau/secboot: support for r367 ACR
    r367 uses a different hsflcn_desc layout and LS firmware signature
    format, requiring a rewrite of some functions.
    
    It also makes use of the shadow region, and uses SEC as the boot falcon.
    
    Signed-off-by: Alexandre Courbot <acourbot@nvidia.com>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/subdev/secboot/acr.h
+++ drivers/gpu/drm/nouveau/nvkm/subdev/secboot/acr.h
@@ -64,5 +64,6 @@ void *nvkm_acr_load_firmware(const struct nvkm_subdev *, const char *, size_t);
 struct nvkm_acr *acr_r352_new(unsigned long);
 struct nvkm_acr *acr_r361_new(unsigned long);
 struct nvkm_acr *acr_r364_new(unsigned long);
+struct nvkm_acr *acr_r367_new(enum nvkm_secboot_falcon, unsigned long);
 
 #endif
