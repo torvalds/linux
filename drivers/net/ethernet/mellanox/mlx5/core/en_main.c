d30d9ccbfac7cf9a12a088d57aaf0891732e2bca
Eric Dumazet <edumazet@google.com>
2016-11-16 06:21:34 -0800

    net/mlx5e: remove napi_hash_del() calls
    Calling napi_hash_del() after netif_napi_del() is pointless.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Saeed Mahameed <saeedm@mellanox.com>
    Acked-by: Saeed Mahameed <saeedm@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlx5/core/en_main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -1533,7 +1533,6 @@ err_close_icosq_cq:
 
 err_napi_del:
 	netif_napi_del(&c->napi);
-	napi_hash_del(&c->napi);
 	kfree(c);
 
 	return err;
@@ -1553,9 +1552,6 @@ static void mlx5e_close_channel(struct mlx5e_channel *c)
 	mlx5e_close_tx_cqs(c);
 	mlx5e_close_cq(&c->icosq.cq);
 	netif_napi_del(&c->napi);
-
-	napi_hash_del(&c->napi);
-	synchronize_rcu();
 
 	kfree(c);
 }
