a0aba89763f863261609e3e6edff138e4114384f
Jes Sorensen <Jes.Sorensen@redhat.com>
2016-11-18 16:44:22 -0500

    rtl8xxxu: Fix memory leak in handling rxdesc16 packets
    A device running without RX package aggregation could return more data
    in the USB packet than the actual network packet. In this case the
    could would clone the skb but then determine that that there was no
    packet to handle and exit without freeing the cloned skb first.
    
    This has so far only been observed with 8188eu devices, but could
    affect others.
    
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_core.c
+++ drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_core.c
@@ -5197,7 +5197,12 @@ int rtl8xxxu_parse_rxdesc16(struct rtl8xxxu_priv *priv, struct sk_buff *skb)
 		pkt_offset = roundup(pkt_len + drvinfo_sz + desc_shift +
 				     sizeof(struct rtl8xxxu_rxdesc16), 128);
 
-		if (pkt_cnt > 1)
+		/*
+		 * Only clone the skb if there's enough data at the end to
+		 * at least cover the rx descriptor
+		 */
+		if (pkt_cnt > 1 &&
+		    urb_len > (pkt_offset + sizeof(struct rtl8xxxu_rxdesc16)))
 			next_skb = skb_clone(skb, GFP_ATOMIC);
 
 		rx_status = IEEE80211_SKB_RXCB(skb);
