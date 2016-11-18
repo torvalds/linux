46d567e65b9c6e47e2319b9ce5800aec5797c50a
Walt Feasel <waltfeasel@gmail.com>
2016-11-18 17:39:13 -0500

    staging: dgnc: dgnc_driver.h Spelling correction
    Make spelling correction for 'statements'
    
    Signed-off-by: Walt Feasel <waltfeasel@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_driver.h
+++ drivers/staging/dgnc/dgnc_driver.h
@@ -27,7 +27,7 @@
 
 /* Driver defines */
 
-/* Driver identification and error statments */
+/* Driver identification and error statements */
 #define	PROCSTR		"dgnc"			/* /proc entries */
 #define	DEVSTR		"/dev/dg/dgnc"		/* /dev entries */
 #define	DRVSTR		"dgnc"			/* Driver name string */
