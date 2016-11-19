26ce8a4fce038639d19a1c140a73656615bffc4a
Walt Feasel <waltfeasel@gmail.com>
2016-11-19 11:50:52 -0500

    staging: speakup: varhandlers.c Comment style modifications
    Make modifications to comment style
    
    Signed-off-by: Walt Feasel <waltfeasel@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/speakup/varhandlers.c
+++ drivers/staging/speakup/varhandlers.c
@@ -266,7 +266,8 @@ int spk_set_string_var(const char *page, struct st_var_header *var, int len)
 	return 0;
 }
 
-/* spk_set_mask_bits sets or clears the punc/delim/repeat bits,
+/*
+ * spk_set_mask_bits sets or clears the punc/delim/repeat bits,
  * if input is null uses the defaults.
  * values for how: 0 clears bits of chars supplied,
  * 1 clears allk, 2 sets bits for chars
