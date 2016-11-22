98a387cdfe139c31a108a4623171bcdac82decf8
Banajit Goswami <bgoswami@codeaurora.org>
2016-11-21 17:49:35 -0800

    ANDROID: GKI: ALSA: jack: Add support to report second microphone
    ALSA jack framework currently supports reporting only
    one single microphone. Add support to report presence of
    the second microphone to userspace.
    
    Test: build
    Bug: 149430094
    Change-Id: I72d16892f2fa563c026bf90d847d9365dcf2233a
    Signed-off-by: Phani Kumar Uppalapati <phaniu@codeaurora.org>
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>
    Signed-off-by: Sudheer Papothi <spapothi@codeaurora.org>
    Signed-off-by: Meng Wang <mwang@codeaurora.org>
    (cherry picked from commit b071d2a1b2a52b1f13ae14769e473f1ee4f66702)
    Signed-off-by: Hridya Valsaraju <hridya@google.com>

--- include/sound/jack.h
+++ include/sound/jack.h
@@ -61,6 +61,9 @@ enum snd_jack_types {
 	SND_JACK_OC_HPHL        = 0x0040,
 	SND_JACK_OC_HPHR        = 0x0080,
 	SND_JACK_UNSUPPORTED    = 0x0100,
+	SND_JACK_MICROPHONE2    = 0x0200,
+	SND_JACK_ANC_HEADPHONE  = SND_JACK_HEADPHONE | SND_JACK_MICROPHONE |
+				  SND_JACK_MICROPHONE2,
 
 	/* Kept separate from switches to facilitate implementation */
 	SND_JACK_BTN_0		= 0x4000,
