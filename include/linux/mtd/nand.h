61babe943938bb41fcd7e2b1dec7d1537ea3892f
Randy Dunlap <rdunlap@infradead.org>
2016-11-21 18:32:08 -0800

    mtd: nand: fix nand.h kernel-doc warnings
    Fix kernel-doc warnings in <linux/mtd/nand.h>:
    
    ..//include/linux/mtd/nand.h:658: warning: No description found for parameter 'tCEH_min'
    ..//include/linux/mtd/nand.h:877: warning: No description found for parameter 'data_interface'
    
    Fixes: eee64b700e26 ("mtd: nand: Introduce nand_data_interface")
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- include/linux/mtd/nand.h
+++ include/linux/mtd/nand.h
@@ -615,7 +615,7 @@ struct nand_buffers {
  * @tALS_min: ALE setup time
  * @tAR_min: ALE to RE# delay
  * @tCEA_max: CE# access time
- * @tCEH_min:
+ * @tCEH_min: CE# high hold time
  * @tCH_min:  CE# hold time
  * @tCHZ_max: CE# high to output hi-Z
  * @tCLH_min: CLE hold time
@@ -801,6 +801,7 @@ nand_get_sdr_timings(const struct nand_data_interface *conf)
  *			supported, 0 otherwise.
  * @jedec_params:	[INTERN] holds the JEDEC parameter page when JEDEC is
  *			supported, 0 otherwise.
+ * @data_interface:	[INTERN] NAND interface timing information
  * @read_retries:	[INTERN] the number of read retry modes supported
  * @onfi_set_features:	[REPLACEABLE] set the features for ONFI nand
  * @onfi_get_features:	[REPLACEABLE] get the features for ONFI nand
