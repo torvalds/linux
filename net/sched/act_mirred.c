c491680f8f489926eebfdf2cd006767fc8bdaa49
Daniel Borkmann <daniel@iogearbox.net>
2016-11-26 01:28:06 +0100

    bpf: reuse dev_is_mac_header_xmit for redirect
    Commit dcf800344a91 ("net/sched: act_mirred: Refactor detection whether
    dev needs xmit at mac header") added dev_is_mac_header_xmit(); since it's
    also useful elsewhere, move it to if_arp.h and reuse it for BPF.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/act_mirred.c
+++ net/sched/act_mirred.c
@@ -21,6 +21,7 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/gfp.h>
+#include <linux/if_arp.h>
 #include <net/net_namespace.h>
 #include <net/netlink.h>
 #include <net/pkt_sched.h>
@@ -72,20 +73,6 @@ static const struct nla_policy mirred_policy[TCA_MIRRED_MAX + 1] = {
 
 static unsigned int mirred_net_id;
 static struct tc_action_ops act_mirred_ops;
-
-static bool dev_is_mac_header_xmit(const struct net_device *dev)
-{
-	switch (dev->type) {
-	case ARPHRD_TUNNEL:
-	case ARPHRD_TUNNEL6:
-	case ARPHRD_SIT:
-	case ARPHRD_IPGRE:
-	case ARPHRD_VOID:
-	case ARPHRD_NONE:
-		return false;
-	}
-	return true;
-}
 
 static int tcf_mirred_init(struct net *net, struct nlattr *nla,
 			   struct nlattr *est, struct tc_action **a, int ovr,
