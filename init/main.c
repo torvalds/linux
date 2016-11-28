4d217a5adccf5e806790c37c61cc374a08bd7381
Arnd Bergmann <arnd@arndb.de>
2016-11-28 15:59:13 +0100

    module: fix DEBUG_SET_MODULE_RONX typo
    The newly added 'rodata_enabled' global variable is protected by
    the wrong #ifdef, leading to a link error when CONFIG_DEBUG_SET_MODULE_RONX
    is turned on:
    
    kernel/module.o: In function `disable_ro_nx':
    module.c:(.text.unlikely.disable_ro_nx+0x88): undefined reference to `rodata_enabled'
    kernel/module.o: In function `module_disable_ro':
    module.c:(.text.module_disable_ro+0x8c): undefined reference to `rodata_enabled'
    kernel/module.o: In function `module_enable_ro':
    module.c:(.text.module_enable_ro+0xb0): undefined reference to `rodata_enabled'
    
    CONFIG_SET_MODULE_RONX does not exist, so use the correct one instead.
    
    Fixes: 39290b389ea2 ("module: extend 'rodata=off' boot cmdline parameter to module mappings")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Jessica Yu <jeyu@redhat.com>

--- init/main.c
+++ init/main.c
@@ -915,7 +915,7 @@ static int try_to_run_init_process(const char *init_filename)
 
 static noinline void __init kernel_init_freeable(void);
 
-#if defined(CONFIG_DEBUG_RODATA) || defined(CONFIG_SET_MODULE_RONX)
+#if defined(CONFIG_DEBUG_RODATA) || defined(CONFIG_DEBUG_SET_MODULE_RONX)
 bool rodata_enabled __ro_after_init = true;
 static int __init set_debug_rodata(char *str)
 {
