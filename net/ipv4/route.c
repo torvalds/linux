2b9e162b21a7931a5788456edb65199ebd8b0e21
Lorenzo Colitti <lorenzo@google.com>
2016-11-30 02:56:47 +0900

    UPSTREAM: net: ipv4: Don't crash if passing a null sk to ip_rt_update_pmtu.
    Commit e2d118a1cb5e ("net: inet: Support UID-based routing in IP
    protocols.") made __build_flow_key call sock_net(sk) to determine
    the network namespace of the passed-in socket. This crashes if sk
    is NULL.
    
    Fix this by getting the network namespace from the skb instead.
    
    Change-Id: Ife860a7af8471bf40cc987f7803059d7bb9cc7e3
    Fixes: e2d118a1cb5e ("net: inet: Support UID-based routing in IP protocols.")
    Reported-by: Erez Shitrit <erezsh@dev.mellanox.co.il>
    Signed-off-by: Lorenzo Colitti <lorenzo@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -531,13 +531,14 @@ static void __build_flow_key(const struct net *net, struct flowi4 *fl4,
 static void build_skb_flow_key(struct flowi4 *fl4, const struct sk_buff *skb,
 			       const struct sock *sk)
 {
+	const struct net *net = dev_net(skb->dev);
 	const struct iphdr *iph = ip_hdr(skb);
 	int oif = skb->dev->ifindex;
 	u8 tos = RT_TOS(iph->tos);
 	u8 prot = iph->protocol;
 	u32 mark = skb->mark;
 
-	__build_flow_key(sock_net(sk), fl4, sk, iph, oif, tos, prot, mark, 0);
+	__build_flow_key(net, fl4, sk, iph, oif, tos, prot, mark, 0);
 }
 
 static void build_sk_flow_key(struct flowi4 *fl4, const struct sock *sk)
