501634759d55a5b56967de6d9465acf02bbc3565
Dan Carpenter <dan.carpenter@oracle.com>
2016-11-30 22:22:54 +0300

    ACPI / CPPC: set an error code on probe error path
    We should return -EINVAL (instead of 0) if get_cpu_device() fails.
    
    Fixes: 158c998ea44b (ACPI / CPPC: add sysfs support to compute delivered performance)
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/cppc_acpi.c
+++ drivers/acpi/cppc_acpi.c
@@ -784,8 +784,10 @@ int acpi_cppc_processor_probe(struct acpi_processor *pr)
 
 	/* Add per logical CPU nodes for reading its feedback counters. */
 	cpu_dev = get_cpu_device(pr->id);
-	if (!cpu_dev)
+	if (!cpu_dev) {
+		ret = -EINVAL;
 		goto out_free;
+	}
 
 	ret = kobject_init_and_add(&cpc_ptr->kobj, &cppc_ktype, &cpu_dev->kobj,
 			"acpi_cppc");
