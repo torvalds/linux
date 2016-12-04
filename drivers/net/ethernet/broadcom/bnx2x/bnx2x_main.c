360d9df2acd9f0b89aabaf16fca08954f113bd4e
Mintz, Yuval <Yuval.Mintz@cavium.com>
2016-12-04 15:30:18 +0200

    bnx2x: Prevent tunnel config for 577xx
    Only the 578xx adapters are capable of configuring UDP ports for
    the purpose of tunnelling - doing the same on 577xx might lead to
    a firmware assertion.
    We're already not claiming support for any related feature for such
    devices, but we also need to prevent the configuration of the UDP
    ports to the device in this case.
    
    Fixes: f34fa14cc033 ("bnx2x: Add vxlan RSS support")
    Reported-by: Anikina Anna <anikina@gmail.com>
    Signed-off-by: Yuval Mintz <Yuval.Mintz@cavium.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/bnx2x/bnx2x_main.c
+++ drivers/net/ethernet/broadcom/bnx2x/bnx2x_main.c
@@ -10138,7 +10138,7 @@ static void __bnx2x_add_udp_port(struct bnx2x *bp, u16 port,
 {
 	struct bnx2x_udp_tunnel *udp_port = &bp->udp_tunnel_ports[type];
 
-	if (!netif_running(bp->dev) || !IS_PF(bp))
+	if (!netif_running(bp->dev) || !IS_PF(bp) || CHIP_IS_E1x(bp))
 		return;
 
 	if (udp_port->count && udp_port->dst_port == port) {
@@ -10163,7 +10163,7 @@ static void __bnx2x_del_udp_port(struct bnx2x *bp, u16 port,
 {
 	struct bnx2x_udp_tunnel *udp_port = &bp->udp_tunnel_ports[type];
 
-	if (!IS_PF(bp))
+	if (!IS_PF(bp) || CHIP_IS_E1x(bp))
 		return;
 
 	if (!udp_port->count || udp_port->dst_port != port) {
