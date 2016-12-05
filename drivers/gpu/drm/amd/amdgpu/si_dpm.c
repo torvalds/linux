f3dd47e0f683cfcca9e1d31f61d33aed37c5f122
Alex Deucher <alexander.deucher@amd.com>
2016-12-05 12:31:14 -0500

    drm/amdgpu/si: load the proper firmware on 0x87 oland boards
    commit 5a23f2720589ec4757bc62183902d2518f02026e upstream.
    
    New variant.
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/amd/amdgpu/si_dpm.c
+++ drivers/gpu/drm/amd/amdgpu/si_dpm.c
@@ -7713,6 +7713,7 @@ static int si_dpm_init_microcode(struct amdgpu_device *adev)
 		    (adev->pdev->revision == 0x80) ||
 		    (adev->pdev->revision == 0x81) ||
 		    (adev->pdev->revision == 0x83) ||
+		    (adev->pdev->revision == 0x87) ||
 		    (adev->pdev->device == 0x6604) ||
 		    (adev->pdev->device == 0x6605))
 			chip_name = "oland_k";
