56bc1c42610aa6328e6c4b222dc076acffb40243
Zeyu Fan <Zeyu.Fan@amd.com>
2016-12-07 11:28:03 -0500

    drm/amd/display: Fix hdmi not lighting up on Hawaii.
    Signed-off-by: Zeyu Fan <Zeyu.Fan@amd.com>
    Acked-by: Jordan Lazare <Jordan.Lazare@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dce/dce_link_encoder.c
+++ drivers/gpu/drm/amd/display/dc/dce/dce_link_encoder.c
@@ -1233,7 +1233,7 @@ void dce110_link_encoder_enable_tmds_output(
 	/* Enable the PHY */
 
 	cntl.action = TRANSMITTER_CONTROL_ENABLE;
-	cntl.engine_id = ENGINE_ID_UNKNOWN;
+	cntl.engine_id = enc->preferred_engine;
 	cntl.transmitter = enc110->base.transmitter;
 	cntl.pll_id = clock_source;
 	if (hdmi) {
