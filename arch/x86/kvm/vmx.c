37f0e8fe6b10ee2ab52576caa721ee1282de74a6
Junaid Shahid <junaids@google.com>
2016-12-06 16:46:15 -0800

    kvm: x86: mmu: Do not use bit 63 for tracking special SPTEs
    MMIO SPTEs currently set both bits 62 and 63 to distinguish them as special
    PTEs. However, bit 63 is used as the SVE bit in Intel EPT PTEs. The SVE bit
    is ignored for misconfigured PTEs but not necessarily for not-Present PTEs.
    Since MMIO SPTEs use an EPT misconfiguration, so using bit 63 for them is
    acceptable. However, the upcoming fast access tracking feature adds another
    type of special tracking PTE, which uses not-Present PTEs and hence should
    not set bit 63.
    
    In order to use common bits to distinguish both type of special PTEs, we
    now use only bit 62 as the special bit.
    
    Signed-off-by: Junaid Shahid <junaids@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -5236,10 +5236,10 @@ static void ept_set_mmio_spte_mask(void)
 	/*
 	 * EPT Misconfigurations can be generated if the value of bits 2:0
 	 * of an EPT paging-structure entry is 110b (write/execute).
-	 * Also, magic bits (0x3ull << 62) is set to quickly identify mmio
-	 * spte.
+	 * Also, special bit (62) is set to quickly identify mmio spte.
 	 */
-	kvm_mmu_set_mmio_spte_mask((0x3ull << 62) | 0x6ull);
+	kvm_mmu_set_mmio_spte_mask(SPTE_SPECIAL_MASK |
+				   VMX_EPT_MISCONFIG_WX_VALUE);
 }
 
 #define VMX_XSS_EXIT_BITMAP 0
