20977374c5eb850180bcd163b9ef568ba0f88451
Andy Yan <andy.yan@rock-chips.com>
2016-12-08 16:58:07 +0800

    power: reset: reboot-mode: treat unrecognized reboot mode as normal mode
    Some bootloader will check the reboot mode to take different action, so
    we treat unrecognized reboot mode as normal mode to prevent the system
    run into abnormal case.
    
    Change-Id: I88063a5b41e4e645443229fa490b2b55db5ccf27
    Signed-off-by: Andy Yan <andy.yan@rock-chips.com>

--- drivers/power/reset/reboot-mode.c
+++ drivers/power/reset/reboot-mode.c
@@ -57,6 +57,8 @@ static int reboot_mode_notify(struct notifier_block *this,
 
 	reboot = container_of(this, struct reboot_mode_driver, reboot_notifier);
 	magic = get_reboot_mode_magic(reboot, cmd);
+	if (!magic)
+		magic = get_reboot_mode_magic(reboot, NULL);
 	if (magic)
 		reboot->write(magic);
 
