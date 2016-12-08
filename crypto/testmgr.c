04b46fbdea5e31ffd745a34fa61269a69ba9f47a
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2016-12-08 08:23:52 +0000

    crypto: testmgr - fix overlap in chunked tests again
    Commit 7e4c7f17cde2 ("crypto: testmgr - avoid overlap in chunked tests")
    attempted to address a problem in the crypto testmgr code where chunked
    test cases are copied to memory in a way that results in overlap.
    
    However, the fix recreated the exact same issue for other chunked tests,
    by putting IDX3 within 492 bytes of IDX1, which causes overlap if the
    first chunk exceeds 492 bytes, which is the case for at least one of
    the xts(aes) test cases.
    
    So increase IDX3 by another 1000 bytes.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -63,7 +63,7 @@ int alg_test(const char *driver, const char *alg, u32 type, u32 mask)
  */
 #define IDX1		32
 #define IDX2		32400
-#define IDX3		511
+#define IDX3		1511
 #define IDX4		8193
 #define IDX5		22222
 #define IDX6		17101
