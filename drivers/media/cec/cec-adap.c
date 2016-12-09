2129ba09890ca6d943ce9bae3f3c86e5846a8a33
Hans Verkuil <hansverk@cisco.com>
2016-12-09 11:48:34 -0200

    UPSTREAM: [media] cec: move cec_report_phys_addr into cec_config_thread_func
    It's only a small function and this makes it easier to switch to
    transmitting the message with adap->lock held in the next patch.
    
    Signed-off-by: Hans Verkuil <hansverk@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>
    (cherry picked from commit d3d64bc7408f1ff0b0ff8354056e2a48eda5886d)
    
    Change-Id: I936ba05b9b70681d57d52e10645f8865a27f5689
    Signed-off-by: Nickey Yang <nickey.yang@rock-chips.com>

--- drivers/media/cec/cec-adap.c
+++ drivers/media/cec/cec-adap.c
@@ -30,7 +30,6 @@
 
 #include "cec-priv.h"
 
-static int cec_report_phys_addr(struct cec_adapter *adap, unsigned int la_idx);
 static void cec_fill_msg_report_features(struct cec_adapter *adap,
 					 struct cec_msg *msg,
 					 unsigned int la_idx);
@@ -1275,7 +1274,13 @@ configured:
 			cec_transmit_msg(adap, &msg, false);
 		}
 
-		cec_report_phys_addr(adap, i);
+		/* Report Physical Address */
+		cec_msg_report_physical_addr(&msg, adap->phys_addr,
+					     las->primary_device_type[i]);
+		dprintk(2, "config: la %d pa %x.%x.%x.%x\n",
+			las->log_addr[i],
+			cec_phys_addr_exp(adap->phys_addr));
+		cec_transmit_msg(adap, &msg, false);
 	}
 	mutex_lock(&adap->lock);
 	adap->kthread_config = NULL;
@@ -1559,22 +1564,6 @@ static void cec_fill_msg_report_features(struct cec_adapter *adap,
 			op_is_dev_features = true;
 		}
 	}
-}
-
-/* Transmit the Report Physical Address message */
-static int cec_report_phys_addr(struct cec_adapter *adap, unsigned int la_idx)
-{
-	const struct cec_log_addrs *las = &adap->log_addrs;
-	struct cec_msg msg = { };
-
-	/* Report Physical Address */
-	msg.msg[0] = (las->log_addr[la_idx] << 4) | 0x0f;
-	cec_msg_report_physical_addr(&msg, adap->phys_addr,
-				     las->primary_device_type[la_idx]);
-	dprintk(2, "config: la %d pa %x.%x.%x.%x\n",
-		las->log_addr[la_idx],
-			cec_phys_addr_exp(adap->phys_addr));
-	return cec_transmit_msg(adap, &msg, false);
 }
 
 /* Transmit the Feature Abort message */
