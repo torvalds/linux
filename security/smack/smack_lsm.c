83a1e53f392075e291a90746241dce45c6f9429a
Seung-Woo Kim <sw0312.kim@samsung.com>
2016-12-12 17:35:26 +0900

    Smack: ignore private inode for file functions
    The access to fd from anon_inode is always failed because there is
    no set xattr operations. So this patch fixes to ignore private
    inode including anon_inode for file functions.
    
    It was only ignored for smack_file_receive() to share dma-buf fd,
    but dma-buf has other functions like ioctl and mmap.
    
    Reference: https://lkml.org/lkml/2015/4/17/16
    
    Signed-off-by: Seung-Woo Kim <sw0312.kim@samsung.com>
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>

--- security/smack/smack_lsm.c
+++ security/smack/smack_lsm.c
@@ -1647,6 +1647,9 @@ static int smack_file_ioctl(struct file *file, unsigned int cmd,
 	struct smk_audit_info ad;
 	struct inode *inode = file_inode(file);
 
+	if (unlikely(IS_PRIVATE(inode)))
+		return 0;
+
 	smk_ad_init(&ad, __func__, LSM_AUDIT_DATA_PATH);
 	smk_ad_setfield_u_fs_path(&ad, file->f_path);
 
@@ -1676,6 +1679,9 @@ static int smack_file_lock(struct file *file, unsigned int cmd)
 	int rc;
 	struct inode *inode = file_inode(file);
 
+	if (unlikely(IS_PRIVATE(inode)))
+		return 0;
+
 	smk_ad_init(&ad, __func__, LSM_AUDIT_DATA_PATH);
 	smk_ad_setfield_u_fs_path(&ad, file->f_path);
 	rc = smk_curacc(smk_of_inode(inode), MAY_LOCK, &ad);
@@ -1701,6 +1707,9 @@ static int smack_file_fcntl(struct file *file, unsigned int cmd,
 	struct smk_audit_info ad;
 	int rc = 0;
 	struct inode *inode = file_inode(file);
+
+	if (unlikely(IS_PRIVATE(inode)))
+		return 0;
 
 	switch (cmd) {
 	case F_GETLK:
@@ -1753,6 +1762,9 @@ static int smack_mmap_file(struct file *file,
 	int rc;
 
 	if (file == NULL)
+		return 0;
+
+	if (unlikely(IS_PRIVATE(file_inode(file))))
 		return 0;
 
 	isp = file_inode(file)->i_security;
