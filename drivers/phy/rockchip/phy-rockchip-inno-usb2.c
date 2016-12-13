6099cc2bb0ce2463caaab7171d897af6fa699fb7
William Wu <wulf@rock-chips.com>
2016-12-13 17:29:24 +0800

    phy: rockchip-inno-usb2: support otg vbus always powered on
    Some platforms (e.g. RK3399 BOX board) otg port connector
    interface is not standard, that is a Type-A connector with
    vbus always powered on, looks like to work as host mode,
    however, the otg port still need to support DRD mode.
    
    In the current code, if otg vbus is always powered on, it
    will cause USB2 PHY to detect a floating charger in error
    case and power off USB2 PHY. This patch adds a new property
    "rockchip,vbus-always-on" to fix this issue. With this patch,
    we handle this case as otg host only mode, and avoid to do
    charger detection and power off USB2 PHY.
    
    Change-Id: I69e5e87021f3f2d654793e547264aec55ac664ef
    Signed-off-by: William Wu <wulf@rock-chips.com>
    Signed-off-by: Frank Wang <frank.wang@rock-chips.com>

--- drivers/phy/rockchip/phy-rockchip-inno-usb2.c
+++ drivers/phy/rockchip/phy-rockchip-inno-usb2.c
@@ -160,6 +160,7 @@ struct rockchip_usb2phy_cfg {
  * @port_id: flag for otg port or host port.
  * @suspended: phy suspended flag.
  * @vbus_attached: otg device vbus status.
+ * @vbus_always_on: otg vbus is always powered on.
  * @bvalid_irq: IRQ number assigned for vbus valid rise detection.
  * @ls_irq: IRQ number assigned for linestate detection.
  * @otg_mux_irq: IRQ number which multiplex otg-id/otg-bvalid/linestate
@@ -178,6 +179,7 @@ struct rockchip_usb2phy_port {
 	unsigned int	port_id;
 	bool		suspended;
 	bool		vbus_attached;
+	bool		vbus_always_on;
 	int		bvalid_irq;
 	int		ls_irq;
 	int		otg_mux_irq;
@@ -410,7 +412,8 @@ static int rockchip_usb2phy_init(struct phy *phy)
 
 	if (rport->port_id == USB2PHY_PORT_OTG) {
 		if (rport->mode != USB_DR_MODE_HOST &&
-		    rport->mode != USB_DR_MODE_UNKNOWN) {
+		    rport->mode != USB_DR_MODE_UNKNOWN &&
+		    !rport->vbus_always_on) {
 			/* clear bvalid status and enable bvalid detect irq */
 			ret = property_enable(rphy->grf,
 					      &rport->port_cfg->bvalid_det_clr,
@@ -505,7 +508,8 @@ static int rockchip_usb2phy_exit(struct phy *phy)
 
 	if (rport->port_id == USB2PHY_PORT_OTG &&
 	    rport->mode != USB_DR_MODE_HOST &&
-	    rport->mode != USB_DR_MODE_UNKNOWN) {
+	    rport->mode != USB_DR_MODE_UNKNOWN &&
+	    !rport->vbus_always_on) {
 		cancel_delayed_work_sync(&rport->otg_sm_work);
 		cancel_delayed_work_sync(&rport->chg_work);
 	} else if (rport->port_id == USB2PHY_PORT_HOST)
@@ -997,9 +1001,13 @@ static int rockchip_usb2phy_otg_port_init(struct rockchip_usb2phy *rphy,
 
 	mutex_init(&rport->mutex);
 
+	rport->vbus_always_on =
+		of_property_read_bool(child_np, "rockchip,vbus-always-on");
+
 	rport->mode = of_usb_get_dr_mode_by_phy(child_np, -1);
 	if (rport->mode == USB_DR_MODE_HOST ||
-	    rport->mode == USB_DR_MODE_UNKNOWN) {
+	    rport->mode == USB_DR_MODE_UNKNOWN ||
+	    rport->vbus_always_on) {
 		ret = 0;
 		goto out;
 	}
