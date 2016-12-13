84582c8ab9479ffa4532afa95ab8d8f96b5478dc
Arnd Bergmann <arnd@arndb.de>
2016-12-12 16:41:35 -0800

    slub: avoid false-postive warning
    The slub allocator gives us some incorrect warnings when
    CONFIG_PROFILE_ANNOTATED_BRANCHES is set, as the unlikely() macro
    prevents it from seeing that the return code matches what it was before:
    
      mm/slub.c: In function `kmem_cache_free_bulk':
      mm/slub.c:262:23: error: `df.s' may be used uninitialized in this function [-Werror=maybe-uninitialized]
      mm/slub.c:2943:3: error: `df.cnt' may be used uninitialized in this function [-Werror=maybe-uninitialized]
      mm/slub.c:2933:4470: error: `df.freelist' may be used uninitialized in this function [-Werror=maybe-uninitialized]
      mm/slub.c:2943:3: error: `df.tail' may be used uninitialized in this function [-Werror=maybe-uninitialized]
    
    I have not been able to come up with a perfect way for dealing with
    this, the three options I see are:
    
     - add a bogus initialization, which would increase the runtime overhead
     - replace unlikely() with unlikely_notrace()
     - remove the unlikely() annotation completely
    
    I checked the object code for a typical x86 configuration and the last
    two cases produce the same result, so I went for the last one, which is
    the simplest.
    
    Link: http://lkml.kernel.org/r/20161024155704.3114445-1-arnd@arndb.de
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Laura Abbott <labbott@fedoraproject.org>
    Cc: Alexander Potapenko <glider@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/slub.c
+++ mm/slub.c
@@ -3076,7 +3076,7 @@ void kmem_cache_free_bulk(struct kmem_cache *s, size_t size, void **p)
 		struct detached_freelist df;
 
 		size = build_detached_freelist(s, size, p, &df);
-		if (unlikely(!df.page))
+		if (!df.page)
 			continue;
 
 		slab_free(df.s, df.page, df.freelist, df.tail, df.cnt,_RET_IP_);
