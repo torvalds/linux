7e73e2156c5ff4a2219ac832e58c7193775e3f8f
Sami Tolvanen <samitolvanen@google.com>
2016-12-14 12:32:25 -0800

    Revert "FROMLIST: arm64: Disable TTBR0_EL1 during normal kernel execution"
    This reverts commit 5775ca34829caf0664c8ccc02fd0e93cb6022e0f.
    
    Bug: 31432001
    Change-Id: I9b07c2f01bc2bcfed51f60ab487034639f5e1960
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- arch/arm64/include/asm/mmu_context.h
+++ arch/arm64/include/asm/mmu_context.h
@@ -23,7 +23,6 @@
 #include <linux/sched.h>
 
 #include <asm/cacheflush.h>
-#include <asm/cpufeature.h>
 #include <asm/proc-fns.h>
 #include <asm-generic/mm_hooks.h>
 #include <asm/cputype.h>
@@ -114,7 +113,7 @@ static inline void cpu_uninstall_idmap(void)
 	local_flush_tlb_all();
 	cpu_set_default_tcr_t0sz();
 
-	if (mm != &init_mm && !system_uses_ttbr0_pan())
+	if (mm != &init_mm)
 		cpu_switch_mm(mm->pgd, mm);
 }
 
@@ -174,26 +173,20 @@ enter_lazy_tlb(struct mm_struct *mm, struct task_struct *tsk)
 {
 }
 
-#ifdef CONFIG_ARM64_SW_TTBR0_PAN
-static inline void update_saved_ttbr0(struct task_struct *tsk,
-				      struct mm_struct *mm)
-{
-	if (system_uses_ttbr0_pan()) {
-		BUG_ON(mm->pgd == swapper_pg_dir);
-		task_thread_info(tsk)->ttbr0 =
-			virt_to_phys(mm->pgd) | ASID(mm) << 48;
-	}
-}
-#else
-static inline void update_saved_ttbr0(struct task_struct *tsk,
-				      struct mm_struct *mm)
-{
-}
-#endif
-
-static inline void __switch_mm(struct mm_struct *next)
+/*
+ * This is the actual mm switch as far as the scheduler
+ * is concerned.  No registers are touched.  We avoid
+ * calling the CPU specific function when the mm hasn't
+ * actually changed.
+ */
+static inline void
+switch_mm(struct mm_struct *prev, struct mm_struct *next,
+	  struct task_struct *tsk)
 {
 	unsigned int cpu = smp_processor_id();
+
+	if (prev == next)
+		return;
 
 	/*
 	 * init_mm.pgd does not contain any user mappings and it is always
@@ -207,23 +200,7 @@ static inline void __switch_mm(struct mm_struct *next)
 	check_and_switch_context(next, cpu);
 }
 
-static inline void
-switch_mm(struct mm_struct *prev, struct mm_struct *next,
-	  struct task_struct *tsk)
-{
-	if (prev != next)
-		__switch_mm(next);
-
-	/*
-	 * Update the saved TTBR0_EL1 of the scheduled-in task as the previous
-	 * value may have not been initialised yet (activate_mm caller) or the
-	 * ASID has changed since the last run (following the context switch
-	 * of another thread of the same process).
-	 */
-	update_saved_ttbr0(tsk, next);
-}
-
 #define deactivate_mm(tsk,mm)	do { } while (0)
-#define activate_mm(prev,next)	switch_mm(prev, next, current)
+#define activate_mm(prev,next)	switch_mm(prev, next, NULL)
 
 #endif
