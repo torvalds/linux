62383779bf598c4407cf5699c198991d56d6540c
Hajime Tazaki <thehajime@gmail.com>
2016-12-14 09:13:45 +0900

    lkl: follow up fix for Linux 4.9
    This includes the signature update of free_thread_stack(), implicit
    symbols addition of CPUIDLE_TEXT.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- arch/lkl/include/asm/thread_info.h
+++ arch/lkl/include/asm/thread_info.h
@@ -45,7 +45,7 @@ static inline struct thread_info *current_thread_info(void)
 
 /* thread information allocation */
 unsigned long *alloc_thread_stack_node(struct task_struct *, int node);
-void free_thread_stack(unsigned long *);
+void free_thread_stack(struct task_struct *tsk);
 
 void threads_init(void);
 void threads_cleanup(void);
