874bcd00f520cac297aefade201c4efc07fc8d17
Corentin Labbe <clabbe.montjoie@gmail.com>
2016-12-15 20:36:33 +0100

    apm-emulation: move APM_MINOR_DEV to include/linux/miscdevice.h
    This patch move the define for APM_MINOR_DEV to include/linux/miscdevice.h
    It is better that all minor number definitions are in the same place.
    
    Signed-off-by: Corentin Labbe <clabbe.montjoie@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/miscdevice.h
+++ include/linux/miscdevice.h
@@ -22,6 +22,7 @@
 /*#define ADB_MOUSE_MINOR	10	FIXME OBSOLETE */
 #define WATCHDOG_MINOR		130	/* Watchdog timer     */
 #define TEMP_MINOR		131	/* Temperature Sensor */
+#define APM_MINOR_DEV		134
 #define RTC_MINOR		135
 #define EFI_RTC_MINOR		136	/* EFI Time services */
 #define VHCI_MINOR		137
