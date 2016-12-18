2aaa957361f9b84fb72ce0124ebb1e5835235ba8
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2016-12-18 07:38:11 +0100

    serial: pic32_uart: Fix 'request_irq' and 'free_irq' inconsistancy
    'request_irq' and 'free_irq' should have the same 'dev_id'.
    Here one uses 'port', and the other one uses 'sport'.
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/pic32_uart.c
+++ drivers/tty/serial/pic32_uart.c
@@ -495,13 +495,13 @@ static int pic32_uart_startup(struct uart_port *port)
 
 out_t:
 	kfree(sport->irq_tx_name);
-	free_irq(sport->irq_tx, sport);
+	free_irq(sport->irq_tx, port);
 out_r:
 	kfree(sport->irq_rx_name);
-	free_irq(sport->irq_rx, sport);
+	free_irq(sport->irq_rx, port);
 out_f:
 	kfree(sport->irq_fault_name);
-	free_irq(sport->irq_fault, sport);
+	free_irq(sport->irq_fault, port);
 out_done:
 	return ret;
 }
