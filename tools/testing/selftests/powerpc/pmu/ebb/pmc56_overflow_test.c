df21d2fa733035e4d414379960f94b2516b41296
Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
2016-12-19 17:46:53 +0530

    selftest/powerpc: Wrong PMC initialized in pmc56_overflow test
    Test uses PMC2 to count the event. But PMC1 is being initialized.
    Patch to fix it.
    
    Fixes: 3752e453f6ba ('selftests/powerpc: Add tests of PMU EBBs')
    Signed-off-by: Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- tools/testing/selftests/powerpc/pmu/ebb/pmc56_overflow_test.c
+++ tools/testing/selftests/powerpc/pmu/ebb/pmc56_overflow_test.c
@@ -66,7 +66,7 @@ int pmc56_overflow(void)
 
 	FAIL_IF(ebb_event_enable(&event));
 
-	mtspr(SPRN_PMC1, pmc_sample_period(sample_period));
+	mtspr(SPRN_PMC2, pmc_sample_period(sample_period));
 	mtspr(SPRN_PMC5, 0);
 	mtspr(SPRN_PMC6, 0);
 
