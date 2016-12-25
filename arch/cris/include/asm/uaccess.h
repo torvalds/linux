a8be34459ca92d4f1034921bffd09df5441938be
Al Viro <viro@zeniv.linux.org.uk>
2016-12-25 03:33:03 -0500

    cris: switch to generic extable.h
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/cris/include/asm/uaccess.h
+++ arch/cris/include/asm/uaccess.h
@@ -50,23 +50,7 @@
 #define access_ok(type, addr, size) __access_ok((unsigned long)(addr), (size))
 
 #include <arch/uaccess.h>
-
-/*
- * The exception table consists of pairs of addresses: the first is the
- * address of an instruction that is allowed to fault, and the second is
- * the address at which the program should continue.  No registers are
- * modified, so it is entirely up to the continuation code to figure out
- * what to do.
- *
- * All the routines below use bits of fixup code that are out of line
- * with the main instruction path.  This means when everything is well,
- * we don't even have to jump over them.  Further, they do not intrude
- * on our cache or tlb entries.
- */
-
-struct exception_table_entry {
-	unsigned long insn, fixup;
-};
+#include <asm/extable.h>
 
 /*
  * These are the main single-value transfer routines.  They automatically
