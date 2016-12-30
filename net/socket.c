ab53926305170c84f6531562a99490188af79485
Eric Biggers <ebiggers@google.com>
2016-12-30 17:42:32 -0600

    net: socket: don't set sk_uid to garbage value in ->setattr()
    ->setattr() was recently implemented for socket files to sync the socket
    inode's uid to the new 'sk_uid' member of struct sock.  It does this by
    copying over the ia_uid member of struct iattr.  However, ia_uid is
    actually only valid when ATTR_UID is set in ia_valid, indicating that
    the uid is being changed, e.g. by chown.  Other metadata operations such
    as chmod or utimes leave ia_uid uninitialized.  Therefore, sk_uid could
    be set to a "garbage" value from the stack.
    
    Fix this by only copying the uid over when ATTR_UID is set.
    
    [cherry-pick of net e1a3a60a2ebe991605acb14cd58e39c0545e174e]
    
    Bug: 16355602
    Change-Id: I20e53848e54282b72a388ce12bfa88da5e3e9efe
    Fixes: 86741ec25462 ("net: core: Add a UID field to struct sock.")
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Tested-by: Lorenzo Colitti <lorenzo@google.com>
    Acked-by: Lorenzo Colitti <lorenzo@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/socket.c
+++ net/socket.c
@@ -524,7 +524,7 @@ int sockfs_setattr(struct dentry *dentry, struct iattr *iattr)
 {
 	int err = simple_setattr(dentry, iattr);
 
-	if (!err) {
+	if (!err && (iattr->ia_valid & ATTR_UID)) {
 		struct socket *sock = SOCKET_I(d_inode(dentry));
 
 		sock->sk->sk_uid = iattr->ia_uid;
