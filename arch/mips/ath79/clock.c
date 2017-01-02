b3d91db3f71d5f70ea60d900425a3f96aeb3d065
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-01-02 15:18:21 +0530

    mips: ath79: clock:- Unmap region obtained by of_iomap
    Free memory mapping, if ath79_clocks_init_dt_ng is not successful.
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Fixes: 3bdf1071ba7d ("MIPS: ath79: update devicetree clock support for AR9132")
    Cc: antonynpavlov@gmail.com
    Cc: albeu@free.fr
    Cc: hackpascal@gmail.com
    Cc: sboyd@codeaurora.org
    Cc: linux-mips@linux-mips.org
    Cc: linux-kernel@vger.kernel.org
    Patchwork: https://patchwork.linux-mips.org/patch/14915/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/ath79/clock.c
+++ arch/mips/ath79/clock.c
@@ -508,15 +508,18 @@ static void __init ath79_clocks_init_dt_ng(struct device_node *np)
 		ar9330_clk_init(ref_clk, pll_base);
 	else {
 		pr_err("%s: could not find any appropriate clk_init()\n", dnfn);
-		goto err_clk;
+		goto err_iounmap;
 	}
 
 	if (of_clk_add_provider(np, of_clk_src_onecell_get, &clk_data)) {
 		pr_err("%s: could not register clk provider\n", dnfn);
-		goto err_clk;
+		goto err_iounmap;
 	}
 
 	return;
+
+err_iounmap:
+	iounmap(pll_base);
 
 err_clk:
 	clk_put(ref_clk);
