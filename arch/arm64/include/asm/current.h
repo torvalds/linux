758c67bc727dc78b6de94644e07a5283482487c3
Mark Rutland <mark.rutland@arm.com>
2017-01-03 18:27:01 +0000

    UPSTREAM: arm64: restore get_current() optimisation
    commit 9d84fb27fa135c99c9fe3de33628774a336a70a8 upstream.
    
    Commit c02433dd6de32f04 ("arm64: split thread_info from task stack")
    inverted the relationship between get_current() and
    current_thread_info(), with sp_el0 now holding the current task_struct
    rather than the current thead_info. The new implementation of
    get_current() prevents the compiler from being able to optimize repeated
    calls to either, resulting in a noticeable penalty in some
    microbenchmarks.
    
    This patch restores the previous optimisation by implementing
    get_current() in the same way as our old current_thread_info(), using a
    non-volatile asm statement.
    
    Acked-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Reported-by: Davidlohr Bueso <dbueso@suse.de>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- arch/arm64/include/asm/current.h
+++ arch/arm64/include/asm/current.h
@@ -10,9 +10,17 @@
 #ifdef CONFIG_THREAD_INFO_IN_TASK
 struct task_struct;
 
+/*
+ * We don't use read_sysreg() as we want the compiler to cache the value where
+ * possible.
+ */
 static __always_inline struct task_struct *get_current(void)
 {
-	return (struct task_struct *)read_sysreg(sp_el0);
+	unsigned long sp_el0;
+
+	asm ("mrs %0, sp_el0" : "=r" (sp_el0));
+
+	return (struct task_struct *)sp_el0;
 }
 #define current get_current()
 #else
