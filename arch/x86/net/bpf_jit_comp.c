b55f6ca7380d609a40e0441d345404d100da3d45
Daniel Borkmann <daniel@iogearbox.net>
2017-01-07 00:26:33 +0100

    bpf: change back to orig prog on too many passes
    [ Upstream commit 9d5ecb09d525469abd1a10c096cb5a17206523f2 ]
    
    If after too many passes still no image could be emitted, then
    swap back to the original program as we do in all other cases
    and don't use the one with blinding.
    
    Fixes: 959a75791603 ("bpf, x86: add support for constant blinding")
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/net/bpf_jit_comp.c
+++ arch/x86/net/bpf_jit_comp.c
@@ -1172,6 +1172,8 @@ struct bpf_prog *bpf_int_jit_compile(struct bpf_prog *prog)
 		set_memory_ro((unsigned long)header, header->pages);
 		prog->bpf_func = (void *)image;
 		prog->jited = 1;
+	} else {
+		prog = orig_prog;
 	}
 
 out_addrs:
