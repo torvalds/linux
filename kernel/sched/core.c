8b871f71e32e48921599f308a66f5420dac7f7de
Dietmar Eggemann <dietmar.eggemann@arm.com>
2017-01-08 14:40:38 +0000

    sched/core: Remove remnants of commit fd5c98da1a42
    Commit fd5c98da1a42 "WIP: sched: Store system-wide maximum cpu capacity
    in root domain" was repalced by commit 8148bdfff4f5 "WIP: sched: Update
    max cpu capacity in case of max frequency constraints" which didn't
    remove all the now unused bits.
    
    Change-Id: I067f6366431f43337cffa7a2a8e0de32dd33d2f9
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    (cherry picked from commit 6d284a607cec51bcafca313bc396bc3103b1e876)
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7332,7 +7332,6 @@ static int build_sched_domains(const struct cpumask *cpu_map,
 	enum s_alloc alloc_state;
 	struct sched_domain *sd;
 	struct s_data d;
-	struct rq *rq = NULL;
 	int i, ret = -ENOMEM;
 
 	alloc_state = __visit_domain_allocation_hell(&d, cpu_map);
@@ -7386,7 +7385,6 @@ static int build_sched_domains(const struct cpumask *cpu_map,
 	/* Attach the domains */
 	rcu_read_lock();
 	for_each_cpu(i, cpu_map) {
-		rq = cpu_rq(i);
 		sd = *per_cpu_ptr(d.sd, i);
 		cpu_attach_domain(sd, d.rd, i);
 	}
