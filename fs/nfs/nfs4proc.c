49ad0145c3a81612482ecc064ce6ab908ae3bb1a
Anna Schumaker <Anna.Schumaker@Netapp.com>
2017-01-11 16:51:59 -0500

    NFS: Clean up nfs41_same_server_scope()
    The function is cleaner this way, since we can use the result of
    memcmp() directly
    
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -7120,11 +7120,9 @@ static bool
 nfs41_same_server_scope(struct nfs41_server_scope *a,
 			struct nfs41_server_scope *b)
 {
-	if (a->server_scope_sz == b->server_scope_sz &&
-	    memcmp(a->server_scope, b->server_scope, a->server_scope_sz) == 0)
-		return true;
-
-	return false;
+	if (a->server_scope_sz != b->server_scope_sz)
+		return false;
+	return memcmp(a->server_scope, b->server_scope, a->server_scope_sz) == 0;
 }
 
 static void
