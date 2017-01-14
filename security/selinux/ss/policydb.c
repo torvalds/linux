57152a5be08e240654993984a0f6e54254882626
Markus Elfring <elfring@users.sourceforge.net>
2017-01-14 19:35:59 +0100

    selinux: Return directly after a failed next_entry() in range_read()
    Return directly after a call of the function "next_entry" failed
    at the beginning.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- security/selinux/ss/policydb.c
+++ security/selinux/ss/policydb.c
@@ -1852,7 +1852,7 @@ static int range_read(struct policydb *p, void *fp)
 
 	rc = next_entry(buf, fp, sizeof(u32));
 	if (rc)
-		goto out;
+		return rc;
 
 	nel = le32_to_cpu(buf[0]);
 	for (i = 0; i < nel; i++) {
