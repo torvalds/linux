f21860bac05b609d71757338361d26209ff0759b
Ingo Molnar <mingo@kernel.org>
2017-01-14 17:11:36 +0100

    locking/mutex, sched/wait: Fix the mutex_lock_io_nested() define
    Mike noticed this bogosity:
    
     > > +# define mutex_lock_nest_io(lock, nest_lock) mutex_io(lock)
     >                                                 ^^^^^^^^^^^^^^ typo
    
    This new locking API is not used yet, so this didn't trigger in testing.
    
    Fix it.
    
    Reported-by: Mike Galbraith <efault@gmx.de>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: adilger.kernel@dilger.ca
    Cc: jack@suse.com
    Cc: kernel-team@fb.com
    Cc: mingbo@fb.com
    Cc: tytso@mit.edu
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/mutex.h
+++ include/linux/mutex.h
@@ -179,7 +179,7 @@ extern void mutex_lock_io(struct mutex *lock);
 # define mutex_lock_interruptible_nested(lock, subclass) mutex_lock_interruptible(lock)
 # define mutex_lock_killable_nested(lock, subclass) mutex_lock_killable(lock)
 # define mutex_lock_nest_lock(lock, nest_lock) mutex_lock(lock)
-# define mutex_lock_nest_io(lock, nest_lock) mutex_io(lock)
+# define mutex_lock_io_nested(lock, subclass) mutex_lock(lock)
 #endif
 
 /*
