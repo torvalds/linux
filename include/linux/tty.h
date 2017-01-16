9ed90d20449b01beb71a4e125d291a36c80c4ad4
Rob Herring <robh@kernel.org>
2017-01-16 16:54:28 -0600

    tty: move the non-file related parts of tty_release to new tty_release_struct
    For in-kernel tty users, we need to be able to create and destroy
    'struct tty' that are not associated with a file. The creation side is
    fine, but tty_release() needs to be split into the file handle portion
    and the struct tty portion. Introduce a new function, tty_release_struct,
    to handle just the destroying of a struct tty.
    
    Signed-off-by: Rob Herring <robh@kernel.org>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-By: Sebastian Reichel <sre@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/tty.h
+++ include/linux/tty.h
@@ -528,6 +528,7 @@ extern int tty_alloc_file(struct file *file);
 extern void tty_add_file(struct tty_struct *tty, struct file *file);
 extern void tty_free_file(struct file *file);
 extern struct tty_struct *tty_init_dev(struct tty_driver *driver, int idx);
+extern void tty_release_struct(struct tty_struct *tty, int idx);
 extern int tty_release(struct inode *inode, struct file *filp);
 extern void tty_init_termios(struct tty_struct *tty);
 extern int tty_standard_install(struct tty_driver *driver,
