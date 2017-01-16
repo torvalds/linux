34c426acb75cc21bdf84685e106db0c1a3565057
John Johansen <john.johansen@canonical.com>
2017-01-16 00:42:43 -0800

    apparmor: provide userspace flag indicating binfmt_elf_mmap change
    Commit 9f834ec18def ("binfmt_elf: switch to new creds when switching to new mm")
    changed when the creds are installed by the binfmt_elf handler. This
    affects which creds are used to mmap the executable into the address
    space. Which can have an affect on apparmor policy.
    
    Add a flag to apparmor at
    /sys/kernel/security/apparmor/features/domain/fix_binfmt_elf_mmap
    
    to make it possible to detect this semantic change so that the userspace
    tools and the regression test suite can correctly deal with the change.
    
    BugLink: http://bugs.launchpad.net/bugs/1630069
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/apparmorfs.c
+++ security/apparmor/apparmorfs.c
@@ -796,6 +796,7 @@ static struct aa_fs_entry aa_fs_entry_domain[] = {
 	AA_FS_FILE_BOOLEAN("change_hatv",	1),
 	AA_FS_FILE_BOOLEAN("change_onexec",	1),
 	AA_FS_FILE_BOOLEAN("change_profile",	1),
+	AA_FS_FILE_BOOLEAN("fix_binfmt_elf_mmap",	1),
 	{ }
 };
 
