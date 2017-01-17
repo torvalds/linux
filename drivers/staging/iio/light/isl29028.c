c4f60925984f4b22cb11ba76141855b5562d5423
Brian Masney <masneyb@onstation.org>
2017-01-17 04:24:49 -0500

    staging: iio: isl29028: fix alignment of function arguments
    Two separate calls to regmap_update_bits() in isl29028_set_als_scale()
    and isl29028_set_als_ir_mode() did not have their function arguments
    on the next line aligned correctly to the open parenthesis. This patch
    corrects the alignment.
    
    Signed-off-by: Brian Masney <masneyb@onstation.org>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/light/isl29028.c
+++ drivers/staging/iio/light/isl29028.c
@@ -116,7 +116,7 @@ static int isl29028_set_als_scale(struct isl29028_chip *chip, int lux_scale)
 					ISL29028_CONF_ALS_RANGE_LOW_LUX;
 
 	return regmap_update_bits(chip->regmap, ISL29028_REG_CONFIGURE,
-		ISL29028_CONF_ALS_RANGE_MASK, val);
+				  ISL29028_CONF_ALS_RANGE_MASK, val);
 }
 
 static int isl29028_set_als_ir_mode(struct isl29028_chip *chip,
@@ -148,7 +148,8 @@ static int isl29028_set_als_ir_mode(struct isl29028_chip *chip,
 
 	case ISL29028_MODE_NONE:
 		return regmap_update_bits(chip->regmap, ISL29028_REG_CONFIGURE,
-			ISL29028_CONF_ALS_EN_MASK, ISL29028_CONF_ALS_DIS);
+					  ISL29028_CONF_ALS_EN_MASK,
+					  ISL29028_CONF_ALS_DIS);
 	}
 
 	if (ret < 0)
