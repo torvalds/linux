2057e15945a8b5d867c086371a5fb946fd8221da
Guenter Roeck <linux@roeck-us.net>
2017-01-18 10:37:49 -0800

    Input: cros_ec_keyb - drop unnecessary call to dev_set_drvdata and other changes
    There is no call to platform_get_drvdata() or dev_get_drvdata().
    Drop the unnecessary call to dev_set_drvdata().
    Other relevant changes:
      Use existing variable 'dev' instead of dereferencing it several times
    
    This conversion was done automatically with coccinelle using the
    following semantic patches. The semantic patches and the scripts
    used to generate this commit log are available at
    https://github.com/groeck/coccinelle-patches
    
    - Drop dev_set_drvdata()
    - Use local variable 'struct device *dev' consistently
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/keyboard/cros_ec_keyb.c
+++ drivers/input/keyboard/cros_ec_keyb.c
@@ -242,7 +242,7 @@ static int cros_ec_keyb_probe(struct platform_device *pdev)
 	struct device_node *np;
 	int err;
 
-	np = pdev->dev.of_node;
+	np = dev->of_node;
 	if (!np)
 		return -ENODEV;
 
@@ -272,7 +272,6 @@ static int cros_ec_keyb_probe(struct platform_device *pdev)
 
 	ckdev->ec = ec;
 	ckdev->dev = dev;
-	dev_set_drvdata(dev, ckdev);
 
 	idev->name = CROS_EC_DEV_NAME;
 	idev->phys = ec->phys_name;
