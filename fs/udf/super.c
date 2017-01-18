70f16cef06b5bf72d50b8016e1188995eddc073a
Fabian Frederick <fabf@skynet.be>
2017-01-18 19:39:35 +0100

    udf: allow implicit blocksize specification during mount
    udf_fill_super() used udf_parse_options() to flag UDF_FLAG_BLOCKSIZE_SET
    when blocksize was specified otherwise used 512 bytes
    (bdev_logical_block_size) and 2048 bytes (UDF_DEFAULT_BLOCKSIZE)
    IOW both 1024 and 4096 specifications were required or resulted in
    
    "mount: wrong fs type, bad option, bad superblock on /dev/loop1"
    
    This patch loops through different block values but also updates
    udf_load_vrs() to return -EINVAL instead of 0 when udf_check_vsd()
    fails (and uopt->novrs = 0).
    The later being the reason for the RFC; we have that case when mounting
    a 4kb blocksize against other values but maybe VRS is not mandatory
    there ?
    
    Tested with 512, 1024, 2048 and 4096 blocksize
    
    Reported-by: Jan Kara <jack@suse.com>
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: Jan Kara <jack@suse.cz>

--- fs/udf/super.c
+++ fs/udf/super.c
@@ -1955,7 +1955,7 @@ static int udf_load_vrs(struct super_block *sb, struct udf_options *uopt,
 		if (!nsr_off) {
 			if (!silent)
 				udf_warn(sb, "No VRS found\n");
-			return 0;
+			return -EINVAL;
 		}
 		if (nsr_off == -1)
 			udf_debug("Failed to read sector at offset %d. "
@@ -2159,15 +2159,25 @@ static int udf_fill_super(struct super_block *sb, void *options, int silent)
 		ret = udf_load_vrs(sb, &uopt, silent, &fileset);
 	} else {
 		uopt.blocksize = bdev_logical_block_size(sb->s_bdev);
-		ret = udf_load_vrs(sb, &uopt, silent, &fileset);
-		if (ret == -EAGAIN && uopt.blocksize != UDF_DEFAULT_BLOCKSIZE) {
-			if (!silent)
-				pr_notice("Rescanning with blocksize %d\n",
-					  UDF_DEFAULT_BLOCKSIZE);
-			brelse(sbi->s_lvid_bh);
-			sbi->s_lvid_bh = NULL;
-			uopt.blocksize = UDF_DEFAULT_BLOCKSIZE;
+		while (uopt.blocksize <= 4096) {
 			ret = udf_load_vrs(sb, &uopt, silent, &fileset);
+			if (ret < 0) {
+				if (!silent && ret != -EACCES) {
+					pr_notice("Scanning with blocksize %d failed\n",
+						  uopt.blocksize);
+				}
+				brelse(sbi->s_lvid_bh);
+				sbi->s_lvid_bh = NULL;
+				/*
+				 * EACCES is special - we want to propagate to
+				 * upper layers that we cannot handle RW mount.
+				 */
+				if (ret == -EACCES)
+					break;
+			} else
+				break;
+
+			uopt.blocksize <<= 1;
 		}
 	}
 	if (ret < 0) {
