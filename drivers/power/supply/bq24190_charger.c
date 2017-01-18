cb190af290329c281ed2fbaf41ce6e4686b69922
Liam Breck <liam@networkimprov.net>
2017-01-18 09:26:51 -0800

    power: supply: bq24190_charger: Adjust formatting
    Add breathing room in probe() out* section.
    
    Signed-off-by: Liam Breck <kernel@networkimprov.net>
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Sebastian Reichel <sre@kernel.org>

--- drivers/power/supply/bq24190_charger.c
+++ drivers/power/supply/bq24190_charger.c
@@ -1404,15 +1404,17 @@ static int bq24190_probe(struct i2c_client *client,
 
 out4:
 	bq24190_sysfs_remove_group(bdi);
+
 out3:
 	power_supply_unregister(bdi->battery);
+
 out2:
 	power_supply_unregister(bdi->charger);
+
 out1:
 	pm_runtime_disable(dev);
 	if (bdi->gpio_int)
 		gpio_free(bdi->gpio_int);
-
 	return ret;
 }
 
