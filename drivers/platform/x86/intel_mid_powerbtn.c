1cfd3ba0c146a165951cedf293929a4983a45ae3
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2017-01-19 18:39:49 +0200

    platform/x86: intel_mid_powerbtn: Remove snail address
    The snail address is subject to change. This already happened once.
    Remove the address completely from the file to avoid potential noise
    when update.
    
    While here, adjust copyright years and list authors.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/platform/x86/intel_mid_powerbtn.c
+++ drivers/platform/x86/intel_mid_powerbtn.c
@@ -1,7 +1,10 @@
 /*
  * Power button driver for Intel MID platforms.
  *
- * Copyright (C) 2010 Intel Corp
+ * Copyright (C) 2010,2017 Intel Corp
+ *
+ * Author: Hong Liu <hong.liu@intel.com>
+ * Author: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -11,10 +14,6 @@
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
 
 #include <linux/init.h>
