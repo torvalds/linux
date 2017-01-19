319554f284dda9f2737d09df82ba3610bd8ddea3
Josef Bacik <jbacik@fb.com>
2017-01-19 17:47:46 -0500

    inet: don't use sk_v6_rcv_saddr directly
    When comparing two sockets we need to use inet6_rcv_saddr so we get a NULL
    sk_v6_rcv_saddr if the socket isn't AF_INET6, otherwise our comparison function
    can be wrong.
    
    Fixes: 637bc8b ("inet: reset tb->fastreuseport when adding a reuseport sk")
    Signed-off-by: Josef Bacik <jbacik@fb.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/inet_connection_sock.c
+++ net/ipv4/inet_connection_sock.c
@@ -99,7 +99,7 @@ int inet_rcv_saddr_equal(const struct sock *sk, const struct sock *sk2,
 #if IS_ENABLED(CONFIG_IPV6)
 	if (sk->sk_family == AF_INET6)
 		return ipv6_rcv_saddr_equal(&sk->sk_v6_rcv_saddr,
-					    &sk2->sk_v6_rcv_saddr,
+					    inet6_rcv_saddr(sk2),
 					    sk->sk_rcv_saddr,
 					    sk2->sk_rcv_saddr,
 					    ipv6_only_sock(sk),
