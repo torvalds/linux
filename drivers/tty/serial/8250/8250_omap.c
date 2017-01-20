75f54acc086940ccef8b6d67ba8537529bda104c
Vignesh R <vigneshr@ti.com>
2017-01-20 13:46:52 +0530

    serial: 8250_omap: pause DMA only if DMA transfer in progress
    It is possible that DMA transfer is already complete but, completion
    handler is yet to be called, when dmaengine_pause() is called in case of
    error condition(like break/rx timeout). This leads to dmaengine_pause()
    API to return EINVAL (as descriptor is already NULL) causing
    rx_dma_broken flag to be set and effectively disabling RX DMA.
    Fix this by calling dmaengine_pause() only when transfer is in progress.
    
    Signed-off-by: Vignesh R <vigneshr@ti.com>
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/8250/8250_omap.c
+++ drivers/tty/serial/8250/8250_omap.c
@@ -790,6 +790,7 @@ static void omap_8250_rx_dma_flush(struct uart_8250_port *p)
 {
 	struct omap8250_priv	*priv = p->port.private_data;
 	struct uart_8250_dma	*dma = p->dma;
+	struct dma_tx_state     state;
 	unsigned long		flags;
 	int ret;
 
@@ -800,10 +801,12 @@ static void omap_8250_rx_dma_flush(struct uart_8250_port *p)
 		return;
 	}
 
-	ret = dmaengine_pause(dma->rxchan);
-	if (WARN_ON_ONCE(ret))
-		priv->rx_dma_broken = true;
-
+	ret = dmaengine_tx_status(dma->rxchan, dma->rx_cookie, &state);
+	if (ret == DMA_IN_PROGRESS) {
+		ret = dmaengine_pause(dma->rxchan);
+		if (WARN_ON_ONCE(ret))
+			priv->rx_dma_broken = true;
+	}
 	spin_unlock_irqrestore(&priv->rx_dma_lock, flags);
 
 	__dma_rx_do_complete(p);
