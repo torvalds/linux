5c4fa2a6da7fbc76290d1cb54a7e35633517a522
Hans de Goede <hdegoede@redhat.com>
2017-01-21 11:14:14 -0800

    Input: soc_button_array - debounce the buttons
    The soc_button_array driver was initializing (kzalloc) the
    debounce_interval value to 0, leading to no debouncing at all,
    while the buttons are simple mechanical switches.
    
    This commit sets debounce_interval to 50ms to avoid spurious button
    press reports both on press and release of the button. Note 50ms may
    seem like a lot but soc_button_array is typically used with cheap
    tablets, with not so great buttons. I tried 10ms on my tablet and it
    is not enough, where as 50ms works well.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/misc/soc_button_array.c
+++ drivers/input/misc/soc_button_array.c
@@ -102,6 +102,8 @@ soc_button_device_create(struct platform_device *pdev,
 		gpio_keys[n_buttons].active_low = 1;
 		gpio_keys[n_buttons].desc = info->name;
 		gpio_keys[n_buttons].wakeup = info->wakeup;
+		/* These devices often use cheap buttons, use 50 ms debounce */
+		gpio_keys[n_buttons].debounce_interval = 50;
 		n_buttons++;
 	}
 
