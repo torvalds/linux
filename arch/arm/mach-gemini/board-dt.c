6dbb708a8fb8eef552d8de2b7574d92741b3bb31
Linus Walleij <linus.walleij@linaro.org>
2017-01-21 22:43:08 +0100

    ARM: gemini: convert to ARMv4 multiplatform
    This converts the Gemini platform to ARMv4 multiplatform, deleting
    the local <mach/*> include directory, moving an idiomatic local
    idling function into the .machine_init() call and getting rid of
    the Makefile.boot finally.
    
    Cc: Janos Laube <janos.dev@gmail.com>
    Cc: Paulius Zaleckas <paulius.zaleckas@gmail.com>
    Cc: Hans Ulli Kroll <ulli.kroll@googlemail.com>
    Cc: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-gemini/board-dt.c
+++ arch/arm/mach-gemini/board-dt.c
@@ -7,6 +7,8 @@
 
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
+#include <asm/system_misc.h>
+#include <asm/proc-fns.h>
 
 #ifdef CONFIG_DEBUG_GEMINI
 /* This is needed for LL-debug/earlyprintk/debug-macro.S */
@@ -27,6 +29,27 @@ static void __init gemini_map_io(void)
 #define gemini_map_io NULL
 #endif
 
+static void gemini_idle(void)
+{
+	/*
+	 * Because of broken hardware we have to enable interrupts or the CPU
+	 * will never wakeup... Acctualy it is not very good to enable
+	 * interrupts first since scheduler can miss a tick, but there is
+	 * no other way around this. Platforms that needs it for power saving
+	 * should enable it in init code, since by default it is
+	 * disabled.
+	 */
+
+	/* FIXME: Enabling interrupts here is racy! */
+	local_irq_enable();
+	cpu_do_idle();
+}
+
+static void __init gemini_init_machine(void)
+{
+	arm_pm_idle = gemini_idle;
+}
+
 static const char *gemini_board_compat[] = {
 	"cortina,gemini",
 	NULL,
@@ -34,5 +57,6 @@ static const char *gemini_board_compat[] = {
 
 DT_MACHINE_START(GEMINI_DT, "Gemini (Device Tree)")
 	.map_io		= gemini_map_io,
+	.init_machine	= gemini_init_machine,
 	.dt_compat	= gemini_board_compat,
 MACHINE_END
