f6a3b168e56abe094b5c93dbafc2f0a0bf64e702
Markus Elfring <elfring@users.sourceforge.net>
2017-01-22 11:30:21 +0100

    KVM: Return directly after a failed copy_from_user() in kvm_vm_compat_ioctl()
    * Return directly after a call of the function "copy_from_user" failed
      in a case block.
    
      This issue was detected by using the Coccinelle software.
    
    * Delete the jump label "out" which became unnecessary with
      this refactoring.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -3148,10 +3148,9 @@ static long kvm_vm_compat_ioctl(struct file *filp,
 		struct compat_kvm_dirty_log compat_log;
 		struct kvm_dirty_log log;
 
-		r = -EFAULT;
 		if (copy_from_user(&compat_log, (void __user *)arg,
 				   sizeof(compat_log)))
-			goto out;
+			return -EFAULT;
 		log.slot	 = compat_log.slot;
 		log.padding1	 = compat_log.padding1;
 		log.padding2	 = compat_log.padding2;
@@ -3163,8 +3162,6 @@ static long kvm_vm_compat_ioctl(struct file *filp,
 	default:
 		r = kvm_vm_ioctl(filp, ioctl, arg);
 	}
-
-out:
 	return r;
 }
 #endif
