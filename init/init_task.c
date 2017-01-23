b7e78624284d057c531061c79a9bf41acbe30c6b
Deez Nutz <DeezNutz2017@users.noreply.github.com>
2017-01-23 15:24:14 +0000

    Update init_task.c

--- init/init_task.c
+++ init/init_task.c
@@ -12,11 +12,9 @@
 #include <linux/uaccess.h>
 
 static struct signal_struct init_signals = INIT_SIGNALS(init_signals);
-static struct sighand_struct init_sighand = INIT_SIGHAND(init_sighand);
 
 /* Initial task structure */
 struct task_struct init_task = INIT_TASK(init_task);
-EXPORT_SYMBOL(init_task);
 
 /*
  * Initial thread structure. Alignment of this is handled by a special
@@ -24,6 +22,5 @@ EXPORT_SYMBOL(init_task);
  */
 union thread_union init_thread_union __init_task_data = {
 #ifndef CONFIG_THREAD_INFO_IN_TASK
-	INIT_THREAD_INFO(init_task)
 #endif
 };
