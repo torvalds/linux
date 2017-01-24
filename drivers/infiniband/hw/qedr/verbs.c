865cea40b69741c3da2574176876463233b2b67c
Ram Amrani <Ram.Amrani@Cavium.com>
2017-01-24 13:50:34 +0200

    RDMA/qedr: Return success when not changing QP state
    If the user is requesting us to change the QP state to the same state
    that it is already in, return success instead of failure.
    
    Signed-off-by: Ram Amrani <Ram.Amrani@cavium.com>
    Signed-off-by: Michal Kalderon <Michal.Kalderon@cavium.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/qedr/verbs.c
+++ drivers/infiniband/hw/qedr/verbs.c
@@ -1657,7 +1657,7 @@ static int qedr_update_qp_state(struct qedr_dev *dev,
 	int status = 0;
 
 	if (new_state == qp->state)
-		return 1;
+		return 0;
 
 	switch (qp->state) {
 	case QED_ROCE_QP_STATE_RESET:
