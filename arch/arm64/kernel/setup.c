cbb999dd0b452991f4f698142aa7ffe566c0b415
Geert Uytterhoeven <geert+renesas@glider.be>
2017-01-24 12:43:40 +0100

    arm64: Use __pa_symbol for empty_zero_page
    If CONFIG_DEBUG_VIRTUAL=y and CONFIG_ARM64_SW_TTBR0_PAN=y:
    
        virt_to_phys used for non-linear address: ffffff8008cc0000 (empty_zero_page+0x0/0x1000)
        WARNING: CPU: 0 PID: 0 at arch/arm64/mm/physaddr.c:14 __virt_to_phys+0x28/0x60
        ...
        [<ffffff800809abb4>] __virt_to_phys+0x28/0x60
        [<ffffff8008a02600>] setup_arch+0x46c/0x4d4
    
    Fixes: 2077be6783b5936c ("arm64: Use __pa_symbol for kernel symbols")
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Laura Abbott <labbott@redhat.com>
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -298,7 +298,7 @@ void __init setup_arch(char **cmdline_p)
 	 * faults in case uaccess_enable() is inadvertently called by the init
 	 * thread.
 	 */
-	init_task.thread_info.ttbr0 = virt_to_phys(empty_zero_page);
+	init_task.thread_info.ttbr0 = __pa_symbol(empty_zero_page);
 #endif
 
 #ifdef CONFIG_VT
