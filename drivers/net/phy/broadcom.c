85b4685da52f6680635370c2cfb9a42f04ac9652
Rafał Miłecki <rafal@milecki.pl>
2017-01-25 21:00:25 +0100

    net: phy: broadcom: use auxctl reading helper in BCM54612E code
    Starting with commit 5b4e29005123 ("net: phy: broadcom: add
    bcm54xx_auxctl_read") we have a reading helper so use it and avoid code
    duplication.
    It also means we don't need MII_BCM54XX_AUXCTL_SHDWSEL_MISC define as
    it's the same as MII_BCM54XX_AUXCTL_SHDWSEL_MISC just for reading needs
    (same value shifted by 12 bits).
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/broadcom.c
+++ drivers/net/phy/broadcom.c
@@ -395,10 +395,8 @@ static int bcm54612e_config_aneg(struct phy_device *phydev)
 	    (phydev->interface != PHY_INTERFACE_MODE_RGMII_RXID)) {
 		u16 reg;
 
-		/* Errata: reads require filling in the write selector field */
-		bcm54xx_auxctl_write(phydev, MII_BCM54XX_AUXCTL_SHDWSEL_MISC,
-				     MII_BCM54XX_AUXCTL_MISC_RDSEL_MISC);
-		reg = phy_read(phydev, MII_BCM54XX_AUX_CTL);
+		reg = bcm54xx_auxctl_read(phydev,
+					  MII_BCM54XX_AUXCTL_SHDWSEL_MISC);
 		/* Disable RXD to RXC delay (default set) */
 		reg &= ~MII_BCM54XX_AUXCTL_MISC_RXD_RXC_SKEW;
 		/* Clear shadow selector field */
