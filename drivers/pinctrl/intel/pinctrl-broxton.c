ece2dac9f13eff4ce446c08ca5e76b1b0188089b
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2017-01-25 22:29:34 +0200

    pinctrl: broxton: Rename apl-pinctrl driver
    While we have no users yet rename the platform driver to use the same
    pattern as the rest of Intel SoCs, i.e. use full SoC name in
    'apollolake-pinctrl'.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/intel/pinctrl-broxton.c
+++ drivers/pinctrl/intel/pinctrl-broxton.c
@@ -1004,7 +1004,7 @@ static const struct acpi_device_id bxt_pinctrl_acpi_match[] = {
 MODULE_DEVICE_TABLE(acpi, bxt_pinctrl_acpi_match);
 
 static const struct platform_device_id bxt_pinctrl_platform_ids[] = {
-	{ "apl-pinctrl", (kernel_ulong_t)&apl_pinctrl_soc_data },
+	{ "apollolake-pinctrl", (kernel_ulong_t)&apl_pinctrl_soc_data },
 	{ "broxton-pinctrl", (kernel_ulong_t)&bxt_pinctrl_soc_data },
 	{ },
 };
