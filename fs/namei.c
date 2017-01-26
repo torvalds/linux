1328c727004d432bbdfba0ffa02a166df04c7305
Seth Forshee <seth.forshee@canonical.com>
2017-01-26 14:33:46 -0600

    vfs: open() with O_CREAT should not create inodes with unknown ids
    may_create() rejects creation of inodes with ids which lack a
    mapping into s_user_ns. However for O_CREAT may_o_create() is
    is used instead. Add a similar check there.
    
    Fixes: 036d523641c6 ("vfs: Don't create inodes with a uid or gid unknown to the vfs")
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- fs/namei.c
+++ fs/namei.c
@@ -2941,9 +2941,15 @@ static inline int open_to_namei_flags(int flag)
 
 static int may_o_create(const struct path *dir, struct dentry *dentry, umode_t mode)
 {
+	struct user_namespace *s_user_ns;
 	int error = security_path_mknod(dir, dentry, mode, 0);
 	if (error)
 		return error;
+
+	s_user_ns = dir->dentry->d_sb->s_user_ns;
+	if (!kuid_has_mapping(s_user_ns, current_fsuid()) ||
+	    !kgid_has_mapping(s_user_ns, current_fsgid()))
+		return -EOVERFLOW;
 
 	error = inode_permission(dir->dentry->d_inode, MAY_WRITE | MAY_EXEC);
 	if (error)
