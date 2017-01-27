768851b92cf224a6dc37cb99d78dd94377d51f0e
Gabriel Krisman Bertazi <krisman@collabora.co.uk>
2017-01-26 23:05:45 -0200

    drm: qxl: Drop misleading comment
    No longer true since commit 07f8d9bdb235 ("drm/qxl: add support for > 1
    output").  qxl_num_crtc defaults to 4 and is configurable as a module
    parameter.
    
    Signed-off-by: Gabriel Krisman Bertazi <krisman@collabora.co.uk>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170127010548.27970-1-krisman@collabora.co.uk
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- drivers/gpu/drm/qxl/qxl_fb.c
+++ drivers/gpu/drm/qxl/qxl_fb.c
@@ -399,7 +399,7 @@ int qxl_fbdev_init(struct qxl_device *qdev)
 			      &qxl_fb_helper_funcs);
 
 	ret = drm_fb_helper_init(qdev->ddev, &qfbdev->helper,
-				 qxl_num_crtc /* num_crtc - QXL supports just 1 */,
+				 qxl_num_crtc,
 				 QXLFB_CONN_LIMIT);
 	if (ret)
 		goto free;
