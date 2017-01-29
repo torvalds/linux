aad88bde74dc8530a5a8332a78f608f8e66cd038
Andriy Skulysh <andriy.skulysh@seagate.com>
2017-01-28 19:04:56 -0500

    staging: lustre: ldlm: ASSERTION(flock->blocking_export!=0) failed
    Whole policy structure was zeroed twice. Once during enqueue
    and second time during resend or replay. Policy structure
    should be initialized with default values only in ldlm_lock_new().
    
    Signed-off-by: Andriy Skulysh <andriy.skulysh@seagate.com>
    Signed-off-by: Ben Evans <bevans@cray.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-8349
    Seagate-bug-id: MRP-2536, MRP-2909
    Reviewed-on: http://review.whamcloud.com/21061
    Reviewed-by: Alexander Boyko <alexander.boyko@seagate.com>
    Reviewed-by: Vitaly Fertman <vitaly.fertman@seagate.com>
    Reviewed-by: Mike Pershin <mike.pershin@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ldlm/ldlm_extent.c
+++ drivers/staging/lustre/lustre/ldlm/ldlm_extent.c
@@ -243,7 +243,6 @@ void ldlm_extent_unlink_lock(struct ldlm_lock *lock)
 void ldlm_extent_policy_wire_to_local(const union ldlm_wire_policy_data *wpolicy,
 				      union ldlm_policy_data *lpolicy)
 {
-	memset(lpolicy, 0, sizeof(*lpolicy));
 	lpolicy->l_extent.start = wpolicy->l_extent.start;
 	lpolicy->l_extent.end = wpolicy->l_extent.end;
 	lpolicy->l_extent.gid = wpolicy->l_extent.gid;
--- drivers/staging/lustre/lustre/ldlm/ldlm_inodebits.c
+++ drivers/staging/lustre/lustre/ldlm/ldlm_inodebits.c
@@ -57,7 +57,6 @@
 void ldlm_ibits_policy_wire_to_local(const union ldlm_wire_policy_data *wpolicy,
 				     union ldlm_policy_data *lpolicy)
 {
-	memset(lpolicy, 0, sizeof(*lpolicy));
 	lpolicy->l_inodebits.bits = wpolicy->l_inodebits.bits;
 }
 
