8e7a7362c3cbb67b867a39548f6b6e2eac58c01c
James Simmons <jsimmons@infradead.org>
2017-01-28 19:05:04 -0500

    staging: lustre: header: remove assert from interval_set()
    In the case of interval_tree.h only interval_set()
    uses LASSERT which is removed in this patch and
    interval_set() instead reports a real error. The
    header libcfs.h for interval_tree.h is not needed
    anymore so we can just use the standard linux
    kernel headers instead.h
    
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-6401
    Reviewed-on: https://review.whamcloud.com/22522
    Reviewed-on: https://review.whamcloud.com/24323
    Reviewed-by: Frank Zago <fzago@cray.com>
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ldlm/ldlm_extent.c
+++ drivers/staging/lustre/lustre/ldlm/ldlm_extent.c
@@ -162,7 +162,7 @@ void ldlm_extent_add_lock(struct ldlm_resource *res,
 	struct interval_node *found, **root;
 	struct ldlm_interval *node;
 	struct ldlm_extent *extent;
-	int idx;
+	int idx, rc;
 
 	LASSERT(lock->l_granted_mode == lock->l_req_mode);
 
@@ -176,7 +176,8 @@ void ldlm_extent_add_lock(struct ldlm_resource *res,
 
 	/* node extent initialize */
 	extent = &lock->l_policy_data.l_extent;
-	interval_set(&node->li_node, extent->start, extent->end);
+	rc = interval_set(&node->li_node, extent->start, extent->end);
+	LASSERT(!rc);
 
 	root = &res->lr_itree[idx].lit_root;
 	found = interval_insert(&node->li_node, root);
