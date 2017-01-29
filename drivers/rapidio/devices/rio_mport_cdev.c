0ca36a6bbd85bad72596a15e1505483352333c88
Al Viro <viro@zeniv.linux.org.uk>
2017-01-29 03:12:00 -0500

    rapidio: switch to get_user_pages_fast()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/rapidio/devices/rio_mport_cdev.c
+++ drivers/rapidio/devices/rio_mport_cdev.c
@@ -889,11 +889,9 @@ rio_dma_transfer(struct file *filp, u32 transfer_mode,
 			goto err_req;
 		}
 
-		pinned = get_user_pages_unlocked(
+		pinned = get_user_pages_fast(
 				(unsigned long)xfer->loc_addr & PAGE_MASK,
-				nr_pages,
-				page_list,
-				dir == DMA_FROM_DEVICE ? FOLL_WRITE : 0);
+				nr_pages, dir == DMA_FROM_DEVICE, page_list);
 
 		if (pinned != nr_pages) {
 			if (pinned < 0) {
