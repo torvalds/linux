57a66aafda62aacd15f0d1f4f260cc0efdf1d0e4
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2017-01-30 11:02:00 +0100

    ARM: davinci: da8xx-dt: add OF_DEV_AUXDATA entry for SATA
    This entry is needed for the ahci driver to get a functional clock.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    [nsekhar@ti.com: subject line adjustment]
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- arch/arm/mach-davinci/da8xx-dt.c
+++ arch/arm/mach-davinci/da8xx-dt.c
@@ -42,6 +42,7 @@ static struct of_dev_auxdata da850_auxdata_lookup[] __initdata = {
 	OF_DEV_AUXDATA("ti,da830-ohci", 0x01e25000, "ohci-da8xx", NULL),
 	OF_DEV_AUXDATA("ti,da830-musb", 0x01e00000, "musb-da8xx", NULL),
 	OF_DEV_AUXDATA("ti,da830-usb-phy", 0x01c1417c, "da8xx-usb-phy", NULL),
+	OF_DEV_AUXDATA("ti,da850-ahci", 0x01e18000, "ahci_da850", NULL),
 	{}
 };
 
