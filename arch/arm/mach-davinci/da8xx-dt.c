00bacfbfaf508940fd5fba22372a0935c4a01358
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2017-01-30 11:02:03 +0100

    ARM: davinci: da850: model the SATA refclk
    Register a fixed rate clock modelling the external SATA oscillator
    for da850 (both DT and board file mode).
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- arch/arm/mach-davinci/da8xx-dt.c
+++ arch/arm/mach-davinci/da8xx-dt.c
@@ -50,6 +50,9 @@ static struct of_dev_auxdata da850_auxdata_lookup[] __initdata = {
 
 static void __init da850_init_machine(void)
 {
+	/* All existing boards use 100MHz SATA refclkpn */
+	static const unsigned long sata_refclkpn = 100 * 1000 * 1000;
+
 	int ret;
 
 	ret = da8xx_register_usb20_phy_clk(false);
@@ -59,6 +62,11 @@ static void __init da850_init_machine(void)
 	ret = da8xx_register_usb11_phy_clk(false);
 	if (ret)
 		pr_warn("%s: registering USB 1.1 PHY clock failed: %d",
+			__func__, ret);
+
+	ret = da850_register_sata_refclk(sata_refclkpn);
+	if (ret)
+		pr_warn("%s: registering SATA REFCLK failed: %d",
 			__func__, ret);
 
 	of_platform_default_populate(NULL, da850_auxdata_lookup, NULL);
