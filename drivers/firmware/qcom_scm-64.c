82bcd087029f6056506ea929f11af02622230901
Andy Gross <andy.gross@linaro.org>
2017-02-01 11:28:28 -0600

    firmware: qcom: scm: Fix interrupted SCM calls
    This patch adds a Qualcomm specific quirk to the arm_smccc_smc call.
    
    On Qualcomm ARM64 platforms, the SMC call can return before it has
    completed.  If this occurs, the call can be restarted, but it requires
    using the returned session ID value from the interrupted SMC call.
    
    The quirk stores off the session ID from the interrupted call in the
    quirk structure so that it can be used by the caller.
    
    This patch folds in a fix given by Sricharan R:
    https://lkml.org/lkml/2016/9/28/272
    
    Signed-off-by: Andy Gross <andy.gross@linaro.org>
    Reviewed-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- drivers/firmware/qcom_scm-64.c
+++ drivers/firmware/qcom_scm-64.c
@@ -91,6 +91,7 @@ static int qcom_scm_call(struct device *dev, u32 svc_id, u32 cmd_id,
 	dma_addr_t args_phys = 0;
 	void *args_virt = NULL;
 	size_t alloc_len;
+	struct arm_smccc_quirk quirk = {.id = ARM_SMCCC_QUIRK_QCOM_A6};
 
 	if (unlikely(arglen > N_REGISTER_ARGS)) {
 		alloc_len = N_EXT_QCOM_SCM_ARGS * sizeof(u64);
@@ -131,10 +132,16 @@ static int qcom_scm_call(struct device *dev, u32 svc_id, u32 cmd_id,
 					 qcom_smccc_convention,
 					 ARM_SMCCC_OWNER_SIP, fn_id);
 
+		quirk.state.a6 = 0;
+
 		do {
-			arm_smccc_smc(cmd, desc->arginfo, desc->args[0],
-				      desc->args[1], desc->args[2], x5, 0, 0,
-				      res);
+			arm_smccc_smc_quirk(cmd, desc->arginfo, desc->args[0],
+				      desc->args[1], desc->args[2], x5,
+				      quirk.state.a6, 0, res, &quirk);
+
+			if (res->a0 == QCOM_SCM_INTERRUPTED)
+				cmd = res->a0;
+
 		} while (res->a0 == QCOM_SCM_INTERRUPTED);
 
 		mutex_unlock(&qcom_scm_lock);
