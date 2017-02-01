1fe57d67e093d526d8206b071a086cd11a740945
Chen-Yu Tsai <wens@csie.org>
2017-02-02 00:02:09 +0800

    pinctrl: sunxi: Remove stray printk call in sun5i driver's probe function
    There is a stray printk call in the new sun5i pinctrl driver's probe
    function.
    
    Remove it.
    
    Signed-off-by: Chen-Yu Tsai <wens@csie.org>
    Acked-by: Maxime Ripard <maxime.ripard@free-electrons.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/sunxi/pinctrl-sun5i.c
+++ drivers/pinctrl/sunxi/pinctrl-sun5i.c
@@ -719,8 +719,6 @@ static int sun5i_pinctrl_probe(struct platform_device *pdev)
 {
 	unsigned long variant = (unsigned long)of_device_get_match_data(&pdev->dev);
 
-	printk("prout\n");
-
 	return sunxi_pinctrl_init_with_variant(pdev, &sun5i_pinctrl_data,
 					       variant);
 }
