f820c0ac6cc84d4f9cb22c77d99ea510bd5c382a
Bert Kenward <bkenward@solarflare.com>
2017-02-06 16:50:55 +0000

    sfc: don't rearm interrupts if busy polling
    Since commit 364b6055738b ("net: busy-poll: return busypolling status
    to drivers"), napi_complete_done() returns a boolean that can be used
    by drivers to conditionally rearm interrupts.
    
    Testing with a 7142 shows a small latency improvement of ~100 ns.
    
    Signed-off-by: Bert Kenward <bkenward@solarflare.com>
    Cc: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/sfc/efx.c
+++ drivers/net/ethernet/sfc/efx.c
@@ -328,8 +328,8 @@ static int efx_poll(struct napi_struct *napi, int budget)
 		 * since efx_nic_eventq_read_ack() will have no effect if
 		 * interrupts have already been disabled.
 		 */
-		napi_complete_done(napi, spent);
-		efx_nic_eventq_read_ack(channel);
+		if (napi_complete_done(napi, spent))
+			efx_nic_eventq_read_ack(channel);
 	}
 
 	return spent;
