734e02aaee5addec3afc1d34aff35a381c3b797d
Andrew Morton <akpm@linux-foundation.org>
2017-02-08 10:26:46 +1100

    lib-show_memc-teach-show_mem-to-work-with-the-given-nodemask-checkpatch-fixes
    WARNING: line over 80 characters
    #214: FILE: mm/page_alloc.c:4332:
    +static bool show_mem_node_skip(unsigned int flags, int nid, nodemask_t *nodemask)
    
    WARNING: line over 80 characters
    #230: FILE: mm/page_alloc.c:4338:
    +	 * no node mask - aka implicit memory numa policy. Do not bother with the
    
    WARNING: line over 80 characters
    #231: FILE: mm/page_alloc.c:4339:
    +	 * synchronization - read_mems_allowed_begin - because we do not have to be
    
    total: 0 errors, 3 warnings, 198 lines checked
    
    NOTE: For some of the reported defects, checkpatch may be able to
          mechanically convert to the typical style using --fix or --fix-inplace.
    
    ./patches/lib-show_memc-teach-show_mem-to-work-with-the-given-nodemask.patch has style problems, please review.
    
    NOTE: If any of the errors are false positives, please report
          them to the maintainer, see CHECKPATCH in MAINTAINERS.
    
    Please run checkpatch prior to sending patches
    
    Cc: Michal Hocko <mhocko@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4280,9 +4280,9 @@ static bool show_mem_node_skip(unsigned int flags, int nid, nodemask_t *nodemask
 		return false;
 
 	/*
-	 * no node mask - aka implicit memory numa policy. Do not bother with the
-	 * synchronization - read_mems_allowed_begin - because we do not have to be
-	 * precise here.
+	 * no node mask - aka implicit memory numa policy. Do not bother with
+	 * the synchronization - read_mems_allowed_begin - because we do not
+	 * have to be precise here.
 	 */
 	if (!nodemask)
 		nodemask = &cpuset_current_mems_allowed;
