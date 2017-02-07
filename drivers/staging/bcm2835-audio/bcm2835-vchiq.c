0afad91356392ab53ce3b3438173972b78bf9089
Dan Carpenter <dan.carpenter@oracle.com>
2017-02-07 16:17:34 +0300

    Staging: bcm2835-audio: fix an uninitialized return value
    "ret" isn't necessarily initialized on the success path.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm2835-audio/bcm2835-vchiq.c
+++ drivers/staging/bcm2835-audio/bcm2835-vchiq.c
@@ -432,6 +432,7 @@ static int bcm2835_audio_open_connection(struct bcm2835_alsa_stream *alsa_stream
 	alsa_stream->instance = instance;
 
 	LOG_DBG(" success !\n");
+	ret = 0;
 err_free_mem:
 	LOG_DBG(" .. OUT\n");
 
