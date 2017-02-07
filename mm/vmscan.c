d81f79c08bbed8b3dfbe9a265dbec80c46e8ab9e
Vinayak Menon <vinmenon@codeaurora.org>
2017-02-08 10:27:39 +1100

    mm: vmscan: do not pass reclaimed slab to vmpressure
    During global reclaim, the nr_reclaimed passed to vmpressure includes the
    pages reclaimed from slab.  But the corresponding scanned slab pages is
    not passed.  This can cause total reclaimed pages to be greater than
    scanned, causing an unsigned underflow in vmpressure resulting in a
    critical event being sent to root cgroup.  So do not consider reclaimed
    slab pages for vmpressure calculation.  The reclaimed pages from slab can
    be excluded because the freeing of a page by slab shrinking depends on
    each slab's object population, making the cost model (i.e.  scan:free)
    different from that of LRU.  Also, not every shrinker accounts the pages
    it reclaims.
    
    This is a regression introduced by commit 6b4f7799c6a5 ("mm: vmscan:
    invoke slab shrinkers from shrink_zone()").
    
    Fixes: 6b4f7799c6a5 ("mm: vmscan: invoke slab shrinkers from shrink_zone()")
    Link: http://lkml.kernel.org/r/1485853328-7672-1-git-send-email-vinmenon@codeaurora.org
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
    Cc: Anton Vorontsov <anton.vorontsov@linaro.org>
    Cc: Shiraz Hashim <shashim@codeaurora.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2603,15 +2603,22 @@ static bool shrink_node(pg_data_t *pgdat, struct scan_control *sc)
 				    sc->nr_scanned - nr_scanned,
 				    node_lru_pages);
 
+		/*
+		 * Record the subtree's reclaim efficiency. The reclaimed
+		 * pages from slab is excluded here because the corresponding
+		 * scanned pages is not accounted. Moreover, freeing a page
+		 * by slab shrinking depends on each slab's object population,
+		 * making the cost model (i.e. scan:free) different from that
+		 * of LRU.
+		 */
+		vmpressure(sc->gfp_mask, sc->target_mem_cgroup, true,
+			   sc->nr_scanned - nr_scanned,
+			   sc->nr_reclaimed - nr_reclaimed);
+
 		if (reclaim_state) {
 			sc->nr_reclaimed += reclaim_state->reclaimed_slab;
 			reclaim_state->reclaimed_slab = 0;
 		}
-
-		/* Record the subtree's reclaim efficiency */
-		vmpressure(sc->gfp_mask, sc->target_mem_cgroup, true,
-			   sc->nr_scanned - nr_scanned,
-			   sc->nr_reclaimed - nr_reclaimed);
 
 		if (sc->nr_reclaimed - nr_reclaimed)
 			reclaimable = true;
