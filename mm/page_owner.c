77b230992c16a4a44c9136a0f466787b953d580d
zhong jiang <zhongjiang@huawei.com>
2017-02-08 10:27:46 +1100

    mm/page_owner: align with pageblock_nr pages
    When pfn_valid(pfn) returns false, pfn should be aligned with
    pageblock_nr_pages other than MAX_ORDER_NR_PAGES in init_pages_in_zone,
    because the skipped 2M may be valid pfn, as a result, early allocated
    count will not be accurate.
    
    Link: http://lkml.kernel.org/r/1468938136-24228-1-git-send-email-zhongjiang@huawei.com
    Signed-off-by: zhong jiang <zhongjiang@huawei.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_owner.c
+++ mm/page_owner.c
@@ -527,7 +527,7 @@ static void init_pages_in_zone(pg_data_t *pgdat, struct zone *zone)
 	 */
 	for (; pfn < end_pfn; ) {
 		if (!pfn_valid(pfn)) {
-			pfn = ALIGN(pfn + 1, MAX_ORDER_NR_PAGES);
+			pfn = ALIGN(pfn + 1, pageblock_nr_pages);
 			continue;
 		}
 
