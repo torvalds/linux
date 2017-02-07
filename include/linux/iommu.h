c73e1ac8b2bc6ab18d9f9a96b17ee7388b49a0c0
Joerg Roedel <jroedel@suse.de>
2017-02-07 18:18:46 +0100

    iommu: Add iommu_device_set_fwnode() interface
    Allow to store a fwnode in 'struct iommu_device';
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -214,6 +214,7 @@ struct iommu_ops {
 struct iommu_device {
 	struct list_head list;
 	const struct iommu_ops *ops;
+	struct fwnode_handle *fwnode;
 	struct device dev;
 };
 
@@ -231,6 +232,12 @@ static inline void iommu_device_set_ops(struct iommu_device *iommu,
 					const struct iommu_ops *ops)
 {
 	iommu->ops = ops;
+}
+
+static inline void iommu_device_set_fwnode(struct iommu_device *iommu,
+					   struct fwnode_handle *fwnode)
+{
+	iommu->fwnode = fwnode;
 }
 
 #define IOMMU_GROUP_NOTIFY_ADD_DEVICE		1 /* Device added */
@@ -577,6 +584,11 @@ static inline int  iommu_device_register(struct iommu_device *iommu)
 
 static inline void iommu_device_set_ops(struct iommu_device *iommu,
 					const struct iommu_ops *ops)
+{
+}
+
+static inline void iommu_device_set_fwnode(struct iommu_device *iommu,
+					   struct fwnode_handle *fwnode)
 {
 }
 
