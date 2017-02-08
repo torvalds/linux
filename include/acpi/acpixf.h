097472134851b237bc7c2c1e3996364bdcc61942
Bob Moore <robert.moore@intel.com>
2017-02-08 11:00:22 +0800

    ACPICA: Update version to 20170119
    ACPICA commit 711a8c19d3c646fdc069c38912d9037c7fa5e718
    
    Version 20170119.
    
    Link: https://github.com/acpica/acpica/commit/711a8c19
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/acpixf.h
+++ include/acpi/acpixf.h
@@ -46,7 +46,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20161222
+#define ACPI_CA_VERSION                 0x20170119
 
 #include <acpi/acconfig.h>
 #include <acpi/actypes.h>
