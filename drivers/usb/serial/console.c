0e517c93dc027e49d4523fe32631606b12f0752d
Johan Hovold <johan@kernel.org>
2017-02-08 18:53:09 +0100

    USB: serial: console: clean up sanity checks
    Drop two redundant NULL checks from usb_serial_console_disconnect().
    
    The usb_serial_console_disconnect function is called from the
    USB-serial-device disconnect callback when a device is going away. Hence
    there is no need to check for the serial-device pointer being NULL.
    
    The serial-device port pointers are stored in an array that is a member
    of the serial struct so the address of the first member of the array
    (which the array name decays to) is never NULL either.
    
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Johan Hovold <johan@kernel.org>

--- drivers/usb/serial/console.c
+++ drivers/usb/serial/console.c
@@ -265,8 +265,7 @@ static struct console usbcons = {
 
 void usb_serial_console_disconnect(struct usb_serial *serial)
 {
-	if (serial && serial->port && serial->port[0]
-				&& serial->port[0] == usbcons_info.port) {
+	if (serial->port[0] == usbcons_info.port) {
 		usb_serial_console_exit();
 		usb_serial_put(serial);
 	}
