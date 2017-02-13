842954a8e3ec2d3a5e117c95533eb02ad73b3a2f
Shishir Kulkarni <shishir.kulkarni.2307@gmail.com>
2017-02-14 00:09:00 +0530

    Staging: rtl8188eu: core/rtw_led.c: format line to reduce it below 80 characters
    Fixes checkpatch.pl warning: "WARNING: line over 80 characters"
    
    Signed-off-by: Shishir Kulkarni <shishir.kulkarni.2307@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/core/rtw_led.c
+++ drivers/staging/rtl8188eu/core/rtw_led.c
@@ -39,7 +39,8 @@ void BlinkTimerCallback(unsigned long data)
 /*  */
 void BlinkWorkItemCallback(struct work_struct *work)
 {
-	struct LED_871x *pLed = container_of(work, struct LED_871x, BlinkWorkItem);
+	struct LED_871x *pLed = container_of(work, struct LED_871x,
+						BlinkWorkItem);
 
 	BlinkHandler(pLed);
 }
