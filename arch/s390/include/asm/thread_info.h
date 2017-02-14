2f09ca60a56dd9c217d32d68340e1b08cbbe1ace
Miroslav Benes <mbenes@suse.cz>
2017-02-13 19:42:34 -0600

    livepatch/s390: add TIF_PATCH_PENDING thread flag
    Update a task's patch state when returning from a system call or user
    space interrupt, or after handling a signal.
    
    This greatly increases the chances of a patch operation succeeding.  If
    a task is I/O bound, it can be patched when returning from a system
    call.  If a task is CPU bound, it can be patched when returning from an
    interrupt.  If a task is sleeping on a to-be-patched function, the user
    can send SIGSTOP and SIGCONT to force it to switch.
    
    Since there are two ways the syscall can be restarted on return from a
    signal handling process, it is important to clear the flag before
    do_signal() is called. Otherwise we could miss the migration if we used
    SIGSTOP/SIGCONT procedure or fake signal to migrate patching blocking
    tasks. If we place our hook to sysc_work label in entry before
    TIF_SIGPENDING is evaluated we kill two birds with one stone. The task
    is correctly migrated in all return paths from a syscall.
    
    Signed-off-by: Miroslav Benes <mbenes@suse.cz>
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Acked-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- arch/s390/include/asm/thread_info.h
+++ arch/s390/include/asm/thread_info.h
@@ -56,6 +56,7 @@ int arch_dup_task_struct(struct task_struct *dst, struct task_struct *src);
 #define TIF_SIGPENDING		1	/* signal pending */
 #define TIF_NEED_RESCHED	2	/* rescheduling necessary */
 #define TIF_UPROBE		3	/* breakpointed or single-stepping */
+#define TIF_PATCH_PENDING	4	/* pending live patching update */
 
 #define TIF_31BIT		16	/* 32bit process */
 #define TIF_MEMDIE		17	/* is terminating due to OOM killer */
@@ -74,6 +75,7 @@ int arch_dup_task_struct(struct task_struct *dst, struct task_struct *src);
 #define _TIF_SIGPENDING		_BITUL(TIF_SIGPENDING)
 #define _TIF_NEED_RESCHED	_BITUL(TIF_NEED_RESCHED)
 #define _TIF_UPROBE		_BITUL(TIF_UPROBE)
+#define _TIF_PATCH_PENDING	_BITUL(TIF_PATCH_PENDING)
 
 #define _TIF_31BIT		_BITUL(TIF_31BIT)
 #define _TIF_SINGLE_STEP	_BITUL(TIF_SINGLE_STEP)
