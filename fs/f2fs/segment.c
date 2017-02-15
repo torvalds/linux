5b6c6be2d878bd7ec4dc2cb4e2a2da2779fe52ab
Jaegeuk Kim <jaegeuk@kernel.org>
2017-02-14 19:32:51 -0800

    f2fs: use SSR for warm node as well
    We have had node chains, but haven't used it so far due to stale node blocks.
    Now, we have crc|cp_ver in node footer and give random cp_ver at format time,
    we can start to use it again.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -1563,7 +1563,8 @@ static void allocate_segment_by_default(struct f2fs_sb_info *sbi,
 
 	if (force)
 		new_curseg(sbi, type, true);
-	else if (type == CURSEG_WARM_NODE)
+	else if (!is_set_ckpt_flags(sbi, CP_CRC_RECOVERY_FLAG) &&
+					type == CURSEG_WARM_NODE)
 		new_curseg(sbi, type, false);
 	else if (curseg->alloc_type == LFS && is_next_segment_free(sbi, type))
 		new_curseg(sbi, type, false);
