aac5987a54a25d243d3045d4ae33756a56498a5e
Gustavo Padovan <gustavo.padovan@collabora.com>
2017-02-15 15:55:31 -0200

    [media] tw5864: improve subscribe event handling
    We already check for the V4L2_EVENT_CTRL inside
    v4l2_ctrl_subscribe_event() so just move this function to the default:
    branch of the switch and let it does the job for us.
    
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.com>
    Acked-by: Andrey Utkin <andrey.utkin@corp.bluecherry.net>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/pci/tw5864/tw5864-video.c
+++ drivers/media/pci/tw5864/tw5864-video.c
@@ -664,15 +664,14 @@ static int tw5864_subscribe_event(struct v4l2_fh *fh,
 				  const struct v4l2_event_subscription *sub)
 {
 	switch (sub->type) {
-	case V4L2_EVENT_CTRL:
-		return v4l2_ctrl_subscribe_event(fh, sub);
 	case V4L2_EVENT_MOTION_DET:
 		/*
 		 * Allow for up to 30 events (1 second for NTSC) to be stored.
 		 */
 		return v4l2_event_subscribe(fh, sub, 30, NULL);
+	default:
+		return v4l2_ctrl_subscribe_event(fh, sub);
 	}
-	return -EINVAL;
 }
 
 static void tw5864_frame_interval_set(struct tw5864_input *input)
