55f0cd3fb9c29c20fb94c47e28a9ec8cf704f8c2
H Hartley Sweeten <hsweeten@visionengravers.com>
2017-02-16 13:07:37 -0700

    spi: spi-ep93xx: simplify GPIO chip selects
    This driver requires a GPIO line to be used for the chip select of
    each SPI device.
    
    Remove the ep93xx_spi_chip_ops definition from the platform data
    and use the spi core GPIO handling for the chip selects.
    
    Fix all the ep93xx platforms that use this driver and remove the
    old Documentation.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/linux/platform_data/spi-ep93xx.h
+++ include/linux/platform_data/spi-ep93xx.h
@@ -5,25 +5,14 @@ struct spi_device;
 
 /**
  * struct ep93xx_spi_info - EP93xx specific SPI descriptor
- * @num_chipselect: number of chip selects on this board, must be
- *                  at least one
+ * @chipselect: array of gpio numbers to use as chip selects
+ * @num_chipselect: ARRAY_SIZE(chipselect)
  * @use_dma: use DMA for the transfers
  */
 struct ep93xx_spi_info {
+	int	*chipselect;
 	int	num_chipselect;
 	bool	use_dma;
-};
-
-/**
- * struct ep93xx_spi_chip_ops - operation callbacks for SPI slave device
- * @setup: setup the chip select mechanism
- * @cleanup: cleanup the chip select mechanism
- * @cs_control: control the device chip select
- */
-struct ep93xx_spi_chip_ops {
-	int	(*setup)(struct spi_device *spi);
-	void	(*cleanup)(struct spi_device *spi);
-	void	(*cs_control)(struct spi_device *spi, int value);
 };
 
 #endif /* __ASM_MACH_EP93XX_SPI_H */
