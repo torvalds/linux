5ace69341c97db4f94e32627dc8b741c35e46968
Javier Martinez Canillas <javier@osg.samsung.com>
2017-02-20 17:16:16 -0300

    [media] et8ek8: Export OF device ID as module aliases
    The I2C core always reports a MODALIAS of the form i2c:<foo> even if the
    device was registered via OF, this means that exporting the OF device ID
    table device aliases in the module is not needed. But in order to change
    how the core reports modaliases to user-space, it's better to export it.
    
    Before this patch:
    
    $ modinfo drivers/media/i2c/et8ek8/et8ek8.ko | grep alias
    alias:          i2c:et8ek8
    
    After this patch:
    
    $ modinfo drivers/media/i2c/et8ek8/et8ek8.ko | grep alias
    alias:          i2c:et8ek8
    alias:          of:N*T*Ctoshiba,et8ek8C*
    alias:          of:N*T*Ctoshiba,et8ek8
    
    Signed-off-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/i2c/et8ek8/et8ek8_driver.c
+++ drivers/media/i2c/et8ek8/et8ek8_driver.c
@@ -1485,6 +1485,7 @@ static const struct of_device_id et8ek8_of_table[] = {
 	{ .compatible = "toshiba,et8ek8" },
 	{ },
 };
+MODULE_DEVICE_TABLE(of, et8ek8_of_table);
 
 static const struct i2c_device_id et8ek8_id_table[] = {
 	{ ET8EK8_NAME, 0 },
