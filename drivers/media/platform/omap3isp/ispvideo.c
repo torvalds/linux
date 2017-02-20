dd9a00ab9c3e14effc3f34b43e04504d05676fc9
Sakari Ailus <sakari.ailus@linux.intel.com>
2017-02-20 10:22:18 -0500

    media: omap3isp: Call video_unregister_device() unconditionally
    video_unregister_device() can be called on a never or an already
    unregistered device. Drop the redundant check.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/platform/omap3isp/ispvideo.c
+++ drivers/media/platform/omap3isp/ispvideo.c
@@ -1495,6 +1495,5 @@ int omap3isp_video_register(struct isp_video *video, struct v4l2_device *vdev)
 
 void omap3isp_video_unregister(struct isp_video *video)
 {
-	if (video_is_registered(&video->video))
-		video_unregister_device(&video->video);
+	video_unregister_device(&video->video);
 }
