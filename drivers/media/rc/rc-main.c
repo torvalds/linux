413808685dd7c9b54bbc5af79da2eaddd0fc3cb2
Sean Young <sean@mess.org>
2017-02-22 18:48:01 -0300

    [media] rc: raw decoder for keymap protocol is not loaded on register
    When the protocol is set via the sysfs protocols attribute, the
    decoder is loaded. However, when it is not when a device is first
    plugged in or registered.
    
    Fixes: acc1c3c ("[media] media: rc: load decoder modules on-demand")
    
    Signed-off-by: Sean Young <sean@mess.org>
    Cc: <stable@vger.kernel.org> # v4.5+
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/rc/rc-main.c
+++ drivers/media/rc/rc-main.c
@@ -1663,6 +1663,7 @@ static int rc_setup_rx_device(struct rc_dev *dev)
 {
 	int rc;
 	struct rc_map *rc_map;
+	u64 rc_type;
 
 	if (!dev->map_name)
 		return -EINVAL;
@@ -1677,14 +1678,17 @@ static int rc_setup_rx_device(struct rc_dev *dev)
 	if (rc)
 		return rc;
 
-	if (dev->change_protocol) {
-		u64 rc_type = (1ll << rc_map->rc_type);
+	rc_type = BIT_ULL(rc_map->rc_type);
 
+	if (dev->change_protocol) {
 		rc = dev->change_protocol(dev, &rc_type);
 		if (rc < 0)
 			goto out_table;
 		dev->enabled_protocols = rc_type;
 	}
+
+	if (dev->driver_type == RC_DRIVER_IR_RAW)
+		ir_raw_load_modules(&rc_type);
 
 	set_bit(EV_KEY, dev->input_dev->evbit);
 	set_bit(EV_REP, dev->input_dev->evbit);
