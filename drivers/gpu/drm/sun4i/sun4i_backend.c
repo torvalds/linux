a6fbffb0301196df7735aa4aacd35610858e471f
Chen-Yu Tsai <wens@csie.org>
2017-02-23 16:05:33 +0800

    drm/sun4i: Make sunxi_rgb2yuv_coef constant
    sunxi_rgb2yuv_coef is a table of RGB-to-YUV conversion coefficients.
    They are programmed into the hardware, and can be declared constant.
    
    Reported-by: Priit Laes <plaes@plaes.org>
    Signed-off-by: Chen-Yu Tsai <wens@csie.org>
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- drivers/gpu/drm/sun4i/sun4i_backend.c
+++ drivers/gpu/drm/sun4i/sun4i_backend.c
@@ -24,7 +24,7 @@
 #include "sun4i_backend.h"
 #include "sun4i_drv.h"
 
-static u32 sunxi_rgb2yuv_coef[12] = {
+static const u32 sunxi_rgb2yuv_coef[12] = {
 	0x00000107, 0x00000204, 0x00000064, 0x00000108,
 	0x00003f69, 0x00003ed6, 0x000001c1, 0x00000808,
 	0x000001c1, 0x00003e88, 0x00003fb8, 0x00000808
