727ebb091e1778cc3a4842ae04f8f54da111eee0
Kinglong Mee <kinglongmee@gmail.com>
2017-02-25 19:32:21 +0800

    f2fs: update the comment of default nr_pages to skipping
    Fixes: 2c237ebaa4 ("f2fs: avoid writing node/metapages during writes")
    Signed-off-by: Kinglong Mee <kinglongmee@gmail.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/segment.h
+++ fs/f2fs/segment.h
@@ -736,8 +736,8 @@ static inline bool sec_usage_check(struct f2fs_sb_info *sbi, unsigned int secno)
  * It is very important to gather dirty pages and write at once, so that we can
  * submit a big bio without interfering other data writes.
  * By default, 512 pages for directory data,
- * 512 pages (2MB) * 3 for three types of nodes, and
- * max_bio_blocks for meta are set.
+ * 512 pages (2MB) * 8 for nodes, and
+ * 256 pages * 8 for meta are set.
  */
 static inline int nr_pages_to_skip(struct f2fs_sb_info *sbi, int type)
 {
