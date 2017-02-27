89d790ab31d033d67635f6362d57ea64e47708fa
Masahiro Yamada <yamada.masahiro@socionext.com>
2017-02-27 14:29:01 -0800

    scripts/spelling.txt: add "algined" pattern and fix typo instances
    Fix typos and add the following to the scripts/spelling.txt:
    
      algined||aligned
    
    While we are here, fix the "appplication" in the touched line in
    drivers/block/loop.c.  Also, fix the "may not naturally ..." to
    "may not be naturally ..." in the touched line in mm/page_alloc.
    
    Link: http://lkml.kernel.org/r/1481573103-11329-9-git-send-email-yamada.masahiro@socionext.com
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5925,7 +5925,7 @@ static unsigned long __paginginit calc_memmap_size(unsigned long spanned_pages,
 	 * the zone and SPARSEMEM is in use. If there are holes within the
 	 * zone, each populated memory region may cost us one or two extra
 	 * memmap pages due to alignment because memmap pages for each
-	 * populated regions may not naturally algined on page boundary.
+	 * populated regions may not be naturally aligned on page boundary.
 	 * So the (present_pages >> 4) heuristic is a tradeoff for that.
 	 */
 	if (spanned_pages > present_pages + (present_pages >> 4) &&
