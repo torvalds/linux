01cddfe99008da87dd4f6b8fbfa31e09257b9133
Arnd Bergmann <arnd@arndb.de>
2017-02-27 14:26:44 -0800

    mm,fs,dax: mark dax_iomap_pmd_fault as const
    The two alternative implementations of dax_iomap_fault have different
    prototypes, and one of them is obviously wrong as seen from this build
    warning:
    
      fs/dax.c: In function 'dax_iomap_fault':
      fs/dax.c:1462:35: error: passing argument 2 of 'dax_iomap_pmd_fault' discards 'const' qualifier from pointer target type [-Werror=discarded-qualifiers]
    
    This marks the argument 'const' as in all the related functions.
    
    Fixes: a2d581675d48 ("mm,fs,dax: change ->pmd_fault to ->huge_fault")
    Link: http://lkml.kernel.org/r/20170227203349.3318733-1-arnd@arndb.de
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/dax.c
+++ fs/dax.c
@@ -1436,7 +1436,8 @@ out:
 	return result;
 }
 #else
-static int dax_iomap_pmd_fault(struct vm_fault *vmf, struct iomap_ops *ops)
+static int dax_iomap_pmd_fault(struct vm_fault *vmf,
+			       const struct iomap_ops *ops)
 {
 	return VM_FAULT_FALLBACK;
 }
