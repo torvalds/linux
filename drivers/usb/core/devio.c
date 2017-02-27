0f5e1558306076adec7255b317b89480d8aaa304
Masahiro Yamada <yamada.masahiro@socionext.com>
2017-02-27 14:28:52 -0800

    scripts/spelling.txt: add "an one" pattern and fix typo instances
    Fix typos and add the following to the scripts/spelling.txt:
    
      an one||a one
    
    I dropped the "an" before "one or more" in
    drivers/net/ethernet/sfc/mcdi_pcol.h.
    
    Link: http://lkml.kernel.org/r/1481573103-11329-6-git-send-email-yamada.masahiro@socionext.com
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/usb/core/devio.c
+++ drivers/usb/core/devio.c
@@ -2335,7 +2335,7 @@ static int proc_drop_privileges(struct usb_dev_state *ps, void __user *arg)
 	if (copy_from_user(&data, arg, sizeof(data)))
 		return -EFAULT;
 
-	/* This is an one way operation. Once privileges are
+	/* This is a one way operation. Once privileges are
 	 * dropped, you cannot regain them. You may however reissue
 	 * this ioctl to shrink the allowed interfaces mask.
 	 */
