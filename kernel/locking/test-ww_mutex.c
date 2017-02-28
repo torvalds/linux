2b232e0c3b3a09f3e33750aa20e314f1b80e5361
Chris Wilson <chris@chris-wilson.co.uk>
2017-02-28 09:40:11 +0000

    locking/ww_mutex: Replace cpu_relax() with cond_resched() for tests
    When busy-spinning on a ww_mutex_trylock(), we depend upon the other
    thread advancing and releasing the lock. This can not happen on a single
    CPU unless we relinquish it:
    
      [ ] NMI watchdog: BUG: soft lockup - CPU#0 stuck for 22s! [kworker/0:1:18]
      ...
      [ ] Call Trace:
      [ ]  mutex_trylock()
      [ ]  test_mutex_work+0x31/0x56
      [ ]  process_one_work+0x1b4/0x2f9
      [ ]  worker_thread+0x1b0/0x27c
      [ ]  kthread+0xd1/0xd3
      [ ]  ret_from_fork+0x19/0x30
    
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Fixes: f2a5fec17395 ("locking/ww_mutex: Begin kselftests for ww_mutex")
    Link: http://lkml.kernel.org/r/20170228094011.2595-1-chris@chris-wilson.co.uk
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/locking/test-ww_mutex.c
+++ kernel/locking/test-ww_mutex.c
@@ -50,7 +50,7 @@ static void test_mutex_work(struct work_struct *work)
 
 	if (mtx->flags & TEST_MTX_TRY) {
 		while (!ww_mutex_trylock(&mtx->mutex))
-			cpu_relax();
+			cond_resched();
 	} else {
 		ww_mutex_lock(&mtx->mutex, NULL);
 	}
@@ -88,7 +88,7 @@ static int __test_mutex(unsigned int flags)
 				ret = -EINVAL;
 				break;
 			}
-			cpu_relax();
+			cond_resched();
 		} while (time_before(jiffies, timeout));
 	} else {
 		ret = wait_for_completion_timeout(&mtx.done, TIMEOUT);
