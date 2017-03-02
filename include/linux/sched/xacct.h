3fd220679ce235f0e222978ed5f1244d8f64963a
Jin Qian <jinqian@google.com>
2017-03-02 13:32:59 -0800

    ANDROID: taskstats: track fsync syscalls
    This adds a counter to the taskstats extended accounting fields, which
    tracks the number of times fsync is called, and then plumbs it through
    to the uid_sys_stats driver.
    
    Bug: 120442023
    Change-Id: I6c138de5b2332eea70f57e098134d1d141247b3f
    Signed-off-by: Jin Qian <jinqian@google.com>
    [AmitP: Refactored changes to align with changes from upstream commit
            9a07000400c8 ("sched/headers: Move CONFIG_TASK_XACCT bits from <linux/sched.h> to <linux/sched/xacct.h>")]
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    [tkjos: Needed for storaged fsync accounting ("storaged --uid" and
            "storaged --task").]
    [astrachan: This is modifying a userspace interface and should probably
                be reworked]
    Signed-off-by: Alistair Strachan <astrachan@google.com>

--- include/linux/sched/xacct.h
+++ include/linux/sched/xacct.h
@@ -28,6 +28,11 @@ static inline void inc_syscw(struct task_struct *tsk)
 {
 	tsk->ioac.syscw++;
 }
+
+static inline void inc_syscfs(struct task_struct *tsk)
+{
+	tsk->ioac.syscfs++;
+}
 #else
 static inline void add_rchar(struct task_struct *tsk, ssize_t amt)
 {
@@ -42,6 +47,10 @@ static inline void inc_syscr(struct task_struct *tsk)
 }
 
 static inline void inc_syscw(struct task_struct *tsk)
+{
+}
+
+static inline void inc_syscfs(struct task_struct *tsk)
 {
 }
 #endif
