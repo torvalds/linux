d29ffab591235c46e4eb9c72422c8d6789764db4
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2017-03-02 12:47:27 +0200

    drm: bridge: vga-dac: Add adi,adv7123 compatible string
    The ADV7123 is a transparent VGA DAC. Unlike dumb VGA DACs it can be
    controlled through a power save pin, and requires a power supply.
    However, on most boards where the device is used neither the power save
    signal nor the power supply are controllable.
    
    To avoid developing a separate device-specific driver add an
    "adi,adv7123" compatible entry to the dumb-vga-dac driver. This will
    allow supporting most ADV7123-based boards easily, while allowing future
    development of an adv7123 driver when needed without breaking backward
    compatibility.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Acked-by: Maxime Ripard <maxime.ripard@free-electrons.com>
    Signed-off-by: Archit Taneja <architt@codeaurora.org>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170302104728.7150-4-laurent.pinchart+renesas@ideasonboard.com

--- drivers/gpu/drm/bridge/dumb-vga-dac.c
+++ drivers/gpu/drm/bridge/dumb-vga-dac.c
@@ -237,6 +237,7 @@ static int dumb_vga_remove(struct platform_device *pdev)
 
 static const struct of_device_id dumb_vga_match[] = {
 	{ .compatible = "dumb-vga-dac" },
+	{ .compatible = "adi,adv7123" },
 	{ .compatible = "ti,ths8135" },
 	{},
 };
