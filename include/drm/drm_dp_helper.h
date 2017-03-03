4bb310fd9eea254b68b2f48b376fe149f65e3fbb
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2017-03-03 14:39:33 +0100

    drm/dp: add crtc backpointer to drm_dp_aux
    This backpointer allows DP helpers to access the crtc it's currently
    being used for.
    
    v6: Have the backpointer be to drm_crtc (Sean Paul)
    
    Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170303133936.14964-2-tomeu.vizoso@collabora.com

--- include/drm/drm_dp_helper.h
+++ include/drm/drm_dp_helper.h
@@ -732,6 +732,7 @@ struct drm_dp_aux_msg {
  * @name: user-visible name of this AUX channel and the I2C-over-AUX adapter
  * @ddc: I2C adapter that can be used for I2C-over-AUX communication
  * @dev: pointer to struct device that is the parent for this AUX channel
+ * @crtc: backpointer to the crtc that is currently using this AUX channel
  * @hw_mutex: internal mutex used for locking transfers
  * @transfer: transfers a message representing a single AUX transaction
  *
@@ -768,6 +769,7 @@ struct drm_dp_aux {
 	const char *name;
 	struct i2c_adapter ddc;
 	struct device *dev;
+	struct drm_crtc *crtc;
 	struct mutex hw_mutex;
 	ssize_t (*transfer)(struct drm_dp_aux *aux,
 			    struct drm_dp_aux_msg *msg);
