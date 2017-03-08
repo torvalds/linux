90f16fddcc2802726142b8386c65ccb89f044613
Jan Kara <jack@suse.cz>
2017-03-08 17:48:33 +0100

    block: Make del_gendisk() safer for disks without queues
    Commit 165a5e22fafb "block: Move bdi_unregister() to del_gendisk()"
    added disk->queue dereference to del_gendisk(). Although del_gendisk()
    is not supposed to be called without disk->queue valid and
    blk_unregister_queue() warns in that case, this change will make it oops
    instead. Return to the old more robust behavior of just warning when
    del_gendisk() gets called for gendisk with disk->queue being NULL.
    
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Tested-by: Omar Sandoval <osandov@fb.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- block/genhd.c
+++ block/genhd.c
@@ -681,12 +681,16 @@ void del_gendisk(struct gendisk *disk)
 	disk->flags &= ~GENHD_FL_UP;
 
 	sysfs_remove_link(&disk_to_dev(disk)->kobj, "bdi");
-	/*
-	 * Unregister bdi before releasing device numbers (as they can get
-	 * reused and we'd get clashes in sysfs).
-	 */
-	bdi_unregister(disk->queue->backing_dev_info);
-	blk_unregister_queue(disk);
+	if (disk->queue) {
+		/*
+		 * Unregister bdi before releasing device numbers (as they can
+		 * get reused and we'd get clashes in sysfs).
+		 */
+		bdi_unregister(disk->queue->backing_dev_info);
+		blk_unregister_queue(disk);
+	} else {
+		WARN_ON(1);
+	}
 	blk_unregister_region(disk_devt(disk), disk->minors);
 
 	part_stat_set_all(&disk->part0, 0);
