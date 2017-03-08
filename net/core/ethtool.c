abb521e36b9286c262971974ebaeda2d67dadd86
Jakub Kicinski <jakub.kicinski@netronome.com>
2017-03-08 08:57:00 -0800

    ethtool: add CRC32 as an RSS hash function
    CRC32 engines are usually easily available in hardware and generate
    OK spread for RSS hash.  Add CRC32 RSS hash function to ethtool API.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/ethtool.c
+++ net/core/ethtool.c
@@ -109,6 +109,7 @@ static const char
 rss_hash_func_strings[ETH_RSS_HASH_FUNCS_COUNT][ETH_GSTRING_LEN] = {
 	[ETH_RSS_HASH_TOP_BIT] =	"toeplitz",
 	[ETH_RSS_HASH_XOR_BIT] =	"xor",
+	[ETH_RSS_HASH_CRC32_BIT] =	"crc32",
 };
 
 static const char
