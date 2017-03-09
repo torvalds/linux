251ea0ca408b827e888d622eac5e89e4b9502ea2
Heiko Carstens <heiko.carstens@de.ibm.com>
2017-03-09 10:02:28 +0100

    s390/topology: get rid of core mask array
    Use a single long value instead of a single element array to represent
    the core mask. The array is a leftover from 32/31 bit code so we were
    able to use bitops helper functions.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/sysinfo.h
+++ arch/s390/include/asm/sysinfo.h
@@ -142,7 +142,6 @@ struct sysinfo_3_2_2 {
 
 extern int topology_max_mnest;
 
-#define TOPOLOGY_CORE_BITS	64
 #define TOPOLOGY_NR_MAG		6
 
 struct topology_core {
@@ -152,7 +151,7 @@ struct topology_core {
 	unsigned char pp:2;
 	unsigned char reserved1;
 	unsigned short origin;
-	unsigned long mask[TOPOLOGY_CORE_BITS / BITS_PER_LONG];
+	unsigned long mask;
 };
 
 struct topology_container {
