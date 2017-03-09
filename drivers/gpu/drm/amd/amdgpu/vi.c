d8a8ed9758241e138933c67e40db2db2790eca19
Tom St Denis <tom.stdenis@amd.com>
2017-03-09 13:21:07 -0500

    drm/amd/amdgpu: Disable GFX_PG on Carrizo until compute issues solved
    Currently compute jobs will stall if GFX_PG is enabled.  Until this
    is resolved we'll disable GFX_PG.
    
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/vi.c
+++ drivers/gpu/drm/amd/amdgpu/vi.c
@@ -1051,7 +1051,7 @@ static int vi_common_early_init(void *handle)
 		/* rev0 hardware requires workarounds to support PG */
 		adev->pg_flags = 0;
 		if (adev->rev_id != 0x00) {
-			adev->pg_flags |= AMD_PG_SUPPORT_GFX_PG |
+			adev->pg_flags |=
 				AMD_PG_SUPPORT_GFX_SMG |
 				AMD_PG_SUPPORT_GFX_PIPELINE |
 				AMD_PG_SUPPORT_CP |
