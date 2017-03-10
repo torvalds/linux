f90b0875463e450c5dcb714fb887720ecb53a600
Brian Norris <briannorris@chromium.org>
2017-03-09 18:46:16 -0800

    PCI: Export pci_remap_iospace() and pci_unmap_iospace()
    These are useful for PCIe host drivers, and those drivers can be modules.
    
    [bhelgaas: don't remove __weak; it's removed elsewhere]
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Shawn Lin <shawn.lin@rock-chips.com>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -3383,6 +3383,7 @@ int __weak pci_remap_iospace(const struct resource *res, phys_addr_t phys_addr)
 	return -ENODEV;
 #endif
 }
+EXPORT_SYMBOL(pci_remap_iospace);
 
 /**
  *	pci_unmap_iospace - Unmap the memory mapped I/O space
@@ -3400,6 +3401,7 @@ void pci_unmap_iospace(struct resource *res)
 	unmap_kernel_range(vaddr, resource_size(res));
 #endif
 }
+EXPORT_SYMBOL(pci_unmap_iospace);
 
 static void __pci_set_master(struct pci_dev *dev, bool enable)
 {
