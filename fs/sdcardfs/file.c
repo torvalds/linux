2c852b7279a7a406631d671a9ede48420fb1e85f
Daniel Rosenberg <drosen@google.com>
2017-03-09 21:24:58 -0800

    ANDROID: sdcardfs: copy lower inode attributes in ->ioctl
    Adapted from wrapfs
    commit fbc9c6f83ea6 ("Wrapfs: copy lower inode attributes in ->ioctl")
    commit e97d8e26cc9e ("Wrapfs: use file_inode helper")
    
    Some ioctls (e.g., EXT2_IOC_SETFLAGS) can change inode attributes, so copy
    them from lower inode.
    
    Signed-off-by: Erez Zadok <ezk@cs.sunysb.edu>
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Bug: 35766959
    Change-Id: I0f12684b9dbd4088b4a622c7ea9c03087f40e572

--- fs/sdcardfs/file.c
+++ fs/sdcardfs/file.c
@@ -113,6 +113,10 @@ static long sdcardfs_unlocked_ioctl(struct file *file, unsigned int cmd,
 	if (lower_file->f_op->unlocked_ioctl)
 		err = lower_file->f_op->unlocked_ioctl(lower_file, cmd, arg);
 
+	/* some ioctls can change inode attributes (EXT2_IOC_SETFLAGS) */
+	if (!err)
+		sdcardfs_copy_and_fix_attrs(file_inode(file),
+				      file_inode(lower_file));
 out:
 	return err;
 }
