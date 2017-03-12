6e7408acd04d06c04981c0c0fb5a2462b16fae4f
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2017-03-12 18:12:56 +0100

    cpufreq: intel_pstate: Update pid_params.sample_rate_ns in pid_param_set()
    Fix the debugfs interface for PID tuning to actually update
    pid_params.sample_rate_ns on PID parameters updates, as changing
    pid_params.sample_rate_ms via debugfs has no effect now.
    
    Fixes: a4675fbc4a7a (cpufreq: intel_pstate: Replace timers with utilization update callbacks)
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -989,6 +989,7 @@ static void intel_pstate_update_policies(void)
 static int pid_param_set(void *data, u64 val)
 {
 	*(u32 *)data = val;
+	pid_params.sample_rate_ns = pid_params.sample_rate_ms * NSEC_PER_MSEC;
 	intel_pstate_reset_all_pid();
 	return 0;
 }
