3d2033e33a2aa1d64bae208d83a9be127c633f53
Tobin C. Harding <me@tobin.cc>
2017-03-13 18:54:07 +1100

    staging: ks7010: fix checkpatch whitespace warns
    Checkpatch emits various warnings, errors and checks;
    
     ERROR:TRAILING_WHITESPACE: trailing whitespace
     WARNING:SPACE_BEFORE_TAB: please, no space before tabs
     WARNING:SPACE_BEFORE_TAB: please, no space before tabs
     ERROR:CODE_INDENT: code indent should use tabs where possible
     CHECK:PARENTHESIS_ALIGNMENT: Alignment should match open parenthesis
    
    These are all trivial whitespace fixes. Fix them all in one
    patch. Change only whitespace.
    
    Signed-off-by: Tobin C. Harding <me@tobin.cc>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/ks_wlan_ioctl.h
+++ drivers/staging/ks7010/ks_wlan_ioctl.h
@@ -1,6 +1,6 @@
 /*
  *   Driver for KeyStream 11b/g wireless LAN
- *   
+ *
  *   Copyright (c) 2005-2008 KeyStream Corp.
  *   Copyright (C) 2009 Renesas Technology Corp.
  *
@@ -20,8 +20,8 @@
 /*					SIOCIWFIRSTPRIV + 2 */
 #define KS_WLAN_GET_FIRM_VERSION	SIOCIWFIRSTPRIV + 3
 #ifdef WPS
-#define KS_WLAN_SET_WPS_ENABLE 		SIOCIWFIRSTPRIV + 4
-#define KS_WLAN_GET_WPS_ENABLE 		SIOCIWFIRSTPRIV + 5
+#define KS_WLAN_SET_WPS_ENABLE		SIOCIWFIRSTPRIV + 4
+#define KS_WLAN_GET_WPS_ENABLE		SIOCIWFIRSTPRIV + 5
 #define KS_WLAN_SET_WPS_PROBE_REQ	SIOCIWFIRSTPRIV + 6
 #endif
 #define KS_WLAN_GET_EEPROM_CKSUM	SIOCIWFIRSTPRIV + 7
@@ -60,7 +60,7 @@
 
 int ks_wlan_read_config_file(struct ks_wlan_private *priv);
 int ks_wlan_setup_parameter(struct ks_wlan_private *priv,
-		             unsigned int commit_flag);
+			    unsigned int commit_flag);
 
 #endif /* __KERNEL__ */
 
