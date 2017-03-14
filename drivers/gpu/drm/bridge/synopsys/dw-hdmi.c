496ebbc3cf9959f51a440f69378ae0de812160b9
Algea Cao <algea.cao@rock-chips.com>
2017-03-14 09:28:46 +0800

    drm: bridge: dw-hdmi: enable 3d mode
    Change-Id: I45c4b0ded4aeaf24122d5cc6eb49b6bc72afc060
    Signed-off-by: Algea Cao <algea.cao@rock-chips.com>

--- drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
+++ drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
@@ -3500,6 +3500,7 @@ __dw_hdmi_probe(struct platform_device *pdev,
 	if (!hdmi)
 		return ERR_PTR(-ENOMEM);
 
+	hdmi->connector.stereo_allowed = 1;
 	hdmi->plat_data = plat_data;
 	hdmi->dev = dev;
 	hdmi->sample_rate = 48000;
