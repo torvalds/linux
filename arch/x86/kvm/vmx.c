f0b98c02c18a3c76ccf359478f68c9bae81b5f78
Jim Mattson <jmattson@google.com>
2017-03-15 07:56:11 -0700

    kvm: vmx: Don't use INVVPID when EPT is enabled
    According to the Intel SDM, volume 3, section 28.3.2: Creating and
    Using Cached Translation Information, "No linear mappings are used
    while EPT is in use." INVEPT will invalidate both the guest-physical
    mappings and the combined mappings in the TLBs and paging-structure
    caches, so an INVVPID is superfluous.
    
    Signed-off-by: Jim Mattson <jmattson@google.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -4017,11 +4017,12 @@ static void exit_lmode(struct kvm_vcpu *vcpu)
 
 static inline void __vmx_flush_tlb(struct kvm_vcpu *vcpu, int vpid)
 {
-	vpid_sync_context(vpid);
 	if (enable_ept) {
 		if (!VALID_PAGE(vcpu->arch.mmu.root_hpa))
 			return;
 		ept_sync_context(construct_eptp(vcpu->arch.mmu.root_hpa));
+	} else {
+		vpid_sync_context(vpid);
 	}
 }
 
