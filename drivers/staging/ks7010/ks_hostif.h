2ccabd0521697e9b12dcc9ebb0dd20623ad8ffc4
Tobin C. Harding <me@tobin.cc>
2017-03-15 16:14:37 +1100

    staging: ks7010: fix checkpatch BLOCK_COMMENT_STYLE
    Checkpatch emits WARNING: Block comments use a trailing */ on a
    separate line.
    
    Move comments to (kernel doc format) struct comment.
    
    Signed-off-by: Tobin C. Harding <me@tobin.cc>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/ks_hostif.h
+++ drivers/staging/ks7010/ks_hostif.h
@@ -348,6 +348,13 @@ struct hostif_stop_confirm_t {
 	u16 result_code;
 } __packed;
 
+/**
+ * struct hostif_ps_adhoc_set_request_t - pseudo adhoc mode
+ * @capability: bit5  : preamble
+ *              bit6  : pbcc - Not supported always 0
+ *              bit10 : ShortSlotTime
+ *              bit13 : DSSS-OFDM - Not supported always 0
+ */
 struct hostif_ps_adhoc_set_request_t {
 	struct hostif_hdr header;
 	u16 phy_type;
@@ -360,8 +367,7 @@ struct hostif_ps_adhoc_set_request_t {
 #define CTS_MODE_TRUE	1
 	u16 channel;
 	struct rate_set16_t rate_set;
-	u16 capability;	/* bit5:preamble bit6:pbcc pbcc not supported always 0
-				 * bit10:ShortSlotTime bit13:DSSS-OFDM DSSS-OFDM not supported always 0 */
+	u16 capability;
 	u16 scan_type;
 } __packed;
 
@@ -370,14 +376,20 @@ struct hostif_ps_adhoc_set_confirm_t {
 	u16 result_code;
 } __packed;
 
+/**
+ * struct hostif_infrastructure_set_request_t
+ * @capability: bit5  : preamble
+ *              bit6  : pbcc - Not supported always 0
+ *              bit10 : ShortSlotTime
+ *              bit13 : DSSS-OFDM - Not supported always 0
+ */
 struct hostif_infrastructure_set_request_t {
 	struct hostif_hdr header;
 	u16 phy_type;
 	u16 cts_mode;
 	struct rate_set16_t rate_set;
 	struct ssid_t ssid;
-	u16 capability;	/* bit5:preamble bit6:pbcc pbcc not supported always 0
-				 * bit10:ShortSlotTime bit13:DSSS-OFDM DSSS-OFDM not supported always 0 */
+	u16 capability;
 	u16 beacon_lost_count;
 	u16 auth_type;
 #define AUTH_TYPE_OPEN_SYSTEM 0
@@ -386,14 +398,20 @@ struct hostif_infrastructure_set_request_t {
 	u16 scan_type;
 } __packed;
 
+/**
+ * struct hostif_infrastructure_set2_request_t
+ * @capability: bit5  : preamble
+ *              bit6  : pbcc - Not supported always 0
+ *              bit10 : ShortSlotTime
+ *              bit13 : DSSS-OFDM - Not supported always 0
+ */
 struct hostif_infrastructure_set2_request_t {
 	struct hostif_hdr header;
 	u16 phy_type;
 	u16 cts_mode;
 	struct rate_set16_t rate_set;
 	struct ssid_t ssid;
-	u16 capability;	/* bit5:preamble bit6:pbcc pbcc not supported always 0
-				 * bit10:ShortSlotTime bit13:DSSS-OFDM DSSS-OFDM not supported always 0 */
+	u16 capability;
 	u16 beacon_lost_count;
 	u16 auth_type;
 #define AUTH_TYPE_OPEN_SYSTEM 0
@@ -408,6 +426,13 @@ struct hostif_infrastructure_set_confirm_t {
 	u16 result_code;
 } __packed;
 
+/**
+ * struct hostif_adhoc_set_request_t
+ * @capability: bit5  : preamble
+ *              bit6  : pbcc - Not supported always 0
+ *              bit10 : ShortSlotTime
+ *              bit13 : DSSS-OFDM - Not supported always 0
+ */
 struct hostif_adhoc_set_request_t {
 	struct hostif_hdr header;
 	u16 phy_type;
@@ -415,11 +440,17 @@ struct hostif_adhoc_set_request_t {
 	u16 channel;
 	struct rate_set16_t rate_set;
 	struct ssid_t ssid;
-	u16 capability;	/* bit5:preamble bit6:pbcc pbcc not supported always 0
-				 * bit10:ShortSlotTime bit13:DSSS-OFDM DSSS-OFDM not supported always 0 */
+	u16 capability;
 	u16 scan_type;
 } __packed;
 
+/**
+ * struct hostif_adhoc_set2_request_t
+ * @capability: bit5  : preamble
+ *              bit6  : pbcc - Not supported always 0
+ *              bit10 : ShortSlotTime
+ *              bit13 : DSSS-OFDM - Not supported always 0
+ */
 struct hostif_adhoc_set2_request_t {
 	struct hostif_hdr header;
 	u16 phy_type;
@@ -427,8 +458,7 @@ struct hostif_adhoc_set2_request_t {
 	u16 reserved;
 	struct rate_set16_t rate_set;
 	struct ssid_t ssid;
-	u16 capability;	/* bit5:preamble bit6:pbcc pbcc not supported always 0
-				 * bit10:ShortSlotTime bit13:DSSS-OFDM DSSS-OFDM not supported always 0 */
+	u16 capability;
 	u16 scan_type;
 	struct channel_list_t channel_list;
 	u8 bssid[ETH_ALEN];
