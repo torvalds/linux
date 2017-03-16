b51040fc375c194e20164be489668d4d28001f53
Rostyslav Khudolii <rkhudolii@airtame.com>
2017-03-16 11:22:17 +0200

    ath6kl: Remove old 802.11a-only channels
    Channels 34/38/42/46 can only be used for compatibility with old devices sold
    in Japan. Modern products, such as AR6003/AR6004 don't support these channels.
    Keeping them in the upstream is error prone and requires full network stack
    support. A custom patch should be used in case such compatibility is required.
    
    Without this one, a user is able to start an AP using wpa_supplicant,
    for example, on one of these channels (34/38/42/46), without getting
    any warning/error from the cfg80211 or ath6kl - which is correct
    (since these channels match regdom rules). However, the AR6003 and its
    firmware (we're using v3.4.0.225) will fail and return
    "WMI_CMDERROR_EVENTID" with "INVALID_PARAM" error code.
    
    Signed-off-by: Rostyslav Khudolii <rkhudolii@airtame.com>
    Cc: Attila Sukosd <attila@airtame.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/cfg80211.c
+++ drivers/net/wireless/ath/ath6kl/cfg80211.c
@@ -102,10 +102,8 @@ static struct ieee80211_channel ath6kl_2ghz_channels[] = {
 };
 
 static struct ieee80211_channel ath6kl_5ghz_a_channels[] = {
-	CHAN5G(34, 0), CHAN5G(36, 0),
-	CHAN5G(38, 0), CHAN5G(40, 0),
-	CHAN5G(42, 0), CHAN5G(44, 0),
-	CHAN5G(46, 0), CHAN5G(48, 0),
+	CHAN5G(36, 0), CHAN5G(40, 0),
+	CHAN5G(44, 0), CHAN5G(48, 0),
 	CHAN5G(52, 0), CHAN5G(56, 0),
 	CHAN5G(60, 0), CHAN5G(64, 0),
 	CHAN5G(100, 0), CHAN5G(104, 0),
