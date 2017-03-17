e811898c97f83aef6c1652fbfbf5294bf542159c
Jaegeuk Kim <jaegeuk@kernel.org>
2017-03-17 10:04:15 +0800

    f2fs: don't allow atomic writes for not regular files
    The atomic writes only supports regular files for database.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -1518,6 +1518,9 @@ static int f2fs_ioc_start_atomic_write(struct file *filp)
 	if (!inode_owner_or_capable(inode))
 		return -EACCES;
 
+	if (!S_ISREG(inode->i_mode))
+		return -EINVAL;
+
 	ret = mnt_want_write_file(filp);
 	if (ret)
 		return ret;
