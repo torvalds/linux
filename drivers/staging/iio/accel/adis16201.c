099c4cef24a6cb77e2c03e9e66199d460ed4a5a1
Aishwarya Pant <aishpant@gmail.com>
2017-03-18 03:13:56 +0530

    staging: iio: accel: adis16201: remove iio_dev mlock
    In the driver adis16201 read raw does not require an iio_dev->mlock for
    reads. It can run concurrently as adis_read_reg_16() is protected by a
    transaction lock.
    
    Signed-off-by: Aishwarya Pant <aishpant@gmail.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/accel/adis16201.c
+++ drivers/staging/iio/accel/adis16201.c
@@ -223,17 +223,13 @@ static int adis16201_read_raw(struct iio_dev *indio_dev,
 		default:
 			return -EINVAL;
 		}
-		mutex_lock(&indio_dev->mlock);
 		addr = adis16201_addresses[chan->scan_index];
 		ret = adis_read_reg_16(st, addr, &val16);
-		if (ret) {
-			mutex_unlock(&indio_dev->mlock);
+		if (ret)
 			return ret;
-		}
 		val16 &= (1 << bits) - 1;
 		val16 = (s16)(val16 << (16 - bits)) >> (16 - bits);
 		*val = val16;
-		mutex_unlock(&indio_dev->mlock);
 		return IIO_VAL_INT;
 	}
 	return -EINVAL;
