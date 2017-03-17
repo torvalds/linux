8ff0971f1500953935339c23c91f660793fd4c64
Chao Yu <yuchao0@huawei.com>
2017-03-17 15:43:57 +0800

    f2fs: don't allow volatile writes for non-regular file
    Now f2fs only supports volatile writes for journal db regular file.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -1598,6 +1598,9 @@ static int f2fs_ioc_start_volatile_write(struct file *filp)
 	if (!inode_owner_or_capable(inode))
 		return -EACCES;
 
+	if (!S_ISREG(inode->i_mode))
+		return -EINVAL;
+
 	ret = mnt_want_write_file(filp);
 	if (ret)
 		return ret;
