3a1e37ae007e9d629617a0d5bc07661078defa32
Al Viro <viro@zeniv.linux.org.uk>
2017-03-19 01:01:53 -0400

    arc: get rid of unused declaration
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/arc/include/asm/uaccess.h
+++ arch/arc/include/asm/uaccess.h
@@ -394,9 +394,6 @@ __arc_copy_from_user(void *to, const void __user *from, unsigned long n)
 	return res;
 }
 
-extern unsigned long slowpath_copy_to_user(void __user *to, const void *from,
-					   unsigned long n);
-
 static inline unsigned long
 __arc_copy_to_user(void __user *to, const void *from, unsigned long n)
 {
