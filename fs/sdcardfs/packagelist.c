0084d2b319df47847516b7aab2d814c35100ade1
Daniel Rosenberg <drosen@google.com>
2017-03-21 16:28:27 -0700

    ANDROID: sdcardfs: correct order of descriptors
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Bug: 35331000
    Change-Id: Ia6d16b19c8c911f41231d2a12be0740057edfacf

--- fs/sdcardfs/packagelist.c
+++ fs/sdcardfs/packagelist.c
@@ -48,12 +48,14 @@ static struct kmem_cache *hashtable_entry_cachep;
 static unsigned int full_name_case_hash(const unsigned char *name, unsigned int len)
 {
 	unsigned long hash = init_name_hash();
+
 	while (len--)
 		hash = partial_name_hash(tolower(*name++), hash);
 	return end_name_hash(hash);
 }
 
-static void inline qstr_init(struct qstr *q, const char *name) {
+static inline void qstr_init(struct qstr *q, const char *name)
+{
 	q->name = name;
 	q->len = strlen(q->name);
 	q->hash = full_name_case_hash(q->name, q->len);
