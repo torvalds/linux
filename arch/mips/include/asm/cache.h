4437637c3277fa9aba4e443329b5db3b6d34eedf
Ralf Baechle <ralf@linux-mips.org>
2017-03-21 22:14:30 +0100

    MIPS: Delete unused definition of SMP_CACHE_SHIFT.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/cache.h
+++ arch/mips/include/asm/cache.h
@@ -12,7 +12,6 @@
 #define L1_CACHE_SHIFT		CONFIG_MIPS_L1_CACHE_SHIFT
 #define L1_CACHE_BYTES		(1 << L1_CACHE_SHIFT)
 
-#define SMP_CACHE_SHIFT		L1_CACHE_SHIFT
 #define SMP_CACHE_BYTES		L1_CACHE_BYTES
 
 #define __read_mostly __attribute__((__section__(".data..read_mostly")))
