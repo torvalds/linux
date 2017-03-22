6e9dc861219b8f8966f5a91984dbfaa6de57fb8b
Monk Liu <Monk.Liu@amd.com>
2017-03-22 18:02:40 +0800

    drm/amdgpu:no cg for soc15 of SRIOV
    no CG for SRIOV on SOC15
    
    Signed-off-by: Monk Liu <Monk.Liu@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/soc15.c
+++ drivers/gpu/drm/amd/amdgpu/soc15.c
@@ -773,6 +773,9 @@ static int soc15_common_set_clockgating_state(void *handle,
 {
 	struct amdgpu_device *adev = (struct amdgpu_device *)handle;
 
+	if (amdgpu_sriov_vf(adev))
+		return 0;
+
 	switch (adev->asic_type) {
 	case CHIP_VEGA10:
 		nbio_v6_1_update_medium_grain_clock_gating(adev,
