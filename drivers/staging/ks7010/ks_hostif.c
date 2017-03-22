2c4951adfae24d901c699ebb918bb6e5c7865ab7
Tobin C. Harding <me@tobin.cc>
2017-03-22 13:06:27 +1100

    staging: ks7010: invert if statement conditionals
    Checkpatch emits WARNING: Avoid multiple line dereference.
    
    Function uses if statement blocks to guard the body of the
    function. If we invert these conditionals and return, then the code
    becomes more readable and subsequent code is indented less. The
    checkpatch fix then follows trivially.
    
    Invert conditionals, return from function if new conditional evaluates
    to true. Reduce the level of indentation in subsequent code.
    
    Signed-off-by: Tobin C. Harding <me@tobin.cc>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/ks_hostif.c
+++ drivers/staging/ks7010/ks_hostif.c
@@ -2607,16 +2607,16 @@ void hostif_sme_task(unsigned long dev)
 
 	DPRINTK(3, "\n");
 
-	if (priv->dev_state >= DEVICE_STATE_BOOT) {
-		if (cnt_smeqbody(priv) > 0) {
-			hostif_sme_execute(priv,
-					   priv->sme_i.event_buff[priv->sme_i.
-								  qhead]);
-			inc_smeqhead(priv);
-			if (cnt_smeqbody(priv) > 0)
-				tasklet_schedule(&priv->sme_task);
-		}
-	}
+	if (priv->dev_state < DEVICE_STATE_BOOT)
+		return;
+
+	if (cnt_smeqbody(priv) <= 0)
+		return;
+
+	hostif_sme_execute(priv, priv->sme_i.event_buff[priv->sme_i.qhead]);
+	inc_smeqhead(priv);
+	if (cnt_smeqbody(priv) > 0)
+		tasklet_schedule(&priv->sme_task);
 }
 
 /* send to Station Management Entity module */
