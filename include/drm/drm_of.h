aeaec6aa88444ef86e3aa11b0e481d2ff21df3ba
Rob Herring <robh@kernel.org>
2017-03-22 08:26:05 -0500

    UPSTREAM: drm: of: introduce drm_of_find_panel_or_bridge
    Many drivers have a common pattern of searching the OF graph for either an
    attached panel or bridge and then finding the DRM struct for the panel
    or bridge. Also, most drivers need to handle deferred probing when the
    DRM device is not yet instantiated. Create a common function,
    drm_of_find_panel_or_bridge, to find the connected node and the
    associated DRM panel or bridge device.
    
    (cherry picked from commit 1f2db3034c9f16ed918e34809167546f21d0fcb4)
    
    Change-Id: Ibda889742bfb0ea53181ed2ef9b19a9d9df3303e
    Signed-off-by: Rob Herring <robh@kernel.org>
    Acked-by: Philipp Zabel <p.zabel@pengutronix.de>
    [seanpaul dropped extern from drm_of.h]
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Signed-off-by: Mark Yao <mark.yao@rock-chips.com>

--- include/drm/drm_of.h
+++ include/drm/drm_of.h
@@ -7,6 +7,8 @@ struct component_master_ops;
 struct device;
 struct drm_device;
 struct drm_encoder;
+struct drm_panel;
+struct drm_bridge;
 struct device_node;
 
 #ifdef CONFIG_OF
@@ -18,6 +20,10 @@ extern int drm_of_component_probe(struct device *dev,
 extern int drm_of_encoder_active_endpoint(struct device_node *node,
 					  struct drm_encoder *encoder,
 					  struct of_endpoint *endpoint);
+int drm_of_find_panel_or_bridge(const struct device_node *np,
+				int port, int endpoint,
+				struct drm_panel **panel,
+				struct drm_bridge **bridge);
 #else
 static inline uint32_t drm_of_find_possible_crtcs(struct drm_device *dev,
 						  struct device_node *port)
@@ -36,6 +42,13 @@ drm_of_component_probe(struct device *dev,
 static inline int drm_of_encoder_active_endpoint(struct device_node *node,
 						 struct drm_encoder *encoder,
 						 struct of_endpoint *endpoint)
+{
+	return -EINVAL;
+}
+static inline int drm_of_find_panel_or_bridge(const struct device_node *np,
+					      int port, int endpoint,
+					      struct drm_panel **panel,
+					      struct drm_bridge **bridge)
 {
 	return -EINVAL;
 }
