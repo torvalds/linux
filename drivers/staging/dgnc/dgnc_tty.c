fdbcd023576a4397dcef3acffa1b8c00993cbf90
Abhishek Bhardwaj <abshkbh@gmail.com>
2017-03-22 01:43:39 -0700

    staging: dgnc: remove tracing prints in dgnc_tty.c
    Remove tracing prints in dgnc_tty_send_xchar. The same functionality
    could be achieved by using ftrace.
    
    Signed-off-by: Abhishek Bhardwaj <abshkbh@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_tty.c
+++ drivers/staging/dgnc/dgnc_tty.c
@@ -1858,13 +1858,9 @@ static void dgnc_tty_send_xchar(struct tty_struct *tty, char c)
 	if (!bd || bd->magic != DGNC_BOARD_MAGIC)
 		return;
 
-	dev_dbg(tty->dev, "dgnc_tty_send_xchar start\n");
-
 	spin_lock_irqsave(&ch->ch_lock, flags);
 	bd->bd_ops->send_immediate_char(ch, c);
 	spin_unlock_irqrestore(&ch->ch_lock, flags);
-
-	dev_dbg(tty->dev, "dgnc_tty_send_xchar finish\n");
 }
 
 /* Return modem signals to ld. */
