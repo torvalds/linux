2e263c824a5dfc3adcadac0ac1aed762605f0538
Alex Deucher <alexander.deucher@amd.com>
2017-03-23 01:38:11 -0400

    drm/amdgpu/gfx8: test KIQ before compute rings
    If KIQ isn't working, the compute rings won't work either.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gfx_v8_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v8_0.c
@@ -4980,6 +4980,13 @@ static int gfx_v8_0_kiq_resume(struct amdgpu_device *adev)
 	if (r)
 		goto done;
 
+	ring->ready = true;
+	r = amdgpu_ring_test_ring(ring);
+	if (r) {
+		ring->ready = false;
+		goto done;
+	}
+
 	for (i = 0; i < adev->gfx.num_compute_rings; i++) {
 		ring = &adev->gfx.compute_ring[i];
 
@@ -5007,12 +5014,6 @@ static int gfx_v8_0_kiq_resume(struct amdgpu_device *adev)
 		if (r)
 			ring->ready = false;
 	}
-
-	ring = &adev->gfx.kiq.ring;
-	ring->ready = true;
-	r = amdgpu_ring_test_ring(ring);
-	if (r)
-		ring->ready = false;
 
 done:
 	return r;
