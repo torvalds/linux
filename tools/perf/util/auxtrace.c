c3a0bbc7ad7598dec5a204868bdf8a2b1b51df14
Adrian Hunter <adrian.hunter@intel.com>
2017-03-24 14:15:52 +0200

    perf auxtrace: Fix no_size logic in addr_filter__resolve_kernel_syms()
    Address filtering with kernel symbols incorrectly resulted in the error
    "Cannot determine size of symbol" because the no_size logic was the wrong
    way around.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Tested-by: Andi Kleen <ak@linux.intel.com>
    Cc: stable@vger.kernel.org # v4.9+
    Link: http://lkml.kernel.org/r/1490357752-27942-1-git-send-email-adrian.hunter@intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/auxtrace.c
+++ tools/perf/util/auxtrace.c
@@ -1826,7 +1826,7 @@ static int addr_filter__resolve_kernel_syms(struct addr_filter *filt)
 		filt->addr = start;
 		if (filt->range && !filt->size && !filt->sym_to) {
 			filt->size = size;
-			no_size = !!size;
+			no_size = !size;
 		}
 	}
 
@@ -1840,7 +1840,7 @@ static int addr_filter__resolve_kernel_syms(struct addr_filter *filt)
 		if (err)
 			return err;
 		filt->size = start + size - filt->addr;
-		no_size = !!size;
+		no_size = !size;
 	}
 
 	/* The very last symbol in kallsyms does not imply a particular size */
