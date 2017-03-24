a676bdc422241822130364443a6a65b6520440ba
Roderick Colenbrander <roderick.colenbrander@sony.com>
2017-03-24 15:17:50 -0700

    HID: sony: Make sure to unregister sensors on failure
    Make sure we sure register any sensor when sony_input_configured failes.
    Somehow this line got lost during resolving of merge conflicts in the
    motion sensor patch series and a redudant remove was added as well later
    on.
    
    Signed-off-by: Roderick Colenbrander <roderick.colenbrander@sony.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-sony.c
+++ drivers/hid/hid-sony.c
@@ -2696,6 +2696,8 @@ err_stop:
 		sony_leds_remove(sc);
 	if (sc->quirks & SONY_BATTERY_SUPPORT)
 		sony_battery_remove(sc);
+	if (sc->sensor_dev)
+		sony_unregister_sensors(sc);
 	sony_cancel_work_sync(sc);
 	kfree(sc->output_report_dmabuf);
 	sony_remove_dev_list(sc);
@@ -2782,9 +2784,6 @@ static void sony_remove(struct hid_device *hdev)
 
 	if (sc->touchpad)
 		sony_unregister_touchpad(sc);
-
-	if (sc->sensor_dev)
-		sony_unregister_sensors(sc);
 
 	if (sc->sensor_dev)
 		sony_unregister_sensors(sc);
