182f569660548d16af895af9fede54367c377f47
Ganapathi Bhat <gbhat@marvell.com>
2017-03-27 18:27:44 +0530

    mwifiex: Support USB interrupt endpoint for command response/event
    USB firmware added support for sending command response/event through
    interrupt endpoint, to enhance RX throughput. Added corresponding changes
    required to support this feature. This change takes care of backward
    compatibility with older firmware.
    
    Signed-off-by: Ganapathi Bhat <gbhat@marvell.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/marvell/mwifiex/usb.h
+++ drivers/net/wireless/marvell/mwifiex/usb.h
@@ -90,6 +90,10 @@ struct usb_card_rec {
 	struct urb_context tx_cmd;
 	u8 mc_resync_flag;
 	struct usb_tx_data_port port[MWIFIEX_TX_DATA_PORT];
+	int rx_cmd_ep_type;
+	u8 rx_cmd_interval;
+	int tx_cmd_ep_type;
+	u8 tx_cmd_interval;
 };
 
 struct fw_header {
