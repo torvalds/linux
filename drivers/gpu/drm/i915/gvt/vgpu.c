bf39ec335eb8cc51b4e1c9303ef92b380d204bb1
Zhenyu Wang <zhenyuw@linux.intel.com>
2017-03-27 17:41:02 +0800

    drm/i915/gvt: adjust mem size for low resolution type
    From commit d1a513be1f0a ("drm/i915/gvt: add resolution definition for vGPU
    type"), small type has been restricted to small resolution, so not
    require larger high GM size any more. Change to smaller 384M for more
    VM creation with vGPU enabled which still perform reasonable workload.
    
    Fixes: d1a513be1f0a ("drm/i915/gvt: add resolution definition for vGPU type")
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>

--- drivers/gpu/drm/i915/gvt/vgpu.c
+++ drivers/gpu/drm/i915/gvt/vgpu.c
@@ -72,7 +72,7 @@ static struct {
 	char *name;
 } vgpu_types[] = {
 /* Fixed vGPU type table */
-	{ MB_TO_BYTES(64), MB_TO_BYTES(512), 4, GVT_EDID_1024_768, "8" },
+	{ MB_TO_BYTES(64), MB_TO_BYTES(384), 4, GVT_EDID_1024_768, "8" },
 	{ MB_TO_BYTES(128), MB_TO_BYTES(512), 4, GVT_EDID_1920_1200, "4" },
 	{ MB_TO_BYTES(256), MB_TO_BYTES(1024), 4, GVT_EDID_1920_1200, "2" },
 	{ MB_TO_BYTES(512), MB_TO_BYTES(2048), 4, GVT_EDID_1920_1200, "1" },
