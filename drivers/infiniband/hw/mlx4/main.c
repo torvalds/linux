b5834cc1690102c46f839ac4cbfb1f44c06ad1da
Maor Gottlieb <maorg@mellanox.com>
2017-03-29 06:03:00 +0300

    IB/mlx4: Take write semaphore when changing the vma struct
    [ Upstream commit 22c3653d04bd0c67b75e99d85e0c0bdf83947df5 ]
    
    When the driver disassociate user context, it changes the vma to
    anonymous by setting the vm_ops to null and zap the vma ptes.
    
    In order to avoid race in the kernel, we need to take write lock
    before we change the vma entries.
    
    Fixes: ae184ddeca5db ('IB/mlx4_ib: Disassociate support')
    Signed-off-by: Maor Gottlieb <maorg@mellanox.com>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    Signed-off-by: Doug Ledford <dledford@redhat.com>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/infiniband/hw/mlx4/main.c
+++ drivers/infiniband/hw/mlx4/main.c
@@ -1041,7 +1041,7 @@ static void mlx4_ib_disassociate_ucontext(struct ib_ucontext *ibcontext)
 	/* need to protect from a race on closing the vma as part of
 	 * mlx4_ib_vma_close().
 	 */
-	down_read(&owning_mm->mmap_sem);
+	down_write(&owning_mm->mmap_sem);
 	for (i = 0; i < HW_BAR_COUNT; i++) {
 		vma = context->hw_bar_info[i].vma;
 		if (!vma)
@@ -1059,7 +1059,7 @@ static void mlx4_ib_disassociate_ucontext(struct ib_ucontext *ibcontext)
 		context->hw_bar_info[i].vma->vm_ops = NULL;
 	}
 
-	up_read(&owning_mm->mmap_sem);
+	up_write(&owning_mm->mmap_sem);
 	mmput(owning_mm);
 	put_task_struct(owning_process);
 }
