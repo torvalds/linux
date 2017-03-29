ec63982e90a8793a838dfba9037bde597bb565e1
Tom St Denis <tom.stdenis@amd.com>
2017-03-29 13:01:30 -0400

    drm/amd/amdgpu: Correct ring wptr address in debugfs (v2)
    On gfx9 hardware the value is not wrapped and is a 64-bit value.  So
    we reduce it modulo the ring size.
    
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    
    (v2) use buf_mask instead of computing on the fly
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ring.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ring.c
@@ -320,8 +320,8 @@ static ssize_t amdgpu_debugfs_ring_read(struct file *f, char __user *buf,
 
 	if (*pos < 12) {
 		early[0] = amdgpu_ring_get_rptr(ring);
-		early[1] = amdgpu_ring_get_wptr(ring);
-		early[2] = ring->wptr;
+		early[1] = amdgpu_ring_get_wptr(ring) & ring->buf_mask;
+		early[2] = ring->wptr & ring->buf_mask;
 		for (i = *pos / 4; i < 3 && size; i++) {
 			r = put_user(early[i], (uint32_t *)buf);
 			if (r)
