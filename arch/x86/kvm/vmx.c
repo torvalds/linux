004772313040ed5b78560de1fe5d51ea678977c7
Paolo Bonzini <pbonzini@redhat.com>
2017-03-30 11:55:28 +0200

    KVM: VMX: remove bogus check for invalid EPT violation
    handle_ept_violation is checking for "guest-linear-address invalid" +
    "not a paging-structure walk".  However, _all_ EPT violations without
    a valid guest linear address are paging structure walks, because those
    EPT violations happen when loading the guest PDPTEs.
    
    Therefore, the check can never be true, and even if it were, KVM doesn't
    care about the guest linear address; it only uses the guest *physical*
    address VMCS field.  So, remove the check altogether.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Reviewed-by: Jim Mattson <jmattson@google.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -6208,22 +6208,8 @@ static int handle_ept_violation(struct kvm_vcpu *vcpu)
 	unsigned long exit_qualification;
 	gpa_t gpa;
 	u32 error_code;
-	int gla_validity;
 
 	exit_qualification = vmcs_readl(EXIT_QUALIFICATION);
-
-	gla_validity = (exit_qualification >> 7) & 0x3;
-	if (gla_validity == 0x2) {
-		printk(KERN_ERR "EPT: Handling EPT violation failed!\n");
-		printk(KERN_ERR "EPT: GPA: 0x%lx, GVA: 0x%lx\n",
-			(long unsigned int)vmcs_read64(GUEST_PHYSICAL_ADDRESS),
-			vmcs_readl(GUEST_LINEAR_ADDRESS));
-		printk(KERN_ERR "EPT: Exit qualification is 0x%lx\n",
-			(long unsigned int)exit_qualification);
-		vcpu->run->exit_reason = KVM_EXIT_UNKNOWN;
-		vcpu->run->hw.hardware_exit_reason = EXIT_REASON_EPT_VIOLATION;
-		return 0;
-	}
 
 	/*
 	 * EPT violation happened while executing iret from NMI,
