919a9bb607fe6793144f9ed171f7e5382e9cc65a
Alex Wilson <alex.david.wilson@gmail.com>
2017-03-30 01:16:02 -0600

    staging: fbtft: fix type assignment warning
    Sparse spits out a warning that a __be16 was being assigned to a u16.
    Change the type of txbuf16 to __be16 b/c it's a restricted type and
    prevents mixing endianness.
    
    Signed-off-by: Alex Wilson <alex.david.wilson@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fbtft/fbtft-bus.c
+++ drivers/staging/fbtft/fbtft-bus.c
@@ -114,7 +114,7 @@ EXPORT_SYMBOL(fbtft_write_reg8_bus9);
 int fbtft_write_vmem16_bus8(struct fbtft_par *par, size_t offset, size_t len)
 {
 	u16 *vmem16;
-	u16 *txbuf16 = par->txbuf.buf;
+	__be16 *txbuf16 = par->txbuf.buf;
 	size_t remain;
 	size_t to_copy;
 	size_t tx_array_size;
