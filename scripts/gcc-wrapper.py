8c7f8dfca2f1628e247a5ebd6c75728beec5f4c7
Huang, Tao <huangtao@rock-chips.com>
2017-03-31 15:15:45 +0800

    rk: gcc-wrapper.py ignore memcontrol.c:5337
    Change-Id: I3052d1659e45c7c2b36578abf3d52b97a32c8af3
    Signed-off-by: Huang, Tao <huangtao@rock-chips.com>

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -46,7 +46,7 @@ allowed_warnings = set([
     "ks8851.c:298", # drivers/net/ethernet/micrel/ks8851.c:298:2: warning: 'rxb[0]' may be used uninitialized in this function
     "ks8851.c:421", # drivers/net/ethernet/micrel/ks8851.c:421:20: warning: 'rxb[0]' may be used uninitialized in this function
     "compat_binfmt_elf.c:58", # fs/compat_binfmt_elf.c:58:13: warning: 'cputime_to_compat_timeval' defined but not used
-    "memcontrol.c:5355", # mm/memcontrol.c:5355:12: warning: initialization from incompatible pointer type
+    "memcontrol.c:5337", # mm/memcontrol.c:5337:12: warning: initialization from incompatible pointer type
  ])
 
 # Capture the name of the object file, can find it.
