bbc2a2df92472eba0c5fddeea4a13134e72fefce
Riku Salminen <riku@laatikko.io>
2017-03-31 16:10:32 +0300

    Staging: nvec: Remove FSF's mailing address
    Removed Free Software Foundation's address from the copyright notice
    and replaced it with a link to http://www.gnu.org/licenses
    
    Signed-off-by: Riku Salminen <riku@laatikko.io>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/nvec/nvec-keytable.h
+++ drivers/staging/nvec/nvec-keytable.h
@@ -17,8 +17,7 @@
  * more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * with this program; if not, see http://www.gnu.org/licenses
  */
 
 static unsigned short code_tab_102us[] = {
