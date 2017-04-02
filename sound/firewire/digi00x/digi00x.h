13e005f9f933a35b5e55c9d36f151efe2a8383ec
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2017-04-02 23:48:24 +0900

    ALSA: firewire-digi00x: add support for console models of Digi00x series
    Digi00x series includes two types of unit; rack and console. As long as
    reading information on config rom of Digi 002 console, 'MODEL_ID' field
    has a different value from the one on Digi 002 rack.
    
    We've already got a test report from users with Digi 003 rack. We can
    assume that console type and rack type has different value in the field.
    
    This commit adds a device entry for console type. For following commits,
    this commit also adds a member to 'struct snd_digi00x' to identify console
    type.
    
    $ cd linux-firewire-utils/src
    $ python2 ./crpp < /sys/bus/firewire/devices/fw1/config_rom
                   ROM header and bus information block
                   -----------------------------------------------------------------
    400  0404f9d0  bus_info_length 4, crc_length 4, crc 63952
    404  31333934  bus_name "1394"
    408  60647002  irmc 0, cmc 1, isc 1, bmc 0, cyc_clk_acc 100, max_rec 7 (256)
    40c  00a07e00  company_id 00a07e     |
    410  00a30000  device_id 0000a30000  | EUI-64 00a07e0000a30000
    
                   root directory
                   -----------------------------------------------------------------
    414  00058a39  directory_length 5, crc 35385
    418  0c0043a0  node capabilities
    41c  04000001  hardware version
    420  0300a07e  vendor
    424  81000007  --> descriptor leaf at 440
    428  d1000001  --> unit directory at 42c
    
                   unit directory at 42c
                   -----------------------------------------------------------------
    42c  00046674  directory_length 4, crc 26228
    430  120000a3  specifier id
    434  13000001  version
    438  17000001  model
    43c  81000007  --> descriptor leaf at 458
    
                   descriptor leaf at 440
                   -----------------------------------------------------------------
    440  00055913  leaf_length 5, crc 22803
    444  000050f2  descriptor_type 00, specifier_ID 50f2
    448  80000000
    44c  44696769
    450  64657369
    454  676e0000
    
                   descriptor leaf at 458
                   -----------------------------------------------------------------
    458  0004a6fd  leaf_length 4, crc 42749
    45c  00000000  textual descriptor
    460  00000000  minimal ASCII
    464  44696769  "Digi"
    468  20303032  " 002"
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/digi00x/digi00x.h
+++ sound/firewire/digi00x/digi00x.h
@@ -61,6 +61,7 @@ struct snd_dg00x {
 	/* For asynchronous MIDI controls. */
 	struct snd_rawmidi_substream *in_control;
 	struct snd_fw_async_midi_port out_control;
+	bool is_console;
 };
 
 #define DG00X_ADDR_BASE		0xffffe0000000ull
