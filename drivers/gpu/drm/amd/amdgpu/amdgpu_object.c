803d89ade4ede131405c57ec1a1db32673995b2e
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2017-04-03 13:31:22 +1000

    drm/amdgpu: Refuse to pin or change acceptable domains of prime BOs to VRAM. (v2)
    Migration to VRAM will break the sharing, resulting in rendering on the exporting GPU never becoming
    visible on the importing GPU.
    
    v2: Don't pin BOs to GTT. Instead, refuse to migrate them out of GTT.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_object.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_object.c
@@ -650,6 +650,10 @@ int amdgpu_bo_pin_restricted(struct amdgpu_bo *bo, u32 domain,
 	if (WARN_ON_ONCE(min_offset > max_offset))
 		return -EINVAL;
 
+	/* A shared bo cannot be migrated to VRAM */
+	if (bo->prime_shared_count && (domain == AMDGPU_GEM_DOMAIN_VRAM))
+		return -EINVAL;
+
 	if (bo->pin_count) {
 		uint32_t mem_type = bo->tbo.mem.mem_type;
 
