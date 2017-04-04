d84be51d1c1d3fa148a3abdeeb1455690df59e63
Ritesh Harjani <riteshh@codeaurora.org>
2017-04-04 19:32:07 +0000

    mmc: sdhci-msm: Enable few quirks
    [ Upstream commit a0e3142869d29688de6f77be31aa7a401a4a88f1 ]
    
    sdhc-msm controller needs this SDHCI_QUIRK_CAP_CLOCK_BASE_BROKEN
    & SDHCI_QUIRK2_PRESET_VALUE_BROKEN to be set. Hence setting it.
    
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>
    Signed-off-by: Ritesh Harjani <riteshh@codeaurora.org>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -524,7 +524,9 @@ static const struct sdhci_ops sdhci_msm_ops = {
 static const struct sdhci_pltfm_data sdhci_msm_pdata = {
 	.quirks = SDHCI_QUIRK_BROKEN_CARD_DETECTION |
 		  SDHCI_QUIRK_NO_CARD_NO_RESET |
-		  SDHCI_QUIRK_SINGLE_POWER_WRITE,
+		  SDHCI_QUIRK_SINGLE_POWER_WRITE |
+		  SDHCI_QUIRK_CAP_CLOCK_BASE_BROKEN,
+	.quirks2 = SDHCI_QUIRK2_PRESET_VALUE_BROKEN,
 	.ops = &sdhci_msm_ops,
 };
 
