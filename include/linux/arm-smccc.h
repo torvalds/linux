bec9918bb4dae2099c37791133403c32aa2738c7
Andy Gross <andy.gross@linaro.org>
2017-04-04 19:32:32 +0000

    firmware: qcom: scm: Fix interrupted SCM calls
    [ Upstream commit 82bcd087029f6056506ea929f11af02622230901 ]
    
    This patch adds a Qualcomm specific quirk to the arm_smccc_smc call.
    
    On Qualcomm ARM64 platforms, the SMC call can return before it has
    completed.  If this occurs, the call can be restarted, but it requires
    using the returned session ID value from the interrupted SMC call.
    
    The quirk stores off the session ID from the interrupted call in the
    quirk structure so that it can be used by the caller.
    
    This patch folds in a fix given by Sricharan R:
    https://lkml.org/lkml/2016/9/28/272
    
    Signed-off-by: Andy Gross <andy.gross@linaro.org>
    Reviewed-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/arm-smccc.h
+++ include/linux/arm-smccc.h
@@ -14,9 +14,6 @@
 #ifndef __LINUX_ARM_SMCCC_H
 #define __LINUX_ARM_SMCCC_H
 
-#include <linux/linkage.h>
-#include <linux/types.h>
-
 /*
  * This file provides common defines for ARM SMC Calling Convention as
  * specified in
@@ -60,6 +57,13 @@
 #define ARM_SMCCC_OWNER_TRUSTED_OS	50
 #define ARM_SMCCC_OWNER_TRUSTED_OS_END	63
 
+#define ARM_SMCCC_QUIRK_NONE		0
+#define ARM_SMCCC_QUIRK_QCOM_A6		1 /* Save/restore register a6 */
+
+#ifndef __ASSEMBLY__
+
+#include <linux/linkage.h>
+#include <linux/types.h>
 /**
  * struct arm_smccc_res - Result from SMC/HVC call
  * @a0-a3 result values from registers 0 to 3
@@ -125,4 +129,5 @@ asmlinkage void __arm_smccc_hvc(unsigned long a0, unsigned long a1,
 
 #define arm_smccc_hvc_quirk(...) __arm_smccc_hvc(__VA_ARGS__)
 
+#endif /*__ASSEMBLY__*/
 #endif /*__LINUX_ARM_SMCCC_H*/
