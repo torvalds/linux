00f660eaf37808aa0a537c288c9e2dee73bd4316
Chenbo Feng <fengc@google.com>
2017-04-05 19:00:56 -0700

    Sample program using SO_COOKIE
    Added a per socket traffic monitoring option to illustrate the usage
    of new getsockopt SO_COOKIE. The program is based on the socket traffic
    monitoring program using xt_eBPF and in the new option the data entry
    can be directly accessed using socket cookie. The cookie retrieved
    allow us to lookup an element in the eBPF for a specific socket.
    
    Signed-off-by: Chenbo Feng <fengc@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- samples/bpf/run_cookie_uid_helper_example.sh
+++ samples/bpf/run_cookie_uid_helper_example.sh
@@ -4,11 +4,11 @@ root_dir=$local_dir/../..
 mnt_dir=$(mktemp -d --tmp)
 
 on_exit() {
-	iptables -D INPUT -m bpf --object-pinned ${mnt_dir}/bpf_prog -j ACCEPT
+	iptables -D OUTPUT -m bpf --object-pinned ${mnt_dir}/bpf_prog -j ACCEPT
 	umount ${mnt_dir}
 	rm -r ${mnt_dir}
 }
 
 trap on_exit EXIT
 mount -t bpf bpf ${mnt_dir}
-./per_socket_stats_example ${mnt_dir}/bpf_prog
+./per_socket_stats_example ${mnt_dir}/bpf_prog $1
