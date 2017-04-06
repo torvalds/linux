297e14d5c6b6b51ccb79986b5e1dbbf8b7bd92bc
Antti Palosaari <crope@iki.fi>
2017-04-06 09:17:42 -0300

    [media] si2157: revert si2157: Si2141/2151 tuner support
    'Reset' loop does not look correct. I tested it very many times and
    it never repeated those commands. If problem, it tries to solve,
    really occurs on some situations better solution should be find out.
    
    There is another patch which does not have such hackish looking loop.
    Lets change to it.
    
    Cc: Evgeny Plehov <EvgenyPlehov@ukr.net>
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/tuners/si2157_priv.h
+++ drivers/media/tuners/si2157_priv.h
@@ -42,7 +42,6 @@ struct si2157_dev {
 
 #define SI2157_CHIPTYPE_SI2157 0
 #define SI2157_CHIPTYPE_SI2146 1
-#define SI2157_CHIPTYPE_SI2141 2
 
 /* firmware command struct */
 #define SI2157_ARGLEN      30
@@ -53,6 +52,5 @@ struct si2157_cmd {
 };
 
 #define SI2158_A20_FIRMWARE "dvb-tuner-si2158-a20-01.fw"
-#define SI2141_A10_FIRMWARE "dvb-tuner-si2141-a10-01.fw"
 
 #endif
