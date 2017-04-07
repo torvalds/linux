ba7454e17f90a7dfecc3eb82ec19eb7ac5b6f0e0
David Hildenbrand <david@redhat.com>
2017-04-07 10:50:28 +0200

    KVM: x86: don't take kvm->irq_lock when creating IRQCHIP
    I don't see any reason any more for this lock, seemed to be used to protect
    removal of kvm->arch.vpic / kvm->arch.vioapic when already partially
    inititalized, now access is properly protected using kvm->arch.irqchip_mode
    and this shouldn't be necessary anymore.
    
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4028,10 +4028,8 @@ long kvm_arch_vm_ioctl(struct file *filp,
 			/* Pairs with smp_rmb() when reading irqchip_mode */
 			smp_wmb();
 			mutex_lock(&kvm->slots_lock);
-			mutex_lock(&kvm->irq_lock);
 			kvm_ioapic_destroy(kvm);
 			kvm_pic_destroy(kvm);
-			mutex_unlock(&kvm->irq_lock);
 			mutex_unlock(&kvm->slots_lock);
 			goto create_irqchip_unlock;
 		}
