c98cdff94a6a7877923dec1329c2b76d6247d076
Sean Paul <seanpaul@chromium.org>
2017-04-07 16:15:30 -0400

    Revert "drm: Don't allow interruptions when opening debugfs/crc"
    This reverts
    
    commit b8dfa821c27046ede9bf8eb14cc8109d379428e9
    Author: Chris Wilson <chris@chris-wilson.co.uk>
    Date:   Fri Apr 7 12:17:12 2017 +0100
        drm: Don't allow interruptions when opening debugfs/crc
    
    It reportedly breaks things, so let's revert now and try again later.
    
    Fixes: b8dfa821c270 ("drm: Don't allow interruptions when opening debugfs/crc")
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Daniel Vetter <daniel.vetter@intel.com>
    Cc: Jani Nikula <jani.nikula@linux.intel.com>
    Cc: Sean Paul <seanpaul@chromium.org>
    Cc: David Airlie <airlied@linux.ie>
    Cc: dri-devel@lists.freedesktop.org
    Reported-by: Chris Wilson <chris@chris-wilson.co.uk>
    Acked-by: Chris Wilson <chris@chris-wilson.co.uk>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Sean Paul <seanpaul@chromium.org>

--- drivers/gpu/drm/drm_debugfs_crc.c
+++ drivers/gpu/drm/drm_debugfs_crc.c
@@ -177,8 +177,12 @@ static int crtc_crc_open(struct inode *inode, struct file *filep)
 	 * guess when this particular piece of HW will be ready to start
 	 * generating CRCs.
 	 */
-	wait_event_lock_irq(crc->wq, crtc_crc_data_count(crc), crc->lock);
+	ret = wait_event_interruptible_lock_irq(crc->wq,
+						crtc_crc_data_count(crc),
+						crc->lock);
 	spin_unlock_irq(&crc->lock);
+
+	WARN_ON(ret);
 
 	return 0;
 
