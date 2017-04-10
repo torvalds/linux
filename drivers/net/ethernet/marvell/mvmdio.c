a51e2c9da44acad7494ac3f57c48f296890cbe2a
Russell King <rmk+kernel@armlinux.org.uk>
2017-04-10 16:28:20 +0100

    net: mvmdio: disable interrupt if resource size is too small
    Disable the MDIO interrupt, falling back to polled mode, if the resource
    size does not allow us to access the interrupt registers.  All current
    DT bindings use a size of 0x84, which allows access, but verifying it is
    good practice.
    
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/mvmdio.c
+++ drivers/net/ethernet/marvell/mvmdio.c
@@ -221,6 +221,12 @@ static int orion_mdio_probe(struct platform_device *pdev)
 		clk_prepare_enable(dev->clk);
 
 	dev->err_interrupt = platform_get_irq(pdev, 0);
+	if (dev->err_interrupt > 0 &&
+	    resource_size(r) < MVMDIO_ERR_INT_MASK + 4) {
+		dev_err(&pdev->dev,
+			"disabling interrupt, resource size is too small\n");
+		dev->err_interrupt = 0;
+	}
 	if (dev->err_interrupt > 0) {
 		ret = devm_request_irq(&pdev->dev, dev->err_interrupt,
 					orion_mdio_err_irq,
