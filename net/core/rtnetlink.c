085e1a65f04fb05941de0a071c8d70246cd03178
David Ahern <dsa@cumulusnetworks.com>
2017-04-11 17:02:40 -0700

    rtnetlink: Do not generate notifications for MTU events
    Changing MTU on a link currently causes 3 messages to be sent to userspace:
    
    [LINK]11: dummy1: <BROADCAST,NOARP,UP,LOWER_UP> mtu 1490 qdisc noqueue state UNKNOWN group default
        link/ether f2:52:5c:6d:21:f3 brd ff:ff:ff:ff:ff:ff
    
    [LINK]11: dummy1: <BROADCAST,NOARP,UP,LOWER_UP> mtu 1500 qdisc noqueue state UNKNOWN group default
        link/ether f2:52:5c:6d:21:f3 brd ff:ff:ff:ff:ff:ff
    
    [LINK]11: dummy1: <BROADCAST,NOARP,UP,LOWER_UP> mtu 1500 qdisc noqueue state UNKNOWN group default
        link/ether f2:52:5c:6d:21:f3 brd ff:ff:ff:ff:ff:ff
    
    Remove the messages sent for PRE_CHANGE_MTU and CHANGE_MTU netdev events.
    
    Signed-off-by: David Ahern <dsa@cumulusnetworks.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -4117,7 +4117,6 @@ static int rtnetlink_event(struct notifier_block *this, unsigned long event, voi
 
 	switch (event) {
 	case NETDEV_REBOOT:
-	case NETDEV_CHANGEMTU:
 	case NETDEV_CHANGEADDR:
 	case NETDEV_CHANGENAME:
 	case NETDEV_FEAT_CHANGE:
@@ -4126,7 +4125,6 @@ static int rtnetlink_event(struct notifier_block *this, unsigned long event, voi
 	case NETDEV_NOTIFY_PEERS:
 	case NETDEV_CHANGEUPPER:
 	case NETDEV_RESEND_IGMP:
-	case NETDEV_PRECHANGEMTU:
 	case NETDEV_CHANGEINFODATA:
 	case NETDEV_PRECHANGEUPPER:
 	case NETDEV_CHANGELOWERSTATE:
