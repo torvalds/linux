aef091ae58aab340afb6f4d6a835037e66fa56a1
David Ahern <dsa@cumulusnetworks.com>
2017-04-11 17:02:43 -0700

    rtnetlink: Do not generate notifications for POST_TYPE_CHANGE event
    Changing the master device for a link generates many messages; the one
    generated for POST_TYPE_CHANGE is redundant:
    
    [LINK]11: dummy1: <BROADCAST,NOARP,UP,LOWER_UP> mtu 1500 qdisc noqueue master br1 state UNKNOWN group default
        link/ether 02:02:02:02:02:03 brd ff:ff:ff:ff:ff:ff
    [LINK]11: dummy1: <BROADCAST,NOARP,UP,LOWER_UP> mtu 1500 qdisc noqueue master br1 state UNKNOWN group default
        link/ether 02:02:02:02:02:03 brd ff:ff:ff:ff:ff:ff
    
    Remove POST_TYPE_CHANGE from the list of notifiers that generate
    notifications.
    
    Signed-off-by: David Ahern <dsa@cumulusnetworks.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -4120,7 +4120,6 @@ static int rtnetlink_event(struct notifier_block *this, unsigned long event, voi
 	case NETDEV_CHANGENAME:
 	case NETDEV_FEAT_CHANGE:
 	case NETDEV_BONDING_FAILOVER:
-	case NETDEV_POST_TYPE_CHANGE:
 	case NETDEV_NOTIFY_PEERS:
 	case NETDEV_CHANGEUPPER:
 	case NETDEV_RESEND_IGMP:
