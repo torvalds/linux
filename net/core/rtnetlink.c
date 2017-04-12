27b3b551d8a7af98423b4a2b0d033013c08d812c
David Ahern <dsa@cumulusnetworks.com>
2017-04-11 17:02:47 -0700

    rtnetlink: Do not generate notifications for NETDEV_CHANGE_TX_QUEUE_LEN event
    Changing tx queue length generates identical messages:
    
    [LINK]22: dummy1: <BROADCAST,NOARP,UP,LOWER_UP> mtu 1500 qdisc noqueue state UNKNOWN group default
        link/ether 02:04:f4:b7:5c:d2 brd ff:ff:ff:ff:ff:ff promiscuity 0
        dummy numtxqueues 1 numrxqueues 1 gso_max_size 65536 gso_max_segs 65535
    [LINK]22: dummy1: <BROADCAST,NOARP,UP,LOWER_UP> mtu 1500 qdisc noqueue state UNKNOWN group default
        link/ether 02:04:f4:b7:5c:d2 brd ff:ff:ff:ff:ff:ff promiscuity 0
        dummy numtxqueues 1 numrxqueues 1 gso_max_size 65536 gso_max_segs 65535
    
    Remove NETDEV_CHANGE_TX_QUEUE_LEN from the list of notifiers that generate
    notifications.
    
    Signed-off-by: David Ahern <dsa@cumulusnetworks.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -4123,7 +4123,6 @@ static int rtnetlink_event(struct notifier_block *this, unsigned long event, voi
 	case NETDEV_NOTIFY_PEERS:
 	case NETDEV_RESEND_IGMP:
 	case NETDEV_CHANGEINFODATA:
-	case NETDEV_CHANGE_TX_QUEUE_LEN:
 		rtmsg_ifinfo(RTM_NEWLINK, dev, 0, GFP_KERNEL);
 		break;
 	default:
