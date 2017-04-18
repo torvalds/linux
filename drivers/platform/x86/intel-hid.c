b899830506740faf509f3bb51d3d1e50102aed40
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2017-04-18 15:49:06 +0200

    platform/x86: intel-hid: Add missing ->thaw callback
    The intel-hid driver is missing a PM ->thaw callback allowing the
    device to go back to the operational state after creating a
    hibernation image or when there is an image restoration error during
    resume.
    
    The lack of the ->thaw callback basically means that all events
    signaled by the device are discarded after a hibernation image has
    been created which may be problematic, for example, if the image
    cannot be saved (eg. due to an I/O issue with storage).
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Darren Hart (VMware) <dvhart@infradead.org>

--- drivers/platform/x86/intel-hid.c
+++ drivers/platform/x86/intel-hid.c
@@ -134,6 +134,7 @@ static int intel_hid_pl_resume_handler(struct device *device)
 
 static const struct dev_pm_ops intel_hid_pl_pm_ops = {
 	.freeze  = intel_hid_pl_suspend_handler,
+	.thaw  = intel_hid_pl_resume_handler,
 	.restore  = intel_hid_pl_resume_handler,
 	.suspend  = intel_hid_pl_suspend_handler,
 	.resume  = intel_hid_pl_resume_handler,
