0be0dee64eacd950f8e4b6c45adb5a92392eaaaf
Bart Van Assche <bart.vanassche@sandisk.com>
2017-04-19 14:01:27 -0700

    block: Inline blk_rq_set_prio()
    Since only a single caller remains, inline blk_rq_set_prio(). Initialize
    req->ioprio even if no I/O priority has been set in the bio nor in the
    I/O context.
    
    Signed-off-by: Bart Van Assche <bart.vanassche@sandisk.com>
    Reviewed-by: Adam Manzanares <adam.manzanares@wdc.com>
    Tested-by: Adam Manzanares <adam.manzanares@wdc.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Cc: Matias Bjørling <m@bjorling.me>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -1630,14 +1630,19 @@ out:
 
 void blk_init_request_from_bio(struct request *req, struct bio *bio)
 {
+	struct io_context *ioc = rq_ioc(bio);
+
 	if (bio->bi_opf & REQ_RAHEAD)
 		req->cmd_flags |= REQ_FAILFAST_MASK;
 
 	req->errors = 0;
 	req->__sector = bio->bi_iter.bi_sector;
-	blk_rq_set_prio(req, rq_ioc(bio));
 	if (ioprio_valid(bio_prio(bio)))
 		req->ioprio = bio_prio(bio);
+	else if (ioc)
+		req->ioprio = ioc->ioprio;
+	else
+		req->ioprio = IOPRIO_PRIO_VALUE(IOPRIO_CLASS_NONE, 0);
 	blk_rq_bio_prep(req->q, req, bio);
 }
 EXPORT_SYMBOL_GPL(blk_init_request_from_bio);
