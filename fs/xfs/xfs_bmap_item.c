c4cf1acdb186436b88cc9aeaf08322bc49fec183
Darrick J. Wong <darrick.wong@oracle.com>
2017-04-20 15:09:05 -0700

    xfs: better log intent item refcount checking
    Use ASSERTs on the log intent item refcounts so that we fail noisily if
    anyone tries to double-free the item.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- fs/xfs/xfs_bmap_item.c
+++ fs/xfs/xfs_bmap_item.c
@@ -215,6 +215,7 @@ void
 xfs_bui_release(
 	struct xfs_bui_log_item	*buip)
 {
+	ASSERT(atomic_read(&buip->bui_refcount) > 0);
 	if (atomic_dec_and_test(&buip->bui_refcount)) {
 		xfs_trans_ail_remove(&buip->bui_item, SHUTDOWN_LOG_IO_ERROR);
 		xfs_bui_item_free(buip);
