afbb38fedaa25935b963b07cc10f72a25bb3acbb
James Smart <jsmart2021@gmail.com>
2017-04-21 16:04:53 -0700

    Fix extra line print in rqpair debug print.
    An extra blank line was being added the the rqpair printing.
    
    Remove the extra line feed.
    
    Signed-off-by: Dick Kennedy <dick.kennedy@broadcom.com>
    Signed-off-by: James Smart <james.smart@broadcom.com>
    Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>

--- drivers/scsi/lpfc/lpfc_debugfs.c
+++ drivers/scsi/lpfc/lpfc_debugfs.c
@@ -3128,8 +3128,6 @@ __lpfc_idiag_print_rqpair(struct lpfc_queue *qp, struct lpfc_queue *datqp,
 			datqp->queue_id, datqp->entry_count,
 			datqp->entry_size, datqp->host_index,
 			datqp->hba_index);
-	len +=  snprintf(pbuffer + len, LPFC_QUE_INFO_GET_BUF_SIZE - len, "\n");
-
 	return len;
 }
 
