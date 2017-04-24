e390b55d5aefe2b51569068b2a505d19d72afbf1
Daniel Borkmann <daniel@iogearbox.net>
2017-04-24 22:14:35 +0200

    bpf: make bpf_xdp_adjust_head support mandatory
    Now that also the last in-tree user of the xdp_adjust_head bit has
    been removed, we can remove the flag from struct bpf_prog altogether.
    
    This, at the same time, also makes sure that any future driver for
    XDP comes with bpf_xdp_adjust_head() support right away.
    
    A rejection based on this flag would also mean that tail calls
    couldn't be used with such driver as per c2002f983767 ("bpf: fix
    checking xdp_adjust_head on tail calls") fix, thus lets not allow
    for it in the first place.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -413,8 +413,7 @@ struct bpf_prog {
 				locked:1,	/* Program image locked? */
 				gpl_compatible:1, /* Is filter GPL compatible? */
 				cb_access:1,	/* Is control block accessed? */
-				dst_needed:1,	/* Do we need dst entry? */
-				xdp_adjust_head:1; /* Adjusting pkt head? */
+				dst_needed:1;	/* Do we need dst entry? */
 	kmemcheck_bitfield_end(meta);
 	enum bpf_prog_type	type;		/* Type of BPF program */
 	u32			len;		/* Number of filter blocks */
--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -3346,8 +3346,6 @@ static int fixup_bpf_calls(struct bpf_verifier_env *env)
 			prog->dst_needed = 1;
 		if (insn->imm == BPF_FUNC_get_prandom_u32)
 			bpf_user_rnd_init_once();
-		if (insn->imm == BPF_FUNC_xdp_adjust_head)
-			prog->xdp_adjust_head = 1;
 		if (insn->imm == BPF_FUNC_tail_call) {
 			/* If we tail call into other programs, we
 			 * cannot make any assumptions since they can
@@ -3355,7 +3353,6 @@ static int fixup_bpf_calls(struct bpf_verifier_env *env)
 			 * the program array.
 			 */
 			prog->cb_access = 1;
-			prog->xdp_adjust_head = 1;
 
 			/* mark bpf_tail_call as different opcode to avoid
 			 * conditional branch in the interpeter for every normal
