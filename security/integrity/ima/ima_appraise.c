e1f5e01f4b035ced1c71b40866e4e5c0508fbb0b
Mimi Zohar <zohar@linux.vnet.ibm.com>
2017-04-24 22:06:49 -0400

    ima: define Kconfig IMA_APPRAISE_BOOTPARAM option
    Permit enabling the different "ima_appraise=" modes (eg. log, fix)
    from the boot command line.
    
    Signed-off-by: Mimi Zohar <zohar@linux.vnet.ibm.com>

--- security/integrity/ima/ima_appraise.c
+++ security/integrity/ima/ima_appraise.c
@@ -20,12 +20,14 @@
 
 static int __init default_appraise_setup(char *str)
 {
+#ifdef CONFIG_IMA_APPRAISE_BOOTPARAM
 	if (strncmp(str, "off", 3) == 0)
 		ima_appraise = 0;
 	else if (strncmp(str, "log", 3) == 0)
 		ima_appraise = IMA_APPRAISE_LOG;
 	else if (strncmp(str, "fix", 3) == 0)
 		ima_appraise = IMA_APPRAISE_FIX;
+#endif
 	return 1;
 }
 
