43bcf707ccdc79ee63edb953fcf72e6dc244cfa1
Daniel Borkmann <daniel@iogearbox.net>
2017-04-27 01:39:34 +0200

    bpf: fix _htons occurences in test_progs
    Dave reported that on sparc test_progs generates buggy swapped
    eth->h_proto protocol comparisons:
    
      10: (15) if r3 == 0xdd86 goto pc+9
       R0=imm2,min_value=2,max_value=2 R1=pkt(id=0,off=0,r=14) R2=pkt_end R3=inv
       R4=pkt(id=0,off=14,r=14) R5=inv56 R10=fp
    
    This is due to the unconditional ...
    
      #define htons __builtin_bswap16
      #define ntohs __builtin_bswap16
    
    ... in test_progs that causes this. Make use of asm/byteorder.h
    and use __constant_htons() where possible and only perform the
    bswap16 when on little endian in non-constant case.
    
    Fixes: 6882804c916b ("selftests/bpf: add a test for overlapping packet range checks")
    Fixes: 37821613626e ("selftests/bpf: add l4 load balancer test based on sched_cls")
    Reported-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- tools/testing/selftests/bpf/bpf_util.h
+++ tools/testing/selftests/bpf/bpf_util.h
@@ -6,6 +6,25 @@
 #include <string.h>
 #include <errno.h>
 
+#include <asm/byteorder.h>
+
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+# define __bpf_ntohs(x)		__builtin_bswap16(x)
+# define __bpf_htons(x)		__builtin_bswap16(x)
+#elif __BYTE_ORDER == __BIG_ENDIAN
+# define __bpf_ntohs(x)		(x)
+# define __bpf_htons(x)		(x)
+#else
+# error "Fix your __BYTE_ORDER?!"
+#endif
+
+#define bpf_htons(x)				\
+	(__builtin_constant_p(x) ?		\
+	 __constant_htons(x) : __bpf_htons(x))
+#define bpf_ntohs(x)				\
+	(__builtin_constant_p(x) ?		\
+	 __constant_ntohs(x) : __bpf_ntohs(x))
+
 static inline unsigned int bpf_num_possible_cpus(void)
 {
 	static const char *fcpu = "/sys/devices/system/cpu/possible";
