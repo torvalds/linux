06b4fc520d21b7a6327983d64373505859672e91
Gao Feng <fgao@ikuai8.com>
2017-04-26 19:04:04 +0800

    net: fib: Decrease one unnecessary rt cache flush in fib_disable_ip
    The func fib_flush already flushes the rt cache if necessary, so it
    is not necessary to invoke rt_cache_flush again in fib_disable_ip.
    
    Signed-off-by: Gao Feng <fgao@ikuai8.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/fib_frontend.c
+++ net/ipv4/fib_frontend.c
@@ -1130,7 +1130,8 @@ static void fib_disable_ip(struct net_device *dev, unsigned long event,
 {
 	if (fib_sync_down_dev(dev, event, force))
 		fib_flush(dev_net(dev));
-	rt_cache_flush(dev_net(dev));
+	else
+		rt_cache_flush(dev_net(dev));
 	arp_ifdown(dev);
 }
 
