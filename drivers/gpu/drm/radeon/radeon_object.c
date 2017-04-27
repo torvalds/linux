634b6a8a064576797120b74facf95ba79283b5d5
Julien Isorce <julien.isorce@gmail.com>
2017-04-27 15:10:08 +0100

    drm/radeon: only warn once in radeon_ttm_bo_destroy if va list not empty
    Encountered a dozen of exact same backtraces when mesa's
    pb_cache_release_all_buffers is called after that a gpu reset failed.
    
    v2: Remove superfluous error message added in v1.
    
    bug: https://bugs.freedesktop.org/show_bug.cgi?id=96271
    
    Signed-off-by: Julien Isorce <jisorce@oblong.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/radeon_object.c
+++ drivers/gpu/drm/radeon/radeon_object.c
@@ -81,7 +81,7 @@ static void radeon_ttm_bo_destroy(struct ttm_buffer_object *tbo)
 	list_del_init(&bo->list);
 	mutex_unlock(&bo->rdev->gem.mutex);
 	radeon_bo_clear_surface_reg(bo);
-	WARN_ON(!list_empty(&bo->va));
+	WARN_ON_ONCE(!list_empty(&bo->va));
 	drm_gem_object_release(&bo->gem_base);
 	kfree(bo);
 }
