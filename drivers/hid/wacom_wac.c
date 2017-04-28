37d1601938349e79e9c31a8aba431d5543e09e72
Jason Gerecke <killertofu@gmail.com>
2017-04-28 09:25:30 -0700

    HID: wacom: generic: Scale battery capacity measurements to percentages
    The power_supply subsystem expects us to provide it with capacity values
    measured in percent. In particular, AES devices (HID_DG_BATTERYSTRENGTH)
    use the range 0-255, which needs to be rescaled. The MobileStudio Pro
    (WACOM_HID_WD_BATTERY_LEVEL) uses the range 0-100, but there's no guarantee
    that future devices will share the same range.
    
    Signed-off-by: Jason Gerecke <jason.gerecke@wacom.com>
    Reviewed-by: Ping Cheng <ping.cheng@wacom.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/wacom_wac.c
+++ drivers/hid/wacom_wac.c
@@ -1811,6 +1811,7 @@ static void wacom_wac_pad_battery_event(struct hid_device *hdev, struct hid_fiel
 
 	switch (equivalent_usage) {
 	case WACOM_HID_WD_BATTERY_LEVEL:
+		value = value * 100 / (field->logical_maximum - field->logical_minimum);
 		wacom_wac->hid_data.battery_capacity = value;
 		wacom_wac->hid_data.bat_connected = 1;
 		break;
@@ -2035,6 +2036,7 @@ static void wacom_wac_pen_event(struct hid_device *hdev, struct hid_field *field
 			wacom_wac->hid_data.sense_state = value;
 		return;
 	case HID_DG_BATTERYSTRENGTH:
+		value = value * 100 / (field->logical_maximum - field->logical_minimum);
 		wacom_wac->hid_data.battery_capacity = value;
 		wacom_wac->hid_data.bat_connected = 1;
 		break;
