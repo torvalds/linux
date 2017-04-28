6bdc00d01e202ae11fa1cae0dacbef895434483d
Stefan Wahren <stefan.wahren@i2se.com>
2017-04-28 13:47:21 +0200

    serdev: Restore serdev_device_write_buf for atomic context
    Starting with commit 6fe729c4bdae ("serdev: Add serdev_device_write
    subroutine") the function serdev_device_write_buf cannot be used in
    atomic context anymore (mutex_lock is sleeping). So restore the old
    behavior.
    
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>
    Fixes: 6fe729c4bdae ("serdev: Add serdev_device_write subroutine")
    Acked-by: Rob Herring <robh@kernel.org>
    Reviewed-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/serdev.h
+++ include/linux/serdev.h
@@ -195,6 +195,7 @@ int serdev_device_open(struct serdev_device *);
 void serdev_device_close(struct serdev_device *);
 unsigned int serdev_device_set_baudrate(struct serdev_device *, unsigned int);
 void serdev_device_set_flow_control(struct serdev_device *, bool);
+int serdev_device_write_buf(struct serdev_device *, const unsigned char *, size_t);
 void serdev_device_wait_until_sent(struct serdev_device *, long);
 int serdev_device_get_tiocm(struct serdev_device *);
 int serdev_device_set_tiocm(struct serdev_device *, int, int);
@@ -236,6 +237,12 @@ static inline unsigned int serdev_device_set_baudrate(struct serdev_device *sdev
 	return 0;
 }
 static inline void serdev_device_set_flow_control(struct serdev_device *sdev, bool enable) {}
+static inline int serdev_device_write_buf(struct serdev_device *serdev,
+					  const unsigned char *buf,
+					  size_t count)
+{
+	return -ENODEV;
+}
 static inline void serdev_device_wait_until_sent(struct serdev_device *sdev, long timeout) {}
 static inline int serdev_device_get_tiocm(struct serdev_device *serdev)
 {
@@ -311,12 +318,5 @@ static inline struct device *serdev_tty_port_register(struct tty_port *port,
 }
 static inline void serdev_tty_port_unregister(struct tty_port *port) {}
 #endif /* CONFIG_SERIAL_DEV_CTRL_TTYPORT */
-
-static inline int serdev_device_write_buf(struct serdev_device *serdev,
-					  const unsigned char *data,
-					  size_t count)
-{
-	return serdev_device_write(serdev, data, count, 0);
-}
 
 #endif /*_LINUX_SERDEV_H */
