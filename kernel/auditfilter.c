b7a84deaf8d1b0e62b437a290a40d6380975f126
Nicholas Mc Guire <der.herr@hofr.at>
2017-05-02 10:16:03 -0400

    audit: remove unnecessary semicolon in audit_field_valid()
    The excess ; after the closing parenthesis is just code-noise it has no
    and can be removed.
    
    Signed-off-by: Nicholas Mc Guire <der.herr@hofr.at>
    [PM: tweak subject line]
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- kernel/auditfilter.c
+++ kernel/auditfilter.c
@@ -338,7 +338,7 @@ static int audit_field_valid(struct audit_entry *entry, struct audit_field *f)
 		    entry->rule.listnr != AUDIT_FILTER_USER)
 			return -EINVAL;
 		break;
-	};
+	}
 
 	switch(f->type) {
 	default:
@@ -412,7 +412,7 @@ static int audit_field_valid(struct audit_entry *entry, struct audit_field *f)
 		if (entry->rule.listnr != AUDIT_FILTER_EXIT)
 			return -EINVAL;
 		break;
-	};
+	}
 	return 0;
 }
 
