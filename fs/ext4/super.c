00473374b72a6e5aba01af083f0d5100f3151210
Jan Kara <jack@suse.cz>
2017-05-04 10:58:03 -0400

    ext4: mark superblock writes synchronous for nobarrier mounts
    Commit b685d3d65ac7 "block: treat REQ_FUA and REQ_PREFLUSH as
    synchronous" removed REQ_SYNC flag from WRITE_FUA implementation.
    generic_make_request_checks() however strips REQ_FUA flag from a bio
    when the storage doesn't report volatile write cache and thus write
    effectively becomes asynchronous which can lead to performance
    regressions. This affects superblock writes for ext4. Fix the problem
    by marking superblock writes always as synchronous.
    
    Fixes: b685d3d65ac791406e0dfd8779cc9b3707fea5a3
    CC: linux-ext4@vger.kernel.org
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -4637,7 +4637,7 @@ static int ext4_commit_super(struct super_block *sb, int sync)
 	if (sync) {
 		unlock_buffer(sbh);
 		error = __sync_dirty_buffer(sbh,
-			test_opt(sb, BARRIER) ? REQ_FUA : REQ_SYNC);
+			REQ_SYNC | (test_opt(sb, BARRIER) ? REQ_FUA : 0));
 		if (error)
 			return error;
 
