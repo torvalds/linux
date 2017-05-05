a9980e947ec97297e03d2332d6beff06f5131a98
tiger-yu99 <tigeryu99@hotmail.com>
2017-05-06 00:20:32 +0800

    tee: optee: interruptible RPC sleep
    Prior to this patch RPC sleep was uninterruptible since msleep() is
    uninterruptible. Change to use msleep_interruptible() instead.
    
    Signed-off-by: Tiger Yu <tigeryu99@hotmail.com>
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- drivers/tee/optee/rpc.c
+++ drivers/tee/optee/rpc.c
@@ -140,11 +140,8 @@ static void handle_rpc_func_cmd_wait(struct optee_msg_arg *arg)
 
 	msec_to_wait = arg->params[0].u.value.a;
 
-	/* set task's state to interruptible sleep */
-	set_current_state(TASK_INTERRUPTIBLE);
-
-	/* take a nap */
-	msleep(msec_to_wait);
+	/* Go to interruptible sleep */
+	msleep_interruptible(msec_to_wait);
 
 	arg->ret = TEEC_SUCCESS;
 	return;
