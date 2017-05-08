05c5d0041972d3f3c6d881e94aa351f89a62eb2a
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2017-05-08 17:14:19 +0300

    stmmac: pci: set default number of rx and tx queues
    The commit 26d6851fd24e
    
    	("net: stmmac: set default number of rx and tx queues in stmmac_pci")
    
    missed Intel Quark configuration. Append it here.
    
    Fixes: 26d6851fd24e ("net: stmmac: set default number of rx and tx queues in stmmac_pci")
    Cc: Joao Pinto <Joao.Pinto@synopsys.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Acked-by: Joao Pinto <jpinto@synopsys.com>
    Acked-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_pci.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_pci.c
@@ -145,6 +145,10 @@ static int quark_default_data(struct plat_stmmacenet_data *plat,
 	/* Set the maxmtu to a default of JUMBO_LEN */
 	plat->maxmtu = JUMBO_LEN;
 
+	/* Set default number of RX and TX queues to use */
+	plat->tx_queues_to_use = 1;
+	plat->rx_queues_to_use = 1;
+
 	return 0;
 }
 
