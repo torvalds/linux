ed74f8a8a67952320277a53a0c46a0cdf44fbe8f
Icenowy Zheng <icenowy@aosc.io>
2017-05-15 00:30:33 +0800

    dt-bindings: add binding for the Allwinner DE2 CCU
    Allwinner "Display Engine 2.0" contains some clock controls in it.
    
    In order to add them as clock drivers, we need a device tree binding.
    Add the binding here.
    
    Also add the device tree binding headers.
    
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>
    Acked-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

---
+++ include/dt-bindings/clock/sun8i-de2.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2016 Icenowy Zheng <icenowy@aosc.io>
+ *
+ * SPDX-License-Identifier: (GPL-2.0+ OR MIT)
+ */
+
+#ifndef _DT_BINDINGS_CLOCK_SUN8I_DE2_H_
+#define _DT_BINDINGS_CLOCK_SUN8I_DE2_H_
+
+#define CLK_BUS_MIXER0		0
+#define CLK_BUS_MIXER1		1
+#define CLK_BUS_WB		2
+
+#define CLK_MIXER0		6
+#define CLK_MIXER1		7
+#define CLK_WB			8
+
+#endif /* _DT_BINDINGS_CLOCK_SUN8I_DE2_H_ */
