fa776a3f2b1b62970179bcefc55fae85e5700746
Andrew Jeffery <andrew@aj.id.au>
2017-05-16 15:57:47 +0800

    irqchip/aspeed-vic: Add AST2500 compatible string
    In addition to introducing the new compatible string the bindings
    description is reworked to be more generic.
    
    Acked-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- drivers/irqchip/irq-aspeed-vic.c
+++ drivers/irqchip/irq-aspeed-vic.c
@@ -227,4 +227,5 @@ static int __init avic_of_init(struct device_node *node,
 	return 0;
 }
 
-IRQCHIP_DECLARE(aspeed_new_vic, "aspeed,ast2400-vic", avic_of_init);
+IRQCHIP_DECLARE(ast2400_vic, "aspeed,ast2400-vic", avic_of_init);
+IRQCHIP_DECLARE(ast2500_vic, "aspeed,ast2500-vic", avic_of_init);
