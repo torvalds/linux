8b674308a26a774896652dafe0dac7ee297eed9b
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2017-05-17 08:48:17 +0900

    ALSA: pcm: add const qualifier for read-only table for sampling rate
    There's a read-only table for each sampling rate, while it doesn't have
    const qualifier and can be modified.
    
    This commit add the qualifier. As a result, a symbol for the table
    moves from .data section to .rodata.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/pcm_native.c
+++ sound/core/pcm_native.c
@@ -1986,8 +1986,10 @@ static int snd_pcm_hw_rule_sample_bits(struct snd_pcm_hw_params *params,
 #error "Change this table"
 #endif
 
-static unsigned int rates[] = { 5512, 8000, 11025, 16000, 22050, 32000, 44100,
-                                 48000, 64000, 88200, 96000, 176400, 192000 };
+static const unsigned int rates[] = {
+	5512, 8000, 11025, 16000, 22050, 32000, 44100,
+	48000, 64000, 88200, 96000, 176400, 192000
+};
 
 const struct snd_pcm_hw_constraint_list snd_pcm_known_rates = {
 	.count = ARRAY_SIZE(rates),
