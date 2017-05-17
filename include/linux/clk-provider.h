22833a9165a1c72a54ddc696a3765bd6f87fbb92
Maxime Ripard <maxime.ripard@free-electrons.com>
2017-05-17 09:40:30 +0200

    clk: divider: Make divider_round_rate take the parent clock
    So far, divider_round_rate only considers the parent clock returned by
    clk_hw_get_parent.
    
    This works fine on clocks that have a single parents, this doesn't work on
    muxes, since we will only consider the first parent, while other parents
    may totally be able to provide a better combination.
    
    Clocks in that case cannot use divider_round_rate, so would have to come up
    with a very similar logic to work around it. Instead of having to do
    something like this, and duplicate that logic everywhere, create a
    divider_round_rate parent to allow caller to give an additional parameter
    for the parent clock to consider.
    
    Reviewed-by: Chen-Yu Tsai <wens@csie.org>
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>
    Acked-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: Chen-Yu Tsai <wens@csie.org>

--- include/linux/clk-provider.h
+++ include/linux/clk-provider.h
@@ -412,9 +412,10 @@ extern const struct clk_ops clk_divider_ro_ops;
 unsigned long divider_recalc_rate(struct clk_hw *hw, unsigned long parent_rate,
 		unsigned int val, const struct clk_div_table *table,
 		unsigned long flags);
-long divider_round_rate(struct clk_hw *hw, unsigned long rate,
-		unsigned long *prate, const struct clk_div_table *table,
-		u8 width, unsigned long flags);
+long divider_round_rate_parent(struct clk_hw *hw, struct clk_hw *parent,
+			       unsigned long rate, unsigned long *prate,
+			       const struct clk_div_table *table,
+			       u8 width, unsigned long flags);
 int divider_get_val(unsigned long rate, unsigned long parent_rate,
 		const struct clk_div_table *table, u8 width,
 		unsigned long flags);
@@ -755,6 +756,15 @@ static inline void __clk_hw_set_clk(struct clk_hw *dst, struct clk_hw *src)
 {
 	dst->clk = src->clk;
 	dst->core = src->core;
+}
+
+static inline long divider_round_rate(struct clk_hw *hw, unsigned long rate,
+				      unsigned long *prate,
+				      const struct clk_div_table *table,
+				      u8 width, unsigned long flags)
+{
+	return divider_round_rate_parent(hw, clk_hw_get_parent(hw),
+					 rate, prate, table, width, flags);
 }
 
 /*
