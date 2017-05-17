99caf177f6fd3e67575f6ce05b36e8e041bcef60
shameer <shameerali.kolothum.thodi@huawei.com>
2017-05-17 10:12:05 +0100

    iommu/arm-smmu-v3: Enable ACPI based HiSilicon CMD_PREFETCH quirk(erratum 161010701)
    HiSilicon SMMUv3 on Hip06/Hip07 platforms doesn't support CMD_PREFETCH
    command. The dt based support for this quirk is already present in the
    driver(hisilicon,broken-prefetch-cmd). This adds ACPI support for the
    quirk using the IORT smmu model number.
    
    Signed-off-by: shameer <shameerali.kolothum.thodi@huawei.com>
    Signed-off-by: hanjun <guohanjun@huawei.com>
    [will: rewrote patch]
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- drivers/iommu/arm-smmu-v3.c
+++ drivers/iommu/arm-smmu-v3.c
@@ -414,6 +414,10 @@
 #define MSI_IOVA_LENGTH			0x100000
 
 /* Until ACPICA headers cover IORT rev. C */
+#ifndef ACPI_IORT_SMMU_HISILICON_HI161X
+#define ACPI_IORT_SMMU_HISILICON_HI161X		0x1
+#endif
+
 #ifndef ACPI_IORT_SMMU_V3_CAVIUM_CN99XX
 #define ACPI_IORT_SMMU_V3_CAVIUM_CN99XX		0x2
 #endif
@@ -2616,8 +2620,14 @@ static int arm_smmu_device_hw_probe(struct arm_smmu_device *smmu)
 #ifdef CONFIG_ACPI
 static void acpi_smmu_get_options(u32 model, struct arm_smmu_device *smmu)
 {
-	if (model == ACPI_IORT_SMMU_V3_CAVIUM_CN99XX)
+	switch (model) {
+	case ACPI_IORT_SMMU_V3_CAVIUM_CN99XX:
 		smmu->options |= ARM_SMMU_OPT_PAGE0_REGS_ONLY;
+		break;
+	case ACPI_IORT_SMMU_HISILICON_HI161X:
+		smmu->options |= ARM_SMMU_OPT_SKIP_PREFETCH;
+		break;
+	}
 
 	dev_notice(smmu->dev, "option mask 0x%x\n", smmu->options);
 }
