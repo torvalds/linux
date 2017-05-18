4b04eee7a142e1aef3a1744b30c1b1f29a7b1014
XiaoDong Huang <derrick.huang@rock-chips.com>
2017-05-18 20:06:35 +0800

    arm64: dts: rk3368: add wakeup-config in rockchip-suspend
    Change-Id: Ibf4ba154d59e99332e68ca5451b0045e15fa850d
    Signed-off-by: XiaoDong Huang <derrick.huang@rock-chips.com>

--- include/dt-bindings/suspend/rockchip-rk3368.h
+++ include/dt-bindings/suspend/rockchip-rk3368.h
@@ -39,4 +39,18 @@
 #define RKPM_SLP_SFT_32K_EXT		BIT(17)
 #define RKPM_SLP_SFT_PD_PERI		BIT(18)
 #define RKPM_SLP_SFT_PD_NBSCUS		BIT(19) /* noboot scus in muti-cluster */
+
+/* the wake up source */
+#define RKPM_CLUSTER_L_WKUP_EN		BIT(0)
+#define RKPM_CLUSTER_B_WKUPB_EN		BIT(1)
+#define RKPM_GPIO_WKUP_EN		BIT(2)
+#define RKPM_SDIO_WKUP_EN		BIT(3)
+#define RKPM_SDMMC_WKUP_EN		BIT(4)
+#define RKPM_SIM_WKUP_EN		BIT(5)
+#define RKPM_TIMER_WKUP_EN		BIT(6)
+#define RKPM_USB_WKUP_EN		BIT(7)
+#define RKPM_SFT_WKUP_EN		BIT(8)
+#define RKPM_WDT_M0_WKUP_EN		BIT(9)
+#define RKPM_TIME_OUT_WKUP_EN		BIT(10)
+
 #endif
