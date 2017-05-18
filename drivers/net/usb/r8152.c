db646cc0aefbc5da0b9699ed40d4402e01245f57
Matthias Kaehlcke <mka@chromium.org>
2017-05-18 10:45:33 -0700

    r8152: Remove unused function usb_ocp_read()
    The function is not used, removing it fixes the following warning when
    building with clang:
    
    drivers/net/usb/r8152.c:825:5: error: unused function 'usb_ocp_read'
        [-Werror,-Wunused-function]
    
    Signed-off-by: Matthias Kaehlcke <mka@chromium.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/r8152.c
+++ drivers/net/usb/r8152.c
@@ -841,12 +841,6 @@ int pla_ocp_write(struct r8152 *tp, u16 index, u16 byteen, u16 size, void *data)
 }
 
 static inline
-int usb_ocp_read(struct r8152 *tp, u16 index, u16 size, void *data)
-{
-	return generic_ocp_read(tp, index, size, data, MCU_TYPE_USB);
-}
-
-static inline
 int usb_ocp_write(struct r8152 *tp, u16 index, u16 byteen, u16 size, void *data)
 {
 	return generic_ocp_write(tp, index, byteen, size, data, MCU_TYPE_USB);
