dcab5fb7a5b0472e035da7dfe21168802c1b9d3b
Arnd Bergmann <arnd@arndb.de>
2017-05-18 15:35:55 +0200

    ALSA: pcm/oss: mark snd_pcm_plug_slave_format arg as const
    Only one of the two declarations has the const modifier in the
    argument list, so we get a warning when CONFIG_SND_PCM_OSS_PLUGINS
    is disabled:
    
    sound/core/oss/pcm_oss.c: In function 'snd_pcm_oss_change_params':
    sound/core/oss/pcm_oss.c:898:47: error: passing argument 2 of 'snd_pcm_plug_slave_format' discards 'const' qualifier from pointer target type [-Werror=discarded-qualifiers]
    
    This makes the two declarations match again.
    
    Fixes: e76bf3c4b4e4 ("ALSA: pcm/oss: refer to parameters instead of copying to reduce usage of kernel stack")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/oss/pcm_plugin.h
+++ sound/core/oss/pcm_plugin.h
@@ -172,7 +172,7 @@ snd_pcm_sframes_t snd_pcm_oss_readv3(struct snd_pcm_substream *substream,
 
 static inline snd_pcm_sframes_t snd_pcm_plug_client_size(struct snd_pcm_substream *handle, snd_pcm_uframes_t drv_size) { return drv_size; }
 static inline snd_pcm_sframes_t snd_pcm_plug_slave_size(struct snd_pcm_substream *handle, snd_pcm_uframes_t clt_size) { return clt_size; }
-static inline int snd_pcm_plug_slave_format(int format, struct snd_mask *format_mask) { return format; }
+static inline int snd_pcm_plug_slave_format(int format, const struct snd_mask *format_mask) { return format; }
 
 #endif
 
