48ae0fb49b2e8380e60c5072f18005c8a98c6520
Jim Mattson <jmattson@google.com>
2017-05-22 09:48:33 -0700

    kvm: vmx: Properly handle machine check during VM-entry
    vmx_complete_atomic_exit should call kvm_machine_check for any
    VM-entry failure due to a machine-check event. Such an exit should be
    recognized solely by its basic exit reason (i.e. the low 16 bits of
    the VMCS exit reason field). None of the other VMCS exit information
    fields contain valid information when the VM-exit is due to "VM-entry
    failure due to machine-check event".
    
    Signed-off-by: Jim Mattson <jmattson@google.com>
    Reviewed-by: Xiao Guangrong <xiaoguangrong@tencent.com>
    [Changed VM_EXIT_INTR_INFO condition to better describe its reason.]
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -8630,17 +8630,20 @@ static void vmx_apicv_post_state_restore(struct kvm_vcpu *vcpu)
 
 static void vmx_complete_atomic_exit(struct vcpu_vmx *vmx)
 {
-	u32 exit_intr_info;
+	u32 exit_intr_info = 0;
+	u16 basic_exit_reason = (u16)vmx->exit_reason;
 
-	if (!(vmx->exit_reason == EXIT_REASON_MCE_DURING_VMENTRY
-	      || vmx->exit_reason == EXIT_REASON_EXCEPTION_NMI))
+	if (!(basic_exit_reason == EXIT_REASON_MCE_DURING_VMENTRY
+	      || basic_exit_reason == EXIT_REASON_EXCEPTION_NMI))
 		return;
 
-	vmx->exit_intr_info = vmcs_read32(VM_EXIT_INTR_INFO);
-	exit_intr_info = vmx->exit_intr_info;
+	if (!(vmx->exit_reason & VMX_EXIT_REASONS_FAILED_VMENTRY))
+		exit_intr_info = vmcs_read32(VM_EXIT_INTR_INFO);
+	vmx->exit_intr_info = exit_intr_info;
 
 	/* Handle machine checks before interrupts are enabled */
-	if (is_machine_check(exit_intr_info))
+	if (basic_exit_reason == EXIT_REASON_MCE_DURING_VMENTRY ||
+	    is_machine_check(exit_intr_info))
 		kvm_machine_check();
 
 	/* We need to handle NMIs before interrupts are enabled */
