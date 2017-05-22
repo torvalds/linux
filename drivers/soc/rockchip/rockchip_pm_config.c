916efae9c40c1d7ce3e5e9609053d6cc3904c5d9
XiaoDong Huang <derrick.huang@rock-chips.com>
2017-05-22 19:24:42 +0800

    soc: rockchip: support rk322x pm config
    Change-Id: I29c5685f09a846b62196ab8614ebe168bfed75ef
    Signed-off-by: XiaoDong Huang <derrick.huang@rock-chips.com>

--- drivers/soc/rockchip/rockchip_pm_config.c
+++ drivers/soc/rockchip/rockchip_pm_config.c
@@ -21,6 +21,7 @@
 #define PM_INVALID_GPIO	0xffff
 
 static const struct of_device_id pm_match_table[] = {
+	{ .compatible = "rockchip,pm-rk322x",},
 	{ .compatible = "rockchip,pm-rk3288",},
 	{ .compatible = "rockchip,pm-rk3368",},
 	{ .compatible = "rockchip,pm-rk3399",},
