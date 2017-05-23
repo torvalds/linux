79e68821582d08e369094e7ea45dbfb8fdb37d7e
Adrian Chadd <adrian@freebsd.org>
2017-05-23 18:49:35 +0300

    ath10k: go back to using dma_alloc_coherent() for firmware scratch memory
    This reverts commit b057886524be ("ath10k: do not use coherent memory for
    allocated device memory chunks") in 2015 which converted this allocation from
    dma_map_coherent() to kzalloc() / dma_map_single().
    
    The current problem manifests when using later model NICs with larger
    (>700KiB) scratch spaces in memory.  Although the kzalloc call
    succeeds, the software IOMMU TLB code (via dma_map_single()) panics
    because it can't find 700KiB of linear physmem bounce buffers for DMA.
    Now, this is a bit of a silly failure mode for the dma map API,
    but it's what we currently have to play with.
    
    In these cases, doing kzalloc() works fine, but the dma_map_single()
    call fails.
    
    After chatting with Linus briefly about this, it indeed should be
    using dma_alloc_coherent() for doing larger device memory allocation
    that requires some kind of physical address mapping.
    
    You're not supposed to be using kzalloc and dma_map_* calls for
    large memory regions, and I'm guessing not for long-held mappings
    either.  Typically dma mappings should be temporary for DMA,
    not long held like these.
    
    Now, since hopefully the major annoying underlying problem has also been
    addressed (ie, ath10k is no longer tears down all of these allocations
    and reallocates them every time the vdevs are brought down) fragmentation
    should stop being such a touchy issue.  If it is though, using
    dma_alloc_coherent() use gets us access to the CMB APIs too relatively
    easily and ideally we would be allocating memory early in boot for
    exactly these reasons.
    
    Signed-off-by: Adrian Chadd <adrian@FreeBSD.org>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath10k/wmi.c
+++ drivers/net/wireless/ath/ath10k/wmi.c
@@ -4482,31 +4482,17 @@ static int ath10k_wmi_alloc_chunk(struct ath10k *ar, u32 req_id,
 				  u32 num_units, u32 unit_len)
 {
 	dma_addr_t paddr;
-	u32 pool_size = 0;
+	u32 pool_size;
 	int idx = ar->wmi.num_mem_chunks;
-	void *vaddr = NULL;
+	void *vaddr;
 
-	if (ar->wmi.num_mem_chunks == ARRAY_SIZE(ar->wmi.mem_chunks))
+	pool_size = num_units * round_up(unit_len, 4);
+	vaddr = dma_alloc_coherent(ar->dev, pool_size, &paddr, GFP_KERNEL);
+
+	if (!vaddr)
 		return -ENOMEM;
 
-	while (!vaddr && num_units) {
-		pool_size = num_units * round_up(unit_len, 4);
-		if (!pool_size)
-			return -EINVAL;
-
-		vaddr = kzalloc(pool_size, GFP_KERNEL | __GFP_NOWARN);
-		if (!vaddr)
-			num_units /= 2;
-	}
-
-	if (!num_units)
-		return -ENOMEM;
-
-	paddr = dma_map_single(ar->dev, vaddr, pool_size, DMA_BIDIRECTIONAL);
-	if (dma_mapping_error(ar->dev, paddr)) {
-		kfree(vaddr);
-		return -ENOMEM;
-	}
+	memset(vaddr, 0, pool_size);
 
 	ar->wmi.mem_chunks[idx].vaddr = vaddr;
 	ar->wmi.mem_chunks[idx].paddr = paddr;
@@ -8281,11 +8267,10 @@ void ath10k_wmi_free_host_mem(struct ath10k *ar)
 
 	/* free the host memory chunks requested by firmware */
 	for (i = 0; i < ar->wmi.num_mem_chunks; i++) {
-		dma_unmap_single(ar->dev,
-				 ar->wmi.mem_chunks[i].paddr,
-				 ar->wmi.mem_chunks[i].len,
-				 DMA_BIDIRECTIONAL);
-		kfree(ar->wmi.mem_chunks[i].vaddr);
+		dma_free_coherent(ar->dev,
+				  ar->wmi.mem_chunks[i].len,
+				  ar->wmi.mem_chunks[i].vaddr,
+				  ar->wmi.mem_chunks[i].paddr);
 	}
 
 	ar->wmi.num_mem_chunks = 0;
