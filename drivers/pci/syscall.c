cf53ecd8d65c058aa0584f505a927fd28fe5933d
Matthew Garrett <matthew.garrett@nebula.com>
2017-05-24 14:56:03 +0100

    UBUNTU: SAUCE: (efi-lockdown) PCI: Lock down BAR access when the kernel is locked down
    Any hardware that can potentially generate DMA has to be locked down in
    order to avoid it being possible for an attacker to modify kernel code,
    allowing them to circumvent disabled module loading or module signing.
    Default to paranoid - in future we can potentially relax this for
    sufficiently IOMMU-isolated devices.
    
    Signed-off-by: Matthew Garrett <matthew.garrett@nebula.com>
    Signed-off-by: David Howells <dhowells@redhat.com>
    Acked-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: "Lee, Chun-Yi" <jlee@suse.com>
    cc: linux-pci@vger.kernel.org
    (cherry picked from commit 6999b2411874e2703d2e1bbec9ea42209699a984
     git://git.kernel.org/pub/scm/linux/kernel/git/jwboyer/fedora.git)
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- drivers/pci/syscall.c
+++ drivers/pci/syscall.c
@@ -93,7 +93,8 @@ SYSCALL_DEFINE5(pciconfig_write, unsigned long, bus, unsigned long, dfn,
 	u32 dword;
 	int err = 0;
 
-	if (!capable(CAP_SYS_ADMIN))
+	if (!capable(CAP_SYS_ADMIN) ||
+	    kernel_is_locked_down("Direct PCI access"))
 		return -EPERM;
 
 	dev = pci_get_bus_and_slot(bus, dfn);
