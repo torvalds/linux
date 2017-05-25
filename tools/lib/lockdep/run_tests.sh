a9b94d283caa55e9d292a29b431940482a433cb3
Vishal Thanki <vishalthanki@gmail.com>
2017-05-25 12:58:41 +0000

    tools/lib/lockdep: Remove -lpthread compiler option
    With -lpthread option, the test for ABBA_2threads was
    failing, and test passed if it was removed.
    Since -pthread compiler option is sufficient for linking
    to pthread libraries, this patch removes -lpthread.
    
    Signed-off-by: Vishal Thanki <vishalthanki@gmail.com>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: a.p.zijlstra@chello.nl
    Cc: ben@decadent.org.uk
    Link: http://lkml.kernel.org/r/20170525130005.5947-9-alexander.levin@verizon.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- tools/lib/lockdep/run_tests.sh
+++ tools/lib/lockdep/run_tests.sh
@@ -4,7 +4,7 @@ make &> /dev/null
 
 for i in `ls tests/*.c`; do
 	testname=$(basename "$i" .c)
-	gcc -o tests/$testname -pthread -lpthread $i liblockdep.a -Iinclude -D__USE_LIBLOCKDEP &> /dev/null
+	gcc -o tests/$testname -pthread $i liblockdep.a -Iinclude -D__USE_LIBLOCKDEP &> /dev/null
 	echo -ne "$testname... "
 	if [ $(timeout 1 ./tests/$testname | wc -l) -gt 0 ]; then
 		echo "PASSED!"
@@ -18,7 +18,7 @@ done
 
 for i in `ls tests/*.c`; do
 	testname=$(basename "$i" .c)
-	gcc -o tests/$testname -pthread -lpthread -Iinclude $i &> /dev/null
+	gcc -o tests/$testname -pthread -Iinclude $i &> /dev/null
 	echo -ne "(PRELOAD) $testname... "
 	if [ $(timeout 1 ./lockdep ./tests/$testname | wc -l) -gt 0 ]; then
 		echo "PASSED!"
