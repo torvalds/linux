0e5e7f5e9700661c3ddd95501743fb52fec1ab07
Michael Ellerman <mpe@ellerman.id.au>
2017-05-25 16:33:52 +1000

    powerpc/64: Reclaim CPU_FTR_SUBCORE
    We are running low on CPU feature bits, so we only want to use them when
    it's really necessary.
    
    CPU_FTR_SUBCORE is only used in one place, and only in C, so we don't
    need it in order to make asm patching work. It can only be set on
    "Power8" CPUs, which in practice means POWER8, POWER8E and POWER8NVL.
    There are no plans to implement it on future CPUs, but if there ever
    were we could retrofit it then.
    
    Although KVM uses subcores, it never looks at the CPU feature, it either
    looks at the ISA level or the threads_per_subcore value.
    
    So drop the CPU feature and do a PVR check instead. Drop the device tree
    "subcore" feature as we no longer support doing anything with it, and we
    will drop it from skiboot too.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/kernel/dt_cpu_ftrs.c
+++ arch/powerpc/kernel/dt_cpu_ftrs.c
@@ -643,7 +643,6 @@ static struct dt_cpu_feature_match __initdata
 	{"processor-control-facility", feat_enable_dbell, CPU_FTR_DBELL},
 	{"processor-control-facility-v3", feat_enable_dbell, CPU_FTR_DBELL},
 	{"processor-utilization-of-resources-register", feat_enable_purr, 0},
-	{"subcore", feat_enable, CPU_FTR_SUBCORE},
 	{"no-execute", feat_enable, 0},
 	{"strong-access-ordering", feat_enable, CPU_FTR_SAO},
 	{"cache-inhibited-large-page", feat_enable_large_ci, 0},
