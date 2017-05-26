2ce9e4efbf4289ce48144ec4986f58033890fb6d
John Allen <jallen@linux.vnet.ibm.com>
2017-05-26 10:30:25 -0400

    ibmvnic: Send gratuitous arp on reset
    Send gratuitous arp after any reset.
    
    Signed-off-by: John Allen <jallen@linux.vnet.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/ibm/ibmvnic.c
+++ drivers/net/ethernet/ibm/ibmvnic.c
@@ -1273,6 +1273,7 @@ static int do_reset(struct ibmvnic_adapter *adapter,
 	for (i = 0; i < adapter->req_rx_queues; i++)
 		napi_schedule(&adapter->napi[i]);
 
+	netdev_notify_peers(netdev);
 	return 0;
 }
 
