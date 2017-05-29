ce68349a356d60d942ce5758d835e9cb00dcf61d
Stefan Wahren <stefan.wahren@i2se.com>
2017-05-29 13:57:09 +0200

    net: qualcomm: qca_7k: clean up header includes
    Currently the includes doesn't reflect the dependencies. So
    fix this up by removing all unnecessary entries and add the
    necessary ones explicit.
    
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/qualcomm/qca_7k.c
+++ drivers/net/ethernet/qualcomm/qca_7k.c
@@ -23,11 +23,9 @@
  *   kernel-based SPI device.
  */
 
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/moduleparam.h>
+#include <linux/kernel.h>
+#include <linux/netdevice.h>
 #include <linux/spi/spi.h>
-#include <linux/version.h>
 
 #include "qca_7k.h"
 
