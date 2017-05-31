255ba9587faeef09ccbb8fbf794476deb2dc20d3
Thomas Gleixner <tglx@linutronix.de>
2017-05-31 11:58:32 +0200

    UPSTREAM: genirq: Handle NOAUTOEN interrupt setup proper
    If an interrupt is marked NOAUTOEN then request_irq() installs the action,
    but does not enable the interrupt via startup_irq().  The interrupt is
    enabled via enable_irq() later from the driver. enable_irq() calls
    irq_enable().
    
    That means that for interrupts which have a irq_startup() callback this
    callback is never invoked. Neither is irq_domain_activate_irq() invoked for
    such interrupts.
    
    If an interrupt depends on irq_startup() or irq_domain_activate_irq() then
    the enable via irq_enable() is not enough.
    
    Add a status flag IRQD_IRQ_STARTED_UP and use this to select the proper
    mechanism in enable_irq(). Use the flag also to avoid pointless calls into
    the low level functions.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Marc Zyngier <marc.zyngier@arm.com>
    Cc: dianders@chromium.org
    Cc: jeffy <jeffy.chen@rock-chips.com>
    Cc: Brian Norris <briannorris@chromium.org>
    Cc: tfiga@chromium.org
    Link: http://lkml.kernel.org/r/20170531100212.130986205@linutronix.de
    
    (cherry picked from commit 201d7f47f34bd7cb19161d0426f13b141e381f30)
    
    Conflicts:
    	include/linux/irq.h
    	[due to missing upstream patches:
    	  08d85f3 irqdomain: Avoid activating interrupts more than once
    	  1a3d28a UPSTREAM: genirq: Introduce IRQD_AFFINITY_MANAGED flag
    	  6297714 UPSTREAM: irq: Privatize irq_common_data::state_use_accessors]
    
    Change-Id: Ie8d492c694171fa81e3df61e8561bec160ad37bb
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -206,6 +206,7 @@ enum {
 	IRQD_IRQ_INPROGRESS		= (1 << 18),
 	IRQD_WAKEUP_ARMED		= (1 << 19),
 	IRQD_FORWARDED_TO_VCPU		= (1 << 20),
+	IRQD_IRQ_STARTED		= (1 << 22),
 };
 
 #define __irqd_to_state(d)		((d)->common->state_use_accessors)
@@ -297,6 +298,11 @@ static inline void irqd_set_forwarded_to_vcpu(struct irq_data *d)
 static inline void irqd_clr_forwarded_to_vcpu(struct irq_data *d)
 {
 	__irqd_to_state(d) &= ~IRQD_FORWARDED_TO_VCPU;
+}
+
+static inline bool irqd_is_started(struct irq_data *d)
+{
+	return __irqd_to_state(d) & IRQD_IRQ_STARTED;
 }
 
 static inline irq_hw_number_t irqd_to_hwirq(struct irq_data *d)
