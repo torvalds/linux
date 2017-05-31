32bb6cbb3b4ea5ca24e3fa13e11772c192616e04
Bartosz Golaszewski <brgl@bgdev.pl>
2017-05-31 18:06:57 +0200

    irq/generic-chip: Provide irq_destroy_generic_chip()
    Most users of irq_alloc_generic_chip() call irq_setup_generic_chip()
    too. To simplify the cleanup provide a function that both removes a
    generic chip and frees its memory.
    
    Signed-off-by: Bartosz Golaszewski <brgl@bgdev.pl>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Marc Zyngier <marc.zyngier@arm.com>
    Cc: linux-doc@vger.kernel.org
    Cc: Jonathan Corbet <corbet@lwn.net>
    Link: http://lkml.kernel.org/r/1496246820-13250-3-git-send-email-brgl@bgdev.pl

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -979,6 +979,14 @@ static inline void irq_free_generic_chip(struct irq_chip_generic *gc)
 	kfree(gc);
 }
 
+static inline void irq_destroy_generic_chip(struct irq_chip_generic *gc,
+					    u32 msk, unsigned int clr,
+					    unsigned int set)
+{
+	irq_remove_generic_chip(gc, msk, clr, set);
+	irq_free_generic_chip(gc);
+}
+
 static inline struct irq_chip_type *irq_data_get_chip_type(struct irq_data *d)
 {
 	return container_of(d->chip, struct irq_chip_type, chip);
