41124db869b7e00e12052555f8987867ac01d70c
Luis R. Rodriguez <mcgrof@kernel.org>
2017-06-01 11:08:01 -0700

    fs: warn in case userspace lied about modprobe return
    kmod <= v19 was broken -- it could return 0 to modprobe calls,
    incorrectly assuming that a kernel module was built-in, whereas in
    reality the module was just forming in the kernel. The reason for this
    is an incorrect userspace heuristics. A userspace kmod fix is available
    for it [0], however should userspace break again we could go on with
    an failed get_fs_type() which is hard to debug as the request_module()
    is detected as returning 0. The first suspect would be that there is
    something worth with the kernel's module loader and obviously in this
    case that is not the issue.
    
    Since these issues are painful to debug complain when we know userspace
    has outright lied to us.
    
    [0] http://git.kernel.org/cgit/utils/kernel/kmod/kmod.git/commit/libkmod/libkmod-module.c?id=fd44a98ae2eb5eb32161088954ab21e58e19dfc4
    
    Suggested-by: Rusty Russell <rusty@rustcorp.com.au>
    Cc: Jessica Yu <jeyu@redhat.com>
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/filesystems.c
+++ fs/filesystems.c
@@ -275,8 +275,10 @@ struct file_system_type *get_fs_type(const char *name)
 	int len = dot ? dot - name : strlen(name);
 
 	fs = __get_fs_type(name, len);
-	if (!fs && (request_module("fs-%.*s", len, name) == 0))
+	if (!fs && (request_module("fs-%.*s", len, name) == 0)) {
 		fs = __get_fs_type(name, len);
+		WARN_ONCE(!fs, "request_module fs-%.*s succeeded, but still no fs?\n", len, name);
+	}
 
 	if (dot && fs && !(fs->fs_flags & FS_HAS_SUBTYPE)) {
 		put_filesystem(fs);
