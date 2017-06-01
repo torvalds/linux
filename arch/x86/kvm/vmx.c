d281e13b0bfe745a21061a194e386a949784393f
Jim Mattson <jmattson@google.com>
2017-06-01 12:44:46 -0700

    KVM: nVMX: Update vmcs12->guest_linear_address on nested VM-exit
    The guest-linear address field is set for VM exits due to attempts to
    execute LMSW with a memory operand and VM exits due to attempts to
    execute INS or OUTS for which the relevant segment is usable,
    regardless of whether or not EPT is in use.
    
    Fixes: 119a9c01a5922 ("KVM: nVMX: pass valid guest linear-address to the L1")
    Signed-off-by: Jim Mattson <jmattson@google.com>
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -10712,8 +10712,7 @@ static void sync_vmcs12(struct kvm_vcpu *vcpu, struct vmcs12 *vmcs12)
 		vmcs12->guest_pdptr3 = vmcs_read64(GUEST_PDPTR3);
 	}
 
-	if (nested_cpu_has_ept(vmcs12))
-		vmcs12->guest_linear_address = vmcs_readl(GUEST_LINEAR_ADDRESS);
+	vmcs12->guest_linear_address = vmcs_readl(GUEST_LINEAR_ADDRESS);
 
 	if (nested_cpu_has_vid(vmcs12))
 		vmcs12->guest_intr_status = vmcs_read16(GUEST_INTR_STATUS);
