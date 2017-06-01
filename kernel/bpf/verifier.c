fb9a307d11d62749d75b404f15517d73f5d6e148
Chenbo Feng <fengc@google.com>
2017-05-31 18:15:59 -0700

    bpf: Allow CGROUP_SKB eBPF program to access sk_buff
    This allows cgroup eBPF program to classify packet based on their
    protocol or other detail information. Currently program need
    CAP_NET_ADMIN privilege to attach a cgroup eBPF program, and A
    process with CAP_NET_ADMIN can already see all packets on the system,
    for example, by creating an iptables rules that causes the packet to
    be passed to userspace via NFLOG.
    
    Signed-off-by: Chenbo Feng <fengc@google.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -2426,6 +2426,7 @@ static bool may_access_skb(enum bpf_prog_type type)
 	case BPF_PROG_TYPE_SOCKET_FILTER:
 	case BPF_PROG_TYPE_SCHED_CLS:
 	case BPF_PROG_TYPE_SCHED_ACT:
+	case BPF_PROG_TYPE_CGROUP_SKB:
 		return true;
 	default:
 		return false;
