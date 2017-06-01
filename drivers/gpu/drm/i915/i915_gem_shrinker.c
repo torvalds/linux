1d24ad457c54f0c8a835c33b7f687c454b643fcb
Chris Wilson <chris@chris-wilson.co.uk>
2017-06-01 14:33:29 +0100

    drm/i915: Allow kswapd to pause the device whilst reaping
    In commit 5763ff04dc4e ("drm/i915: Avoid GPU stalls from kswapd") we
    stopped direct reclaim and kswapd from triggering GPU/client stalls
    whilst running (by restricting the objects they could reap to be idle).
    
    However with abusive GPU usage, it becomes quite easy to starve kswapd
    of memory and prevent it from making forward progress towards obtaining
    enough free memory (thus driving the system closer to swap exhaustion).
    Relax the previous restriction to allow kswapd (but not direct reclaim)
    to stall the device whilst reaping purgeable pages.
    
    v2: Also acquire the rpm wakelock to allow kswapd to unbind buffers.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170601133331.5973-1-chris@chris-wilson.co.uk
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@intel.com>

--- drivers/gpu/drm/i915/i915_gem_shrinker.c
+++ drivers/gpu/drm/i915/i915_gem_shrinker.c
@@ -332,6 +332,15 @@ i915_gem_shrinker_scan(struct shrinker *shrinker, struct shrink_control *sc)
 					 sc->nr_to_scan - freed,
 					 I915_SHRINK_BOUND |
 					 I915_SHRINK_UNBOUND);
+	if (freed < sc->nr_to_scan && current_is_kswapd()) {
+		intel_runtime_pm_get(dev_priv);
+		freed += i915_gem_shrink(dev_priv,
+					 sc->nr_to_scan - freed,
+					 I915_SHRINK_ACTIVE |
+					 I915_SHRINK_BOUND |
+					 I915_SHRINK_UNBOUND);
+		intel_runtime_pm_put(dev_priv);
+	}
 
 	shrinker_unlock(dev_priv, unlock);
 
