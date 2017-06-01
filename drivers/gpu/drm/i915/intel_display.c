b82a682d3234115dda6c8d05c31bafe98a66c885
Ville Syrjälä <ville.syrjala@linux.intel.com>
2017-06-01 17:36:18 +0300

    drm/i915: Drop pipe A quirk for Thinkapd T60
    The pipe A force quirk shouldn't needed except on 830. So let's nuke it
    for the IBM Thinkpad T60 945 machines. This quirk pre-dates
    KMS so it's usefulness is doubtful at best now.
    
    The original bug report [1] describes the symptoms as "system hang on
    closing T60 panel lid", and we already dropped a similar quirk for
    another 945 machine in
    commit 736a69ca8c99 ("drm/i915: Drop PIPE-A quirk for 945GSE HP Mini")
    so I'm hopeful we can drop this one as well.
    
    The quirk was added into xf86-video-intel in
    commit 08903abe4dc0 ("Add pipe a force enable quirk for Lenovo T60")
    
    [1] https://bugs.freedesktop.org/show_bug.cgi?id=16494
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170601143619.27840-7-ville.syrjala@linux.intel.com
    Acked-by: Chris Wilson <chris@chris-wilson.co.uk>
    Acked-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -14817,9 +14817,6 @@ static const struct intel_dmi_quirk intel_dmi_quirks[] = {
 };
 
 static struct intel_quirk intel_quirks[] = {
-	/* ThinkPad T60 needs pipe A force quirk (bug #16494) */
-	{ 0x2782, 0x17aa, 0x201a, quirk_pipea_force },
-
 	/* 830 needs to leave pipe A & dpll A up */
 	{ 0x3577, PCI_ANY_ID, PCI_ANY_ID, quirk_pipea_force },
 
