277036f05be242540b7bfe75f226107d04f51b06
Jan Kiszka <jan.kiszka@siemens.com>
2017-06-02 07:43:27 +0200

    platform: Accept const properties
    Aligns us with device_add_properties, the function we call.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>

--- drivers/base/platform.c
+++ drivers/base/platform.c
@@ -344,7 +344,7 @@ EXPORT_SYMBOL_GPL(platform_device_add_data);
  * platform device is released.
  */
 int platform_device_add_properties(struct platform_device *pdev,
-				   struct property_entry *properties)
+				   const struct property_entry *properties)
 {
 	return device_add_properties(&pdev->dev, properties);
 }
