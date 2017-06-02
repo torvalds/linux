c3006a9264681fd0551543f526758208792319d6
Bart Van Assche <bart.vanassche@sandisk.com>
2017-06-02 14:22:01 -0700

    scsi: snic: Remove code that zeroes driver-private command data
    Since the SCSI core zeroes driver-private command data, remove that code
    from the snic driver.
    
    Signed-off-by: Bart Van Assche <bart.vanassche@sandisk.com>
    Reviewed-by: Hannes Reinecke <hare@suse.com>
    Reviewed-by: Narsimhulu Musini <nmusini@cisco.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Cc: Sesidhar Baddela <sebaddel@cisco.com>
    Cc: Johannes Thumshirn <jthumshirn@suse.de>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/snic/snic_scsi.c
+++ drivers/scsi/snic/snic_scsi.c
@@ -359,8 +359,6 @@ snic_queuecommand(struct Scsi_Host *shost, struct scsi_cmnd *sc)
 	SNIC_SCSI_DBG(shost, "sc %p Tag %d (sc %0x) lun %lld in snic_qcmd\n",
 		      sc, snic_cmd_tag(sc), sc->cmnd[0], sc->device->lun);
 
-	memset(scsi_cmd_priv(sc), 0, sizeof(struct snic_internal_io_state));
-
 	ret = snic_issue_scsi_req(snic, tgt, sc);
 	if (ret) {
 		SNIC_HOST_ERR(shost, "Failed to Q, Scsi Req w/ err %d.\n", ret);
