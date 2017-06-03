361f7cc7f33607489bea815f9148e8e9a13ea673
Markus Elfring <elfring@users.sourceforge.net>
2017-06-03 21:43:11 +0200

    ARM: pxa: Improve a size determination in pxa3xx_u2d_probe()
    Replace the specification of a data structure by a pointer dereference
    as the parameter for the operator "sizeof" to make the corresponding size
    determination a bit safer according to the Linux coding style convention.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>

--- arch/arm/mach-pxa/pxa3xx-ulpi.c
+++ arch/arm/mach-pxa/pxa3xx-ulpi.c
@@ -286,7 +286,7 @@ static int pxa3xx_u2d_probe(struct platform_device *pdev)
 	struct resource *r;
 	int err;
 
-	u2d = kzalloc(sizeof(struct pxa3xx_u2d_ulpi), GFP_KERNEL);
+	u2d = kzalloc(sizeof(*u2d), GFP_KERNEL);
 	if (!u2d) {
 		dev_err(&pdev->dev, "failed to allocate memory\n");
 		return -ENOMEM;
