a9d8ea79d7464352820973b237f1d192afc3c14c
Erik Schmauss <erik.schmauss@intel.com>
2017-06-05 16:41:55 +0800

    ACPICA: Disassembler: allow conflicting external declarations to be emitted.
    ACPICA commit 0ed9f2e2ccc112439eaa355b5952a05d6fdb7814
    
    An external declaration is a conflicting declaration when a name has
    been declared as an external and a named object within the same file.
    
    Link: https://github.com/acpica/acpica/commit/0ed9f2e2
    Signed-off-by: Erik Schmauss <erik.schmauss@intel.com>
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpica/aclocal.h
+++ drivers/acpi/acpica/aclocal.h
@@ -1181,6 +1181,7 @@ struct acpi_external_list {
 #define ACPI_EXT_INTERNAL_PATH_ALLOCATED    0x04	/* Deallocate internal path on completion */
 #define ACPI_EXT_EXTERNAL_EMITTED           0x08	/* External() statement has been emitted */
 #define ACPI_EXT_ORIGIN_FROM_OPCODE         0x10	/* External came from a External() opcode */
+#define ACPI_EXT_CONFLICTING_DECLARATION    0x20	/* External has a conflicting declaration within AML */
 
 struct acpi_external_file {
 	char *path;
