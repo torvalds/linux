dad7a3be4960e5545882a0cd8d7613af22874314
Ming Lei <ming.lei@redhat.com>
2017-06-06 23:21:59 +0800

    blk-mq: pass correct hctx to blk_mq_try_issue_directly
    When direct issue is done on request picked up from plug list,
    the hctx need to be updated with the actual hw queue, otherwise
    wrong hctx is used and may hurt performance, especially when
    wrong SRCU readlock is acquired/released
    
    Reported-by: Bart Van Assche <bart.vanassche@sandisk.com>
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -1619,9 +1619,12 @@ static blk_qc_t blk_mq_make_request(struct request_queue *q, struct bio *bio)
 
 		blk_mq_put_ctx(data.ctx);
 
-		if (same_queue_rq)
+		if (same_queue_rq) {
+			data.hctx = blk_mq_map_queue(q,
+					same_queue_rq->mq_ctx->cpu);
 			blk_mq_try_issue_directly(data.hctx, same_queue_rq,
 					&cookie);
+		}
 	} else if (q->nr_hw_queues > 1 && is_sync) {
 		blk_mq_put_ctx(data.ctx);
 		blk_mq_bio_to_request(rq, bio);
