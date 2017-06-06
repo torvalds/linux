92046578ac88e0a93f8ef03240e6c832b0189aa7
Daniel Borkmann <daniel@iogearbox.net>
2017-06-06 18:38:04 +0200

    bpf: cgroup skb progs cannot access ld_abs/ind
    Commit fb9a307d11d6 ("bpf: Allow CGROUP_SKB eBPF program to
    access sk_buff") enabled programs of BPF_PROG_TYPE_CGROUP_SKB
    type to use ld_abs/ind instructions. However, at this point,
    we cannot use them, since offsets relative to SKF_LL_OFF will
    end up pointing skb_mac_header(skb) out of bounds since in the
    egress path it is not yet set at that point in time, but only
    after __dev_queue_xmit() did a general reset on the mac header.
    bpf_internal_load_pointer_neg_helper() will then end up reading
    data from a wrong offset.
    
    BPF_PROG_TYPE_CGROUP_SKB programs can use bpf_skb_load_bytes()
    already to access packet data, which is also more flexible than
    the insns carried over from cBPF.
    
    Fixes: fb9a307d11d6 ("bpf: Allow CGROUP_SKB eBPF program to access sk_buff")
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Cc: Chenbo Feng <fengc@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -2426,7 +2426,6 @@ static bool may_access_skb(enum bpf_prog_type type)
 	case BPF_PROG_TYPE_SOCKET_FILTER:
 	case BPF_PROG_TYPE_SCHED_CLS:
 	case BPF_PROG_TYPE_SCHED_ACT:
-	case BPF_PROG_TYPE_CGROUP_SKB:
 		return true;
 	default:
 		return false;
