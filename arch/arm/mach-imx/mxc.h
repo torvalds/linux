8a35e18b99ad1dfef049631045519cff826e4211
Leonard Crestez <leonard.crestez@nxp.com>
2017-06-06 20:50:42 +0300

    ARM: imx: Add MXC_CPU_IMX6ULL and cpu_is_imx6ull
    [ Upstream commit b3ea575770c7eeb259c77b6861cd14d00eb309df ]
    
    Support for imx6ull is already present but it's based on
    of_machine_is_compatible("fsl,imx6ull") checks. Add it to the MXC_CPU_*
    enumeration as well.
    
    This also fixes /sys/devices/soc0/soc_id reading "Unknown".
    
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>
    Reviewed-by: Fabio Estevam <fabio.estevam@nxp.com>
    Signed-off-by: Shawn Guo <shawnguo@kernel.org>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm/mach-imx/mxc.h
+++ arch/arm/mach-imx/mxc.h
@@ -39,6 +39,7 @@
 #define MXC_CPU_IMX6SX		0x62
 #define MXC_CPU_IMX6Q		0x63
 #define MXC_CPU_IMX6UL		0x64
+#define MXC_CPU_IMX6ULL		0x65
 #define MXC_CPU_IMX7D		0x72
 
 #define IMX_DDR_TYPE_LPDDR2		1
@@ -169,6 +170,11 @@ static inline bool cpu_is_imx6sx(void)
 static inline bool cpu_is_imx6ul(void)
 {
 	return __mxc_cpu_type == MXC_CPU_IMX6UL;
+}
+
+static inline bool cpu_is_imx6ull(void)
+{
+	return __mxc_cpu_type == MXC_CPU_IMX6ULL;
 }
 
 static inline bool cpu_is_imx6q(void)
