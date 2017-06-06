7b08579f5c2a5c0e94b4a9f298339c2fdf0fcc89
Johan Hovold <johan@kernel.org>
2017-06-06 12:54:41 +0200

    tty: drop unused alt_speed from tty_struct
    Drop the now unused alt_speed field from struct tty_struct.
    
    Setting an alt_speed using the ASYNC_SPD flags has been deprecated since
    v2.1.69, and has been broken for all tty drivers but serial-core since
    v3.10 and commit 6865ff222cca ("TTY: do not warn about setting speed via
    SPD_*") without anyone noticing.
    
    Note that serial-core still supports changing speed using TIOCSSERIAL
    and SPD flags (including "alt-speeds"), but also warns about it being
    deprecated since pre-git.
    
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Reviewed-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/tty.h
+++ include/linux/tty.h
@@ -316,7 +316,6 @@ struct tty_struct {
 
 	struct tty_struct *link;
 	struct fasync_struct *fasync;
-	int alt_speed;		/* For magic substitution of 38400 bps */
 	wait_queue_head_t write_wait;
 	wait_queue_head_t read_wait;
 	struct work_struct hangup_work;
