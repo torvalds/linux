413de34ab93edc80ef710c54ceb0987b8496aef3
Liam Breck <kernel@networkimprov.net>
2017-06-07 11:37:52 -0700

    power: supply: core: Add power_supply_prop_precharge
    Battery chargers use POWER_SUPPLY_PROP_PRECHARGE_CURRENT
    Clarify related item POWER_SUPPLY_PROP_CHARGE_TERM_CURRENT
    
    Signed-off-by: Liam Breck <kernel@networkimprov.net>
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.co.uk>

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -242,6 +242,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(time_to_full_avg),
 	POWER_SUPPLY_ATTR(type),
 	POWER_SUPPLY_ATTR(scope),
+	POWER_SUPPLY_ATTR(precharge_current),
 	POWER_SUPPLY_ATTR(charge_term_current),
 	POWER_SUPPLY_ATTR(calibrate),
 	/* Properties of type `const char *' */
