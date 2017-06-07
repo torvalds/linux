c67a3d05024fad00ac160e868967a1e711073993
Johannes Berg <johannes.berg@intel.com>
2017-06-07 09:31:13 +0200

    iwlwifi: mvm: byte-swap constant instead of variable
    Convention has it to byte-swap the constant instead of the variable
    when doing bit checks. This also generates better code when the swap
    is actually needed, since the constant can be swapped at compile-time.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/mvm/rxmq.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/rxmq.c
@@ -851,7 +851,7 @@ void iwl_mvm_rx_mpdu_mq(struct iwl_mvm *mvm, struct napi_struct *napi,
 
 	rcu_read_lock();
 
-	if (le16_to_cpu(desc->status) & IWL_RX_MPDU_STATUS_SRC_STA_FOUND) {
+	if (desc->status & cpu_to_le16(IWL_RX_MPDU_STATUS_SRC_STA_FOUND)) {
 		u8 id = desc->sta_id_flags & IWL_RX_MPDU_SIF_STA_ID_MASK;
 
 		if (!WARN_ON_ONCE(id >= ARRAY_SIZE(mvm->fw_id_to_mac_id))) {
