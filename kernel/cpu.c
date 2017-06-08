57de72125d34f83bfd39615fcc3cc25ca3b9c0ec
Arnd Bergmann <arnd@arndb.de>
2017-06-08 10:55:33 +0200

    cpu/hotplug: Remove unused check_for_tasks() function
    clang -Wunused-function found one remaining function that was
    apparently meant to be removed in a recent code cleanup:
    
    kernel/cpu.c:565:20: warning: unused function 'check_for_tasks' [-Wunused-function]
    
    Sebastian explained: The function became unused unintentionally, but there
    is already a failure check, when a task cannot be removed from the outgoing
    cpu in the scheduler code, so bringing it back is not really giving any
    extra value.
    
    Fixes: 530e9b76ae8f ("cpu/hotplug: Remove obsolete cpu hotplug register/unregister functions")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Cc: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Cc: Anna-Maria Gleixner <anna-maria@linutronix.de>
    Link: http://lkml.kernel.org/r/20170608085544.2257132-1-arnd@arndb.de
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -562,30 +562,6 @@ void clear_tasks_mm_cpumask(int cpu)
 	rcu_read_unlock();
 }
 
-static inline void check_for_tasks(int dead_cpu)
-{
-	struct task_struct *g, *p;
-
-	read_lock(&tasklist_lock);
-	for_each_process_thread(g, p) {
-		if (!p->on_rq)
-			continue;
-		/*
-		 * We do the check with unlocked task_rq(p)->lock.
-		 * Order the reading to do not warn about a task,
-		 * which was running on this cpu in the past, and
-		 * it's just been woken on another cpu.
-		 */
-		rmb();
-		if (task_cpu(p) != dead_cpu)
-			continue;
-
-		pr_warn("Task %s (pid=%d) is on cpu %d (state=%ld, flags=%x)\n",
-			p->comm, task_pid_nr(p), dead_cpu, p->state, p->flags);
-	}
-	read_unlock(&tasklist_lock);
-}
-
 /* Take this CPU down. */
 static int take_cpu_down(void *_param)
 {
