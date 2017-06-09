4068367c9ca7b515a209f9c0c8741309a1e90495
Andrei Vagin <avagin@openvz.org>
2017-06-08 17:32:29 -0700

    fs: don't forget to put old mntns in mntns_install
    Fixes: 4f757f3cbf54 ("make sure that mntns_install() doesn't end up with referral for root")
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Andrei Vagin <avagin@openvz.org>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/namespace.c
+++ fs/namespace.c
@@ -3488,6 +3488,8 @@ static int mntns_install(struct nsproxy *nsproxy, struct ns_common *ns)
 		return err;
 	}
 
+	put_mnt_ns(old_mnt_ns);
+
 	/* Update the pwd and root */
 	set_fs_pwd(fs, &root);
 	set_fs_root(fs, &root);
