f8dd7c70869565085542cffd14bf93705492ce14
David Windsor <dave@nullcore.net>
2017-06-10 22:50:36 -0400

    ext4: Define usercopy region in ext4_inode_cache slab cache
    The ext4 symlink pathnames, stored in struct ext4_inode_info.i_data
    and therefore contained in the ext4_inode_cache slab cache, need
    to be copied to/from userspace.
    
    cache object allocation:
        fs/ext4/super.c:
            ext4_alloc_inode(...):
                struct ext4_inode_info *ei;
                ...
                ei = kmem_cache_alloc(ext4_inode_cachep, GFP_NOFS);
                ...
                return &ei->vfs_inode;
    
        include/trace/events/ext4.h:
                #define EXT4_I(inode) \
                    (container_of(inode, struct ext4_inode_info, vfs_inode))
    
        fs/ext4/namei.c:
            ext4_symlink(...):
                ...
                inode->i_link = (char *)&EXT4_I(inode)->i_data;
    
    example usage trace:
        readlink_copy+0x43/0x70
        vfs_readlink+0x62/0x110
        SyS_readlinkat+0x100/0x130
    
        fs/namei.c:
            readlink_copy(..., link):
                ...
                copy_to_user(..., link, len)
    
            (inlined into vfs_readlink)
            generic_readlink(dentry, ...):
                struct inode *inode = d_inode(dentry);
                const char *link = inode->i_link;
                ...
                readlink_copy(..., link);
    
    In support of usercopy hardening, this patch defines a region in the
    ext4_inode_cache slab cache in which userspace copy operations are
    allowed.
    
    This region is known as the slab cache's usercopy region. Slab caches
    can now check that each dynamically sized copy operation involving
    cache-managed memory falls entirely within the slab's usercopy region.
    
    This patch is modified from Brad Spengler/PaX Team's PAX_USERCOPY
    whitelisting code in the last public patch of grsecurity/PaX based on my
    understanding of the code. Changes or omissions from the original code are
    mine and don't reflect the original grsecurity/PaX code.
    
    Signed-off-by: David Windsor <dave@nullcore.net>
    [kees: adjust commit log, provide usage trace]
    Cc: "Theodore Ts'o" <tytso@mit.edu>
    Cc: Andreas Dilger <adilger.kernel@dilger.ca>
    Cc: linux-ext4@vger.kernel.org
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -1036,11 +1036,13 @@ static void init_once(void *foo)
 
 static int __init init_inodecache(void)
 {
-	ext4_inode_cachep = kmem_cache_create("ext4_inode_cache",
-					     sizeof(struct ext4_inode_info),
-					     0, (SLAB_RECLAIM_ACCOUNT|
-						SLAB_MEM_SPREAD|SLAB_ACCOUNT),
-					     init_once);
+	ext4_inode_cachep = kmem_cache_create_usercopy("ext4_inode_cache",
+				sizeof(struct ext4_inode_info), 0,
+				(SLAB_RECLAIM_ACCOUNT|SLAB_MEM_SPREAD|
+					SLAB_ACCOUNT),
+				offsetof(struct ext4_inode_info, i_data),
+				sizeof_field(struct ext4_inode_info, i_data),
+				init_once);
 	if (ext4_inode_cachep == NULL)
 		return -ENOMEM;
 	return 0;
