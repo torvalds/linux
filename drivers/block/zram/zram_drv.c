f40609d1591fbbd9d391f1f8220173237911ab23
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2017-06-13 09:12:46 +0200

    zram: convert remaining CLASS_ATTR() to CLASS_ATTR_RO()
    I missed converting the last zram attribute to CLASS_ATTR_RO() after
    removing CLASS_ATTR() from the kernel, causing a build breakage.  This
    patch fixes that problem.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1293,7 +1293,7 @@ static ssize_t hot_add_show(struct class *class,
 		return ret;
 	return scnprintf(buf, PAGE_SIZE, "%d\n", ret);
 }
-static CLASS_ATTR(hot_add, 0400, hot_add_show, NULL);
+static CLASS_ATTR_RO(hot_add);
 
 static ssize_t hot_remove_store(struct class *class,
 			struct class_attribute *attr,
