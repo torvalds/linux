ac5b52d2a2980fe34474e23037b02485c61538a3
Karim Eshapa <karim.eshapa@gmail.com>
2017-06-14 17:40:56 +0200

    video: fbdev: omap2: omapfb: displays: panel-dsi-cm: Use time comparison kernel macro.
    Use time_before_eq() kernel macro for time comparison.
    
    Signed-off-by: Karim Eshapa <karim.eshapa@gmail.com>
    Cc: tomi.valkeinen@ti.com
    Cc: peter.ujfalusi@ti.com
    Cc: tj@kernel.org
    Cc: mingo@kernel.org
    [b.zolnierkie: patch description fixup]
    Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>

--- drivers/video/fbdev/omap2/omapfb/displays/panel-dsi-cm.c
+++ drivers/video/fbdev/omap2/omapfb/displays/panel-dsi-cm.c
@@ -100,7 +100,7 @@ static void hw_guard_wait(struct panel_drv_data *ddata)
 {
 	unsigned long wait = ddata->hw_guard_end - jiffies;
 
-	if ((long)wait > 0 && wait <= ddata->hw_guard_wait) {
+	if ((long)wait > 0 && time_before_eq(wait, ddata->hw_guard_wait)) {
 		set_current_state(TASK_UNINTERRUPTIBLE);
 		schedule_timeout(wait);
 	}
