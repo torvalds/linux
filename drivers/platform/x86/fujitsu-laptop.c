08df5d476fa817ff9c4a5a194a171e43f27a91c1
Michał Kępień <kernel@kempniu.pl>
2017-06-16 06:40:53 +0200

    platform/x86: fujitsu-laptop: remove redundant safety checks
    Do not check whether the pointer passed to ACPI add callbacks is NULL as
    it is earlier dereferenced anyway in the bus-level probe callback,
    acpi_device_probe().
    
    Do not check the value of acpi_disabled in fujitsu_init(), because it is
    already done by acpi_bus_register_driver(), which is the first function
    called by fujitsu_init().
    
    Signed-off-by: Michał Kępień <kernel@kempniu.pl>
    Reviewed-by: Jonathan Woithe <jwoithe@just42.net>
    Signed-off-by: Darren Hart (VMware) <dvhart@infradead.org>

--- drivers/platform/x86/fujitsu-laptop.c
+++ drivers/platform/x86/fujitsu-laptop.c
@@ -408,9 +408,6 @@ static int acpi_fujitsu_bl_add(struct acpi_device *device)
 	if (acpi_video_get_backlight_type() != acpi_backlight_vendor)
 		return -ENODEV;
 
-	if (!device)
-		return -EINVAL;
-
 	priv = devm_kzalloc(&device->dev, sizeof(*priv), GFP_KERNEL);
 	if (!priv)
 		return -ENOMEM;
@@ -798,9 +795,6 @@ static int acpi_fujitsu_laptop_add(struct acpi_device *device)
 	int error;
 	int i;
 
-	if (!device)
-		return -EINVAL;
-
 	priv = devm_kzalloc(&device->dev, sizeof(*priv), GFP_KERNEL);
 	if (!priv)
 		return -ENOMEM;
@@ -1018,9 +1012,6 @@ MODULE_DEVICE_TABLE(acpi, fujitsu_ids);
 static int __init fujitsu_init(void)
 {
 	int ret;
-
-	if (acpi_disabled)
-		return -ENODEV;
 
 	ret = acpi_bus_register_driver(&acpi_fujitsu_bl_driver);
 	if (ret)
