4cc74389a551dc95fce72d58c11e55a93b6ecd19
Weinan Li <weinan.z.li@intel.com>
2017-06-19 08:49:17 +0800

    drm/i915/gvt: remove scheduler_mutex in per-engine workload_thread
    For the vGPU workloads, now GVT-g use per vGPU scheduler, the per-ring
    work_thread only pick workload belongs to the current vGPU. And with time
    slice based scheduler, it waits all the engines become idle before do vGPU
    switch. So we can run free dispatch in per-ring work_thread, different ring
    running in different 'vGPU' won't happen.
    
    For the workloads between vGPU and Host, this scheduler_mutex can't block
    host to dispatch workload into other ring engines.
    
    Here remove this mutex since it impacts the performance when applications
    use more than 1 ring engines in 1 vgpu.
    
    ring0 running in vGPU1, ring1 running in Host. Will happen.
    ring0 running in vGPU1, ring1 running in vGPU2. Won't happen.
    
    Signed-off-by: Weinan Li <weinan.z.li@intel.com>
    Signed-off-by: Ping Gao <ping.a.gao@intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>

--- drivers/gpu/drm/i915/gvt/scheduler.c
+++ drivers/gpu/drm/i915/gvt/scheduler.c
@@ -464,8 +464,6 @@ struct workload_thread_param {
 	int ring_id;
 };
 
-static DEFINE_MUTEX(scheduler_mutex);
-
 static int workload_thread(void *priv)
 {
 	struct workload_thread_param *p = (struct workload_thread_param *)priv;
@@ -496,8 +494,6 @@ static int workload_thread(void *priv)
 
 		if (!workload)
 			break;
-
-		mutex_lock(&scheduler_mutex);
 
 		gvt_dbg_sched("ring id %d next workload %p vgpu %d\n",
 				workload->ring_id, workload,
@@ -537,9 +533,6 @@ complete:
 					FORCEWAKE_ALL);
 
 		intel_runtime_pm_put(gvt->dev_priv);
-
-		mutex_unlock(&scheduler_mutex);
-
 	}
 	return 0;
 }
