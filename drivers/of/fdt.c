0ef5adca5b212c705a7c61cf721db223fafecf07
Frank Rowand <frank.rowand@sony.com>
2017-06-20 16:46:28 -0700

    of: fdt.c header comment typo
    Fix typo in header comment for early_init_dt_scan_memory()
    
    Signed-off-by: Frank Rowand <frank.rowand@sony.com>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- drivers/of/fdt.c
+++ drivers/of/fdt.c
@@ -1050,7 +1050,7 @@ u64 __init dt_mem_next_cell(int s, const __be32 **cellp)
 }
 
 /**
- * early_init_dt_scan_memory - Look for an parse memory nodes
+ * early_init_dt_scan_memory - Look for and parse memory nodes
  */
 int __init early_init_dt_scan_memory(unsigned long node, const char *uname,
 				     int depth, void *data)
