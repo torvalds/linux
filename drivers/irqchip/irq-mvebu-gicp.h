a68a63cb4dfc30e8a79b444aabc7747bb7621acf
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-06-21 15:29:14 +0200

    irqchip/irq-mvebu-gicp: Add new driver for Marvell GICP
    This commit adds a simple driver for the Marvell GICP, a hardware unit
    that converts memory writes into GIC SPI interrupts. The driver provides
    a number of functions to the ICU driver to allocate GICP interrupts, and
    get the physical addresses that the ICUs should write to to set/clear
    interrupts.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Reviewed-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

---
+++ drivers/irqchip/irq-mvebu-gicp.h
@@ -0,0 +1,11 @@
+#ifndef __MVEBU_GICP_H__
+#define __MVEBU_GICP_H__
+
+#include <linux/types.h>
+
+struct device_node;
+
+int mvebu_gicp_get_doorbells(struct device_node *dn, phys_addr_t *setspi,
+			     phys_addr_t *clrspi);
+
+#endif /* __MVEBU_GICP_H__ */
