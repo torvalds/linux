cd0ae1d395a8bfc208437ce612413e58f5137499
Michael Holzheu <holzheu@linux.vnet.ibm.com>
2017-06-21 19:23:18 +0200

    s390/crash: Remove unused KEXEC_NOTE_BYTES
    After commmit 692f66f26a4c19 ("crash: move crashkernel parsing and vmcore
    related code under CONFIG_CRASH_CORE") the KEXEC_NOTE_BYTES macro is not
    used anymore and for s390 we create the ELF header in the new kernel
    anyway. Therefore remove the macro.
    
    Reported-by: Xunlei Pang <xpang@redhat.com>
    Reviewed-by: Mikhail Zaslonko <zaslonko@linux.vnet.ibm.com>
    Signed-off-by: Michael Holzheu <holzheu@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- include/linux/kexec.h
+++ include/linux/kexec.h
@@ -63,15 +63,6 @@
 #define KEXEC_CORE_NOTE_NAME	CRASH_CORE_NOTE_NAME
 
 /*
- * The per-cpu notes area is a list of notes terminated by a "NULL"
- * note header.  For kdump, the code in vmcore.c runs in the context
- * of the second kernel to combine them into one note.
- */
-#ifndef KEXEC_NOTE_BYTES
-#define KEXEC_NOTE_BYTES	CRASH_CORE_NOTE_BYTES
-#endif
-
-/*
  * This structure is used to hold the arguments that are used when loading
  * kernel binaries.
  */
