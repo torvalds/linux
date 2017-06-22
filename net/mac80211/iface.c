79af1f866193de29e65a4dba7d0dab14b0c0ff93
Johannes Berg <johannes.berg@intel.com>
2017-06-22 12:20:29 +0200

    mac80211: avoid allocating TXQs that won't be used
    For AP_VLAN and monitor interfaces we'll never use the TXQs
    we allocated, so avoid doing so.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/iface.c
+++ net/mac80211/iface.c
@@ -1772,7 +1772,9 @@ int ieee80211_if_add(struct ieee80211_local *local, const char *name,
 				 sizeof(void *));
 		int txq_size = 0;
 
-		if (local->ops->wake_tx_queue)
+		if (local->ops->wake_tx_queue &&
+		    type != NL80211_IFTYPE_AP_VLAN &&
+		    type != NL80211_IFTYPE_MONITOR)
 			txq_size += sizeof(struct txq_info) +
 				    local->hw.txq_data_size;
 
