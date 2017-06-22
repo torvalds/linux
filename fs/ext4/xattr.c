0bd454c04f02e7bb101d8ff510b54826eda4a5f0
Tahsin Erdogan <tahsin@google.com>
2017-06-21 22:02:06 -0400

    ext4: ext4_xattr_value_same() should return false for external data
    ext4_xattr_value_same() is used as a quick optimization in case the new
    xattr value is identical to the previous value. When xattr value is
    stored in a xattr inode the check becomes expensive so it is better to
    just assume that they are not equal.
    
    Signed-off-by: Tahsin Erdogan <tahsin@google.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/xattr.c
+++ fs/ext4/xattr.c
@@ -1426,6 +1426,9 @@ static int ext4_xattr_value_same(struct ext4_xattr_search *s,
 {
 	void *value;
 
+	/* When e_value_inum is set the value is stored externally. */
+	if (s->here->e_value_inum)
+		return 0;
 	if (le32_to_cpu(s->here->e_value_size) != i->value_len)
 		return 0;
 	value = ((void *)s->base) + le16_to_cpu(s->here->e_value_offs);
