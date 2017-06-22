0cb259c47a4df466d641c1f07ae3eccaa9ba3ccb
Jon Derrick <jonathan.derrick@intel.com>
2017-06-22 09:15:42 -0600

    PCI: vmd: Move SRCU cleanup after bus, child device removal
    Recent __call_srcu() changes have exposed that we need to cleanup SRCU
    structures after pci_stop_root_bus() calls into vmd_msi_free().
    
    Fixes: 3906b91844d6 ("PCI: vmd: Use SRCU as a local RCU to prevent delaying global RCU")
    Signed-off-by: Jon Derrick <jonathan.derrick@intel.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Keith Busch <keith.busch@intel.com>
    Cc: <stable@vger.kernel.org> # 4.11

--- drivers/pci/host/vmd.c
+++ drivers/pci/host/vmd.c
@@ -736,10 +736,10 @@ static void vmd_remove(struct pci_dev *dev)
 	struct vmd_dev *vmd = pci_get_drvdata(dev);
 
 	vmd_detach_resources(vmd);
-	vmd_cleanup_srcu(vmd);
 	sysfs_remove_link(&vmd->dev->dev.kobj, "domain");
 	pci_stop_root_bus(vmd->bus);
 	pci_remove_root_bus(vmd->bus);
+	vmd_cleanup_srcu(vmd);
 	vmd_teardown_dma_ops(vmd);
 	irq_domain_remove(vmd->irq_domain);
 }
