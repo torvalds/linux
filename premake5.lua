d172e5620c5838a47526748885c9819d93b335bd
czlc <czlc1983@gmail.com>
2017-06-24 18:48:46 +0800

    加入预定义宏

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,17 @@
+local PD = {"__KERNEL__"}
+local file_path = "D:\\github\\czlc\\linux\\arch\\x86\\configs\\x86_64_defconfig"
+local file_data = io.open(file_path, "r"):read("*a")
+
+for w in string.gmatch(file_data, "[^\n\r]+") do
+	if (string.sub(w, 1, 1) ~= "#") then
+		local M,C = string.gsub(w, "(%w+)=y", "%1")
+		if (C == 1) then
+			table.insert(PD, M)
+		end
+	end
+end
+
+
 workspace "kernel"
    configurations { "Debug" }
    
@@ -6,4 +20,4 @@ project "kernel"
 	language "C"
 	files { "**.*" }
 	includedirs { "." }
-	defines {"__KERNEL__"}
+	defines(PD)
