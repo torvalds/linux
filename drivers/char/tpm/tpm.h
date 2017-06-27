b5d0ebc99bf5d0801a5ecbe958caa3d68b8eaee8
Enric Balletbo i Serra <enric.balletbo@collabora.com>
2017-06-27 12:27:24 +0200

    tpm: do not suspend/resume if power stays on
    The suspend/resume behavior of the TPM can be controlled by setting
    "powered-while-suspended" in the DTS. This is useful for the cases
    when hardware does not power-off the TPM.
    
    Signed-off-by: Sonny Rao <sonnyrao@chromium.org>
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    Reviewed-by: Jason Gunthorpe <jgunthorpe@obsidianresearch.com>
    Reviewed-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
    Signed-off-by: James Morris <james.l.morris@oracle.com>

--- drivers/char/tpm/tpm.h
+++ drivers/char/tpm/tpm.h
@@ -174,6 +174,7 @@ enum tpm_chip_flags {
 	TPM_CHIP_FLAG_IRQ		= BIT(2),
 	TPM_CHIP_FLAG_VIRTUAL		= BIT(3),
 	TPM_CHIP_FLAG_HAVE_TIMEOUTS	= BIT(4),
+	TPM_CHIP_FLAG_ALWAYS_POWERED	= BIT(5),
 };
 
 struct tpm_bios_log {
