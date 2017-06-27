1d8f83604c4244d93c5a49f5107624769df6248f
Yan, Zheng <zyan@redhat.com>
2017-06-27 11:57:56 +0800

    ceph: new mount option that specifies fscache uniquifier
    Current ceph uses FSID as primary index key of fscache data. This
    allows ceph to retain cached data across remount. But this causes
    problem (kernel opps, fscache does not support sharing data) when
    a filesystem get mounted several times (with fscache enabled, with
    different mount options).
    
    The fix is adding a new mount option, which specifies uniquifier
    for fscache.
    
    Signed-off-by: "Yan, Zheng" <zyan@redhat.com>
    Acked-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- fs/ceph/super.h
+++ fs/ceph/super.h
@@ -73,6 +73,7 @@ struct ceph_mount_options {
 	char *snapdir_name;   /* default ".snap" */
 	char *mds_namespace;  /* default NULL */
 	char *server_path;    /* default  "/" */
+	char *fscache_uniq;   /* default NULL */
 };
 
 struct ceph_fs_client {
