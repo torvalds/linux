6916363f3083837ed5adb3df2dd90d6b97017dff
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2017-06-27 18:19:11 +0200

    fs/dcache: init in_lookup_hashtable
    in_lookup_hashtable was introduced in commit 94bdd655caba ("parallel
    lookups machinery, part 3") and never initialized but since it is in
    the data it is all zeros. But we need this for -RT.
    
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: linux-fsdevel@vger.kernel.org
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/dcache.c
+++ fs/dcache.c
@@ -3608,6 +3608,11 @@ EXPORT_SYMBOL(d_genocide);
 
 void __init vfs_caches_init_early(void)
 {
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(in_lookup_hashtable); i++)
+		INIT_HLIST_BL_HEAD(&in_lookup_hashtable[i]);
+
 	dcache_init_early();
 	inode_init_early();
 }
