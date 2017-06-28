01e1932a1748e20b69ba23d0a01db5eb3a525782
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-06-28 16:39:32 +0530

    iommu/vt-d: Constify intel_dma_ops
    Most dma_map_ops structures are never modified. Constify these
    structures such that these can be write-protected.
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/intel-iommu.c
+++ drivers/iommu/intel-iommu.c
@@ -3954,7 +3954,7 @@ static int intel_mapping_error(struct device *dev, dma_addr_t dma_addr)
 	return !dma_addr;
 }
 
-struct dma_map_ops intel_dma_ops = {
+const struct dma_map_ops intel_dma_ops = {
 	.alloc = intel_alloc_coherent,
 	.free = intel_free_coherent,
 	.map_sg = intel_map_sg,
