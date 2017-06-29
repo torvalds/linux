fc3ba81a5adac413312019413c91b1e6a5d8d1fa
John Hsu <supercraig0719@gmail.com>
2017-06-29 11:41:30 +0800

    ASoC: nau8825: change crosstalk-bypass property to bool type
    The property type of "nuvoton,crosstalk-bypass" changes to boolean.
    The document is updated as well.
    
    Signed-off-by: John Hsu <KCHSU0@nuvoton.com>
    Signed-off-by: John Hsu <supercraig0719@gmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/nau8825.c
+++ sound/soc/codecs/nau8825.c
@@ -2506,10 +2506,8 @@ static int nau8825_read_device_properties(struct device *dev,
 		&nau8825->jack_eject_debounce);
 	if (ret)
 		nau8825->jack_eject_debounce = 0;
-	ret = device_property_read_u32(dev, "nuvoton,crosstalk-bypass",
-		&nau8825->xtalk_bypass);
-	if (ret)
-		nau8825->xtalk_bypass = 1;
+	nau8825->xtalk_bypass = device_property_read_bool(dev,
+		"nuvoton,crosstalk-bypass");
 
 	nau8825->mclk = devm_clk_get(dev, "mclk");
 	if (PTR_ERR(nau8825->mclk) == -EPROBE_DEFER) {
