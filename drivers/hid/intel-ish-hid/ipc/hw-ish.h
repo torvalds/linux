1694130910cd654a9215fbc0244975e3cf208fc1
Song Hongyan <hongyan.song@intel.com>
2017-06-29 13:43:34 -0700

    HID: intel-ish-hid: Enable Gemini Lake ish driver
    Added PCI ID for Gemini Lake ISH.
    
    Signed-off-by: Song Hongyan <hongyan.song@intel.com>
    Acked-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/intel-ish-hid/ipc/hw-ish.h
+++ drivers/hid/intel-ish-hid/ipc/hw-ish.h
@@ -27,6 +27,7 @@
 #define APL_Ax_DEVICE_ID	0x5AA2
 #define SPT_Ax_DEVICE_ID	0x9D35
 #define CNL_Ax_DEVICE_ID	0x9DFC
+#define GLK_Ax_DEVICE_ID	0x31A2
 
 #define	REVISION_ID_CHT_A0	0x6
 #define	REVISION_ID_CHT_Ax_SI	0x0
