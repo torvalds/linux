df674efa32b522fc024d26b0a667a7985e8b87da
Dan Carpenter <dan.carpenter@oracle.com>
2017-06-30 11:01:47 +0300

    phy: phy-twl4030-usb: silence an uninitialized variable warning
    The "check" variable isn't necessarily initialized when we print it out
    in the debugging messages.  It's a pretty haphazard affair and it
    doesn't matter very much what we initialize "check" to.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- drivers/phy/ti/phy-twl4030-usb.c
+++ drivers/phy/ti/phy-twl4030-usb.c
@@ -185,7 +185,7 @@ struct twl4030_usb {
 static int twl4030_i2c_write_u8_verify(struct twl4030_usb *twl,
 		u8 module, u8 data, u8 address)
 {
-	u8 check;
+	u8 check = 0xFF;
 
 	if ((twl_i2c_write_u8(module, data, address) >= 0) &&
 	    (twl_i2c_read_u8(module, &check, address) >= 0) &&
