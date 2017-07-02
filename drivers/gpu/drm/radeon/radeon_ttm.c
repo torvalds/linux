46886dbfbda2187adbe9028f4d0a190ce96200f1
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-07-02 14:36:47 +0530

    drm: radeon: radeon_ttm: constify ttm_place structures.
    ttm_place are not supposed to change at runtime. All functions
    working with ttm_place provided by <drm/ttm/ttm_placement.h> work
    with const ttm_place. So mark the non-const structs as const.
    
    File size before:
       text	   data	    bss	    dec	    hex	filename
       9235	    344	    136	   9715	   25f3 drivers/gpu/drm/radeon/radeon_ttm.o
    
    File size After adding 'const':
       text	   data	    bss	    dec	    hex	filename
       9267	    312	    136	   9715	   25f3 drivers/gpu/drm/radeon/radeon_ttm.o
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/radeon_ttm.c
+++ drivers/gpu/drm/radeon/radeon_ttm.c
@@ -178,7 +178,7 @@ static int radeon_init_mem_type(struct ttm_bo_device *bdev, uint32_t type,
 static void radeon_evict_flags(struct ttm_buffer_object *bo,
 				struct ttm_placement *placement)
 {
-	static struct ttm_place placements = {
+	static const struct ttm_place placements = {
 		.fpfn = 0,
 		.lpfn = 0,
 		.flags = TTM_PL_MASK_CACHING | TTM_PL_FLAG_SYSTEM
