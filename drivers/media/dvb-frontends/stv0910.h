cd21b334943719f880e707eb91895fc916a88000
Daniel Scheller <d.scheller@gmx.net>
2017-07-03 13:20:54 -0400

    media: dvb-frontends: add ST STV0910 DVB-S/S2 demodulator frontend driver
    This adds a multi frontend driver for the ST STV0910 DVB-S/S2 demodulator
    frontends. The driver code originates from the Digital Devices' dddvb
    vendor driver package as of version 0.9.29, and has been cleaned up from
    core API usage which isn't supported yet in the kernel, and additionally
    all obvious style issues have been resolved. All camel case and allcaps
    have been converted to kernel_case and lowercase. Patches have been sent
    to the vendor package maintainers to fix this aswell. Signal statistics
    acquisition has been refactored to comply with standards.
    
    Permission to reuse and mainline the driver code was formally granted by
    Ralph Metzler <rjkm@metzlerbros.de>.
    
    Signed-off-by: Daniel Scheller <d.scheller@gmx.net>
    Tested-by: Richard Scobie <r.scobie@clear.net.nz>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

---
+++ drivers/media/dvb-frontends/stv0910.h
@@ -0,0 +1,32 @@
+#ifndef _STV0910_H_
+#define _STV0910_H_
+
+#include <linux/types.h>
+#include <linux/i2c.h>
+
+struct stv0910_cfg {
+	u32 clk;
+	u8  adr;
+	u8  parallel;
+	u8  rptlvl;
+	u8  single;
+};
+
+#if IS_REACHABLE(CONFIG_DVB_STV0910)
+
+extern struct dvb_frontend *stv0910_attach(struct i2c_adapter *i2c,
+					   struct stv0910_cfg *cfg, int nr);
+
+#else
+
+static inline struct dvb_frontend *stv0910_attach(struct i2c_adapter *i2c,
+						  struct stv0910_cfg *cfg,
+						  int nr)
+{
+	pr_warn("%s: driver disabled by Kconfig\n", __func__);
+	return NULL;
+}
+
+#endif /* CONFIG_DVB_STV0910 */
+
+#endif /* _STV0910_H_ */
