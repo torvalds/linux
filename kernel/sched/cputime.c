3b9c08ae3dd44201b3a188aef34d6ddf73434015
Ingo Molnar <mingo@kernel.org>
2017-07-04 11:53:40 +0200

    Revert "sched/cputime: Refactor the cputime_adjust() code"
    This reverts commit 72298e5c92c50edd8cb7cfda4519483ce65fa166.
    
    As Peter explains:
    
    > Argh, no... That code was perfectly fine. The new code otoh is
    > convoluted.
    >
    > The old code had the following form:
    >
    >         if (exception1)
    >           deal with exception1
    >
    >         if (execption2)
    >           deal with exception2
    >
    >         do normal stuff
    >
    > Which is as simple and straight forward as it gets.
    >
    > The new code otoh reads like:
    >
    >         if (!exception1) {
    >                 if (exception2)
    >                   deal with exception 2
    >                 else
    >                   do normal stuff
    >         }
    
    So restore the old form.
    
    Also fix the comment describing the logic, as it was confusing.
    
    Requested-by: Peter Zijlstra <peterz@infradead.org>
    Cc: Gustavo A. R. Silva <garsilva@embeddedor.com>
    Cc: Frans Klaver <fransklaver@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Stanislaw Gruszka <sgruszka@redhat.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Wanpeng Li <wanpeng.li@hotmail.com>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -611,17 +611,23 @@ static void cputime_adjust(struct task_cputime *curr,
 	utime = curr->utime;
 
 	/*
-	 * If either stime or both stime and utime are 0, assume all runtime is
-	 * userspace. Once a task gets some ticks, the monotonicy code at
-	 * 'update' will ensure things converge to the observed ratio.
+	 * If either stime or utime are 0, assume all runtime is userspace.
+	 * Once a task gets some ticks, the monotonicy code at 'update:'
+	 * will ensure things converge to the observed ratio.
 	 */
-	if (stime != 0) {
-		if (utime == 0)
-			stime = rtime;
-		else
-			stime = scale_stime(stime, rtime, stime + utime);
+	if (stime == 0) {
+		utime = rtime;
+		goto update;
 	}
 
+	if (utime == 0) {
+		stime = rtime;
+		goto update;
+	}
+
+	stime = scale_stime(stime, rtime, stime + utime);
+
+update:
 	/*
 	 * Make sure stime doesn't go backwards; this preserves monotonicity
 	 * for utime because rtime is monotonic.
