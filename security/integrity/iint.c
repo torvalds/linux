02c324a55ed9ee4d790eaa9ac8e7cdecbe3e0a22
Jeff Layton <jlayton@redhat.com>
2017-07-06 10:54:21 -0400

    integrity: remove unneeded initializations in integrity_iint_cache entries
    The init_once routine memsets the whole object to 0, and then
    explicitly sets some of the fields to 0 again. Just remove the explicit
    initializations.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Mimi Zohar <zohar@linux.vnet.ibm.com>

--- security/integrity/iint.c
+++ security/integrity/iint.c
@@ -153,15 +153,11 @@ static void init_once(void *foo)
 	struct integrity_iint_cache *iint = foo;
 
 	memset(iint, 0, sizeof(*iint));
-	iint->version = 0;
-	iint->flags = 0UL;
-	iint->atomic_flags = 0;
 	iint->ima_file_status = INTEGRITY_UNKNOWN;
 	iint->ima_mmap_status = INTEGRITY_UNKNOWN;
 	iint->ima_bprm_status = INTEGRITY_UNKNOWN;
 	iint->ima_read_status = INTEGRITY_UNKNOWN;
 	iint->evm_status = INTEGRITY_UNKNOWN;
-	iint->measured_pcrs = 0;
 	mutex_init(&iint->mutex);
 }
 
