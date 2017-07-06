5784f87df16619f6a55255aa827faba431a6ee7a
Kalle Valo <kvalo@qca.qualcomm.com>
2017-07-06 09:42:20 +0300

    ath10k: fix indenting in ath10k_wmi_update_noa()
    Commit 59ae1d127ac0 ("networking: introduce and use skb_put_data()") introduced
    a new checkpatch warning:
    
    drivers/net/wireless/ath/ath10k/wmi.c:3308: code indent should use tabs where possible
    
    Fix that.
    
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath10k/wmi.c
+++ drivers/net/wireless/ath/ath10k/wmi.c
@@ -3305,7 +3305,7 @@ static void ath10k_wmi_update_noa(struct ath10k *ar, struct ath10k_vif *arvif,
 	if (arvif->u.ap.noa_data)
 		if (!pskb_expand_head(bcn, 0, arvif->u.ap.noa_len, GFP_ATOMIC))
 			skb_put_data(bcn, arvif->u.ap.noa_data,
-			             arvif->u.ap.noa_len);
+				     arvif->u.ap.noa_len);
 }
 
 static int ath10k_wmi_op_pull_swba_ev(struct ath10k *ar, struct sk_buff *skb,
