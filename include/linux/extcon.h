b1605321a56750196b5d612b63cce907dafba189
Mayank Rana <mrana@codeaurora.org>
2017-07-06 16:42:06 -0700

    ANDROID: GKI: extcon: Fix Add usage of blocking notifier chain
    The previous partial cherry-pick commit ea6eb0f3e56e missed out some
    code that's needed for functionality. So, pick that up too.
    
    Change-Id: I0e8e3b29d169ca833f85e42d2df98dd8380cbf46
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>
    Signed-off-by: Jack Pham <jackp@codeaurora.org>
    Bug: 150893404
    (cherry picked from commit 9dce266c7ad8f075f3b7b13cbbb99e058e57d30b)
    Signed-off-by: Saravana Kannan <saravanak@google.com>

--- include/linux/extcon.h
+++ include/linux/extcon.h
@@ -215,6 +215,8 @@ extern int extcon_unregister_notifier(struct extcon_dev *edev, unsigned int id,
 				struct notifier_block *nb);
 extern int extcon_register_blocking_notifier(struct extcon_dev *edev,
 		unsigned int id, struct notifier_block *nb);
+extern int extcon_unregister_blocking_notifier(struct extcon_dev *edev,
+		unsigned int id, struct notifier_block *nb);
 extern int devm_extcon_register_notifier(struct device *dev,
 				struct extcon_dev *edev, unsigned int id,
 				struct notifier_block *nb);
@@ -244,6 +246,8 @@ extern struct extcon_dev *extcon_get_edev_by_phandle(struct device *dev,
 /* Following API get the name of extcon device. */
 extern const char *extcon_get_edev_name(struct extcon_dev *edev);
 
+extern int extcon_blocking_sync(struct extcon_dev *edev, unsigned int id,
+							bool val);
 #else /* CONFIG_EXTCON */
 static inline int extcon_get_state(struct extcon_dev *edev, unsigned int id)
 {
@@ -276,6 +280,13 @@ static inline int extcon_unregister_notifier(struct extcon_dev *edev,
 }
 
 static inline int extcon_register_blocking_notifier(struct extcon_dev *edev,
+					unsigned int id,
+					struct notifier_block *nb)
+{
+	return 0;
+}
+
+static inline int extcon_unregister_blocking_notifier(struct extcon_dev *edev,
 					unsigned int id,
 					struct notifier_block *nb)
 {
