618b8c20d03c9ea06711bd36d906322ba35c0add
Nikolay Borisov <nborisov@suse.com>
2017-07-10 15:49:32 -0700

    include/linux/mmzone.h: remove ancient/ambiguous comment
    Currently pg_data_t is just a struct which describes a NUMA node memory
    layout.  Let's keep the comment simple and remove ambiguity.
    
    Link: http://lkml.kernel.org/r/1498220534-22717-1-git-send-email-nborisov@suse.com
    Signed-off-by: Nikolay Borisov <nborisov@suse.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -603,12 +603,9 @@ extern struct page *mem_map;
 #endif
 
 /*
- * The pg_data_t structure is used in machines with CONFIG_DISCONTIGMEM
- * (mostly NUMA machines?) to denote a higher-level memory zone than the
- * zone denotes.
- *
  * On NUMA machines, each NUMA node would have a pg_data_t to describe
- * it's memory layout.
+ * it's memory layout. On UMA machines there is a single pglist_data which
+ * describes the whole memory.
  *
  * Memory statistics and page replacement data structures are maintained on a
  * per-zone basis.
