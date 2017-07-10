1d1c3436119a6fb92adb8ae72d9a1bcb7f5d0d6a
Feras Daoud <ferasda@mellanox.com>
2017-07-10 18:35:06 +0300

    net/mlx5e: IPoIB, Fix driver name retrieved by ethtool
    Printing an enhanced IPoIB device information using
    "ethtool -i DEVNAME", prints the low level driver name: mlx5_core.
    This commit changes the name to mlx5_core [ib_ipoib], to include the
    ipoib device driver infromation.
    
    Fixes: 076b0936e5fb ("net/mlx5e: IPoIB, Add ethtool support")
    Signed-off-by: Feras Daoud <ferasda@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/ipoib/ethtool.c
+++ drivers/net/ethernet/mellanox/mlx5/core/ipoib/ethtool.c
@@ -39,6 +39,8 @@ static void mlx5i_get_drvinfo(struct net_device *dev,
 	struct mlx5e_priv *priv = mlx5i_epriv(dev);
 
 	mlx5e_ethtool_get_drvinfo(priv, drvinfo);
+	strlcpy(drvinfo->driver, DRIVER_NAME "[ib_ipoib]",
+		sizeof(drvinfo->driver));
 }
 
 static void mlx5i_get_strings(struct net_device *dev,
