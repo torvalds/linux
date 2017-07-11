8a39374ef8b849dfd18e7d8dd52ef8ffee0f1042
Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
2017-07-11 16:33:13 +0200

    drm/atomic: Add __must_check to drm_atomic_helper_swap_state.
    Now that all drivers check the return value, convert swap_state to
    __must_check. This is done separately to force build warnings if we
    missed a driver.
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170711143314.2148-12-maarten.lankhorst@linux.intel.com
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- include/drm/drm_atomic_helper.h
+++ include/drm/drm_atomic_helper.h
@@ -86,7 +86,8 @@ void
 drm_atomic_helper_disable_planes_on_crtc(struct drm_crtc_state *old_crtc_state,
 					 bool atomic);
 
-int drm_atomic_helper_swap_state(struct drm_atomic_state *state, bool stall);
+int __must_check drm_atomic_helper_swap_state(struct drm_atomic_state *state,
+					      bool stall);
 
 /* nonblocking commit helpers */
 int drm_atomic_helper_setup_commit(struct drm_atomic_state *state,
