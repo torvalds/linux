b11fb73743fc406204e0749ead18560aeda8b136
Steven Rostedt (VMware) <rostedt@goodmis.org>
2017-07-11 15:43:24 -0400

    tracing: Fixup trace file header alignment
    The addition of TGID to the tracing header added a check to see if TGID
    shoudl be displayed or not, and updated the header accordingly.
    Unfortunately, it broke the default header.
    
    Also add constant strings to use for spacing. This does remove the
    visibility of the header a bit, but cuts it down from the extended lines
    much greater than 80 characters.
    
    Before this change:
    
     # tracer: function
     #
     #                            _-----=> irqs-off
     #                           / _----=> need-resched
     #                          | / _---=> hardirq/softirq
     #                          || / _--=> preempt-depth
     #                          ||| /     delay
     #           TASK-PID   CPU#||||    TIMESTAMP  FUNCTION
     #              | |       | ||||       |         |
            swapper/0-1     [000] ....     0.277830: migration_init <-do_one_initcall
            swapper/0-1     [002] d...    13.861967: Unknown type 1201
            swapper/0-1     [002] d..1    13.861970: Unknown type 1202
    
    After this change:
    
     # tracer: function
     #
     #                              _-----=> irqs-off
     #                             / _----=> need-resched
     #                            | / _---=> hardirq/softirq
     #                            || / _--=> preempt-depth
     #                            ||| /     delay
     #           TASK-PID   CPU#  ||||    TIMESTAMP  FUNCTION
     #              | |       |   ||||       |         |
            swapper/0-1     [000] ....     0.278245: migration_init <-do_one_initcall
            swapper/0-1     [003] d...    13.861189: Unknown type 1201
            swapper/0-1     [003] d..1    13.861192: Unknown type 1202
    
    Cc: Joel Fernandes <joelaf@google.com>
    Fixes: 441dae8f2f29 ("tracing: Add support for display of tgid in trace output")
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -3358,14 +3358,23 @@ static void print_func_help_header_irq(struct trace_buffer *buf, struct seq_file
 				       unsigned int flags)
 {
 	bool tgid = flags & TRACE_ITER_RECORD_TGID;
+	const char tgid_space[] = "          ";
+	const char space[] = "  ";
 
-	seq_printf(m, "#                          %s  _-----=> irqs-off\n",	    tgid ? "          " : "");
-	seq_printf(m, "#                          %s / _----=> need-resched\n",	    tgid ? "          " : "");
-	seq_printf(m, "#                          %s| / _---=> hardirq/softirq\n",  tgid ? "          " : "");
-	seq_printf(m, "#                          %s|| / _--=> preempt-depth\n",    tgid ? "          " : "");
-	seq_printf(m, "#                          %s||| /     delay\n",		    tgid ? "          " : "");
-	seq_printf(m, "#           TASK-PID   CPU#%s||||    TIMESTAMP  FUNCTION\n", tgid ? "   TGID   " : "");
-	seq_printf(m, "#              | |       | %s||||       |         |\n",	    tgid ? "     |    " : "");
+	seq_printf(m, "#                          %s  _-----=> irqs-off\n",
+		   tgid ? tgid_space : space);
+	seq_printf(m, "#                          %s / _----=> need-resched\n",
+		   tgid ? tgid_space : space);
+	seq_printf(m, "#                          %s| / _---=> hardirq/softirq\n",
+		   tgid ? tgid_space : space);
+	seq_printf(m, "#                          %s|| / _--=> preempt-depth\n",
+		   tgid ? tgid_space : space);
+	seq_printf(m, "#                          %s||| /     delay\n",
+		   tgid ? tgid_space : space);
+	seq_printf(m, "#           TASK-PID   CPU#%s||||    TIMESTAMP  FUNCTION\n",
+		   tgid ? "   TGID   " : space);
+	seq_printf(m, "#              | |       | %s||||       |         |\n",
+		   tgid ? "     |    " : space);
 }
 
 void
