b6db6bfe7188058b34b6385ea68c0be37cff5714
Geert Uytterhoeven <geert+renesas@glider.be>
2017-07-11 11:56:24 +0200

    pinctrl: sh-pfc: r8a7796: Fix MSIOF3 SS2_E mux
    Fix a copy-and-paste bug in the MSIOF3 SS2_E mux array.
    
    Fixes: 4753231cc9468390 ("pinctrl: sh-pfc: r8a7796: Add MSIOF pins, groups and functions")
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- drivers/pinctrl/sh-pfc/pfc-r8a7796.c
+++ drivers/pinctrl/sh-pfc/pfc-r8a7796.c
@@ -3082,7 +3082,7 @@ static const unsigned int msiof3_ss2_e_pins[] = {
 	RCAR_GP_PIN(2, 0),
 };
 static const unsigned int msiof3_ss2_e_mux[] = {
-	MSIOF3_SS1_E_MARK,
+	MSIOF3_SS2_E_MARK,
 };
 static const unsigned int msiof3_txd_e_pins[] = {
 	/* TXD */
