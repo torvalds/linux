e2ae8ab4b571e2e4094a28acb60649bc2732c67f
Kees Cook <keescook@chromium.org>
2017-07-12 14:35:58 -0700

    kexec_file: adjust declaration of kexec_purgatory
    Defining kexec_purgatory as a zero-length char array upsets compile time
    size checking.  Since this is built on a per-arch basis, define it as an
    unsized char array (like is done for other similar things, e.g.  linker
    sections).  This silences the warning generated by the future
    CONFIG_FORTIFY_SOURCE, which did not like the memcmp() of a "0 byte"
    array.  This drops the __weak and uses an extern instead, since both
    users define kexec_purgatory.
    
    Link: http://lkml.kernel.org/r/1497903987-21002-4-git-send-email-keescook@chromium.org
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Acked-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Cc: Daniel Micay <danielmicay@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/kexec_file.c
+++ kernel/kexec_file.c
@@ -26,13 +26,6 @@
 #include <linux/vmalloc.h>
 #include "kexec_internal.h"
 
-/*
- * Declare these symbols weak so that if architecture provides a purgatory,
- * these will be overridden.
- */
-char __weak kexec_purgatory[0];
-size_t __weak kexec_purgatory_size = 0;
-
 static int kexec_calculate_store_digests(struct kimage *image);
 
 /* Architectures can provide this probe function */
