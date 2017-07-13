f0c6e88288d65c93bbc7da4fb6f7d51b2733228a
Dan Carpenter <dan.carpenter@oracle.com>
2017-07-13 10:46:49 +0300

    RDMA/ocrdma: Fix error codes in ocrdma_create_srq()
    If either of these allocations fail then we return ERR_PTR(0).  That's
    equivalent to NULL and results in a NULL pointer dereference in the
    caller.
    
    Fixes: fe2caefcdf58 ("RDMA/ocrdma: Add driver for Emulex OneConnect IBoE RDMA adapter")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/ocrdma/ocrdma_verbs.c
+++ drivers/infiniband/hw/ocrdma/ocrdma_verbs.c
@@ -1902,6 +1902,7 @@ struct ib_srq *ocrdma_create_srq(struct ib_pd *ibpd,
 		goto err;
 
 	if (udata == NULL) {
+		status = -ENOMEM;
 		srq->rqe_wr_id_tbl = kzalloc(sizeof(u64) * srq->rq.max_cnt,
 			    GFP_KERNEL);
 		if (srq->rqe_wr_id_tbl == NULL)
