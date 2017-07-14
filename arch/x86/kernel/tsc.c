af5768507c051ceb9fe12bee59202bd83115c073
Dou Liyang <douly.fnst@cn.fujitsu.com>
2017-07-14 11:34:07 +0800

    x86/timers: Make recalibrate_cpu_khz() void
    recalibrate_cpu_khz() is called from powernow K7 and Pentium 4/Xeon
    CPU freq driver. It recalibrates cpu frequency in case of SMP = n
    and doesn't need to return anything.
    
    Mark it void, also remove the #else branch.
    
    Signed-off-by: Dou Liyang <douly.fnst@cn.fujitsu.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lkml.kernel.org/r/1500003247-17368-2-git-send-email-douly.fnst@cn.fujitsu.com

--- arch/x86/kernel/tsc.c
+++ arch/x86/kernel/tsc.c
@@ -812,13 +812,13 @@ unsigned long native_calibrate_cpu(void)
 	return tsc_pit_min;
 }
 
-int recalibrate_cpu_khz(void)
+void recalibrate_cpu_khz(void)
 {
 #ifndef CONFIG_SMP
 	unsigned long cpu_khz_old = cpu_khz;
 
 	if (!boot_cpu_has(X86_FEATURE_TSC))
-		return -ENODEV;
+		return;
 
 	cpu_khz = x86_platform.calibrate_cpu();
 	tsc_khz = x86_platform.calibrate_tsc();
@@ -828,10 +828,6 @@ int recalibrate_cpu_khz(void)
 		cpu_khz = tsc_khz;
 	cpu_data(0).loops_per_jiffy = cpufreq_scale(cpu_data(0).loops_per_jiffy,
 						    cpu_khz_old, cpu_khz);
-
-	return 0;
-#else
-	return -ENODEV;
 #endif
 }
 
