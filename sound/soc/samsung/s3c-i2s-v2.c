2d397c518eead8b6c3cd817c29bc2d4fb1c41830
Krzysztof Kozlowski <krzk@kernel.org>
2017-07-17 07:44:21 +0200

    ASoC: samsung: Remove non-existing CONFIG_CPU_S3C2413
    There is no CONFIG_CPU_S3C2413 so get rid of it.
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/samsung/s3c-i2s-v2.c
+++ sound/soc/samsung/s3c-i2s-v2.c
@@ -27,7 +27,7 @@
 
 #undef S3C_IIS_V2_SUPPORTED
 
-#if defined(CONFIG_CPU_S3C2412) || defined(CONFIG_CPU_S3C2413) \
+#if defined(CONFIG_CPU_S3C2412) \
 	|| defined(CONFIG_ARCH_S3C64XX) || defined(CONFIG_CPU_S5PV210)
 #define S3C_IIS_V2_SUPPORTED
 #endif
