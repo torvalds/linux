67f512e6707f41911c788c6a36990fce1a7ae1c0
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-07-17 23:39:50 +0530

    rtlwifi: rtl8192se: constify pci_device_id.
    pci_device_id are not supposed to change at runtime. All functions
    working with pci_device_id provided by <linux/pci.h> work with
    const pci_device_id. So mark the non-const structs as const.
    
    File size before:
       text	   data	    bss	    dec	    hex	filename
       2817	   1040	      0	   3857	    f11 realtek/rtlwifi/rtl8192se/sw.o
    
    File size After adding 'const':
       text	   data	    bss	    dec	    hex	filename
       3009	    848	      0	   3857	    f11 realtek/rtlwifi/rtl8192se/sw.o
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtlwifi/rtl8192se/sw.c
+++ drivers/net/wireless/realtek/rtlwifi/rtl8192se/sw.c
@@ -398,7 +398,7 @@ static const struct rtl_hal_cfg rtl92se_hal_cfg = {
 	.maps[RTL_RC_HT_RATEMCS15] = DESC_RATEMCS15,
 };
 
-static struct pci_device_id rtl92se_pci_ids[] = {
+static const struct pci_device_id rtl92se_pci_ids[] = {
 	{RTL_PCI_DEVICE(PCI_VENDOR_ID_REALTEK, 0x8192, rtl92se_hal_cfg)},
 	{RTL_PCI_DEVICE(PCI_VENDOR_ID_REALTEK, 0x8171, rtl92se_hal_cfg)},
 	{RTL_PCI_DEVICE(PCI_VENDOR_ID_REALTEK, 0x8172, rtl92se_hal_cfg)},
