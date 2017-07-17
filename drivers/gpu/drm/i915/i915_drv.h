58947144af34a08eee3388c1f039f199e80c0c6f
Daniel Vetter <daniel.vetter@ffwll.ch>
2017-07-17 09:06:19 +0200

    drm/i915: Update DRIVER_DATE to 20170717
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -80,8 +80,8 @@
 
 #define DRIVER_NAME		"i915"
 #define DRIVER_DESC		"Intel Graphics"
-#define DRIVER_DATE		"20170703"
-#define DRIVER_TIMESTAMP	1499064041
+#define DRIVER_DATE		"20170717"
+#define DRIVER_TIMESTAMP	1500275179
 
 /* Use I915_STATE_WARN(x) and I915_STATE_WARN_ON() (rather than WARN() and
  * WARN_ON()) for hw state sanity checks to check for unexpected conditions
