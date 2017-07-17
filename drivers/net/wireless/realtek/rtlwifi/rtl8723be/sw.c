8c6f0f5bf1e15019cfa531e940b8ba1bdd9f6e08
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-07-17 23:39:53 +0530

    rtlwifi: rtl8723be: constify pci_device_id.
    pci_device_id are not supposed to change at runtime. All functions
    working with pci_device_id provided by <linux/pci.h> work with
    const pci_device_id. So mark the non-const structs as const.
    
    File size before:
       text	   data	    bss	    dec	    hex	filename
       3032	    912	      0	   3944	    f68	realtek/rtlwifi/rtl8723be/sw.o
    
    File size After adding 'const':
       text	   data	    bss	    dec	    hex	filename
       3096	    848	      0	   3944	    f68	realtek/rtlwifi/rtl8723be/sw.o
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtlwifi/rtl8723be/sw.c
+++ drivers/net/wireless/realtek/rtlwifi/rtl8723be/sw.c
@@ -375,7 +375,7 @@ static const struct rtl_hal_cfg rtl8723be_hal_cfg = {
 	.maps[RTL_RC_HT_RATEMCS15] = DESC92C_RATEMCS15,
 };
 
-static struct pci_device_id rtl8723be_pci_ids[] = {
+static const struct pci_device_id rtl8723be_pci_ids[] = {
 	{RTL_PCI_DEVICE(PCI_VENDOR_ID_REALTEK, 0xB723, rtl8723be_hal_cfg)},
 	{},
 };
