e79dfb512110ce6759f139a286f0e1d6bc5e4c58
Imre Deak <imre.deak@intel.com>
2017-07-20 01:50:57 +0300

    drm/i915: Simplify scaler init during CRTC HW readout
    The crtc state starts out being bzero'd, so no need to clear
    scaler_users. Also intel_crtc_init_scalers() knows already which
    platforms have scalers, so no need for the platform check here.
    Similarly intel_crtc_init_scalers() will init scaler_id as required,
    so no need to do it here separately.
    
    Cc: Chandra Konduru <chandra.konduru@intel.com>
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Mahesh Kumar <mahesh1.kumar@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20170719225057.20131-2-imre.deak@intel.com
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -8989,12 +8989,7 @@ static bool haswell_get_pipe_config(struct intel_crtc *crtc,
 	u64 power_domain_mask;
 	bool active;
 
-	if (INTEL_GEN(dev_priv) >= 9) {
-		intel_crtc_init_scalers(crtc, pipe_config);
-
-		pipe_config->scaler_state.scaler_id = -1;
-		pipe_config->scaler_state.scaler_users &= ~(1 << SKL_CRTC_INDEX);
-	}
+	intel_crtc_init_scalers(crtc, pipe_config);
 
 	power_domain = POWER_DOMAIN_PIPE(crtc->pipe);
 	if (!intel_display_power_get_if_enabled(dev_priv, power_domain))
