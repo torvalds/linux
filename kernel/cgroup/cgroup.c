7a0cf0e74ab6cfd8e561f5f12860d4ff8844905a
Waiman Long <longman@redhat.com>
2017-07-21 11:14:51 -0400

    cgroup: update debug controller to print out thread mode information
    Update debug controller so that it prints out debug info about thread
    mode.
    
     1) The relationship between proc_cset and threaded_csets are displayed.
     2) The status of being a thread root or threaded cgroup is displayed.
    
    This patch is extracted from Waiman's larger patch.
    
    v2: - Removed [thread root] / [threaded] from debug.cgroup_css_links
          file as the same information is available from cgroup.type.
          Suggested by Waiman.
        - Threaded marking is moved to the previous patch.
    
    Patch-originally-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -333,7 +333,7 @@ static bool cgroup_has_tasks(struct cgroup *cgrp)
 	return cgrp->nr_populated_csets;
 }
 
-static bool cgroup_is_threaded(struct cgroup *cgrp)
+bool cgroup_is_threaded(struct cgroup *cgrp)
 {
 	return cgrp->dom_cgrp != cgrp;
 }
@@ -372,7 +372,7 @@ static bool cgroup_can_be_thread_root(struct cgroup *cgrp)
 }
 
 /* is @cgrp root of a threaded subtree? */
-static bool cgroup_is_thread_root(struct cgroup *cgrp)
+bool cgroup_is_thread_root(struct cgroup *cgrp)
 {
 	/* thread root should be a domain */
 	if (cgroup_is_threaded(cgrp))
