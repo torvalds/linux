bc86a41e96c5b6f07453c405e036d95acc673389
Mikulas Patocka <mpatocka@redhat.com>
2017-07-21 11:58:38 -0400

    dm integrity: test for corrupted disk format during table load
    If the dm-integrity superblock was corrupted in such a way that the
    journal_sections field was zero, the integrity target would deadlock
    because it would wait forever for free space in the journal.
    
    Detect this situation and refuse to activate the device.
    
    Signed-off-by: Mikulas Patocka <mpatocka@redhat.com>
    Cc: stable@vger.kernel.org
    Fixes: 7eada909bfd7 ("dm: add integrity target")
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>

--- drivers/md/dm-integrity.c
+++ drivers/md/dm-integrity.c
@@ -3028,6 +3028,11 @@ static int dm_integrity_ctr(struct dm_target *ti, unsigned argc, char **argv)
 		ti->error = "Block size doesn't match the information in superblock";
 		goto bad;
 	}
+	if (!le32_to_cpu(ic->sb->journal_sections)) {
+		r = -EINVAL;
+		ti->error = "Corrupted superblock, journal_sections is 0";
+		goto bad;
+	}
 	/* make sure that ti->max_io_len doesn't overflow */
 	if (ic->sb->log2_interleave_sectors < MIN_LOG2_INTERLEAVE_SECTORS ||
 	    ic->sb->log2_interleave_sectors > MAX_LOG2_INTERLEAVE_SECTORS) {
