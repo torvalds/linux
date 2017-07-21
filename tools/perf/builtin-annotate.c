ecd5f9959d2c9540482977ff1208ea67fbfb8cc9
Taeung Song <treeze.taeung@gmail.com>
2017-07-21 11:38:48 -0300

    perf annotate: Do not overwrite sample->period
    In fixing the --show-total-period option it was noticed that the value
    of sample->period was being overwritten, fix it.
    
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Milian Wolff <milian.wolff@kdab.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Fixes: fd36f3dd7933 ("perf hist: Pass struct sample to __hists__add_entry()")
    Link: http://lkml.kernel.org/r/1500500215-16646-1-git-send-email-treeze.taeung@gmail.com
    [ split from a larger patch, added the Fixes tag ]
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/builtin-annotate.c
+++ tools/perf/builtin-annotate.c
@@ -177,7 +177,6 @@ static int perf_evsel__add_sample(struct perf_evsel *evsel,
 	 */
 	process_branch_stack(sample->branch_stack, al, sample);
 
-	sample->period = 1;
 	sample->weight = 1;
 
 	he = hists__add_entry(hists, al, NULL, NULL, NULL, sample, true);
