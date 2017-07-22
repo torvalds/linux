6c7fce6fa86a110d6455662d823c4e09f8f7be4a
Sean Wang <sean.wang@mediatek.com>
2017-07-22 20:45:55 +0800

    net: ethernet: mediatek: avoid potential invalid memory access
    Potential dangerous invalid memory might be accessed if invalid mac value
    reflected from the forward port field in rxd4 caused by possible potential
    hardware defects. So added a simple sanity checker to avoid the kind of
    situation happening.
    
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Acked-by: John Crispin <john@phrozen.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mediatek/mtk_eth_soc.c
+++ drivers/net/ethernet/mediatek/mtk_eth_soc.c
@@ -947,6 +947,10 @@ static int mtk_poll_rx(struct napi_struct *napi, int budget,
 		      RX_DMA_FPORT_MASK;
 		mac--;
 
+		if (unlikely(mac < 0 || mac >= MTK_MAC_COUNT ||
+			     !eth->netdev[mac]))
+			goto release_desc;
+
 		netdev = eth->netdev[mac];
 
 		if (unlikely(test_bit(MTK_RESETTING, &eth->state)))
