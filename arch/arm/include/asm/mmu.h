50b2b2e691cd4ff30331ba9a6156b29a07b60f90
Nicolas Pitre <nicolas.pitre@linaro.org>
2017-07-22 01:02:40 -0400

    ARM: add ELF_FDPIC support
    This includes the necessary code to recognise the FDPIC format on ARM
    and the ptrace command definitions used by the common ptrace code.
    
    Based on patches originally from Mickael Guene <mickael.guene@st.com>.
    
    Signed-off-by: Nicolas Pitre <nico@linaro.org>
    Acked-by: Mickael GUENE <mickael.guene@st.com>
    Tested-by: Vincent Abriou <vincent.abriou@st.com>
    Tested-by: Andras Szemzo <szemzo.andras@gmail.com>

--- arch/arm/include/asm/mmu.h
+++ arch/arm/include/asm/mmu.h
@@ -33,6 +33,10 @@ typedef struct {
  */
 typedef struct {
 	unsigned long	end_brk;
+#ifdef CONFIG_BINFMT_ELF_FDPIC
+	unsigned long	exec_fdpic_loadmap;
+	unsigned long	interp_fdpic_loadmap;
+#endif
 } mm_context_t;
 
 #endif
