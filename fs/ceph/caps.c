b74fceae734dbd45f79b93bd262b03c39f538413
Jeff Layton <jlayton@redhat.com>
2017-07-25 10:50:41 -0400

    ceph: use errseq_t for writeback error reporting
    Ensure that when writeback errors are marked that we report those to all
    file descriptions that were open at the time of the error.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Reviewed-by: "Yan, Zheng" <zyan@redhat.com>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- fs/ceph/caps.c
+++ fs/ceph/caps.c
@@ -2113,7 +2113,7 @@ int ceph_fsync(struct file *file, loff_t start, loff_t end, int datasync)
 
 	dout("fsync %p%s\n", inode, datasync ? " datasync" : "");
 
-	ret = filemap_write_and_wait_range(inode->i_mapping, start, end);
+	ret = file_write_and_wait_range(file, start, end);
 	if (ret < 0)
 		goto out;
 
