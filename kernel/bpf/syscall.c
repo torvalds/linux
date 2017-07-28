9975a54b3c9ecf029cbf5dd7a8c9701b1d74029e
Daniel Borkmann <daniel@iogearbox.net>
2017-07-28 17:05:25 +0200

    bpf: fix bpf_prog_get_info_by_fd to dump correct xlated_prog_len
    bpf_prog_size(prog->len) is not the correct length we want to dump
    back to user space. The code in bpf_prog_get_info_by_fd() uses this
    to copy prog->insnsi to user space, but bpf_prog_size(prog->len) also
    includes the size of struct bpf_prog itself plus program instructions
    and is usually used either in context of accounting or for bpf_prog_alloc()
    et al, thus we copy out of bounds in bpf_prog_get_info_by_fd()
    potentially. Use the correct bpf_prog_insn_size() instead.
    
    Fixes: 1e2709769086 ("bpf: Add BPF_OBJ_GET_INFO_BY_FD")
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -1312,7 +1312,7 @@ static int bpf_prog_get_info_by_fd(struct bpf_prog *prog,
 	}
 
 	ulen = info.xlated_prog_len;
-	info.xlated_prog_len = bpf_prog_size(prog->len);
+	info.xlated_prog_len = bpf_prog_insn_size(prog);
 	if (info.xlated_prog_len && ulen) {
 		uinsns = u64_to_user_ptr(info.xlated_prog_insns);
 		ulen = min_t(u32, info.xlated_prog_len, ulen);
