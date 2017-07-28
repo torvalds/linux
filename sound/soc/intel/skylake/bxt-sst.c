3fbbcf4dadb6c01d6411bac715a7448f96c3724c
Dronamraju Santosh P K <santosh.pavan.kumarx.dronamraju@intel.com>
2017-07-28 16:12:14 +0530

    ASoC: Intel: bxtn: Remove code loader reference in cleanup
    Since Broxton does not use code loader DMA, remove code loader cleanup
    in its dsp cleanup routine. Remove the iounmap too as it is done in
    skl_free_dsp().
    
    Signed-off-by: Dronamraju Santosh P K <santosh.pavan.kumarx.dronamraju@intel.com>
    Signed-off-by: Guneshwor Singh <guneshwor.o.singh@intel.com>
    Acked-By: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/skylake/bxt-sst.c
+++ sound/soc/intel/skylake/bxt-sst.c
@@ -629,11 +629,6 @@ void bxt_sst_dsp_cleanup(struct device *dev, struct skl_sst *ctx)
 		release_firmware(ctx->dsp->fw);
 	skl_freeup_uuid_list(ctx);
 	skl_ipc_free(&ctx->ipc);
-	ctx->dsp->cl_dev.ops.cl_cleanup_controller(ctx->dsp);
-
-	if (ctx->dsp->addr.lpe)
-		iounmap(ctx->dsp->addr.lpe);
-
 	ctx->dsp->ops->free(ctx->dsp);
 }
 EXPORT_SYMBOL_GPL(bxt_sst_dsp_cleanup);
