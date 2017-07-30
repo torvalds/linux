9558df3a8251f1c636eaf3d2222b6da2eb5d1086
Andrew Lunn <andrew@lunn.ch>
2017-07-30 22:11:06 +0200

    net: fec: Issue error for missing but expected PHY
    If the PHY is missing but expected, e.g. because of a typ0 in the dt
    file, it is not possible to open the interface. ip link returns:
    
    RTNETLINK answers: No such device
    
    It is not very obvious what the problem is. Add a netdev_err() in this
    case to make it easier to debug the issue.
    
    [   21.409385] fec 2188000.ethernet eth0: Unable to connect to phy
    RTNETLINK answers: No such device
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Acked-by: Fugang Duan <fugang.duan@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -1904,8 +1904,10 @@ static int fec_enet_mii_probe(struct net_device *ndev)
 		phy_dev = of_phy_connect(ndev, fep->phy_node,
 					 &fec_enet_adjust_link, 0,
 					 fep->phy_interface);
-		if (!phy_dev)
+		if (!phy_dev) {
+			netdev_err(ndev, "Unable to connect to phy\n");
 			return -ENODEV;
+		}
 	} else {
 		/* check for attached phy */
 		for (phy_id = 0; (phy_id < PHY_MAX_ADDR); phy_id++) {
