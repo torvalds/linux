ed10eab5f52e466f83176ce7780d45f4dfefe6d3
Lynn Lei <lynnl.yet@gmail.com>
2017-08-01 17:20:38 +0200

    video: fbdev: sm712fb.c: using __func__ macro for pr_debug
    Inside function sm7xx_vga_setup()
    
    Using a generic function macro for debug information display
    
    Signed-off-by: Lynn Lei <lynnl.wit@gmail.com>
    Cc: Sudip Mukherjee <sudipm.mukherjee@gmail.com>
    Cc: Teddy Wang <teddy.wang@siliconmotion.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>

--- drivers/video/fbdev/sm712fb.c
+++ drivers/video/fbdev/sm712fb.c
@@ -785,7 +785,7 @@ static void __init sm7xx_vga_setup(char *options)
 	smtc_scr_info.lfb_height = 0;
 	smtc_scr_info.lfb_depth = 0;
 
-	pr_debug("sm7xx_vga_setup = %s\n", options);
+	pr_debug("%s = %s\n", __func__, options);
 
 	for (i = 0; i < ARRAY_SIZE(vesa_mode_table); i++) {
 		if (strstr(options, vesa_mode_table[i].index)) {
