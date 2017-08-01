f4ef19108608c81769db69976999d056c070a6f0
Wanpeng Li <wanpeng.li@hotmail.com>
2017-08-01 16:05:25 -0700

    KVM: X86: Fix loss of pending INIT due to race
    When SMP VM start, AP may lost INIT because of receiving INIT between
    kvm_vcpu_ioctl_x86_get/set_vcpu_events.
    
           vcpu 0                             vcpu 1
                                       kvm_vcpu_ioctl_x86_get_vcpu_events
                                         events->smi.latched_init = 0
      send INIT to vcpu1
        set vcpu1's pending_events
                                       kvm_vcpu_ioctl_x86_set_vcpu_events
                                          if (events->smi.latched_init == 0)
                                            clear INIT in pending_events
    
    This patch fixes it by just update SMM related flags if we are in SMM.
    
    Thanks Peng Hao for the report and original commit message.
    
    Reported-by: Peng Hao <peng.hao2@zte.com.cn>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Radim Krčmář <rkrcmar@redhat.com>
    Signed-off-by: Wanpeng Li <wanpeng.li@hotmail.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -3159,15 +3159,18 @@ static int kvm_vcpu_ioctl_x86_set_vcpu_events(struct kvm_vcpu *vcpu,
 		kvm_set_hflags(vcpu, hflags);
 
 		vcpu->arch.smi_pending = events->smi.pending;
-		if (events->smi.smm_inside_nmi)
-			vcpu->arch.hflags |= HF_SMM_INSIDE_NMI_MASK;
-		else
-			vcpu->arch.hflags &= ~HF_SMM_INSIDE_NMI_MASK;
-		if (lapic_in_kernel(vcpu)) {
-			if (events->smi.latched_init)
-				set_bit(KVM_APIC_INIT, &vcpu->arch.apic->pending_events);
+
+		if (events->smi.smm) {
+			if (events->smi.smm_inside_nmi)
+				vcpu->arch.hflags |= HF_SMM_INSIDE_NMI_MASK;
 			else
-				clear_bit(KVM_APIC_INIT, &vcpu->arch.apic->pending_events);
+				vcpu->arch.hflags &= ~HF_SMM_INSIDE_NMI_MASK;
+			if (lapic_in_kernel(vcpu)) {
+				if (events->smi.latched_init)
+					set_bit(KVM_APIC_INIT, &vcpu->arch.apic->pending_events);
+				else
+					clear_bit(KVM_APIC_INIT, &vcpu->arch.apic->pending_events);
+			}
 		}
 	}
 
