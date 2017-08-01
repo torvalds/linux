f9fb9d0b8519a795eeaca5108205593b66068cce
Jack Morgenstein <jackm@dev.mellanox.co.il>
2017-08-01 16:43:45 +0300

    net/mlx4_core: Fix namespace misalignment in QinQ VST support commit
    The cited commit introduced the following new enum value in file
    include/linux/mlx4/device.h:
    
        MLX4_DEV_CAP_FLAG2_SVLAN_BY_QP
    
    However the value of MLX4_DEV_CAP_FLAG2_SVLAN_BY_QP needs to stay
    consistent with the value used in another namespace in
    function dump_dev_cap_flags2(), which is manually kept in sync.
    The change here restores that consistency.
    
    Fixes: 7c3d21c8153c ("net/mlx4_core: Preparation for VF vlan protocol 802.1ad")
    Reported-by: Mukesh Kacker <mukesh.kacker@oracle.com>
    Signed-off-by: Jack Morgenstein <jackm@dev.mellanox.co.il>
    Signed-off-by: Tariq Toukan <tariqt@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlx4/fw.c
+++ drivers/net/ethernet/mellanox/mlx4/fw.c
@@ -159,7 +159,7 @@ static void dump_dev_cap_flags2(struct mlx4_dev *dev, u64 flags)
 		[32] = "Loopback source checks support",
 		[33] = "RoCEv2 support",
 		[34] = "DMFS Sniffer support (UC & MC)",
-		[35] = "QinQ VST mode support",
+		[36] = "QinQ VST mode support",
 		[37] = "sl to vl mapping table change event support",
 	};
 	int i;
