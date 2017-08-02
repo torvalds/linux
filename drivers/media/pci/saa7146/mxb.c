34e7b45a01ef914cf6d5d49e0968ad013e221376
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-08-02 13:14:55 -0400

    media: saa7146: mxb: constify pci_device_id
    pci_device_id are not supposed to change at runtime. All functions
    working with pci_device_id provided by <media/drv-intf/saa7146.h>
    and <linux/pci.h> work with const pci_device_id. So mark the non-const
    structs as const.
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/pci/saa7146/mxb.c
+++ drivers/media/pci/saa7146/mxb.c
@@ -819,7 +819,7 @@ static struct saa7146_pci_extension_data mxb = {
 	.ext = &extension,
 };
 
-static struct pci_device_id pci_tbl[] = {
+static const struct pci_device_id pci_tbl[] = {
 	{
 		.vendor    = PCI_VENDOR_ID_PHILIPS,
 		.device	   = PCI_DEVICE_ID_PHILIPS_SAA7146,
