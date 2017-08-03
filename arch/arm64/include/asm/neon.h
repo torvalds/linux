0fc9179ad0bf2f97790c0568442299679ca346cf
Dave Martin <Dave.Martin@arm.com>
2017-08-03 17:23:20 +0100

    arm64: neon: Add missing header guard in <asm/neon.h>
    asm/neon.h doesn't have a header inclusion guard, but it should
    have one for consistency with other headers.
    
    This patch adds a suitable guard.
    
    Signed-off-by: Dave Martin <Dave.Martin@arm.com>
    Reviewed-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/neon.h
+++ arch/arm64/include/asm/neon.h
@@ -8,6 +8,9 @@
  * published by the Free Software Foundation.
  */
 
+#ifndef __ASM_NEON_H
+#define __ASM_NEON_H
+
 #include <linux/types.h>
 #include <asm/fpsimd.h>
 
@@ -17,3 +20,5 @@
 
 void kernel_neon_begin_partial(u32 num_regs);
 void kernel_neon_end(void);
+
+#endif /* ! __ASM_NEON_H */
