35a84dec00a707aed97c1ff9ebb1cd1eb67c7052
Shih-Wei Li <shihwei@cs.columbia.edu>
2017-08-03 11:45:21 +0200

    KVM: arm64: Move HCR_INT_OVERRIDE to default HCR_EL2 guest flag
    We always set the IMO and FMO bits in the HCR_EL2 when running the
    guest, regardless if we use the vgic or not.  By moving these flags to
    HCR_GUEST_FLAGS we can avoid one of the extra save/restore operations of
    HCR_EL2 in the world switch code, and we can also soon get rid of the
    other one.
    
    This is safe, because even though the IMO and FMO bits control both
    taking the interrupts to EL2 and remapping ICC_*_EL1 to ICV_*_EL1 when
    executed at EL1, as long as we ensure that these bits are clear when
    running the EL1 host, we're OK, because we reset the HCR_EL2 to only
    have the HCR_RW bit set when returning to EL1 on non-VHE systems.
    
    Reviewed-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Shih-Wei Li <shihwei@cs.columbia.edu>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm64/kvm/hyp/switch.c
+++ arch/arm64/kvm/hyp/switch.c
@@ -174,8 +174,6 @@ static void __hyp_text __vgic_save_state(struct kvm_vcpu *vcpu)
 		__vgic_v3_save_state(vcpu);
 	else
 		__vgic_v2_save_state(vcpu);
-
-	write_sysreg(read_sysreg(hcr_el2) & ~HCR_INT_OVERRIDE, hcr_el2);
 }
 
 static void __hyp_text __vgic_restore_state(struct kvm_vcpu *vcpu)
@@ -183,7 +181,6 @@ static void __hyp_text __vgic_restore_state(struct kvm_vcpu *vcpu)
 	u64 val;
 
 	val = read_sysreg(hcr_el2);
-	val |= 	HCR_INT_OVERRIDE;
 	val |= vcpu->arch.irq_lines;
 	write_sysreg(val, hcr_el2);
 
