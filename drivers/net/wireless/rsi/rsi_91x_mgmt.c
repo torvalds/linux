6345016d4bea6a82300c209d99e7f593800e3b4e
Prameela Rani Garnepudi <prameela.j04cs@gmail.com>
2017-08-03 19:56:39 +0530

    rsi: fix uninitialized descriptor pointer issue
    This patch fixes the uninitialized descriptor pointer
    issue in function rsi_send_internal_mgmt_frame().
    Descriptor should point to start of the skb data.
    
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Fixes: 9a629fafe7d8 ("rsi: immediate wakeup bit and priority for TX command packets")
    Signed-off-by: Prameela Rani Garnepudi <prameela.j04cs@gmail.com>
    Signed-off-by: Amitkumar Karwar <amit.karwar@redpinesignals.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/rsi/rsi_91x_mgmt.c
+++ drivers/net/wireless/rsi/rsi_91x_mgmt.c
@@ -274,6 +274,7 @@ static int rsi_send_internal_mgmt_frame(struct rsi_common *common,
 		rsi_dbg(ERR_ZONE, "%s: Unable to allocate skb\n", __func__);
 		return -ENOMEM;
 	}
+	desc = (struct rsi_cmd_desc *)skb->data;
 	desc->desc_dword0.len_qno |= cpu_to_le16(DESC_IMMEDIATE_WAKEUP);
 	skb->priority = MGMT_SOFT_Q;
 	tx_params = (struct skb_info *)&IEEE80211_SKB_CB(skb)->driver_data;
