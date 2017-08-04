79e21d65409385a57d2c77aeb95825a28a8058c7
Eric W. Biederman <ebiederm@xmission.com>
2017-08-04 14:01:50 -0500

    signal/x86/traps: Move more code into do_trap_no_signal so it can be reused
    The function do_trap_no_signal embodies almost all of the work of the
    function do_trap.  The exceptions are setting of thread.error_code and
    thread.trap_nr in the case when the signal will be sent, and reporting
    which signal will be sent with show_signal.
    
    Filling in struct siginfo and then calling do_trap is problematic as
    filling in struct siginfo is an fiddly process that can through
    inattention has resulted in fields not initialized and the wrong
    fields being filled in.
    
    To avoid this error prone situation I am replacing force_sig_info with
    a set of functions that take as arguments the information needed to
    send a specific kind of signal.
    
    The function do_trap is called in the context of several different
    kinds of signals today.  Having a solid do_trap_no_signal that
    can be reused allows call sites that send different kinds of
    signals to reuse all of the code in do_trap_no_signal.
    
    Modify do_trap_no_signal to have a single exit there signals
    where be sent (aka returning -1) to allow more of the signal
    sending path to be moved to from do_trap to do_trap_no_signal.
    
    Move setting thread.trap_nr and thread.error_code into do_trap_no_signal
    so the code does not need to be duplicated.
    
    Make the type of the string that is passed into do_trap_no_signal to
    const.  The only user of that str is die and it already takes a const
    string, so this just makes it explicit that the string won't change.
    
    All of this prepares the way for using do_trap_no_signal outside
    of do_trap.
    
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- arch/x86/kernel/traps.c
+++ arch/x86/kernel/traps.c
@@ -189,7 +189,7 @@ int fixup_bug(struct pt_regs *regs, int trapnr)
 }
 
 static nokprobe_inline int
-do_trap_no_signal(struct task_struct *tsk, int trapnr, char *str,
+do_trap_no_signal(struct task_struct *tsk, int trapnr, const char *str,
 		  struct pt_regs *regs,	long error_code)
 {
 	if (v8086_mode(regs)) {
@@ -202,10 +202,7 @@ do_trap_no_signal(struct task_struct *tsk, int trapnr, char *str,
 						error_code, trapnr))
 				return 0;
 		}
-		return -1;
-	}
-
-	if (!user_mode(regs)) {
+	} else if (!user_mode(regs)) {
 		if (fixup_exception(regs, trapnr))
 			return 0;
 
@@ -213,6 +210,18 @@ do_trap_no_signal(struct task_struct *tsk, int trapnr, char *str,
 		tsk->thread.trap_nr = trapnr;
 		die(str, regs, error_code);
 	}
+
+	/*
+	 * We want error_code and trap_nr set for userspace faults and
+	 * kernelspace faults which result in die(), but not
+	 * kernelspace faults which are fixed up.  die() gives the
+	 * process no chance to handle the signal and notice the
+	 * kernel fault information, so that won't result in polluting
+	 * the information about previously queued, but not yet
+	 * delivered, faults.  See also do_general_protection below.
+	 */
+	tsk->thread.error_code = error_code;
+	tsk->thread.trap_nr = trapnr;
 
 	return -1;
 }
@@ -271,17 +280,6 @@ do_trap(int trapnr, int signr, char *str, struct pt_regs *regs,
 
 	if (!do_trap_no_signal(tsk, trapnr, str, regs, error_code))
 		return;
-	/*
-	 * We want error_code and trap_nr set for userspace faults and
-	 * kernelspace faults which result in die(), but not
-	 * kernelspace faults which are fixed up.  die() gives the
-	 * process no chance to handle the signal and notice the
-	 * kernel fault information, so that won't result in polluting
-	 * the information about previously queued, but not yet
-	 * delivered, faults.  See also do_general_protection below.
-	 */
-	tsk->thread.error_code = error_code;
-	tsk->thread.trap_nr = trapnr;
 
 	show_signal(tsk, signr, "trap ", str, regs, error_code);
 
