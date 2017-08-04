319af40a0053fea2ecadcf10bef7e796c91ea8c0
Finley Xiao <finley.xiao@rock-chips.com>
2017-08-04 09:52:31 +0800

    cpufreq: dt: Add rk3328 compatible to use generic cpufreq driver
    This patch adds the rk3328 compatible string for supporting
    the generic cpufreq driver on RK3328.
    
    Signed-off-by: Finley Xiao <finley.xiao@rock-chips.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -76,6 +76,7 @@ static const struct of_device_id machines[] __initconst = {
 	{ .compatible = "rockchip,rk3188", },
 	{ .compatible = "rockchip,rk3228", },
 	{ .compatible = "rockchip,rk3288", },
+	{ .compatible = "rockchip,rk3328", },
 	{ .compatible = "rockchip,rk3366", },
 	{ .compatible = "rockchip,rk3368", },
 	{ .compatible = "rockchip,rk3399", },
