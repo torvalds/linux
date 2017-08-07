dfc8e9165cfe0b50438f51df5a82ab1dcc08baf4
Arnd Bergmann <arnd@arndb.de>
2017-08-07 17:22:14 +0200

    fbcon: mark dummy functions 'inline'
    The newly introduced fb_console_init/exit declarations have a
    dummy alternative that is marked 'static' in the header but not
    inline, leading to a warning whenever the header gets included:
    
    In file included from drivers/video/fbdev/core/fbmem.c:35:0:
    include/linux/fbcon.h:9:13: error: 'fb_console_exit' defined but not used [-Werror=unused-function]
    
    This adds the intended 'inline'.
    
    Fixes: 6104c37094e7 ("fbcon: Make fbcon a built-time depency for fbdev")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Sean Paul <seanpaul@chromium.org>
    Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>

--- include/linux/fbcon.h
+++ include/linux/fbcon.h
@@ -5,8 +5,8 @@
 void __init fb_console_init(void);
 void __exit fb_console_exit(void);
 #else
-static void fb_console_init(void) {}
-static void fb_console_exit(void) {}
+static inline void fb_console_init(void) {}
+static inline void fb_console_exit(void) {}
 #endif
 
 #endif /* _LINUX_FBCON_H */
