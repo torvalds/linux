b123718b105bb837a0463823404505b04d8f0586
Ralf Baechle <ralf@linux-mips.org>
2017-08-07 21:14:18 +0200

    MIPS: signal: Remove unreachable code from force_fcr31_sig().
    Based on discussion with Linus remove the impossible to reach code
    rather than replacing it with a BUG().
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Link: http://lkml.kernel.org/r/20170718140651.15973-4-ebiederm@xmission.com

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -734,8 +734,7 @@ void force_fcr31_sig(unsigned long fcr31, void __user *fault_addr,
 		si.si_code = FPE_FLTUND;
 	else if (fcr31 & FPU_CSR_INE_X)
 		si.si_code = FPE_FLTRES;
-	else
-		si.si_code = __SI_FAULT;
+
 	force_sig_info(SIGFPE, &si, tsk);
 }
 
