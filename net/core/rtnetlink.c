8515ae38435895ba2862840d3e82140fc0a77554
Florian Westphal <fw@strlen.de>
2017-08-10 16:52:59 +0200

    rtnetlink: switch rtnl_link_get_slave_info_data_size to rcu
    David Ahern reports following splat:
     RTNL: assertion failed at net/core/dev.c (5717)
     netdev_master_upper_dev_get+0x5f/0x70
     if_nlmsg_size+0x158/0x240
     rtnl_calcit.isra.26+0xa3/0xf0
    
    rtnl_link_get_slave_info_data_size currently assumes RTNL protection, but
    there appears to be no hard requirement for this, so use rcu instead.
    
    At the time of this writing, there are three 'get_slave_size' callbacks
    (now invoked under rcu): bond_get_slave_size, vrf_get_slave_size and
    br_port_get_slave_size, all return constant only (i.e. they don't sleep).
    
    Fixes: 6853dd488119 ("rtnetlink: protect handler table with rcu")
    Reported-by: David Ahern <dsahern@gmail.com>
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Acked-by: David Ahern <dsahern@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -402,16 +402,24 @@ static size_t rtnl_link_get_slave_info_data_size(const struct net_device *dev)
 {
 	struct net_device *master_dev;
 	const struct rtnl_link_ops *ops;
+	size_t size = 0;
 
-	master_dev = netdev_master_upper_dev_get((struct net_device *) dev);
+	rcu_read_lock();
+
+	master_dev = netdev_master_upper_dev_get_rcu((struct net_device *)dev);
 	if (!master_dev)
-		return 0;
+		goto out;
+
 	ops = master_dev->rtnl_link_ops;
 	if (!ops || !ops->get_slave_size)
-		return 0;
+		goto out;
 	/* IFLA_INFO_SLAVE_DATA + nested data */
-	return nla_total_size(sizeof(struct nlattr)) +
+	size = nla_total_size(sizeof(struct nlattr)) +
 	       ops->get_slave_size(master_dev, dev);
+
+out:
+	rcu_read_unlock();
+	return size;
 }
 
 static size_t rtnl_link_get_size(const struct net_device *dev)
