2cf5a5e6adba1021a608f58fcedce7201d671086
Alex Deucher <alexdeucher@gmail.com>
2017-08-10 14:22:22 -0400

    drm/amd/display/dc: make dce120_link_encoder_create static
    Only used in dce120_resource.c.
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dce120/dce120_resource.c
+++ drivers/gpu/drm/amd/display/dc/dce120/dce120_resource.c
@@ -535,7 +535,7 @@ static const struct encoder_feature_support link_enc_feature = {
 		.flags.bits.IS_YCBCR_CAPABLE = true
 };
 
-struct link_encoder *dce120_link_encoder_create(
+static struct link_encoder *dce120_link_encoder_create(
 	const struct encoder_init_data *enc_init_data)
 {
 	struct dce110_link_encoder *enc110 =
