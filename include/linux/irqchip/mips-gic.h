16ae123e89d603a69d980bd76c4bb686f219ba0e
Paul Burton <paul.burton@imgtec.com>
2017-08-12 21:36:37 -0700

    MIPS: VDSO: Avoid use of linux/irqchip/mips-gic.h
    Our VDSO code makes use of macros from linux/irqchip/mips-gic.h to
    provide offsets to register values, but these are trivial offsets to the
    two 32 bit halves of a 64 bit value. Replace use of the macros with zero
    (ie. omit adding an offset) and the size of the low 32 bit of the value.
    This removes our need for linux/irqchip/mips-gic.h & prepares us for it
    to be removed.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>
    Acked-by: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Jason Cooper <jason@lakedaemon.net>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/17047/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/linux/irqchip/mips-gic.h
+++ include/linux/irqchip/mips-gic.h
@@ -11,8 +11,4 @@
 #include <linux/clocksource.h>
 #include <linux/ioport.h>
 
-/* User Mode Visible Section Register Map */
-#define GIC_UMV_SH_COUNTER_31_00_OFS	0x0000
-#define GIC_UMV_SH_COUNTER_63_32_OFS	0x0004
-
 #endif /* __LINUX_IRQCHIP_MIPS_GIC_H */
