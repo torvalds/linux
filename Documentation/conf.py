0e4c2b75897c6a621546b4f11a42c6072e833f81
Markus Heiser <markus.heiser@darmarit.de>
2017-08-14 10:53:33 +0200

    docs: fix minimal sphinx version in conf.py
    according to what Documentation/doc-guide/sphinx.rst says::
    
      The ReST markups currently used by the Documentation/ files
      are meant to be built with ``Sphinx`` version 1.3 or upper.
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>
    Signed-off-by: Jonathan Corbet <corbet@lwn.net>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -29,7 +29,7 @@ from load_config import loadConfig
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.2'
+needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
