42271d951c1f9a6a3ae0392d245256d973b2fc58
Bhumika Goyal <bhumirks@gmail.com>
2017-08-14 17:08:49 +0530

    ASoC: txx9: make snd_soc_platform_driver const
    Make this const as it is only passed as the 2nd argument to the function
    devm_snd_soc_register_platform, which is of type const.
    Done using Coccinelle.
    
    Signed-off-by: Bhumika Goyal <bhumirks@gmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/txx9/txx9aclc.c
+++ sound/soc/txx9/txx9aclc.c
@@ -403,7 +403,7 @@ static int txx9aclc_pcm_remove(struct snd_soc_platform *platform)
 	return 0;
 }
 
-static struct snd_soc_platform_driver txx9aclc_soc_platform = {
+static const struct snd_soc_platform_driver txx9aclc_soc_platform = {
 	.probe		= txx9aclc_pcm_probe,
 	.remove		= txx9aclc_pcm_remove,
 	.ops		= &txx9aclc_pcm_ops,
