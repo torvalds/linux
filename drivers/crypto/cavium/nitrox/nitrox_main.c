32e67b9af077e905388f1746bf429ea0c39b6917
Christophe Jaillet <christophe.jaillet@wanadoo.fr>
2017-08-16 07:16:06 +0200

    crypto: cavium/nitrox - Fix an error handling path in 'nitrox_probe()'
    'err' is known to be 0 at this point.
    If 'kzalloc()' fails, returns -ENOMEM instead of 0 which means success.
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/cavium/nitrox/nitrox_main.c
+++ drivers/crypto/cavium/nitrox/nitrox_main.c
@@ -513,8 +513,10 @@ static int nitrox_probe(struct pci_dev *pdev,
 	pci_set_master(pdev);
 
 	ndev = kzalloc(sizeof(*ndev), GFP_KERNEL);
-	if (!ndev)
+	if (!ndev) {
+		err = -ENOMEM;
 		goto ndev_fail;
+	}
 
 	pci_set_drvdata(pdev, ndev);
 	ndev->pdev = pdev;
