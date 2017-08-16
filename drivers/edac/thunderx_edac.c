3eaef0fa39f7badb0bf25bb28ab96cf06b29d45c
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2017-08-16 06:58:21 +0200

    EDAC, thunderx: Fix error handling path in thunderx_lmc_probe()
    Return the proper error value if ioremap() fails (and not 0).
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Cc: David Daney <david.daney@cavium.com>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: linux-edac <linux-edac@vger.kernel.org>
    Cc: linux-mips@linux-mips.org
    Link: http://lkml.kernel.org/r/20170816045821.14165-1-christophe.jaillet@wanadoo.fr
    [ Massage commit message, remove newline. ]
    Signed-off-by: Borislav Petkov <bp@suse.de>

--- drivers/edac/thunderx_edac.c
+++ drivers/edac/thunderx_edac.c
@@ -774,11 +774,10 @@ static int thunderx_lmc_probe(struct pci_dev *pdev,
 
 	lmc->xor_bank = lmc_control & LMC_CONTROL_XOR_BANK;
 
-	l2c_ioaddr = ioremap(L2C_CTL | FIELD_PREP(THUNDERX_NODE, lmc->node),
-			     PAGE_SIZE);
-
+	l2c_ioaddr = ioremap(L2C_CTL | FIELD_PREP(THUNDERX_NODE, lmc->node), PAGE_SIZE);
 	if (!l2c_ioaddr) {
 		dev_err(&pdev->dev, "Cannot map L2C_CTL\n");
+		ret = -ENOMEM;
 		goto err_free;
 	}
 
