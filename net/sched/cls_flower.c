d978db8dbebb60a51492da966689dfc0c9216c44
Jiri Pirko <jiri@mellanox.com>
2017-08-16 17:15:18 +0200

    net: sched: cls_flower: fix ndo_setup_tc type for stats call
    I made a stupid mistake using TC_CLSFLOWER_STATS instead of
    TC_SETUP_CLSFLOWER. Funny thing is that both are defined as "2" so it
    actually did not cause any harm. Anyway, fixing it now.
    
    Fixes: 2572ac53c46f ("net: sched: make type an argument for ndo_setup_tc")
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/cls_flower.c
+++ net/sched/cls_flower.c
@@ -289,7 +289,7 @@ static void fl_hw_update_stats(struct tcf_proto *tp, struct cls_fl_filter *f)
 	cls_flower.cookie = (unsigned long) f;
 	cls_flower.exts = &f->exts;
 
-	dev->netdev_ops->ndo_setup_tc(dev, TC_CLSFLOWER_STATS,
+	dev->netdev_ops->ndo_setup_tc(dev, TC_SETUP_CLSFLOWER,
 				      &cls_flower);
 }
 
