8031c3ddc70ab93099e7d1814382dba39f57b43e
Shaohua Li <shli@fb.com>
2017-08-17 10:35:11 -0700

    md/bitmap: copy correct data for bitmap super
    raid5 cache could write bitmap superblock before bitmap superblock is
    initialized. The bitmap superblock is less than 512B. The current code will
    only copy the superblock to a new page and write the whole 512B, which will
    zero the the data after the superblock. Unfortunately the data could include
    bitmap, which we should preserve. The patch will make superblock read do 4k
    chunk and we always copy the 4k data to new page, so the superblock write will
    old data to disk and we don't change the bitmap.
    
    Reported-by: Song Liu <songliubraving@fb.com>
    Reviewed-by: Song Liu <songliubraving@fb.com>
    Cc: stable@vger.kernel.org (4.10+)
    Signed-off-by: Shaohua Li <shli@fb.com>

--- drivers/md/bitmap.c
+++ drivers/md/bitmap.c
@@ -625,7 +625,7 @@ re_read:
 		err = read_sb_page(bitmap->mddev,
 				   offset,
 				   sb_page,
-				   0, sizeof(bitmap_super_t));
+				   0, PAGE_SIZE);
 	}
 	if (err)
 		return err;
@@ -2118,7 +2118,7 @@ int bitmap_resize(struct bitmap *bitmap, sector_t blocks,
 	if (store.sb_page && bitmap->storage.sb_page)
 		memcpy(page_address(store.sb_page),
 		       page_address(bitmap->storage.sb_page),
-		       sizeof(bitmap_super_t));
+		       PAGE_SIZE);
 	bitmap_file_unmap(&bitmap->storage);
 	bitmap->storage = store;
 
