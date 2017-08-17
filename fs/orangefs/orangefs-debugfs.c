5e273a0e06ee5a50bb9cd40026177feca060c101
Markus Elfring <elfring@users.sourceforge.net>
2017-08-17 21:18:01 +0200

    orangefs: Use kcalloc() in orangefs_prepare_cdm_array()
    * A multiplication for the size determination of a memory allocation
      indicated that an array data structure should be processed.
      Thus use the corresponding function "kcalloc".
    
      This issue was detected by using the Coccinelle software.
    
    * Replace the specification of a data structure by a pointer dereference
      to make the corresponding size determination a bit safer according to
      the Linux coding style convention.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Signed-off-by: Mike Marshall <hubcap@omnibond.com>

--- fs/orangefs/orangefs-debugfs.c
+++ fs/orangefs/orangefs-debugfs.c
@@ -571,9 +571,7 @@ static int orangefs_prepare_cdm_array(char *debug_array_string)
 		goto out;
 	}
 
-	cdm_array =
-		kzalloc(cdm_element_count * sizeof(struct client_debug_mask),
-			GFP_KERNEL);
+	cdm_array = kcalloc(cdm_element_count, sizeof(*cdm_array), GFP_KERNEL);
 	if (!cdm_array) {
 		rc = -ENOMEM;
 		goto out;
