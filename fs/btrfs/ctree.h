4335958de2a43c6790c7f6aa0682aa7189983fa4
Liu Bo <bo.li.liu@oracle.com>
2017-08-18 15:15:20 -0600

    Btrfs: remove BUG() in btrfs_extent_inline_ref_size
    Now that btrfs_get_extent_inline_ref_type() can report if type is a
    valid one and all callers can gracefully deal with that, we don't need
    to crash here.
    
    Signed-off-by: Liu Bo <bo.li.liu@oracle.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -1804,7 +1804,6 @@ static inline u32 btrfs_extent_inline_ref_size(int type)
 	if (type == BTRFS_EXTENT_DATA_REF_KEY)
 		return sizeof(struct btrfs_extent_data_ref) +
 		       offsetof(struct btrfs_extent_inline_ref, offset);
-	BUG();
 	return 0;
 }
 
