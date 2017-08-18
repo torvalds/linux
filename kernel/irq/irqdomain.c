0d12ec075a18f53e6f58ec95a4f534da2641bf9b
David Daney <david.daney@cavium.com>
2017-08-17 17:53:33 -0700

    irqdomain: Check for NULL function pointer in irq_domain_free_irqs_hierarchy()
    A follow-on patch will call irq_domain_free_irqs_hierarchy() when the
    free() function pointer may be NULL.
    
    Add a NULL pointer check to handle this new use case.
    
    Signed-off-by: David Daney <david.daney@cavium.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Alexandre Courbot <gnurou@gmail.com>
    Cc: Linus Walleij <linus.walleij@linaro.org>
    Cc: linux-gpio@vger.kernel.org
    Link: http://lkml.kernel.org/r/1503017616-3252-5-git-send-email-david.daney@cavium.com

--- kernel/irq/irqdomain.c
+++ kernel/irq/irqdomain.c
@@ -1362,7 +1362,8 @@ static void irq_domain_free_irqs_hierarchy(struct irq_domain *domain,
 					   unsigned int irq_base,
 					   unsigned int nr_irqs)
 {
-	domain->ops->free(domain, irq_base, nr_irqs);
+	if (domain->ops->free)
+		domain->ops->free(domain, irq_base, nr_irqs);
 }
 
 int irq_domain_alloc_irqs_hierarchy(struct irq_domain *domain,
