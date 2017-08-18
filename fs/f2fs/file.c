9071bb1c094f70ab08253b9335e62b8a79e20b15
Chao Yu <yuchao0@huawei.com>
2017-08-18 23:37:36 +0800

    f2fs: clear FI_HOT_DATA correctly
    This patch fixes to clear FI_HOT_DATA correctly in below path:
    - error handling in f2fs_ioc_start_atomic_write
    - after commit atomic write in f2fs_ioc_commit_atomic_write
    - after drop atomic write in drop_inmem_pages
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -1617,6 +1617,7 @@ static int f2fs_ioc_start_atomic_write(struct file *filp)
 	ret = filemap_write_and_wait_range(inode->i_mapping, 0, LLONG_MAX);
 	if (ret) {
 		clear_inode_flag(inode, FI_ATOMIC_FILE);
+		clear_inode_flag(inode, FI_HOT_DATA);
 		goto out;
 	}
 
@@ -1655,6 +1656,7 @@ static int f2fs_ioc_commit_atomic_write(struct file *filp)
 		ret = f2fs_do_sync_file(filp, 0, LLONG_MAX, 0, true);
 		if (!ret) {
 			clear_inode_flag(inode, FI_ATOMIC_FILE);
+			clear_inode_flag(inode, FI_HOT_DATA);
 			stat_dec_atomic_write(inode);
 		}
 	} else {
