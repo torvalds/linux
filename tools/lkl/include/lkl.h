f8a7327b8e9acdc3afd9230f3f0a50e2a9187380
Octavian Purdila <tavi@cs.pub.ro>
2017-08-18 18:42:15 +0300

    lkl: export MSG_DONWAIT, ip and icmp headers
    Signed-off-by: Octavian Purdila <tavi@cs.pub.ro>

--- tools/lkl/include/lkl.h
+++ tools/lkl/include/lkl.h
@@ -10,7 +10,18 @@ extern "C" {
 #ifdef __cplusplus
 #define class __lkl__class
 #endif
+
+/*
+ * Avoid collisions between Android which defines __unused and
+ * linux/icmp.h which uses __unused as a structure field.
+ */
+#pragma push_macro("__unused")
+#undef __unused
+
 #include <lkl/asm/syscalls.h>
+
+#pragma pop_macro("__unused")
+
 #ifdef __cplusplus
 #undef class
 #endif
