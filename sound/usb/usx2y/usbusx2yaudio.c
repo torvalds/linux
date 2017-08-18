31cb1fb41df3b871d337544ed5199a135e606f69
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-08-18 13:15:21 +0530

    ALSA: usb: constify snd_pcm_ops structures
    snd_pcm_ops are not supposed to change at runtime. All functions
    working with snd_pcm_ops provided by <sound/pcm.h> work with
    const snd_pcm_ops. So mark the non-const structs as const.
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/usb/usx2y/usbusx2yaudio.c
+++ sound/usb/usx2y/usbusx2yaudio.c
@@ -905,7 +905,7 @@ static int snd_usX2Y_pcm_close(struct snd_pcm_substream *substream)
 }
 
 
-static struct snd_pcm_ops snd_usX2Y_pcm_ops = 
+static const struct snd_pcm_ops snd_usX2Y_pcm_ops =
 {
 	.open =		snd_usX2Y_pcm_open,
 	.close =	snd_usX2Y_pcm_close,
