51218298a25e6942957c5595f2abf130d47d5df9
Alexandre Belloni <alexandre.belloni@free-electrons.com>
2017-08-21 00:01:46 +0200

    alarmtimer: Ensure RTC module is not unloaded
    When registering the rtc device to be used to handle alarm timers,
    get_device is used to ensure the device doesn't go away but the module can
    still be unloaded.
    
    Call try_module_get to ensure the rtc driver will not go away.
    
    Reported-and-tested-by: Michal Simek <monstr@monstr.eu>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: John Stultz <john.stultz@linaro.org>
    Cc: Stephen Boyd <sboyd@codeaurora.org>
    Link: http://lkml.kernel.org/r/20170820220146.30969-1-alexandre.belloni@free-electrons.com

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -28,6 +28,7 @@
 #include <linux/workqueue.h>
 #include <linux/freezer.h>
 #include <linux/compat.h>
+#include <linux/module.h>
 
 #include "posix-timers.h"
 
@@ -103,6 +104,11 @@ static int alarmtimer_rtc_add_device(struct device *dev,
 
 	spin_lock_irqsave(&rtcdev_lock, flags);
 	if (!rtcdev) {
+		if (!try_module_get(rtc->owner)) {
+			spin_unlock_irqrestore(&rtcdev_lock, flags);
+			return -1;
+		}
+
 		rtcdev = rtc;
 		/* hold a reference so it doesn't go away */
 		get_device(dev);
