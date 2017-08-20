1bee324a5627ab05ff9899709a613b8739813eda
Linus Walleij <linus.walleij@linaro.org>
2017-08-20 23:39:07 +0200

    mmc: ops: export mmc_get_status()
    This function retrieves the status of the card with the default
    number of retries. Since the block layer wants to use this, and
    since the block layer is a loadable kernel module, we need to
    export this symbol.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/core/mmc_ops.c
+++ drivers/mmc/core/mmc_ops.c
@@ -83,6 +83,7 @@ int mmc_send_status(struct mmc_card *card, u32 *status)
 {
 	return __mmc_send_status(card, status, MMC_CMD_RETRIES);
 }
+EXPORT_SYMBOL_GPL(mmc_send_status);
 
 static int _mmc_select_card(struct mmc_host *host, struct mmc_card *card)
 {
