f0e72c29fd063aca325afee25a6f9bcc22871837
Daniel Scheller <d.scheller@gmx.net>
2017-08-20 06:29:14 -0400

    media: dvb-frontends/stv0910: fix FE_HAS_LOCK check order in tune()
    It should first read the status and then check if FE has lock.
    
    Signed-off-by: Daniel Scheller <d.scheller@gmx.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/dvb-frontends/stv0910.c
+++ drivers/media/dvb-frontends/stv0910.c
@@ -1581,13 +1581,15 @@ static int tune(struct dvb_frontend *fe, bool re_tune,
 			return r;
 		state->tune_time = jiffies;
 	}
-	if (*status & FE_HAS_LOCK)
-		return 0;
-	*delay = HZ;
 
 	r = read_status(fe, status);
 	if (r)
 		return r;
+
+	if (*status & FE_HAS_LOCK)
+		return 0;
+	*delay = HZ;
+
 	return 0;
 }
 
