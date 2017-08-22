432295d7b906dae11129fc2870289465c907e5f2
Chris Wilson <chris@chris-wilson.co.uk>
2017-08-22 12:05:15 +0100

    drm/i915: Assert the context is not closed on object-close
    During the context-close, we should be decoupling all the vma from the
    object so that upon object-closing we shouldn't see any vma from the
    already closed contexts. So include a check upon closing the object that
    the context is still open.
    
    v2: Eek, the fpriv check is required for shared objects. Double eek, BAT
    passed?
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20170822110517.22277-1-chris@chris-wilson.co.uk
    Reviewed-by: Michał Winiarski <michal.winiarski@intel.com>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -3253,11 +3253,11 @@ void i915_gem_close_object(struct drm_gem_object *gem, struct drm_file *file)
 		struct i915_gem_context *ctx = lut->ctx;
 		struct i915_vma *vma;
 
+		GEM_BUG_ON(ctx->file_priv == ERR_PTR(-EBADF));
 		if (ctx->file_priv != fpriv)
 			continue;
 
 		vma = radix_tree_delete(&ctx->handles_vma, lut->handle);
-
 		if (!i915_vma_is_ggtt(vma))
 			i915_vma_close(vma);
 
