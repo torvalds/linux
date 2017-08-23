1c56cd8ee945abdf955ad28a1f5c7576cd288be2
Michael Ellerman <mpe@ellerman.id.au>
2017-08-23 23:56:22 +1000

    powerpc/oops: Use IS_ENABLED() for oops markers
    Just because it looks less gross.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/kernel/traps.c
+++ arch/powerpc/kernel/traps.c
@@ -208,17 +208,18 @@ static int __die(const char *str, struct pt_regs *regs, long err)
 	else
 		printk("BE ");
 
-#ifdef CONFIG_PREEMPT
-	pr_cont("PREEMPT ");
-#endif
-#ifdef CONFIG_SMP
-	pr_cont("SMP NR_CPUS=%d ", NR_CPUS);
-#endif
+	if (IS_ENABLED(CONFIG_PREEMPT))
+		pr_cont("PREEMPT ");
+
+	if (IS_ENABLED(CONFIG_SMP))
+		pr_cont("SMP NR_CPUS=%d ", NR_CPUS);
+
 	if (debug_pagealloc_enabled())
 		pr_cont("DEBUG_PAGEALLOC ");
-#ifdef CONFIG_NUMA
-	pr_cont("NUMA ");
-#endif
+
+	if (IS_ENABLED(CONFIG_NUMA))
+		pr_cont("NUMA ");
+
 	pr_cont("%s\n", ppc_md.name ? ppc_md.name : "");
 
 	if (notify_die(DIE_OOPS, str, regs, err, 255, SIGSEGV) == NOTIFY_STOP)
