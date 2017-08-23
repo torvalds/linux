03fef6c5c2296ee6d43f59af57973e82c229039c
Arnd Bergmann <arnd@arndb.de>
2017-08-23 15:22:53 +0200

    staging: rtlwifi: simplify logical operation
    gcc notices a very complicated way to check a value
    for being equal to one, and warns about it:
    
    drivers/staging/rtlwifi/btcoexist/halbtc8822b1ant.c: In function 'halbtc8822b1ant_set_ext_ant_switch':
    drivers/staging/rtlwifi/btcoexist/halbtc8822b1ant.c:2105:9: error: '~' on a boolean expression [-Werror=bool-operation]
             ~switch_polatiry_inverse :
             ^
    drivers/staging/rtlwifi/btcoexist/halbtc8822b1ant.c:2105:9: note: did you mean to use logical not?
             ~switch_polatiry_inverse :
             ^
    
    This simplifies this expression to make it more readable
    and to make gcc happy.
    
    Fixes: 56bde846304e ("staging: r8822be: Add existing rtlwifi and rtl_pci parts for new driver")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtlwifi/btcoexist/halbtc8822b1ant.c
+++ drivers/staging/rtlwifi/btcoexist/halbtc8822b1ant.c
@@ -2101,9 +2101,7 @@ static void halbtc8822b1ant_set_ext_ant_switch(struct btc_coexist *btcoexist,
 	 * 0xcbd[1:0] = 2b'01 => Ant to BTG,
 	 * 0xcbd[1:0] = 2b'10 => Ant to WLG
 	 */
-	switch_polatiry_inverse = (rfe_type->ext_ant_switch_ctrl_polarity == 1 ?
-					   ~switch_polatiry_inverse :
-					   switch_polatiry_inverse);
+	switch_polatiry_inverse = rfe_type->ext_ant_switch_ctrl_polarity == 1;
 
 	switch (pos_type) {
 	default:
