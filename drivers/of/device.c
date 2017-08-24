08ab58d9de3eb8498ae0585001d0975e46217a39
Bjorn Andersson <bjorn.andersson@linaro.org>
2017-08-23 18:04:04 -0700

    of/device: Prevent buffer overflow in of_device_modalias()
    As of_device_get_modalias() returns the number of bytes that would have
    been written to the target string, regardless of how much did fit in the
    buffer, it's possible that the returned index points beyond the buffer
    passed to of_device_modalias() - causing memory beyond the buffer to be
    null terminated.
    
    Fixes: 0634c2958927 ("of: Add function for generating a DT modalias with a newline")
    Cc: Rob Herring <robh@kernel.org>
    Cc: stable@vger.kernel.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- drivers/of/device.c
+++ drivers/of/device.c
@@ -257,6 +257,8 @@ ssize_t of_device_modalias(struct device *dev, char *str, ssize_t len)
 	ssize_t sl = of_device_get_modalias(dev, str, len - 2);
 	if (sl < 0)
 		return sl;
+	if (sl > len - 2)
+		return -ENOMEM;
 
 	str[sl++] = '\n';
 	str[sl] = 0;
