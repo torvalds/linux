56d859e11aad4016e1cf864d65a0954d83120571
Pavel Tatashin <pasha.tatashin@oracle.com>
2017-08-25 18:06:04 -0400

    HID: multitouch: support buttons and trackpoint on Lenovo X1 Tab Gen2
    On the 2nd generation Lenovo Tablet only clickpad is working; the
    trackpoint and three mouse buttons do not work.
    
    hid_multitouch must export all inputs in order to get trackpoint and
    buttons to function.
    
    Signed-off-by: Pavel Tatashin <pasha.tatashin@oracle.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -1420,6 +1420,12 @@ static const struct hid_device_id mt_devices[] = {
 			USB_VENDOR_ID_ALPS_JP,
 			HID_DEVICE_ID_ALPS_U1_DUAL_3BTN_PTP) },
 
+	/* Lenovo X1 TAB Gen 2 */
+	{ .driver_data = MT_CLS_WIN_8_DUAL,
+		HID_DEVICE(BUS_USB, HID_GROUP_MULTITOUCH_WIN_8,
+			   USB_VENDOR_ID_LENOVO,
+			   USB_DEVICE_ID_LENOVO_X1_TAB) },
+
 	/* Anton devices */
 	{ .driver_data = MT_CLS_EXPORT_ALL_INPUTS,
 		MT_USB_DEVICE(USB_VENDOR_ID_ANTON,
