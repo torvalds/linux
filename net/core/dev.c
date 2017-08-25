1e22391e8fbec9c3709bad82b997b108d1c6228b
Jesper Dangaard Brouer <brouer@redhat.com>
2017-08-25 15:04:32 +0200

    net: missing call of trace_napi_poll in busy_poll_stop
    Noticed that busy_poll_stop() also invoke the drivers napi->poll()
    function pointer, but didn't have an associated call to trace_napi_poll()
    like all other call sites.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -5289,6 +5289,7 @@ static void busy_poll_stop(struct napi_struct *napi, void *have_poll_lock)
 	 * Ideally, a new ndo_busy_poll_stop() could avoid another round.
 	 */
 	rc = napi->poll(napi, BUSY_POLL_BUDGET);
+	trace_napi_poll(napi, rc, BUSY_POLL_BUDGET);
 	netpoll_poll_unlock(have_poll_lock);
 	if (rc == BUSY_POLL_BUDGET)
 		__napi_schedule(napi);
