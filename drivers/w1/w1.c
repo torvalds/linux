f6887531c0c6b106dbe9aa5ed6eaa166d93df52e
Andrew Worsley <amworsley@gmail.com>
2017-08-27 21:34:23 +1000

    drivers: w1: Add 1w slave driver for DS28E05 EEPROM
    Add a one wire driver for the DS28E05 one wire slave chip. This chip
    requires OverDrive support to talk to it.
    
    Signed-off-by: Andrew Worsley <amworsley@gmail.com>
    Acked-by: Evgeniy Polyakov <zbr@ioremap.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/w1/w1.c
+++ drivers/w1/w1.c
@@ -729,6 +729,8 @@ int w1_attach_slave_device(struct w1_master *dev, struct w1_reg_num *rn)
 	atomic_set(&sl->refcnt, 1);
 	atomic_inc(&sl->master->refcnt);
 	dev->slave_count++;
+	dev_info(&dev->dev, "Attaching one wire slave %02x.%012llx crc %02x\n",
+		  rn->family, (unsigned long long)rn->id, rn->crc);
 
 	/* slave modules need to be loaded in a context with unlocked mutex */
 	mutex_unlock(&dev->mutex);
