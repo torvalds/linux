d11dd6cdc3883f3c74f841f4d40dfe57c0b9756c
Davide Caratti <dcaratti@redhat.com>
2017-08-28 11:52:07 +0200

    netfilter: conntrack: remove unused code in nf_conntrack_proto_generic.c
    L4 protocol helpers for DCCP, SCTP and UDPlite can't be built as kernel
    modules anymore, so we can remove code enclosed in
     #ifdef CONFIG_NF_CT_PROTO_{DCCP,SCTP,UDPLITE}_MODULE
    
    Signed-off-by: Davide Caratti <dcaratti@redhat.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nf_conntrack_proto_generic.c
+++ net/netfilter/nf_conntrack_proto_generic.c
@@ -17,20 +17,8 @@ static unsigned int nf_ct_generic_timeout __read_mostly = 600*HZ;
 static bool nf_generic_should_process(u8 proto)
 {
 	switch (proto) {
-#ifdef CONFIG_NF_CT_PROTO_SCTP_MODULE
-	case IPPROTO_SCTP:
-		return false;
-#endif
-#ifdef CONFIG_NF_CT_PROTO_DCCP_MODULE
-	case IPPROTO_DCCP:
-		return false;
-#endif
 #ifdef CONFIG_NF_CT_PROTO_GRE_MODULE
 	case IPPROTO_GRE:
-		return false;
-#endif
-#ifdef CONFIG_NF_CT_PROTO_UDPLITE_MODULE
-	case IPPROTO_UDPLITE:
 		return false;
 #endif
 	default:
