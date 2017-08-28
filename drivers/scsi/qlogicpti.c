fbf252335f8e2bd39d4ce59b1db0836a40ab227b
Hannes Reinecke <hare@suse.de>
2017-08-28 10:14:26 +0200

    scsi: qlogicpti: fixup qlogicpti_reset() definition
    A merge error crept in when formatting commit af167bc ("scsi: qlogicpti:
    move bus reset to host reset")
    
    Fixes: af167bc ("scsi: qlogicpti: move bus reset to host reset")
    Signed-off-by: Hannes Reinecke <hare@suse.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/qlogicpti.c
+++ drivers/scsi/qlogicpti.c
@@ -1250,9 +1250,10 @@ static int qlogicpti_abort(struct scsi_cmnd *Cmnd)
 	return return_status;
 }
 
-static int qlogicpti_reset(struct Scsi_Host *host)
+static int qlogicpti_reset(struct scsi_cmnd *Cmnd)
 {
 	u_short param[6];
+	struct Scsi_Host *host = Cmnd->device->host;
 	struct qlogicpti *qpti = (struct qlogicpti *) host->hostdata;
 	int return_status = SUCCESS;
 
