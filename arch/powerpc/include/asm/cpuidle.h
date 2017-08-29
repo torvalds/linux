aafc8a8300ec0f20b99c72e5a10e22535cb99dd1
Nicholas Piggin <npiggin@gmail.com>
2017-08-29 21:36:35 +1000

    powerpc/64s: Move IDLE_STATE_ENTER_SEQ[_NORET] into idle_book3s.S
    This macro is only used in idle_book3s.S, move it in there and add a
    more descriptive comment.
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    [mpe: Split out of larger patch and write change log]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/cpuidle.h
+++ arch/powerpc/include/asm/cpuidle.h
@@ -101,20 +101,4 @@ static inline void report_invalid_psscr_val(u64 psscr_val, int err)
 
 #endif
 
-/* Idle state entry routines */
-#ifdef	CONFIG_PPC_P7_NAP
-#define IDLE_STATE_ENTER_SEQ(IDLE_INST)                         \
-	/* Magic NAP/SLEEP/WINKLE mode enter sequence */	\
-	std	r0,0(r1);					\
-	ptesync;						\
-	ld	r0,0(r1);					\
-236:	cmpd	cr0,r0,r0;					\
-	bne	236b;						\
-	IDLE_INST;						\
-
-#define	IDLE_STATE_ENTER_SEQ_NORET(IDLE_INST)			\
-	IDLE_STATE_ENTER_SEQ(IDLE_INST)                         \
-	b	.
-#endif /* CONFIG_PPC_P7_NAP */
-
 #endif
