c16a854e4463078aedad601fac76341760a66dd1
Biju Das <biju.das@bp.renesas.com>
2017-08-29 14:52:06 +0100

    mmc: renesas_sdhi: Add r8a7743/5 support
    Add support for r8a7743/5 SoC.Renesas RZ/G1[ME] (R8A7743/5) SDHI
    is identical to the R-Car Gen2 family.
    
    Signed-off-by: Biju Das <biju.das@bp.renesas.com>
    Acked-by: Simon Horman <horms+renesas@verge.net.au>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/renesas_sdhi_sys_dmac.c
+++ drivers/mmc/host/renesas_sdhi_sys_dmac.c
@@ -98,6 +98,8 @@ static const struct of_device_id renesas_sdhi_sys_dmac_of_match[] = {
 	{ .compatible = "renesas,sdhi-r7s72100", .data = &of_rz_compatible, },
 	{ .compatible = "renesas,sdhi-r8a7778", .data = &of_rcar_gen1_compatible, },
 	{ .compatible = "renesas,sdhi-r8a7779", .data = &of_rcar_gen1_compatible, },
+	{ .compatible = "renesas,sdhi-r8a7743", .data = &of_rcar_gen2_compatible, },
+	{ .compatible = "renesas,sdhi-r8a7745", .data = &of_rcar_gen2_compatible, },
 	{ .compatible = "renesas,sdhi-r8a7790", .data = &of_rcar_gen2_compatible, },
 	{ .compatible = "renesas,sdhi-r8a7791", .data = &of_rcar_gen2_compatible, },
 	{ .compatible = "renesas,sdhi-r8a7792", .data = &of_rcar_gen2_compatible, },
