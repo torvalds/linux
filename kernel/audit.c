b3b4fdf6a8ae9ea51ea274e3f2f8a6a58b98cc0b
Paul Moore <paul@paul-moore.com>
2017-09-01 09:44:57 -0400

    audit: convert audit_ever_enabled to a boolean
    We were treating it as a boolean, let's make it a boolean to help
    avoid future mistakes.
    
    Reviewed-by: Richard Guy Briggs <rgb@redhat.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- kernel/audit.c
+++ kernel/audit.c
@@ -86,7 +86,7 @@ static int	audit_initialized;
 #define AUDIT_ON	1
 #define AUDIT_LOCKED	2
 u32		audit_enabled = AUDIT_OFF;
-u32		audit_ever_enabled = !!AUDIT_OFF;
+bool		audit_ever_enabled = !!AUDIT_OFF;
 
 EXPORT_SYMBOL_GPL(audit_enabled);
 
