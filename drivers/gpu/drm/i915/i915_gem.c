6910d85250cd6e7790db3968225fd7472e4683c2
Ville Syrjälä <ville.syrjala@linux.intel.com>
2017-09-01 20:12:51 +0300

    drm/i915: Add __rcu to radix tree slot pointer
    radix_tree_for_each_slot() wants an __rcu annotated pointer for the
    slot. So let's add the annotation.
    
    Fixes the following sparse warnings:
    i915_gem.c:2217:9: warning: incorrect type in assignment (different address spaces)
    i915_gem.c:2217:9:    expected void **slot
    i915_gem.c:2217:9:    got void [noderef] <asn:4>**
    i915_gem.c:2217:9: warning: incorrect type in assignment (different address spaces)
    i915_gem.c:2217:9:    expected void **slot
    i915_gem.c:2217:9:    got void [noderef] <asn:4>**
    i915_gem.c:2217:9: warning: incorrect type in argument 1 (different address spaces)
    i915_gem.c:2217:9:    expected void [noderef] <asn:4>**slot
    i915_gem.c:2217:9:    got void **slot
    i915_gem.c:2217:9: warning: incorrect type in assignment (different address spaces)
    i915_gem.c:2217:9:    expected void **slot
    i915_gem.c:2217:9:    got void [noderef] <asn:4>**
    
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Fixes: 96d776345277 ("drm/i915: Use a radixtree for random access to the object's backing storage")
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20170901171252.31025-1-ville.syrjala@linux.intel.com
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    (cherry picked from commit c23aa71bcfe8a9d597ae5fe4c1527fac20254d0a)
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -2212,7 +2212,7 @@ i915_gem_object_put_pages_gtt(struct drm_i915_gem_object *obj,
 static void __i915_gem_object_reset_page_iter(struct drm_i915_gem_object *obj)
 {
 	struct radix_tree_iter iter;
-	void **slot;
+	void __rcu **slot;
 
 	radix_tree_for_each_slot(slot, &obj->mm.get_page.radix, &iter, 0)
 		radix_tree_delete(&obj->mm.get_page.radix, iter.index);
