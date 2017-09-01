831c4c81e8ceba7d34b7fc66da1693bc4b9e7e11
Sean Young <sean@mess.org>
2017-09-01 09:55:59 -0300

    [media] media: rc: ensure that protocols are enabled for scancode drivers
    rc scancode drivers without change_protocol should have all
    protocols enabled at all time. This was only true for cec and
    ir-kbd-i2c.
    
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/cec/cec-core.c
+++ drivers/media/cec/cec-core.c
@@ -277,7 +277,6 @@ struct cec_adapter *cec_allocate_adapter(const struct cec_adap_ops *ops,
 	adap->rc->input_id.version = 1;
 	adap->rc->driver_name = CEC_NAME;
 	adap->rc->allowed_protocols = RC_PROTO_BIT_CEC;
-	adap->rc->enabled_protocols = RC_PROTO_BIT_CEC;
 	adap->rc->priv = adap;
 	adap->rc->map_name = RC_MAP_CEC;
 	adap->rc->timeout = MS_TO_NS(100);
