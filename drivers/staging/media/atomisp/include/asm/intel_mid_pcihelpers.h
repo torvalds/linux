5fc90b632ea9a107ed6b71eb7058bb58e1b5493b
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2017-09-01 09:36:34 -0400

    media: staging: atomisp: Remove dead code for MID (#1)
    Remove dead code. If someone needs it the P-Unit semaphore is handled by
    I2C DesignWare driver (drivers/i2c/busses/i2c-designware-baytrail.c).
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/staging/media/atomisp/include/asm/intel_mid_pcihelpers.h
+++ drivers/staging/media/atomisp/include/asm/intel_mid_pcihelpers.h
@@ -33,5 +33,3 @@ void intel_mid_msgbus_write32(u8 port, u32 addr, u32 data);
 u32 intel_mid_msgbus_read32_raw_ext(u32 cmd, u32 cmd_ext);
 void intel_mid_msgbus_write32_raw_ext(u32 cmd, u32 cmd_ext, u32 data);
 u32 intel_mid_soc_stepping(void);
-int intel_mid_dw_i2c_acquire_ownership(void);
-int intel_mid_dw_i2c_release_ownership(void);
