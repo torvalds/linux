c88bcd63eb3fd10413849b1c1ef8c376b7ab4ecd
Olliver Schinagl <oliver@schinagl.nl>
2017-09-04 10:03:43 +0200

    drm/sun4i: i2c: consolidate ddc items
    The ddc items can be consolidated more into one place.
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

--- drivers/gpu/drm/sun4i/sun4i_hdmi_enc.c
+++ drivers/gpu/drm/sun4i/sun4i_hdmi_enc.c
@@ -30,6 +30,7 @@
 #include "sun4i_crtc.h"
 #include "sun4i_drv.h"
 #include "sun4i_hdmi.h"
+#include "sun4i_hdmi_i2c.h"
 
 static inline struct sun4i_hdmi *
 drm_encoder_to_sun4i_hdmi(struct drm_encoder *encoder)
@@ -308,24 +309,6 @@ static const struct sun4i_hdmi_variant sun4i_variant = {
 				  SUN4I_HDMI_PLL_CTRL_BWS |
 				  SUN4I_HDMI_PLL_CTRL_PLL_EN,
 
-	.ddc_clk_reg		= REG_FIELD(SUN4I_HDMI_DDC_CLK_REG, 0, 6),
-	.ddc_clk_pre_divider	= 2,
-	.ddc_clk_m_offset	= 1,
-
-	.field_ddc_en		= REG_FIELD(SUN4I_HDMI_DDC_CTRL_REG, 31, 31),
-	.field_ddc_start	= REG_FIELD(SUN4I_HDMI_DDC_CTRL_REG, 30, 30),
-	.field_ddc_reset	= REG_FIELD(SUN4I_HDMI_DDC_CTRL_REG, 0, 0),
-	.field_ddc_addr_reg	= REG_FIELD(SUN4I_HDMI_DDC_ADDR_REG, 0, 31),
-	.field_ddc_slave_addr	= REG_FIELD(SUN4I_HDMI_DDC_ADDR_REG, 0, 6),
-	.field_ddc_int_status	= REG_FIELD(SUN4I_HDMI_DDC_INT_STATUS_REG, 0, 8),
-	.field_ddc_fifo_clear	= REG_FIELD(SUN4I_HDMI_DDC_FIFO_CTRL_REG, 31, 31),
-	.field_ddc_fifo_rx_thres = REG_FIELD(SUN4I_HDMI_DDC_FIFO_CTRL_REG, 4, 7),
-	.field_ddc_fifo_tx_thres = REG_FIELD(SUN4I_HDMI_DDC_FIFO_CTRL_REG, 0, 3),
-	.field_ddc_byte_count	= REG_FIELD(SUN4I_HDMI_DDC_BYTE_COUNT_REG, 0, 9),
-	.field_ddc_cmd		= REG_FIELD(SUN4I_HDMI_DDC_CMD_REG, 0, 2),
-	.field_ddc_sda_en	= REG_FIELD(SUN4I_HDMI_DDC_LINE_CTRL_REG, 9, 9),
-	.field_ddc_sck_en	= REG_FIELD(SUN4I_HDMI_DDC_LINE_CTRL_REG, 8, 8),
-
 	.ddc_fifo_reg		= SUN4I_HDMI_DDC_FIFO_DATA_REG,
 	.ddc_fifo_has_dir	= true,
 };
@@ -358,27 +341,6 @@ static const struct sun4i_hdmi_variant sun5i_variant = {
 				  SUN4I_HDMI_PLL_CTRL_HV_IS_33 |
 				  SUN4I_HDMI_PLL_CTRL_BWS |
 				  SUN4I_HDMI_PLL_CTRL_PLL_EN,
-
-	.ddc_clk_reg		= REG_FIELD(SUN4I_HDMI_DDC_CLK_REG, 0, 6),
-	.ddc_clk_pre_divider	= 2,
-	.ddc_clk_m_offset	= 1,
-
-	.field_ddc_en		= REG_FIELD(SUN4I_HDMI_DDC_CTRL_REG, 31, 31),
-	.field_ddc_start	= REG_FIELD(SUN4I_HDMI_DDC_CTRL_REG, 30, 30),
-	.field_ddc_reset	= REG_FIELD(SUN4I_HDMI_DDC_CTRL_REG, 0, 0),
-	.field_ddc_addr_reg	= REG_FIELD(SUN4I_HDMI_DDC_ADDR_REG, 0, 31),
-	.field_ddc_slave_addr	= REG_FIELD(SUN4I_HDMI_DDC_ADDR_REG, 0, 6),
-	.field_ddc_int_status	= REG_FIELD(SUN4I_HDMI_DDC_INT_STATUS_REG, 0, 8),
-	.field_ddc_fifo_clear	= REG_FIELD(SUN4I_HDMI_DDC_FIFO_CTRL_REG, 31, 31),
-	.field_ddc_fifo_rx_thres = REG_FIELD(SUN4I_HDMI_DDC_FIFO_CTRL_REG, 4, 7),
-	.field_ddc_fifo_tx_thres = REG_FIELD(SUN4I_HDMI_DDC_FIFO_CTRL_REG, 0, 3),
-	.field_ddc_byte_count	= REG_FIELD(SUN4I_HDMI_DDC_BYTE_COUNT_REG, 0, 9),
-	.field_ddc_cmd		= REG_FIELD(SUN4I_HDMI_DDC_CMD_REG, 0, 2),
-	.field_ddc_sda_en	= REG_FIELD(SUN4I_HDMI_DDC_LINE_CTRL_REG, 9, 9),
-	.field_ddc_sck_en	= REG_FIELD(SUN4I_HDMI_DDC_LINE_CTRL_REG, 8, 8),
-
-	.ddc_fifo_reg		= SUN4I_HDMI_DDC_FIFO_DATA_REG,
-	.ddc_fifo_has_dir	= true,
 };
 
 static const struct sun4i_hdmi_variant sun6i_variant = {
@@ -414,28 +376,7 @@ static const struct sun4i_hdmi_variant sun6i_variant = {
 				  SUN4I_HDMI_PLL_CTRL_HV_IS_33 |
 				  SUN4I_HDMI_PLL_CTRL_PLL_EN,
 
-	.ddc_clk_reg		= REG_FIELD(SUN6I_HDMI_DDC_CLK_REG, 0, 6),
-	.ddc_clk_pre_divider	= 1,
-	.ddc_clk_m_offset	= 2,
-
 	.tmds_clk_div_offset	= 1,
-
-	.field_ddc_en		= REG_FIELD(SUN6I_HDMI_DDC_CTRL_REG, 0, 0),
-	.field_ddc_start	= REG_FIELD(SUN6I_HDMI_DDC_CTRL_REG, 27, 27),
-	.field_ddc_reset	= REG_FIELD(SUN6I_HDMI_DDC_CTRL_REG, 31, 31),
-	.field_ddc_addr_reg	= REG_FIELD(SUN6I_HDMI_DDC_ADDR_REG, 1, 31),
-	.field_ddc_slave_addr	= REG_FIELD(SUN6I_HDMI_DDC_ADDR_REG, 1, 7),
-	.field_ddc_int_status	= REG_FIELD(SUN6I_HDMI_DDC_INT_STATUS_REG, 0, 8),
-	.field_ddc_fifo_clear	= REG_FIELD(SUN6I_HDMI_DDC_FIFO_CTRL_REG, 18, 18),
-	.field_ddc_fifo_rx_thres = REG_FIELD(SUN6I_HDMI_DDC_FIFO_CTRL_REG, 4, 7),
-	.field_ddc_fifo_tx_thres = REG_FIELD(SUN6I_HDMI_DDC_FIFO_CTRL_REG, 0, 3),
-	.field_ddc_byte_count	= REG_FIELD(SUN6I_HDMI_DDC_CMD_REG, 16, 25),
-	.field_ddc_cmd		= REG_FIELD(SUN6I_HDMI_DDC_CMD_REG, 0, 2),
-	.field_ddc_sda_en	= REG_FIELD(SUN6I_HDMI_DDC_CTRL_REG, 6, 6),
-	.field_ddc_sck_en	= REG_FIELD(SUN6I_HDMI_DDC_CTRL_REG, 4, 4),
-
-	.ddc_fifo_reg		= SUN6I_HDMI_DDC_FIFO_DATA_REG,
-	.ddc_fifo_thres_incl	= true,
 };
 
 static const struct regmap_config sun4i_hdmi_regmap_config = {
--- drivers/gpu/drm/sun4i/sun4i_hdmi_i2c.c
+++ drivers/gpu/drm/sun4i/sun4i_hdmi_i2c.c
@@ -13,20 +13,126 @@
 #include <linux/iopoll.h>
 
 #include "sun4i_hdmi.h"
-
-#define SUN4I_HDMI_DDC_INT_STATUS_ERROR_MASK ( \
-	SUN4I_HDMI_DDC_INT_STATUS_ILLEGAL_FIFO_OPERATION | \
-	SUN4I_HDMI_DDC_INT_STATUS_DDC_RX_FIFO_UNDERFLOW | \
-	SUN4I_HDMI_DDC_INT_STATUS_DDC_TX_FIFO_OVERFLOW | \
-	SUN4I_HDMI_DDC_INT_STATUS_ARBITRATION_ERROR | \
-	SUN4I_HDMI_DDC_INT_STATUS_ACK_ERROR | \
-	SUN4I_HDMI_DDC_INT_STATUS_BUS_ERROR \
-)
+#include "sun4i_hdmi_i2c.h"
 
 /* FIFO request bit is set when FIFO level is above RX_THRESHOLD during read */
 #define RX_THRESHOLD SUN4I_HDMI_DDC_FIFO_CTRL_RX_THRES_MAX
 
-static int fifo_transfer(struct sun4i_hdmi *hdmi, u8 *buf, int len, bool read)
+struct sun4i_hdmi_i2c_variant {
+	struct reg_field ddc_clk_reg;
+	u8 ddc_clk_pre_divider;
+	u8 ddc_clk_m_offset;
+
+	/* Register fields for I2C adapter */
+	struct reg_field	field_ddc_en;
+	struct reg_field	field_ddc_start;
+	struct reg_field	field_ddc_reset;
+	struct reg_field	field_ddc_addr_reg;
+	struct reg_field	field_ddc_slave_addr;
+	struct reg_field	field_ddc_int_mask;
+	struct reg_field	field_ddc_int_status;
+	struct reg_field	field_ddc_fifo_clear;
+	struct reg_field	field_ddc_fifo_rx_thres;
+	struct reg_field	field_ddc_fifo_tx_thres;
+	struct reg_field	field_ddc_byte_count;
+	struct reg_field	field_ddc_cmd;
+	struct reg_field	field_ddc_sda_en;
+	struct reg_field	field_ddc_sck_en;
+
+	/* DDC FIFO register offset */
+	u32			ddc_fifo_reg;
+
+	/*
+	 * DDC FIFO threshold boundary conditions
+	 *
+	 * This is used to cope with the threshold boundary condition
+	 * being slightly different on sun5i and sun6i.
+	 *
+	 * On sun5i the threshold is exclusive, i.e. does not include,
+	 * the value of the threshold. ( > for RX; < for TX )
+	 * On sun6i the threshold is inclusive, i.e. includes, the
+	 * value of the threshold. ( >= for RX; <= for TX )
+	 */
+	bool			ddc_fifo_thres_incl;
+
+	bool			ddc_fifo_has_dir;
+};
+
+struct sun4i_hdmi_i2c_drv {
+	struct device		*dev;
+
+	void __iomem		*base;
+	struct regmap		*regmap;
+
+	struct clk		*ddc_clk;
+
+	struct i2c_adapter	adap;
+
+	struct regmap_field	*field_ddc_en;
+	struct regmap_field	*field_ddc_start;
+	struct regmap_field	*field_ddc_reset;
+	struct regmap_field	*field_ddc_addr_reg;
+	struct regmap_field	*field_ddc_slave_addr;
+	struct regmap_field	*field_ddc_int_mask;
+	struct regmap_field	*field_ddc_int_status;
+	struct regmap_field	*field_ddc_fifo_clear;
+	struct regmap_field	*field_ddc_fifo_rx_thres;
+	struct regmap_field	*field_ddc_fifo_tx_thres;
+	struct regmap_field	*field_ddc_byte_count;
+	struct regmap_field	*field_ddc_cmd;
+	struct regmap_field	*field_ddc_sda_en;
+	struct regmap_field	*field_ddc_sck_en;
+
+	const struct sun4i_hdmi_i2c_variant	*variant;
+};
+
+static const struct sun4i_hdmi_i2c_variant sun4i_variant = {
+	.ddc_clk_reg		= REG_FIELD(SUN4I_HDMI_DDC_CLK_REG, 0, 6),
+	.ddc_clk_pre_divider	= 2,
+	.ddc_clk_m_offset	= 1,
+
+	.field_ddc_en		= REG_FIELD(SUN4I_HDMI_DDC_CTRL_REG, 31, 31),
+	.field_ddc_start	= REG_FIELD(SUN4I_HDMI_DDC_CTRL_REG, 30, 30),
+	.field_ddc_reset	= REG_FIELD(SUN4I_HDMI_DDC_CTRL_REG, 0, 0),
+	.field_ddc_addr_reg	= REG_FIELD(SUN4I_HDMI_DDC_ADDR_REG, 0, 31),
+	.field_ddc_slave_addr	= REG_FIELD(SUN4I_HDMI_DDC_ADDR_REG, 0, 6),
+	.field_ddc_int_status	= REG_FIELD(SUN4I_HDMI_DDC_INT_STATUS_REG, 0, 8),
+	.field_ddc_fifo_clear	= REG_FIELD(SUN4I_HDMI_DDC_FIFO_CTRL_REG, 31, 31),
+	.field_ddc_fifo_rx_thres = REG_FIELD(SUN4I_HDMI_DDC_FIFO_CTRL_REG, 4, 7),
+	.field_ddc_fifo_tx_thres = REG_FIELD(SUN4I_HDMI_DDC_FIFO_CTRL_REG, 0, 3),
+	.field_ddc_byte_count	= REG_FIELD(SUN4I_HDMI_DDC_BYTE_COUNT_REG, 0, 9),
+	.field_ddc_cmd		= REG_FIELD(SUN4I_HDMI_DDC_CMD_REG, 0, 2),
+	.field_ddc_sda_en	= REG_FIELD(SUN4I_HDMI_DDC_LINE_CTRL_REG, 9, 9),
+	.field_ddc_sck_en	= REG_FIELD(SUN4I_HDMI_DDC_LINE_CTRL_REG, 8, 8),
+
+	.ddc_fifo_reg		= SUN4I_HDMI_DDC_FIFO_DATA_REG,
+	.ddc_fifo_has_dir	= true,
+};
+
+static const struct sun4i_hdmi_i2c_variant sun6i_variant = {
+	.ddc_clk_reg		= REG_FIELD(SUN6I_HDMI_DDC_CLK_REG, 0, 6),
+	.ddc_clk_pre_divider	= 1,
+	.ddc_clk_m_offset	= 2,
+
+	.field_ddc_en		= REG_FIELD(SUN6I_HDMI_DDC_CTRL_REG, 0, 0),
+	.field_ddc_start	= REG_FIELD(SUN6I_HDMI_DDC_CTRL_REG, 27, 27),
+	.field_ddc_reset	= REG_FIELD(SUN6I_HDMI_DDC_CTRL_REG, 31, 31),
+	.field_ddc_addr_reg	= REG_FIELD(SUN6I_HDMI_DDC_ADDR_REG, 1, 31),
+	.field_ddc_slave_addr	= REG_FIELD(SUN6I_HDMI_DDC_ADDR_REG, 1, 7),
+	.field_ddc_int_status	= REG_FIELD(SUN6I_HDMI_DDC_INT_STATUS_REG, 0, 8),
+	.field_ddc_fifo_clear	= REG_FIELD(SUN6I_HDMI_DDC_FIFO_CTRL_REG, 18, 18),
+	.field_ddc_fifo_rx_thres = REG_FIELD(SUN6I_HDMI_DDC_FIFO_CTRL_REG, 4, 7),
+	.field_ddc_fifo_tx_thres = REG_FIELD(SUN6I_HDMI_DDC_FIFO_CTRL_REG, 0, 3),
+	.field_ddc_byte_count	= REG_FIELD(SUN6I_HDMI_DDC_CMD_REG, 16, 25),
+	.field_ddc_cmd		= REG_FIELD(SUN6I_HDMI_DDC_CMD_REG, 0, 2),
+	.field_ddc_sda_en	= REG_FIELD(SUN6I_HDMI_DDC_CTRL_REG, 6, 6),
+	.field_ddc_sck_en	= REG_FIELD(SUN6I_HDMI_DDC_CTRL_REG, 4, 4),
+
+	.ddc_fifo_reg		= SUN6I_HDMI_DDC_FIFO_DATA_REG,
+	.ddc_fifo_thres_incl	= true,
+};
+
+static int fifo_transfer(struct sun4i_hdmi_i2c_drv *drv, u8 *buf, int len, bool read)
 {
 	/*
 	 * 1 byte takes 9 clock cycles (8 bits + 1 ACK) = 90 us for 100 kHz
@@ -42,7 +148,7 @@ static int fifo_transfer(struct sun4i_hdmi *hdmi, u8 *buf, int len, bool read)
 	 * RX_THRESHOLD number of bytes, instead of RX_THRESHOLD + 1.
 	 */
 	int read_len = RX_THRESHOLD +
-		(hdmi->variant->ddc_fifo_thres_incl ? 0 : 1);
+		(drv->variant->ddc_fifo_thres_incl ? 0 : 1);
 
 	/*
 	 * Limit transfer length by FIFO threshold or FIFO size.
@@ -51,7 +157,7 @@ static int fifo_transfer(struct sun4i_hdmi *hdmi, u8 *buf, int len, bool read)
 	len = min_t(int, len, read ? read_len : SUN4I_HDMI_DDC_FIFO_SIZE);
 
 	/* Wait until error, FIFO request bit set or transfer complete */
-	if (regmap_field_read_poll_timeout(hdmi->field_ddc_int_status, reg,
+	if (regmap_field_read_poll_timeout(drv->field_ddc_int_status, reg,
 					   reg & mask, len * byte_time_ns,
 					   100000))
 		return -ETIMEDOUT;
@@ -60,37 +166,37 @@ static int fifo_transfer(struct sun4i_hdmi *hdmi, u8 *buf, int len, bool read)
 		return -EIO;
 
 	if (read)
-		readsb(hdmi->base + hdmi->variant->ddc_fifo_reg, buf, len);
+		readsb(drv->base + drv->variant->ddc_fifo_reg, buf, len);
 	else
-		writesb(hdmi->base + hdmi->variant->ddc_fifo_reg, buf, len);
+		writesb(drv->base + drv->variant->ddc_fifo_reg, buf, len);
 
 	/* Clear FIFO request bit by forcing a write to that bit */
-	regmap_field_force_write(hdmi->field_ddc_int_status,
+	regmap_field_force_write(drv->field_ddc_int_status,
 				 SUN4I_HDMI_DDC_INT_STATUS_FIFO_REQUEST);
 
 	return len;
 }
 
-static int xfer_msg(struct sun4i_hdmi *hdmi, struct i2c_msg *msg)
+static int xfer_msg(struct sun4i_hdmi_i2c_drv *drv, struct i2c_msg *msg)
 {
 	int i, len;
 	u32 reg;
 
 	/* Set FIFO direction */
-	if (hdmi->variant->ddc_fifo_has_dir) {
-		reg = readl(hdmi->base + SUN4I_HDMI_DDC_CTRL_REG);
+	if (drv->variant->ddc_fifo_has_dir) {
+		reg = readl(drv->base + SUN4I_HDMI_DDC_CTRL_REG);
 		reg &= ~SUN4I_HDMI_DDC_CTRL_FIFO_DIR_MASK;
 		reg |= (msg->flags & I2C_M_RD) ?
 		       SUN4I_HDMI_DDC_CTRL_FIFO_DIR_READ :
 		       SUN4I_HDMI_DDC_CTRL_FIFO_DIR_WRITE;
-		writel(reg, hdmi->base + SUN4I_HDMI_DDC_CTRL_REG);
+		writel(reg, drv->base + SUN4I_HDMI_DDC_CTRL_REG);
 	}
 
 	/* Clear address register (not cleared by soft reset) */
-	regmap_field_write(hdmi->field_ddc_addr_reg, 0);
+	regmap_field_write(drv->field_ddc_addr_reg, 0);
 
 	/* Set I2C address */
-	regmap_field_write(hdmi->field_ddc_slave_addr, msg->addr);
+	regmap_field_write(drv->field_ddc_slave_addr, msg->addr);
 
 	/*
 	 * Set FIFO RX/TX thresholds and clear FIFO
@@ -98,47 +204,47 @@ static int xfer_msg(struct sun4i_hdmi *hdmi, struct i2c_msg *msg)
 	 * If threshold is inclusive, we can set the TX threshold to
 	 * 0 instead of 1.
 	 */
-	regmap_field_write(hdmi->field_ddc_fifo_tx_thres,
-			   hdmi->variant->ddc_fifo_thres_incl ? 0 : 1);
-	regmap_field_write(hdmi->field_ddc_fifo_rx_thres, RX_THRESHOLD);
-	regmap_field_write(hdmi->field_ddc_fifo_clear, 1);
-	if (regmap_field_read_poll_timeout(hdmi->field_ddc_fifo_clear,
+	regmap_field_write(drv->field_ddc_fifo_tx_thres,
+			   drv->variant->ddc_fifo_thres_incl ? 0 : 1);
+	regmap_field_write(drv->field_ddc_fifo_rx_thres, RX_THRESHOLD);
+	regmap_field_write(drv->field_ddc_fifo_clear, 1);
+	if (regmap_field_read_poll_timeout(drv->field_ddc_fifo_clear,
 					   reg, !reg, 100, 2000))
 		return -EIO;
 
 	/* Set transfer length */
-	regmap_field_write(hdmi->field_ddc_byte_count, msg->len);
+	regmap_field_write(drv->field_ddc_byte_count, msg->len);
 
 	/* Set command */
-	regmap_field_write(hdmi->field_ddc_cmd,
+	regmap_field_write(drv->field_ddc_cmd,
 			   msg->flags & I2C_M_RD ?
 			   SUN4I_HDMI_DDC_CMD_IMPLICIT_READ :
 			   SUN4I_HDMI_DDC_CMD_IMPLICIT_WRITE);
 
 	/* Clear interrupt status bits by forcing a write */
-	regmap_field_force_write(hdmi->field_ddc_int_status,
+	regmap_field_force_write(drv->field_ddc_int_status,
 				 SUN4I_HDMI_DDC_INT_STATUS_ERROR_MASK |
 				 SUN4I_HDMI_DDC_INT_STATUS_FIFO_REQUEST |
 				 SUN4I_HDMI_DDC_INT_STATUS_TRANSFER_COMPLETE);
 
 	/* Start command */
-	regmap_field_write(hdmi->field_ddc_start, 1);
+	regmap_field_write(drv->field_ddc_start, 1);
 
 	/* Transfer bytes */
 	for (i = 0; i < msg->len; i += len) {
-		len = fifo_transfer(hdmi, msg->buf + i, msg->len - i,
+		len = fifo_transfer(drv, msg->buf + i, msg->len - i,
 				    msg->flags & I2C_M_RD);
 		if (len <= 0)
 			return len;
 	}
 
 	/* Wait for command to finish */
-	if (regmap_field_read_poll_timeout(hdmi->field_ddc_start,
+	if (regmap_field_read_poll_timeout(drv->field_ddc_start,
 					   reg, !reg, 100, 100000))
 		return -EIO;
 
 	/* Check for errors */
-	regmap_field_read(hdmi->field_ddc_int_status, &reg);
+	regmap_field_read(drv->field_ddc_int_status, &reg);
 	if ((reg & SUN4I_HDMI_DDC_INT_STATUS_ERROR_MASK) ||
 	    !(reg & SUN4I_HDMI_DDC_INT_STATUS_TRANSFER_COMPLETE)) {
 		return -EIO;
@@ -150,7 +256,7 @@ static int xfer_msg(struct sun4i_hdmi *hdmi, struct i2c_msg *msg)
 static int sun4i_hdmi_i2c_xfer(struct i2c_adapter *adap,
 			       struct i2c_msg *msgs, int num)
 {
-	struct sun4i_hdmi *hdmi = i2c_get_adapdata(adap);
+	struct sun4i_hdmi_i2c_drv *drv = i2c_get_adapdata(adap);
 	u32 reg;
 	int err, i, ret = num;
 
@@ -162,30 +268,30 @@ static int sun4i_hdmi_i2c_xfer(struct i2c_adapter *adap,
 	}
 
 	/* DDC clock needs to be enabled for the module to work */
-	clk_prepare_enable(hdmi->ddc_clk);
-	clk_set_rate(hdmi->ddc_clk, 100000);
+	clk_prepare_enable(drv->ddc_clk);
+	clk_set_rate(drv->ddc_clk, 100000);
 
 	/* Reset I2C controller */
-	regmap_field_write(hdmi->field_ddc_en, 1);
-	regmap_field_write(hdmi->field_ddc_reset, 1);
-	if (regmap_field_read_poll_timeout(hdmi->field_ddc_reset,
+	regmap_field_write(drv->field_ddc_en, 1);
+	regmap_field_write(drv->field_ddc_reset, 1);
+	if (regmap_field_read_poll_timeout(drv->field_ddc_reset,
 					   reg, !reg, 100, 2000)) {
-		clk_disable_unprepare(hdmi->ddc_clk);
+		clk_disable_unprepare(drv->ddc_clk);
 		return -EIO;
 	}
 
-	regmap_field_write(hdmi->field_ddc_sck_en, 1);
-	regmap_field_write(hdmi->field_ddc_sda_en, 1);
+	regmap_field_write(drv->field_ddc_sck_en, 1);
+	regmap_field_write(drv->field_ddc_sda_en, 1);
 
 	for (i = 0; i < num; i++) {
-		err = xfer_msg(hdmi, &msgs[i]);
+		err = xfer_msg(drv, &msgs[i]);
 		if (err) {
 			ret = err;
 			break;
 		}
 	}
 
-	clk_disable_unprepare(hdmi->ddc_clk);
+	clk_disable_unprepare(drv->ddc_clk);
 	return ret;
 }
 
@@ -199,123 +305,141 @@ static const struct i2c_algorithm sun4i_hdmi_i2c_algorithm = {
 	.functionality	= sun4i_hdmi_i2c_func,
 };
 
-static int sun4i_hdmi_init_regmap_fields(struct sun4i_hdmi *hdmi)
+static int sun4i_hdmi_i2c_init_regmap_fields(struct sun4i_hdmi_i2c_drv *drv)
 {
-	hdmi->field_ddc_en =
-		devm_regmap_field_alloc(hdmi->dev, hdmi->regmap,
-					hdmi->variant->field_ddc_en);
-	if (IS_ERR(hdmi->field_ddc_en))
-		return PTR_ERR(hdmi->field_ddc_en);
+	drv->field_ddc_en =
+		devm_regmap_field_alloc(drv->dev, drv->regmap,
+					drv->variant->field_ddc_en);
+	if (IS_ERR(drv->field_ddc_en))
+		return PTR_ERR(drv->field_ddc_en);
 
-	hdmi->field_ddc_start =
-		devm_regmap_field_alloc(hdmi->dev, hdmi->regmap,
-					hdmi->variant->field_ddc_start);
-	if (IS_ERR(hdmi->field_ddc_start))
-		return PTR_ERR(hdmi->field_ddc_start);
+	drv->field_ddc_start =
+		devm_regmap_field_alloc(drv->dev, drv->regmap,
+					drv->variant->field_ddc_start);
+	if (IS_ERR(drv->field_ddc_start))
+		return PTR_ERR(drv->field_ddc_start);
 
-	hdmi->field_ddc_reset =
-		devm_regmap_field_alloc(hdmi->dev, hdmi->regmap,
-					hdmi->variant->field_ddc_reset);
-	if (IS_ERR(hdmi->field_ddc_reset))
-		return PTR_ERR(hdmi->field_ddc_reset);
+	drv->field_ddc_reset =
+		devm_regmap_field_alloc(drv->dev, drv->regmap,
+					drv->variant->field_ddc_reset);
+	if (IS_ERR(drv->field_ddc_reset))
+		return PTR_ERR(drv->field_ddc_reset);
 
-	hdmi->field_ddc_addr_reg =
-		devm_regmap_field_alloc(hdmi->dev, hdmi->regmap,
-					hdmi->variant->field_ddc_addr_reg);
-	if (IS_ERR(hdmi->field_ddc_addr_reg))
-		return PTR_ERR(hdmi->field_ddc_addr_reg);
+	drv->field_ddc_addr_reg =
+		devm_regmap_field_alloc(drv->dev, drv->regmap,
+					drv->variant->field_ddc_addr_reg);
+	if (IS_ERR(drv->field_ddc_addr_reg))
+		return PTR_ERR(drv->field_ddc_addr_reg);
 
-	hdmi->field_ddc_slave_addr =
-		devm_regmap_field_alloc(hdmi->dev, hdmi->regmap,
-					hdmi->variant->field_ddc_slave_addr);
-	if (IS_ERR(hdmi->field_ddc_slave_addr))
-		return PTR_ERR(hdmi->field_ddc_slave_addr);
+	drv->field_ddc_slave_addr =
+		devm_regmap_field_alloc(drv->dev, drv->regmap,
+					drv->variant->field_ddc_slave_addr);
+	if (IS_ERR(drv->field_ddc_slave_addr))
+		return PTR_ERR(drv->field_ddc_slave_addr);
 
-	hdmi->field_ddc_int_mask =
-		devm_regmap_field_alloc(hdmi->dev, hdmi->regmap,
-					hdmi->variant->field_ddc_int_mask);
-	if (IS_ERR(hdmi->field_ddc_int_mask))
-		return PTR_ERR(hdmi->field_ddc_int_mask);
+	drv->field_ddc_int_mask =
+		devm_regmap_field_alloc(drv->dev, drv->regmap,
+					drv->variant->field_ddc_int_mask);
+	if (IS_ERR(drv->field_ddc_int_mask))
+		return PTR_ERR(drv->field_ddc_int_mask);
 
-	hdmi->field_ddc_int_status =
-		devm_regmap_field_alloc(hdmi->dev, hdmi->regmap,
-					hdmi->variant->field_ddc_int_status);
-	if (IS_ERR(hdmi->field_ddc_int_status))
-		return PTR_ERR(hdmi->field_ddc_int_status);
+	drv->field_ddc_int_status =
+		devm_regmap_field_alloc(drv->dev, drv->regmap,
+					drv->variant->field_ddc_int_status);
+	if (IS_ERR(drv->field_ddc_int_status))
+		return PTR_ERR(drv->field_ddc_int_status);
 
-	hdmi->field_ddc_fifo_clear =
-		devm_regmap_field_alloc(hdmi->dev, hdmi->regmap,
-					hdmi->variant->field_ddc_fifo_clear);
-	if (IS_ERR(hdmi->field_ddc_fifo_clear))
-		return PTR_ERR(hdmi->field_ddc_fifo_clear);
+	drv->field_ddc_fifo_clear =
+		devm_regmap_field_alloc(drv->dev, drv->regmap,
+					drv->variant->field_ddc_fifo_clear);
+	if (IS_ERR(drv->field_ddc_fifo_clear))
+		return PTR_ERR(drv->field_ddc_fifo_clear);
 
-	hdmi->field_ddc_fifo_rx_thres =
-		devm_regmap_field_alloc(hdmi->dev, hdmi->regmap,
-					hdmi->variant->field_ddc_fifo_rx_thres);
-	if (IS_ERR(hdmi->field_ddc_fifo_rx_thres))
-		return PTR_ERR(hdmi->field_ddc_fifo_rx_thres);
+	drv->field_ddc_fifo_rx_thres =
+		devm_regmap_field_alloc(drv->dev, drv->regmap,
+					drv->variant->field_ddc_fifo_rx_thres);
+	if (IS_ERR(drv->field_ddc_fifo_rx_thres))
+		return PTR_ERR(drv->field_ddc_fifo_rx_thres);
 
-	hdmi->field_ddc_fifo_tx_thres =
-		devm_regmap_field_alloc(hdmi->dev, hdmi->regmap,
-					hdmi->variant->field_ddc_fifo_tx_thres);
-	if (IS_ERR(hdmi->field_ddc_fifo_tx_thres))
-		return PTR_ERR(hdmi->field_ddc_fifo_tx_thres);
+	drv->field_ddc_fifo_tx_thres =
+		devm_regmap_field_alloc(drv->dev, drv->regmap,
+					drv->variant->field_ddc_fifo_tx_thres);
+	if (IS_ERR(drv->field_ddc_fifo_tx_thres))
+		return PTR_ERR(drv->field_ddc_fifo_tx_thres);
 
-	hdmi->field_ddc_byte_count =
-		devm_regmap_field_alloc(hdmi->dev, hdmi->regmap,
-					hdmi->variant->field_ddc_byte_count);
-	if (IS_ERR(hdmi->field_ddc_byte_count))
-		return PTR_ERR(hdmi->field_ddc_byte_count);
+	drv->field_ddc_byte_count =
+		devm_regmap_field_alloc(drv->dev, drv->regmap,
+					drv->variant->field_ddc_byte_count);
+	if (IS_ERR(drv->field_ddc_byte_count))
+		return PTR_ERR(drv->field_ddc_byte_count);
 
-	hdmi->field_ddc_cmd =
-		devm_regmap_field_alloc(hdmi->dev, hdmi->regmap,
-					hdmi->variant->field_ddc_cmd);
-	if (IS_ERR(hdmi->field_ddc_cmd))
-		return PTR_ERR(hdmi->field_ddc_cmd);
+	drv->field_ddc_cmd =
+		devm_regmap_field_alloc(drv->dev, drv->regmap,
+					drv->variant->field_ddc_cmd);
+	if (IS_ERR(drv->field_ddc_cmd))
+		return PTR_ERR(drv->field_ddc_cmd);
 
-	hdmi->field_ddc_sda_en =
-		devm_regmap_field_alloc(hdmi->dev, hdmi->regmap,
-					hdmi->variant->field_ddc_sda_en);
-	if (IS_ERR(hdmi->field_ddc_sda_en))
-		return PTR_ERR(hdmi->field_ddc_sda_en);
+	drv->field_ddc_sda_en =
+		devm_regmap_field_alloc(drv->dev, drv->regmap,
+					drv->variant->field_ddc_sda_en);
+	if (IS_ERR(drv->field_ddc_sda_en))
+		return PTR_ERR(drv->field_ddc_sda_en);
 
-	hdmi->field_ddc_sck_en =
-		devm_regmap_field_alloc(hdmi->dev, hdmi->regmap,
-					hdmi->variant->field_ddc_sck_en);
-	if (IS_ERR(hdmi->field_ddc_sck_en))
-		return PTR_ERR(hdmi->field_ddc_sck_en);
+	drv->field_ddc_sck_en =
+		devm_regmap_field_alloc(drv->dev, drv->regmap,
+					drv->variant->field_ddc_sck_en);
+	if (IS_ERR(drv->field_ddc_sck_en))
+		return PTR_ERR(drv->field_ddc_sck_en);
 
 	return 0;
 }
 
 int sun4i_hdmi_i2c_create(struct device *dev, struct sun4i_hdmi *hdmi)
 {
-	struct i2c_adapter *adap;
+	struct sun4i_hdmi_i2c_drv *drv;
 	int ret = 0;
 
 	ret = sun4i_ddc_create(hdmi, hdmi->ddc_parent_clk);
 	if (ret)
 		return ret;
 
-	ret = sun4i_hdmi_init_regmap_fields(hdmi);
-	if (ret)
-		return ret;
-
-	adap = devm_kzalloc(dev, sizeof(*adap), GFP_KERNEL);
-	if (!adap)
+	drv = devm_kzalloc(dev, sizeof(*drv), GFP_KERNEL);
+	if (!drv)
 		return -ENOMEM;
 
-	adap->owner = THIS_MODULE;
-	adap->class = I2C_CLASS_DDC;
-	adap->algo = &sun4i_hdmi_i2c_algorithm;
-	strlcpy(adap->name, "sun4i_hdmi_i2c adapter", sizeof(adap->name));
-	i2c_set_adapdata(adap, hdmi);
+	drv->dev = dev;
 
-	ret = i2c_add_adapter(adap);
+	drv->base = hdmi->base;
+	drv->regmap = hdmi->regmap;
+	drv->ddc_clk = hdmi->ddc_clk;
+
+	if (of_device_is_compatible(dev->of_node, "allwinner,sun4i-a10-hdmi"))
+		drv->variant = &sun4i_variant;
+	if (of_device_is_compatible(dev->of_node, "allwinner,sun5i-a10s-hdmi"))
+		drv->variant = &sun4i_variant;
+	if (of_device_is_compatible(dev->of_node, "allwinner,sun6i-a31-hdmi"))
+		drv->variant = &sun6i_variant;
+	if (!drv->variant) {
+		dev_err(dev, "unsupported platform variant");
+		return -EINVAL;
+	}
+
+	ret = sun4i_hdmi_i2c_init_regmap_fields(drv);
 	if (ret)
 		return ret;
 
-	hdmi->i2c = adap;
+
+	i2c_set_adapdata(&drv->adap, drv);
+	drv->adap.owner = THIS_MODULE;
+	drv->adap.class = I2C_CLASS_DDC;
+	drv->adap.algo = &sun4i_hdmi_i2c_algorithm;
+	strlcpy(drv->adap.name, "sun4i_hdmi_i2c adapter", sizeof(drv->adap.name));
+
+	ret = i2c_add_adapter(&drv->adap);
+	if (ret)
+		return ret;
+
+	hdmi->i2c = &drv->adap;
 
 	return ret;
 }
