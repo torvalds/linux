6a1a0927491121ef090a7e573057828ed2007692
Tao Huang <huangtao@rock-chips.com>
2017-09-04 14:34:30 +0800

    soc: rockchip: add android version support
    Add config ANDROID_VERSION, default is Android 7.1.
    And add ANDROID_VERSION macro.
    
    Change-Id: I6309ea9118c1d847224f329f6c2d6ab7e073ee5a
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

---
+++ include/soc/rockchip/android-version.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2017 Rockchip Electronics Co. Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ */
+
+#ifndef ANDROID_VERSION
+#define ANDROID_VERSION(a, b, c, r) \
+	(((a) << 24) + ((b) << 16) + ((c) << 8) + (r))
+#endif
