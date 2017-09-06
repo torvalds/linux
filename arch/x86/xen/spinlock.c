e6fd28eb3522b31f79a938e24674f77268a120fd
Juergen Gross <jgross@suse.com>
2017-09-06 19:36:25 +0200

    locking/spinlocks, paravirt, xen: Correct the xen_nopvspin case
    With the boot parameter "xen_nopvspin" specified a Xen guest should not
    make use of paravirt spinlocks, but behave as if running on bare
    metal. This is not true, however, as the qspinlock code will fall back
    to a test-and-set scheme when it is detecting a hypervisor.
    
    In order to avoid this disable the virt_spin_lock_key.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Waiman Long <longman@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: akataria@vmware.com
    Cc: boris.ostrovsky@oracle.com
    Cc: chrisw@sous-sol.org
    Cc: hpa@zytor.com
    Cc: jeremy@goop.org
    Cc: rusty@rustcorp.com.au
    Cc: virtualization@lists.linux-foundation.org
    Cc: xen-devel@lists.xenproject.org
    Link: http://lkml.kernel.org/r/20170906173625.18158-3-jgross@suse.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/xen/spinlock.c
+++ arch/x86/xen/spinlock.c
@@ -10,6 +10,7 @@
 #include <linux/slab.h>
 
 #include <asm/paravirt.h>
+#include <asm/qspinlock.h>
 
 #include <xen/interface/xen.h>
 #include <xen/events.h>
@@ -129,6 +130,7 @@ void __init xen_init_spinlocks(void)
 
 	if (!xen_pvspin) {
 		printk(KERN_DEBUG "xen: PV spinlocks disabled\n");
+		static_branch_disable(&virt_spin_lock_key);
 		return;
 	}
 	printk(KERN_DEBUG "xen: PV spinlocks enabled\n");
