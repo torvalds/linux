33ba90aa4d4432b884fc0ed57ba9dc12eb8fa288
Jan Glauber <jglauber@cavium.com>
2017-09-08 10:10:33 +0200

    PCI: Avoid slot reset if bridge itself is broken
    When checking to see if a PCI slot can safely be reset, we previously
    checked to see if any of the children had their PCI_DEV_FLAGS_NO_BUS_RESET
    flag set.
    
    Some PCIe root port bridges do not behave well after a slot reset, and may
    cause the device in the slot to become unusable.
    
    Add a check for PCI_DEV_FLAGS_NO_BUS_RESET being set in the bridge device
    to prevent the slot from being reset.
    
    Signed-off-by: Jan Glauber <jglauber@cavium.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Alex Williamson <alex.williamson@redhat.com>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -4424,6 +4424,10 @@ static bool pci_slot_resetable(struct pci_slot *slot)
 {
 	struct pci_dev *dev;
 
+	if (slot->bus->self &&
+	    (slot->bus->self->dev_flags & PCI_DEV_FLAGS_NO_BUS_RESET))
+		return false;
+
 	list_for_each_entry(dev, &slot->bus->devices, bus_list) {
 		if (!dev->slot || dev->slot != slot)
 			continue;
