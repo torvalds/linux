766154b7d47b092605171df8930b864efc8ef5c8
Loic Poulain <loic.poulain@linaro.org>
2017-09-08 15:57:55 +0200

    Bluetooth: btqcomsmd: retrieve BD address from DT property
    Retrieve BD address from the local-bd-address property.
    This address must be unique and is usually added in the DT
    by the bootloader which has access to the provisioned data.
    
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/btqcomsmd.c
+++ drivers/bluetooth/btqcomsmd.c
@@ -15,6 +15,8 @@
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/rpmsg.h>
+#include <linux/of.h>
+
 #include <linux/soc/qcom/wcnss_ctrl.h>
 #include <linux/platform_device.h>
 
@@ -155,6 +157,15 @@ static int btqcomsmd_probe(struct platform_device *pdev)
 						   btqcomsmd_cmd_callback, btq);
 	if (IS_ERR(btq->cmd_channel))
 		return PTR_ERR(btq->cmd_channel);
+
+	/* The local-bd-address property is usually injected by the
+	 * bootloader which has access to the allocated BD address.
+	 */
+	if (!of_property_read_u8_array(pdev->dev.of_node, "local-bd-address",
+				       (u8 *)&btq->bdaddr, sizeof(bdaddr_t))) {
+		dev_info(&pdev->dev, "BD address %pMR retrieved from device-tree",
+			 &btq->bdaddr);
+	}
 
 	hdev = hci_alloc_dev();
 	if (!hdev)
