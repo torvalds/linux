9dfd8010161e334243a560cad37fe60ed7c7f037
Meng Wang <mwang@codeaurora.org>
2017-09-11 13:52:47 +0800

    ANDROID: GKI: ASoC: msm: qdsp6v2: Add TrueHD HDMI compress pass-though
    Add TrueHD audio format for compressed HDMI pass-through
    
    Change-Id: Id8a0b8c3a01c881ae284f13c2c0a36959c2413ac
    Signed-off-by: Ben Romberger <bromberg@codeaurora.org>
    Signed-off-by: Meng Wang <mwang@codeaurora.org>
    Bug: 151372815
    (cherry picked from commit cd79e0f7bcafff4e74796f5d4afffcf37f7c69a7)
    Signed-off-by: Saravana Kannan <saravanak@google.com>

--- include/uapi/sound/compress_params.h
+++ include/uapi/sound/compress_params.h
@@ -104,7 +104,8 @@
 #define SND_AUDIOCODEC_APE                   ((__u32) 0x00000021)
 #define SND_AUDIOCODEC_DSD                   ((__u32) 0x00000022)
 #define SND_AUDIOCODEC_APTX                  ((__u32) 0x00000023)
-#define SND_AUDIOCODEC_MAX                   SND_AUDIOCODEC_APTX
+#define SND_AUDIOCODEC_TRUEHD                ((__u32) 0x00000024)
+#define SND_AUDIOCODEC_MAX                   SND_AUDIOCODEC_TRUEHD
 
 /*
  * Profile and modes are listed with bit masks. This allows for a
