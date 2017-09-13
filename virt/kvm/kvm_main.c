5e0018b3e39e9b44dbfb380b83026e55d2f65b91
Davidlohr Bueso <dave@stgolabs.net>
2017-09-13 13:08:22 -0700

    kvm: Serialize wq active checks in kvm_vcpu_wake_up()
    This is a generic call and can be suceptible to races
    in reading the wq task_list while another task is adding
    itself to the list. Add a full barrier by using the
    swq_has_sleeper() helper.
    
    Signed-off-by: Davidlohr Bueso <dbueso@suse.de>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -2187,7 +2187,7 @@ bool kvm_vcpu_wake_up(struct kvm_vcpu *vcpu)
 	struct swait_queue_head *wqp;
 
 	wqp = kvm_arch_vcpu_wq(vcpu);
-	if (swait_active(wqp)) {
+	if (swq_has_sleeper(wqp)) {
 		swake_up(wqp);
 		++vcpu->stat.halt_wakeup;
 		return true;
