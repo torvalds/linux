0a56eabc4e3f730782e4a9f3af4f60aa03a8a849
Fabrice Gasnier <fabrice.gasnier@st.com>
2017-09-18 12:05:30 +0200

    iio: trigger: stm32-timer: preset shouldn't be buffered
    Currently, setting preset value (ARR) will update directly 'Auto reload
    value' only on 1st write access. But then, ARPE is set. This makes
    ARR a shadow register. Preset value should be updated upon each
    write request: ensure ARPE is 0. This fixes successive writes to
    preset attribute.
    
    Fixes: 4adec7da0536 ("iio: stm32 trigger: Add quadrature encoder device")
    Signed-off-by: Fabrice Gasnier <fabrice.gasnier@st.com>
    Cc: <Stable@vger.kernel.org>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/trigger/stm32-timer-trigger.c
+++ drivers/iio/trigger/stm32-timer-trigger.c
@@ -681,8 +681,9 @@ static ssize_t stm32_count_set_preset(struct iio_dev *indio_dev,
 	if (ret)
 		return ret;
 
+	/* TIMx_ARR register shouldn't be buffered (ARPE=0) */
+	regmap_update_bits(priv->regmap, TIM_CR1, TIM_CR1_ARPE, 0);
 	regmap_write(priv->regmap, TIM_ARR, preset);
-	regmap_update_bits(priv->regmap, TIM_CR1, TIM_CR1_ARPE, TIM_CR1_ARPE);
 
 	return len;
 }
