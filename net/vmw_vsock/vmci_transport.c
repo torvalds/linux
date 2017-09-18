173f4c5ebbd803023e42799d956cf174dea92db5
Corentin Labbe <clabbe.montjoie@gmail.com>
2017-09-18 20:18:55 +0200

    vsock: vmci: Remove unneeded linux/miscdevice.h include
    net/vmw_vsock/vmci_transport.c does not use any miscdevice so this patch
    remove this unnecessary inclusion.
    
    Signed-off-by: Corentin Labbe <clabbe.montjoie@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/vmw_vsock/vmci_transport.c
+++ net/vmw_vsock/vmci_transport.c
@@ -21,7 +21,6 @@
 #include <linux/kernel.h>
 #include <linux/kmod.h>
 #include <linux/list.h>
-#include <linux/miscdevice.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/net.h>
