abca5fc535a3ee0f36fb6d4468a453eaae769921
Al Viro <viro@zeniv.linux.org.uk>
2017-09-19 18:17:46 -0400

    sched_rr_get_interval(): move compat to native, get rid of set_fs()
    switch to using timespec64 internally, while we are at it
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- kernel/compat.c
+++ kernel/compat.c
@@ -562,22 +562,6 @@ COMPAT_SYSCALL_DEFINE4(migrate_pages, compat_pid_t, pid,
 }
 #endif
 
-COMPAT_SYSCALL_DEFINE2(sched_rr_get_interval,
-		       compat_pid_t, pid,
-		       struct compat_timespec __user *, interval)
-{
-	struct timespec t;
-	int ret;
-	mm_segment_t old_fs = get_fs();
-
-	set_fs(KERNEL_DS);
-	ret = sys_sched_rr_get_interval(pid, (struct timespec __user *)&t);
-	set_fs(old_fs);
-	if (compat_put_timespec(&t, interval))
-		return -EFAULT;
-	return ret;
-}
-
 /*
  * Allocate user-space memory for the duration of a single system call,
  * in order to marshall parameters inside a compat thunk.
