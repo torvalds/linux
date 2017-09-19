e2a963621bf61a101aba90a8bf8605a8a16c9f89
Greg Hackmann <ghackmann@google.com>
2017-09-19 10:55:17 -0700

    clocksource: arch_timer: make virtual counter access configurable
    CVE-2017-13218
    Change-Id: Ibdb1fd768b748002b90bfc165612c12c8311f8a2

--- drivers/clocksource/arm_arch_timer.c
+++ drivers/clocksource/arm_arch_timer.c
@@ -333,7 +333,10 @@ static void arch_counter_set_user_access(void)
 			| ARCH_TIMER_USR_PCT_ACCESS_EN);
 
 	/* Enable user access to the virtual counter */
-	cntkctl |= ARCH_TIMER_USR_VCT_ACCESS_EN;
+	if (IS_ENABLED(CONFIG_ARM_ARCH_TIMER_VCT_ACCESS))
+		cntkctl |= ARCH_TIMER_USR_VCT_ACCESS_EN;
+	else
+		cntkctl &= ~ARCH_TIMER_USR_VCT_ACCESS_EN;
 
 	arch_timer_set_cntkctl(cntkctl);
 }
