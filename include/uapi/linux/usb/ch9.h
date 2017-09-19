13713e63bdb306f9a58d359b15edd60f34eac5ee
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2017-09-19 15:07:17 +0200

    USB: fix out-of-bounds in usb_set_configuration
    commit bd7a3fe770ebd8391d1c7d072ff88e9e76d063eb upstream.
    
    Andrey Konovalov reported a possible out-of-bounds problem for a USB interface
    association descriptor.  He writes:
    	It seems there's no proper size check of a USB_DT_INTERFACE_ASSOCIATION
    	descriptor. It's only checked that the size is >= 2 in
    	usb_parse_configuration(), so find_iad() might do out-of-bounds access
    	to intf_assoc->bInterfaceCount.
    
    And he's right, we don't check for crazy descriptors of this type very well, so
    resolve this problem.  Yet another issue found by syzkaller...
    
    Reported-by: Andrey Konovalov <andreyknvl@google.com>
    Tested-by: Andrey Konovalov <andreyknvl@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/uapi/linux/usb/ch9.h
+++ include/uapi/linux/usb/ch9.h
@@ -717,6 +717,7 @@ struct usb_interface_assoc_descriptor {
 	__u8  iFunction;
 } __attribute__ ((packed));
 
+#define USB_DT_INTERFACE_ASSOCIATION_SIZE	8
 
 /*-------------------------------------------------------------------------*/
 
