79bf31a3b2a7ca467cfec8ff97d359a77065d01f
Shaohua Li <shli@fb.com>
2017-09-21 09:55:28 -0700

    md: fix a race condition for flush request handling
    md_submit_flush_data calls pers->make_request, which missed the suspend check.
    Fix it with the new md_handle_request API.
    
    Reported-by: Nate Dailey <nate.dailey@stratus.com>
    Tested-by: Nate Dailey <nate.dailey@stratus.com>
    Fix: cc27b0c78c79(md: fix deadlock between mddev_suspend() and md_write_start())
    Cc: stable@vger.kernel.org
    Reviewed-by: NeilBrown <neilb@suse.com>
    Signed-off-by: Shaohua Li <shli@fb.com>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -447,16 +447,22 @@ static void md_submit_flush_data(struct work_struct *ws)
 	struct mddev *mddev = container_of(ws, struct mddev, flush_work);
 	struct bio *bio = mddev->flush_bio;
 
+	/*
+	 * must reset flush_bio before calling into md_handle_request to avoid a
+	 * deadlock, because other bios passed md_handle_request suspend check
+	 * could wait for this and below md_handle_request could wait for those
+	 * bios because of suspend check
+	 */
+	mddev->flush_bio = NULL;
+	wake_up(&mddev->sb_wait);
+
 	if (bio->bi_iter.bi_size == 0)
 		/* an empty barrier - all done */
 		bio_endio(bio);
 	else {
 		bio->bi_opf &= ~REQ_PREFLUSH;
-		mddev->pers->make_request(mddev, bio);
+		md_handle_request(mddev, bio);
 	}
-
-	mddev->flush_bio = NULL;
-	wake_up(&mddev->sb_wait);
 }
 
 void md_flush_request(struct mddev *mddev, struct bio *bio)
