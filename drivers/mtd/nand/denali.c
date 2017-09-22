c70b5eb20a5dd9b3e4f68e041ad7526c94a2945e
Masahiro Yamada <yamada.masahiro@socionext.com>
2017-09-22 12:46:47 +0900

    mtd: nand: denali: remove unneeded init of ECC_ENABLE register
    The ECC correction is properly enabled/disabled before the page
    read/write.  There is no need to set up this at the beginning of
    the probe.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- drivers/mtd/nand/denali.c
+++ drivers/mtd/nand/denali.c
@@ -1101,8 +1101,6 @@ static void denali_hw_init(struct denali_nand_info *denali)
 	iowrite32(CHIP_EN_DONT_CARE__FLAG, denali->reg + CHIP_ENABLE_DONT_CARE);
 
 	iowrite32(0xffff, denali->reg + SPARE_AREA_MARKER);
-
-	iowrite32(1, denali->reg + ECC_ENABLE);
 }
 
 int denali_calc_ecc_bytes(int step_size, int strength)
