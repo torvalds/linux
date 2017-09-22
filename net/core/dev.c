581fe0ea61584d88072527ae9fb9dcb9d1f2783e
Willem de Bruijn <willemb@google.com>
2017-09-22 19:42:37 -0400

    net: orphan frags on stand-alone ptype in dev_queue_xmit_nit
    Zerocopy skbs frags are copied when the skb is looped to a local sock.
    Commit 1080e512d44d ("net: orphan frags on receive") introduced calls
    to skb_orphan_frags to deliver_skb and __netif_receive_skb for this.
    
    With msg_zerocopy, these skbs can also exist in the tx path and thus
    loop from dev_queue_xmit_nit. This already calls deliver_skb in its
    loop. But it does not orphan before a separate pt_prev->func().
    
    Add the missing skb_orphan_frags_rx.
    
    Changes
      v1->v2: handle skb_orphan_frags_rx failure
    
    Fixes: 1f8b977ab32d ("sock: enable MSG_ZEROCOPY")
    Signed-off-by: Willem de Bruijn <willemb@google.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -1948,8 +1948,12 @@ again:
 		goto again;
 	}
 out_unlock:
-	if (pt_prev)
-		pt_prev->func(skb2, skb->dev, pt_prev, skb->dev);
+	if (pt_prev) {
+		if (!skb_orphan_frags_rx(skb2, GFP_ATOMIC))
+			pt_prev->func(skb2, skb->dev, pt_prev, skb->dev);
+		else
+			kfree_skb(skb2);
+	}
 	rcu_read_unlock();
 }
 EXPORT_SYMBOL_GPL(dev_queue_xmit_nit);
