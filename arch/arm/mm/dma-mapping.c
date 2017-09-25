b337e1c40d3ea2d6f09228c4ff203f006abc9095
Vladimir Murzin <vladimir.murzin@arm.com>
2017-09-25 10:29:07 +0100

    ARM: 8698/1: dma-mapping: Mark atomic_pool as __ro_after_init
    atomic_pool is setup once while init stage and never changed after
    that, so it is good candidate for __ro_after_init.
    
    Since we are here mark atomic_pool_size with __init_data.
    
    Signed-off-by: Vladimir Murzin <vladimir.murzin@arm.com>
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -382,9 +382,9 @@ static void __dma_free_remap(void *cpu_addr, size_t size)
 }
 
 #define DEFAULT_DMA_COHERENT_POOL_SIZE	SZ_256K
-static struct gen_pool *atomic_pool;
+static struct gen_pool *atomic_pool __ro_after_init;
 
-static size_t atomic_pool_size = DEFAULT_DMA_COHERENT_POOL_SIZE;
+static size_t atomic_pool_size __initdata = DEFAULT_DMA_COHERENT_POOL_SIZE;
 
 static int __init early_coherent_pool(char *p)
 {
