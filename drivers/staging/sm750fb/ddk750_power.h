3ec58059d16ba2a9e47c697546e5cb92dbf4f10a
Keerthi Reddy <keerthigd4990@gmail.com>
2017-10-02 15:01:14 +0530

    Staging: sm750fb: remove typedef for enum in ddk750_power.{c,h}
    Using typedef will hide that 'DPMS_t' is enum. Removing this
    will make sure that we are actually working with enum. Also it is
    not a good coding style to use typedef
    
    In this commit remove typedef and lowercaser the name 'DPMS_t'. And
    also drop '_t' which traditionally means typedef.
    
    Signed-off-by: Keerthi Reddy <keerthigd4990@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/ddk750_power.h
+++ drivers/staging/sm750fb/ddk750_power.h
@@ -1,20 +1,19 @@
 #ifndef DDK750_POWER_H__
 #define DDK750_POWER_H__
 
-typedef enum _DPMS_t {
+enum dpms {
 	crtDPMS_ON = 0x0,
 	crtDPMS_STANDBY = 0x1,
 	crtDPMS_SUSPEND = 0x2,
 	crtDPMS_OFF = 0x3,
-}
-DPMS_t;
+};
 
 #define setDAC(off) {							\
 	poke32(MISC_CTRL,						\
 	       (peek32(MISC_CTRL) & ~MISC_CTRL_DAC_POWER_OFF) | (off)); \
 }
 
-void ddk750_set_dpms(DPMS_t state);
+void ddk750_set_dpms(enum dpms state);
 void sm750_set_power_mode(unsigned int powerMode);
 void sm750_set_current_gate(unsigned int gate);
 
