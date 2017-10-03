d5c334870eb18649476233f5a0ce4eb907c23265
Anup Patel <anup.patel@broadcom.com>
2017-10-03 10:52:59 +0530

    dmaengine: bcm-sba-raid: Use common GPL comment header
    This patch makes the comment header of Broadcom SBA RAID driver
    similar to the GPL comment header used across Broadcom driver
    sources.
    
    Signed-off-by: Anup Patel <anup.patel@broadcom.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/bcm-sba-raid.c
+++ drivers/dma/bcm-sba-raid.c
@@ -1,9 +1,14 @@
 /*
  * Copyright (C) 2017 Broadcom
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 /*
