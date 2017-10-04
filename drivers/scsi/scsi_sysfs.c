fbce4d97fd4333bcffd00a73b9d98412be630332
Hannes Reinecke <hare@suse.de>
2017-10-04 10:28:56 +0200

    scsi: fixup kernel warning during rmmod()
    Calling rmmod() on a FC driver will results in warnings like
    
    WARNING: CPU: 60 PID: 14640 at fs/sysfs/group.c:237 device_del+0x54/0x240()
    sysfs group ffffffff81eff140 not found for kobject '3:0:0:3'
    
    The problem here is that during scsi_remove_target() we will iterate
    over all devices, but fail to remove any of those as the call to
    scsi_device_get() fails the check to module_is_live().  Hence the
    devices will not be removed at this point, but all intermediate
    structures like fc rport etc. will be.  Later on during
    scsi_forget_host() the devices are removed for real, but the device
    parent is already removed and causes this warning.
    
    Signed-off-by: Hannes Reinecke <hare@suse.com>
    Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
    Reviewed-by: Kyle Fortin <kyle.fortin@oracle.com>
    Tested-by: Don Brace <don.brace@microsemi.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/scsi_sysfs.c
+++ drivers/scsi/scsi_sysfs.c
@@ -1376,13 +1376,19 @@ static void __scsi_remove_target(struct scsi_target *starget)
 	spin_lock_irqsave(shost->host_lock, flags);
  restart:
 	list_for_each_entry(sdev, &shost->__devices, siblings) {
+		/*
+		 * We cannot call scsi_device_get() here, as
+		 * we might've been called from rmmod() causing
+		 * scsi_device_get() to fail the module_is_live()
+		 * check.
+		 */
 		if (sdev->channel != starget->channel ||
 		    sdev->id != starget->id ||
-		    scsi_device_get(sdev))
+		    !get_device(&sdev->sdev_gendev))
 			continue;
 		spin_unlock_irqrestore(shost->host_lock, flags);
 		scsi_remove_device(sdev);
-		scsi_device_put(sdev);
+		put_device(&sdev->sdev_gendev);
 		spin_lock_irqsave(shost->host_lock, flags);
 		goto restart;
 	}
