dac95906003fec1b4801115830cc14ec61c74960
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2017-10-04 11:23:10 -0700

    torture: Suppress CPU stall warnings during shutdown ftrace dump
    The torture_shutdown() function directly invokes ftrace_dump(), which
    can result in RCU CPU stall warnings when the ftrace buffer is large,
    which it usually is.  This commit therefore invoks rcu_ftrace_dump()
    in place of ftrace_dump(), suppressing RCU CPU stall warnings during
    this time.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/torture.c
+++ kernel/torture.c
@@ -47,6 +47,7 @@
 #include <linux/ktime.h>
 #include <asm/byteorder.h>
 #include <linux/torture.h>
+#include "rcu/rcu.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Paul E. McKenney <paulmck@us.ibm.com>");
@@ -500,7 +501,7 @@ static int torture_shutdown(void *arg)
 		torture_shutdown_hook();
 	else
 		VERBOSE_TOROUT_STRING("No torture_shutdown_hook(), skipping.");
-	ftrace_dump(DUMP_ALL);
+	rcu_ftrace_dump(DUMP_ALL);
 	kernel_power_off();	/* Shut down the system. */
 	return 0;
 }
