39654531be4cd6f358ada4d54ea9ae01d3adff08
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2017-10-05 09:49:26 -0400

    media: i2c-addr.h: get rid of now unused defines
    Some of the previously used I2C addresses there aren't used
    anymore. So, get rid of them.
    
    Acked-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- include/media/i2c-addr.h
+++ include/media/i2c-addr.h
@@ -11,21 +11,14 @@
  */
 
 /* bttv address list */
-#define I2C_ADDR_TSA5522	0xc2
 #define I2C_ADDR_TDA7432	0x8a
 #define I2C_ADDR_TDA8425	0x82
 #define I2C_ADDR_TDA9840	0x84
-#define I2C_ADDR_TDA9850	0xb6 /* also used by 9855,9873 */
 #define I2C_ADDR_TDA9874	0xb0 /* also used by 9875 */
 #define I2C_ADDR_TDA9875	0xb0
-#define I2C_ADDR_HAUPEE		0xa0
-#define I2C_ADDR_STBEE		0xae
-#define I2C_ADDR_VHX		0xc0
 #define I2C_ADDR_MSP3400	0x80
 #define I2C_ADDR_MSP3400_ALT	0x88
 #define I2C_ADDR_TEA6300	0x80 /* also used by 6320 */
-#define I2C_ADDR_DPL3518	0x84
-#define I2C_ADDR_TDA9887	0x86
 
 /*
  * i2c bus addresses for the chips supported by tvaudio.c
