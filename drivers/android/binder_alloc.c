bcda4a7869f34ffa05f4416e5e2cb55479cfe66a
Sherry Yang <sherryy@android.com>
2017-10-05 17:13:47 -0400

    FROMLIST: android: binder: Fix null ptr dereference in debug msg
    (from https://patchwork.kernel.org/patch/9990323/)
    
    Don't access next->data in kernel debug message when the
    next buffer is null.
    
    Bug: 36007193
    Change-Id: Ib8240d7e9a7087a2256e88c0ae84b9df0f2d0224
    Acked-by: Arve Hjønnevåg <arve@android.com>
    Signed-off-by: Sherry Yang <sherryy@android.com>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -561,7 +561,7 @@ static void binder_delete_free_buffer(struct binder_alloc *alloc,
 		binder_alloc_debug(BINDER_DEBUG_BUFFER_ALLOC,
 				   "%d: merge free, buffer %pK do not share page with %pK or %pK\n",
 				   alloc->pid, buffer->data,
-				   prev->data, next->data);
+				   prev->data, next ? next->data : NULL);
 		binder_update_page_range(alloc, 0, buffer_start_page(buffer),
 					 buffer_start_page(buffer) + PAGE_SIZE);
 	}
