0a5f14ce67a6e093e651d3cd75e6ac281123d93a
Florian Fainelli <f.fainelli@gmail.com>
2017-10-11 10:57:49 -0700

    net: dsa: tag_brcm: Indicate to master netdevice port + queue
    We need to tell the DSA master network device doing the actual
    transmission what the desired switch port and queue number is for it to
    resolve that to the internal transmit queue it is mapped to.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/dsa.h
+++ include/net/dsa.h
@@ -516,4 +516,9 @@ static inline int call_dsa_notifiers(unsigned long val, struct net_device *dev,
 }
 #endif
 
+/* Broadcom tag specific helpers to insert and extract queue/port number */
+#define BRCM_TAG_SET_PORT_QUEUE(p, q)	((p) << 8 | q)
+#define BRCM_TAG_GET_PORT(v)		((v) >> 8)
+#define BRCM_TAG_GET_QUEUE(v)		((v) & 0xff)
+
 #endif
