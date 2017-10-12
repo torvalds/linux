07fd1761e1cd2b5ecdb470fc228d52ccdf75e05b
Cyril Bur <cyrilbur@gmail.com>
2017-10-12 21:17:16 +1100

    powerpc/tm: Add commandline option to disable hardware transactional memory
    Currently the kernel relies on firmware to inform it whether or not the
    CPU supports HTM and as long as the kernel was built with
    CONFIG_PPC_TRANSACTIONAL_MEM=y then it will allow userspace to make
    use of the facility.
    
    There may be situations where it would be advantageous for the kernel
    to not allow userspace to use HTM, currently the only way to achieve
    this is to recompile the kernel with CONFIG_PPC_TRANSACTIONAL_MEM=n.
    
    This patch adds a simple commandline option so that HTM can be
    disabled at boot time.
    
    Signed-off-by: Cyril Bur <cyrilbur@gmail.com>
    [mpe: Simplify to a bool, move to prom.c, put doco in the right place.
     Always disable, regardless of initial state, to avoid user confusion.]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/kernel/prom.c
+++ arch/powerpc/kernel/prom.c
@@ -658,6 +658,35 @@ static void __init early_reserve_mem(void)
 #endif
 }
 
+#ifdef CONFIG_PPC_TRANSACTIONAL_MEM
+static bool tm_disabled __initdata;
+
+static int __init parse_ppc_tm(char *str)
+{
+	bool res;
+
+	if (kstrtobool(str, &res))
+		return -EINVAL;
+
+	tm_disabled = !res;
+
+	return 0;
+}
+early_param("ppc_tm", parse_ppc_tm);
+
+static void __init tm_init(void)
+{
+	if (tm_disabled) {
+		pr_info("Disabling hardware transactional memory (HTM)\n");
+		cur_cpu_spec->cpu_user_features2 &=
+			~(PPC_FEATURE2_HTM_NOSC | PPC_FEATURE2_HTM);
+		cur_cpu_spec->cpu_features &= ~CPU_FTR_TM;
+	}
+}
+#else
+static void tm_init(void) { }
+#endif /* CONFIG_PPC_TRANSACTIONAL_MEM */
+
 void __init early_init_devtree(void *params)
 {
 	phys_addr_t limit;
@@ -766,6 +795,8 @@ void __init early_init_devtree(void *params)
 	if (of_flat_dt_is_compatible(of_get_flat_dt_root(), "sony,ps3"))
 		powerpc_firmware_features |= FW_FEATURE_PS3_POSSIBLE;
 #endif
+
+	tm_init();
 
 	DBG(" <- early_init_devtree()\n");
 }
