4df714be4dcf40bfb0d4af0f851a6e1977afa02e
Will Deacon <will.deacon@arm.com>
2017-10-12 13:20:48 +0100

    locking/atomic: Add atomic_cond_read_acquire()
    smp_cond_load_acquire() provides a way to spin on a variable with acquire
    semantics until some conditional expression involving the variable is
    satisfied. Architectures such as arm64 can potentially enter a low-power
    state, waking up only when the value of the variable changes, which
    reduces the system impact of tight polling loops.
    
    This patch makes the same interface available to users of atomic_t,
    atomic64_t and atomic_long_t, rather than require messy accesses to the
    structure internals.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: Jeremy.Linton@arm.com
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Waiman Long <longman@redhat.com>
    Cc: linux-arm-kernel@lists.infradead.org
    Link: http://lkml.kernel.org/r/1507810851-306-3-git-send-email-will.deacon@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/asm-generic/atomic-long.h
+++ include/asm-generic/atomic-long.h
@@ -243,4 +243,7 @@ static inline long atomic_long_add_unless(atomic_long_t *l, long a, long u)
 #define atomic_long_inc_not_zero(l) \
 	ATOMIC_LONG_PFX(_inc_not_zero)((ATOMIC_LONG_PFX(_t) *)(l))
 
+#define atomic_long_cond_read_acquire(v, c) \
+	ATOMIC_LONG_PFX(_cond_read_acquire)((ATOMIC_LONG_PFX(_t) *)(v), (c))
+
 #endif  /*  _ASM_GENERIC_ATOMIC_LONG_H  */
