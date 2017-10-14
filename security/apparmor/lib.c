e3bcfc148588e409685479f3d20ba3d66ae30035
Colin Ian King <colin.king@canonical.com>
2017-10-14 13:14:38 +0100

    apparmor: remove unused redundant variable stop
    The boolean variable 'stop' is being set but never read. This
    is a redundant variable and can be removed.
    
    Cleans up clang warning: Value stored to 'stop' is never read
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/lib.c
+++ security/apparmor/lib.c
@@ -423,7 +423,6 @@ int aa_check_perms(struct aa_profile *profile, struct aa_perms *perms,
 		   void (*cb)(struct audit_buffer *, void *))
 {
 	int type, error;
-	bool stop = false;
 	u32 denied = request & (~perms->allow | perms->deny);
 
 	if (likely(!denied)) {
@@ -444,8 +443,6 @@ int aa_check_perms(struct aa_profile *profile, struct aa_perms *perms,
 		else
 			type = AUDIT_APPARMOR_DENIED;
 
-		if (denied & perms->stop)
-			stop = true;
 		if (denied == (denied & perms->hide))
 			error = -ENOENT;
 
