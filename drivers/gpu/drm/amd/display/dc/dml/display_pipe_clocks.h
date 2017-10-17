378c4a2d609dfc048c4416a387ddec06994cc307
Harry Wentland <harry.wentland@amd.com>
2017-10-16 22:11:18 -0400

    Revert "amdgpu/dc: drop display_pipe_clocks.c."
    This reverts commit b3fbdcec5e6c16c93867289ae0960a64c423eb34.
    
    Unfortunately these clash with our DML update from the HW guys
    as it's starting to get used now.
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dml/display_pipe_clocks.h
+++ drivers/gpu/drm/amd/display/dc/dml/display_pipe_clocks.h
@@ -22,31 +22,20 @@
  * Authors: AMD
  *
  */
-#ifndef __DISPLAY_MODE_LIB_H__
-#define __DISPLAY_MODE_LIB_H__
+#ifndef __DISPLAY_PIPE_CLOCKS_H__
+#define __DISPLAY_PIPE_CLOCKS_H__
 
 #include "dml_common_defs.h"
-#include "soc_bounding_box.h"
-#include "display_watermark.h"
-#include "display_pipe_clocks.h"
-#include "display_rq_dlg_calc.h"
-#include "display_mode_support.h"
 
-enum dml_project {
-	DML_PROJECT_UNDEFINED,
-	DML_PROJECT_RAVEN1
-};
+struct display_mode_lib;
 
-struct display_mode_lib {
-	struct _vcs_dpi_ip_params_st ip;
-	struct _vcs_dpi_soc_bounding_box_st soc;
-	struct _vcs_dpi_mode_evaluation_st me;
-	enum dml_project project;
-	struct dml_ms_internal_vars vars;
-	struct _vcs_dpi_wm_calc_pipe_params_st wm_param[DC__NUM_PIPES__MAX];
-	struct dal_logger *logger;
-};
+struct _vcs_dpi_display_pipe_clock_st dml_clks_get_pipe_clocks(
+		struct display_mode_lib *mode_lib,
+		struct _vcs_dpi_display_e2e_pipe_params_st *e2e,
+		unsigned int num_pipes);
 
-void dml_init_instance(struct display_mode_lib *lib, enum dml_project project);
-
+bool dml_clks_pipe_clock_requirement_fit_power_constraint(
+		struct display_mode_lib *mode_lib,
+		struct _vcs_dpi_display_e2e_pipe_params_st *e2e,
+		unsigned int num_dpp_in_grp);
 #endif
