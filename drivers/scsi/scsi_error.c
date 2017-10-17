a8bbb2ab4e4fac8ebd2a402d5d81500cbeaaebaf
Hannes Reinecke <hare@suse.de>
2017-10-17 09:10:56 +0200

    scsi: scsi_error: Do not retry illegal function error
    Hitachi USP-V returns 'ILLEGAL FUNCTION' when the internal staging
    mechanism encountered an error. These errors should not be retried on
    another path.
    
    [mkp: s/invalid/illegal/]
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/scsi_error.c
+++ drivers/scsi/scsi_error.c
@@ -579,6 +579,7 @@ int scsi_check_sense(struct scsi_cmnd *scmd)
 	case ILLEGAL_REQUEST:
 		if (sshdr.asc == 0x20 || /* Invalid command operation code */
 		    sshdr.asc == 0x21 || /* Logical block address out of range */
+		    sshdr.asc == 0x22 || /* Invalid function */
 		    sshdr.asc == 0x24 || /* Invalid field in cdb */
 		    sshdr.asc == 0x26) { /* Parameter value invalid */
 			set_host_byte(scmd, DID_TARGET_FAILURE);
