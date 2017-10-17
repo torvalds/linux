8fd2d6ea66272f849943269c7ea771ce85fd8fb1
Joshua Kinard <kumba@gentoo.org>
2017-10-17 14:26:12 -0400

    MIPS: Use !pci_is_root_bus(bus) in ops-bridge.c
    This is a manual cherrypick of commit c7ddc3d137b7 from Alastair
    Bridgewater's IP35 tree that replaces two cases of
    "if (bus->number > 0)" with a more correct "if (!pci_is_root_bus(bus))"
    in arch/mips/pci/ops-bridge.c.
    
    Cc: linux-mips@linux-mips.org
    Cc: Alastair Bridgewater <alastair.bridgewater@gmail.com>
    Suggested-by: Alastair Bridgewater <alastair.bridgewater@gmail.com>
    Signed-off-by: Joshua Kinard <kumba@gentoo.org>
    Patchwork: https://patchwork.linux-mips.org/patch/17501/
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Cc: Linux/MIPS <linux-mips@linux-mips.org>

--- arch/mips/pci/ops-bridge.c
+++ arch/mips/pci/ops-bridge.c
@@ -167,7 +167,7 @@ oh_my_gawd:
 static int pci_read_config(struct pci_bus *bus, unsigned int devfn,
 			   int where, int size, u32 * value)
 {
-	if (bus->number > 0)
+	if (!pci_is_root_bus(bus))
 		return pci_conf1_read_config(bus, devfn, where, size, value);
 
 	return pci_conf0_read_config(bus, devfn, where, size, value);
@@ -310,7 +310,7 @@ oh_my_gawd:
 static int pci_write_config(struct pci_bus *bus, unsigned int devfn,
 	int where, int size, u32 value)
 {
-	if (bus->number > 0)
+	if (!pci_is_root_bus(bus))
 		return pci_conf1_write_config(bus, devfn, where, size, value);
 
 	return pci_conf0_write_config(bus, devfn, where, size, value);
