21ec54168b368f1a98097dee00625ec8ec2d47f3
Darrick J. Wong <darrick.wong@oracle.com>
2017-10-17 21:37:32 -0700

    xfs: create block pointer check functions
    Create some helper functions to check that a block pointer points
    within the filesystem (or AG) and doesn't point at static metadata.
    We will use this for scrub.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Dave Chinner <dchinner@redhat.com>

--- fs/xfs/libxfs/xfs_rtbitmap.c
+++ fs/xfs/libxfs/xfs_rtbitmap.c
@@ -1086,3 +1086,15 @@ xfs_rtalloc_query_all(
 
 	return xfs_rtalloc_query_range(tp, &keys[0], &keys[1], fn, priv);
 }
+
+/*
+ * Verify that an realtime block number pointer doesn't point off the
+ * end of the realtime device.
+ */
+bool
+xfs_verify_rtbno(
+	struct xfs_mount	*mp,
+	xfs_rtblock_t		rtbno)
+{
+	return rtbno < mp->m_sb.sb_rblocks;
+}
