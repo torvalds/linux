e5729f86a2987c9404f9b2fb494b9a6fc4412baf
Thiago Jung Bauermann <bauerman@linux.vnet.ibm.com>
2017-10-17 22:53:14 -0200

    ima: Remove redundant conditional operator
    A non-zero value is converted to 1 when assigned to a bool variable, so the
    conditional operator in is_ima_appraise_enabled is redundant.
    
    The value of a comparison operator is either 1 or 0 so the conditional
    operator in ima_inode_setxattr is redundant as well.
    
    Confirmed that the patch is correct by comparing the object file from
    before and after the patch. They are identical.
    
    Signed-off-by: Thiago Jung Bauermann <bauerman@linux.vnet.ibm.com>
    Signed-off-by: Mimi Zohar <zohar@linux.vnet.ibm.com>

--- security/integrity/ima/ima_appraise.c
+++ security/integrity/ima/ima_appraise.c
@@ -40,7 +40,7 @@ __setup("ima_appraise=", default_appraise_setup);
  */
 bool is_ima_appraise_enabled(void)
 {
-	return (ima_appraise & IMA_APPRAISE_ENFORCE) ? 1 : 0;
+	return ima_appraise & IMA_APPRAISE_ENFORCE;
 }
 
 /*
@@ -405,7 +405,7 @@ int ima_inode_setxattr(struct dentry *dentry, const char *xattr_name,
 		if (!xattr_value_len || (xvalue->type >= IMA_XATTR_LAST))
 			return -EINVAL;
 		ima_reset_appraise_flags(d_backing_inode(dentry),
-			 (xvalue->type == EVM_IMA_XATTR_DIGSIG) ? 1 : 0);
+			xvalue->type == EVM_IMA_XATTR_DIGSIG);
 		result = 0;
 	}
 	return result;
