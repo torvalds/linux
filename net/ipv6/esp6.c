d3cc547d9ccbe223b06e87256fdd571eb63762b7
Colin Ian King <colin.king@canonical.com>
2017-10-19 15:09:47 +0200

    esp6: remove redundant initialization of esph
    The pointer esph is being initialized with a value that is never
    read and then being updated.  Remove the redundant initialization
    and move the declaration and initializtion of esph to the local
    code block.
    
    Cleans up clang warning:
    net/ipv6/esp6.c:562:21: warning: Value stored to 'esph' during its
    initialization is never read
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- net/ipv6/esp6.c
+++ net/ipv6/esp6.c
@@ -559,14 +559,14 @@ static void esp_input_restore_header(struct sk_buff *skb)
 static void esp_input_set_header(struct sk_buff *skb, __be32 *seqhi)
 {
 	struct xfrm_state *x = xfrm_input_state(skb);
-	struct ip_esp_hdr *esph = (struct ip_esp_hdr *)skb->data;
 
 	/* For ESN we move the header forward by 4 bytes to
 	 * accomodate the high bits.  We will move it back after
 	 * decryption.
 	 */
 	if ((x->props.flags & XFRM_STATE_ESN)) {
-		esph = skb_push(skb, 4);
+		struct ip_esp_hdr *esph = skb_push(skb, 4);
+
 		*seqhi = esph->spi;
 		esph->spi = esph->seq_no;
 		esph->seq_no = XFRM_SKB_CB(skb)->seq.input.hi;
