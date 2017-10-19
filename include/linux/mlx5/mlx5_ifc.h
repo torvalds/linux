4d350f1f89ee8ea84be1ef09717bf392fa5a3b45
Maor Gottlieb <maorg@mellanox.com>
2017-10-19 08:25:54 +0300

    IB/mlx5: Update tunnel offloads bits
    This patch updates the mlx5_ifc with the following:
    - Fix tunnel_stateless_gre typo.
    - max_geneve_opt_len - Maximum geneve options length.
    - tunnel_stateless_geneve_rx - If set, receive Stateless
      Offloads for Geneve tunneled (inner) packets are supported.
    
    Signed-off-by: Maor Gottlieb <maorg@mellanox.com>
    Reviewed-by: Mark Bloch <markb@mellanox.com>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- include/linux/mlx5/mlx5_ifc.h
+++ include/linux/mlx5/mlx5_ifc.h
@@ -614,7 +614,9 @@ struct mlx5_ifc_per_protocol_networking_offload_caps_bits {
 	u8         swp[0x1];
 	u8         swp_csum[0x1];
 	u8         swp_lso[0x1];
-	u8         reserved_at_23[0x1d];
+	u8         reserved_at_23[0x1b];
+	u8         max_geneve_opt_len[0x1];
+	u8         tunnel_stateless_geneve_rx[0x1];
 
 	u8         reserved_at_40[0x10];
 	u8         lro_min_mss_size[0x10];
