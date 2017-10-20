ee74d9967b829232723939cb7c9b100b29f6ec98
Eric Dumazet <edumazet@google.com>
2017-10-20 11:29:57 -0700

    tun: do not arm flow_gc_timer in tun_flow_init()
    Timer is properly armed on demand from tun_flow_update(),
    so there is no need to arm it at tun init.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -1197,8 +1197,6 @@ static void tun_flow_init(struct tun_struct *tun)
 
 	tun->ageing_time = TUN_FLOW_EXPIRE;
 	setup_timer(&tun->flow_gc_timer, tun_flow_cleanup, (unsigned long)tun);
-	mod_timer(&tun->flow_gc_timer,
-		  round_jiffies_up(jiffies + tun->ageing_time));
 }
 
 static void tun_flow_uninit(struct tun_struct *tun)
