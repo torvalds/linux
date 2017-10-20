ac26963a1175c813e3ed21c0d2435b083173136e
Brijesh Singh <brijesh.singh@amd.com>
2017-10-20 09:30:57 -0500

    percpu: Introduce DEFINE_PER_CPU_DECRYPTED
    KVM guest defines three per-CPU variables (steal-time, apf_reason, and
    kvm_pic_eoi) which are shared between a guest and a hypervisor.
    
    When SEV is active, memory is encrypted with a guest-specific key, and if
    the guest OS wants to share the memory region with the hypervisor then it
    must clear the C-bit (i.e set decrypted) before sharing it.
    
    DEFINE_PER_CPU_DECRYPTED can be used to define the per-CPU variables
    which will be shared between a guest and a hypervisor.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Borislav Petkov <bp@suse.de>
    Acked-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: Borislav Petkov <bp@suse.de>
    Cc: linux-arch@vger.kernel.org
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: kvm@vger.kernel.org
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Christoph Lameter <cl@linux.com>
    Link: https://lkml.kernel.org/r/20171020143059.3291-16-brijesh.singh@amd.com

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -778,6 +778,24 @@
 #endif
 
 /*
+ * Memory encryption operates on a page basis. Since we need to clear
+ * the memory encryption mask for this section, it needs to be aligned
+ * on a page boundary and be a page-size multiple in length.
+ *
+ * Note: We use a separate section so that only this section gets
+ * decrypted to avoid exposing more than we wish.
+ */
+#ifdef CONFIG_AMD_MEM_ENCRYPT
+#define PERCPU_DECRYPTED_SECTION					\
+	. = ALIGN(PAGE_SIZE);						\
+	*(.data..percpu..decrypted)					\
+	. = ALIGN(PAGE_SIZE);
+#else
+#define PERCPU_DECRYPTED_SECTION
+#endif
+
+
+/*
  * Default discarded sections.
  *
  * Some archs want to discard exit text/data at runtime rather than
@@ -815,6 +833,7 @@
 	. = ALIGN(cacheline);						\
 	*(.data..percpu)						\
 	*(.data..percpu..shared_aligned)				\
+	PERCPU_DECRYPTED_SECTION					\
 	VMLINUX_SYMBOL(__per_cpu_end) = .;
 
 /**
