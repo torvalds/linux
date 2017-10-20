60d5a1a47b9a8381c08d2263b11ac9c757c87746
Carlo Caione <carlo@endlessm.com>
2017-10-20 12:18:55 +0100

    ASoC: rt5651: Enable jack detection on JD* pins
    Enable jack detection for the RT5651 codec on the JD* pins.
    
    Signed-off-by: Carlo Caione <carlo@endlessm.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/sound/rt5651.h
+++ include/sound/rt5651.h
@@ -11,11 +11,19 @@
 #ifndef __LINUX_SND_RT5651_H
 #define __LINUX_SND_RT5651_H
 
+enum rt5651_jd_src {
+	RT5651_JD_NULL,
+	RT5651_JD1_1,
+	RT5651_JD1_2,
+	RT5651_JD2,
+};
+
 struct rt5651_platform_data {
 	/* IN2 can optionally be differential */
 	bool in2_diff;
 
 	bool dmic_en;
+	enum rt5651_jd_src jd_src;
 };
 
 #endif
