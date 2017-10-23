e2f744a82d725ab55091cccfb8e527b4220471f0
weiyi.lu@mediatek.com <weiyi.lu@mediatek.com>
2017-10-23 12:10:34 +0800

    clk: mediatek: Add MT2712 clock support
    Add MT2712 clock support, include topckgen, apmixedsys,
    infracfg, pericfg, mcucfg and subsystem clocks.
    
    Signed-off-by: Weiyi Lu <weiyi.lu@mediatek.com>
    [sboyd@codeaurora.org: Static on top_clk_data]
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/mediatek/clk-mtk.h
+++ drivers/clk/mediatek/clk-mtk.h
@@ -207,6 +207,8 @@ struct mtk_pll_data {
 	uint32_t en_mask;
 	uint32_t pd_reg;
 	uint32_t tuner_reg;
+	uint32_t tuner_en_reg;
+	uint8_t tuner_en_bit;
 	int pd_shift;
 	unsigned int flags;
 	const struct clk_ops *ops;
