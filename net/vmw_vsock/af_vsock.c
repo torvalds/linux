c1eef220c1760762753b602c382127bfccee226d
Cong Wang <xiyou.wangcong@gmail.com>
2017-10-24 15:30:37 -0700

    vsock: always call vsock_init_tables()
    Although CONFIG_VSOCKETS_DIAG depends on CONFIG_VSOCKETS,
    vsock_init_tables() is not always called, it is called only
    if other modules call its caller. Therefore if we only
    enable CONFIG_VSOCKETS_DIAG, it would crash kernel on uninitialized
    vsock_bind_table.
    
    This patch fixes it by moving vsock_init_tables() to its own
    module_init().
    
    Fixes: 413a4317aca7 ("VSOCK: add sock_diag interface")
    Reported-by: syzkaller bot
    Cc: Stefan Hajnoczi <stefanha@redhat.com>
    Cc: Jorgen Hansen <jhansen@vmware.com>
    Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/vmw_vsock/af_vsock.c
+++ net/vmw_vsock/af_vsock.c
@@ -195,7 +195,7 @@ static int vsock_auto_bind(struct vsock_sock *vsk)
 	return __vsock_bind(sk, &local_addr);
 }
 
-static void vsock_init_tables(void)
+static int __init vsock_init_tables(void)
 {
 	int i;
 
@@ -204,6 +204,7 @@ static void vsock_init_tables(void)
 
 	for (i = 0; i < ARRAY_SIZE(vsock_connected_table); i++)
 		INIT_LIST_HEAD(&vsock_connected_table[i]);
+	return 0;
 }
 
 static void __vsock_insert_bound(struct list_head *list,
@@ -1957,8 +1958,6 @@ int __vsock_core_init(const struct vsock_transport *t, struct module *owner)
 	vsock_proto.owner = owner;
 	transport = t;
 
-	vsock_init_tables();
-
 	vsock_device.minor = MISC_DYNAMIC_MINOR;
 	err = misc_register(&vsock_device);
 	if (err) {
@@ -2018,6 +2017,8 @@ const struct vsock_transport *vsock_core_get_transport(void)
 	return transport;
 }
 EXPORT_SYMBOL_GPL(vsock_core_get_transport);
+
+module_init(vsock_init_tables);
 
 MODULE_AUTHOR("VMware, Inc.");
 MODULE_DESCRIPTION("VMware Virtual Socket Family");
