d61c22c2b5072a20cd5d232fffd438f985f655d0
Martijn Coenen <maco@android.com>
2017-10-24 16:37:39 +0200

    ANDROID: binder: show high watermark of alloc->pages.
    Show the high watermark of the index into the alloc->pages
    array, to facilitate sizing the buffer on a per-process
    basis.
    
    Change-Id: I2b40cd16628e0ee45216c51dc9b3c5b0c862032e
    Signed-off-by: Martijn Coenen <maco@android.com>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -282,6 +282,9 @@ static int binder_update_page_range(struct binder_alloc *alloc, int allocate,
 			goto err_vm_insert_page_failed;
 		}
 
+		if (index + 1 > alloc->pages_high)
+			alloc->pages_high = index + 1;
+
 		trace_binder_alloc_page_end(alloc, index);
 		/* vm_insert_page does not seem to increment the refcount */
 	}
@@ -855,6 +858,7 @@ void binder_alloc_print_pages(struct seq_file *m,
 	}
 	mutex_unlock(&alloc->mutex);
 	seq_printf(m, "  pages: %d:%d:%d\n", active, lru, free);
+	seq_printf(m, "  pages high watermark: %zu\n", alloc->pages_high);
 }
 
 /**
