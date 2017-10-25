3172c605eb3b611f118ff08e60b50e08bfdcc2a6
Greentime Hu <greentime@andestech.com>
2017-10-25 10:31:28 +0800

    nds32: ELF definitions
    This patch adds definitions for the ELF format, relocation types, vdso
    locations and EXEC_PAGESIZE.
    
    Signed-off-by: Vincent Chen <vincentc@andestech.com>
    Signed-off-by: Greentime Hu <greentime@andestech.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

---
+++ arch/nds32/include/uapi/asm/auxvec.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2005-2017 Andes Technology Corporation
+
+#ifndef __ASM_AUXVEC_H
+#define __ASM_AUXVEC_H
+
+/* VDSO location */
+#define AT_SYSINFO_EHDR	33
+
+#define AT_VECTOR_SIZE_ARCH 1
+
+#endif
