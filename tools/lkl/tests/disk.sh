17bbe4a02016550cbccf52fa0a9585b145bf2f16
Octavian Purdila <tavi@cs.pub.ro>
2017-10-29 20:44:29 +0100

    lkl tools: tests: disk.sh: detect the disk test path executable
    This allows us to run the script from directories then where the disk
    test is located.
    
    Signed-off-by: Octavian Purdila <tavi@cs.pub.ro>

--- tools/lkl/tests/disk.sh
+++ tools/lkl/tests/disk.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+
 if [ "$1" = "-t" ]; then
     shift
     fstype=$1
@@ -17,14 +19,15 @@ dd if=/dev/zero of=$file bs=1024 count=102400
 
 yes | mkfs.$fstype $file >/dev/null
 
-if file ./disk | grep PE32; then
+if file $script_dir/disk | grep PE32; then
     WRAP=wine
-elif file ./disk | grep ARM; then
+elif file $script_dir/disk | grep ARM; then
     WRAP=qemu-arm-static
 fi
 
 
-${TEST_CMD} $WRAP ./disk -d $file -t $fstype $LKL_TEST_DEBUG $@ || err=$?
+${TEST_CMD} $WRAP $script_dir/disk -d $file -t $fstype $LKL_TEST_DEBUG $@ || \
+    err=$?
 
 rm $file || true
 
