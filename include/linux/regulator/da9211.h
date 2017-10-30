707ce9eac5fc3b68f98c887dddea3911a8fc4f9f
James Ban <James.Ban.opensource@diasemi.com>
2017-10-30 11:32:38 +0900

    regulator: da9211: update for supporting da9223/4/5
    This is update for supporting additional devices da9223/4/5.
    Only device strings is added because only package type is different.
    
    Signed-off-by: James Ban <James.Ban..opensource@diasemi.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/linux/regulator/da9211.h
+++ include/linux/regulator/da9211.h
@@ -1,6 +1,6 @@
 /*
  * da9211.h - Regulator device driver for DA9211/DA9212
- * /DA9213/DA9214/DA9215
+ * /DA9213/DA9223/DA9214/DA9224/DA9215/DA9225
  * Copyright (C) 2015  Dialog Semiconductor Ltd.
  *
  * This program is free software; you can redistribute it and/or
@@ -25,8 +25,11 @@ enum da9211_chip_id {
 	DA9211,
 	DA9212,
 	DA9213,
+	DA9223,
 	DA9214,
+	DA9224,
 	DA9215,
+	DA9225,
 };
 
 struct da9211_pdata {
