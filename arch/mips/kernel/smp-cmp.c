7e7bf0ec1a7cd4599941ae2c79c3039dc622853b
James Hogan <jhogan@kernel.org>
2017-11-01 23:20:23 +0000

    MIPS: smp-cmp: Fix vpe_id build error
    The smp-cmp build has been (further) broken since commit 856fbcee6099
    ("MIPS: Store core & VP IDs in GlobalNumber-style variable") in
    v4.14-rc1 like so:
    
    arch/mips/kernel/smp-cmp.c: In function ‘cmp_init_secondary’:
    arch/mips/kernel/smp-cmp.c:53:4: error: ‘struct cpuinfo_mips’ has no member named ‘vpe_id’
       c->vpe_id = (read_c0_tcbind() >> TCBIND_CURVPE_SHIFT) &
        ^
    
    Fix by replacing vpe_id with cpu_set_vpe_id().
    
    Fixes: 856fbcee6099 ("MIPS: Store core & VP IDs in GlobalNumber-style variable")
    Signed-off-by: James Hogan <jhogan@kernel.org>
    Reviewed-by: Paul Burton <paul.burton@imgtec.com>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/17569/
    Signed-off-by: James Hogan <jhogan@kernel.org>

--- arch/mips/kernel/smp-cmp.c
+++ arch/mips/kernel/smp-cmp.c
@@ -50,8 +50,8 @@ static void cmp_init_secondary(void)
 
 #ifdef CONFIG_MIPS_MT_SMP
 	if (cpu_has_mipsmt)
-		c->vpe_id = (read_c0_tcbind() >> TCBIND_CURVPE_SHIFT) &
-			TCBIND_CURVPE;
+		cpu_set_vpe_id(c, (read_c0_tcbind() >> TCBIND_CURVPE_SHIFT) &
+				  TCBIND_CURVPE);
 #endif
 }
 
