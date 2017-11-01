5843f4e02fbe86a59981e35adc6cabebee46fdc0
Thierry Reding <treding@nvidia.com>
2017-11-01 15:20:04 +0100

    drm/fourcc: Fix fourcc_mod_code() definition
    Avoid a compiler warnings when the val parameter is an expression.
    
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- include/uapi/drm/drm_fourcc.h
+++ include/uapi/drm/drm_fourcc.h
@@ -188,7 +188,7 @@ extern "C" {
 #define DRM_FORMAT_RESERVED	      ((1ULL << 56) - 1)
 
 #define fourcc_mod_code(vendor, val) \
-	((((__u64)DRM_FORMAT_MOD_VENDOR_## vendor) << 56) | (val & 0x00ffffffffffffffULL))
+	((((__u64)DRM_FORMAT_MOD_VENDOR_## vendor) << 56) | ((val) & 0x00ffffffffffffffULL))
 
 /*
  * Format Modifier tokens:
