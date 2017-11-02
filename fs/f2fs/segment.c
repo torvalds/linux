0537b8115387f0b032472037d19e3025f8733ef7
Chao Yu <yuchao0@huawei.com>
2017-11-02 20:41:02 +0800

    f2fs: remove dead code in update_meta_page
    After commit a468f0ef516f ("f2fs: use crc and cp version to determine
    roll-forward recovery"), last caller of update_meta_page passing @src
    with NULL is gone, so remove related dead code there.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -1991,12 +1991,8 @@ struct page *get_sum_page(struct f2fs_sb_info *sbi, unsigned int segno)
 void update_meta_page(struct f2fs_sb_info *sbi, void *src, block_t blk_addr)
 {
 	struct page *page = grab_meta_page(sbi, blk_addr);
-	void *dst = page_address(page);
 
-	if (src)
-		memcpy(dst, src, PAGE_SIZE);
-	else
-		memset(dst, 0, PAGE_SIZE);
+	memcpy(page_address(page), src, PAGE_SIZE);
 	set_page_dirty(page);
 	f2fs_put_page(page, 1);
 }
