42e0442f8a237d3de9ea3f2dd2be2739e6db7fdb
Sean Young <sean@mess.org>
2017-11-02 16:39:16 -0400

    media: rc: move ir-lirc-codec.c contents into lirc_dev.c
    Since removing the lirc kapi, ir-lirc-codec.c only contains lirc fops
    so the file name is no longer correct. By moving its content into
    lirc_dev.c the ugly extern struct lirc_fops is not longer needed,
    and everything lirc related is in one file.
    
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/rc/rc-core-priv.h
+++ drivers/media/rc/rc-core-priv.h
@@ -279,8 +279,6 @@ void ir_lirc_raw_event(struct rc_dev *dev, struct ir_raw_event ev);
 void ir_lirc_scancode_event(struct rc_dev *dev, struct lirc_scancode *lsc);
 int ir_lirc_register(struct rc_dev *dev);
 void ir_lirc_unregister(struct rc_dev *dev);
-
-extern const struct file_operations lirc_fops;
 #else
 static inline int lirc_dev_init(void) { return 0; }
 static inline void lirc_dev_exit(void) {}
