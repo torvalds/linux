f4b3526d83c40dd8bf5948b9d7a1b2c340f0dcc8
David Howells <dhowells@redhat.com>
2017-11-02 15:27:48 +0000

    afs: Connect up the CB.ProbeUuid
    The handler for the CB.ProbeUuid operation in the cache manager is
    implemented, but isn't listed in the switch-statement of operation
    selection, so won't be used.  Fix this by adding it.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- fs/afs/cmservice.c
+++ fs/afs/cmservice.c
@@ -121,6 +121,9 @@ bool afs_cm_incoming_call(struct afs_call *call)
 	case CBProbe:
 		call->type = &afs_SRXCBProbe;
 		return true;
+	case CBProbeUuid:
+		call->type = &afs_SRXCBProbeUuid;
+		return true;
 	case CBTellMeAboutYourself:
 		call->type = &afs_SRXCBTellMeAboutYourself;
 		return true;
