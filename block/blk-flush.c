598906f814280762157629ba8833bf5cb11def74
Ming Lei <ming.lei@redhat.com>
2017-11-02 23:24:35 +0800

    blk-flush: use blk_mq_request_bypass_insert()
    In the following patch, we will use RQF_FLUSH_SEQ to decide:
    
    1) if the flag isn't set, the flush rq need to be inserted via
    blk_insert_flush()
    
    2) otherwise, the flush rq need to be dispatched directly since
    it is in flush machinery now.
    
    So we use blk_mq_request_bypass_insert() for requests of bypassing
    flush machinery, just like the legacy path did.
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-flush.c
+++ block/blk-flush.c
@@ -463,7 +463,7 @@ void blk_insert_flush(struct request *rq)
 	if ((policy & REQ_FSEQ_DATA) &&
 	    !(policy & (REQ_FSEQ_PREFLUSH | REQ_FSEQ_POSTFLUSH))) {
 		if (q->mq_ops)
-			blk_mq_sched_insert_request(rq, false, false, false, false);
+			blk_mq_request_bypass_insert(rq, false);
 		else
 			list_add_tail(&rq->queuelist, &q->queue_head);
 		return;
