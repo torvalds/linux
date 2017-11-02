eaa8c79eadc04b06d1434be09207074c9989cefb
Colin Ian King <colin.king@canonical.com>
2017-11-02 12:49:08 -0400

    media: drxd: make const array fastIncrDecLUT static
    Don't populate array fastIncrDecLUT on the stack but instead make it
    static. Makes the object code smaller by over 360 bytes:
    
       text	   data	    bss	    dec	    hex	filename
      32680	    944	     64	  33688	   8398	drxd_hard.o
    
       text	   data	    bss	    dec	    hex	filename
      32223	   1040	     64	  33327	   822f	drxd_hard.o
    
    (gcc version 7.2.0 x86_64)
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/dvb-frontends/drxd_hard.c
+++ drivers/media/dvb-frontends/drxd_hard.c
@@ -640,7 +640,7 @@ static int SetCfgIfAgc(struct drxd_state *state, struct SCfgAgc *cfg)
 				const u16 maxRur = 8;
 				static const u16 slowIncrDecLUT[] = {
 					3, 4, 4, 5, 6 };
-				const u16 fastIncrDecLUT[] = {
+				static const u16 fastIncrDecLUT[] = {
 					14, 15, 15, 16,
 					17, 18, 18, 19,
 					20, 21, 22, 23,
