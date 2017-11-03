2e2e35d512798193b4165ed884a5d1d9b181e231
Lu Baolu <baolu.lu@linux.intel.com>
2017-11-03 10:51:32 -0600

    iommu/vt-d: Missing checks for pasid tables if allocation fails
    intel_svm_alloc_pasid_tables() might return an error but never be
    checked by the callers. Later when intel_svm_bind_mm() is called,
    there are no checks for valid pasid tables before enabling them.
    
    Signed-off-by: Ashok Raj <ashok.raj@intel.com>
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Liu, Yi L <yi.l.liu@intel.com>
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

--- drivers/iommu/intel-svm.c
+++ drivers/iommu/intel-svm.c
@@ -292,7 +292,7 @@ int intel_svm_bind_mm(struct device *dev, int *pasid, int flags, struct svm_dev_
 	int pasid_max;
 	int ret;
 
-	if (WARN_ON(!iommu))
+	if (WARN_ON(!iommu || !iommu->pasid_table))
 		return -EINVAL;
 
 	if (dev_is_pci(dev)) {
