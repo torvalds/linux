dc259accec0845ddf56e87337c0b211026eca0ae
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2017-11-05 05:51:43 -0800

    rcu: Account for rcu_all_qs() in cond_resched()
    If cond_resched() returns false, then it has already invoked
    rcu_all_qs().  This is also invoked (now redundantly) by
    rcu_note_voluntary_context_switch().  This commit therefore changes
    cond_resched_rcu_qs() to invoke rcu_note_voluntary_context_switch_lite()
    instead of rcu_note_voluntary_context_switch() to avoid the redundant
    invocation of rcu_all_qs().
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -197,7 +197,7 @@ static inline void exit_tasks_rcu_finish(void) { }
 #define cond_resched_rcu_qs() \
 do { \
 	if (!cond_resched()) \
-		rcu_note_voluntary_context_switch(current); \
+		rcu_note_voluntary_context_switch_lite(current); \
 } while (0)
 
 /*
