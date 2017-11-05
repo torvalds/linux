0d6b1fbb536a51e88e968cce6a618bffc75312db
Octavian Purdila <tavi@cs.pub.ro>
2017-11-05 03:25:39 +0100

    lkl tools: tests: convert disk.sh to use tap13 output
    Signed-off-by: Octavian Purdila <tavi@cs.pub.ro>

--- tools/lkl/tests/disk.sh
+++ tools/lkl/tests/disk.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
-set -e
-
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+
+source $script_dir/test.sh
+
+function prepfs()
+{
+    set -e
+
+    file=`mktemp`
+
+    dd if=/dev/zero of=$file bs=1024 count=102400
+
+    yes | mkfs.$1 $file
+
+    if ! [ -z $ANDROID_WDIR ]; then
+        adb shell mkdir -p $ANDROID_WDIR
+        adb push $file $ANDROID_WDIR
+        rm $file
+        file=$ANDROID_WDIR/$(basename $file)
+    fi
+
+    export_vars file
+}
+
+function cleanfs()
+{
+    set -e
+
+    if ! [ -z $ANDROID_WDIR ]; then
+        adb shell rm $1
+        adb shell rm $ANDROID_WDIR/disk
+    else
+        rm $1
+    fi
+}
 
 if [ "$1" = "-t" ]; then
     shift
@@ -14,21 +46,15 @@ if [ -z "$fstype" ]; then
     fstype="ext4"
 fi
 
-file=`mktemp`
-dd if=/dev/zero of=$file bs=1024 count=102400
-
-yes | mkfs.$fstype $file >/dev/null
-
-if file $script_dir/disk | grep PE32; then
-    WRAP=wine
-elif file $script_dir/disk | grep ARM; then
-    WRAP=qemu-arm-static
+if [ -z $(which mkfs.$fstype) ]; then
+    lkl_test_plan 0 "disk $fstype"
+    echo "no mkfs.$fstype command"
+    exit 0
 fi
 
+lkl_test_plan 1 "disk $fstype"
+lkl_test_run 1 prepfs $fstype
+lkl_test_exec $script_dir/disk -d $file -t $fstype $@
+lkl_test_plan 1 "disk $fstype"
+lkl_test_run 1 cleanfs $file
 
-${TEST_CMD} $WRAP $script_dir/disk -d $file -t $fstype $LKL_TEST_DEBUG $@ || \
-    err=$?
-
-rm $file || true
-
-exit $err
