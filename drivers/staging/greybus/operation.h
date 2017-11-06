200543c1b94da9249b35ea1b8726cd0cdee128d5
Bryan O'Donoghue <pure.logic@nexus-software.ie>
2017-11-06 01:32:21 +0000

    staging: greybus: operation: add private data with get/set accessors
    Asynchronous operation completion handler's lives are made easier if there
    is a generic pointer that can store private data associated with the
    operation. This patch adds a pointer field to struct gb_operation and
    get/set methods to access that pointer.
    
    Signed-off-by: Bryan O'Donoghue <pure.logic@nexus-software.ie>
    Cc: Johan Hovold <johan@kernel.org>
    Cc: Alex Elder <elder@kernel.org>
    Cc: Mitch Tasman <tasman@leaflabs.com>
    Cc: greybus-dev@lists.linaro.org
    Cc: devel@driverdev.osuosl.org
    Cc: linux-kernel@vger.kernel.org
    Reviewed-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/greybus/operation.h
+++ drivers/staging/greybus/operation.h
@@ -105,6 +105,8 @@ struct gb_operation {
 
 	int			active;
 	struct list_head	links;		/* connection->operations */
+
+	void			*private;
 };
 
 static inline bool
@@ -204,6 +206,17 @@ static inline int gb_operation_unidirectional(struct gb_connection *connection,
 {
 	return gb_operation_unidirectional_timeout(connection, type,
 			request, request_size, GB_OPERATION_TIMEOUT_DEFAULT);
+}
+
+static inline void *gb_operation_get_data(struct gb_operation *operation)
+{
+	return operation->private;
+}
+
+static inline void gb_operation_set_data(struct gb_operation *operation,
+					 void *data)
+{
+	operation->private = data;
 }
 
 int gb_operation_init(void);
