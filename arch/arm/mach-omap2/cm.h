a529f8de260e33237204f18c6c1540212a9653c7
Bhumika Goyal <bhumirks@gmail.com>
2017-11-06 14:15:39 +0100

    ARM: OMAP2+: CM: make some pointers and function arguments as const
    Make the pointer cm_ll_data of type cm_ll_data as const as it does not
    modify the fields of the structure it points too.
    After this change, make the argument of cm_register function as const as
    it is used to initialise cm_ll_data or used inside an if condition.
    Make the pointer argument of cm_unregister function as const as it is only
    used inside an if condition.
    Add const to the function prototypes too.
    
    Signed-off-by: Bhumika Goyal <bhumirks@gmail.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/cm.h
+++ arch/arm/mach-omap2/cm.h
@@ -72,8 +72,8 @@ int omap_cm_wait_module_idle(u8 part, s16 prcm_mod, u16 idlest_reg,
 			     u8 idlest_shift);
 int omap_cm_module_enable(u8 mode, u8 part, u16 inst, u16 clkctrl_offs);
 int omap_cm_module_disable(u8 part, u16 inst, u16 clkctrl_offs);
-extern int cm_register(struct cm_ll_data *cld);
-extern int cm_unregister(struct cm_ll_data *cld);
+extern int cm_register(const struct cm_ll_data *cld);
+extern int cm_unregister(const struct cm_ll_data *cld);
 int omap_cm_init(void);
 int omap2_cm_base_init(void);
 
