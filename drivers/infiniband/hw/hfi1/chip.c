e8d5aff65092aabf89624d18e048c9bf1ce65a2c
Jan Sokolowski <jan.sokolowski@intel.com>
2017-11-06 06:39:07 -0800

    IB/hfi1: Send 'reboot' as planned down remote reason
    On host shutdown, driver sends 'SMA_Disabled' as a reason
    for link down. This is incorrect.
    
    Send 'reboot' as a linkdown reason.
    
    Signed-off-by: Jan Sokolowski <jan.sokolowski@intel.com>
    Reviewed-by: Jakub Byczkowski <jakub.byczkowski@intel.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/hfi1/chip.c
+++ drivers/infiniband/hw/hfi1/chip.c
@@ -9786,9 +9786,9 @@ void hfi1_quiet_serdes(struct hfi1_pportdata *ppd)
 	cancel_delayed_work_sync(&ppd->start_link_work);
 
 	ppd->offline_disabled_reason =
-			HFI1_ODR_MASK(OPA_LINKDOWN_REASON_SMA_DISABLED);
-	set_link_down_reason(ppd, OPA_LINKDOWN_REASON_SMA_DISABLED, 0,
-			     OPA_LINKDOWN_REASON_SMA_DISABLED);
+			HFI1_ODR_MASK(OPA_LINKDOWN_REASON_REBOOT);
+	set_link_down_reason(ppd, OPA_LINKDOWN_REASON_REBOOT, 0,
+			     OPA_LINKDOWN_REASON_REBOOT);
 	set_link_state(ppd, HLS_DN_OFFLINE);
 
 	/* disable the port */
