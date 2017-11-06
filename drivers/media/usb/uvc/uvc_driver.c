e96cdc9a0aa2fcaa276a76e8ffa86fc10a1d3d99
Nicolas Dufresne <nicolas.dufresne@collabora.com>
2017-11-06 15:13:28 -0500

    media: uvcvideo: Add D3DFMT_L8 support
    Microsoft HoloLense UVC sensor uses D3DFMT instead of FOURCC when
    exposing formats. This adds support for D3DFMT_L8 as exposed from
    the Acer Windows Mixed Reality Headset.
    
    Signed-off-by: Nicolas Dufresne <nicolas.dufresne@collabora.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -94,6 +94,11 @@ static struct uvc_format_desc uvc_fmts[] = {
 		.fcc		= V4L2_PIX_FMT_GREY,
 	},
 	{
+		.name		= "Greyscale 8-bit (D3DFMT_L8)",
+		.guid		= UVC_GUID_FORMAT_D3DFMT_L8,
+		.fcc		= V4L2_PIX_FMT_GREY,
+	},
+	{
 		.name		= "Greyscale 10-bit (Y10 )",
 		.guid		= UVC_GUID_FORMAT_Y10,
 		.fcc		= V4L2_PIX_FMT_Y10,
