f1b026a3310a441f504640dd3d9765eb533386b8
Wanpeng Li <wanpeng.li@hotmail.com>
2017-11-05 16:54:48 -0800

    KVM: nVMX: Validate the IA32_BNDCFGS on nested VM-entry
    According to the SDM, if the "load IA32_BNDCFGS" VM-entry controls is 1, the
    following checks are performed on the field for the IA32_BNDCFGS MSR:
     - Bits reserved in the IA32_BNDCFGS MSR must be 0.
     - The linear address in bits 63:12 must be canonical.
    
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Radim Krčmář <rkrcmar@redhat.com>
    Cc: Jim Mattson <jmattson@google.com>
    Signed-off-by: Wanpeng Li <wanpeng.li@hotmail.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -10876,6 +10876,11 @@ static int check_vmentry_postreqs(struct kvm_vcpu *vcpu, struct vmcs12 *vmcs12,
 			return 1;
 	}
 
+	if ((vmcs12->vm_entry_controls & VM_ENTRY_LOAD_BNDCFGS) &&
+		(is_noncanonical_address(vmcs12->guest_bndcfgs & PAGE_MASK, vcpu) ||
+		(vmcs12->guest_bndcfgs & MSR_IA32_BNDCFGS_RSVD)))
+			return 1;
+
 	return 0;
 }
 
