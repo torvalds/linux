f353dca59d5d9c99d1eaf1265d7416a9d15fceee
Todd Kjos <tkjos@google.com>
2017-11-08 00:43:37 +0000

    Revert "ANDROID: sched/rt: schedtune: Add boost retention to RT"
    This reverts commit d194ba5d712f051ff6c025f3484bb72f219764e3.
    
    Reason for revert: Broke some builds. Will fix and resubmit.
    
    Change-Id: I4e6fa1562346eda1bbf058f1d5ace5ba6256ce07

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1398,7 +1398,6 @@ extern void resched_cpu(int cpu);
 
 extern struct rt_bandwidth def_rt_bandwidth;
 extern void init_rt_bandwidth(struct rt_bandwidth *rt_b, u64 period, u64 runtime);
-extern void init_rt_schedtune_timer(struct sched_rt_entity *rt_se);
 
 extern struct dl_bandwidth def_dl_bandwidth;
 extern void init_dl_bandwidth(struct dl_bandwidth *dl_b, u64 period, u64 runtime);
