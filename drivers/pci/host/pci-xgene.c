92e31454f1ee0a078baf418d6f1cc20a7109a87f
Bjorn Helgaas <bhelgaas@google.com>
2017-11-09 18:12:01 -0600

    PCI: xgene: Rename xgene_pcie_probe_bridge() to xgene_pcie_probe()
    Rename xgene_pcie_probe_bridge() to xgene_pcie_probe() to follow the
    convention of other drivers.  No functional change intended.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>

--- drivers/pci/host/pci-xgene.c
+++ drivers/pci/host/pci-xgene.c
@@ -628,7 +628,7 @@ static struct pci_ops xgene_pcie_ops = {
 	.write = pci_generic_config_write32,
 };
 
-static int xgene_pcie_probe_bridge(struct platform_device *pdev)
+static int xgene_pcie_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	struct device_node *dn = dev->of_node;
@@ -709,7 +709,7 @@ static struct platform_driver xgene_pcie_driver = {
 		.of_match_table = of_match_ptr(xgene_pcie_match_table),
 		.suppress_bind_attrs = true,
 	},
-	.probe = xgene_pcie_probe_bridge,
+	.probe = xgene_pcie_probe,
 };
 builtin_platform_driver(xgene_pcie_driver);
 #endif
