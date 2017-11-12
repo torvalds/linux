ee21b18b6bdbb17a6e0b26255d2f662baf0d8409
Vasily Averin <vvs@virtuozzo.com>
2017-11-12 22:28:46 +0300

    netdev: exit_net cleanup check added
    Be sure that dev_base_head list initialized in net_init hook was return
    to initial state
    
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -8667,6 +8667,8 @@ static void __net_exit netdev_exit(struct net *net)
 {
 	kfree(net->dev_name_head);
 	kfree(net->dev_index_head);
+	if (net != &init_net)
+		WARN_ON_ONCE(!list_empty(&net->dev_base_head));
 }
 
 static struct pernet_operations __net_initdata netdev_net_ops = {
