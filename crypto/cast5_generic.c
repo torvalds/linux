1af39daaad4a868a3abd220e0ec9643473398c96
Martin Kepplinger <martink@posteo.de>
2017-11-14 10:25:15 +0100

    crypto: replace FSF address with web source in license notices
    A few years ago the FSF moved and "59 Temple Place" is wrong. Having this
    still in our source files feels old and unmaintained.
    
    Let's take the license statement serious and not confuse users.
    
    As https://www.gnu.org/licenses/gpl-howto.html suggests, we replace the
    postal address with "<http://www.gnu.org/licenses/>".
    
    Signed-off-by: Martin Kepplinger <martink@posteo.de>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/cast5_generic.c
+++ crypto/cast5_generic.c
@@ -16,8 +16,7 @@
 * any later version.
 *
 * You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
