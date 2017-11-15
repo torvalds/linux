7237c0c00f520e2899e8273e31054abe71290177
Arnav Borborah <arnavborborah11@gmail.com>
2017-11-15 07:49:31 -0500

    Fix Windows Bug

--- init/main.c
+++ init/main.c
@@ -9,6 +9,45 @@
  *  Simplified starting of init:  Michael A. Griffith <grif@acm.org>
  */
 
+#ifdef _WIN32
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    srand(time(NULL));
+    int r = rand() % 10 + 1;
+    int correct = 0; 
+    int guess; 
+    int counter = 0; 
+
+    printf("Guess my number! "); 
+
+    do {
+        scanf("%d", &guess);
+        if (guess == r) {
+            counter++;
+            printf("You guessed correctly in %d tries! Congratulations!\n", counter);
+            correct = 1; 
+        }
+
+        if (guess < r) {
+            counter++;
+            printf("Your guess is too low. Guess again. ");
+        }
+
+        if (guess > r) { 
+            counter++; 
+            printf("Your guess is too high. Guess again. ");
+        }
+    } while (correct == 0);
+
+    return 0;
+}
+
+#else
+
 #define DEBUG		/* Enable initcall_debug */
 
 #include <linux/types.h>
@@ -1098,3 +1137,4 @@ static noinline void __init kernel_init_freeable(void)
 	integrity_load_keys();
 	load_default_modules();
 }
+#endif
