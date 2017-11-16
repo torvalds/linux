4da2ce250f986060750fcc5b29112914e31803ba
Michal Hocko <mhocko@suse.com>
2017-11-15 17:33:26 -0800

    mm: distinguish CMA and MOVABLE isolation in has_unmovable_pages()
    Joonsoo has noticed that "mm: drop migrate type checks from
    has_unmovable_pages" would break CMA allocator because it relies on
    has_unmovable_pages returning false even for CMA pageblocks which in
    fact don't have to be movable:
    
     alloc_contig_range
       start_isolate_page_range
         set_migratetype_isolate
           has_unmovable_pages
    
    This is a result of the code sharing between CMA and memory hotplug
    while each one has a different idea of what has_unmovable_pages should
    return.  This is unfortunate but fixing it properly would require a lot
    of code duplication.
    
    Fix the issue by introducing the requested migrate type argument and
    special case MIGRATE_CMA case where CMA page blocks are handled
    properly.  This will work for memory hotplug because it requires
    MIGRATE_MOVABLE.
    
    Link: http://lkml.kernel.org/r/20171019122118.y6cndierwl2vnguj@dhcp22.suse.cz
    Signed-off-by: Michal Hocko <mhocko@suse.com>
    Reported-by: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Tested-by: Stefan Wahren <stefan.wahren@i2se.com>
    Tested-by: Ran Wang <ran.wang_1@nxp.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Igor Mammedov <imammedo@redhat.com>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: Reza Arbab <arbab@linux.vnet.ibm.com>
    Cc: Vitaly Kuznetsov <vkuznets@redhat.com>
    Cc: Xishi Qiu <qiuxishi@huawei.com>
    Cc: Yasuaki Ishimatsu <yasu.isimatu@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -7353,6 +7353,7 @@ void *__init alloc_large_system_hash(const char *tablename,
  * race condition. So you can't expect this function should be exact.
  */
 bool has_unmovable_pages(struct zone *zone, struct page *page, int count,
+			 int migratetype,
 			 bool skip_hwpoisoned_pages)
 {
 	unsigned long pfn, iter, found;
@@ -7362,6 +7363,15 @@ bool has_unmovable_pages(struct zone *zone, struct page *page, int count,
 	 * If ZONE_MOVABLE, the zone never contains unmovable pages
 	 */
 	if (zone_idx(zone) == ZONE_MOVABLE)
+		return false;
+
+	/*
+	 * CMA allocations (alloc_contig_range) really need to mark isolate
+	 * CMA pageblocks even when they are not movable in fact so consider
+	 * them movable here.
+	 */
+	if (is_migrate_cma(migratetype) &&
+			is_migrate_cma(get_pageblock_migratetype(page)))
 		return false;
 
 	pfn = page_to_pfn(page);
@@ -7446,7 +7456,7 @@ bool is_pageblock_removable_nolock(struct page *page)
 	if (!zone_spans_pfn(zone, pfn))
 		return false;
 
-	return !has_unmovable_pages(zone, page, 0, true);
+	return !has_unmovable_pages(zone, page, 0, MIGRATE_MOVABLE, true);
 }
 
 #if (defined(CONFIG_MEMORY_ISOLATION) && defined(CONFIG_COMPACTION)) || defined(CONFIG_CMA)
