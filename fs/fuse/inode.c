df206988e03e8510e08b2ee77f3e65010ed97768
Johannes Weiner <hannes@cmpxchg.org>
2017-11-15 17:38:34 -0800

    fs: fuse: account fuse_inode slab memory as reclaimable
    Fuse inodes are currently included in the unreclaimable slab counts -
    SUnreclaim in /proc/meminfo, slab_unreclaimable in /proc/vmstat and the
    per-cgroup memory.stat.  But they are reclaimable just like other
    filesystems' inodes, and /proc/sys/vm/drop_caches frees them easily.
    
    Mark the slab cache reclaimable.
    
    Link: http://lkml.kernel.org/r/20171102202727.12539-1-hannes@cmpxchg.org
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -1273,9 +1273,9 @@ static int __init fuse_fs_init(void)
 	int err;
 
 	fuse_inode_cachep = kmem_cache_create("fuse_inode",
-					      sizeof(struct fuse_inode), 0,
-					      SLAB_HWCACHE_ALIGN|SLAB_ACCOUNT,
-					      fuse_inode_init_once);
+			sizeof(struct fuse_inode), 0,
+			SLAB_HWCACHE_ALIGN|SLAB_ACCOUNT|SLAB_RECLAIM_ACCOUNT,
+			fuse_inode_init_once);
 	err = -ENOMEM;
 	if (!fuse_inode_cachep)
 		goto out;
