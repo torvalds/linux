de35089cc82c33f8be1dec5a0c3d7a05cf91720d
Heiko Carstens <heiko.carstens@de.ibm.com>
2017-11-17 09:50:40 +0100

    s390/disassembler: remove confusing code
    When searching the opcode offset table within find_insn() the check
    "entry->opcode == 0" was intended to clarify that 1-byte opcodes, the
    first one being 0, are special.
    
    However there is no mnemonic for an illegal opcode starting with 0.
    Therefore there is also no opcode offset table entry that matches,
    which again means that the check never is true. Therefore just remove
    the confusing check, and add a comment which hopefully explains how
    this works.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/kernel/dis.c
+++ arch/s390/kernel/dis.c
@@ -396,9 +396,14 @@ struct s390_insn *find_insn(unsigned char *code)
 	unsigned char opfrag;
 	int i;
 
+	/* Search the opcode offset table to find an entry which
+	 * matches the beginning of the opcode. If there is no match
+	 * the last entry will be used, which is the default entry for
+	 * unknown instructions as well as 1-byte opcode instructions.
+	 */
 	for (i = 0; i < ARRAY_SIZE(opcode_offset); i++) {
 		entry = &opcode_offset[i];
-		if (entry->opcode == code[0] || entry->opcode == 0)
+		if (entry->opcode == code[0])
 			break;
 	}
 
