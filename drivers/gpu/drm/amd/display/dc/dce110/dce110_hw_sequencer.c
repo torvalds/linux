93984bbc70b3f321b8f6a3ec303e31b084e54230
Shirish S <shirish.s@amd.com>
2017-11-20 10:37:08 +0530

    drm/amd/display: check plane state before validating fbc
    While validation fbc, array_mode of the pipe is accessed
    without checking plane_state exists for it.
    Causing to null pointer dereferencing followed by
    reboot when a crtc associated with external display(not
    connected) is page flipped.
    
    This patch adds a check for plane_state before using
    it to validate fbc.
    
    Signed-off-by: Shirish S <shirish.s@amd.com>
    Reviewed-by: Roman Li <Roman.Li@amd.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dce110/dce110_hw_sequencer.c
+++ drivers/gpu/drm/amd/display/dc/dce110/dce110_hw_sequencer.c
@@ -1774,6 +1774,10 @@ static enum dc_status validate_fbc(struct dc *dc,
 	if (pipe_ctx->stream->sink->link->psr_enabled)
 		return DC_ERROR_UNEXPECTED;
 
+	/* Nothing to compress */
+	if (!pipe_ctx->plane_state)
+		return DC_ERROR_UNEXPECTED;
+
 	/* Only for non-linear tiling */
 	if (pipe_ctx->plane_state->tiling_info.gfx8.array_mode == DC_ARRAY_LINEAR_GENERAL)
 		return DC_ERROR_UNEXPECTED;
