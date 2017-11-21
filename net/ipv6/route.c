bbfcd77631573ac4a9f57eb6169e04256a111bc1
Ido Schimmel <idosch@mellanox.com>
2017-11-21 09:50:12 +0200

    ipv6: Do not consider linkdown nexthops during multipath
    When the 'ignore_routes_with_linkdown' sysctl is set, we should not
    consider linkdown nexthops during route lookup.
    
    While the code correctly verifies that the initially selected route
    ('match') has a carrier, it does not perform the same check in the
    subsequent multipath selection, resulting in a potential packet loss.
    
    In case the chosen route does not have a carrier and the sysctl is set,
    choose the initially selected route.
    
    Fixes: 35103d11173b ("net: ipv6 sysctl option to ignore routes when nexthop link is down")
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Acked-by: David Ahern <dsahern@gmail.com>
    Acked-by: Andy Gospodarek <andy@greyhouse.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -472,6 +472,11 @@ static struct rt6_info *rt6_multipath_select(struct rt6_info *match,
 				&match->rt6i_siblings, rt6i_siblings) {
 			route_choosen--;
 			if (route_choosen == 0) {
+				struct inet6_dev *idev = sibling->rt6i_idev;
+
+				if (!netif_carrier_ok(sibling->dst.dev) &&
+				    idev->cnf.ignore_routes_with_linkdown)
+					break;
 				if (rt6_score_route(sibling, oif, strict) < 0)
 					break;
 				match = sibling;
