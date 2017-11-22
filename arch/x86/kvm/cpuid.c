80fef315a74d79d765dbf58d9481843a364c50d6
Yang Zhong <yang.zhong@intel.com>
2017-11-22 15:27:29 +0800

    KVM: Expose new cpu features to guest
    Intel IceLake cpu has added new cpu features,AVX512_VBMI2/GFNI/
    VAES/VPCLMULQDQ/AVX512_VNNI/AVX512_BITALG. Those new cpu features
    need expose to guest VM.
    
    The bit definition:
    CPUID.(EAX=7,ECX=0):ECX[bit 06] AVX512_VBMI2
    CPUID.(EAX=7,ECX=0):ECX[bit 08] GFNI
    CPUID.(EAX=7,ECX=0):ECX[bit 09] VAES
    CPUID.(EAX=7,ECX=0):ECX[bit 10] VPCLMULQDQ
    CPUID.(EAX=7,ECX=0):ECX[bit 11] AVX512_VNNI
    CPUID.(EAX=7,ECX=0):ECX[bit 12] AVX512_BITALG
    
    The release document ref below link:
    https://software.intel.com/sites/default/files/managed/c5/15/\
    architecture-instruction-set-extensions-programming-reference.pdf
    
    The kernel dependency commit in kvm.git:
    (c128dbfa0f879f8ce7b79054037889b0b2240728)
    
    Signed-off-by: Yang Zhong <yang.zhong@intel.com>
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -394,7 +394,8 @@ static inline int __do_cpuid_ent(struct kvm_cpuid_entry2 *entry, u32 function,
 	/* cpuid 7.0.ecx*/
 	const u32 kvm_cpuid_7_0_ecx_x86_features =
 		F(AVX512VBMI) | F(LA57) | F(PKU) | 0 /*OSPKE*/ |
-		F(AVX512_VPOPCNTDQ) | F(UMIP);
+		F(AVX512_VPOPCNTDQ) | F(UMIP) | F(AVX512_VBMI2) | F(GFNI) |
+		F(VAES) | F(VPCLMULQDQ) | F(AVX512_VNNI) | F(AVX512_BITALG);
 
 	/* cpuid 7.0.edx*/
 	const u32 kvm_cpuid_7_0_edx_x86_features =
