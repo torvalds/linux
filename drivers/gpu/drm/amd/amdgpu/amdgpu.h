fa7c7939b4bf112cd06ba166b739244077898990
Michel Dänzer <michel.daenzer@amd.com>
2017-11-22 15:55:21 +0100

    drm/amdgpu: Use unsigned ring indices in amdgpu_queue_mgr_map
    This matches the corresponding UAPI fields. Treating the ring index as
    signed could result in accessing random unrelated memory if the MSB was
    set.
    
    Fixes: effd924d2f3b ("drm/amdgpu: untie user ring ids from kernel ring
                          ids v6")
    Cc: stable@vger.kernel.org
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Michel Dänzer <michel.daenzer@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -717,7 +717,7 @@ int amdgpu_queue_mgr_fini(struct amdgpu_device *adev,
 			  struct amdgpu_queue_mgr *mgr);
 int amdgpu_queue_mgr_map(struct amdgpu_device *adev,
 			 struct amdgpu_queue_mgr *mgr,
-			 int hw_ip, int instance, int ring,
+			 u32 hw_ip, u32 instance, u32 ring,
 			 struct amdgpu_ring **out_ring);
 
 /*
