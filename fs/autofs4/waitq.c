116ceac9741e5ab0337b0c855ba80966bfc899e2
Eric W. Biederman <ebiederm@xmission.com>
2017-11-29 17:49:45 -0600

    autofs4: Modify autofs_wait to use current_uid() and current_gid()
    The code used to do that and then I mucked with it and never quite put
    the code back.  Today the code references current_cred()->uid and
    current_cred()->gid which is equivalent but more wordy, and not
    idiomatic.
    
    Fixes: 93faccbbfa95 ("fs: Better permission checking for submounts")
    Fixes: 069d5ac9ae0d ("autofs:  Fix automounts by using current_real_cred()->uid")
    Acked-by:  Ian Kent <raven@themaw.net>
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- fs/autofs4/waitq.c
+++ fs/autofs4/waitq.c
@@ -443,8 +443,8 @@ int autofs4_wait(struct autofs_sb_info *sbi,
 		memcpy(&wq->name, &qstr, sizeof(struct qstr));
 		wq->dev = autofs4_get_dev(sbi);
 		wq->ino = autofs4_get_ino(sbi);
-		wq->uid = current_cred()->uid;
-		wq->gid = current_cred()->gid;
+		wq->uid = current_uid();
+		wq->gid = current_gid();
 		wq->pid = pid;
 		wq->tgid = tgid;
 		wq->status = -EINTR; /* Status return if interrupted */
