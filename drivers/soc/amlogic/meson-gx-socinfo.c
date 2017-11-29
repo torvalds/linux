044d71bc6cdee8980d0fdc35ec79a0d5818b2ce3
Arnaud Patard <arnaud.patard@rtp-net.org>
2017-11-29 16:09:46 +0100

    meson-gx-socinfo: Fix package id parsing
    I've noticed the following message while booting a S905X based board:
    
    soc soc0: Amlogic Meson GXL (S905D) Revision 21:82 (b:2) Detected
    
    The S905D string is obviously wrong. The vendor code does:
    ...
            ver = (readl(assist_hw_rev) >> 8) & 0xff;
            meson_cpu_version[MESON_CPU_VERSION_LVL_MINOR] = ver;
            ver =  (readl(assist_hw_rev) >> 16) & 0xff;
            meson_cpu_version[MESON_CPU_VERSION_LVL_PACK] = ver;
    ...
    
    while the current code does:
    ...
    ...
    
    This means that the current mainline code has package id and minor
    version reversed.
    
    Fixes: a9daaba2965e8 ("soc: Add Amlogic SoC Information driver")
    Signed-off-by: Arnaud Patard <apatard@hupstream.com>
    Acked-by: Neil Armstrong <narmstrong@baylibre.com>
    Signed-off-by: Kevin Hilman <khilman@baylibre.com>

--- drivers/soc/amlogic/meson-gx-socinfo.c
+++ drivers/soc/amlogic/meson-gx-socinfo.c
@@ -20,8 +20,8 @@
 #define AO_SEC_SOCINFO_OFFSET	AO_SEC_SD_CFG8
 
 #define SOCINFO_MAJOR	GENMASK(31, 24)
-#define SOCINFO_MINOR	GENMASK(23, 16)
-#define SOCINFO_PACK	GENMASK(15, 8)
+#define SOCINFO_PACK	GENMASK(23, 16)
+#define SOCINFO_MINOR	GENMASK(15, 8)
 #define SOCINFO_MISC	GENMASK(7, 0)
 
 static const struct meson_gx_soc_id {
