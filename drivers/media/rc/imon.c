6a489f760ea1b789ebc01eed70e169ce58371360
Sean Young <sean@mess.org>
2017-12-02 06:10:34 -0500

    media: imon: auto-config ffdc 26 device
    Another device with the 0xffdc device id, this one with 0x26 in the
    config byte. Its an iMON Inside + iMON IR. It does respond to rc-6,
    but seems to produce random garbage rather than a scancode.
    
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/rc/imon.c
+++ drivers/media/rc/imon.c
@@ -1975,6 +1975,11 @@ static void imon_get_ffdc_type(struct imon_context *ictx)
 		detected_display_type = IMON_DISPLAY_TYPE_LCD;
 		allowed_protos = RC_PROTO_BIT_RC6_MCE;
 		break;
+	/* no display, iMON IR */
+	case 0x26:
+		dev_info(ictx->dev, "0xffdc iMON Inside, iMON IR");
+		ictx->display_supported = false;
+		break;
 	default:
 		dev_info(ictx->dev, "Unknown 0xffdc device, defaulting to VFD and iMON IR");
 		detected_display_type = IMON_DISPLAY_TYPE_VFD;
