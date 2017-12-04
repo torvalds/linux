11474e9091cf2002e948647fd9f63a7f027e488a
Zhenyu Wang <zhenyuw@linux.intel.com>
2017-12-04 10:42:58 +0800

    drm/i915/gvt: set max priority for gvt context
    This is to workaround guest driver hang regression after
    preemption enable that gvt hasn't enabled handling of that
    for guest workload. So in effect this disables preemption
    for gvt context now.
    
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    (cherry picked from commit 1603660b3342269c95fcafee1945790342a8c28e)

--- drivers/gpu/drm/i915/gvt/scheduler.c
+++ drivers/gpu/drm/i915/gvt/scheduler.c
@@ -757,6 +757,9 @@ int intel_vgpu_init_gvt_context(struct intel_vgpu *vgpu)
 	if (IS_ERR(vgpu->shadow_ctx))
 		return PTR_ERR(vgpu->shadow_ctx);
 
+	if (INTEL_INFO(vgpu->gvt->dev_priv)->has_logical_ring_preemption)
+		vgpu->shadow_ctx->priority = INT_MAX;
+
 	vgpu->shadow_ctx->engine[RCS].initialised = true;
 
 	bitmap_zero(vgpu->shadow_ctx_desc_updated, I915_NUM_ENGINES);
