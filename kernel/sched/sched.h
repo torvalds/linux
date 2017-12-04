7e1a9208f6c7e66bb4e5d2ed18dfd191230f431b
Juri Lelli <juri.lelli@arm.com>
2017-12-04 11:23:24 +0100

    sched/cpufreq: Move arch_scale_{freq,cpu}_capacity() outside of #ifdef CONFIG_SMP
    Currently, frequency and cpu capacity scaling is only performed on
    CONFIG_SMP systems (as CFS PELT signals are only present for such
    systems). However, other scheduling classes want to do freq/cpu scaling,
    and for !CONFIG_SMP configurations as well.
    
    arch_scale_freq_capacity() is useful to implement frequency scaling even
    on !CONFIG_SMP platforms, so we simply move it outside CONFIG_SMP
    ifdeffery.
    
    Even if arch_scale_cpu_capacity() is not useful on !CONFIG_SMP platforms,
    we make a default implementation available for such configurations anyway
    to simplify scheduler code doing CPU scale invariance.
    
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: alessio.balsini@arm.com
    Cc: bristot@redhat.com
    Cc: claudio@evidence.eu.com
    Cc: dietmar.eggemann@arm.com
    Cc: joelaf@google.com
    Cc: juri.lelli@redhat.com
    Cc: luca.abeni@santannapisa.it
    Cc: mathieu.poirier@linaro.org
    Cc: morten.rasmussen@arm.com
    Cc: patrick.bellasi@arm.com
    Cc: rjw@rjwysocki.net
    Cc: tkjos@android.com
    Cc: tommaso.cucinotta@santannapisa.it
    Cc: vincent.guittot@linaro.org
    Cc: viresh.kumar@linaro.org
    Link: http://lkml.kernel.org/r/20171204102325.5110-8-juri.lelli@redhat.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1670,9 +1670,6 @@ static inline int hrtick_enabled(struct rq *rq)
 
 #endif /* CONFIG_SCHED_HRTICK */
 
-#ifdef CONFIG_SMP
-extern void sched_avg_update(struct rq *rq);
-
 #ifndef arch_scale_freq_capacity
 static __always_inline
 unsigned long arch_scale_freq_capacity(int cpu)
@@ -1680,6 +1677,9 @@ unsigned long arch_scale_freq_capacity(int cpu)
 	return SCHED_CAPACITY_SCALE;
 }
 #endif
+
+#ifdef CONFIG_SMP
+extern void sched_avg_update(struct rq *rq);
 
 #ifndef arch_scale_cpu_capacity
 static __always_inline
@@ -1698,6 +1698,13 @@ static inline void sched_rt_avg_update(struct rq *rq, u64 rt_delta)
 	sched_avg_update(rq);
 }
 #else
+#ifndef arch_scale_cpu_capacity
+static __always_inline
+unsigned long arch_scale_cpu_capacity(void __always_unused *sd, int cpu)
+{
+	return SCHED_CAPACITY_SCALE;
+}
+#endif
 static inline void sched_rt_avg_update(struct rq *rq, u64 rt_delta) { }
 static inline void sched_avg_update(struct rq *rq) { }
 #endif
