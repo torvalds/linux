560d454bae08b5d5a132c5520177dede066334b7
Matthew Wilcox <willy@infradead.org>
2017-12-04 04:30:18 -0500

    mm: Convert __do_page_cache_readahead to XArray
    This one is trivial.
    
    Signed-off-by: Matthew Wilcox <willy@infradead.org>

--- mm/readahead.c
+++ mm/readahead.c
@@ -176,9 +176,7 @@ unsigned int __do_page_cache_readahead(struct address_space *mapping,
 		if (page_offset > end_index)
 			break;
 
-		rcu_read_lock();
-		page = radix_tree_lookup(&mapping->i_pages, page_offset);
-		rcu_read_unlock();
+		page = xa_load(&mapping->i_pages, page_offset);
 		if (page && !xa_is_value(page)) {
 			/*
 			 * Page already present?  Kick off the current batch of
