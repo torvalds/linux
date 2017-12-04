ae0bba6b38110e8b57b92aa820df8f78d3965fe6
Andi Kleen <ak@linux.intel.com>
2017-12-03 20:38:01 -0500

    ext4: support fast symlinks from ext3 file systems
    commit fc82228a5e3860502dbf3bfa4a9570cb7093cf7f upstream.
    
    407cd7fb83c0 (ext4: change fast symlink test to not rely on i_blocks)
    broke ~10 years old ext3 file systems created by 2.6.17. Any ELF
    executable fails because the /lib/ld-linux.so.2 fast symlink
    cannot be read anymore.
    
    The patch assumed fast symlinks were created in a specific way,
    but that's not true on these really old file systems.
    
    The new behavior is apparently needed only with the large EA inode
    feature.
    
    Revert to the old behavior if the large EA inode feature is not set.
    
    This makes my old VM boot again.
    
    Fixes: 407cd7fb83c0 (ext4: change fast symlink test to not rely on i_blocks)
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Reviewed-by: Andreas Dilger <adilger@dilger.ca>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -149,6 +149,15 @@ static int ext4_meta_trans_blocks(struct inode *inode, int lblocks,
  */
 int ext4_inode_is_fast_symlink(struct inode *inode)
 {
+	if (!(EXT4_I(inode)->i_flags & EXT4_EA_INODE_FL)) {
+		int ea_blocks = EXT4_I(inode)->i_file_acl ?
+				EXT4_CLUSTER_SIZE(inode->i_sb) >> 9 : 0;
+
+		if (ext4_has_inline_data(inode))
+			return 0;
+
+		return (S_ISLNK(inode->i_mode) && inode->i_blocks - ea_blocks == 0);
+	}
 	return S_ISLNK(inode->i_mode) && inode->i_size &&
 	       (inode->i_size < EXT4_N_BLOCKS * 4);
 }
