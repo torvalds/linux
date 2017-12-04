fdb1a2236b07948e83e0a777e1795d4f07e52c33
Christian König <christian.koenig@amd.com>
2017-12-04 11:17:54 +0100

    drm/ttm: swap consecutive allocated cached pages v3
    When we detect consecutive allocation of pages swap them to avoid
    accidentally freeing them as huge page.
    
    v2: use swap
    v3: check if it's really the first allocated page
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Roger He <Hongbo.He@amd.com>
    Reviewed-by: Michel Dänzer <michel.daenzer@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/ttm/ttm_page_alloc.c
+++ drivers/gpu/drm/ttm/ttm_page_alloc.c
@@ -877,7 +877,7 @@ static int ttm_get_pages(struct page **pages, unsigned npages, int flags,
 #endif
 	struct list_head plist;
 	struct page *p = NULL;
-	unsigned count;
+	unsigned count, first;
 	int r;
 
 	/* No pool for cached pages */
@@ -918,12 +918,17 @@ static int ttm_get_pages(struct page **pages, unsigned npages, int flags,
 		}
 #endif
 
+		first = i;
 		while (npages) {
 			p = alloc_page(gfp_flags);
 			if (!p) {
 				pr_debug("Unable to allocate page\n");
 				return -ENOMEM;
 			}
+
+			/* Swap the pages if we detect consecutive order */
+			if (i > first && pages[i - 1] == p - 1)
+				swap(p, pages[i - 1]);
 
 			pages[i++] = p;
 			--npages;
