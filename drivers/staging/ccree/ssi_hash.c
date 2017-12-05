aece09024414b54158e03aa45f4a4436e7cb996c
Dan Carpenter <dan.carpenter@oracle.com>
2017-12-05 17:37:17 +0300

    staging: ccree: Uninitialized return in ssi_ahash_import()
    The return value isn't initialized on some success paths.
    
    Fixes: c5f39d07860c ("staging: ccree: fix leak of import() after init()")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ccree/ssi_hash.c
+++ drivers/staging/ccree/ssi_hash.c
@@ -1769,7 +1769,7 @@ static int ssi_ahash_import(struct ahash_request *req, const void *in)
 	struct device *dev = drvdata_to_dev(ctx->drvdata);
 	struct ahash_req_ctx *state = ahash_request_ctx(req);
 	u32 tmp;
-	int rc;
+	int rc = 0;
 
 	memcpy(&tmp, in, sizeof(u32));
 	if (tmp != CC_EXPORT_MAGIC) {
