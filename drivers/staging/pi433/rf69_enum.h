21a9758984c299120df737de80d421e0b4035231
Simon Sandström <simon@nikanor.nu>
2017-12-05 23:08:39 +0100

    staging: pi433: Fix indentation in rf69_enum.h
    Basically just 's/    /\t/', to fix checkpatch.pl warnings:
    "please, no spaces at the start of a line".
    
    Signed-off-by: Simon Sandström <simon@nikanor.nu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/pi433/rf69_enum.h
+++ drivers/staging/pi433/rf69_enum.h
@@ -19,164 +19,163 @@
 #define RF69_ENUM_H
 
 enum optionOnOff {
-    optionOff,
-    optionOn
+	optionOff,
+	optionOn
 };
 
 enum mode {
-    mode_sleep,
-    standby,
-    synthesizer,
-    transmit,
-    receive
+	mode_sleep,
+	standby,
+	synthesizer,
+	transmit,
+	receive
 };
 
 enum dataMode {
-    packet,
-    continuous,
-    continuousNoSync
+	packet,
+	continuous,
+	continuousNoSync
 };
 
 enum modulation {
-    OOK,
-    FSK
+	OOK,
+	FSK
 };
 
 enum modShaping {
-    shapingOff,
-    shaping1_0,
-    shaping0_5,
-    shaping0_3,
-    shapingBR,
-    shaping2BR
+	shapingOff,
+	shaping1_0,
+	shaping0_5,
+	shaping0_3,
+	shapingBR,
+	shaping2BR
 };
 
 enum paRamp {
-    ramp3400,
-    ramp2000,
-    ramp1000,
-    ramp500,
-    ramp250,
-    ramp125,
-    ramp100,
-    ramp62,
-    ramp50,
-    ramp40,
-    ramp31,
-    ramp25,
-    ramp20,
-    ramp15,
-    ramp12,
-    ramp10
+	ramp3400,
+	ramp2000,
+	ramp1000,
+	ramp500,
+	ramp250,
+	ramp125,
+	ramp100,
+	ramp62,
+	ramp50,
+	ramp40,
+	ramp31,
+	ramp25,
+	ramp20,
+	ramp15,
+	ramp12,
+	ramp10
 };
 
 enum antennaImpedance {
-    fiftyOhm,
-    twohundretOhm
+	fiftyOhm,
+	twohundretOhm
 };
 
 enum lnaGain {
-    automatic,
-    max,
-    maxMinus6,
-    maxMinus12,
-    maxMinus24,
-    maxMinus36,
-    maxMinus48,
-    undefined
+	automatic,
+	max,
+	maxMinus6,
+	maxMinus12,
+	maxMinus24,
+	maxMinus36,
+	maxMinus48,
+	undefined
 };
 
 enum dccPercent {
-    dcc16Percent,
-    dcc8Percent,
-    dcc4Percent,
-    dcc2Percent,
-    dcc1Percent,
-    dcc0_5Percent,
-    dcc0_25Percent,
-    dcc0_125Percent
+	dcc16Percent,
+	dcc8Percent,
+	dcc4Percent,
+	dcc2Percent,
+	dcc1Percent,
+	dcc0_5Percent,
+	dcc0_25Percent,
+	dcc0_125Percent
 };
 
 enum mantisse {
-    mantisse16,
-    mantisse20,
-    mantisse24
+	mantisse16,
+	mantisse20,
+	mantisse24
 };
 
 enum thresholdType {
-    fixed,
-    peak,
-    average
+	fixed,
+	peak,
+	average
 };
 
 enum thresholdStep {
-    step_0_5db,
-    step_1_0db,
-    step_1_5db,
-    step_2_0db,
-    step_3_0db,
-    step_4_0db,
-    step_5_0db,
-    step_6_0db
+	step_0_5db,
+	step_1_0db,
+	step_1_5db,
+	step_2_0db,
+	step_3_0db,
+	step_4_0db,
+	step_5_0db,
+	step_6_0db
 };
 
 enum thresholdDecrement {
-    dec_every8th,
-    dec_every4th,
-    dec_every2nd,
-    dec_once,
-    dec_twice,
-    dec_4times,
-    dec_8times,
-    dec_16times
+	dec_every8th,
+	dec_every4th,
+	dec_every2nd,
+	dec_once,
+	dec_twice,
+	dec_4times,
+	dec_8times,
+	dec_16times
 };
 
 enum flag {
-    modeSwitchCompleted,
-    readyToReceive,
-    readyToSend,
-    pllLocked,
-    rssiExceededThreshold,
-    timeout,
-    automode,
-    syncAddressMatch,
-    fifoFull,
-//    fifoNotEmpty, collision with next enum; replaced by following enum...
-    fifoEmpty,
-    fifoLevelBelowThreshold,
-    fifoOverrun,
-    packetSent,
-    payloadReady,
-    crcOk,
-    batteryLow
+	modeSwitchCompleted,
+	readyToReceive,
+	readyToSend,
+	pllLocked,
+	rssiExceededThreshold,
+	timeout,
+	automode,
+	syncAddressMatch,
+	fifoFull,
+//	fifoNotEmpty, collision with next enum; replaced by following enum...
+	fifoEmpty,
+	fifoLevelBelowThreshold,
+	fifoOverrun,
+	packetSent,
+	payloadReady,
+	crcOk,
+	batteryLow
 };
 
 enum fifoFillCondition {
-    afterSyncInterrupt,
-    always
+	afterSyncInterrupt,
+	always
 };
 
 enum packetFormat {
-    packetLengthFix,
-    packetLengthVar
+	packetLengthFix,
+	packetLengthVar
 };
 
 enum txStartCondition {
-    fifoLevel,
-    fifoNotEmpty
+	fifoLevel,
+	fifoNotEmpty
 };
 
 enum addressFiltering {
-    filteringOff,
-    nodeAddress,
-    nodeOrBroadcastAddress
+	filteringOff,
+	nodeAddress,
+	nodeOrBroadcastAddress
 };
 
 enum dagc {
-    normalMode,
-    improve,
-    improve4LowModulationIndex
+	normalMode,
+	improve,
+	improve4LowModulationIndex
 };
-
 
 #endif
