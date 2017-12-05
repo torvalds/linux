500943e57db8d3e298e98f595f835c5b613e843b
Hugh Dickins <hughd@google.com>
2017-12-04 20:13:35 -0800

    kaiser: fix unlikely error in alloc_ldt_struct()
    An error from kaiser_add_mapping() here is not at all likely, but
    Eric Biggers rightly points out that __free_ldt_struct() relies on
    new_ldt->size being initialized: move that up.
    
    Signed-off-by: Hugh Dickins <hughd@google.com>
    Acked-by: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kernel/ldt.c
+++ arch/x86/kernel/ldt.c
@@ -79,11 +79,11 @@ static struct ldt_struct *alloc_ldt_struct(int size)
 
 	ret = kaiser_add_mapping((unsigned long)new_ldt->entries, alloc_size,
 				 __PAGE_KERNEL);
+	new_ldt->size = size;
 	if (ret) {
 		__free_ldt_struct(new_ldt);
 		return NULL;
 	}
-	new_ldt->size = size;
 	return new_ldt;
 }
 
