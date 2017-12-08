528dd16a7c228e1383a291781744ed2c0ac99429
Chris Wilson <chris@chris-wilson.co.uk>
2017-12-08 01:23:02 +0000

    drm/i915: Include the global reset count for intel_engine_dump()
    Since a global reset affects the engine, include that along side the
    per-engine reset counter when pretty printing the engine state in
    intel_engine_dump().
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20171208012303.25504-4-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/intel_engine_cs.c
+++ drivers/gpu/drm/i915/intel_engine_cs.c
@@ -1693,8 +1693,9 @@ void intel_engine_dump(struct intel_engine_cs *engine,
 		   engine->hangcheck.seqno,
 		   jiffies_to_msecs(jiffies - engine->hangcheck.action_timestamp),
 		   engine->timeline->inflight_seqnos);
-	drm_printf(m, "\tReset count: %d\n",
-		   i915_reset_engine_count(error, engine));
+	drm_printf(m, "\tReset count: %d (global %d)\n",
+		   i915_reset_engine_count(error, engine),
+		   i915_reset_count(error));
 
 	rcu_read_lock();
 
