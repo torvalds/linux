13a70f350665580708ab11f725d3578eaacbf2d0
Jin Yao <yao.jin@linux.intel.com>
2017-12-08 21:13:43 +0800

    perf tools: Create function to parse time percent
    Current perf report/script/... have a --time option to limit the time
    range of output. But right now it only supports absolute time, add
    support for time percentage.
    
    For example:
    
    1. Select the second 10% time slice
       perf report --time 10%/2
    
    2. Select from 0% to 10% time slice
       perf report --time 0%-10%
    
    It also support the multiple time ranges.
    
    3. Select the first and second 10% time slices
       perf report --time 10%/1,10%/2
    
    4. Select from 0% to 10% and 30% to 40% slices
       perf report --time 0%-10%,30%-40%
    
    Changelog:
    
    v4: An issue is found. Following passes.
        perf script --time 10%/10x12321xsdfdasfdsafdsafdsa
    
        Now it uses strtol to replace atoi.
    
    Committer notes:
    
    This just puts in place the infrastructure, so the examples in this cset
    comment will only work later, after more patches in this series are
    applied.
    
    Signed-off-by: Jin Yao <yao.jin@linux.intel.com>
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Kan Liang <kan.liang@intel.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1512738826-2628-4-git-send-email-yao.jin@linux.intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/time-utils.h
+++ tools/perf/util/time-utils.h
@@ -13,6 +13,9 @@ int parse_nsec_time(const char *str, u64 *ptime);
 
 int perf_time__parse_str(struct perf_time_interval *ptime, const char *ostr);
 
+int perf_time__percent_parse_str(struct perf_time_interval *ptime_buf, int num,
+				 const char *ostr, u64 start, u64 end);
+
 bool perf_time__skip_sample(struct perf_time_interval *ptime, u64 timestamp);
 
 int timestamp__scnprintf_usec(u64 timestamp, char *buf, size_t sz);
