2b83ff96f51d0b039c4561b9f95c824d7bddb85c
Matthieu CASTET <matthieu.castet@parrot.com>
2017-12-12 11:10:44 +0100

    led: core: Fix brightness setting when setting delay_off=0
    With the current code, the following sequence won't work :
    echo timer > trigger
    
    echo 0 >  delay_off
    * at this point we call
    ** led_delay_off_store
    ** led_blink_set
    *** stop timer
    ** led_blink_setup
    ** led_set_software_blink
    *** if !delay_on, led off
    *** if !delay_off, set led_set_brightness_nosleep <--- LED_BLINK_SW is set but timer is stop
    *** otherwise start timer/set LED_BLINK_SW flag
    
    echo xxx > brightness
    * led_set_brightness
    ** if LED_BLINK_SW
    *** if brightness=0, led off
    *** else apply brightness if next timer <--- timer is stop, and will never apply new setting
    ** otherwise set led_set_brightness_nosleep
    
    To fix that, when we delete the timer, we should clear LED_BLINK_SW.
    
    Cc: linux-leds@vger.kernel.org
    Signed-off-by: Matthieu CASTET <matthieu.castet@parrot.com>
    Signed-off-by: Jacek Anaszewski <jacek.anaszewski@gmail.com>

--- drivers/leds/led-core.c
+++ drivers/leds/led-core.c
@@ -186,7 +186,7 @@ void led_blink_set(struct led_classdev *led_cdev,
 		   unsigned long *delay_on,
 		   unsigned long *delay_off)
 {
-	del_timer_sync(&led_cdev->blink_timer);
+	led_stop_software_blink(led_cdev);
 
 	clear_bit(LED_BLINK_ONESHOT, &led_cdev->work_flags);
 	clear_bit(LED_BLINK_ONESHOT_STOP, &led_cdev->work_flags);
