f16de3c17fcbbcf01c81f2897e4b8fe4a5ca470c
Kamal Mostafa <kamal@canonical.com>
2017-12-13 11:43:14 -0800

    UBUNTU: [debian] support for ship_extras_package=false
    If optional .mk variable 'ship_extras_package' is explicitly set to false,
    then do not construct the linux-image-extra package; instead just log all
    of the "extra" modules which were pointlessly built yet won't be shipped.
    This feature may be useful for config debugging and for custom kernel
    development.
    
    Ignore: yes
    
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- debian/rules.d/2-binary-arch.mk
+++ debian/rules.d/2-binary-arch.mk
@@ -453,6 +453,7 @@ endif
 endif
 
 binary-%: pkgimg = $(bin_pkg_name)-$*
+binary-%: pkgdir_ex = $(CURDIR)/debian/$(extra_pkg_name)-$*
 binary-%: pkgimg_ex = $(extra_pkg_name)-$*
 binary-%: pkghdr = $(hdrs_pkg_name)-$*
 binary-%: dbgpkg = $(bin_pkg_name)-$*-dbgsym
@@ -477,6 +478,14 @@ binary-%: install-%
 	dh_builddeb -p$(pkgimg)
 
 ifeq ($(do_extras_package),true)
+  ifeq ($(ship_extras_package),false)
+	# If $(ship_extras_package) is explicitly set to false, then do not
+	# construct the linux-image-extra package; instead just log all of the
+	# "extra" modules which were pointlessly built yet won't be shipped.
+	find $(pkgdir_ex) -name '*.ko' | sort \
+		| sed 's|^$(pkgdir_ex)/|NOT-SHIPPED |' \
+		| tee -a $(target_flavour).not-shipped.log;
+  else
 	if [ -f $(DEBIAN)/control.d/$(target_flavour).inclusion-list ] ; then \
 		dh_installchangelogs -p$(pkgimg_ex); \
 		dh_installdocs -p$(pkgimg_ex); \
@@ -488,6 +497,7 @@ ifeq ($(do_extras_package),true)
 		dh_md5sums -p$(pkgimg_ex); \
 		dh_builddeb -p$(pkgimg_ex); \
 	fi
+  endif
 endif
 
 	dh_installchangelogs -p$(pkghdr)
