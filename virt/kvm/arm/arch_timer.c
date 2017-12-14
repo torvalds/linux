0eb7c33cadf6b2f1a94e58ded8b0eb89b4eba382
Christoffer Dall <christoffer.dall@linaro.org>
2017-12-15 00:30:12 +0100

    KVM: arm/arm64: Fix timer enable flow
    When enabling the timer on the first run, we fail to ever restore the
    state and mark it as loaded.  That means, that in the initial entry to
    the VCPU ioctl, unless we exit to userspace for some reason such as a
    pending signal, if the guest programs a timer and blocks, we will wait
    forever, because we never read back the hardware state (the loaded flag
    is not set), and so we think the timer is disabled, and we never
    schedule a background soft timer.
    
    The end result?  The VCPU blocks forever, and the only solution is to
    kill the thread.
    
    Fixes: 4a2c4da1250d ("arm/arm64: KVM: Load the timer state when enabling the timer")
    Reported-by: Marc Zyngier <marc.zyngier@arm.com>
    Reviewed-by: Marc Zyngier <marc.zyngier@arm.com>
    Tested-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- virt/kvm/arm/arch_timer.c
+++ virt/kvm/arm/arch_timer.c
@@ -846,10 +846,7 @@ int kvm_timer_enable(struct kvm_vcpu *vcpu)
 no_vgic:
 	preempt_disable();
 	timer->enabled = 1;
-	if (!irqchip_in_kernel(vcpu->kvm))
-		kvm_timer_vcpu_load_user(vcpu);
-	else
-		kvm_timer_vcpu_load_vgic(vcpu);
+	kvm_timer_vcpu_load(vcpu);
 	preempt_enable();
 
 	return 0;
