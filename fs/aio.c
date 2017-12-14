01a658e1e92745255bfcae1977adc2f563a159ac
Christoph Hellwig <hch@lst.de>
2017-12-14 14:25:02 +0100

    aio: don't print the page size at boot time
    The page size is in no way related to the aio code, and printing it in
    the (debug) dmesg at every boot serves no purpose.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Jeff Moyer <jmoyer@redhat.com>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/aio.c
+++ fs/aio.c
@@ -264,9 +264,6 @@ static int __init aio_setup(void)
 
 	kiocb_cachep = KMEM_CACHE(aio_kiocb, SLAB_HWCACHE_ALIGN|SLAB_PANIC);
 	kioctx_cachep = KMEM_CACHE(kioctx,SLAB_HWCACHE_ALIGN|SLAB_PANIC);
-
-	pr_debug("sizeof(struct page) = %zu\n", sizeof(struct page));
-
 	return 0;
 }
 __initcall(aio_setup);
