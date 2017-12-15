18cec7e0ddd5e28b7722f7049d715873373be3e9
Joel Fernandes <joelaf@google.com>
2017-12-15 07:39:44 -0800

    sched/fair: Remove impossible condition from find_idlest_group_cpu()
    find_idlest_group_cpu() goes through CPUs of a group previous selected by
    find_idlest_group(). find_idlest_group() returns NULL if the local group is the
    selected one and doesn't execute find_idlest_group_cpu if the group to which
    'cpu' belongs to is chosen. So we're always guaranteed to call
    find_idlest_group_cpu() with a group to which 'cpu' is non-local.
    
    This makes one of the conditions in find_idlest_group_cpu() an impossible one,
    which we can get rid off.
    
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Brendan Jackman <brendan.jackman@arm.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Android Kernel <kernel-team@android.com>
    Cc: Atish Patra <atish.patra@oracle.com>
    Cc: Chris Redpath <Chris.Redpath@arm.com>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: EAS Dev <eas-dev@lists.linaro.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Josef Bacik <jbacik@fb.com>
    Cc: Juri Lelli <juri.lelli@arm.com>
    Cc: Len Brown <lenb@kernel.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Morten Ramussen <morten.rasmussen@arm.com>
    Cc: Patrick Bellasi <patrick.bellasi@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rafael J. Wysocki <rjw@rjwysocki.net>
    Cc: Rohit Jain <rohit.k.jain@oracle.com>
    Cc: Saravana Kannan <skannan@quicinc.com>
    Cc: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Cc: Steve Muckle <smuckle@google.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vikram Mulukutla <markivx@codeaurora.org>
    Cc: Viresh Kumar <viresh.kumar@linaro.org>
    Link: http://lkml.kernel.org/r/20171215153944.220146-3-joelaf@google.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5950,7 +5950,7 @@ find_idlest_group_cpu(struct sched_group *group, struct task_struct *p, int this
 			}
 		} else if (shallowest_idle_cpu == -1) {
 			load = weighted_cpuload(cpu_rq(i));
-			if (load < min_load || (load == min_load && i == this_cpu)) {
+			if (load < min_load) {
 				min_load = load;
 				least_loaded_cpu = i;
 			}
