29d6b29f5040d3385510bd9766f5f7823537b26a
Sean Nyekjaer <sean.nyekjaer@prevas.dk>
2017-12-15 08:33:32 +0100

    mtd: spi-nor: indent issi section
    Signed-off-by: Sean Nyekjaer <sean.nyekjaer@prevas.dk>
    Signed-off-by: Cyrille Pitchen <cyrille.pitchen@wedev4u.fr>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -1045,10 +1045,10 @@ static const struct flash_info spi_nor_ids[] = {
 	{ "640s33b",  INFO(0x898913, 0, 64 * 1024, 128, 0) },
 
 	/* ISSI */
-	{ "is25cd512", INFO(0x7f9d20, 0, 32 * 1024,   2, SECT_4K) },
-	{ "is25lq040b", INFO(0x9d4013, 0, 64 * 1024,  8,
+	{ "is25cd512",  INFO(0x7f9d20, 0, 32 * 1024,   2, SECT_4K) },
+	{ "is25lq040b", INFO(0x9d4013, 0, 64 * 1024,   8,
 			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
-	{ "is25lp128", INFO(0x9d6018, 0, 64 * 1024, 256,
+	{ "is25lp128",  INFO(0x9d6018, 0, 64 * 1024, 256,
 			SECT_4K | SPI_NOR_DUAL_READ) },
 
 	/* Macronix */
