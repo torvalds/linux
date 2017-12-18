fa2d41adb953235c4acaa98f6c980fd9eabe0062
Colin Ian King <colin.king@canonical.com>
2017-12-18 17:47:07 +0000

    bpf: make function skip_callee static and return NULL rather than 0
    Function skip_callee is local to the source and does not need to
    be in global scope, so make it static. Also return NULL rather than 0.
    Cleans up two sparse warnings:
    
    symbol 'skip_callee' was not declared. Should it be static?
    Using plain integer as NULL pointer
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -823,6 +823,7 @@ next:
 	return 0;
 }
 
+static
 struct bpf_verifier_state *skip_callee(struct bpf_verifier_env *env,
 				       const struct bpf_verifier_state *state,
 				       struct bpf_verifier_state *parent,
@@ -867,7 +868,7 @@ bug:
 	verbose(env, "verifier bug regno %d tmp %p\n", regno, tmp);
 	verbose(env, "regno %d parent frame %d current frame %d\n",
 		regno, parent->curframe, state->curframe);
-	return 0;
+	return NULL;
 }
 
 static int mark_reg_read(struct bpf_verifier_env *env,
