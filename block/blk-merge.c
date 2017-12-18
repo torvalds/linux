6a501bf0807b5dc024fe52a4f956800a352c39ab
Ming Lei <ming.lei@redhat.com>
2017-12-18 20:22:14 +0800

    blk-merge: compute bio->bi_seg_front_size efficiently
    It is enough to check and compute bio->bi_seg_front_size just
    after the 1st segment is found, but current code checks that
    for each bvec, which is inefficient.
    
    This patch follows the way in  __blk_recalc_rq_segments()
    for computing bio->bi_seg_front_size, and it is more efficient
    and code becomes more readable too.
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-merge.c
+++ block/blk-merge.c
@@ -146,13 +146,14 @@ static struct bio *blk_bio_segment_split(struct request_queue *q,
 			bvprvp = &bvprv;
 			sectors += bv.bv_len >> 9;
 
-			if (nsegs == 1 && seg_size > front_seg_size)
-				front_seg_size = seg_size;
 			continue;
 		}
 new_segment:
 		if (nsegs == queue_max_segments(q))
 			goto split;
+
+		if (nsegs == 1 && seg_size > front_seg_size)
+			front_seg_size = seg_size;
 
 		nsegs++;
 		bvprv = bv;
@@ -160,8 +161,6 @@ new_segment:
 		seg_size = bv.bv_len;
 		sectors += bv.bv_len >> 9;
 
-		if (nsegs == 1 && seg_size > front_seg_size)
-			front_seg_size = seg_size;
 	}
 
 	do_split = false;
@@ -174,6 +173,8 @@ split:
 			bio = new;
 	}
 
+	if (nsegs == 1 && seg_size > front_seg_size)
+		front_seg_size = seg_size;
 	bio->bi_seg_front_size = front_seg_size;
 	if (seg_size > bio->bi_seg_back_size)
 		bio->bi_seg_back_size = seg_size;
