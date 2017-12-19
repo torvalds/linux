9fe712df08ea805868789bb6800f2226f5fd5285
Horia Geantă <horia.geanta@nxp.com>
2017-12-19 12:16:06 +0200

    crypto: caam - save Era in driver's private data
    Save Era in driver's private data for further usage,
    like deciding whether an erratum applies or a feature is available
    based on its value.
    
    Signed-off-by: Horia Geantă <horia.geanta@nxp.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/caam/intern.h
+++ drivers/crypto/caam/intern.h
@@ -84,6 +84,7 @@ struct caam_drv_private {
 	u8 qi_present;		/* Nonzero if QI present in device */
 	int secvio_irq;		/* Security violation interrupt number */
 	int virt_en;		/* Virtualization enabled in CAAM */
+	int era;		/* CAAM Era (internal HW revision) */
 
 #define	RNG4_MAX_HANDLES 2
 	/* RNG4 block */
