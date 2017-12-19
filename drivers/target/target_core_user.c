3a9a64a25f5f6506cec2011f71faf48320929727
Mike Christie <mchristi@redhat.com>
2017-12-19 04:03:56 -0600

    target_core_user: add cmd id to broken ring message
    Log cmd id that was not found in the tcmu_handle_completions
    lookup failure path.
    
    Signed-off-by: Mike Christie <mchristi@redhat.com>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>

--- drivers/target/target_core_user.c
+++ drivers/target/target_core_user.c
@@ -1098,7 +1098,8 @@ static unsigned int tcmu_handle_completions(struct tcmu_dev *udev)
 
 		cmd = idr_remove(&udev->commands, entry->hdr.cmd_id);
 		if (!cmd) {
-			pr_err("cmd_id not found, ring is broken\n");
+			pr_err("cmd_id %u not found, ring is broken\n",
+			       entry->hdr.cmd_id);
 			set_bit(TCMU_DEV_BIT_BROKEN, &udev->flags);
 			break;
 		}
