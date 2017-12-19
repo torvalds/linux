7f05b467a735aba1476d9ae8e0ae9d9d8e60066c
Shannon Nelson <shannon.nelson@oracle.com>
2017-12-19 15:35:47 -0800

    xfrm: check for xdo_dev_state_free
    The current XFRM code assumes that we've implemented the
    xdo_dev_state_free() callback, even if it is meaningless to the driver.
    This patch adds a check for it before calling, as done in other APIs,
    to prevent a NULL function pointer kernel crash.
    
    Signed-off-by: Shannon Nelson <shannon.nelson@oracle.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- include/net/xfrm.h
+++ include/net/xfrm.h
@@ -1933,7 +1933,8 @@ static inline void xfrm_dev_state_free(struct xfrm_state *x)
 	 struct net_device *dev = xso->dev;
 
 	if (dev && dev->xfrmdev_ops) {
-		dev->xfrmdev_ops->xdo_dev_state_free(x);
+		if (dev->xfrmdev_ops->xdo_dev_state_free)
+			dev->xfrmdev_ops->xdo_dev_state_free(x);
 		xso->dev = NULL;
 		dev_put(dev);
 	}
