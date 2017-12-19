842b854459462a6065d3595f8485601800bbf5d3
Shih-Yuan Lee (FourDollars) <sylee@canonical.com>
2017-12-19 17:39:49 +0800

    platform/x86: dell-wmi: Add an event created by Dell Latitude 5495
    The Dell Latitude 5495 has the mic mute key.
    
    Signed-off-by: Shih-Yuan Lee (FourDollars) <sylee@canonical.com>
    Reviewed-by: Pali Rohár <pali.rohar@gmail.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/platform/x86/dell-wmi.c
+++ drivers/platform/x86/dell-wmi.c
@@ -261,6 +261,9 @@ static const u16 bios_to_linux_keycode[256] = {
  * override them.
  */
 static const struct key_entry dell_wmi_keymap_type_0010[] = {
+	/* Mic mute */
+	{ KE_KEY, 0x150, { KEY_MICMUTE } },
+
 	/* Fn-lock */
 	{ KE_IGNORE, 0x151, { KEY_RESERVED } },
 
