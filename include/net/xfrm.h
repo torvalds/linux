2271d5190ec60b06921c2e4e184fd1f4fad4e634
Steffen Klassert <steffen.klassert@secunet.com>
2017-12-20 10:41:48 +0100

    xfrm: Allow IPsec GSO with software crypto for local sockets.
    With support of async crypto operations in the GSO codepath
    we have everything in place to allow GSO for local sockets.
    This patch enables the GSO codepath.
    
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- include/net/xfrm.h
+++ include/net/xfrm.h
@@ -1910,6 +1910,8 @@ static inline bool xfrm_dst_offload_ok(struct dst_entry *dst)
 		return false;
 
 	xdst = (struct xfrm_dst *) dst;
+	if (!x->xso.offload_handle && !xdst->child->xfrm)
+		return true;
 	if (x->xso.offload_handle && (x->xso.dev == xfrm_dst_path(dst)->dev) &&
 	    !xdst->child->xfrm)
 		return true;
