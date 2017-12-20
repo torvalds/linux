926aae273019cc137ed3711077b89a71319e5983
Bryan Tan <bryantan@vmware.com>
2017-12-20 09:50:57 -0800

    RDMA/vmw_pvrdma: Add UAR SRQ macros in ABI header file
    Support for SRQs were added in the vmw_pvrdma userlevel library
    before two necessary macros were added into the kernel ABI header
    file. Add the two UAR SRQ macros that are required by the userlevel
    library so that the library can rely on the kernel ABI header file
    for these SRQ macro definitions.
    
    Fixes: 8b10ba783c9d ("RDMA/vmw_pvrdma: Add shared receive queue support")
    Reviewed-by: Adit Ranadive <aditr@vmware.com>
    Reviewed-by: Aditya Sarwade <asarwade@vmware.com>
    Reviewed-by: Jorgen Hansen <jhansen@vmware.com>
    Signed-off-by: Bryan Tan <bryantan@vmware.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- include/uapi/rdma/vmw_pvrdma-abi.h
+++ include/uapi/rdma/vmw_pvrdma-abi.h
@@ -58,6 +58,8 @@
 #define PVRDMA_UAR_CQ_ARM_SOL		BIT(29)		/* Arm solicited bit. */
 #define PVRDMA_UAR_CQ_ARM		BIT(30)		/* Arm bit. */
 #define PVRDMA_UAR_CQ_POLL		BIT(31)		/* Poll bit. */
+#define PVRDMA_UAR_SRQ_OFFSET		8		/* SRQ doorbell. */
+#define PVRDMA_UAR_SRQ_RECV		BIT(30)		/* Recv bit. */
 
 enum pvrdma_wr_opcode {
 	PVRDMA_WR_RDMA_WRITE,
