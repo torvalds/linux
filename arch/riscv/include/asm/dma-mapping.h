b8ee205af46cd060648814bc248fe57d85f61ac9
Christoph Hellwig <hch@lst.de>
2017-12-20 15:56:04 +0100

    riscv: remove the unused dma_capable helper
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- arch/riscv/include/asm/dma-mapping.h
+++ arch/riscv/include/asm/dma-mapping.h
@@ -27,12 +27,4 @@ static inline const struct dma_map_ops *get_arch_dma_ops(struct bus_type *bus)
 	return &dma_noop_ops;
 }
 
-static inline bool dma_capable(struct device *dev, dma_addr_t addr, size_t size)
-{
-	if (!dev->dma_mask)
-		return false;
-
-	return addr + size - 1 <= *dev->dma_mask;
-}
-
 #endif	/* __ASM_RISCV_DMA_MAPPING_H */
