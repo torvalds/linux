95bff4b580e7e6c895c5ecbc0c9f703635c2972d
Steffen Klassert <steffen.klassert@secunet.com>
2017-12-20 10:41:42 +0100

    xfrm: Allow to use the layer2 IPsec GSO codepath for software crypto.
    We now have support for asynchronous crypto operations in the layer 2 TX
    path. This was the missing part to allow the GSO codepath for software
    crypto, so allow this codepath now.
    
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- net/xfrm/xfrm_device.c
+++ net/xfrm/xfrm_device.c
@@ -202,8 +202,8 @@ bool xfrm_dev_offload_ok(struct sk_buff *skb, struct xfrm_state *x)
 	if (!x->type_offload || x->encap)
 		return false;
 
-	if ((x->xso.offload_handle && (dev == xfrm_dst_path(dst)->dev)) &&
-	     !xdst->child->xfrm && x->type->get_mtu) {
+	if ((!dev || (x->xso.offload_handle && (dev == xfrm_dst_path(dst)->dev))) &&
+	     (!xdst->child->xfrm && x->type->get_mtu)) {
 		mtu = x->type->get_mtu(x, xdst->child_mtu_cached);
 
 		if (skb->len <= mtu)
