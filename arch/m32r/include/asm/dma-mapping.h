8c10fd7f88759e49dd0ae7145ebe9ce89da1c392
Christoph Hellwig <hch@lst.de>
2017-12-20 15:55:40 +0100

    m32r: remove the unused dma_capable helper
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- arch/m32r/include/asm/dma-mapping.h
+++ arch/m32r/include/asm/dma-mapping.h
@@ -14,11 +14,4 @@ static inline const struct dma_map_ops *get_arch_dma_ops(struct bus_type *bus)
 	return &dma_noop_ops;
 }
 
-static inline bool dma_capable(struct device *dev, dma_addr_t addr, size_t size)
-{
-	if (!dev->dma_mask)
-		return false;
-	return addr + size - 1 <= *dev->dma_mask;
-}
-
 #endif /* _ASM_M32R_DMA_MAPPING_H */
