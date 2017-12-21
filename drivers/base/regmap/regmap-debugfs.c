a5ba91c380b8bcca21e6166fc71c5e5ac9f0db68
Bartosz Golaszewski <brgl@bgdev.pl>
2017-12-21 12:12:50 +0100

    regmap: debugfs: emit a debug message when locking is disabled
    We currently silently omit creating the debugfs entries when regmap
    locking is disabled. Users may not be aware of the reason for which
    regmap files don't show up in debugfs. Add a dev_dbg() message
    explaining that.
    
    Signed-off-by: Bartosz Golaszewski <brgl@bgdev.pl>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/base/regmap/regmap-debugfs.c
+++ drivers/base/regmap/regmap-debugfs.c
@@ -529,8 +529,10 @@ void regmap_debugfs_init(struct regmap *map, const char *name)
 	struct regmap_range_node *range_node;
 	const char *devname = "dummy";
 
-	if (map->debugfs_disable)
+	if (map->debugfs_disable) {
+		dev_dbg(map->dev, "regmap locking disabled - not creating debugfs entries\n");
 		return;
+	}
 
 	/* If we don't have the debugfs root yet, postpone init */
 	if (!regmap_debugfs_root) {
