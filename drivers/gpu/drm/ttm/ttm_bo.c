6a83a553033d6c3d8fb4683a9c87833ace44121b
Christian König <christian.koenig@amd.com>
2017-12-21 19:04:15 +0100

    drm/ttm: drop the spin in delayed delete if the trylock doesn't work
    Thomas actually noticed that, but I didn't realized what he meant until
    now.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Roger He <Hongbo.He@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/ttm/ttm_bo.c
+++ drivers/gpu/drm/ttm/ttm_bo.c
@@ -592,6 +592,8 @@ static bool ttm_bo_delayed_delete(struct ttm_bo_device *bdev, bool remove_all)
 
 		} else if (reservation_object_trylock(bo->resv)) {
 			ttm_bo_cleanup_refs(bo, false, !remove_all, true);
+		} else {
+			spin_unlock(&glob->lru_lock);
 		}
 
 		kref_put(&bo->list_kref, ttm_bo_release_list);
