20bfe9784bc491ed84e21e285e96b47ebec1fe6e
Octavian Purdila <tavi@cs.pub.ro>
2017-12-21 13:58:30 +0100

    lkl tools: jenkins test script: use the new test runner
    Signed-off-by: Octavian Purdila <tavi@cs.pub.ro>

--- tools/lkl/scripts/lkl-jenkins.sh
+++ tools/lkl/scripts/lkl-jenkins.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-export LKL_TEST_DHCP=1
+script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+basedir=$(cd $script_dir/../../..; pwd)
 
-make mrproper
-cd tools/lkl
-make -j4
 export PATH=$PATH:/sbin
-sudo killall netserver || true
-make test
-make clean
-CROSS_COMPILE=i686-w64-mingw32- make -j4
-CROSS_COMPILE=i686-w64-mingw32- make test
-cd ../..
-./tools/lkl/scripts/checkpatch.sh
+
+build_and_test()
+{
+    cd $basedir
+    make mrproper
+    cd tools/lkl
+    make clean-conf
+    make -j4
+    ./tests/run.py
+}
+
+build_and_test
+CROSS_COMPILE=i686-w64-mingw32- build_and_test
