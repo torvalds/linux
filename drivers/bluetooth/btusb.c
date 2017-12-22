7f3e85d820fd170f9cb94a8a036c824cf1b29832
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2017-12-22 08:43:23 +0100

    Revert "Bluetooth: btusb: driver to enable the usb-wakeup feature"
    This reverts commit ffc7565746bb2fc063fc0f484c55ef6d8951a6ee which is
    commit a0085f2510e8976614ad8f766b209448b385492f upstream.
    
    It causes problems with working systems, as noted by a number of the
    ChromeOS developers.
    
    Cc: Sukumar Ghorai <sukumar.ghorai@intel.com>
    Cc: Amit K Bag <amit.k.bag@intel.com>
    Cc: Oliver Neukum <oneukum@suse.com>
    Cc: Marcel Holtmann <marcel@holtmann.org>
    Cc: Matthias Kaehlcke <mka@chromium.org>
    Reported-by: Guenter Roeck <linux@roeck-us.net>
    Reported-by: Brian Norris <briannorris@chromium.org>
    Acked-by: Brian Norris <briannorris@chromium.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -1050,10 +1050,6 @@ static int btusb_open(struct hci_dev *hdev)
 		return err;
 
 	data->intf->needs_remote_wakeup = 1;
-	/* device specific wakeup source enabled and required for USB
-	 * remote wakeup while host is suspended
-	 */
-	device_wakeup_enable(&data->udev->dev);
 
 	if (test_and_set_bit(BTUSB_INTR_RUNNING, &data->flags))
 		goto done;
@@ -1117,7 +1113,6 @@ static int btusb_close(struct hci_dev *hdev)
 		goto failed;
 
 	data->intf->needs_remote_wakeup = 0;
-	device_wakeup_disable(&data->udev->dev);
 	usb_autopm_put_interface(data->intf);
 
 failed:
