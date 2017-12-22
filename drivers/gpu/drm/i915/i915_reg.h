25da77f83068bfc95d08ddfb8e6c53983901e278
Oscar Mateo <oscar.mateo@intel.com>
2017-12-22 14:38:49 -0800

    drm/i915: Stop getting the fault address from RING_FAULT_REG
    This register does not contain it. Instead, we have to look into FAULT_TLB_DATA0 & 1
    (where, by the way, we can also get the address space).
    
    v2: Right formatting
    v3:
      - Use 12 (as per the register format) instead of PAGE_SIZE (Chris)
      - s/BITS_44_TO_47/HIGHBITS (Chris)
      - Right formatting, this time for real
    
    Fixes: b03ec3d67ab8 ("drm/i915: There is only one fault register from GEN8 onwards")
    Signed-off-by: Oscar Mateo <oscar.mateo@intel.com>
    Cc: Michel Thierry <michel.thierry@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/1513982329-32191-1-git-send-email-oscar.mateo@intel.com
    Reviewed-by: Michel Thierry <michel.thierry@intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    (cherry picked from commit 5a3f58dfd1420347be838546e00a4a9e847bda30)
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -2489,6 +2489,8 @@ enum i915_power_well_id {
 
 #define GEN8_FAULT_TLB_DATA0		_MMIO(0x4b10)
 #define GEN8_FAULT_TLB_DATA1		_MMIO(0x4b14)
+#define   FAULT_VA_HIGH_BITS		(0xf << 0)
+#define   FAULT_GTT_SEL			(1 << 4)
 
 #define FPGA_DBG		_MMIO(0x42300)
 #define   FPGA_DBG_RM_NOCLAIM	(1<<31)
