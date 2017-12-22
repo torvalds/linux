3c1490913f3bcf750261649e7ffd62390187058c
Sudip Mukherjee <sudipm.mukherjee@gmail.com>
2017-12-22 15:52:05 +0000

    net: sch: api: fix tcf_block_get
    The build of mips bcm47xx_defconfig is failing with the error:
    net/sched/sch_fq_codel.c: In function 'fq_codel_init':
    net/sched/sch_fq_codel.c:487:8: error:
    	too many arguments to function 'tcf_block_get'
    
    While adding the extack support, the commit missed adding it in the
    headers when CONFIG_NET_CLS is not defined.
    
    Fixes: 8d1a77f974ca ("net: sch: api: add extack support in tcf_block_get")
    Signed-off-by: Sudip Mukherjee <sudipm.mukherjee@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/pkt_cls.h
+++ include/net/pkt_cls.h
@@ -79,7 +79,8 @@ int tcf_classify(struct sk_buff *skb, const struct tcf_proto *tp,
 #else
 static inline
 int tcf_block_get(struct tcf_block **p_block,
-		  struct tcf_proto __rcu **p_filter_chain, struct Qdisc *q)
+		  struct tcf_proto __rcu **p_filter_chain, struct Qdisc *q,
+		  struct netlink_ext_ack *extack)
 {
 	return 0;
 }
