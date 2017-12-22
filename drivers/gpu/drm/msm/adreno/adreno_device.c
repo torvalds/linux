9dcfbc182f1aac0aa5ea194733d21e67dd2ba1fd
Archit Taneja <architt@codeaurora.org>
2017-12-22 15:41:13 +0530

    drm/msm: Fix NULL deref in adreno_load_gpu
    The msm/kms driver should work even if there is no GPU device specified
    in DT. Currently, we get a NULL dereference crash in adreno_load_gpu
    since the driver assumes that priv->gpu_pdev is non-NULL.
    
    Perform an additional check on priv->gpu_pdev before trying to retrieve
    the msm_gpu pointer from it.
    
    v2: Incorporate Jordan's comments:
    - Simplify the check to share the same error message.
    - Use dev_err_once() to avoid an error message every time we open the
      drm device fd.
    
    Fixes: eec874ce5ff1 (drm/msm/adreno: load gpu at probe/bind time)
    
    Signed-off-by: Archit Taneja <architt@codeaurora.org>
    Acked-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/adreno/adreno_device.c
+++ drivers/gpu/drm/msm/adreno/adreno_device.c
@@ -130,11 +130,14 @@ struct msm_gpu *adreno_load_gpu(struct drm_device *dev)
 {
 	struct msm_drm_private *priv = dev->dev_private;
 	struct platform_device *pdev = priv->gpu_pdev;
-	struct msm_gpu *gpu = platform_get_drvdata(priv->gpu_pdev);
+	struct msm_gpu *gpu = NULL;
 	int ret;
 
+	if (pdev)
+		gpu = platform_get_drvdata(pdev);
+
 	if (!gpu) {
-		dev_err(dev->dev, "no adreno device\n");
+		dev_err_once(dev->dev, "no GPU device was found\n");
 		return NULL;
 	}
 
