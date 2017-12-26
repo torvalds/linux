d97c2e0d635e39b5b63784deb3212e846ebf76dc
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2017-12-26 01:50:20 +0100

    PM / wakeup: Drop redundant check from device_set_wakeup_enable()
    Since both device_wakeup_enable() and device_wakeup_disable() check
    if dev is not NULL and whether or not power.can_wakeup is set for it,
    device_set_wakeup_enable() doesn't have to do that, so drop that
    check from it.
    
    No intentional changes in functionality.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -464,9 +464,6 @@ EXPORT_SYMBOL_GPL(device_init_wakeup);
  */
 int device_set_wakeup_enable(struct device *dev, bool enable)
 {
-	if (!dev || !dev->power.can_wakeup)
-		return -EINVAL;
-
 	return enable ? device_wakeup_enable(dev) : device_wakeup_disable(dev);
 }
 EXPORT_SYMBOL_GPL(device_set_wakeup_enable);
