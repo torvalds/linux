869de5cf96cf3952e8f998581cd368e38cebf8d2
Igor Grinberg <grinberg@compulab.co.il>
2017-12-26 15:30:36 +0200

    clk: pxa: unbreak lookup of CLK_POUT
    Since switching to clk drivers, the CLK_POUT cannot be searched for by
    clk_get() API and thus it returns with ENOENT.
    Register it with the clk_lookup and thus unbreak the users of it.
    
    Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>
    Acked-by: Robert Jarzmik <robert.jarzmik@free.fr>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/pxa/clk-pxa3xx.c
+++ drivers/clk/pxa/clk-pxa3xx.c
@@ -329,12 +329,16 @@ static void __init pxa3xx_dummy_clocks_init(void)
 
 static void __init pxa3xx_base_clocks_init(void)
 {
+	struct clk *clk;
+
 	pxa3xx_register_plls();
 	pxa3xx_register_core();
 	clk_register_clk_pxa3xx_system_bus();
 	clk_register_clk_pxa3xx_ac97();
 	clk_register_clk_pxa3xx_smemc();
-	clk_register_gate(NULL, "CLK_POUT", "osc_13mhz", 0, OSCC, 11, 0, NULL);
+	clk = clk_register_gate(NULL, "CLK_POUT",
+				"osc_13mhz", 0, OSCC, 11, 0, NULL);
+	clk_register_clkdev(clk, "CLK_POUT", NULL);
 	clkdev_pxa_register(CLK_OSTIMER, "OSTIMER0", NULL,
 			    clk_register_fixed_factor(NULL, "os-timer0",
 						      "osc_13mhz", 0, 1, 4));
