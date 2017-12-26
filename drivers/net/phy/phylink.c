5837f963300b63264dd1baf32e96063e807ca28c
Russell King <rmk+kernel@armlinux.org.uk>
2017-12-26 23:15:12 +0000

    phylink: ensure we report link down when LOS asserted
    [ Upstream commit ac817f5ad066697e4d4d35ec68c974eba2c5f17a ]
    
    Although we disable the netdev carrier, we fail to report in the kernel
    log that the link went down.  Fix this.
    
    Fixes: 9525ae83959b ("phylink: add phylink infrastructure")
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/phy/phylink.c
+++ drivers/net/phy/phylink.c
@@ -1428,9 +1428,8 @@ static void phylink_sfp_link_down(void *upstream)
 	WARN_ON(!lockdep_rtnl_is_held());
 
 	set_bit(PHYLINK_DISABLE_LINK, &pl->phylink_disable_state);
+	queue_work(system_power_efficient_wq, &pl->resolve);
 	flush_work(&pl->resolve);
-
-	netif_carrier_off(pl->netdev);
 }
 
 static void phylink_sfp_link_up(void *upstream)
