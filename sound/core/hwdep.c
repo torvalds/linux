446bd647ceee73fbed50404daece9cbcec751f66
Al Viro <viro@zeniv.linux.org.uk>
2017-12-28 16:30:22 -0500

    snd_hwdep_dsp_load(): don't bother with access_ok()
    the only remaining instance of ->dsp_load() doesn't need it.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- sound/core/hwdep.c
+++ sound/core/hwdep.c
@@ -233,8 +233,6 @@ static int snd_hwdep_dsp_load(struct snd_hwdep *hw,
 	/* check whether the dsp was already loaded */
 	if (hw->dsp_loaded & (1 << info.index))
 		return -EBUSY;
-	if (!access_ok(VERIFY_READ, info.image, info.length))
-		return -EFAULT;
 	err = hw->ops.dsp_load(hw, &info);
 	if (err < 0)
 		return err;
