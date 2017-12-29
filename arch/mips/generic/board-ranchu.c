f2d0b0d5c1718487f8a27d3ca3ec0f05ca3adedd
Miodrag Dinic <miodrag.dinic@mips.com>
2017-12-29 16:41:47 +0100

    MIPS: ranchu: Add Ranchu as a new generic-based board
    Provide amendments to the MIPS generic platform framework so that
    the new generic-based board Ranchu can be chosen to be built.
    
    The Ranchu board is intended to be used by Android emulator. The name
    "Ranchu" originates from Android development community. "Goldfish" and
    "Ranchu" are terms used for two generations of virtual boards used by
    Android emulator. The name "Ranchu" is a newer one among the two, and
    this patch deals with Ranchu. However, for historical reasons, some
    devices/drivers still contain the name "Goldfish".
    
    MIPS Ranchu machine includes a number of Goldfish devices. The support
    for Virtio devices is also included. Ranchu board supports up to 16
    Virtio devices which can be attached using Virtio MMIO Bus. This is
    summarized in the following picture:
    
           ABUS
            ||----MIPS CPU
            ||       |                    IRQs
            ||----Goldfish PIC------------(32)--------
            ||                     | | | | | | | | |
            ||----Goldfish TTY------ | | | | | | | |
            ||                       | | | | | | | |
            ||----Goldfish RTC-------- | | | | | | |
            ||                         | | | | | | |
            ||----Goldfish FB----------- | | | | | |
            ||                           | | | | | |
            ||----Goldfish Events--------- | | | | |
            ||                             | | | | |
            ||----Goldfish Audio------------ | | | |
            ||                               | | | |
            ||----Goldfish Battery------------ | | |
            ||                                 | | |
            ||----Android PIPE------------------ | |
            ||                                   | |
            ||----Virtio MMIO Bus                | |
            ||    |    |    |                    | |
            ||    |    |   (virtio-block)--------- |
            ||   (16)  |                           |
            ||    |   (virtio-net)------------------
    
    Device Tree is created on the QEMU side based on the information about
    devices IO map and IRQ numbers. Kernel will load this DTB using UHI
    boot protocol DTB handover mode.
    
    Signed-off-by: Miodrag Dinic <miodrag.dinic@mips.com>
    Signed-off-by: Goran Ferenc <goran.ferenc@mips.com>
    Signed-off-by: Aleksandar Markovic <aleksandar.markovic@mips.com>
    Reviewed-by: James Hogan <jhogan@kernel.org>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: Paul Burton <paul.burton@mips.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/18138/
    Signed-off-by: James Hogan <jhogan@kernel.org>

---
+++ arch/mips/generic/board-ranchu.c
@@ -0,0 +1,92 @@
+/*
+ * Support code for virtual Ranchu board for MIPS.
+ *
+ * Author: Miodrag Dinic <miodrag.dinic@mips.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
+ */
+
+#include <linux/of_address.h>
+#include <linux/types.h>
+
+#include <asm/machine.h>
+#include <asm/mipsregs.h>
+#include <asm/time.h>
+
+#define GOLDFISH_TIMER_LOW		0x00
+#define GOLDFISH_TIMER_HIGH		0x04
+
+static __init u64 read_rtc_time(void __iomem *base)
+{
+	u32 time_low;
+	u32 time_high;
+
+	/*
+	 * Reading the low address latches the high value
+	 * as well so there is no fear that we may read
+	 * inaccurate high value.
+	 */
+	time_low = readl(base + GOLDFISH_TIMER_LOW);
+	time_high = readl(base + GOLDFISH_TIMER_HIGH);
+
+	return ((u64)time_high << 32) | time_low;
+}
+
+static __init unsigned int ranchu_measure_hpt_freq(void)
+{
+	u64 rtc_start, rtc_current, rtc_delta;
+	unsigned int start, count;
+	struct device_node *np;
+	void __iomem *rtc_base;
+
+	np = of_find_compatible_node(NULL, NULL, "google,goldfish-rtc");
+	if (!np)
+		panic("%s(): Failed to find 'google,goldfish-rtc' dt node!",
+		      __func__);
+
+	rtc_base = of_iomap(np, 0);
+	if (!rtc_base)
+		panic("%s(): Failed to ioremap Goldfish RTC base!", __func__);
+
+	/*
+	 * Poll the nanosecond resolution RTC for one
+	 * second to calibrate the CPU frequency.
+	 */
+	rtc_start = read_rtc_time(rtc_base);
+	start = read_c0_count();
+
+	do {
+		rtc_current = read_rtc_time(rtc_base);
+		rtc_delta = rtc_current - rtc_start;
+	} while (rtc_delta < NSEC_PER_SEC);
+
+	count = read_c0_count() - start;
+
+	/*
+	 * Make sure the frequency will be a round number.
+	 * Without this correction, the returned value may vary
+	 * between subsequent emulation executions.
+	 *
+	 * TODO: Set this value using device tree.
+	 */
+	count += 5000;
+	count -= count % 10000;
+
+	iounmap(rtc_base);
+
+	return count;
+}
+
+static const struct of_device_id ranchu_of_match[] __initconst = {
+	{
+		.compatible = "mti,ranchu",
+	},
+};
+
+MIPS_MACHINE(ranchu) = {
+	.matches = ranchu_of_match,
+	.measure_hpt_freq = ranchu_measure_hpt_freq,
+};
