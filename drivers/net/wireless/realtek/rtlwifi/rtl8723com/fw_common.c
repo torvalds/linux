874e837d67d0db179c9771f38fd21df07c703e93
Ping-Ke Shih <pkshih@realtek.com>
2017-12-29 16:31:12 +0800

    rtlwifi: fill FW version and subversion
    This commit fills FW version information of RTL8188EE and RTL8723, so
    the btcoex can cooperate with FW correctly.
    Also, we can display this version in debugfs.
    
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtlwifi/rtl8723com/fw_common.c
+++ drivers/net/wireless/realtek/rtlwifi/rtl8723com/fw_common.c
@@ -200,6 +200,8 @@ int rtl8723_download_fw(struct ieee80211_hw *hw,
 		return 1;
 
 	pfwheader = (struct rtlwifi_firmware_header *)rtlhal->pfirmware;
+	rtlhal->fw_version = le16_to_cpu(pfwheader->version);
+	rtlhal->fw_subversion = pfwheader->subversion;
 	pfwdata = rtlhal->pfirmware;
 	fwsize = rtlhal->fwsize;
 
