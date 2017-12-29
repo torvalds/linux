981a2b6e055bce9a9f780fb074ffdf9a9bab0d47
Ping-Ke Shih <pkshih@realtek.com>
2017-12-29 16:31:09 +0800

    rtlwifi: rtl8723: Add error handling to no existing firmware
    Without firmware, driver wastes time to download and wait for MCU bootup,
    and then kernel core dump finally. If request_firmware fails, the value
    max_fw_size=0 is set, so we check the value before downloading firmware.
    
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtlwifi/rtl8723com/fw_common.c
+++ drivers/net/wireless/realtek/rtlwifi/rtl8723com/fw_common.c
@@ -196,7 +196,7 @@ int rtl8723_download_fw(struct ieee80211_hw *hw,
 	enum version_8723e version = rtlhal->version;
 	int max_page;
 
-	if (!rtlhal->pfirmware)
+	if (rtlpriv->max_fw_size == 0 || !rtlhal->pfirmware)
 		return 1;
 
 	pfwheader = (struct rtlwifi_firmware_header *)rtlhal->pfirmware;
