5c65a2fdb36b21c3f6ac534c5e12f81cf67726b1
Elad Wexler <elad.wexler@gmail.com>
2017-12-30 18:11:51 +0200

    tools: usb: usbip_device_driver: prefer 'unsigned int' to 'unsigned'
    Fixup a coding style issue
    
    Signed-off-by: Elad Wexler <elad.wexler@gmail.com>
    Acked-by: Shuah Khan <shuahkh@osg.samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- tools/usb/usbip/libsrc/usbip_device_driver.c
+++ tools/usb/usbip/libsrc/usbip_device_driver.c
@@ -77,7 +77,7 @@ int read_usb_vudc_device(struct udev_device *sdev, struct usbip_usb_device *dev)
 	const char *path, *name;
 	char filepath[SYSFS_PATH_MAX];
 	struct usb_device_descriptor descr;
-	unsigned i;
+	unsigned int i;
 	FILE *fd = NULL;
 	struct udev_device *plat;
 	const char *speed;
