f0ede2c6282b4a710b63dd8566b0aa1e2afbe225
Chunfeng Yun <chunfeng.yun@mediatek.com>
2018-01-03 16:53:18 +0800

    usb: mtu3: supports remote wakeup for mt2712 with two SSUSB IPs
    The old way of usb wakeup only supports platform with single SSUSB IP,
    such as mt8173, but mt2712 has two SSUSB IPs, so rebuild its flow and
    also supports the new glue layer of usb wakeup on mt2712 which is
    different from mt8173.
    
    Signed-off-by: Chunfeng Yun <chunfeng.yun@mediatek.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/mtu3/mtu3_dr.h
+++ drivers/usb/mtu3/mtu3_dr.h
@@ -18,8 +18,7 @@ int ssusb_wakeup_of_property_parse(struct ssusb_mtk *ssusb,
 				struct device_node *dn);
 int ssusb_host_enable(struct ssusb_mtk *ssusb);
 int ssusb_host_disable(struct ssusb_mtk *ssusb, bool suspend);
-int ssusb_wakeup_enable(struct ssusb_mtk *ssusb);
-void ssusb_wakeup_disable(struct ssusb_mtk *ssusb);
+void ssusb_wakeup_set(struct ssusb_mtk *ssusb, bool enable);
 
 #else
 
