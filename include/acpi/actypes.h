bc4d413a819f9d0764a80a55875a5d7e1f4efed4
Mario Limonciello <mario.limonciello@dell.com>
2018-01-03 15:06:28 -0800

    ACPICA: Recognize the Windows 10 version 1607 and 1703 OSI strings
    ACPICA commit 35a4a3ea723b3066f575e63e5f0116f7ce65e713
    
    The public Microsoft document listing recognized OSI strings [1]
    shows that these two strings were introduced.
    version 1607 / Anniversary Update / "Redstone 1"
    version 1703 / Creators Update / "Redstone 2"
    
    [1] http://download.microsoft.com/download/7/e/7/7e7662cf-cbea-470b-a97e-ce7ce0d98dc2/winacpi_osi.docx
    
    Link: https://github.com/acpica/acpica/commit/35a4a3ea
    Signed-off-by: Mario Limonciello <mario.limonciello@dell.com>
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Erik Schmauss <erik.schmauss@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/actypes.h
+++ include/acpi/actypes.h
@@ -1301,6 +1301,8 @@ typedef enum {
 #define ACPI_OSI_WIN_7                  0x0B
 #define ACPI_OSI_WIN_8                  0x0C
 #define ACPI_OSI_WIN_10                 0x0D
+#define ACPI_OSI_WIN_10_RS1             0x0E
+#define ACPI_OSI_WIN_10_RS2             0x0F
 
 /* Definitions of getopt */
 
