02dd3291b2f095bbc88e1d2628fd5bf2e92de69b
Jesper Dangaard Brouer <brouer@redhat.com>
2018-01-03 11:26:14 +0100

    bpf: finally expose xdp_rxq_info to XDP bpf-programs
    Now all XDP driver have been updated to setup xdp_rxq_info and assign
    this to xdp_buff->rxq.  Thus, it is now safe to enable access to some
    of the xdp_rxq_info struct members.
    
    This patch extend xdp_md and expose UAPI to userspace for
    ingress_ifindex and rx_queue_index.  Access happens via bpf
    instruction rewrite, that load data directly from struct xdp_rxq_info.
    
    * ingress_ifindex map to xdp_rxq_info->dev->ifindex
    * rx_queue_index  map to xdp_rxq_info->queue_index
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -899,6 +899,9 @@ struct xdp_md {
 	__u32 data;
 	__u32 data_end;
 	__u32 data_meta;
+	/* Below access go though struct xdp_rxq_info */
+	__u32 ingress_ifindex; /* rxq->dev->ifindex */
+	__u32 rx_queue_index;  /* rxq->queue_index  */
 };
 
 enum sk_action {
