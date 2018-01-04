f1b95b71f19562a17bfc34c355efe3a86049f8bb
Ji Zhang <ji.zhang@mediatek.com>
2018-01-04 16:53:50 +0100

    fbdev: arm64 use __raw I/O memory api
    Since arm64 also has __raw I/O accessors, add __aarch64__ in fb.h.
    This is a supplement for commmit 981409b25e2a ("fbdev: arm has __raw
    I/O accessors, use them in fb.h").
    
    Signed-off-by: Ji Zhang <ji.zhang@mediatek.com>
    Cc: Matthias Brugger <matthias.bgg@gmail.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>

--- include/linux/fb.h
+++ include/linux/fb.h
@@ -564,7 +564,10 @@ static inline struct apertures_struct *alloc_apertures(unsigned int max_num) {
 #define fb_memcpy_fromfb sbus_memcpy_fromio
 #define fb_memcpy_tofb sbus_memcpy_toio
 
-#elif defined(__i386__) || defined(__alpha__) || defined(__x86_64__) || defined(__hppa__) || defined(__sh__) || defined(__powerpc__) || defined(__avr32__) || defined(__bfin__) || defined(__arm__)
+#elif defined(__i386__) || defined(__alpha__) || defined(__x86_64__) ||	\
+	defined(__hppa__) || defined(__sh__) || defined(__powerpc__) ||	\
+	defined(__avr32__) || defined(__bfin__) || defined(__arm__) ||	\
+	defined(__aarch64__)
 
 #define fb_readb __raw_readb
 #define fb_readw __raw_readw
