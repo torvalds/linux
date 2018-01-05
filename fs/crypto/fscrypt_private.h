e4de782a0960b9620c084881f12fb5e73ebff82b
Eric Biggers <ebiggers@google.com>
2018-01-05 10:44:54 -0800

    fscrypt: move fscrypt_info_cachep declaration to fscrypt_private.h
    The fscrypt_info kmem_cache is internal to fscrypt; filesystems don't
    need to access it.  So move its declaration into fscrypt_private.h.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/crypto/fscrypt_private.h
+++ fs/crypto/fscrypt_private.h
@@ -72,6 +72,7 @@ typedef enum {
 #define FS_CTX_HAS_BOUNCE_BUFFER_FL		0x00000002
 
 /* crypto.c */
+extern struct kmem_cache *fscrypt_info_cachep;
 extern int fscrypt_initialize(unsigned int cop_flags);
 extern struct workqueue_struct *fscrypt_read_workqueue;
 extern int fscrypt_do_page_crypto(const struct inode *inode,
