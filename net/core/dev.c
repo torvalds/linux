82aaff2f63443e1d6cc4a186ed9c2a5718123906
Jakub Kicinski <jakub.kicinski@netronome.com>
2018-01-10 01:20:02 -0800

    net: free RX queue structures
    Looks like commit e817f85652c1 ("xdp: generic XDP handling of
    xdp_rxq_info") replaced kvfree(dev->_rx) in free_netdev() with
    a call to netif_free_rx_queues() which doesn't actually free
    the rings?
    
    While at it remove the unnecessary temporary variable.
    
    Fixes: e817f85652c1 ("xdp: generic XDP handling of xdp_rxq_info")
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Simon Horman <simon.horman@netronome.com>
    Acked-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -7653,16 +7653,15 @@ err_rxq_info:
 static void netif_free_rx_queues(struct net_device *dev)
 {
 	unsigned int i, count = dev->num_rx_queues;
-	struct netdev_rx_queue *rx;
 
 	/* netif_alloc_rx_queues alloc failed, resources have been unreg'ed */
 	if (!dev->_rx)
 		return;
 
-	rx = dev->_rx;
-
 	for (i = 0; i < count; i++)
-		xdp_rxq_info_unreg(&rx[i].xdp_rxq);
+		xdp_rxq_info_unreg(&dev->_rx[i].xdp_rxq);
+
+	kvfree(dev->_rx);
 }
 
 static void netdev_init_one_queue(struct net_device *dev,
