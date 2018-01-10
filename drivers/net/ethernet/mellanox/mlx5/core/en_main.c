e213f5b6dd5d1a7ab6a5d11c851916e25fac7e49
Wei Yongjun <weiyongjun1@huawei.com>
2018-01-10 07:30:53 +0000

    net/mlx5e: fix error return code in mlx5e_alloc_rq()
    Fix to return a negative error code from the xdp_rxq_info_reg() error
    handling case instead of 0, as done elsewhere in this function.
    
    Fixes: 0ddf543226ac ("xdp/mlx5: setup xdp_rxq_info")
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Reviewed-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlx5/core/en_main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -582,7 +582,8 @@ static int mlx5e_alloc_rq(struct mlx5e_channel *c,
 		goto err_rq_wq_destroy;
 	}
 
-	if (xdp_rxq_info_reg(&rq->xdp_rxq, rq->netdev, rq->ix) < 0)
+	err = xdp_rxq_info_reg(&rq->xdp_rxq, rq->netdev, rq->ix);
+	if (err < 0)
 		goto err_rq_wq_destroy;
 
 	rq->buff.map_dir = rq->xdp_prog ? DMA_BIDIRECTIONAL : DMA_FROM_DEVICE;
