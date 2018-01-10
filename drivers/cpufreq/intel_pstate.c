d8de7a44e11f98f2c2a4c2e12e79ba9ffb839306
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2018-01-10 11:38:52 -0800

    cpufreq: intel_pstate: Add Skylake servers support
    Currently intel_pstate can function only in HWP mode on Skylake servers.
    When HWP feature is not enabled on the processor then acpi-cpufreq is
    driver is used.
    
    Based on the power and performance tests using intel_pstate scaling
    algorithm the results are comparable. But intel_pstate brings in
    additional features:
     - Display of turbo frequency range, which many users like to see
     - Place limits in the turbo frequency range when platform allows
    
    Since these tests are done only using non PID algorithm introduced in
    kernel version 4.14, this patch is not a backport candidate. So each user
    has to carefully weigh the benefits before he backports.
    
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -1620,6 +1620,7 @@ static const struct x86_cpu_id intel_pstate_cpu_ids[] = {
 	ICPU(INTEL_FAM6_XEON_PHI_KNM,		knl_funcs),
 	ICPU(INTEL_FAM6_ATOM_GOLDMONT,		core_funcs),
 	ICPU(INTEL_FAM6_ATOM_GEMINI_LAKE,       core_funcs),
+	ICPU(INTEL_FAM6_SKYLAKE_X,		core_funcs),
 	{}
 };
 MODULE_DEVICE_TABLE(x86cpu, intel_pstate_cpu_ids);
