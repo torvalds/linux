73f8a5453d40d1cfcbbd4d5ae482f172cc7f9a75
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2018-01-11 11:08:33 +0100

    staging: vme: vme_user.c: remove redundant license text
    Now that the SPDX tag is in all drivers/staging/vme/ files, that
    identifies the license in a specific and legally-defined manner.  So the
    extra GPL text wording can be removed as it is no longer needed at all.
    
    This is done on a quest to remove the 700+ different ways that files in
    the kernel describe the GPL license text.  And there's unneeded stuff
    like the address (sometimes incorrect) for the FSF which is never
    needed.
    
    No copyright headers or other non-license-description text was removed.
    
    Cc: Martyn Welch <martyn@welchs.me.uk>
    Cc: Manohar Vanga <manohar.vanga@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vme/devices/vme_user.c
+++ drivers/staging/vme/devices/vme_user.c
@@ -8,12 +8,6 @@
  * Based on work by:
  *   Tom Armistead and Ajit Prem
  *     Copyright 2004 Motorola Inc.
- *
- *
- * This program is free software; you can redistribute  it and/or modify it
- * under  the terms of  the GNU General  Public License as published by the
- * Free Software Foundation;  either version 2 of the  License, or (at your
- * option) any later version.
  */
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
