3b65ca50d24ce33cb92d88840e289135c92b40ed
Ray Jui <ray.jui@broadcom.com>
2018-01-11 12:36:16 -0800

    PCI: iproc: Fix NULL pointer dereference for BCMA
    With the inbound DMA mapping supported added, the iProc PCIe driver
    parses DT property "dma-ranges" through call to
    "of_pci_dma_range_parser_init()". In the case of BCMA, this results in a
    NULL pointer deference due to a missing of_node.
    
    Fix this by adding a guard in pcie-iproc-platform.c to only enable the
    inbound DMA mapping logic when DT property "dma-ranges" is present.
    
    Fixes: dd9d4e7498de3 ("PCI: iproc: Add inbound DMA mapping support")
    Reported-by: Rafał Miłecki <rafal@milecki.pl>
    Signed-off-by: Ray Jui <ray.jui@broadcom.com>
    [lorenzo.pieralisi@arm.com: updated commit log]
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Tested-by: Rafał Miłecki <rafal@milecki.pl>
    cc: <stable@vger.kernel.org> # 4.10+

--- drivers/pci/host/pcie-iproc.c
+++ drivers/pci/host/pcie-iproc.c
@@ -1378,9 +1378,11 @@ int iproc_pcie_setup(struct iproc_pcie *pcie, struct list_head *res)
 		}
 	}
 
-	ret = iproc_pcie_map_dma_ranges(pcie);
-	if (ret && ret != -ENOENT)
-		goto err_power_off_phy;
+	if (pcie->need_ib_cfg) {
+		ret = iproc_pcie_map_dma_ranges(pcie);
+		if (ret && ret != -ENOENT)
+			goto err_power_off_phy;
+	}
 
 #ifdef CONFIG_ARM
 	pcie->sysdata.private_data = pcie;
