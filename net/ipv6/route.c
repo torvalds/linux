6802f3adcb3f201b81a3f396d82aa0ddb5923cd5
Ido Schimmel <idosch@mellanox.com>
2018-01-12 22:07:36 +0200

    ipv6: Fix build with gcc-4.4.5
    Emil reported the following compiler errors:
    
    net/ipv6/route.c: In function `rt6_sync_up`:
    net/ipv6/route.c:3586: error: unknown field `nh_flags` specified in initializer
    net/ipv6/route.c:3586: warning: missing braces around initializer
    net/ipv6/route.c:3586: warning: (near initialization for `arg.<anonymous>`)
    net/ipv6/route.c: In function `rt6_sync_down_dev`:
    net/ipv6/route.c:3695: error: unknown field `event` specified in initializer
    net/ipv6/route.c:3695: warning: missing braces around initializer
    net/ipv6/route.c:3695: warning: (near initialization for `arg.<anonymous>`)
    
    Problem is with the named initializers for the anonymous union members.
    Fix this by adding curly braces around the initialization.
    
    Fixes: 4c981e28d373 ("ipv6: Prepare to handle multiple netdev events")
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Reported-by: Emil S Tantilov <emils.tantilov@gmail.com>
    Tested-by: Emil S Tantilov <emils.tantilov@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -3583,7 +3583,9 @@ void rt6_sync_up(struct net_device *dev, unsigned int nh_flags)
 {
 	struct arg_netdev_event arg = {
 		.dev = dev,
-		.nh_flags = nh_flags,
+		{
+			.nh_flags = nh_flags,
+		},
 	};
 
 	if (nh_flags & RTNH_F_DEAD && netif_carrier_ok(dev))
@@ -3692,7 +3694,9 @@ void rt6_sync_down_dev(struct net_device *dev, unsigned long event)
 {
 	struct arg_netdev_event arg = {
 		.dev = dev,
-		.event = event,
+		{
+			.event = event,
+		},
 	};
 
 	fib6_clean_all(dev_net(dev), fib6_ifdown, &arg);
