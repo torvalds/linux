df6703e15c493fcc7b7f7576dff17d82e460945b
Su Yue <suy.fnst@cn.fujitsu.com>
2018-01-12 11:08:02 +0800

    btrfs: correct wrong comment about magic number of index_cnt
    There is no function named btrfs_get_inode_index_count.
    Explanation for magic number index_cnt=2 in btrfs_new_inode() is
    actually located in btrfs_set_inode_index_count().
    
    So replace 'btrfs_get_inode_index_count' in the comment by
    'btrfs_set_inode_index_count'.
    
    Signed-off-by: Su Yue <suy.fnst@cn.fujitsu.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -6315,7 +6315,7 @@ static struct inode *btrfs_new_inode(struct btrfs_trans_handle *trans,
 	}
 	/*
 	 * index_cnt is ignored for everything but a dir,
-	 * btrfs_get_inode_index_count has an explanation for the magic
+	 * btrfs_set_inode_index_count has an explanation for the magic
 	 * number
 	 */
 	BTRFS_I(inode)->index_cnt = 2;
