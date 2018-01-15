6d64d2dc10ba572c2dbf6a90650b5c3111525e15
Finley Xiao <finley.xiao@rock-chips.com>
2018-01-15 16:36:18 +0800

    soc: rockchip: power-domain: Modify power domain driver for rk3228
    This driver is modified to support RK3228 SoC.
    RK3228 SoC is only support idle.
    
    Change-Id: I1c3e942de13fd241e3893818e2584c25bad69b27
    Signed-off-by: Finley Xiao <finley.xiao@rock-chips.com>

---
+++ include/dt-bindings/power/rk3228-power.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018 Fuzhou Rockchip Electronics Co., Ltd
+ *
+ * SPDX-License-Identifier: (GPL-2.0+ OR MIT)
+ */
+
+#ifndef __DT_BINDINGS_POWER_RK3228_POWER_H__
+#define __DT_BINDINGS_POWER_RK3228_POWER_H__
+
+/**
+ * RK3228 idle id Summary.
+ */
+
+#define RK3228_PD_CORE		0
+#define RK3228_PD_MSCH		1
+#define RK3228_PD_BUS		2
+#define RK3228_PD_SYS		3
+#define RK3228_PD_VIO		4
+#define RK3228_PD_VOP		5
+#define RK3228_PD_VPU		6
+#define RK3228_PD_RKVDEC	7
+#define RK3228_PD_GPU		8
+#define RK3228_PD_PERI		9
+#define RK3228_PD_GMAC		10
+
+#endif
