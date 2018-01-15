18d267c273a96a600279b576f492439f969ca6a6
Chao Yu <yuchao0@huawei.com>
2018-01-15 17:16:46 +0800

    f2fs: fix to drop all inmem pages correctly
    In commit 57864ae5ce3a ("f2fs: limit # of inmemory pages"), we have
    limited memory footprint of all inmem pages with 20% of total memory,
    otherwise, if we exceed the threshold, we will try to drop all inmem
    pages to avoid excessive memory pressure resulting in performance
    regression.
    
    But in some unrelated error paths, we will also drop all inmem pages,
    which should be wrong, fix it in this patch.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -2082,7 +2082,7 @@ static int f2fs_write_begin(struct file *file, struct address_space *mapping,
 	struct f2fs_sb_info *sbi = F2FS_I_SB(inode);
 	struct page *page = NULL;
 	pgoff_t index = ((unsigned long long) pos) >> PAGE_SHIFT;
-	bool need_balance = false;
+	bool need_balance = false, drop_atomic = false;
 	block_t blkaddr = NULL_ADDR;
 	int err = 0;
 
@@ -2091,6 +2091,7 @@ static int f2fs_write_begin(struct file *file, struct address_space *mapping,
 	if (f2fs_is_atomic_file(inode) &&
 			!available_free_memory(sbi, INMEM_PAGES)) {
 		err = -ENOMEM;
+		drop_atomic = true;
 		goto fail;
 	}
 
@@ -2171,7 +2172,7 @@ repeat:
 fail:
 	f2fs_put_page(page, 1);
 	f2fs_write_failed(mapping, pos + len);
-	if (f2fs_is_atomic_file(inode))
+	if (drop_atomic)
 		drop_inmem_pages_all(sbi);
 	return err;
 }
