42157277af17d5c05946c700eb03877d60760d3c
Kirill Tkhai <ktkhai@virtuozzo.com>
2018-01-16 12:31:54 +0300

    net: Remove spinlock from get_net_ns_by_id()
    idr_find() is safe under rcu_read_lock() and
    maybe_get_net() guarantees that net is alive.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/net_namespace.c
+++ net/core/net_namespace.c
@@ -273,11 +273,9 @@ struct net *get_net_ns_by_id(struct net *net, int id)
 		return NULL;
 
 	rcu_read_lock();
-	spin_lock_bh(&net->nsid_lock);
 	peer = idr_find(&net->netns_ids, id);
 	if (peer)
 		peer = maybe_get_net(peer);
-	spin_unlock_bh(&net->nsid_lock);
 	rcu_read_unlock();
 
 	return peer;
