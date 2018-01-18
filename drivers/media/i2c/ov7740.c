4852fdca8818972d0ea5b5ce7114da628f9954a4
Arnd Bergmann <arnd@arndb.de>
2018-01-18 08:18:12 -0500

    media: i2c: ov7740: use gpio/consumer.h instead of gpio.h
    When built on a platform without gpiolib support, we run into
    a couple of compile errors in ov7740, including:
    
    drivers/media/i2c/ov7740.c: In function 'ov7740_set_power':
    drivers/media/i2c/ov7740.c:307:4: error: implicit declaration of function 'gpiod_direction_output'; did you mean 'gpio_direction_output'? [-Werror=implicit-function-declaration]
        gpiod_direction_output(ov7740->pwdn_gpio, 0);
    drivers/media/i2c/ov7740.c:914:4: error: 'GPIOD_OUT_HIGH' undeclared (first use in this function); did you mean 'GPIOF_INIT_HIGH'?
    
    Changing it to use the correct header file solves the problem.
    
    Fixes: 39c5c4471b8d ("media: i2c: Add the ov7740 image sensor driver")
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/i2c/ov7740.c
+++ drivers/media/i2c/ov7740.c
@@ -3,7 +3,7 @@
 
 #include <linux/clk.h>
 #include <linux/delay.h>
-#include <linux/gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/i2c.h>
 #include <linux/module.h>
 #include <linux/pm_runtime.h>
