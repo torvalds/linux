ce765ecd8d1037705c9d08159bc74995df3aeeb7
Connor O'Brien <connoro@google.com>
2018-01-22 18:28:08 -0800

    ANDROID: cpufreq: Add time_in_state to /proc/uid directories
    Add per-uid files that report the data in binary format rather than
    text, to allow faster reading & parsing by userspace.
    
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Bug: 72339335
    Bug: 127641090
    Test: compare values to those reported in /proc/uid_time_in_state
    Change-Id: I463039ea7f17b842be4c70024fe772539fe2ce02

--- fs/proc/uid.c
+++ fs/proc/uid.c
@@ -2,6 +2,7 @@
  * /proc/uid support
  */
 
+#include <linux/cpufreq_times.h>
 #include <linux/fs.h>
 #include <linux/hashtable.h>
 #include <linux/init.h>
@@ -82,7 +83,20 @@ struct uid_entry {
 	.fop	= FOP,					\
 }
 
-static const struct uid_entry uid_base_stuff[] = {};
+#ifdef CONFIG_CPU_FREQ_TIMES
+static const struct file_operations proc_uid_time_in_state_operations = {
+	.open		= single_uid_time_in_state_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+#endif
+
+static const struct uid_entry uid_base_stuff[] = {
+#ifdef CONFIG_CPU_FREQ_TIMES
+	NOD("time_in_state", 0444, NULL, &proc_uid_time_in_state_operations),
+#endif
+};
 
 static const struct inode_operations proc_uid_def_inode_operations = {
 	.setattr	= proc_setattr,
