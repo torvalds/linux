0671fae134bb95325ddb35405656af3c9236548d
Chao Yu <yuchao0@huawei.com>
2018-01-25 19:40:08 +0800

    f2fs: support large nat bitmap
    Previously, we will store all nat version bitmap in checkpoint pack block,
    so our total node entry number has a limitation which caused total node
    number can not exceed (3900 * 8) block * 455 node/block = 14196000. So
    that once user wants to create more nodes in large size image, it becomes
    a bottleneck, that's unreasonable.
    
    This patch detects the new layout of nat/sit version bitmap in image in
    order to enable supporting large nat bitmap.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -1826,6 +1826,12 @@ static inline void *__bitmap_ptr(struct f2fs_sb_info *sbi, int flag)
 	struct f2fs_checkpoint *ckpt = F2FS_CKPT(sbi);
 	int offset;
 
+	if (is_set_ckpt_flags(sbi, CP_LARGE_NAT_BITMAP_FLAG)) {
+		offset = (flag == SIT_BITMAP) ?
+			le32_to_cpu(ckpt->nat_ver_bitmap_bytesize) : 0;
+		return &ckpt->sit_nat_version_bitmap + offset;
+	}
+
 	if (__cp_payload(sbi) > 0) {
 		if (flag == NAT_BITMAP)
 			return &ckpt->sit_nat_version_bitmap;
