58a22fc44539ad7fd4c07c9fcc156cad1e3340ea
Philipp Zabel <p.zabel@pengutronix.de>
2018-01-25 10:37:52 +0100

    gpu: ipu-cpmem: add 16-bit grayscale support to ipu_cpmem_set_image
    Add the missing offset calculation for 16-bit grayscale images. Since
    the IPU only supports capturing greyscale in raw passthrough mode, it
    is the same as 16-bit bayer formats.
    
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/gpu/ipu-v3/ipu-cpmem.c
+++ drivers/gpu/ipu-v3/ipu-cpmem.c
@@ -795,6 +795,7 @@ int ipu_cpmem_set_image(struct ipuv3_channel *ch, struct ipu_image *image)
 	case V4L2_PIX_FMT_SGBRG16:
 	case V4L2_PIX_FMT_SGRBG16:
 	case V4L2_PIX_FMT_SRGGB16:
+	case V4L2_PIX_FMT_Y16:
 		offset = image->rect.left * 2 +
 			 image->rect.top * pix->bytesperline;
 		break;
