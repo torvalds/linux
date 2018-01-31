3096a739d2ccbfd6a626e388228a16558f76d79d
Max Gurtovoy <maxg@mellanox.com>
2018-01-31 18:31:26 +0200

    nvme: delete NVME_CTRL_LIVE --> NVME_CTRL_CONNECTING transition
    There is no logical reason to move from live state to connecting
    state. In case of initial connection establishment, the transition
    should be NVME_CTRL_NEW --> NVME_CTRL_CONNECTING --> NVME_CTRL_LIVE.
    In case of error recovery or reset, the transition should be
    NVME_CTRL_LIVE --> NVME_CTRL_RESETTING --> NVME_CTRL_CONNECTING -->
    NVME_CTRL_LIVE.
    
    Signed-off-by: Max Gurtovoy <maxg@mellanox.com>
    Reviewed-by: James Smart <james.smart@broadcom.com>
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -297,7 +297,6 @@ bool nvme_change_ctrl_state(struct nvme_ctrl *ctrl,
 	case NVME_CTRL_CONNECTING:
 		switch (old_state) {
 		case NVME_CTRL_NEW:
-		case NVME_CTRL_LIVE:
 		case NVME_CTRL_RESETTING:
 			changed = true;
 			/* FALLTHRU */
