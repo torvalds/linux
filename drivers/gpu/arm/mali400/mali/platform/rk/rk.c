cc941b22d4f366491dd7311451b10667a885d57c
Zhen Chen <chenzhen@rock-chips.com>
2018-01-31 19:59:13 +0800

    MALI Utgard: rk.c: change licence announcement to use GPLv2
    Change-Id: I1230425dd33b48b7dc3d8b49d8912713ebb97ca5
    Signed-off-by: Zhen Chen <chenzhen@rock-chips.com>

--- drivers/gpu/arm/mali400/mali/platform/rk/rk.c
+++ drivers/gpu/arm/mali400/mali/platform/rk/rk.c
@@ -1,11 +1,10 @@
 /*
- * This confidential and proprietary software may be used only as
- * authorised by a licensing agreement from ARM Limited
- * (C) COPYRIGHT 2009-2010, 2012 ARM Limited
- * ALL RIGHTS RESERVED
- * The entire notice above must be reproduced on all authorised
- * copies and copies may only be made to the extent permitted
- * by a licensing agreement from ARM Limited.
+ * (C) COPYRIGHT RockChip Limited. All rights reserved.
+ *
+ * This program is free software and is provided to you under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation, and any use by you of this program is subject to the terms
+ * of such GNU licence.
  */
 
 /**
