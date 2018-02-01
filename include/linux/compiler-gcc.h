fff8ad7c18a238a32a56056980fccd40ed8cce83
Geert Uytterhoeven <geert@linux-m68k.org>
2018-02-01 11:21:59 +0100

    compiler-gcc.h: __nostackprotector needs gcc-4.4 and up
    commit d9afaaa4ff7af8b87d4a205e48cb8a6f666d7f01 upstream.
    
    Gcc versions before 4.4 do not recognize the __optimize__ compiler
    attribute:
    
        warning: ‘__optimize__’ attribute directive ignored
    
    Fixes: 7375ae3a0b79ea07 ("compiler-gcc.h: Introduce __nostackprotector function attribute")
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -167,8 +167,6 @@
 
 #if GCC_VERSION >= 40100
 # define __compiletime_object_size(obj) __builtin_object_size(obj, 0)
-
-#define __nostackprotector	__attribute__((__optimize__("no-stack-protector")))
 #endif
 
 #if GCC_VERSION >= 40300
@@ -198,6 +196,7 @@
 
 #if GCC_VERSION >= 40400
 #define __optimize(level)	__attribute__((__optimize__(level)))
+#define __nostackprotector	__optimize("no-stack-protector")
 #endif /* GCC_VERSION >= 40400 */
 
 #if GCC_VERSION >= 40500
