1497e5c94926f10f62e426bc1413b8c0fb39d9e2
Finley Xiao <finley.xiao@rock-chips.com>
2018-02-01 20:36:37 +0800

    clk: rockchip: rk3288: Add TSP clock
    Change-Id: I02185c5ab7a1072d271cd51161f6d4b05d327673
    Signed-off-by: Finley Xiao <finley.xiao@rock-chips.com>
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>

--- include/dt-bindings/clock/rk3288-cru.h
+++ include/dt-bindings/clock/rk3288-cru.h
@@ -91,6 +91,11 @@
 #define SCLK_VIP_OUT		127
 #define SCLK_DDRCLK		128
 #define SCLK_I2S_SRC		129
+#define SCLK_TSPOUT		130
+#define SCLK_TSP		131
+#define SCLK_HSADC0_TSP		132
+#define SCLK_HSADC1_TSP		133
+#define SCLK_27M_TSP		134
 
 #define SCLK_MAC_PLL		150
 #define SCLK_MAC		151
