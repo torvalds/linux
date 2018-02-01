48128397b04679717cfd419d55ec86456b84eb61
Jiankang Chen <chenjiankang1@huawei.com>
2018-01-31 16:16:52 -0800

    mm/page_alloc.c: fix comment in __get_free_pages()
    __get_free_pages() will return a virtual address, but it is not just a
    32-bit address, for example on a 64-bit system.  And this comment really
    confuses new readers of mm.
    
    Link: http://lkml.kernel.org/r/1511780964-64864-1-git-send-email-chenjiankang1@huawei.com
    Signed-off-by: Jiankang Chen <chenjiankang1@huawei.com>
    Reported-by: Hanjun Guo <guohanjun@huawei.com>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Yisheng Xie <xieyisheng1@huawei.com>
    Cc: Kefeng Wang <wangkefeng.wang@huawei.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4278,7 +4278,7 @@ unsigned long __get_free_pages(gfp_t gfp_mask, unsigned int order)
 	struct page *page;
 
 	/*
-	 * __get_free_pages() returns a 32-bit address, which cannot represent
+	 * __get_free_pages() returns a virtual address, which cannot represent
 	 * a highmem page
 	 */
 	VM_BUG_ON((gfp_mask & __GFP_HIGHMEM) != 0);
