b11a632c442eef34a0afeba61fab923241f317e9
John Fastabend <john.fastabend@gmail.com>
2018-02-05 10:17:43 -0800

    net: add a UID to use for ULP socket assignment
    Create a UID field and enum that can be used to assign ULPs to
    sockets. This saves a set of string comparisons if the ULP id
    is known.
    
    For sockmap, which is added in the next patches, a ULP is used to
    hook into TCP sockets close state. In this case the ULP being added
    is done at map insert time and the ULP is known and done on the kernel
    side. In this case the named lookup is not needed. Because we don't
    want to expose psock internals to user space socket options a user
    visible flag is also added. For TLS this is set for BPF it will be
    cleared.
    
    Alos remove pr_notice, user gets an error code back and should check
    that rather than rely on logs.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -1983,6 +1983,10 @@ enum hrtimer_restart tcp_pace_kick(struct hrtimer *timer);
 #define TCP_ULP_MAX		128
 #define TCP_ULP_BUF_MAX		(TCP_ULP_NAME_MAX*TCP_ULP_MAX)
 
+enum {
+	TCP_ULP_TLS,
+};
+
 struct tcp_ulp_ops {
 	struct list_head	list;
 
@@ -1991,7 +1995,9 @@ struct tcp_ulp_ops {
 	/* cleanup ulp */
 	void (*release)(struct sock *sk);
 
+	int		uid;
 	char		name[TCP_ULP_NAME_MAX];
+	bool		user_visible;
 	struct module	*owner;
 };
 int tcp_register_ulp(struct tcp_ulp_ops *type);
