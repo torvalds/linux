3627c826d74fcf480d7b7a1778921ea68dbd273a
Zheng Yang <zhengyang@rock-chips.com>
2018-02-07 11:17:01 +0800

    drm: rockchip: dw-hdmi: fix 10bit not work when booting up with uboot logo
    Default value of hdmi->colordepth is defined by input color mode,
    so there is no need to set value again when color depth property
    is created.
    
    Change-Id: I2e242fabdaadc0c3b41e48f806cbded5f619c455
    Signed-off-by: Zheng Yang <zhengyang@rock-chips.com>

--- drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
+++ drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
@@ -872,7 +872,6 @@ dw_hdmi_rockchip_attach_properties(struct drm_connector *connector,
 		if (prop) {
 			hdmi->color_depth_property = prop;
 			drm_object_attach_property(&connector->base, prop, 0);
-			hdmi->colordepth = 8;
 		}
 	}
 
