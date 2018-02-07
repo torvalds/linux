6e1a9a5ca4a4c45983ecbaec229b0dc3f33a4715
Tao Huang <huangtao@rock-chips.com>
2018-02-07 19:22:12 +0800

    video/rockchip/lcdc/rk3368_lcdc: fix compile warning
    drivers/video/rockchip/lcdc/rk3368_lcdc.c:2198:2: warning: this if
    clause does not guard... [-Wmisleading-indentation]
    
    Change-Id: I0cbd3ba10b891b67782b1a105eaac3d473e52527
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- drivers/video/rockchip/lcdc/rk3368_lcdc.c
+++ drivers/video/rockchip/lcdc/rk3368_lcdc.c
@@ -2200,8 +2200,8 @@ static int rk3368_load_screen(struct rk_lcdc_driver *dev_drv, bool initscreen)
 	    dev_drv->trsm_ops &&
 	    dev_drv->trsm_ops->enable)
 		dev_drv->trsm_ops->enable();
-		if (screen->init)
-			screen->init();
+	if (screen->init)
+		screen->init();
 	/*if (!lcdc_dev->standby)
 		lcdc_msk_reg(lcdc_dev, SYS_CTRL,
 			m_STANDBY_EN, v_STANDBY_EN(0));*/
