8141273a3c0096cd2080a12a11b82d400b500443
Hajime Tazaki <thehajime@gmail.com>
2018-02-07 12:25:53 +0900

    lkl: handle ro_after_init properlly
    Since section ro_after_init contains writable data located inside
    .rodata section (which is read-only), resulting rodata section is
    writable.  Since we don't have implementation of mark_rodata_ro() in our
    arch, those section keep writable forever.
    
    So this commit moves data.ro_after_init section from rodata to the
    end of sections so that rodata section will be read-only.
    
    Without this commit there is no issue, but valgrind somehow puzzled with
    malformed sections when ro_after_init section used, and results
    unresolved symbols after a valgrind run.
    
    This commit fixes #349.
    
    Fixes: d2ec3f77de8e ("pty: make ptmx file ops read-only after init")
    
    The commit uses (d07a980c1b8d7ac18854bae94a4e7aeabce933b8, s390: add
    proper __ro_after_init support) as a reference.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- arch/lkl/include/asm/vmlinux.lds.h
+++ arch/lkl/include/asm/vmlinux.lds.h
@@ -1,6 +1,9 @@
 #ifndef _LKL_VMLINUX_LDS_H
 #define _LKL_VMLINUX_LDS_H
 
+/* we encode our own __ro_after_init section */
+#define RO_AFTER_INIT_DATA
+
 #ifdef __MINGW32__
 #define RODATA_SECTION .rdata
 #endif
