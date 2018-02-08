f3ff6fb5db68bcd460e9880d5fb4902520dd645b
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-02-08 14:56:40 +0900

    kconfig: echo stdin to stdout if either is redirected
    If stdio is not tty, conf_askvalue() puts additional new line to
    prevent prompts from being concatenated into a single line.  This
    care is missing in conf_choice(), so a 'choice' prompt and the next
    prompt are shown in the same line.
    
    Move the code into xfgets() to cater to all cases.  To improve this
    more, let's echo stdin to stdout.  This clarifies what keys were
    input from stdio and the stdout looks like as if it were from tty.
    
    I removed the isatty(2) check since stderr is unrelated here.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Ulf Magnusson <ulfalizer@gmail.com>

--- scripts/kconfig/conf.c
+++ scripts/kconfig/conf.c
@@ -76,6 +76,9 @@ static void xfgets(char *str, int size, FILE *in)
 {
 	if (!fgets(str, size, in))
 		fprintf(stderr, "\nError in reading or end of file.\n");
+
+	if (!tty_stdio)
+		printf("%s", str);
 }
 
 static int conf_askvalue(struct symbol *sym, const char *def)
@@ -106,8 +109,6 @@ static int conf_askvalue(struct symbol *sym, const char *def)
 	case oldaskconfig:
 		fflush(stdout);
 		xfgets(line, sizeof(line), stdin);
-		if (!tty_stdio)
-			printf("\n");
 		return 1;
 	default:
 		break;
@@ -490,7 +491,7 @@ int main(int ac, char **av)
 	bindtextdomain(PACKAGE, LOCALEDIR);
 	textdomain(PACKAGE);
 
-	tty_stdio = isatty(0) && isatty(1) && isatty(2);
+	tty_stdio = isatty(0) && isatty(1);
 
 	while ((opt = getopt_long(ac, av, "s", long_opts, NULL)) != -1) {
 		if (opt == 's') {
