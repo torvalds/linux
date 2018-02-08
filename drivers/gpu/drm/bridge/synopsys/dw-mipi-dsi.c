7f497cc7fc34bc471f770b80bb2115e6646d6fb7
Philippe CORNU <philippe.cornu@st.com>
2018-02-08 15:58:05 +0100

    drm/bridge/synopsys: dsi: Adopt SPDX identifiers
    Add SPDX identifiers to the Synopsys DesignWare MIPI DSI
    host controller driver.
    
    Signed-off-by: Philippe Cornu <philippe.cornu@st.com>
    Acked-by: Philippe Ombredanne <pombredanne@nexB.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180208145805.24762-1-philippe.cornu@st.com

--- drivers/gpu/drm/bridge/synopsys/dw-mipi-dsi.c
+++ drivers/gpu/drm/bridge/synopsys/dw-mipi-dsi.c
@@ -1,11 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
  * Copyright (c) 2016, Fuzhou Rockchip Electronics Co., Ltd
  * Copyright (C) STMicroelectronics SA 2017
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
  *
  * Modified by Philippe Cornu <philippe.cornu@st.com>
  * This generic Synopsys DesignWare MIPI DSI host driver is based on the
