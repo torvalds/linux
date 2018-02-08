0a90448ead4a7683f9064852a8bb4150639d0fd2
Florian Echtler <floe@butterbrot.org>
2018-02-08 03:43:03 -0500

    media: add missing blob structure field for tag id
    The SUR40 can recognize specific printed patterns directly in hardware;
    this information (i.e. the pattern id) is present but currently unused
    in the blob structure.
    
    Signed-off-by: Florian Echtler <floe@butterbrot.org>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/input/touchscreen/sur40.c
+++ drivers/input/touchscreen/sur40.c
@@ -81,7 +81,10 @@ struct sur40_blob {
 
 	__le32 area;       /* size in pixels/pressure (?) */
 
-	u8 padding[32];
+	u8 padding[24];
+
+	__le32 tag_id;     /* valid when type == 0x04 (SUR40_TAG) */
+	__le32 unknown;
 
 } __packed;
 
