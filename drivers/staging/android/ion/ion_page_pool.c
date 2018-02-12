acb42f8e89efcf116aa56f4be31776d809e8aa79
Yisheng Xie <xieyisheng1@huawei.com>
2018-02-12 18:43:13 +0800

    staging: android: ion: Cleanup ion_page_pool_alloc_pages
    ion_page_pool_alloc_pages calls alloc_pages to allocate pages for page
    pools. If alloc_pages return NULL, it will return NULL, or it will
    return the pages allocate from alloc_pages. So we can just return
    alloc_pages without any judgement.
    
    Acked-by: Sumit Semwal <sumit.semwal@linaro.org>
    Signed-off-by: Yisheng Xie <xieyisheng1@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -11,13 +11,9 @@
 
 #include "ion.h"
 
-static void *ion_page_pool_alloc_pages(struct ion_page_pool *pool)
+static inline struct page *ion_page_pool_alloc_pages(struct ion_page_pool *pool)
 {
-	struct page *page = alloc_pages(pool->gfp_mask, pool->order);
-
-	if (!page)
-		return NULL;
-	return page;
+	return alloc_pages(pool->gfp_mask, pool->order);
 }
 
 static void ion_page_pool_free_pages(struct ion_page_pool *pool,
