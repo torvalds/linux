3be4ddf4fc06e6dcfef876020b6b4228dea5cfc1
Andy Whitcroft <apw@canonical.com>
2018-02-13 14:48:57 +0000

    UBUNTU: [Packaging] correct disablement of libbfd
    We used to use HAVE_CPLUS_DEMANGLE to disable libbfd.  Upstream has changed
    but the name of this control and its semantics.  It now only switches us
    to use the c++ demangler and only then if libbfd is disabled.
    
    Use the newly added HAVE_NO_LIBBFD to switch off libbfd and switch to
    the new name of the c++ demangle selector.
    
    BugLink: http://bugs.launchpad.net/bugs/1748922
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- debian/rules.d/2-binary-arch.mk
+++ debian/rules.d/2-binary-arch.mk
@@ -636,7 +636,7 @@ ifeq ($(do_tools_perf),true)
 	cd $(builddirpa) && $(kmake) silentoldconfig
 	cd $(builddirpa) && $(kmake) prepare
 	cd $(builddirpa)/tools/perf && \
-		$(kmake) prefix=/usr HAVE_CPLUS_DEMANGLE=1 CROSS_COMPILE=$(CROSS_COMPILE) NO_LIBPYTHON=1 NO_LIBPERL=1 PYTHON=python2.7
+		$(kmake) prefix=/usr HAVE_NO_LIBBFD=1 HAVE_CPUS_DEMANGLE_SUPPORT=1 CROSS_COMPILE=$(CROSS_COMPILE) NO_LIBPYTHON=1 NO_LIBPERL=1 PYTHON=python2.7
 endif
 ifeq ($(do_tools_x86),true)
 	cd $(builddirpa)/tools/power/x86/x86_energy_perf_policy && make CROSS_COMPILE=$(CROSS_COMPILE)
