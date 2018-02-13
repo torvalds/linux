d9dc7a3500a58de9bf3861d1a96ffeab42624b4f
Jason Gunthorpe <jgg@mellanox.com>
2018-02-13 12:18:36 +0200

    IB/uverbs: Hold the uobj write lock after allocate
    This clarifies the design intention that time between allocate and
    commit has the uobj exclusive to the caller. We already guarantee
    this by delaying publishing the uobj pointer via idr_insert,
    fd_install, list_add, etc.
    
    Additionally holding the usecnt lock during this period provides
    extra clarity and more protection against future mistakes.
    
    Fixes: 3832125624b7 ("IB/core: Add support for idr types")
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- drivers/infiniband/core/rdma_core.c
+++ drivers/infiniband/core/rdma_core.c
@@ -141,7 +141,12 @@ static struct ib_uobject *alloc_uobj(struct ib_ucontext *context,
 	 */
 	uobj->context = context;
 	uobj->type = type;
-	atomic_set(&uobj->usecnt, 0);
+	/*
+	 * Allocated objects start out as write locked to deny any other
+	 * syscalls from accessing them until they are committed. See
+	 * rdma_alloc_commit_uobject
+	 */
+	atomic_set(&uobj->usecnt, -1);
 	kref_init(&uobj->ref);
 
 	return uobj;
@@ -526,6 +531,10 @@ int rdma_alloc_commit_uobject(struct ib_uobject *uobj)
 				uobj->id);
 		return ret;
 	}
+
+	/* matches atomic_set(-1) in alloc_uobj */
+	lockdep_check(uobj, true);
+	atomic_set(&uobj->usecnt, 0);
 
 	uobj->type->type_class->alloc_commit(uobj);
 	up_read(&uobj->context->cleanup_rwsem);
