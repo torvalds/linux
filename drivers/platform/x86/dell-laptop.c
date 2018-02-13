eca39e7f0cdb9bde4003a29149fa695e876c6f73
Laszlo Toth <laszlth@gmail.com>
2018-02-13 21:43:43 +0100

    platform/x86: dell-laptop: fix kbd_get_state's request value
    Commit 9862b43624a5 ("platform/x86: dell-laptop: Allocate buffer on heap
    rather than globally")
    broke one request, changed it back to the original value.
    
    Tested on a Dell E6540, backlight came back.
    
    Fixes: 9862b43624a5 ("platform/x86: dell-laptop: Allocate buffer on heap rather than globally")
    Signed-off-by: Laszlo Toth <laszlth@gmail.com>
    Reviewed-by: Pali Rohár <pali.rohar@gmail.com>
    Reviewed-by: Mario Limonciello <mario.limonciello@dell.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/platform/x86/dell-laptop.c
+++ drivers/platform/x86/dell-laptop.c
@@ -1279,7 +1279,7 @@ static int kbd_get_state(struct kbd_state *state)
 	struct calling_interface_buffer buffer;
 	int ret;
 
-	dell_fill_request(&buffer, 0, 0, 0, 0);
+	dell_fill_request(&buffer, 0x1, 0, 0, 0);
 	ret = dell_send_request(&buffer,
 				CLASS_KBD_BACKLIGHT, SELECT_KBD_BACKLIGHT);
 	if (ret)
