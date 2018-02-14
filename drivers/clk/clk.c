541debae0adf0bee96137724fa1ce81d3102d14c
Jerome Brunet <jbrunet@baylibre.com>
2018-02-14 14:43:37 +0100

    clk: call the clock init() callback before any other ops callback
    Some clocks may need to initialize things, whatever it is, before
    being able to properly operate. Move the .init() call before any
    other callback, such recalc_rate() or get_phase(), so the clock
    is properly setup before being used.
    
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>
    Signed-off-by: Michael Turquette <mturquette@baylibre.com>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -2930,6 +2930,17 @@ static int __clk_core_init(struct clk_core *core)
 	}
 
 	/*
+	 * optional platform-specific magic
+	 *
+	 * The .init callback is not used by any of the basic clock types, but
+	 * exists for weird hardware that must perform initialization magic.
+	 * Please consider other ways of solving initialization problems before
+	 * using this callback, as its use is discouraged.
+	 */
+	if (core->ops->init)
+		core->ops->init(core->hw);
+
+	/*
 	 * Set clk's accuracy.  The preferred method is to use
 	 * .recalc_accuracy. For simple clocks and lazy developers the default
 	 * fallback is to use the parent's accuracy.  If a clock doesn't have a
@@ -3005,17 +3016,6 @@ static int __clk_core_init(struct clk_core *core)
 			__clk_recalc_rates(orphan, 0);
 		}
 	}
-
-	/*
-	 * optional platform-specific magic
-	 *
-	 * The .init callback is not used by any of the basic clock types, but
-	 * exists for weird hardware that must perform initialization magic.
-	 * Please consider other ways of solving initialization problems before
-	 * using this callback, as its use is discouraged.
-	 */
-	if (core->ops->init)
-		core->ops->init(core->hw);
 
 	kref_init(&core->ref);
 out:
