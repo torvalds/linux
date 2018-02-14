36372922847c8686971bd714f24ca64558dff467
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2018-02-14 03:01:24 +0000

    ASoC: ak4613: don't have .use_pmdown_time
    snd_soc_runtime_ignore_pmdown_time() returns "ignore pmdown time"
    decision. Then, it collects each component driver's .use_pmdown_time
    settings by using AND operation.
    But, ak4613 has no reason to ignore/use pmdown time.
    Thus, It should has use_pmdown_time=0, it means it doesn't have
    decision right of "ignore pmdown time".
    
    Otherwise, ak4613 paired driver's pmdown_time settings doesn't
    have meaning. In other words, "ignore pmdown time" is determined
    irrespective of the value of paired driver's settings
    (= ignore will be 0 by ak4613).
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/ak4613.c
+++ sound/soc/codecs/ak4613.c
@@ -610,7 +610,6 @@ static const struct snd_soc_component_driver soc_component_dev_ak4613 = {
 	.dapm_routes		= ak4613_intercon,
 	.num_dapm_routes	= ARRAY_SIZE(ak4613_intercon),
 	.idle_bias_on		= 1,
-	.use_pmdown_time	= 1,
 	.endianness		= 1,
 	.non_legacy_dai_naming	= 1,
 };
