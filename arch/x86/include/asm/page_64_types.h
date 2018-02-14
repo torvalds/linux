b83ce5ee91471d19c403ff91227204fb37c95fb2
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2018-02-14 14:16:48 +0300

    x86/mm/64: Make __PHYSICAL_MASK_SHIFT always 52
    __PHYSICAL_MASK_SHIFT is used to define the mask that helps to extract
    physical address from a page table entry.
    
    Although real physical address space available may differ between
    machines, it's safe to use 52 as __PHYSICAL_MASK_SHIFT. Unused bits
    above log2(MAXPHYADDR) up to bit 51 are reserved and must be 0.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-mm@kvack.org
    Link: http://lkml.kernel.org/r/20180214111656.88514-2-kirill.shutemov@linux.intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/page_64_types.h
+++ arch/x86/include/asm/page_64_types.h
@@ -52,11 +52,12 @@
 #define __START_KERNEL_map	_AC(0xffffffff80000000, UL)
 
 /* See Documentation/x86/x86_64/mm.txt for a description of the memory map. */
-#ifdef CONFIG_X86_5LEVEL
+
 #define __PHYSICAL_MASK_SHIFT	52
+
+#ifdef CONFIG_X86_5LEVEL
 #define __VIRTUAL_MASK_SHIFT	56
 #else
-#define __PHYSICAL_MASK_SHIFT	46
 #define __VIRTUAL_MASK_SHIFT	47
 #endif
 
