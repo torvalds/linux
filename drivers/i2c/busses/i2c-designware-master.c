d1fa74520dcdbeae891b30035e6c51aafa35306d
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2018-02-17 22:58:43 +0200

    i2c: designware: Consider SCL GPIO optional
    GPIO library can return -ENOSYS for the failed request.
    Instead of failing ->probe() in this case override error code to 0.
    
    Fixes: ca382f5b38f3 ("i2c: designware: add i2c gpio recovery option")
    Reported-by: Dominik Brodowski <linux@dominikbrodowski.net>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Tested-by: Dominik Brodowski <linux@dominikbrodowski.net>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/busses/i2c-designware-master.c
+++ drivers/i2c/busses/i2c-designware-master.c
@@ -644,7 +644,7 @@ static int i2c_dw_init_recovery_info(struct dw_i2c_dev *dev)
 	gpio = devm_gpiod_get(dev->dev, "scl", GPIOD_OUT_HIGH);
 	if (IS_ERR(gpio)) {
 		r = PTR_ERR(gpio);
-		if (r == -ENOENT)
+		if (r == -ENOENT || r == -ENOSYS)
 			return 0;
 		return r;
 	}
