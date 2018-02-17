687ca6395f10ac8a54fd5285dda91ed9efac9e23
Kirill Kapranov <kirill.kirillovich.kapranov@gmail.com>
2018-02-17 23:01:40 +0200

    USB: adutux: Add waiting in transfer abortion
    Add waiting for an URB transmit finish that let the last URB to be sent
    (to be not discarded) during 'release' procedure. W/o this waiting,the
    last frame will be nearly always lost.
    
    A test case: an attempt of sending a single frame:
    echo -en "\001mk255" >/dev/adutux0
    
    Signed-off-by: Kirill Kapranov <kirill.kirillovich.kapranov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/misc/adutux.c
+++ drivers/usb/misc/adutux.c
@@ -132,6 +132,8 @@ static void adu_abort_transfers(struct adu_device *dev)
 	spin_lock_irqsave(&dev->buflock, flags);
 	if (!dev->out_urb_finished) {
 		spin_unlock_irqrestore(&dev->buflock, flags);
+		wait_event_timeout(dev->write_wait, dev->out_urb_finished,
+			COMMAND_TIMEOUT);
 		usb_kill_urb(dev->interrupt_out_urb);
 	} else
 		spin_unlock_irqrestore(&dev->buflock, flags);
