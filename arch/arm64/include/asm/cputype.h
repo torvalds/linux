04c4927359b1f09310bfee92e7187c9022be3e00
Bhupesh Sharma <bhsharma@redhat.com>
2018-02-19 12:09:54 +0530

    arm64: Fix compilation error while accessing MPIDR_HWID_BITMASK from .S files
    Since commit e1a50de37860 (arm64: cputype: Silence Sparse warnings),
    compilation of arm64 architecture is broken with the following error
    messages:
    
      AR      arch/arm64/kernel/built-in.o
      arch/arm64/kernel/head.S: Assembler messages:
      arch/arm64/kernel/head.S:677: Error: found 'L', expected: ')'
      arch/arm64/kernel/head.S:677: Error: found 'L', expected: ')'
      arch/arm64/kernel/head.S:677: Error: found 'L', expected: ')'
      arch/arm64/kernel/head.S:677: Error: junk at end of line, first
      unrecognized character is `L'
      arch/arm64/kernel/head.S:677: Error: unexpected characters following
      instruction at operand 2 -- `movz x1,:abs_g1_s:0xff00ffffffUL'
      arch/arm64/kernel/head.S:677: Error: unexpected characters following
      instruction at operand 2 -- `movk x1,:abs_g0_nc:0xff00ffffffUL'
    
    This patch fixes the same by using the UL() macro correctly for
    assigning the MPIDR_HWID_BITMASK macro value.
    
    Fixes: e1a50de37860 ("arm64: cputype: Silence Sparse warnings")
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Bhupesh Sharma <bhsharma@redhat.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/cputype.h
+++ arch/arm64/include/asm/cputype.h
@@ -20,7 +20,7 @@
 
 #define MPIDR_UP_BITMASK	(0x1 << 30)
 #define MPIDR_MT_BITMASK	(0x1 << 24)
-#define MPIDR_HWID_BITMASK	0xff00ffffffUL
+#define MPIDR_HWID_BITMASK	UL(0xff00ffffff)
 
 #define MPIDR_LEVEL_BITS_SHIFT	3
 #define MPIDR_LEVEL_BITS	(1 << MPIDR_LEVEL_BITS_SHIFT)
