1c90d9f1c80ba28ddb61c3dbdc55ac686c073d75
Alexandre Belloni <alexandre.belloni@bootlin.com>
2018-02-20 22:05:29 +0100

    rtc: ds1511: let the core handle invalid time
    Returning a valid time when the time is invalid is a bad practice, because
    then userspace is not able to react on the information. Also, it doesn't
    make sense to return epoch because it is already the default time.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/rtc/rtc-ds1511.c
+++ drivers/rtc/rtc-ds1511.c
@@ -277,10 +277,6 @@ static int ds1511_rtc_read_time(struct device *dev, struct rtc_time *rtc_tm)
 
 	rtc_tm->tm_mon--;
 
-	if (rtc_valid_tm(rtc_tm) < 0) {
-		dev_err(dev, "retrieved date/time is not valid.\n");
-		rtc_time_to_tm(0, rtc_tm);
-	}
 	return 0;
 }
 
