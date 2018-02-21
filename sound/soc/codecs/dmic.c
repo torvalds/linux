15b7c5de7c0c4f55d529ca71a5a0feca4cf03ab3
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2018-02-21 02:06:29 +0000

    ASoC: dmic: don't use codec anymore
    commit 6d6c3946d877 ("ASoC: dmic: replace codec to component")
    replaced codec to component, but
    commit 05c9b302eda7 ("ASoC: dmic: Add optional wakeup delay")
    used codec again. This patch fix it up.
    
    Fixes: 05c9b302eda7 ("ASoC: dmic: Add optional wakeup delay")
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/dmic.c
+++ sound/soc/codecs/dmic.c
@@ -37,8 +37,8 @@ struct dmic {
 
 static int dmic_aif_event(struct snd_soc_dapm_widget *w,
 			  struct snd_kcontrol *kcontrol, int event) {
-	struct snd_soc_codec *codec = snd_soc_dapm_to_codec(w->dapm);
-	struct dmic *dmic = snd_soc_codec_get_drvdata(codec);
+	struct snd_soc_component *component = snd_soc_dapm_to_component(w->dapm);
+	struct dmic *dmic = snd_soc_component_get_drvdata(component);
 
 	switch (event) {
 	case SND_SOC_DAPM_POST_PMU:
