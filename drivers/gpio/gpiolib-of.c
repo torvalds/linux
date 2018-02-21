6662ae6af82df10259a70c7569b4c12ea7f3ba93
Maxime Ripard <maxime.ripard@bootlin.com>
2018-02-21 09:11:00 +0100

    gpiolib: Keep returning EPROBE_DEFER when we should
    Commits c85823390215 ("gpio: of: Support SPI nonstandard GPIO properties")
    and 6a537d48461d ("gpio: of: Support regulator nonstandard GPIO
    properties") have introduced a regression in the way error codes from
    of_get_named_gpiod_flags are handled.
    
    Previously, those errors codes were returned immediately, but the two
    commits mentioned above are now overwriting the error pointer, meaning that
    whatever value has been returned will be dropped in favor of whatever the
    two new functions will return.
    
    This might not be a big deal except for EPROBE_DEFER, on which GPIOlib
    customers will depend on, and that will now be returned as an hard error
    which means that they will not probe anymore, instead of gently deferring
    their probe.
    
    Since EPROBE_DEFER basically means that we have found a valid property but
    there was no GPIO controller registered to handle it, fix this issues by
    returning it as soon as we encounter it.
    
    Fixes: c85823390215 ("gpio: of: Support SPI nonstandard GPIO properties")
    Fixes: 6a537d48461d ("gpio: of: Support regulator nonstandard GPIO properties")
    Signed-off-by: Maxime Ripard <maxime.ripard@bootlin.com>
    [Fold in fix to the fix]
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpiolib-of.c
+++ drivers/gpio/gpiolib-of.c
@@ -241,6 +241,19 @@ struct gpio_desc *of_find_gpio(struct device *dev, const char *con_id,
 
 		desc = of_get_named_gpiod_flags(dev->of_node, prop_name, idx,
 						&of_flags);
+		/*
+		 * -EPROBE_DEFER in our case means that we found a
+		 * valid GPIO property, but no controller has been
+		 * registered so far.
+		 *
+		 * This means we don't need to look any further for
+		 * alternate name conventions, and we should really
+		 * preserve the return code for our user to be able to
+		 * retry probing later.
+		 */
+		if (IS_ERR(desc) && PTR_ERR(desc) == -EPROBE_DEFER)
+			return desc;
+
 		if (!IS_ERR(desc) || (PTR_ERR(desc) != -ENOENT))
 			break;
 	}
