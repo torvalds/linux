718c02ce184877c380ce1d905bb02b62cac32f32
David Wu <david.wu@rock-chips.com>
2018-02-25 16:58:56 +0800

    pwm: rockchip: Make pwm pinctrl setting after pwm enabled
    If the PWM pinctrl uses default state, the iomux setting will
    be done at probe, the PWM may not be enabled at this moment.
    It will make PWM into an intermediate state, destroy the default
    hardware state, the PWM is not ready for work yet. So it is better
    for doing PWM pinctrl setting after PWM enabled.
    
    Change-Id: Iea34a7baf6a4d7df0c631f7f4fdab5b9d61bbd5f
    Signed-off-by: David Wu <david.wu@rock-chips.com>

--- drivers/pwm/pwm-rockchip.c
+++ drivers/pwm/pwm-rockchip.c
@@ -11,6 +11,7 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
+#include <linux/pinctrl/consumer.h>
 #include <linux/platform_device.h>
 #include <linux/pwm.h>
 #include <linux/time.h>
@@ -37,6 +38,8 @@ struct rockchip_pwm_chip {
 	struct pwm_chip chip;
 	struct clk *clk;
 	struct clk *pclk;
+	struct pinctrl *pinctrl;
+	struct pinctrl_state *active_state;
 	const struct rockchip_pwm_data *data;
 	void __iomem *base;
 	unsigned long clk_rate;
@@ -246,6 +249,8 @@ static int rockchip_pwm_apply(struct pwm_chip *chip, struct pwm_device *pwm,
 			goto out;
 	}
 
+	if (state->enabled)
+		ret = pinctrl_select_state(pc->pinctrl, pc->active_state);
 out:
 	clk_disable(pc->pclk);
 
@@ -385,6 +390,18 @@ static int rockchip_pwm_probe(struct platform_device *pdev)
 	if (ret) {
 		dev_err(&pdev->dev, "Can't prepare enable APB clk: %d\n", ret);
 		goto err_clk;
+	}
+
+	pc->pinctrl = devm_pinctrl_get(&pdev->dev);
+	if (IS_ERR(pc->pinctrl)) {
+		dev_err(&pdev->dev, "Get pinctrl failed!\n");
+		return PTR_ERR(pc->pinctrl);
+	}
+
+	pc->active_state = pinctrl_lookup_state(pc->pinctrl, "active");
+	if (IS_ERR(pc->active_state)) {
+		dev_err(&pdev->dev, "No active pinctrl state\n");
+		return PTR_ERR(pc->active_state);
 	}
 
 	platform_set_drvdata(pdev, pc);
