5c155c50244a0c6e1a7778ae7b4d2753e5e1f617
Kirill Tkhai <ktkhai@virtuozzo.com>
2018-02-26 16:02:03 +0300

    net: Convert ip6gre_net_ops
    These pernet_operations are similar to bond_net_ops. Exit method
    unregisters all net ip6gre devices, and it looks like another
    pernet_operations are not interested in foreign net ip6gre list
    or net_generic()->tunnels_wc. Init method registers net device.
    So, it's possible to mark them async.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/ip6_gre.c
+++ net/ipv6/ip6_gre.c
@@ -1517,6 +1517,7 @@ static struct pernet_operations ip6gre_net_ops = {
 	.exit_batch = ip6gre_exit_batch_net,
 	.id   = &ip6gre_net_id,
 	.size = sizeof(struct ip6gre_net),
+	.async = true,
 };
 
 static int ip6gre_tunnel_validate(struct nlattr *tb[], struct nlattr *data[],
