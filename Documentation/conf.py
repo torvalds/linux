86ac846209adf8fb98373e443a661a65208ee82b
Octavian Purdila <tavi@cs.pub.ro>
2018-02-26 19:54:45 +0100

    Documentation: add asciicast directive
    Add asciicast directive that allows inserting asciinema "asciicasts" in
    docs. The directive accepts a single mandatory parameter which is the
    filename that stores the asciicast:
    
    .. asciicast:: ascii.cast
    
    Signed-off-by: Octavian Purdila <tavi@cs.pub.ro>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -134,6 +134,7 @@ except:
     None
 
 extensions.append("ditaa")
+extensions.append("asciicast")
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
