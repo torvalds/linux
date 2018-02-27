065fa2523da83c674d8533144d9c9bac986ccf80
Tero Kristo <t-kristo@ti.com>
2018-02-27 15:30:35 +0200

    crypto: omap-crypto - Verify page zone scatterlists before starting DMA
    In certain platforms like DRA7xx having memory > 2GB with LPAE enabled
    has a constraint that DMA can be done with the initial 2GB and marks it
    as ZONE_DMA. But openssl when used with cryptodev does not make sure that
    input buffer is DMA capable. So, adding a check to verify if the input
    buffer is capable of DMA.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/omap-crypto.c
+++ drivers/crypto/omap-crypto.c
@@ -104,6 +104,10 @@ static int omap_crypto_check_sg(struct scatterlist *sg, int total, int bs,
 			return OMAP_CRYPTO_NOT_ALIGNED;
 		if (!IS_ALIGNED(sg->length, bs))
 			return OMAP_CRYPTO_NOT_ALIGNED;
+#ifdef CONFIG_ZONE_DMA
+		if (page_zonenum(sg_page(sg)) != ZONE_DMA)
+			return OMAP_CRYPTO_NOT_ALIGNED;
+#endif
 
 		len += sg->length;
 		sg = sg_next(sg);
