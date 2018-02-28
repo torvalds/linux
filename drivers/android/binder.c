d8125f25c60f4a1fb8c1a799c601925c6c074bb8
Greg Kroah-Hartman <gregkh@google.com>
2018-02-28 17:02:13 +0100

    Revert "binder: add missing binder_unlock()"
    This reverts commit bf5a6a7256834ad88104d3a99cc935c9ac49b527.
    
    There is no binder_unlock() in this branch, so this patch needs to be
    reverted.
    
    Change-Id: I22dca6eee1e93e712b9b1038c42edc29e679aad6
    Cc: Guenter Roeck <linux@roeck-us.net>
    Cc: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -4553,10 +4553,8 @@ static unsigned int binder_poll(struct file *filp,
 	bool wait_for_proc_work;
 
 	thread = binder_get_thread(proc);
-	if (!thread) {
-		binder_unlock(__func__);
+	if (!thread)
 		return POLLERR;
-	}
 
 	binder_inner_proc_lock(thread->proc);
 	thread->looper |= BINDER_LOOPER_STATE_POLL;
