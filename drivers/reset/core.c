6691dffab0ab6301bb7b489b1dcf9f5efdef202f
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2018-02-28 14:08:57 +0100

    reset: add support for non-DT systems
    The reset framework only supports device-tree. There are some platforms
    however, which need to use it even in legacy, board-file based mode.
    
    An example of such architecture is the DaVinci family of SoCs which
    supports both device tree and legacy boot modes and we don't want to
    introduce any regressions.
    
    We're currently working on converting the platform from its hand-crafted
    clock API to using the common clock framework. Part of the overhaul will
    be representing the chip's power sleep controller's reset lines using
    the reset framework.
    
    This changeset extends the core reset code with a new reset lookup
    entry structure. It contains data allowing the reset core to associate
    reset lines with devices by comparing the dev_id and con_id strings.
    
    It also provides a function allowing drivers to register lookup entries
    with the framework.
    
    The new lookup function is only called as a fallback in case the
    of_node field is NULL and doesn't change anything for current users.
    
    Tested with a dummy reset driver with several lookup entries.
    
    An example lookup table registration from a driver can be found below:
    
    static struct reset_control_lookup foobar_reset_lookup[] = {
    	RESET_LOOKUP("foo.0", "foo", 15),
    	RESET_LOOKUP("bar.0", NULL,   5),
    };
    
    foobar_probe()
    {
    ...
    
            reset_controller_add_lookup(&rcdev, foobar_reset_lookup,
                                        ARRAY_SIZE(foobar_reset_lookup));
    
    ...
    }
    
    Cc: Sekhar Nori <nsekhar@ti.com>
    Cc: Kevin Hilman <khilman@baylibre.com>
    Cc: David Lechner <david@lechnology.com>
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/reset/core.c
+++ drivers/reset/core.c
@@ -23,6 +23,9 @@
 static DEFINE_MUTEX(reset_list_mutex);
 static LIST_HEAD(reset_controller_list);
 
+static DEFINE_MUTEX(reset_lookup_mutex);
+static LIST_HEAD(reset_lookup_list);
+
 /**
  * struct reset_control - a reset control
  * @rcdev: a pointer to the reset controller device
@@ -147,6 +150,36 @@ int devm_reset_controller_register(struct device *dev,
 	return ret;
 }
 EXPORT_SYMBOL_GPL(devm_reset_controller_register);
+
+/**
+ * reset_controller_add_lookup - register a set of lookup entries
+ * @rcdev: initialized reset controller device owning the reset line
+ * @lookup: array of reset lookup entries
+ * @num_entries: number of entries in the lookup array
+ */
+void reset_controller_add_lookup(struct reset_controller_dev *rcdev,
+				 struct reset_control_lookup *lookup,
+				 unsigned int num_entries)
+{
+	struct reset_control_lookup *entry;
+	unsigned int i;
+
+	mutex_lock(&reset_lookup_mutex);
+	for (i = 0; i < num_entries; i++) {
+		entry = &lookup[i];
+
+		if (!entry->dev_id) {
+			pr_warn("%s(): reset lookup entry has no dev_id, skipping\n",
+				__func__);
+			continue;
+		}
+
+		entry->rcdev = rcdev;
+		list_add_tail(&entry->list, &reset_lookup_list);
+	}
+	mutex_unlock(&reset_lookup_mutex);
+}
+EXPORT_SYMBOL_GPL(reset_controller_add_lookup);
 
 static inline struct reset_control_array *
 rstc_to_array(struct reset_control *rstc) {
@@ -493,6 +526,43 @@ struct reset_control *__of_reset_control_get(struct device_node *node,
 }
 EXPORT_SYMBOL_GPL(__of_reset_control_get);
 
+static struct reset_control *
+__reset_control_get_from_lookup(struct device *dev, const char *con_id,
+				bool shared, bool optional)
+{
+	const struct reset_control_lookup *lookup;
+	const char *dev_id = dev_name(dev);
+	struct reset_control *rstc = NULL;
+
+	if (!dev)
+		return ERR_PTR(-EINVAL);
+
+	mutex_lock(&reset_lookup_mutex);
+
+	list_for_each_entry(lookup, &reset_lookup_list, list) {
+		if (strcmp(lookup->dev_id, dev_id))
+			continue;
+
+		if ((!con_id && !lookup->con_id) ||
+		    ((con_id && lookup->con_id) &&
+		     !strcmp(con_id, lookup->con_id))) {
+			mutex_lock(&reset_list_mutex);
+			rstc = __reset_control_get_internal(lookup->rcdev,
+							    lookup->index,
+							    shared);
+			mutex_unlock(&reset_list_mutex);
+			break;
+		}
+	}
+
+	mutex_unlock(&reset_lookup_mutex);
+
+	if (!rstc)
+		return optional ? NULL : ERR_PTR(-ENOENT);
+
+	return rstc;
+}
+
 struct reset_control *__reset_control_get(struct device *dev, const char *id,
 					  int index, bool shared, bool optional)
 {
@@ -500,7 +570,7 @@ struct reset_control *__reset_control_get(struct device *dev, const char *id,
 		return __of_reset_control_get(dev->of_node, id, index, shared,
 					      optional);
 
-	return optional ? NULL : ERR_PTR(-EINVAL);
+	return __reset_control_get_from_lookup(dev, id, shared, optional);
 }
 EXPORT_SYMBOL_GPL(__reset_control_get);
 
