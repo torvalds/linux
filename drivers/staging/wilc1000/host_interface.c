172148b8ba1ee4ba7dcad6d3cf7d54c1dbd7ae1d
Ajay Singh <ajay.kathat@microchip.com>
2018-03-02 19:52:42 +0530

    staging: wilc1000: fix line over 80 char in host_int_parse_join_bss_param()
    Fix 'line over 80 characters' issue found by checkpatch.pl script.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.c
+++ drivers/staging/wilc1000/host_interface.c
@@ -3904,129 +3904,129 @@ static void *host_int_parse_join_bss_param(struct network_info *info)
 	ies_len = info->ies_len;
 
 	param = kzalloc(sizeof(*param), GFP_KERNEL);
-	if (param) {
-		param->dtim_period = info->dtim_period;
-		param->beacon_period = info->beacon_period;
-		param->cap_info = info->cap_info;
-		memcpy(param->bssid, info->bssid, 6);
-		memcpy((u8 *)param->ssid, info->ssid,
-		       info->ssid_len + 1);
-		param->ssid_len = info->ssid_len;
-		memset(param->rsn_pcip_policy, 0xFF, 3);
-		memset(param->rsn_auth_policy, 0xFF, 3);
+	if (!param)
+		return NULL;
 
-		while (index < ies_len) {
-			if (ies[index] == SUPP_RATES_IE) {
-				rates_no = ies[index + 1];
-				param->supp_rates[0] = rates_no;
-				index += 2;
+	param->dtim_period = info->dtim_period;
+	param->beacon_period = info->beacon_period;
+	param->cap_info = info->cap_info;
+	memcpy(param->bssid, info->bssid, 6);
+	memcpy((u8 *)param->ssid, info->ssid, info->ssid_len + 1);
+	param->ssid_len = info->ssid_len;
+	memset(param->rsn_pcip_policy, 0xFF, 3);
+	memset(param->rsn_auth_policy, 0xFF, 3);
 
-				for (i = 0; i < rates_no; i++)
-					param->supp_rates[i + 1] = ies[index + i];
+	while (index < ies_len) {
+		if (ies[index] == SUPP_RATES_IE) {
+			rates_no = ies[index + 1];
+			param->supp_rates[0] = rates_no;
+			index += 2;
 
-				index += rates_no;
-			} else if (ies[index] == EXT_SUPP_RATES_IE) {
-				ext_rates_no = ies[index + 1];
-				if (ext_rates_no > (MAX_RATES_SUPPORTED - rates_no))
-					param->supp_rates[0] = MAX_RATES_SUPPORTED;
-				else
-					param->supp_rates[0] += ext_rates_no;
-				index += 2;
-				for (i = 0; i < (param->supp_rates[0] - rates_no); i++)
-					param->supp_rates[rates_no + i + 1] = ies[index + i];
+			for (i = 0; i < rates_no; i++)
+				param->supp_rates[i + 1] = ies[index + i];
 
-				index += ext_rates_no;
-			} else if (ies[index] == HT_CAPABILITY_IE) {
-				param->ht_capable = true;
-				index += ies[index + 1] + 2;
-			} else if ((ies[index] == WMM_IE) &&
-				   (ies[index + 2] == 0x00) && (ies[index + 3] == 0x50) &&
-				   (ies[index + 4] == 0xF2) &&
-				   (ies[index + 5] == 0x02) &&
-				   ((ies[index + 6] == 0x00) || (ies[index + 6] == 0x01)) &&
-				   (ies[index + 7] == 0x01)) {
-				param->wmm_cap = true;
+			index += rates_no;
+		} else if (ies[index] == EXT_SUPP_RATES_IE) {
+			ext_rates_no = ies[index + 1];
+			if (ext_rates_no > (MAX_RATES_SUPPORTED - rates_no))
+				param->supp_rates[0] = MAX_RATES_SUPPORTED;
+			else
+				param->supp_rates[0] += ext_rates_no;
+			index += 2;
+			for (i = 0; i < (param->supp_rates[0] - rates_no); i++)
+				param->supp_rates[rates_no + i + 1] = ies[index + i];
 
-				if (ies[index + 8] & BIT(7))
-					param->uapsd_cap = true;
-				index += ies[index + 1] + 2;
-			} else if ((ies[index] == P2P_IE) &&
-				 (ies[index + 2] == 0x50) && (ies[index + 3] == 0x6f) &&
-				 (ies[index + 4] == 0x9a) &&
-				 (ies[index + 5] == 0x09) && (ies[index + 6] == 0x0c)) {
-				u16 p2p_cnt;
+			index += ext_rates_no;
+		} else if (ies[index] == HT_CAPABILITY_IE) {
+			param->ht_capable = true;
+			index += ies[index + 1] + 2;
+		} else if ((ies[index] == WMM_IE) &&
+			   (ies[index + 2] == 0x00) && (ies[index + 3] == 0x50) &&
+			   (ies[index + 4] == 0xF2) &&
+			   (ies[index + 5] == 0x02) &&
+			   ((ies[index + 6] == 0x00) || (ies[index + 6] == 0x01)) &&
+			   (ies[index + 7] == 0x01)) {
+			param->wmm_cap = true;
 
-				param->tsf = info->tsf_lo;
-				param->noa_enabled = 1;
-				param->idx = ies[index + 9];
+			if (ies[index + 8] & BIT(7))
+				param->uapsd_cap = true;
+			index += ies[index + 1] + 2;
+		} else if ((ies[index] == P2P_IE) &&
+			 (ies[index + 2] == 0x50) && (ies[index + 3] == 0x6f) &&
+			 (ies[index + 4] == 0x9a) &&
+			 (ies[index + 5] == 0x09) && (ies[index + 6] == 0x0c)) {
+			u16 p2p_cnt;
 
-				if (ies[index + 10] & BIT(7)) {
-					param->opp_enabled = 1;
-					param->ct_window = ies[index + 10];
-				} else {
-					param->opp_enabled = 0;
-				}
+			param->tsf = info->tsf_lo;
+			param->noa_enabled = 1;
+			param->idx = ies[index + 9];
 
-				param->cnt = ies[index + 11];
-				p2p_cnt = index + 12;
-
-				memcpy(param->duration, ies + p2p_cnt, 4);
-				p2p_cnt += 4;
-
-				memcpy(param->interval, ies + p2p_cnt, 4);
-				p2p_cnt += 4;
-
-				memcpy(param->start_time, ies + p2p_cnt, 4);
-
-				index += ies[index + 1] + 2;
-			} else if ((ies[index] == RSN_IE) ||
-				 ((ies[index] == WPA_IE) && (ies[index + 2] == 0x00) &&
-				  (ies[index + 3] == 0x50) && (ies[index + 4] == 0xF2) &&
-				  (ies[index + 5] == 0x01)))	{
-				u16 rsn_idx = index;
-
-				if (ies[rsn_idx] == RSN_IE)	{
-					param->mode_802_11i = 2;
-				} else {
-					if (param->mode_802_11i == 0)
-						param->mode_802_11i = 1;
-					rsn_idx += 4;
-				}
-
-				rsn_idx += 7;
-				param->rsn_grp_policy = ies[rsn_idx];
-				rsn_idx++;
-				offset = ies[rsn_idx] * 4;
-				pcipher_cnt = (ies[rsn_idx] > 3) ? 3 : ies[rsn_idx];
-				rsn_idx += 2;
-
-				for (i = pcipher_total_cnt, j = 0; i < pcipher_cnt + pcipher_total_cnt && i < 3; i++, j++)
-					param->rsn_pcip_policy[i] = ies[rsn_idx + ((j + 1) * 4) - 1];
-
-				pcipher_total_cnt += pcipher_cnt;
-				rsn_idx += offset;
-
-				offset = ies[rsn_idx] * 4;
-
-				auth_cnt = (ies[rsn_idx] > 3) ? 3 : ies[rsn_idx];
-				rsn_idx += 2;
-
-				for (i = auth_total_cnt, j = 0; i < auth_total_cnt + auth_cnt; i++, j++)
-					param->rsn_auth_policy[i] = ies[rsn_idx + ((j + 1) * 4) - 1];
-
-				auth_total_cnt += auth_cnt;
-				rsn_idx += offset;
-
-				if (ies[index] == RSN_IE) {
-					param->rsn_cap[0] = ies[rsn_idx];
-					param->rsn_cap[1] = ies[rsn_idx + 1];
-					rsn_idx += 2;
-				}
-				param->rsn_found = true;
-				index += ies[index + 1] + 2;
+			if (ies[index + 10] & BIT(7)) {
+				param->opp_enabled = 1;
+				param->ct_window = ies[index + 10];
 			} else {
-				index += ies[index + 1] + 2;
+				param->opp_enabled = 0;
 			}
+
+			param->cnt = ies[index + 11];
+			p2p_cnt = index + 12;
+
+			memcpy(param->duration, ies + p2p_cnt, 4);
+			p2p_cnt += 4;
+
+			memcpy(param->interval, ies + p2p_cnt, 4);
+			p2p_cnt += 4;
+
+			memcpy(param->start_time, ies + p2p_cnt, 4);
+
+			index += ies[index + 1] + 2;
+		} else if ((ies[index] == RSN_IE) ||
+			 ((ies[index] == WPA_IE) && (ies[index + 2] == 0x00) &&
+			  (ies[index + 3] == 0x50) && (ies[index + 4] == 0xF2) &&
+			  (ies[index + 5] == 0x01)))	{
+			u16 rsn_idx = index;
+
+			if (ies[rsn_idx] == RSN_IE)	{
+				param->mode_802_11i = 2;
+			} else {
+				if (param->mode_802_11i == 0)
+					param->mode_802_11i = 1;
+				rsn_idx += 4;
+			}
+
+			rsn_idx += 7;
+			param->rsn_grp_policy = ies[rsn_idx];
+			rsn_idx++;
+			offset = ies[rsn_idx] * 4;
+			pcipher_cnt = (ies[rsn_idx] > 3) ? 3 : ies[rsn_idx];
+			rsn_idx += 2;
+
+			for (i = pcipher_total_cnt, j = 0; i < pcipher_cnt + pcipher_total_cnt && i < 3; i++, j++)
+				param->rsn_pcip_policy[i] = ies[rsn_idx + ((j + 1) * 4) - 1];
+
+			pcipher_total_cnt += pcipher_cnt;
+			rsn_idx += offset;
+
+			offset = ies[rsn_idx] * 4;
+
+			auth_cnt = (ies[rsn_idx] > 3) ? 3 : ies[rsn_idx];
+			rsn_idx += 2;
+
+			for (i = auth_total_cnt, j = 0; i < auth_total_cnt + auth_cnt; i++, j++)
+				param->rsn_auth_policy[i] = ies[rsn_idx + ((j + 1) * 4) - 1];
+
+			auth_total_cnt += auth_cnt;
+			rsn_idx += offset;
+
+			if (ies[index] == RSN_IE) {
+				param->rsn_cap[0] = ies[rsn_idx];
+				param->rsn_cap[1] = ies[rsn_idx + 1];
+				rsn_idx += 2;
+			}
+			param->rsn_found = true;
+			index += ies[index + 1] + 2;
+		} else {
+			index += ies[index + 1] + 2;
 		}
 	}
 
