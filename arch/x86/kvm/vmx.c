f7eaeb0ad815730dc63e8974e6f9f4d93b8ca8bb
Sean Christopherson <sean.j.christopherson@intel.com>
2018-03-05 12:04:36 -0800

    KVM: VMX: don't configure RM TSS for unrestricted guest
    An unrestricted guest can run with CR0.PG==0 and/or CR0.PE==0,
    e.g. it can run in Real Mode without requiring host emulation.
    The RM TSS is only used for emulating RM, i.e. it will never
    be used when unrestricted guest is enabled and so doesn't need
    to be configured.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -6044,6 +6044,9 @@ static int vmx_set_tss_addr(struct kvm *kvm, unsigned int addr)
 {
 	int ret;
 
+	if (enable_unrestricted_guest)
+		return 0;
+
 	ret = x86_set_memory_region(kvm, TSS_PRIVATE_MEMSLOT, addr,
 				    PAGE_SIZE * 3);
 	if (ret)
