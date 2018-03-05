68cb8117132c0f18a9ed7f25a51a90cf6d45dbf5
Joe Perches <joe@perches.com>
2018-03-04 22:17:13 -0700

    hv: Synthetic typo correction
    Just a trivial tyop fix.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hv/hv.c
+++ drivers/hv/hv.c
@@ -217,7 +217,7 @@ void hv_synic_free(void)
 }
 
 /*
- * hv_synic_init - Initialize the Synthethic Interrupt Controller.
+ * hv_synic_init - Initialize the Synthetic Interrupt Controller.
  *
  * If it is already initialized by another entity (ie x2v shim), we need to
  * retrieve the initialized message and event pages.  Otherwise, we create and
