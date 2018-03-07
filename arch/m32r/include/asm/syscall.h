553b085c2075f6a4a2591108554f830fa61e881f
Arnd Bergmann <arnd@arndb.de>
2018-03-07 21:36:19 +0100

    arch: remove m32r port
    The Mitsubishi/Renesas m32r architecture has been around for many years,
    but the Linux port has been obsolete for a very long time as well, with
    the last significant updates done for linux-2.6.14.
    
    While some m32r microcontrollers are still being marketed by Renesas,
    those are apparently no longer possible to support, mainly due to the
    lack of an external memory interface.
    
    Hirokazu Takata was the maintainer until the architecture got marked
    Orphaned in 2014.
    
    Link: http://www.linux-m32r.org/
    Link: https://www.renesas.com/en-eu/products/microcontrollers-microprocessors/m32r.html
    Cc: Hirokazu Takata <takata@linux-m32r.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/m32r/include/asm/syscall.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _ASM_M32R_SYSCALL_H
-#define _ASM_M32R_SYSCALL_H
-
-/* Definitions for the system call vector.  */
-#define SYSCALL_VECTOR          "2"
-#define SYSCALL_VECTOR_ADDRESS  "0xa0"
-
-#endif /* _ASM_M32R_SYSCALL_H */
--- arch/m32r/include/uapi/asm/resource.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
-#ifndef _ASM_M32R_RESOURCE_H
-#define _ASM_M32R_RESOURCE_H
-
-#include <asm-generic/resource.h>
-
-#endif  /* _ASM_M32R_RESOURCE_H */
