fd8773f9f544955f6f47dc2ac3ab85ad64376b7f
Arnd Bergmann <arnd@arndb.de>
2018-03-07 21:21:59 +0100

    arch: remove frv port
    The Fujitsu FRV kernel port has been around for a long time, but has not
    seen regular updates in several years and instead was marked 'Orphaned'
    in 2016 by long-time maintainer David Howells.
    
    The SoC product line apparently is apparently still around in the form
    of the Socionext Milbeaut image processor, but this one no longer uses
    the FRV CPU cores.
    
    This removes all FRV specific files from the kernel.
    
    Link: http://www.socionext.com/en/products/assp/milbeaut/
    Cc: David Howells <dhowells@redhat.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/frv/include/uapi/asm/param.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
-#ifndef _ASM_PARAM_H
-#define _ASM_PARAM_H
-
-#define EXEC_PAGESIZE	16384
-
-#include <asm-generic/param.h>
-
-#endif /* _ASM_PARAM_H */
--- arch/frv/include/uapi/asm/statfs.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
-#ifndef _ASM_STATFS_H
-#define _ASM_STATFS_H
-
-#include <asm-generic/statfs.h>
-
-#endif /* _ASM_STATFS_H */
-
