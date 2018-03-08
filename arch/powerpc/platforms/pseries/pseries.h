16560e88320d276036b393d4ba51cf0184f6caca
Michael Ellerman <mpe@ellerman.id.au>
2018-03-08 13:54:39 +1100

    powerpc/pseries: Move smp_query_cpu_stopped() etc. out of plpar_wrappers.h
    smp_query_cpu_stopped() and related #defines are currently in
    plpar_wrappers.h. The function actually does an RTAS call, not an
    hcall, and basically has nothing to do with plpar_wrappers.h
    
    Move it into pseries.h, where it can easily be used by the only two
    callers in pseries/smp.c and pseries/hotplug-cpu.c.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/platforms/pseries/pseries.h
+++ arch/powerpc/platforms/pseries/pseries.h
@@ -27,6 +27,14 @@ extern int pSeries_machine_check_exception(struct pt_regs *regs);
 
 #ifdef CONFIG_SMP
 extern void smp_init_pseries(void);
+
+/* Get state of physical CPU from query_cpu_stopped */
+int smp_query_cpu_stopped(unsigned int pcpu);
+#define QCSS_STOPPED 0
+#define QCSS_STOPPING 1
+#define QCSS_NOT_STOPPED 2
+#define QCSS_HARDWARE_ERROR -1
+#define QCSS_HARDWARE_BUSY -2
 #else
 static inline void smp_init_pseries(void) { };
 #endif
