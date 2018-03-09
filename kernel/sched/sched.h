a07630b8b2c16f82fd5b71d890079f4dd7599c1d
Patrick Bellasi <patrick.bellasi@arm.com>
2018-03-09 09:52:44 +0000

    sched/cpufreq/schedutil: Use util_est for OPP selection
    When schedutil looks at the CPU utilization, the current PELT value for
    that CPU is returned straight away. In certain scenarios this can have
    undesired side effects and delays on frequency selection.
    
    For example, since the task utilization is decayed at wakeup time, a
    long sleeping big task newly enqueued does not add immediately a
    significant contribution to the target CPU. This introduces some latency
    before schedutil will be able to detect the best frequency required by
    that task.
    
    Moreover, the PELT signal build-up time is a function of the current
    frequency, because of the scale invariant load tracking support. Thus,
    starting from a lower frequency, the utilization build-up time will
    increase even more and further delays the selection of the actual
    frequency which better serves the task requirements.
    
    In order to reduce these kind of latencies, we integrate the usage
    of the CPU's estimated utilization in the sugov_get_util function.
    
    This allows to properly consider the expected utilization of a CPU which,
    for example, has just got a big task running after a long sleep period.
    Ultimately this allows to select the best frequency to run a task
    right after its wake-up.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Morten Rasmussen <morten.rasmussen@arm.com>
    Cc: Paul Turner <pjt@google.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Steve Muckle <smuckle@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Todd Kjos <tkjos@android.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Link: http://lkml.kernel.org/r/20180309095245.11071-4-patrick.bellasi@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2163,6 +2163,13 @@ static inline unsigned long cpu_util_dl(struct rq *rq)
 
 static inline unsigned long cpu_util_cfs(struct rq *rq)
 {
-	return rq->cfs.avg.util_avg;
+	unsigned long util = READ_ONCE(rq->cfs.avg.util_avg);
+
+	if (sched_feat(UTIL_EST)) {
+		util = max_t(unsigned long, util,
+			     READ_ONCE(rq->cfs.avg.util_est.enqueued));
+	}
+
+	return util;
 }
 #endif
