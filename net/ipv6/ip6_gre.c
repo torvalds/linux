e41c7c68ea771683cae5a7f81c268f38d7912ecb
William Tu <u9012063@gmail.com>
2018-03-09 07:34:42 -0800

    ip6erspan: make sure enough headroom at xmit.
    The patch adds skb_cow_header() to ensure enough headroom
    at ip6erspan_tunnel_xmit before pushing the erspan header
    to the skb.
    
    Signed-off-by: William Tu <u9012063@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/ip6_gre.c
+++ net/ipv6/ip6_gre.c
@@ -903,6 +903,9 @@ static netdev_tx_t ip6erspan_tunnel_xmit(struct sk_buff *skb,
 		truncate = true;
 	}
 
+	if (skb_cow_head(skb, dev->needed_headroom))
+		goto tx_err;
+
 	t->parms.o_flags &= ~TUNNEL_KEY;
 	IPCB(skb)->flags = 0;
 
