12ce5002e72ac64b834f7d63dc6a81d1c68336cf
Hegi <rene@hegewald.it>
2018-03-10 18:16:46 +0100

    replace many if´s with case

--- build.sh
+++ build.sh
@@ -53,131 +53,121 @@ function prepare_SD
   fi
 }
 
-if [[ -d drivers ]];
-then
-  action=$1
-  LANG=C
-  #git pull
-  #git reset --hard v4.14
-  CFLAGS=-j$(grep ^processor /proc/cpuinfo  | wc -l)
-  #export INSTALL_MOD_PATH=$(dirname $(pwd))/mod/;
-#  if [[ ! -z ${#INSTALL_MOD_PATH}  ]]; then
-#    rm -r $INSTALL_MOD_PATH/lib/modules 2>/dev/null
-#    #echo $INSTALL_MOD_PATH
-#  fi
+#Test if .config exists for building Kernel
+if [[ -e ".config" ]]; then
+	action=$1
+	LANG=C
+	#git pull
+	#git reset --hard v4.14
+	CFLAGS=-j$(grep ^processor /proc/cpuinfo  | wc -l)
+	#export INSTALL_MOD_PATH=$(dirname $(pwd))/mod/;
+	#if [[ ! -z ${#INSTALL_MOD_PATH}  ]]; then
+	#	rm -r $INSTALL_MOD_PATH/lib/modules 2>/dev/null
+	#	echo $INSTALL_MOD_PATH
+	#fi
 
-  if [[ "$action" == "reset" ]];
-  then
-    git reset --hard v4.14
-    action=importconfig
-  fi
+	case "$action" in
+		"reset")
+			##Reset Git
+	    		git reset --hard v4.14
+			#call self and Import Config
+    			$0 importconfig
+			;;
 
-  if [[ "$action" == "update" ]];
-  then
-    git pull
-  fi
+		"update")
+    			git pull
+			;;
 
-  if [[ "$action" == "umount" ]];
-  then
-    umount /media/$USER/BPI-BOOT
-    umount /media/$USER/BPI-ROOT
-  fi
+  		"umount")
+			umount /media/$USER/BPI-BOOT
+			umount /media/$USER/BPI-ROOT
+			;;
 
+		"defconfig")
+			nano arch/arm/configs/mt7623n_evb_fwu_defconfig
+			;;
 
-  if [[ "$action" == "defconfig" ]];
-  then
-    nano arch/arm/configs/mt7623n_evb_fwu_defconfig
-  fi
-
-  if [[ "$action" == "importconfig" ]];
-  then
-#    cp ../mt7623n_evb_bpi_defconfig arch/arm/configs/
-#    make mt7623n_evb_bpi_defconfig
-#    cp ../mt7623n_evb_ryderlee_defconfig arch/arm/configs/
-#	make mt7623n_evb_ryderlee_defconfig
-#    cp ../mt7623n_evb_fwu_defconfig arch/arm/configs/
-	make mt7623n_evb_fwu_defconfig
-  fi
-
-  if [[ "$action" == "config" ]];
-  then
-    make menuconfig
-  fi
-
-  if [[ -z "$action" ]];
-  then
-    kernver=$(make kernelversion)
-    gitbranch=$(git rev-parse --abbrev-ref HEAD)
- #   set -x
-    exec 3> >(tee build.log)
-	export LOCALVERSION="-${gitbranch}"
-#    make --debug && make modules_install
-    make ${CFLAGS} 2>&3 #&& make modules_install 2>&3
-    ret=$?
-#    set +x
-    exec 3>&-
-    if [[ $ret == 0 ]];
-    then
-      cat arch/arm/boot/zImage arch/arm/boot/dts/mt7623n-bananapi-bpi-r2.dtb > arch/arm/boot/zImage-dtb
-      mkimage -A arm -O linux -T kernel -C none -a 80008000 -e 80008000 -n "Linux Kernel $kernver-$gitbranch" -d arch/arm/boot/zImage-dtb ./uImage
-      echo "==========================================="
-      echo "1) pack"
-      if [[ $crosscompile -eq 0 ]];then
-        echo "2) install to System"
-      else
-        echo "2) install to SD-Card"
-      fi;
-      read -n1 -p "choice [12]:" choice
-      echo
-      if [[ "$choice" == "1" ]]; then
-        prepare_SD
-        echo "pack..."
-        olddir=$(pwd)
-        cd ../SD
-        fname=bpi-r2_${kernver}_${gitbranch}.tar.gz
-        tar -cz --owner=root --group=root -f $fname BPI-BOOT BPI-ROOT
-		md5sum $fname > $fname.md5
-        ls -lh $(pwd)"/"$fname
-        cd $olddir
-      elif [[ "$choice" == "2" ]];then
-        if [[ $crosscompile -eq 0 ]];
-        then
-          kernelfile=/boot/bananapi/bpi-r2/linux/uImage
-          if [[ -e $kernelfile ]];then
-		    echo "backup of kernel: $kernelfile.bak"
-		    cp $kernelfile $kernelfile.bak
-            cp ./uImage $kernelfile
-            sudo make modules_install
-          else
-            echo "actual Kernel not found...is /boot mounted?"
-		  fi
-        else
-          read -p "Press [enter] to copy data to SD-Card..."
-          if  [[ -d /media/$USER/BPI-BOOT ]];
-          then
-            kernelfile=/media/$USER/BPI-BOOT/bananapi/bpi-r2/linux/uImage
-            if [[ -e $kernelfile ]];then
-		      echo "backup of kernel: $kernelfile.bak"
-		      cp $kernelfile $kernelfile.bak
-		    fi
-		    echo "copy new kernel"
-            cp ./uImage /media/$USER/BPI-BOOT/bananapi/bpi-r2/linux/uImage
-            echo "copy modules (root needed because of ext-fs permission)"
-            export INSTALL_MOD_PATH=/media/$USER/BPI-ROOT/;
-            echo "INSTALL_MOD_PATH: $INSTALL_MOD_PATH"
-            sudo make ARCH=$ARCH INSTALL_MOD_PATH=$INSTALL_MOD_PATH modules_install
-            #sudo cp -r ../mod/lib/modules /media/$USER/BPI-ROOT/lib/
-            if [[ -n "$(grep 'CONFIG_MT76=' .config)" ]];then
-              echo "MT76 set,don't forget the firmware-files...";
-            fi
-            sync
-          else
-            echo "SD-Card not found!"
-          fi
-        fi
-      else
-        echo "wrong option: $choice"
-      fi
-    fi
-  fi
+		"importconfig")
+			#    cp ../mt7623n_evb_bpi_defconfig arch/arm/configs/
+			#    make mt7623n_evb_bpi_defconfig
+			#    cp ../mt7623n_evb_ryderlee_defconfig arch/arm/configs/
+			#	make mt7623n_evb_ryderlee_defconfig
+			#    cp ../mt7623n_evb_fwu_defconfig arch/arm/configs/
+			make mt7623n_evb_fwu_defconfig
+			;;
+	  	"config")
+			make menuconfig
+			;;
+		*)
+    			kernver=$(make kernelversion)
+			gitbranch=$(git rev-parse --abbrev-ref HEAD)
+	    		exec 3> >(tee build.log)
+			export LOCALVERSION="-${gitbranch}"
+			#    make --debug && make modules_install
+			make ${CFLAGS} 2>&3 #&& make modules_install 2>&3
+			ret=$?
+			#    set +x
+			exec 3>&-
+			if [[ $ret == 0 ]]; then
+				cat arch/arm/boot/zImage arch/arm/boot/dts/mt7623n-bananapi-bpi-r2.dtb > arch/arm/boot/zImage-dtb
+				mkimage -A arm -O linux -T kernel -C none -a 80008000 -e 80008000 -n "Linux Kernel $kernver-$gitbranch" -d arch/arm/boot/zImage-dtb ./uImage
+				echo "==========================================="
+				echo "1) pack"
+				if [[ $crosscompile -eq 0 ]];then
+					echo "2) install to System"
+				else
+					echo "2) install to SD-Card"
+				fi;
+				read -n1 -p "choice [12]:" choice
+				echo
+				if [[ "$choice" == "1" ]]; then
+					prepare_SD
+					echo "pack..."
+					olddir=$(pwd)
+					cd ../SD
+					fname=bpi-r2_${kernver}_${gitbranch}.tar.gz
+					tar -cz --owner=root --group=root -f $fname BPI-BOOT BPI-ROOT
+					md5sum $fname > $fname.md5
+					ls -lh $(pwd)"/"$fname
+					cd $olddir
+				elif [[ "$choice" == "2" ]];then
+					if [[ $crosscompile -eq 0 ]]; then
+						kernelfile=/boot/bananapi/bpi-r2/linux/uImage
+						if [[ -e $kernelfile ]];then
+							echo "backup of kernel: $kernelfile.bak"
+							cp $kernelfile $kernelfile.bak
+							cp ./uImage $kernelfile
+							sudo make modules_install
+						else
+							echo "actual Kernel not found...is /boot mounted?"
+						fi
+					else
+						read -p "Press [enter] to copy data to SD-Card..."
+						if  [[ -d /media/$USER/BPI-BOOT ]]; then
+							kernelfile=/media/$USER/BPI-BOOT/bananapi/bpi-r2/linux/uImage
+							if [[ -e $kernelfile ]];then
+								echo "backup of kernel: $kernelfile.bak"
+								cp $kernelfile $kernelfile.bak
+							fi
+							echo "copy new kernel"
+							cp ./uImage /media/$USER/BPI-BOOT/bananapi/bpi-r2/linux/uImage
+							echo "copy modules (root needed because of ext-fs permission)"
+							export INSTALL_MOD_PATH=/media/$USER/BPI-ROOT/;
+							echo "INSTALL_MOD_PATH: $INSTALL_MOD_PATH"
+							sudo make ARCH=$ARCH INSTALL_MOD_PATH=$INSTALL_MOD_PATH modules_install
+							#sudo cp -r ../mod/lib/modules /media/$USER/BPI-ROOT/lib/
+							if [[ -n "$(grep 'CONFIG_MT76=' .config)" ]];then
+								echo "MT76 set,don't forget the firmware-files...";
+							fi
+							sync
+						else
+							echo "SD-Card not found!"
+						fi
+					fi
+				else
+					echo "wrong option: $choice"
+				fi
+			fi
+ 			;;
+	esac
 fi
