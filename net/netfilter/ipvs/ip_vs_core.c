554855ccdf37262def7fed557a5efec39f2c0f47
Kirill Tkhai <ktkhai@virtuozzo.com>
2018-03-15 12:11:25 +0300

    net: Convert ipvs_core_ops
    These pernet_operations register and unregister nf hooks,
    /proc entries, sysctl, percpu statistics. There are several
    global lists, and the only list modified without exclusive
    locks is ip_vs_conn_tab in ip_vs_conn_flush(). We iterate
    the list and force the timers expire at the moment. Since
    there were possible several timer expirations before this
    patch, and since they are safe, the patch does not invent
    new parallelism of their destruction. These pernet_operations
    look safe to be converted.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netfilter/ipvs/ip_vs_core.c
+++ net/netfilter/ipvs/ip_vs_core.c
@@ -2289,6 +2289,7 @@ static struct pernet_operations ipvs_core_ops = {
 	.exit = __ip_vs_cleanup,
 	.id   = &ip_vs_net_id,
 	.size = sizeof(struct netns_ipvs),
+	.async = true,
 };
 
 static struct pernet_operations ipvs_core_dev_ops = {
