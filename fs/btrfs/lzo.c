4eeb97c67a90a2193cb622f4df8076cba7c42023
Nikolay Borisov <nborisov@suse.com>
2018-03-15 14:36:24 +0200

    btrfs: Remove unused tot_len var from lzo_decompress
    Added already unused in a6fa6fae40ec ("btrfs: Add lzo compression
    support").
    
    Signed-off-by: Nikolay Borisov <nborisov@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/lzo.c
+++ fs/btrfs/lzo.c
@@ -382,14 +382,12 @@ static int lzo_decompress(struct list_head *ws, unsigned char *data_in,
 	struct workspace *workspace = list_entry(ws, struct workspace, list);
 	size_t in_len;
 	size_t out_len;
-	size_t tot_len;
 	int ret = 0;
 	char *kaddr;
 	unsigned long bytes;
 
 	BUG_ON(srclen < LZO_LEN);
 
-	tot_len = read_compress_length(data_in);
 	data_in += LZO_LEN;
 
 	in_len = read_compress_length(data_in);
