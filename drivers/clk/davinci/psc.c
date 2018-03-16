f41e5275757ed2516153b2dea616ef9e4afbfbcf
David Lechner <david@lechnology.com>
2018-03-15 21:52:31 -0500

    clk: davinci: Add platform information for TI DM644x PSC
    This adds platform-specific declarations for the PSC clocks on TI
    DM644x based systems.
    
    Signed-off-by: David Lechner <david@lechnology.com>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/davinci/psc.c
+++ drivers/clk/davinci/psc.c
@@ -494,6 +494,7 @@ static const struct platform_device_id davinci_psc_id_table[] = {
 	{ .name = "da850-psc1", .driver_data = (kernel_ulong_t)&da850_psc1_init_data },
 	{ .name = "dm355-psc",  .driver_data = (kernel_ulong_t)&dm355_psc_init_data  },
 	{ .name = "dm365-psc",  .driver_data = (kernel_ulong_t)&dm365_psc_init_data  },
+	{ .name = "dm644x-psc", .driver_data = (kernel_ulong_t)&dm644x_psc_init_data },
 	{ }
 };
 
