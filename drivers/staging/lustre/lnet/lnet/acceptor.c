80782927e3aa8be17c804c35ad310b7d6f8c1499
Justin Skists <j.skists@gmail.com>
2018-03-17 09:15:41 +0000

    staging: lustre: Fix unneeded byte-ordering cast
    Fix sparse warning:
    
      CHECK   drivers/staging//lustre/lnet/lnet/acceptor.c
    drivers/staging//lustre/lnet/lnet/acceptor.c:243:30: warning: cast to
    restricted __le32
    
    LNET_PROTO_TCP_MAGIC, as a define, is already CPU byte-ordered when
    compared to 'magic', so no need for a cast.
    
    Signed-off-by: Justin Skists <j.skists@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/lnet/acceptor.c
+++ drivers/staging/lustre/lnet/lnet/acceptor.c
@@ -240,7 +240,7 @@ lnet_accept(struct socket *sock, __u32 magic)
 			return -EPROTO;
 		}
 
-		if (magic == le32_to_cpu(LNET_PROTO_TCP_MAGIC))
+		if (magic == LNET_PROTO_TCP_MAGIC)
 			str = "'old' socknal/tcpnal";
 		else
 			str = "unrecognised";
