4989d4f07a8e738b33a79099ddbdd8e125a4da1b
Herbert Xu <herbert@gondor.apana.org.au>
2018-03-20 07:41:00 +0800

    crypto: api - Remove unused crypto_type lookup function
    The lookup function in crypto_type was only used for the implicit
    IV generators which have been completely removed from the crypto
    API.
    
    This patch removes the lookup function as it is now useless.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/api.c
+++ crypto/api.c
@@ -485,20 +485,14 @@ struct crypto_alg *crypto_find_alg(const char *alg_name,
 				   const struct crypto_type *frontend,
 				   u32 type, u32 mask)
 {
-	struct crypto_alg *(*lookup)(const char *name, u32 type, u32 mask) =
-		crypto_alg_mod_lookup;
-
 	if (frontend) {
 		type &= frontend->maskclear;
 		mask &= frontend->maskclear;
 		type |= frontend->type;
 		mask |= frontend->maskset;
-
-		if (frontend->lookup)
-			lookup = frontend->lookup;
 	}
 
-	return lookup(alg_name, type, mask);
+	return crypto_alg_mod_lookup(alg_name, type, mask);
 }
 EXPORT_SYMBOL_GPL(crypto_find_alg);
 
