ccce1028fa2278a1f887d2114ab13be7a51dd70a
Ritesh Harjani <riteshh@codeaurora.org>
2018-03-19 15:49:54 +0530

    ANDROID: sdcardfs: Fix sdcardfs to stop creating cases-sensitive duplicate entries.
    sdcardfs_name_match gets a 'name' argument from the underlying FS.
    This need not be null terminated string.
    So in sdcardfs_name_match -> qstr_case_eq -> we should use
    str_n_case_eq.
    
    This happens because few of the entries in lower level FS may not be
    NULL terminated and may have some garbage characters passed while
    doing sdcardfs_name_match.
    
    For e.g.
     # dmesg |grep Download
     [  103.646386] sdcardfs_name_match: q1->name=.nomedia, q1->len=8,
     q2->name=Download\x17\x80\x03, q2->len=8
     [  104.021340] sdcardfs_name_match: q1->name=.nomedia, q1->len=8,
     q2->name=Download\x17\x80\x03, q2->len=8
     [  105.196864] sdcardfs_name_match: q1->name=.nomedia, q1->len=8,
     q2->name=Download\x17\x80\x03, q2->len=8
     [  109.113521] sdcardfs_name_match: q1->name=logs, q1->len=4,
     q2->name=Download\x17\x80\x03, q2->len=8
    
    Now when we try to create a directory with different case for a such
    files. SDCARDFS creates a entry if it could not find the underlying
    entry in it's dcache.
    
    To reproduce:-
    1. bootup the device wait for some time after sdcardfs mounting to
       complete.
    2. cd /storage/emulated/0
    3. echo 3 > /proc/sys/vm/drop_caches
    4. mkdir download
    
    We now start seeing two entries with name.
    Download & download.
    
    Change-Id: I976d92a220a607dd8cdb96c01c2041c5c2bc3326
    Signed-off-by: Ritesh Harjani <riteshh@codeaurora.org>
    bug: 75987238

--- fs/sdcardfs/sdcardfs.h
+++ fs/sdcardfs/sdcardfs.h
@@ -669,7 +669,7 @@ static inline bool str_n_case_eq(const char *s1, const char *s2, size_t len)
 
 static inline bool qstr_case_eq(const struct qstr *q1, const struct qstr *q2)
 {
-	return q1->len == q2->len && str_case_eq(q1->name, q2->name);
+	return q1->len == q2->len && str_n_case_eq(q1->name, q2->name, q2->len);
 }
 
 #define QSTR_LITERAL(string) QSTR_INIT(string, sizeof(string)-1)
