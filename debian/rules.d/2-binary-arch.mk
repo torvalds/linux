1606fed670863a003e8d9c3ca5fc1397a841e1ad
Andy Whitcroft <apw@canonical.com>
2018-03-19 14:37:29 +0000

    UBUNTU: [Packaging] nvidia -- build and sign nvidia packages and ship signatures
    BugLink: http://bugs.launchpad.net/bugs/1764792
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- debian/rules.d/2-binary-arch.mk
+++ debian/rules.d/2-binary-arch.mk
@@ -395,6 +395,10 @@ endif
 	$(if $(filter true,$(enable_zfs)),$(call build_dkms, $(mods_pkg_name)-$*, $(pkgdir)/lib/modules/$(abi_release)-$*/kernel, spl, pool/universe/s/spl-linux/spl-dkms_$(dkms_spl_linux_version)_all.deb))
 	$(if $(filter true,$(enable_zfs)),$(call build_dkms, $(mods_pkg_name)-$*, $(pkgdir)/lib/modules/$(abi_release)-$*/kernel, zfs, pool/universe/z/zfs-linux/zfs-dkms_$(dkms_zfs_linux_version)_all.deb))
 
+ifeq ($(do_dkms_nvidia),true)
+	$(call build_dkms, $(bldinfo_pkg_name)-$*, $(pkgdir_bldinfo)/usr/lib/linux/$(abi_release)-$*/signatures, nvidia-390, nvidia-kernel-source-390 nvidia-dkms-390)
+endif
+
 	# Build the final ABI information.
 	install -d $(abidir)
 	sed -e 's/^\(.\+\)[[:space:]]\+\(.\+\)[[:space:]]\(.\+\)$$/\3 \2 \1/'	\
