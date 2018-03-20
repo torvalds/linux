f3f59fbc54b76945ebc92772bd86f60728205da3
Thomas Gleixner <tglx@linutronix.de>
2018-03-20 14:17:04 +0100

    genirq: Remove license boilerplate/references
    Now that SPDX identifiers are in place, remove the boilerplate or
    references.
    
    The change in timings.c has been acked by the author.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Kate Stewart <kstewart@linuxfoundation.org>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Philippe Ombredanne <pombredanne@nexb.com>
    Link: https://lkml.kernel.org/r/20180314212030.668321222@linutronix.de

--- kernel/irq/debugfs.c
+++ kernel/irq/debugfs.c
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
-/*
- * Copyright 2017 Thomas Gleixner <tglx@linutronix.de>
- *
- * This file is licensed under the GPL V2.
- */
+// Copyright 2017 Thomas Gleixner <tglx@linutronix.de>
+
 #include <linux/irqdomain.h>
 #include <linux/irq.h>
 #include <linux/uaccess.h>
