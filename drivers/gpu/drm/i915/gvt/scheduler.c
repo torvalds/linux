41e403d04e7050c8d88682939febcdbe117d4c82
Weinan Li <weinan.z.li@intel.com>
2018-03-21 15:40:32 +0800

    Revert "drm/i915/gvt: set max priority for gvt context"
    This reverts commit 11474e9091cf2002e948647fd9f63a7f027e488a.
    
    There are issues which will block the host preemption before, instead of
    disabling it use one workaround "setting max priority for gvt context"
    to avoid the gvt context be preempted by the host. Now the issues have been
    cleared, so revert this patch to enable host preemption.
    
    v2:
    - refine description(Zhenyu)
    
    Signed-off-by: Weinan Li <weinan.z.li@intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Signed-off-by: Zhi Wang <zhi.a.wang@intel.com>

--- drivers/gpu/drm/i915/gvt/scheduler.c
+++ drivers/gpu/drm/i915/gvt/scheduler.c
@@ -1156,9 +1156,6 @@ int intel_vgpu_setup_submission(struct intel_vgpu *vgpu)
 	if (IS_ERR(s->shadow_ctx))
 		return PTR_ERR(s->shadow_ctx);
 
-	if (HAS_LOGICAL_RING_PREEMPTION(vgpu->gvt->dev_priv))
-		s->shadow_ctx->sched.priority = INT_MAX;
-
 	bitmap_zero(s->shadow_ctx_desc_updated, I915_NUM_ENGINES);
 
 	s->workloads = kmem_cache_create_usercopy("gvt-g_vgpu_workload",
