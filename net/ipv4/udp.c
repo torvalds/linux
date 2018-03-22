fc18999ed2a217f249e9880363b28a89c839737e
Kirill Tkhai <ktkhai@virtuozzo.com>
2018-03-22 21:34:46 +0300

    net: Convert udp_sysctl_ops
    These pernet_operations just initialize udp4 defaults.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -2842,7 +2842,8 @@ static int __net_init udp_sysctl_init(struct net *net)
 }
 
 static struct pernet_operations __net_initdata udp_sysctl_ops = {
-	.init       = udp_sysctl_init,
+	.init	= udp_sysctl_init,
+	.async	= true,
 };
 
 void __init udp_init(void)
