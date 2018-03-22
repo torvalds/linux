47319f7186b8746521b0ee674054b29d7b35578b
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-03-22 14:58:33 +0100

    printk: change message to pr_info
    To allow userspace to prevent this message from appearing in the
    console by changing the log priority.
    
    This matches other informative messages that the power subsystem emits
    when the system changes power states.
    
    Link: http://lkml.kernel.org/r/20180322135833.16602-1-tomeu.vizoso@collabora.com
    To: linux-kernel@vger.kernel.org
    Cc: kernel@collabora.com
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Signed-off-by: Petr Mladek <pmladek@suse.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -2161,7 +2161,7 @@ void suspend_console(void)
 {
 	if (!console_suspend_enabled)
 		return;
-	printk("Suspending console(s) (use no_console_suspend to debug)\n");
+	pr_info("Suspending console(s) (use no_console_suspend to debug)\n");
 	console_lock();
 	console_suspended = 1;
 	up_console_sem();
