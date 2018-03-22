ea89c065482179b2bf9f9b6788b06a6e0c68a73b
Thomas Gleixner <tglx@linutronix.de>
2018-03-23 00:05:29 +0100

    x86/tsc: Get rid of rdtscll()
    Commit 99770737ca7e ("x86/asm/tsc: Add rdtscll() merge helper") added
    rdtscll() in August 2015 along with the comment:
    
     /* Deprecated, keep it for a cycle for easier merging: */
    
    12 cycles later it's really overdue for removal.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/cpu/mcheck/mce-inject.c
+++ arch/x86/kernel/cpu/mcheck/mce-inject.c
@@ -491,7 +491,7 @@ static void do_inject(void)
 	unsigned int cpu = i_mce.extcpu;
 	u8 b = i_mce.bank;
 
-	rdtscll(i_mce.tsc);
+	i_mce.tsc = rdtsc_ordered();
 
 	if (i_mce.misc)
 		i_mce.status |= MCI_STATUS_MISCV;
