fe3c948918e7e3d18eed85571d32a2f7c4b63a84
Feifei Xu <Feifei.Xu@amd.com>
2018-03-23 14:44:28 -0500

    drm/amdgpu: Add nbio 7.4 support for vega20 (v3)
    Some register offset in nbio v7.4 are different with v7.0.
    We need a seperate nbio_v7_4.c for vega20.
    
    v2: fix doorbell range for sdma (Alex)
    v3: squash in static fix (kbuild test robot)
    
    Signed-off-by: Feifei Xu <Feifei.Xu@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/nbio_v7_4.h
+++ drivers/gpu/drm/amd/amdgpu/nbio_v7_4.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Advanced Micro Devices, Inc.
+ * Copyright 2018 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -21,41 +21,11 @@
  *
  */
 
-#ifndef __SOC15_H__
-#define __SOC15_H__
+#ifndef __NBIO_V7_4_H__
+#define __NBIO_V7_4_H__
 
-#include "nbio_v6_1.h"
-#include "nbio_v7_0.h"
-#include "nbio_v7_4.h"
+#include "soc15_common.h"
 
-#define SOC15_FLUSH_GPU_TLB_NUM_WREG		4
-#define SOC15_FLUSH_GPU_TLB_NUM_REG_WAIT	1
-
-extern const struct amd_ip_funcs soc15_common_ip_funcs;
-
-struct soc15_reg_golden {
-	u32	hwip;
-	u32	instance;
-	u32	segment;
-	u32	reg;
-	u32	and_mask;
-	u32	or_mask;
-};
-
-#define SOC15_REG_ENTRY(ip, inst, reg)	ip##_HWIP, inst, reg##_BASE_IDX, reg
-
-#define SOC15_REG_GOLDEN_VALUE(ip, inst, reg, and_mask, or_mask) \
-	{ ip##_HWIP, inst, reg##_BASE_IDX, reg, and_mask, or_mask }
-
-void soc15_grbm_select(struct amdgpu_device *adev,
-		    u32 me, u32 pipe, u32 queue, u32 vmid);
-int soc15_set_ip_blocks(struct amdgpu_device *adev);
-
-void soc15_program_register_sequence(struct amdgpu_device *adev,
-					     const struct soc15_reg_golden *registers,
-					     const u32 array_size);
-
-int vega10_reg_base_init(struct amdgpu_device *adev);
-int vega20_reg_base_init(struct amdgpu_device *adev);
+extern const struct amdgpu_nbio_funcs nbio_v7_4_funcs;
 
 #endif
