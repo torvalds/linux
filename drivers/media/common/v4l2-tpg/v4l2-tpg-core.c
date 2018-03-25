e605e9e339379730c84a6b51bf9a12da2d64b77b
Hans Verkuil <hverkuil@xs4all.nl>
2018-03-25 17:40:30 -0400

    media: v4l2-tpg-core.c: add space after %
    I know, it's a measly space, but I can't stand it since the
    V4L2_PIX_FMT_NV24 case before this case does it right.
    
    So add the space in order to restore blessed symmetry and
    consistency and to make the world whole again...
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/common/v4l2-tpg/v4l2-tpg-core.c
+++ drivers/media/common/v4l2-tpg/v4l2-tpg-core.c
@@ -1149,7 +1149,7 @@ static void gen_twopix(struct tpg_data *tpg,
 	case V4L2_PIX_FMT_NV42:
 		buf[0][offset] = r_y_h;
 		buf[1][2 * offset] = b_v;
-		buf[1][(2 * offset + 1) %8] = g_u_s;
+		buf[1][(2 * offset + 1) % 8] = g_u_s;
 		break;
 
 	case V4L2_PIX_FMT_YUYV:
