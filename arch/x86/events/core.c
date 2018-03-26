631fe154edb0a37308d0116a0f9b7bba9dca6218
Davidlohr Bueso <dave@stgolabs.net>
2018-03-26 14:09:27 -0700

    perf/x86: Update rdpmc_always_available static key to the modern API
    No changes in refcount semantics -- use DEFINE_STATIC_KEY_FALSE()
    for initialization and replace:
    
      static_key_slow_inc|dec()   =>   static_branch_inc|dec()
      static_key_false()          =>   static_branch_unlikely()
    
    Added a '_key' suffix to rdpmc_always_available, for better self-documentation.
    
    Signed-off-by: Davidlohr Bueso <dbueso@suse.de>
    Cc: Davidlohr Bueso <dave@stgolabs.net>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: akpm@linux-foundation.org
    Link: http://lkml.kernel.org/r/20180326210929.5244-5-dave@stgolabs.net
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/events/core.c
+++ arch/x86/events/core.c
@@ -48,7 +48,7 @@ DEFINE_PER_CPU(struct cpu_hw_events, cpu_hw_events) = {
 	.enabled = 1,
 };
 
-struct static_key rdpmc_always_available = STATIC_KEY_INIT_FALSE;
+DEFINE_STATIC_KEY_FALSE(rdpmc_always_available_key);
 
 u64 __read_mostly hw_cache_event_ids
 				[PERF_COUNT_HW_CACHE_MAX]
@@ -2206,9 +2206,9 @@ static ssize_t set_attr_rdpmc(struct device *cdev,
 		 * but only root can trigger it, so it's okay.
 		 */
 		if (val == 2)
-			static_key_slow_inc(&rdpmc_always_available);
+			static_branch_inc(&rdpmc_always_available_key);
 		else
-			static_key_slow_dec(&rdpmc_always_available);
+			static_branch_dec(&rdpmc_always_available_key);
 		on_each_cpu(refresh_pce, NULL, 1);
 	}
 
