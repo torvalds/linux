be6d7e1f7d368f617c82290b834073a3e8a41f17
Kieran Bingham <kieran.bingham@ideasonboard.com>
2018-03-27 17:46:02 +0100

    FROMLIST: media: uvcvideo: queue: Support asynchronous buffer handling
    The buffer queue interface currently operates sequentially, processing
    buffers after they have fully completed.
    
    In preparation for supporting parallel tasks operating on the buffers,
    we will need to support buffers being processed on multiple CPUs.
    
    Adapt the uvc_queue_next_buffer() such that a reference count tracks the
    active use of the buffer, returning the buffer to the VB2 stack at
    completion.
    
    Change-Id: Ia42cca9bed7852175202017b042590d844e8c6b0
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Signed-off-by: William Wu <william.wu@rock-chips.com>
    (am from https://patchwork.kernel.org/patch/10311051/)

--- drivers/media/usb/uvc/uvcvideo.h
+++ drivers/media/usb/uvc/uvcvideo.h
@@ -382,6 +382,9 @@ struct uvc_buffer {
 	unsigned int bytesused;
 
 	u32 pts;
+
+	/* asynchronous buffer handling */
+	struct kref ref;
 };
 
 #define UVC_QUEUE_DISCONNECTED		(1 << 0)
@@ -685,6 +688,7 @@ extern void uvc_queue_cancel(struct uvc_video_queue *queue, int disconnect);
 extern struct uvc_buffer *uvc_queue_next_buffer(struct uvc_video_queue *queue,
 		struct uvc_buffer *buf);
 struct uvc_buffer *uvc_queue_get_current_buffer(struct uvc_video_queue *queue);
+void uvc_queue_buffer_release(struct uvc_buffer *buf);
 extern int uvc_queue_mmap(struct uvc_video_queue *queue,
 		struct vm_area_struct *vma);
 extern unsigned int uvc_queue_poll(struct uvc_video_queue *queue,
