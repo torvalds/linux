1921e997eb4761ae2285ad763555eed9c4abec46
Alexandra Sandulescu <alecsandra.sandulescu@gmail.com>
2018-03-28 17:18:59 +0300

    tools: assignments: 2-uart: Migrate code from former repo
    tools: assignments: 2-uart: Apply checkpatch.pl
    
    Signed-off-by: Alexandra Sandulescu <alecsandra.sandulescu@gmail.com>

---
+++ tools/labs/templates/assignments/2-uart/checker/_test/uart16550.h
@@ -0,0 +1,46 @@
+#ifndef UART16550_H
+#define UART16550_H
+
+#define OPTION_COM1			1
+#define OPTION_COM2			2
+#define OPTION_BOTH			3
+
+#define UART16550_COM1_SELECTED		0x01
+#define UART16550_COM2_SELECTED		0x02
+
+#define MAX_NUMBER_DEVICES		2
+
+#ifndef _UART16550_REGS_H
+
+#define UART16550_BAUD_1200		96
+#define UART16550_BAUD_2400		48
+#define UART16550_BAUD_4800		24
+#define UART16550_BAUD_9600		12
+#define UART16550_BAUD_19200		6
+#define UART16550_BAUD_38400		3
+#define UART16550_BAUD_56000		2
+#define UART16550_BAUD_115200		1
+
+#define UART16550_LEN_5			0x00
+#define UART16550_LEN_6			0x01
+#define UART16550_LEN_7			0x02
+#define UART16550_LEN_8			0x03
+
+#define UART16550_STOP_1		0x00
+#define UART16550_STOP_2		0x04
+
+#define UART16550_PAR_NONE		0x00
+#define UART16550_PAR_ODD		0x08
+#define UART16550_PAR_EVEN		0x18
+#define UART16550_PAR_STICK		0x20
+
+#endif
+
+#define UART16550_IOCTL_SET_LINE	1
+
+struct uart16550_line_info {
+	unsigned char baud, len, par, stop;
+};
+
+#endif
+
