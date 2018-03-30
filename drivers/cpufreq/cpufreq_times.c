535aacd93e02fed3b205c701670fb7e6ae168720
Connor O'Brien <connoro@google.com>
2018-03-30 16:43:26 -0700

    ANDROID: cpufreq: times: allocate enough space for a uid_entry
    since the variable called uid_entry is a pointer, need to use
    sizeof(*uid_entry) to allocate enough space for a full uid_entry
    struct.
    
    Bug: 74338318
    Change-Id: I488a7cab849398ef7b1f4712b7746f8cf645209d
    Signed-off-by: Connor O'Brien <connoro@google.com>

--- drivers/cpufreq/cpufreq_times.c
+++ drivers/cpufreq/cpufreq_times.c
@@ -88,7 +88,7 @@ static struct uid_entry *find_or_register_uid_locked(uid_t uid)
 {
 	struct uid_entry *uid_entry, *temp;
 	unsigned int max_state = READ_ONCE(next_offset);
-	size_t alloc_size = sizeof(uid_entry) + max_state *
+	size_t alloc_size = sizeof(*uid_entry) + max_state *
 		sizeof(uid_entry->time_in_state[0]);
 
 	uid_entry = find_uid_entry_locked(uid);
