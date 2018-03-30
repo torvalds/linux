e5b1db0186bfb3bede41e412b27c9bcf2b336622
Bjorn Helgaas <bhelgaas@google.com>
2018-03-30 14:41:49 -0500

    PCI: Remove unused pcie_get_minimum_link()
    In some cases pcie_get_minimum_link() returned misleading information
    because it found the slowest link and the narrowest link without
    considering the total bandwidth of the link.
    
    For example, consider a path with these two links:
    
      - 16.0 GT/s  x1 link  (16.0 * 10^9 * 128 / 130) *  1 / 8 = 1969 MB/s
      -  2.5 GT/s x16 link  ( 2.5 * 10^9 *   8 /  10) * 16 / 8 = 4000 MB/s
    
    The available bandwidth of the path is limited by the 16 GT/s link to about
    1969 MB/s, but pcie_get_minimum_link() returned 2.5 GT/s x1, which
    corresponds to only 250 MB/s.
    
    Callers should use pcie_print_link_status() instead, or
    pcie_bandwidth_available() if they need more detailed information.
    
    Remove pcie_get_minimum_link() since there are no callers left.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -1080,8 +1080,6 @@ int pcie_get_readrq(struct pci_dev *dev);
 int pcie_set_readrq(struct pci_dev *dev, int rq);
 int pcie_get_mps(struct pci_dev *dev);
 int pcie_set_mps(struct pci_dev *dev, int mps);
-int pcie_get_minimum_link(struct pci_dev *dev, enum pci_bus_speed *speed,
-			  enum pcie_link_width *width);
 u32 pcie_bandwidth_available(struct pci_dev *dev, struct pci_dev **limiting_dev,
 			     enum pci_bus_speed *speed,
 			     enum pcie_link_width *width);
