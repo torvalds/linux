6cb5aa02bfbd5081549988aa76cd3598eb1acdab
Yunlong Song <yunlong.song@huawei.com>
2018-04-02 20:22:20 +0800

    f2fs: make assignment of t->dentry_bitmap more readable
    In make_dentry_ptr_block, it is confused with "&" for t->dentry_bitmap
    but without "&" for t->dentry, so delete "&" to make code more readable.
    
    Signed-off-by: Yunlong Song <yunlong.song@huawei.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -536,7 +536,7 @@ static inline void make_dentry_ptr_block(struct inode *inode,
 	d->inode = inode;
 	d->max = NR_DENTRY_IN_BLOCK;
 	d->nr_bitmap = SIZE_OF_DENTRY_BITMAP;
-	d->bitmap = &t->dentry_bitmap;
+	d->bitmap = t->dentry_bitmap;
 	d->dentry = t->dentry;
 	d->filename = t->filename;
 }
