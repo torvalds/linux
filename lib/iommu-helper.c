79c1879ee5473e3404469b07f9bccfe6d0814b93
Christoph Hellwig <hch@lst.de>
2018-04-03 15:41:07 +0200

    iommu-helper: mark iommu_is_span_boundary as inline
    This avoids selecting IOMMU_HELPER just for this function.  And we only
    use it once or twice in normal builds so this often even is a size
    reduction.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- lib/iommu-helper.c
+++ lib/iommu-helper.c
@@ -4,17 +4,7 @@
  */
 
 #include <linux/bitmap.h>
-#include <linux/bug.h>
-
-int iommu_is_span_boundary(unsigned int index, unsigned int nr,
-			   unsigned long shift,
-			   unsigned long boundary_size)
-{
-	BUG_ON(!is_power_of_2(boundary_size));
-
-	shift = (shift + index) & (boundary_size - 1);
-	return shift + nr > boundary_size;
-}
+#include <linux/iommu-helper.h>
 
 unsigned long iommu_area_alloc(unsigned long *map, unsigned long size,
 			       unsigned long start, unsigned int nr,
