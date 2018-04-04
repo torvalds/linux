0d8029a0480bb90f570d77105a91746fa1afdc49
Christian Lütke-Stetzkamp <christian@lkamp.de>
2018-04-04 22:15:12 +0200

    staging: mt7621-mmc: Remove whitespace errors in dbg.h
    The whitespace errors in the file dbg.h are fixed by using the
    cleanfile script. Indentations with whitespaces are not changed
    in this patch.
    
    Signed-off-by: Christian Lütke-Stetzkamp <christian@lkamp.de>
    Reviewed-by: NeilBrown <neil@brown.name>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/mt7621-mmc/dbg.h
+++ drivers/staging/mt7621-mmc/dbg.h
@@ -40,9 +40,9 @@ extern u32 sdio_pro_enable;
 /* for a type command, e.g. CMD53, 2 blocks */
 struct cmd_profile {
     u32 max_tc;    /* Max tick count */
-    u32 min_tc; 	
+    u32 min_tc;
     u32 tot_tc;    /* total tick count */
-    u32 tot_bytes;  
+    u32 tot_bytes;
     u32 count;     /* the counts of the command */
 };
 
@@ -51,27 +51,27 @@ struct sdio_profile {
     u32 total_tc;         /* total tick count of CMD52 and CMD53 */
     u32 total_tx_bytes;   /* total bytes of CMD53 Tx */
     u32 total_rx_bytes;   /* total bytes of CMD53 Rx */
-    
+
     /*CMD52*/
-    struct cmd_profile cmd52_tx; 
-    struct cmd_profile cmd52_rx; 
+    struct cmd_profile cmd52_tx;
+    struct cmd_profile cmd52_rx;
 
     /*CMD53 in byte unit */
-    struct cmd_profile cmd53_tx_byte[512]; 
-    struct cmd_profile cmd53_rx_byte[512];            	
-    
+    struct cmd_profile cmd53_tx_byte[512];
+    struct cmd_profile cmd53_rx_byte[512];
+
     /*CMD53 in block unit */
-    struct cmd_profile cmd53_tx_blk[100]; 
-    struct cmd_profile cmd53_rx_blk[100];         
+    struct cmd_profile cmd53_tx_blk[100];
+    struct cmd_profile cmd53_rx_blk[100];
 };
 
 //==========================
 typedef enum {
-    SD_TOOL_ZONE = 0, 
-    SD_TOOL_DMA_SIZE  = 1,	
+    SD_TOOL_ZONE = 0,
+    SD_TOOL_DMA_SIZE  = 1,
     SD_TOOL_PM_ENABLE = 2,
-    SD_TOOL_SDIO_PROFILE = 3,     
-} msdc_dbg;	
+    SD_TOOL_SDIO_PROFILE = 3,
+} msdc_dbg;
 
 typedef enum {
     MODE_PIO = 0,
@@ -123,12 +123,12 @@ do {    \
 do { \
     printk(KERN_ERR TAG"%d -> "fmt" <- %s() : L<%d> PID<%s><0x%x>\n", \
         host->id,  ##args , __FUNCTION__, __LINE__, current->comm, current->pid); \
-} while(0); 
+} while(0);
 
 #if 1
-//defined CONFIG_MTK_MMC_CD_POLL        
+//defined CONFIG_MTK_MMC_CD_POLL
 #define INIT_MSG(fmt, args...)
-#define IRQ_MSG(fmt, args...) 
+#define IRQ_MSG(fmt, args...)
 #else
 #define INIT_MSG(fmt, args...) \
 do { \
@@ -144,13 +144,13 @@ do { \
 } while(0);
 #endif
 
-int msdc_debug_proc_init(void); 
+int msdc_debug_proc_init(void);
 
 #if 0 /* --- chhung */
 void msdc_init_gpt(void);
 extern void GPT_GetCounter64(UINT32 *cntL32, UINT32 *cntH32);
 #endif /* end of --- */
 u32 msdc_time_calc(u32 old_L32, u32 old_H32, u32 new_L32, u32 new_H32);
-void msdc_performance(u32 opcode, u32 sizes, u32 bRx, u32 ticks);   
+void msdc_performance(u32 opcode, u32 sizes, u32 bRx, u32 ticks);
 
 #endif
