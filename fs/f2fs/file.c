cd79eb2b5e451ca0be15338684252aef56dd319d
Chao Yu <yuchao0@huawei.com>
2018-04-04 17:35:13 +0800

    f2fs: remove redundant block plug
    For buffered IO, we don't need to use block plug to cache bio,
    for direct IO, generic f2fs_direct_IO has already added block
    plug, so let's remove redundant one in .write_iter.
    
    As Yunlei described in his patch:
    
    -f2fs_file_write_iter
      -blk_start_plug
        -__generic_file_write_iter
    	...
    	  -do_blockdev_direct_IO
    	    -blk_start_plug
    		...
    	    -blk_finish_plug
    	...
      -blk_finish_plug
    
    which may conduct performance decrease in our platform
    
    Signed-off-by: Yunlei He <heyunlei@huawei.com>
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -2695,7 +2695,6 @@ static ssize_t f2fs_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 {
 	struct file *file = iocb->ki_filp;
 	struct inode *inode = file_inode(file);
-	struct blk_plug plug;
 	ssize_t ret;
 
 	if (unlikely(f2fs_cp_error(F2FS_I_SB(inode))))
@@ -2740,9 +2739,7 @@ static ssize_t f2fs_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 				return err;
 			}
 		}
-		blk_start_plug(&plug);
 		ret = __generic_file_write_iter(iocb, from);
-		blk_finish_plug(&plug);
 		clear_inode_flag(inode, FI_NO_PREALLOC);
 
 		/* if we couldn't write data, we should deallocate blocks. */
