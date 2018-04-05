5844a486daf2705dcdbfabe869a698bdfe629f54
Guenter Roeck <linux@roeck-us.net>
2018-04-05 16:24:32 -0700

    include/linux/mm.h: provide consistent declaration for num_poisoned_pages
    clang reports the following compile warning.
    
      In file included from mm/vmscan.c:56:
      ./include/linux/swapops.h:327:22: warning:
    	section attribute is specified on redeclared variable [-Wsection]
      extern atomic_long_t num_poisoned_pages __read_mostly;
                           ^
      ./include/linux/mm.h:2585:22: note: previous declaration is here
      extern atomic_long_t num_poisoned_pages;
                         ^
    
    Let's use __read_mostly everywhere.
    
    Link: http://lkml.kernel.org/r/1519686565-8224-1-git-send-email-linux@roeck-us.net
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Matthias Kaehlcke <mka@chromium.org>
    Cc: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2592,7 +2592,7 @@ extern int get_hwpoison_page(struct page *page);
 extern int sysctl_memory_failure_early_kill;
 extern int sysctl_memory_failure_recovery;
 extern void shake_page(struct page *p, int access);
-extern atomic_long_t num_poisoned_pages;
+extern atomic_long_t num_poisoned_pages __read_mostly;
 extern int soft_offline_page(struct page *page, int flags);
 
 
