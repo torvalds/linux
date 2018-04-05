8f041940aaf2674dcccd15753d68ff885f90d5e7
Geert Uytterhoeven <geert@linux-m68k.org>
2018-04-05 14:57:11 +0200

    cifs: Use ULL suffix for 64-bit constant
    [ Upstream commit 3995bbf53bd2047f2720c6fdd4bf38f6d942a0c0 ]
    
    On 32-bit (e.g. with m68k-linux-gnu-gcc-4.1):
    
        fs/cifs/inode.c: In function ‘simple_hashstr’:
        fs/cifs/inode.c:713: warning: integer constant is too large for ‘long’ type
    
    Fixes: 7ea884c77e5c97f1 ("smb3: Fix root directory when server returns inode number of zero")
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Reviewed-by: Aurelien Aptel <aaptel@suse.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/cifs/inode.c
+++ fs/cifs/inode.c
@@ -712,7 +712,7 @@ cgfi_exit:
 /* Simple function to return a 64 bit hash of string.  Rarely called */
 static __u64 simple_hashstr(const char *str)
 {
-	const __u64 hash_mult =  1125899906842597L; /* a big enough prime */
+	const __u64 hash_mult =  1125899906842597ULL; /* a big enough prime */
 	__u64 hash = 0;
 
 	while (*str)
