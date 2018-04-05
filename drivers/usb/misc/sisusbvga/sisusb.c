cedf3d01cb002602a4d366a07984f7506dce8f34
teaberryy <ekisneels@gmail.com>
2018-04-05 11:30:45 +0200

    Added USB sisusb for USB2VGA
    Specifically for the Magic Control Technology Corp. Magic Control Technology Corp. dongle.

--- drivers/usb/misc/sisusbvga/sisusb.c
+++ drivers/usb/misc/sisusbvga/sisusb.c
@@ -3192,6 +3192,7 @@ static const struct usb_device_id sisusb_table[] = {
 	{ USB_DEVICE(0x0711, 0x0918) },
 	{ USB_DEVICE(0x0711, 0x0920) },
 	{ USB_DEVICE(0x0711, 0x0950) },
+	{ USB_DEVICE(0x0711, 0x5100) },
 	{ USB_DEVICE(0x0711, 0x5200) },
 	{ USB_DEVICE(0x182d, 0x021c) },
 	{ USB_DEVICE(0x182d, 0x0269) },
