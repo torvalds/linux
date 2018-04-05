8bd30c1090995ce800ae257fbbabfd6b6899b57c
Dou Liyang <douly.fnst@cn.fujitsu.com>
2018-04-05 16:23:46 -0700

    mm/kmemleak.c: make kmemleak_boot_config() __init
    The early_param() is only called during kernel initialization, So Linux
    marks the functions of it with __init macro to save memory.
    
    But it forgot to mark the kmemleak_boot_config().  So, Make it __init as
    well.
    
    Link: http://lkml.kernel.org/r/20180117034720.26897-1-douly.fnst@cn.fujitsu.com
    Signed-off-by: Dou Liyang <douly.fnst@cn.fujitsu.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/kmemleak.c
+++ mm/kmemleak.c
@@ -1963,7 +1963,7 @@ static void kmemleak_disable(void)
 /*
  * Allow boot-time kmemleak disabling (enabled by default).
  */
-static int kmemleak_boot_config(char *str)
+static int __init kmemleak_boot_config(char *str)
 {
 	if (!str)
 		return -EINVAL;
