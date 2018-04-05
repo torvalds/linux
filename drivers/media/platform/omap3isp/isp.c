3f4836beb2ebeb0211d9911d878a267d687e0e6e
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2018-04-05 10:42:48 -0400

    media: omap3isp/isp: remove an unused static var
    The isp_xclk_init_data const data isn't used anywere.
    
    drivers/media/platform/omap3isp/isp.c:294:35: warning: ‘isp_xclk_init_data’ defined but not used [-Wunused-const-variable=]
     static const struct clk_init_data isp_xclk_init_data = {
                                       ^~~~~~~~~~~~~~~~~~
    
    Fixes: 9b28ee3c9122 ("[media] omap3isp: Use the common clock framework")
    
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/platform/omap3isp/isp.c
+++ drivers/media/platform/omap3isp/isp.c
@@ -284,13 +284,6 @@ static const struct clk_ops isp_xclk_ops = {
 
 static const char *isp_xclk_parent_name = "cam_mclk";
 
-static const struct clk_init_data isp_xclk_init_data = {
-	.name = "cam_xclk",
-	.ops = &isp_xclk_ops,
-	.parent_names = &isp_xclk_parent_name,
-	.num_parents = 1,
-};
-
 static struct clk *isp_xclk_src_get(struct of_phandle_args *clkspec, void *data)
 {
 	unsigned int idx = clkspec->args[0];
