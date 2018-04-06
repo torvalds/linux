660661afcd40ed7f515ef3369721ed58e80c0fc5
Victor Gu <xigu@marvell.com>
2018-04-06 16:55:31 +0200

    PCI: aardvark: Fix logic in advk_pcie_{rd,wr}_conf()
    The PCI configuration space read/write functions were special casing
    the situation where PCI_SLOT(devfn) != 0, and returned
    PCIBIOS_DEVICE_NOT_FOUND in this case.
    
    However, while this is what is intended for the root bus, it is not
    intended for the child busses, as it prevents discovering devices with
    PCI_SLOT(x) != 0. Therefore, we return PCIBIOS_DEVICE_NOT_FOUND only
    if we're on the root bus.
    
    Fixes: 8c39d710363c1 ("PCI: aardvark: Add Aardvark PCI host controller driver")
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Victor Gu <xigu@marvell.com>
    Reviewed-by: Wilson Ding <dingwei@marvell.com>
    Reviewed-by: Nadav Haklai <nadavh@marvell.com>
    [Thomas: tweak commit log.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>

--- drivers/pci/host/pci-aardvark.c
+++ drivers/pci/host/pci-aardvark.c
@@ -437,7 +437,7 @@ static int advk_pcie_rd_conf(struct pci_bus *bus, u32 devfn,
 	u32 reg;
 	int ret;
 
-	if (PCI_SLOT(devfn) != 0) {
+	if ((bus->number == pcie->root_bus_nr) && PCI_SLOT(devfn) != 0) {
 		*val = 0xffffffff;
 		return PCIBIOS_DEVICE_NOT_FOUND;
 	}
@@ -491,7 +491,7 @@ static int advk_pcie_wr_conf(struct pci_bus *bus, u32 devfn,
 	int offset;
 	int ret;
 
-	if (PCI_SLOT(devfn) != 0)
+	if ((bus->number == pcie->root_bus_nr) && PCI_SLOT(devfn) != 0)
 		return PCIBIOS_DEVICE_NOT_FOUND;
 
 	if (where % size)
