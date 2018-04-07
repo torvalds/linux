7d2f6abb402ae38ec4bb7beabb3980bb834b1e0d
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2018-04-07 19:11:55 +0200

    time: hrtimer: Use timerqueue_iterate_next() to get to the next timer
    Use timerqueue_iterate_next() to get to the next timer in
    __hrtimer_next_event_base() without browsing the timerqueue
    details diredctly.
    
    No intentional changes in functionality.
    
    Suggested-by: Frederic Weisbecker <frederic@kernel.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -505,9 +505,7 @@ static ktime_t __hrtimer_next_event_base(struct hrtimer_cpu_base *cpu_base,
 		timer = container_of(next, struct hrtimer, node);
 		if (timer == exclude) {
 			/* Get to the next timer in the queue. */
-			struct rb_node *rbn = rb_next(&next->node);
-
-			next = rb_entry_safe(rbn, struct timerqueue_node, node);
+			next = timerqueue_iterate_next(next);
 			if (!next)
 				continue;
 
