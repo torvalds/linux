425f1e6272567c4d91221957d8b65fc5f6aae501
Rafał Miłecki <rafal@milecki.pl>
2018-04-08 22:57:32 +0200

    MIPS: BCM47XX: Add support for Netgear WNR1000 V3
    This adds support for detecting this model board and registers some LEDs
    and buttons.
    
    There are two uncommon things regarding this device:
    1) It can use two different "board_id" ID values.
       Unit I have uses "U12H139T00_NETGEAR" value. This magic is also used
       in firmware file header. There are two reports (one from an OpenWrt
       user) of a different "U12H139T50_NETGEAR" magic though.
    2) Power LEDs share GPIOs with buttons.
       Amber one seems to share GPIO 2 with WPS button and green one seems
       to share GPIO 3 with reset button. It remains unknown how to support
       them and handle buttons at the same time. For that reason they aren't
       added to the list of supported LEDs.
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: Hauke Mehrtens <hauke@hauke-m.de>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/19004/
    Signed-off-by: James Hogan <jhogan@kernel.org>

--- arch/mips/bcm47xx/buttons.c
+++ arch/mips/bcm47xx/buttons.c
@@ -412,6 +412,12 @@ bcm47xx_buttons_netgear_wndr4500v1[] __initconst = {
 };
 
 static const struct gpio_keys_button
+bcm47xx_buttons_netgear_wnr1000_v3[] __initconst = {
+	BCM47XX_GPIO_KEY(2, KEY_WPS_BUTTON),
+	BCM47XX_GPIO_KEY(3, KEY_RESTART),
+};
+
+static const struct gpio_keys_button
 bcm47xx_buttons_netgear_wnr3500lv1[] __initconst = {
 	BCM47XX_GPIO_KEY(4, KEY_RESTART),
 	BCM47XX_GPIO_KEY(6, KEY_WPS_BUTTON),
@@ -669,6 +675,9 @@ int __init bcm47xx_buttons_register(void)
 		break;
 	case BCM47XX_BOARD_NETGEAR_WNDR4500V1:
 		err = bcm47xx_copy_bdata(bcm47xx_buttons_netgear_wndr4500v1);
+		break;
+	case BCM47XX_BOARD_NETGEAR_WNR1000_V3:
+		err = bcm47xx_copy_bdata(bcm47xx_buttons_netgear_wnr1000_v3);
 		break;
 	case BCM47XX_BOARD_NETGEAR_WNR3500L:
 		err = bcm47xx_copy_bdata(bcm47xx_buttons_netgear_wnr3500lv1);
