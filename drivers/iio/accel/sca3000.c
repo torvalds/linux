5d6ad5a030675c6527956cf8737657b6be81b41b
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2018-04-08 21:44:01 +0200

    iio: sca3000: Fix an error handling path in 'sca3000_probe()'
    commit 4a5b45383ca371e123ba103d34d4b3b87616245c upstream.
    
    Use 'devm_iio_kfifo_allocate()' instead of 'iio_kfifo_allocate()' in order
    to simplify code and avoid a memory leak in an error path in
    'sca3000_probe()'. A call to 'sca3000_unconfigure_ring()' was missing.
    
    Sent via the next merge window as unimportant bug and there are
    other patches dependent on it.
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Cc: <Stable@vger.kernel.org>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/iio/accel/sca3000.c
+++ drivers/iio/accel/sca3000.c
@@ -1277,7 +1277,7 @@ static int sca3000_configure_ring(struct iio_dev *indio_dev)
 {
 	struct iio_buffer *buffer;
 
-	buffer = iio_kfifo_allocate();
+	buffer = devm_iio_kfifo_allocate(&indio_dev->dev);
 	if (!buffer)
 		return -ENOMEM;
 
@@ -1285,11 +1285,6 @@ static int sca3000_configure_ring(struct iio_dev *indio_dev)
 	indio_dev->modes |= INDIO_BUFFER_SOFTWARE;
 
 	return 0;
-}
-
-static void sca3000_unconfigure_ring(struct iio_dev *indio_dev)
-{
-	iio_kfifo_free(indio_dev->buffer);
 }
 
 static inline
@@ -1546,8 +1541,6 @@ static int sca3000_remove(struct spi_device *spi)
 	sca3000_stop_all_interrupts(st);
 	if (spi->irq)
 		free_irq(spi->irq, indio_dev);
-
-	sca3000_unconfigure_ring(indio_dev);
 
 	return 0;
 }
