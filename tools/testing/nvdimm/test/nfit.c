9484e12d7999a3c82d8732b60c0149f038bdd985
Dan Williams <dan.j.williams@intel.com>
2018-04-09 14:29:33 -0700

    tools/testing/nvdimm: enable labels for nfit_test.1 dimms
    Enable test cases for the kernel's fallback to label-less mode.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- tools/testing/nvdimm/test/nfit.c
+++ tools/testing/nvdimm/test/nfit.c
@@ -2265,6 +2265,9 @@ static void nfit_test1_setup(struct nfit_test *t)
 	set_bit(ND_CMD_ARS_STATUS, &acpi_desc->bus_cmd_force_en);
 	set_bit(ND_CMD_CLEAR_ERROR, &acpi_desc->bus_cmd_force_en);
 	set_bit(ND_INTEL_ENABLE_LSS_STATUS, &acpi_desc->dimm_cmd_force_en);
+	set_bit(ND_CMD_GET_CONFIG_SIZE, &acpi_desc->dimm_cmd_force_en);
+	set_bit(ND_CMD_GET_CONFIG_DATA, &acpi_desc->dimm_cmd_force_en);
+	set_bit(ND_CMD_SET_CONFIG_DATA, &acpi_desc->dimm_cmd_force_en);
 }
 
 static int nfit_test_blk_do_io(struct nd_blk_region *ndbr, resource_size_t dpa,
