3d102fc0e7b02d4e16752c15aa92923405b01388
Gilad Ben-Yossef <gilad@benyossef.com>
2018-04-10 16:33:17 -0700

    checkpatch: add Crypto ON_STACK to declaration_macros
    Add the crypto API *_ON_STACK to $declaration_macros.
    
    Resolves the following false warning:
    
    WARNING: Missing a blank line after declarations
    +			int err;
    +			SHASH_DESC_ON_STACK(desc, ctx_p->shash_tfm);
    
    Link: http://lkml.kernel.org/r/1518941636-4484-1-git-send-email-gilad@benyossef.com
    Signed-off-by: Gilad Ben-Yossef <gilad@benyossef.com>
    Signed-off-by: Joe Perches <joe@perches.com>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -791,7 +791,8 @@ our $FuncArg = qr{$Typecast{0,1}($LvalOrFunc|$Constant|$String)};
 our $declaration_macros = qr{(?x:
 	(?:$Storage\s+)?(?:[A-Z_][A-Z0-9]*_){0,2}(?:DEFINE|DECLARE)(?:_[A-Z0-9]+){1,6}\s*\(|
 	(?:$Storage\s+)?[HLP]?LIST_HEAD\s*\(|
-	(?:$Storage\s+)?${Type}\s+uninitialized_var\s*\(
+	(?:$Storage\s+)?${Type}\s+uninitialized_var\s*\(|
+	(?:SKCIPHER_REQUEST|SHASH_DESC|AHASH_REQUEST)_ON_STACK\s*\(
 )};
 
 sub deparenthesize {
