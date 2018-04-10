38dca988bb208e66d6fdb6346f7266f3d2d1a8a4
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-04-10 16:34:14 -0700

    checkpatch: allow space between colon and bracket
    Allow a space between a colon and subsequent opening bracket.  This
    sequence may occur in inline assembler statements like
    
    	asm(
    		"ldr %[out], [%[in]]\n\t"
    		: [out] "=r" (ret)
    		: [in] "r" (addr)
    	);
    
    Link: http://lkml.kernel.org/r/20180403191655.23700-1-xypron.glpk@gmx.de
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>
    Acked-by: Joe Perches <joe@perches.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -4089,7 +4089,7 @@ sub process {
 			my ($where, $prefix) = ($-[1], $1);
 			if ($prefix !~ /$Type\s+$/ &&
 			    ($where != 0 || $prefix !~ /^.\s+$/) &&
-			    $prefix !~ /[{,]\s+$/) {
+			    $prefix !~ /[{,:]\s+$/) {
 				if (ERROR("BRACKET_SPACE",
 					  "space prohibited before open square bracket '['\n" . $herecurr) &&
 				    $fix) {
