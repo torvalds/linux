16a3c17575c41d8f2e8406e89b9f3ba1eaabea19
Stefan Popa <stefan.popa@analog.com>
2018-04-11 14:52:12 +0300

    iio:dac:ad5686: Change license description
    Add GPLv2+ SPDX identifier and remove license notice to keep the whole
    purpose of using an SPDx id.
    
    Signed-off-by: Stefan Popa <stefan.popa@analog.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/dac/ad5686.c
+++ drivers/iio/dac/ad5686.c
@@ -1,9 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
  * AD5686R, AD5685R, AD5684R Digital to analog converters  driver
  *
  * Copyright 2011 Analog Devices Inc.
- *
- * Licensed under the GPL-2.
  */
 
 #include <linux/interrupt.h>
