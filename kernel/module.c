2c8fd268f41884bef5d37acda08d8006dc7da0ea
Jia Zhang <zhang.jia@linux.alibaba.com>
2018-04-11 11:53:33 +0800

    module: Do not access sig_enforce directly
    Call is_module_sig_enforced() instead.
    
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>
    Signed-off-by: Jessica Yu <jeyu@kernel.org>

--- kernel/module.c
+++ kernel/module.c
@@ -2785,7 +2785,7 @@ static int module_sig_check(struct load_info *info, int flags)
 	}
 
 	/* Not having a signature is only an error if we're strict. */
-	if (err == -ENOKEY && !sig_enforce)
+	if (err == -ENOKEY && !is_module_sig_enforced())
 		err = 0;
 
 	return err;
