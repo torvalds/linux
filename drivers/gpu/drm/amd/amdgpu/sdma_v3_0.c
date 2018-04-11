c3f27c08ec15b61bed2a1af592ab5bdc89fe7dee
Leo Liu <leo.liu@amd.com>
2018-04-11 15:22:20 -0500

    drm/amdgpu: add VEGAM SDMA golden settings
    Signed-off-by: Leo Liu <leo.liu@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/sdma_v3_0.c
+++ drivers/gpu/drm/amd/amdgpu/sdma_v3_0.c
@@ -211,6 +211,7 @@ static void sdma_v3_0_init_golden_registers(struct amdgpu_device *adev)
 		break;
 	case CHIP_POLARIS11:
 	case CHIP_POLARIS12:
+	case CHIP_VEGAM:
 		amdgpu_device_program_register_sequence(adev,
 							golden_settings_polaris11_a11,
 							ARRAY_SIZE(golden_settings_polaris11_a11));
