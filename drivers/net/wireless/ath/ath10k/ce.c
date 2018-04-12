1e8f77502341035f079039c9aa9a647ebc9d881a
Manikanta Pubbisetty <mpubbise@codeaurora.org>
2018-04-12 17:46:50 +0530

    ath10k: correct target assert problem due to CE5 stuck
    Correct a minor bug in the commit 0628467f97b5 ("ath10k: fix
    copy engine 5 destination ring stuck") which introduced a change to fix
    firmware assert that happens when ring indices of copy engine 5 are stuck
    for a specific duration, problem with this fix is that it did not use
    ring arithmatic. As a result,firmware asserts did not go away entirely
    athough the frequency of occurrence has reduced. Using ring arithmatic
    to fix the issue.
    
    Tested on QCA9984(fw version-10.4-3.4-00082).
    
    Fixes: 0628467f97b5 ("ath10k: fix copy engine 5 destination ring stuck)
    Signed-off-by: Manikanta Pubbisetty <mpubbise@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/ath10k/ce.c
+++ drivers/net/wireless/ath/ath10k/ce.c
@@ -620,7 +620,7 @@ void ath10k_ce_rx_update_write_idx(struct ath10k_ce_pipe *pipe, u32 nentries)
 	/* Prevent CE ring stuck issue that will occur when ring is full.
 	 * Make sure that write index is 1 less than read index.
 	 */
-	if ((cur_write_idx + nentries)  == dest_ring->sw_index)
+	if (((cur_write_idx + nentries) & nentries_mask) == dest_ring->sw_index)
 		nentries -= 1;
 
 	write_index = CE_RING_IDX_ADD(nentries_mask, write_index, nentries);
