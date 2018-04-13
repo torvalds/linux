1cbf29da3628b661379acba7b08a07ef1e5da3b5
Petr Tesarik <ptesarik@suse.cz>
2018-04-13 15:35:34 -0700

    kexec: export PG_swapbacked to VMCOREINFO
    Since commit 6326fec1122c ("mm: Use owner_priv bit for PageSwapCache,
    valid when PageSwapBacked"), PG_swapcache is an alias for
    PG_owner_priv_1, which may be also used for other purposes.
    
    To know whether the bit indeed has the PG_swapcache meaning, it is
    necessary to check PG_swapbacked, hence this bit must be exported.
    
    Link: http://lkml.kernel.org/r/20180410161345.142e142d@ezekiel.suse.cz
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Dave Young <dyoung@redhat.com>
    Cc: Xunlei Pang <xlpang@redhat.com>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Hari Bathini <hbathini@linux.vnet.ibm.com>
    Cc: "Kirill A. Shutemov" <kirill.shutemov@linux.intel.com>
    Cc: "Marc-Andr Lureau" <marcandre.lureau@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/crash_core.c
+++ kernel/crash_core.c
@@ -454,6 +454,7 @@ static int __init crash_save_vmcoreinfo_init(void)
 	VMCOREINFO_NUMBER(PG_lru);
 	VMCOREINFO_NUMBER(PG_private);
 	VMCOREINFO_NUMBER(PG_swapcache);
+	VMCOREINFO_NUMBER(PG_swapbacked);
 	VMCOREINFO_NUMBER(PG_slab);
 #ifdef CONFIG_MEMORY_FAILURE
 	VMCOREINFO_NUMBER(PG_hwpoison);
