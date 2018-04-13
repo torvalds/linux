c7d2a0eb6c028ba064bfe92d7667977418142c7c
Evan Green <evgreen@chromium.org>
2018-04-13 13:33:36 -0700

    clk: qcom: Base rcg parent rate off plan frequency
    _freq_tbl_determine_rate uses the pre_div found in the clock plan
    multiplied by the requested rate from the caller to determine the
    best parent rate to set. If the requested rate is not exactly equal
    to the rate that was found in the clock plan, then using the requested
    rate in parent rate calculations is incorrect. For instance, if 150MHz
    was requested, but 200MHz was the match found, and that plan had a
    pre_div of 3, then the parent should be set to 600MHz, not 450MHz.
    
    Signed-off-by: Evan Green <evgreen@chromium.org>
    Fixes: bcd61c0f535a ("clk: qcom: Add support for root clock generators (RCGs)")
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/qcom/clk-rcg2.c
+++ drivers/clk/qcom/clk-rcg2.c
@@ -211,6 +211,7 @@ static int _freq_tbl_determine_rate(struct clk_hw *hw, const struct freq_tbl *f,
 	clk_flags = clk_hw_get_flags(hw);
 	p = clk_hw_get_parent_by_index(hw, index);
 	if (clk_flags & CLK_SET_RATE_PARENT) {
+		rate = f->freq;
 		if (f->pre_div) {
 			rate /= 2;
 			rate *= f->pre_div + 1;
