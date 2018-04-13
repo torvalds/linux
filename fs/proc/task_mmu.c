27cca866e3fce0961e13b38b87e5322fe153e437
Ram Pai <linuxram@us.ibm.com>
2018-04-13 23:55:07 +1000

    mm/pkeys, x86, powerpc: Display pkey in smaps if arch supports pkeys
    Currently the architecture specific code is expected to display the
    protection keys in smap for a given vma. This can lead to redundant
    code and possibly to divergent formats in which the key gets
    displayed.
    
    This patch changes the implementation. It displays the pkey only if
    the architecture support pkeys, i.e arch_pkeys_enabled() returns true.
    
    x86 arch_show_smap() function is not needed anymore, delete it.
    
    Signed-off-by: Thiago Jung Bauermann <bauerman@linux.vnet.ibm.com>
    Signed-off-by: Ram Pai <linuxram@us.ibm.com>
    [mpe: Split out from larger patch, rebased on header changes]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Reviewed-by: Dave Hansen <dave.hansen@intel.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -18,6 +18,7 @@
 #include <linux/page_idle.h>
 #include <linux/shmem_fs.h>
 #include <linux/uaccess.h>
+#include <linux/pkeys.h>
 
 #include <asm/elf.h>
 #include <asm/tlb.h>
@@ -730,10 +731,6 @@ static int smaps_hugetlb_range(pte_t *pte, unsigned long hmask,
 }
 #endif /* HUGETLB_PAGE */
 
-void __weak arch_show_smap(struct seq_file *m, struct vm_area_struct *vma)
-{
-}
-
 #define SEQ_PUT_DEC(str, val) \
 		seq_put_decimal_ull_width(m, str, (val) >> 10, 8)
 static int show_smap(struct seq_file *m, void *v, int is_pid)
@@ -838,7 +835,8 @@ static int show_smap(struct seq_file *m, void *v, int is_pid)
 		seq_puts(m, " kB\n");
 	}
 	if (!rollup_mode) {
-		arch_show_smap(m, vma);
+		if (arch_pkeys_enabled())
+			seq_printf(m, "ProtectionKey:  %8u\n", vma_pkey(vma));
 		show_smap_vma_flags(m, vma);
 	}
 	m_cache_vma(m, vma);
