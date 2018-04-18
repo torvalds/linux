095fbc1d4e7e10b8321b8bf42b9fecb8115518b9
Christian Lütke-Stetzkamp <christian@lkamp.de>
2018-04-18 17:27:46 +0200

    staging: mt7621-mmc: Remove unused field dma_left_size of msdc_host
    The dma_left_size field of msdc_host is never used, remove it.
    
    Signed-off-by: Christian Lütke-Stetzkamp <christian@lkamp.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/mt7621-mmc/mt6575_sd.h
+++ drivers/staging/mt7621-mmc/mt6575_sd.h
@@ -924,7 +924,6 @@ struct msdc_host {
 
 	struct msdc_dma             dma;            /* dma channel */
 	u32                         dma_addr;       /* dma transfer address */
-	u32                         dma_left_size;  /* dma transfer left size */
 	u32                         dma_xfer_size;  /* dma transfer size in bytes */
 	int                         dma_xfer;       /* dma transfer mode */
 
