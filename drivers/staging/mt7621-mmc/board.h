d1a0db1eb3aa9f420d46135004560faa0108baa5
Christian Lütke-Stetzkamp <christian@lkamp.de>
2018-04-18 17:27:20 +0200

    staging: mt7621-mmc: Remove unused code from board.h
    The board.h file contins lot of never used code, to clean up the
    driver, this code is removed.
    
    Signed-off-by: Christian Lütke-Stetzkamp <christian@lkamp.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/mt7621-mmc/board.h
+++ drivers/staging/mt7621-mmc/board.h
@@ -36,24 +36,12 @@
 #ifndef __ARCH_ARM_MACH_BOARD_H
 #define __ARCH_ARM_MACH_BOARD_H
 
-#include <generated/autoconf.h>
-#include <linux/pm.h>
-/* --- chhung */
-// #include <mach/mt6575.h>
-// #include <board-custom.h>
-/* end of chhung */
-
-typedef void (*sdio_irq_handler_t)(void *);  /* external irq handler */
-typedef void (*pm_callback_t)(pm_message_t state, void *data);
-
 #define MSDC_CD_PIN_EN      (1 << 0)  /* card detection pin is wired   */
 #define MSDC_WP_PIN_EN      (1 << 1)  /* write protection pin is wired */
 #define MSDC_RST_PIN_EN     (1 << 2)  /* emmc reset pin is wired       */
 #define MSDC_SDIO_IRQ       (1 << 3)  /* use internal sdio irq (bus)   */
 #define MSDC_EXT_SDIO_IRQ   (1 << 4)  /* use external sdio irq         */
 #define MSDC_REMOVABLE      (1 << 5)  /* removable slot                */
-#define MSDC_UHS1           (1 << 8)  /* uhs-1 mode support            */
-#define MSDC_DDR            (1 << 9)  /* ddr mode support              */
 
 #define MSDC_SMPL_RISING    (0)
 #define MSDC_SMPL_FALLING   (1)
@@ -63,13 +51,6 @@ typedef void (*pm_callback_t)(pm_message_t state, void *data);
 #define MSDC_CD_PIN         (2)
 #define MSDC_WP_PIN         (3)
 #define MSDC_RST_PIN        (4)
-
-enum {
-	MSDC_CLKSRC_48MHZ = 0,
-//    MSDC_CLKSRC_26MHZ = 0,
-//    MSDC_CLKSRC_197MHZ = 1,
-//    MSDC_CLKSRC_208MHZ = 2
-};
 
 struct msdc_hw {
 	unsigned char  clk_src;          /* host clock source */
@@ -89,46 +70,15 @@ struct msdc_hw {
 	void (*ext_power_off)(void);
 
 	/* external sdio irq operations */
-	void (*request_sdio_eirq)(sdio_irq_handler_t sdio_irq_handler, void *data);
 	void (*enable_sdio_eirq)(void);
 	void (*disable_sdio_eirq)(void);
 
 	/* external cd irq operations */
-	void (*request_cd_eirq)(sdio_irq_handler_t cd_irq_handler, void *data);
 	void (*enable_cd_eirq)(void);
 	void (*disable_cd_eirq)(void);
 	int  (*get_cd_status)(void);
-
-	/* power management callback for external module */
-	void (*register_pm)(pm_callback_t pm_cb, void *data);
 };
 
 extern struct msdc_hw msdc0_hw;
-extern struct msdc_hw msdc1_hw;
-extern struct msdc_hw msdc2_hw;
-extern struct msdc_hw msdc3_hw;
-
-/*GPS driver*/
-#define GPS_FLAG_FORCE_OFF  0x0001
-struct mt3326_gps_hardware {
-	int (*ext_power_on)(int);
-	int (*ext_power_off)(int);
-};
-
-extern struct mt3326_gps_hardware mt3326_gps_hw;
-
-/* NAND driver */
-struct mt6575_nand_host_hw {
-	unsigned int nfi_bus_width;		    /* NFI_BUS_WIDTH */
-	unsigned int nfi_access_timing;		/* NFI_ACCESS_TIMING */
-	unsigned int nfi_cs_num;			/* NFI_CS_NUM */
-	unsigned int nand_sec_size;			/* NAND_SECTOR_SIZE */
-	unsigned int nand_sec_shift;		/* NAND_SECTOR_SHIFT */
-	unsigned int nand_ecc_size;
-	unsigned int nand_ecc_bytes;
-	unsigned int nand_ecc_mode;
-};
-
-extern struct mt6575_nand_host_hw mt6575_nand_hw;
 
 #endif /* __ARCH_ARM_MACH_BOARD_H */
