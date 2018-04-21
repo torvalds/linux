553b0c6416e4426d0c8e552ff809c0a590dc3989
Miquel Raynal <miquel.raynal@bootlin.com>
2018-04-21 20:00:43 +0200

    mtd: rawnand: lpc32xx_slc: fix the probe function error path
    An error after nand_scan_tail() should trigger a nand_cleanup() and not
    a nand_release(). The latter doing an mtd_device_unregister() which is
    not needed if mtd_device_register() failed.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@bootlin.com>

--- drivers/mtd/nand/raw/lpc32xx_slc.c
+++ drivers/mtd/nand/raw/lpc32xx_slc.c
@@ -926,12 +926,12 @@ static int lpc32xx_nand_probe(struct platform_device *pdev)
 	res = mtd_device_register(mtd, host->ncfg->parts,
 				  host->ncfg->num_parts);
 	if (res)
-		goto release_nand;
+		goto cleanup_nand;
 
 	return 0;
 
-release_nand:
-	nand_release(mtd);
+cleanup_nand:
+	nand_cleanup(chip);
 release_dma:
 	dma_release_channel(host->dma_chan);
 unprepare_clk:
