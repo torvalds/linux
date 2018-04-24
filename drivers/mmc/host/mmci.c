99d02d6cd5610711d91f286bb67a57142028e9e6
Thomas Gleixner <tglx@linutronix.de>
2018-04-24 18:40:49 +0200

    mmc: mmci: Remove bogus local_irq_save()
    On !RT interrupt runs with interrupts disabled. On RT it's in a
    thread, so no need to disable interrupts at all.
    Remove the local_irq_save() invocation.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/mmci.c
+++ drivers/mmc/host/mmci.c
@@ -1253,14 +1253,11 @@ static irqreturn_t mmci_pio_irq(int irq, void *dev_id)
 	struct sg_mapping_iter *sg_miter = &host->sg_miter;
 	struct variant_data *variant = host->variant;
 	void __iomem *base = host->base;
-	unsigned long flags;
 	u32 status;
 
 	status = readl(base + MMCISTATUS);
 
 	dev_dbg(mmc_dev(host->mmc), "irq1 (pio) %08x\n", status);
-
-	local_irq_save(flags);
 
 	do {
 		unsigned int remain, len;
@@ -1300,8 +1297,6 @@ static irqreturn_t mmci_pio_irq(int irq, void *dev_id)
 	} while (1);
 
 	sg_miter_stop(sg_miter);
-
-	local_irq_restore(flags);
 
 	/*
 	 * If we have less than the fifo 'half-full' threshold to transfer,
