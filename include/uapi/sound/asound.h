a4e7dd35b9dac21fa7b33e8788b51c7fbc7a49f1
Baolin Wang <baolin.wang@linaro.org>
2018-04-24 20:06:10 +0800

    ALSA: Avoid using timespec for struct snd_ctl_elem_value
    The struct snd_ctl_elem_value will use 'timespec' type variables to record
    timestamp, which is not year 2038 safe on 32bits system.
    
    Since there are no drivers will implemented the tstamp member of the
    struct snd_ctl_elem_value, and also the stucture size will not be changed
    if we change timespec to s64 for tstamp member of struct snd_ctl_elem_value.
    
    From Takashi's comments, "In the library, applications are not expected
    to access to this structure directly. The applications get opaque pointer
    to the structure and must use any control APIs to operate it. Actually the
    library produce no API to handle 'struct snd_ctl_elem_value.tstamp'. This
    means that we can drop this member from alsa-lib without decline of
    functionality." Thus we can simply remove the tstamp member to avoid using
    the type which is not year 2038 safe on 32bits system.
    
    Signed-off-by: Baolin Wang <baolin.wang@linaro.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/uapi/sound/asound.h
+++ include/uapi/sound/asound.h
@@ -957,8 +957,7 @@ struct snd_ctl_elem_value {
 		} bytes;
 		struct snd_aes_iec958 iec958;
 	} value;		/* RO */
-	struct timespec tstamp;
-	unsigned char reserved[128-sizeof(struct timespec)];
+	unsigned char reserved[128];
 };
 
 struct snd_ctl_tlv {
