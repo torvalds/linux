60d4125d79c3bc41c123fd0a333d2eb159c8b141
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2018-04-25 16:01:48 +0200

    staging: ks7010: align values in frequency_list array
    To avoid a long line align values in static array
    frequency_list array.
    
    Signed-off-by: Sergio Paracuellos <sergio.paracuellos@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/ks_wlan_net.c
+++ drivers/staging/ks7010/ks_wlan_net.c
@@ -31,7 +31,8 @@ static int wep_on_off;
 #include <net/iw_handler.h>	/* New driver API */
 
 /* Frequency list (map channels to frequencies) */
-static const long frequency_list[] = { 2412, 2417, 2422, 2427, 2432, 2437, 2442,
+static const long frequency_list[] = {
+	2412, 2417, 2422, 2427, 2432, 2437, 2442,
 	2447, 2452, 2457, 2462, 2467, 2472, 2484
 };
 
