a657b85fc1e87009b7d0928358928d7c06f43d66
Hu Kejun <william.hu@rock-chips.com>
2018-04-25 16:40:39 +0800

    media: i2c: ov5695: initialize sensor driver after io_domain
    Change-Id: Ia0d754d941ba9bda874efe787a985f00cf1be779
    Signed-off-by: Hu Kejun <william.hu@rock-chips.com>

--- drivers/media/i2c/ov5695.c
+++ drivers/media/i2c/ov5695.c
@@ -1412,7 +1412,18 @@ static struct i2c_driver ov5695_i2c_driver = {
 	.id_table	= ov5695_match_id,
 };
 
-module_i2c_driver(ov5695_i2c_driver);
+static int __init sensor_mod_init(void)
+{
+	return i2c_add_driver(&ov5695_i2c_driver);
+}
+
+static void __exit sensor_mod_exit(void)
+{
+	i2c_del_driver(&ov5695_i2c_driver);
+}
+
+device_initcall_sync(sensor_mod_init);
+module_exit(sensor_mod_exit);
 
 MODULE_DESCRIPTION("OmniVision ov5695 sensor driver");
 MODULE_LICENSE("GPL v2");
