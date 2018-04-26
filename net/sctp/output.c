22d7be267eaa8114dcc28d66c1c347f667d7878a
Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
2018-04-26 16:58:58 -0300

    sctp: remove sctp_transport_pmtu_check
    We are now keeping the MTU information synced between asoc, transport
    and dst, which makes the check at sctp_packet_config() not needed
    anymore. As it was the sole caller to this function, lets remove it.
    
    Signed-off-by: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sctp/output.c
+++ net/sctp/output.c
@@ -118,9 +118,6 @@ void sctp_packet_config(struct sctp_packet *packet, __u32 vtag,
 		sctp_transport_route(tp, NULL, sp);
 		if (asoc->param_flags & SPP_PMTUD_ENABLE)
 			sctp_assoc_sync_pmtu(asoc);
-	} else if (!sctp_transport_pmtu_check(tp)) {
-		if (asoc->param_flags & SPP_PMTUD_ENABLE)
-			sctp_assoc_sync_pmtu(asoc);
 	}
 
 	/* If there a is a prepend chunk stick it on the list before
