9e8d438e8ba43a38def2890a65a26917f2233a83
Florian Fainelli <f.fainelli@gmail.com>
2018-04-27 12:41:49 -0700

    net: phy: Fix modular PHYLIB build
    After commit c59530d0d5dc ("net: Move PHY statistics code into PHY
    library helpers") we made net/core/ethtool.c reference symbols which are
    part of the library which can be modular. David introduced a temporary
    fix with 1ecd6e8ad996 ("phy: Temporary build fix after phylib changes.")
    which would prevent such modularity.
    
    This is not desireable of course, so instead, just inline the functions
    into include/linux/phy.h to keep both options available.
    
    Fixes: c59530d0d5dc ("net: Move PHY statistics code into PHY library helpers")
    Fixes: 1ecd6e8ad996 ("phy: Temporary build fix after phylib changes.")
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -1066,27 +1066,53 @@ int phy_ethtool_nway_reset(struct net_device *ndev);
 #if IS_ENABLED(CONFIG_PHYLIB)
 int __init mdio_bus_init(void);
 void mdio_bus_exit(void);
-int phy_ethtool_get_strings(struct phy_device *phydev, u8 *data);
-int phy_ethtool_get_sset_count(struct phy_device *phydev);
-int phy_ethtool_get_stats(struct phy_device *phydev,
-			  struct ethtool_stats *stats, u64 *data);
-#else
-int phy_ethtool_get_strings(struct phy_device *phydev, u8 *data)
-{
-	return -EOPNOTSUPP;
-}
-
-int phy_ethtool_get_sset_count(struct phy_device *phydev)
-{
-	return -EOPNOTSUPP;
-}
-
-int phy_ethtool_get_stats(struct phy_device *phydev,
-			  struct ethtool_stats *stats, u64 *data)
-{
-	return -EOPNOTSUPP;
-}
 #endif
+
+/* Inline function for use within net/core/ethtool.c (built-in) */
+static inline int phy_ethtool_get_strings(struct phy_device *phydev, u8 *data)
+{
+	if (!phydev->drv)
+		return -EIO;
+
+	mutex_lock(&phydev->lock);
+	phydev->drv->get_strings(phydev, data);
+	mutex_unlock(&phydev->lock);
+
+	return 0;
+}
+
+static inline int phy_ethtool_get_sset_count(struct phy_device *phydev)
+{
+	int ret;
+
+	if (!phydev->drv)
+		return -EIO;
+
+	if (phydev->drv->get_sset_count &&
+	    phydev->drv->get_strings &&
+	    phydev->drv->get_stats) {
+		mutex_lock(&phydev->lock);
+		ret = phydev->drv->get_sset_count(phydev);
+		mutex_unlock(&phydev->lock);
+
+		return ret;
+	}
+
+	return -EOPNOTSUPP;
+}
+
+static inline int phy_ethtool_get_stats(struct phy_device *phydev,
+					struct ethtool_stats *stats, u64 *data)
+{
+	if (!phydev->drv)
+		return -EIO;
+
+	mutex_lock(&phydev->lock);
+	phydev->drv->get_stats(phydev, stats, data);
+	mutex_unlock(&phydev->lock);
+
+	return 0;
+}
 
 extern struct bus_type mdio_bus_type;
 
