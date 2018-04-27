a2a330ad66313084c9432b32862aa7e1255da9b4
Shirish S <shirish.s@amd.com>
2018-04-27 15:47:21 +0530

    drm/amd/display: remove need of modeset flag for overlay planes (V2)
    This patch is in continuation to the
    "843e3c7 drm/amd/display: defer modeset check in dm_update_planes_state"
    where we started to eliminate the dependency on
    DRM_MODE_ATOMIC_ALLOW_MODESET to be set by the user space,
    which as such is not mandatory.
    
    After deferring, this patch eliminates the dependency on the flag
    for overlay planes.
    
    This has to be done in stages as its a pretty complex and requires thorough
    testing before we free primary planes as well from dependency on modeset
    flag.
    
    V2: Simplified the plane type check.
    
    Signed-off-by: Shirish S <shirish.s@amd.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -4104,7 +4104,7 @@ static void amdgpu_dm_commit_planes(struct drm_atomic_state *state,
 		}
 		spin_unlock_irqrestore(&crtc->dev->event_lock, flags);
 
-		if (!pflip_needed) {
+		if (!pflip_needed || plane->type == DRM_PLANE_TYPE_OVERLAY) {
 			WARN_ON(!dm_new_plane_state->dc_state);
 
 			plane_states_constructed[planes_count] = dm_new_plane_state->dc_state;
@@ -4827,7 +4827,8 @@ static int dm_update_planes_state(struct dc *dc,
 
 		/* Remove any changed/removed planes */
 		if (!enable) {
-			if (pflip_needed)
+			if (pflip_needed &&
+			    plane->type != DRM_PLANE_TYPE_OVERLAY)
 				continue;
 
 			if (!old_plane_crtc)
@@ -4874,7 +4875,8 @@ static int dm_update_planes_state(struct dc *dc,
 			if (!dm_new_crtc_state->stream)
 				continue;
 
-			if (pflip_needed)
+			if (pflip_needed &&
+			    plane->type != DRM_PLANE_TYPE_OVERLAY)
 				continue;
 
 			WARN_ON(dm_new_plane_state->dc_state);
