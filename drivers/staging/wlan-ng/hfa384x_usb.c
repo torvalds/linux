d4b9a864dea322088535300c12c09cd654d3a1fc
Tim Collier <osdevtc@gmail.com>
2018-05-02 20:30:50 +0100

    staging: wlan-ng: fix line-break style issue
    Fix checkpatch warning due to line break after '(', leaving an over 80
    character warning due to long macro name.
    
    Signed-off-by: Tim Collier <osdevtc@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/hfa384x_usb.c
+++ drivers/staging/wlan-ng/hfa384x_usb.c
@@ -3439,8 +3439,7 @@ static void hfa384x_usbin_rx(struct wlandevice *wlandev, struct sk_buff *skb)
 
 	default:
 		netdev_warn(hw->wlandev->netdev, "Received frame on unsupported port=%d\n",
-			    HFA384x_RXSTATUS_MACPORT_GET(
-				    usbin->rxfrm.desc.status));
+			    HFA384x_RXSTATUS_MACPORT_GET(usbin->rxfrm.desc.status));
 		break;
 	}
 }
