7af76c5f23abc7afedf449e7d2960f463cbc4097
Michael Ellerman <mpe@ellerman.id.au>
2018-05-02 23:07:29 +1000

    powerpc/stacktrace: Update copyright
    This now has new code in it written by Nick and I, and switch to a
    SPDX tag.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Reviewed-by: Nicholas Piggin <npiggin@gmail.com>

--- arch/powerpc/kernel/stacktrace.c
+++ arch/powerpc/kernel/stacktrace.c
@@ -1,13 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0
+
 /*
- * Stack trace utility
+ * Stack trace utility functions etc.
  *
  * Copyright 2008 Christoph Hellwig, IBM Corp.
  * Copyright 2018 SUSE Linux GmbH
- *
- *      This program is free software; you can redistribute it and/or
- *      modify it under the terms of the GNU General Public License
- *      as published by the Free Software Foundation; either version
- *      2 of the License, or (at your option) any later version.
+ * Copyright 2018 Nick Piggin, Michael Ellerman, IBM Corp.
  */
 
 #include <linux/export.h>
