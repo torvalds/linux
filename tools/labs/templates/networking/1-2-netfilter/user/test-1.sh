38d6d01c9727c149847c93211d6ed7b6015b3b02
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2018-05-03 02:08:19 +0300

    tools: labs: Update setup for QEMU image and Linux kernel 4.15
    Signed-off-by: Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>

--- tools/labs/templates/networking/1-2-netfilter/user/test-1.sh
+++ tools/labs/templates/networking/1-2-netfilter/user/test-1.sh
@@ -6,17 +6,17 @@
 #
 
 # insert module
-insmod filter.ko || exit 1
+insmod ../kernel/filter.ko || exit 1
 
 # listen for connections on localhost, port 60000 (run in background)
-nc -l -p 60000 &
+../../netcat -l -p 60000 &
 
 # wait for netcat to start listening
 sleep 1
 
 # connect to localhost, port 60000, starting a connection using local
 # port number 600001;
-echo "Should show up in filter." | nc 127.0.0.1 60000
+echo "Should show up in filter." | ../../netcat -q 2 127.0.0.1 60000
 
 # look for filter message in dmesg output
 echo "Check dmesg output."
