855978bbbeeda4ccacf4834e4bfea3e2a0b25ea4
Ajay Singh <ajay.kathat@microchip.com>
2018-05-02 12:43:30 +0530

    staging: wilc1000: remove unnecessary bracket used in switch in wilc_mgmt_frame_register()
    Cleanup patch to remove the curly braces used in 'case' statement to follow as
    per linux standard.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wfi_cfgoperations.c
+++ drivers/staging/wilc1000/wilc_wfi_cfgoperations.c
@@ -1741,23 +1741,17 @@ void wilc_mgmt_frame_register(struct wiphy *wiphy, struct wireless_dev *wdev,
 
 	switch (frame_type) {
 	case PROBE_REQ:
-	{
 		vif->frame_reg[0].type = frame_type;
 		vif->frame_reg[0].reg = reg;
-	}
-	break;
+		break;
 
 	case ACTION:
-	{
 		vif->frame_reg[1].type = frame_type;
 		vif->frame_reg[1].reg = reg;
-	}
-	break;
+		break;
 
 	default:
-	{
 		break;
-	}
 	}
 
 	if (!wl->initialized)
