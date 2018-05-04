b26862450d74b2a17e22c5d7cdec1707d03b82b6
Shuah Khan (Samsung OSG) <shuah@kernel.org>
2018-05-04 10:11:52 -0600

    selftests: lib: return Kselftest Skip code for skipped tests
    When lib test(s) is skipped because of unmet dependencies and/or
    unsupported configuration, it returns non-zero value hich is treated
    as a fail by the Kselftest framework. This leads to false negative result
    even when the test could not be run.
    
    Change it to return kselftest skip code when a test gets skipped to
    clearly report that the test could not be run.
    
    Kselftest framework SKIP code is 4 and the framework prints appropriate
    messages to indicate that the test is skipped.
    
    Signed-off-by: Shuah Khan (Samsung OSG) <shuah@kernel.org>

--- tools/testing/selftests/lib/prime_numbers.sh
+++ tools/testing/selftests/lib/prime_numbers.sh
@@ -2,9 +2,12 @@
 # SPDX-License-Identifier: GPL-2.0
 # Checks fast/slow prime_number generation for inconsistencies
 
+# Kselftest framework requirement - SKIP code is 4.
+ksft_skip=4
+
 if ! /sbin/modprobe -q -r prime_numbers; then
-	echo "prime_numbers: [SKIP]"
-	exit 77
+	echo "prime_numbers: module prime_numbers is not found [SKIP]"
+	exit $ksft_skip
 fi
 
 if /sbin/modprobe -q prime_numbers selftest=65536; then
