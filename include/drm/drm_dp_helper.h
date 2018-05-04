2f065d8ae918159791474049ab67a0cb85723b81
Matt Atwood <matthew.s.atwood@intel.com>
2018-05-04 15:18:00 -0700

    drm/dp: Correctly mask DP_TRAINING_AUX_RD_INTERVAL values for DP 1.4
    DP_TRAINING_AUX_RD_INTERVAL with DP 1.3 spec changed bit scheeme from 8
    bits to 7 in DPCD 0x000e. The 8th bit is used to identify extended
    receiver capabilities. For panels that use this new feature wait interval
    would be increased by 512 ms, when spec is max 16 ms. This behavior is
    described in table 2-158 of DP 1.4 spec address 0000eh.
    
    With the introduction of DP 1.4 spec main link clock recovery was
    standardized to 100 us regardless of TRAINING_AUX_RD_INTERVAL value.
    
    To avoid breaking panels that are not spec compiant we now warn on
    invalid values.
    
    V2: commit title/message, masking all 7 bits, warn on out of spec values.
    V3: commit message, make link train clock recovery follow DP 1.4 spec.
    V4: style changes
    V5: typo
    V6: print statement revisions, DP_REV to DPCD_REV, comment correction
    V7: typo
    V8: Style
    V9: Strip out DPCD_REV_XX into seperate patch
    v10: DPCD_REV_XX to DP_DPCD_REV_XX
    
    Signed-off-by: Matt Atwood <matthew.s.atwood@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180504221800.17830-2-matthew.s.atwood@intel.com

--- include/drm/drm_dp_helper.h
+++ include/drm/drm_dp_helper.h
@@ -124,6 +124,7 @@
 # define DP_DPCD_DISPLAY_CONTROL_CAPABLE     (1 << 3) /* edp v1.2 or higher */
 
 #define DP_TRAINING_AUX_RD_INTERVAL         0x00e   /* XXX 1.2? */
+# define DP_TRAINING_AUX_RD_MASK            0x7F    /* XXX 1.2? */
 
 #define DP_ADAPTER_CAP			    0x00f   /* 1.2 */
 # define DP_FORCE_LOAD_SENSE_CAP	    (1 << 0)
