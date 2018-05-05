dec60f3a9b7251f2657d743d96ba9a83dca02351
Mathieu Malaterre <malat@debian.org>
2018-05-05 21:54:05 +0200

    agp: uninorth: make two functions static
    Both ‘uninorth_remove_memory’ and ‘null_cache_flush’ can be made
    static. So make them.
    
    Silence the following gcc warning (W=1):
    
      drivers/char/agp/uninorth-agp.c:198:5: warning: no previous prototype for ‘uninorth_remove_memory’ [-Wmissing-prototypes]
    
    and
    
      drivers/char/agp/uninorth-agp.c:473:6: warning: no previous prototype for ‘null_cache_flush’ [-Wmissing-prototypes]
    
    Signed-off-by: Mathieu Malaterre <malat@debian.org>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/char/agp/uninorth-agp.c
+++ drivers/char/agp/uninorth-agp.c
@@ -195,7 +195,7 @@ static int uninorth_insert_memory(struct agp_memory *mem, off_t pg_start, int ty
 	return 0;
 }
 
-int uninorth_remove_memory(struct agp_memory *mem, off_t pg_start, int type)
+static int uninorth_remove_memory(struct agp_memory *mem, off_t pg_start, int type)
 {
 	size_t i;
 	u32 *gp;
@@ -470,7 +470,7 @@ static int uninorth_free_gatt_table(struct agp_bridge_data *bridge)
 	return 0;
 }
 
-void null_cache_flush(void)
+static void null_cache_flush(void)
 {
 	mb();
 }
