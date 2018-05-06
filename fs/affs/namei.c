87fbd639c02ec96d67738e40b6521fb070ed7168
Al Viro <viro@zeniv.linux.org.uk>
2018-05-06 12:20:40 -0400

    affs_lookup: switch to d_splice_alias()
    Making something exportable takes more than providing ->s_export_ops.
    In particular, ->lookup() *MUST* use d_splice_alias() instead of
    d_add().
    
    Reading Documentation/filesystems/nfs/Exporting would've been a good idea;
    as it is, exporting AFFS is badly (and exploitably) broken.
    
    Partially-Fixes: ed4433d72394 "fs/affs: make affs exportable"
    Acked-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/affs/namei.c
+++ fs/affs/namei.c
@@ -201,6 +201,7 @@ affs_lookup(struct inode *dir, struct dentry *dentry, unsigned int flags)
 	struct super_block *sb = dir->i_sb;
 	struct buffer_head *bh;
 	struct inode *inode = NULL;
+	struct dentry *res;
 
 	pr_debug("%s(\"%pd\")\n", __func__, dentry);
 
@@ -223,14 +224,12 @@ affs_lookup(struct inode *dir, struct dentry *dentry, unsigned int flags)
 		}
 		affs_brelse(bh);
 		inode = affs_iget(sb, ino);
-		if (IS_ERR(inode)) {
-			affs_unlock_dir(dir);
-			return ERR_CAST(inode);
-		}
 	}
-	d_add(dentry, inode);
+	res = d_splice_alias(inode, dentry);
+	if (!IS_ERR_OR_NULL(res))
+		res->d_fsdata = dentry->d_fsdata;
 	affs_unlock_dir(dir);
-	return NULL;
+	return res;
 }
 
 int
