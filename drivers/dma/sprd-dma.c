e891e41ee301a57fc74a4a0d4da60fdc9669e50c
Wei Yongjun <weiyongjun1@huawei.com>
2018-05-07 01:40:34 +0000

    dmaengine: sprd: Fix potential NULL dereference in sprd_dma_probe()
    platform_get_resource() may fail and return NULL, so we should
    better check it's return value to avoid a NULL pointer dereference
    a bit later in the code.
    
    This is detected by Coccinelle semantic patch.
    
    @@
    expression pdev, res, n, t, e, e1, e2;
    @@
    
    res = platform_get_resource(pdev, t, n);
    + if (!res)
    +   return -EINVAL;
    ... when != res == NULL
    e = devm_ioremap_nocache(e1, res->start, e2);
    
    Fixes: 9b3b8171f7f4 ("dmaengine: sprd: Add Spreadtrum DMA driver")
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Reviewed-by: Baolin Wang <baolin.wang@linaro.org>
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/sprd-dma.c
+++ drivers/dma/sprd-dma.c
@@ -807,6 +807,8 @@ static int sprd_dma_probe(struct platform_device *pdev)
 	}
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (!res)
+		return -EINVAL;
 	sdev->glb_base = devm_ioremap_nocache(&pdev->dev, res->start,
 					      resource_size(res));
 	if (!sdev->glb_base)
