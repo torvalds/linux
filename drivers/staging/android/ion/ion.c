8a95c13224f9be12a71794e17862bb1410334108
Nathan Chancellor <natechancellor@gmail.com>
2018-05-06 18:18:54 -0700

    staging: android: ion: Remove unnecessary blank line
    Fixes a checkpatch.pl warning.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Acked-by: Laura Abbott <labbott@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -95,7 +95,6 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 		goto err1;
 	}
 
-
 	INIT_LIST_HEAD(&buffer->attachments);
 	mutex_init(&buffer->lock);
 	mutex_lock(&dev->buffer_lock);
