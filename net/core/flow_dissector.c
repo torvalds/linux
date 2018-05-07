d869dea664e662a4380ccf59cb9be9888a7af53a
Paolo Abeni <pabeni@redhat.com>
2018-05-07 12:06:03 +0200

    flow_dissector: do not rely on implicit casts
    This change fixes a couple of type mismatch reported by the sparse
    tool, explicitly using the requested type for the offending arguments.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Acked-by: Jon Maloy <jon.maloy@ericsson.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/flow_dissector.c
+++ net/core/flow_dissector.c
@@ -1316,7 +1316,7 @@ u32 skb_get_poff(const struct sk_buff *skb)
 {
 	struct flow_keys_basic keys;
 
-	if (!skb_flow_dissect_flow_keys_basic(skb, &keys, 0, 0, 0, 0, 0))
+	if (!skb_flow_dissect_flow_keys_basic(skb, &keys, NULL, 0, 0, 0, 0))
 		return 0;
 
 	return __skb_get_poff(skb, skb->data, &keys, skb_headlen(skb));
