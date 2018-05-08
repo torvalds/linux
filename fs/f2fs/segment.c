e72a2cca82d8e8809be75012ded23781434d31fa
Sheng Yong <shengyong1@huawei.com>
2018-05-08 17:51:34 +0800

    f2fs: clear discard_wake earlier
    If SBI_NEED_FSCK is set, discard_wake will never be cleared. As a
    result, the condition of wait_event_interruptible_timeout() is always
    true, which gets discard thread run too frequently.
    
    Signed-off-by: Sheng Yong <shengyong1@huawei.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -1482,6 +1482,10 @@ static int issue_discard_thread(void *data)
 				kthread_should_stop() || freezing(current) ||
 				dcc->discard_wake,
 				msecs_to_jiffies(wait_ms));
+
+		if (dcc->discard_wake)
+			dcc->discard_wake = 0;
+
 		if (try_to_freeze())
 			continue;
 		if (f2fs_readonly(sbi->sb))
@@ -1492,9 +1496,6 @@ static int issue_discard_thread(void *data)
 			wait_ms = dpolicy.max_interval;
 			continue;
 		}
-
-		if (dcc->discard_wake)
-			dcc->discard_wake = 0;
 
 		if (sbi->gc_mode == GC_URGENT)
 			__init_discard_policy(sbi, &dpolicy, DPOLICY_FORCE, 1);
