36780d7eee827047bd9e736f6e2c0be650f30b4e
Ben Skeggs <bskeggs@redhat.com>
2018-05-08 20:39:46 +1000

    drm/nouveau/fault: add infrastructure to support fault buffers
    GPU-specific support will be added separately.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/subdev/fault.h
+++ drivers/gpu/drm/nouveau/include/nvkm/subdev/fault.h
@@ -3,6 +3,12 @@
 #include <core/subdev.h>
 
 struct nvkm_fault {
+	const struct nvkm_fault_func *func;
 	struct nvkm_subdev subdev;
+
+	struct nvkm_fault_buffer *buffer[1];
+	int buffer_nr;
+
+	struct nvkm_event event;
 };
 #endif
