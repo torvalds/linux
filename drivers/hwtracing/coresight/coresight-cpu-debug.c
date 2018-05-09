40fe78a242dd39f36e51907129831500f90d7d5b
Leo Yan <leo.yan@linaro.org>
2018-05-09 12:06:08 -0600

    coresight: Remove %px for printing pcsr value
    The driver prints pcsr twice: the first time it uses specifier %px to
    print hexadecimal pcsr value and the second time uses specifier %pS for
    output kernel symbols.
    
    As suggested by Kees, using %pS should be sufficient and %px isn't
    necessary; the reason is if the pcsr is a kernel space address, we can
    easily get to know the code line from %pS format, on the other hand, if
    the pcsr value doesn't fall into kernel space range (e.g. if the CPU is
    stuck in firmware), %pS also gives out pcsr hexadecimal value.
    
    So this commit removes useless %px and update section "Output format"
    in the document for alignment between the code and document.
    
    Suggested-by: Kees Cook <keescook@chromium.org>
    Cc: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Leo Yan <leo.yan@linaro.org>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hwtracing/coresight/coresight-cpu-debug.c
+++ drivers/hwtracing/coresight/coresight-cpu-debug.c
@@ -303,7 +303,7 @@ static void debug_dump_regs(struct debug_drvdata *drvdata)
 	}
 
 	pc = debug_adjust_pc(drvdata);
-	dev_emerg(dev, " EDPCSR:  [<%px>] %pS\n", (void *)pc, (void *)pc);
+	dev_emerg(dev, " EDPCSR:  %pS\n", (void *)pc);
 
 	if (drvdata->edcidsr_present)
 		dev_emerg(dev, " EDCIDSR: %08x\n", drvdata->edcidsr);
