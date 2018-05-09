418ecc848d789528c084edaacdc338fbeeff895d
Joseph Chen <chenjh@rock-chips.com>
2018-05-09 16:11:57 +0800

    soc: rockchip: support rk3308 pm config
    Change-Id: Icf51062e9c1ce27244c8a3744b61a5baab7e7024
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- drivers/soc/rockchip/rockchip_pm_config.c
+++ drivers/soc/rockchip/rockchip_pm_config.c
@@ -24,6 +24,7 @@ static const struct of_device_id pm_match_table[] = {
 	{ .compatible = "rockchip,pm-px30",},
 	{ .compatible = "rockchip,pm-rk322x",},
 	{ .compatible = "rockchip,pm-rk3288",},
+	{ .compatible = "rockchip,pm-rk3308",},
 	{ .compatible = "rockchip,pm-rk3328",},
 	{ .compatible = "rockchip,pm-rk3368",},
 	{ .compatible = "rockchip,pm-rk3399",},
