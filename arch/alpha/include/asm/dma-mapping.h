6db615431a21b6057f68ed87583a663ee69f7601
Christoph Hellwig <hch@lst.de>
2018-05-09 16:04:51 +0200

    alpha: use dma_direct_ops for jensen
    The generic dma_direct implementation does the same thing as the alpha
    pci-noop implementation, just with more bells and whistles.  And unlike
    the current code it at least has a theoretical chance to actually compile.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Matt Turner <mattst88@gmail.com>

--- arch/alpha/include/asm/dma-mapping.h
+++ arch/alpha/include/asm/dma-mapping.h
@@ -6,7 +6,11 @@ extern const struct dma_map_ops *dma_ops;
 
 static inline const struct dma_map_ops *get_arch_dma_ops(struct bus_type *bus)
 {
+#ifdef CONFIG_ALPHA_JENSEN
+	return &dma_direct_ops;
+#else
 	return dma_ops;
+#endif
 }
 
 #endif	/* _ALPHA_DMA_MAPPING_H */
