a4333c3a6ba9ca9cff50a3c1d1bf193dc5489e1c
Manu Gautam <mgautam@codeaurora.org>
2018-05-09 23:09:20 +0530

    usb: dwc3: Add Qualcomm DWC3 glue driver
    DWC3 controller on Qualcomm SOCs has a Qscratch wrapper.
    Some of its uses are described below resulting in need to
    have a separate glue driver instead of using dwc3-of-simple:
     - It exposes register interface to override vbus-override
       and lane0-pwr-present signals going to hardware. These
       must be updated in peripheral mode for DWC3 if vbus lines
       are not connected to hardware block. Otherwise RX termination
       in SS mode or DP pull-up is not applied by device controller.
     - pwr_events_irq_stat support to check if USB2 PHY is in L2 state
       before glue driver proceeds with suspend.
     - Support for wakeup interrupts lines that are asserted whenever
       there is any wakeup event on USB3 or USB2 bus.
     - Support to replace pip3 clock going to DWC3 with utmi clock
       for hardware configuration where SSPHY is not used with DWC3.
    
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/dwc3/dwc3-of-simple.c
+++ drivers/usb/dwc3/dwc3-of-simple.c
@@ -208,7 +208,6 @@ static const struct dev_pm_ops dwc3_of_simple_dev_pm_ops = {
 };
 
 static const struct of_device_id of_dwc3_simple_match[] = {
-	{ .compatible = "qcom,dwc3" },
 	{ .compatible = "rockchip,rk3399-dwc3" },
 	{ .compatible = "xlnx,zynqmp-dwc3" },
 	{ .compatible = "cavium,octeon-7130-usb-uctl" },
