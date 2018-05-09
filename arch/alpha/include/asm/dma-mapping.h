f5e82fa26063e6fad10624ff600457d878fa6e41
Christoph Hellwig <hch@lst.de>
2018-05-09 16:04:52 +0200

    alpha: simplify get_arch_dma_ops
    Remove the dma_ops indirection.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Matt Turner <mattst88@gmail.com>

--- arch/alpha/include/asm/dma-mapping.h
+++ arch/alpha/include/asm/dma-mapping.h
@@ -2,14 +2,14 @@
 #ifndef _ALPHA_DMA_MAPPING_H
 #define _ALPHA_DMA_MAPPING_H
 
-extern const struct dma_map_ops *dma_ops;
+extern const struct dma_map_ops alpha_pci_ops;
 
 static inline const struct dma_map_ops *get_arch_dma_ops(struct bus_type *bus)
 {
 #ifdef CONFIG_ALPHA_JENSEN
 	return &dma_direct_ops;
 #else
-	return dma_ops;
+	return &alpha_pci_ops;
 #endif
 }
 
