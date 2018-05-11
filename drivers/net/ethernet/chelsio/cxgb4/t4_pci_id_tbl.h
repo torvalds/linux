f4a313b9713933af5c7928dbb6bedc8077ade572
Ganesh Goudar <ganeshgr@chelsio.com>
2018-05-11 18:37:34 +0530

    cxgb4: Add new T5 device id
    Add 0x50ad device id for new T5 card.
    
    Signed-off-by: Ganesh Goudar <ganeshgr@chelsio.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/chelsio/cxgb4/t4_pci_id_tbl.h
+++ drivers/net/ethernet/chelsio/cxgb4/t4_pci_id_tbl.h
@@ -187,6 +187,7 @@ CH_PCI_DEVICE_ID_TABLE_DEFINE_BEGIN
 	CH_PCI_ID_TABLE_FENTRY(0x50aa), /* Custom T580-CR */
 	CH_PCI_ID_TABLE_FENTRY(0x50ab), /* Custom T520-CR */
 	CH_PCI_ID_TABLE_FENTRY(0x50ac), /* Custom T540-BT */
+	CH_PCI_ID_TABLE_FENTRY(0x50ad), /* Custom T520-CR */
 
 	/* T6 adapters:
 	 */
