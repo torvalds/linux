0c5dfa51caf77461da90b3af1f1ec28490255bc0
Sekhar Nori <nsekhar@ti.com>
2018-05-11 20:51:36 +0530

    ARM: davinci: board-dm646x-evm: set VPIF capture card name
    [ Upstream commit bb7298a7e87cf3430eb62be8746e5d7a07ca9d7c ]
    
    VPIF capture driver expects card name to be set since it
    uses it without checking for NULL. The commit which
    introduced VPIF display and capture support added card
    name only for display, not for capture.
    
    Set it in platform data to probe driver successfully.
    
    While at it, also fix the display card name to something more
    appropriate.
    
    Fixes: 85609c1ccda6 ("DaVinci: DM646x - platform changes for vpif capture and display drivers")
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm/mach-davinci/board-dm646x-evm.c
+++ arch/arm/mach-davinci/board-dm646x-evm.c
@@ -539,7 +539,7 @@ static struct vpif_display_config dm646x_vpif_display_config = {
 		.outputs = dm6467_ch0_outputs,
 		.output_count = ARRAY_SIZE(dm6467_ch0_outputs),
 	},
-	.card_name	= "DM646x EVM",
+	.card_name	= "DM646x EVM Video Display",
 };
 
 /**
@@ -698,6 +698,7 @@ static struct vpif_capture_config dm646x_vpif_capture_cfg = {
 			.fid_pol = 0,
 		},
 	},
+	.card_name = "DM646x EVM Video Capture",
 };
 
 static void __init evm_init_video(void)
