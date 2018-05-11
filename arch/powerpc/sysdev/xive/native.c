8a792262f320245de0174e6bcb551312f2e2debe
Russell Currey <ruscur@russell.cc>
2018-05-11 18:03:13 +1000

    powerpc/xive: Remove (almost) unused macros
    The GETFIELD and SETFIELD macros in xive-regs.h aren't used except for
    a single instance of GETFIELD, so replace that and remove them.
    
    These macros are also defined in vas.h, so either those should be
    eventually replaced or the macros moved into bitops.h.
    
    Signed-off-by: Russell Currey <ruscur@russell.cc>
    [mpe: Rewrite the assignment to 'he' to avoid ffs() etc.]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/sysdev/xive/native.c
+++ arch/powerpc/sysdev/xive/native.c
@@ -341,7 +341,7 @@ static void xive_native_update_pending(struct xive_cpu *xc)
 	 * of the hypervisor interrupt (if any)
 	 */
 	cppr = ack & 0xff;
-	he = GETFIELD(TM_QW3_NSR_HE, (ack >> 8));
+	he = (ack >> 8) >> 6;
 	switch(he) {
 	case TM_QW3_NSR_HE_NONE: /* Nothing to see here */
 		break;
