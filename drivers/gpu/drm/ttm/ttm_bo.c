967c650d4973dd87355c8540a18e6fa58844e547
Junwei Zhang <Jerry.Zhang@amd.com>
2018-05-11 10:54:40 +0800

    drm/ttm: remove priority hard code when initializing ttm bo
    Then priority could be set before initialization.
    By default, it requires to kzalloc ttm bo. In fact, we always do so.
    
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: David Zhou <david1.zhou@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/ttm/ttm_bo.c
+++ drivers/gpu/drm/ttm/ttm_bo.c
@@ -1175,7 +1175,6 @@ int ttm_bo_init_reserved(struct ttm_bo_device *bdev,
 	reservation_object_init(&bo->ttm_resv);
 	atomic_inc(&bo->bdev->glob->bo_count);
 	drm_vma_node_reset(&bo->vma_node);
-	bo->priority = 0;
 
 	/*
 	 * For ttm_bo_type_device buffers, allocate
