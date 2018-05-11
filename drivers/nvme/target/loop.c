eb464833a2e787996474ad33dafa2c5336d4c477
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2018-05-11 02:38:15 -0400

    nvmet-loop: use nr_phys_segments when map rq to sgl
    Use blk_rq_nr_phys_segments() instead of blk_rq_payload_bytes() to check
    if a command contains data to me mapped.  This fixes the case where
    a struct requests contains LBAs, but no data will actually be send,
    e.g. the pending Write Zeroes support.
    
    Signed-off-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/nvme/target/loop.c
+++ drivers/nvme/target/loop.c
@@ -174,7 +174,7 @@ static blk_status_t nvme_loop_queue_rq(struct blk_mq_hw_ctx *hctx,
 			&queue->nvme_sq, &nvme_loop_ops))
 		return BLK_STS_OK;
 
-	if (blk_rq_payload_bytes(req)) {
+	if (blk_rq_nr_phys_segments(req)) {
 		iod->sg_table.sgl = iod->first_sgl;
 		if (sg_alloc_table_chained(&iod->sg_table,
 				blk_rq_nr_phys_segments(req),
