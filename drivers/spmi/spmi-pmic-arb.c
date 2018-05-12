dcece9822b38d8963070239e3362b2824fb34966
Patrick Daly <pdaly@codeaurora.org>
2018-05-11 18:47:45 -0700

    spmi: pmic-arb: Use asynchronous probe
    A spmi controller may have many child devices, which together may take
    a signifigant amount of time to register and probe. Reduce this effect
    by probing asynchronously.
    
    After:
    [    0.277217] calling__software_resume+0x0/0x24c_@_1
    Before:
    [    0.319295] calling__software_resume+0x0/0x24c_@_1
    
    Change-Id: I95704a2ae7aae98e23dc9f29c7d023867d60388d
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>
    Signed-off-by: Swathi Sridhar <swatsrid@codeaurora.org>

--- drivers/spmi/spmi-pmic-arb.c
+++ drivers/spmi/spmi-pmic-arb.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2012-2015, 2017, 2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2018, 2021, The Linux Foundation. All rights reserved.
  */
 #include <linux/bitmap.h>
 #include <linux/delay.h>
@@ -1452,6 +1452,7 @@ static struct platform_driver spmi_pmic_arb_driver = {
 	.driver		= {
 		.name	= "spmi_pmic_arb",
 		.of_match_table = spmi_pmic_arb_match_table,
+		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
 	},
 };
 module_platform_driver(spmi_pmic_arb_driver);
