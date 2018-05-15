14b06ddd181832cad58b42d797b77bcdb495f4c9
Gustavo Pimentel <gustavo.pimentel@synopsys.com>
2018-05-15 15:41:44 +0100

    misc: pci_endpoint_test: Add DesignWare EP entry
    Add the DesignWare EP device ID entry to pci_endpoint_test driver table.
    
    Allow the device to be recognized and handled by the pci_endpoint_test
    driver.
    
    Signed-off-by: Gustavo Pimentel <gustavo.pimentel@synopsys.com>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Acked-by: Kishon Vijay Abraham I <kishon@ti.com>

--- drivers/misc/pci_endpoint_test.c
+++ drivers/misc/pci_endpoint_test.c
@@ -634,6 +634,7 @@ static void pci_endpoint_test_remove(struct pci_dev *pdev)
 static const struct pci_device_id pci_endpoint_test_tbl[] = {
 	{ PCI_DEVICE(PCI_VENDOR_ID_TI, PCI_DEVICE_ID_TI_DRA74x) },
 	{ PCI_DEVICE(PCI_VENDOR_ID_TI, PCI_DEVICE_ID_TI_DRA72x) },
+	{ PCI_DEVICE(PCI_VENDOR_ID_SYNOPSYS, 0xedda) },
 	{ }
 };
 MODULE_DEVICE_TABLE(pci, pci_endpoint_test_tbl);
