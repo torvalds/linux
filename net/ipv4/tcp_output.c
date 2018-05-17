200d95f4575934e49f872109cce18c5e72383eb8
Eric Dumazet <edumazet@google.com>
2018-05-17 14:47:27 -0700

    tcp: add TCPAckCompressed SNMP counter
    This counter tracks number of ACK packets that the host has not sent,
    thanks to ACK compression.
    
    Sample output :
    
    $ nstat -n;sleep 1;nstat|egrep "IpInReceives|IpOutRequests|TcpInSegs|TcpOutSegs|TcpExtTCPAckCompressed"
    IpInReceives                    123250             0.0
    IpOutRequests                   3684               0.0
    TcpInSegs                       123251             0.0
    TcpOutSegs                      3684               0.0
    TcpExtTCPAckCompressed          119252             0.0
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -165,6 +165,8 @@ static inline void tcp_event_ack_sent(struct sock *sk, unsigned int pkts)
 	struct tcp_sock *tp = tcp_sk(sk);
 
 	if (unlikely(tp->compressed_ack)) {
+		NET_ADD_STATS(sock_net(sk), LINUX_MIB_TCPACKCOMPRESSED,
+			      tp->compressed_ack);
 		tp->compressed_ack = 0;
 		if (hrtimer_try_to_cancel(&tp->compressed_ack_timer) == 1)
 			__sock_put(sk);
