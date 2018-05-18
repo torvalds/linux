488aa3c045a7de12a39babec5196260b9f0e8881
Geert Uytterhoeven <geert+renesas@glider.be>
2018-05-18 14:48:52 +0200

    ARM: shmobile: r8a7791: Remove legacy SMP fallback code
    As of commit f89a51700d7ae0ef ("ARM: shmobile: r8a7791: Prioritize DT
    APMU support") in v4.8, non-DT enablement of SMP was left as a fallback
    for backwards compatibility with old DTBs.
    
    The time has come to drop backwards compatibility, hence remove the
    fallback code.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/setup-r8a7791.c
+++ arch/arm/mach-shmobile/setup-r8a7791.c
@@ -20,7 +20,6 @@
 #include <asm/mach/arch.h>
 
 #include "common.h"
-#include "r8a7791.h"
 #include "rcar-gen2.h"
 
 static const char *const r8a7791_boards_compat_dt[] __initconst = {
@@ -30,7 +29,6 @@ static const char *const r8a7791_boards_compat_dt[] __initconst = {
 
 DT_MACHINE_START(R8A7791_DT, "Generic R8A7791 (Flattened Device Tree)")
 	.smp_init	= smp_init_ops(shmobile_smp_init_fallback_ops),
-	.smp		= smp_ops(r8a7791_smp_ops),
 	.init_early	= shmobile_init_delay,
 	.init_time	= rcar_gen2_timer_init,
 	.init_late	= shmobile_init_late,
