6b14787ab11d8fc2225132f0fe8275531fdefc72
Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
2018-05-20 00:05:02 +0300

    sh_eth: fix typo in comment to BCULR write
    Simon has noticed a typo in the comment accompaining the BCULR write --
    fix it and move the comment before the write (following the style of
    the other comments), while at it...
    
    Reported-by: Simon Horman <horms@verge.net.au>
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/renesas/sh_eth.c
+++ drivers/net/ethernet/renesas/sh_eth.c
@@ -1481,8 +1481,9 @@ static int sh_eth_dev_init(struct net_device *ndev)
 	if (mdp->cd->nbst)
 		sh_eth_modify(ndev, EDMR, EDMR_NBST, EDMR_NBST);
 
+	/* Burst cycle count upper-limit */
 	if (mdp->cd->bculr)
-		sh_eth_write(ndev, 0x800, BCULR);	/* Burst sycle set */
+		sh_eth_write(ndev, 0x800, BCULR);
 
 	sh_eth_write(ndev, mdp->cd->fcftr_value, FCFTR);
 
