78c47830a5cbb5d22dc91819a95af3d5c4bec084
Robin Murphy <robin.murphy@arm.com>
2018-05-21 12:35:13 +0100

    dma-debug: check scatterlist segments
    Drivers/subsystems creating scatterlists for DMA should be taking care
    to respect the scatter-gather limitations of the appropriate device, as
    described by dma_parms. A DMA API implementation cannot feasibly split
    a scatterlist into *more* entries than originally passed, so it is not
    well defined what they should do when given a segment larger than the
    limit they are also required to respect.
    
    Conversely, devices which are less limited than the rather conservative
    defaults, or indeed have no limitations at all (e.g. GPUs with their own
    internal MMU), should be encouraged to set appropriate dma_parms, as
    they may get more efficient DMA mapping performance out of it.
    
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- lib/dma-debug.c
+++ lib/dma-debug.c
@@ -1286,6 +1286,32 @@ out:
 	put_hash_bucket(bucket, &flags);
 }
 
+static void check_sg_segment(struct device *dev, struct scatterlist *sg)
+{
+#ifdef CONFIG_DMA_API_DEBUG_SG
+	unsigned int max_seg = dma_get_max_seg_size(dev);
+	u64 start, end, boundary = dma_get_seg_boundary(dev);
+
+	/*
+	 * Either the driver forgot to set dma_parms appropriately, or
+	 * whoever generated the list forgot to check them.
+	 */
+	if (sg->length > max_seg)
+		err_printk(dev, NULL, "DMA-API: mapping sg segment longer than device claims to support [len=%u] [max=%u]\n",
+			   sg->length, max_seg);
+	/*
+	 * In some cases this could potentially be the DMA API
+	 * implementation's fault, but it would usually imply that
+	 * the scatterlist was built inappropriately to begin with.
+	 */
+	start = sg_dma_address(sg);
+	end = start + sg_dma_len(sg) - 1;
+	if ((start ^ end) & ~boundary)
+		err_printk(dev, NULL, "DMA-API: mapping sg segment across boundary [start=0x%016llx] [end=0x%016llx] [boundary=0x%016llx]\n",
+			   start, end, boundary);
+#endif
+}
+
 void debug_dma_map_page(struct device *dev, struct page *page, size_t offset,
 			size_t size, int direction, dma_addr_t dma_addr,
 			bool map_single)
@@ -1415,6 +1441,8 @@ void debug_dma_map_sg(struct device *dev, struct scatterlist *sg,
 		if (!PageHighMem(sg_page(s))) {
 			check_for_illegal_area(dev, sg_virt(s), sg_dma_len(s));
 		}
+
+		check_sg_segment(dev, s);
 
 		add_dma_entry(entry);
 	}
