e25028c8ded011d19f9a11164807507c94febc01
Eric Auger <eric.auger@redhat.com>
2018-05-22 09:55:18 +0200

    KVM: arm/arm64: Bump VGIC_V3_MAX_CPUS to 512
    Let's raise the number of supported vcpus along with
    vgic v3 now that HW is looming with more physical CPUs.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Acked-by: Christoffer Dall <christoffer.dall@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- include/kvm/arm_vgic.h
+++ include/kvm/arm_vgic.h
@@ -28,7 +28,7 @@
 
 #include <linux/irqchip/arm-gic-v4.h>
 
-#define VGIC_V3_MAX_CPUS	255
+#define VGIC_V3_MAX_CPUS	512
 #define VGIC_V2_MAX_CPUS	8
 #define VGIC_NR_IRQS_LEGACY     256
 #define VGIC_NR_SGIS		16
