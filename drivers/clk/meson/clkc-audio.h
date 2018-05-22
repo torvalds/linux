3054a55c5dd2619a597d6e96d8589318f2b210ad
Jerome Brunet <jbrunet@baylibre.com>
2018-05-22 18:34:55 +0200

    clk: meson: add axg audio sclk divider driver
    Add a driver to control the clock divider found in the sample clock
    generator of the axg audio clock controller.
    
    The sclk divider accumulates specific features which make the generic
    divider unsuitable to control it:
    - zero based divider (div = val + 1), but zero value gates the clock,
      so minimum divider value is 2.
    - lrclk variant may adjust the duty cycle depending the divider value
      and the 'hi' value.
    
    Acked-by: Neil Armstrong <narmstrong@baylibre.com>
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>

--- drivers/clk/meson/clkc-audio.h
+++ drivers/clk/meson/clkc-audio.h
@@ -15,6 +15,14 @@ struct meson_clk_triphase_data {
 	struct parm ph2;
 };
 
+struct meson_sclk_div_data {
+	struct parm div;
+	struct parm hi;
+	unsigned int cached_div;
+	struct clk_duty cached_duty;
+};
+
 extern const struct clk_ops meson_clk_triphase_ops;
+extern const struct clk_ops meson_sclk_div_ops;
 
 #endif /* __MESON_CLKC_AUDIO_H */
