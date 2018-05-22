f242132bc440c71dd82dcb2112c95c97fffea976
Bjorn Helgaas <bhelgaas@google.com>
2018-05-22 07:56:12 -0500

    xtensa/PCI: Make variables static
    Make these variables static, since they're only used in this file:
    
      pci_ctrl_head
      pci_ctrl_tail
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- arch/xtensa/kernel/pci.c
+++ arch/xtensa/kernel/pci.c
@@ -41,8 +41,8 @@
  * pci_bus_add_device
  */
 
-struct pci_controller* pci_ctrl_head;
-struct pci_controller** pci_ctrl_tail = &pci_ctrl_head;
+static struct pci_controller *pci_ctrl_head;
+static struct pci_controller **pci_ctrl_tail = &pci_ctrl_head;
 
 static int pci_bus_count;
 
