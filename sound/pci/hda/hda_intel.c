b529ef2464ad3c9fcfa260c98d258b51d61418f0
Hans de Goede <hdegoede@redhat.com>
2018-05-23 15:27:03 +0200

    ALSA: hda: Add Clevo W35xSS_370SS to the power_save blacklist
    Power-saving is causing a plop and silences the first 2 seconds
    (give or take) of audio, silencing notifications sounds on Medion /
    Clevo W35xSS_370SS laptops.
    
    Add the Clevo W35xSS_370SS to the power_save blacklist.
    
    BugLink: https://bugzilla.redhat.com/show_bug.cgi?id=1581607
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -2210,6 +2210,8 @@ static struct snd_pci_quirk power_save_blacklist[] = {
 	SND_PCI_QUIRK(0x1849, 0x0c0c, "Asrock B85M-ITX", 0),
 	/* https://bugzilla.redhat.com/show_bug.cgi?id=1525104 */
 	SND_PCI_QUIRK(0x1043, 0x8733, "Asus Prime X370-Pro", 0),
+	/* https://bugzilla.redhat.com/show_bug.cgi?id=1581607 */
+	SND_PCI_QUIRK(0x1558, 0x3501, "Clevo W35xSS_370SS", 0),
 	/* https://bugzilla.redhat.com/show_bug.cgi?id=1520902 */
 	SND_PCI_QUIRK(0x8086, 0x2068, "Intel NUC7i3BNB", 0),
 	/* https://bugzilla.redhat.com/show_bug.cgi?id=1572975 */
