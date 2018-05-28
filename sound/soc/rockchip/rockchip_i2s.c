d86472234c483b1c23708b2bbb98fa7edd66bf15
Sugar Zhang <sugar.zhang@rock-chips.com>
2018-05-28 22:31:26 +0800

    ASoC: rockchip: add 'rockchip,no-dmaengine' for i2s/tdm/pdm
    This patch add support for property 'rockchip,no-dmaengine'.
    it is a boolean property. if present, driver will do not
    register pcm dmaengine, only just register dai. if the dai
    is part of multi-dais, the property should be present. Please
    refer to rockchip,multidais.txt about multi-dais usage.
    
    Change-Id: I9aa2ddb15f5a27202f90bf32804f214435cc0b73
    Signed-off-by: Sugar Zhang <sugar.zhang@rock-chips.com>

--- sound/soc/rockchip/rockchip_i2s.c
+++ sound/soc/rockchip/rockchip_i2s.c
@@ -697,6 +697,8 @@ static int rockchip_i2s_probe(struct platform_device *pdev)
 		goto err_suspend;
 	}
 
+	if (of_property_read_bool(node, "rockchip,no-dmaengine"))
+		return ret;
 	ret = devm_snd_dmaengine_pcm_register(&pdev->dev, NULL, 0);
 	if (ret) {
 		dev_err(&pdev->dev, "Could not register PCM\n");
