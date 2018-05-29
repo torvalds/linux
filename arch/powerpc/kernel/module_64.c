5f489579ef918970f655b3afc19c03732f16cbc4
Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
2018-05-29 12:21:00 +0530

    powerpc64/module elfv1: Set opd addresses after module relocation
    commit 59fe7eaf3598a89cbcd72e645b1d08afd76f7b29 upstream.
    
    module_frob_arch_sections() is called before the module is moved to its
    final location. The function descriptor section addresses we are setting
    here are thus invalid. Fix this by processing opd section during
    module_finalize()
    
    Fixes: 5633e85b2c313 ("powerpc64: Add .opd based function descriptor dereference")
    Cc: stable@vger.kernel.org # v4.16
    Signed-off-by: Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/powerpc/kernel/module_64.c
+++ arch/powerpc/kernel/module_64.c
@@ -360,11 +360,6 @@ int module_frob_arch_sections(Elf64_Ehdr *hdr,
 		else if (strcmp(secstrings+sechdrs[i].sh_name,"__versions")==0)
 			dedotify_versions((void *)hdr + sechdrs[i].sh_offset,
 					  sechdrs[i].sh_size);
-		else if (!strcmp(secstrings + sechdrs[i].sh_name, ".opd")) {
-			me->arch.start_opd = sechdrs[i].sh_addr;
-			me->arch.end_opd = sechdrs[i].sh_addr +
-					   sechdrs[i].sh_size;
-		}
 
 		/* We don't handle .init for the moment: rename to _init */
 		while ((p = strstr(secstrings + sechdrs[i].sh_name, ".init")))
