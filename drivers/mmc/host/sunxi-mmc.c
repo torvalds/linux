af6b8ff4bce4ea43e89236339f84fc0a26044c42
Ulf Hansson <ulf.hansson@linaro.org>
2018-05-30 15:02:16 +0200

    mmc: sunxi: Use ifdef rather than __maybe_unused
    To be consistent with code in other mmc host drivers, convert to check the
    correct PM config #ifdef in favor of using __maybe_unused.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/sunxi-mmc.c
+++ drivers/mmc/host/sunxi-mmc.c
@@ -1432,7 +1432,8 @@ static int sunxi_mmc_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static int __maybe_unused sunxi_mmc_runtime_resume(struct device *dev)
+#ifdef CONFIG_PM
+static int sunxi_mmc_runtime_resume(struct device *dev)
 {
 	struct mmc_host	*mmc = dev_get_drvdata(dev);
 	struct sunxi_mmc_host *host = mmc_priv(mmc);
@@ -1449,7 +1450,7 @@ static int __maybe_unused sunxi_mmc_runtime_resume(struct device *dev)
 	return 0;
 }
 
-static int __maybe_unused sunxi_mmc_runtime_suspend(struct device *dev)
+static int sunxi_mmc_runtime_suspend(struct device *dev)
 {
 	struct mmc_host	*mmc = dev_get_drvdata(dev);
 	struct sunxi_mmc_host *host = mmc_priv(mmc);
@@ -1459,6 +1460,7 @@ static int __maybe_unused sunxi_mmc_runtime_suspend(struct device *dev)
 
 	return 0;
 }
+#endif
 
 static const struct dev_pm_ops sunxi_mmc_pm_ops = {
 	SET_RUNTIME_PM_OPS(sunxi_mmc_runtime_suspend,
