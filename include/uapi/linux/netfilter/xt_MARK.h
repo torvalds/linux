d6ddd2a0f58bbef6b6bbd8b113162b67de6263d8
xxx <xxx@example.com>
2018-05-30 15:17:27 +0800

    note: type of kernel_sync, ipc

--- include/uapi/linux/netfilter/xt_MARK.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
-#ifndef _XT_MARK_H_target
-#define _XT_MARK_H_target
-
-#include <linux/netfilter/xt_mark.h>
-
-#endif /*_XT_MARK_H_target */
