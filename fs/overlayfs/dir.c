dd8ac699ed6e1ef296da8cb9d12d36428b901871
Miklos Szeredi <mszeredi@redhat.com>
2018-05-31 11:06:11 +0200

    ovl: return EIO on internal error
    EIO better represents an internal error than ENOENT.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/overlayfs/dir.c
+++ fs/overlayfs/dir.c
@@ -191,7 +191,7 @@ struct dentry *ovl_create_real(struct inode *dir, struct dentry *newdentry,
 		 * Not quite sure if non-instantiated dentry is legal or not.
 		 * VFS doesn't seem to care so check and warn here.
 		 */
-		err = -ENOENT;
+		err = -EIO;
 	}
 out:
 	if (err) {
