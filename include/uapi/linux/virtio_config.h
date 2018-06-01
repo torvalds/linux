cfecc2918d2b3c5e86ff1a6c95eabbbb17bb8fd3
Tiwei Bie <tiwei.bie@intel.com>
2018-06-01 12:02:39 +0800

    virtio_pci: support enabling VFs
    There is a new feature bit allocated in virtio spec to
    support SR-IOV (Single Root I/O Virtualization):
    
    https://github.com/oasis-tcs/virtio-spec/issues/11
    
    This patch enables the support for this feature bit in
    virtio driver.
    
    Signed-off-by: Tiwei Bie <tiwei.bie@intel.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- include/uapi/linux/virtio_config.h
+++ include/uapi/linux/virtio_config.h
@@ -49,7 +49,7 @@
  * transport being used (eg. virtio_ring), the rest are per-device feature
  * bits. */
 #define VIRTIO_TRANSPORT_F_START	28
-#define VIRTIO_TRANSPORT_F_END		34
+#define VIRTIO_TRANSPORT_F_END		38
 
 #ifndef VIRTIO_CONFIG_NO_LEGACY
 /* Do we get callbacks when the ring is completely used, even if we've
@@ -71,4 +71,9 @@
  * this is for compatibility with legacy systems.
  */
 #define VIRTIO_F_IOMMU_PLATFORM		33
+
+/*
+ * Does the device support Single Root I/O Virtualization?
+ */
+#define VIRTIO_F_SR_IOV			37
 #endif /* _UAPI_LINUX_VIRTIO_CONFIG_H */
