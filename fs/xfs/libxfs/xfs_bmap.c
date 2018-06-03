17ba2cc7b5b9ee6a44baf5e169c8901f1f049ea8
Darrick J. Wong <darrick.wong@oracle.com>
2018-06-03 16:10:13 -0700

    xfs: don't assert when reporting on-disk corruption while loading btree
    Don't bother ASSERTing when we're already going to log and return the
    corruption status.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Dave Chinner <dchinner@redhat.com>

--- fs/xfs/libxfs/xfs_bmap.c
+++ fs/xfs/libxfs/xfs_bmap.c
@@ -1248,7 +1248,6 @@ xfs_iread_extents(
 
 		num_recs = xfs_btree_get_numrecs(block);
 		if (unlikely(i + num_recs > nextents)) {
-			ASSERT(i + num_recs <= nextents);
 			xfs_warn(ip->i_mount,
 				"corrupt dinode %Lu, (btree extents).",
 				(unsigned long long) ip->i_ino);
