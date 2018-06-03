3f0b3cf46e0542ac4b4241c579b944b755d11b67
Trond Myklebust <trond.myklebust@hammerspace.com>
2018-06-03 13:05:21 -0400

    NFS: Filter cache invalidation when holding a delegation
    If the client holds a delegation, then ensure we filter out attempts
    to invalidate the size, owner, group owner, or mode unless we made the
    change, in which case, check that NFS_INO_REVAL_FORCED is set by the
    caller.
    Always filter out attempts to invalidate the change attribute and
    size, since we are authoritative for those.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfs/inode.c
+++ fs/nfs/inode.c
@@ -195,10 +195,16 @@ bool nfs_check_cache_invalid(struct inode *inode, unsigned long flags)
 static void nfs_set_cache_invalid(struct inode *inode, unsigned long flags)
 {
 	struct nfs_inode *nfsi = NFS_I(inode);
-	bool have_delegation = nfs_have_delegated_attributes(inode);
+	bool have_delegation = NFS_PROTO(inode)->have_delegation(inode, FMODE_READ);
 
-	if (have_delegation)
-		flags &= ~(NFS_INO_INVALID_CHANGE|NFS_INO_REVAL_PAGECACHE);
+	if (have_delegation) {
+		if (!(flags & NFS_INO_REVAL_FORCED))
+			flags &= ~NFS_INO_INVALID_OTHER;
+		flags &= ~(NFS_INO_INVALID_CHANGE
+				| NFS_INO_INVALID_SIZE
+				| NFS_INO_REVAL_PAGECACHE);
+	}
+
 	if (inode->i_mapping->nrpages == 0)
 		flags &= ~NFS_INO_INVALID_DATA;
 	nfsi->cache_validity |= flags;
