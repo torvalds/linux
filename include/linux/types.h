6d0e8d53849fb491549a01893d786bf33d4b28df
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-06-07 17:10:27 -0700

    include/linux/types.h: define aligned_ types based on uapi header
    <uapi/linux/types.h> has the same typedefs except that it prefixes them
    with double-underscore for user space.  Use them for the kernel space
    typedefs.
    
    Link: http://lkml.kernel.org/r/1526350925-14922-2-git-send-email-yamada.masahiro@socionext.com
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Lihao Liang <lianglihao@huawei.com>
    Cc: Philippe Ombredanne <pombredanne@nexb.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/types.h
+++ include/linux/types.h
@@ -115,9 +115,9 @@ typedef		__s64		int64_t;
 #endif
 
 /* this is a special 64bit data type that is 8-byte aligned */
-#define aligned_u64 __u64 __attribute__((aligned(8)))
-#define aligned_be64 __be64 __attribute__((aligned(8)))
-#define aligned_le64 __le64 __attribute__((aligned(8)))
+#define aligned_u64		__aligned_u64
+#define aligned_be64		__aligned_be64
+#define aligned_le64		__aligned_le64
 
 /**
  * The type used for indexing onto a disc or disc partition.
