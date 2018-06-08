0319c9a0ef4a07d5cc0f8f42d71fb33e5cfc612d
Bjorn Helgaas <bhelgaas@google.com>
2018-06-08 08:39:38 -0500

    PCI/AER: Squash aerdrv_errprint.c into aerdrv.c
    Squash aerdrv_errprint.c into aerdrv.c.  No functional change intended.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Keith Busch <keith.busch@intel.com>

--- drivers/pci/pcie/aer/aerdrv.h
+++ drivers/pci/pcie/aer/aerdrv.h
@@ -77,8 +77,6 @@ struct aer_rpc {
 };
 
 extern struct bus_type pcie_port_bus_type;
-void aer_print_error(struct pci_dev *dev, struct aer_err_info *info);
-void aer_print_port_info(struct pci_dev *dev, struct aer_err_info *info);
 irqreturn_t aer_irq(int irq, void *context);
 
 #ifdef CONFIG_ACPI_APEI
