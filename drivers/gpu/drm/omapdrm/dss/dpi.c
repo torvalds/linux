f79fa7da6a2953c9964f285dfddc61f92ced0a5a
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2018-06-08 16:03:14 +0300

    drm/omap: dpi: Don't fixup video mode in dpi_set_mode()
    The video mode is aleady fixed up by the .check_timings() operation,
    there's no need to repeat that when enabling the DPI output.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Sebastian Reichel <sebastian.reichel@collabora.co.uk>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/omapdrm/dss/dpi.c
+++ drivers/gpu/drm/omapdrm/dss/dpi.c
@@ -347,10 +347,9 @@ static int dpi_set_dispc_clk(struct dpi_data *dpi, unsigned long pck_req,
 
 static int dpi_set_mode(struct dpi_data *dpi)
 {
-	struct videomode *vm = &dpi->vm;
+	const struct videomode *vm = &dpi->vm;
 	int lck_div = 0, pck_div = 0;
 	unsigned long fck = 0;
-	unsigned long pck;
 	int r = 0;
 
 	if (dpi->pll)
@@ -361,15 +360,6 @@ static int dpi_set_mode(struct dpi_data *dpi)
 				&lck_div, &pck_div);
 	if (r)
 		return r;
-
-	pck = fck / lck_div / pck_div;
-
-	if (pck != vm->pixelclock) {
-		DSSWARN("Could not find exact pixel clock. Requested %lu Hz, got %lu Hz\n",
-			vm->pixelclock, pck);
-
-		vm->pixelclock = pck;
-	}
 
 	dss_mgr_set_timings(&dpi->output, vm);
 
