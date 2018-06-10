6a529317ff31e27885e41443e5658ef3654ce0bc
Golan Ben Ami <golan.ben.ami@intel.com>
2018-06-10 14:12:26 +0300

    iwlwifi: configure power scheme to balanced for 22560 devices
    As a temporary stage in the 22560 devices bring up, we disabled
    power save, to avoid bugs related to that domain.
    Now we would like to use power save so enable the balanced mode,
    and allow configuring the mode via module parameter.
    
    Signed-off-by: Golan Ben Ami <golan.ben.ami@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/mvm/ops.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/ops.c
@@ -720,10 +720,6 @@ iwl_op_mode_mvm_start(struct iwl_trans *trans, const struct iwl_cfg *cfg,
 		trans_cfg.rx_buf_size = rb_size_default;
 	}
 
-	/* TODO: remove when balanced power mode is fw supported */
-	if (mvm->trans->cfg->device_family >= IWL_DEVICE_FAMILY_22560)
-		iwlmvm_mod_params.power_scheme = IWL_POWER_SCHEME_CAM;
-
 	trans->wide_cmd_header = true;
 	trans_cfg.bc_table_dword =
 		mvm->trans->cfg->device_family < IWL_DEVICE_FAMILY_22560;
