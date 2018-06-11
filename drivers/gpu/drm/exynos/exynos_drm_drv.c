dc684af6fc8d3b9be7fda67e380e91bfdc773aa5
Marek Szyprowski <m.szyprowski@samsung.com>
2018-06-11 14:24:59 +0200

    drm/exynos: Suspend/resume display pipeline as early/late as possible
    In the current code, exynos_drm_suspend() function is called after all
    real devices (CRTCs, Encoders, etc) are suspended, because Exynos DRM
    virtual platform device is created as last device in the system (as
    a part of DRM registration). None of the devices for real hardware
    modules has its own system suspend/resume callbacks, so it doesn't
    change any order of the executed code, but it has a side-effect:
    runtime PM callbacks for real devices are not executed, because those
    devices are considered by PM core as already suspended. This might
    cause issues on boards with complex pipelines, where something
    depends on the runtime PM state of the given device.
    
    To ensure that exynos_drm_suspend() is called before any suspend
    callback from the real devices, assign it to .prepare callback. Same
    for exynos_drm_resume(), using .complete callback ensures that all
    real devices have been resumed when calling it.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_drv.c
+++ drivers/gpu/drm/exynos/exynos_drm_drv.c
@@ -146,7 +146,6 @@ static struct drm_driver exynos_drm_driver = {
 	.minor	= DRIVER_MINOR,
 };
 
-#ifdef CONFIG_PM_SLEEP
 static int exynos_drm_suspend(struct device *dev)
 {
 	struct drm_device *drm_dev = dev_get_drvdata(dev);
@@ -169,25 +168,23 @@ static int exynos_drm_suspend(struct device *dev)
 	return 0;
 }
 
-static int exynos_drm_resume(struct device *dev)
+static void exynos_drm_resume(struct device *dev)
 {
 	struct drm_device *drm_dev = dev_get_drvdata(dev);
 	struct exynos_drm_private *private;
 
 	if (!drm_dev)
-		return 0;
+		return;
 
 	private = drm_dev->dev_private;
 	drm_atomic_helper_resume(drm_dev, private->suspend_state);
 	exynos_drm_fbdev_resume(drm_dev);
 	drm_kms_helper_poll_enable(drm_dev);
-
-	return 0;
 }
-#endif
 
 static const struct dev_pm_ops exynos_drm_pm_ops = {
-	SET_SYSTEM_SLEEP_PM_OPS(exynos_drm_suspend, exynos_drm_resume)
+	.prepare = exynos_drm_suspend,
+	.complete = exynos_drm_resume,
 };
 
 /* forward declaration */
