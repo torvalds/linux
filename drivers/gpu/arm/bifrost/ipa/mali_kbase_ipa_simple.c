d26da80d60e3d1f9a954086d96d1e3890960e399
Zhen Chen <chenzhen@rock-chips.com>
2018-06-15 11:19:09 +0800

    MALI: bifrost: ipa: make poll_temperature_thread freezable
    Change-Id: I27343b919271be4618687a2311b6bf54e24cefce
    Signed-off-by: Zhen Chen <chenzhen@rock-chips.com>

--- drivers/gpu/arm/bifrost/ipa/mali_kbase_ipa_simple.c
+++ drivers/gpu/arm/bifrost/ipa/mali_kbase_ipa_simple.c
@@ -20,6 +20,7 @@
  *
  */
 
+#include <linux/freezer.h>
 #include <uapi/linux/thermal.h>
 #include <linux/thermal.h>
 #ifdef CONFIG_DEVFREQ_THERMAL
@@ -154,6 +155,8 @@ static int poll_temperature(void *data)
 	int temp;
 #endif
 
+	set_freezable();
+
 	while (!kthread_should_stop()) {
 		struct thermal_zone_device *tz = READ_ONCE(model_data->gpu_tz);
 
@@ -173,6 +176,8 @@ static int poll_temperature(void *data)
 		WRITE_ONCE(model_data->current_temperature, temp);
 
 		msleep_interruptible(READ_ONCE(model_data->temperature_poll_interval_ms));
+
+		try_to_freeze();
 	}
 
 	return 0;
