6075e4ae6e729778ac687a9ffb0c0d5223a0a19e
Arnd Bergmann <arnd@arndb.de>
2018-06-18 12:05:00 +0200

    s390: hypfs: use ktime_get_seconds() instead of get_seconds()
    time_t and get_seconds() are deprecated because they will overflow on
    32-bit architectures in the future. This is not a problem on 64-bit s390,
    but we should use proper interfaces anyway.
    
    Besides moving to the time64_t based interface, the CLOCK_MONOTONIC
    based ktime_get_seconds() is preferred for kernel internal timekeeping
    because it does not behave in unexpected ways during leap second changes
    or settimeofday() calls.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/hypfs/inode.c
+++ arch/s390/hypfs/inode.c
@@ -36,7 +36,7 @@ struct hypfs_sb_info {
 	kuid_t uid;			/* uid used for files and dirs */
 	kgid_t gid;			/* gid used for files and dirs */
 	struct dentry *update_file;	/* file to trigger update */
-	time_t last_update;		/* last update time in secs since 1970 */
+	time64_t last_update;		/* last update, CLOCK_MONOTONIC time */
 	struct mutex lock;		/* lock to protect update process */
 };
 
@@ -52,7 +52,7 @@ static void hypfs_update_update(struct super_block *sb)
 	struct hypfs_sb_info *sb_info = sb->s_fs_info;
 	struct inode *inode = d_inode(sb_info->update_file);
 
-	sb_info->last_update = get_seconds();
+	sb_info->last_update = ktime_get_seconds();
 	inode->i_atime = inode->i_mtime = inode->i_ctime = current_time(inode);
 }
 
@@ -179,7 +179,7 @@ static ssize_t hypfs_write_iter(struct kiocb *iocb, struct iov_iter *from)
 	 *    to restart data collection in this case.
 	 */
 	mutex_lock(&fs_info->lock);
-	if (fs_info->last_update == get_seconds()) {
+	if (fs_info->last_update == ktime_get_seconds()) {
 		rc = -EBUSY;
 		goto out;
 	}
