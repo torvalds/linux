0f67706e6600cab897e1806133eea7f5000a2789
Thomas Zimmermann <tdz@users.sourceforge.net>
2018-06-18 13:01:51 +0200

    drm/i915: Replace drm_gem_object_{un/reference} with {put,get} functions
    This patch unifies the naming of DRM functions for reference counting
    of struct drm_gem_object. The resulting code is more aligned with the
    rest of the Linux kernel interfaces.
    
    Signed-off-by: Thomas Zimmermann <tdz@users.sourceforge.net>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180618110154.30462-3-tdz@users.sourceforge.net

--- drivers/gpu/drm/i915/i915_gem_object.h
+++ drivers/gpu/drm/i915/i915_gem_object.h
@@ -337,12 +337,9 @@ __attribute__((nonnull))
 static inline struct drm_i915_gem_object *
 i915_gem_object_get(struct drm_i915_gem_object *obj)
 {
-	drm_gem_object_reference(&obj->base);
+	drm_gem_object_get(&obj->base);
 	return obj;
 }
-
-__deprecated
-extern void drm_gem_object_reference(struct drm_gem_object *);
 
 __attribute__((nonnull))
 static inline void
@@ -350,9 +347,6 @@ i915_gem_object_put(struct drm_i915_gem_object *obj)
 {
 	__drm_gem_object_unreference(&obj->base);
 }
-
-__deprecated
-extern void drm_gem_object_unreference(struct drm_gem_object *);
 
 __deprecated
 extern void drm_gem_object_unreference_unlocked(struct drm_gem_object *);
