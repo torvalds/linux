3b0636483c356fda2d653b89044237f4be1e48c8
Naveen Manohar <naveen.m@intel.com>
2018-06-18 13:29:38 -0500

    ASoC: Intel: common: Add Geminilake Dialog+Maxim machine driver entry
    This patch adds da7219_max98357a machine driver entry into
    machine table
    
    Signed-off-by: Naveen Manohar <naveen.m@intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 65a33883c778befcb85ef45285763fd8ac1b2ba3)

--- sound/soc/intel/common/soc-acpi-intel-glk-match.c
+++ sound/soc/intel/common/soc-acpi-intel-glk-match.c
@@ -9,11 +9,23 @@
 #include <sound/soc-acpi.h>
 #include <sound/soc-acpi-intel-match.h>
 
+static struct snd_soc_acpi_codecs glk_codecs = {
+	.num_codecs = 1,
+	.codecs = {"MX98357A"}
+};
+
 struct snd_soc_acpi_mach snd_soc_acpi_intel_glk_machines[] = {
 	{
 		.id = "INT343A",
 		.drv_name = "glk_alc298s_i2s",
 		.fw_filename = "intel/dsp_fw_glk.bin",
+	},
+	{
+		.id = "DLGS7219",
+		.drv_name = "glk_da7219_max98357a",
+		.fw_filename = "intel/dsp_fw_glk.bin",
+		.machine_quirk = snd_soc_acpi_codec_list,
+		.quirk_data = &glk_codecs,
 	},
 	{},
 };
