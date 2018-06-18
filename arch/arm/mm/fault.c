3ee6a4498724a464ba9882007c4aef8ec01eba10
Eric W. Biederman <ebiederm@xmission.com>
2018-06-18 14:50:26 -0500

    signal/arm: Use force_sig_fault where appropriate
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- arch/arm/mm/fault.c
+++ arch/arm/mm/fault.c
@@ -161,12 +161,8 @@ __do_user_fault(struct task_struct *tsk, unsigned long addr,
 		unsigned int fsr, unsigned int sig, int code,
 		struct pt_regs *regs)
 {
-	struct siginfo si;
-
 	if (addr > TASK_SIZE)
 		harden_branch_predictor();
-
-	clear_siginfo(&si);
 
 #ifdef CONFIG_DEBUG_USER
 	if (((user_debug & UDBG_SEGV) && (sig == SIGSEGV)) ||
@@ -181,11 +177,7 @@ __do_user_fault(struct task_struct *tsk, unsigned long addr,
 	tsk->thread.address = addr;
 	tsk->thread.error_code = fsr;
 	tsk->thread.trap_no = 14;
-	si.si_signo = sig;
-	si.si_errno = 0;
-	si.si_code = code;
-	si.si_addr = (void __user *)addr;
-	force_sig_info(sig, &si, tsk);
+	force_sig_fault(sig, code, (void __user *)addr, tsk);
 }
 
 void do_bad_area(unsigned long addr, unsigned int fsr, struct pt_regs *regs)
