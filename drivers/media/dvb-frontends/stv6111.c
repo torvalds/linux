29ff8406740243e5fc98e269dfef907af5bcd11b
Daniel Scheller <d.scheller@gmx.net>
2018-06-19 14:50:15 -0400

    media: dvb-frontends/stv6111: cleanup and fix licensing boilerplates
    The stv6111.h doesn't carry any header nor any licensing boilerplate at
    all, so copy this from the main driver file stv6111.c.
    
    [mchehab@kernel.org: kept only the part of this patch that copied
     the license from stv6111.c into stv6111.h]
    
    Cc: Ralph Metzler <rjkm@metzlerbros.de>
    Cc: Manfred Voelkel <mvoelkel@DigitalDevices.de>
    Signed-off-by: Daniel Scheller <d.scheller@gmx.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/dvb-frontends/stv6111.c
+++ drivers/media/dvb-frontends/stv6111.c
@@ -11,7 +11,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
  */
 
 #include <linux/kernel.h>
