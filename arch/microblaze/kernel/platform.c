2dbeb7040a21846facbeea876e85cbde41fa81e9
Rob Herring <robh@kernel.org>
2018-06-19 15:36:19 -0600

    microblaze: remove unecessary of_platform_bus_probe call
    The call to of_platform_bus_probe has no effect because the DT core
    already probes default buses like "simple-bus" before this call.
    Michal Simek said 'xlnx,compound' hasn't been used in a long time, so
    that match entry isn't needed.
    
    Cc: Michal Simek <monstr@monstr.eu>
    Signed-off-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/microblaze/kernel/platform.c
+++ arch/microblaze/kernel/platform.c
@@ -14,15 +14,8 @@
 #include <linux/of_platform.h>
 #include <asm/setup.h>
 
-static struct of_device_id xilinx_of_bus_ids[] __initdata = {
-	{ .compatible = "simple-bus", },
-	{ .compatible = "xlnx,compound", },
-	{}
-};
-
 static int __init microblaze_device_probe(void)
 {
-	of_platform_bus_probe(NULL, xilinx_of_bus_ids, NULL);
 	of_platform_reset_gpio_probe();
 	return 0;
 }
