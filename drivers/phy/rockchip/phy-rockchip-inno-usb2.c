f7a26c41a7a58c432222c496824c2b1a4387922d
William Wu <william.wu@rock-chips.com>
2018-06-20 15:11:07 +0800

    phy: rockchip: rockchip-inno-usb2: check iddig upon resume
    Because USB OTG id irq is disabled during system suspend
    and enabled after resume, so the usb2 phy doesn't notice
    any id status change upon resume. It may cause two issues:
    
    1. Plug in OTG cable and USB device when system enter
       suspend, it will fail to detect the OTG cable and
       USB device after resume.
    
    2. Plug out OTG cable and USB device when system enter
       suspend, and then connect USB to PC or USB charger
       after resume, it will fail to detect USB charge type.
    
    This patch restores the OTG id status before enter suspend，
    and check the id status upon resume, and set the extcon
    state and vbus if id status has changed.
    
    Change-Id: Iaca14841cc287e7d82e1cffd64ff18bba86d3ba4
    Signed-off-by: William Wu <william.wu@rock-chips.com>
    Signed-off-by: Frank Wang <frank.wang@rock-chips.com>

--- drivers/phy/rockchip/phy-rockchip-inno-usb2.c
+++ drivers/phy/rockchip/phy-rockchip-inno-usb2.c
@@ -206,6 +206,7 @@ struct rockchip_usb2phy_cfg {
  * @port_id: flag for otg port or host port.
  * @low_power_en: enable enter low power when suspend.
  * @perip_connected: flag for periphyeral connect status.
+ * @prev_iddig: previous otg port id pin status.
  * @suspended: phy suspended flag.
  * @utmi_avalid: utmi avalid status usage flag.
  *	true	- use avalid to get vbus status
@@ -235,6 +236,7 @@ struct rockchip_usb2phy_port {
 	unsigned int	port_id;
 	bool		low_power_en;
 	bool		perip_connected;
+	bool		prev_iddig;
 	bool		suspended;
 	bool		utmi_avalid;
 	bool		vbus_attached;
@@ -1567,6 +1569,7 @@ static int rockchip_usb2phy_otg_port_init(struct rockchip_usb2phy *rphy,
 	rport->vbus_attached = false;
 	rport->vbus_enabled = false;
 	rport->perip_connected = false;
+	rport->prev_iddig = true;
 
 	mutex_init(&rport->mutex);
 
@@ -2095,6 +2098,8 @@ static int rockchip_usb2phy_pm_suspend(struct device *dev)
 		if (rport->port_id == USB2PHY_PORT_OTG &&
 		    rport->id_irq > 0) {
 			mutex_lock(&rport->mutex);
+			rport->prev_iddig = property_enabled(rphy->grf,
+						&rport->port_cfg->utmi_iddig);
 			ret = rockchip_usb2phy_enable_id_irq(rphy, rport,
 							     false);
 			mutex_unlock(&rport->mutex);
@@ -2126,6 +2131,7 @@ static int rockchip_usb2phy_pm_resume(struct device *dev)
 	struct rockchip_usb2phy *rphy = dev_get_drvdata(dev);
 	struct rockchip_usb2phy_port *rport;
 	unsigned int index;
+	bool iddig;
 	int ret = 0;
 
 	if (rphy->phy_cfg->phy_tuning)
@@ -2139,6 +2145,8 @@ static int rockchip_usb2phy_pm_resume(struct device *dev)
 		if (rport->port_id == USB2PHY_PORT_OTG &&
 		    rport->id_irq > 0) {
 			mutex_lock(&rport->mutex);
+			iddig = property_enabled(rphy->grf,
+						 &rport->port_cfg->utmi_iddig);
 			ret = rockchip_usb2phy_enable_id_irq(rphy, rport,
 							     true);
 			mutex_unlock(&rport->mutex);
@@ -2148,19 +2156,17 @@ static int rockchip_usb2phy_pm_resume(struct device *dev)
 				return ret;
 			}
 
-			if (!property_enabled(rphy->grf,
-					      &rport->port_cfg->utmi_iddig) &&
-			    !extcon_get_state(rphy->edev, EXTCON_USB_HOST)) {
+			if (iddig != rport->prev_iddig) {
 				dev_dbg(&rport->phy->dev,
-					"port power on when resume\n");
+					"iddig changed during resume\n");
+				rport->prev_iddig = iddig;
 				extcon_set_state_sync(rphy->edev,
 						      EXTCON_USB_HOST,
-						      true);
-				/* Enable VBUS supply */
+						      !iddig);
 				extcon_set_state_sync(rphy->edev,
 						      EXTCON_USB_VBUS_EN,
-						      true);
-				ret = rockchip_set_vbus_power(rport, true);
+						      !iddig);
+				ret = rockchip_set_vbus_power(rport, !iddig);
 				if (ret)
 					return ret;
 			}
