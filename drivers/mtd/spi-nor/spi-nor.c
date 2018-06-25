d720a43333b0c21694a00bd67a24fac8bebfbc3f
Robert Marko <robimarko@gmail.com>
2018-06-25 13:17:48 +0200

    mtd: spi-nor: Add Winbond w25q128jv support
    Datasheet:
    http://www.winbond.com/resource-files/w25q128jv%20revf%2003272018%20plus.pdf
    
    Testing done on Mikrotik Routerboard  wAP R board.
    It does not support Dual or Quad modes.
    
    Signed-off-by: Robert Marko <robimarko@gmail.com>
    Reviewed-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@bootlin.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -1531,6 +1531,11 @@ static const struct flash_info spi_nor_ids[] = {
 			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
 			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
 	},
+	{
+		"w25q128jv", INFO(0xef7018, 0, 64 * 1024, 256,
+			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
+			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
+	},
 	{ "w25q80", INFO(0xef5014, 0, 64 * 1024,  16, SECT_4K) },
 	{ "w25q80bl", INFO(0xef4014, 0, 64 * 1024,  16, SECT_4K) },
 	{ "w25q128", INFO(0xef4018, 0, 64 * 1024, 256, SECT_4K) },
