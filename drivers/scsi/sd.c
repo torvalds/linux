2b33ab371e026cfcea4cbdf233c6b8776dde9ba8
Bart Van Assche <bart.vanassche@wdc.com>
2018-06-25 16:49:38 -0700

    scsi: sd: Remove a superfluous assignment
    Since blk_rq_bytes(req) returns req->__data_len, assigning that value to
    req->__data_len is superfluous. Hence remove that assignment.
    
    See also commit 5db44863b6eb ("[SCSI] sd: Implement support for WRITE SAME").
    
    Signed-off-by: Bart Van Assche <bart.vanassche@wdc.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -2028,7 +2028,6 @@ static int sd_done(struct scsi_cmnd *SCpnt)
 				} else {
 					sdkp->device->no_write_same = 1;
 					sd_config_write_same(sdkp);
-					req->__data_len = blk_rq_bytes(req);
 					req->rq_flags |= RQF_QUIET;
 				}
 				break;
