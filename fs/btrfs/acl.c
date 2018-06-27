5ee552da503961e6df785b8495bcfc98eb30eb2a
Chengguang Xu <cgxu519@gmx.com>
2018-06-27 12:16:36 +0800

    btrfs: remove unnecessary -ERANGE check in btrfs_get_acl
    There is no chance to get into -ERANGE error condition because we first
    call btrfs_getxattr to get the length of the attribute, then we do a
    subsequent call with the size from the first call.  Between the 2 calls
    the size shouldn't change. So remove the unnecessary -ERANGE error
    check.
    
    Signed-off-by: Chengguang Xu <cgxu519@gmx.com>
    Reviewed-by: Nikolay Borisov <nborisov@suse.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/acl.c
+++ fs/btrfs/acl.c
@@ -42,7 +42,7 @@ struct posix_acl *btrfs_get_acl(struct inode *inode, int type)
 	}
 	if (size > 0) {
 		acl = posix_acl_from_xattr(&init_user_ns, value, size);
-	} else if (size == -ERANGE || size == -ENODATA || size == 0) {
+	} else if (size == -ENODATA || size == 0) {
 		acl = NULL;
 	} else {
 		acl = ERR_PTR(-EIO);
