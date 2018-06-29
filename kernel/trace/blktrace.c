e1a413245a564683697a3d02ec197b72cf009b89
Liu Bo <bo.liu@linux.alibaba.com>
2018-06-29 09:56:08 +0800

    Blktrace: bail out early if block debugfs is not configured
    Since @blk_debugfs_root couldn't be configured dynamically, we can
    save a few memory allocation if it's not there.
    
    Signed-off-by: Liu Bo <bo.liu@linux.alibaba.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- kernel/trace/blktrace.c
+++ kernel/trace/blktrace.c
@@ -494,6 +494,9 @@ static int do_blk_trace_setup(struct request_queue *q, char *name, dev_t dev,
 	if (!buts->buf_size || !buts->buf_nr)
 		return -EINVAL;
 
+	if (!blk_debugfs_root)
+		return -ENOENT;
+
 	strncpy(buts->name, name, BLKTRACE_BDEV_SIZE);
 	buts->name[BLKTRACE_BDEV_SIZE - 1] = '\0';
 
@@ -517,9 +520,6 @@ static int do_blk_trace_setup(struct request_queue *q, char *name, dev_t dev,
 		goto err;
 
 	ret = -ENOENT;
-
-	if (!blk_debugfs_root)
-		goto err;
 
 	dir = debugfs_lookup(buts->name, blk_debugfs_root);
 	if (!dir)
