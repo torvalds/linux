5650729f9a1bbf65b57139d855dabe0a7e6cb494
Jerome Brunet <jbrunet@baylibre.com>
2018-06-29 17:09:20 +0200

    ASoC: es7134: remove 64kHz rate from the supported rates
    64Khz is actually not supported by the es7134 according to the datasheet
    
    Fixes: 9000b59d7a12 ("ASoC: es7134: add es7134 DAC driver")
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/es7134.c
+++ sound/soc/codecs/es7134.c
@@ -48,7 +48,11 @@ static struct snd_soc_dai_driver es7134_dai = {
 		.stream_name = "Playback",
 		.channels_min = 2,
 		.channels_max = 2,
-		.rates = SNDRV_PCM_RATE_8000_192000,
+		.rates = (SNDRV_PCM_RATE_8000_48000 |
+			  SNDRV_PCM_RATE_88200      |
+			  SNDRV_PCM_RATE_96000      |
+			  SNDRV_PCM_RATE_176400     |
+			  SNDRV_PCM_RATE_192000),
 		.formats = (SNDRV_PCM_FMTBIT_S16_LE  |
 			    SNDRV_PCM_FMTBIT_S18_3LE |
 			    SNDRV_PCM_FMTBIT_S20_3LE |
