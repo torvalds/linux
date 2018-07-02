dae0c332b6b37bdb6eeed9df6fa168b10def8acc
Geert Uytterhoeven <geert@linux-m68k.org>
2018-07-02 13:55:50 +0200

    Input: hilkbd - Add casts to HP9000/300 I/O accessors
    Internally, hilkbd uses "unsigned long" I/O addresses everywhere.
    This works fine as:
      - On PA-RISC, hilkbd uses the gsc_{read,write}b() I/O accessors, which
        take "unsigned long" addresses,
      - On m68k, hilkbd uses {read,write}b(), which are currently mapped to
        {in,out}_8(), and convert the passed addresses to pointers
        internally.
    
    However, the asm-generic version of {read,write}b() does not perform
    such conversions, and requires passing pointers instead.  Hence add
    casts to prepare for switching m68k to the asm-generic version.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Acked-by: Greg Ungerer <gerg@linux-m68k.org>
    Acked-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/keyboard/hilkbd.c
+++ drivers/input/keyboard/hilkbd.c
@@ -57,8 +57,8 @@ MODULE_LICENSE("GPL v2");
  #define HIL_DATA		0x1
  #define HIL_CMD		0x3
  #define HIL_IRQ		2
- #define hil_readb(p)		readb(p)
- #define hil_writeb(v,p)	writeb((v),(p))
+ #define hil_readb(p)		readb((const volatile void __iomem *)(p))
+ #define hil_writeb(v, p)	writeb((v), (volatile void __iomem *)(p))
 
 #else
 #error "HIL is not supported on this platform"
