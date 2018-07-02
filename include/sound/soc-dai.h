e2cfd2c9673f4e9d08971fed53fe1f41d8270ef0
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2018-07-02 06:22:15 +0000

    ASoC: soc-dai.h: convert to SPDX identifiers
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/sound/soc-dai.h
+++ include/sound/soc-dai.h
@@ -1,11 +1,8 @@
-/*
+/* SPDX-License-Identifier: GPL-2.0
+ *
  * linux/sound/soc-dai.h -- ALSA SoC Layer
  *
  * Copyright:	2005-2008 Wolfson Microelectronics. PLC.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  *
  * Digital Audio Interface (DAI) API.
  */
