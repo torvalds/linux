5cccc555108b602f92101a939143925095ff9b1d
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2018-07-02 22:05:31 +0200

    leds: default-on trigger: make use of module_led_trigger()
    This removes some boilerplate from the driver.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Jacek Anaszewski <jacek.anaszewski@gmail.com>

--- drivers/leds/trigger/ledtrig-default-on.c
+++ drivers/leds/trigger/ledtrig-default-on.c
@@ -26,19 +26,7 @@ static struct led_trigger defon_led_trigger = {
 	.name     = "default-on",
 	.activate = defon_trig_activate,
 };
-
-static int __init defon_trig_init(void)
-{
-	return led_trigger_register(&defon_led_trigger);
-}
-
-static void __exit defon_trig_exit(void)
-{
-	led_trigger_unregister(&defon_led_trigger);
-}
-
-module_init(defon_trig_init);
-module_exit(defon_trig_exit);
+module_led_trigger(defon_led_trigger);
 
 MODULE_AUTHOR("Nick Forbes <nick.forbes@incepta.com>");
 MODULE_DESCRIPTION("Default-ON LED trigger");
