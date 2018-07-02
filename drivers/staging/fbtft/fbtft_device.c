0d7fa8fa9cbc659ad825aacc3dc53e03400894ea
Radek Dostál <radek.dostal@streamunlimited.com>
2018-07-02 09:28:49 +0200

    staging: fbtft: indent fbtft_device_display - last entry - pdev - dev
    The dev section was opening curly bracket, but not adding ident, which
    resulted in two times "}," after each other with same indentation. Add
    ident at the right place fixes this problem.
    
    This formatting issue is not detectable by checkpatch.pl
    
    Signed-off-by: Radek Dostál <radek.dostal@streamunlimited.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fbtft/fbtft_device.c
+++ drivers/staging/fbtft/fbtft_device.c
@@ -1223,13 +1223,13 @@ static struct fbtft_device_display displays[] = {
 			.name = "",
 			.id = 0,
 			.dev = {
-			.release = fbtft_device_pdev_release,
-			.platform_data = &(struct fbtft_platform_data) {
-				.gpios = (const struct fbtft_gpio []) {
-					{},
+				.release = fbtft_device_pdev_release,
+				.platform_data = &(struct fbtft_platform_data) {
+					.gpios = (const struct fbtft_gpio []) {
+						{},
+					},
 				},
 			},
-		},
 		},
 	}
 };
