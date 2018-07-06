03bbc508a312d9f4833f1cb0f7b2da7517787b61
Chris Wilson <chris@chris-wilson.co.uk>
2018-07-06 12:45:10 +0100

    drm/i915/selftests: Skip live_execlists if the GPU is terminally wedged
    If the GPU is irrecoverably wedged, we can not execute any requests
    making testing execlists (request execution) pointless. Skip!
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Matthew Auld <matthew.auld@intel.com>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180706114510.18467-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/selftests/intel_lrc.c
+++ drivers/gpu/drm/i915/selftests/intel_lrc.c
@@ -455,5 +455,8 @@ int intel_execlists_live_selftests(struct drm_i915_private *i915)
 	if (!HAS_EXECLISTS(i915))
 		return 0;
 
+	if (i915_terminally_wedged(&i915->gpu_error))
+		return 0;
+
 	return i915_subtests(tests, i915);
 }
