4929c9428a171145f82f81aae0c3c25ef7d82837
Deepti Raghavan <deeptir@mit.edu>
2018-07-09 17:53:39 +0000

    tcp: expose both send and receive intervals for rate sample
    Congestion control algorithms, which access the rate sample
    through the tcp_cong_control function, only have access to the maximum
    of the send and receive interval, for cases where the acknowledgment
    rate may be inaccurate due to ACK compression or decimation. Algorithms
    may want to use send rates and receive rates as separate signals.
    
    Signed-off-by: Deepti Raghavan <deeptir@mit.edu>
    Acked-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -954,6 +954,8 @@ struct rate_sample {
 	u32  prior_delivered;	/* tp->delivered at "prior_mstamp" */
 	s32  delivered;		/* number of packets delivered over interval */
 	long interval_us;	/* time for tp->delivered to incr "delivered" */
+	u32 snd_interval_us;	/* snd interval for delivered packets */
+	u32 rcv_interval_us;	/* rcv interval for delivered packets */
 	long rtt_us;		/* RTT of last (S)ACKed packet (or -1) */
 	int  losses;		/* number of packets marked lost upon ACK */
 	u32  acked_sacked;	/* number of packets newly (S)ACKed upon ACK */
