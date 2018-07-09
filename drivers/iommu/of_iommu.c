78f307be3e0bc85f25dc05baa60f7435a88c2abf
Rob Herring <robh@kernel.org>
2018-07-09 09:41:51 -0600

    iommu: Stop deferring probe at end of initcalls
    The IOMMU subsystem has its own mechanism to not defer probe if driver
    support is missing. Now that the driver core supports stopping deferring
    probe if drivers aren't built-in (and probed), use the driver core
    support so the IOMMU specific support can be removed.
    
    Acked-by: Joerg Roedel <jroedel@suse.de>
    Cc: iommu@lists.linux-foundation.org
    Signed-off-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/iommu/of_iommu.c
+++ drivers/iommu/of_iommu.c
@@ -133,7 +133,7 @@ static int of_iommu_xlate(struct device *dev,
 	 * a proper probe-ordering dependency mechanism in future.
 	 */
 	if (!ops)
-		return -EPROBE_DEFER;
+		return driver_deferred_probe_check_state(dev);
 
 	return ops->of_xlate(dev, iommu_spec);
 }
