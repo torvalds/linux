dc2865ac3527d76fe04ee1f1a3ffc101a60faba0
Boris Brezillon <boris.brezillon@bootlin.com>
2018-07-09 22:09:40 +0200

    MIPS: txx9: Move the ndfc.h header to include/linux/platform_data/txx9
    This way we will be able to compile the ndfmc driver when
    COMPILE_TEST=y.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@bootlin.com>
    Acked-by: Paul Burton <paul.burton@mips.com>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- drivers/mtd/nand/raw/txx9ndfmc.c
+++ drivers/mtd/nand/raw/txx9ndfmc.c
@@ -20,7 +20,7 @@
 #include <linux/mtd/nand_ecc.h>
 #include <linux/mtd/partitions.h>
 #include <linux/io.h>
-#include <asm/txx9/ndfmc.h>
+#include <linux/platform_data/txx9/ndfmc.h>
 
 /* TXX9 NDFMC Registers */
 #define TXX9_NDFDTR	0x00
