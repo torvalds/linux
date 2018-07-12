e87a4a92fba3721eb06ba8d061b550e09e3d063a
Will Deacon <will.deacon@arm.com>
2018-07-12 11:37:40 +0100

    Revert "arm64: fix infinite stacktrace"
    This reverts commit 7e7df71fd57ff2894d96abb0080922bf39460a79.
    
    When unwinding out of the IRQ stack and onto the interrupted EL1 stack,
    we cannot rely on the frame pointer being strictly increasing, as this
    could terminate the backtrace early depending on how the stacks have
    been allocated.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/kernel/stacktrace.c
+++ arch/arm64/kernel/stacktrace.c
@@ -56,9 +56,6 @@ int notrace unwind_frame(struct task_struct *tsk, struct stackframe *frame)
 	frame->fp = READ_ONCE_NOCHECK(*(unsigned long *)(fp));
 	frame->pc = READ_ONCE_NOCHECK(*(unsigned long *)(fp + 8));
 
-	if (frame->fp <= fp)
-		return -EINVAL;
-
 #ifdef CONFIG_FUNCTION_GRAPH_TRACER
 	if (tsk->ret_stack &&
 			(frame->pc == (unsigned long)return_to_handler)) {
