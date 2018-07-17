6b0daa78207b622d768cce2a8251c830256bade9
Eric Biggers <ebiggers@google.com>
2018-07-17 10:09:26 -0700

    crypto: arm64/sha256 - increase cra_priority of scalar implementations
    Commit b73b7ac0a774 ("crypto: sha256_generic - add cra_priority") gave
    sha256-generic and sha224-generic a cra_priority of 100, to match the
    convention for generic implementations.  But sha256-arm64 and
    sha224-arm64 also have priority 100, so their order relative to the
    generic implementations became ambiguous.
    
    Therefore, increase their priority to 125 so that they have higher
    priority than the generic implementations but lower priority than the
    NEON implementations which have priority 150.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- arch/arm64/crypto/sha256-glue.c
+++ arch/arm64/crypto/sha256-glue.c
@@ -67,7 +67,7 @@ static struct shash_alg algs[] = { {
 	.descsize		= sizeof(struct sha256_state),
 	.base.cra_name		= "sha256",
 	.base.cra_driver_name	= "sha256-arm64",
-	.base.cra_priority	= 100,
+	.base.cra_priority	= 125,
 	.base.cra_blocksize	= SHA256_BLOCK_SIZE,
 	.base.cra_module	= THIS_MODULE,
 }, {
@@ -79,7 +79,7 @@ static struct shash_alg algs[] = { {
 	.descsize		= sizeof(struct sha256_state),
 	.base.cra_name		= "sha224",
 	.base.cra_driver_name	= "sha224-arm64",
-	.base.cra_priority	= 100,
+	.base.cra_priority	= 125,
 	.base.cra_blocksize	= SHA224_BLOCK_SIZE,
 	.base.cra_module	= THIS_MODULE,
 } };
