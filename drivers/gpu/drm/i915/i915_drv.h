05c72e77ccda89ff624108b1b59a0fc43843f343
Ville Syrjälä <ville.syrjala@linux.intel.com>
2018-07-17 20:42:14 +0300

    drm/i915: Nuke the LVDS lid notifier
    We broke the LVDS notifier resume thing in (presumably) commit
    e2c8b8701e2d ("drm/i915: Use atomic helpers for suspend, v2.") as
    we no longer duplicate the current state in the LVDS notifier and
    thus we never resume it properly either.
    
    Instead of trying to fix it again let's just kill off the lid
    notifier entirely. None of the machines tested thus far have
    apparently needed it. Originally the lid notifier was added to
    work around cases where the VBIOS was clobbering some of the
    hardware state behind the driver's back, mostly on Thinkpads.
    We now have a few report of Thinkpads working just fine without
    the notifier. So maybe it was misdiagnosed originally, or
    something else has changed (ACPI video stuff perhaps?).
    
    If we do end up finding a machine where the VBIOS is still causing
    problems I would suggest that we first try setting various bits in
    the VBIOS scratch registers. There are several to choose from that
    may instruct the VBIOS to steer clear.
    
    With the notifier gone we'll also stop looking at the panel status
    in ->detect().
    
    v2: Nuke enum modeset_restore (Rodrigo)
    
    Cc: stable@vger.kernel.org
    Cc: Wolfgang Draxinger <wdraxinger.maillist@draxit.de>
    Cc: Vito Caputo <vcaputo@pengaru.com>
    Cc: kitsunyan <kitsunyan@airmail.cc>
    Cc: Joonas Saarinen <jza@saunalahti.fi>
    Tested-by: Vito Caputo <vcaputo@pengaru.com> # Thinkapd X61s
    Tested-by: kitsunyan <kitsunyan@airmail.cc> # ThinkPad X200
    Tested-by: Joonas Saarinen <jza@saunalahti.fi> # Fujitsu Siemens U9210
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=105902
    References: https://lists.freedesktop.org/archives/intel-gfx/2018-June/169315.html
    References: https://bugs.freedesktop.org/show_bug.cgi?id=21230
    Fixes: e2c8b8701e2d ("drm/i915: Use atomic helpers for suspend, v2.")
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180717174216.22252-1-ville.syrjala@linux.intel.com
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1002,12 +1002,6 @@ struct i915_gem_mm {
 
 #define I915_ENGINE_WEDGED_TIMEOUT  (60 * HZ)  /* Reset but no recovery? */
 
-enum modeset_restore {
-	MODESET_ON_LID_OPEN,
-	MODESET_DONE,
-	MODESET_SUSPENDED,
-};
-
 #define DP_AUX_A 0x40
 #define DP_AUX_B 0x10
 #define DP_AUX_C 0x20
@@ -1730,8 +1724,6 @@ struct drm_i915_private {
 
 	unsigned long quirks;
 
-	enum modeset_restore modeset_restore;
-	struct mutex modeset_restore_lock;
 	struct drm_atomic_state *modeset_restore_state;
 	struct drm_modeset_acquire_ctx reset_ctx;
 
