0f3e250f2efc9232e381de97fa7764adafce8975
Michael Straube <straube.linux@gmail.com>
2018-07-21 20:57:36 +0200

    staging: rtl8188eu: refactor rtw_is_cckrates_included()
    Refactor rtw_is_cckrates_included() to improve readability and
    slightly reduce object file size.
    
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/core/rtw_ieee80211.c
+++ drivers/staging/rtl8188eu/core/rtw_ieee80211.c
@@ -74,14 +74,14 @@ int rtw_get_bit_value_from_ieee_value(u8 val)
 
 uint rtw_is_cckrates_included(u8 *rate)
 {
-	u32 i = 0;
+	while (*rate) {
+		u8 r = *rate & 0x7f;
 
-	while (rate[i] != 0) {
-		if  ((((rate[i]) & 0x7f) == 2) || (((rate[i]) & 0x7f) == 4) ||
-		     (((rate[i]) & 0x7f) == 11)  || (((rate[i]) & 0x7f) == 22))
+		if (r == 2 || r == 4 || r == 11 || r == 22)
 			return true;
-		i++;
+		rate++;
 	}
+
 	return false;
 }
 
