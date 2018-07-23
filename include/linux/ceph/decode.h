f7e52d8efe8588c5d4b4c78eb33da81d89486c1a
Ilya Dryomov <idryomov@gmail.com>
2018-07-23 14:11:40 +0200

    libceph: remove now unused ceph_{en,de}code_timespec()
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- include/linux/ceph/decode.h
+++ include/linux/ceph/decode.h
@@ -214,18 +214,6 @@ static inline void ceph_encode_timespec64(struct ceph_timespec *tv,
 	tv->tv_sec = cpu_to_le32((u32)ts->tv_sec);
 	tv->tv_nsec = cpu_to_le32((u32)ts->tv_nsec);
 }
-static inline void ceph_decode_timespec(struct timespec *ts,
-					const struct ceph_timespec *tv)
-{
-	ts->tv_sec = (__kernel_time_t)le32_to_cpu(tv->tv_sec);
-	ts->tv_nsec = (long)le32_to_cpu(tv->tv_nsec);
-}
-static inline void ceph_encode_timespec(struct ceph_timespec *tv,
-					const struct timespec *ts)
-{
-	tv->tv_sec = cpu_to_le32((u32)ts->tv_sec);
-	tv->tv_nsec = cpu_to_le32((u32)ts->tv_nsec);
-}
 
 /*
  * sockaddr_storage <-> ceph_sockaddr
