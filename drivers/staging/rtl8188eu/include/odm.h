51a2ee0a4e62961131879f8f315052c48265829d
Arnd Bergmann <arnd@arndb.de>
2018-07-24 13:13:28 +0200

    staging: rtl8188eu/rtl8723bs: fix character encoding
    drivers/staging/rtl8188eu/include/odm.h uses an incorrect encoding
    for the '...' character in two comments, which makes it one of the
    few non-UTF-8 source files.
    
    This removes the odd characters and uses the same ASCII representation
    that we have in the regular rtlwifi driver. The second instance
    of drivers/staging/rtl8723bs/hal/odm.h is garbled in a different way,
    so I change it to be the same as well even though it is already
    plain ASCII.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/odm.h
+++ drivers/staging/rtl8188eu/include/odm.h
@@ -708,7 +708,7 @@ struct odm_dm_struct {
 /* HOOK BEFORE REG INIT----------- */
 	/*  ODM Platform info AP/ADSL/CE/MP = 1/2/3/4 */
 	u8	SupportPlatform;
-	/*  ODM Support Ability DIG/RATR/TX_PWR_TRACK/ ¡K¡K = 1/2/3/¡K */
+	/*  ODM Support Ability DIG/RATR/TX_PWR_TRACK/... = 1/2/3/... */
 	u32	SupportAbility;
 	/*  ODM PCIE/USB/SDIO/GSPI = 0/1/2/3 */
 	u8	SupportInterface;
