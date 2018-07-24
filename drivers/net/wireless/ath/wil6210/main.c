6ccae584014ef7074359eb4151086beef66ecfa9
Hamad Kadmany <hkadmany@codeaurora.org>
2018-07-24 10:44:34 +0300

    wil6210: increase firmware ready timeout
    Firmware ready event may take longer than
    current timeout in some scenarios, for example
    with multiple RFs connected where each
    requires an initial calibration.
    
    Increase the timeout to support these scenarios.
    
    Signed-off-by: Hamad Kadmany <hkadmany@codeaurora.org>
    Signed-off-by: Maya Erez <merez@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/wil6210/main.c
+++ drivers/net/wireless/ath/wil6210/main.c
@@ -1282,7 +1282,7 @@ static int wil_get_otp_info(struct wil6210_priv *wil)
 
 static int wil_wait_for_fw_ready(struct wil6210_priv *wil)
 {
-	ulong to = msecs_to_jiffies(1000);
+	ulong to = msecs_to_jiffies(2000);
 	ulong left = wait_for_completion_timeout(&wil->wmi_ready, to);
 
 	if (0 == left) {
