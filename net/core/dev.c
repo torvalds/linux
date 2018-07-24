7effaf06c3cdef6855e127886c7405b9ab62f90d
Tariq Toukan <tariqt@mellanox.com>
2018-07-24 14:12:20 +0300

    net: rollback orig value on failure of dev_qdisc_change_tx_queue_len
    Fix dev_change_tx_queue_len so it rolls back original value
    upon a failure in dev_qdisc_change_tx_queue_len.
    This is already done for notifirers' failures, share the code.
    
    In case of failure in dev_qdisc_change_tx_queue_len, some tx queues
    would still be of the new length, while they should be reverted.
    Currently, the revert is not done, and is marked with a TODO label
    in dev_qdisc_change_tx_queue_len, and should find some nice solution
    to do it.
    Yet it is still better to not apply the newly requested value.
    
    Fixes: 48bfd55e7e41 ("net_sched: plug in qdisc ops change_tx_queue_len")
    Signed-off-by: Tariq Toukan <tariqt@mellanox.com>
    Reviewed-by: Eran Ben Elisha <eranbe@mellanox.com>
    Reported-by: Ran Rozenstein <ranro@mellanox.com>
    Cc: Cong Wang <xiyou.wangcong@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -7149,16 +7149,19 @@ int dev_change_tx_queue_len(struct net_device *dev, unsigned long new_len)
 		dev->tx_queue_len = new_len;
 		res = call_netdevice_notifiers(NETDEV_CHANGE_TX_QUEUE_LEN, dev);
 		res = notifier_to_errno(res);
-		if (res) {
-			netdev_err(dev,
-				   "refused to change device tx_queue_len\n");
-			dev->tx_queue_len = orig_len;
-			return res;
-		}
-		return dev_qdisc_change_tx_queue_len(dev);
+		if (res)
+			goto err_rollback;
+		res = dev_qdisc_change_tx_queue_len(dev);
+		if (res)
+			goto err_rollback;
 	}
 
 	return 0;
+
+err_rollback:
+	netdev_err(dev, "refused to change device tx_queue_len\n");
+	dev->tx_queue_len = orig_len;
+	return res;
 }
 
 /**
