d3094361a91f21a5b6092b4d50c47be570ae02cf
Michael Straube <straube.linux@gmail.com>
2018-07-26 21:46:59 +0200

    staging: rtl8188eu: remove unused sreset_get_wifi_status()
    The function sreset_get_wifi_status() is never used, so remove it.
    Discovered by cppcheck.
    
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/rtw_sreset.h
+++ drivers/staging/rtl8188eu/include/rtw_sreset.h
@@ -25,7 +25,6 @@ struct sreset_priv {
 #define	WIFI_RX_HANG			BIT(5)
 #define		WIFI_IF_NOT_EXIST	BIT(6)
 
-u8 sreset_get_wifi_status(struct adapter *padapter);
 void sreset_set_wifi_error_status(struct adapter *padapter, u32 status);
 
 #endif
