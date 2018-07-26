a56489d4b3c914eb30b724ff25debc2e59c7950e
Florian Schmaus <flo@geekplace.eu>
2018-07-26 12:17:37 +0800

    bcache: do not assign in if condition register_bcache()
    Fixes an error condition reported by checkpatch.pl which is caused by
    assigning a variable in an if condition.
    
    Signed-off-by: Florian Schmaus <flo@geekplace.eu>
    Signed-off-by: Coly Li <colyli@suse.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -2165,8 +2165,12 @@ static ssize_t register_bcache(struct kobject *k, struct kobj_attribute *attr,
 	if (!try_module_get(THIS_MODULE))
 		return -EBUSY;
 
-	if (!(path = kstrndup(buffer, size, GFP_KERNEL)) ||
-	    !(sb = kmalloc(sizeof(struct cache_sb), GFP_KERNEL)))
+	path = kstrndup(buffer, size, GFP_KERNEL);
+	if (!path)
+		goto err;
+
+	sb = kmalloc(sizeof(struct cache_sb), GFP_KERNEL);
+	if (!sb)
 		goto err;
 
 	err = "failed to open device";
