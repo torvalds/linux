1c02d502c20809a2a5f71ec16a930a61ed779b81
Eric Sandeen <sandeen@sandeen.net>
2018-07-26 09:11:27 -0700

    xfs: remove deprecated barrier/nobarrier mount
    The barrier mount options have been no-ops and deprecated since
    
    4cf4573 xfs: deprecate barrier/nobarrier mount option
    
    i.e. kernel 4.10 / December 2016, with a stated deprecation schedule
    after v4.15.  Should be fair game to remove them now.
    
    Signed-off-by: Eric Sandeen <sandeen@redhat.com>
    Reviewed-by: Carlos Maiolino <cmaiolino@redhat.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -217,7 +217,6 @@ typedef struct xfs_mount {
 #define XFS_MOUNT_SMALL_INUMS	(1ULL << 14)	/* user wants 32bit inodes */
 #define XFS_MOUNT_32BITINODES	(1ULL << 15)	/* inode32 allocator active */
 #define XFS_MOUNT_NOUUID	(1ULL << 16)	/* ignore uuid during mount */
-#define XFS_MOUNT_BARRIER	(1ULL << 17)
 #define XFS_MOUNT_IKEEP		(1ULL << 18)	/* keep empty inode clusters*/
 #define XFS_MOUNT_SWALLOC	(1ULL << 19)	/* turn on stripe width
 						 * allocation */
