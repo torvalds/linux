6453b532f2c8856a80381e6b9a1f5ea2f12294df
Diana Craciun <diana.craciun@nxp.com>
2018-07-28 09:06:33 +1000

    powerpc/64: Make stf barrier PPC_BOOK3S_64 specific.
    NXP Book3E platforms are not vulnerable to speculative store
    bypass, so make the mitigations PPC_BOOK3S_64 specific.
    
    Signed-off-by: Diana Craciun <diana.craciun@nxp.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/kernel/security.c
+++ arch/powerpc/kernel/security.c
@@ -176,6 +176,7 @@ ssize_t cpu_show_spectre_v2(struct device *dev, struct device_attribute *attr, c
 	return s.len;
 }
 
+#ifdef CONFIG_PPC_BOOK3S_64
 /*
  * Store-forwarding barrier support.
  */
@@ -323,3 +324,4 @@ static __init int stf_barrier_debugfs_init(void)
 }
 device_initcall(stf_barrier_debugfs_init);
 #endif /* CONFIG_DEBUG_FS */
+#endif /* CONFIG_PPC_BOOK3S_64 */
