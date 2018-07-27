538c48f27ac669cebd6d9abe1ce8b46d55f917ee
Len Brown <len.brown@intel.com>
2018-07-27 12:55:08 -0400

    tools/power turbostat: version 18.07.27
    Signed-off-by: Len Brown <len.brown@intel.com>

--- tools/power/x86/turbostat/turbostat.c
+++ tools/power/x86/turbostat/turbostat.c
@@ -5096,7 +5096,7 @@ int get_and_dump_counters(void)
 }
 
 void print_version() {
-	fprintf(outf, "turbostat version 18.06.20"
+	fprintf(outf, "turbostat version 18.07.27"
 		" - Len Brown <lenb@kernel.org>\n");
 }
 
