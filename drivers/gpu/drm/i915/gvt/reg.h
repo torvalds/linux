69ca5af4ff9a3ff96e4595c2b7522c01a2641779
Zhenyu Wang <zhenyuw@linux.intel.com>
2018-07-31 11:02:13 +0800

    drm/i915/gvt: Move some MMIO definitions to reg.h
    To consolidate all gvt private MMIO definition in one place,
    this moves some not yet used in i915 to reg.h.
    
    Reviewed-by: Hang Yuan <hang.yuan@intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>

--- drivers/gpu/drm/i915/gvt/reg.h
+++ drivers/gpu/drm/i915/gvt/reg.h
@@ -77,4 +77,13 @@
 #define _RING_CTL_BUF_SIZE(ctl) (((ctl) & RB_TAIL_SIZE_MASK) + \
 		I915_GTT_PAGE_SIZE)
 
+#define TRVATTL3PTRDW(i)	_MMIO(0x4de0 + (i) * 4)
+#define TRNULLDETCT		_MMIO(0x4de8)
+#define TRINVTILEDETCT		_MMIO(0x4dec)
+#define TRVADR			_MMIO(0x4df0)
+#define TRTTE			_MMIO(0x4df4)
+#define RING_EXCC(base)		_MMIO((base) + 0x28)
+#define RING_GFX_MODE(base)	_MMIO((base) + 0x29c)
+#define VF_GUARDBAND		_MMIO(0x83a4)
+
 #endif
