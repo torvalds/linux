b0a39d356ae1478a5876bb02ba08af155a3a5554
Mark Brown <broonie@kernel.org>
2018-08-01 14:18:50 +0100

    ASoC: wcd9335: Fix build due to CLASS-H Controller support
    This reverts commit c8cb5f775c8dac (ASoC: vert "ASoC: wcd9335: add
    CLASS-H Controller support) due to missing dependencies.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/wcd9335.c
+++ sound/soc/codecs/wcd9335.c
@@ -21,7 +21,6 @@
 #include <dt-bindings/mfd/wcd9335.h>
 #include <linux/mfd/wcd9335/wcd9335.h>
 #include <linux/mfd/wcd9335/registers.h>
-#include "wcd-clsh.h"
 
 #define WCD9335_RATES_MASK (SNDRV_PCM_RATE_8000 | SNDRV_PCM_RATE_16000 |\
 			    SNDRV_PCM_RATE_32000 | SNDRV_PCM_RATE_48000 |\
@@ -182,7 +181,6 @@ struct wcd9335_codec {
 	int sido_ccl_cnt;
 	enum wcd_clock_type clk_type;
 
-	struct wcd_clsh_ctrl *clsh_ctrl;
 	u32 hph_mode;
 };
 
@@ -1068,13 +1066,6 @@ static int wcd9335_codec_probe(struct snd_soc_component *component)
 	int i;
 
 	snd_soc_component_init_regmap(component, wcd->regmap);
-	/* Class-H Init*/
-	wcd->clsh_ctrl = wcd_clsh_ctrl_alloc(component, wcd->version);
-	if (IS_ERR(wcd->clsh_ctrl))
-		return PTR_ERR(wcd->clsh_ctrl);
-
-	/* Default HPH Mode to Class-H HiFi */
-	wcd->hph_mode = CLS_H_HIFI;
 	wcd->component = component;
 
 	wcd9335_codec_init(component);
@@ -1089,7 +1080,6 @@ static void wcd9335_codec_remove(struct snd_soc_component *comp)
 {
 	struct wcd9335_codec *wcd = dev_get_drvdata(comp->dev);
 
-	wcd_clsh_ctrl_free(wcd->clsh_ctrl);
 	free_irq(regmap_irq_get_virq(wcd->irq_data, WCD9335_IRQ_SLIMBUS), wcd);
 }
 
