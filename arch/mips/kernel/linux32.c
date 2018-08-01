3a1c0fc5927f001d2c70998c08cad42ee5f64849
Paul Burton <paul.burton@mips.com>
2018-08-01 13:15:17 -0700

    MIPS: Remove unused sys_32_mmap2
    The sys_32_mmap2 function has been unused since we started using syscall
    wrappers in commit dbda6ac08976 ("MIPS: CVE-2009-0029: Enable syscall
    wrappers."), and is indeed identical to the sys_mips_mmap2 function that
    replaced it in sys32_call_table.
    
    Remove the dead code.
    
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Patchwork: https://patchwork.linux-mips.org/patch/20107/
    Cc: James Hogan <jhogan@kernel.org>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: linux-mips@linux-mips.org

--- arch/mips/kernel/linux32.c
+++ arch/mips/kernel/linux32.c
@@ -61,16 +61,6 @@
 #define merge_64(r1, r2) ((((r2) & 0xffffffffUL) << 32) + ((r1) & 0xffffffffUL))
 #endif
 
-SYSCALL_DEFINE6(32_mmap2, unsigned long, addr, unsigned long, len,
-	unsigned long, prot, unsigned long, flags, unsigned long, fd,
-	unsigned long, pgoff)
-{
-	if (pgoff & (~PAGE_MASK >> 12))
-		return -EINVAL;
-	return ksys_mmap_pgoff(addr, len, prot, flags, fd,
-			       pgoff >> (PAGE_SHIFT-12));
-}
-
 #define RLIM_INFINITY32 0x7fffffff
 #define RESOURCE32(x) ((x > RLIM_INFINITY32) ? RLIM_INFINITY32 : x)
 
