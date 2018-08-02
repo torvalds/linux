1eb576881ff884dd6d10272b96cc336d156492c2
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2018-08-02 16:03:36 +0100

    ASoC: apq8096: remove auto rebinding
    Remove auto rebinding support, as component framework can deadlock
    in few usecases if we are trying to add new/remove component within
    a bind/unbind callbacks.
    
    Card rebinding is ASoC core feature so all the previous component
    framework stuff in q6dsp remains removed.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/qcom/apq8096.c
+++ sound/soc/qcom/apq8096.c
@@ -48,7 +48,6 @@ static int apq8096_platform_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	card->dev = dev;
-	card->auto_bind = true;
 	dev_set_drvdata(dev, card);
 	ret = qcom_snd_parse_of(card);
 	if (ret) {
@@ -74,7 +73,6 @@ static int apq8096_platform_remove(struct platform_device *pdev)
 {
 	struct snd_soc_card *card = dev_get_drvdata(&pdev->dev);
 
-	card->auto_bind = false;
 	snd_soc_unregister_card(card);
 	kfree(card->dai_link);
 	kfree(card);
