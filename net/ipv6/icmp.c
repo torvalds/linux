5f379ef51bc967567bbddacdcdecb772d4d7c3b3
Georg Kohmann <geokohma@cisco.com>
2018-08-02 13:56:58 +0200

    ipv6: icmp: Updating pmtu for link local route
    When a ICMPV6_PKT_TOOBIG is received from a link local address the pmtu will
    be updated on a route with an arbitrary interface index. Subsequent packets
    sent back to the same link local address may therefore end up not
    considering the updated pmtu.
    
    Current behavior breaks TAHI v6LC4.1.4 Reduce PMTU On-link. Referring to RFC
    1981: Section 3: "Note that Path MTU Discovery must be performed even in
    cases where a node "thinks" a destination is attached to the same link as
    itself. In a situation such as when a neighboring router acts as proxy [ND]
    for some destination, the destination can to appear to be directly
    connected but is in fact more than one hop away."
    
    Using the interface index from the incoming ICMPV6_PKT_TOOBIG when updating
    the pmtu.
    
    Signed-off-by: Georg Kohmann <geokohma@cisco.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/icmp.c
+++ net/ipv6/icmp.c
@@ -92,7 +92,7 @@ static void icmpv6_err(struct sk_buff *skb, struct inet6_skb_parm *opt,
 	struct net *net = dev_net(skb->dev);
 
 	if (type == ICMPV6_PKT_TOOBIG)
-		ip6_update_pmtu(skb, net, info, 0, 0, sock_net_uid(net, NULL));
+		ip6_update_pmtu(skb, net, info, skb->dev->ifindex, 0, sock_net_uid(net, NULL));
 	else if (type == NDISC_REDIRECT)
 		ip6_redirect(skb, net, skb->dev->ifindex, 0,
 			     sock_net_uid(net, NULL));
