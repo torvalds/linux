9aba2f801eea5070f1d5588cd4052588437b9eea
Ganesh Goudar <ganeshgr@chelsio.com>
2018-08-02 15:34:52 +0530

    net: Fix coding style in skb_push()
    Signed-off-by: Ganesh Goudar <ganeshgr@chelsio.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -1715,7 +1715,7 @@ void *skb_push(struct sk_buff *skb, unsigned int len)
 {
 	skb->data -= len;
 	skb->len  += len;
-	if (unlikely(skb->data<skb->head))
+	if (unlikely(skb->data < skb->head))
 		skb_under_panic(skb, len, __builtin_return_address(0));
 	return skb->data;
 }
