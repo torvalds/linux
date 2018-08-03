a6bcfc89694ed8cb482a82cdc8b93aae63a8b691
Li RongQing <lirongqing@baidu.com>
2018-08-03 15:45:21 +0800

    net: check extack._msg before print
    dev_set_mtu_ext is able to fail with a valid mtu value, at that
    condition, extack._msg is not set and random since it is in stack,
    then kernel will crash when print it.
    
    Fixes: 7a4c53bee3324a ("net: report invalid mtu value via netlink extack")
    Signed-off-by: Zhang Yu <zhangyu31@baidu.com>
    Signed-off-by: Li RongQing <lirongqing@baidu.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -7583,8 +7583,9 @@ int dev_set_mtu(struct net_device *dev, int new_mtu)
 	struct netlink_ext_ack extack;
 	int err;
 
+	memset(&extack, 0, sizeof(extack));
 	err = dev_set_mtu_ext(dev, new_mtu, &extack);
-	if (err)
+	if (err && extack._msg)
 		net_err_ratelimited("%s: %s\n", dev->name, extack._msg);
 	return err;
 }
