ea84cd1bcb18059427a2f832df1f2cb0719f5b73
Matthias Kaehlcke <mka@chromium.org>
2018-08-03 13:05:10 -0700

    UPSTREAM: PM / devfreq: Don't adjust to user limits in governors
    Several governors use the user space limits df->min/max_freq to adjust
    the target frequency. This is not necessary, since update_devfreq()
    already takes care of this. Instead the governor can request the available
    min/max frequency by setting the target frequency to DEVFREQ_MIN/MAX_FREQ
    and let update_devfreq() take care of any adjustments.
    
    Signed-off-by: Matthias Kaehlcke <mka@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>
    Reviewed-by: Chanwoo Choi <cw00.choi@samsung.com>
    Signed-off-by: MyungJoo Ham <myungjoo.ham@samsung.com>
    (cherry picked from commit 6ff66e2a008337b8a005fd0ae2037bed716262cc)
    
    Change-Id: I185bb2765d1b4d4052391d427bad55d4146c024d
    Signed-off-by: Finley Xiao <finley.xiao@rock-chips.com>

--- drivers/devfreq/governor.h
+++ drivers/devfreq/governor.h
@@ -25,6 +25,9 @@
 #define DEVFREQ_GOV_SUSPEND			0x4
 #define DEVFREQ_GOV_RESUME			0x5
 
+#define DEVFREQ_MIN_FREQ			0
+#define DEVFREQ_MAX_FREQ			ULONG_MAX
+
 /**
  * struct devfreq_governor - Devfreq policy governor
  * @node:		list node - contains registered devfreq governors
