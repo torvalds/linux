7f4de521001f4ea705d505c9f91f58d0f56a0e6d
Alexandru Gheorghe <alexandru-cosmin.gheorghe@arm.com>
2018-08-04 17:15:21 +0100

    drm/atomic: Add __drm_atomic_helper_plane_reset
    There are a lot of drivers that subclass drm_plane_state, all of them
    duplicate the code that links together the plane with plane_state.
    
    On top of that, drivers that enable core properties also have to
    duplicate the code for initializing the properties to their default
    values, which in all cases are the same as the defaults from core.
    
    Change since v1:
    - Make it consistent with the other helpers and require that both
      plane and state not be NULL, suggested by Boris Brezillon and
      Philipp Zabel.
    
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Alexandru Gheorghe <alexandru-cosmin.gheorghe@arm.com>
    Reviewed-by: Philipp Zabel <p.zabel@pengutronix.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180804161530.12275-2-alexandru-cosmin.gheorghe@arm.com

--- include/drm/drm_atomic_helper.h
+++ include/drm/drm_atomic_helper.h
@@ -156,6 +156,8 @@ void __drm_atomic_helper_crtc_destroy_state(struct drm_crtc_state *state);
 void drm_atomic_helper_crtc_destroy_state(struct drm_crtc *crtc,
 					  struct drm_crtc_state *state);
 
+void __drm_atomic_helper_plane_reset(struct drm_plane *plane,
+				     struct drm_plane_state *state);
 void drm_atomic_helper_plane_reset(struct drm_plane *plane);
 void __drm_atomic_helper_plane_duplicate_state(struct drm_plane *plane,
 					       struct drm_plane_state *state);
