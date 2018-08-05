a9df8cd5e3fcd046c20e036073b1f90854b3e863
Sohil Ladhani <supertakumi86@gmail.com>
2018-08-05 16:59:55 +0530

    Staging: rtlwifi: base: Modified the line ending with a parenthesis
    This patch fixes the "Lines should not end with a '('" warning reported by
    checkpatch.pl script. The line containing 'rtl_mrate_idx_to_arfr_id' function
    previously ended with '(', which did not conform to the linux kernel
    coding style.
    
    Signed-off-by: Sohil Ladhani <supertakumi86@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtlwifi/base.c
+++ drivers/staging/rtlwifi/base.c
@@ -685,9 +685,8 @@ static void _rtl_query_protection_mode(struct ieee80211_hw *hw,
 	}
 }
 
-u8 rtl_mrate_idx_to_arfr_id(
-	struct ieee80211_hw *hw, u8 rate_index,
-	enum wireless_mode wirelessmode)
+u8 rtl_mrate_idx_to_arfr_id(struct ieee80211_hw *hw, u8 rate_index,
+			    enum wireless_mode wirelessmode)
 {
 	struct rtl_priv *rtlpriv = rtl_priv(hw);
 	struct rtl_phy *rtlphy = &rtlpriv->phy;
