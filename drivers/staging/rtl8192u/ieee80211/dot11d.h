b83ff82670f3793360d1d360599fe7fcd9ec019b
John Whitmore <johnfwhitmore@gmail.com>
2018-08-08 22:00:31 +0100

    staging:rtl8192u: Rename dot11d_update_country_ie() parameters - Style
    The function dot11d_update_country_ie() uses parameter names which
    cause a checkpatch issue due to the use of CamelCase naming.
    
    This is a coding style change which should have no impact on runtime
    code execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/ieee80211/dot11d.h
+++ drivers/staging/rtl8192u/ieee80211/dot11d.h
@@ -46,9 +46,9 @@ struct rt_dot11d_info {
 void dot11d_init(struct ieee80211_device *dev);
 void dot11d_reset(struct ieee80211_device *dev);
 void dot11d_update_country_ie(struct ieee80211_device *dev,
-			      u8 *pTaddr,
-			      u16 CoutryIeLen,
-			      u8 *pCoutryIe);
+			      u8 *addr,
+			      u16 coutry_ie_len,
+			      u8 *coutry_ie);
 u8 DOT11D_GetMaxTxPwrInDbm(struct ieee80211_device *dev, u8 Channel);
 void DOT11D_ScanComplete(struct ieee80211_device *dev);
 int IsLegalChannel(struct ieee80211_device *dev, u8 channel);
