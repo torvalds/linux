b207de3ec531ff364843708c9fea968700aae8fe
Colin Ian King <colin.king@canonical.com>
2018-08-09 13:56:09 +0100

    ftrace: Remove unused pointer ftrace_swapper_pid
    Pointer ftrace_swapper_pid is defined but is never used hence it is
    redundant and can be removed. The use of this variable was removed
    in commit 345ddcc882d8 ("ftrace: Have set_ftrace_pid use the bitmap
    like events do").
    
    Cleans up clang warning:
    warning: 'ftrace_swapper_pid' defined but not used [-Wunused-const-variable=]
    
    Link: http://lkml.kernel.org/r/20180809125609.13142-1-colin.king@canonical.com
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/trace/ftrace.c
+++ kernel/trace/ftrace.c
@@ -1020,8 +1020,6 @@ static __init void ftrace_profile_tracefs(struct dentry *d_tracer)
 }
 #endif /* CONFIG_FUNCTION_PROFILER */
 
-static struct pid * const ftrace_swapper_pid = &init_struct_pid;
-
 #ifdef CONFIG_FUNCTION_GRAPH_TRACER
 static int ftrace_graph_active;
 #else
