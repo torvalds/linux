58c0f79887d5e425fe6a9fd542778e50df69e9c6
Rongyi Chen <chenyi@tt-cool.com>
2018-08-10 23:16:38 +0800

    clk: sunxi-ng: h6: fix PWM gate/reset offset
    Currently the register offset of the PWM bus gate in Allwinner H6 clock
    driver is wrong.
    
    Fix this issue.
    
    Fixes: 542353ea ("clk: sunxi-ng: add support for the Allwinner H6 CCU")
    Signed-off-by: Rongyi Chen <chenyi@tt-cool.com>
    [Icenowy: refactor commit message]
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>
    Signed-off-by: Chen-Yu Tsai <wens@csie.org>

--- drivers/clk/sunxi-ng/ccu-sun50i-h6.c
+++ drivers/clk/sunxi-ng/ccu-sun50i-h6.c
@@ -352,7 +352,7 @@ static SUNXI_CCU_GATE(bus_dbg_clk, "bus-dbg", "psi-ahb1-ahb2",
 static SUNXI_CCU_GATE(bus_psi_clk, "bus-psi", "psi-ahb1-ahb2",
 		      0x79c, BIT(0), 0);
 
-static SUNXI_CCU_GATE(bus_pwm_clk, "bus-pwm", "apb1", 0x79c, BIT(0), 0);
+static SUNXI_CCU_GATE(bus_pwm_clk, "bus-pwm", "apb1", 0x7ac, BIT(0), 0);
 
 static SUNXI_CCU_GATE(bus_iommu_clk, "bus-iommu", "apb1", 0x7bc, BIT(0), 0);
 
