8b5163eb988067093064ecb10265da27fd000cad
Max Filippov <jcmvbkbc@gmail.com>
2018-08-13 18:21:08 -0700

    xtensa: drop variant IRQ support
    If an xtensa core provides an additional IRQ controller it should be
    treated as a separate piece of hardware and be driven by an irqchip
    driver.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- drivers/irqchip/irq-xtensa-pic.c
+++ drivers/irqchip/irq-xtensa-pic.c
@@ -55,14 +55,12 @@ static void xtensa_irq_unmask(struct irq_data *d)
 
 static void xtensa_irq_enable(struct irq_data *d)
 {
-	variant_irq_enable(d->hwirq);
 	xtensa_irq_unmask(d);
 }
 
 static void xtensa_irq_disable(struct irq_data *d)
 {
 	xtensa_irq_mask(d);
-	variant_irq_disable(d->hwirq);
 }
 
 static void xtensa_irq_ack(struct irq_data *d)
