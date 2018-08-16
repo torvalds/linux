f90d1e0c7804b52e12fea501aa46a12c1ff6a567
Alexander Popov <alex.popov@linux.com>
2018-08-17 01:17:00 +0300

    lkdtm: Add a test for STACKLEAK
    Introduce an lkdtm test for the STACKLEAK feature: check that the
    current task stack is properly erased (filled with STACKLEAK_POISON).
    
    Signed-off-by: Alexander Popov <alex.popov@linux.com>
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>
    Tested-by: Laura Abbott <labbott@redhat.com>
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- drivers/misc/lkdtm/lkdtm.h
+++ drivers/misc/lkdtm/lkdtm.h
@@ -83,4 +83,7 @@ void lkdtm_USERCOPY_STACK_FRAME_FROM(void);
 void lkdtm_USERCOPY_STACK_BEYOND(void);
 void lkdtm_USERCOPY_KERNEL(void);
 
+/* lkdtm_stackleak.c */
+void lkdtm_STACKLEAK_ERASING(void);
+
 #endif
