e5c0f8ded390528fe009e5e1d579ffe6935cf3cb
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2018-08-22 16:34:46 +0100

    ASoC: sof: intel: skl, cnl, apl dai: update comment header to C++ style.
    Update the header comment to C++ style re upstream review comments.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- sound/soc/sof/intel/hda-dai.c
+++ sound/soc/sof/intel/hda-dai.c
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: (GPL-2.0 OR BSD-3-Clause)
-/*
- * This file is provided under a dual BSD/GPLv2 license.  When using or
- * redistributing this file, you may do so under either license.
- *
- * Copyright(c) 2018 Intel Corporation. All rights reserved.
- *
- * Authors: Keyon Jie <yang.jie@linux.intel.com>
- */
+//
+// This file is provided under a dual BSD/GPLv2 license.  When using or
+// redistributing this file, you may do so under either license.
+//
+// Copyright(c) 2018 Intel Corporation. All rights reserved.
+//
+// Authors: Keyon Jie <yang.jie@linux.intel.com>
+//
 
 #include <sound/pcm_params.h>
 #include "../sof-priv.h"
