9d2ab99573970838108add835442a03e23f8577b
Kunihiko Hayashi <hayashi.kunihiko@socionext.com>
2018-08-22 21:13:02 +0900

    ata: libahci_platform: add reset control support
    Add support to get and control a list of resets for the device
    as optional and shared. These resets must be kept de-asserted until
    the device is enabled.
    
    This is specified as shared because some SoCs like UniPhier series
    have common reset controls with all ahci controller instances.
    
    However, according to Thierry's view,
    https://www.spinics.net/lists/linux-ide/msg55357.html
    some hardware-specific drivers already use their own resets,
    and the common reset make a path to occur double controls of resets.
    
    The ahci_platform_get_resources() can get and control the reset
    only when the second argument includes AHCI_PLATFORM_GET_RESETS bit.
    
    Suggested-by: Hans de Goede <hdegoede@redhat.com>
    Cc: Thierry Reding <thierry.reding@gmail.com>
    Signed-off-by: Kunihiko Hayashi <hayashi.kunihiko@socionext.com>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- include/linux/ahci_platform.h
+++ include/linux/ahci_platform.h
@@ -43,4 +43,6 @@ int ahci_platform_resume_host(struct device *dev);
 int ahci_platform_suspend(struct device *dev);
 int ahci_platform_resume(struct device *dev);
 
+#define AHCI_PLATFORM_GET_RESETS	0x01
+
 #endif /* _AHCI_PLATFORM_H */
