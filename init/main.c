3f5c15d8a7d86fb642fe07df58c2065190f3e531
Paul Menzel <pmenzel@molgen.mpg.de>
2018-08-21 21:58:37 -0700

    init/main.c: log init process file name
    Add a log message to `run_init_process()`.
    
    This log message serves two purposes.
    
    1.  If the init process is not specified on the Linux Kernel command
        line, the user sees, what file was chosen.
    
    2.  The time stamps shows exactly, when the Linux kernel handed over
        control to the init process.
    
    Link: http://lkml.kernel.org/r/b1fc97fa-4aa9-1904-ddb5-859e78995c41@molgen.mpg.de
    Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- init/main.c
+++ init/main.c
@@ -1002,6 +1002,7 @@ void __init load_default_modules(void)
 static int run_init_process(const char *init_filename)
 {
 	argv_init[0] = init_filename;
+	pr_info("Run %s as init process\n", init_filename);
 	return do_execve(getname_kernel(init_filename),
 		(const char __user *const __user *)argv_init,
 		(const char __user *const __user *)envp_init);
