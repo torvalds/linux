48440ab6dc275a3144474e8c5f45fab854d6e20f
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2018-08-22 19:58:45 -0700

    nvmet: remove unreachable code
    Get rid of the unreachable code in the nvmet_parse_discovery_cmd().
    Keep the error message identical to the admin-cmd.c and io-cmd*.c
    
    Signed-off-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/nvme/target/discovery.c
+++ drivers/nvme/target/discovery.c
@@ -219,12 +219,10 @@ u16 nvmet_parse_discovery_cmd(struct nvmet_req *req)
 			return NVME_SC_INVALID_OPCODE | NVME_SC_DNR;
 		}
 	default:
-		pr_err("unsupported cmd %d\n", cmd->common.opcode);
+		pr_err("unhandled cmd %d\n", cmd->common.opcode);
 		return NVME_SC_INVALID_OPCODE | NVME_SC_DNR;
 	}
 
-	pr_err("unhandled cmd %d\n", cmd->common.opcode);
-	return NVME_SC_INVALID_OPCODE | NVME_SC_DNR;
 }
 
 int __init nvmet_init_discovery(void)
