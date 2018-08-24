15c480efab01197c965ce0562a43ffedd852b8f9
Daniel Borkmann <daniel@iogearbox.net>
2018-08-24 22:08:51 +0200

    bpf, sockmap: fix psock refcount leak in bpf_tcp_recvmsg
    In bpf_tcp_recvmsg() we first took a reference on the psock, however
    once we find that there are skbs in the normal socket's receive queue
    we return with processing them through tcp_recvmsg(). Problem is that
    we leak the taken reference on the psock in that path. Given we don't
    really do anything with the psock at this point, move the skb_queue_empty()
    test before we fetch the psock to fix this case.
    
    Fixes: 8934ce2fd081 ("bpf: sockmap redirect ingress support")
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- kernel/bpf/sockmap.c
+++ kernel/bpf/sockmap.c
@@ -912,6 +912,8 @@ static int bpf_tcp_recvmsg(struct sock *sk, struct msghdr *msg, size_t len,
 
 	if (unlikely(flags & MSG_ERRQUEUE))
 		return inet_recv_error(sk, msg, len, addr_len);
+	if (!skb_queue_empty(&sk->sk_receive_queue))
+		return tcp_recvmsg(sk, msg, len, nonblock, flags, addr_len);
 
 	rcu_read_lock();
 	psock = smap_psock_sk(sk);
@@ -921,9 +923,6 @@ static int bpf_tcp_recvmsg(struct sock *sk, struct msghdr *msg, size_t len,
 	if (unlikely(!refcount_inc_not_zero(&psock->refcnt)))
 		goto out;
 	rcu_read_unlock();
-
-	if (!skb_queue_empty(&sk->sk_receive_queue))
-		return tcp_recvmsg(sk, msg, len, nonblock, flags, addr_len);
 
 	lock_sock(sk);
 bytes_ready:
