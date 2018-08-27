cff7499d7eb4196f21fdd1bf643aad12d846ee5d
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2018-08-27 18:35:51 +0300

    extcon: Make static analyzer happy about union assignment
    When assign unions we need to supply non-scalar value, otherwise
    static analyzer is not happy:
    
    CHECK   drivers/extcon/extcon.c
    drivers/extcon/extcon.c:631:22: warning: cast to non-scalar
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- drivers/extcon/extcon.c
+++ drivers/extcon/extcon.c
@@ -628,7 +628,7 @@ int extcon_get_property(struct extcon_dev *edev, unsigned int id,
 	unsigned long flags;
 	int index, ret = 0;
 
-	*prop_val = (union extcon_property_value)(0);
+	*prop_val = (union extcon_property_value){0};
 
 	if (!edev)
 		return -EINVAL;
