ac771ed746c800749ec5adbb4b7c22ce1d89943e
Alexandre Belloni <alexandre.belloni@bootlin.com>
2018-08-27 23:23:44 +0200

    rtc: rv8803: add Epson RX8803 support
    The Epson rx8803 is mostly similar to the Microcrystal RV8803 but the size
    of the offset register is 4 bits vs 6 bits but it has a configurable
    temperature compensation.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/rtc/rtc-rv8803.c
+++ drivers/rtc/rtc-rv8803.c
@@ -615,6 +615,7 @@ static int rv8803_probe(struct i2c_client *client,
 
 static const struct i2c_device_id rv8803_id[] = {
 	{ "rv8803", rv_8803 },
+	{ "rx8803", rv_8803 },
 	{ "rx8900", rx_8900 },
 	{ }
 };
@@ -623,6 +624,10 @@ MODULE_DEVICE_TABLE(i2c, rv8803_id);
 static const struct of_device_id rv8803_of_match[] = {
 	{
 		.compatible = "microcrystal,rv8803",
+		.data = (void *)rv_8803
+	},
+	{
+		.compatible = "epson,rx8803",
 		.data = (void *)rv_8803
 	},
 	{
