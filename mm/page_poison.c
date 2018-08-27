e728d92e61d9da0620ab74006e638c857eab20b7
Wei Wang <wei.w.wang@intel.com>
2018-08-27 09:32:18 +0800

    UPSTREAM: mm/page_poison: expose page_poisoning_enabled to kernel modules
    In some usages, e.g. virtio-balloon, a kernel module needs to know if
    page poisoning is in use. This patch exposes the page_poisoning_enabled
    function to kernel modules.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Change-Id: Icab2f64ef17850ac8b636bd302653bb5f1960e8c
    (cherry picked from d95f58f4a6ca002126c36c530fb096519c94baef)
    Signed-off-by: Alexander Potapenko <glider@google.com>
    Signed-off-by: Todd Kjos <tkjos@google.com>

--- mm/page_poison.c
+++ mm/page_poison.c
@@ -18,6 +18,11 @@ static int __init early_page_poison_param(char *buf)
 }
 early_param("page_poison", early_page_poison_param);
 
+/**
+ * page_poisoning_enabled - check if page poisoning is enabled
+ *
+ * Return true if page poisoning is enabled, or false if not.
+ */
 bool page_poisoning_enabled(void)
 {
 	/*
@@ -30,6 +35,7 @@ bool page_poisoning_enabled(void)
 		(!IS_ENABLED(CONFIG_ARCH_SUPPORTS_DEBUG_PAGEALLOC) &&
 		debug_pagealloc_enabled()));
 }
+EXPORT_SYMBOL_GPL(page_poisoning_enabled);
 
 static void poison_page(struct page *page)
 {
