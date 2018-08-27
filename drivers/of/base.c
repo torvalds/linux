69116e3bae2b166c69fa39a08c85866c8d613ee9
Rob Herring <robh@kernel.org>
2018-08-27 09:50:09 -0500

    of: make PowerMac cache node search conditional on CONFIG_PPC_PMAC
    [ Upstream commit f6707fd6241e483f6fea2caae82d876e422bb11a ]
    
    Cache nodes under the cpu node(s) is PowerMac specific according to the
    comment above, so make the code enforce that.
    
    Signed-off-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/of/base.c
+++ drivers/of/base.c
@@ -2066,7 +2066,7 @@ struct device_node *of_find_next_cache_node(const struct device_node *np)
 	/* OF on pmac has nodes instead of properties named "l2-cache"
 	 * beneath CPU nodes.
 	 */
-	if (!strcmp(np->type, "cpu"))
+	if (IS_ENABLED(CONFIG_PPC_PMAC) && !strcmp(np->type, "cpu"))
 		for_each_child_of_node(np, child)
 			if (!strcmp(child->type, "cache"))
 				return child;
