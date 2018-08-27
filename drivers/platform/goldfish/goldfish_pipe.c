84ae527aa58c0f790233fec287d3a060a7501baa
Roman Kiryanov <rkir@google.com>
2018-08-27 11:23:00 -0700

    platform: goldfish: pipe: Update the comment for GFP_ATOMIC
    Provide an explanation why GFP_ATOMIC is needed to prevent changing it to
    other values.
    
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/platform/goldfish/goldfish_pipe.c
+++ drivers/platform/goldfish/goldfish_pipe.c
@@ -640,7 +640,10 @@ static int get_free_pipe_id_locked(struct goldfish_pipe_dev *dev)
 			return id;
 
 	{
-		/* Reallocate the array */
+		/* Reallocate the array.
+		 * Since get_free_pipe_id_locked runs with interrupts disabled,
+		 * we don't want to make calls that could lead to sleep.
+		 */
 		u32 new_capacity = 2 * dev->pipes_capacity;
 		struct goldfish_pipe **pipes =
 			kcalloc(new_capacity, sizeof(*pipes), GFP_ATOMIC);
