979f541851d9f5589ace7e47693c4f6bd5b9187e
Nicolas Adell <nicolas.adell@actia.fr>
2018-08-27 15:59:56 +0200

    usb: chipidea: imx: enable OTG overcurrent in case USB subsystem is already started
    [ Upstream commit 1dedbdf2bbb1ede8d96f35f9845ecae179dc1988 ]
    
    When initializing the USB subsystem before starting the kernel,
    OTG overcurrent detection is disabled. In case the OTG polarity of
    overcurrent is low active, the overcurrent detection is never enabled
    again and events cannot be reported as expected. Because imx usb
    overcurrent polarity is low active by default, only detection needs
    to be enable in usbmisc init function.
    
    Signed-off-by: Nicolas Adell <nicolas.adell@actia.fr>
    Signed-off-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/usb/chipidea/usbmisc_imx.c
+++ drivers/usb/chipidea/usbmisc_imx.c
@@ -343,6 +343,8 @@ static int usbmisc_imx6q_init(struct imx_usbmisc_data *data)
 	} else if (data->oc_polarity == 1) {
 		/* High active */
 		reg &= ~(MX6_BM_OVER_CUR_DIS | MX6_BM_OVER_CUR_POLARITY);
+	} else {
+		reg &= ~(MX6_BM_OVER_CUR_DIS);
 	}
 	writel(reg, usbmisc->base + data->index * 4);
 
