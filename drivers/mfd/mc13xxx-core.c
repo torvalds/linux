dd72391c9a5380a96351e9dba7a0a7cf7fd23a39
Fabio Estevam <fabio.estevam@nxp.com>
2018-08-28 17:02:40 -0300

    mfd: mc13xxx-core: Fix PMIC shutdown when reading ADC values
    [ Upstream commit 55143439b7b501882bea9d95a54adfe00ffc79a3 ]
    
    When trying to read any MC13892 ADC channel on a imx51-babbage board:
    
    The MC13892 PMIC shutdowns completely.
    
    After debugging this issue and comparing the MC13892 and MC13783
    initializations done in the vendor kernel, it was noticed that the
    CHRGRAWDIV bit of the ADC0 register was not being set.
    
    This bit is set by default after power on, but the driver was
    clearing it.
    
    After setting this bit it is possible to read the ADC values correctly.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>
    Tested-by: Chris Healy <cphealy@gmail.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/mfd/mc13xxx-core.c
+++ drivers/mfd/mc13xxx-core.c
@@ -278,7 +278,8 @@ int mc13xxx_adc_do_conversion(struct mc13xxx *mc13xxx, unsigned int mode,
 	if (ret)
 		goto out;
 
-	adc0 = MC13XXX_ADC0_ADINC1 | MC13XXX_ADC0_ADINC2;
+	adc0 = MC13XXX_ADC0_ADINC1 | MC13XXX_ADC0_ADINC2 |
+	       MC13XXX_ADC0_CHRGRAWDIV;
 	adc1 = MC13XXX_ADC1_ADEN | MC13XXX_ADC1_ADTRIGIGN | MC13XXX_ADC1_ASC;
 
 	/*
