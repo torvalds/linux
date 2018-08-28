3c44dafb98c4b5049d47f68b09c66fd20f3d7ed9
Elazar Leibovich <elazar@lightbitslabs.com>
2018-08-28 22:22:54 +0300

    bad checkpatch

--- fs/pnode.c
+++ fs/pnode.c
@@ -14,7 +14,7 @@
 #include "pnode.h"
 
 /* return the next shared peer mount of @p */
-static inline struct mount *next_peer(struct mount *p)
+  static inline struct mount *next_peer(struct mount *p)
 {
 	return list_entry(p->mnt_share.next, struct mount, mnt_share);
 }
