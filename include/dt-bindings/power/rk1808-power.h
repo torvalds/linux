c5a692d3ea9ac350804c52c005e97a0db9e1fe11
Elaine Zhang <zhangqing@rock-chips.com>
2018-08-29 14:32:48 +0800

    dt-bindings: power: add power-domain header for RK1808 SoCs
    According to a description from TRM, add all the power domains
    
    Change-Id: Id8c4af687c877e206f8ce08416dcb4e41a78ce46
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>

---
+++ include/dt-bindings/power/rk1808-power.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __DT_BINDINGS_POWER_RK1808_POWER_H__
+#define __DT_BINDINGS_POWER_RK1808_POWER_H__
+
+/* VD_CORE */
+#define RK1808_PD_A35_0		0
+#define RK1808_PD_A35_1		1
+#define RK1808_PD_SCU		2
+#define RK1808_VD_CORE          3
+
+/* VD_NPU */
+#define RK1808_VD_NPU           4
+
+/* VD_LOGIC */
+#define RK1808_PD_DDR           5
+#define RK1808_PD_PCIE		6
+#define RK1808_PD_VPU		7
+#define RK1808_PD_VIO		8
+
+#endif
