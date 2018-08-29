c99601f672fdb057955a5359ab25958c054b07c5
Linus Walleij <linus.walleij@linaro.org>
2018-08-29 17:07:29 +0200

    gpio: ts5500: Include the right header
    This is a GPIO driver so include only <linux/gpio/driver.h>.
    
    Cc: kernel@savoirfairelinux.com
    Cc: Vivien Didelot <vivien.didelot@savoirfairelinux.com>
    Cc: Jerome Oufella <jerome.oufella@savoirfairelinux.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-ts5500.c
+++ drivers/gpio/gpio-ts5500.c
@@ -23,7 +23,7 @@
  */
 
 #include <linux/bitops.h>
-#include <linux/gpio.h>
+#include <linux/gpio/driver.h>
 #include <linux/io.h>
 #include <linux/module.h>
 #include <linux/platform_data/gpio-ts5500.h>
