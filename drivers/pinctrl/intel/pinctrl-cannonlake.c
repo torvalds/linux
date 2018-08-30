05a100e4ac7d338121351eb8673b5d93f0317322
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2018-08-30 19:27:45 +0300

    pinctrl: cannonlake: Define PM ops via INTEL_PINCTRL_PM_OPS()
    Instead of open coding same structure definition for PM operations,
    replace it with a common macro.
    
    No functional change intended.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Acked-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/intel/pinctrl-cannonlake.c
+++ drivers/pinctrl/intel/pinctrl-cannonlake.c
@@ -832,10 +832,7 @@ static int cnl_pinctrl_probe(struct platform_device *pdev)
 	return intel_pinctrl_probe_by_hid(pdev);
 }
 
-static const struct dev_pm_ops cnl_pinctrl_pm_ops = {
-	SET_LATE_SYSTEM_SLEEP_PM_OPS(intel_pinctrl_suspend,
-				     intel_pinctrl_resume)
-};
+static INTEL_PINCTRL_PM_OPS(cnl_pinctrl_pm_ops);
 
 static struct platform_driver cnl_pinctrl_driver = {
 	.probe = cnl_pinctrl_probe,
