c36219d9d8df11641d28c6bd0698459485b1709b
Andrew F. Davis <afd@ti.com>
2018-08-31 14:13:25 -0500

    gpio: davinci: Remove unneeded GPIO macro
    This macro does nothing and has only one user, remove it.
    
    Signed-off-by: Andrew F. Davis <afd@ti.com>
    Tested-by: Keerthy <j-keerthy@ti.com>
    Acked-by: Keerthy <j-keerthy@ti.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- include/linux/platform_data/gpio-davinci.h
+++ include/linux/platform_data/gpio-davinci.h
@@ -45,11 +45,6 @@ struct davinci_gpio_controller {
 	int			irqs[MAX_INT_PER_BANK];
 };
 
-/*
- * basic gpio routines
- */
-#define	GPIO(X)		(X)	/* 0 <= X <= (DAVINCI_N_GPIO - 1) */
-
 /* Convert GPIO signal to GPIO pin number */
 #define GPIO_TO_PIN(bank, gpio)	(16 * (bank) + (gpio))
 
