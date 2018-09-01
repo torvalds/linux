6cfdb150e3fca6cd84a79116451071d6f3759946
Tomasz Duszynski <tduszyns@gmail.com>
2018-09-01 21:52:14 +0200

    iio: pressure: ms5611: switch to SPDX identifier
    Drop boilerplate license text and use SPDX identifier instead.
    
    Signed-off-by: Tomasz Duszynski <tduszyns@gmail.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/pressure/ms5611_i2c.c
+++ drivers/iio/pressure/ms5611_i2c.c
@@ -1,11 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * MS5611 pressure and temperature sensor driver (I2C bus)
  *
  * Copyright (c) Tomasz Duszynski <tduszyns@gmail.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  *
  * 7-bit I2C slave addresses:
  *
