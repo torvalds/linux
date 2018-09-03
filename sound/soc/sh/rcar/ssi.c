c7cf0304d41f68b47b3eaf6ef2f8ca9c3debfefc
Jiada Wang <jiada_wang@mentor.com>
2018-09-03 07:08:58 +0000

    ASoC: rsnd: fixup not to call clk_get/set under non-atomic
    commit 4d230d12710646788af581ba0155d83ab48b955c upstream.
    
    Clocking operations clk_get/set_rate, are non-atomic,
    they shouldn't be called in soc_pcm_trigger() which is atomic.
    
    Following issue was found due to execution of clk_get_rate() causes
    sleep in soc_pcm_trigger(), which shouldn't be blocked.
    
    We can reproduce this issue by following
    	> enable CONFIG_DEBUG_ATOMIC_SLEEP=y
    	> compile, and boot
    	> mount -t debugfs none /sys/kernel/debug
    	> while true; do cat /sys/kernel/debug/clk/clk_summary > /dev/null; done &
    	> while true; do aplay xxx; done
    
    This patch adds support to .prepare callback, and moves non-atomic
    clocking operations to it. As .prepare is non-atomic, it is always
    called before trigger_start/trigger_stop.
    
    	BUG: sleeping function called from invalid context at kernel/locking/mutex.c:620
    	in_atomic(): 1, irqs_disabled(): 128, pid: 2242, name: aplay
    	INFO: lockdep is turned off.
    	irq event stamp: 5964
    	hardirqs last enabled at (5963): [<ffff200008e59e40>] mutex_lock_nested+0x6e8/0x6f0
    	hardirqs last disabled at (5964): [<ffff200008e623f0>] _raw_spin_lock_irqsave+0x24/0x68
    	softirqs last enabled at (5502): [<ffff200008081838>] __do_softirq+0x560/0x10c0
    	softirqs last disabled at (5495): [<ffff2000080c2e78>] irq_exit+0x160/0x25c
    	Preemption disabled at:[ 62.904063] [<ffff200008be4d48>] snd_pcm_stream_lock+0xb4/0xc0
    	CPU: 2 PID: 2242 Comm: aplay Tainted: G B C 4.9.54+ #186
    	Hardware name: Renesas Salvator-X board based on r8a7795 (DT)
    	Call trace:
    	[<ffff20000808fe48>] dump_backtrace+0x0/0x37c
    	[<ffff2000080901d8>] show_stack+0x14/0x1c
    	[<ffff2000086f4458>] dump_stack+0xfc/0x154
    	[<ffff2000081134a0>] ___might_sleep+0x57c/0x58c
    	[<ffff2000081136b8>] __might_sleep+0x208/0x21c
    	[<ffff200008e5980c>] mutex_lock_nested+0xb4/0x6f0
    	[<ffff2000087cac74>] clk_prepare_lock+0xb0/0x184
    	[<ffff2000087cb094>] clk_core_get_rate+0x14/0x54
    	[<ffff2000087cb0f4>] clk_get_rate+0x20/0x34
    	[<ffff20000113aa00>] rsnd_adg_ssi_clk_try_start+0x158/0x4f8 [snd_soc_rcar]
    	[<ffff20000113da00>] rsnd_ssi_init+0x668/0x7a0 [snd_soc_rcar]
    	[<ffff200001133ff4>] rsnd_soc_dai_trigger+0x4bc/0xcf8 [snd_soc_rcar]
    	[<ffff200008c1af24>] soc_pcm_trigger+0x2a4/0x2d4
    
    Fixes: e7d850dd10f4 ("ASoC: rsnd: use mod base common method on SSI-parent")
    Signed-off-by: Jiada Wang <jiada_wang@mentor.com>
    Signed-off-by: Timo Wischer <twischer@de.adit-jv.com>
    [Kuninori: tidyup for upstream]
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Tested-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Cc: stable@vger.kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- sound/soc/sh/rcar/ssi.c
+++ sound/soc/sh/rcar/ssi.c
@@ -280,7 +280,7 @@ static int rsnd_ssi_master_clk_start(struct rsnd_mod *mod,
 	if (rsnd_ssi_is_multi_slave(mod, io))
 		return 0;
 
-	if (ssi->usrcnt > 1) {
+	if (ssi->rate) {
 		if (ssi->rate != rate) {
 			dev_err(dev, "SSI parent/child should use same rate\n");
 			return -EINVAL;
@@ -482,7 +482,6 @@ static int rsnd_ssi_init(struct rsnd_mod *mod,
 			 struct rsnd_priv *priv)
 {
 	struct rsnd_ssi *ssi = rsnd_mod_to_ssi(mod);
-	int ret;
 
 	if (!rsnd_ssi_is_run_mods(mod, io))
 		return 0;
@@ -492,10 +491,6 @@ static int rsnd_ssi_init(struct rsnd_mod *mod,
 	ssi->usrcnt++;
 
 	rsnd_mod_power_on(mod);
-
-	ret = rsnd_ssi_master_clk_start(mod, io);
-	if (ret < 0)
-		return ret;
 
 	rsnd_ssi_config_init(mod, io);
 
@@ -847,6 +842,13 @@ static int rsnd_ssi_pointer(struct rsnd_mod *mod,
 	return 0;
 }
 
+static int rsnd_ssi_prepare(struct rsnd_mod *mod,
+			    struct rsnd_dai_stream *io,
+			    struct rsnd_priv *priv)
+{
+	return rsnd_ssi_master_clk_start(mod, io);
+}
+
 static struct rsnd_mod_ops rsnd_ssi_pio_ops = {
 	.name	= SSI_NAME,
 	.probe	= rsnd_ssi_common_probe,
@@ -859,6 +861,7 @@ static struct rsnd_mod_ops rsnd_ssi_pio_ops = {
 	.pointer= rsnd_ssi_pointer,
 	.pcm_new = rsnd_ssi_pcm_new,
 	.hw_params = rsnd_ssi_hw_params,
+	.prepare = rsnd_ssi_prepare,
 };
 
 static int rsnd_ssi_dma_probe(struct rsnd_mod *mod,
@@ -935,6 +938,7 @@ static struct rsnd_mod_ops rsnd_ssi_dma_ops = {
 	.pcm_new = rsnd_ssi_pcm_new,
 	.fallback = rsnd_ssi_fallback,
 	.hw_params = rsnd_ssi_hw_params,
+	.prepare = rsnd_ssi_prepare,
 };
 
 int rsnd_ssi_is_dma_mode(struct rsnd_mod *mod)
