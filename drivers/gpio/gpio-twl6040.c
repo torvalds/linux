fc4f8f322f6e588edbecfaa82a987d46125aeb1f
Linus Walleij <linus.walleij@linaro.org>
2018-09-03 09:54:55 +0200

    gpio: twl6040: Include the right header
    This is a GPIO driver so include only <linux/gpio/driver.h>.
    
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-twl6040.c
+++ drivers/gpio/gpio-twl6040.c
@@ -26,7 +26,7 @@
 #include <linux/init.h>
 #include <linux/kthread.h>
 #include <linux/irq.h>
-#include <linux/gpio.h>
+#include <linux/gpio/driver.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
 
