328b5f417a4ac929e20d98b989a2babac66c8520
Joe Perches <joe@perches.com>
2018-09-04 15:46:06 -0700

    checkpatch: add optional static const to blank line declarations test
    Using a static const struct definition as part of a series of
    declarations produces a false positive "Missing a blank line after
    declarations" for code like:
    
      WARNING: Missing a blank line after declarations
      #710: FILE: drivers/gpu/drm/tidss/tidss_scale_coefs.c:137:
      +       int inc;
      +       static const struct {
    
    So fix it.
    
    Link: http://lkml.kernel.org/r/5905126e70b0ed1781e49265fd5c49c5090d0223.camel@perches.com
    Signed-off-by: Joe Perches <joe@perches.com>
    Reported-by: Jyri Sarha <jsarha@ti.com>
    Cc: "Valkeinen, Tomi" <tomi.valkeinen@ti.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -3311,7 +3311,7 @@ sub process {
 			# known declaration macros
 		      $sline =~ /^\+\s+$declaration_macros/ ||
 			# start of struct or union or enum
-		      $sline =~ /^\+\s+(?:union|struct|enum|typedef)\b/ ||
+		      $sline =~ /^\+\s+(?:static\s+)?(?:const\s+)?(?:union|struct|enum|typedef)\b/ ||
 			# start or end of block or continuation of declaration
 		      $sline =~ /^\+\s+(?:$|[\{\}\.\#\"\?\:\(\[])/ ||
 			# bitfield continuation
