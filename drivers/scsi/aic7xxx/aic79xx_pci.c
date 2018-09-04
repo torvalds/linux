5227388d599e4c8221193897ff10764b43ee26cb
YueHaibing <yuehaibing@huawei.com>
2018-09-04 03:36:01 +0000

    scsi: aic7xxx: remove set but not used variable 'shared_scb_data'
    Fixes gcc '-Wunused-but-set-variable' warning:
    
    drivers/scsi/aic7xxx/aic79xx_pci.c: In function 'ahd_pci_config':
    drivers/scsi/aic7xxx/aic79xx_pci.c:291:19: warning:
     variable 'shared_scb_data' set but not used [-Wunused-but-set-variable]
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/aic7xxx/aic79xx_pci.c
+++ drivers/scsi/aic7xxx/aic79xx_pci.c
@@ -288,13 +288,11 @@ ahd_find_pci_device(ahd_dev_softc_t pci)
 int
 ahd_pci_config(struct ahd_softc *ahd, const struct ahd_pci_identity *entry)
 {
-	struct scb_data *shared_scb_data;
 	u_int		 command;
 	uint32_t	 devconfig;
 	uint16_t	 subvendor; 
 	int		 error;
 
-	shared_scb_data = NULL;
 	ahd->description = entry->name;
 	/*
 	 * Record if this is an HP board.
