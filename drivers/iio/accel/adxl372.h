94dbb46c7a8f0ee27394bc81f34b666dc4a14b71
Stefan Popa <stefan.popa@analog.com>
2018-09-04 17:12:32 +0300

    iio: adxl372: Add support for I2C communication
    The adxl372 is designed to communicate in either SPI or I2C protocol. It
    autodetects the format being used, requiring no configuration control to
    select the format.
    
    Signed-off-by: Stefan Popa <stefan.popa@analog.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/accel/adxl372.h
+++ drivers/iio/accel/adxl372.h
@@ -8,6 +8,8 @@
 #ifndef _ADXL372_H_
 #define _ADXL372_H_
 
+#define ADXL372_REVID	0x03
+
 int adxl372_probe(struct device *dev, struct regmap *regmap,
 		  int irq, const char *name);
 bool adxl372_readable_noinc_reg(struct device *dev, unsigned int reg);
