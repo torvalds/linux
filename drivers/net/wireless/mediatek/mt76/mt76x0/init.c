b6f66e8b227fd0292e3a75c8e7799f4c2f90dbaf
Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
2018-09-06 11:18:41 +0200

    mt76x0: init hw capabilities
    commit 0ae976a11b4fb5704b597e103b5189237641c1a1 upstream.
    
    Enable hw capabilities supported by mt76-usb layer
    - fast_xmit
    - tx/rx amsdu
    - MFP
    - non-linear tx skbs
    
    [This is one line hw feature backport from 0ae976a11b4f ("mt76x0: init
    hw capabilities"), which add also other different features, however
    those are not supported in 4.19.
    
    802.11w is supported by mac80211 and mt76x0u driver in 4.19 correctly
    fall-back to software encryption when 802.11w ciphers are used.
    
    Without the patch we fail to associate with WPA3 APs, so this is
    considered as fix.]
    
    Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    [remove marking non-working features on 4.19, make topic correspond the change]
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/wireless/mediatek/mt76/mt76x0/init.c
+++ drivers/net/wireless/mediatek/mt76/mt76x0/init.c
@@ -681,6 +681,7 @@ int mt76x0_register_device(struct mt76x0_dev *dev)
 	ieee80211_hw_set(hw, SUPPORTS_HT_CCK_RATES);
 	ieee80211_hw_set(hw, AMPDU_AGGREGATION);
 	ieee80211_hw_set(hw, SUPPORTS_RC_TABLE);
+	ieee80211_hw_set(hw, MFP_CAPABLE);
 	hw->max_rates = 1;
 	hw->max_report_rates = 7;
 	hw->max_rate_tries = 1;
