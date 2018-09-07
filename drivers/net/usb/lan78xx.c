b4de9ac2c750127c2d8bc9a737eca611405e1171
Ben Hutchings <ben.hutchings@codethink.co.uk>
2018-09-07 01:13:40 +0100

    net: lan78xx: Fix misplaced tasklet_schedule() call
    Commit 136f55f66019 ("net: lan78xx: fix rx handling before first
    packet is send") was not correctly backported to 4.4.  The call to
    tasklet_schedule() belongs in lan78xx_link_reset().
    
    Fixes: d1fc12d8475c ("net: lan78xx: fix rx handling before first packet is send")
    Signed-off-by: Ben Hutchings <ben.hutchings@codethink.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/usb/lan78xx.c
+++ drivers/net/usb/lan78xx.c
@@ -902,6 +902,8 @@ static int lan78xx_link_reset(struct lan78xx_net *dev)
 
 		ret = lan78xx_update_flowcontrol(dev, ecmd.duplex, ladv, radv);
 		netif_carrier_on(dev->net);
+
+		tasklet_schedule(&dev->bh);
 	}
 
 	return ret;
@@ -1361,8 +1363,6 @@ static void lan78xx_init_mac_address(struct lan78xx_net *dev)
 			netif_dbg(dev, ifup, dev->net,
 				  "MAC address set to random addr");
 		}
-
-		tasklet_schedule(&dev->bh);
 	}
 
 	ret = lan78xx_write_reg(dev, MAF_LO(0), addr_lo);
