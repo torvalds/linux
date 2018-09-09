70165a445b00c7bb0f68c955da3c100817ac0c87
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2018-09-09 22:25:52 +0900

    ALSA: fireface: fix memory leak in ff400_switch_fetching_mode()
    commit 36f3a6e02c143a7e9e4e143e416371f67bc1fae6 upstream.
    
    An allocated memory forgets to be released.
    
    Fixes: 76fdb3a9e13 ('ALSA: fireface: add support for Fireface 400')
    Cc: <stable@vger.kernel.org> # 4.12+
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- sound/firewire/fireface/ff-protocol-ff400.c
+++ sound/firewire/fireface/ff-protocol-ff400.c
@@ -146,6 +146,7 @@ static int ff400_switch_fetching_mode(struct snd_ff *ff, bool enable)
 {
 	__le32 *reg;
 	int i;
+	int err;
 
 	reg = kzalloc(sizeof(__le32) * 18, GFP_KERNEL);
 	if (reg == NULL)
@@ -163,9 +164,11 @@ static int ff400_switch_fetching_mode(struct snd_ff *ff, bool enable)
 			reg[i] = cpu_to_le32(0x00000001);
 	}
 
-	return snd_fw_transaction(ff->unit, TCODE_WRITE_BLOCK_REQUEST,
-				  FF400_FETCH_PCM_FRAMES, reg,
-				  sizeof(__le32) * 18, 0);
+	err = snd_fw_transaction(ff->unit, TCODE_WRITE_BLOCK_REQUEST,
+				 FF400_FETCH_PCM_FRAMES, reg,
+				 sizeof(__le32) * 18, 0);
+	kfree(reg);
+	return err;
 }
 
 static void ff400_dump_sync_status(struct snd_ff *ff,
