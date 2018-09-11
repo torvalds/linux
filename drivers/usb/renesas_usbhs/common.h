794f97a4b964a5f9880529a561ab26ed781bca56
Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
2018-09-11 17:47:05 +0900

    usb: renesas_usbhs: Add multiple clocks management
    R-Car Gen3 needs to enable clocks of both host and peripheral.
    Since [eo]hci-platform disables the reset(s) when the drivers are
    removed, renesas_usbhs driver doesn't work correctly. To fix this
    issue, this patch adds multiple clocks management on this
    renesas_usbhs driver.
    
    Signed-off-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/renesas_usbhs/common.h
+++ drivers/usb/renesas_usbhs/common.h
@@ -8,6 +8,7 @@
 #ifndef RENESAS_USB_DRIVER_H
 #define RENESAS_USB_DRIVER_H
 
+#include <linux/clk.h>
 #include <linux/extcon.h>
 #include <linux/platform_device.h>
 #include <linux/reset.h>
@@ -278,6 +279,7 @@ struct usbhs_priv {
 
 	struct phy *phy;
 	struct reset_control *rsts;
+	struct clk *clks[2];
 };
 
 /*
