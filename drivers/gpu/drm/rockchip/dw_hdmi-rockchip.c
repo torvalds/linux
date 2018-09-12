d28c45ee343db5617455253fa8cd9ee615f6b31a
Heiko Stuebner <heiko@sntech.de>
2018-09-12 14:47:36 +0200

    UPSTREAM: drm/rockchip: dw_hdmi: Allow outputs that don't need output switching
    So far we always encountered socs with 2 output crtcs needing the driver
    to tell the hdmi block which output to connect to. But there also exist
    socs with only one crtc like the rk3228, rk3328 and rk3368.
    
    So adapt the register field to simply carry a negative value to signal
    that no output-switching is necessary.
    
    changes in v3:
    - fixed wording issue found by Robin Murphy
    
    Change-Id: I368645dd1f07eedb801363ae65824ba876d66560
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Tested-by: Robin Murphy <robin.murphy@arm.com>
    Reviewed-by: Zheng Yang <zhengyang@rock-chips.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180912124740.20343-3-heiko@sntech.de
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>
    (cherry picked from commit 96c4704fec95db487432a72cbe244c1db2e090bc)

--- drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
+++ drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
@@ -36,7 +36,7 @@
  * @lcdsel_lit: reg value of selecting vop little for HDMI
  */
 struct rockchip_hdmi_chip_data {
-	u32	lcdsel_grf_reg;
+	int	lcdsel_grf_reg;
 	u32	lcdsel_big;
 	u32	lcdsel_lit;
 };
@@ -244,6 +244,9 @@ static void dw_hdmi_rockchip_encoder_enable(struct drm_encoder *encoder)
 	struct rockchip_hdmi *hdmi = to_rockchip_hdmi(encoder);
 	u32 val;
 	int ret;
+
+	if (hdmi->chip_data->lcdsel_grf_reg < 0)
+		return;
 
 	ret = drm_of_encoder_active_endpoint_id(hdmi->dev->of_node, encoder);
 	if (ret)
