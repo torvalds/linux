36cf35b7864002c2601e4bda4d78d5622ad92544
Paul Kocialkowski <paul.kocialkowski@bootlin.com>
2018-09-13 10:51:53 -0400

    media: v4l: Add definition for the Sunxi tiled NV12 format
    This introduces support for the Sunxi tiled NV12 format, where each
    component of the YUV frame is divided into macroblocks. Hence, the size
    of each plane requires specific alignment. The pixels inside each
    macroblock are coded in linear order (line after line from top to
    bottom).
    
    This tiled NV12 format is used by the video engine on Allwinner
    platforms: it is the default format for decoded frames (and the only
    one available in the oldest supported platforms).
    
    Signed-off-by: Paul Kocialkowski <paul.kocialkowski@bootlin.com>
    Signed-off-by: Maxime Ripard <maxime.ripard@bootlin.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/v4l2-core/v4l2-ioctl.c
+++ drivers/media/v4l2-core/v4l2-ioctl.c
@@ -1337,6 +1337,7 @@ static void v4l_fill_fmtdesc(struct v4l2_fmtdesc *fmt)
 		case V4L2_PIX_FMT_SE401:	descr = "GSPCA SE401"; break;
 		case V4L2_PIX_FMT_S5C_UYVY_JPG:	descr = "S5C73MX interleaved UYVY/JPEG"; break;
 		case V4L2_PIX_FMT_MT21C:	descr = "Mediatek Compressed Format"; break;
+		case V4L2_PIX_FMT_SUNXI_TILED_NV12: descr = "Sunxi Tiled NV12 Format"; break;
 		default:
 			WARN(1, "Unknown pixelformat 0x%08x\n", fmt->pixelformat);
 			if (fmt->description[0])
