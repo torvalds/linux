fe8169a722c5b42240dd04cadc02d5085f2f48fb
Tao Huang <huangtao@rock-chips.com>
2018-09-13 15:17:27 +0800

    rk: gcc-wrapper.py ignore halphyrf_8188e_ce.c:2208 and halphyrf_8723b_ce.c:2879
    trigger by ARM_LPAE
    
    Change-Id: I941ec62d2b9e1fe78af6807f63e2aa1d73cc0e41
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -54,6 +54,8 @@ allowed_warnings = set([
     "bounds.c:15", # kernel/bounds.c:15:6: warning: no previous prototype for ‘foo’
     "cpufeature.h:157", # arch/arm64/include/asm/cpufeature.h:157:68: warning: signed and unsigned type in conditional expression
     "sched.h:1211", # include/linux/sched.h:1211:1: warning: type qualifiers ignored on function return type
+    "halphyrf_8188e_ce.c:2208", # drivers/net/wireless/rockchip_wlan/rtl8189es/hal/phydm/rtl8188e/halphyrf_8188e_ce.c:2208:1: warning: the frame size of 1056 bytes is larger than 1024 bytes
+    "halphyrf_8723b_ce.c:2879", # drivers/net/wireless/rockchip_wlan/rtl8723bu/hal/phydm/rtl8723b/halphyrf_8723b_ce.c:2879:1: warning: the frame size of 1056 bytes is larger than 1024 bytes
  ])
 
 # Capture the name of the object file, can find it.
