659902db7008f233e3d514c774b5ff3d46c8edd8
zhong jiang <zhongjiang@huawei.com>
2018-09-14 13:04:37 +0800

    ieee802154: ca8210: remove redundant condition check before debugfs_remove
    debugfs_remove has taken the IS_ERR into account. Just
    remove the unnecessary condition.
    
    Signed-off-by: zhong jiang <zhongjiang@huawei.com>
    Signed-off-by: Stefan Schmidt <stefan@datenfreihafen.org>

--- drivers/net/ieee802154/ca8210.c
+++ drivers/net/ieee802154/ca8210.c
@@ -3043,8 +3043,7 @@ static void ca8210_test_interface_clear(struct ca8210_priv *priv)
 {
 	struct ca8210_test *test = &priv->test;
 
-	if (!IS_ERR(test->ca8210_dfs_spi_int))
-		debugfs_remove(test->ca8210_dfs_spi_int);
+	debugfs_remove(test->ca8210_dfs_spi_int);
 	kfifo_free(&test->up_fifo);
 	dev_info(&priv->spi->dev, "Test interface removed\n");
 }
