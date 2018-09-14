57d8df68eb53cc15e5bdfc14bfb28a18543109eb
Mika Westerberg <mika.westerberg@linux.intel.com>
2018-09-14 12:58:37 +0300

    thunderbolt: Add support for Intel Tiger Lake
    Tiger Lake integrated Thunderbolt/USB4 controller is quite close to
    Intel Ice Lake. By default it is still using firmware based connection
    manager so we can use most of the Ice Lake flows in Tiger Lake as well.
    We check if the firmware connection manager is running and in that case
    use it, otherwise use the software based connection manager.
    
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Acked-by: Yehezkel Bernat <yehezkelshb@gmail.com>

--- drivers/thunderbolt/nhi.c
+++ drivers/thunderbolt/nhi.c
@@ -1270,6 +1270,10 @@ static struct pci_device_id nhi_ids[] = {
 	  .driver_data = (kernel_ulong_t)&icl_nhi_ops },
 	{ PCI_VDEVICE(INTEL, PCI_DEVICE_ID_INTEL_ICL_NHI1),
 	  .driver_data = (kernel_ulong_t)&icl_nhi_ops },
+	{ PCI_VDEVICE(INTEL, PCI_DEVICE_ID_INTEL_TGL_NHI0),
+	  .driver_data = (kernel_ulong_t)&icl_nhi_ops },
+	{ PCI_VDEVICE(INTEL, PCI_DEVICE_ID_INTEL_TGL_NHI1),
+	  .driver_data = (kernel_ulong_t)&icl_nhi_ops },
 
 	/* Any USB4 compliant host */
 	{ PCI_DEVICE_CLASS(PCI_CLASS_SERIAL_USB_USB4, ~0) },
