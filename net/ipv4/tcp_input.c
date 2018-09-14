e747775172a2d4dc4dae794f248f9687ba793f54
Eric Dumazet <edumazet@google.com>
2018-09-14 16:24:07 +0800

    tcp: fix a stale ooo_last_skb after a replace
    [ Upstream commit 76f0dcbb5ae1a7c3dbeec13dd98233b8e6b0b32a ]
    
    When skb replaces another one in ooo queue, I forgot to also
    update tp->ooo_last_skb as well, if the replaced skb was the last one
    in the queue.
    
    To fix this, we simply can re-use the code that runs after an insertion,
    trying to merge skbs at the right of current skb.
    
    This not only fixes the bug, but also remove all small skbs that might
    be a subset of the new one.
    
    Example:
    
    We receive segments 2001:3001,  4001:5001
    
    Then we receive 2001:8001 : We should replace 2001:3001 with the big
    skb, but also remove 4001:50001 from the queue to save space.
    
    packetdrill test demonstrating the bug
    
    0.000 socket(..., SOCK_STREAM, IPPROTO_TCP) = 3
    +0 setsockopt(3, SOL_SOCKET, SO_REUSEADDR, [1], 4) = 0
    +0 bind(3, ..., ...) = 0
    +0 listen(3, 1) = 0
    
    +0 < S 0:0(0) win 32792 <mss 1000,sackOK,nop,nop,nop,wscale 7>
    +0 > S. 0:0(0) ack 1 <mss 1460,nop,nop,sackOK,nop,wscale 7>
    +0.100 < . 1:1(0) ack 1 win 1024
    +0 accept(3, ..., ...) = 4
    
    +0.01 < . 1001:2001(1000) ack 1 win 1024
    +0    > . 1:1(0) ack 1 <nop,nop, sack 1001:2001>
    
    +0.01 < . 1001:3001(2000) ack 1 win 1024
    +0    > . 1:1(0) ack 1 <nop,nop, sack 1001:2001 1001:3001>
    
    Fixes: 9f5afeae5152 ("tcp: use an RB tree for ooo receive queue")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reported-by: Yuchung Cheng <ycheng@google.com>
    Cc: Yaogong Wang <wygivan@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Mao Wenan <maowenan@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -4465,7 +4465,7 @@ coalesce_done:
 				NET_INC_STATS(sock_net(sk),
 					      LINUX_MIB_TCPOFOMERGE);
 				__kfree_skb(skb1);
-				goto add_sack;
+				goto merge_right;
 			}
 		} else if (tcp_try_coalesce(sk, skb1, skb, &fragstolen)) {
 			goto coalesce_done;
@@ -4477,6 +4477,7 @@ coalesce_done:
 	rb_link_node(&skb->rbnode, parent, p);
 	rb_insert_color(&skb->rbnode, &tp->out_of_order_queue);
 
+merge_right:
 	/* Remove other segments covered by skb. */
 	while ((q = rb_next(&skb->rbnode)) != NULL) {
 		skb1 = rb_entry(q, struct sk_buff, rbnode);
