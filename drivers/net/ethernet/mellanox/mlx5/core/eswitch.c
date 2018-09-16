83eb2fdd0483ea9195fa4a46532e57b2df1b9974
Eran Ben Elisha <eranbe@mellanox.com>
2018-09-16 14:45:27 +0300

    net/mlx5: E-Switch, Fix out of bound access when setting vport rate
    [ Upstream commit 11aa5800ed66ed0415b7509f02881c76417d212a ]
    
    The code that deals with eswitch vport bw guarantee was going beyond the
    eswitch vport array limit, fix that.  This was pointed out by the kernel
    address sanitizer (KASAN).
    
    The error from KASAN log:
    [2018-09-15 15:04:45] BUG: KASAN: slab-out-of-bounds in
    mlx5_eswitch_set_vport_rate+0x8c1/0xae0 [mlx5_core]
    
    Fixes: c9497c98901c ("net/mlx5: Add support for setting VF min rate")
    Signed-off-by: Eran Ben Elisha <eranbe@mellanox.com>
    Reviewed-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
@@ -1922,7 +1922,7 @@ static u32 calculate_vports_min_rate_divider(struct mlx5_eswitch *esw)
 	u32 max_guarantee = 0;
 	int i;
 
-	for (i = 0; i <= esw->total_vports; i++) {
+	for (i = 0; i < esw->total_vports; i++) {
 		evport = &esw->vports[i];
 		if (!evport->enabled || evport->info.min_rate < max_guarantee)
 			continue;
@@ -1942,7 +1942,7 @@ static int normalize_vports_min_rate(struct mlx5_eswitch *esw, u32 divider)
 	int err;
 	int i;
 
-	for (i = 0; i <= esw->total_vports; i++) {
+	for (i = 0; i < esw->total_vports; i++) {
 		evport = &esw->vports[i];
 		if (!evport->enabled)
 			continue;
