1830dad34c070161fda2ff1db77b39ffa78aa380
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2018-09-16 16:45:46 -0700

    slimbus: ngd: register ngd driver only once.
    Move ngd platform driver out of loop so that it registers only once.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/slimbus/qcom-ngd-ctrl.c
+++ drivers/slimbus/qcom-ngd-ctrl.c
@@ -1342,7 +1342,6 @@ static int of_qcom_slim_ngd_register(struct device *parent,
 		ngd->base = ctrl->base + ngd->id * data->offset +
 					(ngd->id - 1) * data->size;
 		ctrl->ngd = ngd;
-		platform_driver_register(&qcom_slim_ngd_driver);
 
 		return 0;
 	}
@@ -1441,6 +1440,7 @@ static int qcom_slim_ngd_ctrl_probe(struct platform_device *pdev)
 	init_completion(&ctrl->reconf);
 	init_completion(&ctrl->qmi.qmi_comp);
 
+	platform_driver_register(&qcom_slim_ngd_driver);
 	return of_qcom_slim_ngd_register(dev, ctrl);
 }
 
