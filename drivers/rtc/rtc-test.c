c7080e2016074363298e05d5e4d91eaf243a58c2
Alexandre Belloni <alexandre.belloni@bootlin.com>
2018-09-19 03:16:32 +0200

    rtc: test: Switch to SPDX identifier
    Replace the license boilerplate by an SPDX identifier
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/rtc/rtc-test.c
+++ drivers/rtc/rtc-test.c
@@ -1,11 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * An RTC test device/driver
  * Copyright (C) 2005 Tower Technologies
  * Author: Alessandro Zummo <a.zummo@towertech.it>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  */
 
 #include <linux/module.h>
