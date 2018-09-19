df43519eb706edfe951284a825642ce2e1d38d09
Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
2018-09-19 10:14:00 +0300

    regulator: bd718x7: add missing linux/of.h inclusion
    0-Day tests found compilation error on x86. Driver won't
    compile on x86_64 as "of_match_ptr" is not found. Add missing
    include <linux/of.h>
    
    Signed-off-by: Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/bd71837-regulator.c
+++ drivers/regulator/bd71837-regulator.c
@@ -9,6 +9,7 @@
 #include <linux/kernel.h>
 #include <linux/mfd/rohm-bd718x7.h>
 #include <linux/module.h>
+#include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/regulator/driver.h>
 #include <linux/regulator/machine.h>
