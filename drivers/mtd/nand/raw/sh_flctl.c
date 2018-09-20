ed896ddfae942b6a87bf3a273f88a11d8f85317d
Nathan Chancellor <natechancellor@gmail.com>
2018-09-20 16:30:25 -0700

    mtd: rawnand: sh_flctl: Use proper enum for flctl_dma_fifo0_transfer
    [ Upstream commit e2bfa4ca23d9b5a7bdfcf21319fad9b59e38a05c ]
    
    Clang warns when one enumerated type is converted implicitly to another:
    
    drivers/mtd/nand/raw/sh_flctl.c:483:46: warning: implicit conversion
    from enumeration type 'enum dma_transfer_direction' to different
    enumeration type 'enum dma_data_direction' [-Wenum-conversion]
                    flctl_dma_fifo0_transfer(flctl, buf, rlen, DMA_DEV_TO_MEM) > 0)
                    ~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~
    drivers/mtd/nand/raw/sh_flctl.c:542:46: warning: implicit conversion
    from enumeration type 'enum dma_transfer_direction' to different
    enumeration type 'enum dma_data_direction' [-Wenum-conversion]
                    flctl_dma_fifo0_transfer(flctl, buf, rlen, DMA_MEM_TO_DEV) > 0)
                    ~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~
    2 warnings generated.
    
    Use the proper enums from dma_data_direction to satisfy Clang.
    
    DMA_MEM_TO_DEV = DMA_TO_DEVICE = 1
    DMA_DEV_TO_MEM = DMA_FROM_DEVICE = 2
    
    Reported-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Reviewed-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/mtd/nand/raw/sh_flctl.c
+++ drivers/mtd/nand/raw/sh_flctl.c
@@ -480,7 +480,7 @@ static void read_fiforeg(struct sh_flctl *flctl, int rlen, int offset)
 
 	/* initiate DMA transfer */
 	if (flctl->chan_fifo0_rx && rlen >= 32 &&
-		flctl_dma_fifo0_transfer(flctl, buf, rlen, DMA_DEV_TO_MEM) > 0)
+		flctl_dma_fifo0_transfer(flctl, buf, rlen, DMA_FROM_DEVICE) > 0)
 			goto convert;	/* DMA success */
 
 	/* do polling transfer */
@@ -539,7 +539,7 @@ static void write_ec_fiforeg(struct sh_flctl *flctl, int rlen,
 
 	/* initiate DMA transfer */
 	if (flctl->chan_fifo0_tx && rlen >= 32 &&
-		flctl_dma_fifo0_transfer(flctl, buf, rlen, DMA_MEM_TO_DEV) > 0)
+		flctl_dma_fifo0_transfer(flctl, buf, rlen, DMA_TO_DEVICE) > 0)
 			return;	/* DMA success */
 
 	/* do polling transfer */
