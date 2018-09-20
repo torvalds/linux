97f329748f81338f2c4260ebc99b8ae9c5710e73
Suzuki K Poulose <suzuki.poulose@arm.com>
2018-09-20 13:17:55 -0600

    UPSTREAM: coresight: perf: Add helper to retrieve sink configuration
    (Upstream commit d25054ee8d18c937058a1b69b35fa5bfdef471f3).
    
    We can always find the sink configuration for a given perf_output_handle.
    Add a helper to retrieve the sink configuration for a given
    perf_output_handle. This will be used to get rid of the set_buffer()
    call back.
    
    Cc: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Bug: 140266694
    Change-Id: I0a9d8d9d8370fea5e4245697dca1e510d3f9438c
    Signed-off-by: Yabin Cui <yabinc@google.com>

--- drivers/hwtracing/coresight/coresight-etm-perf.c
+++ drivers/hwtracing/coresight/coresight-etm-perf.c
@@ -23,20 +23,6 @@
 static struct pmu etm_pmu;
 static bool etm_perf_up;
 
-/**
- * struct etm_event_data - Coresight specifics associated to an event
- * @work:		Handle to free allocated memory outside IRQ context.
- * @mask:		Hold the CPU(s) this event was set for.
- * @snk_config:		The sink configuration.
- * @path:		An array of path, each slot for one CPU.
- */
-struct etm_event_data {
-	struct work_struct work;
-	cpumask_t mask;
-	void *snk_config;
-	struct list_head * __percpu *path;
-};
-
 static DEFINE_PER_CPU(struct perf_output_handle, ctx_handle);
 static DEFINE_PER_CPU(struct coresight_device *, csdev_src);
 
