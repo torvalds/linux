8169671ababf306e22f8cf064184032ff74eb508
Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
2018-09-21 10:21:00 +0300

    drm/i915: Update DRIVER_DATE to 20180921
    Signed-off-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -86,8 +86,8 @@
 
 #define DRIVER_NAME		"i915"
 #define DRIVER_DESC		"Intel Graphics"
-#define DRIVER_DATE		"20180906"
-#define DRIVER_TIMESTAMP	1536242083
+#define DRIVER_DATE		"20180921"
+#define DRIVER_TIMESTAMP	1537514460
 
 /* Use I915_STATE_WARN(x) and I915_STATE_WARN_ON() (rather than WARN() and
  * WARN_ON()) for hw state sanity checks to check for unexpected conditions
