0ff9f49646353ce31312411e7e7bd2281492a40e
John Garry <john.garry@huawei.com>
2018-09-22 01:25:26 +0800

    drm/hisilicon: hibmc: Don't overwrite fb helper surface depth
    Currently the driver overwrites the surface depth provided by the fb
    helper to give an invalid bpp/surface depth combination.
    
    This has been exposed by commit 70109354fed2 ("drm: Reject unknown legacy
    bpp and depth for drm_mode_addfb ioctl"), which now causes the driver to
    fail to probe.
    
    Fix by not overwriting the surface depth.
    
    Fixes: d1667b86795a ("drm/hisilicon/hibmc: Add support for frame buffer")
    Signed-off-by: John Garry <john.garry@huawei.com>
    Reviewed-by: Xinliang Liu <z.liuxinliang@hisilicon.com>
    Signed-off-by: Xinliang Liu <z.liuxinliang@hisilicon.com>

--- drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_fbdev.c
+++ drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_fbdev.c
@@ -71,7 +71,6 @@ static int hibmc_drm_fb_create(struct drm_fb_helper *helper,
 	DRM_DEBUG_DRIVER("surface width(%d), height(%d) and bpp(%d)\n",
 			 sizes->surface_width, sizes->surface_height,
 			 sizes->surface_bpp);
-	sizes->surface_depth = 32;
 
 	bytes_per_pixel = DIV_ROUND_UP(sizes->surface_bpp, 8);
 
