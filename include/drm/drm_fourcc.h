059b5eb5d9554dea689430ad3872dcb37a35053d
Gerd Hoffmann <kraxel@redhat.com>
2018-09-21 15:46:59 +0200

    drm: move native byte order quirk to new drm_driver_legacy_fb_format function
    Turns out we need the pixel format fixup not only for the addfb ioctl,
    but also for fbdev emulation code.
    
    Ideally we would place it in drm_mode_legacy_fb_format().  That would
    create alot of churn though, and most drivers don't care because they
    never ever run on a big endian platform.  So add a new
    drm_driver_legacy_fb_format() function instead which looks at the
    mode_config->quirk_addfb_prefer_host_byte_order flag.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/20180921134704.12826-2-kraxel@redhat.com

--- include/drm/drm_fourcc.h
+++ include/drm/drm_fourcc.h
@@ -88,6 +88,8 @@ const struct drm_format_info *
 drm_get_format_info(struct drm_device *dev,
 		    const struct drm_mode_fb_cmd2 *mode_cmd);
 uint32_t drm_mode_legacy_fb_format(uint32_t bpp, uint32_t depth);
+uint32_t drm_driver_legacy_fb_format(struct drm_device *dev,
+				     uint32_t bpp, uint32_t depth);
 int drm_format_num_planes(uint32_t format);
 int drm_format_plane_cpp(uint32_t format, int plane);
 int drm_format_horz_chroma_subsampling(uint32_t format);
