1668e3b6f8f8ed2ce685691c92b90dfadeaa3f2f
Luo Jiaxing <luojiaxing@huawei.com>
2018-09-24 23:06:29 +0800

    scsi: hisi_sas: Move evaluation of hisi_hba in hisi_sas_task_prep()
    In evaluating hisi_hba, the sas_port may be NULL, so for safety relocate
    the the check to value possible NULL deference.
    
    Signed-off-by: Luo Jiaxing <luojiaxing@huawei.com>
    Signed-off-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/hisi_sas/hisi_sas_main.c
+++ drivers/scsi/hisi_sas/hisi_sas_main.c
@@ -287,13 +287,13 @@ static int hisi_sas_task_prep(struct sas_task *task,
 			      int *pass)
 {
 	struct domain_device *device = task->dev;
-	struct hisi_hba *hisi_hba = dev_to_hisi_hba(device);
+	struct hisi_hba *hisi_hba;
 	struct hisi_sas_device *sas_dev = device->lldd_dev;
 	struct hisi_sas_port *port;
 	struct hisi_sas_slot *slot;
 	struct hisi_sas_cmd_hdr	*cmd_hdr_base;
 	struct asd_sas_port *sas_port = device->port;
-	struct device *dev = hisi_hba->dev;
+	struct device *dev;
 	int dlvry_queue_slot, dlvry_queue, rc, slot_idx;
 	int n_elem = 0, n_elem_req = 0, n_elem_resp = 0;
 	struct hisi_sas_dq *dq;
@@ -313,6 +313,9 @@ static int hisi_sas_task_prep(struct sas_task *task,
 			task->task_done(task);
 		return -ECOMM;
 	}
+
+	hisi_hba = dev_to_hisi_hba(device);
+	dev = hisi_hba->dev;
 
 	if (DEV_IS_GONE(sas_dev)) {
 		if (sas_dev)
