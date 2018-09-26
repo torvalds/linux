15b796bc3d5cc8f776f5cb0770f9f3e8b282e4d1
Kaike Wan <kaike.wan@intel.com>
2018-09-26 10:27:03 -0700

    IB/hfi1: Add static trace for iowait
    This patch adds the static trace for resource wait.
    
    Reviewed-by: Mitko Haralanov <mitko.haralanov@intel.com>
    Signed-off-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
    Signed-off-by: Kaike Wan <kaike.wan@intel.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- drivers/infiniband/hw/hfi1/iowait.c
+++ drivers/infiniband/hw/hfi1/iowait.c
@@ -4,9 +4,11 @@
  *
  */
 #include "iowait.h"
+#include "trace_iowait.h"
 
 void iowait_set_flag(struct iowait *wait, u32 flag)
 {
+	trace_hfi1_iowait_set(wait, flag);
 	set_bit(flag, &wait->flags);
 }
 
@@ -17,6 +19,7 @@ bool iowait_flag_set(struct iowait *wait, u32 flag)
 
 inline void iowait_clear_flag(struct iowait *wait, u32 flag)
 {
+	trace_hfi1_iowait_clear(wait, flag);
 	clear_bit(flag, &wait->flags);
 }
 
