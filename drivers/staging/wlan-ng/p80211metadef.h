88e3676ad5de67ba9048289e95115644c22815fd
Tim Collier <osdevtc@gmail.com>
2018-09-26 23:06:14 +0100

    staging: wlan-ng: rename DIDmib_dot11phy_dot11PhyOperationTable in p80211metadef.h
    Rename DIDmib_dot11phy_dot11PhyOperationTable in p80211metadef.h to
    DIDMIB_DOT11PHY_OPERATIONTABLE to fix "Avoid CamelCase" message from
    checkpatch and conform to the coding style guidelines. Also shorten
    name by removing repeated use of "DOT11PHY" to ease readability and
    reduce long lines.
    
    Signed-off-by: Tim Collier <osdevtc@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/p80211metadef.h
+++ drivers/staging/wlan-ng/p80211metadef.h
@@ -203,7 +203,7 @@
 			P80211DID_MKITEM(6) | 0x10000000)
 #define DIDMIB_CAT_DOT11PHY \
 			P80211DID_MKSECTION(3)
-#define DIDmib_dot11phy_dot11PhyOperationTable \
+#define DIDMIB_DOT11PHY_OPERATIONTABLE \
 			(P80211DID_MKSECTION(3) | \
 			P80211DID_MKGROUP(1))
 #define DIDmib_dot11phy_dot11PhyTxPowerTable_dot11CurrentTxPowerLevel \
