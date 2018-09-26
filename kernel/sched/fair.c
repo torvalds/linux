31bced01fe77f5164201190bb488f09575000c1d
Valentin Schneider <valentin.schneider@arm.com>
2018-09-26 16:12:07 +0100

    sched/fair: Don't increase sd->balance_interval on newidle balance
    [ Upstream commit 3f130a37c442d5c4d66531b240ebe9abfef426b5 ]
    
    When load_balance() fails to move some load because of task affinity,
    we end up increasing sd->balance_interval to delay the next periodic
    balance in the hopes that next time we look, that annoying pinned
    task(s) will be gone.
    
    However, idle_balance() pays no attention to sd->balance_interval, yet
    it will still lead to an increase in balance_interval in case of
    pinned tasks.
    
    If we're going through several newidle balances (e.g. we have a
    periodic task), this can lead to a huge increase of the
    balance_interval in a very small amount of time.
    
    To prevent that, don't increase the balance interval when going
    through a newidle balance.
    
    This is a similar approach to what is done in commit 58b26c4c0257
    ("sched: Increment cache_nice_tries only on periodic lb"), where we
    disregard newidle balance and rely on periodic balance for more stable
    results.
    
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Dietmar.Eggemann@arm.com
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: patrick.bellasi@arm.com
    Cc: vincent.guittot@linaro.org
    Link: http://lkml.kernel.org/r/1537974727-30788-2-git-send-email-valentin.schneider@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8819,13 +8819,22 @@ out_all_pinned:
 	sd->nr_balance_failed = 0;
 
 out_one_pinned:
+	ld_moved = 0;
+
+	/*
+	 * idle_balance() disregards balance intervals, so we could repeatedly
+	 * reach this code, which would lead to balance_interval skyrocketting
+	 * in a short amount of time. Skip the balance_interval increase logic
+	 * to avoid that.
+	 */
+	if (env.idle == CPU_NEWLY_IDLE)
+		goto out;
+
 	/* tune up the balancing interval */
 	if (((env.flags & LBF_ALL_PINNED) &&
 			sd->balance_interval < MAX_PINNED_INTERVAL) ||
 			(sd->balance_interval < sd->max_interval))
 		sd->balance_interval *= 2;
-
-	ld_moved = 0;
 out:
 	return ld_moved;
 }
