4e673ed4e2bfac00b3c3043a84e007874c17c84d
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2018-09-26 17:40:06 -0400

    media: rcar-vin: fix redeclaration of symbol
    When adding support for parallel subdev for Gen3 it was missed that the
    symbol 'i' in rvin_group_link_notify() was already declared, remove the
    dupe as it's only used as a loop variable this have no functional
    change. This fixes warning:
    
        rcar-core.c:117:52: originally declared here
        rcar-core.c:173:30: warning: symbol 'i' shadows an earlier one
    
    Fixes: 1284605dc821cebd ("media: rcar-vin: Handle parallel subdev in link_notify")
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Acked-by: Jacopo Mondi <jacopo+renesas@jmondi.org>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/platform/rcar-vin/rcar-core.c
+++ drivers/media/platform/rcar-vin/rcar-core.c
@@ -170,7 +170,6 @@ static int rvin_group_link_notify(struct media_link *link, u32 flags,
 
 	if (csi_id == -ENODEV) {
 		struct v4l2_subdev *sd;
-		unsigned int i;
 
 		/*
 		 * Make sure the source entity subdevice is registered as
