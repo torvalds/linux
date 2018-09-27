bf30d9c95f57c29ca9e071d57276afeef6683479
Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
2018-09-27 13:57:30 -0700

    drm/i915/dp: Fix link retraining comment in intel_dp_long_pulse()
    commit 49af5d95b9b3c21a84ad115a9db9acbc036d849a upstream.
    
    Comment claims link needs to be retrained because the connected sink raised
    a long pulse to indicate link loss. If the sink did so,
    intel_dp_hotplug() would have handled link retraining. Looking at the
    logs in Bugzilla referenced in commit '3cf71bc9904d ("drm/i915: Re-apply
    Perform link quality check, unconditionally during long pulse"")', the
    issue is that the sink does not trigger an interrupt. What we want is
    ->detect() from user space to check link status and retrain. Ville's
    review for the original patch also indicates the same root cause. So,
    rewrite the comment.
    
    v2: Patch split and rewrote comment.
    
    Cc: Lyude Paul <lyude@redhat.com>
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Cc: Jani Nikula <jani.nikula@linux.intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Cc: Jan-Marek Glogowski <glogow@fbihome.de>
    References: 3cf71bc9904d ("drm/i915: Re-apply "Perform link quality check, unconditionally during long pulse"")
    Signed-off-by: Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180927205735.16651-1-dhinakaran.pandiyan@intel.com
    (cherry picked from commit 9ebd8202393dde9d3678c9ec162c1aa63ba17eac)
    Fixes: 399334708b4f ("drm/i915: Re-apply "Perform link quality check, unconditionally during long pulse"")
    Cc: stable@vger.kernel.org
    Signed-off-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -4741,16 +4741,9 @@ intel_dp_long_pulse(struct intel_connector *connector,
 		goto out;
 	} else {
 		/*
-		 * If display is now connected check links status,
-		 * there has been known issues of link loss triggering
-		 * long pulse.
-		 *
-		 * Some sinks (eg. ASUS PB287Q) seem to perform some
-		 * weird HPD ping pong during modesets. So we can apparently
-		 * end up with HPD going low during a modeset, and then
-		 * going back up soon after. And once that happens we must
-		 * retrain the link to get a picture. That's in case no
-		 * userspace component reacted to intermittent HPD dip.
+		 * Some external monitors do not signal loss of link
+		 * synchronization with an IRQ_HPD, so force a link status
+		 * check.
 		 */
 		struct intel_encoder *encoder = &dp_to_dig_port(intel_dp)->base;
 
