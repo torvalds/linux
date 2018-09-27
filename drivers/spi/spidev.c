5e9feb3544a72321553d17fcfa0c7e88435ea7ad
Lukasz Majewski <lukma@denx.de>
2018-09-28 00:31:52 +0200

    spidev: Enable the Liebherr's BK4 board to work with spidev driver
    With this commit the DSPI driver on the BK4 board can be used for SPI
    transmission managed from user space (via /dev/spidev0.0).
    
    Example usage/testing:
    insmod ./spi-fsl-dspi.ko
    ./spidev_test -D /dev/spidev0.0 -s 3000000 -v -H -b 8 -p "\xCC\x11\x22\x74"
    
    Signed-off-by: Lukasz Majewski <lukma@denx.de>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -669,6 +669,7 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "lineartechnology,ltc2488" },
 	{ .compatible = "ge,achc" },
 	{ .compatible = "semtech,sx1301" },
+	{ .compatible = "lwn,bk4" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
