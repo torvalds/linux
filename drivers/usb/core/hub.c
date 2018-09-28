bd0e6c9614b95352eb31d0207df16dc156c527fa
Zeng Tao <prime.zeng@hisilicon.com>
2018-09-28 19:27:52 +0800

    usb: hub: try old enumeration scheme first for high speed devices
    The new scheme is required just to support legacy low and full-speed
    devices. For high speed devices, it will slower the enumeration speed.
    So in this patch we try the "old" enumeration scheme first for high speed
    devices, and this is what Windows does since Windows 8.
    
    Signed-off-by: Zeng Tao <prime.zeng@hisilicon.com>
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Reviewed-by: Roger Quadros <rogerq@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -2661,11 +2661,13 @@ static bool use_new_scheme(struct usb_device *udev, int retry,
 {
 	int old_scheme_first_port =
 		port_dev->quirks & USB_PORT_QUIRK_OLD_SCHEME;
+	int quick_enumeration = (udev->speed == USB_SPEED_HIGH);
 
 	if (udev->speed >= USB_SPEED_SUPER)
 		return false;
 
-	return USE_NEW_SCHEME(retry, old_scheme_first_port || old_scheme_first);
+	return USE_NEW_SCHEME(retry, old_scheme_first_port || old_scheme_first
+			      || quick_enumeration);
 }
 
 /* Is a USB 3.0 port in the Inactive or Compliance Mode state?
