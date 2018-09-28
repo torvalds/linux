36e765392e48e0322222347c4d21078c0b94758c
Ming Lei <ming.lei@redhat.com>
2018-09-28 16:42:20 +0800

    blk-mq: complete req in softirq context in case of single queue
    Lot of controllers may have only one irq vector for completing IO
    request. And usually affinity of the only irq vector is all possible
    CPUs, however, on most of ARCH, there may be only one specific CPU
    for handling this interrupt.
    
    So if all IOs are completed in hardirq context, it is inevitable to
    degrade IO performance because of increased irq latency.
    
    This patch tries to address this issue by allowing to complete request
    in softirq context, like the legacy IO path.
    
    IOPS is observed as ~13%+ in the following randread test on raid0 over
    virtio-scsi.
    
    mdadm --create --verbose /dev/md0 --level=0 --chunk=1024 --raid-devices=8 /dev/sdb /dev/sdc /dev/sdd /dev/sde /dev/sdf /dev/sdg /dev/sdh /dev/sdi
    
    fio --time_based --name=benchmark --runtime=30 --filename=/dev/md0 --nrfiles=1 --ioengine=libaio --iodepth=32 --direct=1 --invalidate=1 --verify=0 --verify_fatal=0 --numjobs=32 --rw=randread --blocksize=4k
    
    Cc: Dongli Zhang <dongli.zhang@oracle.com>
    Cc: Zach Marano <zmarano@google.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Bart Van Assche <bvanassche@acm.org>
    Cc: Jianchao Wang <jianchao.w.wang@oracle.com>
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -568,6 +568,20 @@ static void __blk_mq_complete_request(struct request *rq)
 	if (!blk_mq_mark_complete(rq))
 		return;
 
+	/*
+	 * Most of single queue controllers, there is only one irq vector
+	 * for handling IO completion, and the only irq's affinity is set
+	 * as all possible CPUs. On most of ARCHs, this affinity means the
+	 * irq is handled on one specific CPU.
+	 *
+	 * So complete IO reqeust in softirq context in case of single queue
+	 * for not degrading IO performance by irqsoff latency.
+	 */
+	if (rq->q->nr_hw_queues == 1) {
+		__blk_complete_request(rq);
+		return;
+	}
+
 	if (!test_bit(QUEUE_FLAG_SAME_COMP, &rq->q->queue_flags)) {
 		rq->q->softirq_done_fn(rq);
 		return;
