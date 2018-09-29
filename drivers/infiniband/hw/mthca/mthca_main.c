39f2495618c5e980d2873ea3f2d1877dd253e07a
Wei Yongjun <weiyongjun1@huawei.com>
2018-09-29 03:55:16 +0000

    IB/mthca: Fix error return code in __mthca_init_one()
    Fix to return a negative error code from the mthca_cmd_init() error
    handling case instead of 0, as done elsewhere in this function.
    
    Fixes: 80fd8238734c ("[PATCH] IB/mthca: Encapsulate command interface init")
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- drivers/infiniband/hw/mthca/mthca_main.c
+++ drivers/infiniband/hw/mthca/mthca_main.c
@@ -986,7 +986,8 @@ static int __mthca_init_one(struct pci_dev *pdev, int hca_type)
 		goto err_free_dev;
 	}
 
-	if (mthca_cmd_init(mdev)) {
+	err = mthca_cmd_init(mdev);
+	if (err) {
 		mthca_err(mdev, "Failed to init command interface, aborting.\n");
 		goto err_free_dev;
 	}
