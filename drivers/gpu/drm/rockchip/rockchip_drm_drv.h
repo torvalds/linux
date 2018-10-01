2d4f7bdafd709a5cf38f42358bd4c31c621a3e1b
Nickey Yang <nickey.yang@rock-chips.com>
2018-10-01 14:38:43 +0200

    drm/rockchip: dsi: migrate to use dw-mipi-dsi bridge driver
    Add the ROCKCHIP DSI controller driver that uses the Synopsys DesignWare
    MIPI DSI host controller bridge and remove the old separate one.
    
    changes:
    
    v2:
       add err_pllref, remove unnecessary encoder.enable & disable
       correct spelling mistakes
    v3:
       call dw_mipi_dsi_unbind() in dw_mipi_dsi_rockchip_unbind()
       fix typo, use of_device_get_match_data(),
       change some bind() logic into probe()
       add 'dev_set_drvdata()'
    v4:
      return -EINVAL when can not get best_freq
      add a clarifying comment when get vco
      add review tag
    v5:
      keep our power domain enabled while touching GRF
    v6:
      change func name dw_mipi_encoder_disable to
      dw_mipi_dsi_encoder_disable
    v7:
      none
    v8: Heiko
      add Archit's Review tag
      adapt to recent changes in the original rockchip-dsi driver
      beautify grf-handling
      split hw-setup (resources, dsi-host) from bind into probe
    v2-new: Heiko
      add SPDX header instead of license blurb
      drop old versioning to not confuse people
    v3-new: Heiko
      include ordering
      moved hwaccess from mode_set to enable callback
      move pllref_clk enablement to bind (needed by bridge mode_set->lane_mbps)
    v4-new: Heiko
      rebase against recent rockchip-dsi changes
      move to call component_add in the new glue host-attach
    
    Signed-off-by: Nickey Yang <nickey.yang@rock-chips.com>
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Archit Taneja <architt@codeaurora.org>
    Reviewed-by: Andrzej Hajda <a.hajda@samsung.com>
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Signed-off-by: Andrzej Hajda <a.hajda@samsung.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181001123845.11818-6-heiko@sntech.de

--- drivers/gpu/drm/rockchip/rockchip_drm_drv.h
+++ drivers/gpu/drm/rockchip/rockchip_drm_drv.h
@@ -67,7 +67,7 @@ int rockchip_drm_wait_vact_end(struct drm_crtc *crtc, unsigned int mstimeout);
 int rockchip_drm_endpoint_is_subdriver(struct device_node *ep);
 extern struct platform_driver cdn_dp_driver;
 extern struct platform_driver dw_hdmi_rockchip_pltfm_driver;
-extern struct platform_driver dw_mipi_dsi_driver;
+extern struct platform_driver dw_mipi_dsi_rockchip_driver;
 extern struct platform_driver inno_hdmi_driver;
 extern struct platform_driver rockchip_dp_driver;
 extern struct platform_driver rockchip_lvds_driver;
