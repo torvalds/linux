88a10b15173d76338e0808e4915edbd1aae5f264
Tony Luck <tony.luck@intel.com>
2018-10-01 15:43:13 -0700

    EDAC: Don't add devices under /sys/bus/edac
    Nobody(*) uses them. Dropping this will allow us to make the total
    number of memory controllers configurable (as we won't have to worry
    about duplicated device names under this directory).
    
    (*) https://lkml.kernel.org/r/20180927221054.580220e5@coco.lan
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Acked-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
    CC: Aristeu Rozanski Filho <arozansk@redhat.com>
    CC: Greg KH <gregkh@linuxfoundation.org>
    CC: Justin Ernst <justin.ernst@hpe.com>
    CC: Mauro Carvalho Chehab <mchehab@kernel.org>
    CC: Russ Anderson <rja@hpe.com>
    CC: linux-edac <linux-edac@vger.kernel.org>
    Link: http://lkml.kernel.org/r/20181001224313.GA9487@agluck-desk

--- drivers/edac/edac_mc_sysfs.c
+++ drivers/edac/edac_mc_sysfs.c
@@ -405,7 +405,6 @@ static int edac_create_csrow_object(struct mem_ctl_info *mci,
 				    struct csrow_info *csrow, int index)
 {
 	csrow->dev.type = &csrow_attr_type;
-	csrow->dev.bus = mci->bus;
 	csrow->dev.groups = csrow_dev_groups;
 	device_initialize(&csrow->dev);
 	csrow->dev.parent = &mci->dev;
@@ -636,7 +635,6 @@ static int edac_create_dimm_object(struct mem_ctl_info *mci,
 	dimm->mci = mci;
 
 	dimm->dev.type = &dimm_attr_type;
-	dimm->dev.bus = mci->bus;
 	device_initialize(&dimm->dev);
 
 	dimm->dev.parent = &mci->dev;
@@ -940,7 +938,6 @@ int edac_create_sysfs_mci_device(struct mem_ctl_info *mci,
 	device_initialize(&mci->dev);
 
 	mci->dev.parent = mci_pdev;
-	mci->dev.bus = mci->bus;
 	mci->dev.groups = groups;
 	dev_set_name(&mci->dev, "mc%d", mci->mc_idx);
 	dev_set_drvdata(&mci->dev, mci);
