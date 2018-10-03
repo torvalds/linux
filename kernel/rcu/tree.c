8dd3b54689d9a2103c0817f2b7adc51760a45551
Paul E. McKenney <paulmck@linux.ibm.com>
2018-10-03 11:02:05 -0700

    rcutorture: Print GP age upon forward-progress failure
    Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -2682,6 +2682,8 @@ void rcu_fwd_progress_check(unsigned long j)
 	struct rcu_data *rdp;
 
 	if (rcu_gp_in_progress()) {
+		pr_info("%s: GP age %lu jiffies\n",
+			__func__, jiffies - rcu_state.gp_start);
 		show_rcu_gp_kthreads();
 	} else {
 		preempt_disable();
