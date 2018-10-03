b723a7911d028fb55f67a63c4c4d895f72e059d4
Amir Goldstein <amir73il@gmail.com>
2018-10-04 00:25:32 +0300

    fanotify: fix collision of internal and uapi mark flags
    The new mark flag FAN_MARK_FILESYSTEMS collides with existing internal
    flag FAN_MARK_ONDIR. Change internal flag value to avoid the collision.
    
    Fixes: d54f4fba889b ("fanotify: add API to attach/detach super block mark")
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Jan Kara <jack@suse.cz>

--- include/linux/fanotify.h
+++ include/linux/fanotify.h
@@ -5,7 +5,7 @@
 #include <uapi/linux/fanotify.h>
 
 /* not valid from userspace, only kernel internal */
-#define FAN_MARK_ONDIR		0x00000100
+#define FAN_MARK_ONDIR		0x80000000
 
 #define FAN_GROUP_FLAG(group, flag) \
 	((group)->fanotify_data.flags & (flag))
