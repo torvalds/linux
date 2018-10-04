6f053e36bda96461245d0d01f374dac4f7cab54c
Oliver Neukum <oneukum@suse.com>
2018-10-04 15:49:06 +0200

    cdc-acm: fix race between reset and control messaging
    commit 9397940ed812b942c520e0c25ed4b2c64d57e8b9 upstream.
    
    If a device splits up a control message and a reset() happens
    between the parts, the message is lost and already recieved parts
    must be dropped.
    
    Signed-off-by: Oliver Neukum <oneukum@suse.com>
    Fixes: 1aba579f3cf51 ("cdc-acm: handle read pipe errors")
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/class/cdc-acm.c
+++ drivers/usb/class/cdc-acm.c
@@ -1654,6 +1654,7 @@ static int acm_pre_reset(struct usb_interface *intf)
 	struct acm *acm = usb_get_intfdata(intf);
 
 	clear_bit(EVENT_RX_STALL, &acm->flags);
+	acm->nb_index = 0; /* pending control transfers are lost */
 
 	return 0;
 }
