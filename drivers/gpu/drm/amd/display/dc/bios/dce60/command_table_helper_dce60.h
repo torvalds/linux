c4a54f70a6afd1a91072ef06cb083221ff189cca
Mauro Rossi <issor.oruam@gmail.com>
2018-10-05 22:53:25 +0200

    drm/amd/display: dc/bios: add support for DCE6
    [Why]
    command_table_helper.c requires changes for DCE6 support
    
    [How]
    DCE6 targets added replicating and adapting the existing DCE8 implementation.
    
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/bios/dce60/command_table_helper_dce60.h
+++ drivers/gpu/drm/amd/display/dc/bios/dce60/command_table_helper_dce60.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-15 Advanced Micro Devices, Inc.
+ * Copyright 2020 Mauro Rossi <issor.oruam@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -23,35 +23,11 @@
  *
  */
 
-#ifndef __DAL_COMMAND_TABLE_HELPER2_H__
-#define __DAL_COMMAND_TABLE_HELPER2_H__
+#ifndef __DAL_COMMAND_TABLE_HELPER_DCE60_H__
+#define __DAL_COMMAND_TABLE_HELPER_DCE60_H__
 
-#if defined(CONFIG_DRM_AMD_DC_SI)
-#include "dce60/command_table_helper_dce60.h"
-#endif
-#include "dce80/command_table_helper_dce80.h"
-#include "dce110/command_table_helper_dce110.h"
-#include "dce112/command_table_helper2_dce112.h"
-#include "command_table_helper_struct.h"
+struct command_table_helper;
 
-bool dal_bios_parser_init_cmd_tbl_helper2(const struct command_table_helper **h,
-	enum dce_version dce);
+const struct command_table_helper *dal_cmd_tbl_helper_dce60_get_table(void);
 
-bool dal_cmd_table_helper_controller_id_to_atom2(
-	enum controller_id id,
-	uint8_t *atom_id);
-
-uint32_t dal_cmd_table_helper_encoder_mode_bp_to_atom2(
-	enum signal_type s,
-	bool enable_dp_audio);
-
-bool dal_cmd_table_helper_clock_source_id_to_ref_clk_src2(
-	enum clock_source_id id,
-	uint32_t *ref_clk_src_id);
-
-uint8_t dal_cmd_table_helper_transmitter_bp_to_atom2(
-	enum transmitter t);
-
-uint8_t dal_cmd_table_helper_encoder_id_to_atom2(
-	enum encoder_id id);
 #endif
