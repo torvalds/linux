a7dd5d7c2dd83e9a1444ac8ffa68b801c9a5d5f0
YueHaibing <yuehaibing@huawei.com>
2018-10-05 04:15:45 +0000

    b43: remove set but not used variable 'wl'
    Fixes gcc '-Wunused-but-set-variable' warning:
    
    drivers/net/wireless/broadcom/b43/main.c: In function 'b43_one_core_detach':
    drivers/net/wireless/broadcom/b43/main.c:5496:17: warning:
     variable 'wl' set but not used [-Wunused-but-set-variable]
    
    After commit 644aa4d62096 ("b43: remove list of IEEE 802.11 devices")
    'wl' is not used any more.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/broadcom/b43/main.c
+++ drivers/net/wireless/broadcom/b43/main.c
@@ -5493,13 +5493,11 @@ err_powerdown:
 static void b43_one_core_detach(struct b43_bus_dev *dev)
 {
 	struct b43_wldev *wldev;
-	struct b43_wl *wl;
 
 	/* Do not cancel ieee80211-workqueue based work here.
 	 * See comment in b43_remove(). */
 
 	wldev = b43_bus_get_wldev(dev);
-	wl = wldev->wl;
 	b43_debugfs_remove_device(wldev);
 	b43_wireless_core_detach(wldev);
 	list_del(&wldev->list);
