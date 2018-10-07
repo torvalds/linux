009cf451d42101a919bbe160cb6624301996e78e
John Whitmore <johnfwhitmore@gmail.com>
2018-10-07 22:40:18 +0100

    staging:rtl8192u: Remove extra blank lines - Style
    Remove the extra blank lines to clear checkpatch issue.
    
    This is a simple coding style change which should have no impact
    on runtime code execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/ieee80211/ieee80211_module.c
+++ drivers/staging/rtl8192u/ieee80211/ieee80211_module.c
@@ -94,7 +94,6 @@ static inline void ieee80211_networks_initialize(struct ieee80211_device *ieee)
 		list_add_tail(&ieee->networks[i].list, &ieee->network_free_list);
 }
 
-
 struct net_device *alloc_ieee80211(int sizeof_priv)
 {
 	struct ieee80211_device *ieee;
@@ -120,7 +119,6 @@ struct net_device *alloc_ieee80211(int sizeof_priv)
 		goto failed;
 	}
 	ieee80211_networks_initialize(ieee);
-
 
 	/* Default fragmentation threshold is maximum payload size */
 	ieee->fts = DEFAULT_FTS;
@@ -190,7 +188,6 @@ struct net_device *alloc_ieee80211(int sizeof_priv)
 
 	return NULL;
 }
-
 
 void free_ieee80211(struct net_device *dev)
 {
