8ad0f087e46c83078f0010e97014c787dcba5f5b
John Whitmore <johnfwhitmore@gmail.com>
2018-10-07 22:40:15 +0100

    staging:rtl8192u: Add missing SPDX-License-Identifier - Style
    Add the missing SPDX-License-Identifier tag to file to clear the
    checkpatch issue.
    
    This is a coding style change which should have no impact on runtime
    code execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/ieee80211/ieee80211_module.c
+++ drivers/staging/rtl8192u/ieee80211/ieee80211_module.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
 /*******************************************************************************
  *
  *  Copyright(c) 2004 Intel Corporation. All rights reserved.
