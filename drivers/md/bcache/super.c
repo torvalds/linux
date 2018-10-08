91bafdf081b8ad8ab4977918ee45dffe3d744060
Dongbo Cao <cdbdyx@163.com>
2018-10-08 20:41:17 +0800

    bcache: remove useless parameter of bch_debug_init()
    Parameter "struct kobject *kobj" in bch_debug_init() is useless,
    remove it in this patch.
    
    Signed-off-by: Dongbo Cao <cdbdyx@163.com>
    Signed-off-by: Coly Li <colyli@suse.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -2389,7 +2389,7 @@ static int __init bcache_init(void)
 	    sysfs_create_files(bcache_kobj, files))
 		goto err;
 
-	bch_debug_init(bcache_kobj);
+	bch_debug_init();
 	closure_debug_init();
 
 	return 0;
