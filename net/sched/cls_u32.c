db04ff4863bf439500036b1882a9f2236f3c1d76
Al Viro <viro@zeniv.linux.org.uk>
2018-10-08 06:22:41 -0400

    net: sched: cls_u32: the tp_c argument of u32_set_parms() is always tp->data
    It must be tc_u_common associated with that tp (i.e. tp->data).
    Proof:
    	* both ->ht_up and ->tp_c are assign-once
    	* ->tp_c of anything inserted into tp_c->hlist is tp_c
    	* hnodes never get reinserted into the lists or moved
    between those, so anything found by u32_lookup_ht(tp->data, ...)
    will have ->tp_c equal to tp->data.
    	* tp->root->tp_c == tp->data.
    	* ->ht_up of anything inserted into hnode->ht[...] is
    equal to hnode.
    	* knodes never get reinserted into hash chains or moved
    between those, so anything returned by u32_lookup_key(ht, ...)
    will have ->ht_up equal to ht.
    	* any knode returned by u32_get(tp, ...) will have ->ht_up->tp_c
    point to tp->data
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Jamal Hadi Salim <jhs@mojatatu.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/cls_u32.c
+++ net/sched/cls_u32.c
@@ -956,8 +956,7 @@ static int u32_change(struct net *net, struct sk_buff *in_skb,
 		if (!new)
 			return -ENOMEM;
 
-		err = u32_set_parms(net, tp, base,
-				    rtnl_dereference(n->ht_up)->tp_c, new, tb,
+		err = u32_set_parms(net, tp, base, tp_c, new, tb,
 				    tca[TCA_RATE], ovr, extack);
 
 		if (err) {
@@ -1124,7 +1123,7 @@ static int u32_change(struct net *net, struct sk_buff *in_skb,
 	}
 #endif
 
-	err = u32_set_parms(net, tp, base, ht->tp_c, n, tb, tca[TCA_RATE], ovr,
+	err = u32_set_parms(net, tp, base, tp_c, n, tb, tca[TCA_RATE], ovr,
 			    extack);
 	if (err == 0) {
 		struct tc_u_knode __rcu **ins;
