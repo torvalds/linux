49218c83e25b6f0708f246b07d570b2c43a98223
Manasi Navare <manasi.d.navare@intel.com>
2018-10-09 14:28:04 -0700

    drm/i915/dp: Link train Fallback on eDP only if fallback link BW can fit panel's native mode
    commit 041444458835d7fb2c9f042598bfe16bf375b15d upstream.
    
    This patch fixes the original commit c0cfb10d9e1de49 ("drm/i915/edp:
    Do not do link training fallback or prune modes on EDP") that causes
    a blank screen in case of certain eDP panels (Eg: seen on Dell XPS13 9350)
    where first link training fails and a retraining is required by falling
    back to lower link rate/lane count.
    In case of some panels they advertise higher link rate/lane count
    than whats required for supporting the panel's native mode.
    But we always link train at highest link rate/lane count for eDP
    and if that fails we can still fallback to lower link rate/lane count
    as long as the fallback link BW still fits the native mode to avoid
    pruning the panel's native mode yet retraining at fallback values
    to recover from a blank screen.
    
    v3:
    * Add const for fixed_mode (Ville)
    v2:
    * Send uevent if link failure on eDP unconditionally
    
    Fixes: c0cfb10d9e1d ("drm/i915/edp: Do not do link training fallback or prune modes on EDP")
    Cc: Clinton Taylor <clinton.a.taylor@intel.com>
    Cc: Jani Nikula <jani.nikula@linux.intel.com>
    Cc: Ville Syrjala <ville.syrjala@linux.intel.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Lucas De Marchi <lucas.demarchi@intel.com>
    Cc: <stable@vger.kernel.org> # v4.17+
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=107489
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=105338
    Signed-off-by: Manasi Navare <manasi.d.navare@intel.com>
    Tested-by: Alexander Wilson <alexander.wilson@ncf.edu>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181009212804.702-1-manasi.d.navare@intel.com
    (cherry picked from commit 1e712535c51ab025ebc776d4405683d81521996d)
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -401,6 +401,22 @@ static bool intel_dp_link_params_valid(struct intel_dp *intel_dp, int link_rate,
 	return true;
 }
 
+static bool intel_dp_can_link_train_fallback_for_edp(struct intel_dp *intel_dp,
+						     int link_rate,
+						     uint8_t lane_count)
+{
+	const struct drm_display_mode *fixed_mode =
+		intel_dp->attached_connector->panel.fixed_mode;
+	int mode_rate, max_rate;
+
+	mode_rate = intel_dp_link_required(fixed_mode->clock, 18);
+	max_rate = intel_dp_max_data_rate(link_rate, lane_count);
+	if (mode_rate > max_rate)
+		return false;
+
+	return true;
+}
+
 int intel_dp_get_link_train_fallback_values(struct intel_dp *intel_dp,
 					    int link_rate, uint8_t lane_count)
 {
@@ -410,9 +426,23 @@ int intel_dp_get_link_train_fallback_values(struct intel_dp *intel_dp,
 				    intel_dp->num_common_rates,
 				    link_rate);
 	if (index > 0) {
+		if (intel_dp_is_edp(intel_dp) &&
+		    !intel_dp_can_link_train_fallback_for_edp(intel_dp,
+							      intel_dp->common_rates[index - 1],
+							      lane_count)) {
+			DRM_DEBUG_KMS("Retrying Link training for eDP with same parameters\n");
+			return 0;
+		}
 		intel_dp->max_link_rate = intel_dp->common_rates[index - 1];
 		intel_dp->max_link_lane_count = lane_count;
 	} else if (lane_count > 1) {
+		if (intel_dp_is_edp(intel_dp) &&
+		    !intel_dp_can_link_train_fallback_for_edp(intel_dp,
+							      intel_dp_max_common_rate(intel_dp),
+							      lane_count >> 1)) {
+			DRM_DEBUG_KMS("Retrying Link training for eDP with same parameters\n");
+			return 0;
+		}
 		intel_dp->max_link_rate = intel_dp_max_common_rate(intel_dp);
 		intel_dp->max_link_lane_count = lane_count >> 1;
 	} else {
