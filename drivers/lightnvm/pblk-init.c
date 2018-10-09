3f4f4c639f2930bf658cddef4ebac5b2b96417d5
Zhoujie Wu <zjwu@marvell.com>
2018-10-09 13:12:14 +0200

    lightnvm: pblk: consider max hw sectors supported for max_write_pgs
    [ Upstream commit 8a57fc3823d08edb1661a06d9e0a8c2365ac561e ]
    
    When do GC, the number of read/write sectors are determined
    by max_write_pgs(see gc_rq preparation in pblk_gc_line_prepare_ws).
    
    Due to max_write_pgs doesn't consider max hw sectors
    supported by nvme controller(128K), which leads to GC
    tries to read 64 * 4K in one command, and see below error
    caused by pblk_bio_map_addr in function pblk_submit_read_gc.
    
    [ 2923.005376] pblk: could not add page to bio
    [ 2923.005377] pblk: could not allocate GC bio (18446744073709551604)
    
    Signed-off-by: Zhoujie Wu <zjwu@marvell.com>
    Reviewed-by: Javier González <javier@cnexlabs.com>
    Signed-off-by: Matias Bjørling <mb@lightnvm.io>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/lightnvm/pblk-init.c
+++ drivers/lightnvm/pblk-init.c
@@ -375,6 +375,8 @@ static int pblk_core_init(struct pblk *pblk)
 	pblk->min_write_pgs = geo->ws_opt;
 	max_write_ppas = pblk->min_write_pgs * geo->all_luns;
 	pblk->max_write_pgs = min_t(int, max_write_ppas, NVM_MAX_VLBA);
+	pblk->max_write_pgs = min_t(int, pblk->max_write_pgs,
+		queue_max_hw_sectors(dev->q) / (geo->csecs >> SECTOR_SHIFT));
 	pblk_set_sec_per_write(pblk, pblk->min_write_pgs);
 
 	if (pblk->max_write_pgs > PBLK_MAX_REQ_ADDRS) {
