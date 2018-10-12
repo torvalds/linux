242b4a39be7364979cbccf5bc446d8db45cb2014
YueHaibing <yuehaibing@huawei.com>
2018-10-12 10:36:23 +0000

    scsi: arcmsr: Remove set but not used variables 'id, lun'
    Fixes gcc '-Wunused-but-set-variable' warning:
    
    drivers/scsi/arcmsr/arcmsr_hba.c: In function 'arcmsr_drain_donequeue':
    drivers/scsi/arcmsr/arcmsr_hba.c:1320:10: warning:
     variable 'lun' set but not used [-Wunused-but-set-variable]
    
    drivers/scsi/arcmsr/arcmsr_hba.c:1320:6: warning:
     variable 'id' set but not used [-Wunused-but-set-variable]
    
    Never used since introduction in commit ae52e7f09ff5 ("arcmsr: Support 1024 scatter-gather list entries and improve AP while FW trapped and behaviors of EHs").
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/arcmsr/arcmsr_hba.c
+++ drivers/scsi/arcmsr/arcmsr_hba.c
@@ -1317,13 +1317,10 @@ static void arcmsr_report_ccb_state(struct AdapterControlBlock *acb,
 
 static void arcmsr_drain_donequeue(struct AdapterControlBlock *acb, struct CommandControlBlock *pCCB, bool error)
 {
-	int id, lun;
 	if ((pCCB->acb != acb) || (pCCB->startdone != ARCMSR_CCB_START)) {
 		if (pCCB->startdone == ARCMSR_CCB_ABORTED) {
 			struct scsi_cmnd *abortcmd = pCCB->pcmd;
 			if (abortcmd) {
-				id = abortcmd->device->id;
-				lun = abortcmd->device->lun;				
 				abortcmd->result |= DID_ABORT << 16;
 				arcmsr_ccb_complete(pCCB);
 				printk(KERN_NOTICE "arcmsr%d: pCCB ='0x%p' isr got aborted command \n",
