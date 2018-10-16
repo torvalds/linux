80a23f70d546a6fc74561ce7f9e4e65cb6580791
Jon Derrick <jonathan.derrick@intel.com>
2018-10-15 18:48:07 -0600

    PCI: vmd: Detach resources after stopping root bus
    [ Upstream commit dc8af3a827df6d4bb925d3b81b7ec94a7cce9482 ]
    
    The VMD removal path calls pci_stop_root_busi(), which tears down the pcie
    tree, including detaching all of the attached drivers. During driver
    detachment, devices may use pci_release_region() to release resources.
    This path relies on the resource being accessible in resource tree.
    
    By detaching the child domain from the parent resource domain prior to
    stopping the bus, we are preventing the list traversal from finding the
    resource to be freed. If we instead detach the resource after stopping
    the bus, we will have properly freed the resource and detaching is
    simply accounting at that point.
    
    Without this order, the resource is never freed and is orphaned on VMD
    removal, leading to a warning:
    
    [  181.940162] Trying to free nonexistent resource <e5a10000-e5a13fff>
    
    Fixes: 2c2c5c5cd213 ("x86/PCI: VMD: Attach VMD resources to parent domain's resource tree")
    Signed-off-by: Jon Derrick <jonathan.derrick@intel.com>
    [lorenzo.pieralisi@arm.com: updated commit log]
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Reviewed-by: Keith Busch <keith.busch@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/pci/controller/vmd.c
+++ drivers/pci/controller/vmd.c
@@ -818,12 +818,12 @@ static void vmd_remove(struct pci_dev *dev)
 {
 	struct vmd_dev *vmd = pci_get_drvdata(dev);
 
-	vmd_detach_resources(vmd);
 	sysfs_remove_link(&vmd->dev->dev.kobj, "domain");
 	pci_stop_root_bus(vmd->bus);
 	pci_remove_root_bus(vmd->bus);
 	vmd_cleanup_srcu(vmd);
 	vmd_teardown_dma_ops(vmd);
+	vmd_detach_resources(vmd);
 	irq_domain_remove(vmd->irq_domain);
 }
 
