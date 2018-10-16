02c558b2d5d679fbbcaa5b9689484c7e0f8abb7b
John Fastabend <john.fastabend@gmail.com>
2018-10-16 11:08:04 -0700

    bpf: sockmap, support for msg_peek in sk_msg with redirect ingress
    This adds support for the MSG_PEEK flag when doing redirect to ingress
    and receiving on the sk_msg psock queue. Previously the flag was
    being ignored which could confuse applications if they expected the
    flag to work as normal.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -2089,7 +2089,7 @@ int tcp_bpf_sendmsg_redir(struct sock *sk, struct sk_msg *msg, u32 bytes,
 int tcp_bpf_recvmsg(struct sock *sk, struct msghdr *msg, size_t len,
 		    int nonblock, int flags, int *addr_len);
 int __tcp_bpf_recvmsg(struct sock *sk, struct sk_psock *psock,
-		      struct msghdr *msg, int len);
+		      struct msghdr *msg, int len, int flags);
 
 /* Call BPF_SOCK_OPS program that returns an int. If the return value
  * is < 0, then the BPF op failed (for example if the loaded BPF
