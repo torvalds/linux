b0c32912150565130c61104b7789ba4aa0e5f93d
Hai Nguyen Pham <hai.pham.ud@renesas.com>
2018-10-17 11:13:22 +0200

    iommu/ipmmu-vmsa: Hook up r8a77990 DT matching code
    Support the R-Car E3 (r8a77990) IPMMU.
    
    Signed-off-by: Hai Nguyen Pham <hai.pham.ud@renesas.com>
    Signed-off-by: Kazuya Mizuguchi <kazuya.mizuguchi.ks@renesas.com>
    [simon: rebased; dropped no longer required IOMMU_OF_DECLARE hunk]
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/ipmmu-vmsa.c
+++ drivers/iommu/ipmmu-vmsa.c
@@ -762,6 +762,7 @@ static const struct soc_device_attribute soc_rcar_gen3[] = {
 	{ .soc_id = "r8a7796", },
 	{ .soc_id = "r8a77965", },
 	{ .soc_id = "r8a77970", },
+	{ .soc_id = "r8a77990", },
 	{ .soc_id = "r8a77995", },
 	{ /* sentinel */ }
 };
@@ -948,6 +949,9 @@ static const struct of_device_id ipmmu_of_ids[] = {
 		.data = &ipmmu_features_rcar_gen3,
 	}, {
 		.compatible = "renesas,ipmmu-r8a77970",
+		.data = &ipmmu_features_rcar_gen3,
+	}, {
+		.compatible = "renesas,ipmmu-r8a77990",
 		.data = &ipmmu_features_rcar_gen3,
 	}, {
 		.compatible = "renesas,ipmmu-r8a77995",
