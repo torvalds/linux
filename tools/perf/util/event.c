27faeebd00813cd52a140e8944b6b32729b50e61
David S. Miller <davem@davemloft.net>
2018-10-18 11:32:29 -0700

    sparc: Revert unintended perf changes.
    Some local debugging hacks accidently slipped into the VDSO commit.
    
    Sorry!
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- tools/perf/util/event.c
+++ tools/perf/util/event.c
@@ -1081,7 +1081,6 @@ void *cpu_map_data__alloc(struct cpu_map *map, size_t *size, u16 *type, int *max
 	}
 
 	*size += sizeof(struct cpu_map_data);
-	*size = PERF_ALIGN(*size, sizeof(u64));
 	return zalloc(*size);
 }
 
@@ -1561,9 +1560,7 @@ struct map *thread__find_map(struct thread *thread, u8 cpumode, u64 addr,
 
 		return NULL;
 	}
-#if 0
 try_again:
-#endif
 	al->map = map_groups__find(mg, al->addr);
 	if (al->map == NULL) {
 		/*
@@ -1575,7 +1572,6 @@ try_again:
 		 * "[vdso]" dso, but for now lets use the old trick of looking
 		 * in the whole kernel symbol list.
 		 */
-#if 0
 		if (cpumode == PERF_RECORD_MISC_USER && machine &&
 		    mg != &machine->kmaps &&
 		    machine__kernel_ip(machine, al->addr)) {
@@ -1583,7 +1579,6 @@ try_again:
 			load_map = true;
 			goto try_again;
 		}
-#endif
 	} else {
 		/*
 		 * Kernel maps might be changed when loading symbols so loading
