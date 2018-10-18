4f4be03bdeb32761f89cec3f2bec5e69dd41bc82
Anirudh Venkataramanan <anirudh.venkataramanan@intel.com>
2018-10-18 08:37:09 -0700

    ice: Poll for link status change
    When the physical link goes up or down, the driver is supposed to
    receive a link status event (LSE). The driver currently has the code
    to handle LSEs but there is no firmware support for this feature yet.
    So this patch adds the ability for the driver to poll for link status
    changes. The polling itself is done in ice_watchdog_subtask.
    
    For namespace cleanliness, this patch also removes code that handles
    LSE. This code will be reintroduced once the feature is officially
    supported.
    
    Signed-off-by: Anirudh Venkataramanan <anirudh.venkataramanan@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/ice/ice_common.h
+++ drivers/net/ethernet/intel/ice/ice_common.h
@@ -86,12 +86,6 @@ enum ice_status
 ice_aq_set_link_restart_an(struct ice_port_info *pi, bool ena_link,
 			   struct ice_sq_cd *cd);
 enum ice_status
-ice_aq_get_link_info(struct ice_port_info *pi, bool ena_lse,
-		     struct ice_link_status *link, struct ice_sq_cd *cd);
-enum ice_status
-ice_aq_set_event_mask(struct ice_hw *hw, u8 port_num, u16 mask,
-		      struct ice_sq_cd *cd);
-enum ice_status
 ice_dis_vsi_txq(struct ice_port_info *pi, u8 num_queues, u16 *q_ids,
 		u32 *q_teids, enum ice_disq_rst_src rst_src, u16 vmvf_num,
 		struct ice_sq_cd *cmd_details);
