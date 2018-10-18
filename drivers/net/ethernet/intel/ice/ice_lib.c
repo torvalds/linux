f3aaaaaae2633281ad2a4d9b835d4093f0b0a2c8
Anirudh Venkataramanan <anirudh.venkataramanan@intel.com>
2018-10-18 08:37:03 -0700

    ice: Make ice_msix_clean_rings static
    commit 158a08a694c4e ("ice: remove ndo_poll_controller") removed
    ice_netpoll and introduced a namespace warning for ice_msix_clean_rings.
    Fix the namespace warning by making ice_msix_clean_rings static.
    
    Signed-off-by: Anirudh Venkataramanan <anirudh.venkataramanan@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/ice/ice_lib.c
+++ drivers/net/ethernet/intel/ice/ice_lib.c
@@ -433,7 +433,7 @@ int ice_vsi_clear(struct ice_vsi *vsi)
  * @irq: interrupt number
  * @data: pointer to a q_vector
  */
-irqreturn_t ice_msix_clean_rings(int __always_unused irq, void *data)
+static irqreturn_t ice_msix_clean_rings(int __always_unused irq, void *data)
 {
 	struct ice_q_vector *q_vector = (struct ice_q_vector *)data;
 
