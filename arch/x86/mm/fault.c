162041425193602b15774c61740ad8e7dc157df3
Dave Hansen <dave.hansen@linux.intel.com>
2018-10-19 07:08:42 -0700

    x86/mm: Kill stray kernel fault handling comment
    I originally had matching user and kernel comments, but the kernel
    one got improved.  Some errant conflict resolution kicked the commment
    somewhere wrong.  Kill it.
    
    Reported-by: Eric W. Biederman <ebiederm@xmission.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Jann Horn <jannh@google.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Sean Christopherson <sean.j.christopherson@intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Fixes: aa37c51b94 ("x86/mm: Break out user address space handling")
    Link: http://lkml.kernel.org/r/20181019140842.12F929FA@viggo.jf.intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -963,7 +963,6 @@ bad_area_access_error(struct pt_regs *regs, unsigned long error_code,
 		__bad_area(regs, error_code, address, vma, SEGV_ACCERR);
 }
 
-/* Handle faults in the kernel portion of the address space */
 static void
 do_sigbus(struct pt_regs *regs, unsigned long error_code, unsigned long address,
 	  u32 *pkey, unsigned int fault)
