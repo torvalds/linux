81fa7a69c2174ed8de314b9c231ef30a8718e5e1
Gustavo A. R. Silva <gustavo@embeddedor.com>
2018-10-19 11:19:13 +0200

    dt-bindings: phy: Update SERDES_MAX to be SERDES_MAX + 1
    SERDES_MAX is a valid value to index ctrl->phys in
    drivers/phy/mscc/phy-ocelot-serdes.c. But, currently,
    there is an out-of-bounds bug in the mentioned driver
    when reading from ctrl->phys, because the size of
    array ctrl->phys is SERDES_MAX.
    
    Partially fix this by updating SERDES_MAX to be SERDES6G_MAX + 1.
    
    Notice that this is the first part of the solution to
    the out-of-bounds bug mentioned above. Although this
    change is not dependent on any other one.
    
    Suggested-by: Quentin Schulz <quentin.schulz@bootlin.com>
    Reviewed-by: Quentin Schulz <quentin.schulz@bootlin.com>
    Acked-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/dt-bindings/phy/phy-ocelot-serdes.h
+++ include/dt-bindings/phy/phy-ocelot-serdes.h
@@ -7,6 +7,6 @@
 #define SERDES1G_MAX	SERDES1G(5)
 #define SERDES6G(x)	(SERDES1G_MAX + 1 + (x))
 #define SERDES6G_MAX	SERDES6G(2)
-#define SERDES_MAX	SERDES6G_MAX
+#define SERDES_MAX	(SERDES6G_MAX + 1)
 
 #endif
