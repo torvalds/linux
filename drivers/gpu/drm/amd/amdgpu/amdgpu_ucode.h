4a94ba8f376e6923fd36651b9f2f0b54b4a4608d
Hawking Zhang <Hawking.Zhang@amd.com>
2018-10-23 16:49:11 +0800

    drm/amdgpu: rename rlc autoload to backdoor autoload
    This is to differentiate rlc backdoor autoload from rlc
    frontdoor autoload
    
    Signed-off-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Reviewed-by: Le Ma <Le.Ma@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ucode.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ucode.h
@@ -247,7 +247,7 @@ enum amdgpu_firmware_load_type {
 	AMDGPU_FW_LOAD_DIRECT = 0,
 	AMDGPU_FW_LOAD_SMU,
 	AMDGPU_FW_LOAD_PSP,
-	AMDGPU_FW_LOAD_RLC_AUTO,
+	AMDGPU_FW_LOAD_RLC_BACKDOOR_AUTO,
 };
 
 /* conform to smu_ucode_xfer_cz.h */
