ebd09753b5707cc083c52e1d0ec7f45dccdb73bf
Nickhu <nickhu@andestech.com>
2018-10-25 10:24:15 +0800

    nds32: Perf porting
    This is the commit that porting the perf for nds32.
    
    1.Raw event:
    	The raw events start with 'r'.
    		Usage:
    			perf stat -e rXYZ ./app
    			X: the index of performance counter.
    			YZ: the index(convert to hexdecimal) of events
    
    		Example:
    			'perf stat -e r101 ./app' means the counter 1 will count the instruction
    		event.
    
    		The index of counter and events can be found in
    		"Andes System Privilege Architecture Version 3 Manual".
    
    Or you can perform the 'perf list' to find the symbolic name of raw events.
    
    2.Perf mmap2:
    
    	Fix unexpected perf mmap2() page fault
    
    	When the mmap2() called by perf application,
    	you will encounter such condition:"failed to write."
    	With return value -EFAULT
    
    	This is due to the page fault caused by "reading" buffer
    	from the mapped legal address region to write to the descriptor.
    	The page_fault handler will get a VM_FAULT_SIGBUS return value,
    	which should not happens here.(Due to this is a read request.)
    
    	You can refer to kernel/events/core.c:perf_mmap_fault(...)
    	If "(vmf->pgoff && (vmf->flags & FAULT_FLAG_WRITE))" is evaluated
    	as true, you will get VM_FAULT_SIGBUS as return value.
    
    	However, this is not an write request. The flags which indicated
    	why the page fault happens is wrong.
    
    	Furthermore, NDS32 SPAv3 is not able to detect it is read or write.
    	It only know  either it is instruction fetch or data access.
    
    	Therefore, by removing the wrong flag assignment(actually, the hardware
    	is not able to show the reason), we can fix this bug.
    
    3.Perf multiple events map to same counter.
    
    	When there are multiple events map to the same counter, the counter
    	counts inaccurately. This is because each counter only counts one event
    	in the same time.
    	So when there are multiple events map to same counter, they have to take
    	turns in each context.
    
    	There are two solution:
    	1. Print the error message when multiple events map to the same counter.
    	But print the error message would let the program hang in loop. The ltp
    	(linux test program) would be failed when the program hang in loop.
    
    	2. Don't print the error message, the ltp would pass. But the user need to
    	have the knowledge that don't count the events which map to the same
    	counter, or the user will get the inaccurate results.
    
    	We choose method 2 for the solution
    
    Signed-off-by: Nickhu <nickhu@andestech.com>
    Acked-by: Greentime Hu <greentime@andestech.com>
    Signed-off-by: Greentime Hu <greentime@andestech.com>

--- arch/nds32/mm/fault.c
+++ arch/nds32/mm/fault.c
@@ -9,6 +9,7 @@
 #include <linux/init.h>
 #include <linux/hardirq.h>
 #include <linux/uaccess.h>
+#include <linux/perf_event.h>
 
 #include <asm/pgtable.h>
 #include <asm/tlbflush.h>
@@ -169,8 +170,6 @@ good_area:
 			mask = VM_EXEC;
 		else {
 			mask = VM_READ | VM_WRITE;
-			if (vma->vm_flags & VM_WRITE)
-				flags |= FAULT_FLAG_WRITE;
 		}
 	} else if (entry == ENTRY_TLB_MISC) {
 		switch (error_code & ITYPE_mskETYPE) {
@@ -231,11 +230,17 @@ good_area:
 	 * attempt. If we go through a retry, it is extremely likely that the
 	 * page will be found in page cache at that point.
 	 */
+	perf_sw_event(PERF_COUNT_SW_PAGE_FAULTS, 1, regs, addr);
 	if (flags & FAULT_FLAG_ALLOW_RETRY) {
-		if (fault & VM_FAULT_MAJOR)
+		if (fault & VM_FAULT_MAJOR) {
 			tsk->maj_flt++;
-		else
+			perf_sw_event(PERF_COUNT_SW_PAGE_FAULTS_MAJ,
+				      1, regs, addr);
+		} else {
 			tsk->min_flt++;
+			perf_sw_event(PERF_COUNT_SW_PAGE_FAULTS_MIN,
+				      1, regs, addr);
+		}
 		if (fault & VM_FAULT_RETRY) {
 			flags &= ~FAULT_FLAG_ALLOW_RETRY;
 			flags |= FAULT_FLAG_TRIED;
