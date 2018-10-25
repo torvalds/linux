56bc7bac088757185ef10677b722eca19170313b
Daniel Rosenberg <drosen@google.com>
2018-10-25 16:22:50 -0700

    ANDROID: sdcardfs: Add sandbox
    Android/sandbox is treated the same as Android/data
    
    Bug: 27915347
    Test: ls -l /sdcard/Android/sandbox/*somepackage* after
          creating the folder.
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    
    Change-Id: I7ef440a88df72198303c419e1f2f7c4657f9c170
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- fs/sdcardfs/derived_perm.c
+++ fs/sdcardfs/derived_perm.c
@@ -62,6 +62,7 @@ void get_derived_permission_new(struct dentry *parent, struct dentry *dentry,
 	int err;
 	struct qstr q_Android = QSTR_LITERAL("Android");
 	struct qstr q_data = QSTR_LITERAL("data");
+	struct qstr q_sandbox = QSTR_LITERAL("sandbox");
 	struct qstr q_obb = QSTR_LITERAL("obb");
 	struct qstr q_media = QSTR_LITERAL("media");
 	struct qstr q_cache = QSTR_LITERAL("cache");
@@ -108,6 +109,9 @@ void get_derived_permission_new(struct dentry *parent, struct dentry *dentry,
 		break;
 	case PERM_ANDROID:
 		if (qstr_case_eq(name, &q_data)) {
+			/* App-specific directories inside; let anyone traverse */
+			info->data->perm = PERM_ANDROID_DATA;
+		} else if (qstr_case_eq(name, &q_sandbox)) {
 			/* App-specific directories inside; let anyone traverse */
 			info->data->perm = PERM_ANDROID_DATA;
 		} else if (qstr_case_eq(name, &q_obb)) {
