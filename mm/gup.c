d4faa40259b8524a09c1b0fcd38a446db0c0b770
Wei Yang <richard.weiyang@gmail.com>
2018-10-26 15:07:55 -0700

    mm: remove unnecessary local variable addr in __get_user_pages_fast()
    The local variable `addr' in __get_user_pages_fast() is just a shadow of
    `start'.  Since `start' never changes after assignment to `addr', it is
    fine to replace `start' with it.
    
    Also the meaning of [start, end] is more obvious than [addr, end] when
    passed to gup_pgd_range().
    
    Link: http://lkml.kernel.org/r/20180925021448.20265-1-richard.weiyang@gmail.com
    Signed-off-by: Wei Yang <richard.weiyang@gmail.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/gup.c
+++ mm/gup.c
@@ -1780,12 +1780,11 @@ bool gup_fast_permitted(unsigned long start, int nr_pages, int write)
 int __get_user_pages_fast(unsigned long start, int nr_pages, int write,
 			  struct page **pages)
 {
-	unsigned long addr, len, end;
+	unsigned long len, end;
 	unsigned long flags;
 	int nr = 0;
 
 	start &= PAGE_MASK;
-	addr = start;
 	len = (unsigned long) nr_pages << PAGE_SHIFT;
 	end = start + len;
 
@@ -1807,7 +1806,7 @@ int __get_user_pages_fast(unsigned long start, int nr_pages, int write,
 
 	if (gup_fast_permitted(start, nr_pages, write)) {
 		local_irq_save(flags);
-		gup_pgd_range(addr, end, write, pages, &nr);
+		gup_pgd_range(start, end, write, pages, &nr);
 		local_irq_restore(flags);
 	}
 
