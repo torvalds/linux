0252894f53fc2693672308d5453d5076b7516637
Misha Komarovskiy <zombah@gmail.com>
2018-10-27 02:07:23 +0300

    platform/x86: ideapad: Add Y530-15ICH to no_hw_rfkill
    Lenovo Legion Y530-15ICH is another model without
    hardware radio switch. Add it to no_hw_rfkill to
    enable wireless.
    
    Signed-off-by: Misha Komarovskiy <zombah@gmail.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/platform/x86/ideapad-laptop.c
+++ drivers/platform/x86/ideapad-laptop.c
@@ -1147,6 +1147,13 @@ static const struct dmi_system_id no_hw_rfkill_list[] = {
 		},
 	},
 	{
+		.ident = "Lenovo Legion Y530-15ICH",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
+			DMI_MATCH(DMI_PRODUCT_VERSION, "Lenovo Legion Y530-15ICH"),
+		},
+	},
+	{
 		.ident = "Lenovo Legion Y720-15IKB",
 		.matches = {
 			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
