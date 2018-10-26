7b0e0c0e35f5c7eca719404ca0e38bc964657330
Oscar Salvador <osalvador@suse.de>
2018-10-26 15:03:58 -0700

    mm/page_alloc.c: clean up check_for_memory()
    check_for_memory() looks a bit confusing.  First of all, we have this:
    
    if (N_MEMORY == N_NORMAL_MEMORY)
    	return;
    
    Checking the ENUM declaration, looks like N_MEMORY canot be equal to
    N_NORMAL_MEMORY.
    
    I could not find where N_MEMORY is set to N_NORMAL_MEMORY, or the other
    way around either, so unless I am missing something, this condition will
    never evaluate to true.  It makes sense to get rid of it.
    
    Moving forward, the operations within the loop look a bit confusing as
    well.
    
    We set N_HIGH_MEMORY unconditionally, and then we set N_NORMAL_MEMORY in
    case we have CONFIG_HIGHMEM (N_NORMAL_MEMORY != N_HIGH_MEMORY) and zone <=
    ZONE_NORMAL.  (N_HIGH_MEMORY falls back to N_NORMAL_MEMORY on
    !CONFIG_HIGHMEM systems, and that is why we can just go ahead and set
    N_HIGH_MEMORY unconditionally)
    
    Although this works, it is a bit subtle.
    
    I think that this could be easier to follow:
    
    First, we should only set N_HIGH_MEMORY in case we have CONFIG_HIGHMEM.
    And then we should set N_NORMAL_MEMORY in case zone <= ZONE_NORMAL,
    without further checking whether we have CONFIG_HIGHMEM or not.
    
    Link: http://lkml.kernel.org/r/20180828210158.4617-1-osalvador@techadventures.net
    Signed-off-by: Oscar Salvador <osalvador@suse.de>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Michael Hocko <mhocko@suse.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Pavel Tatashin <pavel.tatashin@microsoft.com
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6803,15 +6803,12 @@ static void check_for_memory(pg_data_t *pgdat, int nid)
 {
 	enum zone_type zone_type;
 
-	if (N_MEMORY == N_NORMAL_MEMORY)
-		return;
-
 	for (zone_type = 0; zone_type <= ZONE_MOVABLE - 1; zone_type++) {
 		struct zone *zone = &pgdat->node_zones[zone_type];
 		if (populated_zone(zone)) {
-			node_set_state(nid, N_HIGH_MEMORY);
-			if (N_NORMAL_MEMORY != N_HIGH_MEMORY &&
-			    zone_type <= ZONE_NORMAL)
+			if (IS_ENABLED(CONFIG_HIGHMEM))
+				node_set_state(nid, N_HIGH_MEMORY);
+			if (zone_type <= ZONE_NORMAL)
 				node_set_state(nid, N_NORMAL_MEMORY);
 			break;
 		}
