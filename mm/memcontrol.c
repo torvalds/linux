591edfb10a949d635ed770c6e85ec5286206c07e
Roman Gushchin <guro@fb.com>
2018-10-26 15:03:23 -0700

    mm: drain memcg stocks on css offlining
    Memcg charge is batched using per-cpu stocks, so an offline memcg can be
    pinned by a cached charge up to a moment, when a process belonging to some
    other cgroup will charge some memory on the same cpu.  In other words,
    cached charges can prevent a memory cgroup from being reclaimed for some
    time, without any clear need.
    
    Let's optimize it by explicit draining of all stocks on css offlining.  As
    draining is performed asynchronously, and is skipped if any parallel
    draining is happening, it's cheap.
    
    Link: http://lkml.kernel.org/r/20180827162621.30187-2-guro@fb.com
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Reviewed-by: Shakeel Butt <shakeelb@google.com>
    Acked-by: Michal Hocko <mhocko@kernel.org>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Konstantin Khlebnikov <koct9i@gmail.com>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4573,6 +4573,8 @@ static void mem_cgroup_css_offline(struct cgroup_subsys_state *css)
 	memcg_offline_kmem(memcg);
 	wb_memcg_offline(memcg);
 
+	drain_all_stock(memcg);
+
 	mem_cgroup_id_put(memcg);
 }
 
