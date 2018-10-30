e0d70bcb38d7eae5eb4da2db130a29d7007eac74
Bo YU <tsu.yubo@gmail.com>
2018-10-30 08:01:14 -0400

    kobject: drop unnecessary cast "%llu" for u64
    There is no searon for u64 var cast to unsigned long long type.
    
    Signed-off-by: Bo YU <tsu.yubo@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- lib/kobject_uevent.c
+++ lib/kobject_uevent.c
@@ -579,7 +579,7 @@ int kobject_uevent_env(struct kobject *kobj, enum kobject_action action,
 
 	mutex_lock(&uevent_sock_mutex);
 	/* we will send an event, so request a new sequence number */
-	retval = add_uevent_var(env, "SEQNUM=%llu", (unsigned long long)++uevent_seqnum);
+	retval = add_uevent_var(env, "SEQNUM=%llu", ++uevent_seqnum);
 	if (retval) {
 		mutex_unlock(&uevent_sock_mutex);
 		goto exit;
