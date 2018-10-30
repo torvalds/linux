b49dec1cf8ff1e0b204dd2c30b95a92d75591146
Borislav Petkov <bp@suse.de>
2018-10-30 15:07:17 -0700

    kernel/panic.c: filter out a potential trailing newline
    If a call to panic() terminates the string with a \n , the result puts the
    closing brace ']---' on a newline because panic() itself adds \n too.
    
    Now, if one goes and removes the newline chars from all panic()
    invocations - and the stats right now look like this:
    
    ~300 calls with a \n
    ~500 calls without a \n
    
    one is destined to a neverending game of whack-a-mole because the usual
    thing to do is add a newline at the end of a string a function is supposed
    to print.
    
    Therefore, simply zap any \n at the end of the panic string to avoid
    touching so many places in the kernel.
    
    Link: http://lkml.kernel.org/r/20181009205019.2786-1-bp@alien8.de
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Acked-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Acked-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/panic.c
+++ kernel/panic.c
@@ -136,7 +136,7 @@ void panic(const char *fmt, ...)
 {
 	static char buf[1024];
 	va_list args;
-	long i, i_next = 0;
+	long i, i_next = 0, len;
 	int state = 0;
 	int old_cpu, this_cpu;
 	bool _crash_kexec_post_notifiers = crash_kexec_post_notifiers;
@@ -173,8 +173,12 @@ void panic(const char *fmt, ...)
 	console_verbose();
 	bust_spinlocks(1);
 	va_start(args, fmt);
-	vsnprintf(buf, sizeof(buf), fmt, args);
+	len = vscnprintf(buf, sizeof(buf), fmt, args);
 	va_end(args);
+
+	if (len && buf[len - 1] == '\n')
+		buf[len - 1] = '\0';
+
 	pr_emerg("Kernel panic - not syncing: %s\n", buf);
 #ifdef CONFIG_DEBUG_BUGVERBOSE
 	/*
