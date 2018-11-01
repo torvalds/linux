0d1c7d0fa9d77462c384dbf8e7fdd2c12f50e651
Alexandru Gheorghe <alexandru-cosmin.gheorghe@arm.com>
2018-11-01 15:11:32 +0000

    drm/selftests: Add tests for drm_format_info* helpers
    Add selftests for the following newly added functions:
     - drm_format_info_block_width
     - drm_format_info_block_height
     - drm_format_info_min_pitch
    
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Alexandru Gheorghe <alexandru-cosmin.gheorghe@arm.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181101151051.1509-6-alexandru-cosmin.gheorghe@arm.com

--- drivers/gpu/drm/selftests/test-drm_modeset_common.h
+++ drivers/gpu/drm/selftests/test-drm_modeset_common.h
@@ -14,5 +14,8 @@
 #define FAIL_ON(x) FAIL((x), "%s", "FAIL_ON(" __stringify(x) ")\n")
 
 int igt_check_plane_state(void *ignored);
+int igt_check_drm_format_block_width(void *ignored);
+int igt_check_drm_format_block_height(void *ignored);
+int igt_check_drm_format_min_pitch(void *ignored);
 
 #endif
