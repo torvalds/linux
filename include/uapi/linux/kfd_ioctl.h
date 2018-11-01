0d406e7972dd47c54959281782e05f52c8a10c95
Dmitry V. Levin <ldv@altlinux.org>
2018-11-01 14:03:08 +0300

    uapi: fix linux/kfd_ioctl.h userspace compilation errors
    commit aba118389a6fb2ad7958de0f37b5869852bd38cf upstream.
    
    Consistently use types provided by <linux/types.h> via <drm/drm.h>
    to fix the following linux/kfd_ioctl.h userspace compilation errors:
    
    /usr/include/linux/kfd_ioctl.h:250:2: error: unknown type name 'uint32_t'
      uint32_t reset_type;
    /usr/include/linux/kfd_ioctl.h:251:2: error: unknown type name 'uint32_t'
      uint32_t reset_cause;
    /usr/include/linux/kfd_ioctl.h:252:2: error: unknown type name 'uint32_t'
      uint32_t memory_lost;
    /usr/include/linux/kfd_ioctl.h:253:2: error: unknown type name 'uint32_t'
      uint32_t gpu_id;
    
    Fixes: 0c119abad7f0d ("drm/amd: Add kfd ioctl defines for hw_exception event")
    Cc: <stable@vger.kernel.org> # v4.19
    Signed-off-by: Dmitry V. Levin <ldv@altlinux.org>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/uapi/linux/kfd_ioctl.h
+++ include/uapi/linux/kfd_ioctl.h
@@ -247,10 +247,10 @@ struct kfd_hsa_memory_exception_data {
 
 /* hw exception data */
 struct kfd_hsa_hw_exception_data {
-	uint32_t reset_type;
-	uint32_t reset_cause;
-	uint32_t memory_lost;
-	uint32_t gpu_id;
+	__u32 reset_type;
+	__u32 reset_cause;
+	__u32 memory_lost;
+	__u32 gpu_id;
 };
 
 /* Event data */
