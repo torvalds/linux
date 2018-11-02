546ec7b471bcae78463426a218b8c7f35bbb71f4
Jordan Crouse <jcrouse@codeaurora.org>
2018-11-02 09:25:18 -0600

    drm/msm/gpu: Allocate the correct size for the GPU memptrs
    Allocate the correct buffer size for the GPU memptrs. The incorrect
    size hasn't affected us thus far since the incorrect size was larger
    than the intended size and we're still stuck on page sized
    granularity anyway but technically correct is the best kind of
    correct.
    
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/msm_gpu.c
+++ drivers/gpu/drm/msm/msm_gpu.c
@@ -917,7 +917,8 @@ int msm_gpu_init(struct drm_device *drm, struct platform_device *pdev,
 		goto fail;
 	}
 
-	memptrs = msm_gem_kernel_new(drm, sizeof(*gpu->memptrs_bo),
+	memptrs = msm_gem_kernel_new(drm,
+		sizeof(struct msm_rbmemptrs) * nr_rings,
 		MSM_BO_UNCACHED, gpu->aspace, &gpu->memptrs_bo,
 		&memptrs_iova);
 
