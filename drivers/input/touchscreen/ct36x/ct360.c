0039514fc69cf0d2fc616dda67a0089663583b3f
Tao Huang <huangtao@rock-chips.com>
2018-11-03 15:18:42 +0800

    drivers/input/touchscreen: remove unused ct36x drivers
    Change-Id: Iaab79b3a681c693caa948bd9acd49096f87b0bf9
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- drivers/input/touchscreen/ct36x/ct360.c
+++
@@ -1,4 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-
-struct ct36x_ops ct360_ops = {
-};
--- drivers/input/touchscreen/ct36x_ts/generic.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-
-#ifndef GENERIC_H
-#define GENERIC_H
-
-#define CT36X_TS_I2C_SPEED			400000
-
-#endif
-
