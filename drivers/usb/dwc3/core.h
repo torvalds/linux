89a9cc47513e91bc91ba2c438d5e422a0c8d05e7
Thinh Nguyen <thinh.nguyen@synopsys.com>
2018-11-02 18:41:42 -0700

    usb: dwc3: Set default mode for DWC_usb3 v3.30a and higher
    DWC_usb31 and DWC_usb3 v3.30a and higher do not support OTG mode. If
    the controller supports DRD but the dr_mode is not specified or set to
    OTG, then set the mode to peripheral.
    
    Signed-off-by: Thinh Nguyen <thinhn@synopsys.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -1107,6 +1107,7 @@ struct dwc3 {
 #define DWC3_REVISION_290A	0x5533290a
 #define DWC3_REVISION_300A	0x5533300a
 #define DWC3_REVISION_310A	0x5533310a
+#define DWC3_REVISION_330A	0x5533330a
 
 /*
  * NOTICE: we're using bit 31 as a "is usb 3.1" flag. This is really
