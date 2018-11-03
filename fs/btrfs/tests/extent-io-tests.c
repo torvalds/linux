d9cb2459b2e96264246e3a78f43c2f74c2c72461
Qu Wenruo <wqu@suse.com>
2018-11-03 17:24:52 +0800

    btrfs: tests: Use BTRFS_MAX_EXTENT_SIZE to replace the intermediate number
    In extent-io self test, we need 2 ordered extents at its maximum size to
    do the test.
    
    Instead of using the intermediate numbers, use BTRFS_MAX_EXTENT_SIZE for
    @max_bytes, and twice @max_bytes for @total_dirty.  This should explain
    why we need all these magic numbers and prevent people to modify them by
    accident.
    
    Reviewed-by: Nikolay Borisov <nborisov@suse.com>
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/tests/extent-io-tests.c
+++ fs/btrfs/tests/extent-io-tests.c
@@ -62,8 +62,9 @@ static int test_find_delalloc(u32 sectorsize)
 	struct page *page;
 	struct page *locked_page = NULL;
 	unsigned long index = 0;
-	u64 total_dirty = SZ_256M;
-	u64 max_bytes = SZ_128M;
+	/* In this test we need at least 2 file extents at its maximum size */
+	u64 max_bytes = BTRFS_MAX_EXTENT_SIZE;
+	u64 total_dirty = 2 * max_bytes;
 	u64 start, end, test_start;
 	u64 found;
 	int ret = -EINVAL;
