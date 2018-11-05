c3d6a64bd1e82ea486bff46b17d5403c2d2602d6
Sabyasachi Gupta <sabyasachi.linux@gmail.com>
2018-11-05 08:58:23 +0530

    powerpc/pasemi: Use dma_zalloc_coherent()
    Replace dma_alloc_coherent() + memset() with dma_zalloc_coherent().
    
    Signed-off-by: Sabyasachi Gupta <sabyasachi.linux@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/platforms/pasemi/dma_lib.c
+++ arch/powerpc/platforms/pasemi/dma_lib.c
@@ -255,14 +255,12 @@ int pasemi_dma_alloc_ring(struct pasemi_dmachan *chan, int ring_size)
 
 	chan->ring_size = ring_size;
 
-	chan->ring_virt = dma_alloc_coherent(&dma_pdev->dev,
+	chan->ring_virt = dma_zalloc_coherent(&dma_pdev->dev,
 					     ring_size * sizeof(u64),
 					     &chan->ring_dma, GFP_KERNEL);
 
 	if (!chan->ring_virt)
 		return -ENOMEM;
-
-	memset(chan->ring_virt, 0, ring_size * sizeof(u64));
 
 	return 0;
 }
