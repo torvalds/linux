f96afa767baffba7645f5e10998f5178948bb9aa
Quentin Monnet <quentin.monnet@netronome.com>
2018-11-07 12:28:45 +0000

    selftests/bpf: enable (uncomment) all tests in test_libbpf.sh
    libbpf is now able to load successfully test_l4lb_noinline.o and
    samples/bpf/tracex3_kern.o.
    
    For the test_l4lb_noinline, uncomment related tests from test_libbpf.c
    and remove the associated "TODO".
    
    For tracex3_kern.o, instead of loading a program from samples/bpf/ that
    might not have been compiled at this stage, try loading a program from
    BPF selftests. Since this test case is about loading a program compiled
    without the "-target bpf" flag, change the Makefile to compile one
    program accordingly (instead of passing the flag for compiling all
    programs).
    
    Regarding test_xdp_noinline.o: in its current shape the program fails to
    load because it provides no version section, but the loader needs one.
    The test was added to make sure that libbpf could load XDP programs even
    if they do not provide a version number in a dedicated section. But
    libbpf is already capable of doing that: in our case loading fails
    because the loader does not know that this is an XDP program (it does
    not need to, since it does not attach the program). So trying to load
    test_xdp_noinline.o does not bring much here: just delete this subtest.
    
    For the record, the error message obtained with tracex3_kern.o was
    fixed by commit e3d91b0ca523 ("tools/libbpf: handle issues with bpf ELF
    objects containing .eh_frames")
    
    I have not been abled to reproduce the "libbpf: incorrect bpf_call
    opcode" error for test_l4lb_noinline.o, even with the version of libbpf
    present at the time when test_libbpf.sh and test_libbpf_open.c were
    created.
    
    RFC -> v1:
    - Compile test_xdp without the "-target bpf" flag, and try to load it
      instead of ../../samples/bpf/tracex3_kern.o.
    - Delete test_xdp_noinline.o subtest.
    
    Cc: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Quentin Monnet <quentin.monnet@netronome.com>
    Acked-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Acked-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- tools/testing/selftests/bpf/test_libbpf.sh
+++ tools/testing/selftests/bpf/test_libbpf.sh
@@ -33,17 +33,11 @@ trap exit_handler 0 2 3 6 9
 
 libbpf_open_file test_l4lb.o
 
-# TODO: fix libbpf to load noinline functions
-# [warning] libbpf: incorrect bpf_call opcode
-#libbpf_open_file test_l4lb_noinline.o
+# Load a program with BPF-to-BPF calls
+libbpf_open_file test_l4lb_noinline.o
 
-# TODO: fix test_xdp_meta.c to load with libbpf
-# [warning] libbpf: test_xdp_meta.o doesn't provide kernel version
-#libbpf_open_file test_xdp_meta.o
-
-# TODO: fix libbpf to handle .eh_frame
-# [warning] libbpf: relocation failed: no section(10)
-#libbpf_open_file ../../../../samples/bpf/tracex3_kern.o
+# Load a program compiled without the "-target bpf" flag
+libbpf_open_file test_xdp.o
 
 # Success
 exit 0
