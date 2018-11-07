68cb7d531e6a87250a51b8a4ee1c79b3445aeff3
Paolo Abeni <pabeni@redhat.com>
2018-11-07 12:38:31 +0100

    ip: factor out protocol delivery helper
    So that we can re-use it at the UDP level in a later patch
    
    rfc v3 -> v1
     - add the helper declaration into the ip header
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/ip.h
+++ include/net/ip.h
@@ -155,6 +155,7 @@ int ip_rcv(struct sk_buff *skb, struct net_device *dev, struct packet_type *pt,
 void ip_list_rcv(struct list_head *head, struct packet_type *pt,
 		 struct net_device *orig_dev);
 int ip_local_deliver(struct sk_buff *skb);
+void ip_protocol_deliver_rcu(struct net *net, struct sk_buff *skb, int proto);
 int ip_mr_input(struct sk_buff *skb);
 int ip_output(struct net *net, struct sock *sk, struct sk_buff *skb);
 int ip_mc_output(struct net *net, struct sock *sk, struct sk_buff *skb);
