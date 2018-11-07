3b73e842c7265a645f373a2de956ad36292f819d
Heiner Kallweit <hkallweit1@gmail.com>
2018-11-07 08:52:46 +0100

    net: phy: realtek: load driver for all PHYs with a Realtek OUI
    Instead of listing every single PHYID, load the driver for every PHYID
    with a Realtek OUI, independent of model number and revision.
    
    This patch also improves two further aspects:
    - constify realtek_tbl[]
    - the mask should have been 0xffffffff instead of 0x001fffff so far,
      by masking out some bits a PHY from another vendor could have been
      matched
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/realtek.c
+++ drivers/net/phy/realtek.c
@@ -305,15 +305,8 @@ static struct phy_driver realtek_drvs[] = {
 
 module_phy_driver(realtek_drvs);
 
-static struct mdio_device_id __maybe_unused realtek_tbl[] = {
-	{ 0x001cc816, 0x001fffff },
-	{ 0x001cc910, 0x001fffff },
-	{ 0x001cc912, 0x001fffff },
-	{ 0x001cc913, 0x001fffff },
-	{ 0x001cc914, 0x001fffff },
-	{ 0x001cc915, 0x001fffff },
-	{ 0x001cc916, 0x001fffff },
-	{ 0x001cc961, 0x001fffff },
+static const struct mdio_device_id __maybe_unused realtek_tbl[] = {
+	{ 0x001cc800, GENMASK(31, 10) },
 	{ }
 };
 
