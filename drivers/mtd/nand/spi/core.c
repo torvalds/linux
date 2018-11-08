10949af1681d5bb5cdbcc012815c6e40eec17d02
Schrempf Frieder <frieder.schrempf@kontron.De>
2018-11-08 08:32:11 +0000

    mtd: spinand: Add initial support for Toshiba TC58CVG2S0H
    Add minimal support for the Toshiba TC58CVG2S0H SPI NAND chip.
    
    Signed-off-by: Frieder Schrempf <frieder.schrempf@kontron.de>
    Acked-by: Clément Péron <peron.clem@gmail.com>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- drivers/mtd/nand/spi/core.c
+++ drivers/mtd/nand/spi/core.c
@@ -766,6 +766,7 @@ static const struct nand_ops spinand_ops = {
 static const struct spinand_manufacturer *spinand_manufacturers[] = {
 	&macronix_spinand_manufacturer,
 	&micron_spinand_manufacturer,
+	&toshiba_spinand_manufacturer,
 	&winbond_spinand_manufacturer,
 };
 
