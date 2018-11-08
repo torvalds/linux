8da6e9183275c837e7d2401b3018ff81f657a19a
Kirill Tkhai <ktkhai@virtuozzo.com>
2018-11-08 12:05:20 +0300

    fuse: Kill fasync only if interrupt is queued in queue_interrupt()
    We should sent signal only in case of interrupt is really queued.  Not a
    real problem, but this makes the code clearer and intuitive.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -480,9 +480,9 @@ static void queue_interrupt(struct fuse_iqueue *fiq, struct fuse_req *req)
 	if (list_empty(&req->intr_entry)) {
 		list_add_tail(&req->intr_entry, &fiq->interrupts);
 		wake_up_locked(&fiq->waitq);
+		kill_fasync(&fiq->fasync, SIGIO, POLL_IN);
 	}
 	spin_unlock(&fiq->waitq.lock);
-	kill_fasync(&fiq->fasync, SIGIO, POLL_IN);
 }
 
 static void request_wait_answer(struct fuse_conn *fc, struct fuse_req *req)
