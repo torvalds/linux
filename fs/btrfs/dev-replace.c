53e62fb5a4aedeff05aa1efd277de32dc765f4bf
Anand Jain <anand.jain@oracle.com>
2018-11-11 22:22:24 +0800

    btrfs: dev-replace: add explicit check for replace result "no error"
    We recast the replace return status
    BTRFS_IOCTL_DEV_REPLACE_RESULT_SCRUB_INPROGRESS to 0, to indicate no
    error.
    And since BTRFS_IOCTL_DEV_REPLACE_RESULT_NO_ERROR should also return 0,
    which is also declared as 0, so we just return. Instead add it to the if
    statement so that there is enough clarity while reading the code.
    
    Signed-off-by: Anand Jain <anand.jain@oracle.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/dev-replace.c
+++ fs/btrfs/dev-replace.c
@@ -539,8 +539,9 @@ int btrfs_dev_replace_by_ioctl(struct btrfs_fs_info *fs_info,
 					args->start.cont_reading_from_srcdev_mode);
 	args->result = ret;
 	/* don't warn if EINPROGRESS, someone else might be running scrub */
-	if (ret == BTRFS_IOCTL_DEV_REPLACE_RESULT_SCRUB_INPROGRESS)
-		ret = 0;
+	if (ret == BTRFS_IOCTL_DEV_REPLACE_RESULT_SCRUB_INPROGRESS ||
+	    ret == BTRFS_IOCTL_DEV_REPLACE_RESULT_NO_ERROR)
+		return 0;
 
 	return ret;
 }
