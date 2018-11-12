7894a7e7ea3de6c1fd5b2b43f3a73106c94ef584
Vijendar Mukunda <Vijendar.Mukunda@amd.com>
2018-11-12 11:04:54 +0530

    ASoC: amd: create ACP3x PCM platform device
    ACP 3x IP has I2S controller device as one of IP blocks.
    Create a platform device for it, so that the PCM platform driver
    can be bound to this device. Pass PCI resources like MMIO, irq
    to the platform device.
    
    Signed-off-by: Maruthi Bayyavarapu <maruthi.bayyavarapu@amd.com>
    Tested-by: Ravulapati Vishnu vardhan Rao <vishnuvardhanrao.ravulapati@amd.com>
    Signed-off-by: Vijendar Mukunda <vijendar.mukunda@amd.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/raven/acp3x.h
+++ sound/soc/amd/raven/acp3x.h
@@ -1,6 +1,10 @@
 #include "chip_offset_byte.h"
 
 #define ACP3x_PHY_BASE_ADDRESS 0x1240000
+#define	ACP3x_I2S_MODE	0
+#define	ACP3x_REG_START	0x1240000
+#define	ACP3x_REG_END	0x1250200
+#define I2S_MODE	0x04
 
 static inline u32 rv_readl(void __iomem *base_addr)
 {
