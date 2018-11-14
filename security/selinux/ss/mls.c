318d38951e9526a647376c1b3acaad8197317b24
Paul Moore <paul@paul-moore.com>
2018-11-13 21:44:33 -0500

    UPSTREAM: selinux: fix non-MLS handling in mls_context_to_sid()
    Commit 95ffe194204a ("selinux: refactor mls_context_to_sid() and make
    it stricter") inadvertently changed how we handle labels that did not
    contain MLS information.  This patch restores the proper behavior in
    mls_context_to_sid() and adds a comment explaining the proper
    behavior to help ensure this doesn't happen again.
    
    Fixes: 95ffe194204a ("selinux: refactor mls_context_to_sid() and make it stricter")
    Reported-by: Stephen Smalley <sds@tycho.nsa.gov>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    (cherry picked from commit 877181a8d9dc663f7a73f77f50af714d7888ec3b)
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I503260e72b6b05b1d4f2be7bc8b04e4538ca5c5f

--- security/selinux/ss/mls.c
+++ security/selinux/ss/mls.c
@@ -245,9 +245,13 @@ int mls_context_to_sid(struct policydb *pol,
 	char *rangep[2];
 
 	if (!pol->mls_enabled) {
-		if ((def_sid != SECSID_NULL && oldc) || (*scontext) == '\0')
-			return 0;
-		return -EINVAL;
+		/*
+		 * With no MLS, only return -EINVAL if there is a MLS field
+		 * and it did not come from an xattr.
+		 */
+		if (oldc && def_sid == SECSID_NULL)
+			return -EINVAL;
+		return 0;
 	}
 
 	/*
