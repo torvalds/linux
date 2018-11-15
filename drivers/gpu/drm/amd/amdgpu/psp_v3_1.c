900cc36338188773230c3c7fa099619bf532517b
YueHaibing <yuehaibing@huawei.com>
2018-11-15 20:00:35 +0800

    drm/amdgpu: remove set but not used variable 'ring'
    Fixes gcc '-Wunused-but-set-variable' warning:
    
    drivers/gpu/drm/amd/amdgpu/psp_v10_0.c: In function 'psp_v10_0_ring_stop':
    drivers/gpu/drm/amd/amdgpu/psp_v10_0.c:230:19: warning:
     variable 'ring' set but not used [-Wunused-but-set-variable]
    
    drivers/gpu/drm/amd/amdgpu/psp_v3_1.c: In function 'psp_v3_1_ring_stop':
    drivers/gpu/drm/amd/amdgpu/psp_v3_1.c:359:19: warning:
     variable ‘ring’ set but not used [-Wunused-but-set-variable]
    
    It not used since commit
    4ef72453311a ("drm/amdgpu: added api for stopping psp ring (v2)")
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Reviewed-by: Huang Rui <ray.huang@amd.com>
    Acked-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/psp_v3_1.c
+++ drivers/gpu/drm/amd/amdgpu/psp_v3_1.c
@@ -356,11 +356,8 @@ static int psp_v3_1_ring_stop(struct psp_context *psp,
 			      enum psp_ring_type ring_type)
 {
 	int ret = 0;
-	struct psp_ring *ring;
 	unsigned int psp_ring_reg = 0;
 	struct amdgpu_device *adev = psp->adev;
-
-	ring = &psp->km_ring;
 
 	/* Write the ring destroy command to C2PMSG_64 */
 	psp_ring_reg = 3 << 16;
