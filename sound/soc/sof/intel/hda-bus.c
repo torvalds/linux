68cc156d4ff7ff512d76e66107ae35ed5445ff44
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2018-11-17 10:19:05 -0600

    ASoC: SOF: hda: better split between HDA and non-HDA platforms
    Most inits can be conditionally included. the bus->reg_lock spinlock
    is required in all cases.
    
    TODO: remove the static variable (unclear why it's needed)
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/sof/intel/hda-bus.c
+++ sound/soc/sof/intel/hda-bus.c
@@ -86,20 +86,22 @@ void sof_hda_bus_init(struct hdac_bus *bus, struct device *dev,
 
 	bus->io_ops = &io_ops;
 	INIT_LIST_HEAD(&bus->stream_list);
-	INIT_LIST_HEAD(&bus->codec_list);
 
-#if IS_ENABLED(CONFIG_SND_SOC_SOF_HDA)
-	bus->ops = &bus_ops;
-	INIT_WORK(&bus->unsol_work, snd_hdac_bus_process_unsol_events);
-#endif
-	spin_lock_init(&bus->reg_lock);
-	mutex_init(&bus->cmd_mutex);
 	bus->irq = -1;
-
 	bus->ext_ops = ext_ops;
-	INIT_LIST_HEAD(&bus->hlink_list);
 	bus->idx = idx++;
 
+	spin_lock_init(&bus->reg_lock);
+
+#if IS_ENABLED(CONFIG_SND_SOC_SOF_HDA)
+	INIT_LIST_HEAD(&bus->codec_list);
+	INIT_LIST_HEAD(&bus->hlink_list);
+
+	mutex_init(&bus->cmd_mutex);
 	mutex_init(&bus->lock);
+	bus->ops = &bus_ops;
+	INIT_WORK(&bus->unsol_work, snd_hdac_bus_process_unsol_events);
 	bus->cmd_dma_state = true;
+#endif
+
 }
