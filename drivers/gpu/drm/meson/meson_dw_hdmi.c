c376a9b7751a1f111665656cd9b8ea9e4b7c270f
Neil Armstrong <narmstrong@baylibre.com>
2018-11-18 14:06:11 +0100

    WIP: drm/meson: Output in YUV444 if sink supports it
    With the YUV420 handling, we can dynamically setup the HDMI output
    pixel format depending on the mode and connector info.
    So now, we can output in YUV444, which is the native video pipeline
    format, directly to the HDMI Sink if it's supported without
    necessarily involving the HDMI Controller CSC.
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    
    %% original patch: 0197-WIP-drm-meson-Output-in-YUV444-if-sink-supports-it.patch

--- drivers/gpu/drm/meson/meson_dw_hdmi.c
+++ drivers/gpu/drm/meson/meson_dw_hdmi.c
@@ -721,7 +721,10 @@ static int meson_venc_hdmi_encoder_atomic_check(struct drm_encoder *encoder,
 		dw_hdmi->output_bus_format = MEDIA_BUS_FMT_UYYVYY8_0_5X24;
 	} else {
 		dw_hdmi->input_bus_format = MEDIA_BUS_FMT_YUV8_1X24;
-		dw_hdmi->output_bus_format = MEDIA_BUS_FMT_RGB888_1X24;
+		if (info->color_formats & DRM_COLOR_FORMAT_YCRCB444)
+			dw_hdmi->output_bus_format = MEDIA_BUS_FMT_YUV8_1X24;
+		else
+			dw_hdmi->output_bus_format = MEDIA_BUS_FMT_RGB888_1X24;
 	}
 
 	return 0;
