249279c677aff01d6c4eaa99c1fb0bb6f8edd3f9
Dave Chinner <dchinner@redhat.com>
2018-11-19 13:31:09 -0800

    xfs: extent shifting doesn't fully invalidate page cache
    [ Upstream commit 7f9f71be84bcab368e58020a42f6d0dd97adf0ce ]
    
    The extent shifting code uses a flush and invalidate mechainsm prior
    to shifting extents around. This is similar to what
    xfs_free_file_space() does, but it doesn't take into account things
    like page cache vs block size differences, and it will fail if there
    is a page that it currently busy.
    
    xfs_flush_unmap_range() handles all of these cases, so just convert
    xfs_prepare_shift() to us that mechanism rather than having it's own
    special sauce.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/xfs/xfs_bmap_util.c
+++ fs/xfs/xfs_bmap_util.c
@@ -1244,13 +1244,7 @@ xfs_prepare_shift(
 	 * Writeback and invalidate cache for the remainder of the file as we're
 	 * about to shift down every extent from offset to EOF.
 	 */
-	error = filemap_write_and_wait_range(VFS_I(ip)->i_mapping, offset, -1);
-	if (error)
-		return error;
-	error = invalidate_inode_pages2_range(VFS_I(ip)->i_mapping,
-					offset >> PAGE_SHIFT, -1);
-	if (error)
-		return error;
+	error = xfs_flush_unmap_range(ip, offset, XFS_ISIZE(ip));
 
 	/*
 	 * Clean out anything hanging around in the cow fork now that
