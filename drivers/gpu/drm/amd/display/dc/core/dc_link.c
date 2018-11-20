923fe4951282cbdfce05186c10380bbc45b5e03b
David Francis <David.Francis@amd.com>
2018-11-20 09:42:58 -0500

    drm/amd/display: Detach backlight from stream
    [Why]
    Backlight is conceptually a property of links, not streams.
    All backlight programming is done on links, but there is a
    stream property bl_pwm_level that is used to restore backlight
    on dpms on and s3 resume.  This is unnecessary, as backlight
    is already restored by hardware with no driver intervention.
    
    [How]
    Remove bl_pwm_level, and the stream argument to set_backlight
    
    Signed-off-by: David Francis <David.Francis@amd.com>
    Reviewed-by: Harry Wentland <Harry.Wentland@amd.com>
    Acked-by: Anthony Koo <Anthony.Koo@amd.com>
    Acked-by: Leo Li <sunpeng.li@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_link.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_link.c
@@ -2186,8 +2186,7 @@ int dc_link_get_backlight_level(const struct dc_link *link)
 
 bool dc_link_set_backlight_level(const struct dc_link *link,
 		uint32_t backlight_pwm_u16_16,
-		uint32_t frame_ramp,
-		const struct dc_stream_state *stream)
+		uint32_t frame_ramp)
 {
 	struct dc  *core_dc = link->ctx->dc;
 	struct abm *abm = core_dc->res_pool->abm;
@@ -2201,10 +2200,6 @@ bool dc_link_set_backlight_level(const struct dc_link *link,
 		(abm == NULL) ||
 		(abm->funcs->set_backlight_level_pwm == NULL))
 		return false;
-
-	if (stream)
-		((struct dc_stream_state *)stream)->bl_pwm_level =
-				backlight_pwm_u16_16;
 
 	use_smooth_brightness = dmcu->funcs->is_dmcu_initialized(dmcu);
 
@@ -2633,11 +2628,6 @@ void core_link_enable_stream(
 
 		if (dc_is_dp_signal(pipe_ctx->stream->signal))
 			enable_stream_features(pipe_ctx);
-
-		dc_link_set_backlight_level(pipe_ctx->stream->link,
-				pipe_ctx->stream->bl_pwm_level,
-				0,
-				pipe_ctx->stream);
 	}
 
 }
