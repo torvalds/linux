396bc9d40d694befa1c2c88f9873afc62a189b5f
Geert Uytterhoeven <geert+renesas@glider.be>
2018-11-21 10:44:32 +0100

    clk: renesas: r8a77970: Add CPEX clock
    Implement support for the CPEX clock on R-Car V3M.  This clock can be
    selected as a clock source for CMT1 (Compare Match Timer Type 1).
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Acked-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/renesas/r8a77970-cpg-mssr.c
+++ drivers/clk/renesas/r8a77970-cpg-mssr.c
@@ -96,6 +96,7 @@ static const struct cpg_core_clk r8a77970_core_clks[] __initconst = {
 
 	DEF_FIXED("cl",		R8A77970_CLK_CL,    CLK_PLL1_DIV2, 48, 1),
 	DEF_FIXED("cp",		R8A77970_CLK_CP,    CLK_EXTAL,	    2, 1),
+	DEF_FIXED("cpex",	R8A77970_CLK_CPEX,  CLK_EXTAL,	    2, 1),
 
 	DEF_DIV6P1("canfd",	R8A77970_CLK_CANFD, CLK_PLL1_DIV4, 0x244),
 	DEF_DIV6P1("mso",	R8A77970_CLK_MSO,   CLK_PLL1_DIV4, 0x014),
