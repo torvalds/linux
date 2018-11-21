13f8d9c16693afb908ead3d2a758adbe6a79eccd
Yonatan Cohen <yonatanc@mellanox.com>
2018-11-21 13:48:39 +0200

    IB/mlx5: Fix XRC QP support after introducing extended atomic
    Extended atomics are supported with RC and XRC QP types, but the commit
    citied in the Fixes line added an unneeded check to
    to_mlx5_access_flags. This broke XRC QPs.
    
    The following ib_atomic_bw invocation over XRC reproduces the issue:
       ib_atomic_bw -d mlx5_1 --connection=XRC --atomic_type=FETCH_AND_ADD
    
    It is safe to remove such checks because the QP type was already checked
    in ib_modify_qp_is_ok(), which was previously called from
    mlx5_ib_modify_qp.
    
    Fixes: a60109dc9a95 ("IB/mlx5: Add support for extended atomic operations")
    Signed-off-by: Yonatan Cohen <yonatanc@mellanox.com>
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- drivers/infiniband/hw/mlx5/qp.c
+++ drivers/infiniband/hw/mlx5/qp.c
@@ -2633,8 +2633,7 @@ static int to_mlx5_access_flags(struct mlx5_ib_qp *qp,
 
 	if (access_flags & IB_ACCESS_REMOTE_READ)
 		*hw_access_flags |= MLX5_QP_BIT_RRE;
-	if ((access_flags & IB_ACCESS_REMOTE_ATOMIC) &&
-	    qp->ibqp.qp_type == IB_QPT_RC) {
+	if (access_flags & IB_ACCESS_REMOTE_ATOMIC) {
 		int atomic_mode;
 
 		atomic_mode = get_atomic_mode(dev, qp->ibqp.qp_type);
