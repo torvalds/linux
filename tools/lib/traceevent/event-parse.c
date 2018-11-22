0631ca3a6e6edd23a2ca7cab707d1abf291a097d
Adrian Hunter <adrian.hunter@intel.com>
2018-11-22 13:29:37 +0200

    tools lib traceevent: Fix compile warnings in tools/lib/traceevent/event-parse.c
    Fix following warnings:
    
      event-parse.c: In function ‘tep_find_event_by_name’:
      event-parse.c:3521:21: warning: ‘event’ may be used uninitialized in this function [-Wmaybe-uninitialized]
        pevent->last_event = event;
        ~~~~~~~~~~~~~~~~~~~^~~~~~~
        CC       ui/gtk/hists.o
        LINK     plugin_mac80211.so
        CC       nlattr.o
      event-parse.c: In function ‘tep_data_lat_fmt’:
      event-parse.c:5200:4: warning: ‘migrate_disable’ may be used uninitialized in this function [-Wmaybe-uninitialized]
          trace_seq_printf(s, "%d", migrate_disable);
          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      event-parse.c:5207:4: warning: ‘lock_depth’ may be used uninitialized in this function [-Wmaybe-uninitialized]
          trace_seq_printf(s, "%d", lock_depth);
          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        LINK     plugin_sched_switch.so
        LINK     plugin_function.so
        LINK     plugin_xen.so
      event-parse.c: In function ‘tep_event_info’:
      event-parse.c:5047:7: warning: ‘len_arg’ may be used uninitialized in this function [-Wmaybe-uninitialized]
             trace_seq_printf(s, format, len_arg, (char)val);
             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      event-parse.c:4884:6: note: ‘len_arg’ was declared here
        int len_arg;
            ^~~~~~~
      event-parse.c:4338:11: warning: ‘vsize’ may be used uninitialized in this function [-Wmaybe-uninitialized]
           val = tep_read_number(pevent, bptr, vsize);
                 ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      event-parse.c:4224:6: note: ‘vsize’ was declared here
        int vsize;
            ^~~~~
    
    $ gcc --version
      gcc (Clear Linux OS for Intel Architecture) 8.2.1 20180502
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Cc: Tzvetomir Stoyanov (VMware) <tz.stoyanov@gmail.com>
    Link: http://lkml.kernel.org/r/20181122112937.10582-1-adrian.hunter@intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/lib/traceevent/event-parse.c
+++ tools/lib/traceevent/event-parse.c
@@ -3498,7 +3498,7 @@ struct tep_event_format *
 tep_find_event_by_name(struct tep_handle *pevent,
 		       const char *sys, const char *name)
 {
-	struct tep_event_format *event;
+	struct tep_event_format *event = NULL;
 	int i;
 
 	if (pevent->last_event &&
@@ -4221,7 +4221,7 @@ static struct tep_print_arg *make_bprint_args(char *fmt, void *data, int size, s
 	unsigned long long ip, val;
 	char *ptr;
 	void *bptr;
-	int vsize;
+	int vsize = 0;
 
 	field = pevent->bprint_buf_field;
 	ip_field = pevent->bprint_ip_field;
@@ -4881,7 +4881,7 @@ static void pretty_print(struct trace_seq *s, void *data, int size, struct tep_e
 	char format[32];
 	int show_func;
 	int len_as_arg;
-	int len_arg;
+	int len_arg = 0;
 	int len;
 	int ls;
 
@@ -5146,8 +5146,8 @@ void tep_data_lat_fmt(struct tep_handle *pevent,
 	static int migrate_disable_exists;
 	unsigned int lat_flags;
 	unsigned int pc;
-	int lock_depth;
-	int migrate_disable;
+	int lock_depth = 0;
+	int migrate_disable = 0;
 	int hardirq;
 	int softirq;
 	void *data = record->data;
