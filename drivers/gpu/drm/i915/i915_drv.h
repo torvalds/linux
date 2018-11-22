835cb5c500b15f4acab7a4c86275997994517c2f
Jani Nikula <jani.nikula@intel.com>
2018-11-22 16:03:03 +0200

    drm/i915: Update DRIVER_DATE to 20181122
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -88,8 +88,8 @@
 
 #define DRIVER_NAME		"i915"
 #define DRIVER_DESC		"Intel Graphics"
-#define DRIVER_DATE		"20181102"
-#define DRIVER_TIMESTAMP	1541153051
+#define DRIVER_DATE		"20181122"
+#define DRIVER_TIMESTAMP	1542895382
 
 /* Use I915_STATE_WARN(x) and I915_STATE_WARN_ON() (rather than WARN() and
  * WARN_ON()) for hw state sanity checks to check for unexpected conditions
