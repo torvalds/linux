3e27c79c4b121a64e85f26ab8957e5d0a09ff28e
Viresh Kumar <viresh.kumar@linaro.org>
2018-11-23 10:36:07 +0530

    OPP: Fix parsing of multiple phandles in "operating-points-v2" property
    We currently return error if more than one phandle is present in the
    "operating-points-v2" property, which is incorrect. We only want to
    check the count of phandles here and set index to 0 if only one phandle
    is present.
    
    Fix it.
    
    Fixes: 5ed4cecd75e9 ("OPP: Pass OPP table to _of_add_opp_table_v{1|2}()")
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/opp/of.c
+++ drivers/opp/of.c
@@ -579,10 +579,8 @@ int dev_pm_opp_of_add_table_indexed(struct device *dev, int index)
 		 */
 		count = of_count_phandle_with_args(dev->of_node,
 						   "operating-points-v2", NULL);
-		if (count != 1)
-			return -ENODEV;
-
-		index = 0;
+		if (count == 1)
+			index = 0;
 	}
 
 	opp_table = dev_pm_opp_get_opp_table_indexed(dev, index);
