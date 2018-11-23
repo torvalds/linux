b36d95846117dc47cd5a7e854a862aa2b509026b
Paul Kocialkowski <paul.kocialkowski@bootlin.com>
2018-11-23 10:24:54 +0100

    drm/sun4i: frontend: Add support for the BGRX8888 output format
    This introduces support for the BGRX8888 output format for the frontend,
    with its associated output format value definition.
    
    Signed-off-by: Paul Kocialkowski <paul.kocialkowski@bootlin.com>
    Signed-off-by: Maxime Ripard <maxime.ripard@bootlin.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181123092515.2511-23-paul.kocialkowski@bootlin.com

--- drivers/gpu/drm/sun4i/sun4i_frontend.c
+++ drivers/gpu/drm/sun4i/sun4i_frontend.c
@@ -145,6 +145,10 @@ static int sun4i_frontend_drm_format_to_input_sequence(uint32_t fmt, u32 *val)
 static int sun4i_frontend_drm_format_to_output_fmt(uint32_t fmt, u32 *val)
 {
 	switch (fmt) {
+	case DRM_FORMAT_BGRX8888:
+		*val = SUN4I_FRONTEND_OUTPUT_FMT_DATA_FMT_BGRX8888;
+		return 0;
+
 	case DRM_FORMAT_XRGB8888:
 		*val = SUN4I_FRONTEND_OUTPUT_FMT_DATA_FMT_XRGB8888;
 		return 0;
