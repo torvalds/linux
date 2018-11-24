07e00822cca5975b6df245554b8495dfee8482e0
Matheus Tavares <matheus.bernardino@usp.br>
2018-11-23 22:23:10 -0200

    staging:iio:ad2s90: Replace license text w/ SPDX identifier
    This patch removes the license boilerplate text at the top of ad2s90.c
    and, instead, adds the SPDX GPL-2.0 license identifier, which solves the
    checkpatch.pl warning:
    "WARNING: Missing or malformed SPDX-License-Identifier tag in line 1".
    
    Signed-off-by: Matheus Tavares <matheus.bernardino@usp.br>
    Acked-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/staging/iio/resolver/ad2s90.c
+++ drivers/staging/iio/resolver/ad2s90.c
@@ -1,12 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * ad2s90.c simple support for the ADI Resolver to Digital Converters: AD2S90
  *
  * Copyright (c) 2010-2010 Analog Devices Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
  */
 #include <linux/types.h>
 #include <linux/mutex.h>
