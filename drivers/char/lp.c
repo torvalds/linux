1c3de93621b310276ae4d966a14ed01b711312c2
Sudip Mukherjee <sudipm.mukherjee@gmail.com>
2018-11-25 19:47:33 +0000

    char: lp: move trailing statement to next line
    Fix checkpatch errors for trailing if else statements.
    
    Signed-off-by: Sudip Mukherjee <sudipm.mukherjee@gmail.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/char/lp.c
+++ drivers/char/lp.c
@@ -223,12 +223,15 @@ static void lp_error (int minor)
 		return;
 
 	polling = lp_table[minor].dev->port->irq == PARPORT_IRQ_NONE;
-	if (polling) lp_release_parport (&lp_table[minor]);
+	if (polling)
+		lp_release_parport (&lp_table[minor]);
 	prepare_to_wait(&lp_table[minor].waitq, &wait, TASK_INTERRUPTIBLE);
 	schedule_timeout(LP_TIMEOUT_POLLED);
 	finish_wait(&lp_table[minor].waitq, &wait);
-	if (polling) lp_claim_parport_or_block (&lp_table[minor]);
-	else parport_yield_blocking (lp_table[minor].dev);
+	if (polling)
+		lp_claim_parport_or_block (&lp_table[minor]);
+	else
+		parport_yield_blocking (lp_table[minor].dev);
 }
 
 static int lp_check_status(int minor)
