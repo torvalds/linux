f5d5510e7389fa264337fb524346bac9eb93adc8
Jaegeuk Kim <jaegeuk@kernel.org>
2018-11-26 14:20:32 -0800

    f2fs: avoid build warn of fall_through
    After merging the f2fs tree, today's linux-next build
     (x86_64_allmodconfig) produced this warning:
    
     In file included from fs/f2fs/dir.c:11:
     fs/f2fs/f2fs.h: In function '__mark_inode_dirty_flag':
     fs/f2fs/f2fs.h:2388:6: warning: this statement may fall through [-Wimplicit-fallthrough=]
        if (set)
           ^
     fs/f2fs/f2fs.h:2390:2: note: here
       case FI_DATA_EXIST:
       ^~~~
    
     Exposed by my use of -Wimplicit-fallthrough
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -2387,6 +2387,7 @@ static inline void __mark_inode_dirty_flag(struct inode *inode,
 	case FI_NEW_INODE:
 		if (set)
 			return;
+		/* fall through */
 	case FI_DATA_EXIST:
 	case FI_INLINE_DOTS:
 	case FI_PIN_FILE:
