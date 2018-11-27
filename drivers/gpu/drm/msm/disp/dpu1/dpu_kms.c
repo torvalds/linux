01665c64b47597753a57efabfea36e9cb2443cff
Jeykumar Sankaran <jsanka@codeaurora.org>
2018-11-27 15:58:13 -0800

    drm/msm/dpu: set geometry for iommu domain
    Specify geometry for DPU iommu domain which sets
    the address space for gem allocations.
    
    Signed-off-by: Jeykumar Sankaran <jsanka@codeaurora.org>
    Suggested-by: Jordan Crouse <jcrouse@codeaurora.org>
    Suggested-by: Vivek Gautam <vivek.gautam@codeaurora.org>
    Acked-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
@@ -788,6 +788,9 @@ static int _dpu_kms_mmu_init(struct dpu_kms *dpu_kms)
 	if (!domain)
 		return 0;
 
+	domain->geometry.aperture_start = 0x1000;
+	domain->geometry.aperture_end = 0xffffffff;
+
 	aspace = msm_gem_address_space_create(dpu_kms->dev->dev,
 			domain, "dpu1");
 	if (IS_ERR(aspace)) {
