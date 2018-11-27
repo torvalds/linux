2b78cf0ce7c97bff53e8c4adfd4d4acb23776d37
Young_X <YangX92@hotmail.com>
2018-11-27 06:33:16 +0000

    ASoC: au8540: use 64-bit arithmetic instead of 32-bit
    Add suffix ULL to constant 256 in order to give the compiler complete
    information about the proper arithmetic to use.
    
    Notice that such constant is used in a context that expects an
    expression of type u64 (64 bits, unsigned) and the following
    expression is currently being evaluated using 32-bit arithmetic:
    
        256 * fs * 2 * mclk_src_scaling[i].param
    
    Signed-off-by: Young_X <YangX92@hotmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit cd7fdc45bc69a62b4e22c6e875f1f1aea566256d)

--- sound/soc/codecs/nau8540.c
+++ sound/soc/codecs/nau8540.c
@@ -585,7 +585,7 @@ static int nau8540_calc_fll_param(unsigned int fll_in,
 	fvco_max = 0;
 	fvco_sel = ARRAY_SIZE(mclk_src_scaling);
 	for (i = 0; i < ARRAY_SIZE(mclk_src_scaling); i++) {
-		fvco = 256 * fs * 2 * mclk_src_scaling[i].param;
+		fvco = 256ULL * fs * 2 * mclk_src_scaling[i].param;
 		if (fvco > NAU_FVCO_MIN && fvco < NAU_FVCO_MAX &&
 			fvco_max < fvco) {
 			fvco_max = fvco;
