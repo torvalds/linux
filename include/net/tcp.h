85bdf7db5b53cdcc7a901db12bcb3d0063e3866d
Eric Dumazet <edumazet@google.com>
2018-11-27 14:42:02 -0800

    tcp: make tcp_space() aware of socket backlog
    Jean-Louis Dupond reported poor iscsi TCP receive performance
    that we tracked to backlog drops.
    
    Apparently we fail to send window updates reflecting the
    fact that we are under stress.
    
    Note that we might lack a proper window increase when
    backlog is fully processed, since __release_sock() clears
    sk->sk_backlog.len _after_ all skbs have been processed.
    
    This should not matter in practice. If we had a significant
    load through socket backlog, we are in a dangerous
    situation.
    
    Reported-by: Jean-Louis Dupond <jean-louis@dupond.be>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Neal Cardwell <ncardwell@google.com>
    Acked-by: Yuchung Cheng <ycheng@google.com>
    Tested-by: Jean-Louis Dupond<jean-louis@dupond.be>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -1368,7 +1368,7 @@ static inline int tcp_win_from_space(const struct sock *sk, int space)
 /* Note: caller must be prepared to deal with negative returns */
 static inline int tcp_space(const struct sock *sk)
 {
-	return tcp_win_from_space(sk, sk->sk_rcvbuf -
+	return tcp_win_from_space(sk, sk->sk_rcvbuf - sk->sk_backlog.len -
 				  atomic_read(&sk->sk_rmem_alloc));
 }
 
