8b955e00d4a56a36763e9349a78483515560a3ca
Roman Li <Roman.Li@amd.com>
2018-11-27 17:16:37 -0500

    drm/amd/display: Fix 6x4K displays light-up on Vega20
    [Why]
    More than 4x4K didn't lightup on Vega20 due to low dcfclk value.
    Powerplay expects valid min requirement for dcfclk from DC.
    
    [How]
    Update min_dcfclock_khz based on min_engine_clock value.
    
    Reviewed-by: Hersen Wu <hersenxs.wu@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>
    Reviewed-by: Evan Quan <evan.quan@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Roman Li <Roman.Li@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dce/dce_clk_mgr.c
+++ drivers/gpu/drm/amd/display/dc/dce/dce_clk_mgr.c
@@ -591,6 +591,8 @@ static void dce11_pplib_apply_display_requirements(
 			dc,
 			context->bw.dce.sclk_khz);
 
+	pp_display_cfg->min_dcfclock_khz = pp_display_cfg->min_engine_clock_khz;
+
 	pp_display_cfg->min_engine_clock_deep_sleep_khz
 			= context->bw.dce.sclk_deep_sleep_khz;
 
