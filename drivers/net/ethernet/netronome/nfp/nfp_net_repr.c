9db8bbcb9bc68e7f58e13d30eec87905e2b8825d
Jakub Kicinski <jakub.kicinski@netronome.com>
2018-11-27 22:24:53 -0800

    nfp: run representor TX locklessly
    Our representors are software devices built on top of the PF
    vNIC, the only state they have are per-cpu stats, so make
    the TX run locklessly.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: John Hurley <john.hurley@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/netronome/nfp/nfp_net_repr.c
+++ drivers/net/ethernet/netronome/nfp/nfp_net_repr.c
@@ -299,6 +299,8 @@ int nfp_repr_init(struct nfp_app *app, struct net_device *netdev,
 
 	SWITCHDEV_SET_OPS(netdev, &nfp_port_switchdev_ops);
 
+	netdev->features |= NETIF_F_LLTX;
+
 	if (nfp_app_has_tc(app)) {
 		netdev->features |= NETIF_F_HW_TC;
 		netdev->hw_features |= NETIF_F_HW_TC;
