22569b881d373153ce0a214f89920851918dd77d
Christophe Leroy <christophe.leroy@c-s.fr>
2018-11-29 14:07:17 +0000

    powerpc/8xx: Enable 8M hugepage support with HW assistance
    HW assistance naturally supports 8M huge pages without
    further modifications.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/mm/tlb_nohash.c
+++ arch/powerpc/mm/tlb_nohash.c
@@ -97,6 +97,9 @@ struct mmu_psize_def mmu_psize_defs[MMU_PAGE_COUNT] = {
 		.shift	= 14,
 	},
 #endif
+	[MMU_PAGE_8M] = {
+		.shift	= 23,
+	},
 };
 #else
 struct mmu_psize_def mmu_psize_defs[MMU_PAGE_COUNT] = {
