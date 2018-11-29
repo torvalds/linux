5af543be14cdad5d515631c5fe6984813fe0db70
Christophe Leroy <christophe.leroy@c-s.fr>
2018-11-29 14:07:13 +0000

    powerpc/8xx: Temporarily disable 16k pages and hugepages
    In preparation of making use of hardware assistance in TLB handlers,
    this patch temporarily disables 16K pages and hugepages. The reason
    is that when using HW assistance in 4K pages mode, the linux model
    fit with the HW model for 4K pages and 8M pages.
    
    However for 16K pages and 512K mode some additional work is needed
    to get linux model fit with HW model.
    For the 8M pages, they will naturaly come back when we switch to
    HW assistance, without any additional handling.
    In order to keep the following patch smaller, the removal of the
    current special handling for 8M pages gets removed here as well.
    
    Therefore the 4K pages mode will be implemented first and without
    support for 512k hugepages. Then the 512k hugepages will be brought
    back. And the 16K pages will be implemented in the following step.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/mm/tlb_nohash.c
+++ arch/powerpc/mm/tlb_nohash.c
@@ -97,12 +97,6 @@ struct mmu_psize_def mmu_psize_defs[MMU_PAGE_COUNT] = {
 		.shift	= 14,
 	},
 #endif
-	[MMU_PAGE_512K] = {
-		.shift	= 19,
-	},
-	[MMU_PAGE_8M] = {
-		.shift	= 23,
-	},
 };
 #else
 struct mmu_psize_def mmu_psize_defs[MMU_PAGE_COUNT] = {
