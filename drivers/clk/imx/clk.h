d3ff9728134eeaa13d5bbb23c3f2e188f99cfdf9
Abel Vesa <abel.vesa@nxp.com>
2018-12-01 10:52:14 +0000

    clk: imx: Add imx composite clock
    Since a lot of clocks on imx8m are formed by a mux, gate, predivider and
    divider, the idea here is to combine all of those into one composite clock,
    but we need to deal with both predivider and divider at the same time and
    therefore we add the imx8m_clk_composite_divider_ops and register
    the composite clock with those.
    
    Signed-off-by: Abel Vesa <abel.vesa@nxp.com>
    Suggested-by: Sascha Hauer <s.hauer@pengutronix.de>
    Reviewed-by: Sascha Hauer <s.hauer@pengutronix.de>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/imx/clk.h
+++ drivers/clk/imx/clk.h
@@ -227,4 +227,20 @@ struct clk *imx_clk_cpu(const char *name, const char *parent_name,
 		struct clk *div, struct clk *mux, struct clk *pll,
 		struct clk *step);
 
+struct clk *imx8m_clk_composite_flags(const char *name,
+					const char **parent_names,
+					int num_parents, void __iomem *reg,
+					unsigned long flags);
+
+#define __imx8m_clk_composite(name, parent_names, reg, flags) \
+	imx8m_clk_composite_flags(name, parent_names, \
+		ARRAY_SIZE(parent_names), reg, \
+		flags | CLK_SET_RATE_NO_REPARENT | CLK_OPS_PARENT_ENABLE)
+
+#define imx8m_clk_composite(name, parent_names, reg) \
+	__imx8m_clk_composite(name, parent_names, reg, 0)
+
+#define imx8m_clk_composite_critical(name, parent_names, reg) \
+	__imx8m_clk_composite(name, parent_names, reg, CLK_IS_CRITICAL)
+
 #endif
