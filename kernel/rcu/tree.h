b2c1955b88495c1531b2080ba4fad119c0a03cc1
Paul E. McKenney <paulmck@linux.ibm.com>
2018-11-30 19:12:04 -0800

    rcu: Remove unused rcu_cpu_kthread_cpu per-CPU variable
    The rcu_cpu_kthread_cpu used to provide debugfs information, but is no
    longer used.  This commit therefore removes it.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>

--- kernel/rcu/tree.h
+++ kernel/rcu/tree.h
@@ -408,10 +408,6 @@ static const char *tp_rcu_varname __used __tracepoint_string = rcu_name;
 
 int rcu_dynticks_snap(struct rcu_data *rdp);
 
-#ifdef CONFIG_RCU_BOOST
-DECLARE_PER_CPU(int, rcu_cpu_kthread_cpu);
-#endif /* #ifdef CONFIG_RCU_BOOST */
-
 /* Forward declarations for rcutree_plugin.h */
 static void rcu_bootup_announce(void);
 static void rcu_qs(void);
