092eb2b1e5e4c548fc7e80b377d5bc33003a1726
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2018-11-30 18:07:39 -0600

    ASoC: SOF: topology: use cpu_to_le32
    Fix sparse issue
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/sof/topology.c
+++ sound/soc/sof/topology.c
@@ -342,7 +342,7 @@ static int sof_control_load_bytes(struct snd_soc_component *scomp,
 			return -EINVAL;
 		}
 		if (cdata->data->size + sizeof(const struct sof_abi_hdr) !=
-		    control->priv.size) {
+		    le32_to_cpu(control->priv.size)) {
 			dev_err(sdev->dev,
 				"error: Conflict in bytes vs. priv size.\n");
 			return -EINVAL;
