aa5beec32e8b78bfcf621e3c3daebfb1644b6365
Eric Anholt <eric@anholt.net>
2018-12-03 14:24:38 -0800

    drm/v3d: Invalidate the caches from the outside in.
    This would be a fairly obscure race, but let's make sure we don't ever
    lose it.
    
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181203222438.25417-6-eric@anholt.net
    Reviewed-by: Dave Emett <david.emett@broadcom.com>

--- drivers/gpu/drm/v3d/v3d_gem.c
+++ drivers/gpu/drm/v3d/v3d_gem.c
@@ -171,11 +171,15 @@ v3d_invalidate_slices(struct v3d_dev *v3d, int core)
 void
 v3d_invalidate_caches(struct v3d_dev *v3d)
 {
+	/* Invalidate the caches from the outside in.  That way if
+	 * another CL's concurrent use of nearby memory were to pull
+	 * an invalidated cacheline back in, we wouldn't leave stale
+	 * data in the inner cache.
+	 */
 	v3d_flush_l3(v3d);
-
 	v3d_invalidate_l2c(v3d, 0);
-	v3d_invalidate_slices(v3d, 0);
 	v3d_flush_l2t(v3d, 0);
+	v3d_invalidate_slices(v3d, 0);
 }
 
 static void
