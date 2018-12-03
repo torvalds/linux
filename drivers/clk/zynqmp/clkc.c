9a43be9cedd516f188e6333d3b43402386723eff
Dan Carpenter <dan.carpenter@oracle.com>
2018-12-03 17:52:01 +0300

    clk: zynqmp: Off by one in zynqmp_is_valid_clock()
    The > comparison should be >= to prevent reading beyond the end of the
    clock[] array.
    
    (The clock[] array is allocated in zynqmp_clk_setup() and has
    clock_max_idx elements.)
    
    Fixes: 3fde0e16d016 ("drivers: clk: Add ZynqMP clock driver")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/zynqmp/clkc.c
+++ drivers/clk/zynqmp/clkc.c
@@ -128,7 +128,7 @@ static const struct zynqmp_eemi_ops *eemi_ops;
  */
 static inline int zynqmp_is_valid_clock(u32 clk_id)
 {
-	if (clk_id > clock_max_idx)
+	if (clk_id >= clock_max_idx)
 		return -ENODEV;
 
 	return clock[clk_id].valid;
