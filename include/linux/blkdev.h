6e0de61107f03c3222550d9b548cd331d31d82d1
Jens Axboe <axboe@kernel.dk>
2018-12-05 06:50:40 -0700

    blk-mq: remove QUEUE_FLAG_POLL from default MQ flags
    We only support polling if we have poll queues now, but the flag is
    being set by default. Remove the default QUEUE_FLAG_POLL setting, we'll
    set it in blk_mq_init_allocated_queue() if we have poll queues available
    for this device.
    
    Fixes: 6544d229bf43 ("block: enable polling by default if a poll map is initalized")
    Reported-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -606,8 +606,7 @@ struct request_queue {
 				 (1 << QUEUE_FLAG_ADD_RANDOM))
 
 #define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_IO_STAT) |		\
-				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
-				 (1 << QUEUE_FLAG_POLL))
+				 (1 << QUEUE_FLAG_SAME_COMP))
 
 void blk_queue_flag_set(unsigned int flag, struct request_queue *q);
 void blk_queue_flag_clear(unsigned int flag, struct request_queue *q);
