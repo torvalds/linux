23fe04c0c58c0432d26fd9c8d974d2fd9ab8a493
Mark Rutland <mark.rutland@arm.com>
2018-12-07 18:08:18 +0000

    arm64: smccc: use asm EXPORT_SYMBOL()
    For a while now it's been possible to use EXPORT_SYMBOL() in assembly
    files, which allows us to place exports immediately after assembly
    functions, as we do for C functions.
    
    As a step towards removing arm64ksyms.c, let's move the SMCCC exports to
    the assembly file the functions are defined in.
    
    There should be no functional change as a result of this patch.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/kernel/arm64ksyms.c
+++ arch/arm64/kernel/arm64ksyms.c
@@ -26,7 +26,6 @@
 #include <linux/syscalls.h>
 #include <linux/uaccess.h>
 #include <linux/io.h>
-#include <linux/arm-smccc.h>
 #include <linux/kprobes.h>
 
 #include <asm/checksum.h>
@@ -63,8 +62,4 @@ EXPORT_SYMBOL(__memmove);
 EXPORT_SYMBOL(_mcount);
 NOKPROBE_SYMBOL(_mcount);
 #endif
-
-	/* arm-smccc */
-EXPORT_SYMBOL(__arm_smccc_smc);
-EXPORT_SYMBOL(__arm_smccc_hvc);
 
