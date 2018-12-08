04fa26bab06d9335f15a5d529c4bba25cd507a34
Andrew Lunn <andrew@lunn.ch>
2018-12-08 16:12:12 +0100

    net: phy: mdio-gpio: Add platform_data support for phy_mask
    It is sometimes necessary to instantiate a bit-banging MDIO bus as a
    platform device, without the aid of device tree.
    
    When device tree is being used, the bus is not scanned for devices,
    only those devices which are in device tree are probed. Without device
    tree, by default, all addresses on the bus are scanned. This may then
    find a device which is not a PHY, e.g. a switch. And the switch may
    have registers containing values which look like a PHY. So during the
    scan, a PHY device is wrongly created.
    
    After the bus has been registered, a search is made for
    mdio_board_info structures which indicates devices on the bus, and the
    driver which should be used for them. This is typically used to
    instantiate Ethernet switches from platform drivers.  However, if the
    scanning of the bus has created a PHY device at the same location as
    indicated into the board info for a switch, the switch device is not
    created, since the address is already busy.
    
    This can be avoided by setting the phy_mask of the mdio bus. This mask
    prevents addresses on the bus being scanned.
    
    v2
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ include/linux/platform_data/mdio-gpio.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * MDIO-GPIO bus platform data structure
+ */
+
+#ifndef __LINUX_MDIO_GPIO_PDATA_H
+#define __LINUX_MDIO_GPIO_PDATA_H
+
+struct mdio_gpio_platform_data {
+	u32 phy_mask;
+};
+
+#endif /* __LINUX_MDIO_GPIO_PDATA_H */
