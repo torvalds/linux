a6c73a9fe7eae4c36884abfe980ef0fccd699085
Hans de Goede <hdegoede@redhat.com>
2018-12-08 14:01:13 +0100

    ASoC: Intel: bytcr_rt5640: Add quirk for the Prowise PT301 tablet
    Add a quirk for the Prowise PT301 tablet, this BYTCR tablet has no CHAN
    package in its ACPI tables and uses SSP0-AIF1 rather then SSP0-AIF2 which
    is the default for BYTCR devices.
    
    Also it uses IN1 for its MIC and JD2 for jack-detect, rather then the
    default IN3 and JD1.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Acked-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 271248f4c2bf56dc6b3582e37c7ac19dd483d989)

--- sound/soc/intel/boards/bytcr_rt5640.c
+++ sound/soc/intel/boards/bytcr_rt5640.c
@@ -674,6 +674,20 @@ static const struct dmi_system_id byt_rt5640_quirk_table[] = {
 					BYT_RT5640_MCLK_EN),
 	},
 	{
+		/* Prowise PT301 */
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Prowise"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "PT301"),
+		},
+		.driver_data = (void *)(BYT_RT5640_IN1_MAP |
+					BYT_RT5640_JD_SRC_JD2_IN4N |
+					BYT_RT5640_OVCD_TH_2000UA |
+					BYT_RT5640_OVCD_SF_0P75 |
+					BYT_RT5640_DIFF_MIC |
+					BYT_RT5640_SSP0_AIF1 |
+					BYT_RT5640_MCLK_EN),
+	},
+	{
 		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "TECLAST"),
 			DMI_MATCH(DMI_BOARD_NAME, "tPAD"),
