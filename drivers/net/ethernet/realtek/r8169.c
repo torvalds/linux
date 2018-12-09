ee28b30cbbe0b490f52ba222c9d392b99581545e
Heiner Kallweit <hkallweit1@gmail.com>
2018-12-09 22:05:11 +0100

    r8169: fix crash if CONFIG_DEBUG_SHIRQ is enabled
    If CONFIG_DEBUG_SHIRQ is enabled __free_irq() intentionally fires
    a spurious interrupt. This interrupt causes a crash because
    tp->dev->phydev is NULL at that time.
    
    Fixes: 38caff5a445b ("r8169: handle all interrupt events in the hard irq handler")
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/realtek/r8169.c
+++ drivers/net/ethernet/realtek/r8169.c
@@ -6469,7 +6469,7 @@ static irqreturn_t rtl8169_interrupt(int irq, void *dev_instance)
 		goto out;
 	}
 
-	if (status & LinkChg)
+	if (status & LinkChg && tp->dev->phydev)
 		phy_mac_interrupt(tp->dev->phydev);
 
 	if (unlikely(status & RxFIFOOver &&
