0a95cba52bf2fb90b15c22e43aca80a1ad10c9bd
Reinette Chatre <reinette.chatre@intel.com>
2018-12-10 13:21:54 -0800

    x86/intel_rdt: Ensure a CPU remains online for the region's pseudo-locking sequence
    commit 80b71c340f17705ec145911b9a193ea781811b16 upstream.
    
    The user triggers the creation of a pseudo-locked region when writing
    the requested schemata to the schemata resctrl file. The pseudo-locking
    of a region is required to be done on a CPU that is associated with the
    cache on which the pseudo-locked region will reside. In order to run the
    locking code on a specific CPU, the needed CPU has to be selected and
    ensured to remain online during the entire locking sequence.
    
    At this time, the cpu_hotplug_lock is not taken during the pseudo-lock
    region creation and it is thus possible for a CPU to be selected to run
    the pseudo-locking code and then that CPU to go offline before the
    thread is able to run on it.
    
    Fix this by ensuring that the cpu_hotplug_lock is taken while the CPU on
    which code has to run needs to be controlled. Since the cpu_hotplug_lock
    is always taken before rdtgroup_mutex the lock order is maintained.
    
    Fixes: e0bdfe8e36f3 ("x86/intel_rdt: Support creation/removal of pseudo-locked region")
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Fenghua Yu <fenghua.yu@intel.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: gavin.hindman@intel.com
    Cc: jithu.joseph@intel.com
    Cc: stable <stable@vger.kernel.org>
    Cc: x86-ml <x86@kernel.org>
    Link: https://lkml.kernel.org/r/b7b17432a80f95a1fa21a1698ba643014f58ad31.1544476425.git.reinette.chatre@intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kernel/cpu/intel_rdt_ctrlmondata.c
+++ arch/x86/kernel/cpu/intel_rdt_ctrlmondata.c
@@ -23,6 +23,7 @@
 
 #define pr_fmt(fmt)	KBUILD_MODNAME ": " fmt
 
+#include <linux/cpu.h>
 #include <linux/kernfs.h>
 #include <linux/seq_file.h>
 #include <linux/slab.h>
@@ -310,9 +311,11 @@ ssize_t rdtgroup_schemata_write(struct kernfs_open_file *of,
 		return -EINVAL;
 	buf[nbytes - 1] = '\0';
 
+	cpus_read_lock();
 	rdtgrp = rdtgroup_kn_lock_live(of->kn);
 	if (!rdtgrp) {
 		rdtgroup_kn_unlock(of->kn);
+		cpus_read_unlock();
 		return -ENOENT;
 	}
 	rdt_last_cmd_clear();
@@ -367,6 +370,7 @@ ssize_t rdtgroup_schemata_write(struct kernfs_open_file *of,
 
 out:
 	rdtgroup_kn_unlock(of->kn);
+	cpus_read_unlock();
 	return ret ?: nbytes;
 }
 
