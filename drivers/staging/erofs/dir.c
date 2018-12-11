019ec6c14fbdfaeb361c84f68158fb9097f41004
Aaron Strahlberger <aaron.strahlberger@posteo.de>
2018-12-11 11:54:41 +0100

    staging: erofs: Fix spelling issue
    Changed "stoped" to "stopped".
    
    Signed-off-by: Aaron Strahlberger <aaron.strahlberger@posteo.de>
    Signed-off-by: Julius Wiedmann <julius.wiedmann@fau.de>
    Signed-off-by: Dominik Huber <domi250@gmx.de>
    Reviewed-by: Gao Xiang <gaoxiang25@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/erofs/dir.c
+++ drivers/staging/erofs/dir.c
@@ -69,8 +69,8 @@ static int erofs_fill_dentries(struct dir_context *ctx,
 #endif
 
 		if (!dir_emit(ctx, de_name, de_namelen,
-					le64_to_cpu(de->nid), d_type))
-			/* stoped by some reason */
+			      le64_to_cpu(de->nid), d_type))
+			/* stopped by some reason */
 			return 1;
 		++de;
 		*ofs += sizeof(struct erofs_dirent);
