748fe0889c1ff12d378946bd5326e8ee8eacf5cf
Arnaldo Carvalho de Melo <acme@redhat.com>
2018-12-11 15:48:47 -0300

    perf tools: Add missing sigqueue() prototype for systems lacking it
    There are systems such as the Android NDK API level 24 has the
    sigqueue() function but doesn't provide a prototype, adding noise to the
    build:
    
      util/evlist.c: In function 'perf_evlist__prepare_workload':
      util/evlist.c:1494:4: warning: implicit declaration of function 'sigqueue' [-Wimplicit-function-declaration]
          if (sigqueue(getppid(), SIGUSR1, val))
          ^
      util/evlist.c:1494:4: warning: nested extern declaration of 'sigqueue' [-Wnested-externs]
    
    Define a LACKS_SIGQUEUE_PROTOTYPE define so that code needing that can
    get a prototype.
    
    Checked in the bionic git repo to be available since level 23:
    
    https://android.googlesource.com/platform/bionic/+/master/libc/include/signal.h#123
    
      int sigqueue(pid_t __pid, int __signal, const union sigval __value) __INTRODUCED_IN(23);
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Link: https://lkml.kernel.org/n/tip-lmhpev1uni9kdrv7j29glyov@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/evlist.c
+++ tools/perf/util/evlist.c
@@ -34,6 +34,10 @@
 #include <linux/log2.h>
 #include <linux/err.h>
 
+#ifdef LACKS_SIGQUEUE_PROTOTYPE
+int sigqueue(pid_t pid, int sig, const union sigval value);
+#endif
+
 #define FD(e, x, y) (*(int *)xyarray__entry(e->fd, x, y))
 #define SID(e, x, y) xyarray__entry(e->sample_id, x, y)
 
