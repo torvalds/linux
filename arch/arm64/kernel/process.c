0a1213fa7432778b71a1c0166bf56660a3aab030
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2018-12-12 13:08:44 +0100

    arm64: enable per-task stack canaries
    This enables the use of per-task stack canary values if GCC has
    support for emitting the stack canary reference relative to the
    value of sp_el0, which holds the task struct pointer in the arm64
    kernel.
    
    The $(eval) extends KBUILD_CFLAGS at the moment the make rule is
    applied, which means asm-offsets.o (which we rely on for the offset
    value) is built without the arguments, and everything built afterwards
    has the options set.
    
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -59,7 +59,7 @@
 #include <asm/processor.h>
 #include <asm/stacktrace.h>
 
-#ifdef CONFIG_STACKPROTECTOR
+#if defined(CONFIG_STACKPROTECTOR) && !defined(CONFIG_STACKPROTECTOR_PER_TASK)
 #include <linux/stackprotector.h>
 unsigned long __stack_chk_guard __read_mostly;
 EXPORT_SYMBOL(__stack_chk_guard);
