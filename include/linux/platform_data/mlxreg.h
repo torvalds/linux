946e4e02b11889cb161b15ff4712a8ba21a50eb6
Vadim Pasternak <vadimp@mellanox.com>
2018-12-12 23:59:14 +0000

    platform_data/mlxreg: Add capability field to core platform data
    Add capability field to "mlxreg_core_platform_data" structure.
    The purpose of this register is to provide additional info to platform
    driver through the atribute related capability register.
    
    Signed-off-by: Vadim Pasternak <vadimp@mellanox.com>
    Signed-off-by: Darren Hart (VMware) <dvhart@infradead.org>

--- include/linux/platform_data/mlxreg.h
+++ include/linux/platform_data/mlxreg.h
@@ -61,6 +61,7 @@ struct mlxreg_hotplug_device {
  * @reg: attribute register;
  * @mask: attribute access mask;
  * @bit: attribute effective bit;
+ * @capability: attribute capability register;
  * @mode: access mode;
  * @np - pointer to node platform associated with attribute;
  * @hpdev - hotplug device data;
@@ -72,6 +73,7 @@ struct mlxreg_core_data {
 	u32 reg;
 	u32 mask;
 	u32 bit;
+	u32 capability;
 	umode_t	mode;
 	struct device_node *np;
 	struct mlxreg_hotplug_device hpdev;
