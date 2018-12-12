d84273453689038be914a31b28f372147ceafee6
Sakari Ailus <sakari.ailus@linux.intel.com>
2018-12-12 06:13:10 -0500

    media: ipu3-imgu: Fix firmware binary location
    The firmware binary is located under "intel" directory in the
    linux-firmware repository. Reflect this in the driver.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/staging/media/ipu3/ipu3-css-fw.h
+++ drivers/staging/media/ipu3/ipu3-css-fw.h
@@ -6,7 +6,7 @@
 
 /******************* Firmware file definitions *******************/
 
-#define IMGU_FW_NAME			"ipu3-fw.bin"
+#define IMGU_FW_NAME			"intel/ipu3-fw.bin"
 
 typedef u32 imgu_fw_ptr;
 
