a062d16449c0d2e00d5a54123c9cfadea3f6c763
Vincent Guittot <vincent.guittot@linaro.org>
2018-12-14 17:01:56 +0100

    sched/fair: Trigger asym_packing during idle load balance
    Newly idle load balancing is not always triggered when a CPU becomes idle.
    This prevents the scheduler from getting a chance to migrate the task
    for asym packing.
    
    Enable active migration during idle load balance too.
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: valentin.schneider@arm.com
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8832,7 +8832,7 @@ static int need_active_balance(struct lb_env *env)
 {
 	struct sched_domain *sd = env->sd;
 
-	if (env->idle == CPU_NEWLY_IDLE) {
+	if (env->idle != CPU_NOT_IDLE) {
 
 		/*
 		 * ASYM_PACKING needs to force migrate tasks from busy but
