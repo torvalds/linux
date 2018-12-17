7edcbbf29006be46d5dbce5bac9aae73f3056d14
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2018-12-17 12:22:43 +0100

    ACPI: PM: Loop in full LPS0 mode only
    After a previous change, all non-wakeup GPEs are disabled for
    suspend-to-idle unless full Low-Power S0 (LPS0) mode is in use, so
    it is not necessary to do anything in acpi_s2idle_wake() unless in
    full LPS0 mode, which is only when lps0_device_handle is set.
    
    Modify the code accordingly.
    
    Tested-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/sleep.c
+++ drivers/acpi/sleep.c
@@ -985,6 +985,8 @@ static int acpi_s2idle_prepare(void)
 
 static void acpi_s2idle_wake(void)
 {
+	if (!lps0_device_handle)
+		return;
 
 	if (pm_debug_messages_on)
 		lpi_check_constraints();
@@ -1003,8 +1005,7 @@ static void acpi_s2idle_wake(void)
 		 * takes too much time for EC wakeup events to survive, so look
 		 * for them now.
 		 */
-		if (lps0_device_handle)
-			acpi_ec_dispatch_gpe();
+		acpi_ec_dispatch_gpe();
 	}
 }
 
