7c617e0c5f37d2f681b3a568c3642115a32b7427
Alexandre Belloni <alexandre.belloni@bootlin.com>
2018-12-18 22:52:12 +0100

    rtc: pcf8523: Add rv8523 compatible
    The Microcrystal RV-8523 is compatible with the PCF8523.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/rtc/rtc-pcf8523.c
+++ drivers/rtc/rtc-pcf8523.c
@@ -374,6 +374,7 @@ MODULE_DEVICE_TABLE(i2c, pcf8523_id);
 #ifdef CONFIG_OF
 static const struct of_device_id pcf8523_of_match[] = {
 	{ .compatible = "nxp,pcf8523" },
+	{ .compatible = "microcrystal,rv8523" },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, pcf8523_of_match);
