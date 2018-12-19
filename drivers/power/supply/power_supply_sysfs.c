e58a053453641e4b1b6c84ad3f91bed2a8ebd259
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2018-12-18 20:16:06 -0800

    ANDROID: GKI: power_supply: add batt_age_level property
    Add batt_age_level property to support loading multiple profiles
    based on the battery age level specified by the user.
    
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>
    (cherry picked from commit 0fe2e0aadcff78083a7f85f74c6e931a7c74dcc1)
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 150789066
    Change-Id: I41e61c615f2d1df988a0de83e4e2dbddd7a36441

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -450,6 +450,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(fg_reset),
 	POWER_SUPPLY_ATTR(qc_opti_disable),
 	POWER_SUPPLY_ATTR(cc_soc),
+	POWER_SUPPLY_ATTR(batt_age_level),
 	/* Charge pump properties */
 	POWER_SUPPLY_ATTR(cp_status1),
 	POWER_SUPPLY_ATTR(cp_status2),
