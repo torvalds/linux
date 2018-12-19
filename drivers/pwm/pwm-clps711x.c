f236d188683667274824de662d124720198730b7
Alexander Shiyan <shc_work@mail.ru>
2018-12-19 13:39:25 +0300

    pwm: clps711x: Switch to SPDX identifier
    Adopt the SPDX license identifier headers to ease license compliance
    management.
    
    Signed-off-by: Alexander Shiyan <shc_work@mail.ru>
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- drivers/pwm/pwm-clps711x.c
+++ drivers/pwm/pwm-clps711x.c
@@ -1,12 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
  * Cirrus Logic CLPS711X PWM driver
- *
- * Copyright (C) 2014 Alexander Shiyan <shc_work@mail.ru>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Author: Alexander Shiyan <shc_work@mail.ru>
  */
 
 #include <linux/clk.h>
