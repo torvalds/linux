2521529422b974738a97308c82e6f953d06bf309
Elaine Zhang <zhangqing@rock-chips.com>
2018-12-20 15:05:00 +0800

    clk: rockchip: rk3366: add FRAC_MAX_PRATE limit for spdif/uart/i2s
    Change-Id: I9bcc2431708398d07ba9b29a41f1c50b29fcf8e7
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>

--- drivers/clk/rockchip/clk-rk3366.c
+++ drivers/clk/rockchip/clk-rk3366.c
@@ -21,6 +21,9 @@
 #include "clk.h"
 
 #define RK3366_GRF_SOC_STATUS0	0x480
+#define RK3366_I2S_FRAC_MAX_PRATE       600000000
+#define RK3366_UART_FRAC_MAX_PRATE	600000000
+#define RK3366_SPDIF_FRAC_MAX_PRATE	600000000
 
 enum rk3366_plls {
 	apll, dpll, cpll, gpll, npll, mpll, wpll, bpll,
@@ -305,7 +308,7 @@ static struct rockchip_clk_branch rk3366_clk_branches[] __initdata = {
 	COMPOSITE_FRACMUX(0, "i2s_8ch_frac", "i2s_8ch_src", CLK_SET_RATE_PARENT,
 			RK3368_CLKSEL_CON(28), 0,
 			RK3368_CLKGATE_CON(6), 2, GFLAGS,
-			&rk3366_i2s_8ch_fracmux, 0),
+			&rk3366_i2s_8ch_fracmux, RK3366_I2S_FRAC_MAX_PRATE),
 	COMPOSITE_NODIV(SCLK_I2S_8CH_OUT, "i2s_8ch_clkout", mux_i2s_8ch_clkout_p, 0,
 			RK3368_CLKSEL_CON(27), 15, 1, MFLAGS,
 			RK3368_CLKGATE_CON(6), 0, GFLAGS),
@@ -318,7 +321,7 @@ static struct rockchip_clk_branch rk3366_clk_branches[] __initdata = {
 	COMPOSITE_FRACMUX(0, "spdif_8ch_frac", "spdif_8ch_src", CLK_SET_RATE_PARENT,
 			RK3368_CLKSEL_CON(32), 0,
 			RK3368_CLKGATE_CON(6), 5, GFLAGS,
-			&rk3366_spdif_8ch_fracmux, 0),
+			&rk3366_spdif_8ch_fracmux, RK3366_SPDIF_FRAC_MAX_PRATE),
 	GATE(SCLK_SPDIF_8CH, "sclk_spdif_8ch", "spdif_8ch_mux", CLK_SET_RATE_PARENT,
 			RK3368_CLKGATE_CON(6), 6, GFLAGS),
 
@@ -328,7 +331,7 @@ static struct rockchip_clk_branch rk3366_clk_branches[] __initdata = {
 	COMPOSITE_FRACMUX(0, "i2s_2ch_frac", "i2s_2ch_src", CLK_SET_RATE_PARENT,
 			RK3368_CLKSEL_CON(54), 0,
 			RK3368_CLKGATE_CON(5), 14, GFLAGS,
-			&rk3366_i2s_2ch_fracmux, 0),
+			&rk3366_i2s_2ch_fracmux, RK3366_I2S_FRAC_MAX_PRATE),
 	GATE(SCLK_I2S_2CH, "sclk_i2s_2ch", "i2s_2ch_mux", CLK_SET_RATE_PARENT,
 			RK3368_CLKGATE_CON(5), 15, GFLAGS),
 
@@ -547,7 +550,7 @@ static struct rockchip_clk_branch rk3366_clk_branches[] __initdata = {
 	COMPOSITE_FRACMUX(0, "uart0_frac", "uart0_src", CLK_SET_RATE_PARENT,
 			RK3368_CLKSEL_CON(34), 0,
 			RK3368_CLKGATE_CON(2), 1, GFLAGS,
-			&rk3366_uart0_fracmux, 0),
+			&rk3366_uart0_fracmux, RK3366_UART_FRAC_MAX_PRATE),
 
 	COMPOSITE_NOMUX(0, "uart3_src", "uart_src", 0,
 			RK3368_CLKSEL_CON(39), 0, 7, DFLAGS,
@@ -555,7 +558,7 @@ static struct rockchip_clk_branch rk3366_clk_branches[] __initdata = {
 	COMPOSITE_FRACMUX(0, "uart3_frac", "uart3_src", CLK_SET_RATE_PARENT,
 			RK3368_CLKSEL_CON(40), 0,
 			RK3368_CLKGATE_CON(2), 7, GFLAGS,
-			&rk3366_uart3_fracmux, 0),
+			&rk3366_uart3_fracmux, RK3366_UART_FRAC_MAX_PRATE),
 
 	/*
 	 * Clock-Architecture Diagram 6
