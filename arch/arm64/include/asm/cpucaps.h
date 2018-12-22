552ae76face5584085845646c5f57e10c1a4ebdc
Marc Zyngier <maz@kernel.org>
2018-12-22 12:00:10 +0000

    arm64: Detect the ARMv8.4 TTL feature
    In order to reduce the cost of TLB invalidation, the ARMv8.4 TTL
    feature allows TLBs to be issued with a level allowing for quicker
    invalidation.
    
    Let's detect the feature for now. Further patches will implement
    its actual usage.
    
    Reviewed-by : Suzuki K Polose <suzuki.poulose@arm.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -62,7 +62,8 @@
 #define ARM64_HAS_GENERIC_AUTH			52
 #define ARM64_HAS_32BIT_EL1			53
 #define ARM64_BTI				54
+#define ARM64_HAS_ARMv8_4_TTL			55
 
-#define ARM64_NCAPS				55
+#define ARM64_NCAPS				56
 
 #endif /* __ASM_CPUCAPS_H */
