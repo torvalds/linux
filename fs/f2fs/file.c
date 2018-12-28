8a4ef7d33c074034d3c0e9d7fde9e8a703d9e8a5
Jaegeuk Kim <jaegeuk@kernel.org>
2018-12-28 11:00:38 -0800

    f2fs: wait on atomic writes to count F2FS_CP_WB_DATA
    BugLink: https://bugs.launchpad.net/bugs/1821074
    
    commit 31867b23d7d1ee3535136c6a410a6cf56f666bfc upstream.
    
    Otherwise, we can get wrong counts incurring checkpoint hang.
    
    IO_W (CP:  -24, Data:   24, Flush: (   0    0    1), Discard: (   0    0))
    
    Thread A                        Thread B
    - f2fs_write_data_pages
     -  __write_data_page
      - f2fs_submit_page_write
       - inc_page_count(F2FS_WB_DATA)
         type is F2FS_WB_DATA due to file is non-atomic one
    - f2fs_ioc_start_atomic_write
     - set_inode_flag(FI_ATOMIC_FILE)
                                    - f2fs_write_end_io
                                     - dec_page_count(F2FS_WB_CP_DATA)
                                       type is F2FS_WB_DATA due to file becomes
                                       atomic one
    
    Cc: <stable@vger.kernel.org>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -1750,10 +1750,12 @@ static int f2fs_ioc_start_atomic_write(struct file *filp)
 
 	down_write(&F2FS_I(inode)->i_gc_rwsem[WRITE]);
 
-	if (!get_dirty_pages(inode))
-		goto skip_flush;
-
-	f2fs_msg(F2FS_I_SB(inode)->sb, KERN_WARNING,
+	/*
+	 * Should wait end_io to count F2FS_WB_CP_DATA correctly by
+	 * f2fs_is_atomic_file.
+	 */
+	if (get_dirty_pages(inode))
+		f2fs_msg(F2FS_I_SB(inode)->sb, KERN_WARNING,
 		"Unexpected flush for atomic writes: ino=%lu, npages=%u",
 					inode->i_ino, get_dirty_pages(inode));
 	ret = filemap_write_and_wait_range(inode->i_mapping, 0, LLONG_MAX);
@@ -1761,7 +1763,7 @@ static int f2fs_ioc_start_atomic_write(struct file *filp)
 		up_write(&F2FS_I(inode)->i_gc_rwsem[WRITE]);
 		goto out;
 	}
-skip_flush:
+
 	set_inode_flag(inode, FI_ATOMIC_FILE);
 	clear_inode_flag(inode, FI_ATOMIC_REVOKE_REQUEST);
 	up_write(&F2FS_I(inode)->i_gc_rwsem[WRITE]);
