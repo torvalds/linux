993a110319a4a60aadbd02f6defdebe048f7773b
Dave Young <dyoung@redhat.com>
2018-12-28 09:12:47 +0800

    x86/kexec: Fix a kexec_file_load() failure
    Commit
    
      b6664ba42f14 ("s390, kexec_file: drop arch_kexec_mem_walk()")
    
    changed the behavior of kexec_locate_mem_hole(): it will try to allocate
    free memory only when kbuf.mem is initialized to zero.
    
    However, x86's kexec_file_load() implementation reuses a struct
    kexec_buf allocated on the stack and its kbuf.mem member gets set by
    each kexec_add_buffer() invocation.
    
    The second kexec_add_buffer() will reuse the same kbuf but not
    reinitialize kbuf.mem.
    
    Therefore, explictily reset kbuf.mem each time in order for
    kexec_locate_mem_hole() to locate a free memory region each time.
    
     [ bp: massage commit message. ]
    
    Fixes: b6664ba42f14 ("s390, kexec_file: drop arch_kexec_mem_walk()")
    Signed-off-by: Dave Young <dyoung@redhat.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Acked-by: Baoquan He <bhe@redhat.com>
    Cc: "Eric W. Biederman" <ebiederm@xmission.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: AKASHI Takahiro <takahiro.akashi@linaro.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Philipp Rudo <prudo@linux.vnet.ibm.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Cc: Yannik Sembritzki <yannik@sembritzki.me>
    Cc: Yi Wang <wang.yi59@zte.com.cn>
    Cc: kexec@lists.infradead.org
    Cc: x86-ml <x86@kernel.org>
    Link: https://lkml.kernel.org/r/20181228011247.GA9999@dhcp-128-65.nay.redhat.com

--- arch/x86/kernel/kexec-bzimage64.c
+++ arch/x86/kernel/kexec-bzimage64.c
@@ -434,6 +434,7 @@ static void *bzImage64_load(struct kimage *image, char *kernel,
 	kbuf.memsz = PAGE_ALIGN(header->init_size);
 	kbuf.buf_align = header->kernel_alignment;
 	kbuf.buf_min = MIN_KERNEL_LOAD_ADDR;
+	kbuf.mem = KEXEC_BUF_MEM_UNKNOWN;
 	ret = kexec_add_buffer(&kbuf);
 	if (ret)
 		goto out_free_params;
@@ -448,6 +449,7 @@ static void *bzImage64_load(struct kimage *image, char *kernel,
 		kbuf.bufsz = kbuf.memsz = initrd_len;
 		kbuf.buf_align = PAGE_SIZE;
 		kbuf.buf_min = MIN_INITRD_LOAD_ADDR;
+		kbuf.mem = KEXEC_BUF_MEM_UNKNOWN;
 		ret = kexec_add_buffer(&kbuf);
 		if (ret)
 			goto out_free_params;
