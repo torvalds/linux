173aec2d5a9fa5f40e462661a8283fcafe04764f
Christian Borntraeger <borntraeger@de.ibm.com>
2018-12-28 10:59:06 +0100

    KVM: s390: add enhanced sort facilty to cpu model
    This enables stfle.150 and adds the subfunctions for SORTL. Bit 150 is
    added to the list of facilities that will be enabled when there is no
    cpu model involved as sortl requires no additional handling from
    userspace, e.g. for migration.
    
    Please note that a cpu model enabled user space can and will have the
    final decision on the facility bits for a guests.
    
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Reviewed-by: Collin Walling <walling@linux.ibm.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>

--- arch/s390/tools/gen_facilities.c
+++ arch/s390/tools/gen_facilities.c
@@ -93,6 +93,7 @@ static struct facility_def facility_defs[] = {
 			131, /* enhanced-SOP 2 and side-effect */
 			139, /* multiple epoch facility */
 			146, /* msa extension 8 */
+			150, /* enhanced sort */
 			155, /* msa extension 9 */
 			-1  /* END */
 		}
