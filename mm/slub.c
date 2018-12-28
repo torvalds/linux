1265ef2de4cef44cec91ba47009958eec6162836
Wei Yang <richard.weiyang@gmail.com>
2018-12-28 00:33:06 -0800

    mm/slub.c: remove validation on cpu_slab in __flush_cpu_slab()
    cpu_slab is a per cpu variable which is allocated in all or none.  If a
    cpu_slab failed to be allocated, the slub is not usable.
    
    We could use cpu_slab without validation in __flush_cpu_slab().
    
    Link: http://lkml.kernel.org/r/20181103141218.22844-1-richard.weiyang@gmail.com
    Signed-off-by: Wei Yang <richard.weiyang@gmail.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/slub.c
+++ mm/slub.c
@@ -2317,12 +2317,10 @@ static inline void __flush_cpu_slab(struct kmem_cache *s, int cpu)
 {
 	struct kmem_cache_cpu *c = per_cpu_ptr(s->cpu_slab, cpu);
 
-	if (likely(c)) {
-		if (c->page)
-			flush_slab(s, c);
+	if (c->page)
+		flush_slab(s, c);
 
-		unfreeze_partials(s, c);
-	}
+	unfreeze_partials(s, c);
 }
 
 static void flush_cpu_slab(void *d)
