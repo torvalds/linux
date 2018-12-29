075df31aed44cd9b9f2fd6522b16183415ee3351
Linus Walleij <linus.walleij@linaro.org>
2018-12-29 15:49:08 +0100

    ARM: ixp4xx: Pass IRQ resource to beeper
    All IXP4xx devices except the beeper passes the IRQ as a
    resource, augment the NSLU2 beeper to do the same.
    
    This is a prerequisite for SPARSE_IRQ.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ixp4xx/nslu2-setup.c
+++ arch/arm/mach-ixp4xx/nslu2-setup.c
@@ -125,10 +125,18 @@ static struct platform_device nslu2_i2c_gpio = {
 	},
 };
 
+static struct resource nslu2_beeper_resources[] = {
+	{
+		.start	= IRQ_IXP4XX_TIMER2,
+		.flags	= IORESOURCE_IRQ,
+	},
+};
+
 static struct platform_device nslu2_beeper = {
 	.name			= "ixp4xx-beeper",
 	.id			= NSLU2_GPIO_BUZZ,
-	.num_resources		= 0,
+	.resource		= nslu2_beeper_resources,
+	.num_resources		= ARRAY_SIZE(nslu2_beeper_resources),
 };
 
 static struct resource nslu2_uart_resources[] = {
