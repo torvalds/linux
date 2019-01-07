a220765c613d1a5f07d5e3b05c8f28311c0a20b2
David Wu <david.wu@rock-chips.com>
2019-01-07 18:01:06 +0800

    iio: adc: rockchip_saradc: Just get referenced voltage once at probe
    The referenced voltage is not changed after initiation, so just only
    get referenced voltage once.
    
    Change-Id: I1eeab03f68855fafe010db328ec7bbcfa7d52310
    Signed-off-by: David Wu <david.wu@rock-chips.com>

--- drivers/iio/adc/rockchip_saradc.c
+++ drivers/iio/adc/rockchip_saradc.c
@@ -49,6 +49,7 @@ struct rockchip_saradc {
 	struct clk		*clk;
 	struct completion	completion;
 	struct regulator	*vref;
+	int			uv_vref;
 	struct reset_control	*reset;
 	const struct rockchip_saradc_data *data;
 	u16			last_val;
@@ -115,13 +116,7 @@ static int rockchip_saradc_read_raw(struct iio_dev *indio_dev,
 		mutex_unlock(&indio_dev->mlock);
 		return IIO_VAL_INT;
 	case IIO_CHAN_INFO_SCALE:
-		ret = regulator_get_voltage(info->vref);
-		if (ret < 0) {
-			dev_err(&indio_dev->dev, "failed to get voltage\n");
-			return ret;
-		}
-
-		*val = ret / 1000;
+		*val = info->uv_vref / 1000;
 		*val2 = chan->scan_type.realbits;
 		return IIO_VAL_FRACTIONAL_LOG2;
 	default:
@@ -493,6 +488,13 @@ static int rockchip_saradc_probe(struct platform_device *pdev)
 	if (ret) {
 		dev_err(&pdev->dev, "failed to register devm action, %d\n",
 			ret);
+		return ret;
+	}
+
+	info->uv_vref = regulator_get_voltage(info->vref);
+	if (info->uv_vref < 0) {
+		dev_err(&pdev->dev, "failed to get voltage\n");
+		ret = info->uv_vref;
 		return ret;
 	}
 
