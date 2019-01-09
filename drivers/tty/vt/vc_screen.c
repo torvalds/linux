fad08b205cf134759c28d182905bf27762017192
Nicolas Pitre <nicolas.pitre@linaro.org>
2019-01-08 22:55:04 -0500

    vcs: fasync(): make it consistent with poll()
    We use POLLPRI not POLLIN to wait for data with poll() as there is
    never any incoming data stream per se. Let's use the same semantic
    with fasync() for consistency, including the fact that a vt may go away.
    
    No known user space ever relied on the SIGIO reason so far, let alone
    FASYNC, so the risk of breakage is pretty much nonexistent.
    
    Signed-off-by: Nicolas Pitre <nico@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/vt/vc_screen.c
+++ drivers/tty/vt/vc_screen.c
@@ -93,9 +93,18 @@ vcs_notifier(struct notifier_block *nb, unsigned long code, void *_param)
 	struct vcs_poll_data *poll =
 		container_of(nb, struct vcs_poll_data, notifier);
 	int currcons = poll->cons_num;
+	int fa_band;
 
-	if (code != VT_UPDATE && code != VT_DEALLOCATE)
+	switch (code) {
+	case VT_UPDATE:
+		fa_band = POLL_PRI;
+		break;
+	case VT_DEALLOCATE:
+		fa_band = POLL_HUP;
+		break;
+	default:
 		return NOTIFY_DONE;
+	}
 
 	if (currcons == 0)
 		currcons = fg_console;
@@ -106,7 +115,7 @@ vcs_notifier(struct notifier_block *nb, unsigned long code, void *_param)
 
 	poll->event = code;
 	wake_up_interruptible(&poll->waitq);
-	kill_fasync(&poll->fasync, SIGIO, POLL_IN);
+	kill_fasync(&poll->fasync, SIGIO, fa_band);
 	return NOTIFY_OK;
 }
 
