2e5d949bdb79b7dee8307edd81e395578bcd566f
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2019-01-14 14:08:56 +0200

    ASoC: SOF: Add token for tone generator sample rate
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- include/uapi/sound/sof/tokens.h
+++ include/uapi/sound/sof/tokens.h
@@ -90,6 +90,9 @@
 #define SOF_TKN_INTEL_DMIC_PDM_CLK_EDGE		705
 #define SOF_TKN_INTEL_DMIC_PDM_SKEW		706
 
+/* Tone */
+#define SOF_TKN_TONE_SAMPLE_RATE		800
+
 #define SOF_TKN_EFFECT_TYPE                     900
 
 #endif
