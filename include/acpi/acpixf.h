075470d88930dd16858a844f9dfb611c11f798d3
Bob Moore <robert.moore@intel.com>
2019-01-14 09:55:26 -0800

    ACPICA: Update version to 20190108
    ACPICA commit aace846bb8f9b5877e100017f37fefc09dd2fde6
    
    Version 20190108
    
    Link: https://github.com/acpica/acpica/commit/aace846b
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Erik Schmauss <erik.schmauss@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/acpixf.h
+++ include/acpi/acpixf.h
@@ -12,7 +12,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20181213
+#define ACPI_CA_VERSION                 0x20190108
 
 #include <acpi/acconfig.h>
 #include <acpi/actypes.h>
