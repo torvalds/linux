cf6d25d82d2ecd7209e179fe9ba58a1e987a79ac
Matthias Reichl <hias@horus.com>
2019-01-15 17:51:07 +0100

    ASoC: core: Don't defer probe on optional, NULL components
    cpu and platform are optional components in DAI links. For example
    codec-codec links usually have no platform set.
    
    Call snd_soc_find_component only if the name or of_node of
    a cpu or platform is set. Otherwise it will return NULL and
    soc_init_dai_link bails out immediately with -EPROBE_DEFER,
    meaning registering a card with NULL cpu or platform in DAI links
    can never succeed.
    
    Fixes: 8780cf1142a5 ("ASoC: soc-core: defer card probe until all component is added to list")
    
    Signed-off-by: Matthias Reichl <hias@horus.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 2833548ecbb385a289124077ab4d812258a867d5)

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1151,7 +1151,8 @@ static int soc_init_dai_link(struct snd_soc_card *card,
 	 * Defer card registartion if platform dai component is not added to
 	 * component list.
 	 */
-	if (!soc_find_component(link->platform->of_node, link->platform->name))
+	if ((link->platform->of_node || link->platform->name) &&
+	    !soc_find_component(link->platform->of_node, link->platform->name))
 		return -EPROBE_DEFER;
 
 	/*
@@ -1170,7 +1171,8 @@ static int soc_init_dai_link(struct snd_soc_card *card,
 	 * Defer card registartion if cpu dai component is not added to
 	 * component list.
 	 */
-	if (!soc_find_component(link->cpu_of_node, link->cpu_name))
+	if ((link->cpu_of_node || link->cpu_name) &&
+	    !soc_find_component(link->cpu_of_node, link->cpu_name))
 		return -EPROBE_DEFER;
 
 	/*
