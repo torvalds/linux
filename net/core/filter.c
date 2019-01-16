c61c27687a5abce11431e6de1adb6e36099b9859
Mathieu Malaterre <malat@debian.org>
2019-01-16 20:35:41 +0100

    bpf: Correctly annotate implicit fall through in bpf_base_func_proto
    There is a plan to build the kernel with -Wimplicit-fallthrough and
    this place in the code produced a warnings (W=1).
    
    To preserve as much of the existing comment only change a ‘:’ into a ‘,’.
    This is enough change, to match the regular expression expected by GCC.
    
    This commit removes the following warning:
    
      net/core/filter.c:5310:6: warning: this statement may fall through [-Wimplicit-fallthrough=]
    
    Signed-off-by: Mathieu Malaterre <malat@debian.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- net/core/filter.c
+++ net/core/filter.c
@@ -5309,7 +5309,7 @@ bpf_base_func_proto(enum bpf_func_id func_id)
 	case BPF_FUNC_trace_printk:
 		if (capable(CAP_SYS_ADMIN))
 			return bpf_get_trace_printk_proto();
-		/* else: fall through */
+		/* else, fall through */
 	default:
 		return NULL;
 	}
