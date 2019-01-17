2193e751d63873f0911063eba92f49241fafb64f
Sahil Chandna <chandna@codeaurora.org>
2019-01-17 16:43:13 +0530

    ANDROID: GKI: power_supply: Add SCALE_MODE_EN power-supply property
    Add POWER_SUPPLY_PROP_SCALE_MODE_EN property to indicate
    the status of filtered voltage based SOC scaling.
    
    Signed-off-by: Sahil Chandna <chandna@codeaurora.org>
    (cherry picked from commit e9ab4eff9e48f99d318dd2bd3173a3a8cd0afd8e)
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 150789066
    Change-Id: I2d110348eb5ecc85847879b9bc783ddc39e934b7

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -451,6 +451,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(qc_opti_disable),
 	POWER_SUPPLY_ATTR(cc_soc),
 	POWER_SUPPLY_ATTR(batt_age_level),
+	POWER_SUPPLY_ATTR(scale_mode_en),
 	/* Charge pump properties */
 	POWER_SUPPLY_ATTR(cp_status1),
 	POWER_SUPPLY_ATTR(cp_status2),
