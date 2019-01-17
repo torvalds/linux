747bd2a36c9c9a0edd5f393664d962205b31f404
Thomas Gleixner <tglx@linutronix.de>
2019-01-18 00:14:21 +0100

    crypto: morus - Convert to SPDX license identifiers
    The license boiler plate text is not ideal for machine parsing. The kernel
    uses SPDX license identifiers for that purpose, which replace the boiler
    plate text.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ondrej Mosnacek <omosnacek@gmail.com>
    Cc: Herbert Xu <herbert@gondor.apana.org.au>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: linux-crypto@vger.kernel.org
    Acked-by: Ondrej Mosnacek <omosnacek@gmail.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/morus640.c
+++ crypto/morus640.c
@@ -1,13 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * The MORUS-640 Authenticated-Encryption Algorithm
  *
  * Copyright (c) 2016-2018 Ondrej Mosnacek <omosnacek@gmail.com>
  * Copyright (C) 2017-2018 Red Hat, Inc. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
  */
 
 #include <asm/unaligned.h>
