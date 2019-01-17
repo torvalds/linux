2f7726f955572e587d5f50fbe9b2deed5334bd90
Aubrey Li <aubrey.li@intel.com>
2019-01-18 02:38:20 +0800

    x86/fpu: Track AVX-512 usage of tasks
    User space tools which do automated task placement need information
    about AVX-512 usage of tasks, because AVX-512 usage could cause core
    turbo frequency drop and impact the running task on the sibling CPU.
    
    The XSAVE hardware structure has bits that indicate when valid state
    is present in registers unique to AVX-512 use.  Use these bits to
    indicate when AVX-512 has been in use and add per-task AVX-512 state
    timestamp tracking to context switch.
    
    Well-written AVX-512 applications are expected to clear the AVX-512
    state when not actively using AVX-512 registers, so the tracking
    mechanism is imprecise and can theoretically miss AVX-512 usage during
    context switch. But it has been measured to be precise enough to be
    useful under real-world workloads like tensorflow and linpack.
    
    If higher precision is required, suggest user space tools to use the
    PMU-based mechanisms in combination.
    
    Signed-off-by: Aubrey Li <aubrey.li@linux.intel.com>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Arjan van de Ven <arjan@linux.intel.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Fenghua Yu <fenghua.yu@intel.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Tim Chen <tim.c.chen@linux.intel.com>
    Cc: aubrey.li@intel.com
    Link: http://lkml.kernel.org/r/20190117183822.31333-1-aubrey.li@intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/fpu/internal.h
+++ arch/x86/include/asm/fpu/internal.h
@@ -376,6 +376,13 @@ static inline int copy_fpregs_to_fpstate(struct fpu *fpu)
 {
 	if (likely(use_xsave())) {
 		copy_xregs_to_kernel(&fpu->state.xsave);
+
+		/*
+		 * AVX512 state is tracked here because its use is
+		 * known to slow the max clock speed of the core.
+		 */
+		if (fpu->state.xsave.header.xfeatures & XFEATURE_MASK_AVX512)
+			fpu->avx512_timestamp = jiffies;
 		return 1;
 	}
 
