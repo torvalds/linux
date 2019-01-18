c2c7560f7a8d221d3bc01be3e3daa20a84980916
Maxime Ripard <maxime.ripard@bootlin.com>
2019-01-18 15:51:29 +0100

    drm/sun4i: Set the coef_rdy bit right after the coef have been set
    The COEF_RDY bit is used to tell the hardware that new FIR filters
    coefficients have been written to the registers and that the hardware
    should take them into account starting next frame.
    
    Reviewed-by: Paul Kocialkowski <paul.kocialkowski@bootlin.com>
    Signed-off-by: Maxime Ripard <maxime.ripard@bootlin.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190118145133.21281-20-paul.kocialkowski@bootlin.com

--- drivers/gpu/drm/sun4i/sun4i_frontend.c
+++ drivers/gpu/drm/sun4i/sun4i_frontend.c
@@ -96,6 +96,10 @@ static void sun4i_frontend_scaler_init(struct sun4i_frontend *frontend)
 			     sun4i_frontend_vert_coef[i]);
 	}
 
+	regmap_write_bits(frontend->regs,
+			  SUN4I_FRONTEND_FRM_CTRL_REG,
+			  SUN4I_FRONTEND_FRM_CTRL_COEF_RDY,
+			  SUN4I_FRONTEND_FRM_CTRL_COEF_RDY);
 }
 
 int sun4i_frontend_init(struct sun4i_frontend *frontend)
