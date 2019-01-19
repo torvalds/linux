1cf5cb155a435b00d4c0681c19bfc2267371f329
Elias Kouskoumvekakis <eliaskousk@gmail.com>
2019-01-19 03:43:38 +0200

    Use line buffered egrep in pts symlink oneliner of qemu lab script

--- tools/labs/qemu/qemu.sh
+++ tools/labs/qemu/qemu.sh
@@ -15,7 +15,7 @@ case $ARCH in
 	;;
 esac
 
-echo info chardev | nc -U -l qemu.mon | egrep -o "/dev/pts/[0-9]*" | xargs -I PTS ln -fs PTS serial.pts &
+echo info chardev | nc -U -l qemu.mon | egrep --line-buffered -o "/dev/pts/[0-9]*" | xargs -I PTS ln -fs PTS serial.pts &
 $qemu "$@" -monitor unix:qemu.mon
 rm qemu.mon 
 rm serial.pts
