84bfe20980b819baa731f6020172e923891ff449
Andrew Lunn <andrew@lunn.ch>
2019-01-21 19:05:51 +0100

    net: phy: Remove redundent License text when SPDX header is present
    The SPDX header makes any license text redundent. Remove it.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/bcm-phy-lib.c
+++ drivers/net/phy/bcm-phy-lib.c
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2015-2017 Broadcom
- *
- * This program is distributed "as is" WITHOUT ANY WARRANTY of any
- * kind, whether express or implied; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
  */
 
 #include "bcm-phy-lib.h"
