6d3d8065bb14dcd3d36b63f81fff9bc7f4388bda
Nathan Chancellor <natechancellor@gmail.com>
2019-01-21 16:52:15 -0700

    drm/amdkfd: Fix if preprocessor statement above kfd_fill_iolink_info_for_cpu
    Clang warns:
    
    drivers/gpu/drm/amd/amdgpu/../amdkfd/kfd_crat.c:866:5: warning:
    'CONFIG_X86_64' is not defined, evaluates to 0 [-Wundef]
        ^
    1 warning generated.
    
    Fixes: d1c234e2cd10 ("drm/amdkfd: Allow building KFD on ARM64 (v2)")
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_crat.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_crat.c
@@ -863,7 +863,7 @@ static int kfd_fill_mem_info_for_cpu(int numa_node_id, int *avail_size,
 	return 0;
 }
 
-#if CONFIG_X86_64
+#ifdef CONFIG_X86_64
 static int kfd_fill_iolink_info_for_cpu(int numa_node_id, int *avail_size,
 				uint32_t *num_entries,
 				struct crat_subtype_iolink *sub_type_hdr)
