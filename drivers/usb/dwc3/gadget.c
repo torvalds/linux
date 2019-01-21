28ebe0143087b53db8fa760034003c9ae80e5157
Felipe Balbi <felipe.balbi@linux.intel.com>
2019-01-21 13:01:16 +0200

    UPSTREAM: usb: dwc3: gadget: early giveback if End Transfer already completed
    There is a rare race condition that may happen during a Disconnect
    Interrupt if we have a started request that happens to be
    dequeued *after* completion of End Transfer command. If that happens,
    that request will be left waiting for completion of an End Transfer
    command that will never happen.
    
    If End Transfer command has already completed before, we are safe to
    giveback the request straight away.
    
    Change-Id: I98d4f8a42459d752be969cf3aabc9b01d5bcd212
    Tested-by: Thinh Nguyen <thinhn@synopsys.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>
    Signed-off-by: William Wu <william.wu@rock-chips.com>
    (cherry picked from commit 9f45581f5eec6786c6eded2b3c85345d82a910c9)

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1541,7 +1541,10 @@ static int dwc3_gadget_ep_dequeue(struct usb_ep *ep,
 				goto out0;
 
 			dwc3_gadget_move_cancelled_request(req);
-			goto out0;
+			if (dep->flags & DWC3_EP_TRANSFER_STARTED)
+				goto out0;
+			else
+				goto out1;
 		}
 		dev_err(dwc->dev, "request %pK was not queued to %s\n",
 				request, ep->name);
@@ -1549,6 +1552,7 @@ static int dwc3_gadget_ep_dequeue(struct usb_ep *ep,
 		goto out0;
 	}
 
+out1:
 	dwc3_gadget_giveback(dep, req, -ECONNRESET);
 
 out0:
