c8b6ac1a9d1fb26adaeaafc3fb872e6713dfeaf7
Kenneth D'souza <kdsouza@redhat.com>
2019-01-21 11:51:59 +1000

    CIFS: Show locallease in /proc/mounts for cifs shares mounted with locallease feature.
    Missing parameter that should be displayed in the mount list
    
    Reviewed-by: Ronnie Sahlberg <lsahlber@redhat.com>
    Signed-off-by: Kenneth D'souza <kdsouza@redhat.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/cifs/cifsfs.c
+++ fs/cifs/cifsfs.c
@@ -483,6 +483,8 @@ cifs_show_options(struct seq_file *s, struct dentry *root)
 		seq_puts(s, ",seal");
 	if (tcon->nocase)
 		seq_puts(s, ",nocase");
+	if (tcon->local_lease)
+		seq_puts(s, ",locallease");
 	if (tcon->retry)
 		seq_puts(s, ",hard");
 	else
