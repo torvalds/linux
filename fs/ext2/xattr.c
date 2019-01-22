032cdc39793fc814d78d7302c3e81048a76f3da5
Jan Kara <jack@suse.cz>
2019-01-22 12:35:36 +0100

    ext2: Set superblock revision when enabling xattr feature
    When setting the first xattr, we automatically enable
    EXT2_FEATURE_COMPAT_EXT_ATTR. However we forget to call
    ext2_update_dynamic_rev() so in theory if the filesystem was created as
    ancient one without features support, this could be missed. The
    consequences are minor anyway - since the feature is compat one, only
    old e2fsck which does not understand xattrs could do something bad.
    
    Reported-by: Andreas Dilger <adilger@dilger.ca>
    Signed-off-by: Jan Kara <jack@suse.cz>

--- fs/ext2/xattr.c
+++ fs/ext2/xattr.c
@@ -342,6 +342,7 @@ static void ext2_xattr_update_super_block(struct super_block *sb)
 		return;
 
 	spin_lock(&EXT2_SB(sb)->s_lock);
+	ext2_update_dynamic_rev(sb);
 	EXT2_SET_COMPAT_FEATURE(sb, EXT2_FEATURE_COMPAT_EXT_ATTR);
 	spin_unlock(&EXT2_SB(sb)->s_lock);
 	mark_buffer_dirty(EXT2_SB(sb)->s_sbh);
