4c3510483d26420d2c2c7cc075ad872286cc5932
Peter Oskolkov <posk@google.com>
2019-01-22 10:02:53 -0800

    selftests: net: ip_defrag: cover new IPv6 defrag behavior
    This patch adds several changes to the ip_defrag selftest, to cover
    new IPv6 defrag behavior:
    
    - min IPv6 frag size is now 8 instead of 1280
    
    - new test cases to cover IPv6 defragmentation in nf_conntrack_reasm.c
    
    - new "permissive" mode in negative (overlap) tests: netfilter
    sometimes drops invalid packets without passing them to IPv6
    underneath, and thus defragmentation sometimes succeeds when
    it is expected to fail; so the permissive mode does not fail the
    test if the correct reassembled datagram is received instead of a
    timeout.
    
    Signed-off-by: Peter Oskolkov <posk@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- tools/testing/selftests/net/ip_defrag.sh
+++ tools/testing/selftests/net/ip_defrag.sh
@@ -20,6 +20,10 @@ setup() {
 	ip netns exec "${NETNS}" sysctl -w net.ipv6.ip6frag_low_thresh=7000000 >/dev/null 2>&1
 	ip netns exec "${NETNS}" sysctl -w net.ipv6.ip6frag_time=1 >/dev/null 2>&1
 
+	ip netns exec "${NETNS}" sysctl -w net.netfilter.nf_conntrack_frag6_high_thresh=9000000 >/dev/null 2>&1
+	ip netns exec "${NETNS}" sysctl -w net.netfilter.nf_conntrack_frag6_low_thresh=7000000  >/dev/null 2>&1
+	ip netns exec "${NETNS}" sysctl -w net.netfilter.nf_conntrack_frag6_timeout=1 >/dev/null 2>&1
+
 	# DST cache can get full with a lot of frags, with GC not keeping up with the test.
 	ip netns exec "${NETNS}" sysctl -w net.ipv6.route.max_size=65536 >/dev/null 2>&1
 }
@@ -42,5 +46,17 @@ ip netns exec "${NETNS}" ./ip_defrag -6
 
 echo "ipv6 defrag with overlaps"
 ip netns exec "${NETNS}" ./ip_defrag -6o
+
+# insert an nf_conntrack rule so that the codepath in nf_conntrack_reasm.c taken
+ip netns exec "${NETNS}" ip6tables -A INPUT  -m conntrack --ctstate INVALID -j ACCEPT
+
+echo "ipv6 nf_conntrack defrag"
+ip netns exec "${NETNS}" ./ip_defrag -6
+
+echo "ipv6 nf_conntrack defrag with overlaps"
+# netfilter will drop some invalid packets, so we run the test in
+# permissive mode: i.e. pass the test if the packet is correctly assembled
+# even if we sent an overlap
+ip netns exec "${NETNS}" ./ip_defrag -6op
 
 echo "all tests done"
