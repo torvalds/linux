071d184a19f673bbb69a593c2ff4bd7acc0d1fe6
Doug Berger <opendmb@gmail.com>
2019-01-22 21:05:10 +0100

    ARM: 8826/1: mm: initialize pfn limits with find_limits()
    The max_low_pfn value must be set before sparse_init() is called to
    keep the early memblock allocations and frees balanced for kmemleak
    initialization when sparsemem is enabled.
    
    This commit accomplishes that by replacing the local variables min,
    max_low, and max_high with the global limit variables min_low_pfn,
    max_low_pfn, and max_pfn respectively in bootmem_init(). The global
    variables are initialized directly by find_limits() and used in the
    remainder of the function.
    
    Fixes: 9099daed9c69 ("mm: kmemleak: avoid using __va() on addresses that don't have a lowmem mapping")
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Mike Rapoport <rppt@linux.ibm.com>
    Signed-off-by: Doug Berger <opendmb@gmail.com>
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- arch/arm/mm/init.c
+++ arch/arm/mm/init.c
@@ -278,15 +278,12 @@ void __init arm_memblock_init(const struct machine_desc *mdesc)
 
 void __init bootmem_init(void)
 {
-	unsigned long min, max_low, max_high;
-
 	memblock_allow_resize();
-	max_low = max_high = 0;
 
-	find_limits(&min, &max_low, &max_high);
+	find_limits(&min_low_pfn, &max_low_pfn, &max_pfn);
 
-	early_memtest((phys_addr_t)min << PAGE_SHIFT,
-		      (phys_addr_t)max_low << PAGE_SHIFT);
+	early_memtest((phys_addr_t)min_low_pfn << PAGE_SHIFT,
+		      (phys_addr_t)max_low_pfn << PAGE_SHIFT);
 
 	/*
 	 * Sparsemem tries to allocate bootmem in memory_present(),
@@ -304,16 +301,7 @@ void __init bootmem_init(void)
 	 * the sparse mem_map arrays initialized by sparse_init()
 	 * for memmap_init_zone(), otherwise all PFNs are invalid.
 	 */
-	zone_sizes_init(min, max_low, max_high);
-
-	/*
-	 * This doesn't seem to be used by the Linux memory manager any
-	 * more, but is used by ll_rw_block.  If we can get rid of it, we
-	 * also get rid of some of the stuff above as well.
-	 */
-	min_low_pfn = min;
-	max_low_pfn = max_low;
-	max_pfn = max_high;
+	zone_sizes_init(min_low_pfn, max_low_pfn, max_pfn);
 }
 
 /*
