7654fff18fcc5f692f528c1840ee2b7737838cf7
Christian Brauner <christian@brauner.io>
2019-01-23 12:41:15 +0100

    UPSTREAM: binderfs: respect limit on binder control creation
    We currently adhere to the reserved devices limit when creating new
    binderfs devices in binderfs instances not located in the inital ipc
    namespace. But it is still possible to rob the host instances of their 4
    reserved devices by creating the maximum allowed number of devices in a
    single binderfs instance located in a non-initial ipc namespace and then
    mounting 4 separate binderfs instances in non-initial ipc namespaces. That
    happens because the limit is currently not respected for the creation of
    the initial binder-control device node. Block this nonsense by performing
    the same check in binderfs_binder_ctl_create() that we perform in
    binderfs_binder_device_create().
    
    Fixes: 36bdf3cae09d ("binderfs: reserve devices for initial mount")
    Signed-off-by: Christian Brauner <christian@brauner.io>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Bug: 136497735
    (cherry picked from commit da8ddba566ff0a883237dbc8c5dadef1ca769e19)
    Change-Id: I7e170260ce79fc23a034ce75450d58ff39a7b902
    Signed-off-by: Hridya Valsaraju <hridya@google.com>

--- drivers/android/binderfs.c
+++ drivers/android/binderfs.c
@@ -395,6 +395,11 @@ static int binderfs_binder_ctl_create(struct super_block *sb)
 	struct inode *inode = NULL;
 	struct dentry *root = sb->s_root;
 	struct binderfs_info *info = sb->s_fs_info;
+#if defined(CONFIG_IPC_NS)
+	bool use_reserve = (info->ipc_ns == &init_ipc_ns);
+#else
+	bool use_reserve = true;
+#endif
 
 	device = kzalloc(sizeof(*device), GFP_KERNEL);
 	if (!device)
@@ -413,7 +418,10 @@ static int binderfs_binder_ctl_create(struct super_block *sb)
 
 	/* Reserve a new minor number for the new device. */
 	mutex_lock(&binderfs_minors_mutex);
-	minor = ida_alloc_max(&binderfs_minors, BINDERFS_MAX_MINOR, GFP_KERNEL);
+	minor = ida_alloc_max(&binderfs_minors,
+			      use_reserve ? BINDERFS_MAX_MINOR :
+					    BINDERFS_MAX_MINOR_CAPPED,
+			      GFP_KERNEL);
 	mutex_unlock(&binderfs_minors_mutex);
 	if (minor < 0) {
 		ret = minor;
