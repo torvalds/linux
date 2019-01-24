edd8fb55d7f1d1cf39e1b2416e91b699bcd40ede
Tomas Winkler <tomas.winkler@intel.com>
2019-01-24 14:45:02 +0200

    mei: me: add ice lake point device id.
    commit efe814e90b98aed6d655b5a4092b9114b8b26e42 upstream.
    
    Add icelake mei device id.
    
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/mei/hw-me-regs.h
+++ drivers/misc/mei/hw-me-regs.h
@@ -139,6 +139,8 @@
 #define MEI_DEV_ID_CNP_H      0xA360  /* Cannon Point H */
 #define MEI_DEV_ID_CNP_H_4    0xA364  /* Cannon Point H 4 (iTouch) */
 
+#define MEI_DEV_ID_ICP_LP     0x34E0  /* Ice Lake Point LP */
+
 /*
  * MEI HW Section
  */
