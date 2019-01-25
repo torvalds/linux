a09e1c84143ecd9985b3f5712d25ca43f5eb57a0
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-01-25 14:35:03 -0600

    ASoC: Intel: cht_bsw_max98090_ti: platform name fixup support
    Add helper to override dailink platform name, if passed as parameter
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 7e7e24d7c7ff0e85956288915aaa7e682e2ccd55)

--- sound/soc/intel/boards/cht_bsw_max98090_ti.c
+++ sound/soc/intel/boards/cht_bsw_max98090_ti.c
@@ -28,6 +28,7 @@
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
+#include <sound/soc-acpi.h>
 #include <sound/jack.h>
 #include "../../codecs/max98090.h"
 #include "../atom/sst-atom-controls.h"
@@ -420,6 +421,8 @@ static int snd_cht_mc_probe(struct platform_device *pdev)
 	int ret_val = 0;
 	struct cht_mc_private *drv;
 	const char *mclk_name;
+	struct snd_soc_acpi_mach *mach;
+	const char *platform_name;
 	int quirks = 0;
 
 	dmi_id = dmi_first_match(cht_max98090_quirk_table);
@@ -441,6 +444,15 @@ static int snd_cht_mc_probe(struct platform_device *pdev)
 		if (ret_val)
 			dev_dbg(dev, "Unable to add GPIO mapping table\n");
 	}
+
+	/* override plaform name, if required */
+	mach = (&pdev->dev)->platform_data;
+	platform_name = mach->mach_params.platform;
+
+	ret_val = snd_soc_fixup_dai_links_platform_name(&snd_soc_card_cht,
+							platform_name);
+	if (ret_val)
+		return ret_val;
 
 	/* register the soc card */
 	snd_soc_card_cht.dev = &pdev->dev;
