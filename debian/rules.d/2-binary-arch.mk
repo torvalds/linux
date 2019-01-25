838f9892acce0d43e521cc8cc65375059d6196e6
Seth Forshee <seth.forshee@canonical.com>
2019-01-25 10:57:21 -0700

    UBUNTU: [Packaging] remove handoff check for uefi signing
    This check doesn't work for arm64 and is no longer necessary for
    x86, so remove it.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>
    Signed-off-by: dann frazier <dann.frazier@canonical.com>
    Acked-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- debian/rules.d/2-binary-arch.mk
+++ debian/rules.d/2-binary-arch.mk
@@ -134,13 +134,8 @@ endif
 
 ifeq ($(uefi_signed),true)
 	install -d $(signingv)
-	# Check to see if this supports handoff, if not do not sign it.
-	# Check the identification area magic and version >= 0x020b
-	handoff=`dd if="$(pkgdir_bin)/boot/$(instfile)-$(abi_release)-$*" bs=1 skip=514 count=6 2>/dev/null | od -s | gawk '($$1 == 0 && $$2 == 25672 && $$3 == 21362 && $$4 >= 523) { print "GOOD" }'`; \
-	if [ "$$handoff" = "GOOD" ]; then \
-		cp -p $(pkgdir_bin)/boot/$(instfile)-$(abi_release)-$* \
-			$(signingv)/$(instfile)-$(abi_release)-$*.efi; \
-	fi
+	cp -p $(pkgdir_bin)/boot/$(instfile)-$(abi_release)-$* \
+		$(signingv)/$(instfile)-$(abi_release)-$*.efi;
 endif
 ifeq ($(opal_signed),true)
 	install -d $(signingv)
