b972d2eaf0c7021579755eec6b2b79e0f5bc7930
James Morse <james.morse@arm.com>
2019-01-29 18:48:57 +0000

    ACPI / APEI: Use separate fixmap pages for arm64 NMI-like notifications
    Now that ghes notification helpers provide the fixmap slots and
    take the lock themselves, multiple NMI-like notifications can
    be used on arm64.
    
    These should be named after their notification method as they can't
    all be called 'NMI'. x86's NOTIFY_NMI already is, change the SEA
    fixmap entry to be called FIX_APEI_GHES_SEA.
    
    Future patches can add support for FIX_APEI_GHES_SEI and
    FIX_APEI_GHES_SDEI_{NORMAL,CRITICAL}.
    
    Because all of ghes.c builds on both architectures, provide a
    constant for each fixmap entry that the architecture will never
    use.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- arch/arm64/include/asm/fixmap.h
+++ arch/arm64/include/asm/fixmap.h
@@ -55,7 +55,7 @@ enum fixed_addresses {
 #ifdef CONFIG_ACPI_APEI_GHES
 	/* Used for GHES mapping from assorted contexts */
 	FIX_APEI_GHES_IRQ,
-	FIX_APEI_GHES_NMI,
+	FIX_APEI_GHES_SEA,
 #endif /* CONFIG_ACPI_APEI_GHES */
 
 #ifdef CONFIG_UNMAP_KERNEL_AT_EL0
