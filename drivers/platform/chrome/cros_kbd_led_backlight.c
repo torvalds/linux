0e27a11f3201a9835d5bb584c9842d8d47326073
Enric Balletbo i Serra <enric.balletbo@collabora.com>
2019-01-29 10:38:21 +0100

    platform/chrome: cros_kbd_led_backlight: switch to SPDX identifier
    Adopt the SPDX license identifier headers to ease license compliance
    management. Also change the term 'Chrome OS' for 'ChromeOS' to be
    coherent with other drivers.
    
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

--- drivers/platform/chrome/cros_kbd_led_backlight.c
+++ drivers/platform/chrome/cros_kbd_led_backlight.c
@@ -1,18 +1,7 @@
-/*
- *  Keyboard backlight LED driver for Chrome OS.
- *
- *  Copyright (C) 2012 Google, Inc.
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- */
+// SPDX-License-Identifier: GPL-2.0+
+// Keyboard backlight LED driver for ChromeOS
+//
+// Copyright (C) 2012 Google, Inc.
 
 #include <linux/acpi.h>
 #include <linux/leds.h>
