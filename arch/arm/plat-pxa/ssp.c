5d918cd203d4b4ef2e87c22e181a9a0341ca7f7e
YueHaibing <yuehaibing@huawei.com>
2019-01-29 16:03:24 +0800

    ARM: pxa: ssp: Fix "WARNING: invalid free of devm_ allocated data"
    [ Upstream commit 9ee8578d953023cc57e7e736ae48502c707c0210 ]
    
    Since commit 1c459de1e645 ("ARM: pxa: ssp: use devm_ functions")
    kfree, iounmap, clk_put etc are not needed anymore in remove path.
    
    Fixes: 1c459de1e645 ("ARM: pxa: ssp: use devm_ functions")
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    [ commit message spelling fix ]
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/arm/plat-pxa/ssp.c
+++ arch/arm/plat-pxa/ssp.c
@@ -183,17 +183,11 @@ static int pxa_ssp_probe(struct platform_device *pdev)
 
 static int pxa_ssp_remove(struct platform_device *pdev)
 {
-	struct resource *res;
 	struct ssp_device *ssp;
 
 	ssp = platform_get_drvdata(pdev);
 	if (ssp == NULL)
 		return -ENODEV;
-
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	release_mem_region(res->start, resource_size(res));
-
-	clk_put(ssp->clk);
 
 	mutex_lock(&ssp_lock);
 	list_del(&ssp->node);
