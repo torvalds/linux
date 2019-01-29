b3c3498112ffafa5f613bb482f9723996bfd5e4f
Paolo Valente <paolo.valente@linaro.org>
2019-01-29 12:06:36 +0100

    block, bfq: port commit "cfq-iosched: improve hw_tag detection"
    The original commit is commit 1a1238a7dd48 ("cfq-iosched: improve hw_tag
    detection") and has the following commit message:
    
    If active queue hasn't enough requests and idle window opens, cfq will
    not dispatch sufficient requests to hardware. In such situation, current
    code will zero hw_tag. But this is because cfq doesn't dispatch enough
    requests instead of hardware queue doesn't work. Don't zero hw_tag in
    such case.
    
    Signed-off-by: Paolo Valente <paolo.valente@linaro.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -4786,6 +4786,8 @@ static void bfq_insert_requests(struct blk_mq_hw_ctx *hctx,
 
 static void bfq_update_hw_tag(struct bfq_data *bfqd)
 {
+	struct bfq_queue *bfqq = bfqd->in_service_queue;
+
 	bfqd->max_rq_in_driver = max_t(int, bfqd->max_rq_in_driver,
 				       bfqd->rq_in_driver);
 
@@ -4799,6 +4801,17 @@ static void bfq_update_hw_tag(struct bfq_data *bfqd)
 	 * requests.
 	 */
 	if (bfqd->rq_in_driver + bfqd->queued <= BFQ_HW_QUEUE_THRESHOLD)
+		return;
+
+	/*
+	 * If active queue hasn't enough requests and can idle, bfq might not
+	 * dispatch sufficient requests to hardware. Don't zero hw_tag in this
+	 * case
+	 */
+	if (bfqq && bfq_bfqq_has_short_ttime(bfqq) &&
+	    bfqq->dispatched + bfqq->queued[0] + bfqq->queued[1] <
+	    BFQ_HW_QUEUE_THRESHOLD &&
+	    bfqd->rq_in_driver < BFQ_HW_QUEUE_THRESHOLD)
 		return;
 
 	if (bfqd->hw_tag_samples++ < BFQ_HW_QUEUE_SAMPLES)
