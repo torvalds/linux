07b344f494ddda9f061b396407c96df8c46c82b5
Toshiaki Makita <makita.toshiaki@lab.ntt.co.jp>
2019-01-29 09:45:58 +0900

    virtio_net: Use xdp_return_frame to free xdp_frames on destroying vqs
    put_page() can work as a fallback for freeing xdp_frames, but the
    appropriate way is to use xdp_return_frame().
    
    Fixes: cac320c850ef ("virtio_net: convert to use generic xdp_frame and xdp_return_frame API")
    Signed-off-by: Toshiaki Makita <makita.toshiaki@lab.ntt.co.jp>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Acked-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -2669,7 +2669,7 @@ static void free_unused_bufs(struct virtnet_info *vi)
 			if (!is_xdp_raw_buffer_queue(vi, i))
 				dev_kfree_skb(buf);
 			else
-				put_page(virt_to_head_page(buf));
+				xdp_return_frame(buf);
 		}
 	}
 
