e79718d7a314efe61ff5468ccbdd7eb34c13b0d3
Marcelo Henrique Cerri <marcelo.cerri@canonical.com>
2019-01-30 14:13:18 -0200

    UBUNTU: SAUCE: selftests: net: fix "from" match test in fib_rule_tests.sh
    Fix the IPv4 address of the dummy0 interface and ensure that ip_forward
    is enabled in the network space to get a valid response when checking
    for routes between the gateway and other hosts.
    
    Signed-off-by: Marcelo Henrique Cerri <marcelo.cerri@canonical.com>

--- tools/testing/selftests/net/fib_rule_tests.sh
+++ tools/testing/selftests/net/fib_rule_tests.sh
@@ -54,8 +54,10 @@ setup()
 
 	$IP link add dummy0 type dummy
 	$IP link set dev dummy0 up
-	$IP address add 198.51.100.1/24 dev dummy0
+	$IP address add 192.51.100.1/24 dev dummy0
 	$IP -6 address add 2001:db8:1::1/64 dev dummy0
+
+	ip netns exec testns sysctl -w net.ipv4.ip_forward=1
 
 	set +e
 }
