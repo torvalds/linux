7b2d0fdaa846da7b73f8f0ebbee0fe8eda48a284
Shuah Khan <shuah@kernel.org>
2019-01-31 11:54:16 -0700

    selftests: ir: fix warning: "%s" directive output may be truncated ’ directive output may be truncated
    BugLink: https://bugs.launchpad.net/bugs/1828410
    
    [ Upstream commit ed675ed9da6d951322efd72d739d6b5ce1c18f02 ]
    
    Fix the following warning by sizing the buffer to max. of sysfs
    path max. size + d_name max. size.
    
    gcc -Wall -O2 -I../../../include/uapi ir_loopback.c  -o ../tools/testing/selftests/ir/ir_loopback
    ir_loopback.c: In function ‘lirc_open’:
    ir_loopback.c:71:37: warning: ‘%s’ directive output may be truncated writing up to 255 bytes into a region of size 95 [-Wformat-truncation=]
        snprintf(buf, sizeof(buf), "/dev/%s", dent->d_name);
                                         ^~
    In file included from /usr/include/stdio.h:862:0,
                     from ir_loopback.c:14:
    /usr/include/x86_64-linux-gnu/bits/stdio2.h:64:10: note: ‘__builtin___snprintf_chk’ output between 6 and 261 bytes into a destination of size 100
       return __builtin___snprintf_chk (__s, __n, __USE_FORTIFY_LEVEL - 1,
              ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
            __bos (__s), __fmt, __va_arg_pack ());
            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Shuah Khan <shuah@kernel.org>
    Acked-by: Sean Young <sean@mess.org>
    Signed-off-by: Shuah Khan <shuah@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- tools/testing/selftests/ir/ir_loopback.c
+++ tools/testing/selftests/ir/ir_loopback.c
@@ -27,6 +27,8 @@
 
 #define TEST_SCANCODES	10
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+#define SYSFS_PATH_MAX 256
+#define DNAME_PATH_MAX 256
 
 static const struct {
 	enum rc_proto proto;
@@ -56,7 +58,7 @@ static const struct {
 int lirc_open(const char *rc)
 {
 	struct dirent *dent;
-	char buf[100];
+	char buf[SYSFS_PATH_MAX + DNAME_PATH_MAX];
 	DIR *d;
 	int fd;
 
