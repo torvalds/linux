e3df4c6e4836ce93cd5cf92d9cbdeaf4439a0241
Michal Hocko <mhocko@suse.com>
2019-02-01 14:21:12 -0800

    mm, memory_hotplug: __offline_pages fix wrong locking
    Jan has noticed that we do double unlock on some failure paths when
    offlining a page range.  This is indeed the case when
    test_pages_in_a_zone respp.  start_isolate_page_range fail.  This was an
    omission when forward porting the debugging patch from an older kernel.
    
    Fix the issue by dropping mem_hotplug_done from the failure condition
    and keeping the single unlock in the catch all failure path.
    
    Link: http://lkml.kernel.org/r/20190115120307.22768-1-mhocko@kernel.org
    Fixes: 7960509329c2 ("mm, memory_hotplug: print reason for the offlining failure")
    Signed-off-by: Michal Hocko <mhocko@suse.com>
    Reported-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Tested-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Oscar Salvador <osalvador@suse.de>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/memory_hotplug.c
+++ mm/memory_hotplug.c
@@ -1570,7 +1570,6 @@ static int __ref __offline_pages(unsigned long start_pfn,
 	   we assume this for now. .*/
 	if (!test_pages_in_a_zone(start_pfn, end_pfn, &valid_start,
 				  &valid_end)) {
-		mem_hotplug_done();
 		ret = -EINVAL;
 		reason = "multizone range";
 		goto failed_removal;
@@ -1585,7 +1584,6 @@ static int __ref __offline_pages(unsigned long start_pfn,
 				       MIGRATE_MOVABLE,
 				       SKIP_HWPOISON | REPORT_FAILURE);
 	if (ret) {
-		mem_hotplug_done();
 		reason = "failure to isolate range";
 		goto failed_removal;
 	}
