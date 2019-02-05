ffd2e26e35a549e5ee082e8979002f5bf53fac3b
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-02-05 15:23:29 -0600

    ASoC: Intel: icl-match.c.c: remove prefix for SOF files
    Prefix is now handled in the code. This allows for default and
    alternate paths, and more flexibility for OEMs and distros
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/intel/common/soc-acpi-intel-icl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-icl-match.c
@@ -27,8 +27,8 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_icl_machines[] = {
 	{
 		.id = "10EC5682",
 		.drv_name = "sof_rt5682",
-		.sof_fw_filename = "intel/sof-icl.ri",
-		.sof_tplg_filename = "intel/sof-icl-rt5682.tplg",
+		.sof_fw_filename = "sof-icl.ri",
+		.sof_tplg_filename = "sof-icl-rt5682.tplg",
 		.asoc_plat_name = "0000:00:1f.3",
 	},
 	{},
