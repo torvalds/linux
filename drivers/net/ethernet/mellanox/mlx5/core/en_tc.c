68931c7dd71efd9be19f0b5777f2c6e78a9f1185
Roi Dayan <roid@mellanox.com>
2019-02-05 09:41:43 +0200

    net/mlx5e: Update check for merged eswitch device
    The current check only validates if both netdevs use the same ops
    which means both are vf reps or both uplink reps.
    
    Unlike the case where the two uplinks are bonded (VF LAG), under
    multipath scheme the switchdev parent id is not unified between the
    uplink reps (and all the associated vf reps). However, we still want
    to duplicate in the driver encap flows, adjust the merged eswitch
    check for that matter.
    
    Signed-off-by: Roi Dayan <roid@mellanox.com>
    Reviewed-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -2360,10 +2360,9 @@ static bool is_merged_eswitch_dev(struct mlx5e_priv *priv,
 	peer_priv = netdev_priv(peer_netdev);
 
 	return (MLX5_CAP_ESW(priv->mdev, merged_eswitch) &&
-		(priv->netdev->netdev_ops == peer_netdev->netdev_ops) &&
-		same_hw_devs(priv, peer_priv) &&
-		MLX5_VPORT_MANAGER(peer_priv->mdev) &&
-		(peer_priv->mdev->priv.eswitch->mode == SRIOV_OFFLOADS));
+		mlx5e_eswitch_rep(priv->netdev) &&
+		mlx5e_eswitch_rep(peer_netdev) &&
+		same_hw_devs(priv, peer_priv));
 }
 
 
