d9111d36024de07784f2e1ba2ccf70b16035f378
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2019-02-05 09:46:43 +0900

    ASoC: rsnd: fixup rsnd_ssi_master_clk_start() user count check
    commit 4d230d1271064 ("ASoC: rsnd: fixup not to call clk_get/set
    under non-atomic") added new rsnd_ssi_prepare() and moved
    rsnd_ssi_master_clk_start() to .prepare.
    But, ssi user count (= ssi->usrcnt) is incremented at .init
    (= rsnd_ssi_init()).
    Because of these timing exchange, ssi->usrcnt check at
    rsnd_ssi_master_clk_start() should be adjusted.
    Otherwise, 2nd master clock setup will be no check.
    This patch fixup this issue.
    
    Fixes: commit 4d230d1271064 ("ASoC: rsnd: fixup not to call clk_get/set under non-atomic")
    Reported-by: Yusuke Goda <yusuke.goda.sx@renesas.com>
    Reported-by: Valentine Barshak <valentine.barshak@cogentembedded.com>
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Tested-by: Yusuke Goda <yusuke.goda.sx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sh/rcar/ssi.c
+++ sound/soc/sh/rcar/ssi.c
@@ -286,7 +286,7 @@ static int rsnd_ssi_master_clk_start(struct rsnd_mod *mod,
 	if (rsnd_ssi_is_multi_slave(mod, io))
 		return 0;
 
-	if (ssi->usrcnt > 1) {
+	if (ssi->usrcnt > 0) {
 		if (ssi->rate != rate) {
 			dev_err(dev, "SSI parent/child should use same rate\n");
 			return -EINVAL;
