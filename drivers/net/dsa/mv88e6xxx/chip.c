bcd3d9d9246f35c62c5d652d20569a76dbdf4bd1
Miquel Raynal <miquel.raynal@bootlin.com>
2019-02-05 12:07:28 +0100

    net: dsa: mv88e6xxx: Prevent suspend to RAM
    On one hand, the mv88e6xxx driver has a work queue called in loop
    which will attempt register accesses after MDIO bus suspension, that
    entirely freezes the platform during suspend.
    
    On the other hand, the DSA core is not ready yet to support suspend to
    RAM operation because so far there is no way to recover reliably the
    switch configuration.
    
    To avoid the kernel to freeze when suspending with a switch driven by
    the mv88e6xxx driver, we choose to prevent the driver suspension and
    in the same way, the whole platform.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Reviewed-by: Vivien Didelot <vivien.didelot@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/mv88e6xxx/chip.c
+++ drivers/net/dsa/mv88e6xxx/chip.c
@@ -4764,6 +4764,21 @@ static const void *pdata_device_get_match_data(struct device *dev)
 	return NULL;
 }
 
+/* There is no suspend to RAM support at DSA level yet, the switch configuration
+ * would be lost after a power cycle so prevent it to be suspended.
+ */
+static int __maybe_unused mv88e6xxx_suspend(struct device *dev)
+{
+	return -EOPNOTSUPP;
+}
+
+static int __maybe_unused mv88e6xxx_resume(struct device *dev)
+{
+	return 0;
+}
+
+static SIMPLE_DEV_PM_OPS(mv88e6xxx_pm_ops, mv88e6xxx_suspend, mv88e6xxx_resume);
+
 static int mv88e6xxx_probe(struct mdio_device *mdiodev)
 {
 	struct dsa_mv88e6xxx_pdata *pdata = mdiodev->dev.platform_data;
@@ -4948,6 +4963,7 @@ static struct mdio_driver mv88e6xxx_driver = {
 	.mdiodrv.driver = {
 		.name = "mv88e6085",
 		.of_match_table = mv88e6xxx_of_match,
+		.pm = &mv88e6xxx_pm_ops,
 	},
 };
 
