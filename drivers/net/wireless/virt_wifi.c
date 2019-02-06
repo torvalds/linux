f440b125450dfc65ce0386eb231da2f663c45ca0
Cody Schuffelen <schuffelen@google.com>
2019-02-06 15:54:15 -0800

    virt_wifi: Remove REGULATORY_WIPHY_SELF_MANAGED
    REGULATORY_WIPHY_SELF_MANAGED as set here breaks NL80211_CMD_GET_REG,
    because it expects the wiphy to do regulatory management. Since
    virt_wifi does not do regulatory management, this triggers a WARN_ON in
    NL80211_CMD_GET_REG and fails the netlink command.
    
    Removing REGULATORY_WIPHY_SELF_MANAGED fixes the problem and the virtual
    wireless network continues to work.
    
    Signed-off-by: Cody Schuffelen <schuffelen@google.com>
    Acked-by: Alistair Strachan <astrachan@google.com>
    Acked-by: Greg Hartman <ghartman@google.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/virt_wifi.c
+++ drivers/net/wireless/virt_wifi.c
@@ -360,7 +360,6 @@ static struct wiphy *virt_wifi_make_wiphy(void)
 	wiphy->bands[NL80211_BAND_5GHZ] = &band_5ghz;
 	wiphy->bands[NL80211_BAND_60GHZ] = NULL;
 
-	wiphy->regulatory_flags = REGULATORY_WIPHY_SELF_MANAGED;
 	wiphy->interface_modes = BIT(NL80211_IFTYPE_STATION);
 
 	priv = wiphy_priv(wiphy);
