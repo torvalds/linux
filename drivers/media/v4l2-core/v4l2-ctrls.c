398d768025c171245918828b330697f9442d0fc1
Hans Verkuil <hverkuil-cisco@xs4all.nl>
2019-02-07 06:49:44 -0500

    media: v4l2-ctrls.c: fix epoll() by calling poll_wait first
    The epoll function expects that whenever the poll file op is
    called, the poll_wait function is also called. That didn't
    always happen in v4l2_ctrl_poll(). Fix this, otherwise epoll()
    would timeout when it shouldn't.
    
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/v4l2-core/v4l2-ctrls.c
+++ drivers/media/v4l2-core/v4l2-ctrls.c
@@ -4166,9 +4166,9 @@ __poll_t v4l2_ctrl_poll(struct file *file, struct poll_table_struct *wait)
 {
 	struct v4l2_fh *fh = file->private_data;
 
+	poll_wait(file, &fh->wait, wait);
 	if (v4l2_event_pending(fh))
 		return EPOLLPRI;
-	poll_wait(file, &fh->wait, wait);
 	return 0;
 }
 EXPORT_SYMBOL(v4l2_ctrl_poll);
