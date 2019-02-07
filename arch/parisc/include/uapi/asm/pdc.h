3b26fdafbe2f42627e11ca5c67af565e5c67995f
Sven Schnelle <svens@stackframe.org>
2019-02-07 22:31:57 +0100

    parisc: Add PDC_CRASH_PREP PDC function number
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    Signed-off-by: Helge Deller <deller@gmx.de>

--- arch/parisc/include/uapi/asm/pdc.h
+++ arch/parisc/include/uapi/asm/pdc.h
@@ -167,6 +167,7 @@
 #define PDC_SOFT_POWER_INFO	0	/* return info about the soft power switch */
 #define PDC_SOFT_POWER_ENABLE	1	/* enable/disable soft power switch */
 
+#define PDC_CRASH_PREP 25		/* Prepare system for crash dump */
 
 /* HVERSION dependent */
 
