866e11d8fa9e16c4f5f728dc61d3b9a9582c6225
Connor O'Brien <connoro@google.com>
2019-02-08 12:30:41 -0800

    ANDROID: cpufreq: times: record fast switch frequency transitions
    cpufreq_times_record_transition() is not called when fast switch is
    enabled, leading /proc/uid_time_in_state to attribute all time on a
    cluster to a single frequency. To fix this, add a call to
    cpufreq_times_record_transition() in the fast switch path.
    
    Also revise cpufreq_times_record_transition() to simplify the new call
    and more closely align with cpufreq_stats_record_transition().
    
    Bug: 121287027
    Bug: 127641090
    Test: /proc/uid_time_in_state shows times for more than one freq per
    cluster
    Change-Id: Ib63d19006878fafb88475e401ef243bdd8b11979
    Signed-off-by: Connor O'Brien <connoro@google.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -356,7 +356,7 @@ static void cpufreq_notify_transition(struct cpufreq_policy *policy,
 		}
 
 		cpufreq_stats_record_transition(policy, freqs->new);
-		cpufreq_times_record_transition(freqs);
+		cpufreq_times_record_transition(policy, freqs->new);
 		policy->cur = freqs->new;
 	}
 }
@@ -1866,9 +1866,15 @@ EXPORT_SYMBOL(cpufreq_unregister_notifier);
 unsigned int cpufreq_driver_fast_switch(struct cpufreq_policy *policy,
 					unsigned int target_freq)
 {
+	int ret;
+
 	target_freq = clamp_val(target_freq, policy->min, policy->max);
 
-	return cpufreq_driver->fast_switch(policy, target_freq);
+	ret = cpufreq_driver->fast_switch(policy, target_freq);
+	if (ret)
+		cpufreq_times_record_transition(policy, ret);
+
+	return ret;
 }
 EXPORT_SYMBOL_GPL(cpufreq_driver_fast_switch);
 
