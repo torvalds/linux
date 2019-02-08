0102868e655ada9bdf929fb8eb467e4bf2264949
Michal Zelencik <michalzelencik@gmail.com>
2019-02-08 09:15:32 +0100

    Implement socket_fetch and socket_update
    We decided to use exported function user_get_super() to obtain
    superblock pointer.
    
    Signed-off-by: Michal Zelencik <michalzelencik@gmail.com>

--- security/medusa/l2/kobject_socket.c
+++ security/medusa/l2/kobject_socket.c
@@ -21,13 +21,47 @@ int socket_kern2kobj(struct socket_kobject * sock_kobj, struct socket * sock)
 {
 }
 
+static struct socket_kobject storage;
+
 struct medusa_kobject_s *socket_fetch(struct medusa_kobject_s *kobj)
 {
+	struct socket * sock;
+	struct inode *inode;
+	struct super_block *sb;
+	struct socket_kobject *s_kobj = (struct socket_kobject*) kobj;
+
+	if(s_kobj)
+		sb = user_get_super(s_kobj->dev);
+	if(sb)
+		inode = ilookup(sb, s_kobj->ino);
+	if(inode)
+		sock = SOCKET_I(inode);
+	if(sock) {
+		socket_kern2kobj(&storage, sock);
+		return (struct medusa_kobject_s *)&storage;
+	}
+
 	return NULL;
 }
 
 medusa_answer_t socket_update(struct medusa_kobject_s *kobj)
 {
+	struct socket * sock;
+	struct inode *inode;
+	struct super_block *sb;
+	struct socket_kobject *s_kobj = (struct socket_kobject*) kobj;
+
+	if(s_kobj)
+		sb = user_get_super(s_kobj->dev);
+	if(sb)
+		inode = ilookup(sb, s_kobj->ino);
+	if(inode)
+		sock = SOCKET_I(inode);
+	if(sock) {
+		socket_kobj2kern(s_kobj, sock);
+		return MED_YES;
+	}
+
 	return MED_ERR;
 }
 
