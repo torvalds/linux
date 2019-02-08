47e3268ffeabe5b0029fc18c8db2d55ed9afef88
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-02-08 17:45:56 -0600

    ASoC: soc-acpi: remove asoc_plat_name field
    This field was never used, let's remove it
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit ecefff3e5b9b6a427a1a78c0c3f5eb147fd2d761)

--- include/sound/soc-acpi.h
+++ include/sound/soc-acpi.h
@@ -86,8 +86,6 @@ struct snd_soc_acpi_mach_params {
  *  is not constant since this field may be updated at run-time
  * @sof_fw_filename: Sound Open Firmware file name, if enabled
  * @sof_tplg_filename: Sound Open Firmware topology file name, if enabled
- * @asoc_plat_name: ASoC platform name, used for binding machine drivers
- * if non NULL
  * @new_mach_data: machine driver private data fixup
  */
 /* Descriptor for SST ASoC machine driver */
@@ -102,7 +100,6 @@ struct snd_soc_acpi_mach {
 	struct snd_soc_acpi_mach_params mach_params;
 	const char *sof_fw_filename;
 	const char *sof_tplg_filename;
-	const char *asoc_plat_name;
 	struct platform_device * (*new_mach_data)(void *pdata);
 };
 
