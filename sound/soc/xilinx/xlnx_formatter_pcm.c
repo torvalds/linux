95d14640d9843f277214893541aef3acb7456a25
Stephen Rothwell <sfr@canb.auug.org.au>
2019-02-08 13:18:23 +1100

    ASoC: xlnx: fix up for snd_pcm_lib_preallocate_pages_for_all() API change
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/soc/xilinx/xlnx_formatter_pcm.c
+++ sound/soc/xilinx/xlnx_formatter_pcm.c
@@ -536,10 +536,11 @@ static int xlnx_formatter_pcm_new(struct snd_soc_pcm_runtime *rtd)
 {
 	struct snd_soc_component *component = snd_soc_rtdcom_lookup(rtd,
 								    DRV_NAME);
-	return snd_pcm_lib_preallocate_pages_for_all(rtd->pcm,
+	snd_pcm_lib_preallocate_pages_for_all(rtd->pcm,
 			SNDRV_DMA_TYPE_DEV, component->dev,
 			xlnx_pcm_hardware.buffer_bytes_max,
 			xlnx_pcm_hardware.buffer_bytes_max);
+	return 0;
 }
 
 static const struct snd_pcm_ops xlnx_formatter_pcm_ops = {
