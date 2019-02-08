746c9398f5ac2b3f5730da4ed09e99ef4bb50b4a
Michael S. Tsirkin <mst@redhat.com>
2019-02-08 01:02:55 -0500

    arch: move common mmap flags to linux/mman.h
    Now that we have 3 mmap flags shared by all architectures,
    let's move them into the common header.
    
    This will help discourage future architectures from duplicating code.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/parisc/include/uapi/asm/mman.h
+++ arch/parisc/include/uapi/asm/mman.h
@@ -10,9 +10,7 @@
 #define PROT_GROWSDOWN	0x01000000	/* mprotect flag: extend change to start of growsdown vma */
 #define PROT_GROWSUP	0x02000000	/* mprotect flag: extend change to end of growsup vma */
 
-#define MAP_SHARED	0x01		/* Share changes */
-#define MAP_PRIVATE	0x02		/* Changes are private */
-#define MAP_SHARED_VALIDATE 0x03	/* share + validate extension flags */
+/* 0x01 - 0x03 are defined in linux/mman.h */
 #define MAP_TYPE	0x2b		/* Mask for type of mapping, includes bits 0x08 and 0x20 */
 #define MAP_FIXED	0x04		/* Interpret addr exactly */
 #define MAP_ANONYMOUS	0x10		/* don't use a file */
