1dbd6b406d44ad8b89af06db33055c27fa0c3851
Ashay Jaiswal <ashayj@codeaurora.org>
2019-02-10 21:13:27 +0530

    ANDROID: GKI: power_supply: Add "THERM_ICL_LIMIT" property
    Add "THERM_ICL_LIMIT" property to allow userspace
    thermal throttling algorithm to control input current
    limit of charger.
    
    Signed-off-by: Ashay Jaiswal <ashayj@codeaurora.org>
    Signed-off-by: Guru Das Srinagesh <gurus@codeaurora.org>
    (cherry picked from commit d859ce8eb9a40f7b371a657428245d833a756610)
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 150789066
    Change-Id: Ib527712b3dc5cde3555813c26f1a4b3a10b23cc1

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -454,6 +454,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(scale_mode_en),
 	POWER_SUPPLY_ATTR(voltage_vph),
 	POWER_SUPPLY_ATTR(chip_version),
+	POWER_SUPPLY_ATTR(therm_icl_limit),
 	/* Charge pump properties */
 	POWER_SUPPLY_ATTR(cp_status1),
 	POWER_SUPPLY_ATTR(cp_status2),
