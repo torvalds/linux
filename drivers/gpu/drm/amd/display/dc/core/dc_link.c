9cc032b239399a43290bb203b399d0ab3ec81e27
Martin Tsai <martin.tsai@amd.com>
2019-02-11 15:26:21 +0800

    drm/amd/display: Poll pending DOWN_REP before enabling the link
    [Why]
    With special monitor combination on MST, the UP_REQ could come
    after clear payload table. It makes the pending DOWN_REP fail
    to be handled after link training and the new DOWN_REQ will be queued
    until time out .
    
    [How]
    To move the current polling pending DOWN_REP procedure to after
    clear payload table to make sure the pending DOWN_REP can be
    done before enabling the link.
    
    Signed-off-by: Martin Tsai <martin.tsai@amd.com>
    Reviewed-by: Tony Cheng <Tony.Cheng@amd.com>
    Acked-by: Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_link.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_link.c
@@ -1466,13 +1466,13 @@ static enum dc_status enable_link_dp_mst(
 	if (link->cur_link_settings.lane_count != LANE_COUNT_UNKNOWN)
 		return DC_OK;
 
-	/* to make sure the pending down rep can be processed
-	 * before clear payload table
-	 */
-	dm_helpers_dp_mst_poll_pending_down_reply(link->ctx, link);
-
 	/* clear payload table */
 	dm_helpers_dp_mst_clear_payload_allocation_table(link->ctx, link);
+
+	/* to make sure the pending down rep can be processed
+	 * before enabling the link
+	 */
+	dm_helpers_dp_mst_poll_pending_down_reply(link->ctx, link);
 
 	/* set the sink to MST mode before enabling the link */
 	dp_enable_mst_on_sink(link, true);
