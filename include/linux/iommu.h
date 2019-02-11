db04d4a3d72f0c5ee34609559f535d11ab47303c
Tom Murphy <murphyt7@tcd.ie>
2019-02-11 15:50:33 +0000

    iommu: Fix flush_tlb_all typo
    Fix typo, flush_tlb_all should be flush_iotlb_all.
    
    Signed-off-by: Tom Murphy <murphyt7@tcd.ie>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -167,7 +167,7 @@ struct iommu_resv_region {
  * @detach_dev: detach device from an iommu domain
  * @map: map a physically contiguous memory region to an iommu domain
  * @unmap: unmap a physically contiguous memory region from an iommu domain
- * @flush_tlb_all: Synchronously flush all hardware TLBs for this domain
+ * @flush_iotlb_all: Synchronously flush all hardware TLBs for this domain
  * @iotlb_range_add: Add a given iova range to the flush queue for this domain
  * @iotlb_sync: Flush all queued ranges from the hardware TLBs and empty flush
  *            queue
