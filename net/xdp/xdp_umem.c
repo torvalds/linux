e451eb51068496054d071af10b3530af4002a4f4
Davidlohr Bueso <dave@stgolabs.net>
2019-02-11 08:15:29 -0800

    xsk: share the mmap_sem for page pinning
    Holding mmap_sem exclusively for a gup() is an overkill. Lets
    share the lock and replace the gup call for gup_longterm(), as
    it is better suited for the lifetime of the pinning.
    
    Fixes: c0c77d8fb787 ("xsk: add user memory registration support sockopt")
    Signed-off-by: Davidlohr Bueso <dbueso@suse.de>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Bjorn Topel <bjorn.topel@intel.com>
    Cc: Magnus Karlsson <magnus.karlsson@intel.com>
    CC: netdev@vger.kernel.org
    Acked-by: Björn Töpel <bjorn.topel@intel.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- net/xdp/xdp_umem.c
+++ net/xdp/xdp_umem.c
@@ -259,10 +259,10 @@ static int xdp_umem_pin_pages(struct xdp_umem *umem)
 	if (!umem->pgs)
 		return -ENOMEM;
 
-	down_write(&current->mm->mmap_sem);
-	npgs = get_user_pages(umem->address, umem->npgs,
-			      gup_flags, &umem->pgs[0], NULL);
-	up_write(&current->mm->mmap_sem);
+	down_read(&current->mm->mmap_sem);
+	npgs = get_user_pages_longterm(umem->address, umem->npgs,
+				       gup_flags, &umem->pgs[0], NULL);
+	up_read(&current->mm->mmap_sem);
 
 	if (npgs != umem->npgs) {
 		if (npgs >= 0) {
