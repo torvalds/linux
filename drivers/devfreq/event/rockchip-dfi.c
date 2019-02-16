726409698feeac20de12fd107159573539b3e063
Yangtao Li <tiny.windzz@gmail.com>
2019-02-16 10:18:25 -0500

    PM / devfreq: rockchip-dfi: remove unneeded semicolon
    The semicolon is unneeded, so remove it.
    
    Signed-off-by: Yangtao Li <tiny.windzz@gmail.com>
    Reviewed-by: Chanwoo Choi <cw00.choi@samsung.com>
    Signed-off-by: MyungJoo Ham <myungjoo.ham@samsung.com>

--- drivers/devfreq/event/rockchip-dfi.c
+++ drivers/devfreq/event/rockchip-dfi.c
@@ -211,7 +211,7 @@ static int rockchip_dfi_probe(struct platform_device *pdev)
 	if (IS_ERR(data->clk)) {
 		dev_err(dev, "Cannot get the clk dmc_clk\n");
 		return PTR_ERR(data->clk);
-	};
+	}
 
 	/* try to find the optional reference to the pmu syscon */
 	node = of_parse_phandle(np, "rockchip,pmu", 0);
