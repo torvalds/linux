d63916aeba5768a9aedcd3bfddf32ffff4dae076
Shahar S Matityahu <shahar.s.matityahu@intel.com>
2019-02-17 14:46:22 +0200

    iwlwifi: dbg_ini: fix the dram monitor header size
    Add sizeof(struct iwl_fw_ini_error_dump_range) to the header of the dram
    monitor.
    
    Signed-off-by: Shahar S Matityahu <shahar.s.matityahu@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/fw/dbg.c
+++ drivers/net/wireless/intel/iwlwifi/fw/dbg.c
@@ -1528,7 +1528,8 @@ static u32 iwl_dump_ini_paging_get_size(struct iwl_fw_runtime *fwrt,
 static u32 iwl_dump_ini_mon_dram_get_size(struct iwl_fw_runtime *fwrt,
 					  struct iwl_fw_ini_region_cfg *reg)
 {
-	u32 size = sizeof(struct iwl_fw_ini_monitor_dram_dump);
+	u32 size = sizeof(struct iwl_fw_ini_monitor_dram_dump) +
+		sizeof(struct iwl_fw_ini_error_dump_range);
 
 	if (fwrt->trans->num_blocks)
 		size += fwrt->trans->fw_mon[0].size;
