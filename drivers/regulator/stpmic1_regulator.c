48593a99a1f6245000fa90606dd9ec5eb96b54a6
Pascal PAILLET-LME <p.paillet@st.com>
2019-02-19 10:04:34 +0000

    regulator: stpmic1: Change buck1 voltage range
    Change buck1 voltage range to be conform with the data-sheet.
    
    Signed-off-by: pascal paillet <p.paillet@st.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/stpmic1_regulator.c
+++ drivers/regulator/stpmic1_regulator.c
@@ -75,9 +75,10 @@ enum {
 #define STPMIC1_BUCK_MODE_NORMAL 0
 #define STPMIC1_BUCK_MODE_LP BUCK_HPLP_ENABLE_MASK
 
-struct regulator_linear_range buck1_ranges[] = {
-	REGULATOR_LINEAR_RANGE(600000, 0, 30, 25000),
-	REGULATOR_LINEAR_RANGE(1350000, 31, 63, 0),
+static const struct regulator_linear_range buck1_ranges[] = {
+	REGULATOR_LINEAR_RANGE(725000, 0, 4, 0),
+	REGULATOR_LINEAR_RANGE(725000, 5, 36, 25000),
+	REGULATOR_LINEAR_RANGE(1500000, 37, 63, 0),
 };
 
 struct regulator_linear_range buck2_ranges[] = {
