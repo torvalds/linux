1fdafbd023793e754aa6ca73c877a375e83b8239
Christian König <christian.koenig@amd.com>
2019-02-20 13:46:24 +0100

    drm/amdgpu: fix dma mask check in gmc_v6_0.c
    This got messed up by "drm: change func to better detect wether swiotlb
    is needed".
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Michael D Labriola <michael.d.labriola@gmail.com>
    Link: https://patchwork.freedesktop.org/patch/287070/

--- drivers/gpu/drm/amd/amdgpu/gmc_v6_0.c
+++ drivers/gpu/drm/amd/amdgpu/gmc_v6_0.c
@@ -886,7 +886,7 @@ static int gmc_v6_0_sw_init(void *handle)
 		pci_set_consistent_dma_mask(adev->pdev, DMA_BIT_MASK(32));
 		dev_warn(adev->dev, "amdgpu: No coherent DMA available.\n");
 	}
-	adev->need_swiotlb = drm_get_max_iomem() > ((u64)1 << dma_bits);
+	adev->need_swiotlb = drm_need_swiotlb(dma_bits);
 
 	r = gmc_v6_0_init_microcode(adev);
 	if (r) {
