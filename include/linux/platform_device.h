a3dbb3d1ce6d757e60d09d962e611bad0e3be666
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2019-02-20 11:12:39 +0000

    UPSTREAM: drivers: provide devm_platform_ioremap_resource()
    There are currently 1200+ instances of using platform_get_resource()
    and devm_ioremap_resource() together in the kernel tree.
    
    This patch wraps these two calls in a single helper. Thanks to that
    we don't have to declare a local variable for struct resource * and can
    omit the redundant argument for resource type. We also have one
    function call less.
    
    Change-Id: Ibca8e80bb3724ee7dcfb1754ae93dbe6220bf556
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Finley Xiao <finley.xiao@rock-chips.com>
    (cherry picked from commit 7945f929f1a77a1c8887a97ca07f87626858ff42)

--- include/linux/platform_device.h
+++ include/linux/platform_device.h
@@ -51,6 +51,9 @@ extern struct device platform_bus;
 extern void arch_setup_pdev_archdata(struct platform_device *);
 extern struct resource *platform_get_resource(struct platform_device *,
 					      unsigned int, unsigned int);
+extern void __iomem *
+devm_platform_ioremap_resource(struct platform_device *pdev,
+			       unsigned int index);
 extern int platform_get_irq(struct platform_device *, unsigned int);
 extern int platform_irq_count(struct platform_device *);
 extern struct resource *platform_get_resource_byname(struct platform_device *,
