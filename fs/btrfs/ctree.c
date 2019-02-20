253002f2e3f4e2bbb0cbdd9e9fe2f5b9ec88f694
Filipe Manana <fdmanana@suse.com>
2019-02-20 11:11:43 +0000

    Btrfs: remove assertion when searching for a key in a node/leaf
    At ctree.c:key_search(), the assertion that verifies the first key on a
    child extent buffer corresponds to the key at a specific slot in the
    parent has a disadvantage: we effectively hit a BUG_ON() which requires
    rebooting the machine later. It also does not tell any information about
    which extent buffer is affected, from which root, the expected and found
    keys, etc.
    
    However as of commit 581c1760415c48 ("btrfs: Validate child tree block's
    level and first key"), that assertion is not needed since at the time we
    read an extent buffer from disk we validate that its first key matches the
    key, at the respective slot, in the parent extent buffer. Therefore just
    remove the assertion at key_search().
    
    Reviewed-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: Filipe Manana <fdmanana@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/ctree.c
+++ fs/btrfs/ctree.c
@@ -2544,26 +2544,6 @@ done:
 	return ret;
 }
 
-static void key_search_validate(struct extent_buffer *b,
-				const struct btrfs_key *key,
-				int level)
-{
-#ifdef CONFIG_BTRFS_ASSERT
-	struct btrfs_disk_key disk_key;
-
-	btrfs_cpu_key_to_disk(&disk_key, key);
-
-	if (level == 0)
-		ASSERT(!memcmp_extent_buffer(b, &disk_key,
-		    offsetof(struct btrfs_leaf, items[0].key),
-		    sizeof(disk_key)));
-	else
-		ASSERT(!memcmp_extent_buffer(b, &disk_key,
-		    offsetof(struct btrfs_node, ptrs[0].key),
-		    sizeof(disk_key)));
-#endif
-}
-
 static int key_search(struct extent_buffer *b, const struct btrfs_key *key,
 		      int level, int *prev_cmp, int *slot)
 {
@@ -2572,7 +2552,6 @@ static int key_search(struct extent_buffer *b, const struct btrfs_key *key,
 		return *prev_cmp;
 	}
 
-	key_search_validate(b, key, level);
 	*slot = 0;
 
 	return 0;
