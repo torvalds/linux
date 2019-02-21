b51905798195eeb427c873643b3ada0d7bd991a7
Jan Kara <jack@suse.cz>
2019-02-21 11:47:23 +0100

    fanotify: Make waits for fanotify events only killable
    Making waits for response to fanotify permission events interruptible
    can result in EINTR returns from open(2) or other syscalls when there's
    e.g. AV software that's monitoring the file. Orion reports that e.g.
    bash is complaining like:
    
    bash: /etc/bash_completion.d/itweb-settings.bash: Interrupted system call
    
    So for now convert the wait from interruptible to only killable one.
    That is mostly invisible to userspace. Sadly this breaks hibernation
    with fanotify permission events pending again but we have to put more
    thought into how to fix this without regressing userspace visible
    behavior.
    
    Reported-by: Orion Poplawski <orion@nwra.com>
    Signed-off-by: Jan Kara <jack@suse.cz>

--- fs/notify/fanotify/fanotify.c
+++ fs/notify/fanotify/fanotify.c
@@ -92,8 +92,8 @@ static int fanotify_get_response(struct fsnotify_group *group,
 
 	pr_debug("%s: group=%p event=%p\n", __func__, group, event);
 
-	ret = wait_event_interruptible(group->fanotify_data.access_waitq,
-				       event->state == FAN_EVENT_ANSWERED);
+	ret = wait_event_killable(group->fanotify_data.access_waitq,
+				  event->state == FAN_EVENT_ANSWERED);
 	/* Signal pending? */
 	if (ret < 0) {
 		spin_lock(&group->notification_lock);
