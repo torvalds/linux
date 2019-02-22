589a7543e74e0d4dac7aed750e03b35801297d0a
Hu Kejun <william.hu@rock-chips.com>
2019-02-22 10:38:44 +0800

    media: rockchip: isp1: change version to v0.1.1
    Change-Id: I3b13c01a69ea07e15dcf4b2ef1d04d2f46fd1ea2
    Signed-off-by: Hu Kejun <william.hu@rock-chips.com>

--- drivers/media/platform/rockchip/isp1/version.h
+++ drivers/media/platform/rockchip/isp1/version.h
@@ -11,8 +11,32 @@
  *v0.1.0:
  *1. First version;
  *
+ *v0.1.1:
+ *1. request isp irqs independently for rk3326/rk1808;
+ *2. fix demosaic is not bypass for grey sensor;
+ *3. rk3326 use csi2host instead of old mipi host;
+ *4. isp v12/v13 add raw stream;
+ *5. selfpath support interlace input;
+ *6. speed up stream off;
+ *7. support for RK3368;
+ *8. fix dvp data width config;
+ *9. fix sp rgb output format;
+ *10. del nonsupport yuv format;
+ *11. fix high fps preview blurred bug;
+ *12. support otp information;
+ *13. add module and lens name to match iq file;
+ *14. change vm149c driver and add vm149c dts node;
+ *15. support iesharp/demosaiclp/wdr for isp v12/v13;
+ *16. check first iq param is set or not;
+ *17. add macro to switch between old mipi and new mipi;
+ *18. stop isp when too many errors are reported;
+ *19. use tasklet to get 3A states;
+ *20. stop mipi with shutdown lan;
+ *21. check for capture S_FMT;
+ *22. raw patch with default sensor fmt&size;
+ *
  */
 
-#define RKISP1_DRIVER_VERSION KERNEL_VERSION(0, 1, 0x0)
+#define RKISP1_DRIVER_VERSION KERNEL_VERSION(0, 1, 0x1)
 
 #endif
