b997d1db8cae56cdc647f04523da0aa03e03948f
Andy Yan <andy.yan@rock-chips.com>
2019-02-22 19:09:15 +0800

    dt-bindings: add BOOT_PANIC and BOOT_WATCHDOG
    Change-Id: I66e3d7caa7b8591bd5af8d481ba773f5e38e9471
    Signed-off-by: Andy Yan <andy.yan@rock-chips.com>

--- include/dt-bindings/soc/rockchip,boot-mode.h
+++ include/dt-bindings/soc/rockchip,boot-mode.h
@@ -10,7 +10,11 @@
 #define BOOT_BL_DOWNLOAD	(REBOOT_FLAG + 1)
 /* enter recovery */
 #define BOOT_RECOVERY		(REBOOT_FLAG + 3)
- /* enter fastboot mode */
+/* reboot by panic */
+#define BOOT_PANIC		(REBOOT_FLAG + 7)
+/* reboot by watchdog */
+#define BOOT_WATCHDOG		(REBOOT_FLAG + 8)
+/* enter fastboot mode */
 #define BOOT_FASTBOOT		(REBOOT_FLAG + 9)
 /* enter charging mode */
 #define BOOT_CHARGING		(REBOOT_FLAG + 11)
