451431aa7ea253214779fac78b7873b4f2dcd1b7
Takashi Iwai <tiwai@suse.de>
2019-02-25 16:49:27 +0100

    ALSA: hda: Extend i915 component bind timeout
    I set 10 seconds for the timeout of the i915 audio component binding
    with a hope that recent machines are fast enough to handle all probe
    tasks in that period, but I was too optimistic.  The binding may take
    longer than that, and this caused a problem on the machine with both
    audio and graphics driver modules loaded in parallel, as Paul Menzel
    experienced.  This problem haven't hit so often just because the KMS
    driver is loaded in initrd on most machines.
    
    As a simple workaround, extend the timeout to 60 seconds.
    
    Fixes: f9b54e1961c7 ("ALSA: hda/i915: Allow delayed i915 audio component binding")
    Reported-by: Paul Menzel <pmenzel+alsa-devel@molgen.mpg.de>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit cfc35f9c128cea8fce6a5513b1de50d36f3b209f)

--- sound/hda/hdac_i915.c
+++ sound/hda/hdac_i915.c
@@ -146,9 +146,9 @@ int snd_hdac_i915_init(struct hdac_bus *bus)
 		return -ENODEV;
 	if (!acomp->ops) {
 		request_module("i915");
-		/* 10s timeout */
+		/* 60s timeout */
 		wait_for_completion_timeout(&bind_complete,
-					    msecs_to_jiffies(10 * 1000));
+					    msecs_to_jiffies(60 * 1000));
 	}
 	if (!acomp->ops) {
 		dev_info(bus->dev, "couldn't bind with audio component\n");
