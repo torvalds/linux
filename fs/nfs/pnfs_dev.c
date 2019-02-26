39a5201a2b450c2bf0447fb65305654c65f7f954
Trond Myklebust <trond.myklebust@hammerspace.com>
2019-02-26 14:27:58 -0500

    NFSv4/pnfs: Fix barriers in nfs4_mark_deviceid_unavailable()
    Fix the memory barriers in nfs4_mark_deviceid_unavailable() and
    nfs4_test_deviceid_unavailable().
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfs/pnfs_dev.c
+++ fs/nfs/pnfs_dev.c
@@ -287,7 +287,9 @@ void
 nfs4_mark_deviceid_unavailable(struct nfs4_deviceid_node *node)
 {
 	node->timestamp_unavailable = jiffies;
+	smp_mb__before_atomic();
 	set_bit(NFS_DEVICEID_UNAVAILABLE, &node->flags);
+	smp_mb__after_atomic();
 }
 EXPORT_SYMBOL_GPL(nfs4_mark_deviceid_unavailable);
 
@@ -302,6 +304,7 @@ nfs4_test_deviceid_unavailable(struct nfs4_deviceid_node *node)
 		if (time_in_range(node->timestamp_unavailable, start, end))
 			return true;
 		clear_bit(NFS_DEVICEID_UNAVAILABLE, &node->flags);
+		smp_mb__after_atomic();
 	}
 	return false;
 }
