549020c81475485f34f389ea1c3adf8c053072e3
Quentin Perret <quentin.perret@arm.com>
2019-02-28 10:19:30 +0000

    ANDROID: sched: Disable find_best_target() by default
    Now that the mainline EAS wake-up path has been extended to cope with
    prefer-idle tasks, the need for a dedicated Android-specific wake-up
    routine (find_best_target()) becomes less clear. Indeed, main reasons
    for introducting find_best_target() in the first place were:
    
      1. the energy_diff function was very slow, so we couldn't afford to
         use it on all CPUs for each wake-up for latency reasons;
    
      2. schedtune provides additional information about tasks (the
         prefer-idle flag in particular) which needed to be taken into
         account in the placement algorithm.
    
    Now that the energy diff calculation is much faster (with the simplified
    energy model) and that the EAS path is aware of prefer-idle tasks, there
    is no clear reason to use find_best_target() any more.
    
    So, let's disable it for now to minimize the amount of out-of-tree code
    used in the scheduler. If using the mainline path doesn't cause
    regressions, it is a good sign find_best_target() can be removed safely,
    eventually. Otherwise, reverting back to the old behaviour is trivial
    since this patch only changes the sched_feat default, but doesn't remove
    the fbt() code path.
    
    Bug: 120440300
    Change-Id: Idb5d68a3c4af7d2212e0922ab6d9a089170b5e1c
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -94,7 +94,7 @@ SCHED_FEAT(UTIL_EST, true)
 /*
  * Fast pre-selection of CPU candidates for EAS.
  */
-SCHED_FEAT(FIND_BEST_TARGET, true)
+SCHED_FEAT(FIND_BEST_TARGET, false)
 
 /*
  * Energy aware scheduling algorithm choices:
