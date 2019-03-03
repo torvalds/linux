2b1eeaf81d3f91c7994090efcbc8805f6f02bff6
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2019-03-03 18:24:33 +0100

    ath10k: Drop WARN_ON()s that always trigger during system resume
    BugLink: https://bugs.launchpad.net/bugs/1832749
    
    commit 9e80ad37f6788ed52b89a3cfcd593e0aa69b216d upstream.
    
    ath10k_mac_vif_chan() always returns an error for the given vif
    during system-wide resume which reliably triggers two WARN_ON()s
    in ath10k_bss_info_changed() and they are not particularly
    useful in that code path, so drop them.
    
    Tested: QCA6174 hw3.2 PCI with WLAN.RM.2.0-00180-QCARMSWPZ-1
    Tested: QCA6174 hw3.2 SDIO with WLAN.RMH.4.4.1-00007-QCARMSWP-1
    
    Fixes: cd93b83ad927 ("ath10k: support for multicast rate control")
    Fixes: f279294e9ee2 ("ath10k: add support for configuring management packet rate")
    Cc: stable@vger.kernel.org
    Reviewed-by: Brian Norris <briannorris@chromium.org>
    Tested-by: Brian Norris <briannorris@chromium.org>
    Tested-by: Claire Chang <tientzu@chromium.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Connor Kuehl <connor.kuehl@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- drivers/net/wireless/ath/ath10k/mac.c
+++ drivers/net/wireless/ath/ath10k/mac.c
@@ -5705,7 +5705,7 @@ static void ath10k_bss_info_changed(struct ieee80211_hw *hw,
 	}
 
 	if (changed & BSS_CHANGED_MCAST_RATE &&
-	    !WARN_ON(ath10k_mac_vif_chan(arvif->vif, &def))) {
+	    !ath10k_mac_vif_chan(arvif->vif, &def)) {
 		band = def.chan->band;
 		rateidx = vif->bss_conf.mcast_rate[band] - 1;
 
@@ -5743,7 +5743,7 @@ static void ath10k_bss_info_changed(struct ieee80211_hw *hw,
 	}
 
 	if (changed & BSS_CHANGED_BASIC_RATES) {
-		if (WARN_ON(ath10k_mac_vif_chan(vif, &def))) {
+		if (ath10k_mac_vif_chan(vif, &def)) {
 			mutex_unlock(&ar->conf_mutex);
 			return;
 		}
