faf96501305a706f8e01a67b4f78a4e09b57116a
Kai-Heng Feng <kai.heng.feng@canonical.com>
2019-03-05 16:55:21 +0800

    platform/x86: ideapad-laptop: Add Ideapad 530S-14ARR to no_hw_rfkill list
    This platform doesn't have hardware switch for rfkill, so add it to
    no_hw_rfkill list to prevent wireless gets hard-blocked.
    
    BugLink: https://bugs.launchpad.net/bugs/1818204
    Signed-off-by: Kai-Heng Feng <kai.heng.feng@canonical.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/platform/x86/ideapad-laptop.c
+++ drivers/platform/x86/ideapad-laptop.c
@@ -1098,6 +1098,13 @@ static const struct dmi_system_id no_hw_rfkill_list[] = {
 		},
 	},
 	{
+		.ident = "Lenovo ideapad 530S-14ARR",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
+			DMI_MATCH(DMI_PRODUCT_VERSION, "Lenovo ideapad 530S-14ARR"),
+		},
+	},
+	{
 		.ident = "Lenovo ideapad Y700-14ISK",
 		.matches = {
 			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
