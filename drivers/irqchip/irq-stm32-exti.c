3390ea2b6d7638c0da8950da54604b34bb8b1fab
Fabien Dessenne <fabien.dessenne@st.com>
2019-03-07 19:40:36 +0100

    irqchip/stm32: Don't set rising configuration registers at init
    BugLink: https://bugs.launchpad.net/bugs/1830906
    
    [ Upstream commit 6a77623d78b307b34d4cf7886da6a907689bf388 ]
    
    The rising configuration status register (rtsr) is not banked.
    As it is shared with the co-processor, it should not be written at probe
    time, else the co-processor configuration will be lost.
    
    Fixes: f9fc1745501e ("irqchip/stm32: Add host and driver data structures")
    Signed-off-by: Fabien Dessenne <fabien.dessenne@st.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/irqchip/irq-stm32-exti.c
+++ drivers/irqchip/irq-stm32-exti.c
@@ -716,7 +716,6 @@ stm32_exti_chip_data *stm32_exti_chip_init(struct stm32_exti_host_data *h_data,
 	const struct stm32_exti_bank *stm32_bank;
 	struct stm32_exti_chip_data *chip_data;
 	void __iomem *base = h_data->base;
-	u32 irqs_mask;
 
 	stm32_bank = h_data->drv_data->exti_banks[bank_idx];
 	chip_data = &h_data->chips_data[bank_idx];
@@ -724,10 +723,6 @@ stm32_exti_chip_data *stm32_exti_chip_init(struct stm32_exti_host_data *h_data,
 	chip_data->reg_bank = stm32_bank;
 
 	raw_spin_lock_init(&chip_data->rlock);
-
-	/* Determine number of irqs supported */
-	writel_relaxed(~0UL, base + stm32_bank->rtsr_ofst);
-	irqs_mask = readl_relaxed(base + stm32_bank->rtsr_ofst);
 
 	/*
 	 * This IP has no reset, so after hot reboot we should
