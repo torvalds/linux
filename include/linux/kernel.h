2dc0e68d5ada6d29554c760bee498c2612530d12
Masahiro Yamada <yamada.masahiro@socionext.com>
2019-03-07 16:27:11 -0800

    linux/kernel.h: use 'short' to define USHRT_MAX, SHRT_MAX, SHRT_MIN
    The commit log of 44f564a4bf6a ("ipc: add definitions of USHORT_MAX and
    others") did not explain why it used (s16) and (u16) instead of (short)
    and (unsigned short).
    
    Let's use (short) and (unsigned short), which is more sensible, and more
    consistent with the other MAX/MIN defines.
    
    As you see in include/uapi/asm-generic/int-ll64.h, s16/u16 are
    typedef'ed as signed/unsigned short.  So, this commit does not have a
    functional change.
    
    Remove the unneeded parentheses around ~0U while we are here.
    
    Link: http://lkml.kernel.org/r/1549156242-20806-1-git-send-email-yamada.masahiro@socionext.com
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Zhang Yanmin <yanmin.zhang@intel.com>
    Cc: Alex Elder <elder@linaro.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -17,9 +17,9 @@
 #include <asm/div64.h>
 #include <uapi/linux/kernel.h>
 
-#define USHRT_MAX	((u16)(~0U))
-#define SHRT_MAX	((s16)(USHRT_MAX>>1))
-#define SHRT_MIN	((s16)(-SHRT_MAX - 1))
+#define USHRT_MAX	((unsigned short)~0U)
+#define SHRT_MAX	((short)(USHRT_MAX>>1))
+#define SHRT_MIN	((short)(-SHRT_MAX - 1))
 #define INT_MAX		((int)(~0U>>1))
 #define INT_MIN		(-INT_MAX - 1)
 #define UINT_MAX	(~0U)
