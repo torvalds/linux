54da1d43c609bd250984290f9d08965ecaae53a0
José Roberto de Souza <jose.souza@intel.com>
2019-03-07 16:00:43 -0800

    drm/i915/psr: Only lookup for enabled CRTCs when forcing a fastset
    Forcing a specific CRTC to the eDP connector was causing the
    intel_psr_fastset_force() to mark mode_chaged in the wrong and
    disabled CRTC causing no update in the PSR state.
    
    Looks like our internal state track do not clear output_types and
    has_psr in the disabled CRTCs, not sure if this is the expected
    behavior or not but in the mean time this fix the issue.
    
    Cc: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Cc: Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
    Reviewed-by: Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
    Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190308000050.6226-2-jose.souza@intel.com

--- drivers/gpu/drm/i915/intel_psr.c
+++ drivers/gpu/drm/i915/intel_psr.c
@@ -981,7 +981,8 @@ retry:
 
 		intel_crtc_state = to_intel_crtc_state(crtc_state);
 
-		if (intel_crtc_has_type(intel_crtc_state, INTEL_OUTPUT_EDP) &&
+		if (crtc_state->active &&
+		    intel_crtc_has_type(intel_crtc_state, INTEL_OUTPUT_EDP) &&
 		    intel_crtc_state->has_psr) {
 			/* Mark mode as changed to trigger a pipe->update() */
 			crtc_state->mode_changed = true;
