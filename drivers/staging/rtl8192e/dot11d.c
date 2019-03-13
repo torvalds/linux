5af3304506aeb5fed63843c4114a6c4d8205ca56
Jules Irenge <jbi.octave@gmail.com>
2019-03-13 14:53:19 +0000

    staging: rtl8192e: add SPDX GPL-2.0 license identifier
    Add the SPDX GPL-2.0 license identifier to fix checkpatch.pl warning
    Issue found by checkpatch.pl warning:
    "WARNING: Missing or malformed SPDX-License-Identifier tag in line 1"
    
    Signed-off-by: Jules Irenge <jbi.octave@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/dot11d.c
+++ drivers/staging/rtl8192e/dot11d.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
 /******************************************************************************
  * Copyright(c) 2008 - 2010 Realtek Corporation. All rights reserved.
  *
