b67436152959361bd3960b948de61c055ef5cd76
Alexandre Belloni <alexandre.belloni@bootlin.com>
2019-03-15 14:35:08 +0100

    rtc: mv: convert to SPDX identifier
    Use SPDX-License-Identifier instead of a verbose license text.
    
    Reviewed-by: Gregory CLEMENT <gregory.clement@bootlin.com>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/rtc/rtc-mv.c
+++ drivers/rtc/rtc-mv.c
@@ -1,9 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Driver for the RTC in Marvell SoCs.
- *
- * This file is licensed under the terms of the GNU General Public
- * License version 2.  This program is licensed "as is" without any
- * warranty of any kind, whether express or implied.
  */
 
 #include <linux/init.h>
