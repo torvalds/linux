9f29b00c582641b865d3476dd81944375415ae0e
Gregory CLEMENT <gregory.clement@bootlin.com>
2019-03-15 10:52:29 +0100

    iio: adc: lpc32xx: Cleanup headers
    A few headers is useless: remove them.
    
    Signed-off-by: Gregory CLEMENT <gregory.clement@bootlin.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/adc/lpc32xx_adc.c
+++ drivers/iio/adc/lpc32xx_adc.c
@@ -9,17 +9,12 @@
 
 #include <linux/clk.h>
 #include <linux/completion.h>
-#include <linux/device.h>
 #include <linux/err.h>
 #include <linux/iio/iio.h>
-#include <linux/iio/sysfs.h>
 #include <linux/interrupt.h>
 #include <linux/io.h>
-#include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/of.h>
 #include <linux/platform_device.h>
-#include <linux/slab.h>
 
 /*
  * LPC32XX registers definitions
