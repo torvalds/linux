a7b4deeb02b978bc59808cb13c93ba84f01023a4
Anusha Srivatsa <anusha.srivatsa@intel.com>
2019-03-18 13:01:32 -0700

    drm/i915/cml: Add CML PCI IDS
    Comet Lake is a Intel Processor containing Gen9
    Intel HD Graphics. This patch adds the initial set of
    PCI IDs. Comet Lake comes off of Coffee Lake - adding
    the IDs to Coffee Lake ID list.
    
    More support and features will be in the patches that follow.
    
    v2: Split IDs according to GT. (Rodrigo)
    
    v3: Update IDs.
    
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Cc: Lucas De Marchi <lucas.demarchi@intel.com>
    Signed-off-by: Anusha Srivatsa <anusha.srivatsa@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190318200133.9666-1-anusha.srivatsa@intel.com

--- drivers/gpu/drm/i915/i915_pci.c
+++ drivers/gpu/drm/i915/i915_pci.c
@@ -795,6 +795,8 @@ static const struct pci_device_id pciidlist[] = {
 	INTEL_WHL_U_GT2_IDS(&intel_coffeelake_gt2_info),
 	INTEL_AML_CFL_GT2_IDS(&intel_coffeelake_gt2_info),
 	INTEL_WHL_U_GT3_IDS(&intel_coffeelake_gt3_info),
+	INTEL_CML_GT1_IDS(&intel_coffeelake_gt1_info),
+	INTEL_CML_GT2_IDS(&intel_coffeelake_gt2_info),
 	INTEL_CNL_IDS(&intel_cannonlake_info),
 	INTEL_ICL_11_IDS(&intel_icelake_11_info),
 	{0, 0, 0}
