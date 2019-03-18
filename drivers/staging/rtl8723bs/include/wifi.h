403fa6deca578f2eabc59d54dd58d1802fe04c9a
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2019-03-18 20:22:21 +0900

    staging: rtl8723bs: do not use __constant_cpu_to_le16
    cpu_to_le16() is capable enough to detect __builtin_constant_p()
    and to use an appropriate compile time ___constant_swahbXX()
    function.
    
    So we can use cpu_to_le16() instead of __constant_cpu_to_le16().
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/include/wifi.h
+++ drivers/staging/rtl8723bs/include/wifi.h
@@ -266,8 +266,8 @@ enum WIFI_REG_DOMAIN {
 
 #define SetFrameType(pbuf, type)	\
 	do {	\
-		*(unsigned short *)(pbuf) &= __constant_cpu_to_le16(~(BIT(3) | BIT(2))); \
-		*(unsigned short *)(pbuf) |= __constant_cpu_to_le16(type); \
+		*(unsigned short *)(pbuf) &= cpu_to_le16(~(BIT(3) | BIT(2))); \
+		*(unsigned short *)(pbuf) |= cpu_to_le16(type); \
 	} while (0)
 
 #define GetFrameSubType(pbuf)	(le16_to_cpu(*(__le16 *)(pbuf)) & (BIT(7) |\
