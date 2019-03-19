14b5f54b78292fdcf12ffb5e914f8fd2d190bcf5
Peng Fan <peng.fan@nxp.com>
2019-03-19 14:34:32 +0100

    ARM: 8850/1: use memblocks_present
    arm_memory_present is doing same thing as memblocks_present, so
    let's use common code memblocks_present instead of platform
    specific arm_memory_present.
    
    Patchwork: https://patchwork.kernel.org/patch/10805693/
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- arch/arm/mm/init.c
+++ arch/arm/mm/init.c
@@ -182,21 +182,6 @@ int pfn_valid(unsigned long pfn)
 EXPORT_SYMBOL(pfn_valid);
 #endif
 
-#ifndef CONFIG_SPARSEMEM
-static void __init arm_memory_present(void)
-{
-}
-#else
-static void __init arm_memory_present(void)
-{
-	struct memblock_region *reg;
-
-	for_each_memblock(memory, reg)
-		memory_present(0, memblock_region_memory_base_pfn(reg),
-			       memblock_region_memory_end_pfn(reg));
-}
-#endif
-
 static bool arm_memblock_steal_permitted = true;
 
 phys_addr_t __init arm_memblock_steal(phys_addr_t size, phys_addr_t align)
@@ -293,7 +278,7 @@ void __init bootmem_init(void)
 	 * Sparsemem tries to allocate bootmem in memory_present(),
 	 * so must be done after the fixed reservations
 	 */
-	arm_memory_present();
+	memblocks_present();
 
 	/*
 	 * sparse_init() needs the bootmem allocator up and running.
