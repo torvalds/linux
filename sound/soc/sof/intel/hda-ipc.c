9bfc74c46cf2044818f32dc98f0c5a194e1f8a67
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2019-03-20 09:53:26 +0100

    ASoC: SOF: Intel: remove an always true condition check
    msg->msg_data is pre-allocated during SOF probing, if the allocation
    fails, the probing is aborted. Moreover, in hda-ipc.c that buffer is
    used before being checked. Remove the redundant test.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- sound/soc/sof/intel/hda-ipc.c
+++ sound/soc/sof/intel/hda-ipc.c
@@ -105,7 +105,7 @@ int hda_dsp_ipc_get_reply(struct snd_sof_dev *sdev,
 	}
 
 	/* read the message */
-	if (msg->msg_data && size > 0)
+	if (size > 0)
 		sof_mailbox_read(sdev, sdev->host_box.offset,
 				 msg->reply_data, size);
 
