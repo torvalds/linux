43ce4c1feadbc84c772518a2d1974f6ba1b15089
David Howells <dhowells@redhat.com>
2019-03-21 09:22:36 +0000

    vfs: Add a single-or-reconfig keying to vfs_get_super()
    Add an additional keying mode to vfs_get_super() to indicate that only a
    single superblock should exist in the system, and that, if it does, further
    mounts should invoke reconfiguration upon it.
    
    This allows mount_single() to be replaced.
    
    [Fix by Eric Biggers folded in]
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/fs_context.h
+++ include/linux/fs_context.h
@@ -141,6 +141,7 @@ extern void put_fs_context(struct fs_context *fc);
  */
 enum vfs_get_super_keying {
 	vfs_get_single_super,	/* Only one such superblock may exist */
+	vfs_get_single_reconf_super, /* As above, but reconfigure if it exists */
 	vfs_get_keyed_super,	/* Superblocks with different s_fs_info keys may exist */
 	vfs_get_independent_super, /* Multiple independent superblocks may exist */
 };
@@ -153,6 +154,9 @@ extern int get_tree_nodev(struct fs_context *fc,
 			 int (*fill_super)(struct super_block *sb,
 					   struct fs_context *fc));
 extern int get_tree_single(struct fs_context *fc,
+			 int (*fill_super)(struct super_block *sb,
+					   struct fs_context *fc));
+extern int get_tree_single_reconf(struct fs_context *fc,
 			 int (*fill_super)(struct super_block *sb,
 					   struct fs_context *fc));
 extern int get_tree_keyed(struct fs_context *fc,
