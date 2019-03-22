9571710f0c4e4ad7ef98b1ae491fe3009e827b1a
Xiaojie Yuan <xiaojie.yuan@amd.com>
2019-03-22 13:14:25 +0800

    drm/amdgpu/sdma5: set clock gating for navi14
    same as navi10.
    
    Signed-off-by: Xiaojie Yuan <xiaojie.yuan@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/sdma_v5_0.c
+++ drivers/gpu/drm/amd/amdgpu/sdma_v5_0.c
@@ -1493,6 +1493,7 @@ static int sdma_v5_0_set_clockgating_state(void *handle,
 
 	switch (adev->asic_type) {
 	case CHIP_NAVI10:
+	case CHIP_NAVI14:
 		sdma_v5_0_update_medium_grain_clock_gating(adev,
 				state == AMD_CG_STATE_GATE ? true : false);
 		sdma_v5_0_update_medium_grain_light_sleep(adev,
