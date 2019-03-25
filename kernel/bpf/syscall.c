2a68c65abae66d28e2acb3245cb156ae2ea6eb1d
David Howells <dhowells@redhat.com>
2019-03-25 15:09:50 -0700

    UBUNTU: SAUCE: (efi-lockdown) bpf: Restrict kernel image access functions when the kernel is locked down
    There are some bpf functions can be used to read kernel memory:
    bpf_probe_read, bpf_probe_write_user and bpf_trace_printk.  These allow
    private keys in kernel memory (e.g. the hibernation image signing key) to
    be read by an eBPF program and kernel memory to be altered without
    restriction.
    
    Completely prohibit the use of BPF when the kernel is locked down.
    
    Suggested-by: Alexei Starovoitov <alexei.starovoitov@gmail.com>
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: netdev@vger.kernel.org
    cc: Chun-Yi Lee <jlee@suse.com>
    cc: Alexei Starovoitov <alexei.starovoitov@gmail.com>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Matthew Garrett <matthewgarrett@google.com>
    Acked-by: Tyler Hicks <tyhicks@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -2593,6 +2593,9 @@ SYSCALL_DEFINE3(bpf, int, cmd, union bpf_attr __user *, uattr, unsigned int, siz
 	if (sysctl_unprivileged_bpf_disabled && !capable(CAP_SYS_ADMIN))
 		return -EPERM;
 
+	if (kernel_is_locked_down("BPF"))
+		return -EPERM;
+
 	err = bpf_check_uarg_tail_zero(uattr, sizeof(attr), size);
 	if (err)
 		return err;
