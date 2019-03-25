45d4b7b9cb88526f6d5bd4c03efab88d75d10e4f
Yazen Ghannam <yazen.ghannam@amd.com>
2019-03-25 16:34:22 +0000

    x86/MCE: Add an MCE-record filtering function
    Some systems may report spurious MCA errors. In general, spurious MCA
    errors may be disabled by clearing a particular bit in MCA_CTL. However,
    clearing a bit in MCA_CTL may not be recommended for some errors, so the
    only option is to ignore them.
    
    An MCA error is printed and handled after it has been added to the MCE
    event pool. So an MCA error can be ignored by not adding it to that pool
    in the first place.
    
    Add such a filtering function.
    
     [ bp: Move function prototype to the internal header and massage. ]
    
    Signed-off-by: Yazen Ghannam <yazen.ghannam@amd.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: "clemej@gmail.com" <clemej@gmail.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Pu Wen <puwen@hygon.cn>
    Cc: Qiuxu Zhuo <qiuxu.zhuo@intel.com>
    Cc: "rafal@milecki.pl" <rafal@milecki.pl>
    Cc: Shirish S <Shirish.S@amd.com>
    Cc: <stable@vger.kernel.org> # 5.0.x
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Vishal Verma <vishal.l.verma@intel.com>
    Cc: x86-ml <x86@kernel.org>
    Link: https://lkml.kernel.org/r/20190325163410.171021-1-Yazen.Ghannam@amd.com

--- arch/x86/kernel/cpu/mce/core.c
+++ arch/x86/kernel/cpu/mce/core.c
@@ -1775,6 +1775,11 @@ static void __mcheck_cpu_init_timer(void)
 	mce_start_timer(t);
 }
 
+bool filter_mce(struct mce *m)
+{
+	return false;
+}
+
 /* Handle unconfigured int18 (should never happen) */
 static void unexpected_machine_check(struct pt_regs *regs, long error_code)
 {
