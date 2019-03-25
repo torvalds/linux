cb8be119d21d8a0affc3598a928dd0baf5da238f
Simon Horman <horms+renesas@verge.net.au>
2019-03-25 17:35:53 +0100

    math64: New DIV64_U64_ROUND_CLOSEST helper
    Provide DIV64_U64_ROUND_CLOSEST helper which performs division rounded to
    the closest integer using an unsigned 64bit dividend and divisor.
    
    This will be used in a follow-up patch to allow calculation of clock
    divisors with high frequency parents in the R-Car Gen3 CPG MSSR driver
    where overflow occurs if either the dividend or divisor is 32bit.
    
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- include/linux/math64.h
+++ include/linux/math64.h
@@ -284,4 +284,17 @@ static inline u64 mul_u64_u32_div(u64 a, u32 mul, u32 divisor)
 #define DIV64_U64_ROUND_UP(ll, d)	\
 	({ u64 _tmp = (d); div64_u64((ll) + _tmp - 1, _tmp); })
 
+/**
+ * DIV64_U64_ROUND_CLOSEST - unsigned 64bit divide with 64bit divisor rounded to nearest integer
+ * @dividend: unsigned 64bit dividend
+ * @divisor: unsigned 64bit divisor
+ *
+ * Divide unsigned 64bit dividend by unsigned 64bit divisor
+ * and round to closest integer.
+ *
+ * Return: dividend / divisor rounded to nearest integer
+ */
+#define DIV64_U64_ROUND_CLOSEST(dividend, divisor)	\
+	({ u64 _tmp = (divisor); div64_u64((dividend) + _tmp / 2, _tmp); })
+
 #endif /* _LINUX_MATH64_H */
