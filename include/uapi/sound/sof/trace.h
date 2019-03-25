03670025d8bb104c2a7bd6218759b631b6a5d123
Bartosz Kokoszko <bartoszx.kokoszko@linux.intel.com>
2019-03-25 09:43:23 +0100

    ASoC: SOF: remove TRACE_CLASS defines from trace.h
    I've removed TRACE_CLASS_* defines since they are not
    required by kernel (i.e. the only user of it is sof-logger).
    
    Signed-off-by: Bartosz Kokoszko <bartoszx.kokoszko@linux.intel.com>

--- include/uapi/sound/sof/trace.h
+++ include/uapi/sound/sof/trace.h
@@ -24,37 +24,6 @@ struct system_time {
 	uint32_t val_u;  /* Upper dword of current host time value */
 } __packed;
 
-/* trace event classes - high 8 bits*/
-#define TRACE_CLASS_IRQ		(1 << 24)
-#define TRACE_CLASS_IPC		(2 << 24)
-#define TRACE_CLASS_PIPE	(3 << 24)
-#define TRACE_CLASS_HOST	(4 << 24)
-#define TRACE_CLASS_DAI		(5 << 24)
-#define TRACE_CLASS_DMA		(6 << 24)
-#define TRACE_CLASS_SSP		(7 << 24)
-#define TRACE_CLASS_COMP	(8 << 24)
-#define TRACE_CLASS_WAIT	(9 << 24)
-#define TRACE_CLASS_LOCK	(10 << 24)
-#define TRACE_CLASS_MEM		(11 << 24)
-#define TRACE_CLASS_MIXER	(12 << 24)
-#define TRACE_CLASS_BUFFER	(13 << 24)
-#define TRACE_CLASS_VOLUME	(14 << 24)
-#define TRACE_CLASS_SWITCH	(15 << 24)
-#define TRACE_CLASS_MUX		(16 << 24)
-#define TRACE_CLASS_SRC         (17 << 24)
-#define TRACE_CLASS_TONE        (18 << 24)
-#define TRACE_CLASS_EQ_FIR      (19 << 24)
-#define TRACE_CLASS_EQ_IIR      (20 << 24)
-#define TRACE_CLASS_SA		(21 << 24)
-#define TRACE_CLASS_DMIC	(22 << 24)
-#define TRACE_CLASS_POWER	(23 << 24)
-#define TRACE_CLASS_IDC		(24 << 24)
-#define TRACE_CLASS_CPU		(25 << 24)
-#define TRACE_CLASS_EDF		(27 << 24)
-#define TRACE_CLASS_KPB		(28 << 24)
-#define TRACE_CLASS_SELECTOR	(29 << 24)
-#define TRACE_CLASS_SCHEDULE	(30 << 24)
-
 #define LOG_ENABLE		1  /* Enable logging */
 #define LOG_DISABLE		0  /* Disable logging */
 
