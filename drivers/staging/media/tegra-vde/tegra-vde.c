506bce733590fb625989ecf53f3eb8f9afa838c9
Jules Irenge <jbi.octave@gmail.com>
2019-03-25 17:42:06 +0000

    staging: tegra-vde: remove unnecessary boilerplate license identifier text
    Remove unnecessary boilerplate license identifier text.
    
    Signed-off-by: Jules Irenge <jbi.octave@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/media/tegra-vde/tegra-vde.c
+++ drivers/staging/media/tegra-vde/tegra-vde.c
@@ -4,10 +4,6 @@
  *
  * Copyright (C) 2016-2017 Dmitry Osipenko <digetx@gmail.com>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version
- * 2 of the License, or (at your option) any later version.
  */
 
 #include <linux/clk.h>
