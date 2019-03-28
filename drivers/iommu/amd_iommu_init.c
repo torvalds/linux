71f3397a1c311c7aad7d141871c5b3404d18b6d0
Joerg Roedel <jroedel@suse.de>
2019-03-28 11:44:59 +0100

    iommu/amd: Reserve exclusion range in iova-domain
    [ Upstream commit 8aafaaf2212192012f5bae305bb31cdf7681d777 ]
    
    If a device has an exclusion range specified in the IVRS
    table, this region needs to be reserved in the iova-domain
    of that device. This hasn't happened until now and can cause
    data corruption on data transfered with these devices.
    
    Treat exclusion ranges as reserved regions in the iommu-core
    to fix the problem.
    
    Fixes: be2a022c0dd0 ('x86, AMD IOMMU: add functions to parse IOMMU memory mapping requirements for devices')
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Reviewed-by: Gary R Hook <gary.hook@amd.com>
    Signed-off-by: Sasha Levin (Microsoft) <sashal@kernel.org>

--- drivers/iommu/amd_iommu_init.c
+++ drivers/iommu/amd_iommu_init.c
@@ -2001,6 +2001,9 @@ static int __init init_unity_map_range(struct ivmd_header *m)
 	if (e == NULL)
 		return -ENOMEM;
 
+	if (m->flags & IVMD_FLAG_EXCL_RANGE)
+		init_exclusion_range(m);
+
 	switch (m->type) {
 	default:
 		kfree(e);
@@ -2047,9 +2050,7 @@ static int __init init_memory_definitions(struct acpi_table_header *table)
 
 	while (p < end) {
 		m = (struct ivmd_header *)p;
-		if (m->flags & IVMD_FLAG_EXCL_RANGE)
-			init_exclusion_range(m);
-		else if (m->flags & IVMD_FLAG_UNITY_MAP)
+		if (m->flags & (IVMD_FLAG_UNITY_MAP | IVMD_FLAG_EXCL_RANGE))
 			init_unity_map_range(m);
 
 		p += m->length;
