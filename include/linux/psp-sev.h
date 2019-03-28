d6112ea0cb344d6f5ed519991e24f69ba4b43d0e
Singh, Brijesh <brijesh.singh@amd.com>
2019-03-28 21:58:52 +0000

    crypto: ccp - introduce SEV_GET_ID2 command
    The current definition and implementation of the SEV_GET_ID command
    does not provide the length of the unique ID returned by the firmware.
    As per the firmware specification, the firmware may return an ID
    length that is not restricted to 64 bytes as assumed by the SEV_GET_ID
    command.
    
    Introduce the SEV_GET_ID2 command to overcome with the SEV_GET_ID
    limitations. Deprecate the SEV_GET_ID in the favor of SEV_GET_ID2.
    
    At the same time update SEV API web link.
    
    Cc: Janakarajan Natarajan <Janakarajan.Natarajan@amd.com>
    Cc: Herbert Xu <herbert@gondor.apana.org.au>
    Cc: Gary Hook <gary.hook@amd.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Nathaniel McCallum <npmccallum@redhat.com>
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/linux/psp-sev.h
+++ include/linux/psp-sev.h
@@ -5,8 +5,7 @@
  *
  * Author: Brijesh Singh <brijesh.singh@amd.com>
  *
- * SEV spec 0.14 is available at:
- * http://support.amd.com/TechDocs/55766_SEV-KM API_Specification.pdf
+ * SEV API spec is available at https://developer.amd.com/sev
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
