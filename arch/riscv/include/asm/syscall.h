7af20b60f6dba0df784c19d5669a86fdc43eae1f
Dmitry V. Levin <ldv@altlinux.org>
2019-03-29 20:12:21 +0300

    riscv: Fix syscall_get_arguments() and syscall_set_arguments()
    commit 10a16997db3d99fc02c026cf2c6e6c670acafab0 upstream.
    
    RISC-V syscall arguments are located in orig_a0,a1..a5 fields
    of struct pt_regs.
    
    Due to an off-by-one bug and a bug in pointer arithmetic
    syscall_get_arguments() was reading s3..s7 fields instead of a1..a5.
    Likewise, syscall_set_arguments() was writing s3..s7 fields
    instead of a1..a5.
    
    Link: http://lkml.kernel.org/r/20190329171221.GA32456@altlinux.org
    
    Fixes: e2c0cdfba7f69 ("RISC-V: User-facing API")
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Will Drewry <wad@chromium.org>
    Cc: Albert Ou <aou@eecs.berkeley.edu>
    Cc: linux-riscv@lists.infradead.org
    Cc: stable@vger.kernel.org # v4.15+
    Acked-by: Palmer Dabbelt <palmer@sifive.com>
    Signed-off-by: Dmitry V. Levin <ldv@altlinux.org>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/riscv/include/asm/syscall.h
+++ arch/riscv/include/asm/syscall.h
@@ -78,10 +78,11 @@ static inline void syscall_get_arguments(struct task_struct *task,
 	if (i == 0) {
 		args[0] = regs->orig_a0;
 		args++;
-		i++;
 		n--;
+	} else {
+		i--;
 	}
-	memcpy(args, &regs->a1 + i * sizeof(regs->a1), n * sizeof(args[0]));
+	memcpy(args, &regs->a1 + i, n * sizeof(args[0]));
 }
 
 static inline void syscall_set_arguments(struct task_struct *task,
@@ -93,10 +94,11 @@ static inline void syscall_set_arguments(struct task_struct *task,
         if (i == 0) {
                 regs->orig_a0 = args[0];
                 args++;
-                i++;
                 n--;
-        }
-	memcpy(&regs->a1 + i * sizeof(regs->a1), args, n * sizeof(regs->a0));
+	} else {
+		i--;
+	}
+	memcpy(&regs->a1 + i, args, n * sizeof(regs->a1));
 }
 
 #endif	/* _ASM_RISCV_SYSCALL_H */
