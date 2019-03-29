44dc1b1fab787d265b9b3064bd564c87b6b86397
Qian Cai <cai@lca.pw>
2019-03-28 20:43:23 -0700

    mm/debug.c: add a cast to u64 for atomic64_read()
    atomic64_read() on ppc64le returns "long int", so fix the same way as
    commit d549f545e690 ("drm/virtio: use %llu format string form
    atomic64_t") by adding a cast to u64, which makes it work on all arches.
    
        In file included from ./include/linux/printk.h:7,
                         from ./include/linux/kernel.h:15,
                         from mm/debug.c:9:
        mm/debug.c: In function 'dump_mm':
        ./include/linux/kern_levels.h:5:18: warning: format '%llx' expects argument of type 'long long unsigned int', but argument 19 has type 'long int' [-Wformat=]
         #define KERN_SOH "A"  /* ASCII Start Of Header */
                          ^~~~~~
        ./include/linux/kern_levels.h:8:20: note: in expansion of macro
        'KERN_SOH'
         #define KERN_EMERG KERN_SOH "0" /* system is unusable */
                            ^~~~~~~~
        ./include/linux/printk.h:297:9: note: in expansion of macro 'KERN_EMERG'
          printk(KERN_EMERG pr_fmt(fmt), ##__VA_ARGS__)
                 ^~~~~~~~~~
        mm/debug.c:133:2: note: in expansion of macro 'pr_emerg'
          pr_emerg("mm %px mmap %px seqnum %llu task_size %lu"
          ^~~~~~~~
        mm/debug.c:140:17: note: format string is defined here
           "pinned_vm %llx data_vm %lx exec_vm %lx stack_vm %lx"
                      ~~~^
                      %lx
    
    Link: http://lkml.kernel.org/r/20190310183051.87303-1-cai@lca.pw
    Fixes: 70f8a3ca68d3 ("mm: make mm->pinned_vm an atomic64 counter")
    Signed-off-by: Qian Cai <cai@lca.pw>
    Acked-by: Davidlohr Bueso <dbueso@suse.de>
    Cc: Jason Gunthorpe <jgg@mellanox.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/debug.c
+++ mm/debug.c
@@ -168,7 +168,7 @@ void dump_mm(const struct mm_struct *mm)
 		mm_pgtables_bytes(mm),
 		mm->map_count,
 		mm->hiwater_rss, mm->hiwater_vm, mm->total_vm, mm->locked_vm,
-		atomic64_read(&mm->pinned_vm),
+		(u64)atomic64_read(&mm->pinned_vm),
 		mm->data_vm, mm->exec_vm, mm->stack_vm,
 		mm->start_code, mm->end_code, mm->start_data, mm->end_data,
 		mm->start_brk, mm->brk, mm->start_stack,
