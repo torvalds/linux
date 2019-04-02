0398635d80c47d1ff14dba31c29cc38958ebc8a2
Seth Forshee <seth.forshee@canonical.com>
2019-04-02 09:25:23 -0500

    Revert "UBUNTU: SAUCE: (snapdragon) cpufreq: Add apq8016 to cpufreq-dt-platdev blacklist"
    BugLink: https://bugs.launchpad.net/bugs/1820868
    
    This reverts commit f2a52900cd0a1194b67929ba4612296fdc80ccb3.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -120,7 +120,6 @@ static const struct of_device_id blacklist[] __initconst = {
 
 	{ .compatible = "nvidia,tegra124", },
 
-	{ .compatible = "qcom,apq8016", },
 	{ .compatible = "qcom,apq8096", },
 	{ .compatible = "qcom,msm8996", },
 
