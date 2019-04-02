cdd93e0809dd5482e60c5b450d114e171b4775ef
Sami Tolvanen <samitolvanen@google.com>
2019-04-02 11:59:46 -0700

    ANDROID: x86/cpu/vmware: use the full form of inl in VMWARE_PORT
    LLVM's assembler doesn't accept the short form inl (%%dx) instruction,
    but instead insists on the output register to be explicitly specified:
    
      <inline asm>:1:7: error: invalid operand for instruction
              inl (%dx)
                 ^
      LLVM ERROR: Error parsing inline asm
    
    Bug: 133186739
    Change-Id: I0519034f4a66bd72f23d206d4638578836a49ff5
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- arch/x86/kernel/cpu/vmware.c
+++ arch/x86/kernel/cpu/vmware.c
@@ -45,7 +45,7 @@
 #define VMWARE_PORT_CMD_VCPU_RESERVED	31
 
 #define VMWARE_PORT(cmd, eax, ebx, ecx, edx)				\
-	__asm__("inl (%%dx)" :						\
+	__asm__("inl (%%dx), %%eax" :					\
 			"=a"(eax), "=c"(ecx), "=d"(edx), "=b"(ebx) :	\
 			"0"(VMWARE_HYPERVISOR_MAGIC),			\
 			"1"(VMWARE_PORT_CMD_##cmd),			\
