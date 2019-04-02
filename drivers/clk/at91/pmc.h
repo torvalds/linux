a436c2a447e59740e8bc5c604abac33419099673
Alexandre Belloni <alexandre.belloni@bootlin.com>
2019-04-02 14:50:54 +0200

    clk: at91: add sam9x60 PLL driver
    The PLLs on the sam9x60 (PLLA and USB PLL) use a different register set and
    programming model than the previous SoCs.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/at91/pmc.h
+++ drivers/clk/at91/pmc.h
@@ -69,6 +69,7 @@ struct clk_pll_characteristics {
 	struct clk_range *output;
 	u16 *icpll;
 	u8 *out;
+	u8 upll : 1;
 };
 
 struct clk_programmable_layout {
@@ -168,6 +169,11 @@ at91_clk_register_pll(struct regmap *regmap, const char *name,
 struct clk_hw * __init
 at91_clk_register_plldiv(struct regmap *regmap, const char *name,
 			 const char *parent_name);
+
+struct clk_hw * __init
+sam9x60_clk_register_pll(struct regmap *regmap, spinlock_t *lock,
+			 const char *name, const char *parent_name, u8 id,
+			 const struct clk_pll_characteristics *characteristics);
 
 struct clk_hw * __init
 at91_clk_register_programmable(struct regmap *regmap, const char *name,
