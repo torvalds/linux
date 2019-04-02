b5d87567057051c5391d0f8a75bdae87975049af
Seth Forshee <seth.forshee@canonical.com>
2019-04-02 08:31:36 -0500

    UBUNTU: [Packaging] remove snapdragon flavour support
    BugLink: https://bugs.launchpad.net/bugs/1820868
    
    Snapdragon is being broken out into its own topic branch, so
    remove support for the arm64 snapdragon flavour from the master
    kernel.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- debian.master/rules.d/arm64.mk
+++ debian.master/rules.d/arm64.mk
@@ -2,17 +2,12 @@ human_arch	= ARMv8
 build_arch	= arm64
 header_arch	= arm64
 defconfig	= defconfig
-flavours	= generic snapdragon
+flavours	= generic
 build_image	= Image.gz
 kernel_file	= arch/$(build_arch)/boot/Image.gz
 install_file	= vmlinuz
 no_dumpfile = true
 uefi_signed     = true
-
-# The uboot used in ubuntu core can't handle Image.gz, so
-# create this flavour to generate a Image just for them
-build_image_snapdragon	= Image
-kernel_file_snapdragon	= arch/$(build_arch)/boot/Image
 
 loader		= grub
 vdso		= vdso_install
