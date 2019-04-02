8863eca8c46affaa91ae35390b00358b925483eb
Clark Wang <xiaoning.wang@nxp.com>
2019-04-02 12:45:53 +0000

    spi: lpspi: add missing complete in abort func at dma mode
    Add the missing complete operations for dma_completion to fix the
    problem of blocking at the wait_for_completion_interruptible()
    function when use spi_slave_abort().
    
    Signed-off-by: Clark Wang <xiaoning.wang@nxp.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-fsl-lpspi.c
+++ drivers/spi/spi-fsl-lpspi.c
@@ -482,7 +482,13 @@ static int fsl_lpspi_slave_abort(struct spi_controller *controller)
 				spi_controller_get_devdata(controller);
 
 	fsl_lpspi->slave_aborted = true;
-	complete(&fsl_lpspi->xfer_done);
+	if (!fsl_lpspi->usedma)
+		complete(&fsl_lpspi->xfer_done);
+	else {
+		complete(&fsl_lpspi->dma_tx_completion);
+		complete(&fsl_lpspi->dma_rx_completion);
+	}
+
 	return 0;
 }
 
