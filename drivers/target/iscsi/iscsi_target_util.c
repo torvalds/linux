b0055acaedf56a2717a6e2a4b700f1959a1b60df
Bart Van Assche <bvanassche@acm.org>
2019-04-02 12:58:11 -0700

    scsi: target/iscsi: Detect conn_cmd_list corruption early
    Certain behavior of an initiator can cause the target driver to send both a
    reject and a SCSI response. If that happens two target_put_sess_cmd() calls
    will occur without the command having been removed from conn_cmd_list. In
    other words, conn_cmd_list will get corrupted once the freed memory is
    reused. Although the Linux kernel can detect list corruption if list
    debugging is enabled, in this case the context in which list corruption is
    detected is not related to the context that caused list corruption.  Hence
    add WARN_ON() statements that report the context that is causing list
    corruption.
    
    Cc: Mike Christie <mchristi@redhat.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Hannes Reinecke <hare@suse.de>
    Cc: Nicholas Bellinger <nab@linux-iscsi.org>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/target/iscsi/iscsi_target_util.c
+++ drivers/target/iscsi/iscsi_target_util.c
@@ -769,6 +769,8 @@ void iscsit_free_cmd(struct iscsi_cmd *cmd, bool shutdown)
 	struct se_cmd *se_cmd = cmd->se_cmd.se_tfo ? &cmd->se_cmd : NULL;
 	int rc;
 
+	WARN_ON(!list_empty(&cmd->i_conn_node));
+
 	__iscsit_free_cmd(cmd, shutdown);
 	if (se_cmd) {
 		rc = transport_generic_free_cmd(se_cmd, shutdown);
