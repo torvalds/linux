7235560ac77a2516b84b86946debaa4fb951ed9b
Evan Green <evgreen@chromium.org>
2019-04-03 14:34:28 -0700

    platform/chrome: Add support for v1 of host sleep event
    Add support in code for the new forms of the host sleep event.
    Detects the presence of this version of the command at runtime,
    and use whichever form the EC supports. At this time, always
    request the default timeout, and only report the failing response
    via a WARN_ONCE(). Future versions could accept the sleep parameter
    from outside the driver, and return the response information to
    usermode or elsewhere.
    
    Signed-off-by: Evan Green <evgreen@chromium.org>
    Reviewed-by: Rajat Jain <rajatja@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Acked-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/cros_ec.h
+++ include/linux/mfd/cros_ec.h
@@ -121,6 +121,7 @@ struct cros_ec_command {
  * @pkt_xfer: Send packet to EC and get response.
  * @lock: One transaction at a time.
  * @mkbp_event_supported: True if this EC supports the MKBP event protocol.
+ * @host_sleep_v1: True if this EC supports the sleep v1 command.
  * @event_notifier: Interrupt event notifier for transport devices.
  * @event_data: Raw payload transferred with the MKBP event.
  * @event_size: Size in bytes of the event data.
@@ -154,6 +155,7 @@ struct cros_ec_device {
 			struct cros_ec_command *msg);
 	struct mutex lock;
 	bool mkbp_event_supported;
+	bool host_sleep_v1;
 	struct blocking_notifier_head event_notifier;
 
 	struct ec_response_get_next_event_v1 event_data;
