a5f50c501321249d67611353dde6d68d48c5b959
Jagan Teki <jagan@amarulasolutions.com>
2019-04-03 16:05:34 -0700

    Input: goodix - add GT5663 CTP support
    GT5663 is capacitive touch controller with customized smart
    wakeup gestures.
    
    Add support for it by adding compatible and supported chip data.
    
    The chip data on GT5663 is similar to GT1151, like
    - config data register has 0x8050 address
    - config data register max len is 240
    - config data checksum has 16-bit
    
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>
    Reviewed-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/touchscreen/goodix.c
+++ drivers/input/touchscreen/goodix.c
@@ -219,6 +219,7 @@ static const struct goodix_chip_data *goodix_get_chip_data(u16 id)
 {
 	switch (id) {
 	case 1151:
+	case 5663:
 	case 5688:
 		return &gt1x_chip_data;
 
@@ -997,6 +998,7 @@ MODULE_DEVICE_TABLE(acpi, goodix_acpi_match);
 #ifdef CONFIG_OF
 static const struct of_device_id goodix_of_match[] = {
 	{ .compatible = "goodix,gt1151" },
+	{ .compatible = "goodix,gt5663" },
 	{ .compatible = "goodix,gt5688" },
 	{ .compatible = "goodix,gt911" },
 	{ .compatible = "goodix,gt9110" },
