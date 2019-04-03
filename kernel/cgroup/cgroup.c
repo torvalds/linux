d6e486ee0ef2f99a4069d9186e53dac61b28cb3c
Shakeel Butt <shakeelb@google.com>
2019-04-03 16:03:54 -0700

    cgroup: remove extra cgroup_migrate_finish() call
    The callers of cgroup_migrate_prepare_dst() correctly call
    cgroup_migrate_finish() for success and failure cases both. No need to
    call it in cgroup_migrate_prepare_dst() in failure case.
    
    Signed-off-by: Shakeel Butt <shakeelb@google.com>
    Reviewed-by: Daniel Jordan <daniel.m.jordan@oracle.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -2602,7 +2602,7 @@ int cgroup_migrate_prepare_dst(struct cgroup_mgctx *mgctx)
 
 		dst_cset = find_css_set(src_cset, src_cset->mg_dst_cgrp);
 		if (!dst_cset)
-			goto err;
+			return -ENOMEM;
 
 		WARN_ON_ONCE(src_cset->mg_dst_cset || dst_cset->mg_dst_cset);
 
@@ -2634,9 +2634,6 @@ int cgroup_migrate_prepare_dst(struct cgroup_mgctx *mgctx)
 	}
 
 	return 0;
-err:
-	cgroup_migrate_finish(mgctx);
-	return -ENOMEM;
 }
 
 /**
