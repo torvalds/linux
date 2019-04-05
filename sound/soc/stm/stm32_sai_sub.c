d4f1e3ef958663e46c0e088c0671a2f1aa80f16c
Arnaud Pouliquen <arnaud.pouliquen@st.com>
2019-04-05 11:19:11 +0200

    ASoC: stm32: fix sai driver name initialisation
    commit 17d3069ccf06970e2db3f7cbf4335f207524279e upstream.
    
    This patch fixes the sai driver structure overwriting which results in
    a cpu dai name equal NULL.
    
    Fixes: 3e086ed ("ASoC: stm32: add SAI driver")
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- sound/soc/stm/stm32_sai_sub.c
+++ sound/soc/stm/stm32_sai_sub.c
@@ -1194,7 +1194,6 @@ static int stm32_sai_sub_dais_init(struct platform_device *pdev,
 	if (!sai->cpu_dai_drv)
 		return -ENOMEM;
 
-	sai->cpu_dai_drv->name = dev_name(&pdev->dev);
 	if (STM_SAI_IS_PLAYBACK(sai)) {
 		memcpy(sai->cpu_dai_drv, &stm32_sai_playback_dai,
 		       sizeof(stm32_sai_playback_dai));
@@ -1204,6 +1203,7 @@ static int stm32_sai_sub_dais_init(struct platform_device *pdev,
 		       sizeof(stm32_sai_capture_dai));
 		sai->cpu_dai_drv->capture.stream_name = sai->cpu_dai_drv->name;
 	}
+	sai->cpu_dai_drv->name = dev_name(&pdev->dev);
 
 	return 0;
 }
