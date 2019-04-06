dcd0998ab5110762b4369815758f4382a4806f96
Hariprasad Kelam <hariprasad.kelam@gmail.com>
2019-04-06 16:29:36 +0530

    f2fs: data: fix warning Using plain integer as NULL pointer
    changed passing function argument "0 to NULL" to fix below sparse
    warning
    
    fs/f2fs/data.c:426:47: warning: Using plain integer as NULL pointer
    
    Signed-off-by: Hariprasad Kelam <hariprasad.kelam@gmail.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Reviewed-by: Mukesh Ojha <mojha@codeaurora.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -425,7 +425,7 @@ static void __submit_merged_write_cond(struct f2fs_sb_info *sbi,
 
 void f2fs_submit_merged_write(struct f2fs_sb_info *sbi, enum page_type type)
 {
-	__submit_merged_write_cond(sbi, NULL, 0, 0, type, true);
+	__submit_merged_write_cond(sbi, NULL, NULL, 0, type, true);
 }
 
 void f2fs_submit_merged_write_cond(struct f2fs_sb_info *sbi,
