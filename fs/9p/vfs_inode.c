5e8a0770c01a77783376c7ffac4f86e21685434e
Al Viro <viro@zeniv.linux.org.uk>
2019-04-10 15:00:26 -0400

    9p: switch to ->free_inode()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/9p/vfs_inode.c
+++ fs/9p/vfs_inode.c
@@ -253,19 +253,13 @@ struct inode *v9fs_alloc_inode(struct super_block *sb)
 }
 
 /**
- * v9fs_destroy_inode - destroy an inode
+ * v9fs_free_inode - destroy an inode
  *
  */
 
-static void v9fs_i_callback(struct rcu_head *head)
+void v9fs_free_inode(struct inode *inode)
 {
-	struct inode *inode = container_of(head, struct inode, i_rcu);
 	kmem_cache_free(v9fs_inode_cache, V9FS_I(inode));
-}
-
-void v9fs_destroy_inode(struct inode *inode)
-{
-	call_rcu(&inode->i_rcu, v9fs_i_callback);
 }
 
 int v9fs_init_inode(struct v9fs_session_info *v9ses,
