d3692cb5c56daa2fb8046a8f4ebe1b2b8b22c895
Yong Zhi <yong.zhi@intel.com>
2019-04-10 12:27:31 -0500

    ASoC: Intel: common: add Geminilake Realtek+Maxim machine driver entry
    This patch adds glk_rt5682_max98357a_i2s machine driver entry into
    machine table. Both Skylake and SOF platform drivers can use this
    machine drivers.
    
    Signed-off-by: Yong Zhi <yong.zhi@intel.com>
    Signed-off-by: Naveen Manohar <naveen.m@intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/common/soc-acpi-intel-glk-match.c
+++ sound/soc/intel/common/soc-acpi-intel-glk-match.c
@@ -31,6 +31,15 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_glk_machines[] = {
 		.sof_fw_filename = "sof-glk.ri",
 		.sof_tplg_filename = "sof-glk-da7219.tplg",
 	},
+	{
+		.id = "10EC5682",
+		.drv_name = "glk_rt5682_max98357a",
+		.fw_filename = "intel/dsp_fw_glk.bin",
+		.machine_quirk = snd_soc_acpi_codec_list,
+		.quirk_data = &glk_codecs,
+		.sof_fw_filename = "sof-glk.ri",
+		.sof_tplg_filename = "sof-glk-rt5682.tplg",
+	},
 	{},
 };
 EXPORT_SYMBOL_GPL(snd_soc_acpi_intel_glk_machines);
