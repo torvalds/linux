63440a630d3b0cb6ab7c8507dc27a383fc82ed57
Robert Jones <rjones@gateworks.com>
2019-04-10 17:12:09 -0700

    iio: accel: add support to LIS2DE12
    Add support to STMicroelectronics LIS2DE12 accelerometer in
    st_accel framework.
    
    http://www.st.com/resource/en/datasheet/lis2de12.pdf
    
    Signed-off-by: Robert Jones <rjones@gateworks.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/accel/st_accel_i2c.c
+++ drivers/iio/accel/st_accel_i2c.c
@@ -102,6 +102,10 @@ static const struct of_device_id st_accel_of_match[] = {
 		.compatible = "st,lis3de",
 		.data = LIS3DE_ACCEL_DEV_NAME,
 	},
+	{
+		.compatible = "st,lis2de12",
+		.data = LIS2DE12_ACCEL_DEV_NAME,
+	},
 	{},
 };
 MODULE_DEVICE_TABLE(of, st_accel_of_match);
@@ -140,6 +144,7 @@ static const struct i2c_device_id st_accel_id_table[] = {
 	{ LIS3LV02DL_ACCEL_DEV_NAME },
 	{ LIS2DW12_ACCEL_DEV_NAME },
 	{ LIS3DE_ACCEL_DEV_NAME },
+	{ LIS2DE12_ACCEL_DEV_NAME },
 	{},
 };
 MODULE_DEVICE_TABLE(i2c, st_accel_id_table);
