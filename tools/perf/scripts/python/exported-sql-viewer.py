c6aba1bf258ff1ce201f112dafe1bdde601573dd
Adrian Hunter <adrian.hunter@intel.com>
2019-04-12 14:38:23 +0300

    perf scripts python: exported-sql-viewer.py: Change python2 to python
    Now that there is also support for python3, there is no need to specify
    python2 explicitly.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Link: http://lkml.kernel.org/r/20190412113830.4126-2-adrian.hunter@intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/scripts/python/exported-sql-viewer.py
+++ tools/perf/scripts/python/exported-sql-viewer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # SPDX-License-Identifier: GPL-2.0
 # exported-sql-viewer.py: view data from sql database
 # Copyright (c) 2014-2018, Intel Corporation.
