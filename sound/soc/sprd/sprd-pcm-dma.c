1587a061ef562de0d97c82a95863e191bcd69d63
Baolin Wang <baolin.wang@linaro.org>
2019-04-12 14:40:17 +0800

    ASoC: sprd: Add reserved DMA memory support
    For Spreadtrum audio platform driver, it need allocate a larger DMA buffer
    dynamically to copy audio data between userspace and kernel space, but that
    will increase the risk of memory allocation failure especially the system
    is under heavy load situation.
    
    To make sure the audio can work in this scenario, we usually reserve one
    region of memory to be used as a shared pool of DMA buffers for the
    platform component. So add of_reserved_mem_device_init_by_idx() function
    to initialize the shared pool of DMA buffers to be used by the platform
    component.
    
    Signed-off-by: Baolin Wang <baolin.wang@linaro.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sprd/sprd-pcm-dma.c
+++ sound/soc/sprd/sprd-pcm-dma.c
@@ -6,6 +6,7 @@
 #include <linux/dma/sprd-dma.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/of_reserved_mem.h>
 #include <linux/platform_device.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
@@ -530,7 +531,13 @@ static const struct snd_soc_component_driver sprd_soc_component = {
 
 static int sprd_soc_platform_probe(struct platform_device *pdev)
 {
+	struct device_node *np = pdev->dev.of_node;
 	int ret;
+
+	ret = of_reserved_mem_device_init_by_idx(&pdev->dev, np, 0);
+	if (ret)
+		dev_warn(&pdev->dev,
+			 "no reserved DMA memory for audio platform device\n");
 
 	ret = devm_snd_soc_register_component(&pdev->dev, &sprd_soc_component,
 					      NULL, 0);
