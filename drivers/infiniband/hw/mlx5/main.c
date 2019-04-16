7068730704966bd8021ca97fb1dcba2549e019ba
Jason Gunthorpe <jgg@mellanox.com>
2019-04-16 14:07:25 +0300

    RDMA/mlx5: Do not allow the user to write to the clock page
    BugLink: https://bugs.launchpad.net/bugs/1830929
    
    commit c660133c339f9ab684fdf568c0d51b9ae5e86002 upstream.
    
    The intent of this VMA was to be read-only from user space, but the
    VM_MAYWRITE masking was missed, so mprotect could make it writable.
    
    Cc: stable@vger.kernel.org
    Fixes: 5c99eaecb1fc ("IB/mlx5: Mmap the HCA's clock info to user-space")
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    Reviewed-by: Haggai Eran <haggaie@mellanox.com>
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/infiniband/hw/mlx5/main.c
+++ drivers/infiniband/hw/mlx5/main.c
@@ -1982,6 +1982,7 @@ static int mlx5_ib_mmap_clock_info_page(struct mlx5_ib_dev *dev,
 
 	if (vma->vm_flags & VM_WRITE)
 		return -EPERM;
+	vma->vm_flags &= ~VM_MAYWRITE;
 
 	if (!dev->mdev->clock_info_page)
 		return -EOPNOTSUPP;
@@ -2147,6 +2148,7 @@ static int mlx5_ib_mmap(struct ib_ucontext *ibcontext, struct vm_area_struct *vm
 
 		if (vma->vm_flags & VM_WRITE)
 			return -EPERM;
+		vma->vm_flags &= ~VM_MAYWRITE;
 
 		/* Don't expose to user-space information it shouldn't have */
 		if (PAGE_SIZE > 4096)
