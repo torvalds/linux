f921931f80fdb85353011b0ae7f9a3c1593881a5
Hemant Kumar <hemantk@codeaurora.org>
2019-04-15 18:54:50 -0700

    ANDROID: GKI: usb: gadget: support claiming indexed endpoints by name
    Partial cherry-pick of commit ee9b63c156e1 ("Give unique name to gsi
    endpoints") to support claiming an endpoint by name that has been
    prefixed with an index.
    
    Bug: 174539520
    Change-Id: I3d6e9a3f9eb3fd0c445afc3b76f28dc9199374f4
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    (cherry picked from commit ee9b63c156e12ea607b4eb00d80256ce8218ddcb)
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/usb/gadget/epautoconf.c
+++ drivers/usb/gadget/epautoconf.c
@@ -207,7 +207,7 @@ void usb_ep_autoconfig_reset (struct usb_gadget *gadget)
 EXPORT_SYMBOL_GPL(usb_ep_autoconfig_reset);
 
 /**
- * usb_ep_autoconfig_by_name - Used to pick the endpoint by name. eg ep1in-gsi
+ * usb_ep_autoconfig_by_name - Used to pick the endpoint by name. eg gsi-epin1
  * @gadget: The device to which the endpoint must belong.
  * @desc: Endpoint descriptor, with endpoint direction and transfer mode
  *	initialized.
@@ -223,8 +223,12 @@ struct usb_ep *usb_ep_autoconfig_by_name(
 	struct usb_ep	*ep;
 	bool ep_found = false;
 
+	if (!ep_name || !strlen(ep_name))
+		goto err;
+
 	list_for_each_entry(ep, &gadget->ep_list, ep_list)
-		if (strcmp(ep->name, ep_name) == 0 && !ep->driver_data) {
+		if (strncmp(ep->name, ep_name, strlen(ep_name)) == 0 &&
+				!ep->driver_data) {
 			ep_found = true;
 			break;
 		}
@@ -239,6 +243,7 @@ struct usb_ep *usb_ep_autoconfig_by_name(
 		return ep;
 	}
 
+err:
 	pr_err("%s:error finding ep %s\n", __func__, ep_name);
 	return NULL;
 }
