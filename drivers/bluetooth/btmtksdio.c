bcaa7d72dffddfa4196a37108d67fc12fb4edfca
Sean Wang <sean.wang@mediatek.com>
2019-04-18 17:08:01 +0800

    Bluetooth: btmtksdio: Fix hdev->stat.byte_rx accumulation
    Accumulate hdev->stat.byte_rx only for valid packets as btmtkuart doing.
    
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/btmtksdio.c
+++ drivers/bluetooth/btmtksdio.c
@@ -391,8 +391,6 @@ static int btmtksdio_rx_packet(struct btmtksdio_dev *bdev, u16 rx_size)
 	if (err < 0)
 		goto err_kfree_skb;
 
-	bdev->hdev->stat.byte_rx += rx_size;
-
 	sdio_hdr = (void *)skb->data;
 
 	/* We assume the default error as -EILSEQ simply to make the error path
@@ -456,6 +454,8 @@ static int btmtksdio_rx_packet(struct btmtksdio_dev *bdev, u16 rx_size)
 
 	/* Complete frame */
 	(&pkts[i])->recv(bdev->hdev, skb);
+
+	bdev->hdev->stat.byte_rx += rx_size;
 
 	return 0;
 
