523275a8b38932af7008349a6f92ff59d432425d
Jerry Lin <wahahab11@gmail.com>
2019-04-18 14:48:55 +0800

    staging: olpc_dcon: Convert all uses of old GPIO API to new descriptor API
    This commit eliminate all uses of legacy integer base GPIO API in
    olpc_dcon_xo_1_5.c and replace them with new descriptor GPIO API like
    those in olpc_dcon_xo_1.c.
    
    Also pull some common code with olpc_dcon_xo_1.c to olpc_dcon.h for code
    sharing.
    
    Signed-off-by: Jerry Lin <wahahab11@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/olpc_dcon/olpc_dcon_xo_1.c
+++ drivers/staging/olpc_dcon/olpc_dcon_xo_1.c
@@ -26,11 +26,6 @@ enum dcon_gpios {
 	OLPC_DCON_BLANK,
 };
 
-struct dcon_gpio {
-	const char *name;
-	unsigned long flags;
-};
-
 static const struct dcon_gpio gpios_asis[] = {
 	[OLPC_DCON_STAT0] = { .name = "dcon_stat0", .flags = GPIOD_ASIS },
 	[OLPC_DCON_STAT1] = { .name = "dcon_stat1", .flags = GPIOD_ASIS },
@@ -39,7 +34,7 @@ static const struct dcon_gpio gpios_asis[] = {
 	[OLPC_DCON_BLANK] = { .name = "dcon_blank", .flags = GPIOD_ASIS },
 };
 
-struct gpio_desc *gpios[5];
+static struct gpio_desc *gpios[5];
 
 static int dcon_init_xo_1(struct dcon_priv *dcon)
 {
