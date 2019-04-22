b226a893d2b39a112470e14732ccef60006ad159
Saurav Kashyap <skashyap@marvell.com>
2019-04-21 22:45:01 -0700

    scsi: qedf: Update the driver version to 8.37.25.20
    Update the driver version to 8.37.25.20.
    
    Signed-off-by: Saurav Kashyap <skashyap@marvell.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/qedf/qedf_version.h
+++ drivers/scsi/qedf/qedf_version.h
@@ -7,9 +7,9 @@
  *  this source tree.
  */
 
-#define QEDF_VERSION		"8.37.25.19"
+#define QEDF_VERSION		"8.37.25.20"
 #define QEDF_DRIVER_MAJOR_VER		8
 #define QEDF_DRIVER_MINOR_VER		37
 #define QEDF_DRIVER_REV_VER		25
-#define QEDF_DRIVER_ENG_VER		19
+#define QEDF_DRIVER_ENG_VER		20
 
