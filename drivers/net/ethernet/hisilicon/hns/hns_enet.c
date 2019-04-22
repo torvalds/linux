e875a409594b8e3e0c3f69182f7a0ece5aa3c164
Jun Xiao <xiaojun2@hisilicon.com>
2019-04-23 00:48:57 +0800

    net: hns: Fix WARNING when hns modules installed
    Commit dfdf26babc98 upstream
    
    this patch need merge to 4.19.y stable kernel
    
    Fix Conflict:already fixed the confilct dfdf26babc98 with Yonglong Liu
    
    stable candidate:user cannot connect to the internet via hns dev
    by default setting without this patch
    
    we have already verified this patch on kunpeng916 platform,
    and it works well.
    
    Signed-off-by: Yonglong Liu <liuyonglong@huawei.com>
    Signed-off-by: Jun Xiao <xiaojun2@hisilicon.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/hisilicon/hns/hns_enet.c
+++ drivers/net/ethernet/hisilicon/hns/hns_enet.c
@@ -1169,6 +1169,12 @@ int hns_nic_init_phy(struct net_device *ndev, struct hnae_handle *h)
 	if (!h->phy_dev)
 		return 0;
 
+	phy_dev->supported &= h->if_support;
+	phy_dev->advertising = phy_dev->supported;
+
+	if (h->phy_if == PHY_INTERFACE_MODE_XGMII)
+		phy_dev->autoneg = false;
+
 	if (h->phy_if != PHY_INTERFACE_MODE_XGMII) {
 		phy_dev->dev_flags = 0;
 
@@ -1179,15 +1185,6 @@ int hns_nic_init_phy(struct net_device *ndev, struct hnae_handle *h)
 	}
 	if (unlikely(ret))
 		return -ENODEV;
-
-	phy_dev->supported &= h->if_support;
-	phy_dev->advertising = phy_dev->supported;
-
-	if (h->phy_if == PHY_INTERFACE_MODE_XGMII)
-		phy_dev->autoneg = false;
-
-	if (h->phy_if == PHY_INTERFACE_MODE_SGMII)
-		phy_stop(phy_dev);
 
 	return 0;
 }
