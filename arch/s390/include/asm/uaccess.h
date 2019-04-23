fcc1ce5b4f42b4de3a8313bd9aacce39c60215d1
Vasily Gorbik <gor@linux.ibm.com>
2019-04-23 15:36:36 +0200

    s390/kasan: fix strncpy_from_user kasan checks
    [ Upstream commit 01eb42afb45719cb41bb32c278e068073738899d ]
    
    arch/s390/lib/uaccess.c is built without kasan instrumentation. Kasan
    checks are performed explicitly in copy_from_user/copy_to_user
    functions. But since those functions could be inlined, calls from
    files like uaccess.c with instrumentation disabled won't generate
    kasan reports. This is currently the case with strncpy_from_user
    function which was revealed by newly added kasan test. Avoid inlining of
    copy_from_user/copy_to_user when the kernel is built with kasan support
    to make sure kasan checks are fully functional.
    
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/s390/include/asm/uaccess.h
+++ arch/s390/include/asm/uaccess.h
@@ -56,8 +56,10 @@ raw_copy_from_user(void *to, const void __user *from, unsigned long n);
 unsigned long __must_check
 raw_copy_to_user(void __user *to, const void *from, unsigned long n);
 
+#ifndef CONFIG_KASAN
 #define INLINE_COPY_FROM_USER
 #define INLINE_COPY_TO_USER
+#endif
 
 #ifdef CONFIG_HAVE_MARCH_Z10_FEATURES
 
