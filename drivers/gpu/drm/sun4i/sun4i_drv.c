2e55caf6d037776159ec45da526937b2a2fbee1a
Paul Kocialkowski <paul.kocialkowski@bootlin.com>
2019-04-24 11:04:13 +0200

    drm/sun4i: Unbind components before releasing DRM and memory
    BugLink: https://bugs.launchpad.net/bugs/1836577
    
    [ Upstream commit e02bc29b2cfa7806830d6da8b2322cddd67e8dfe ]
    
    Our components may still be using the DRM device driver (if only to
    access our driver's private data), so make sure to unbind them before
    the final drm_dev_put.
    
    Also release our reserved memory after component unbind instead of
    before to match reverse creation order.
    
    Fixes: f5a9ed867c83 ("drm/sun4i: Fix component unbinding and component master deletion")
    Signed-off-by: Paul Kocialkowski <paul.kocialkowski@bootlin.com>
    Reviewed-by: Chen-Yu Tsai <wens@csie.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190424090413.6918-1-paul.kocialkowski@bootlin.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/gpu/drm/sun4i/sun4i_drv.c
+++ drivers/gpu/drm/sun4i/sun4i_drv.c
@@ -146,10 +146,11 @@ static void sun4i_drv_unbind(struct device *dev)
 	drm_dev_unregister(drm);
 	drm_kms_helper_poll_fini(drm);
 	drm_mode_config_cleanup(drm);
-	of_reserved_mem_device_release(dev);
-	drm_dev_put(drm);
 
 	component_unbind_all(dev, NULL);
+	of_reserved_mem_device_release(dev);
+
+	drm_dev_put(drm);
 }
 
 static const struct component_master_ops sun4i_drv_master_ops = {
