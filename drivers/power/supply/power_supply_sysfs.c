cb77fe70ecc53defacbe3d28f7ffea8f495e2a45
Ashay Jaiswal <ashayj@codeaurora.org>
2019-04-25 15:20:24 +0530

    ANDROID: GKI: power_supply: add properties to report parallel connection topology
    Add power_supply properties to report and the connection topology
    of parallel charger(input and output).
    
    Signed-off-by: Ashay Jaiswal <ashayj@codeaurora.org>
    (cherry picked from commit ad45db3e1f38bc72949d57d4926e7d5ef7c58e56)
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 150789066
    Change-Id: Id4e9e3c23d83e58906e0cb3dd208b57089eed81a

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -496,6 +496,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(cp_irq_status),
 	POWER_SUPPLY_ATTR(cp_ilim),
 	POWER_SUPPLY_ATTR(irq_status),
+	POWER_SUPPLY_ATTR(parallel_output_mode),
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	POWER_SUPPLY_ATTR(charge_charger_state),
