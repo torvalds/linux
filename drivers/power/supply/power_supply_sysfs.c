f7b16318cef851f460aa30c9471b524ce56370d6
Ashay Jaiswal <ashayj@codeaurora.org>
2019-04-25 15:20:24 +0530

    ANDROID: GKI: power_supply: add POWER_SUPPLY_PROP_IRQ_STATUS property
    Add "POWER_SUPPLY_PROP_IRQ_STATUS" property, this can be used
    by drivers to report interrupt status.
    
    Signed-off-by: Ashay Jaiswal <ashayj@codeaurora.org>
    (cherry picked from commit 74b1cb85b8eb3fd14b21d5625dc7a39a50e9377a)
    Signed-off-by: Mark Salyzyn <salyyzn@google.com>
    Bug: 150789066
    Change-Id: I846f98647844754b71de8b1ecd90abe0c2058709

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -495,6 +495,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(cp_toggle_switcher),
 	POWER_SUPPLY_ATTR(cp_irq_status),
 	POWER_SUPPLY_ATTR(cp_ilim),
+	POWER_SUPPLY_ATTR(irq_status),
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	POWER_SUPPLY_ATTR(charge_charger_state),
