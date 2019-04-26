8139ad043d632c0e9e12d760068a7a8e91659aa1
Andrey Ryabinin <aryabinin@virtuozzo.com>
2019-04-25 22:23:58 -0700

    mm/page_alloc.c: avoid potential NULL pointer dereference
    ac.preferred_zoneref->zone passed to alloc_flags_nofragment() can be NULL.
    'zone' pointer unconditionally derefernced in alloc_flags_nofragment().
    Bail out on NULL zone to avoid potential crash.  Currently we don't see
    any crashes only because alloc_flags_nofragment() has another bug which
    allows compiler to optimize away all accesses to 'zone'.
    
    Link: http://lkml.kernel.org/r/20190423120806.3503-1-aryabinin@virtuozzo.com
    Fixes: 6bb154504f8b ("mm, page_alloc: spread allocations across zones before introducing fragmentation")
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Acked-by: Mel Gorman <mgorman@techsingularity.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3432,6 +3432,9 @@ alloc_flags_nofragment(struct zone *zone, gfp_t gfp_mask)
 		alloc_flags |= ALLOC_KSWAPD;
 
 #ifdef CONFIG_ZONE_DMA32
+	if (!zone)
+		return alloc_flags;
+
 	if (zone_idx(zone) != ZONE_NORMAL)
 		goto out;
 
