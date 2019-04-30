95e0cf3caeb11e1b0398c747b5cfa12828263824
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2019-04-30 11:47:34 +0200

    i2c: synquacer: fix enumeration of slave devices
    The I2C host driver for SynQuacer fails to populate the of_node and
    ACPI companion fields of the struct i2c_adapter it instantiates,
    resulting in enumeration of the subordinate I2C bus to fail.
    
    Fixes: 0d676a6c4390 ("i2c: add support for Socionext SynQuacer I2C controller")
    Cc: <stable@vger.kernel.org> # v4.19+
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/busses/i2c-synquacer.c
+++ drivers/i2c/busses/i2c-synquacer.c
@@ -597,6 +597,8 @@ static int synquacer_i2c_probe(struct platform_device *pdev)
 	i2c->adapter = synquacer_i2c_ops;
 	i2c_set_adapdata(&i2c->adapter, i2c);
 	i2c->adapter.dev.parent = &pdev->dev;
+	i2c->adapter.dev.of_node = pdev->dev.of_node;
+	ACPI_COMPANION_SET(&i2c->adapter.dev, ACPI_COMPANION(&pdev->dev));
 	i2c->adapter.nr = pdev->id;
 	init_completion(&i2c->completion);
 
