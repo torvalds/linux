f70abd75b7c6c04d3219d0b3a0f3f15411b042fb
Bard liao <yung-chuan.liao@linux.intel.com>
2019-04-30 17:37:44 -0500

    ASoC: Intel: add sof-rt5682 machine driver
    The machine driver is a generic machine driver for SOF with rt5682
    codec. it currently supports BYT/CHT/ICL/CML. Depending on the SOC
    hdmi/speaker and DMIC support are added dynamically.
    
    Only add information related to SOF since the machine driver was
    only tested with SOF.
    
    Signed-off-by: Sathya Prakash M R <sathya.prakash.m.r@intel.com>
    Signed-off-by: Bard liao <yung-chuan.liao@linux.intel.com>
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/common/soc-acpi-intel-byt-match.c
+++ sound/soc/intel/common/soc-acpi-intel-byt-match.c
@@ -185,6 +185,12 @@ struct snd_soc_acpi_mach  snd_soc_acpi_intel_baytrail_machines[] = {
 		.sof_fw_filename = "sof-byt.ri",
 		.sof_tplg_filename = "sof-byt-es8316.tplg",
 	},
+	{
+		.id = "10EC5682",
+		.drv_name = "sof_rt5682",
+		.sof_fw_filename = "sof-byt.ri",
+		.sof_tplg_filename = "sof-byt-rt5682.tplg",
+	},
 	/* some Baytrail platforms rely on RT5645, use CHT machine driver */
 	{
 		.id = "10EC5645",
