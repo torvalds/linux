b424e432e770d6dd572765459d5b6a96a19c5286
Michal Kubecek <mkubecek@suse.cz>
2019-05-02 16:15:10 +0200

    netlink: add validation of NLA_F_NESTED flag
    Add new validation flag NL_VALIDATE_NESTED which adds three consistency
    checks of NLA_F_NESTED_FLAG:
    
      - the flag is set on attributes with NLA_NESTED{,_ARRAY} policy
      - the flag is not set on attributes with other policies except NLA_UNSPEC
      - the flag is set on attribute passed to nla_parse_nested()
    
    Signed-off-by: Michal Kubecek <mkubecek@suse.cz>
    
    v2: change error messages to mention NLA_F_NESTED explicitly
    Reviewed-by: Johannes Berg <johannes@sipsolutions.net>
    Reviewed-by: David Ahern <dsahern@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- lib/nlattr.c
+++ lib/nlattr.c
@@ -184,6 +184,21 @@ static int validate_nla(const struct nlattr *nla, int maxtype,
 		}
 	}
 
+	if (validate & NL_VALIDATE_NESTED) {
+		if ((pt->type == NLA_NESTED || pt->type == NLA_NESTED_ARRAY) &&
+		    !(nla->nla_type & NLA_F_NESTED)) {
+			NL_SET_ERR_MSG_ATTR(extack, nla,
+					    "NLA_F_NESTED is missing");
+			return -EINVAL;
+		}
+		if (pt->type != NLA_NESTED && pt->type != NLA_NESTED_ARRAY &&
+		    pt->type != NLA_UNSPEC && (nla->nla_type & NLA_F_NESTED)) {
+			NL_SET_ERR_MSG_ATTR(extack, nla,
+					    "NLA_F_NESTED not expected");
+			return -EINVAL;
+		}
+	}
+
 	switch (pt->type) {
 	case NLA_EXACT_LEN:
 		if (attrlen != pt->len)
