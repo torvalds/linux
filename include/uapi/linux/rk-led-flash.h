b2a218ec9115b4f54d64d41b61f9295dd14b0fb4
Cai YiWei <cyw@rock-chips.com>
2019-05-07 19:32:34 +0800

    media: rockchip: add led flash header
    Change-Id: Ia7833a62b7b04f2f282f88b9bb2b79886f1e4c18
    Signed-off-by: Cai YiWei <cyw@rock-chips.com>

---
+++ include/uapi/linux/rk-led-flash.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+/* Copyright (c) 2018 Fuzhou Rockchip Electronics Co., Ltd. */
+#ifndef RK_LED_FLASH_H
+#define RK_LED_FLASH_H
+
+#define RK_VIDIOC_FLASH_TIMEINFO \
+	_IOR('V', BASE_VIDIOC_PRIVATE + 0, struct timeval)
+
+#endif
