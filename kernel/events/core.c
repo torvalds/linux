9fd2e48b9ae17978b2c2a98c055c774d5d90bce8
Song Liu <songliubraving@fb.com>
2019-05-07 09:15:45 -0700

    perf/core: Allow non-privileged uprobe for user processes
    Currently, non-privileged user could only use uprobe with
    
        kernel.perf_event_paranoid = -1
    
    However, setting perf_event_paranoid to -1 leaks other users' processes to
    non-privileged uprobes.
    
    To introduce proper permission control of uprobes, we are building the
    following system:
    
      A daemon with CAP_SYS_ADMIN is in charge to create uprobes via tracefs;
      Users asks the daemon to create uprobes;
      Then user can attach uprobe only to processes owned by the user.
    
    This patch allows non-privileged user to attach uprobe to processes owned
    by the user.
    
    The following example shows how to use uprobe with non-privileged user.
    This is based on Brendan's blog post [1]
    
    1. Create uprobe with root:
    
      sudo perf probe -x 'readline%return +0($retval):string'
    
    2. Then non-root user can use the uprobe as:
    
      perf record -vvv -e probe_bash:readline__return -p <pid> sleep 20
      perf script
    
    [1] http://www.brendangregg.com/blog/2015-06-28/linux-ftrace-uprobe.html
    
    Signed-off-by: Song Liu <songliubraving@fb.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: <kernel-team@fb.com>
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lkml.kernel.org/r/20190507161545.788381-1-songliubraving@fb.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -8532,9 +8532,9 @@ static int perf_tp_event_match(struct perf_event *event,
 	if (event->hw.state & PERF_HES_STOPPED)
 		return 0;
 	/*
-	 * All tracepoints are from kernel-space.
+	 * If exclude_kernel, only trace user-space tracepoints (uprobes)
 	 */
-	if (event->attr.exclude_kernel)
+	if (event->attr.exclude_kernel && !user_mode(regs))
 		return 0;
 
 	if (!perf_tp_filter_match(event, data))
