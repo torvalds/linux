b4e82fee2abb338c3a86b2aa9d7287123cf66503
Razor246 <36740429+Razor246@users.noreply.github.com>
2019-05-09 13:09:27 +0100

    Update msi.h

--- arch/x86/include/asm/msi.h
+++ arch/x86/include/asm/msi.h
@@ -10,5 +10,6 @@ int pci_msi_prepare(struct irq_domain *domain, struct device *dev, int nvec,
 		    msi_alloc_info_t *arg);
 
 void pci_msi_set_desc(msi_alloc_info_t *arg, struct msi_desc *desc);
+void irq_msi_compose_msg(struct irq_data *data, struct msi_msg *msg);
 
 #endif /* _ASM_X86_MSI_H */
