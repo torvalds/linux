93cb52c1578b81f296d26c2c9796ee7019d0bf84
Razor246 <36740429+Razor246@users.noreply.github.com>
2019-05-09 13:22:28 +0100

    Update setup.h

--- arch/x86/include/asm/setup.h
+++ arch/x86/include/asm/setup.h
@@ -62,6 +62,12 @@ extern void x86_ce4100_early_setup(void);
 static inline void x86_ce4100_early_setup(void) { }
 #endif
 
+#ifdef CONFIG_X86_PS4
+extern void x86_ps4_early_setup(void);
+#else
+static inline void x86_ps4_early_setup(void) { }
+#endif
+
 #ifndef _SETUP
 
 #include <asm/espfix.h>
