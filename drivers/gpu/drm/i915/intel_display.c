2033012982231533e545b002f744a60699fe968c
Ville Syrjälä <ville.syrjala@linux.intel.com>
2019-05-09 15:21:58 +0300

    drm/i915: Bump fb stride limit to 128KiB for gen4+ and 256KiB for gen7+
    With gtt remapping plugged in we can simply raise the stride
    limit on gen4+. Let's just pick the limit to match the render
    engine max stride (256KiB on gen7+, 128KiB on gen4+).
    
    No remapping CCS because the virtual address of each page actually
    matters due to the new hash mode
    (WaCompressedResourceDisplayNewHashMode:skl,kbl etc.), and no
    remapping on gen2/3 due extra complications from fence alignment
    and gen2 2KiB GTT tile size. Also no real benefit since the
    display engine limits already match the other limits.
    
    v2: Rebase due to is_ccs_modifier()
    v3: Tweak the comment and commit msg
    v4: Fix gen4+ stride limit to be 128KiB
    
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch> #v3
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190509122159.24376-8-ville.syrjala@linux.intel.com
    Reviewed-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -2519,6 +2519,19 @@ static
 u32 intel_fb_max_stride(struct drm_i915_private *dev_priv,
 			u32 pixel_format, u64 modifier)
 {
+	/*
+	 * Arbitrary limit for gen4+ chosen to match the
+	 * render engine max stride.
+	 *
+	 * The new CCS hash mode makes remapping impossible
+	 */
+	if (!is_ccs_modifier(modifier)) {
+		if (INTEL_GEN(dev_priv) >= 7)
+			return 256*1024;
+		else if (INTEL_GEN(dev_priv) >= 4)
+			return 128*1024;
+	}
+
 	return intel_plane_fb_max_stride(dev_priv, pixel_format, modifier);
 }
 
