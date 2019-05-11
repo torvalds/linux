27d79a2b2bf0dbec1cc3d8ea269db4d5a0dac2f3
Shuah Khan <skhan@linuxfoundation.org>
2019-05-10 19:38:39 -0600

    selftests: fix bpf build/test workflow regression when KBUILD_OUTPUT is set
    commit 8ce72dc32578 ("selftests: fix headers_install circular dependency")
    broke bpf build/test workflow. When KBUILD_OUTPUT is set, bpf objects end
    up in KBUILD_OUTPUT build directory instead of in ../selftests/bpf.
    
    The following bpf workflow breaks when it can't find the test_verifier:
    
    cd tools/testing/selftests/bpf; make; ./test_verifier;
    
    Fix it to set OUTPUT only when it is undefined in lib.mk. It didn't need
    to be set in the first place.
    
    Fixes: 8ce72dc32578 ("selftests: fix headers_install circular dependency")
    Reported-by: Alexei Starovoitov <alexei.starovoitov@gmail.com>
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>

--- tools/testing/selftests/lib.mk
+++ tools/testing/selftests/lib.mk
@@ -3,15 +3,9 @@
 CC := $(CROSS_COMPILE)gcc
 
 ifeq (0,$(MAKELEVEL))
-    ifneq ($(O),)
-	OUTPUT := $(O)
-    else
-	ifneq ($(KBUILD_OUTPUT),)
-		OUTPUT := $(KBUILD_OUTPUT)
-	else
-		OUTPUT := $(shell pwd)
-		DEFAULT_INSTALL_HDR_PATH := 1
-	endif
+    ifeq ($(OUTPUT),)
+	OUTPUT := $(shell pwd)
+	DEFAULT_INSTALL_HDR_PATH := 1
     endif
 endif
 selfdir = $(realpath $(dir $(filter %/lib.mk,$(MAKEFILE_LIST))))
