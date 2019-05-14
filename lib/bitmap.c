cdc90a1871d6e64080f4506e900c6ef88e6fb39f
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2019-05-14 15:42:43 -0700

    lib/bitmap.c: guard exotic bitmap functions by CONFIG_NUMA
    The bitmap_remap, _bitremap, _onto and _fold functions are only used,
    via their node_ wrappers, in mm/mempolicy.c, which is only built for
    CONFIG_NUMA.  The helper bitmap_ord_to_pos used by these functions is
    global, but its only external caller is node_random() in lib/nodemask.c,
    which is also guarded by CONFIG_NUMA.
    
    For !CONFIG_NUMA:
    
    add/remove: 0/6 grow/shrink: 0/0 up/down: 0/-621 (-621)
    Function                                     old     new   delta
    bitmap_pos_to_ord                             20       -     -20
    bitmap_ord_to_pos                             70       -     -70
    bitmap_bitremap                               81       -     -81
    bitmap_fold                                  113       -    -113
    bitmap_onto                                  123       -    -123
    bitmap_remap                                 214       -    -214
    Total: Before=4776, After=4155, chg -13.00%
    
    Link: http://lkml.kernel.org/r/20190329205353.6010-2-linux@rasmusvillemoes.dk
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Cc: Andy Shevchenko <andy.shevchenko@gmail.com>
    Cc: Yury Norov <yury.norov@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/bitmap.c
+++ lib/bitmap.c
@@ -649,6 +649,7 @@ int bitmap_parselist_user(const char __user *ubuf,
 EXPORT_SYMBOL(bitmap_parselist_user);
 
 
+#ifdef CONFIG_NUMA
 /**
  * bitmap_pos_to_ord - find ordinal of set bit at given position in bitmap
  *	@buf: pointer to a bitmap
@@ -952,6 +953,7 @@ void bitmap_fold(unsigned long *dst, const unsigned long *orig,
 	for_each_set_bit(oldbit, orig, nbits)
 		set_bit(oldbit % sz, dst);
 }
+#endif /* CONFIG_NUMA */
 
 /*
  * Common code for bitmap_*_region() routines.
