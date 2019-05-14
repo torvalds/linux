8cecd0ba854799cda72d03a470e7de9eed3ed6c4
Goldwyn Rodrigues <rgoldwyn@suse.com>
2019-05-14 18:54:27 -0500

    iomap: add a filesystem hook for direct I/O bio submission
    This helps filesystems to perform tasks on the bio while submitting for
    I/O. This could be post-write operations such as data CRC or data
    replication for fs-handled RAID.
    
    Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
    Reviewed-by: Nikolay Borisov <nborisov@suse.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Goldwyn Rodrigues <rgoldwyn@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- include/linux/iomap.h
+++ include/linux/iomap.h
@@ -252,6 +252,8 @@ int iomap_writepages(struct address_space *mapping,
 struct iomap_dio_ops {
 	int (*end_io)(struct kiocb *iocb, ssize_t size, int error,
 		      unsigned flags);
+	blk_qc_t (*submit_io)(struct inode *inode, struct iomap *iomap,
+			struct bio *bio, loff_t file_offset);
 };
 
 ssize_t iomap_dio_rw(struct kiocb *iocb, struct iov_iter *iter,
