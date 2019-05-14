5c92bf4d717392a41cc0df0e5c2e917810f1d829
Yurii Pavlovskyi <yurii.pavlovskyi@gmail.com>
2019-05-14 20:54:50 +0200

    platform/x86: asus-wmi: Increase input buffer size of WMI methods
    [ Upstream commit 98e865a522983f2afde075648ec9d15ea4bb9194 ]
    
    The asus-nb-wmi driver is matched by WMI alias but fails to load on TUF
    Gaming series laptops producing multiple ACPI errors in the kernel log.
    
    The input buffer for WMI method invocation size is 2 dwords, whereas
    3 are expected by this model.
    
    FX505GM:
    ..
    Method (WMNB, 3, Serialized)
    {
        P8XH (Zero, 0x11)
        CreateDWordField (Arg2, Zero, IIA0)
        CreateDWordField (Arg2, 0x04, IIA1)
        CreateDWordField (Arg2, 0x08, IIA2)
        Local0 = (Arg1 & 0xFFFFFFFF)
        ...
    
    Compare with older K54C:
    ...
    Method (WMNB, 3, NotSerialized)
    {
        CreateDWordField (Arg2, 0x00, IIA0)
        CreateDWordField (Arg2, 0x04, IIA1)
        Local0 = (Arg1 & 0xFFFFFFFF)
        ...
    
    Increase buffer size to 3 dwords. No negative consequences of this change
    are expected, as the input buffer size is not verified. The original
    function is replaced by a wrapper for a new method passing value 0 for the
    last parameter. The new function will be used to control RGB keyboard
    backlight.
    
    Signed-off-by: Yurii Pavlovskyi <yurii.pavlovskyi@gmail.com>
    Reviewed-by: Daniel Drake <drake@endlessm.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/platform/x86/asus-wmi.c
+++ drivers/platform/x86/asus-wmi.c
@@ -85,6 +85,7 @@ static bool ashs_present(void)
 struct bios_args {
 	u32 arg0;
 	u32 arg1;
+	u32 arg2; /* At least TUF Gaming series uses 3 dword input buffer. */
 } __packed;
 
 /*
@@ -211,11 +212,13 @@ static void asus_wmi_input_exit(struct asus_wmi *asus)
 	asus->inputdev = NULL;
 }
 
-int asus_wmi_evaluate_method(u32 method_id, u32 arg0, u32 arg1, u32 *retval)
+static int asus_wmi_evaluate_method3(u32 method_id,
+		u32 arg0, u32 arg1, u32 arg2, u32 *retval)
 {
 	struct bios_args args = {
 		.arg0 = arg0,
 		.arg1 = arg1,
+		.arg2 = arg2,
 	};
 	struct acpi_buffer input = { (acpi_size) sizeof(args), &args };
 	struct acpi_buffer output = { ACPI_ALLOCATE_BUFFER, NULL };
@@ -246,6 +249,11 @@ exit:
 		return -ENODEV;
 
 	return 0;
+}
+
+int asus_wmi_evaluate_method(u32 method_id, u32 arg0, u32 arg1, u32 *retval)
+{
+	return asus_wmi_evaluate_method3(method_id, arg0, arg1, 0, retval);
 }
 EXPORT_SYMBOL_GPL(asus_wmi_evaluate_method);
 
