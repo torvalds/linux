c0b0ebb1719984513333f88d0c2a2bb3fdb61474
Jonas Karlman <jonas@kwiboo.se>
2019-05-16 19:40:07 +0530

    drm: Add reference counting on HDR metadata blob
    This adds reference count for HDR metadata blob,
    handled as part of duplicate and destroy connector
    state functions.
    
    v2: Removed the hdr_metadata_changed initialization as
    the variable is dropped and not required.
    
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/1558015817-12025-3-git-send-email-uma.shankar@intel.com

--- drivers/gpu/drm/drm_atomic_state_helper.c
+++ drivers/gpu/drm/drm_atomic_state_helper.c
@@ -391,6 +391,9 @@ __drm_atomic_helper_connector_duplicate_state(struct drm_connector *connector,
 		drm_connector_get(connector);
 	state->commit = NULL;
 
+	if (state->hdr_output_metadata)
+		drm_property_blob_get(state->hdr_output_metadata);
+
 	/* Don't copy over a writeback job, they are used only once */
 	state->writeback_job = NULL;
 }
@@ -438,6 +441,8 @@ __drm_atomic_helper_connector_destroy_state(struct drm_connector_state *state)
 
 	if (state->writeback_job)
 		drm_writeback_cleanup_job(state->writeback_job);
+
+	drm_property_blob_put(state->hdr_output_metadata);
 }
 EXPORT_SYMBOL(__drm_atomic_helper_connector_destroy_state);
 
