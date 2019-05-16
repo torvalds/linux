9783da2384c5623d376e4641bbce9339be1001eb
Christoph Niedermaier <cniedermaier@dh-electronics.com>
2019-05-16 13:01:49 +0200

    spi: spidev: Add dhcom-board to compatibility list
    Prepare to use devices on the external SPI interface
    on a DH electronics Development Board.
    
    Signed-off-by: Christoph Niedermaier <cniedermaier@dh-electronics.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -672,6 +672,7 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "ge,achc" },
 	{ .compatible = "semtech,sx1301" },
 	{ .compatible = "lwn,bk4" },
+	{ .compatible = "dh,dhcom-board" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
