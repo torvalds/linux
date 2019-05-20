48b48750c3f9f91a4e340d0f796734ddd178ad9d
Al Viro <viro@zeniv.linux.org.uk>
2019-05-20 13:44:57 +0100

    zsmalloc: don't bother with dentry_operations
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -1817,11 +1817,7 @@ static void lock_zspage(struct zspage *zspage)
 static struct dentry *zs_mount(struct file_system_type *fs_type,
 				int flags, const char *dev_name, void *data)
 {
-	static const struct dentry_operations ops = {
-		.d_dname = simple_dname,
-	};
-
-	return mount_pseudo(fs_type, "zsmalloc:", NULL, &ops, ZSMALLOC_MAGIC);
+	return mount_pseudo(fs_type, "zsmalloc:", NULL, NULL, ZSMALLOC_MAGIC);
 }
 
 static struct file_system_type zsmalloc_fs = {
