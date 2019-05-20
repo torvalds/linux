0e6edceb8f18a4e31526d83e6099fef1f29c3af5
Wanpeng Li <wanpengli@tencent.com>
2019-05-20 16:18:06 +0800

    KVM: LAPIC: Fix lapic_timer_advance_ns parameter overflow
    After commit c3941d9e0 (KVM: lapic: Allow user to disable adaptive tuning of
    timer advancement), '-1' enables adaptive tuning starting from default
    advancment of 1000ns. However, we should expose an int instead of an overflow
    uint module parameter.
    
    Before patch:
    
    /sys/module/kvm/parameters/lapic_timer_advance_ns:4294967295
    
    After patch:
    
    /sys/module/kvm/parameters/lapic_timer_advance_ns:-1
    
    Fixes: c3941d9e0 (KVM: lapic: Allow user to disable adaptive tuning of timer advancement)
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Radim Krčmář <rkrcmar@redhat.com>
    Cc: Sean Christopherson <sean.j.christopherson@intel.com>
    Cc: Liran Alon <liran.alon@oracle.com>
    Reviewed-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Wanpeng Li <wanpengli@tencent.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -143,7 +143,7 @@ module_param(tsc_tolerance_ppm, uint, S_IRUGO | S_IWUSR);
  * tuning, i.e. allows priveleged userspace to set an exact advancement time.
  */
 static int __read_mostly lapic_timer_advance_ns = -1;
-module_param(lapic_timer_advance_ns, uint, S_IRUGO | S_IWUSR);
+module_param(lapic_timer_advance_ns, int, S_IRUGO | S_IWUSR);
 
 static bool __read_mostly vector_hashing = true;
 module_param(vector_hashing, bool, S_IRUGO);
