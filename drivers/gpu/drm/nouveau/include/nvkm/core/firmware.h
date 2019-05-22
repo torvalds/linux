475cf02b834599c8a7a21a1e2d11449b7217d856
Ben Skeggs <bskeggs@redhat.com>
2019-05-22 16:15:54 +1000

    drm/nouveau/core: support versioned firmware loading
    We have a need for this now with updated SEC2 LS FW images that have an
    incompatible interface from the previous version.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/core/firmware.h
+++ drivers/gpu/drm/nouveau/include/nvkm/core/firmware.h
@@ -3,7 +3,10 @@
 #define __NVKM_FIRMWARE_H__
 #include <core/subdev.h>
 
+int nvkm_firmware_get_version(const struct nvkm_subdev *, const char *fwname,
+			      int min_version, int max_version,
+			      const struct firmware **);
 int nvkm_firmware_get(const struct nvkm_subdev *, const char *fwname,
-		      const struct firmware **fw);
-void nvkm_firmware_put(const struct firmware *fw);
+		      const struct firmware **);
+void nvkm_firmware_put(const struct firmware *);
 #endif
