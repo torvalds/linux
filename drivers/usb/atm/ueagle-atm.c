5e456a9263da2adc95d2744e2add5835f298d08d
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2019-05-24 10:30:39 +0200

    USB: atm: ueagle-atm.c: fix SPDX tag to be BSD2
    Thomas rightly points out that I got the BSD clause count wrong for this
    driver, it should be 2, not 3 based on the text in the license here, so
    fix that up.
    
    He also raises the question that the license text points to a v2 only
    license, yet the text in the header says "or later".  Given that the
    text in the header says "or later" and all of the other USB atm drivers
    are licensed in that way, I am going to leave the string as-is for that
    mark.
    
    Reported-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Matthieu CASTET <castet.matthieu@free.fr>
    Cc: Stanislaw Gruszka <stf_xl@wp.pl>
    Cc: Damien Bergamini <damien.bergamini@free.fr>
    Cc: Duncan Sands <duncan.sands@free.fr>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/atm/ueagle-atm.c
+++ drivers/usb/atm/ueagle-atm.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: (GPL-2.0+ OR BSD-3-Clause)
+// SPDX-License-Identifier: (GPL-2.0+ OR BSD-2-Clause)
 /*-
  * Copyright (c) 2003, 2004
  *	Damien Bergamini <damien.bergamini@free.fr>. All rights reserved.
