aa21cdfe5cd1c4a09e6c5fb22e4b6e65880131a8
Jon Lin <jon.lin@rock-chips.com>
2019-05-24 20:37:31 +0800

    drivers: rkflash: fix build error
    fix build without nand support.
    
    Change-Id: I9a5a2d63b335c42bcff99853b790796dd35355de
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/rkflash/rkflash_blk.c
+++ drivers/rkflash/rkflash_blk.c
@@ -229,12 +229,15 @@ static unsigned int rk_partition_init(struct flash_part *part)
 
 static int rkflash_proc_show(struct seq_file *m, void *v)
 {
-	int real_size = 0;
 	char *ftl_buf = kzalloc(4096, GFP_KERNEL);
+
+#if IS_ENABLED(CONFIG_RK_NANDC_NAND) || IS_ENABLED(CONFIG_RK_SFC_NAND)
+	int real_size = 0;
 
 	real_size = rknand_proc_ftlread(4096, ftl_buf);
 	if (real_size > 0)
 		seq_printf(m, "%s", ftl_buf);
+#endif
 	seq_printf(m, "Totle Read %ld KB\n", totle_read_data >> 1);
 	seq_printf(m, "Totle Write %ld KB\n", totle_write_data >> 1);
 	seq_printf(m, "totle_write_count %ld\n", totle_write_count);
