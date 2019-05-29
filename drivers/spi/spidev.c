c6e2d2c7ca4e6d09bc07ab5ced2e687cc842a860
Marek Vasut <marex@denx.de>
2019-05-29 12:24:51 +0200

    spidev: Add M53Menlo CPLD compatible
    Add compatible string for Menlosystems CPLD present on the M53Menlo
    board. This CPLD is used to communicate with various custom sensors.
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    To: linux-spi@vger.kernel.org
    Cc: Mark Brown <broonie@kernel.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -673,6 +673,7 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "semtech,sx1301" },
 	{ .compatible = "lwn,bk4" },
 	{ .compatible = "dh,dhcom-board" },
+	{ .compatible = "menlo,m53cpld" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
