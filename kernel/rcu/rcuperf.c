b3f3886c59f649ace424d132bd8c06e3611c71a8
Xiao Yang <ice_yangxiao@163.com>
2019-05-31 23:15:45 +0800

    rcuperf: Fix perf_type module-parameter description
    The rcu_bh rcuperf type was removed by commit 620d246065cd("rcuperf:
    Remove the "rcu_bh" and "sched" torture types"), but it lives on in the
    MODULE_PARM_DESC() of perf_type.  This commit therefore changes that
    module-parameter description to substitute srcu for rcu_bh.
    
    Signed-off-by: Xiao Yang <ice_yangxiao@163.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>

--- kernel/rcu/rcuperf.c
+++ kernel/rcu/rcuperf.c
@@ -89,7 +89,7 @@ torture_param(int, writer_holdoff, 0, "Holdoff (us) between GPs, zero to disable
 
 static char *perf_type = "rcu";
 module_param(perf_type, charp, 0444);
-MODULE_PARM_DESC(perf_type, "Type of RCU to performance-test (rcu, rcu_bh, ...)");
+MODULE_PARM_DESC(perf_type, "Type of RCU to performance-test (rcu, srcu, ...)");
 
 static int nrealreaders;
 static int nrealwriters;
