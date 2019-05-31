6672e11cad662ce6631e04c38f92a140a99c042c
Jordan Crouse <jcrouse@codeaurora.org>
2019-05-31 16:09:38 -0600

    drm/msm/adreno: Ensure that the zap shader region is big enough
    Before loading the zap shader we should ensure that the reserved memory
    region is big enough to hold the loaded file.
    
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Reviewed-by: Jeffrey Hugo <jeffrey.l.hugo@gmail.com>
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/adreno/adreno_gpu.c
+++ drivers/gpu/drm/msm/adreno/adreno_gpu.c
@@ -67,7 +67,6 @@ static int zap_shader_load_mdt(struct msm_gpu *gpu, const char *fwname,
 		return ret;
 
 	mem_phys = r.start;
-	mem_size = resource_size(&r);
 
 	/* Request the MDT file for the firmware */
 	fw = adreno_request_fw(to_adreno_gpu(gpu), fwname);
@@ -80,6 +79,13 @@ static int zap_shader_load_mdt(struct msm_gpu *gpu, const char *fwname,
 	mem_size = qcom_mdt_get_size(fw);
 	if (mem_size < 0) {
 		ret = mem_size;
+		goto out;
+	}
+
+	if (mem_size > resource_size(&r)) {
+		DRM_DEV_ERROR(dev,
+			"memory region is too small to load the MDT\n");
+		ret = -E2BIG;
 		goto out;
 	}
 
