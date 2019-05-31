fde777791eb83f6c5845b81af89de6be60a2b2ce
Heikki Krogerus <heikki.krogerus@linux.intel.com>
2019-05-31 17:15:40 +0300

    device connection: Find connections also by checking the references
    We can also use this API to find named references that the
    device nodes have by using fwnode_property_get_reference_args()
    function.
    
    Signed-off-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Tested-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/base/devcon.c
+++ drivers/base/devcon.c
@@ -38,6 +38,28 @@ fwnode_graph_devcon_match(struct fwnode_handle *fwnode, const char *con_id,
 	return NULL;
 }
 
+static void *
+fwnode_devcon_match(struct fwnode_handle *fwnode, const char *con_id,
+		    void *data, devcon_match_fn_t match)
+{
+	struct device_connection con = { };
+	void *ret;
+	int i;
+
+	for (i = 0; ; i++) {
+		con.fwnode = fwnode_find_reference(fwnode, con_id, i);
+		if (IS_ERR(con.fwnode))
+			break;
+
+		ret = match(&con, -1, data);
+		fwnode_handle_put(con.fwnode);
+		if (ret)
+			return ret;
+	}
+
+	return NULL;
+}
+
 /**
  * device_connection_find_match - Find physical connection to a device
  * @dev: Device with the connection
@@ -63,6 +85,10 @@ void *device_connection_find_match(struct device *dev, const char *con_id,
 
 	if (fwnode) {
 		ret = fwnode_graph_devcon_match(fwnode, con_id, data, match);
+		if (ret)
+			return ret;
+
+		ret = fwnode_devcon_match(fwnode, con_id, data, match);
 		if (ret)
 			return ret;
 	}
