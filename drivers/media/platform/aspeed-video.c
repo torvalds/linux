c8b996ca28d0a8a5dfb423fef6d7ffce7279b19b
Jae Hyun Yoo <jae.hyun.yoo@linux.intel.com>
2019-05-31 18:15:42 -0400

    media: aspeed: remove IRQF_SHARED flag
    Video Engine has a dedicated interrupt line so this driver doesn't
    need to use IRQF_SHARED flag so remove it. Also, it'd be good for
    following what Thomas recommended in the IRQF_ONESHOT support
    patch like below:
    
    "Note that for now IRQF_ONESHOT cannot be used with IRQF_SHARED to
    avoid complex accounting mechanisms."
    
    Signed-off-by: Jae Hyun Yoo <jae.hyun.yoo@linux.intel.com>
    Reviewed-by: Eddie James <eajames@linux.ibm.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/platform/aspeed-video.c
+++ drivers/media/platform/aspeed-video.c
@@ -1601,8 +1601,7 @@ static int aspeed_video_init(struct aspeed_video *video)
 	}
 
 	rc = devm_request_threaded_irq(dev, irq, NULL, aspeed_video_irq,
-				       IRQF_ONESHOT | IRQF_SHARED, DEVICE_NAME,
-				       video);
+				       IRQF_ONESHOT, DEVICE_NAME, video);
 	if (rc < 0) {
 		dev_err(dev, "Unable to request IRQ %d\n", irq);
 		return rc;
