1ae4de23eddef465104277e79fa669bec7b3d288
Uros Bizjak <ubizjak@gmail.com>
2019-06-02 21:11:56 +0200

    KVM: VMX: remove unneeded 'asm volatile ("")' from vmcs_write64
    __vmcs_writel uses volatile asm, so there is no need to insert another
    one between the first and the second call to __vmcs_writel in order
    to prevent unwanted code moves for 32bit targets.
    
    Signed-off-by: Uros Bizjak <ubizjak@gmail.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx/ops.h
+++ arch/x86/kvm/vmx/ops.h
@@ -146,7 +146,6 @@ static __always_inline void vmcs_write64(unsigned long field, u64 value)
 
 	__vmcs_writel(field, value);
 #ifndef CONFIG_X86_64
-	asm volatile ("");
 	__vmcs_writel(field+1, value >> 32);
 #endif
 }
