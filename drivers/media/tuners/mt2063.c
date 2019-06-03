4ba774edce179a885edc23762cd0a44ea2c8107a
Thomas Gleixner <tglx@linutronix.de>
2019-06-03 07:44:54 +0200

    treewide: Replace GPLv2 boilerplate/reference with SPDX - rule 238
    Based on 2 normalized pattern(s):
    
      the original driver s license is gpl as declared with module_license
      this program is free software you can redistribute it and or modify
      it under the terms of the gnu general public license as published by
      the free software foundation under version 2 of the license this
      program is distributed in the hope that it will be useful but
      without any warranty without even the implied warranty of
      merchantability or fitness for a particular purpose see the gnu
      general public license for more details
    
      the original driver s license is gpl as declared with module_license
      [copyright] [c] [2010] [2012] [mauro] [carvalho] [chehab] [driver]
      [modified] [by] [in] [order] [to] [work] [with] [upstream] [drxk]
      [driver] [and] [tons] [of] [bugs] [got] [fixed] [and] [converted]
      [to] [use] [dvb] [usb] [v2] this program is free software you can
      redistribute it and or modify it under the terms of the gnu general
      public license as published by the free software foundation under
      version 2 of the license this program is distributed in the hope
      that it will be useful but without any warranty without even the
      implied warranty of merchantability or fitness for a particular
      purpose see the gnu general public license for more details
    
    extracted by the scancode license scanner the SPDX license identifier
    
      GPL-2.0-only
    
    has been chosen to replace the boilerplate/reference in 2 file(s).
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Allison Randal <allison@lohutok.net>
    Reviewed-by: Enrico Weigelt <info@metux.net>
    Reviewed-by: Alexios Zavras <alexios.zavras@intel.com>
    Cc: linux-spdx@vger.kernel.org
    Link: https://lkml.kernel.org/r/20190602204654.186977917@linutronix.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/media/tuners/mt2063.c
+++ drivers/media/tuners/mt2063.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * Driver for mt2063 Micronas tuner
  *
@@ -7,16 +8,6 @@
  *		Henry Wang <Henry.wang@AzureWave.com>
  * Made publicly available by Terratec, at:
  *	http://linux.terratec.de/files/TERRATEC_H7/20110323_TERRATEC_H7_Linux.tar.gz
- * The original driver's license is GPL, as declared with MODULE_LICENSE()
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation under version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  */
 
 #include <linux/init.h>
