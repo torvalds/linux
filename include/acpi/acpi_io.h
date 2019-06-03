9fe51603d953419b8da74f455269eca8e6b84228
Qian Cai <cai@lca.pw>
2019-06-03 16:28:35 -0400

    ACPI: OSL: Make a W=1 kernel-doc warning go away
    It appears that kernel-doc does not understand the return type *__ref,
    
    drivers/acpi/osl.c:306: warning: cannot understand function prototype:
    'void __iomem *__ref acpi_os_map_iomem(acpi_physical_address phys,
    acpi_size size)
    
    Signed-off-by: Qian Cai <cai@lca.pw>
    [ rjw: Subject ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/acpi_io.h
+++ include/acpi/acpi_io.h
@@ -16,8 +16,8 @@ static inline void __iomem *acpi_os_ioremap(acpi_physical_address phys,
 
 extern bool acpi_permanent_mmap;
 
-void __iomem *__ref
-acpi_os_map_iomem(acpi_physical_address phys, acpi_size size);
+void __iomem __ref
+*acpi_os_map_iomem(acpi_physical_address phys, acpi_size size);
 void __ref acpi_os_unmap_iomem(void __iomem *virt, acpi_size size);
 void __iomem *acpi_os_get_iomem(acpi_physical_address phys, unsigned int size);
 
