f86588302360e01fd0e0a4fc1a8d1b05e85e5173
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-06-03 11:18:16 -0500

    ASoC: SOF: bump to ABI 3.6
    We had a couple of misses with ABI changes, e.g. for Xtensa oops
    information and the integration of sound trigger, before we set-up a
    formal process to track evolutions.
    
    With this patch, the SOF kernel patches are officially aligned with
    the firmware 3.6 level. Changing this level has no impact on existing
    users and is fully backwards-compatible.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/uapi/sound/sof/abi.h
+++ include/uapi/sound/sof/abi.h
@@ -26,7 +26,7 @@
 
 /* SOF ABI version major, minor and patch numbers */
 #define SOF_ABI_MAJOR 3
-#define SOF_ABI_MINOR 5
+#define SOF_ABI_MINOR 6
 #define SOF_ABI_PATCH 0
 
 /* SOF ABI version number. Format within 32bit word is MMmmmppp */
