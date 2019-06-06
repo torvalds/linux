c7a787de7f3f3639b259d13190376af38068515c
YueHaibing <yuehaibing@huawei.com>
2019-06-06 04:28:40 +0000

    phy: usb: phy-brcm-usb: Fix platform_no_drv_owner.cocci warnings
    Remove .owner field if calls are used which set it automatically
    Generated by: scripts/coccinelle/api/platform_no_drv_owner.cocci
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- drivers/phy/broadcom/phy-brcm-usb.c
+++ drivers/phy/broadcom/phy-brcm-usb.c
@@ -443,7 +443,6 @@ static struct platform_driver brcm_usb_driver = {
 	.probe		= brcm_usb_phy_probe,
 	.driver		= {
 		.name	= "brcmstb-usb-phy",
-		.owner	= THIS_MODULE,
 		.pm = &brcm_usb_phy_pm_ops,
 		.of_match_table = brcm_usb_dt_ids,
 	},
