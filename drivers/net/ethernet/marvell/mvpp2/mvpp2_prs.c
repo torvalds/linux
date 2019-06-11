97605ba68790ff94358a50bc33f27b85431e5e67
Maxime Chevallier <maxime.chevallier@bootlin.com>
2019-06-11 11:51:43 +0200

    net: mvpp2: prs: Use the correct helpers when removing all VID filters
    [ Upstream commit 6b7a3430c163455cf8a514d636bda52b04654972 ]
    
    When removing all VID filters, the mvpp2_prs_vid_entry_remove would be
    called with the TCAM id incorrectly used as a VID, causing the wrong
    TCAM entries to be invalidated.
    
    Fix this by directly invalidating entries in the VID range.
    
    Fixes: 56beda3db602 ("net: mvpp2: Add hardware offloading for VLAN filtering")
    Suggested-by: Yuri Chipchev <yuric@marvell.com>
    Signed-off-by: Maxime Chevallier <maxime.chevallier@bootlin.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/marvell/mvpp2/mvpp2_prs.c
+++ drivers/net/ethernet/marvell/mvpp2/mvpp2_prs.c
@@ -2025,8 +2025,10 @@ void mvpp2_prs_vid_remove_all(struct mvpp2_port *port)
 
 	for (tid = MVPP2_PRS_VID_PORT_FIRST(port->id);
 	     tid <= MVPP2_PRS_VID_PORT_LAST(port->id); tid++) {
-		if (priv->prs_shadow[tid].valid)
-			mvpp2_prs_vid_entry_remove(port, tid);
+		if (priv->prs_shadow[tid].valid) {
+			mvpp2_prs_hw_inv(priv, tid);
+			priv->prs_shadow[tid].valid = false;
+		}
 	}
 }
 
