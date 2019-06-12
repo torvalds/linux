e2e0e09758a6f7597de0f9b819647addfb71b6bd
Gen Zhang <blackgod016574@gmail.com>
2019-06-12 21:28:21 +0800

    selinux: fix a missing-check bug in selinux_add_mnt_opt( )
    In selinux_add_mnt_opt(), 'val' is allocated by kmemdup_nul(). It returns
    NULL when fails. So 'val' should be checked. And 'mnt_opts' should be
    freed when error.
    
    Signed-off-by: Gen Zhang <blackgod016574@gmail.com>
    Fixes: 757cbe597fe8 ("LSM: new method: ->sb_add_mnt_opt()")
    Cc: <stable@vger.kernel.org>
    [PM: fixed some indenting problems]
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -1052,15 +1052,24 @@ static int selinux_add_mnt_opt(const char *option, const char *val, int len,
 	if (token == Opt_error)
 		return -EINVAL;
 
-	if (token != Opt_seclabel)
+	if (token != Opt_seclabel) {
 		val = kmemdup_nul(val, len, GFP_KERNEL);
+		if (!val) {
+			rc = -ENOMEM;
+			goto free_opt;
+		}
+	}
 	rc = selinux_add_opt(token, val, mnt_opts);
 	if (unlikely(rc)) {
 		kfree(val);
-		if (*mnt_opts) {
-			selinux_free_mnt_opts(*mnt_opts);
-			*mnt_opts = NULL;
-		}
+		goto free_opt;
+	}
+	return rc;
+
+free_opt:
+	if (*mnt_opts) {
+		selinux_free_mnt_opts(*mnt_opts);
+		*mnt_opts = NULL;
 	}
 	return rc;
 }
