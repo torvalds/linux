4cee883e97b6095e23f5d72543da93e1506ff8c2
Dragos Tarcatu <dragos_tarcatu@mentor.com>
2019-06-12 11:57:01 -0500

    ASOC: SOF: ipc: prevent logging trace messages
    If the firmware is set for verbose tracing, the kernel log is flooded
    with ipc rx/ipc rx done messages. Prevent logging those unless the
    verbose IPC debugging config option is set.
    
    Signed-off-by: Dragos Tarcatu <dragos_tarcatu@mentor.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sof/ipc.c
+++ sound/soc/sof/ipc.c
@@ -196,7 +196,8 @@ static void ipc_log_header(struct device *dev, u8 *text, u32 cmd)
 #else
 static inline void ipc_log_header(struct device *dev, u8 *text, u32 cmd)
 {
-	dev_dbg(dev, "%s: 0x%x\n", text, cmd);
+	if ((cmd & SOF_GLB_TYPE_MASK) != SOF_IPC_GLB_TRACE_MSG)
+		dev_dbg(dev, "%s: 0x%x\n", text, cmd);
 }
 #endif
 
