7623ae793c28cc0928c5d1292542dbb92fc2e9e2
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2019-06-12 12:23:33 -0500

    ASoC: SOF: Intel: hda: save handle to sdev in sof_intel_hda_stream
    Add a snd_sof_dev member to sof_intel_hda_stream. This will be
    used to access the snd_sof_dev during link hw_params callback.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sof/intel/hda.h
+++ sound/soc/sof/intel/hda.h
@@ -409,6 +409,7 @@ static inline struct hda_bus *sof_to_hbus(struct snd_sof_dev *s)
 }
 
 struct sof_intel_hda_stream {
+	struct snd_sof_dev *sdev;
 	struct hdac_ext_stream hda_stream;
 	struct sof_intel_stream stream;
 	int hw_params_upon_resume; /* set up hw_params upon resume */
