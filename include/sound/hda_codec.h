3e985effb25edcf500a58f41be71cc049b1b837f
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2019-06-12 11:36:54 -0500

    ALSA: hda: remove an unused field from struct hda_codec
    The .jacks field in struct hda_codec is unused and seems to be a
    duplicate of .jacktbl, remove it.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/sound/hda_codec.h
+++ include/sound/hda_codec.h
@@ -281,9 +281,6 @@ struct hda_codec {
 	unsigned long jackpoll_interval; /* In jiffies. Zero means no poll, rely on unsol events */
 	struct delayed_work jackpoll_work;
 
-	/* jack detection */
-	struct snd_array jacks;
-
 	int depop_delay; /* depop delay in ms, -1 for default delay time */
 
 	/* fix-up list */
