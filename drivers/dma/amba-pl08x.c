718745f87f95d0a8c3cdeb52abd20bfc2b063d12
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2019-06-12 14:25:52 +0200

    dmaengine: amba-pl08x: no need to cast away call to debugfs_create_file()
    No need to check the return value of debugfs_create_file(), so no need
    to provide a fake "cast away" of the return value either.
    
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Vinod Koul <vkoul@kernel.org>
    Cc: dmaengine@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/amba-pl08x.c
+++ drivers/dma/amba-pl08x.c
@@ -2520,9 +2520,8 @@ DEFINE_SHOW_ATTRIBUTE(pl08x_debugfs);
 static void init_pl08x_debugfs(struct pl08x_driver_data *pl08x)
 {
 	/* Expose a simple debugfs interface to view all clocks */
-	(void) debugfs_create_file(dev_name(&pl08x->adev->dev),
-			S_IFREG | S_IRUGO, NULL, pl08x,
-			&pl08x_debugfs_fops);
+	debugfs_create_file(dev_name(&pl08x->adev->dev), S_IFREG | S_IRUGO,
+			    NULL, pl08x, &pl08x_debugfs_fops);
 }
 
 #else
