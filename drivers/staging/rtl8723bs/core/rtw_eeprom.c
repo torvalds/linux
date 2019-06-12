65699f341f1575a60670164d3a910505bfaa1b5f
Nishka Dasgupta <nishkadg.linux@gmail.com>
2019-06-12 23:34:31 +0530

    staging: rtl8723bs: core: Remove eeprom_write16()
    Remove unused function eeprom_write16.
    Issue found with Coccinelle.
    
    Signed-off-by: Nishka Dasgupta <nishkadg.linux@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/core/rtw_eeprom.c
+++ drivers/staging/rtl8723bs/core/rtw_eeprom.c
@@ -166,68 +166,6 @@ out:
 _func_exit_;
 }
 
-void eeprom_write16(_adapter *padapter, u16 reg, u16 data)
-{
-	u8 x;
-
-_func_enter_;
-
-	x = rtw_read8(padapter, EE_9346CR);
-
-	x &= ~(_EEDI | _EEDO | _EESK | _EEM0);
-	x |= _EEM1 | _EECS;
-	rtw_write8(padapter, EE_9346CR, x);
-
-	shift_out_bits(padapter, EEPROM_EWEN_OPCODE, 5);
-
-	if (padapter->EepromAddressSize == 8)	/*CF+ and SDIO*/
-		shift_out_bits(padapter, 0, 6);
-	else									/*USB*/
-		shift_out_bits(padapter, 0, 4);
-
-	standby(padapter);
-
-/* Commented out by rcnjko, 2004.0
-*	 Erase this particular word.  Write the erase opcode and register
-*	 number in that order. The opcode is 3bits in length; reg is 6 bits long.
-*	shift_out_bits(Adapter, EEPROM_ERASE_OPCODE, 3);
-*	shift_out_bits(Adapter, reg, Adapter->EepromAddressSize);
-*
-*	if (wait_eeprom_cmd_done(Adapter ) == false)
-*	{
-*		return;
-*	}
-*/
-
-	standby(padapter);
-
-	/* write the new word to the EEPROM*/
-
-	/* send the write opcode the EEPORM*/
-	shift_out_bits(padapter, EEPROM_WRITE_OPCODE, 3);
-
-	/* select which word in the EEPROM that we are writing to.*/
-	shift_out_bits(padapter, reg, padapter->EepromAddressSize);
-
-	/* write the data to the selected EEPROM word.*/
-	shift_out_bits(padapter, data, 16);
-
-	if (wait_eeprom_cmd_done(padapter) == false) {
-
-		goto exit;
-	}
-
-	standby(padapter);
-
-	shift_out_bits(padapter, EEPROM_EWDS_OPCODE, 5);
-	shift_out_bits(padapter, reg, 4);
-
-	eeprom_clean(padapter);
-exit:
-_func_exit_;
-	return;
-}
-
 u16 eeprom_read16(_adapter *padapter, u16 reg) /*ReadEEprom*/
 {
 
