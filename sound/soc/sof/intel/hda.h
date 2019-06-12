13063a2ccf79d232889491105a7bd9d552e417fc
Zhu Yingjiang <yingjiang.zhu@linux.intel.com>
2019-06-12 12:23:43 -0500

    ASoC: SOF: Intel: hda: add function for hda stop chip
    Add common hda_dsp_ctrl_stop_chip() function to stop controller with
    the same function handling both HDA and non-HDA cases. This function
    disables IRQs and clears status masks. When CONFIG_SND_SOC_SOF_HDA
    is defined, also disables the CORB/RIRB, and stops i/o.
    
    Signed-off-by: Zhu Yingjiang <yingjiang.zhu@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sof/intel/hda.h
+++ sound/soc/sof/intel/hda.h
@@ -538,7 +538,7 @@ int hda_dsp_ctrl_link_reset(struct snd_sof_dev *sdev, bool reset);
 void hda_dsp_ctrl_misc_clock_gating(struct snd_sof_dev *sdev, bool enable);
 int hda_dsp_ctrl_clock_power_gating(struct snd_sof_dev *sdev, bool enable);
 int hda_dsp_ctrl_init_chip(struct snd_sof_dev *sdev, bool full_reset);
-
+void hda_dsp_ctrl_stop_chip(struct snd_sof_dev *sdev);
 /*
  * HDA bus operations.
  */
