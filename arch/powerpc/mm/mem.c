9739ab7eda459f0669ec9807e0d9be5020bab88c
Christoph Hellwig <hch@lst.de>
2019-06-13 10:24:46 +0200

    powerpc: enable a 30-bit ZONE_DMA for 32-bit pmac
    With the strict dma mask checking introduced with the switch to
    the generic DMA direct code common wifi chips on 32-bit powerbooks
    stopped working.  Add a 30-bit ZONE_DMA to the 32-bit pmac builds
    to allow them to reliably allocate dma coherent memory.
    
    Fixes: 65a21b71f948 ("powerpc/dma: remove dma_nommu_dma_supported")
    Reported-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Tested-by: Larry Finger <Larry.Finger@lwfinger.net>
    Acked-by: Larry Finger <Larry.Finger@lwfinger.net>
    Tested-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/mm/mem.c
+++ arch/powerpc/mm/mem.c
@@ -253,7 +253,8 @@ void __init paging_init(void)
 	       (long int)((top_of_ram - total_ram) >> 20));
 
 #ifdef CONFIG_ZONE_DMA
-	max_zone_pfns[ZONE_DMA]	= min(max_low_pfn, 0x7fffffffUL >> PAGE_SHIFT);
+	max_zone_pfns[ZONE_DMA]	= min(max_low_pfn,
+			((1UL << ARCH_ZONE_DMA_BITS) - 1) >> PAGE_SHIFT);
 #endif
 	max_zone_pfns[ZONE_NORMAL] = max_low_pfn;
 #ifdef CONFIG_HIGHMEM
