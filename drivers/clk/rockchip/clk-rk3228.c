794e94ca83450c436313df18291e139cf5f9121f
Heiko Stuebner <heiko@sntech.de>
2019-06-14 10:59:48 +0200

    clk: rockchip: export HDMIPHY clock on rk3228
    Export the hdmiphy clock mux via the newly added clock-id.
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Tested-by: Justin Swartz <justin.swartz@risingedge.co.za>

--- drivers/clk/rockchip/clk-rk3228.c
+++ drivers/clk/rockchip/clk-rk3228.c
@@ -256,7 +256,7 @@ static struct rockchip_clk_branch rk3228_clk_branches[] __initdata = {
 			RK2928_CLKGATE_CON(4), 0, GFLAGS),
 
 	/* PD_MISC */
-	MUX(0, "hdmiphy", mux_hdmiphy_p, CLK_SET_RATE_PARENT,
+	MUX(SCLK_HDMI_PHY, "hdmiphy", mux_hdmiphy_p, CLK_SET_RATE_PARENT,
 			RK2928_MISC_CON, 13, 1, MFLAGS),
 	MUX(0, "usb480m_phy", mux_usb480m_phy_p, CLK_SET_RATE_PARENT,
 			RK2928_MISC_CON, 14, 1, MFLAGS),
