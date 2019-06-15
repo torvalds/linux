a78cf9657ba5426f54aa93a067c10d097944c082
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2019-06-15 10:23:57 +1000

    PCI/ACPI: Evaluate PCI Boot Configuration _DSM
    Evaluate _DSM Function #5, the "PCI Boot Configuration" function.  If the
    result is 0, the OS should preserve any resource assignments made by the
    firmware.
    
    Link: https://lore.kernel.org/r/20190615002359.29577-2-benh@kernel.crashing.org
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    [bhelgaas: commit log]
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -505,6 +505,8 @@ struct pci_host_bridge {
 	unsigned int	native_shpc_hotplug:1;	/* OS may use SHPC hotplug */
 	unsigned int	native_pme:1;		/* OS may use PCIe PME */
 	unsigned int	native_ltr:1;		/* OS may use PCIe LTR */
+	unsigned int	preserve_config:1;	/* Preserve FW resource setup */
+
 	/* Resource alignment requirements */
 	resource_size_t (*align_resource)(struct pci_dev *dev,
 			const struct resource *res,
