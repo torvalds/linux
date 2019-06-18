35b52b3315a21d3314f859814717ca3583660f73
kbuild test robot <lkp@intel.com>
2019-06-18 10:32:31 +0200

    platform/chrome: cros_ec_debugfs: cros_ec_uptime_fops can be static
    Fixes: 909447f683b3 ("platform/chrome: cros_ec_debugfs: Add debugfs entry to retrieve EC uptime")
    Signed-off-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

--- drivers/platform/chrome/cros_ec_debugfs.c
+++ drivers/platform/chrome/cros_ec_debugfs.c
@@ -286,7 +286,7 @@ static const struct file_operations cros_ec_pdinfo_fops = {
 	.llseek = default_llseek,
 };
 
-const struct file_operations cros_ec_uptime_fops = {
+static const struct file_operations cros_ec_uptime_fops = {
 	.owner = THIS_MODULE,
 	.open = simple_open,
 	.read = cros_ec_uptime_read,
