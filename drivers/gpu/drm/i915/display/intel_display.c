4c888e7bd26f58deb27c2e6ddc90000b89ee9393
Ville Syrjälä <ville.syrjala@linux.intel.com>
2019-06-19 20:08:39 +0300

    drm/i915: Fix various tracepoints for gen2
    Gen2 doesn't have a frame counter and apparently we no longer provide
    a fake .get_vblank_counter() hook for it. That means all tracepoints
    calling that hook will oops. Update the tracepoints to use
    intel_crtc_get_vblank_counter() which will gracefully fall back to
    using the software counter. This is actually a better approach since
    we now get (hopefully accurate) frame numbers in the traces.
    
    This also gets rid of the raw driver->get_vblank_counter() calls, which
    we need to do in order to switch to the per-crtc vblank vfuncs.
    
    v2: Deal with new tracepoints
    v3: Use a distinct variable name for the internal crtc iterator (Chris)
    
    Cc: Shawn Guo <shawn.guo@linaro.org>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Fixes: 967dd4841787 ("drm: remove drm_vblank_no_hw_counter assignment from driver code")
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190619170842.20579-2-ville.syrjala@linux.intel.com

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -1839,7 +1839,7 @@ static void intel_enable_pipe(const struct intel_crtc_state *new_crtc_state)
 		/* FIXME: assert CPU port conditions for SNB+ */
 	}
 
-	trace_intel_pipe_enable(dev_priv, pipe);
+	trace_intel_pipe_enable(crtc);
 
 	reg = PIPECONF(cpu_transcoder);
 	val = I915_READ(reg);
@@ -1880,7 +1880,7 @@ static void intel_disable_pipe(const struct intel_crtc_state *old_crtc_state)
 	 */
 	assert_planes_disabled(crtc);
 
-	trace_intel_pipe_disable(dev_priv, pipe);
+	trace_intel_pipe_disable(crtc);
 
 	reg = PIPECONF(cpu_transcoder);
 	val = I915_READ(reg);
