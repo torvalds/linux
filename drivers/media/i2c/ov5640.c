2f1fc5efe4a59117001544830c183da1765af86f
Luca Weiss <luca@z3ntu.xyz>
2019-06-21 14:20:25 -0300

    media: ov5640: Add support for flash and lens devices
    Parse async sub-devices related to the sensor by switching the async
    sub-device registration function.
    
    Signed-off-by: Luca Weiss <luca@z3ntu.xyz>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/i2c/ov5640.c
+++ drivers/media/i2c/ov5640.c
@@ -3054,7 +3054,7 @@ static int ov5640_probe(struct i2c_client *client)
 	if (ret)
 		goto entity_cleanup;
 
-	ret = v4l2_async_register_subdev(&sensor->sd);
+	ret = v4l2_async_register_subdev_sensor_common(&sensor->sd);
 	if (ret)
 		goto free_ctrls;
 
