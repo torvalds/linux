4f182835508c2d3dc6f5cc0e56b65b4c1ca325f8
Nikhil Badola <nikhil.badola@freescale.com>
2019-06-24 15:22:15 +0800

    usb: fsl: Set USB_EN bit to select ULPI phy
    Set USB_EN bit to select ULPI phy for USB controller version 2.5
    
    Signed-off-by: Nikhil Badola <nikhil.badola@freescale.com>
    Signed-off-by: Yinbo Zhu <yinbo.zhu@nxp.com>
    Link: https://lore.kernel.org/r/20190624072219.15258-1-yinbo.zhu@nxp.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/ehci-fsl.c
+++ drivers/usb/host/ehci-fsl.c
@@ -122,6 +122,12 @@ static int fsl_ehci_drv_probe(struct platform_device *pdev)
 		tmp |= 0x4;
 		iowrite32be(tmp, hcd->regs + FSL_SOC_USB_CTRL);
 	}
+
+	/* Set USB_EN bit to select ULPI phy for USB controller version 2.5 */
+	if (pdata->controller_ver == FSL_USB_VER_2_5 &&
+	    pdata->phy_mode == FSL_USB2_PHY_ULPI)
+		iowrite32be(USB_CTRL_USB_EN, hcd->regs + FSL_SOC_USB_CTRL);
+
 	/*
 	 * Enable UTMI phy and program PTS field in UTMI mode before asserting
 	 * controller reset for USB Controller version 2.5
