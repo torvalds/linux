02511a3fb5cfa6259b394eb84ba85b182b9a77f4
Andrea Arcangeli <aarcange@redhat.com>
2019-06-25 10:17:27 -0400

    powerpc: fix off by one in max_zone_pfn initialization for ZONE_DMA
    [ Upstream commit 03800e0526ee25ed7c843ca1e57b69ac2a5af642 ]
    
    25078dc1f74be16b858e914f52cc8f4d03c2271a first introduced an off by
    one error in the ZONE_DMA initialization of PPC_BOOK3E_64=y and since
    9739ab7eda459f0669ec9807e0d9be5020bab88c the off by one applies to
    PPC32=y too. This simply corrects the off by one and should resolve
    crashes like below:
    
    [   65.179101] page 0x7fff outside node 0 zone DMA [ 0x0 - 0x7fff ]
    
    Unfortunately in various MM places "max" means a non inclusive end of
    range. free_area_init_nodes max_zone_pfn parameter is one case and
    MAX_ORDER is another one (unrelated) that comes by memory.
    
    Reported-by: Zorro Lang <zlang@redhat.com>
    Fixes: 25078dc1f74b ("powerpc: use mm zones more sensibly")
    Fixes: 9739ab7eda45 ("powerpc: enable a 30-bit ZONE_DMA for 32-bit pmac")
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20190625141727.2883-1-aarcange@redhat.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/powerpc/mm/mem.c
+++ arch/powerpc/mm/mem.c
@@ -249,7 +249,7 @@ void __init paging_init(void)
 
 #ifdef CONFIG_ZONE_DMA
 	max_zone_pfns[ZONE_DMA]	= min(max_low_pfn,
-			((1UL << ARCH_ZONE_DMA_BITS) - 1) >> PAGE_SHIFT);
+				      1UL << (ARCH_ZONE_DMA_BITS - PAGE_SHIFT));
 #endif
 	max_zone_pfns[ZONE_NORMAL] = max_low_pfn;
 #ifdef CONFIG_HIGHMEM
