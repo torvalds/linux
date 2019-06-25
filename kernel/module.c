93651f80dcb616b8c9115cdafc8e57a781af22d0
Yang Yingliang <yangyingliang@huawei.com>
2019-06-25 17:40:28 +0800

    modules: fix compile error if don't have strict module rwx
    If CONFIG_ARCH_HAS_STRICT_MODULE_RWX is not defined,
    we need stub for module_enable_nx() and module_enable_x().
    
    If CONFIG_ARCH_HAS_STRICT_MODULE_RWX is defined, but
    CONFIG_STRICT_MODULE_RWX is disabled, we need stub for
    module_enable_nx.
    
    Move frob_text() outside of the CONFIG_STRICT_MODULE_RWX,
    because it is needed anyway.
    
    Fixes: 2eef1399a866 ("modules: fix BUG when load module with rodata=n")
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Jessica Yu <jeyu@kernel.org>

--- kernel/module.c
+++ kernel/module.c
@@ -1898,7 +1898,7 @@ static void mod_sysfs_teardown(struct module *mod)
 	mod_sysfs_fini(mod);
 }
 
-#ifdef CONFIG_STRICT_MODULE_RWX
+#ifdef CONFIG_ARCH_HAS_STRICT_MODULE_RWX
 /*
  * LKM RO/NX protection: protect module's text/ro-data
  * from modification and any data from execution.
@@ -1921,6 +1921,7 @@ static void frob_text(const struct module_layout *layout,
 		   layout->text_size >> PAGE_SHIFT);
 }
 
+#ifdef CONFIG_STRICT_MODULE_RWX
 static void frob_rodata(const struct module_layout *layout,
 			int (*set_memory)(unsigned long start, int num_pages))
 {
@@ -2033,15 +2034,19 @@ void set_all_modules_text_ro(void)
 	}
 	mutex_unlock(&module_mutex);
 }
-#else
+#else /* !CONFIG_STRICT_MODULE_RWX */
 static void module_enable_nx(const struct module *mod) { }
-#endif
-
+#endif /*  CONFIG_STRICT_MODULE_RWX */
 static void module_enable_x(const struct module *mod)
 {
 	frob_text(&mod->core_layout, set_memory_x);
 	frob_text(&mod->init_layout, set_memory_x);
 }
+#else /* !CONFIG_ARCH_HAS_STRICT_MODULE_RWX */
+static void module_enable_nx(const struct module *mod) { }
+static void module_enable_x(const struct module *mod) { }
+#endif /* CONFIG_ARCH_HAS_STRICT_MODULE_RWX */
+
 
 #ifdef CONFIG_LIVEPATCH
 /*
