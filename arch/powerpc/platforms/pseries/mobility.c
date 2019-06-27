5d8b1f9dea17b4bf5e5f088f39eeab32c7e487be
Nathan Lynch <nathanl@linux.ibm.com>
2019-06-27 00:30:44 -0500

    powerpc/pseries/mobility: Add pr_debug() for device tree changes
    When investigating issues with partition migration or resource
    reassignments it is helpful to have a log of which nodes and
    properties in the device tree have changed. Use pr_debug() so it's
    easy to enable these at runtime with the dynamic debug facility.
    
    Signed-off-by: Nathan Lynch <nathanl@linux.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20190627053044.9238-3-nathanl@linux.ibm.com

--- arch/powerpc/platforms/pseries/mobility.c
+++ arch/powerpc/platforms/pseries/mobility.c
@@ -67,6 +67,8 @@ static int delete_dt_node(__be32 phandle)
 	if (!dn)
 		return -ENOENT;
 
+	pr_debug("removing node %pOFfp\n", dn);
+
 	dlpar_detach_node(dn);
 	of_node_put(dn);
 	return 0;
@@ -125,6 +127,7 @@ static int update_dt_property(struct device_node *dn, struct property **prop,
 	}
 
 	if (!more) {
+		pr_debug("updating node %pOF property %s\n", dn, name);
 		of_update_property(dn, new_prop);
 		*prop = NULL;
 	}
@@ -242,6 +245,8 @@ static int add_dt_node(__be32 parent_phandle, __be32 drc_index)
 	rc = dlpar_attach_node(dn, parent_dn);
 	if (rc)
 		dlpar_free_cc_nodes(dn);
+
+	pr_debug("added node %pOFfp\n", dn);
 
 	of_node_put(parent_dn);
 	return rc;
