5f0e6ce18e65c463c52d00997397967ea809688b
Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
2019-06-28 12:30:06 +0200

    video: fbdev: intelfb: return -ENOMEM on framebuffer_alloc() failure
    Fix error code from -ENODEV to -ENOMEM.
    
    Cc: Maik Broemme <mbroemme@libmpq.org>
    Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>

--- drivers/video/fbdev/intelfb/intelfbdrv.c
+++ drivers/video/fbdev/intelfb/intelfbdrv.c
@@ -493,7 +493,7 @@ static int intelfb_pci_register(struct pci_dev *pdev,
 	info = framebuffer_alloc(sizeof(struct intelfb_info), &pdev->dev);
 	if (!info) {
 		ERR_MSG("Could not allocate memory for intelfb_info.\n");
-		return -ENODEV;
+		return -ENOMEM;
 	}
 	if (fb_alloc_cmap(&info->cmap, 256, 1) < 0) {
 		ERR_MSG("Could not allocate cmap for intelfb_info.\n");
