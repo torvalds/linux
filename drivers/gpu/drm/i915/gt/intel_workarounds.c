2c903da50f5a9522b134e488bd0f92646c46f3c0
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2019-06-28 15:07:19 +0300

    drm/i915: whitelist PS_(DEPTH|INVOCATION)_COUNT
    CFL:C0+ changed the status of those registers which are now
    blacklisted by default.
    
    This is breaking a number of CTS tests on GL & Vulkan :
    
      KHR-GL45.pipeline_statistics_query_tests_ARB.functional_fragment_shader_invocations (GL)
    
      dEQP-VK.query_pool.statistics_query.fragment_shader_invocations.* (Vulkan)
    
    v2: Only use one whitelist entry (Lionel)
    
    Bspec: 14091
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Cc: stable@vger.kernel.org
    Acked-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190628120720.21682-3-lionel.g.landwerlin@intel.com

--- drivers/gpu/drm/i915/gt/intel_workarounds.c
+++ drivers/gpu/drm/i915/gt/intel_workarounds.c
@@ -1092,10 +1092,25 @@ static void glk_whitelist_build(struct intel_engine_cs *engine)
 
 static void cfl_whitelist_build(struct intel_engine_cs *engine)
 {
+	struct i915_wa_list *w = &engine->whitelist;
+
 	if (engine->class != RENDER_CLASS)
 		return;
 
-	gen9_whitelist_build(&engine->whitelist);
+	gen9_whitelist_build(w);
+
+	/*
+	 * WaAllowPMDepthAndInvocationCountAccessFromUMD:cfl,whl,cml,aml
+	 *
+	 * This covers 4 register which are next to one another :
+	 *   - PS_INVOCATION_COUNT
+	 *   - PS_INVOCATION_COUNT_UDW
+	 *   - PS_DEPTH_COUNT
+	 *   - PS_DEPTH_COUNT_UDW
+	 */
+	whitelist_reg_ext(w, PS_INVOCATION_COUNT,
+			  RING_FORCE_TO_NONPRIV_RD |
+			  RING_FORCE_TO_NONPRIV_RANGE_4);
 }
 
 static void cnl_whitelist_build(struct intel_engine_cs *engine)
