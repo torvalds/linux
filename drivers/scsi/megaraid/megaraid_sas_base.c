1175b88452cad208894412b955ee698934968aed
Shivasharan S <shivasharan.srikanteshwara@broadcom.com>
2019-06-28 02:50:39 -0700

    scsi: megaraid_sas: Enable msix_load_balance for Invader and later controllers
    Load balancing IO completions across all available MSI-X vectors should be
    enabled for Invader and later generation controllers only.  This needs to
    be disabled for older controllers.  Add an adapter type check before
    setting msix_load_balance flag.
    
    Fixes: 1d15d9098ad1 ("scsi: megaraid_sas: Load balance completions across all MSI-X")
    Signed-off-by: Shivasharan S <shivasharan.srikanteshwara@broadcom.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/megaraid/megaraid_sas_base.c
+++ drivers/scsi/megaraid/megaraid_sas_base.c
@@ -5945,7 +5945,8 @@ static int megasas_init_fw(struct megasas_instance *instance)
 					instance->is_rdpq = (scratch_pad_1 & MR_RDPQ_MODE_OFFSET) ?
 								1 : 0;
 
-				if (!instance->msix_combined) {
+				if (instance->adapter_type >= INVADER_SERIES &&
+				    !instance->msix_combined) {
 					instance->msix_load_balance = true;
 					instance->smp_affinity_enable = false;
 				}
