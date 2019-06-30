e9f2a760b158551bfbef6db31d2cae45ab8072e5
Paolo Bonzini <pbonzini@redhat.com>
2019-06-30 08:36:21 -0400

    KVM: x86: change kvm_mmu_page_get_gfn BUG_ON to WARN_ON
    Note that in such a case it is quite likely that KVM will BUG_ON
    in __pte_list_remove when the VM is closed.  However, there is no
    immediate risk of memory corruption in the host so a WARN_ON is
    enough and it lets you gather traces for debugging.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/mmu.c
+++ arch/x86/kvm/mmu.c
@@ -1098,10 +1098,16 @@ static gfn_t kvm_mmu_page_get_gfn(struct kvm_mmu_page *sp, int index)
 
 static void kvm_mmu_page_set_gfn(struct kvm_mmu_page *sp, int index, gfn_t gfn)
 {
-	if (sp->role.direct)
-		BUG_ON(gfn != kvm_mmu_page_get_gfn(sp, index));
-	else
+	if (!sp->role.direct) {
 		sp->gfns[index] = gfn;
+		return;
+	}
+
+	if (WARN_ON(gfn != kvm_mmu_page_get_gfn(sp, index)))
+		pr_err_ratelimited("gfn mismatch under direct page %llx "
+				   "(expected %llx, got %llx)\n",
+				   sp->gfn,
+				   kvm_mmu_page_get_gfn(sp, index), gfn);
 }
 
 /*
