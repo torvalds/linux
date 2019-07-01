85a0cc34a11668e4bff3132582e794da132d4aa0
Clement Leger <cleger@kalray.eu>
2019-07-01 09:02:45 +0200

    remoteproc: copy parent dma_pfn_offset for vdev
    [ Upstream commit 72f64cabc4bd6985c7355f5547bd3637c82762ac ]
    
    When preparing the subdevice for the vdev, also copy dma_pfn_offset
    since this is used for sub device dma allocations. Without that, there
    is incoherency between the parent dma settings and the childs one,
    potentially leading to dma_alloc_coherent failure (due to phys_to_dma
    using dma_pfn_offset for translation).
    
    Fixes: 086d08725d34 ("remoteproc: create vdev subdevice with specific dma memory pool")
    Signed-off-by: Clement Leger <cleger@kalray.eu>
    Acked-by: Loic Pallardy <loic.pallardy@st.com>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -512,6 +512,7 @@ static int rproc_handle_vdev(struct rproc *rproc, struct fw_rsc_vdev *rsc,
 	/* Initialise vdev subdevice */
 	snprintf(name, sizeof(name), "vdev%dbuffer", rvdev->index);
 	rvdev->dev.parent = rproc->dev.parent;
+	rvdev->dev.dma_pfn_offset = rproc->dev.parent->dma_pfn_offset;
 	rvdev->dev.release = rproc_rvdev_release;
 	dev_set_name(&rvdev->dev, "%s#%s", dev_name(rvdev->dev.parent), name);
 	dev_set_drvdata(&rvdev->dev, rvdev);
