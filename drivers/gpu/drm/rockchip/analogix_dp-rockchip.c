3bbb6e5527d48834ed77d1083a6a7800ecf342d2
Sandy Huang <hjc@rock-chips.com>
2019-07-02 11:36:58 +0800

    drm/rockchip: analogix_dp: init encoder port
    Change-Id: Ie978e48d427162adb17c674097ed37140a497769
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>

--- drivers/gpu/drm/rockchip/analogix_dp-rockchip.c
+++ drivers/gpu/drm/rockchip/analogix_dp-rockchip.c
@@ -378,6 +378,8 @@ static int rockchip_dp_drm_create_encoder(struct rockchip_dp_device *dp)
 	struct device *dev = dp->dev;
 	int ret;
 
+	encoder->port = dev->of_node;
+
 	encoder->possible_crtcs = drm_of_find_possible_crtcs(drm_dev,
 							     dev->of_node);
 	DRM_DEBUG_KMS("possible_crtcs = 0x%x\n", encoder->possible_crtcs);
