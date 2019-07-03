7641c1bafacdfcf0cb5e7da59238fbc501da92d6
Fuqian Huang <huangfq.daxian@gmail.com>
2019-07-04 00:28:21 +0800

    macintosh: Use kmemdup rather than duplicating its implementation
    kmemdup is introduced to duplicate a region of memory in a neat way.
    Rather than kmalloc/kzalloc + memcpy, which the programmer needs to
    write the size twice (sometimes lead to mistakes), kmemdup improves
    readability, leads to smaller code and also reduce the chances of mistakes.
    Suggestion to use kmemdup rather than using kmalloc/kzalloc + memcpy.
    
    Signed-off-by: Fuqian Huang <huangfq.daxian@gmail.com>
    [chleroy: Fixed parenthesis alignment]
    Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20190703162821.32322-1-huangfq.daxian@gmail.com

--- drivers/macintosh/adbhid.c
+++ drivers/macintosh/adbhid.c
@@ -789,15 +789,14 @@ adbhid_input_register(int id, int default_id, int original_handler_id,
 
 	switch (default_id) {
 	case ADB_KEYBOARD:
-		hid->keycode = kmalloc(sizeof(adb_to_linux_keycodes), GFP_KERNEL);
+		hid->keycode = kmemdup(adb_to_linux_keycodes,
+				       sizeof(adb_to_linux_keycodes), GFP_KERNEL);
 		if (!hid->keycode) {
 			err = -ENOMEM;
 			goto fail;
 		}
 
 		sprintf(hid->name, "ADB keyboard");
-
-		memcpy(hid->keycode, adb_to_linux_keycodes, sizeof(adb_to_linux_keycodes));
 
 		switch (original_handler_id) {
 		default:
