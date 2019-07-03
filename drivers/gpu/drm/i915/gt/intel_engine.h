21de5a9e34fb0f3efa31df5712cedc0ae97f7b4d
Chris Wilson <chris@chris-wilson.co.uk>
2019-07-03 16:52:25 +0100

    drm/i915/gt: Ignore forcewake acquisition for posting_reads
    We don't care about the result of the read, so it may be garbage, we
    only care that the mmio is flushed. As such, we can forgo using an
    individual forcewake and lock around any posting-read for an engine.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190703155225.9501-4-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/gt/intel_engine.h
+++ drivers/gpu/drm/i915/gt/intel_engine.h
@@ -51,7 +51,7 @@ struct drm_printer;
 #define ENGINE_READ16(...)	__ENGINE_READ_OP(read16, __VA_ARGS__)
 #define ENGINE_READ(...)	__ENGINE_READ_OP(read, __VA_ARGS__)
 #define ENGINE_READ_FW(...)	__ENGINE_READ_OP(read_fw, __VA_ARGS__)
-#define ENGINE_POSTING_READ(...) __ENGINE_READ_OP(posting_read, __VA_ARGS__)
+#define ENGINE_POSTING_READ(...) __ENGINE_READ_OP(posting_read_fw, __VA_ARGS__)
 #define ENGINE_POSTING_READ16(...) __ENGINE_READ_OP(posting_read16, __VA_ARGS__)
 
 #define ENGINE_READ64(engine__, lower_reg__, upper_reg__) \
