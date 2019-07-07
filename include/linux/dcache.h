a2bbe66493ee380eb25e080e7fcdd1278a847f7e
Al Viro <viro@zeniv.linux.org.uk>
2019-07-07 09:57:53 -0400

    constify dentry argument of dentry_path()/dentry_path_raw()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/dcache.h
+++ include/linux/dcache.h
@@ -300,8 +300,8 @@ char *dynamic_dname(struct dentry *, char *, int, const char *, ...);
 extern char *__d_path(const struct path *, const struct path *, char *, int);
 extern char *d_absolute_path(const struct path *, char *, int);
 extern char *d_path(const struct path *, char *, int);
-extern char *dentry_path_raw(struct dentry *, char *, int);
-extern char *dentry_path(struct dentry *, char *, int);
+extern char *dentry_path_raw(const struct dentry *, char *, int);
+extern char *dentry_path(const struct dentry *, char *, int);
 
 /* Allocation counts.. */
 
