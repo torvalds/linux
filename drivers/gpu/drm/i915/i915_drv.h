a17ce803dffa8e34fda39f8af721a4005c451fef
Rodrigo Vivi <rodrigo.vivi@intel.com>
2019-07-08 13:09:06 -0700

    drm/i915: Update DRIVER_DATE to 20190708
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -99,8 +99,8 @@
 
 #define DRIVER_NAME		"i915"
 #define DRIVER_DESC		"Intel Graphics"
-#define DRIVER_DATE		"20190619"
-#define DRIVER_TIMESTAMP	1560947544
+#define DRIVER_DATE		"20190708"
+#define DRIVER_TIMESTAMP	1562616546
 
 /* Use I915_STATE_WARN(x) and I915_STATE_WARN_ON() (rather than WARN() and
  * WARN_ON()) for hw state sanity checks to check for unexpected conditions
