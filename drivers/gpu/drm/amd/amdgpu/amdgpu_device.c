65e60f6e06e55ea214d9f56a68f93a6f7c4b8455
Le Ma <le.ma@amd.com>
2019-07-09 09:30:19 -0500

    drm/amdgpu: add Arcturus gpu info firmware
    Add GPU info firmware for Arcturus.
    
    Signed-off-by: Le Ma <le.ma@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -70,6 +70,7 @@ MODULE_FIRMWARE("amdgpu/vega12_gpu_info.bin");
 MODULE_FIRMWARE("amdgpu/raven_gpu_info.bin");
 MODULE_FIRMWARE("amdgpu/picasso_gpu_info.bin");
 MODULE_FIRMWARE("amdgpu/raven2_gpu_info.bin");
+MODULE_FIRMWARE("amdgpu/arcturus_gpu_info.bin");
 MODULE_FIRMWARE("amdgpu/navi10_gpu_info.bin");
 MODULE_FIRMWARE("amdgpu/navi14_gpu_info.bin");
 
@@ -1386,6 +1387,9 @@ static int amdgpu_device_parse_gpu_info_fw(struct amdgpu_device *adev)
 			chip_name = "picasso";
 		else
 			chip_name = "raven";
+		break;
+	case CHIP_ARCTURUS:
+		chip_name = "arcturus";
 		break;
 	case CHIP_NAVI10:
 		chip_name = "navi10";
