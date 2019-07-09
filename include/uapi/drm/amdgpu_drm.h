d8f4981e2e8a968411105db568f3d48256b2ebbc
Felix Kuehling <Felix.Kuehling@amd.com>
2019-07-08 20:09:21 -0400

    drm/amdgpu: Add flag to wipe VRAM on release
    This memory allocation flag will be used to indicate BOs containing
    sensitive data that should not be leaked to other processes.
    
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- include/uapi/drm/amdgpu_drm.h
+++ include/uapi/drm/amdgpu_drm.h
@@ -128,6 +128,10 @@ extern "C" {
  * for the second page onward should be set to NC.
  */
 #define AMDGPU_GEM_CREATE_MQD_GFX9		(1 << 8)
+/* Flag that BO may contain sensitive data that must be wiped before
+ * releasing the memory
+ */
+#define AMDGPU_GEM_CREATE_VRAM_WIPE_ON_RELEASE	(1 << 9)
 
 struct drm_amdgpu_gem_create_in  {
 	/** the requested memory size */
