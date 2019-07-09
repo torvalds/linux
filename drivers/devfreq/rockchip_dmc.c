1fae7b16329ecfa68ba004dd51b71ce692b16ea9
Finley Xiao <finley.xiao@rock-chips.com>
2019-07-09 20:01:39 +0800

    PM / devfreq: rockchip_dmc: Use raw_smp_processor_id()
    Fix this warning when DEBUG_PREEMPT is selected:
    BUG: using smp_processor_id() in preemptible [00000000] code: devfreq_wq/46
    caller is debug_smp_processor_id+0x1c/0x24
    CPU: 4 PID: 46 Comm: devfreq_wq Tainted: G           O    4.4.179-gaa55fe4a4414 #71
    Hardware name: Rockchip RK3399 Excavator Board (Linux Opensource) (DT)
    Workqueue: devfreq_wq devfreq_monitor
    Call trace:
    [<ffffff800808866c>] dump_backtrace+0x0/0x228
    [<ffffff80080888b8>] show_stack+0x24/0x30
    [<ffffff80083c91a0>] dump_stack+0x88/0xb0
    [<ffffff80083e11f0>] check_preemption_disabled+0xd4/0xfc
    [<ffffff80083e1234>] debug_smp_processor_id+0x1c/0x24
    [<ffffff80087a8ce0>] rockchip_dmcfreq_target+0x114/0x340
    [<ffffff80087a5ff0>] update_devfreq+0x120/0x1b4
    [<ffffff80087a60b8>] devfreq_monitor+0x34/0x8c
    [<ffffff80080b3164>] process_one_work+0x23c/0x3d8
    [<ffffff80080b3340>] process_scheduled_works+0x40/0x44
    [<ffffff80080b3c8c>] rescuer_thread+0x174/0x26c
    [<ffffff80080b9020>] kthread+0xdc/0xec
    [<ffffff8008082ef0>] ret_from_fork+0x10/0x20
    
    Change-Id: Idc05e25403de04108f597e69643e91868728b873
    Signed-off-by: Finley Xiao <finley.xiao@rock-chips.com>

--- drivers/devfreq/rockchip_dmc.c
+++ drivers/devfreq/rockchip_dmc.c
@@ -1353,7 +1353,7 @@ static int rockchip_dmcfreq_target(struct device *dev, unsigned long *freq,
 	 * Go to specified cpufreq and block other cpufreq changes since
 	 * set_rate needs to complete during vblank.
 	 */
-	cpu_cur = smp_processor_id();
+	cpu_cur = raw_smp_processor_id();
 	policy = cpufreq_cpu_get(cpu_cur);
 	if (!policy) {
 		dev_err(dev, "cpu%d policy NULL\n", cpu_cur);
