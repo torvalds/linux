384329ddc42ad91a9f1f0eab3c53ce0b0402bc60
Nishka Dasgupta <nishkadg.linux@gmail.com>
2019-07-12 12:47:45 +0530

    staging: rtl8188eu: Replace function rtl88eu_phy_rf_config()
    Remove function rtl88eu_phy_rf_config as all it does is call
    rtl88e_phy_rf6052_config.
    Rename rtl88e_phy_rf6052_config to rtl88eu_phy_rf_config and change its
    type from static to non-static to maintain compatibility with call
    sites.
    Issue found with Coccinelle.
    
    Signed-off-by: Nishka Dasgupta <nishkadg.linux@gmail.com>
    Link: https://lore.kernel.org/r/20190712071746.2474-8-nishkadg.linux@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/hal/rf_cfg.c
+++ drivers/staging/rtl8188eu/hal/rf_cfg.c
@@ -218,7 +218,7 @@ static bool rtl88e_phy_config_rf_with_headerfile(struct adapter *adapt)
 	return true;
 }
 
-static bool rtl88e_phy_rf6052_config(struct adapter *adapt)
+bool rtl88eu_phy_rf_config(struct adapter *adapt)
 {
 	struct hal_data_8188e *hal_data = adapt->HalData;
 	u32 u4val = 0;
@@ -245,9 +245,4 @@ static bool rtl88e_phy_rf6052_config(struct adapter *adapt)
 	phy_set_bb_reg(adapt, pphyreg->rfintfs, BRFSI_RFENV, u4val);
 
 	return rtstatus;
-}
-
-bool rtl88eu_phy_rf_config(struct adapter *adapt)
-{
-	return rtl88e_phy_rf6052_config(adapt);
 }
