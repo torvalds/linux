98ef2046f28b642ad35935972a173194ea58a21d
Denis Efremov <efremov@linux.com>
2019-07-11 20:54:56 -0700

    mm: remove the exporting of totalram_pages
    Previously totalram_pages was the global variable.  Currently,
    totalram_pages is the static inline function from the include/linux/mm.h
    However, the function is also marked as EXPORT_SYMBOL, which is at best an
    odd combination.  Because there is no point for the static inline function
    from a public header to be exported, this commit removes the
    EXPORT_SYMBOL() marking.  It will be still possible to use the function in
    modules because all the symbols it depends on are exported.
    
    Link: http://lkml.kernel.org/r/20190710141031.15642-1-efremov@linux.com
    Fixes: ca79b0c211af6 ("mm: convert totalram_pages and totalhigh_pages variables to atomic")
    Signed-off-by: Denis Efremov <efremov@linux.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Pavel Tatashin <pavel.tatashin@microsoft.com>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Mike Rapoport <rppt@linux.ibm.com>
    Cc: Alexander Duyck <alexander.h.duyck@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -224,8 +224,6 @@ int sysctl_lowmem_reserve_ratio[MAX_NR_ZONES] = {
 	[ZONE_MOVABLE] = 0,
 };
 
-EXPORT_SYMBOL(totalram_pages);
-
 static char * const zone_names[MAX_NR_ZONES] = {
 #ifdef CONFIG_ZONE_DMA
 	 "DMA",
