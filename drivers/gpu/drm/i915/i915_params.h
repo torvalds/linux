f774f09649192f326fa030564afd3f8f5d82c1e4
Michal Wajdeczko <michal.wajdeczko@intel.com>
2019-07-12 11:14:45 +0000

    drm/i915/guc: Turn on GuC/HuC auto mode
    Using "enable_guc" modparam auto mode (-1) will let driver
    decide on which platforms and in which configuration we want
    to use GuC/HuC firmwares.
    
    Today driver will enable HuC firmware authentication by GuC
    only on Gen11+ platforms as HuC firmware is required to unlock
    advanced video codecs in media driver.
    
    Legacy platforms with GuC/HuC are not affected by this change
    as for them driver still defaults to disabled(0) in auto mode.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190712111445.21040-3-michal.wajdeczko@intel.com

--- drivers/gpu/drm/i915/i915_params.h
+++ drivers/gpu/drm/i915/i915_params.h
@@ -54,7 +54,7 @@ struct drm_printer;
 	param(int, disable_power_well, -1) \
 	param(int, enable_ips, 1) \
 	param(int, invert_brightness, 0) \
-	param(int, enable_guc, 0) \
+	param(int, enable_guc, -1) \
 	param(int, guc_log_level, -1) \
 	param(char *, guc_firmware_path, NULL) \
 	param(char *, huc_firmware_path, NULL) \
