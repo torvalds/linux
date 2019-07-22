408bfc382efca14cc02c57ae935d5b7efd72e02a
huhai <huhai@kylinos.cn>
2019-07-22 19:55:59 +0800

    i40e: add check on i40e_configure_tx_ring() return value
    When i40e_configure_tx_ring(vsi->tx_rings[i]) returns an error, we should
    exit from i40e_vsi_configure_tx and return the error, instead of continuing
    to check whether xdp is enable, and configure the xdp transmit ring.
    
    Signed-off-by: huhai <huhai@kylinos.cn>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -3364,7 +3364,7 @@ static int i40e_vsi_configure_tx(struct i40e_vsi *vsi)
 	for (i = 0; (i < vsi->num_queue_pairs) && !err; i++)
 		err = i40e_configure_tx_ring(vsi->tx_rings[i]);
 
-	if (!i40e_enabled_xdp_vsi(vsi))
+	if (err || !i40e_enabled_xdp_vsi(vsi))
 		return err;
 
 	for (i = 0; (i < vsi->num_queue_pairs) && !err; i++)
