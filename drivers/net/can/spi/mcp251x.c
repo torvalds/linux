77654a6da00f7a712288f2684229bf63b9c41760
Marc Kleine-Budde <mkl@pengutronix.de>
2019-07-24 14:28:21 +0200

    can: mcp251x: remove unnecessary blank lines
    This patch removes unnecessary blank lines, so that checkpatch doesn't
    complain anymore.
    
    Acked-by: Sean Nyekjaer <sean@geanix.com>
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/spi/mcp251x.c
+++ drivers/net/can/spi/mcp251x.c
@@ -74,7 +74,6 @@
 #define RTS_TXB2		0x04
 #define INSTRUCTION_RTS(n)	(0x80 | ((n) & 0x07))
 
-
 /* MPC251x registers */
 #define CANSTAT	      0x0e
 #define CANCTRL	      0x0f
@@ -897,7 +896,6 @@ static irqreturn_t mcp251x_can_ist(int irq, void *dev_id)
 			}
 			netif_wake_queue(net);
 		}
-
 	}
 	mutex_unlock(&priv->mcp_lock);
 	return IRQ_HANDLED;
