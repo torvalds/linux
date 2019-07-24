c55cb6c28ebc414ceb9c3367e4a4c50d425e4447
Yunying Sun <yunying.sun@intel.com>
2019-07-24 16:29:32 +0800

    perf/x86/intel: Fix invalid Bit 13 for Icelake MSR_OFFCORE_RSP_x register
    [ Upstream commit 3b238a64c3009fed36eaea1af629d9377759d87d ]
    
    The Intel SDM states that bit 13 of Icelake's MSR_OFFCORE_RSP_x
    register is valid, and used for counting hardware generated prefetches
    of L3 cache. Update the bitmask to allow bit 13.
    
    Before:
    $ perf stat -e cpu/event=0xb7,umask=0x1,config1=0x1bfff/u sleep 3
     Performance counter stats for 'sleep 3':
       <not supported>      cpu/event=0xb7,umask=0x1,config1=0x1bfff/u
    
    After:
    $ perf stat -e cpu/event=0xb7,umask=0x1,config1=0x1bfff/u sleep 3
     Performance counter stats for 'sleep 3':
                 9,293      cpu/event=0xb7,umask=0x1,config1=0x1bfff/u
    
    Signed-off-by: Yunying Sun <yunying.sun@intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Kan Liang <kan.liang@linux.intel.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: acme@kernel.org
    Cc: alexander.shishkin@linux.intel.com
    Cc: bp@alien8.de
    Cc: hpa@zytor.com
    Cc: jolsa@redhat.com
    Cc: namhyung@kernel.org
    Link: https://lkml.kernel.org/r/20190724082932.12833-1-yunying.sun@intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/x86/events/intel/core.c
+++ arch/x86/events/intel/core.c
@@ -263,8 +263,8 @@ static struct event_constraint intel_icl_event_constraints[] = {
 };
 
 static struct extra_reg intel_icl_extra_regs[] __read_mostly = {
-	INTEL_UEVENT_EXTRA_REG(0x01b7, MSR_OFFCORE_RSP_0, 0x3fffff9fffull, RSP_0),
-	INTEL_UEVENT_EXTRA_REG(0x01bb, MSR_OFFCORE_RSP_1, 0x3fffff9fffull, RSP_1),
+	INTEL_UEVENT_EXTRA_REG(0x01b7, MSR_OFFCORE_RSP_0, 0x3fffffbfffull, RSP_0),
+	INTEL_UEVENT_EXTRA_REG(0x01bb, MSR_OFFCORE_RSP_1, 0x3fffffbfffull, RSP_1),
 	INTEL_UEVENT_PEBS_LDLAT_EXTRA_REG(0x01cd),
 	INTEL_UEVENT_EXTRA_REG(0x01c6, MSR_PEBS_FRONTEND, 0x7fff17, FE),
 	EVENT_EXTRA_END
