63b87c310a8326c2d0721ab6c981493d590aa542
Sean Paul <seanpaul@chromium.org>
2019-07-26 10:20:47 -0400

    drm/mst: Fix sphinx warnings in drm_dp_msg_connector register functions
    Fixes the following warnings:
    
    ../drivers/gpu/drm/drm_dp_mst_topology.c:1593: warning: Excess function parameter 'drm_connector' description in 'drm_dp_mst_connector_late_register'
    ../drivers/gpu/drm/drm_dp_mst_topology.c:1613: warning: Excess function parameter 'drm_connector' description in 'drm_dp_mst_connector_early_unregister'
    ../drivers/gpu/drm/drm_dp_mst_topology.c:1594: warning: Function parameter or member 'connector' not described in 'drm_dp_mst_connector_late_register'
    ../drivers/gpu/drm/drm_dp_mst_topology.c:1614: warning: Function parameter or member 'connector' not described in 'drm_dp_mst_connector_early_unregister'
    
    Fixes: 562836a269e3 ("drm/dp_mst: Enable registration of AUX devices for MST ports")
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Cc: Leo Li <sunpeng.li@amd.com>
    Cc: Lyude Paul <lyude@redhat.com>
    Cc: Harry Wentland <harry.wentland@amd.com>
    Cc: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Cc: Maxime Ripard <maxime.ripard@bootlin.com>
    Cc: Sean Paul <sean@poorly.run>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: dri-devel@lists.freedesktop.org
    Reviewed-by: Lyude Paul <lyude@redhat.com>
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190726142057.224121-1-sean@poorly.run

--- drivers/gpu/drm/drm_dp_mst_topology.c
+++ drivers/gpu/drm/drm_dp_mst_topology.c
@@ -1579,7 +1579,7 @@ static void build_mst_prop_path(const struct drm_dp_mst_branch *mstb,
 
 /**
  * drm_dp_mst_connector_late_register() - Late MST connector registration
- * @drm_connector: The MST connector
+ * @connector: The MST connector
  * @port: The MST port for this connector
  *
  * Helper to register the remote aux device for this MST port. Drivers should
@@ -1601,7 +1601,7 @@ EXPORT_SYMBOL(drm_dp_mst_connector_late_register);
 
 /**
  * drm_dp_mst_connector_early_unregister() - Early MST connector unregistration
- * @drm_connector: The MST connector
+ * @connector: The MST connector
  * @port: The MST port for this connector
  *
  * Helper to unregister the remote aux device for this MST port, registered by
