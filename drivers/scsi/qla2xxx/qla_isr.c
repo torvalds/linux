5e5402c147083786db2238302e25c44b7a7dc5e9
Quinn Tran <qutran@marvell.com>
2019-07-26 09:07:30 -0700

    scsi: qla2xxx: Skip FW dump on LOOP initialization error
    Firmware dump captured during LOOP Init error does not yield any
    significant information.  This patch removes call to trigger firmware dump
    collection during Loop Initialization.
    
    Signed-off-by: Quinn Tran <qutran@marvell.com>
    Signed-off-by: Himanshu Madhani <hmadhani@marvell.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/qla2xxx/qla_isr.c
+++ drivers/scsi/qla2xxx/qla_isr.c
@@ -776,7 +776,6 @@ skip_rio:
 	case MBA_LOOP_INIT_ERR:
 		ql_log(ql_log_warn, vha, 0x5090,
 		    "LOOP INIT ERROR (%x).\n", mb[1]);
-		ha->isp_ops->fw_dump(vha, 1);
 		set_bit(ISP_ABORT_NEEDED, &vha->dpc_flags);
 		break;
 
