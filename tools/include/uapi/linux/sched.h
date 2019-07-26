71cb265a2bdc7767db5ab37b80d8910cc1fca772
Arnaldo Carvalho de Melo <acme@redhat.com>
2019-07-26 15:41:09 -0300

    UPSTREAM: tools headers UAPI: Sync sched.h with the kernel
    To get the changes in:
    
      a509a7cd7974 ("sched/uclamp: Extend sched_setattr() to support utilization clamping")
      1d6362fa0cfc ("sched/core: Allow sched_setattr() to use the current policy")
      7f192e3cd316 ("fork: add clone3")
    
    And silence this perf build warning:
    
      Warning: Kernel ABI header at 'tools/include/uapi/linux/sched.h' differs from latest version at 'include/uapi/linux/sched.h'
      diff -u tools/include/uapi/linux/sched.h include/uapi/linux/sched.h
    
    No changes in tools/ due to the above.
    
    Bug: 120440300
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Christian Brauner <christian@brauner.io>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Luis Cláudio Gonçalves <lclaudio@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Patrick Bellasi <patrick.bellasi@arm.com>
    Link: https://lkml.kernel.org/n/tip-mtrpsjrux5hgyr5uf8l1aa46@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    (cherry picked from commit c093de6bd3c50d3dd597ff9fa5cf7a30acbb3eb7)
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Change-Id: I670c5b5204fa9a9a926f6c89ba275975738e7b3c
    Signed-off-by: Quentin Perret <qperret@google.com>

--- tools/include/uapi/linux/sched.h
+++ tools/include/uapi/linux/sched.h
@@ -2,6 +2,8 @@
 #ifndef _UAPI_LINUX_SCHED_H
 #define _UAPI_LINUX_SCHED_H
 
+#include <linux/types.h>
+
 /*
  * cloning flags:
  */
@@ -31,6 +33,20 @@
 #define CLONE_IO		0x80000000	/* Clone io context */
 
 /*
+ * Arguments for the clone3 syscall
+ */
+struct clone_args {
+	__aligned_u64 flags;
+	__aligned_u64 pidfd;
+	__aligned_u64 child_tid;
+	__aligned_u64 parent_tid;
+	__aligned_u64 exit_signal;
+	__aligned_u64 stack;
+	__aligned_u64 stack_size;
+	__aligned_u64 tls;
+};
+
+/*
  * Scheduling policies
  */
 #define SCHED_NORMAL		0
@@ -50,9 +66,21 @@
 #define SCHED_FLAG_RESET_ON_FORK	0x01
 #define SCHED_FLAG_RECLAIM		0x02
 #define SCHED_FLAG_DL_OVERRUN		0x04
+#define SCHED_FLAG_KEEP_POLICY		0x08
+#define SCHED_FLAG_KEEP_PARAMS		0x10
+#define SCHED_FLAG_UTIL_CLAMP_MIN	0x20
+#define SCHED_FLAG_UTIL_CLAMP_MAX	0x40
+
+#define SCHED_FLAG_KEEP_ALL	(SCHED_FLAG_KEEP_POLICY | \
+				 SCHED_FLAG_KEEP_PARAMS)
+
+#define SCHED_FLAG_UTIL_CLAMP	(SCHED_FLAG_UTIL_CLAMP_MIN | \
+				 SCHED_FLAG_UTIL_CLAMP_MAX)
 
 #define SCHED_FLAG_ALL	(SCHED_FLAG_RESET_ON_FORK	| \
 			 SCHED_FLAG_RECLAIM		| \
-			 SCHED_FLAG_DL_OVERRUN)
+			 SCHED_FLAG_DL_OVERRUN		| \
+			 SCHED_FLAG_KEEP_ALL		| \
+			 SCHED_FLAG_UTIL_CLAMP)
 
 #endif /* _UAPI_LINUX_SCHED_H */
