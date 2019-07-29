b1507b2d34d642ebabed70d547d276e3268b7db6
Gustavo A. R. Silva <gustavo@embeddedor.com>
2019-07-28 19:02:59 -0500

    mmc: sdhci-s3c: Mark expected switch fall-through
    Mark switch cases where we are expecting to fall through.
    
    This patch fixes the following warnings:
    
    drivers/mmc/host/sdhci-s3c.c: In function 'sdhci_s3c_probe':
    drivers/mmc/host/sdhci-s3c.c:613:19: warning: this statement may fall through [-Wimplicit-fallthrough=]
       host->mmc->caps |= MMC_CAP_8_BIT_DATA;
    drivers/mmc/host/sdhci-s3c.c:614:2: note: here
      case 4:
      ^~~~
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/sdhci-s3c.c
+++ drivers/mmc/host/sdhci-s3c.c
@@ -611,6 +611,7 @@ static int sdhci_s3c_probe(struct platform_device *pdev)
 	switch (pdata->max_width) {
 	case 8:
 		host->mmc->caps |= MMC_CAP_8_BIT_DATA;
+		/* Fall through */
 	case 4:
 		host->mmc->caps |= MMC_CAP_4_BIT_DATA;
 		break;
