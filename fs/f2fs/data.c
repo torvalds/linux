75a037f3604ceb781ae23167e0cdfbe5d71533d7
Jaegeuk Kim <jaegeuk@kernel.org>
2019-07-31 13:27:05 -0700

    f2fs: fix livelock in swapfile writes
    This patch fixes livelock in the below call path when writing swap pages.
    
    [46374.617256] c2    701  __switch_to+0xe4/0x100
    [46374.617265] c2    701  __schedule+0x80c/0xbc4
    [46374.617273] c2    701  schedule+0x74/0x98
    [46374.617281] c2    701  rwsem_down_read_failed+0x190/0x234
    [46374.617291] c2    701  down_read+0x58/0x5c
    [46374.617300] c2    701  f2fs_map_blocks+0x138/0x9a8
    [46374.617310] c2    701  get_data_block_dio_write+0x74/0x104
    [46374.617320] c2    701  __blockdev_direct_IO+0x1350/0x3930
    [46374.617331] c2    701  f2fs_direct_IO+0x55c/0x8bc
    [46374.617341] c2    701  __swap_writepage+0x1d0/0x3e8
    [46374.617351] c2    701  swap_writepage+0x44/0x54
    [46374.617360] c2    701  shrink_page_list+0x140/0xe80
    [46374.617371] c2    701  shrink_inactive_list+0x510/0x918
    [46374.617381] c2    701  shrink_node_memcg+0x2d4/0x804
    [46374.617391] c2    701  shrink_node+0x10c/0x2f8
    [46374.617400] c2    701  do_try_to_free_pages+0x178/0x38c
    [46374.617410] c2    701  try_to_free_pages+0x348/0x4b8
    [46374.617419] c2    701  __alloc_pages_nodemask+0x7f8/0x1014
    [46374.617429] c2    701  pagecache_get_page+0x184/0x2cc
    [46374.617438] c2    701  f2fs_new_node_page+0x60/0x41c
    [46374.617449] c2    701  f2fs_new_inode_page+0x50/0x7c
    [46374.617460] c2    701  f2fs_init_inode_metadata+0x128/0x530
    [46374.617472] c2    701  f2fs_add_inline_entry+0x138/0xd64
    [46374.617480] c2    701  f2fs_do_add_link+0xf4/0x178
    [46374.617488] c2    701  f2fs_create+0x1e4/0x3ac
    [46374.617497] c2    701  path_openat+0xdc0/0x1308
    [46374.617507] c2    701  do_filp_open+0x78/0x124
    [46374.617516] c2    701  do_sys_open+0x134/0x248
    [46374.617525] c2    701  SyS_openat+0x14/0x20
    
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1372,7 +1372,7 @@ static int get_data_block_dio_write(struct inode *inode, sector_t iblock,
 	return __get_data_block(inode, iblock, bh_result, create,
 				F2FS_GET_BLOCK_DIO, NULL,
 				f2fs_rw_hint_to_seg_type(inode->i_write_hint),
-				true);
+				IS_SWAPFILE(inode) ? false : true);
 }
 
 static int get_data_block_dio(struct inode *inode, sector_t iblock,
