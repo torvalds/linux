ee9d66182392695535cc9fccfcb40c16f72de2a9
Sebastien Tisserant <stisserant@wallix.com>
2019-08-01 12:06:08 -0500

    SMB3: Kernel oops mounting a encryptData share with CONFIG_DEBUG_VIRTUAL
    Fix kernel oops when mounting a encryptData CIFS share with
    CONFIG_DEBUG_VIRTUAL
    
    Signed-off-by: Sebastien Tisserant <stisserant@wallix.com>
    Reviewed-by: Pavel Shilovsky <pshilov@microsoft.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/cifs/smb2ops.c
+++ fs/cifs/smb2ops.c
@@ -3489,7 +3489,15 @@ fill_transform_hdr(struct smb2_transform_hdr *tr_hdr, unsigned int orig_len,
 static inline void smb2_sg_set_buf(struct scatterlist *sg, const void *buf,
 				   unsigned int buflen)
 {
-	sg_set_page(sg, virt_to_page(buf), buflen, offset_in_page(buf));
+	void *addr;
+	/*
+	 * VMAP_STACK (at least) puts stack into the vmalloc address space
+	 */
+	if (is_vmalloc_addr(buf))
+		addr = vmalloc_to_page(buf);
+	else
+		addr = virt_to_page(buf);
+	sg_set_page(sg, addr, buflen, offset_in_page(buf));
 }
 
 /* Assumes the first rqst has a transform header as the first iov.
