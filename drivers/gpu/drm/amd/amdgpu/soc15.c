a2d15255eac3473f09fb70b2da764153e8480b74
Prike Liang <Prike.Liang@amd.com>
2019-08-02 15:04:27 +0800

    drm/amdgpu: enable mmhub clock gating for rn
    Enable mmhub midle grain and light sleep clock gating.
    
    Signed-off-by: Prike Liang <Prike.Liang@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/soc15.c
+++ drivers/gpu/drm/amd/amdgpu/soc15.c
@@ -1161,7 +1161,9 @@ static int soc15_common_early_init(void *handle)
 				 AMD_CG_SUPPORT_GFX_3D_CGLS |
 				 AMD_CG_SUPPORT_GFX_CGCG |
 				 AMD_CG_SUPPORT_GFX_CGLS |
-				 AMD_CG_SUPPORT_GFX_CP_LS;
+				 AMD_CG_SUPPORT_GFX_CP_LS |
+				 AMD_CG_SUPPORT_MC_MGCG |
+				 AMD_CG_SUPPORT_MC_LS;
 		adev->pg_flags = 0;
 		adev->external_rev_id = adev->rev_id + 0x91;
 
