5c7ed4385424b1c2ce0c27c41448e080e44ebb09
Dan Williams <dan.j.williams@intel.com>
2019-08-05 08:27:06 -0600

    HMAT: Skip publishing target info for nodes with no online memory
    There are multiple scenarios where the HMAT may contain information
    about proximity domains that are not currently online. Rather than fail
    to report any HMAT data just elide those offline domains.
    
    If and when those domains are later onlined they can be added to the
    HMEM reporting at that point.
    
    This was found while testing EFI_MEMORY_SP support which reserves
    "specific purpose" memory from the general allocation pool. If that
    reservation results in an empty numa-node then the node is not marked
    online leading a spurious:
    
        "acpi/hmat: Ignoring HMAT: Invalid table"
    
    ...result for HMAT parsing.
    
    Reviewed-by: Dave Hansen <dave.hansen@linux.intel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Keith Busch <keith.busch@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/hmat/hmat.c
+++ drivers/acpi/hmat/hmat.c
@@ -108,9 +108,6 @@ static __init void alloc_memory_target(unsigned int mem_pxm)
 {
 	struct memory_target *target;
 
-	if (pxm_to_node(mem_pxm) == NUMA_NO_NODE)
-		return;
-
 	target = find_mem_target(mem_pxm);
 	if (target)
 		return;
@@ -618,7 +615,16 @@ static void hmat_register_target_perf(struct memory_target *target)
 
 static void hmat_register_target(struct memory_target *target)
 {
-	if (!node_online(pxm_to_node(target->memory_pxm)))
+	int nid = pxm_to_node(target->memory_pxm);
+
+	/*
+	 * Skip offline nodes. This can happen when memory
+	 * marked EFI_MEMORY_SP, "specific purpose", is applied
+	 * to all the memory in a promixity domain leading to
+	 * the node being marked offline / unplugged, or if
+	 * memory-only "hotplug" node is offline.
+	 */
+	if (nid == NUMA_NO_NODE || !node_online(nid))
 		return;
 
 	mutex_lock(&target_lock);
