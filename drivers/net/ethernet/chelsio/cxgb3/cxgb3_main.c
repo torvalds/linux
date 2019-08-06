debea2cd3193ac868289e8893c3a719c265b0612
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2019-08-06 10:55:12 +0200

    net: cxgb3_main: Fix a resource leak in a error path in 'init_one()'
    A call to 'kfree_skb()' is missing in the error handling path of
    'init_one()'.
    This is already present in 'remove_one()' but is missing here.
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/chelsio/cxgb3/cxgb3_main.c
+++ drivers/net/ethernet/chelsio/cxgb3/cxgb3_main.c
@@ -3269,7 +3269,7 @@ static int init_one(struct pci_dev *pdev, const struct pci_device_id *ent)
 	if (!adapter->regs) {
 		dev_err(&pdev->dev, "cannot map device registers\n");
 		err = -ENOMEM;
-		goto out_free_adapter;
+		goto out_free_adapter_nofail;
 	}
 
 	adapter->pdev = pdev;
@@ -3396,6 +3396,9 @@ out_free_dev:
 	for (i = ai->nports0 + ai->nports1 - 1; i >= 0; --i)
 		if (adapter->port[i])
 			free_netdev(adapter->port[i]);
+
+out_free_adapter_nofail:
+	kfree_skb(adapter->nofail_skb);
 
 out_free_adapter:
 	kfree(adapter);
