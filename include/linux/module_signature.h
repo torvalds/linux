70433f67ec3a54710744902d782f8954325e25b8
Stephen Rothwell <sfr@canb.auug.org.au>
2019-08-06 12:15:19 +1000

    MODSIGN: make new include file self contained
    Fixes: c8424e776b09 ("MODSIGN: Export module signature definitions")
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Mimi Zohar <zohar@linux.ibm.com>

--- include/linux/module_signature.h
+++ include/linux/module_signature.h
@@ -9,6 +9,8 @@
 #ifndef _LINUX_MODULE_SIGNATURE_H
 #define _LINUX_MODULE_SIGNATURE_H
 
+#include <linux/types.h>
+
 /* In stripped ARM and x86-64 modules, ~ is surprisingly rare. */
 #define MODULE_SIG_STRING "~Module signature appended~\n"
 
