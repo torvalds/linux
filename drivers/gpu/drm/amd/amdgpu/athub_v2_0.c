89b8d6da24b0c89445fd53ad991c58bf85304cbd
Xiaojie Yuan <xiaojie.yuan@amd.com>
2019-08-06 13:42:03 +0800

    drm/amdgpu/athub2: set clock gating for navi12
    add navi12 define
    
    Signed-off-by: Xiaojie Yuan <xiaojie.yuan@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/athub_v2_0.c
+++ drivers/gpu/drm/amd/amdgpu/athub_v2_0.c
@@ -75,6 +75,7 @@ int athub_v2_0_set_clockgating(struct amdgpu_device *adev,
 	switch (adev->asic_type) {
 	case CHIP_NAVI10:
 	case CHIP_NAVI14:
+	case CHIP_NAVI12:
 		athub_v2_0_update_medium_grain_clock_gating(adev,
 				state == AMD_CG_STATE_GATE ? true : false);
 		athub_v2_0_update_medium_grain_light_sleep(adev,
