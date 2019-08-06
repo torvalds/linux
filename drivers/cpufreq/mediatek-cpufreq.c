683df830e6bd12e9eb7da1fbdfccc6ac444508ae
Fabien Parent <fparent@baylibre.com>
2019-08-06 11:50:29 +0200

    cpufreq: mediatek-cpufreq: Add compatible for MT8516
    Add the compatible for MT8516 in order to take advantage of the
    MediaTek CPUFreq driver for Mediatek's MT8516 SoC.
    
    Signed-off-by: Fabien Parent <fparent@baylibre.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/cpufreq/mediatek-cpufreq.c
+++ drivers/cpufreq/mediatek-cpufreq.c
@@ -535,6 +535,7 @@ static const struct of_device_id mtk_cpufreq_machines[] __initconst = {
 	{ .compatible = "mediatek,mt817x", },
 	{ .compatible = "mediatek,mt8173", },
 	{ .compatible = "mediatek,mt8176", },
+	{ .compatible = "mediatek,mt8516", },
 
 	{ }
 };
