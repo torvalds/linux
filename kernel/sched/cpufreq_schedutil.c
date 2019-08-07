01e8f487cecbcec531a12bd0364aabf1d3fe57cc
Douglas RAILLARD <douglas.raillard@arm.com>
2019-08-07 16:33:40 +0100

    sched/cpufreq: Align trace event behavior of fast switching
    [ Upstream commit 77c84dd1881d0f0176cb678d770bfbda26c54390 ]
    
    Fast switching path only emits an event for the CPU of interest, whereas the
    regular path emits an event for all the CPUs that had their frequency changed,
    i.e. all the CPUs sharing the same policy.
    
    With the current behavior, looking at cpu_frequency event for a given CPU that
    is using the fast switching path will not give the correct frequency signal.
    
    Signed-off-by: Douglas RAILLARD <douglas.raillard@arm.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -118,6 +118,7 @@ static void sugov_fast_switch(struct sugov_policy *sg_policy, u64 time,
 			      unsigned int next_freq)
 {
 	struct cpufreq_policy *policy = sg_policy->policy;
+	int cpu;
 
 	if (!sugov_update_next_freq(sg_policy, time, next_freq))
 		return;
@@ -127,7 +128,11 @@ static void sugov_fast_switch(struct sugov_policy *sg_policy, u64 time,
 		return;
 
 	policy->cur = next_freq;
-	trace_cpu_frequency(next_freq, smp_processor_id());
+
+	if (trace_cpu_frequency_enabled()) {
+		for_each_cpu(cpu, policy->cpus)
+			trace_cpu_frequency(next_freq, cpu);
+	}
 }
 
 static void sugov_deferred_update(struct sugov_policy *sg_policy, u64 time,
