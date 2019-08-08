5f595063affa8590a03a4f3d30bb28b1560f9d49
Luis Araneda <luaraneda@gmail.com>
2019-08-08 08:52:42 -0400

    ARM: zynq: Support smp in thumb mode
    Add .arm directive to headsmp.S to ensure that the
    CPU starts in 32-bit ARM mode and the correct code
    size is copied on smp bring-up.
    This is related to the fix applied to SoCFPGA by
    commit 5616f36713ea
    ("ARM: SoCFPGA: Fix secondary CPU startup in thumb2 kernel")
    
    Additionally, start secondary CPUs on secondary_startup_arm
    to automatically switch from ARM to thumb on a thumb kernel
    
    Signed-off-by: Luis Araneda <luaraneda@gmail.com>
    Suggested-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/mach-zynq/platsmp.c
+++ arch/arm/mach-zynq/platsmp.c
@@ -81,7 +81,7 @@ EXPORT_SYMBOL(zynq_cpun_start);
 
 static int zynq_boot_secondary(unsigned int cpu, struct task_struct *idle)
 {
-	return zynq_cpun_start(__pa_symbol(secondary_startup), cpu);
+	return zynq_cpun_start(__pa_symbol(secondary_startup_arm), cpu);
 }
 
 /*
