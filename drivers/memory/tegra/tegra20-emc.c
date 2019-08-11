d039cf2834e9eb7cfd14f245172ca4f4a67c106b
Dmitry Osipenko <digetx@gmail.com>
2019-08-12 00:00:32 +0300

    memory: tegra: Include io.h instead of iopoll.h
    The register polling code was gone, but the included header change was
    missed. Fix it up for consistency.
    
    Acked-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/memory/tegra/tegra20-emc.c
+++ drivers/memory/tegra/tegra20-emc.c
@@ -10,7 +10,7 @@
 #include <linux/completion.h>
 #include <linux/err.h>
 #include <linux/interrupt.h>
-#include <linux/iopoll.h>
+#include <linux/io.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/of.h>
