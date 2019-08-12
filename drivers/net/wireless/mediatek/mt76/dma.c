5a90107d79e70d2596fb68be3018e8b7e0ae4534
Felix Fietkau <nbd@nbd.name>
2019-08-12 15:50:59 +0200

    mt76: dma: reset q->rx_head on rx reset
    Fixes rx of the first frame if a fragmented rx was interrupted by the reset
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/dma.c
+++ drivers/net/wireless/mediatek/mt76/dma.c
@@ -431,6 +431,12 @@ mt76_dma_rx_reset(struct mt76_dev *dev, enum mt76_rxq_id qid)
 	mt76_dma_rx_cleanup(dev, q);
 	mt76_dma_sync_idx(dev, q);
 	mt76_dma_rx_fill(dev, q);
+
+	if (!q->rx_head)
+		return;
+
+	dev_kfree_skb(q->rx_head);
+	q->rx_head = NULL;
 }
 
 static void
