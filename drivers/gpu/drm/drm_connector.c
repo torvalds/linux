12db36bc3cec76aae1c214d787eb0a54bc40fceb
Sean Paul <seanpaul@chromium.org>
2019-08-12 10:01:03 -0400

    drm: Fix kerneldoc warns in connector-related docs
    Fixes the following warnings:
    ../drivers/gpu/drm/drm_connector.c:989: WARNING: Unexpected indentation.
    ../drivers/gpu/drm/drm_connector.c:993: WARNING: Unexpected indentation.
    ../include/drm/drm_connector.h:544: WARNING: Inline interpreted text or phrase reference start-string without end-string.
    ../include/drm/drm_connector.h:544: WARNING: Inline interpreted text or phrase reference start-string without end-string.
    
    Changes in v2:
    - Use () instead of & for functions (Sam)
    
    Fixes: 1b27fbdde1df ("drm: Add drm_atomic_get_(old|new)_connector_for_encoder() helpers")
    Fixes: bb5a45d40d50 ("drm/hdcp: update content protection property with uevent")
    Cc: Ramalingam C <ramalingam.c@intel.com>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: Pekka Paalanen <pekka.paalanen@collabora.com>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Cc: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Cc: Sean Paul <seanpaul@chromium.org>
    Cc: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Cc: Maxime Ripard <maxime.ripard@bootlin.com>
    Cc: Sean Paul <sean@poorly.run>
    Cc: David Airlie <airlied@linux.ie>
    Cc: dri-devel@lists.freedesktop.org
    Reviewed-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190812140112.6702-1-sean@poorly.run

--- drivers/gpu/drm/drm_connector.c
+++ drivers/gpu/drm/drm_connector.c
@@ -986,12 +986,14 @@ static const struct drm_prop_enum_list hdmi_colorspaces[] = {
  *	- Kernel sends uevent with the connector id and property id through
  *	  @drm_hdcp_update_content_protection, upon below kernel triggered
  *	  scenarios:
- *		DESIRED -> ENABLED	(authentication success)
- *		ENABLED -> DESIRED	(termination of authentication)
+ *
+ *		- DESIRED -> ENABLED (authentication success)
+ *		- ENABLED -> DESIRED (termination of authentication)
  *	- Please note no uevents for userspace triggered property state changes,
  *	  which can't fail such as
- *		DESIRED/ENABLED -> UNDESIRED
- *		UNDESIRED -> DESIRED
+ *
+ *		- DESIRED/ENABLED -> UNDESIRED
+ *		- UNDESIRED -> DESIRED
  *	- Userspace is responsible for polling the property or listen to uevents
  *	  to determine when the value transitions from ENABLED to DESIRED.
  *	  This signifies the link is no longer protected and userspace should
