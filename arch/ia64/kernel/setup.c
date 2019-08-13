08f9530a0f883b11fc4062a20703e8920c5980c7
Christoph Hellwig <hch@lst.de>
2019-08-13 09:24:52 +0200

    tty/serial: remove the sn_console driver
    The SGI SN2 support is about to be removed.  Remove this driver that
    depends on the SN2 support.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Link: https://lkml.kernel.org/r/20190813072514.23299-7-hch@lst.de
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/kernel/setup.c
+++ arch/ia64/kernel/setup.c
@@ -463,13 +463,6 @@ early_console_setup (char *cmdline)
 {
 	int earlycons = 0;
 
-#ifdef CONFIG_SERIAL_SGI_L1_CONSOLE
-	{
-		extern int sn_serial_console_early_setup(void);
-		if (!sn_serial_console_early_setup())
-			earlycons++;
-	}
-#endif
 #ifdef CONFIG_EFI_PCDP
 	if (!efi_setup_pcdp_console(cmdline))
 		earlycons++;
