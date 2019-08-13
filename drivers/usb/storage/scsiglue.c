115f3bad1a36791b5293aeebde342a95a516198e
Matthias Maennich <maennich@google.com>
2019-08-13 13:17:06 +0100

    usb-storage: remove single-use define for debugging
    USB_STORAGE was defined as "usb-storage: " and used in a single location
    as argument to printk. In order to be able to use the name
    'USB_STORAGE', drop the definition and use the string directly for the
    printk call.
    
    Signed-off-by: Matthias Maennich <maennich@google.com>
    Link: https://lore.kernel.org/r/20190813121733.52480-10-maennich@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/storage/scsiglue.c
+++ drivers/usb/storage/scsiglue.c
@@ -379,7 +379,7 @@ static int queuecommand_lck(struct scsi_cmnd *srb,
 
 	/* check for state-transition errors */
 	if (us->srb != NULL) {
-		printk(KERN_ERR USB_STORAGE "Error in %s: us->srb = %p\n",
+		printk(KERN_ERR "usb-storage: Error in %s: us->srb = %p\n",
 			__func__, us->srb);
 		return SCSI_MLQUEUE_HOST_BUSY;
 	}
