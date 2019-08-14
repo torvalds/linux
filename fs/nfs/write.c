06c9fdf3b9f1acc6e53753c99c54c39764cc979f
Trond Myklebust <trond.myklebust@hammerspace.com>
2019-08-14 15:42:43 -0400

    NFS: On fatal writeback errors, we need to call nfs_inode_remove_request()
    If the writeback error is fatal, we need to remove the tracking structures
    (i.e. the nfs_page) from the inode.
    
    Fixes: 6fbda89b257f ("NFS: Replace custom error reporting mechanism...")
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfs/write.c
+++ fs/nfs/write.c
@@ -57,6 +57,7 @@ static const struct rpc_call_ops nfs_commit_ops;
 static const struct nfs_pgio_completion_ops nfs_async_write_completion_ops;
 static const struct nfs_commit_completion_ops nfs_commit_completion_ops;
 static const struct nfs_rw_ops nfs_rw_write_ops;
+static void nfs_inode_remove_request(struct nfs_page *req);
 static void nfs_clear_request_commit(struct nfs_page *req);
 static void nfs_init_cinfo_from_inode(struct nfs_commit_info *cinfo,
 				      struct inode *inode);
@@ -591,7 +592,9 @@ release_request:
 
 static void nfs_write_error(struct nfs_page *req, int error)
 {
+	nfs_set_pageerror(page_file_mapping(req->wb_page));
 	nfs_mapping_set_error(req->wb_page, error);
+	nfs_inode_remove_request(req);
 	nfs_end_page_writeback(req);
 	nfs_release_request(req);
 }
