a982eeb09b6030e567b8b815277c8c9197168040
Jackie Liu <liuyun01@kylinos.cn>
2019-08-14 17:35:22 +0800

    io_uring: fix an issue when IOSQE_IO_LINK is inserted into defer list
    This patch may fix two issues:
    
    First, when IOSQE_IO_DRAIN set, the next IOs need to be inserted into
    defer list to delay execution, but link io will be actively scheduled to
    run by calling io_queue_sqe.
    
    Second, when multiple LINK_IOs are inserted together with defer_list,
    the LINK_IO is no longer keep order.
    
       |-------------|
       |   LINK_IO   |      ----> insert to defer_list  -----------
       |-------------|                                            |
       |   LINK_IO   |      ----> insert to defer_list  ----------|
       |-------------|                                            |
       |   LINK_IO   |      ----> insert to defer_list  ----------|
       |-------------|                                            |
       |   NORMAL_IO |      ----> insert to defer_list  ----------|
       |-------------|                                            |
                                                                  |
                                  queue_work at same time   <-----|
    
    Fixes: 9e645e1105c ("io_uring: add support for sqe links")
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -2023,6 +2023,15 @@ static int io_queue_sqe(struct io_ring_ctx *ctx, struct io_kiocb *req,
 {
 	int ret;
 
+	ret = io_req_defer(ctx, req, s->sqe);
+	if (ret) {
+		if (ret != -EIOCBQUEUED) {
+			io_free_req(req);
+			io_cqring_add_event(ctx, s->sqe->user_data, ret);
+		}
+		return 0;
+	}
+
 	ret = __io_submit_sqe(ctx, req, s, true);
 	if (ret == -EAGAIN && !(req->flags & REQ_F_NOWAIT)) {
 		struct io_uring_sqe *sqe_copy;
@@ -2092,13 +2101,6 @@ err_req:
 		io_free_req(req);
 err:
 		io_cqring_add_event(ctx, s->sqe->user_data, ret);
-		return;
-	}
-
-	ret = io_req_defer(ctx, req, s->sqe);
-	if (ret) {
-		if (ret != -EIOCBQUEUED)
-			goto err_req;
 		return;
 	}
 
