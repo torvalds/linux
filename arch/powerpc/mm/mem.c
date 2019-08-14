ad628a34ec4e3558bf838195f60bbaa4c2b68f2a
Christophe Leroy <christophe.leroy@c-s.fr>
2019-08-14 14:36:10 +0000

    powerpc/mm: don't display empty early ioremap area
    On the 8xx, the layout displayed at boot is:
    
    [    0.000000] Memory: 121856K/131072K available (5728K kernel code, 592K rwdata, 1248K rodata, 560K init, 448K bss, 9216K reserved, 0K cma-reserved)
    [    0.000000] Kernel virtual memory layout:
    [    0.000000]   * 0xffefc000..0xffffc000  : fixmap
    [    0.000000]   * 0xffefc000..0xffefc000  : early ioremap
    [    0.000000]   * 0xc9000000..0xffefc000  : vmalloc & ioremap
    [    0.000000] SLUB: HWalign=16, Order=0-3, MinObjects=0, CPUs=1, Nodes=1
    
    Remove display of an empty early ioremap.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/f6267226038cb25a839b567319e240576e3f8565.1565793287.git.christophe.leroy@c-s.fr

--- arch/powerpc/mm/mem.c
+++ arch/powerpc/mm/mem.c
@@ -306,8 +306,9 @@ void __init mem_init(void)
 	pr_info("  * 0x%08lx..0x%08lx  : consistent mem\n",
 		IOREMAP_TOP, IOREMAP_TOP + CONFIG_CONSISTENT_SIZE);
 #endif /* CONFIG_NOT_COHERENT_CACHE */
-	pr_info("  * 0x%08lx..0x%08lx  : early ioremap\n",
-		ioremap_bot, IOREMAP_TOP);
+	if (ioremap_bot != IOREMAP_TOP)
+		pr_info("  * 0x%08lx..0x%08lx  : early ioremap\n",
+			ioremap_bot, IOREMAP_TOP);
 	pr_info("  * 0x%08lx..0x%08lx  : vmalloc & ioremap\n",
 		VMALLOC_START, VMALLOC_END);
 #endif /* CONFIG_PPC32 */
