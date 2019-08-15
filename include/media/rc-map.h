fa992b335aa778db112e91aac19c45e4d914a1f4
Christian Hewitt <christianshewitt@gmail.com>
2019-08-15 11:59:16 -0300

    media: rc: add keymap for WeTeK Play 2 remote
    The WeTek Play 2 Android STB ships with an unusual remote where the
    main up/down/left/right/enter controls are surrounded with an outer
    ring of additional keys which are listed in clockwise order.
    
    Signed-off-by: Christian Hewitt <christianshewitt@gmail.com>
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- include/media/rc-map.h
+++ include/media/rc-map.h
@@ -271,6 +271,7 @@ struct rc_map *rc_map_get(const char *name);
 #define RC_MAP_VIDEOMATE_S350            "rc-videomate-s350"
 #define RC_MAP_VIDEOMATE_TV_PVR          "rc-videomate-tv-pvr"
 #define RC_MAP_WETEK_HUB                 "rc-wetek-hub"
+#define RC_MAP_WETEK_PLAY2               "rc-wetek-play2"
 #define RC_MAP_WINFAST                   "rc-winfast"
 #define RC_MAP_WINFAST_USBII_DELUXE      "rc-winfast-usbii-deluxe"
 #define RC_MAP_SU3000                    "rc-su3000"
