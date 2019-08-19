5f0917a281c66fb310988d4c38f3c55b7023b8ff
Vasily Gorbik <gor@linux.ibm.com>
2019-08-19 17:55:44 +0200

    s390/cmma: reuse kstrtobool for option value parsing
    "cmma" option setup already recognises some textual values. Yet kstrtobool
    is a more common way to parse boolean values, reuse it to unify option
    value parsing behavior and simplify code a bit.
    
    While at it, __setup value parsing callbacks are expected to return
    1 when an option is recognized, and returning any other value won't
    trigger any error message currently, so simply return 1.
    
    Reviewed-by: Philipp Rudo <prudo@linux.ibm.com>
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>

--- arch/s390/mm/page-states.c
+++ arch/s390/mm/page-states.c
@@ -21,17 +21,11 @@ static int cmma_flag = 1;
 
 static int __init cmma(char *str)
 {
-	char *parm;
+	bool enabled;
 
-	parm = strstrip(str);
-	if (strcmp(parm, "yes") == 0 || strcmp(parm, "on") == 0) {
-		cmma_flag = 1;
-		return 1;
-	}
-	cmma_flag = 0;
-	if (strcmp(parm, "no") == 0 || strcmp(parm, "off") == 0)
-		return 1;
-	return 0;
+	if (!kstrtobool(str, &enabled))
+		cmma_flag = enabled;
+	return 1;
 }
 __setup("cmma=", cmma);
 
