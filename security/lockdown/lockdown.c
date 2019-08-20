95f5e95f41dff31b2a4566c5a8975c08a49ae4e3
Matthew Garrett <mjg59@srcf.ucam.org>
2019-08-19 17:17:49 -0700

    x86/msr: Restrict MSR access when the kernel is locked down
    Writing to MSRs should not be allowed if the kernel is locked down, since
    it could lead to execution of arbitrary code in kernel mode.  Based on a
    patch by Kees Cook.
    
    Signed-off-by: Matthew Garrett <mjg59@google.com>
    Signed-off-by: David Howells <dhowells@redhat.com>
    Acked-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    cc: x86@kernel.org
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/lockdown/lockdown.c
+++ security/lockdown/lockdown.c
@@ -24,6 +24,7 @@ static char *lockdown_reasons[LOCKDOWN_CONFIDENTIALITY_MAX+1] = {
 	[LOCKDOWN_HIBERNATION] = "hibernation",
 	[LOCKDOWN_PCI_ACCESS] = "direct PCI access",
 	[LOCKDOWN_IOPORT] = "raw io port access",
+	[LOCKDOWN_MSR] = "raw MSR access",
 	[LOCKDOWN_INTEGRITY_MAX] = "integrity",
 	[LOCKDOWN_CONFIDENTIALITY_MAX] = "confidentiality",
 };
