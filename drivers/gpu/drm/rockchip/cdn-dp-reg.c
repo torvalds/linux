43fd724887148503c6b90efe556dbbe9e251892c
Wenping Zhang <wenping.zhang@rock-chips.com>
2019-08-20 15:54:17 +0800

    drm/rockchip: cdn_dp: fix issue 1 lane dp device can't output audio.
    Change-Id: Ic148a2c5a5bf12ded9f83201d9a4dffe406b4e3f
    Signed-off-by: Wenping Zhang <wenping.zhang@rock-chips.com>

--- drivers/gpu/drm/rockchip/cdn-dp-reg.c
+++ drivers/gpu/drm/rockchip/cdn-dp-reg.c
@@ -998,13 +998,23 @@ static void cdn_dp_audio_config_i2s(struct cdn_dp_device *dp,
 	writel(I2S_DEC_START, dp->regs + AUDIO_SRC_CNTL);
 }
 
-static void cdn_dp_audio_config_spdif(struct cdn_dp_device *dp)
+static void cdn_dp_audio_config_spdif(struct cdn_dp_device *dp,
+				      struct audio_info *audio)
 {
 	u32 val;
+	int sub_pckt_num = 1;
 
+	if (audio->channels == 2) {
+		if (dp->link.num_lanes == 1)
+			sub_pckt_num = 2;
+		else
+			sub_pckt_num = 4;
+	}
 	writel(SYNC_WR_TO_CH_ZERO, dp->regs + FIFO_CNTL);
 
-	val = MAX_NUM_CH(2) | AUDIO_TYPE_LPCM | CFG_SUB_PCKT_NUM(4);
+	val = MAX_NUM_CH(audio->channels);
+	val |= AUDIO_TYPE_LPCM;
+	val |= CFG_SUB_PCKT_NUM(sub_pckt_num);
 	writel(val, dp->regs + SMPL2PKT_CNFG);
 	writel(SMPL2PKT_EN, dp->regs + SMPL2PKT_CNTL);
 
@@ -1054,7 +1064,7 @@ int cdn_dp_audio_config(struct cdn_dp_device *dp, struct audio_info *audio)
 	if (audio->format == AFMT_I2S)
 		cdn_dp_audio_config_i2s(dp, audio);
 	else if (audio->format == AFMT_SPDIF)
-		cdn_dp_audio_config_spdif(dp);
+		cdn_dp_audio_config_spdif(dp, audio);
 
 	ret = cdn_dp_reg_write(dp, AUDIO_PACK_CONTROL, AUDIO_PACK_EN);
 
