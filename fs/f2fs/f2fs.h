950d47f233522ad8ad4b914ae9ddc03db2811edb
Chao Yu <yuchao0@huawei.com>
2019-08-21 23:13:35 +0800

    f2fs: optimize case-insensitive lookups
    This patch ports below casefold enhancement patch from ext4 to f2fs
    
    commit 3ae72562ad91 ("ext4: optimize case-insensitive lookups")
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -2941,7 +2941,8 @@ struct dentry *f2fs_get_parent(struct dentry *child);
 
 extern int f2fs_ci_compare(const struct inode *parent,
 			   const struct qstr *name,
-			   const struct qstr *entry);
+			   const struct qstr *entry,
+			   bool quick);
 
 /*
  * dir.c
