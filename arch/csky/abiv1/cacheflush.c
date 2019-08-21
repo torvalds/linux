4ad35c1f56386c8e7019c921bba1af109fde9693
Guo Ren <ren_guo@c-sky.com>
2019-08-21 19:15:52 +0800

    csky: Fixup 610 vipt cache flush mechanism
    610 has vipt aliasing issue, so we need to finish the cache flush
    apis mentioned in cachetlb.rst to avoid data corruption.
    
    Here is the list of modified apis in the patch:
    
     - flush_kernel_dcache_page      (new add)
     - flush_dcache_mmap_lock        (new add)
     - flush_dcache_mmap_unlock      (new add)
     - flush_kernel_vmap_range       (new add)
     - invalidate_kernel_vmap_range  (new add)
     - flush_anon_page               (new add)
     - flush_cache_range             (new add)
     - flush_cache_vmap              (flush all)
     - flush_cache_vunmap            (flush all)
     - flush_cache_mm                (only dcache flush)
     - flush_icache_page             (just nop)
     - copy_from_user_page           (remove no need flush)
     - copy_to_user_page             (remove no need flush)
    
    Change to V2:
     - Fixup compile error with xa_lock*(&mapping->i_pages)
    
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Christoph Hellwig <hch@infradead.org>

--- arch/csky/abiv1/cacheflush.c
+++ arch/csky/abiv1/cacheflush.c
@@ -54,3 +54,23 @@ void update_mmu_cache(struct vm_area_struct *vma, unsigned long addr,
 			icache_inv_all();
 	}
 }
+
+void flush_kernel_dcache_page(struct page *page)
+{
+	struct address_space *mapping;
+
+	mapping = page_mapping_file(page);
+
+	if (!mapping || mapping_mapped(mapping))
+		dcache_wbinv_all();
+}
+EXPORT_SYMBOL(flush_kernel_dcache_page);
+
+void flush_cache_range(struct vm_area_struct *vma, unsigned long start,
+	unsigned long end)
+{
+	dcache_wbinv_all();
+
+	if (vma->vm_flags & VM_EXEC)
+		icache_inv_all();
+}
