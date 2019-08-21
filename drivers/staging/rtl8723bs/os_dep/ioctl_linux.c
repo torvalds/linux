5ab706bd0bcaea0296426ecca333f69bad60d24e
Saurav Girepunje <saurav.girepunje@gmail.com>
2019-08-21 23:46:34 +0530

    staging: rtl8723bs: os_dep: Drop condition with no effect
    As the "else if" and "else" branch body are identical the condition
    has no effect. So drop the "else if" condition.
    
    Signed-off-by: Saurav Girepunje <saurav.girepunje@gmail.com>
    Link: https://lore.kernel.org/r/20190821181631.GA11082@saurav
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/os_dep/ioctl_linux.c
+++ drivers/staging/rtl8723bs/os_dep/ioctl_linux.c
@@ -212,8 +212,6 @@ static char *translate_scan(struct adapter *padapter,
 	} else if (ht_cap) {
 		if (mcs_rate&0x8000) { /* MCS15 */
 			max_rate = (bw_40MHz) ? ((short_GI)?300:270):((short_GI)?144:130);
-		} else if (mcs_rate&0x0080) { /* MCS7 */
-			max_rate = (bw_40MHz) ? ((short_GI)?150:135):((short_GI)?72:65);
 		} else { /* default MCS7 */
 			/* DBG_871X("wx_get_scan, mcs_rate_bitmap = 0x%x\n", mcs_rate); */
 			max_rate = (bw_40MHz) ? ((short_GI)?150:135):((short_GI)?72:65);
