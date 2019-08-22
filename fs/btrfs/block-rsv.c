426551f6866a369c045c77a16725f41a097fac99
Josef Bacik <josef@toxicpanda.com>
2019-08-22 15:19:02 -0400

    btrfs: use btrfs_try_granting_tickets in update_global_rsv
    We have some annoying xfstests tests that will create a very small fs,
    fill it up, delete it, and repeat to make sure everything works right.
    This trips btrfs up sometimes because we may commit a transaction to
    free space, but most of the free metadata space was being reserved by
    the global reserve.  So we commit and update the global reserve, but the
    space is simply added to bytes_may_use directly, instead of trying to
    add it to existing tickets.  This results in ENOSPC when we really did
    have space.  Fix this by calling btrfs_try_granting_tickets once we add
    back our excess space to wake any pending tickets.
    
    Reviewed-by: Nikolay Borisov <nborisov@suse.com>
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/block-rsv.c
+++ fs/btrfs/block-rsv.c
@@ -305,6 +305,7 @@ void btrfs_update_global_block_rsv(struct btrfs_fs_info *fs_info)
 		btrfs_space_info_update_bytes_may_use(fs_info, sinfo,
 						      -num_bytes);
 		block_rsv->reserved = block_rsv->size;
+		btrfs_try_granting_tickets(fs_info, sinfo);
 	}
 
 	if (block_rsv->reserved == block_rsv->size)
