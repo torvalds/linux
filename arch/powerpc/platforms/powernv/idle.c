512a5a6452b6d742b6f713184414d28cb6413080
Claudio Carvalho <cclaudio@linux.ibm.com>
2019-08-22 00:48:37 -0300

    powerpc/powernv: Access LDBAR only if ultravisor disabled
    LDBAR is a per-thread SPR populated and used by the thread-imc pmu
    driver to dump the data counter into memory. It contains memory along
    with few other configuration bits. LDBAR is populated and enabled only
    when any of the thread imc pmu events are monitored.
    
    In ultravisor enabled systems, LDBAR becomes ultravisor privileged and
    an attempt to write to it will cause a Hypervisor Emulation Assistance
    interrupt.
    
    In ultravisor enabled systems, the ultravisor is responsible to maintain
    the LDBAR (e.g. save and restore it).
    
    This restricts LDBAR access to only when ultravisor is disabled.
    
    Signed-off-by: Claudio Carvalho <cclaudio@linux.ibm.com>
    Reviewed-by: Ram Pai <linuxram@us.ibm.com>
    Reviewed-by: Ryan Grimm <grimm@linux.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20190822034838.27876-7-cclaudio@linux.ibm.com

--- arch/powerpc/platforms/powernv/idle.c
+++ arch/powerpc/platforms/powernv/idle.c
@@ -675,7 +675,8 @@ static unsigned long power9_idle_stop(unsigned long psscr, bool mmu_on)
 		sprs.ptcr	= mfspr(SPRN_PTCR);
 		sprs.rpr	= mfspr(SPRN_RPR);
 		sprs.tscr	= mfspr(SPRN_TSCR);
-		sprs.ldbar	= mfspr(SPRN_LDBAR);
+		if (!firmware_has_feature(FW_FEATURE_ULTRAVISOR))
+			sprs.ldbar = mfspr(SPRN_LDBAR);
 
 		sprs_saved = true;
 
@@ -789,7 +790,8 @@ core_woken:
 	mtspr(SPRN_MMCR0,	sprs.mmcr0);
 	mtspr(SPRN_MMCR1,	sprs.mmcr1);
 	mtspr(SPRN_MMCR2,	sprs.mmcr2);
-	mtspr(SPRN_LDBAR,	sprs.ldbar);
+	if (!firmware_has_feature(FW_FEATURE_ULTRAVISOR))
+		mtspr(SPRN_LDBAR, sprs.ldbar);
 
 	mtspr(SPRN_SPRG3,	local_paca->sprg_vdso);
 
