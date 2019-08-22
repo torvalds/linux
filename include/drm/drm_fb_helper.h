0a8459693238a339de9705da4e26f6ffbb6a4ebc
Gerd Hoffmann <kraxel@redhat.com>
2019-08-22 11:06:43 +0200

    fbdev: drop res_id parameter from remove_conflicting_pci_framebuffers
    Since commit b0e999c95581 ("fbdev: list all pci memory bars as
    conflicting apertures") the parameter was used for some sanity checks
    only, to make sure we detect any issues with the new approach to just
    list all memory bars as apertures.
    
    No issues turned up so far, so continue to cleanup:  Drop the res_id
    parameter, drop the sanity checks.  Also downgrade the logging from
    "info" level to "debug" level and update documentation.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/20190822090645.25410-2-kraxel@redhat.com

--- include/drm/drm_fb_helper.h
+++ include/drm/drm_fb_helper.h
@@ -560,7 +560,7 @@ drm_fb_helper_remove_conflicting_pci_framebuffers(struct pci_dev *pdev,
 	 * otherwise the vga fbdev driver falls over.
 	 */
 #if IS_REACHABLE(CONFIG_FB)
-	ret = remove_conflicting_pci_framebuffers(pdev, resource_id, name);
+	ret = remove_conflicting_pci_framebuffers(pdev, name);
 #endif
 	if (ret == 0)
 		ret = vga_remove_vgacon(pdev);
