97aad5ce88164bc5f884b5234f015ac553ea2fe5
YueHaibing <yuehaibing@huawei.com>
2019-08-22 22:37:47 +0800

    ASoC: mediatek: mt2701: Fix -Wunused-const-variable warnings
    sound/soc/mediatek/mt2701/mt2701-afe-common.h:66:27: warning:
     mt2701_afe_backup_list defined but not used [-Wunused-const-variable=]
    
    mt2701_afe_backup_list is only used in mt2701-afe-pcm.c,
    so just move the definition over there.
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Link: https://lore.kernel.org/r/20190822143747.20944-1-yuehaibing@huawei.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/mediatek/mt2701/mt2701-afe-pcm.c
+++ sound/soc/mediatek/mt2701/mt2701-afe-pcm.c
@@ -60,6 +60,27 @@ static const struct mt2701_afe_rate mt2701_afe_i2s_rates[] = {
 	{ .rate = 352800, .regvalue = 24 },
 };
 
+static const unsigned int mt2701_afe_backup_list[] = {
+	AUDIO_TOP_CON0,
+	AUDIO_TOP_CON4,
+	AUDIO_TOP_CON5,
+	ASYS_TOP_CON,
+	AFE_CONN0,
+	AFE_CONN1,
+	AFE_CONN2,
+	AFE_CONN3,
+	AFE_CONN15,
+	AFE_CONN16,
+	AFE_CONN17,
+	AFE_CONN18,
+	AFE_CONN19,
+	AFE_CONN20,
+	AFE_CONN21,
+	AFE_CONN22,
+	AFE_DAC_CON0,
+	AFE_MEMIF_PBUF_SIZE,
+};
+
 static int mt2701_dai_num_to_i2s(struct mtk_base_afe *afe, int num)
 {
 	struct mt2701_afe_private *afe_priv = afe->platform_priv;
