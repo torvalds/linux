d4ed7463d02aef4b2270ec2a680813cd8b17def7
Heiner Kallweit <hkallweit1@gmail.com>
2019-08-23 20:07:26 +0200

    r8169: fix DMA issue on MIPS platform
    As reported by Aaro this patch causes network problems on
    MIPS Loongson platform. Therefore revert it.
    
    Fixes: f072218cca5b ("r8169: remove not needed call to dma_sync_single_for_device")
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Reported-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/realtek/r8169_main.c
+++ drivers/net/ethernet/realtek/r8169_main.c
@@ -5822,6 +5822,10 @@ process_pkt:
 			skb->tail += pkt_size;
 			skb->len = pkt_size;
 
+			dma_sync_single_for_device(tp_to_dev(tp),
+						   le64_to_cpu(desc->addr),
+						   pkt_size, DMA_FROM_DEVICE);
+
 			rtl8169_rx_csum(skb, status);
 			skb->protocol = eth_type_trans(skb, dev);
 
