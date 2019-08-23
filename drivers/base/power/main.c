352686974fae111d30f3c666677caf665d6bc807
zhuguangqing <zhuguangqing@xiaomi.com>
2019-08-23 09:04:06 +0800

    ANDROID: Log which device failed to suspend in dpm_suspend_start()
    Problem background: In the process of suspend, maybe some device suspend
    callback failed in dpm_suspend_start()/dpm_prepare()/dpm_suspend().
    Because it's after suspend_console(), so printf() is disabled now.
    Currently we can see "Some devices failed to suspend, or early wake
    event detected" by log_suspend_abort_reason() in bugreport. There are
    many devices but we don't know which exactly device failed. So we
    want to do a little change to record which device failed.
    
    Note: I checked upstream LTS kernel, then I found the
    patch can not be sent upstream, because it uses function
    log_suspend_abort_reason() in wakeup_reason.c, the initial
    patch(https://patchwork.kernel.org/patch/3827331/) for adding
    /kernel/power/wakeup_reason.c is not accepted by upstream which
    was merged in AOSP common kernels. So maybe the patch could
    only be sent to AOSP common kernels.
    
    Test: manual - Use modified version for daily use two days, from
    bugreport we can see which device failed.
    Bug: 120445600
    Change-Id: I326c87ca1263496db79d08ec615f12fc22452d7a
    Signed-off-by: zhuguangqing <zhuguangqing@xiaomi.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -2007,6 +2007,7 @@ int dpm_prepare(pm_message_t state)
 			}
 			pr_info("Device %s not prepared for power transition: code %d\n",
 				dev_name(dev), error);
+			dpm_save_failed_dev(dev_name(dev));
 			put_device(dev);
 			break;
 		}
