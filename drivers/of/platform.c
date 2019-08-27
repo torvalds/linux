8ef4df5ae1091af6c8aa65938a80f9ad9fa4460f
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2019-08-27 21:40:00 +0200

    Revert "of/platform: Fix device_links_supplier_sync_state_resume() warning"
    This reverts commit 5adf5781019dd21233d6d13ab0d78bf03a13d4f1.
    
    Based on a lot of email and in-person discussions, this patch series is
    being reworked to address a number of issues that were pointed out that
    needed to be taken care of before it should be merged.  It will be
    resubmitted with those changes hopefully soon.
    
    Cc: Frank Rowand <frowand.list@gmail.com>
    Cc: Saravana Kannan <saravanak@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/of/platform.c
+++ drivers/of/platform.c
@@ -723,8 +723,7 @@ arch_initcall_sync(of_platform_default_populate_init);
 
 static int __init of_platform_sync_state_init(void)
 {
-	if (of_have_populated_dt())
-		device_links_supplier_sync_state_resume();
+	device_links_supplier_sync_state_resume();
 	return 0;
 }
 late_initcall_sync(of_platform_sync_state_init);
