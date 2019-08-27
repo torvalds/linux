19feeee5c5af114f3cb8ad2772680b7a1d16f043
Suman Anna <s-anna@ti.com>
2019-08-26 19:14:51 -0500

    ARM: OMAP2+: Plug in device_enable/idle ops for IOMMUs
    The OMAP IOMMU driver requires the device_enable/idle platform
    data ops on all the IOMMU devices to be able to enable and disable
    the clocks after commit db8918f61d51 ("iommu/omap: streamline
    enable/disable through runtime pm callbacks"). Plug in these
    pdata ops for all the existing IOMMUs through pdata quirks to
    maintain functionality.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/pdata-quirks.c
+++ arch/arm/mach-omap2/pdata-quirks.c
@@ -89,6 +89,8 @@ static struct iommu_platform_data omap3_iommu_pdata = {
 	.reset_name = "mmu",
 	.assert_reset = omap_device_assert_hardreset,
 	.deassert_reset = omap_device_deassert_hardreset,
+	.device_enable = omap_device_enable,
+	.device_idle = omap_device_idle,
 };
 
 static int omap3_sbc_t3730_twl_callback(struct device *dev,
@@ -424,6 +426,8 @@ static struct iommu_platform_data omap4_iommu_pdata = {
 	.reset_name = "mmu_cache",
 	.assert_reset = omap_device_assert_hardreset,
 	.deassert_reset = omap_device_deassert_hardreset,
+	.device_enable = omap_device_enable,
+	.device_idle = omap_device_idle,
 };
 #endif
 
