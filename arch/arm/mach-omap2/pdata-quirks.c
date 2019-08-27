0af3e1a491dde06dfcb0b063d5b543dfebded0f9
Suman Anna <s-anna@ti.com>
2019-08-26 19:14:52 -0500

    ARM: OMAP2+: Add pdata for OMAP3 ISP IOMMU
    The OMAP3 ISP IOMMU does not have any reset lines, so it didn't
    need any pdata previously. The OMAP IOMMU driver now requires the
    platform data ops for device_enable/idle on all the IOMMU devices
    after commit db8918f61d51 ("iommu/omap: streamline enable/disable
    through runtime pm callbacks") to enable/disable the clocks properly
    and maintain the reference count and the omap_hwmod state machine.
    So, add these callbacks through iommu pdata quirks for the OMAP3
    ISP IOMMU.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/pdata-quirks.c
+++ arch/arm/mach-omap2/pdata-quirks.c
@@ -93,6 +93,11 @@ static struct iommu_platform_data omap3_iommu_pdata = {
 	.device_idle = omap_device_idle,
 };
 
+static struct iommu_platform_data omap3_iommu_isp_pdata = {
+	.device_enable = omap_device_enable,
+	.device_idle = omap_device_idle,
+};
+
 static int omap3_sbc_t3730_twl_callback(struct device *dev,
 					   unsigned gpio,
 					   unsigned ngpio)
@@ -621,6 +626,8 @@ static struct of_dev_auxdata omap_auxdata_lookup[] = {
 #ifdef CONFIG_ARCH_OMAP3
 	OF_DEV_AUXDATA("ti,omap2-iommu", 0x5d000000, "5d000000.mmu",
 		       &omap3_iommu_pdata),
+	OF_DEV_AUXDATA("ti,omap2-iommu", 0x480bd400, "480bd400.mmu",
+		       &omap3_iommu_isp_pdata),
 	OF_DEV_AUXDATA("ti,omap3-smartreflex-core", 0x480cb000,
 		       "480cb000.smartreflex", &omap_sr_pdata[OMAP_SR_CORE]),
 	OF_DEV_AUXDATA("ti,omap3-smartreflex-mpu-iva", 0x480c9000,
