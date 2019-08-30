0f1979b402df5f0dd86425830ddaa191d70f3655
Christoph Hellwig <hch@lst.de>
2019-08-30 18:12:35 +0200

    m68k: Remove ioremap_fullcache()
    No callers of this function.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20190830161237.23033-2-hch@lst.de
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- arch/m68k/include/asm/kmap.h
+++ arch/m68k/include/asm/kmap.h
@@ -37,13 +37,6 @@ static inline void __iomem *ioremap_wt(unsigned long physaddr,
 	return __ioremap(physaddr, size, IOMAP_WRITETHROUGH);
 }
 
-#define ioremap_fullcache ioremap_fullcache
-static inline void __iomem *ioremap_fullcache(unsigned long physaddr,
-					      unsigned long size)
-{
-	return __ioremap(physaddr, size, IOMAP_FULL_CACHING);
-}
-
 #define memset_io memset_io
 static inline void memset_io(volatile void __iomem *addr, unsigned char val,
 			     int count)
