58985a9d2d03e977db93bf574a16162766a318fe
Valdis Klētnieks <valdis.kletnieks@vt.edu>
2019-08-30 12:42:39 -0400

    drivers/staging/exfat - by default, prohibit mount of fat/vfat
    Concerns have been raised about the exfat driver accidentally mounting
    fat/vfat file systems.  Add an extra configure option to help prevent that.
    
    Signed-off-by: Valdis Kletnieks <valdis.kletnieks@vt.edu>
    Link: https://lore.kernel.org/r/245727.1567183359@turing-police
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/exfat/exfat_super.c
+++ drivers/staging/exfat/exfat_super.c
@@ -486,10 +486,16 @@ static int ffsMountVol(struct super_block *sb)
 			break;
 
 	if (i < 53) {
+#ifdef CONFIG_EXFAT_DONT_MOUNT_VFAT
+		ret = -EINVAL;
+		printk(KERN_INFO "EXFAT: Attempted to mount VFAT filesystem\n");
+		goto out;
+#else
 		if (GET16(p_pbr->bpb + 11)) /* num_fat_sectors */
 			ret = fat16_mount(sb, p_pbr);
 		else
 			ret = fat32_mount(sb, p_pbr);
+#endif
 	} else {
 		ret = exfat_mount(sb, p_pbr);
 	}
