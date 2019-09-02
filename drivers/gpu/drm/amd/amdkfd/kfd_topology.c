f5d843d4ea3b8738a42a4b22432ce95f1df7ca51
Huang Rui <ray.huang@amd.com>
2019-09-02 23:25:29 +0800

    drm/amdkfd: add renoir kfd topology
    This patch adds renoir kfd topology which is the same with Raven.
    
    Signed-off-by: Huang Rui <ray.huang@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_topology.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_topology.c
@@ -1317,6 +1317,7 @@ int kfd_topology_add_device(struct kfd_dev *gpu)
 	case CHIP_VEGA12:
 	case CHIP_VEGA20:
 	case CHIP_RAVEN:
+	case CHIP_RENOIR:
 	case CHIP_ARCTURUS:
 	case CHIP_NAVI10:
 	case CHIP_NAVI14:
