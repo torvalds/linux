373f9092df9556685174aeec9ac5658a8d3bff72
Juliet Kim <julietk@linux.vnet.ibm.com>
2019-09-05 17:30:01 -0400

    net/ibmvnic: free reset work of removed device from queue
    [ Upstream commit 1c2977c094998de032fee6e898c88b4a05483d08 ]
    
    Commit 36f1031c51a2 ("ibmvnic: Do not process reset during or after
     device removal") made the change to exit reset if the driver has been
    removed, but does not free reset work items of the adapter from queue.
    
    Ensure all reset work items are freed when breaking out of the loop early.
    
    Fixes: 36f1031c51a2 ("ibmnvic: Do not process reset during or after device removal”)
    Signed-off-by: Juliet Kim <julietk@linux.vnet.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/ibm/ibmvnic.c
+++ drivers/net/ethernet/ibm/ibmvnic.c
@@ -2000,7 +2000,10 @@ static void __ibmvnic_reset(struct work_struct *work)
 	while (rwi) {
 		if (adapter->state == VNIC_REMOVING ||
 		    adapter->state == VNIC_REMOVED)
-			goto out;
+			kfree(rwi);
+			rc = EBUSY;
+			break;
+		}
 
 		if (adapter->force_reset_recovery) {
 			adapter->force_reset_recovery = false;
@@ -2026,7 +2029,7 @@ static void __ibmvnic_reset(struct work_struct *work)
 		netdev_dbg(adapter->netdev, "Reset failed\n");
 		free_all_rwi(adapter);
 	}
-out:
+
 	adapter->resetting = false;
 	if (we_lock_rtnl)
 		rtnl_unlock();
