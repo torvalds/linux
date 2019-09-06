9415743e4c8a029bfa46654e3a0a681a8db473f8
Abhishek Shah <abhishek.shah@broadcom.com>
2019-09-06 09:28:13 +0530

    PCI: iproc: Invalidate PAXB address mapping before programming it
    Invalidate PAXB inbound/outbound address mapping on probe before
    programming it.
    
    Kernel relies on outbound/inbound windows VALID bit in OARR registers to
    detect if a window was programmed and if it is set it does not overwrite
    it.
    
    This causes issues on soft reboot (eg kexec) since the host controller
    does not go through a HW reset on softboot so the kernel detects valid
    outbound/inbound windows configuration and is not able to reprogramme
    it as expected.
    
    Therefore, in order to make sure outbound/inbound windows are
    reprogrammed on soft reboot (eg kexec), invalidate memory windows on
    each probe to fix the issue.
    
    Signed-off-by: Abhishek Shah <abhishek.shah@broadcom.com>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Reviewed-by: Ray Jui <ray.jui@broadcom.com>
    Reviewed-by: Andrew Murray <andrew.murray@arm.com>

--- drivers/pci/controller/pcie-iproc.c
+++ drivers/pci/controller/pcie-iproc.c
@@ -1245,6 +1245,32 @@ out:
 	return ret;
 }
 
+static void iproc_pcie_invalidate_mapping(struct iproc_pcie *pcie)
+{
+	struct iproc_pcie_ib *ib = &pcie->ib;
+	struct iproc_pcie_ob *ob = &pcie->ob;
+	int idx;
+
+	if (pcie->ep_is_internal)
+		return;
+
+	if (pcie->need_ob_cfg) {
+		/* iterate through all OARR mapping regions */
+		for (idx = ob->nr_windows - 1; idx >= 0; idx--) {
+			iproc_pcie_write_reg(pcie,
+					     MAP_REG(IPROC_PCIE_OARR0, idx), 0);
+		}
+	}
+
+	if (pcie->need_ib_cfg) {
+		/* iterate through all IARR mapping regions */
+		for (idx = 0; idx < ib->nr_regions; idx++) {
+			iproc_pcie_write_reg(pcie,
+					     MAP_REG(IPROC_PCIE_IARR0, idx), 0);
+		}
+	}
+}
+
 static int iproce_pcie_get_msi(struct iproc_pcie *pcie,
 			       struct device_node *msi_node,
 			       u64 *msi_addr)
@@ -1516,6 +1542,8 @@ int iproc_pcie_setup(struct iproc_pcie *pcie, struct list_head *res)
 
 	iproc_pcie_perst_ctrl(pcie, true);
 	iproc_pcie_perst_ctrl(pcie, false);
+
+	iproc_pcie_invalidate_mapping(pcie);
 
 	if (pcie->need_ob_cfg) {
 		ret = iproc_pcie_map_ranges(pcie, res);
