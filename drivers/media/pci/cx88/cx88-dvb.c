e6f45ea2e7211850d2301a726d195a61fde0ccda
Daniel Gonzalez Cabanelas <dgcbueu@gmail.com>
2019-09-11 14:57:06 -0300

    media: cx88: Add support for NotOnlyTV LV3H card
    Add support for the PCI hybrid card NotOnlyTV LV3H. This card consists of:
    - A/V Controller: Conexant CX-23883
    - Tuner: Xceive XC3028L
    - DVB-T Decoder: Zarlink ZL10353
    
    This patch adds support for DVB-T,  Analog TV, FM radio, composite and
    S-video inputs. The IR input isn't supported.
    
    Since the PCI subsystem ID (14f1:8852) is the same as the Geniatech
    X8000-MT, but they are different boards, the card is only supported via
    insmod option: options cx88xx card=91
    
    For the record this is the eeprom dump, useful if someone wanted to
    implement the card auto detection:
    
    07 ff ff ff f1 14 52 88 04 04 32 55 f8 00 a2 02
    a1 00 40 63 06 11 44 30 03 df 40 80 00 20 00 73
    3c 10 00 00 00 00 ff ff ff ff ff ff ff ff ff ff
    ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
    ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
    ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
    ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
    ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
    ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
    ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
    ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
    ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
    ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
    ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
    ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
    ff ff ff ff ff ff ff ff ff ff ff ff ff ff a0 00
    
    Signed-off-by: Daniel Gonzalez Cabanelas <dgcbueu@gmail.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/pci/cx88/cx88-dvb.c
+++ drivers/media/pci/cx88/cx88-dvb.c
@@ -1378,6 +1378,7 @@ static int dvb_register(struct cx8802_dev *dev)
 				fe->ops.tuner_ops.set_config(fe, &ctl);
 		}
 		break;
+	case CX88_BOARD_NOTONLYTV_LV3H:
 	case CX88_BOARD_PINNACLE_HYBRID_PCTV:
 	case CX88_BOARD_WINFAST_DTV1800H:
 		fe0->dvb.frontend = dvb_attach(zl10353_attach,
