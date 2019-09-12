f39b683d35dfa93a58f1b400a8ec0ff81296b37c
Steffen Klassert <steffen.klassert@secunet.com>
2019-09-12 13:01:44 +0200

    ixgbe: Fix secpath usage for IPsec TX offload.
    The ixgbe driver currently does IPsec TX offloading
    based on an existing secpath. However, the secpath
    can also come from the RX side, in this case it is
    misinterpreted for TX offload and the packets are
    dropped with a "bad sa_idx" error. Fix this by using
    the xfrm_offload() function to test for TX offload.
    
    Fixes: 592594704761 ("ixgbe: process the Tx ipsec offload")
    Reported-by: Michael Marley <michael@michaelmarley.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -36,6 +36,7 @@
 #include <net/vxlan.h>
 #include <net/mpls.h>
 #include <net/xdp_sock.h>
+#include <net/xfrm.h>
 
 #include "ixgbe.h"
 #include "ixgbe_common.h"
@@ -8697,7 +8698,7 @@ netdev_tx_t ixgbe_xmit_frame_ring(struct sk_buff *skb,
 #endif /* IXGBE_FCOE */
 
 #ifdef CONFIG_IXGBE_IPSEC
-	if (secpath_exists(skb) &&
+	if (xfrm_offload(skb) &&
 	    !ixgbe_ipsec_tx(tx_ring, first, &ipsec_tx))
 		goto out_drop;
 #endif
