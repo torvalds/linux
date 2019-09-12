929deb849b9e4319015070ead7ca976a4f16e303
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2019-09-12 13:39:32 +0900

    ASoC: soc-core: create rtd->codec_dais first
    soc_new_pcm_runtime() allocs rtd and rtd->codec_dais.
    This patch allocs both first, and setup these after that.
    This is prepare for soc_new_pcm_runtime() cleanup.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://lore.kernel.org/r/87blvqf7qz.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -372,12 +372,6 @@ static struct snd_soc_pcm_runtime *soc_new_pcm_runtime(
 	if (!rtd)
 		return NULL;
 
-	INIT_LIST_HEAD(&rtd->component_list);
-	rtd->card = card;
-	rtd->dai_link = dai_link;
-	if (!rtd->dai_link->ops)
-		rtd->dai_link->ops = &null_snd_soc_ops;
-
 	rtd->codec_dais = kcalloc(dai_link->num_codecs,
 					sizeof(struct snd_soc_dai *),
 					GFP_KERNEL);
@@ -385,6 +379,12 @@ static struct snd_soc_pcm_runtime *soc_new_pcm_runtime(
 		kfree(rtd);
 		return NULL;
 	}
+
+	INIT_LIST_HEAD(&rtd->component_list);
+	rtd->card = card;
+	rtd->dai_link = dai_link;
+	if (!rtd->dai_link->ops)
+		rtd->dai_link->ops = &null_snd_soc_ops;
 
 	/* see for_each_card_rtds */
 	list_add_tail(&rtd->list, &card->rtd_list);
