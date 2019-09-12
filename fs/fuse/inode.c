f22f812d5ce75a18b56073a7a63862e6ea764070
Miklos Szeredi <mszeredi@redhat.com>
2019-09-12 14:28:13 +0200

    fuse: fix request limit
    The size of struct fuse_req was reduced from 392B to 144B on a non-debug
    config, thus the sanitize_global_limit() helper was setting a larger
    default limit.  This doesn't really reflect reduction in the memory used by
    requests, since the fields removed from fuse_req were added to fuse_args
    derived structs; e.g. sizeof(struct fuse_writepages_args) is 248B, thus
    resulting in slightly more memory being used for writepage requests
    overalll (due to using 256B slabs).
    
    Make the calculatation ignore the size of fuse_req and use the old 392B
    value.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -826,9 +826,12 @@ static const struct super_operations fuse_super_operations = {
 
 static void sanitize_global_limit(unsigned *limit)
 {
+	/*
+	 * The default maximum number of async requests is calculated to consume
+	 * 1/2^13 of the total memory, assuming 392 bytes per request.
+	 */
 	if (*limit == 0)
-		*limit = ((totalram_pages() << PAGE_SHIFT) >> 13) /
-			 sizeof(struct fuse_req);
+		*limit = ((totalram_pages() << PAGE_SHIFT) >> 13) / 392;
 
 	if (*limit >= 1 << 16)
 		*limit = (1 << 16) - 1;
