ee73e2795b416b829f0e00e7c43154922dff495b
Chris Wilson <chris@chris-wilson.co.uk>
2019-09-12 14:23:13 +0100

    drm/i915/tgl: Disable preemption while being debugged
    We see failures where the context continues executing past a
    preemption event, eventually leading to situations where a request has
    executed before we have event submitted it to HW! It seems like tgl is
    ignoring our RING_TAIL updates, but more likely is that there is a
    missing update required for our semaphore waits around preemption.
    
    v2: And disable internal semaphore usage
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Acked-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190912132313.12751-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_pci.c
+++ drivers/gpu/drm/i915/i915_pci.c
@@ -798,6 +798,7 @@ static const struct intel_device_info intel_tigerlake_12_info = {
 	.engine_mask =
 		BIT(RCS0) | BIT(BCS0) | BIT(VECS0) | BIT(VCS0) | BIT(VCS2),
 	.has_rc6 = false, /* XXX disabled for debugging */
+	.has_logical_ring_preemption = false, /* XXX disabled for debugging */
 };
 
 #undef GEN
