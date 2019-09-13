88ae25e46cbe9980ad2029c287ec22426d44c532
Gokul Sriram Palanisamy <gokulsri@codeaurora.org>
2019-09-13 17:26:08 +0530

    mailbox: qcom: Add support for IPQ8074 APCS
    Add support of IPQ8074 with IPC register offset as 8.
    
    Signed-off-by: Gokul Sriram Palanisamy <gokulsri@codeaurora.org>
    Signed-off-by: Sricharan R <sricharan@codeaurora.org>
    Signed-off-by: Jassi Brar <jaswinder.singh@linaro.org>

--- drivers/mailbox/qcom-apcs-ipc-mailbox.c
+++ drivers/mailbox/qcom-apcs-ipc-mailbox.c
@@ -126,6 +126,7 @@ static const struct of_device_id qcom_apcs_ipc_of_match[] = {
 	{ .compatible = "qcom,sc7180-apss-shared", .data = (void *)12 },
 	{ .compatible = "qcom,sdm845-apss-shared", .data = (void *)12 },
 	{ .compatible = "qcom,sm8150-apss-shared", .data = (void *)12 },
+	{ .compatible = "qcom,ipq8074-apcs-apps-global", .data = (void *)8 },
 	{}
 };
 MODULE_DEVICE_TABLE(of, qcom_apcs_ipc_of_match);
