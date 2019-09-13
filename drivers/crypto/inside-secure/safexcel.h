7468ab22d7ab968b99c75552c879cb5d96e50523
Pascal van Leeuwen <pascalvanl@gmail.com>
2019-09-13 11:10:40 +0200

    crypto: inside-secure - Add support for the cfb(sm4) skcipher
    This patch adds support for SM4 in CFB mode, i.e. skcipher cfb(sm4).
    
    changes since v1:
    - nothing
    
    Signed-off-by: Pascal van Leeuwen <pvanleeuwen@verimatrix.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/inside-secure/safexcel.h
+++ drivers/crypto/inside-secure/safexcel.h
@@ -882,5 +882,6 @@ extern struct safexcel_alg_template safexcel_alg_hmac_sm3;
 extern struct safexcel_alg_template safexcel_alg_ecb_sm4;
 extern struct safexcel_alg_template safexcel_alg_cbc_sm4;
 extern struct safexcel_alg_template safexcel_alg_ofb_sm4;
+extern struct safexcel_alg_template safexcel_alg_cfb_sm4;
 
 #endif
