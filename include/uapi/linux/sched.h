61129dd29f7962f278b618a2a3e8fdb986a66dc8
Seth Forshee <seth.forshee@canonical.com>
2019-09-17 09:18:53 +0200

    sched: Add __ASSEMBLY__ guards around struct clone_args
    The addition of struct clone_args to uapi/linux/sched.h is not protected
    by __ASSEMBLY__ guards, causing a failure to build from source for glibc
    on RISC-V. Add the guards to fix this.
    
    Fixes: 7f192e3cd316 ("fork: add clone3")
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>
    Cc: <stable@vger.kernel.org>
    Acked-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20190917071853.12385-1-seth.forshee@canonical.com
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- include/uapi/linux/sched.h
+++ include/uapi/linux/sched.h
@@ -33,6 +33,7 @@
 #define CLONE_NEWNET		0x40000000	/* New network namespace */
 #define CLONE_IO		0x80000000	/* Clone io context */
 
+#ifndef __ASSEMBLY__
 /*
  * Arguments for the clone3 syscall
  */
@@ -46,6 +47,7 @@ struct clone_args {
 	__aligned_u64 stack_size;
 	__aligned_u64 tls;
 };
+#endif
 
 /*
  * Scheduling policies
