42fd8baab31f53bed2952485fcf0e92f244c5e55
Qian Cai <cai@lca.pw>
2019-09-17 10:34:54 -0400

    sched/core: Convert vcpu_is_preempted() from macro to an inline function
    Clang reports this warning:
    
      kernel/locking/osq_lock.c:25:19: warning: unused function 'node_cpu' [-Wunused-function]
    
    due to osq_lock() calling vcpu_is_preempted(node_cpu(node->prev))), but
    vcpu_is_preempted() is compiled away. Fix it by converting the dummy
    vcpu_is_preempted() from a macro to a proper static inline function.
    
    Signed-off-by: Qian Cai <cai@lca.pw>
    Acked-by: Mel Gorman <mgorman@suse.de>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: bsegall@google.com
    Cc: dietmar.eggemann@arm.com
    Cc: juri.lelli@redhat.com
    Cc: rostedt@goodmis.org
    Cc: vincent.guittot@linaro.org
    Link: https://lkml.kernel.org/r/1568730894-10483-1-git-send-email-cai@lca.pw
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1856,7 +1856,10 @@ static inline void set_task_cpu(struct task_struct *p, unsigned int cpu)
  * running or not.
  */
 #ifndef vcpu_is_preempted
-# define vcpu_is_preempted(cpu)	false
+static inline bool vcpu_is_preempted(int cpu)
+{
+	return false;
+}
 #endif
 
 extern long sched_setaffinity(pid_t pid, const struct cpumask *new_mask);
