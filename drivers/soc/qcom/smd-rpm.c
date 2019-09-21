83a81c1b8690310b98f0804aef134c4318234866
Angelo G. Del Regno <kholk11@gmail.com>
2019-09-21 12:12:05 +0200

    soc: qcom: smd-rpm: Add MSM8976 compatible
    Add a compatible for the RPM on the Qualcomm MSM8976 platform:
    this is also valid for MSM8956 and their APQ variants.
    
    Signed-off-by: Angelo G. Del Regno <kholk11@gmail.com>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/soc/qcom/smd-rpm.c
+++ drivers/soc/qcom/smd-rpm.c
@@ -232,6 +232,7 @@ static const struct of_device_id qcom_smd_rpm_of_match[] = {
 	{ .compatible = "qcom,rpm-apq8084" },
 	{ .compatible = "qcom,rpm-msm8916" },
 	{ .compatible = "qcom,rpm-msm8974" },
+	{ .compatible = "qcom,rpm-msm8976" },
 	{ .compatible = "qcom,rpm-msm8996" },
 	{ .compatible = "qcom,rpm-msm8998" },
 	{ .compatible = "qcom,rpm-sdm660" },
