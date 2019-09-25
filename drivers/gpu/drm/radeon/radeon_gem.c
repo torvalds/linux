d224d76f24c5bd4d53a4d504b8ab39aab15036aa
Andrey Konovalov <andreyknvl@google.com>
2019-09-25 16:48:51 -0700

    UPSTREAM: drm/radeon: untag user pointers in radeon_gem_userptr_ioctl
    (Upstream commit 4fdfae8d8f855d79b7d83fcd590b6ac7ed0099cf).
    
    This patch is a part of a series that extends kernel ABI to allow to pass
    tagged user pointers (with the top byte set to something else other than
    0x00) as syscall arguments.
    
    In radeon_gem_userptr_ioctl() an MMU notifier is set up with a (tagged)
    userspace pointer.  The untagged address should be used so that MMU
    notifiers for the untagged address get correctly matched up with the right
    BO.  This funcation also calls radeon_ttm_tt_pin_userptr(), which uses
    provided user pointers for vma lookups, which can only by done with
    untagged pointers.
    
    This patch untags user pointers in radeon_gem_userptr_ioctl().
    
    Link: http://lkml.kernel.org/r/c856babeb67195b35603b8d5ba386a2819cec5ff.1563904656.git.andreyknvl@google.com
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Reviewed-by: Khalid Aziz <khalid.aziz@oracle.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Suggested-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Acked-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: Eric Auger <eric.auger@redhat.com>
    Cc: Jens Wiklander <jens.wiklander@linaro.org>
    Cc: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
    Cc: Mike Rapoport <rppt@linux.ibm.com>
    Cc: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Bug: 135692346
    Change-Id: I7b33a1e1994b588c307cf5d909233de9bc0abddc

--- drivers/gpu/drm/radeon/radeon_gem.c
+++ drivers/gpu/drm/radeon/radeon_gem.c
@@ -291,6 +291,8 @@ int radeon_gem_userptr_ioctl(struct drm_device *dev, void *data,
 	uint32_t handle;
 	int r;
 
+	args->addr = untagged_addr(args->addr);
+
 	if (offset_in_page(args->addr | args->size))
 		return -EINVAL;
 
