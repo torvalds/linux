a0d46f7a0fa5e2a447a25c87612040babf7a3e18
Denis Efremov <efremov@linux.com>
2019-09-25 23:58:58 +0300

    rtlwifi: Remove excessive check in _rtl_ps_inactive_ps()
    There is no need to check "rtlhal->interface == INTF_PCI" twice in
    _rtl_ps_inactive_ps(). The nested check is always true. Thus, the
    expression can be simplified.
    
    Signed-off-by: Denis Efremov <efremov@linux.com>
    Acked-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtlwifi/ps.c
+++ drivers/net/wireless/realtek/rtlwifi/ps.c
@@ -161,8 +161,7 @@ static void _rtl_ps_inactive_ps(struct ieee80211_hw *hw)
 	if (ppsc->inactive_pwrstate == ERFON &&
 	    rtlhal->interface == INTF_PCI) {
 		if ((ppsc->reg_rfps_level & RT_RF_OFF_LEVL_ASPM) &&
-		    RT_IN_PS_LEVEL(ppsc, RT_PS_LEVEL_ASPM) &&
-		    rtlhal->interface == INTF_PCI) {
+		    RT_IN_PS_LEVEL(ppsc, RT_PS_LEVEL_ASPM)) {
 			rtlpriv->intf_ops->disable_aspm(hw);
 			RT_CLEAR_PS_LEVEL(ppsc, RT_PS_LEVEL_ASPM);
 		}
