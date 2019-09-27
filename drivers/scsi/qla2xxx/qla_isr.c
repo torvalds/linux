9bc6157f5fd0e898c94f3018d088a3419bde0d8f
Daniel Wagner <dwagner@suse.de>
2019-09-27 09:30:31 +0200

    scsi: qla2xxx: Remove WARN_ON_ONCE in qla2x00_status_cont_entry()
    Commit 88263208dd23 ("scsi: qla2xxx: Complain if sp->done() is not called
    from the completion path") introduced the WARN_ON_ONCE in
    qla2x00_status_cont_entry(). The assumption was that there is only one
    status continuations element. According to the firmware documentation it is
    possible that multiple status continuations are emitted by the firmware.
    
    Fixes: 88263208dd23 ("scsi: qla2xxx: Complain if sp->done() is not called from the completion path")
    Link: https://lore.kernel.org/r/20190927073031.62296-1-dwagner@suse.de
    Cc: Bart Van Assche <bvanassche@acm.org>
    Reviewed-by: Hannes Reinecke <hare@suse.com>
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Daniel Wagner <dwagner@suse.de>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/qla2xxx/qla_isr.c
+++ drivers/scsi/qla2xxx/qla_isr.c
@@ -2837,8 +2837,6 @@ qla2x00_status_cont_entry(struct rsp_que *rsp, sts_cont_entry_t *pkt)
 	if (sense_len == 0) {
 		rsp->status_srb = NULL;
 		sp->done(sp, cp->result);
-	} else {
-		WARN_ON_ONCE(true);
 	}
 }
 
