12cc923f1ccc1df467e046b02a72c2b3b321b6a2
Romain Perier <romain.perier@gmail.com>
2019-09-29 18:30:13 +0200

    tasklet: Introduce new initialization API
    Nowadays, modern kernel subsystems that use callbacks pass the data
    structure associated with a given callback as argument to the callback.
    The tasklet subsystem remains one which passes an arbitrary unsigned
    long to the callback function. This has several problems:
    
    - This keeps an extra field for storing the argument in each tasklet
      data structure, it bloats the tasklet_struct structure with a redundant
      .data field
    
    - No type checking can be performed on this argument. Instead of
      using container_of() like other callback subsystems, it forces callbacks
      to do explicit type cast of the unsigned long argument into the required
      object type.
    
    - Buffer overflows can overwrite the .func and the .data field, so
      an attacker can easily overwrite the function and its first argument
      to whatever it wants.
    
    Add a new tasklet initialization API, via DECLARE_TASKLET() and
    tasklet_setup(), which will replace the existing ones.
    
    This work is greatly inspired by the timer_struct conversion series,
    see commit e99e88a9d2b0 ("treewide: setup_timer() -> timer_setup()")
    
    To avoid problems with both -Wcast-function-type (which is enabled in
    the kernel via -Wextra is several subsystems), and with mismatched
    function prototypes when build with Control Flow Integrity enabled,
    this adds the "use_callback" member to let the tasklet caller choose
    which union member to call through. Once all old API uses are removed,
    this and the .data member will be removed as well. (On 64-bit this does
    not grow the struct size as the new member fills the hole after atomic_t,
    which is also "int" sized.)
    
    Signed-off-by: Romain Perier <romain.perier@gmail.com>
    Co-developed-by: Allen Pais <allen.lkml@gmail.com>
    Signed-off-by: Allen Pais <allen.lkml@gmail.com>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Co-developed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -547,7 +547,10 @@ static void tasklet_action_common(struct softirq_action *a,
 				if (!test_and_clear_bit(TASKLET_STATE_SCHED,
 							&t->state))
 					BUG();
-				t->func(t->data);
+				if (t->use_callback)
+					t->callback(t);
+				else
+					t->func(t->data);
 				tasklet_unlock(t);
 				continue;
 			}
@@ -573,6 +576,18 @@ static __latent_entropy void tasklet_hi_action(struct softirq_action *a)
 	tasklet_action_common(a, this_cpu_ptr(&tasklet_hi_vec), HI_SOFTIRQ);
 }
 
+void tasklet_setup(struct tasklet_struct *t,
+		   void (*callback)(struct tasklet_struct *))
+{
+	t->next = NULL;
+	t->state = 0;
+	atomic_set(&t->count, 0);
+	t->callback = callback;
+	t->use_callback = true;
+	t->data = 0;
+}
+EXPORT_SYMBOL(tasklet_setup);
+
 void tasklet_init(struct tasklet_struct *t,
 		  void (*func)(unsigned long), unsigned long data)
 {
@@ -580,6 +595,7 @@ void tasklet_init(struct tasklet_struct *t,
 	t->state = 0;
 	atomic_set(&t->count, 0);
 	t->func = func;
+	t->use_callback = false;
 	t->data = data;
 }
 EXPORT_SYMBOL(tasklet_init);
