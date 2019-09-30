7cd4cb94cf4f3bfd070d78714d4fe249700250a8
YueHaibing <yuehaibing@huawei.com>
2019-09-30 17:43:27 +0800

    scsi: bfa: Make restart_bfa static
    Fix sparse warning:
    
    drivers/scsi/bfa/bfad.c:1491:1: warning:
     symbol 'restart_bfa' was not declared. Should it be static?
    
    Link: https://lore.kernel.org/r/20190930094327.46836-1-yuehaibing@huawei.com
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/bfa/bfad.c
+++ drivers/scsi/bfa/bfad.c
@@ -1487,8 +1487,7 @@ bfad_pci_error_detected(struct pci_dev *pdev, pci_channel_state_t state)
 	return ret;
 }
 
-int
-restart_bfa(struct bfad_s *bfad)
+static int restart_bfa(struct bfad_s *bfad)
 {
 	unsigned long flags;
 	struct pci_dev *pdev = bfad->pcidev;
