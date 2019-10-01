5c56119031734d156fd064a2677c7aaa38f54138
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2019-10-01 15:10:42 +0300

    drm/i915: extend audio CDCLK>=2*BCLK constraint to more platforms
    The CDCLK>=2*BCLK constraint applies to all generations since gen10.
    Extend the constraint logic in audio get/put_power().
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

--- drivers/gpu/drm/i915/display/intel_audio.c
+++ drivers/gpu/drm/i915/display/intel_audio.c
@@ -852,7 +852,7 @@ static unsigned long i915_audio_component_get_power(struct device *kdev)
 
 	/* Force CDCLK to 2*BCLK as long as we need audio to be powered. */
 	if (dev_priv->audio_power_refcount++ == 0) {
-		if (IS_CANNONLAKE(dev_priv) || IS_GEMINILAKE(dev_priv))
+		if (INTEL_GEN(dev_priv) >= 10 || IS_GEMINILAKE(dev_priv))
 			glk_force_audio_cdclk(dev_priv, true);
 
 		if (INTEL_GEN(dev_priv) >= 10 || IS_GEMINILAKE(dev_priv))
@@ -871,7 +871,7 @@ static void i915_audio_component_put_power(struct device *kdev,
 
 	/* Stop forcing CDCLK to 2*BCLK if no need for audio to be powered. */
 	if (--dev_priv->audio_power_refcount == 0)
-		if (IS_CANNONLAKE(dev_priv) || IS_GEMINILAKE(dev_priv))
+		if (INTEL_GEN(dev_priv) >= 10 || IS_GEMINILAKE(dev_priv))
 			glk_force_audio_cdclk(dev_priv, false);
 
 	intel_display_power_put(dev_priv, POWER_DOMAIN_AUDIO, cookie);
