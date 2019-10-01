5be5515a8ea198de6eb204a0ff25faf98b8ff719
Julio Faracco <jcfaracco@gmail.com>
2019-10-01 11:39:04 -0300

    net: core: dev: replace state xoff flag comparison by netif_xmit_stopped method
    Function netif_schedule_queue() has a hardcoded comparison between queue
    state and any xoff flag. This comparison does the same thing as method
    netif_xmit_stopped(). In terms of code clarity, it is better. See other
    methods like: generic_xdp_tx() and dev_direct_xmit().
    
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -2771,7 +2771,7 @@ static struct dev_kfree_skb_cb *get_kfree_skb_cb(const struct sk_buff *skb)
 void netif_schedule_queue(struct netdev_queue *txq)
 {
 	rcu_read_lock();
-	if (!(txq->state & QUEUE_STATE_ANY_XOFF)) {
+	if (!netif_xmit_stopped(txq)) {
 		struct Qdisc *q = rcu_dereference(txq->qdisc);
 
 		__netif_schedule(q);
