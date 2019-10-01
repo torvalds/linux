adc311a5bbf643eef6702006c03d34d827392f05
Luca Coelho <luciano.coelho@intel.com>
2019-10-01 09:38:08 +0300

    iwlwifi: bump FW API to 53 for 22000 series
    Start supporting API version 53 for 22000 series.
    
    Change-Id: I5725e46394f3f53c3069723fd513cc53c7df383d
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/cfg/22000.c
+++ drivers/net/wireless/intel/iwlwifi/cfg/22000.c
@@ -57,7 +57,7 @@
 #include "iwl-prph.h"
 
 /* Highest firmware API version supported */
-#define IWL_22000_UCODE_API_MAX	52
+#define IWL_22000_UCODE_API_MAX	53
 
 /* Lowest firmware API version supported */
 #define IWL_22000_UCODE_API_MIN	39
