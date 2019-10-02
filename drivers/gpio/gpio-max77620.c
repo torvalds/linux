579249a08348442c09ddffdf459ac2290e077b4e
Thierry Reding <treding@nvidia.com>
2019-10-02 14:28:23 +0200

    gpio: max77620: Use correct unit for debounce times
    [ Upstream commit fffa6af94894126994a7600c6f6f09b892e89fa9 ]
    
    The gpiod_set_debounce() function takes the debounce time in
    microseconds. Adjust the switch/case values in the MAX77620 GPIO to use
    the correct unit.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Link: https://lore.kernel.org/r/20191002122825.3948322-1-thierry.reding@gmail.com
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpio/gpio-max77620.c
+++ drivers/gpio/gpio-max77620.c
@@ -163,13 +163,13 @@ static int max77620_gpio_set_debounce(struct max77620_gpio *mgpio,
 	case 0:
 		val = MAX77620_CNFG_GPIO_DBNC_None;
 		break;
-	case 1 ... 8:
+	case 1000 ... 8000:
 		val = MAX77620_CNFG_GPIO_DBNC_8ms;
 		break;
-	case 9 ... 16:
+	case 9000 ... 16000:
 		val = MAX77620_CNFG_GPIO_DBNC_16ms;
 		break;
-	case 17 ... 32:
+	case 17000 ... 32000:
 		val = MAX77620_CNFG_GPIO_DBNC_32ms;
 		break;
 	default:
