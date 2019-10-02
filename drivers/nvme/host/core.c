6abff1b9f7b8884a46b7bd80b49e7af0b5625aeb
Sagi Grimberg <sagi@grimberg.me>
2019-10-02 10:52:25 -0700

    nvme: fix possible deadlock when nvme_update_formats fails
    nvme_update_formats may fail to revalidate the namespace and
    attempt to remove the namespace. This may lead to a deadlock
    as nvme_ns_remove will attempt to acquire the subsystem lock
    which is already acquired by the passthru command with effects.
    
    Move the invalid namepsace removal to after the passthru command
    releases the subsystem lock.
    
    Reported-by: Judy Brock <judy.brock@samsung.com>
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -1306,8 +1306,6 @@ static void nvme_update_formats(struct nvme_ctrl *ctrl)
 		if (ns->disk && nvme_revalidate_disk(ns->disk))
 			nvme_set_queue_dying(ns);
 	up_read(&ctrl->namespaces_rwsem);
-
-	nvme_remove_invalid_namespaces(ctrl, NVME_NSID_ALL);
 }
 
 static void nvme_passthru_end(struct nvme_ctrl *ctrl, u32 effects)
@@ -1323,6 +1321,7 @@ static void nvme_passthru_end(struct nvme_ctrl *ctrl, u32 effects)
 		nvme_unfreeze(ctrl);
 		nvme_mpath_unfreeze(ctrl->subsys);
 		mutex_unlock(&ctrl->subsys->lock);
+		nvme_remove_invalid_namespaces(ctrl, NVME_NSID_ALL);
 		mutex_unlock(&ctrl->scan_lock);
 	}
 	if (effects & NVME_CMD_EFFECTS_CCC)
