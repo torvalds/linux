b07aa789c551cd719b597b5133dbacb0d3983272
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-10-04 19:01:33 -0500

    Revert "drm/i915: Sanitize the TypeC FIA lane configuration decoding"
    This reverts commit 2cb73a9e8fe67c881281590e5344fb218dcfeaee.

--- drivers/gpu/drm/i915/display/intel_tc.c
+++ drivers/gpu/drm/i915/display/intel_tc.c
@@ -51,16 +51,15 @@ int intel_tc_port_fia_max_lane_count(struct intel_digital_port *dig_port)
 	switch (lane_mask) {
 	default:
 		MISSING_CASE(lane_mask);
-		/* fall-through */
-	case 0x1:
-	case 0x2:
-	case 0x4:
-	case 0x8:
+	case 1:
+	case 2:
+	case 4:
+	case 8:
 		return 1;
-	case 0x3:
-	case 0xc:
+	case 3:
+	case 12:
 		return 2;
-	case 0xf:
+	case 15:
 		return 4;
 	}
 }
