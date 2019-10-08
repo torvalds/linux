3de1627851321fc5589b2a49bf52755a60c8276f
Chris Wilson <chris@chris-wilson.co.uk>
2019-10-08 08:11:21 +0100

    drm/i915/selftests: Assign the mock_engine->uncore shortcut
    Set up the engine->uncore shortcut on mock_engine creation.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191008071121.25088-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/gt/mock_engine.c
+++ drivers/gpu/drm/i915/gt/mock_engine.c
@@ -248,6 +248,7 @@ struct intel_engine_cs *mock_engine(struct drm_i915_private *i915,
 	/* minimal engine setup for requests */
 	engine->base.i915 = i915;
 	engine->base.gt = &i915->gt;
+	engine->base.uncore = i915->gt.uncore;
 	snprintf(engine->base.name, sizeof(engine->base.name), "%s", name);
 	engine->base.id = id;
 	engine->base.mask = BIT(id);
