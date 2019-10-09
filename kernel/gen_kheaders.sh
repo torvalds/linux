700dea5a0bea9f64eba89fae7cb2540326fdfdc1
Dmitry Goldin <dgoldin+lkml@protonmail.ch>
2019-10-09 13:42:14 +0000

    kheaders: substituting --sort in archive creation
    The option --sort=ORDER was only introduced in tar 1.28 (2014), which
    is rather new and might not be available in some setups.
    
    This patch tries to replicate the previous behaviour as closely as
    possible to fix the kheaders build for older environments. It does
    not produce identical archives compared to the previous version due
    to minor sorting differences but produces reproducible results itself
    in my tests.
    
    Reported-by: Andreas Schwab <schwab@suse.de>
    Signed-off-by: Dmitry Goldin <dgoldin+lkml@protonmail.ch>
    Tested-by: Andreas Schwab <schwab@suse.de>
    Tested-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- kernel/gen_kheaders.sh
+++ kernel/gen_kheaders.sh
@@ -71,10 +71,13 @@ done | cpio --quiet -pd $cpio_dir >/dev/null 2>&1
 find $cpio_dir -type f -print0 |
 	xargs -0 -P8 -n1 perl -pi -e 'BEGIN {undef $/;}; s/\/\*((?!SPDX).)*?\*\///smg;'
 
-# Create archive and try to normalize metadata for reproducibility
-tar "${KBUILD_BUILD_TIMESTAMP:+--mtime=$KBUILD_BUILD_TIMESTAMP}" \
-    --owner=0 --group=0 --sort=name --numeric-owner \
-    -Jcf $tarfile -C $cpio_dir/ . > /dev/null
+# Create archive and try to normalize metadata for reproducibility.
+# For compatibility with older versions of tar, files are fed to tar
+# pre-sorted, as --sort=name might not be available.
+find $cpio_dir -printf "./%P\n" | LC_ALL=C sort | \
+    tar "${KBUILD_BUILD_TIMESTAMP:+--mtime=$KBUILD_BUILD_TIMESTAMP}" \
+    --owner=0 --group=0 --numeric-owner --no-recursion \
+    -Jcf $tarfile -C $cpio_dir/ -T - > /dev/null
 
 echo "$src_files_md5" >  kernel/kheaders.md5
 echo "$obj_files_md5" >> kernel/kheaders.md5
