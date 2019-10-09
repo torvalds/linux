2b6ff0224cbc2b713299c5357f254cbb772fb25e
Eric Biggers <ebiggers@google.com>
2019-10-09 14:35:49 -0700

    ANDROID: sdcardfs: evict dentries on fscrypt key removal
    Use the fscrypt key removal notifier chain to make sdcardfs evict its
    dentries when an fscrypt key is about to be removed.  This is needed for
    the FS_IOC_REMOVE_ENCRYPTION_KEY ioctl to properly "lock" the encrypted
    files underneath sdcardfs when an Android user is stopped.
    
    Test: pm create-user 10
          am start-user 10
          find /data/media/10/    # filenames are in plaintext form
          am stop-user 10
          find /data/media/10/    # filenames are in ciphertext form
    
          (But currently the kernel and vold still warn about other files
          still being open, due to b/140762419)
    
    Bug: 120446149
    Bug: 142275883
    Change-Id: I83b451a2bc40c72fcd01d24aa5c34ad8de427534
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -21,6 +21,7 @@
 #include "sdcardfs.h"
 #include <linux/fs_context.h>
 #include <linux/fs_parser.h>
+#include <linux/fscrypt.h>
 #include <linux/module.h>
 #include <linux/types.h>
 #include <linux/parser.h>
@@ -293,6 +294,9 @@ static int __sdcardfs_fill_super(
 	list_add(&sb_info->list, &sdcardfs_super_list);
 	mutex_unlock(&sdcardfs_super_list_lock);
 
+	sb_info->fscrypt_nb.notifier_call = sdcardfs_on_fscrypt_key_removed;
+	fscrypt_register_key_removal_notifier(&sb_info->fscrypt_nb);
+
 	if (!(fc->sb_flags & SB_SILENT))
 		pr_info("sdcardfs: mounted on top of %s type %s\n",
 				dev_name, lower_sb->s_type->name);
@@ -331,6 +335,9 @@ void sdcardfs_kill_sb(struct super_block *sb)
 
 	if (sb->s_magic == SDCARDFS_SUPER_MAGIC && sb->s_fs_info) {
 		sbi = SDCARDFS_SB(sb);
+
+		fscrypt_unregister_key_removal_notifier(&sbi->fscrypt_nb);
+
 		mutex_lock(&sdcardfs_super_list_lock);
 		list_del(&sbi->list);
 		mutex_unlock(&sdcardfs_super_list_lock);
