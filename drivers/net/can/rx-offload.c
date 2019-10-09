a85ce0107d6bd14f78424de2fac4420d46f15d4e
Marc Kleine-Budde <mkl@pengutronix.de>
2019-10-09 15:15:07 +0200

    can: rx-offload: can_rx_offload_offload_one(): increment rx_fifo_errors on queue overflow or OOM
    [ Upstream commit 4e9016bee3bf0c24963097edace034ff205b565c ]
    
    If the rx-offload skb_queue is full or the skb allocation fails (due to OOM),
    the mailbox contents is discarded.
    
    This patch adds the incrementing of the rx_fifo_errors statistics counter.
    
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/can/rx-offload.c
+++ drivers/net/can/rx-offload.c
@@ -134,8 +134,10 @@ static struct sk_buff *can_rx_offload_offload_one(struct can_rx_offload *offload
 
 		ret = offload->mailbox_read(offload, &cf_overflow,
 					    &timestamp, n);
-		if (ret)
+		if (ret) {
 			offload->dev->stats.rx_dropped++;
+			offload->dev->stats.rx_fifo_errors++;
+		}
 
 		return NULL;
 	}
