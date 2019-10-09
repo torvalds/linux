c0981afd8cd6e7176b4b9fe2186087695463ebde
Wambui Karuga <wambui.karugax@gmail.com>
2019-10-09 20:07:03 +0300

    staging: kpc2000: Remove unnecessary return variable
    Remove unnecessary variable `val` in kp_spi_read_reg() that only holds
    the return value from readq().
    Issue found by coccinelle using the script:
    
    @@
    local idexpression ret;
    expression e;
    @@
    
    -ret =
    +return
         e;
    -return ret;
    
    Signed-off-by: Wambui Karuga <wambui.karugax@gmail.com>
    Acked-by: Julia Lawall <julia.lawall@lip6.fr>
    Link: https://lore.kernel.org/r/20191009170703.GA2869@wambui
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/kpc2000/kpc2000_spi.c
+++ drivers/staging/kpc2000/kpc2000_spi.c
@@ -162,14 +162,12 @@ union kp_spi_ffctrl {
 kp_spi_read_reg(struct kp_spi_controller_state *cs, int idx)
 {
 	u64 __iomem *addr = cs->base;
-	u64 val;
 
 	addr += idx;
 	if ((idx == KP_SPI_REG_CONFIG) && (cs->conf_cache >= 0))
 		return cs->conf_cache;
 
-	val = readq(addr);
-	return val;
+	return readq(addr);
 }
 
 	static inline void
