48d9b0d43105e0da2b7c135eedd24e51234fb5e4
Logan Gunthorpe <logang@deltatee.com>
2019-10-10 17:36:26 -0600

    block: account statistics for passthrough requests
    Presently, passthrough requests are not accounted for because
    blk_do_io_stat() expressly rejects them. Based on some digging
    in the history, this doesn't seem like a concious decision but
    one that evolved from the change from blk_fs_request() to
    blk_rq_is_passthrough().
    
    To support this, call blk_account_io_start() in blk_execute_rq_nowait()
    and remove the passthrough check in blk_do_io_stat().
    
    Link: https://lore.kernel.org/linux-block/20191010100526.GA27209@lst.de/
    Signed-off-by: Logan Gunthorpe <logang@deltatee.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk.h
+++ block/blk.h
@@ -242,14 +242,11 @@ int blk_dev_init(void);
  * Contribute to IO statistics IFF:
  *
  *	a) it's attached to a gendisk, and
- *	b) the queue had IO stats enabled when this request was started, and
- *	c) it's a file system request
+ *	b) the queue had IO stats enabled when this request was started
  */
 static inline bool blk_do_io_stat(struct request *rq)
 {
-	return rq->rq_disk &&
-	       (rq->rq_flags & RQF_IO_STAT) &&
-		!blk_rq_is_passthrough(rq);
+	return rq->rq_disk && (rq->rq_flags & RQF_IO_STAT);
 }
 
 static inline void req_set_nomerge(struct request_queue *q, struct request *req)
