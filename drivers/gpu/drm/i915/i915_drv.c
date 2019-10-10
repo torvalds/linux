2edb3de9e599e79db4028335df2a64f3ad014b2a
Ville Syrjälä <ville.syrjala@linux.intel.com>
2019-10-10 17:51:23 +0300

    drm/i915: Shrink eDRAM ways/sets arrays
    Make the ways/sets arrays static cosnt u8 to shrink things a bit.
    
        text	   data	    bss	    dec	    hex	filename
    -  23935	    629	    128	  24692	   6074	i915_drv.o
    +  23818	    629	    128	  24575	   5fff	i915_drv.o
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191010145127.7487-1-ville.syrjala@linux.intel.com
    Reviewed-by: Ramalingam C <ramalingam.c@intel.com>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -1073,8 +1073,8 @@ intel_get_dram_info(struct drm_i915_private *dev_priv)
 
 static u32 gen9_edram_size_mb(struct drm_i915_private *dev_priv, u32 cap)
 {
-	const unsigned int ways[8] = { 4, 8, 12, 16, 16, 16, 16, 16 };
-	const unsigned int sets[4] = { 1, 1, 2, 2 };
+	static const u8 ways[8] = { 4, 8, 12, 16, 16, 16, 16, 16 };
+	static const u8 sets[4] = { 1, 1, 2, 2 };
 
 	return EDRAM_NUM_BANKS(cap) *
 		ways[EDRAM_WAYS_IDX(cap)] *
