832392db9747b9c95724d37fc6a5dadd3d4ec514
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2019-10-11 13:22:58 +0300

    platform/x86: i2c-multi-instantiate: Fail the probe if no IRQ provided
    For APIC case of interrupt we don't fail a ->probe() of the driver,
    which makes kernel to print a lot of warnings from the children.
    
    We have two options here:
    - switch to platform_get_irq_optional(), though it won't stop children
      to be probed and failed
    - fail the ->probe() of i2c-multi-instantiate
    
    Since the in reality we never had devices in the wild where IRQ resource
    is optional, the latter solution suits the best.
    
    Fixes: 799d3379a672 ("platform/x86: i2c-multi-instantiate: Introduce IOAPIC IRQ support")
    Reported-by: Ammy Yi <ammy.yi@intel.com>
    Cc: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Cc: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>

--- drivers/platform/x86/i2c-multi-instantiate.c
+++ drivers/platform/x86/i2c-multi-instantiate.c
@@ -108,6 +108,7 @@ static int i2c_multi_inst_probe(struct platform_device *pdev)
 			if (ret < 0) {
 				dev_dbg(dev, "Error requesting irq at index %d: %d\n",
 					inst_data[i].irq_idx, ret);
+				goto error;
 			}
 			board_info.irq = ret;
 			break;
