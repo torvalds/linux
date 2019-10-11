d45df4b851b3e827e695db05337d0c768573e962
Mark Salyzyn <salyzyn@google.com>
2019-10-11 08:14:54 -0700

    ANDROID: GKI: export saved_command_line
    Some driver modules require the ability to parse the kernel command
    line outside the <modulename>.parm=<val> set on Android.  For example
    androidboot.console=.  Past attempts ('03) to upstream and export
    this key piece of content has come under resistance because
    preference has been to support the existing api __setup(str,func),
    which is only available to built-in, and push modules towards only
    using the documented kernel parameter interface cited above or to
    Device Tree and not react to any other out-of-band content.
    
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Test: compile
    Bug: 142496416
    Change-Id: Ia349dce6f7e467b40b9fd95e40f52ea9a8f7e51d

--- init/main.c
+++ init/main.c
@@ -133,6 +133,7 @@ void (*__initdata late_time_init)(void);
 char __initdata boot_command_line[COMMAND_LINE_SIZE];
 /* Untouched saved command line (eg. for /proc) */
 char *saved_command_line;
+EXPORT_SYMBOL_GPL(saved_command_line);
 /* Command line for parameter parsing */
 static char *static_command_line;
 /* Command line for per-initcall parameter parsing */
