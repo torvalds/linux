74f151bae8571acdafb38510b9b43bf08398003a
Shengfei Xu <xsf@rock-chips.com>
2019-10-14 10:55:36 +0800

    power: battery/charger: move battery/charger drivers to power/supply
    Change-Id: I70092715368a9d5c7241258a97dc51e55659499b
    Signed-off-by: Shengfei Xu <xsf@rock-chips.com>

--- drivers/mfd/rk808.c
+++ drivers/mfd/rk808.c
@@ -257,6 +257,8 @@ static const struct mfd_cell rk816s[] = {
 static const struct mfd_cell rk817s[] = {
 	{ .name = "rk808-clkout",},
 	{ .name = "rk808-regulator",},
+	{ .name = "rk817-battery", .of_compatible = "rk817,battery", },
+	{ .name = "rk817-charger", .of_compatible = "rk817,charger", },
 	{
 		.name = "rk805-pwrkey",
 		.num_resources = ARRAY_SIZE(rk817_pwrkey_resources),
@@ -276,6 +278,8 @@ static const struct mfd_cell rk817s[] = {
 static const struct mfd_cell rk818s[] = {
 	{ .name = "rk808-clkout", },
 	{ .name = "rk808-regulator", },
+	{ .name = "rk818-battery", .of_compatible = "rk818-battery", },
+	{ .name = "rk818-charger", },
 	{
 		.name = "rk808-rtc",
 		.num_resources = ARRAY_SIZE(rtc_resources),
