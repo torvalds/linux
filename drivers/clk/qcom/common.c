ffe37ede0a9eb524162c683f48da8558d5323ddb
Taniya Das <tdas@codeaurora.org>
2019-10-14 15:53:05 +0530

    clk: qcom: common: Return NULL from clk_hw OF provider
    Return NULL in the cases where the clk_hw is not registered with the
    clock provider, but the clock consumer still requests for a clock id.
    
    Signed-off-by: Taniya Das <tdas@codeaurora.org>
    Link: https://lkml.kernel.org/r/20191014102308.27441-3-tdas@codeaurora.org
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/qcom/common.c
+++ drivers/clk/qcom/common.c
@@ -218,7 +218,7 @@ static struct clk_hw *qcom_cc_clk_hw_get(struct of_phandle_args *clkspec,
 		return ERR_PTR(-EINVAL);
 	}
 
-	return cc->rclks[idx] ? &cc->rclks[idx]->hw : ERR_PTR(-ENOENT);
+	return cc->rclks[idx] ? &cc->rclks[idx]->hw : NULL;
 }
 
 int qcom_cc_really_probe(struct platform_device *pdev,
