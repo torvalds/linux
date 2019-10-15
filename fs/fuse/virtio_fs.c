3f22c7467136adfa6d2a7baf7cd5c573f0641bd1
Miklos Szeredi <mszeredi@redhat.com>
2019-10-15 16:11:41 +0200

    virtio-fs: don't show mount options
    Virtio-fs does not accept any mount options, so it's confusing and wrong to
    show any in /proc/mounts.
    
    Reported-by: Stefan Hajnoczi <stefanha@redhat.com> 
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/virtio_fs.c
+++ fs/fuse/virtio_fs.c
@@ -992,6 +992,7 @@ static int virtio_fs_fill_super(struct super_block *sb)
 		.destroy = true,
 		.no_control = true,
 		.no_force_umount = true,
+		.no_mount_options = true,
 	};
 
 	mutex_lock(&virtio_fs_mutex);
