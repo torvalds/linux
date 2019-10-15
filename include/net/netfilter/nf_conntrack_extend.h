2ad9d7747c10d17cc06447944fefd4c29ae11eb1
Florian Westphal <fw@strlen.de>
2019-10-15 15:19:15 +0200

    netfilter: conntrack: free extension area immediately
    Instead of waiting for rcu grace period just free it directly.
    
    This is safe because conntrack lookup doesn't consider extensions.
    
    Other accesses happen while ct->ext can't be free'd, either because
    a ct refcount was taken or because the conntrack hash bucket lock or
    the dying list spinlock have been taken.
    
    This allows to remove __krealloc in a followup patch, netfilter was the
    only user.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/net/netfilter/nf_conntrack_extend.h
+++ include/net/netfilter/nf_conntrack_extend.h
@@ -43,7 +43,6 @@ enum nf_ct_ext_id {
 
 /* Extensions: optional stuff which isn't permanently in struct. */
 struct nf_ct_ext {
-	struct rcu_head rcu;
 	u8 offset[NF_CT_EXT_NUM];
 	u8 len;
 	char data[0];
@@ -71,15 +70,6 @@ static inline void *__nf_ct_ext_find(const struct nf_conn *ct, u8 id)
 
 /* Destroy all relationships */
 void nf_ct_ext_destroy(struct nf_conn *ct);
-
-/* Free operation. If you want to free a object referred from private area,
- * please implement __nf_ct_ext_free() and call it.
- */
-static inline void nf_ct_ext_free(struct nf_conn *ct)
-{
-	if (ct->ext)
-		kfree_rcu(ct->ext, rcu);
-}
 
 /* Add this type, returns pointer to data or NULL. */
 void *nf_ct_ext_add(struct nf_conn *ct, enum nf_ct_ext_id id, gfp_t gfp);
