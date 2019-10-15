931cccc967730d43bb6325506baf51c8f83a9a58
Andrea Parri <parri.andrea@gmail.com>
2019-10-15 13:46:46 +0200

    Drivers: hv: vmbus: Add module parameter to cap the VMBus version
    Currently, Linux guests negotiate the VMBus version with Hyper-V
    and use the highest available VMBus version they can connect to.
    This has some drawbacks: by using the highest available version,
    certain code paths are never executed and can not be tested when
    the guest runs on the newest host.
    
    Add the module parameter "max_version", to upper-bound the VMBus
    versions guests can negotiate.
    
    Suggested-by: Dexuan Cui <decui@microsoft.com>
    Signed-off-by: Andrea Parri <parri.andrea@gmail.com>
    Reviewed-by: Wei Liu <wei.liu@kernel.org>
    Reviewed-by: Michael Kelley <mikelley@microsoft.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/hv/connection.c
+++ drivers/hv/connection.c
@@ -14,6 +14,7 @@
 #include <linux/wait.h>
 #include <linux/delay.h>
 #include <linux/mm.h>
+#include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
 #include <linux/hyperv.h>
@@ -54,6 +55,16 @@ static __u32 vmbus_versions[] = {
 	VERSION_WIN7,
 	VERSION_WS2008
 };
+
+/*
+ * Maximal VMBus protocol version guests can negotiate.  Useful to cap the
+ * VMBus version for testing and debugging purpose.
+ */
+static uint max_version = VERSION_WIN10_V5_2;
+
+module_param(max_version, uint, S_IRUGO);
+MODULE_PARM_DESC(max_version,
+		 "Maximal VMBus protocol version which can be negotiated");
 
 int vmbus_negotiate_version(struct vmbus_channel_msginfo *msginfo, u32 version)
 {
@@ -240,6 +251,8 @@ int vmbus_connect(void)
 			goto cleanup;
 
 		version = vmbus_versions[i];
+		if (version > max_version)
+			continue;
 
 		ret = vmbus_negotiate_version(msginfo, version);
 		if (ret == -ETIMEDOUT)
