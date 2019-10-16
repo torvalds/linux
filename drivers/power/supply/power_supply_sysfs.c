d46562ac691f297aafed0a196b397b32648aa3a2
Guru Das Srinagesh <gurus@codeaurora.org>
2019-10-16 13:21:26 -0700

    ANDROID: GKI: power: supply: Add VOLTAGE_STEP property
    This property exposes the step size (in uV) to be used while
    incrementing or decrementing voltage of a PSY.
    
    Signed-off-by: Guru Das Srinagesh <gurus@codeaurora.org>
    (cherry picked from commit 4cde627bd0b8a987bf4104ce07a33ee87c390a44)
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 150789066
    Change-Id: Ic70e933daf6b6f3bafc0f0fe77064c8395e611d1

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -483,6 +483,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(resistance_avg),
 	POWER_SUPPLY_ATTR(batt_res_filt_cnts),
 	POWER_SUPPLY_ATTR(aicl_done),
+	POWER_SUPPLY_ATTR(voltage_step),
 	/* Charge pump properties */
 	POWER_SUPPLY_ATTR(cp_status1),
 	POWER_SUPPLY_ATTR(cp_status2),
