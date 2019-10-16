5415ddfa94293c5ec12f6f1ef27922c8358f8fae
Saravana Kannan <saravanak@google.com>
2019-10-15 17:34:22 -0700

    ANDROID: driver core: Allow fwnode_operations.add_links to differentiate errors
    When add_links() still has suppliers that it needs to link to in the
    future, this patch allows it to differentiate between suppliers that are
    needed for probing vs suppliers are needed for sync_state() correctness.
    
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Bug: 142657042
    Change-Id: If8b2a11dc6d815287c9242aea0ee1c26ef316d96

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -2296,7 +2296,7 @@ int device_add(struct device *dev)
 	struct device *parent;
 	struct kobject *kobj;
 	struct class_interface *class_intf;
-	int error = -EINVAL;
+	int error = -EINVAL, fw_ret;
 	struct kobject *glue_dir = NULL;
 
 	dev = get_device(dev);
@@ -2412,9 +2412,13 @@ int device_add(struct device *dev)
 	 */
 	device_link_add_missing_supplier_links();
 
-	if (fwnode_has_op(dev->fwnode, add_links)
-	    && fwnode_call_int_op(dev->fwnode, add_links, dev))
-		device_link_wait_for_mandatory_supplier(dev, true);
+	if (fwnode_has_op(dev->fwnode, add_links)) {
+		fw_ret = fwnode_call_int_op(dev->fwnode, add_links, dev);
+		if (fw_ret == -ENODEV)
+			device_link_wait_for_mandatory_supplier(dev);
+		else if (fw_ret)
+			device_link_wait_for_optional_supplier(dev);
+	}
 
 	bus_probe_device(dev);
 	if (parent)
