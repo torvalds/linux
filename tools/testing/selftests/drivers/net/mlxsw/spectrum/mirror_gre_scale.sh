49c65e4ff19772847e030e04121bece0517eb32c
Danielle Ratson <danieller@mellanox.com>
2019-10-17 09:55:15 +0300

    selftests: mlxsw: Generalize the parameters of mirror_gre test
    Use the number of analyzers taken from the devlink command, instead of
    hard-coded value, in order to make the test more generic.
    
    Signed-off-by: Danielle Ratson <danieller@mellanox.com>
    Acked-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- tools/testing/selftests/drivers/net/mlxsw/spectrum/mirror_gre_scale.sh
+++ tools/testing/selftests/drivers/net/mlxsw/spectrum/mirror_gre_scale.sh
@@ -4,10 +4,13 @@ source ../mirror_gre_scale.sh
 mirror_gre_get_target()
 {
 	local should_fail=$1; shift
+	local target
+
+	target=$(devlink_resource_size_get span_agents)
 
 	if ((! should_fail)); then
-		echo 3
+		echo $target
 	else
-		echo 4
+		echo $((target + 1))
 	fi
 }
