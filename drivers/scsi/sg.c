c35a5cfb41509c2214228aa321509ffd91cbf063
Al Viro <viro@zeniv.linux.org.uk>
2019-10-17 20:39:21 +0100

    scsi: sg: sg_read(): simplify reading ->pack_id of userland sg_io_hdr_t
    We don't need to allocate a temporary buffer and read the entire structure
    in it, only to fetch a single field and free what we'd allocated.  Just use
    get_user() and be done with it...
    
    Link: https://lore.kernel.org/r/20191017193925.25539-4-viro@ZenIV.linux.org.uk
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Acked-by: Douglas Gilbert <dgilbert@interlog.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/sg.c
+++ drivers/scsi/sg.c
@@ -441,17 +441,8 @@ sg_read(struct file *filp, char __user *buf, size_t count, loff_t * ppos)
 		}
 		if (old_hdr->reply_len < 0) {
 			if (count >= SZ_SG_IO_HDR) {
-				sg_io_hdr_t *new_hdr;
-				new_hdr = kmalloc(SZ_SG_IO_HDR, GFP_KERNEL);
-				if (!new_hdr) {
-					retval = -ENOMEM;
-					goto free_old_hdr;
-				}
-				retval =__copy_from_user
-				    (new_hdr, buf, SZ_SG_IO_HDR);
-				req_pack_id = new_hdr->pack_id;
-				kfree(new_hdr);
-				if (retval) {
+				sg_io_hdr_t __user *p = (void __user *)buf;
+				if (get_user(req_pack_id, &p->pack_id)) {
 					retval = -EFAULT;
 					goto free_old_hdr;
 				}
