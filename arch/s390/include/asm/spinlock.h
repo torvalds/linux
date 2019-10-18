cceb018377a123dd77d3239bbdbfbdf50f0d6a71
Heiko Carstens <heiko.carstens@de.ibm.com>
2019-10-18 13:23:16 +0200

    s390/alternatives: make use of asm_inline
    This is the s390 version of commit 40576e5e63ea ("x86: alternative.h:
    use asm_inline for all alternative variants").
    
    See commit eb111869301e ("compiler-types.h: add asm_inline
    definition") for more details.
    
    With this change the compiler will not generate many out-of-line
    versions for the three instruction sized arch_spin_unlock() function
    anymore. Due to this gcc seems to change a lot of other inline
    decisions which results in a net 6k text size growth according to
    bloat-o-meter (gcc 9.2 with defconfig).
    But that's still better than having many out-of-line versions of
    arch_spin_unlock().
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>

--- arch/s390/include/asm/spinlock.h
+++ arch/s390/include/asm/spinlock.h
@@ -85,7 +85,7 @@ static inline int arch_spin_trylock(arch_spinlock_t *lp)
 static inline void arch_spin_unlock(arch_spinlock_t *lp)
 {
 	typecheck(int, lp->lock);
-	asm volatile(
+	asm_inline volatile(
 		ALTERNATIVE("", ".long 0xb2fa0070", 49)	/* NIAI 7 */
 		"	sth	%1,%0\n"
 		: "=Q" (((unsigned short *) &lp->lock)[1])
