76643555a145b06ec6dcc0d1cc0691575dceffc9
Yunsheng Lin <linyunsheng@huawei.com>
2019-10-19 16:03:53 +0800

    net: hns3: make struct hns3_enet_ring cacheline aligned
    Since struct hns3_enet_ring is a frequently used in critical data
    path, so make it cacheline aligned as struct hns3_enet_tqp_vector.
    
    Signed-off-by: Yunsheng Lin <linyunsheng@huawei.com>
    Signed-off-by: Huazhong Tan <tanhuazhong@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/hisilicon/hns3/hns3_enet.h
+++ drivers/net/ethernet/hisilicon/hns3/hns3_enet.h
@@ -435,7 +435,7 @@ struct hns3_enet_ring {
 	int pending_buf;
 	struct sk_buff *skb;
 	struct sk_buff *tail_skb;
-};
+} ____cacheline_internodealigned_in_smp;
 
 enum hns3_flow_level_range {
 	HNS3_FLOW_LOW = 0,
