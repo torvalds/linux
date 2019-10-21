240b62d381fe5b66c496a6fd55edff3976a9be51
Juergen Gross <jgross@suse.com>
2019-10-21 12:04:15 +0200

    ia64: remove stale paravirt leftovers
    Remove the last leftovers from IA64 Xen pv-guest support.
    
    PARAVIRT is long gone from IA64 Kconfig and Xen IA64 support, too.
    
    Due to lack of infrastructure no testing done.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Link: https://lore.kernel.org/r/20191021100415.7642-1-jgross@suse.com

--- arch/ia64/include/asm/irqflags.h
+++ arch/ia64/include/asm/irqflags.h
@@ -36,11 +36,7 @@ static inline void arch_maybe_save_ip(unsigned long flags)
 static inline unsigned long arch_local_save_flags(void)
 {
 	ia64_stop();
-#ifdef CONFIG_PARAVIRT
-	return ia64_get_psr_i();
-#else
 	return ia64_getreg(_IA64_REG_PSR);
-#endif
 }
 
 static inline unsigned long arch_local_irq_save(void)
