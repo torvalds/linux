d6ee51637239de0066e2720351f0cda0db5371b3
Sagar Shrikant Kadam <sagar.kadam@sifive.com>
2019-10-22 17:22:18 +0000

    mtd: spi-nor: Add support for is25wp256
    Update the spi_nor_id table for is25wp256 (32MB) device from ISSI,
    present on HiFive Unleashed dev board (Rev: A00).
    
    Use the post bfpt fixup hook for the is25wp256 device, as done for
    the is25lp256 device to overwrite the wrong address width advertised
    by BFPT.
    
    Signed-off-by: Sagar Shrikant Kadam <sagar.kadam@sifive.com>
    [tudor.ambarus@microchip.com: rebase, split and adapt for latest spi-nor/next]
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Reviewed-by: Vignesh Raghavendra <vigneshr@ti.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -2412,6 +2412,10 @@ static const struct flash_info spi_nor_ids[] = {
 			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 	{ "is25wp128",  INFO(0x9d7018, 0, 64 * 1024, 256,
 			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	{ "is25wp256", INFO(0x9d7019, 0, 64 * 1024, 512,
+			    SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
+			    SPI_NOR_4B_OPCODES)
+		       .fixups = &is25lp256_fixups },
 
 	/* Macronix */
 	{ "mx25l512e",   INFO(0xc22010, 0, 64 * 1024,   1, SECT_4K) },
