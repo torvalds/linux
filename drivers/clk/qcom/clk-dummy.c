e1793a57e50a9d480b800d934d4b1a308116429a
David Collins <collinsd@codeaurora.org>
2019-10-21 18:09:16 -0700

    clk: qcom: clk-dummy: convert into a platform driver
    Modify the clk-dummy driver so that it registers a platform
    driver instead of using CLK_OF_DECLARE().  This is needed for
    device struct based dependency tracking to work properly.
    
    Change-Id: I545c6e6a11e349e6eec0c75c106932d3909c54c8
    Signed-off-by: David Collins <collinsd@codeaurora.org>
    Signed-off-by: Vivek Aknurwar <viveka@codeaurora.org>

--- drivers/clk/qcom/clk-dummy.c
+++ drivers/clk/qcom/clk-dummy.c
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2018, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2018-2019, The Linux Foundation. All rights reserved.
  */
 
 #include <linux/clk-provider.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
@@ -83,7 +85,7 @@ static struct clk *clk_register_dummy(struct device *dev, const char *name,
 	struct clk_init_data init = {};
 
 	/* allocate dummy clock */
-	dummy = kzalloc(sizeof(*dummy), GFP_KERNEL);
+	dummy = devm_kzalloc(dev, sizeof(*dummy), GFP_KERNEL);
 	if (!dummy)
 		return ERR_PTR(-ENOMEM);
 
@@ -94,17 +96,15 @@ static struct clk *clk_register_dummy(struct device *dev, const char *name,
 	dummy->hw.init = &init;
 
 	/* register the clock */
-	clk = clk_register(dev, &dummy->hw);
-	if (IS_ERR(clk)) {
-		kfree(dummy);
+	clk = devm_clk_register(dev, &dummy->hw);
+	if (IS_ERR(clk))
 		return clk;
-	}
 
 	dummy->reset.of_node = node;
 	dummy->reset.ops = &dummy_reset_ops;
 	dummy->reset.nr_resets = RESET_MAX;
 
-	if (reset_controller_register(&dummy->reset))
+	if (devm_reset_controller_register(dev, &dummy->reset))
 		pr_err("Failed to register reset controller for %s\n", name);
 	else
 		pr_info("Successfully registered dummy reset controller for %s\n", name);
@@ -112,26 +112,65 @@ static struct clk *clk_register_dummy(struct device *dev, const char *name,
 	return clk;
 }
 
-/**
- * of_dummy_clk_setup() - Setup function for simple fixed rate clock
- */
-static void of_dummy_clk_setup(struct device_node *node)
+static int dummy_clk_probe(struct platform_device *pdev)
 {
-	struct clk *clk;
+	struct device_node *node = pdev->dev.of_node;
 	const char *clk_name = "dummy_clk";
+	struct clk *clk;
+	int ret;
 
 	of_property_read_string(node, "clock-output-names", &clk_name);
 
-	clk = clk_register_dummy(NULL, clk_name, 0, node);
+	clk = clk_register_dummy(&pdev->dev, clk_name, 0, node);
 	if (!IS_ERR(clk)) {
-		of_clk_add_provider(node, of_clk_src_simple_get, clk);
+		ret = of_clk_add_provider(node, of_clk_src_simple_get, clk);
+		if (ret)
+			return ret;
 	} else {
-		pr_err("Failed to register dummy clock controller for %s\n",
-								clk_name);
-		return;
+		ret = PTR_ERR(clk);
+		pr_err("Failed to register dummy clock controller for %s, ret=%d\n",
+		       clk_name, ret);
+		return ret;
 	}
 
-	pr_info("Successfully registered dummy clock controller for %s\n",
-								clk_name);
+	dev_info(&pdev->dev, "Successfully registered dummy clock controller for %s\n",
+		 clk_name);
+	return 0;
 }
-CLK_OF_DECLARE(dummy_clk, "qcom,dummycc", of_dummy_clk_setup);
+
+static int dummy_clk_remove(struct platform_device *pdev)
+{
+	of_clk_del_provider(pdev->dev.of_node);
+
+	return 0;
+}
+
+static const struct of_device_id dummy_clk_match_table[] = {
+	{ .compatible = "qcom,dummycc" },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, dummy_clk_match_table);
+
+static struct platform_driver dummy_clk_driver = {
+	.probe = dummy_clk_probe,
+	.remove = dummy_clk_remove,
+	.driver = {
+		.name = "clk-dummy",
+		.of_match_table = dummy_clk_match_table,
+	},
+};
+
+static int __init dummy_clk_init(void)
+{
+	return platform_driver_register(&dummy_clk_driver);
+}
+arch_initcall(dummy_clk_init);
+
+static void __exit dummy_clk_exit(void)
+{
+	platform_driver_unregister(&dummy_clk_driver);
+}
+module_exit(dummy_clk_exit);
+
+MODULE_DESCRIPTION("QTI Dummy Clock Driver");
+MODULE_LICENSE("GPL v2");
