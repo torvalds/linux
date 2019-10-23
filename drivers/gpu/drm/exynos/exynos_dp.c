bd19c4527056b3e42e8c286136660aa14d0b6c90
Boris Brezillon <boris.brezillon@collabora.com>
2019-10-23 17:44:53 +0200

    drm/exynos: Don't reset bridge->next
    bridge->next is only points to the new bridge if drm_bridge_attach()
    succeeds. No need to reset it manually here.
    
    Note that this change is part of the attempt to make the bridge chain
    a double-linked list. In order to do that we must patch all drivers
    manipulating the bridge->next field.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Acked-by: Inki Dae <inki.dae@samsung.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191023154512.9762-3-boris.brezillon@collabora.com

--- drivers/gpu/drm/exynos/exynos_dp.c
+++ drivers/gpu/drm/exynos/exynos_dp.c
@@ -110,7 +110,6 @@ static int exynos_dp_bridge_attach(struct analogix_dp_plat_data *plat_data,
 		if (ret) {
 			DRM_DEV_ERROR(dp->dev,
 				      "Failed to attach bridge to drm\n");
-			bridge->next = NULL;
 			return ret;
 		}
 	}
