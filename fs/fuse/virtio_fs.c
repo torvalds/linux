80da5a809d193c60d090cbdf4fe316781bc07965
zhengbin <zhengbin13@huawei.com>
2019-10-23 10:02:49 +0800

    virtiofs: Remove set but not used variable 'fc'
    Fixes gcc '-Wunused-but-set-variable' warning:
    
    fs/fuse/virtio_fs.c: In function virtio_fs_wake_pending_and_unlock:
    fs/fuse/virtio_fs.c:983:20: warning: variable fc set but not used [-Wunused-but-set-variable]
    
    It is not used since commit 7ee1e2e631db ("virtiofs: No need to check
    fpq->connected state")
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: zhengbin <zhengbin13@huawei.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/virtio_fs.c
+++ fs/fuse/virtio_fs.c
@@ -980,7 +980,6 @@ __releases(fiq->lock)
 {
 	unsigned int queue_id = VQ_REQUEST; /* TODO multiqueue */
 	struct virtio_fs *fs;
-	struct fuse_conn *fc;
 	struct fuse_req *req;
 	struct virtio_fs_vq *fsvq;
 	int ret;
@@ -993,7 +992,6 @@ __releases(fiq->lock)
 	spin_unlock(&fiq->lock);
 
 	fs = fiq->priv;
-	fc = fs->vqs[queue_id].fud->fc;
 
 	pr_debug("%s: opcode %u unique %#llx nodeid %#llx in.len %u out.len %u\n",
 		  __func__, req->in.h.opcode, req->in.h.unique,
