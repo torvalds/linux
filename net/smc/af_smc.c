f536dffc0b79738c3104af999318279dccbaa261
Ursula Braun <ubraun@linux.ibm.com>
2019-10-23 15:44:05 +0200

    net/smc: fix closing of fallback SMC sockets
    For SMC sockets forced to fallback to TCP, the file is propagated
    from the outer SMC to the internal TCP socket. When closing the SMC
    socket, the internal TCP socket file pointer must be restored to the
    original NULL value, otherwise memory leaks may show up (found with
    CONFIG_DEBUG_KMEMLEAK).
    
    The internal TCP socket is released in smc_clcsock_release(), which
    calls __sock_release() function in net/socket.c. This calls the
    needed iput(SOCK_INODE(sock)) only, if the file pointer has been reset
    to the original NULL-value.
    
    Fixes: 07603b230895 ("net/smc: propagate file from SMC to TCP socket")
    Signed-off-by: Ursula Braun <ubraun@linux.ibm.com>
    Signed-off-by: Karsten Graul <kgraul@linux.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/smc/af_smc.c
+++ net/smc/af_smc.c
@@ -123,6 +123,12 @@ struct proto smc_proto6 = {
 };
 EXPORT_SYMBOL_GPL(smc_proto6);
 
+static void smc_restore_fallback_changes(struct smc_sock *smc)
+{
+	smc->clcsock->file->private_data = smc->sk.sk_socket;
+	smc->clcsock->file = NULL;
+}
+
 static int __smc_release(struct smc_sock *smc)
 {
 	struct sock *sk = &smc->sk;
@@ -141,6 +147,7 @@ static int __smc_release(struct smc_sock *smc)
 		}
 		sk->sk_state = SMC_CLOSED;
 		sk->sk_state_change(sk);
+		smc_restore_fallback_changes(smc);
 	}
 
 	sk->sk_prot->unhash(sk);
