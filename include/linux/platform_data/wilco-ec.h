3c4d77b68928df6c2bf07f4c3ba8e5d5e490bf4e
Nick Crews <ncrews@chromium.org>
2019-10-24 16:28:05 -0600

    platform/chrome: wilco_ec: Add charging config driver
    Add a device to control the charging algorithm used on Wilco devices,
    which will be picked up by the drivers/power/supply/wilco-charger.c
    driver. See Documentation/ABI/testing/sysfs-class-power-wilco for the
    userspace interface and other info.
    
    Signed-off-by: Nick Crews <ncrews@chromium.org>
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

--- include/linux/platform_data/wilco-ec.h
+++ include/linux/platform_data/wilco-ec.h
@@ -29,6 +29,7 @@
  * @data_size: Size of the data buffer used for EC communication.
  * @debugfs_pdev: The child platform_device used by the debugfs sub-driver.
  * @rtc_pdev: The child platform_device used by the RTC sub-driver.
+ * @charger_pdev: Child platform_device used by the charger config sub-driver.
  * @telem_pdev: The child platform_device used by the telemetry sub-driver.
  */
 struct wilco_ec_device {
@@ -41,6 +42,7 @@ struct wilco_ec_device {
 	size_t data_size;
 	struct platform_device *debugfs_pdev;
 	struct platform_device *rtc_pdev;
+	struct platform_device *charger_pdev;
 	struct platform_device *telem_pdev;
 };
 
