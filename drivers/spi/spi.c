15f794bd977a0135328fbdd8a83cc64c1d267b39
Gregory CLEMENT <gregory.clement@bootlin.com>
2019-10-24 16:13:09 +0200

    spi: Fix NULL pointer when setting SPI_CS_HIGH for GPIO CS
    Even if the flag use_gpio_descriptors is set, it is possible that
    cs_gpiods was not allocated, which leads to a kernel crash.
    
    Reported-by: "kernelci.org bot" <bot@kernelci.org>
    Fixes: 3e5ec1db8bfe ("spi: Fix SPI_CS_HIGH setting when using native and GPIO CS")
    Signed-off-by: Gregory CLEMENT <gregory.clement@bootlin.com>
    Link: https://lore.kernel.org/r/20191024141309.22434-1-gregory.clement@bootlin.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Cc: <stable@vger.kernel.org>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -1780,7 +1780,8 @@ static int of_spi_parse_dt(struct spi_controller *ctlr, struct spi_device *spi,
 	 * handled in the gpiolib, so all gpio chip selects are "active high"
 	 * in the logical sense, the gpiolib will invert the line if need be.
 	 */
-	if ((ctlr->use_gpio_descriptors) && ctlr->cs_gpiods[spi->chip_select])
+	if ((ctlr->use_gpio_descriptors) && ctlr->cs_gpiods &&
+	    ctlr->cs_gpiods[spi->chip_select])
 		spi->mode |= SPI_CS_HIGH;
 
 	/* Device speed */
