a9a808084d6ac015ac68b4223ac5f6ef31f8076e
Bart Van Assche <bvanassche@acm.org>
2019-10-25 09:50:08 -0700

    block: Remove the synchronize_rcu() call from __blk_mq_update_nr_hw_queues()
    Since the blk_mq_{,un}freeze_queue() calls in __blk_mq_update_nr_hw_queues()
    already serialize __blk_mq_update_nr_hw_queues() against
    blk_mq_queue_tag_busy_iter(), the synchronize_rcu() call in
    __blk_mq_update_nr_hw_queues() is not necessary. Hence remove it.
    
    Note: the synchronize_rcu() call in __blk_mq_update_nr_hw_queues() was
    introduced by commit f5bbbbe4d635 ("blk-mq: sync the update nr_hw_queues with
    blk_mq_queue_tag_busy_iter"). Commit 530ca2c9bd69 ("blk-mq: Allow blocking
    queue tag iter callbacks") removed the rcu_read_{,un}lock() calls that
    correspond to the synchronize_rcu() call in __blk_mq_update_nr_hw_queues().
    
    Reviewed-by: Ming Lei <ming.lei@redhat.com>
    Cc: Jianchao Wang <jianchao.w.wang@oracle.com>
    Cc: Christoph Hellwig <hch@infradead.org>
    Cc: Hannes Reinecke <hare@suse.com>
    Cc: Johannes Thumshirn <jthumshirn@suse.de>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -3243,10 +3243,6 @@ static void __blk_mq_update_nr_hw_queues(struct blk_mq_tag_set *set,
 	list_for_each_entry(q, &set->tag_list, tag_set_list)
 		blk_mq_freeze_queue(q);
 	/*
-	 * Sync with blk_mq_queue_tag_busy_iter.
-	 */
-	synchronize_rcu();
-	/*
 	 * Switch IO scheduler to 'none', cleaning up the data associated
 	 * with the previous scheduler. We will switch back once we are done
 	 * updating the new sw to hw queue mappings.
