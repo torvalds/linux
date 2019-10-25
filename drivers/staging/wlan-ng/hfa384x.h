95d8aa5c56f6e2aa629d7ef347f0b301d094ff5d
Tim Collier <osdevtc@gmail.com>
2019-10-25 09:41:24 +0100

    staging: wlan-ng: correct parameter alignment in hfa384x.h
    Realign parameter in function declaration to fix checkpatch.pl warning
    that parameter needed to be aligned with the opening parenthesis of
    the declaration.
    
    Signed-off-by: Tim Collier <osdevtc@gmail.com>
    Link: https://lore.kernel.org/r/20191025084126.9181-4-osdevtc@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/hfa384x.h
+++ drivers/staging/wlan-ng/hfa384x.h
@@ -1374,7 +1374,7 @@ void hfa384x_create(struct hfa384x *hw, struct usb_device *usb);
 void hfa384x_destroy(struct hfa384x *hw);
 
 int hfa384x_corereset(struct hfa384x *hw, int holdtime, int settletime,
-		       int genesis);
+		      int genesis);
 int hfa384x_drvr_disable(struct hfa384x *hw, u16 macport);
 int hfa384x_drvr_enable(struct hfa384x *hw, u16 macport);
 int hfa384x_drvr_flashdl_enable(struct hfa384x *hw);
