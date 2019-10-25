c1c8058dfb9852eb5adf968b7617a9a4771a08ce
Cristiane Naves <cristianenavescardoso09@gmail.com>
2019-10-25 13:13:40 -0300

    iommu/virtio: Remove unused variable
    Remove the variable of return. Issue found by
    coccicheck(scripts/coccinelle/misc/returnvar.cocci)
    
    Signed-off-by: Cristiane Naves <cristianenavescardoso09@gmail.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/virtio-iommu.c
+++ drivers/iommu/virtio-iommu.c
@@ -153,7 +153,6 @@ static off_t viommu_get_write_desc_offset(struct viommu_dev *viommu,
  */
 static int __viommu_sync_req(struct viommu_dev *viommu)
 {
-	int ret = 0;
 	unsigned int len;
 	size_t write_len;
 	struct viommu_request *req;
@@ -182,7 +181,7 @@ static int __viommu_sync_req(struct viommu_dev *viommu)
 		kfree(req);
 	}
 
-	return ret;
+	return 0;
 }
 
 static int viommu_sync_req(struct viommu_dev *viommu)
