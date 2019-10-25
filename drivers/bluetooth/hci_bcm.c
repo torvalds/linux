d462af20dbfa1b9b1a831412f32d9d6757b82459
Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
2019-10-25 14:54:26 -0700

    Bluetooth: hci_bcm: Add compatible string for BCM43540
    The BCM43540 chip is a 802.11 a/b/g/n/ac + Bluetooth 4.1 combo module.
    This patch adds a compatible string match to the serdev driver for the
    Bluetooth part of the chip.
    
    Signed-off-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/hci_bcm.c
+++ drivers/bluetooth/hci_bcm.c
@@ -1424,6 +1424,7 @@ static const struct of_device_id bcm_bluetooth_of_match[] = {
 	{ .compatible = "brcm,bcm4345c5" },
 	{ .compatible = "brcm,bcm4330-bt" },
 	{ .compatible = "brcm,bcm43438-bt" },
+	{ .compatible = "brcm,bcm43540-bt" },
 	{ },
 };
 MODULE_DEVICE_TABLE(of, bcm_bluetooth_of_match);
