22fb3ad0cc5f578398953ddcab9c8239a08caccd
Linus Walleij <linus.walleij@linaro.org>
2019-10-26 23:47:32 +0200

    mfd: db8500-prcmu: Support U8420-sysclk firmware
    There is a distinct version of the Ux500 U8420 variant
    with "sysclk", as can be seen from the vendor code that
    didn't make it upstream, this firmware lacks the
    ULPPLL (ultra-low power phase locked loop) which in
    effect means that the timer clock is instead wired to
    the 32768 Hz always-on clock.
    
    This has some repercussions when enabling the timer
    clock as the code as it stands will disable the timer
    clock on these platforms (lacking the so-called
    "doze mode") and obtaining the wrong rate of the timer
    clock.
    
    The timer frequency is of course needed very early in
    the boot, and as a consequence, we need to shuffle
    around the early PRCMU init code: whereas in the past
    we did not need to look up the PRCMU firmware version
    in the early init, but now we need to know the version
    before the core system timers are registered so we
    restructure the platform callbacks to the PRCMU so as
    not to take any arguments and instead look up the
    resources it needs directly from the device tree
    when initializing.
    
    As we do not yet support any platforms using this
    firmware it is not a regression, but as PostmarketOS
    is starting to support products with this firmware we
    need to fix this up.
    
    The low rate of 32kHz also makes the MTU timer unsuitable
    as delay timer but this needs to be fixed in a separate
    patch.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Reviewed-by: Stephan Gerhold <stephan@gerhold.net>
    Acked-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/dbx500-prcmu.h
+++ include/linux/mfd/dbx500-prcmu.h
@@ -190,6 +190,7 @@ enum ddr_pwrst {
 #define PRCMU_FW_PROJECT_U8500_MBL2	12 /* Customer specific */
 #define PRCMU_FW_PROJECT_U8520		13
 #define PRCMU_FW_PROJECT_U8420		14
+#define PRCMU_FW_PROJECT_U8420_SYSCLK	17
 #define PRCMU_FW_PROJECT_A9420		20
 /* [32..63] 9540 and derivatives */
 #define PRCMU_FW_PROJECT_U9540		32
@@ -211,9 +212,9 @@ struct prcmu_fw_version {
 
 #if defined(CONFIG_UX500_SOC_DB8500)
 
-static inline void prcmu_early_init(u32 phy_base, u32 size)
+static inline void prcmu_early_init(void)
 {
-	return db8500_prcmu_early_init(phy_base, size);
+	return db8500_prcmu_early_init();
 }
 
 static inline int prcmu_set_power_state(u8 state, bool keep_ulp_clk,
@@ -401,7 +402,7 @@ static inline int prcmu_config_a9wdog(u8 num, bool sleep_auto_off)
 }
 #else
 
-static inline void prcmu_early_init(u32 phy_base, u32 size) {}
+static inline void prcmu_early_init(void) {}
 
 static inline int prcmu_set_power_state(u8 state, bool keep_ulp_clk,
 	bool keep_ap_pll)
