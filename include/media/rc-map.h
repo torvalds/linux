4f0fac3b1aa5f356e0625f7d767ec71e1c198a73
Clément Péron <peron.clem@gmail.com>
2019-10-27 21:07:37 +0100

    media: rc: add keymap for Beelink GS1 remote control
    Beelink GS1 Andoid TV Box ships with a simple NEC remote.
    
    Signed-off-by: Clément Péron <peron.clem@gmail.com>
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>

--- include/media/rc-map.h
+++ include/media/rc-map.h
@@ -168,6 +168,7 @@ struct rc_map *rc_map_get(const char *name);
 #define RC_MAP_AVERMEDIA_RM_KS           "rc-avermedia-rm-ks"
 #define RC_MAP_AVERTV_303                "rc-avertv-303"
 #define RC_MAP_AZUREWAVE_AD_TU700        "rc-azurewave-ad-tu700"
+#define RC_MAP_BEELINK_GS1               "rc-beelink-gs1"
 #define RC_MAP_BEHOLD                    "rc-behold"
 #define RC_MAP_BEHOLD_COLUMBUS           "rc-behold-columbus"
 #define RC_MAP_BUDGET_CI_OLD             "rc-budget-ci-old"
