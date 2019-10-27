2fd61f796875b78469f35101cc2124ffed6343e5
Fabio Estevam <festevam@gmail.com>
2019-10-27 10:35:31 -0700

    Input: s3c2410_ts - remove unneeded gpio.h header file
    There is no gpio functions used in the driver that is exported
    by the gpio.h header, so remove this unneeded header.
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>
    Link: https://lore.kernel.org/r/20191026185958.24158-2-festevam@gmail.com
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/touchscreen/s3c2410_ts.c
+++ drivers/input/touchscreen/s3c2410_ts.c
@@ -13,7 +13,6 @@
 #include <linux/errno.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/gpio.h>
 #include <linux/input.h>
 #include <linux/delay.h>
 #include <linux/interrupt.h>
