7b10db555257d1248398643a23e10cf36b50d516
YueHaibing <yuehaibing@huawei.com>
2019-10-28 21:25:56 +0800

    scsi: lpfc: Make lpfc_debugfs_ras_log_data static
    Fix sparse warning:
    
    drivers/scsi/lpfc/lpfc_debugfs.c:2083:1: warning:
     symbol 'lpfc_debugfs_ras_log_data' was not declared. Should it be static?
    
    Link: https://lore.kernel.org/r/20191028132556.16272-1-yuehaibing@huawei.com
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Reviewed-by: James Smart <james.smart@broadcom.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/lpfc/lpfc_debugfs.c
+++ drivers/scsi/lpfc/lpfc_debugfs.c
@@ -2079,8 +2079,8 @@ lpfc_debugfs_lockstat_write(struct file *file, const char __user *buf,
 }
 #endif
 
-int
-lpfc_debugfs_ras_log_data(struct lpfc_hba *phba, char *buffer, int size)
+static int lpfc_debugfs_ras_log_data(struct lpfc_hba *phba,
+				     char *buffer, int size)
 {
 	int copied = 0;
 	struct lpfc_dmabuf *dmabuf, *next;
