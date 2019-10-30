8dbdf414cd962176b0d6d7a20d21c79772a99d5f
Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
2019-10-30 21:48:59 +0300

    mtd: spi-nor: fix silent truncation in spi_nor_read()
    commit a719a75a7761e4139dd099330d9fe3589d844f9b upstream.
    
    spi_nor_read() assigns the result of 'ssize_t spi_nor_read_data()'
    to the 'int ret' variable, while 'ssize_t' is a 64-bit type and *int*
    is a 32-bit type on the 64-bit machines. This silent truncation isn't
    really valid, so fix up the variable's type.
    
    Fixes: 59451e1233bd ("mtd: spi-nor: change return value of read/write")
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -1294,7 +1294,7 @@ static int spi_nor_read(struct mtd_info *mtd, loff_t from, size_t len,
 			size_t *retlen, u_char *buf)
 {
 	struct spi_nor *nor = mtd_to_spi_nor(mtd);
-	int ret;
+	ssize_t ret;
 
 	dev_dbg(nor->dev, "from 0x%08x, len %zd\n", (u32)from, len);
 
