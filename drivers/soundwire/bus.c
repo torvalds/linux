53e3e050b4dc481b65ad7de314f594ace7e953d7
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-11-04 12:47:01 -0600

    soundwire: bus: fix race condition with initialization_complete signaling
    Waiting for the enumeration to be complete may not be enough for a
    Slave driver, there is a possible race condition between resume
    operations and initializations handled in an interrupt thread, which
    can results in settings not being fully restored after system or
    pm_runtime resume.
    
    This patch builds on the changes added for enumeration_complete,
    init_completion() is called when the Slave device becomes UNATTACHED,
    as done with enumeration_complete.
    
    The difference with the enumeration_complete case is that complete()
    is signaled after the Slave device is fully initialized after the
    .update_status() callback is called.
    
    A Slave device driver can decide to wait on either of the two
    complete() cases, depending on its initialization code and
    requirements.
    
    Signed-off-by: Rander Wang <rander.wang@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/bus.c
+++ drivers/soundwire/bus.c
@@ -648,6 +648,7 @@ static void sdw_modify_slave_status(struct sdw_slave *slave,
 			__func__, slave->dev_num);
 
 		init_completion(&slave->enumeration_complete);
+		init_completion(&slave->initialization_complete);
 
 	} else if ((status == SDW_SLAVE_ATTACHED) &&
 		   (slave->status == SDW_SLAVE_UNATTACHED)) {
@@ -1052,6 +1053,7 @@ int sdw_handle_slave_status(struct sdw_bus *bus,
 {
 	enum sdw_slave_status prev_status;
 	struct sdw_slave *slave;
+	bool attached_initializing;
 	int i, ret = 0;
 
 	/* first check if any Slaves fell off the bus */
@@ -1097,6 +1099,8 @@ int sdw_handle_slave_status(struct sdw_bus *bus,
 		if (!slave)
 			continue;
 
+		attached_initializing = false;
+
 		switch (status[i]) {
 		case SDW_SLAVE_UNATTACHED:
 			if (slave->status == SDW_SLAVE_UNATTACHED)
@@ -1123,6 +1127,8 @@ int sdw_handle_slave_status(struct sdw_bus *bus,
 			if (prev_status == SDW_SLAVE_ALERT)
 				break;
 
+			attached_initializing = true;
+
 			ret = sdw_initialize_slave(slave);
 			if (ret)
 				dev_err(bus->dev,
@@ -1141,6 +1147,8 @@ int sdw_handle_slave_status(struct sdw_bus *bus,
 		if (ret)
 			dev_err(slave->bus->dev,
 				"Update Slave status failed:%d\n", ret);
+		if (attached_initializing)
+			complete(&slave->initialization_complete);
 	}
 
 	return ret;
