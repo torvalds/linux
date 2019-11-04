5bea7fedb7fe4d5e6d3ba9f385dd3619fb004ce7
yu kuai <yukuai3@huawei.com>
2019-11-04 21:27:23 +0800

    drm/amdgpu: remove set but not used variable 'dig_connector'
    Fixes gcc '-Wunused-but-set-variable' warning:
    
    drivers/gpu/drm/amd/amdgpu/atombios_dp.c: In function
    ‘amdgpu_atombios_dp_get_panel_mode’:
    drivers/gpu/drm/amd/amdgpu/atombios_dp.c:364:36: warning: variable
    ‘dig_connector’ set but not used [-Wunused-but-set-variable]
    
    It is never used, so can be removed.
    
    Fixes: d38ceaf99ed0 ("drm/amdgpu: add core driver (v4)")
    Signed-off-by: yu kuai <yukuai3@huawei.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/atombios_dp.c
+++ drivers/gpu/drm/amd/amdgpu/atombios_dp.c
@@ -361,15 +361,12 @@ int amdgpu_atombios_dp_get_panel_mode(struct drm_encoder *encoder,
 			       struct drm_connector *connector)
 {
 	struct amdgpu_connector *amdgpu_connector = to_amdgpu_connector(connector);
-	struct amdgpu_connector_atom_dig *dig_connector;
 	int panel_mode = DP_PANEL_MODE_EXTERNAL_DP_MODE;
 	u16 dp_bridge = amdgpu_connector_encoder_get_dp_bridge_encoder_id(connector);
 	u8 tmp;
 
 	if (!amdgpu_connector->con_priv)
 		return panel_mode;
-
-	dig_connector = amdgpu_connector->con_priv;
 
 	if (dp_bridge != ENCODER_OBJECT_ID_NONE) {
 		/* DP bridge chips */
