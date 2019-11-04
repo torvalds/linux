7b3fe663da71273710fc8f7fa0c71515e96c6786
Hawking Zhang <Hawking.Zhang@amd.com>
2019-11-04 16:20:06 +0800

    drm/amdgpu: disallow direct upload save restore list from gfx driver
    [ Upstream commit 58f46d4b65021083ef4b4d49c6e2c58e5783f626 ]
    
    Direct uploading save/restore list via mmio register writes breaks the security
    policy. Instead, the driver should pass s&r list to psp.
    
    For all the ASICs that use rlc v2_1 headers, the driver actually upload s&r list
    twice, in non-psp ucode front door loading phase and gfx pg initialization phase.
    The latter is not allowed.
    
    VG12 is the only exception where the driver still keeps legacy approach for S&R
    list uploading. In theory, this can be elimnated if we have valid srcntl ucode
    for VG12.
    
    Signed-off-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Reviewed-by: Candice Li <Candice.Li@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/amdgpu/gfx_v9_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v9_0.c
@@ -2187,7 +2187,8 @@ static void gfx_v9_0_init_pg(struct amdgpu_device *adev)
 	 * And it's needed by gfxoff feature.
 	 */
 	if (adev->gfx.rlc.is_rlc_v2_1) {
-		gfx_v9_1_init_rlc_save_restore_list(adev);
+		if (adev->asic_type == CHIP_VEGA12)
+			gfx_v9_1_init_rlc_save_restore_list(adev);
 		gfx_v9_0_enable_save_restore_machine(adev);
 	}
 
