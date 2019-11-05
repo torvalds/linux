78a917bea6ede80a0635a4fb4f87d713bb6fbeb1
Alexander Shishkin <alexander.shishkin@linux.intel.com>
2019-11-05 09:57:02 +0200

    perf/core: Consistently fail fork on allocation failures
    [ Upstream commit 697d877849d4b34ab58d7078d6930bad0ef6fc66 ]
    
    Commit:
    
      313ccb9615948 ("perf: Allocate context task_ctx_data for child event")
    
    makes the inherit path skip over the current event in case of task_ctx_data
    allocation failure. This, however, is inconsistent with allocation failures
    in perf_event_alloc(), which would abort the fork.
    
    Correct this by returning an error code on task_ctx_data allocation
    failure and failing the fork in that case.
    
    Signed-off-by: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vince Weaver <vincent.weaver@maine.edu>
    Link: https://lkml.kernel.org/r/20191105075702.60319-1-alexander.shishkin@linux.intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -11377,7 +11377,7 @@ inherit_event(struct perf_event *parent_event,
 						   GFP_KERNEL);
 		if (!child_ctx->task_ctx_data) {
 			free_event(child_event);
-			return NULL;
+			return ERR_PTR(-ENOMEM);
 		}
 	}
 
