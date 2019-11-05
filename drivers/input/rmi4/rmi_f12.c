f6aabe1ff1d9d7bad0879253011216438bdb2530
Andrew Duggan <aduggan@synaptics.com>
2019-11-04 16:06:44 -0800

    Input: synaptics-rmi4 - disable the relative position IRQ in the F12 driver
    This patch fixes an issue seen on HID touchpads which report finger
    positions using RMI4 Function 12. The issue manifests itself as
    spurious button presses as described in:
    https://www.spinics.net/lists/linux-input/msg58618.html
    
    Commit 24d28e4f1271 ("Input: synaptics-rmi4 - convert irq distribution
    to irq_domain") switched the RMI4 driver to using an irq_domain to handle
    RMI4 function interrupts. Functions with more then one interrupt now have
    each interrupt mapped to their own IRQ and IRQ handler. The result of
    this change is that the F12 IRQ handler was now getting called twice. Once
    for the absolute data interrupt and once for the relative data interrupt.
    For HID devices, calling rmi_f12_attention() a second time causes the
    attn_data data pointer and size to be set incorrectly. When the touchpad
    button is pressed, F30 will generate an interrupt and attempt to read the
    F30 data from the invalid attn_data data pointer and report incorrect
    button events.
    
    This patch disables the F12 relative interrupt which prevents
    rmi_f12_attention() from being called twice.
    
    Signed-off-by: Andrew Duggan <aduggan@synaptics.com>
    Reported-by: Simon Wood <simon@mungewell.org>
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/r/20191025002527.3189-2-aduggan@synaptics.com
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/rmi4/rmi_f12.c
+++ drivers/input/rmi4/rmi_f12.c
@@ -55,6 +55,9 @@ struct f12_data {
 
 	const struct rmi_register_desc_item *data15;
 	u16 data15_offset;
+
+	unsigned long *abs_mask;
+	unsigned long *rel_mask;
 };
 
 static int rmi_f12_read_sensor_tuning(struct f12_data *f12)
@@ -291,9 +294,18 @@ static int rmi_f12_write_control_regs(struct rmi_function *fn)
 static int rmi_f12_config(struct rmi_function *fn)
 {
 	struct rmi_driver *drv = fn->rmi_dev->driver;
+	struct f12_data *f12 = dev_get_drvdata(&fn->dev);
+	struct rmi_2d_sensor *sensor;
 	int ret;
 
-	drv->set_irq_bits(fn->rmi_dev, fn->irq_mask);
+	sensor = &f12->sensor;
+
+	if (!sensor->report_abs)
+		drv->clear_irq_bits(fn->rmi_dev, f12->abs_mask);
+	else
+		drv->set_irq_bits(fn->rmi_dev, f12->abs_mask);
+
+	drv->clear_irq_bits(fn->rmi_dev, f12->rel_mask);
 
 	ret = rmi_f12_write_control_regs(fn);
 	if (ret)
@@ -315,8 +327,11 @@ static int rmi_f12_probe(struct rmi_function *fn)
 	struct rmi_device_platform_data *pdata = rmi_get_platform_data(rmi_dev);
 	struct rmi_driver_data *drvdata = dev_get_drvdata(&rmi_dev->dev);
 	u16 data_offset = 0;
+	int mask_size;
 
 	rmi_dbg(RMI_DEBUG_FN, &fn->dev, "%s\n", __func__);
+
+	mask_size = BITS_TO_LONGS(drvdata->irq_count) * sizeof(unsigned long);
 
 	ret = rmi_read(fn->rmi_dev, query_addr, &buf);
 	if (ret < 0) {
@@ -332,9 +347,18 @@ static int rmi_f12_probe(struct rmi_function *fn)
 		return -ENODEV;
 	}
 
-	f12 = devm_kzalloc(&fn->dev, sizeof(struct f12_data), GFP_KERNEL);
+	f12 = devm_kzalloc(&fn->dev, sizeof(struct f12_data) + mask_size * 2,
+			GFP_KERNEL);
 	if (!f12)
 		return -ENOMEM;
+
+	f12->abs_mask = (unsigned long *)((char *)f12
+			+ sizeof(struct f12_data));
+	f12->rel_mask = (unsigned long *)((char *)f12
+			+ sizeof(struct f12_data) + mask_size);
+
+	set_bit(fn->irq_pos, f12->abs_mask);
+	set_bit(fn->irq_pos + 1, f12->rel_mask);
 
 	f12->has_dribble = !!(buf & BIT(3));
 
