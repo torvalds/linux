c3a12cc1ec4c9cb52a82d3691f35019816bda0a7
Javier F. Arias <jarias.linux@gmail.com>
2019-11-05 14:14:19 -0500

    staging: rtl8723bs: Simplify boolean expression
    Simplify expression that it's inverting a boolean value.
    Issue found by Coccinelle.
    
    Signed-off-by: Javier F. Arias <jarias.linux@gmail.com>
    Link: https://lore.kernel.org/r/9799bed4370f8cc1bd6a1735721fcd91fac30e09.1572640293.git.jarias.linux@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/rtl8723b_dm.c
+++ drivers/staging/rtl8723bs/hal/rtl8723b_dm.c
@@ -189,7 +189,7 @@ void rtl8723b_HalDmWatchDog(struct adapter *Adapter)
 		bBtDisabled = hal_btcoex_IsBtDisabled(Adapter);
 
 		ODM_CmnInfoUpdate(&pHalData->odmpriv, ODM_CMNINFO_BT_ENABLED,
-				  ((bBtDisabled == true)?false:true));
+				  !bBtDisabled);
 
 		ODM_DMWatchdog(&pHalData->odmpriv);
 	}
