3bbc53f4ae1686b501d92d4a5fd400f4958c8a98
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2019-11-07 10:19:24 +0100

    hrtimer: Remove the comment about not used HRTIMER_SOFTIRQ
    The softirq `HRTIMER_SOFTIRQ' was not used since commit c6eb3f70d448
    ("hrtimer: Get rid of hrtimer softirq").
    
    But it got used again, beginning with commit 5da70160462e ("hrtimer:
    Implement support for softirq based hrtimers"), which did not remove the
    comment. Remove it now.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lkml.kernel.org/r/20191107091924.13410-1-bigeasy@linutronix.de

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -520,8 +520,7 @@ enum
 	IRQ_POLL_SOFTIRQ,
 	TASKLET_SOFTIRQ,
 	SCHED_SOFTIRQ,
-	HRTIMER_SOFTIRQ, /* Unused, but kept as tools rely on the
-			    numbering. Sigh! */
+	HRTIMER_SOFTIRQ,
 	RCU_SOFTIRQ,    /* Preferable RCU should always be the last softirq */
 
 	NR_SOFTIRQS
