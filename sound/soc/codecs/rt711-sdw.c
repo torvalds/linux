d39d9eb70ba2c442c2d6cccbbe79f719a8614897
Shuming Fan <shumingf@realtek.com>
2019-11-07 14:40:22 +0800

    ASoC: rt711: enable wake_capable
    Signed-off-by: Shuming Fan <shumingf@realtek.com>

--- sound/soc/codecs/rt711-sdw.c
+++ sound/soc/codecs/rt711-sdw.c
@@ -402,6 +402,9 @@ static int rt711_read_prop(struct sdw_slave *slave)
 	/* set the timeout values */
 	prop->clk_stop_timeout = 20;
 
+	/* wake-up event */
+	prop->wake_capable = 1;
+
 	return 0;
 }
 
