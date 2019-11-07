b2bf5015dae3a427166768bc6ca4f300247f9554
H. Nikolaus Schaller <hns@goldelico.com>
2019-11-07 11:30:45 +0100

    net: wireless: ti: remove local VENDOR_ID and DEVICE_ID definitions
    They are already included from mmc/sdio_ids.h and do not need
    a local definition.
    
    Fixes: 884f38607897 ("mmc: core: move some sdio IDs out of quirks file")
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>
    Acked-by: Kalle Valo <kvalo@codeaurora.org>
    Cc: <stable@vger.kernel.org> # v4.11+
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/net/wireless/ti/wl1251/sdio.c
+++ drivers/net/wireless/ti/wl1251/sdio.c
@@ -22,14 +22,6 @@
 
 #include "wl1251.h"
 
-#ifndef SDIO_VENDOR_ID_TI
-#define SDIO_VENDOR_ID_TI		0x104c
-#endif
-
-#ifndef SDIO_DEVICE_ID_TI_WL1251
-#define SDIO_DEVICE_ID_TI_WL1251	0x9066
-#endif
-
 struct wl1251_sdio {
 	struct sdio_func *func;
 	u32 elp_val;
