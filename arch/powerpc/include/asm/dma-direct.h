cb6f6392dbefb45b6aabd9a6f566440ef642b6f8
Christoph Hellwig <hch@lst.de>
2019-11-12 17:08:49 +0100

    powerpc: remove support for NULL dev in __phys_to_dma / __dma_to_phys
    Support for calling the DMA API functions without a valid device pointer
    was removed a while ago, so remove the stale support for that from the
    powerpc __phys_to_dma / __dma_to_phys helpers.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Michael Ellerman <mpe@ellerman.id.au>
    Reviewed-by: Nicolas Saenz Julienne <nsaenzjulienne@suse.de>

--- arch/powerpc/include/asm/dma-direct.h
+++ arch/powerpc/include/asm/dma-direct.h
@@ -4,15 +4,11 @@
 
 static inline dma_addr_t __phys_to_dma(struct device *dev, phys_addr_t paddr)
 {
-	if (!dev)
-		return paddr + PCI_DRAM_OFFSET;
 	return paddr + dev->archdata.dma_offset;
 }
 
 static inline phys_addr_t __dma_to_phys(struct device *dev, dma_addr_t daddr)
 {
-	if (!dev)
-		return daddr - PCI_DRAM_OFFSET;
 	return daddr - dev->archdata.dma_offset;
 }
 #endif /* ASM_POWERPC_DMA_DIRECT_H */
