ad92b10c0e9fc4870bf3cba3dbbfffd3abd0cee2
Nickey Yang <nickey.yang@rock-chips.com>
2019-11-14 15:27:04 +0800

    drm/bridge: synopsys: dw-hdmi: set scrambler bypassed when hdmi1.4
    Set resolution to 4K when connect an HDMI2.0 monitor
    will enable scrambler feature.
    At this time, change to connect an hdmi1.4 monitor will
    cause black screen because we do not set scrambler feature bypassed.
    So fix this.
    
    Change-Id: Ic14b683719c8234c316dc1cacaa62c72a38e0294
    Signed-off-by: Nickey Yang <nickey.yang@rock-chips.com>

--- drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
+++ drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
@@ -2186,6 +2186,8 @@ static void hdmi_av_composer(struct dw_hdmi *hdmi,
 				    HDMI_MC_SWRSTZ);
 			drm_scdc_set_scrambling(&hdmi->i2c->adap, 0);
 		}
+	} else {
+		hdmi_writeb(hdmi, 0, HDMI_FC_SCRAMBLER_CTRL);
 	}
 
 	/* Set up horizontal active pixel width */
