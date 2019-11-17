67de34c20fa7a7aeae50c55dd2751e6df80c3e2c
Joseph Kust <joseph.kust@rockwellcollins.com>
2019-11-17 15:55:47 -0600

    mtd: spi-nor: Add support for sst26vf016b
    Adds support for sst26vf016b, a smaller variant of the sst26vf064b.
    Tested on a sama5d3 SOC using the atmel,at91rm9200-spi controller.
    The dual read could not be tested.
    
    SPI_NOR_QUAD_READ is not added because it requires that the IOC bit
    from the Configuration Register to be set to 1, which is not the case:
    its default value at power-up is zero and we don't set it to one in
    spi-nor either.
    
    Signed-off-by: Joseph Kust <joseph.kust@rockwellcollins.com>
    Signed-off-by: Brandon Maier <brandon.maier@rockwellcollins.com>
    [tudor.ambarus@microchip.com: Drop quad read flag, amend commit description]
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -2577,6 +2577,8 @@ static const struct flash_info spi_nor_ids[] = {
 	{ "sst25wf080",  INFO(0xbf2505, 0, 64 * 1024, 16, SECT_4K | SST_WRITE) },
 	{ "sst26wf016b", INFO(0xbf2651, 0, 64 * 1024, 32, SECT_4K |
 			      SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	{ "sst26vf016b", INFO(0xbf2641, 0, 64 * 1024, 32, SECT_4K |
+			      SPI_NOR_DUAL_READ) },
 	{ "sst26vf064b", INFO(0xbf2643, 0, 64 * 1024, 128, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 
 	/* ST Microelectronics -- newer production may have feature updates */
