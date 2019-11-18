a25ecd9d1e60241df905b29fb84765eb74545c4f
Colin Ian King <colin.king@canonical.com>
2019-11-18 11:40:59 +0000

    bpf: Fix memory leak on object 'data'
    The error return path on when bpf_fentry_test* tests fail does not
    kfree 'data'. Fix this by adding the missing kfree.
    
    Addresses-Coverity: ("Resource leak")
    
    Fixes: faeb2dce084a ("bpf: Add kernel test functions for fentry testing")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20191118114059.37287-1-colin.king@canonical.com

--- net/bpf/test_run.c
+++ net/bpf/test_run.c
@@ -161,8 +161,10 @@ static void *bpf_test_init(const union bpf_attr *kattr, u32 size,
 	    bpf_fentry_test3(4, 5, 6) != 15 ||
 	    bpf_fentry_test4((void *)7, 8, 9, 10) != 34 ||
 	    bpf_fentry_test5(11, (void *)12, 13, 14, 15) != 65 ||
-	    bpf_fentry_test6(16, (void *)17, 18, 19, (void *)20, 21) != 111)
+	    bpf_fentry_test6(16, (void *)17, 18, 19, (void *)20, 21) != 111) {
+		kfree(data);
 		return ERR_PTR(-EFAULT);
+	}
 	return data;
 }
 
