74dd135590b6fde1cd78d7017baaf2dc92ce8d14
Sathyanarayana Nujella <sathyanarayana.nujella@intel.com>
2019-11-17 16:44:08 -0800

    ASoC: Intel: common: Add mach table for tgl-max98357a-rt5682
    Update tgl mach table to include machine with max98357a & ALC5682.
    
    Signed-off-by: Sathyanarayana Nujella <sathyanarayana.nujella@intel.com>
    Signed-off-by: Jairaj Arava <jairaj.arava@intel.com>

--- sound/soc/intel/common/soc-acpi-intel-tgl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-tgl-match.c
@@ -9,12 +9,25 @@
 #include <sound/soc-acpi.h>
 #include <sound/soc-acpi-intel-match.h>
 
+static struct snd_soc_acpi_codecs tgl_codecs = {
+	.num_codecs = 1,
+	.codecs = {"MX98357A"}
+};
+
 struct snd_soc_acpi_mach snd_soc_acpi_intel_tgl_machines[] = {
 	{
 		.id = "10EC1308",
 		.drv_name = "tgl_rt1308",
 		.sof_fw_filename = "sof-tgl.ri",
 		.sof_tplg_filename = "sof-tgl-rt1308.tplg",
+	},
+	{
+		.id = "10EC5682",
+		.drv_name = "tgl_max98357a_rt5682",
+		.machine_quirk = snd_soc_acpi_codec_list,
+		.quirk_data = &tgl_codecs,
+		.sof_fw_filename = "sof-tgl.ri",
+		.sof_tplg_filename = "sof-tgl-max98357a-rt5682.tplg",
 	},
 	{},
 };
