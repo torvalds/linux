65d153c8ed6505075740f005122d01298f6e474a
Sun Ke <sunke32@huawei.com>
2019-11-19 14:09:11 +0800

    nbd:fix memory leak in nbd_get_socket()
    commit dff10bbea4be47bdb615b036c834a275b7c68133 upstream.
    
    Before returning NULL, put the sock first.
    
    Cc: stable@vger.kernel.org
    Fixes: cf1b2326b734 ("nbd: verify socket is supported during setup")
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Reviewed-by: Mike Christie <mchristi@redhat.com>
    Signed-off-by: Sun Ke <sunke32@huawei.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/block/nbd.c
+++ drivers/block/nbd.c
@@ -945,6 +945,7 @@ static struct socket *nbd_get_socket(struct nbd_device *nbd, unsigned long fd,
 	if (sock->ops->shutdown == sock_no_shutdown) {
 		dev_err(disk_to_dev(nbd->disk), "Unsupported socket: shutdown callout must be supported.\n");
 		*err = -EINVAL;
+		sockfd_put(sock);
 		return NULL;
 	}
 
