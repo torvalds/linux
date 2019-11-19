a82055af595946aea461528e551e6ae064b3d560
Pablo Neira Ayuso <pablo@netfilter.org>
2019-11-19 23:05:54 +0100

    netfilter: nft_payload: add VLAN offload support
    Match on ethertype and set up protocol dependency. Check for protocol
    dependency before accessing the tci field. Allow to match on the
    encapsulated ethertype too.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/flow_dissector.h
+++ include/net/flow_dissector.h
@@ -48,9 +48,12 @@ struct flow_dissector_key_tags {
 };
 
 struct flow_dissector_key_vlan {
-	u16	vlan_id:12,
-		vlan_dei:1,
-		vlan_priority:3;
+	union {
+		u16	vlan_id:12,
+			vlan_dei:1,
+			vlan_priority:3;
+		__be16	vlan_tci;
+	};
 	__be16	vlan_tpid;
 };
 
