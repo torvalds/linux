e3cb0c7102f04c83bf1a7cb1d052e92749310b46
Alexander Duyck <alexander.h.duyck@linux.intel.com>
2019-11-20 14:25:53 -0800

    x86/ioperm: Fix use of deprecated config option
    The commit
    
      111e7b15cf10 ("x86/ioperm: Extend IOPL config to control ioperm() as well")
    
    replaced X86_IOPL_EMULATION with X86_IOPL_IOPERM. However it appears
    that there was at least one spot missed as tss_update_io_bitmap() still
    had a reference to it contained in the code.
    
    The result of this is that it exposed a NULL pointer dereference as seen
    below with a linux-next next-20191120 kernel:
    
      BUG: kernel NULL pointer dereference, address: 0000000000000000
      #PF: supervisor read access in kernel mode
      #PF: error_code(0x0000) - not-present page
      PGD 0 P4D 0
      Oops: 0000 [#1] SMP PTI
      CPU: 5 PID: 1542 Comm: ovs-vswitchd Tainted: G        W 5.4.0-rc8-next-20191120 #125
      RIP: 0010:tss_update_io_bitmap+0x4e/0x180
      Code: 10 31 c0 65 48 03 1d 69 54 5d 6d 65 48 8b 04 25 40 8c 01 00 48 8b 10 \
    	  f7 c2 00 00 40 00 0f 84 8c 00 00 00 4c 8b a0 c0 22 00 00 <49> 8b 04 \
    	  24 48 39 43 68 74 2e 8b 53 70 41 39 54 24 0c 48 8d 7b 78
      RSP: 0018:ffffb8888a0ebf08 EFLAGS: 00010006
      RAX: ffff8a429811a680 RBX: ffff8a4c3f946000 RCX: 0000000000000011
      RDX: 0000000000400080 RSI: 0000000000400080 RDI: 0000000000000000
      RBP: ffffb8888a0ebf30 R08: 00007ffffb5d7ce0 R09: 0000000000000000
      R10: 0000000000000000 R11: 0000000000000000 R12: 0000000000000000
      R13: 0000000000000000 R14: 0000000000000000 R15: 0000000000000000
      FS:  00007f68a9635c40(0000) GS:ffff8a4c3f940000(0000) knlGS:0000000000000000
      CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
      CR2: 0000000000000000 CR3: 000000103572a001 CR4: 00000000001606e0
      Call Trace:
       ? syscall_slow_exit_work+0x39/0xdb
       do_syscall_64+0x1a5/0x200
       entry_SYSCALL_64_after_hwframe+0x44/0xa9
      RIP: 0033:0x7f68a7aff797
    
    Fixes: 111e7b15cf10 ("x86/ioperm: Extend IOPL config to control ioperm() as well")
    Signed-off-by: Alexander Duyck <alexander.h.duyck@linux.intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: x86-ml <x86@kernel.org>
    Link: https://lkml.kernel.org/r/20191120222426.3060.18462.stgit@localhost.localdomain

--- arch/x86/kernel/process.c
+++ arch/x86/kernel/process.c
@@ -382,8 +382,7 @@ void tss_update_io_bitmap(void)
 	if (test_thread_flag(TIF_IO_BITMAP)) {
 		struct thread_struct *t = &current->thread;
 
-		if (IS_ENABLED(CONFIG_X86_IOPL_EMULATION) &&
-		    t->iopl_emul == 3) {
+		if (IS_ENABLED(CONFIG_X86_IOPL_IOPERM) && t->iopl_emul == 3) {
 			*base = IO_BITMAP_OFFSET_VALID_ALL;
 		} else {
 			struct io_bitmap *iobm = t->io_bitmap;
