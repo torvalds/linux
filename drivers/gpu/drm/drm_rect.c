94fee4a74d04b4298892850287c2a879dbd01e2a
Daniel Vetter <daniel.vetter@ffwll.ch>
2019-11-26 15:52:13 +0100

    drm/rect: update kerneldoc for drm_rect_clip_scaled()
    This was forgotten in f96bdf564f3e ("drm/rect: Handle rounding errors
    in drm_rect_clip_scaled, v3.")
    
    Spotted while reviewing patches from Ville touching this area.
    
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Fixes: f96bdf564f3e ("drm/rect: Handle rounding errors in drm_rect_clip_scaled, v3.")
    Cc: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Cc: Benjamin Gaignard <benjamin.gaignard@st.com>
    Cc: Ville Syrjala <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191126145213.380079-1-daniel.vetter@ffwll.ch

--- drivers/gpu/drm/drm_rect.c
+++ drivers/gpu/drm/drm_rect.c
@@ -81,11 +81,13 @@ static u32 clip_scaled(int src, int dst, int *clip)
  * @clip: clip rectangle
  *
  * Clip rectangle @dst by rectangle @clip. Clip rectangle @src by the
- * same amounts multiplied by @hscale and @vscale.
+ * the corresponding amounts, retaining the vertical and horizontal scaling
+ * factors from @src to @dst.
  *
  * RETURNS:
+ *
  * %true if rectangle @dst is still visible after being clipped,
- * %false otherwise
+ * %false otherwise.
  */
 bool drm_rect_clip_scaled(struct drm_rect *src, struct drm_rect *dst,
 			  const struct drm_rect *clip)
