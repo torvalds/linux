68421940b0d66f3806bfd0cd0f78a6935c75cb55
Gao, Fred <fred.gao@intel.com>
2019-11-27 00:08:29 +0800

    drm/i915/gvt: Update force-to-nonpriv register whitelist
    Host print below warning message when creating guest:
        "gvt: vgpu(1) Invalid FORCE_NONPRIV write 10002349".
    
    Add register 0x2348 in force-to-nonpriv whitelist as required
    by guest.
    
    Signed-off-by: Gao, Fred <fred.gao@intel.com>
    Reviewed-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>

--- drivers/gpu/drm/i915/gvt/handlers.c
+++ drivers/gpu/drm/i915/gvt/handlers.c
@@ -460,6 +460,7 @@ static int pipeconf_mmio_write(struct intel_vgpu *vgpu, unsigned int offset,
 static i915_reg_t force_nonpriv_white_list[] = {
 	GEN9_CS_DEBUG_MODE1, //_MMIO(0x20ec)
 	GEN9_CTX_PREEMPT_REG,//_MMIO(0x2248)
+	PS_INVOCATION_COUNT,//_MMIO(0x2348)
 	GEN8_CS_CHICKEN1,//_MMIO(0x2580)
 	_MMIO(0x2690),
 	_MMIO(0x2694),
