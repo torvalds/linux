c3d03c5a196f21381c1a2166a4648beba13d3d1f
Zhan Liu <zhan.liu@amd.com>
2019-11-28 14:12:11 -0500

    drm/amd/display: Include num_vmid and num_dsc within NV14's resource caps
    [Why]
    "num_vmid" and "num_dsc" are missing within NV14's resource caps structure.
    
    [How]
    Add the missing parts.
    
    Signed-off-by: Zhan Liu <zhan.liu@amd.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn20/dcn20_resource.c
+++ drivers/gpu/drm/amd/display/dc/dcn20/dcn20_resource.c
@@ -854,6 +854,8 @@ static const struct resource_caps res_cap_nv14 = {
 		.num_pll = 5,
 		.num_dwb = 1,
 		.num_ddc = 5,
+		.num_vmid = 16,
+		.num_dsc = 5,
 };
 
 static const struct dc_debug_options debug_defaults_drv = {
