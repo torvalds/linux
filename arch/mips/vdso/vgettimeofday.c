cf84512fe7aac932c67fc54183cdcd7b2218aac1
Vincenzo Frascino <vincenzo.frascino@arm.com>
2019-11-29 14:36:58 +0000

    UPSTREAM: mips: Fix gettimeofday() in the vdso library
    [ Upstream commit 7d2aa4bb90f5f6f1b8de8848c26042403f2d7bf9 ]
    
    The libc provides a discovery mechanism for vDSO library and its
    symbols. When a symbol is not exposed by the vDSOs the libc falls back
    on the system calls.
    
    With the introduction of the unified vDSO library on mips this behavior
    is not honored anymore by the kernel in the case of gettimeofday().
    
    The issue has been noticed and reported due to a dhclient failure on the
    CI20 board:
    
    root@letux:~# dhclient
    ../../../../lib/isc/unix/time.c:200: Operation not permitted
    root@letux:~#
    
    Restore the original behavior fixing gettimeofday() in the vDSO library.
    
    Reported-by: H. Nikolaus Schaller <hns@goldelico.com>
    Tested-by: H. Nikolaus Schaller <hns@goldelico.com> # CI20 with JZ4780
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Signed-off-by: Paul Burton <paulburton@kernel.org>
    Cc: mips-creator-ci20-dev@googlegroups.com
    Cc: letux-kernel@openphoenux.org
    Cc: linux-mips@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 520e3bd3dee4266603b97ad5f5cedb88736a09c5)
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 154668398
    Change-Id: Idd4277e8fca39eb6c4fbb2bce3b8f229548bf169

--- arch/mips/vdso/vgettimeofday.c
+++ arch/mips/vdso/vgettimeofday.c
@@ -17,11 +17,21 @@ int __vdso_clock_gettime(clockid_t clock,
 	return __cvdso_clock_gettime32(clock, ts);
 }
 
+#ifdef CONFIG_MIPS_CLOCK_VSYSCALL
+
+/*
+ * This is behind the ifdef so that we don't provide the symbol when there's no
+ * possibility of there being a usable clocksource, because there's nothing we
+ * can do without it. When libc fails the symbol lookup it should fall back on
+ * the standard syscall path.
+ */
 int __vdso_gettimeofday(struct __kernel_old_timeval *tv,
 			struct timezone *tz)
 {
 	return __cvdso_gettimeofday(tv, tz);
 }
+
+#endif /* CONFIG_MIPS_CLOCK_VSYSCALL */
 
 int __vdso_clock_getres(clockid_t clock_id,
 			struct old_timespec32 *res)
@@ -43,11 +53,21 @@ int __vdso_clock_gettime(clockid_t clock,
 	return __cvdso_clock_gettime(clock, ts);
 }
 
+#ifdef CONFIG_MIPS_CLOCK_VSYSCALL
+
+/*
+ * This is behind the ifdef so that we don't provide the symbol when there's no
+ * possibility of there being a usable clocksource, because there's nothing we
+ * can do without it. When libc fails the symbol lookup it should fall back on
+ * the standard syscall path.
+ */
 int __vdso_gettimeofday(struct __kernel_old_timeval *tv,
 			struct timezone *tz)
 {
 	return __cvdso_gettimeofday(tv, tz);
 }
+
+#endif /* CONFIG_MIPS_CLOCK_VSYSCALL */
 
 int __vdso_clock_getres(clockid_t clock_id,
 			struct __kernel_timespec *res)
