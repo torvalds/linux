822f82fbc0b1d931e8cc63913e8d8a90bcf0ced6
Jani Nikula <jani.nikula@intel.com>
2019-11-29 12:29:33 +0200

    video: smscufx: don't restore fb_mmap after deferred IO cleanup
    Deferred IO now preserves the fb_ops.
    
    Cc: Steve Glendinning <steve.glendinning@shawell.net>
    Cc: linux-fbdev@vger.kernel.org
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/480dcc682481e6972e5648181d7e92120929ec6b.1575022735.git.jani.nikula@intel.com

--- drivers/video/fbdev/smscufx.c
+++ drivers/video/fbdev/smscufx.c
@@ -1170,7 +1170,6 @@ static int ufx_ops_release(struct fb_info *info, int user)
 		fb_deferred_io_cleanup(info);
 		kfree(info->fbdefio);
 		info->fbdefio = NULL;
-		info->fbops->fb_mmap = ufx_ops_mmap;
 	}
 
 	pr_debug("released /dev/fb%d user=%d count=%d",
