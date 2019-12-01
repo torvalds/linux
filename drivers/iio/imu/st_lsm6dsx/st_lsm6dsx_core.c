757b4bcaa0d8fa74500b14b1655f6638ceafd056
Lorenzo Bianconi <lorenzo@kernel.org>
2019-12-01 23:38:18 +0200

    iio: imu: st_lsm6dsx: fix checkpatch warning
    Fix following checkpatch warning:
    
    CHECK: Alignment should match open parenthesis
    +static int st_lsm6dsx_read_event(struct iio_dev *iio_dev,
    +                                  const struct iio_chan_spec *chan,
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/imu/st_lsm6dsx/st_lsm6dsx_core.c
+++ drivers/iio/imu/st_lsm6dsx/st_lsm6dsx_core.c
@@ -1587,11 +1587,11 @@ static int st_lsm6dsx_event_setup(struct st_lsm6dsx_hw *hw, int state)
 }
 
 static int st_lsm6dsx_read_event(struct iio_dev *iio_dev,
-				   const struct iio_chan_spec *chan,
-				   enum iio_event_type type,
-				   enum iio_event_direction dir,
-				   enum iio_event_info info,
-				   int *val, int *val2)
+				 const struct iio_chan_spec *chan,
+				 enum iio_event_type type,
+				 enum iio_event_direction dir,
+				 enum iio_event_info info,
+				 int *val, int *val2)
 {
 	struct st_lsm6dsx_sensor *sensor = iio_priv(iio_dev);
 	struct st_lsm6dsx_hw *hw = sensor->hw;
