8b9aac1a990b2fa590026b46b4e4ec45ebe8dfea
Daniel W. S. Almeida <dwlsalmeida@gmail.com>
2019-12-01 17:15:41 +0100

    media: dvb_dummy_fe: Add blank line after declaration
    Fix checkpatch.pl error by adding a blank line
    
    Signed-off-by: Daniel W. S. Almeida <dwlsalmeida@gmail.com>
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/dvb-frontends/dvb_dummy_fe.c
+++ drivers/media/dvb-frontends/dvb_dummy_fe.c
@@ -99,9 +99,10 @@ static int dvb_dummy_fe_set_voltage(struct dvb_frontend *fe,
 	return 0;
 }
 
-static void dvb_dummy_fe_release(struct dvb_frontend* fe)
+static void dvb_dummy_fe_release(struct dvb_frontend *fe)
 {
 	struct dvb_dummy_fe_state* state = fe->demodulator_priv;
+
 	kfree(state);
 }
 
