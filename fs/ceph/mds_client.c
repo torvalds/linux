668c9a61e3bf31b0dff108009add7fcd4c387869
Deepa Dinamani <deepa.kernel@gmail.com>
2019-12-02 21:19:42 -0800

    fs: ceph: Delete timespec64_trunc() usage
    Since ceph always uses ns granularity, skip the
    truncation which is a no-op.
    
    Signed-off-by: Deepa Dinamani <deepa.kernel@gmail.com>
    Cc: jlayton@kernel.org
    Cc: ceph-devel@vger.kernel.org
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/ceph/mds_client.c
+++ fs/ceph/mds_client.c
@@ -2069,7 +2069,6 @@ struct ceph_mds_request *
 ceph_mdsc_create_request(struct ceph_mds_client *mdsc, int op, int mode)
 {
 	struct ceph_mds_request *req = kzalloc(sizeof(*req), GFP_NOFS);
-	struct timespec64 ts;
 
 	if (!req)
 		return ERR_PTR(-ENOMEM);
@@ -2088,8 +2087,7 @@ ceph_mdsc_create_request(struct ceph_mds_client *mdsc, int op, int mode)
 	init_completion(&req->r_safe_completion);
 	INIT_LIST_HEAD(&req->r_unsafe_item);
 
-	ktime_get_coarse_real_ts64(&ts);
-	req->r_stamp = timespec64_trunc(ts, mdsc->fsc->sb->s_time_gran);
+	ktime_get_coarse_real_ts64(&req->r_stamp);
 
 	req->r_op = op;
 	req->r_direct_mode = mode;
