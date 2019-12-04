a56be671734ec5ab7fcfc9ac681d01698fc8bdf5
Jerome Brunet <jbrunet@baylibre.com>
2019-12-04 14:27:13 +0100

    mtd: spi-nor: add Gigadevice gd25lq128d support
    Tested on the Amlogic aml-libretech-pc platform which does not support
    dual or quad modes
    
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>
    [tudor.ambarus@microchip.com: order entry alphabetically]
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -2388,6 +2388,11 @@ static const struct flash_info spi_nor_ids[] = {
 			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
 	},
 	{
+		"gd25lq128d", INFO(0xc86018, 0, 64 * 1024, 256,
+			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
+			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
+	},
+	{
 		"gd25q128", INFO(0xc84018, 0, 64 * 1024, 256,
 			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
 			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
