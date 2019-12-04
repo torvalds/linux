36582a5a456100ebe4983e3d63b8cbc7e62a0ddc
Ming Lei <ming.lei@redhat.com>
2019-12-04 19:31:14 +0800

    brd: remove max_hw_sectors queue limit
    Now we depend on blk_queue_split() to respect most of queue limit
    (the only one exception could be dma alignment), however
    blk_queue_split() isn't used for brd, so this limit isn't respected
    since v4.3.
    
    Also max_hw_sectors limit doesn't play a big role for brd, which is
    added since brd is added to tree for unknown reason.
    
    So remove it.
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/brd.c
+++ drivers/block/brd.c
@@ -382,7 +382,6 @@ static struct brd_device *brd_alloc(int i)
 		goto out_free_dev;
 
 	blk_queue_make_request(brd->brd_queue, brd_make_request);
-	blk_queue_max_hw_sectors(brd->brd_queue, 1024);
 
 	/* This is so fdisk will align partitions on 4k, because of
 	 * direct_access API needing 4k alignment, returning a PFN
