2703bafcdbf7d41562b8e8c877ed94c3aec70ac4
Felix Fietkau <nbd@nbd.name>
2019-12-05 17:32:41 +0100

    mt76: fix rx dma ring descriptor state on reset
    To avoid having the hardware potentially write to memory behind stale
    descriptors, set the dma-done flag on all of them before cleaning up
    allocated rx buffers
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/dma.c
+++ drivers/net/wireless/mediatek/mt76/dma.c
@@ -437,7 +437,7 @@ mt76_dma_rx_reset(struct mt76_dev *dev, enum mt76_rxq_id qid)
 	int i;
 
 	for (i = 0; i < q->ndesc; i++)
-		q->desc[i].ctrl &= ~cpu_to_le32(MT_DMA_CTL_DMA_DONE);
+		q->desc[i].ctrl = cpu_to_le32(MT_DMA_CTL_DMA_DONE);
 
 	mt76_dma_rx_cleanup(dev, q);
 	mt76_dma_sync_idx(dev, q);
