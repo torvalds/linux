7b49fabfc35c28deae514f07d563b0fb42c7cfea
Tudor Ambarus <tudor.ambarus@microchip.com>
2019-12-05 09:53:45 +0000

    crypto: atmel-tdes - Constify value to write to hw
    atmel_tdes_write_n() should not modify its value argument.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/atmel-tdes.c
+++ drivers/crypto/atmel-tdes.c
@@ -189,7 +189,7 @@ static inline void atmel_tdes_write(struct atmel_tdes_dev *dd,
 }
 
 static void atmel_tdes_write_n(struct atmel_tdes_dev *dd, u32 offset,
-					u32 *value, int count)
+			       const u32 *value, int count)
 {
 	for (; count--; value++, offset += 4)
 		atmel_tdes_write(dd, offset, *value);
