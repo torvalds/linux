b0ba4bf2a8f56103d71b6e7942baa853747aea92
Andrey Konovalov <andreyknvl@google.com>
2019-12-04 16:52:50 -0800

    UPSTREAM: vhost, kcov: collect coverage from vhost_worker
    (Upstream commit 8f6a7f96dc29cefe16ab60f06f9c3a43510b96fd.)
    
    Add kcov_remote_start()/kcov_remote_stop() annotations to the
    vhost_worker() function, which is responsible for processing vhost
    works.
    
    Since vhost_worker() threads are spawned per vhost device instance the
    common kcov handle is used for kcov_remote_start()/stop() annotations
    (see Documentation/dev-tools/kcov.rst for details).  As the result kcov
    can now be used to collect coverage from vhost worker threads.
    
    Link: http://lkml.kernel.org/r/e49d5d154e5da6c9ada521d2b7ce10a49ce9f98b.1572366574.git.andreyknvl@google.com
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Cc: Alan Stern <stern@rowland.harvard.edu>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Anders Roxell <anders.roxell@linaro.org>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: David Windsor <dwindsor@gmail.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Elena Reshetova <elena.reshetova@intel.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Jason Wang <jasowang@redhat.com>
    Cc: Marco Elver <elver@google.com>
    Cc: "Michael S. Tsirkin" <mst@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Bug: 147413187
    Change-Id: Ie99a67ede00a839a28472877e5c3263db69d1c58

--- drivers/vhost/vhost.c
+++ drivers/vhost/vhost.c
@@ -31,6 +31,7 @@
 #include <linux/sched/signal.h>
 #include <linux/interval_tree_generic.h>
 #include <linux/nospec.h>
+#include <linux/kcov.h>
 
 #include "vhost.h"
 
@@ -359,7 +360,9 @@ static int vhost_worker(void *data)
 		llist_for_each_entry_safe(work, work_next, node, node) {
 			clear_bit(VHOST_WORK_QUEUED, &work->flags);
 			__set_current_state(TASK_RUNNING);
+			kcov_remote_start_common(dev->kcov_handle);
 			work->fn(work);
+			kcov_remote_stop();
 			if (need_resched())
 				schedule();
 		}
@@ -522,6 +525,7 @@ long vhost_dev_set_owner(struct vhost_dev *dev)
 
 	/* No owner, become one */
 	dev->mm = get_task_mm(current);
+	dev->kcov_handle = kcov_common_handle();
 	worker = kthread_create(vhost_worker, dev, "vhost-%d", current->pid);
 	if (IS_ERR(worker)) {
 		err = PTR_ERR(worker);
@@ -547,6 +551,7 @@ err_worker:
 	if (dev->mm)
 		mmput(dev->mm);
 	dev->mm = NULL;
+	dev->kcov_handle = 0;
 err_mm:
 	return err;
 }
@@ -658,6 +663,7 @@ void vhost_dev_cleanup(struct vhost_dev *dev)
 	if (dev->worker) {
 		kthread_stop(dev->worker);
 		dev->worker = NULL;
+		dev->kcov_handle = 0;
 	}
 	if (dev->mm)
 		mmput(dev->mm);
