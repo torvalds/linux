9a6bed4fe0c8bf57785cbc4db9f86086cb9b193d
Jeff Layton <jlayton@kernel.org>
2019-12-05 08:41:25 -0500

    ceph: ensure we have a new cap before continuing in fill_inode
    If the caller passes in a NULL cap_reservation, and we can't allocate
    one then ensure that we fail gracefully.
    
    Signed-off-by: Jeff Layton <jlayton@kernel.org>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- fs/ceph/inode.c
+++ fs/ceph/inode.c
@@ -753,8 +753,11 @@ static int fill_inode(struct inode *inode, struct page *locked_page,
 	info_caps = le32_to_cpu(info->cap.caps);
 
 	/* prealloc new cap struct */
-	if (info_caps && ceph_snap(inode) == CEPH_NOSNAP)
+	if (info_caps && ceph_snap(inode) == CEPH_NOSNAP) {
 		new_cap = ceph_get_cap(mdsc, caps_reservation);
+		if (!new_cap)
+			return -ENOMEM;
+	}
 
 	/*
 	 * prealloc xattr data, if it looks like we'll need it.  only
