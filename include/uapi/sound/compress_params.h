cf675d71fe50a61b2c417af69f8f12c036def07c
Sanjana B <sanjb@codeaurora.org>
2019-12-06 17:08:57 +0530

    ANDROID: GKI: kernel: sound: update codec options with block size
    Add block size for dsd in codec options.
    
    Change-Id: I2653cc85ca5a526917d12ef5b2f5d5fa776945a2
    Bug: 155087902
    Signed-off-by: Sanjana B <sanjb@codeaurora.org>
    (cherry picked from commit fa70ef709268933ac02daf50179cde4ab868140d)
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- include/uapi/sound/compress_params.h
+++ include/uapi/sound/compress_params.h
@@ -419,6 +419,13 @@ struct snd_dec_aptx {
 	__u32 nap;
 };
 
+/** struct snd_dec_dsd - codec for DSD format
+ * @blk_size - dsd channel block size
+ */
+struct snd_dec_dsd {
+	__u32 blk_size;
+};
+
 /** struct snd_dec_pcm - codec options for PCM format
  * @num_channels: Number of channels
  * @ch_map: Channel map for the above corresponding channels
@@ -445,6 +452,7 @@ union snd_codec_options {
 	struct snd_dec_thd truehd;
 	struct snd_dec_pcm pcm_dec;
 	struct snd_dec_amrwb_plus amrwbplus;
+	struct snd_dec_dsd dsd_dec;
 };
 
 /** struct snd_codec_desc - description of codec capabilities
