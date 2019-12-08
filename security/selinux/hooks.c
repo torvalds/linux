1d794c2702cef02a75c115ede6c626cbbf29033e
Hridya Valsaraju <hridya@google.com>
2019-12-08 12:43:44 -0800

    FROMLIST: security: selinux: allow per-file labelling for binderfs
    This patch allows genfscon per-file labeling for binderfs.
    This is required to have separate permissions to allow
    access to binder, hwbinder and vndbinder devices which are
    relocating to binderfs.
    
    Acked-by: Jeff Vander Stoep <jeffv@google.com>
    Acked-by: Mark Salyzyn <salyzyn@android.com>
    Signed-off-by: Hridya Valsaraju <hridya@google.com>
    
    Bug: 136497735
    (cherry picked from commit 7a4b51947475a7f67e2bd06c4a4c768e2e64a975
    git://git.kernel.org/pub/scm/linux/kernel/git/pcmoore/selinux.git
    master)
    Link: https://lore.kernel.org/patchwork/patch/1175776/
    Change-Id: I105cc54b30ddd4120dc23a363bddc2f9d00e4dc4

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -871,6 +871,7 @@ static int selinux_set_mnt_opts(struct super_block *sb,
 	    !strcmp(sb->s_type->name, "tracefs") ||
 	    !strcmp(sb->s_type->name, "sysfs") ||
 	    !strcmp(sb->s_type->name, "pstore") ||
+	    !strcmp(sb->s_type->name, "binder") ||
 	    !strcmp(sb->s_type->name, "cgroup") ||
 	    !strcmp(sb->s_type->name, "cgroup2"))
 		sbsec->flags |= SE_SBGENFS;
