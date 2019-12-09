556d687a4ccd54ab50a721ddde42c820545effd9
Arnd Bergmann <arnd@arndb.de>
2019-12-09 16:16:20 +0100

    compat: ARM64: always include asm-generic/compat.h
    In order to use compat_* type defininitions in device drivers
    outside of CONFIG_COMPAT, move the inclusion of asm-generic/compat.h
    ahead of the #ifdef.
    
    All other architectures already do this.
    
    Acked-by: Will Deacon <will@kernel.org>
    Reviewed-by: Ben Hutchings <ben.hutchings@codethink.co.uk>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm64/include/asm/compat.h
+++ arch/arm64/include/asm/compat.h
@@ -4,6 +4,9 @@
  */
 #ifndef __ASM_COMPAT_H
 #define __ASM_COMPAT_H
+
+#include <asm-generic/compat.h>
+
 #ifdef CONFIG_COMPAT
 
 /*
@@ -12,8 +15,6 @@
 #include <linux/types.h>
 #include <linux/sched.h>
 #include <linux/sched/task_stack.h>
-
-#include <asm-generic/compat.h>
 
 #define COMPAT_USER_HZ		100
 #ifdef __AARCH64EB__
