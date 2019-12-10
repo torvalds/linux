52e4607dace1eeeb2e012fca291dc4e6cb449bff
Paul Cercueil <paul@crapouillou.net>
2019-12-10 15:41:39 +0100

    gpu/drm: ingenic: Use the plane's src_[x,y] to configure DMA length
    Instead of obtaining the width/height of the framebuffer from the CRTC
    state, obtain it from the current plane state.
    
    v2: No change
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191210144142.33143-3-paul@crapouillou.net
    # *** extracted tags ***
    Acked-by: Sam Ravnborg <sam@ravnborg.org>

--- drivers/gpu/drm/ingenic/ingenic-drm.c
+++ drivers/gpu/drm/ingenic/ingenic-drm.c
@@ -376,8 +376,8 @@ static void ingenic_drm_plane_atomic_update(struct drm_plane *plane,
 
 	if (state && state->fb) {
 		addr = drm_fb_cma_get_gem_addr(state->fb, state, 0);
-		width = state->crtc->state->adjusted_mode.hdisplay;
-		height = state->crtc->state->adjusted_mode.vdisplay;
+		width = state->src_w >> 16;
+		height = state->src_h >> 16;
 		cpp = state->fb->format->cpp[plane->index];
 
 		priv->dma_hwdesc->addr = addr;
