d39d994a910b4a3797bdc6497ac2a2ffe31355c7
Rander Wang <rander.wang@intel.com>
2019-12-11 15:03:20 +0800

    soundwire: intel: refine function for wakeen event processing
    if a slave has been attached to a bus, the slave->dev_num_sticky
    should be non-zero, so we can check this value to skip the
    ghost devices defined in ACPI table but not populated in hardware.
    
    Signed-off-by: Rander Wang <rander.wang@intel.com>

--- drivers/soundwire/intel.c
+++ drivers/soundwire/intel.c
@@ -1509,13 +1509,13 @@ static void intel_master_process_wakeen_event(struct sdw_master_device *md)
 	 * the wakeen event and let codec driver check codec status
 	 */
 	list_for_each_entry(slave, &bus->slaves, node) {
-		if (slave->prop.wake_capable) {
-			if (slave->status != SDW_SLAVE_ATTACHED &&
-			    slave->status != SDW_SLAVE_ALERT)
-				continue;
-
+		/*
+		 * discard devices that are defined in ACPI tables but
+		 * not physically present and devices that cannot
+		 * generate wakes
+		 */
+		if (slave->dev_num_sticky && slave->prop.wake_capable)
 			pm_request_resume(&slave->dev);
-		}
 	}
 }
 
