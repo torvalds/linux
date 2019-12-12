f8e963178919fb37426efa781aa595705ce25dd3
Paul Kocialkowski <paul.kocialkowski@bootlin.com>
2019-12-12 16:31:11 +0100

    UPSTREAM: rtc: hym8563: Read the valid flag directly instead of caching it
    The RTC has a valid bit in the seconds register that indicates whether
    power was lost since the pevious time set. This bit is currently read
    once at probe time, cached and updated with set_time.
    
    Howeever, caching the bit may prevent detecting power loss at runtime
    (which can happen if the RTC's supply is distinct from the the platform's).
    
    Writing the seconds register when setting time will clear the bit,
    so there should be no downside in reading the bit directly instead of
    caching it.
    
    Change-Id: I85d1dcfb32b12ed24d5f42f67fb9beb5d8d03876
    Signed-off-by: Paul Kocialkowski <paul.kocialkowski@bootlin.com>
    Link: https://lore.kernel.org/r/20191212153111.966923-2-paul.kocialkowski@bootlin.com
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>
    (cherry picked from commit e2ed7507ae89b92b67025f221448b543e2333b21)

--- drivers/rtc/rtc-hym8563.c
+++ drivers/rtc/rtc-hym8563.c
@@ -86,7 +86,6 @@
 struct hym8563 {
 	struct i2c_client	*client;
 	struct rtc_device	*rtc;
-	bool			valid;
 #ifdef CONFIG_COMMON_CLK
 	struct clk_hw		clkout_hw;
 #endif
@@ -99,16 +98,16 @@ struct hym8563 {
 static int hym8563_rtc_read_time(struct device *dev, struct rtc_time *tm)
 {
 	struct i2c_client *client = to_i2c_client(dev);
-	struct hym8563 *hym8563 = i2c_get_clientdata(client);
 	u8 buf[7];
 	int ret;
 
-	if (!hym8563->valid) {
-		dev_warn(&client->dev, "no valid clock/calendar values available\n");
+	ret = i2c_smbus_read_i2c_block_data(client, HYM8563_SEC, 7, buf);
+
+	if (buf[0] & HYM8563_SEC_VL) {
+		dev_warn(&client->dev,
+			 "no valid clock/calendar values available\n");
 		return -EINVAL;
 	}
-
-	ret = i2c_smbus_read_i2c_block_data(client, HYM8563_SEC, 7, buf);
 
 	tm->tm_sec = bcd2bin(buf[0] & HYM8563_SEC_MASK);
 	tm->tm_min = bcd2bin(buf[1] & HYM8563_MIN_MASK);
@@ -124,7 +123,6 @@ static int hym8563_rtc_read_time(struct device *dev, struct rtc_time *tm)
 static int hym8563_rtc_set_time(struct device *dev, struct rtc_time *tm)
 {
 	struct i2c_client *client = to_i2c_client(dev);
-	struct hym8563 *hym8563 = i2c_get_clientdata(client);
 	u8 buf[7];
 	int ret;
 
@@ -162,8 +160,6 @@ static int hym8563_rtc_set_time(struct device *dev, struct rtc_time *tm)
 	ret = i2c_smbus_write_byte_data(client, HYM8563_CTL1, 0);
 	if (ret < 0)
 		return ret;
-
-	hym8563->valid = true;
 
 	return 0;
 }
@@ -562,9 +558,8 @@ static int hym8563_probe(struct i2c_client *client,
 	if (ret < 0)
 		return ret;
 
-	hym8563->valid = !(ret & HYM8563_SEC_VL);
 	dev_dbg(&client->dev, "rtc information is %s\n",
-		hym8563->valid ? "valid" : "invalid");
+		(ret & HYM8563_SEC_VL) ? "invalid" : "valid");
 
 	hym8563->rtc = devm_rtc_device_register(&client->dev, client->name,
 						&hym8563_rtc_ops, THIS_MODULE);
