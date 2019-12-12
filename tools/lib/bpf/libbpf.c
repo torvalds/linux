67d69ccdf389f3b7fabb00e2d81473a915b3b64d
Jakub Sitnicki <jakub@cloudflare.com>
2019-12-12 11:22:50 +0100

    libbpf: Recognize SK_REUSEPORT programs from section name
    Allow loading BPF object files that contain SK_REUSEPORT programs without
    having to manually set the program type before load if the the section name
    is set to "sk_reuseport".
    
    Makes user-space code needed to load SK_REUSEPORT BPF program more concise.
    
    Signed-off-by: Jakub Sitnicki <jakub@cloudflare.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20191212102259.418536-2-jakub@cloudflare.com

--- tools/lib/bpf/libbpf.c
+++ tools/lib/bpf/libbpf.c
@@ -4983,6 +4983,7 @@ static const struct {
 	enum bpf_attach_type attach_type;
 } section_names[] = {
 	BPF_PROG_SEC("socket",			BPF_PROG_TYPE_SOCKET_FILTER),
+	BPF_PROG_SEC("sk_reuseport",		BPF_PROG_TYPE_SK_REUSEPORT),
 	BPF_PROG_SEC("kprobe/",			BPF_PROG_TYPE_KPROBE),
 	BPF_PROG_SEC("uprobe/",			BPF_PROG_TYPE_KPROBE),
 	BPF_PROG_SEC("kretprobe/",		BPF_PROG_TYPE_KPROBE),
