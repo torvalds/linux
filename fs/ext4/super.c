cf2834a5ed57562d6a1a8170724704149f0ae0a4
Theodore Ts'o <tytso@mit.edu>
2019-12-14 16:42:52 -0500

    ext4: treat buffers contining write errors as valid in ext4_sb_bread()
    In commit 7963e5ac9012 ("ext4: treat buffers with write errors as
    containing valid data") we missed changing ext4_sb_bread() to use
    ext4_buffer_uptodate().  So fix this oversight.
    
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -154,7 +154,7 @@ ext4_sb_bread(struct super_block *sb, sector_t block, int op_flags)
 
 	if (bh == NULL)
 		return ERR_PTR(-ENOMEM);
-	if (buffer_uptodate(bh))
+	if (ext4_buffer_uptodate(bh))
 		return bh;
 	ll_rw_block(REQ_OP_READ, REQ_META | op_flags, 1, &bh);
 	wait_on_buffer(bh);
