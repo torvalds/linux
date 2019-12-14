1faa9d3a3ea7852761ff403f5a9d4a409c0bb199
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-12-14 22:13:51 +0900

    ALSA: control: remove useless assignment in .info callback of PCM chmap element
    Control elements for PCM chmap return information to userspace abount
    the maximum number of available PCM channels as the number of values
    in the element.
    
    In current implementation the number is once initialized to zero, then
    assigned to. This is useless and this commit fixes it.
    
    Fixes: 2d3391ec0ecc ("ALSA: PCM: channel mapping API implementation")
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Link: https://lore.kernel.org/r/20191214131351.28950-1-o-takashi@sakamocchi.jp
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/pcm_lib.c
+++ sound/core/pcm_lib.c
@@ -2308,7 +2308,6 @@ static int pcm_chmap_ctl_info(struct snd_kcontrol *kcontrol,
 	struct snd_pcm_chmap *info = snd_kcontrol_chip(kcontrol);
 
 	uinfo->type = SNDRV_CTL_ELEM_TYPE_INTEGER;
-	uinfo->count = 0;
 	uinfo->count = info->max_channels;
 	uinfo->value.integer.min = 0;
 	uinfo->value.integer.max = SNDRV_CHMAP_LAST;
