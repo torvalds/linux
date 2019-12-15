1ee7826ab68f7e9fa1a01533983acf6a6f62e297
Aditya Pakki <pakki001@umn.edu>
2019-12-15 09:23:48 -0600

    mac80211: Remove redundant assertion
    In wiphy_to_ieee80211_hw, the assertion to check if wiphy is NULL is
    repeated in wiphy_priv. The patch removes the duplicated BUG_ON check.
    
    Signed-off-by: Aditya Pakki <pakki001@umn.edu>
    Link: https://lore.kernel.org/r/20191215152348.20912-1-pakki001@umn.edu
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/util.c
+++ net/mac80211/util.c
@@ -39,7 +39,6 @@ const void *const mac80211_wiphy_privid = &mac80211_wiphy_privid;
 struct ieee80211_hw *wiphy_to_ieee80211_hw(struct wiphy *wiphy)
 {
 	struct ieee80211_local *local;
-	BUG_ON(!wiphy);
 
 	local = wiphy_priv(wiphy);
 	return &local->hw;
