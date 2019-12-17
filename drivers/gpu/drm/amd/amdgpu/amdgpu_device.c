5a7489a7e189ee2be889485f90c8cf24ea4b9a40
Monk Liu <Monk.Liu@amd.com>
2019-12-17 18:16:44 +0800

    drm/amdgpu: fix KIQ ring test fail in TDR of SRIOV
    issues:
    MEC is ruined by the amdkfd_pre_reset after VF FLR done
    
    fix:
    amdkfd_pre_reset() would ruin MEC after hypervisor finished the VF FLR,
    the correct sequence is do amdkfd_pre_reset before VF FLR but there is
    a limitation to block this sequence:
    if we do pre_reset() before VF FLR, it would go KIQ way to do register
    access and stuck there, because KIQ probably won't work by that time
    (e.g. you already made GFX hang)
    
    so the best way right now is to simply remove it.
    
    Signed-off-by: Monk Liu <Monk.Liu@amd.com>
    Reviewed-by: Emily Deng <Emily.Deng@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -3669,8 +3669,6 @@ static int amdgpu_device_reset_sriov(struct amdgpu_device *adev,
 	if (r)
 		return r;
 
-	amdgpu_amdkfd_pre_reset(adev);
-
 	/* Resume IP prior to SMC */
 	r = amdgpu_device_ip_reinit_early_sriov(adev);
 	if (r)
