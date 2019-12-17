277fdbc03557664894c1584748a7c2332b895deb
Fabrizio Castro <fabrizio.castro@bp.renesas.com>
2019-12-17 13:45:56 +0000

    UPSTREAM: drm: of: Add drm_of_lvds_get_dual_link_pixel_order
    An LVDS dual-link connection is made of two links, with even
    pixels transitting on one link, and odd pixels on the other
    link. The device tree can be used to fully describe dual-link
    LVDS connections between encoders and bridges/panels.
    The sink of an LVDS dual-link connection is made of two ports,
    the corresponding OF graph port nodes can be marked
    with either dual-lvds-even-pixels or dual-lvds-odd-pixels,
    and that fully describes an LVDS dual-link connection,
    including pixel order.
    
    drm_of_lvds_get_dual_link_pixel_order is a new helper
    added by this patch, given the source port nodes it
    returns DRM_LVDS_DUAL_LINK_EVEN_ODD_PIXELS if the source
    port nodes belong to an LVDS dual-link connection, with even
    pixels expected to be generated from the first port, and odd
    pixels expected to be generated from the second port.
    If the new helper returns DRM_LVDS_DUAL_LINK_ODD_EVEN_PIXELS,
    odd pixels are expected to be generated from the first port,
    and even pixels from the other port.
    
    Signed-off-by: Fabrizio Castro <fabrizio.castro@bp.renesas.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    (cherry picked from commit 6529007522ded00b8912c079250620fa7a732166)
    Change-Id: I9dcf69801d75592bc6e721b40734cb766a29366a
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>

--- include/drm/drm_of.h
+++ include/drm/drm_of.h
@@ -16,6 +16,18 @@ struct drm_panel;
 struct drm_bridge;
 struct device_node;
 
+/**
+ * enum drm_lvds_dual_link_pixels - Pixel order of an LVDS dual-link connection
+ * @DRM_LVDS_DUAL_LINK_EVEN_ODD_PIXELS: Even pixels are expected to be generated
+ *    from the first port, odd pixels from the second port
+ * @DRM_LVDS_DUAL_LINK_ODD_EVEN_PIXELS: Odd pixels are expected to be generated
+ *    from the first port, even pixels from the second port
+ */
+enum drm_lvds_dual_link_pixels {
+	DRM_LVDS_DUAL_LINK_EVEN_ODD_PIXELS = 0,
+	DRM_LVDS_DUAL_LINK_ODD_EVEN_PIXELS = 1,
+};
+
 #ifdef CONFIG_OF
 uint32_t drm_of_crtc_port_mask(struct drm_device *dev,
 			    struct device_node *port);
@@ -35,6 +47,8 @@ int drm_of_find_panel_or_bridge(const struct device_node *np,
 				int port, int endpoint,
 				struct drm_panel **panel,
 				struct drm_bridge **bridge);
+int drm_of_lvds_get_dual_link_pixel_order(const struct device_node *port1,
+					  const struct device_node *port2);
 #else
 static inline uint32_t drm_of_crtc_port_mask(struct drm_device *dev,
 					  struct device_node *port)
@@ -74,6 +88,12 @@ static inline int drm_of_find_panel_or_bridge(const struct device_node *np,
 					      int port, int endpoint,
 					      struct drm_panel **panel,
 					      struct drm_bridge **bridge)
+{
+	return -EINVAL;
+}
+
+int drm_of_lvds_get_dual_link_pixel_order(const struct device_node *port1,
+					  const struct device_node *port2)
 {
 	return -EINVAL;
 }
