833a20b8d4924b63ef7700e43fe4941963002822
Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
2019-12-19 16:30:40 +0300

    ARC: asm-offsets: remove duplicate entry
    We define 'PT_user_r25' twice in asm-offsets.c
    It's not a big issue as we define it to the same value, however
    let's fix it.
    
    Signed-off-by: Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/kernel/asm-offsets.c
+++ arch/arc/kernel/asm-offsets.c
@@ -66,7 +66,6 @@ int main(void)
 
 	DEFINE(SZ_CALLEE_REGS, sizeof(struct callee_regs));
 	DEFINE(SZ_PT_REGS, sizeof(struct pt_regs));
-	DEFINE(PT_user_r25, offsetof(struct pt_regs, user_r25));
 
 	return 0;
 }
