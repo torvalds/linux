6b8651aac1dca6140dd7fb4c9fec2736ed3f6223
Zhengyuan Liu <liuzhengyuan@kylinos.cn>
2019-12-20 10:21:26 +0800

    raid6/test: fix a compilation error
    The compilation error is redeclaration showed as following:
    
            In file included from ../../../include/linux/limits.h:6,
                             from /usr/include/x86_64-linux-gnu/bits/local_lim.h:38,
                             from /usr/include/x86_64-linux-gnu/bits/posix1_lim.h:161,
                             from /usr/include/limits.h:183,
                             from /usr/lib/gcc/x86_64-linux-gnu/8/include-fixed/limits.h:194,
                             from /usr/lib/gcc/x86_64-linux-gnu/8/include-fixed/syslimits.h:7,
                             from /usr/lib/gcc/x86_64-linux-gnu/8/include-fixed/limits.h:34,
                             from ../../../include/linux/raid/pq.h:30,
                             from algos.c:14:
            ../../../include/linux/types.h:114:15: error: conflicting types for ‘int64_t’
             typedef s64   int64_t;
                           ^~~~~~~
            In file included from /usr/include/stdint.h:34,
                             from /usr/lib/gcc/x86_64-linux-gnu/8/include/stdint.h:9,
                             from /usr/include/inttypes.h:27,
                             from ../../../include/linux/raid/pq.h:29,
                             from algos.c:14:
            /usr/include/x86_64-linux-gnu/bits/stdint-intn.h:27:19: note: previous \
            declaration of ‘int64_t’ was here
             typedef __int64_t int64_t;
    
    Fixes: 54d50897d544 ("linux/kernel.h: split *_MAX and *_MIN macros into <linux/limits.h>")
    Signed-off-by: Zhengyuan Liu <liuzhengyuan@kylinos.cn>
    Signed-off-by: Song Liu <songliubraving@fb.com>

--- include/linux/raid/pq.h
+++ include/linux/raid/pq.h
@@ -27,7 +27,6 @@ extern const char raid6_empty_zero_page[PAGE_SIZE];
 
 #include <errno.h>
 #include <inttypes.h>
-#include <limits.h>
 #include <stddef.h>
 #include <sys/mman.h>
 #include <sys/time.h>
