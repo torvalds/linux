3ee50c896d712dc2fc8f34c2cd1918d035e74045
Eneas U de Queiroz <cotequeiroz@gmail.com>
2019-12-20 16:02:15 -0300

    crypto: qce - save a sg table slot for result buf
    When ctr-aes-qce is used for gcm-mode, an extra sg entry for the
    authentication tag is present, causing trouble when the qce driver
    prepares the dst-results sg table for dma.
    
    It computes the number of entries needed with sg_nents_for_len, leaving
    out the tag entry.  Then it creates a sg table with that number plus
    one, used to store a result buffer.
    
    When copying the sg table, there's no limit to the number of entries
    copied, so the extra slot is filled with the authentication tag sg.
    When the driver tries to add the result sg, the list is full, and it
    returns EINVAL.
    
    By limiting the number of sg entries copied to the dest table, the slot
    for the result buffer is guaranteed to be unused.
    
    Signed-off-by: Eneas U de Queiroz <cotequeiroz@gmail.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/qce/skcipher.c
+++ drivers/crypto/qce/skcipher.c
@@ -95,13 +95,13 @@ qce_skcipher_async_req_handle(struct crypto_async_request *async_req)
 
 	sg_init_one(&rctx->result_sg, qce->dma.result_buf, QCE_RESULT_BUF_SZ);
 
-	sg = qce_sgtable_add(&rctx->dst_tbl, req->dst);
+	sg = qce_sgtable_add(&rctx->dst_tbl, req->dst, rctx->dst_nents - 1);
 	if (IS_ERR(sg)) {
 		ret = PTR_ERR(sg);
 		goto error_free;
 	}
 
-	sg = qce_sgtable_add(&rctx->dst_tbl, &rctx->result_sg);
+	sg = qce_sgtable_add(&rctx->dst_tbl, &rctx->result_sg, 1);
 	if (IS_ERR(sg)) {
 		ret = PTR_ERR(sg);
 		goto error_free;
