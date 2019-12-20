8e1a20096bfbc1fc05f68abcb8cf20fc07dca8a1
Xu Wang <vulab@iscas.ac.cn>
2019-12-20 07:19:38 +0000

    usb: cdns3: gadget: Remove unneeded variable ret
    Remove unneeded variable ret used to store return value,just return 0.
    
    Signed-off-by: Xu Wang <vulab@iscas.ac.cn>
    Acked-by: Roger Quadros <rogerq@ti.com>
    Link: https://lore.kernel.org/r/1576826378-4387-1-git-send-email-vulab@iscas.ac.cn
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/cdns3/gadget.c
+++ drivers/usb/cdns3/gadget.c
@@ -2389,7 +2389,6 @@ static int cdns3_gadget_udc_stop(struct usb_gadget *gadget)
 	struct cdns3_endpoint *priv_ep;
 	u32 bEndpointAddress;
 	struct usb_ep *ep;
-	int ret = 0;
 	int val;
 
 	priv_dev->gadget_driver = NULL;
@@ -2413,7 +2412,7 @@ static int cdns3_gadget_udc_stop(struct usb_gadget *gadget)
 	writel(0, &priv_dev->regs->usb_ien);
 	writel(USB_CONF_DEVDS, &priv_dev->regs->usb_conf);
 
-	return ret;
+	return 0;
 }
 
 static const struct usb_gadget_ops cdns3_gadget_ops = {
