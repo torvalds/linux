501444905fcb4166589fda99497c273ac5efc65e
Sean Christopherson <sean.j.christopherson@intel.com>
2019-12-20 20:45:00 -0800

    x86/centaur: Use common IA32_FEAT_CTL MSR initialization
    Use the recently added IA32_FEAT_CTL MSR initialization sequence to
    opportunistically enable VMX support when running on a Centaur CPU.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lkml.kernel.org/r/20191221044513.21680-7-sean.j.christopherson@intel.com

--- arch/x86/kernel/cpu/centaur.c
+++ arch/x86/kernel/cpu/centaur.c
@@ -250,6 +250,8 @@ static void init_centaur(struct cpuinfo_x86 *c)
 	set_cpu_cap(c, X86_FEATURE_LFENCE_RDTSC);
 #endif
 
+	init_ia32_feat_ctl(c);
+
 	if (cpu_has(c, X86_FEATURE_VMX))
 		centaur_detect_vmx_virtcap(c);
 }
