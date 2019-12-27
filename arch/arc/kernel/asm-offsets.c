7ecc6c1d5c8dbc713c647512a5267ca0eafe3e1c
Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
2019-12-27 21:03:43 +0300

    ARC: pt_regs: remove hardcoded registers offset
    Replace hardcoded registers offset numbers by calculated via
    offsetof.
    
    Signed-off-by: Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/kernel/asm-offsets.c
+++ arch/arc/kernel/asm-offsets.c
@@ -67,5 +67,14 @@ int main(void)
 	DEFINE(SZ_CALLEE_REGS, sizeof(struct callee_regs));
 	DEFINE(SZ_PT_REGS, sizeof(struct pt_regs));
 
+#ifdef CONFIG_ISA_ARCV2
+	OFFSET(PT_r12, pt_regs, r12);
+	OFFSET(PT_r30, pt_regs, r30);
+#endif
+#ifdef CONFIG_ARC_HAS_ACCL_REGS
+	OFFSET(PT_r58, pt_regs, r58);
+	OFFSET(PT_r59, pt_regs, r59);
+#endif
+
 	return 0;
 }
