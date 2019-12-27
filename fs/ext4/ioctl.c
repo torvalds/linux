ffc2c1d651ee2bb24c5224ac7967c4907c50a389
Martijn Coenen <maco@android.com>
2019-12-27 14:37:40 +0100

    FROMGIT: ext4: Add EXT4_IOC_FSGETXATTR/EXT4_IOC_FSSETXATTR to compat_ioctl.
    These are backed by 'struct fsxattr' which has the same size on all
    architectures.
    
    Signed-off-by: Martijn Coenen <maco@android.com>
    Link: https://lore.kernel.org/patchwork/patch/1172430/
    Signed-off-by: Martijn Coenen <maco@google.com>
    (cherry picked from commit a54d8d34d2354f3a2a9dda00d9dd6666a50c486b git://git.kernel.org/pub/scm/linux/kernel/git/tytso/ext4.git dev)
    Change-Id: Ie7a047b94415ef7c19fa534f47741ebd79dcc909

--- fs/ext4/ioctl.c
+++ fs/ext4/ioctl.c
@@ -1301,6 +1301,8 @@ long ext4_compat_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	case FS_IOC_GETFSMAP:
 	case FS_IOC_ENABLE_VERITY:
 	case FS_IOC_MEASURE_VERITY:
+	case EXT4_IOC_FSGETXATTR:
+	case EXT4_IOC_FSSETXATTR:
 		break;
 	default:
 		return -ENOIOCTLCMD;
