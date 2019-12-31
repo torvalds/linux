1ee48a61aa57dbdbc3cd2808d8b28df40d938e44
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2019-12-31 16:00:07 +0200

    drm/i915: Limit audio CDCLK>=2*BCLK constraint back to GLK only
    Revert changes done in commit f6ec9483091f ("drm/i915: extend audio
    CDCLK>=2*BCLK constraint to more platforms"). Audio drivers
    communicate with i915 over HDA bus multiple times during system
    boot-up and each of these transactions result in matching
    get_power/put_power calls to i915, and depending on the platform,
    a modeset change causing visible flicker.
    
    GLK is the only platform with minimum CDCLK significantly lower
    than BCLK, and thus for GLK setting a higher CDCLK is mandatory.
    
    For other platforms, minimum CDCLK is close but below 2*BCLK
    (e.g. on ICL, CDCLK=176.4kHz with BCLK=96kHz). Spec-wise the constraint
    should be set, but in practise no communication errors have been
    reported and the downside if set is the flicker observed at boot-time.
    
    Revert to old behaviour until better mechanism to manage
    probe-time clocks is available.
    
    The full CDCLK>=2*BCLK constraint is still enforced at pipe
    enable time in intel_crtc_compute_min_cdclk().
    
    Bugzilla: https://gitlab.freedesktop.org/drm/intel/issues/913
    Fixes: f6ec9483091f ("drm/i915: extend audio CDCLK>=2*BCLK constraint to more platforms")
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191231140007.31728-1-kai.vehmanen@linux.intel.com

--- drivers/gpu/drm/i915/display/intel_audio.c
+++ drivers/gpu/drm/i915/display/intel_audio.c
@@ -856,7 +856,7 @@ static unsigned long i915_audio_component_get_power(struct device *kdev)
 		}
 
 		/* Force CDCLK to 2*BCLK as long as we need audio powered. */
-		if (INTEL_GEN(dev_priv) >= 10 || IS_GEMINILAKE(dev_priv))
+		if (IS_GEMINILAKE(dev_priv))
 			glk_force_audio_cdclk(dev_priv, true);
 
 		if (INTEL_GEN(dev_priv) >= 10 || IS_GEMINILAKE(dev_priv))
@@ -875,7 +875,7 @@ static void i915_audio_component_put_power(struct device *kdev,
 
 	/* Stop forcing CDCLK to 2*BCLK if no need for audio to be powered. */
 	if (--dev_priv->audio_power_refcount == 0)
-		if (INTEL_GEN(dev_priv) >= 10 || IS_GEMINILAKE(dev_priv))
+		if (IS_GEMINILAKE(dev_priv))
 			glk_force_audio_cdclk(dev_priv, false);
 
 	intel_display_power_put(dev_priv, POWER_DOMAIN_AUDIO, cookie);
