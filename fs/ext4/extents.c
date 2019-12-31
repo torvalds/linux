de7454854d6e8e3c3585c6df4b0b99017820aa0f
Eric Biggers <ebiggers@google.com>
2019-12-31 12:04:44 -0600

    ext4: add missing braces in ext4_ext_drop_refs()
    For clarity, add braces to the loop in ext4_ext_drop_refs().
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Link: https://lore.kernel.org/r/20191231180444.46586-9-ebiggers@kernel.org
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Reviewed-by: Ritesh Harjani <riteshh@linux.ibm.com>
    Reviewed-by: Jan Kara <jack@suse.cz>

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -690,11 +690,12 @@ void ext4_ext_drop_refs(struct ext4_ext_path *path)
 	if (!path)
 		return;
 	depth = path->p_depth;
-	for (i = 0; i <= depth; i++, path++)
+	for (i = 0; i <= depth; i++, path++) {
 		if (path->p_bh) {
 			brelse(path->p_bh);
 			path->p_bh = NULL;
 		}
+	}
 }
 
 /*
