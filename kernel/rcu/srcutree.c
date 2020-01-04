b68c6146512d92f6d570d26e1873497ade2cc4cb
Paul E. McKenney <paulmck@kernel.org>
2020-01-03 16:36:59 -0800

    srcu: Add data_race() to ->srcu_lock_count and ->srcu_unlock_count arrays
    The srcu_data structure's ->srcu_lock_count and ->srcu_unlock_count arrays
    are read and written locklessly, so this commit adds the data_race()
    to the diagnostic-print loads from these arrays in order mark them as
    known and approved data-racy accesses.
    
    This data race was reported by KCSAN. Not appropriate for backporting due
    to failure being unlikely and due to this being used only by rcutorture.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/srcutree.c
+++ kernel/rcu/srcutree.c
@@ -1281,8 +1281,8 @@ void srcu_torture_stats_print(struct srcu_struct *ssp, char *tt, char *tf)
 		struct srcu_data *sdp;
 
 		sdp = per_cpu_ptr(ssp->sda, cpu);
-		u0 = sdp->srcu_unlock_count[!idx];
-		u1 = sdp->srcu_unlock_count[idx];
+		u0 = data_race(sdp->srcu_unlock_count[!idx]);
+		u1 = data_race(sdp->srcu_unlock_count[idx]);
 
 		/*
 		 * Make sure that a lock is always counted if the corresponding
@@ -1290,8 +1290,8 @@ void srcu_torture_stats_print(struct srcu_struct *ssp, char *tt, char *tf)
 		 */
 		smp_rmb();
 
-		l0 = sdp->srcu_lock_count[!idx];
-		l1 = sdp->srcu_lock_count[idx];
+		l0 = data_race(sdp->srcu_lock_count[!idx]);
+		l1 = data_race(sdp->srcu_lock_count[idx]);
 
 		c0 = l0 - u0;
 		c1 = l1 - u1;
