42d068472ddf532f3ca2bcdd06d1ca6b53f57e5e
Yuti Amonkar <yamonkar@cadence.com>
2020-01-06 13:22:40 +0100

    phy: Add DisplayPort configuration options
    Allow DisplayPort PHYs to be configured through the generic
    functions through a custom structure added to the generic union.
    The configuration structure is used for reconfiguration of
    DisplayPort PHYs during link training operation.
    
    The parameters added here are the ones defined in the DisplayPort
    spec v1.4 which include link rate, number of lanes, voltage swing
    and pre-emphasis.
    
    Add the DisplayPort phy mode to the generic phy_mode enum.
    
    Signed-off-by: Yuti Amonkar <yamonkar@cadence.com>
    Reviewed-by: Maxime Ripard <mripard@kernel.org>
    Reviewed-by: Jyri Sarha <jsarha@ti.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- include/linux/phy/phy.h
+++ include/linux/phy/phy.h
@@ -16,6 +16,7 @@
 #include <linux/pm_runtime.h>
 #include <linux/regulator/consumer.h>
 
+#include <linux/phy/phy-dp.h>
 #include <linux/phy/phy-mipi-dphy.h>
 
 struct phy;
@@ -40,6 +41,7 @@ enum phy_mode {
 	PHY_MODE_MIPI_DPHY,
 	PHY_MODE_SATA,
 	PHY_MODE_LVDS,
+	PHY_MODE_DP
 };
 
 /**
@@ -47,9 +49,12 @@ enum phy_mode {
  *
  * @mipi_dphy:	Configuration set applicable for phys supporting
  *		the MIPI_DPHY phy mode.
+ * @dp:		Configuration set applicable for phys supporting
+ *		the DisplayPort protocol.
  */
 union phy_configure_opts {
 	struct phy_configure_opts_mipi_dphy	mipi_dphy;
+	struct phy_configure_opts_dp		dp;
 };
 
 /**
