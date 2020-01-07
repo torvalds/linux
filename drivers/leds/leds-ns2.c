c7896490dd1a4e6b346e8a475e4a433356362770
Linus Walleij <linus.walleij@linaro.org>
2020-01-07 15:10:28 +0100

    leds: ns2: Absorb platform data
    Nothing in the kernel includes the external header
    <linux/platform_data/leds-kirkwood-ns2.h> so just push the
    contents into the ns2 leds driver. If someone wants to use
    platform data or board files to describe this device they
    should be able to do so using GPIO machine descriptors but
    in any case device tree should be the way forward for these
    systems in all cases I can think of, and the driver already
    supports that.
    
    Cc: Simon Guinot <simon.guinot@sequanux.org>
    Cc: Vincent Donnefort <vdonnefort@gmail.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Tested-by: Simon Guinot <simon.guinot@sequanux.org>
    Signed-off-by: Pavel Machek <pavel@ucw.cz>

--- drivers/leds/leds-ns2.c
+++ drivers/leds/leds-ns2.c
@@ -12,13 +12,37 @@
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
-#include <linux/gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/leds.h>
 #include <linux/module.h>
-#include <linux/platform_data/leds-kirkwood-ns2.h>
 #include <linux/of.h>
-#include <linux/of_gpio.h>
 #include "leds.h"
+
+enum ns2_led_modes {
+	NS_V2_LED_OFF,
+	NS_V2_LED_ON,
+	NS_V2_LED_SATA,
+};
+
+struct ns2_led_modval {
+	enum ns2_led_modes	mode;
+	int			cmd_level;
+	int			slow_level;
+};
+
+struct ns2_led {
+	const char	*name;
+	const char	*default_trigger;
+	unsigned	cmd;
+	unsigned	slow;
+	int		num_modes;
+	struct ns2_led_modval *modval;
+};
+
+struct ns2_led_platform_data {
+	int		num_leds;
+	struct ns2_led	*leds;
+};
 
 /*
  * The Network Space v2 dual-GPIO LED is wired to a CPLD. Three different LED
