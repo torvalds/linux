43631922f314fed15c808b71d77ebff04038f93f
Herbert Xu <herbert@gondor.apana.org.au>
2020-01-08 12:37:35 +0800

    UPSTREAM: crypto: curve25519 - Fix selftest build error
    If CRYPTO_CURVE25519 is y, CRYPTO_LIB_CURVE25519_GENERIC will be
    y, but CRYPTO_LIB_CURVE25519 may be set to m, this causes build
    errors:
    
    lib/crypto/curve25519-selftest.o: In function `curve25519':
    curve25519-selftest.c:(.text.unlikely+0xc): undefined reference to `curve25519_arch'
    lib/crypto/curve25519-selftest.o: In function `curve25519_selftest':
    curve25519-selftest.c:(.init.text+0x17e): undefined reference to `curve25519_base_arch'
    
    This is because the curve25519 self-test code is being controlled
    by the GENERIC option rather than the overall CURVE25519 option,
    as is the case with blake2s.  To recap, the GENERIC and ARCH options
    for CURVE25519 are internal only and selected by users such as
    the Crypto API, or the externally visible CURVE25519 option which
    in turn is selected by wireguard.  The self-test is specific to the
    the external CURVE25519 option and should not be enabled by the
    Crypto API.
    
    This patch fixes this by splitting the GENERIC module from the
    CURVE25519 module with the latter now containing just the self-test.
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Fixes: aa127963f1ca ("crypto: lib/curve25519 - re-add selftests")
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Reviewed-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    (cherry picked from commit a8bdf2c42ee4d1ee42af1f3601f85de94e70a421)
    Bug: 152722841
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I933971a947ecd3845237f2a0d1786b4fc50727bc

--- lib/crypto/curve25519-generic.c
+++ lib/crypto/curve25519-generic.c
@@ -11,24 +11,13 @@
 
 #include <crypto/curve25519.h>
 #include <linux/module.h>
-#include <linux/init.h>
 
-bool curve25519_selftest(void);
+const u8 curve25519_null_point[CURVE25519_KEY_SIZE] __aligned(32) = { 0 };
+const u8 curve25519_base_point[CURVE25519_KEY_SIZE] __aligned(32) = { 9 };
 
-static int __init mod_init(void)
-{
-	if (!IS_ENABLED(CONFIG_CRYPTO_MANAGER_DISABLE_TESTS) &&
-	    WARN_ON(!curve25519_selftest()))
-		return -ENODEV;
-	return 0;
-}
-
-static void __exit mod_exit(void)
-{
-}
-
-module_init(mod_init);
-module_exit(mod_exit);
+EXPORT_SYMBOL(curve25519_null_point);
+EXPORT_SYMBOL(curve25519_base_point);
+EXPORT_SYMBOL(curve25519_generic);
 
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("Curve25519 scalar multiplication");
