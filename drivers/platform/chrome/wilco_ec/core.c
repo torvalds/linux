ffd7263e63fe80a39c1222a6d68e05095ed04d03
Daniel Campello <campello@chromium.org>
2020-01-08 09:35:20 -0700

    platform/chrome: wilco_ec: Fix unregistration order
    The unregistration should happen in the opposite order of
    the registration, so change it accordingly.
    
    No real issue has been noticed, but it is good practice to
    keep the correct unregistration order.
    
    Signed-off-by: Daniel Campello <campello@chromium.org>
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

--- drivers/platform/chrome/wilco_ec/core.c
+++ drivers/platform/chrome/wilco_ec/core.c
@@ -137,9 +137,9 @@ static int wilco_ec_remove(struct platform_device *pdev)
 {
 	struct wilco_ec_device *ec = platform_get_drvdata(pdev);
 
+	platform_device_unregister(ec->telem_pdev);
 	platform_device_unregister(ec->charger_pdev);
 	wilco_ec_remove_sysfs(ec);
-	platform_device_unregister(ec->telem_pdev);
 	platform_device_unregister(ec->rtc_pdev);
 	if (ec->debugfs_pdev)
 		platform_device_unregister(ec->debugfs_pdev);
