bbee78199fc516d40893642daa24171032b19c60
Eric Biggers <ebiggers@google.com>
2020-01-09 21:10:06 -0800

    FROMLIST: scsi: ufs: add program_key() variant op
    On Snapdragon SoCs, the Linux kernel isn't permitted to directly access
    the standard UFS crypto configuration registers.  Instead, programming
    and evicting keys must be done through vendor-specific SMC calls.
    
    To support this hardware, add a ->program_key() method to
    'struct ufs_hba_variant_ops'.  This allows overriding the UFS standard
    key programming procedure.
    
    Link: https://lore.kernel.org/r/20200110061634.46742-5-ebiggers@kernel.org
    Bug: 137270441
    Bug: 147259927
    Change-Id: Ia561d5a51421baaf78de52a1eaec496093a0d0ad
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -280,6 +280,8 @@ struct ufs_pwr_mode_info {
 	struct ufs_pa_layer_attr info;
 };
 
+union ufs_crypto_cfg_entry;
+
 /**
  * struct ufs_hba_variant_ops - variant specific callbacks
  * @name: variant name
@@ -306,6 +308,7 @@ struct ufs_pwr_mode_info {
  * @resume: called during host controller PM callback
  * @dbg_register_dump: used to dump controller debug information
  * @phy_initialization: used to initialize phys
+ * @program_key: program an inline encryption key into a keyslot
  */
 struct ufs_hba_variant_ops {
 	const char *name;
@@ -334,6 +337,8 @@ struct ufs_hba_variant_ops {
 	int     (*resume)(struct ufs_hba *, enum ufs_pm_op);
 	void	(*dbg_register_dump)(struct ufs_hba *hba);
 	int	(*phy_initialization)(struct ufs_hba *);
+	int	(*program_key)(struct ufs_hba *hba,
+			       const union ufs_crypto_cfg_entry *cfg, int slot);
 };
 
 struct keyslot_mgmt_ll_ops;
