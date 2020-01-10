53435cd88e6ac0491ddbfa03111667229f7b8693
Marco Felsch <m.felsch@pengutronix.de>
2020-01-09 17:04:36 -0800

    Input: edt-ft5x06 - make wakeup-source switchable
    Since day one the touch controller acts as wakeup-source. This seems to
    be wrong since the device supports deep-sleep mechanism [1] which
    requires a reset to leave it. Also some designs won't use the
    touchscreen as wakeup-source.
    
    According discussion [2] we decided to break backward compatibility and
    go the common way by using the 'wakeup-source' device-property.
    
    [1] https://www.newhavendisplay.com/appnotes/datasheets/touchpanel/FT5x26.pdf
    [2] https://patchwork.kernel.org/patch/11149037/
    
    Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>
    Tested-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/touchscreen/edt-ft5x06.c
+++ drivers/input/touchscreen/edt-ft5x06.c
@@ -1208,7 +1208,6 @@ static int edt_ft5x06_ts_probe(struct i2c_client *client,
 		return error;
 
 	edt_ft5x06_ts_prepare_debugfs(tsdata, dev_driver_string(&client->dev));
-	device_init_wakeup(&client->dev, 1);
 
 	dev_dbg(&client->dev,
 		"EDT FT5x06 initialized: IRQ %d, WAKE pin %d, Reset pin %d.\n",
