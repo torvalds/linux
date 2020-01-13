1b2c868083195f9ae374707bab83739f09a5b261
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-01-13 15:04:26 -0600

    ASoC: Intel: bytcht_es8316: removed unused variable
    fix GCC warning with W=1
    
    sound/soc/intel//boards/bytcht_es8316.c:237:40: warning:
    ‘byt_cht_es8316_dai_params’ defined but not used
    [-Wunused-const-variable=]
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20200113210428.27457-17-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/boards/bytcht_es8316.c
+++ sound/soc/intel/boards/bytcht_es8316.c
@@ -234,14 +234,6 @@ static int byt_cht_es8316_init(struct snd_soc_pcm_runtime *runtime)
 	return 0;
 }
 
-static const struct snd_soc_pcm_stream byt_cht_es8316_dai_params = {
-	.formats = SNDRV_PCM_FMTBIT_S24_LE,
-	.rate_min = 48000,
-	.rate_max = 48000,
-	.channels_min = 2,
-	.channels_max = 2,
-};
-
 static int byt_cht_es8316_codec_fixup(struct snd_soc_pcm_runtime *rtd,
 			    struct snd_pcm_hw_params *params)
 {
