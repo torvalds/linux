23274faf96500700da83c4f0ff12d78ae03d5604
Chen Yu <yu.c.chen@intel.com>
2020-01-14 12:06:49 +0800

    tools/power turbostat: Support Ice Lake server
    From a turbostat point of view, Ice Lake server looks like Sky Lake server.
    
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- tools/power/x86/turbostat/turbostat.c
+++ tools/power/x86/turbostat/turbostat.c
@@ -4622,6 +4622,9 @@ unsigned int intel_model_duplicates(unsigned int model)
 
 	case INTEL_FAM6_ATOM_TREMONT_D:
 		return INTEL_FAM6_ATOM_GOLDMONT_D;
+
+	case INTEL_FAM6_ICELAKE_X:
+		return INTEL_FAM6_SKYLAKE_X;
 	}
 	return model;
 }
