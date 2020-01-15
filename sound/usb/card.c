a3afa29942b84b4e2548beacccc3a68b8d77e3dc
Dan Carpenter <dan.carpenter@oracle.com>
2020-01-15 20:46:04 +0300

    ALSA: usb-audio: unlock on error in probe
    We need to unlock before we returning on this error path.
    
    Fixes: 73ac9f5e5b43 ("ALSA: usb-audio: Add boot quirk for MOTU M Series")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Link: https://lore.kernel.org/r/20200115174604.rhanfgy4j3uc65cx@kili.mountain
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/usb/card.c
+++ sound/usb/card.c
@@ -602,7 +602,7 @@ static int usb_audio_probe(struct usb_interface *intf,
 	if (! chip) {
 		err = snd_usb_apply_boot_quirk_once(dev, intf, quirk, id);
 		if (err < 0)
-			return err;
+			goto __error;
 
 		/* it's a fresh one.
 		 * now look for an empty slot and create a new card instance
