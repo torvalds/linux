76ac84d52720e35395d352dada760afc03fff317
Hans Verkuil <hverkuil-cisco@xs4all.nl>
2020-01-16 20:12:27 -0800

    Revert "Input: synaptics-rmi4 - don't increment rmiaddr for SMBus transfers"
    commit 8ff771f8c8d55d95f102cf88a970e541a8bd6bcf upstream.
    
    This reverts commit a284e11c371e446371675668d8c8120a27227339.
    
    This causes problems (drifting cursor) with at least the F11 function that
    reads more than 32 bytes.
    
    The real issue is in the F54 driver, and so this should be fixed there, and
    not in rmi_smbus.c.
    
    So first revert this bad commit, then fix the real problem in F54 in another
    patch.
    
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Reported-by: Timo Kaufmann <timokau@zoho.com>
    Fixes: a284e11c371e ("Input: synaptics-rmi4 - don't increment rmiaddr for SMBus transfers")
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/r/20200115124819.3191024-2-hverkuil-cisco@xs4all.nl
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/input/rmi4/rmi_smbus.c
+++ drivers/input/rmi4/rmi_smbus.c
@@ -166,6 +166,7 @@ static int rmi_smb_write_block(struct rmi_transport_dev *xport, u16 rmiaddr,
 		/* prepare to write next block of bytes */
 		cur_len -= SMB_MAX_COUNT;
 		databuff += SMB_MAX_COUNT;
+		rmiaddr += SMB_MAX_COUNT;
 	}
 exit:
 	mutex_unlock(&rmi_smb->page_mutex);
@@ -217,6 +218,7 @@ static int rmi_smb_read_block(struct rmi_transport_dev *xport, u16 rmiaddr,
 		/* prepare to read next block of bytes */
 		cur_len -= SMB_MAX_COUNT;
 		databuff += SMB_MAX_COUNT;
+		rmiaddr += SMB_MAX_COUNT;
 	}
 
 	retval = 0;
