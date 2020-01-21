953c4a08dfc9ffe763a8340ac10f459d6c6cc4eb
Martin Schiller <ms@dev.tdt.de>
2020-01-21 07:00:34 +0100

    wan/hdlc_x25: fix skb handling
    o call skb_reset_network_header() before hdlc->xmit()
     o change skb proto to HDLC (0x0019) before hdlc->xmit()
     o call dev_queue_xmit_nit() before hdlc->xmit()
    
    This changes make it possible to trace (tcpdump) outgoing layer2
    (ETH_P_HDLC) packets
    
    Additionally call skb_reset_network_header() after each skb_push() /
    skb_pull().
    
    Signed-off-by: Martin Schiller <ms@dev.tdt.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/wan/hdlc_x25.c
+++ drivers/net/wan/hdlc_x25.c
@@ -71,10 +71,11 @@ static int x25_data_indication(struct net_device *dev, struct sk_buff *skb)
 {
 	unsigned char *ptr;
 
-	skb_push(skb, 1);
-
 	if (skb_cow(skb, 1))
 		return NET_RX_DROP;
+
+	skb_push(skb, 1);
+	skb_reset_network_header(skb);
 
 	ptr  = skb->data;
 	*ptr = X25_IFACE_DATA;
@@ -88,6 +89,13 @@ static int x25_data_indication(struct net_device *dev, struct sk_buff *skb)
 static void x25_data_transmit(struct net_device *dev, struct sk_buff *skb)
 {
 	hdlc_device *hdlc = dev_to_hdlc(dev);
+
+	skb_reset_network_header(skb);
+	skb->protocol = hdlc_type_trans(skb, dev);
+
+	if (dev_nit_active(dev))
+		dev_queue_xmit_nit(skb, dev);
+
 	hdlc->xmit(skb, dev); /* Ignore return value :-( */
 }
 
@@ -102,6 +110,7 @@ static netdev_tx_t x25_xmit(struct sk_buff *skb, struct net_device *dev)
 	switch (skb->data[0]) {
 	case X25_IFACE_DATA:	/* Data to be transmitted */
 		skb_pull(skb, 1);
+		skb_reset_network_header(skb);
 		if ((result = lapb_data_request(dev, skb)) != LAPB_OK)
 			dev_kfree_skb(skb);
 		return NETDEV_TX_OK;
