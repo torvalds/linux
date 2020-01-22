47c6ba1030d3216c732db686b27f7bd711e7210b
Eric Biggers <ebiggers@google.com>
2020-01-21 16:43:41 -0800

    ANDROID: dm: enable may_passthrough_inline_crypto on some targets
    dm-linear obviously can pass through inline crypto support.
    
    In addition, we need dm-bow to pass through inline crypto support, as
    the userdata partition in Android may be located on top of dm-bow.
    
    Bug: 137270441
    Change-Id: Ib9b0f67b77391fa450394568ceed441c1508de6e
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- drivers/md/dm-linear.c
+++ drivers/md/dm-linear.c
@@ -62,6 +62,7 @@ static int linear_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 	ti->num_secure_erase_bios = 1;
 	ti->num_write_same_bios = 1;
 	ti->num_write_zeroes_bios = 1;
+	ti->may_passthrough_inline_crypto = true;
 	ti->private = lc;
 	return 0;
 
