1a8e9cf40c9a6a2e40b1e924b13ed303aeea4418
Vasily Averin <vvs@virtuozzo.com>
2020-01-23 12:05:10 +0300

    jbd2_seq_info_next should increase position index
    if seq_file .next fuction does not change position index,
    read after some lseek can generate unexpected output.
    
    Script below generates endless output
     $ q=;while read -r r;do echo "$((++q)) $r";done </proc/fs/jbd2/DEV/info
    
    https://bugzilla.kernel.org/show_bug.cgi?id=206283
    
    Fixes: 1f4aace60b0e ("fs/seq_file.c: simplify seq_file iteration code and interface")
    Cc: stable@kernel.org
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Link: https://lore.kernel.org/r/d13805e5-695e-8ac3-b678-26ca2313629f@virtuozzo.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/jbd2/journal.c
+++ fs/jbd2/journal.c
@@ -982,6 +982,7 @@ static void *jbd2_seq_info_start(struct seq_file *seq, loff_t *pos)
 
 static void *jbd2_seq_info_next(struct seq_file *seq, void *v, loff_t *pos)
 {
+	(*pos)++;
 	return NULL;
 }
 
