b5e82e3c89c78ee0407ea8e8087af5519b6c7bae
Lorenzo Bianconi <lorenzo@kernel.org>
2020-01-25 12:48:50 +0100

    net: socionext: fix possible user-after-free in netsec_process_rx
    Fix possible use-after-free in in netsec_process_rx that can occurs if
    the first packet is sent to the normal networking stack and the
    following one is dropped by the bpf program attached to the xdp hook.
    Fix the issue defining the skb pointer in the 'budget' loop
    
    Fixes: ba2b232108d3c ("net: netsec: add XDP support")
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Acked-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Acked-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/socionext/netsec.c
+++ drivers/net/ethernet/socionext/netsec.c
@@ -935,7 +935,6 @@ static int netsec_process_rx(struct netsec_priv *priv, int budget)
 	struct netsec_rx_pkt_info rx_info;
 	enum dma_data_direction dma_dir;
 	struct bpf_prog *xdp_prog;
-	struct sk_buff *skb = NULL;
 	u16 xdp_xmit = 0;
 	u32 xdp_act = 0;
 	int done = 0;
@@ -949,6 +948,7 @@ static int netsec_process_rx(struct netsec_priv *priv, int budget)
 		struct netsec_de *de = dring->vaddr + (DESC_SZ * idx);
 		struct netsec_desc *desc = &dring->desc[idx];
 		struct page *page = virt_to_page(desc->addr);
+		struct sk_buff *skb = NULL;
 		u32 xdp_result = XDP_PASS;
 		u16 pkt_len, desc_len;
 		dma_addr_t dma_handle;
