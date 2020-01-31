7dfaa98f646bbc29c3575bc184f2b37aa2dd62c5
Yury Norov <yury.norov@gmail.com>
2020-01-30 22:16:47 -0800

    lib/find_bit.c: uninline helper _find_next_bit()
    It saves 25% of .text for arm64, and more for BE architectures.
    
    Before:
      $ size lib/find_bit.o
         text    data     bss     dec     hex filename
         1012      56       0    1068     42c lib/find_bit.o
    
    After:
      $ size lib/find_bit.o
         text    data     bss     dec     hex filename
          776      56       0     832     340 lib/find_bit.o
    
    Link: http://lkml.kernel.org/r/20200103202846.21616-3-yury.norov@gmail.com
    Signed-off-by: Yury Norov <yury.norov@gmail.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Allison Randal <allison@lohutok.net>
    Cc: William Breathitt Gray <vilhelm.gray@gmail.com>
    Cc: Joe Perches <joe@perches.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/find_bit.c
+++ lib/find_bit.c
@@ -27,7 +27,7 @@
  *    searching it for one bits.
  *  - The optional "addr2", which is anded with "addr1" if present.
  */
-static inline unsigned long _find_next_bit(const unsigned long *addr1,
+static unsigned long _find_next_bit(const unsigned long *addr1,
 		const unsigned long *addr2, unsigned long nbits,
 		unsigned long start, unsigned long invert, unsigned long le)
 {
