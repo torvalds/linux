1525c20cff96cd60549f8dd44f9b2fb15ee1715b
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2020-01-31 22:49:28 +0200

    drm/tiny/st7735r: No need to set ->owner for spi_register_driver()
    The spi_register_driver() will set the ->owner member to THIS_MODULE.
    
    Cc: Noralf Trønnes <noralf@tronnes.org>
    Cc: dri-devel@lists.freedesktop.org
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: David Lechner <david@lechnology.com>
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200131204923.48928-4-andriy.shevchenko@linux.intel.com

--- drivers/gpu/drm/tiny/st7735r.c
+++ drivers/gpu/drm/tiny/st7735r.c
@@ -276,7 +276,6 @@ static void st7735r_shutdown(struct spi_device *spi)
 static struct spi_driver st7735r_spi_driver = {
 	.driver = {
 		.name = "st7735r",
-		.owner = THIS_MODULE,
 		.of_match_table = st7735r_of_match,
 	},
 	.id_table = st7735r_id,
