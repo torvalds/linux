dc42cf74d98a39af30ecaf78e7fad7e265ae528f
Hans Verkuil <hverkuil-cisco@xs4all.nl>
2020-02-03 12:41:19 +0100

    media: v4l2-dev.h: remove VFL_TYPE_GRABBER
    The last driver was converted to use VFL_TYPE_VIDEO, so the old
    _GRABBER enum can now be removed.
    
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- include/media/v4l2-dev.h
+++ include/media/v4l2-dev.h
@@ -25,7 +25,6 @@
  * enum vfl_devnode_type - type of V4L2 device node
  *
  * @VFL_TYPE_VIDEO:	for video input/output devices
- * @VFL_TYPE_GRABBER:	deprecated, same as VFL_TYPE_VIDEO
  * @VFL_TYPE_VBI:	for vertical blank data (i.e. closed captions, teletext)
  * @VFL_TYPE_RADIO:	for radio tuners
  * @VFL_TYPE_SUBDEV:	for V4L2 subdevices
@@ -35,7 +34,6 @@
  */
 enum vfl_devnode_type {
 	VFL_TYPE_VIDEO,
-	VFL_TYPE_GRABBER = VFL_TYPE_VIDEO,
 	VFL_TYPE_VBI,
 	VFL_TYPE_RADIO,
 	VFL_TYPE_SUBDEV,
