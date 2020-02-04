875014cb79ea4251e8800994640be1a8012b4133
Stephen Boyd <swboyd@chromium.org>
2020-02-04 11:12:06 -0800

    spi: spi-geni-qcom: Drop of.h include
    This driver doesn't call any DT functions like of_get_property(). Remove
    the of.h include as it isn't used.
    
    Cc: Girish Mahadevan <girishm@codeaurora.org>
    Cc: Dilip Kota <dkota@codeaurora.org>
    Cc: Alok Chauhan <alokc@codeaurora.org>
    Cc: Douglas Anderson <dianders@chromium.org>
    Signed-off-by: Stephen Boyd <swboyd@chromium.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Link: https://lore.kernel.org/r/20200204191206.97036-4-swboyd@chromium.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-geni-qcom.c
+++ drivers/spi/spi-geni-qcom.c
@@ -6,7 +6,6 @@
 #include <linux/io.h>
 #include <linux/log2.h>
 #include <linux/module.h>
-#include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
 #include <linux/qcom-geni-se.h>
