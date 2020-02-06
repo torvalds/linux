20ba262f8631aadefa87921481fe569ecc387f20
Eric Auger <eric.auger@redhat.com>
2020-02-06 11:47:09 +0100

    selftests: KVM: AMD Nested test infrastructure
    Add the basic infrastructure needed to test AMD nested SVM.
    This is largely copied from the KVM unit test infrastructure.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Reviewed-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- tools/testing/selftests/kvm/include/x86_64/processor.h
+++ tools/testing/selftests/kvm/include/x86_64/processor.h
@@ -56,6 +56,26 @@ enum x86_register {
 	R15,
 };
 
+/* General Registers in 64-Bit Mode */
+struct gpr64_regs {
+	u64 rax;
+	u64 rcx;
+	u64 rdx;
+	u64 rbx;
+	u64 rsp;
+	u64 rbp;
+	u64 rsi;
+	u64 rdi;
+	u64 r8;
+	u64 r9;
+	u64 r10;
+	u64 r11;
+	u64 r12;
+	u64 r13;
+	u64 r14;
+	u64 r15;
+};
+
 struct desc64 {
 	uint16_t limit0;
 	uint16_t base0;
