0b5faf6ba7fb78bb1fe7336d23ea1978386a6c3a
Jens Axboe <axboe@kernel.dk>
2020-02-06 21:42:51 -0700

    io_uring: allow AT_FDCWD for non-file openat/openat2/statx
    Don't just check for dirfd == -1, we should allow AT_FDCWD as well for
    relative lookups.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -4543,7 +4543,7 @@ static int io_req_needs_file(struct io_kiocb *req, int fd)
 {
 	if (!io_op_defs[req->opcode].needs_file)
 		return 0;
-	if (fd == -1 && io_op_defs[req->opcode].fd_non_neg)
+	if ((fd == -1 || fd == AT_FDCWD) && io_op_defs[req->opcode].fd_non_neg)
 		return 0;
 	return 1;
 }
