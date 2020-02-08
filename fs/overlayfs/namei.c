791d8a9831feabddb80ccbcf90ef9a0847f607a8
Greg Kroah-Hartman <gregkh@google.com>
2020-02-08 14:23:00 +0100

    Revert "Revert "FROMLIST: overlayfs: internal getxattr operations without sepolicy checking""
    This reverts commit d987f9051569578e851d2fc164420260d39504d2 which was
    reverting 4aa24aec05e506016655d78a073625214ab27398.
    
    All should be good now.
    
    Cc: Mark Salyzyn <salyzyn@android.com>
    Cc: kernel-team@android.com
    Cc: Mark Salyzyn <salyzyn@google.com>
    Bug: 133515582
    Bug: 136124883
    Bug: 129319403
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Ic764d90114d41daf6defdbfbc4db43a8c224e6dc

--- fs/overlayfs/namei.c
+++ fs/overlayfs/namei.c
@@ -106,10 +106,11 @@ int ovl_check_fb_len(struct ovl_fb *fb, int fb_len)
 
 static struct ovl_fh *ovl_get_fh(struct dentry *dentry, const char *name)
 {
-	int res, err;
+	ssize_t res;
+	int err;
 	struct ovl_fh *fh = NULL;
 
-	res = vfs_getxattr(dentry, name, NULL, 0);
+	res = ovl_do_vfs_getxattr(dentry, name, NULL, 0);
 	if (res < 0) {
 		if (res == -ENODATA || res == -EOPNOTSUPP)
 			return NULL;
@@ -123,7 +124,7 @@ static struct ovl_fh *ovl_get_fh(struct dentry *dentry, const char *name)
 	if (!fh)
 		return ERR_PTR(-ENOMEM);
 
-	res = vfs_getxattr(dentry, name, fh->buf, res);
+	res = ovl_do_vfs_getxattr(dentry, name, fh->buf, res);
 	if (res < 0)
 		goto fail;
 
@@ -141,10 +142,10 @@ out:
 	return NULL;
 
 fail:
-	pr_warn_ratelimited("failed to get origin (%i)\n", res);
+	pr_warn_ratelimited("failed to get origin (%zi)\n", res);
 	goto out;
 invalid:
-	pr_warn_ratelimited("invalid origin (%*phN)\n", res, fh);
+	pr_warn_ratelimited("invalid origin (%*phN)\n", (int)res, fh);
 	goto out;
 }
 
