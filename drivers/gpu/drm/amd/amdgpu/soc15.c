e520859cde724ea451561520ceceb5a630eef4f2
James Zhu <James.Zhu@amd.com>
2020-02-10 10:28:00 -0500

    drm/amdgpu: enable VCN2.5 DPG mode for Arcturus
    Enable VCN2.5 DPG mode for arcturus after below items are applied.
    ASD: 0x21000023
    SOS: 0x17003B
    VCN firmware Version ENC: 1.1 DEC: 1 VEP: 0 Revision: 16
    VBIOS: 23
    
    Signed-off-by: James Zhu <James.Zhu@amd.com>
    Reviewed-by: Leo Liu <leo.liu@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/soc15.c
+++ drivers/gpu/drm/amd/amdgpu/soc15.c
@@ -1221,7 +1221,7 @@ static int soc15_common_early_init(void *handle)
 			AMD_CG_SUPPORT_IH_CG |
 			AMD_CG_SUPPORT_VCN_MGCG |
 			AMD_CG_SUPPORT_JPEG_MGCG;
-		adev->pg_flags = 0;
+		adev->pg_flags = AMD_PG_SUPPORT_VCN | AMD_PG_SUPPORT_VCN_DPG;
 		adev->external_rev_id = adev->rev_id + 0x32;
 		break;
 	case CHIP_RENOIR:
