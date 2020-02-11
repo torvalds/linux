a12d7f3cbdc72c7625881c8dc2660fc2c979fdf2
Jason A. Donenfeld <Jason@zx2c4.com>
2020-02-11 20:47:08 +0100

    wireguard: device: use icmp_ndo_send helper
    Because wireguard is calling icmp from network device context, it should
    use the ndo helper so that the rate limiting applies correctly.  This
    commit adds a small test to the wireguard test suite to ensure that the
    new functions continue doing the right thing in the context of
    wireguard. It does this by setting up a condition that will definately
    evoke an icmp error message from the driver, but along a nat'd path.
    
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/wireguard/device.c
+++ drivers/net/wireguard/device.c
@@ -203,9 +203,9 @@ err_peer:
 err:
 	++dev->stats.tx_errors;
 	if (skb->protocol == htons(ETH_P_IP))
-		icmp_send(skb, ICMP_DEST_UNREACH, ICMP_HOST_UNREACH, 0);
+		icmp_ndo_send(skb, ICMP_DEST_UNREACH, ICMP_HOST_UNREACH, 0);
 	else if (skb->protocol == htons(ETH_P_IPV6))
-		icmpv6_send(skb, ICMPV6_DEST_UNREACH, ICMPV6_ADDR_UNREACH, 0);
+		icmpv6_ndo_send(skb, ICMPV6_DEST_UNREACH, ICMPV6_ADDR_UNREACH, 0);
 	kfree_skb(skb);
 	return ret;
 }
