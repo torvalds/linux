60083063b755e29685902609e024ecd0c4a1a7d9
Geert Uytterhoeven <geert+renesas@glider.be>
2020-02-13 09:38:04 +0100

    powerpc/time: Replace <linux/clk-provider.h> by <linux/of_clk.h>
    The PowerPC time code is not a clock provider, and just needs to call
    of_clk_init().
    
    Hence it can include <linux/of_clk.h> instead of <linux/clk-provider.h>.
    
    Remove the #ifdef protecting the of_clk_init() call, as a stub is
    available for the !CONFIG_COMMON_CLK case.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Stephen Boyd <sboyd@kernel.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200213083804.24315-1-geert+renesas@glider.be

--- arch/powerpc/kernel/time.c
+++ arch/powerpc/kernel/time.c
@@ -50,7 +50,7 @@
 #include <linux/irq.h>
 #include <linux/delay.h>
 #include <linux/irq_work.h>
-#include <linux/clk-provider.h>
+#include <linux/of_clk.h>
 #include <linux/suspend.h>
 #include <linux/sched/cputime.h>
 #include <linux/processor.h>
@@ -1149,9 +1149,7 @@ void __init time_init(void)
 	init_decrementer_clockevent();
 	tick_setup_hrtimer_broadcast();
 
-#ifdef CONFIG_COMMON_CLK
 	of_clk_init(NULL);
-#endif
 }
 
 /*
