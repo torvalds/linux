4e867c9a50ff1a07ed0b86c3b1c8bc773933d728
Hangbin Liu <liuhangbin@gmail.com>
2020-02-13 17:40:54 +0800

    selftests: forwarding: vxlan_bridge_1d: fix tos value
    After commit 71130f29979c ("vxlan: fix tos value before xmit") we start
    strict vxlan xmit tos value by RT_TOS(), which limits the tos value less
    than 0x1E. With current value 0x40 the test will failed with "v1: Expected
    to capture 10 packets, got 0". So let's choose a smaller tos value for
    testing.
    
    Fixes: d417ecf533fe ("selftests: forwarding: vxlan_bridge_1d: Add a TOS test")
    Signed-off-by: Hangbin Liu <liuhangbin@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- tools/testing/selftests/net/forwarding/vxlan_bridge_1d.sh
+++ tools/testing/selftests/net/forwarding/vxlan_bridge_1d.sh
@@ -516,9 +516,9 @@ test_tos()
 	RET=0
 
 	tc filter add dev v1 egress pref 77 prot ip \
-		flower ip_tos 0x40 action pass
-	vxlan_ping_test $h1 192.0.2.3 "-Q 0x40" v1 egress 77 10
-	vxlan_ping_test $h1 192.0.2.3 "-Q 0x30" v1 egress 77 0
+		flower ip_tos 0x11 action pass
+	vxlan_ping_test $h1 192.0.2.3 "-Q 0x11" v1 egress 77 10
+	vxlan_ping_test $h1 192.0.2.3 "-Q 0x12" v1 egress 77 0
 	tc filter del dev v1 egress pref 77 prot ip
 
 	log_test "VXLAN: envelope TOS inheritance"
