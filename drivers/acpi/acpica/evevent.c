4444b178529300a173b927bbef3e65d328fc15be
Bob Moore <robert.moore@intel.com>
2020-02-14 10:47:57 -0800

    ACPICA: Fix a comment "enable" fixed events -> "disable" all fixed events.
    ACPICA commit af4462c6f41ebc9bf63b5370818c5fd96524e7a9
    
    Link: https://github.com/acpica/acpica/commit/af4462c6
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Erik Kaneda <erik.kaneda@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpica/evevent.c
+++ drivers/acpi/acpica/evevent.c
@@ -130,7 +130,7 @@ static acpi_status acpi_ev_fixed_event_initialize(void)
 
 	/*
 	 * Initialize the structure that keeps track of fixed event handlers and
-	 * enable the fixed events.
+	 * disable all of the fixed events.
 	 */
 	for (i = 0; i < ACPI_NUM_FIXED_EVENTS; i++) {
 		acpi_gbl_fixed_event_handlers[i].handler = NULL;
