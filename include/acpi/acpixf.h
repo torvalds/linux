c7be17a6b41a67835ce4be33a0c38cf473d54c66
Bob Moore <robert.moore@intel.com>
2020-02-14 10:48:04 -0800

    ACPICA: Update version to 20200214
    ACPICA commit ac0c1b8a43a317702bb11e11fd5067a1c59e3002
    
    Version 20200214
    
    Link: https://github.com/acpica/acpica/commit/ac0c1b8a
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Erik Kaneda <erik.kaneda@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/acpixf.h
+++ include/acpi/acpixf.h
@@ -12,7 +12,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20200110
+#define ACPI_CA_VERSION                 0x20200214
 
 #include <acpi/acconfig.h>
 #include <acpi/actypes.h>
