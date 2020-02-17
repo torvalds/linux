16985f2d25095899685952296f128a71f0aff05c
Oliver O'Halloran <oohall@gmail.com>
2020-02-17 13:48:32 +1100

    powerpc/powernv: Treat an empty reboot string as default
    Treat an empty reboot cmd string the same as a NULL string. This squashes a
    spurious unsupported reboot message that sometimes gets out when using
    xmon.
    
    Signed-off-by: Oliver O'Halloran <oohall@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200217024833.30580-1-oohall@gmail.com

--- arch/powerpc/platforms/powernv/setup.c
+++ arch/powerpc/platforms/powernv/setup.c
@@ -229,7 +229,7 @@ static void  __noreturn pnv_restart(char *cmd)
 	pnv_prepare_going_down();
 
 	do {
-		if (!cmd)
+		if (!cmd || !strlen(cmd))
 			rc = opal_cec_reboot();
 		else if (strcmp(cmd, "full") == 0)
 			rc = opal_cec_reboot2(OPAL_REBOOT_FULL_IPL, NULL);
