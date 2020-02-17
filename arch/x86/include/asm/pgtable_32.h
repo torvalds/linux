86977da9cb71c8293263c630ac920dd1537de9e5
Al Viro <viro@zeniv.linux.org.uk>
2020-02-16 19:39:36 -0500

    TEST_ACCESS_OK _never_ had been checked anywhere
    Once upon a time the predecessor of that thing (TEST_VERIFY_AREA)
    used to be.  However, that had been gone for years now (and
    the patch that introduced TEST_ACCESS_OK has not touched any
    ifdefs - they got gradually removed later).  Just bury it...
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/x86/include/asm/pgtable_32.h
+++ arch/x86/include/asm/pgtable_32.h
@@ -32,13 +32,6 @@ extern pmd_t initial_pg_pmd[];
 void paging_init(void);
 void sync_initial_page_table(void);
 
-/*
- * Define this if things work differently on an i386 and an i486:
- * it will (on an i486) warn about kernel memory accesses that are
- * done without a 'access_ok( ..)'
- */
-#undef TEST_ACCESS_OK
-
 #ifdef CONFIG_X86_PAE
 # include <asm/pgtable-3level.h>
 #else
