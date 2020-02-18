929a3af90f0f4bd7132d83552c1a98c83f60ef7e
Pavel Begunkov <asml.silence@gmail.com>
2020-02-19 00:19:09 +0300

    io_uring: fix use-after-free by io_cleanup_req()
    io_cleanup_req() should be called before req->io is freed, and so
    shouldn't be after __io_free_req() -> __io_req_aux_free(). Also,
    it will be ignored for in io_free_req_many(), which use
    __io_req_aux_free().
    
    Place cleanup_req() into __io_req_aux_free().
    
    Fixes: 99bc4c38537d774 ("io_uring: fix iovec leaks")
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -1260,6 +1260,9 @@ static void __io_req_aux_free(struct io_kiocb *req)
 {
 	struct io_ring_ctx *ctx = req->ctx;
 
+	if (req->flags & REQ_F_NEED_CLEANUP)
+		io_cleanup_req(req);
+
 	kfree(req->io);
 	if (req->file) {
 		if (req->flags & REQ_F_FIXED_FILE)
@@ -1274,9 +1277,6 @@ static void __io_req_aux_free(struct io_kiocb *req)
 static void __io_free_req(struct io_kiocb *req)
 {
 	__io_req_aux_free(req);
-
-	if (req->flags & REQ_F_NEED_CLEANUP)
-		io_cleanup_req(req);
 
 	if (req->flags & REQ_F_INFLIGHT) {
 		struct io_ring_ctx *ctx = req->ctx;
