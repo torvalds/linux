c9c2240eee092c392d27fa5f69016bcf51c6ed58
Vitor Soares <Vitor.Soares@synopsys.com>
2020-02-19 01:20:41 +0100

    i3c: master: add i3c_for_each_dev helper
    Introduce i3c_for_each_dev(), an i3c device iterator for use by i3cdev.
    
    Signed-off-by: Vitor Soares <vitor.soares@synopsys.com>
    Change-Id: I1b0d188c25e66109da6bf524f059fc069c9d5faa
    (cherry picked from commit 56dbdd266dc73648809bbc070379d1516b5011a7)

--- drivers/i3c/internals.h
+++ drivers/i3c/internals.h
@@ -32,6 +32,7 @@ int i3c_dev_pending_read_notify_locked(struct i3c_dev_desc *dev,
 				       struct i3c_priv_xfer *pending_read,
 				       struct i3c_priv_xfer *ibi_notify);
 int i3c_dev_is_ibi_enabled_locked(struct i3c_dev_desc *dev);
+int i3c_for_each_dev(void *data, int (*fn)(struct device *, void *));
 int i3c_dev_control_pec(struct i3c_dev_desc *dev, bool pec);
 int i3c_master_getmrl_locked(struct i3c_master_controller *master,
 			     struct i3c_device_info *info);
