47e12855a91d2a9ca18d8fe10382e5a8d87b1417
Al Viro <viro@zeniv.linux.org.uk>
2020-02-22 11:01:44 -0500

    powerpc: switch to ->regset_get()
    Note: compat variant of REGSET_TM_CGPR is almost certainly wrong;
    it claims to be 48*64bit, but just as compat REGSET_GPR it stores
    44*32bit of (truncated) registers + 4 32bit zeros... followed by
    48 more 32bit zeroes.  Might be too late to change - it's a userland
    ABI, after all ;-/
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/powerpc/kernel/ptrace/ptrace-spe.c
+++ arch/powerpc/kernel/ptrace/ptrace-spe.c
@@ -23,25 +23,17 @@ int evr_active(struct task_struct *target, const struct user_regset *regset)
 }
 
 int evr_get(struct task_struct *target, const struct user_regset *regset,
-	    unsigned int pos, unsigned int count, void *kbuf, void __user *ubuf)
+	    struct membuf to)
 {
-	int ret;
-
 	flush_spe_to_thread(target);
 
-	ret = user_regset_copyout(&pos, &count, &kbuf, &ubuf,
-				  &target->thread.evr,
-				  0, sizeof(target->thread.evr));
+	membuf_write(&to, &target->thread.evr, sizeof(target->thread.evr));
 
 	BUILD_BUG_ON(offsetof(struct thread_struct, acc) + sizeof(u64) !=
 		     offsetof(struct thread_struct, spefscr));
 
-	if (!ret)
-		ret = user_regset_copyout(&pos, &count, &kbuf, &ubuf,
-					  &target->thread.acc,
-					  sizeof(target->thread.evr), -1);
-
-	return ret;
+	return membuf_write(&to, &target->thread.acc,
+				sizeof(u64) + sizeof(u32));
 }
 
 int evr_set(struct task_struct *target, const struct user_regset *regset,
