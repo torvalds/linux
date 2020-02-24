caaeb5f71040c6928de11d1bff7c4b6c4f6a53b7
Mark Salyzyn <salyzyn@google.com>
2020-02-24 14:40:49 -0800

    ANDROID: GKI: power: supply: Add POWER_SUPPLY_PROP_ALIGNMENT
    Partially taken from commit 3c7e776d03e956a0889c849c384d18400ef0e28e
    ("p9221: add wireless/align node for move to center")
    
    Signed-off-by: Wilson Chan <wtchan@google.com>
    Bug: 140835784
    Signed-off-by: YiHsiang Peng <yihsiangpeng@google.com>
    (cherry picked from commit 3c7e776d03e956a0889c849c384d18400ef0e28e)
    (cherry picked from commit bc1980052972b75422db80dfd5b5756c2d42f978)
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 150789066
    Change-Id: I468f18c1f1d761565e15944b5bc58f82244924d4

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -498,6 +498,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(cp_ilim),
 	POWER_SUPPLY_ATTR(irq_status),
 	POWER_SUPPLY_ATTR(parallel_output_mode),
+	POWER_SUPPLY_ATTR(alignment),
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	POWER_SUPPLY_ATTR(charge_charger_state),
