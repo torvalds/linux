860fbde438dc88d2fedf75965963b96c9041a0d5
Dmitry Osipenko <digetx@gmail.com>
2020-02-25 01:40:52 +0300

    cpuidle: Refactor and move out NVIDIA Tegra20 driver into drivers/cpuidle
    The driver's code is refactored in a way that will make it easy to
    support Tegra30/114/124 SoCs by this unified driver later on. The
    current functionality is equal to the old Tegra20 driver, only the
    code's structure changed a tad. This is also a proper platform driver
    now.
    
    Acked-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- arch/arm/mach-tegra/cpuidle.c
+++ arch/arm/mach-tegra/cpuidle.c
@@ -14,6 +14,7 @@
 
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/platform_device.h>
 
 #include <soc/tegra/fuse.h>
 
@@ -23,8 +24,7 @@ void __init tegra_cpuidle_init(void)
 {
 	switch (tegra_get_chip_id()) {
 	case TEGRA20:
-		if (IS_ENABLED(CONFIG_ARCH_TEGRA_2x_SOC))
-			tegra20_cpuidle_init();
+		platform_device_register_simple("tegra-cpuidle", -1, NULL, 0);
 		break;
 	case TEGRA30:
 		if (IS_ENABLED(CONFIG_ARCH_TEGRA_3x_SOC))
@@ -35,16 +35,6 @@ void __init tegra_cpuidle_init(void)
 		if (IS_ENABLED(CONFIG_ARCH_TEGRA_114_SOC) ||
 		    IS_ENABLED(CONFIG_ARCH_TEGRA_124_SOC))
 			tegra114_cpuidle_init();
-		break;
-	}
-}
-
-void tegra_cpuidle_pcie_irqs_in_use(void)
-{
-	switch (tegra_get_chip_id()) {
-	case TEGRA20:
-		if (IS_ENABLED(CONFIG_ARCH_TEGRA_2x_SOC))
-			tegra20_cpuidle_pcie_irqs_in_use();
 		break;
 	}
 }
