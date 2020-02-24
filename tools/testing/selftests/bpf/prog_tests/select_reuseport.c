779e422d11985e408fc148f0cca0f4b403f6c5fa
Jakub Sitnicki <jakub@cloudflare.com>
2020-02-24 14:53:26 +0100

    selftests/bpf: Run reuseport tests only with supported socket types
    SOCKMAP and SOCKHASH map types can be used with reuseport BPF programs but
    don't support yet storing UDP sockets. Instead of marking UDP tests with
    SOCK{MAP,HASH} as skipped, don't run them at all.
    
    Skipped test might signal that the test environment is not suitable for
    running the test, while in reality the functionality is not implemented in
    the kernel yet.
    
    Before:
    
      sh# ./test_progs -t select_reuseport
      …
      #40 select_reuseport:OK
      Summary: 1/126 PASSED, 30 SKIPPED, 0 FAILED
    
    After:
    
      sh# ./test_progs  -t select_reuseport
      …
      #40 select_reuseport:OK
      Summary: 1/98 PASSED, 2 SKIPPED, 0 FAILED
    
    The remaining two skipped tests are SYN cookies tests, which will be
    addressed in the subsequent patch.
    
    Fixes: 11318ba8cafd ("selftests/bpf: Extend SK_REUSEPORT tests to cover SOCKMAP/SOCKHASH")
    Reported-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Jakub Sitnicki <jakub@cloudflare.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/20200224135327.121542-1-jakub@cloudflare.com

--- tools/testing/selftests/bpf/prog_tests/select_reuseport.c
+++ tools/testing/selftests/bpf/prog_tests/select_reuseport.c
@@ -807,6 +807,12 @@ static void test_config(int sotype, sa_family_t family, bool inany)
 	char s[MAX_TEST_NAME];
 	const struct test *t;
 
+	/* SOCKMAP/SOCKHASH don't support UDP yet */
+	if (sotype == SOCK_DGRAM &&
+	    (inner_map_type == BPF_MAP_TYPE_SOCKMAP ||
+	     inner_map_type == BPF_MAP_TYPE_SOCKHASH))
+		return;
+
 	for (t = tests; t < tests + ARRAY_SIZE(tests); t++) {
 		snprintf(s, sizeof(s), "%s %s/%s %s %s",
 			 maptype_str(inner_map_type),
@@ -815,13 +821,6 @@ static void test_config(int sotype, sa_family_t family, bool inany)
 
 		if (!test__start_subtest(s))
 			continue;
-
-		if (sotype == SOCK_DGRAM &&
-		    inner_map_type != BPF_MAP_TYPE_REUSEPORT_SOCKARRAY) {
-			/* SOCKMAP/SOCKHASH don't support UDP yet */
-			test__skip();
-			continue;
-		}
 
 		setup_per_test(sotype, family, inany, t->no_inner_map);
 		t->fn(sotype, family);
