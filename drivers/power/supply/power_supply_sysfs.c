2c0ee0ec5185ecbe39ca6d3c1c61763a96e05eb4
Mark Salyzyn <salyzyn@google.com>
2020-02-24 14:40:49 -0800

    ANDROID: GKI: power: power_supply: Add FG_RESET power supply property
    Add a new power-supply property (POWER_SUPPLY_PROP_FG_RESET)
    to to reset FG/QG.
    
    Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>
    (cherry picked from commit 37f8e3dcdedcc3e38a91617193c8cfedaa8575fd)
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 150789066
    Change-Id: Id3bf1ab5740a064d6ce7e42ff499839dbb457b17

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -447,6 +447,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(fcc_stepper_enable),
 	POWER_SUPPLY_ATTR(toggle_stat),
 	POWER_SUPPLY_ATTR(main_fcc_max),
+	POWER_SUPPLY_ATTR(fg_reset),
 	/* Charge pump properties */
 	POWER_SUPPLY_ATTR(cp_status1),
 	POWER_SUPPLY_ATTR(cp_status2),
