30ab3bbf58d2628a8641ad72772c91c6907e37d6
Rodrigo Vivi <rodrigo.vivi@intel.com>
2020-02-24 17:32:20 -0800

    drm/i915: Update DRIVER_DATE to 20200224
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -112,8 +112,8 @@
 
 #define DRIVER_NAME		"i915"
 #define DRIVER_DESC		"Intel Graphics"
-#define DRIVER_DATE		"20200114"
-#define DRIVER_TIMESTAMP	1579001978
+#define DRIVER_DATE		"20200224"
+#define DRIVER_TIMESTAMP	1582594340
 
 struct drm_i915_gem_object;
 
