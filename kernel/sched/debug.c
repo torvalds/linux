96e74ebf8d594496f3dda5f8e26af6b4e161e4e9
Valentin Schneider <valentin.schneider@arm.com>
2020-02-26 12:45:43 +0000

    sched/debug: Add task uclamp values to SCHED_DEBUG procfs
    Requested and effective uclamp values can be a bit tricky to decipher when
    playing with cgroup hierarchies. Add them to a task's procfs when
    SCHED_DEBUG is enabled.
    
    Reviewed-by: Qais Yousef <qais.yousef@arm.com>
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lkml.kernel.org/r/20200226124543.31986-4-valentin.schneider@arm.com

--- kernel/sched/debug.c
+++ kernel/sched/debug.c
@@ -947,6 +947,12 @@ void proc_sched_show_task(struct task_struct *p, struct pid_namespace *ns,
 	P(se.avg.util_est.ewma);
 	P(se.avg.util_est.enqueued);
 #endif
+#ifdef CONFIG_UCLAMP_TASK
+	__PS("uclamp.min", p->uclamp[UCLAMP_MIN].value);
+	__PS("uclamp.max", p->uclamp[UCLAMP_MAX].value);
+	__PS("effective uclamp.min", uclamp_eff_value(p, UCLAMP_MIN));
+	__PS("effective uclamp.max", uclamp_eff_value(p, UCLAMP_MAX));
+#endif
 	P(policy);
 	P(prio);
 	if (task_has_dl_policy(p)) {
