a11398ef87ae38e008c69bea143513e9a36a6577
Enric Balletbo i Serra <enric.balletbo@collabora.com>
2020-02-26 12:27:23 +0100

    drm/mediatek: Remove debug messages for function calls
    Equivalent information can be nowadays obtained using function tracer.
    
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    Reviewed-by: CK Hu <ck.hu@mediatek.com>
    Signed-off-by: Chun-Kuang Hu <chunkuang.hu@kernel.org>

--- drivers/gpu/drm/mediatek/mtk_drm_drv.c
+++ drivers/gpu/drm/mediatek/mtk_drm_drv.c
@@ -572,7 +572,6 @@ static int mtk_drm_sys_suspend(struct device *dev)
 	int ret;
 
 	ret = drm_mode_config_helper_suspend(drm);
-	DRM_DEBUG_DRIVER("mtk_drm_sys_suspend\n");
 
 	return ret;
 }
@@ -584,7 +583,6 @@ static int mtk_drm_sys_resume(struct device *dev)
 	int ret;
 
 	ret = drm_mode_config_helper_resume(drm);
-	DRM_DEBUG_DRIVER("mtk_drm_sys_resume\n");
 
 	return ret;
 }
