986a427e567c8c4f0ee1652f21442073db531d48
Hridya Valsaraju <hridya@google.com>
2020-02-28 11:46:35 -0800

    ANDROID: GKI: drivers: thermal: Fix ABI diff for struct thermal_cooling_device
    Add new fields to struct thermal_cooling_device.
    
    Test: make
    Bug: 149945768
    
    Change-Id: Ib0d55a0ee7ad8f835a5e969b7ef268d420085acf
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>
    (cherry picked commit from 8a12149c264c7b871932ad90f76e5981452bb4bb)
    Signed-off-by: Hridya Valsaraju <hridya@google.com>

--- include/linux/thermal.h
+++ include/linux/thermal.h
@@ -139,6 +139,8 @@ struct thermal_cooling_device {
 	struct mutex lock; /* protect thermal_instances list */
 	struct list_head thermal_instances;
 	struct list_head node;
+	unsigned long sysfs_cur_state_req;
+	unsigned long sysfs_min_state_req;
 };
 
 struct thermal_attr {
