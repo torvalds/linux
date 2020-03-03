a4037d1f1fc4e92b69d7196d4568c33078d465ea
YueHaibing <yuehaibing@huawei.com>
2020-03-03 16:56:04 +0800

    powerpc/pmac/smp: Drop unnecessary volatile qualifier
    core99_l2_cache/core99_l3_cache do not need to be marked as volatile,
    remove it.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200303085604.24952-1-yuehaibing@huawei.com

--- arch/powerpc/platforms/powermac/smp.c
+++ arch/powerpc/platforms/powermac/smp.c
@@ -664,8 +664,8 @@ static void core99_init_caches(int cpu)
 {
 #ifndef CONFIG_PPC64
 	/* L2 and L3 cache settings to pass from CPU0 to CPU1 on G4 cpus */
-	volatile static long int core99_l2_cache;
-	volatile static long int core99_l3_cache;
+	static long int core99_l2_cache;
+	static long int core99_l3_cache;
 
 	if (!cpu_has_feature(CPU_FTR_L2CR))
 		return;
