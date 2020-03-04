1e2328e762548c7d17b7ba8ded9f409d05710dd1
Carlos Neira <cneirabustos@gmail.com>
2020-03-04 17:41:55 -0300

    fs/nsfs.c: Added ns_match
    ns_match returns true if the namespace inode and dev_t matches the ones
    provided by the caller.
    
    Signed-off-by: Carlos Neira <cneirabustos@gmail.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200304204157.58695-2-cneirabustos@gmail.com

--- include/linux/proc_ns.h
+++ include/linux/proc_ns.h
@@ -85,6 +85,8 @@ typedef struct ns_common *ns_get_path_helper_t(void *);
 extern int ns_get_path_cb(struct path *path, ns_get_path_helper_t ns_get_cb,
 			    void *private_data);
 
+extern bool ns_match(const struct ns_common *ns, dev_t dev, ino_t ino);
+
 extern int ns_get_name(char *buf, size_t size, struct task_struct *task,
 			const struct proc_ns_operations *ns_ops);
 extern void nsfs_init(void);
