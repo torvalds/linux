7deaf533087dad3a2c445dccd6521d705d7d3adc
Madalin Bucur <madalin.bucur@nxp.com>
2020-03-04 18:04:27 +0200

    fsl/fman: detect FMan erratum A050385
    [ Upstream commit b281f7b93b258ce1419043bbd898a29254d5c9c7 ]
    
    Detect the presence of the A050385 erratum.
    
    Signed-off-by: Madalin Bucur <madalin.bucur@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/freescale/fman/fman.c
+++ drivers/net/ethernet/freescale/fman/fman.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2008-2015 Freescale Semiconductor Inc.
+ * Copyright 2020 NXP
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -565,6 +566,10 @@ struct fman_cfg {
 	u32 total_num_of_tasks;
 	u32 qmi_def_tnums_thresh;
 };
+
+#ifdef CONFIG_DPAA_ERRATUM_A050385
+static bool fman_has_err_a050385;
+#endif
 
 static irqreturn_t fman_exceptions(struct fman *fman,
 				   enum fman_exceptions exception)
@@ -2517,6 +2522,14 @@ struct fman *fman_bind(struct device *fm_dev)
 }
 EXPORT_SYMBOL(fman_bind);
 
+#ifdef CONFIG_DPAA_ERRATUM_A050385
+bool fman_has_errata_a050385(void)
+{
+	return fman_has_err_a050385;
+}
+EXPORT_SYMBOL(fman_has_errata_a050385);
+#endif
+
 static irqreturn_t fman_err_irq(int irq, void *handle)
 {
 	struct fman *fman = (struct fman *)handle;
@@ -2843,6 +2856,11 @@ static struct fman *read_dts_node(struct platform_device *of_dev)
 			__func__);
 		goto fman_free;
 	}
+
+#ifdef CONFIG_DPAA_ERRATUM_A050385
+	fman_has_err_a050385 =
+		of_property_read_bool(fm_node, "fsl,erratum-a050385");
+#endif
 
 	return fman;
 
