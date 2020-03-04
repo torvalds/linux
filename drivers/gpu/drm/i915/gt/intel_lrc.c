8e9f84cf5cac248a1c6a5daa4942879c8b765058
Chris Wilson <chris@chris-wilson.co.uk>
2020-03-04 12:18:49 +0000

    drm/i915/gt: Propagate change in error status to children on unhold
    As we release the head requests back into the queue, propagate any
    change in error status that may have occurred while the requests were
    temporarily suspended.
    
    Closes: https://gitlab.freedesktop.org/drm/intel/issues/1277
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200304121849.2448028-2-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/gt/intel_lrc.c
+++ drivers/gpu/drm/i915/gt/intel_lrc.c
@@ -2600,6 +2600,10 @@ static void __execlists_unhold(struct i915_request *rq)
 			struct i915_request *w =
 				container_of(p->waiter, typeof(*w), sched);
 
+			/* Propagate any change in error status */
+			if (rq->fence.error)
+				i915_request_set_error_once(w, rq->fence.error);
+
 			if (w->engine != rq->engine)
 				continue;
 
