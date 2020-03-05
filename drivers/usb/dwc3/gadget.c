a114c4ca64bd522aec1790c7e5c60c882f699d8f
Thinh Nguyen <Thinh.Nguyen@synopsys.com>
2020-03-05 13:23:49 -0800

    usb: dwc3: gadget: Don't clear flags before transfer ended
    We track END_TRANSFER command completion. Don't clear transfer
    started/ended flag prematurely. Otherwise, we'd run into the problem
    with restarting transfer before END_TRANSFER command finishes.
    
    Fixes: 6d8a019614f3 ("usb: dwc3: gadget: check for Missed Isoc from event status")
    Signed-off-by: Thinh Nguyen <thinhn@synopsys.com>
    Signed-off-by: Felipe Balbi <balbi@kernel.org>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -2570,10 +2570,8 @@ static void dwc3_gadget_endpoint_transfer_in_progress(struct dwc3_ep *dep,
 
 	dwc3_gadget_ep_cleanup_completed_requests(dep, event, status);
 
-	if (stop) {
+	if (stop)
 		dwc3_stop_active_transfer(dep, true, true);
-		dep->flags = DWC3_EP_ENABLED;
-	}
 
 	/*
 	 * WORKAROUND: This is the 2nd half of U1/U2 -> U0 workaround.
