0ef6845c8c1c1d46bda4e8325f6f23b199e8daac
Jay Cornwall <jay.cornwall@amd.com>
2020-03-06 11:42:21 -0600

    drm/amdkfd: Add aldebaran trap handler support
    Similar to arcturus, but ARCH/ACC VGPRs may now be split unevenly.
    A new field in SQ_WAVE_GPR_ALLOC tracks the boundary between the two
    sets of VGPRs.
    
    Squash below patches:
    
    drm/amdkfd: Use preprocessor for IP-specific trap handler code
    drm/amdkfd: Fix VGPR restore race in gfx8/gfx9 trap handler
    drm/amdkfd: Remove duplicated code in gfx9 trap handler
    drm/amdkfd: Separate ARCH/ACC VGPR restore in trap handler
    drm/amdkfd: Reverse order of ARCH/ACC VGPR restore in trap handler
    
    Signed-off-by: Jay Cornwall <jay.cornwall@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_device.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_device.c
@@ -656,11 +656,14 @@ static void kfd_cwsr_init(struct kfd_dev *kfd)
 			BUILD_BUG_ON(sizeof(cwsr_trap_gfx8_hex) > PAGE_SIZE);
 			kfd->cwsr_isa = cwsr_trap_gfx8_hex;
 			kfd->cwsr_isa_size = sizeof(cwsr_trap_gfx8_hex);
-		} else if (kfd->device_info->asic_family == CHIP_ARCTURUS
-			|| kfd->device_info->asic_family == CHIP_ALDEBARAN) {
+		} else if (kfd->device_info->asic_family == CHIP_ARCTURUS) {
 			BUILD_BUG_ON(sizeof(cwsr_trap_arcturus_hex) > PAGE_SIZE);
 			kfd->cwsr_isa = cwsr_trap_arcturus_hex;
 			kfd->cwsr_isa_size = sizeof(cwsr_trap_arcturus_hex);
+		} else if (kfd->device_info->asic_family == CHIP_ALDEBARAN) {
+			BUILD_BUG_ON(sizeof(cwsr_trap_aldebaran_hex) > PAGE_SIZE);
+			kfd->cwsr_isa = cwsr_trap_aldebaran_hex;
+			kfd->cwsr_isa_size = sizeof(cwsr_trap_aldebaran_hex);
 		} else if (kfd->device_info->asic_family < CHIP_NAVI10) {
 			BUILD_BUG_ON(sizeof(cwsr_trap_gfx9_hex) > PAGE_SIZE);
 			kfd->cwsr_isa = cwsr_trap_gfx9_hex;
