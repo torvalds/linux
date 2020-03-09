dacf470b26418e91bef195342b25a234c94052e3
Pavel Machek <pavel@ucw.cz>
2020-03-09 23:33:23 +0100

    net: slcan, slip -- no need for goto when if () will do
    No need to play with gotos to jump over single statement.
    
    Signed-off-by: Pavel Machek <pavel@ucw.cz>
    Acked-by: Oliver Hartkopp <socketcan@hartkopp.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/can/slcan.c
+++ drivers/net/can/slcan.c
@@ -348,11 +348,8 @@ static void slcan_write_wakeup(struct tty_struct *tty)
 
 	rcu_read_lock();
 	sl = rcu_dereference(tty->disc_data);
-	if (!sl)
-		goto out;
-
-	schedule_work(&sl->tx_work);
-out:
+	if (sl)
+		schedule_work(&sl->tx_work);
 	rcu_read_unlock();
 }
 
