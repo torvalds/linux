a2b9d4eadb7727772698d06e4cbeb1a1e2538675
Dominic Chen <d.c.ddcc@gmail.com>
2020-03-09 14:53:23 -0400

    fpga: dfl: afu: support debug access to memory-mapped afu regions
    Allow debug access to memory-mapped regions using e.g. gdb.
    
    Signed-off-by: Dominic Chen <d.c.ddcc@gmail.com>
    Acked-by: Wu Hao <hao.wu@intel.com>
    Signed-off-by: Moritz Fischer <mdf@kernel.org>

--- drivers/fpga/dfl-afu-main.c
+++ drivers/fpga/dfl-afu-main.c
@@ -750,6 +750,12 @@ static long afu_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	return -EINVAL;
 }
 
+static const struct vm_operations_struct afu_vma_ops = {
+#ifdef CONFIG_HAVE_IOREMAP_PROT
+	.access = generic_access_phys,
+#endif
+};
+
 static int afu_mmap(struct file *filp, struct vm_area_struct *vma)
 {
 	struct platform_device *pdev = filp->private_data;
@@ -778,6 +784,9 @@ static int afu_mmap(struct file *filp, struct vm_area_struct *vma)
 	if ((vma->vm_flags & VM_WRITE) &&
 	    !(region.flags & DFL_PORT_REGION_WRITE))
 		return -EPERM;
+
+	/* Support debug access to the mapping */
+	vma->vm_ops = &afu_vma_ops;
 
 	vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
 
