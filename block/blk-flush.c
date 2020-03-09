ce24f736f2e047d1489dc51f0aa66d5a6c5dfb12
Guoqing Jiang <guoqing.jiang@cloud.ionos.com>
2020-03-09 22:41:38 +0100

    block: cleanup comment for blk_flush_complete_seq
    Remove the comment about return value, since it is not valid after
    commit 404b8f5a03d84 ("block: cleanup kick/queued handling").
    
    Signed-off-by: Guoqing Jiang <guoqing.jiang@cloud.ionos.com>
    Reviewed-by: Nikolay Borisov <nborisov@suse.com>
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Reviewed-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-flush.c
+++ block/blk-flush.c
@@ -160,9 +160,6 @@ static void blk_account_io_flush(struct request *rq)
  *
  * CONTEXT:
  * spin_lock_irq(fq->mq_flush_lock)
- *
- * RETURNS:
- * %true if requests were added to the dispatch queue, %false otherwise.
  */
 static void blk_flush_complete_seq(struct request *rq,
 				   struct blk_flush_queue *fq,
