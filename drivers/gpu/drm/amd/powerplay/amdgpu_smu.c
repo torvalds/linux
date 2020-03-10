7bef6ea9721bb72bfe00810b9d928062a95e8549
Prike Liang <Prike.Liang@amd.com>
2020-03-10 08:20:01 +0800

    drm/amd/powerplay: fix the coverity warning about negative check for an unsigned value
    There will be a coverity warning because min and max are both unsigned.
    
    Signed-off-by: Prike Liang <Prike.Liang@amd.com>
    Reviewed-by: Evan Quan <evan.quan@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/amdgpu_smu.c
+++ drivers/gpu/drm/amd/powerplay/amdgpu_smu.c
@@ -214,9 +214,6 @@ int smu_set_soft_freq_range(struct smu_context *smu, enum smu_clk_type clk_type,
 {
 	int ret = 0;
 
-	if (min < 0 && max < 0)
-		return -EINVAL;
-
 	if (!smu_clk_dpm_is_enabled(smu, clk_type))
 		return 0;
 
