456ce3aa8cd4c82d0a0b32d40ea676221daee5df
Martijn Coenen <maco@android.com>
2020-03-10 14:12:30 +0100

    UPSTREAM: loop: Only change blocksize when needed.
    Return early in loop_set_block_size() if the requested block size is
    identical to the one we already have; this avoids expensive calls to
    freeze the block queue.
    
    Bug: 148607611
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Martijn Coenen <maco@android.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 7e81f99afd91c937f0e66dc135e26c1c4f78b003)
    Change-Id: I61778680579dbfeeb193133527a3926d376e0bac

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -1490,16 +1490,16 @@ static int loop_set_block_size(struct loop_device *lo, unsigned long arg)
 	if (arg < 512 || arg > PAGE_SIZE || !is_power_of_2(arg))
 		return -EINVAL;
 
-	if (lo->lo_queue->limits.logical_block_size != arg) {
-		sync_blockdev(lo->lo_device);
-		kill_bdev(lo->lo_device);
-	}
+	if (lo->lo_queue->limits.logical_block_size == arg)
+		return 0;
+
+	sync_blockdev(lo->lo_device);
+	kill_bdev(lo->lo_device);
 
 	blk_mq_freeze_queue(lo->lo_queue);
 
 	/* kill_bdev should have truncated all the pages */
-	if (lo->lo_queue->limits.logical_block_size != arg &&
-			lo->lo_device->bd_inode->i_mapping->nrpages) {
+	if (lo->lo_device->bd_inode->i_mapping->nrpages) {
 		err = -EAGAIN;
 		pr_warn("%s: loop%d (%s) has still dirty pages (nrpages=%lu)\n",
 			__func__, lo->lo_number, lo->lo_file_name,
