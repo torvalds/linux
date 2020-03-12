b35f14f410416f06ec54d187dedc372405757290
Andrii Nakryiko <andriin@fb.com>
2020-03-12 11:50:33 -0700

    libbpf: Split BTF presence checks into libbpf- and kernel-specific parts
    Needs for application BTF being present differs between user-space libbpf needs and kernel
    needs. Currently, BTF is mandatory only in kernel only when BPF application is
    using STRUCT_OPS. While libbpf itself relies more heavily on presense of BTF:
      - for BTF-defined maps;
      - for Kconfig externs;
      - for STRUCT_OPS as well.
    
    Thus, checks for presence and validness of bpf_object's BPF needs to be
    performed separately, which is patch does.
    
    Fixes: 5327644614a1 ("libbpf: Relax check whether BTF is mandatory")
    Reported-by: Michal Rostecki <mrostecki@opensuse.org>
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Cc: Quentin Monnet <quentin@isovalent.com>
    Link: https://lore.kernel.org/bpf/20200312185033.736911-1-andriin@fb.com

--- tools/lib/bpf/libbpf.c
+++ tools/lib/bpf/libbpf.c
@@ -2284,9 +2284,16 @@ static void bpf_object__sanitize_btf_ext(struct bpf_object *obj)
 	}
 }
 
-static bool bpf_object__is_btf_mandatory(const struct bpf_object *obj)
+static bool libbpf_needs_btf(const struct bpf_object *obj)
 {
-	return obj->efile.st_ops_shndx >= 0 || obj->nr_extern > 0;
+	return obj->efile.btf_maps_shndx >= 0 ||
+	       obj->efile.st_ops_shndx >= 0 ||
+	       obj->nr_extern > 0;
+}
+
+static bool kernel_needs_btf(const struct bpf_object *obj)
+{
+	return obj->efile.st_ops_shndx >= 0;
 }
 
 static int bpf_object__init_btf(struct bpf_object *obj,
@@ -2322,7 +2329,7 @@ static int bpf_object__init_btf(struct bpf_object *obj,
 		}
 	}
 out:
-	if (err && bpf_object__is_btf_mandatory(obj)) {
+	if (err && libbpf_needs_btf(obj)) {
 		pr_warn("BTF is required, but is missing or corrupted.\n");
 		return err;
 	}
@@ -2346,7 +2353,7 @@ static int bpf_object__finalize_btf(struct bpf_object *obj)
 	btf_ext__free(obj->btf_ext);
 	obj->btf_ext = NULL;
 
-	if (bpf_object__is_btf_mandatory(obj)) {
+	if (libbpf_needs_btf(obj)) {
 		pr_warn("BTF is required, but is missing or corrupted.\n");
 		return -ENOENT;
 	}
@@ -2410,7 +2417,7 @@ static int bpf_object__sanitize_and_load_btf(struct bpf_object *obj)
 			obj->btf_ext = NULL;
 		}
 
-		if (bpf_object__is_btf_mandatory(obj))
+		if (kernel_needs_btf(obj))
 			return err;
 	}
 	return 0;
