0af98cc1baf98d939a9dcbcb512c12b89300ef24
Frank Wang <frank.wang@rock-chips.com>
2020-03-12 10:17:11 +0800

    usb: dwc2: fix otg host fail after system resume
    Fix commit 079b3c7cdb59 ("usb: dwc2: make hcd into L3 power
    off state when suspend").
    
    This fixes otg-host restore failed after system resume for
    dwc2 related interrupts were not enabled when no gadget had
    been bounded before.
    
    Change-Id: Iceac66acf063c6226dcafb2086149122511ab5c1
    Signed-off-by: Frank Wang <frank.wang@rock-chips.com>

--- drivers/usb/dwc2/platform.c
+++ drivers/usb/dwc2/platform.c
@@ -605,6 +605,7 @@ static int __maybe_unused dwc2_suspend(struct device *dev)
 static int __maybe_unused dwc2_resume(struct device *dev)
 {
 	struct dwc2_hsotg *dwc2 = dev_get_drvdata(dev);
+	unsigned long flags;
 	int ret = 0;
 
 	if (dwc2->ll_hw_enabled) {
@@ -615,9 +616,13 @@ static int __maybe_unused dwc2_resume(struct device *dev)
 
 	/* Stop hcd if dr_mode is host and PD is power off when suspend */
 	if (dwc2->op_state == OTG_STATE_A_HOST && dwc2_is_device_mode(dwc2)) {
+		spin_lock_irqsave(&dwc2->lock, flags);
 		dwc2_hcd_disconnect(dwc2, true);
 		dwc2->op_state = OTG_STATE_B_PERIPHERAL;
 		dwc2->lx_state = DWC2_L3;
+		if (!dwc2->driver)
+			dwc2_hsotg_core_init_disconnected(dwc2, false);
+		spin_unlock_irqrestore(&dwc2->lock, flags);
 	}
 
 	if (dwc2_is_device_mode(dwc2))
