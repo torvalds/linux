ad6fd4d3114afc0fe9211f8dd3e58a72acb0e867
Shannon Nelson <snelson@pensando.io>
2020-03-16 20:22:07 -0700

    ionic: deinit rss only if selected
    Don't bother de-initing RSS if it wasn't selected.
    
    Fixes: aa3198819bea ("ionic: Add RSS support")
    Signed-off-by: Shannon Nelson <snelson@pensando.io>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/pensando/ionic/ionic_lif.c
+++ drivers/net/ethernet/pensando/ionic/ionic_lif.c
@@ -2066,7 +2066,8 @@ static void ionic_lif_deinit(struct ionic_lif *lif)
 	clear_bit(IONIC_LIF_F_INITED, lif->state);
 
 	ionic_rx_filters_deinit(lif);
-	ionic_lif_rss_deinit(lif);
+	if (lif->netdev->features & NETIF_F_RXHASH)
+		ionic_lif_rss_deinit(lif);
 
 	napi_disable(&lif->adminqcq->napi);
 	ionic_lif_qcq_deinit(lif, lif->notifyqcq);
