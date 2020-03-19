c29342a7366b625c9e221499f808e09047f7cd0a
Chao Yu <yuchao0@huawei.com>
2020-03-19 19:57:57 +0800

    f2fs: fix to avoid potential deadlock
    We should always check F2FS_I(inode)->cp_task condition in prior to other
    conditions in __should_serialize_io() to avoid deadloop described in
    commit 040d2bb318d1 ("f2fs: fix to avoid deadloop if data_flush is on"),
    however we break this rule when we support compression, fix it.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -2962,15 +2962,17 @@ next:
 static inline bool __should_serialize_io(struct inode *inode,
 					struct writeback_control *wbc)
 {
-	if (!S_ISREG(inode->i_mode))
-		return false;
-	if (f2fs_compressed_file(inode))
-		return true;
-	if (IS_NOQUOTA(inode))
-		return false;
 	/* to avoid deadlock in path of data flush */
 	if (F2FS_I(inode)->cp_task)
 		return false;
+
+	if (!S_ISREG(inode->i_mode))
+		return false;
+	if (IS_NOQUOTA(inode))
+		return false;
+
+	if (f2fs_compressed_file(inode))
+		return true;
 	if (wbc->sync_mode != WB_SYNC_ALL)
 		return true;
 	if (get_dirty_pages(inode) >= SM_I(F2FS_I_SB(inode))->min_seq_blocks)
