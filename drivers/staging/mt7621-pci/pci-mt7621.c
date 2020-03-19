8e37c13d958050abe9e4389355be57c6fb4119b5
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2020-03-19 17:14:16 +0100

    staging: mt7621-pci: don't return if get gpio fails
    In some platforms gpio's are not used for reset but
    for other purposes. Because of that when we try to
    get them are valid gpio's but are already assigned
    to do other function. To avoid those kind of problems
    in those platforms just notice the fail in the kernel
    but continue doing normal boot.
    
    Signed-off-by: Sergio Paracuellos <sergio.paracuellos@gmail.com>
    Link: https://lore.kernel.org/r/20200319161416.19033-1-sergio.paracuellos@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/mt7621-pci/pci-mt7621.c
+++ drivers/staging/mt7621-pci/pci-mt7621.c
@@ -363,10 +363,8 @@ static int mt7621_pcie_parse_port(struct mt7621_pcie *pcie,
 
 	port->gpio_rst = devm_gpiod_get_index_optional(dev, "reset", slot,
 						       GPIOD_OUT_LOW);
-	if (IS_ERR(port->gpio_rst)) {
-		dev_err(dev, "Failed to get GPIO for PCIe%d\n", slot);
-		return PTR_ERR(port->gpio_rst);
-	}
+	if (IS_ERR(port->gpio_rst))
+		dev_notice(dev, "Failed to get GPIO for PCIe%d\n", slot);
 
 	port->slot = slot;
 	port->pcie = pcie;
