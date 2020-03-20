2db9dbf71bf98d02a0bf33e798e5bfd2a9944696
Bernd Edlinger <bernd.edlinger@hotmail.de>
2020-03-20 21:27:24 +0100

    proc: Use new infrastructure to fix deadlocks in execve
    This changes lock_trace to use the new exec_update_mutex
    instead of cred_guard_mutex.
    
    This fixes possible deadlocks when the trace is accessing
    /proc/$pid/stack for instance.
    
    This should be safe, as the credentials are only used for reading,
    and task->mm is updated on execve under the new exec_update_mutex.
    
    Signed-off-by: Bernd Edlinger <bernd.edlinger@hotmail.de>
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -405,11 +405,11 @@ print0:
 
 static int lock_trace(struct task_struct *task)
 {
-	int err = mutex_lock_killable(&task->signal->cred_guard_mutex);
+	int err = mutex_lock_killable(&task->signal->exec_update_mutex);
 	if (err)
 		return err;
 	if (!ptrace_may_access(task, PTRACE_MODE_ATTACH_FSCREDS)) {
-		mutex_unlock(&task->signal->cred_guard_mutex);
+		mutex_unlock(&task->signal->exec_update_mutex);
 		return -EPERM;
 	}
 	return 0;
@@ -417,7 +417,7 @@ static int lock_trace(struct task_struct *task)
 
 static void unlock_trace(struct task_struct *task)
 {
-	mutex_unlock(&task->signal->cred_guard_mutex);
+	mutex_unlock(&task->signal->exec_update_mutex);
 }
 
 #ifdef CONFIG_STACKTRACE
