55b474c41e586a5c21c7ab81ff474eb6bacb4322
Michal Kubecek <mkubecek@suse.cz>
2020-03-21 00:46:50 +0100

    netlink: check for null extack in cookie helpers
    Unlike NL_SET_ERR_* macros, nl_set_extack_cookie_u64() and
    nl_set_extack_cookie_u32() helpers do not check extack argument for null
    and neither do their callers, as syzbot recently discovered for
    ethnl_parse_header().
    
    Instead of fixing the callers and leaving the trap in place, add check of
    null extack to both helpers to make them consistent with NL_SET_ERR_*
    macros.
    
    v2: drop incorrect second Fixes tag
    
    Fixes: 2363d73a2f3e ("ethtool: reject unrecognized request flags")
    Reported-by: syzbot+258a9089477493cea67b@syzkaller.appspotmail.com
    Signed-off-by: Michal Kubecek <mkubecek@suse.cz>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netlink.h
+++ include/linux/netlink.h
@@ -115,6 +115,8 @@ static inline void nl_set_extack_cookie_u64(struct netlink_ext_ack *extack,
 {
 	u64 __cookie = cookie;
 
+	if (!extack)
+		return;
 	memcpy(extack->cookie, &__cookie, sizeof(__cookie));
 	extack->cookie_len = sizeof(__cookie);
 }
@@ -124,6 +126,8 @@ static inline void nl_set_extack_cookie_u32(struct netlink_ext_ack *extack,
 {
 	u32 __cookie = cookie;
 
+	if (!extack)
+		return;
 	memcpy(extack->cookie, &__cookie, sizeof(__cookie));
 	extack->cookie_len = sizeof(__cookie);
 }
