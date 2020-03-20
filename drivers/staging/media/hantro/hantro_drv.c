8e4aaa68786319ea02ad30e55c1f753bf83418ab
Philipp Zabel <p.zabel@pengutronix.de>
2020-03-20 14:12:54 +0100

    media: hantro: add initial i.MX8MQ support
    This enables h.264, MPEG-2, and VP8 decoding on the Hantro G1 on
    i.MX8MQ, with post-processing support.
    
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/staging/media/hantro/hantro_drv.c
+++ drivers/staging/media/hantro/hantro_drv.c
@@ -499,6 +499,9 @@ static const struct of_device_id of_hantro_match[] = {
 	{ .compatible = "rockchip,rk3328-vpu", .data = &rk3328_vpu_variant, },
 	{ .compatible = "rockchip,rk3288-vpu", .data = &rk3288_vpu_variant, },
 #endif
+#ifdef CONFIG_VIDEO_HANTRO_IMX8M
+	{ .compatible = "nxp,imx8mq-vpu", .data = &imx8mq_vpu_variant, },
+#endif
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, of_hantro_match);
