ff44f672d74178b3be19d41a169b98b3e391d4ce
Willy Wolff <willy.mh.wolff.ml@gmail.com>
2020-03-21 09:27:40 +0000

    thermal/drivers/cpufreq_cooling: Fix return of cpufreq_set_cur_state
    When setting the cooling device current state from userspace via sysfs,
    the operation fails by returning an -EINVAL.
    
    It appears the recent changes with the per-policy frequency QoS
    introduced a regression as reported by:
    
     https://lkml.org/lkml/2020/3/20/599
    
    The function freq_qos_update_request returns 0 or 1 describing update
    effectiveness, and a negative error code on failure. However,
    cpufreq_set_cur_state returns 0 on success or an error code otherwise.
    
    Consider the QoS update as successful if the function does not return
    an error.
    
    Fixes: 3000ce3c52f8b ("cpufreq: Use per-policy frequency QoS")
    Signed-off-by: Willy Wolff <willy.mh.wolff.ml@gmail.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/20200321092740.7vvwfxsebcrznydh@macmini.local

--- drivers/thermal/cpufreq_cooling.c
+++ drivers/thermal/cpufreq_cooling.c
@@ -431,6 +431,7 @@ static int cpufreq_set_cur_state(struct thermal_cooling_device *cdev,
 				 unsigned long state)
 {
 	struct cpufreq_cooling_device *cpufreq_cdev = cdev->devdata;
+	int ret;
 
 	/* Request state should be less than max_level */
 	if (WARN_ON(state > cpufreq_cdev->max_level))
@@ -442,8 +443,9 @@ static int cpufreq_set_cur_state(struct thermal_cooling_device *cdev,
 
 	cpufreq_cdev->cpufreq_state = state;
 
-	return freq_qos_update_request(&cpufreq_cdev->qos_req,
-				get_state_freq(cpufreq_cdev, state));
+	ret = freq_qos_update_request(&cpufreq_cdev->qos_req,
+				      get_state_freq(cpufreq_cdev, state));
+	return ret < 0 ? ret : 0;
 }
 
 /* Bind cpufreq callbacks to thermal cooling device ops */
