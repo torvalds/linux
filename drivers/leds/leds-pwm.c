141f15c66d9472c642f38aae010ed68289036d7c
Denis Osterland-Heim <denis.osterland@diehl.com>
2020-03-21 08:15:53 +0000

    leds: pwm: remove header
    The header is only used by leds_pwm.c, so move contents to leds_pwm.c
    and remove it.
    Apply minor changes suggested by checkpatch.
    Remove deprecated and unused pwm_id member.
    
    Suggested-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Denis Osterland-Heim <Denis.Osterland@diehl.com>
    Signed-off-by: Pavel Machek <pavel@ucw.cz>

--- drivers/leds/leds-pwm.c
+++ drivers/leds/leds-pwm.c
@@ -16,8 +16,20 @@
 #include <linux/leds.h>
 #include <linux/err.h>
 #include <linux/pwm.h>
-#include <linux/leds_pwm.h>
 #include <linux/slab.h>
+
+struct led_pwm {
+	const char	*name;
+	const char	*default_trigger;
+	u8		active_low;
+	unsigned int	max_brightness;
+	unsigned int	pwm_period_ns;
+};
+
+struct led_pwm_platform_data {
+	int		num_leds;
+	struct led_pwm	*leds;
+};
 
 struct led_pwm_data {
 	struct led_classdev	cdev;
