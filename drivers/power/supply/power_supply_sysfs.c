3bc16e441fd4bc7ad17f4d37784a8aaba9a16d79
Anirudh Ghayal <aghayal@codeaurora.org>
2020-03-21 10:24:04 +0530

    ANDROID: GKI: power_supply: Add FG_TYPE power-supply property
    Use the FG_TYPE property to report the type of FG
    present in the system. For example on QG, it is
    voltage-mode based QG or cc-mode based QG.
    
    Change-Id: Iaad3ad29b2d778eed215d420a23b601587163e83
    Bug: 155087902
    Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>
    (cherry picked from commit 1c044cbc16076f1e0cd328e6b6684eb57c1ba07b)
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -506,6 +506,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(parallel_output_mode),
 	POWER_SUPPLY_ATTR(alignment),
 	POWER_SUPPLY_ATTR(moisture_detection_enabled),
+	POWER_SUPPLY_ATTR(fg_type),
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	POWER_SUPPLY_ATTR(charge_charger_state),
