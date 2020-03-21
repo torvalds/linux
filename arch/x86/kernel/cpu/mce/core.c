077168e241ec5a3b273652acb1e85f8bc1dc2d81
Wei Huang <wei.huang2@amd.com>
2020-03-21 14:38:00 -0500

    x86/mce/amd: Add PPIN support for AMD MCE
    Newer AMD CPUs support a feature called protected processor
    identification number (PPIN). This feature can be detected via
    CPUID_Fn80000008_EBX[23].
    
    However, CPUID alone is not enough to read the processor identification
    number - MSR_AMD_PPIN_CTL also needs to be configured properly. If, for
    any reason, MSR_AMD_PPIN_CTL[PPIN_EN] can not be turned on, such as
    disabled in BIOS, the CPU capability bit X86_FEATURE_AMD_PPIN needs to
    be cleared.
    
    When the X86_FEATURE_AMD_PPIN capability is available, the
    identification number is issued together with the MCE error info in
    order to keep track of the source of MCE errors.
    
     [ bp: Massage. ]
    
    Co-developed-by: Smita Koralahalli Channabasappa <smita.koralahallichannabasappa@amd.com>
    Signed-off-by: Smita Koralahalli Channabasappa <smita.koralahallichannabasappa@amd.com>
    Signed-off-by: Wei Huang <wei.huang2@amd.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Acked-by: Tony Luck <tony.luck@intel.com>
    Link: https://lkml.kernel.org/r/20200321193800.3666964-1-wei.huang2@amd.com

--- arch/x86/kernel/cpu/mce/core.c
+++ arch/x86/kernel/cpu/mce/core.c
@@ -142,6 +142,8 @@ void mce_setup(struct mce *m)
 
 	if (this_cpu_has(X86_FEATURE_INTEL_PPIN))
 		rdmsrl(MSR_PPIN, m->ppin);
+	else if (this_cpu_has(X86_FEATURE_AMD_PPIN))
+		rdmsrl(MSR_AMD_PPIN, m->ppin);
 
 	m->microcode = boot_cpu_data.microcode;
 }
