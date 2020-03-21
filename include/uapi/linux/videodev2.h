4b9158e03ab4ba1523b3acfc364c7c4ceb3ad43d
Hu Kejun <william.hu@rock-chips.com>
2020-03-21 10:43:04 +0800

    media: v4l: add dataformat for rockchip luma/ispp stat
    Signed-off-by: Hu Kejun <william.hu@rock-chips.com>
    Change-Id: I3e883b8066f27003bd50ce830aeb662dd4dc861f

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -723,8 +723,10 @@ struct v4l2_pix_format {
 /* Vendor specific - used for Rockchip ISP1 camera sub-system */
 #define V4L2_META_FMT_RK_ISP1_PARAMS	v4l2_fourcc('R', 'K', '1', 'P') /* Rockchip ISP1 params */
 #define V4L2_META_FMT_RK_ISP1_STAT_3A	v4l2_fourcc('R', 'K', '1', 'S') /* Rockchip ISP1 3A statistics */
+#define V4L2_META_FMT_RK_ISP1_STAT_LUMA	v4l2_fourcc('R', 'K', '1', 'L') /* Rockchip ISP1 luma statistics */
 
 #define V4L2_META_FMT_RK_ISPP_PARAMS	v4l2_fourcc('R', 'K', 'P', 'P') /* Rockchip ISPP params */
+#define V4L2_META_FMT_RK_ISPP_STAT	v4l2_fourcc('R', 'K', 'P', 'S') /* Rockchip ISPP statistics */
 
 /* priv field value to indicates that subsequent fields are valid. */
 #define V4L2_PIX_FMT_PRIV_MAGIC		0xfeedcafe
