660d1eb22301c290945450414fb0be5ae25526f1
Trond Myklebust <trond.myklebust@hammerspace.com>
2020-03-23 15:40:20 -0400

    pNFS/flexfile: Don't merge layout segments if the mirrors don't match
    Check that the number of mirrors, and the mirror information matches
    before deciding to merge layout segments in pNFS/flexfiles.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfs/flexfilelayout/flexfilelayout.c
+++ fs/nfs/flexfilelayout/flexfilelayout.c
@@ -284,6 +284,23 @@ static void _ff_layout_free_lseg(struct nfs4_ff_layout_segment *fls)
 }
 
 static bool
+ff_lseg_match_mirrors(struct pnfs_layout_segment *l1,
+		struct pnfs_layout_segment *l2)
+{
+	const struct nfs4_ff_layout_segment *fl1 = FF_LAYOUT_LSEG(l1);
+	const struct nfs4_ff_layout_segment *fl2 = FF_LAYOUT_LSEG(l1);
+	u32 i;
+
+	if (fl1->mirror_array_cnt != fl2->mirror_array_cnt)
+		return false;
+	for (i = 0; i < fl1->mirror_array_cnt; i++) {
+		if (fl1->mirror_array[i] != fl2->mirror_array[i])
+			return false;
+	}
+	return true;
+}
+
+static bool
 ff_lseg_range_is_after(const struct pnfs_layout_range *l1,
 		const struct pnfs_layout_range *l2)
 {
@@ -317,6 +334,8 @@ ff_lseg_merge(struct pnfs_layout_segment *new,
 	new_end = pnfs_calc_offset_end(new->pls_range.offset,
 			new->pls_range.length);
 	if (new_end < old->pls_range.offset)
+		return false;
+	if (!ff_lseg_match_mirrors(new, old))
 		return false;
 
 	/* Mergeable: copy info from 'old' to 'new' */
