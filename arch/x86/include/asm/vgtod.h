be7cb4739a04ece9c7c3a07c209908fe41a8ea08
Vincenzo Frascino <vincenzo.frascino@arm.com>
2020-03-23 12:41:09 +0000

    UPSTREAM: um: Fix header inclusion
    User Mode Linux is a flavor of x86 that from the vDSO prospective always
    falls back on system calls. This implies that it does not require any
    of the unified vDSO definitions and their inclusion causes side effects
    like this:
    
      In file included from include/vdso/processor.h:10:0,
                          from include/vdso/datapage.h:17,
                          from arch/x86/include/asm/vgtod.h:7,
                          from arch/x86/um/../kernel/sys_ia32.c:49:
      >> arch/x86/include/asm/vdso/processor.h:11:29: error: redefinition of 'rep_nop'
          static __always_inline void rep_nop(void)
                                      ^~~~~~~
         In file included from include/linux/rcupdate.h:30:0,
                          from include/linux/rculist.h:11,
                          from include/linux/pid.h:5,
                          from include/linux/sched.h:14,
                          from arch/x86/um/../kernel/sys_ia32.c:25:
         arch/x86/um/asm/processor.h:24:20: note: previous definition of 'rep_nop' was here
          static inline void rep_nop(void)
    
    Make sure that the unnecessary headers are not included when um is built
    to address the problem.
    
    Fixes: abc22418db02 ("x86/vdso: Enable x86 to use common headers")
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lkml.kernel.org/r/20200323124109.7104-1-vincenzo.frascino@arm.com
    (cherry picked from commit 1c1a18b00d7e25d1bed3507880de2da07be704a2)
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Ifc94c3b01a2021a293759dedb44e8ff70c6cebf3

--- arch/x86/include/asm/vgtod.h
+++ arch/x86/include/asm/vgtod.h
@@ -2,6 +2,11 @@
 #ifndef _ASM_X86_VGTOD_H
 #define _ASM_X86_VGTOD_H
 
+/*
+ * This check is required to prevent ARCH=um to include
+ * unwanted headers.
+ */
+#ifdef CONFIG_GENERIC_GETTIMEOFDAY
 #include <linux/compiler.h>
 #include <asm/clocksource.h>
 #include <vdso/datapage.h>
@@ -12,6 +17,7 @@ typedef u64 gtod_long_t;
 #else
 typedef unsigned long gtod_long_t;
 #endif
+#endif /* CONFIG_GENERIC_GETTIMEOFDAY */
 
 extern int vclocks_used;
 static inline bool vclock_was_used(int vclock)
