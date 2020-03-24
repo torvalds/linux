0aaf19130262059591be51bb745b59788a18c24c
Suman Anna <s-anna@ti.com>
2020-03-24 13:00:27 +0200

    remoteproc/omap: Add support for DRA7xx remote processors
    DRA7xx/AM57xx SoCs have two IPU and up to two DSP processor subsystems
    for offloading different computation algorithms. The IPU processor
    subsystem contains dual-core ARM Cortex-M4 processors, and is very
    similar to those on OMAP5. The DSP processor subsystem is based on
    the TI's standard TMS320C66x DSP CorePac core.
    
    Support has been added to the OMAP remoteproc driver through new
    DRA7xx specific compatibles for properly probing and booting all
    the different processor subsystem instances on DRA7xx/AM57xx
    SoCs - IPU1, IPU2, DSP1 & DSP2. A build dependency with SOC_DRA7XX
    is added to enable the driver to be built in DRA7xx-only configuration.
    
    The DSP boot address programming needed enhancement for DRA7xx as the
    boot register fields are different on DRA7 compared to OMAP4 and OMAP5
    SoCs. The register on DRA7xx contains additional fields within the
    register and the boot address bit-field is right-shifted by 10 bits.
    The internal memory parsing logic has also been updated to compute
    the device addresses for the L2 RAM for DSP devices using relative
    addressing logic, and to parse two additional RAMs at L1 level - L1P
    and L1D. This allows the remoteproc driver to support loading into
    these regions for a small subset of firmware images requiring as
    such. The most common usage would be to use the L1 programmable
    RAMs as L1 Caches.
    
    The firmware lookup logic also has to be adjusted for DRA7xx as
    there are (can be) more than one instance of both the IPU and DSP
    remote processors for the first time in OMAP4+ SoCs.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>
    [t-kristo@ti.com: moved address translation quirks to pdata]
    Signed-off-by: Tero Kristo <t-kristo@ti.com>
    Reviewed-by: Andrew F. Davis <afd@ti.com>
    Acked-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Link: https://lore.kernel.org/r/20200324110035.29907-8-t-kristo@ti.com
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/remoteproc/omap_remoteproc.c
+++ drivers/remoteproc/omap_remoteproc.c
@@ -34,10 +34,13 @@
  * struct omap_rproc_boot_data - boot data structure for the DSP omap rprocs
  * @syscon: regmap handle for the system control configuration module
  * @boot_reg: boot register offset within the @syscon regmap
+ * @boot_reg_shift: bit-field shift required for the boot address value in
+ *		    @boot_reg
  */
 struct omap_rproc_boot_data {
 	struct regmap *syscon;
 	unsigned int boot_reg;
+	unsigned int boot_reg_shift;
 };
 
 /**
@@ -161,6 +164,8 @@ static int omap_rproc_write_dsp_boot_addr(struct rproc *rproc)
 	struct omap_rproc *oproc = rproc->priv;
 	struct omap_rproc_boot_data *bdata = oproc->boot_data;
 	u32 offset = bdata->boot_reg;
+	u32 value;
+	u32 mask;
 
 	if (rproc->bootaddr & (SZ_1K - 1)) {
 		dev_err(dev, "invalid boot address 0x%llx, must be aligned on a 1KB boundary\n",
@@ -168,7 +173,10 @@ static int omap_rproc_write_dsp_boot_addr(struct rproc *rproc)
 		return -EINVAL;
 	}
 
-	return regmap_write(bdata->syscon, offset, rproc->bootaddr);
+	value = rproc->bootaddr >> bdata->boot_reg_shift;
+	mask = ~(SZ_1K - 1) >> bdata->boot_reg_shift;
+
+	return regmap_update_bits(bdata->syscon, offset, mask, value);
 }
 
 /*
@@ -297,6 +305,13 @@ static const struct omap_rproc_mem_data ipu_mems[] = {
 	{ },
 };
 
+static const struct omap_rproc_mem_data dra7_dsp_mems[] = {
+	{ .name = "l2ram", .dev_addr = 0x800000 },
+	{ .name = "l1pram", .dev_addr = 0xe00000 },
+	{ .name = "l1dram", .dev_addr = 0xf00000 },
+	{ },
+};
+
 static const struct omap_rproc_dev_data omap4_dsp_dev_data = {
 	.device_name	= "dsp",
 };
@@ -311,6 +326,16 @@ static const struct omap_rproc_dev_data omap5_dsp_dev_data = {
 };
 
 static const struct omap_rproc_dev_data omap5_ipu_dev_data = {
+	.device_name	= "ipu",
+	.mems		= ipu_mems,
+};
+
+static const struct omap_rproc_dev_data dra7_dsp_dev_data = {
+	.device_name	= "dsp",
+	.mems		= dra7_dsp_mems,
+};
+
+static const struct omap_rproc_dev_data dra7_ipu_dev_data = {
 	.device_name	= "ipu",
 	.mems		= ipu_mems,
 };
@@ -331,6 +356,14 @@ static const struct of_device_id omap_rproc_of_match[] = {
 	{
 		.compatible     = "ti,omap5-ipu",
 		.data           = &omap5_ipu_dev_data,
+	},
+	{
+		.compatible     = "ti,dra7-dsp",
+		.data           = &dra7_dsp_dev_data,
+	},
+	{
+		.compatible     = "ti,dra7-ipu",
+		.data           = &dra7_ipu_dev_data,
 	},
 	{
 		/* end */
@@ -383,6 +416,9 @@ static int omap_rproc_get_boot_data(struct platform_device *pdev,
 		dev_err(&pdev->dev, "couldn't get the boot register\n");
 		return -EINVAL;
 	}
+
+	of_property_read_u32_index(np, "ti,bootreg", 2,
+				   &oproc->boot_data->boot_reg_shift);
 
 	return 0;
 }
