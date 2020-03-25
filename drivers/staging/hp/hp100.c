285d15c25702215dcd49d68551eff37f9025af16
Soumyajit Deb <debsoumyajit100@gmail.com>
2020-03-25 17:57:52 +0530

    staging: hp100: Add spaces in if statement.
    Add space between if and open parenthesis to improve
    code readability and to adhere to the standard linux kernel
    coding style. Also, shift the next line to the right by a single space
    as it is the continuation of the above if statement.
    
    Signed-off-by: Soumyajit Deb <debsoumyajit100@gmail.com>
    Link: https://lore.kernel.org/r/20200325122752.38600-1-debsoumyajit100@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/hp/hp100.c
+++ drivers/staging/hp/hp100.c
@@ -553,8 +553,8 @@ static int hp100_probe1(struct net_device *dev, int ioaddr, u_char bus,
 			 * can map the full 32bit address space.
 			 * Also, we can have EISA Busmaster cards (not tested),
 			 * so beware !!! - Jean II */
-			if((bus == HP100_BUS_PCI) &&
-			   (pci_set_dma_mask(pci_dev, DMA_BIT_MASK(32)))) {
+			if ((bus == HP100_BUS_PCI) &&
+			    (pci_set_dma_mask(pci_dev, DMA_BIT_MASK(32)))) {
 				/* Gracefully fallback to shared memory */
 				goto busmasterfail;
 			}
