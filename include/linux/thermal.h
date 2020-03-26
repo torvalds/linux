8c83a60c3bb8654879296b09ec1d7e9d33c51c2d
Hridya Valsaraju <hridya@google.com>
2020-03-26 12:51:06 -0700

    ANDROID: GKI: Fix ABI diff for struct thermal_cooling_device_ops
    Adds members set/get_min_state() to struct
    thermal_cooling_device_ops.
    
    Also adds the field min_state_throttle to struct thermal_governor.
    
    Test: build
    Bug: 149945768
    Change-Id: I085feb9d3b818dcf9754f0f624166c360593bce6
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>
    (cherry picked from commit 8a12149c264c7b871932ad90f76e5981452bb4bb)
    [hridya: cherry-picked only the ABI diff]
    Signed-off-by: Hridya Valsaraju <hridya@google.com>

--- include/linux/thermal.h
+++ include/linux/thermal.h
@@ -134,6 +134,10 @@ struct thermal_cooling_device_ops {
 	int (*get_max_state) (struct thermal_cooling_device *, unsigned long *);
 	int (*get_cur_state) (struct thermal_cooling_device *, unsigned long *);
 	int (*set_cur_state) (struct thermal_cooling_device *, unsigned long);
+	int (*set_min_state)(struct thermal_cooling_device *cdev,
+				unsigned long target);
+	int (*get_min_state)(struct thermal_cooling_device *cdev,
+				unsigned long *target);
 	int (*get_requested_power)(struct thermal_cooling_device *,
 				   struct thermal_zone_device *, u32 *);
 	int (*state2power)(struct thermal_cooling_device *,
@@ -256,6 +260,7 @@ struct thermal_governor {
 	void (*unbind_from_tz)(struct thermal_zone_device *tz);
 	int (*throttle)(struct thermal_zone_device *tz, int trip);
 	struct list_head	governor_list;
+	int min_state_throttle;
 };
 
 /* Structure that holds binding parameters for a zone */
