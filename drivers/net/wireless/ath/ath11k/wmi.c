9a8074e3bcd7956ec6b4f7c26360af1b0b0abe38
Colin Ian King <colin.king@canonical.com>
2020-03-27 19:26:39 +0000

    ath11k: fix error message to correctly report the command that failed
    Currently the error message refers to the command WMI_TWT_DIeABLE_CMDID
    which looks like a cut-n-paste mangled typo. Fix the message to match
    the command WMI_BSS_COLOR_CHANGE_ENABLE_CMDID that failed.
    
    Fixes: 5a032c8d1953 ("ath11k: add WMI calls required for handling BSS color")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200327192639.363354-1-colin.king@canonical.com

--- drivers/net/wireless/ath/ath11k/wmi.c
+++ drivers/net/wireless/ath/ath11k/wmi.c
@@ -2919,7 +2919,7 @@ int ath11k_wmi_send_bss_color_change_enable_cmd(struct ath11k *ar, u32 vdev_id,
 	ret = ath11k_wmi_cmd_send(wmi, skb,
 				  WMI_BSS_COLOR_CHANGE_ENABLE_CMDID);
 	if (ret) {
-		ath11k_warn(ab, "Failed to send WMI_TWT_DIeABLE_CMDID");
+		ath11k_warn(ab, "Failed to send WMI_BSS_COLOR_CHANGE_ENABLE_CMDID");
 		dev_kfree_skb(skb);
 	}
 	return ret;
