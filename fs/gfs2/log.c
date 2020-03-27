75b46c437f6b0f8e37032a407c7373f85f5c26a8
Bob Peterson <rpeterso@redhat.com>
2020-03-27 15:23:14 -0500

    gfs2: Fix oversight in gfs2_ail1_flush
    Ordinarily, function gfs2_ail1_start_one issues a write request
    for one item on the ail1 list, then returns -EBUSY. This makes the
    caller, gfs2_ail1_flush, loop around and start another. However,
    it was not clearing the -EBUSY return code each time through the loop.
    So on rare occasions, like when the wbc runs out of nr_to_write, it
    remained set to -EBUSY, which triggered an error and withdraw.
    
    This patch sets the return code to 0 each time through the restart
    loop so this won't happen anymore.
    
    Signed-off-by: Bob Peterson <rpeterso@redhat.com>

--- fs/gfs2/log.c
+++ fs/gfs2/log.c
@@ -188,13 +188,14 @@ void gfs2_ail1_flush(struct gfs2_sbd *sdp, struct writeback_control *wbc)
 	struct list_head *head = &sdp->sd_ail1_list;
 	struct gfs2_trans *tr;
 	struct blk_plug plug;
-	int ret = 0;
+	int ret;
 	unsigned long flush_start = jiffies;
 
 	trace_gfs2_ail_flush(sdp, wbc, 1);
 	blk_start_plug(&plug);
 	spin_lock(&sdp->sd_ail_lock);
 restart:
+	ret = 0;
 	if (time_after(jiffies, flush_start + (HZ * 600))) {
 		dump_ail_list(sdp);
 		goto out;
