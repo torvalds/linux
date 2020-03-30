e02f6c01748df77b3fe202bbf7dde0aae6ecced7
Marc Zyngier <maz@kernel.org>
2020-03-30 10:41:58 +0100

    Revert "irqchip/xilinx: Do not call irq_set_default_host()"
    This reverts commit 9c2d4f525c002591f4e0c14a37663663aaba1656, which
    breaks a number of PPC platforms.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/44b64be7-9240-fd52-af90-e0245220f38b@xilinx.com

--- drivers/irqchip/irq-xilinx-intc.c
+++ drivers/irqchip/irq-xilinx-intc.c
@@ -250,6 +250,7 @@ static int __init xilinx_intc_of_init(struct device_node *intc,
 		}
 	} else {
 		primary_intc = irqc;
+		irq_set_default_host(primary_intc->root_domain);
 		set_handle_irq(xil_intc_handle_irq);
 	}
 
