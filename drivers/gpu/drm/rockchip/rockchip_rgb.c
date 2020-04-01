0f7832c079e2bd8efde63cde9e6576b5678f7db9
Sandy Huang <hjc@rock-chips.com>
2020-04-01 16:18:50 +0800

    drm/rockchip: rgb: Add supoort bt656 and bt1120
    Change-Id: I9a0fe7933979e58dc76f6a12611ff539086ba04a
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>

--- drivers/gpu/drm/rockchip/rockchip_rgb.c
+++ drivers/gpu/drm/rockchip/rockchip_rgb.c
@@ -188,6 +188,12 @@ rockchip_rgb_encoder_atomic_check(struct drm_encoder *encoder,
 	case MEDIA_BUS_FMT_SRGB888_DUMMY_4X8:
 		s->output_mode = ROCKCHIP_OUT_MODE_S888_DUMMY;
 		break;
+	case MEDIA_BUS_FMT_YUYV8_2X8:
+		s->output_mode = ROCKCHIP_OUT_MODE_BT656;
+		break;
+	case MEDIA_BUS_FMT_YUYV8_1X16:
+		s->output_mode = ROCKCHIP_OUT_MODE_BT1120;
+		break;
 	case MEDIA_BUS_FMT_RGB888_1X24:
 	case MEDIA_BUS_FMT_RGB666_1X24_CPADHI:
 	default:
