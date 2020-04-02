dcf1763546d76c372f3136c8d6b2b6e77f140cf0
Li Xinhai <lixinhai.lxh@gmail.com>
2020-04-01 21:10:48 -0700

    mm/mempolicy: support MPOL_MF_STRICT for huge page mapping
    MPOL_MF_STRICT is used in mbind() for purposes:
    
    (1) MPOL_MF_STRICT is set alone without MPOL_MF_MOVE or
        MPOL_MF_MOVE_ALL, to check if there is misplaced page and return -EIO;
    
    (2) MPOL_MF_STRICT is set with MPOL_MF_MOVE or MPOL_MF_MOVE_ALL, to
        check if there is misplaced page which is failed to isolate, or page
        is success on isolate but failed to move, and return -EIO.
    
    For non hugepage mapping, (1) and (2) are implemented as expectation.  For
    hugepage mapping, (1) is not implemented.  And in (2), the part about
    failed to isolate and report -EIO is not implemented.
    
    This patch implements the missed parts for hugepage mapping.  Benefits
    with it applied:
    
    - User space can apply same code logic to handle mbind() on hugepage and
      non hugepage mapping;
    
    - Reliably using MPOL_MF_STRICT alone to check whether there is
      misplaced page or not when bind policy on address range, especially for
      address range which contains both hugepage and non hugepage mapping.
    
    Analysis of potential impact to existing users:
    
    - If MPOL_MF_STRICT alone was previously used, hugetlb pages not
      following the memory policy would not cause an EIO error.  After this
      change, hugetlb pages are treated like all other pages.  If
      MPOL_MF_STRICT alone is used and hugetlb pages do not follow memory
      policy an EIO error will be returned.
    
    - For users who using MPOL_MF_STRICT with MPOL_MF_MOVE or
      MPOL_MF_MOVE_ALL, the semantic about some pages could not be moved will
      not be changed by this patch, because failed to isolate and failed to
      move have same effects to users, so their existing code will not be
      impacted.
    
    In mbind man page, the note about 'MPOL_MF_STRICT is ignored on huge page
    mappings' can be removed after this patch is applied.
    
    Mike:
    
    : The current behavior with MPOL_MF_STRICT and hugetlb pages is inconsistent
    : and does not match documentation (as described above).  The special
    : behavior for hugetlb pages ideally should have been removed when hugetlb
    : page migration was introduced.  It is unlikely that anyone relies on
    : today's inconsistent behavior, and removing one more case of special
    : handling for hugetlb pages is a good thing.
    
    Signed-off-by: Li Xinhai <lixinhai.lxh@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
    Reviewed-by: Naoya Horiguchi <naoya.horiguchi@nec.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: linux-man <linux-man@vger.kernel.org>
    Link: http://lkml.kernel.org/r/1581559627-6206-1-git-send-email-lixinhai.lxh@gmail.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/mempolicy.c
+++ mm/mempolicy.c
@@ -557,9 +557,10 @@ static int queue_pages_hugetlb(pte_t *pte, unsigned long hmask,
 			       unsigned long addr, unsigned long end,
 			       struct mm_walk *walk)
 {
+	int ret = 0;
 #ifdef CONFIG_HUGETLB_PAGE
 	struct queue_pages *qp = walk->private;
-	unsigned long flags = qp->flags;
+	unsigned long flags = (qp->flags & MPOL_MF_VALID);
 	struct page *page;
 	spinlock_t *ptl;
 	pte_t entry;
@@ -571,16 +572,44 @@ static int queue_pages_hugetlb(pte_t *pte, unsigned long hmask,
 	page = pte_page(entry);
 	if (!queue_pages_required(page, qp))
 		goto unlock;
+
+	if (flags == MPOL_MF_STRICT) {
+		/*
+		 * STRICT alone means only detecting misplaced page and no
+		 * need to further check other vma.
+		 */
+		ret = -EIO;
+		goto unlock;
+	}
+
+	if (!vma_migratable(walk->vma)) {
+		/*
+		 * Must be STRICT with MOVE*, otherwise .test_walk() have
+		 * stopped walking current vma.
+		 * Detecting misplaced page but allow migrating pages which
+		 * have been queued.
+		 */
+		ret = 1;
+		goto unlock;
+	}
+
 	/* With MPOL_MF_MOVE, we migrate only unshared hugepage. */
 	if (flags & (MPOL_MF_MOVE_ALL) ||
-	    (flags & MPOL_MF_MOVE && page_mapcount(page) == 1))
-		isolate_huge_page(page, qp->pagelist);
+	    (flags & MPOL_MF_MOVE && page_mapcount(page) == 1)) {
+		if (!isolate_huge_page(page, qp->pagelist) &&
+			(flags & MPOL_MF_STRICT))
+			/*
+			 * Failed to isolate page but allow migrating pages
+			 * which have been queued.
+			 */
+			ret = 1;
+	}
 unlock:
 	spin_unlock(ptl);
 #else
 	BUG();
 #endif
-	return 0;
+	return ret;
 }
 
 #ifdef CONFIG_NUMA_BALANCING
