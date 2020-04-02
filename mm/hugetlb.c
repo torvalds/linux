d4af73e3f8d09b0417ba45082f289fc38bd3a970
Vlastimil Babka <vbabka@suse.cz>
2020-04-01 21:11:48 -0700

    mm/hugetlb: remove unnecessary memory fetch in PageHeadHuge()
    Commit f1e61557f023 ("mm: pack compound_dtor and compound_order into one
    word in struct page") changed compound_dtor from a pointer to an array
    index in order to pack it.  To check if page has the hugeltbfs
    compound_dtor, we can just compare the index directly without fetching the
    function pointer.  Said commit did that with PageHuge() and we can do the
    same with PageHeadHuge() to make the code a bit smaller and faster.
    
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Neha Agarwal <nehaagarwal@google.com>
    Link: http://lkml.kernel.org/r/20200311172440.6988-1-vbabka@suse.cz
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -1528,7 +1528,7 @@ int PageHeadHuge(struct page *page_head)
 	if (!PageHead(page_head))
 		return 0;
 
-	return get_compound_page_dtor(page_head) == free_huge_page;
+	return page_head[1].compound_dtor == HUGETLB_PAGE_DTOR;
 }
 
 /*
