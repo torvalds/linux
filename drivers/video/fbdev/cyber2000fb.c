43ea4d090509de46e3ad7688560743dcad05d329
Jules Irenge <jbi.octave@gmail.com>
2020-04-03 17:05:02 +0100

    video: Add missing annotation for cyber2000fb_enable_ddc() and cyber2000fb_disable_ddc()
    Sparse reports warnings at cyber2000fb_enable_ddc()
    	and cyber2000fb_disable_ddc()
    
    warning: context imbalance in cyber2000fb_enable_ddc()
    	- wrong count at exit
    
    warning: context imbalance in cyber2000fb_disable_ddc()
    	- unexpected unlock
    
    The root cause is the missing annotation at cyber2000fb_enable_ddc()
    	and cyber2000fb_disable_ddc()
    
    Add the missing __acquires(&cfb->reg_b0_lock) annotation
    Add the missing __releases(&cfb->reg_b0_lock) annotation
    
    Signed-off-by: Jules Irenge <jbi.octave@gmail.com>
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200403160505.2832-3-jbi.octave@gmail.com

--- drivers/video/fbdev/cyber2000fb.c
+++ drivers/video/fbdev/cyber2000fb.c
@@ -1160,12 +1160,14 @@ EXPORT_SYMBOL(cyber2000fb_detach);
 #define DDC_SDA_IN	(1 << 6)
 
 static void cyber2000fb_enable_ddc(struct cfb_info *cfb)
+	__acquires(&cfb->reg_b0_lock)
 {
 	spin_lock(&cfb->reg_b0_lock);
 	cyber2000fb_writew(0x1bf, 0x3ce, cfb);
 }
 
 static void cyber2000fb_disable_ddc(struct cfb_info *cfb)
+	__releases(&cfb->reg_b0_lock)
 {
 	cyber2000fb_writew(0x0bf, 0x3ce, cfb);
 	spin_unlock(&cfb->reg_b0_lock);
