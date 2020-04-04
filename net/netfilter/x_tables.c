3800aa06f0f09e8056ba4c1cb65cccbcc0b1cac9
Tao Huang <huangtao@rock-chips.com>
2020-04-04 15:55:57 +0800

    netfilter: x_tables: Call xt_init early when CONFIG_ROCKCHIP_THUNDER_BOOT=y
    Change-Id: If9aaae57704e064b719731efa3112d4e652f0069
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- net/netfilter/x_tables.c
+++ net/netfilter/x_tables.c
@@ -1924,6 +1924,10 @@ static void __exit xt_fini(void)
 	kfree(xt);
 }
 
+#ifdef CONFIG_ROCKCHIP_THUNDER_BOOT
+rootfs_initcall(xt_init);
+#else
 module_init(xt_init);
+#endif
 module_exit(xt_fini);
 
