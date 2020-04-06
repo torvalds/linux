a4837980fd9fa4c70a821d11831698901baef56b
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2020-04-06 14:39:32 +0300

    net: revert default NAPI poll timeout to 2 jiffies
    For HZ < 1000 timeout 2000us rounds up to 1 jiffy but expires randomly
    because next timer interrupt could come shortly after starting softirq.
    
    For commonly used CONFIG_HZ=1000 nothing changes.
    
    Fixes: 7acf8a1e8a28 ("Replace 2 jiffies with sysctl netdev_budget_usecs to enable softirq tuning")
    Reported-by: Dmitry Yakunin <zeil@yandex-team.ru>
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -4140,7 +4140,8 @@ EXPORT_SYMBOL(netdev_max_backlog);
 
 int netdev_tstamp_prequeue __read_mostly = 1;
 int netdev_budget __read_mostly = 300;
-unsigned int __read_mostly netdev_budget_usecs = 2000;
+/* Must be at least 2 jiffes to guarantee 1 jiffy timeout */
+unsigned int __read_mostly netdev_budget_usecs = 2 * USEC_PER_SEC / HZ;
 int weight_p __read_mostly = 64;           /* old backlog weight */
 int dev_weight_rx_bias __read_mostly = 1;  /* bias for backlog weight */
 int dev_weight_tx_bias __read_mostly = 1;  /* bias for output_queue quota */
