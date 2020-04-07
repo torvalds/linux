4708f31885a0d3e41a03c39501df697e73406f6a
Palmer Dabbelt <palmerdabbelt@google.com>
2020-04-06 20:08:00 -0700

    mm: prevent a warning when casting void* -> enum
    I recently build the RISC-V port with LLVM trunk, which has introduced a
    new warning when casting from a pointer to an enum of a smaller size.
    This patch simply casts to a long in the middle to stop the warning.  I'd
    be surprised this is the only one in the kernel, but it's the only one I
    saw.
    
    Signed-off-by: Palmer Dabbelt <palmerdabbelt@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Link: http://lkml.kernel.org/r/20200227211741.83165-1-palmer@dabbelt.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/rmap.c
+++ mm/rmap.c
@@ -1372,7 +1372,7 @@ static bool try_to_unmap_one(struct page *page, struct vm_area_struct *vma,
 	struct page *subpage;
 	bool ret = true;
 	struct mmu_notifier_range range;
-	enum ttu_flags flags = (enum ttu_flags)arg;
+	enum ttu_flags flags = (enum ttu_flags)(long)arg;
 
 	/* munlock has nothing to gain from examining un-locked vmas */
 	if ((flags & TTU_MUNLOCK) && !(vma->vm_flags & VM_LOCKED))
