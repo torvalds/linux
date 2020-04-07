97fd8955415750cf47f4b2b799fd0527c848d2d0
Daniel Vetter <daniel.vetter@intel.com>
2020-04-07 15:30:02 +0200

    UPSTREAM: dma-buf: Fix SET_NAME ioctl uapi
    The uapi is the same on 32 and 64 bit, but the number isn't. Everyone
    who botched this please re-read:
    
    https://www.kernel.org/doc/html/v5.4-preprc-cpu/ioctl/botching-up-ioctls.html
    
    Also, the type argument for the ioctl macros is for the type the void
    __user *arg pointer points at, which in this case would be the
    variable-sized char[] of a 0 terminated string. So this was botched in
    more than just the usual ways.
    
    Change-Id: Iaffffa806de8f6f10cd167881bb63fdabd6724a2
    Cc: Sumit Semwal <sumit.semwal@linaro.org>
    Cc: Chenbo Feng <fengc@google.com>
    Cc: Greg Hackmann <ghackmann@google.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: linux-media@vger.kernel.org
    Cc: linaro-mm-sig@lists.linaro.org
    Cc: minchan@kernel.org
    Cc: surenb@google.com
    Cc: jenhaochen@google.com
    Cc: Martin Liu <liumartin@google.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Tested-by: Martin Liu <liumartin@google.com>
    Reviewed-by: Martin Liu <liumartin@google.com>
    Signed-off-by: Sumit Semwal <sumit.semwal@linaro.org>
      [sumits: updated some checkpatch fixes, corrected author email]
    Link: https://patchwork.freedesktop.org/patch/msgid/20200407133002.3486387-1-daniel.vetter@ffwll.ch
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>
    (cherry picked from commit a5bff92eaac45bdf6221badf9505c26792fdf99e)

--- include/uapi/linux/dma-buf.h
+++ include/uapi/linux/dma-buf.h
@@ -41,6 +41,12 @@ struct dma_buf_sync {
 
 #define DMA_BUF_BASE		'b'
 #define DMA_BUF_IOCTL_SYNC	_IOW(DMA_BUF_BASE, 0, struct dma_buf_sync)
+
+/* 32/64bitness of this uapi was botched in android, there's no difference
+ * between them in actual uapi, they're just different numbers.
+ */
 #define DMA_BUF_SET_NAME	_IOW(DMA_BUF_BASE, 1, const char *)
+#define DMA_BUF_SET_NAME_A	_IOW(DMA_BUF_BASE, 1, u32)
+#define DMA_BUF_SET_NAME_B	_IOW(DMA_BUF_BASE, 1, u64)
 
 #endif
