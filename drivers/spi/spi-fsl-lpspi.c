cb75b0c4dee881fe9d486e725b778395ce0b0426
Aishwarya R <aishwaryarj100@gmail.com>
2020-04-07 18:25:57 +0530

    spi: spi-fsl-lpspi: Fix indentation and open brace should be on the previous line
    Resolved open brace { should be on the previous line checkpatch
    error and fix the indentation
    
    Signed-off-by: Aishwarya R <aishwaryarj100@gmail.com>
    Link: https://lore.kernel.org/r/20200407125557.6520-1-aishwaryarj100@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-fsl-lpspi.c
+++ drivers/spi/spi-fsl-lpspi.c
@@ -186,14 +186,13 @@ static bool fsl_lpspi_can_dma(struct spi_controller *controller,
 
 	bytes_per_word = fsl_lpspi_bytes_per_word(transfer->bits_per_word);
 
-	switch (bytes_per_word)
-	{
-		case 1:
-		case 2:
-		case 4:
-			break;
-		default:
-			return false;
+	switch (bytes_per_word) {
+	case 1:
+	case 2:
+	case 4:
+		break;
+	default:
+		return false;
 	}
 
 	return true;
