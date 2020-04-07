a8972573eb5c784a9c199fedbfdfb694e0ca4233
John Hubbard <jhubbard@nvidia.com>
2020-04-06 20:10:55 -0700

    checkpatch: support "base-commit:" format
    In order to support the get-lore-mbox.py tool described in [1], I ran:
    
        git format-patch --base=<commit> --cover-letter <revrange>
    
    ...  which generated a "base-commit: <commit-hash>" tag at the end of the
    cover letter.  However, checkpatch.pl generated an error upon encounting
    "base-commit:" in the cover letter:
    
        "ERROR: Please use git commit description style..."
    
    ...  because it found the "commit" keyword, and failed to recognize that
    it was part of the "base-commit" phrase, and as such, should not be
    subjected to the same commit description style rules.
    
    Update checkpatch.pl to include a special case for "base-commit:" (at the
    start of the line, possibly with some leading whitespace) so that that tag
    no longer generates a checkpatch error.
    
    [1] https://lwn.net/Articles/811528/ "Better tools for kernel
        developers"
    
    Suggested-by: Joe Perches <joe@perches.com>
    Signed-off-by: John Hubbard <jhubbard@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Joe Perches <joe@perches.com>
    Cc: Andy Whitcroft <apw@canonical.com>
    Cc: Konstantin Ryabitsev <konstantin@linuxfoundation.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Link: http://lkml.kernel.org/r/20200213055004.69235-2-jhubbard@nvidia.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -2780,7 +2780,7 @@ sub process {
 
 # Check for git id commit length and improperly formed commit descriptions
 		if ($in_commit_log && !$commit_log_possible_stack_dump &&
-		    $line !~ /^\s*(?:Link|Patchwork|http|https|BugLink):/i &&
+		    $line !~ /^\s*(?:Link|Patchwork|http|https|BugLink|base-commit):/i &&
 		    $line !~ /^This reverts commit [0-9a-f]{7,40}/ &&
 		    ($line =~ /\bcommit\s+[0-9a-f]{5,}\b/i ||
 		     ($line =~ /(?:\s|^)[0-9a-f]{12,40}(?:[\s"'\(\[]|$)/i &&
