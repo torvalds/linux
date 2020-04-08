4902f7fcb3bcb4ce088db97bfd194401a784cc60
Yihao Wu <wuyihao@linux.alibaba.com>
2020-04-08 18:11:36 +0800

    cpuidle-haltpoll: Fix small typo
    Fix a spelling typo in cpuidle-haltpoll.c.
    
    Signed-off-by: Yihao Wu <wuyihao@linux.alibaba.com>
    [ rjw: Subject & changelog ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpuidle/cpuidle-haltpoll.c
+++ drivers/cpuidle/cpuidle-haltpoll.c
@@ -94,7 +94,7 @@ static void haltpoll_uninit(void)
 	haltpoll_cpuidle_devices = NULL;
 }
 
-static bool haltpool_want(void)
+static bool haltpoll_want(void)
 {
 	return kvm_para_has_hint(KVM_HINTS_REALTIME) || force;
 }
@@ -110,7 +110,7 @@ static int __init haltpoll_init(void)
 
 	cpuidle_poll_state_init(drv);
 
-	if (!kvm_para_available() || !haltpool_want())
+	if (!kvm_para_available() || !haltpoll_want())
 		return -ENODEV;
 
 	ret = cpuidle_register_driver(drv);
