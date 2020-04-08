fdc63ff0e49c588884992b4b2656345a5e878b32
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2020-04-08 21:13:10 +0300

    ftrace/x86: Fix trace event registration for syscalls without arguments
    The refactoring of SYSCALL_DEFINE0() macros removed the ABI stubs and
    simply defines __abi_sys_$NAME as alias of __do_sys_$NAME.
    
    As a result kallsyms_lookup() returns "__do_sys_$NAME" which does not match
    with the declared trace event name.
    
    See also commit 1c758a2202a6 ("tracing/x86: Update syscall trace events to
    handle new prefixed syscall func names").
    
    Add __do_sys_ to the valid prefixes which are checked in
    arch_syscall_match_sym_name().
    
    Fixes: d2b5de495ee9 ("x86/entry: Refactor SYSCALL_DEFINE0 macros")
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Link: https://lkml.kernel.org/r/158636958997.7900.16485049455470033557.stgit@buzz

--- arch/x86/include/asm/ftrace.h
+++ arch/x86/include/asm/ftrace.h
@@ -61,11 +61,12 @@ static inline bool arch_syscall_match_sym_name(const char *sym, const char *name
 {
 	/*
 	 * Compare the symbol name with the system call name. Skip the
-	 * "__x64_sys", "__ia32_sys" or simple "sys" prefix.
+	 * "__x64_sys", "__ia32_sys", "__do_sys" or simple "sys" prefix.
 	 */
 	return !strcmp(sym + 3, name + 3) ||
 		(!strncmp(sym, "__x64_", 6) && !strcmp(sym + 9, name + 3)) ||
-		(!strncmp(sym, "__ia32_", 7) && !strcmp(sym + 10, name + 3));
+		(!strncmp(sym, "__ia32_", 7) && !strcmp(sym + 10, name + 3)) ||
+		(!strncmp(sym, "__do_sys", 8) && !strcmp(sym + 8, name + 3));
 }
 
 #ifndef COMPILE_OFFSETS
