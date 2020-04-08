2f1ea39870c95aa9fff6a0b48757625a0b22f551
Manish Rangankar <mrangankar@marvell.com>
2020-04-07 23:43:27 -0700

    scsi: qedi: Remove additional char from boot target iqnname
    While parsing the iSCSI TLV data to MFW request, a newline was added to the
    firmware boot target iqnname string. Because of this, we were getting the
    following error even after the boot target was successfully logged in:
    
    "[qedi_get_protocol_tlv_data:1197]:1: Boot target not set"
    
    Remove the trailing newline.
    
    [mkp: clarified commit desc]
    
    Link: https://lore.kernel.org/r/20200408064332.19377-2-mrangankar@marvell.com
    Reviewed-by: Lee Duncan <lduncan@suse.com>
    Signed-off-by: Manish Rangankar <mrangankar@marvell.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/qedi/qedi_main.c
+++ drivers/scsi/qedi/qedi_main.c
@@ -921,7 +921,7 @@ static void qedi_get_boot_tgt_info(struct nvm_iscsi_block *block,
 	ipv6_en = !!(block->generic.ctrl_flags &
 		     NVM_ISCSI_CFG_GEN_IPV6_ENABLED);
 
-	snprintf(tgt->iscsi_name, sizeof(tgt->iscsi_name), "%s\n",
+	snprintf(tgt->iscsi_name, sizeof(tgt->iscsi_name), "%s",
 		 block->target[index].target_name.byte);
 
 	tgt->ipv6_en = ipv6_en;
