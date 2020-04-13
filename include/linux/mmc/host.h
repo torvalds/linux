6db96e5810e0a6a345b7d78549de7676ae5b2662
Baolin Wang <baolin.wang7@gmail.com>
2020-04-13 10:46:03 +0800

    mmc: host: Introduce the request_atomic() for the host
    The SD host controller can process one request in the atomic context if
    the card is nonremovable, which means we can submit next request in the
    irq hard handler when using the MMC host software queue to reduce the
    latency. Thus this patch adds a new API request_atomic() for the host
    controller, as well as adding support for host software queue to submit
    a request by the new request_atomic() API.
    
    Moreover there is an unusual case that the card is busy when trying to
    send a command, and we can not polling the card status in interrupt
    context by using request_atomic() to dispatch requests. Thus we should
    queue a work to try again in the non-atomic context in case the host
    releases the busy signal later.
    
    Suggested-by: Adrian Hunter <adrian.hunter@intel.com>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Baolin Wang <baolin.wang7@gmail.com>
    Link: https://lore.kernel.org/r/a344e27e506cb2329073cbd5cf65e15cc3cbeba9.1586744073.git.baolin.wang7@gmail.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -92,6 +92,9 @@ struct mmc_host_ops {
 			    int err);
 	void	(*pre_req)(struct mmc_host *host, struct mmc_request *req);
 	void	(*request)(struct mmc_host *host, struct mmc_request *req);
+	/* Submit one request to host in atomic context. */
+	int	(*request_atomic)(struct mmc_host *host,
+				  struct mmc_request *req);
 
 	/*
 	 * Avoid calling the next three functions too often or in a "fast
