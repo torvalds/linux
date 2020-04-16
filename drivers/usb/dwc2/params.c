d67f2d749f80a4784700ba57fd5711e2666ae517
William Wu <william.wu@rock-chips.com>
2020-04-16 11:22:49 +0800

    usb: dwc2: use buffer dma for device mode on Rockchip SoCs
    The DWC2 driver auto detects the hardware and enable
    the gadget descriptor DMA if the DWC2 IP supports it.
    However, the gadget descriptor DMA has some unexpected
    compatibility issues, so we use buffer DMA instead of
    desc DMA.
    
    Change-Id: I1fed77f7d9bec1e0916b44d80813fb6248d461f0
    Signed-off-by: William Wu <william.wu@rock-chips.com>
    Signed-off-by: Frank Wang <frank.wang@rock-chips.com>

--- drivers/usb/dwc2/params.c
+++ drivers/usb/dwc2/params.c
@@ -91,6 +91,7 @@ static void dwc2_set_rk_params(struct dwc2_hsotg *hsotg)
 		GAHBCFG_HBSTLEN_SHIFT;
 	p->power_down = DWC2_POWER_DOWN_PARAM_NONE;
 	p->lpm = false;
+	p->g_dma_desc = false;
 }
 
 static void dwc2_set_ltq_params(struct dwc2_hsotg *hsotg)
