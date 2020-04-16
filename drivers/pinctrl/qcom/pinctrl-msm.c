3b0d6a555d229b5a0956d9f301612fd78a891b7e
John Stultz <john.stultz@linaro.org>
2020-04-16 05:37:43 +0000

    ANDROID: pinctrl: qcom: Allow pinctrl-msm code to be loadable as a module
    Tweaks to allow pinctrl-msm code to be loadable as a module.
    This is needed in order to support having the qcom-scm driver,
    which pinctrl-msm calls into, configured as a module.
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Bug: 153049053
    Change-Id: I11a6e20b66456cecfecc0610f3746047d33053e1

--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -1331,3 +1331,6 @@ int msm_pinctrl_remove(struct platform_device *pdev)
 }
 EXPORT_SYMBOL(msm_pinctrl_remove);
 
+MODULE_DESCRIPTION("Qualcomm Technologies, Inc. pinctrl-msm driver");
+MODULE_LICENSE("GPL v2");
+
