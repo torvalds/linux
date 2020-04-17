a7df11520997e8c67e28675e3170ae7e92e4a165
Lorenzo Bianconi <lorenzo@kernel.org>
2020-04-17 13:10:05 +0200

    mt76: remove PS_NULLFUNC_STACK capability
    remove IEEE80211_HW_PS_NULLFUNC_STACK capability from mt76_phy_init
    routine since 802.11 ps is not currently supported by any devices and it
    will conflict with mt7663 ps fw support
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mac80211.c
+++ drivers/net/wireless/mediatek/mt76/mac80211.c
@@ -294,7 +294,6 @@ mt76_phy_init(struct mt76_dev *dev, struct ieee80211_hw *hw)
 		hw->max_tx_fragments = 16;
 
 	ieee80211_hw_set(hw, SIGNAL_DBM);
-	ieee80211_hw_set(hw, PS_NULLFUNC_STACK);
 	ieee80211_hw_set(hw, AMPDU_AGGREGATION);
 	ieee80211_hw_set(hw, SUPPORTS_RC_TABLE);
 	ieee80211_hw_set(hw, SUPPORT_FAST_XMIT);
