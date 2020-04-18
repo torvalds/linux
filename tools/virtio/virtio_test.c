4cfb93935337d8f9680d865b0987583e462cfcc7
Eugenio Pérez <eperezma@redhat.com>
2020-04-18 12:22:14 +0200

    tools/virtio: Use __vring_new_virtqueue in virtio_test.c
    As updated in ("2a2d1382fe9d virtio: Add improved queue allocation API")
    
    Signed-off-by: Eugenio Pérez <eperezma@redhat.com>
    Link: https://lore.kernel.org/r/20200418102217.32327-6-eperezma@redhat.com
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tools/virtio/virtio_test.c
+++ tools/virtio/virtio_test.c
@@ -105,10 +105,9 @@ static void vq_info_add(struct vdev_info *dev, int num)
 	assert(r >= 0);
 	memset(info->ring, 0, vring_size(num, 4096));
 	vring_init(&info->vring, num, info->ring, 4096);
-	info->vq = vring_new_virtqueue(info->idx,
-				       info->vring.num, 4096, &dev->vdev,
-				       true, false, info->ring,
-				       vq_notify, vq_callback, "test");
+	info->vq =
+		__vring_new_virtqueue(info->idx, info->vring, &dev->vdev, true,
+				      false, vq_notify, vq_callback, "test");
 	assert(info->vq);
 	info->vq->priv = info;
 	vhost_vq_setup(dev, info);
