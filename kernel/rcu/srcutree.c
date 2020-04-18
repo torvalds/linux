7fef6cff8f2814bf8eb632e2bb8f0a987ffd9ece
Ethon Paul <ethp@qq.com>
2020-04-18 19:46:47 +0800

    srcu: Fix a typo in comment "amoritized"->"amortized"
    This commit fixes a typo in a comment.
    
    Signed-off-by: Ethon Paul <ethp@qq.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/srcutree.c
+++ kernel/rcu/srcutree.c
@@ -766,7 +766,7 @@ static void srcu_flip(struct srcu_struct *ssp)
  * it, if this function was preempted for enough time for the counters
  * to wrap, it really doesn't matter whether or not we expedite the grace
  * period.  The extra overhead of a needlessly expedited grace period is
- * negligible when amoritized over that time period, and the extra latency
+ * negligible when amortized over that time period, and the extra latency
  * of a needlessly non-expedited grace period is similarly negligible.
  */
 static bool srcu_might_be_idle(struct srcu_struct *ssp)
