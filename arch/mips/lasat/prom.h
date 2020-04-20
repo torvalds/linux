10760dde9be317a1abb426b2db9d6a698086cac9
Thomas Bogendoerfer <tsbogend@alpha.franken.de>
2020-04-20 14:22:29 +0200

    MIPS: Remove support for LASAT
    All LASAT has probably gone bad, so let's remove Linux support.
    
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/lasat/prom.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef __PROM_H
-#define __PROM_H
-
-extern void (*prom_display)(const char *string, int pos, int clear);
-extern void (*prom_monitor)(void);
-
-#endif /* __PROM_H */
