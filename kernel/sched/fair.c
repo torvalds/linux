17c891ab349138e8d8a59ca2700f42ce8af96f4e
Muchun Song <songmuchun@bytedance.com>
2020-04-21 22:41:23 +0800

    sched/fair: Use __this_cpu_read() in wake_wide()
    The code is executed with preemption(and interrupts) disabled,
    so it's safe to use __this_cpu_write().
    
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20200421144123.33580-1-songmuchun@bytedance.com

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5718,7 +5718,7 @@ static int wake_wide(struct task_struct *p)
 {
 	unsigned int master = current->wakee_flips;
 	unsigned int slave = p->wakee_flips;
-	int factor = this_cpu_read(sd_llc_size);
+	int factor = __this_cpu_read(sd_llc_size);
 
 	if (master < slave)
 		swap(master, slave);
