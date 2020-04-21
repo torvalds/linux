bdf1b69b38fb6da64f08ba60287fe184af5aa8ff
Wu Liangqing <wlq@rock-chips.com>
2020-04-21 18:10:18 +0800

    mmc: dw_mmc-rockchip: set xfer timer 100ms
    Fixes: 1d1a1f03f3a7 ("mmc: dw_mmc: add xfer timer for avoid DTO without actual data payload")
    Signed-off-by: Wu Liangqing <wlq@rock-chips.com>
    Signed-off-by: Shawn Lin <shawn.lin@rock-chips.com>
    Change-Id: I00ae095cff35b3960e1f9189d03f9908fed0aa33

--- drivers/mmc/host/dw_mmc.c
+++ drivers/mmc/host/dw_mmc.c
@@ -1949,7 +1949,7 @@ static void dw_mci_set_xfer_timeout(struct dw_mci *host)
 				   host->bus_hz);
 
 	/* add a bit spare time */
-	xfer_ms += 10;
+	xfer_ms += 100;
 
 	spin_lock_irqsave(&host->irq_lock, irqflags);
 	if (!test_bit(EVENT_XFER_COMPLETE, &host->pending_events))
