190070ae4c87f68f3d901c9c28985b3a1f9843af
Jason Yan <yanaijie@huawei.com>
2020-04-22 15:19:03 +0800

    omapfb/dss: fix comparison to bool warning
    Fix the following coccicheck warning:
    
    drivers/video/fbdev/omap2/omapfb/dss/hdmi4.c:461:15-32: WARNING:
    Comparison to bool
    drivers/video/fbdev/omap2/omapfb/dss/dispc.c:891:5-35: WARNING:
    Comparison of 0/1 to bool variable
    
    Signed-off-by: Jason Yan <yanaijie@huawei.com>
    Reviewed-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Cc: Andrew F. Davis <afd@ti.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200422071903.637-1-yanaijie@huawei.com

--- drivers/video/fbdev/omap2/omapfb/dss/dispc.c
+++ drivers/video/fbdev/omap2/omapfb/dss/dispc.c
@@ -888,7 +888,7 @@ static void dispc_ovl_set_color_mode(enum omap_plane plane,
 static void dispc_ovl_configure_burst_type(enum omap_plane plane,
 		enum omap_dss_rotation_type rotation_type)
 {
-	if (dss_has_feature(FEAT_BURST_2D) == 0)
+	if (!dss_has_feature(FEAT_BURST_2D))
 		return;
 
 	if (rotation_type == OMAP_DSS_ROT_TILER)
