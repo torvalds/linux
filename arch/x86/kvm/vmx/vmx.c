7ab0abdb553e62704be5b7879a0971acf9f36a06
Sean Christopherson <sean.j.christopherson@intel.com>
2020-04-22 19:25:50 -0700

    KVM: VMX: Use vmx_get_rflags() to query RFLAGS in vmx_interrupt_blocked()
    Use vmx_get_rflags() instead of manually reading vmcs.GUEST_RFLAGS when
    querying RFLAGS.IF so that multiple checks against interrupt blocking in
    a single run loop only require a single VMREAD.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Message-Id: <20200423022550.15113-14-sean.j.christopherson@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -4536,7 +4536,7 @@ bool vmx_interrupt_blocked(struct kvm_vcpu *vcpu)
 	if (is_guest_mode(vcpu) && nested_exit_on_intr(vcpu))
 		return false;
 
-	return !(vmcs_readl(GUEST_RFLAGS) & X86_EFLAGS_IF) ||
+	return !(vmx_get_rflags(vcpu) & X86_EFLAGS_IF) ||
 	       (vmcs_read32(GUEST_INTERRUPTIBILITY_INFO) &
 		(GUEST_INTR_STATE_STI | GUEST_INTR_STATE_MOV_SS));
 }
