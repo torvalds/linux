6a2782c0b2a0a2ed1d877d3a708308b36496950c
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2020-04-23 09:29:45 +0200

    media: atomisp: remove ISP version macros from sh_css_legacy.h
    This header is really version-independent. So, just get rid
    of the macros from it.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/staging/media/atomisp/pci/atomisp2/css2400/sh_css_legacy.h
+++ drivers/staging/media/atomisp/pci/atomisp2/css2400/sh_css_legacy.h
@@ -31,17 +31,9 @@ enum ia_css_pipe_id {
 	IA_CSS_PIPE_ID_VIDEO,
 	IA_CSS_PIPE_ID_CAPTURE,
 	IA_CSS_PIPE_ID_YUVPP,
-#ifndef ISP2401
 	IA_CSS_PIPE_ID_ACC,
 	IA_CSS_PIPE_ID_NUM
-#else
-	IA_CSS_PIPE_ID_ACC
-#endif
 };
-
-#ifdef ISP2401
-#define IA_CSS_PIPE_ID_NUM (IA_CSS_PIPE_ID_ACC + 1)
-#endif
 
 struct ia_css_pipe_extra_config {
 	bool enable_raw_binning;
@@ -71,9 +63,8 @@ enum ia_css_err
 sh_css_set_black_frame(struct ia_css_stream *stream,
 		       const struct ia_css_frame *raw_black_frame);
 
-#ifndef ISP2401
+/* ISP2400 */
 void
 sh_css_enable_cont_capt(bool enable, bool stop_copy_preview);
 
-#endif
 #endif /* _SH_CSS_LEGACY_H_ */
