69f57058badded5c523871bbaaaf60b637bcf623
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2020-04-23 16:45:02 +0300

    hyper-v: Use UUID API for exporting the GUID (part 2)
    This is a follow up to the commit 1d3c9c075462
      ("hyper-v: Use UUID API for exporting the GUID")
    which starts the conversion.
    
    There is export_guid() function which exports guid_t to the u8 array.
    Use it instead of open coding variant.
    
    This allows to hide the uuid_t internals.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Link: https://lore.kernel.org/r/20200423134505.78221-1-andriy.shevchenko@linux.intel.com
    Signed-off-by: Wei Liu <wei.liu@kernel.org>

--- drivers/hv/hv_trace.h
+++ drivers/hv/hv_trace.h
@@ -44,10 +44,8 @@ TRACE_EVENT(vmbus_onoffer,
 			   __entry->monitorid = offer->monitorid;
 			   __entry->is_ddc_int = offer->is_dedicated_interrupt;
 			   __entry->connection_id = offer->connection_id;
-			   memcpy(__entry->if_type,
-				  &offer->offer.if_type.b, 16);
-			   memcpy(__entry->if_instance,
-				  &offer->offer.if_instance.b, 16);
+			   export_guid(__entry->if_type, &offer->offer.if_type);
+			   export_guid(__entry->if_instance, &offer->offer.if_instance);
 			   __entry->chn_flags = offer->offer.chn_flags;
 			   __entry->mmio_mb = offer->offer.mmio_megabytes;
 			   __entry->sub_idx = offer->offer.sub_channel_index;
