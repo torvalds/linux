d339cd02b888eb8c4508fd772120782eac59a9fa
Huacai Chen <chenhc@lemote.com>
2020-04-24 18:56:45 +0800

    MIPS: Move unaligned load/store helpers to inst.h
    Move unaligned load/store helpers from unaligned.c to inst.h, then
    other parts of the kernel can use these helpers.
    
    Use __ASSEMBLY__ to guard the definition of "LONG" in asm.h to avoid
    build error on IPxx platforms.
    
    Signed-off-by: Huacai Chen <chenhc@lemote.com>
    Signed-off-by: Pei Huang <huangpei@loongson.cn>
    Reviewed-by: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/include/asm/asm.h
+++ arch/mips/include/asm/asm.h
@@ -202,7 +202,9 @@ symbol		=	value
 #define LONG_SRA	sra
 #define LONG_SRAV	srav
 
+#ifdef __ASSEMBLY__
 #define LONG		.word
+#endif
 #define LONGSIZE	4
 #define LONGMASK	3
 #define LONGLOG		2
@@ -225,7 +227,9 @@ symbol		=	value
 #define LONG_SRA	dsra
 #define LONG_SRAV	dsrav
 
+#ifdef __ASSEMBLY__
 #define LONG		.dword
+#endif
 #define LONGSIZE	8
 #define LONGMASK	7
 #define LONGLOG		3
