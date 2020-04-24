ded6ff15a1911af7dd641b4cc1a1a2e161f08e1f
Sergey Ryazanov <ryazanov.s.a@gmail.com>
2020-04-24 03:49:22 +0300

    ath9k: invalidate all calibrations at once
    Previously after the calibration validity period is over,
    calibrations are invalidated in a one at time manner. So, for AR9002
    family, which has three calibrations, the full recalibration interval
    becomes 3 x ATH_RESTART_CALINTERVAL. And each next calibration will be
    separated by the ATH_RESTART_CALINTERVAL time from a previous one.
    
    It seems like it is better to do whole recalibration at once. Also, this
    change makes the driver behaviour a little simpler. So, invalidate all
    calibrations at once at the end of the calibration validity interval.
    
    This change affects only AR9002 chips family, since the AR9003 utilize
    only a single calibration.
    
    Signed-off-by: Sergey Ryazanov <ryazanov.s.a@gmail.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200424004923.17129-6-ryazanov.s.a@gmail.com

--- drivers/net/wireless/ath/ath9k/calib.c
+++ drivers/net/wireless/ath/ath9k/calib.c
@@ -209,14 +209,17 @@ bool ath9k_hw_reset_calvalid(struct ath_hw *ah)
 		return true;
 	}
 
-	if (!(ah->supp_cals & currCal->calData->calType))
-		return true;
+	currCal = ah->cal_list;
+	do {
+		ath_dbg(common, CALIBRATE, "Resetting Cal %d state for channel %u\n",
+			currCal->calData->calType,
+			ah->curchan->chan->center_freq);
 
-	ath_dbg(common, CALIBRATE, "Resetting Cal %d state for channel %u\n",
-		currCal->calData->calType, ah->curchan->chan->center_freq);
+		ah->caldata->CalValid &= ~currCal->calData->calType;
+		currCal->calState = CAL_WAITING;
 
-	ah->caldata->CalValid &= ~currCal->calData->calType;
-	currCal->calState = CAL_WAITING;
+		currCal = currCal->calNext;
+	} while (currCal != ah->cal_list);
 
 	return false;
 }
