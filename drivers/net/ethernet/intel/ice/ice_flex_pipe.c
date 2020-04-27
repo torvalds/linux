f8d530ac29fe9248f5e58ca5bcf4c368f8393ccf
Wei Yongjun <weiyongjun1@huawei.com>
2020-04-27 12:12:28 +0000

    ice: Fix error return code in ice_add_prof()
    Fix to return a error code from the error handling case
    instead of 0, as done elsewhere in this function.
    
    Fixes: 31ad4e4ee1e4 ("ice: Allocate flow profile")
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/intel/ice/ice_flex_pipe.c
+++ drivers/net/ethernet/intel/ice/ice_flex_pipe.c
@@ -2962,8 +2962,10 @@ ice_add_prof(struct ice_hw *hw, enum ice_block blk, u64 id, u8 ptypes[],
 
 	/* add profile info */
 	prof = devm_kzalloc(ice_hw_to_dev(hw), sizeof(*prof), GFP_KERNEL);
-	if (!prof)
+	if (!prof) {
+		status = ICE_ERR_NO_MEMORY;
 		goto err_ice_add_prof;
+	}
 
 	prof->profile_cookie = id;
 	prof->prof_id = prof_id;
