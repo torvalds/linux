24ac99e97fa7b8f0db9b48413a76def9cf73295c
Michael Ellerman <mpe@ellerman.id.au>
2020-04-28 22:31:52 +1000

    powerpc: Drop unneeded cast in task_pt_regs()
    There's no need to cast in task_pt_regs() as tsk->thread.regs should
    already be a struct pt_regs. If someone's using task_pt_regs() on
    something that's not a task but happens to have a thread.regs then
    we'll deal with them later.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200428123152.73566-1-mpe@ellerman.id.au

--- arch/powerpc/include/asm/processor.h
+++ arch/powerpc/include/asm/processor.h
@@ -306,7 +306,7 @@ struct thread_struct {
 }
 #endif
 
-#define task_pt_regs(tsk)	((struct pt_regs *)(tsk)->thread.regs)
+#define task_pt_regs(tsk)	((tsk)->thread.regs)
 
 unsigned long get_wchan(struct task_struct *p);
 
