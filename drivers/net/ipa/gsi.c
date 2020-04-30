0b1ba18aec3e599c16cca7d99969282fb9db7e92
Alex Elder <elder@linaro.org>
2020-04-30 16:35:12 -0500

    net: ipa: zero return code before issuing generic EE command
    Zero the result code stored in a field of the scratch 0 register
    before issuing a generic EE command.  This just guarantees that
    the value we read later was actually written as a result of the
    command.
    
    Also add the definitions of two more possible result codes that can
    be returned when issuing flow control enable or disable commands:
      INCORRECT_CHANNEL_STATE: - channel must be in started state
      INCORRECT_DIRECTION - flow control is only valid for TX channels
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ipa/gsi.c
+++ drivers/net/ipa/gsi.c
@@ -1041,6 +1041,7 @@ static void gsi_isr_gp_int1(struct gsi *gsi)
 
 	complete(&gsi->completion);
 }
+
 /* Inter-EE interrupt handler */
 static void gsi_isr_glob_ee(struct gsi *gsi)
 {
@@ -1493,6 +1494,12 @@ static int gsi_generic_command(struct gsi *gsi, u32 channel_id,
 	struct completion *completion = &gsi->completion;
 	u32 val;
 
+	/* First zero the result code field */
+	val = ioread32(gsi->virt + GSI_CNTXT_SCRATCH_0_OFFSET);
+	val &= ~GENERIC_EE_RESULT_FMASK;
+	iowrite32(val, gsi->virt + GSI_CNTXT_SCRATCH_0_OFFSET);
+
+	/* Now issue the command */
 	val = u32_encode_bits(opcode, GENERIC_OPCODE_FMASK);
 	val |= u32_encode_bits(channel_id, GENERIC_CHID_FMASK);
 	val |= u32_encode_bits(GSI_EE_MODEM, GENERIC_EE_FMASK);
