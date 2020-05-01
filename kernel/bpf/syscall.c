138c67677ff5ac0bce7131033c39d52a81e87a60
Andrii Nakryiko <andriin@fb.com>
2020-05-01 11:56:22 -0700

    bpf: Fix use-after-free of bpf_link when priming half-fails
    If bpf_link_prime() succeeds to allocate new anon file, but then fails to
    allocate ID for it, link priming is considered to be failed and user is
    supposed ot be able to directly kfree() bpf_link, because it was never exposed
    to user-space.
    
    But at that point file already keeps a pointer to bpf_link and will eventually
    call bpf_link_release(), so if bpf_link was kfree()'d by caller, that would
    lead to use-after-free.
    
    Fix this by first allocating ID and only then allocating file. Adding ID to
    link_idr is ok, because link at that point still doesn't have its ID set, so
    no user-space process can create a new FD for it.
    
    Fixes: a3b80e107894 ("bpf: Allocate ID for bpf_link")
    Reported-by: syzbot+39b64425f91b5aab714d@syzkaller.appspotmail.com
    Suggested-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/20200501185622.3088964-1-andriin@fb.com

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -2348,17 +2348,18 @@ int bpf_link_prime(struct bpf_link *link, struct bpf_link_primer *primer)
 	if (fd < 0)
 		return fd;
 
-	file = anon_inode_getfile("bpf_link", &bpf_link_fops, link, O_CLOEXEC);
-	if (IS_ERR(file)) {
-		put_unused_fd(fd);
-		return PTR_ERR(file);
-	}
 
 	id = bpf_link_alloc_id(link);
 	if (id < 0) {
 		put_unused_fd(fd);
-		fput(file);
 		return id;
+	}
+
+	file = anon_inode_getfile("bpf_link", &bpf_link_fops, link, O_CLOEXEC);
+	if (IS_ERR(file)) {
+		bpf_link_free_id(id);
+		put_unused_fd(fd);
+		return PTR_ERR(file);
 	}
 
 	primer->link = link;
