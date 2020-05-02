b865a7fed34294949928248be7ea56b1c6928bfb
Greg Kroah-Hartman <gregkh@google.com>
2020-05-02 09:41:26 +0200

    ANDROID: GKI: mm.h: add Android ABI padding to a structure
    Try to mitigate potential future driver core api changes by adding a
    padding to struct vm_operations_struct.
    
    Based on a change made to the RHEL/CENTOS 8 kernel.
    
    Bug: 151154716
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I78f84148ef4d3524bd6c5b78e53e06503a4ac3ae

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -26,6 +26,7 @@
 #include <linux/page_ref.h>
 #include <linux/memremap.h>
 #include <linux/overflow.h>
+#include <linux/android_kabi.h>
 
 struct mempolicy;
 struct anon_vma;
@@ -461,6 +462,11 @@ struct vm_operations_struct {
 	 */
 	struct page *(*find_special_page)(struct vm_area_struct *vma,
 					  unsigned long addr);
+
+	ANDROID_KABI_RESERVE(1);
+	ANDROID_KABI_RESERVE(2);
+	ANDROID_KABI_RESERVE(3);
+	ANDROID_KABI_RESERVE(4);
 };
 
 static inline void vma_init(struct vm_area_struct *vma, struct mm_struct *mm)
