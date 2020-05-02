c6adf0c7b9f5daa9959994c2876872724ff4a706
Greg Kroah-Hartman <gregkh@google.com>
2020-05-02 09:39:06 +0200

    ANDROID: GKI: ioport.h: add Android ABI padding to a structure
    Try to mitigate potential future driver core api changes by adding a
    padding to struct resource.
    
    Based on a change made to the RHEL/CENTOS 8 kernel.
    
    Bug: 151154716
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I5ff4401c540be7d095f1db27370281b7220af030

--- include/linux/ioport.h
+++ include/linux/ioport.h
@@ -13,6 +13,7 @@
 #include <linux/compiler.h>
 #include <linux/types.h>
 #include <linux/bits.h>
+#include <linux/android_kabi.h>
 /*
  * Resources are tree-like, allowing
  * nesting etc..
@@ -24,6 +25,11 @@ struct resource {
 	unsigned long flags;
 	unsigned long desc;
 	struct resource *parent, *sibling, *child;
+
+	ANDROID_KABI_RESERVE(1);
+	ANDROID_KABI_RESERVE(2);
+	ANDROID_KABI_RESERVE(3);
+	ANDROID_KABI_RESERVE(4);
 };
 
 /*
