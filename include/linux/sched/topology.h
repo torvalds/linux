c89d039e6bb7eac202f23fba5679097e3d930615
Greg Kroah-Hartman <gregkh@google.com>
2020-05-02 09:35:50 +0200

    ANDROID: GKI: sched: add Android ABI padding to some structures
    Try to mitigate potential future driver core api changes by adding a
    padding to struct user_struct and struct sched_domain.
    
    Based on a change made to the RHEL/CENTOS 8 kernel.
    
    Bug: 151154716
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Ie8f685122767b690a116193aefd8c5e3b6ef8f17

--- include/linux/sched/topology.h
+++ include/linux/sched/topology.h
@@ -3,6 +3,7 @@
 #define _LINUX_SCHED_TOPOLOGY_H
 
 #include <linux/topology.h>
+#include <linux/android_kabi.h>
 
 #include <linux/sched/idle.h>
 
@@ -143,6 +144,10 @@ struct sched_domain {
 	struct sched_domain_shared *shared;
 
 	unsigned int span_weight;
+
+	ANDROID_KABI_RESERVE(1);
+	ANDROID_KABI_RESERVE(2);
+
 	/*
 	 * Span of all CPUs in this domain.
 	 *
