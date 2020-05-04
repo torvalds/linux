d24224dea57108f96d13579f20206c339bb8e52f
Jason Yan <yanaijie@huawei.com>
2020-05-04 19:34:06 +0800

    dmaengine: qcom_hidma: use true,false for bool variable
    Fix the following coccicheck warning:
    
    drivers/dma/qcom/hidma.c:553:1-17: WARNING: Assignment of 0/1 to bool
    variable
    
    Signed-off-by: Jason Yan <yanaijie@huawei.com>
    Acked By: Sinan Kaya <okaya@kernel.org>
    Link: https://lore.kernel.org/r/20200504113406.41530-1-yanaijie@huawei.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/qcom/hidma.c
+++ drivers/dma/qcom/hidma.c
@@ -550,7 +550,7 @@ static void hidma_free_chan_resources(struct dma_chan *dmach)
 		kfree(mdesc);
 	}
 
-	mchan->allocated = 0;
+	mchan->allocated = false;
 	spin_unlock_irqrestore(&mchan->lock, irqflags);
 }
 
