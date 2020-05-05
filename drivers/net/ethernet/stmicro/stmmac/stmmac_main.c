d66e67bd4cc76626253bd51ee034bbbaede4e9ba
YueHaibing <yuehaibing@huawei.com>
2020-05-05 16:42:56 +0800

    net: stmmac: Remove unused inline function stmmac_rx_threshold_count
    There's no caller in-tree since
    commit 2af6106ae949 ("net: stmmac: Introducing support for Page Pool")
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -3543,15 +3543,6 @@ static void stmmac_rx_vlan(struct net_device *dev, struct sk_buff *skb)
 	}
 }
 
-
-static inline int stmmac_rx_threshold_count(struct stmmac_rx_queue *rx_q)
-{
-	if (rx_q->rx_zeroc_thresh < STMMAC_RX_THRESH)
-		return 0;
-
-	return 1;
-}
-
 /**
  * stmmac_rx_refill - refill used skb preallocated buffers
  * @priv: driver private structure
