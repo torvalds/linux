d4dd6c0a404a2ed3843b4b685d6990c4438a64c0
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2020-05-06 18:30:19 +0300

    spi: dw: Remove unused variable in CR0 configuring hooks
    After enabling new IP support in the driver couple of variables
    were left unused compiler is not happy about:
    
    .../spi-dw.c: In function ‘dw_spi_update_cr0’:
    .../spi-dw.c:264:17: warning: unused variable ‘dws’ [-Wunused-variable]
      264 |  struct dw_spi *dws = spi_controller_get_devdata(master);
          |                 ^~~
    .../spi-dw.c: In function ‘dw_spi_update_cr0_v1_01a’:
    .../spi-dw.c:285:17: warning: unused variable ‘dws’ [-Wunused-variable]
      285 |  struct dw_spi *dws = spi_controller_get_devdata(master);
          |                 ^~~
    
    Drop them for good.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Link: https://lore.kernel.org/r/20200506153025.21441-2-andriy.shevchenko@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-dw.c
+++ drivers/spi/spi-dw.c
@@ -261,7 +261,6 @@ static irqreturn_t dw_spi_irq(int irq, void *dev_id)
 u32 dw_spi_update_cr0(struct spi_controller *master, struct spi_device *spi,
 		      struct spi_transfer *transfer)
 {
-	struct dw_spi *dws = spi_controller_get_devdata(master);
 	struct chip_data *chip = spi_get_ctldata(spi);
 	u32 cr0;
 
@@ -282,7 +281,6 @@ u32 dw_spi_update_cr0_v1_01a(struct spi_controller *master,
 			     struct spi_device *spi,
 			     struct spi_transfer *transfer)
 {
-	struct dw_spi *dws = spi_controller_get_devdata(master);
 	struct chip_data *chip = spi_get_ctldata(spi);
 	u32 cr0;
 
