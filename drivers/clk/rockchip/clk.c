124c0977a0c8a7696c0c9fe93f675cf3cee2b368
Elaine Zhang <zhangqing@rock-chips.com>
2020-05-06 09:38:43 +0800

    clk: rockchip: add a COMPOSITE_DCLK clock-type
    The CLK_SET_RATE_PARENT flag make the parent clock and the child clk is 1:1.
    If the DCLK frequency is too low, the PLL frequency will be very
    low, which will affect the output waveform quality of PLL, and PLL
    locking may be abnormal, so add a new COMPOSITE_DCLK clock-type to handle
    that.
    
    Change-Id: If9bee9ebf157fcf034aed246b3aa1cff503ef9cf
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>

--- drivers/clk/rockchip/clk.c
+++ drivers/clk/rockchip/clk.c
@@ -588,6 +588,16 @@ void rockchip_clk_register_branches(struct rockchip_clk_provider *ctx,
 				list->div_width, list->div_flags,
 				ctx->reg_base, &ctx->lock);
 			break;
+		case branch_dclk_divider:
+			clk = rockchip_clk_register_dclk_branch(list->name,
+				list->parent_names, list->num_parents,
+				ctx->reg_base, list->muxdiv_offset, list->mux_shift,
+				list->mux_width, list->mux_flags,
+				list->div_offset, list->div_shift, list->div_width,
+				list->div_flags, list->div_table,
+				list->gate_offset, list->gate_shift,
+				list->gate_flags, flags, list->max_prate, &ctx->lock);
+			break;
 		}
 
 		/* none of the cases above matched */
