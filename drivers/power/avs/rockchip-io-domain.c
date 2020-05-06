6dd5f4c92c1eb16b2a0f2ca73563a59d3e6369db
Tao Huang <huangtao@rock-chips.com>
2020-05-06 16:00:10 +0800

    PM / AVS: rockchip-io: Call rockchip_iodomain_driver_init() early when CONFIG_ROCKCHIP_THUNDER_BOOT=y
    Change-Id: I68756377411299e3a4bacbae462ae18b8c31c072
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- drivers/power/avs/rockchip-io-domain.c
+++ drivers/power/avs/rockchip-io-domain.c
@@ -690,7 +690,21 @@ static struct platform_driver rockchip_iodomain_driver = {
 	},
 };
 
+#ifdef CONFIG_ROCKCHIP_THUNDER_BOOT
+static int __init rockchip_iodomain_driver_init(void)
+{
+	return platform_driver_register(&rockchip_iodomain_driver);
+}
+fs_initcall(rockchip_iodomain_driver_init);
+
+static void __exit rockchip_iodomain_driver_exit(void)
+{
+	platform_driver_unregister(&rockchip_iodomain_driver);
+}
+module_exit(rockchip_iodomain_driver_exit);
+#else
 module_platform_driver(rockchip_iodomain_driver);
+#endif
 
 MODULE_DESCRIPTION("Rockchip IO-domain driver");
 MODULE_AUTHOR("Heiko Stuebner <heiko@sntech.de>");
