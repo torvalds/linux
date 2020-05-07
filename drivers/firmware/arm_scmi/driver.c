5a897e3ab42942e0fff51738357b1e0f91690dcf
Sudeep Holla <sudeep.holla@arm.com>
2020-05-07 15:49:05 +0100

    firmware: arm_scmi: fix psci dependency
    When CONFIG_ARM_PSCI_FW is disabled but CONFIG_HAVE_ARM_SMCCC is enabled,
    arm-scmi runs into a link failure:
    
    arm-linux-gnueabi-ld: drivers/firmware/arm_scmi/smc.o: in function `smc_send_message':
    smc.c:(.text+0x200): undefined reference to `arm_smccc_1_1_get_conduit'
    
    Change from HAVE_ARM_SMCCC to ARM_PSCI_FW config dependency for now.
    We rely on PSCI bindings anyways for the conduit and this should be
    fine.
    
    Link: https://lore.kernel.org/r/20200507144905.11397-1-sudeep.holla@arm.com
    Fixes: 1dc6558062da ("firmware: arm_scmi: Add smc/hvc transport")
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>

--- drivers/firmware/arm_scmi/driver.c
+++ drivers/firmware/arm_scmi/driver.c
@@ -901,7 +901,7 @@ ATTRIBUTE_GROUPS(versions);
 /* Each compatible listed below must have descriptor associated with it */
 static const struct of_device_id scmi_of_match[] = {
 	{ .compatible = "arm,scmi", .data = &scmi_mailbox_desc },
-#ifdef CONFIG_HAVE_ARM_SMCCC
+#ifdef CONFIG_ARM_PSCI_FW
 	{ .compatible = "arm,scmi-smc", .data = &scmi_smc_desc},
 #endif
 	{ /* Sentinel */ },
