22e21e51ce755399fd42055a3f668ee4af370881
Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
2020-05-07 19:20:20 +0200

    PCI: brcmstb: Assert fundamental reset on initialization
    While preparing the driver for upstream this detail was missed.
    
    If not asserted during the initialization process, devices connected on
    the bus will not be made aware of the internal reset happening. This,
    potentially resulting in unexpected behavior.
    
    Link: https://lore.kernel.org/r/20200507172020.18000-1-nsaenzjulienne@suse.de
    Fixes: c0452137034b ("PCI: brcmstb: Add Broadcom STB PCIe host controller driver")
    Signed-off-by: Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Acked-by: Florian Fainelli <f.fainelli@gmail.com>

--- drivers/pci/controller/pcie-brcmstb.c
+++ drivers/pci/controller/pcie-brcmstb.c
@@ -697,6 +697,7 @@ static int brcm_pcie_setup(struct brcm_pcie *pcie)
 
 	/* Reset the bridge */
 	brcm_pcie_bridge_sw_init_set(pcie, 1);
+	brcm_pcie_perst_set(pcie, 1);
 
 	usleep_range(100, 200);
 
