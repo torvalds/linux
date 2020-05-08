746c6237ece641da79df09abcf87bc29f6f9665b
Xiongfeng Wang <wangxiongfeng2@huawei.com>
2020-05-08 09:32:59 +0800

    sunrpc: add missing newline when printing parameter 'pool_mode' by sysfs
    When I cat parameter '/sys/module/sunrpc/parameters/pool_mode', it
    displays as follows. It is better to add a newline for easy reading.
    
    [root@hulk-202 ~]# cat /sys/module/sunrpc/parameters/pool_mode
    global[root@hulk-202 ~]#
    
    Signed-off-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- net/sunrpc/svc.c
+++ net/sunrpc/svc.c
@@ -88,15 +88,15 @@ param_get_pool_mode(char *buf, const struct kernel_param *kp)
 	switch (*ip)
 	{
 	case SVC_POOL_AUTO:
-		return strlcpy(buf, "auto", 20);
+		return strlcpy(buf, "auto\n", 20);
 	case SVC_POOL_GLOBAL:
-		return strlcpy(buf, "global", 20);
+		return strlcpy(buf, "global\n", 20);
 	case SVC_POOL_PERCPU:
-		return strlcpy(buf, "percpu", 20);
+		return strlcpy(buf, "percpu\n", 20);
 	case SVC_POOL_PERNODE:
-		return strlcpy(buf, "pernode", 20);
+		return strlcpy(buf, "pernode\n", 20);
 	default:
-		return sprintf(buf, "%d", *ip);
+		return sprintf(buf, "%d\n", *ip);
 	}
 }
 
