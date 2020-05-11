79fc7a9fffa8f6166315df6929c0cef5c9a04580
Omer Shpigelman <oshpigelman@habana.ai>
2020-05-11 10:46:29 +0300

    habanalabs: add gaudi profiler module
    Add the GAUDI code to initialize the ASIC's profiler. The profile receives
    its initialization values from the user, same as in Goya, but the code to
    initialize is in the driver because the configuration space of the
    device is not directly exposed to the user.
    
    Signed-off-by: Omer Shpigelman <oshpigelman@habana.ai>
    Reviewed-by: Oded Gabbay <oded.gabbay@gmail.com>
    Signed-off-by: Oded Gabbay <oded.gabbay@gmail.com>

--- drivers/misc/habanalabs/gaudi/gaudiP.h
+++ drivers/misc/habanalabs/gaudi/gaudiP.h
@@ -252,6 +252,8 @@ void gaudi_init_security(struct hl_device *hdev);
 void gaudi_add_device_attr(struct hl_device *hdev,
 			struct attribute_group *dev_attr_grp);
 void gaudi_set_pll_profile(struct hl_device *hdev, enum hl_pll_frequency freq);
+int gaudi_debug_coresight(struct hl_device *hdev, void *data);
+void gaudi_halt_coresight(struct hl_device *hdev);
 int gaudi_get_clk_rate(struct hl_device *hdev, u32 *cur_clk, u32 *max_clk);
 
 #endif /* GAUDIP_H_ */
