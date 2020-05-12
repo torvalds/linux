b4f1874c62168159fdb419ced4afc77c1b51c475
Loïc Yhuel <loic.yhuel@gmail.com>
2020-05-12 06:01:13 +0200

    tpm: check event log version before reading final events
    This fixes the boot issues since 5.3 on several Dell models when the TPM
    is enabled. Depending on the exact grub binary, booting the kernel would
    freeze early, or just report an error parsing the final events log.
    
    We get an event log in the SHA-1 format, which doesn't have a
    tcg_efi_specid_event_head in the first event, and there is a final events
    table which doesn't match the crypto agile format.
    __calc_tpm2_event_size reads bad "count" and "efispecid->num_algs", and
    either fails, or loops long enough for the machine to be appear frozen.
    
    So we now only parse the final events table, which is per the spec always
    supposed to be in the crypto agile format, when we got a event log in this
    format.
    
    Fixes: c46f3405692de ("tpm: Reserve the TPM final events table")
    Fixes: 166a2809d65b2 ("tpm: Don't duplicate events from the final event log in the TCG2 log")
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1779611
    Signed-off-by: Loïc Yhuel <loic.yhuel@gmail.com>
    Link: https://lore.kernel.org/r/20200512040113.277768-1-loic.yhuel@gmail.com
    Reviewed-by: Javier Martinez Canillas <javierm@redhat.com>
    Reviewed-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Reviewed-by: Matthew Garrett <mjg59@google.com>
    [ardb: warn when final events table is missing or in the wrong format]
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- drivers/firmware/efi/tpm.c
+++ drivers/firmware/efi/tpm.c
@@ -62,8 +62,11 @@ int __init efi_tpm_eventlog_init(void)
 	tbl_size = sizeof(*log_tbl) + log_tbl->size;
 	memblock_reserve(efi.tpm_log, tbl_size);
 
-	if (efi.tpm_final_log == EFI_INVALID_TABLE_ADDR)
+	if (efi.tpm_final_log == EFI_INVALID_TABLE_ADDR ||
+	    log_tbl->version != EFI_TCG2_EVENT_LOG_FORMAT_TCG_2) {
+		pr_warn(FW_BUG "TPM Final Events table missing or invalid\n");
 		goto out;
+	}
 
 	final_tbl = early_memremap(efi.tpm_final_log, sizeof(*final_tbl));
 
