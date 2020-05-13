6955a76fbcd56d27c84c01353101048e366d070f
Christoph Hellwig <hch@lst.de>
2020-05-13 08:46:58 +0200

    bpfilter: switch to kernel_write
    While pipes don't really need sb_writers projection, __kernel_write is an
    interface better kept private, and the additional rw_verify_area does not
    hurt here.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- net/bpfilter/bpfilter_kern.c
+++ net/bpfilter/bpfilter_kern.c
@@ -50,7 +50,7 @@ static int __bpfilter_process_sockopt(struct sock *sk, int optname,
 	req.len = optlen;
 	if (!bpfilter_ops.info.pid)
 		goto out;
-	n = __kernel_write(bpfilter_ops.info.pipe_to_umh, &req, sizeof(req),
+	n = kernel_write(bpfilter_ops.info.pipe_to_umh, &req, sizeof(req),
 			   &pos);
 	if (n != sizeof(req)) {
 		pr_err("write fail %zd\n", n);
