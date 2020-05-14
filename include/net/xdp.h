34cc0b338a61de3eee3a2bfcaf4f9d6e9fae091a
Jesper Dangaard Brouer <brouer@redhat.com>
2020-05-14 12:49:33 +0200

    xdp: Xdp_frame add member frame_sz and handle in convert_to_xdp_frame
    Use hole in struct xdp_frame, when adding member frame_sz, which keeps
    same sizeof struct (32 bytes)
    
    Drivers ixgbe and sfc had bug cases where the necessary/expected
    tailroom was not reserved. This can lead to some hard to catch memory
    corruption issues. Having the drivers frame_sz this can be detected when
    packet length/end via xdp->data_end exceed the xdp_data_hard_end
    pointer, which accounts for the reserved the tailroom.
    
    When detecting this driver issue, simply fail the conversion with NULL,
    which results in feedback to driver (failing xdp_do_redirect()) causing
    driver to drop packet. Given the lack of consistent XDP stats, this can
    be hard to troubleshoot. And given this is a driver bug, we want to
    generate some more noise in form of a WARN stack dump (to ID the driver
    code that inlined convert_to_xdp_frame).
    
    Inlining the WARN macro is problematic, because it adds an asm
    instruction (on Intel CPUs ud2) what influence instruction cache
    prefetching. Thus, introduce xdp_warn and macro XDP_WARN, to avoid this
    and at the same time make identifying the function and line of this
    inlined function easier.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Link: https://lore.kernel.org/bpf/158945337313.97035.10015729316710496600.stgit@firesoul

--- include/net/xdp.h
+++ include/net/xdp.h
@@ -89,7 +89,8 @@ struct xdp_frame {
 	void *data;
 	u16 len;
 	u16 headroom;
-	u16 metasize;
+	u32 metasize:8;
+	u32 frame_sz:24;
 	/* Lifetime of xdp_rxq_info is limited to NAPI/enqueue time,
 	 * while mem info is valid on remote CPU.
 	 */
@@ -103,6 +104,10 @@ static inline void xdp_scrub_frame(struct xdp_frame *frame)
 	frame->data = NULL;
 	frame->dev_rx = NULL;
 }
+
+/* Avoids inlining WARN macro in fast-path */
+void xdp_warn(const char *msg, const char *func, const int line);
+#define XDP_WARN(msg) xdp_warn(msg, __func__, __LINE__)
 
 struct xdp_frame *xdp_convert_zc_to_xdp_frame(struct xdp_buff *xdp);
 
@@ -124,6 +129,12 @@ struct xdp_frame *convert_to_xdp_frame(struct xdp_buff *xdp)
 	if (unlikely((headroom - metasize) < sizeof(*xdp_frame)))
 		return NULL;
 
+	/* Catch if driver didn't reserve tailroom for skb_shared_info */
+	if (unlikely(xdp->data_end > xdp_data_hard_end(xdp))) {
+		XDP_WARN("Driver BUG: missing reserved tailroom");
+		return NULL;
+	}
+
 	/* Store info in top of packet */
 	xdp_frame = xdp->data_hard_start;
 
@@ -131,6 +142,7 @@ struct xdp_frame *convert_to_xdp_frame(struct xdp_buff *xdp)
 	xdp_frame->len  = xdp->data_end - xdp->data;
 	xdp_frame->headroom = headroom - sizeof(*xdp_frame);
 	xdp_frame->metasize = metasize;
+	xdp_frame->frame_sz = xdp->frame_sz;
 
 	/* rxq only valid until napi_schedule ends, convert to xdp_mem_info */
 	xdp_frame->mem = xdp->rxq->mem;
