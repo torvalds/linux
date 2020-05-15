9f369f7e4660d05b5318aa413db199a70dfb2c4f
Marcin Rajwa <marcin.rajwa@linux.intel.com>
2020-05-15 16:59:53 +0300

    ASoC: SOF: inform DSP that driver is going to be removed
    This patch invokes the DSP power down notifier to inform
    DSP that driver is going to be removed.
    The module removal entails DSP power down, disabling of
    IRQs and more. Therefore it is highly recommended to inform
    the DSP about this upcoming event.
    
    Due to hardware limitations on some Intel platforms it is necessary
    to power gate all LPSRAM banks that were enabled prior to controller
    reset. Otherwise, an attempt to write LPSRAM control registers may
    have no effect.
    
    Signed-off-by: Marcin Rajwa <marcin.rajwa@linux.intel.com>
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    Reviewed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Link: https://lore.kernel.org/r/20200515135958.17511-4-kai.vehmanen@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sof/core.c
+++ sound/soc/sof/core.c
@@ -343,6 +343,12 @@ int snd_sof_device_remove(struct device *dev)
 {
 	struct snd_sof_dev *sdev = dev_get_drvdata(dev);
 	struct snd_sof_pdata *pdata = sdev->pdata;
+	int ret;
+
+	ret = snd_sof_dsp_power_down_notify(sdev);
+	if (ret < 0)
+		dev_warn(dev, "error: %d failed to prepare DSP for device removal",
+			 ret);
 
 	if (IS_ENABLED(CONFIG_SND_SOC_SOF_PROBE_WORK_QUEUE))
 		cancel_work_sync(&sdev->probe_work);
