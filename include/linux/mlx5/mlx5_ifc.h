356d411c26735bcc62718c4c9181014255dc302d
Raed Salem <raeds@mellanox.com>
2020-05-15 15:16:52 -0700

    net/mlx5: Cleanup mlx5_ifc_fte_match_set_misc2_bits
    Remove the "metadata_reg_b" field and all uses of this field in code
    to match the device specification. As this field is not in use in SW
    steering it is safe to remove it.
    
    Signed-off-by: Raed Salem <raeds@mellanox.com>
    Reviewed-by: Alex Vesker <valex@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- include/linux/mlx5/mlx5_ifc.h
+++ include/linux/mlx5/mlx5_ifc.h
@@ -584,9 +584,7 @@ struct mlx5_ifc_fte_match_set_misc2_bits {
 
 	u8         metadata_reg_a[0x20];
 
-	u8         metadata_reg_b[0x20];
-
-	u8         reserved_at_1c0[0x40];
+	u8         reserved_at_1a0[0x60];
 };
 
 struct mlx5_ifc_fte_match_set_misc3_bits {
