13f15b59ad70549545272cc8f0bf4d68a8ef9c14
Heiner Kallweit <hkallweit1@gmail.com>
2020-05-17 00:05:08 +0200

    r8169: remove remaining call to mdiobus_unregister
    After having switched to devm_mdiobus_register() also this remaining
    call to mdiobus_unregister() can be removed.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/realtek/r8169_main.c
+++ drivers/net/ethernet/realtek/r8169_main.c
@@ -4990,7 +4990,6 @@ static void rtl_remove_one(struct pci_dev *pdev)
 	netif_napi_del(&tp->napi);
 
 	unregister_netdev(dev);
-	mdiobus_unregister(tp->phydev->mdio.bus);
 
 	rtl_release_firmware(tp);
 
