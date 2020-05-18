e80884e89ed30c7543320c27f300d2efaf43a9c8
Sudeep Holla <sudeep.holla@arm.com>
2020-05-18 10:12:19 +0100

    BACKPORT: firmware: smccc: Drop smccc_version enum and use ARM_SMCCC_VERSION_1_x instead
    Instead of maintaining 2 sets of enums/macros for tracking SMCCC version,
    let us drop smccc_version enum and use ARM_SMCCC_VERSION_1_x directly
    instead.
    
    This is in preparation to drop smccc_version here and move it separately
    under drivers/firmware/smccc.
    
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>
    Tested-by: Etienne Carriere <etienne.carriere@st.com>
    Reviewed-by: Steven Price <steven.price@arm.com>
    Reviewed-by: Etienne Carriere <etienne.carriere@st.com>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Link: https://lore.kernel.org/r/20200518091222.27467-5-sudeep.holla@arm.com
    Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit ad5a57dfe434b02ab28852703d7ad5510998ccef)
    
    Change-Id: I8b3625d3adbaea003c1dadcbc028c62a2f4f59ba
    Signed-off-by: XiaoDong Huang <derrick.huang@rock-chips.com>

--- include/linux/psci.h
+++ include/linux/psci.h
@@ -31,11 +31,6 @@ enum psci_conduit {
 	PSCI_CONDUIT_HVC,
 };
 
-enum smccc_version {
-	SMCCC_VERSION_1_0,
-	SMCCC_VERSION_1_1,
-};
-
 struct psci_operations {
 	u32 (*get_version)(void);
 	int (*cpu_suspend)(u32 state, unsigned long entry_point);
@@ -46,7 +41,7 @@ struct psci_operations {
 			unsigned long lowest_affinity_level);
 	int (*migrate_info_type)(void);
 	enum psci_conduit conduit;
-	enum smccc_version smccc_version;
+	u32 smccc_version;
 };
 
 extern struct psci_operations psci_ops;
