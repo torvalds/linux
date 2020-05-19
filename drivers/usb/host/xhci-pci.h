78ef1b1ea193d1b977864f8c20968fcf4f1dbff4
Vinod Koul <vkoul@kernel.org>
2020-05-19 15:00:02 +0530

    usb: xhci: make symbols static
    When renesas module is not built, we get compiler warning on xhci driver
    with W=1
    
      CC [M]  drivers/usb/host/xhci-rcar.o
    drivers/usb/host/xhci-pci.h:13:5: warning: no previous prototype for ‘renesas_xhci_check_request_fw’ [-Wmissing-prototypes]
     int renesas_xhci_check_request_fw(struct pci_dev *dev,
         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    drivers/usb/host/xhci-pci.h:19:6: warning: no previous prototype for ‘renesas_xhci_pci_exit’ [-Wmissing-prototypes]
     void renesas_xhci_pci_exit(struct pci_dev *dev) { };
          ^~~~~~~~~~~~~~~~~~~~~
    
    We have defined these symbols when CONFIG_USB_XHCI_PCI_RENESAS is not
    defined, but missed making then static.
    
    Reported-by: kbuild test robot <lkp@intel.com>
    Fixes: 8bd5741e3145 ("usb: renesas-xhci: Add the renesas xhci driver")
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Link: https://lore.kernel.org/r/20200519093002.1152144-1-vkoul@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/xhci-pci.h
+++ drivers/usb/host/xhci-pci.h
@@ -10,13 +10,13 @@ int renesas_xhci_check_request_fw(struct pci_dev *dev,
 void renesas_xhci_pci_exit(struct pci_dev *dev);
 
 #else
-int renesas_xhci_check_request_fw(struct pci_dev *dev,
-				  const struct pci_device_id *id)
+static int renesas_xhci_check_request_fw(struct pci_dev *dev,
+					 const struct pci_device_id *id)
 {
 	return 0;
 }
 
-void renesas_xhci_pci_exit(struct pci_dev *dev) { };
+static void renesas_xhci_pci_exit(struct pci_dev *dev) { };
 
 #endif
 
