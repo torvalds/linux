9f46753616b63b2749f5dad9c3d2e695c907cdea
Jaegeuk Kim <jaegeuk@google.com>
2020-05-19 15:37:19 -0700

    ANDROID: GKI: support mm_event for FS/IO/UFS path
    This is stub for FS/IO/UFS part in mm_event.
    
    Bug: 149182139
    Change-Id: Iae01a7d7c27224f2d7dea287f1d713103d8c7398
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>

--- include/linux/blk_types.h
+++ include/linux/blk_types.h
@@ -212,6 +212,8 @@ struct bio {
 
 	struct bio_set		*bi_pool;
 
+	ktime_t bi_alloc_ts;			/* for mm_event */
+
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
 
--- include/linux/mm_event.h
+++ include/linux/mm_event.h
@@ -10,7 +10,12 @@ enum mm_event_type {
 	MM_RECLAIM = 4,
 	MM_SWP_FAULT = 5,
 	MM_KERN_ALLOC = 6,
-	MM_TYPE_NUM = 7,
+	BLK_READ_SUBMIT_BIO = 7,
+	UFS_READ_QUEUE_CMD = 8,
+	UFS_READ_SEND_CMD = 9,
+	UFS_READ_COMPL_CMD = 10,
+	F2FS_READ_DATA = 11,
+	MM_TYPE_NUM = 12,
 };
 
 struct mm_event_task {
