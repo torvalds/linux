d9f2a1af817d5c9ee5a6adf70c5a217a583d3af0
Miquel Raynal <miquel.raynal@bootlin.com>
2020-05-19 15:00:32 +0200

    mtd: rawnand: vf610: Stop using nand_release()
    This helper is not very useful and very often people get confused:
    they use nand_release() instead of nand_cleanup().
    
    Let's stop using nand_release() by calling mtd_device_unregister() and
    nand_cleanup() directly.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Cc: Stefan Agner <stefan@agner.ch>
    Link: https://lore.kernel.org/linux-mtd/20200519130035.1883-60-miquel.raynal@bootlin.com

--- drivers/mtd/nand/raw/vf610_nfc.c
+++ drivers/mtd/nand/raw/vf610_nfc.c
@@ -917,8 +917,12 @@ err_disable_clk:
 static int vf610_nfc_remove(struct platform_device *pdev)
 {
 	struct vf610_nfc *nfc = platform_get_drvdata(pdev);
+	struct nand_chip *chip = &nfc->chip;
+	int ret;
 
-	nand_release(&nfc->chip);
+	ret = mtd_device_unregister(nand_to_mtd(chip));
+	WARN_ON(ret);
+	nand_cleanup(chip);
 	clk_disable_unprepare(nfc->clk);
 	return 0;
 }
