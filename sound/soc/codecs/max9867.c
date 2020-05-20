d0de8c69f90196fb872f1739756bb0348144d18c
Pavel Dobias <dobias@2n.cz>
2020-05-20 09:19:00 +0200

    ASoC: max9867: don't use regmap defaults
    MAX9867 codec doesn't have reset pin, so the registers don't
    have default values in the case of reboot without powering off.
    Remove the reg_defaults struct and let the regmap read initial
    state of the registers at startup instead of taking them from
    reg_defaults struct.
    
    Signed-off-by: Pavel Dobias <dobias@2n.cz>
    Link: https://lore.kernel.org/r/20200520071904.15801-2-dobias@2n.cz
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/max9867.c
+++ sound/soc/codecs/max9867.c
@@ -463,35 +463,10 @@ static bool max9867_volatile_register(struct device *dev, unsigned int reg)
 	}
 }
 
-static const struct reg_default max9867_reg[] = {
-	{ 0x04, 0x00 },
-	{ 0x05, 0x00 },
-	{ 0x06, 0x00 },
-	{ 0x07, 0x00 },
-	{ 0x08, 0x00 },
-	{ 0x09, 0x00 },
-	{ 0x0A, 0x00 },
-	{ 0x0B, 0x00 },
-	{ 0x0C, 0x00 },
-	{ 0x0D, 0x00 },
-	{ 0x0E, 0x40 },
-	{ 0x0F, 0x40 },
-	{ 0x10, 0x00 },
-	{ 0x11, 0x00 },
-	{ 0x12, 0x00 },
-	{ 0x13, 0x00 },
-	{ 0x14, 0x00 },
-	{ 0x15, 0x00 },
-	{ 0x16, 0x00 },
-	{ 0x17, 0x00 },
-};
-
 static const struct regmap_config max9867_regmap = {
 	.reg_bits	= 8,
 	.val_bits	= 8,
 	.max_register	= MAX9867_REVISION,
-	.reg_defaults	= max9867_reg,
-	.num_reg_defaults = ARRAY_SIZE(max9867_reg),
 	.volatile_reg	= max9867_volatile_register,
 	.cache_type	= REGCACHE_RBTREE,
 };
