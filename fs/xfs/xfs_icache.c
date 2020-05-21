43d24bcf19d139b904752b5727def241920cff37
Darrick J. Wong <darrick.wong@oracle.com>
2020-05-21 13:08:47 -0700

    xfs: remove unused xfs_inode_ag_iterator function
    Not used by anyone, so get rid of it.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Brian Foster <bfoster@redhat.com>

--- fs/xfs/xfs_icache.c
+++ fs/xfs/xfs_icache.c
@@ -1006,17 +1006,6 @@ xfs_inode_ag_iterator_flags(
 }
 
 int
-xfs_inode_ag_iterator(
-	struct xfs_mount	*mp,
-	int			(*execute)(struct xfs_inode *ip, int flags,
-					   void *args),
-	int			flags,
-	void			*args)
-{
-	return xfs_inode_ag_iterator_flags(mp, execute, flags, args, 0);
-}
-
-int
 xfs_inode_ag_iterator_tag(
 	struct xfs_mount	*mp,
 	int			(*execute)(struct xfs_inode *ip, int flags,
