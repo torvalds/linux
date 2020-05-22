178c72350d39d69448ac7b0e5be2dfbaef6d6d6d
Thomas Zimmermann <tzimmermann@suse.de>
2020-05-22 15:52:29 +0200

    drm/aspeed: Set driver CMA functions with DRM_GEM_CMA_DRIVER_OPS
    DRM_GEM_CMA_DRIVER_OPS sets the functions in struct drm_driver
    to their defaults. No functional changes are made.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Joel Stanley <joel@jms.id.au>
    Acked-by: Emil Velikov <emil.velikov@collabora.com>
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200603083132.4610-5-tzimmermann@suse.de

--- drivers/gpu/drm/aspeed/aspeed_gfx_drv.c
+++ drivers/gpu/drm/aspeed/aspeed_gfx_drv.c
@@ -193,12 +193,7 @@ DEFINE_DRM_GEM_CMA_FOPS(fops);
 
 static struct drm_driver aspeed_gfx_driver = {
 	.driver_features        = DRIVER_GEM | DRIVER_MODESET | DRIVER_ATOMIC,
-	.gem_create_object	= drm_gem_cma_create_object_default_funcs,
-	.dumb_create		= drm_gem_cma_dumb_create,
-	.prime_handle_to_fd	= drm_gem_prime_handle_to_fd,
-	.prime_fd_to_handle	= drm_gem_prime_fd_to_handle,
-	.gem_prime_import_sg_table = drm_gem_cma_prime_import_sg_table,
-	.gem_prime_mmap		= drm_gem_prime_mmap,
+	DRM_GEM_CMA_DRIVER_OPS,
 	.fops = &fops,
 	.name = "aspeed-gfx-drm",
 	.desc = "ASPEED GFX DRM",
