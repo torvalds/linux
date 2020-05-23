316107119f473e764cf5e50437333c8b83bec0da
Heiner Kallweit <hkallweit1@gmail.com>
2020-05-23 17:40:25 +0200

    ethtool: propagate get_coalesce return value
    get_coalesce returns 0 or ERRNO, but the return value isn't checked.
    The returned coalesce data may be invalid if an ERRNO is set,
    therefore better check and propagate the return value.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ethtool/ioctl.c
+++ net/ethtool/ioctl.c
@@ -1510,11 +1510,14 @@ static noinline_for_stack int ethtool_get_coalesce(struct net_device *dev,
 						   void __user *useraddr)
 {
 	struct ethtool_coalesce coalesce = { .cmd = ETHTOOL_GCOALESCE };
+	int ret;
 
 	if (!dev->ethtool_ops->get_coalesce)
 		return -EOPNOTSUPP;
 
-	dev->ethtool_ops->get_coalesce(dev, &coalesce);
+	ret = dev->ethtool_ops->get_coalesce(dev, &coalesce);
+	if (ret)
+		return ret;
 
 	if (copy_to_user(useraddr, &coalesce, sizeof(coalesce)))
 		return -EFAULT;
