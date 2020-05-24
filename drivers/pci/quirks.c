a77e92f05b90d4e845900dcc63ee26c45f058a78
Kevin Buettner <kevinb@redhat.com>
2020-05-24 00:35:29 -0700

    PCI: Avoid FLR for AMD Starship USB 3.0
    [ Upstream commit 5727043c73fdfe04597971b5f3f4850d879c1f4f ]
    
    The AMD Starship USB 3.0 host controller advertises Function Level Reset
    support, but it apparently doesn't work.  Add a quirk to prevent use of FLR
    on this device.
    
    Without this quirk, when attempting to assign (pass through) an AMD
    Starship USB 3.0 host controller to a guest OS, the system becomes
    increasingly unresponsive over the course of several minutes, eventually
    requiring a hard reset.  Shortly after attempting to start the guest, I see
    these messages:
    
      vfio-pci 0000:05:00.3: not ready 1023ms after FLR; waiting
      vfio-pci 0000:05:00.3: not ready 2047ms after FLR; waiting
      vfio-pci 0000:05:00.3: not ready 4095ms after FLR; waiting
      vfio-pci 0000:05:00.3: not ready 8191ms after FLR; waiting
    
    And then eventually:
    
      vfio-pci 0000:05:00.3: not ready 65535ms after FLR; giving up
      INFO: NMI handler (perf_event_nmi_handler) took too long to run: 0.000 msecs
      perf: interrupt took too long (642744 > 2500), lowering kernel.perf_event_max_sample_rate to 1000
      INFO: NMI handler (perf_event_nmi_handler) took too long to run: 82.270 msecs
      INFO: NMI handler (perf_event_nmi_handler) took too long to run: 680.608 msecs
      INFO: NMI handler (perf_event_nmi_handler) took too long to run: 100.952 msecs
      ...
      watchdog: BUG: soft lockup - CPU#3 stuck for 22s! [qemu-system-x86:7487]
    
    Tested on a Micro-Star International Co., Ltd. MS-7C59/Creator TRX40
    motherboard with an AMD Ryzen Threadripper 3970X.
    
    Link: https://lore.kernel.org/r/20200524003529.598434ff@f31-4.lan
    Signed-off-by: Kevin Buettner <kevinb@redhat.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -4960,6 +4960,7 @@ DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_INTEL, 0x443, quirk_intel_qat_vf_cap);
  * FLR may cause the following to devices to hang:
  *
  * AMD Starship/Matisse HD Audio Controller 0x1487
+ * AMD Starship USB 3.0 Host Controller 0x148c
  * AMD Matisse USB 3.0 Host Controller 0x149c
  * Intel 82579LM Gigabit Ethernet Controller 0x1502
  * Intel 82579V Gigabit Ethernet Controller 0x1503
@@ -4970,6 +4971,7 @@ static void quirk_no_flr(struct pci_dev *dev)
 	dev->dev_flags |= PCI_DEV_FLAGS_NO_FLR_RESET;
 }
 DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_AMD, 0x1487, quirk_no_flr);
+DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_AMD, 0x148c, quirk_no_flr);
 DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_AMD, 0x149c, quirk_no_flr);
 DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_INTEL, 0x1502, quirk_no_flr);
 DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_INTEL, 0x1503, quirk_no_flr);
