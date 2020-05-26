11ea09b9e2ed0d9680a890f8fffa204dcb1a2654
Serge Semin <Sergey.Semin@baikalelectronics.ru>
2020-05-27 01:20:54 +0300

    dt-bindings: clk: Add Baikal-T1 CCU Dividers binding
    After being gained by the CCU PLLs the signals must be transformed to
    be suitable for the clock-consumers. This is done by a set of dividers
    embedded into the CCU. A first block of dividers is used to create
    reference clocks for AXI-bus of high-speed peripheral IP-cores of the
    chip. The second block dividers alter the PLLs output signals to be then
    consumed by SoC peripheral devices. Both block DT nodes are ordinary
    clock-providers with standard set of properties supported. But in addition
    to that each clock provider can be used to reset the corresponding clock
    domain. This makes the AXI-bus and System Devices CCU DT nodes to be also
    reset-providers.
    
    Signed-off-by: Serge Semin <Sergey.Semin@baikalelectronics.ru>
    Cc: Alexey Malahov <Alexey.Malahov@baikalelectronics.ru>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: linux-mips@vger.kernel.org
    Link: https://lore.kernel.org/r/20200526222056.18072-3-Sergey.Semin@baikalelectronics.ru
    Reviewed-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- include/dt-bindings/clock/bt1-ccu.h
+++ include/dt-bindings/clock/bt1-ccu.h
@@ -13,4 +13,36 @@
 #define CCU_PCIE_PLL			3
 #define CCU_ETH_PLL			4
 
+#define CCU_AXI_MAIN_CLK		0
+#define CCU_AXI_DDR_CLK			1
+#define CCU_AXI_SATA_CLK		2
+#define CCU_AXI_GMAC0_CLK		3
+#define CCU_AXI_GMAC1_CLK		4
+#define CCU_AXI_XGMAC_CLK		5
+#define CCU_AXI_PCIE_M_CLK		6
+#define CCU_AXI_PCIE_S_CLK		7
+#define CCU_AXI_USB_CLK			8
+#define CCU_AXI_HWA_CLK			9
+#define CCU_AXI_SRAM_CLK		10
+
+#define CCU_SYS_SATA_REF_CLK		0
+#define CCU_SYS_APB_CLK			1
+#define CCU_SYS_GMAC0_TX_CLK		2
+#define CCU_SYS_GMAC0_PTP_CLK		3
+#define CCU_SYS_GMAC1_TX_CLK		4
+#define CCU_SYS_GMAC1_PTP_CLK		5
+#define CCU_SYS_XGMAC_REF_CLK		6
+#define CCU_SYS_XGMAC_PTP_CLK		7
+#define CCU_SYS_USB_CLK			8
+#define CCU_SYS_PVT_CLK			9
+#define CCU_SYS_HWA_CLK			10
+#define CCU_SYS_UART_CLK		11
+#define CCU_SYS_I2C1_CLK		12
+#define CCU_SYS_I2C2_CLK		13
+#define CCU_SYS_GPIO_CLK		14
+#define CCU_SYS_TIMER0_CLK		15
+#define CCU_SYS_TIMER1_CLK		16
+#define CCU_SYS_TIMER2_CLK		17
+#define CCU_SYS_WDT_CLK			18
+
 #endif /* __DT_BINDINGS_CLOCK_BT1_CCU_H */
