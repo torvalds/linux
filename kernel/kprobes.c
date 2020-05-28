d002b8bc6dbc20e9043e279196cff8795dba05fe
Adrian Hunter <adrian.hunter@intel.com>
2020-05-28 11:00:58 +0300

    kprobes: Add symbols for kprobe insn pages
    Symbols are needed for tools to describe instruction addresses. Pages
    allocated for kprobe's purposes need symbols to be created for them.
    Add such symbols to be visible via /proc/kallsyms.
    
    Note: kprobe insn pages are not used if ftrace is configured. To see the
    effect of this patch, the kernel must be configured with:
    
    	# CONFIG_FUNCTION_TRACER is not set
    	CONFIG_KPROBES=y
    
    and for optimised kprobes:
    
    	CONFIG_OPTPROBES=y
    
    Example on x86:
    
    	# perf probe __schedule
    	Added new event:
    	  probe:__schedule     (on __schedule)
    	# cat /proc/kallsyms | grep '\[__builtin__kprobes\]'
    	ffffffffc00d4000 t kprobe_insn_page     [__builtin__kprobes]
    	ffffffffc00d6000 t kprobe_optinsn_page  [__builtin__kprobes]
    
    Note: This patch adds "__builtin__kprobes" as a module name in
    /proc/kallsyms for symbols for pages allocated for kprobes' purposes, even
    though "__builtin__kprobes" is not a module.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Masami Hiramatsu <mhiramat@kernel.org>
    Link: https://lkml.kernel.org/r/20200528080058.20230-1-adrian.hunter@intel.com

--- kernel/kprobes.c
+++ kernel/kprobes.c
@@ -118,6 +118,7 @@ struct kprobe_insn_cache kprobe_insn_slots = {
 	.mutex = __MUTEX_INITIALIZER(kprobe_insn_slots.mutex),
 	.alloc = alloc_insn_page,
 	.free = free_insn_page,
+	.sym = KPROBE_INSN_PAGE_SYM,
 	.pages = LIST_HEAD_INIT(kprobe_insn_slots.pages),
 	.insn_size = MAX_INSN_SIZE,
 	.nr_garbage = 0,
@@ -290,12 +291,34 @@ bool __is_insn_slot_addr(struct kprobe_insn_cache *c, unsigned long addr)
 	return ret;
 }
 
+int kprobe_cache_get_kallsym(struct kprobe_insn_cache *c, unsigned int *symnum,
+			     unsigned long *value, char *type, char *sym)
+{
+	struct kprobe_insn_page *kip;
+	int ret = -ERANGE;
+
+	rcu_read_lock();
+	list_for_each_entry_rcu(kip, &c->pages, list) {
+		if ((*symnum)--)
+			continue;
+		strlcpy(sym, c->sym, KSYM_NAME_LEN);
+		*type = 't';
+		*value = (unsigned long)kip->insns;
+		ret = 0;
+		break;
+	}
+	rcu_read_unlock();
+
+	return ret;
+}
+
 #ifdef CONFIG_OPTPROBES
 /* For optimized_kprobe buffer */
 struct kprobe_insn_cache kprobe_optinsn_slots = {
 	.mutex = __MUTEX_INITIALIZER(kprobe_optinsn_slots.mutex),
 	.alloc = alloc_insn_page,
 	.free = free_insn_page,
+	.sym = KPROBE_OPTINSN_PAGE_SYM,
 	.pages = LIST_HEAD_INIT(kprobe_optinsn_slots.pages),
 	/* .insn_size is initialized later */
 	.nr_garbage = 0,
@@ -2195,6 +2218,28 @@ static void kprobe_remove_area_blacklist(unsigned long start, unsigned long end)
 static void kprobe_remove_ksym_blacklist(unsigned long entry)
 {
 	kprobe_remove_area_blacklist(entry, entry + 1);
+}
+
+int __weak arch_kprobe_get_kallsym(unsigned int *symnum, unsigned long *value,
+				   char *type, char *sym)
+{
+	return -ERANGE;
+}
+
+int kprobe_get_kallsym(unsigned int symnum, unsigned long *value, char *type,
+		       char *sym)
+{
+#ifdef __ARCH_WANT_KPROBES_INSN_SLOT
+	if (!kprobe_cache_get_kallsym(&kprobe_insn_slots, &symnum, value, type, sym))
+		return 0;
+#ifdef CONFIG_OPTPROBES
+	if (!kprobe_cache_get_kallsym(&kprobe_optinsn_slots, &symnum, value, type, sym))
+		return 0;
+#endif
+#endif
+	if (!arch_kprobe_get_kallsym(&symnum, value, type, sym))
+		return 0;
+	return -ERANGE;
 }
 
 int __init __weak arch_populate_kprobe_blacklist(void)
