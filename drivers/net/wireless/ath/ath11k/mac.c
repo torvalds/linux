9529cba988b74091b3a67916b1c119f5b189b8b6
Muna Sinada <msinada@codeaurora.org>
2020-05-28 15:54:43 -0700

    ath11k: reset trigger frame MAC padding duration
    The value was 3 and it's reserved value. Corrected to maintain fw
    defaults.
    
    Signed-off-by: Muna Sinada <msinada@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/1590706483-27609-1-git-send-email-msinada@codeaurora.org

--- drivers/net/wireless/ath/ath11k/mac.c
+++ drivers/net/wireless/ath/ath11k/mac.c
@@ -3562,7 +3562,7 @@ static int ath11k_mac_copy_he_cap(struct ath11k *ar,
 		memcpy(he_cap_elem->phy_cap_info, band_cap->he_cap_phy_info,
 		       sizeof(he_cap_elem->phy_cap_info));
 
-		he_cap_elem->mac_cap_info[1] |=
+		he_cap_elem->mac_cap_info[1] &=
 			IEEE80211_HE_MAC_CAP1_TF_MAC_PAD_DUR_MASK;
 		he_cap_elem->phy_cap_info[4] &=
 			~IEEE80211_HE_PHY_CAP4_BEAMFORMEE_MAX_STS_UNDER_80MHZ_MASK;
