7c582bf4ed84f3eb58bdd1f63024a14c17551e7d
James Morse <james.morse@arm.com>
2020-05-29 15:06:54 +0000

    KVM: arm64: Stop writing aarch32's CSSELR into ACTLR
    aarch32 has pairs of registers to access the high and low parts of 64bit
    registers. KVM has a union of 64bit sys_regs[] and 32bit copro[]. The
    32bit accessors read the high or low part of the 64bit sys_reg[] value
    through the union.
    
    Both sys_reg_descs[] and cp15_regs[] list access_csselr() as the accessor
    for CSSELR{,_EL1}. access_csselr() is only aware of the 64bit sys_regs[],
    and expects r->reg to be 'CSSELR_EL1' in the enum, index 2 of the 64bit
    array.
    
    cp15_regs[] uses the 32bit copro[] alias of sys_regs[]. Here CSSELR is
    c0_CSSELR which is the same location in sys_reg[]. r->reg is 'c0_CSSELR',
    index 4 in the 32bit array.
    
    access_csselr() uses the 32bit r->reg value to access the 64bit array,
    so reads and write the wrong value. sys_regs[4], is ACTLR_EL1, which
    is subsequently save/restored when we enter the guest.
    
    ACTLR_EL1 is supposed to be read-only for the guest. This register
    only affects execution at EL1, and the host's value is restored before
    we return to host EL1.
    
    Convert the 32bit register index back to the 64bit version.
    
    Suggested-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/r/20200529150656.7339-2-james.morse@arm.com

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -1319,10 +1319,16 @@ static bool access_clidr(struct kvm_vcpu *vcpu, struct sys_reg_params *p,
 static bool access_csselr(struct kvm_vcpu *vcpu, struct sys_reg_params *p,
 			  const struct sys_reg_desc *r)
 {
+	int reg = r->reg;
+
+	/* See the 32bit mapping in kvm_host.h */
+	if (p->is_aarch32)
+		reg = r->reg / 2;
+
 	if (p->is_write)
-		vcpu_write_sys_reg(vcpu, p->regval, r->reg);
+		vcpu_write_sys_reg(vcpu, p->regval, reg);
 	else
-		p->regval = vcpu_read_sys_reg(vcpu, r->reg);
+		p->regval = vcpu_read_sys_reg(vcpu, reg);
 	return true;
 }
 
