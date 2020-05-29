9b733ddbfa52e10e5d203bee2dbc9185fceada3a
Greg Kroah-Hartman <gregkh@google.com>
2020-05-29 15:03:12 +0200

    Revert "ANDROID: sdcardfs: Enable modular sdcardfs"
    This reverts commit c0dbfed5ff9fce7a701a33587ea5fea6a84f4dc3 as it is
    not needed now that sdcardfs is gone.
    
    Bug: 157700134
    Cc: Daniel Rosenberg <drosen@google.com>
    Cc: Guenter Roeck <groeck@chromium.org>
    Cc: Alistair Strachan <astrachan@google.com>
    Cc: Yongqin Liu <yongqin.liu@linaro.org>
    Cc: Daniel Rosenberg <drosen@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Ie7f7bd51fe7e6b9ddb27883d11d959cf69d0361c

--- fs/fs_struct.c
+++ fs/fs_struct.c
@@ -45,7 +45,6 @@ void set_fs_pwd(struct fs_struct *fs, const struct path *path)
 	if (old_pwd.dentry)
 		path_put(&old_pwd);
 }
-EXPORT_SYMBOL_GPL(set_fs_pwd);
 
 static inline int replace_path(struct path *p, const struct path *old, const struct path *new)
 {
@@ -91,7 +90,6 @@ void free_fs_struct(struct fs_struct *fs)
 	path_put(&fs->pwd);
 	kmem_cache_free(fs_cachep, fs);
 }
-EXPORT_SYMBOL_GPL(free_fs_struct);
 
 void exit_fs(struct task_struct *tsk)
 {
@@ -130,7 +128,6 @@ struct fs_struct *copy_fs_struct(struct fs_struct *old)
 	}
 	return fs;
 }
-EXPORT_SYMBOL_GPL(copy_fs_struct);
 
 int unshare_fs_struct(void)
 {
--- security/security.c
+++ security/security.c
@@ -603,7 +603,6 @@ int security_path_chown(const struct path *path, kuid_t uid, kgid_t gid)
 		return 0;
 	return call_int_hook(path_chown, 0, path, uid, gid);
 }
-EXPORT_SYMBOL_GPL(security_path_chown);
 
 int security_path_chroot(const struct path *path)
 {
