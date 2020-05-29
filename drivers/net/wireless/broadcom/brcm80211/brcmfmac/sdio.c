528158a8d4522bed5a35bb1a942638a79bca6acd
Double Lo <double.lo@cypress.com>
2020-05-28 22:49:35 -0500

    brcmfmac: fix 4339 CRC error under SDIO 3.0 SDR104 mode
    This patch fixes 4339 CRC error while running Tput test with
    suspend/resume test script.
    
    The continuous failure messages before system crash:
    brcmfmac: brcmf_sdiod_sglist_rw: CMD53 sg block read failed -84
    brcmfmac: brcmf_sdio_rxglom: glom read of 25600 bytes failed: -5
    brcmfmac: brcmf_sdio_rxfail: abort command, terminate frame
    brcmfmac: brcmf_sdiod_sglist_rw: CMD53 sg block read failed -84
    brcmfmac: brcmf_sdio_rxglom: glom read of 24576 bytes failed: -5
    brcmfmac: brcmf_sdio_rxfail: abort command, terminate frame
    
    Signed-off-by: Double Lo <double.lo@cypress.com>
    Signed-off-by: Chi-hsien Lin <chi-hsien.lin@cypress.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200529034938.124533-3-chi-hsien.lin@cypress.com

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
@@ -43,6 +43,10 @@
 #define CY_4373_F2_WATERMARK    0x40
 #define CY_4373_F1_MESBUSYCTRL  (CY_4373_F2_WATERMARK | SBSDIO_MESBUSYCTRL_ENAB)
 #define CY_43012_F2_WATERMARK    0x60
+#define CY_4339_F2_WATERMARK    48
+#define CY_4339_MES_WATERMARK	80
+#define CY_4339_MESBUSYCTRL	(CY_4339_MES_WATERMARK | \
+				 SBSDIO_MESBUSYCTRL_ENAB)
 #define CY_4359_F2_WATERMARK	0x40
 #define CY_4359_F1_MESBUSYCTRL	(CY_4359_F2_WATERMARK | SBSDIO_MESBUSYCTRL_ENAB)
 
@@ -4208,6 +4212,19 @@ static void brcmf_sdio_firmware_callback(struct device *dev, int err,
 			devctl |= SBSDIO_DEVCTL_F2WM_ENAB;
 			brcmf_sdiod_writeb(sdiod, SBSDIO_DEVICE_CTL, devctl,
 					   &err);
+			break;
+		case SDIO_DEVICE_ID_BROADCOM_4339:
+			brcmf_dbg(INFO, "set F2 watermark to 0x%x*4 bytes for 4339\n",
+				  CY_4339_F2_WATERMARK);
+			brcmf_sdiod_writeb(sdiod, SBSDIO_WATERMARK,
+					   CY_4339_F2_WATERMARK, &err);
+			devctl = brcmf_sdiod_readb(sdiod, SBSDIO_DEVICE_CTL,
+						   &err);
+			devctl |= SBSDIO_DEVCTL_F2WM_ENAB;
+			brcmf_sdiod_writeb(sdiod, SBSDIO_DEVICE_CTL, devctl,
+					   &err);
+			brcmf_sdiod_writeb(sdiod, SBSDIO_FUNC1_MESBUSYCTRL,
+					   CY_4339_MESBUSYCTRL, &err);
 			break;
 		case SDIO_DEVICE_ID_BROADCOM_4359:
 			brcmf_dbg(INFO, "set F2 watermark to 0x%x*4 bytes\n",
