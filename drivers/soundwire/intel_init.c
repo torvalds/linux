4ab34412fc62fba29ba00d76cf2c46585b3e5ba3
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-06-01 02:21:01 +0800

    soundwire: intel_init: pass link information as platform data
    It's not clear how this code ever worked, the link information is used
    in intel.c but never passed as platform_data.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Link: https://lore.kernel.org/r/20200531182102.27840-6-yung-chuan.liao@linux.intel.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/soundwire/intel_init.c
+++ drivers/soundwire/intel_init.c
@@ -128,6 +128,8 @@ static struct sdw_intel_ctx
 		pdevinfo.name = "int-sdw";
 		pdevinfo.id = i;
 		pdevinfo.fwnode = acpi_fwnode_handle(adev);
+		pdevinfo.data = link;
+		pdevinfo.size_data = sizeof(*link);
 
 		pdev = platform_device_register_full(&pdevinfo);
 		if (IS_ERR(pdev)) {
