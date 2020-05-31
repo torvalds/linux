14a7ef71fbd0cc1e19da6fcd0521217712487fcf
Hans de Goede <hdegoede@redhat.com>
2020-05-31 11:30:25 +0200

    drm: panel-orientation-quirks: Use generic orientation-data for Acer S1003
    [ Upstream commit a05caf9e62a85d12da27e814ac13195f4683f21c ]
    
    The Acer S1003 has proper DMI strings for sys-vendor and product-name,
    so we do not need to match by BIOS-date.
    
    This means that the Acer S1003 can use the generic lcd800x1280_rightside_up
    drm_dmi_panel_orientation_data struct which is also used by other quirks.
    
    Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200531093025.28050-2-hdegoede@redhat.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/drm_panel_orientation_quirks.c
+++ drivers/gpu/drm/drm_panel_orientation_quirks.c
@@ -30,12 +30,6 @@ struct drm_dmi_panel_orientation_data {
 	int orientation;
 };
 
-static const struct drm_dmi_panel_orientation_data acer_s1003 = {
-	.width = 800,
-	.height = 1280,
-	.orientation = DRM_MODE_PANEL_ORIENTATION_RIGHT_UP,
-};
-
 static const struct drm_dmi_panel_orientation_data asus_t100ha = {
 	.width = 800,
 	.height = 1280,
@@ -100,7 +94,7 @@ static const struct dmi_system_id orientation_data[] = {
 		  DMI_EXACT_MATCH(DMI_SYS_VENDOR, "Acer"),
 		  DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "One S1003"),
 		},
-		.driver_data = (void *)&acer_s1003,
+		.driver_data = (void *)&lcd800x1280_rightside_up,
 	}, {	/* Asus T100HA */
 		.matches = {
 		  DMI_EXACT_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
