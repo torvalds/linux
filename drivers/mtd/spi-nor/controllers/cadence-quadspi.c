41b5ed6e677ca73cb031b7657eefb5cf27071be3
Vignesh Raghavendra <vigneshr@ti.com>
2020-06-01 12:34:42 +0530

    mtd: spi-nor: cadence-quadspi: Drop redundant WREN in erase path
    Drop redundant WREN command in cqspi_erase() as SPI NOR core takes care
    of sending WREN command before sending erase command.
    
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>
    Reviewed-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Acked-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Link: https://lore.kernel.org/r/20200601070444.16923-7-vigneshr@ti.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/mtd/spi-nor/controllers/cadence-quadspi.c
+++ drivers/mtd/spi-nor/controllers/cadence-quadspi.c
@@ -1016,11 +1016,6 @@ static int cqspi_erase(struct spi_nor *nor, loff_t offs)
 	if (ret)
 		return ret;
 
-	/* Send write enable, then erase commands. */
-	ret = nor->controller_ops->write_reg(nor, SPINOR_OP_WREN, NULL, 0);
-	if (ret)
-		return ret;
-
 	/* Set up command buffer. */
 	ret = cqspi_command_write_addr(nor, nor->erase_opcode, offs);
 	if (ret)
