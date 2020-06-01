9d81c25455d2038925f4a1138e18707e8d976fd2
Tao Huang <huangtao@rock-chips.com>
2020-06-01 15:20:41 +0800

    net: wireless: rockchip_wlan: rtl8821cs: Fix gcc warning
    Change-Id: I2652111b1c6329405c86a2bcdabf6e2900d47400
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- drivers/net/wireless/rockchip_wlan/rtl8821cs/core/rtw_ap.c
+++ drivers/net/wireless/rockchip_wlan/rtl8821cs/core/rtw_ap.c
@@ -171,7 +171,7 @@ void rtw_add_bcn_ie(_adapter *padapter, WLAN_BSSID_EX *pnetwork, u8 index, u8 *d
 	u8	bmatch = _FALSE;
 	u8	*pie = pnetwork->IEs;
 	u8	*p = NULL, *dst_ie = NULL, *premainder_ie = NULL, *pbackup_remainder_ie = NULL;
-	u32	i, offset, ielen, ie_offset, remainder_ielen = 0;
+	u32	i, offset, ielen = 0, ie_offset, remainder_ielen = 0;
 
 	for (i = sizeof(NDIS_802_11_FIXED_IEs); i < pnetwork->IELength;) {
 		pIE = (PNDIS_802_11_VARIABLE_IEs)(pnetwork->IEs + i);
