899c537c25f9547a0f591ed2013be0f9f68f2df3
Guru Das Srinagesh <gurus@codeaurora.org>
2020-06-02 15:31:05 -0700

    drm/i915: Use 64-bit division macro
    Since the PWM framework is switching struct pwm_state.duty_cycle's
    datatype to u64, prepare for this transition by using DIV_ROUND_UP_ULL
    to handle a 64-bit dividend.
    
    Signed-off-by: Guru Das Srinagesh <gurus@codeaurora.org>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Acked-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- drivers/gpu/drm/i915/display/intel_panel.c
+++ drivers/gpu/drm/i915/display/intel_panel.c
@@ -1929,7 +1929,7 @@ static int pwm_setup_backlight(struct intel_connector *connector,
 		return retval;
 	}
 
-	level = DIV_ROUND_UP(pwm_get_duty_cycle(panel->backlight.pwm) * 100,
+	level = DIV_ROUND_UP_ULL(pwm_get_duty_cycle(panel->backlight.pwm) * 100,
 			     CRC_PMIC_PWM_PERIOD_NS);
 	panel->backlight.level =
 		intel_panel_compute_brightness(connector, level);
