251af0cda614e5ad6bfda059bd120aed7432891d
Miaohe Lin <linmiaohe@huawei.com>
2020-06-01 21:49:29 -0700

    include/linux/swap.h: delete meaningless __add_to_swap_cache() declaration
    Since commit 8d93b41c09d1 ("mm: Convert add_to_swap_cache to XArray"),
    __add_to_swap_cache and add_to_swap_cache are combined into one
    function.  There is no __add_to_swap_cache() anymore.
    
    Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: "Huang, Ying" <ying.huang@intel.com>
    Link: http://lkml.kernel.org/r/1590810326-2493-1-git-send-email-linmiaohe@huawei.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -414,7 +414,6 @@ extern unsigned long total_swapcache_pages(void);
 extern void show_swap_cache_info(void);
 extern int add_to_swap(struct page *page);
 extern int add_to_swap_cache(struct page *, swp_entry_t, gfp_t);
-extern int __add_to_swap_cache(struct page *page, swp_entry_t entry);
 extern void __delete_from_swap_cache(struct page *, swp_entry_t entry);
 extern void delete_from_swap_cache(struct page *);
 extern void free_page_and_swap_cache(struct page *);
