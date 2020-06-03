332516947ef83343b9bcc7275ff99db392592c94
Serge Semin <Sergey.Semin@baikalelectronics.ru>
2020-06-03 03:07:53 +0300

    hwmon: (bt1-pvt) Define Temp- and Volt-to-N poly as maybe-unused
    Clang-based kernel building with W=1 warns that some static const
    variables are unused:
    
    drivers/hwmon/bt1-pvt.c:67:30: warning: unused variable 'poly_temp_to_N' [-Wunused-const-variable]
    static const struct pvt_poly poly_temp_to_N = {
                                 ^
    drivers/hwmon/bt1-pvt.c:99:30: warning: unused variable 'poly_volt_to_N' [-Wunused-const-variable]
    static const struct pvt_poly poly_volt_to_N = {
                                 ^
    
    Indeed these polynomials are utilized only when the PVT sensor alarms are
    enabled. In that case they are used to convert the temperature and
    voltage alarm limits from normal quantities (Volts and degree Celsius) to
    the sensor data representation N = [0, 1023]. Otherwise when alarms are
    disabled the driver only does the detected data conversion to the human
    readable form and doesn't need that polynomials defined. So let's mark the
    Temp-to-N and Volt-to-N polynomials with __maybe_unused attribute.
    
    Note gcc with W=1 doesn't notice the problem.
    
    Fixes: 87976ce2825d ("hwmon: Add Baikal-T1 PVT sensor driver")
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Serge Semin <Sergey.Semin@baikalelectronics.ru>
    Cc: Maxim Kaurkin <Maxim.Kaurkin@baikalelectronics.ru>
    Cc: Alexey Malahov <Alexey.Malahov@baikalelectronics.ru>
    Link: https://lore.kernel.org/r/20200603000753.391-1-Sergey.Semin@baikalelectronics.ru
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/bt1-pvt.c
+++ drivers/hwmon/bt1-pvt.c
@@ -64,7 +64,7 @@ static const struct pvt_sensor_info pvt_info[] = {
  *     48380,
  * where T = [-48380, 147438] mC and N = [0, 1023].
  */
-static const struct pvt_poly poly_temp_to_N = {
+static const struct pvt_poly __maybe_unused poly_temp_to_N = {
 	.total_divider = 10000,
 	.terms = {
 		{4, 18322, 10000, 10000},
@@ -96,7 +96,7 @@ static const struct pvt_poly poly_N_to_temp = {
  * N = (18658e-3*V - 11572) / 10,
  * V = N * 10^5 / 18658 + 11572 * 10^4 / 18658.
  */
-static const struct pvt_poly poly_volt_to_N = {
+static const struct pvt_poly __maybe_unused poly_volt_to_N = {
 	.total_divider = 10,
 	.terms = {
 		{1, 18658, 1000, 1},
