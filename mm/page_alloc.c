833d8a426f78f19b166d93bda0569d3d6d507dba
Wei Yang <richard.weiyang@gmail.com>
2020-06-03 15:58:26 -0700

    mm/page_alloc.c: bad_[reason|flags] is not necessary when PageHWPoison
    Patch series "mm/page_alloc.c: cleanup on check page", v3.
    
    This patchset does some cleanup related to check page.
    
    1. Remove unnecessary bad_reason assignment
    2. Remove bad_flags to bad_page()
    3. Rename function for naming convention
    4. Extract common part to check page
    
    Thanks for suggestions from David Rientjes and Anshuman Khandual.
    
    This patch (of 5):
    
    Since function returns directly, bad_[reason|flags] is not used any where.
    And move this to the first.
    
    This is a following cleanup for commit e570f56cccd21 ("mm:
    check_new_page_bad() directly returns in __PG_HWPOISON case")
    
    Signed-off-by: Wei Yang <richard.weiyang@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Anshuman Khandual <anshuman.khandual@arm.com>
    Cc: David Rientjes <rientjes@google.com>
    Link: http://lkml.kernel.org/r/20200411220357.9636-2-richard.weiyang@gmail.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2097,19 +2097,17 @@ static void check_new_page_bad(struct page *page)
 	const char *bad_reason = NULL;
 	unsigned long bad_flags = 0;
 
+	if (unlikely(page->flags & __PG_HWPOISON)) {
+		/* Don't complain about hwpoisoned pages */
+		page_mapcount_reset(page); /* remove PageBuddy */
+		return;
+	}
 	if (unlikely(atomic_read(&page->_mapcount) != -1))
 		bad_reason = "nonzero mapcount";
 	if (unlikely(page->mapping != NULL))
 		bad_reason = "non-NULL mapping";
 	if (unlikely(page_ref_count(page) != 0))
 		bad_reason = "nonzero _refcount";
-	if (unlikely(page->flags & __PG_HWPOISON)) {
-		bad_reason = "HWPoisoned (hardware-corrupted)";
-		bad_flags = __PG_HWPOISON;
-		/* Don't complain about hwpoisoned pages */
-		page_mapcount_reset(page); /* remove PageBuddy */
-		return;
-	}
 	if (unlikely(page->flags & PAGE_FLAGS_CHECK_AT_PREP)) {
 		bad_reason = "PAGE_FLAGS_CHECK_AT_PREP flag set";
 		bad_flags = PAGE_FLAGS_CHECK_AT_PREP;
