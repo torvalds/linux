43bd11238da345ad4b09a585ae2a038c6f8b7113
Evan Green <evgreen@chromium.org>
2020-06-04 10:59:11 -0700

    ath10k: Acquire tx_lock in tx error paths
    [ Upstream commit a738e766e3ed92c4ee5ec967777276b5ce11dd2c ]
    
    ath10k_htt_tx_free_msdu_id() has a lockdep assertion that htt->tx_lock
    is held. Acquire the lock in a couple of error paths when calling that
    function to ensure this condition is met.
    
    Fixes: 6421969f248fd ("ath10k: refactor tx pending management")
    Fixes: e62ee5c381c59 ("ath10k: Add support for htt_data_tx_desc_64 descriptor")
    Signed-off-by: Evan Green <evgreen@chromium.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200604105901.1.I5b8b0c7ee0d3e51a73248975a9da61401b8f3900@changeid
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/wireless/ath/ath10k/htt_tx.c
+++ drivers/net/wireless/ath/ath10k/htt_tx.c
@@ -1329,7 +1329,9 @@ static int ath10k_htt_tx_32(struct ath10k_htt *htt,
 err_unmap_msdu:
 	dma_unmap_single(dev, skb_cb->paddr, msdu->len, DMA_TO_DEVICE);
 err_free_msdu_id:
+	spin_lock_bh(&htt->tx_lock);
 	ath10k_htt_tx_free_msdu_id(htt, msdu_id);
+	spin_unlock_bh(&htt->tx_lock);
 err:
 	return res;
 }
@@ -1536,7 +1538,9 @@ static int ath10k_htt_tx_64(struct ath10k_htt *htt,
 err_unmap_msdu:
 	dma_unmap_single(dev, skb_cb->paddr, msdu->len, DMA_TO_DEVICE);
 err_free_msdu_id:
+	spin_lock_bh(&htt->tx_lock);
 	ath10k_htt_tx_free_msdu_id(htt, msdu_id);
+	spin_unlock_bh(&htt->tx_lock);
 err:
 	return res;
 }
