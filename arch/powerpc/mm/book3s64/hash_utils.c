55bd9ac468397c4f12a33b7ec714b5d0362c3aa2
Joe Perches <joe@perches.com>
2020-06-05 07:18:06 -0700

    powerpc/mm: Fix typo in IS_ENABLED()
    IS_ENABLED() matches names exactly, so the missing "CONFIG_" prefix
    means this code would never be built.
    
    Also fixes a missing newline in pr_warn().
    
    Fixes: 970d54f99cea ("powerpc/book3s64/hash: Disable 16M linear mapping size if not aligned")
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/202006050717.A2F9809E@keescook

--- arch/powerpc/mm/book3s64/hash_utils.c
+++ arch/powerpc/mm/book3s64/hash_utils.c
@@ -663,11 +663,10 @@ static void __init htab_init_page_sizes(void)
 		 * Pick a size for the linear mapping. Currently, we only
 		 * support 16M, 1M and 4K which is the default
 		 */
-		if (IS_ENABLED(STRICT_KERNEL_RWX) &&
+		if (IS_ENABLED(CONFIG_STRICT_KERNEL_RWX) &&
 		    (unsigned long)_stext % 0x1000000) {
 			if (mmu_psize_defs[MMU_PAGE_16M].shift)
-				pr_warn("Kernel not 16M aligned, "
-					"disabling 16M linear map alignment");
+				pr_warn("Kernel not 16M aligned, disabling 16M linear map alignment\n");
 			aligned = false;
 		}
 
