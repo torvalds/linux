6c55a7a5a73efe8ef4e54985bb6176c89f1dfd3e
Thomas Zimmermann <tzimmermann@suse.de>
2020-06-05 09:32:19 +0200

    drm/imx: Use GEM CMA object functions
    Create GEM objects with drm_gem_cma_create_object_default_funcs(), which
    allocates the object and sets CMA's default object functions. Corresponding
    callbacks in struct drm_driver are cleared. No functional changes are made.
    
    Driver and object-function instances use the same callback functions, with
    the exception of vunmap. The implementation of vunmap is empty and left out
    in CMA's default object functions.
    
    v3:
    	* convert to DRIVER_OPS macro in a separate patch
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200605073247.4057-16-tzimmermann@suse.de

--- drivers/gpu/drm/imx/imx-drm-core.c
+++ drivers/gpu/drm/imx/imx-drm-core.c
@@ -146,16 +146,12 @@ static const struct drm_ioctl_desc imx_drm_ioctls[] = {
 
 static struct drm_driver imx_drm_driver = {
 	.driver_features	= DRIVER_MODESET | DRIVER_GEM | DRIVER_ATOMIC,
-	.gem_free_object_unlocked = drm_gem_cma_free_object,
-	.gem_vm_ops		= &drm_gem_cma_vm_ops,
+	.gem_create_object	= drm_gem_cma_create_object_default_funcs,
 	.dumb_create		= drm_gem_cma_dumb_create,
 
 	.prime_handle_to_fd	= drm_gem_prime_handle_to_fd,
 	.prime_fd_to_handle	= drm_gem_prime_fd_to_handle,
-	.gem_prime_get_sg_table	= drm_gem_cma_prime_get_sg_table,
 	.gem_prime_import_sg_table = drm_gem_cma_prime_import_sg_table,
-	.gem_prime_vmap		= drm_gem_cma_prime_vmap,
-	.gem_prime_vunmap	= drm_gem_cma_prime_vunmap,
 	.gem_prime_mmap		= drm_gem_cma_prime_mmap,
 	.ioctls			= imx_drm_ioctls,
 	.num_ioctls		= ARRAY_SIZE(imx_drm_ioctls),
