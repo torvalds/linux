c6131162549664de286ca21991b73b968b913de6
Thomas Zimmermann <tzimmermann@suse.de>
2020-06-05 09:32:22 +0200

    drm/ingenic: Set GEM CMA functions with DRM_GEM_CMA_DRIVER_OPS
    DRM_GEM_CMA_DRIVER_OPS sets the functions in struct drm_driver
    to their defaults. No functional changes are made.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Tested-by: Paul Cercueil <paul@crapouillou.net>
    Reviewed-by: Paul Cercueil <paul@crapouillou.net>
    Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200605073247.4057-19-tzimmermann@suse.de

--- drivers/gpu/drm/ingenic/ingenic-drm.c
+++ drivers/gpu/drm/ingenic/ingenic-drm.c
@@ -519,14 +519,7 @@ static struct drm_driver ingenic_drm_driver_data = {
 	.patchlevel		= 0,
 
 	.fops			= &ingenic_drm_fops,
-
-	.gem_create_object	= drm_gem_cma_create_object_default_funcs,
-	.dumb_create		= drm_gem_cma_dumb_create,
-
-	.prime_handle_to_fd	= drm_gem_prime_handle_to_fd,
-	.prime_fd_to_handle	= drm_gem_prime_fd_to_handle,
-	.gem_prime_import_sg_table = drm_gem_cma_prime_import_sg_table,
-	.gem_prime_mmap		= drm_gem_cma_prime_mmap,
+	DRM_GEM_CMA_DRIVER_OPS,
 
 	.irq_handler		= ingenic_drm_irq_handler,
 };
