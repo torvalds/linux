eeef5a6c0b6fe9e49abfbd50af8dcec9c92b7211
Thomas Zimmermann <tzimmermann@suse.de>
2020-06-05 09:32:20 +0200

    drm/imx: Set GEM CMA functions with DRM_GEM_CMA_DRIVER_OPS
    DRM_GEM_CMA_DRIVER_OPS sets the functions in struct drm_driver
    to their defaults. No functional changes are made.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200605073247.4057-17-tzimmermann@suse.de

--- drivers/gpu/drm/imx/imx-drm-core.c
+++ drivers/gpu/drm/imx/imx-drm-core.c
@@ -146,13 +146,7 @@ static const struct drm_ioctl_desc imx_drm_ioctls[] = {
 
 static struct drm_driver imx_drm_driver = {
 	.driver_features	= DRIVER_MODESET | DRIVER_GEM | DRIVER_ATOMIC,
-	.gem_create_object	= drm_gem_cma_create_object_default_funcs,
-	.dumb_create		= drm_gem_cma_dumb_create,
-
-	.prime_handle_to_fd	= drm_gem_prime_handle_to_fd,
-	.prime_fd_to_handle	= drm_gem_prime_fd_to_handle,
-	.gem_prime_import_sg_table = drm_gem_cma_prime_import_sg_table,
-	.gem_prime_mmap		= drm_gem_cma_prime_mmap,
+	DRM_GEM_CMA_DRIVER_OPS,
 	.ioctls			= imx_drm_ioctls,
 	.num_ioctls		= ARRAY_SIZE(imx_drm_ioctls),
 	.fops			= &imx_drm_driver_fops,
