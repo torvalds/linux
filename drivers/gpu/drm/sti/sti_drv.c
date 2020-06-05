9ddf22ad804e429ccaec23100c206148582bdf9d
Thomas Zimmermann <tzimmermann@suse.de>
2020-06-05 09:32:40 +0200

    drm/sti: Set GEM CMA functions with DRM_GEM_CMA_DRIVER_OPS
    DRM_GEM_CMA_DRIVER_OPS sets the functions in struct drm_driver
    to their defaults. No functional changes are made.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200605073247.4057-37-tzimmermann@suse.de

--- drivers/gpu/drm/sti/sti_drv.c
+++ drivers/gpu/drm/sti/sti_drv.c
@@ -132,14 +132,8 @@ DEFINE_DRM_GEM_CMA_FOPS(sti_driver_fops);
 
 static struct drm_driver sti_driver = {
 	.driver_features = DRIVER_MODESET | DRIVER_GEM | DRIVER_ATOMIC,
-	.gem_create_object = drm_gem_cma_create_object_default_funcs,
-	.dumb_create = drm_gem_cma_dumb_create,
 	.fops = &sti_driver_fops,
-
-	.prime_handle_to_fd = drm_gem_prime_handle_to_fd,
-	.prime_fd_to_handle = drm_gem_prime_fd_to_handle,
-	.gem_prime_import_sg_table = drm_gem_cma_prime_import_sg_table,
-	.gem_prime_mmap = drm_gem_cma_prime_mmap,
+	DRM_GEM_CMA_DRIVER_OPS,
 
 	.debugfs_init = sti_drm_dbg_init,
 
