c99e1d0c91ac8d7db3062ea1af315f21295701d7
Chuhong Yuan <hslester96@gmail.com>
2020-06-09 00:22:26 +0800

    mmc: sdhci-of-arasan: Add missed checks for devm_clk_register()
    These functions do not check the return value of devm_clk_register():
      - sdhci_arasan_register_sdcardclk()
      - sdhci_arasan_register_sampleclk()
    
    Therefore, add the missed checks to fix them.
    
    Fixes: c390f2110adf1 ("mmc: sdhci-of-arasan: Add ability to export card clock")
    Signed-off-by: Chuhong Yuan <hslester96@gmail.com>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Link: https://lore.kernel.org/r/20200608162226.3259186-1-hslester96@gmail.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/sdhci-of-arasan.c
+++ drivers/mmc/host/sdhci-of-arasan.c
@@ -1299,6 +1299,8 @@ sdhci_arasan_register_sdcardclk(struct sdhci_arasan_data *sdhci_arasan,
 	clk_data->sdcardclk_hw.init = &sdcardclk_init;
 	clk_data->sdcardclk =
 		devm_clk_register(dev, &clk_data->sdcardclk_hw);
+	if (IS_ERR(clk_data->sdcardclk))
+		return PTR_ERR(clk_data->sdcardclk);
 	clk_data->sdcardclk_hw.init = NULL;
 
 	ret = of_clk_add_provider(np, of_clk_src_simple_get,
@@ -1349,6 +1351,8 @@ sdhci_arasan_register_sampleclk(struct sdhci_arasan_data *sdhci_arasan,
 	clk_data->sampleclk_hw.init = &sampleclk_init;
 	clk_data->sampleclk =
 		devm_clk_register(dev, &clk_data->sampleclk_hw);
+	if (IS_ERR(clk_data->sampleclk))
+		return PTR_ERR(clk_data->sampleclk);
 	clk_data->sampleclk_hw.init = NULL;
 
 	ret = of_clk_add_provider(np, of_clk_src_simple_get,
