8e0fa5d7b3df6e7e23e13a7250b00afe32d4e618
David Sterba <dsterba@suse.com>
2020-06-09 19:21:48 +0200

    Revert "btrfs: remove BTRFS_INODE_READDIO_NEED_LOCK"
    This reverts commit 5f008163a559d566a0ee1190a0a24f3eec6f1ea7.
    
    The patch is a simplification after direct IO port to iomap
    infrastructure, which gets reverted.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -4811,7 +4811,10 @@ static int btrfs_setsize(struct inode *inode, struct iattr *attr)
 
 		truncate_setsize(inode, newsize);
 
+		/* Disable nonlocked read DIO to avoid the endless truncate */
+		btrfs_inode_block_unlocked_dio(BTRFS_I(inode));
 		inode_dio_wait(inode);
+		btrfs_inode_resume_unlocked_dio(BTRFS_I(inode));
 
 		ret = btrfs_truncate(inode, newsize == oldsize);
 		if (ret && inode->i_nlink) {
