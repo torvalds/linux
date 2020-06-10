b9f327f1afc7c061233adf585736857d20c6ac06
Ben Skeggs <bskeggs@redhat.com>
2020-06-10 14:46:01 +1000

    drm/nouveau/mmu/gp100-: enable mmu invalidate depth optimisation
    This causes us to invalidate MMU only at the level we made modifications -
    ie: if we've only modified PTEs, there's no need to have MMU dump the PDs
    it's fetched into L2.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/subdev/mmu/vmmgp100.c
+++ drivers/gpu/drm/nouveau/nvkm/subdev/mmu/vmmgp100.c
@@ -466,7 +466,6 @@ void
 gp100_vmm_flush(struct nvkm_vmm *vmm, int depth)
 {
 	u32 type = (5 /* CACHE_LEVEL_UP_TO_PDE3 */ - depth) << 24;
-	type = 0; /*XXX: need to confirm stuff works with depth enabled... */
 	if (atomic_read(&vmm->engref[NVKM_SUBDEV_BAR]))
 		type |= 0x00000004; /* HUB_ONLY */
 	type |= 0x00000001; /* PAGE_ALL */
