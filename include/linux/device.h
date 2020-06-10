b73e7575f3e015db6240fee767c578f6528e05aa
Saravana Kannan <saravanak@google.com>
2020-06-09 18:19:33 -0700

    FROMGIT: driver core: Add device_is_dependent() to linux/device.h
    DT implementation of fw_devlink needs this function to detect cycles. So
    make it available.
    
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Tested-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Rob Herring <robh@kernel.org>
    (cherry picked from commit 7d34ca3854845398cb36866d14bbdc43dcec1ad0
     https://git.kernel.org/pub/scm/linux/kernel/git/robh/linux.git/ for-next)
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Bug: 160736143
    Change-Id: I8a457ed80292a5f1f2850b76eba226a7ed4461f7

--- include/linux/device.h
+++ include/linux/device.h
@@ -829,6 +829,7 @@ extern int device_change_owner(struct device *dev, kuid_t kuid, kgid_t kgid);
 extern const char *device_get_devnode(struct device *dev,
 				      umode_t *mode, kuid_t *uid, kgid_t *gid,
 				      const char **tmp);
+extern int device_is_dependent(struct device *dev, void *target);
 
 static inline bool device_supports_offline(struct device *dev)
 {
