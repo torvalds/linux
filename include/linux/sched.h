4f311afc2035f7b33d97e69ffaa2e6cd67fca16d
Peter Zijlstra <peterz@infradead.org>
2020-06-10 12:14:09 +0200

    sched/core: Fix CONFIG_GCC_PLUGIN_RANDSTRUCT build fail
    As a temporary build fix, the proper cleanup needs more work.
    
    Reported-by: Guenter Roeck <linux@roeck-us.net>
    Reported-by: Eric Biggers <ebiggers@kernel.org>
    Suggested-by: Eric Biggers <ebiggers@kernel.org>
    Suggested-by: Kees Cook <keescook@chromium.org>
    Fixes: a148866489fb ("sched: Replace rq::wake_list")
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -654,8 +654,10 @@ struct task_struct {
 	unsigned int			ptrace;
 
 #ifdef CONFIG_SMP
-	struct llist_node		wake_entry;
-	unsigned int			wake_entry_type;
+	struct {
+		struct llist_node		wake_entry;
+		unsigned int			wake_entry_type;
+	};
 	int				on_cpu;
 #ifdef CONFIG_THREAD_INFO_IN_TASK
 	/* Current CPU: */
