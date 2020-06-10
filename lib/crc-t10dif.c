29195232fa2f722449c1c6850277b4ca9fa5f781
Eric Biggers <ebiggers@google.com>
2020-06-09 23:39:43 -0700

    crc-t10dif: clean up some more things
    - Correctly compare the algorithm name in crc_t10dif_notify().
    
    - Use proper NOTIFY_* status codes instead of 0.
    
    - Consistently use CRC_T10DIF_STRING instead of "crct10dif" directly.
    
    - Use a proper type for the shash_desc context.
    
    - Use crypto_shash_driver_name() instead of open-coding it.
    
    - Make crc_t10dif_transform_show() use snprintf() rather than sprintf().
      This isn't actually necessary since the buffer has size PAGE_SIZE
      and CRYPTO_MAX_ALG_NAME < PAGE_SIZE, but it's good practice.
    
    - Give the "transform" sysfs file mode 0444 rather than 0644,
      since it doesn't implement a setter method.
    
    - Adjust the module description to not be the same as crct10dif-generic.
    
    Cc: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- lib/crc-t10dif.c
+++ lib/crc-t10dif.c
@@ -26,11 +26,11 @@ static int crc_t10dif_notify(struct notifier_block *self, unsigned long val, voi
 	struct crypto_alg *alg = data;
 
 	if (val != CRYPTO_MSG_ALG_LOADED ||
-	    strncmp(alg->cra_name, CRC_T10DIF_STRING, strlen(CRC_T10DIF_STRING)))
-		return 0;
+	    strcmp(alg->cra_name, CRC_T10DIF_STRING))
+		return NOTIFY_DONE;
 
 	schedule_work(&crct10dif_rehash_work);
-	return 0;
+	return NOTIFY_OK;
 }
 
 static void crc_t10dif_rehash(struct work_struct *work)
@@ -40,7 +40,7 @@ static void crc_t10dif_rehash(struct work_struct *work)
 	mutex_lock(&crc_t10dif_mutex);
 	old = rcu_dereference_protected(crct10dif_tfm,
 					lockdep_is_held(&crc_t10dif_mutex));
-	new = crypto_alloc_shash("crct10dif", 0, 0);
+	new = crypto_alloc_shash(CRC_T10DIF_STRING, 0, 0);
 	if (IS_ERR(new)) {
 		mutex_unlock(&crc_t10dif_mutex);
 		return;
@@ -64,7 +64,7 @@ __u16 crc_t10dif_update(__u16 crc, const unsigned char *buffer, size_t len)
 {
 	struct {
 		struct shash_desc shash;
-		char ctx[2];
+		__u16 crc;
 	} desc;
 	int err;
 
@@ -73,14 +73,13 @@ __u16 crc_t10dif_update(__u16 crc, const unsigned char *buffer, size_t len)
 
 	rcu_read_lock();
 	desc.shash.tfm = rcu_dereference(crct10dif_tfm);
-	*(__u16 *)desc.ctx = crc;
-
+	desc.crc = crc;
 	err = crypto_shash_update(&desc.shash, buffer, len);
 	rcu_read_unlock();
 
 	BUG_ON(err);
 
-	return *(__u16 *)desc.ctx;
+	return desc.crc;
 }
 EXPORT_SYMBOL(crc_t10dif_update);
 
@@ -111,7 +110,6 @@ module_exit(crc_t10dif_mod_fini);
 static int crc_t10dif_transform_show(char *buffer, const struct kernel_param *kp)
 {
 	struct crypto_shash *tfm;
-	const char *name;
 	int len;
 
 	if (static_branch_unlikely(&crct10dif_fallback))
@@ -119,15 +117,15 @@ static int crc_t10dif_transform_show(char *buffer, const struct kernel_param *kp
 
 	rcu_read_lock();
 	tfm = rcu_dereference(crct10dif_tfm);
-	name = crypto_tfm_alg_driver_name(crypto_shash_tfm(tfm));
-	len = sprintf(buffer, "%s\n", name);
+	len = snprintf(buffer, PAGE_SIZE, "%s\n",
+		       crypto_shash_driver_name(tfm));
 	rcu_read_unlock();
 
 	return len;
 }
 
-module_param_call(transform, NULL, crc_t10dif_transform_show, NULL, 0644);
+module_param_call(transform, NULL, crc_t10dif_transform_show, NULL, 0444);
 
-MODULE_DESCRIPTION("T10 DIF CRC calculation");
+MODULE_DESCRIPTION("T10 DIF CRC calculation (library API)");
 MODULE_LICENSE("GPL");
 MODULE_SOFTDEP("pre: crct10dif");
