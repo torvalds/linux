72ef7fe96fd20d3d0e538e165b393819f99870ad
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2020-06-11 10:58:44 +0200

    drm/panfrost: Add compatible string for bifrost
    Mesa now supports some Bifrost devices, so enable it.
    
    Tested-by: Heiko Stuebner <heiko@sntech.de>
    Reviewed-by: Steven Price <steven.price@arm.com>
    Reviewed-by: Heiko Stuebner <heiko@sntech.de>
    Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Signed-off-by: Rob Herring <robh@kernel.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200611085900.49740-2-tomeu.vizoso@collabora.com

--- drivers/gpu/drm/panfrost/panfrost_drv.c
+++ drivers/gpu/drm/panfrost/panfrost_drv.c
@@ -677,6 +677,7 @@ static const struct of_device_id dt_match[] = {
 	{ .compatible = "arm,mali-t830", .data = &default_data, },
 	{ .compatible = "arm,mali-t860", .data = &default_data, },
 	{ .compatible = "arm,mali-t880", .data = &default_data, },
+	{ .compatible = "arm,mali-bifrost", .data = &default_data, },
 	{}
 };
 MODULE_DEVICE_TABLE(of, dt_match);
