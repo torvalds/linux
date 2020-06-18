3499f9ad5d78375856f692ae86e9f5fc50a08993
Ravulapati Vishnu vardhan rao <Vishnuvardhanrao.Ravulapati@amd.com>
2020-06-18 12:56:10 +0530

    ASoC: amd: Removing unnecessary instance initialization
    In trigger we already get the selected instance details
    from runtime->private_data.So, removing the local
    initialization which may corrupt the instance selected
    details and this leads to corrupt data.
    
    Signed-off-by: Ravulapati Vishnu vardhan rao <Vishnuvardhanrao.Ravulapati@amd.com>
    Link: https://lore.kernel.org/r/20200618072624.27047-1-Vishnuvardhanrao.Ravulapati@amd.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/raven/acp3x-i2s.c
+++ sound/soc/amd/raven/acp3x-i2s.c
@@ -151,20 +151,12 @@ static int acp3x_i2s_trigger(struct snd_pcm_substream *substream,
 	struct i2s_stream_instance *rtd;
 	struct snd_soc_pcm_runtime *prtd;
 	struct snd_soc_card *card;
-	struct acp3x_platform_info *pinfo;
 	u32 ret, val, period_bytes, reg_val, ier_val, water_val;
 	u32 buf_size, buf_reg;
 
 	prtd = substream->private_data;
 	rtd = substream->runtime->private_data;
 	card = prtd->card;
-	pinfo = snd_soc_card_get_drvdata(card);
-	if (pinfo) {
-		if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK)
-			rtd->i2s_instance = pinfo->play_i2s_instance;
-		else
-			rtd->i2s_instance = pinfo->cap_i2s_instance;
-	}
 	period_bytes = frames_to_bytes(substream->runtime,
 			substream->runtime->period_size);
 	buf_size = frames_to_bytes(substream->runtime,
