5be206eaac9a68992fc3b06fb5dd5634e323de86
Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
2020-06-23 00:20:20 +0300

    Revert "tpm: selftest: cleanup after unseal with wrong auth/policy test"
    The reverted commit illegitly uses tpm2-tools. External dependencies are
    absolutely forbidden from these tests. There is also the problem that
    clearing is not necessarily wanted behavior if the test/target computer is
    not used only solely for testing.
    
    Fixes: a9920d3bad40 ("tpm: selftest: cleanup after unseal with wrong auth/policy test")
    Cc: Tadeusz Struk <tadeusz.struk@intel.com>
    Cc: stable@vger.kernel.org
    Cc: linux-integrity@vger.kernel.org
    Cc: linux-kselftest@vger.kernel.org
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>

--- tools/testing/selftests/tpm2/test_smoke.sh
+++ tools/testing/selftests/tpm2/test_smoke.sh
@@ -8,8 +8,3 @@ ksft_skip=4
 
 python -m unittest -v tpm2_tests.SmokeTest
 python -m unittest -v tpm2_tests.AsyncTest
-
-CLEAR_CMD=$(which tpm2_clear)
-if [ -n $CLEAR_CMD ]; then
-	tpm2_clear -T device
-fi
