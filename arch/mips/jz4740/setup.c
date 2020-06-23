c211ab5f5e6e304aee43bb5ef6919fbe4e41ef64
Paul Cercueil <paul@crapouillou.net>
2020-06-23 20:24:30 +0200

    MIPS: ingenic: Add support for the JZ4725B SoC
    Add preliminary support for boards based on the JZ4725B SoC from
    Ingenic.
    
    The JZ4725B SoC is supposed to be older than the JZ4740 SoC, but its
    internals are much closer to what can be found on the JZ4750 and newer
    SoCs.
    
    It is low-power SoC with a MIPS32r1 SoC running at ~360 MHz, and no FPU.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/jz4740/setup.c
+++ arch/mips/jz4740/setup.c
@@ -57,6 +57,8 @@ static unsigned long __init get_board_mach_type(const void *fdt)
 		return MACH_INGENIC_JZ4780;
 	if (!fdt_node_check_compatible(fdt, 0, "ingenic,jz4770"))
 		return MACH_INGENIC_JZ4770;
+	if (!fdt_node_check_compatible(fdt, 0, "ingenic,jz4725b"))
+		return MACH_INGENIC_JZ4725B;
 
 	return MACH_INGENIC_JZ4740;
 }
@@ -99,6 +101,8 @@ const char *get_system_type(void)
 		return "JZ4780";
 	case MACH_INGENIC_JZ4770:
 		return "JZ4770";
+	case MACH_INGENIC_JZ4725B:
+		return "JZ4725B";
 	default:
 		return "JZ4740";
 	}
