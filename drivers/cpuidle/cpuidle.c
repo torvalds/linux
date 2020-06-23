81e6737518a1e392ead4e568a4ee70bb7c371458
Chen Yu <yu.c.chen@intel.com>
2020-06-23 14:31:31 +0800

    PM: s2idle: Clear _TIF_POLLING_NRFLAG before suspend to idle
    Suspend to idle was found to not work on Goldmont CPU recently.
    
    The issue happens due to:
    
     1. On Goldmont the CPU in idle can only be woken up via IPIs,
        not POLLING mode, due to commit 08e237fa56a1 ("x86/cpu: Add
        workaround for MONITOR instruction erratum on Goldmont based
        CPUs")
    
     2. When the CPU is entering suspend to idle process, the
        _TIF_POLLING_NRFLAG remains on, because cpuidle_enter_s2idle()
        doesn't match call_cpuidle() exactly.
    
     3. Commit b2a02fc43a1f ("smp: Optimize send_call_function_single_ipi()")
        makes use of _TIF_POLLING_NRFLAG to avoid sending IPIs to idle
        CPUs.
    
     4. As a result, some IPIs related functions might not work
        well during suspend to idle on Goldmont. For example, one
        suspected victim:
    
        tick_unfreeze() -> timekeeping_resume() -> hrtimers_resume()
        -> clock_was_set() -> on_each_cpu() might wait forever,
        because the IPIs will not be sent to the CPUs which are
        sleeping with _TIF_POLLING_NRFLAG set, and Goldmont CPU
        could not be woken up by only setting _TIF_NEED_RESCHED
        on the monitor address.
    
    To avoid that, clear the _TIF_POLLING_NRFLAG flag before invoking
    enter_s2idle_proper() in cpuidle_enter_s2idle() in analogy with the
    call_cpuidle() code flow.
    
    Fixes: b2a02fc43a1f ("smp: Optimize send_call_function_single_ipi()")
    Suggested-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Suggested-by: Rafael J. Wysocki <rafael@kernel.org>
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>
    [ rjw: Subject / changelog ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -13,6 +13,7 @@
 #include <linux/mutex.h>
 #include <linux/sched.h>
 #include <linux/sched/clock.h>
+#include <linux/sched/idle.h>
 #include <linux/notifier.h>
 #include <linux/pm_qos.h>
 #include <linux/cpu.h>
@@ -186,7 +187,7 @@ int cpuidle_enter_s2idle(struct cpuidle_driver *drv, struct cpuidle_device *dev)
 	 * be frozen safely.
 	 */
 	index = find_deepest_state(drv, dev, U64_MAX, 0, true);
-	if (index > 0)
+	if (index > 0 && !current_clr_polling_and_test())
 		enter_s2idle_proper(drv, dev, index);
 
 	return index;
