b4730ae6a443afe611afb4fb651c885c51003c15
Alexander Lobakin <alobakin@pm.me>
2020-06-23 10:43:48 +0000

    net: ethtool: add missing string for NETIF_F_GSO_TUNNEL_REMCSUM
    Commit e585f2363637 ("udp: Changes to udp_offload to support remote
    checksum offload") added new GSO type and a corresponding netdev
    feature, but missed Ethtool's 'netdev_features_strings' table.
    Give it a name so it will be exposed to userspace and become available
    for manual configuration.
    
    v3:
     - decouple from "netdev_features_strings[] cleanup" series;
     - no functional changes.
    
    v2:
     - don't split the "Fixes:" tag across lines;
     - no functional changes.
    
    Fixes: e585f2363637 ("udp: Changes to udp_offload to support remote checksum offload")
    Signed-off-by: Alexander Lobakin <alobakin@pm.me>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ethtool/common.c
+++ net/ethtool/common.c
@@ -40,6 +40,7 @@ const char netdev_features_strings[NETDEV_FEATURE_COUNT][ETH_GSTRING_LEN] = {
 	[NETIF_F_GSO_UDP_TUNNEL_BIT] =	 "tx-udp_tnl-segmentation",
 	[NETIF_F_GSO_UDP_TUNNEL_CSUM_BIT] = "tx-udp_tnl-csum-segmentation",
 	[NETIF_F_GSO_PARTIAL_BIT] =	 "tx-gso-partial",
+	[NETIF_F_GSO_TUNNEL_REMCSUM_BIT] = "tx-tunnel-remcsum-segmentation",
 	[NETIF_F_GSO_SCTP_BIT] =	 "tx-sctp-segmentation",
 	[NETIF_F_GSO_ESP_BIT] =		 "tx-esp-segmentation",
 	[NETIF_F_GSO_UDP_L4_BIT] =	 "tx-udp-segmentation",
