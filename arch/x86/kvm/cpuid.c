2e8cd7a3b82874e9cb3e42d916dda05cd790cc1a
Like Xu <like.xu@linux.intel.com>
2020-06-24 09:59:28 +0800

    kvm: x86: limit the maximum number of vPMU fixed counters to 3
    Some new Intel platforms (such as TGL) already have the
    fourth fixed counter TOPDOWN.SLOTS, but it has not been
    fully enabled on KVM and the host.
    
    Therefore, we limit edx.split.num_counters_fixed to 3,
    so that it does not break the kvm-unit-tests PMU test
    case and bad-handled userspace.
    
    Signed-off-by: Like Xu <like.xu@linux.intel.com>
    Message-Id: <20200624015928.118614-1-like.xu@linux.intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -606,7 +606,7 @@ static inline int __do_cpuid_func(struct kvm_cpuid_array *array, u32 function)
 		eax.split.bit_width = cap.bit_width_gp;
 		eax.split.mask_length = cap.events_mask_len;
 
-		edx.split.num_counters_fixed = cap.num_counters_fixed;
+		edx.split.num_counters_fixed = min(cap.num_counters_fixed, MAX_FIXED_COUNTERS);
 		edx.split.bit_width_fixed = cap.bit_width_fixed;
 		edx.split.reserved = 0;
 
