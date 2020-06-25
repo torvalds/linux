3022c6a1b4b76c40aab9dcdf5142c501f3d3ae8c
Dan Williams <dan.j.williams@intel.com>
2020-06-25 16:51:03 -0700

    driver-core: Introduce DEVICE_ATTR_ADMIN_{RO,RW}
    A common pattern for using plain DEVICE_ATTR() instead of
    DEVICE_ATTR_RO() and DEVICE_ATTR_RW() is for attributes that want to
    limit read to only root.  I.e. many users of DEVICE_ATTR() are
    specifying 0400 or 0600 for permissions.
    
    Given the expectation that CAP_SYS_ADMIN is needed to access these
    sensitive attributes and an explicit helper with the _ADMIN_ identifier
    for DEVICE_ATTR_ADMIN_{RO,RW}.
    
    Cc: "Rafael J. Wysocki" <rafael.j.wysocki@intel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Link: https://lore.kernel.org/r/159312906372.1850128.11611897078988158727.stgit@dwillia2-desk3.amr.corp.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/device.h
+++ include/linux/device.h
@@ -128,8 +128,12 @@ ssize_t device_store_bool(struct device *dev, struct device_attribute *attr,
 		__ATTR_PREALLOC(_name, _mode, _show, _store)
 #define DEVICE_ATTR_RW(_name) \
 	struct device_attribute dev_attr_##_name = __ATTR_RW(_name)
+#define DEVICE_ATTR_ADMIN_RW(_name) \
+	struct device_attribute dev_attr_##_name = __ATTR_RW_MODE(_name, 0600)
 #define DEVICE_ATTR_RO(_name) \
 	struct device_attribute dev_attr_##_name = __ATTR_RO(_name)
+#define DEVICE_ATTR_ADMIN_RO(_name) \
+	struct device_attribute dev_attr_##_name = __ATTR_RO_MODE(_name, 0400)
 #define DEVICE_ATTR_WO(_name) \
 	struct device_attribute dev_attr_##_name = __ATTR_WO(_name)
 #define DEVICE_ULONG_ATTR(_name, _mode, _var) \
