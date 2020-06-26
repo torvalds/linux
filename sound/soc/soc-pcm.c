b6d6e9ea8fb9d477f07e6e4ad8b8a38a47f6870e
Colton Lewis <colton.w.lewis@protonmail.com>
2020-06-26 05:40:24 +0000

    snd/soc: correct trivial kernel-doc inconsistency
    Silence documentation build warning by correcting kernel-doc comment
    for snd_soc_runtime_action.
    
    ./sound/soc/soc-pcm.c:220: warning: Function parameter or member 'action' not described in 'snd_soc_runtime_action'
    
    Signed-off-by: Colton Lewis <colton.w.lewis@protonmail.com>
    Link: https://lore.kernel.org/r/20200626053953.68797-1-colton.w.lewis@protonmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/soc-pcm.c
+++ sound/soc/soc-pcm.c
@@ -208,6 +208,7 @@ static inline void dpcm_remove_debugfs_state(struct snd_soc_dpcm *dpcm)
  * PCM runtime components
  * @rtd: ASoC PCM runtime that is activated
  * @stream: Direction of the PCM stream
+ * @action: Activate stream if 1. Deactivate if -1.
  *
  * Increments/Decrements the active count for all the DAIs and components
  * attached to a PCM runtime.
