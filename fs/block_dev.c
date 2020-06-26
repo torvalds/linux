5ff9f19231a0e670b3d79c563f1b0b185abeca91
Christoph Hellwig <hch@lst.de>
2020-06-26 10:01:54 +0200

    block: simplify set_init_blocksize
    The loop to increase the initial block size doesn't really make any
    sense, as the AND operation won't match for powers of two if it didn't
    for the initial block size.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/block_dev.c
+++ fs/block_dev.c
@@ -106,13 +106,7 @@ EXPORT_SYMBOL(invalidate_bdev);
 static void set_init_blocksize(struct block_device *bdev)
 {
 	unsigned bsize = bdev_logical_block_size(bdev);
-	loff_t size = i_size_read(bdev->bd_inode);
 
-	while (bsize < PAGE_SIZE) {
-		if (size & bsize)
-			break;
-		bsize <<= 1;
-	}
 	bdev->bd_block_size = bsize;
 	bdev->bd_inode->i_blkbits = blksize_bits(bsize);
 }
