0ee2872f105b997ba5f09f7fdae542e4cbc1d676
Sven Van Asbroeck <thesven73@gmail.com>
2020-06-29 15:53:06 -0400

    mtd: spi-nor: winbond: Add support for w25q64jvm
    This chip is (nearly) identical to the Winbond w25q64 which is
    already supported by Linux. Compared to the w25q64, the 'jvm'
    has a different JEDEC ID.
    
    Signed-off-by: Sven Van Asbroeck <thesven73@gmail.com>
    [tudor.ambarus@microchip.com: Order entry alphabetically, update
    subject, update Sven's email address]
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Link: https://lore.kernel.org/r/20200629195306.1030-1-TheSven73@gmail.com

--- drivers/mtd/spi-nor/winbond.c
+++ drivers/mtd/spi-nor/winbond.c
@@ -68,6 +68,7 @@ static const struct flash_info winbond_parts[] = {
 	{ "w25q64dw", INFO(0xef6017, 0, 64 * 1024, 128,
 			   SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
 			   SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB) },
+	{ "w25q64jvm", INFO(0xef7017, 0, 64 * 1024, 128, SECT_4K) },
 	{ "w25q128fw", INFO(0xef6018, 0, 64 * 1024, 256,
 			    SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
 			    SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB) },
