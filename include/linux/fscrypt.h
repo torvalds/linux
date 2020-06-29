6883a1e06ba8720b828e229b795aa9280fa2fb70
Eric Biggers <ebiggers@google.com>
2020-06-29 09:15:36 -0700

    Revert "ANDROID: fscrypt: add key removal notifier chain"
    This reverts commit 98261f39e992543a404353ba2d8f36cb739f329f
    because it was only needed for sdcardfs, which has now been removed.
    
    Bug: 157700134
    Bug: 142275883
    Change-Id: Iaf22521ef47d48ce9c9c0604a8b2412e3b8c1faa
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- include/linux/fscrypt.h
+++ include/linux/fscrypt.h
@@ -177,8 +177,6 @@ int fscrypt_ioctl_add_key(struct file *filp, void __user *arg);
 int fscrypt_ioctl_remove_key(struct file *filp, void __user *arg);
 int fscrypt_ioctl_remove_key_all_users(struct file *filp, void __user *arg);
 int fscrypt_ioctl_get_key_status(struct file *filp, void __user *arg);
-int fscrypt_register_key_removal_notifier(struct notifier_block *nb);
-int fscrypt_unregister_key_removal_notifier(struct notifier_block *nb);
 
 /* keysetup.c */
 int fscrypt_get_encryption_info(struct inode *inode);
@@ -384,18 +382,6 @@ static inline int fscrypt_ioctl_get_key_status(struct file *filp,
 					       void __user *arg)
 {
 	return -EOPNOTSUPP;
-}
-
-static inline int fscrypt_register_key_removal_notifier(
-						struct notifier_block *nb)
-{
-	return 0;
-}
-
-static inline int fscrypt_unregister_key_removal_notifier(
-						struct notifier_block *nb)
-{
-	return 0;
 }
 
 /* keysetup.c */
