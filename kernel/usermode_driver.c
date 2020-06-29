33c326014fe69304244868cc793c2c77be533125
Eric W. Biederman <ebiederm@xmission.com>
2020-06-29 08:28:33 -0500

    umd: Stop using split_argv
    There is exactly one argument so there is nothing to split.  All
    split_argv does now is cause confusion and avoid the need for a cast
    when passing a "const char *" string to call_usermodehelper_setup.
    
    So avoid confusion and the possibility of an odd driver name causing
    problems by just using a fixed argv array with a cast in the call to
    call_usermodehelper_setup.
    
    v1: https://lkml.kernel.org/r/87sged3a9n.fsf_-_@x220.int.ebiederm.org
    Link: https://lkml.kernel.org/r/20200702164140.4468-16-ebiederm@xmission.com
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Tested-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- kernel/usermode_driver.c
+++ kernel/usermode_driver.c
@@ -160,27 +160,21 @@ static void umd_cleanup(struct subprocess_info *info)
 int fork_usermode_driver(struct umd_info *info)
 {
 	struct subprocess_info *sub_info;
-	char **argv = NULL;
+	const char *argv[] = { info->driver_name, NULL };
 	int err;
 
 	if (WARN_ON_ONCE(info->tgid))
 		return -EBUSY;
 
 	err = -ENOMEM;
-	argv = argv_split(GFP_KERNEL, info->driver_name, NULL);
-	if (!argv)
-		goto out;
-
-	sub_info = call_usermodehelper_setup(info->driver_name, argv, NULL,
-					     GFP_KERNEL,
+	sub_info = call_usermodehelper_setup(info->driver_name,
+					     (char **)argv, NULL, GFP_KERNEL,
 					     umd_setup, umd_cleanup, info);
 	if (!sub_info)
 		goto out;
 
 	err = call_usermodehelper_exec(sub_info, UMH_WAIT_EXEC);
 out:
-	if (argv)
-		argv_free(argv);
 	return err;
 }
 EXPORT_SYMBOL_GPL(fork_usermode_driver);
