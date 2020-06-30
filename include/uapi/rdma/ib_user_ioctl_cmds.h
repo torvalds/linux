6c01e6b218aea09ec9947cbf88a4db97b4dd155c
Yishai Hadas <yishaih@mellanox.com>
2020-06-30 12:39:16 +0300

    IB/uverbs: Expose UAPI to query MR
    Expose UAPI to query MR, this will let user space application that
    didn't allocate the MR but has access to by owning the matching command
    FD to retrieve its information.
    
    Link: https://lore.kernel.org/r/20200630093916.332097-8-leon@kernel.org
    Signed-off-by: Yishai Hadas <yishaih@mellanox.com>
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- include/uapi/rdma/ib_user_ioctl_cmds.h
+++ include/uapi/rdma/ib_user_ioctl_cmds.h
@@ -248,6 +248,7 @@ enum uverbs_methods_mr {
 	UVERBS_METHOD_DM_MR_REG,
 	UVERBS_METHOD_MR_DESTROY,
 	UVERBS_METHOD_ADVISE_MR,
+	UVERBS_METHOD_QUERY_MR,
 };
 
 enum uverbs_attrs_mr_destroy_ids {
@@ -259,6 +260,14 @@ enum uverbs_attrs_advise_mr_cmd_attr_ids {
 	UVERBS_ATTR_ADVISE_MR_ADVICE,
 	UVERBS_ATTR_ADVISE_MR_FLAGS,
 	UVERBS_ATTR_ADVISE_MR_SGE_LIST,
+};
+
+enum uverbs_attrs_query_mr_cmd_attr_ids {
+	UVERBS_ATTR_QUERY_MR_HANDLE,
+	UVERBS_ATTR_QUERY_MR_RESP_LKEY,
+	UVERBS_ATTR_QUERY_MR_RESP_RKEY,
+	UVERBS_ATTR_QUERY_MR_RESP_LENGTH,
+	UVERBS_ATTR_QUERY_MR_RESP_IOVA,
 };
 
 enum uverbs_attrs_create_counters_cmd_attr_ids {
