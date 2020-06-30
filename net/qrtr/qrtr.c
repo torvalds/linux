28541f3d324f6de1e545e2875283b6cef95c5d36
Carl Huang <cjhuang@codeaurora.org>
2020-06-30 14:52:51 +0800

    net: qrtr: free flow in __qrtr_node_release
    The flow is allocated in qrtr_tx_wait, but not freed when qrtr node
    is released. (*slot) becomes NULL after radix_tree_iter_delete is
    called in __qrtr_node_release. The fix is to save (*slot) to a
    vairable and then free it.
    
    This memory leak is catched when kmemleak is enabled in kernel,
    the report looks like below:
    
    unreferenced object 0xffffa0de69e08420 (size 32):
      comm "kworker/u16:3", pid 176, jiffies 4294918275 (age 82858.876s)
      hex dump (first 32 bytes):
        00 00 00 00 00 00 00 00 28 84 e0 69 de a0 ff ff  ........(..i....
        28 84 e0 69 de a0 ff ff 03 00 00 00 00 00 00 00  (..i............
      backtrace:
        [<00000000e252af0a>] qrtr_node_enqueue+0x38e/0x400 [qrtr]
        [<000000009cea437f>] qrtr_sendmsg+0x1e0/0x2a0 [qrtr]
        [<000000008bddbba4>] sock_sendmsg+0x5b/0x60
        [<0000000003beb43a>] qmi_send_message.isra.3+0xbe/0x110 [qmi_helpers]
        [<000000009c9ae7de>] qmi_send_request+0x1c/0x20 [qmi_helpers]
    
    Signed-off-by: Carl Huang <cjhuang@codeaurora.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/qrtr/qrtr.c
+++ net/qrtr/qrtr.c
@@ -166,6 +166,7 @@ static void __qrtr_node_release(struct kref *kref)
 {
 	struct qrtr_node *node = container_of(kref, struct qrtr_node, ref);
 	struct radix_tree_iter iter;
+	struct qrtr_tx_flow *flow;
 	unsigned long flags;
 	void __rcu **slot;
 
@@ -181,8 +182,9 @@ static void __qrtr_node_release(struct kref *kref)
 
 	/* Free tx flow counters */
 	radix_tree_for_each_slot(slot, &node->qrtr_tx_flow, &iter, 0) {
+		flow = *slot;
 		radix_tree_iter_delete(&node->qrtr_tx_flow, &iter, slot);
-		kfree(*slot);
+		kfree(flow);
 	}
 	kfree(node);
 }
