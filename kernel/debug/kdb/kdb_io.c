fcdb84cc5b31c2f4e333051f467264f5821bc61a
Cengiz Can <cengiz@kernel.wtf>
2020-06-30 11:29:23 +0300

    kdb: remove unnecessary null check of dbg_io_ops
    `kdb_msg_write` operates on a global `struct kgdb_io *` called
    `dbg_io_ops`.
    
    It's initialized in `debug_core.c` and checked throughout the debug
    flow.
    
    There's a null check in `kdb_msg_write` which triggers static analyzers
    and gives the (almost entirely wrong) impression that it can be null.
    
    Coverity scanner caught this as CID 1465042.
    
    I have removed the unnecessary null check and eliminated false-positive
    forward null dereference warning.
    
    Signed-off-by: Cengiz Can <cengiz@kernel.wtf>
    Link: https://lore.kernel.org/r/20200630082922.28672-1-cengiz@kernel.wtf
    Reviewed-by: Sumit Garg <sumit.garg@linaro.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Tested-by: Douglas Anderson <dianders@chromium.org>
    Signed-off-by: Daniel Thompson <daniel.thompson@linaro.org>

--- kernel/debug/kdb/kdb_io.c
+++ kernel/debug/kdb/kdb_io.c
@@ -545,18 +545,18 @@ static int kdb_search_string(char *searched, char *searchfor)
 static void kdb_msg_write(const char *msg, int msg_len)
 {
 	struct console *c;
+	const char *cp;
+	int len;
 
 	if (msg_len == 0)
 		return;
 
-	if (dbg_io_ops) {
-		const char *cp = msg;
-		int len = msg_len;
+	cp = msg;
+	len = msg_len;
 
-		while (len--) {
-			dbg_io_ops->write_char(*cp);
-			cp++;
-		}
+	while (len--) {
+		dbg_io_ops->write_char(*cp);
+		cp++;
 	}
 
 	for_each_console(c) {
