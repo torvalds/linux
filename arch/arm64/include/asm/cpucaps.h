975ebc7b68994a47c436d28a885377f45b3c17bc
Will Deacon <will@kernel.org>
2020-06-30 14:02:22 +0100

    BACKPORT: arm64: cpufeatures: Add capability for LDAPR instruction
    Armv8.3 introduced the LDAPR instruction, which provides weaker memory
    ordering semantics than LDARi (RCpc vs RCsc). Generally, we provide an
    RCsc implementation when implementing the Linux memory model, but LDAPR
    can be used as a useful alternative to dependency ordering, particularly
    when the compiler is capable of breaking the dependencies.
    
    Since LDAPR is not available on all CPUs, add a cpufeature to detect it at
    runtime and allow the instruction to be used with alternative code
    patching.
    
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit 364a5a8ae8dc2dd457e2fefb4da3f3fd2c0ba8b1)
    Signed-off-by: Will Deacon <willdeacon@google.com>
    [will: Fixed trivial cpucaps conflict with ARM64_KVM_PROTECTED_MODE]
    Bug: 145210207
    Change-Id: Ie0c9515dae382c8de49f793b8d40fef2f40dfeda

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -66,8 +66,9 @@
 #define ARM64_HAS_TLB_RANGE			56
 #define ARM64_MTE				57
 #define ARM64_WORKAROUND_1508412		58
-#define ARM64_KVM_PROTECTED_MODE		59
+#define ARM64_HAS_LDAPR				59
+#define ARM64_KVM_PROTECTED_MODE		60
 
-#define ARM64_NCAPS				60
+#define ARM64_NCAPS				61
 
 #endif /* __ASM_CPUCAPS_H */
