bbeba3e58f040a4297a5ba88ebf6e2b16adc3657
Steven Rostedt (VMware) <rostedt@goodmis.org>
2020-06-30 13:05:29 -0400

    ring-buffer: Call trace_clock_local() directly for RETPOLINE kernels
    After doing some benchmarks and examining the code, I found that the ring
    buffer clock calls were quite expensive, and noticed that it uses
    retpolines. This is because the ring buffer clock is programmable, and can
    be set. But in most cases it simply uses the fastest ns unit clock which is
    the trace_clock_local(). For RETPOLINE builds, checking if the ring buffer
    clock is set to trace_clock_local() and then calling it directly has brought
    the time of an event on my i7 box from an average of 93 nanoseconds an event
    down to 83 nanoseconds an event, and the minimum time from 81 nanoseconds to
    68 nanoseconds!
    
    Suggested-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/trace/ring_buffer.c
+++ kernel/trace/ring_buffer.c
@@ -970,8 +970,16 @@ __poll_t ring_buffer_poll_wait(struct trace_buffer *buffer, int cpu,
 
 static inline u64 rb_time_stamp(struct trace_buffer *buffer)
 {
+	u64 ts;
+
+	/* Skip retpolines :-( */
+	if (IS_ENABLED(CONFIG_RETPOLINE) && likely(buffer->clock == trace_clock_local))
+		ts = trace_clock_local();
+	else
+		ts = buffer->clock();
+
 	/* shift to debug/test normalization and TIME_EXTENTS */
-	return buffer->clock() << DEBUG_SHIFT;
+	return ts << DEBUG_SHIFT;
 }
 
 u64 ring_buffer_time_stamp(struct trace_buffer *buffer, int cpu)
