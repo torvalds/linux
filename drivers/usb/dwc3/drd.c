7679defc3e290d4cb8377f8fc2d8b6f3cca32eb2
Lee Jones <lee.jones@linaro.org>
2020-07-02 15:46:00 +0100

    usb: dwc3: drd: File headers are not doc headers
    Demote drd.c's file header to a standard comment block.
    
    Fixes the following W=1 build warnings:
    
     drivers/usb/dwc3/drd.c:20: warning: Function parameter or member 'dwc' not described in 'dwc3_otg_disable_events'
     drivers/usb/dwc3/drd.c:20: warning: Function parameter or member 'disable_mask' not described in 'dwc3_otg_disable_events'
    
    Cc: Felipe Balbi <balbi@kernel.org>
    Cc: Roger Quadros <rogerq@ti.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Link: https://lore.kernel.org/r/20200702144625.2533530-6-lee.jones@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/dwc3/drd.c
+++ drivers/usb/dwc3/drd.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
-/**
+/*
  * drd.c - DesignWare USB3 DRD Controller Dual-role support
  *
  * Copyright (C) 2017 Texas Instruments Incorporated - http://www.ti.com
