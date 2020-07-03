38931aee2a1218f29841381b5fe2014adddc19d0
Jon Lin <jon.lin@rock-chips.com>
2020-07-03 19:26:48 +0800

    Revert "spi: rockchip: add SPI slave support"
    This reverts commit 87ec170ff74449011531585e26dc4c6ea887b0e3 which is
    commit d065f41a3f0d0974593122e4864e158bb945696e upstream.
    
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>
    Change-Id: If48d08ec8e51829bb6bf8050d377c7060b75e140

--- drivers/spi/spi-rockchip.c
+++ drivers/spi/spi-rockchip.c
@@ -552,8 +552,6 @@ static void rockchip_spi_config(struct rockchip_spi *rs)
 		cr0 |= (1 << CR0_FBM_OFFSET);/* First Bit Mode */
 	cr0 |= (rs->tmode << CR0_XFM_OFFSET);
 	cr0 |= (rs->type << CR0_FRF_OFFSET);
-	if (spi_controller_is_slave(rs->master))
-		cr0 |= (CR0_OPM_SLAVE << CR0_OPM_OFFSET);
 
 	if (rs->use_dma) {
 		if (rs->tx)
@@ -715,12 +713,7 @@ static int rockchip_spi_probe(struct platform_device *pdev)
 	struct resource *mem;
 	u32 rsd_nsecs;
 
-	if (of_property_read_bool(pdev->dev.of_node, "spi-slave"))
-		master = spi_alloc_slave(&pdev->dev,
-					 sizeof(struct rockchip_spi));
-	else
-		master = spi_alloc_master(&pdev->dev,
-					  sizeof(struct rockchip_spi));
+	master = spi_alloc_master(&pdev->dev, sizeof(struct rockchip_spi));
 	if (!master)
 		return -ENOMEM;
 
