e1c08cf23172ed6fb228d75efc9f4c80a6812116
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2020-07-04 00:50:43 +0200

    usb: dwc2: Add missing cleanups when usb_add_gadget_udc() fails
    Call dwc2_debugfs_exit() and dwc2_hcd_remove() (if the HCD was enabled
    earlier) when usb_add_gadget_udc() has failed. This ensures that the
    debugfs entries created by dwc2_debugfs_init() as well as the HCD are
    cleaned up in the error path.
    
    Fixes: 207324a321a866 ("usb: dwc2: Postponed gadget registration to the udc class driver")
    Acked-by: Minas Harutyunyan <hminas@synopsys.com>
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Signed-off-by: Felipe Balbi <balbi@kernel.org>

--- drivers/usb/dwc2/platform.c
+++ drivers/usb/dwc2/platform.c
@@ -584,12 +584,16 @@ static int dwc2_driver_probe(struct platform_device *dev)
 		if (retval) {
 			hsotg->gadget.udc = NULL;
 			dwc2_hsotg_remove(hsotg);
-			goto error_init;
+			goto error_debugfs;
 		}
 	}
 #endif /* CONFIG_USB_DWC2_PERIPHERAL || CONFIG_USB_DWC2_DUAL_ROLE */
 	return 0;
 
+error_debugfs:
+	dwc2_debugfs_exit(hsotg);
+	if (hsotg->hcd_enabled)
+		dwc2_hcd_remove(hsotg);
 error_init:
 	if (hsotg->params.activate_stm_id_vb_detection)
 		regulator_disable(hsotg->usb33d);
