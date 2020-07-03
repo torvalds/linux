ae8eb8ba521c8e72e9e5504765929e55c038ee3a
Chanwoo Choi <cw00.choi@samsung.com>
2020-07-03 15:04:50 +0900

    memory: samsung: exynos5422-dmc: Use delayed timer as default
    Use delayed timer as default instead of deferrable timer
    in order to monitor the DMC status regardless of CPU idle.
    
    Reviewed-by: Krzysztof Kozlowski <krzk@kernel.org>
    Reviewed-by: Lukasz Luba <lukasz.luba@arm.com>
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- drivers/memory/samsung/exynos5422-dmc.c
+++ drivers/memory/samsung/exynos5422-dmc.c
@@ -945,6 +945,7 @@ static int exynos5_dmc_get_cur_freq(struct device *dev, unsigned long *freq)
  * It provides to the devfreq framework needed functions and polling period.
  */
 static struct devfreq_dev_profile exynos5_dmc_df_profile = {
+	.timer = DEVFREQ_TIMER_DELAYED,
 	.target = exynos5_dmc_target,
 	.get_dev_status = exynos5_dmc_get_status,
 	.get_cur_freq = exynos5_dmc_get_cur_freq,
