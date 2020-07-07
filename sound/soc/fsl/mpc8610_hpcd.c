a5911ac5790acaf98c929b826b3f7b4a438f9759
Liao Pingfang <liao.pingfang@zte.com.cn>
2020-07-07 19:47:47 +0800

    ASoC: fsl: mpc8610_hpcd: Add missing of_node_put()
    After finishing using device node got from of_find_compatible_node(),
    of_node_put() needs to be called.
    
    Signed-off-by: Liao Pingfang <liao.pingfang@zte.com.cn>
    Link: https://lore.kernel.org/r/1594122467-11615-1-git-send-email-wang.yi59@zte.com.cn
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/fsl/mpc8610_hpcd.c
+++ sound/soc/fsl/mpc8610_hpcd.c
@@ -426,9 +426,11 @@ static int __init mpc8610_hpcd_init(void)
 	guts_np = of_find_compatible_node(NULL, NULL, "fsl,mpc8610-guts");
 	if (of_address_to_resource(guts_np, 0, &res)) {
 		pr_err("mpc8610-hpcd: missing/invalid global utilities node\n");
+		of_node_put(guts_np);
 		return -EINVAL;
 	}
 	guts_phys = res.start;
+	of_node_put(guts_np);
 
 	return platform_driver_register(&mpc8610_hpcd_driver);
 }
