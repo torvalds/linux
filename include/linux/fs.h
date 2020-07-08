c843843e714c8f17280d7db009412b1b1baf448b
Daniel Rosenberg <drosen@google.com>
2020-07-08 02:12:35 -0700

    fs: Add standard casefolding support
    This adds general supporting functions for filesystems that use
    utf8 casefolding. It provides standard dentry_operations and adds the
    necessary structures in struct super_block to allow this standardization.
    
    The new dentry operations are functionally equivalent to the existing
    operations in ext4 and f2fs, apart from the use of utf8_casefold_hash to
    avoid an allocation.
    
    By providing a common implementation, all users can benefit from any
    optimizations without needing to port over improvements.
    
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Reviewed-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1371,6 +1371,12 @@ extern int send_sigurg(struct fown_struct *fown);
 #define SB_ACTIVE	(1<<30)
 #define SB_NOUSER	(1<<31)
 
+/* These flags relate to encoding and casefolding */
+#define SB_ENC_STRICT_MODE_FL	(1 << 0)
+
+#define sb_has_strict_encoding(sb) \
+	(sb->s_encoding_flags & SB_ENC_STRICT_MODE_FL)
+
 /*
  *	Umount options
  */
@@ -1440,6 +1446,10 @@ struct super_block {
 #endif
 #ifdef CONFIG_FS_VERITY
 	const struct fsverity_operations *s_vop;
+#endif
+#ifdef CONFIG_UNICODE
+	struct unicode_map *s_encoding;
+	__u16 s_encoding_flags;
 #endif
 	struct hlist_bl_head	s_roots;	/* alternate root dentries for NFS */
 	struct list_head	s_mounts;	/* list of mounts; _not_ for fs use */
@@ -3261,6 +3271,12 @@ extern int __generic_file_fsync(struct file *, loff_t, loff_t, int);
 extern int generic_file_fsync(struct file *, loff_t, loff_t, int);
 
 extern int generic_check_addressable(unsigned, u64);
+
+#ifdef CONFIG_UNICODE
+extern int generic_ci_d_hash(const struct dentry *dentry, struct qstr *str);
+extern int generic_ci_d_compare(const struct dentry *dentry, unsigned int len,
+				const char *str, const struct qstr *name);
+#endif
 
 #ifdef CONFIG_MIGRATION
 extern int buffer_migrate_page(struct address_space *,
