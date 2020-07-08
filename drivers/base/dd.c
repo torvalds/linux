fe940d7362e6d7a5e5086581d0462e00ba766992
Lukas Wunner <lukas@wunner.de>
2020-07-08 14:12:22 +0200

    driver core: Drop mention of obsolete bus rwsem from kernel-doc
    15 years ago, commit 6eded061b126 ("Fix up bus code and remove use of
    rwsem") removed the bus rwsem, but left over a reference to it in a
    kernel-doc comment.  Drop it.
    
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Link: https://lore.kernel.org/r/b1af31b0e351bcbc056fe1ec44500737a7998d43.1594210157.git.lukas@wunner.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/dd.c
+++ drivers/base/dd.c
@@ -430,10 +430,9 @@ static void driver_sysfs_remove(struct device *dev)
  * Allow manual attachment of a driver to a device.
  * Caller must have already set @dev->driver.
  *
- * Note that this does not modify the bus reference count
- * nor take the bus's rwsem. Please verify those are accounted
- * for before calling this. (It is ok to call with no other effort
- * from a driver's probe() method.)
+ * Note that this does not modify the bus reference count.
+ * Please verify that is accounted for before calling this.
+ * (It is ok to call with no other effort from a driver's probe() method.)
  *
  * This function must be called with the device lock held.
  */
