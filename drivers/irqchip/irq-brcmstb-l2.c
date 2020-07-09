c8d8d6fc478a30f3e8ea5372664dd2a808c4311e
Justin Chen <justinpopo6@gmail.com>
2020-07-09 15:30:12 -0700

    irqchip/brcmstb-l2: Set controller as wake-up source
    Utilize the Broadcom interrupt controller standard property
    "brcm,irq-can-wake" to flag whether this particular interrupt controller
    instance is wake-up capable.
    
    Since we do not know what type of parent interrupt controller we are
    interfaced with, ensure that enable_irq_wake() is called early on.
    
    Signed-off-by: Justin Chen <justinpopo6@gmail.com>
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20200709223016.989-3-f.fainelli@gmail.com

--- drivers/irqchip/irq-brcmstb-l2.c
+++ drivers/irqchip/irq-brcmstb-l2.c
@@ -254,6 +254,7 @@ static int __init brcmstb_l2_intc_of_init(struct device_node *np,
 		 */
 		data->gc->wake_enabled = 0xffffffff;
 		ct->chip.irq_set_wake = irq_gc_set_wake;
+		enable_irq_wake(parent_irq);
 	}
 
 	pr_info("registered L2 intc (%pOF, parent irq: %d)\n", np, parent_irq);
