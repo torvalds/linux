2781ec63dfd2ff93fb5e6800d8927070b11b55c1
Chen-Yu Tsai <wens@csie.org>
2020-07-11 09:10:30 +0800

    drm: sun4i: hdmi: Fix inverted HPD result
    [ Upstream commit baa1841eb797eadce6c907bdaed7cd6f01815404 ]
    
    When the extra HPD polling in sun4i_hdmi was removed, the result of
    HPD was accidentally inverted.
    
    Fix this by inverting the check.
    
    Fixes: bda8eaa6dee7 ("drm: sun4i: hdmi: Remove extra HPD polling")
    Signed-off-by: Chen-Yu Tsai <wens@csie.org>
    Tested-by: Mans Rullgard <mans@mansr.com>
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200711011030.21997-1-wens@kernel.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/sun4i/sun4i_hdmi_enc.c
+++ drivers/gpu/drm/sun4i/sun4i_hdmi_enc.c
@@ -243,7 +243,7 @@ sun4i_hdmi_connector_detect(struct drm_connector *connector, bool force)
 	unsigned long reg;
 
 	reg = readl(hdmi->base + SUN4I_HDMI_HPD_REG);
-	if (reg & SUN4I_HDMI_HPD_HIGH) {
+	if (!(reg & SUN4I_HDMI_HPD_HIGH)) {
 		cec_phys_addr_invalidate(hdmi->cec_adap);
 		return connector_status_disconnected;
 	}
