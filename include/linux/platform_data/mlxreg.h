17727a3b4879324818ea6f2ebc3f68432173ce24
Vadim Pasternak <vadimp@mellanox.com>
2020-07-14 15:02:02 +0300

    platform_data/mlxreg: Add presence register field for FAN devices
    Add new field 'reg_prsnt' to the structure 'mlxreg_core_data' to
    provide the number FAN drawers equpped within the system. The purpose
    is to allow mapping between FAN drawers and FAN rotors (tachometer),
    since FAN drawer can be eqipped with a few rotors.
    
    Signed-off-by: Vadim Pasternak <vadimp@mellanox.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- include/linux/platform_data/mlxreg.h
+++ include/linux/platform_data/mlxreg.h
@@ -75,6 +75,7 @@ struct mlxreg_hotplug_device {
  * @mask: attribute access mask;
  * @bit: attribute effective bit;
  * @capability: attribute capability register;
+ * @reg_prsnt: attribute presence register;
  * @mode: access mode;
  * @np - pointer to node platform associated with attribute;
  * @hpdev - hotplug device data;
@@ -88,6 +89,7 @@ struct mlxreg_core_data {
 	u32 mask;
 	u32 bit;
 	u32 capability;
+	u32 reg_prsnt;
 	umode_t	mode;
 	struct device_node *np;
 	struct mlxreg_hotplug_device hpdev;
