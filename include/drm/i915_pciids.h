32d4ec9a1681de020a5c70c1c57b0161cd9982c2
Ville Syrjälä <ville.syrjala@linux.intel.com>
2020-07-16 20:21:03 +0300

    drm/i915: Sort CFL PCI IDs
    Sort the CFL PCI IDs numerically. Some order seems better than
    randomness.
    
    Cc: Alexei Podtelezhnikov <apodtele@gmail.com>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200716172106.2656-12-ville.syrjala@linux.intel.com
    Reviewed-by: Anusha Srivatsa <anusha.srivatsa@intel.com>

--- include/drm/i915_pciids.h
+++ include/drm/i915_pciids.h
@@ -497,8 +497,8 @@
 	INTEL_VGA_DEVICE(0x3E9C, info)
 
 #define INTEL_CFL_H_GT2_IDS(info) \
-	INTEL_VGA_DEVICE(0x3E9B, info), /* Halo GT2 */ \
-	INTEL_VGA_DEVICE(0x3E94, info)  /* Halo GT2 */
+	INTEL_VGA_DEVICE(0x3E94, info),  /* Halo GT2 */ \
+	INTEL_VGA_DEVICE(0x3E9B, info) /* Halo GT2 */
 
 /* CFL U GT2 */
 #define INTEL_CFL_U_GT2_IDS(info) \
