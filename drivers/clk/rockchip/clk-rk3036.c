1acaeb727938f75ac504de5c8cb11e6b2d3b5725
Elaine Zhang <zhangqing@rock-chips.com>
2020-07-16 17:37:51 +0800

    clk: rockchip: rk3036: support driver build as tristate module
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>
    Change-Id: I281ae6074a52e01832b357363b92191efadecff9

--- drivers/clk/rockchip/clk-rk3036.c
+++ drivers/clk/rockchip/clk-rk3036.c
@@ -9,8 +9,10 @@
 
 #include <linux/clk-provider.h>
 #include <linux/io.h>
+#include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
+#include <linux/of_device.h>
 #include <linux/syscore_ops.h>
 #include <dt-bindings/clock/rk3036-cru.h>
 #include "clk.h"
@@ -505,3 +507,31 @@ static void __init rk3036_clk_init(struct device_node *np)
 	}
 }
 CLK_OF_DECLARE(rk3036_cru, "rockchip,rk3036-cru", rk3036_clk_init);
+
+static int __init clk_rk3036_probe(struct platform_device *pdev)
+{
+	struct device_node *np = pdev->dev.of_node;
+
+	rk3036_clk_init(np);
+
+	return 0;
+}
+
+static const struct of_device_id clk_rk3036_match_table[] = {
+	{
+		.compatible = "rockchip,rk3036-cru",
+	},
+	{ }
+};
+MODULE_DEVICE_TABLE(of, clk_rk3036_match_table);
+
+static struct platform_driver clk_rk3036_driver = {
+	.driver		= {
+		.name	= "clk-rk3036",
+		.of_match_table = clk_rk3036_match_table,
+	},
+};
+builtin_platform_driver_probe(clk_rk3036_driver, clk_rk3036_probe);
+
+MODULE_DESCRIPTION("Rockchip RK3036 Clock Driver");
+MODULE_LICENSE("GPL");
