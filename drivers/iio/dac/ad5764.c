d808988e7dc16d3cbac5cf2f46985f0267589421
Lee Jones <lee.jones@linaro.org>
2020-07-16 14:59:23 +0100

    iio: dac: ad5764: Fix misdocumenting and formatting error
    Attribute descriptions must match the name exactly.
    
    Kerneldoc expects attributes/parameters to be in '@*.: ' format.
    
    Fixes the following W=1 kernel build warning(s):
    
     drivers/iio/dac/ad5764.c:42: warning: Function parameter or member 'channels' not described in 'ad5764_chip_info'
     drivers/iio/dac/ad5764.c:67: warning: Function parameter or member 'lock' not described in 'ad5764_state'
    
    Cc: Michael Hennerich <Michael.Hennerich@analog.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/dac/ad5764.c
+++ drivers/iio/dac/ad5764.c
@@ -33,9 +33,8 @@
  * struct ad5764_chip_info - chip specific information
  * @int_vref:	Value of the internal reference voltage in uV - 0 if external
  *		reference voltage is used
- * @channel	channel specification
+ * @channels:	channel specification
 */
-
 struct ad5764_chip_info {
 	unsigned long int_vref;
 	const struct iio_chan_spec *channels;
@@ -46,7 +45,7 @@ struct ad5764_chip_info {
  * @spi:		spi_device
  * @chip_info:		chip info
  * @vref_reg:		vref supply regulators
- * @lock		lock to protect the data buffer during SPI ops
+ * @lock:		lock to protect the data buffer during SPI ops
  * @data:		spi transfer buffers
  */
 
