f97dbf48ca43009e8b8bcdf07f47fc9f06149b36
Saravana Kannan <saravanak@google.com>
2020-07-17 17:06:36 -0700

    irqchip/mtk-sysirq: Convert to a platform driver
    This driver can work as a platform driver. So covert it to a platform
    driver.
    
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Reviewed-by: Hanks Chen <hanks.chen@mediatek.com>
    Link: https://lore.kernel.org/r/20200718000637.3632841-4-saravanak@google.com

--- drivers/irqchip/irq-mtk-sysirq.c
+++ drivers/irqchip/irq-mtk-sysirq.c
@@ -231,4 +231,6 @@ out_free_chip:
 	kfree(chip_data);
 	return ret;
 }
-IRQCHIP_DECLARE(mtk_sysirq, "mediatek,mt6577-sysirq", mtk_sysirq_of_init);
+IRQCHIP_PLATFORM_DRIVER_BEGIN(mtk_sysirq)
+IRQCHIP_MATCH("mediatek,mt6577-sysirq", mtk_sysirq_of_init)
+IRQCHIP_PLATFORM_DRIVER_END(mtk_sysirq)
