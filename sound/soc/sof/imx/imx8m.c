f23a8e9402f899e3555ac8aff3d3d050adafe39e
Daniel Baluta <daniel.baluta@nxp.com>
2020-07-20 10:20:43 +0300

    ASoC: SOF: imx8m: Fix SAI DAI driver for i.MX8M
    This must match DAI name from topology. Also, sai-port
    is too generic. Physical DAI port on i.MX8MP is labeled SAI3.
    
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    Reviewed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Reviewed-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
    Link: https://lore.kernel.org/r/20200720072046.8152-5-daniel.baluta@oss.nxp.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sof/imx/imx8m.c
+++ sound/soc/sof/imx/imx8m.c
@@ -239,7 +239,7 @@ static int imx8m_ipc_pcm_params(struct snd_sof_dev *sdev,
 
 static struct snd_soc_dai_driver imx8m_dai[] = {
 {
-	.name = "sai-port",
+	.name = "sai3",
 	.playback = {
 		.channels_min = 1,
 		.channels_max = 32,
