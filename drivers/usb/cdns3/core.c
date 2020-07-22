7e8b59c1fc6bf78b8117efd8d354bcb99efed2ea
Chunfeng Yun <chunfeng.yun@mediatek.com>
2020-07-22 15:51:11 +0800

    usb: cdns3: convert to devm_platform_ioremap_resource_byname
    Use devm_platform_ioremap_resource_byname() to simplify code
    
    Cc: Peter Chen <peter.chen@nxp.com>
    Reviewed-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Chunfeng Yun <chunfeng.yun@mediatek.com>
    Link: https://lore.kernel.org/r/1595404275-8449-3-git-send-email-chunfeng.yun@mediatek.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/cdns3/core.c
+++ drivers/usb/cdns3/core.c
@@ -423,8 +423,7 @@ static int cdns3_probe(struct platform_device *pdev)
 	if (cdns->dev_irq < 0)
 		dev_err(dev, "couldn't get peripheral irq\n");
 
-	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "dev");
-	regs = devm_ioremap_resource(dev, res);
+	regs = devm_platform_ioremap_resource_byname(pdev, "dev");
 	if (IS_ERR(regs))
 		return PTR_ERR(regs);
 	cdns->dev_regs	= regs;
