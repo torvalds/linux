7b04d6d60fcfb5b2200ffebb9cfb90927bdfeec7
Song Liu <songliubraving@fb.com>
2020-07-23 11:06:44 -0700

    bpf: Separate bpf_get_[stack|stackid] for perf events BPF
    Calling get_perf_callchain() on perf_events from PEBS entries may cause
    unwinder errors. To fix this issue, the callchain is fetched early. Such
    perf_events are marked with __PERF_SAMPLE_CALLCHAIN_EARLY.
    
    Similarly, calling bpf_get_[stack|stackid] on perf_events from PEBS may
    also cause unwinder errors. To fix this, add separate version of these
    two helpers, bpf_get_[stack|stackid]_pe. These two hepers use callchain in
    bpf_perf_event_data_kern->data->callchain.
    
    Signed-off-by: Song Liu <songliubraving@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200723180648.1429892-2-songliubraving@fb.com

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -1675,6 +1675,8 @@ extern const struct bpf_func_proto bpf_get_current_comm_proto;
 extern const struct bpf_func_proto bpf_get_stackid_proto;
 extern const struct bpf_func_proto bpf_get_stack_proto;
 extern const struct bpf_func_proto bpf_get_task_stack_proto;
+extern const struct bpf_func_proto bpf_get_stackid_proto_pe;
+extern const struct bpf_func_proto bpf_get_stack_proto_pe;
 extern const struct bpf_func_proto bpf_sock_map_update_proto;
 extern const struct bpf_func_proto bpf_sock_hash_update_proto;
 extern const struct bpf_func_proto bpf_get_current_cgroup_id_proto;
