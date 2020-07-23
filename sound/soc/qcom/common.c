627ab55d745b2a413e0a2856f2ad2e422a697971
Stephan Gerhold <stephan@gerhold.net>
2020-07-23 20:38:59 +0200

    ASoC: qcom: common: Use snd_soc_dai_link_set_capabilities()
    Commit a2120089251f ("ASoC: qcom: common: set correct directions for dailinks")
    introduced a call to q6afe_is_rx_port() to set the dpcm_playback/capture
    parameters correctly. This is necessary because those parameters are now
    validated to match the capabilities of the DAIs. [1]
    
    The disadvantage of introducing the call to q6afe_is_rx_port() is that
    it makes the qcom_snd_parse_of() helper dependent on the QDSP6 driver.
    When the ADSP is bypassed (e.g. in apq8016-sbc) QDSP6 is not used.
    
    There is a generic solution for this now: The correct direction for the links
    is already defined by the DAI capabilities (e.g. rx ports only support playback).
    
    Commit 25612477d20b ("ASoC: soc-dai: set dai_link dpcm_ flags with a helper")
    introduced the snd_soc_dai_link_set_capabilities() function that we can use
    to set dpcm_playback/dpcm_capture according to the capabilities of the DAIs.
    
    Use that for both FE/BE DAI links to avoid the dependency on the QDSP6 driver.
    
    [1]: https://lore.kernel.org/alsa-devel/20200616085409.GA110999@gerhold.net/
    
    Signed-off-by: Stephan Gerhold <stephan@gerhold.net>
    Tested-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Reviewed-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Cc: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Link: https://lore.kernel.org/r/20200723183904.321040-3-stephan@gerhold.net
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/qcom/common.c
+++ sound/soc/qcom/common.c
@@ -4,7 +4,6 @@
 
 #include <linux/module.h>
 #include "common.h"
-#include "qdsp6/q6afe.h"
 
 int qcom_snd_parse_of(struct snd_soc_card *card)
 {
@@ -102,15 +101,6 @@ int qcom_snd_parse_of(struct snd_soc_card *card)
 			}
 			link->no_pcm = 1;
 			link->ignore_pmdown_time = 1;
-
-			if (q6afe_is_rx_port(link->id)) {
-				link->dpcm_playback = 1;
-				link->dpcm_capture = 0;
-			} else {
-				link->dpcm_playback = 0;
-				link->dpcm_capture = 1;
-			}
-
 		} else {
 			dlc = devm_kzalloc(dev, sizeof(*dlc), GFP_KERNEL);
 			if (!dlc)
@@ -123,10 +113,9 @@ int qcom_snd_parse_of(struct snd_soc_card *card)
 			link->codecs->dai_name = "snd-soc-dummy-dai";
 			link->codecs->name = "snd-soc-dummy";
 			link->dynamic = 1;
-			link->dpcm_playback = 1;
-			link->dpcm_capture = 1;
 		}
 
+		snd_soc_dai_link_set_capabilities(link);
 		link->ignore_suspend = 1;
 		link->nonatomic = 1;
 		link->stream_name = link->name;
