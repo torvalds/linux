12d9516944c9beacf82194069197bc286af6dc31
Anmol Karn <anmol.karan123@gmail.com>
2020-07-23 12:51:15 +0530

    staging: vc04_services: vchiq-mmal: Fixed tab styling issue in mmal-vchiq.c
    This cahnge fixes a checkpatch error for "code indent should use tabs where possible".
    
    compile Tested only
    [Linux-next-20200722]
    
    Signed-off-by: Anmol Karn <anmol.karan123@gmail.com>
    Link: https://lore.kernel.org/r/20200723072115.408070-1-anmol.karan123@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vc04_services/vchiq-mmal/mmal-vchiq.c
+++ drivers/staging/vc04_services/vchiq-mmal/mmal-vchiq.c
@@ -295,12 +295,12 @@ static void buffer_to_host_work_cb(struct work_struct *work)
 	vchiq_use_service(instance->service_handle);
 	ret = vchiq_bulk_receive(instance->service_handle,
 				 msg_context->u.bulk.buffer->buffer,
-			         /* Actual receive needs to be a multiple
-			          * of 4 bytes
-			          */
-			         (len + 3) & ~3,
-			         msg_context,
-			         VCHIQ_BULK_MODE_CALLBACK);
+				 /* Actual receive needs to be a multiple
+				  * of 4 bytes
+				  */
+				(len + 3) & ~3,
+				msg_context,
+				VCHIQ_BULK_MODE_CALLBACK);
 
 	vchiq_release_service(instance->service_handle);
 
