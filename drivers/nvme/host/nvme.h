f783f444ceaa7eab043a7a5ae0e34c68743c0358
Logan Gunthorpe <logang@deltatee.com>
2020-07-24 11:25:15 -0600

    nvme: introduce nvme_ctrl_get_by_path()
    nvme_ctrl_get_by_path() is analogous to blkdev_get_by_path() except it
    gets a struct nvme_ctrl from the path to its char dev (/dev/nvme0).
    It makes use of filp_open() to open the file and uses the private
    data to obtain a pointer to the struct nvme_ctrl. If the fops of the
    file do not match, -EINVAL is returned.
    
    The purpose of this function is to support NVMe-OF target passthru
    and is exported under the NVME_TARGET_PASSTHRU namespace.
    
    Signed-off-by: Logan Gunthorpe <logang@deltatee.com>
    Reviewed-by: Keith Busch <kbusch@kernel.org>
    Reviewed-by: Sagi Grimberg <sagi@grimberg.me>
    Reviewed-by: Max Gurtovoy <maxg@mellanox.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/nvme/host/nvme.h
+++ drivers/nvme/host/nvme.h
@@ -793,5 +793,6 @@ static inline void nvme_hwmon_init(struct nvme_ctrl *ctrl) { }
 u32 nvme_command_effects(struct nvme_ctrl *ctrl, struct nvme_ns *ns,
 			 u8 opcode);
 void nvme_execute_passthru_rq(struct request *rq);
+struct nvme_ctrl *nvme_ctrl_get_by_path(const char *path);
 
 #endif /* _NVME_H */
