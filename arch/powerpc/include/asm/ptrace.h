df4d4ef22446b3a789a4efd74d34f2ec1e24deb2
Michael Ellerman <mpe@ellerman.id.au>
2020-07-24 23:17:24 +1000

    powerpc/32s: Fix CONFIG_BOOK3S_601 uses
    We have two uses of CONFIG_BOOK3S_601, which doesn't exist. Fix them
    to use CONFIG_PPC_BOOK3S_601 which is the correct symbol.
    
    Fixes: 12c3f1fd87bf ("powerpc/32s: get rid of CPU_FTR_601 feature")
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200724131728.1643966-5-mpe@ellerman.id.au

--- arch/powerpc/include/asm/ptrace.h
+++ arch/powerpc/include/asm/ptrace.h
@@ -243,7 +243,7 @@ static inline void set_trap_norestart(struct pt_regs *regs)
 }
 
 #define arch_has_single_step()	(1)
-#ifndef CONFIG_BOOK3S_601
+#ifndef CONFIG_PPC_BOOK3S_601
 #define arch_has_block_step()	(true)
 #else
 #define arch_has_block_step()	(false)
