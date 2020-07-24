73b51112cf1adec2fea6a7a8227317f061a0d24f
Eric Biggers <ebiggers@google.com>
2020-07-24 18:44:57 +0000

    FROMLIST: iomap: support direct I/O with fscrypt using blk-crypto
    Set bio crypt contexts on bios by calling into fscrypt when required.
    No DUN contiguity checks are done - callers are expected to set up the
    iomap correctly to ensure that each bio submitted by iomap will not have
    blocks with incontiguous DUNs by calling fscrypt_limit_io_blocks()
    appropriately.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Co-developed-by: Satya Tangirala <satyat@google.com>
    Signed-off-by: Satya Tangirala <satyat@google.com>
    
    Bug: 162255927
    Link: https://lore.kernel.org/r/20200724184501.1651378-4-satyat@google.com
    Change-Id: I34bd73001d53c854b5905799d3a9c31762914763
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- fs/iomap/direct-io.c
+++ fs/iomap/direct-io.c
@@ -6,6 +6,7 @@
 #include <linux/module.h>
 #include <linux/compiler.h>
 #include <linux/fs.h>
+#include <linux/fscrypt.h>
 #include <linux/iomap.h>
 #include <linux/backing-dev.h>
 #include <linux/uio.h>
@@ -185,11 +186,14 @@ static void
 iomap_dio_zero(struct iomap_dio *dio, struct iomap *iomap, loff_t pos,
 		unsigned len)
 {
+	struct inode *inode = file_inode(dio->iocb->ki_filp);
 	struct page *page = ZERO_PAGE(0);
 	int flags = REQ_SYNC | REQ_IDLE;
 	struct bio *bio;
 
 	bio = bio_alloc(GFP_KERNEL, 1);
+	fscrypt_set_bio_crypt_ctx(bio, inode, pos >> inode->i_blkbits,
+				  GFP_KERNEL);
 	bio_set_dev(bio, iomap->bdev);
 	bio->bi_iter.bi_sector = iomap_sector(iomap, pos);
 	bio->bi_private = dio;
@@ -272,6 +276,8 @@ iomap_dio_bio_actor(struct inode *inode, loff_t pos, loff_t length,
 		}
 
 		bio = bio_alloc(GFP_KERNEL, nr_pages);
+		fscrypt_set_bio_crypt_ctx(bio, inode, pos >> inode->i_blkbits,
+					  GFP_KERNEL);
 		bio_set_dev(bio, iomap->bdev);
 		bio->bi_iter.bi_sector = iomap_sector(iomap, pos);
 		bio->bi_write_hint = dio->iocb->ki_hint;
