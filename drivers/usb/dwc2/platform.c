ca6377900974c3e22c379c48057aac51139d29fc
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2020-07-27 15:34:15 +0200

    Revert "usb: dwc2: override PHY input signals with usb role switch support"
    This reverts commit bc0f0d4a5853e32ba97a0318f774570428fc5634.
    
    It was not meant to be applied yet.
    
    Cc: Minas Harutyunyan <hminas@synopsys.com>
    Cc: Amelie Delaunay <amelie.delaunay@st.com>
    Cc: Felipe Balbi <balbi@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/dwc2/platform.c
+++ drivers/usb/dwc2/platform.c
@@ -317,8 +317,6 @@ static int dwc2_driver_remove(struct platform_device *dev)
 	if (hsotg->params.activate_stm_id_vb_detection)
 		regulator_disable(hsotg->usb33d);
 
-	dwc2_drd_exit(hsotg);
-
 	if (hsotg->ll_hw_enabled)
 		dwc2_lowlevel_hw_disable(hsotg);
 
@@ -535,13 +533,6 @@ static int dwc2_driver_probe(struct platform_device *dev)
 		dwc2_writel(hsotg, ggpio, GGPIO);
 	}
 
-	retval = dwc2_drd_init(hsotg);
-	if (retval) {
-		if (retval != -EPROBE_DEFER)
-			dev_err(hsotg->dev, "failed to initialize dual-role\n");
-		goto error_init;
-	}
-
 	if (hsotg->dr_mode != USB_DR_MODE_HOST) {
 		retval = dwc2_gadget_init(hsotg);
 		if (retval)
@@ -616,8 +607,6 @@ static int __maybe_unused dwc2_suspend(struct device *dev)
 
 	if (is_device_mode)
 		dwc2_hsotg_suspend(dwc2);
-
-	dwc2_drd_suspend(dwc2);
 
 	if (dwc2->params.activate_stm_id_vb_detection) {
 		unsigned long flags;
@@ -698,8 +687,6 @@ static int __maybe_unused dwc2_resume(struct device *dev)
 
 	/* Need to restore FORCEDEVMODE/FORCEHOSTMODE */
 	dwc2_force_dr_mode(dwc2);
-
-	dwc2_drd_resume(dwc2);
 
 	if (dwc2_is_device_mode(dwc2))
 		ret = dwc2_hsotg_resume(dwc2);
