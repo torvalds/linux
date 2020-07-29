bee18dd57e89f4e7aa79f6054b9238b99e45c191
Abhilasha Rao <abhilasha.hv@samsung.corp-partner.google.com>
2020-07-29 15:43:52 +0530

    ANDROID: GKI: pid: Export for find_task_by_vpid
        - Add EXPORT_SYMBOL_GPL for find_task_by_vpid() so that drivers
        can be loadable as a module.
    
        - This API is required by loadable driver module from samsung to
        read process related information based on pid and thread id.
        To get information on when a certain process or thread was started,
        duration of run, Average load contributed by it.
    
    Signed-off-by: Abhilasha Rao <abhilasha.hv@samsung.corp-partner.google.com>
    Bug: 158067689
    Change-Id: I0db9cc50c93eedff0f3e9dea0ac09a5d17d118f0

--- kernel/pid.c
+++ kernel/pid.c
@@ -420,6 +420,7 @@ struct task_struct *find_task_by_vpid(pid_t vnr)
 {
 	return find_task_by_pid_ns(vnr, task_active_pid_ns(current));
 }
+EXPORT_SYMBOL_GPL(find_task_by_vpid);
 
 struct task_struct *find_get_task_by_vpid(pid_t nr)
 {
