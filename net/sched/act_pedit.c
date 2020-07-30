a0d716d8e42abcb973afed27c6fa44bd146d2616
Gustavo A. R. Silva <gustavoars@kernel.org>
2020-07-29 22:17:00 -0500

    net/sched: act_pedit: Use flex_array_size() helper in memcpy()
    Make use of the flex_array_size() helper to calculate the size of a
    flexible array member within an enclosing structure.
    
    This helper offers defense-in-depth against potential integer
    overflows, while at the same time makes it explicitly clear that
    we are dealing with a flexible array member.
    
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/act_pedit.c
+++ net/sched/act_pedit.c
@@ -436,8 +436,7 @@ static int tcf_pedit_dump(struct sk_buff *skb, struct tc_action *a,
 		return -ENOBUFS;
 
 	spin_lock_bh(&p->tcf_lock);
-	memcpy(opt->keys, p->tcfp_keys,
-	       p->tcfp_nkeys * sizeof(struct tc_pedit_key));
+	memcpy(opt->keys, p->tcfp_keys, flex_array_size(opt, keys, p->tcfp_nkeys));
 	opt->index = p->tcf_index;
 	opt->nkeys = p->tcfp_nkeys;
 	opt->flags = p->tcfp_flags;
