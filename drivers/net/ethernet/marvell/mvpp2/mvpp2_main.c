d6526926de7397a97308780911565e31a6b67b59
Lorenzo Bianconi <lorenzo@kernel.org>
2020-07-31 10:38:32 +0200

    net: mvpp2: fix memory leak in mvpp2_rx
    Release skb memory in mvpp2_rx() if mvpp2_rx_refill routine fails
    
    Fixes: b5015854674b ("net: mvpp2: fix refilling BM pools in RX path")
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Acked-by: Matteo Croce <mcroce@microsoft.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/mvpp2/mvpp2_main.c
+++ drivers/net/ethernet/marvell/mvpp2/mvpp2_main.c
@@ -3508,6 +3508,7 @@ static int mvpp2_rx(struct mvpp2_port *port, struct napi_struct *napi,
 		err = mvpp2_rx_refill(port, bm_pool, pp, pool);
 		if (err) {
 			netdev_err(port->dev, "failed to refill BM pools\n");
+			dev_kfree_skb_any(skb);
 			goto err_drop_frame;
 		}
 
