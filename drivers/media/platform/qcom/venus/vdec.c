e1c69c4eef61ffe295b747992c6fd849e6cd747d
Vikash Garodia <vgarodia@codeaurora.org>
2020-08-04 13:48:45 +0200

    media: venus: fixes for list corruption
    There are few list handling issues while adding and deleting
    node in the registered buf list in the driver.
    1. list addition - buffer added into the list during buf_init
    while not deleted during cleanup.
    2. list deletion - In capture streamoff, the list was reinitialized.
    As a result, if any node was present in the list, it would
    lead to issue while cleaning up that node during buf_cleanup.
    
    Corresponding call traces below:
    [  165.751014] Call trace:
    [  165.753541]  __list_add_valid+0x58/0x88
    [  165.757532]  venus_helper_vb2_buf_init+0x74/0xa8 [venus_core]
    [  165.763450]  vdec_buf_init+0x34/0xb4 [venus_dec]
    [  165.768271]  __buf_prepare+0x598/0x8a0 [videobuf2_common]
    [  165.773820]  vb2_core_qbuf+0xb4/0x334 [videobuf2_common]
    [  165.779298]  vb2_qbuf+0x78/0xb8 [videobuf2_v4l2]
    [  165.784053]  v4l2_m2m_qbuf+0x80/0xf8 [v4l2_mem2mem]
    [  165.789067]  v4l2_m2m_ioctl_qbuf+0x2c/0x38 [v4l2_mem2mem]
    [  165.794624]  v4l_qbuf+0x48/0x58
    
    [ 1797.556001] Call trace:
    [ 1797.558516]  __list_del_entry_valid+0x88/0x9c
    [ 1797.562989]  vdec_buf_cleanup+0x54/0x228 [venus_dec]
    [ 1797.568088]  __buf_prepare+0x270/0x8a0 [videobuf2_common]
    [ 1797.573625]  vb2_core_qbuf+0xb4/0x338 [videobuf2_common]
    [ 1797.579082]  vb2_qbuf+0x78/0xb8 [videobuf2_v4l2]
    [ 1797.583830]  v4l2_m2m_qbuf+0x80/0xf8 [v4l2_mem2mem]
    [ 1797.588843]  v4l2_m2m_ioctl_qbuf+0x2c/0x38 [v4l2_mem2mem]
    [ 1797.594389]  v4l_qbuf+0x48/0x58
    
    Signed-off-by: Vikash Garodia <vgarodia@codeaurora.org>
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Signed-off-by: Stanimir Varbanov <stanimir.varbanov@linaro.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/platform/qcom/venus/vdec.c
+++ drivers/media/platform/qcom/venus/vdec.c
@@ -1088,8 +1088,6 @@ static int vdec_stop_capture(struct venus_inst *inst)
 		break;
 	}
 
-	INIT_LIST_HEAD(&inst->registeredbufs);
-
 	return ret;
 }
 
@@ -1189,6 +1187,14 @@ static int vdec_buf_init(struct vb2_buffer *vb)
 static void vdec_buf_cleanup(struct vb2_buffer *vb)
 {
 	struct venus_inst *inst = vb2_get_drv_priv(vb->vb2_queue);
+	struct vb2_v4l2_buffer *vbuf = to_vb2_v4l2_buffer(vb);
+	struct venus_buffer *buf = to_venus_buffer(vbuf);
+
+	mutex_lock(&inst->lock);
+	if (vb->type == V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE)
+		if (!list_empty(&inst->registeredbufs))
+			list_del_init(&buf->reg_list);
+	mutex_unlock(&inst->lock);
 
 	inst->buf_count--;
 	if (!inst->buf_count)
