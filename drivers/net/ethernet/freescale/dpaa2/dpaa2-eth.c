02afa9c66bb954c6959877c70d9e128dcf0adce7
YueHaibing <yuehaibing@huawei.com>
2020-08-04 21:26:43 +0800

    dpaa2-eth: Fix passing zero to 'PTR_ERR' warning
    Fix smatch warning:
    
    drivers/net/ethernet/freescale/dpaa2/dpaa2-eth.c:2419
     alloc_channel() warn: passing zero to 'ERR_PTR'
    
    setup_dpcon() should return ERR_PTR(err) instead of zero in error
    handling case.
    
    Fixes: d7f5a9d89a55 ("dpaa2-eth: defer probe on object allocate")
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/dpaa2/dpaa2-eth.c
+++ drivers/net/ethernet/freescale/dpaa2/dpaa2-eth.c
@@ -2370,7 +2370,7 @@ close:
 free:
 	fsl_mc_object_free(dpcon);
 
-	return NULL;
+	return ERR_PTR(err);
 }
 
 static void free_dpcon(struct dpaa2_eth_priv *priv,
@@ -2394,8 +2394,8 @@ alloc_channel(struct dpaa2_eth_priv *priv)
 		return NULL;
 
 	channel->dpcon = setup_dpcon(priv);
-	if (IS_ERR_OR_NULL(channel->dpcon)) {
-		err = PTR_ERR_OR_ZERO(channel->dpcon);
+	if (IS_ERR(channel->dpcon)) {
+		err = PTR_ERR(channel->dpcon);
 		goto err_setup;
 	}
 
