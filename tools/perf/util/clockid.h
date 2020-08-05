6953beb4ddab87ad00029d8a11c1f985df09981e
Jiri Olsa <jolsa@kernel.org>
2020-08-05 11:34:38 +0200

    perf clockid: Move parse_clockid() to new clockid object
    Move parse_clockid and all needed clcckid related stuff into clockid
    object. We are going to add clockid_name function in following change,
    so it's better it's placed in separated object and not in
    builtin-record.c.
    
    No functional change is intended.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Geneviève Bastien <gbastien@versatic.net>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Jeremie Galarneau <jgalar@efficios.com>
    Cc: Michael Petlan <mpetlan@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lore.kernel.org/lkml/20200805093444.314999-2-jolsa@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

---
+++ tools/perf/util/clockid.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __PERF_CLOCKID_H
+#define __PERF_CLOCKID_H
+
+struct option;
+int parse_clockid(const struct option *opt, const char *str, int unset);
+
+#endif
