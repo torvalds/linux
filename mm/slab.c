dabc3e291d56e3125113101bc6d53d1a1738294d
Kees Cook <keescook@chromium.org>
2020-08-06 23:18:24 -0700

    mm/slab: add naive detection of double free
    Similar to commit ce6fa91b9363 ("mm/slub.c: add a naive detection of
    double free or corruption"), add a very cheap double-free check for SLAB
    under CONFIG_SLAB_FREELIST_HARDENED.  With this added, the
    "SLAB_FREE_DOUBLE" LKDTM test passes under SLAB:
    
      lkdtm: Performing direct entry SLAB_FREE_DOUBLE
      lkdtm: Attempting double slab free ...
      ------------[ cut here ]------------
      WARNING: CPU: 2 PID: 2193 at mm/slab.c:757 ___cache _free+0x325/0x390
    
    [keescook@chromium.org: fix misplaced __free_one()]
      Link: http://lkml.kernel.org/r/202006261306.0D82A2B@keescook
      Link: https://lore.kernel.org/lkml/7ff248c7-d447-340c-a8e2-8c02972aca70@infradead.org
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Acked-by: Randy Dunlap <rdunlap@infradead.org>	[build tested]
    Cc: Roman Gushchin <guro@fb.com>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Alexander Popov <alex.popov@linux.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Vinayak Menon <vinmenon@codeaurora.org>
    Cc: Matthew Garrett <mjg59@google.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Vijayanand Jitta <vjitta@codeaurora.org>
    Link: http://lkml.kernel.org/r/20200625215548.389774-3-keescook@chromium.org
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/slab.c
+++ mm/slab.c
@@ -588,6 +588,16 @@ static int transfer_objects(struct array_cache *to,
 	return nr;
 }
 
+/* &alien->lock must be held by alien callers. */
+static __always_inline void __free_one(struct array_cache *ac, void *objp)
+{
+	/* Avoid trivial double-free. */
+	if (IS_ENABLED(CONFIG_SLAB_FREELIST_HARDENED) &&
+	    WARN_ON_ONCE(ac->avail > 0 && ac->entry[ac->avail - 1] == objp))
+		return;
+	ac->entry[ac->avail++] = objp;
+}
+
 #ifndef CONFIG_NUMA
 
 #define drain_alien_cache(cachep, alien) do { } while (0)
@@ -767,7 +777,7 @@ static int __cache_free_alien(struct kmem_cache *cachep, void *objp,
 			STATS_INC_ACOVERFLOW(cachep);
 			__drain_alien_cache(cachep, ac, page_node, &list);
 		}
-		ac->entry[ac->avail++] = objp;
+		__free_one(ac, objp);
 		spin_unlock(&alien->lock);
 		slabs_destroy(cachep, &list);
 	} else {
@@ -3466,7 +3476,7 @@ void ___cache_free(struct kmem_cache *cachep, void *objp,
 		}
 	}
 
-	ac->entry[ac->avail++] = objp;
+	__free_one(ac, objp);
 }
 
 /**
