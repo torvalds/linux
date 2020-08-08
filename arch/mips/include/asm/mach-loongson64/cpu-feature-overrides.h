433c1ca0d441ee0b88fdd83c84ee6d6d43080dcd
Jiaxun Yang <jiaxun.yang@flygoat.com>
2020-08-08 20:32:27 +0800

    MIPS: Loongson64: Do not override watch and ejtag feature
    Do not override ejtag feature to 0 as Loongson 3A1000+ do have ejtag.
    For watch, as KVM emulated CPU doesn't have watch feature, we should
    not enable it unconditionally.
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Reviewed-by: Huacai Chen <chenhc@lemote.com>
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/include/asm/mach-loongson64/cpu-feature-overrides.h
+++ arch/mips/include/asm/mach-loongson64/cpu-feature-overrides.h
@@ -26,7 +26,6 @@
 #define cpu_has_counter		1
 #define cpu_has_dc_aliases	(PAGE_SIZE < 0x4000)
 #define cpu_has_divec		0
-#define cpu_has_ejtag		0
 #define cpu_has_inclusive_pcaches	1
 #define cpu_has_llsc		1
 #define cpu_has_mcheck		0
@@ -42,7 +41,6 @@
 #define cpu_has_veic		0
 #define cpu_has_vint		0
 #define cpu_has_vtag_icache	0
-#define cpu_has_watch		1
 #define cpu_has_wsbh		1
 #define cpu_has_ic_fills_f_dc	1
 #define cpu_hwrena_impl_bits	0xc0000000
