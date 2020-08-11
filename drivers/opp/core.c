8979ef70850eb469e1094279259d1ef393ffe85f
Stephen Boyd <swboyd@chromium.org>
2020-08-11 14:28:36 -0700

    opp: Put opp table in dev_pm_opp_set_rate() for empty tables
    We get the opp_table pointer at the top of the function and so we should
    put the pointer at the end of the function like all other exit paths
    from this function do.
    
    Cc: v5.7+ <stable@vger.kernel.org> # v5.7+
    Fixes: aca48b61f963 ("opp: Manage empty OPP tables with clk handle")
    Reviewed-by: Rajendra Nayak <rnayak@codeaurora.org>
    Signed-off-by: Stephen Boyd <swboyd@chromium.org>
    [ Viresh: Split the patch into two ]
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/opp/core.c
+++ drivers/opp/core.c
@@ -893,8 +893,10 @@ int dev_pm_opp_set_rate(struct device *dev, unsigned long target_freq)
 		 * have OPP table for the device, while others don't and
 		 * opp_set_rate() just needs to behave like clk_set_rate().
 		 */
-		if (!_get_opp_count(opp_table))
-			return 0;
+		if (!_get_opp_count(opp_table)) {
+			ret = 0;
+			goto put_opp_table;
+		}
 
 		if (!opp_table->required_opp_tables && !opp_table->regulators &&
 		    !opp_table->paths) {
