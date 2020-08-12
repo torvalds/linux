8a03703c5e759d29cde005f80f34acb071fab893
Marco Ballesio <balejs@google.com>
2020-08-12 07:25:00 -0700

    ANDROID: sched: add "frozen" field to task_struct
    use one of the ANDROID_KABI_RESERVED fields for the v2 freezer "frozen"
    bit.
    
    Bug: 163547360
    Test: built and booted
    Signed-off-by: Marco Ballesio <balejs@google.com>
    Change-Id: I7d7aed173a09580b8eff1ccf39ca4f162fbaddc8

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1292,8 +1292,9 @@ struct task_struct {
 	/* Used by LSM modules for access restriction: */
 	void				*security;
 #endif
+	/* task is frozen/stopped (used by the cgroup freezer) */
+	ANDROID_KABI_USE(1, unsigned frozen:1);
 
-	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
 	ANDROID_KABI_RESERVE(3);
 	ANDROID_KABI_RESERVE(4);
