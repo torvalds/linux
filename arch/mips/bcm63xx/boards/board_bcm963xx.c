08b5666db72273d2054a2a48961c6c33f9d697b2
Álvaro Fernández Rojas <noltari@gmail.com>
2020-08-12 09:52:35 +0200

    MIPS: BCM63xx: switch to SPDX license identifier
    Use SPDX license indentifier instead of local reference to COPYING.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
    Acked-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/bcm63xx/boards/board_bcm963xx.c
+++ arch/mips/bcm63xx/boards/board_bcm963xx.c
@@ -1,8 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
- * This file is subject to the terms and conditions of the GNU General Public
- * License.  See the file "COPYING" in the main directory of this archive
- * for more details.
- *
  * Copyright (C) 2008 Maxime Bizon <mbizon@freebox.fr>
  * Copyright (C) 2008 Florian Fainelli <florian@openwrt.org>
  */
