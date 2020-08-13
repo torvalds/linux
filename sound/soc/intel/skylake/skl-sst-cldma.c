3b4d60f0f7dd13594916bcd6644ea6208597d993
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-08-13 15:01:43 -0500

    ASoC: Intel: Skylake: cldma: remove redundant initialization
    Fix cppcheck warning:
    
    sound/soc/intel/skylake/skl-sst-cldma.c:248:10: style: Variable 'ret'
    is assigned a value that is never used. [unreadVariable]
     int ret = 0;
             ^
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20200813200147.61990-18-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/skylake/skl-sst-cldma.c
+++ sound/soc/intel/skylake/skl-sst-cldma.c
@@ -245,7 +245,7 @@ static int
 skl_cldma_copy_to_buf(struct sst_dsp *ctx, const void *bin,
 			u32 total_size, bool wait)
 {
-	int ret = 0;
+	int ret;
 	bool start = true;
 	unsigned int excess_bytes;
 	u32 size;
