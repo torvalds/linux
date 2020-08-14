2f22f2ce4b1a2f4bb0d3a3ee854be113284f8d94
Eric Biggers <ebiggers@google.com>
2020-08-14 12:05:10 -0700

    Revert "ANDROID: ASoC: Fix 'allmodconfig' build break"
    This reverts commit 972e02b5256aa6d40427413b2d2f19ed626156c3 because it
    was a fix for commit 9cb7ec3e9be4 ("ANDROID: ASoC: core - add hostless
    DAI support"), which got reverted by commit da171b5d560c ("ANDROID:
    Revert "ANDROID: ASoC: core - add hostless DAI support"").  Therefore
    it's no longer needed.
    
    Bug: 144369166
    Change-Id: If3ed9dcb04d9879d8fb4a74076c029cf8ce9a609
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -2284,7 +2284,6 @@ void arch_setup_dma_ops(struct device *dev, u64 dma_base, u64 size,
 #endif
 	dev->archdata.dma_ops_setup = true;
 }
-EXPORT_SYMBOL_GPL(arch_setup_dma_ops);
 
 void arch_teardown_dma_ops(struct device *dev)
 {
