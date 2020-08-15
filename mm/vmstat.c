a8a4b7aeaf841311cb13ff0f6c4710c7a00e68d4
Baoquan He <bhe@redhat.com>
2020-08-14 17:30:07 -0700

    Revert "mm/vmstat.c: do not show lowmem reserve protection information of empty zone"
    This reverts commit 26e7deadaae175.
    
    Sonny reported that one of their tests started failing on the latest
    kernel on their Chrome OS platform.  The root cause is that the above
    commit removed the protection line of empty zone, while the parser used in
    the test relies on the protection line to mark the end of each zone.
    
    Let's revert it to avoid breaking userspace testing or applications.
    
    Fixes: 26e7deadaae175 ("mm/vmstat.c: do not show lowmem reserve protection information of empty zone)"
    Reported-by: Sonny Rao <sonnyrao@chromium.org>
    Signed-off-by: Baoquan He <bhe@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Cc: <stable@vger.kernel.org>	[5.8.x]
    Link: http://lkml.kernel.org/r/20200811075412.12872-1-bhe@redhat.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1642,18 +1642,18 @@ static void zoneinfo_show_print(struct seq_file *m, pg_data_t *pgdat,
 		   zone->present_pages,
 		   zone_managed_pages(zone));
 
-	/* If unpopulated, no other information is useful */
-	if (!populated_zone(zone)) {
-		seq_putc(m, '\n');
-		return;
-	}
-
 	seq_printf(m,
 		   "\n        protection: (%ld",
 		   zone->lowmem_reserve[0]);
 	for (i = 1; i < ARRAY_SIZE(zone->lowmem_reserve); i++)
 		seq_printf(m, ", %ld", zone->lowmem_reserve[i]);
 	seq_putc(m, ')');
+
+	/* If unpopulated, no other information is useful */
+	if (!populated_zone(zone)) {
+		seq_putc(m, '\n');
+		return;
+	}
 
 	for (i = 0; i < NR_VM_ZONE_STAT_ITEMS; i++)
 		seq_printf(m, "\n      %-12s %lu", zone_stat_name(i),
