3551e954f5d95faf3dbc340d422da7624658c230
Valentin Schneider <valentin.schneider@arm.com>
2020-08-17 12:30:01 +0100

    sched/topology: Mark SD_OVERLAP as SDF_NEEDS_GROUPS
    A sched_domain can only have overlapping sched_groups if it has more than
    one group.
    
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: https://lore.kernel.org/r/20200817113003.20802-16-valentin.schneider@arm.com

--- include/linux/sched/sd_flags.h
+++ include/linux/sched/sd_flags.h
@@ -143,8 +143,9 @@ SD_FLAG(SD_PREFER_SIBLING, SDF_NEEDS_GROUPS)
  * sched_groups of this level overlap
  *
  * SHARED_PARENT: Set for all NUMA levels above NODE.
+ * NEEDS_GROUPS: Overlaps can only exist with more than one group.
  */
-SD_FLAG(SD_OVERLAP, SDF_SHARED_PARENT)
+SD_FLAG(SD_OVERLAP, SDF_SHARED_PARENT | SDF_NEEDS_GROUPS)
 
 /*
  * Cross-node balancing
