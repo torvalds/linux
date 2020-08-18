38e03d092699891c3237b5aee9e8029d4ede0956
Jason Gunthorpe <jgg@nvidia.com>
2020-08-18 15:05:22 +0300

    RDMA/ucma: Add missing locking around rdma_leave_multicast()
    All entry points to the rdma_cm from a ULP must be single threaded,
    even this error unwinds. Add the missing locking.
    
    Fixes: 7c11910783a1 ("RDMA/ucma: Put a lock around every call to the rdma_cm layer")
    Link: https://lore.kernel.org/r/20200818120526.702120-11-leon@kernel.org
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/infiniband/core/ucma.c
+++ drivers/infiniband/core/ucma.c
@@ -1535,7 +1535,9 @@ static ssize_t ucma_process_join(struct ucma_file *file,
 	return 0;
 
 err3:
+	mutex_lock(&ctx->mutex);
 	rdma_leave_multicast(ctx->cm_id, (struct sockaddr *) &mc->addr);
+	mutex_unlock(&ctx->mutex);
 	ucma_cleanup_mc_events(mc);
 err2:
 	xa_erase(&multicast_table, mc->id);
