7084566a236fbc98beb11430d8d67dd08b2ac151
Andrii Nakryiko <andriin@fb.com>
2020-08-18 18:36:05 -0700

    tools/bpftool: Remove libbpf_internal.h usage in bpftool
    Most netlink-related functions were unique to bpftool usage, so I moved them
    into net.c. Few functions are still used by both bpftool and libbpf itself
    internally, so I've copy-pasted them (libbpf_nl_get_link,
    libbpf_netlink_open). It's a bit of duplication of code, but better separation
    of libbpf as a library with public API and bpftool, relying on unexposed
    functions in libbpf.
    
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200819013607.3607269-3-andriin@fb.com

--- tools/lib/bpf/libbpf_internal.h
+++ tools/lib/bpf/libbpf_internal.h
@@ -130,18 +130,6 @@ int bpf_object__section_size(const struct bpf_object *obj, const char *name,
 int bpf_object__variable_offset(const struct bpf_object *obj, const char *name,
 				__u32 *off);
 
-struct nlattr;
-typedef int (*libbpf_dump_nlmsg_t)(void *cookie, void *msg, struct nlattr **tb);
-int libbpf_netlink_open(unsigned int *nl_pid);
-int libbpf_nl_get_link(int sock, unsigned int nl_pid,
-		       libbpf_dump_nlmsg_t dump_link_nlmsg, void *cookie);
-int libbpf_nl_get_class(int sock, unsigned int nl_pid, int ifindex,
-			libbpf_dump_nlmsg_t dump_class_nlmsg, void *cookie);
-int libbpf_nl_get_qdisc(int sock, unsigned int nl_pid, int ifindex,
-			libbpf_dump_nlmsg_t dump_qdisc_nlmsg, void *cookie);
-int libbpf_nl_get_filter(int sock, unsigned int nl_pid, int ifindex, int handle,
-			 libbpf_dump_nlmsg_t dump_filter_nlmsg, void *cookie);
-
 struct btf_ext_info {
 	/*
 	 * info points to the individual info section (e.g. func_info and
