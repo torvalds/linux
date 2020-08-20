8334d69e65f6064aede4fb56fe15ae30630d5337
Paul Lawrence <paullawrence@google.com>
2020-08-20 12:45:41 -0700

    ANDROID: Incremental fs: Separate pseudo-file code
    Also fixed two bugs in the process:
    
    is_pseudo_filename was not previously checking for .log, so an attempt
    to create a .log would succeed.
    
    All ioctls could be called on all files. ioctls now set on the correct
    files.
    
    Bug: 162856396
    Test: incfs_test passes
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: I3f1e87d018836f51a97897880dd70181db4f7169

--- fs/incfs/vfs.h
+++ fs/incfs/vfs.h
@@ -6,8 +6,33 @@
 #ifndef _INCFS_VFS_H
 #define _INCFS_VFS_H
 
+enum FILL_PERMISSION {
+	CANT_FILL = 0,
+	CAN_FILL = 1,
+};
+
+extern const struct file_operations incfs_file_ops;
+extern const struct inode_operations incfs_file_inode_ops;
+
 void incfs_kill_sb(struct super_block *sb);
 struct dentry *incfs_mount_fs(struct file_system_type *type, int flags,
 			      const char *dev_name, void *data);
+int incfs_link(struct dentry *what, struct dentry *where);
+int incfs_unlink(struct dentry *dentry);
+
+static inline struct mount_info *get_mount_info(struct super_block *sb)
+{
+	struct mount_info *result = sb->s_fs_info;
+
+	WARN_ON(!result);
+	return result;
+}
+
+static inline struct super_block *file_superblock(struct file *f)
+{
+	struct inode *inode = file_inode(f);
+
+	return inode->i_sb;
+}
 
 #endif
