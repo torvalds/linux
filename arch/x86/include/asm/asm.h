a850958c072404f75dd41782cb4ff34b8625b47d
Kees Cook <keescook@chromium.org>
2020-08-21 12:43:03 -0700

    x86/asm: Avoid generating unused kprobe sections
    When !CONFIG_KPROBES, do not generate kprobe sections. This makes
    sure there are no unexpected sections encountered by the linker scripts.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20200821194310.3089815-23-keescook@chromium.org

--- arch/x86/include/asm/asm.h
+++ arch/x86/include/asm/asm.h
@@ -138,11 +138,15 @@
 # define _ASM_EXTABLE_FAULT(from, to)				\
 	_ASM_EXTABLE_HANDLE(from, to, ex_handler_fault)
 
-# define _ASM_NOKPROBE(entry)					\
+# ifdef CONFIG_KPROBES
+#  define _ASM_NOKPROBE(entry)					\
 	.pushsection "_kprobe_blacklist","aw" ;			\
 	_ASM_ALIGN ;						\
 	_ASM_PTR (entry);					\
 	.popsection
+# else
+#  define _ASM_NOKPROBE(entry)
+# endif
 
 #else /* ! __ASSEMBLY__ */
 # define _EXPAND_EXTABLE_HANDLE(x) #x
