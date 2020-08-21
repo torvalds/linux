5381b0ed54b6af3c0e8184b43e34154e17904848
Azhar Shaikh <azhar.shaikh@intel.com>
2020-08-21 14:47:22 -0700

    platform/chrome: cros_ec_typec: Send enum values to usb_role_switch_set_role()
    usb_role_switch_set_role() has the second argument as enum for usb_role.
    Currently depending upon the data role i.e. UFP(0) or DFP(1) is sent.
    This eventually translates to USB_ROLE_NONE in case of UFP and
    USB_ROLE_DEVICE in case of DFP. Correct this by sending correct enum
    values as USB_ROLE_DEVICE in case of UFP and USB_ROLE_HOST in case of
    DFP.
    
    Fixes: 7e7def15fa4b ("platform/chrome: cros_ec_typec: Add USB mux control")
    Signed-off-by: Azhar Shaikh <azhar.shaikh@intel.com>
    Cc: Prashant Malani <pmalani@chromium.org>
    Reviewed-by: Prashant Malani <pmalani@chromium.org>
    Reviewed-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

--- drivers/platform/chrome/cros_ec_typec.c
+++ drivers/platform/chrome/cros_ec_typec.c
@@ -622,7 +622,8 @@ static int cros_typec_port_update(struct cros_typec_data *typec, int port_num)
 		dev_warn(typec->dev, "Configure muxes failed, err = %d\n", ret);
 
 	return usb_role_switch_set_role(typec->ports[port_num]->role_sw,
-					!!(resp.role & PD_CTRL_RESP_ROLE_DATA));
+					resp.role & PD_CTRL_RESP_ROLE_DATA
+					? USB_ROLE_HOST : USB_ROLE_DEVICE);
 }
 
 static int cros_typec_get_cmd_version(struct cros_typec_data *typec)
