29aaebbca4abc4cceb38738483051abefafb6950
Chris Wilson <chris@chris-wilson.co.uk>
2020-08-22 17:02:09 +0100

    iommu/vt-d: Handle 36bit addressing for x86-32
    Beware that the address size for x86-32 may exceed unsigned long.
    
    [    0.368971] UBSAN: shift-out-of-bounds in drivers/iommu/intel/iommu.c:128:14
    [    0.369055] shift exponent 36 is too large for 32-bit type 'long unsigned int'
    
    If we don't handle the wide addresses, the pages are mismapped and the
    device read/writes go astray, detected as DMAR faults and leading to
    device failure. The behaviour changed (from working to broken) in commit
    fa954e683178 ("iommu/vt-d: Delegate the dma domain to upper layer"), but
    the error looks older.
    
    Fixes: fa954e683178 ("iommu/vt-d: Delegate the dma domain to upper layer")
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Acked-by: Lu Baolu <baolu.lu@linux.intel.com>
    Cc: James Sewart <jamessewart@arista.com>
    Cc: Lu Baolu <baolu.lu@linux.intel.com>
    Cc: Joerg Roedel <jroedel@suse.de>
    Cc: <stable@vger.kernel.org> # v5.3+
    Link: https://lore.kernel.org/r/20200822160209.28512-1-chris@chris-wilson.co.uk
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -123,29 +123,29 @@ static inline unsigned int level_to_offset_bits(int level)
 	return (level - 1) * LEVEL_STRIDE;
 }
 
-static inline int pfn_level_offset(unsigned long pfn, int level)
+static inline int pfn_level_offset(u64 pfn, int level)
 {
 	return (pfn >> level_to_offset_bits(level)) & LEVEL_MASK;
 }
 
-static inline unsigned long level_mask(int level)
+static inline u64 level_mask(int level)
 {
-	return -1UL << level_to_offset_bits(level);
+	return -1ULL << level_to_offset_bits(level);
 }
 
-static inline unsigned long level_size(int level)
+static inline u64 level_size(int level)
 {
-	return 1UL << level_to_offset_bits(level);
+	return 1ULL << level_to_offset_bits(level);
 }
 
-static inline unsigned long align_to_level(unsigned long pfn, int level)
+static inline u64 align_to_level(u64 pfn, int level)
 {
 	return (pfn + level_size(level) - 1) & level_mask(level);
 }
 
 static inline unsigned long lvl_to_nr_pages(unsigned int lvl)
 {
-	return  1 << min_t(int, (lvl - 1) * LEVEL_STRIDE, MAX_AGAW_PFN_WIDTH);
+	return 1UL << min_t(int, (lvl - 1) * LEVEL_STRIDE, MAX_AGAW_PFN_WIDTH);
 }
 
 /* VT-d pages must always be _smaller_ than MM pages. Otherwise things
