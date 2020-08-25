1fae3d964880a6419c581b5cdb1efb70a0f3a8a2
Christopher Snowhill <chris@kode54.net>
2020-08-25 05:51:08 +0200

    media: Implements support for the MyGica iGrabber
    This small change adds the device identifier for the MyGica iGrabber,
    which features an Empia EM2860, a Philips SAA7113, Empia EMP202, and
    no tuner. Verified that both the composite and S-Video inputs work
    with NTSC source. It does not appear to need any port twiddling on
    pre-init.
    
    Signed-off-by: Christopher Snowhill <chris@kode54.net>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/usb/em28xx/em28xx.h
+++ drivers/media/usb/em28xx/em28xx.h
@@ -151,6 +151,7 @@
 #define EM2882_BOARD_ZOLID_HYBRID_TV_STICK		102
 #define EM2861_BOARD_MAGIX_VIDEOWANDLER2          103
 #define EM28178_BOARD_PCTV_461E_V2                104
+#define EM2860_BOARD_MYGICA_IGRABBER              105
 
 /* Limits minimum and default number of buffers */
 #define EM28XX_MIN_BUF 4
