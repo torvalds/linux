6b7898eb180df12767933466b7855b23103ad489
Jens Axboe <axboe@kernel.dk>
2020-08-25 07:58:00 -0600

    io_uring: fix imbalanced sqo_mm accounting
    We do the initial accounting of locked_vm and pinned_vm before we have
    setup ctx->sqo_mm, which means we can end up having not accounted the
    memory at setup time, but still decrement it when we exit. This causes
    an imbalance in the accounting.
    
    Setup ctx->sqo_mm earlier in io_uring_create(), before we do the first
    accounting of mm->{locked,pinned}_vm. This also unifies the state
    grabbing for the ctx, and eliminates a failure case in
    io_sq_offload_start().
    
    Fixes: f74441e6311a ("io_uring: account locked memory before potential error case")
    Reported-by: Robert M. Muncrief <rmuncrief@humanavance.com>
    Reported-by: Niklas Schnelle <schnelle@linux.ibm.com>
    Tested-by: Niklas Schnelle <schnelle@linux.ibm.com>
    Tested-by: Robert M. Muncrief <rmuncrief@humanavance.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -7447,9 +7447,6 @@ static int io_sq_offload_start(struct io_ring_ctx *ctx,
 {
 	int ret;
 
-	mmgrab(current->mm);
-	ctx->sqo_mm = current->mm;
-
 	if (ctx->flags & IORING_SETUP_SQPOLL) {
 		ret = -EPERM;
 		if (!capable(CAP_SYS_ADMIN))
@@ -7494,10 +7491,6 @@ static int io_sq_offload_start(struct io_ring_ctx *ctx,
 	return 0;
 err:
 	io_finish_async(ctx);
-	if (ctx->sqo_mm) {
-		mmdrop(ctx->sqo_mm);
-		ctx->sqo_mm = NULL;
-	}
 	return ret;
 }
 
@@ -8546,6 +8539,9 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 	ctx->compat = in_compat_syscall();
 	ctx->user = user;
 	ctx->creds = get_current_cred();
+
+	mmgrab(current->mm);
+	ctx->sqo_mm = current->mm;
 
 	/*
 	 * Account memory _before_ installing the file descriptor. Once
