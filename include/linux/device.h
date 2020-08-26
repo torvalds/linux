e1f82a0dcf388d98bcc7ad195c03bd812405e6b2
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2020-08-26 13:44:59 +0300

    driver core: Annotate dev_err_probe() with __must_check
    We have got already new users of this API which interpret it differently
    and miss the opportunity to optimize their code.
    
    In order to avoid similar cases in the future, annotate dev_err_probe()
    with __must_check.
    
    Fixes: a787e5400a1c ("driver core: add device probe log helper")
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Link: https://lore.kernel.org/r/20200826104459.81979-1-andriy.shevchenko@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/device.h
+++ include/linux/device.h
@@ -931,7 +931,7 @@ void device_links_supplier_sync_state_pause(void);
 void device_links_supplier_sync_state_resume(void);
 
 extern __printf(3, 4)
-int dev_err_probe(const struct device *dev, int err, const char *fmt, ...);
+int __must_check dev_err_probe(const struct device *dev, int err, const char *fmt, ...);
 
 /* Create alias, so I can be autoloaded. */
 #define MODULE_ALIAS_CHARDEV(major,minor) \
