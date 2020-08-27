ea2523492072636c024a5f3bf52f0371b4f1f6ba
Linus Walleij <linus.walleij@linaro.org>
2020-08-27 10:48:28 +0200

    BACKPORT: power: supply: gpio-charger: Convert to GPIO descriptors
    This converts the GPIO charger to use exclusively GPIO
    descriptors, moving the two remaining platforms passing
    global GPIO numbers over to using a GPIO descriptor table.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Cc: Robert Jarzmik <robert.jarzmik@free.fr>
    Cc: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>
    
     Conflicts:
    	arch/arm/mach-pxa/tosa.c
            arch/arm/mach-sa1100/collie.c
            drivers/power/supply/gpio-charger.c
    
    (cherry picked from commit 17529bcf0ae20f1ac6d7846762bf0c6ad91dbb7f)
    Signed-off-by: Ziyuan Xu <xzy.xu@rock-chips.com>
    Change-Id: I22fb3dc7d42163d7a715d219665a14474cdd7422

--- include/linux/power/gpio-charger.h
+++ include/linux/power/gpio-charger.h
@@ -22,18 +22,12 @@
  * struct gpio_charger_platform_data - platform_data for gpio_charger devices
  * @name:		Name for the chargers power_supply device
  * @type:		Type of the charger
- * @gpio:		GPIO which is used to indicate the chargers status
- * @gpio_active_low:	Should be set to 1 if the GPIO is active low otherwise 0
  * @supplied_to:	Array of battery names to which this chargers supplies power
  * @num_supplicants:	Number of entries in the supplied_to array
  */
 struct gpio_charger_platform_data {
 	const char *name;
 	enum power_supply_type type;
-
-	int gpio;
-	int gpio_active_low;
-
 	char **supplied_to;
 	size_t num_supplicants;
 };
