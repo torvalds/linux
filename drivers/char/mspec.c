fe69795e84a553b838504e0acdb4a57c3fbdd200
Denis Efremov <efremov@linux.com>
2020-08-28 00:34:21 +0300

    char: mspec: Use kvzalloc() in mspec_mmap()
    Use kvzalloc() in mspec_mmap() instead of open-coding it.
    
    Signed-off-by: Denis Efremov <efremov@linux.com>
    Link: https://lore.kernel.org/r/20200827213421.50429-1-efremov@linux.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/char/mspec.c
+++ drivers/char/mspec.c
@@ -195,10 +195,7 @@ mspec_mmap(struct file *file, struct vm_area_struct *vma,
 
 	pages = vma_pages(vma);
 	vdata_size = sizeof(struct vma_data) + pages * sizeof(long);
-	if (vdata_size <= PAGE_SIZE)
-		vdata = kzalloc(vdata_size, GFP_KERNEL);
-	else
-		vdata = vzalloc(vdata_size);
+	vdata = kvzalloc(vdata_size, GFP_KERNEL);
 	if (!vdata)
 		return -ENOMEM;
 
