e2d1baca2b3396371db0a6e1643e487519122069
Nicolas Dichtel <nicolas.dichtel@6wind.com>
2020-08-28 15:30:55 +0200

    gtp: remove useless rcu_read_lock()
    The rtnl lock is taken just the line above, no need to take the rcu also.
    
    Fixes: 1788b8569f5d ("gtp: fix use-after-free in gtp_encap_destroy()")
    Signed-off-by: Nicolas Dichtel <nicolas.dichtel@6wind.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/gtp.c
+++ drivers/net/gtp.c
@@ -1071,7 +1071,6 @@ static int gtp_genl_new_pdp(struct sk_buff *skb, struct genl_info *info)
 	}
 
 	rtnl_lock();
-	rcu_read_lock();
 
 	gtp = gtp_find_dev(sock_net(skb->sk), info->attrs);
 	if (!gtp) {
@@ -1100,7 +1099,6 @@ static int gtp_genl_new_pdp(struct sk_buff *skb, struct genl_info *info)
 	}
 
 out_unlock:
-	rcu_read_unlock();
 	rtnl_unlock();
 	return err;
 }
