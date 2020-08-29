75ae04206a4d0e4f541c1d692b7febd1c0fdb814
Helge Deller <deller@gmx.de>
2020-08-29 14:11:35 +0200

    parisc: Define O_NONBLOCK to become 000200000
    HPUX has separate NDELAY & NONBLOCK values. In the past we wanted to
    be able to run HP-UX binaries natively on parisc Linux which is why
    we defined O_NONBLOCK to 000200004 to distinguish NDELAY & NONBLOCK
    bits.
    But with 2 bits set in this bitmask we often ran into compatibility
    issues with other Linux applications which often only test one bit (or
    even compare the values).
    
    To avoid such issues in the future, this patch changes O_NONBLOCK to
    become 000200000. That way old programs will still be functional, and
    for new programs we now have only one bit set.
    
    Update the comment about SOCK_NONBLOCK too.
    
    Signed-off-by: Helge Deller <deller@gmx.de>

--- arch/parisc/include/uapi/asm/fcntl.h
+++ arch/parisc/include/uapi/asm/fcntl.h
@@ -8,7 +8,7 @@
 #define O_LARGEFILE	000004000
 #define __O_SYNC	000100000
 #define O_SYNC		(__O_SYNC|O_DSYNC)
-#define O_NONBLOCK	000200004 /* HPUX has separate NDELAY & NONBLOCK */
+#define O_NONBLOCK	000200000
 #define O_NOCTTY	000400000 /* not fcntl */
 #define O_DSYNC		001000000
 #define O_NOATIME	004000000
