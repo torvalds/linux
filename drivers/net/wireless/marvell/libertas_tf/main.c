2c92790b12033797474b9ba1b2e56a95360f66cd
YueHaibing <yuehaibing@huawei.com>
2020-08-29 19:59:24 +0800

    libertas_tf: Remove unused macro QOS_CONTROL_LEN
    There is no caller in tree.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200829115924.7572-1-yuehaibing@huawei.com

--- drivers/net/wireless/marvell/libertas_tf/main.c
+++ drivers/net/wireless/marvell/libertas_tf/main.c
@@ -15,7 +15,6 @@
 /* thinfirm version: 5.132.X.pX */
 #define LBTF_FW_VER_MIN		0x05840300
 #define LBTF_FW_VER_MAX		0x0584ffff
-#define QOS_CONTROL_LEN		2
 
 /* Module parameters */
 unsigned int lbtf_debug;
