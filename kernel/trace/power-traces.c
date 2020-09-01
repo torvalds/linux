0bbae53bf635d40f07010d56140d57834e0b2909
Sidath Senanayake <sidaths@google.com>
2020-09-01 23:56:01 +0100

    ANDROID: perf: Export clock_set_rate tracepoint
    This allows for clock rate changes to be reported from
    loadable kernel modules.
    
    Bug: 167449286
    Bug: 166776693
    Change-Id: I30eb2d742aecedf3638334f85f510a69b71acb84
    Signed-off-by: Sidath Senanayake <sidaths@google.com>
    (cherry picked from commit daddf4fb363cbc5a183fc62fcfdd0b62748a8c26)
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- kernel/trace/power-traces.c
+++ kernel/trace/power-traces.c
@@ -20,3 +20,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(cpu_frequency);
 EXPORT_TRACEPOINT_SYMBOL_GPL(powernv_throttle);
 EXPORT_TRACEPOINT_SYMBOL_GPL(device_pm_callback_start);
 EXPORT_TRACEPOINT_SYMBOL_GPL(device_pm_callback_end);
+EXPORT_TRACEPOINT_SYMBOL_GPL(clock_set_rate);
