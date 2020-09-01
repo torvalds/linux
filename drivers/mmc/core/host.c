a920d2b2cfb708e2d9254e7940b35538bcff43a5
Matthias Schiffer <matthias.schiffer@ew.tq-group.com>
2020-09-01 10:50:04 +0200

    UPSTREAM: mmc: core: Allow setting slot index via device tree alias
    As with GPIO, UART and others, allow specifying the device index via the
    aliases node in the device tree.
    
    On embedded devices, there is often a combination of removable (e.g.
    SD card) and non-removable MMC devices (e.g. eMMC).
    Therefore the index might change depending on
    
    * host of removable device
    * removable card present or not
    
    This makes it difficult to hardcode the root device, if it is on the
    non-removable device. E.g. if SD card is present eMMC will be mmcblk1,
    if SD card is not present at boot, eMMC will be mmcblk0.
    
    Alternative solutions like PARTUUIDs do not cover the case where multiple
    mmcblk devices contain the same image. This is a common issue on devices
    that can boot both from eMMC (for regular boot) and SD cards (as a
    temporary boot medium for development). When a firmware image is
    installed to eMMC after a test boot via SD card, there will be no
    reliable way to refer to a specific device using (PART)UUIDs oder
    LABELs.
    
    The demand for this feature has led to multiple attempts to implement
    it, dating back at least to 2012 (see
    https://www.spinics.net/lists/linux-mmc/msg26586.html for a previous
    discussion from 2014).
    
    All indices defined in the aliases node will be reserved for use by the
    respective MMC device, moving the indices of devices that don't have an
    alias up into the non-reserved range. If the aliases node is not found,
    the driver will act as before.
    
    This is a rebased and cleaned up version of
    https://www.spinics.net/lists/linux-mmc/msg26588.html .
    
    Based-on-patch-by: Sascha Hauer <s.hauer@pengutronix.de>
    Link: https://lkml.org/lkml/2020/8/5/194
    Signed-off-by: Matthias Schiffer <matthias.schiffer@ew.tq-group.com>
    Link: https://lore.kernel.org/r/20200901085004.2512-2-matthias.schiffer@ew.tq-group.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    
     Conflicts:
    	drivers/mmc/core/host.c
    
    Change-Id: I86a98bab7767d630172a552e0dc72d90aaebc2a3
    (cherry picked from commit fa2d0aa96941e8dc347e49a04c75468e3002ee20)
    Signed-off-by: Ziyuan Xu <xzy.xu@rock-chips.com>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -366,6 +366,20 @@ int mmc_of_parse(struct mmc_host *host)
 EXPORT_SYMBOL(mmc_of_parse);
 
 /**
+ * mmc_first_nonreserved_index() - get the first index that is not reserved
+ */
+static int mmc_first_nonreserved_index(void)
+{
+	int max;
+
+	max = of_alias_get_highest_id("mmc");
+	if (max < 0)
+		return 0;
+
+	return max + 1;
+}
+
+/**
  *	mmc_alloc_host - initialise the per-host structure.
  *	@extra: sizeof private data structure
  *	@dev: pointer to host device model structure
@@ -376,6 +390,7 @@ struct mmc_host *mmc_alloc_host(int extra, struct device *dev)
 {
 	int err;
 	struct mmc_host *host;
+	int alias_id, min_idx, max_idx;
 
 	host = kzalloc(sizeof(struct mmc_host) + extra, GFP_KERNEL);
 	if (!host)
@@ -384,7 +399,16 @@ struct mmc_host *mmc_alloc_host(int extra, struct device *dev)
 	/* scanning will be enabled when we're ready */
 	host->rescan_disable = 1;
 
-	err = ida_simple_get(&mmc_host_ida, 0, 0, GFP_KERNEL);
+	alias_id = of_alias_get_id(dev->of_node, "mmc");
+	if (alias_id >= 0) {
+		min_idx = alias_id;
+		max_idx = alias_id + 1;
+	} else {
+		min_idx = mmc_first_nonreserved_index();
+		max_idx = 0;
+	}
+
+	err = ida_simple_get(&mmc_host_ida, min_idx, max_idx, GFP_KERNEL);
 	if (err < 0) {
 		kfree(host);
 		return NULL;
