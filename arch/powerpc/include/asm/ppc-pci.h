b3abe590c80e0ba55b6fce48762232d90dbc37a5
Oliver O'Halloran <oohall@gmail.com>
2020-09-02 13:51:38 +1000

    powerpc/pci: Remove unimplemented prototypes
    The corresponding definitions were deleted in commit 3d5134ee8341
    ("[POWERPC] Rewrite IO allocation & mapping on powerpc64") which
    was merged a mere 13 years ago.
    
    Signed-off-by: Oliver O'Halloran <oohall@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200902035138.1762531-1-oohall@gmail.com

--- arch/powerpc/include/asm/ppc-pci.h
+++ arch/powerpc/include/asm/ppc-pci.h
@@ -13,10 +13,6 @@
 
 extern unsigned long isa_io_base;
 
-extern void pci_setup_phb_io(struct pci_controller *hose, int primary);
-extern void pci_setup_phb_io_dynamic(struct pci_controller *hose, int primary);
-
-
 extern struct list_head hose_list;
 
 extern struct pci_dev *isa_bridge_pcidev;	/* may be NULL if no ISA bus */
