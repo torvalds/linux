65a24d4c2712182a3ca450cf9f019ee5ab332533
Xu Wang <vulab@iscas.ac.cn>
2020-09-03 08:27:19 +0000

    Bluetooth: hci_qca: remove redundant null check
    Because clk_disable_unprepare already checked
    NULL clock parameter, so the additional check is
    unnecessary, just remove it.
    
    Signed-off-by: Xu Wang <vulab@iscas.ac.cn>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/hci_qca.c
+++ drivers/bluetooth/hci_qca.c
@@ -2007,8 +2007,7 @@ static int qca_serdev_probe(struct serdev_device *serdev)
 		err = hci_uart_register_device(&qcadev->serdev_hu, &qca_proto);
 		if (err) {
 			BT_ERR("Rome serdev registration failed");
-			if (qcadev->susclk)
-				clk_disable_unprepare(qcadev->susclk);
+			clk_disable_unprepare(qcadev->susclk);
 			return err;
 		}
 	}
