d5653a994a665c66f26a92b57a1d4dcb3639c9ea
Robin Murphy <robin.murphy@arm.com>
2020-09-03 22:04:03 +0100

    drm/msm: Drop local dma_parms
    Since commit 9495b7e92f71 ("driver core: platform: Initialize dma_parms
    for platform devices"), struct platform_device already provides a
    dma_parms structure, so we can save allocating another one.
    
    Also the DMA segment size is simply a size, not a bitmask.
    
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -453,15 +453,7 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 	if (ret)
 		goto err_msm_uninit;
 
-	if (!dev->dma_parms) {
-		dev->dma_parms = devm_kzalloc(dev, sizeof(*dev->dma_parms),
-					      GFP_KERNEL);
-		if (!dev->dma_parms) {
-			ret = -ENOMEM;
-			goto err_msm_uninit;
-		}
-	}
-	dma_set_max_seg_size(dev, DMA_BIT_MASK(32));
+	dma_set_max_seg_size(dev, UINT_MAX);
 
 	msm_gem_shrinker_init(ddev);
 
