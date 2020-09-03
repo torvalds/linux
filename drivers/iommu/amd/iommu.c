e52d58d54a321d4fe9d0ecdabe4f8774449f0d6e
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2020-09-03 09:38:22 +0000

    iommu/amd: Use cmpxchg_double() when updating 128-bit IRTE
    When using 128-bit interrupt-remapping table entry (IRTE) (a.k.a GA mode),
    current driver disables interrupt remapping when it updates the IRTE
    so that the upper and lower 64-bit values can be updated safely.
    
    However, this creates a small window, where the interrupt could
    arrive and result in IO_PAGE_FAULT (for interrupt) as shown below.
    
      IOMMU Driver            Device IRQ
      ============            ===========
      irte.RemapEn=0
           ...
       change IRTE            IRQ from device ==> IO_PAGE_FAULT !!
           ...
      irte.RemapEn=1
    
    This scenario has been observed when changing irq affinity on a system
    running I/O-intensive workload, in which the destination APIC ID
    in the IRTE is updated.
    
    Instead, use cmpxchg_double() to update the 128-bit IRTE at once without
    disabling the interrupt remapping. However, this means several features,
    which require GA (128-bit IRTE) support will also be affected if cmpxchg16b
    is not supported (which is unprecedented for AMD processors w/ IOMMU).
    
    Fixes: 880ac60e2538 ("iommu/amd: Introduce interrupt remapping ops structure")
    Reported-by: Sean Osborne <sean.m.osborne@oracle.com>
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Tested-by: Erik Rockstrom <erik.rockstrom@oracle.com>
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>
    Link: https://lore.kernel.org/r/20200903093822.52012-3-suravee.suthikulpanit@amd.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -3292,6 +3292,7 @@ out:
 static int modify_irte_ga(u16 devid, int index, struct irte_ga *irte,
 			  struct amd_ir_data *data)
 {
+	bool ret;
 	struct irq_remap_table *table;
 	struct amd_iommu *iommu;
 	unsigned long flags;
@@ -3309,10 +3310,18 @@ static int modify_irte_ga(u16 devid, int index, struct irte_ga *irte,
 
 	entry = (struct irte_ga *)table->table;
 	entry = &entry[index];
-	entry->lo.fields_remap.valid = 0;
-	entry->hi.val = irte->hi.val;
-	entry->lo.val = irte->lo.val;
-	entry->lo.fields_remap.valid = 1;
+
+	ret = cmpxchg_double(&entry->lo.val, &entry->hi.val,
+			     entry->lo.val, entry->hi.val,
+			     irte->lo.val, irte->hi.val);
+	/*
+	 * We use cmpxchg16 to atomically update the 128-bit IRTE,
+	 * and it cannot be updated by the hardware or other processors
+	 * behind us, so the return value of cmpxchg16 should be the
+	 * same as the old value.
+	 */
+	WARN_ON(!ret);
+
 	if (data)
 		data->ref = entry;
 
