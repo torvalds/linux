ea68573d408fdf8df31dc93e826c35a1f6218f36
Alex Deucher <alexander.deucher@amd.com>
2020-09-06 12:05:12 -0400

    drm/amdgpu: Fail to load on RAVEN if SME is active
    Due to hardware bugs, scatter/gather display on raven requires
    a 1:1 IOMMU mapping, however, SME (System Memory Encryption)
    requires an indirect IOMMU mapping because the encryption bit
    is beyond the DMA mask of the chip.  As such, the two are
    incompatible.
    
    Acked-by: Joerg Roedel <jroedel@suse.de>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Acked-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -1101,6 +1101,16 @@ static int amdgpu_pci_probe(struct pci_dev *pdev,
 		return -ENODEV;
 	}
 
+	/* Due to hardware bugs, S/G Display on raven requires a 1:1 IOMMU mapping,
+	 * however, SME requires an indirect IOMMU mapping because the encryption
+	 * bit is beyond the DMA mask of the chip.
+	 */
+	if (mem_encrypt_active() && ((flags & AMD_ASIC_MASK) == CHIP_RAVEN)) {
+		dev_info(&pdev->dev,
+			 "SME is not compatible with RAVEN\n");
+		return -ENOTSUPP;
+	}
+
 #ifdef CONFIG_DRM_AMDGPU_SI
 	if (!amdgpu_si_support) {
 		switch (flags & AMD_ASIC_MASK) {
