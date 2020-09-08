936303f8eace3d0a3f7f61ff00c0cd0b6f328ac1
Thomas Gleixner <tglx@linutronix.de>
2020-09-08 14:34:50 +0200

    net/qlge: Convert to SPDX license identifiers
    All files related to this driver contain the following notice:
    
      See LICENSE.qlge for copyright and licensing details.
    
    LICENSE.qlge can be found in
    Documentation/networking/device_drivers/qlogic/. The file contains:
    
      - A copyright notice
    
        This copyright notice is redundant as all files contain the same
        copyright notice already
    
      - A license notice
    
        You may modify and redistribute the device driver code under the
        GNU General Public License (a copy of which is attached hereto as
        Exhibit A) published by the Free Software Foundation (version 2).
    
        This can be replaced with the corresponding SPDX license identifier
        (GPL-2.0-only) in the source files which reference this license
        file.
    
      - The full GPLv2 license text
    
        A redundant copy of LICENSES/preferred/GPL-2.0
    
    Remove the notices and add the SPDX license identifier GPL-2.0-only to the
    source files.
    
    Finally remove the now redundant LICENSE.qlge file.
    
    Reviewed-by: Richard Fontana <rfontana@redhat.com>
    Reviewed-by: Jilayne Lovejoy <opensource@jilayne.com>
    Reviewed-by: Alexios Zavras <alexios.zavras@intel.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Lukas Bulwahn <lukas.bulwahn@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/qlge/qlge.h
+++ drivers/staging/qlge/qlge.h
@@ -1,8 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * QLogic QLA41xx NIC HBA Driver
  * Copyright (c)  2003-2006 QLogic Corporation
- *
- * See LICENSE.qlge for copyright and licensing details.
  */
 #ifndef _QLGE_H_
 #define _QLGE_H_
