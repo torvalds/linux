31a3271ff11badc50d6dc0efb4da393e4c3bf388
Daniel Wagner <dwagner@suse.de>
2020-09-08 10:15:16 +0200

    scsi: qla2xxx: Handle incorrect entry_type entries
    It was observed on an ISP8324 16Gb HBA with fw=8.08.203 (d0d5) in a
    PowerPC64 machine that pkt->entry_type was MBX_IOCB_TYPE/0x39 with an
    sp->type SRB_SCSI_CMD which is invalid and should not be possible.
    
    Reading the entry_type from the crash dump shows the expected value of
    STATUS_TYPE/0x03 but the call trace shows that qla24xx_mbx_iocb_entry() is
    used.
    
    Add a check to verify for consistency and reset the HBA if an invalid state
    is reached. Obviously, this is only a workaround until the real problem is
    solved.
    
    Link: https://lore.kernel.org/r/20200908081516.8561-5-dwagner@suse.de
    Reviewed-by: Arun Easi <aeasi@marvell.com>
    Signed-off-by: Daniel Wagner <dwagner@suse.de>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/qla2xxx/qla_isr.c
+++ drivers/scsi/qla2xxx/qla_isr.c
@@ -3406,6 +3406,32 @@ void qla24xx_nvme_ls4_iocb(struct scsi_qla_host *vha,
 	sp->done(sp, comp_status);
 }
 
+static void qla24xx_process_mbx_iocb_response(struct scsi_qla_host *vha,
+	struct rsp_que *rsp, struct sts_entry_24xx *pkt)
+{
+	struct qla_hw_data *ha = vha->hw;
+	srb_t *sp;
+	static const char func[] = "MBX-IOCB2";
+
+	sp = qla2x00_get_sp_from_handle(vha, func, rsp->req, pkt);
+	if (!sp)
+		return;
+
+	if (sp->type == SRB_SCSI_CMD ||
+	    sp->type == SRB_NVME_CMD ||
+	    sp->type == SRB_TM_CMD) {
+		ql_log(ql_log_warn, vha, 0x509d,
+			"Inconsistent event entry type %d\n", sp->type);
+		if (IS_P3P_TYPE(ha))
+			set_bit(FCOE_CTX_RESET_NEEDED, &vha->dpc_flags);
+		else
+			set_bit(ISP_ABORT_NEEDED, &vha->dpc_flags);
+		return;
+	}
+
+	qla24xx_mbx_iocb_entry(vha, rsp->req, (struct mbx_24xx_entry *)pkt);
+}
+
 /**
  * qla24xx_process_response_queue() - Process response queue entries.
  * @vha: SCSI driver HA context
@@ -3513,8 +3539,7 @@ process_err:
 			    (struct abort_entry_24xx *)pkt);
 			break;
 		case MBX_IOCB_TYPE:
-			qla24xx_mbx_iocb_entry(vha, rsp->req,
-			    (struct mbx_24xx_entry *)pkt);
+			qla24xx_process_mbx_iocb_response(vha, rsp, pkt);
 			break;
 		case VP_CTRL_IOCB_TYPE:
 			qla_ctrlvp_completed(vha, rsp->req,
