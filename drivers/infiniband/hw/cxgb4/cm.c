8d539c6109f203bb237c7c1eb73d4665869bab1a
Potnuri Bharat Teja <bharat@chelsio.com>
2020-09-09 21:07:07 +0530

    RDMA/iw_cxgb4: Disable delayed ack by default
    Receive side delayed ack mode is needed only for certain area networks/
    connections. Therefore disable it by default.
    
    Link: https://lore.kernel.org/r/20200909153707.2795-1-bharat@chelsio.com
    Signed-off-by: Potnuri Bharat Teja <bharat@chelsio.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/infiniband/hw/cxgb4/cm.c
+++ drivers/infiniband/hw/cxgb4/cm.c
@@ -77,9 +77,9 @@ static int enable_ecn;
 module_param(enable_ecn, int, 0644);
 MODULE_PARM_DESC(enable_ecn, "Enable ECN (default=0/disabled)");
 
-static int dack_mode = 1;
+static int dack_mode;
 module_param(dack_mode, int, 0644);
-MODULE_PARM_DESC(dack_mode, "Delayed ack mode (default=1)");
+MODULE_PARM_DESC(dack_mode, "Delayed ack mode (default=0)");
 
 uint c4iw_max_read_depth = 32;
 module_param(c4iw_max_read_depth, int, 0644);
