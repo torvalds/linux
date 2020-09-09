7025329d208cae45937d2a0910786a45b9981475
Ben Ben-Ishay <benishay@nvidia.com>
2020-09-09 17:36:39 +0300

    net/mlx5: Add SHAMPO caps, HW bits and enumerations
    This commit adds SHAMPO bit to hca_cap and SHAMPO capabilities structure,
    SHAMPO related HW spec hardware fields and enumerations.
    SHAMPO stands for: split headers and merge payload offload.
    SHAMPO new fields:
    WQ:
     - headers_mkey: mkey that represents the headers buffer, where the packets
       headers will be written by the HW.
    
     - shampo_enable: flag to verify if the WQ supports SHAMPO feature.
    
     - log_reservation_size: the log of the reservation size where the data of
       the packet will be written by the HW.
    
     - log_max_num_of_packets_per_reservation: log of the maximum number of
       packets that can be written to the same reservation.
    
     - log_headers_entry_size: log of the header entry size of the headers buffer.
    
     - log_headers_buffer_entry_num: log of the entries number of the headers buffer.
    
    RQ:
     - shampo_no_match_alignment_granularity: the HW alignment granularity
       in case the received packet doesn't match the current session.
    
     - shampo_match_criteria_type: the type of match criteria.
    
     - reservation_timeout: the maximum time that the HW will hold the
       reservation.
    
    mlx5_ifc_shampo_cap_bits, the capabilities of the SHAMPO feature:
     - shampo_log_max_reservation_size: the maximum allowed value of the field
       WQ.log_reservation_size.
    
     - log_reservation_size: the minimum allowed value of the field
       WQ.log_reservation_size.
    
     - shampo_min_mss_size: the minimum payload size of packet that can open
       a new session or be merged to a session.
    
     - shampo_max_log_headers_entry_size: the maximum allowed value of the field
       WQ.log_headers_entry_size
    
    Signed-off-by: Ben Ben-Ishay <benishay@nvidia.com>
    Reviewed-by: Tariq Toukan <tariqt@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>

--- include/linux/mlx5/device.h
+++ include/linux/mlx5/device.h
@@ -1186,6 +1186,7 @@ enum mlx5_cap_type {
 	MLX5_CAP_VDPA_EMULATION = 0x13,
 	MLX5_CAP_DEV_EVENT = 0x14,
 	MLX5_CAP_IPSEC,
+	MLX5_CAP_DEV_SHAMPO = 0x1d,
 	MLX5_CAP_GENERAL_2 = 0x20,
 	MLX5_CAP_PORT_SELECTION = 0x25,
 	/* NUM OF CAP Types */
@@ -1430,6 +1431,9 @@ enum mlx5_qcam_feature_groups {
 
 #define MLX5_CAP_IPSEC(mdev, cap)\
 	MLX5_GET(ipsec_cap, (mdev)->caps.hca[MLX5_CAP_IPSEC]->cur, cap)
+
+#define MLX5_CAP_DEV_SHAMPO(mdev, cap)\
+	MLX5_GET(shampo_cap, mdev->caps.hca_cur[MLX5_CAP_DEV_SHAMPO], cap)
 
 enum {
 	MLX5_CMD_STAT_OK			= 0x0,
