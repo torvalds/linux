12856e7acde4702b7c3238c15fcba86ff6aa507f
Matthew Rosato <mjrosato@linux.ibm.com>
2020-09-10 10:59:55 -0400

    PCI/IOV: Mark VFs as not implementing PCI_COMMAND_MEMORY
    For VFs, the Memory Space Enable bit in the Command Register is
    hard-wired to 0.
    
    Add a new bit to signify devices where the Command Register Memory
    Space Enable bit does not control the device's response to MMIO
    accesses.
    
    Fixes: abafbc551fdd ("vfio-pci: Invalidate mmaps and block MMIO access on disabled memory")
    Signed-off-by: Matthew Rosato <mjrosato@linux.ibm.com>
    Acked-by: Bjorn Helgaas <bhelgaas@google.com>
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -445,6 +445,7 @@ struct pci_dev {
 	unsigned int	is_probed:1;		/* Device probing in progress */
 	unsigned int	link_active_reporting:1;/* Device capable of reporting link active */
 	unsigned int	no_vf_scan:1;		/* Don't scan for VFs after IOV enablement */
+	unsigned int	no_command_memory:1;	/* No PCI_COMMAND_MEMORY */
 	pci_dev_flags_t dev_flags;
 	atomic_t	enable_cnt;	/* pci_enable_device has been called */
 
