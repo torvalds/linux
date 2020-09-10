71957b6112cdb0b3357fbfe6d0de5de02d544a55
Stanley Chu <stanley.chu@mediatek.com>
2020-09-10 09:37:56 +0800

    scsi: ufs: ufs-mediatek: Fix build warnings with make W=1
    Fix build warnings with make W=1 as below,
    
    1.
    >> drivers/scsi/ufs/ufs-mediatek.c:116:22: warning: format '%d' expects
    >> argument of type 'int', but argument 4 has type 'long int'
    
    2.
      CC [M]  drivers/scsi/ufs/ufs-mediatek.o
    ../drivers/scsi/ufs/ufs-mediatek.c:749: error: Cannot parse struct or union!
    
    /** is used specifically with kernel-doc tool.
    As a quick fix by removing dubious /** in the comment block of
    struct ufs_hba_variant_ops ufs_hba_mtk_vops.
    
    Link: https://lore.kernel.org/r/20200910013756.11385-1-stanley.chu@mediatek.com
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/ufs/ufs-mediatek.c
+++ drivers/scsi/ufs/ufs-mediatek.c
@@ -113,7 +113,7 @@ static void ufs_mtk_init_reset_control(struct ufs_hba *hba,
 {
 	*rc = devm_reset_control_get(hba->dev, str);
 	if (IS_ERR(*rc)) {
-		dev_info(hba->dev, "Failed to get reset control %s: %d\n",
+		dev_info(hba->dev, "Failed to get reset control %s: %ld\n",
 			 str, PTR_ERR(*rc));
 		*rc = NULL;
 	}
@@ -727,13 +727,13 @@ static void ufs_mtk_fixup_dev_quirks(struct ufs_hba *hba)
 	ufshcd_fixup_dev_quirks(hba, ufs_mtk_dev_fixups);
 }
 
-/**
+/*
  * struct ufs_hba_mtk_vops - UFS MTK specific variant operations
  *
  * The variant operations configure the necessary controller and PHY
  * handshake during initialization.
  */
-static struct ufs_hba_variant_ops ufs_hba_mtk_vops = {
+static const struct ufs_hba_variant_ops ufs_hba_mtk_vops = {
 	.name                = "mediatek.ufshci",
 	.init                = ufs_mtk_init,
 	.setup_clocks        = ufs_mtk_setup_clocks,
