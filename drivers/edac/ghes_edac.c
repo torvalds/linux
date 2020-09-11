251c54ea26fa6029b01a76161a37a12fde5124e4
Borislav Petkov <bp@suse.de>
2020-09-11 18:17:30 +0200

    EDAC/ghes: Check whether the driver is on the safe list correctly
    With CONFIG_DEBUG_TEST_DRIVER_REMOVE=y, a system would try to probe,
    unregister and probe again a driver.
    
    When ghes_edac is attempted to be loaded on a system which is not on
    the safe platforms list, ghes_edac_register() would return early. The
    unregister counterpart ghes_edac_unregister() would still attempt to
    unregister and exit early at the refcount test, leading to the refcount
    underflow below.
    
    In order to not do *anything* on the unregister path too, reuse the
    force_load parameter and check it on that path too, before fumbling with
    the refcount.
    
      ghes_edac: ghes_edac_register: entry
      ghes_edac: ghes_edac_register: return -ENODEV
      ------------[ cut here ]------------
      refcount_t: underflow; use-after-free.
      WARNING: CPU: 10 PID: 1 at lib/refcount.c:28 refcount_warn_saturate+0xb9/0x100
      Modules linked in:
      CPU: 10 PID: 1 Comm: swapper/0 Not tainted 5.9.0-rc4+ #12
      Hardware name: GIGABYTE MZ01-CE1-00/MZ01-CE1-00, BIOS F02 08/29/2018
      RIP: 0010:refcount_warn_saturate+0xb9/0x100
      Code: 82 e8 fb 8f 4d 00 90 0f 0b 90 90 c3 80 3d 55 4c f5 00 00 75 88 c6 05 4c 4c f5 00 01 90 48 c7 c7 d0 8a 10 82 e8 d8 8f 4d 00 90 <0f> 0b 90 90 c3 80 3d 30 4c f5 00 00 0f 85 61 ff ff ff c6 05 23 4c
      RSP: 0018:ffffc90000037d58 EFLAGS: 00010292
      RAX: 0000000000000026 RBX: ffff88840b8da000 RCX: 0000000000000000
      RDX: 0000000000000001 RSI: ffffffff8216b24f RDI: 00000000ffffffff
      RBP: ffff88840c662e00 R08: 0000000000000001 R09: 0000000000000001
      R10: 0000000000000001 R11: 0000000000000046 R12: 0000000000000000
      R13: 0000000000000001 R14: 0000000000000000 R15: 0000000000000000
      FS:  0000000000000000(0000) GS:ffff88840ee80000(0000) knlGS:0000000000000000
      CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
      CR2: 0000000000000000 CR3: 0000800002211000 CR4: 00000000003506e0
      Call Trace:
       ghes_edac_unregister
       ghes_remove
       platform_drv_remove
       really_probe
       driver_probe_device
       device_driver_attach
       __driver_attach
       ? device_driver_attach
       ? device_driver_attach
       bus_for_each_dev
       bus_add_driver
       driver_register
       ? bert_init
       ghes_init
       do_one_initcall
       ? rcu_read_lock_sched_held
       kernel_init_freeable
       ? rest_init
       kernel_init
       ret_from_fork
       ...
      ghes_edac: ghes_edac_unregister: FALSE, refcount: -1073741824
    
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lkml.kernel.org/r/20200911164950.GB19320@zn.tnic

--- drivers/edac/ghes_edac.c
+++ drivers/edac/ghes_edac.c
@@ -508,6 +508,7 @@ int ghes_edac_register(struct ghes *ghes, struct device *dev)
 		if (!force_load && idx < 0)
 			return -ENODEV;
 	} else {
+		force_load = true;
 		idx = 0;
 	}
 
@@ -628,6 +629,9 @@ void ghes_edac_unregister(struct ghes *ghes)
 {
 	struct mem_ctl_info *mci;
 	unsigned long flags;
+
+	if (!force_load)
+		return;
 
 	mutex_lock(&ghes_reg_mutex);
 
