4e723e7565c4031568fb9db18253cfbf6442831d
Olivier Moysan <olivier.moysan@st.com>
2020-09-11 10:15:07 +0200

    ASoC: stm32: sai: add pm_runtime support
    Enable support of pm_runtime on STM32 SAI driver to allow
    SAI power state monitoring.
    pm_runtime_put_autosuspend() is called from ASoC framework
    on pcm device close.
    The pmdown_time delay is available in runtime context, and may be set
    in SAI driver to take into account shutdown delay on playback.
    However, this shutdown delay is already handled in the DAPMs
    of the audio codec linked to SAI CPU DAI.
    So, the choice is made, not to support this delay on CPU DAI side.
    
    Signed-off-by: Olivier Moysan <olivier.moysan@st.com>
    Link: https://lore.kernel.org/r/20200911081507.7276-1-olivier.moysan@st.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/stm/stm32_sai_sub.c
+++ sound/soc/stm/stm32_sai_sub.c
@@ -12,6 +12,7 @@
 #include <linux/module.h>
 #include <linux/of_irq.h>
 #include <linux/of_platform.h>
+#include <linux/pm_runtime.h>
 #include <linux/regmap.h>
 
 #include <sound/asoundef.h>
@@ -1559,10 +1560,14 @@ static int stm32_sai_sub_probe(struct platform_device *pdev)
 
 	ret = snd_soc_register_component(&pdev->dev, &stm32_component,
 					 &sai->cpu_dai_drv, 1);
-	if (ret)
+	if (ret) {
 		snd_dmaengine_pcm_unregister(&pdev->dev);
+		return ret;
+	}
 
-	return ret;
+	pm_runtime_enable(&pdev->dev);
+
+	return 0;
 }
 
 static int stm32_sai_sub_remove(struct platform_device *pdev)
@@ -1572,6 +1577,7 @@ static int stm32_sai_sub_remove(struct platform_device *pdev)
 	clk_unprepare(sai->pdata->pclk);
 	snd_dmaengine_pcm_unregister(&pdev->dev);
 	snd_soc_unregister_component(&pdev->dev);
+	pm_runtime_disable(&pdev->dev);
 
 	return 0;
 }
