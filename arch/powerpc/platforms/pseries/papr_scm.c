ca78ef2f08ccfa29b711d644964cdf9d7ace15e5
Vaibhav Jain <vaibhav@linux.ibm.com>
2020-09-12 13:44:51 +0530

    powerpc/papr_scm: Fix warning triggered by perf_stats_show()
    A warning is reported by the kernel in case perf_stats_show() returns
    an error code. The warning is of the form below:
    
     papr_scm ibm,persistent-memory:ibm,pmemory@44100001:
     	  Failed to query performance stats, Err:-10
     dev_attr_show: perf_stats_show+0x0/0x1c0 [papr_scm] returned bad count
     fill_read_buffer: dev_attr_show+0x0/0xb0 returned bad count
    
    On investigation it looks like that the compiler is silently
    truncating the return value of drc_pmem_query_stats() from 'long' to
    'int', since the variable used to store the return code 'rc' is an
    'int'. This truncated value is then returned back as a 'ssize_t' back
    from perf_stats_show() to 'dev_attr_show()' which thinks of it as a
    large unsigned number and triggers this warning..
    
    To fix this we update the type of variable 'rc' from 'int' to
    'ssize_t' that prevents the compiler from truncating the return value
    of drc_pmem_query_stats() and returning correct signed value back from
    perf_stats_show().
    
    Fixes: 2d02bf835e57 ("powerpc/papr_scm: Fetch nvdimm performance stats from PHYP")
    Signed-off-by: Vaibhav Jain <vaibhav@linux.ibm.com>
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200912081451.66225-1-vaibhav@linux.ibm.com

--- arch/powerpc/platforms/pseries/papr_scm.c
+++ arch/powerpc/platforms/pseries/papr_scm.c
@@ -785,7 +785,8 @@ static int papr_scm_ndctl(struct nvdimm_bus_descriptor *nd_desc,
 static ssize_t perf_stats_show(struct device *dev,
 			       struct device_attribute *attr, char *buf)
 {
-	int index, rc;
+	int index;
+	ssize_t rc;
 	struct seq_buf s;
 	struct papr_scm_perf_stat *stat;
 	struct papr_scm_perf_stats *stats;
@@ -820,7 +821,7 @@ static ssize_t perf_stats_show(struct device *dev,
 
 free_stats:
 	kfree(stats);
-	return rc ? rc : seq_buf_used(&s);
+	return rc ? rc : (ssize_t)seq_buf_used(&s);
 }
 DEVICE_ATTR_ADMIN_RO(perf_stats);
 
