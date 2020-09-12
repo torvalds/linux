cbd632ea8ee4ae07d12e85ed07aa5d667a1f47d8
Jason Yan <yanaijie@huawei.com>
2020-09-12 15:22:11 +0800

    spi: bcm2835: Make polling_limit_us static
    This eliminates the following sparse warning:
    
    drivers/spi/spi-bcm2835.c:78:14: warning: symbol 'polling_limit_us' was
    not declared. Should it be static?
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Jason Yan <yanaijie@huawei.com>
    Link: https://lore.kernel.org/r/20200912072211.602735-1-yanaijie@huawei.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-bcm2835.c
+++ drivers/spi/spi-bcm2835.c
@@ -75,7 +75,7 @@
 #define DRV_NAME	"spi-bcm2835"
 
 /* define polling limits */
-unsigned int polling_limit_us = 30;
+static unsigned int polling_limit_us = 30;
 module_param(polling_limit_us, uint, 0664);
 MODULE_PARM_DESC(polling_limit_us,
 		 "time in us to run a transfer in polling mode\n");
