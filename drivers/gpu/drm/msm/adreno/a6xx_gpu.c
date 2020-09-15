e9ba8d550dd1e28870a0bdc7c11af026c2a94702
Jordan Crouse <jcrouse@codeaurora.org>
2020-09-15 10:35:51 -0600

    drm/msm: Fix the a650 hw_apriv check
    Commit 604234f33658 ("drm/msm: Enable expanded apriv support for a650")
    was checking the result of adreno_is_a650() before the gpu revision
    got probed in adreno_gpu_init() so it was always coming across as
    false. Snoop into the revision ID ahead of time to correctly set the
    hw_apriv flag so that it can be used by msm_gpu to properly setup
    global buffers.
    
    Fixes: 604234f33658 ("drm/msm: Enable expanded apriv support for a650")
    Reported-by: Jonathan Marek <jonathan@marek.ca>
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Tested-by: Jonathan Marek <jonathan@marek.ca>
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/adreno/a6xx_gpu.c
+++ drivers/gpu/drm/msm/adreno/a6xx_gpu.c
@@ -1182,6 +1182,8 @@ struct msm_gpu *a6xx_gpu_init(struct drm_device *dev)
 {
 	struct msm_drm_private *priv = dev->dev_private;
 	struct platform_device *pdev = priv->gpu_pdev;
+	struct adreno_platform_config *config = pdev->dev.platform_data;
+	const struct adreno_info *info;
 	struct device_node *node;
 	struct a6xx_gpu *a6xx_gpu;
 	struct adreno_gpu *adreno_gpu;
@@ -1197,7 +1199,14 @@ struct msm_gpu *a6xx_gpu_init(struct drm_device *dev)
 
 	adreno_gpu->registers = NULL;
 
-	if (adreno_is_a650(adreno_gpu))
+	/*
+	 * We need to know the platform type before calling into adreno_gpu_init
+	 * so that the hw_apriv flag can be correctly set. Snoop into the info
+	 * and grab the revision number
+	 */
+	info = adreno_info(config->rev);
+
+	if (info && info->revn == 650)
 		adreno_gpu->base.hw_apriv = true;
 
 	ret = adreno_gpu_init(dev, pdev, adreno_gpu, &funcs, 1);
