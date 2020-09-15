2a32dbdc2c7db5463483fa01fb220fd1b770c6bc
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2020-09-16 02:23:47 +0300

    drm: rcar-du: Put reference to VSP device
    The reference to the VSP device acquired with of_find_device_by_node()
    in rcar_du_vsp_init() is never released. Fix it with a drmm action,
    which gets run both in the probe error path and in the remove path.
    
    Fixes: 6d62ef3ac30b ("drm: rcar-du: Expose the VSP1 compositor through KMS planes")
    Reported-by: Yu Kuai <yukuai3@huawei.com>
    Reviewed-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>

--- drivers/gpu/drm/rcar-du/rcar_du_vsp.c
+++ drivers/gpu/drm/rcar-du/rcar_du_vsp.c
@@ -13,6 +13,7 @@
 #include <drm/drm_fourcc.h>
 #include <drm/drm_gem_cma_helper.h>
 #include <drm/drm_gem_framebuffer_helper.h>
+#include <drm/drm_managed.h>
 #include <drm/drm_plane_helper.h>
 #include <drm/drm_vblank.h>
 
@@ -341,6 +342,13 @@ static const struct drm_plane_funcs rcar_du_vsp_plane_funcs = {
 	.atomic_destroy_state = rcar_du_vsp_plane_atomic_destroy_state,
 };
 
+static void rcar_du_vsp_cleanup(struct drm_device *dev, void *res)
+{
+	struct rcar_du_vsp *vsp = res;
+
+	put_device(vsp->vsp);
+}
+
 int rcar_du_vsp_init(struct rcar_du_vsp *vsp, struct device_node *np,
 		     unsigned int crtcs)
 {
@@ -356,6 +364,10 @@ int rcar_du_vsp_init(struct rcar_du_vsp *vsp, struct device_node *np,
 		return -ENXIO;
 
 	vsp->vsp = &pdev->dev;
+
+	ret = drmm_add_action(rcdu->ddev, rcar_du_vsp_cleanup, vsp);
+	if (ret < 0)
+		return ret;
 
 	ret = vsp1_du_init(vsp->vsp);
 	if (ret < 0)
