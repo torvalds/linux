93f634069707cfe562c38739f5062feccbe9a834
Johnny Chuang <johnny.chuang.emc@gmail.com>
2020-09-16 10:26:57 -0700

    Input: elants_i2c - fix typo for an attribute to show calibration count
    Fixed typo for command from 0xE0 to 0xD0.
    
    Fixes: cf520c643012 ("Input: elants_i2c - provide an attribute to show calibration count")
    Signed-off-by: Johnny Chuang <johnny.chuang.emc@gmail.com>
    Link: https://lore.kernel.org/r/1600238783-32303-1-git-send-email-johnny.chuang.emc@gmail.com
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/touchscreen/elants_i2c.c
+++ drivers/input/touchscreen/elants_i2c.c
@@ -90,7 +90,7 @@
 /* FW read command, 0x53 0x?? 0x0, 0x01 */
 #define E_ELAN_INFO_FW_VER	0x00
 #define E_ELAN_INFO_BC_VER	0x10
-#define E_ELAN_INFO_REK		0xE0
+#define E_ELAN_INFO_REK		0xD0
 #define E_ELAN_INFO_TEST_VER	0xE0
 #define E_ELAN_INFO_FW_ID	0xF0
 #define E_INFO_OSR		0xD6
