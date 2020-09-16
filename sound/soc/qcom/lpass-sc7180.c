dcde34c47d8f7befc647fda65e8efa8cc0e795ca
Douglas Anderson <dianders@chromium.org>
2020-09-16 11:15:55 -0700

    ASoC: qcom: lpass-sc7180: Add MODULE_DEVICE_TABLE
    The lpass-sc7180 driver can be built as a module but is lacking a
    MODULE_DEVICE_TABLE.  This means it won't auto-load.  Fix this
    oversight.
    
    Fixes: 24caf8d9eb10 ("ASoC: qcom: lpass-sc7180: Add platform driver for lpass audio")
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Link: https://lore.kernel.org/r/20200916111545.1.I4c3758817d94c433bafeac344a395e21ea6657e3@changeid
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/qcom/lpass-sc7180.c
+++ sound/soc/qcom/lpass-sc7180.c
@@ -200,6 +200,7 @@ static const struct of_device_id sc7180_lpass_cpu_device_id[] = {
 	{.compatible = "qcom,sc7180-lpass-cpu", .data = &sc7180_data},
 	{}
 };
+MODULE_DEVICE_TABLE(of, sc7180_lpass_cpu_device_id);
 
 static struct platform_driver sc7180_lpass_cpu_platform_driver = {
 	.driver = {
