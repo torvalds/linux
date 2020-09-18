871c433bae565306cfc3a1a386bf7328bc9d31cb
Rustam Kovhaev <rkovhaev@gmail.com>
2020-09-18 05:05:00 -0700

    KVM: use struct_size() and flex_array_size() helpers in kvm_io_bus_unregister_dev()
    Make use of the struct_size() helper to avoid any potential type
    mistakes and protect against potential integer overflows
    Make use of the flex_array_size() helper to calculate the size of a
    flexible array member within an enclosing structure
    
    Suggested-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Signed-off-by: Rustam Kovhaev <rkovhaev@gmail.com>
    Message-Id: <20200918120500.954436-1-rkovhaev@gmail.com>
    Reviewed-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -4350,10 +4350,10 @@ void kvm_io_bus_unregister_dev(struct kvm *kvm, enum kvm_bus bus_idx,
 	new_bus = kmalloc(struct_size(bus, range, bus->dev_count - 1),
 			  GFP_KERNEL_ACCOUNT);
 	if (new_bus) {
-		memcpy(new_bus, bus, sizeof(*bus) + i * sizeof(struct kvm_io_range));
+		memcpy(new_bus, bus, struct_size(bus, range, i));
 		new_bus->dev_count--;
 		memcpy(new_bus->range + i, bus->range + i + 1,
-		       (new_bus->dev_count - i) * sizeof(struct kvm_io_range));
+				flex_array_size(new_bus, range, new_bus->dev_count - i));
 	} else {
 		pr_err("kvm: failed to shrink bus, removing it completely\n");
 		for (j = 0; j < bus->dev_count; j++) {
