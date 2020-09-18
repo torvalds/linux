766d95b1ed93ebdd07ac87490e60e442342f5dc4
Boyan Karatotev <boyan.karatotev@arm.com>
2020-09-18 11:47:13 +0100

    kselftests/arm64: add nop checks for PAuth tests
    PAuth adds sign/verify controls to enable and disable groups of
    instructions in hardware for compatibility with libraries that do not
    implement PAuth. The kernel always enables them if it detects PAuth.
    
    Add a test that checks that each group of instructions is enabled, if the
    kernel reports PAuth as detected.
    
    Note: For groups, for the purpose of this patch, we intend instructions
    that use a certain key.
    
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>
    Reviewed-by: Vincenzo Frascino <Vincenzo.Frascino@arm.com>
    Reviewed-by: Amit Daniel Kachhap <amit.kachhap@arm.com>
    Acked-by: Shuah Khan <skhan@linuxfoundation.org>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20200918104715.182310-3-boian4o1@gmail.com
    Signed-off-by: Will Deacon <will@kernel.org>

--- tools/testing/selftests/arm64/pauth/helper.h
+++ tools/testing/selftests/arm64/pauth/helper.h
@@ -4,6 +4,15 @@
 #ifndef _HELPER_H_
 #define _HELPER_H_
 
+#include <stdlib.h>
+
 void pac_corruptor(void);
+
+/* PAuth sign a value with key ia and modifier value 0 */
+size_t keyia_sign(size_t val);
+size_t keyib_sign(size_t val);
+size_t keyda_sign(size_t val);
+size_t keydb_sign(size_t val);
+size_t keyg_sign(size_t val);
 
 #endif
--- tools/testing/selftests/arm64/pauth/pac.c
+++ tools/testing/selftests/arm64/pauth/pac.c
@@ -8,11 +8,24 @@
 #include "../../kselftest_harness.h"
 #include "helper.h"
 
+#define PAC_COLLISION_ATTEMPTS 10
+/*
+ * The kernel sets TBID by default. So bits 55 and above should remain
+ * untouched no matter what.
+ * The VA space size is 48 bits. Bigger is opt-in.
+ */
+#define PAC_MASK (~0xff80ffffffffffff)
 #define ASSERT_PAUTH_ENABLED() \
 do { \
 	unsigned long hwcaps = getauxval(AT_HWCAP); \
 	/* data key instructions are not in NOP space. This prevents a SIGILL */ \
 	ASSERT_NE(0, hwcaps & HWCAP_PACA) TH_LOG("PAUTH not enabled"); \
+} while (0)
+#define ASSERT_GENERIC_PAUTH_ENABLED() \
+do { \
+	unsigned long hwcaps = getauxval(AT_HWCAP); \
+	/* generic key instructions are not in NOP space. This prevents a SIGILL */ \
+	ASSERT_NE(0, hwcaps & HWCAP_PACG) TH_LOG("Generic PAUTH not enabled"); \
 } while (0)
 
 sigjmp_buf jmpbuf;
@@ -39,6 +52,44 @@ TEST(corrupt_pac)
 		pac_corruptor();
 		ASSERT_TRUE(0) TH_LOG("SIGSEGV/SIGILL signal did not occur");
 	}
+}
+
+/*
+ * There are no separate pac* and aut* controls so checking only the pac*
+ * instructions is sufficient
+ */
+TEST(pac_instructions_not_nop)
+{
+	size_t keyia = 0;
+	size_t keyib = 0;
+	size_t keyda = 0;
+	size_t keydb = 0;
+
+	ASSERT_PAUTH_ENABLED();
+
+	for (int i = 0; i < PAC_COLLISION_ATTEMPTS; i++) {
+		keyia |= keyia_sign(i) & PAC_MASK;
+		keyib |= keyib_sign(i) & PAC_MASK;
+		keyda |= keyda_sign(i) & PAC_MASK;
+		keydb |= keydb_sign(i) & PAC_MASK;
+	}
+
+	ASSERT_NE(0, keyia) TH_LOG("keyia instructions did nothing");
+	ASSERT_NE(0, keyib) TH_LOG("keyib instructions did nothing");
+	ASSERT_NE(0, keyda) TH_LOG("keyda instructions did nothing");
+	ASSERT_NE(0, keydb) TH_LOG("keydb instructions did nothing");
+}
+
+TEST(pac_instructions_not_nop_generic)
+{
+	size_t keyg = 0;
+
+	ASSERT_GENERIC_PAUTH_ENABLED();
+
+	for (int i = 0; i < PAC_COLLISION_ATTEMPTS; i++)
+		keyg |= keyg_sign(i) & PAC_MASK;
+
+	ASSERT_NE(0, keyg)  TH_LOG("keyg instructions did nothing");
 }
 
 TEST_HARNESS_MAIN
