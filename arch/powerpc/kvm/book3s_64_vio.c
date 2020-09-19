eb173559c97c3b5d8905cd8a93a5d3f3fb24916d
Jing Xiangfeng <jingxiangfeng@huawei.com>
2020-09-19 15:12:30 +0800

    KVM: PPC: Book3S: Remove redundant initialization of variable ret
    The variable ret is being initialized with '-ENOMEM' that is meaningless.
    So remove it.
    
    Signed-off-by: Jing Xiangfeng <jingxiangfeng@huawei.com>
    Reviewed-by: Fabiano Rosas <farosas@linux.ibm.com>
    Signed-off-by: Paul Mackerras <paulus@ozlabs.org>

--- arch/powerpc/kvm/book3s_64_vio.c
+++ arch/powerpc/kvm/book3s_64_vio.c
@@ -283,7 +283,7 @@ long kvm_vm_ioctl_create_spapr_tce(struct kvm *kvm,
 	struct kvmppc_spapr_tce_table *siter;
 	struct mm_struct *mm = kvm->mm;
 	unsigned long npages, size = args->size;
-	int ret = -ENOMEM;
+	int ret;
 
 	if (!args->size || args->page_shift < 12 || args->page_shift > 34 ||
 		(args->offset + args->size > (ULLONG_MAX >> args->page_shift)))
