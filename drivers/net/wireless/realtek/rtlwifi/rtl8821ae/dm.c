ff4d1d726e7fcdce5d73b196cbb8665335e2fce0
Jason Yan <yanaijie@huawei.com>
2020-09-19 15:44:28 +0800

    rtlwifi: rtl8821ae: use true,false for bool variable large_cfo_hit
    This addresses the following coccinelle warning:
    
    drivers/net/wireless/realtek/rtlwifi/rtl8821ae/dm.c:2680:27-47: WARNING:
    Comparison of 0/1 to bool variable
    drivers/net/wireless/realtek/rtlwifi/rtl8821ae/dm.c:2683:3-23: WARNING:
    Assignment of 0/1 to bool variable
    drivers/net/wireless/realtek/rtlwifi/rtl8821ae/dm.c:2686:3-23: WARNING:
    Assignment of 0/1 to bool variable
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Jason Yan <yanaijie@huawei.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200919074428.3459234-1-yanaijie@huawei.com

--- drivers/net/wireless/realtek/rtlwifi/rtl8821ae/dm.c
+++ drivers/net/wireless/realtek/rtlwifi/rtl8821ae/dm.c
@@ -2677,13 +2677,13 @@ static void rtl8821ae_dm_dynamic_atc_switch(struct ieee80211_hw *hw)
 						(rtldm->cfo_ave_pre - cfo_ave) :
 						(cfo_ave - rtldm->cfo_ave_pre);
 
-		if (cfo_ave_diff > 20 && rtldm->large_cfo_hit == 0) {
+		if (cfo_ave_diff > 20 && !rtldm->large_cfo_hit) {
 			rtl_dbg(rtlpriv, COMP_DIG, DBG_LOUD,
 				"first large CFO hit\n");
-			rtldm->large_cfo_hit = 1;
+			rtldm->large_cfo_hit = true;
 			return;
 		} else
-			rtldm->large_cfo_hit = 0;
+			rtldm->large_cfo_hit = false;
 
 		rtldm->cfo_ave_pre = cfo_ave;
 
