b3e79e7682e075326df8041b826b03453acacd0a
Serge Semin <Sergey.Semin@baikalelectronics.ru>
2020-09-20 14:14:44 +0300

    mtd: physmap: Add Baikal-T1 physically mapped ROM support
    Baikal-T1 Boot Controller provides an access to a RO storages, which are
    physically mapped into the SoC MMIO space. In particularly there are
    Internal ROM embedded into the SoC with a pre-installed firmware,
    externally attached SPI flash (also accessed in the read-only mode) and a
    memory region, which mirrors one of them in accordance with the currently
    enabled system boot mode (also called Boot ROM).
    
    This commit adds the Internal ROM support to the physmap driver of the MTD
    kernel subsystem. The driver will create the Internal ROM MTD as long as
    it is defined in the system dts file. The physically mapped SPI flash
    region will be used to implement the SPI-mem interface. The mirroring
    memory region won't be accessible directly since it's redundant due to
    both bootable regions being exposed anyway.
    
    Note we had to create a dedicated code for the ROMs since read from the
    corresponding memory regions must be done via the dword-aligned addresses.
    
    Signed-off-by: Serge Semin <Sergey.Semin@baikalelectronics.ru>
    Cc: Alexey Malahov <Alexey.Malahov@baikalelectronics.ru>
    Cc: Pavel Parkhomenko <Pavel.Parkhomenko@baikalelectronics.ru>
    Cc: Lee Jones <lee.jones@linaro.org>
    Cc: linux-mips@vger.kernel.org
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Link: https://lore.kernel.org/linux-mtd/20200920111445.21816-1-Sergey.Semin@baikalelectronics.ru

--- drivers/mtd/maps/physmap-core.c
+++ drivers/mtd/maps/physmap-core.c
@@ -41,6 +41,7 @@
 #include <linux/pm_runtime.h>
 #include <linux/gpio/consumer.h>
 
+#include "physmap-bt1-rom.h"
 #include "physmap-gemini.h"
 #include "physmap-ixp4xx.h"
 #include "physmap-versatile.h"
@@ -370,6 +371,10 @@ static int physmap_flash_of_init(struct platform_device *dev)
 		info->maps[i].swap = swap;
 		info->maps[i].bankwidth = bankwidth;
 		info->maps[i].device_node = dp;
+
+		err = of_flash_probe_bt1_rom(dev, dp, &info->maps[i]);
+		if (err)
+			return err;
 
 		err = of_flash_probe_gemini(dev, dp, &info->maps[i]);
 		if (err)
