e2c53515b2a63828d226250fd62b38039f613977
Wan Ahmad Zainie <wan.ahmad.zainie.wan.mohamad@intel.com>
2020-09-21 10:44:59 +0800

    usb: dwc3: of-simple: Add compatible string for Intel Keem Bay platform
    Add compatible string to use this generic glue layer to support
    Intel Keem Bay platform's dwc3 controller.
    
    Signed-off-by: Wan Ahmad Zainie <wan.ahmad.zainie.wan.mohamad@intel.com>
    Signed-off-by: Felipe Balbi <balbi@kernel.org>

--- drivers/usb/dwc3/dwc3-of-simple.c
+++ drivers/usb/dwc3/dwc3-of-simple.c
@@ -177,6 +177,7 @@ static const struct of_device_id of_dwc3_simple_match[] = {
 	{ .compatible = "sprd,sc9860-dwc3" },
 	{ .compatible = "allwinner,sun50i-h6-dwc3" },
 	{ .compatible = "hisilicon,hi3670-dwc3" },
+	{ .compatible = "intel,keembay-dwc3" },
 	{ /* Sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, of_dwc3_simple_match);
