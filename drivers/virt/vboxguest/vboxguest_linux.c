03c95e591a894a5213aed4e0f2c434df1047e595
Qinglang Miao <miaoqinglang@huawei.com>
2020-09-21 21:11:13 +0800

    virt: vbox: simplify the return expression of vbg_input_open()
    Simplify the return expression.
    
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Qinglang Miao <miaoqinglang@huawei.com>
    Link: https://lore.kernel.org/r/20200921131113.93459-1-miaoqinglang@huawei.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/virt/vboxguest/vboxguest_linux.c
+++ drivers/virt/vboxguest/vboxguest_linux.c
@@ -202,13 +202,8 @@ static int vbg_input_open(struct input_dev *input)
 {
 	struct vbg_dev *gdev = input_get_drvdata(input);
 	u32 feat = VMMDEV_MOUSE_GUEST_CAN_ABSOLUTE | VMMDEV_MOUSE_NEW_PROTOCOL;
-	int ret;
 
-	ret = vbg_core_set_mouse_status(gdev, feat);
-	if (ret)
-		return ret;
-
-	return 0;
+	return vbg_core_set_mouse_status(gdev, feat);
 }
 
 /**
