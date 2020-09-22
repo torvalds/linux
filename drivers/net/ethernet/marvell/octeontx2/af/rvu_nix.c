8f900363df52311cab72869668905b19d1888ef5
George Cherian <george.cherian@marvell.com>
2020-09-22 18:37:26 +0530

    octeontx2-af: Add support for VLAN based RSS hashing
    Added support for PF/VF drivers to choose RSS flow key algorithm
    with VLAN tag included in hashing input data. Only CTAG is considered.
    
    Signed-off-by: George Cherian <george.cherian@marvell.com>
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_nix.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_nix.c
@@ -2509,6 +2509,14 @@ static int set_flowkey_fields(struct nix_rx_flowkey_alg *alg, u32 flow_cfg)
 			field->ltype_match = NPC_LT_LE_GTPU;
 			field->ltype_mask = 0xF;
 			break;
+		case NIX_FLOW_KEY_TYPE_VLAN:
+			field->lid = NPC_LID_LB;
+			field->hdr_offset = 2; /* Skip TPID (2-bytes) */
+			field->bytesm1 = 1; /* 2 Bytes (Actually 12 bits) */
+			field->ltype_match = NPC_LT_LB_CTAG;
+			field->ltype_mask = 0xF;
+			field->fn_mask = 1; /* Mask out the first nibble */
+			break;
 		}
 		field->ena = 1;
 
