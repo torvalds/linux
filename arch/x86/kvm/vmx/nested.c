ebec153a056108ca7818caad549d3ad7c0f2369d
Sean Christopherson <sean.j.christopherson@intel.com>
2020-09-23 11:44:51 -0700

    KVM: nVMX: Drop redundant VMCS switch and free_nested() call
    Remove the explicit switch to vmcs01 and the call to free_nested() in
    nested_vmx_free_vcpu().  free_nested(), which is called unconditionally
    by vmx_leave_nested(), ensures vmcs01 is loaded prior to freeing vmcs02
    and friends.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Message-Id: <20200923184452.980-7-sean.j.christopherson@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx/nested.c
+++ arch/x86/kvm/vmx/nested.c
@@ -326,8 +326,6 @@ void nested_vmx_free_vcpu(struct kvm_vcpu *vcpu)
 {
 	vcpu_load(vcpu);
 	vmx_leave_nested(vcpu);
-	vmx_switch_vmcs(vcpu, &to_vmx(vcpu)->vmcs01);
-	free_nested(vcpu);
 	vcpu_put(vcpu);
 }
 
