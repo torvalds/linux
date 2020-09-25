62c774ed483174b994c79e0c9f596b315f1ddfaf
Jens Axboe <axboe@kernel.dk>
2020-09-25 09:01:53 -0600

    io_uring: don't unconditionally set plug->nowait = true
    This causes all the bios to be submitted with REQ_NOWAIT, which can be
    problematic on either btrfs or on file systems that otherwise use a mix
    of block devices where only some of them support it.
    
    For now, just remove the setting of plug->nowait = true.
    
    Reported-by: Dan Melnic <dmm@fb.com>
    Reported-by: Brian Foster <bfoster@redhat.com>
    Fixes: b63534c41e20 ("io_uring: re-issue block requests that failed because of resources")
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -6353,9 +6353,6 @@ static void io_submit_state_start(struct io_submit_state *state,
 				  struct io_ring_ctx *ctx, unsigned int max_ios)
 {
 	blk_start_plug(&state->plug);
-#ifdef CONFIG_BLOCK
-	state->plug.nowait = true;
-#endif
 	state->comp.nr = 0;
 	INIT_LIST_HEAD(&state->comp.list);
 	state->comp.ctx = ctx;
