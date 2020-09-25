c6ed9c53ba1f362763d33d67657e59675a28a9a2
Marc Zyngier <maz@kernel.org>
2020-09-25 16:22:00 +0100

    FROMGIT: ARM: Handle no IPI being registered in show_ipi_list()
    As SMP-on-UP is a valid configuration on 32bit ARM, do not assume that
    IPIs are populated in show_ipi_list().
    
    Reported-by: Guillaume Tucker <guillaume.tucker@collabora.com>
    Reported-by: kernelci.org bot <bot@kernelci.org>
    Tested-by: Guillaume Tucker <guillaume.tucker@collabora.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    
    Bug: 140053385
    (cherry picked from commit 220387048d859896ccc362c0ebf9bc1e0fa62eb9
     https://git.kernel.org/pub/scm/linux/kernel/git/maz/arm-platforms.git irq/ipi-as-irq)
    Change-Id: Ic34d58cf86c1cc3c233567a572fe96861dd8139f
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -541,7 +541,12 @@ void show_ipi_list(struct seq_file *p, int prec)
 	unsigned int cpu, i;
 
 	for (i = 0; i < NR_IPI; i++) {
-		unsigned int irq = irq_desc_get_irq(ipi_desc[i]);
+		unsigned int irq;
+
+		if (!ipi_desc[i])
+			continue;
+
+		irq = irq_desc_get_irq(ipi_desc[i]);
 		seq_printf(p, "%*s%u: ", prec - 1, "IPI", i);
 
 		for_each_online_cpu(cpu)
