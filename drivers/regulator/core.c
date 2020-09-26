e9bb4a068b206f61ef01057cfeafdb852fb244c5
AngeloGioacchino Del Regno <kholk11@gmail.com>
2020-09-26 14:55:43 +0200

    regulator: core: Enlarge max OF property name length to 64 chars
    Some regulator drivers may be defining very long names: this is the
    case with the qcom_smd and qcom_spmi regulators, where we need to
    parse the regulator parents from DT.
    
    For clarity, this is an example:
    { "l13a", QCOM_SMD_RPM_LDOA, 13, &pm660_ht_lvpldo,
      "vdd_l8_l9_l10_l11_l12_l13_l14" },
    pm660-regulators {
    	...
    	vdd_l8_l9_l10_l11_l12_l13_l14-supply = <&vreg_s4a_2p04>
    	...
    };
    Now, with a 32 characters limit, the function is trying to parse,
    exactly, "vdd_l8_l9_l10_l11_l12_l13_l14-s" (32 chars) instead of
    the right one, which is 37 chars long in this specific case.
    
    ... And this is not only the case with PM660/PM660L, but also with
    PMA8084, PM8916, PM8950 and others that are not implemented yet.
    
    The length of 64 chars was chosen based on the longest parsed property
    name that I could find, which is in PM8916, and would be 53 characters
    long.
    At that point, rounding that to 64 looked like being the best idea.
    
    Signed-off-by: AngeloGioacchino Del Regno <kholk11@gmail.com>
    Link: https://lore.kernel.org/r/20200926125549.13191-2-kholk11@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -404,11 +404,11 @@ err_node_put:
 static struct device_node *of_get_regulator(struct device *dev, const char *supply)
 {
 	struct device_node *regnode = NULL;
-	char prop_name[32]; /* 32 is max size of property name */
+	char prop_name[64]; /* 64 is max size of property name */
 
 	dev_dbg(dev, "Looking up %s-supply from device tree\n", supply);
 
-	snprintf(prop_name, 32, "%s-supply", supply);
+	snprintf(prop_name, 64, "%s-supply", supply);
 	regnode = of_parse_phandle(dev->of_node, prop_name, 0);
 
 	if (!regnode) {
