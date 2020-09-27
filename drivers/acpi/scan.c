05de068614ac4b5bc37a0f53df713cf4475ae924
Tian Tao <tiantao6@hisilicon.com>
2020-09-27 09:14:28 +0800

    ACPI: scan: Replace ACPI_DEBUG_PRINT() with pr_debug()
    Fix the following W=1 kernel build warning(s):
    
    drivers/acpi/scan.c: In function ‘acpi_bus_get_wakeup_device_flags’:
    drivers/acpi/scan.c:902:43: warning: suggest braces around empty body in
    an ‘if’ statement [-Wempty-body]
    
    by using pr_debug() to instead of the ACPI_DEBUG_PRINT() macro, which
    should only be used by ACPICA code, to print a debug message.
    
    Signed-off-by: Tian Tao <tiantao6@hisilicon.com>
    [ rjw: Subject and changelog edits ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -898,8 +898,7 @@ static void acpi_bus_get_wakeup_device_flags(struct acpi_device *device)
 	 */
 	err = acpi_device_sleep_wake(device, 0, 0, 0);
 	if (err)
-		ACPI_DEBUG_PRINT((ACPI_DB_INFO,
-				"error in _DSW or _PSW evaluation\n"));
+		pr_debug("error in _DSW or _PSW evaluation\n");
 }
 
 static void acpi_bus_init_power_state(struct acpi_device *device, int state)
