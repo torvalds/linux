00e8c44a147aa237867ba38b096da5e28b85a57b
John Fastabend <john.fastabend@gmail.com>
2020-09-28 18:50:47 -0700

    bpf, selftests: Fix cast to smaller integer type 'int' warning in raw_tp
    Fix warning in bpf selftests,
    
    progs/test_raw_tp_test_run.c:18:10: warning: cast to smaller integer type 'int' from 'struct task_struct *' [-Wpointer-to-int-cast]
    
    Change int type cast to long to fix. Discovered with gcc-9 and llvm-11+
    where llvm was recent main branch.
    
    Fixes: 09d8ad16885ee ("selftests/bpf: Add raw_tp_test_run")
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/160134424745.11199.13841922833336698133.stgit@john-Precision-5820-Tower

--- tools/testing/selftests/bpf/progs/test_raw_tp_test_run.c
+++ tools/testing/selftests/bpf/progs/test_raw_tp_test_run.c
@@ -15,7 +15,7 @@ int BPF_PROG(rename, struct task_struct *task, char *comm)
 	count++;
 	if ((__u64) task == 0x1234ULL && (__u64) comm == 0x5678ULL) {
 		on_cpu = bpf_get_smp_processor_id();
-		return (int)task + (int)comm;
+		return (long)task + (long)comm;
 	}
 
 	return 0;
