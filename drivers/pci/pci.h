454d082dbb47febbfe2998c3953e62cd917abbbf
Bjorn Helgaas <bhelgaas@google.com>
2020-09-29 14:20:15 -0500

    PCI/PM: Remove unused PCI_PM_BUS_WAIT
    476e7faefc43 ("PCI PM: Do not wait for buses in B2 or B3 during resume")
    removed the last use of PCI_PM_BUS_WAIT.  Remove the definition as well.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -46,7 +46,6 @@ int pci_bus_error_reset(struct pci_dev *dev);
 #define PCI_PM_D2_DELAY         200
 #define PCI_PM_D3HOT_WAIT       10
 #define PCI_PM_D3COLD_WAIT      100
-#define PCI_PM_BUS_WAIT         50
 
 /**
  * struct pci_platform_pm_ops - Firmware PM callbacks
