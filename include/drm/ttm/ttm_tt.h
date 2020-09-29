1e17792de06fe74daf44a97a8dff7d357c783acc
Christian König <christian.koenig@amd.com>
2020-09-29 17:33:17 +0200

    drm/ttm: remove TTM_PAGE_FLAG_WRITE
    Not used any more.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Michael J. Ruhl <michael.j.ruhl@intel.com>
    Link: https://patchwork.freedesktop.org/patch/393429/

--- include/drm/ttm/ttm_tt.h
+++ include/drm/ttm/ttm_tt.h
@@ -34,7 +34,6 @@ struct ttm_resource;
 struct ttm_buffer_object;
 struct ttm_operation_ctx;
 
-#define TTM_PAGE_FLAG_WRITE           (1 << 3)
 #define TTM_PAGE_FLAG_SWAPPED         (1 << 4)
 #define TTM_PAGE_FLAG_ZERO_ALLOC      (1 << 6)
 #define TTM_PAGE_FLAG_DMA32           (1 << 7)
