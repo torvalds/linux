346dbbb8f7a0f94eb4264d1e0a253be2edca6eb8
Hawking Zhang <Hawking.Zhang@amd.com>
2020-09-30 23:09:57 +0800

    drm/amdgpu: enable GDDR6 save-restore support for navy_flounder
    add mp0 11_0_11 for navy_flounder to the mem training
    supported list, otherwise the modeprobe would fail
    on navy_flounder with latest vbios.
    
    Signed-off-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_atomfirmware.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_atomfirmware.c
@@ -543,6 +543,7 @@ int amdgpu_mem_train_support(struct amdgpu_device *adev)
 		case HW_REV(11, 0, 0):
 		case HW_REV(11, 0, 5):
 		case HW_REV(11, 0, 7):
+		case HW_REV(11, 0, 11):
 			ret = 1;
 			break;
 		default:
