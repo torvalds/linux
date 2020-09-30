fea99111244bae44e7d82a973744d27ea1567814
David Howells <dhowells@redhat.com>
2020-09-30 19:52:08 +0100

    rxrpc: The server keyring isn't network-namespaced
    The keyring containing the server's tokens isn't network-namespaced, so it
    shouldn't be looked up with a network namespace.  It is expected to be
    owned specifically by the server, so namespacing is unnecessary.
    
    Fixes: a58946c158a0 ("keys: Pass the network namespace into request_key mechanism")
    Signed-off-by: David Howells <dhowells@redhat.com>

--- net/rxrpc/key.c
+++ net/rxrpc/key.c
@@ -940,7 +940,7 @@ int rxrpc_server_keyring(struct rxrpc_sock *rx, sockptr_t optval, int optlen)
 	if (IS_ERR(description))
 		return PTR_ERR(description);
 
-	key = request_key_net(&key_type_keyring, description, sock_net(&rx->sk), NULL);
+	key = request_key(&key_type_keyring, description, NULL);
 	if (IS_ERR(key)) {
 		kfree(description);
 		_leave(" = %ld", PTR_ERR(key));
