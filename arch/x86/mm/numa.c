73bf7382debb1a93fef5ff38222c6a3b62dfea44
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2020-09-30 22:05:43 +0800

    x86: Support Generic Initiator only proximity domains
    In common with memoryless domains only register GI domains
    if the proximity node is not online. If a domain is already
    a memory containing domain, or a memoryless domain there is
    nothing to do just because it also contains a Generic Initiator.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Acked-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- arch/x86/mm/numa.c
+++ arch/x86/mm/numa.c
@@ -748,6 +748,27 @@ static void __init init_memory_less_node(int nid)
 }
 
 /*
+ * A node may exist which has one or more Generic Initiators but no CPUs and no
+ * memory.
+ *
+ * This function must be called after init_cpu_to_node(), to ensure that any
+ * memoryless CPU nodes have already been brought online, and before the
+ * node_data[nid] is needed for zone list setup in build_all_zonelists().
+ *
+ * When this function is called, any nodes containing either memory and/or CPUs
+ * will already be online and there is no need to do anything extra, even if
+ * they also contain one or more Generic Initiators.
+ */
+void __init init_gi_nodes(void)
+{
+	int nid;
+
+	for_each_node_state(nid, N_GENERIC_INITIATOR)
+		if (!node_online(nid))
+			init_memory_less_node(nid);
+}
+
+/*
  * Setup early cpu_to_node.
  *
  * Populate cpu_to_node[] only if x86_cpu_to_apicid[],
