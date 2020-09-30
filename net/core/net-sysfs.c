000fe2685b0a59ade602918a95de450c73aaf6fc
Ye Bin <yebin10@huawei.com>
2020-09-30 09:08:38 +0800

    net-sysfs: Fix inconsistent of format with argument type in net-sysfs.c
    Fix follow warnings:
    [net/core/net-sysfs.c:1161]: (warning) %u in format string (no. 1)
    	requires 'unsigned int' but the argument type is 'int'.
    [net/core/net-sysfs.c:1162]: (warning) %u in format string (no. 1)
    	requires 'unsigned int' but the argument type is 'int'.
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Ye Bin <yebin10@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/net-sysfs.c
+++ net/core/net-sysfs.c
@@ -1158,8 +1158,8 @@ static ssize_t traffic_class_show(struct netdev_queue *queue,
 	 * belongs to the root device it will be reported with just the
 	 * traffic class, so just "0" for TC 0 for example.
 	 */
-	return dev->num_tc < 0 ? sprintf(buf, "%u%d\n", tc, dev->num_tc) :
-				 sprintf(buf, "%u\n", tc);
+	return dev->num_tc < 0 ? sprintf(buf, "%d%d\n", tc, dev->num_tc) :
+				 sprintf(buf, "%d\n", tc);
 }
 
 #ifdef CONFIG_XPS
