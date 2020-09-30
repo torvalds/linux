46d43ee48d4fed3f8a5c93295f5a414c71a30fd4
Alexandre Belloni <alexandre.belloni@bootlin.com>
2020-09-30 16:56:54 +0200

    i2c: at91: remove legacy DMA left overs
    Commit dc6df6e90de9 ("i2c: at91: remove legacy DMA support") removed legcy
    DMA support from the driver. Remove the last use of the definitions from
    linux/platform_data/dma-atmel.h and stop including this header.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Reviewed-by: Codrin Ciubotariu <codrin.ciubotariu@microchip.com>
    [wsa: fixed typo in subject]
    Signed-off-by: Wolfram Sang <wsa@kernel.org>

--- drivers/i2c/busses/i2c-at91.h
+++ drivers/i2c/busses/i2c-at91.h
@@ -18,7 +18,6 @@
 #include <linux/dma-mapping.h>
 #include <linux/dmaengine.h>
 #include <linux/i2c.h>
-#include <linux/platform_data/dma-atmel.h>
 #include <linux/platform_device.h>
 
 #define AT91_I2C_TIMEOUT	msecs_to_jiffies(100)	/* transfer timeout */
@@ -123,7 +122,6 @@ struct at91_twi_pdata {
 	bool has_adv_dig_filtr;
 	bool has_ana_filtr;
 	bool has_clear_cmd;
-	struct at_dma_slave dma_slave;
 };
 
 struct at91_twi_dma {
