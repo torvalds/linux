3031e0288e60f09533339e61117b83099a6e126e
Peter Xu <peterx@redhat.com>
2020-09-30 21:22:28 -0400

    KVM: selftests: Always clear dirty bitmap after iteration
    We used not to clear the dirty bitmap before because KVM_GET_DIRTY_LOG
    would overwrite it the next time it copies the dirty log onto it.
    In the upcoming dirty ring tests we'll start to fetch dirty pages from
    a ring buffer, so no one is going to clear the dirty bitmap for us.
    
    Reviewed-by: Andrew Jones <drjones@redhat.com>
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Message-Id: <20201001012228.5916-1-peterx@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- tools/testing/selftests/kvm/dirty_log_test.c
+++ tools/testing/selftests/kvm/dirty_log_test.c
@@ -195,7 +195,7 @@ static void vm_dirty_log_verify(enum vm_guest_mode mode, unsigned long *bmap)
 				    page);
 		}
 
-		if (test_bit_le(page, bmap)) {
+		if (test_and_clear_bit_le(page, bmap)) {
 			host_dirty_count++;
 			/*
 			 * If the bit is set, the value written onto
