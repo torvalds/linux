388e201d41fa1ed8f2dce0f0567f56f8e919ffb0
Vineetha G. Jaya Kumaran <vineetha.g.jaya.kumaran@intel.com>
2020-10-01 23:56:09 +0800

    net: stmmac: Modify configuration method of EEE timers
    Ethtool manual stated that the tx-timer is the "the amount of time the
    device should stay in idle mode prior to asserting its Tx LPI". The
    previous implementation for "ethtool --set-eee tx-timer" sets the LPI TW
    timer duration which is not correct. Hence, this patch fixes the
    "ethtool --set-eee tx-timer" to configure the EEE LPI timer.
    
    The LPI TW Timer will be using the defined default value instead of
    "ethtool --set-eee tx-timer" which follows the EEE LS timer implementation.
    
    Changelog V2
    *Not removing/modifying the eee_timer.
    *EEE LPI timer can be configured through ethtool and also the eee_timer
    module param.
    *EEE TW Timer will be configured with default value only, not able to be
    configured through ethtool or module param. This follows the implementation
    of the EEE LS Timer.
    
    Fixes: d765955d2ae0 ("stmmac: add the Energy Efficient Ethernet support")
    Signed-off-by: Vineetha G. Jaya Kumaran <vineetha.g.jaya.kumaran@intel.com>
    Signed-off-by: Voon Weifeng <weifeng.voon@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
@@ -665,6 +665,7 @@ static int stmmac_ethtool_op_get_eee(struct net_device *dev,
 	edata->eee_enabled = priv->eee_enabled;
 	edata->eee_active = priv->eee_active;
 	edata->tx_lpi_timer = priv->tx_lpi_timer;
+	edata->tx_lpi_enabled = priv->tx_lpi_enabled;
 
 	return phylink_ethtool_get_eee(priv->phylink, edata);
 }
@@ -678,6 +679,10 @@ static int stmmac_ethtool_op_set_eee(struct net_device *dev,
 	if (!priv->dma_cap.eee)
 		return -EOPNOTSUPP;
 
+	if (priv->tx_lpi_enabled != edata->tx_lpi_enabled)
+		netdev_warn(priv->dev,
+			    "Setting EEE tx-lpi is not supported\n");
+
 	if (!edata->eee_enabled)
 		stmmac_disable_eee_mode(priv);
 
@@ -685,7 +690,12 @@ static int stmmac_ethtool_op_set_eee(struct net_device *dev,
 	if (ret)
 		return ret;
 
-	priv->tx_lpi_timer = edata->tx_lpi_timer;
+	if (edata->eee_enabled &&
+	    priv->tx_lpi_timer != edata->tx_lpi_timer) {
+		priv->tx_lpi_timer = edata->tx_lpi_timer;
+		stmmac_eee_init(priv);
+	}
+
 	return 0;
 }
 
