4b74fd793a77f6149d68dbf95574311af9506e39
Chuck Lever <chuck.lever@oracle.com>
2020-10-01 19:00:00 -0400

    NFSD: Map nfserr_wrongsec outside of nfsd_dispatch
    Refactor: Handle this NFS version-specific mapping in the only
    place where nfserr_wrongsec is generated.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/nfssvc.c
+++ fs/nfsd/nfssvc.c
@@ -964,8 +964,6 @@ static __be32 map_new_errors(u32 vers, __be32 nfserr)
 {
 	if (nfserr == nfserr_jukebox && vers == 2)
 		return nfserr_dropit;
-	if (nfserr == nfserr_wrongsec && vers < 4)
-		return nfserr_acces;
 	return nfserr;
 }
 
