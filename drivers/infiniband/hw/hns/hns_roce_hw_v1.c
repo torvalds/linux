b8e3130dd96b7b2d6d92e62dcd1515af30212fe2
Jason Gunthorpe <jgg@nvidia.com>
2020-10-03 20:20:02 -0300

    RDMA: Remove uverbs_ex_cmd_mask values that are linked to functions
    Since a while now the uverbs layer checks if the driver implements a
    function before allowing the ucmd to proceed. This largely obsoletes the
    cmd_mask stuff, but there is some tricky bits in drivers preventing it
    from being removed.
    
    Remove the easy elements of uverbs_ex_cmd_mask by pre-setting them in the
    core code. These are triggered soley based on the related ops function
    pointer.
    
    query_device_ex is not triggered based on an op, but all drivers already
    implement something compatible with the extension, so enable it globally
    too.
    
    Link: https://lore.kernel.org/r/2-v1-caa70ba3d1ab+1436e-ucmd_mask_jgg@nvidia.com
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/infiniband/hw/hns/hns_roce_hw_v1.c
+++ drivers/infiniband/hw/hns/hns_roce_hw_v1.c
@@ -2062,11 +2062,6 @@ static void hns_roce_v1_write_cqc(struct hns_roce_dev *hr_dev,
 		       CQ_CONTEXT_CQC_BYTE_32_CQ_CONS_IDX_S, 0);
 }
 
-static int hns_roce_v1_modify_cq(struct ib_cq *cq, u16 cq_count, u16 cq_period)
-{
-	return -EOPNOTSUPP;
-}
-
 static int hns_roce_v1_req_notify_cq(struct ib_cq *ibcq,
 				     enum ib_cq_notify_flags flags)
 {
@@ -4347,7 +4342,6 @@ static void hns_roce_v1_cleanup_eq_table(struct hns_roce_dev *hr_dev)
 
 static const struct ib_device_ops hns_roce_v1_dev_ops = {
 	.destroy_qp = hns_roce_v1_destroy_qp,
-	.modify_cq = hns_roce_v1_modify_cq,
 	.poll_cq = hns_roce_v1_poll_cq,
 	.post_recv = hns_roce_v1_post_recv,
 	.post_send = hns_roce_v1_post_send,
@@ -4367,7 +4361,6 @@ static const struct hns_roce_hw hns_roce_hw_v1 = {
 	.set_mtu = hns_roce_v1_set_mtu,
 	.write_mtpt = hns_roce_v1_write_mtpt,
 	.write_cqc = hns_roce_v1_write_cqc,
-	.modify_cq = hns_roce_v1_modify_cq,
 	.clear_hem = hns_roce_v1_clear_hem,
 	.modify_qp = hns_roce_v1_modify_qp,
 	.query_qp = hns_roce_v1_query_qp,
