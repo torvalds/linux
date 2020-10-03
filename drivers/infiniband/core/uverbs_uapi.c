bd2a40cc2463766ed1a55d94a4ccbdcd621da323
Jason Gunthorpe <jgg@nvidia.com>
2020-10-03 20:20:09 -0300

    RDMA/core Remove uverbs_ex_cmd_mask
    No driver sets it, and the core code sets a maximum mask, simply remove
    it.
    
    Disabled operations are now handled either by having a NULL ops pointer,
    or by having the common driver callbacks check for unsupported extended
    attributes.
    
    Link: https://lore.kernel.org/r/9-v1-caa70ba3d1ab+1436e-ucmd_mask_jgg@nvidia.com
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/infiniband/core/uverbs_uapi.c
+++ drivers/infiniband/core/uverbs_uapi.c
@@ -79,10 +79,7 @@ static int uapi_create_write(struct uverbs_api *uapi,
 
 	method_elm->is_ex = def->write.is_ex;
 	method_elm->handler = def->func_write;
-	if (def->write.is_ex)
-		method_elm->disabled = !(ibdev->uverbs_ex_cmd_mask &
-					 BIT_ULL(def->write.command_num));
-	else
+	if (!def->write.is_ex)
 		method_elm->disabled = !(ibdev->uverbs_cmd_mask &
 					 BIT_ULL(def->write.command_num));
 
--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -2667,7 +2667,6 @@ struct ib_device {
 	const struct attribute_group	*groups[3];
 
 	u64			     uverbs_cmd_mask;
-	u64			     uverbs_ex_cmd_mask;
 
 	char			     node_desc[IB_DEVICE_NODE_DESC_MAX];
 	__be64			     node_guid;
