8031b93efa8d393b7e38fa66b836ac157a2f354d
Codrin Ciubotariu <codrin.ciubotariu@microchip.com>
2020-10-04 12:45:05 +0300

    ASoC: mchp-spdifrx: convert to devm_platform_get_and_ioremap_resource
    Use the helper function that wraps the calls to platform_get_resource()
    and devm_ioremap_resource() together.
    
    Signed-off-by: Codrin Ciubotariu <codrin.ciubotariu@microchip.com>
    Link: https://lore.kernel.org/r/20201004094505.1041898-1-codrin.ciubotariu@microchip.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/atmel/mchp-spdifrx.c
+++ sound/soc/atmel/mchp-spdifrx.c
@@ -873,8 +873,7 @@ static int mchp_spdifrx_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	/* Map I/O registers. */
-	mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	base = devm_ioremap_resource(&pdev->dev, mem);
+	base = devm_platform_get_and_ioremap_resource(pdev, 0, &mem);
 	if (IS_ERR(base))
 		return PTR_ERR(base);
 
