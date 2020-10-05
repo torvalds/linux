353e228eb355be5a65a3c0996c774a0f46737fda
Mark Rutland <mark.rutland@arm.com>
2020-10-05 17:43:03 +0100

    arm64: initialize per-cpu offsets earlier
    The current initialization of the per-cpu offset register is difficult
    to follow and this initialization is not always early enough for
    upcoming instrumentation with KCSAN, where the instrumentation callbacks
    use the per-cpu offset.
    
    To make it possible to support KCSAN, and to simplify reasoning about
    early bringup code, let's initialize the per-cpu offset earlier, before
    we run any C code that may consume it. To do so, this patch adds a new
    init_this_cpu_offset() helper that's called before the usual
    primary/secondary start functions. For consistency, this is also used to
    re-initialize the per-cpu offset after the runtime per-cpu areas have
    been allocated (which can change CPU0's offset).
    
    So that init_this_cpu_offset() isn't subject to any instrumentation that
    might consume the per-cpu offset, it is marked with noinstr, preventing
    instrumentation.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20201005164303.21389-1-mark.rutland@arm.com
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -192,10 +192,7 @@ asmlinkage notrace void secondary_start_kernel(void)
 	u64 mpidr = read_cpuid_mpidr() & MPIDR_HWID_BITMASK;
 	struct mm_struct *mm = &init_mm;
 	const struct cpu_operations *ops;
-	unsigned int cpu;
-
-	cpu = task_cpu(current);
-	set_my_cpu_offset(per_cpu_offset(cpu));
+	unsigned int cpu = smp_processor_id();
 
 	/*
 	 * All kernel threads share the same mm context; grab a
@@ -435,7 +432,13 @@ void __init smp_cpus_done(unsigned int max_cpus)
 
 void __init smp_prepare_boot_cpu(void)
 {
-	set_my_cpu_offset(per_cpu_offset(smp_processor_id()));
+	/*
+	 * Now that setup_per_cpu_areas() has allocated the runtime per-cpu
+	 * areas it is only safe to read the CPU0 boot-time area, and we must
+	 * reinitialize the offset to point to the runtime area.
+	 */
+	init_this_cpu_offset();
+
 	cpuinfo_store_boot_cpu();
 
 	/*
