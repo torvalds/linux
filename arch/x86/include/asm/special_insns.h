0888e1030d3e3e5ce9dfd8e030cf13a2e9a1519a
Dave Jiang <dave.jiang@intel.com>
2020-10-05 08:11:22 -0700

    x86/asm: Carve out a generic movdir64b() helper for general usage
    Carve out the MOVDIR64B inline asm primitive into a generic helper so
    that it can be used by other functions. Move it to special_insns.h and
    have iosubmit_cmds512() call it.
    
     [ bp: Massage commit message. ]
    
    Suggested-by: Michael Matz <matz@suse.de>
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Reviewed-by: Borislav Petkov <bp@suse.de>
    Link: https://lkml.kernel.org/r/20201005151126.657029-2-dave.jiang@intel.com

--- arch/x86/include/asm/special_insns.h
+++ arch/x86/include/asm/special_insns.h
@@ -234,6 +234,28 @@ static inline void clwb(volatile void *__p)
 
 #define nop() asm volatile ("nop")
 
+/* The dst parameter must be 64-bytes aligned */
+static inline void movdir64b(void *dst, const void *src)
+{
+	const struct { char _[64]; } *__src = src;
+	struct { char _[64]; } *__dst = dst;
+
+	/*
+	 * MOVDIR64B %(rdx), rax.
+	 *
+	 * Both __src and __dst must be memory constraints in order to tell the
+	 * compiler that no other memory accesses should be reordered around
+	 * this one.
+	 *
+	 * Also, both must be supplied as lvalues because this tells
+	 * the compiler what the object is (its size) the instruction accesses.
+	 * I.e., not the pointers but what they point to, thus the deref'ing '*'.
+	 */
+	asm volatile(".byte 0x66, 0x0f, 0x38, 0xf8, 0x02"
+		     : "+m" (*__dst)
+		     :  "m" (*__src), "a" (__dst), "d" (__src));
+}
+
 #endif /* __KERNEL__ */
 
 #endif /* _ASM_X86_SPECIAL_INSNS_H */
