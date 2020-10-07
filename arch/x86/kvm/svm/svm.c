2f04a04d06509fbba0a4069dba5d71010139d921
Sean Christopherson <sean.j.christopherson@intel.com>
2020-10-06 18:44:14 -0700

    KVM: SVM: Drop VMXE check from svm_set_cr4()
    [ Upstream commit 311a06593b9a3944a63ed176b95cb8d857f7c83b ]
    
    Drop svm_set_cr4()'s explicit check CR4.VMXE now that common x86 handles
    the check by incorporating VMXE into the CR4 reserved bits, via
    kvm_cpu_caps.  SVM obviously does not set X86_FEATURE_VMX.
    
    No functional change intended.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Message-Id: <20201007014417.29276-4-sean.j.christopherson@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -1697,9 +1697,6 @@ int svm_set_cr4(struct kvm_vcpu *vcpu, unsigned long cr4)
 	unsigned long host_cr4_mce = cr4_read_shadow() & X86_CR4_MCE;
 	unsigned long old_cr4 = to_svm(vcpu)->vmcb->save.cr4;
 
-	if (cr4 & X86_CR4_VMXE)
-		return 1;
-
 	if (npt_enabled && ((old_cr4 ^ cr4) & X86_CR4_PGE))
 		svm_flush_tlb(vcpu);
 
