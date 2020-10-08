722faae3826ad45ea3207916f631e187ec93aab0
Taehee Yoo <ap420073@gmail.com>
2020-10-08 15:51:57 +0000

    mt76: mt7915: set fops_sta_stats.owner to THIS_MODULE
    [ Upstream commit 5efbe3b1b8992d5f837388091920945c23212159 ]
    
    If THIS_MODULE is not set, the module would be removed while debugfs is
    being used.
    It eventually makes kernel panic.
    
    Fixes: ec9742a8f38e ("mt76: mt7915: add .sta_add_debugfs support")
    Signed-off-by: Taehee Yoo <ap420073@gmail.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/wireless/mediatek/mt76/mt7915/debugfs.c
+++ drivers/net/wireless/mediatek/mt76/mt7915/debugfs.c
@@ -460,6 +460,7 @@ static const struct file_operations fops_sta_stats = {
 	.read = seq_read,
 	.llseek = seq_lseek,
 	.release = single_release,
+	.owner = THIS_MODULE,
 };
 
 void mt7915_sta_add_debugfs(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
