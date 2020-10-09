f205edf77315a33eee82a7615fb57e9297957fe9
Josef Bacik <josef@toxicpanda.com>
2020-10-09 09:28:24 -0400

    btrfs: check reclaim_size in need_preemptive_reclaim
    If we're flushing space for tickets then we have
    space_info->reclaim_size set and we do not need to do background
    reclaim.
    
    Reviewed-by: Nikolay Borisov <nborisov@suse.com>
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/space-info.c
+++ fs/btrfs/space-info.c
@@ -818,6 +818,13 @@ static bool need_preemptive_reclaim(struct btrfs_fs_info *fs_info,
 	if ((space_info->bytes_used + space_info->bytes_reserved) >= thresh)
 		return false;
 
+	/*
+	 * We have tickets queued, bail so we don't compete with the async
+	 * flushers.
+	 */
+	if (space_info->reclaim_size)
+		return false;
+
 	if (!btrfs_calc_reclaim_metadata_size(fs_info, space_info))
 		return false;
 
