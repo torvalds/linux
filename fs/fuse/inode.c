413daa1a3f4a50af7172a862f391867711e9bc04
Miklos Szeredi <mszeredi@redhat.com>
2020-10-09 12:40:11 +0200

    fuse: connection remove fix
    Re-add lost removal of fc from fuse_conn_list and the control filesystem.
    
    Reported-by: kernel test robot <rong.a.chen@intel.com>
    Fixes: fcee216beb9c ("fuse: split fuse_mount off of fuse_conn")
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -1547,6 +1547,13 @@ void fuse_conn_destroy(struct fuse_mount *fm)
 
 	fuse_abort_conn(fc);
 	fuse_wait_aborted(fc);
+
+	if (!list_empty(&fc->entry)) {
+		mutex_lock(&fuse_mutex);
+		list_del(&fc->entry);
+		fuse_ctl_remove_conn(fc);
+		mutex_unlock(&fuse_mutex);
+	}
 }
 EXPORT_SYMBOL_GPL(fuse_conn_destroy);
 
