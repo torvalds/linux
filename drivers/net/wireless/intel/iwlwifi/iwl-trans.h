b7d96bca1f004b5f26ee51ea9c9749a28dac8316
Luca Coelho <luciano.coelho@intel.com>
2020-10-09 12:21:23 +0300

    Revert "iwlwifi: remove wide_cmd_header field"
    This reverts commit 0a8159cbd11abf586693f1ec5264d0df587b71d8.
    
    It turns out that this flag is used by iwldvm, so we can't get rid of
    it.  This broke iwldvm devices with BAD_COMMAND errors.
    
    Fixes: 0a8159cbd11a ("iwlwifi: remove wide_cmd_header field")
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/iwlwifi.20201009122123.3e4ee0ad7a71.Id6d95ae601f048aeb4d2ed63a1712e469da84369@changeid

--- drivers/net/wireless/intel/iwlwifi/iwl-trans.h
+++ drivers/net/wireless/intel/iwlwifi/iwl-trans.h
@@ -968,6 +968,7 @@ struct iwl_trans_txqs {
  * @hw_id_str: a string with info about HW ID. Set during transport allocation.
  * @pm_support: set to true in start_hw if link pm is supported
  * @ltr_enabled: set to true if the LTR is enabled
+ * @wide_cmd_header: true when ucode supports wide command header format
  * @num_rx_queues: number of RX queues allocated by the transport;
  *	the transport must set this before calling iwl_drv_start()
  * @iml_len: the length of the image loader
@@ -1009,6 +1010,7 @@ struct iwl_trans {
 
 	const struct iwl_hcmd_arr *command_groups;
 	int command_groups_size;
+	bool wide_cmd_header;
 
 	u8 num_rx_queues;
 
