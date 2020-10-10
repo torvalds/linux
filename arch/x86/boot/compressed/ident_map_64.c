e5ceb9a02402b984feecb95a82239be151c9f4e2
Arvind Sankar <nivedita@alum.mit.edu>
2020-10-10 15:11:10 -0400

    x86/boot/64: Initialize 5-level paging variables earlier
    Commit
    
      ca0e22d4f011 ("x86/boot/compressed/64: Always switch to own page table")
    
    started using a new set of pagetables even without KASLR.
    
    After that commit, initialize_identity_maps() is called before the
    5-level paging variables are setup in choose_random_location(), which
    will not work if 5-level paging is actually enabled.
    
    Fix this by moving the initialization of __pgtable_l5_enabled,
    pgdir_shift and ptrs_per_p4d into cleanup_trampoline(), which is called
    immediately after the finalization of whether the kernel is executing
    with 4- or 5-level paging. This will be earlier than anything that might
    require those variables, and keeps the 4- vs 5-level paging code all in
    one place.
    
    Fixes: ca0e22d4f011 ("x86/boot/compressed/64: Always switch to own page table")
    Signed-off-by: Arvind Sankar <nivedita@alum.mit.edu>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Joerg Roedel <jroedel@suse.de>
    Tested-by: Joerg Roedel <jroedel@suse.de>
    Tested-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Link: https://lkml.kernel.org/r/20201010191110.4060905-1-nivedita@alum.mit.edu

--- arch/x86/boot/compressed/ident_map_64.c
+++ arch/x86/boot/compressed/ident_map_64.c
@@ -33,12 +33,6 @@
 #define __PAGE_OFFSET __PAGE_OFFSET_BASE
 #include "../../mm/ident_map.c"
 
-#ifdef CONFIG_X86_5LEVEL
-unsigned int __pgtable_l5_enabled;
-unsigned int pgdir_shift = 39;
-unsigned int ptrs_per_p4d = 1;
-#endif
-
 /* Used by PAGE_KERN* macros: */
 pteval_t __default_kernel_pte_mask __read_mostly = ~0;
 
