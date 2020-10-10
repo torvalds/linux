06d44142d49dc2e02d255ea9d72dc4c20f20388f
Matthew Wilcox (Oracle) <willy@infradead.org>
2020-10-10 11:47:55 -0400

    mm: Fix READ_ONLY_THP warning
    These counters only exist if CONFIG_READ_ONLY_THP_FOR_FS is defined,
    but we do not need to warn if the filesystem natively supports large
    folios.
    
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>

--- include/linux/pagemap.h
+++ include/linux/pagemap.h
@@ -326,7 +326,7 @@ static inline void filemap_nr_thps_inc(struct address_space *mapping)
 	if (!mapping_large_folio_support(mapping))
 		atomic_inc(&mapping->nr_thps);
 #else
-	WARN_ON_ONCE(1);
+	WARN_ON_ONCE(mapping_large_folio_support(mapping) == 0);
 #endif
 }
 
@@ -336,7 +336,7 @@ static inline void filemap_nr_thps_dec(struct address_space *mapping)
 	if (!mapping_large_folio_support(mapping))
 		atomic_dec(&mapping->nr_thps);
 #else
-	WARN_ON_ONCE(1);
+	WARN_ON_ONCE(mapping_large_folio_support(mapping) == 0);
 #endif
 }
 
