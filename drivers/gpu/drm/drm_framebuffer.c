bbdc899cf77d2d2d90ac55929be67af0a520775f
Narendra Muppalla <nmuppall@quicinc.com>
2020-10-12 16:29:48 -0700

    Revert "ANDROID: drivers: gpu: drm: add support for secure framebuffer"
    Necessary changes to support secure framebuffer
    have been made to vendor specific drivers. No need to retain
    support in DRM framework.
    
    This reverts commit 840c67f5c1c4f88c9cb1f20f39b6adde53415d30.
    
    Signed-off-by: Narendra Muppalla <NarendraM@codeaurora.org>
    Change-Id: Ia745208ae1c2c49443659292d31cbfa6b0c4f7e8

--- drivers/gpu/drm/drm_framebuffer.c
+++ drivers/gpu/drm/drm_framebuffer.c
@@ -296,8 +296,7 @@ drm_internal_framebuffer_create(struct drm_device *dev,
 	struct drm_framebuffer *fb;
 	int ret;
 
-	if (r->flags & ~(DRM_MODE_FB_INTERLACED | DRM_MODE_FB_MODIFIERS |
-			DRM_MODE_FB_SECURE)) {
+	if (r->flags & ~(DRM_MODE_FB_INTERLACED | DRM_MODE_FB_MODIFIERS)) {
 		DRM_DEBUG_KMS("bad framebuffer flags 0x%08x\n", r->flags);
 		return ERR_PTR(-EINVAL);
 	}
