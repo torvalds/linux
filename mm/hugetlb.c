7db5e7b67e3e8d7aa96beb2a4551d211b76a05ba
Wei Yang <richard.weiyang@linux.alibaba.com>
2020-10-13 16:56:20 -0700

    mm/hugetlb: not necessary to coalesce regions recursively
    Patch series "mm/hugetlb: code refine and simplification", v4.
    
    Following are some cleanups for hugetlb.  Simple testing with
    tools/testing/selftests/vm/map_hugetlb passes.
    
    This patch (of 7):
    
    Per my understanding, we keep the regions ordered and would always
    coalesce regions properly.  So the task to keep this property is just to
    coalesce its neighbour.
    
    Let's simplify this.
    
    Signed-off-by: Wei Yang <richard.weiyang@linux.alibaba.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Baoquan He <bhe@redhat.com>
    Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Link: https://lkml.kernel.org/r/20200901014636.29737-1-richard.weiyang@linux.alibaba.com
    Link: https://lkml.kernel.org/r/20200831022351.20916-1-richard.weiyang@linux.alibaba.com
    Link: https://lkml.kernel.org/r/20200831022351.20916-2-richard.weiyang@linux.alibaba.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -309,8 +309,7 @@ static void coalesce_file_region(struct resv_map *resv, struct file_region *rg)
 		list_del(&rg->link);
 		kfree(rg);
 
-		coalesce_file_region(resv, prg);
-		return;
+		rg = prg;
 	}
 
 	nrg = list_next_entry(rg, link);
@@ -320,9 +319,6 @@ static void coalesce_file_region(struct resv_map *resv, struct file_region *rg)
 
 		list_del(&rg->link);
 		kfree(rg);
-
-		coalesce_file_region(resv, nrg);
-		return;
 	}
 }
 
