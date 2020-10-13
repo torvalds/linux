f8fd52535c7326d72645c9878d7897aaf44db51c
Wei Yang <richard.weiyang@linux.alibaba.com>
2020-10-13 16:57:11 -0700

    mm: remove unused alloc_page_vma_node()
    No one use this macro anymore.
    
    Also fix code style of policy_node().
    
    Signed-off-by: Wei Yang <richard.weiyang@linux.alibaba.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Link: https://lkml.kernel.org/r/20200921021401.84508-1-richard.weiyang@linux.alibaba.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/mempolicy.c
+++ mm/mempolicy.c
@@ -1882,8 +1882,7 @@ nodemask_t *policy_nodemask(gfp_t gfp, struct mempolicy *policy)
 }
 
 /* Return the node id preferred by the given mempolicy, or the given id */
-static int policy_node(gfp_t gfp, struct mempolicy *policy,
-								int nd)
+static int policy_node(gfp_t gfp, struct mempolicy *policy, int nd)
 {
 	if (policy->mode == MPOL_PREFERRED && !(policy->flags & MPOL_F_LOCAL))
 		nd = policy->v.preferred_node;
