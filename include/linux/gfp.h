ab00db216c9c78cc0a68bc4e27889c1ee374598d
Michal Hocko <mhocko@suse.com>
2020-10-13 16:56:07 -0700

    include/linux/gfp.h: clarify usage of GFP_ATOMIC in !preemptible contexts
    There is a general understanding that GFP_ATOMIC/GFP_NOWAIT are to be used
    from atomic contexts.  E.g.  from within a spin lock or from the IRQ
    context.  This is correct but there are some atomic contexts where the
    above doesn't hold.  One of them would be an NMI context.  Page allocator
    has never supported that and the general fear of this context didn't let
    anybody to actually even try to use the allocator there.  Good, but let's
    be more specific about that.
    
    Another such a context, and that is where people seem to be more daring,
    is raw_spin_lock.  Mostly because it simply resembles regular spin lock
    which is supported by the allocator and there is not any implementation
    difference with !RT kernels in the first place.  Be explicit that such a
    context is not supported by the allocator.  The underlying reason is that
    zone->lock would have to become raw_spin_lock as well and that has turned
    out to be a problem for RT
    (http://lkml.kernel.org/r/87mu305c1w.fsf@nanos.tec.linutronix.de).
    
    Signed-off-by: Michal Hocko <mhocko@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Uladzislau Rezki <urezki@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@linux.vnet.ibm.com>
    Link: https://lkml.kernel.org/r/20200929123010.5137-1-mhocko@kernel.org
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/gfp.h
+++ include/linux/gfp.h
@@ -238,7 +238,9 @@ struct vm_area_struct;
  * %__GFP_FOO flags as necessary.
  *
  * %GFP_ATOMIC users can not sleep and need the allocation to succeed. A lower
- * watermark is applied to allow access to "atomic reserves"
+ * watermark is applied to allow access to "atomic reserves".
+ * The current implementation doesn't support NMI and few other strict
+ * non-preemptive contexts (e.g. raw_spin_lock). The same applies to %GFP_NOWAIT.
  *
  * %GFP_KERNEL is typical for kernel-internal allocations. The caller requires
  * %ZONE_NORMAL or a lower zone for direct access but can direct reclaim.
