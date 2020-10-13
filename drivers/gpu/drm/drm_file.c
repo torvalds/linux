88adaf726ee3fc04c88198ce7632b9ebc3e7d9b1
Narendra Muppalla <NarendraM@codeaurora.org>
2020-10-12 18:04:06 -0700

    Revert "ANDROID: drivers: gpu: drm: fix bugs encountered while fuzzing"
    Necessary changes to fix bugs encountered while fuzzing have been
    made to vendor specific drivers.
    
    This reverts commit 9a6a3c73590559bce0b247a564e101a79427eeac.
    
    Bug: 163689280
    Change-Id: I1a8f1c8ea51cc6d2d2a669a8ee513ef2c369687a
    Signed-off-by: Narendra Muppalla <NarendraM@codeaurora.org>

--- drivers/gpu/drm/drm_file.c
+++ drivers/gpu/drm/drm_file.c
@@ -56,8 +56,6 @@
 /* from BKL pushdown */
 DEFINE_MUTEX(drm_global_mutex);
 
-#define MAX_DRM_OPEN_COUNT		128
-
 bool drm_dev_needs_global_mutex(struct drm_device *dev)
 {
 	/*
@@ -425,11 +423,6 @@ int drm_open(struct inode *inode, struct file *filp)
 
 	if (!atomic_fetch_inc(&dev->open_count))
 		need_setup = 1;
-
-	if (atomic_read(&dev->open_count) >= MAX_DRM_OPEN_COUNT) {
-		retcode = -EPERM;
-		goto err_undo;
-	}
 
 	/* share address_space across all char-devs of a single device */
 	filp->f_mapping = dev->anon_inode->i_mapping;
