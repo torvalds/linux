92aeeafb544d7b75c38cb0677e691f72c8aa1d6a
Chengming Gui <Jack.Gui@amd.com>
2020-10-13 12:18:27 +0800

    drm/amd/amdgpu: set the default value of noretry to 1 for some dGPUs
    noretry = 0 cause some dGPU's kfd page fault tests fail,
    so set noretry to 1 for these special ASICs:
    vega20/navi10/navi14
    
    v2: merge raven and default case due to the same setting
    v3: remove ARCTURUS
    
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Acked-by: Felix Kuhling <Felix.Kuehling@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_gmc.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_gmc.c
@@ -425,20 +425,27 @@ void amdgpu_gmc_noretry_set(struct amdgpu_device *adev)
 	struct amdgpu_gmc *gmc = &adev->gmc;
 
 	switch (adev->asic_type) {
+	case CHIP_VEGA20:
+	case CHIP_NAVI10:
+	case CHIP_NAVI14:
+		/*
+		 * noretry = 0 will cause kfd page fault tests fail
+		 * for some ASICs, so set default to 1 for these ASICs.
+		 */
+		if (amdgpu_noretry == -1)
+			gmc->noretry = 1;
+		else
+			gmc->noretry = amdgpu_noretry;
+		break;
 	case CHIP_RAVEN:
+	default:
 		/* Raven currently has issues with noretry
 		 * regardless of what we decide for other
 		 * asics, we should leave raven with
 		 * noretry = 0 until we root cause the
 		 * issues.
-		 */
-		if (amdgpu_noretry == -1)
-			gmc->noretry = 0;
-		else
-			gmc->noretry = amdgpu_noretry;
-		break;
-	default:
-		/* default this to 0 for now, but we may want
+		 *
+		 * default this to 0 for now, but we may want
 		 * to change this in the future for certain
 		 * GPUs as it can increase performance in
 		 * certain cases.
