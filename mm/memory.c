a7069ee3f891b22727303dfe0857ae47ef0c4936
Yanfei Xu <yanfei.xu@windriver.com>
2020-10-13 16:53:29 -0700

    mm/memory.c: replace vmf->vma with variable vma
    The code has declared a vma_struct named vma which is assigned a value of
    vmf->vma.  Thus, use variable vma directly here.
    
    Signed-off-by: Yanfei Xu <yanfei.xu@windriver.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Link: http://lkml.kernel.org/r/20200818084607.37616-1-yanfei.xu@windriver.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/memory.c
+++ mm/memory.c
@@ -3597,7 +3597,7 @@ static vm_fault_t __do_fault(struct vm_fault *vmf)
 	 *				# flush A, B to clear the writeback
 	 */
 	if (pmd_none(*vmf->pmd) && !vmf->prealloc_pte) {
-		vmf->prealloc_pte = pte_alloc_one(vmf->vma->vm_mm);
+		vmf->prealloc_pte = pte_alloc_one(vma->vm_mm);
 		if (!vmf->prealloc_pte)
 			return VM_FAULT_OOM;
 		smp_wmb(); /* See comment in __pte_alloc() */
