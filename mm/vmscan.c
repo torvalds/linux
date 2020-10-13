01c4776ba08ca9ab8cf58fb27d311868193dd368
Hui Su <sh_def@163.com>
2020-10-13 16:56:49 -0700

    mm/vmscan: fix comments for isolate_lru_page()
    fix comments for isolate_lru_page():
    s/fundamentnal/fundamental
    
    Signed-off-by: Hui Su <sh_def@163.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Link: https://lkml.kernel.org/r/20200927173923.GA8058@rlk
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1754,7 +1754,7 @@ static unsigned long isolate_lru_pages(unsigned long nr_to_scan,
  * Restrictions:
  *
  * (1) Must be called with an elevated refcount on the page. This is a
- *     fundamentnal difference from isolate_lru_pages (which is called
+ *     fundamental difference from isolate_lru_pages (which is called
  *     without a stable reference).
  * (2) the lru_lock must not be held.
  * (3) interrupts must be enabled.
