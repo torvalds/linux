ce81bb256a224259ab686742a6284930cbe4f1fa
Chris Kennelly <ckennelly@google.com>
2020-10-15 20:12:32 -0700

    fs/binfmt_elf: use PT_LOAD p_align values for suitable start address
    Patch series "Selecting Load Addresses According to p_align", v3.
    
    The current ELF loading mechancism provides page-aligned mappings.  This
    can lead to the program being loaded in a way unsuitable for file-backed,
    transparent huge pages when handling PIE executables.
    
    While specifying -z,max-page-size=0x200000 to the linker will generate
    suitably aligned segments for huge pages on x86_64, the executable needs
    to be loaded at a suitably aligned address as well.  This alignment
    requires the binary's cooperation, as distinct segments need to be
    appropriately paddded to be eligible for THP.
    
    For binaries built with increased alignment, this limits the number of
    bits usable for ASLR, but provides some randomization over using fixed
    load addresses/non-PIE binaries.
    
    This patch (of 2):
    
    The current ELF loading mechancism provides page-aligned mappings.  This
    can lead to the program being loaded in a way unsuitable for file-backed,
    transparent huge pages when handling PIE executables.
    
    For binaries built with increased alignment, this limits the number of
    bits usable for ASLR, but provides some randomization over using fixed
    load addresses/non-PIE binaries.
    
    Tested by verifying program with -Wl,-z,max-page-size=0x200000 loading.
    
    [akpm@linux-foundation.org: fix max() warning]
    [ckennelly@google.com: augment comment]
      Link: https://lkml.kernel.org/r/20200821233848.3904680-2-ckennelly@google.com
    
    Signed-off-by: Chris Kennelly <ckennelly@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Song Liu <songliubraving@fb.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Hugh Dickens <hughd@google.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Sandeep Patil <sspatil@google.com>
    Cc: Fangrui Song <maskray@google.com>
    Cc: Nick Desaulniers <ndesaulniers@google.com>
    Cc: "Kirill A. Shutemov" <kirill.shutemov@linux.intel.com>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Shuah Khan <shuah@kernel.org>
    Link: https://lkml.kernel.org/r/20200820170541.1132271-1-ckennelly@google.com
    Link: https://lkml.kernel.org/r/20200820170541.1132271-2-ckennelly@google.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -13,6 +13,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
+#include <linux/log2.h>
 #include <linux/mm.h>
 #include <linux/mman.h>
 #include <linux/errno.h>
@@ -419,6 +420,26 @@ static int elf_read(struct file *file, void *buf, size_t len, loff_t pos)
 		return (rv < 0) ? rv : -EIO;
 	}
 	return 0;
+}
+
+static unsigned long maximum_alignment(struct elf_phdr *cmds, int nr)
+{
+	unsigned long alignment = 0;
+	int i;
+
+	for (i = 0; i < nr; i++) {
+		if (cmds[i].p_type == PT_LOAD) {
+			unsigned long p_align = cmds[i].p_align;
+
+			/* skip non-power of two alignments as invalid */
+			if (!is_power_of_2(p_align))
+				continue;
+			alignment = max(alignment, p_align);
+		}
+	}
+
+	/* ensure we align to at least one page */
+	return ELF_PAGEALIGN(alignment);
 }
 
 /**
@@ -1008,6 +1029,7 @@ out_free_interp:
 		int elf_prot, elf_flags;
 		unsigned long k, vaddr;
 		unsigned long total_size = 0;
+		unsigned long alignment;
 
 		if (elf_ppnt->p_type != PT_LOAD)
 			continue;
@@ -1086,6 +1108,9 @@ out_free_interp:
 				load_bias = ELF_ET_DYN_BASE;
 				if (current->flags & PF_RANDOMIZE)
 					load_bias += arch_mmap_rnd();
+				alignment = maximum_alignment(elf_phdata, elf_ex->e_phnum);
+				if (alignment)
+					load_bias &= ~(alignment - 1);
 				elf_flags |= MAP_FIXED;
 			} else
 				load_bias = 0;
