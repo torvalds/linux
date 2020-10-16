e2333dad2d4a3543f07beb5da61401046e54f2ff
Matthew Wilcox (Oracle) <willy@infradead.org>
2020-10-15 20:05:43 -0700

    mm/huge_memory: fix can_split_huge_page assumption of THP size
    Ask the page how many subpages it has instead of assuming it's PMD size.
    
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: SeongJae Park <sjpark@amazon.de>
    Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Acked-by: "Huang, Ying" <ying.huang@intel.com>
    Link: https://lkml.kernel.org/r/20200908195539.25896-8-willy@infradead.org
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -2582,9 +2582,9 @@ bool can_split_huge_page(struct page *page, int *pextra_pins)
 
 	/* Additional pins from page cache */
 	if (PageAnon(page))
-		extra_pins = PageSwapCache(page) ? HPAGE_PMD_NR : 0;
+		extra_pins = PageSwapCache(page) ? thp_nr_pages(page) : 0;
 	else
-		extra_pins = HPAGE_PMD_NR;
+		extra_pins = thp_nr_pages(page);
 	if (pextra_pins)
 		*pextra_pins = extra_pins;
 	return total_mapcount(page) == page_count(page) - extra_pins - 1;
