b94e02822debdf0cc473556aad7dcc859f216653
Oscar Salvador <osalvador@suse.de>
2020-10-15 20:07:29 -0700

    mm,hwpoison: try to narrow window race for free pages
    Aristeu Rozanski reported that a customer test case started to report
    -EBUSY after the hwpoison rework patchset.
    
    There is a race window between spotting a free page and taking it off its
    buddy freelist, so it might be that by the time we try to take it off, the
    page has been already allocated.
    
    This patch tries to handle such race window by trying to handle the new
    type of page again if the page was allocated under us.
    
    Reported-by: Aristeu Rozanski <aris@ruivo.org>
    Signed-off-by: Oscar Salvador <osalvador@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Tested-by: Aristeu Rozanski <aris@ruivo.org>
    Acked-by: Naoya Horiguchi <naoya.horiguchi@nec.com>
    Cc: "Aneesh Kumar K.V" <aneesh.kumar@linux.ibm.com>
    Cc: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Dmitry Yakunin <zeil@yandex-team.ru>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Oscar Salvador <osalvador@suse.com>
    Cc: Qian Cai <cai@lca.pw>
    Cc: Tony Luck <tony.luck@intel.com>
    Link: https://lkml.kernel.org/r/20200922135650.1634-15-osalvador@suse.de
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -1903,6 +1903,7 @@ int soft_offline_page(unsigned long pfn, int flags)
 {
 	int ret;
 	struct page *page;
+	bool try_again = true;
 
 	if (!pfn_valid(pfn))
 		return -ENXIO;
@@ -1918,6 +1919,7 @@ int soft_offline_page(unsigned long pfn, int flags)
 		return 0;
 	}
 
+retry:
 	get_online_mems();
 	ret = get_any_page(page, pfn, flags);
 	put_online_mems();
@@ -1925,7 +1927,10 @@ int soft_offline_page(unsigned long pfn, int flags)
 	if (ret > 0)
 		ret = soft_offline_in_use_page(page);
 	else if (ret == 0)
-		ret = soft_offline_free_page(page);
+		if (soft_offline_free_page(page) && try_again) {
+			try_again = false;
+			goto retry;
+		}
 
 	return ret;
 }
