b08d29a90fd5aaf705743ac332fab99e621bbfaf
Sugar Zhang <sugar.zhang@rock-chips.com>
2020-10-18 16:14:33 +0800

    ASoC: rockchip: pdm: Add support for rk3568 pdm
    Signed-off-by: Sugar Zhang <sugar.zhang@rock-chips.com>
    Change-Id: I41b6c2a443e118f3a125ce7376fa349d61d78b6b

--- sound/soc/rockchip/rockchip_pdm.c
+++ sound/soc/rockchip/rockchip_pdm.c
@@ -540,6 +540,8 @@ static const struct of_device_id rockchip_pdm_match[] = {
 	  .data = (void *)RK_PDM_RK3308 },
 	{ .compatible = "rockchip,rk3308-pdm",
 	  .data = (void *)RK_PDM_RK3308 },
+	{ .compatible = "rockchip,rk3568-pdm",
+	  .data = (void *)RK_PDM_RV1126 },
 	{ .compatible = "rockchip,rv1126-pdm",
 	  .data = (void *)RK_PDM_RV1126 },
 	{},
