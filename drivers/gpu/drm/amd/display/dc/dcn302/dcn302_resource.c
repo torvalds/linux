91bda9e9d248c749f99cc85538d16444507466f7
Chris Park <Chris.Park@amd.com>
2020-10-19 14:32:14 -0400

    drm/amd/display: Update panel register
    [Why]
    Incorrect panel register settings are
    applied for power sequence because the
    register macro is not defined in resource.
    
    [How]
    Implement same register space to future
    resource files.
    
    Signed-off-by: Chris Park <Chris.Park@amd.com>
    Reviewed-by: Joshua Aberback <Joshua.Aberback@amd.com>
    Acked-by: Qingqing Zhuo <qingqing.zhuo@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn302/dcn302_resource.c
+++ drivers/gpu/drm/amd/display/dc/dcn302/dcn302_resource.c
@@ -967,7 +967,7 @@ static const struct encoder_feature_support link_enc_feature = {
 		[id] = {\
 				LE_DCN3_REG_LIST(id), \
 				UNIPHY_DCN2_REG_LIST(phyid), \
-				DPCS_DCN2_REG_LIST(id), \
+				SRI(DP_DPHY_INTERNAL_CTRL, DP, id) \
 		}
 
 static const struct dcn10_link_enc_registers link_enc_regs[] = {
