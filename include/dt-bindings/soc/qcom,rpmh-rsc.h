419d3f80494fd8d2368909d9ae4270df9243db2a
Lina Iyer <ilina@codeaurora.org>
2020-10-19 14:48:53 -0600

    dt-bindings: Define FAST_PATH_TCS for use in RSC definition
    Add a new definition for fast-path TCS configuration.
    
    Change-Id: I610b14dcd893885e5b1c32c434caa7aa7102fd4c
    Signed-off-by: Lina Iyer <ilina@codeaurora.org>
    Signed-off-by: Maulik Shah <quic_mkshah@quicinc.com>

--- include/dt-bindings/soc/qcom,rpmh-rsc.h
+++ include/dt-bindings/soc/qcom,rpmh-rsc.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (c) 2016-2018, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2020, The Linux Foundation. All rights reserved.
  */
 
 #ifndef __DT_QCOM_RPMH_RSC_H__
@@ -10,5 +10,6 @@
 #define WAKE_TCS	1
 #define ACTIVE_TCS	2
 #define CONTROL_TCS	3
+#define FAST_PATH_TCS	4
 
 #endif /* __DT_QCOM_RPMH_RSC_H__ */
