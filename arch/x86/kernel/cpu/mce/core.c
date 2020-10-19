633cdaf29ec4aae29868320adb3a4f1c5b8c0eac
Tom Rix <trix@redhat.com>
2020-10-19 13:08:03 -0700

    x86/mce: Remove unneeded break
    A break is not needed if it is preceded by a return.
    
    Signed-off-by: Tom Rix <trix@redhat.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lkml.kernel.org/r/20201019200803.17619-1-trix@redhat.com

--- arch/x86/kernel/cpu/mce/core.c
+++ arch/x86/kernel/cpu/mce/core.c
@@ -1811,11 +1811,9 @@ static int __mcheck_cpu_ancient_init(struct cpuinfo_x86 *c)
 	case X86_VENDOR_INTEL:
 		intel_p5_mcheck_init(c);
 		return 1;
-		break;
 	case X86_VENDOR_CENTAUR:
 		winchip_mcheck_init(c);
 		return 1;
-		break;
 	default:
 		return 0;
 	}
