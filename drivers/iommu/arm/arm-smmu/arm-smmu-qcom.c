07a7f2caaa5a2619934491bab3c47b261c554fb0
Bjorn Andersson <bjorn.andersson@linaro.org>
2020-10-19 11:23:22 -0700

    iommu/arm-smmu-qcom: Read back stream mappings
    The Qualcomm boot loader configures stream mapping for the peripherals
    that it accesses and in particular it sets up the stream mapping for the
    display controller to be allowed to scan out a splash screen or EFI
    framebuffer.
    
    Read back the stream mappings during initialization and make the
    arm-smmu driver maintain the streams in bypass mode.
    
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Tested-by: Steev Klimaszewski <steev@kali.org>
    Acked-by: Robin Murphy <robin.murphy@arm.com>
    Link: https://lore.kernel.org/r/20201019182323.3162386-3-bjorn.andersson@linaro.org
    Signed-off-by: Will Deacon <will@kernel.org>

--- drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
@@ -23,6 +23,28 @@ static const struct of_device_id qcom_smmu_client_of_match[] __maybe_unused = {
 	{ }
 };
 
+static int qcom_smmu_cfg_probe(struct arm_smmu_device *smmu)
+{
+	u32 smr;
+	int i;
+
+	for (i = 0; i < smmu->num_mapping_groups; i++) {
+		smr = arm_smmu_gr0_read(smmu, ARM_SMMU_GR0_SMR(i));
+
+		if (FIELD_GET(ARM_SMMU_SMR_VALID, smr)) {
+			smmu->smrs[i].id = FIELD_GET(ARM_SMMU_SMR_ID, smr);
+			smmu->smrs[i].mask = FIELD_GET(ARM_SMMU_SMR_MASK, smr);
+			smmu->smrs[i].valid = true;
+
+			smmu->s2crs[i].type = S2CR_TYPE_BYPASS;
+			smmu->s2crs[i].privcfg = S2CR_PRIVCFG_DEFAULT;
+			smmu->s2crs[i].cbndx = 0xff;
+		}
+	}
+
+	return 0;
+}
+
 static int qcom_smmu_def_domain_type(struct device *dev)
 {
 	const struct of_device_id *match =
@@ -61,6 +83,7 @@ static int qcom_smmu500_reset(struct arm_smmu_device *smmu)
 }
 
 static const struct arm_smmu_impl qcom_smmu_impl = {
+	.cfg_probe = qcom_smmu_cfg_probe,
 	.def_domain_type = qcom_smmu_def_domain_type,
 	.reset = qcom_smmu500_reset,
 };
