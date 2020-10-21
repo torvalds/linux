29052f1b92f2bcb0419c544459f4c919bfb20898
Marc Zyngier <maz@kernel.org>
2020-10-21 21:52:54 +0100

    KVM: arm64: Simplify __kvm_enable_ssbs()
    Move the setting of SSBS directly into the HVC handler, using
    the C helpers rather than the inline asssembly code.
    
    Reviewed-by: Alexandru Elisei <alexandru.elisei@arm.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/include/asm/kvm_asm.h
+++ arch/arm64/include/asm/kvm_asm.h
@@ -189,8 +189,6 @@ extern void __kvm_timer_set_cntvoff(u64 cntvoff);
 
 extern int __kvm_vcpu_run(struct kvm_vcpu *vcpu);
 
-extern void __kvm_enable_ssbs(void);
-
 extern u64 __vgic_v3_get_ich_vtr_el2(void);
 extern u64 __vgic_v3_read_vmcr(void);
 extern void __vgic_v3_write_vmcr(u32 vmcr);
