a6a9cffad0a28a4a7a3a91b6ee13dd1baae4dfcb
Ronnie Sahlberg <lsahlber@redhat.com>
2020-10-21 10:22:33 +1000

    cifs: add files to host new mount api
    This will make it easier in the future, but also will allow us to
    shrink connect.c which is getting too big, and harder to read
    
    Signed-off-by: Ronnie Sahlberg <lsahlber@redhat.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Reviewed-by: Aurelien Aptel <aaptel@suse.com>

---
+++ fs/cifs/fs_context.c
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ *   Copyright (C) 2020, Microsoft Corporation.
+ *
+ *   Author(s): Steve French <stfrench@microsoft.com>
+ *              David Howells <dhowells@redhat.com>
+ */
+
---
+++ fs/cifs/fs_context.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ *   Copyright (C) 2020, Microsoft Corporation.
+ *
+ *   Author(s): Steve French <stfrench@microsoft.com>
+ *              David Howells <dhowells@redhat.com>
+ */
+
+#ifndef _FS_CONTEXT_H
+#define _FS_CONTEXT_H
+
+
+
+#endif
+
