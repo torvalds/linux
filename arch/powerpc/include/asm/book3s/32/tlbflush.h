a54d310856b9c1fe15ad67a2f8ee9edc02965a3a
Christophe Leroy <christophe.leroy@csgroup.eu>
2020-10-22 06:29:28 +0000

    powerpc/mm: Remove flush_tlb_page_nohash() prototype.
    flush_tlb_page_nohash() was removed by
    commit 703b41ad1a87 ("powerpc/mm: remove flush_tlb_page_nohash")
    
    Remove stale prototype and comment.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/4a58831da6d6ba4fe309b94aa1dd8f02982d46b2.1603348103.git.christophe.leroy@csgroup.eu

--- arch/powerpc/include/asm/book3s/32/tlbflush.h
+++ arch/powerpc/include/asm/book3s/32/tlbflush.h
@@ -8,7 +8,6 @@
  */
 extern void flush_tlb_mm(struct mm_struct *mm);
 extern void flush_tlb_page(struct vm_area_struct *vma, unsigned long vmaddr);
-extern void flush_tlb_page_nohash(struct vm_area_struct *vma, unsigned long addr);
 extern void flush_tlb_range(struct vm_area_struct *vma, unsigned long start,
 			    unsigned long end);
 extern void flush_tlb_kernel_range(unsigned long start, unsigned long end);
