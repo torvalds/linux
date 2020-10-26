109ccff96d05451c81c03e985abaebce557e929c
Jens Axboe <axboe@kernel.dk>
2020-10-26 14:32:29 -0600

    UPSTREAM: x86: Wire up TIF_NOTIFY_SIGNAL
    [ Upstream commit c8d5ed67936fddbe2ae845fc80397718006322d7 ]
    
    The generic entry code has support for TIF_NOTIFY_SIGNAL already. Just
    provide the TIF bit.
    
    [ tglx: Adopted to other TIF changes in x86 ]
    
    Change-Id: I3188c956ab5ed6abac721db8a12f3229f4c7d13c
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20201026203230.386348-4-axboe@kernel.dk
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 4b1dcf8ec9b2f11b57f1ff5dcaa1f8575c7dacb5)
    Bug: 268174392
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- arch/x86/include/asm/thread_info.h
+++ arch/x86/include/asm/thread_info.h
@@ -93,6 +93,7 @@ struct thread_info {
 #define TIF_NOTSC		16	/* TSC is not accessible in userland */
 #define TIF_IA32		17	/* IA32 compatibility process */
 #define TIF_SLD			18	/* Restore split lock detection on context switch */
+#define TIF_NOTIFY_SIGNAL	19	/* signal notifications exist */
 #define TIF_MEMDIE		20	/* is terminating due to OOM killer */
 #define TIF_POLLING_NRFLAG	21	/* idle is polling for TIF_NEED_RESCHED */
 #define TIF_IO_BITMAP		22	/* uses I/O bitmap */
@@ -121,6 +122,7 @@ struct thread_info {
 #define _TIF_NOCPUID		(1 << TIF_NOCPUID)
 #define _TIF_NOTSC		(1 << TIF_NOTSC)
 #define _TIF_IA32		(1 << TIF_IA32)
+#define _TIF_NOTIFY_SIGNAL	(1 << TIF_NOTIFY_SIGNAL)
 #define _TIF_SLD		(1 << TIF_SLD)
 #define _TIF_POLLING_NRFLAG	(1 << TIF_POLLING_NRFLAG)
 #define _TIF_IO_BITMAP		(1 << TIF_IO_BITMAP)
