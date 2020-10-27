c0ae11b3caf7c20b33c8e720ecaff0a0d3bf79c1
Greg Kroah-Hartman <gregkh@google.com>
2020-10-27 10:23:59 +0100

    Revert "x86: remove address space overrides using set_fs()"
    This reverts commit 47058bb54b57962b3958a936ddbc59355e4c5504.
    
    Bug: 171770067
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Ied453f67ec0d28d993c01bf86301218da146b756

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -482,6 +482,10 @@ extern unsigned int fpu_user_xstate_size;
 
 struct perf_event;
 
+typedef struct {
+	unsigned long		seg;
+} mm_segment_t;
+
 struct thread_struct {
 	/* Cached TLS descriptors: */
 	struct desc_struct	tls_array[GDT_ENTRY_TLS_ENTRIES];
@@ -533,6 +537,8 @@ struct thread_struct {
 	 * interrupts.
 	 */
 	unsigned long		iopl_emul;
+
+	mm_segment_t		addr_limit;
 
 	unsigned int		sig_on_uaccess_err:1;
 
@@ -780,12 +786,15 @@ static inline void spin_lock_prefetch(const void *x)
 #define INIT_THREAD  {							  \
 	.sp0			= TOP_OF_INIT_STACK,			  \
 	.sysenter_cs		= __KERNEL_CS,				  \
+	.addr_limit		= KERNEL_DS,				  \
 }
 
 #define KSTK_ESP(task)		(task_pt_regs(task)->sp)
 
 #else
-#define INIT_THREAD { }
+#define INIT_THREAD  {						\
+	.addr_limit		= KERNEL_DS,			\
+}
 
 extern unsigned long KSTK_ESP(struct task_struct *task);
 
