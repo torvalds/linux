d95203169d8095edeb6b4fb6770b023f3b807490
Greg Kroah-Hartman <gregkh@google.com>
2020-10-27 10:20:21 +0100

    Revert "powerpc: remove address space overrides using set_fs()"
    This reverts commit 5ae4998b5d6fc703a16c9fa935fb7d335843bf22.
    
    Bug: 171770067
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I4de8c34ddda2f5b5b23db1a97485ce12f6962f9e

--- arch/powerpc/lib/sstep.c
+++ arch/powerpc/lib/sstep.c
@@ -108,11 +108,11 @@ static nokprobe_inline long address_ok(struct pt_regs *regs,
 {
 	if (!user_mode(regs))
 		return 1;
-	if (__access_ok(ea, nb))
+	if (__access_ok(ea, nb, USER_DS))
 		return 1;
-	if (__access_ok(ea, 1))
+	if (__access_ok(ea, 1, USER_DS))
 		/* Access overlaps the end of the user region */
-		regs->dar = TASK_SIZE_MAX - 1;
+		regs->dar = USER_DS.seg;
 	else
 		regs->dar = ea;
 	return 0;
