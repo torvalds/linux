fa3bfa3527b31562dba28d49c0a18b722b42e226
Chris Wilson <chris@chris-wilson.co.uk>
2020-10-29 21:30:42 +0000

    drm: Quieten [zero] EDID carping
    We have a few displays in CI that always report their EDID as a bunch of
    zeroes. This is consistent behaviour, so one assumes intentional
    indication of an "absent" EDID. Flagging these consistent warnings
    detracts from CI.
    
    One option would be to ignore the zero EDIDs as intentional behaviour,
    but Ville would like to keep the information available for debugging.
    The simple alternative then is to reduce the loglevel for all the EDID
    dumping from WARN to DEBUG so the information is present but not annoy
    CI. Note that the bad EDID dumping is already only shown if
    drm.debug=KMS, it's just the loglevel chosen was set to be caught by CI
    if it ever occurred as it was expected to be an internal error not
    external.
    
    Closes: https://gitlab.freedesktop.org/drm/intel/-/issues/2203
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20201029213042.11672-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/drm_edid.c
+++ drivers/gpu/drm/drm_edid.c
@@ -1844,7 +1844,7 @@ static void connector_bad_edid(struct drm_connector *connector,
 	if (connector->bad_edid_counter++ && !drm_debug_enabled(DRM_UT_KMS))
 		return;
 
-	drm_warn(connector->dev, "%s: EDID is invalid:\n", connector->name);
+	drm_dbg_kms(connector->dev, "%s: EDID is invalid:\n", connector->name);
 	for (i = 0; i < num_blocks; i++) {
 		u8 *block = edid + i * EDID_LENGTH;
 		char prefix[20];
@@ -1856,7 +1856,7 @@ static void connector_bad_edid(struct drm_connector *connector,
 		else
 			sprintf(prefix, "\t[%02x] GOOD ", i);
 
-		print_hex_dump(KERN_WARNING,
+		print_hex_dump(KERN_DEBUG,
 			       prefix, DUMP_PREFIX_NONE, 16, 1,
 			       block, EDID_LENGTH, false);
 	}
