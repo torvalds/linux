3fcbb15c1a91a318fb9367bee24603af24648a2d
Prasad Sodagudi <psodagud@codeaurora.org>
2020-10-28 18:26:21 -0700

    ANDROID: android: Create debug_symbols driver
    Introduce new API to expose symbols useful for debugging the GKI kernel.
    Symbols exported from this driver would be difficult to maintain via the
    traditional EXPORT_SYMBOL_GPL.
    
    Bug: 160245776
    Change-Id: I92053450bd74788889d1b7a569e291a35cd525ea
    Signed-off-by: Elliot Berman <eberman@codeaurora.org>
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>

---
+++ include/linux/android_debug_symbols.h
@@ -0,0 +1,42 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2021, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _ANDROID_DEBUG_SYMBOLS_H
+#define _ANDROID_DEBUG_SYMBOLS_H
+
+enum android_debug_symbol {
+	ADS_SDATA = 0,
+	ADS_BSS_END,
+	ADS_PER_CPU_START,
+	ADS_PER_CPU_END,
+	ADS_START_RO_AFTER_INIT,
+	ADS_END_RO_AFTER_INIT,
+	ADS_LINUX_BANNER,
+	ADS_END
+};
+
+enum android_debug_per_cpu_symbol {
+	ADS_IRQ_STACK_PTR = 0,
+	ADS_DEBUG_PER_CPU_END
+};
+
+#ifdef CONFIG_ANDROID_DEBUG_SYMBOLS
+
+void *android_debug_symbol(enum android_debug_symbol symbol);
+void *android_debug_per_cpu_symbol(enum android_debug_per_cpu_symbol symbol);
+
+#else /* !CONFIG_ANDROID_DEBUG_SYMBOLS */
+
+static inline void *android_debug_symbol(enum android_debug_symbol symbol)
+{
+	return NULL;
+}
+static inline void *android_debug_per_cpu_symbol(enum android_debug_per_cpu_symbol symbol)
+{
+	return NULL;
+}
+#endif /* CONFIG_ANDROID_DEBUG_SYMBOLS */
+
+#endif /* _ANDROID_DEBUG_SYMBOLS_H */
