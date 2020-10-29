8d8d53cf8fd028310b1189165b939cde124895d7
Alexey Kardashevskiy <aik@ozlabs.ru>
2020-10-29 12:52:40 +1100

    dma-mapping: Allow mixing bypass and mapped DMA operation
    At the moment we allow bypassing DMA ops only when we can do this for
    the entire RAM. However there are configs with mixed type memory
    where we could still allow bypassing IOMMU in most cases;
    POWERPC with persistent memory is one example.
    
    This adds an arch hook to determine where bypass can still work and
    we invoke direct DMA API. The following patch checks the bus limit
    on POWERPC to allow or disallow direct mapping.
    
    This adds a ARCH_HAS_DMA_MAP_DIRECT config option to make the arch_xxxx
    hooks no-op by default.
    
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- include/linux/dma-map-ops.h
+++ include/linux/dma-map-ops.h
@@ -314,6 +314,20 @@ static inline void arch_dma_mark_clean(phys_addr_t paddr, size_t size)
 void *arch_dma_set_uncached(void *addr, size_t size);
 void arch_dma_clear_uncached(void *addr, size_t size);
 
+#ifdef CONFIG_ARCH_HAS_DMA_MAP_DIRECT
+bool arch_dma_map_page_direct(struct device *dev, phys_addr_t addr);
+bool arch_dma_unmap_page_direct(struct device *dev, dma_addr_t dma_handle);
+bool arch_dma_map_sg_direct(struct device *dev, struct scatterlist *sg,
+		int nents);
+bool arch_dma_unmap_sg_direct(struct device *dev, struct scatterlist *sg,
+		int nents);
+#else
+#define arch_dma_map_page_direct(d, a)		(false)
+#define arch_dma_unmap_page_direct(d, a)	(false)
+#define arch_dma_map_sg_direct(d, s, n)		(false)
+#define arch_dma_unmap_sg_direct(d, s, n)	(false)
+#endif
+
 #ifdef CONFIG_ARCH_HAS_SETUP_DMA_OPS
 void arch_setup_dma_ops(struct device *dev, u64 dma_base, u64 size,
 		const struct iommu_ops *iommu, bool coherent);
