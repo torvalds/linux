49a361327332c9221438397059067f9b205f690d
Dai Ngo <dai.ngo@oracle.com>
2020-10-29 15:07:16 -0400

    NFSD: fix missing refcount in nfsd4_copy by nfsd4_do_async_copy
    Need to initialize nfsd4_copy's refcount to 1 to avoid use-after-free
    warning when nfs4_put_copy is called from nfsd4_cb_offload_release.
    
    Fixes: ce0887ac96d3 ("NFSD add nfs4 inter ssc to nfsd4_copy")
    Signed-off-by: Dai Ngo <dai.ngo@oracle.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/nfs4proc.c
+++ fs/nfsd/nfs4proc.c
@@ -1486,6 +1486,7 @@ do_callback:
 	cb_copy = kzalloc(sizeof(struct nfsd4_copy), GFP_KERNEL);
 	if (!cb_copy)
 		goto out;
+	refcount_set(&cb_copy->refcount, 1);
 	memcpy(&cb_copy->cp_res, &copy->cp_res, sizeof(copy->cp_res));
 	cb_copy->cp_clp = copy->cp_clp;
 	cb_copy->nfserr = copy->nfserr;
