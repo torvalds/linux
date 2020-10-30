7bf481d7e75a474e2a0f564b4de6bc2f1eeafc9a
Parav Pandit <parav@nvidia.com>
2020-10-30 22:44:16 +0200

    net/mlx5: E-Switch, let user to enable disable metadata
    Currently each packet inserted in eswitch is tagged with a internal
    metadata to indicate source vport. Metadata tagging is not always
    needed. Metadata insertion is needed for multi-port RoCE, failover
    between representors and stacked devices. In many other cases,
    metadata enablement is not needed.
    
    Metadata insertion slows down the packet processing rate of the E-switch
    when it is in switchdev mode.
    
    Below table show performance gain with metadata disabled for VXLAN
    offload rules in both SMFS and DMFS steering mode on ConnectX-5 device.
    
    ----------------------------------------------
    | steering | metadata | pkt size | rx pps    |
    | mode     |          |          | (million) |
    ----------------------------------------------
    | smfs     | disabled | 128Bytes | 42        |
    ----------------------------------------------
    | smfs     | enabled  | 128Bytes | 36        |
    ----------------------------------------------
    | dmfs     | disabled | 128Bytes | 42        |
    ----------------------------------------------
    | dmfs     | enabled  | 128Bytes | 36        |
    ----------------------------------------------
    
    Hence, allow user to disable metadata using driver specific devlink
    parameter. Metadata setting of the eswitch is applicable only for the
    switchdev mode.
    
    Example to show and disable metadata before changing eswitch mode:
    $ devlink dev param show pci/0000:06:00.0 name esw_port_metadata
    pci/0000:06:00.0:
      name esw_port_metadata type driver-specific
        values:
          cmode runtime value true
    
    $ devlink dev param set pci/0000:06:00.0 \
    	  name esw_port_metadata value false cmode runtime
    
    $ devlink dev eswitch set pci/0000:06:00.0 mode switchdev
    
    Signed-off-by: Parav Pandit <parav@nvidia.com>
    Reviewed-by: Roi Dayan <roid@nvidia.com>
    Reviewed-by: Vu Pham <vuhuong@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    ---
    changelog:
    v1->v2:
     - added performance numbers in commit log
     - updated commit log and documentation for switchdev mode
     - added explicit note on when user can disable metadata in
       documentation

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch.h
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch.h
@@ -314,6 +314,8 @@ int esw_offloads_enable(struct mlx5_eswitch *esw);
 void esw_offloads_cleanup_reps(struct mlx5_eswitch *esw);
 int esw_offloads_init_reps(struct mlx5_eswitch *esw);
 
+bool mlx5_esw_vport_match_metadata_supported(const struct mlx5_eswitch *esw);
+int mlx5_esw_offloads_vport_metadata_set(struct mlx5_eswitch *esw, bool enable);
 u32 mlx5_esw_match_metadata_alloc(struct mlx5_eswitch *esw);
 void mlx5_esw_match_metadata_free(struct mlx5_eswitch *esw, u32 metadata);
 
--- drivers/net/ethernet/mellanox/mlx5/core/eswitch_offloads.c
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch_offloads.c
@@ -2351,8 +2351,7 @@ static void esw_offloads_devcom_cleanup(struct mlx5_eswitch *esw)
 	mlx5_devcom_unregister_component(devcom, MLX5_DEVCOM_ESW_OFFLOADS);
 }
 
-static bool
-esw_check_vport_match_metadata_supported(const struct mlx5_eswitch *esw)
+bool mlx5_esw_vport_match_metadata_supported(const struct mlx5_eswitch *esw)
 {
 	if (!MLX5_CAP_ESW(esw->dev, esw_uplink_ingress_acl))
 		return false;
@@ -2449,6 +2448,28 @@ static int esw_offloads_metadata_init(struct mlx5_eswitch *esw)
 
 metadata_err:
 	esw_offloads_metadata_uninit(esw);
+	return err;
+}
+
+int mlx5_esw_offloads_vport_metadata_set(struct mlx5_eswitch *esw, bool enable)
+{
+	int err = 0;
+
+	down_write(&esw->mode_lock);
+	if (esw->mode != MLX5_ESWITCH_NONE) {
+		err = -EBUSY;
+		goto done;
+	}
+	if (!mlx5_esw_vport_match_metadata_supported(esw)) {
+		err = -EOPNOTSUPP;
+		goto done;
+	}
+	if (enable)
+		esw->flags |= MLX5_ESWITCH_VPORT_MATCH_METADATA;
+	else
+		esw->flags &= ~MLX5_ESWITCH_VPORT_MATCH_METADATA;
+done:
+	up_write(&esw->mode_lock);
 	return err;
 }
 
@@ -2673,9 +2694,6 @@ int esw_offloads_enable(struct mlx5_eswitch *esw)
 	if (err)
 		goto err_metadata;
 
-	if (esw_check_vport_match_metadata_supported(esw))
-		esw->flags |= MLX5_ESWITCH_VPORT_MATCH_METADATA;
-
 	err = esw_offloads_metadata_init(esw);
 	if (err)
 		goto err_metadata;
@@ -2725,7 +2743,6 @@ err_pool:
 err_vport_metadata:
 	esw_offloads_metadata_uninit(esw);
 err_metadata:
-	esw->flags &= ~MLX5_ESWITCH_VPORT_MATCH_METADATA;
 	mlx5_rdma_disable_roce(esw->dev);
 	mutex_destroy(&esw->offloads.termtbl_mutex);
 	return err;
@@ -2761,7 +2778,6 @@ void esw_offloads_disable(struct mlx5_eswitch *esw)
 	esw_offloads_steering_cleanup(esw);
 	mapping_destroy(esw->offloads.reg_c0_obj_pool);
 	esw_offloads_metadata_uninit(esw);
-	esw->flags &= ~MLX5_ESWITCH_VPORT_MATCH_METADATA;
 	mlx5_rdma_disable_roce(esw->dev);
 	mutex_destroy(&esw->offloads.termtbl_mutex);
 	esw->offloads.encap = DEVLINK_ESWITCH_ENCAP_MODE_NONE;
