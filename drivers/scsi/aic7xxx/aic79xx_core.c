ec199a8df6989915bd2f099e868f09d6ea6b7f06
Vaibhav Gupta <vaibhavgupta40@gmail.com>
2020-11-02 22:17:08 +0530

    scsi: aic79xx: Use generic power management
    Drivers should do only device-specific jobs. But in general, drivers using
    legacy PCI PM framework for .suspend()/.resume() have to manage many PCI
    PM-related tasks themselves which can be done by PCI Core itself. This
    brings extra load on the driver and it directly calls PCI helper functions
    to handle them.
    
    Switch to the new generic framework by updating function signatures and
    define a "struct dev_pm_ops" variable to bind PM callbacks. Also, remove
    unnecessary calls to the PCI Helper functions along with the legacy
    .suspend & .resume bindings.
    
    Link: https://lore.kernel.org/r/20201102164730.324035-8-vaibhavgupta40@gmail.com
    Signed-off-by: Vaibhav Gupta <vaibhavgupta40@gmail.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/aic7xxx/aic79xx_core.c
+++ drivers/scsi/aic7xxx/aic79xx_core.c
@@ -7866,11 +7866,9 @@ ahd_pause_and_flushwork(struct ahd_softc *ahd)
 	ahd->flags &= ~AHD_ALL_INTERRUPTS;
 }
 
-#ifdef CONFIG_PM
-int
+int __maybe_unused
 ahd_suspend(struct ahd_softc *ahd)
 {
-
 	ahd_pause_and_flushwork(ahd);
 
 	if (LIST_FIRST(&ahd->pending_scbs) != NULL) {
@@ -7881,15 +7879,13 @@ ahd_suspend(struct ahd_softc *ahd)
 	return (0);
 }
 
-void
+void __maybe_unused
 ahd_resume(struct ahd_softc *ahd)
 {
-
 	ahd_reset(ahd, /*reinit*/TRUE);
 	ahd_intr_enable(ahd, TRUE); 
 	ahd_restart(ahd);
 }
-#endif
 
 /************************** Busy Target Table *********************************/
 /*
