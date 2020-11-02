eaf148359d009d70aec13ae3772675b25890822e
Vaibhav Gupta <vaibhavgupta40@gmail.com>
2020-11-02 22:17:16 +0530

    scsi: mpt3sas_scsih: Drop PCI Wakeup calls from .resume
    The driver calls pci_enable_wake(...., false) in scsih_resume(), and there
    is no corresponding pci_enable_wake(...., true) in scsih_suspend().  Either
    it should do enable-wake the device in .suspend() or should not invoke
    pci_enable_wake() at all.
    
    Concluding that this driver doesn't support enable-wake and PCI core calls
    pci_enable_wake(pci_dev, PCI_D0, false) during resume, drop it from
    scsih_resume().
    
    Link: https://lore.kernel.org/r/20201102164730.324035-16-vaibhavgupta40@gmail.com
    Signed-off-by: Vaibhav Gupta <vaibhavgupta40@gmail.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/mpt3sas/mpt3sas_scsih.c
+++ drivers/scsi/mpt3sas/mpt3sas_scsih.c
@@ -12104,7 +12104,6 @@ scsih_resume(struct pci_dev *pdev)
 		 pdev, pci_name(pdev), device_state);
 
 	pci_set_power_state(pdev, PCI_D0);
-	pci_enable_wake(pdev, PCI_D0, 0);
 	pci_restore_state(pdev);
 	ioc->pdev = pdev;
 	r = mpt3sas_base_map_resources(ioc);
