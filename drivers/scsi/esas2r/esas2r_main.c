bf7e38aa0ee1f06130fb3019f42d59b767da73a9
Lee Jones <lee.jones@linaro.org>
2020-11-02 14:23:55 +0000

    scsi: esas2r: esas2r_main: Demote non-conformant kernel-doc header
    Fixes the following W=1 kernel build warning(s):
    
     drivers/scsi/esas2r/esas2r_main.c:911: warning: Function parameter or member 'a' not described in 'esas2r_check_active_queue'
     drivers/scsi/esas2r/esas2r_main.c:911: warning: Function parameter or member 'abort_request' not described in 'esas2r_check_active_queue'
     drivers/scsi/esas2r/esas2r_main.c:911: warning: Function parameter or member 'cmd' not described in 'esas2r_check_active_queue'
     drivers/scsi/esas2r/esas2r_main.c:911: warning: Function parameter or member 'queue' not described in 'esas2r_check_active_queue'
    
    Link: https://lore.kernel.org/r/20201102142359.561122-16-lee.jones@linaro.org
    Cc: Bradley Grove <linuxdrivers@attotech.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/esas2r/esas2r_main.c
+++ drivers/scsi/esas2r/esas2r_main.c
@@ -894,15 +894,11 @@ static void complete_task_management_request(struct esas2r_adapter *a,
 	esas2r_free_request(a, rq);
 }
 
-/**
+/*
  * Searches the specified queue for the specified queue for the command
  * to abort.
  *
- * @param [in] a
- * @param [in] abort_request
- * @param [in] cmd
- * t
- * @return 0 on failure, 1 if command was not found, 2 if command was found
+ * Return 0 on failure, 1 if command was not found, 2 if command was found
  */
 static int esas2r_check_active_queue(struct esas2r_adapter *a,
 				     struct esas2r_request **abort_request,
