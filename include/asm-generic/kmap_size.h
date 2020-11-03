4f8b96cd47b06f1e3ec71c1a3216113efe8dbfb5
Thomas Gleixner <tglx@linutronix.de>
2020-11-03 10:27:17 +0100

    asm-generic: Provide kmap_size.h
    kmap_types.h is a misnomer because the old atomic MAP based array does not
    exist anymore and the whole indirection of architectures including
    kmap_types.h is inconinstent and does not allow to provide guard page
    debugging for this misfeature.
    
    Add a common header file which defines the mapping stack size for all
    architectures. Will be used when converting architectures over to a
    generic kmap_local/atomic implementation.
    
    The array size is chosen with the following constraints in mind:
    
        - The deepest nest level in one context is 3 according to code
          inspection.
    
        - The worst case nesting for the upcoming reemptible version would be:
    
          2 maps in task context and a fault inside
          2 maps in the fault handler
          3 maps in softirq
          2 maps in interrupt
    
    So a total of 16 is sufficient and probably overestimated.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Link: https://lore.kernel.org/r/20201103095857.078043987@linutronix.de

---
+++ include/asm-generic/kmap_size.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_GENERIC_KMAP_SIZE_H
+#define _ASM_GENERIC_KMAP_SIZE_H
+
+/* For debug this provides guard pages between the maps */
+#ifdef CONFIG_DEBUG_HIGHMEM
+# define KM_MAX_IDX	33
+#else
+# define KM_MAX_IDX	16
+#endif
+
+#endif
