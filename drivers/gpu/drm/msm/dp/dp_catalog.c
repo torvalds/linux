231a04fcc6cb5b0e5f72c015d36462a17355f925
Kuogee Hsieh <khsieh@codeaurora.org>
2020-11-03 12:49:00 -0800

    drm/msm/dp: deinitialize mainlink if link training failed
    DP compo phy have to be enable to start link training. When
    link training failed phy need to be disabled so that next
    link traning can be proceed smoothly at next plug in. This
    patch de-initialize mainlink to disable phy if link training
    failed. This prevent system crash due to
    disp_cc_mdss_dp_link_intf_clk stuck at "off" state.  This patch
    also perform checking power_on flag at dp_display_enable() and
    dp_display_disable() to avoid crashing when unplug cable while
    display is off.
    
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/dp/dp_catalog.c
+++ drivers/gpu/drm/msm/dp/dp_catalog.c
@@ -572,7 +572,7 @@ void dp_catalog_ctrl_hpd_config(struct dp_catalog *dp_catalog)
 	dp_write_aux(catalog, REG_DP_DP_HPD_CTRL, DP_DP_HPD_CTRL_HPD_EN);
 }
 
-u32 dp_catalog_hpd_get_state_status(struct dp_catalog *dp_catalog)
+u32 dp_catalog_link_is_connected(struct dp_catalog *dp_catalog)
 {
 	struct dp_catalog_private *catalog = container_of(dp_catalog,
 				struct dp_catalog_private, dp_catalog);
