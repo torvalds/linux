d981059e13ffa9ed03a73472e932d070323bd057
Dexuan Cui <decui@microsoft.com>
2020-11-02 17:11:36 -0800

    x86/hyperv: Enable 15-bit APIC ID if the hypervisor supports it
    When a Linux VM runs on Hyper-V, if the VM has CPUs with >255 APIC IDs,
    the CPUs can't be the destination of IOAPIC interrupts, because the
    IOAPIC RTE's Dest Field has only 8 bits. Currently the hackery driver
    drivers/iommu/hyperv-iommu.c is used to ensure IOAPIC interrupts are
    only routed to CPUs that don't have >255 APIC IDs. However, there is
    an issue with kdump, because the kdump kernel can run on any CPU, and
    hence IOAPIC interrupts can't work if the kdump kernel run on a CPU
    with a >255 APIC ID.
    
    The kdump issue can be fixed by the Extended Dest ID, which is introduced
    recently by David Woodhouse (for IOAPIC, see the field virt_destid_8_14 in
    struct IO_APIC_route_entry). Of course, the Extended Dest ID needs the
    support of the underlying hypervisor. The latest Hyper-V has added the
    support recently: with this commit, on such a Hyper-V host, Linux VM
    does not use hyperv-iommu.c because hyperv_prepare_irq_remapping()
    returns -ENODEV; instead, Linux kernel's generic support of Extended Dest
    ID from David is used, meaning that Linux VM is able to support up to
    32K CPUs, and IOAPIC interrupts can be routed to all the CPUs.
    
    On an old Hyper-V host that doesn't support the Extended Dest ID, nothing
    changes with this commit: Linux VM is still able to bring up the CPUs with
    > 255 APIC IDs with the help of hyperv-iommu.c, but IOAPIC interrupts still
    can not go to such CPUs, and the kdump kernel still can not work properly
    on such CPUs.
    
    [ tglx: Updated comment as suggested by David ]
    
    Signed-off-by: Dexuan Cui <decui@microsoft.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: David Woodhouse <dwmw@amazon.co.uk>
    Link: https://lore.kernel.org/r/20201103011136.59108-1-decui@microsoft.com

--- arch/x86/kernel/cpu/mshyperv.c
+++ arch/x86/kernel/cpu/mshyperv.c
@@ -366,9 +366,38 @@ static void __init ms_hyperv_init_platform(void)
 #endif
 }
 
+static bool __init ms_hyperv_x2apic_available(void)
+{
+	return x2apic_supported();
+}
+
+/*
+ * If ms_hyperv_msi_ext_dest_id() returns true, hyperv_prepare_irq_remapping()
+ * returns -ENODEV and the Hyper-V IOMMU driver is not used; instead, the
+ * generic support of the 15-bit APIC ID is used: see __irq_msi_compose_msg().
+ *
+ * Note: for a VM on Hyper-V, the I/O-APIC is the only device which
+ * (logically) generates MSIs directly to the system APIC irq domain.
+ * There is no HPET, and PCI MSI/MSI-X interrupts are remapped by the
+ * pci-hyperv host bridge.
+ */
+static bool __init ms_hyperv_msi_ext_dest_id(void)
+{
+	u32 eax;
+
+	eax = cpuid_eax(HYPERV_CPUID_VIRT_STACK_INTERFACE);
+	if (eax != HYPERV_VS_INTERFACE_EAX_SIGNATURE)
+		return false;
+
+	eax = cpuid_eax(HYPERV_CPUID_VIRT_STACK_PROPERTIES);
+	return eax & HYPERV_VS_PROPERTIES_EAX_EXTENDED_IOAPIC_RTE;
+}
+
 const __initconst struct hypervisor_x86 x86_hyper_ms_hyperv = {
 	.name			= "Microsoft Hyper-V",
 	.detect			= ms_hyperv_platform,
 	.type			= X86_HYPER_MS_HYPERV,
+	.init.x2apic_available	= ms_hyperv_x2apic_available,
+	.init.msi_ext_dest_id	= ms_hyperv_msi_ext_dest_id,
 	.init.init_platform	= ms_hyperv_init_platform,
 };
