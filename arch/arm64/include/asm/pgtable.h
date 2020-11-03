8d6acfe80da165cfc9a6fadee7d73f8fd67a43ea
Mark Rutland <mark.rutland@arm.com>
2020-11-03 10:22:29 +0000

    arm64: consistently use reserved_pg_dir
    [ Upstream commit 833be850f1cabd0e3b5337c0fcab20a6e936dd48 ]
    
    Depending on configuration options and specific code paths, we either
    use the empty_zero_page or the configuration-dependent reserved_ttbr0
    as a reserved value for TTBR{0,1}_EL1.
    
    To simplify this code, let's always allocate and use the same
    reserved_pg_dir, replacing reserved_ttbr0. Note that this is allocated
    (and hence pre-zeroed), and is also marked as read-only in the kernel
    Image mapping.
    
    Keeping this separate from the empty_zero_page potentially helps with
    robustness as the empty_zero_page is used in a number of cases where a
    failure to map it read-only could allow it to become corrupted.
    
    The (presently unused) swapper_pg_end symbol is also removed, and
    comments are added wherever we rely on the offsets between the
    pre-allocated pg_dirs to keep these cases easily identifiable.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20201103102229.8542-1-mark.rutland@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -530,6 +530,7 @@ extern pgd_t swapper_pg_dir[PTRS_PER_PGD];
 extern pgd_t idmap_pg_dir[PTRS_PER_PGD];
 extern pgd_t idmap_pg_end[];
 extern pgd_t tramp_pg_dir[PTRS_PER_PGD];
+extern pgd_t reserved_pg_dir[PTRS_PER_PGD];
 
 extern void set_swapper_pgd(pgd_t *pgdp, pgd_t pgd);
 
