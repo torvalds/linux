fb5103f9d6ce197b4d0b67b4e60e68470f5293d1
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-11-04 01:22:23 +0800

    regmap/SoundWire: sdw: add support for SoundWire 1.2 MBQ
    The SoundWire 1.1 specification only allowed for reads and writes of
    bytes. The SoundWire 1.2 specification adds a new capability to
    transfer "Multi-Byte Quantities" (MBQ) across the bus. The transfers
    still happens one-byte-at-a-time, but the update is atomic.
    
    For example when writing a 16-bit volume, the first byte transferred
    is only taken into account when the second byte is successfully
    transferred.
    
    The mechanism is symmetrical for read and writes:
    - On a read, the address of the last byte to be read is modified by
    setting the MBQ bit
    - On a write, the address of all but the last byte to be written are
    modified by setting the MBQ bit. The address for the last byte relies
    on the MBQ bit being cleared.
    
    The current definitions for MBQ-based controls in the SDCA draft
    standard are limited to 16 bits for volumes, so for now this is the
    only supported format. An update will be provided if and when support
    for 24-bit and 32-bit values is specified by the SDCA standard.
    
    One possible objection is that this code could have been handled with
    regmap-sdw.c. However this is a new spec addition not handled by every
    SoundWire 1.1 and non-SDCA device, so there's no reason to load code
    that will never be used.
    
    Also in practice it's extremely unlikely that CONFIG_REGMAP would not
    be selected with CONFIG_REGMAP_MBQ selected. However there's no
    functional dependency between the two modules so they can be selected
    separately.
    
    Reviewed-by: Rander Wang <rander.wang@linux.intel.com>
    Reviewed-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
    Reviewed-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Reviewed-by: Vinod Koul <vkoul@kernel.org>
    Link: https://lore.kernel.org/r/20201103172226.4278-3-yung-chuan.liao@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/linux/regmap.h
+++ include/linux/regmap.h
@@ -570,6 +570,10 @@ struct regmap *__regmap_init_sdw(struct sdw_slave *sdw,
 				 const struct regmap_config *config,
 				 struct lock_class_key *lock_key,
 				 const char *lock_name);
+struct regmap *__regmap_init_sdw_mbq(struct sdw_slave *sdw,
+				     const struct regmap_config *config,
+				     struct lock_class_key *lock_key,
+				     const char *lock_name);
 struct regmap *__regmap_init_spi_avmm(struct spi_device *spi,
 				      const struct regmap_config *config,
 				      struct lock_class_key *lock_key,
@@ -619,6 +623,10 @@ struct regmap *__devm_regmap_init_sdw(struct sdw_slave *sdw,
 				 const struct regmap_config *config,
 				 struct lock_class_key *lock_key,
 				 const char *lock_name);
+struct regmap *__devm_regmap_init_sdw_mbq(struct sdw_slave *sdw,
+					  const struct regmap_config *config,
+					  struct lock_class_key *lock_key,
+					  const char *lock_name);
 struct regmap *__devm_regmap_init_slimbus(struct slim_device *slimbus,
 				 const struct regmap_config *config,
 				 struct lock_class_key *lock_key,
@@ -818,6 +826,19 @@ bool regmap_ac97_default_volatile(struct device *dev, unsigned int reg);
 				sdw, config)
 
 /**
+ * regmap_init_sdw_mbq() - Initialise register map
+ *
+ * @sdw: Device that will be interacted with
+ * @config: Configuration for register map
+ *
+ * The return value will be an ERR_PTR() on error or a valid pointer to
+ * a struct regmap.
+ */
+#define regmap_init_sdw_mbq(sdw, config)					\
+	__regmap_lockdep_wrapper(__regmap_init_sdw_mbq, #config,		\
+				sdw, config)
+
+/**
  * regmap_init_spi_avmm() - Initialize register map for Intel SPI Slave
  * to AVMM Bus Bridge
  *
@@ -987,6 +1008,20 @@ bool regmap_ac97_default_volatile(struct device *dev, unsigned int reg);
  */
 #define devm_regmap_init_sdw(sdw, config)				\
 	__regmap_lockdep_wrapper(__devm_regmap_init_sdw, #config,	\
+				sdw, config)
+
+/**
+ * devm_regmap_init_sdw_mbq() - Initialise managed register map
+ *
+ * @sdw: Device that will be interacted with
+ * @config: Configuration for register map
+ *
+ * The return value will be an ERR_PTR() on error or a valid pointer
+ * to a struct regmap. The regmap will be automatically freed by the
+ * device management code.
+ */
+#define devm_regmap_init_sdw_mbq(sdw, config)			\
+	__regmap_lockdep_wrapper(__devm_regmap_init_sdw_mbq, #config,   \
 				sdw, config)
 
 /**
