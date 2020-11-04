e0d80b647c12d5deb163365005742739745960cf
Lee Jones <lee.jones@linaro.org>
2020-11-04 14:51:52 -0800

    Input: cros_ec_keyb - struct headers should start with 'struct <name>'
    Fixes the following W=1 kernel build warning(s):
    
     drivers/input/keyboard/cros_ec_keyb.c:72: warning: cannot understand function prototype: 'struct cros_ec_bs_map '
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Reviewed-by: Benson Leung <bleung@chromium.org>
    Link: https://lore.kernel.org/r/20201104162427.2984742-8-lee.jones@linaro.org
    [dtor: fixed up docbook comments for cros_ec_keyb structure as well]
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/keyboard/cros_ec_keyb.c
+++ drivers/input/keyboard/cros_ec_keyb.c
@@ -27,7 +27,9 @@
 
 #include <asm/unaligned.h>
 
-/*
+/**
+ * struct cros_ec_keyb - Structure representing EC keyboard device
+ *
  * @rows: Number of rows in the keypad
  * @cols: Number of columns in the keypad
  * @row_shift: log2 or number of rows, rounded up
@@ -58,10 +60,9 @@ struct cros_ec_keyb {
 	struct notifier_block notifier;
 };
 
-
 /**
- * cros_ec_bs_map - Struct mapping Linux keycodes to EC button/switch bitmap
- * #defines
+ * struct cros_ec_bs_map - Mapping between Linux keycodes and EC button/switch
+ *	bitmap #defines
  *
  * @ev_type: The type of the input event to generate (e.g., EV_KEY).
  * @code: A linux keycode
