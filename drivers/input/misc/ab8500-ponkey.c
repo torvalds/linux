d8c58078e8aad8378fc4d0d112ed19c34ad9fca9
Lee Jones <lee.jones@linaro.org>
2020-11-04 11:12:31 -0800

    Input: ab8500-ponkey - fix incorrect name in 'ab8500_ponkey' doc header
    Fixes the following W=1 kernel build warning(s):
    
     drivers/input/misc/ab8500-ponkey.c:32: warning: Function parameter or member 'idev' not described in 'ab8500_ponkey'
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Link: https://lore.kernel.org/r/20201104162427.2984742-5-lee.jones@linaro.org
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/misc/ab8500-ponkey.c
+++ drivers/input/misc/ab8500-ponkey.c
@@ -19,7 +19,7 @@
 
 /**
  * struct ab8500_ponkey - ab8500 ponkey information
- * @input_dev: pointer to input device
+ * @idev: pointer to input device
  * @ab8500: ab8500 parent
  * @irq_dbf: irq number for falling transition
  * @irq_dbr: irq number for rising transition
