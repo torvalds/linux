0eba770790426553f45b8643bcd77b854e045057
Michael Walle <michael@walle.cc>
2020-11-05 20:27:45 +0100

    clk: composite: add devm_clk_hw_register_composite_pdata()
    This will simplify drivers which would only unregister the clk in their
    remove() op.
    
    Signed-off-by: Michael Walle <michael@walle.cc>
    Link: https://lore.kernel.org/r/20201105192746.19564-3-michael@walle.cc
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- include/linux/clk-provider.h
+++ include/linux/clk-provider.h
@@ -1062,6 +1062,13 @@ struct clk_hw *clk_hw_register_composite_pdata(struct device *dev,
 		struct clk_hw *rate_hw, const struct clk_ops *rate_ops,
 		struct clk_hw *gate_hw, const struct clk_ops *gate_ops,
 		unsigned long flags);
+struct clk_hw *devm_clk_hw_register_composite_pdata(struct device *dev,
+		const char *name, const struct clk_parent_data *parent_data,
+		int num_parents,
+		struct clk_hw *mux_hw, const struct clk_ops *mux_ops,
+		struct clk_hw *rate_hw, const struct clk_ops *rate_ops,
+		struct clk_hw *gate_hw, const struct clk_ops *gate_ops,
+		unsigned long flags);
 void clk_hw_unregister_composite(struct clk_hw *hw);
 
 struct clk *clk_register(struct device *dev, struct clk_hw *hw);
