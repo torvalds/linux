1d094cefc37e5ed4dec44a41841c8628f6b548a2
Marco Elver <elver@google.com>
2020-11-06 10:34:56 +0100

    kcsan: Fix encoding masks and regain address bit
    The watchpoint encoding masks for size and address were off-by-one bit
    each, with the size mask using 1 unnecessary bit and the address mask
    missing 1 bit. However, due to the way the size is shifted into the
    encoded watchpoint, we were effectively wasting and never using the
    extra bit.
    
    For example, on x86 with PAGE_SIZE==4K, we have 1 bit for the is-write
    bit, 14 bits for the size bits, and then 49 bits left for the address.
    Prior to this fix we would end up with this usage:
    
    	[ write<1> | size<14> | wasted<1> | address<48> ]
    
    Fix it by subtracting 1 bit from the GENMASK() end and start ranges of
    size and address respectively. The added static_assert()s verify that
    the masks are as expected. With the fixed version, we get the expected
    usage:
    
    	[ write<1> | size<14> |             address<49> ]
    
    Functionally no change is expected, since that extra address bit is
    insignificant for enabled architectures.
    
    Acked-by: Boqun Feng <boqun.feng@gmail.com>
    Signed-off-by: Marco Elver <elver@google.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/kcsan/encoding.h
+++ kernel/kcsan/encoding.h
@@ -37,14 +37,12 @@
  */
 #define WATCHPOINT_ADDR_BITS (BITS_PER_LONG-1 - WATCHPOINT_SIZE_BITS)
 
-/*
- * Masks to set/retrieve the encoded data.
- */
-#define WATCHPOINT_WRITE_MASK BIT(BITS_PER_LONG-1)
-#define WATCHPOINT_SIZE_MASK                                                   \
-	GENMASK(BITS_PER_LONG-2, BITS_PER_LONG-2 - WATCHPOINT_SIZE_BITS)
-#define WATCHPOINT_ADDR_MASK                                                   \
-	GENMASK(BITS_PER_LONG-3 - WATCHPOINT_SIZE_BITS, 0)
+/* Bitmasks for the encoded watchpoint access information. */
+#define WATCHPOINT_WRITE_MASK	BIT(BITS_PER_LONG-1)
+#define WATCHPOINT_SIZE_MASK	GENMASK(BITS_PER_LONG-2, WATCHPOINT_ADDR_BITS)
+#define WATCHPOINT_ADDR_MASK	GENMASK(WATCHPOINT_ADDR_BITS-1, 0)
+static_assert(WATCHPOINT_ADDR_MASK == (1UL << WATCHPOINT_ADDR_BITS) - 1);
+static_assert((WATCHPOINT_WRITE_MASK ^ WATCHPOINT_SIZE_MASK ^ WATCHPOINT_ADDR_MASK) == ~0UL);
 
 static inline bool check_encodable(unsigned long addr, size_t size)
 {
