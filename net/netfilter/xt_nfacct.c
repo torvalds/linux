0ef083d51ba1f4a2e890807d49d53ad20eee0ffd
Kaixu Xia <kaixuxia@tencent.com>
2020-11-06 16:20:13 +0800

    netfilter: Remove unnecessary conversion to bool
    Here we could use the '!=' expression to fix the following coccicheck
    warning:
    
    ./net/netfilter/xt_nfacct.c:30:41-46: WARNING: conversion to bool not needed here
    
    Reported-by: Tosk Robot <tencent_os_robot@tencent.com>
    Signed-off-by: Kaixu Xia <kaixuxia@tencent.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/xt_nfacct.c
+++ net/netfilter/xt_nfacct.c
@@ -27,7 +27,7 @@ static bool nfacct_mt(const struct sk_buff *skb, struct xt_action_param *par)
 
 	overquota = nfnl_acct_overquota(xt_net(par), info->nfacct);
 
-	return overquota == NFACCT_UNDERQUOTA ? false : true;
+	return overquota != NFACCT_UNDERQUOTA;
 }
 
 static int
