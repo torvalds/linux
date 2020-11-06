38e86f5c2645f3c16f698fa7e66b4eb23da5369c
John Stultz <john.stultz@linaro.org>
2020-11-06 04:27:09 +0000

    pinctrl: qcom: Allow pinctrl-msm code to be loadable as a module
    Tweaks to allow pinctrl-msm code to be loadable as a module.
    
    This is needed in order to support having the qcom-scm driver,
    which pinctrl-msm calls into, configured as a module.
    
    This requires that we tweak Kconfigs selecting PINCTRL_MSM to
    also depend on QCOM_SCM || QCOM_SCM=n so that we match the
    module setting of QCOM_SCM.
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Andy Gross <agross@kernel.org>
    Cc: Bjorn Andersson <bjorn.andersson@linaro.org>
    Cc: Joerg Roedel <joro@8bytes.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Jason Cooper <jason@lakedaemon.net>
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Linus Walleij <linus.walleij@linaro.org>
    Cc: Vinod Koul <vkoul@kernel.org>
    Cc: Kalle Valo <kvalo@codeaurora.org>
    Cc: Maulik Shah <mkshah@codeaurora.org>
    Cc: Lina Iyer <ilina@codeaurora.org>
    Cc: Saravana Kannan <saravanak@google.com>
    Cc: Todd Kjos <tkjos@google.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: linux-arm-msm@vger.kernel.org
    Cc: iommu@lists.linux-foundation.org
    Cc: linux-gpio@vger.kernel.org
    Link: https://lore.kernel.org/r/20201106042710.55979-2-john.stultz@linaro.org
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -1443,3 +1443,5 @@ int msm_pinctrl_remove(struct platform_device *pdev)
 }
 EXPORT_SYMBOL(msm_pinctrl_remove);
 
+MODULE_DESCRIPTION("Qualcomm Technologies, Inc. TLMM driver");
+MODULE_LICENSE("GPL v2");
