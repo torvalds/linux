b2896458b850ec7cb69b054b195b4b399f7e1f22
Jiri Slaby <jslaby@suse.cz>
2020-11-09 10:36:53 +0100

    x86/platform/uv: Drop last traces of uv_flush_tlb_others
    Commit 39297dde7390 ("x86/platform/uv: Remove UV BAU TLB Shootdown
    Handler") removed uv_flush_tlb_others. Its declaration was removed also
    from asm/uv/uv.h. But only for the CONFIG_X86_UV=y case. The inline
    definition (!X86_UV case) is still in place.
    
    So remove this implementation with everything what was added to support
    uv_flush_tlb_others:
    * include of asm/tlbflush.h
    * forward declarations of struct cpumask, mm_struct, and flush_tlb_info
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Mike Travis <mike.travis@hpe.com>
    Acked-by: Steve Wahl <steve.wahl@hpe.com>
    Link: https://lore.kernel.org/r/20201109093653.2042-1-jslaby@suse.cz

--- arch/x86/include/asm/uv/uv.h
+++ arch/x86/include/asm/uv/uv.h
@@ -2,13 +2,7 @@
 #ifndef _ASM_X86_UV_UV_H
 #define _ASM_X86_UV_UV_H
 
-#include <asm/tlbflush.h>
-
 enum uv_system_type {UV_NONE, UV_LEGACY_APIC, UV_X2APIC};
-
-struct cpumask;
-struct mm_struct;
-struct flush_tlb_info;
 
 #ifdef CONFIG_X86_UV
 #include <linux/efi.h>
@@ -44,10 +38,6 @@ static inline int is_uv_system(void)	{ return 0; }
 static inline int is_uv_hubbed(int uv)	{ return 0; }
 static inline void uv_cpu_init(void)	{ }
 static inline void uv_system_init(void)	{ }
-static inline const struct cpumask *
-uv_flush_tlb_others(const struct cpumask *cpumask,
-		    const struct flush_tlb_info *info)
-{ return cpumask; }
 
 #endif	/* X86_UV */
 
