8e0559921f9afc01fe0457c5e136ce4a7ae8f0d3
Bhaumik Bhatt <bbhatt@codeaurora.org>
2020-11-09 12:47:22 -0800

    bus: mhi: core: Skip device wake in error or shutdown states
    MHI client drivers can request a device wake even if the device
    may be in an error state or undergoing a shutdown. To prevent
    unnecessary device wake processing, check for the device state
    and bail out early so that the clients are made aware of the
    device state sooner.
    
    Signed-off-by: Bhaumik Bhatt <bbhatt@codeaurora.org>
    Reviewed-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

--- drivers/bus/mhi/core/pm.c
+++ drivers/bus/mhi/core/pm.c
@@ -827,6 +827,10 @@ int __mhi_device_get_sync(struct mhi_controller *mhi_cntrl)
 
 	/* Wake up the device */
 	read_lock_bh(&mhi_cntrl->pm_lock);
+	if (MHI_PM_IN_ERROR_STATE(mhi_cntrl->pm_state)) {
+		read_unlock_bh(&mhi_cntrl->pm_lock);
+		return -EIO;
+	}
 	mhi_cntrl->wake_get(mhi_cntrl, true);
 	if (MHI_PM_IN_SUSPEND_STATE(mhi_cntrl->pm_state))
 		mhi_trigger_resume(mhi_cntrl);
