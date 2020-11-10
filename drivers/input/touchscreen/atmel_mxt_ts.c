feedaacdadfc332e1a6e436f3adfbc67e244db47
Linus Walleij <linus.walleij@linaro.org>
2020-11-09 18:00:06 -0800

    Input: atmel_mxt_ts - fix up inverted RESET handler
    This driver uses GPIO descriptors to drive the touchscreen RESET line. In
    the existing device trees this has in conflict with intution been flagged
    as GPIO_ACTIVE_HIGH and the driver then applies the reverse action by
    driving the line low (setting to 0) to enter reset state and driving the
    line high (setting to 1) to get out of reset state.
    
    The correct way to handle active low GPIO lines is to provide the
    GPIO_ACTIVE_LOW in the device tree (thus properly describing the hardware)
    and letting the GPIO framework invert the assertion (driving high) to a
    low level and vice versa.
    
    This is considered a bug since the device trees are incorrectly
    mis-specifying the line as active high.
    
    Fix the driver and all device trees specifying a reset line.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Reviewed-by: Philippe Schenker <philippe.schenker@toradex.com>
    Acked-by: Krzysztof Kozlowski <krzk@kernel.org>
    Link: https://lore.kernel.org/r/20201104153032.1387747-1-linus.walleij@linaro.org
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -3134,8 +3134,9 @@ static int mxt_probe(struct i2c_client *client, const struct i2c_device_id *id)
 	if (error)
 		return error;
 
+	/* Request the RESET line as asserted so we go into reset */
 	data->reset_gpio = devm_gpiod_get_optional(&client->dev,
-						   "reset", GPIOD_OUT_LOW);
+						   "reset", GPIOD_OUT_HIGH);
 	if (IS_ERR(data->reset_gpio)) {
 		error = PTR_ERR(data->reset_gpio);
 		dev_err(&client->dev, "Failed to get reset gpio: %d\n", error);
@@ -3153,8 +3154,9 @@ static int mxt_probe(struct i2c_client *client, const struct i2c_device_id *id)
 	disable_irq(client->irq);
 
 	if (data->reset_gpio) {
+		/* Wait a while and then de-assert the RESET GPIO line */
 		msleep(MXT_RESET_GPIO_TIME);
-		gpiod_set_value(data->reset_gpio, 1);
+		gpiod_set_value(data->reset_gpio, 0);
 		msleep(MXT_RESET_INVALID_CHG);
 	}
 
