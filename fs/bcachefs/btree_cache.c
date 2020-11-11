e648448ca562af0cb11729ce6fad0f860fa42cb4
Kent Overstreet <kent.overstreet@gmail.com>
2020-11-11 18:59:41 -0500

    bcachefs: Fix missing memalloc_nofs_restore()
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/btree_cache.c
+++ fs/bcachefs/btree_cache.c
@@ -329,9 +329,9 @@ restart:
 			clear_btree_node_accessed(b);
 	}
 
-	memalloc_nofs_restore(flags);
 	mutex_unlock(&bc->lock);
 out:
+	memalloc_nofs_restore(flags);
 	return (unsigned long) freed * btree_pages(c);
 }
 
