50c0133cd154090446bc19e466df57502f422644
Jon Hunter <jonathanh@nvidia.com>
2020-11-11 10:37:08 +0000

    phy: tegra: Don't warn on probe deferral
    Deferred probe is an expected return value for devm_regulator_bulk_get().
    Given that the driver deals with it properly, there's no need to output a
    warning that may potentially confuse users.
    
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Acked-by: Thierry Reding <treding@nvidia.com>
    Acked-by: JC Kuo <jckuo@nvidia.com>
    Link: https://lore.kernel.org/r/20201111103708.152566-1-jonathanh@nvidia.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/phy/tegra/xusb.c
+++ drivers/phy/tegra/xusb.c
@@ -1198,7 +1198,7 @@ static int tegra_xusb_padctl_probe(struct platform_device *pdev)
 	err = devm_regulator_bulk_get(&pdev->dev, padctl->soc->num_supplies,
 				      padctl->supplies);
 	if (err < 0) {
-		dev_err(&pdev->dev, "failed to get regulators: %d\n", err);
+		dev_err_probe(&pdev->dev, err, "failed to get regulators\n");
 		goto remove;
 	}
 
