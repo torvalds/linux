ff4e5fbad06f762b8551da56e8fd64ad14c8aa3e
Niklas Schnelle <schnelle@linux.ibm.com>
2020-11-12 09:23:01 +0100

    nvme-pci: drop min() from nr_io_queues assignment
    in nvme_setup_io_queues() the number of I/O queues is set to either 1 in
    case of a quirky Apple device or to the min of nvme_max_io_queues() or
    dev->nr_allocated_queues - 1.
    This is unnecessarily complicated as dev->nr_allocated_queues is only
    assigned once and is nvme_max_io_queues() + 1.
    
    Signed-off-by: Niklas Schnelle <schnelle@linux.ibm.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -2113,8 +2113,7 @@ static int nvme_setup_io_queues(struct nvme_dev *dev)
 	if (dev->ctrl.quirks & NVME_QUIRK_SHARED_TAGS)
 		nr_io_queues = 1;
 	else
-		nr_io_queues = min(nvme_max_io_queues(dev),
-				   dev->nr_allocated_queues - 1);
+		nr_io_queues = dev->nr_allocated_queues - 1;
 
 	result = nvme_set_queue_count(&dev->ctrl, &nr_io_queues);
 	if (result < 0)
