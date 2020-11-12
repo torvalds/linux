e5decb2eb5f4d1f64ba9196b4bad0e26a441c81c
Chuck Lever <chuck.lever@oracle.com>
2020-11-12 14:47:14 -0500

    svcrdma: Catch another Reply chunk overflow case
    When space in the Reply chunk runs out in the middle of a segment,
    we end up passing a zero-length SGL to rdma_rw_ctx_init(), and it
    oopses.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- net/sunrpc/xprtrdma/svc_rdma_rw.c
+++ net/sunrpc/xprtrdma/svc_rdma_rw.c
@@ -460,6 +460,8 @@ svc_rdma_build_writes(struct svc_rdma_write_info *info,
 		offset += info->wi_seg_off;
 
 		write_len = min(remaining, length - info->wi_seg_off);
+		if (!write_len)
+			goto out_overflow;
 		ctxt = svc_rdma_get_rw_ctxt(rdma,
 					    (write_len >> PAGE_SHIFT) + 2);
 		if (!ctxt)
