fe3f1d5d7cd3062c0cb8fe70dd77470019dedd19
Thomas Gleixner <tglx@linutronix.de>
2020-11-13 15:02:09 +0100

    sh: Get rid of nmi_count()
    nmi_count() is a historical leftover and SH is the only user. Replace it
    with regular per cpu accessors.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Frederic Weisbecker <frederic@kernel.org>
    Link: https://lore.kernel.org/r/20201113141732.844232404@linutronix.de

--- arch/sh/kernel/irq.c
+++ arch/sh/kernel/irq.c
@@ -44,7 +44,7 @@ int arch_show_interrupts(struct seq_file *p, int prec)
 
 	seq_printf(p, "%*s: ", prec, "NMI");
 	for_each_online_cpu(j)
-		seq_printf(p, "%10u ", nmi_count(j));
+		seq_printf(p, "%10u ", per_cpu(irq_stat.__nmi_count, j);
 	seq_printf(p, "  Non-maskable interrupts\n");
 
 	seq_printf(p, "%*s: %10u\n", prec, "ERR", atomic_read(&irq_err_count));
