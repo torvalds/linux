e78acf7efebff9184ad4add02b62a1f486a8cde8
Andres Freund <andres@anarazel.de>
2020-11-13 12:49:16 -0800

    thermal: intel_pch_thermal: Add PCI ids for Lewisburg PCH.
    I noticed that I couldn't read the PCH temperature on my workstation
    (C620 series chipset, w/ 2x Xeon Gold 5215 CPUs) directly, but had to go
    through IPMI. Looking at the data sheet, it looks to me like the
    existing intel PCH thermal driver should work without changes for
    Lewisburg.
    
    I suspect there's some other PCI IDs missing. But I hope somebody at
    Intel would have an easier time figuring that out than I...
    
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Cc: Tushar Dave <tushar.n.dave@intel.com>
    Cc: Zhang Rui <rui.zhang@intel.com>
    Cc: linux-pm@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Link: https://lore.kernel.org/lkml/20200115184415.1726953-1-andres@anarazel.de/
    Signed-off-by: Andres Freund <andres@anarazel.de>
    Reviewed-by: Pandruvada, Srinivas <srinivas.pandruvada@linux.intel.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/20201113204916.1144907-1-andres@anarazel.de

--- drivers/thermal/intel/intel_pch_thermal.c
+++ drivers/thermal/intel/intel_pch_thermal.c
@@ -28,6 +28,7 @@
 #define PCH_THERMAL_DID_CNL_H	0xA379 /* CNL-H PCH */
 #define PCH_THERMAL_DID_CNL_LP	0x02F9 /* CNL-LP PCH */
 #define PCH_THERMAL_DID_CML_H	0X06F9 /* CML-H PCH */
+#define PCH_THERMAL_DID_LWB	0xA1B1 /* Lewisburg PCH */
 
 /* Wildcat Point-LP  PCH Thermal registers */
 #define WPT_TEMP	0x0000	/* Temperature */
@@ -340,6 +341,7 @@ enum board_ids {
 	board_skl,
 	board_cnl,
 	board_cml,
+	board_lwb,
 };
 
 static const struct board_info {
@@ -365,7 +367,11 @@ static const struct board_info {
 	[board_cml] = {
 		.name = "pch_cometlake",
 		.ops = &pch_dev_ops_wpt,
-	}
+	},
+	[board_lwb] = {
+		.name = "pch_lewisburg",
+		.ops = &pch_dev_ops_wpt,
+	},
 };
 
 static int intel_pch_thermal_probe(struct pci_dev *pdev,
@@ -479,6 +485,8 @@ static const struct pci_device_id intel_pch_thermal_id[] = {
 		.driver_data = board_cnl, },
 	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCH_THERMAL_DID_CML_H),
 		.driver_data = board_cml, },
+	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCH_THERMAL_DID_LWB),
+		.driver_data = board_lwb, },
 	{ 0, },
 };
 MODULE_DEVICE_TABLE(pci, intel_pch_thermal_id);
