9ca718743ad8402958637bfc196d7b62371a1b9f
Francis Laniel <laniel_francis@privacyrequired.com>
2020-11-15 18:08:05 +0100

    Modify return value of nla_strlcpy to match that of strscpy.
    nla_strlcpy now returns -E2BIG if src was truncated when written to dst.
    It also returns this error value if dstsize is 0 or higher than INT_MAX.
    
    For example, if src is "foo\0" and dst is 3 bytes long, the result will be:
    1. "foG" after memcpy (G means garbage).
    2. "fo\0" after memset.
    3. -E2BIG is returned because src was not completely written into dst.
    
    The callers of nla_strlcpy were modified to take into account this modification.
    
    Signed-off-by: Francis Laniel <laniel_francis@privacyrequired.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/net/netlink.h
+++ include/net/netlink.h
@@ -506,7 +506,7 @@ int __nla_parse(struct nlattr **tb, int maxtype, const struct nlattr *head,
 		struct netlink_ext_ack *extack);
 int nla_policy_len(const struct nla_policy *, int);
 struct nlattr *nla_find(const struct nlattr *head, int len, int attrtype);
-size_t nla_strlcpy(char *dst, const struct nlattr *nla, size_t dstsize);
+ssize_t nla_strlcpy(char *dst, const struct nlattr *nla, size_t dstsize);
 char *nla_strdup(const struct nlattr *nla, gfp_t flags);
 int nla_memcpy(void *dest, const struct nlattr *src, int count);
 int nla_memcmp(const struct nlattr *nla, const void *data, size_t size);
