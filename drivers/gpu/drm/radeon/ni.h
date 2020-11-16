6faf027f51472d72b6e47654f9333486709cd667
Lee Jones <lee.jones@linaro.org>
2020-11-16 17:36:42 +0000

    drm/radeon/ni_dma: Move 'cayman_gpu_check_soft_reset()'s prototype to shared header
    Fixes the following W=1 kernel build warning(s):
    
     drivers/gpu/drm/radeon/ni.c:1733:5: warning: no previous prototype for ‘cayman_gpu_check_soft_reset’ [-Wmissing-prototypes]
     1733 | u32 cayman_gpu_check_soft_reset(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: "Christian König" <christian.koenig@amd.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: amd-gfx@lists.freedesktop.org
    Cc: dri-devel@lists.freedesktop.org
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/ni.h
+++ drivers/gpu/drm/radeon/ni.h
@@ -32,5 +32,6 @@ void cayman_cp_int_cntl_setup(struct radeon_device *rdev,
 			      int ring, u32 cp_int_cntl);
 void cayman_vm_decode_fault(struct radeon_device *rdev,
 			    u32 status, u32 addr);
+u32 cayman_gpu_check_soft_reset(struct radeon_device *rdev);
 
 #endif				/* __NI_H__ */
