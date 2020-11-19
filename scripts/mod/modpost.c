d834db9f2cc16fad53c172058aa0c7bb3f3091ed
Will McVicker <willmcvicker@google.com>
2020-11-19 13:46:37 -0800

    ANDROID: modules: re-introduce the MODULE_SCMVERSION config
    Config MODULE_SCMVERSION introduces a new module attribute --
    `scmversion` -- which can be used to identify a given module's SCM
    version.  This is very useful for developers that update their kernel
    independently from their kernel modules or vice-versa since the SCM
    version provided by UTS_RELEASE (`uname -r`) will now differ from the
    module's vermagic attribute.
    
    For example, we have a CI setup that tests new kernel changes on the
    hikey960 and db845c devices without updating their kernel modules. When
    these tests fail, we need to be able to identify the exact device
    configuration the test was using. By including MODULE_SCMVERSION, we can
    identify the exact kernel and modules' SCM versions for debugging the
    failures.
    
    Additionally, by exposing the SCM version via the sysfs node
    /sys/module/MODULENAME/scmversion, one can also verify the SCM versions
    of the modules loaded from the initramfs. Currently, modinfo can only
    retrieve module attributes from the module's ko on disk and not from the
    actual module that is loaded in RAM.
    
    You can retrieve the SCM version in two ways,
    
    1) By using modinfo:
        > modinfo -F scmversion MODULENAME
    2) By module sysfs node:
        > cat /sys/module/MODULENAME/scmversion
    
    Bug: 180027765
    Link: https://lore.kernel.org/all/20210121213641.3477522-1-willmcvicker@google.com/
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Change-Id: Ib7c72c72f95c4545adb7cd4e842729557039ce3a

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -29,6 +29,8 @@ static bool modversions;
 static bool all_versions;
 /* If we are modposting external module set to 1 */
 static bool external_module;
+#define MODULE_SCMVERSION_SIZE 64
+static char module_scmversion[MODULE_SCMVERSION_SIZE];
 /* Only warn about unresolved symbols */
 static bool warn_unresolved;
 
@@ -1999,6 +2001,9 @@ static void add_header(struct buffer *b, struct module *mod)
 	if (!external_module)
 		buf_printf(b, "\nMODULE_INFO(intree, \"Y\");\n");
 
+	if (module_scmversion[0] != '\0')
+		buf_printf(b, "\nMODULE_INFO(scmversion, \"%s\");\n", module_scmversion);
+
 	buf_printf(b,
 		   "\n"
 		   "#ifdef CONFIG_RETPOLINE\n"
@@ -2327,7 +2332,7 @@ int main(int argc, char **argv)
 	LIST_HEAD(dump_lists);
 	struct dump_list *dl, *dl2;
 
-	while ((opt = getopt(argc, argv, "ei:mnT:o:awENd:")) != -1) {
+	while ((opt = getopt(argc, argv, "ei:mnT:o:awENd:v:")) != -1) {
 		switch (opt) {
 		case 'e':
 			external_module = true;
@@ -2363,6 +2368,9 @@ int main(int argc, char **argv)
 			break;
 		case 'd':
 			missing_namespace_deps = optarg;
+			break;
+		case 'v':
+			strncpy(module_scmversion, optarg, sizeof(module_scmversion) - 1);
 			break;
 		default:
 			exit(1);
