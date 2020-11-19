abc72b646066075acf9121a2a68aad39f550813d
Siddharth Gupta <sidgup@codeaurora.org>
2020-11-19 13:05:33 -0800

    remoteproc: coredump: Add minidump functionality
    This change adds a new kind of core dump mechanism which instead of dumping
    entire program segments of the firmware, dumps sections of the remoteproc
    memory which are sufficient to allow debugging the firmware. This function
    thus uses section headers instead of program headers during creation of the
    core dump elf.
    
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Co-developed-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>
    Signed-off-by: Siddharth Gupta <sidgup@codeaurora.org>
    Link: https://lore.kernel.org/r/1605819935-10726-3-git-send-email-sidgup@codeaurora.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- include/linux/remoteproc.h
+++ include/linux/remoteproc.h
@@ -657,6 +657,7 @@ int rproc_boot(struct rproc *rproc);
 void rproc_shutdown(struct rproc *rproc);
 int rproc_set_firmware(struct rproc *rproc, const char *fw_name);
 void rproc_report_crash(struct rproc *rproc, enum rproc_crash_type type);
+void rproc_coredump_using_sections(struct rproc *rproc);
 int rproc_coredump_add_segment(struct rproc *rproc, dma_addr_t da, size_t size);
 int rproc_coredump_add_custom_segment(struct rproc *rproc,
 				      dma_addr_t da, size_t size,
