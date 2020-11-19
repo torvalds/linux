c35a25795ed5cbcdf1ca93959ae8e5ec49822ace
Daniel Rosenberg <drosen@google.com>
2020-11-19 06:09:02 +0000

    libfs: Add generic function for setting dentry_ops
    This adds a function to set dentry operations at lookup time that will
    work for both encrypted filenames and casefolded filenames.
    
    A filesystem that supports both features simultaneously can use this
    function during lookup preparations to set up its dentry operations once
    fscrypt no longer does that itself.
    
    Currently the casefolding dentry operation are always set if the
    filesystem defines an encoding because the features is toggleable on
    empty directories. Unlike in the encryption case, the dentry operations
    used come from the parent. Since we don't know what set of functions
    we'll eventually need, and cannot change them later, we enable the
    casefolding operations if the filesystem supports them at all.
    
    By splitting out the various cases, we support as few dentry operations
    as we can get away with, maximizing compatibility with overlayfs, which
    will not function if a filesystem supports certain dentry_operations.
    
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Reviewed-by: Theodore Ts'o <tytso@mit.edu>
    Reviewed-by: Eric Biggers <ebiggers@google.com>
    Reviewed-by: Gabriel Krisman Bertazi <krisman@collabora.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -3202,6 +3202,7 @@ extern int generic_ci_d_hash(const struct dentry *dentry, struct qstr *str);
 extern int generic_ci_d_compare(const struct dentry *dentry, unsigned int len,
 				const char *str, const struct qstr *name);
 #endif
+extern void generic_set_encrypted_ci_d_ops(struct dentry *dentry);
 
 #ifdef CONFIG_MIGRATION
 extern int buffer_migrate_page(struct address_space *,
