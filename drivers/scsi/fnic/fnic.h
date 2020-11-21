74ae6d6a6805f9fe162e4527d8ff0f7d2f9bc0d0
Karan Tilak Kumar <kartilak@cisco.com>
2020-11-20 17:51:34 -0800

    scsi: fnic: Set scsi_set_resid() only for underflow
    Set scsi_set_resid() only if FCPIO_ICMND_CMPL_RESID_UNDER is set.
    
    Link: https://lore.kernel.org/r/20201121015134.18872-1-kartilak@cisco.com
    Reviewed-by: Arulprabhu Ponnusamy <arulponn@cisco.com>
    Co-developed-by: Satish Kharat <satishkh@cisco.com>
    Signed-off-by: Satish Kharat <satishkh@cisco.com>
    Signed-off-by: Karan Tilak Kumar <kartilak@cisco.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/fnic/fnic.h
+++ drivers/scsi/fnic/fnic.h
@@ -39,7 +39,7 @@
 
 #define DRV_NAME		"fnic"
 #define DRV_DESCRIPTION		"Cisco FCoE HBA Driver"
-#define DRV_VERSION		"1.6.0.51"
+#define DRV_VERSION		"1.6.0.52"
 #define PFX			DRV_NAME ": "
 #define DFX                     DRV_NAME "%d: "
 
--- drivers/scsi/fnic/fnic_scsi.c
+++ drivers/scsi/fnic/fnic_scsi.c
@@ -921,10 +921,11 @@ static void fnic_fcpio_icmnd_cmpl_handler(struct fnic *fnic,
 	case FCPIO_SUCCESS:
 		sc->result = (DID_OK << 16) | icmnd_cmpl->scsi_status;
 		xfer_len = scsi_bufflen(sc);
-		scsi_set_resid(sc, icmnd_cmpl->residual);
 
-		if (icmnd_cmpl->flags & FCPIO_ICMND_CMPL_RESID_UNDER)
+		if (icmnd_cmpl->flags & FCPIO_ICMND_CMPL_RESID_UNDER) {
 			xfer_len -= icmnd_cmpl->residual;
+			scsi_set_resid(sc, icmnd_cmpl->residual);
+		}
 
 		if (icmnd_cmpl->scsi_status == SAM_STAT_CHECK_CONDITION)
 			atomic64_inc(&fnic_stats->misc_stats.check_condition);
