70f16fab5272bfc5b83c24bc1a8e877697bf17cc
Bjorn Andersson <bjorn.andersson@linaro.org>
2020-11-24 12:57:43 -0600

    Revert "i2c: qcom-geni: Disable DMA processing on the Lenovo Yoga C630"
    A combination of recent bug fixes by Doug Anderson and the proper
    definition of iommu streams means that this hack is no longer needed.
    Let's clean up the code by reverting '127068abe85b ("i2c: qcom-geni:
    Disable DMA processing on the Lenovo Yoga C630")'.
    
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Acked-by: Shawn Guo <shawn.guo@linaro.org>
    Acked-by: Caleb Connolly <caleb@connolly.tech>
    Tested-by: Steev Klimaszewski <steev@kali.org>
    Reviewed-by: Akash Asthana <akashast@codeaurora.org>
    Signed-off-by: Wolfram Sang <wsa@kernel.org>

--- drivers/i2c/busses/i2c-qcom-geni.c
+++ drivers/i2c/busses/i2c-qcom-geni.c
@@ -353,13 +353,11 @@ static int geni_i2c_rx_one_msg(struct geni_i2c_dev *gi2c, struct i2c_msg *msg,
 {
 	dma_addr_t rx_dma;
 	unsigned long time_left;
-	void *dma_buf = NULL;
+	void *dma_buf;
 	struct geni_se *se = &gi2c->se;
 	size_t len = msg->len;
 
-	if (!of_machine_is_compatible("lenovo,yoga-c630"))
-		dma_buf = i2c_get_dma_safe_msg_buf(msg, 32);
-
+	dma_buf = i2c_get_dma_safe_msg_buf(msg, 32);
 	if (dma_buf)
 		geni_se_select_mode(se, GENI_SE_DMA);
 	else
@@ -394,13 +392,11 @@ static int geni_i2c_tx_one_msg(struct geni_i2c_dev *gi2c, struct i2c_msg *msg,
 {
 	dma_addr_t tx_dma;
 	unsigned long time_left;
-	void *dma_buf = NULL;
+	void *dma_buf;
 	struct geni_se *se = &gi2c->se;
 	size_t len = msg->len;
 
-	if (!of_machine_is_compatible("lenovo,yoga-c630"))
-		dma_buf = i2c_get_dma_safe_msg_buf(msg, 32);
-
+	dma_buf = i2c_get_dma_safe_msg_buf(msg, 32);
 	if (dma_buf)
 		geni_se_select_mode(se, GENI_SE_DMA);
 	else
