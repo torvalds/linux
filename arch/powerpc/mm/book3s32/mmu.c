035b19a15a98907916a42a6b1d025877c42f10ad
Christophe Leroy <christophe.leroy@csgroup.eu>
2020-11-25 07:10:46 +0000

    powerpc/32s: Always map kernel text and rodata with BATs
    Since commit 2b279c0348af ("powerpc/32s: Allow mapping with BATs with
    DEBUG_PAGEALLOC"), there is no real situation where mapping without
    BATs is required.
    
    In order to simplify memory handling, always map kernel text
    and rodata with BATs even when "nobats" kernel parameter is set.
    
    Also fix the 603 TLB miss exceptions that don't require anymore
    kernel page table if DEBUG_PAGEALLOC.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/da51f7ec632825a4ce43290a904aad61648408c0.1606285013.git.christophe.leroy@csgroup.eu

--- arch/powerpc/mm/book3s32/mmu.c
+++ arch/powerpc/mm/book3s32/mmu.c
@@ -157,11 +157,9 @@ unsigned long __init mmu_mapin_ram(unsigned long base, unsigned long top)
 	unsigned long done;
 	unsigned long border = (unsigned long)__init_begin - PAGE_OFFSET;
 
-	if (__map_without_bats) {
-		pr_debug("RAM mapped without BATs\n");
-		return base;
-	}
-	if (debug_pagealloc_enabled()) {
+
+	if (debug_pagealloc_enabled() || __map_without_bats) {
+		pr_debug_once("Read-Write memory mapped without BATs\n");
 		if (base >= border)
 			return base;
 		if (top >= border)
