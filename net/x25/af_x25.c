139d6eb149de0cb8726d0d8e722ab8d4ddcc60bb
Martin Schiller <ms@dev.tdt.de>
2020-11-26 07:35:57 +0100

    net/x25: remove x25_kill_by_device()
    Remove obsolete function x25_kill_by_device(). It's not used any more.
    
    Signed-off-by: Martin Schiller <ms@dev.tdt.de>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/x25/af_x25.c
+++ net/x25/af_x25.c
@@ -200,22 +200,6 @@ static void x25_remove_socket(struct sock *sk)
 }
 
 /*
- *	Kill all bound sockets on a dropped device.
- */
-static void x25_kill_by_device(struct net_device *dev)
-{
-	struct sock *s;
-
-	write_lock_bh(&x25_list_lock);
-
-	sk_for_each(s, &x25_list)
-		if (x25_sk(s)->neighbour && x25_sk(s)->neighbour->dev == dev)
-			x25_disconnect(s, ENETUNREACH, 0, 0);
-
-	write_unlock_bh(&x25_list_lock);
-}
-
-/*
  *	Handle device status changes.
  */
 static int x25_device_event(struct notifier_block *this, unsigned long event,
