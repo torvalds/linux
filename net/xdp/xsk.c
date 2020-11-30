e392081837283fbe5df1837fd85012ae5bfae098
Björn Töpel <bjorn.topel@intel.com>
2020-11-30 19:51:59 +0100

    xsk: Check need wakeup flag in sendmsg()
    Add a check for need wake up in sendmsg(), so that if a user calls
    sendmsg() when no wakeup is needed, do not trigger a wakeup.
    
    To simplify the need wakeup check in the syscall, unconditionally
    enable the need wakeup flag for Tx. This has a side-effect for poll();
    If poll() is called for a socket without enabled need wakeup, a Tx
    wakeup is unconditionally performed.
    
    The wakeup matrix for AF_XDP now looks like:
    
    need wakeup | poll()       | sendmsg()   | recvmsg()
    ------------+--------------+-------------+------------
    disabled    | wake Tx      | wake Tx     | nop
    enabled     | check flag;  | check flag; | check flag;
                |   wake Tx/Rx |   wake Tx   |   wake Rx
    
    Signed-off-by: Björn Töpel <bjorn.topel@intel.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Magnus Karlsson <magnus.karlsson@intel.com>
    Link: https://lore.kernel.org/bpf/20201130185205.196029-5-bjorn.topel@gmail.com

--- net/xdp/xsk.c
+++ net/xdp/xsk.c
@@ -522,13 +522,17 @@ static int xsk_sendmsg(struct socket *sock, struct msghdr *m, size_t total_len)
 	bool need_wait = !(m->msg_flags & MSG_DONTWAIT);
 	struct sock *sk = sock->sk;
 	struct xdp_sock *xs = xdp_sk(sk);
+	struct xsk_buff_pool *pool;
 
 	if (unlikely(!xsk_is_bound(xs)))
 		return -ENXIO;
 	if (unlikely(need_wait))
 		return -EOPNOTSUPP;
 
-	return __xsk_sendmsg(sk);
+	pool = xs->pool;
+	if (pool->cached_need_wakeup & XDP_WAKEUP_TX)
+		return __xsk_sendmsg(sk);
+	return 0;
 }
 
 static int xsk_recvmsg(struct socket *sock, struct msghdr *m, size_t len, int flags)
