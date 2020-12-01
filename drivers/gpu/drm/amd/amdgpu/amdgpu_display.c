5cd5f0070030e130912028f2bfdf64a1502dd73a
Roman Li <roman.li@amd.com>
2020-12-01 14:05:36 -0500

    drm/amd/display: add S/G support for Vangogh
    [Why]
    Scatter/gather feature is supported on Vangogh.
    
    [How]
    Add GTT domain support for Vangogh to enable
    display buffers in system memory.
    
    Signed-off-by: Roman Li <roman.li@amd.com>
    Reviewed-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
@@ -522,6 +522,7 @@ uint32_t amdgpu_display_supported_domains(struct amdgpu_device *adev,
 				domain |= AMDGPU_GEM_DOMAIN_GTT;
 			break;
 		case CHIP_RENOIR:
+		case CHIP_VANGOGH:
 			domain |= AMDGPU_GEM_DOMAIN_GTT;
 			break;
 
