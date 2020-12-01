fe8395168d844bef1551edcc6f6b96b2e8eff1d3
YueHaibing <yuehaibing@huawei.com>
2020-12-01 14:42:04 -0800

    net/mlx5e: Remove duplicated include
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_rx.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_rx.c
@@ -52,7 +52,6 @@
 #include "en/xsk/rx.h"
 #include "en/health.h"
 #include "en/params.h"
-#include "en/txrx.h"
 
 static struct sk_buff *
 mlx5e_skb_from_cqe_mpwrq_linear(struct mlx5e_rq *rq, struct mlx5e_mpw_info *wi,
