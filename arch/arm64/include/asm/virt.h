c1c1567f22af014eb5ac51029f105e1ab2b537a0
David Brazdil <dbrazdil@google.com>
2020-12-02 18:41:22 +0000

    UPSTREAM: KVM: arm64: Fix EL2 mode availability checks
    With protected nVHE hyp code interception host's PSCI SMCs, the host
    starts seeing new CPUs boot in EL1 instead of EL2. The kernel logic
    that keeps track of the boot mode needs to be adjusted.
    
    Add a static key enabled if KVM protected mode initialization is
    successful.
    
    When the key is enabled, is_hyp_mode_available continues to report
    `true` because its users either treat it as a check whether KVM will be
    / was initialized, or whether stub HVCs can be made (eg. hibernate).
    
    is_hyp_mode_mismatched is changed to report `false` when the key is
    enabled. That's because all cores' modes matched at the point of KVM
    init and KVM will not allow cores not present at init to boot. That
    said, the function is never used after KVM is initialized.
    
    Signed-off-by: David Brazdil <dbrazdil@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20201202184122.26046-27-dbrazdil@google.com
    (cherry picked from commit f19f6644a5433cfae8a068445b149bc2247c1445)
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Change-Id: I5c14d42952672b2756b731663b9406463af2b5dd
    Bug: 178098380
    Test: atest VirtualizationHostTestCases on an EL2-enabled device

--- arch/arm64/include/asm/virt.h
+++ arch/arm64/include/asm/virt.h
@@ -65,9 +65,19 @@ extern u32 __boot_cpu_mode[2];
 void __hyp_set_vectors(phys_addr_t phys_vector_base);
 void __hyp_reset_vectors(void);
 
+DECLARE_STATIC_KEY_FALSE(kvm_protected_mode_initialized);
+
 /* Reports the availability of HYP mode */
 static inline bool is_hyp_mode_available(void)
 {
+	/*
+	 * If KVM protected mode is initialized, all CPUs must have been booted
+	 * in EL2. Avoid checking __boot_cpu_mode as CPUs now come up in EL1.
+	 */
+	if (IS_ENABLED(CONFIG_KVM) &&
+	    static_branch_likely(&kvm_protected_mode_initialized))
+		return true;
+
 	return (__boot_cpu_mode[0] == BOOT_CPU_MODE_EL2 &&
 		__boot_cpu_mode[1] == BOOT_CPU_MODE_EL2);
 }
@@ -75,6 +85,14 @@ static inline bool is_hyp_mode_available(void)
 /* Check if the bootloader has booted CPUs in different modes */
 static inline bool is_hyp_mode_mismatched(void)
 {
+	/*
+	 * If KVM protected mode is initialized, all CPUs must have been booted
+	 * in EL2. Avoid checking __boot_cpu_mode as CPUs now come up in EL1.
+	 */
+	if (IS_ENABLED(CONFIG_KVM) &&
+	    static_branch_likely(&kvm_protected_mode_initialized))
+		return false;
+
 	return __boot_cpu_mode[0] != __boot_cpu_mode[1];
 }
 
