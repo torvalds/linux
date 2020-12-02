3eaf2da98993ecdf6e46a2b27cd31ef84a12e45f
Marek Szyprowski <m.szyprowski@samsung.com>
2020-12-02 07:47:59 +0100

    phy: samsung: Merge Kconfig for Exynos5420 and Exynos5250
    Exynos5420 variant of USB2 PHY is handled by the same code as the
    Exynos5250 one. Introducing a separate Kconfig symbol for it was an
    over-engineering, which turned out to cause build break for certain
    configurations:
    
    ERROR: modpost: "exynos5420_usb2_phy_config" [drivers/phy/samsung/phy-exynos-usb2.ko] undefined!
    
    Fix this by removing PHY_EXYNOS5420_USB2 symbol and using
    PHY_EXYNOS5250_USB2 also for Exynos5420 SoCs.
    
    Reported-by: Markus Reichl <m.reichl@fivetechno.de>
    Fixes: 81b534f7e9b2 ("phy: samsung: Add support for the Exynos5420 variant of the USB2 PHY")
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Acked-by: Krzysztof Kozlowski <krzk@kernel.org>
    Reviewed-by: Jaehoon Chung <jh80.chung@samsung.com>
    Link: https://lore.kernel.org/r/20201202064759.24300-1-m.szyprowski@samsung.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/phy/samsung/phy-samsung-usb2.c
+++ drivers/phy/samsung/phy-samsung-usb2.c
@@ -127,8 +127,6 @@ static const struct of_device_id samsung_usb2_phy_of_match[] = {
 		.compatible = "samsung,exynos5250-usb2-phy",
 		.data = &exynos5250_usb2_phy_config,
 	},
-#endif
-#ifdef CONFIG_PHY_EXYNOS5420_USB2
 	{
 		.compatible = "samsung,exynos5420-usb2-phy",
 		.data = &exynos5420_usb2_phy_config,
