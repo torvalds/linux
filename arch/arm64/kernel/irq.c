42b8c0cb3c23392773ad2803477f78c716880fc0
Lucas Wei <lucaswei@google.com>
2020-12-03 20:01:44 +0800

    ANDROID: GKI: soc: qcom: export `irq_stack_ptr`
    The Qualcomm minidump feature needs irq_stack_ptr for debugging with
    ramdumps. Export it.
    
    Bug: 172988823
    Bug: 176525217
    Change-Id: I3e3de735f017a17b912ae10e1153ae5fd27636ef
    Signed-off-by: Lucas Wei <lucaswei@google.com>
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- arch/arm64/kernel/irq.c
+++ arch/arm64/kernel/irq.c
@@ -38,6 +38,7 @@ unsigned long irq_err_count;
 DEFINE_PER_CPU(struct nmi_ctx, nmi_contexts);
 
 DEFINE_PER_CPU(unsigned long *, irq_stack_ptr);
+EXPORT_PER_CPU_SYMBOL_GPL(irq_stack_ptr);
 
 int arch_show_interrupts(struct seq_file *p, int prec)
 {
