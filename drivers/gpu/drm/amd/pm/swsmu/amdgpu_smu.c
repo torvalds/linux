b4bc9f10c740c8fb3d78e5ac44c1604a5b84cd98
Aaron Liu <aaron.liu@amd.com>
2020-12-03 16:44:46 +0800

    drm/amd/pm: partially enable swsmu for yellow carp(V2)
    This patch is to partially enable swSMU for yellow carp for the moment.
    V2: rename smu_v13_0 to smu_v13_0_1.
    
    Signed-off-by: Aaron Liu <aaron.liu@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Huang Rui <ray.huang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
+++ drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
@@ -658,6 +658,9 @@ static int smu_late_init(void *handle)
 
 	smu_set_fine_grain_gfx_freq_parameters(smu);
 
+	if (adev->asic_type == CHIP_YELLOW_CARP)
+		return 0;
+
 	if (!smu->pm_enabled)
 		return 0;
 
@@ -1302,6 +1305,9 @@ static int smu_hw_init(void *handle)
 		smu_dpm_set_jpeg_enable(smu, true);
 		smu_set_gfx_cgpg(&adev->smu, true);
 	}
+
+	if (adev->asic_type == CHIP_YELLOW_CARP)
+		return 0;
 
 	if (!smu->pm_enabled)
 		return 0;
