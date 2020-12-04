78b35fd94cf91d782f6e65ed45f0355215f8fb94
Dan Carpenter <dan.carpenter@oracle.com>
2020-12-04 17:23:16 +0300

    virtio_net: Fix error code in probe()
    [ Upstream commit 411ea23a76526e6efed0b601abb603d3c981b333 ]
    
    Set a negative error code intead of returning success if the MTU has
    been changed to something invalid.
    
    Fixes: fe36cbe0671e ("virtio_net: clear MTU when out of range")
    Reported-by: Robert Buhren <robert.buhren@sect.tu-berlin.de>
    Reported-by: Felicitas Hetzelt <file@sect.tu-berlin.de>
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Link: https://lore.kernel.org/r/X8pGVJSeeCdII1Ys@mwanda
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -3072,6 +3072,7 @@ static int virtnet_probe(struct virtio_device *vdev)
 			dev_err(&vdev->dev,
 				"device MTU appears to have changed it is now %d < %d",
 				mtu, dev->min_mtu);
+			err = -EINVAL;
 			goto free;
 		}
 
