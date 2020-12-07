91438aebcd56ed3cfa480e4546c7b06e9db3c73c
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2020-12-07 18:46:06 +0100

    ACPI: scan: Fix up _DEP-related terminology with supplier/consumer
    The ACPI namespace scanning code uses the terms master/slave when
    populating the list of _DEP dependencies, but that use has no
    external exposures and is not mandated by nor associated with any
    external specifications.
    
    Change the language used through-out to supplier/consumer.
    
    No functional impact.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -51,8 +51,8 @@ static u64 spcr_uart_addr;
 
 struct acpi_dep_data {
 	struct list_head node;
-	acpi_handle master;
-	acpi_handle slave;
+	acpi_handle supplier;
+	acpi_handle consumer;
 };
 
 void acpi_scan_lock_acquire(void)
@@ -1881,8 +1881,8 @@ static void acpi_device_dep_initialize(struct acpi_device *adev)
 		if (!dep)
 			return;
 
-		dep->master = dep_devices.handles[i];
-		dep->slave  = adev->handle;
+		dep->supplier = dep_devices.handles[i];
+		dep->consumer  = adev->handle;
 		adev->dep_unmet++;
 
 		mutex_lock(&acpi_dep_list_lock);
@@ -2058,8 +2058,8 @@ void acpi_walk_dep_device_list(acpi_handle handle)
 
 	mutex_lock(&acpi_dep_list_lock);
 	list_for_each_entry_safe(dep, tmp, &acpi_dep_list, node) {
-		if (dep->master == handle) {
-			acpi_bus_get_device(dep->slave, &adev);
+		if (dep->supplier == handle) {
+			acpi_bus_get_device(dep->consumer, &adev);
 			if (!adev)
 				continue;
 
