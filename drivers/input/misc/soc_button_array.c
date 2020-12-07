223f61b8c5ad80f01900bc25f8073dfa4f23a2be
Hans de Goede <hdegoede@redhat.com>
2020-12-06 21:59:42 -0800

    Input: soc_button_array - add Lenovo Yoga Tablet2 1051L to the dmi_use_low_level_irq list
    Add the Lenovo Yoga Tablet2 1051L to the list of devices where the
    ACPI AML code is poking the GPIO config register directly changing
    the IRQ type to a low_level_irq, which we need to work around.
    
    This fixes the home button on the Lenovo Yoga Tablet2 1051L not
    working.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://lore.kernel.org/r/20201206161245.24798-1-hdegoede@redhat.com
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/misc/soc_button_array.c
+++ drivers/input/misc/soc_button_array.c
@@ -83,6 +83,17 @@ static const struct dmi_system_id dmi_use_low_level_irq[] = {
 			DMI_MATCH(DMI_PRODUCT_NAME, "One S1003"),
 		},
 	},
+	{
+		/*
+		 * Lenovo Yoga Tab2 1051L, something messes with the home-button
+		 * IRQ settings, leading to a non working home-button.
+		 */
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "60073"),
+			DMI_MATCH(DMI_PRODUCT_VERSION, "1051L"),
+		},
+	},
 	{} /* Terminating entry */
 };
 
