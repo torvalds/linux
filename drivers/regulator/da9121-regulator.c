9536ce63705952be5214544e3b048c78f932e794
Adam Ward <Adam.Ward.opensource@diasemi.com>
2020-12-07 17:15:16 +0000

    regulator: da9121: Fix index used for DT property
    Signed-off-by: Adam Ward <Adam.Ward.opensource@diasemi.com>
    Link: https://lore.kernel.org/r/2cf324b68d37c4059c7995e8cab5fc9a608ea65d.1607361013.git.Adam.Ward.opensource@diasemi.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/da9121-regulator.c
+++ drivers/regulator/da9121-regulator.c
@@ -381,7 +381,7 @@ static int da9121_of_parse_cb(struct device_node *np,
 		uint32_t ripple_reg;
 		int ret;
 
-		if (of_property_read_u32(da9121_matches[pdata->num_buck].of_node,
+		if (of_property_read_u32(da9121_matches[pdata->num_buck-1].of_node,
 				"dlg,ripple-cancel", &ripple_cancel)) {
 			if (pdata->num_buck > 1)
 				ripple_reg = DA9xxx_REG_BUCK_BUCK2_7;
