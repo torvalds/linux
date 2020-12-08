1b2ac1d95fb1619ed7dc345fe2e4ca4df2a81e3d
Chris Chiu <chiu@endlessos.org>
2020-12-08 14:04:14 +0800

    ASoC: Intel: bytcr_rt5640: Add quirk for ARCHOS Cesium 140
    [ Upstream commit 1bea2256aa96a2d7b1b576eb74e29d79edc9bea8 ]
    
    Tha ARCHOS Cesium 140 tablet has problem with the jack-sensing,
    thus the heaset functions are not working.
    
    Add quirk for this model to select the correct input map, jack-detect
    options and channel map to enable jack sensing and headset microphone.
    This device uses IN1 for its internal MIC and JD2 for jack-detect.
    
    Signed-off-by: Chris Chiu <chiu@endlessos.org>
    Acked-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20201208060414.27646-1-chiu@endlessos.org
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- sound/soc/intel/boards/bytcr_rt5640.c
+++ sound/soc/intel/boards/bytcr_rt5640.c
@@ -439,6 +439,18 @@ static const struct dmi_system_id byt_rt5640_quirk_table[] = {
 	},
 	{
 		.matches = {
+			DMI_EXACT_MATCH(DMI_SYS_VENDOR, "ARCHOS"),
+			DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "ARCHOS 140 CESIUM"),
+		},
+		.driver_data = (void *)(BYT_RT5640_IN1_MAP |
+					BYT_RT5640_JD_SRC_JD2_IN4N |
+					BYT_RT5640_OVCD_TH_2000UA |
+					BYT_RT5640_OVCD_SF_0P75 |
+					BYT_RT5640_SSP0_AIF1 |
+					BYT_RT5640_MCLK_EN),
+	},
+	{
+		.matches = {
 			DMI_EXACT_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
 			DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "ME176C"),
 		},
