3d1387b3b8f6cc1ccdbb5f0d7af24df02f4baef9
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2020-12-08 07:57:13 +0100

    media: vidtv: fix some warnings
    As reported by sparse:
    
    	drivers/media/test-drivers/vidtv/vidtv_ts.h:47:47: warning: array of flexible structures
    	drivers/media/test-drivers/vidtv/vidtv_channel.c:458:54: warning: incorrect type in argument 3 (different base types)
    	drivers/media/test-drivers/vidtv/vidtv_channel.c:458:54:    expected unsigned short [usertype] service_id
    	drivers/media/test-drivers/vidtv/vidtv_channel.c:458:54:    got restricted __be16 [usertype] service_id
    	drivers/media/test-drivers/vidtv/vidtv_s302m.c:471 vidtv_s302m_encoder_init() warn: possible memory leak of 'e'
    
    Address such warnings.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/test-drivers/vidtv/vidtv_psi.h
+++ drivers/media/test-drivers/vidtv/vidtv_psi.h
@@ -743,7 +743,7 @@ struct vidtv_psi_table_eit {
 struct vidtv_psi_table_eit
 *vidtv_psi_eit_table_init(u16 network_id,
 			  u16 transport_stream_id,
-			  u16 service_id);
+			  __be16 service_id);
 
 /**
  * struct vidtv_psi_eit_write_args - Arguments for writing an EIT section
