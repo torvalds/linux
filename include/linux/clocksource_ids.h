b2c67cbe9f447312f5cdd7c6641b463f2349aec0
Thomas Gleixner <tglx@linutronix.de>
2020-12-09 14:09:27 +0800

    time: Add mechanism to recognize clocksource in time_get_snapshot
    System time snapshots are not conveying information about the current
    clocksource which was used, but callers like the PTP KVM guest
    implementation have the requirement to evaluate the clocksource type to
    select the appropriate mechanism.
    
    Introduce a clocksource id field in struct clocksource which is by default
    set to CSID_GENERIC (0). Clocksource implementations can set that field to
    a value which allows to identify the clocksource.
    
    Store the clocksource id of the current clocksource in the
    system_time_snapshot so callers can evaluate which clocksource was used to
    take the snapshot and act accordingly.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Jianyong Wu <jianyong.wu@arm.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20201209060932.212364-5-jianyong.wu@arm.com

---
+++ include/linux/clocksource_ids.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_CLOCKSOURCE_IDS_H
+#define _LINUX_CLOCKSOURCE_IDS_H
+
+/* Enum to give clocksources a unique identifier */
+enum clocksource_ids {
+	CSID_GENERIC		= 0,
+	CSID_MAX,
+};
+
+#endif
