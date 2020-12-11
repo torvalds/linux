12e3ef8b3e7c0840dee76c4da7a0af8d5982c90d
kernel test robot <lkp@intel.com>
2020-12-11 11:16:15 +0100

    scsi: megaraid: Fix ifnullfree.cocci warnings
    NULL check before vfree is not needed.
    
    Generated by: scripts/coccinelle/free/ifnullfree.cocci
    
    Link: https://lore.kernel.org/r/alpine.DEB.2.22.394.2012111113060.2669@hadrien
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Julia Lawall <julia.lawall@inria.fr>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/megaraid/megaraid_sas_fusion.c
+++ drivers/scsi/megaraid/megaraid_sas_fusion.c
@@ -3920,8 +3920,7 @@ megasas_free_host_crash_buffer(struct megasas_instance *instance)
 {
 	unsigned int i;
 	for (i = 0; i < instance->drv_buf_alloc; i++) {
-		if (instance->crash_buf[i])
-			vfree(instance->crash_buf[i]);
+		vfree(instance->crash_buf[i]);
 	}
 	instance->drv_buf_index = 0;
 	instance->drv_buf_alloc = 0;
