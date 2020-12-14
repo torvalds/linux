caaf95e0f23f9ed240b02251aab0f6fdb652b33d
Andra Paraschiv <andraprs@amazon.com>
2020-12-14 18:11:19 +0200

    vm_sockets: Add VMADDR_FLAG_TO_HOST vsock flag
    Add VMADDR_FLAG_TO_HOST vsock flag that is used to setup a vsock
    connection where all the packets are forwarded to the host.
    
    Then, using this type of vsock channel, vsock communication between
    sibling VMs can be built on top of it.
    
    Changelog
    
    v3 -> v4
    
    * Update the "VMADDR_FLAG_TO_HOST" value, as the size of the field has
      been updated to 1 byte.
    
    v2 -> v3
    
    * Update comments to mention when the flag is set in the connect and
      listen paths.
    
    v1 -> v2
    
    * New patch in v2, it was split from the first patch in the series.
    * Remove the default value for the vsock flags field.
    * Update the naming for the vsock flag to "VMADDR_FLAG_TO_HOST".
    
    Signed-off-by: Andra Paraschiv <andraprs@amazon.com>
    Reviewed-by: Stefano Garzarella <sgarzare@redhat.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/uapi/linux/vm_sockets.h
+++ include/uapi/linux/vm_sockets.h
@@ -115,6 +115,26 @@
 
 #define VMADDR_CID_HOST 2
 
+/* The current default use case for the vsock channel is the following:
+ * local vsock communication between guest and host and nested VMs setup.
+ * In addition to this, implicitly, the vsock packets are forwarded to the host
+ * if no host->guest vsock transport is set.
+ *
+ * Set this flag value in the sockaddr_vm corresponding field if the vsock
+ * packets need to be always forwarded to the host. Using this behavior,
+ * vsock communication between sibling VMs can be setup.
+ *
+ * This way can explicitly distinguish between vsock channels created for
+ * different use cases, such as nested VMs (or local communication between
+ * guest and host) and sibling VMs.
+ *
+ * The flag can be set in the connect logic in the user space application flow.
+ * In the listen logic (from kernel space) the flag is set on the remote peer
+ * address. This happens for an incoming connection when it is routed from the
+ * host and comes from the guest (local CID and remote CID > VMADDR_CID_HOST).
+ */
+#define VMADDR_FLAG_TO_HOST 0x01
+
 /* Invalid vSockets version. */
 
 #define VM_SOCKETS_INVALID_VERSION -1U
