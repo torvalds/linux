8776682ce31cf08fac7bfa3427bf496a44dc8722
Philipp Gerlesberger <Philipp.Gerlesberger@fau.de>
2020-12-14 12:01:45 +0100

    media: atomisp: Convert comments to C99 initializers
    The struct initalizers have been changed as recommended on
    https://kernelnewbies.org/KernelJanitors/Todo
    Also remove all the false, 0, and NULL members.
    
    Link: https://lore.kernel.org/linux-media/20201214110156.6152-2-Philipp.Gerlesberger@fau.de
    Co-developed-by: Andrey Khlopkov <ij72uhux@stud.informatik.uni-erlangen.de>
    Signed-off-by: Andrey Khlopkov <ij72uhux@stud.informatik.uni-erlangen.de>
    Signed-off-by: Philipp Gerlesberger <Philipp.Gerlesberger@fau.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/staging/media/atomisp/pci/runtime/rmgr/src/rmgr_vbuf.c
+++ drivers/staging/media/atomisp/pci/runtime/rmgr/src/rmgr_vbuf.c
@@ -30,34 +30,22 @@ static struct ia_css_rmgr_vbuf_handle handle_table[NUM_HANDLES];
 /*
  * @brief VBUF resource pool - refpool
  */
-static struct ia_css_rmgr_vbuf_pool refpool = {
-	false,			/* copy_on_write */
-	false,			/* recycle */
-	0,			/* size */
-	0,			/* index */
-	NULL,			/* handles */
-};
+static struct ia_css_rmgr_vbuf_pool refpool;
 
 /*
  * @brief VBUF resource pool - writepool
  */
 static struct ia_css_rmgr_vbuf_pool writepool = {
-	true,			/* copy_on_write */
-	false,			/* recycle */
-	0,			/* size */
-	0,			/* index */
-	NULL,			/* handles */
+	.copy_on_write	= true,
 };
 
 /*
  * @brief VBUF resource pool - hmmbufferpool
  */
 static struct ia_css_rmgr_vbuf_pool hmmbufferpool = {
-	true,			/* copy_on_write */
-	true,			/* recycle */
-	32,			/* size */
-	0,			/* index */
-	NULL,			/* handles */
+	.copy_on_write	= true,
+	.recycle	= true,
+	.size		= 32,
 };
 
 struct ia_css_rmgr_vbuf_pool *vbuf_ref = &refpool;
