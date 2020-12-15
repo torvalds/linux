4a3c73e717348e1ba5c9fce2051cff40ab1d5be8
Lecopzer Chen <lecopzer.chen@mediatek.com>
2020-12-14 19:13:23 -0800

    BACKPORT: mm/cma.c: remove redundant cma_mutex lock
    The cma_mutex which protects alloc_contig_range() was first appeared in
    commit 7ee793a62fa8c ("cma: Remove potential deadlock situation"), at that
    time, there is no guarantee the behavior of concurrency inside
    alloc_contig_range().
    
    After commit 2c7452a075d4db2dc ("mm/page_isolation.c: make
    start_isolate_page_range() fail if already isolated")
    
      > However, two subsystems (CMA and gigantic
      > huge pages for example) could attempt operations on the same range.  If
      > this happens, one thread may 'undo' the work another thread is doing.
      > This can result in pageblocks being incorrectly left marked as
      > MIGRATE_ISOLATE and therefore not available for page allocation.
    
    The concurrency inside alloc_contig_range() was clarified.
    
    Now we can find that hugepage and virtio call alloc_contig_range() without
    any lock, thus cma_mutex is "redundant" in cma_alloc() now.
    
    Link: https://lkml.kernel.org/r/20201020102241.3729-1-lecopzer.chen@mediatek.com
    Signed-off-by: Lecopzer Chen <lecopzer.chen@mediatek.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Matthias Brugger <matthias.bgg@gmail.com>
    Cc: YJ Chiang <yj.chiang@mediatek.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit a4efc174b382fcdb62e2d90d39e78a274a975e38)
    minchan: Resolved minor conflict with alloc_contig_range in mm/cma.c ]
    Bug: 181887812
    Change-Id: I0138e571f8131ed1d53ee91b61a037bc762d824a

--- mm/cma.c
+++ mm/cma.c
@@ -41,7 +41,6 @@
 
 struct cma cma_areas[MAX_CMA_AREAS];
 unsigned cma_area_count;
-static DEFINE_MUTEX(cma_mutex);
 
 phys_addr_t cma_get_base(const struct cma *cma)
 {
@@ -480,9 +479,7 @@ struct page *cma_alloc(struct cma *cma, size_t count, unsigned int align,
 		mutex_unlock(&cma->lock);
 
 		pfn = cma->base_pfn + (bitmap_no << cma->order_per_bit);
-		mutex_lock(&cma_mutex);
 		ret = alloc_contig_range(pfn, pfn + count, MIGRATE_CMA, gfp_mask);
-		mutex_unlock(&cma_mutex);
 		if (ret == 0) {
 			page = pfn_to_page(pfn);
 			break;
