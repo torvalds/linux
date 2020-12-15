15d5de496bebfd7c0261987423480e98d1a14495
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2020-12-14 19:03:50 -0800

    mm: slab: clarify krealloc()'s behavior with __GFP_ZERO
    Patch series "slab: provide and use krealloc_array()", v3.
    
    Andy brought to my attention the fact that users allocating an array of
    equally sized elements should check if the size multiplication doesn't
    overflow.  This is why we have helpers like kmalloc_array().
    
    However we don't have krealloc_array() equivalent and there are many users
    who do their own multiplication when calling krealloc() for arrays.
    
    This series provides krealloc_array() and uses it in a couple places.
    
    A separate series will follow adding devm_krealloc_array() which is needed
    in the xilinx adc driver.
    
    This patch (of 9):
    
    __GFP_ZERO is ignored by krealloc() (unless we fall-back to kmalloc()
    path, in which case it's honored).  Point that out in the kerneldoc.
    
    Link: https://lkml.kernel.org/r/20201109110654.12547-1-brgl@bgdev.pl
    Link: https://lkml.kernel.org/r/20201109110654.12547-2-brgl@bgdev.pl
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Cc: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Cc: Sumit Semwal <sumit.semwal@linaro.org>
    Cc: Gustavo Padovan <gustavo@padovan.org>
    Cc: Christian Knig <christian.koenig@amd.com>
    Cc: Mauro Carvalho Chehab <mchehab@kernel.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Robert Richter <rric@kernel.org>
    Cc: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Cc: Maxime Ripard <mripard@kernel.org>
    Cc: Thomas Zimmermann <tzimmermann@suse.de>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Linus Walleij <linus.walleij@linaro.org>
    Cc: "Michael S . Tsirkin" <mst@redhat.com>
    Cc: Jason Wang <jasowang@redhat.com>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Jaroslav Kysela <perex@perex.cz>
    Cc: Takashi Iwai <tiwai@suse.com>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Takashi Iwai <tiwai@suse.de>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/slab_common.c
+++ mm/slab_common.c
@@ -1091,9 +1091,9 @@ static __always_inline void *__do_krealloc(const void *p, size_t new_size,
  * @flags: the type of memory to allocate.
  *
  * The contents of the object pointed to are preserved up to the
- * lesser of the new and old sizes.  If @p is %NULL, krealloc()
- * behaves exactly like kmalloc().  If @new_size is 0 and @p is not a
- * %NULL pointer, the object pointed to is freed.
+ * lesser of the new and old sizes (__GFP_ZERO flag is effectively ignored).
+ * If @p is %NULL, krealloc() behaves exactly like kmalloc().  If @new_size
+ * is 0 and @p is not a %NULL pointer, the object pointed to is freed.
  *
  * Return: pointer to the allocated memory or %NULL in case of error
  */
