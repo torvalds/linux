9fc286ed39266080c084bc7fad656c2e16ca43fa
Marc Kleine-Budde <mkl@pengutronix.de>
2020-12-15 11:32:37 +0100

    can: m_can: m_can_class_unregister(): remove erroneous m_can_clk_stop()
    commit c4aec381ab98c9189d47b935832541d520f1f67f upstream.
    
    In m_can_class_register() the clock is started, but stopped on exit. When
    calling m_can_class_unregister(), the clock is stopped a second time.
    
    This patch removes the erroneous m_can_clk_stop() in  m_can_class_unregister().
    
    Fixes: f524f829b75a ("can: m_can: Create a m_can platform framework")
    Cc: Dan Murphy <dmurphy@ti.com>
    Cc: Sriram Dash <sriram.dash@samsung.com>
    Reviewed-by: Sean Nyekjaer <sean@geanix.com>
    Link: https://lore.kernel.org/r/20201215103238.524029-2-mkl@pengutronix.de
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/can/m_can/m_can.c
+++ drivers/net/can/m_can/m_can.c
@@ -1914,8 +1914,6 @@ EXPORT_SYMBOL_GPL(m_can_class_resume);
 void m_can_class_unregister(struct m_can_classdev *m_can_dev)
 {
 	unregister_candev(m_can_dev->net);
-
-	m_can_clk_stop(m_can_dev);
 }
 EXPORT_SYMBOL_GPL(m_can_class_unregister);
 
