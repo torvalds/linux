6a772b98a8c5d697e00bf760a29ffe893cdb93dd
Quentin Perret <qperret@google.com>
2020-12-15 17:55:03 +0000

    Revert "ANDROID: cpu/hotplug: add migration to paused_cpus"
    This reverts commit e19b8ce907a4ccc2f9dbeba218c50d1ff07fd321.
    
    CPU Pause causes major merge conflicts with the 5.11 scheduler changes
    (migrate-disable specifically), so lets revert Pause temporarily as it
    is not needed urgently in android-mainline.
    
    Signed-off-by: Quentin Perret <qperret@google.com>
    Change-Id: Ibc5e502844d5e2e72cc48eede1032c89b16f5ccf

--- include/linux/sched/hotplug.h
+++ include/linux/sched/hotplug.h
@@ -11,8 +11,6 @@ extern int sched_cpu_activate(unsigned int cpu);
 extern int sched_cpus_activate(struct cpumask *cpus);
 extern int sched_cpu_deactivate(unsigned int cpu);
 extern int sched_cpus_deactivate_nosync(struct cpumask *cpus);
-extern int sched_cpu_drain_rq(unsigned int cpu);
-extern void sched_cpu_drain_rq_wait(unsigned int cpu);
 
 #ifdef CONFIG_HOTPLUG_CPU
 extern int sched_cpu_dying(unsigned int cpu);
