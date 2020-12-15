afee4410bc6c50e1422c5a45d633ad0e478ea960
Steve French <stfrench@microsoft.com>
2020-12-15 15:17:43 -0600

    cifs: update internal module version number
    To 2.30
    
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/cifs/cifsfs.h
+++ fs/cifs/cifsfs.h
@@ -160,5 +160,5 @@ extern struct dentry *cifs_smb3_do_mount(struct file_system_type *fs_type,
 extern const struct export_operations cifs_export_ops;
 #endif /* CONFIG_CIFS_NFSD_EXPORT */
 
-#define CIFS_VERSION   "2.29"
+#define CIFS_VERSION   "2.30"
 #endif				/* _CIFSFS_H */
