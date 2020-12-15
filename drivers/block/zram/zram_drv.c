3d711a382735d2c34d3ba2075a5aa83a894f4a57
Rui Salvaterra <rsalvaterra@gmail.com>
2020-12-14 19:14:35 -0800

    zram: break the strict dependency from lzo
    From the beginning, the zram block device always enabled CRYPTO_LZO,
    since lzo-rle is hardcoded as the fallback compression algorithm.  As a
    consequence, on systems where another compression algorithm is chosen
    (e.g.  CRYPTO_ZSTD), the lzo kernel module becomes unused, while still
    having to be built/loaded.
    
    This patch removes the hardcoded lzo-rle dependency and allows the user
    to select the default compression algorithm for zram at build time.  The
    previous behaviour is kept, as the default algorithm is still lzo-rle.
    
    Link: https://lkml.kernel.org/r/20201207121245.50529-1-rsalvaterra@gmail.com
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>
    Suggested-by: Sergey Senozhatsky <sergey.senozhatsky.work@gmail.com>
    Suggested-by: Minchan Kim <minchan@kernel.org>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -42,7 +42,7 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
-static const char *default_compressor = "lzo-rle";
+static const char *default_compressor = CONFIG_ZRAM_DEF_COMP;
 
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
