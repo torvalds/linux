6251cf847de4ad64fa972923dc8deed3f304e271
Gustavo A. R. Silva <gustavoars@kernel.org>
2020-12-15 20:43:04 -0800

    UPSTREAM: lib/stackdepot.c: replace one-element array with flexible-array member
    Patch series "lib/stackdepot.c: Replace one-element array with flexible-array member".
    
    This series aims to replace a one-element array with a flexible-array
    member.  Also, make use of the struct_size(), flexible_array_size() and
    array_size() helpers.
    
    This patch (of 3):
    
    There is a regular need in the kernel to provide a way to declare having a
    dynamically sized set of trailing elements in a structure.  Kernel code
    should always use “flexible array members”[1] for these cases.  The
    older style of one-element or zero-length arrays should no longer be
    used[2].
    
    Refactor the code according to the use of a flexible-array member in
    struct stack_record, instead of a one-element array, and use the
    struct_size() helper to calculate the size for the allocation.
    
    [1] https://en.wikipedia.org/wiki/Flexible_array_member
    [2] https://www.kernel.org/doc/html/v5.9-rc1/process/deprecated.html#zero-length-and-one-element-arrays
    
    Link: https://lkml.kernel.org/r/cover.1601565471.git.gustavoars@kernel.org
    Link: https://lore.kernel.org/lkml/5f75876b.x9zdN10esiC0qLHV%25lkp@intel.com/
    Link: https://lkml.kernel.org/r/2f1e6a17aaa891ad9c58817cf0a10b8ab8894f59.1601565471.git.gustavoars@kernel.org
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 3a2b67e6e3fdb14c6da4c6909454d7a1d9b24f70)
    Bug: 172318110
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Change-Id: Ic882af5c57d592b9e436371793860406496afacd

--- lib/stackdepot.c
+++ lib/stackdepot.c
@@ -62,7 +62,7 @@ struct stack_record {
 	u32 hash;			/* Hash in the hastable */
 	u32 size;			/* Number of frames in the stack */
 	union handle_parts handle;
-	unsigned long entries[1];	/* Variable-sized array of entries. */
+	unsigned long entries[];	/* Variable-sized array of entries. */
 };
 
 static void *stack_slabs[STACK_ALLOC_MAX_SLABS];
@@ -104,9 +104,8 @@ static bool init_stack_slab(void **prealloc)
 static struct stack_record *depot_alloc_stack(unsigned long *entries, int size,
 		u32 hash, void **prealloc, gfp_t alloc_flags)
 {
-	int required_size = offsetof(struct stack_record, entries) +
-		sizeof(unsigned long) * size;
 	struct stack_record *stack;
+	size_t required_size = struct_size(stack, entries, size);
 
 	required_size = ALIGN(required_size, 1 << STACK_ALLOC_ALIGN);
 
