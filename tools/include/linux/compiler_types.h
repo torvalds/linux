ffb9beb13e8daf3fcb6bab470d07962b05d619b7
Arnaldo Carvalho de Melo <acme@redhat.com>
2020-12-18 10:16:08 -0300

    tools headers: Add conditional __has_builtin()
    As it'll be used by the ctype.h sync with its kernel source original.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/include/linux/compiler_types.h
+++ tools/include/linux/compiler_types.h
@@ -2,6 +2,17 @@
 #ifndef __LINUX_COMPILER_TYPES_H
 #define __LINUX_COMPILER_TYPES_H
 
+/* Builtins */
+
+/*
+ * __has_builtin is supported on gcc >= 10, clang >= 3 and icc >= 21.
+ * In the meantime, to support gcc < 10, we implement __has_builtin
+ * by hand.
+ */
+#ifndef __has_builtin
+#define __has_builtin(x) (0)
+#endif
+
 /* Compiler specific macros. */
 #ifdef __GNUC__
 #include <linux/compiler-gcc.h>
