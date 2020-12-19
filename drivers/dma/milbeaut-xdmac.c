41f95d84fcf58fcbdeaffb28bcec290cecf1608e
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2020-12-19 14:28:00 +0100

    dmaengine: milbeaut-xdmac: Fix a resource leak in the error handling path of the probe function
    commit d645148cc82ca7fbacaa601414a552184e9c6dd3 upstream.
    
    'disable_xdmac()' should be called in the error handling path of the
    probe function to undo a previous 'enable_xdmac()' call, as already
    done in the remove function.
    
    Fixes: a6e9be055d47 ("dmaengine: milbeaut-xdmac: Add XDMAC driver for Milbeaut platforms")
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Link: https://lore.kernel.org/r/20201219132800.183254-1-christophe.jaillet@wanadoo.fr
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/dma/milbeaut-xdmac.c
+++ drivers/dma/milbeaut-xdmac.c
@@ -351,7 +351,7 @@ static int milbeaut_xdmac_probe(struct platform_device *pdev)
 
 	ret = dma_async_device_register(ddev);
 	if (ret)
-		return ret;
+		goto disable_xdmac;
 
 	ret = of_dma_controller_register(dev->of_node,
 					 of_dma_simple_xlate, mdev);
@@ -364,6 +364,8 @@ static int milbeaut_xdmac_probe(struct platform_device *pdev)
 
 unregister_dmac:
 	dma_async_device_unregister(ddev);
+disable_xdmac:
+	disable_xdmac(mdev);
 	return ret;
 }
 
