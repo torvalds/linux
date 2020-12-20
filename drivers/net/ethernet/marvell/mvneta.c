c7d068a2d711ff48b40cac452afbc82cdc35ebb9
Dinghao Liu <dinghao.liu@zju.edu.cn>
2020-12-20 16:29:30 +0800

    net: ethernet: mvneta: Fix error handling in mvneta_probe
    [ Upstream commit 58f60329a6be35a5653edb3fd2023ccef9eb9943 ]
    
    When mvneta_port_power_up() fails, we should execute
    cleanup functions after label err_netdev to avoid memleak.
    
    Fixes: 41c2b6b4f0f80 ("net: ethernet: mvneta: Add back interface mode validation")
    Signed-off-by: Dinghao Liu <dinghao.liu@zju.edu.cn>
    Link: https://lore.kernel.org/r/20201220082930.21623-1-dinghao.liu@zju.edu.cn
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/marvell/mvneta.c
+++ drivers/net/ethernet/marvell/mvneta.c
@@ -5232,7 +5232,7 @@ static int mvneta_probe(struct platform_device *pdev)
 	err = mvneta_port_power_up(pp, pp->phy_interface);
 	if (err < 0) {
 		dev_err(&pdev->dev, "can't power up port\n");
-		return err;
+		goto err_netdev;
 	}
 
 	/* Armada3700 network controller does not support per-cpu
