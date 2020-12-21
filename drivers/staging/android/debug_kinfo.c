180f8b73199bd665c03ff43853170280fc82e02d
Jone Chou <jonechou@google.com>
2020-12-21 23:38:18 +0800

    ANDROID: kernel: add module info for debug_kinfo
    Backup module kallsyms information for bootloader usage
    
    Bug: 176088154
    Bug: 175726952
    Change-Id: I800f7afe17d02536ef7694d3a2123003bbd034a7
    (cherry picked from commit 70e94804683fea94bd6087f3b66e4c1068e52262)
    [elavila: Port to android12-5.10]
    Signed-off-by: J. Avila <elavila@google.com>
    Signed-off-by: Jone Chou <jonechou@google.com>

--- drivers/staging/android/debug_kinfo.c
+++ drivers/staging/android/debug_kinfo.c
@@ -158,6 +158,10 @@ static int debug_kinfo_probe(struct platform_device *pdev)
 	info->thread_size = THREAD_SIZE;
 	info->swapper_pg_dir_pa = (u64)__pa_symbol(swapper_pg_dir);
 	strlcpy(info->last_uts_release, init_utsname()->release, sizeof(info->last_uts_release));
+	info->enabled_modules_tree_lookup = IS_ENABLED(CONFIG_MODULES_TREE_LOOKUP);
+	info->mod_core_layout_offset = offsetof(struct module, core_layout);
+	info->mod_init_layout_offset = offsetof(struct module, init_layout);
+	info->mod_kallsyms_offset = offsetof(struct module, kallsyms);
 
 	update_kernel_all_info(all_info);
 
