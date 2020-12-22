523929f1cac3e869492ea376c9d86af11ec0e5c5
Qu Wenruo <wqu@suse.com>
2020-12-22 13:59:23 +0800

    btrfs: make btrfs_dio_private::bytes u32
    btrfs_dio_private::bytes is only assigned from bio::bi_iter::bi_size,
    which is never larger than U32.
    
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/btrfs_inode.h
+++ fs/btrfs/btrfs_inode.h
@@ -325,7 +325,8 @@ struct btrfs_dio_private {
 	struct inode *inode;
 	u64 logical_offset;
 	u64 disk_bytenr;
-	u64 bytes;
+	/* Used for bio::bi_size */
+	u32 bytes;
 
 	/*
 	 * References to this structure. There is one reference per in-flight
