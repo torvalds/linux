ab099cc6e5d488d60c72f41a696f5c8ec159f9f1
Zheng Yongjun <zhengyongjun3@huawei.com>
2020-12-22 21:33:26 +0800

    mfd: mcp-sa11x0: Use DIV_ROUND_UP to calculate rw_timeout
    Don't open-code DIV_ROUND_UP() kernel macro.
    
    Signed-off-by: Zheng Yongjun <zhengyongjun3@huawei.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/mcp-sa11x0.c
+++ drivers/mfd/mcp-sa11x0.c
@@ -214,8 +214,7 @@ static int mcp_sa11x0_probe(struct platform_device *dev)
 	 * rate.  This is the period for 3 64-bit frames.  Always
 	 * round this time up.
 	 */
-	mcp->rw_timeout = (64 * 3 * 1000000 + mcp->sclk_rate - 1) /
-			  mcp->sclk_rate;
+	mcp->rw_timeout = DIV_ROUND_UP(64 * 3 * 1000000, mcp->sclk_rate);
 
 	ret = mcp_host_add(mcp, data->codec_pdata);
 	if (ret == 0)
