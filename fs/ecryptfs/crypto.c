902af369942f8d0a6bdaa8466ff0d84d3d9b03a8
Zheng Yongjun <zhengyongjun3@huawei.com>
2020-12-24 21:22:33 +0800

    ecryptfs: use DEFINE_MUTEX() for mutex lock
    mutex lock can be initialized automatically with DEFINE_MUTEX()
    rather than explicitly calling mutex_init().
    
    Signed-off-by: Zheng Yongjun <zhengyongjun3@huawei.com>
    Signed-off-by: Tyler Hicks <code@tyhicks.com>

--- fs/ecryptfs/crypto.c
+++ fs/ecryptfs/crypto.c
@@ -1590,11 +1590,10 @@ out:
 
 struct kmem_cache *ecryptfs_key_tfm_cache;
 static struct list_head key_tfm_list;
-struct mutex key_tfm_list_mutex;
+DEFINE_MUTEX(key_tfm_list_mutex);
 
 int __init ecryptfs_init_crypto(void)
 {
-	mutex_init(&key_tfm_list_mutex);
 	INIT_LIST_HEAD(&key_tfm_list);
 	return 0;
 }
