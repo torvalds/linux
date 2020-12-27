8c1b0c9acefe044aa53631be8635cd3b7661203b
Greg Kroah-Hartman <gregkh@google.com>
2020-12-27 13:23:49 +0100

    ANDROID: USB: gadget: f_fs.c: Fix merge issue
    The merge of 5.10.3 into android12-5.10 caused a build issue in the
    f_fs.c file due to an upstream patch being accepted that was a bit
    different from what was applied to the tree earlier.
    
    Fixes: 499df25c94d7 ("Merge 5.10.3 into android12-5.10")
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I41a8809038e7638697955f82609bcb3aea0293fa

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -1327,7 +1327,6 @@ static long ffs_epfile_ioctl(struct file *file, unsigned code,
 		struct usb_endpoint_descriptor desc1, *desc;
 
 		switch (epfile->ffs->gadget->speed) {
-		case USB_SPEED_SUPER_PLUS:
 		case USB_SPEED_SUPER:
 		case USB_SPEED_SUPER_PLUS:
 			desc_idx = 2;
