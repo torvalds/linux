0e1d9ca1766f5d95fb881f57b6c4a1ffa63d4648
Viresh Kumar <viresh.kumar@linaro.org>
2020-12-28 10:51:04 +0530

    opp: Call the missing clk_put() on error
    Fix the clock reference counting by calling the missing clk_put() in the
    error path.
    
    Cc: v5.10 <stable@vger.kernel.org> # v5.10
    Fixes: dd461cd9183f ("opp: Allow dev_pm_opp_get_opp_table() to return -EPROBE_DEFER")
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/opp/core.c
+++ drivers/opp/core.c
@@ -1101,7 +1101,7 @@ static struct opp_table *_allocate_opp_table(struct device *dev, int index)
 	ret = dev_pm_opp_of_find_icc_paths(dev, opp_table);
 	if (ret) {
 		if (ret == -EPROBE_DEFER)
-			goto remove_opp_dev;
+			goto put_clk;
 
 		dev_warn(dev, "%s: Error finding interconnect paths: %d\n",
 			 __func__, ret);
@@ -1113,6 +1113,9 @@ static struct opp_table *_allocate_opp_table(struct device *dev, int index)
 
 	return opp_table;
 
+put_clk:
+	if (!IS_ERR(opp_table->clk))
+		clk_put(opp_table->clk);
 remove_opp_dev:
 	_remove_opp_dev(opp_dev, opp_table);
 err:
