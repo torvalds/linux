83cd7dfb99e9540bafd18f9c164b184dcedb0fb6
Ziyuan Xu <xzy.xu@rock-chips.com>
2020-12-30 09:00:44 +0800

    gpio: rockchip: clear pendings before isr_handler is valid
    Change-Id: I93625437bc4e0096fbc6eca42f6bb3852a672d94
    Signed-off-by: Ziyuan Xu <xzy.xu@rock-chips.com>

--- drivers/gpio/gpio-rockchip.c
+++ drivers/gpio/gpio-rockchip.c
@@ -522,6 +522,7 @@ static int rockchip_interrupts_register(struct rockchip_pin_bank *bank)
 	 * things enabled, so for us that's all masked and all enabled.
 	 */
 	rockchip_gpio_writel(bank, 0xffffffff, bank->gpio_regs->int_mask);
+	rockchip_gpio_writel(bank, 0xffffffff, bank->gpio_regs->port_eoi);
 	rockchip_gpio_writel(bank, 0xffffffff, bank->gpio_regs->int_en);
 	gc->mask_cache = 0xffffffff;
 
