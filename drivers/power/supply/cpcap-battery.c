49d8c198fab51cbe06a6dee313621ada4d4c6590
Tony Lindgren <tony@atomide.com>
2020-12-30 12:19:11 +0200

    power: supply: cpcap: Add missing IRQF_ONESHOT to fix regression
    [ Upstream commit e62333e26be649bfc3c167b9f2bbca38b92332c5 ]
    
    Commit 25d76fed7ffe ("phy: cpcap-usb: Use IRQF_ONESHOT") started causing
    errors loading phy-cpcap-usb driver:
    
    cpcap_battery cpcap_battery.0: failed to register power supply
    genirq: Flags mismatch irq 211. 00002080 (se0conn) vs. 00000080 (se0conn)
    cpcap-usb-phy cpcap-usb-phy.0: could not get irq se0conn: -16
    
    Let's fix this by adding the missing IRQF_ONESHOT to also cpcap-battery
    and cpcap-charger drivers.
    
    Fixes: 25d76fed7ffe ("phy: cpcap-usb: Use IRQF_ONESHOT")
    Reported-by: Merlijn Wajer <merlijn@wizzup.org>
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/power/supply/cpcap-battery.c
+++ drivers/power/supply/cpcap-battery.c
@@ -666,7 +666,7 @@ static int cpcap_battery_init_irq(struct platform_device *pdev,
 
 	error = devm_request_threaded_irq(ddata->dev, irq, NULL,
 					  cpcap_battery_irq_thread,
-					  IRQF_SHARED,
+					  IRQF_SHARED | IRQF_ONESHOT,
 					  name, ddata);
 	if (error) {
 		dev_err(ddata->dev, "could not get irq %s: %i\n",
