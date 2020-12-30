a62529e888faea3cf2ecfea678080bcd301f4e73
Yishai Hadas <yishaih@nvidia.com>
2020-12-30 15:01:19 +0200

    RDMA/mlx5: Use the correct obj_id upon DEVX TIR creation
    [ Upstream commit 8798e4ad0abe0ba1221928a46561981c510be0c6 ]
    
    Use the correct obj_id upon DEVX TIR creation by strictly taking the tirn
    24 bits and not the general obj_id which is 32 bits.
    
    Fixes: 7efce3691d33 ("IB/mlx5: Add obj create and destroy functionality")
    Link: https://lore.kernel.org/r/20201230130121.180350-2-leon@kernel.org
    Signed-off-by: Yishai Hadas <yishaih@nvidia.com>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/infiniband/hw/mlx5/devx.c
+++ drivers/infiniband/hw/mlx5/devx.c
@@ -1067,7 +1067,9 @@ static void devx_obj_build_destroy_cmd(void *in, void *out, void *din,
 		MLX5_SET(general_obj_in_cmd_hdr, din, opcode, MLX5_CMD_OP_DESTROY_RQT);
 		break;
 	case MLX5_CMD_OP_CREATE_TIR:
-		MLX5_SET(general_obj_in_cmd_hdr, din, opcode, MLX5_CMD_OP_DESTROY_TIR);
+		*obj_id = MLX5_GET(create_tir_out, out, tirn);
+		MLX5_SET(destroy_tir_in, din, opcode, MLX5_CMD_OP_DESTROY_TIR);
+		MLX5_SET(destroy_tir_in, din, tirn, *obj_id);
 		break;
 	case MLX5_CMD_OP_CREATE_TIS:
 		MLX5_SET(general_obj_in_cmd_hdr, din, opcode, MLX5_CMD_OP_DESTROY_TIS);
