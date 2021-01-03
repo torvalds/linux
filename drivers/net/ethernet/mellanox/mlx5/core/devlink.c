7ab91f2b03367f9d25dd807ebdfb0d67295e0e41
Shay Drory <shayd@nvidia.com>
2021-01-03 10:09:59 +0200

    net/mlx5: Disallow RoCE on lag device
    In lag mode, setting roce enabled/disable of lag device have no effect.
    e.g.: bond device (roce/vf_lag) roce status remain unchanged.
    Therefore disable it and add an error message.
    
    Fixes: cc9defcbb8fa ("net/mlx5: Handle "enable_roce" devlink param")
    Signed-off-by: Shay Drory <shayd@nvidia.com>
    Reviewed-by: Moshe Shemesh <moshe@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/devlink.c
+++ drivers/net/ethernet/mellanox/mlx5/core/devlink.c
@@ -273,8 +273,8 @@ static int mlx5_devlink_enable_roce_validate(struct devlink *devlink, u32 id,
 		NL_SET_ERR_MSG_MOD(extack, "Device doesn't support RoCE");
 		return -EOPNOTSUPP;
 	}
-	if (mlx5_core_is_mp_slave(dev)) {
-		NL_SET_ERR_MSG_MOD(extack, "Multi port slave device can't configure RoCE");
+	if (mlx5_core_is_mp_slave(dev) || mlx5_lag_is_active(dev)) {
+		NL_SET_ERR_MSG_MOD(extack, "Multi port slave/Lag device can't configure RoCE");
 		return -EOPNOTSUPP;
 	}
 
