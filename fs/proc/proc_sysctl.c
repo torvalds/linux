d8fc9b667d483614974d80f59405f0db4dfc72e2
Masahiro Yamada <masahiroy@kernel.org>
2021-01-04 17:32:21 +0900

    sysctl: use min() helper for namecmp()
    Make it slightly readable by using min().
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>
    Acked-by: Kees Cook <keescook@chromium.org>

--- fs/proc/proc_sysctl.c
+++ fs/proc/proc_sysctl.c
@@ -94,14 +94,9 @@ static void sysctl_print_dir(struct ctl_dir *dir)
 
 static int namecmp(const char *name1, int len1, const char *name2, int len2)
 {
-	int minlen;
 	int cmp;
 
-	minlen = len1;
-	if (minlen > len2)
-		minlen = len2;
-
-	cmp = memcmp(name1, name2, minlen);
+	cmp = memcmp(name1, name2, min(len1, len2));
 	if (cmp == 0)
 		cmp = len1 - len2;
 	return cmp;
