87f3248cdb9aeac35129cb4337ce541a945cb35c
Guo Ren <guoren@linux.alibaba.com>
2021-01-04 03:37:07 +0000

    csky: Reconstruct VDSO framework
    Reconstruct vdso framework to support future vsyscall,
    vgettimeofday features. These are very important features to reduce
    system calls into the kernel for performance improvement.
    
    The patch is reference RISC-V's
    
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>
    Cc: Palmer Dabbelt <palmerdabbelt@google.com>

--- arch/csky/abiv2/inc/abi/vdso.h
+++ arch/csky/abiv2/inc/abi/vdso.h
@@ -3,21 +3,7 @@
 #ifndef __ABI_CSKY_VDSO_H
 #define __ABI_CSKY_VDSO_H
 
-#include <linux/uaccess.h>
+/* movi r7, 173 */
+#define SET_SYSCALL_ID	.long 0x008bea07
 
-static inline int setup_vdso_page(unsigned short *ptr)
-{
-	int err = 0;
-
-	/* movi r7, 173 */
-	err |= __put_user(0xea07, ptr);
-	err |= __put_user(0x008b,      ptr+1);
-
-	/* trap 0 */
-	err |= __put_user(0xc000,   ptr+2);
-	err |= __put_user(0x2020,   ptr+3);
-
-	return err;
-}
-
-#endif /* __ABI_CSKY_STRING_H */
+#endif /* __ABI_CSKY_VDSO_H */
