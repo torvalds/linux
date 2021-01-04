19fe4ac6f0e7163daf9375a4d39947389ae465fa
Chris Wilson <chris@chris-wilson.co.uk>
2021-01-04 20:39:05 +0000

    drm/i915: Disable RPM wakeref assertions during driver shutdown
    As with the regular suspend paths, also disable the wakeref assertions
    as we disable the driver during shutdown.
    
    Reported-by: Hans de Goede <hdegoede@redhat.com>
    Closes: https://gitlab.freedesktop.org/drm/intel/-/issues/2899
    Fixes: fe0f1e3bfdfe ("drm/i915: Shut down displays gracefully on reboot")
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Cc: Hans de Goede <hdegoede@redhat.com>
    Tested-by: Hans de Goede <hdegoede@redhat.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210104203905.19248-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -1046,6 +1046,8 @@ static void intel_shutdown_encoders(struct drm_i915_private *dev_priv)
 
 void i915_driver_shutdown(struct drm_i915_private *i915)
 {
+	disable_rpm_wakeref_asserts(&i915->runtime_pm);
+
 	i915_gem_suspend(i915);
 
 	drm_kms_helper_poll_disable(&i915->drm);
@@ -1059,6 +1061,8 @@ void i915_driver_shutdown(struct drm_i915_private *i915)
 
 	intel_suspend_encoders(i915);
 	intel_shutdown_encoders(i915);
+
+	enable_rpm_wakeref_asserts(&i915->runtime_pm);
 }
 
 static bool suspend_to_idle(struct drm_i915_private *dev_priv)
