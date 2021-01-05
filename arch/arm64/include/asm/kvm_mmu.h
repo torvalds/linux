6ec6259d7084ed32e164c9f7b69049464dd90fa5
David Brazdil <dbrazdil@google.com>
2021-01-05 18:05:38 +0000

    KVM: arm64: Apply hyp relocations at runtime
    KVM nVHE code runs under a different VA mapping than the kernel, hence
    so far it avoided using absolute addressing because the VA in a constant
    pool is relocated by the linker to a kernel VA (see hyp_symbol_addr).
    
    Now the kernel has access to a list of positions that contain a kimg VA
    but will be accessed only in hyp execution context. These are generated
    by the gen-hyprel build-time tool and stored in .hyp.reloc.
    
    Add early boot pass over the entries and convert the kimg VAs to hyp VAs.
    Note that this requires for .hyp* ELF sections to be mapped read-write
    at that point.
    
    Signed-off-by: David Brazdil <dbrazdil@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20210105180541.65031-6-dbrazdil@google.com

--- arch/arm64/include/asm/kvm_mmu.h
+++ arch/arm64/include/asm/kvm_mmu.h
@@ -129,6 +129,7 @@ alternative_cb_end
 void kvm_update_va_mask(struct alt_instr *alt,
 			__le32 *origptr, __le32 *updptr, int nr_inst);
 void kvm_compute_layout(void);
+void kvm_apply_hyp_relocations(void);
 
 static __always_inline unsigned long __kern_hyp_va(unsigned long v)
 {
