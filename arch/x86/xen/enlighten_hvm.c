b36b0fe96af13460278bf9b173beced1bd15f85d
David Woodhouse <dwmw@amazon.co.uk>
2021-01-06 15:39:56 +0000

    x86/xen: Add xen_no_vector_callback option to test PCI INTX delivery
    It's useful to be able to test non-vector event channel delivery, to make
    sure Linux will work properly on older Xen which doesn't have it.
    
    It's also useful for those working on Xen and Xen-compatible hypervisors,
    because there are guest kernels still in active use which use PCI INTX
    even when vector delivery is available.
    
    Signed-off-by: David Woodhouse <dwmw@amazon.co.uk>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Link: https://lore.kernel.org/r/20210106153958.584169-4-dwmw2@infradead.org
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- arch/x86/xen/enlighten_hvm.c
+++ arch/x86/xen/enlighten_hvm.c
@@ -188,6 +188,8 @@ static int xen_cpu_dead_hvm(unsigned int cpu)
        return 0;
 }
 
+static bool no_vector_callback __initdata;
+
 static void __init xen_hvm_guest_init(void)
 {
 	if (xen_pv_domain())
@@ -207,7 +209,7 @@ static void __init xen_hvm_guest_init(void)
 
 	xen_panic_handler_init();
 
-	if (xen_feature(XENFEAT_hvm_callback_vector))
+	if (!no_vector_callback && xen_feature(XENFEAT_hvm_callback_vector))
 		xen_have_vector_callback = 1;
 
 	xen_hvm_smp_init();
@@ -232,6 +234,13 @@ static __init int xen_parse_nopv(char *arg)
 	return 0;
 }
 early_param("xen_nopv", xen_parse_nopv);
+
+static __init int xen_parse_no_vector_callback(char *arg)
+{
+	no_vector_callback = true;
+	return 0;
+}
+early_param("xen_no_vector_callback", xen_parse_no_vector_callback);
 
 bool __init xen_hvm_need_lapic(void)
 {
