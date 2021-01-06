4621dc6a5bf1235249e92231db30c96dfd1a18b9
David Woodhouse <dwmw@amazon.co.uk>
2021-01-06 15:39:57 +0000

    x86/xen: Don't register Xen IPIs when they aren't going to be used
    In the case where xen_have_vector_callback is false, we still register
    the IPI vectors in xen_smp_intr_init() for the secondary CPUs even
    though they aren't going to be used. Stop doing that.
    
    Signed-off-by: David Woodhouse <dwmw@amazon.co.uk>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Link: https://lore.kernel.org/r/20210106153958.584169-5-dwmw2@infradead.org
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- arch/x86/xen/enlighten_hvm.c
+++ arch/x86/xen/enlighten_hvm.c
@@ -164,10 +164,10 @@ static int xen_cpu_up_prepare_hvm(unsigned int cpu)
 	else
 		per_cpu(xen_vcpu_id, cpu) = cpu;
 	rc = xen_vcpu_setup(cpu);
-	if (rc)
+	if (rc || !xen_have_vector_callback)
 		return rc;
 
-	if (xen_have_vector_callback && xen_feature(XENFEAT_hvm_safe_pvclock))
+	if (xen_feature(XENFEAT_hvm_safe_pvclock))
 		xen_setup_timer(cpu);
 
 	rc = xen_smp_intr_init(cpu);
