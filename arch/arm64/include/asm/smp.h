8f15907b7461c43986a0fa2f21ce4a381a3ae1f0
Prateek Sood <prsood@codeaurora.org>
2021-01-06 21:18:49 +0530

    ANDROID: ipi: Add function to return nr_ipi and ipi_desc
    For implementation of IPI stats, nr_ipi and ipi_desc are useful.
    This information helps in getting per cpu IPI count and total
    IPI count from vendor module.
    
    Presently, there are no helper functions to get nr_ipi and
    ipi_desc on vendor side. And if we do with a new exported symbol
    that does kallsyms_lookup_name on each static variable takes
    more than 80ms during boot up or for the first access of each
    static variable is an overhead.
    
    So lets add helper functions to return nr_ipi and ipi_desc
    value to get things done in an efficient way.
    
    Bug: 177393446
    Change-Id: I698dbbb0301d4f1529ef7484f25fe7540378008c
    Signed-off-by: Prateek Sood <prsood@codeaurora.org>
    Signed-off-by: Mukesh Ojha <mojha@codeaurora.org>

--- arch/arm64/include/asm/smp.h
+++ arch/arm64/include/asm/smp.h
@@ -89,6 +89,8 @@ extern void secondary_entry(void);
 
 extern void arch_send_call_function_single_ipi(int cpu);
 extern void arch_send_call_function_ipi_mask(const struct cpumask *mask);
+extern int nr_ipi_get(void);
+extern struct irq_desc **ipi_desc_get(void);
 
 #ifdef CONFIG_ARM64_ACPI_PARKING_PROTOCOL
 extern void arch_send_wakeup_ipi_mask(const struct cpumask *mask);
