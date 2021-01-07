cc3456226176385aed8aa6ebb021ebb1380a0183
Israel Rukshin <israelr@nvidia.com>
2021-01-07 17:34:13 +0200

    nvmet: Use nvmet_is_port_enabled helper for pi_enable
    Remove code duplication.
    
    Signed-off-by: Israel Rukshin <israelr@nvidia.com>
    Reviewed-by: Max Gurtovoy <mgurtovoy@nvidia.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/nvme/target/configfs.c
+++ drivers/nvme/target/configfs.c
@@ -266,10 +266,8 @@ static ssize_t nvmet_param_pi_enable_store(struct config_item *item,
 	if (strtobool(page, &val))
 		return -EINVAL;
 
-	if (port->enabled) {
-		pr_err("Disable port before setting pi_enable value.\n");
+	if (nvmet_is_port_enabled(port, __func__))
 		return -EACCES;
-	}
 
 	port->pi_enable = val;
 	return count;
