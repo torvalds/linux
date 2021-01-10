3188677d4901fbfb7a363ab2558e0a4bc76deecb
Linus Walleij <linus.walleij@linaro.org>
2021-01-10 15:01:59 +0100

    power: supply: max8903: Absorb pdata header
    The platform data header is not included by any other file in
    the kernel but the driver itself. Decomission the stand-alone
    header and absorb it into the driver itself.
    
    Cc: Chris Lapa <chris@lapa.com.au>
    Cc: MyungJoo Ham <myungjoo.ham@samsung.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>

--- drivers/power/supply/max8903_charger.c
+++ drivers/power/supply/max8903_charger.c
@@ -15,7 +15,37 @@
 #include <linux/slab.h>
 #include <linux/power_supply.h>
 #include <linux/platform_device.h>
-#include <linux/power/max8903_charger.h>
+
+struct max8903_pdata {
+	/*
+	 * GPIOs
+	 * cen, chg, flt, dcm and usus are optional.
+	 * dok and uok are not optional depending on the status of
+	 * dc_valid and usb_valid.
+	 */
+	int cen;	/* Charger Enable input */
+	int dok;	/* DC(Adapter) Power OK output */
+	int uok;	/* USB Power OK output */
+	int chg;	/* Charger status output */
+	int flt;	/* Fault output */
+	int dcm;	/* Current-Limit Mode input (1: DC, 2: USB) */
+	int usus;	/* USB Suspend Input (1: suspended) */
+
+	/*
+	 * DC(Adapter/TA) is wired
+	 * When dc_valid is true,
+	 *	dok should be valid.
+	 *
+	 * At least one of dc_valid or usb_valid should be true.
+	 */
+	bool dc_valid;
+	/*
+	 * USB is wired
+	 * When usb_valid is true,
+	 *	uok should be valid.
+	 */
+	bool usb_valid;
+};
 
 struct max8903_data {
 	struct max8903_pdata *pdata;
