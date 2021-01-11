7445a59eed931678be335b92cb183af57fa42772
Jan Kara <jack@suse.cz>
2021-01-11 17:47:16 +0100

    FROMGIT: Revert "blk-mq, elevator: Count requests per hctx to improve performance"
    This reverts commit b445547ec1bbd3e7bf4b1c142550942f70527d95.
    
    Since both mq-deadline and BFQ completely ignore hctx they are passed to
    their dispatch function and dispatch whatever request they deem fit
    checking whether any request for a particular hctx is queued is just
    pointless since we'll very likely get a request from a different hctx
    anyway. In the following commit we'll deal with lock contention in these
    IO schedulers in presence of multiple HW queues in a different way.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Change-Id: Ibd7dbe69ae1799f2efce5788986e2f1aad88f66d
    BUG: 187357408
    (cherry picked from commit 2490aeca0081bb168e96fb7b1746d676be84369f git://git.kernel.dk/linux-block/ for-5.14/block)
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -4640,9 +4640,6 @@ static bool bfq_has_work(struct blk_mq_hw_ctx *hctx)
 {
 	struct bfq_data *bfqd = hctx->queue->elevator->elevator_data;
 
-	if (!atomic_read(&hctx->elevator_queued))
-		return false;
-
 	/*
 	 * Avoiding lock: a race on bfqd->busy_queues should cause at
 	 * most a call to dispatch for nothing
@@ -5557,7 +5554,6 @@ static void bfq_insert_requests(struct blk_mq_hw_ctx *hctx,
 		rq = list_first_entry(list, struct request, queuelist);
 		list_del_init(&rq->queuelist);
 		bfq_insert_request(hctx, rq, at_head);
-		atomic_inc(&hctx->elevator_queued);
 	}
 }
 
@@ -5925,7 +5921,6 @@ static void bfq_finish_requeue_request(struct request *rq)
 
 		bfq_completed_request(bfqq, bfqd);
 		bfq_finish_requeue_request_body(bfqq);
-		atomic_dec(&rq->mq_hctx->elevator_queued);
 
 		spin_unlock_irqrestore(&bfqd->lock, flags);
 	} else {
