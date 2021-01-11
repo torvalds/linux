665fe4dce83d14177f46fac814964ec107f196b5
Jiansong Chen <Jiansong.Chen@amd.com>
2021-01-11 17:41:25 +0800

    drm/amdgpu: enable gpu recovery for navy_flounder
    Enable gpu recovery for navy_flounder by default to trigger
    reset once needed.
    
    Signed-off-by: Jiansong Chen <Jiansong.Chen@amd.com>
    Reviewed-by: Tao Zhou <tao.zhou1@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -4206,6 +4206,7 @@ bool amdgpu_device_should_recover_gpu(struct amdgpu_device *adev)
 		case CHIP_NAVI14:
 		case CHIP_NAVI12:
 		case CHIP_SIENNA_CICHLID:
+		case CHIP_NAVY_FLOUNDER:
 			break;
 		default:
 			goto disabled;
