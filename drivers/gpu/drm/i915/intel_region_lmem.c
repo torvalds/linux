0dbfc194355593406bad809d58178c0742f99013
Jani Nikula <jani.nikula@intel.com>
2021-01-12 19:22:46 +0200

    drm/i915/lmem: make intel_region_lmem_ops static
    There are no users outside of intel_region_lmem.c.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210112172246.11933-1-jani.nikula@intel.com

--- drivers/gpu/drm/i915/intel_region_lmem.c
+++ drivers/gpu/drm/i915/intel_region_lmem.c
@@ -95,7 +95,7 @@ region_lmem_init(struct intel_memory_region *mem)
 	return ret;
 }
 
-const struct intel_memory_region_ops intel_region_lmem_ops = {
+static const struct intel_memory_region_ops intel_region_lmem_ops = {
 	.init = region_lmem_init,
 	.release = region_lmem_release,
 	.create_object = __i915_gem_lmem_object_create,
