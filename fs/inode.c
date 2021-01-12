e20b14db050ae25ef7c5a2d9a41dd62b7e5e75c1
Eric Biggers <ebiggers@google.com>
2021-01-12 11:02:45 -0800

    fs: only specify I_DIRTY_TIME when needed in generic_update_time()
    generic_update_time() always passes I_DIRTY_TIME to
    __mark_inode_dirty(), which doesn't really make sense because (a)
    generic_update_time() might be asked to do only an i_version update, not
    also a timestamps update; and (b) I_DIRTY_TIME is only supposed to be
    set in i_state if the filesystem has lazytime enabled, so using it
    unconditionally in generic_update_time() is inconsistent.
    
    As a result there is a weird edge case where if only an i_version update
    was requested (not also a timestamps update) but it is no longer needed
    (i.e. inode_maybe_inc_iversion() returns false), then I_DIRTY_TIME will
    be set in i_state even if the filesystem isn't mounted with lazytime.
    
    Fix this by only passing I_DIRTY_TIME to __mark_inode_dirty() if the
    timestamps were updated and the filesystem has lazytime enabled.
    
    Link: https://lore.kernel.org/r/20210112190253.64307-4-ebiggers@kernel.org
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Jan Kara <jack@suse.cz>

--- fs/inode.c
+++ fs/inode.c
@@ -1743,24 +1743,26 @@ static int relatime_need_update(struct vfsmount *mnt, struct inode *inode,
 
 int generic_update_time(struct inode *inode, struct timespec64 *time, int flags)
 {
-	int iflags = I_DIRTY_TIME;
-	bool dirty = false;
+	int dirty_flags = 0;
 
-	if (flags & S_ATIME)
-		inode->i_atime = *time;
-	if (flags & S_VERSION)
-		dirty = inode_maybe_inc_iversion(inode, false);
-	if (flags & S_CTIME)
-		inode->i_ctime = *time;
-	if (flags & S_MTIME)
-		inode->i_mtime = *time;
-	if ((flags & (S_ATIME | S_CTIME | S_MTIME)) &&
-	    !(inode->i_sb->s_flags & SB_LAZYTIME))
-		dirty = true;
+	if (flags & (S_ATIME | S_CTIME | S_MTIME)) {
+		if (flags & S_ATIME)
+			inode->i_atime = *time;
+		if (flags & S_CTIME)
+			inode->i_ctime = *time;
+		if (flags & S_MTIME)
+			inode->i_mtime = *time;
 
-	if (dirty)
-		iflags |= I_DIRTY_SYNC;
-	__mark_inode_dirty(inode, iflags);
+		if (inode->i_sb->s_flags & SB_LAZYTIME)
+			dirty_flags |= I_DIRTY_TIME;
+		else
+			dirty_flags |= I_DIRTY_SYNC;
+	}
+
+	if ((flags & S_VERSION) && inode_maybe_inc_iversion(inode, false))
+		dirty_flags |= I_DIRTY_SYNC;
+
+	__mark_inode_dirty(inode, dirty_flags);
 	return 0;
 }
 EXPORT_SYMBOL(generic_update_time);
