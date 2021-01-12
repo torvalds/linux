aa9223a602544fea033b63e1b1d6c8d9eaa4ed43
AngeloGioacchino Del Regno <angelogioacchino.delregno@somainline.org>
2021-01-12 20:26:30 +0100

    drm/msm/dpu: Disable autorefresh in command mode
    When a command mode display is used, it may be retaining the bootloader
    configuration which, in most of the cases, enables the autorefresh
    feature in order to keep the splash up.
    
    Since there is no autorefresh management in this driver, wire up the
    autorefresh ops in the dpu_hw_pingpong and disable the feature when
    preparing for cmd commit: instead of disabling it when initializing
    the command mode, this road was chosen as to open future possibility
    of enabling and managing the autorefresh feature in the driver.
    
    Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@somainline.org>
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_hw_pingpong.h
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_hw_pingpong.h
@@ -63,6 +63,8 @@ struct dpu_hw_dither_cfg {
  *  @setup_tearcheck : program tear check values
  *  @enable_tearcheck : enables tear check
  *  @get_vsync_info : retries timing info of the panel
+ *  @setup_autorefresh : configure and enable the autorefresh config
+ *  @get_autorefresh : retrieve autorefresh config from hardware
  *  @setup_dither : function to program the dither hw block
  *  @get_line_count: obtain current vertical line counter
  */
@@ -93,6 +95,18 @@ struct dpu_hw_pingpong_ops {
 	 */
 	int (*get_vsync_info)(struct dpu_hw_pingpong *pp,
 			struct dpu_hw_pp_vsync_info  *info);
+
+	/**
+	 * configure and enable the autorefresh config
+	 */
+	void (*setup_autorefresh)(struct dpu_hw_pingpong *pp,
+				  u32 frame_count, bool enable);
+
+	/**
+	 * retrieve autorefresh config from hardware
+	 */
+	bool (*get_autorefresh)(struct dpu_hw_pingpong *pp,
+				u32 *frame_count);
 
 	/**
 	 * poll until write pointer transmission starts
