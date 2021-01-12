1c2cdf0b603a3b0c763288ad92e9f3f1555925cf
Aya Levin <ayal@nvidia.com>
2021-01-12 13:34:29 +0200

    net/mlx5e: Set PTP channel pointer explicitly to NULL
    When closing the PTP channel, set its pointer explicitly to NULL. PTP
    channel is opened on demand, the code verify the pointer validity before
    access. Nullify it when closing the PTP channel to avoid unexpected
    behavior.
    
    Fixes: 145e5637d941 ("net/mlx5e: Add TX PTP port object support")
    Signed-off-by: Aya Levin <ayal@nvidia.com>
    Reviewed-by: Tariq Toukan <tariqt@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -2503,8 +2503,10 @@ void mlx5e_close_channels(struct mlx5e_channels *chs)
 {
 	int i;
 
-	if (chs->port_ptp)
+	if (chs->port_ptp) {
 		mlx5e_port_ptp_close(chs->port_ptp);
+		chs->port_ptp = NULL;
+	}
 
 	for (i = 0; i < chs->num; i++)
 		mlx5e_close_channel(chs->c[i]);
