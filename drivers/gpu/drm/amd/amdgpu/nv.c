b7dd14c730e490cb86c760f4dd44b9c7721390ff
Aaron Liu <aaron.liu@amd.com>
2021-01-12 16:48:09 +0800

    drm/amdgpu: add ATHUB Clock Gating support for yellow carp
    ATHUB MGCG/MGLS is enabled by default.
    Adding ATHUB MGCG/MGLS flag to ensure athub mgcg/ls enabled.
    
    Signed-off-by: Aaron Liu <aaron.liu@amd.com>
    Reviewed-by: Huang Rui <ray.huang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/nv.c
+++ drivers/gpu/drm/amd/amdgpu/nv.c
@@ -1338,7 +1338,9 @@ static int nv_common_early_init(void *handle)
 			AMD_CG_SUPPORT_MC_LS |
 			AMD_CG_SUPPORT_SDMA_LS |
 			AMD_CG_SUPPORT_HDP_MGCG |
-			AMD_CG_SUPPORT_HDP_LS;
+			AMD_CG_SUPPORT_HDP_LS |
+			AMD_CG_SUPPORT_ATHUB_MGCG |
+			AMD_CG_SUPPORT_ATHUB_LS;
 		adev->pg_flags = AMD_PG_SUPPORT_GFX_PG;
 		adev->external_rev_id = adev->rev_id + 0x01;
 		break;
