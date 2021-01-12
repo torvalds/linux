8e482856b5b9efc79e9a1bc11d67f2975c36777a
Nick Desaulniers <ndesaulniers@google.com>
2021-01-11 16:04:01 -0800

    Revert "ANDROID: arm64: lse: fix LSE atomics with LTO"
    This reverts commit 3bd6dd6a79706abe1bef1fd7d8bf3b04e530ef17.
    
    Fixed upstream by
    commit e0d5896bd356 ("arm64: lse: fix LSE atomics with LLVM's integrated assembler")
    which landed in v4.19.164.
    
    Bug: 117299373
    Bug: 133186739
    Bug: 141693040
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    Change-Id: I1495c03a1512a7f9c3da9d5e4517619c2291dba8

--- arch/arm64/include/asm/lse.h
+++ arch/arm64/include/asm/lse.h
@@ -22,13 +22,6 @@
 
 #else	/* __ASSEMBLER__ */
 
-#ifdef CONFIG_LTO_CLANG
-#define __LSE_PREAMBLE	".arch_extension lse\n"
-#else
-__asm__(".arch_extension	lse");
-#define __LSE_PREAMBLE
-#endif
-
 /* Move the ll/sc atomics out-of-line */
 #define __LL_SC_INLINE		notrace
 #define __LL_SC_PREFIX(x)	__ll_sc_##x
