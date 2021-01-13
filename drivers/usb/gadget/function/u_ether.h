a884cb5580a2060b32a7d93fada5a4e4de5da490
Lorenzo Colitti <lorenzo@google.com>
2021-01-14 08:42:22 +0900

    FROMGIT: usb: gadget: u_ether: support configuring interface names.
    This patch allows the administrator to configure the interface
    name of a function using u_ether (e.g., eem, ncm, rndis).
    
    Currently, all such interfaces, regardless of function type, are
    always called usb0, usb1, etc. This makes it very cumbersome to
    use more than one such type at a time, because userspace cannnot
    easily tell the interfaces apart and apply the right
    configuration to each one. Interface renaming in userspace based
    on driver doesn't help, because the interfaces all have the same
    driver. Without this patch, doing this require hacks/workarounds
    such as setting fixed MAC addresses on the functions, and then
    renaming by MAC address, or scraping configfs after each
    interface is created to find out what it is.
    
    Setting the interface name is done by writing to the same
    "ifname" configfs attribute that reports the interface name after
    the function is bound. The write must contain an interface
    pattern such as "usb%d" (which will cause the net core to pick
    the next available interface name starting with "usb").
    This patch does not allow writing an exact interface name (as
    opposed to a pattern) because if the interface already exists at
    bind time, the bind will fail and the whole gadget will fail to
    activate. This could be allowed in a future patch.
    
    For compatibility with current userspace, when reading an ifname
    that has not currently been set, the result is still "(unnamed
    net_device)". Once a write to ifname happens, then reading ifname
    will return whatever was last written.
    
    Tested by configuring an rndis function and an ncm function on
    the same gadget, and writing "rndis%d" to ifname on the rndis
    function and "ncm%d" to ifname on the ncm function. When the
    gadget was bound, the rndis interface was rndis0 and the ncm
    interface was ncm0.
    
    Signed-off-by: Lorenzo Colitti <lorenzo@google.com>
    (cherry picked from commit 63d152149b2d0860ccf8c4e6596b6175b2b7ace6
     https://git.kernel.org/pub/scm/linux/kernel/git/gregkh/usb.git usb-next)
    Link: https://lore.kernel.org/r/20210113234222.3272933-1-lorenzo@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lorenzo Colitti <lorenzo@google.com>
    Change-Id: I04deb6cc1d8a5b8ee82404940de2a79c06fbafe7
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/usb/gadget/function/u_ether.h
+++ drivers/usb/gadget/function/u_ether.h
@@ -244,6 +244,18 @@ unsigned gether_get_qmult(struct net_device *net);
  */
 int gether_get_ifname(struct net_device *net, char *name, int len);
 
+/**
+ * gether_set_ifname - set an ethernet-over-usb link interface name
+ * @net: device representing this link
+ * @name: new interface name
+ * @len: length of @name
+ *
+ * This sets the interface name of this ethernet-over-usb link.
+ * A single terminating newline, if any, is ignored.
+ * Returns zero on success, else negative errno.
+ */
+int gether_set_ifname(struct net_device *net, const char *name, int len);
+
 void gether_cleanup(struct eth_dev *dev);
 
 /* connect/disconnect is handled by individual functions */
