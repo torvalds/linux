e634ae98b807dd591a6c904c293df4285752f0a4
Chunfeng Yun <chunfeng.yun@mediatek.com>
2021-01-13 10:42:19 +0800

    usb: gadget: bdc: remove bdc_ep_set_halt() declaration
    No definition for bdc_ep_set_halt(), so remove it.
    
    Cc: Florian Fainelli <f.fainelli@gmail.com>
    Acked-by: Florian Fainelli <f.fainelli@gmail.com>
    Acked-by: Felipe Balbi <balbi@kernel.org>
    Signed-off-by: Chunfeng Yun <chunfeng.yun@mediatek.com>
    Link: https://lore.kernel.org/r/1610505748-30616-2-git-send-email-chunfeng.yun@mediatek.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/gadget/udc/bdc/bdc_cmd.h
+++ drivers/usb/gadget/udc/bdc/bdc_cmd.h
@@ -16,7 +16,6 @@ int bdc_dconfig_ep(struct bdc *, struct bdc_ep *);
 int bdc_stop_ep(struct bdc *, int);
 int bdc_ep_set_stall(struct bdc *, int);
 int bdc_ep_clear_stall(struct bdc *, int);
-int bdc_ep_set_halt(struct bdc_ep *, u32 , int);
 int bdc_ep_bla(struct bdc *, struct bdc_ep *, dma_addr_t);
 int bdc_function_wake(struct bdc*, u8);
 int bdc_function_wake_fh(struct bdc*, u8);
