1eb8f690bcb565a6600f8b6dcc78f7b239ceba17
Borislav Petkov <bp@suse.de>
2021-01-14 10:36:59 +0100

    x86/topology: Make __max_die_per_package available unconditionally
    Move it outside of CONFIG_SMP in order to avoid ifdeffery at the usage
    sites.
    
    Fixes: 76e2fc63ca40 ("x86/cpu/amd: Set __max_die_per_package on AMD")
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lkml.kernel.org/r/20210114111814.5346-1-bp@alien8.de

--- arch/x86/kernel/cpu/topology.c
+++ arch/x86/kernel/cpu/topology.c
@@ -25,10 +25,10 @@
 #define BITS_SHIFT_NEXT_LEVEL(eax)	((eax) & 0x1f)
 #define LEVEL_MAX_SIBLINGS(ebx)		((ebx) & 0xffff)
 
-#ifdef CONFIG_SMP
 unsigned int __max_die_per_package __read_mostly = 1;
 EXPORT_SYMBOL(__max_die_per_package);
 
+#ifdef CONFIG_SMP
 /*
  * Check if given CPUID extended toplogy "leaf" is implemented
  */
