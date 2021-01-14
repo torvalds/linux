7a3c90df20db037db978418925d0c30aa105c2d6
Viresh Kumar <viresh.kumar@linaro.org>
2021-01-14 17:05:23 +0530

    arch: powerpc: Stop building and using oprofile
    The "oprofile" user-space tools don't use the kernel OPROFILE support
    any more, and haven't in a long time. User-space has been converted to
    the perf interfaces.
    
    This commits stops building oprofile for powerpc and removes any
    reference to it from directories in arch/powerpc/ apart from
    arch/powerpc/oprofile, which will be removed in the next commit (this is
    broken into two commits as the size of the commit became very big, ~5k
    lines).
    
    Note that the member "oprofile_cpu_type" in "struct cpu_spec" isn't
    removed as it was also used by other parts of the code.
    
    Suggested-by: Christoph Hellwig <hch@infradead.org>
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Robert Richter <rric@kernel.org>
    Acked-by: William Cohen <wcohen@redhat.com>
    Acked-by: Al Viro <viro@zeniv.linux.org.uk>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/powerpc/platforms/cell/spufs/spufs.h
+++ arch/powerpc/platforms/cell/spufs/spufs.h
@@ -281,7 +281,6 @@ void spu_del_from_rq(struct spu_context *ctx);
 int spu_activate(struct spu_context *ctx, unsigned long flags);
 void spu_deactivate(struct spu_context *ctx);
 void spu_yield(struct spu_context *ctx);
-void spu_switch_notify(struct spu *spu, struct spu_context *ctx);
 void spu_switch_log_notify(struct spu *spu, struct spu_context *ctx,
 		u32 type, u32 val);
 void spu_set_timeslice(struct spu_context *ctx);
