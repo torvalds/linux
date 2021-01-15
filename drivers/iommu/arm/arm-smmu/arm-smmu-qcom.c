d8498b1e4ecc75d023c59acb30e15e3d8d8f07ea
Vinod Koul <vkoul@kernel.org>
2021-01-15 14:33:22 +0530

    iommu: arm-smmu-impl: Add SM8350 qcom iommu implementation
    Add SM8350 qcom iommu implementation to the table of
    qcom_smmu_impl_of_match table which brings in iommu support for SM8350
    SoC
    
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Link: https://lore.kernel.org/r/20210115090322.2287538-2-vkoul@kernel.org
    Signed-off-by: Will Deacon <will@kernel.org>

--- drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
@@ -333,6 +333,7 @@ static const struct of_device_id __maybe_unused qcom_smmu_impl_of_match[] = {
 	{ .compatible = "qcom,sdm845-smmu-500" },
 	{ .compatible = "qcom,sm8150-smmu-500" },
 	{ .compatible = "qcom,sm8250-smmu-500" },
+	{ .compatible = "qcom,sm8350-smmu-500" },
 	{ }
 };
 
