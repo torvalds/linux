ad1cc6b75a79471d14b64c32bf13067659ba2bac
Martin Kaiser <martin@kaiser.cx>
2021-01-15 22:24:34 +0100

    PCI: dwc: Remove IRQ handler and data in one go
    Call irq_set_chained_handler_and_data() to clear the chained handler
    and the handler's data under irq_desc->lock.
    
    See also 2cf5a03cb29d ("PCI/keystone: Fix race in installing chained
    IRQ handler").
    
    Link: https://lore.kernel.org/r/20210115212435.19940-2-martin@kaiser.cx
    Signed-off-by: Martin Kaiser <martin@kaiser.cx>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>

--- drivers/pci/controller/dwc/pcie-designware-host.c
+++ drivers/pci/controller/dwc/pcie-designware-host.c
@@ -258,10 +258,8 @@ int dw_pcie_allocate_domains(struct pcie_port *pp)
 
 static void dw_pcie_free_msi(struct pcie_port *pp)
 {
-	if (pp->msi_irq) {
-		irq_set_chained_handler(pp->msi_irq, NULL);
-		irq_set_handler_data(pp->msi_irq, NULL);
-	}
+	if (pp->msi_irq)
+		irq_set_chained_handler_and_data(pp->msi_irq, NULL, NULL);
 
 	irq_domain_remove(pp->msi_domain);
 	irq_domain_remove(pp->irq_domain);
