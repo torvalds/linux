1c6d1c4f6874345a31d12a9f3843876c49d637fa
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2021-01-15 13:54:33 +0900

    ASoC: da*: sync parameter naming (rate/sample_bits)
    This patch syncs naming rule.
    
     - xxx_rates;
     + xxx_rate;
    
     - xxx_samplebits;
     + xxx_sample_bits;
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://lore.kernel.org/r/875z3yolgf.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/da7218.c
+++ sound/soc/codecs/da7218.c
@@ -2194,9 +2194,9 @@ static struct snd_soc_dai_driver da7218_dai = {
 		.formats = DA7218_FORMATS,
 	},
 	.ops = &da7218_dai_ops,
-	.symmetric_rates = 1,
+	.symmetric_rate = 1,
 	.symmetric_channels = 1,
-	.symmetric_samplebits = 1,
+	.symmetric_sample_bits = 1,
 };
 
 
