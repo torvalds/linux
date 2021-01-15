df1d4b466bb6a4eccb899ab761f5fbc7b3f95b67
Bob Moore <robert.moore@intel.com>
2021-01-15 10:48:26 -0800

    ACPICA: Update version to 20210105
    ACPICA commit 28cb42013541950cf378582a5a5a5587061498ca
    
    Version 20210105.
    
    Link: https://github.com/acpica/acpica/commit/28cb4201
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Erik Kaneda <erik.kaneda@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/acpixf.h
+++ include/acpi/acpixf.h
@@ -12,7 +12,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20201217
+#define ACPI_CA_VERSION                 0x20210105
 
 #include <acpi/acconfig.h>
 #include <acpi/actypes.h>
