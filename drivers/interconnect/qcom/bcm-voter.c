6d75baa3d23e38ef159ab96080da1e240345d026
Mike Tipton <mdtipton@codeaurora.org>
2021-01-15 17:43:20 -0800

    interconnect: qcom: Add support for mask-based BCMs
    Some BCMs aren't directly associated with the data path (i.e. ACV) and
    therefore don't communicate using BW. Instead, they are simply
    enabled/disabled with a simple bit mask. Add support for these.
    
    Change-Id: I9b73e0ce05a09338d22571ff311c5b058fb8e1b2
    Signed-off-by: Mike Tipton <mdtipton@codeaurora.org>

--- drivers/interconnect/qcom/bcm-voter.c
+++ drivers/interconnect/qcom/bcm-voter.c
@@ -85,6 +85,11 @@ static void bcm_aggregate(struct qcom_icc_bcm *bcm, bool init)
 
 		temp = agg_peak[bucket] * bcm->vote_scale;
 		bcm->vote_y[bucket] = bcm_div(temp, bcm->aux_data.unit);
+
+		if (bcm->enable_mask && (bcm->vote_x[bucket] || bcm->vote_y[bucket])) {
+			bcm->vote_x[bucket] = 0;
+			bcm->vote_y[bucket] = bcm->enable_mask;
+		}
 	}
 
 	if (bcm->keepalive) {
