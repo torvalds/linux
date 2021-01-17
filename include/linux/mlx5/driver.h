34b0c04c884cbcb327535e55279708e5ca5b96ed
Parav Pandit <parav@nvidia.com>
2021-01-17 11:26:33 +0200

    Revert "RDMA/mlx5: Fix devlink deadlock on net namespace deletion"
    commit de641d74fb00f5b32f054ee154e31fb037e0db88 upstream.
    
    This reverts commit fbdd0049d98d44914fc57d4b91f867f4996c787b.
    
    Due to commit in fixes tag, netdevice events were received only in one net
    namespace of mlx5_core_dev. Due to this when netdevice events arrive in
    net namespace other than net namespace of mlx5_core_dev, they are missed.
    
    This results in empty GID table due to RDMA device being detached from its
    net device.
    
    Hence, revert back to receive netdevice events in all net namespaces to
    restore back RDMA functionality in non init_net net namespace. The
    deadlock will have to be addressed in another patch.
    
    Fixes: fbdd0049d98d ("RDMA/mlx5: Fix devlink deadlock on net namespace deletion")
    Link: https://lore.kernel.org/r/20210117092633.10690-1-leon@kernel.org
    Signed-off-by: Parav Pandit <parav@nvidia.com>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/mlx5/driver.h
+++ include/linux/mlx5/driver.h
@@ -1213,22 +1213,4 @@ static inline bool mlx5_is_roce_enabled(struct mlx5_core_dev *dev)
 	return val.vbool;
 }
 
-/**
- * mlx5_core_net - Provide net namespace of the mlx5_core_dev
- * @dev: mlx5 core device
- *
- * mlx5_core_net() returns the net namespace of mlx5 core device.
- * This can be called only in below described limited context.
- * (a) When a devlink instance for mlx5_core is registered and
- *     when devlink reload operation is disabled.
- *     or
- * (b) during devlink reload reload_down() and reload_up callbacks
- *     where it is ensured that devlink instance's net namespace is
- *     stable.
- */
-static inline struct net *mlx5_core_net(struct mlx5_core_dev *dev)
-{
-	return devlink_net(priv_to_devlink(dev));
-}
-
 #endif /* MLX5_DRIVER_H */
