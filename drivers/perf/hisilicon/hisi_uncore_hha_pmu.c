c2c4d5c051b23dd79ff82d6232347245e11d7c9c
Rikard Falkeborn <rikard.falkeborn@gmail.com>
2021-01-17 22:28:46 +0100

    perf: hisi: Constify static struct attribute_group
    The only usage is to put their addresses in an array of pointers to
    const struct attribute group. Make them const to allow the compiler
    to put them in read-only memory.
    
    Signed-off-by: Rikard Falkeborn <rikard.falkeborn@gmail.com>
    Link: https://lore.kernel.org/r/20210117212847.21319-4-rikard.falkeborn@gmail.com
    Signed-off-by: Will Deacon <will@kernel.org>

--- drivers/perf/hisilicon/hisi_uncore_hha_pmu.c
+++ drivers/perf/hisilicon/hisi_uncore_hha_pmu.c
@@ -331,7 +331,7 @@ static struct attribute *hisi_hha_pmu_identifier_attrs[] = {
 	NULL
 };
 
-static struct attribute_group hisi_hha_pmu_identifier_group = {
+static const struct attribute_group hisi_hha_pmu_identifier_group = {
 	.attrs = hisi_hha_pmu_identifier_attrs,
 };
 
