c12208668aefd91f33e41183a3e6f85979ac953f
Ahmed S. Darwish <a.darwish@linutronix.de>
2021-01-18 11:09:53 +0100

    scsi: isci: Switch back to original libsas event notifiers
    libsas event notifiers required an extension where gfp_t flags must be
    explicitly passed. For bisectability, a temporary _gfp() variant of such
    functions were added. All call sites then got converted use the _gfp()
    variants and explicitly pass GFP context. Having no callers left, the
    original libsas notifiers were then modified to accept gfp_t flags by
    default.
    
    Switch back to the original libas API, while still passing GFP context.
    The libsas _gfp() variants will be removed afterwards.
    
    Link: https://lore.kernel.org/r/20210118100955.1761652-18-a.darwish@linutronix.de
    Cc: Artur Paszkiewicz <artur.paszkiewicz@intel.com>
    Reviewed-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Ahmed S. Darwish <a.darwish@linutronix.de>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/isci/port.c
+++ drivers/scsi/isci/port.c
@@ -164,8 +164,8 @@ static void isci_port_bc_change_received(struct isci_host *ihost,
 		"%s: isci_phy = %p, sas_phy = %p\n",
 		__func__, iphy, &iphy->sas_phy);
 
-	sas_notify_port_event_gfp(&iphy->sas_phy,
-				  PORTE_BROADCAST_RCVD, GFP_ATOMIC);
+	sas_notify_port_event(&iphy->sas_phy,
+			      PORTE_BROADCAST_RCVD, GFP_ATOMIC);
 	sci_port_bcn_enable(iport);
 }
 
@@ -224,8 +224,8 @@ static void isci_port_link_up(struct isci_host *isci_host,
 	/* Notify libsas that we have an address frame, if indeed
 	 * we've found an SSP, SMP, or STP target */
 	if (success)
-		sas_notify_port_event_gfp(&iphy->sas_phy,
-					  PORTE_BYTES_DMAED, GFP_ATOMIC);
+		sas_notify_port_event(&iphy->sas_phy,
+				      PORTE_BYTES_DMAED, GFP_ATOMIC);
 }
 
 
@@ -271,8 +271,8 @@ static void isci_port_link_down(struct isci_host *isci_host,
 	 * isci_port_deformed and isci_dev_gone functions.
 	 */
 	sas_phy_disconnected(&isci_phy->sas_phy);
-	sas_notify_phy_event_gfp(&isci_phy->sas_phy,
-				 PHYE_LOSS_OF_SIGNAL, GFP_ATOMIC);
+	sas_notify_phy_event(&isci_phy->sas_phy,
+			     PHYE_LOSS_OF_SIGNAL, GFP_ATOMIC);
 
 	dev_dbg(&isci_host->pdev->dev,
 		"%s: isci_port = %p - Done\n", __func__, isci_port);
