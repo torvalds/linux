94b6df08da9c4e0f96d3fbd2ec79c40d8cdac4ae
Felix Fietkau <nbd@nbd.name>
2021-01-19 13:19:53 +0100

    mt76: fix crash on tearing down ext phy
    Only clear dev->phy2 after the phy is gone, the driver may still need to access
    it until shutdown is complete
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mac80211.c
+++ drivers/net/wireless/mediatek/mt76/mac80211.c
@@ -387,9 +387,9 @@ void mt76_unregister_phy(struct mt76_phy *phy)
 {
 	struct mt76_dev *dev = phy->dev;
 
-	dev->phy2 = NULL;
 	mt76_tx_status_check(dev, NULL, true);
 	ieee80211_unregister_hw(phy->hw);
+	dev->phy2 = NULL;
 }
 EXPORT_SYMBOL_GPL(mt76_unregister_phy);
 
