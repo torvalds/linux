09846950a1b63a91235d2b4b260afa04280d5388
Huang Rui <ray.huang@amd.com>
2021-01-19 13:35:21 +0800

    drm/amdgpu: remove gpu info firmware of green sardine
    commit acc214bfafbafcd29d5d25d1ede5f11c14ffc147 upstream.
    
    The ip discovery is supported on green sardine, it doesn't need gpu info
    firmware anymore.
    
    Signed-off-by: Huang Rui <ray.huang@amd.com>
    Reviewed-by: Prike Liang <Prike.Liang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: stable@vger.kernel.org # 5.10.x
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -80,7 +80,6 @@ MODULE_FIRMWARE("amdgpu/renoir_gpu_info.bin");
 MODULE_FIRMWARE("amdgpu/navi10_gpu_info.bin");
 MODULE_FIRMWARE("amdgpu/navi14_gpu_info.bin");
 MODULE_FIRMWARE("amdgpu/navi12_gpu_info.bin");
-MODULE_FIRMWARE("amdgpu/green_sardine_gpu_info.bin");
 
 #define AMDGPU_RESUME_MS		2000
 
