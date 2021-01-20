6eed1f3fba404dc0a1835c3a96694ea4152cd50e
Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>
2021-01-20 12:23:06 -0500

    drm/amd/display: Fix unused variable warning
    [Why]
    The dm struct is only being used if DCN config is defined and this
    causes a unused variable warning if DCN option is not set.
    
    [How]
    Remove the compile flag so the variable is used (there also seems to be
    a duplicate guard due to a bad rebase) so remove the outer guard to fix
    the warning.
    
    Fixes this warning
    drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c: In function 'dm_set_vblank':
    drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c:5380:33: warning: unused variable 'dm' [-Wunused-variable]
    
    Fixes: 98ab5f3513f9 ("drm/amd/display: Fix deadlock during gpu reset v3")
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: Nicholas Kazlauskas <Nicholas.Kazlauskas@amd.com>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -5394,7 +5394,6 @@ static inline int dm_set_vblank(struct drm_crtc *crtc, bool enable)
 	if (!dc_interrupt_set(adev->dm.dc, irq_source, enable))
 		return -EBUSY;
 
-#if defined(CONFIG_DRM_AMD_DC_DCN)
 	if (amdgpu_in_reset(adev))
 		return 0;
 
@@ -5414,7 +5413,6 @@ static inline int dm_set_vblank(struct drm_crtc *crtc, bool enable)
 
 	mutex_unlock(&dm->dc_lock);
 
-#endif
 	return 0;
 }
 
