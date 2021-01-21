1f8644d56e91f1c3cbeb1a36c3a998397d06b9dc
Lukasz Luba <lukasz.luba@arm.com>
2021-01-21 17:04:45 +0000

    drm/panfrost: Add governor data with pre-defined thresholds
    The simple_ondemand devfreq governor uses two thresholds to decide about
    the frequency change: upthreshold, downdifferential. These two tunable
    change the behavior of the governor decision, e.g. how fast to increase
    the frequency or how rapidly limit the frequency. This patch adds needed
    governor data with thresholds values gathered experimentally in different
    workloads.
    
    Signed-off-by: Lukasz Luba <lukasz.luba@arm.com>
    Reviewed-by: Steven Price <steven.price@arm.com>
    Signed-off-by: Steven Price <steven.price@arm.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210121170445.19761-1-lukasz.luba@arm.com

--- drivers/gpu/drm/panfrost/panfrost_devfreq.h
+++ drivers/gpu/drm/panfrost/panfrost_devfreq.h
@@ -4,6 +4,7 @@
 #ifndef __PANFROST_DEVFREQ_H__
 #define __PANFROST_DEVFREQ_H__
 
+#include <linux/devfreq.h>
 #include <linux/spinlock.h>
 #include <linux/ktime.h>
 
@@ -17,6 +18,7 @@ struct panfrost_devfreq {
 	struct devfreq *devfreq;
 	struct opp_table *regulators_opp_table;
 	struct thermal_cooling_device *cooling;
+	struct devfreq_simple_ondemand_data gov_data;
 	bool opp_of_table_added;
 
 	ktime_t busy_time;
