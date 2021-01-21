7306177c9ed33008376b7d41e39601c308e7a40e
Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
2021-01-21 16:19:35 +0000

    drm/i915: Check for all subplatform bits
    commit 8f6d08c9af284d74276da6681348e4673f13caea upstream.
    
    Current code is checking only 2 bits in the subplatform, but actually 3
    bits are allocated for the field. Check all 3 bits.
    
    Fixes: 805446c8347c ("drm/i915: Introduce concept of a sub-platform")
    Cc: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Signed-off-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210121161936.746591-1-tvrtko.ursulin@linux.intel.com
    (cherry picked from commit 27b695ee1af9bb36605e67055874ec081306ac28)
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1347,7 +1347,7 @@ intel_subplatform(const struct intel_runtime_info *info, enum intel_platform p)
 {
 	const unsigned int pi = __platform_mask_index(info, p);
 
-	return info->platform_mask[pi] & INTEL_SUBPLATFORM_BITS;
+	return info->platform_mask[pi] & ((1 << INTEL_SUBPLATFORM_BITS) - 1);
 }
 
 static __always_inline bool
