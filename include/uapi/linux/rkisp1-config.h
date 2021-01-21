fc672d806bd77eff26117479e90ccdcfd2a8ecb4
Heiko Stuebner <heiko.stuebner@theobroma-systems.com>
2021-01-21 15:44:06 +0100

    media: rockchip: rkisp1: carry ip version information
    The IP block evolved from its rk3288/rk3399 base and the vendor
    designates them with a numerical version. rk3399 for example
    is designated V10 probably meaning V1.0.
    
    There doesn't seem to be an actual version register we could read that
    information from, so allow the match_data to carry that information
    for future differentiation.
    
    Also carry that information in the hw_revision field of the media-
    controller API, so that userspace also has access to that.
    
    The added versions are:
    - V10: at least rk3288 + rk3399
    - V11: seemingly unused as of now, but probably appeared in some soc
    - V12: at least rk3326 + px30
    - V13: at least rk1808
    
    [fix checkpatch warning don't use multiple blank lines]
    
    Signed-off-by: Heiko Stuebner <heiko.stuebner@theobroma-systems.com>
    Signed-off-by: Dafna Hirschfeld <dafna.hirschfeld@collabora.com>
    Reviewed-by: Ezequiel Garcia <ezequiel@collabora.com>
    Acked-by: Helen Koike <helen.koike@collabora.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- include/uapi/linux/rkisp1-config.h
+++ include/uapi/linux/rkisp1-config.h
@@ -123,6 +123,21 @@
 #define RKISP1_CIF_ISP_STAT_AFM           (1U << 2)
 #define RKISP1_CIF_ISP_STAT_HIST          (1U << 3)
 
+/**
+ * enum rkisp1_cif_isp_version - ISP variants
+ *
+ * @RKISP1_V10: used at least in rk3288 and rk3399
+ * @RKISP1_V11: declared in the original vendor code, but not used
+ * @RKISP1_V12: used at least in rk3326 and px30
+ * @RKISP1_V13: used at least in rk1808
+ */
+enum rkisp1_cif_isp_version {
+	RKISP1_V10 = 10,
+	RKISP1_V11,
+	RKISP1_V12,
+	RKISP1_V13,
+};
+
 enum rkisp1_cif_isp_histogram_mode {
 	RKISP1_CIF_ISP_HISTOGRAM_MODE_DISABLE,
 	RKISP1_CIF_ISP_HISTOGRAM_MODE_RGB_COMBINED,
