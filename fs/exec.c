435ac6214e00eab191296cd6cfdc316aa07e7794
Christian Brauner <christian.brauner@ubuntu.com>
2021-01-21 14:19:41 +0100

    would_dump: handle idmapped mounts
    When determining whether or not to create a coredump the vfs will verify
    that the caller is privileged over the inode. Make the would_dump()
    helper handle idmapped mounts by passing down the mount's user namespace
    of the exec file. If the initial user namespace is passed nothing
    changes so non-idmapped mounts will see identical behavior as before.
    
    Link: https://lore.kernel.org/r/20210121131959.646623-23-christian.brauner@ubuntu.com
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: David Howells <dhowells@redhat.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: linux-fsdevel@vger.kernel.org
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1404,15 +1404,15 @@ EXPORT_SYMBOL(begin_new_exec);
 void would_dump(struct linux_binprm *bprm, struct file *file)
 {
 	struct inode *inode = file_inode(file);
-	if (inode_permission(&init_user_ns, inode, MAY_READ) < 0) {
+	struct user_namespace *mnt_userns = file_mnt_user_ns(file);
+	if (inode_permission(mnt_userns, inode, MAY_READ) < 0) {
 		struct user_namespace *old, *user_ns;
 		bprm->interp_flags |= BINPRM_FLAGS_ENFORCE_NONDUMP;
 
 		/* Ensure mm->user_ns contains the executable */
 		user_ns = old = bprm->mm->user_ns;
 		while ((user_ns != &init_user_ns) &&
-		       !privileged_wrt_inode_uidgid(user_ns, &init_user_ns,
-						    inode))
+		       !privileged_wrt_inode_uidgid(user_ns, mnt_userns, inode))
 			user_ns = user_ns->parent;
 
 		if (old != user_ns) {
