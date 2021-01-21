9308579fef3ddde19da9d45e23bf36d41932417f
Jinyang He <hejinyang@loongson.cn>
2021-01-21 13:31:36 +0800

    MIPS: microMIPS: Fix the judgment of mm_jr16_op and mm_jalr_op
    mm16_r5_format.rt is 5 bits, so directly judge the value if equal or not.
    mm_jalr_op requires 7th to 16th bits. These 10 which bits generated by
    shifting u_format.uimmediate by 6 may be affected by sign extension.
    Thus, take out the 10 bits for comparison.
    
    Without this patch, errors may occur, such as these bits are all ones.
    
    Signed-off-by: Jinyang He <hejinyang@loongson.cn>
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/kernel/process.c
+++ arch/mips/kernel/process.c
@@ -294,8 +294,8 @@ static inline int is_jump_ins(union mips_instruction *ip)
 	 * microMIPS is kind of more fun...
 	 */
 	if (mm_insn_16bit(ip->word >> 16)) {
-		if ((ip->mm16_r5_format.opcode == mm_pool16c_op &&
-		    (ip->mm16_r5_format.rt & mm_jr16_op) == mm_jr16_op))
+		if (ip->mm16_r5_format.opcode == mm_pool16c_op &&
+		    ip->mm16_r5_format.rt == mm_jr16_op)
 			return 1;
 		return 0;
 	}
@@ -307,7 +307,7 @@ static inline int is_jump_ins(union mips_instruction *ip)
 	if (ip->r_format.opcode != mm_pool32a_op ||
 			ip->r_format.func != mm_pool32axf_op)
 		return 0;
-	return ((ip->u_format.uimmediate >> 6) & mm_jalr_op) == mm_jalr_op;
+	return ((ip->u_format.uimmediate >> 6) & GENMASK(9, 0)) == mm_jalr_op;
 #else
 	if (ip->j_format.opcode == j_op)
 		return 1;
