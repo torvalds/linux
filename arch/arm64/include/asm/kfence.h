16591e4945565b450c37c8c2eaeae52cd6fc3fb7
Marco Elver <elver@google.com>
2021-01-22 09:18:37 +0000

    FROMGIT: arm64, kfence: enable KFENCE for ARM64
    Add architecture specific implementation details for KFENCE and enable
    KFENCE for the arm64 architecture. In particular, this implements the
    required interface in <asm/kfence.h>.
    
    KFENCE requires that attributes for pages from its memory pool can
    individually be set. Therefore, force the entire linear map to be mapped
    at page granularity. Doing so may result in extra memory allocated for
    page tables in case rodata=full is not set; however, currently
    CONFIG_RODATA_FULL_DEFAULT_ENABLED=y is the default, and the common case
    is therefore not affected by this change.
    
    Link: https://lkml.kernel.org/r/20201103175841.3495947-4-elver@google.com
    Signed-off-by: Alexander Potapenko <glider@google.com>
    Signed-off-by: Marco Elver <elver@google.com>
    Reviewed-by: Dmitry Vyukov <dvyukov@google.com>
    Co-developed-by: Alexander Potapenko <glider@google.com>
    Reviewed-by: Jann Horn <jannh@google.com>
    Reviewed-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Andrey Konovalov <andreyknvl@google.com>
    Cc: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Christopher Lameter <cl@linux.com>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Eric Dumazet <edumazet@google.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Hillf Danton <hdanton@sina.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Joern Engel <joern@purestorage.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Paul E. McKenney <paulmck@kernel.org>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: SeongJae Park <sjpark@amazon.de>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Bug: 177201466
    (cherry picked from commit d3cb0555da9b469c3f9ebe663d4c0d6265757175
        https://github.com/hnaz/linux-mm v5.11-rc4-mmots-2021-01-21-20-10)
    Test: CONFIG_KFENCE_KUNIT_TEST=y passes on Cuttlefish
    Signed-off-by: Alexander Potapenko <glider@google.com>
    Change-Id: Ibe9d26c2c9088862ba8855c18e0c5210a68c7f50

---
+++ arch/arm64/include/asm/kfence.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __ASM_KFENCE_H
+#define __ASM_KFENCE_H
+
+#include <asm/cacheflush.h>
+
+#define KFENCE_SKIP_ARCH_FAULT_HANDLER "el1_sync"
+
+static inline bool arch_kfence_init_pool(void) { return true; }
+
+static inline bool kfence_protect_page(unsigned long addr, bool protect)
+{
+	set_memory_valid(addr, 1, !protect);
+
+	return true;
+}
+
+#endif /* __ASM_KFENCE_H */
--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -1469,7 +1469,12 @@ int arch_add_memory(int nid, u64 start, u64 size,
 		return -EINVAL;
 	}
 
-	if (rodata_full || debug_pagealloc_enabled())
+	/*
+	 * KFENCE requires linear map to be mapped at page granularity, so that
+	 * it is possible to protect/unprotect single pages in the KFENCE pool.
+	 */
+	if (rodata_full || debug_pagealloc_enabled() ||
+	    IS_ENABLED(CONFIG_KFENCE))
 		flags = NO_BLOCK_MAPPINGS | NO_CONT_MAPPINGS;
 
 	__create_pgd_mapping(swapper_pg_dir, start, __phys_to_virt(start),
