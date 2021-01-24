9d5c8190683a462dbc787658467a0da17011ea5f
Pavel Begunkov <asml.silence@gmail.com>
2021-01-24 15:08:14 +0000

    io_uring: fix sleeping under spin in __io_clean_op
    [   27.629441] BUG: sleeping function called from invalid context
    	at fs/file.c:402
    [   27.631317] in_atomic(): 1, irqs_disabled(): 1, non_block: 0,
    	pid: 1012, name: io_wqe_worker-0
    [   27.633220] 1 lock held by io_wqe_worker-0/1012:
    [   27.634286]  #0: ffff888105e26c98 (&ctx->completion_lock)
    	{....}-{2:2}, at: __io_req_complete.part.102+0x30/0x70
    [   27.649249] Call Trace:
    [   27.649874]  dump_stack+0xac/0xe3
    [   27.650666]  ___might_sleep+0x284/0x2c0
    [   27.651566]  put_files_struct+0xb8/0x120
    [   27.652481]  __io_clean_op+0x10c/0x2a0
    [   27.653362]  __io_cqring_fill_event+0x2c1/0x350
    [   27.654399]  __io_req_complete.part.102+0x41/0x70
    [   27.655464]  io_openat2+0x151/0x300
    [   27.656297]  io_issue_sqe+0x6c/0x14e0
    [   27.660991]  io_wq_submit_work+0x7f/0x240
    [   27.662890]  io_worker_handle_work+0x501/0x8a0
    [   27.664836]  io_wqe_worker+0x158/0x520
    [   27.667726]  kthread+0x134/0x180
    [   27.669641]  ret_from_fork+0x1f/0x30
    
    Instead of cleaning files on overflow, return back overflow cancellation
    into io_uring_cancel_files(). Previously it was racy to clean
    REQ_F_OVERFLOW flag, but we got rid of it, and can do it through
    repetitive attempts targeting all matching requests.
    
    Reported-by: Abaci <abaci@linux.alibaba.com>
    Reported-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Cc: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -1025,6 +1025,7 @@ static ssize_t io_import_iovec(int rw, struct io_kiocb *req,
 static int io_setup_async_rw(struct io_kiocb *req, const struct iovec *iovec,
 			     const struct iovec *fast_iov,
 			     struct iov_iter *iter, bool force);
+static void io_req_drop_files(struct io_kiocb *req);
 
 static struct kmem_cache *req_cachep;
 
@@ -1048,8 +1049,7 @@ EXPORT_SYMBOL(io_uring_get_socket);
 
 static inline void io_clean_op(struct io_kiocb *req)
 {
-	if (req->flags & (REQ_F_NEED_CLEANUP | REQ_F_BUFFER_SELECTED |
-			  REQ_F_INFLIGHT))
+	if (req->flags & (REQ_F_NEED_CLEANUP | REQ_F_BUFFER_SELECTED))
 		__io_clean_op(req);
 }
 
@@ -1394,6 +1394,8 @@ static void io_req_clean_work(struct io_kiocb *req)
 			free_fs_struct(fs);
 		req->work.flags &= ~IO_WQ_WORK_FS;
 	}
+	if (req->flags & REQ_F_INFLIGHT)
+		io_req_drop_files(req);
 
 	io_put_identity(req->task->io_uring, req);
 }
@@ -6230,9 +6232,6 @@ static void __io_clean_op(struct io_kiocb *req)
 		}
 		req->flags &= ~REQ_F_NEED_CLEANUP;
 	}
-
-	if (req->flags & REQ_F_INFLIGHT)
-		io_req_drop_files(req);
 }
 
 static int io_issue_sqe(struct io_kiocb *req, bool force_nonblock,
@@ -8879,6 +8878,7 @@ static void io_uring_cancel_files(struct io_ring_ctx *ctx,
 		io_wq_cancel_cb(ctx->io_wq, io_cancel_task_cb, &cancel, true);
 		io_poll_remove_all(ctx, task, files);
 		io_kill_timeouts(ctx, task, files);
+		io_cqring_overflow_flush(ctx, true, task, files);
 		/* cancellations _may_ trigger task work */
 		io_run_task_work();
 		schedule();
