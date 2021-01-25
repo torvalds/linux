5d9a29660bfe81c94b5dc79030c6f563f1a41716
Mike Tipton <mdtipton@codeaurora.org>
2021-01-25 14:33:26 -0800

    interconnect: qcom: Set floor of 1 for BCM unit/width
    Some BCMs that aren't directly associated with NOC ports (i.e. ACV)
    publish their width as 0 in cmd_db. The client votes are multiplied by
    this width when adjusting for node/BCM width differences. This means a
    zero width also zeroes the client votes, making it impossible to vote on
    these zero-width BCMs. Set a width floor of 1 to allow aggregation on
    these nodes.
    
    Also set a floor on the BCM units. They should never by 0 in cmd_db, but
    if they are it would cause a div-by-zero.
    
    Change-Id: I14ba89411ec02077db6d2c27b6de932c5b7dfe1e
    Signed-off-by: Mike Tipton <mdtipton@codeaurora.org>

--- drivers/interconnect/qcom/icc-rpmh.c
+++ drivers/interconnect/qcom/icc-rpmh.c
@@ -194,8 +194,8 @@ int qcom_icc_bcm_init(struct qcom_icc_bcm *bcm, struct device *dev)
 		return -EINVAL;
 	}
 
-	bcm->aux_data.unit = le32_to_cpu(data->unit);
-	bcm->aux_data.width = le16_to_cpu(data->width);
+	bcm->aux_data.unit = max_t(u32, 1, le32_to_cpu(data->unit));
+	bcm->aux_data.width = max_t(u16, 1, le16_to_cpu(data->width));
 	bcm->aux_data.vcd = data->vcd;
 	bcm->aux_data.reserved = data->reserved;
 	INIT_LIST_HEAD(&bcm->list);
