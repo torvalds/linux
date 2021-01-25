f4514249d6483effbc08984ed908fc741de798f0
Dan Carpenter <dan.carpenter@oracle.com>
2021-01-25 14:13:44 +0300

    ALSA: fireface: remove unnecessary condition in hwdep_read()
    Smatch complains that "count" is not clamped when "ff->dev_lock_changed"
    and it leads to an information leak.  Fortunately, that's not actually
    possible and the condition can be deleted.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Acked-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Link: https://lore.kernel.org/r/YA6n6I8EcNAO5ZFs@mwanda
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/fireface/ff-hwdep.c
+++ sound/firewire/fireface/ff-hwdep.c
@@ -35,13 +35,11 @@ static long hwdep_read(struct snd_hwdep *hwdep, char __user *buf,  long count,
 	}
 
 	memset(&event, 0, sizeof(event));
-	if (ff->dev_lock_changed) {
-		event.lock_status.type = SNDRV_FIREWIRE_EVENT_LOCK_STATUS;
-		event.lock_status.status = (ff->dev_lock_count > 0);
-		ff->dev_lock_changed = false;
+	event.lock_status.type = SNDRV_FIREWIRE_EVENT_LOCK_STATUS;
+	event.lock_status.status = (ff->dev_lock_count > 0);
+	ff->dev_lock_changed = false;
 
-		count = min_t(long, count, sizeof(event.lock_status));
-	}
+	count = min_t(long, count, sizeof(event.lock_status));
 
 	spin_unlock_irq(&ff->lock);
 
