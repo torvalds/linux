a190069bc3f66394c50e256811de31df1d08c631
Paul Lawrence <paullawrence@google.com>
2021-01-26 13:31:18 -0800

    Revert "ANDROID: Incremental fs: RCU locks instead of mutex for pending_reads."
    This reverts commit 883c150ca9f7cfbd16ee4347ebf5e44a9ac02ea2.
    
    Set incfs back to rvc shipping incfs
    
    Bug: 178509184
    Test: incfs_test passes
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: I492c8cacd213df35c093a582a7f1c598913db104

--- fs/incfs/data_mgmt.h
+++ fs/incfs/data_mgmt.h
@@ -10,7 +10,6 @@
 #include <linux/types.h>
 #include <linux/mutex.h>
 #include <linux/spinlock.h>
-#include <linux/rcupdate.h>
 #include <linux/completion.h>
 #include <linux/wait.h>
 #include <crypto/hash.h>
@@ -124,13 +123,13 @@ struct mount_info {
 	wait_queue_head_t mi_pending_reads_notif_wq;
 
 	/*
-	 * Protects - RCU safe:
+	 * Protects:
 	 *  - reads_list_head
 	 *  - mi_pending_reads_count
 	 *  - mi_last_pending_read_number
 	 *  - data_file_segment.reads_list_head
 	 */
-	spinlock_t pending_read_lock;
+	struct mutex mi_pending_reads_mutex;
 
 	/* List of active pending_read objects */
 	struct list_head mi_reads_list_head;
@@ -176,8 +175,6 @@ struct pending_read {
 	struct list_head mi_reads_list;
 
 	struct list_head segment_reads_list;
-
-	struct rcu_head rcu;
 };
 
 struct data_file_segment {
