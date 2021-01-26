cd96fe600cc4924d8d0cc6e3161870219c0d2c12
Luo Jiaxing <luojiaxing@huawei.com>
2021-01-26 19:04:28 +0800

    scsi: hisi_sas: Add trace FIFO debugfs support
    The controller provides trace FIFO DFX tool to assist link fault debugging
    and link optimization. This tool can be helpful when debugging link faults
    without SAS analyzers. Each PHY has an independent trace FIFO interface.
    
    The user can configure the trace FIFO tool of one PHY by using the
    following six interfaces:
    
    signal_sel: select signal group applies to different scenarios.
      0x0: linkrate negotiation
      0x1: Host 12G TX train
      0x2: Disk 12G TX train
      0x3: SAS PHY CTRL DFX 0
      0x4: SAS PHY CTRL DFX 1
      0x5: SAS PCS DFX
      other: linkrate negotiation
    dump_mask: The masked hardware status bit will not be updated.
    dump_mode: determines how to dump data after trigger signal is generated.
      0x0: dump forever
      0x1: dump 32 data after trigger signal is generated
      0x2: no more dump after trigger signal is generated
    trigger_mode: determines the trigger mode, level or edge.
      0x0: dump when trigger signal changed
      0x1: dump when trigger signal's level equal to trigger_level
      0x2: dump when trigger signal's level different from trigger_level
    trigger_level: determines the trigger level.
    trigger_msk: mask trigger signal
    
    The user can get 32-byte values from hardware by reading the rd_data.
    These values consitute the status record of the hardware at different time
    points.
    
    Link: https://lore.kernel.org/r/1611659068-131975-6-git-send-email-john.garry@huawei.com
    Signed-off-by: Luo Jiaxing <luojiaxing@huawei.com>
    Signed-off-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/hisi_sas/hisi_sas.h
+++ drivers/scsi/hisi_sas/hisi_sas.h
@@ -44,6 +44,7 @@
 
 #define HISI_SAS_IOST_ITCT_CACHE_NUM 64
 #define HISI_SAS_IOST_ITCT_CACHE_DW_SZ 10
+#define HISI_SAS_FIFO_DATA_DW_SIZE 32
 
 #define HISI_SAS_STATUS_BUF_SZ (sizeof(struct hisi_sas_status_buffer))
 #define HISI_SAS_COMMAND_TABLE_SZ (sizeof(union hisi_sas_command_table))
@@ -154,6 +155,16 @@ enum hisi_sas_phy_event {
 	HISI_PHYES_NUM,
 };
 
+struct hisi_sas_debugfs_fifo {
+	u32 signal_sel;
+	u32 dump_msk;
+	u32 dump_mode;
+	u32 trigger;
+	u32 trigger_msk;
+	u32 trigger_mode;
+	u32 rd_data[HISI_SAS_FIFO_DATA_DW_SIZE];
+};
+
 struct hisi_sas_phy {
 	struct work_struct	works[HISI_PHYES_NUM];
 	struct hisi_hba	*hisi_hba;
@@ -175,6 +186,9 @@ struct hisi_sas_phy {
 	enum sas_linkrate	maximum_linkrate;
 	int enable;
 	atomic_t down_cnt;
+
+	/* Trace FIFO */
+	struct hisi_sas_debugfs_fifo fifo;
 };
 
 struct hisi_sas_port {
@@ -474,6 +488,7 @@ struct hisi_hba {
 	struct dentry *debugfs_dir;
 	struct dentry *debugfs_dump_dentry;
 	struct dentry *debugfs_bist_dentry;
+	struct dentry *debugfs_fifo_dentry;
 };
 
 /* Generic HW DMA host memory structures */
