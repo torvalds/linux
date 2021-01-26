93f1c150cb0d043e1e8985db7824b4e2e1ac653f
Eric Biggers <ebiggers@google.com>
2021-01-25 16:14:48 -0800

    mmc: core: Add basic support for inline encryption
    In preparation for adding CQHCI crypto engine (inline encryption)
    support, add the code required to make mmc_core and mmc_block aware of
    inline encryption.  Specifically:
    
    - Add a capability flag MMC_CAP2_CRYPTO to struct mmc_host.  Drivers
      will set this if the host and driver support inline encryption.
    
    - Embed a blk_keyslot_manager in struct mmc_host.  Drivers will
      initialize this (as a device-managed resource) if the host and driver
      support inline encryption.  mmc_block registers this keyslot manager
      with the request_queue of any MMC card attached to the host.
    
    - Make mmc_block copy the crypto keyslot and crypto data unit number
      from struct request to struct mmc_request, so that drivers will have
      access to them.
    
    - If the MMC host is reset, reprogram all the keyslots to ensure that
      the software state stays in sync with the hardware state.
    
    Co-developed-by: Satya Tangirala <satyat@google.com>
    Signed-off-by: Satya Tangirala <satyat@google.com>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Reviewed-by: Satya Tangirala <satyat@google.com>
    Reviewed-and-tested-by: Peng Zhou <peng.zhou@mediatek.com>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Link: https://lore.kernel.org/r/20210126001456.382989-2-ebiggers@kernel.org
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/core/queue.c
+++ drivers/mmc/core/queue.c
@@ -19,6 +19,7 @@
 #include "block.h"
 #include "core.h"
 #include "card.h"
+#include "crypto.h"
 #include "host.h"
 
 #define MMC_DMA_MAP_MERGE_SEGMENTS	512
@@ -407,6 +408,8 @@ static void mmc_setup_queue(struct mmc_queue *mq, struct mmc_card *card)
 	mutex_init(&mq->complete_lock);
 
 	init_waitqueue_head(&mq->wait);
+
+	mmc_crypto_setup_queue(mq->queue, host);
 }
 
 static inline bool mmc_merge_capable(struct mmc_host *host)
