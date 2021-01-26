26988c4722d24108ba5812714c909f942a2c5abe
Paul Lawrence <paullawrence@google.com>
2021-01-26 13:31:07 -0800

    Revert "ANDROID: Incremental fs: Add INCFS_IOC_GET_BLOCK_COUNT"
    This reverts commit 1652f2647e8f4ffc8e1a3e9a368563d39fa25132.
    
    Set incfs back to rvc shipping incfs
    
    Bug: 178509184
    Test: incfs_test passes
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: Ifdbdbffee413a16e8c222da60a371ac95797501a

--- fs/incfs/data_mgmt.h
+++ fs/incfs/data_mgmt.h
@@ -249,14 +249,7 @@ struct data_file {
 	/* For mapped files, the offset into the actual file */
 	loff_t df_mapped_offset;
 
-	/* Number of data blocks written to file */
-	atomic_t df_data_blocks_written;
-
-	/* Number of data blocks in the status block */
-	u32 df_initial_data_blocks_written;
-
-	/* Offset to status metadata header */
-	loff_t df_status_offset;
+	struct file_attr n_attr;
 
 	struct mtree *df_hash_tree;
 
