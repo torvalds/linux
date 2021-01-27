33cf440d594bfbf81fc20604957bc64f02d0b560
Alex Deucher <alexander.deucher@amd.com>
2021-01-26 21:57:00 -0500

    drm/amdgpu: disable gpu reset on Vangogh for now
    Until the issues in the SMU firmware are fixed.
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Acked-by: Huang Rui <ray.huang@amd.com>
    Signed-off-by: Huang Rui <ray.huang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -4211,7 +4211,6 @@ bool amdgpu_device_should_recover_gpu(struct amdgpu_device *adev)
 		case CHIP_NAVI12:
 		case CHIP_SIENNA_CICHLID:
 		case CHIP_NAVY_FLOUNDER:
-		case CHIP_VANGOGH:
 			break;
 		default:
 			goto disabled;
