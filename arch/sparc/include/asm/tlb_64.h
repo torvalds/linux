c7bd8010a335260927e3643e79360272f9aca266
Will Deacon <will@kernel.org>
2021-01-27 23:53:46 +0000

    tlb: arch: Remove empty __tlb_remove_tlb_entry() stubs
    If __tlb_remove_tlb_entry() is not defined by the architecture then
    we provide an empty definition in asm-generic/tlb.h.
    
    Remove the redundant empty definitions for sparc64 and x86.
    
    Suggested-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Yu Zhao <yuzhao@google.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Link: https://lkml.kernel.org/r/20210127235347.1402-6-will@kernel.org

--- arch/sparc/include/asm/tlb_64.h
+++ arch/sparc/include/asm/tlb_64.h
@@ -24,7 +24,6 @@ void flush_tlb_pending(void);
 
 #define tlb_start_vma(tlb, vma) do { } while (0)
 #define tlb_end_vma(tlb, vma)	do { } while (0)
-#define __tlb_remove_tlb_entry(tlb, ptep, address) do { } while (0)
 #define tlb_flush(tlb)	flush_tlb_pending()
 
 /*
