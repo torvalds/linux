dcdb4f12963f3f4200e24e1dad78564a98736f67
Nicholas Piggin <npiggin@gmail.com>
2021-01-30 23:08:32 +1000

    powerpc/cell: tidy up pervasive declarations
    These are declared in ras.h and defined in ras.c so remove them from
    pervasive.h
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20210130130852.2952424-23-npiggin@gmail.com

--- arch/powerpc/platforms/cell/pervasive.h
+++ arch/powerpc/platforms/cell/pervasive.h
@@ -13,9 +13,6 @@
 #define PERVASIVE_H
 
 extern void cbe_pervasive_init(void);
-extern void cbe_system_error_exception(struct pt_regs *regs);
-extern void cbe_maintenance_exception(struct pt_regs *regs);
-extern void cbe_thermal_exception(struct pt_regs *regs);
 
 #ifdef CONFIG_PPC_IBM_CELL_RESETBUTTON
 extern int cbe_sysreset_hack(void);
