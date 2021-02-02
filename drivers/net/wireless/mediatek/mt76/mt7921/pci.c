7715a1d54bdf47f89194b0a088e07dcd76820f51
Ryder Lee <ryder.lee@mediatek.com>
2021-02-02 21:57:59 +0800

    mt76: use PCI_VENDOR_ID_MEDIATEK to avoid open coded
    Use PCI standard defines.
    
    Signed-off-by: Ryder Lee <ryder.lee@mediatek.com>
    Acked-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7921/pci.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/pci.c
@@ -13,7 +13,7 @@
 #include "../trace.h"
 
 static const struct pci_device_id mt7921_pci_device_table[] = {
-	{ PCI_DEVICE(0x14c3, 0x7961) },
+	{ PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x7961) },
 	{ },
 };
 
