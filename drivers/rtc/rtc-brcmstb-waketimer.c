ae1247f79ddf8107b2d7b6889abbc5885c895b4e
Alexandre Belloni <alexandre.belloni@bootlin.com>
2021-02-02 12:22:01 +0100

    rtc: brcmstb-waketimer: quiet maybe-unused variable warning
    When CONFIG_OF is disabled then the matching table is not referenced.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Acked-by: Florian Fainelli <f.fainelli@gmail.com>
    Link: https://lore.kernel.org/r/20210202112219.3610853-5-alexandre.belloni@bootlin.com

--- drivers/rtc/rtc-brcmstb-waketimer.c
+++ drivers/rtc/rtc-brcmstb-waketimer.c
@@ -306,7 +306,7 @@ static int brcmstb_waketmr_resume(struct device *dev)
 static SIMPLE_DEV_PM_OPS(brcmstb_waketmr_pm_ops,
 			 brcmstb_waketmr_suspend, brcmstb_waketmr_resume);
 
-static const struct of_device_id brcmstb_waketmr_of_match[] = {
+static const __maybe_unused struct of_device_id brcmstb_waketmr_of_match[] = {
 	{ .compatible = "brcm,brcmstb-waketimer" },
 	{ /* sentinel */ },
 };
