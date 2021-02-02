f5a5589c72509abaeb705123b64e7f5a078becf0
Wei Wang <weiwan@google.com>
2021-02-02 11:34:08 -0800

    tcp: use a smaller percpu_counter batch size for sk_alloc
    Currently, a percpu_counter with the default batch size (2*nr_cpus) is
    used to record the total # of active sockets per protocol. This means
    sk_sockets_allocated_read_positive() could be off by +/-2*(nr_cpus^2).
    This under/over-estimation could lead to wrong memory suppression
    conditions in __sk_raise_mem_allocated().
    Fix this by using a more reasonable fixed batch size of 16.
    
    See related commit cf86a086a180 ("net/dst: use a smaller percpu_counter
    batch for dst entries accounting") that addresses a similar issue.
    
    Signed-off-by: Wei Wang <weiwan@google.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Soheil Hassas Yeganeh <soheil@google.com>
    Link: https://lore.kernel.org/r/20210202193408.1171634-1-weiwan@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1350,14 +1350,18 @@ sk_memory_allocated_sub(struct sock *sk, int amt)
 	atomic_long_sub(amt, sk->sk_prot->memory_allocated);
 }
 
+#define SK_ALLOC_PERCPU_COUNTER_BATCH 16
+
 static inline void sk_sockets_allocated_dec(struct sock *sk)
 {
-	percpu_counter_dec(sk->sk_prot->sockets_allocated);
+	percpu_counter_add_batch(sk->sk_prot->sockets_allocated, -1,
+				 SK_ALLOC_PERCPU_COUNTER_BATCH);
 }
 
 static inline void sk_sockets_allocated_inc(struct sock *sk)
 {
-	percpu_counter_inc(sk->sk_prot->sockets_allocated);
+	percpu_counter_add_batch(sk->sk_prot->sockets_allocated, 1,
+				 SK_ALLOC_PERCPU_COUNTER_BATCH);
 }
 
 static inline u64
