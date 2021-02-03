2502610927ee51c11f3208c7a11a5661a7c78fa3
Pavel Shilovsky <pshilov@microsoft.com>
2021-02-02 22:34:32 -0600

    smb3: fix crediting for compounding when only one request in flight
    commit 91792bb8089b63b7b780251eb83939348ac58a64 upstream.
    
    Currently we try to guess if a compound request is going to
    succeed waiting for credits or not based on the number of
    requests in flight. This approach doesn't work correctly
    all the time because there may be only one request in
    flight which is going to bring multiple credits satisfying
    the compound request.
    
    Change the behavior to fail a request only if there are no requests
    in flight at all and proceed waiting for credits otherwise.
    
    Cc: <stable@vger.kernel.org> # 5.1+
    Signed-off-by: Pavel Shilovsky <pshilov@microsoft.com>
    Reviewed-by: Tom Talpey <tom@talpey.com>
    Reviewed-by: Shyam Prasad N <nspmangalore@gmail.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/cifs/transport.c
+++ fs/cifs/transport.c
@@ -655,10 +655,22 @@ wait_for_compound_request(struct TCP_Server_Info *server, int num,
 	spin_lock(&server->req_lock);
 	if (*credits < num) {
 		/*
-		 * Return immediately if not too many requests in flight since
-		 * we will likely be stuck on waiting for credits.
+		 * If the server is tight on resources or just gives us less
+		 * credits for other reasons (e.g. requests are coming out of
+		 * order and the server delays granting more credits until it
+		 * processes a missing mid) and we exhausted most available
+		 * credits there may be situations when we try to send
+		 * a compound request but we don't have enough credits. At this
+		 * point the client needs to decide if it should wait for
+		 * additional credits or fail the request. If at least one
+		 * request is in flight there is a high probability that the
+		 * server will return enough credits to satisfy this compound
+		 * request.
+		 *
+		 * Return immediately if no requests in flight since we will be
+		 * stuck on waiting for credits.
 		 */
-		if (server->in_flight < num - *credits) {
+		if (server->in_flight == 0) {
 			spin_unlock(&server->req_lock);
 			return -ENOTSUPP;
 		}
