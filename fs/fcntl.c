b1efc196446ae0e331045ad0ae9149021bc1642f
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2021-02-03 15:41:56 +0300

    fcntl: make F_GETOWN(EX) return 0 on dead owner task
    [ Upstream commit cc4a3f885e8f2bc3c86a265972e94fef32d68f67 ]
    
    Currently there is no way to differentiate the file with alive owner
    from the file with dead owner but pid of the owner reused. That's why
    CRIU can't actually know if it needs to restore file owner or not,
    because if it restores owner but actual owner was dead, this can
    introduce unexpected signals to the "false"-owner (which reused the
    pid).
    
    Let's change the api, so that F_GETOWN(EX) returns 0 in case actual
    owner is dead already. This comports with the POSIX spec, which
    states that a PID of 0 indicates that no signal will be sent.
    
    Cc: Jeff Layton <jlayton@kernel.org>
    Cc: "J. Bruce Fields" <bfields@fieldses.org>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: linux-fsdevel@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Cc: Cyrill Gorcunov <gorcunov@gmail.com>
    Cc: Andrei Vagin <avagin@gmail.com>
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Signed-off-by: Jeff Layton <jlayton@kernel.org>
    Stable-dep-of: f671a691e299 ("fcntl: fix potential deadlocks for &fown_struct.lock")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/fcntl.c
+++ fs/fcntl.c
@@ -148,11 +148,15 @@ void f_delown(struct file *filp)
 
 pid_t f_getown(struct file *filp)
 {
-	pid_t pid;
+	pid_t pid = 0;
 	read_lock(&filp->f_owner.lock);
-	pid = pid_vnr(filp->f_owner.pid);
-	if (filp->f_owner.pid_type == PIDTYPE_PGID)
-		pid = -pid;
+	rcu_read_lock();
+	if (pid_task(filp->f_owner.pid, filp->f_owner.pid_type)) {
+		pid = pid_vnr(filp->f_owner.pid);
+		if (filp->f_owner.pid_type == PIDTYPE_PGID)
+			pid = -pid;
+	}
+	rcu_read_unlock();
 	read_unlock(&filp->f_owner.lock);
 	return pid;
 }
@@ -200,11 +204,14 @@ static int f_setown_ex(struct file *filp, unsigned long arg)
 static int f_getown_ex(struct file *filp, unsigned long arg)
 {
 	struct f_owner_ex __user *owner_p = (void __user *)arg;
-	struct f_owner_ex owner;
+	struct f_owner_ex owner = {};
 	int ret = 0;
 
 	read_lock(&filp->f_owner.lock);
-	owner.pid = pid_vnr(filp->f_owner.pid);
+	rcu_read_lock();
+	if (pid_task(filp->f_owner.pid, filp->f_owner.pid_type))
+		owner.pid = pid_vnr(filp->f_owner.pid);
+	rcu_read_unlock();
 	switch (filp->f_owner.pid_type) {
 	case PIDTYPE_PID:
 		owner.type = F_OWNER_TID;
