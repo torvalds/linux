e798c45c009ca5fa3fc8987f9215fb0283b3304d
Vivek Aknurwar <viveka@codeaurora.org>
2021-02-02 16:34:13 -0800

    clk: qcom: clk-rpmh: Update probe debug messages to WARN
    Missing RPMh resources error causes various dependent drivers to fail,
    resulting unknown behaviour of device during kernel boot. Adding WARN
    message emphasizes more to debug wrt clk-rpmh, which overall saves efforts
    to analyze failure kernel logs.
    
    Change-Id: Ic210f04f9d2051519bc870712bfb006e12617d9b
    Signed-off-by: Vivek Aknurwar <viveka@codeaurora.org>

--- drivers/clk/qcom/clk-rpmh.c
+++ drivers/clk/qcom/clk-rpmh.c
@@ -617,7 +617,7 @@ static int clk_rpmh_probe(struct platform_device *pdev)
 		if (!res_addr) {
 			if (rpmh_clk->optional)
 				continue;
-			dev_err(&pdev->dev, "missing RPMh resource address for %s\n",
+			WARN(1, "clk-rpmh: Missing RPMh resource address for %s\n",
 				rpmh_clk->res_name);
 			return -ENODEV;
 		}
@@ -625,8 +625,7 @@ static int clk_rpmh_probe(struct platform_device *pdev)
 		data = cmd_db_read_aux_data(rpmh_clk->res_name, &aux_data_len);
 		if (IS_ERR(data)) {
 			ret = PTR_ERR(data);
-			dev_err(&pdev->dev,
-				"error reading RPMh aux data for %s (%d)\n",
+			WARN(1, "clk-rpmh: error reading RPMh aux data for %s (%d)\n",
 				rpmh_clk->res_name, ret);
 			return ret;
 		}
