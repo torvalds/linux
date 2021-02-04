50f9a6c254c60bbad4cde050dbc39b46b61f7a5d
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2021-02-04 13:43:57 +0100

    gpio: uapi: use the preferred SPDX license identifier
    GPL-2.0 license identifier is deprecated. User-space projects that want
    to include the kernel header with their source-code will be unable to
    become fully REUSE compliant due to the reuse tool complaining about
    deprecated licenses. Change the SPDX identifier to GPL-2.0-only.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>

--- include/uapi/linux/gpio.h
+++ include/uapi/linux/gpio.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+/* SPDX-License-Identifier: GPL-2.0-only WITH Linux-syscall-note */
 /*
  * <linux/gpio.h> - userspace ABI for the GPIO character devices
  *
