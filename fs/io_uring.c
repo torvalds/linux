aec18a57edad562d620f7d19016de1fc0cc2208c
Pavel Begunkov <asml.silence@gmail.com>
2021-02-04 19:22:46 +0000

    io_uring: drop mm/files between task_work_submit
    Since SQPOLL task can be shared and so task_work entries can be a mix of
    them, we need to drop mm and files before trying to issue next request.
    
    Cc: stable@vger.kernel.org # 5.10+
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -2205,6 +2205,9 @@ static void __io_req_task_submit(struct io_kiocb *req)
 	else
 		__io_req_task_cancel(req, -EFAULT);
 	mutex_unlock(&ctx->uring_lock);
+
+	if (ctx->flags & IORING_SETUP_SQPOLL)
+		io_sq_thread_drop_mm_files();
 }
 
 static void io_req_task_submit(struct callback_head *cb)
