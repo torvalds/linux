2e9f4afadc702d18f19e8c1183b10307e871a1d7
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-02-06 00:02:15 +0200

    net: mscc: ocelot: avoid unneeded "lp" variable in LAG join
    The index of the LAG is equal to the logical port ID that all the
    physical port members have, which is further equal to the index of the
    first physical port that is a member of the LAG.
    
    The code gets a bit carried away with logic like this:
    
    	if (a == b)
    		c = a;
    	else
    		c = b;
    
    which can be simplified, of course, into:
    
    	c = b;
    
    (with a being port, b being lp, c being lag)
    
    This further makes the "lp" variable redundant, since we can use "lag"
    everywhere where "lp" (logical port) was used. So instead of a "c = b"
    assignment, we can do a complete deletion of b. Only one comment here:
    
    		if (bond_mask) {
    			lp = __ffs(bond_mask);
    			ocelot->lags[lp] = 0;
    		}
    
    lp was clobbered before, because it was used as a temporary variable to
    hold the new smallest port ID from the bond. Now that we don't have "lp"
    any longer, we'll just avoid the temporary variable and zeroize the
    bonding mask directly.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Reviewed-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/mscc/ocelot.c
+++ drivers/net/ethernet/mscc/ocelot.c
@@ -1338,7 +1338,7 @@ int ocelot_port_lag_join(struct ocelot *ocelot, int port,
 			 struct netdev_lag_upper_info *info)
 {
 	u32 bond_mask = 0;
-	int lag, lp;
+	int lag;
 
 	if (info->tx_type != NETDEV_LAG_TX_TYPE_HASH)
 		return -EOPNOTSUPP;
@@ -1347,22 +1347,18 @@ int ocelot_port_lag_join(struct ocelot *ocelot, int port,
 
 	bond_mask = ocelot_get_bond_mask(ocelot, bond);
 
-	lp = __ffs(bond_mask);
+	lag = __ffs(bond_mask);
 
 	/* If the new port is the lowest one, use it as the logical port from
 	 * now on
 	 */
-	if (port == lp) {
-		lag = port;
+	if (port == lag) {
 		ocelot->lags[port] = bond_mask;
 		bond_mask &= ~BIT(port);
-		if (bond_mask) {
-			lp = __ffs(bond_mask);
-			ocelot->lags[lp] = 0;
-		}
+		if (bond_mask)
+			ocelot->lags[__ffs(bond_mask)] = 0;
 	} else {
-		lag = lp;
-		ocelot->lags[lp] |= BIT(port);
+		ocelot->lags[lag] |= BIT(port);
 	}
 
 	ocelot_setup_lag(ocelot, lag);
