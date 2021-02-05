97860deed342fe3264e01f3e1494444a71073d6a
Saravana Kannan <saravanak@google.com>
2021-02-05 14:26:42 -0800

    UPSTREAM: irqdomain: Mark fwnodes when their irqdomain is added/removed
    This allows fw_devlink to recognize irqdomain drivers that don't use the
    device-driver model to initialize the device. fw_devlink will use this
    information to make sure consumers of such irqdomain aren't indefinitely
    blocked from probing, waiting for the irqdomain device to appear and
    bind to a driver.
    
    Tested-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Link: https://lore.kernel.org/r/20210205222644.2357303-7-saravanak@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit ed1054a02aa2323f1676093d6e58cde4484d8867)
    Bug: 181264536
    Change-Id: I9e97c4b27d09f7ab60b1a3bc67c360e550d8bab0

--- kernel/irq/irqdomain.c
+++ kernel/irq/irqdomain.c
@@ -196,6 +196,7 @@ struct irq_domain *__irq_domain_add(struct fwnode_handle *fwnode, int size,
 	}
 
 	fwnode_handle_get(fwnode);
+	fwnode_dev_initialized(fwnode, true);
 
 	/* Fill structure */
 	INIT_RADIX_TREE(&domain->revmap_tree, GFP_KERNEL);
@@ -244,6 +245,7 @@ void irq_domain_remove(struct irq_domain *domain)
 
 	pr_debug("Removed domain %s\n", domain->name);
 
+	fwnode_dev_initialized(domain->fwnode, false);
 	fwnode_handle_put(domain->fwnode);
 	if (domain->flags & IRQ_DOMAIN_NAME_ALLOCATED)
 		kfree(domain->name);
