ba3bcb35d783805f2b3ebf2ebf61702cd68f4959
Horatiu Vultur <horatiu.vultur@microchip.com>
2021-02-06 22:47:34 +0100

    switchdev: mrp: Remove SWITCHDEV_ATTR_ID_MRP_PORT_STAT
    commit 059d2a1004981dce19f0127dabc1b4ec927d202a upstream.
    
    Now that MRP started to use also SWITCHDEV_ATTR_ID_PORT_STP_STATE to
    notify HW, then SWITCHDEV_ATTR_ID_MRP_PORT_STAT is not used anywhere
    else, therefore we can remove it.
    
    Fixes: c284b545900830 ("switchdev: mrp: Extend switchdev API to offload MRP")
    Signed-off-by: Horatiu Vultur <horatiu.vultur@microchip.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/net/switchdev.h
+++ include/net/switchdev.h
@@ -41,7 +41,6 @@ enum switchdev_attr_id {
 	SWITCHDEV_ATTR_ID_BRIDGE_MC_DISABLED,
 	SWITCHDEV_ATTR_ID_BRIDGE_MROUTER,
 #if IS_ENABLED(CONFIG_BRIDGE_MRP)
-	SWITCHDEV_ATTR_ID_MRP_PORT_STATE,
 	SWITCHDEV_ATTR_ID_MRP_PORT_ROLE,
 #endif
 };
@@ -60,7 +59,6 @@ struct switchdev_attr {
 		bool vlan_filtering;			/* BRIDGE_VLAN_FILTERING */
 		bool mc_disabled;			/* MC_DISABLED */
 #if IS_ENABLED(CONFIG_BRIDGE_MRP)
-		u8 mrp_port_state;			/* MRP_PORT_STATE */
 		u8 mrp_port_role;			/* MRP_PORT_ROLE */
 #endif
 	} u;
