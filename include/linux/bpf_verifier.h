f3c4b01689d392373301e6e60d1b02c5b4020afc
Andrei Matei <andreimatei1@gmail.com>
2021-02-06 20:10:24 -0500

    bpf: Allow variable-offset stack access
    [ Upstream commit 01f810ace9ed37255f27608a0864abebccf0aab3 ]
    
    Before this patch, variable offset access to the stack was dissalowed
    for regular instructions, but was allowed for "indirect" accesses (i.e.
    helpers). This patch removes the restriction, allowing reading and
    writing to the stack through stack pointers with variable offsets. This
    makes stack-allocated buffers more usable in programs, and brings stack
    pointers closer to other types of pointers.
    
    The motivation is being able to use stack-allocated buffers for data
    manipulation. When the stack size limit is sufficient, allocating
    buffers on the stack is simpler than per-cpu arrays, or other
    alternatives.
    
    In unpriviledged programs, variable-offset reads and writes are
    disallowed (they were already disallowed for the indirect access case)
    because the speculative execution checking code doesn't support them.
    Additionally, when writing through a variable-offset stack pointer, if
    any pointers are in the accessible range, there's possilibities of later
    leaking pointers because the write cannot be tracked precisely.
    
    Writes with variable offset mark the whole range as initialized, even
    though we don't know which stack slots are actually written. This is in
    order to not reject future reads to these slots. Note that this doesn't
    affect writes done through helpers; like before, helpers need the whole
    stack range to be initialized to begin with.
    All the stack slots are in range are considered scalars after the write;
    variable-offset register spills are not tracked.
    
    For reads, all the stack slots in the variable range needs to be
    initialized (but see above about what writes do), otherwise the read is
    rejected. All register spilled in stack slots that might be read are
    marked as having been read, however reads through such pointers don't do
    register filling; the target register will always be either a scalar or
    a constant zero.
    
    Signed-off-by: Andrei Matei <andreimatei1@gmail.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20210207011027.676572-2-andreimatei1@gmail.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/linux/bpf_verifier.h
+++ include/linux/bpf_verifier.h
@@ -187,7 +187,7 @@ struct bpf_func_state {
 	 * 0 = main function, 1 = first callee.
 	 */
 	u32 frameno;
-	/* subprog number == index within subprog_stack_depth
+	/* subprog number == index within subprog_info
 	 * zero == main subprog
 	 */
 	u32 subprogno;
@@ -390,6 +390,7 @@ struct bpf_verifier_env {
 	u32 used_map_cnt;		/* number of used maps */
 	u32 id_gen;			/* used to generate unique reg IDs */
 	bool allow_ptr_leaks;
+	bool allow_uninit_stack;
 	bool allow_ptr_to_map_access;
 	bool bpf_capable;
 	bool bypass_spec_v1;
