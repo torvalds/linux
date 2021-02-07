6e4ea8aace02479186b3fdaab48d7acfe06d8715
Sameer Pujar <spujar@nvidia.com>
2021-02-07 23:06:57 +0530

    ASoC: audio-graph: Rename functions needed for export
    Following functions are renamed for a better global visibility.
      graph_card_probe() --> audio_graph_card_probe()
      graph_parse_of()   --> audio_graph_parse_of()
      graph_remove()     --> audio_graph_remove() [exported as well]
    
    The references of these are updated in audio graph and Tegra audio
    graph card drivers.
    
    Signed-off-by: Sameer Pujar <spujar@nvidia.com>
    Cc: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Acked-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://lore.kernel.org/r/1612719418-5858-2-git-send-email-spujar@nvidia.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/tegra/tegra_audio_graph_card.c
+++ sound/soc/tegra/tegra_audio_graph_card.c
@@ -184,7 +184,7 @@ static int tegra_audio_graph_card_probe(struct snd_soc_card *card)
 		return PTR_ERR(priv->clk_plla_out0);
 	}
 
-	return graph_card_probe(card);
+	return audio_graph_card_probe(card);
 }
 
 static int tegra_audio_graph_probe(struct platform_device *pdev)
@@ -201,12 +201,12 @@ static int tegra_audio_graph_probe(struct platform_device *pdev)
 
 	card->probe = tegra_audio_graph_card_probe;
 
-	/* graph_parse_of() depends on below */
+	/* audio_graph_parse_of() depends on below */
 	card->component_chaining = 1;
 	priv->simple.ops = &tegra_audio_graph_ops;
 	priv->simple.force_dpcm = 1;
 
-	return graph_parse_of(&priv->simple, dev);
+	return audio_graph_parse_of(&priv->simple, dev);
 }
 
 static const struct tegra_audio_cdata tegra210_data = {
