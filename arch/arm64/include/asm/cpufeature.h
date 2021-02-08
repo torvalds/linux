a31f6cf381a6f89c9f3ac4992376d2c92b07ce14
Marc Zyngier <maz@kernel.org>
2021-02-08 09:57:20 +0000

    FROMGIT: arm64: cpufeature: Use IDreg override in __read_sysreg_by_encoding()
    __read_sysreg_by_encoding() is used by a bunch of cpufeature helpers,
    which should take the feature override into account. Let's do that.
    
    For a good measure (and because we are likely to need to further
    down the line), make this helper available to the rest of the
    non-modular kernel.
    
    Code that needs to know the *real* features of a CPU can still
    use read_sysreg_s(), and find the bare, ugly truth.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Reviewed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Acked-by: David Brazdil <dbrazdil@google.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/20210208095732.3267263-12-maz@kernel.org
    Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit b3341ae0efa235726ad69e53ce83c6a3c445bda8
     git://git.kernel.org/pub/scm/linux/kernel/git/arm64/linux.git for-next/cpufeature)
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Bug: 175544340
    Change-Id: Iac1fc235a5611efcd2c45dd6ec813e535e7deac9

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -606,6 +606,7 @@ void __init setup_cpu_features(void);
 void check_local_cpu_capabilities(void);
 
 u64 read_sanitised_ftr_reg(u32 id);
+u64 __read_sysreg_by_encoding(u32 sys_id);
 
 static inline bool cpu_supports_mixed_endian_el0(void)
 {
