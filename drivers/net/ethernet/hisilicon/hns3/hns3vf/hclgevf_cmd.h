7ceb40b8207ea4aefed96c6dd22625b93aa121f9
Peng Li <lipeng321@huawei.com>
2021-02-09 10:42:00 +0800

    net: hns3: remove unused macro definition
    Some macros are defined but unused, so remove them.
    
    Signed-off-by: Peng Li <lipeng321@huawei.com>
    Signed-off-by: Huazhong Tan <tanhuazhong@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/hisilicon/hns3/hns3vf/hclgevf_cmd.h
+++ drivers/net/ethernet/hisilicon/hns3/hns3vf/hclgevf_cmd.h
@@ -278,7 +278,6 @@ struct hclgevf_cfg_tx_queue_pointer_cmd {
 
 #define HCLGEVF_NIC_CMQ_DESC_NUM	1024
 #define HCLGEVF_NIC_CMQ_DESC_NUM_S	3
-#define HCLGEVF_NIC_CMDQ_INT_SRC_REG	0x27100
 
 #define HCLGEVF_QUERY_DEV_SPECS_BD_NUM		4
 
