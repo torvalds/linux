63dbb0db3af0a053f6f29a1b530ec74223977773
Felix Kuehling <Felix.Kuehling@amd.com>
2021-02-11 16:02:05 -0500

    drm/amdgpu: Make noretry the default on Aldebaran
    This is needed for best machine learning performance. XNACK can still
    be enabled per-process if needed.
    
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Philip Yang <Philip.Yang@amd.com>
    Tested-by: Alex Sierra <alex.sierra@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_gmc.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_gmc.c
@@ -514,6 +514,7 @@ void amdgpu_gmc_noretry_set(struct amdgpu_device *adev)
 	switch (adev->asic_type) {
 	case CHIP_VEGA10:
 	case CHIP_VEGA20:
+	case CHIP_ALDEBARAN:
 		/*
 		 * noretry = 0 will cause kfd page fault tests fail
 		 * for some ASICs, so set default to 1 for these ASICs.
