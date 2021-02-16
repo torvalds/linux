fb0f236beccbf74da90429d417864cfc6fc6673a
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2021-02-16 17:50:50 +0200

    leds: lp50xx: Update headers block to reflect reality
    The OF is not used in the driver, thus the OF headers are not needed,
    but mod_devicetable.h is missed.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Pavel Machek <pavel@ucw.cz>

--- drivers/leds/leds-lp50xx.c
+++ drivers/leds/leds-lp50xx.c
@@ -6,10 +6,9 @@
 #include <linux/i2c.h>
 #include <linux/init.h>
 #include <linux/leds.h>
+#include <linux/mod_devicetable.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
-#include <linux/of.h>
-#include <linux/of_gpio.h>
 #include <linux/regmap.h>
 #include <linux/regulator/consumer.h>
 #include <linux/slab.h>
