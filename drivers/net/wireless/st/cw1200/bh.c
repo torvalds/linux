1c22233a745e46dbc11db30d1f65aaae01e26eb5
Chen Lin <chen.lin5@zte.com.cn>
2021-02-16 11:41:58 +0800

    cw1200: Remove unused function pointer typedef cw1200_wsm_handler
    Remove the 'cw1200_wsm_handler' typedef as it is not used.
    
    Signed-off-by: Chen Lin <chen.lin5@zte.com.cn>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/1613446918-4532-1-git-send-email-chen45464546@163.com

--- drivers/net/wireless/st/cw1200/bh.c
+++ drivers/net/wireless/st/cw1200/bh.c
@@ -42,9 +42,6 @@ enum cw1200_bh_pm_state {
 	CW1200_BH_RESUME,
 };
 
-typedef int (*cw1200_wsm_handler)(struct cw1200_common *priv,
-	u8 *data, size_t size);
-
 static void cw1200_bh_work(struct work_struct *work)
 {
 	struct cw1200_common *priv =
