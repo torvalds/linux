320c8057eceb18c5d836fcbe0ffb0035fcfe28ff
Jens Axboe <axboe@kernel.dk>
2021-02-17 08:48:00 -0700

    arch: setup PF_IO_WORKER threads like PF_KTHREAD
    [ Upstream commit 4727dc20e0422211a0e0c72b1ace4ed6096df8a6 ]
    
    PF_IO_WORKER are kernel threads too, but they aren't PF_KTHREAD in the
    sense that we don't assign ->set_child_tid with our own structure. Just
    ensure that every arch sets up the PF_IO_WORKER threads like kthreads
    in the arch implementation of copy_thread().
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/openrisc/kernel/process.c
+++ arch/openrisc/kernel/process.c
@@ -167,7 +167,7 @@ copy_thread(unsigned long clone_flags, unsigned long usp, unsigned long arg,
 	sp -= sizeof(struct pt_regs);
 	kregs = (struct pt_regs *)sp;
 
-	if (unlikely(p->flags & PF_KTHREAD)) {
+	if (unlikely(p->flags & (PF_KTHREAD | PF_IO_WORKER))) {
 		memset(kregs, 0, sizeof(struct pt_regs));
 		kregs->gpr[20] = usp; /* fn, kernel thread */
 		kregs->gpr[22] = arg;
