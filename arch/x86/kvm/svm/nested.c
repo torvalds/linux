d2df592fd8c6c14a43e08314a91101d60b32da01
Paolo Bonzini <pbonzini@redhat.com>
2021-02-18 09:55:39 -0500

    KVM: nSVM: prepare guest save area while is_guest_mode is true
    Right now, enter_svm_guest_mode is calling nested_prepare_vmcb_save and
    nested_prepare_vmcb_control.  This results in is_guest_mode being false
    until the end of nested_prepare_vmcb_control.
    
    This is a problem because nested_prepare_vmcb_save can in turn cause
    changes to the intercepts and these have to be applied to the "host VMCB"
    (stored in svm->nested.hsave) and then merged with the VMCB12 intercepts
    into svm->vmcb.
    
    In particular, without this change we forget to set the CR0 read and CR0
    write intercepts when running a real mode L2 guest with NPT disabled.
    The guest is therefore able to see the CR0.PG bit that KVM sets to
    enable "paged real mode".  This patch fixes the svm.flat mode_switch
    test case with npt=0.  There are no other problematic calls in
    nested_prepare_vmcb_save.
    
    Moving is_guest_mode to the end is done since commit 06fc7772690d
    ("KVM: SVM: Activate nested state only when guest state is complete",
    2010-04-25).  However, back then KVM didn't grab a different VMCB
    when updating the intercepts, it had already copied/merged L1's stuff
    to L0's VMCB, and then updated L0's VMCB regardless of is_nested().
    Later recalc_intercepts was introduced in commit 384c63684397
    ("KVM: SVM: Add function to recalculate intercept masks", 2011-01-12).
    This introduced the bug, because recalc_intercepts now throws away
    the intercept manipulations that svm_set_cr0 had done in the meanwhile
    to svm->vmcb.
    
    [1] https://lore.kernel.org/kvm/1266493115-28386-1-git-send-email-joerg.roedel@amd.com/
    
    Reviewed-by: Sean Christopherson <seanjc@google.com>
    Tested-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/svm/nested.c
+++ arch/x86/kvm/svm/nested.c
@@ -469,8 +469,8 @@ int enter_svm_guest_mode(struct vcpu_svm *svm, u64 vmcb12_gpa,
 
 	svm->nested.vmcb12_gpa = vmcb12_gpa;
 	load_nested_vmcb_control(svm, &vmcb12->control);
-	nested_prepare_vmcb_save(svm, vmcb12);
 	nested_prepare_vmcb_control(svm);
+	nested_prepare_vmcb_save(svm, vmcb12);
 
 	ret = nested_svm_load_cr3(&svm->vcpu, vmcb12->save.cr3,
 				  nested_npt_enabled(svm));
