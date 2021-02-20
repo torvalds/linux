b8719b37cd609f443daa8c4f47817fa6ab37e520
Chao Yu <yuchao0@huawei.com>
2021-02-20 17:38:41 +0800

    f2fs: avoid unused f2fs_show_compress_options()
    LKP reports:
    
       fs/f2fs/super.c:1516:20: warning: unused function 'f2fs_show_compress_options' [-Wunused-function]
       static inline void f2fs_show_compress_options(struct seq_file *seq,
    
    Fix this issue by covering f2fs_show_compress_options() with
    CONFIG_F2FS_FS_COMPRESSION macro.
    
    Fixes: 4c8ff7095bef ("f2fs: support data compression")
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1619,6 +1619,7 @@ static inline void f2fs_show_quota_options(struct seq_file *seq,
 #endif
 }
 
+#ifdef CONFIG_F2FS_FS_COMPRESSION
 static inline void f2fs_show_compress_options(struct seq_file *seq,
 							struct super_block *sb)
 {
@@ -1664,6 +1665,7 @@ static inline void f2fs_show_compress_options(struct seq_file *seq,
 	else if (F2FS_OPTION(sbi).compress_mode == COMPR_MODE_USER)
 		seq_printf(seq, ",compress_mode=%s", "user");
 }
+#endif
 
 static int f2fs_show_options(struct seq_file *seq, struct dentry *root)
 {
