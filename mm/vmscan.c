aa8f690d71000bb3861347d46c3c8cb6a9894f21
Suren Baghdasaryan <surenb@google.com>
2021-02-22 18:13:47 -0800

    ANDROID: vmscan: Fix sparse warnings for kswapd_threads
    Fix the following warning by declaring kswapd_threads as static:
    mm/vmscan.c:175:5: sparse: symbol 'kswapd_threads' was not declared.
    
    Fixes: 0d61a651e4dd ("ANDROID: vmscan: Support multiple kswapd threads per node")
    Bug: 171351667
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: I6ccb1e0e0f597fda27ed6893d254a77341139084

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -172,7 +172,7 @@ struct scan_control {
 int vm_swappiness = 60;
 
 #define DEF_KSWAPD_THREADS_PER_NODE 1
-int kswapd_threads = DEF_KSWAPD_THREADS_PER_NODE;
+static int kswapd_threads = DEF_KSWAPD_THREADS_PER_NODE;
 static int __init kswapd_per_node_setup(char *str)
 {
 	int tmp;
