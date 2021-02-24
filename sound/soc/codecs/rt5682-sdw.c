628fc9d9a0e6e52fd3116b5591cb4e90473db9ad
Rikard Falkeborn <rikard.falkeborn@gmail.com>
2021-02-24 22:19:15 +0100

    ASoC: rt*: Constify static struct sdw_slave_ops
    The only usage of these is to assign their address to the ops field in
    the sdw_driver struct, which is a pointer to const. Make them const to
    allow the compiler to put them in read-only memory.
    
    Signed-off-by: Rikard Falkeborn <rikard.falkeborn@gmail.com>
    Link: https://lore.kernel.org/r/20210224211918.39109-2-rikard.falkeborn@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/rt5682-sdw.c
+++ sound/soc/codecs/rt5682-sdw.c
@@ -683,7 +683,7 @@ static int rt5682_interrupt_callback(struct sdw_slave *slave,
 	return 0;
 }
 
-static struct sdw_slave_ops rt5682_slave_ops = {
+static const struct sdw_slave_ops rt5682_slave_ops = {
 	.read_prop = rt5682_read_prop,
 	.interrupt_callback = rt5682_interrupt_callback,
 	.update_status = rt5682_update_status,
