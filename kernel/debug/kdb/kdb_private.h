d027fdc4fa30a409e7425219d0d089e4e4ebdc4d
Sumit Garg <sumit.garg@linaro.org>
2021-02-24 12:46:53 +0530

    kdb: Remove redundant function definitions/prototypes
    Cleanup kdb code to get rid of unused function definitions/prototypes.
    
    Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
    Link: https://lore.kernel.org/r/20210224071653.409150-1-sumit.garg@linaro.org
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Signed-off-by: Daniel Thompson <daniel.thompson@linaro.org>

--- kernel/debug/kdb/kdb_private.h
+++ kernel/debug/kdb/kdb_private.h
@@ -207,9 +207,7 @@ extern unsigned long kdb_task_state(const struct task_struct *p,
 				    unsigned long mask);
 extern void kdb_ps_suppressed(void);
 extern void kdb_ps1(const struct task_struct *p);
-extern void kdb_print_nameval(const char *name, unsigned long val);
 extern void kdb_send_sig(struct task_struct *p, int sig);
-extern void kdb_meminfo_proc_show(void);
 extern char kdb_getchar(void);
 extern char *kdb_getstr(char *, size_t, const char *);
 extern void kdb_gdb_state_pass(char *buf);
