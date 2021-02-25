75b5f9edb5fd23dbed274f946a2b4a19bbaaa234
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2021-02-24 17:56:42 -0800

    nvmet: replace white spaces with tabs
    Instead of the using the whitespaces use tab spacing in the
    nvmet_execute_identify_ns().
    
    Signed-off-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    Reviewed-by: Sagi Grimberg <sagi@grimberg.me>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/nvme/target/admin-cmd.c
+++ drivers/nvme/target/admin-cmd.c
@@ -513,7 +513,7 @@ static void nvmet_execute_identify_ns(struct nvmet_req *req)
 	default:
 		id->nuse = id->nsze;
 		break;
-        }
+	}
 
 	if (req->ns->bdev)
 		nvmet_bdev_set_limits(req->ns->bdev, id);
