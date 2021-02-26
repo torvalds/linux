ff96a0a256a413e9d047397fa5362d9bb4dd00e7
Huang Yiwei <hyiwei@codeaurora.org>
2021-02-26 16:41:15 +0800

    ANDROID: timer: calc_index vendor hook adjustment
    Move the calc_index vendor hook one line ahead to cover more
    use cases.
    
    Bug: 181296757
    Signed-off-by: Huang Yiwei <hyiwei@codeaurora.org>
    Change-Id: I52231a3ccbe622021232c7a54354c5ac02cf3952

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -506,8 +506,8 @@ static inline unsigned calc_index(unsigned long expires, unsigned lvl,
 	 *
 	 * Round up with level granularity to prevent this.
 	 */
-	expires = (expires + LVL_GRAN(lvl)) >> LVL_SHIFT(lvl);
 	trace_android_vh_timer_calc_index(lvl, &expires);
+	expires = (expires + LVL_GRAN(lvl)) >> LVL_SHIFT(lvl);
 	*bucket_expiry = expires << LVL_SHIFT(lvl);
 	return LVL_OFFS(lvl) + (expires & LVL_MASK);
 }
