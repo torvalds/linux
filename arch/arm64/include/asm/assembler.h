4c4dcd3541f83d216f2e403cb83dd431e09759b1
Ard Biesheuvel <ardb@kernel.org>
2021-03-02 10:01:11 +0100

    arm64: assembler: introduce wxN aliases for wN registers
    The AArch64 asm syntax has this slightly tedious property that the names
    used in mnemonics to refer to registers depend on whether the opcode in
    question targets the entire 64-bits (xN), or only the least significant
    8, 16 or 32 bits (wN). When writing parameterized code such as macros,
    this can be annoying, as macro arguments don't lend themselves to
    indexed lookups, and so generating a reference to wN in a macro that
    receives xN as an argument is problematic.
    
    For instance, an upcoming patch that modifies the implementation of the
    cond_yield macro to be able to refer to 32-bit registers would need to
    modify invocations such as
    
      cond_yield	3f, x8
    
    to
    
      cond_yield	3f, 8
    
    so that the second argument can be token pasted after x or w to emit the
    correct register reference. Unfortunately, this interferes with the self
    documenting nature of the first example, where the second argument is
    obviously a register, whereas in the second example, one would need to
    go and look at the code to find out what '8' means.
    
    So let's fix this by defining wxN aliases for all xN registers, which
    resolve to the 32-bit alias of each respective 64-bit register. This
    allows the macro implementation to paste the xN reference after a w to
    obtain the correct register name.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Acked-by: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20210302090118.30666-3-ardb@kernel.org
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/assembler.h
+++ arch/arm64/include/asm/assembler.h
@@ -23,6 +23,14 @@
 #include <asm/ptrace.h>
 #include <asm/thread_info.h>
 
+	/*
+	 * Provide a wxN alias for each wN register so what we can paste a xN
+	 * reference after a 'w' to obtain the 32-bit version.
+	 */
+	.irp	n,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30
+	wx\n	.req	w\n
+	.endr
+
 	.macro save_and_disable_daif, flags
 	mrs	\flags, daif
 	msr	daifset, #0xf
