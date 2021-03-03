e36299efe7d749976fbdaaf756dee6ef32543c2c
Marco Elver <elver@google.com>
2021-03-03 10:38:45 +0100

    kcsan, debugfs: Move debugfs file creation out of early init
    Commit 56348560d495 ("debugfs: do not attempt to create a new file
    before the filesystem is initalized") forbids creating new debugfs files
    until debugfs is fully initialized.  This means that KCSAN's debugfs
    file creation, which happened at the end of __init(), no longer works.
    And was apparently never supposed to work!
    
    However, there is no reason to create KCSAN's debugfs file so early.
    This commit therefore moves its creation to a late_initcall() callback.
    
    Cc: "Rafael J. Wysocki" <rafael@kernel.org>
    Cc: stable <stable@vger.kernel.org>
    Fixes: 56348560d495 ("debugfs: do not attempt to create a new file before the filesystem is initalized")
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Marco Elver <elver@google.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/kcsan/debugfs.c
+++ kernel/kcsan/debugfs.c
@@ -261,7 +261,9 @@ static const struct file_operations debugfs_ops =
 	.release = single_release
 };
 
-void __init kcsan_debugfs_init(void)
+static void __init kcsan_debugfs_init(void)
 {
 	debugfs_create_file("kcsan", 0644, NULL, NULL, &debugfs_ops);
 }
+
+late_initcall(kcsan_debugfs_init);
