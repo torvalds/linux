417789df4a03bc820b082bcc503f0d4c5e4704b9
Paolo Abeni <pabeni@redhat.com>
2021-03-04 13:32:15 -0800

    mptcp: fix missing wakeup
    __mptcp_clean_una() can free write memory and should wake-up
    user-space processes when needed.
    
    When such function is invoked by the MPTCP receive path, the wakeup
    is not needed, as the TCP stack will later trigger subflow_write_space
    which will do the wakeup as needed.
    
    Other __mptcp_clean_una() call sites need an additional wakeup check
    Let's bundle the relevant code in a new helper and use it.
    
    Closes: https://github.com/multipath-tcp/mptcp_net-next/issues/165
    Fixes: 6e628cd3a8f7 ("mptcp: use mptcp release_cb for delayed tasks")
    Fixes: 64b9cea7a0af ("mptcp: fix spurious retransmissions")
    Tested-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -1061,6 +1061,12 @@ out:
 	}
 }
 
+static void __mptcp_clean_una_wakeup(struct sock *sk)
+{
+	__mptcp_clean_una(sk);
+	mptcp_write_space(sk);
+}
+
 static void mptcp_enter_memory_pressure(struct sock *sk)
 {
 	struct mptcp_subflow_context *subflow;
@@ -2270,7 +2276,7 @@ static void __mptcp_retrans(struct sock *sk)
 	struct sock *ssk;
 	int ret;
 
-	__mptcp_clean_una(sk);
+	__mptcp_clean_una_wakeup(sk);
 	dfrag = mptcp_rtx_head(sk);
 	if (!dfrag)
 		return;
@@ -2983,7 +2989,7 @@ static void mptcp_release_cb(struct sock *sk)
 	}
 
 	if (test_and_clear_bit(MPTCP_CLEAN_UNA, &mptcp_sk(sk)->flags))
-		__mptcp_clean_una(sk);
+		__mptcp_clean_una_wakeup(sk);
 	if (test_and_clear_bit(MPTCP_ERROR_REPORT, &mptcp_sk(sk)->flags))
 		__mptcp_error_report(sk);
 
