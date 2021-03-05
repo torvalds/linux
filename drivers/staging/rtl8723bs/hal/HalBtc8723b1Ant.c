74dbe4034b37c9df9d282459d95e4601e1bd4b18
Marco Cesati <marcocesati@gmail.com>
2021-03-05 11:01:46 +0100

    staging: HalBtc8723b1Ant: remove useless typedef's
    This commit fixes the following checkpatch.pl warnings:
    
        WARNING: do not add new typedefs
        #24: FILE: hal/HalBtc8723b1Ant.h:24:
        +typedef enum _BT_INFO_SRC_8723B_1ANT {
    
        WARNING: do not add new typedefs
        #31: FILE: hal/HalBtc8723b1Ant.h:31:
        +typedef enum _BT_8723B_1ANT_BT_STATUS {
    
        WARNING: do not add new typedefs
        #41: FILE: hal/HalBtc8723b1Ant.h:41:
        +typedef enum _BT_8723B_1ANT_WIFI_STATUS {
    
        WARNING: do not add new typedefs
        #51: FILE: hal/HalBtc8723b1Ant.h:51:
        +typedef enum _BT_8723B_1ANT_COEX_ALGO {
    
        WARNING: do not add new typedefs
        #66: FILE: hal/HalBtc8723b1Ant.h:66:
        +typedef struct _COEX_DM_8723B_1ANT {
    
        WARNING: do not add new typedefs
        #121: FILE: hal/HalBtc8723b1Ant.h:121:
        +typedef struct _COEX_STA_8723B_1ANT {
    
    Signed-off-by: Marco Cesati <marco.cesati@gmail.com>
    Link: https://lore.kernel.org/r/20210305100146.30687-1-marco.cesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/HalBtc8723b1Ant.c
+++ drivers/staging/rtl8723bs/hal/HalBtc8723b1Ant.c
@@ -8,10 +8,10 @@
 #include "Mp_Precomp.h"
 
 /*  Global variables, these are static variables */
-static COEX_DM_8723B_1ANT GLCoexDm8723b1Ant;
-static PCOEX_DM_8723B_1ANT pCoexDm = &GLCoexDm8723b1Ant;
-static COEX_STA_8723B_1ANT GLCoexSta8723b1Ant;
-static PCOEX_STA_8723B_1ANT	pCoexSta = &GLCoexSta8723b1Ant;
+static struct COEX_DM_8723B_1ANT GLCoexDm8723b1Ant;
+static struct COEX_DM_8723B_1ANT * pCoexDm = &GLCoexDm8723b1Ant;
+static struct COEX_STA_8723B_1ANT GLCoexSta8723b1Ant;
+static struct COEX_STA_8723B_1ANT * pCoexSta = &GLCoexSta8723b1Ant;
 
 static const char *const GLBtInfoSrc8723b1Ant[] = {
 	"BT Info[wifi fw]",
