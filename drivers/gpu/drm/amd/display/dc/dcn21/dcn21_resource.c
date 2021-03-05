b40528bcc10bc3d4e4f5d765898e64f570d61a19
Holger Hoffstätte <holger@applied-asynchrony.com>
2021-03-05 15:23:18 +0100

    drm/amdgpu/display: use GFP_ATOMIC in dcn21_validate_bandwidth_fp()
    commit 680174cfd1e1cea70a8f30ccb44d8fbdf996018e upstream.
    
    After fixing nested FPU contexts caused by 41401ac67791 we're still seeing
    complaints about spurious kernel_fpu_end(). As it turns out this was
    already fixed for dcn20 in commit f41ed88cbd ("drm/amdgpu/display:
    use GFP_ATOMIC in dcn20_validate_bandwidth_internal") but never moved
    forward to dcn21.
    
    Signed-off-by: Holger Hoffstätte <holger@applied-asynchrony.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/amd/display/dc/dcn21/dcn21_resource.c
+++ drivers/gpu/drm/amd/display/dc/dcn21/dcn21_resource.c
@@ -1183,7 +1183,7 @@ static noinline bool dcn21_validate_bandwidth_fp(struct dc *dc,
 	int vlevel = 0;
 	int pipe_split_from[MAX_PIPES];
 	int pipe_cnt = 0;
-	display_e2e_pipe_params_st *pipes = kzalloc(dc->res_pool->pipe_count * sizeof(display_e2e_pipe_params_st), GFP_KERNEL);
+	display_e2e_pipe_params_st *pipes = kzalloc(dc->res_pool->pipe_count * sizeof(display_e2e_pipe_params_st), GFP_ATOMIC);
 	DC_LOGGER_INIT(dc->ctx->logger);
 
 	BW_VAL_TRACE_COUNT();
