138a6e1dc35ec5d49515547955d0f917065b7213
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2021-03-05 14:02:40 +0200

    gpiolib: Read "gpio-line-names" from a firmware node
    commit b41ba2ec54a70908067034f139aa23d0dd2985ce upstream.
    
    On STM32MP1, the GPIO banks are subnodes of pin-controller@50002000,
    see arch/arm/boot/dts/stm32mp151.dtsi. The driver for
    pin-controller@50002000 is in drivers/pinctrl/stm32/pinctrl-stm32.c
    and iterates over all of its DT subnodes when registering each GPIO
    bank gpiochip. Each gpiochip has:
    
      - gpio_chip.parent = dev,
        where dev is the device node of the pin controller
      - gpio_chip.of_node = np,
        which is the OF node of the GPIO bank
    
    Therefore, dev_fwnode(chip->parent) != of_fwnode_handle(chip.of_node),
    i.e. pin-controller@50002000 != pin-controller@50002000/gpio@5000*000.
    
    The original code behaved correctly, as it extracted the "gpio-line-names"
    from of_fwnode_handle(chip.of_node) = pin-controller@50002000/gpio@5000*000.
    
    To achieve the same behaviour, read property from the firmware node.
    
    Fixes: 7cba1a4d5e162 ("gpiolib: generalize devprop_gpiochip_set_names() for device properties")
    Reported-by: Marek Vasut <marex@denx.de>
    Reported-by: Roman Guskov <rguskov@dh-electronics.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Tested-by: Marek Vasut <marex@denx.de>
    Reviewed-by: Marek Vasut <marex@denx.de>
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -364,22 +364,18 @@ static int gpiochip_set_desc_names(struct gpio_chip *gc)
  *
  * Looks for device property "gpio-line-names" and if it exists assigns
  * GPIO line names for the chip. The memory allocated for the assigned
- * names belong to the underlying software node and should not be released
+ * names belong to the underlying firmware node and should not be released
  * by the caller.
  */
 static int devprop_gpiochip_set_names(struct gpio_chip *chip)
 {
 	struct gpio_device *gdev = chip->gpiodev;
-	struct device *dev = chip->parent;
+	struct fwnode_handle *fwnode = dev_fwnode(&gdev->dev);
 	const char **names;
 	int ret, i;
 	int count;
 
-	/* GPIO chip may not have a parent device whose properties we inspect. */
-	if (!dev)
-		return 0;
-
-	count = device_property_string_array_count(dev, "gpio-line-names");
+	count = fwnode_property_string_array_count(fwnode, "gpio-line-names");
 	if (count < 0)
 		return 0;
 
@@ -393,7 +389,7 @@ static int devprop_gpiochip_set_names(struct gpio_chip *chip)
 	if (!names)
 		return -ENOMEM;
 
-	ret = device_property_read_string_array(dev, "gpio-line-names",
+	ret = fwnode_property_read_string_array(fwnode, "gpio-line-names",
 						names, count);
 	if (ret < 0) {
 		dev_warn(&gdev->dev, "failed to read GPIO line names\n");
