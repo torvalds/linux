bc5c0903a739d50ff4370f2c0b9f4ea97a4149b9
Paul Lawrence <paullawrence@google.com>
2021-03-08 09:32:40 -0800

    ANDROID: Incremental fs: Support STATX_ATTR_VERITY
    Bug: 181242243
    Test: incfs_test passes
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: Id996e0d5d95c8b42254d1e1e0c1dad9317183a17

--- fs/incfs/vfs.c
+++ fs/incfs/vfs.c
@@ -57,6 +57,9 @@ static void evict_inode(struct inode *inode);
 
 static int incfs_setattr(struct user_namespace *ns, struct dentry *dentry,
 			 struct iattr *ia);
+static int incfs_getattr(struct user_namespace *ns, const struct path *path,
+			 struct kstat *stat, u32 request_mask,
+			 unsigned int query_flags);
 static ssize_t incfs_getxattr(struct dentry *d, const char *name,
 			void *value, size_t size);
 static ssize_t incfs_setxattr(struct user_namespace *ns, struct dentry *d,
@@ -125,7 +128,7 @@ const struct file_operations incfs_file_ops = {
 
 const struct inode_operations incfs_file_inode_ops = {
 	.setattr = incfs_setattr,
-	.getattr = simple_getattr,
+	.getattr = incfs_getattr,
 	.listxattr = incfs_listxattr
 };
 
@@ -1560,6 +1563,29 @@ static int incfs_setattr(struct user_namespace *ns, struct dentry *dentry,
 		ia->ia_mode &= ~0222;
 
 	return simple_setattr(ns, dentry, ia);
+}
+
+
+static int incfs_getattr(struct user_namespace *ns, const struct path *path,
+			 struct kstat *stat, u32 request_mask,
+			 unsigned int query_flags)
+{
+	struct inode *inode = d_inode(path->dentry);
+
+	if (IS_VERITY(inode))
+		stat->attributes |= STATX_ATTR_VERITY;
+	stat->attributes_mask |= STATX_ATTR_VERITY;
+	generic_fillattr(ns, inode, stat);
+
+	/*
+	 * TODO: stat->blocks is wrong at this point. It should be number of
+	 * blocks in the backing file. But that information is not (necessarily)
+	 * available yet - incfs_open_dir_file may not have been called.
+	 * Solution is probably to store the backing file block count in an
+	 * xattr whenever it's changed.
+	 */
+
+	return 0;
 }
 
 static ssize_t incfs_getxattr(struct dentry *d, const char *name,
