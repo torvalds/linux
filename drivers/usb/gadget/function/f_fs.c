423432072d163acf0089bf2821d98404539d7d4c
kernel test robot <lkp@intel.com>
2021-03-08 15:09:51 +0800

    USB: gadget: f_fs: fix memdup_user.cocci warnings
    drivers/usb/gadget/function/f_fs.c:3829:8-15: WARNING opportunity for memdup_user
    
     Use memdup_user rather than duplicating its implementation
     This is a little bit restricted to reduce false positives
    
    Generated by: scripts/coccinelle/api/memdup_user.cocci
    
    Fixes: 8704fd73bf56 ("USB: gadget: f_fs: remove likely/unlikely")
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: kernel test robot <lkp@intel.com>
    Link: https://lore.kernel.org/r/20210308070951.GA83949@8a16bdd473dc
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -3827,14 +3827,9 @@ static char *ffs_prepare_buffer(const char __user *buf, size_t len)
 	if (!len)
 		return NULL;
 
-	data = kmalloc(len, GFP_KERNEL);
-	if (!data)
-		return ERR_PTR(-ENOMEM);
-
-	if (copy_from_user(data, buf, len)) {
-		kfree(data);
-		return ERR_PTR(-EFAULT);
-	}
+	data = memdup_user(buf, len);
+	if (IS_ERR(data))
+		return ERR_PTR(PTR_ERR(data));
 
 	pr_vdebug("Buffer from user space:\n");
 	ffs_dump_mem("", data, len);
