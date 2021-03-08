4ca052b4ea621d0002a5e5feace51f60ad5e6b23
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2021-03-08 11:31:28 +0100

    media: uvcvideo: Support devices that report an OT as an entity source
    Some devices reference an output terminal as the source of extension
    units. This is incorrect, as output terminals only have an input pin,
    and thus can't be connected to any entity in the forward direction. The
    resulting topology would cause issues when registering the media
    controller graph. To avoid this problem, connect the extension unit to
    the source of the output terminal instead.
    
    While at it, and while no device has been reported to be affected by
    this issue, also handle forward scans where two output terminals would
    be connected together, and skip the terminals found through such an
    invalid connection.
    
    Reported-and-tested-by: John Nealy <jnealy3@yahoo.com>
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -1716,6 +1716,31 @@ static int uvc_scan_chain_forward(struct uvc_video_chain *chain,
 				return -EINVAL;
 			}
 
+			/*
+			 * Some devices reference an output terminal as the
+			 * source of extension units. This is incorrect, as
+			 * output terminals only have an input pin, and thus
+			 * can't be connected to any entity in the forward
+			 * direction. The resulting topology would cause issues
+			 * when registering the media controller graph. To
+			 * avoid this problem, connect the extension unit to
+			 * the source of the output terminal instead.
+			 */
+			if (UVC_ENTITY_IS_OTERM(entity)) {
+				struct uvc_entity *source;
+
+				source = uvc_entity_by_id(chain->dev,
+							  entity->baSourceID[0]);
+				if (!source) {
+					uvc_dbg(chain->dev, DESCR,
+						"Can't connect extension unit %u in chain\n",
+						forward->id);
+					break;
+				}
+
+				forward->baSourceID[0] = source->id;
+			}
+
 			list_add_tail(&forward->chain, &chain->entities);
 			if (!found)
 				uvc_dbg_cont(PROBE, " (->");
@@ -1733,6 +1758,13 @@ static int uvc_scan_chain_forward(struct uvc_video_chain *chain,
 					"Unsupported input terminal %u\n",
 					forward->id);
 				return -EINVAL;
+			}
+
+			if (UVC_ENTITY_IS_OTERM(entity)) {
+				uvc_dbg(chain->dev, DESCR,
+					"Unsupported connection between output terminals %u and %u\n",
+					entity->id, forward->id);
+				break;
 			}
 
 			list_add_tail(&forward->chain, &chain->entities);
