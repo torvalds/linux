8c44390d8872ebf3a28558b59a0074df39b3da8f
Felix Kuehling <Felix.Kuehling@amd.com>
2021-03-09 15:03:28 -0500

    drm/amdkfd: Bump KFD API version
    Indicate the availability reliable SRAM EDC state in the new bit
    in the device properties.
    
    Proposed userspace changes:
    https://github.com/RadeonOpenCompute/ROCT-Thunk-Interface/commit/7cdd63475c36bb9f49bb960f90f9a8cdb7e80a21
    
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Reviewed-by: Kent Russell <kent.russell@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- include/uapi/linux/kfd_ioctl.h
+++ include/uapi/linux/kfd_ioctl.h
@@ -29,9 +29,10 @@
 /*
  * - 1.1 - initial version
  * - 1.3 - Add SMI events support
+ * - 1.4 - Indicate new SRAM EDC bit in device properties
  */
 #define KFD_IOCTL_MAJOR_VERSION 1
-#define KFD_IOCTL_MINOR_VERSION 3
+#define KFD_IOCTL_MINOR_VERSION 4
 
 struct kfd_ioctl_get_version_args {
 	__u32 major_version;	/* from KFD */
