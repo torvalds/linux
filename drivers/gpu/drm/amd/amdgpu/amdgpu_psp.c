03597b47d69cf5ea88828f34e42d5eb0c17e23fd
Kevin Wang <kevin1.wang@amd.com>
2021-03-09 18:28:59 +0800

    Revert "drm/amdgpu: add psp RAP L0 check support"
    This reverts commit d86fd724e59af96ae5ab6630f4f07a076e9b80cd.
    
    Disable PSP RAP L0 self test until to RAP feature ready.
    
    Signed-off-by: Kevin Wang <kevin1.wang@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_psp.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_psp.c
@@ -2365,7 +2365,6 @@ static int psp_hw_init(void *handle)
 {
 	int ret;
 	struct amdgpu_device *adev = (struct amdgpu_device *)handle;
-	enum ta_rap_status status = TA_RAP_STATUS__SUCCESS;
 
 	mutex_lock(&adev->firmware.mutex);
 	/*
@@ -2383,14 +2382,7 @@ static int psp_hw_init(void *handle)
 	}
 
 	mutex_unlock(&adev->firmware.mutex);
-
-	ret = psp_rap_invoke(&adev->psp, TA_CMD_RAP__VALIDATE_L0, &status);
-	if (ret || status != TA_RAP_STATUS__SUCCESS) {
-		dev_err(adev->dev, "RAP: (%d) Failed to Invoke Validate L0, status %d\n",
-			ret, status);
-	}
-
-	return ret;
+	return 0;
 
 failed:
 	adev->firmware.load_type = AMDGPU_FW_LOAD_DIRECT;
