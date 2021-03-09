1db19c151819dea7a0dc4d888250d25abaf229ca
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2021-03-09 10:08:08 +0900

    ASoC: soc-pcm: fixup dpcm_be_dai_startup() user count
    At dpcm_be_dai_startup_unwind(), it indicates error message at (1)
    if this function was called with no users.
    But, it doesn't use "continue" here. Thus, users will be a
    negative number at (2)
    
    	void dpcm_be_dai_startup_unwind(...)
    	{
    		...
    		for_each_dpcm_be(...) {
    			...
    (1)			if (be->dpcm[stream].users == 0)
    				dev_err(...);
    
    (2)			if (--be->dpcm[stream].users != 0)
    				continue;
    
    At dpcm_be_dai_startup(), it indicates error message if
    user reached to MAX USERS at (A).
    But, it doesn't use "continue" here. Thus, it will be over
    MAX USERS at (B).
    
    	int dpcm_be_dai_startup(...)
    	{
    		...
    		for_each_dpcm_be(...) {
    			...
    (A)			if (be->dpcm[stream].users == DPCM_MAX_BE_USERS)
    				dev_err(...);
    
    (B)			if (be->dpcm[stream].users++ != 0)
    				continue;
    
    These are just bug. This patch fixup these.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://lore.kernel.org/r/87czw9uoav.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/soc-pcm.c
+++ sound/soc/soc-pcm.c
@@ -1432,10 +1432,12 @@ static void dpcm_be_dai_startup_unwind(struct snd_soc_pcm_runtime *fe,
 		struct snd_pcm_substream *be_substream =
 			snd_soc_dpcm_get_substream(be, stream);
 
-		if (be->dpcm[stream].users == 0)
+		if (be->dpcm[stream].users == 0) {
 			dev_err(be->dev, "ASoC: no users %s at close - state %d\n",
 				stream ? "capture" : "playback",
 				be->dpcm[stream].state);
+			continue;
+		}
 
 		if (--be->dpcm[stream].users != 0)
 			continue;
@@ -1472,10 +1474,12 @@ int dpcm_be_dai_startup(struct snd_soc_pcm_runtime *fe, int stream)
 			continue;
 
 		/* first time the dpcm is open ? */
-		if (be->dpcm[stream].users == DPCM_MAX_BE_USERS)
+		if (be->dpcm[stream].users == DPCM_MAX_BE_USERS) {
 			dev_err(be->dev, "ASoC: too many users %s at open %d\n",
 				stream ? "capture" : "playback",
 				be->dpcm[stream].state);
+			continue;
+		}
 
 		if (be->dpcm[stream].users++ != 0)
 			continue;
@@ -1517,10 +1521,12 @@ unwind:
 		if (!snd_soc_dpcm_be_can_update(fe, be, stream))
 			continue;
 
-		if (be->dpcm[stream].users == 0)
+		if (be->dpcm[stream].users == 0) {
 			dev_err(be->dev, "ASoC: no users %s at close %d\n",
 				stream ? "capture" : "playback",
 				be->dpcm[stream].state);
+			continue;
+		}
 
 		if (--be->dpcm[stream].users != 0)
 			continue;
