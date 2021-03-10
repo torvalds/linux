2584d7e7f87ad0a57683dda8e7f17d8630c2eefc
Tony Lindgren <tony@atomide.com>
2021-03-10 14:04:37 +0200

    ARM: OMAP2+: Drop legacy platform data for omap4 hwmod
    We can now probe interconnects with simple-pm-bus and genpd.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/io.c
+++ arch/arm/mach-omap2/io.c
@@ -615,7 +615,6 @@ void __init omap4430_init_early(void)
 	omap44xx_voltagedomains_init();
 	omap44xx_powerdomains_init();
 	omap44xx_clockdomains_init();
-	omap44xx_hwmod_init();
 	omap_hwmod_init_postsetup();
 	omap_l2_cache_init();
 	omap_clk_soc_init = omap4xxx_dt_clk_init;
