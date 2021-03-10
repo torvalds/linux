1c11fac563781df3382a86730e1d6d6fbd50931c
Qingqing Zhuo <qingqing.zhuo@amd.com>
2021-03-10 12:15:29 -0500

    drm/amd/display: Remove unused defines
    [Why]
    CONFIG_DRM_AMD_DC_DCN3_0 has been folded into
    CONFIG_DRM_AMD_DC_DCN and is not needed.
    
    [How]
    Drop CONFIG_DRM_AMD_DC_DCN3_0.
    
    Signed-off-by: Qingqing Zhuo <qingqing.zhuo@amd.com>
    Reviewed-by: Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -976,12 +976,10 @@ static void event_mall_stutter(struct work_struct *work)
 	else
 		dm->active_vblank_irq_count--;
 
-#if defined(CONFIG_DRM_AMD_DC_DCN3_0)
 	dc_allow_idle_optimizations(
 		dm->dc, dm->active_vblank_irq_count == 0);
 
 	DRM_DEBUG_DRIVER("Allow idle optimizations (MALL): %d\n", dm->active_vblank_irq_count == 0);
-#endif
 
 
 	mutex_unlock(&dm->dc_lock);
