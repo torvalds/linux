a620bbaa3510a6f14f6643bf232390ad1c821c80
Michael Kelley <mikelley@microsoft.com>
2021-03-10 10:47:49 -0800

    asm-generic/hyperv: Add missing function prototypes per -W1 warnings
    Add two function prototypes for -W1 warnings generated by the
    kernel test robot.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Michael Kelley <mikelley@microsoft.com>
    Link: https://lore.kernel.org/r/1615402069-39462-1-git-send-email-mikelley@microsoft.com
    Signed-off-by: Wei Liu <wei.liu@kernel.org>

--- include/asm-generic/mshyperv.h
+++ include/asm-generic/mshyperv.h
@@ -94,6 +94,8 @@ static inline void vmbus_signal_eom(struct hv_message *msg, u32 old_msg_type)
 
 void hv_setup_vmbus_handler(void (*handler)(void));
 void hv_remove_vmbus_handler(void);
+void hv_setup_stimer0_handler(void (*handler)(void));
+void hv_remove_stimer0_handler(void);
 
 void hv_setup_kexec_handler(void (*handler)(void));
 void hv_remove_kexec_handler(void);
