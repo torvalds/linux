24149445adaceb3948d5ad5bbb63c62ff2914e83
Liujie Xie <xieliujie@oppo.com>
2021-03-11 20:40:06 +0800

    ANDROID: vendor_hooks: Add hooks for memory when debug
    Add vendors hooks for recording memory used
    
    Bug: 182443489
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    Change-Id: I62d8bb2b6650d8b187b433f97eb833ef0b784df1

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4692,7 +4692,7 @@ __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 	int no_progress_loops;
 	unsigned int cpuset_mems_cookie;
 	int reserve_flags;
-
+	unsigned long alloc_start = jiffies;
 	/*
 	 * We also sanity check to catch abuse of atomic reserves being used by
 	 * callers that are not in atomic context.
@@ -4934,6 +4934,7 @@ fail:
 	warn_alloc(gfp_mask, ac->nodemask,
 			"page allocation failure: order:%u", order);
 got_pg:
+	trace_android_vh_alloc_pages_slowpath(gfp_mask, order, alloc_start);
 	return page;
 }
 
