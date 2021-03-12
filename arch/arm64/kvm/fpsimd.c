b145a8437aab2799969f6ad8e384b557872333c2
Marc Zyngier <maz@kernel.org>
2021-03-12 14:30:52 +0000

    KVM: arm64: Save guest's ZCR_EL1 before saving the FPSIMD state
    Make sure the guest's ZCR_EL1 is saved before we save/flush the
    state. This will be useful in later patches.
    
    Acked-by: Will Deacon <will@kernel.org>
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/fpsimd.c
+++ arch/arm64/kvm/fpsimd.c
@@ -121,10 +121,10 @@ void kvm_arch_vcpu_put_fp(struct kvm_vcpu *vcpu)
 	local_irq_save(flags);
 
 	if (vcpu->arch.flags & KVM_ARM64_FP_ENABLED) {
-		fpsimd_save_and_flush_cpu_state();
-
 		if (guest_has_sve)
 			__vcpu_sys_reg(vcpu, ZCR_EL1) = read_sysreg_el1(SYS_ZCR);
+
+		fpsimd_save_and_flush_cpu_state();
 	} else if (host_has_sve) {
 		/*
 		 * The FPSIMD/SVE state in the CPU has not been touched, and we
