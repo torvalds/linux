f32b84d7c977e1906a4781b93b3c93090b6cd675
Josef Bacik <josef@toxicpanda.com>
2021-03-12 15:25:20 -0500

    btrfs: do proper error handling in btrfs_update_reloc_root
    [ Upstream commit 592fbcd50c99b8adf999a2a54f9245caff333139 ]
    
    We call btrfs_update_root in btrfs_update_reloc_root, which can fail for
    all sorts of reasons, including IO errors.  Instead of panicing the box
    lets return the error, now that all callers properly handle those
    errors.
    
    Reviewed-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/btrfs/relocation.c
+++ fs/btrfs/relocation.c
@@ -897,7 +897,7 @@ int btrfs_update_reloc_root(struct btrfs_trans_handle *trans,
 	int ret;
 
 	if (!have_reloc_root(root))
-		goto out;
+		return 0;
 
 	reloc_root = root->reloc_root;
 	root_item = &reloc_root->root_item;
@@ -930,10 +930,8 @@ int btrfs_update_reloc_root(struct btrfs_trans_handle *trans,
 
 	ret = btrfs_update_root(trans, fs_info->tree_root,
 				&reloc_root->root_key, root_item);
-	BUG_ON(ret);
 	btrfs_put_root(reloc_root);
-out:
-	return 0;
+	return ret;
 }
 
 /*
