ea2c679edc4120354ff15818f78ad64921534c03
Colin Ian King <colin.king@canonical.com>
2021-03-15 12:33:03 +0000

    selftests/timers: Fix spelling mistake "clocksourc" -> "clocksource"
    There is a spelling mistake in a comment. Fix it.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>

--- tools/testing/selftests/timers/clocksource-switch.c
+++ tools/testing/selftests/timers/clocksource-switch.c
@@ -3,7 +3,7 @@
  *		(C) Copyright IBM 2012
  *		Licensed under the GPLv2
  *
- *  NOTE: This is a meta-test which quickly changes the clocksourc and
+ *  NOTE: This is a meta-test which quickly changes the clocksource and
  *  then uses other tests to detect problems. Thus this test requires
  *  that the inconsistency-check and nanosleep tests be present in the
  *  same directory it is run from.
