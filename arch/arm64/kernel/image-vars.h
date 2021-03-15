9833905e2dc7f3f6b270b9e7a5bc873146693351
Will Deacon <will@kernel.org>
2021-03-15 14:35:02 +0000

    FROMLIST: KVM: arm64: Link position-independent string routines into .hyp.text
    Pull clear_page(), copy_page(), memcpy() and memset() into the nVHE hyp
    code and ensure that we always execute the '__pi_' entry point on the
    offchance that it changes in future.
    
    [ qperret: Commit title nits and added linker script alias ]
    
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Quentin Perret <qperret@google.com>
    Link: https://lore.kernel.org/r/20210315143536.214621-3-qperret@google.com
    Bug: 178098380
    Change-Id: Ie4a82f71d87f3151f8d5e1054b0fd1f8ed44d583

--- arch/arm64/kernel/image-vars.h
+++ arch/arm64/kernel/image-vars.h
@@ -104,6 +104,17 @@ KVM_NVHE_ALIAS(kvm_arm_hyp_percpu_base);
 /* PMU available static key */
 KVM_NVHE_ALIAS(kvm_arm_pmu_available);
 
+/* Position-independent library routines */
+KVM_NVHE_ALIAS_HYP(clear_page, __pi_clear_page);
+KVM_NVHE_ALIAS_HYP(copy_page, __pi_copy_page);
+KVM_NVHE_ALIAS_HYP(memcpy, __pi_memcpy);
+KVM_NVHE_ALIAS_HYP(memset, __pi_memset);
+
+#ifdef CONFIG_KASAN
+KVM_NVHE_ALIAS_HYP(__memcpy, __pi_memcpy);
+KVM_NVHE_ALIAS_HYP(__memset, __pi_memset);
+#endif
+
 #endif /* CONFIG_KVM */
 
 #endif /* __ARM64_KERNEL_IMAGE_VARS_H */
