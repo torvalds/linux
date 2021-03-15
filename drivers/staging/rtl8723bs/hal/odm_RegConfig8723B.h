fdf5eec368a7de13c7e30591b36955ee7352499c
Marco Cesati <marcocesati@gmail.com>
2021-03-15 18:05:53 +0100

    Staging: rtl8723bs: fix spaces in odm_RegConfig8723B.h
    This commit fixes the following checkpatch.pl errors:
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #10: FILE: ./hal/odm_RegConfig8723B.h:10:
        +void odm_ConfigRFReg_8723B(struct DM_ODM_T * pDM_Odm,
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #17: FILE: ./hal/odm_RegConfig8723B.h:17:
        +void odm_ConfigRF_RadioA_8723B(struct DM_ODM_T * pDM_Odm, u32 Addr, u32 Data);
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #19: FILE: ./hal/odm_RegConfig8723B.h:19:
        +void odm_ConfigMAC_8723B(struct DM_ODM_T * pDM_Odm, u32 Addr, u8 Data);
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #21: FILE: ./hal/odm_RegConfig8723B.h:21:
        +void odm_ConfigBB_AGC_8723B(struct DM_ODM_T * pDM_Odm,
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #27: FILE: ./hal/odm_RegConfig8723B.h:27:
        +void odm_ConfigBB_PHY_REG_PG_8723B(struct DM_ODM_T * pDM_Odm,
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #36: FILE: ./hal/odm_RegConfig8723B.h:36:
        +void odm_ConfigBB_PHY_8723B(struct DM_ODM_T * pDM_Odm,
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #42: FILE: ./hal/odm_RegConfig8723B.h:42:
        +void odm_ConfigBB_TXPWR_LMT_8723B(struct DM_ODM_T * pDM_Odm,
    
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Marco Cesati <marcocesati@gmail.com>
    Link: https://lore.kernel.org/r/20210315170618.2566-33-marcocesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/odm_RegConfig8723B.h
+++ drivers/staging/rtl8723bs/hal/odm_RegConfig8723B.h
@@ -7,24 +7,24 @@
 #ifndef __INC_ODM_REGCONFIG_H_8723B
 #define __INC_ODM_REGCONFIG_H_8723B
 
-void odm_ConfigRFReg_8723B(struct DM_ODM_T * pDM_Odm,
+void odm_ConfigRFReg_8723B(struct DM_ODM_T *pDM_Odm,
 			   u32 Addr,
 			   u32 Data,
 			   enum ODM_RF_RADIO_PATH_E RF_PATH,
 			   u32 RegAddr
 );
 
-void odm_ConfigRF_RadioA_8723B(struct DM_ODM_T * pDM_Odm, u32 Addr, u32 Data);
+void odm_ConfigRF_RadioA_8723B(struct DM_ODM_T *pDM_Odm, u32 Addr, u32 Data);
 
-void odm_ConfigMAC_8723B(struct DM_ODM_T * pDM_Odm, u32 Addr, u8 Data);
+void odm_ConfigMAC_8723B(struct DM_ODM_T *pDM_Odm, u32 Addr, u8 Data);
 
-void odm_ConfigBB_AGC_8723B(struct DM_ODM_T * pDM_Odm,
+void odm_ConfigBB_AGC_8723B(struct DM_ODM_T *pDM_Odm,
 			    u32 Addr,
 			    u32 Bitmask,
 			    u32 Data
 );
 
-void odm_ConfigBB_PHY_REG_PG_8723B(struct DM_ODM_T * pDM_Odm,
+void odm_ConfigBB_PHY_REG_PG_8723B(struct DM_ODM_T *pDM_Odm,
 				   u32 Band,
 				   u32 RfPath,
 				   u32 TxNum,
@@ -33,13 +33,13 @@ void odm_ConfigBB_PHY_REG_PG_8723B(struct DM_ODM_T * pDM_Odm,
 				   u32 Data
 );
 
-void odm_ConfigBB_PHY_8723B(struct DM_ODM_T * pDM_Odm,
+void odm_ConfigBB_PHY_8723B(struct DM_ODM_T *pDM_Odm,
 			    u32 Addr,
 			    u32 Bitmask,
 			    u32 Data
 );
 
-void odm_ConfigBB_TXPWR_LMT_8723B(struct DM_ODM_T * pDM_Odm,
+void odm_ConfigBB_TXPWR_LMT_8723B(struct DM_ODM_T *pDM_Odm,
 				  u8 *Regulation,
 				  u8 *Band,
 				  u8 *Bandwidth,
