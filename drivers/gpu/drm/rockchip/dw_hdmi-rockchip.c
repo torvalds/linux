cc27baeeb765c4976f438b871c5797e08dd573e0
Andy Yan <andy.yan@rock-chips.com>
2021-03-15 09:13:43 +0800

    drm/rockchip: dw-hdmi: Support multi connector attach one crtc
    VOP support one VP output to multi connectors with
    the same scan out timing.
    
    Change-Id: I4f31140135b0ebca89caa6a94be2f1a558e34a31
    Signed-off-by: Andy Yan <andy.yan@rock-chips.com>

--- drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
+++ drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
@@ -847,7 +847,7 @@ dw_hdmi_rockchip_encoder_atomic_check(struct drm_encoder *encoder,
 
 	s->output_type = DRM_MODE_CONNECTOR_HDMIA;
 	s->tv_state = &conn_state->tv;
-	s->output_if = VOP_OUTPUT_IF_HDMI0;
+	s->output_if |= VOP_OUTPUT_IF_HDMI0;
 
 	s->output_mode = output_mode;
 	s->bus_format = bus_format;
