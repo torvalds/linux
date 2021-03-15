e3c15adbbffe2a2d1e5dda06ccdafc815cf1c929
Marco Cesati <marcocesati@gmail.com>
2021-03-15 18:05:56 +0100

    Staging: rtl8723bs: fix spaces in rtl8723b_dm.c
    This commit fixes the following checkpatch.pl errors:
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #28: FILE: ./hal/rtl8723b_dm.c:28:
        +	struct DM_ODM_T * pDM_Odm = &(pHalData->odmpriv);
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #77: FILE: ./hal/rtl8723b_dm.c:77:
        +	struct DM_ODM_T * pDM_Odm = &(pHalData->odmpriv);
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #132: FILE: ./hal/rtl8723b_dm.c:132:
        +	struct DM_ODM_T * pDM_Odm = &(pHalData->odmpriv);
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #206: FILE: ./hal/rtl8723b_dm.c:206:
        +	struct DM_ODM_T * pDM_Odm = &pHalData->odmpriv;
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #232: FILE: ./hal/rtl8723b_dm.c:232:
        +	struct DM_ODM_T * pDM_Odm = &pHalData->odmpriv;
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #233: FILE: ./hal/rtl8723b_dm.c:233:
        +	struct DIG_T * pDM_DigTable = &pDM_Odm->DM_DigTable;
    
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Marco Cesati <marcocesati@gmail.com>
    Link: https://lore.kernel.org/r/20210315170618.2566-36-marcocesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/rtl8723b_dm.c
+++ drivers/staging/rtl8723bs/hal/rtl8723b_dm.c
@@ -25,7 +25,7 @@ static void Init_ODM_ComInfo_8723b(struct adapter *Adapter)
 {
 
 	struct hal_com_data *pHalData = GET_HAL_DATA(Adapter);
-	struct DM_ODM_T * pDM_Odm = &(pHalData->odmpriv);
+	struct DM_ODM_T *pDM_Odm = &pHalData->odmpriv;
 	struct dm_priv *pdmpriv = &pHalData->dmpriv;
 	u8 cut_ver, fab_ver;
 
@@ -74,7 +74,7 @@ static void Update_ODM_ComInfo_8723b(struct adapter *Adapter)
 	struct dvobj_priv *dvobj = adapter_to_dvobj(Adapter);
 	struct pwrctrl_priv *pwrctrlpriv = adapter_to_pwrctl(Adapter);
 	struct hal_com_data *pHalData = GET_HAL_DATA(Adapter);
-	struct DM_ODM_T * pDM_Odm = &(pHalData->odmpriv);
+	struct DM_ODM_T *pDM_Odm = &pHalData->odmpriv;
 	struct dm_priv *pdmpriv = &pHalData->dmpriv;
 	int i;
 	u8 zero = 0;
@@ -129,7 +129,7 @@ void rtl8723b_InitHalDm(struct adapter *Adapter)
 {
 	struct hal_com_data *pHalData = GET_HAL_DATA(Adapter);
 	struct dm_priv *pdmpriv = &pHalData->dmpriv;
-	struct DM_ODM_T * pDM_Odm = &(pHalData->odmpriv);
+	struct DM_ODM_T *pDM_Odm = &pHalData->odmpriv;
 
 	pdmpriv->DM_Type = DM_Type_ByDriver;
 	pdmpriv->DMFlag = DYNAMIC_FUNC_DISABLE;
@@ -203,7 +203,7 @@ void rtl8723b_hal_dm_in_lps(struct adapter *padapter)
 	u32 PWDB_rssi = 0;
 	struct mlme_priv *pmlmepriv = &padapter->mlmepriv;
 	struct hal_com_data *pHalData = GET_HAL_DATA(padapter);
-	struct DM_ODM_T * pDM_Odm = &pHalData->odmpriv;
+	struct DM_ODM_T *pDM_Odm = &pHalData->odmpriv;
 	struct sta_priv *pstapriv = &padapter->stapriv;
 	struct sta_info *psta = NULL;
 
@@ -229,8 +229,8 @@ void rtl8723b_HalDmWatchDog_in_LPS(struct adapter *Adapter)
 	struct hal_com_data *pHalData = GET_HAL_DATA(Adapter);
 	struct mlme_priv *pmlmepriv = &Adapter->mlmepriv;
 	struct dm_priv *pdmpriv = &pHalData->dmpriv;
-	struct DM_ODM_T * pDM_Odm = &pHalData->odmpriv;
-	struct DIG_T * pDM_DigTable = &pDM_Odm->DM_DigTable;
+	struct DM_ODM_T *pDM_Odm = &pHalData->odmpriv;
+	struct DIG_T *pDM_DigTable = &pDM_Odm->DM_DigTable;
 	struct sta_priv *pstapriv = &Adapter->stapriv;
 	struct sta_info *psta = NULL;
 
