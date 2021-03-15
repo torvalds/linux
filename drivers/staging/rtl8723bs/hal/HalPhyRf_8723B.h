6bfa2a5adcfadb036a12b1771e42c6081c9a82fe
Marco Cesati <marcocesati@gmail.com>
2021-03-15 18:05:37 +0100

    Staging: rtl8723bs: fix spaces in HalPhyRf_8723B.h
    This commit fixes the following checkpatch.pl errors:
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #19: FILE: ./hal/HalPhyRf_8723B.h:19:
        +void ConfigureTxpowerTrack_8723B(struct TXPWRTRACK_CFG * pConfig);
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #22: FILE: ./hal/HalPhyRf_8723B.h:22:
        +	struct DM_ODM_T * pDM_Odm,
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #29: FILE: ./hal/HalPhyRf_8723B.h:29:
        +	struct DM_ODM_T * pDM_Odm,
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #44: FILE: ./hal/HalPhyRf_8723B.h:44:
        +void ODM_SetIQCbyRFpath(struct DM_ODM_T * pDM_Odm, u32 RFpath);
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #49: FILE: ./hal/HalPhyRf_8723B.h:49:
        +void PHY_LCCalibrate_8723B(struct DM_ODM_T * pDM_Odm);
    
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Marco Cesati <marcocesati@gmail.com>
    Link: https://lore.kernel.org/r/20210315170618.2566-17-marcocesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/HalPhyRf_8723B.h
+++ drivers/staging/rtl8723bs/hal/HalPhyRf_8723B.h
@@ -16,17 +16,17 @@
 #define	RF_T_METER_8723B					0x42	/*  */
 
 
-void ConfigureTxpowerTrack_8723B(struct TXPWRTRACK_CFG * pConfig);
+void ConfigureTxpowerTrack_8723B(struct TXPWRTRACK_CFG *pConfig);
 
 void DoIQK_8723B(
-	struct DM_ODM_T * pDM_Odm,
+	struct DM_ODM_T *pDM_Odm,
 	u8 DeltaThermalIndex,
 	u8 ThermalValue,
 	u8 Threshold
 );
 
 void ODM_TxPwrTrackSetPwr_8723B(
-	struct DM_ODM_T * pDM_Odm,
+	struct DM_ODM_T *pDM_Odm,
 	enum PWRTRACK_METHOD Method,
 	u8 RFPath,
 	u8 ChannelMappedIndex
@@ -41,12 +41,12 @@ void PHY_IQCalibrate_8723B(
 	u8 RF_Path
 );
 
-void ODM_SetIQCbyRFpath(struct DM_ODM_T * pDM_Odm, u32 RFpath);
+void ODM_SetIQCbyRFpath(struct DM_ODM_T *pDM_Odm, u32 RFpath);
 
 /*  */
 /*  LC calibrate */
 /*  */
-void PHY_LCCalibrate_8723B(struct DM_ODM_T * pDM_Odm);
+void PHY_LCCalibrate_8723B(struct DM_ODM_T *pDM_Odm);
 
 /*  */
 /*  AP calibrate */
