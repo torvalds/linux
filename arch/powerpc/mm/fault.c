d738ee8d56de38c91610741f672ec5c1ffae76fc
Nicholas Piggin <npiggin@gmail.com>
2021-03-16 20:42:02 +1000

    powerpc/64e/interrupt: handle bad_page_fault in C
    With non-volatile registers saved on interrupt, bad_page_fault
    can now be called by do_page_fault.
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20210316104206.407354-9-npiggin@gmail.com

--- arch/powerpc/mm/fault.c
+++ arch/powerpc/mm/fault.c
@@ -552,12 +552,9 @@ static long __do_page_fault(struct pt_regs *regs)
 	if (likely(entry)) {
 		instruction_pointer_set(regs, extable_fixup(entry));
 		return 0;
-	} else if (!IS_ENABLED(CONFIG_PPC_BOOK3E_64)) {
+	} else {
 		__bad_page_fault(regs, err);
 		return 0;
-	} else {
-		/* 32 and 64e handle the bad page fault in asm */
-		return err;
 	}
 }
 NOKPROBE_SYMBOL(__do_page_fault);
