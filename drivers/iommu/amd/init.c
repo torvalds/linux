4b8ef157ca832f812b3302b1800548bd92c207de
Joerg Roedel <jroedel@suse.de>
2021-03-17 10:10:37 +0100

    iommu/amd: Keep track of amd_iommu_irq_remap state
    The amd_iommu_irq_remap variable is set to true in amd_iommu_prepare().
    But if initialization fails it is not set to false. Fix that and
    correctly keep track of whether irq remapping is enabled or not.
    
    References: https://bugzilla.kernel.org/show_bug.cgi?id=212133
    References: https://bugzilla.suse.com/show_bug.cgi?id=1183132
    Fixes: b34f10c2dc59 ("iommu/amd: Stop irq_remapping_select() matching when remapping is disabled")
    Cc: stable@vger.kernel.org # v5.11
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Link: https://lore.kernel.org/r/20210317091037.31374-4-joro@8bytes.org
    Acked-by: Huang Rui <ray.huang@amd.com>

--- drivers/iommu/amd/init.c
+++ drivers/iommu/amd/init.c
@@ -3002,8 +3002,11 @@ int __init amd_iommu_prepare(void)
 	amd_iommu_irq_remap = true;
 
 	ret = iommu_go_to_state(IOMMU_ACPI_FINISHED);
-	if (ret)
+	if (ret) {
+		amd_iommu_irq_remap = false;
 		return ret;
+	}
+
 	return amd_iommu_irq_remap ? 0 : -ENODEV;
 }
 
