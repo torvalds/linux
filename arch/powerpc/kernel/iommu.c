3c0468d4451eb6b4f6604370639f163f9637a479
Leonardo Bras <leobras.c@gmail.com>
2021-03-18 14:44:14 -0300

    powerpc/kernel/iommu: Align size for IOMMU_PAGE_SIZE() to save TCEs
    Currently both iommu_alloc_coherent() and iommu_free_coherent() align the
    desired allocation size to PAGE_SIZE, and gets system pages and IOMMU
    mappings (TCEs) for that value.
    
    When IOMMU_PAGE_SIZE < PAGE_SIZE, this behavior may cause unnecessary
    TCEs to be created for mapping the whole system page.
    
    Example:
    - PAGE_SIZE = 64k, IOMMU_PAGE_SIZE() = 4k
    - iommu_alloc_coherent() is called for 128 bytes
    - 1 system page (64k) is allocated
    - 16 IOMMU pages (16 x 4k) are allocated (16 TCEs used)
    
    It would be enough to use a single TCE for this, so 15 TCEs are
    wasted in the process.
    
    Update iommu_*_coherent() to make sure the size alignment happens only
    for IOMMU_PAGE_SIZE() before calling iommu_alloc() and iommu_free().
    
    Also, on iommu_range_alloc(), replace ALIGN(n, 1 << tbl->it_page_shift)
    with IOMMU_PAGE_ALIGN(n, tbl), which is easier to read and does the
    same.
    
    Signed-off-by: Leonardo Bras <leobras.c@gmail.com>
    Reviewed-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20210318174414.684630-1-leobras.c@gmail.com

--- arch/powerpc/kernel/iommu.c
+++ arch/powerpc/kernel/iommu.c
@@ -889,6 +889,7 @@ void *iommu_alloc_coherent(struct device *dev, struct iommu_table *tbl,
 	unsigned int order;
 	unsigned int nio_pages, io_order;
 	struct page *page;
+	size_t size_io = size;
 
 	size = PAGE_ALIGN(size);
 	order = get_order(size);
@@ -915,8 +916,9 @@ void *iommu_alloc_coherent(struct device *dev, struct iommu_table *tbl,
 	memset(ret, 0, size);
 
 	/* Set up tces to cover the allocated range */
-	nio_pages = size >> tbl->it_page_shift;
-	io_order = get_iommu_order(size, tbl);
+	size_io = IOMMU_PAGE_ALIGN(size_io, tbl);
+	nio_pages = size_io >> tbl->it_page_shift;
+	io_order = get_iommu_order(size_io, tbl);
 	mapping = iommu_alloc(dev, tbl, ret, nio_pages, DMA_BIDIRECTIONAL,
 			      mask >> tbl->it_page_shift, io_order, 0);
 	if (mapping == DMA_MAPPING_ERROR) {
@@ -931,10 +933,9 @@ void iommu_free_coherent(struct iommu_table *tbl, size_t size,
 			 void *vaddr, dma_addr_t dma_handle)
 {
 	if (tbl) {
-		unsigned int nio_pages;
+		size_t size_io = IOMMU_PAGE_ALIGN(size, tbl);
+		unsigned int nio_pages = size_io >> tbl->it_page_shift;
 
-		size = PAGE_ALIGN(size);
-		nio_pages = size >> tbl->it_page_shift;
 		iommu_free(tbl, dma_handle, nio_pages);
 		size = PAGE_ALIGN(size);
 		free_pages((unsigned long)vaddr, get_order(size));
