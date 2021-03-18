d2fd2311de909a7f4e99b4bd11a19e6b671d6a6b
Alex Elder <elder@linaro.org>
2021-03-18 13:59:27 -0500

    net: ipa: fix assumptions about DMA address size
    Some build time checks in ipa_table_validate_build() assume that a
    DMA address is 64 bits wide.  That is more restrictive than it has
    to be.  A route or filter table is 64 bits wide no matter what the
    size of a DMA address is on the AP.  The code actually uses a
    pointer to __le64 to access table entries, and a fixed constant
    IPA_TABLE_ENTRY_SIZE to describe the size of those entries.
    
    Loosen up two checks so they still verify some requirements, but
    such that they do not assume the size of a DMA address is 64 bits.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ipa/ipa_table.c
+++ drivers/net/ipa/ipa_table.c
@@ -126,13 +126,15 @@ static void ipa_table_validate_build(void)
 	 */
 	BUILD_BUG_ON(ARCH_DMA_MINALIGN % IPA_TABLE_ALIGN);
 
-	/* Filter and route tables contain DMA addresses that refer to
-	 * filter or route rules.  We use a fixed constant to represent
-	 * the size of either type of table entry.  Code in ipa_table_init()
-	 * uses a pointer to __le64 to initialize table entriews.
+	/* Filter and route tables contain DMA addresses that refer
+	 * to filter or route rules.  But the size of a table entry
+	 * is 64 bits regardless of what the size of an AP DMA address
+	 * is.  A fixed constant defines the size of an entry, and
+	 * code in ipa_table_init() uses a pointer to __le64 to
+	 * initialize tables.
 	 */
-	BUILD_BUG_ON(IPA_TABLE_ENTRY_SIZE != sizeof(dma_addr_t));
-	BUILD_BUG_ON(sizeof(dma_addr_t) != sizeof(__le64));
+	BUILD_BUG_ON(sizeof(dma_addr_t) > IPA_TABLE_ENTRY_SIZE);
+	BUILD_BUG_ON(sizeof(__le64) != IPA_TABLE_ENTRY_SIZE);
 
 	/* A "zero rule" is used to represent no filtering or no routing.
 	 * It is a 64-bit block of zeroed memory.  Code in ipa_table_init()
