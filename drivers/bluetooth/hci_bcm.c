81534d4835de52758176909f3e446f1d49809241
Linus Walleij <linus.walleij@linaro.org>
2021-03-18 09:43:09 +0100

    Bluetooth: btbcm: Add BCM4330 and BCM4334 compatibles
    Add the missing brcm,bcm4330-bt and brcm,bcm4334-bt to the
    match table so device trees can use this compatible as well
    and not need to use the fallback brcm,bcm4329-bt.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/hci_bcm.c
+++ drivers/bluetooth/hci_bcm.c
@@ -1499,6 +1499,8 @@ static struct bcm_device_data bcm43438_device_data = {
 static const struct of_device_id bcm_bluetooth_of_match[] = {
 	{ .compatible = "brcm,bcm20702a1" },
 	{ .compatible = "brcm,bcm4329-bt" },
+	{ .compatible = "brcm,bcm4330-bt" },
+	{ .compatible = "brcm,bcm4334-bt" },
 	{ .compatible = "brcm,bcm4345c5" },
 	{ .compatible = "brcm,bcm4330-bt" },
 	{ .compatible = "brcm,bcm43438-bt", .data = &bcm43438_device_data },
