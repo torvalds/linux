60d64b2f414d6b80c82a6532af08232272a37052
Nikita Travkin <nikitos.tr@gmail.com>
2021-03-19 22:23:20 +0500

    FROMLIST: dt-bindings: soc: qcom: Add bindings for Qualcomm Memshare service
    Add DT bindings for memshare: QMI service that allocates
    memory per remote processor request.
    
    Signed-off-by: Nikita Travkin <nikitos.tr@gmail.com>
    Link: https://lore.kernel.org/r/20210319172321.22248-2-nikitos.tr@gmail.com

---
+++ include/dt-bindings/soc/qcom,memshare.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __DT_QCOM_MEMSHARE_H__
+#define __DT_QCOM_MEMSHARE_H__
+
+#define MEMSHARE_PROC_MPSS_V01 0
+#define MEMSHARE_PROC_ADSP_V01 1
+#define MEMSHARE_PROC_WCNSS_V01 2
+
+#endif /* __DT_QCOM_MEMSHARE_H__ */
