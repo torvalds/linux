a05432fb49b6439d0c5b803053dfdd875940116d
Pavel Begunkov <asml.silence@gmail.com>
2021-03-19 17:22:38 +0000

    io_uring: refactor io_free_req_deferred()
    We don't care about ret value in io_free_req_deferred(), make the code a
    bit more concise.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -2192,11 +2192,8 @@ static void io_put_req_deferred_cb(struct callback_head *cb)
 
 static void io_free_req_deferred(struct io_kiocb *req)
 {
-	int ret;
-
 	req->task_work.func = io_put_req_deferred_cb;
-	ret = io_req_task_work_add(req);
-	if (unlikely(ret))
+	if (unlikely(io_req_task_work_add(req)))
 		io_req_task_work_add_fallback(req, io_put_req_deferred_cb);
 }
 
