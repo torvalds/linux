7a440cc78392c3caf805ef0afc7ead031e4d0830
Quentin Perret <qperret@google.com>
2021-03-19 10:01:21 +0000

    KVM: arm64: Enable access to sanitized CPU features at EL2
    Introduce the infrastructure in KVM enabling to copy CPU feature
    registers into EL2-owned data-structures, to allow reading sanitised
    values directly at EL2 in nVHE.
    
    Given that only a subset of these features are being read by the
    hypervisor, the ones that need to be copied are to be listed under
    <asm/kvm_cpufeature.h> together with the name of the nVHE variable that
    will hold the copy. This introduces only the infrastructure enabling
    this copy. The first users will follow shortly.
    
    Signed-off-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20210319100146.1149909-14-qperret@google.com

---
+++ arch/arm64/include/asm/kvm_cpufeature.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2020 - Google LLC
+ * Author: Quentin Perret <qperret@google.com>
+ */
+
+#ifndef __ARM64_KVM_CPUFEATURE_H__
+#define __ARM64_KVM_CPUFEATURE_H__
+
+#include <asm/cpufeature.h>
+
+#include <linux/build_bug.h>
+
+#if defined(__KVM_NVHE_HYPERVISOR__)
+#define DECLARE_KVM_HYP_CPU_FTR_REG(name) extern struct arm64_ftr_reg name
+#define DEFINE_KVM_HYP_CPU_FTR_REG(name) struct arm64_ftr_reg name
+#else
+#define DECLARE_KVM_HYP_CPU_FTR_REG(name) extern struct arm64_ftr_reg kvm_nvhe_sym(name)
+#define DEFINE_KVM_HYP_CPU_FTR_REG(name) BUILD_BUG()
+#endif
+
+#endif
