6f4eea90465ad0cd5f3d041b9b2c728426f2b8d4
Vincent Chen <vincent.chen@sifive.com>
2021-03-22 22:26:03 +0800

    riscv: Introduce alternative mechanism to apply errata solution
    Introduce the "alternative" mechanism from ARM64 and x86 to apply the CPU
    vendors' errata solution at runtime. The main purpose of this patch is
    to provide a framework. Therefore, the implementation is quite basic for
    now so that some scenarios could not use this schemei, such as patching
    code to a module, relocating the patching code and heterogeneous CPU
    topology.
    
    Users could use the macro ALTERNATIVE to apply an errata to the existing
    code flow. In the macro ALTERNATIVE, users need to specify the manufacturer
    information(vendorid, archid, and impid) for this errata. Therefore, kernel
    will know this errata is suitable for which CPU core. During the booting
    procedure, kernel will select the errata required by the CPU core and then
    patch it. It means that the kernel only applies the errata to the specified
    CPU core. In this case, the vendor's errata does not affect each other at
    runtime. The above patching procedure only occurs during the booting phase,
    so we only take the overhead of the "alternative" mechanism once.
    
    This "alternative" mechanism is enabled by default to ensure that all
    required errata will be applied. However, users can disable this feature by
    the Kconfig "CONFIG_RISCV_ERRATA_ALTERNATIVE".
    
    Signed-off-by: Vincent Chen <vincent.chen@sifive.com>
    Reviewed-by: Anup Patel <anup@brainfault.org>
    Signed-off-by: Palmer Dabbelt <palmerdabbelt@google.com>

---
+++ arch/riscv/include/asm/errata_list.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2021 Sifive.
+ */
+#ifndef ASM_ERRATA_LIST_H
+#define ASM_ERRATA_LIST_H
+
+#ifdef CONFIG_ERRATA_SIFIVE
+#define	ERRATA_SIFIVE_NUMBER 0
+#endif
+
+#endif
