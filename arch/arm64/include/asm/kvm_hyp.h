2b123e3f301792d338aad4cc9bca19c79ad6625a
Marc Zyngier <maz@kernel.org>
2021-03-22 13:32:34 +0000

    FROMGIT: KVM: arm64: Drop the CPU_FTR_REG_HYP_COPY infrastructure
    Now that the read_ctr macro has been specialised for nVHE,
    the whole CPU_FTR_REG_HYP_COPY infrastrcture looks completely
    overengineered.
    
    Simplify it by populating the two u64 quantities (MMFR0 and 1)
    that the hypervisor need.
    
    Bug: 178098380
    Reviewed-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    (cherry picked from commit 7c4199375ae347449fbde43cc8bf174ae6383d8e
     git://git.kernel.org/pub/scm/linux/kernel/git/kvmarm/kvmarm.git next)
    Signed-off-by: Quentin Perret <qperret@google.com>
    Change-Id: Iaec199ce66316933922fa02660a7216f6420f35c

--- arch/arm64/include/asm/kvm_hyp.h
+++ arch/arm64/include/asm/kvm_hyp.h
@@ -116,4 +116,7 @@ int __pkvm_init(phys_addr_t phys, unsigned long size, unsigned long nr_cpus,
 void __noreturn __host_enter(struct kvm_cpu_context *host_ctxt);
 #endif
 
+extern u64 kvm_nvhe_sym(id_aa64mmfr0_el1_sys_val);
+extern u64 kvm_nvhe_sym(id_aa64mmfr1_el1_sys_val);
+
 #endif /* __ARM64_KVM_HYP_H__ */
