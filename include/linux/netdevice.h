744b8376632208137fe4acc9967b93e2970732a3
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-03-22 13:31:48 +0200

    net: move the ptype_all and ptype_base declarations to include/linux/netdevice.h
    ptype_all and ptype_base are declared in net/core/dev.c as non-static,
    because they are used by net-procfs.c too. However, a "make W=1" build
    complains that there was no previous declaration of ptype_all and
    ptype_base in a header file, so this way of declaring things constitutes
    a violation of coding style.
    
    Let's move the extern declarations of ptype_all and ptype_base to the
    linux/netdevice.h file, which is included by net-procfs.c too.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -5336,6 +5336,9 @@ do {								\
 #define PTYPE_HASH_SIZE	(16)
 #define PTYPE_HASH_MASK	(PTYPE_HASH_SIZE - 1)
 
+extern struct list_head ptype_all __read_mostly;
+extern struct list_head ptype_base[PTYPE_HASH_SIZE] __read_mostly;
+
 extern struct net_device *blackhole_netdev;
 
 #endif	/* _LINUX_NETDEVICE_H */
