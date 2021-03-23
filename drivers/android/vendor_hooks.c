8947e06ff728dc94dd88d3e36508881fb0eef2ce
lijianzhong <lijianzhong@xiaomi.com>
2021-03-23 20:31:45 +0800

    ANDROID: cgroup: Add vendor hook for cpuset.
    This hook allows us to capture information when a process is forked so
    that we can stat and set some key task's CPU affinity in the ko module
    later. This patch, along with aosp/2548175, is necessary for our
    affinity settings.
    
    Bug: 183674818
    Signed-off-by: lijianzhong <lijianzhong@xiaomi.com>
    Change-Id: Ib93e05e5f6c338c5f7ada56bfebdd705f87f1f66
    (cherry picked from commit a188361628461c58a4dfc72869d9acb1dfa2542f)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -229,4 +229,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_read_done);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_preset);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_uid);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_user);
-
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpuset_fork);
--- include/trace/hooks/cgroup.h
+++ include/trace/hooks/cgroup.h
@@ -24,6 +24,10 @@ DECLARE_RESTRICTED_HOOK(android_rvh_cgroup_force_kthread_migration,
 struct cgroup_taskset;
 struct cgroup_subsys;
 
+DECLARE_RESTRICTED_HOOK(android_rvh_cpuset_fork,
+	TP_PROTO(struct task_struct *p, bool *inherit_cpus),
+	TP_ARGS(p, inherit_cpus), 1);
+
 DECLARE_RESTRICTED_HOOK(android_rvh_cpu_cgroup_attach,
 	TP_PROTO(struct cgroup_taskset *tset),
 	TP_ARGS(tset), 1);
