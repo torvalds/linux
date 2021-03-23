037950869be3e79fa90dd52954af24abcbca2445
Chris Chiu <chris.chiu@canonical.com>
2021-03-23 16:52:19 +0800

    block: clear GD_NEED_PART_SCAN later in bdev_disk_changed
    [ Upstream commit 5116784039f0421e9a619023cfba3e302c3d9adc ]
    
    The GD_NEED_PART_SCAN is set by bdev_check_media_change to initiate
    a partition scan while removing a block device. It should be cleared
    after blk_drop_paritions because blk_drop_paritions could return
    -EBUSY and then the consequence __blkdev_get has no chance to do
    delete_partition if GD_NEED_PART_SCAN already cleared.
    
    It causes some problems on some card readers. Ex. Realtek card
    reader 0bda:0328 and 0bda:0158. The device node of the partition
    will not disappear after the memory card removed. Thus the user
    applications can not update the device mapping correctly.
    
    BugLink: https://bugs.launchpad.net/ubuntu/+source/linux/+bug/1920874
    Signed-off-by: Chris Chiu <chris.chiu@canonical.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20210323085219.24428-1-chris.chiu@canonical.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/block_dev.c
+++ fs/block_dev.c
@@ -1404,12 +1404,12 @@ int bdev_disk_changed(struct block_device *bdev, bool invalidate)
 
 	lockdep_assert_held(&bdev->bd_mutex);
 
-	clear_bit(GD_NEED_PART_SCAN, &bdev->bd_disk->state);
-
 rescan:
 	ret = blk_drop_partitions(bdev);
 	if (ret)
 		return ret;
+
+	clear_bit(GD_NEED_PART_SCAN, &disk->state);
 
 	/*
 	 * Historically we only set the capacity to zero for devices that
