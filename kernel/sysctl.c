b7cc6ec744b307db59568c654a8904a5928aa855
Mel Gorman <mgorman@suse.de>
2021-03-24 13:39:16 +0000

    sched/numa: Allow runtime enabling/disabling of NUMA balance without SCHED_DEBUG
    The ability to enable/disable NUMA balancing is not a debugging feature
    and should not depend on CONFIG_SCHED_DEBUG.  For example, machines within
    a HPC cluster may disable NUMA balancing temporarily for some jobs and
    re-enable it for other jobs without needing to reboot.
    
    This patch removes the dependency on CONFIG_SCHED_DEBUG for
    kernel.numa_balancing sysctl. The other numa balancing related sysctls
    are left as-is because if they need to be tuned then it is more likely
    that NUMA balancing needs to be fixed instead.
    
    Signed-off-by: Mel Gorman <mgorman@suse.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Valentin Schneider <valentin.schneider@arm.com>
    Link: https://lkml.kernel.org/r/20210324133916.GQ15768@suse.de

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1753,6 +1753,9 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= SYSCTL_ONE,
 	},
+#endif /* CONFIG_NUMA_BALANCING */
+#endif /* CONFIG_SCHED_DEBUG */
+#ifdef CONFIG_NUMA_BALANCING
 	{
 		.procname	= "numa_balancing",
 		.data		= NULL, /* filled in by handler */
@@ -1763,7 +1766,6 @@ static struct ctl_table kern_table[] = {
 		.extra2		= SYSCTL_ONE,
 	},
 #endif /* CONFIG_NUMA_BALANCING */
-#endif /* CONFIG_SCHED_DEBUG */
 	{
 		.procname	= "sched_rt_period_us",
 		.data		= &sysctl_sched_rt_period,
