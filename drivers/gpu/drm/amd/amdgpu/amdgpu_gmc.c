9b7f1e04677a9742ff93c66222c01bb30a24779a
Philip Cox <Philip.Cox@amd.com>
2021-03-24 09:15:45 -0400

    drm/amdgpu: Set amdgpu.noretry=1 for Arcturus
    Setting amdgpu.noretry=1 as default for Arcturus.
    
    Signed-off-by: Philip Cox <Philip.Cox@amd.com>
    Reviewed-by: Kent Russell <kent.russell@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_gmc.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_gmc.c
@@ -514,6 +514,7 @@ void amdgpu_gmc_noretry_set(struct amdgpu_device *adev)
 	switch (adev->asic_type) {
 	case CHIP_VEGA10:
 	case CHIP_VEGA20:
+	case CHIP_ARCTURUS:
 	case CHIP_ALDEBARAN:
 		/*
 		 * noretry = 0 will cause kfd page fault tests fail
