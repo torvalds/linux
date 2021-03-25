4f30ba1cce36d413c46097c1f3f96891c662a6d6
Linus Walleij <linus.walleij@linaro.org>
2021-03-25 15:13:04 +0100

    arm64: barrier: Remove spec_bar() macro
    The spec_bar() macro was introduced in
    commit bd4fb6d270bc ("arm64: Add support for SB barrier and patch in over DSB; ISB sequences")
    as a way for C to insert a speculation barrier and was then
    used in one single place: set_fs().
    
    Later on
    commit 3d2403fd10a1 ("arm64: uaccess: remove set_fs()")
    deleted set_fs() altogether and as noted in the commit
    on the new path the regular sb() assembly macro will
    be used.
    
    Delete the remnant.
    
    Cc: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Link: https://lore.kernel.org/r/20210325141304.1607595-1-linus.walleij@linaro.org
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/barrier.h
+++ arch/arm64/include/asm/barrier.h
@@ -25,10 +25,6 @@
 #define psb_csync()	asm volatile("hint #17" : : : "memory")
 #define csdb()		asm volatile("hint #20" : : : "memory")
 
-#define spec_bar()	asm volatile(ALTERNATIVE("dsb nsh\nisb\n",		\
-						 SB_BARRIER_INSN"nop\n",	\
-						 ARM64_HAS_SB))
-
 #ifdef CONFIG_ARM64_PSEUDO_NMI
 #define pmr_sync()						\
 	do {							\
