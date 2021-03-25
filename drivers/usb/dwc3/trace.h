3fc63d0724bbac83352456e073c113b24115576f
Thinh Nguyen <Thinh.Nguyen@synopsys.com>
2021-03-24 18:53:26 -0700

    usb: dwc3: trace: Print register read and write offset
    Currently dwc3 only prints the virtual address of a register when doing
    register read/write. However, these hashed addresses are difficult to read.
    Also, since we use %p, we may get some useless (___ptrval___) prints if the
    address is not randomized enough. Let's include the register offset to help
    read the register read and write tracepoints.
    
    Acked-by: Felipe Balbi <balbi@kernel.org>
    Signed-off-by: Thinh Nguyen <Thinh.Nguyen@synopsys.com>
    Link: https://lore.kernel.org/r/cb38aa7dec109a8965691b53039a8b317d026189.1616636706.git.Thinh.Nguyen@synopsys.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/dwc3/trace.h
+++ drivers/usb/dwc3/trace.h
@@ -32,8 +32,10 @@ DECLARE_EVENT_CLASS(dwc3_log_io,
 		__entry->offset = offset;
 		__entry->value = value;
 	),
-	TP_printk("addr %p value %08x", __entry->base + __entry->offset,
-			__entry->value)
+	TP_printk("addr %p offset %04x value %08x",
+		__entry->base + __entry->offset,
+		__entry->offset,
+		__entry->value)
 );
 
 DEFINE_EVENT(dwc3_log_io, dwc3_readl,
