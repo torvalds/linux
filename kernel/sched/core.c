c4681f3f1cfcfde0c95ff72f0bdb43f9ffd7f00e
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2021-03-25 01:45:15 +0100

    sched/core: Use -EINVAL in sched_dynamic_mode()
    -1 is -EPERM which is a somewhat odd error to return from
    sched_dynamic_write(). No other callers care about which negative
    value is used.
    
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: https://lore.kernel.org/r/20210325004515.531631-2-linux@rasmusvillemoes.dk

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5384,7 +5384,7 @@ static int sched_dynamic_mode(const char *str)
 	if (!strcmp(str, "full"))
 		return preempt_dynamic_full;
 
-	return -1;
+	return -EINVAL;
 }
 
 static void sched_dynamic_update(int mode)
