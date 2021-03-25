847ff389215100973dafaddbb8b79b0339c9300e
Tao Huang <huangtao@rock-chips.com>
2021-03-25 20:45:36 +0800

    dt-bindings: soc: rockchip: Revert "rk: revert to v4.19"
    This reverts part of commit 251c226c3504 ("rk: revert to v4.19").
    To keep the changes of include/dt-bindings/soc/rockchip,boot-mode.h.
    
    Change-Id: I4bcb721da1bea0600c92d4e23d33320bff8d0caf
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- include/dt-bindings/soc/rockchip,boot-mode.h
+++ include/dt-bindings/soc/rockchip,boot-mode.h
@@ -10,7 +10,15 @@
 #define BOOT_BL_DOWNLOAD	(REBOOT_FLAG + 1)
 /* enter recovery */
 #define BOOT_RECOVERY		(REBOOT_FLAG + 3)
- /* enter fastboot mode */
+/* reboot by panic */
+#define BOOT_PANIC		(REBOOT_FLAG + 7)
+/* reboot by watchdog */
+#define BOOT_WATCHDOG		(REBOOT_FLAG + 8)
+/* enter fastboot mode */
 #define BOOT_FASTBOOT		(REBOOT_FLAG + 9)
+/* enter charging mode */
+#define BOOT_CHARGING		(REBOOT_FLAG + 11)
+/* enter usb mass storage mode */
+#define BOOT_UMS		(REBOOT_FLAG + 12)
 
 #endif
