831cb78a2a5e86fe705ef4e3095c7cbc587c6a57
Jens Axboe <axboe@kernel.dk>
2021-03-26 08:57:10 -0600

    kernel: don't call do_exit() for PF_IO_WORKER threads
    [ Upstream commit 10442994ba195efef6fdcc0c3699e4633cb5161b ]
    
    Right now we're never calling get_signal() from PF_IO_WORKER threads, but
    in preparation for doing so, don't handle a fatal signal for them. The
    workers have state they need to cleanup when exiting, so just return
    instead of calling do_exit() on their behalf. The threads themselves will
    detect a fatal signal and do proper shutdown.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/signal.c
+++ kernel/signal.c
@@ -2755,13 +2755,21 @@ relock:
 		}
 
 		/*
+		 * PF_IO_WORKER threads will catch and exit on fatal signals
+		 * themselves. They have cleanup that must be performed, so
+		 * we cannot call do_exit() on their behalf.
+		 */
+		if (current->flags & PF_IO_WORKER)
+			goto out;
+
+		/*
 		 * Death signals, no core dump.
 		 */
 		do_group_exit(ksig->info.si_signo);
 		/* NOTREACHED */
 	}
 	spin_unlock_irq(&sighand->siglock);
-
+out:
 	ksig->sig = signr;
 	return ksig->sig > 0;
 }
