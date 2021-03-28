f6b6db2dfd617522de702d85095d6d33e3b86e93
Fabio Estevam <festevam@gmail.com>
2021-03-28 15:30:33 -0300

    pinctrl: imx: Remove unneeded of_match_ptr()
    i.MX is a DT-only platform, so of_match_ptr() can be safely
    removed.
    
    Remove the unneeded of_match_ptr().
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>
    Link: https://lore.kernel.org/r/20210328183034.555702-1-festevam@gmail.com
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/freescale/pinctrl-imx25.c
+++ drivers/pinctrl/freescale/pinctrl-imx25.c
@@ -323,7 +323,7 @@ static int imx25_pinctrl_probe(struct platform_device *pdev)
 static struct platform_driver imx25_pinctrl_driver = {
 	.driver = {
 		.name = "imx25-pinctrl",
-		.of_match_table = of_match_ptr(imx25_pinctrl_of_match),
+		.of_match_table = imx25_pinctrl_of_match,
 	},
 	.probe = imx25_pinctrl_probe,
 };
--- drivers/pinctrl/freescale/pinctrl-imx7d.c
+++ drivers/pinctrl/freescale/pinctrl-imx7d.c
@@ -386,7 +386,7 @@ static int imx7d_pinctrl_probe(struct platform_device *pdev)
 static struct platform_driver imx7d_pinctrl_driver = {
 	.driver = {
 		.name = "imx7d-pinctrl",
-		.of_match_table = of_match_ptr(imx7d_pinctrl_of_match),
+		.of_match_table = imx7d_pinctrl_of_match,
 	},
 	.probe = imx7d_pinctrl_probe,
 };
