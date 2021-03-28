acebb5597ff182e52a225400a459052a70dae706
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2021-03-28 10:09:32 +0530

    kernel/printk.c: Fixed mundane typos
    s/sempahore/semaphore/
    s/exacly/exactly/
    s/unregistred/unregistered/
    s/interation/iteration/
    
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>
    Reviewed-by: Petr Mladek <pmladek@suse.com>
    [pmladek@suse.com: Removed 4th hunk. The string has already been removed in the meantime.]
    Signed-off-by: Petr Mladek <pmladek@suse.com>
    Link: https://lore.kernel.org/r/20210328043932.8310-1-unixbhaskar@gmail.com

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -262,7 +262,7 @@ static void __up_console_sem(unsigned long ip)
  * definitely not the perfect debug tool (we don't know if _WE_
  * hold it and are racing, but it helps tracking those weird code
  * paths in the console code where we end up in places I want
- * locked without the console sempahore held).
+ * locked without the console semaphore held).
  */
 static int console_locked, console_suspended;
 
@@ -2331,7 +2331,7 @@ static int __init console_setup(char *str)
 	/*
 	 * console="" or console=null have been suggested as a way to
 	 * disable console output. Use ttynull that has been created
-	 * for exacly this purpose.
+	 * for exactly this purpose.
 	 */
 	if (str[0] == 0 || strcmp(str, "null") == 0) {
 		__add_preferred_console("ttynull", 0, NULL, NULL, true);
@@ -3106,7 +3106,7 @@ void __init console_init(void)
  *
  * To mitigate this problem somewhat, only unregister consoles whose memory
  * intersects with the init section. Note that all other boot consoles will
- * get unregistred when the real preferred console is registered.
+ * get unregistered when the real preferred console is registered.
  */
 static int __init printk_late_init(void)
 {
