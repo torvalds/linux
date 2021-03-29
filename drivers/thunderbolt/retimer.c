08fe7ae1857080f5075df5ac7fef2ecd4e289117
Dan Carpenter <dan.carpenter@oracle.com>
2021-03-29 09:08:01 +0300

    thunderbolt: Fix off by one in tb_port_find_retimer()
    This array uses 1-based indexing so it corrupts memory one element
    beyond of the array.  Fix it by making the array one element larger.
    
    Fixes: dacb12877d92 ("thunderbolt: Add support for on-board retimers")
    Cc: stable@vger.kernel.org
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>

--- drivers/thunderbolt/retimer.c
+++ drivers/thunderbolt/retimer.c
@@ -406,7 +406,7 @@ static struct tb_retimer *tb_port_find_retimer(struct tb_port *port, u8 index)
  */
 int tb_retimer_scan(struct tb_port *port)
 {
-	u32 status[TB_MAX_RETIMER_INDEX] = {};
+	u32 status[TB_MAX_RETIMER_INDEX + 1] = {};
 	int ret, i, last_idx = 0;
 
 	if (!port->cap_usb4)
