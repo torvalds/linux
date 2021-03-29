8bc5cbf5b65cac4fcdc11a38c5fd36db6884ebd0
Martin Kaiser <martin@kaiser.cx>
2021-03-29 11:32:15 +0200

    staging: rtl8188eu: (trivial) remove a duplicate debug print
    Keep the one that shows the wakeup capability.
    
    Signed-off-by: Martin Kaiser <martin@kaiser.cx>
    Link: https://lore.kernel.org/r/20210329093215.16186-1-martin@kaiser.cx
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/os_dep/usb_intf.c
+++ drivers/staging/rtl8188eu/os_dep/usb_intf.c
@@ -376,7 +376,6 @@ static struct adapter *rtw_usb_if1_init(struct dvobj_priv *dvobj,
 		dvobj->pusbdev->do_remote_wakeup = 1;
 		pusb_intf->needs_remote_wakeup = 1;
 		device_init_wakeup(&pusb_intf->dev, 1);
-		pr_debug("\n  padapter->pwrctrlpriv.bSupportRemoteWakeup~~~~~~\n");
 		pr_debug("\n  padapter->pwrctrlpriv.bSupportRemoteWakeup~~~[%d]~~~\n",
 			 device_may_wakeup(&pusb_intf->dev));
 	}
