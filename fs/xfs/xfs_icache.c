7e2a8af528396d275962b33af9e5350da10c01f3
Christoph Hellwig <hch@lst.de>
2021-03-29 11:11:39 -0700

    xfs: don't clear the "dinode core" in xfs_inode_alloc
    The xfs_icdinode structure just contains a random mix of inode field,
    which are all read from the on-disk inode and mostly not looked at
    before reading the inode or initializing a new inode cluster.  The
    only exceptions are the forkoff and blocks field, which are used
    in sanity checks for freshly allocated inodes.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_icache.c
+++ fs/xfs/xfs_icache.c
@@ -63,8 +63,9 @@ xfs_inode_alloc(
 	memset(&ip->i_df, 0, sizeof(ip->i_df));
 	ip->i_flags = 0;
 	ip->i_delayed_blks = 0;
-	memset(&ip->i_d, 0, sizeof(ip->i_d));
 	ip->i_d.di_flags2 = mp->m_ino_geo.new_diflags2;
+	ip->i_d.di_nblocks = 0;
+	ip->i_d.di_forkoff = 0;
 	ip->i_sick = 0;
 	ip->i_checked = 0;
 	INIT_WORK(&ip->i_ioend_work, xfs_end_io);
