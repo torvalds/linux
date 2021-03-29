56cfe6f820a6315291eb5a1b82bb49633b993d3b
Dmitry Osipenko <digetx@gmail.com>
2021-03-28 22:57:48 -0700

    Input: elants_i2c - drop zero-checking of ABS_MT_TOUCH_MAJOR resolution
    Drop unnecessary zero-checking of ABS_MT_TOUCH_MAJOR resolution since
    there is no difference between setting resolution to 0 vs not setting
    it at all. This change makes code cleaner a tad.
    
    Suggested-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Link: https://lore.kernel.org/r/20210328235507.19240-1-digetx@gmail.com
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/touchscreen/elants_i2c.c
+++ drivers/input/touchscreen/elants_i2c.c
@@ -1449,8 +1449,7 @@ static int elants_i2c_probe(struct i2c_client *client,
 
 	input_abs_set_res(ts->input, ABS_MT_POSITION_X, ts->x_res);
 	input_abs_set_res(ts->input, ABS_MT_POSITION_Y, ts->y_res);
-	if (ts->major_res > 0)
-		input_abs_set_res(ts->input, ABS_MT_TOUCH_MAJOR, ts->major_res);
+	input_abs_set_res(ts->input, ABS_MT_TOUCH_MAJOR, ts->major_res);
 
 	error = input_mt_init_slots(ts->input, MAX_CONTACT_NUM,
 				    INPUT_MT_DIRECT | INPUT_MT_DROP_UNUSED);
