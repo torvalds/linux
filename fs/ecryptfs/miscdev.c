65bbb7b84611c59725276ed1e03105fa002da9af
Lee Jones <lee.jones@linaro.org>
2021-03-30 17:44:54 +0100

    ecryptfs: miscdev: File headers are not good kernel-doc candidates
    Supply description for the 'daemon' param too.
    
    Fixes the following W=1 kernel build warning(s):
    
     fs/ecryptfs/miscdev.c:19: warning: cannot understand function prototype: 'atomic_t ecryptfs_num_miscdev_opens; '
     fs/ecryptfs/miscdev.c:323: warning: Function parameter or member 'daemon' not described in 'ecryptfs_miscdev_response'
    
    Cc: Tyler Hicks <code@tyhicks.com>
    Cc: "Michael A. Halcrow" <mhalcrow@us.ibm.com>
    Cc: ecryptfs@vger.kernel.org
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Tyler Hicks <code@tyhicks.com>

--- fs/ecryptfs/miscdev.c
+++ fs/ecryptfs/miscdev.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only
-/**
+/*
  * eCryptfs: Linux filesystem encryption layer
  *
  * Copyright (C) 2008 International Business Machines Corp.
@@ -312,6 +312,7 @@ out_unlock_daemon:
 
 /**
  * ecryptfs_miscdev_response - miscdevess response to message previously sent to daemon
+ * @daemon: eCryptfs daemon object
  * @data: Bytes comprising struct ecryptfs_message
  * @data_size: sizeof(struct ecryptfs_message) + data len
  * @seq: Sequence number for miscdev response packet
