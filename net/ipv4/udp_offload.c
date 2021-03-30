18f25dc399901426dff61e676ba603ff52c666f7
Paolo Abeni <pabeni@redhat.com>
2021-03-30 12:28:50 +0200

    udp: skip L4 aggregation for UDP tunnel packets
    If NETIF_F_GRO_FRAGLIST or NETIF_F_GRO_UDP_FWD are enabled, and there
    are UDP tunnels available in the system, udp_gro_receive() could end-up
    doing L4 aggregation (either SKB_GSO_UDP_L4 or SKB_GSO_FRAGLIST) at
    the outer UDP tunnel level for packets effectively carrying and UDP
    tunnel header.
    
    That could cause inner protocol corruption. If e.g. the relevant
    packets carry a vxlan header, different vxlan ids will be ignored/
    aggregated to the same GSO packet. Inner headers will be ignored, too,
    so that e.g. TCP over vxlan push packets will be held in the GRO
    engine till the next flush, etc.
    
    Just skip the SKB_GSO_UDP_L4 and SKB_GSO_FRAGLIST code path if the
    current packet could land in a UDP tunnel, and let udp_gro_receive()
    do GRO via udp_sk(sk)->gro_receive.
    
    The check implemented in this patch is broader than what is strictly
    needed, as the existing UDP tunnel could be e.g. configured on top of
    a different device: we could end-up skipping GRO at-all for some packets.
    
    Anyhow, that is a very thin corner case and covering it will add quite
    a bit of complexity.
    
    v1 -> v2:
     - hopefully clarify the commit message
    
    Fixes: 9fd1ff5d2ac7 ("udp: Support UDP fraglist GRO/GSO.")
    Fixes: 36707061d6ba ("udp: allow forwarding of plain (non-fraglisted) UDP GRO packets")
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/udp_offload.c
+++ net/ipv4/udp_offload.c
@@ -515,21 +515,24 @@ struct sk_buff *udp_gro_receive(struct list_head *head, struct sk_buff *skb,
 	unsigned int off = skb_gro_offset(skb);
 	int flush = 1;
 
+	/* we can do L4 aggregation only if the packet can't land in a tunnel
+	 * otherwise we could corrupt the inner stream
+	 */
 	NAPI_GRO_CB(skb)->is_flist = 0;
-	if (skb->dev->features & NETIF_F_GRO_FRAGLIST)
-		NAPI_GRO_CB(skb)->is_flist = sk ? !udp_sk(sk)->gro_enabled: 1;
+	if (!sk || !udp_sk(sk)->gro_receive) {
+		if (skb->dev->features & NETIF_F_GRO_FRAGLIST)
+			NAPI_GRO_CB(skb)->is_flist = sk ? !udp_sk(sk)->gro_enabled : 1;
 
-	if ((!sk && (skb->dev->features & NETIF_F_GRO_UDP_FWD)) ||
-	    (sk && udp_sk(sk)->gro_enabled) || NAPI_GRO_CB(skb)->is_flist) {
-		pp = call_gro_receive(udp_gro_receive_segment, head, skb);
+		if ((!sk && (skb->dev->features & NETIF_F_GRO_UDP_FWD)) ||
+		    (sk && udp_sk(sk)->gro_enabled) || NAPI_GRO_CB(skb)->is_flist)
+			pp = call_gro_receive(udp_gro_receive_segment, head, skb);
 		return pp;
 	}
 
-	if (!sk || NAPI_GRO_CB(skb)->encap_mark ||
+	if (NAPI_GRO_CB(skb)->encap_mark ||
 	    (uh->check && skb->ip_summed != CHECKSUM_PARTIAL &&
 	     NAPI_GRO_CB(skb)->csum_cnt == 0 &&
-	     !NAPI_GRO_CB(skb)->csum_valid) ||
-	    !udp_sk(sk)->gro_receive)
+	     !NAPI_GRO_CB(skb)->csum_valid))
 		goto out;
 
 	/* mark that this skb passed once through the tunnel gro layer */
