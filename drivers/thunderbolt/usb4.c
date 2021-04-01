0f28879cf6836f170773a9456c856e1f08f56764
Mika Westerberg <mika.westerberg@linux.intel.com>
2021-04-01 20:16:25 +0300

    thunderbolt: Log the link as TBT instead of TBT3
    The upstream port can be connected to any previous generation
    Thunderbolt port so logging as "TBT" is more accurate than "TBT3.
    
    No functional changes.
    
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/thunderbolt/usb4.c
+++ drivers/thunderbolt/usb4.c
@@ -260,7 +260,7 @@ int usb4_switch_setup(struct tb_switch *sw)
 	parent = tb_switch_parent(sw);
 	downstream_port = tb_port_at(tb_route(sw), parent);
 	sw->link_usb4 = link_is_usb4(downstream_port);
-	tb_sw_dbg(sw, "link: %s\n", sw->link_usb4 ? "USB4" : "TBT3");
+	tb_sw_dbg(sw, "link: %s\n", sw->link_usb4 ? "USB4" : "TBT");
 
 	xhci = val & ROUTER_CS_6_HCI;
 	tbt3 = !(val & ROUTER_CS_6_TNS);
