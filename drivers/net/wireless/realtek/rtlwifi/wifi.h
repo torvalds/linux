987e9bcdd0b76a9d35f0b82013294429b401f7bb
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2021-04-02 19:09:35 +0200

    rtlwifi: remove rtl_get_tid_h
    'rtl_get_tid_h()' is the same as 'ieee80211_get_tid()'.
    So this function can be removed to save a line of code.
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/db340a67a95c119e4f9ba8fa99aea1c73d0dcfc9.1617383263.git.christophe.jaillet@wanadoo.fr

--- drivers/net/wireless/realtek/rtlwifi/wifi.h
+++ drivers/net/wireless/realtek/rtlwifi/wifi.h
@@ -3086,14 +3086,9 @@ static inline __le16 rtl_get_fc(struct sk_buff *skb)
 	return rtl_get_hdr(skb)->frame_control;
 }
 
-static inline u16 rtl_get_tid_h(struct ieee80211_hdr *hdr)
-{
-	return (ieee80211_get_qos_ctl(hdr))[0] & IEEE80211_QOS_CTL_TID_MASK;
-}
-
 static inline u16 rtl_get_tid(struct sk_buff *skb)
 {
-	return rtl_get_tid_h(rtl_get_hdr(skb));
+	return ieee80211_get_tid(rtl_get_hdr(skb));
 }
 
 static inline struct ieee80211_sta *get_sta(struct ieee80211_hw *hw,
