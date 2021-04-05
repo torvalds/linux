1e1032b0c4afaed7739a6681ff6b4cb120b82994
Hengqi Chen <hengqi.chen@gmail.com>
2021-04-05 12:01:19 +0800

    libbpf: Fix KERNEL_VERSION macro
    Add missing ')' for KERNEL_VERSION macro.
    
    Signed-off-by: Hengqi Chen <hengqi.chen@gmail.com>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20210405040119.802188-1-hengqi.chen@gmail.com

--- tools/lib/bpf/bpf_helpers.h
+++ tools/lib/bpf/bpf_helpers.h
@@ -51,7 +51,7 @@
 #endif
 
 #ifndef KERNEL_VERSION
-#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + ((c) > 255 ? 255 : (c))
+#define KERNEL_VERSION(a, b, c) (((a) << 16) + ((b) << 8) + ((c) > 255 ? 255 : (c)))
 #endif
 
 /*
