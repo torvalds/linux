31331cb84bf4449a2f902b47966787891f6a3de0
Simon Xue <xxm@rock-chips.com>
2021-04-06 16:21:41 +0800

    iommu/rockchip: use status register instead of flags as parameter
    When iommu fall into interrupt, status register indicate more information
    than read or write flag, so use status register as parameter when callback
    report_iommu_fault
    
    Change-Id: I07c6b9996f305eb970bd1d1d2b0f2a7db53cc6ba
    Signed-off-by: Simon Xue <xxm@rock-chips.com>

--- drivers/iommu/rockchip-iommu.c
+++ drivers/iommu/rockchip-iommu.c
@@ -592,7 +592,7 @@ static irqreturn_t rk_iommu_irq(int irq, void *dev_id)
 			 */
 			if (iommu->domain)
 				report_iommu_fault(iommu->domain, iommu->dev, iova,
-						   flags);
+						   status);
 			else
 				dev_err(iommu->dev, "Page fault while iommu not attached to domain?\n");
 
