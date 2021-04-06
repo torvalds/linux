8f3f46d77c8133ccce52e055a8b4b577699acc5f
Liujie Xie <xieliujie@oppo.com>
2021-04-06 11:27:34 +0800

    ANDROID: vendor_hooks: Add hooks to recognize special worker thread.
    Recognize special worker thread.
    
    Bug: 184571803
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    Change-Id: I2ad112627eb18b98d0868bac6bd00bbfd84b34c4

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -40,6 +40,7 @@
 #include <trace/hooks/thermal.h>
 #include <trace/hooks/ufshcd.h>
 #include <trace/hooks/cgroup.h>
+#include <trace/hooks/workqueue.h>
 #include <trace/hooks/sys.h>
 #include <trace/hooks/traps.h>
 #include <trace/hooks/avc.h>
@@ -183,6 +184,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_uclamp_eff_get);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpufreq_transition);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cgroup_set_task);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_syscall_prctl_finished);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_create_worker);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_check_preempt_tick);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_check_preempt_wakeup_ignore);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_replace_next_task_fair);
