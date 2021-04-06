d90b15edbefb2229d22c6a69113e954e761e1072
Keqian Zhu <zhukeqian1@huawei.com>
2021-04-06 14:35:04 +0800

    KVM: x86: Remove unused function declaration
    kvm_mmu_slot_largepage_remove_write_access() is decared but not used,
    just remove it.
    
    Signed-off-by: Keqian Zhu <zhukeqian1@huawei.com>
    Message-Id: <20210406063504.17552-1-zhukeqian1@huawei.com>
    Reviewed-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1443,8 +1443,6 @@ void kvm_mmu_zap_collapsible_sptes(struct kvm *kvm,
 				   const struct kvm_memory_slot *memslot);
 void kvm_mmu_slot_leaf_clear_dirty(struct kvm *kvm,
 				   struct kvm_memory_slot *memslot);
-void kvm_mmu_slot_largepage_remove_write_access(struct kvm *kvm,
-					struct kvm_memory_slot *memslot);
 void kvm_mmu_zap_all(struct kvm *kvm);
 void kvm_mmu_invalidate_mmio_sptes(struct kvm *kvm, u64 gen);
 unsigned long kvm_mmu_calculate_default_mmu_pages(struct kvm *kvm);
