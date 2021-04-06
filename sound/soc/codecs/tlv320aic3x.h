a96d2ba2d8248d5e8170f2f44f98d4a33329b08a
Jiri Prchal <jiri.prchal@aksignal.cz>
2021-04-06 16:24:38 +0200

    ASoC: codecs: tlv320aic3x: move I2C to separated file
    Moved I2C related staff to separated source file.
    
    Signed-off-by: Jiri Prchal <jiri.prchal@aksignal.cz>
    Link: https://lore.kernel.org/r/20210406142439.102396-4-jiri.prchal@aksignal.cz
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/tlv320aic3x.h
+++ sound/soc/codecs/tlv320aic3x.h
@@ -9,6 +9,13 @@
 #ifndef _AIC3X_H
 #define _AIC3X_H
 
+struct device;
+struct regmap_config;
+
+extern const struct regmap_config aic3x_regmap;
+int aic3x_probe(struct device *dev, struct regmap *regmap, kernel_ulong_t driver_data);
+int aic3x_remove(struct device *dev);
+
 #define AIC3X_MODEL_3X 0
 #define AIC3X_MODEL_33 1
 #define AIC3X_MODEL_3007 2
