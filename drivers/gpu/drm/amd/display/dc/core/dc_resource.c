70c14a1d7ee4d25d2024967979c8ba618849af54
Calvin Hou <Calvin.Hou@amd.com>
2021-04-07 15:07:45 -0400

    drm/amd/display: remove checking sink in is_timing_changed
    [Why]
    Sometimes, such as sleep wake, the link->local sink pointer changed,
    but the dc_stream_state->sink pointer is not changed. The checking
    of timing_changed reports wrong result, lead to link tear down
    unexpected wrongly.
    
    [How]
    SST compare local sink, MST compare proper remote link.
    
    Signed-off-by: Calvin Hou <Calvin.Hou@amd.com>
    Reviewed-by: Aric Cyr <Aric.Cyr@amd.com>
    Acked-by: Wayne Lin <Wayne.Lin@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_resource.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_resource.c
@@ -1706,12 +1706,6 @@ static bool is_timing_changed(struct dc_stream_state *cur_stream,
 	if (cur_stream == NULL)
 		return true;
 
-	/* If sink pointer changed, it means this is a hotplug, we should do
-	 * full hw setting.
-	 */
-	if (cur_stream->sink != new_stream->sink)
-		return true;
-
 	/* If output color space is changed, need to reprogram info frames */
 	if (cur_stream->output_color_space != new_stream->output_color_space)
 		return true;
