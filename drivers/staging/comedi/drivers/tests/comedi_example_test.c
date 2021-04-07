5b7b4ce1d1163beb41b7c76db428c285d6989a62
Ian Abbott <abbotti@mev.co.uk>
2021-04-07 19:13:41 +0100

    staging: comedi: tests: example_test: Rename to 'comedi_example_test'
    Rename the "example_test" module to "comedi_example_test" to make the
    name more relevant to Comedi.
    
    Cc: Spencer E. Olson <olsonse@umich.edu>
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Link: https://lore.kernel.org/r/20210407181342.1117754-6-abbotti@mev.co.uk
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/tests/comedi_example_test.c
+++ drivers/staging/comedi/drivers/tests/comedi_example_test.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0+
 /* vim: set ts=8 sw=8 noet tw=80 nowrap: */
 /*
- *  comedi/drivers/tests/example_test.c
+ *  comedi/drivers/tests/comedi_example_test.c
  *  Example set of unit tests.
  *
  *  COMEDI - Linux Control and Measurement Device Interface
