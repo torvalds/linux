5d3945e84b801a1fe068e0e98a0ca38b84d7d0b5
Xiaofei Tan <tanxiaofei@huawei.com>
2021-04-07 15:06:46 +0800

    tty: tty_ldisc: Fix an issue of code indent should use tabs
    Fix an issue of code indent should use tabs, reported by checkpatch.pl.
    
    Signed-off-by: Xiaofei Tan <tanxiaofei@huawei.com>
    Link: https://lore.kernel.org/r/1617779210-51576-7-git-send-email-tanxiaofei@huawei.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/tty_ldisc.c
+++ drivers/tty/tty_ldisc.c
@@ -459,7 +459,7 @@ static int tty_ldisc_open(struct tty_struct *tty, struct tty_ldisc *ld)
 	WARN_ON(test_and_set_bit(TTY_LDISC_OPEN, &tty->flags));
 	if (ld->ops->open) {
 		int ret;
-                /* BTM here locks versus a hangup event */
+		/* BTM here locks versus a hangup event */
 		ret = ld->ops->open(tty);
 		if (ret)
 			clear_bit(TTY_LDISC_OPEN, &tty->flags);
