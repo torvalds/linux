994d6608efe4a4c8834bdc5014c86f4bc6aceea6
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2021-04-09 03:58:48 -0500

    iommu/amd: Remove performance counter pre-initialization test
    In early AMD desktop/mobile platforms (during 2013), when the IOMMU
    Performance Counter (PMC) support was first introduced in
    commit 30861ddc9cca ("perf/x86/amd: Add IOMMU Performance Counter
    resource management"), there was a HW bug where the counters could not
    be accessed. The result was reading of the counter always return zero.
    
    At the time, the suggested workaround was to add a test logic prior
    to initializing the PMC feature to check if the counters can be programmed
    and read back the same value. This has been working fine until the more
    recent desktop/mobile platforms start enabling power gating for the PMC,
    which prevents access to the counters. This results in the PMC support
    being disabled unnecesarily.
    
    Unfortunatly, there is no documentation of since which generation
    of hardware the original PMC HW bug was fixed. Although, it was fixed
    soon after the first introduction of the PMC. Base on this, we assume
    that the buggy platforms are less likely to be in used, and it should
    be relatively safe to remove this legacy logic.
    
    Link: https://lore.kernel.org/linux-iommu/alpine.LNX.3.20.13.2006030935570.3181@monopod.intra.ispras.ru/
    Bugzilla: https://bugzilla.kernel.org/show_bug.cgi?id=201753
    Cc: Tj (Elloe Linux) <ml.linux@elloe.vision>
    Cc: Shuah Khan <skhan@linuxfoundation.org>
    Cc: Alexander Monakov <amonakov@ispras.ru>
    Cc: David Coe <david.coe@live.co.uk>
    Cc: Paul Menzel <pmenzel@molgen.mpg.de>
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Tested-by: Shuah Khan <skhan@linuxfoundation.org>
    Link: https://lore.kernel.org/r/20210409085848.3908-3-suravee.suthikulpanit@amd.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/amd/init.c
+++ drivers/iommu/amd/init.c
@@ -1712,32 +1712,15 @@ static int __init init_iommu_all(struct acpi_table_header *table)
 	return 0;
 }
 
-static int iommu_pc_get_set_reg(struct amd_iommu *iommu, u8 bank, u8 cntr,
-				u8 fxn, u64 *value, bool is_write);
-
 static void init_iommu_perf_ctr(struct amd_iommu *iommu)
 {
+	u64 val;
 	struct pci_dev *pdev = iommu->dev;
-	u64 val = 0xabcd, val2 = 0, save_reg = 0;
 
 	if (!iommu_feature(iommu, FEATURE_PC))
 		return;
 
 	amd_iommu_pc_present = true;
-
-	/* save the value to restore, if writable */
-	if (iommu_pc_get_set_reg(iommu, 0, 0, 0, &save_reg, false))
-		goto pc_false;
-
-	/* Check if the performance counters can be written to */
-	if ((iommu_pc_get_set_reg(iommu, 0, 0, 0, &val, true)) ||
-	    (iommu_pc_get_set_reg(iommu, 0, 0, 0, &val2, false)) ||
-	    (val != val2))
-		goto pc_false;
-
-	/* restore */
-	if (iommu_pc_get_set_reg(iommu, 0, 0, 0, &save_reg, true))
-		goto pc_false;
 
 	pci_info(pdev, "IOMMU performance counters supported\n");
 
@@ -1745,11 +1728,6 @@ static void init_iommu_perf_ctr(struct amd_iommu *iommu)
 	iommu->max_banks = (u8) ((val >> 12) & 0x3f);
 	iommu->max_counters = (u8) ((val >> 7) & 0xf);
 
-	return;
-
-pc_false:
-	pci_err(pdev, "Unable to read/write to IOMMU perf counter.\n");
-	amd_iommu_pc_present = false;
 	return;
 }
 
