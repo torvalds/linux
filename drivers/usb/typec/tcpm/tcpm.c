03bf239b596c6dace0f7a95dbeefc3f7c9a2bca7
Hans de Goede <hdegoede@redhat.com>
2021-04-09 15:40:32 +0200

    UPSTREAM: usb: typec: tcpm: Add support for altmodes
    Add support for altmodes described in the usb-connector fwnode
    associated with the Type-C controller by calling the new
    typec_port_register_altmodes_from_fwnode() helper for this.
    
    Reviewed-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://lore.kernel.org/r/20210409134033.105834-3-hdegoede@redhat.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Change-Id: I0d7de11b08f1c297e78c7f4cbea90b6d85f8abe5
    Signed-off-by: yubing.zhang <yubing.zhang@rock-chips.com>
    (cherry picked from commit 55d8b34772e0728a224198ba605eed8cfc570aa0)

--- drivers/usb/typec/tcpm/tcpm.c
+++ drivers/usb/typec/tcpm/tcpm.c
@@ -6522,6 +6522,10 @@ struct tcpm_port *tcpm_register_port(struct device *dev, struct tcpc_dev *tcpc)
 		goto out_role_sw_put;
 	}
 
+	typec_port_register_altmodes(port->typec_port,
+				     &tcpm_altmode_ops, port,
+				     port->port_altmode, ALTMODE_DISCOVERY_MAX);
+
 	mutex_lock(&port->lock);
 	tcpm_init(port);
 	mutex_unlock(&port->lock);
