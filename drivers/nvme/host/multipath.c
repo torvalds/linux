e234f1f8bb6dda941390e5d3f20b8f2d958f163d
Niklas Cassel <Niklas.Cassel@wdc.com>
2021-04-10 20:15:45 +0000

    nvme-multipath: remove single trailing whitespace
    There is a single trailing whitespace in multipath.c.
    Since this is just a single whitespace, the chances of this affecting
    backports to stable should be quite low, so let's just remove it.
    
    Signed-off-by: Niklas Cassel <niklas.cassel@wdc.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/nvme/host/multipath.c
+++ drivers/nvme/host/multipath.c
@@ -674,7 +674,7 @@ void nvme_mpath_add_disk(struct nvme_ns *ns, struct nvme_id_ns *id)
 			queue_work(nvme_wq, &ns->ctrl->ana_work);
 		}
 	} else {
-		ns->ana_state = NVME_ANA_OPTIMIZED; 
+		ns->ana_state = NVME_ANA_OPTIMIZED;
 		nvme_mpath_set_live(ns);
 	}
 
