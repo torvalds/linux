f70865db5ff35f5ed0c7e9ef63e7cca3d4947f04
Pavel Begunkov <asml.silence@gmail.com>
2021-04-11 01:46:40 +0100

    io_uring: return back safer resurrect
    Revert of revert of "io_uring: wait potential ->release() on resurrect",
    which adds a helper for resurrect not racing completion reinit, as was
    removed because of a strange bug with no clear root or link to the
    patch.
    
    Was improved, instead of rcu_synchronize(), just wait_for_completion()
    because we're at 0 refs and it will happen very shortly. Specifically
    use non-interruptible version to ignore all pending signals that may
    have ended prior interruptible wait.
    
    This reverts commit cb5e1b81304e089ee3ca948db4d29f71902eb575.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/7a080c20f686d026efade810b116b72f88abaff9.1618101759.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -1081,6 +1081,18 @@ static inline void io_req_set_rsrc_node(struct io_kiocb *req)
 	}
 }
 
+static void io_refs_resurrect(struct percpu_ref *ref, struct completion *compl)
+{
+	bool got = percpu_ref_tryget(ref);
+
+	/* already at zero, wait for ->release() */
+	if (!got)
+		wait_for_completion(compl);
+	percpu_ref_resurrect(ref);
+	if (got)
+		percpu_ref_put(ref);
+}
+
 static bool io_match_task(struct io_kiocb *head,
 			  struct task_struct *task,
 			  struct files_struct *files)
@@ -9788,12 +9800,11 @@ static int __io_uring_register(struct io_ring_ctx *ctx, unsigned opcode,
 			if (ret < 0)
 				break;
 		} while (1);
-
 		mutex_lock(&ctx->uring_lock);
 
 		if (ret) {
-			percpu_ref_resurrect(&ctx->refs);
-			goto out_quiesce;
+			io_refs_resurrect(&ctx->refs, &ctx->ref_comp);
+			return ret;
 		}
 	}
 
@@ -9886,7 +9897,6 @@ out:
 	if (io_register_op_must_quiesce(opcode)) {
 		/* bring the ctx back to life */
 		percpu_ref_reinit(&ctx->refs);
-out_quiesce:
 		reinit_completion(&ctx->ref_comp);
 	}
 	return ret;
