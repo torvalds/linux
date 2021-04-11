38c694e98f6a6c8dfa48f2ba6f442363ed836efb
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2021-04-11 14:51:06 +0200

    ASoC: cs35l35: Fix an error handling path in 'cs35l35_i2c_probe()'
    If 'devm_regmap_init_i2c()' fails, there is no need to goto err. We should
    return directly as already done by the surrounding error handling paths.
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Link: https://lore.kernel.org/r/15720439769ba94ffb65c90217392b0758b08f61.1618145369.git.christophe.jaillet@wanadoo.fr
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/cs35l35.c
+++ sound/soc/codecs/cs35l35.c
@@ -1488,7 +1488,7 @@ static int cs35l35_i2c_probe(struct i2c_client *i2c_client,
 	if (IS_ERR(cs35l35->regmap)) {
 		ret = PTR_ERR(cs35l35->regmap);
 		dev_err(dev, "regmap_init() failed: %d\n", ret);
-		goto err;
+		return ret;
 	}
 
 	for (i = 0; i < ARRAY_SIZE(cs35l35_supplies); i++)
