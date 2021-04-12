993e2d4bd9efc41f0943de39e9374ffdfde62e87
Trond Myklebust <trond.myklebust@hammerspace.com>
2021-04-12 12:15:50 -0400

    NFSv4: Don't modify the change attribute cached in the inode
    When the client is caching data and a write delegation is held, then the
    server may send a CB_GETATTR to query the attributes. When this happens,
    the client is supposed to bump the change attribute value that it
    returns if it holds cached data.
    However that process uses a value that is stored in the delegation. We
    do not want to bump the change attribute held in the inode.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfs/write.c
+++ fs/nfs/write.c
@@ -764,9 +764,6 @@ static void nfs_inode_add_request(struct inode *inode, struct nfs_page *req)
 	 * with invalidate/truncate.
 	 */
 	spin_lock(&mapping->private_lock);
-	if (!nfs_have_writebacks(inode) &&
-	    NFS_PROTO(inode)->have_delegation(inode, FMODE_WRITE))
-		inode_inc_iversion_raw(inode);
 	if (likely(!PageSwapCache(req->wb_page))) {
 		set_bit(PG_MAPPED, &req->wb_flags);
 		SetPagePrivate(req->wb_page);
