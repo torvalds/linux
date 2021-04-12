d3de0d11a219f32b185d525cca2568817e22d3a1
James Smart <jsmart2021@gmail.com>
2021-04-11 18:31:21 -0700

    scsi: lpfc: Fix lpfc_hdw_queue attribute being ignored
    The lpfc_hdw_queue attribute is to set the number of hardware queues to be
    created on the adapter. Normally, the value is set to a default, which
    allows the hw queue count to be sized dynamically based on adapter
    capabilities, CPU/platform architecture, or CPU type. Currently, when
    lpfc_hdw_queue is set to a specific value, is has no effect and the dynamic
    sizing occurs.
    
    The routine checking whether parameters are default or not ignores the
    lpfc_hdw_queue setting and invokes the dynamic logic.
    
    Fix the routine to additionally check the lpfc_hdw_queue attribute value
    before using dynamic scaling. Additionally, SLI-3 supports only a small
    number of queues with dedicated functions, thus it needs to be exempted
    from the variable scaling and set to the expected values.
    
    Link: https://lore.kernel.org/r/20210412013127.2387-11-jsmart2021@gmail.com
    Co-developed-by: Justin Tee <justin.tee@broadcom.com>
    Signed-off-by: Justin Tee <justin.tee@broadcom.com>
    Signed-off-by: James Smart <jsmart2021@gmail.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/lpfc/lpfc_attr.c
+++ drivers/scsi/lpfc/lpfc_attr.c
@@ -5813,7 +5813,9 @@ lpfc_irq_chann_init(struct lpfc_hba *phba, uint32_t val)
 	}
 
 	/* Check if default setting was passed */
-	if (val == LPFC_IRQ_CHANN_DEF)
+	if (val == LPFC_IRQ_CHANN_DEF &&
+	    phba->cfg_hdw_queue == LPFC_HBA_HDWQ_DEF &&
+	    phba->sli_rev == LPFC_SLI_REV4)
 		lpfc_assign_default_irq_chann(phba);
 
 	if (phba->irq_chann_mode != NORMAL_MODE) {
@@ -5852,7 +5854,12 @@ lpfc_irq_chann_init(struct lpfc_hba *phba, uint32_t val)
 			phba->cfg_irq_chann = LPFC_IRQ_CHANN_DEF;
 			return -EINVAL;
 		}
-		phba->cfg_irq_chann = val;
+		if (phba->sli_rev == LPFC_SLI_REV4) {
+			phba->cfg_irq_chann = val;
+		} else {
+			phba->cfg_irq_chann = 2;
+			phba->cfg_hdw_queue = 1;
+		}
 	}
 
 	return 0;
@@ -7409,7 +7416,8 @@ lpfc_get_cfgparam(struct lpfc_hba *phba)
 		phba->cfg_hdw_queue = phba->sli4_hba.num_present_cpu;
 	if (phba->cfg_irq_chann == 0)
 		phba->cfg_irq_chann = phba->sli4_hba.num_present_cpu;
-	if (phba->cfg_irq_chann > phba->cfg_hdw_queue)
+	if (phba->cfg_irq_chann > phba->cfg_hdw_queue &&
+	    phba->sli_rev == LPFC_SLI_REV4)
 		phba->cfg_irq_chann = phba->cfg_hdw_queue;
 
 	phba->cfg_soft_wwnn = 0L;
