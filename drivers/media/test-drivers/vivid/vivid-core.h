2d8b2a6431b38f4cb4046636117940b0cb0b3ecf
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2021-04-12 13:02:10 +0200

    media: vivid: remove stream_sliced_vbi_cap field
    Vivid tracks the VBI capture mode in vivid_dev->stream_sliced_vbi_cap
    field.  We can just look at the buffer type instead, and drop the field.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/test-drivers/vivid/vivid-core.h
+++ drivers/media/test-drivers/vivid/vivid-core.h
@@ -429,7 +429,6 @@ struct vivid_dev {
 	u32				vbi_cap_seq_start;
 	u32				vbi_cap_seq_count;
 	bool				vbi_cap_streaming;
-	bool				stream_sliced_vbi_cap;
 	u32				meta_cap_seq_start;
 	u32				meta_cap_seq_count;
 	bool				meta_cap_streaming;
