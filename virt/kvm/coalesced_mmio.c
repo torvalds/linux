2a20592baff59c5351c5200ec667e1a2aa22af85
Sean Christopherson <seanjc@google.com>
2021-04-12 15:20:49 -0700

    KVM: Stop looking for coalesced MMIO zones if the bus is destroyed
    commit 5d3c4c79384af06e3c8e25b7770b6247496b4417 upstream.
    
    Abort the walk of coalesced MMIO zones if kvm_io_bus_unregister_dev()
    fails to allocate memory for the new instance of the bus.  If it can't
    instantiate a new bus, unregister_dev() destroys all devices _except_ the
    target device.   But, it doesn't tell the caller that it obliterated the
    bus and invoked the destructor for all devices that were on the bus.  In
    the coalesced MMIO case, this can result in a deleted list entry
    dereference due to attempting to continue iterating on coalesced_zones
    after future entries (in the walk) have been deleted.
    
    Opportunistically add curly braces to the for-loop, which encompasses
    many lines but sneaks by without braces due to the guts being a single
    if statement.
    
    Fixes: f65886606c2d ("KVM: fix memory leak in kvm_io_bus_unregister_dev()")
    Cc: stable@vger.kernel.org
    Reported-by: Hao Sun <sunhao.th@gmail.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20210412222050.876100-3-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- virt/kvm/coalesced_mmio.c
+++ virt/kvm/coalesced_mmio.c
@@ -174,21 +174,36 @@ int kvm_vm_ioctl_unregister_coalesced_mmio(struct kvm *kvm,
 					   struct kvm_coalesced_mmio_zone *zone)
 {
 	struct kvm_coalesced_mmio_dev *dev, *tmp;
+	int r;
 
 	if (zone->pio != 1 && zone->pio != 0)
 		return -EINVAL;
 
 	mutex_lock(&kvm->slots_lock);
 
-	list_for_each_entry_safe(dev, tmp, &kvm->coalesced_zones, list)
+	list_for_each_entry_safe(dev, tmp, &kvm->coalesced_zones, list) {
 		if (zone->pio == dev->zone.pio &&
 		    coalesced_mmio_in_range(dev, zone->addr, zone->size)) {
-			kvm_io_bus_unregister_dev(kvm,
+			r = kvm_io_bus_unregister_dev(kvm,
 				zone->pio ? KVM_PIO_BUS : KVM_MMIO_BUS, &dev->dev);
 			kvm_iodevice_destructor(&dev->dev);
+
+			/*
+			 * On failure, unregister destroys all devices on the
+			 * bus _except_ the target device, i.e. coalesced_zones
+			 * has been modified.  No need to restart the walk as
+			 * there aren't any zones left.
+			 */
+			if (r)
+				break;
 		}
+	}
 
 	mutex_unlock(&kvm->slots_lock);
 
+	/*
+	 * Ignore the result of kvm_io_bus_unregister_dev(), from userspace's
+	 * perspective, the coalesced MMIO is most definitely unregistered.
+	 */
 	return 0;
 }
