2b9ea5a8cf1bdc82f494da5a90191aa8b042980d
Lorenzo Bianconi <lorenzo@kernel.org>
2021-04-13 14:13:54 +0200

    mt76: mt7921: add mt7921_dma_cleanup in mt7921_unregister_device
    In order to avoid memory leaks, clean the dma engine unloading the
    module
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7921/init.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/init.c
@@ -272,8 +272,9 @@ int mt7921_register_device(struct mt7921_dev *dev)
 void mt7921_unregister_device(struct mt7921_dev *dev)
 {
 	mt76_unregister_device(&dev->mt76);
-	mt7921_mcu_exit(dev);
 	mt7921_tx_token_put(dev);
+	mt7921_dma_cleanup(dev);
+	mt7921_mcu_exit(dev);
 
 	tasklet_disable(&dev->irq_tasklet);
 	mt76_free_device(&dev->mt76);
