33bae277f655cc5c548d9a12944ed797fd803d9e
Tao Huang <huangtao@rock-chips.com>
2021-04-13 18:40:34 +0800

    gpu: remove unused rogue_m driver
    Change-Id: I2d38df29926bc7c2e7e618b777cd36768cab0d0e
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- drivers/gpu/rogue_m/build/linux/config/compilers/arm-eabi.mk
+++
@@ -1,3 +0,0 @@
-# SPDX-License-Identifier: GPL-2.0
-# 32-bit ARM EABI compiler
-TARGET_PRIMARY_ARCH := target_armv7-a
--- drivers/gpu/rogue_m/build/linux/config/compilers/i386-linux-gnu.mk
+++
@@ -1,3 +0,0 @@
-# SPDX-License-Identifier: GPL-2.0
-# 32-bit x86 compiler
-TARGET_PRIMARY_ARCH   := target_i686
