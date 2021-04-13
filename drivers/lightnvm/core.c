1c6b0bc73fac9306462bd4794d00520690e97ef8
Tian Tao <tiantao6@hisilicon.com>
2021-04-13 10:52:55 +0000

    lightnvm: return the correct return value
    When memdup_user returns an error, memdup_user has two different return
    values, use PTR_ERR to get the correct return value.
    
    Signed-off-by: Tian Tao <tiantao6@hisilicon.com>
    Signed-off-by: Matias Bjørling <matias.bjorling@wdc.com>
    Link: https://lore.kernel.org/r/20210413105257.159260-3-matias.bjorling@wdc.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/lightnvm/core.c
+++ drivers/lightnvm/core.c
@@ -1257,7 +1257,7 @@ static long nvm_ioctl_info(struct file *file, void __user *arg)
 
 	info = memdup_user(arg, sizeof(struct nvm_ioctl_info));
 	if (IS_ERR(info))
-		return -EFAULT;
+		return PTR_ERR(info);
 
 	info->version[0] = NVM_VERSION_MAJOR;
 	info->version[1] = NVM_VERSION_MINOR;
