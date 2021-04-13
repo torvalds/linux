870e04ae45ea2e569d1ca2780439b16e988da08d
Lijun Pan <lijunp213@gmail.com>
2021-04-13 14:33:39 -0500

    ibmvnic: queue reset work in system_long_wq
    The reset process for ibmvnic commonly takes multiple seconds, clearly
    making it inappropriate for schedule_work/system_wq. The reason to make
    this change is that ibmvnic's use of the default system-wide workqueue
    for a relatively long-running work item can negatively affect other
    workqueue users. So, queue the relatively slow reset job to the
    system_long_wq.
    
    Suggested-by: Nathan Lynch <nathanl@linux.ibm.com>
    Signed-off-by: Lijun Pan <lijunp213@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/ibm/ibmvnic.c
+++ drivers/net/ethernet/ibm/ibmvnic.c
@@ -2292,8 +2292,9 @@ static void __ibmvnic_reset(struct work_struct *work)
 	adapter = container_of(work, struct ibmvnic_adapter, ibmvnic_reset);
 
 	if (test_and_set_bit_lock(0, &adapter->resetting)) {
-		schedule_delayed_work(&adapter->ibmvnic_delayed_reset,
-				      IBMVNIC_RESET_DELAY);
+		queue_delayed_work(system_long_wq,
+				   &adapter->ibmvnic_delayed_reset,
+				   IBMVNIC_RESET_DELAY);
 		return;
 	}
 
@@ -2437,7 +2438,7 @@ static int ibmvnic_reset(struct ibmvnic_adapter *adapter,
 	list_add_tail(&rwi->list, &adapter->rwi_list);
 	netdev_dbg(adapter->netdev, "Scheduling reset (reason %s)\n",
 		   reset_reason_to_string(reason));
-	schedule_work(&adapter->ibmvnic_reset);
+	queue_work(system_long_wq, &adapter->ibmvnic_reset);
 
 	ret = 0;
 err:
