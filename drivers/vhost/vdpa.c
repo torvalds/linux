3b8b6399666a29daa30b0bb3f5c9e3fc81c5a6a6
Jason Wang <jasowang@redhat.com>
2021-04-13 17:15:57 +0800

    vhost-vdpa: fix vm_flags for virtqueue doorbell mapping
    commit 3a3e0fad16d40a2aa68ddf7eea4acdf48b22dd44 upstream.
    
    The virtqueue doorbell is usually implemented via registeres but we
    don't provide the necessary vma->flags like VM_PFNMAP. This may cause
    several issues e.g when userspace tries to map the doorbell via vhost
    IOTLB, kernel may panic due to the page is not backed by page
    structure. This patch fixes this by setting the necessary
    vm_flags. With this patch, try to map doorbell via IOTLB will fail
    with bad address.
    
    Cc: stable@vger.kernel.org
    Fixes: ddd89d0a059d ("vhost_vdpa: support doorbell mapping via mmap")
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    Link: https://lore.kernel.org/r/20210413091557.29008-1-jasowang@redhat.com
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/vhost/vdpa.c
+++ drivers/vhost/vdpa.c
@@ -997,6 +997,7 @@ static int vhost_vdpa_mmap(struct file *file, struct vm_area_struct *vma)
 	if (vma->vm_end - vma->vm_start != notify.size)
 		return -ENOTSUPP;
 
+	vma->vm_flags |= VM_IO | VM_PFNMAP | VM_DONTEXPAND | VM_DONTDUMP;
 	vma->vm_ops = &vhost_vdpa_vm_ops;
 	return 0;
 }
