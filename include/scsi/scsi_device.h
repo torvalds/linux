886874af943953bc4ee575d828c3e3cc7d1aebe9
Bart Van Assche <bvanassche@acm.org>
2021-04-15 15:08:08 -0700

    scsi: core: Remove an incorrect comment
    scsi_device.sdev_target is used in more code than the single_lun code,
    hence remove the comment next to the definition of the sdev_target member.
    
    Link: https://lore.kernel.org/r/20210415220826.29438-3-bvanassche@acm.org
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Cc: Hannes Reinecke <hare@suse.de>
    Cc: Daniel Wagner <dwagner@suse.de>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- include/scsi/scsi_device.h
+++ include/scsi/scsi_device.h
@@ -146,7 +146,7 @@ struct scsi_device {
 	struct scsi_vpd __rcu *vpd_pg80;
 	struct scsi_vpd __rcu *vpd_pg89;
 	unsigned char current_tag;	/* current tag */
-	struct scsi_target      *sdev_target;   /* used only for single_lun */
+	struct scsi_target      *sdev_target;
 
 	blist_flags_t		sdev_bflags; /* black/white flags as also found in
 				 * scsi_devinfo.[hc]. For now used only to
