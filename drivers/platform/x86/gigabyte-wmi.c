8779c688edbbb387aa2f6953ce7bcaa56012cc6e
Julian Labus <julian@labus-online.de>
2021-04-15 09:45:26 +0200

    platform/x86: gigabyte-wmi: add X570 AORUS ELITE
    Add the X570 AORUS ELITE to gigabyte_wmi_known_working_platforms
    
    Signed-off-by: Julian Labus <julian@labus-online.de>
    Acked-By: Thomas Weißschuh <thomas@weissschuh.net>
    Link: https://lore.kernel.org/r/20210415074526.1782-1-julian@labus-online.de
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/platform/x86/gigabyte-wmi.c
+++ drivers/platform/x86/gigabyte-wmi.c
@@ -148,6 +148,10 @@ static const struct dmi_system_id gigabyte_wmi_known_working_platforms[] = {
 	}},
 	{ .matches = {
 		DMI_EXACT_MATCH(DMI_BOARD_VENDOR, "Gigabyte Technology Co., Ltd."),
+		DMI_EXACT_MATCH(DMI_BOARD_NAME, "X570 AORUS ELITE"),
+	}},
+	{ .matches = {
+		DMI_EXACT_MATCH(DMI_BOARD_VENDOR, "Gigabyte Technology Co., Ltd."),
 		DMI_EXACT_MATCH(DMI_BOARD_NAME, "X570 I AORUS PRO WIFI"),
 	}},
 	{ }
