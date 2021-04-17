d5a2abb0db9ea05f24d1e48d3e4787247e0c5248
Lorenzo Bianconi <lorenzo@kernel.org>
2021-04-17 18:51:37 +0200

    mt76: mt7921: remove leftover function declaration
    Get rid of leftover mt7921_mcu_add_bss_info routine declaration
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7921/mt7921.h
+++ drivers/net/wireless/mediatek/mt76/mt7921/mt7921.h
@@ -257,8 +257,6 @@ void mt7921_dma_prefetch(struct mt7921_dev *dev);
 void mt7921_dma_cleanup(struct mt7921_dev *dev);
 int mt7921_run_firmware(struct mt7921_dev *dev);
 int mt7921_mcu_init(struct mt7921_dev *dev);
-int mt7921_mcu_add_bss_info(struct mt7921_phy *phy,
-			    struct ieee80211_vif *vif, int enable);
 int mt7921_mcu_add_key(struct mt7921_dev *dev, struct ieee80211_vif *vif,
 		       struct mt7921_sta *msta, struct ieee80211_key_conf *key,
 		       enum set_key_cmd cmd);
