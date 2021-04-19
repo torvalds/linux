7240803b2b850a9b4b005a7a95f17ce362d24bfb
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2021-04-19 14:24:58 +0300

    mmc: mmc_spi: Use already parsed IRQ
    SPI core already parses and maps IRQ for us if provided.
    Use it instead of double parsing in mmc_spi_get_pdata().
    
    Due to above, change condition, since SPI core can hold
    an error pointer as invalid IRQ.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Link: https://lore.kernel.org/r/20210419112459.25241-5-andriy.shevchenko@linux.intel.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/of_mmc_spi.c
+++ drivers/mmc/host/of_mmc_spi.c
@@ -64,8 +64,8 @@ struct mmc_spi_platform_data *mmc_spi_get_pdata(struct spi_device *spi)
 	if (mmc_of_parse_voltage(mmc, &oms->pdata.ocr_mask) < 0)
 		goto err_ocr;
 
-	oms->detect_irq = irq_of_parse_and_map(np, 0);
-	if (oms->detect_irq != 0) {
+	oms->detect_irq = spi->irq;
+	if (oms->detect_irq > 0) {
 		oms->pdata.init = of_mmc_spi_init;
 		oms->pdata.exit = of_mmc_spi_exit;
 	} else {
