6b3aeafbc12c18036809108e301efe8056249233
Ansuel Smith <ansuelsmth@gmail.com>
2021-04-20 20:33:42 +0200

    thermal/drivers/tsens: Add support for ipq8064-tsens
    Add support for tsens present in ipq806x SoCs based on generic msm8960
    tsens driver.
    
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>
    Reviewed-by: Thara Gopinath <thara.gopinath@linaro.org>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/20210420183343.2272-9-ansuelsmth@gmail.com

--- drivers/thermal/qcom/tsens.c
+++ drivers/thermal/qcom/tsens.c
@@ -968,6 +968,9 @@ static SIMPLE_DEV_PM_OPS(tsens_pm_ops, tsens_suspend, tsens_resume);
 
 static const struct of_device_id tsens_table[] = {
 	{
+		.compatible = "qcom,ipq8064-tsens",
+		.data = &data_8960,
+	}, {
 		.compatible = "qcom,mdm9607-tsens",
 		.data = &data_9607,
 	}, {
