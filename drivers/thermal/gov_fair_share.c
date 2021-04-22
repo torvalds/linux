1a93369810660905f5e89b527cd709fa6832f7c8
Lukasz Luba <lukasz.luba@arm.com>
2021-04-22 16:36:23 +0100

    thermal/core/fair share: Use the lockless __thermal_cdev_update() function
    Use the new helper function and avoid unnecessery second lock/unlock,
    which was present in old approach with thermal_cdev_update().
    
    Signed-off-by: Lukasz Luba <lukasz.luba@arm.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/20210422153624.6074-3-lukasz.luba@arm.com

--- drivers/thermal/gov_fair_share.c
+++ drivers/thermal/gov_fair_share.c
@@ -107,10 +107,9 @@ static int fair_share_throttle(struct thermal_zone_device *tz, int trip)
 		instance->target = get_target_state(tz, cdev, percentage,
 						    cur_trip_level);
 
-		mutex_lock(&instance->cdev->lock);
-		instance->cdev->updated = false;
-		mutex_unlock(&instance->cdev->lock);
-		thermal_cdev_update(cdev);
+		mutex_lock(&cdev->lock);
+		__thermal_cdev_update(cdev);
+		mutex_unlock(&cdev->lock);
 	}
 
 	mutex_unlock(&tz->lock);
