29a812e4f3462c7020368a1f5b6cd7814225d983
Wei Ming Chen <jj251510319013@gmail.com>
2021-04-23 21:24:17 +0800

    usb: gadget: function: fix typo in f_hid.c
    Replace `me` with `be`
    
    Acked-by: Felipe Balbi <balbi@kernel.org>
    Signed-off-by: Wei Ming Chen <jj251510319013@gmail.com>
    Link: https://lore.kernel.org/r/20210423132417.4385-1-jj251510319013@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/gadget/function/f_hid.c
+++ drivers/usb/gadget/function/f_hid.c
@@ -1117,7 +1117,7 @@ static struct usb_function *hidg_alloc(struct usb_function_instance *fi)
 	hidg->func.setup   = hidg_setup;
 	hidg->func.free_func = hidg_free;
 
-	/* this could me made configurable at some point */
+	/* this could be made configurable at some point */
 	hidg->qlen	   = 4;
 
 	return &hidg->func;
