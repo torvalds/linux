a03e880a700d9e217f7660bfc3616d93783b6d8c
Bhanuprakash Modem <bhanuprakash.modem@intel.com>
2021-04-23 19:46:09 +0530

    drm/i915/display: Fix state mismatch in drm infoframe
    While reading the SDP infoframe, we are getting filtered with
    the encoder type INTEL_OUTPUT_DDI which causes the infoframe
    mismatch. This patch will drop encoder->type check as we can
    mask individual infoframe type.
    
    [1025.606556] i915 0000:00:02.0: [drm] *ERROR* mismatch in drm infoframe
    [1025.607865] i915 0000:00:02.0: [drm] *ERROR* expected:
    [1025.607879] i915 0000:00:02.0: HDMI infoframe: Dynamic Range and Mastering, version 1, length 26
    [1025.607889] i915 0000:00:02.0: length: 26
    [1025.607898] i915 0000:00:02.0: metadata type: 0
    [1025.608292] i915 0000:00:02.0: eotf: 2
    [1025.608302] i915 0000:00:02.0: x[0]: 35400
    [1025.608312] i915 0000:00:02.0: y[0]: 14599
    [1025.609115] i915 0000:00:02.0: x[1]: 8500
    [1025.609947] i915 0000:00:02.0: y[1]: 39850
    [1025.609959] i915 0000:00:02.0: x[2]: 6550
    [1025.609970] i915 0000:00:02.0: y[2]: 2300
    [1025.609980] i915 0000:00:02.0: white point x: 15634
    [1025.609989] i915 0000:00:02.0: white point y: 16450
    [1025.610381] i915 0000:00:02.0: max_display_mastering_luminance: 1000
    [1025.610392] i915 0000:00:02.0: min_display_mastering_luminance: 500
    [1025.610401] i915 0000:00:02.0: max_cll: 500
    [1025.610816] i915 0000:00:02.0: max_fall: 1000
    [1025.612457] i915 0000:00:02.0: [drm] *ERROR* found:
    [1025.614354] ------------[ cut here ]------------
    [1025.616244] pipe state doesn't match!
    [1025.617640] WARNING: CPU: 6 PID: 2114 at drivers/gpu/drm/i915/display/intel_display.c:9332 intel_atomic_commit_tail+0x14d4/0x17c0 [i915]
    
    V2:
    * Drop encoder->type check
    
    V3:
    * Remove internal reviews
    
    Cc: Uma Shankar <uma.shankar@intel.com>
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Bhanuprakash Modem <bhanuprakash.modem@intel.com>
    Reviewed-by: Uma Shankar <uma.shankar@intel.com>
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210423141609.28568-1-bhanuprakash.modem@intel.com

--- drivers/gpu/drm/i915/display/intel_dp.c
+++ drivers/gpu/drm/i915/display/intel_dp.c
@@ -3031,9 +3031,6 @@ void intel_read_dp_sdp(struct intel_encoder *encoder,
 		       struct intel_crtc_state *crtc_state,
 		       unsigned int type)
 {
-	if (encoder->type != INTEL_OUTPUT_DDI)
-		return;
-
 	switch (type) {
 	case DP_SDP_VSC:
 		intel_read_dp_vsc_sdp(encoder, crtc_state,
