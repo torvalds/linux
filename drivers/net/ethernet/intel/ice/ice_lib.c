b38b7f2bb418510397714c7fb3bed64b6fd024e5
Salil Mehta <salil.mehta@huawei.com>
2021-04-23 01:00:18 +0100

    ice: Re-organizes reqstd/avail {R, T}XQ check/code for efficiency
    If user has explicitly requested the number of {R,T}XQs, then it is
    unnecessary to get the count of already available {R,T}XQs from the
    PF avail_{r,t}xqs bitmap. This value will get overridden by user specified
    value in any case.
    
    Re-organize this code for improving the flow, readability and efficiency.
    This scope of improvement was found during the review of the ICE driver
    code.
    
    Fixes: 87324e747fde ("ice: Implement ethtool ops for channels")
    Cc: intel-wired-lan@lists.osuosl.org
    Tested-by: Tony Brelinski <tonyx.brelinski@intel.com>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>

--- drivers/net/ethernet/intel/ice/ice_lib.c
+++ drivers/net/ethernet/intel/ice/ice_lib.c
@@ -163,12 +163,13 @@ static void ice_vsi_set_num_qs(struct ice_vsi *vsi, u16 vf_id)
 
 	switch (vsi->type) {
 	case ICE_VSI_PF:
-		vsi->alloc_txq = min3(pf->num_lan_msix,
-				      ice_get_avail_txq_count(pf),
-				      (u16)num_online_cpus());
 		if (vsi->req_txq) {
 			vsi->alloc_txq = vsi->req_txq;
 			vsi->num_txq = vsi->req_txq;
+		} else {
+			vsi->alloc_txq = min3(pf->num_lan_msix,
+					      ice_get_avail_txq_count(pf),
+					      (u16)num_online_cpus());
 		}
 
 		pf->num_lan_tx = vsi->alloc_txq;
@@ -177,12 +178,13 @@ static void ice_vsi_set_num_qs(struct ice_vsi *vsi, u16 vf_id)
 		if (!test_bit(ICE_FLAG_RSS_ENA, pf->flags)) {
 			vsi->alloc_rxq = 1;
 		} else {
-			vsi->alloc_rxq = min3(pf->num_lan_msix,
-					      ice_get_avail_rxq_count(pf),
-					      (u16)num_online_cpus());
 			if (vsi->req_rxq) {
 				vsi->alloc_rxq = vsi->req_rxq;
 				vsi->num_rxq = vsi->req_rxq;
+			} else {
+				vsi->alloc_rxq = min3(pf->num_lan_msix,
+						      ice_get_avail_rxq_count(pf),
+						      (u16)num_online_cpus());
 			}
 		}
 
