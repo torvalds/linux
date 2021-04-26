615cee49b3ca55f54d527f7a6a7d0fd4fd6fef6b
Colin Ian King <colin.king@canonical.com>
2021-04-26 10:47:35 +0100

    io_uring: Fix uninitialized variable up.resv
    The variable up.resv is not initialized and is being checking for a
    non-zero value in the call to _io_register_rsrc_update. Fix this by
    explicitly setting the variable to 0.
    
    Addresses-Coverity: ("Uninitialized scalar variable)"
    Fixes: c3bdad027183 ("io_uring: add generic rsrc update with tags")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Link: https://lore.kernel.org/r/20210426094735.8320-1-colin.king@canonical.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -5842,6 +5842,7 @@ static int io_files_update(struct io_kiocb *req, unsigned int issue_flags)
 	up.data = req->rsrc_update.arg;
 	up.nr = 0;
 	up.tags = 0;
+	up.resv = 0;
 
 	mutex_lock(&ctx->uring_lock);
 	ret = __io_register_rsrc_update(ctx, IORING_RSRC_FILE,
