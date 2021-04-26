637822e63b79ee8a729f7ba2645a26cf5a524ee4
Can Guo <cang@codeaurora.org>
2021-04-25 20:48:39 -0700

    scsi: ufs: core: Cancel rpm_dev_flush_recheck_work during system suspend
    During ufs system suspend, leaving rpm_dev_flush_recheck_work running or
    pending is risky because concurrency may happen between system
    suspend/resume and runtime resume routine. Fix this by cancelling
    rpm_dev_flush_recheck_work synchronously during system suspend.
    
    Link: https://lore.kernel.org/r/1619408921-30426-3-git-send-email-cang@codeaurora.org
    Fixes: 51dd905bd2f6 ("scsi: ufs: Fix WriteBooster flush during runtime suspend")
    Reviewed-by: Daejun Park <daejun7.park@samsung.com>
    Signed-off-by: Can Guo <cang@codeaurora.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -8975,6 +8975,8 @@ int ufshcd_system_suspend(struct ufs_hba *hba)
 	if (!hba->is_powered)
 		return 0;
 
+	cancel_delayed_work_sync(&hba->rpm_dev_flush_recheck_work);
+
 	if ((ufs_get_pm_lvl_to_dev_pwr_mode(hba->spm_lvl) ==
 	     hba->curr_dev_pwr_mode) &&
 	    (ufs_get_pm_lvl_to_link_pwr_state(hba->spm_lvl) ==
