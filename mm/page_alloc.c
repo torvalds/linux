77febec206262bd80c4176f2281a7970cfe69536
David Hildenbrand <david@redhat.com>
2021-04-29 23:00:58 -0700

    mm/page_alloc: drop pr_info_ratelimited() in alloc_contig_range()
    The information that some PFNs are busy is:
    
    a) not helpful for ordinary users: we don't even know *who* called
       alloc_contig_range(). This is certainly not worth a pr_info.*().
    
    b) not really helpful for debugging: we don't have any details *why*
       these PFNs are busy, and that is what we usually care about.
    
    c) not complete: there are other cases where we fail alloc_contig_range()
       using different paths that are not getting recorded.
    
    For example, we reach this path once we succeeded in isolating pageblocks,
    but failed to migrate some pages - which can happen easily on ZONE_NORMAL
    (i.e., has_unmovable_pages() is racy) but also on ZONE_MOVABLE i.e., we
    would have to retry longer to migrate).
    
    For example via virtio-mem when unplugging memory, we can create quite
    some noise (especially with ZONE_NORMAL) that is not of interest to users
    - it's expected that some allocations may fail as memory is busy.
    
    Let's just drop that pr_info_ratelimit() and rather implement a dynamic
    debugging mechanism in the future that can give us a better reason why
    alloc_contig_range() failed on specific pages.
    
    Link: https://lkml.kernel.org/r/20210301150945.77012-1-david@redhat.com
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Zi Yan <ziy@nvidia.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Reviewed-by: Oscar Salvador <osalvador@suse.de>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -8676,8 +8676,6 @@ int alloc_contig_range(unsigned long start, unsigned long end,
 
 	/* Make sure the range is really isolated. */
 	if (test_pages_isolated(outer_start, end, 0)) {
-		pr_info_ratelimited("%s: [%lx, %lx) PFNs busy\n",
-			__func__, outer_start, end);
 		ret = -EBUSY;
 		goto done;
 	}
