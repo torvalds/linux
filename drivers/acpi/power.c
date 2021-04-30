5db91e9cb5b3f645a9540d2ab67a19e464d89754
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2021-04-30 15:32:22 +0200

    Revert "ACPI: scan: Turn off unused power resources during initialization"
    Revert commit 4b9ee772eaa8 ("ACPI: scan: Turn off unused power
    resources during initialization") that is reported to cause
    initialization issues to occur.
    
    Reported-by: Shujun Wang <wsj20369@163.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/power.c
+++ drivers/acpi/power.c
@@ -996,7 +996,6 @@ void acpi_resume_power_resources(void)
 
 	mutex_unlock(&power_resource_list_lock);
 }
-#endif
 
 void acpi_turn_off_unused_power_resources(void)
 {
@@ -1017,3 +1016,4 @@ void acpi_turn_off_unused_power_resources(void)
 
 	mutex_unlock(&power_resource_list_lock);
 }
+#endif
