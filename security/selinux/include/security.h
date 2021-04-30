9d55580966da3a812780156da2878cd46d8b3548
Bram Bonné <brambonne@google.com>
2021-04-30 11:50:19 +0200

    ANDROID: selinux: modify RTM_GETNEIGH{TBL}
    Map the permission gating RTM_GETNEIGH/RTM_GETNEIGHTBL messages to a
    new permission so that it can be distinguished from the other netlink
    route permissions in selinux policy. The new permission is triggered by
    a flag set in system images T and up.
    
    This change is intended to be backported to all kernels that a T system
    image can run on top of.
    
    Bug: 171572148
    Test: atest NetworkInterfaceTest
    Test: atest CtsSelinuxTargetSdkCurrentTestCases
    Test: atest bionic-unit-tests-static
    Test: On Cuttlefish, run combinations of:
        - Policy bit set or omitted (see https://r.android.com/1701847)
        - This patch applied or omitted
        - App having nlmsg_readneigh permission or not
      Verify that only the combination of this patch + the policy bit being
      set + the app not having the nlmsg_readneigh permission prevents the
      app from sending RTM_GETNEIGH messages.
    
    Change-Id: I4bcfce4decb34ea9388eeedfc4be67403de8a980
    Signed-off-by: Bram Bonné <brambonne@google.com>
    (cherry picked from commit fac07550bdac9adea0dbe3edbdbec7a9a690a178)

--- security/selinux/include/security.h
+++ security/selinux/include/security.h
@@ -98,6 +98,7 @@ struct selinux_state {
 	bool initialized;
 	bool policycap[__POLICYDB_CAPABILITY_MAX];
 	bool android_netlink_route;
+	bool android_netlink_getneigh;
 
 	struct page *status_page;
 	struct mutex status_lock;
@@ -225,6 +226,13 @@ static inline bool selinux_android_nlroute_getlink(void)
 	struct selinux_state *state = &selinux_state;
 
 	return state->android_netlink_route;
+}
+
+static inline bool selinux_android_nlroute_getneigh(void)
+{
+	struct selinux_state *state = &selinux_state;
+
+	return state->android_netlink_getneigh;
 }
 
 struct selinux_policy_convert_data;
