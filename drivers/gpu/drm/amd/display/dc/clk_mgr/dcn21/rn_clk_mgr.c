9119e993cfd16e65b34d626e8e0b51a847bb2a02
Anthony Wang <anthony1.wang@amd.com>
2021-04-30 09:09:02 -0400

    drm/amd/display: Handle potential dpp_inst mismatch with pipe_idx
    [Why]
    In some pipe harvesting configs, we will select the incorrect
    dpp_inst when programming DTO. This is because when any intermediate
    pipe is fused, resource instances are no longer in 1:1
    correspondence with pipe index.
    
    [How]
    When looping through pipes to program DTO, get the dpp_inst
    associated with each pipe from res_pool.
    
    Signed-off-by: Anthony Wang <anthony1.wang@amd.com>
    Reviewed-by: Yongqiang Sun <yongqiang.sun@amd.com>
    Acked-by: Stylon Wang <stylon.wang@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/clk_mgr/dcn21/rn_clk_mgr.c
+++ drivers/gpu/drm/amd/display/dc/clk_mgr/dcn21/rn_clk_mgr.c
@@ -106,10 +106,10 @@ static void rn_update_clocks_update_dpp_dto(struct clk_mgr_internal *clk_mgr,
 	for (i = 0; i < clk_mgr->base.ctx->dc->res_pool->pipe_count; i++) {
 		int dpp_inst, dppclk_khz, prev_dppclk_khz;
 
-		/* Loop index will match dpp->inst if resource exists,
-		 * and we want to avoid dependency on dpp object
+		/* Loop index may not match dpp->inst if some pipes disabled,
+		 * so select correct inst from res_pool
 		 */
-		dpp_inst = i;
+		dpp_inst = clk_mgr->base.ctx->dc->res_pool->dpps[i]->inst;
 		dppclk_khz = context->res_ctx.pipe_ctx[i].plane_res.bw.dppclk_khz;
 
 		prev_dppclk_khz = clk_mgr->dccg->pipe_dppclk_khz[i];
