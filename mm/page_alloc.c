cb66bede617581309883432e9a633e8cade2a36e
Mel Gorman <mgorman@techsingularity.net>
2021-04-29 23:01:42 -0700

    mm/page_alloc: rename alloced to allocated
    Patch series "Introduce a bulk order-0 page allocator with two in-tree users", v6.
    
    This series introduces a bulk order-0 page allocator with sunrpc and the
    network page pool being the first users.  The implementation is not
    efficient as semantics needed to be ironed out first.  If no other
    semantic changes are needed, it can be made more efficient.  Despite that,
    this is a performance-related for users that require multiple pages for an
    operation without multiple round-trips to the page allocator.  Quoting the
    last patch for the high-speed networking use-case
    
                Kernel          XDP stats       CPU     pps           Delta
                Baseline        XDP-RX CPU      total   3,771,046       n/a
                List            XDP-RX CPU      total   3,940,242    +4.49%
                Array           XDP-RX CPU      total   4,249,224   +12.68%
    
    Via the SUNRPC traces of svc_alloc_arg()
    
    	Single page: 25.007 us per call over 532,571 calls
    	Bulk list:    6.258 us per call over 517,034 calls
    	Bulk array:   4.590 us per call over 517,442 calls
    
    Both potential users in this series are corner cases (NFS and high-speed
    networks) so it is unlikely that most users will see any benefit in the
    short term.  Other potential other users are batch allocations for page
    cache readahead, fault around and SLUB allocations when high-order pages
    are unavailable.  It's unknown how much benefit would be seen by
    converting multiple page allocation calls to a single batch or what
    difference it may make to headline performance.
    
    Light testing of my own running dbench over NFS passed.  Chuck and Jesper
    conducted their own tests and details are included in the changelogs.
    
    Patch 1 renames a variable name that is particularly unpopular
    
    Patch 2 adds a bulk page allocator
    
    Patch 3 adds an array-based version of the bulk allocator
    
    Patches 4-5 adds micro-optimisations to the implementation
    
    Patches 6-7 SUNRPC user
    
    Patches 8-9 Network page_pool user
    
    This patch (of 9):
    
    Review feedback of the bulk allocator twice found problems with "alloced"
    being a counter for pages allocated.  The naming was based on the API name
    "alloc" and was based on the idea that verbal communication about malloc
    tends to use the fake word "malloced" instead of the fake word mallocated.
    To be consistent, this preparation patch renames alloced to allocated in
    rmqueue_bulk so the bulk allocator and per-cpu allocator use similar names
    when the bulk allocator is introduced.
    
    Link: https://lkml.kernel.org/r/20210325114228.27719-1-mgorman@techsingularity.net
    Link: https://lkml.kernel.org/r/20210325114228.27719-2-mgorman@techsingularity.net
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
    Reviewed-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Reviewed-by: Alexander Lobakin <alobakin@pm.me>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Chuck Lever <chuck.lever@oracle.com>
    Cc: Jesper Dangaard Brouer <brouer@redhat.com>
    Cc: Christoph Hellwig <hch@infradead.org>
    Cc: Alexander Duyck <alexander.duyck@gmail.com>
    Cc: Ilias Apalodimas <ilias.apalodimas@linaro.org>
    Cc: David Miller <davem@davemloft.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2949,7 +2949,7 @@ static int rmqueue_bulk(struct zone *zone, unsigned int order,
 			unsigned long count, struct list_head *list,
 			int migratetype, unsigned int alloc_flags)
 {
-	int i, alloced = 0;
+	int i, allocated = 0;
 
 	spin_lock(&zone->lock);
 	for (i = 0; i < count; ++i) {
@@ -2972,7 +2972,7 @@ static int rmqueue_bulk(struct zone *zone, unsigned int order,
 		 * pages are ordered properly.
 		 */
 		list_add_tail(&page->lru, list);
-		alloced++;
+		allocated++;
 		if (is_migrate_cma(get_pcppage_migratetype(page)))
 			__mod_zone_page_state(zone, NR_FREE_CMA_PAGES,
 					      -(1 << order));
@@ -2981,12 +2981,12 @@ static int rmqueue_bulk(struct zone *zone, unsigned int order,
 	/*
 	 * i pages were removed from the buddy list even if some leak due
 	 * to check_pcp_refill failing so adjust NR_FREE_PAGES based
-	 * on i. Do not confuse with 'alloced' which is the number of
+	 * on i. Do not confuse with 'allocated' which is the number of
 	 * pages added to the pcp list.
 	 */
 	__mod_zone_page_state(zone, NR_FREE_PAGES, -(i << order));
 	spin_unlock(&zone->lock);
-	return alloced;
+	return allocated;
 }
 
 #ifdef CONFIG_NUMA
