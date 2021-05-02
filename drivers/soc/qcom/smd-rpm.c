d8ea59e7e3d166098e6ecb81f84e7f4c5760325c
Bartosz Dudziak <bartosz.dudziak@snejp.pl>
2021-05-02 14:20:25 +0200

    soc: qcom: smd-rpm: Add MSM8226 compatible
    Add a compatible for the RPM on the Qualcomm MSM8226 platform.
    
    Signed-off-by: Bartosz Dudziak <bartosz.dudziak@snejp.pl>
    Link: https://lore.kernel.org/r/20210502122027.9351-3-bartosz.dudziak@snejp.pl
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/soc/qcom/smd-rpm.c
+++ drivers/soc/qcom/smd-rpm.c
@@ -233,6 +233,7 @@ static void qcom_smd_rpm_remove(struct rpmsg_device *rpdev)
 static const struct of_device_id qcom_smd_rpm_of_match[] = {
 	{ .compatible = "qcom,rpm-apq8084" },
 	{ .compatible = "qcom,rpm-ipq6018" },
+	{ .compatible = "qcom,rpm-msm8226" },
 	{ .compatible = "qcom,rpm-msm8916" },
 	{ .compatible = "qcom,rpm-msm8936" },
 	{ .compatible = "qcom,rpm-msm8974" },
