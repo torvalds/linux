b63ecaea97aac3020be0e5736253e88cefbc950b
Derek Fang <derek.fang@realtek.com>
2021-05-03 11:17:32 +0800

    ASoC: rt1019: Add non_legacy_dai_naming config
    Register the codec dai name as 'rt1019-aif' by adding
    non_legacy_dai_naming configuration.
    
    Signed-off-by: Derek Fang <derek.fang@realtek.com>
    Link: https://lore.kernel.org/r/20210503031732.22035-1-derek.fang@realtek.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/rt1019.c
+++ sound/soc/codecs/rt1019.c
@@ -522,6 +522,7 @@ static const struct snd_soc_component_driver soc_component_dev_rt1019 = {
 	.num_dapm_widgets	= ARRAY_SIZE(rt1019_dapm_widgets),
 	.dapm_routes		= rt1019_dapm_routes,
 	.num_dapm_routes	= ARRAY_SIZE(rt1019_dapm_routes),
+	.non_legacy_dai_naming	= 1,
 };
 
 static const struct regmap_config rt1019_regmap = {
