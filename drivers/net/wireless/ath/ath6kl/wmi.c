34f4c208a8ce5e640a523f089cfe22c7b4c8f2f8
Anirudh Rayabharam <mail@anirudhrb.com>
2021-05-03 13:57:10 +0200

    ath6kl: return error code in ath6kl_wmi_set_roam_lrssi_cmd()
    [ Upstream commit 54433367840b46a1555c8ed36c4c0cfc5dbf1358 ]
    
    Propagate error code from failure of ath6kl_wmi_cmd_send() to the
    caller.
    
    Signed-off-by: Anirudh Rayabharam <mail@anirudhrb.com>
    Cc: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20210503115736.2104747-44-gregkh@linuxfoundation.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/wireless/ath/ath6kl/wmi.c
+++ drivers/net/wireless/ath/ath6kl/wmi.c
@@ -776,10 +776,8 @@ int ath6kl_wmi_set_roam_lrssi_cmd(struct wmi *wmi, u8 lrssi)
 	cmd->info.params.roam_rssi_floor = DEF_LRSSI_ROAM_FLOOR;
 	cmd->roam_ctrl = WMI_SET_LRSSI_SCAN_PARAMS;
 
-	ath6kl_wmi_cmd_send(wmi, 0, skb, WMI_SET_ROAM_CTRL_CMDID,
+	return ath6kl_wmi_cmd_send(wmi, 0, skb, WMI_SET_ROAM_CTRL_CMDID,
 			    NO_SYNC_WMIFLAG);
-
-	return 0;
 }
 
 int ath6kl_wmi_force_roam_cmd(struct wmi *wmi, const u8 *bssid)
