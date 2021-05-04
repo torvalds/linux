bfccc4eade2bec1493f891ebcd3c6751eee971c9
Lai Jiangshan <laijs@linux.alibaba.com>
2021-05-04 21:50:14 +0200

    KVM/VMX: Invoke NMI non-IST entry instead of IST entry
    commit a217a6593cec8b315d4c2f344bae33660b39b703 upstream.
    
    In VMX, the host NMI handler needs to be invoked after NMI VM-Exit.
    Before commit 1a5488ef0dcf6 ("KVM: VMX: Invoke NMI handler via indirect
    call instead of INTn"), this was done by INTn ("int $2"). But INTn
    microcode is relatively expensive, so the commit reworked NMI VM-Exit
    handling to invoke the kernel handler by function call.
    
    But this missed a detail. The NMI entry point for direct invocation is
    fetched from the IDT table and called on the kernel stack.  But on 64-bit
    the NMI entry installed in the IDT expects to be invoked on the IST stack.
    It relies on the "NMI executing" variable on the IST stack to work
    correctly, which is at a fixed position in the IST stack.  When the entry
    point is unexpectedly called on the kernel stack, the RSP-addressed "NMI
    executing" variable is obviously also on the kernel stack and is
    "uninitialized" and can cause the NMI entry code to run in the wrong way.
    
    Provide a non-ist entry point for VMX which shares the C-function with
    the regular NMI entry and invoke the new asm entry point instead.
    
    On 32-bit this just maps to the regular NMI entry point as 32-bit has no
    ISTs and is not affected.
    
    [ tglx: Made it independent for backporting, massaged changelog ]
    
    Fixes: 1a5488ef0dcf6 ("KVM: VMX: Invoke NMI handler via indirect call instead of INTn")
    Signed-off-by: Lai Jiangshan <laijs@linux.alibaba.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Lai Jiangshan <laijs@linux.alibaba.com>
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/r/87r1imi8i1.ffs@nanos.tec.linutronix.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -36,6 +36,7 @@
 #include <asm/debugreg.h>
 #include <asm/desc.h>
 #include <asm/fpu/internal.h>
+#include <asm/idtentry.h>
 #include <asm/io.h>
 #include <asm/irq_remapping.h>
 #include <asm/kexec.h>
@@ -6354,18 +6355,17 @@ static void vmx_apicv_post_state_restore(struct kvm_vcpu *vcpu)
 
 void vmx_do_interrupt_nmi_irqoff(unsigned long entry);
 
-static void handle_interrupt_nmi_irqoff(struct kvm_vcpu *vcpu, u32 intr_info)
+static void handle_interrupt_nmi_irqoff(struct kvm_vcpu *vcpu,
+					unsigned long entry)
 {
-	unsigned int vector = intr_info & INTR_INFO_VECTOR_MASK;
-	gate_desc *desc = (gate_desc *)host_idt_base + vector;
-
 	kvm_before_interrupt(vcpu);
-	vmx_do_interrupt_nmi_irqoff(gate_offset(desc));
+	vmx_do_interrupt_nmi_irqoff(entry);
 	kvm_after_interrupt(vcpu);
 }
 
 static void handle_exception_nmi_irqoff(struct vcpu_vmx *vmx)
 {
+	const unsigned long nmi_entry = (unsigned long)asm_exc_nmi_noist;
 	u32 intr_info = vmx_get_intr_info(&vmx->vcpu);
 
 	/* if exit due to PF check for async PF */
@@ -6376,18 +6376,20 @@ static void handle_exception_nmi_irqoff(struct vcpu_vmx *vmx)
 		kvm_machine_check();
 	/* We need to handle NMIs before interrupts are enabled */
 	else if (is_nmi(intr_info))
-		handle_interrupt_nmi_irqoff(&vmx->vcpu, intr_info);
+		handle_interrupt_nmi_irqoff(&vmx->vcpu, nmi_entry);
 }
 
 static void handle_external_interrupt_irqoff(struct kvm_vcpu *vcpu)
 {
 	u32 intr_info = vmx_get_intr_info(vcpu);
+	unsigned int vector = intr_info & INTR_INFO_VECTOR_MASK;
+	gate_desc *desc = (gate_desc *)host_idt_base + vector;
 
 	if (WARN_ONCE(!is_external_intr(intr_info),
 	    "KVM: unexpected VM-Exit interrupt info: 0x%x", intr_info))
 		return;
 
-	handle_interrupt_nmi_irqoff(vcpu, intr_info);
+	handle_interrupt_nmi_irqoff(vcpu, gate_offset(desc));
 }
 
 static void vmx_handle_exit_irqoff(struct kvm_vcpu *vcpu)
