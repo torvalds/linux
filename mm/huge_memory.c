aaa9705b4af3608fd759c9ba8d0003f7a83fb335
Miaohe Lin <linmiaohe@huawei.com>
2021-05-04 18:33:55 -0700

    mm/huge_memory.c: make get_huge_zero_page() return bool
    It's guaranteed that huge_zero_page will not be NULL if
    huge_zero_refcount is increased successfully.
    
    When READ_ONCE(huge_zero_page) is returned, there must be a
    huge_zero_page and it can be replaced with returning
    'true' when we do not care about the value of huge_zero_page.
    
    We can thus make it return bool to save READ_ONCE cpu cycles as the
    return value is just used to check if huge_zero_page exists.
    
    Link: https://lkml.kernel.org/r/20210318122722.13135-3-linmiaohe@huawei.com
    Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
    Reviewed-by: Zi Yan <ziy@nvidia.com>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Cc: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Michel Lespinasse <walken@google.com>
    Cc: Ralph Campbell <rcampbell@nvidia.com>
    Cc: Thomas Hellstrm (Intel) <thomas_os@shipmail.org>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Wei Yang <richard.weiyang@linux.alibaba.com>
    Cc: William Kucharski <william.kucharski@oracle.com>
    Cc: Yang Shi <yang.shi@linux.alibaba.com>
    Cc: yuleixzhang <yulei.kernel@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -77,18 +77,18 @@ bool transparent_hugepage_enabled(struct vm_area_struct *vma)
 	return false;
 }
 
-static struct page *get_huge_zero_page(void)
+static bool get_huge_zero_page(void)
 {
 	struct page *zero_page;
 retry:
 	if (likely(atomic_inc_not_zero(&huge_zero_refcount)))
-		return READ_ONCE(huge_zero_page);
+		return true;
 
 	zero_page = alloc_pages((GFP_TRANSHUGE | __GFP_ZERO) & ~__GFP_MOVABLE,
 			HPAGE_PMD_ORDER);
 	if (!zero_page) {
 		count_vm_event(THP_ZERO_PAGE_ALLOC_FAILED);
-		return NULL;
+		return false;
 	}
 	count_vm_event(THP_ZERO_PAGE_ALLOC);
 	preempt_disable();
@@ -101,7 +101,7 @@ retry:
 	/* We take additional reference here. It will be put back by shrinker */
 	atomic_set(&huge_zero_refcount, 2);
 	preempt_enable();
-	return READ_ONCE(huge_zero_page);
+	return true;
 }
 
 static void put_huge_zero_page(void)
