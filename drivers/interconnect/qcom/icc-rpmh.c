796c5f9e431d359396a94ad672ca7db0d8c71324
Mike Tipton <mdtipton@codeaurora.org>
2021-05-05 00:09:22 -0700

    interconnect: qcom: icc-rpmh: Enable BCMs needed for QoS configuration
    Some QoS blocks require voting for BCMs before their registers can be
    accessed, since they require clocks and regulators controlled by BCM.
    Vote for these BCMs before configuring QoS during probe.
    
    Change-Id: Ic93d224c7dc56c8e56d527179a0c2d1e888e3822
    Signed-off-by: Mike Tipton <mdtipton@codeaurora.org>

--- drivers/interconnect/qcom/icc-rpmh.c
+++ drivers/interconnect/qcom/icc-rpmh.c
@@ -218,6 +218,72 @@ int qcom_icc_bcm_init(struct qcom_icc_bcm *bcm, struct device *dev)
 }
 EXPORT_SYMBOL_GPL(qcom_icc_bcm_init);
 
+static bool bcm_needs_qos_proxy(struct qcom_icc_bcm *bcm)
+{
+	int i;
+
+	if (bcm->voter_idx == 0)
+		for (i = 0; i < bcm->num_nodes; i++)
+			if (bcm->nodes[i]->qosbox)
+				return true;
+
+	return false;
+}
+
+static int enable_qos_deps(struct qcom_icc_provider *qp)
+{
+	struct qcom_icc_bcm *bcm;
+	struct bcm_voter *voter;
+	bool keepalive;
+	int ret, i;
+
+	for (i = 0; i < qp->num_bcms; i++) {
+		bcm = qp->bcms[i];
+		if (bcm_needs_qos_proxy(bcm)) {
+			keepalive = bcm->keepalive;
+			bcm->keepalive = true;
+
+			voter = qp->voters[bcm->voter_idx];
+			qcom_icc_bcm_voter_add(voter, bcm);
+			ret = qcom_icc_bcm_voter_commit(voter);
+
+			bcm->keepalive = keepalive;
+
+			if (ret) {
+				dev_err(qp->dev, "failed to vote BW to %s for QoS\n",
+					bcm->name);
+				return ret;
+			}
+		}
+	}
+
+	ret = clk_bulk_prepare_enable(qp->num_clks, qp->clks);
+	if (ret) {
+		dev_err(qp->dev, "failed to enable clocks for QoS\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+static void disable_qos_deps(struct qcom_icc_provider *qp)
+{
+	struct qcom_icc_bcm *bcm;
+	struct bcm_voter *voter;
+	int i;
+
+	clk_bulk_disable_unprepare(qp->num_clks, qp->clks);
+
+	for (i = 0; i < qp->num_bcms; i++) {
+		bcm = qp->bcms[i];
+		if (bcm_needs_qos_proxy(bcm)) {
+			voter = qp->voters[bcm->voter_idx];
+			qcom_icc_bcm_voter_add(voter, bcm);
+			qcom_icc_bcm_voter_commit(voter);
+		}
+	}
+}
+
 static struct regmap *qcom_icc_rpmh_map(struct platform_device *pdev,
 					const struct qcom_icc_desc *desc)
 {
@@ -307,14 +373,12 @@ int qcom_icc_rpmh_probe(struct platform_device *pdev)
 	if (qp->num_clks < 0)
 		return qp->num_clks;
 
-	ret = clk_bulk_prepare_enable(qp->num_clks, qp->clks);
-	if (ret) {
-		dev_err(&pdev->dev, "failed to enable clocks\n");
-		return ret;
-	}
-
 	for (i = 0; i < qp->num_bcms; i++)
 		qcom_icc_bcm_init(qp->bcms[i], dev);
+
+	ret = enable_qos_deps(qp);
+	if (ret)
+		return ret;
 
 	for (i = 0; i < num_nodes; i++) {
 		qn = qnodes[i];
@@ -341,6 +405,8 @@ int qcom_icc_rpmh_probe(struct platform_device *pdev)
 
 		data->nodes[i] = node;
 	}
+
+	disable_qos_deps(qp);
 
 	data->num_nodes = num_nodes;
 	platform_set_drvdata(pdev, qp);
