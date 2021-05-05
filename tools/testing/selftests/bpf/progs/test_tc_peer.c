096eccdef0b32f47e9354231ddc3aaaf9527d51c
Jussi Maki <joamaki@gmail.com>
2021-05-05 08:59:25 +0000

    selftests/bpf: Rewrite test_tc_redirect.sh as prog_tests/tc_redirect.c
    As discussed in [0], this ports test_tc_redirect.sh to the test_progs
    framework and removes the old test.
    
    This makes it more in line with rest of the tests and makes it possible
    to run this test case with vmtest.sh and under the bpf CI.
    
    The upcoming skb_change_head() helper fix in [0] is depending on it and
    extending the test case to redirect a packet from L3 device to veth.
    
      [0] https://lore.kernel.org/bpf/20210427135550.807355-1-joamaki@gmail.com
    
    Signed-off-by: Jussi Maki <joamaki@gmail.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/bpf/20210505085925.783985-1-joamaki@gmail.com

--- tools/testing/selftests/bpf/progs/test_tc_peer.c
+++ tools/testing/selftests/bpf/progs/test_tc_peer.c
@@ -8,38 +8,25 @@
 
 #include <bpf/bpf_helpers.h>
 
-enum {
-	dev_src,
-	dev_dst,
-};
+static volatile const __u32 IFINDEX_SRC;
+static volatile const __u32 IFINDEX_DST;
 
-struct bpf_map_def SEC("maps") ifindex_map = {
-	.type		= BPF_MAP_TYPE_ARRAY,
-	.key_size	= sizeof(int),
-	.value_size	= sizeof(int),
-	.max_entries	= 2,
-};
-
-static __always_inline int get_dev_ifindex(int which)
-{
-	int *ifindex = bpf_map_lookup_elem(&ifindex_map, &which);
-
-	return ifindex ? *ifindex : 0;
-}
-
-SEC("chk_egress") int tc_chk(struct __sk_buff *skb)
+SEC("classifier/chk_egress")
+int tc_chk(struct __sk_buff *skb)
 {
 	return TC_ACT_SHOT;
 }
 
-SEC("dst_ingress") int tc_dst(struct __sk_buff *skb)
+SEC("classifier/dst_ingress")
+int tc_dst(struct __sk_buff *skb)
 {
-	return bpf_redirect_peer(get_dev_ifindex(dev_src), 0);
+	return bpf_redirect_peer(IFINDEX_SRC, 0);
 }
 
-SEC("src_ingress") int tc_src(struct __sk_buff *skb)
+SEC("classifier/src_ingress")
+int tc_src(struct __sk_buff *skb)
 {
-	return bpf_redirect_peer(get_dev_ifindex(dev_dst), 0);
+	return bpf_redirect_peer(IFINDEX_DST, 0);
 }
 
 char __license[] SEC("license") = "GPL";
