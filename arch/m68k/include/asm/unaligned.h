637be9183e0475c430fc77162c222bcaab887989
Arnd Bergmann <arnd@arndb.de>
2021-05-08 00:07:46 +0200

    asm-generic: use asm-generic/unaligned.h for most architectures
    There are several architectures that just duplicate the contents
    of asm-generic/unaligned.h, so change those over to use the
    file directly, to make future modifications easier.
    
    The exceptions are:
    
    - arm32 sets HAVE_EFFICIENT_UNALIGNED_ACCESS, but wants the
      unaligned-struct version
    
    - ppc64le disables HAVE_EFFICIENT_UNALIGNED_ACCESS but includes
      the access-ok version
    
    - most m68k also uses the access-ok version without setting
      HAVE_EFFICIENT_UNALIGNED_ACCESS.
    
    - sh4a has a custom inline asm version
    
    - openrisc is the only one using the memmove version that
      generally leads to worse code.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- arch/m68k/include/asm/unaligned.h
+++ arch/m68k/include/asm/unaligned.h
@@ -2,15 +2,8 @@
 #ifndef _ASM_M68K_UNALIGNED_H
 #define _ASM_M68K_UNALIGNED_H
 
-
 #ifdef CONFIG_CPU_HAS_NO_UNALIGNED
-#include <linux/unaligned/be_struct.h>
-#include <linux/unaligned/le_byteshift.h>
-#include <linux/unaligned/generic.h>
-
-#define get_unaligned	__get_unaligned_be
-#define put_unaligned	__put_unaligned_be
-
+#include <asm-generic/unaligned.h>
 #else
 /*
  * The m68k can do unaligned accesses itself.
