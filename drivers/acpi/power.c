29038ae2ae566d9441e81cda3539db17c20bf06a
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2021-05-10 14:02:17 +0200

    Revert "Revert "ACPI: scan: Turn off unused power resources during initialization""
    Revert commit 5db91e9cb5b3 ("Revert "ACPI: scan: Turn off unused
    power resources during initialization") which was not necessary.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/power.c
+++ drivers/acpi/power.c
@@ -996,6 +996,7 @@ void acpi_resume_power_resources(void)
 
 	mutex_unlock(&power_resource_list_lock);
 }
+#endif
 
 void acpi_turn_off_unused_power_resources(void)
 {
@@ -1016,4 +1017,3 @@ void acpi_turn_off_unused_power_resources(void)
 
 	mutex_unlock(&power_resource_list_lock);
 }
-#endif
