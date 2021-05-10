e2e8e4e8187509a77cb6328d876d9c09c07c2e82
Andy Shevchenko <andy.shevchenko@gmail.com>
2021-05-10 12:50:34 +0300

    leds: lm36274: Correct headers (of*.h -> mod_devicetable.h)
    There is no user of of*.h headers, but mod_devicetable.h.
    Update header block accordingly.
    
    Signed-off-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Signed-off-by: Pavel Machek <pavel@ucw.cz>

--- drivers/leds/leds-lm36274.c
+++ drivers/leds/leds-lm36274.c
@@ -7,8 +7,8 @@
 #include <linux/err.h>
 #include <linux/leds.h>
 #include <linux/leds-ti-lmu-common.h>
+#include <linux/mod_devicetable.h>
 #include <linux/module.h>
-#include <linux/of_device.h>
 #include <linux/platform_device.h>
 
 #include <linux/mfd/ti-lmu.h>
