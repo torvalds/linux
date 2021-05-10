d6f67afbdf9df5301641b2ef7ac4030abab3e067
Johannes Thumshirn <johannes.thumshirn@wdc.com>
2021-05-10 22:39:38 +0900

    btrfs: return 0 for dev_extent_hole_check_zoned hole_start in case of error
    Commit 7000babddac6 ("btrfs: assign proper values to a bool variable in
    dev_extent_hole_check_zoned") assigned false to the hole_start parameter
    of dev_extent_hole_check_zoned().
    
    The hole_start parameter is not boolean and returns the start location of
    the found hole.
    
    Fixes: 7000babddac6 ("btrfs: assign proper values to a bool variable in dev_extent_hole_check_zoned")
    Signed-off-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -1458,7 +1458,7 @@ static bool dev_extent_hole_check_zoned(struct btrfs_device *device,
 		/* Given hole range was invalid (outside of device) */
 		if (ret == -ERANGE) {
 			*hole_start += *hole_size;
-			*hole_size = false;
+			*hole_size = 0;
 			return true;
 		}
 
