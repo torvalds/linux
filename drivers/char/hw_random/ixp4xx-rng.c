479dfb6d56020548161ecfd3ba0b3770ac5f4bc2
Linus Walleij <linus.walleij@linaro.org>
2021-05-11 12:08:40 +0200

    hw_random: ixp4xx: Use SPDX license tag
    This switches the IXP4xx hardware random driver to use
    the SPDX type license tag.
    
    Cc: Deepak Saxena <dsaxena@plexity.net>
    Acked-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/char/hw_random/ixp4xx-rng.c
+++ drivers/char/hw_random/ixp4xx-rng.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * drivers/char/hw_random/ixp4xx-rng.c
  *
@@ -8,10 +9,6 @@
  * Copyright 2005 (c) MontaVista Software, Inc.
  *
  * Fixes by Michael Buesch
- *
- * This file is licensed under  the terms of the GNU General Public
- * License version 2. This program is licensed "as is" without any
- * warranty of any kind, whether express or implied.
  */
 
 #include <linux/kernel.h>
