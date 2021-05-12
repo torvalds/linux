36c159655637851978da5053a969946d082a6458
Greg Kroah-Hartman <gregkh@google.com>
2021-05-12 10:11:40 +0200

    Revert "mmc: block: Issue a cache flush only when it's enabled"
    This reverts commit 20d6f231f8f17ceb61e379b93aadca7c6042aa65 which
    showed up in 5.10.36 and broke the abi.  It should not be needed in
    Android systems at this time so it is safe to revert.
    
    Bug: 161946584
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: If85580eb86669f957de49faee773a2ed7690bcd2

--- drivers/mmc/core/mmc_ops.c
+++ drivers/mmc/core/mmc_ops.c
@@ -988,7 +988,9 @@ int mmc_flush_cache(struct mmc_card *card)
 {
 	int err = 0;
 
-	if (mmc_cache_enabled(card->host)) {
+	if (mmc_card_mmc(card) &&
+			(card->ext_csd.cache_size > 0) &&
+			(card->ext_csd.cache_ctrl & 1)) {
 		err = mmc_switch(card, EXT_CSD_CMD_SET_NORMAL,
 				 EXT_CSD_FLUSH_CACHE, 1,
 				 MMC_CACHE_FLUSH_TIMEOUT_MS);
