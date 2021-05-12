3b5169c2eb81e822445469a077223f8eb0729a59
Guenter Roeck <linux@roeck-us.net>
2021-05-12 15:48:09 -0700

    hwmon: (adm9240) Fix writes into inX_max attributes
    When converting the driver to use the devm_hwmon_device_register_with_info
    API, the wrong register was selected when writing into inX_max attributes.
    Fix it.
    
    Fixes: 124b7e34a5a6 ("hwmon: (adm9240) Convert to devm_hwmon_device_register_with_info API")
    Reported-by: Chris Packham <Chris.Packham@alliedtelesis.co.nz>
    Tested-by: Chris Packham <chris.packham@alliedtelesis.co.nz>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/adm9240.c
+++ drivers/hwmon/adm9240.c
@@ -485,7 +485,7 @@ static int adm9240_in_write(struct device *dev, u32 attr, int channel, long val)
 		reg = ADM9240_REG_IN_MIN(channel);
 		break;
 	case hwmon_in_max:
-		reg = ADM9240_REG_IN(channel);
+		reg = ADM9240_REG_IN_MAX(channel);
 		break;
 	default:
 		return -EOPNOTSUPP;
