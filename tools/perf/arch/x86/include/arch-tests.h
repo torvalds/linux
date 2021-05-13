4e277d0d83a5b5aadbe033af3ce7bffbcc51a6fd
Rob Herring <robh@kernel.org>
2021-05-13 12:46:13 -0500

    perf tests: Consolidate test__arch_unwind_sample declaration
    There's no reason for making the test__arch_unwind_sample declaration per
    arch. Currently that's done 2 different ways either with a declaration in
    arch-tests.h or with an arch define. Unify all this with an unconditional
    declaration in tests.h.
    
    Signed-off-by: Rob Herring <robh@kernel.org>
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: John Garry <john.garry@huawei.com>
    Cc: Leo Yan <leo.yan@linaro.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Mathieu Poirier <mathieu.poirier@linaro.org>
    Cc: Matt Fleming <matt.fleming@intel.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Will Deacon <will@kernel.org>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: masayoshi mizuma <msys.mizuma@gmail.com>
    Link: http://lore.kernel.org/lkml/20210513174614.2242210-1-robh@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/arch/x86/include/arch-tests.h
+++ tools/perf/arch/x86/include/arch-tests.h
@@ -12,13 +12,6 @@ int test__intel_pt_pkt_decoder(struct test *test, int subtest);
 int test__bp_modify(struct test *test, int subtest);
 int test__x86_sample_parsing(struct test *test, int subtest);
 
-#ifdef HAVE_DWARF_UNWIND_SUPPORT
-struct thread;
-struct perf_sample;
-int test__arch_unwind_sample(struct perf_sample *sample,
-			     struct thread *thread);
-#endif
-
 extern struct test arch_tests[];
 
 #endif
