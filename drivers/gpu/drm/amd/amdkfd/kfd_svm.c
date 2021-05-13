7a3ae1e249c2241ed520b0dec08b7b877b427a44
Philip Yang <Philip.Yang@amd.com>
2021-05-13 12:16:01 -0400

    Revert "drm/amdkfd: flush TLB after updating GPU page table"
    This reverts commit 1704ac8e439e3dcf97431e268bd64fde1874d807.
    
    After "drm/amdgpu: flush TLB if valid PDE turns into PTE" is checked
    in, this workaround is not needed.
    
    Signed-off-by: Philip Yang <Philip.Yang@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_svm.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_svm.c
@@ -1241,9 +1241,6 @@ static int svm_range_map_to_gpus(struct svm_range *prange,
 				break;
 			}
 		}
-
-		amdgpu_amdkfd_flush_gpu_tlb_pasid((struct kgd_dev *)adev,
-						  p->pasid);
 	}
 
 	return r;
