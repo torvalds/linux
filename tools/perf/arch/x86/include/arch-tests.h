19d71c2cbe060ca8b7da0a43ee549f8352211155
Rob Herring <robh@kernel.org>
2021-05-13 12:46:14 -0500

    perf tests: Drop __maybe_unused on x86 test declarations
    Function declarations don't need __maybe_unused annotations, only the
    implementations do. Drop them on the perf x86 tests.
    
    Signed-off-by: Rob Herring <robh@kernel.org>
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: John Garry <john.garry@huawei.com>
    Cc: Leo Yan <leo.yan@linaro.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Mathieu Poirier <mathieu.poirier@linaro.org>
    Cc: Matt Fleming <matt.fleming@intel.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Will Deacon <will@kernel.org>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: masayoshi mizuma <msys.mizuma@gmail.com>
    Link: http://lore.kernel.org/lkml/20210513174614.2242210-2-robh@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/arch/x86/include/arch-tests.h
+++ tools/perf/arch/x86/include/arch-tests.h
@@ -2,12 +2,11 @@
 #ifndef ARCH_TESTS_H
 #define ARCH_TESTS_H
 
-#include <linux/compiler.h>
 struct test;
 
 /* Tests */
-int test__rdpmc(struct test *test __maybe_unused, int subtest);
-int test__insn_x86(struct test *test __maybe_unused, int subtest);
+int test__rdpmc(struct test *test, int subtest);
+int test__insn_x86(struct test *test, int subtest);
 int test__intel_pt_pkt_decoder(struct test *test, int subtest);
 int test__bp_modify(struct test *test, int subtest);
 int test__x86_sample_parsing(struct test *test, int subtest);
