b0f5d4891ea3a80ad619312355494dc25073e219
Gulam Mohamed <gulam.mohamed@oracle.com>
2021-05-14 15:18:42 +0200

    block: fix a race between del_gendisk and BLKRRPART
    [ Upstream commit bc6a385132601c29a6da1dbf8148c0d3c9ad36dc ]
    
    When BLKRRPART is called concurrently with del_gendisk, the partitions
    rescan can create a stale partition that will never be be cleaned up.
    
    Fix this by checking the the disk is up before rescanning partitions
    while under bd_mutex.
    
    Signed-off-by: Gulam Mohamed <gulam.mohamed@oracle.com>
    [hch: split from a larger patch]
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Ming Lei <ming.lei@redhat.com>
    Link: https://lore.kernel.org/r/20210514131842.1600568-3-hch@lst.de
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/block_dev.c
+++ fs/block_dev.c
@@ -1408,6 +1408,9 @@ int bdev_disk_changed(struct block_device *bdev, bool invalidate)
 
 	lockdep_assert_held(&bdev->bd_mutex);
 
+	if (!(disk->flags & GENHD_FL_UP))
+		return -ENXIO;
+
 rescan:
 	ret = blk_drop_partitions(bdev);
 	if (ret)
