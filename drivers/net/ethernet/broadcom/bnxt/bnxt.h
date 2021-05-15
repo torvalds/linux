7e31bcbb335ec8cc4b948915e652d013a5816371
Michael Chan <michael.chan@broadcom.com>
2021-05-15 03:25:19 -0400

    bnxt_en: Fix context memory setup for 64K page size.
    [ Upstream commit 702279d2ce4650000bb6302013630304e359dc13 ]
    
    There was a typo in the code that checks for 64K BNXT_PAGE_SHIFT in
    bnxt_hwrm_set_pg_attr().  Fix it and make the code more understandable
    with a new macro BNXT_SET_CTX_PAGE_ATTR().
    
    Fixes: 1b9394e5a2ad ("bnxt_en: Configure context memory on new devices.")
    Reviewed-by: Edwin Peer <edwin.peer@broadcom.com>
    Signed-off-by: Michael Chan <michael.chan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.h
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.h
@@ -1440,6 +1440,16 @@ struct bnxt_ctx_pg_info {
 #define BNXT_MAX_TQM_RINGS		\
 	(BNXT_MAX_TQM_SP_RINGS + BNXT_MAX_TQM_FP_RINGS)
 
+#define BNXT_SET_CTX_PAGE_ATTR(attr)					\
+do {									\
+	if (BNXT_PAGE_SIZE == 0x2000)					\
+		attr = FUNC_BACKING_STORE_CFG_REQ_SRQ_PG_SIZE_PG_8K;	\
+	else if (BNXT_PAGE_SIZE == 0x10000)				\
+		attr = FUNC_BACKING_STORE_CFG_REQ_QPC_PG_SIZE_PG_64K;	\
+	else								\
+		attr = FUNC_BACKING_STORE_CFG_REQ_QPC_PG_SIZE_PG_4K;	\
+} while (0)
+
 struct bnxt_ctx_mem_info {
 	u32	qp_max_entries;
 	u16	qp_min_qp1_entries;
