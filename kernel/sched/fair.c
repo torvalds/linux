f57d24297768b9f5dcaff0c02205ca8018a1ee03
Wei Wang <wvw@google.com>
2021-05-14 21:06:44 -0700

    ANDROID: sched/fair: export sysctl_sched_min_granularity symbol
    Export sysctl_sched_min_granularity to be able to access from vendor modules.
    
    Bug: 170487162
    Signed-off-by: Wei Wang <wvw@google.com>
    Change-Id: I31d63adaa66280eb4c1c1daa75208c6b61c83781

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -60,6 +60,7 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_L
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
 unsigned int sysctl_sched_min_granularity			= 750000ULL;
+EXPORT_SYMBOL_GPL(sysctl_sched_min_granularity);
 static unsigned int normalized_sysctl_sched_min_granularity	= 750000ULL;
 
 /*
