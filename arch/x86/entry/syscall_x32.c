2e958a8a510d956ec8528f0bd20e309b5bb5156c
Masahiro Yamada <masahiroy@kernel.org>
2021-05-17 16:38:09 +0900

    x86/entry/x32: Rename __x32_compat_sys_* to __x64_compat_sys_*
    The SYSCALL macros are mapped to symbols as follows:
    
      __SYSCALL_COMMON(nr, sym)  -->  __x64_<sym>
      __SYSCALL_X32(nr, sym)     -->  __x32_<sym>
    
    Originally, the syscalls in the x32 special range (512-547) were all
    compat.
    
    This assumption is now broken after the following commits:
    
      55db9c0e8534 ("net: remove compat_sys_{get,set}sockopt")
      5f764d624a89 ("fs: remove the compat readv/writev syscalls")
      598b3cec831f ("fs: remove compat_sys_vmsplice")
      c3973b401ef2 ("mm: remove compat_process_vm_{readv,writev}")
    
    Those commits redefined __x32_sys_* to __x64_sys_* because there is no stub
    like __x32_sys_*.
    
    Defining them as follows is more sensible and cleaner.
    
      __SYSCALL_COMMON(nr, sym)  -->  __x64_<sym>
      __SYSCALL_X32(nr, sym)     -->  __x64_<sym>
    
    This works because both x86_64 and x32 use the same ABI (RDI, RSI, RDX,
    R10, R8, R9)
    
    The ugly #define __x32_sys_* will go away.
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20210517073815.97426-2-masahiroy@kernel.org

--- arch/x86/entry/syscall_x32.c
+++ arch/x86/entry/syscall_x32.c
@@ -8,27 +8,15 @@
 #include <asm/unistd.h>
 #include <asm/syscall.h>
 
-/*
- * Reuse the 64-bit entry points for the x32 versions that occupy different
- * slots in the syscall table.
- */
-#define __x32_sys_readv		__x64_sys_readv
-#define __x32_sys_writev	__x64_sys_writev
-#define __x32_sys_getsockopt	__x64_sys_getsockopt
-#define __x32_sys_setsockopt	__x64_sys_setsockopt
-#define __x32_sys_vmsplice	__x64_sys_vmsplice
-#define __x32_sys_process_vm_readv	__x64_sys_process_vm_readv
-#define __x32_sys_process_vm_writev	__x64_sys_process_vm_writev
-
 #define __SYSCALL_64(nr, sym)
 
-#define __SYSCALL_X32(nr, sym) extern long __x32_##sym(const struct pt_regs *);
+#define __SYSCALL_X32(nr, sym) extern long __x64_##sym(const struct pt_regs *);
 #define __SYSCALL_COMMON(nr, sym) extern long __x64_##sym(const struct pt_regs *);
 #include <asm/syscalls_64.h>
 #undef __SYSCALL_X32
 #undef __SYSCALL_COMMON
 
-#define __SYSCALL_X32(nr, sym) [nr] = __x32_##sym,
+#define __SYSCALL_X32(nr, sym) [nr] = __x64_##sym,
 #define __SYSCALL_COMMON(nr, sym) [nr] = __x64_##sym,
 
 asmlinkage const sys_call_ptr_t x32_sys_call_table[__NR_x32_syscall_max+1] = {
