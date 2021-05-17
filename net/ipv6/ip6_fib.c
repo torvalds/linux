67db5ca73b1f98584ae9b6ed35c1c670677c9001
Ido Schimmel <idosch@OSS.NVIDIA.COM>
2021-05-17 21:15:20 +0300

    ipv6: Use a more suitable label name
    The 'out_timer' label was added in commit 63152fc0de4d ("[NETNS][IPV6]
    ip6_fib - gc timer per namespace") when the timer was allocated on the
    heap.
    
    Commit 417f28bb3407 ("netns: dont alloc ipv6 fib timer list") removed
    the allocation, but kept the label name.
    
    Rename it to a more suitable name.
    
    Signed-off-by: Ido Schimmel <idosch@nvidia.com>
    Reviewed-by: David Ahern <dsahern@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/ip6_fib.c
+++ net/ipv6/ip6_fib.c
@@ -2362,7 +2362,7 @@ static int __net_init fib6_net_init(struct net *net)
 
 	net->ipv6.rt6_stats = kzalloc(sizeof(*net->ipv6.rt6_stats), GFP_KERNEL);
 	if (!net->ipv6.rt6_stats)
-		goto out_timer;
+		goto out_notifier;
 
 	/* Avoid false sharing : Use at least a full cache line */
 	size = max_t(size_t, size, L1_CACHE_BYTES);
@@ -2407,7 +2407,7 @@ out_fib_table_hash:
 	kfree(net->ipv6.fib_table_hash);
 out_rt6_stats:
 	kfree(net->ipv6.rt6_stats);
-out_timer:
+out_notifier:
 	fib6_notifier_exit(net);
 	return -ENOMEM;
 }
