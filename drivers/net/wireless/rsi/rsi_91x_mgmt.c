c707db1b2e7ba62e78998544a257fc68a3c4edd7
Yang Shen <shenyang39@huawei.com>
2021-05-17 13:01:39 +0800

    rsi: Fix missing function name in comments
    Fixes the following W=1 kernel build warning(s):
    
     drivers/net/wireless/rsi/rsi_91x_mgmt.c:1550: warning: This comment starts with '/**', but isn't a kernel-doc comment. Refer Documentation/doc-guide/kernel-doc.rst
    
    Cc: Amitkumar Karwar <amitkarwar@gmail.com>
    Cc: Siva Rebbagondla <siva8118@gmail.com>
    Signed-off-by: Yang Shen <shenyang39@huawei.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20210517050141.61488-10-shenyang39@huawei.com

--- drivers/net/wireless/rsi/rsi_91x_mgmt.c
+++ drivers/net/wireless/rsi/rsi_91x_mgmt.c
@@ -1547,8 +1547,8 @@ static int rsi_eeprom_read(struct rsi_common *common)
 }
 
 /**
- * This function sends a frame to block/unblock
- * data queues in the firmware
+ * rsi_send_block_unblock_frame() - This function sends a frame to block/unblock
+ *                                  data queues in the firmware
  *
  * @common: Pointer to the driver private structure.
  * @block_event: Event block if true, unblock if false
