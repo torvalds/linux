8f6368a9c92645e72fdd55862aa09821cdb81b43
John Clements <john.clements@amd.com>
2021-05-17 16:36:26 +0800

    drm/amdgpu: Conditionally reset RAS counters on boot
    Only clear RAS error counters if perestent EDC harvesting is not supported
    
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: John Clements <john.clements@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ras.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ras.h
@@ -625,4 +625,7 @@ void amdgpu_ras_set_error_query_ready(struct amdgpu_device *adev, bool ready);
 bool amdgpu_ras_need_emergency_restart(struct amdgpu_device *adev);
 
 void amdgpu_release_ras_context(struct amdgpu_device *adev);
+
+int amdgpu_persistent_edc_harvesting_supported(struct amdgpu_device *adev);
+
 #endif
