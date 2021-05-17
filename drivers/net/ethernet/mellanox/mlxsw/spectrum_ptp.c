33d89128a9602c34d2875dbf7153d9bdbccd7511
Danielle Ratson <danieller@nvidia.com>
2021-05-17 20:03:57 +0300

    mlxsw: Verify the accessed index doesn't exceed the array length
    [ Upstream commit 837ec05cfea08284c575e8e834777b107da5ff9d ]
    
    There are few cases in which an array index queried from a fw register,
    is accessed without any validation that it doesn't exceed the array
    length.
    
    Add a proper length validation, so accessing memory past the end of an
    array will be forbidden.
    
    Signed-off-by: Danielle Ratson <danieller@nvidia.com>
    Signed-off-by: Ido Schimmel <idosch@nvidia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum_ptp.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum_ptp.c
@@ -568,10 +568,13 @@ void mlxsw_sp1_ptp_got_timestamp(struct mlxsw_sp *mlxsw_sp, bool ingress,
 				 u8 domain_number, u16 sequence_id,
 				 u64 timestamp)
 {
+	unsigned int max_ports = mlxsw_core_max_ports(mlxsw_sp->core);
 	struct mlxsw_sp_port *mlxsw_sp_port;
 	struct mlxsw_sp1_ptp_key key;
 	u8 types;
 
+	if (WARN_ON_ONCE(local_port >= max_ports))
+		return;
 	mlxsw_sp_port = mlxsw_sp->ports[local_port];
 	if (!mlxsw_sp_port)
 		return;
