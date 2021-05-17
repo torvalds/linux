e2a86800d58639b3acde7eaeb9eb393dca066e08
Mike Rapoport <rppt@linux.ibm.com>
2021-05-17 21:15:15 +0300

    memblock: free_unused_memmap: use pageblock units instead of MAX_ORDER
    The code that frees unused memory map uses rounds start and end of the
    holes that are freed to MAX_ORDER_NR_PAGES to preserve continuity of the
    memory map for MAX_ORDER regions.
    
    Lots of core memory management functionality relies on homogeneity of the
    memory map within each pageblock which size may differ from MAX_ORDER in
    certain configurations.
    
    Although currently, for the architectures that use free_unused_memmap(),
    pageblock_order and MAX_ORDER are equivalent, it is cleaner to have common
    notation thought mm code.
    
    Replace MAX_ORDER_NR_PAGES with pageblock_nr_pages and update the comments
    to make it more clear why the alignment to pageblock boundaries is
    required.
    
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>
    Tested-by: Tony Lindgren <tony@atomide.com>

--- mm/memblock.c
+++ mm/memblock.c
@@ -1943,11 +1943,11 @@ static void __init free_unused_memmap(void)
 		start = min(start, ALIGN(prev_end, PAGES_PER_SECTION));
 #else
 		/*
-		 * Align down here since the VM subsystem insists that the
-		 * memmap entries are valid from the bank start aligned to
-		 * MAX_ORDER_NR_PAGES.
+		 * Align down here since many operations in VM subsystem
+		 * presume that there are no holes in the memory map inside
+		 * a pageblock
 		 */
-		start = round_down(start, MAX_ORDER_NR_PAGES);
+		start = round_down(start, pageblock_nr_pages);
 #endif
 
 		/*
@@ -1958,11 +1958,11 @@ static void __init free_unused_memmap(void)
 			free_memmap(prev_end, start);
 
 		/*
-		 * Align up here since the VM subsystem insists that the
-		 * memmap entries are valid from the bank end aligned to
-		 * MAX_ORDER_NR_PAGES.
+		 * Align up here since many operations in VM subsystem
+		 * presume that there are no holes in the memory map inside
+		 * a pageblock
 		 */
-		prev_end = ALIGN(end, MAX_ORDER_NR_PAGES);
+		prev_end = ALIGN(end, pageblock_nr_pages);
 	}
 
 #ifdef CONFIG_SPARSEMEM
