f63815eb1d909a4121806e60928108ff040bf291
Masahiro Yamada <masahiroy@kernel.org>
2021-05-17 16:38:12 +0900

    x86/unistd: Define X32_NR_syscalls only for 64-bit kernel
    X32_NR_syscalls is needed only when building a 64bit kernel.
    
    Move it to proper #ifdef guard.
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20210517073815.97426-5-masahiroy@kernel.org

--- arch/x86/include/asm/unistd.h
+++ arch/x86/include/asm/unistd.h
@@ -26,11 +26,11 @@
 #  define __ARCH_WANT_COMPAT_SYS_PWRITEV64
 #  define __ARCH_WANT_COMPAT_SYS_PREADV64V2
 #  define __ARCH_WANT_COMPAT_SYS_PWRITEV64V2
+#  define X32_NR_syscalls (__NR_x32_syscall_max + 1)
 
 # endif
 
 # define NR_syscalls (__NR_syscall_max + 1)
-# define X32_NR_syscalls (__NR_x32_syscall_max + 1)
 # define IA32_NR_syscalls (__NR_ia32_syscall_max + 1)
 
 # define __ARCH_WANT_NEW_STAT
