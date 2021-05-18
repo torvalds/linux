ecd7beb37871827c449842ec2c9db106ee5e82f6
Keoseong Park <keosung.park@samsung.com>
2021-05-18 21:12:17 +0900

    scsi: ufs: core: Clean up whitespace
    checkpatch reports the following errors:
    
    	ERROR: space prohibited before that ',' (ctx:WxW)
    	#945: FILE: drivers/scsi/ufs/ufshcd.h:945:
    	+int ufshcd_init(struct ufs_hba * , void __iomem * , unsigned int);
    	                                  ^
    
    	ERROR: space prohibited before that ',' (ctx:WxW)
    	#945: FILE: drivers/scsi/ufs/ufshcd.h:945:
    	+int ufshcd_init(struct ufs_hba * , void __iomem * , unsigned int);
    	                                                   ^
    Remove unnecessary whitespace in ufshcd.h.
    
    Link: https://lore.kernel.org/r/2038148563.21621340102306.JavaMail.epsvc@epcpadp3
    Signed-off-by: Keoseong Park <keosung.park@samsung.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -959,7 +959,7 @@ static inline void ufshcd_rmwl(struct ufs_hba *hba, u32 mask, u32 val, u32 reg)
 int ufshcd_alloc_host(struct device *, struct ufs_hba **);
 void ufshcd_dealloc_host(struct ufs_hba *);
 int ufshcd_hba_enable(struct ufs_hba *hba);
-int ufshcd_init(struct ufs_hba * , void __iomem * , unsigned int);
+int ufshcd_init(struct ufs_hba *, void __iomem *, unsigned int);
 int ufshcd_link_recovery(struct ufs_hba *hba);
 int ufshcd_make_hba_operational(struct ufs_hba *hba);
 void ufshcd_remove(struct ufs_hba *);
