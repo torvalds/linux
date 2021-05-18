4e7ce4f71d70da34bebe8c52edbc1d36dd5df799
Gokul krishna Krishnakumar <quic_gokukris@quicinc.com>
2021-05-17 18:58:25 -0700

    ANDROID: rproc: Add vendor hook for recovery
    Add vendor hook for rproc recovery to allow vendor enhancements.
    
    Bug: 215224270
    Change-Id: If6f0846c141a4ad40748d552002f65b94d9c52d5
    Signed-off-by: Siddharth Gupta <quic_sidgup@quicinc.com>
    Signed-off-by: Gokul krishna Krishnakumar <quic_gokukris@quicinc.com>
    [quic_gurus: Merge conflicts, added headers]
    Signed-off-by: Guru Das Srinagesh <quic_gurus@quicinc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -34,6 +34,7 @@
 #include <trace/hooks/creds.h>
 #include <trace/hooks/selinux.h>
 #include <trace/hooks/syscall_check.h>
+#include <trace/hooks/remoteproc.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -152,3 +153,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_post_init_entity_util_avg);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_uclamp_eff_get);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_util_est_update);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_setscheduler_uclamp);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rproc_recovery);
