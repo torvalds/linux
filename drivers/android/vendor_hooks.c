15acc8fac61185e8d511dfd485f392e4ad783cf8
Guangming Cao <Guangming.Cao@mediatek.com>
2021-05-18 15:31:28 +0800

    ANDROID: iommu: Add vendor hook for iova allocation and free tracking
    Add vendor hook for iommu_dma_alloc_iova and iommu_dma_free_iova
    to allow vendor enhancement.
    
    Bug: 187861158
    
    Change-Id: I2ebe8b24318bd5292edc66a94d7109332d478212
    Signed-off-by: Guangming Cao <Guangming.Cao@mediatek.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -175,6 +175,8 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_sea);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_mem_abort);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_sp_pc_abort);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_iommu_setup_dma_ops);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_iommu_alloc_iova);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_iommu_free_iova);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_pick_next_entity);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_check_preempt_wakeup);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cma_alloc_start);
