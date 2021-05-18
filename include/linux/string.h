6dbefad40815a61aecbcf9b552e87ef57ab8cc7d
Kees Cook <keescook@chromium.org>
2021-05-17 20:16:57 -0700

    string.h: Introduce memset_startat() for wiping trailing members and padding
    A common idiom in kernel code is to wipe the contents of a structure
    starting from a given member. These open-coded cases are usually difficult
    to read and very sensitive to struct layout changes. Like memset_after(),
    introduce a new helper, memset_startat() that takes the target struct
    instance, the byte to write, and the member name where zeroing should
    start.
    
    Note that this doesn't zero padding preceding the target member. For
    those cases, memset_after() should be used on the preceding member.
    
    Cc: Steffen Klassert <steffen.klassert@secunet.com>
    Cc: Herbert Xu <herbert@gondor.apana.org.au>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Jakub Kicinski <kuba@kernel.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Francis Laniel <laniel_francis@privacyrequired.com>
    Cc: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Cc: Daniel Axtens <dja@axtens.net>
    Cc: netdev@vger.kernel.org
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- include/linux/string.h
+++ include/linux/string.h
@@ -289,6 +289,24 @@ static inline void memcpy_and_pad(void *dest, size_t dest_len,
 })
 
 /**
+ * memset_startat - Set a value starting at a member to the end of a struct
+ *
+ * @obj: Address of target struct instance
+ * @v: Byte value to repeatedly write
+ * @member: struct member to start writing at
+ *
+ * Note that if there is padding between the prior member and the target
+ * member, memset_after() should be used to clear the prior padding.
+ */
+#define memset_startat(obj, v, member)					\
+({									\
+	u8 *__ptr = (u8 *)(obj);					\
+	typeof(v) __val = (v);						\
+	memset(__ptr + offsetof(typeof(*(obj)), member), __val,		\
+	       sizeof(*(obj)) - offsetof(typeof(*(obj)), member));	\
+})
+
+/**
  * str_has_prefix - Test if a string has a given prefix
  * @str: The string to test
  * @prefix: The string to see if @str starts with
