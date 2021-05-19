fcffbcf48df325e39e4f50c6264b39d0de60e34a
Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
2021-05-19 12:39:25 -0400

    drm/amd/display: Add DCN3.1 BIOS parser support
    Updated interfaces for multiple eDP backlight caps.
    Added support for interpreting the new BIOS command tables on DCN3.1.
    
    Acked-by: Huang Rui <ray.huang@amd.com>
    Signed-off-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/bios/command_table_helper2.c
+++ drivers/gpu/drm/amd/display/dc/bios/command_table_helper2.c
@@ -72,6 +72,11 @@ bool dal_bios_parser_init_cmd_tbl_helper2(
 	case DCN_VERSION_2_1:
 	case DCN_VERSION_3_0:
 	case DCN_VERSION_3_01:
+#if defined(CONFIG_DRM_AMD_DC_DCN3_1)
+	case DCN_VERSION_3_1:
+		*h = dal_cmd_tbl_helper_dce112_get_table2();
+		return true;
+#endif
 	case DCN_VERSION_3_02:
 	case DCN_VERSION_3_03:
 		*h = dal_cmd_tbl_helper_dce112_get_table2();
