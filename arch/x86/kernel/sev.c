c25bbdb564060adaad5c3a8a10765c13487ba6a3
Joerg Roedel <jroedel@suse.de>
2021-05-19 15:52:45 +0200

    x86/sev-es: Forward page-faults which happen during emulation
    When emulating guest instructions for MMIO or IOIO accesses, the #VC
    handler might get a page-fault and will not be able to complete. Forward
    the page-fault in this case to the correct handler instead of killing
    the machine.
    
    Fixes: 0786138c78e7 ("x86/sev-es: Add a Runtime #VC Exception Handler")
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Cc: stable@vger.kernel.org # v5.10+
    Link: https://lkml.kernel.org/r/20210519135251.30093-3-joro@8bytes.org

--- arch/x86/kernel/sev.c
+++ arch/x86/kernel/sev.c
@@ -1270,6 +1270,10 @@ static __always_inline void vc_forward_exception(struct es_em_ctxt *ctxt)
 	case X86_TRAP_UD:
 		exc_invalid_op(ctxt->regs);
 		break;
+	case X86_TRAP_PF:
+		write_cr2(ctxt->fi.cr2);
+		exc_page_fault(ctxt->regs, error_code);
+		break;
 	case X86_TRAP_AC:
 		exc_alignment_check(ctxt->regs, error_code);
 		break;
