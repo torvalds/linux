84048039d7774c363951ee6fc41c5d26f50f72fd
Vlastimil Babka <vbabka@suse.cz>
2021-05-21 01:25:06 +0200

    mm, slub: don't disable irq for debug_check_no_locks_freed()
    In slab_free_hook() we disable irqs around the debug_check_no_locks_freed()
    call, which is unnecessary, as irqs are already being disabled inside the call.
    This seems to be leftover from the past where there were more calls inside the
    irq disabled sections. Remove the irq disable/enable operations.
    
    Mel noted:
    > Looks like it was needed for kmemcheck which went away back in 4.15
    
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    Acked-by: Mel Gorman <mgorman@techsingularity.net>

--- mm/slub.c
+++ mm/slub.c
@@ -1591,20 +1591,8 @@ static __always_inline bool slab_free_hook(struct kmem_cache *s,
 {
 	kmemleak_free_recursive(x, s->flags);
 
-	/*
-	 * Trouble is that we may no longer disable interrupts in the fast path
-	 * So in order to make the debug calls that expect irqs to be
-	 * disabled we need to disable interrupts temporarily.
-	 */
-#ifdef CONFIG_LOCKDEP
-	{
-		unsigned long flags;
+	debug_check_no_locks_freed(x, s->object_size);
 
-		local_irq_save(flags);
-		debug_check_no_locks_freed(x, s->object_size);
-		local_irq_restore(flags);
-	}
-#endif
 	if (!(s->flags & SLAB_DEBUG_OBJECTS))
 		debug_check_no_obj_freed(x, s->object_size);
 
