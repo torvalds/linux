714be77e976a4b013b935b3223b2ef68856084d0
Sudeep Holla <sudeep.holla@arm.com>
2021-05-21 16:10:31 +0100

    firmware: arm_ffa: Add support for SMCCC as transport to FFA driver
    There are requests to keep the transport separate in order to allow
    other possible transports like virtio. So let us keep the SMCCC transport
    specific routines abstracted.
    
    It is kept simple for now. Once we add another transport, we can develop
    better abstraction.
    
    Link: https://lore.kernel.org/r/20210521151033.181846-4-sudeep.holla@arm.com
    Tested-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>

--- drivers/firmware/arm_ffa/common.h
+++ drivers/firmware/arm_ffa/common.h
@@ -16,9 +16,13 @@ typedef void (ffa_fn)(ffa_value_t, ffa_value_t *);
 int arm_ffa_bus_init(void);
 void arm_ffa_bus_exit(void);
 
+#ifdef CONFIG_ARM_FFA_SMCCC
+int __init ffa_transport_init(ffa_fn **invoke_ffa_fn);
+#else
 static inline int __init ffa_transport_init(ffa_fn **invoke_ffa_fn)
 {
 	return -EOPNOTSUPP;
 }
+#endif
 
 #endif /* _FFA_COMMON_H */
