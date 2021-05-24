1f42faf1d25de2ae239f322fda8af1c92c20e953
Fuad Tabba <tabba@google.com>
2021-05-24 09:29:56 +0100

    arm64: __clean_dcache_area_poc to take end parameter instead of size
    To be consistent with other functions with similar names and
    functionality in cacheflush.h, cache.S, and cachetlb.rst, change
    to specify the range in terms of start and end, as opposed to
    start and size.
    
    Because the code is shared with __dma_clean_area, it changes the
    parameters for that as well. However, __dma_clean_area is local to
    cache.S, so no other users are affected.
    
    No functional change intended.
    
    Reported-by: Will Deacon <will@kernel.org>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>
    Link: https://lore.kernel.org/r/20210524083001.2586635-14-tabba@google.com
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/cacheflush.h
+++ arch/arm64/include/asm/cacheflush.h
@@ -60,7 +60,7 @@ extern void __flush_icache_range(unsigned long start, unsigned long end);
 extern void invalidate_icache_range(unsigned long start, unsigned long end);
 extern void __flush_dcache_area(unsigned long start, unsigned long end);
 extern void __inval_dcache_area(unsigned long start, unsigned long end);
-extern void __clean_dcache_area_poc(void *addr, size_t len);
+extern void __clean_dcache_area_poc(unsigned long start, unsigned long end);
 extern void __clean_dcache_area_pop(void *addr, size_t len);
 extern void __clean_dcache_area_pou(void *addr, size_t len);
 extern long __flush_cache_user_range(unsigned long start, unsigned long end);
