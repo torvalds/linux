31283877e3cd4a48d75cffdafe05746f2cb28b15
Marc Zyngier <maz@kernel.org>
2021-05-24 18:07:52 +0100

    KVM: arm64: Prevent mixed-width VM creation
    commit 66e94d5cafd4decd4f92d16a022ea587d7f4094f upstream.
    
    It looks like we have tolerated creating mixed-width VMs since...
    forever. However, that was never the intention, and we'd rather
    not have to support that pointless complexity.
    
    Forbid such a setup by making sure all the vcpus have the same
    register width.
    
    Reported-by: Steven Price <steven.price@arm.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Cc: stable@vger.kernel.org
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Link: https://lore.kernel.org/r/20210524170752.1549797-1-maz@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/include/asm/kvm_emulate.h
+++ arch/arm64/include/asm/kvm_emulate.h
@@ -505,4 +505,9 @@ static __always_inline void __kvm_skip_instr(struct kvm_vcpu *vcpu)
 	write_sysreg_el2(*vcpu_pc(vcpu), SYS_ELR);
 }
 
+static inline bool vcpu_has_feature(struct kvm_vcpu *vcpu, int feature)
+{
+	return test_bit(feature, vcpu->arch.features);
+}
+
 #endif /* __ARM64_KVM_EMULATE_H__ */
