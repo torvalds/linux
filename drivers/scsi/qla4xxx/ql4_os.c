891e2639deae721dc43764a44fa255890dc34313
Mike Christie <michael.christie@oracle.com>
2021-05-25 13:17:55 -0500

    scsi: iscsi: Stop queueing during ep_disconnect
    During ep_disconnect we have been doing iscsi_suspend_tx/queue to block new
    I/O but every driver except cxgbi and iscsi_tcp can still get I/O from
    __iscsi_conn_send_pdu() if we haven't called iscsi_conn_failure() before
    ep_disconnect. This could happen if we were terminating the session, and
    the logout timed out before it was even sent to libiscsi.
    
    Fix the issue by adding a helper which reverses the bind_conn call that
    allows new I/O to be queued. Drivers implementing ep_disconnect can use this
    to make sure new I/O is not queued to them when handling the disconnect.
    
    Link: https://lore.kernel.org/r/20210525181821.7617-3-michael.christie@oracle.com
    Reviewed-by: Lee Duncan <lduncan@suse.com>
    Signed-off-by: Mike Christie <michael.christie@oracle.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/qla4xxx/ql4_os.c
+++ drivers/scsi/qla4xxx/ql4_os.c
@@ -259,6 +259,7 @@ static struct iscsi_transport qla4xxx_iscsi_transport = {
 	.start_conn             = qla4xxx_conn_start,
 	.create_conn            = qla4xxx_conn_create,
 	.bind_conn              = qla4xxx_conn_bind,
+	.unbind_conn		= iscsi_conn_unbind,
 	.stop_conn              = iscsi_conn_stop,
 	.destroy_conn           = qla4xxx_conn_destroy,
 	.set_param              = iscsi_set_param,
