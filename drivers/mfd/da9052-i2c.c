8b201402ea027e44fbe66933a4912b647fd35e89
Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
2021-05-26 08:47:01 -0400

    mfd: da9052: Simplify getting of_device_id match data
    Use of_device_get_match_data() to make the code slightly smaller.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
    Acked-by: Adam Thomson <Adam.Thomson.Opensource@diasemi.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/da9052-i2c.c
+++ drivers/mfd/da9052-i2c.c
@@ -155,13 +155,8 @@ static int da9052_i2c_probe(struct i2c_client *client,
 		return ret;
 
 #ifdef CONFIG_OF
-	if (!id) {
-		struct device_node *np = client->dev.of_node;
-		const struct of_device_id *deviceid;
-
-		deviceid = of_match_node(dialog_dt_ids, np);
-		id = deviceid->data;
-	}
+	if (!id)
+		id = of_device_get_match_data(&client->dev);
 #endif
 
 	if (!id) {
