fac61e6931d18929d9e69f4e0a48f5529c21f2e7
Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
2021-05-26 08:47:00 -0400

    mfd: max8998: Simplify getting of_device_id match data
    Use of_device_get_match_data() to make the code slightly smaller.  There
    is also no need to wrap it in a check for CONFIG_OF, because dev.of_node
    will be set only with OF support.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/max8998.c
+++ drivers/mfd/max8998.c
@@ -12,6 +12,7 @@
 #include <linux/i2c.h>
 #include <linux/interrupt.h>
 #include <linux/of.h>
+#include <linux/of_device.h>
 #include <linux/of_irq.h>
 #include <linux/pm_runtime.h>
 #include <linux/mutex.h>
@@ -155,11 +156,8 @@ static struct max8998_platform_data *max8998_i2c_parse_dt_pdata(
 static inline unsigned long max8998_i2c_get_driver_data(struct i2c_client *i2c,
 						const struct i2c_device_id *id)
 {
-	if (IS_ENABLED(CONFIG_OF) && i2c->dev.of_node) {
-		const struct of_device_id *match;
-		match = of_match_node(max8998_dt_match, i2c->dev.of_node);
-		return (unsigned long)match->data;
-	}
+	if (i2c->dev.of_node)
+		return (unsigned long)of_device_get_match_data(&i2c->dev);
 
 	return id->driver_data;
 }
