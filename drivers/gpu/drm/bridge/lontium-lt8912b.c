2986d2763daaaa9280ac499ea17e7031a81ac0b7
Zou Wei <zou_wei@huawei.com>
2021-05-27 17:50:18 +0800

    drm/bridge: lt8912b: fix platform_no_drv_owner.cocci warnings
    ./drivers/gpu/drm/bridge/lontium-lt8912b.c:758:3-8: No need to set .owner here. The core will do it.
    
     Remove .owner field if calls are used which set it automatically
    
    Generated by: scripts/coccinelle/api/platform_no_drv_owner.cocci
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Zou Wei <zou_wei@huawei.com>
    Reviewed-by: Robert Foss <robert.foss@linaro.org>
    Signed-off-by: Robert Foss <robert.foss@linaro.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/1622109018-54648-1-git-send-email-zou_wei@huawei.com

--- drivers/gpu/drm/bridge/lontium-lt8912b.c
+++ drivers/gpu/drm/bridge/lontium-lt8912b.c
@@ -755,7 +755,6 @@ static struct i2c_driver lt8912_i2c_driver = {
 	.driver = {
 		.name = "lt8912",
 		.of_match_table = lt8912_dt_match,
-		.owner = THIS_MODULE,
 	},
 	.probe = lt8912_probe,
 	.remove = lt8912_remove,
