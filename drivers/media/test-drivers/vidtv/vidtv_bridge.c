2bd28fe587a5c8267b7030e33c9bcc7776130a36
Evgeny Novikov <novikov@ispras.ru>
2021-05-27 11:26:24 +0200

    media: vidtv: Fix memory leak in remove
    [ Upstream commit 76e21bb8be4f5f987f3006d197196fe6af63f656 ]
    
    vidtv_bridge_remove() releases and cleans up everything except for dvb
    itself. The patch adds this missed release.
    
    Found by Linux Driver Verification project (linuxtesting.org).
    
    Signed-off-by: Evgeny Novikov <novikov@ispras.ru>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/media/test-drivers/vidtv/vidtv_bridge.c
+++ drivers/media/test-drivers/vidtv/vidtv_bridge.c
@@ -557,6 +557,7 @@ static int vidtv_bridge_remove(struct platform_device *pdev)
 	dvb_dmxdev_release(&dvb->dmx_dev);
 	dvb_dmx_release(&dvb->demux);
 	dvb_unregister_adapter(&dvb->adapter);
+	kfree(dvb);
 	dev_info(&pdev->dev, "Successfully removed vidtv\n");
 
 	return 0;
