4f6a0f31c62764ad57545d86fca8499afe42555b
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-05-27 18:59:59 +0300

    net: stmmac: the XPCS obscures a potential "PHY not found" error
    [ Upstream commit 4751d2aa321f2828d8c5d2f7ce4ed18a01e47f46 ]
    
    stmmac_mdio_register() has logic to search for PHYs on the MDIO bus and
    assign them IRQ lines, as well as to set priv->plat->phy_addr.
    
    If no PHY is found, the "found" variable remains set to 0 and the
    function errors out.
    
    After the introduction of commit f213bbe8a9d6 ("net: stmmac: Integrate
    it with DesignWare XPCS"), the "found" variable was immediately reused
    for searching for a PCS on the same MDIO bus.
    
    This can result in 2 types of potential problems (none of them seems to
    be seen on the only Intel system that sets has_xpcs = true, otherwise it
    would have been reported):
    
    1. If a PCS is found but a PHY is not, then the code happily exits with
       no error. One might say "yes, but this is not possible, because
       of_mdiobus_register will probe a PHY for all MDIO addresses,
       including for the XPCS, so if an XPCS exists, then a PHY certainly
       exists too". Well, that is not true, see intel_mgbe_common_data():
    
    	/* Ensure mdio bus scan skips intel serdes and pcs-xpcs */
    	plat->mdio_bus_data->phy_mask = 1 << INTEL_MGBE_ADHOC_ADDR;
    	plat->mdio_bus_data->phy_mask |= 1 << INTEL_MGBE_XPCS_ADDR;
    
    2. A PHY is found but an MDIO device with the XPCS PHY ID isn't, and in
       that case, the error message will be "No PHY found". Confusing.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Link: https://lore.kernel.org/r/20210527155959.3270478-1-olteanv@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_mdio.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_mdio.c
@@ -441,6 +441,12 @@ int stmmac_mdio_register(struct net_device *ndev)
 		found = 1;
 	}
 
+	if (!found && !mdio_node) {
+		dev_warn(dev, "No PHY found\n");
+		err = -ENODEV;
+		goto no_phy_found;
+	}
+
 	/* Try to probe the XPCS by scanning all addresses. */
 	if (priv->hw->xpcs) {
 		struct mdio_xpcs_args *xpcs = &priv->hw->xpcs_args;
@@ -449,6 +455,7 @@ int stmmac_mdio_register(struct net_device *ndev)
 
 		xpcs->bus = new_bus;
 
+		found = 0;
 		for (addr = 0; addr < max_addr; addr++) {
 			xpcs->addr = addr;
 
@@ -458,13 +465,12 @@ int stmmac_mdio_register(struct net_device *ndev)
 				break;
 			}
 		}
-	}
 
-	if (!found && !mdio_node) {
-		dev_warn(dev, "No PHY found\n");
-		mdiobus_unregister(new_bus);
-		mdiobus_free(new_bus);
-		return -ENODEV;
+		if (!found && !mdio_node) {
+			dev_warn(dev, "No XPCS found\n");
+			err = -ENODEV;
+			goto no_xpcs_found;
+		}
 	}
 
 bus_register_done:
@@ -472,6 +478,9 @@ bus_register_done:
 
 	return 0;
 
+no_xpcs_found:
+no_phy_found:
+	mdiobus_unregister(new_bus);
 bus_register_fail:
 	mdiobus_free(new_bus);
 	return err;
