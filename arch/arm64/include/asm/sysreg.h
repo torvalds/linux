21cfe6edbadb703b674ae2ddf78862d00d24bfc5
Will Deacon <will@kernel.org>
2021-05-27 12:03:19 +0100

    arm64: mm: Remove unused support for Normal-WT memory type
    The Normal-WT memory type is unused, so remove it and reclaim a MAIR.
    
    Cc: Christoph Hellwig <hch@lst.de>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/20210527110319.22157-4-will@kernel.org
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/sysreg.h
+++ arch/arm64/include/asm/sysreg.h
@@ -704,7 +704,6 @@
 #define MAIR_ATTR_DEVICE_nGnRnE		UL(0x00)
 #define MAIR_ATTR_DEVICE_nGnRE		UL(0x04)
 #define MAIR_ATTR_NORMAL_NC		UL(0x44)
-#define MAIR_ATTR_NORMAL_WT		UL(0xbb)
 #define MAIR_ATTR_NORMAL_TAGGED		UL(0xf0)
 #define MAIR_ATTR_NORMAL		UL(0xff)
 #define MAIR_ATTR_MASK			UL(0xff)
