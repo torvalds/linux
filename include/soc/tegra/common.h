4333e0300023c701d4c7bf0b834179ca19d4ddf8
Dmitry Osipenko <digetx@gmail.com>
2021-05-28 02:54:02 +0300

    soc/tegra: Add stub for soc_is_tegra()
    Add stub required for compile-testing of drivers.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- include/soc/tegra/common.h
+++ include/soc/tegra/common.h
@@ -6,6 +6,15 @@
 #ifndef __SOC_TEGRA_COMMON_H__
 #define __SOC_TEGRA_COMMON_H__
 
+#include <linux/types.h>
+
+#ifdef CONFIG_ARCH_TEGRA
 bool soc_is_tegra(void);
+#else
+static inline bool soc_is_tegra(void)
+{
+	return false;
+}
+#endif
 
 #endif /* __SOC_TEGRA_COMMON_H__ */
