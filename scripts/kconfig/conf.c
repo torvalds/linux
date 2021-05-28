43ac711053fc6d94a3f16141c4efe20059a9d918
Masahiro Yamada <masahiroy@kernel.org>
2021-05-29 02:13:21 +0900

    kconfig: constify long_opts
    getopt_long() does not modify the long_opts structure.
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>

--- scripts/kconfig/conf.c
+++ scripts/kconfig/conf.c
@@ -678,7 +678,7 @@ static void check_conf(struct menu *menu)
 		check_conf(child);
 }
 
-static struct option long_opts[] = {
+static const struct option long_opts[] = {
 	{"help",          no_argument,       NULL,            'h'},
 	{"silent",        no_argument,       NULL,            's'},
 	{"oldaskconfig",  no_argument,       &input_mode_opt, oldaskconfig},
