673b9ba7a1404fa5beda936b8ad509b70a516b52
Yang Yingliang <yangyingliang@huawei.com>
2021-05-29 16:20:57 +0800

    cifsd: fix memleak in ksmbd_vfs_stream_read()
    Before ksmbd_vfs_stream_read() return, memory allocate in
    ksmbd_vfs_getcasexattr() need be freed.
    
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Namjae Jeon <namjae.jeon@samsung.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/cifsd/vfs.c
+++ fs/cifsd/vfs.c
@@ -290,6 +290,7 @@ static int ksmbd_vfs_stream_read(struct ksmbd_file *fp, char *buf, loff_t *pos,
 	}
 
 	memcpy(buf, &stream_buf[*pos], count);
+	kvfree(stream_buf);
 	return v_len > count ? count : v_len;
 }
 
