e83a26473a7bc79498c486a542a7b933b2994466
Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
2021-05-31 09:39:00 +0200

    nfc: st21nfca: drop ftrace-like debugging messages
    Now that the kernel has ftrace, any debugging calls that just do "made
    it to this function!" and "leaving this function!" can be removed.
    Better to use standard debugging tools.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
    Link: https://lore.kernel.org/r/20210531073902.7111-5-krzysztof.kozlowski@canonical.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/nfc/st21nfca/i2c.c
+++ drivers/nfc/st21nfca/i2c.c
@@ -502,9 +502,6 @@ static int st21nfca_hci_i2c_probe(struct i2c_client *client,
 	struct st21nfca_i2c_phy *phy;
 	int r;
 
-	dev_dbg(&client->dev, "%s\n", __func__);
-	dev_dbg(&client->dev, "IRQ: %d\n", client->irq);
-
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C)) {
 		nfc_err(&client->dev, "Need I2C_FUNC_I2C\n");
 		return -ENODEV;
@@ -567,8 +564,6 @@ static int st21nfca_hci_i2c_probe(struct i2c_client *client,
 static int st21nfca_hci_i2c_remove(struct i2c_client *client)
 {
 	struct st21nfca_i2c_phy *phy = i2c_get_clientdata(client);
-
-	dev_dbg(&client->dev, "%s\n", __func__);
 
 	st21nfca_hci_remove(phy->hdev);
 
