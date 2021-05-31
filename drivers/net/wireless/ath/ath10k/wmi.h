a8b1de7f4f68d558fb952e765aa25e90b50a2388
Shaokun Zhang <zhangshaokun@hisilicon.com>
2021-05-31 16:07:39 +0800

    ath10k: remove the repeated declaration
    Functions 'ath10k_pci_free_pipes' and 'ath10k_wmi_alloc_skb'
    are declared twice in their header file, so remove the repeated
    declaration.
    
    Cc: Kalle Valo <kvalo@codeaurora.org>
    Signed-off-by: Shaokun Zhang <zhangshaokun@hisilicon.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/1622448459-50805-1-git-send-email-zhangshaokun@hisilicon.com

--- drivers/net/wireless/ath/ath10k/wmi.h
+++ drivers/net/wireless/ath/ath10k/wmi.h
@@ -7418,7 +7418,6 @@ int ath10k_wmi_wait_for_unified_ready(struct ath10k *ar);
 struct sk_buff *ath10k_wmi_alloc_skb(struct ath10k *ar, u32 len);
 int ath10k_wmi_connect(struct ath10k *ar);
 
-struct sk_buff *ath10k_wmi_alloc_skb(struct ath10k *ar, u32 len);
 int ath10k_wmi_cmd_send(struct ath10k *ar, struct sk_buff *skb, u32 cmd_id);
 int ath10k_wmi_cmd_send_nowait(struct ath10k *ar, struct sk_buff *skb,
 			       u32 cmd_id);
