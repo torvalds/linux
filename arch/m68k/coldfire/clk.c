63aadb77669a6856b26d73da85b4f788731524a3
Arnd Bergmann <arnd@arndb.de>
2021-05-31 11:12:55 +0200

    m68k: coldfire: use clkdev_lookup on most coldfire
    Coldfire is now the only target that implements the clk_get()/clk_put()
    helpers itself rather than using the common implementation.
    
    Most coldfire variants only have two distinct clocks and use the clk
    code purely for lookup. Change those over to use clkdev_lookup instead
    but leave the custom clk interface for those two clocks.
    
    Also leave the four SoCs that have gated clocks.
    
    Acked-by: Greg Ungerer <gerg@linux-m68k.org>
    Tested-by: Greg Ungerer <gerg@linux-m68k.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/m68k/coldfire/clk.c
+++ arch/m68k/coldfire/clk.c
@@ -71,7 +71,6 @@ struct clk_ops clk_ops1 = {
 	.disable	= __clk_disable1,
 };
 #endif /* MCFPM_PPMCR1 */
-#endif /* MCFPM_PPMCR0 */
 
 struct clk *clk_get(struct device *dev, const char *id)
 {
@@ -86,6 +85,14 @@ struct clk *clk_get(struct device *dev, const char *id)
 	return ERR_PTR(-ENOENT);
 }
 EXPORT_SYMBOL(clk_get);
+
+void clk_put(struct clk *clk)
+{
+	if (clk->enabled != 0)
+		pr_warn("clk_put %s still enabled\n", clk->name);
+}
+EXPORT_SYMBOL(clk_put);
+#endif /* MCFPM_PPMCR0 */
 
 int clk_enable(struct clk *clk)
 {
@@ -116,13 +123,6 @@ void clk_disable(struct clk *clk)
 	spin_unlock_irqrestore(&clk_lock, flags);
 }
 EXPORT_SYMBOL(clk_disable);
-
-void clk_put(struct clk *clk)
-{
-	if (clk->enabled != 0)
-		pr_warn("clk_put %s still enabled\n", clk->name);
-}
-EXPORT_SYMBOL(clk_put);
 
 unsigned long clk_get_rate(struct clk *clk)
 {
