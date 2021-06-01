7ea7a1e05c7ff5ffc9f9ec1f0849f6ceb7fcd57c
Tong Tiangen <tongtiangen@huawei.com>
2021-06-01 18:01:28 +0800

    brcmfmac: Fix a double-free in brcmf_sdio_bus_reset
    brcmf_sdiod_remove has been called inside brcmf_sdiod_probe when fails,
    so there's no need to call another one. Otherwise, sdiodev->freezer
    would be double freed.
    
    Fixes: 7836102a750a ("brcmfmac: reset SDIO bus on a firmware crash")
    Signed-off-by: Tong Tiangen <tongtiangen@huawei.com>
    Reviewed-by: Arend van Spriel <arend.vanspriel@broadcom.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20210601100128.69561-1-tongtiangen@huawei.com

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
@@ -4162,7 +4162,6 @@ static int brcmf_sdio_bus_reset(struct device *dev)
 	if (ret) {
 		brcmf_err("Failed to probe after sdio device reset: ret %d\n",
 			  ret);
-		brcmf_sdiod_remove(sdiodev);
 	}
 
 	return ret;
