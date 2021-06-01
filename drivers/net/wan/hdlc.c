30cd458be244a929e929512fa63bbcf45225e752
Peng Li <lipeng321@huawei.com>
2021-06-01 21:23:16 +0800

    net: hdlc: remove redundant blank lines
    This patch removes some redundant blank lines.
    
    Signed-off-by: Peng Li <lipeng321@huawei.com>
    Signed-off-by: Guangbin Huang <huangguangbin2@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/wan/hdlc.c
+++ drivers/net/wan/hdlc.c
@@ -36,7 +36,6 @@
 #include <linux/slab.h>
 #include <net/net_namespace.h>
 
-
 static const char* version = "HDLC support module revision 1.22";
 
 #undef DEBUG_LINK
@@ -82,16 +81,12 @@ static inline void hdlc_proto_start(struct net_device *dev)
 		hdlc->proto->start(dev);
 }
 
-
-
 static inline void hdlc_proto_stop(struct net_device *dev)
 {
 	hdlc_device *hdlc = dev_to_hdlc(dev);
 	if (hdlc->proto->stop)
 		hdlc->proto->stop(dev);
 }
-
-
 
 static int hdlc_device_event(struct notifier_block *this, unsigned long event,
 			     void *ptr)
@@ -141,8 +136,6 @@ carrier_exit:
 	return NOTIFY_DONE;
 }
 
-
-
 /* Must be called by hardware driver when HDLC device is being opened */
 int hdlc_open(struct net_device *dev)
 {
@@ -175,8 +168,6 @@ int hdlc_open(struct net_device *dev)
 	return 0;
 }
 
-
-
 /* Must be called by hardware driver when HDLC device is being closed */
 void hdlc_close(struct net_device *dev)
 {
@@ -197,8 +188,6 @@ void hdlc_close(struct net_device *dev)
 	if (hdlc->proto->close)
 		hdlc->proto->close(dev);
 }
-
-
 
 int hdlc_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 {
@@ -271,8 +260,6 @@ void unregister_hdlc_device(struct net_device *dev)
 	rtnl_unlock();
 }
 
-
-
 int attach_hdlc_protocol(struct net_device *dev, struct hdlc_proto *proto,
 			 size_t size)
 {
@@ -296,7 +283,6 @@ int attach_hdlc_protocol(struct net_device *dev, struct hdlc_proto *proto,
 
 	return 0;
 }
-
 
 int detach_hdlc_protocol(struct net_device *dev)
 {
@@ -323,7 +309,6 @@ int detach_hdlc_protocol(struct net_device *dev)
 	return 0;
 }
 
-
 void register_hdlc_protocol(struct hdlc_proto *proto)
 {
 	rtnl_lock();
@@ -331,7 +316,6 @@ void register_hdlc_protocol(struct hdlc_proto *proto)
 	first_proto = proto;
 	rtnl_unlock();
 }
-
 
 void unregister_hdlc_protocol(struct hdlc_proto *proto)
 {
@@ -346,8 +330,6 @@ void unregister_hdlc_protocol(struct hdlc_proto *proto)
 	*p = proto->next;
 	rtnl_unlock();
 }
-
-
 
 MODULE_AUTHOR("Krzysztof Halasa <khc@pm.waw.pl>");
 MODULE_DESCRIPTION("HDLC support module");
@@ -369,11 +351,9 @@ static struct packet_type hdlc_packet_type __read_mostly = {
 	.func = hdlc_rcv,
 };
 
-
 static struct notifier_block hdlc_notifier = {
 	.notifier_call = hdlc_device_event,
 };
-
 
 static int __init hdlc_module_init(void)
 {
@@ -386,14 +366,11 @@ static int __init hdlc_module_init(void)
 	return 0;
 }
 
-
-
 static void __exit hdlc_module_exit(void)
 {
 	dev_remove_pack(&hdlc_packet_type);
 	unregister_netdevice_notifier(&hdlc_notifier);
 }
-
 
 module_init(hdlc_module_init);
 module_exit(hdlc_module_exit);
