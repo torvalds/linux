92638b4e1b47f97d7269e74465dedf73096f777d
Peter Collingbourne <pcc@google.com>
2021-06-02 16:52:27 -0700

    mm: arch: remove indirection level in alloc_zeroed_user_highpage_movable()
    In an upcoming change we would like to add a flag to
    GFP_HIGHUSER_MOVABLE so that it would no longer be an OR
    of GFP_HIGHUSER and __GFP_MOVABLE. This poses a problem for
    alloc_zeroed_user_highpage_movable() which passes __GFP_MOVABLE
    into an arch-specific __alloc_zeroed_user_highpage() hook which ORs
    in GFP_HIGHUSER.
    
    Since __alloc_zeroed_user_highpage() is only ever called from
    alloc_zeroed_user_highpage_movable(), we can remove one level
    of indirection here. Remove __alloc_zeroed_user_highpage(),
    make alloc_zeroed_user_highpage_movable() the hook, and use
    GFP_HIGHUSER_MOVABLE in the hook implementations so that they will
    pick up the new flag that we are going to add.
    
    Signed-off-by: Peter Collingbourne <pcc@google.com>
    Link: https://linux-review.googlesource.com/id/Ic6361c657b2cdcd896adbe0cf7cb5a7fbb1ed7bf
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/20210602235230.3928842-2-pcc@google.com
    Signed-off-by: Will Deacon <will@kernel.org>

--- include/linux/highmem.h
+++ include/linux/highmem.h
@@ -152,36 +152,7 @@ static inline void clear_user_highpage(struct page *page, unsigned long vaddr)
 }
 #endif
 
-#ifndef __HAVE_ARCH_ALLOC_ZEROED_USER_HIGHPAGE
-/**
- * __alloc_zeroed_user_highpage - Allocate a zeroed HIGHMEM page for a VMA with caller-specified movable GFP flags
- * @movableflags: The GFP flags related to the pages future ability to move like __GFP_MOVABLE
- * @vma: The VMA the page is to be allocated for
- * @vaddr: The virtual address the page will be inserted into
- *
- * This function will allocate a page for a VMA but the caller is expected
- * to specify via movableflags whether the page will be movable in the
- * future or not
- *
- * An architecture may override this function by defining
- * __HAVE_ARCH_ALLOC_ZEROED_USER_HIGHPAGE and providing their own
- * implementation.
- */
-static inline struct page *
-__alloc_zeroed_user_highpage(gfp_t movableflags,
-			struct vm_area_struct *vma,
-			unsigned long vaddr)
-{
-	struct page *page = alloc_page_vma(GFP_HIGHUSER | movableflags,
-			vma, vaddr);
-
-	if (page)
-		clear_user_highpage(page, vaddr);
-
-	return page;
-}
-#endif
-
+#ifndef __HAVE_ARCH_ALLOC_ZEROED_USER_HIGHPAGE_MOVABLE
 /**
  * alloc_zeroed_user_highpage_movable - Allocate a zeroed HIGHMEM page for a VMA that the caller knows can move
  * @vma: The VMA the page is to be allocated for
@@ -189,13 +160,23 @@ __alloc_zeroed_user_highpage(gfp_t movableflags,
  *
  * This function will allocate a page for a VMA that the caller knows will
  * be able to migrate in the future using move_pages() or reclaimed
+ *
+ * An architecture may override this function by defining
+ * __HAVE_ARCH_ALLOC_ZEROED_USER_HIGHPAGE_MOVABLE and providing their own
+ * implementation.
  */
 static inline struct page *
 alloc_zeroed_user_highpage_movable(struct vm_area_struct *vma,
-					unsigned long vaddr)
+				   unsigned long vaddr)
 {
-	return __alloc_zeroed_user_highpage(__GFP_MOVABLE, vma, vaddr);
+	struct page *page = alloc_page_vma(GFP_HIGHUSER_MOVABLE, vma, vaddr);
+
+	if (page)
+		clear_user_highpage(page, vaddr);
+
+	return page;
 }
+#endif
 
 static inline void clear_highpage(struct page *page)
 {
