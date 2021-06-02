3e5feb11a82945c88a1518fd2682ca8de66c37d3
Yang Yingliang <yangyingliang@huawei.com>
2021-06-02 16:55:44 +0800

    i3c: master: svc: fix doc warning in svc-i3c-master.c
    Fix the following make W=1 warning:
    
      drivers/i3c/master/svc-i3c-master.c:207: warning: expecting prototype for struct svc_i3c_i3c_dev_data. Prototype was for struct svc_i3c_i2c_dev_data instead
    
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Reviewed-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Link: https://lore.kernel.org/r/20210602085544.4101980-1-yangyingliang@huawei.com

--- drivers/i3c/master/svc-i3c-master.c
+++ drivers/i3c/master/svc-i3c-master.c
@@ -195,7 +195,7 @@ struct svc_i3c_master {
 };
 
 /**
- * struct svc_i3c_i3c_dev_data - Device specific data
+ * struct svc_i3c_i2c_dev_data - Device specific data
  * @index: Index in the master tables corresponding to this device
  * @ibi: IBI slot index in the master structure
  * @ibi_pool: IBI pool associated to this device
