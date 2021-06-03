e2bebb924d6f4cef14133ed0b198e6efec4a7aec
Manasi Navare <manasi.d.navare@intel.com>
2021-06-03 14:53:38 -0700

    drm/i915: Initialize the mbus_offset to fix Klockwork issue
    Static analysis identified an issue in skl_crtc_allocate_ddb where
    mbus_offset may be used uninitialized.
    This patch fixes it.
    
    Fixes: 835c176cb1c4 ("drm/i915: Introduce MBUS relative dbuf offsets")
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Manasi Navare <manasi.d.navare@intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210603215338.13804-1-manasi.d.navare@intel.com

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -4163,7 +4163,7 @@ skl_crtc_allocate_ddb(struct intel_atomic_state *state, struct intel_crtc *crtc)
 	struct intel_crtc_state *crtc_state;
 	struct skl_ddb_entry ddb_slices;
 	enum pipe pipe = crtc->pipe;
-	unsigned int mbus_offset;
+	unsigned int mbus_offset = 0;
 	u32 ddb_range_size;
 	u32 dbuf_slice_mask;
 	u32 start, end;
