8959a239242754054e70391c05c06d8c4eb2bc77
Al Viro <viro@zeniv.linux.org.uk>
2021-06-03 10:34:55 -0400

    fuse_fill_write_pages(): don't bother with iov_iter_single_seg_count()
    another rudiment of fault-in originally having been limited to the
    first segment, same as in generic_perform_write() and friends.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1178,7 +1178,6 @@ static ssize_t fuse_fill_write_pages(struct fuse_io_args *ia,
 		if (!tmp) {
 			unlock_page(page);
 			put_page(page);
-			bytes = min(bytes, iov_iter_single_seg_count(ii));
 			goto again;
 		}
 
