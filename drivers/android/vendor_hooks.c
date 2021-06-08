3567f516024d93b7e353408d2b1e92b219caf8d2
Maulik Shah <mkshah@codeaurora.org>
2021-06-08 14:33:31 +0530

    ANDROID: cpuidle-psci: Add vendor hook for cpuidle psci enter and exit
    Add vendor hook for cpuidle to allow vendor modules for s2idle
    enhancements.
    
    Bug: 190353898
    Bug: 195914333
    Change-Id: I02b1393e6119e32eae96010ba5951405dbdada2b
    Signed-off-by: Maulik Shah <mkshah@codeaurora.org>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -35,6 +35,7 @@
 #include <trace/hooks/net.h>
 #include <trace/hooks/timer.h>
 #include <trace/hooks/pm_domain.h>
+#include <trace/hooks/cpuidle_psci.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -133,3 +134,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ptype_head);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_kfree_skb);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_timer_calc_index);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_allow_domain_state);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpuidle_psci_enter);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpuidle_psci_exit);
