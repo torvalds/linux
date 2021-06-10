0adf32c033a5ff3273295655a3e7cb3f698cbb9b
Paolo Abeni <pabeni@redhat.com>
2021-06-10 15:59:43 -0700

    selftests: mptcp: enable syncookie only in absence of reorders
    [ Upstream commit 2395da0e17935ce9158cdfae433962bdb6cbfa67 ]
    
    Syncookie validation may fail for OoO packets, causing spurious
    resets and self-tests failures, so let's force syncookie only
    for tests iteration with no OoO.
    
    Fixes: fed61c4b584c ("selftests: mptcp: make 2nd net namespace use tcp syn cookies unconditionally")
    Closes: https://github.com/multipath-tcp/mptcp_net-next/issues/198
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/testing/selftests/net/mptcp/mptcp_connect.sh
+++ tools/testing/selftests/net/mptcp/mptcp_connect.sh
@@ -195,9 +195,6 @@ ip -net "$ns4" link set ns4eth3 up
 ip -net "$ns4" route add default via 10.0.3.2
 ip -net "$ns4" route add default via dead:beef:3::2
 
-# use TCP syn cookies, even if no flooding was detected.
-ip netns exec "$ns2" sysctl -q net.ipv4.tcp_syncookies=2
-
 set_ethtool_flags() {
 	local ns="$1"
 	local dev="$2"
@@ -664,6 +661,14 @@ for sender in $ns1 $ns2 $ns3 $ns4;do
 	if [ $ret -ne 0 ] ;then
 		echo "FAIL: Could not even run loopback v6 test" 2>&1
 		exit $ret
+	fi
+
+	# ns1<->ns2 is not subject to reordering/tc delays. Use it to test
+	# mptcp syncookie support.
+	if [ $sender = $ns1 ]; then
+		ip netns exec "$ns2" sysctl -q net.ipv4.tcp_syncookies=2
+	else
+		ip netns exec "$ns2" sysctl -q net.ipv4.tcp_syncookies=1
 	fi
 
 	run_tests "$ns2" $sender 10.0.1.2
