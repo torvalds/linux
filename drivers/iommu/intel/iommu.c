521f546b4e4cedfbfbb5787f940a592dd20dd1f2
Lu Baolu <baolu.lu@linux.intel.com>
2021-06-10 10:00:57 +0800

    iommu/vt-d: Support asynchronous IOMMU nested capabilities
    Current VT-d implementation supports nested translation only if all
    underlying IOMMUs support the nested capability. This is unnecessary
    as the upper layer is allowed to create different containers and set
    them with different type of iommu backend. The IOMMU driver needs to
    guarantee that devices attached to a nested mode iommu_domain should
    support nested capabilility.
    
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Link: https://lore.kernel.org/r/20210517065701.5078-1-baolu.lu@linux.intel.com
    Link: https://lore.kernel.org/r/20210610020115.1637656-6-baolu.lu@linux.intel.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -4757,6 +4757,13 @@ static int prepare_domain_attach_device(struct iommu_domain *domain,
 	if (!iommu)
 		return -ENODEV;
 
+	if ((dmar_domain->flags & DOMAIN_FLAG_NESTING_MODE) &&
+	    !ecap_nest(iommu->ecap)) {
+		dev_err(dev, "%s: iommu not support nested translation\n",
+			iommu->name);
+		return -EINVAL;
+	}
+
 	/* check if this iommu agaw is sufficient for max mapped address */
 	addr_width = agaw_to_width(iommu->agaw);
 	if (addr_width > cap_mgaw(iommu->cap))
@@ -5457,7 +5464,7 @@ intel_iommu_enable_nesting(struct iommu_domain *domain)
 	int ret = -ENODEV;
 
 	spin_lock_irqsave(&device_domain_lock, flags);
-	if (nested_mode_support() && list_empty(&dmar_domain->devices)) {
+	if (list_empty(&dmar_domain->devices)) {
 		dmar_domain->flags |= DOMAIN_FLAG_NESTING_MODE;
 		dmar_domain->flags &= ~DOMAIN_FLAG_USE_FIRST_LEVEL;
 		ret = 0;
