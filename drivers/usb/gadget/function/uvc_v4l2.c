778353e25745bba4590d25c49f4340b80d938d11
Mark Huang <huangjc@rock-chips.com>
2021-06-10 08:53:29 +0800

    usb: gadget: uvc: support nv12 format
    Change-Id: Iafa6bdb1540ef9f3ac766bf25e917a76069f942b
    Signed-off-by: Mark Huang <huangjc@rock-chips.com>

--- drivers/usb/gadget/function/uvc_v4l2.c
+++ drivers/usb/gadget/function/uvc_v4l2.c
@@ -58,6 +58,7 @@ struct uvc_format {
 
 static struct uvc_format uvc_formats[] = {
 	{ 16, V4L2_PIX_FMT_YUYV  },
+	{ 12, V4L2_PIX_FMT_NV12  },
 	{ 0,  V4L2_PIX_FMT_MJPEG },
 	{ 0,  V4L2_PIX_FMT_H264  },
 	{ 0,  V4L2_PIX_FMT_H265  },
