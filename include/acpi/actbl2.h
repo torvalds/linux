9f8c7baedabc9693fbd7890f8fda40578bde4f73
Erik Kaneda <erik.kaneda@intel.com>
2021-06-09 20:41:51 -0700

    ACPICA: Add PRMT module header to facilitate parsing
    ACPICA commit bd46cb07e614fd85ea69e54c1f6f0ae0a5fb20ab
    
    This structure is used in to parse PRMT in other Operating Systems
    that relies on using subtable headers in order to parse ACPI tables.
    Although the PRMT doesn't have "subtables" it has a list of module
    information structures that act as subtables.
    
    Link: https://github.com/acpica/acpica/commit/bd46cb07
    Signed-off-by: Erik Kaneda <erik.kaneda@intel.com>
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/actbl2.h
+++ include/acpi/actbl2.h
@@ -1728,6 +1728,11 @@ struct acpi_table_prmt_header {
 	u32 module_info_count;
 };
 
+struct acpi_prmt_module_header {
+	u16 revision;
+	u16 length;
+};
+
 struct acpi_prmt_module_info {
 	u16 revision;
 	u16 length;
