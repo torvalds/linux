67a066b35765d13a55a56edd9b1f54dee9e441e1
Dmitry Osipenko <digetx@gmail.com>
2021-06-10 19:23:13 +0300

    of: reserved-memory: Add stub for RESERVEDMEM_OF_DECLARE()
    The reserved-memory Kconfig could be disabled when drivers are
    compile-tested. In this case RESERVEDMEM_OF_DECLARE() produces a
    noisy warning about the orphaned __reservedmem_of_table section.
    Add the missing stub that fixes the warning. In particular this is
    needed for compile-testing of NVIDIA Tegra210 memory driver which
    uses reserved-memory.
    
    Reported-by: Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Acked-by: Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
    Link: https://lore.kernel.org/r/20210610162313.20942-1-digetx@gmail.com
    Signed-off-by: Rob Herring <robh@kernel.org>

--- include/linux/of_reserved_mem.h
+++ include/linux/of_reserved_mem.h
@@ -27,10 +27,10 @@ struct reserved_mem_ops {
 
 typedef int (*reservedmem_of_init_fn)(struct reserved_mem *rmem);
 
+#ifdef CONFIG_OF_RESERVED_MEM
+
 #define RESERVEDMEM_OF_DECLARE(name, compat, init)			\
 	_OF_DECLARE(reservedmem, name, compat, init, reservedmem_of_init_fn)
-
-#ifdef CONFIG_OF_RESERVED_MEM
 
 int of_reserved_mem_device_init_by_idx(struct device *dev,
 				       struct device_node *np, int idx);
@@ -41,6 +41,10 @@ void of_reserved_mem_device_release(struct device *dev);
 
 struct reserved_mem *of_reserved_mem_lookup(struct device_node *np);
 #else
+
+#define RESERVEDMEM_OF_DECLARE(name, compat, init)			\
+	_OF_DECLARE_STUB(reservedmem, name, compat, init, reservedmem_of_init_fn)
+
 static inline int of_reserved_mem_device_init_by_idx(struct device *dev,
 					struct device_node *np, int idx)
 {
