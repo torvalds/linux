4e73bfa3ee37277e3f3e636786fd52d89273bd79
Andreas Kaessens <akaessens@gmail.com>
2021-06-10 15:24:37 +0200

    pinctrl: mcp23s08: Add optional reset GPIO
    The MCP23x port expander RESET# line can be connected to a host GPIO.
    The optional reset-gpio must be set to LOW if the reset is asserted
    at probing time.
    
    On page 5 in the datasheet [0] the "Device Active After Reset high"
    time is specified at 0 µs. Therefore no waiting is needed after the
    reset transition.
    
    [0] https://ww1.microchip.com/downloads/en/DeviceDoc/20001952C.pdf
    
    Signed-off-by: Andreas Kaessens <akaessens@gmail.com>
    Signed-off-by: Darian Biastoch <d.biastoch@gmail.com>
    Link: https://lore.kernel.org/r/20210610132438.3085841-1-akaessens@gmail.com
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-mcp23s08.c
+++ drivers/pinctrl/pinctrl-mcp23s08.c
@@ -9,6 +9,7 @@
 #include <linux/module.h>
 #include <linux/export.h>
 #include <linux/gpio/driver.h>
+#include <linux/gpio/consumer.h>
 #include <linux/slab.h>
 #include <asm/byteorder.h>
 #include <linux/interrupt.h>
@@ -557,6 +558,8 @@ int mcp23s08_probe_one(struct mcp23s08 *mcp, struct device *dev,
 	mcp->chip.can_sleep = true;
 	mcp->chip.parent = dev;
 	mcp->chip.owner = THIS_MODULE;
+
+	mcp->reset_gpio = devm_gpiod_get_optional(dev, "reset", GPIOD_OUT_LOW);
 
 	/* verify MCP_IOCON.SEQOP = 0, so sequential reads work,
 	 * and MCP_IOCON.HAEN = 1, so we work with all chips.
