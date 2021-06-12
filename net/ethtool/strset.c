e175aef902697826d344ce3a12189329848fe898
Jakub Kicinski <kuba@kernel.org>
2021-06-11 18:49:48 -0700

    ethtool: strset: fix message length calculation
    Outer nest for ETHTOOL_A_STRSET_STRINGSETS is not accounted for.
    This may result in ETHTOOL_MSG_STRSET_GET producing a warning like:
    
        calculated message payload length (684) not sufficient
        WARNING: CPU: 0 PID: 30967 at net/ethtool/netlink.c:369 ethnl_default_doit+0x87a/0xa20
    
    and a splat.
    
    As usually with such warnings three conditions must be met for the warning
    to trigger:
     - there must be no skb size rounding up (e.g. reply_size of 684);
     - string set must be per-device (so that the header gets populated);
     - the device name must be at least 12 characters long.
    
    all in all with current user space it looks like reading priv flags
    is the only place this could potentially happen. Or with syzbot :)
    
    Reported-by: syzbot+59aa77b92d06cd5a54f2@syzkaller.appspotmail.com
    Fixes: 71921690f974 ("ethtool: provide string sets with STRSET_GET request")
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ethtool/strset.c
+++ net/ethtool/strset.c
@@ -353,6 +353,8 @@ static int strset_reply_size(const struct ethnl_req_info *req_base,
 	int len = 0;
 	int ret;
 
+	len += nla_total_size(0); /* ETHTOOL_A_STRSET_STRINGSETS */
+
 	for (i = 0; i < ETH_SS_COUNT; i++) {
 		const struct strset_info *set_info = &data->sets[i];
 
