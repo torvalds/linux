2b6d2833cd1d8a43a837a45da65860ef086443dc
Zhihao Cheng <chengzhihao1@huawei.com>
2021-06-15 17:39:05 +0800

    mtd: mtd_blkdevs: Initialize rq.limits.discard_granularity
    Since commit b35fd7422c2f8("block: check queue's limits.discard_granularity
    in __blkdev_issue_discard()") checks rq.limits.discard_granularity in
    __blkdev_issue_discard(), we may get following warnings on formatted ftl:
    
      WARNING: CPU: 2 PID: 7313 at block/blk-lib.c:51
      __blkdev_issue_discard+0x2a7/0x390
    
    Reproducer:
      1. ftl_format /dev/mtd0
      2. modprobe ftl
      3. mkfs.vfat /dev/ftla
      4. mount -odiscard /dev/ftla temp
      5. dd if=/dev/zero of=temp/tst bs=1M count=10 oflag=direct
      6. dd if=/dev/zero of=temp/tst bs=1M count=10 oflag=direct
    
    Fix it by initializing rq.limits.discard_granularity if device supports
    discard operation.
    
    Signed-off-by: Zhihao Cheng <chengzhihao1@huawei.com>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Link: https://lore.kernel.org/linux-mtd/20210615093905.3473709-1-chengzhihao1@huawei.com

--- drivers/mtd/mtd_blkdevs.c
+++ drivers/mtd/mtd_blkdevs.c
@@ -419,6 +419,7 @@ int add_mtd_blktrans_dev(struct mtd_blktrans_dev *new)
 	if (tr->discard) {
 		blk_queue_flag_set(QUEUE_FLAG_DISCARD, new->rq);
 		blk_queue_max_discard_sectors(new->rq, UINT_MAX);
+		new->rq->limits.discard_granularity = tr->blksize;
 	}
 
 	gd->queue = new->rq;
