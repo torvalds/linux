2df1abffc474648af02cb29ba95d98990bfc2e5e
Siddharth Gupta <sidgup@codeaurora.org>
2021-06-14 19:21:10 -0700

    remoteproc: core: Fix cdev remove and rproc del
    [ Upstream commit 930eec0be20c93a53160c74005a1485a230e6911 ]
    
    The rproc_char_device_remove() call currently unmaps the cdev
    region instead of simply deleting the cdev that was added as a
    part of the rproc_char_device_add() call. This change fixes that
    behaviour, and also fixes the order in which device_del() and
    cdev_del() need to be called.
    
    Signed-off-by: Siddharth Gupta <sidgup@codeaurora.org>
    Link: https://lore.kernel.org/r/1623723671-5517-4-git-send-email-sidgup@codeaurora.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -2290,7 +2290,6 @@ int rproc_del(struct rproc *rproc)
 	mutex_unlock(&rproc->lock);
 
 	rproc_delete_debug_dir(rproc);
-	rproc_char_device_remove(rproc);
 
 	/* the rproc is downref'ed as soon as it's removed from the klist */
 	mutex_lock(&rproc_list_mutex);
@@ -2301,6 +2300,7 @@ int rproc_del(struct rproc *rproc)
 	synchronize_rcu();
 
 	device_del(&rproc->dev);
+	rproc_char_device_remove(rproc);
 
 	return 0;
 }
