e43805c28df6394254d1f49a388a1c70cae208a1
Yang Yingliang <yangyingliang@huawei.com>
2021-06-15 21:51:56 +0800

    ASoC: img-i2s-in: Use devm_platform_get_and_ioremap_resource()
    Use devm_platform_get_and_ioremap_resource() to simplify
    code.
    
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Link: https://lore.kernel.org/r/20210615135200.1661695-1-yangyingliang@huawei.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/img/img-i2s-in.c
+++ sound/soc/img/img-i2s-in.c
@@ -434,8 +434,7 @@ static int img_i2s_in_probe(struct platform_device *pdev)
 
 	i2s->dev = dev;
 
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	base = devm_ioremap_resource(dev, res);
+	base = devm_platform_get_and_ioremap_resource(pdev, 0, &res);
 	if (IS_ERR(base))
 		return PTR_ERR(base);
 
