afc3a0b4c408b00787d60225e6d667e1e6f93b6a
Yang Yingliang <yangyingliang@huawei.com>
2021-06-15 21:35:15 +0800

    ASoC: hisilicon: Use devm_platform_get_and_ioremap_resource()
    Use devm_platform_get_and_ioremap_resource() to simplify
    code.
    
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Link: https://lore.kernel.org/r/20210615133515.1376290-1-yangyingliang@huawei.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/hisilicon/hi6210-i2s.c
+++ sound/soc/hisilicon/hi6210-i2s.c
@@ -556,8 +556,7 @@ static int hi6210_i2s_probe(struct platform_device *pdev)
 	i2s->dev = dev;
 	spin_lock_init(&i2s->lock);
 
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	i2s->base = devm_ioremap_resource(dev, res);
+	i2s->base = devm_platform_get_and_ioremap_resource(pdev, 0, &res);
 	if (IS_ERR(i2s->base))
 		return PTR_ERR(i2s->base);
 
