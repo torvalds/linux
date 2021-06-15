2cd16cf0d6bbb47adddc633c60ca405f672e64f4
Yang Yingliang <yangyingliang@huawei.com>
2021-06-15 09:39:16 +0800

    ASoC: fsl_easrc: Use devm_platform_get_and_ioremap_resource()
    Use devm_platform_get_and_ioremap_resource() to simplify
    code.
    
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Link: https://lore.kernel.org/r/20210615013922.784296-4-yangyingliang@huawei.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/fsl/fsl_easrc.c
+++ sound/soc/fsl/fsl_easrc.c
@@ -1887,8 +1887,7 @@ static int fsl_easrc_probe(struct platform_device *pdev)
 	easrc->private = easrc_priv;
 	np = dev->of_node;
 
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	regs = devm_ioremap_resource(dev, res);
+	regs = devm_platform_get_and_ioremap_resource(pdev, 0, &res);
 	if (IS_ERR(regs))
 		return PTR_ERR(regs);
 
