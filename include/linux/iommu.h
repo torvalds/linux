910c4406ccc9613de0a54abf910edc4bf8a575c0
Isaac J. Manjarres <isaacm@codeaurora.org>
2021-06-16 06:38:45 -0700

    iommu: Add a map_pages() op for IOMMU drivers
    Add a callback for IOMMU drivers to provide a path for the
    IOMMU framework to call into an IOMMU driver, which can
    call into the io-pgtable code, to map a physically contiguous
    rnage of pages of the same size.
    
    For IOMMU drivers that do not specify a map_pages() callback,
    the existing logic of mapping memory one page block at a time
    will be used.
    
    Signed-off-by: Isaac J. Manjarres <isaacm@codeaurora.org>
    Suggested-by: Will Deacon <will@kernel.org>
    Acked-by: Lu Baolu <baolu.lu@linux.intel.com>
    Signed-off-by: Georgi Djakov <quic_c_gdjako@quicinc.com>
    Link: https://lore.kernel.org/r/1623850736-389584-5-git-send-email-quic_c_gdjako@quicinc.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -180,6 +180,8 @@ struct iommu_iotlb_gather {
  * @attach_dev: attach device to an iommu domain
  * @detach_dev: detach device from an iommu domain
  * @map: map a physically contiguous memory region to an iommu domain
+ * @map_pages: map a physically contiguous set of pages of the same size to
+ *             an iommu domain.
  * @unmap: unmap a physically contiguous memory region from an iommu domain
  * @unmap_pages: unmap a number of pages of the same size from an iommu domain
  * @flush_iotlb_all: Synchronously flush all hardware TLBs for this domain
@@ -230,6 +232,9 @@ struct iommu_ops {
 	void (*detach_dev)(struct iommu_domain *domain, struct device *dev);
 	int (*map)(struct iommu_domain *domain, unsigned long iova,
 		   phys_addr_t paddr, size_t size, int prot, gfp_t gfp);
+	int (*map_pages)(struct iommu_domain *domain, unsigned long iova,
+			 phys_addr_t paddr, size_t pgsize, size_t pgcount,
+			 int prot, gfp_t gfp, size_t *mapped);
 	size_t (*unmap)(struct iommu_domain *domain, unsigned long iova,
 		     size_t size, struct iommu_iotlb_gather *iotlb_gather);
 	size_t (*unmap_pages)(struct iommu_domain *domain, unsigned long iova,
