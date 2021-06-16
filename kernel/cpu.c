57d2ff9b1679862b73fc58cd336604365d4c6220
Lee Jones <lee.jones@linaro.org>
2021-06-16 16:58:43 +0100

    Revert "ANDROID: cpu: send KOBJ_ONLINE event when enabling cpus"
    This reverts commit e6120dd58d7de91152800a806e7a4fecd7870636.
    
    The original issue has been solved.  This commit is now superflouous.
    
    Bug: 120444461
    Cc: Thierry Strudel <tstrudel@google.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I549ef73c423cd3c4bc6d30e8e1ca723549c3b95e

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1512,7 +1512,6 @@ void __weak arch_thaw_secondary_cpus_end(void)
 void thaw_secondary_cpus(void)
 {
 	int cpu, error;
-	struct device *cpu_device;
 
 	/* Allow everyone to use the CPU hotplug again */
 	cpu_maps_update_begin();
@@ -1530,12 +1529,6 @@ void thaw_secondary_cpus(void)
 		trace_suspend_resume(TPS("CPU_ON"), cpu, false);
 		if (!error) {
 			pr_info("CPU%d is up\n", cpu);
-			cpu_device = get_cpu_device(cpu);
-			if (!cpu_device)
-				pr_err("%s: failed to get cpu%d device\n",
-				       __func__, cpu);
-			else
-				kobject_uevent(&cpu_device->kobj, KOBJ_ONLINE);
 			continue;
 		}
 		pr_warn("Error taking CPU%d up: %d\n", cpu, error);
