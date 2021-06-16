6a03cd9843fa4dcf827dc3ad60fa9b4217f3057c
Christoph Hellwig <hch@lst.de>
2021-06-16 09:15:47 +0200

    loop: fix order of cleaning up the queue and freeing the tagset
    We must release the queue before freeing the tagset.
    
    Fixes: 1c99502fae35 ("loop: use blk_mq_alloc_disk and blk_cleanup_disk")
    Reported-by: Bruno Goncalves <bgoncalv@redhat.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -2183,8 +2183,8 @@ out:
 static void loop_remove(struct loop_device *lo)
 {
 	del_gendisk(lo->lo_disk);
-	blk_mq_free_tag_set(&lo->tag_set);
 	blk_cleanup_disk(lo->lo_disk);
+	blk_mq_free_tag_set(&lo->tag_set);
 	mutex_destroy(&lo->lo_mutex);
 	kfree(lo);
 }
