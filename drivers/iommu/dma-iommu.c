8ce4904bfd22de04ac3cd35d469c0a3337bdeb7b
Jean-Philippe Brucker <jean-philippe@linaro.org>
2021-06-18 17:21:00 +0200

    iommu/virtio: Enable x86 support
    With the VIOT support in place, x86 platforms can now use the
    virtio-iommu.
    
    Because the other x86 IOMMU drivers aren't yet ready to use the
    acpi_dma_setup() path, x86 doesn't implement arch_setup_dma_ops() at the
    moment. Similarly to Vt-d and AMD IOMMU, clear the DMA ops and call
    iommu_setup_dma_ops() from probe_finalize().
    
    Acked-by: Joerg Roedel <jroedel@suse.de>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Tested-by: Eric Auger <eric.auger@redhat.com>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Link: https://lore.kernel.org/r/20210618152059.1194210-6-jean-philippe@linaro.org
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/dma-iommu.c
+++ drivers/iommu/dma-iommu.c
@@ -1330,6 +1330,7 @@ out_err:
 	 pr_warn("Failed to set up IOMMU for device %s; retaining platform DMA ops\n",
 		 dev_name(dev));
 }
+EXPORT_SYMBOL_GPL(iommu_setup_dma_ops);
 
 static struct iommu_dma_msi_page *iommu_dma_get_msi_page(struct device *dev,
 		phys_addr_t msi_addr, struct iommu_domain *domain)
