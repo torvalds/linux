0d0f9e5f2fa7aacf22892078a1065fa5d0ce941b
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2021-06-18 13:45:11 +1000

    powerpc/microwatt: Populate platform bus from device-tree
    Just like any other embedded platform.
    
    Add an empty soc node.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Paul Mackerras <paulus@ozlabs.org>
    Reviewed-by: Segher Boessenkool <segher@kernel.crashing.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/YMwWx98+PMibZq/G@thinks.paulus.ozlabs.org

--- arch/powerpc/platforms/microwatt/setup.c
+++ arch/powerpc/platforms/microwatt/setup.c
@@ -8,6 +8,8 @@
 #include <linux/kernel.h>
 #include <linux/stddef.h>
 #include <linux/init.h>
+#include <linux/of.h>
+#include <linux/of_platform.h>
 #include <asm/machdep.h>
 #include <asm/time.h>
 
@@ -15,6 +17,12 @@ static int __init microwatt_probe(void)
 {
 	return of_machine_is_compatible("microwatt-soc");
 }
+
+static int __init microwatt_populate(void)
+{
+	return of_platform_default_populate(NULL, NULL, NULL);
+}
+machine_arch_initcall(microwatt, microwatt_populate);
 
 define_machine(microwatt) {
 	.name			= "microwatt",
