929be906fa83151c4c77c07cd3d780439d38b153
Steve French <stfrench@microsoft.com>
2021-06-18 00:31:49 -0500

    cifs: use SPDX-Licence-Identifier
    Add SPDX license identifier and replace license boilerplate.
    Corrects various checkpatch errors with the older format for
    noting the LGPL license.
    
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/cifs/cifs_fs_sb.h
+++ fs/cifs/cifs_fs_sb.h
@@ -1,18 +1,9 @@
+/* SPDX-License-Identifier: LGPL-2.1 */
 /*
  *   fs/cifs/cifs_fs_sb.h
  *
  *   Copyright (c) International Business Machines  Corp., 2002,2004
  *   Author(s): Steve French (sfrench@us.ibm.com)
- *
- *   This library is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Lesser General Public License as published
- *   by the Free Software Foundation; either version 2.1 of the License, or
- *   (at your option) any later version.
- *
- *   This library is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU Lesser General Public License for more details.
  *
  */
 #include <linux/rbtree.h>
