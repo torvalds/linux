4f911138c8da94bcff84f1d093d28e378703c43f
Amir Goldstein <amir73il@gmail.com>
2021-06-19 12:26:16 +0300

    fs: add generic helper for filling statx attribute flags
    The immutable and append-only properties on an inode are published on
    the inode's i_flags and enforced by the VFS.
    
    Create a helper to fill the corresponding STATX_ATTR_ flags in the kstat
    structure from the inode's i_flags.
    
    Only orange was converted to use this helper.
    Other filesystems could use it in the future.
    
    Suggested-by: Miklos Szeredi <miklos@szeredi.hu>
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -3355,6 +3355,7 @@ extern int page_symlink(struct inode *inode, const char *symname, int len);
 extern const struct inode_operations page_symlink_inode_operations;
 extern void kfree_link(void *);
 void generic_fillattr(struct user_namespace *, struct inode *, struct kstat *);
+void generic_fill_statx_attr(struct inode *inode, struct kstat *stat);
 extern int vfs_getattr_nosec(const struct path *, struct kstat *, u32, unsigned int);
 extern int vfs_getattr(const struct path *, struct kstat *, u32, unsigned int);
 void __inode_add_bytes(struct inode *inode, loff_t bytes);
