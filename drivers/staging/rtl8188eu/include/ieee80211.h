872cfb096d3a009709b56f23f5280a359bbe58e2
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2021-06-19 22:14:34 +0200

    staging: rtl8188eu: remove "rtw_ieee80211_back_parties"
    This enum is unused. It is also close to "ieee80211_back_parties" in
    "include/linux/ieee80211.h", which should be used if needed.
    
    So remove it
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Link: https://lore.kernel.org/r/f389e0d54a5a8a46c9a696207541aa1cb433676b.1624132543.git.christophe.jaillet@wanadoo.fr
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/ieee80211.h
+++ drivers/staging/rtl8188eu/include/ieee80211.h
@@ -559,13 +559,6 @@ enum rtw_ieee80211_ht_actioncode {
 	RTW_WLAN_ACTION_HI_INFO_EXCHG = 8,
 };
 
-/* BACK (block-ack) parties */
-enum rtw_ieee80211_back_parties {
-	RTW_WLAN_BACK_RECIPIENT = 0,
-	RTW_WLAN_BACK_INITIATOR = 1,
-	RTW_WLAN_BACK_TIMER = 2,
-};
-
 #define OUI_MICROSOFT 0x0050f2 /* Microsoft (also used in Wi-Fi specs)
 				* 00:50:F2
 				*/
