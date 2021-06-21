5d299f44d7658f4423e33a0b9915bc8d81687511
Kees Cook <keescook@chromium.org>
2021-06-21 16:23:22 -0700

    jfs: Avoid field-overflowing memcpy()
    In preparation for FORTIFY_SOURCE performing compile-time and run-time
    field array bounds checking for memcpy(), memmove(), and memset(),
    avoid intentionally writing across neighboring fields.
    
    Introduce more unions to cover the full inline data section, so that the
    entire 256 bytes can be addressed by memcpy() without thinking it is
    crossing field boundaries. Additionally adjusts dir memcpy() to use
    existing union names to get the same coverage.
    
    diffoscope shows there are no binary differences before/after excepting
    the name of the initcall, which is line number based:
    
    $ diffoscope --exclude-directory-metadata yes before/fs after/fs
     --- before/fs
     +++ after/fs
     │   --- before/fs/jfs
     ├── +++ after/fs/jfs
     │ │   --- before/fs/jfs/super.o
     │ ├── +++ after/fs/jfs/super.o
     │ │ ├── readelf --wide --symbols {}
     │ │ │ @@ -2,15 +2,15 @@
     │ │ │  Symbol table '.symtab' contains 158 entries:
     │ │ │     Num:    Value          Size Type    Bind   Vis      Ndx Name
     ...
     │ │ │ -     5: 0000000000000000     0 NOTYPE  LOCAL  DEFAULT    6 __initcall__kmod_jfs__319_1049_ini
     t_jfs_fs6
     │ │ │ +     5: 0000000000000000     0 NOTYPE  LOCAL  DEFAULT    6 __initcall__kmod_jfs__319_1050_ini
     t_jfs_fs6
    ...
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>

--- fs/jfs/super.c
+++ fs/jfs/super.c
@@ -939,7 +939,8 @@ static int __init init_jfs_fs(void)
 	jfs_inode_cachep =
 	    kmem_cache_create_usercopy("jfs_ip", sizeof(struct jfs_inode_info),
 			0, SLAB_RECLAIM_ACCOUNT|SLAB_MEM_SPREAD|SLAB_ACCOUNT,
-			offsetof(struct jfs_inode_info, i_inline), IDATASIZE,
+			offsetof(struct jfs_inode_info, i_inline_all),
+			sizeof_field(struct jfs_inode_info, i_inline_all),
 			init_once);
 	if (jfs_inode_cachep == NULL)
 		return -ENOMEM;
