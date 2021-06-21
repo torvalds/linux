0dda4eeb484962ad574fcea77a78a4fbd7bc06ba
Ingo Molnar <mingo@kernel.org>
2021-06-21 10:33:56 +0200

    sched/headers: Reorganize, clean up and optimize kernel/sched/build_policy.c dependencies
    Use all generic headers from kernel/sched/sched.h that are required
    for it to build.
    
    Sort the sections alphabetically.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Peter Zijlstra <peterz@infradead.org>

--- kernel/sched/build_policy.c
+++ kernel/sched/build_policy.c
@@ -12,11 +12,35 @@
  * core.c and fair.c are built separately.
  */
 
+/* Headers: */
+#include <linux/sched/clock.h>
+#include <linux/sched/cputime.h>
+#include <linux/sched/posix-timers.h>
+#include <linux/sched/rt.h>
+
+#include <linux/cpuidle.h>
+#include <linux/jiffies.h>
+#include <linux/livepatch.h>
+#include <linux/psi.h>
+#include <linux/seqlock_api.h>
+#include <linux/slab.h>
+#include <linux/suspend.h>
+#include <linux/tsacct_kern.h>
+#include <linux/vtime.h>
+
+#ifdef CONFIG_PARAVIRT
+# include <asm/paravirt.h>
+#endif
+
+#include <uapi/linux/sched/types.h>
+
 #include "sched.h"
 
 #include "autogroup.h"
 #include "stats.h"
 #include "pelt.h"
+
+/* Source code modules: */
 
 #include "idle.c"
 
