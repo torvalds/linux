62a6ef6a996f5eec73d30d079573a1fa8f95fcd9
Marcin Wojtas <mw@semihalf.com>
2021-06-21 19:30:24 +0200

    net: mdiobus: Introduce fwnode_mdbiobus_register()
    This patch introduces a new helper function that
    wraps acpi_/of_ mdiobus_register() and allows its
    usage via common fwnode_ interface.
    
    Fall back to raw mdiobus_register() in case CONFIG_FWNODE_MDIO
    is not enabled, in order to satisfy compatibility
    in all future user drivers.
    
    Signed-off-by: Marcin Wojtas <mw@semihalf.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/fwnode_mdio.h
+++ include/linux/fwnode_mdio.h
@@ -16,6 +16,7 @@ int fwnode_mdiobus_phy_device_register(struct mii_bus *mdio,
 int fwnode_mdiobus_register_phy(struct mii_bus *bus,
 				struct fwnode_handle *child, u32 addr);
 
+int fwnode_mdiobus_register(struct mii_bus *bus, struct fwnode_handle *fwnode);
 #else /* CONFIG_FWNODE_MDIO */
 int fwnode_mdiobus_phy_device_register(struct mii_bus *mdio,
 				       struct phy_device *phy,
@@ -29,6 +30,17 @@ static inline int fwnode_mdiobus_register_phy(struct mii_bus *bus,
 					      u32 addr)
 {
 	return -EINVAL;
+}
+
+static inline int fwnode_mdiobus_register(struct mii_bus *bus,
+					  struct fwnode_handle *fwnode)
+{
+	/*
+	 * Fall back to mdiobus_register() function to register a bus.
+	 * This way, we don't have to keep compat bits around in drivers.
+	 */
+
+	return mdiobus_register(mdio);
 }
 #endif
 
