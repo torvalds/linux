1a8961307af6791cd7db3ecddf7841750ab84ea2
Daniel Vetter <daniel.vetter@ffwll.ch>
2021-06-22 18:55:05 +0200

    drm/armada: Remove prepare/cleanup_fb hooks
    All they do is refcount the fb, which the atomic helpers already do.
    
    This is was necessary with the legacy helpers and I guess just carry
    over in the conversion. drm_plane_state always has a full reference
    for its ->fb pointer during its entire lifetime,
    see __drm_atomic_helper_plane_destroy_state()
    
    Acked-by: Maxime Ripard <maxime@cerno.tech>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Cc: Russell King <linux@armlinux.org.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210622165511.3169559-10-daniel.vetter@ffwll.ch

--- drivers/gpu/drm/armada/armada_plane.h
+++ drivers/gpu/drm/armada/armada_plane.h
@@ -21,8 +21,6 @@ struct armada_plane_state {
 
 void armada_drm_plane_calc(struct drm_plane_state *state, u32 addrs[2][3],
 	u16 pitches[3], bool interlaced);
-int armada_drm_plane_prepare_fb(struct drm_plane *plane,
-	struct drm_plane_state *state);
 void armada_drm_plane_cleanup_fb(struct drm_plane *plane,
 	struct drm_plane_state *old_state);
 int armada_drm_plane_atomic_check(struct drm_plane *plane,
