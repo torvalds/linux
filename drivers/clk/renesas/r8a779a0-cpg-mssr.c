417ed58dfc5ed6c2ec608d5ee93dd27197190e19
Kieran Bingham <kieran.bingham@ideasonboard.com>
2021-06-23 00:27:10 +0100

    clk: renesas: r8a779a0: Add the DU clock
    The DU clock is added to the S3D1 clock parent. The Renesas BSP lists
    S2D1 as the clock parent, however there is no S2 clock on this platform.
    
    S3D1 is chosen as a best effort guess and demonstrates functionality but
    is not guaranteed to be correct.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Link: https://lore.kernel.org/r/20210622232711.3219697-2-kieran.bingham@ideasonboard.com
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- drivers/clk/renesas/r8a779a0-cpg-mssr.c
+++ drivers/clk/renesas/r8a779a0-cpg-mssr.c
@@ -167,6 +167,7 @@ static const struct mssr_mod_clk r8a779a0_mod_clks[] __initconst = {
 	DEF_MOD("csi41",	400,	R8A779A0_CLK_CSI0),
 	DEF_MOD("csi42",	401,	R8A779A0_CLK_CSI0),
 	DEF_MOD("csi43",	402,	R8A779A0_CLK_CSI0),
+	DEF_MOD("du",		411,	R8A779A0_CLK_S3D1),
 	DEF_MOD("fcpvd0",	508,	R8A779A0_CLK_S3D1),
 	DEF_MOD("fcpvd1",	509,	R8A779A0_CLK_S3D1),
 	DEF_MOD("hscif0",	514,	R8A779A0_CLK_S1D2),
