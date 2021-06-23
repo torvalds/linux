f760c1101f5284acdf3a8132dff257838b9bf75c
Kees Cook <keescook@chromium.org>
2021-06-23 13:39:33 -0700

    lkdtm: Enable DOUBLE_FAULT on all architectures
    commit f123c42bbeff26bfe8bdb08a01307e92d51eec39 upstream
    
    Where feasible, I prefer to have all tests visible on all architectures,
    but to have them wired to XFAIL. DOUBLE_FAIL was set up to XFAIL, but
    wasn't actually being added to the test list.
    
    Fixes: cea23efb4de2 ("lkdtm/bugs: Make double-fault test always available")
    Cc: stable@vger.kernel.org
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Link: https://lore.kernel.org/r/20210623203936.3151093-7-keescook@chromium.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [sudip: adjust context]
    Signed-off-by: Sudip Mukherjee <sudipm.mukherjee@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/lkdtm/core.c
+++ drivers/misc/lkdtm/core.c
@@ -173,9 +173,7 @@ static const struct crashtype crashtypes[] = {
 	CRASHTYPE(USERCOPY_KERNEL),
 	CRASHTYPE(STACKLEAK_ERASING),
 	CRASHTYPE(CFI_FORWARD_PROTO),
-#ifdef CONFIG_X86_32
 	CRASHTYPE(DOUBLE_FAULT),
-#endif
 };
 
 
