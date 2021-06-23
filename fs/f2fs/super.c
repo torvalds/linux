bb51a331824e70cbc89479ea4d78dfb70e2a02bc
Jaegeuk Kim <jaegeuk@google.com>
2021-06-23 01:42:05 -0700

    Revert "f2fs: avoid attaching SB_ACTIVE flag during mount/remount"
    This reverts commit 42bbf0bcc2a57eca1323976af8ebbe58d13cb4e2.

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1908,15 +1908,17 @@ static int f2fs_enable_quotas(struct super_block *sb);
 
 static int f2fs_disable_checkpoint(struct f2fs_sb_info *sbi)
 {
+	unsigned int s_flags = sbi->sb->s_flags;
 	struct cp_control cpc;
 	int err = 0;
 	int ret;
 	block_t unusable;
 
-	if (sbi->sb->s_flags & SB_RDONLY) {
+	if (s_flags & SB_RDONLY) {
 		f2fs_err(sbi, "checkpoint=disable on readonly fs");
 		return -EINVAL;
 	}
+	sbi->sb->s_flags |= SB_ACTIVE;
 
 	f2fs_update_time(sbi, DISABLE_TIME);
 
@@ -1934,13 +1936,13 @@ static int f2fs_disable_checkpoint(struct f2fs_sb_info *sbi)
 	ret = sync_filesystem(sbi->sb);
 	if (ret || err) {
 		err = ret ? ret : err;
-		goto out;
+		goto restore_flag;
 	}
 
 	unusable = f2fs_get_unusable_blocks(sbi);
 	if (f2fs_disable_cp_again(sbi, unusable)) {
 		err = -EAGAIN;
-		goto out;
+		goto restore_flag;
 	}
 
 	down_write(&sbi->gc_lock);
@@ -1956,7 +1958,8 @@ static int f2fs_disable_checkpoint(struct f2fs_sb_info *sbi)
 
 out_unlock:
 	up_write(&sbi->gc_lock);
-out:
+restore_flag:
+	sbi->sb->s_flags = s_flags;	/* Restore SB_RDONLY status */
 	return err;
 }
 
