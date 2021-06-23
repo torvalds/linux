492200f2479d08ac4dddd8a9d59a28b9227a0310
Wolfram Sang <wsa+renesas@sang-engineering.com>
2021-06-23 11:57:32 +0200

    mmc: sh_mmcif: use proper DMAENGINE API for termination
    dmaengine_terminate_all() is deprecated in favor of explicitly saying if
    it should be sync or async. Here, we want dmaengine_terminate_sync()
    because there is no other synchronization code in the driver to handle
    an async case.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://lore.kernel.org/r/20210623095734.3046-3-wsa+renesas@sang-engineering.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/sh_mmcif.c
+++ drivers/mmc/host/sh_mmcif.c
@@ -1164,9 +1164,9 @@ static bool sh_mmcif_end_cmd(struct sh_mmcif_host *host)
 		data->bytes_xfered = 0;
 		/* Abort DMA */
 		if (data->flags & MMC_DATA_READ)
-			dmaengine_terminate_all(host->chan_rx);
+			dmaengine_terminate_sync(host->chan_rx);
 		else
-			dmaengine_terminate_all(host->chan_tx);
+			dmaengine_terminate_sync(host->chan_tx);
 	}
 
 	return false;
