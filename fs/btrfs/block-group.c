54afaae34ee49e98c1c902b444b42832551d090c
David Sterba <dsterba@suse.com>
2021-06-23 17:54:54 +0200

    btrfs: zoned: fix types for u64 division in btrfs_reclaim_bgs_work
    The types in calculation of the used percentage in the reclaiming
    messages are both u64, though bg->length is either 1GiB (non-zoned) or
    the zone size in the zoned mode. The upper limit on zone size is 8GiB so
    this could theoretically overflow in the future, right now the values
    fit.
    
    Fixes: 18bb8bbf13c1 ("btrfs: zoned: automatically reclaim zones")
    CC: stable@vger.kernel.org # 5.13
    Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/block-group.c
+++ fs/btrfs/block-group.c
@@ -1540,7 +1540,7 @@ void btrfs_reclaim_bgs_work(struct work_struct *work)
 			goto next;
 
 		btrfs_info(fs_info, "reclaiming chunk %llu with %llu%% used",
-				bg->start, div_u64(bg->used * 100, bg->length));
+				bg->start, div64_u64(bg->used * 100, bg->length));
 		trace_btrfs_reclaim_block_group(bg);
 		ret = btrfs_relocate_chunk(fs_info, bg->start);
 		if (ret)
