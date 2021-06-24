f916d77eedfeb3efbbcd4b209601671ec1b417f2
Mark Brown <broonie@kernel.org>
2021-06-24 08:53:14 +0200

    tools/nolibc: Implement msleep()
    Allow users to implement shorter delays than a full second by implementing
    msleep().
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- tools/include/nolibc/nolibc.h
+++ tools/include/nolibc/nolibc.h
@@ -2244,6 +2244,19 @@ unsigned int sleep(unsigned int seconds)
 }
 
 static __attribute__((unused))
+int msleep(unsigned int msecs)
+{
+	struct timeval my_timeval = { msecs / 1000, (msecs % 1000) * 1000 };
+
+	if (sys_select(0, 0, 0, 0, &my_timeval) < 0)
+		return (my_timeval.tv_sec * 1000) +
+			(my_timeval.tv_usec / 1000) +
+			!!(my_timeval.tv_usec % 1000);
+	else
+		return 0;
+}
+
+static __attribute__((unused))
 int stat(const char *path, struct stat *buf)
 {
 	int ret = sys_stat(path, buf);
