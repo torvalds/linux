d9b17a030a1bd165a4e396340e7d442f63cfa36c
Peter Zijlstra <peterz@infradead.org>
2021-06-24 11:41:02 +0200

    objtool: Handle __sanitize_cov*() tail calls
    [ Upstream commit f56dae88a81fded66adf2bea9922d1d98d1da14f ]
    
    Turns out the compilers also generate tail calls to __sanitize_cov*(),
    make sure to also patch those out in noinstr code.
    
    Fixes: 0f1441b44e82 ("objtool: Fix noinstr vs KCOV")
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Marco Elver <elver@google.com>
    Link: https://lore.kernel.org/r/20210624095147.818783799@infradead.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/objtool/include/objtool/arch.h
+++ tools/objtool/include/objtool/arch.h
@@ -82,6 +82,7 @@ unsigned long arch_jump_destination(struct instruction *insn);
 unsigned long arch_dest_reloc_offset(int addend);
 
 const char *arch_nop_insn(int len);
+const char *arch_ret_insn(int len);
 
 int arch_decode_hint_reg(struct instruction *insn, u8 sp_reg);
 
