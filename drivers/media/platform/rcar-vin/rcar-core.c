12f6517f9726cd9778ef957a111df102eb1629a3
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2021-06-24 10:40:45 +0200

    media: rcar-vin: Remove explicit device availability check
    The fwnode is retrieved using fwnode_graph_get_endpoint_by_id() without
    the FWNODE_GRAPH_DEVICE_DISABLED flag set. So there is no need to
    explicitly check if the fwnode is available as it always will be when
    the check is performed, remove it.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Reviewed-by: Jacopo Mondi <jacopo@jmondi.org>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/platform/rcar-vin/rcar-core.c
+++ drivers/media/platform/rcar-vin/rcar-core.c
@@ -330,13 +330,6 @@ static int rvin_group_parse_of(struct rvin_dev *vin, unsigned int port,
 		goto out;
 	}
 
-	if (!of_device_is_available(to_of_node(fwnode))) {
-		vin_dbg(vin, "OF device %pOF disabled, ignoring\n",
-			to_of_node(fwnode));
-		ret = -ENOTCONN;
-		goto out;
-	}
-
 	asd = v4l2_async_nf_add_fwnode(&vin->group->notifier, fwnode,
 				       struct v4l2_async_subdev);
 	if (IS_ERR(asd)) {
