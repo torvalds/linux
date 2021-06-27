e149bd742b2db6a63fc078b1ea6843dc9b22678d
Hao Xu <haoxu@linux.alibaba.com>
2021-06-28 05:48:05 +0800

    io_uring: code clean for kiocb_done()
    A simple code clean for kiocb_done()
    
    Signed-off-by: Hao Xu <haoxu@linux.alibaba.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -2782,7 +2782,7 @@ static void kiocb_done(struct kiocb *kiocb, ssize_t ret,
 
 	if (req->flags & REQ_F_CUR_POS)
 		req->file->f_pos = kiocb->ki_pos;
-	if (ret >= 0 && kiocb->ki_complete == io_complete_rw)
+	if (ret >= 0 && check_reissue)
 		__io_complete_rw(req, ret, 0, issue_flags);
 	else
 		io_rw_done(kiocb, ret);
