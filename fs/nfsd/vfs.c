496d83cf0f2fa70cfe256c2499e2d3523d3868f3
Chuck Lever <chuck.lever@oracle.com>
2021-06-28 17:24:27 -0400

    NFSD: Batch release pages during splice read
    Large splice reads call put_page() repeatedly. put_page() is
    relatively expensive to call, so replace it with the new
    svc_rqst_replace_page() helper to help amortize that cost.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Reviewed-by: NeilBrown <neilb@suse.de>

--- fs/nfsd/vfs.c
+++ fs/nfsd/vfs.c
@@ -849,15 +849,10 @@ nfsd_splice_actor(struct pipe_inode_info *pipe, struct pipe_buffer *buf,
 	struct page *page = buf->page;
 
 	if (rqstp->rq_res.page_len == 0) {
-		get_page(page);
-		put_page(*rqstp->rq_next_page);
-		*(rqstp->rq_next_page++) = page;
+		svc_rqst_replace_page(rqstp, page);
 		rqstp->rq_res.page_base = buf->offset;
 	} else if (page != pp[-1]) {
-		get_page(page);
-		if (*rqstp->rq_next_page)
-			put_page(*rqstp->rq_next_page);
-		*(rqstp->rq_next_page++) = page;
+		svc_rqst_replace_page(rqstp, page);
 	}
 	rqstp->rq_res.page_len += sd->len;
 
