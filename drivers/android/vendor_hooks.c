d5ac0297416c24d7fbb36a06c4d40724352ef720
Rick Yiu <rickyiu@google.com>
2021-06-28 14:56:29 +0800

    ANDROID: sched: Add trace for __setscheduler_uclamp
    To know per-task uclamp request.
    
    Bug: 191973176
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: Ibd40391f2228db5daa410198339237879e67a078
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -202,3 +202,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_wake_up_sync);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_wake_flags);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_uclamp_eff_get);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_util_est_update);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_setscheduler_uclamp);
