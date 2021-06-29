6e1cae881a0646f31fe2bda90297d820da1137eb
Matthew Wilcox (Oracle) <willy@infradead.org>
2021-06-28 19:36:15 -0700

    mm/writeback: move __set_page_dirty() to core mm
    Patch series "Further set_page_dirty cleanups".
    
    Prompted by Christoph's recent patches, here are some more patches to
    improve the state of set_page_dirty().  They're all from the folio tree,
    so they've been tested to a certain extent.
    
    This patch (of 6):
    
    Nothing in __set_page_dirty() is specific to buffer_head, so move it to
    mm/page-writeback.c.  That removes the only caller of
    account_page_dirtied() outside of page-writeback.c, so make it static.
    
    Link: https://lkml.kernel.org/r/20210615162342.1669332-1-willy@infradead.org
    Link: https://lkml.kernel.org/r/20210615162342.1669332-2-willy@infradead.org
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1855,7 +1855,6 @@ int __set_page_dirty_nobuffers(struct page *page);
 int __set_page_dirty_no_writeback(struct page *page);
 int redirty_page_for_writepage(struct writeback_control *wbc,
 				struct page *page);
-void account_page_dirtied(struct page *page, struct address_space *mapping);
 void account_page_cleaned(struct page *page, struct address_space *mapping,
 			  struct bdi_writeback *wb);
 int set_page_dirty(struct page *page);
