747aeb2632f6341168b78f28ce16fce0e6cee205
Tejas Upadhyay <tejaskumarx.surendrakumar.upadhyay@intel.com>
2021-06-29 16:19:54 +0530

    drm/i915/ehl: Remove require_force_probe protection
    Removing force probe protection from EHL platform. Did
    not observe warnings, errors, flickering or any visual
    defects while doing ordinary tasks like browsing and
    editing documents in a two monitor setup.
    
    For more info drm-tip idle run results :
    https://intel-gfx-ci.01.org/tree/drm-tip/drmtip.html?
    
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Tejas Upadhyay <tejaskumarx.surendrakumar.upadhyay@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210629104954.927151-1-tejaskumarx.surendrakumar.upadhyay@intel.com

--- drivers/gpu/drm/i915/i915_pci.c
+++ drivers/gpu/drm/i915/i915_pci.c
@@ -845,7 +845,6 @@ static const struct intel_device_info icl_info = {
 static const struct intel_device_info ehl_info = {
 	GEN11_FEATURES,
 	PLATFORM(INTEL_ELKHARTLAKE),
-	.require_force_probe = 1,
 	.platform_engine_mask = BIT(RCS0) | BIT(BCS0) | BIT(VCS0) | BIT(VECS0),
 	.ppgtt_size = 36,
 };
