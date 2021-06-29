79e2311c876c276566e3fb84ba33682eb540874b
Bhupesh Sharma <bhupesh.sharma@linaro.org>
2021-06-29 18:04:07 +0530

    pinctrl: qcom/pinctrl-spmi-gpio: Add compatible for pmic-gpio on SA8155p-adp
    SA8155p-adp PMIC (PMM8155AU) exposes 10 GPIOs.
    
    Add support for the same in the pinctrl driver.
    
    Cc: Linus Walleij <linus.walleij@linaro.org>
    Cc: Bjorn Andersson <bjorn.andersson@linaro.org>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Bhupesh Sharma <bhupesh.sharma@linaro.org>
    Link: https://lore.kernel.org/r/20210629123407.82561-5-bhupesh.sharma@linaro.org
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/qcom/pinctrl-spmi-gpio.c
+++ drivers/pinctrl/qcom/pinctrl-spmi-gpio.c
@@ -1133,6 +1133,7 @@ static const struct of_device_id pmic_gpio_of_match[] = {
 	{ .compatible = "qcom,pmi8994-gpio", .data = (void *) 10 },
 	{ .compatible = "qcom,pmi8998-gpio", .data = (void *) 14 },
 	{ .compatible = "qcom,pmk8350-gpio", .data = (void *) 4 },
+	{ .compatible = "qcom,pmm8155au-gpio", .data = (void *) 10 },
 	{ .compatible = "qcom,pmr735a-gpio", .data = (void *) 4 },
 	{ .compatible = "qcom,pmr735b-gpio", .data = (void *) 4 },
 	/* pms405 has 12 GPIOs with holes on 1, 9, and 10 */
