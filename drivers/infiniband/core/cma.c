a23ba98e91ffda5a1dc84bd904fe698002f96474
Leon Romanovsky <leonro@nvidia.com>
2021-06-29 09:49:33 +0300

    RDMA/core: Always release restrack object
    [ Upstream commit 3d8287544223a3d2f37981c1f9ffd94d0b5e9ffc ]
    
    Change location of rdma_restrack_del() to fix the bug where
    task_struct was acquired but not released, causing to resource leak.
    
      ucma_create_id() {
        ucma_alloc_ctx();
        rdma_create_user_id() {
          rdma_restrack_new();
          rdma_restrack_set_name() {
            rdma_restrack_attach_task.part.0(); <--- task_struct was gotten
          }
        }
        ucma_destroy_private_ctx() {
          ucma_put_ctx();
          rdma_destroy_id() {
            _destroy_id()                       <--- id_priv was freed
          }
        }
      }
    
    Fixes: 889d916b6f8a ("RDMA/core: Don't access cm_id after its destruction")
    Link: https://lore.kernel.org/r/073ec27acb943ca8b6961663c47c5abe78a5c8cc.1624948948.git.leonro@nvidia.com
    Reported-by: Pavel Skripkin <paskripkin@gmail.com>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/infiniband/core/cma.c
+++ drivers/infiniband/core/cma.c
@@ -1856,6 +1856,7 @@ static void _destroy_id(struct rdma_id_private *id_priv,
 {
 	cma_cancel_operation(id_priv, state);
 
+	rdma_restrack_del(&id_priv->res);
 	if (id_priv->cma_dev) {
 		if (rdma_cap_ib_cm(id_priv->id.device, 1)) {
 			if (id_priv->cm_id.ib)
@@ -1865,7 +1866,6 @@ static void _destroy_id(struct rdma_id_private *id_priv,
 				iw_destroy_cm_id(id_priv->cm_id.iw);
 		}
 		cma_leave_mc_groups(id_priv);
-		rdma_restrack_del(&id_priv->res);
 		cma_release_dev(id_priv);
 	}
 
