a29a7506600d9511dc872a82a139dcfb71c49640
Mel Gorman <mgorman@techsingularity.net>
2021-06-30 18:53:41 -0700

    mm/zbud: add kerneldoc fields for zbud_pool
    make W=1 generates the following warning for zbud_pool
    
      mm/zbud.c:105: warning: Function parameter or member 'zpool' not described in 'zbud_pool'
      mm/zbud.c:105: warning: Function parameter or member 'zpool_ops' not described in 'zbud_pool'
    
    Commit 479305fd7172 ("zpool: remove zpool_evict()") removed the
    zpool_evict helper and added the associated zpool and operations structure
    in struct zbud_pool but did not add documentation for the fields.  Add
    rudimentary documentation.
    
    Link: https://lkml.kernel.org/r/20210520084809.8576-10-mgorman@techsingularity.net
    Fixes: 479305fd7172 ("zpool: remove zpool_evict()")
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
    Reviewed-by: Yang Shi <shy828301@gmail.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Dan Streetman <ddstreet@ieee.org>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/zbud.c
+++ mm/zbud.c
@@ -92,6 +92,8 @@ struct zbud_ops {
  * @pages_nr:	number of zbud pages in the pool.
  * @ops:	pointer to a structure of user defined operations specified at
  *		pool creation time.
+ * @zpool:	zpool driver
+ * @zpool_ops:	zpool operations structure with an evict callback
  *
  * This structure is allocated at pool creation time and maintains metadata
  * pertaining to a particular zbud pool.
