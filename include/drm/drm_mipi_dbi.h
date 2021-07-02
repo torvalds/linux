3f5aa5ac0b0f9704f0c60f5fbbbcdc8c043d6eb6
Linus Walleij <linus.walleij@linaro.org>
2021-07-02 15:56:01 +0200

    drm/dbi: Print errors for mipi_dbi_command()
    The macro mipi_dbi_command() does not report errors unless you wrap it
    in another macro to do the error reporting.
    
    Report a rate-limited error so we know what is going on.
    
    After this any code wishing to send command arrays can rely on
    mipi_dbi_command() providing an appropriate error message if something
    goes wrong.
    
    Suggested-by: Noralf Trønnes <noralf@tronnes.org>
    Suggested-by: Douglas Anderson <dianders@chromium.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Reviewed-by: Noralf Trønnes <noralf@tronnes.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210702135601.3952726-1-linus.walleij@linaro.org

--- include/drm/drm_mipi_dbi.h
+++ include/drm/drm_mipi_dbi.h
@@ -183,7 +183,12 @@ int mipi_dbi_buf_copy(void *dst, struct drm_framebuffer *fb,
 #define mipi_dbi_command(dbi, cmd, seq...) \
 ({ \
 	const u8 d[] = { seq }; \
-	mipi_dbi_command_stackbuf(dbi, cmd, d, ARRAY_SIZE(d)); \
+	struct device *dev = &(dbi)->spi->dev;	\
+	int ret; \
+	ret = mipi_dbi_command_stackbuf(dbi, cmd, d, ARRAY_SIZE(d)); \
+	if (ret) \
+		dev_err_ratelimited(dev, "error %d when sending command %#02x\n", ret, cmd); \
+	ret; \
 })
 
 #ifdef CONFIG_DEBUG_FS
