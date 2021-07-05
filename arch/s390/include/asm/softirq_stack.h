b8f4c89fd719289cee5634e0b1e918ab081ece35
Heiko Carstens <hca@linux.ibm.com>
2021-07-05 20:56:40 +0200

    s390/softirq: use call_on_stack() macro
    Reviewed-by: Sven Schnelle <svens@linux.ibm.com>
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>

--- arch/s390/include/asm/softirq_stack.h
+++ arch/s390/include/asm/softirq_stack.h
@@ -7,7 +7,7 @@
 
 static inline void do_softirq_own_stack(void)
 {
-	CALL_ON_STACK(__do_softirq, S390_lowcore.async_stack, 0);
+	call_on_stack(0, S390_lowcore.async_stack, void, __do_softirq);
 }
 
 #endif /* __ASM_S390_SOFTIRQ_STACK_H */
