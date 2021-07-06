9ca41d1fcf58d1eb3c058259e226fadc5ee07a87
Thomas Zimmermann <tzimmermann@suse.de>
2021-07-06 09:27:12 +0200

    drm/arm/komeda: Don't include drm_irq.h
    The header file is not required. Don't include it.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Acked-by: Liviu Dudau <liviu.dudau@arm.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210706072712.7558-1-tzimmermann@suse.de

--- drivers/gpu/drm/arm/display/komeda/komeda_kms.c
+++ drivers/gpu/drm/arm/display/komeda/komeda_kms.c
@@ -13,7 +13,6 @@
 #include <drm/drm_fb_helper.h>
 #include <drm/drm_gem_cma_helper.h>
 #include <drm/drm_gem_framebuffer_helper.h>
-#include <drm/drm_irq.h>
 #include <drm/drm_managed.h>
 #include <drm/drm_probe_helper.h>
 #include <drm/drm_vblank.h>
