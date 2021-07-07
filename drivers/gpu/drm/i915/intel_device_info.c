0f9b145a0a0ab0d3d4143c20e2ca347d8a105e33
Lucas De Marchi <lucas.demarchi@intel.com>
2021-07-07 16:59:20 -0700

    drm/i915: do not abbreviate version in debugfs
    Brevity is not needed here, so just spell out "* version" in the string.
    
    Suggested-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210707235921.2416911-1-lucas.demarchi@intel.com

--- drivers/gpu/drm/i915/intel_device_info.c
+++ drivers/gpu/drm/i915/intel_device_info.c
@@ -96,9 +96,9 @@ static const char *iommu_name(void)
 void intel_device_info_print_static(const struct intel_device_info *info,
 				    struct drm_printer *p)
 {
-	drm_printf(p, "graphics_ver: %u\n", info->graphics_ver);
-	drm_printf(p, "media_ver: %u\n", info->media_ver);
-	drm_printf(p, "display_ver: %u\n", info->display.ver);
+	drm_printf(p, "graphics version: %u\n", info->graphics_ver);
+	drm_printf(p, "media version: %u\n", info->media_ver);
+	drm_printf(p, "display version: %u\n", info->display.ver);
 	drm_printf(p, "gt: %d\n", info->gt);
 	drm_printf(p, "iommu: %s\n", iommu_name());
 	drm_printf(p, "memory-regions: %x\n", info->memory_regions);
