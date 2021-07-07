6407c69dc51fbd7cf7b6760cd8aefb105d96ff5b
Tatyana Nikolova <tatyana.e.nikolova@intel.com>
2021-07-07 14:14:55 -0700

    RDMA/irdma: Fix unused variable total_size warning
    Fix the following unused variable warning:
    
     drivers/infiniband/hw/irdma/uk.c:934:6: warning: variable 'total_size' set but not used [-Wunused-but-set-variable]
    
    Link: https://lore.kernel.org/r/20210707211455.2076-1-tatyana.e.nikolova@intel.com
    Link: https://lkml.org/lkml/2021/7/1/726
    Reported-by: kernel test robot <lkp@intel.com>
    Fixes: 551c46edc769 ("RDMA/irdma: Add user/kernel shared libraries")
    Signed-off-by: Mustafa Ismail <mustafa.ismail@intel.com>
    Signed-off-by: Tatyana Nikolova <tatyana.e.nikolova@intel.com>
    Reviewed-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/infiniband/hw/irdma/uk.c
+++ drivers/infiniband/hw/irdma/uk.c
@@ -931,16 +931,13 @@ enum irdma_status_code irdma_uk_mw_bind(struct irdma_qp_uk *qp,
 enum irdma_status_code irdma_uk_post_receive(struct irdma_qp_uk *qp,
 					     struct irdma_post_rq_info *info)
 {
-	u32 total_size = 0, wqe_idx, i, byte_off;
+	u32 wqe_idx, i, byte_off;
 	u32 addl_frag_cnt;
 	__le64 *wqe;
 	u64 hdr;
 
 	if (qp->max_rq_frag_cnt < info->num_sges)
 		return IRDMA_ERR_INVALID_FRAG_COUNT;
-
-	for (i = 0; i < info->num_sges; i++)
-		total_size += info->sg_list[i].len;
 
 	wqe = irdma_qp_get_next_recv_wqe(qp, &wqe_idx);
 	if (!wqe)
