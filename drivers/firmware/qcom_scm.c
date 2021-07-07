b42000e4b8741bf6591fcbbbefff6aa745f8eb29
John Stultz <john.stultz@linaro.org>
2021-07-07 04:53:20 +0000

    firmware: qcom_scm: Allow qcom_scm driver to be loadable as a permenent module
    Allow the qcom_scm driver to be loadable as a permenent module.
    
    This still uses the "depends on QCOM_SCM || !QCOM_SCM" bit to
    ensure that drivers that call into the qcom_scm driver are
    also built as modules. While not ideal in some cases its the
    only safe way I can find to avoid build errors without having
    those drivers select QCOM_SCM and have to force it on (as
    QCOM_SCM=n can be valid for those drivers).
    
    Reviving this now that Saravana's fw_devlink defaults to on,
    which should avoid loading troubles seen before.
    
    Acked-by: Kalle Valo <kvalo@codeaurora.org>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Acked-by: Will Deacon <will@kernel.org>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Link: https://lore.kernel.org/r/20210707045320.529186-1-john.stultz@linaro.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/firmware/qcom_scm.c
+++ drivers/firmware/qcom_scm.c
@@ -1299,6 +1299,7 @@ static const struct of_device_id qcom_scm_dt_match[] = {
 	{ .compatible = "qcom,scm" },
 	{}
 };
+MODULE_DEVICE_TABLE(of, qcom_scm_dt_match);
 
 static struct platform_driver qcom_scm_driver = {
 	.driver = {
@@ -1315,3 +1316,6 @@ static int __init qcom_scm_init(void)
 	return platform_driver_register(&qcom_scm_driver);
 }
 subsys_initcall(qcom_scm_init);
+
+MODULE_DESCRIPTION("Qualcomm Technologies, Inc. SCM driver");
+MODULE_LICENSE("GPL v2");
