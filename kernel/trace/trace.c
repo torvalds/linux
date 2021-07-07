370364351926e4fcc7c1a486901bfaae0172b7d9
Steven Rostedt (VMware) <rostedt@goodmis.org>
2021-07-07 17:36:25 -0400

    tracing/histogram: Update the documentation for the buckets modifier
    Update both the tracefs README file as well as the histogram.rst to
    include an explanation of what the buckets modifier is and how to use it.
    Include an example with the wakeup_latency example for both log2 and the
    buckets modifiers as there was no existing log2 example.
    
    Link: https://lkml.kernel.org/r/20210707213922.167218794@goodmis.org
    
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Reviewed-by: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -5654,6 +5654,7 @@ static const char readme_msg[] =
 	"\t            .execname   display a common_pid as a program name\n"
 	"\t            .syscall    display a syscall id as a syscall name\n"
 	"\t            .log2       display log2 value rather than raw number\n"
+	"\t            .buckets=size  display values in groups of size rather than raw number\n"
 	"\t            .usecs      display a common_timestamp in microseconds\n\n"
 	"\t    The 'pause' parameter can be used to pause an existing hist\n"
 	"\t    trigger or to start a hist trigger but not log any events\n"
