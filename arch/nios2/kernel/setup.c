4154267a14929b6254a7634ddf0bb392a18cfb46
Kefeng Wang <wangkefeng.wang@huawei.com>
2021-07-07 18:08:46 -0700

    nios2: convert to setup_initial_init_mm()
    Use setup_initial_init_mm() helper to simplify code.
    
    Link: https://lkml.kernel.org/r/20210608083418.137226-10-wangkefeng.wang@huawei.com
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Cc: Ley Foon Tan <ley.foon.tan@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/nios2/kernel/setup.c
+++ arch/nios2/kernel/setup.c
@@ -156,10 +156,7 @@ void __init setup_arch(char **cmdline_p)
 	memory_start = memblock_start_of_DRAM();
 	memory_end = memblock_end_of_DRAM();
 
-	init_mm.start_code = (unsigned long) _stext;
-	init_mm.end_code = (unsigned long) _etext;
-	init_mm.end_data = (unsigned long) _edata;
-	init_mm.brk = (unsigned long) _end;
+	setup_initial_init_mm(_stext, _etext, _edata, _end);
 	init_task.thread.kregs = &fake_regs;
 
 	/* Keep a copy of command line */
