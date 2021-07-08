d402c60da0fdd7e7e8f886c0213e8c928a58c793
Michal Suchanek <msuchanek@suse.de>
2021-07-08 11:46:54 +0200

    efi/tpm: Differentiate missing and invalid final event log table.
    [ Upstream commit 674a9f1f6815849bfb5bf385e7da8fc198aaaba9 ]
    
    Missing TPM final event log table is not a firmware bug.
    
    Clearly if providing event log in the old format makes the final event
    log invalid it should not be provided at least in that case.
    
    Fixes: b4f1874c6216 ("tpm: check event log version before reading final events")
    Signed-off-by: Michal Suchanek <msuchanek@suse.de>
    Reviewed-by: Jarkko Sakkinen <jarkko@kernel.org>
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/firmware/efi/tpm.c
+++ drivers/firmware/efi/tpm.c
@@ -62,9 +62,11 @@ int __init efi_tpm_eventlog_init(void)
 	tbl_size = sizeof(*log_tbl) + log_tbl->size;
 	memblock_reserve(efi.tpm_log, tbl_size);
 
-	if (efi.tpm_final_log == EFI_INVALID_TABLE_ADDR ||
-	    log_tbl->version != EFI_TCG2_EVENT_LOG_FORMAT_TCG_2) {
-		pr_warn(FW_BUG "TPM Final Events table missing or invalid\n");
+	if (efi.tpm_final_log == EFI_INVALID_TABLE_ADDR) {
+		pr_info("TPM Final Events table not present\n");
+		goto out;
+	} else if (log_tbl->version != EFI_TCG2_EVENT_LOG_FORMAT_TCG_2) {
+		pr_warn(FW_BUG "TPM Final Events table invalid\n");
 		goto out;
 	}
 
