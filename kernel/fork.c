9c2958f454f124b8b0016430c8285c794e4db55f
chunhui.li <chunhui.li@mediatek.com>
2021-07-08 18:57:06 +0800

    ANDROID: GKI: Export put_task_stack symbol
      We need dump task->stack in kernel module for debug usage,
    call try_get_task_stack to lock task->stack, and
    try_get_task_stack/put_task_stack should call in pairs,
    but put_task_stack is not exported
    
    Bug: 192990535
    Change-Id: Ifb2f3d16f93039bffeb3e822bc066e42e2d21d13
    Signed-off-by: chunhui.li <chunhui.li@mediatek.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -443,6 +443,7 @@ void put_task_stack(struct task_struct *tsk)
 	if (refcount_dec_and_test(&tsk->stack_refcount))
 		release_task_stack(tsk);
 }
+EXPORT_SYMBOL_GPL(put_task_stack);
 #endif
 
 void free_task(struct task_struct *tsk)
