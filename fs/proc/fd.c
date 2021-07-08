5ded961aa2678530dbde87438af6095f3a2a048c
Kalesh Singh <kaleshsingh@google.com>
2021-07-08 16:54:58 +0000

    Revert "FROMLIST: BACKPORT: procfs/dmabuf: Add inode number to /..."
    Revert submission 1578844
    
    Reason for revert: Will be replaced by upstream version
    Reverted Changes:
    Ic9c551998:FROMLIST: BACKPORT: procfs/dmabuf: Add inode numbe...
    I41407760c:FROMLIST: procfs: Allow reading fdinfo with PTRACE...
    
    Bug: 159126739
    Bug: 167141117
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    Change-Id: If02a6dc9a525193f286a138791de49085cd91972

--- fs/proc/fd.c
+++ fs/proc/fd.c
@@ -54,10 +54,9 @@ static int seq_show(struct seq_file *m, void *v)
 	if (ret)
 		return ret;
 
-	seq_printf(m, "pos:\t%lli\nflags:\t0%o\nmnt_id:\t%i\nino:\t%lu\n",
+	seq_printf(m, "pos:\t%lli\nflags:\t0%o\nmnt_id:\t%i\n",
 		   (long long)file->f_pos, f_flags,
-		   real_mount(file->f_path.mnt)->mnt_id,
-		   file_inode(file)->i_ino);
+		   real_mount(file->f_path.mnt)->mnt_id);
 
 	show_fd_locks(m, file, files);
 	if (seq_has_overflowed(m))
