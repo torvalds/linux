1b48773f9fd09f311d1166ce1dd50652ebe05218
Pavel Begunkov <asml.silence@gmail.com>
2021-07-11 22:41:13 +0100

    io_uring: fix io_drain_req()
    io_drain_req() return whether the request has been consumed or not, not
    an error code. Fix a stupid mistake slipped from optimisation patches.
    
    Reported-by: syzbot+ba6fcd859210f4e9e109@syzkaller.appspotmail.com
    Fixes: 76cc33d79175a ("io_uring: refactor io_req_defer()")
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/4d3c53c4274ffff307c8ae062fc7fda63b978df2.1626039606.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -6019,11 +6019,13 @@ static bool io_drain_req(struct io_kiocb *req)
 
 	ret = io_req_prep_async(req);
 	if (ret)
-		return ret;
+		goto fail;
 	io_prep_async_link(req);
 	de = kmalloc(sizeof(*de), GFP_KERNEL);
 	if (!de) {
-		io_req_complete_failed(req, -ENOMEM);
+		ret = -ENOMEM;
+fail:
+		io_req_complete_failed(req, ret);
 		return true;
 	}
 
