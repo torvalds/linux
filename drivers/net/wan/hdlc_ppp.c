a1739c307a072e46473a2ba239eb60e6d711c96c
Randy Dunlap <rdunlap@infradead.org>
2021-07-11 15:31:47 -0700

    net: hdlc: rename 'mod_init' & 'mod_exit' functions to be module-specific
    Rename module_init & module_exit functions that are named
    "mod_init" and "mod_exit" so that they are unique in both the
    System.map file and in initcall_debug output instead of showing
    up as almost anonymous "mod_init".
    
    This is helpful for debugging and in determining how long certain
    module_init calls take to execute.
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Cc: Krzysztof Halasa <khc@pm.waw.pl>
    Cc: netdev@vger.kernel.org
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Jakub Kicinski <kuba@kernel.org>
    Cc: Martin Schiller <ms@dev.tdt.de>
    Cc: linux-x25@vger.kernel.org
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/wan/hdlc_ppp.c
+++ drivers/net/wan/hdlc_ppp.c
@@ -705,20 +705,20 @@ static int ppp_ioctl(struct net_device *dev, struct ifreq *ifr)
 	return -EINVAL;
 }
 
-static int __init mod_init(void)
+static int __init hdlc_ppp_init(void)
 {
 	skb_queue_head_init(&tx_queue);
 	register_hdlc_protocol(&proto);
 	return 0;
 }
 
-static void __exit mod_exit(void)
+static void __exit hdlc_ppp_exit(void)
 {
 	unregister_hdlc_protocol(&proto);
 }
 
-module_init(mod_init);
-module_exit(mod_exit);
+module_init(hdlc_ppp_init);
+module_exit(hdlc_ppp_exit);
 
 MODULE_AUTHOR("Krzysztof Halasa <khc@pm.waw.pl>");
 MODULE_DESCRIPTION("PPP protocol support for generic HDLC");
