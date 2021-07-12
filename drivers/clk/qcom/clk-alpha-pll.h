f0e9ac4de03e5ce430075bcb4da04ed5aef7e7e5
Mike Tipton <mdtipton@codeaurora.org>
2021-07-12 11:06:04 -0700

    clk: qcom: clk-alpha-pll: Add support for rivian_ole
    Add support for the rivian_ole PLL.
    
    Change-Id: I245a31dde694d291e2ed30f750c877f8f6b3b0b8
    Signed-off-by: Mike Tipton <mdtipton@codeaurora.org>

--- drivers/clk/qcom/clk-alpha-pll.h
+++ drivers/clk/qcom/clk-alpha-pll.h
@@ -22,6 +22,7 @@ enum {
 	CLK_ALPHA_PLL_TYPE_LUCID_EVO,
 	CLK_ALPHA_PLL_TYPE_LUCID_OLE = CLK_ALPHA_PLL_TYPE_LUCID_EVO,
 	CLK_ALPHA_PLL_TYPE_RIVIAN_EVO,
+	CLK_ALPHA_PLL_TYPE_RIVIAN_OLE = CLK_ALPHA_PLL_TYPE_RIVIAN_EVO,
 	CLK_ALPHA_PLL_TYPE_MAX,
 };
 
@@ -177,6 +178,7 @@ extern const struct clk_ops clk_alpha_pll_postdiv_lucid_evo_ops;
 #define clk_alpha_pll_postdiv_lucid_ole_ops clk_alpha_pll_postdiv_lucid_evo_ops
 
 extern const struct clk_ops clk_alpha_pll_rivian_evo_ops;
+#define clk_alpha_pll_rivian_ole_ops clk_alpha_pll_rivian_evo_ops
 
 void clk_alpha_pll_configure(struct clk_alpha_pll *pll, struct regmap *regmap,
 			     const struct alpha_pll_config *config);
@@ -204,4 +206,6 @@ int clk_lucid_evo_pll_configure(struct clk_alpha_pll *pll,
 int clk_rivian_evo_pll_configure(struct clk_alpha_pll *pll,
 				struct regmap *regmap,
 				const struct alpha_pll_config *config);
+#define clk_rivian_ole_pll_configure clk_rivian_evo_pll_configure
+
 #endif
