b515937b414a5b1bbacd6cde1c1f4883808399e3
Tao Zhou <tao.zhou1@amd.com>
2021-07-13 17:45:40 -0400

    drm/amdgpu: add chip early init for cyan_skillfish
    Set cg/pg flags and rev id for cyan_skillfish.
    
    Signed-off-by: Tao Zhou <tao.zhou1@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/nv.c
+++ drivers/gpu/drm/amd/amdgpu/nv.c
@@ -1253,6 +1253,11 @@ static int nv_common_early_init(void *handle)
 		else
 			adev->external_rev_id = adev->rev_id + 0x01;
 		break;
+	case CHIP_CYAN_SKILLFISH:
+		adev->cg_flags = 0;
+		adev->pg_flags = 0;
+		adev->external_rev_id = adev->rev_id + 0x82;
+		break;
 	default:
 		/* FIXME: not supported yet */
 		return -EINVAL;
