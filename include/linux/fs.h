5662c967c69dfd162a0667d69bad776939bedf85
Josef Bacik <josef@toxicpanda.com>
2021-07-14 14:47:25 -0400

    fs: kill sync_inode
    Now that all users of sync_inode() have been deleted, remove
    sync_inode().
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Nikolay Borisov <nborisov@suse.com>
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2457,7 +2457,6 @@ static inline void file_accessed(struct file *file)
 
 extern int file_modified(struct file *file);
 
-int sync_inode(struct inode *inode, struct writeback_control *wbc);
 int sync_inode_metadata(struct inode *inode, int wait);
 
 struct file_system_type {
