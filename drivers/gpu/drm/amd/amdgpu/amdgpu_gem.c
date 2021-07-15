fa5239f2af983ffdf08395a542a7d6356b6222c5
Felix Kuehling <Felix.Kuehling@amd.com>
2021-07-15 15:05:37 -0400

    drm/amdgpu: workaround failed COW checks for Thunk VMAs
    KFD Thunk maps invisible VRAM BOs with PROT_NONE, MAP_PRIVATE.
    is_cow_mapping returns true for these mappings, which causes mmap to fail
    in ttm_bo_mmap_obj.
    
    As a workaround, clear VM_MAYWRITE for PROT_NONE-COW mappings. This
    should prevent the mapping from ever becoming writable and makes
    is_cow_mapping(vm_flags) false.
    
    Fixes: f91142c62161 ("drm/ttm: nuke VM_MIXEDMAP on BO mappings v3")
    Suggested-by: Daniel Vetter <daniel.vetter@intel.com>
    Tested-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210715190537.585456-1-Felix.Kuehling@amd.com
    Signed-off-by: Christian König <christian.koenig@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_gem.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_gem.c
@@ -255,6 +255,15 @@ static int amdgpu_gem_object_mmap(struct drm_gem_object *obj, struct vm_area_str
 	if (bo->flags & AMDGPU_GEM_CREATE_NO_CPU_ACCESS)
 		return -EPERM;
 
+	/* Workaround for Thunk bug creating PROT_NONE,MAP_PRIVATE mappings
+	 * for debugger access to invisible VRAM. Should have used MAP_SHARED
+	 * instead. Clearing VM_MAYWRITE prevents the mapping from ever
+	 * becoming writable and makes is_cow_mapping(vm_flags) false.
+	 */
+	if (is_cow_mapping(vma->vm_flags) &&
+	    !(vma->vm_flags & (VM_READ | VM_WRITE | VM_EXEC)))
+		vma->vm_flags &= ~VM_MAYWRITE;
+
 	return drm_gem_ttm_mmap(obj, vma);
 }
 
