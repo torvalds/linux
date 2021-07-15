86fc6ee6e246438d394e41bb7cc210b0fe724872
Alexei Starovoitov <ast@kernel.org>
2021-07-14 17:54:13 -0700

    bpf: Relax verifier recursion check.
    In the following bpf subprogram:
    static int timer_cb(void *map, void *key, void *value)
    {
        bpf_timer_set_callback(.., timer_cb);
    }
    
    the 'timer_cb' is a pointer to a function.
    ld_imm64 insn is used to carry this pointer.
    bpf_pseudo_func() returns true for such ld_imm64 insn.
    
    Unlike bpf_for_each_map_elem() the bpf_timer_set_callback() is asynchronous.
    Relax control flow check to allow such "recursion" that is seen as an infinite
    loop by check_cfg(). The distinction between bpf_for_each_map_elem() the
    bpf_timer_set_callback() is done in the follow up patch.
    
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Link: https://lore.kernel.org/bpf/20210715005417.78572-8-alexei.starovoitov@gmail.com

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -9463,8 +9463,12 @@ static int visit_func_call_insn(int t, int insn_cnt,
 		init_explored_state(env, t + 1);
 	if (visit_callee) {
 		init_explored_state(env, t);
-		ret = push_insn(t, t + insns[t].imm + 1, BRANCH,
-				env, false);
+		ret = push_insn(t, t + insns[t].imm + 1, BRANCH, env,
+				/* It's ok to allow recursion from CFG point of
+				 * view. __check_func_call() will do the actual
+				 * check.
+				 */
+				bpf_pseudo_func(insns + t));
 	}
 	return ret;
 }
