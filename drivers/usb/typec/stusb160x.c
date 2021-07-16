6b63376722d9e1b915a2948e9b30f4ba2712e3f5
Amelie Delaunay <amelie.delaunay@foss.st.com>
2021-07-16 14:07:18 +0200

    usb: typec: stusb160x: Don't block probing of consumer of "connector" nodes
    Similar as with tcpm this patch lets fw_devlink know not to wait on the
    fwnode to be populated as a struct device.
    
    Without this patch, USB functionality can be broken on some previously
    supported boards.
    
    Fixes: 28ec344bb891 ("usb: typec: tcpm: Don't block probing of consumers of "connector" nodes")
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Amelie Delaunay <amelie.delaunay@foss.st.com>
    Link: https://lore.kernel.org/r/20210716120718.20398-3-amelie.delaunay@foss.st.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/typec/stusb160x.c
+++ drivers/usb/typec/stusb160x.c
@@ -686,6 +686,15 @@ static int stusb160x_probe(struct i2c_client *client)
 		return -ENODEV;
 
 	/*
+	 * This fwnode has a "compatible" property, but is never populated as a
+	 * struct device. Instead we simply parse it to read the properties.
+	 * This it breaks fw_devlink=on. To maintain backward compatibility
+	 * with existing DT files, we work around this by deleting any
+	 * fwnode_links to/from this fwnode.
+	 */
+	fw_devlink_purge_absent_suppliers(fwnode);
+
+	/*
 	 * When both VDD and VSYS power supplies are present, the low power
 	 * supply VSYS is selected when VSYS voltage is above 3.1 V.
 	 * Otherwise VDD is selected.
