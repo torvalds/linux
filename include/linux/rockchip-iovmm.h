eb3fa68f91eed25e58bbc55ec710f2e761cf8eb2
Tao Huang <huangtao@rock-chips.com>
2021-07-19 11:20:25 +0800

    iommu: remove unused rk-iommu/iovmm driver
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>
    Change-Id: Ieca1544f10c294303d0d40c6b4c5e1a797e577a2

--- include/linux/rockchip-iovmm.h
+++ include/linux/rockchip-iovmm.h
@@ -50,69 +50,6 @@ typedef int (*rockchip_iommu_fault_handler_t)(struct device *dev,
 struct scatterlist;
 struct device;
 
-#ifdef CONFIG_RK_IOVMM
-
-int rockchip_iovmm_activate(struct device *dev);
-void rockchip_iovmm_deactivate(struct device *dev);
-
-/* rockchip_iovmm_map() - Maps a list of physical memory chunks
- * @dev: the owner of the IO address space where the mapping is created
- * @sg: list of physical memory chunks to map
- * @offset: length in bytes where the mapping starts
- * @size: how much memory to map in bytes. @offset + @size must not exceed
- *        total size of @sg
- *
- * This function returns mapped IO address in the address space of @dev.
- * Returns minus error number if mapping fails.
- * Caller must check its return code with IS_ERROR_VALUE() if the function
- * succeeded.
- *
- * The caller of this function must ensure that iovmm_cleanup() is not called
- * while this function is called.
- *
- */
-dma_addr_t rockchip_iovmm_map(struct device *dev, struct scatterlist *sg,
-		     off_t offset, size_t size);
-
-/* rockchip_iovmm_unmap() - unmaps the given IO address
- * @dev: the owner of the IO address space where @iova belongs
- * @iova: IO address that needs to be unmapped and freed.
- *
- * The caller of this function must ensure that iovmm_cleanup() is not called
- * while this function is called.
- */
-void rockchip_iovmm_unmap(struct device *dev, dma_addr_t iova);
-
-/* rockchip_iovmm_map_oto - create one to one mapping for the given physical address
- * @dev: the owner of the IO address space to map
- * @phys: physical address to map
- * @size: size of the mapping to create
- *
- * This function return 0 if mapping is successful. Otherwise, minus error
- * value.
- */
-int rockchip_iovmm_map_oto(struct device *dev, phys_addr_t phys, size_t size);
-
-/* rockchip_iovmm_unmap_oto - remove one to one mapping
- * @dev: the owner ofthe IO address space
- * @phys: physical address to remove mapping
- */
-void rockchip_iovmm_unmap_oto(struct device *dev, phys_addr_t phys);
-
-void rockchip_iovmm_set_fault_handler(struct device *dev,
-				       rockchip_iommu_fault_handler_t handler);
-/** rockchip_iovmm_set_fault_handler() - Fault handler for IOMMUs
- * Called when interrupt occurred by the IOMMUs
- * The device drivers of peripheral devices that has a IOMMU can implement
- * a fault handler to resolve address translation fault by IOMMU.
- * The meanings of return value and parameters are described below.
- *
- * return value: non-zero if the fault is correctly resolved.
- *		   zero if the fault is not handled.
- */
-
-int rockchip_iovmm_invalidate_tlb(struct device *dev);
-#else
 static inline int rockchip_iovmm_activate(struct device *dev)
 {
 	return -ENOSYS;
@@ -150,7 +87,5 @@ static inline int rockchip_iovmm_invalidate_tlb(struct device *dev)
 {
 	return -ENOSYS;
 }
-
-#endif /* CONFIG_RK_IOVMM */
 
 #endif /*__ASM_PLAT_IOVMM_H*/
