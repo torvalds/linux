d77e37063bcee42fac93a1d9fb832493a5efd4c1
Vandita Kulkarni <vandita.kulkarni@intel.com>
2021-07-20 12:19:05 +0530

    drm/i915/display: Add write permissions for fec support
    Though there is a write option available on  fec_suport
    debugfs file, so far it has been registering with read
    permissions only.
    
    Suggested-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Vandita Kulkarni <vandita.kulkarni@intel.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210720064907.9771-2-vandita.kulkarni@intel.com

--- drivers/gpu/drm/i915/display/intel_display_debugfs.c
+++ drivers/gpu/drm/i915/display/intel_display_debugfs.c
@@ -2434,7 +2434,7 @@ int intel_connector_debugfs_add(struct drm_connector *connector)
 	}
 
 	if ((DISPLAY_VER(dev_priv) >= 11 || IS_CANNONLAKE(dev_priv)) && ((connector->connector_type == DRM_MODE_CONNECTOR_DisplayPort && !to_intel_connector(connector)->mst_port) || connector->connector_type == DRM_MODE_CONNECTOR_eDP))
-		debugfs_create_file("i915_dsc_fec_support", S_IRUGO, root,
+		debugfs_create_file("i915_dsc_fec_support", 0644, root,
 				    connector, &i915_dsc_fec_support_fops);
 
 	/* Legacy panels doesn't lpsp on any platform */
