e59ba88fc6eed9a3bd76f5e870b532eb1e235f63
Tao Huang <huangtao@rock-chips.com>
2021-07-21 18:39:56 +0800

    soc: rockchip: cpuinfo: system_serial_high/low depends on CONFIG_NO_GKI
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>
    Change-Id: I14ff92773131360d933c288b82894a2f41a0c2ef

--- drivers/soc/rockchip/rockchip-cpuinfo.c
+++ drivers/soc/rockchip/rockchip-cpuinfo.c
@@ -81,12 +81,15 @@ static int rockchip_cpuinfo_probe(struct platform_device *pdev)
 
 	kfree(efuse_buf);
 
+	dev_info(dev, "SoC\t\t: %lx\n", rockchip_soc_id);
+
+#ifdef CONFIG_NO_GKI
 	system_serial_low = crc32(0, buf, 8);
 	system_serial_high = crc32(system_serial_low, buf + 8, 8);
 
-	dev_info(dev, "SoC\t\t: %lx\n", rockchip_soc_id);
 	dev_info(dev, "Serial\t\t: %08x%08x\n",
 		 system_serial_high, system_serial_low);
+#endif
 
 	return 0;
 }
