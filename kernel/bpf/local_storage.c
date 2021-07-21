724f17b7d45d62c71e92471666647a823cb9baa9
Colin Ian King <colin.king@canonical.com>
2021-07-21 12:56:30 +0100

    bpf: Remove redundant intiialization of variable stype
    The variable stype is being initialized with a value that is never
    read, it is being updated later on. The assignment is redundant and
    can be removed.
    
    Addresses-Coverity: ("Unused value")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20210721115630.109279-1-colin.king@canonical.com

--- kernel/bpf/local_storage.c
+++ kernel/bpf/local_storage.c
@@ -406,7 +406,7 @@ static int cgroup_storage_check_btf(const struct bpf_map *map,
 static void cgroup_storage_seq_show_elem(struct bpf_map *map, void *key,
 					 struct seq_file *m)
 {
-	enum bpf_cgroup_storage_type stype = cgroup_storage_type(map);
+	enum bpf_cgroup_storage_type stype;
 	struct bpf_cgroup_storage *storage;
 	int cpu;
 
