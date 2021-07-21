bfccd9a71a08627bcccd40277967516ddd9d5db2
Bjorn Andersson <bjorn.andersson@linaro.org>
2021-07-21 09:30:29 -0700

    phy: qcom-qmp: Fix sc8180x PCIe definition
    A copy paste error was snuck into the patch going upstream that made the
    SC8180x PCIe PHY use the SM8250 serdes table, but while this works
    there's some differences in the tables (and the SC8180x was left
    dangling). So correct the SC8180x definition to use the SC8180x serdes
    table.
    
    Fixes: f839f14e24f2 ("phy: qcom-qmp: Add sc8180x PCIe support")
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Link: https://lore.kernel.org/r/20210721163029.2813497-1-bjorn.andersson@linaro.org
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/phy/qualcomm/phy-qcom-qmp.c
+++ drivers/phy/qualcomm/phy-qcom-qmp.c
@@ -3510,7 +3510,7 @@ static const struct qmp_phy_cfg sc8180x_pciephy_cfg = {
 	.type = PHY_TYPE_PCIE,
 	.nlanes = 1,
 
-	.serdes_tbl		= sm8250_qmp_pcie_serdes_tbl,
+	.serdes_tbl		= sc8180x_qmp_pcie_serdes_tbl,
 	.serdes_tbl_num		= ARRAY_SIZE(sm8250_qmp_pcie_serdes_tbl),
 	.tx_tbl			= sc8180x_qmp_pcie_tx_tbl,
 	.tx_tbl_num		= ARRAY_SIZE(sc8180x_qmp_pcie_tx_tbl),
