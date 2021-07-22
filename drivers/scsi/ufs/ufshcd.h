3586ade72bf40b04171c91897e6df22b91e25bb3
Bart Van Assche <bvanassche@acm.org>
2021-07-21 20:34:33 -0700

    BACKPORT: scsi: ufs: Optimize serialization of setup_xfer_req() calls
    Reduce the number of times the host lock is taken in the hot path.
    Additionally, inline ufshcd_vops_setup_xfer_req() because that function is
    too short to keep it.
    
    Bug: 254441685
    Link: https://lore.kernel.org/r/20210722033439.26550-13-bvanassche@acm.org
    Fixes: a45f937110fa ("scsi: ufs: Optimize host lock on transfer requests send/compl paths")
    Cc: Jaegeuk Kim <jaegeuk@kernel.org>
    Cc: Stanley Chu <stanley.chu@mediatek.com>
    Cc: Can Guo <cang@codeaurora.org>
    Cc: Bean Huo <beanhuo@micron.com>
    Cc: Asutosh Das <asutoshd@codeaurora.org>
    Reviewed-by: Daejun Park <daejun7.park@samsung.com>
    Reviewed-by: Bean Huo <beanhuo@micron.com>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit a024ad0d49550e4ea1156e677cd9382e1eefbdd7)
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: I84662337751a1930c34c32b1aba963652b54347c

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -1304,18 +1304,6 @@ static inline int ufshcd_vops_pwr_change_notify(struct ufs_hba *hba,
 	return -ENOTSUPP;
 }
 
-static inline void ufshcd_vops_setup_xfer_req(struct ufs_hba *hba, int tag,
-					bool is_scsi_cmd)
-{
-	if (hba->vops && hba->vops->setup_xfer_req) {
-		unsigned long flags;
-
-		spin_lock_irqsave(hba->host->host_lock, flags);
-		hba->vops->setup_xfer_req(hba, tag, is_scsi_cmd);
-		spin_unlock_irqrestore(hba->host->host_lock, flags);
-	}
-}
-
 static inline void ufshcd_vops_setup_task_mgmt(struct ufs_hba *hba,
 					int tag, u8 tm_function)
 {
