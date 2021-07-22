21a64910997e0c1d268bebf9b1217ba5804d592d
Steve French <stfrench@microsoft.com>
2021-07-22 13:50:41 -0500

    CIFS: Clarify SMB1 code for POSIX Create
    Coverity also complains about the way we calculate the offset
    (starting from the address of a 4 byte array within the
    header structure rather than from the beginning of the struct
    plus 4 bytes) for SMB1 CIFSPOSIXCreate. This changeset
    doesn't change the address but makes it slightly clearer.
    
    Addresses-Coverity: 711518 ("Out of bounds write")
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/cifs/cifssmb.c
+++ fs/cifs/cifssmb.c
@@ -1081,7 +1081,8 @@ PsxCreat:
 	param_offset = offsetof(struct smb_com_transaction2_spi_req,
 				InformationLevel) - 4;
 	offset = param_offset + params;
-	pdata = (OPEN_PSX_REQ *)(((char *)&pSMB->hdr.Protocol) + offset);
+	/* SMB offsets are from the beginning of SMB which is 4 bytes in, after RFC1001 field */
+	pdata = (OPEN_PSX_REQ *)((char *)(pSMB) + offset + 4);
 	pdata->Level = cpu_to_le16(SMB_QUERY_FILE_UNIX_BASIC);
 	pdata->Permissions = cpu_to_le64(mode);
 	pdata->PosixOpenFlags = cpu_to_le32(posix_flags);
