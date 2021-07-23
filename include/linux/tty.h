8d29e0024437ae9184ae24f817ef0fda80b8cd3c
Jiri Slaby <jslaby@suse.cz>
2021-07-23 12:31:45 +0200

    tty: move tty_buffer definitions to new tty_buffer.h
    tty.h is large enough currently. And I am slowly adding kernel-doc
    documentation, so it grows to unmaintainable long mess. To avoid this,
    split tty.h further into tty_buffer.h and move there tty_buffer-related
    declarations and function prototypes.
    
    Note that many of the tty_buffer.c function prototypes reside now in
    tty_flip.h. But we cannot move struct tty_buffer & friends because:
    * tty_insert_flip_char() in tty_flip.h needs both struct tty_port and
      struct tty_buffer defined.
    * struct tty_port in tty_port.h needs struct tty_buffer defined.
    
    So if we moved struct tty_buffer to tty_flip.h too, tty_flip.h would
    need tty_port.h and that would need tty_flip.h (to have tty_buffer)
    again. Hence we introduce new header tty_buffer.h here to break this
    circular dependency.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Link: https://lore.kernel.org/r/20210723103147.18250-7-jslaby@suse.cz
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/tty.h
+++ include/linux/tty.h
@@ -6,6 +6,7 @@
 #include <linux/major.h>
 #include <linux/termios.h>
 #include <linux/workqueue.h>
+#include <linux/tty_buffer.h>
 #include <linux/tty_driver.h>
 #include <linux/tty_ldisc.h>
 #include <linux/mutex.h>
@@ -29,54 +30,6 @@
  * isn't in use (eg VINTR has no character etc)
  */
 #define __DISABLED_CHAR '\0'
-
-struct tty_buffer {
-	union {
-		struct tty_buffer *next;
-		struct llist_node free;
-	};
-	int used;
-	int size;
-	int commit;
-	int read;
-	int flags;
-	/* Data points here */
-	unsigned long data[];
-};
-
-/* Values for .flags field of tty_buffer */
-#define TTYB_NORMAL	1	/* buffer has no flags buffer */
-
-static inline unsigned char *char_buf_ptr(struct tty_buffer *b, int ofs)
-{
-	return ((unsigned char *)b->data) + ofs;
-}
-
-static inline char *flag_buf_ptr(struct tty_buffer *b, int ofs)
-{
-	return (char *)char_buf_ptr(b, ofs) + b->size;
-}
-
-struct tty_bufhead {
-	struct tty_buffer *head;	/* Queue head */
-	struct work_struct work;
-	struct mutex	   lock;
-	atomic_t	   priority;
-	struct tty_buffer sentinel;
-	struct llist_head free;		/* Free queue head */
-	atomic_t	   mem_used;    /* In-use buffers excluding free list */
-	int		   mem_limit;
-	struct tty_buffer *tail;	/* Active buffer */
-};
-/*
- * When a break, frame error, or parity error happens, these codes are
- * stuffed into the flags buffer.
- */
-#define TTY_NORMAL	0
-#define TTY_BREAK	1
-#define TTY_FRAME	2
-#define TTY_PARITY	3
-#define TTY_OVERRUN	4
 
 #define INTR_CHAR(tty) ((tty)->termios.c_cc[VINTR])
 #define QUIT_CHAR(tty) ((tty)->termios.c_cc[VQUIT])
