d66cd5dea551e974580a64bf80b337b9a09ce63e
Bjorn Andersson <bjorn.andersson@linaro.org>
2021-07-24 20:02:14 -0700

    cpufreq: blacklist Qualcomm sc8180x in cpufreq-dt-platdev
    The Qualcomm SC8180x platform uses the qcom-cpufreq-hw driver, so
    it in the cpufreq-dt-platdev driver's blocklist.
    
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -139,6 +139,7 @@ static const struct of_device_id blocklist[] __initconst = {
 	{ .compatible = "qcom,qcs404", },
 	{ .compatible = "qcom,sc7180", },
 	{ .compatible = "qcom,sc7280", },
+	{ .compatible = "qcom,sc8180x", },
 	{ .compatible = "qcom,sdm845", },
 
 	{ .compatible = "st,stih407", },
