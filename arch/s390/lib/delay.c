3da77cf33cf8caf60d5cf29987885abb997a38fa
Heiko Carstens <hca@linux.ibm.com>
2021-07-25 15:13:12 +0200

    s390/delay: get rid of not needed header includes
    After all the changes to delay.c there are many includes which are not
    needed anymore. Get rid of them.
    
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- arch/s390/lib/delay.c
+++ arch/s390/lib/delay.c
@@ -7,17 +7,10 @@
  *		 Heiko Carstens <heiko.carstens@de.ibm.com>,
  */
 
-#include <linux/sched.h>
+#include <linux/processor.h>
 #include <linux/delay.h>
-#include <linux/timex.h>
-#include <linux/export.h>
-#include <linux/irqflags.h>
-#include <linux/interrupt.h>
-#include <linux/jump_label.h>
-#include <linux/irq.h>
-#include <asm/vtimer.h>
 #include <asm/div64.h>
-#include <asm/idle.h>
+#include <asm/timex.h>
 
 void __delay(unsigned long loops)
 {
