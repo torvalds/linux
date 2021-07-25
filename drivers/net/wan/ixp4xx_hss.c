af996031e1545c47423dfdd024840702ceb5a26c
Jason Wang <wangborong@cdjrlc.com>
2021-07-25 22:42:21 +0800

    net: ixp4xx_hss: use dma_pool_zalloc
    The dma_pool_zalloc combines dma_pool_alloc/memset. Therefore, the
    dma_pool_alloc/memset can be replaced with dma_pool_zalloc which is
    more compact.
    
    Signed-off-by: Jason Wang <wangborong@cdjrlc.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/wan/ixp4xx_hss.c
+++ drivers/net/wan/ixp4xx_hss.c
@@ -975,11 +975,10 @@ static int init_hdlc_queues(struct port *port)
 			return -ENOMEM;
 	}
 
-	port->desc_tab = dma_pool_alloc(dma_pool, GFP_KERNEL,
+	port->desc_tab = dma_pool_zalloc(dma_pool, GFP_KERNEL,
 					&port->desc_tab_phys);
 	if (!port->desc_tab)
 		return -ENOMEM;
-	memset(port->desc_tab, 0, POOL_ALLOC_SIZE);
 	memset(port->rx_buff_tab, 0, sizeof(port->rx_buff_tab)); /* tables */
 	memset(port->tx_buff_tab, 0, sizeof(port->tx_buff_tab));
 
