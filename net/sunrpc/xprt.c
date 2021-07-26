f99fa50880f5300fbbb3c0754ddc7f8738d24fe7
Trond Myklebust <trond.myklebust@hammerspace.com>
2021-07-26 08:03:12 -0400

    SUNRPC/xprtrdma: Fix reconnection locking
    The xprtrdma client code currently relies on the task that initiated the
    connect to hold the XPRT_LOCK for the duration of the connection
    attempt. If the task is woken early, due to some other event, then that
    lock could get released early.
    Avoid races by using the same mechanism that the socket code uses of
    transferring lock ownership to the RDMA connect worker itself. That
    frees us to call rpcrdma_xprt_disconnect() directly since we're now
    guaranteed exclusion w.r.t. other callers.
    
    Fixes: 4cf44be6f1e8 ("xprtrdma: Fix recursion into rpcrdma_xprt_disconnect()")
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>

--- net/sunrpc/xprt.c
+++ net/sunrpc/xprt.c
@@ -877,6 +877,7 @@ out:
 	spin_unlock(&xprt->transport_lock);
 	return ret;
 }
+EXPORT_SYMBOL_GPL(xprt_lock_connect);
 
 void xprt_unlock_connect(struct rpc_xprt *xprt, void *cookie)
 {
@@ -893,6 +894,7 @@ out:
 	spin_unlock(&xprt->transport_lock);
 	wake_up_bit(&xprt->state, XPRT_LOCKED);
 }
+EXPORT_SYMBOL_GPL(xprt_unlock_connect);
 
 /**
  * xprt_connect - schedule a transport connect operation
