827f3164aaa579eee6fd50c6654861d54f282a11
Takashi Iwai <tiwai@suse.de>
2021-07-28 13:23:52 +0200

    ASoC: uniphier: Fix reference to PCM buffer address
    Along with the transition to the managed PCM buffers, the driver now
    accepts the dynamically allocated buffer, while it still kept the
    reference to the old preallocated buffer address.  This patch corrects
    to the right reference via runtime->dma_addr.
    
    (Although this might have been already buggy before the cleanup with
    the managed buffer, let's put Fixes tag to point that; it's a corner
    case, after all.)
    
    Fixes: d55894bc2763 ("ASoC: uniphier: Use managed buffer allocation")
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Link: https://lore.kernel.org/r/20210728112353.6675-5-tiwai@suse.de
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/uniphier/aio-dma.c
+++ sound/soc/uniphier/aio-dma.c
@@ -198,7 +198,7 @@ static int uniphier_aiodma_mmap(struct snd_soc_component *component,
 	vma->vm_page_prot = pgprot_writecombine(vma->vm_page_prot);
 
 	return remap_pfn_range(vma, vma->vm_start,
-			       substream->dma_buffer.addr >> PAGE_SHIFT,
+			       substream->runtime->dma_addr >> PAGE_SHIFT,
 			       vma->vm_end - vma->vm_start, vma->vm_page_prot);
 }
 
