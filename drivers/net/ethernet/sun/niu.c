451395f798a3f0a9f257338bc1fbce9a1d6eaf52
Martin Kaiser <martin@kaiser.cx>
2021-07-29 09:43:54 +0200

    niu: read property length only if we use it
    In three places, the driver calls of_get_property and reads the property
    length although the length is not used. Update the calls to not request
    the length.
    
    Signed-off-by: Martin Kaiser <martin@kaiser.cx>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/sun/niu.c
+++ drivers/net/ethernet/sun/niu.c
@@ -9208,7 +9208,7 @@ static int niu_get_of_props(struct niu *np)
 	else
 		dp = pci_device_to_OF_node(np->pdev);
 
-	phy_type = of_get_property(dp, "phy-type", &prop_len);
+	phy_type = of_get_property(dp, "phy-type", NULL);
 	if (!phy_type) {
 		netdev_err(dev, "%pOF: OF node lacks phy-type property\n", dp);
 		return -EINVAL;
@@ -9242,12 +9242,12 @@ static int niu_get_of_props(struct niu *np)
 		return -EINVAL;
 	}
 
-	model = of_get_property(dp, "model", &prop_len);
+	model = of_get_property(dp, "model", NULL);
 
 	if (model)
 		strcpy(np->vpd.model, model);
 
-	if (of_find_property(dp, "hot-swappable-phy", &prop_len)) {
+	if (of_find_property(dp, "hot-swappable-phy", NULL)) {
 		np->flags |= (NIU_FLAGS_10G | NIU_FLAGS_FIBER |
 			NIU_FLAGS_HOTPLUG_PHY);
 	}
