4e5d74fc6b044c4b0fa5b661e0a8652f08743223
Huiquan Deng <denghuiquan@cdjrlc.com>
2021-07-28 23:39:10 -0700

    auxdisplay: cfag12864bfb: code indent should use tabs where possible
    Resolves the checkpatch error.
    
    Signed-off-by: Huiquan Deng <denghuiquan@cdjrlc.com>
    [reworded and avoid moving the line]
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- drivers/auxdisplay/cfag12864bfb.c
+++ drivers/auxdisplay/cfag12864bfb.c
@@ -38,8 +38,8 @@ static const struct fb_var_screeninfo cfag12864bfb_var = {
 	.yres_virtual = CFAG12864B_HEIGHT,
 	.bits_per_pixel = 1,
 	.red = { 0, 1, 0 },
-      	.green = { 0, 1, 0 },
-      	.blue = { 0, 1, 0 },
+	.green = { 0, 1, 0 },
+	.blue = { 0, 1, 0 },
 	.left_margin = 0,
 	.right_margin = 0,
 	.upper_margin = 0,
@@ -67,7 +67,7 @@ static const struct fb_ops cfag12864bfb_ops = {
 static int cfag12864bfb_probe(struct platform_device *device)
 {
 	int ret = -EINVAL;
- 	struct fb_info *info = framebuffer_alloc(0, &device->dev);
+	struct fb_info *info = framebuffer_alloc(0, &device->dev);
 
 	if (!info)
 		goto none;
