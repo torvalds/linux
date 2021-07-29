d521be8ed93b89364c9334184a43af6a2a759165
Larry Finger <Larry.Finger@lwfinger.net>
2021-07-29 11:48:10 -0500

    staging: r8188eu: Convert header copyright info to SPDX format, part 2
    Before this driver can be incorporated in the drivers/net/wireless tree,
    the copyright info in all files must be converted to SPDX notation.
    This patch converts the next 23 files.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Link: https://lore.kernel.org/r/20210729164814.32097-3-Larry.Finger@lwfinger.net
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/r8188eu/include/odm_reg.h
+++ drivers/staging/r8188eu/include/odm_reg.h
@@ -1,37 +1,10 @@
-/******************************************************************************
- *
- * Copyright(c) 2007 - 2011 Realtek Corporation. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
- *
- *
- ******************************************************************************/
-/*  */
-/*  File Name: odm_reg.h */
-/*  */
-/*  Description: */
-/*  */
-/*  This file is for general register definition. */
-/*  */
-/*  */
-/*  */
+/* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause */
+/* Copyright(c) 2007 - 2011 Realtek Corporation. i*/
+
 #ifndef	__HAL_ODM_REG_H__
 #define __HAL_ODM_REG_H__
 
-/*  */
 /*  Register Definition */
-/*  */
 
 /* MAC REG */
 #define	ODM_BB_RESET					0x002
