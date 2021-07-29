60fc63981693f807baa0e404104dedea0e8b4e61
Jeremy Kerr <jk@codeconstruct.com.au>
2021-07-29 10:20:42 +0800

    mctp: Add sockaddr_mctp to uapi
    This change introduces the user-visible MCTP header, containing the
    protocol-specific addressing definitions.
    
    Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/uapi/linux/mctp.h
+++ include/uapi/linux/mctp.h
@@ -9,7 +9,28 @@
 #ifndef __UAPI_MCTP_H
 #define __UAPI_MCTP_H
 
-struct sockaddr_mctp {
+#include <linux/types.h>
+
+typedef __u8			mctp_eid_t;
+
+struct mctp_addr {
+	mctp_eid_t		s_addr;
 };
+
+struct sockaddr_mctp {
+	unsigned short int	smctp_family;
+	int			smctp_network;
+	struct mctp_addr	smctp_addr;
+	__u8			smctp_type;
+	__u8			smctp_tag;
+};
+
+#define MCTP_NET_ANY		0x0
+
+#define MCTP_ADDR_NULL		0x00
+#define MCTP_ADDR_ANY		0xff
+
+#define MCTP_TAG_MASK		0x07
+#define MCTP_TAG_OWNER		0x08
 
 #endif /* __UAPI_MCTP_H */
