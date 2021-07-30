2127cd21fb78c6e22d92944253afd967b0ff774d
Colin Ian King <colin.king@canonical.com>
2021-07-30 10:50:31 +0100

    scsi: BusLogic: Use %X for u32 sized integer rather than %lX
    An earlier fix changed the print format specifier for adapter->bios_addr to
    use %lX. However, the integer is a u32 so the fix was wrong. Fix this by
    using the correct %X format specifier.
    
    Link: https://lore.kernel.org/r/20210730095031.26981-1-colin.king@canonical.com
    Fixes: 43622697117c ("scsi: BusLogic: use %lX for unsigned long rather than %X")
    Acked-by: Khalid Aziz <khalid@gonehiking.org>
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Addresses-Coverity: ("Invalid type in argument")

--- drivers/scsi/BusLogic.c
+++ drivers/scsi/BusLogic.c
@@ -1711,7 +1711,7 @@ static bool __init blogic_reportconfig(struct blogic_adapter *adapter)
 	if (adapter->adapter_bus_type != BLOGIC_PCI_BUS) {
 		blogic_info("  DMA Channel: None, ", adapter);
 		if (adapter->bios_addr > 0)
-			blogic_info("BIOS Address: 0x%lX, ", adapter,
+			blogic_info("BIOS Address: 0x%X, ", adapter,
 					adapter->bios_addr);
 		else
 			blogic_info("BIOS Address: None, ", adapter);
