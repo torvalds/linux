f5714bbb5b3120b33dfbf3d81ffc0b98ae4cd4c1
Vitaly Kuznetsov <vkuznets@redhat.com>
2021-07-30 14:26:23 +0200

    KVM: x86: Introduce trace_kvm_hv_hypercall_done()
    Hypercall failures are unusual with potentially far going consequences
    so it would be useful to see their results when tracing.
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Reviewed-by: Siddharth Chandrasekaran <sidcha@amazon.de>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <20210730122625.112848-3-vkuznets@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/hyperv.c
+++ arch/x86/kvm/hyperv.c
@@ -2016,6 +2016,7 @@ static void kvm_hv_hypercall_set_result(struct kvm_vcpu *vcpu, u64 result)
 
 static int kvm_hv_hypercall_complete(struct kvm_vcpu *vcpu, u64 result)
 {
+	trace_kvm_hv_hypercall_done(result);
 	kvm_hv_hypercall_set_result(vcpu, result);
 	++vcpu->stat.hypercalls;
 	return kvm_skip_emulated_instruction(vcpu);
