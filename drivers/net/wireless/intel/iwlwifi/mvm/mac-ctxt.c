0f5d44ac6e55551798dd3da0ff847c8df5990822
Zhang Qilong <zhangqilong3@huawei.com>
2021-08-02 17:28:24 +0300

    iwlwifi: mvm: fix a memory leak in iwl_mvm_mac_ctxt_beacon_changed
    If beacon_inject_active is true, we will return without freeing
    beacon.  Fid that by freeing it before returning.
    
    Signed-off-by: Zhang Qilong <zhangqilong3@huawei.com>
    [reworded the commit message]
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Link: https://lore.kernel.org/r/iwlwifi.20210802172232.d16206ca60fc.I9984a9b442c84814c307cee3213044e24d26f38a@changeid
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/mvm/mac-ctxt.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/mac-ctxt.c
@@ -1008,8 +1008,10 @@ int iwl_mvm_mac_ctxt_beacon_changed(struct iwl_mvm *mvm,
 		return -ENOMEM;
 
 #ifdef CONFIG_IWLWIFI_DEBUGFS
-	if (mvm->beacon_inject_active)
+	if (mvm->beacon_inject_active) {
+		dev_kfree_skb(beacon);
 		return -EBUSY;
+	}
 #endif
 
 	ret = iwl_mvm_mac_ctxt_send_beacon(mvm, vif, beacon);
