560c71d4250f5f32b7952c290ddaf3cd4548a3ec
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2021-08-03 16:16:00 +0200

    platform/x86: Replace deprecated CPU-hotplug functions.
    The functions get_online_cpus() and put_online_cpus() have been
    deprecated during the CPU hotplug rework. They map directly to
    cpus_read_lock() and cpus_read_unlock().
    
    Replace deprecated CPU-hotplug functions with the official version.
    The behavior remains unchanged.
    
    Cc: Stuart Hayes <stuart.w.hayes@gmail.com>
    Cc: Hans de Goede <hdegoede@redhat.com>
    Cc: Mark Gross <mgross@linux.intel.com>
    Cc: platform-driver-x86@vger.kernel.org
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Link: https://lore.kernel.org/r/20210803141621.780504-18-bigeasy@linutronix.de
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/platform/x86/dell/dcdbas.c
+++ drivers/platform/x86/dell/dcdbas.c
@@ -278,9 +278,9 @@ int dcdbas_smi_request(struct smi_cmd *smi_cmd)
 	}
 
 	/* SMI requires CPU 0 */
-	get_online_cpus();
+	cpus_read_lock();
 	ret = smp_call_on_cpu(0, raise_smi, smi_cmd, true);
-	put_online_cpus();
+	cpus_read_unlock();
 
 	return ret;
 }
