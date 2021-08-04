9b16c2132f34316bf0b59d24357a788cc1e9e352
Michael Kelley <mikelley@microsoft.com>
2021-08-04 08:52:38 -0700

    arm64: efi: Export screen_info
    The Hyper-V frame buffer driver may be built as a module, and
    it needs access to screen_info. So export screen_info.
    
    Signed-off-by: Michael Kelley <mikelley@microsoft.com>
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Acked-by: Marc Zyngier <maz@kernel.org>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/1628092359-61351-5-git-send-email-mikelley@microsoft.com
    Signed-off-by: Wei Liu <wei.liu@kernel.org>

--- arch/arm64/kernel/efi.c
+++ arch/arm64/kernel/efi.c
@@ -55,6 +55,7 @@ static __init pteval_t create_mapping_protection(efi_memory_desc_t *md)
 
 /* we will fill this structure from the stub, so don't put it in .bss */
 struct screen_info screen_info __section(".data");
+EXPORT_SYMBOL(screen_info);
 
 int __init efi_create_mapping(struct mm_struct *mm, efi_memory_desc_t *md)
 {
