5d79e5ce5489b489cbc4c327305be9dfca0fc9ce
Thara Gopinath <thara.gopinath@linaro.org>
2021-08-04 16:34:20 -0400

    cpufreq: blocklist Qualcomm sm8150 in cpufreq-dt-platdev
    The Qualcomm sm8150 platform uses the qcom-cpufreq-hw driver, so
    add it to the cpufreq-dt-platdev driver's blocklist.
    
    Signed-off-by: Thara Gopinath <thara.gopinath@linaro.org>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -141,6 +141,7 @@ static const struct of_device_id blocklist[] __initconst = {
 	{ .compatible = "qcom,sc7280", },
 	{ .compatible = "qcom,sc8180x", },
 	{ .compatible = "qcom,sdm845", },
+	{ .compatible = "qcom,sm8150", },
 
 	{ .compatible = "st,stih407", },
 	{ .compatible = "st,stih410", },
