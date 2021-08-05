2e9fb2c11e0ec3113fcf0e8e052c99ecd82fcd4e
Bart Van Assche <bvanassche@acm.org>
2021-08-05 10:34:47 -0700

    block/partitions/ldm.c: Fix a kernel-doc warning
    Fix the following kernel-doc warning that appears when building with W=1:
    
    block/partitions/ldm.c:31: warning: expecting prototype for ldm().
    Prototype was for ldm_debug() instead
    
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Link: https://lore.kernel.org/r/20210805173447.3249906-1-bvanassche@acm.org
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/partitions/ldm.c
+++ block/partitions/ldm.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-/**
+/*
  * ldm - Support for Windows Logical Disk Manager (Dynamic Disks)
  *
  * Copyright (C) 2001,2002 Richard Russon <ldm@flatcap.org>
