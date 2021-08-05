7780b3dda212babc7b8988c9f8f82f5324e8f7cf
Ikjoon Jang <ikjn@chromium.org>
2021-08-05 13:39:57 +0800

    usb: xhci-mtk: relax TT periodic bandwidth allocation
    [ Upstream commit 548011957d1d72e0b662300c8b32b81d593b796e ]
    
    Currently xhci-mtk needs software-managed bandwidth allocation for
    periodic endpoints, it allocates the microframe index for the first
    start-split packet for each endpoint. As this index allocation logic
    should avoid the conflicts with other full/low-speed periodic endpoints,
    it uses the worst case byte budgets on high-speed bus bandwidth
    For example, for an isochronos IN endpoint with 192 bytes budget,
    it will consume the whole 4 u-frames(188 * 4) while the actual
    full-speed bus budget should be just 192bytes.
    
    This patch changes the low/full-speed bandwidth allocation logic
    to use "approximate" best case budget for lower speed bandwidth
    management. For the same endpoint from the above example, the
    approximate best case budget is now reduced to (188 * 2) bytes.
    
    Without this patch, many usb audio headsets with 3 interfaces
    (audio input, audio output, and HID) cannot be configured
    on xhci-mtk.
    
    Signed-off-by: Ikjoon Jang <ikjn@chromium.org>
    Link: https://lore.kernel.org/r/20210805133937.1.Ia8174b875bc926c12ce427a5a1415dea31cc35ae@changeid
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/usb/host/xhci-mtk-sch.c
+++ drivers/usb/host/xhci-mtk-sch.c
@@ -408,16 +408,17 @@ static int check_fs_bus_bw(struct mu3h_sch_ep_info *sch_ep, int offset)
 	u32 num_esit, tmp;
 	int base;
 	int i, j;
+	u8 uframes = DIV_ROUND_UP(sch_ep->maxpkt, FS_PAYLOAD_MAX);
 
 	num_esit = XHCI_MTK_MAX_ESIT / sch_ep->esit;
+
+	if (sch_ep->ep_type == INT_IN_EP || sch_ep->ep_type == ISOC_IN_EP)
+		offset++;
+
 	for (i = 0; i < num_esit; i++) {
 		base = offset + i * sch_ep->esit;
 
-		/*
-		 * Compared with hs bus, no matter what ep type,
-		 * the hub will always delay one uframe to send data
-		 */
-		for (j = 0; j < sch_ep->cs_count; j++) {
+		for (j = 0; j < uframes; j++) {
 			tmp = tt->fs_bus_bw[base + j] + sch_ep->bw_cost_per_microframe;
 			if (tmp > FS_PAYLOAD_MAX)
 				return -ESCH_BW_OVERFLOW;
@@ -494,6 +495,8 @@ static void update_sch_tt(struct usb_device *udev,
 	u32 base, num_esit;
 	int bw_updated;
 	int i, j;
+	int offset = sch_ep->offset;
+	u8 uframes = DIV_ROUND_UP(sch_ep->maxpkt, FS_PAYLOAD_MAX);
 
 	num_esit = XHCI_MTK_MAX_ESIT / sch_ep->esit;
 
@@ -502,10 +505,13 @@ static void update_sch_tt(struct usb_device *udev,
 	else
 		bw_updated = -sch_ep->bw_cost_per_microframe;
 
-	for (i = 0; i < num_esit; i++) {
-		base = sch_ep->offset + i * sch_ep->esit;
+	if (sch_ep->ep_type == INT_IN_EP || sch_ep->ep_type == ISOC_IN_EP)
+		offset++;
 
-		for (j = 0; j < sch_ep->cs_count; j++)
+	for (i = 0; i < num_esit; i++) {
+		base = offset + i * sch_ep->esit;
+
+		for (j = 0; j < uframes; j++)
 			tt->fs_bus_bw[base + j] += bw_updated;
 	}
 
