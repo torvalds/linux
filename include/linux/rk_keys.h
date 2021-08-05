10ae725c75d2e948515af2748453d56080125f17
Tao Huang <huangtao@rock-chips.com>
2021-08-05 20:44:15 +0800

    input: remove unused rk_keys driver
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>
    Change-Id: Ia545794ba2db53429faa31b0e65791dd75c8798c

--- include/linux/rk_keys.h
+++ include/linux/rk_keys.h
@@ -8,12 +8,7 @@
 #ifndef _RK_KEYS_H
 #define _RK_KEYS_H
 
-#ifdef CONFIG_KEYBOARD_ROCKCHIP
-void rk_send_power_key(int state);
-void rk_send_wakeup_key(void);
-#else
 static inline void rk_send_power_key(int state) { }
 static inline void rk_send_wakeup_key(void) { }
-#endif
 
 #endif
