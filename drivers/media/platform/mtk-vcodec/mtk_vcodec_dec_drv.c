3766d0d83873d971707d33ccc323d9e6935f031d
Yunfei Dong <yunfei.dong@mediatek.com>
2021-08-06 06:15:30 +0200

    media: mtk-vcodec: enable MT8183 decoder
    Now that all the supporting blocks are present, enable decoder for
    MT8183.
    
    [acourbot: refactor, cleanup and split]
    
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.com>
    Co-developed-by: Alexandre Courbot <acourbot@chromium.org>
    Signed-off-by: Alexandre Courbot <acourbot@chromium.org>
    Signed-off-by: Tzung-Bi Shih <tzungbi@google.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/platform/mtk-vcodec/mtk_vcodec_dec_drv.c
+++ drivers/media/platform/mtk-vcodec/mtk_vcodec_dec_drv.c
@@ -373,11 +373,16 @@ err_dec_pm:
 }
 
 extern const struct mtk_vcodec_dec_pdata mtk_vdec_8173_pdata;
+extern const struct mtk_vcodec_dec_pdata mtk_vdec_8183_pdata;
 
 static const struct of_device_id mtk_vcodec_match[] = {
 	{
 		.compatible = "mediatek,mt8173-vcodec-dec",
 		.data = &mtk_vdec_8173_pdata,
+	},
+	{
+		.compatible = "mediatek,mt8183-vcodec-dec",
+		.data = &mtk_vdec_8183_pdata,
 	},
 	{},
 };
