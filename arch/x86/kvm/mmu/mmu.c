ec607a564f70519b340f7eb4cfc0f4a6b55285ac
Paolo Bonzini <pbonzini@redhat.com>
2021-08-06 07:05:58 -0400

    KVM: x86: clamp host mapping level to max_level in kvm_mmu_max_mapping_level
    This change started as a way to make kvm_mmu_hugepage_adjust a bit simpler,
    but it does fix two bugs as well.
    
    One bug is in zapping collapsible PTEs.  If a large page size is
    disallowed but not all of them, kvm_mmu_max_mapping_level will return the
    host mapping level and the small PTEs will be zapped up to that level.
    However, if e.g. 1GB are prohibited, we can still zap 4KB mapping and
    preserve the 2MB ones. This can happen for example when NX huge pages
    are in use.
    
    The second would happen when userspace backs guest memory
    with a 1gb hugepage but only assign a subset of the page to
    the guest.  1gb pages would be disallowed by the memslot, but
    not 2mb.  kvm_mmu_max_mapping_level() would fall through to the
    host_pfn_mapping_level() logic, see the 1gb hugepage, and map the whole
    thing into the guest.
    
    Fixes: 2f57b7051fe8 ("KVM: x86/mmu: Persist gfn_lpage_is_disallowed() to max_level")
    Cc: stable@vger.kernel.org
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -2910,6 +2910,7 @@ int kvm_mmu_max_mapping_level(struct kvm *kvm,
 			      kvm_pfn_t pfn, int max_level)
 {
 	struct kvm_lpage_info *linfo;
+	int host_level;
 
 	max_level = min(max_level, max_huge_page_level);
 	for ( ; max_level > PG_LEVEL_4K; max_level--) {
@@ -2921,7 +2922,8 @@ int kvm_mmu_max_mapping_level(struct kvm *kvm,
 	if (max_level == PG_LEVEL_4K)
 		return PG_LEVEL_4K;
 
-	return host_pfn_mapping_level(kvm, gfn, pfn, slot);
+	host_level = host_pfn_mapping_level(kvm, gfn, pfn, slot);
+	return min(host_level, max_level);
 }
 
 int kvm_mmu_hugepage_adjust(struct kvm_vcpu *vcpu, gfn_t gfn,
@@ -2945,17 +2947,12 @@ int kvm_mmu_hugepage_adjust(struct kvm_vcpu *vcpu, gfn_t gfn,
 	if (!slot)
 		return PG_LEVEL_4K;
 
-	level = kvm_mmu_max_mapping_level(vcpu->kvm, slot, gfn, pfn, max_level);
-	if (level == PG_LEVEL_4K)
-		return level;
-
-	*req_level = level = min(level, max_level);
-
 	/*
 	 * Enforce the iTLB multihit workaround after capturing the requested
 	 * level, which will be used to do precise, accurate accounting.
 	 */
-	if (huge_page_disallowed)
+	*req_level = level = kvm_mmu_max_mapping_level(vcpu->kvm, slot, gfn, pfn, max_level);
+	if (level == PG_LEVEL_4K || huge_page_disallowed)
 		return PG_LEVEL_4K;
 
 	/*
