ed4520d6a10bbc1d6fdebf325f0395995ce634cf
Tony Lindgren <tony@atomide.com>
2021-08-09 11:27:47 -0500

    soc: ti: Remove pm_runtime_irq_safe() usage for smartreflex
    For the smartreflex device, we need to disable smartreflex on SoC idle,
    and have been using pm_runtime_irq_safe() to do that. But we want to
    remove the irq_safe usage as PM runtime takes a permanent usage count
    on the parent device with it.
    
    In order to remove the need for pm_runtime_irq_safe(), let's gate
    the clock directly in the driver. This removes the need to call PM runtime
    during idle, and allows us to switch to using CPU_PM in the following
    patch.
    
    Note that the smartreflex interconnect target module is configured for smart
    idle, but the clock does not have autoidle capability, and needs to be gated
    manually. If the clock supported autoidle, we would not need to even gate
    the clock.
    
    With this change, we can now remove the related quirk flags for ti-sysc
    also.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@oracle.com>

--- include/linux/power/smartreflex.h
+++ include/linux/power/smartreflex.h
@@ -155,6 +155,7 @@ struct omap_sr {
 	struct voltagedomain		*voltdm;
 	struct dentry			*dbg_dir;
 	unsigned int			irq;
+	struct clk			*fck;
 	int				srid;
 	int				ip_type;
 	int				nvalue_count;
@@ -169,6 +170,7 @@ struct omap_sr {
 	u32				senp_mod;
 	u32				senn_mod;
 	void __iomem			*base;
+	unsigned long			enabled:1;
 };
 
 /**
