64513d269e8971aabb7e787955a1b320e3031306
Guvenc Gulce <guvenc@linux.ibm.com>
2021-08-09 11:05:57 +0200

    net/smc: Correct smc link connection counter in case of smc client
    SMC clients may be assigned to a different link after the initial
    connection between two peers was established. In such a case,
    the connection counter was not correctly set.
    
    Update the connection counter correctly when a smc client connection
    is assigned to a different smc link.
    
    Fixes: 07d51580ff65 ("net/smc: Add connection counters for links")
    Signed-off-by: Guvenc Gulce <guvenc@linux.ibm.com>
    Tested-by: Karsten Graul <kgraul@linux.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/smc/smc_core.h
+++ net/smc/smc_core.h
@@ -446,6 +446,8 @@ void smc_core_exit(void);
 int smcr_link_init(struct smc_link_group *lgr, struct smc_link *lnk,
 		   u8 link_idx, struct smc_init_info *ini);
 void smcr_link_clear(struct smc_link *lnk, bool log);
+void smc_switch_link_and_count(struct smc_connection *conn,
+			       struct smc_link *to_lnk);
 int smcr_buf_map_lgr(struct smc_link *lnk);
 int smcr_buf_reg_lgr(struct smc_link *lnk);
 void smcr_lgr_set_type(struct smc_link_group *lgr, enum smc_lgr_type new_type);
