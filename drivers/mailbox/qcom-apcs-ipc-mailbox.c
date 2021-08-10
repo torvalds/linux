e5c11ee3106072ef4b949eca93db160f55e6b55b
Vladimir Lypak <junak.pub@gmail.com>
2021-08-10 16:44:33 +0000

    mailbox: qcom-apcs-ipc: Add compatible for MSM8953 SoC
    MSM8953 has an APCS block similar to MSM8916 but with different clocks
    which are spread over 2MB IO region next to it.
    
    Signed-off-by: Vladimir Lypak <junak.pub@gmail.com>
    Signed-off-by: Sireesh Kodali <sireeshkodali@protonmail.com>
    Signed-off-by: Jassi Brar <jaswinder.singh@linaro.org>

--- drivers/mailbox/qcom-apcs-ipc-mailbox.c
+++ drivers/mailbox/qcom-apcs-ipc-mailbox.c
@@ -163,6 +163,7 @@ static const struct of_device_id qcom_apcs_ipc_of_match[] = {
 	{ .compatible = "qcom,ipq8074-apcs-apps-global", .data = &ipq8074_apcs_data },
 	{ .compatible = "qcom,msm8916-apcs-kpss-global", .data = &msm8916_apcs_data },
 	{ .compatible = "qcom,msm8939-apcs-kpss-global", .data = &msm8916_apcs_data },
+	{ .compatible = "qcom,msm8953-apcs-kpss-global", .data = &msm8994_apcs_data },
 	{ .compatible = "qcom,msm8994-apcs-kpss-global", .data = &msm8994_apcs_data },
 	{ .compatible = "qcom,msm8996-apcs-hmss-global", .data = &msm8996_apcs_data },
 	{ .compatible = "qcom,msm8998-apcs-hmss-global", .data = &msm8998_apcs_data },
