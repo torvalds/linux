d6c08aba4f29f606769939eb6156efceb7dbb790
Nikolay Aleksandrov <nikolay@nvidia.com>
2021-08-10 18:29:25 +0300

    net: bridge: vlan: add support for mcast query interval global option
    Add support to change and retrieve global vlan multicast query interval
    option.
    
    Signed-off-by: Nikolay Aleksandrov <nikolay@nvidia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bridge/br_private.h
+++ net/bridge/br_private.h
@@ -1184,6 +1184,8 @@ br_multicast_ctx_options_equal(const struct net_bridge_mcast *brmctx1,
 	       brmctx2->multicast_membership_interval &&
 	       brmctx1->multicast_querier_interval ==
 	       brmctx2->multicast_querier_interval &&
+	       brmctx1->multicast_query_interval ==
+	       brmctx2->multicast_query_interval &&
 #if IS_ENABLED(CONFIG_IPV6)
 	       brmctx1->multicast_mld_version ==
 	       brmctx2->multicast_mld_version &&
