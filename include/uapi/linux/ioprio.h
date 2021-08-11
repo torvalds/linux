25bca50e523cbe96c0207fbb92f22ff2bc28e9aa
Damien Le Moal <damien.lemoal@wdc.com>
2021-08-11 12:36:58 +0900

    block: improve ioprio class description comment
    In include/usapi/linux/ioprio.h, change the ioprio class enum comment
    to remove the outdated reference to CFQ and mention BFQ and mq-deadline
    instead. Also document the high priority NCQ command use for RT class
    IOs directed at ATA drives that support NCQ priority.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@wdc.com>
    Link: https://lore.kernel.org/r/20210811033702.368488-3-damien.lemoal@wdc.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/uapi/linux/ioprio.h
+++ include/uapi/linux/ioprio.h
@@ -13,10 +13,12 @@
 #define IOPRIO_PRIO_VALUE(class, data)	(((class) << IOPRIO_CLASS_SHIFT) | data)
 
 /*
- * These are the io priority groups as implemented by CFQ. RT is the realtime
- * class, it always gets premium service. BE is the best-effort scheduling
- * class, the default for any process. IDLE is the idle scheduling class, it
- * is only served when no one else is using the disk.
+ * These are the io priority groups as implemented by the BFQ and mq-deadline
+ * schedulers. RT is the realtime class, it always gets premium service. For
+ * ATA disks supporting NCQ IO priority, RT class IOs will be processed using
+ * high priority NCQ commands. BE is the best-effort scheduling class, the
+ * default for any process. IDLE is the idle scheduling class, it is only
+ * served when no one else is using the disk.
  */
 enum {
 	IOPRIO_CLASS_NONE,
