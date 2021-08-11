ef75702d6d65a1993dc0d6dd423fefaa08901f24
Sai Prakash Ranjan <saiprakash.ranjan@codeaurora.org>
2021-08-11 21:34:26 +0530

    iommu/arm-smmu: Optimize ->tlb_flush_walk() for qcom implementation
    Currently for iommu_unmap() of large scatter-gather list with page size
    elements, the majority of time is spent in flushing of partial walks in
    __arm_lpae_unmap() which is a VA based TLB invalidation invalidating
    page-by-page on iommus like arm-smmu-v2 (TLBIVA).
    
    For example: to unmap a 32MB scatter-gather list with page size elements
    (8192 entries), there are 16->2MB buffer unmaps based on the pgsize (2MB
    for 4K granule) and each of 2MB will further result in 512 TLBIVAs (2MB/4K)
    resulting in a total of 8192 TLBIVAs (512*16) for 16->2MB causing a huge
    overhead.
    
    On qcom implementation, there are several performance improvements for
    TLB cache invalidations in HW like wait-for-safe (for realtime clients
    such as camera and display) and few others to allow for cache
    lookups/updates when TLBI is in progress for the same context bank.
    So the cost of over-invalidation is less compared to the unmap latency
    on several usecases like camera which deals with large buffers. So,
    ASID based TLB invalidations (TLBIASID) can be used to invalidate the
    entire context for partial walk flush thereby improving the unmap
    latency.
    
    For this example of 32MB scatter-gather list unmap, this change results
    in just 16 ASID based TLB invalidations (TLBIASIDs) as opposed to 8192
    TLBIVAs thereby increasing the performance of unmaps drastically.
    
    Test on QTI SM8150 SoC for 10 iterations of iommu_{map_sg}/unmap:
    (average over 10 iterations)
    
    Before this optimization:
    
        size        iommu_map_sg      iommu_unmap
          4K            2.067 us         1.854 us
         64K            9.598 us         8.802 us
          1M          148.890 us       130.718 us
          2M          305.864 us        67.291 us
         12M         1793.604 us       390.838 us
         16M         2386.848 us       518.187 us
         24M         3563.296 us       775.989 us
         32M         4747.171 us      1033.364 us
    
    After this optimization:
    
        size        iommu_map_sg      iommu_unmap
          4K            1.723 us         1.765 us
         64K            9.880 us         8.869 us
          1M          155.364 us       135.223 us
          2M          303.906 us         5.385 us
         12M         1786.557 us        21.250 us
         16M         2391.890 us        27.437 us
         24M         3570.895 us        39.937 us
         32M         4755.234 us        51.797 us
    
    Real world data also shows big difference in unmap performance as below:
    
    There were reports of camera frame drops because of high overhead in
    iommu unmap without this optimization because of frequent unmaps issued
    by camera of about 100MB/s taking more than 100ms thereby causing frame
    drops.
    
    Signed-off-by: Sai Prakash Ranjan <saiprakash.ranjan@codeaurora.org>
    Link: https://lore.kernel.org/r/20210811160426.10312-1-saiprakash.ranjan@codeaurora.org
    Signed-off-by: Will Deacon <will@kernel.org>

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -327,9 +327,16 @@ static void arm_smmu_tlb_inv_range_s2(unsigned long iova, size_t size,
 static void arm_smmu_tlb_inv_walk_s1(unsigned long iova, size_t size,
 				     size_t granule, void *cookie)
 {
-	arm_smmu_tlb_inv_range_s1(iova, size, granule, cookie,
-				  ARM_SMMU_CB_S1_TLBIVA);
-	arm_smmu_tlb_sync_context(cookie);
+	struct arm_smmu_domain *smmu_domain = cookie;
+	struct arm_smmu_cfg *cfg = &smmu_domain->cfg;
+
+	if (cfg->flush_walk_prefer_tlbiasid) {
+		arm_smmu_tlb_inv_context_s1(cookie);
+	} else {
+		arm_smmu_tlb_inv_range_s1(iova, size, granule, cookie,
+					  ARM_SMMU_CB_S1_TLBIVA);
+		arm_smmu_tlb_sync_context(cookie);
+	}
 }
 
 static void arm_smmu_tlb_add_page_s1(struct iommu_iotlb_gather *gather,
