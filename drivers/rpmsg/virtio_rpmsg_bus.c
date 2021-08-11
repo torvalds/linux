f0d1be1482aaab1aaadf68aa959f6d4c098a8e8f
Cai Huoqing <caihuoqing@baidu.com>
2021-08-11 20:31:25 +0800

    rpmsg: virtio: Remove unused including <linux/of_device.h>
    Remove including <linux/of_device.h> that don't need it.
    
    Signed-off-by: Cai Huoqing <caihuoqing@baidu.com>
    Reviewed-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Link: https://lore.kernel.org/r/20210811123125.143-1-caihuoqing@baidu.com

--- drivers/rpmsg/virtio_rpmsg_bus.c
+++ drivers/rpmsg/virtio_rpmsg_bus.c
@@ -17,7 +17,6 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
-#include <linux/of_device.h>
 #include <linux/rpmsg.h>
 #include <linux/rpmsg/byteorder.h>
 #include <linux/rpmsg/ns.h>
