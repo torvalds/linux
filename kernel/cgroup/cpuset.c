ee9707e8593dfb9a375cf4793c3fd03d4142b463
Waiman Long <longman@redhat.com>
2021-08-11 15:57:07 -0400

    cgroup/cpuset: Enable memory migration for cpuset v2
    When a user changes cpuset.cpus, each task in a v2 cpuset will be moved
    to one of the new cpus if it is not there already. For memory, however,
    they won't be migrated to the new nodes when cpuset.mems changes. This is
    an inconsistency in behavior.
    
    In cpuset v1, there is a memory_migrate control file to enable such
    behavior by setting the CS_MEMORY_MIGRATE flag. Make it the default
    for cpuset v2 so that we have a consistent set of behavior for both
    cpus and memory.
    
    There is certainly a cost to make memory migration the default, but it
    is a one time cost that shouldn't really matter as long as cpuset.mems
    isn't changed frequenty.  Update the cgroup-v2.rst file to document the
    new behavior and recommend against changing cpuset.mems frequently.
    
    Since there won't be any concurrent access to the newly allocated cpuset
    structure in cpuset_css_alloc(), we can use the cheaper non-atomic
    __set_bit() instead of the more expensive atomic set_bit().
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2761,11 +2761,15 @@ cpuset_css_alloc(struct cgroup_subsys_state *parent_css)
 		return ERR_PTR(-ENOMEM);
 	}
 
-	set_bit(CS_SCHED_LOAD_BALANCE, &cs->flags);
+	__set_bit(CS_SCHED_LOAD_BALANCE, &cs->flags);
 	nodes_clear(cs->mems_allowed);
 	nodes_clear(cs->effective_mems);
 	fmeter_init(&cs->fmeter);
 	cs->relax_domain_level = -1;
+
+	/* Set CS_MEMORY_MIGRATE for default hierarchy */
+	if (cgroup_subsys_on_dfl(cpuset_cgrp_subsys))
+		__set_bit(CS_MEMORY_MIGRATE, &cs->flags);
 
 	return &cs->css;
 }
