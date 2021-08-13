928adbf65bb1f813487164edde6293bf42d8f195
Wenjing Liu <wenjing.liu@amd.com>
2021-08-13 19:49:43 -0400

    drm/amd/display: update conditions to do dfp cap ext validation
    [why]
    According to DP specs dfp cap ext validation is only for branch
    device withou 128b/132b channel coding support and
    downstream of the branch device doesn't have compression.
    Therefore we are adding conditions to only do dfp cap
    extension validation for branch devcie supporting 8b/10b
    channel coding only and it has no DSC passthrough capability.
    
    Acked-by: Mikita Lipski <mikita.lipski@amd.com>
    Signed-off-by: Wenjing Liu <wenjing.liu@amd.com>
    Reviewed-by: George Shen <george.shen@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_link.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_link.c
@@ -2606,7 +2606,9 @@ static bool dp_active_dongle_validate_timing(
 #if defined(CONFIG_DRM_AMD_DC_DCN)
 	}
 
-	if (dongle_caps->dfp_cap_ext.supported) {
+	if (dpcd_caps->channel_coding_cap.bits.DP_128b_132b_SUPPORTED == 0 &&
+			dpcd_caps->dsc_caps.dsc_basic_caps.fields.dsc_support.DSC_PASSTHROUGH_SUPPORT == 0 &&
+			dongle_caps->dfp_cap_ext.supported) {
 
 		if (dongle_caps->dfp_cap_ext.max_pixel_rate_in_mps < (timing->pix_clk_100hz / 10000))
 			return false;
