761bcac1573efc99042d59add94d468bf17127f0
Pavel Begunkov <asml.silence@gmail.com>
2021-08-15 10:40:19 +0100

    io_uring: don't inflight-track linked timeouts
    Tracking linked timeouts as infligh was needed to make sure that io-wq
    is not destroyed by io_uring_cancel_generic() racing with
    io_async_cancel_one() accessing it. Now, cancellations issued by linked
    timeouts are done in the task context, so it's already synchronised.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/e1b05cf47cb69df2305efdbee8cf7ba36f46c1a3.1628981736.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -5701,8 +5701,6 @@ static int io_timeout_prep(struct io_kiocb *req, const struct io_uring_sqe *sqe,
 
 	data->mode = io_translate_timeout_mode(flags);
 	hrtimer_init(&data->timer, CLOCK_MONOTONIC, data->mode);
-	if (is_timeout_link)
-		io_req_track_inflight(req);
 	return 0;
 }
 
