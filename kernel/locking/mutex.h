43d2d52d704e025518d35c3079fcbff744623166
Thomas Gleixner <tglx@linutronix.de>
2021-08-15 23:28:33 +0200

    locking/mutex: Move the 'struct mutex_waiter' definition from <linux/mutex.h> to the internal header
    Move the mutex waiter declaration from the public <linux/mutex.h> header
    to the internal kernel/locking/mutex.h header.
    
    There is no reason to expose it outside of the core code.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20210815211304.054325923@linutronix.de

--- kernel/locking/mutex.h
+++ kernel/locking/mutex.h
@@ -7,6 +7,19 @@
  *  Copyright (C) 2004, 2005, 2006 Red Hat, Inc., Ingo Molnar <mingo@redhat.com>
  */
 
+/*
+ * This is the control structure for tasks blocked on mutex, which resides
+ * on the blocked task's kernel stack:
+ */
+struct mutex_waiter {
+	struct list_head	list;
+	struct task_struct	*task;
+	struct ww_acquire_ctx	*ww_ctx;
+#ifdef CONFIG_DEBUG_MUTEXES
+	void			*magic;
+#endif
+};
+
 #ifdef CONFIG_DEBUG_MUTEXES
 extern void debug_mutex_lock_common(struct mutex *lock,
 				    struct mutex_waiter *waiter);
