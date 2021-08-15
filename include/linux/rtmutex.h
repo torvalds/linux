e4e17af3b7f8841279b5a429de14907e26845c39
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2021-08-15 23:28:20 +0200

    locking/rtmutex: Reduce <linux/rtmutex.h> header dependencies, only include <linux/rbtree_types.h>
    We have the following header dependency problem on RT:
    
     - <linux/rtmutex.h> needs the definition of 'struct rb_root_cached'.
     - <linux/rbtree.h> includes <linux/kernel.h>, which includes <linux/spinlock.h>
    
    That works nicely for non-RT enabled kernels, but on RT enabled kernels
    spinlocks are based on rtmutexes, which creates another circular header
    dependency as <linux/spinlocks.h> will require <linux/rtmutex.h>.
    
    Include <linux/rbtree_types.h> instead.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20210815211303.598003167@linutronix.de

--- include/linux/rtmutex.h
+++ include/linux/rtmutex.h
@@ -15,7 +15,7 @@
 
 #include <linux/compiler.h>
 #include <linux/linkage.h>
-#include <linux/rbtree.h>
+#include <linux/rbtree_types.h>
 #include <linux/spinlock_types_raw.h>
 
 extern int max_lock_depth; /* for sysctl */
