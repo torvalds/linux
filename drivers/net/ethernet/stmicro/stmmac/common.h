68e9c5dee1cf9f5651a894a151d72b7fc21172d3
Vijayakannan Ayyathurai <vijayakannan.ayyathurai@intel.com>
2021-08-16 14:15:59 +0800

    net: stmmac: add ethtool per-queue statistic framework
    Adding generic ethtool per-queue statistic framework to display the
    statistics for each rx/tx queue. In future, users can avail it to add
    more per-queue specific counters. Number of rx/tx queues displayed is
    depending on the available rx/tx queues in that particular MAC config
    and this number is limited up to the MTL_MAX_{RX|TX}_QUEUES defined
    in the driver.
    
    Ethtool per-queue statistic display will look like below, when users
    start adding more counters.
    
    Example:
     q0_tx_statA:
     q0_tx_statB:
     q0_tx_statC:
     |
     q0_tx_statX:
     .
     .
     .
     qMAX_tx_statA:
     qMAX_tx_statB:
     qMAX_tx_statC:
     |
     qMAX_tx_statX:
    
     q0_rx_statA:
     q0_rx_statB:
     q0_rx_statC:
     |
     q0_rx_statX:
     .
     .
     .
     qMAX_rx_statA:
     qMAX_rx_statB:
     qMAX_rx_statC:
     |
     qMAX_rx_statX:
    
    In addition, this patch has the support on displaying the number of
    packets received and transmitted per queue.
    
    Signed-off-by: Vijayakannan Ayyathurai <vijayakannan.ayyathurai@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/common.h
+++ drivers/net/ethernet/stmicro/stmmac/common.h
@@ -58,6 +58,14 @@
 #undef FRAME_FILTER_DEBUG
 /* #define FRAME_FILTER_DEBUG */
 
+struct stmmac_txq_stats {
+	unsigned long tx_pkt_n;
+};
+
+struct stmmac_rxq_stats {
+	unsigned long rx_pkt_n;
+};
+
 /* Extra statistic and debug information exposed by ethtool */
 struct stmmac_extra_stats {
 	/* Transmit errors */
@@ -189,6 +197,9 @@ struct stmmac_extra_stats {
 	unsigned long mtl_est_hlbf;
 	unsigned long mtl_est_btre;
 	unsigned long mtl_est_btrlm;
+	/* per queue statistics */
+	struct stmmac_txq_stats txq_stats[MTL_MAX_TX_QUEUES];
+	struct stmmac_rxq_stats rxq_stats[MTL_MAX_RX_QUEUES];
 };
 
 /* Safety Feature statistics exposed by ethtool */
