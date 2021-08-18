cf4e6d52f58399c777276172ec250502e19d5e63
Youquan Song <youquan.song@intel.com>
2021-08-18 10:57:01 -0700

    EDAC/i10nm: Retrieve and print retry_rd_err_log registers
    Retrieve and print retry_rd_err_log registers like the earlier change:
    commit e80634a75aba ("EDAC, skx: Retrieve and print retry_rd_err_log registers")
    
    This is a little trickier than on Skylake because of potential
    interference with BIOS use of the same registers. The default
    behavior is to ignore these registers.
    
    A module parameter retry_rd_err_log(default=0) controls the mode of operation:
    - 0=off  : Default.
    - 1=bios : Linux doesn't reset any control bits, but just reports values.
               This is "no harm" mode, but it may miss reporting some data.
    - 2=linux: Linux tries to take control and resets mode bits,
               clears valid/UC bits after reading. This should be
               more reliable (especially if BIOS interference is reduced
               by disabling eMCA reporting mode in BIOS setup).
    
    Co-developed-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>
    Signed-off-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>
    Signed-off-by: Youquan Song <youquan.song@intel.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Link: https://lore.kernel.org/r/20210818175701.1611513-3-tony.luck@intel.com

--- drivers/edac/skx_common.c
+++ drivers/edac/skx_common.c
@@ -532,6 +532,7 @@ static void skx_mce_output_error(struct mem_ctl_info *mci,
 	bool ripv = GET_BITFIELD(m->mcgstatus, 0, 0);
 	bool overflow = GET_BITFIELD(m->status, 62, 62);
 	bool uncorrected_error = GET_BITFIELD(m->status, 61, 61);
+	bool scrub_err = false;
 	bool recoverable;
 	int len;
 	u32 core_err_cnt = GET_BITFIELD(m->status, 38, 52);
@@ -583,6 +584,7 @@ static void skx_mce_output_error(struct mem_ctl_info *mci,
 			break;
 		case 4:
 			optype = "memory scrubbing error";
+			scrub_err = true;
 			break;
 		default:
 			optype = "reserved";
@@ -605,7 +607,7 @@ static void skx_mce_output_error(struct mem_ctl_info *mci,
 	}
 
 	if (skx_show_retry_rd_err_log)
-		skx_show_retry_rd_err_log(res, skx_msg + len, MSG_SIZE - len);
+		skx_show_retry_rd_err_log(res, skx_msg + len, MSG_SIZE - len, scrub_err);
 
 	edac_dbg(0, "%s\n", skx_msg);
 
