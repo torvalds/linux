43e540cc9f2ca12a2364ddf64e5ef929a546550d
Wei Huang <wei.huang2@amd.com>
2021-08-18 11:55:49 -0500

    KVM: SVM: Add 5-level page table support for SVM
    When the 5-level page table is enabled on host OS, the nested page table
    for guest VMs must use 5-level as well. Update get_npt_level() function
    to reflect this requirement. In the meanwhile, remove the code that
    prevents kvm-amd driver from being loaded when 5-level page table is
    detected.
    
    Signed-off-by: Wei Huang <wei.huang2@amd.com>
    Message-Id: <20210818165549.3771014-4-wei.huang2@amd.com>
    [Tweak condition as suggested by Sean. - Paolo]
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -259,7 +259,7 @@ u32 svm_msrpm_offset(u32 msr)
 static int get_max_npt_level(void)
 {
 #ifdef CONFIG_X86_64
-	return PT64_ROOT_4LEVEL;
+	return pgtable_l5_enabled() ? PT64_ROOT_5LEVEL : PT64_ROOT_4LEVEL;
 #else
 	return PT32E_ROOT_LEVEL;
 #endif
@@ -457,11 +457,6 @@ static int has_svm(void)
 
 	if (sev_active()) {
 		pr_info("KVM is unsupported when running as an SEV guest\n");
-		return 0;
-	}
-
-	if (pgtable_l5_enabled()) {
-		pr_info("KVM doesn't yet support 5-level paging on AMD SVM\n");
 		return 0;
 	}
 
