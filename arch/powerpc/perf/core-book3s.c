b1643084d164cea0c107a39bcdf0119fc52619af
Kajol Jain <kjain@linux.ibm.com>
2021-08-18 22:45:54 +0530

    powerpc/perf: Use stack siar instead of mfspr
    Minor optimization in the 'perf_instruction_pointer' function code by
    making use of stack siar instead of mfspr.
    
    Fixes: 75382aa72f06 ("powerpc/perf: Move code to select SIAR or pt_regs into perf_read_regs")
    Signed-off-by: Kajol Jain <kjain@linux.ibm.com>
    Tested-by: Nageswara R Sastry <rnsastry@linux.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20210818171556.36912-1-kjain@linux.ibm.com

--- arch/powerpc/perf/core-book3s.c
+++ arch/powerpc/perf/core-book3s.c
@@ -2269,7 +2269,7 @@ unsigned long perf_instruction_pointer(struct pt_regs *regs)
 		else
 			return regs->nip;
 	} else if (use_siar && siar_valid(regs))
-		return mfspr(SPRN_SIAR) + perf_ip_adjust(regs);
+		return siar + perf_ip_adjust(regs);
 	else if (use_siar)
 		return 0;		// no valid instruction pointer
 	else
