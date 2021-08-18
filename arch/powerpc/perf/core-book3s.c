3c69a5f22223fa3e312689ec218b5059784d49d7
Kajol Jain <kjain@linux.ibm.com>
2021-08-18 22:45:56 +0530

    powerpc/perf: Fix the check for SIAR value
    Incase of random sampling, there can be scenarios where
    Sample Instruction Address Register(SIAR) may not latch
    to the sampled instruction and could result in
    the value of 0. In these scenarios it is preferred to
    return regs->nip. These corner cases are seen in the
    previous generation (p9) also.
    
    Patch adds the check for SIAR value along with regs_use_siar
    and siar_valid checks so that the function will return
    regs->nip incase SIAR is zero.
    
    Patch drops the code under PPMU_P10_DD1 flag check
    which handles SIAR 0 case only for Power10 DD1.
    
    Fixes: 2ca13a4cc56c9 ("powerpc/perf: Use regs->nip when SIAR is zero")
    Signed-off-by: Kajol Jain <kjain@linux.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20210818171556.36912-3-kjain@linux.ibm.com

--- arch/powerpc/perf/core-book3s.c
+++ arch/powerpc/perf/core-book3s.c
@@ -2260,15 +2260,9 @@ unsigned long perf_misc_flags(struct pt_regs *regs)
  */
 unsigned long perf_instruction_pointer(struct pt_regs *regs)
 {
-	bool use_siar = regs_use_siar(regs);
 	unsigned long siar = mfspr(SPRN_SIAR);
 
-	if (ppmu && (ppmu->flags & PPMU_P10_DD1)) {
-		if (siar)
-			return siar;
-		else
-			return regs->nip;
-	} else if (use_siar && siar_valid(regs))
+	if (regs_use_siar(regs) && siar_valid(regs) && siar)
 		return siar + perf_ip_adjust(regs);
 	else
 		return regs->nip;
