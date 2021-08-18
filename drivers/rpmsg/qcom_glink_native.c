08de420a8014ed3fd83b2436f7e8bd9c4fcd9afe
Kees Cook <keescook@chromium.org>
2021-08-17 23:04:33 -0700

    rpmsg: glink: Replace strncpy() with strscpy_pad()
    The use of strncpy() is considered deprecated for NUL-terminated
    strings[1]. Replace strncpy() with strscpy_pad() (as it seems this case
    expects the NUL padding to fill the allocation following the flexible
    array). This additionally silences a warning seen when building under
    -Warray-bounds:
    
    ./include/linux/fortify-string.h:38:30: warning: '__builtin_strncpy' offset 24 from the object at '__mptr' is out of the bounds of referenced subobject 'data' with type 'u8[]' {aka 'unsigned char[]'} at offset 24 [-Warray-bounds]
       38 | #define __underlying_strncpy __builtin_strncpy
          |                              ^
    ./include/linux/fortify-string.h:50:9: note: in expansion of macro '__underlying_strncpy'
       50 |  return __underlying_strncpy(p, q, size);
          |         ^~~~~~~~~~~~~~~~~~~~
    drivers/rpmsg/qcom_glink_native.c: In function 'qcom_glink_work':
    drivers/rpmsg/qcom_glink_native.c:36:5: note: subobject 'data' declared here
       36 |  u8 data[];
          |     ^~~~
    
    [1] https://www.kernel.org/doc/html/latest/process/deprecated.html#strncpy-on-nul-terminated-strings
    
    Cc: Andy Gross <agross@kernel.org>
    Cc: Bjorn Andersson <bjorn.andersson@linaro.org>
    Cc: Ohad Ben-Cohen <ohad@wizery.com>
    Cc: Mathieu Poirier <mathieu.poirier@linaro.org>
    Cc: linux-arm-msm@vger.kernel.org
    Cc: linux-remoteproc@vger.kernel.org
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Link: https://lore.kernel.org/lkml/20210728020745.GB35706@embeddedor
    Link: https://lore.kernel.org/r/20210818060533.3569517-4-keescook@chromium.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -1440,7 +1440,7 @@ static int qcom_glink_rx_open(struct qcom_glink *glink, unsigned int rcid,
 		}
 
 		rpdev->ept = &channel->ept;
-		strncpy(rpdev->id.name, name, RPMSG_NAME_SIZE);
+		strscpy_pad(rpdev->id.name, name, RPMSG_NAME_SIZE);
 		rpdev->src = RPMSG_ADDR_ANY;
 		rpdev->dst = RPMSG_ADDR_ANY;
 		rpdev->ops = &glink_device_ops;
