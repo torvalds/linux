3a29355a22c0275fe864100794fee58a73175d93
Viresh Kumar <viresh.kumar@linaro.org>
2021-08-19 10:00:22 +0530

    gpio: Add virtio-gpio driver
    This patch adds a new driver for Virtio based GPIO devices.
    
    This allows a guest VM running Linux to access GPIO lines provided by
    the host. It supports all basic operations, except interrupts for the
    GPIO lines.
    
    Based on the initial work posted by:
    "Enrico Weigelt, metux IT consult" <lkml@metux.net>.
    
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>

--- include/uapi/linux/virtio_ids.h
+++ include/uapi/linux/virtio_ids.h
@@ -56,6 +56,7 @@
 #define VIRTIO_ID_PMEM			27 /* virtio pmem */
 #define VIRTIO_ID_MAC80211_HWSIM	29 /* virtio mac80211-hwsim */
 #define VIRTIO_ID_BT			40 /* virtio bluetooth */
+#define VIRTIO_ID_GPIO			41 /* virtio gpio */
 
 /*
  * Virtio Transitional IDs
