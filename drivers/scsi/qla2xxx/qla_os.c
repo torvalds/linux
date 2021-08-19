c74ce061f8983bcb048b895bc127447909321fe8
Hannes Reinecke <hare@suse.de>
2021-08-19 11:19:11 +0200

    scsi: qla2xxx: Do not call fc_block_scsi_eh() during bus reset
    When calling bus reset the driver will be doing a full SAN resync, so there
    is no need to wait for any pending RSCNs; they'll be re-issued during
    resync anyway.
    
    Link: https://lore.kernel.org/r/20210819091913.94436-2-hare@suse.de
    Cc: Nilesh Javali <njavali@marvell.com>
    Reviewed-by: Nilesh Javali <njavali@marvell.com>
    Signed-off-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/qla2xxx/qla_os.c
+++ drivers/scsi/qla2xxx/qla_os.c
@@ -1500,7 +1500,6 @@ static int
 qla2xxx_eh_bus_reset(struct scsi_cmnd *cmd)
 {
 	scsi_qla_host_t *vha = shost_priv(cmd->device->host);
-	fc_port_t *fcport = (struct fc_port *) cmd->device->hostdata;
 	int ret = FAILED;
 	unsigned int id;
 	uint64_t lun;
@@ -1515,15 +1514,6 @@ qla2xxx_eh_bus_reset(struct scsi_cmnd *cmd)
 
 	id = cmd->device->id;
 	lun = cmd->device->lun;
-
-	if (!fcport) {
-		return ret;
-	}
-
-	ret = fc_block_scsi_eh(cmd);
-	if (ret != 0)
-		return ret;
-	ret = FAILED;
 
 	if (qla2x00_chip_is_down(vha))
 		return ret;
