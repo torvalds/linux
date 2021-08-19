c9d7b2827dd221f8f90033b69b97373d647d8e9b
Xin Ji <xji@analogixsemi.com>
2021-08-19 14:01:10 +0800

    drm/bridge: anx7625: enable DSI EOTP
    Enable DSI EOTP feature for fixing some panel screen constant shift issue.
    Removing MIPI flag MIPI_DSI_MODE_NO_EOT_PACKET to enable DSI EOTP.
    
    Reviewed-by: Robert Foss <robert.foss@linaro.org>
    Signed-off-by: Xin Ji <xji@analogixsemi.com>
    Signed-off-by: Robert Foss <robert.foss@linaro.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210819060110.3427256-1-xji@analogixsemi.com

--- drivers/gpu/drm/bridge/analogix/anx7625.c
+++ drivers/gpu/drm/bridge/analogix/anx7625.c
@@ -1335,7 +1335,6 @@ static int anx7625_attach_dsi(struct anx7625_data *ctx)
 	dsi->format = MIPI_DSI_FMT_RGB888;
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO	|
 		MIPI_DSI_MODE_VIDEO_SYNC_PULSE	|
-		MIPI_DSI_MODE_NO_EOT_PACKET	|
 		MIPI_DSI_MODE_VIDEO_HSE;
 
 	if (mipi_dsi_attach(dsi) < 0) {
