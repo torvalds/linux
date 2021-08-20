509067bbd2644ed8f59b84b5db95a985633004ba
Zhenguo Zhao <Zhenguo.Zhao1@unisoc.com>
2021-08-20 20:17:49 +0800

    tty: n_gsm: Delete gsm_disconnect when config requester
    When n_gsm config "initiator=0",when gsmld config ,as requester ,it
    doesn't need to send CLD frame data or SABM frame.
    
    Example,when tty dev receive "AT+CMUX=0",it will change ldisc
    n_tty to n_gsm,during config requester,gsmld output "7e 01 ef c3 aa 7e",
    initiator maybe not want to receive the frame data.
    
    [  154.666457] c1 Q>  0) R: UIH(F)
    [  154.669514] c1 C3
    [  154.671519] c1 gsmld_output: 00000000: 7e 01 ef c3 aa 7e
    [  155.014874] c1 Q>  0) R: UIH(F)
    [  155.018000] c1 C3
    [  155.020046] c1 gsmld_output: 00000000: 7e 01 ef c3 aa 7e
    [  155.364425] c1 Q>  0) R: UIH(F)
    [  155.367546] c1 C3
    [  155.369597] c1 gsmld_output: 00000000: 7e 01 ef c3 aa 7e
    
    Signed-off-by: Zhenguo Zhao <Zhenguo.Zhao1@unisoc.com>
    Link: https://lore.kernel.org/r/1629461872-26965-5-git-send-email-zhenguo6858@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/n_gsm.c
+++ drivers/tty/n_gsm.c
@@ -2304,7 +2304,7 @@ static int gsm_config(struct gsm_mux *gsm, struct gsm_config *c)
 	 * configuration
 	 */
 
-	if (need_close || need_restart) {
+	if (gsm->initiator && (need_close || need_restart)) {
 		int ret;
 
 		ret = gsm_disconnect(gsm);
