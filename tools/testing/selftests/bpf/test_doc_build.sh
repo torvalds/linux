00e1116031e154098c55441e4936b32e4b20b31c
Li Zhijian <lizhijian@cn.fujitsu.com>
2021-08-20 10:55:49 +0800

    selftests/bpf: Exit with KSFT_SKIP if no Makefile found
    This would happend when we run the tests after install kselftests
     root@lkp-skl-d01 ~# /kselftests/run_kselftest.sh -t bpf:test_doc_build.sh
     TAP version 13
     1..1
     # selftests: bpf: test_doc_build.sh
     perl: warning: Setting locale failed.
     perl: warning: Please check that your locale settings:
             LANGUAGE = (unset),
             LC_ALL = (unset),
             LC_ADDRESS = "en_US.UTF-8",
             LC_NAME = "en_US.UTF-8",
             LC_MONETARY = "en_US.UTF-8",
             LC_PAPER = "en_US.UTF-8",
             LC_IDENTIFICATION = "en_US.UTF-8",
             LC_TELEPHONE = "en_US.UTF-8",
             LC_MEASUREMENT = "en_US.UTF-8",
             LC_TIME = "en_US.UTF-8",
             LC_NUMERIC = "en_US.UTF-8",
             LANG = "en_US.UTF-8"
         are supported and installed on your system.
     perl: warning: Falling back to the standard locale ("C").
     # skip:    bpftool files not found!
     #
     ok 1 selftests: bpf: test_doc_build.sh # SKIP
    
    Signed-off-by: Li Zhijian <lizhijian@cn.fujitsu.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20210820025549.28325-1-lizhijian@cn.fujitsu.com

--- tools/testing/selftests/bpf/test_doc_build.sh
+++ tools/testing/selftests/bpf/test_doc_build.sh
@@ -10,6 +10,11 @@ KDIR_ROOT_DIR=$(realpath $SCRIPT_REL_DIR/../../../../)
 SCRIPT_REL_DIR=$(dirname $(realpath --relative-to=$KDIR_ROOT_DIR $SCRIPT_REL_PATH))
 cd $KDIR_ROOT_DIR
 
+if [ ! -e $PWD/$SCRIPT_REL_DIR/Makefile ]; then
+	echo -e "skip:    bpftool files not found!\n"
+	exit 4 # KSFT_SKIP=4
+fi
+
 for tgt in docs docs-clean; do
 	make -s -C $PWD/$SCRIPT_REL_DIR $tgt;
 done
