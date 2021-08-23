3accc0faef081b6813967b34f7d05a3edb855cbd
Cédric Le Goater <clg@kaod.org>
2021-08-23 11:00:38 +0200

    powerpc/prom: Fix unused variable ‘reserve_map’ when CONFIG_PPC32 is not set
    This fixes a compile error with W=1.
    
    arch/powerpc/kernel/prom.c: In function ‘early_reserve_mem’:
    arch/powerpc/kernel/prom.c:625:10: error: variable ‘reserve_map’ set but not used [-Werror=unused-but-set-variable]
      __be64 *reserve_map;
              ^~~~~~~~~~~
    cc1: all warnings being treated as errors
    
    Signed-off-by: Cédric Le Goater <clg@kaod.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20210823090039.166120-2-clg@kaod.org

--- arch/powerpc/kernel/prom.c
+++ arch/powerpc/kernel/prom.c
@@ -640,7 +640,9 @@ static void __init early_reserve_mem(void)
 	}
 #endif /* CONFIG_BLK_DEV_INITRD */
 
-#ifdef CONFIG_PPC32
+	if (!IS_ENABLED(CONFIG_PPC32))
+		return;
+
 	/* 
 	 * Handle the case where we might be booting from an old kexec
 	 * image that setup the mem_rsvmap as pairs of 32-bit values
@@ -661,7 +663,6 @@ static void __init early_reserve_mem(void)
 		}
 		return;
 	}
-#endif
 }
 
 #ifdef CONFIG_PPC_TRANSACTIONAL_MEM
