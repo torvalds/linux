854f695c3d41853eb7efcd436023c5ab92a257eb
Wang Kefeng <wangkefeng.wang@huawei.com>
2021-08-23 10:41:43 +0100

    ARM: 9119/1: amba: Properly handle device probe without IRQ domain
    of_amba_device_create() uses irq_of_parse_and_map() to translate
    a DT interrupt specification into a Linux virtual interrupt number.
    
    But it doesn't properly handle the case where the interrupt controller
    is not yet available, eg, when pl011 interrupt is connected to MBIGEN
    interrupt controller, because the mbigen initialization is too late,
    which will lead to no IRQ due to no IRQ domain found, log is shown below,
      "irq: no irq domain found for uart0 !"
    
    use of_irq_get() to return -EPROBE_DEFER as above, and in the function
    amba_device_try_add()/amba_device_add(), it will properly handle in such
    case, also return 0 in other fail cases to be consistent as before.
    
    Cc: Rob Herring <robh+dt@kernel.org>
    Cc: Frank Rowand <frowand.list@gmail.com>
    Reported-by: Ruizhe Lin <linruizhe@huawei.com>
    Reviewed-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>

--- drivers/amba/bus.c
+++ drivers/amba/bus.c
@@ -19,6 +19,7 @@
 #include <linux/clk/clk-conf.h>
 #include <linux/platform_device.h>
 #include <linux/reset.h>
+#include <linux/of_irq.h>
 
 #include <asm/irq.h>
 
@@ -371,11 +372,37 @@ static void amba_device_release(struct device *dev)
 	kfree(d);
 }
 
+static int of_amba_device_decode_irq(struct amba_device *dev)
+{
+	struct device_node *node = dev->dev.of_node;
+	int i, irq = 0;
+
+	if (IS_ENABLED(CONFIG_OF_IRQ) && node) {
+		/* Decode the IRQs and address ranges */
+		for (i = 0; i < AMBA_NR_IRQS; i++) {
+			irq = of_irq_get(node, i);
+			if (irq < 0) {
+				if (irq == -EPROBE_DEFER)
+					return irq;
+				irq = 0;
+			}
+
+			dev->irq[i] = irq;
+		}
+	}
+
+	return 0;
+}
+
 static int amba_device_try_add(struct amba_device *dev, struct resource *parent)
 {
 	u32 size;
 	void __iomem *tmp;
 	int i, ret;
+
+	ret = of_amba_device_decode_irq(dev);
+	if (ret)
+		goto err_out;
 
 	ret = request_resource(parent, &dev->res);
 	if (ret)
