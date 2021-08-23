04a71cdc46a94b13ee876290ad961b4886e24c76
Halil Pasic <pasic@linux.ibm.com>
2021-08-23 15:34:58 +0200

    scsi: core: scsi_ioctl: Fix error code propagation in SG_IO
    Link: https://lore.kernel.org/r/20210823133458.3536824-1-pasic@linux.ibm.com
    Fixes: f2542a3be327 ("scsi: scsi_ioctl: Move the "block layer" SCSI ioctl handling to drivers/scsi")
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Halil Pasic <pasic@linux.ibm.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/scsi_ioctl.c
+++ drivers/scsi/scsi_ioctl.c
@@ -874,7 +874,7 @@ static int scsi_ioctl_sg_io(struct scsi_device *sdev, struct gendisk *disk,
 		return error;
 	if (put_sg_io_hdr(&hdr, argp))
 		return -EFAULT;
-	return 0;
+	return error;
 }
 
 /**
