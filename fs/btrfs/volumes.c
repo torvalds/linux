cdccc03a8a369b59cff5e7ea3292511cfa551120
Anand Jain <anand.jain@oracle.com>
2021-08-24 13:05:22 +0800

    btrfs: remove stale comment about the btrfs_show_devname
    There were few lockdep warnings because btrfs_show_devname() was using
    device_list_mutex as recorded in the commits:
    
      0ccd05285e7f ("btrfs: fix a possible umount deadlock")
      779bf3fefa83 ("btrfs: fix lock dep warning, move scratch dev out of device_list_mutex and uuid_mutex")
    
    And finally, commit 88c14590cdd6 ("btrfs: use RCU in btrfs_show_devname
    for device list traversal") removed the device_list_mutex from
    btrfs_show_devname for performance reasons.
    
    This patch removes a stale comment about the function
    btrfs_show_devname and device_list_mutex.
    
    Signed-off-by: Anand Jain <anand.jain@oracle.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -2305,13 +2305,6 @@ void btrfs_destroy_dev_replace_tgtdev(struct btrfs_device *tgtdev)
 
 	mutex_unlock(&fs_devices->device_list_mutex);
 
-	/*
-	 * The update_dev_time() with in btrfs_scratch_superblocks()
-	 * may lead to a call to btrfs_show_devname() which will try
-	 * to hold device_list_mutex. And here this device
-	 * is already out of device list, so we don't have to hold
-	 * the device_list_mutex lock.
-	 */
 	btrfs_scratch_superblocks(tgtdev->fs_info, tgtdev->bdev,
 				  tgtdev->name->str);
 
