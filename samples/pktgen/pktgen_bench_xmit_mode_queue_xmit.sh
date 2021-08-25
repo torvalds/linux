6c882bdc4bcd63e164f05738e7677b8a62fc0ec1
Juhee Kang <claudiajkang@gmail.com>
2021-08-25 19:57:16 +0900

    samples: pktgen: add trap SIGINT for printing execution result
    All pktgen samples can send indefinitely num messages per thread by
    setting the count option to 0(-n 0). If running sample with setting
    count 0 and press Ctrl-C to stop this program, the program prints the
    result of the execution so far. Currently, the samples besides
    sample{3...5} don't work properly. Because Ctrl-C stops the script, not
    just pktgen.
    
    This is results of samples:
    
        # DEV=eth0 DEST_IP=10.1.0.1 DST_MAC=00:11:22:33:44:55 ./pktgen_sample04_many_flows.sh -n 0
        Running... ctrl^C to stop
        ^CDevice: eth0@0
        Result: OK: 569657(c569538+d118) usec, 84650 (60byte,0frags)
        148597pps 71Mb/sec (71326560bps) errors: 0
    
        # DEV=eth0 DEST_IP=10.1.0.1 DST_MAC=00:11:22:33:44:55 ./pktgen_sample01_simple.sh -n 0
        Running... ctrl^C to stop
        ^C
    
    In order to solve this, this commit adds trap SIGINT. Also, this commit
    changes control_c function to print_result to maintain consistency with
    other samples.
    
    Signed-off-by: Juhee Kang <claudiajkang@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- samples/pktgen/pktgen_bench_xmit_mode_queue_xmit.sh
+++ samples/pktgen/pktgen_bench_xmit_mode_queue_xmit.sh
@@ -69,14 +69,21 @@ for ((thread = $F_THREAD; thread <= $L_THREAD; thread++)); do
     pg_set $dev "xmit_mode queue_xmit"
 done
 
+# Run if user hits control-c
+function print_result {
+    # Print results
+    for ((thread = $F_THREAD; thread <= $L_THREAD; thread++)); do
+        dev=${DEV}@${thread}
+        echo "Device: $dev"
+        cat /proc/net/pktgen/$dev | grep -A2 "Result:"
+    done
+}
+# trap keyboard interrupt (Ctrl-C)
+trap true SIGINT
+
 # start_run
 echo "Running... ctrl^C to stop" >&2
 pg_ctrl "start"
 echo "Done" >&2
 
-# Print results
-for ((thread = $F_THREAD; thread <= $L_THREAD; thread++)); do
-    dev=${DEV}@${thread}
-    echo "Device: $dev"
-    cat /proc/net/pktgen/$dev | grep -A2 "Result:"
-done
+print_result
