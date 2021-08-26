ad70c03f211a9fad32af1c1b551738c741f7c751
Colin Ian King <colin.king@canonical.com>
2021-08-26 13:32:50 +0100

    mfd: ti_am335x_tscadc: Fix spelling mistake "atleast" -> "at least"
    There is a spelling mistake in a dev_err message. Fix it.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/ti_am335x_tscadc.c
+++ drivers/mfd/ti_am335x_tscadc.c
@@ -186,7 +186,7 @@ static	int ti_tscadc_probe(struct platform_device *pdev)
 	}
 
 	if (total_channels == 0) {
-		dev_err(&pdev->dev, "Need atleast one channel.\n");
+		dev_err(&pdev->dev, "Need at least one channel.\n");
 		return -EINVAL;
 	}
 
