torvalds/linux

commit 93993a9497ed707974c9e16210d3c2ff002a756e

soc: rockchip: power-domain: export pd on/off and pd status
Some special applications of video may require:
rockchip_pmu_pd_on(dev)---> force power on pd
rockchip_pmu_pd_off(dev)---> force power down pd
rockchip_pmu_pd_is_on(dev)---> pd status

Change-Id: I264d76559aef0b0540130bf29a4635a3f5380a7c
Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>

include/soc/rockchip/pm_domains.h
@@ -7,6 +7,9 @@
 struct device;
 
 #ifdef CONFIG_ROCKCHIP_PM_DOMAINS
+int rockchip_pmu_pd_on(struct device *dev);
+int rockchip_pmu_pd_off(struct device *dev);
+bool rockchip_pmu_pd_is_on(struct device *dev);
 int rockchip_pmu_idle_request(struct device *dev, bool idle);
 int rockchip_save_qos(struct device *dev);
 int rockchip_restore_qos(struct device *dev);

drivers/soc/rockchip/pm_domains.c
@@ -500,6 +500,65 @@ static int rockchip_pd_power_off(struct generic_pm_domain *domain)
 	return rockchip_pd_power(pd, false);
 }
 
+int rockchip_pmu_pd_on(struct device *dev)
+{
+	struct generic_pm_domain *genpd;
+	struct rockchip_pm_domain *pd;
+
+	if (IS_ERR_OR_NULL(dev))
+		return -EINVAL;
+
+	if (IS_ERR_OR_NULL(dev->pm_domain))
+		return -EINVAL;
+
+	genpd = pd_to_genpd(dev->pm_domain);
+	pd = to_rockchip_pd(genpd);
+
+	return rockchip_pd_power(pd, true);
+}
+EXPORT_SYMBOL(rockchip_pmu_pd_on);
+
+int rockchip_pmu_pd_off(struct device *dev)
+{
+	struct generic_pm_domain *genpd;
+	struct rockchip_pm_domain *pd;
+
+	if (IS_ERR_OR_NULL(dev))
+		return -EINVAL;
+
+	if (IS_ERR_OR_NULL(dev->pm_domain))
+		return -EINVAL;
+
+	genpd = pd_to_genpd(dev->pm_domain);
+	pd = to_rockchip_pd(genpd);
+
+	return rockchip_pd_power(pd, false);
+}
+EXPORT_SYMBOL(rockchip_pmu_pd_off);
+
+bool rockchip_pmu_pd_is_on(struct device *dev)
+{
+	struct generic_pm_domain *genpd;
+	struct rockchip_pm_domain *pd;
+	bool is_on;
+
+	if (IS_ERR_OR_NULL(dev))
+		return false;
+
+	if (IS_ERR_OR_NULL(dev->pm_domain))
+		return false;
+
+	genpd = pd_to_genpd(dev->pm_domain);
+	pd = to_rockchip_pd(genpd);
+
+	rockchip_pmu_lock(pd);
+	is_on = rockchip_pmu_domain_is_on(pd);
+	rockchip_pmu_unlock(pd);
+
+	return is_on;
+}
+EXPORT_SYMBOL(rockchip_pmu_pd_is_on);
+
 static int rockchip_pd_attach_dev(struct generic_pm_domain *genpd,
 				  struct device *dev)
 {

commit bb1c2a5cc52ad39197b6acf00284123e7e21a1ff

soc: rockchip: power-domain: Add missing dummy definition for pd on/off and pd status
Fixes a build break when CONFIG_ROCKCHIP_PM_DOMAINS is not selected.

drivers/video/rockchip/mpp/mpp_common.c:2286:13: error: implicit declaration of function 'rockchip_pmu_pd_is_on' [-Werror,-Wimplicit-function-declaration]
        pd_is_on = rockchip_pmu_pd_is_on(mpp->dev);
                   ^
drivers/video/rockchip/mpp/mpp_common.c:2288:3: error: implicit declaration of function 'rockchip_pmu_pd_on' [-Werror,-Wimplicit-function-declaration]
                rockchip_pmu_pd_on(mpp->dev);
                ^
drivers/video/rockchip/mpp/mpp_common.c:2304:3: error: implicit declaration of function 'rockchip_pmu_pd_off' [-Werror,-Wimplicit-function-declaration]
                rockchip_pmu_pd_off(mpp->dev);
                ^

Fixes: 93993a9497ed ("soc: rockchip: power-domain: export pd on/off and pd status")
Signed-off-by: Tao Huang <huangtao@rock-chips.com>
Change-Id: Idd4039639fb0884a6fccdec0e22f37888a301a98

include/soc/rockchip/pm_domains.h
@@ -15,6 +15,21 @@ int rockchip_save_qos(struct device *dev);
 int rockchip_restore_qos(struct device *dev);
 void rockchip_dump_pmu(void);
 #else
+static inline int rockchip_pmu_pd_on(struct device *dev)
+{
+	return -ENOTSUPP;
+}
+
+static inline int rockchip_pmu_pd_off(struct device *dev)
+{
+	return -ENOTSUPP;
+}
+
+static inline bool rockchip_pmu_pd_is_on(struct device *dev)
+{
+	return true;
+}
+
 static inline int rockchip_pmu_idle_request(struct device *dev, bool idle)
 {
 	return -ENOTSUPP;