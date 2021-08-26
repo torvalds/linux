19ba2e8e2744fb39958a8581ed9dfdd1a322349c
Vandita Kulkarni <vandita.kulkarni@intel.com>
2021-08-26 11:18:11 +0530

    drm/i915/dsi/xelpd: Enable mipi dsi support.
    Enable MIPI DSI support on ADL-P platform.
    The esc clock changes, WA changes are taken care
    in the previous patches.
    As per the Bspec the seq remains to be same as TGL.
    
    Signed-off-by: Vandita Kulkarni <vandita.kulkarni@intel.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210826054811.10572-3-vandita.kulkarni@intel.com

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -10853,6 +10853,7 @@ static void intel_setup_outputs(struct drm_i915_private *dev_priv)
 		intel_ddi_init(dev_priv, PORT_TC2);
 		intel_ddi_init(dev_priv, PORT_TC3);
 		intel_ddi_init(dev_priv, PORT_TC4);
+		icl_dsi_init(dev_priv);
 	} else if (IS_ALDERLAKE_S(dev_priv)) {
 		intel_ddi_init(dev_priv, PORT_A);
 		intel_ddi_init(dev_priv, PORT_TC1);
