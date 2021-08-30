3b5c53bd3ec0995f583727ae9fda4c0035d0744c
Michael Straube <straube.linux@gmail.com>
2021-08-30 14:15:02 +0200

    staging: r8188eu: ensure mac address buffer is properly aligned
    Ensure the mac address buffer in struct eeprom_priv is properly
    aligned for use with functions from <linux/etherdevice.h>.
    
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Link: https://lore.kernel.org/r/20210830121508.6411-3-straube.linux@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/r8188eu/include/rtw_eeprom.h
+++ drivers/staging/r8188eu/include/rtw_eeprom.h
@@ -99,7 +99,7 @@ struct eeprom_priv {
 	u8		bautoload_fail_flag;
 	u8		bloadfile_fail_flag;
 	u8		bloadmac_fail_flag;
-	u8		mac_addr[6];	/* PermanentAddress */
+	u8		mac_addr[6] __aligned(2); /* PermanentAddress */
 	u16		channel_plan;
 	u8		EepromOrEfuse;
 	u8		efuse_eeprom_data[HWSET_MAX_SIZE_512] __aligned(4);
