c02599f210d90c76e42e2eb544511a47cceb60c9
Chao Yu <chao@kernel.org>
2021-09-01 16:06:21 +0800

    f2fs: avoid attaching SB_ACTIVE flag during mount
    Quoted from [1]
    
    "I do remember that I've added this code back then because otherwise
    orphan cleanup was losing updates to quota files. But you're right
    that now I don't see how that could be happening and it would be nice
    if we could get rid of this hack"
    
    [1] https://lore.kernel.org/linux-ext4/99cce8ca-e4a0-7301-840f-2ace67c551f3@huawei.com/T/#m04990cfbc4f44592421736b504afcc346b2a7c00
    
    Related fix in ext4 by
    commit 72ffb49a7b62 ("ext4: do not set SB_ACTIVE in ext4_orphan_cleanup()").
    
    f2fs has the same hack implementation in
    - f2fs_recover_orphan_inodes()
    - f2fs_recover_fsync_data()
    
    Let's get rid of this hack as well in f2fs.
    
    Cc: Zhang Yi <yi.zhang@huawei.com>
    Cc: Jan Kara <jack@suse.cz>
    Acked-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/recovery.c
+++ fs/f2fs/recovery.c
@@ -787,8 +787,6 @@ int f2fs_recover_fsync_data(struct f2fs_sb_info *sbi, bool check_only)
 	}
 
 #ifdef CONFIG_QUOTA
-	/* Needed for iput() to work correctly and not trash data */
-	sbi->sb->s_flags |= SB_ACTIVE;
 	/* Turn on quotas so that they are updated correctly */
 	quota_enabled = f2fs_enable_quota_files(sbi, s_flags & SB_RDONLY);
 #endif
@@ -816,10 +814,8 @@ int f2fs_recover_fsync_data(struct f2fs_sb_info *sbi, bool check_only)
 	err = recover_data(sbi, &inode_list, &tmp_inode_list, &dir_list);
 	if (!err)
 		f2fs_bug_on(sbi, !list_empty(&inode_list));
-	else {
-		/* restore s_flags to let iput() trash data */
-		sbi->sb->s_flags = s_flags;
-	}
+	else
+		f2fs_bug_on(sbi, sbi->sb->s_flags & SB_ACTIVE);
 skip:
 	fix_curseg_write_pointer = !check_only || list_empty(&inode_list);
 
