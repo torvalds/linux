a1bc561bb2d3d9b944878955095f53aeba30a166
Ohhoon Kwon <ohoono.kwon@samsung.com>
2021-09-02 14:56:58 -0700

    mm: sparse: pass section_nr to section_mark_present
    Patch series "mm: sparse: remove __section_nr() function", v4.
    
    This patch (of 3):
    
    With CONFIG_SPARSEMEM_EXTREME enabled, __section_nr() which converts
    mem_section to section_nr could be costly since it iterates all section
    roots to check if the given mem_section is in its range.
    
    Since both callers of section_mark_present already know section_nr, let's
    also pass section_nr as well as mem_section in order to reduce costly
    translation.
    
    Link: https://lkml.kernel.org/r/20210707150212.855-1-ohoono.kwon@samsung.com
    Link: https://lkml.kernel.org/r/20210707150212.855-2-ohoono.kwon@samsung.com
    Signed-off-by: Ohhoon Kwon <ohoono.kwon@samsung.com>
    Acked-by: Mike Rapoport <rppt@linux.ibm.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Cc: Baoquan He <bhe@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/sparse.c
+++ mm/sparse.c
@@ -187,10 +187,9 @@ void __meminit mminit_validate_memmodel_limits(unsigned long *start_pfn,
  * those loops early.
  */
 unsigned long __highest_present_section_nr;
-static void section_mark_present(struct mem_section *ms)
+static void __section_mark_present(struct mem_section *ms,
+		unsigned long section_nr)
 {
-	unsigned long section_nr = __section_nr(ms);
-
 	if (section_nr > __highest_present_section_nr)
 		__highest_present_section_nr = section_nr;
 
@@ -280,7 +279,7 @@ static void __init memory_present(int nid, unsigned long start, unsigned long en
 		if (!ms->section_mem_map) {
 			ms->section_mem_map = sparse_encode_early_nid(nid) |
 							SECTION_IS_ONLINE;
-			section_mark_present(ms);
+			__section_mark_present(ms, section);
 		}
 	}
 }
@@ -934,7 +933,7 @@ int __meminit sparse_add_section(int nid, unsigned long start_pfn,
 
 	ms = __nr_to_section(section_nr);
 	set_section_nid(section_nr, nid);
-	section_mark_present(ms);
+	__section_mark_present(ms, section_nr);
 
 	/* Align memmap to section boundary in the subsection case */
 	if (section_nr_to_pfn(section_nr) != start_pfn)
