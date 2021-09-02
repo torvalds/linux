e32d20c0c88b1cd0a44f882c4f0eb2f536363d1b
Mike Kravetz <mike.kravetz@oracle.com>
2021-09-02 14:58:50 -0700

    hugetlb: before freeing hugetlb page set dtor to appropriate value
    When removing a hugetlb page from the pool the ref count is set to one (as
    the free page has no ref count) and compound page destructor is set to
    NULL_COMPOUND_DTOR.  Since a subsequent call to free the hugetlb page will
    call __free_pages for non-gigantic pages and free_gigantic_page for
    gigantic pages the destructor is not used.
    
    However, consider the following race with code taking a speculative
    reference on the page:
    
    Thread 0				Thread 1
    --------				--------
    remove_hugetlb_page
      set_page_refcounted(page);
      set_compound_page_dtor(page,
               NULL_COMPOUND_DTOR);
    					get_page_unless_zero(page)
    __update_and_free_page
      __free_pages(page,
               huge_page_order(h));
    
    		/* Note that __free_pages() will simply drop
    		   the reference to the page. */
    
    					put_page(page)
    					  __put_compound_page()
    					    destroy_compound_page
    					      NULL_COMPOUND_DTOR
    						BUG: kernel NULL pointer
    						dereference, address:
    						0000000000000000
    
    To address this race, set the dtor to the normal compound page dtor for
    non-gigantic pages.  The dtor for gigantic pages does not matter as
    gigantic pages are changed from a compound page to 'just a group of pages'
    before freeing.  Hence, the destructor is not used.
    
    Link: https://lkml.kernel.org/r/20210809184832.18342-4-mike.kravetz@oracle.com
    Signed-off-by: Mike Kravetz <mike.kravetz@oracle.com>
    Reviewed-by: Muchun Song <songmuchun@bytedance.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Naoya Horiguchi <naoya.horiguchi@linux.dev>
    Cc: Mina Almasry <almasrymina@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -1370,8 +1370,28 @@ static void remove_hugetlb_page(struct hstate *h, struct page *page,
 		h->surplus_huge_pages_node[nid]--;
 	}
 
+	/*
+	 * Very subtle
+	 *
+	 * For non-gigantic pages set the destructor to the normal compound
+	 * page dtor.  This is needed in case someone takes an additional
+	 * temporary ref to the page, and freeing is delayed until they drop
+	 * their reference.
+	 *
+	 * For gigantic pages set the destructor to the null dtor.  This
+	 * destructor will never be called.  Before freeing the gigantic
+	 * page destroy_compound_gigantic_page will turn the compound page
+	 * into a simple group of pages.  After this the destructor does not
+	 * apply.
+	 *
+	 * This handles the case where more than one ref is held when and
+	 * after update_and_free_page is called.
+	 */
 	set_page_refcounted(page);
-	set_compound_page_dtor(page, NULL_COMPOUND_DTOR);
+	if (hstate_is_gigantic(h))
+		set_compound_page_dtor(page, NULL_COMPOUND_DTOR);
+	else
+		set_compound_page_dtor(page, COMPOUND_PAGE_DTOR);
 
 	h->nr_huge_pages--;
 	h->nr_huge_pages_node[nid]--;
