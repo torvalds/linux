2fc7acb69fa3573d4bf7a90c323296d840daf330
Pavel Skripkin <paskripkin@gmail.com>
2021-09-02 23:27:56 +0300

    Bluetooth: hci_uart: fix GPF in h5_recv
    Syzbot hit general protection fault in h5_recv(). The problem was in
    missing NULL check.
    
    hu->serdev can be NULL and we cannot blindly pass &serdev->dev
    somewhere, since it can cause GPF.
    
    Fixes: d9dd833cf6d2 ("Bluetooth: hci_h5: Add runtime suspend")
    Reported-and-tested-by: syzbot+7d41312fe3f123a6f605@syzkaller.appspotmail.com
    Signed-off-by: Pavel Skripkin <paskripkin@gmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/hci_h5.c
+++ drivers/bluetooth/hci_h5.c
@@ -587,9 +587,11 @@ static int h5_recv(struct hci_uart *hu, const void *data, int count)
 		count -= processed;
 	}
 
-	pm_runtime_get(&hu->serdev->dev);
-	pm_runtime_mark_last_busy(&hu->serdev->dev);
-	pm_runtime_put_autosuspend(&hu->serdev->dev);
+	if (hu->serdev) {
+		pm_runtime_get(&hu->serdev->dev);
+		pm_runtime_mark_last_busy(&hu->serdev->dev);
+		pm_runtime_put_autosuspend(&hu->serdev->dev);
+	}
 
 	return 0;
 }
