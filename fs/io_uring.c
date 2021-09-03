31efe48eb5dc4de3e31e84b54f287e9665410ab3
Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
2021-09-03 22:24:36 +0800

    io_uring: fix possible poll event lost in multi shot mode
    IIUC, IORING_POLL_ADD_MULTI is similar to epoll's edge-triggered mode,
    that means once one pure poll request returns one event(cqe), we'll
    need to read or write continually until EAGAIN is returned, then I think
    there is a possible poll event lost race in multi shot mode:
    
    t1  poll request add |                         |
    t2                   |                         |
    t3  event happens    |                         |
    t4  task work add    |                         |
    t5                   | task work run           |
    t6                   |   commit one cqe        |
    t7                   |                         | user app handles cqe
    t8                   |   new event happen      |
    t9                   |   add back to waitqueue |
    t10                  |
    
    After t6 but before t9, if new event happens, there'll be no wakeup
    operation, and if user app has picked up this cqe in t7, read or write
    until EAGAIN is returned. In t8, new event happens and will be lost,
    though this race window maybe small.
    
    To fix this possible race, add poll request back to waitqueue before
    committing cqe.
    
    Fixes: 88e41cf928a6 ("io_uring: add multishot mode for IORING_OP_POLL_ADD")
    Signed-off-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
    Link: https://lore.kernel.org/r/20210903142436.5767-1-xiaoguang.wang@linux.alibaba.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -5098,7 +5098,7 @@ static void io_poll_remove_double(struct io_kiocb *req)
 	}
 }
 
-static bool io_poll_complete(struct io_kiocb *req, __poll_t mask)
+static bool __io_poll_complete(struct io_kiocb *req, __poll_t mask)
 	__must_hold(&req->ctx->completion_lock)
 {
 	struct io_ring_ctx *ctx = req->ctx;
@@ -5120,8 +5120,17 @@ static bool io_poll_complete(struct io_kiocb *req, __poll_t mask)
 	if (flags & IORING_CQE_F_MORE)
 		ctx->cq_extra++;
 
-	io_commit_cqring(ctx);
 	return !(flags & IORING_CQE_F_MORE);
+}
+
+static inline bool io_poll_complete(struct io_kiocb *req, __poll_t mask)
+	__must_hold(&req->ctx->completion_lock)
+{
+	bool done;
+
+	done = __io_poll_complete(req, mask);
+	io_commit_cqring(req->ctx);
+	return done;
 }
 
 static void io_poll_task_func(struct io_kiocb *req, bool *locked)
@@ -5134,7 +5143,7 @@ static void io_poll_task_func(struct io_kiocb *req, bool *locked)
 	} else {
 		bool done;
 
-		done = io_poll_complete(req, req->result);
+		done = __io_poll_complete(req, req->result);
 		if (done) {
 			io_poll_remove_double(req);
 			hash_del(&req->hash_node);
@@ -5142,6 +5151,7 @@ static void io_poll_task_func(struct io_kiocb *req, bool *locked)
 			req->result = 0;
 			add_wait_queue(req->poll.head, &req->poll.wait);
 		}
+		io_commit_cqring(ctx);
 		spin_unlock(&ctx->completion_lock);
 		io_cqring_ev_posted(ctx);
 
