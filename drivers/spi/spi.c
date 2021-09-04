44ea62813f0ab3d718de480504f4dfd0bdd01858
Jakub Kicinski <kuba@kernel.org>
2021-09-03 18:31:40 -0700

    spi: don't include ptp_clock_kernel.h in spi.h
    Commit b42faeee718c ("spi: Add a PTP system timestamp
    to the transfer structure") added an include of ptp_clock_kernel.h
    to spi.h for struct ptp_system_timestamp but a forward declaration
    is enough. Let's use that to limit the number of objects we have
    to rebuild every time we touch networking headers.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Tested-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Link: https://lore.kernel.org/r/20210904013140.2377609-1-kuba@kernel.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -33,6 +33,7 @@
 #include <linux/highmem.h>
 #include <linux/idr.h>
 #include <linux/platform_data/x86/apple.h>
+#include <linux/ptp_clock_kernel.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/spi.h>
