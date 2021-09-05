0319b848b155185815724e1b46103c550627a845
Geert Uytterhoeven <geert@linux-m68k.org>
2021-09-05 11:30:34 +0200

    binfmt: a.out: Fix bogus semicolon
        fs/binfmt_aout.c: In function ‘load_aout_library’:
        fs/binfmt_aout.c:311:27: error: expected ‘)’ before ‘;’ token
          311 |    MAP_FIXED | MAP_PRIVATE;
    	  |                           ^
        fs/binfmt_aout.c:309:10: error: too few arguments to function ‘vm_mmap’
          309 |  error = vm_mmap(file, start_addr, ex.a_text + ex.a_data,
    	  |          ^~~~~~~
        In file included from fs/binfmt_aout.c:12:
        include/linux/mm.h:2626:35: note: declared here
         2626 | extern unsigned long __must_check vm_mmap(struct file *, unsigned long,
    	  |                                   ^~~~~~~
    
    Fix this by reverting the accidental replacement of a comma by a
    semicolon.
    
    Fixes: 42be8b42535183f8 ("binfmt: don't use MAP_DENYWRITE when loading shared libraries via uselib()")
    Reported-by: noreply@ellerman.id.au
    Reported-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/binfmt_aout.c
+++ fs/binfmt_aout.c
@@ -308,7 +308,7 @@ static int load_aout_library(struct file *file)
 	/* Now use mmap to map the library into memory. */
 	error = vm_mmap(file, start_addr, ex.a_text + ex.a_data,
 			PROT_READ | PROT_WRITE | PROT_EXEC,
-			MAP_FIXED | MAP_PRIVATE;
+			MAP_FIXED | MAP_PRIVATE,
 			N_TXTOFF(ex));
 	retval = error;
 	if (error != start_addr)
