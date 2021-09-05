63a4881572d7da18d423a5a0a5421dc30e83d114
Hans de Goede <hdegoede@redhat.com>
2021-09-05 15:05:01 +0200

    drm: panel-orientation-quirks: Add quirk for the Chuwi HiBook
    The Chuwi HiBook uses a panel which has been mounted
    90 degrees rotated. Add a quirk for this.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Reviewed-by: Thierry Reding <treding@nvidia.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210905130501.32980-1-hdegoede@redhat.com

--- drivers/gpu/drm/drm_panel_orientation_quirks.c
+++ drivers/gpu/drm/drm_panel_orientation_quirks.c
@@ -140,6 +140,14 @@ static const struct dmi_system_id orientation_data[] = {
 		  DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "T103HAF"),
 		},
 		.driver_data = (void *)&lcd800x1280_rightside_up,
+	}, {	/* Chuwi HiBook (CWI514) */
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "Hampoo"),
+			DMI_MATCH(DMI_BOARD_NAME, "Cherry Trail CR"),
+			/* Above matches are too generic, add bios-date match */
+			DMI_MATCH(DMI_BIOS_DATE, "05/07/2016"),
+		},
+		.driver_data = (void *)&lcd1200x1920_rightside_up,
 	}, {	/* Chuwi Hi10 Pro (CWI529) */
 		.matches = {
 		  DMI_EXACT_MATCH(DMI_BOARD_VENDOR, "Hampoo"),
