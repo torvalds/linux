38993788f40c78c64ff68aa68877ca6cbeac05a1
Dan Carpenter <dan.carpenter@oracle.com>
2021-09-06 12:43:01 +0300

    ceph: fix off by one bugs in unsafe_request_wait()
    [ Upstream commit 708c87168b6121abc74b2a57d0c498baaf70cbea ]
    
    The "> max" tests should be ">= max" to prevent an out of bounds access
    on the next lines.
    
    Fixes: e1a4541ec0b9 ("ceph: flush the mdlog before waiting on unsafe reqs")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Reviewed-by: Ilya Dryomov <idryomov@gmail.com>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>
    Stable-dep-of: 5bd76b8de5b7 ("ceph: fix NULL pointer dereference for req->r_session")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/ceph/caps.c
+++ fs/ceph/caps.c
@@ -2343,7 +2343,7 @@ retry:
 			list_for_each_entry(req, &ci->i_unsafe_dirops,
 					    r_unsafe_dir_item) {
 				s = req->r_session;
-				if (unlikely(s->s_mds > max)) {
+				if (unlikely(s->s_mds >= max)) {
 					spin_unlock(&ci->i_unsafe_lock);
 					goto retry;
 				}
@@ -2357,7 +2357,7 @@ retry:
 			list_for_each_entry(req, &ci->i_unsafe_iops,
 					    r_unsafe_target_item) {
 				s = req->r_session;
-				if (unlikely(s->s_mds > max)) {
+				if (unlikely(s->s_mds >= max)) {
 					spin_unlock(&ci->i_unsafe_lock);
 					goto retry;
 				}
