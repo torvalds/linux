006a5099fc18a43792b01d002e1e45c5541ea666
Andrii Nakryiko <andrii@kernel.org>
2021-09-07 15:10:23 -0700

    libbpf: Fix build with latest gcc/binutils with LTO
    After updating to binutils 2.35, the build began to fail with an
    assembler error. A bug was opened on the Red Hat Bugzilla a few days
    later for the same issue.
    
    Work around the problem by using the new `symver` attribute (introduced
    in GCC 10) as needed instead of assembler directives.
    
    This addresses Red Hat ([0]) and OpenSUSE ([1]) bug reports, as well as libbpf
    issue ([2]).
    
      [0]: https://bugzilla.redhat.com/show_bug.cgi?id=1863059
      [1]: https://bugzilla.opensuse.org/show_bug.cgi?id=1188749
      [2]: Closes: https://github.com/libbpf/libbpf/issues/338
    
    Co-developed-by: Patrick McCarty <patrick.mccarty@intel.com>
    Co-developed-by: Michal Suchanek <msuchanek@suse.de>
    Signed-off-by: Patrick McCarty <patrick.mccarty@intel.com>
    Signed-off-by: Michal Suchanek <msuchanek@suse.de>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20210907221023.2660953-1-andrii@kernel.org

--- tools/lib/bpf/libbpf_internal.h
+++ tools/lib/bpf/libbpf_internal.h
@@ -90,17 +90,30 @@
 /* Symbol versioning is different between static and shared library.
  * Properly versioned symbols are needed for shared library, but
  * only the symbol of the new version is needed for static library.
+ * Starting with GNU C 10, use symver attribute instead of .symver assembler
+ * directive, which works better with GCC LTO builds.
  */
-#ifdef SHARED
-# define COMPAT_VERSION(internal_name, api_name, version) \
+#if defined(SHARED) && defined(__GNUC__) && __GNUC__ >= 10
+
+#define DEFAULT_VERSION(internal_name, api_name, version) \
+	__attribute__((symver(#api_name "@@" #version)))
+#define COMPAT_VERSION(internal_name, api_name, version) \
+	__attribute__((symver(#api_name "@" #version)))
+
+#elif defined(SHARED)
+
+#define COMPAT_VERSION(internal_name, api_name, version) \
 	asm(".symver " #internal_name "," #api_name "@" #version);
-# define DEFAULT_VERSION(internal_name, api_name, version) \
+#define DEFAULT_VERSION(internal_name, api_name, version) \
 	asm(".symver " #internal_name "," #api_name "@@" #version);
-#else
-# define COMPAT_VERSION(internal_name, api_name, version)
-# define DEFAULT_VERSION(internal_name, api_name, version) \
+
+#else /* !SHARED */
+
+#define COMPAT_VERSION(internal_name, api_name, version)
+#define DEFAULT_VERSION(internal_name, api_name, version) \
 	extern typeof(internal_name) api_name \
 	__attribute__((alias(#internal_name)));
+
 #endif
 
 extern void libbpf_print(enum libbpf_print_level level,
