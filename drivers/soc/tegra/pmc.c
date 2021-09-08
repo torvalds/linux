76d89474310e05d9c592a8beed2aefe4d3396d2d
Cai Huoqing <caihuoqing@baidu.com>
2021-09-08 15:17:24 +0800

    soc/tegra: pmc: Use devm_platform_ioremap_resource()
    Use the devm_platform_ioremap_resource() helper instead of calling
    platform_get_resource() and devm_ioremap_resource() separately.
    
    Signed-off-by: Cai Huoqing <caihuoqing@baidu.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/soc/tegra/pmc.c
+++ drivers/soc/tegra/pmc.c
@@ -2816,8 +2816,7 @@ static int tegra_pmc_probe(struct platform_device *pdev)
 		return err;
 
 	/* take over the memory region from the early initialization */
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	base = devm_ioremap_resource(&pdev->dev, res);
+	base = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(base))
 		return PTR_ERR(base);
 
