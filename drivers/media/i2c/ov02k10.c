07d6f292b6bc18a555f31a13b0010b7528f67bff
Zefa Chen <zefa.chen@rock-chips.com>
2021-09-08 18:11:16 +0800

    media: i2c: ov02k10 fix compile error
    Signed-off-by: Zefa Chen <zefa.chen@rock-chips.com>
    Change-Id: I5c463e531a8b97d874e3dc793095f4f481961ea5

--- drivers/media/i2c/ov02k10.c
+++ drivers/media/i2c/ov02k10.c
@@ -844,7 +844,7 @@ static int ov02k10_g_frame_interval(struct v4l2_subdev *sd,
 	return 0;
 }
 
-static int ov02k10_g_mbus_config(struct v4l2_subdev *sd,
+static int ov02k10_g_mbus_config(struct v4l2_subdev *sd, unsigned int pad_id,
 				 struct v4l2_mbus_config *config)
 {
 	struct ov02k10 *ov02k10 = to_ov02k10(sd);
@@ -861,7 +861,7 @@ static int ov02k10_g_mbus_config(struct v4l2_subdev *sd,
 		V4L2_MBUS_CSI2_CONTINUOUS_CLOCK |
 		V4L2_MBUS_CSI2_CHANNEL_1;
 
-	config->type = V4L2_MBUS_CSI2;
+	config->type = V4L2_MBUS_CSI2_DPHY;
 	config->flags = val;
 
 	return 0;
@@ -1573,7 +1573,6 @@ static const struct v4l2_subdev_core_ops ov02k10_core_ops = {
 static const struct v4l2_subdev_video_ops ov02k10_video_ops = {
 	.s_stream = ov02k10_s_stream,
 	.g_frame_interval = ov02k10_g_frame_interval,
-	.g_mbus_config = ov02k10_g_mbus_config,
 };
 
 static const struct v4l2_subdev_pad_ops ov02k10_pad_ops = {
@@ -1583,6 +1582,7 @@ static const struct v4l2_subdev_pad_ops ov02k10_pad_ops = {
 	.get_fmt = ov02k10_get_fmt,
 	.set_fmt = ov02k10_set_fmt,
 	.get_selection = ov02k10_get_selection,
+	.get_mbus_config = ov02k10_g_mbus_config,
 };
 
 static const struct v4l2_subdev_ops ov02k10_subdev_ops = {
