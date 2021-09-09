6b0be25ca029469b5515119b96a6d684cf7d1e82
Ajay Garg <ajaygargnsit@gmail.com>
2021-09-09 17:36:24 +0530

    usb: gadget: fix for a typo that conveys logically-inverted information.
    Upon calling usb_ep_autoconfig_release(), the endpoint becomes *not
    valid*.
    
    Signed-off-by: Ajay Garg <ajaygargnsit@gmail.com>
    Link: https://lore.kernel.org/r/20210909120624.77557-1-ajaygargnsit@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/gadget/epautoconf.c
+++ drivers/usb/gadget/epautoconf.c
@@ -181,7 +181,7 @@ EXPORT_SYMBOL_GPL(usb_ep_autoconfig);
  * This function can be used during function bind for endpoints obtained
  * from usb_ep_autoconfig(). It unclaims endpoint claimed by
  * usb_ep_autoconfig() to make it available for other functions. Endpoint
- * which was released is no longer invalid and shouldn't be used in
+ * which was released is no longer valid and shouldn't be used in
  * context of function which released it.
  */
 void usb_ep_autoconfig_release(struct usb_ep *ep)
