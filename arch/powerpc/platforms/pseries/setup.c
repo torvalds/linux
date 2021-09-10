452f145eca73945222923525a7eba59cf37909cc
Niklas Schnelle <schnelle@linux.ibm.com>
2021-09-10 16:19:40 +0200

    powerpc: Drop superfluous pci_dev_is_added() calls
    On powerpc, pci_dev_is_added() is called as part of SR-IOV fixups
    that are done under pcibios_add_device() which in turn is only called in
    pci_device_add() whih is called when a PCI device is scanned.
    
    pci_dev_assign_added() is called in pci_bus_add_device() which is only
    called after scanning the device. Thus pci_dev_is_added() is always
    false and can be dropped.
    
    Signed-off-by: Niklas Schnelle <schnelle@linux.ibm.com>
    Reviewed-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Oliver O'Halloran <oohall@gmail.com>
    [mpe: Tweak change log slightly to reflect Oliver's comments]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20210910141940.2598035-2-schnelle@linux.ibm.com

--- arch/powerpc/platforms/pseries/setup.c
+++ arch/powerpc/platforms/pseries/setup.c
@@ -74,7 +74,6 @@
 #include <asm/hvconsole.h>
 
 #include "pseries.h"
-#include "../../../../drivers/pci/pci.h"
 
 DEFINE_STATIC_KEY_FALSE(shared_processor);
 EXPORT_SYMBOL(shared_processor);
@@ -750,7 +749,7 @@ static void pseries_pci_fixup_iov_resources(struct pci_dev *pdev)
 	const int *indexes;
 	struct device_node *dn = pci_device_to_OF_node(pdev);
 
-	if (!pdev->is_physfn || pci_dev_is_added(pdev))
+	if (!pdev->is_physfn)
 		return;
 	/*Firmware must support open sriov otherwise dont configure*/
 	indexes = of_get_property(dn, "ibm,open-sriov-vf-bar-info", NULL);
