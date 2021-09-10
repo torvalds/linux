39371f7d1396fa281eda67d0ede8221e5d8defe5
Meenakshikumar Somasundaram <meenakshikumar.somasundaram@amd.com>
2021-09-10 11:18:41 -0400

    drm/amd/display: Creating a fw boot options bit for an upcoming feature
    [Why]
    Need a bit for x86 driver to enable a FW boot option for an upcoming
    feature.
    
    [How]
    Added a bit in dmub_fw_boot_options for an upcoming feature.
    
    Reviewed-by: Jimmy Kizito <jimmy.kizito@amd.com>
    Reviewed-by: Jun Lei <Jun.Lei@amd.com>
    Acked-by: Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>
    Signed-off-by: Meenakshikumar Somasundaram <meenakshikumar.somasundaram@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dmub/inc/dmub_cmd.h
+++ drivers/gpu/drm/amd/display/dmub/inc/dmub_cmd.h
@@ -372,7 +372,8 @@ union dmub_fw_boot_options {
 		uint32_t reserved2: 1; /**< reserved for an unreleased feature */
 		uint32_t reserved_unreleased1: 1; /**< reserved for an unreleased feature */
 		uint32_t invalid_vbios_data: 1; /**< 1 if VBIOS data table is invalid */
-		uint32_t reserved : 23; /**< reserved */
+		uint32_t reserved_unreleased2: 1; /**< reserved for an unreleased feature */
+		uint32_t reserved : 22; /**< reserved */
 	} bits; /**< boot bits */
 	uint32_t all; /**< 32-bit access to bits */
 };
