6025643596895695956c27119c6b0bfa40d8035b
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2021-09-10 18:18:16 -0700

    sched/fair: Optimize checking for group_asym_packing
    sched_asmy_prefer() always returns false when called on the local group. By
    checking local_group, we can avoid additional checks and invoking
    sched_asmy_prefer() when it is not needed. No functional changes are
    introduced.
    
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Reviewed-by: Len Brown <len.brown@intel.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20210911011819.12184-4-ricardo.neri-calderon@linux.intel.com

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8631,7 +8631,7 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 	}
 
 	/* Check if dst CPU is idle and preferred to this group */
-	if (env->sd->flags & SD_ASYM_PACKING &&
+	if (!local_group && env->sd->flags & SD_ASYM_PACKING &&
 	    env->idle != CPU_NOT_IDLE &&
 	    sgs->sum_h_nr_running &&
 	    sched_asym_prefer(env->dst_cpu, group->asym_prefer_cpu)) {
