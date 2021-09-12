aa54686e285c13bfbafc02e7cc3cf18f2fbca4b0
Dmitry Osipenko <digetx@gmail.com>
2021-09-12 23:29:03 +0300

    soc/tegra: irq: Add stubs needed for compile testing
    Add stubs needed for compile-testing of tegra-cpuidle driver.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- include/soc/tegra/irq.h
+++ include/soc/tegra/irq.h
@@ -6,8 +6,15 @@
 #ifndef __SOC_TEGRA_IRQ_H
 #define __SOC_TEGRA_IRQ_H
 
-#if defined(CONFIG_ARM)
+#include <linux/types.h>
+
+#if defined(CONFIG_ARM) && defined(CONFIG_ARCH_TEGRA)
 bool tegra_pending_sgi(void);
+#else
+static inline bool tegra_pending_sgi(void)
+{
+	return false;
+}
 #endif
 
 #endif /* __SOC_TEGRA_IRQ_H */
