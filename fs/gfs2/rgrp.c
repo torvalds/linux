6edb6ba333d31bb42d9ab8b1b6ff1c5ecbc3813c
Bob Peterson <rpeterso@redhat.com>
2021-09-13 12:34:38 -0500

    gfs2: remove redundant check in gfs2_rgrp_go_lock
    Before this patch, function gfs2_rgrp_go_lock checked if GL_SKIP and
    ar_rgrplvb were both true. However, GL_SKIP is only set for rgrps if
    ar_rgrplvb is true (see gfs2_inplace_reserve). This patch simply removes
    the redundant check.
    
    Signed-off-by: Bob Peterson <rpeterso@redhat.com>
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- fs/gfs2/rgrp.c
+++ fs/gfs2/rgrp.c
@@ -1291,9 +1291,8 @@ static int update_rgrp_lvb(struct gfs2_rgrpd *rgd)
 int gfs2_rgrp_go_lock(struct gfs2_holder *gh)
 {
 	struct gfs2_rgrpd *rgd = gh->gh_gl->gl_object;
-	struct gfs2_sbd *sdp = rgd->rd_sbd;
 
-	if (gh->gh_flags & GL_SKIP && sdp->sd_args.ar_rgrplvb)
+	if (gh->gh_flags & GL_SKIP)
 		return 0;
 	return gfs2_rgrp_bh_get(rgd);
 }
