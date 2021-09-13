6b4a6b7f07883d96e10924b8e702cf62acdc9b90
Abel Vesa <abel.vesa@nxp.com>
2021-09-13 11:24:50 +0300

    clk: imx: Rework imx_clk_hw_pll14xx wrapper
    It looks much cleaner to just have a macro compared to having
    a function that passes NULL as dev to the lower-level
    imx_dev_clk_hw_pll14xx.
    
    Signed-off-by: Abel Vesa <abel.vesa@nxp.com>
    Link: https://lore.kernel.org/r/1631521490-17171-9-git-send-email-abel.vesa@nxp.com
    Reviewed-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/imx/clk.h
+++ drivers/clk/imx/clk.h
@@ -185,6 +185,9 @@ extern struct imx_pll14xx_clk imx_1443x_dram_pll;
 #define imx_clk_hw_divider_flags(name, parent, reg, shift, width, flags) \
 	__imx_clk_hw_divider(name, parent, reg, shift, width, flags)
 
+#define imx_clk_hw_pll14xx(name, parent_name, base, pll_clk) \
+	imx_dev_clk_hw_pll14xx(NULL, name, parent_name, base, pll_clk)
+
 struct clk_hw *imx_dev_clk_hw_pll14xx(struct device *dev, const char *name,
 				const char *parent_name, void __iomem *base,
 				const struct imx_pll14xx_clk *pll_clk);
@@ -292,13 +295,6 @@ static inline struct clk *to_clk(struct clk_hw *hw)
 	if (IS_ERR_OR_NULL(hw))
 		return ERR_CAST(hw);
 	return hw->clk;
-}
-
-static inline struct clk_hw *imx_clk_hw_pll14xx(const char *name, const char *parent_name,
-				  void __iomem *base,
-				  const struct imx_pll14xx_clk *pll_clk)
-{
-	return imx_dev_clk_hw_pll14xx(NULL, name, parent_name, base, pll_clk);
 }
 
 static inline struct clk_hw *imx_clk_hw_fixed(const char *name, int rate)
