6d2ef226f2f18d530e48ead0cb5704505628b797
Linus Torvalds <torvalds@linux-foundation.org>
2021-09-13 10:20:01 -0700

    compiler_attributes.h: drop __has_attribute() support for gcc4
    Now that GCC 5.1 is the minimally supported default, the manual
    workaround for older gcc versions not having __has_attribute() are no
    longer relevant and can be removed.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/compiler_attributes.h
+++ include/linux/compiler_attributes.h
@@ -21,26 +21,6 @@
  */
 
 /*
- * __has_attribute is supported on gcc >= 5, clang >= 2.9 and icc >= 17.
- * In the meantime, to support gcc < 5, we implement __has_attribute
- * by hand.
- */
-#ifndef __has_attribute
-# define __has_attribute(x) __GCC4_has_attribute_##x
-# define __GCC4_has_attribute___assume_aligned__      1
-# define __GCC4_has_attribute___copy__                0
-# define __GCC4_has_attribute___designated_init__     0
-# define __GCC4_has_attribute___externally_visible__  1
-# define __GCC4_has_attribute___no_caller_saved_registers__ 0
-# define __GCC4_has_attribute___noclone__             1
-# define __GCC4_has_attribute___no_profile_instrument_function__ 0
-# define __GCC4_has_attribute___nonstring__           0
-# define __GCC4_has_attribute___no_sanitize_address__ 1
-# define __GCC4_has_attribute___no_sanitize_undefined__ 1
-# define __GCC4_has_attribute___fallthrough__         0
-#endif
-
-/*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-alias-function-attribute
  */
 #define __alias(symbol)                 __attribute__((__alias__(#symbol)))
