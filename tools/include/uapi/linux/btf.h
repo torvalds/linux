b5ea834dde6b6e7f75e51d5f66dac8cd7c97b5ef
Yonghong Song <yhs@fb.com>
2021-09-14 15:30:15 -0700

    bpf: Support for new btf kind BTF_KIND_TAG
    LLVM14 added support for a new C attribute ([1])
      __attribute__((btf_tag("arbitrary_str")))
    This attribute will be emitted to dwarf ([2]) and pahole
    will convert it to BTF. Or for bpf target, this
    attribute will be emitted to BTF directly ([3], [4]).
    The attribute is intended to provide additional
    information for
      - struct/union type or struct/union member
      - static/global variables
      - static/global function or function parameter.
    
    For linux kernel, the btf_tag can be applied
    in various places to specify user pointer,
    function pre- or post- condition, function
    allow/deny in certain context, etc. Such information
    will be encoded in vmlinux BTF and can be used
    by verifier.
    
    The btf_tag can also be applied to bpf programs
    to help global verifiable functions, e.g.,
    specifying preconditions, etc.
    
    This patch added basic parsing and checking support
    in kernel for new BTF_KIND_TAG kind.
    
     [1] https://reviews.llvm.org/D106614
     [2] https://reviews.llvm.org/D106621
     [3] https://reviews.llvm.org/D106622
     [4] https://reviews.llvm.org/D109560
    
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20210914223015.245546-1-yhs@fb.com

--- tools/include/uapi/linux/btf.h
+++ tools/include/uapi/linux/btf.h
@@ -43,7 +43,7 @@ struct btf_type {
 	 * "size" tells the size of the type it is describing.
 	 *
 	 * "type" is used by PTR, TYPEDEF, VOLATILE, CONST, RESTRICT,
-	 * FUNC, FUNC_PROTO and VAR.
+	 * FUNC, FUNC_PROTO, VAR and TAG.
 	 * "type" is a type_id referring to another type.
 	 */
 	union {
@@ -74,6 +74,7 @@ enum {
 	BTF_KIND_VAR		= 14,	/* Variable	*/
 	BTF_KIND_DATASEC	= 15,	/* Section	*/
 	BTF_KIND_FLOAT		= 16,	/* Floating point	*/
+	BTF_KIND_TAG		= 17,	/* Tag */
 
 	NR_BTF_KINDS,
 	BTF_KIND_MAX		= NR_BTF_KINDS - 1,
@@ -171,6 +172,17 @@ struct btf_var_secinfo {
 	__u32	type;
 	__u32	offset;
 	__u32	size;
+};
+
+/* BTF_KIND_TAG is followed by a single "struct btf_tag" to describe
+ * additional information related to the tag applied location.
+ * If component_idx == -1, the tag is applied to a struct, union,
+ * variable or function. Otherwise, it is applied to a struct/union
+ * member or a func argument, and component_idx indicates which member
+ * or argument (0 ... vlen-1).
+ */
+struct btf_tag {
+       __s32   component_idx;
 };
 
 #endif /* _UAPI__LINUX_BTF_H__ */
