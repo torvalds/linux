8aa0fb0fbb82a4d2395be7eaeb994653b2d869fc
Ard Biesheuvel <ardb@kernel.org>
2021-09-14 14:10:35 +0200

    riscv: rely on core code to keep thread_info::cpu updated
    Now that the core code switched back to using thread_info::cpu to keep
    a task's CPU number, we no longer need to keep it in sync explicitly. So
    just drop the code that does this.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Acked-by: Palmer Dabbelt <palmerdabbelt@google.com>
    Acked-by: Mark Rutland <mark.rutland@arm.com>

--- arch/riscv/kernel/asm-offsets.c
+++ arch/riscv/kernel/asm-offsets.c
@@ -33,7 +33,6 @@ void asm_offsets(void)
 	OFFSET(TASK_TI_PREEMPT_COUNT, task_struct, thread_info.preempt_count);
 	OFFSET(TASK_TI_KERNEL_SP, task_struct, thread_info.kernel_sp);
 	OFFSET(TASK_TI_USER_SP, task_struct, thread_info.user_sp);
-	OFFSET(TASK_TI_CPU, task_struct, thread_info.cpu);
 
 	OFFSET(TASK_THREAD_F0,  task_struct, thread.fstate.f[0]);
 	OFFSET(TASK_THREAD_F1,  task_struct, thread.fstate.f[1]);
