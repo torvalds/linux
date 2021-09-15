d3e2ec6cd1636ced67c6f57d95d8ad24f964691d
Brian Norris <briannorris@chromium.org>
2021-09-15 13:50:11 -0700

    drm/rockchip: remove unused psr_list{,_lock}
    Some leftover cleanup from commit 6c836d965bad ("drm/rockchip: Use the
    helpers for PSR").
    
    Cc: Heiko Stuebner <heiko@sntech.de>
    Cc: Sean Paul <seanpaul@chromium.org>
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210915135007.1.I926ef5cef287047c35a17e363c919599c6ee6e4c@changeid

--- drivers/gpu/drm/rockchip/rockchip_drm_drv.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_drv.c
@@ -138,9 +138,6 @@ static int rockchip_drm_bind(struct device *dev)
 
 	drm_dev->dev_private = private;
 
-	INIT_LIST_HEAD(&private->psr_list);
-	mutex_init(&private->psr_list_lock);
-
 	ret = rockchip_drm_init_iommu(drm_dev);
 	if (ret)
 		goto err_free;
