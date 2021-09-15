a5a43cde6601871fdf607486882902a886624d18
Peter Zijlstra <peterz@infradead.org>
2021-09-15 17:12:59 +0200

    x86/mm/64: Improve stack overflow warnings
    [ Upstream commit 44b979fa302cab91bdd2cc982823e5c13202cd4e ]
    
    Current code has an explicit check for hitting the task stack guard;
    but overflowing any of the other stacks will get you a non-descript
    general #DF warning.
    
    Improve matters by using get_stack_info_noinstr() to detetrmine if and
    which stack guard page got hit, enabling a better stack warning.
    
    In specific, Michael Wang reported what turned out to be an NMI
    exception stack overflow, which is now clearly reported as such:
    
      [] BUG: NMI stack guard page was hit at 0000000085fd977b (stack is 000000003a55b09e..00000000d8cce1a5)
    
    Reported-by: Michael Wang <yun.wang@linux.alibaba.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Michael Wang <yun.wang@linux.alibaba.com>
    Link: https://lkml.kernel.org/r/YUTE/NuqnaWbST8n@hirez.programming.kicks-ass.net
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/x86/kernel/dumpstack_64.c
+++ arch/x86/kernel/dumpstack_64.c
@@ -32,8 +32,14 @@ const char *stack_type_name(enum stack_type type)
 {
 	BUILD_BUG_ON(N_EXCEPTION_STACKS != 6);
 
+	if (type == STACK_TYPE_TASK)
+		return "TASK";
+
 	if (type == STACK_TYPE_IRQ)
 		return "IRQ";
+
+	if (type == STACK_TYPE_SOFTIRQ)
+		return "SOFTIRQ";
 
 	if (type == STACK_TYPE_ENTRY) {
 		/*
