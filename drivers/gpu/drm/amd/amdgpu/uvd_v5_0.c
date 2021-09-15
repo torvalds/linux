d82e2c249c8ffaec20fa618611ea2ab4dcfd4d01
Andrey Grodzovsky <andrey.grodzovsky@amd.com>
2021-09-15 15:27:18 -0400

    drm/amdgpu: Fix crash on device remove/driver unload
    Crash:
    BUG: unable to handle page fault for address: 00000000000010e1
    RIP: 0010:vega10_power_gate_vce+0x26/0x50 [amdgpu]
    Call Trace:
    pp_set_powergating_by_smu+0x16a/0x2b0 [amdgpu]
    amdgpu_dpm_set_powergating_by_smu+0x92/0xf0 [amdgpu]
    amdgpu_dpm_enable_vce+0x2e/0xc0 [amdgpu]
    vce_v4_0_hw_fini+0x95/0xa0 [amdgpu]
    amdgpu_device_fini_hw+0x232/0x30d [amdgpu]
    amdgpu_driver_unload_kms+0x5c/0x80 [amdgpu]
    amdgpu_pci_remove+0x27/0x40 [amdgpu]
    pci_device_remove+0x3e/0xb0
    device_release_driver_internal+0x103/0x1d0
    device_release_driver+0x12/0x20
    pci_stop_bus_device+0x79/0xa0
    pci_stop_and_remove_bus_device_locked+0x1b/0x30
    remove_store+0x7b/0x90
    dev_attr_store+0x17/0x30
    sysfs_kf_write+0x4b/0x60
    kernfs_fop_write_iter+0x151/0x1e0
    
    Why:
    VCE/UVD had dependency on SMC block for their suspend but
    SMC block is the first to do HW fini due to some constraints
    
    How:
    Since the original patch was dealing with suspend issues
    move the SMC block dependency back into suspend hooks as
    was done in V1 of the original patches.
    Keep flushing idle work both in suspend and HW fini seuqnces
    since it's essential in both cases.
    
    Fixes: 859e4659273f1d ("drm/amdgpu: add missing cleanups for more ASICs on UVD/VCE suspend")
    Fixes: bf756fb833cbe8 ("drm/amdgpu: add missing cleanups for Polaris12 UVD/VCE on suspend")
    Signed-off-by: Andrey Grodzovsky <andrey.grodzovsky@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/uvd_v5_0.c
+++ drivers/gpu/drm/amd/amdgpu/uvd_v5_0.c
@@ -210,6 +210,19 @@ static int uvd_v5_0_hw_fini(void *handle)
 {
 	struct amdgpu_device *adev = (struct amdgpu_device *)handle;
 
+	cancel_delayed_work_sync(&adev->uvd.idle_work);
+
+	if (RREG32(mmUVD_STATUS) != 0)
+		uvd_v5_0_stop(adev);
+
+	return 0;
+}
+
+static int uvd_v5_0_suspend(void *handle)
+{
+	int r;
+	struct amdgpu_device *adev = (struct amdgpu_device *)handle;
+
 	/*
 	 * Proper cleanups before halting the HW engine:
 	 *   - cancel the delayed idle work
@@ -233,17 +246,6 @@ static int uvd_v5_0_hw_fini(void *handle)
 		amdgpu_device_ip_set_clockgating_state(adev, AMD_IP_BLOCK_TYPE_UVD,
 						       AMD_CG_STATE_GATE);
 	}
-
-	if (RREG32(mmUVD_STATUS) != 0)
-		uvd_v5_0_stop(adev);
-
-	return 0;
-}
-
-static int uvd_v5_0_suspend(void *handle)
-{
-	int r;
-	struct amdgpu_device *adev = (struct amdgpu_device *)handle;
 
 	r = uvd_v5_0_hw_fini(adev);
 	if (r)
