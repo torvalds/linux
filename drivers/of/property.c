3782326577d4b02f3d9940e1c96c3e9b31cf5309
Saravana Kannan <saravanak@google.com>
2021-09-15 01:19:32 -0700

    Revert "of: property: fw_devlink: Add support for "phy-handle" property"
    This reverts commit cf4b94c8530d14017fbddae26aad064ddc42edd4.
    
    Some PHYs pointed to by "phy-handle" will never bind to a driver until a
    consumer attaches to it. And when the consumer attaches to it, they get
    forcefully bound to a generic PHY driver. In such cases, parsing the
    phy-handle property and creating a device link will prevent the consumer
    from ever probing. We don't want that. So revert support for
    "phy-handle" property until we come up with a better mechanism for
    binding PHYs to generic drivers before a consumer tries to attach to it.
    
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Link: https://lore.kernel.org/r/20210915081933.485112-1-saravanak@google.com
    Signed-off-by: Rob Herring <robh@kernel.org>

--- drivers/of/property.c
+++ drivers/of/property.c
@@ -1291,7 +1291,6 @@ DEFINE_SIMPLE_PROP(pwms, "pwms", "#pwm-cells")
 DEFINE_SIMPLE_PROP(resets, "resets", "#reset-cells")
 DEFINE_SIMPLE_PROP(leds, "leds", NULL)
 DEFINE_SIMPLE_PROP(backlight, "backlight", NULL)
-DEFINE_SIMPLE_PROP(phy_handle, "phy-handle", NULL)
 DEFINE_SUFFIX_PROP(regulators, "-supply", NULL)
 DEFINE_SUFFIX_PROP(gpio, "-gpio", "#gpio-cells")
 
@@ -1380,7 +1379,6 @@ static const struct supplier_bindings of_supplier_bindings[] = {
 	{ .parse_prop = parse_resets, },
 	{ .parse_prop = parse_leds, },
 	{ .parse_prop = parse_backlight, },
-	{ .parse_prop = parse_phy_handle, },
 	{ .parse_prop = parse_gpio_compat, },
 	{ .parse_prop = parse_interrupts, },
 	{ .parse_prop = parse_regulators, },
