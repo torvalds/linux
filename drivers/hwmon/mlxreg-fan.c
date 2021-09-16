bc8de07e8812548cc19161af3a2b83849ff03045
Vadim Pasternak <vadimp@nvidia.com>
2021-09-16 22:47:17 +0300

    hwmon: (mlxreg-fan) Extend the maximum number of tachometers
    Extend support of maximum tachometers from 12 to 14 in order to support
    new systems, equipped with more fans.
    
    Signed-off-by: Vadim Pasternak <vadimp@nvidia.com>
    Link: https://lore.kernel.org/r/20210916194719.871413-2-vadimp@nvidia.com
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/mlxreg-fan.c
+++ drivers/hwmon/mlxreg-fan.c
@@ -12,7 +12,7 @@
 #include <linux/regmap.h>
 #include <linux/thermal.h>
 
-#define MLXREG_FAN_MAX_TACHO		12
+#define MLXREG_FAN_MAX_TACHO		14
 #define MLXREG_FAN_MAX_STATE		10
 #define MLXREG_FAN_MIN_DUTY		51	/* 20% */
 #define MLXREG_FAN_MAX_DUTY		255	/* 100% */
@@ -255,6 +255,8 @@ mlxreg_fan_is_visible(const void *data, enum hwmon_sensor_types type, u32 attr,
 
 static const struct hwmon_channel_info *mlxreg_fan_hwmon_info[] = {
 	HWMON_CHANNEL_INFO(fan,
+			   HWMON_F_INPUT | HWMON_F_FAULT,
+			   HWMON_F_INPUT | HWMON_F_FAULT,
 			   HWMON_F_INPUT | HWMON_F_FAULT,
 			   HWMON_F_INPUT | HWMON_F_FAULT,
 			   HWMON_F_INPUT | HWMON_F_FAULT,
