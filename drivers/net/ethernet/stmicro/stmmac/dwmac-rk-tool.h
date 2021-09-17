defb18abcd5ea5246789ceeb4c70981bd6737611
David Wu <david.wu@rock-chips.com>
2021-09-17 15:28:28 +0800

    net: ethernet: stmmac: dwmac-rk: Add config option for dwmac-rk-tool
    Add this config option to reduce memory usage, if it was not necessary.
    
    ./ksize.sh drivers/net/ethernet/stmicro/
    
    before size: 131758 Bytes
    after  size: 121861 Bytes
    save   size: 9897   Bytes
    
    Signed-off-by: David Wu <david.wu@rock-chips.com>
    Change-Id: I96c88d5eb5109fc956a85b8685e1746d76545a87

--- drivers/net/ethernet/stmicro/stmmac/dwmac-rk-tool.h
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-rk-tool.h
@@ -13,8 +13,20 @@ void dwmac_rk_set_rgmii_delayline(struct stmmac_priv *priv, int tx_delay, int rx
 void dwmac_rk_get_rgmii_delayline(struct stmmac_priv *priv, int *tx_delay, int *rx_delay);
 int dwmac_rk_get_phy_interface(struct stmmac_priv *priv);
 
+#ifdef CONFIG_DWMAC_ROCKCHIP_TOOL
 int dwmac_rk_create_loopback_sysfs(struct device *dev);
 int dwmac_rk_remove_loopback_sysfs(struct device *device);
+#else
+static inline int dwmac_rk_create_loopback_sysfs(struct device *dev)
+{
+	return 0;
+}
+
+static inline int dwmac_rk_remove_loopback_sysfs(struct device *device)
+{
+	return 0;
+}
+#endif
 
 #ifdef CONFIG_DWMAC_RK_AUTO_DELAYLINE
 int dwmac_rk_get_rgmii_delayline_from_vendor(struct stmmac_priv *priv);
