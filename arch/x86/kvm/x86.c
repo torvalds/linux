a91a7c7096005113d8e749fd8dfdd3e1eecee263
Lai Jiangshan <laijs@linux.alibaba.com>
2021-09-19 10:42:46 +0800

    KVM: X86: Don't reset mmu context when toggling X86_CR4_PGE
    X86_CR4_PGE doesn't participate in kvm_mmu_role, so the mmu context
    doesn't need to be reset.  It is only required to flush all the guest
    tlb.
    
    It is also inconsistent that X86_CR4_PGE is in KVM_MMU_CR4_ROLE_BITS
    while kvm_mmu_role doesn't use X86_CR4_PGE.  So X86_CR4_PGE is also
    removed from KVM_MMU_CR4_ROLE_BITS.
    
    Signed-off-by: Lai Jiangshan <laijs@linux.alibaba.com>
    Reviewed-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20210919024246.89230-3-jiangshanlai@gmail.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -1024,7 +1024,8 @@ void kvm_post_set_cr4(struct kvm_vcpu *vcpu, unsigned long old_cr4, unsigned lon
 {
 	if ((cr4 ^ old_cr4) & KVM_MMU_CR4_ROLE_BITS)
 		kvm_mmu_reset_context(vcpu);
-	else if (!(cr4 & X86_CR4_PCIDE) && (old_cr4 & X86_CR4_PCIDE))
+	else if (((cr4 ^ old_cr4) & X86_CR4_PGE) ||
+		 (!(cr4 & X86_CR4_PCIDE) && (old_cr4 & X86_CR4_PCIDE)))
 		kvm_make_request(KVM_REQ_TLB_FLUSH_GUEST, vcpu);
 }
 EXPORT_SYMBOL_GPL(kvm_post_set_cr4);
