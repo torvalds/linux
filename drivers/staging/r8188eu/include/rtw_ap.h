e3839fdff128df91b3553c67c495806e0f9925e0
Michael Straube <straube.linux@gmail.com>
2021-09-19 17:36:58 +0200

    staging: r8188eu: remove rtw_check_beacon_data()
    Function rtw_check_beacon_data() is not used, remove it.
    It is the only user of start_bss_network() which is the only user
    of update_hw_ht_param(). Remove these two functions as well.
    
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Link: https://lore.kernel.org/r/20210919153659.20826-6-straube.linux@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/r8188eu/include/rtw_ap.h
+++ drivers/staging/r8188eu/include/rtw_ap.h
@@ -20,7 +20,6 @@ void add_RATid(struct adapter *padapter, struct sta_info *psta,
 	       u8 rssi_level);
 void expire_timeout_chk(struct adapter *padapter);
 void update_sta_info_apmode(struct adapter *padapter, struct sta_info *psta);
-int rtw_check_beacon_data(struct adapter *padapter, u8 *pbuf,  int len);
 void rtw_ap_restore_network(struct adapter *padapter);
 void rtw_set_macaddr_acl(struct adapter *padapter, int mode);
 
