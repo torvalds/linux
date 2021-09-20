c3a4c69360ab43560f212eed326c9d8bde35b14c
Florian Fainelli <f.fainelli@gmail.com>
2021-09-20 14:54:17 -0700

    net: bcmgenet: Request APD, DLL disable and IDDQ-SR
    When interfacing with a Broadcom PHY, request the auto-power down, DLL
    disable and IDDQ-SR modes to be enabled.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/genet/bcmmii.c
+++ drivers/net/ethernet/broadcom/genet/bcmmii.c
@@ -288,7 +288,9 @@ int bcmgenet_mii_probe(struct net_device *dev)
 	struct device_node *dn = kdev->of_node;
 	phy_interface_t phy_iface = priv->phy_interface;
 	struct phy_device *phydev;
-	u32 phy_flags = 0;
+	u32 phy_flags = PHY_BRCM_AUTO_PWRDWN_ENABLE |
+			PHY_BRCM_DIS_TXCRXC_NOENRGY |
+			PHY_BRCM_IDDQ_SUSPEND;
 	int ret;
 
 	/* Communicate the integrated PHY revision */
