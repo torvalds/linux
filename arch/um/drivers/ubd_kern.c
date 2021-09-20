69b80587f650875a473c66df2a1120c0e656362b
Christoph Hellwig <hch@lst.de>
2021-09-20 14:33:25 +0200

    block: drop unused includes in <linux/genhd.h>
    commit b81e0c2372e65e5627864ba034433b64b2fc73f5 upstream.
    
    Drop various include not actually used in genhd.h itself, and
    move the remaning includes closer together.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Link: https://lore.kernel.org/r/20210920123328.1399408-15-hch@lst.de
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Reported-by: Sudip Mukherjee <sudipm.mukherjee@gmail.com>a
    Reported-by: "H. Nikolaus Schaller" <hns@goldelico.com>
    Reported-by: Guenter Roeck <linux@roeck-us.net>
    Cc: "Maciej W. Rozycki" <macro@orcam.me.uk>
    [ resolves MIPS build failure by luck, root cause needs to be fixed in
      Linus's tree properly, but this is needed for now to fix the build - gregkh ]
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/um/drivers/ubd_kern.c
+++ arch/um/drivers/ubd_kern.c
@@ -27,6 +27,7 @@
 #include <linux/blk-mq.h>
 #include <linux/ata.h>
 #include <linux/hdreg.h>
+#include <linux/major.h>
 #include <linux/cdrom.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
