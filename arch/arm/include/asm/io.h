b8bc0e50a32a3c3b51605e41de41b4d7952e6aa4
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2021-09-21 12:57:22 +0100

    ARM: add __arm_iomem_set_ro() to write-protect ioremapped area
    __arm_iomem_set_ro() marks an ioremapped area read-only. This is
    intended for use with __arm_ioremap_exec() to allow the kernel to
    write some code into e.g. SRAM and then write-protect it so the
    kernel doesn't complain about W+X mappings.
    
    Tested-by: Fabio Estevam <festevam@gmail.com>
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>

--- arch/arm/include/asm/io.h
+++ arch/arm/include/asm/io.h
@@ -138,6 +138,7 @@ extern void __iomem *__arm_ioremap_caller(phys_addr_t, size_t, unsigned int,
 	void *);
 extern void __iomem *__arm_ioremap_pfn(unsigned long, unsigned long, size_t, unsigned int);
 extern void __iomem *__arm_ioremap_exec(phys_addr_t, size_t, bool cached);
+void __arm_iomem_set_ro(void __iomem *ptr, size_t size);
 extern void __iounmap(volatile void __iomem *addr);
 
 extern void __iomem * (*arch_ioremap_caller)(phys_addr_t, size_t,
