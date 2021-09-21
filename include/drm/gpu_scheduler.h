f3823da7e4ba7d4781375c2bb786a8a78efc6591
Rob Clark <robdclark@gmail.com>
2021-09-21 09:35:50 -0700

    drm/scheduler: Add fence deadline support
    As the finished fence is the one that is exposed to userspace, and
    therefore the one that other operations, like atomic update, would
    block on, we need to propagate the deadline from from the finished
    fence to the actual hw fence.
    
    v2: Split into drm_sched_fence_set_parent() (ckoenig)
    v3: Ensure a thread calling drm_sched_fence_set_deadline_finished() sees
        fence->parent set before drm_sched_fence_set_parent() does this
        test_bit(DMA_FENCE_FLAG_HAS_DEADLINE_BIT).
    
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Acked-by: Luben Tuikov <luben.tuikov@amd.com>

--- include/drm/gpu_scheduler.h
+++ include/drm/gpu_scheduler.h
@@ -41,6 +41,15 @@
  */
 #define DRM_SCHED_FENCE_DONT_PIPELINE	DMA_FENCE_FLAG_USER_BITS
 
+/**
+ * DRM_SCHED_FENCE_FLAG_HAS_DEADLINE_BIT - A fence deadline hint has been set
+ *
+ * Because we could have a deadline hint can be set before the backing hw
+ * fence is created, we need to keep track of whether a deadline has already
+ * been set.
+ */
+#define DRM_SCHED_FENCE_FLAG_HAS_DEADLINE_BIT	(DMA_FENCE_FLAG_USER_BITS + 1)
+
 enum dma_resv_usage;
 struct dma_resv;
 struct drm_gem_object;
@@ -279,6 +288,12 @@ struct drm_sched_fence {
          * resolved.
          */
 	struct dma_fence		finished;
+
+	/**
+	 * @deadline: deadline set on &drm_sched_fence.finished which
+	 * potentially needs to be propagated to &drm_sched_fence.parent
+	 */
+	ktime_t				deadline;
 
         /**
          * @parent: the fence returned by &drm_sched_backend_ops.run_job
@@ -568,6 +583,8 @@ void drm_sched_entity_set_priority(struct drm_sched_entity *entity,
 				   enum drm_sched_priority priority);
 bool drm_sched_entity_is_ready(struct drm_sched_entity *entity);
 
+void drm_sched_fence_set_parent(struct drm_sched_fence *s_fence,
+				struct dma_fence *fence);
 struct drm_sched_fence *drm_sched_fence_alloc(
 	struct drm_sched_entity *s_entity, void *owner);
 void drm_sched_fence_init(struct drm_sched_fence *fence,
