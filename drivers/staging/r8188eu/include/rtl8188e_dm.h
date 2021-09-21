90602f96f1471b0072651cb771b73278846f40dd
Michael Straube <straube.linux@gmail.com>
2021-09-21 21:46:55 +0200

    staging: r8188eu: remove PowerIndex_backup from struct dm_priv
    The field PowerIndex_backup of struct dm_priv is not used, remove it.
    
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Link: https://lore.kernel.org/r/20210921194658.10654-6-straube.linux@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/r8188eu/include/rtl8188e_dm.h
+++ drivers/staging/r8188eu/include/rtl8188e_dm.h
@@ -30,7 +30,6 @@ struct	dm_priv {
 
 	/* for High Power */
 	u8 DynamicTxHighPowerLvl;/* Tx Power Control for Near/Far Range */
-	u8	PowerIndex_backup[6];
 };
 
 void rtl8188e_init_dm_priv(struct adapter *adapt);
