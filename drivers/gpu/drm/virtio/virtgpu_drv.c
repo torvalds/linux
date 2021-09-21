6198770a1fe01938921f8a383b777ddb881c94b6
Anthoine Bourgeois <anthoine.bourgeois@gmail.com>
2021-09-21 16:20:16 -0700

    drm/virtio: implement context init: probe for feature
    Let's probe for VIRTIO_GPU_F_CONTEXT_INIT.
    
    Create a new DRM_INFO(..) line since the current one is getting
    too long.
    
    Signed-off-by: Anthoine Bourgeois <anthoine.bourgeois@gmail.com>
    Acked-by: Lingfeng Yang <lfy@google.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20210921232024.817-5-gurchetansingh@chromium.org
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- drivers/gpu/drm/virtio/virtgpu_drv.c
+++ drivers/gpu/drm/virtio/virtgpu_drv.c
@@ -172,6 +172,7 @@ static unsigned int features[] = {
 	VIRTIO_GPU_F_EDID,
 	VIRTIO_GPU_F_RESOURCE_UUID,
 	VIRTIO_GPU_F_RESOURCE_BLOB,
+	VIRTIO_GPU_F_CONTEXT_INIT,
 };
 static struct virtio_driver virtio_gpu_driver = {
 	.feature_table = features,
