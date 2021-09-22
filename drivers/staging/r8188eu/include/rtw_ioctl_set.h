6c3fab164bf50b88a7e32c1c856aa59eb3db90b9
Michael Straube <straube.linux@gmail.com>
2021-09-22 22:03:55 +0200

    staging: r8188eu: remove rtw_set_scan_mode()
    Function rtw_set_scan_mode() is not used, remove it.
    
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Link: https://lore.kernel.org/r/20210922200420.9693-23-straube.linux@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/r8188eu/include/rtw_ioctl_set.h
+++ drivers/staging/r8188eu/include/rtw_ioctl_set.h
@@ -21,7 +21,6 @@ u8 rtw_set_802_11_infrastructure_mode(struct adapter *adapter,
 u8 rtw_set_802_11_ssid(struct adapter *adapt, struct ndis_802_11_ssid *ssid);
 u8 rtw_validate_ssid(struct ndis_802_11_ssid *ssid);
 u16 rtw_get_cur_max_rate(struct adapter *adapter);
-int rtw_set_scan_mode(struct adapter *adapter, enum rt_scan_type scan_mode);
 int rtw_set_channel_plan(struct adapter *adapter, u8 channel_plan);
 int rtw_change_ifname(struct adapter *padapter, const char *ifname);
 
