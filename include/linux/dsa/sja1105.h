28da0555c3b542d605e4ca26eea6a740cf2c9174
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-09-22 17:37:25 +0300

    net: dsa: move sja1110_process_meta_tstamp inside the tagging protocol driver
    The problem is that DSA tagging protocols really must not depend on the
    switch driver, because this creates a circular dependency at insmod
    time, and the switch driver will effectively not load when the tagging
    protocol driver is missing.
    
    The code was structured in the way it was for a reason, though. The DSA
    driver-facing API for PTP timestamping relies on the assumption that
    two-step TX timestamps are provided by the hardware in an out-of-band
    manner, typically by raising an interrupt and making that timestamp
    available inside some sort of FIFO which is to be accessed over
    SPI/MDIO/etc.
    
    So the API puts .port_txtstamp into dsa_switch_ops, because it is
    expected that the switch driver needs to save some state (like put the
    skb into a queue until its TX timestamp arrives).
    
    On SJA1110, TX timestamps are provided by the switch as Ethernet
    packets, so this makes them be received and processed by the tagging
    protocol driver. This in itself is great, because the timestamps are
    full 64-bit and do not require reconstruction, and since Ethernet is the
    fastest I/O method available to/from the switch, PTP timestamps arrive
    very quickly, no matter how bottlenecked the SPI connection is, because
    SPI interaction is not needed at all.
    
    DSA's code structure and strict isolation between the tagging protocol
    driver and the switch driver break the natural code organization.
    
    When the tagging protocol driver receives a packet which is classified
    as a metadata packet containing timestamps, it passes those timestamps
    one by one to the switch driver, which then proceeds to compare them
    based on the recorded timestamp ID that was generated in .port_txtstamp.
    
    The communication between the tagging protocol and the switch driver is
    done through a method exported by the switch driver, sja1110_process_meta_tstamp.
    To satisfy build requirements, we force a dependency to build the
    tagging protocol driver as a module when the switch driver is a module.
    However, as explained in the first paragraph, that causes the circular
    dependency.
    
    To solve this, move the skb queue from struct sja1105_private :: struct
    sja1105_ptp_data to struct sja1105_private :: struct sja1105_tagger_data.
    The latter is a data structure for which hacks have already been put
    into place to be able to create persistent storage per switch that is
    accessible from the tagging protocol driver (see sja1105_setup_ports).
    
    With the skb queue directly accessible from the tagging protocol driver,
    we can now move sja1110_process_meta_tstamp into the tagging driver
    itself, and avoid exporting a symbol.
    
    Fixes: 566b18c8b752 ("net: dsa: sja1105: implement TX timestamping for SJA1110")
    Link: https://lore.kernel.org/netdev/20210908220834.d7gmtnwrorhharna@skbuf/
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/linux/dsa/sja1105.h
+++ include/linux/dsa/sja1105.h
@@ -48,6 +48,10 @@ struct sja1105_tagger_data {
 	spinlock_t meta_lock;
 	unsigned long state;
 	u8 ts_id;
+	/* Used on SJA1110 where meta frames are generated only for
+	 * 2-step TX timestamps
+	 */
+	struct sk_buff_head skb_txtstamp_queue;
 };
 
 struct sja1105_skb_cb {
@@ -69,25 +73,20 @@ struct sja1105_port {
 	bool hwts_tx_en;
 };
 
-enum sja1110_meta_tstamp {
-	SJA1110_META_TSTAMP_TX = 0,
-	SJA1110_META_TSTAMP_RX = 1,
-};
+/* Timestamps are in units of 8 ns clock ticks (equivalent to
+ * a fixed 125 MHz clock).
+ */
+#define SJA1105_TICK_NS			8
 
-#if IS_ENABLED(CONFIG_NET_DSA_SJA1105_PTP)
-
-void sja1110_process_meta_tstamp(struct dsa_switch *ds, int port, u8 ts_id,
-				 enum sja1110_meta_tstamp dir, u64 tstamp);
-
-#else
-
-static inline void sja1110_process_meta_tstamp(struct dsa_switch *ds, int port,
-					       u8 ts_id, enum sja1110_meta_tstamp dir,
-					       u64 tstamp)
+static inline s64 ns_to_sja1105_ticks(s64 ns)
 {
+	return ns / SJA1105_TICK_NS;
 }
 
-#endif /* IS_ENABLED(CONFIG_NET_DSA_SJA1105_PTP) */
+static inline s64 sja1105_ticks_to_ns(s64 ticks)
+{
+	return ticks * SJA1105_TICK_NS;
+}
 
 #if IS_ENABLED(CONFIG_NET_DSA_SJA1105)
 
