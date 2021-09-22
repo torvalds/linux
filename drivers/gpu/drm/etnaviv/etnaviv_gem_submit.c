c29d6797228b673c5051d3ed7ef36ae66fe99ac7
Christian König <christian.koenig@amd.com>
2021-09-22 14:16:25 +0200

    drm/etnaviv: stop getting the excl fence separately here
    Just grab all fences in one go.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Lucas Stach <l.stach@pengutronix.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211028132630.2330-3-christian.koenig@amd.com

--- drivers/gpu/drm/etnaviv/etnaviv_gem_submit.c
+++ drivers/gpu/drm/etnaviv/etnaviv_gem_submit.c
@@ -189,7 +189,7 @@ static int submit_fence_sync(struct etnaviv_gem_submit *submit)
 			continue;
 
 		if (bo->flags & ETNA_SUBMIT_BO_WRITE) {
-			ret = dma_resv_get_fences(robj, &bo->excl,
+			ret = dma_resv_get_fences(robj, NULL,
 						  &bo->nr_shared,
 						  &bo->shared);
 			if (ret)
