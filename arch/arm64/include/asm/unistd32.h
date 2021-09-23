ea7c45fde5aa3e761aaddb7902a31a95cb120e7b
André Almeida <andrealmeid@collabora.com>
2021-09-23 14:11:07 -0300

    futex,arm: Wire up sys_futex_waitv()
    Wire up syscall entry point for ARM architectures, for both 32 and 64-bit.
    
    Signed-off-by: André Almeida <andrealmeid@collabora.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20210923171111.300673-19-andrealmeid@collabora.com

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -903,6 +903,8 @@ __SYSCALL(__NR_landlock_add_rule, sys_landlock_add_rule)
 __SYSCALL(__NR_landlock_restrict_self, sys_landlock_restrict_self)
 #define __NR_process_mrelease 448
 __SYSCALL(__NR_process_mrelease, sys_process_mrelease)
+#define __NR_futex_waitv 449
+__SYSCALL(__NR_futex_waitv, sys_futex_waitv)
 
 /*
  * Please add new compat syscalls above this comment and update
