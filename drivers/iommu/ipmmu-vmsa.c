1cdeb52e5c245bd3712f54bc38d199f98e90419a
Nikita Yushchenko <nikita.yoush@cogentembedded.com>
2021-09-23 22:11:16 +0300

    iommu/ipmmu-vmsa: Hook up r8a77980 DT matching code
    Add r8a77980 (R-Car V3H) to the list of supported devices. The hardware
    is the same as on already-supportred V3M and other R-Car Gen3 chips.
    
    Signed-off-by: Nikita Yushchenko <nikita.yoush@cogentembedded.com>
    Link: https://lore.kernel.org/r/20210923191115.22864-1-nikita.yoush@cogentembedded.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/ipmmu-vmsa.c
+++ drivers/iommu/ipmmu-vmsa.c
@@ -972,6 +972,9 @@ static const struct of_device_id ipmmu_of_ids[] = {
 		.compatible = "renesas,ipmmu-r8a77970",
 		.data = &ipmmu_features_rcar_gen3,
 	}, {
+		.compatible = "renesas,ipmmu-r8a77980",
+		.data = &ipmmu_features_rcar_gen3,
+	}, {
 		.compatible = "renesas,ipmmu-r8a77990",
 		.data = &ipmmu_features_rcar_gen3,
 	}, {
