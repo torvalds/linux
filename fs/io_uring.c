9f3a2cb228c28606895d15f13b30d1f7402dc745
Pavel Begunkov <asml.silence@gmail.com>
2021-09-24 17:14:48 +0100

    io_uring: kill extra checks in io_write()
    We don't retry short writes and so we would never get to async setup in
    io_write() in that case. Thus ret2 > 0 is always false and
    iov_iter_advance() is never used. Apparently, the same is found by
    Coverity, which complains on the code.
    
    Fixes: cd65869512ab ("io_uring: use iov_iter state save/restore helpers")
    Reported-by: Dave Jones <davej@codemonkey.org.uk>
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/5b33e61034748ef1022766efc0fb8854cfcf749c.1632500058.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -3605,7 +3605,6 @@ static int io_write(struct io_kiocb *req, unsigned int issue_flags)
 		iov_iter_save_state(iter, state);
 	}
 	req->result = iov_iter_count(iter);
-	ret2 = 0;
 
 	/* Ensure we clear previously set non-block flag */
 	if (!force_nonblock)
@@ -3670,8 +3669,6 @@ done:
 	} else {
 copy_iov:
 		iov_iter_restore(iter, state);
-		if (ret2 > 0)
-			iov_iter_advance(iter, ret2);
 		ret = io_setup_async_rw(req, iovec, inline_vecs, iter, false);
 		return ret ?: -EAGAIN;
 	}
