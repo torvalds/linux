a4ce73910427e960b2c7f4d83229153c327d0ee7
Weizhao Ouyang <o451686892@gmail.com>
2021-09-24 15:43:50 -0700

    mm/debug: sync up MR_CONTIG_RANGE and MR_LONGTERM_PIN
    Sync up MR_CONTIG_RANGE and MR_LONGTERM_PIN to migrate_reason_names.
    
    Link: https://lkml.kernel.org/r/20210921064553.293905-2-o451686892@gmail.com
    Fixes: 310253514bbf ("mm/migrate: rename migration reason MR_CMA to MR_CONTIG_RANGE")
    Fixes: d1e153fea2a8 ("mm/gup: migrate pinned pages out of movable zone")
    Signed-off-by: Weizhao Ouyang <o451686892@gmail.com>
    Reviewed-by: "Huang, Ying" <ying.huang@intel.com>
    Reviewed-by: John Hubbard <jhubbard@nvidia.com>
    Cc: Anshuman Khandual <khandual@linux.vnet.ibm.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Pavel Tatashin <pasha.tatashin@soleen.com>
    Cc: Yang Shi <yang.shi@linux.alibaba.com>
    Cc: Zi Yan <ziy@nvidia.com>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Mina Almasry <almasrymina@google.com>
    Cc: "Matthew Wilcox (Oracle)" <willy@infradead.org>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Wei Xu <weixugc@google.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/debug.c
+++ mm/debug.c
@@ -24,7 +24,8 @@ const char *migrate_reason_names[MR_TYPES] = {
 	"syscall_or_cpuset",
 	"mempolicy_mbind",
 	"numa_misplaced",
-	"cma",
+	"contig_range",
+	"longterm_pin",
 };
 
 const struct trace_print_flags pageflag_names[] = {
