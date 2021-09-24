244f5d597e1ea519c2085fbd9819458688775e42
Sudeep Holla <sudeep.holla@arm.com>
2021-09-24 10:28:58 +0100

    firmware: arm_ffa: Add missing remove callback to ffa_bus_type
    Currently the arm_ffa firmware driver can be built as module and hence
    all the users of FFA driver. If any driver on the ffa bus is removed or
    unregistered, the remove callback on all the device bound to the driver
    being removed should be callback. For that to happen, we must register
    a remove callback on the ffa_bus which is currently missing. This results
    in the probe getting called again without the previous remove callback
    on a device which may result in kernel crash.
    
    Fix the issue by registering the remove callback on the FFA bus.
    
    Link: https://lore.kernel.org/r/20210924092859.3057562-1-sudeep.holla@arm.com
    Fixes: e781858488b9 ("firmware: arm_ffa: Add initial FFA bus support for device enumeration")
    Reported-by: Jens Wiklander <jens.wiklander@linaro.org>
    Tested-by: Jens Wiklander <jens.wiklander@linaro.org>
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>

--- drivers/firmware/arm_ffa/bus.c
+++ drivers/firmware/arm_ffa/bus.c
@@ -49,6 +49,13 @@ static int ffa_device_probe(struct device *dev)
 	return ffa_drv->probe(ffa_dev);
 }
 
+static void ffa_device_remove(struct device *dev)
+{
+	struct ffa_driver *ffa_drv = to_ffa_driver(dev->driver);
+
+	ffa_drv->remove(to_ffa_dev(dev));
+}
+
 static int ffa_device_uevent(struct device *dev, struct kobj_uevent_env *env)
 {
 	struct ffa_device *ffa_dev = to_ffa_dev(dev);
@@ -86,6 +93,7 @@ struct bus_type ffa_bus_type = {
 	.name		= "arm_ffa",
 	.match		= ffa_device_match,
 	.probe		= ffa_device_probe,
+	.remove		= ffa_device_remove,
 	.uevent		= ffa_device_uevent,
 	.dev_groups	= ffa_device_attributes_groups,
 };
