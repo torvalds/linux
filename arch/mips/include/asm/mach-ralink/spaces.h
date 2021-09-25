1958beb80a603d090b8e66f8c3407360bd2d4bed
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2021-09-25 22:32:19 +0200

    Revert "MIPS: ralink: don't define PC_IOBASE but increase IO_SPACE_LIMIT"
    This reverts commit 159697474db41732ef3b6c2e8d9395f09d1f659e.
    
    There is no real need to increase IO_SPACE_LIMIT if PCI_IOBASE
    is properly set to 'mips_io_port_base'. Hence revert this commit
    first before doing anything else.
    
    Acked-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>
    Signed-off-by: Sergio Paracuellos <sergio.paracuellos@gmail.com>
    Link: https://lore.kernel.org/r/20210925203224.10419-2-sergio.paracuellos@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/mips/include/asm/mach-ralink/spaces.h
+++ arch/mips/include/asm/mach-ralink/spaces.h
@@ -2,7 +2,9 @@
 #ifndef __ASM_MACH_RALINK_SPACES_H_
 #define __ASM_MACH_RALINK_SPACES_H_
 
-#define IO_SPACE_LIMIT	0x1fffffff
+#define PCI_IOBASE	_AC(0xa0000000, UL)
+#define PCI_IOSIZE	SZ_16M
+#define IO_SPACE_LIMIT	(PCI_IOSIZE - 1)
 
 #include <asm/mach-generic/spaces.h>
 #endif
