4cc0b13369ac2492d352bfd6a48dadcf66082395
Ding Wei <leo.ding@rock-chips.com>
2021-09-26 11:50:46 +0800

    video: rockchip: mpp: fix issue for CONFIG_IOMMU_SUPPORT=n
    if CONFIG_IOMMU_SUPPORT=n, log:
     error: 'const struct iommu_ops' has no member named 'flush_iotlb_all'
       if (domain && domain->ops && domain->ops->flush_iotlb_all)
    
    Change-Id: I8268e0b5d5a513d1c55b0c755c479049b13bdeb7
    Signed-off-by: Ding Wei <leo.ding@rock-chips.com>

--- drivers/video/rockchip/mpp/mpp_iommu.c
+++ drivers/video/rockchip/mpp/mpp_iommu.c
@@ -487,8 +487,8 @@ int mpp_iommu_flush_tlb(struct mpp_iommu_info *info)
 {
 	struct iommu_domain *domain = info->domain;
 
-	if (domain && domain->ops && domain->ops->flush_iotlb_all)
-		domain->ops->flush_iotlb_all(domain);
+	if (domain && domain->ops)
+		iommu_flush_iotlb_all(domain);
 
 	return 0;
 }
