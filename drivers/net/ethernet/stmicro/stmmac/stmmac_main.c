3e0d5699a975718efe048edc6f61fbe1cf50e51e
Arnd Bergmann <arnd@arndb.de>
2021-09-27 12:02:44 +0200

    net: stmmac: fix gcc-10 -Wrestrict warning
    gcc-10 and later warn about a theoretical array overrun when
    accessing priv->int_name_rx_irq[i] with an out of bounds value
    of 'i':
    
    drivers/net/ethernet/stmicro/stmmac/stmmac_main.c: In function 'stmmac_request_irq_multi_msi':
    drivers/net/ethernet/stmicro/stmmac/stmmac_main.c:3528:17: error: 'snprintf' argument 4 may overlap destination object 'dev' [-Werror=restrict]
     3528 |                 snprintf(int_name, int_name_len, "%s:%s-%d", dev->name, "tx", i);
          |                 ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    drivers/net/ethernet/stmicro/stmmac/stmmac_main.c:3404:60: note: destination object referenced by 'restrict'-qualified argument 1 was declared here
     3404 | static int stmmac_request_irq_multi_msi(struct net_device *dev)
          |                                         ~~~~~~~~~~~~~~~~~~~^~~
    
    The warning is a bit strange since it's not actually about the array
    bounds but rather about possible string operations with overlapping
    arguments, but it's not technically wrong.
    
    Avoid the warning by adding an extra bounds check.
    
    Fixes: 8532f613bc78 ("net: stmmac: introduce MSI Interrupt routines for mac, safety, RX & TX")
    Link: https://lore.kernel.org/all/20210421134743.3260921-1-arnd@kernel.org/
    Reported-by: kernel test robot <lkp@intel.com>
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -3502,6 +3502,8 @@ static int stmmac_request_irq_multi_msi(struct net_device *dev)
 
 	/* Request Rx MSI irq */
 	for (i = 0; i < priv->plat->rx_queues_to_use; i++) {
+		if (i > MTL_MAX_RX_QUEUES)
+			break;
 		if (priv->rx_irq[i] == 0)
 			continue;
 
@@ -3525,6 +3527,8 @@ static int stmmac_request_irq_multi_msi(struct net_device *dev)
 
 	/* Request Tx MSI irq */
 	for (i = 0; i < priv->plat->tx_queues_to_use; i++) {
+		if (i > MTL_MAX_TX_QUEUES)
+			break;
 		if (priv->tx_irq[i] == 0)
 			continue;
 
