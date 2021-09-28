9b955d5d60389e8c65c816d1c6f28188b1d4ebba
Baochen Qiang <bqiang@codeaurora.org>
2021-09-28 14:00:44 +0300

    ath11k: Fix memory leak in ath11k_qmi_driver_event_work
    [ Upstream commit 72de799aa9e3e064b35238ef053d2f0a49db055a ]
    
    The buffer pointed to by event is not freed in case
    ATH11K_FLAG_UNREGISTERING bit is set, resulting in
    memory leak, so fix it.
    
    Tested-on: WCN6855 hw2.0 PCI WLAN.HSP.1.1-01720.1-QCAHSPSWPL_V1_V2_SILICONZ_LITE-1
    
    Fixes: d5c65159f289 ("ath11k: driver for Qualcomm IEEE 802.11ax devices")
    Signed-off-by: Baochen Qiang <bqiang@codeaurora.org>
    Signed-off-by: Jouni Malinen <jouni@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20210913180246.193388-4-jouni@codeaurora.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/wireless/ath/ath11k/qmi.c
+++ drivers/net/wireless/ath/ath11k/qmi.c
@@ -2707,8 +2707,10 @@ static void ath11k_qmi_driver_event_work(struct work_struct *work)
 		list_del(&event->list);
 		spin_unlock(&qmi->event_lock);
 
-		if (test_bit(ATH11K_FLAG_UNREGISTERING, &ab->dev_flags))
+		if (test_bit(ATH11K_FLAG_UNREGISTERING, &ab->dev_flags)) {
+			kfree(event);
 			return;
+		}
 
 		switch (event->type) {
 		case ATH11K_QMI_EVENT_SERVER_ARRIVE:
