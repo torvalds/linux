2b33e01948fdb5806b73abd25c27d23ff851c98e
Miquel Raynal <miquel.raynal@bootlin.com>
2021-09-29 00:22:45 +0200

    mtd: rawnand: pasemi: Keep the driver compatible with on-die ECC engines
    commit f16b7d2a5e810fcf4b15d096246d0d445da9cc88 upstream.
    
    Following the introduction of the generic ECC engine infrastructure, it
    was necessary to reorganize the code and move the ECC configuration in
    the ->attach_chip() hook. Failing to do that properly lead to a first
    series of fixes supposed to stabilize the situation. Unfortunately, this
    only fixed the use of software ECC engines, preventing any other kind of
    engine to be used, including on-die ones.
    
    It is now time to (finally) fix the situation by ensuring that we still
    provide a default (eg. software ECC) but will still support different
    ECC engines such as on-die ECC engines if properly described in the
    device tree.
    
    There are no changes needed on the core side in order to do this, but we
    just need to leverage the logic there which allows:
    1- a subsystem default (set to Host engines in the raw NAND world)
    2- a driver specific default (here set to software ECC engines)
    3- any type of engine requested by the user (ie. described in the DT)
    
    As the raw NAND subsystem has not yet been fully converted to the ECC
    engine infrastructure, in order to provide a default ECC engine for this
    driver we need to set chip->ecc.engine_type *before* calling
    nand_scan(). During the initialization step, the core will consider this
    entry as the default engine for this driver. This value may of course
    be overloaded by the user if the usual DT properties are provided.
    
    Fixes: 8fc6f1f042b2 ("mtd: rawnand: pasemi: Move the ECC initialization to ->attach_chip()")
    Cc: stable@vger.kernel.org
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Link: https://lore.kernel.org/linux-mtd/20210928222258.199726-7-miquel.raynal@bootlin.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/mtd/nand/raw/pasemi_nand.c
+++ drivers/mtd/nand/raw/pasemi_nand.c
@@ -75,9 +75,8 @@ static int pasemi_device_ready(struct nand_chip *chip)
 
 static int pasemi_attach_chip(struct nand_chip *chip)
 {
-	chip->ecc.engine_type = NAND_ECC_ENGINE_TYPE_SOFT;
-
-	if (chip->ecc.algo == NAND_ECC_ALGO_UNKNOWN)
+	if (chip->ecc.engine_type == NAND_ECC_ENGINE_TYPE_SOFT &&
+	    chip->ecc.algo == NAND_ECC_ALGO_UNKNOWN)
 		chip->ecc.algo = NAND_ECC_ALGO_HAMMING;
 
 	return 0;
@@ -153,6 +152,13 @@ static int pasemi_nand_probe(struct platform_device *ofdev)
 
 	/* Enable the following for a flash based bad block table */
 	chip->bbt_options = NAND_BBT_USE_FLASH;
+
+	/*
+	 * This driver assumes that the default ECC engine should be TYPE_SOFT.
+	 * Set ->engine_type before registering the NAND devices in order to
+	 * provide a driver specific default value.
+	 */
+	chip->ecc.engine_type = NAND_ECC_ENGINE_TYPE_SOFT;
 
 	/* Scan to find existence of the device */
 	err = nand_scan(chip, 1);
