597e9c7a4acebcdc29bf87e6672ad8e256beb2f4
Trond Myklebust <trond.myklebust@hammerspace.com>
2021-09-28 11:24:57 -0400

    NFS: Ignore the directory size when marking for revalidation
    [ Upstream commit a6a361c4ca3cc3e6f3b39d1b6bca1de90f5f4b11 ]
    
    If we want to revalidate the directory, then just mark the change
    attribute as invalid.
    
    Fixes: 13c0b082b6a9 ("NFS: Replace use of NFS_INO_REVAL_PAGECACHE when checking cache validity")
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    Tested-by: Benjamin Coddington <bcodding@redhat.com>
    Reviewed-by: Benjamin Coddington <bcodding@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/nfs/dir.c
+++ fs/nfs/dir.c
@@ -1413,7 +1413,7 @@ out_force:
 static void nfs_mark_dir_for_revalidate(struct inode *inode)
 {
 	spin_lock(&inode->i_lock);
-	nfs_set_cache_invalid(inode, NFS_INO_REVAL_PAGECACHE);
+	nfs_set_cache_invalid(inode, NFS_INO_INVALID_CHANGE);
 	spin_unlock(&inode->i_lock);
 }
 
