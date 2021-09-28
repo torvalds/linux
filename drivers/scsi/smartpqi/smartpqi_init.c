80982656b78ec23cb4918a90ee723ac2db83df1c
Mike McGowen <Mike.McGowen@microchip.com>
2021-09-28 18:54:41 -0500

    scsi: smartpqi: Add 3252-8i PCI id
    Add PCI ID information for the Adaptec SmartRAID 3252-8i controller:
    
        9005 / 028F / 9005 / 14A2
    
    Link: https://lore.kernel.org/r/20210928235442.201875-11-don.brace@microchip.com
    Reviewed-by: Scott Benesh <scott.benesh@microchip.com>
    Reviewed-by: Scott Teel <scott.teel@microchip.com>
    Acked-by: John Donnelly <john.p.donnelly@oracle.com>
    Signed-off-by: Mike McGowen <Mike.McGowen@microchip.com>
    Signed-off-by: Don Brace <don.brace@microchip.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/smartpqi/smartpqi_init.c
+++ drivers/scsi/smartpqi/smartpqi_init.c
@@ -9289,6 +9289,10 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       PCI_VENDOR_ID_ADAPTEC2, 0x14a2)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       PCI_VENDOR_ID_ADAPTEC2, 0x14b0)
 	},
 	{
