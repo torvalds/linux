2929bc3329f4c7e4df400acca2b1844492650bfd
Trond Myklebust <trond.myklebust@hammerspace.com>
2021-09-28 12:37:05 -0400

    NFS: Fix up nfs_readdir_inode_mapping_valid()
    The check for duplicate readdir cookies should only care if the change
    attribute is invalid or the data cache is invalid.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    Tested-by: Benjamin Coddington <bcodding@redhat.com>
    Reviewed-by: Benjamin Coddington <bcodding@redhat.com>

--- fs/nfs/dir.c
+++ fs/nfs/dir.c
@@ -411,7 +411,8 @@ out_eof:
 static bool
 nfs_readdir_inode_mapping_valid(struct nfs_inode *nfsi)
 {
-	if (nfsi->cache_validity & (NFS_INO_INVALID_ATTR|NFS_INO_INVALID_DATA))
+	if (nfsi->cache_validity & (NFS_INO_INVALID_CHANGE |
+				    NFS_INO_INVALID_DATA))
 		return false;
 	smp_rmb();
 	return !test_bit(NFS_INO_INVALIDATING, &nfsi->flags);
