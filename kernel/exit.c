670721c7bd2a6e16e40db29b2707a27bdecd6928
Thomas Gleixner <tglx@linutronix.de>
2021-09-28 14:24:28 +0200

    sched: Move kprobes cleanup out of finish_task_switch()
    Doing cleanups in the tail of schedule() is a latency punishment for the
    incoming task. The point of invoking kprobes_task_flush() for a dead task
    is that the instances are returned and cannot leak when __schedule() is
    kprobed.
    
    Move it into the delayed cleanup.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20210928122411.537994026@linutronix.de

--- kernel/exit.c
+++ kernel/exit.c
@@ -64,6 +64,7 @@
 #include <linux/rcuwait.h>
 #include <linux/compat.h>
 #include <linux/io_uring.h>
+#include <linux/kprobes.h>
 
 #include <linux/uaccess.h>
 #include <asm/unistd.h>
@@ -168,6 +169,7 @@ static void delayed_put_task_struct(struct rcu_head *rhp)
 {
 	struct task_struct *tsk = container_of(rhp, struct task_struct, rcu);
 
+	kprobe_flush_task(tsk);
 	perf_event_delayed_put(tsk);
 	trace_sched_process_free(tsk);
 	put_task_struct(tsk);
--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4846,12 +4846,6 @@ static struct rq *finish_task_switch(struct task_struct *prev)
 		if (prev->sched_class->task_dead)
 			prev->sched_class->task_dead(prev);
 
-		/*
-		 * Remove function-return probe instances associated with this
-		 * task and put them back on the free list.
-		 */
-		kprobe_flush_task(prev);
-
 		/* Task is done with its stack. */
 		put_task_stack(prev);
 
