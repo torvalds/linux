e9076e7f23aa087f8b7257f3be9e9586f341e3b1
Bart Van Assche <bvanassche@acm.org>
2021-09-29 11:23:18 -0700

    scsi: core: Fix spelling in a source code comment
    The typo in this source code comment makes the comment confusing. Clear up
    the confusion by fixing the typo.
    
    Link: https://lore.kernel.org/r/20210929182318.2060489-1-bvanassche@acm.org
    Fixes: bc85dc500f9d ("scsi: remove scsi_end_request")
    Cc: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/scsi_lib.c
+++ drivers/scsi/scsi_lib.c
@@ -949,7 +949,7 @@ void scsi_io_completion(struct scsi_cmnd *cmd, unsigned int good_bytes)
 
 	/*
 	 * If there had been no error, but we have leftover bytes in the
-	 * requeues just queue the command up again.
+	 * request just queue the command up again.
 	 */
 	if (likely(result == 0))
 		scsi_io_completion_reprep(cmd, q);
