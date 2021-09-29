be8ef91d61663bfcc79f5c0eb33f9a26ab74de73
Saravana Kannan <saravanak@google.com>
2021-09-29 12:05:49 -0700

    driver core: Reject pointless SYNC_STATE_ONLY device links
    commit f729a592adb6760013c3e48622a5bf256b992452 upstream.
    
    SYNC_STATE_ONLY device links intentionally allow cycles because cyclic
    sync_state() dependencies are valid and necessary.
    
    However a SYNC_STATE_ONLY device link where the consumer and the supplier
    are the same device is pointless because the device link would be deleted
    as soon as the device probes (because it's also the consumer) and won't
    affect when the sync_state() callback is called. It's a waste of CPU cycles
    and memory to create this device link. So reject any attempts to create
    such a device link.
    
    Fixes: 05ef983e0d65 ("driver core: Add device link support for SYNC_STATE_ONLY flag")
    Cc: stable <stable@vger.kernel.org>
    Reported-by: Ulf Hansson <ulf.hansson@linaro.org>
    Reviewed-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Link: https://lore.kernel.org/r/20210929190549.860541-1-saravanak@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -549,7 +549,8 @@ struct device_link *device_link_add(struct device *consumer,
 {
 	struct device_link *link;
 
-	if (!consumer || !supplier || flags & ~DL_ADD_VALID_FLAGS ||
+	if (!consumer || !supplier || consumer == supplier ||
+	    flags & ~DL_ADD_VALID_FLAGS ||
 	    (flags & DL_FLAG_STATELESS && flags & DL_MANAGED_LINK_FLAGS) ||
 	    (flags & DL_FLAG_SYNC_STATE_ONLY &&
 	     flags != DL_FLAG_SYNC_STATE_ONLY) ||
