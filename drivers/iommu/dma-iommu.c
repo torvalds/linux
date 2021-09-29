06e620345d544e559b2961cb5a676ec9c80c8950
David Stevens <stevensd@chromium.org>
2021-09-29 11:32:55 +0900

    iommu/dma: Fix arch_sync_dma for map
    When calling arch_sync_dma, we need to pass it the memory that's
    actually being used for dma. When using swiotlb bounce buffers, this is
    the bounce buffer. Move arch_sync_dma into the __iommu_dma_map_swiotlb
    helper, so it can use the bounce buffer address if necessary.
    
    Now that iommu_dma_map_sg delegates to a function which takes care of
    architectural syncing in the untrusted device case, the call to
    iommu_dma_sync_sg_for_device can be moved so it only occurs for trusted
    devices. Doing the sync for untrusted devices before mapping never
    really worked, since it needs to be able to target swiotlb buffers.
    
    This also moves the architectural sync to before the call to
    __iommu_dma_map, to guarantee that untrusted devices can't see stale
    data they shouldn't see.
    
    Fixes: 82612d66d51d ("iommu: Allow the iommu/dma api to use bounce buffers")
    Signed-off-by: David Stevens <stevensd@chromium.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>
    Link: https://lore.kernel.org/r/20210929023300.335969-3-stevensd@google.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/dma-iommu.c
+++ drivers/iommu/dma-iommu.c
@@ -593,6 +593,9 @@ static dma_addr_t __iommu_dma_map_swiotlb(struct device *dev, phys_addr_t phys,
 		memset(padding_start, 0, padding_size);
 	}
 
+	if (!coherent && !(attrs & DMA_ATTR_SKIP_CPU_SYNC))
+		arch_sync_dma_for_device(phys, org_size, dir);
+
 	iova = __iommu_dma_map(dev, phys, aligned_size, prot, dma_mask);
 	if (iova == DMA_MAPPING_ERROR && is_swiotlb_buffer(dev, phys))
 		swiotlb_tbl_unmap_single(dev, phys, org_size, dir, attrs);
@@ -860,14 +863,9 @@ static dma_addr_t iommu_dma_map_page(struct device *dev, struct page *page,
 {
 	phys_addr_t phys = page_to_phys(page) + offset;
 	bool coherent = dev_is_dma_coherent(dev);
-	dma_addr_t dma_handle;
 
-	dma_handle = __iommu_dma_map_swiotlb(dev, phys, size, dma_get_mask(dev),
+	return __iommu_dma_map_swiotlb(dev, phys, size, dma_get_mask(dev),
 			coherent, dir, attrs);
-	if (!coherent && !(attrs & DMA_ATTR_SKIP_CPU_SYNC) &&
-	    dma_handle != DMA_MAPPING_ERROR)
-		arch_sync_dma_for_device(phys, size, dir);
-	return dma_handle;
 }
 
 static void iommu_dma_unmap_page(struct device *dev, dma_addr_t dma_handle,
@@ -1012,11 +1010,11 @@ static int iommu_dma_map_sg(struct device *dev, struct scatterlist *sg,
 		goto out;
 	}
 
-	if (!(attrs & DMA_ATTR_SKIP_CPU_SYNC))
-		iommu_dma_sync_sg_for_device(dev, sg, nents, dir);
-
 	if (dev_is_untrusted(dev))
 		return iommu_dma_map_sg_swiotlb(dev, sg, nents, dir, attrs);
+
+	if (!(attrs & DMA_ATTR_SKIP_CPU_SYNC))
+		iommu_dma_sync_sg_for_device(dev, sg, nents, dir);
 
 	/*
 	 * Work out how much IOVA space we need, and align the segments to
