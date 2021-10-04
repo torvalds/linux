73740f948252e424a01465155d8737bceae23653
Manasi Navare <manasi.d.navare@intel.com>
2021-10-04 04:59:13 -0700

    drm/atomic: Add the crtc to affected crtc only if uapi.enable = true
    commit 5ec1cebd59300ddd26dbaa96c17c508764eef911 upstream.
    
    In case of a modeset where a mode gets split across multiple CRTCs
    in the driver specific implementation (bigjoiner in i915) we wrongly count
    the affected CRTCs based on the drm_crtc_mask and indicate the stolen CRTC as
    an affected CRTC in atomic_check_only().
    This triggers a warning since affected CRTCs doent match requested CRTC.
    
    To fix this in such bigjoiner configurations, we should only
    increment affected crtcs if that CRTC is enabled in UAPI not
    if it is just used internally in the driver to split the mode.
    
    v3: Add the same uapi crtc_state->enable check in requested
    crtc calc (Ville)
    
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Cc: Simon Ser <contact@emersion.fr>
    Cc: Pekka Paalanen <pekka.paalanen@collabora.co.uk>
    Cc: Daniel Stone <daniels@collabora.com>
    Cc: Daniel Vetter <daniel.vetter@intel.com>
    Cc: dri-devel@lists.freedesktop.org
    Cc: <stable@vger.kernel.org> # v5.11+
    Fixes: 919c2299a893 ("drm/i915: Enable bigjoiner")
    Signed-off-by: Manasi Navare <manasi.d.navare@intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211004115913.23889-1-manasi.d.navare@intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -1310,8 +1310,10 @@ int drm_atomic_check_only(struct drm_atomic_state *state)
 
 	DRM_DEBUG_ATOMIC("checking %p\n", state);
 
-	for_each_new_crtc_in_state(state, crtc, new_crtc_state, i)
-		requested_crtc |= drm_crtc_mask(crtc);
+	for_each_new_crtc_in_state(state, crtc, new_crtc_state, i) {
+		if (new_crtc_state->enable)
+			requested_crtc |= drm_crtc_mask(crtc);
+	}
 
 	for_each_oldnew_plane_in_state(state, plane, old_plane_state, new_plane_state, i) {
 		ret = drm_atomic_plane_check(old_plane_state, new_plane_state);
@@ -1360,8 +1362,10 @@ int drm_atomic_check_only(struct drm_atomic_state *state)
 		}
 	}
 
-	for_each_new_crtc_in_state(state, crtc, new_crtc_state, i)
-		affected_crtc |= drm_crtc_mask(crtc);
+	for_each_new_crtc_in_state(state, crtc, new_crtc_state, i) {
+		if (new_crtc_state->enable)
+			affected_crtc |= drm_crtc_mask(crtc);
+	}
 
 	/*
 	 * For commits that allow modesets drivers can add other CRTCs to the
