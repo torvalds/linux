4f245d5bf0f7432c881e22a77066160a6cba8e03
John Garry <john.garry@huawei.com>
2021-10-05 18:23:33 +0800

    blk-mq: Don't clear driver tags own mapping
    Function blk_mq_clear_rq_mapping() is required to clear the sched tags
    mappings in driver tags rqs[].
    
    But there is no need for a driver tags to clear its own mapping, so skip
    clearing the mapping in this scenario.
    
    Signed-off-by: John Garry <john.garry@huawei.com>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Reviewed-by: Ming Lei <ming.lei@redhat.com>
    Link: https://lore.kernel.org/r/1633429419-228500-9-git-send-email-john.garry@huawei.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -2317,6 +2317,10 @@ static void blk_mq_clear_rq_mapping(struct blk_mq_tags *drv_tags,
 	struct page *page;
 	unsigned long flags;
 
+	/* There is no need to clear a driver tags own mapping */
+	if (drv_tags == tags)
+		return;
+
 	list_for_each_entry(page, &tags->page_list, lru) {
 		unsigned long start = (unsigned long)page_address(page);
 		unsigned long end = start + order_to_size(page->private);
