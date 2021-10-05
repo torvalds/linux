1d58a17ef54599506d44c45ac95be27273a4d2b1
Quentin Perret <qperret@google.com>
2021-10-05 10:01:41 +0100

    KVM: arm64: Fix host stage-2 PGD refcount
    The KVM page-table library refcounts the pages of concatenated stage-2
    PGDs individually. However, when running KVM in protected mode, the
    host's stage-2 PGD is currently managed by EL2 as a single high-order
    compound page, which can cause the refcount of the tail pages to reach 0
    when they shouldn't, hence corrupting the page-table.
    
    Fix this by introducing a new hyp_split_page() helper in the EL2 page
    allocator (matching the kernel's split_page() function), and make use of
    it from host_s2_zalloc_pages_exact().
    
    Fixes: 1025c8c0c6ac ("KVM: arm64: Wrap the host with a stage 2")
    Acked-by: Will Deacon <will@kernel.org>
    Suggested-by: Will Deacon <will@kernel.org>
    Signed-off-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20211005090155.734578-5-qperret@google.com

--- arch/arm64/kvm/hyp/nvhe/page_alloc.c
+++ arch/arm64/kvm/hyp/nvhe/page_alloc.c
@@ -193,6 +193,20 @@ void hyp_get_page(struct hyp_pool *pool, void *addr)
 	hyp_spin_unlock(&pool->lock);
 }
 
+void hyp_split_page(struct hyp_page *p)
+{
+	unsigned short order = p->order;
+	unsigned int i;
+
+	p->order = 0;
+	for (i = 1; i < (1 << order); i++) {
+		struct hyp_page *tail = p + i;
+
+		tail->order = 0;
+		hyp_set_page_refcounted(tail);
+	}
+}
+
 void *hyp_alloc_pages(struct hyp_pool *pool, unsigned short order)
 {
 	unsigned short i = order;
