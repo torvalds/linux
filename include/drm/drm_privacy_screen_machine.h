befe5404a00b3b1547c944738df4a9229909bdc9
Hans de Goede <hdegoede@redhat.com>
2021-10-05 22:23:15 +0200

    drm/privacy-screen: Add X86 specific arch init code
    Add X86 specific arch init code, which fills the privacy-screen lookup
    table by checking for various vendor specific ACPI interfaces for
    controlling the privacy-screen.
    
    This initial version only checks for the Lenovo Thinkpad specific ACPI
    methods for privacy-screen control.
    
    Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>
    Reviewed-by: Lyude Paul <lyude@redhat.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211005202322.700909-4-hdegoede@redhat.com

--- include/drm/drm_privacy_screen_machine.h
+++ include/drm/drm_privacy_screen_machine.h
@@ -31,11 +31,16 @@ struct drm_privacy_screen_lookup {
 void drm_privacy_screen_lookup_add(struct drm_privacy_screen_lookup *lookup);
 void drm_privacy_screen_lookup_remove(struct drm_privacy_screen_lookup *lookup);
 
+#if IS_ENABLED(CONFIG_DRM_PRIVACY_SCREEN) && IS_ENABLED(CONFIG_X86)
+void drm_privacy_screen_lookup_init(void);
+void drm_privacy_screen_lookup_exit(void);
+#else
 static inline void drm_privacy_screen_lookup_init(void)
 {
 }
 static inline void drm_privacy_screen_lookup_exit(void)
 {
 }
+#endif
 
 #endif
