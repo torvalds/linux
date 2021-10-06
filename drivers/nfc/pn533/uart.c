be5f60d8b6f9611ff3a687de5d47d3ed9fb2cfb0
Rikard Falkeborn <rikard.falkeborn@gmail.com>
2021-10-07 00:47:37 +0200

    nfc: pn533: Constify serdev_device_ops
    The only usage of pn532_serdev_ops is to pass its address to
    serdev_device_set_client_ops(), which takes a pointer to const
    serdev_device_ops as argument. Make it const to allow the compiler to
    put it in read-only memory.
    
    Signed-off-by: Rikard Falkeborn <rikard.falkeborn@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/nfc/pn533/uart.c
+++ drivers/nfc/pn533/uart.c
@@ -224,7 +224,7 @@ static int pn532_receive_buf(struct serdev_device *serdev,
 	return i;
 }
 
-static struct serdev_device_ops pn532_serdev_ops = {
+static const struct serdev_device_ops pn532_serdev_ops = {
 	.receive_buf = pn532_receive_buf,
 	.write_wakeup = serdev_device_write_wakeup,
 };
