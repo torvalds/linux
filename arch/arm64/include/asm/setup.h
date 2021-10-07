1dfde0892b325ed1872975053c6745f5148050a2
Anders Roxell <anders.roxell@linaro.org>
2021-10-07 21:56:01 +0200

    arm64: asm: setup.h: export common variables
    When building the kernel with sparse enabled 'C=1' the following
    warnings can be seen:
    
    arch/arm64/kernel/setup.c:58:13: warning: symbol '__fdt_pointer' was not declared. Should it be static?
    arch/arm64/kernel/setup.c:84:25: warning: symbol 'boot_args' was not declared. Should it be static?
    
    Rework so the variables are exported, since these two variable are
    created and used in setup.c, also used in head.S.
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>
    Link: https://lore.kernel.org/r/20211007195601.677474-1-anders.roxell@linaro.org
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/setup.h
+++ arch/arm64/include/asm/setup.h
@@ -8,4 +8,10 @@
 void *get_early_fdt_ptr(void);
 void early_fdt_map(u64 dt_phys);
 
+/*
+ * These two variables are used in the head.S file.
+ */
+extern phys_addr_t __fdt_pointer __initdata;
+extern u64 __cacheline_aligned boot_args[4];
+
 #endif
