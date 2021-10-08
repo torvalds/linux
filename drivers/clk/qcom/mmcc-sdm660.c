ca8460ba12718d19cd37917db307ace6942e8560
AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
2021-10-08 12:20:40 +0200

    clk: qcom: mmcc-sdm660: Add necessary CXCs to venus_gdsc
    As also shown on downstream dts[1], for a correct operation of the
    Venus block, we have to retain MEM/PERIPH when halting the video_core,
    video_axi and video_subcore0 branches: add these CXCs to the main
    Venus GDSC.
    
    [1]: https://github.com/sonyxperiadev/kernel/blob/aosp/LA.UM.6.4.r1/arch/arm/boot/dts/qcom/sdm660-vidc.dtsi#L80
    
    Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Link: https://lore.kernel.org/r/20211008102041.268253-1-angelogioacchino.delregno@collabora.com
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/qcom/mmcc-sdm660.c
+++ drivers/clk/qcom/mmcc-sdm660.c
@@ -2560,6 +2560,8 @@ static struct clk_branch video_subcore0_clk = {
 
 static struct gdsc venus_gdsc = {
 	.gdscr = 0x1024,
+	.cxcs = (unsigned int[]){ 0x1028, 0x1034, 0x1048 },
+	.cxc_count = 3,
 	.pd = {
 		.name = "venus",
 	},
