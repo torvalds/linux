1a84a308acda285cf9fae548262b8e199845b2d1
Nikola Pavlica <pavlica.nikola@gmail.com>
2021-10-11 23:27:31 +0200

    drm/panel-simple: Add Vivax TPC-9150 panel v6
    The model and make of the LCD panel of the Vivax TPC-9150 is unknown,
    hence the panel settings that were retrieved with a FEX dump are named
    after the device NOT the actual panel.
    
    The LCD in question is a 50 pin MISO TFT LCD panel of the resolution
    1024x600 used by the aforementioned device.
    
    Version 2, as Thierry kindly suggested that I fix the order in which the
    panel was ordered compared to others.
    
    Version 3, filling in the required info suggested by Sam. Plus some
    factual issues that I've corrected myself (tested working)
    
    Version 4, rearranged the display parameters and fix invalid bit format
    issue. (Thanks Sam)
    
    Version 5, referred to FEX file instead of manual debugging for
    information.
    
    Version 6, same as above. This time, it'll be documented.
    
    A bit of context first: I experimented with this a long time ago whilst
    I was first learning how to get Linux running on Allwinner boards, I
    didn't have many resources at hand so this was quite slow. Anyways, I
    stumbled upon this guide (https://linux-sunxi.org/LCD) and was reading
    about how to setup the LCD for my tablet. Since I was able to make a
    proper FEX dump, I was also able to read the correct parameters for
    myself without relying on leaked documents or part numbers and whatnot.
    
    In the FEX dump the value lcd_frm IS SET to 1, which means, at least
    according to the document, that this display is INDEED an 18 bit per
    pixel panel. Compiling U-Boot and seeing the tux in proper colors
    confirmed this. As per Sam Ravnborg's suggestion, I've changed the panel
    to his format "MEDIA_BUS_FMT_RGB666_1X7X3_SPWG", however this does not
    lead to any actual change in regards to the functionality since the sunxi
    panel driver just ignores this value. However, hopefully this clears up
    any errors down the road as either the driver becomes advanced enough to
    not ignore this value or that some other piece of software relies on
    this value being known. PS: Apologies to the maintainers that have to
    endure my misjudgement about how these things work.
    
    As for the concerns about a single patch series, I wasn't sure where to
    send the patches as they clearly aren't dt-bindings related and my
    previous patches have ended up in drm-misc-fixes anyway. So I'm guessing
    I'll be fine if I just post them in the list from last time???
    
    Signed-off-by: Nikola Pavlica <pavlica.nikola@gmail.com>
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211011212731.77763-1-pavlica.nikola@gmail.com

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -3498,6 +3498,31 @@ static const struct panel_desc urt_umsh_8596md_parallel = {
 	.bus_format = MEDIA_BUS_FMT_RGB666_1X18,
 };
 
+static const struct drm_display_mode vivax_tpc9150_panel_mode = {
+	.clock = 60000,
+	.hdisplay = 1024,
+	.hsync_start = 1024 + 160,
+	.hsync_end = 1024 + 160 + 100,
+	.htotal = 1024 + 160 + 100 + 60,
+	.vdisplay = 600,
+	.vsync_start = 600 + 12,
+	.vsync_end = 600 + 12 + 10,
+	.vtotal = 600 + 12 + 10 + 13,
+};
+
+static const struct panel_desc vivax_tpc9150_panel = {
+	.modes = &vivax_tpc9150_panel_mode,
+	.num_modes = 1,
+	.bpc = 6,
+	.size = {
+		.width = 200,
+		.height = 115,
+	},
+	.bus_format = MEDIA_BUS_FMT_RGB666_1X7X3_SPWG,
+	.bus_flags = DRM_BUS_FLAG_DE_HIGH,
+	.connector_type = DRM_MODE_CONNECTOR_LVDS,
+};
+
 static const struct drm_display_mode vl050_8048nt_c01_mode = {
 	.clock = 33333,
 	.hdisplay = 800,
@@ -3948,6 +3973,9 @@ static const struct of_device_id platform_of_match[] = {
 	}, {
 		.compatible = "urt,umsh-8596md-20t",
 		.data = &urt_umsh_8596md_parallel,
+	}, {
+		.compatible = "vivax,tpc9150-panel",
+		.data = &vivax_tpc9150_panel,
 	}, {
 		.compatible = "vxt,vl050-8048nt-c01",
 		.data = &vl050_8048nt_c01,
