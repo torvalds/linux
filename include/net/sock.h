56596148ae7233a5c2959ab413d7329ac5002827
Maciej Żenczykowski <maze@google.com>
2021-10-11 10:42:27 -0700

    ANDROID: Different fix for KABI breakage in 5.10.71 in struct sock
    This partially reverts commit 627dc3c79c3b ("ANDROID: Fix up KABI
    breakage in 5.10.71 in struct sock")
    
    Then fixes things up in such a way that
      BUILD_CONFIG=build/build.config.net_test for kernel_test
    still works.
    
    Bug: 202712021
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: I40e24d7924fcffc23a2b8cc5ad50049808aa9455
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -481,6 +481,11 @@ struct sock {
 	u32			sk_ack_backlog;
 	u32			sk_max_ack_backlog;
 	kuid_t			sk_uid;
+#if IS_ENABLED(CONFIG_DEBUG_SPINLOCK) || IS_ENABLED(CONFIG_DEBUG_LOCK_ALLOC)
+	spinlock_t		sk_peer_lock;
+#else
+	/* sk_peer_lock is in the ANDROID_KABI_RESERVE(1) field below */
+#endif
 	struct pid		*sk_peer_pid;
 	const struct cred	*sk_peer_cred;
 
@@ -524,7 +529,11 @@ struct sock {
 #endif
 	struct rcu_head		sk_rcu;
 
+#if IS_ENABLED(CONFIG_DEBUG_SPINLOCK) || IS_ENABLED(CONFIG_DEBUG_LOCK_ALLOC)
+	ANDROID_KABI_RESERVE(1);
+#else
 	ANDROID_KABI_USE(1, spinlock_t sk_peer_lock);
+#endif
 	ANDROID_KABI_RESERVE(2);
 	ANDROID_KABI_RESERVE(3);
 	ANDROID_KABI_RESERVE(4);
