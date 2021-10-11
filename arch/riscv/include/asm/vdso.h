683b33f7e7ec0d827502dd0d8b947bdcfe2df6f1
Kefeng Wang <wangkefeng.wang@huawei.com>
2021-10-11 19:05:59 +0800

    riscv/vdso: Drop unneeded part due to merge issue
    It seems that something is wrong when patch "riscv/vdso:
    Refactor asm/vdso.h" is merged.
    
    Let's fix the merge issue.
    
    Fixes: 8edab02386c3 ("Merge remote-tracking branch 'palmer/riscv-vdso-cleanup' into for-next")
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Palmer Dabbelt <palmerdabbelt@google.com>

--- arch/riscv/include/asm/vdso.h
+++ arch/riscv/include/asm/vdso.h
@@ -8,14 +8,6 @@
 #ifndef _ASM_RISCV_VDSO_H
 #define _ASM_RISCV_VDSO_H
 
-
-/*
- * All systems with an MMU have a VDSO, but systems without an MMU don't
- * support shared libraries and therefor don't have one.
- */
-#ifdef CONFIG_MMU
-
-#include <linux/types.h>
 /*
  * All systems with an MMU have a VDSO, but systems without an MMU don't
  * support shared libraries and therefor don't have one.
@@ -29,9 +21,6 @@
 
 #define VDSO_SYMBOL(base, name)							\
 	(void __user *)((unsigned long)(base) + __vdso_##name##_offset)
-
-#endif /* CONFIG_MMU */
-
 #endif /* !__ASSEMBLY__ */
 
 #endif /* CONFIG_MMU */
