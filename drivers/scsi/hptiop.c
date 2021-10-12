e8fbc28e7fc71da2c820a5d3db49ca9bef7f4f3e
Bart Van Assche <bvanassche@acm.org>
2021-10-12 16:35:34 -0700

    scsi: hptiop: Switch to attribute groups
    struct device supports attribute groups directly but does not support
    struct device_attribute directly. Hence switch to attribute groups.
    
    Link: https://lore.kernel.org/r/20211012233558.4066756-23-bvanassche@acm.org
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/hptiop.c
+++ drivers/scsi/hptiop.c
@@ -1146,11 +1146,13 @@ static struct device_attribute hptiop_attr_fw_version = {
 	.show = hptiop_show_fw_version,
 };
 
-static struct device_attribute *hptiop_attrs[] = {
-	&hptiop_attr_version,
-	&hptiop_attr_fw_version,
+static struct attribute *hptiop_host_attrs[] = {
+	&hptiop_attr_version.attr,
+	&hptiop_attr_fw_version.attr,
 	NULL
 };
+
+ATTRIBUTE_GROUPS(hptiop_host);
 
 static int hptiop_slave_config(struct scsi_device *sdev)
 {
@@ -1168,7 +1170,7 @@ static struct scsi_host_template driver_template = {
 	.info                       = hptiop_info,
 	.emulated                   = 0,
 	.proc_name                  = driver_name,
-	.shost_attrs                = hptiop_attrs,
+	.shost_groups		    = hptiop_host_groups,
 	.slave_configure            = hptiop_slave_config,
 	.this_id                    = -1,
 	.change_queue_depth         = hptiop_adjust_disk_queue_depth,
