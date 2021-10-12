00aeaf329a3a1ea3d3606fefa1d29f69f828bd21
Luo Jiaxing <luojiaxing@huawei.com>
2021-10-12 20:26:27 +0800

    scsi: libsas: Export sas_phy_enable()
    Export sas_phy_enable() so LLDDs can directly use it to control remote
    phys.
    
    We already do this for companion function sas_phy_reset().
    
    Link: https://lore.kernel.org/r/1634041588-74824-4-git-send-email-john.garry@huawei.com
    Signed-off-by: Luo Jiaxing <luojiaxing@huawei.com>
    Signed-off-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/libsas/sas_init.c
+++ drivers/scsi/libsas/sas_init.c
@@ -254,7 +254,7 @@ static int transport_sas_phy_reset(struct sas_phy *phy, int hard_reset)
 	}
 }
 
-static int sas_phy_enable(struct sas_phy *phy, int enable)
+int sas_phy_enable(struct sas_phy *phy, int enable)
 {
 	int ret;
 	enum phy_func cmd;
@@ -286,6 +286,7 @@ static int sas_phy_enable(struct sas_phy *phy, int enable)
 	}
 	return ret;
 }
+EXPORT_SYMBOL_GPL(sas_phy_enable);
 
 int sas_phy_reset(struct sas_phy *phy, int hard_reset)
 {
