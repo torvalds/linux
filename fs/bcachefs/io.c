edeb986b017e9489add4daa7e61bc79cdbfb913c
Kent Overstreet <kent.overstreet@gmail.com>
2021-10-13 13:45:46 -0400

    bcachefs: Don't allocate too-big bios
    This fixes a null ptr deref in bio_alloc_bioset() -> biovec_slab()
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/io.c
+++ fs/bcachefs/io.c
@@ -782,6 +782,8 @@ static struct bio *bch2_write_bio_alloc(struct bch_fs *c,
 				       ? ((unsigned long) buf & (PAGE_SIZE - 1))
 				       : 0), PAGE_SIZE);
 
+	pages = min(pages, BIO_MAX_VECS);
+
 	bio = bio_alloc_bioset(NULL, pages, 0,
 			       GFP_NOIO, &c->bio_write);
 	wbio			= wbio_init(bio);
