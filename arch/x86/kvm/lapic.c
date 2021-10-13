9139a7a64581c80d157027ae20e86f2f24d4292c
Sean Christopherson <seanjc@google.com>
2021-10-12 17:35:54 -0700

    KVM: x86: WARN if APIC HW/SW disable static keys are non-zero on unload
    WARN if the static keys used to track if any vCPU has disabled its APIC
    are left elevated at module exit.  Unlike the underflow case, nothing in
    the static key infrastructure will complain if a key is left elevated,
    and because an elevated key only affects performance, nothing in KVM will
    fail if either key is improperly incremented.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20211013003554.47705-3-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/lapic.c
+++ arch/x86/kvm/lapic.c
@@ -2946,5 +2946,7 @@ int kvm_apic_accept_events(struct kvm_vcpu *vcpu)
 void kvm_lapic_exit(void)
 {
 	static_key_deferred_flush(&apic_hw_disabled);
+	WARN_ON(static_branch_unlikely(&apic_hw_disabled.key));
 	static_key_deferred_flush(&apic_sw_disabled);
+	WARN_ON(static_branch_unlikely(&apic_sw_disabled.key));
 }
