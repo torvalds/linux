ba530fea8ca1b57ee71d4e62f287a5d7ed92f789
Jakub Kicinski <kuba@kernel.org>
2021-10-13 13:54:50 -0700

    ethernet: remove random_ether_addr()
    random_ether_addr() was the original name of the helper which
    was kept for backward compatibility (?) after the rename in
    commit 0a4dd594982a ("etherdevice: Rename random_ether_addr
    to eth_random_addr").
    
    We have a single random_ether_addr() caller left in tree
    while there are 70 callers of eth_random_addr().
    Time to drop this define.
    
    Reviewed-by: Simon Horman <simon.horman@corigine.com>
    Link: https://lore.kernel.org/r/20211013205450.328092-1-kuba@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/linux/etherdevice.h
+++ include/linux/etherdevice.h
@@ -234,8 +234,6 @@ static inline void eth_random_addr(u8 *addr)
 	addr[0] |= 0x02;	/* set local assignment bit (IEEE802) */
 }
 
-#define random_ether_addr(addr) eth_random_addr(addr)
-
 /**
  * eth_broadcast_addr - Assign broadcast address
  * @addr: Pointer to a six-byte array containing the Ethernet address
