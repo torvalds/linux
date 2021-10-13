72ba4505622ddc765f227fb24270fc202193aab5
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2021-10-13 22:32:17 +0200

    iio: dac: ad5592r: Make ad5592r_remove() return void
    Up to now ad5592r_remove() returns zero unconditionally. Make it return
    void instead which makes it easier to see in the callers that there is
    no error to handle.
    
    Also the return value of i2c and spi remove callbacks is ignored anyway.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Reviewed-by: Alexandru Ardelean <ardeleanalex@gmail.com>
    Link: https://lore.kernel.org/r/20211013203223.2694577-10-u.kleine-koenig@pengutronix.de
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/dac/ad5592r.c
+++ drivers/iio/dac/ad5592r.c
@@ -132,7 +132,9 @@ static int ad5592r_spi_probe(struct spi_device *spi)
 
 static int ad5592r_spi_remove(struct spi_device *spi)
 {
-	return ad5592r_remove(&spi->dev);
+	ad5592r_remove(&spi->dev);
+
+	return 0;
 }
 
 static const struct spi_device_id ad5592r_spi_ids[] = {
