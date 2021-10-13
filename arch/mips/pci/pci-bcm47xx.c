a274bdbdfcf7d0631004b20c03d90bbfed70c9d2
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2021-10-13 16:49:39 +0300

    bcm47xx: Get rid of redundant 'else'
    In the snipped like
    
    	if (...)
    		return ...;
    	else
    
    the 'else' is redundant. Get rid of it.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/pci/pci-bcm47xx.c
+++ arch/mips/pci/pci-bcm47xx.c
@@ -93,12 +93,10 @@ int pcibios_plat_dev_init(struct pci_dev *dev)
 #ifdef CONFIG_BCM47XX_SSB
 	if (bcm47xx_bus_type ==	 BCM47XX_BUS_TYPE_SSB)
 		return bcm47xx_pcibios_plat_dev_init_ssb(dev);
-	else
 #endif
 #ifdef CONFIG_BCM47XX_BCMA
 	if  (bcm47xx_bus_type ==  BCM47XX_BUS_TYPE_BCMA)
 		return bcm47xx_pcibios_plat_dev_init_bcma(dev);
-	else
 #endif
-		return 0;
+	return 0;
 }
