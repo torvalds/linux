5197fcd09ab6dcc4df79edec7e8e27575276374c
Yanfei Xu <yanfei.xu@windriver.com>
2021-10-13 21:41:54 +0800

    locking/rwsem: Fix comments about reader optimistic lock stealing conditions
    After the commit 617f3ef95177 ("locking/rwsem: Remove reader
    optimistic spinning"), reader doesn't support optimistic spinning
    anymore, there is no need meet the condition which OSQ is empty.
    
    BTW, add an unlikely() for the max reader wakeup check in the loop.
    
    Signed-off-by: Yanfei Xu <yanfei.xu@windriver.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Waiman Long <longman@redhat.com>
    Link: https://lore.kernel.org/r/20211013134154.1085649-4-yanfei.xu@windriver.com

--- kernel/locking/rwsem.c
+++ kernel/locking/rwsem.c
@@ -56,7 +56,6 @@
  *
  * A fast path reader optimistic lock stealing is supported when the rwsem
  * is previously owned by a writer and the following conditions are met:
- *  - OSQ is empty
  *  - rwsem is not currently writer owned
  *  - the handoff isn't set.
  */
@@ -485,7 +484,7 @@ static void rwsem_mark_wake(struct rw_semaphore *sem,
 		/*
 		 * Limit # of readers that can be woken up per wakeup call.
 		 */
-		if (woken >= MAX_READERS_WAKEUP)
+		if (unlikely(woken >= MAX_READERS_WAKEUP))
 			break;
 	}
 
