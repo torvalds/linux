94a14cfd3b6e639c3fb90994ea24e8513f1b0cce
Darrick J. Wong <djwong@kernel.org>
2021-10-13 15:11:02 -0700

    xfs: fix incorrect decoding in xchk_btree_cur_fsbno
    During review of subsequent patches, Dave and I noticed that this
    function doesn't work quite right -- accessing cur->bc_ino depends on
    the ROOT_IN_INODE flag, not LONG_PTRS.  Fix that and the parentheses
    isssue.  While we're at it, remove the piece that accesses cur->bc_ag,
    because block 0 of an AG is never part of a btree.
    
    Note: This changes the btree scrubber tracepoints behavior -- if the
    cursor has no buffer for a certain level, it will always report
    NULLFSBLOCK.  It is assumed that anyone tracing the online fsck code
    will also be tracing xchk_start/xchk_done or otherwise be aware of what
    exactly is being scrubbed.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Dave Chinner <dchinner@redhat.com>

--- fs/xfs/scrub/trace.c
+++ fs/xfs/scrub/trace.c
@@ -24,10 +24,11 @@ xchk_btree_cur_fsbno(
 	if (level < cur->bc_nlevels && cur->bc_bufs[level])
 		return XFS_DADDR_TO_FSB(cur->bc_mp,
 				xfs_buf_daddr(cur->bc_bufs[level]));
-	if (level == cur->bc_nlevels - 1 && cur->bc_flags & XFS_BTREE_LONG_PTRS)
+
+	if (level == cur->bc_nlevels - 1 &&
+	    (cur->bc_flags & XFS_BTREE_ROOT_IN_INODE))
 		return XFS_INO_TO_FSB(cur->bc_mp, cur->bc_ino.ip->i_ino);
-	if (!(cur->bc_flags & XFS_BTREE_LONG_PTRS))
-		return XFS_AGB_TO_FSB(cur->bc_mp, cur->bc_ag.pag->pag_agno, 0);
+
 	return NULLFSBLOCK;
 }
 
