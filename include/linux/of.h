a3c85b2ee098c4a293148fab4eb96299e92b9524
Nathan Lynch <nathanl@linux.ibm.com>
2021-10-14 12:30:55 -0500

    of: make of_node_check_flag() device_node parameter const
    The device_node argument isn't modified by of_node_check_flag(), so mark it
    const.
    
    Signed-off-by: Nathan Lynch <nathanl@linux.ibm.com>
    Link: https://lore.kernel.org/r/20211014173055.2117872-1-nathanl@linux.ibm.com
    Signed-off-by: Rob Herring <robh@kernel.org>

--- include/linux/of.h
+++ include/linux/of.h
@@ -185,7 +185,7 @@ static inline bool of_node_is_root(const struct device_node *node)
 	return node && (node->parent == NULL);
 }
 
-static inline int of_node_check_flag(struct device_node *n, unsigned long flag)
+static inline int of_node_check_flag(const struct device_node *n, unsigned long flag)
 {
 	return test_bit(flag, &n->_flags);
 }
