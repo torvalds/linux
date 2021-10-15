7db53c21b1c3c25676d1125049bc92c756421cd6
Amadeusz Sławiński <amadeuszx.slawinski@linux.intel.com>
2021-10-15 18:12:52 +0200

    ASoC: core: Remove invalid snd_soc_component_set_jack call
    If snd_soc_component_set_jack() is called after
    snd_soc_component_remove() it may operate on memory which is freed in
    ->remove handler.
    
    Signed-off-by: Amadeusz Sławiński <amadeuszx.slawinski@linux.intel.com>
    Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>
    Link: https://lore.kernel.org/r/20211015161257.27052-2-cezary.rojewski@intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1342,9 +1342,6 @@ static void soc_remove_component(struct snd_soc_component *component,
 	if (probed)
 		snd_soc_component_remove(component);
 
-	/* For framework level robustness */
-	snd_soc_component_set_jack(component, NULL, NULL);
-
 	list_del_init(&component->card_list);
 	snd_soc_dapm_free(snd_soc_component_get_dapm(component));
 	soc_cleanup_component_debugfs(component);
