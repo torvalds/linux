3afe252aa94d54feb3dab2a6202e53fd653b81a0
Michael Grzeschik <m.grzeschik@pengutronix.de>
2021-10-17 23:50:17 +0200

    UPSTREAM: usb: gadget: uvc: ensure the vdev is unset
    Since the uvc video device is created on demand, we have to ensure
    that the struct is always zeroed. Otherwise the previous settings
    might collide with the new values.
    
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>
    Signed-off-by: Michael Grzeschik <m.grzeschik@pengutronix.de>
    Link: https://lore.kernel.org/r/20211017215017.18392-7-m.grzeschik@pengutronix.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: William Wu <william.wu@rock-chips.com>
    (cherry picked from commit e4ce9ed835bcaf4cd3230a53a79645986c25ce0f)
    Change-Id: I4ecb8c85a4d838254b68ea23284e35b04b9678af

--- drivers/usb/gadget/function/f_uvc.c
+++ drivers/usb/gadget/function/f_uvc.c
@@ -586,6 +586,7 @@ uvc_register_video(struct uvc_device *uvc)
 	int ret;
 
 	/* TODO reference counting. */
+	memset(&uvc->vdev, 0, sizeof(uvc->video));
 	uvc->vdev.v4l2_dev = &uvc->v4l2_dev;
 	uvc->vdev.fops = &uvc_v4l2_fops;
 	uvc->vdev.ioctl_ops = &uvc_v4l2_ioctl_ops;
