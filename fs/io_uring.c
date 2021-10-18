f59da9f7efa73a31b6287bd9b8f03a8d536e524f
Kamal Mostafa <kamal@canonical.com>
2021-10-18 10:18:08 -0700

    io_uring: fix splice_fd_in checks backport typo
    The linux-5.10.y backport of commit "io_uring: add ->splice_fd_in checks"
    includes a typo: "|" where "||" should be. (The original upstream commit
    is fine.)
    
    Fixes: 54eb6211b979 ("io_uring: add ->splice_fd_in checks")
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: stable@vger.kernel.org # v5.10
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -5559,7 +5559,7 @@ static int io_timeout_remove_prep(struct io_kiocb *req,
 		return -EINVAL;
 	if (unlikely(req->flags & (REQ_F_FIXED_FILE | REQ_F_BUFFER_SELECT)))
 		return -EINVAL;
-	if (sqe->ioprio || sqe->buf_index || sqe->len || sqe->timeout_flags |
+	if (sqe->ioprio || sqe->buf_index || sqe->len || sqe->timeout_flags ||
 	    sqe->splice_fd_in)
 		return -EINVAL;
 
