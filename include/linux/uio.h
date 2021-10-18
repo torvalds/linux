d9c19d32d86fa54934b632c4314beb067bf98378
Matthew Wilcox (Oracle) <willy@infradead.org>
2021-10-18 10:39:06 -0400

    iov_iter: Add copy_folio_to_iter()
    This wrapper around copy_page_to_iter() works because copy_page_to_iter()
    handles compound pages correctly.
    
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- include/linux/uio.h
+++ include/linux/uio.h
@@ -7,6 +7,7 @@
 
 #include <linux/kernel.h>
 #include <linux/thread_info.h>
+#include <linux/mm_types.h>
 #include <uapi/linux/uio.h>
 
 struct page;
@@ -145,6 +146,12 @@ size_t copy_page_from_iter(struct page *page, size_t offset, size_t bytes,
 size_t _copy_to_iter(const void *addr, size_t bytes, struct iov_iter *i);
 size_t _copy_from_iter(void *addr, size_t bytes, struct iov_iter *i);
 size_t _copy_from_iter_nocache(void *addr, size_t bytes, struct iov_iter *i);
+
+static inline size_t copy_folio_to_iter(struct folio *folio, size_t offset,
+		size_t bytes, struct iov_iter *i)
+{
+	return copy_page_to_iter(&folio->page, offset, bytes, i);
+}
 
 static __always_inline __must_check
 size_t copy_to_iter(const void *addr, size_t bytes, struct iov_iter *i)
