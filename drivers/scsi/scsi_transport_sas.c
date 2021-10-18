3d8fa78ebd61aada5d55de6e04beee7fd916c62b
Sreekanth Reddy <sreekanth.reddy@broadcom.com>
2021-10-18 12:36:11 +0530

    scsi: scsi_transport_sas: Add 22.5 Gbps link rate definitions
    Add 22.5 Gbps link rate definitions.
    
    Link: https://lore.kernel.org/r/20211018070611.26428-1-sreekanth.reddy@broadcom.com
    Reviewed-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Sreekanth Reddy <sreekanth.reddy@broadcom.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/scsi_transport_sas.c
+++ drivers/scsi/scsi_transport_sas.c
@@ -154,6 +154,7 @@ static struct {
 	{ SAS_LINK_RATE_3_0_GBPS,	"3.0 Gbit" },
 	{ SAS_LINK_RATE_6_0_GBPS,	"6.0 Gbit" },
 	{ SAS_LINK_RATE_12_0_GBPS,	"12.0 Gbit" },
+	{ SAS_LINK_RATE_22_5_GBPS,	"22.5 Gbit" },
 };
 sas_bitfield_name_search(linkspeed, sas_linkspeed_names)
 sas_bitfield_name_set(linkspeed, sas_linkspeed_names)
