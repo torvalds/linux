a512242e66cea3de1020d83ca9f2977c0a75530b
Suzuki K Poulose <suzuki.poulose@arm.com>
2021-10-19 17:31:41 +0100

    BACKPORT: arm64: errata: Add workaround for TSB flush failures
    Arm Neoverse-N2 (#2067961) and Cortex-A710 (#2054223) suffers
    from errata, where a TSB (trace synchronization barrier)
    fails to flush the trace data completely, when executed from
    a trace prohibited region. In Linux we always execute it
    after we have moved the PE to trace prohibited region. So,
    we can apply the workaround every time a TSB is executed.
    
    The work around is to issue two TSB consecutively.
    
    NOTE: This errata is defined as LOCAL_CPU_ERRATUM, implying
    that a late CPU could be blocked from booting if it is the
    first CPU that requires the workaround. This is because we
    do not allow setting a cpu_hwcaps after the SMP boot. The
    other alternative is to use "this_cpu_has_cap()" instead
    of the faster system wide check, which may be a bit of an
    overhead, given we may have to do this in nvhe KVM host
    before a guest entry.
    
    Bug: 213931796
    Cc: Will Deacon <will@kernel.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Mathieu Poirier <mathieu.poirier@linaro.org>
    Cc: Mike Leach <mike.leach@linaro.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Anshuman Khandual <anshuman.khandual@arm.com>
    Cc: Marc Zyngier <maz@kernel.org>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Link: https://lore.kernel.org/r/20211019163153.3692640-4-suzuki.poulose@arm.com
    Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit fa82d0b4b833790ac4572377fb777dcea24a9d69)
    [Fix conflict due to another workaround that is not backported
    (TRBE_OVERWRITE). Also manually update cpucaps.h which is autogenerated
    in upstream from arch/arm64/tools/cpucaps which we ignored as part of
    the conflict resolution]
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Change-Id: I6b40eaa0ac14552c867860133a04619cb556bc31

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -69,6 +69,7 @@
 #define ARM64_WORKAROUND_1508412		58
 #define ARM64_HAS_LDAPR				59
 #define ARM64_KVM_PROTECTED_MODE		60
+#define ARM64_WORKAROUND_TSB_FLUSH_FAILURE	61
 
 /* kabi: reserve 62 - 76 for future cpu capabilities */
 #define ARM64_NCAPS				76
