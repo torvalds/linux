92ab2aeca5aee77423736ffef6b3c5022b206adc
Greg Kroah-Hartman <gregkh@google.com>
2021-10-20 16:06:41 +0200

    Revert "ANDROID: GKI: net: add vendor hooks for 'struct sock' lifecycle"
    This reverts commit 0ed7424fa0cc7253718f29972af0a7362bc4a961.
    
    The hooks android_rvh_sk_alloc and android_rvh_sk_free are not used by
    any vendor, so remove them to help with merge issues with future LTS
    releases.
    
    If this is needed by any real user, it can easily be reverted to add it
    back and then the symbol should be added to the abi list at the same
    time to prevent it from being removed again later.
    
    Bug: 203756332
    Bug: 171013716
    Cc: Vignesh Saravanaperumal <vignesh1.s@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I5fa860e75a0640bc02d1dffeb556497badd6f3bf

--- include/trace/hooks/net.h
+++ include/trace/hooks/net.h
@@ -19,15 +19,11 @@ DECLARE_HOOK(android_vh_kfree_skb,
 	TP_PROTO(struct sk_buff *skb), TP_ARGS(skb));
 
 struct nf_conn;
-struct sock;
+struct sock;	/* needed for CRC preservation */
 DECLARE_RESTRICTED_HOOK(android_rvh_nf_conn_alloc,
 	TP_PROTO(struct nf_conn *nf_conn), TP_ARGS(nf_conn), 1);
 DECLARE_RESTRICTED_HOOK(android_rvh_nf_conn_free,
 	TP_PROTO(struct nf_conn *nf_conn), TP_ARGS(nf_conn), 1);
-DECLARE_RESTRICTED_HOOK(android_rvh_sk_alloc,
-	TP_PROTO(struct sock *sock), TP_ARGS(sock), 1);
-DECLARE_RESTRICTED_HOOK(android_rvh_sk_free,
-	TP_PROTO(struct sock *sock), TP_ARGS(sock), 1);
 
 /* macro versions of hooks are no longer required */
 
