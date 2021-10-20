fe67da49f784f031a6ee5ccb4ca1bc4d33f925b6
Eric W. Biederman <ebiederm@xmission.com>
2021-10-20 12:44:00 -0500

    exit/syscall_user_dispatch: Send ordinary signals on failure
    commit 941edc5bf174b67f94db19817cbeab0a93e0c32a upstream.
    
    Use force_fatal_sig instead of calling do_exit directly.  This ensures
    the ordinary signal handling path gets invoked, core dumps as
    appropriate get created, and for multi-threaded processes all of the
    threads are terminated not just a single thread.
    
    When asked Gabriel Krisman Bertazi <krisman@collabora.com> said [1]:
    > ebiederm@xmission.com (Eric W. Biederman) asked:
    >
    > > Why does do_syscal_user_dispatch call do_exit(SIGSEGV) and
    > > do_exit(SIGSYS) instead of force_sig(SIGSEGV) and force_sig(SIGSYS)?
    > >
    > > Looking at the code these cases are not expected to happen, so I would
    > > be surprised if userspace depends on any particular behaviour on the
    > > failure path so I think we can change this.
    >
    > Hi Eric,
    >
    > There is not really a good reason, and the use case that originated the
    > feature doesn't rely on it.
    >
    > Unless I'm missing yet another problem and others correct me, I think
    > it makes sense to change it as you described.
    >
    > > Is using do_exit in this way something you copied from seccomp?
    >
    > I'm not sure, its been a while, but I think it might be just that.  The
    > first prototype of SUD was implemented as a seccomp mode.
    
    If at some point it becomes interesting we could relax
    "force_fatal_sig(SIGSEGV)" to instead say
    "force_sig_fault(SIGSEGV, SEGV_MAPERR, sd->selector)".
    
    I avoid doing that in this patch to avoid making it possible
    to catch currently uncatchable signals.
    
    Cc: Gabriel Krisman Bertazi <krisman@collabora.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Andy Lutomirski <luto@kernel.org>
    [1] https://lkml.kernel.org/r/87mtr6gdvi.fsf@collabora.com
    Link: https://lkml.kernel.org/r/20211020174406.17889-14-ebiederm@xmission.com
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Cc: Thomas Backlund <tmb@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/entry/syscall_user_dispatch.c
+++ kernel/entry/syscall_user_dispatch.c
@@ -47,14 +47,18 @@ bool syscall_user_dispatch(struct pt_regs *regs)
 		 * access_ok() is performed once, at prctl time, when
 		 * the selector is loaded by userspace.
 		 */
-		if (unlikely(__get_user(state, sd->selector)))
-			do_exit(SIGSEGV);
+		if (unlikely(__get_user(state, sd->selector))) {
+			force_fatal_sig(SIGSEGV);
+			return true;
+		}
 
 		if (likely(state == SYSCALL_DISPATCH_FILTER_ALLOW))
 			return false;
 
-		if (state != SYSCALL_DISPATCH_FILTER_BLOCK)
-			do_exit(SIGSYS);
+		if (state != SYSCALL_DISPATCH_FILTER_BLOCK) {
+			force_fatal_sig(SIGSYS);
+			return true;
+		}
 	}
 
 	sd->on_dispatch = true;
