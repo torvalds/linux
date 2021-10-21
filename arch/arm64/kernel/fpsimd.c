31aa126de88e1e9f562f708a9b0ec5917ba97dce
Marc Zyngier <maz@kernel.org>
2021-10-21 15:33:22 +0100

    arm64/fpsimd: Document the use of TIF_FOREIGN_FPSTATE by KVM
    The bit of documentation that talks about TIF_FOREIGN_FPSTATE
    does not mention the ungodly tricks that KVM plays with this flag.
    
    Try and document this for the posterity.
    
    Reviewed-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kernel/fpsimd.c
+++ arch/arm64/kernel/fpsimd.c
@@ -78,7 +78,11 @@
  * indicate whether or not the userland FPSIMD state of the current task is
  * present in the registers. The flag is set unless the FPSIMD registers of this
  * CPU currently contain the most recent userland FPSIMD state of the current
- * task.
+ * task. If the task is behaving as a VMM, then this is will be managed by
+ * KVM which will clear it to indicate that the vcpu FPSIMD state is currently
+ * loaded on the CPU, allowing the state to be saved if a FPSIMD-aware
+ * softirq kicks in. Upon vcpu_put(), KVM will save the vcpu FP state and
+ * flag the register state as invalid.
  *
  * In order to allow softirq handlers to use FPSIMD, kernel_neon_begin() may
  * save the task's FPSIMD context back to task_struct from softirq context.
