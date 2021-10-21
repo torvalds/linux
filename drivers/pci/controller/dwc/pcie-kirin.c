e4c72797fd1609c630ead5bd86d5df16bb0ed5e9
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2021-10-21 11:45:20 +0100

    PCI: kirin: Allow removing the driver
    Now that everything is in place at the poweroff sequence, this driver can
    use module_platform_driver(), which allows it to be removed.
    
    Link: https://lore.kernel.org/r/53b40494252444a9b830827922c4e3a301b8f863.1634812676.git.mchehab+huawei@kernel.org
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Xiaowei Song <songxiaowei@hisilicon.com>

--- drivers/pci/controller/dwc/pcie-kirin.c
+++ drivers/pci/controller/dwc/pcie-kirin.c
@@ -828,7 +828,7 @@ static struct platform_driver kirin_pcie_driver = {
 		.suppress_bind_attrs	= true,
 	},
 };
-builtin_platform_driver(kirin_pcie_driver);
+module_platform_driver(kirin_pcie_driver);
 
 MODULE_DEVICE_TABLE(of, kirin_pcie_match);
 MODULE_DESCRIPTION("PCIe host controller driver for Kirin Phone SoCs");
