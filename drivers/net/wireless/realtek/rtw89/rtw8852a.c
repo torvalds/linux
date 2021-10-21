dea857700a75943017488b1d47f6687cfc037642
Lv Ruyi <lv.ruyi@zte.com.cn>
2021-10-21 04:20:35 +0000

    rtw89: fix error function parameter
    This patch fixes the following Coccinelle warning:
    drivers/net/wireless/realtek/rtw89/rtw8852a.c:753:
    WARNING  possible condition with no effect (if == else)
    
    Reported-by: Zeal Robot <zealci@zte.com.cn>
    Signed-off-by: Lv Ruyi <lv.ruyi@zte.com.cn>
    Acked-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20211021042035.1042463-1-lv.ruyi@zte.com.cn

--- drivers/net/wireless/realtek/rtw89/rtw8852a.c
+++ drivers/net/wireless/realtek/rtw89/rtw8852a.c
@@ -757,7 +757,7 @@ static void rtw8852a_ctrl_ch(struct rtw89_dev *rtwdev, u8 central_ch,
 		else
 			rtw89_phy_write32_idx(rtwdev, R_P1_MODE,
 					      B_P1_MODE_SEL,
-					      1, phy_idx);
+					      0, phy_idx);
 		/* SCO compensate FC setting */
 		sco_comp = rtw8852a_sco_mapping(central_ch);
 		rtw89_phy_write32_idx(rtwdev, R_FC0_BW, B_FC0_BW_INV,
