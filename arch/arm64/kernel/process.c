a32d8ee384c9a70cf34ff14033ccacc142db54e4
Greg Kroah-Hartman <gregkh@google.com>
2021-10-21 09:37:05 +0200

    Revert "ANDROID: mpam: add vendor hook to record MPAM"
    This reverts commit 1aa1f6a7cf67c9cea1a609581eb9c2602fd96576.
    
    The hook android_vh_mpam_set is not used by any vendor, so remove
    it to help with merge issues with future LTS releases.
    
    If this is needed by any real user, it can easily be reverted to add it
    back and then the symbol should be added to the abi list at the same
    time to prevent it from being removed again later.
    
    Bug: 203756332
    Bug: 165333282
    Cc: C-J.Chen <C-J.Chen@mediatek.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I18356743538df7d41a00f54479bf2a0bc3a62e06

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -45,7 +45,6 @@
 #include <linux/thread_info.h>
 #include <linux/prctl.h>
 #include <trace/hooks/fpsimd.h>
-#include <trace/hooks/mpam.h>
 
 #include <asm/alternative.h>
 #include <asm/arch_gicv3.h>
@@ -572,11 +571,6 @@ __notrace_funcgraph struct task_struct *__switch_to(struct task_struct *prev,
 	ssbs_thread_switch(next);
 	erratum_1418040_thread_switch(prev, next);
 	ptrauth_thread_switch_user(next);
-	/*
-	 *  vendor hook is needed before the dsb(),
-	 *  because MPAM is related to cache maintenance.
-	 */
-	trace_android_vh_mpam_set(prev, next);
 
 	/*
 	 * Complete any pending TLB or cache maintenance on this CPU in case
--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -17,7 +17,6 @@
 #include <trace/hooks/dtask.h>
 #include <trace/hooks/cpuidle.h>
 #include <trace/hooks/topology.h>
-#include <trace/hooks/mpam.h>
 #include <trace/hooks/gic.h>
 #include <trace/hooks/wqlockup.h>
 #include <trace/hooks/debug.h>
@@ -123,7 +122,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sched_show_task);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shmem_alloc_page);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpu_idle_enter);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpu_idle_exit);
-EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mpam_set);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_find_busiest_group);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_gic_resume);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_wq_lockup_pool);
