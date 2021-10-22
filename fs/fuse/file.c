484ce65715b06aead8c4901f01ca32c5a240bc71
Miklos Szeredi <mszeredi@redhat.com>
2021-10-22 17:03:03 +0200

    fuse: fix attr version comparison in fuse_read_update_size()
    A READ request returning a short count is taken as indication of EOF, and
    the cached file size is modified accordingly.
    
    Fix the attribute version checking to allow for changes to fc->attr_version
    on other inodes.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -787,7 +787,7 @@ static void fuse_read_update_size(struct inode *inode, loff_t size,
 	struct fuse_inode *fi = get_fuse_inode(inode);
 
 	spin_lock(&fi->lock);
-	if (attr_ver == fi->attr_version && size < inode->i_size &&
+	if (attr_ver >= fi->attr_version && size < inode->i_size &&
 	    !test_bit(FUSE_I_SIZE_UNSTABLE, &fi->state)) {
 		fi->attr_version = atomic64_inc_return(&fc->attr_version);
 		i_size_write(inode, size);
