1825ecc908d4e77735a993a72406e04e9dfbe5eb
Lars-Peter Clausen <lars@metafoo.de>
2021-10-25 09:54:28 +0200

    dmaengine: zynqmp_dma: Correctly handle descriptor callbacks
    DMA clients can provide one of two types of callbacks. For this reason
    dmaengine drivers should not directly invoke `callback`, but always use
    `dmaengine_desc_callback_invoke()`. This makes sure that both types of
    callbacks are handled correctly.
    
    The zynqmp_dma driver currently doesn't do this and only handles the
    `callback` type callback. If the client used the `callback_result` type
    callback it will not be called.
    
    Fix this by switching to `dmaengine_desc_callback_valid()` and
    `dmaengine_desc_callback_invoke()`.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Link: https://lore.kernel.org/r/20211025075428.2094-3-lars@metafoo.de
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/xilinx/zynqmp_dma.c
+++ drivers/dma/xilinx/zynqmp_dma.c
@@ -605,14 +605,12 @@ static void zynqmp_dma_chan_desc_cleanup(struct zynqmp_dma_chan *chan)
 	spin_lock_irqsave(&chan->lock, irqflags);
 
 	list_for_each_entry_safe(desc, next, &chan->done_list, node) {
-		dma_async_tx_callback callback;
-		void *callback_param;
+		struct dmaengine_desc_callback cb;
 
-		callback = desc->async_tx.callback;
-		callback_param = desc->async_tx.callback_param;
-		if (callback) {
+		dmaengine_desc_get_callback(&desc->async_tx, &cb);
+		if (dmaengine_desc_callback_valid(&cb)) {
 			spin_unlock_irqrestore(&chan->lock, irqflags);
-			callback(callback_param);
+			dmaengine_desc_callback_invoke(&cb, NULL);
 			spin_lock_irqsave(&chan->lock, irqflags);
 		}
 
