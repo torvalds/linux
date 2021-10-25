6f1637795f2827d36aec9e0246487f5852e8abf7
Ming Lei <ming.lei@redhat.com>
2021-10-25 10:54:23 +0800

    zram: fix race between zram_reset_device() and disksize_store()
    When the ->init_lock is released in zram_reset_device(), disksize_store()
    can come in and try to allocate meta, but zram_reset_device() is freeing
    free meta, so cause races.
    
    Link: https://lore.kernel.org/linux-block/20210927163805.808907-1-mcgrof@kernel.org/T/#mc617f865a3fa2778e40f317ddf48f6447c20c073
    Reported-by: Luis Chamberlain <mcgrof@kernel.org>
    Reviewed-by: Luis Chamberlain <mcgrof@kernel.org>
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Link: https://lore.kernel.org/r/20211025025426.2815424-2-ming.lei@redhat.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1704,12 +1704,13 @@ static void zram_reset_device(struct zram *zram)
 	set_capacity_and_notify(zram->disk, 0);
 	part_stat_set_all(zram->disk->part0, 0);
 
-	up_write(&zram->init_lock);
 	/* I/O operation under all of CPU are done so let's free */
 	zram_meta_free(zram, disksize);
 	memset(&zram->stats, 0, sizeof(zram->stats));
 	zcomp_destroy(comp);
 	reset_bdev(zram);
+
+	up_write(&zram->init_lock);
 }
 
 static ssize_t disksize_store(struct device *dev,
