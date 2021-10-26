292e6077b04091d138bae6010fb9fdc958170d64
Paolo Abeni <pabeni@redhat.com>
2021-10-26 16:29:14 -0700

    net: introduce sk_forward_alloc_get()
    A later patch will change the MPTCP memory accounting schema
    in such a way that MPTCP sockets will encode the total amount of
    forward allocated memory in two separate fields (one for tx and
    one for rx).
    
    MPTCP sockets will use their own helper to provide the accurate
    amount of fwd allocated memory.
    
    To allow the above, this patch adds a new, optional, sk method to
    fetch the fwd memory, wrap the call in a new helper and use it
    where it is appropriate.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1210,6 +1210,8 @@ struct proto {
 	unsigned int		inuse_idx;
 #endif
 
+	int			(*forward_alloc_get)(const struct sock *sk);
+
 	bool			(*stream_memory_free)(const struct sock *sk, int wake);
 	bool			(*stream_memory_read)(const struct sock *sk);
 	/* Memory pressure */
@@ -1217,6 +1219,7 @@ struct proto {
 	void			(*leave_memory_pressure)(struct sock *sk);
 	atomic_long_t		*memory_allocated;	/* Current allocated memory. */
 	struct percpu_counter	*sockets_allocated;	/* Current number of sockets. */
+
 	/*
 	 * Pressure flag: try to collapse.
 	 * Technical note: it is used by multiple contexts non atomically.
@@ -1293,6 +1296,14 @@ static inline void sk_refcnt_debug_release(const struct sock *sk)
 #endif /* SOCK_REFCNT_DEBUG */
 
 INDIRECT_CALLABLE_DECLARE(bool tcp_stream_memory_free(const struct sock *sk, int wake));
+
+static inline int sk_forward_alloc_get(const struct sock *sk)
+{
+	if (!sk->sk_prot->forward_alloc_get)
+		return sk->sk_forward_alloc;
+
+	return sk->sk_prot->forward_alloc_get(sk);
+}
 
 static inline bool __sk_stream_memory_free(const struct sock *sk, int wake)
 {
