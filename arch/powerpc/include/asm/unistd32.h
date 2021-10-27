b7472e1764bfc0fe3d6578cb281e81c812ca5886
Michael Ellerman <mpe@ellerman.id.au>
2021-10-27 22:29:31 +1100

    Revert "powerpc/audit: Convert powerpc to AUDIT_ARCH_COMPAT_GENERIC"
    This reverts commit 566af8cda399c088763d07464463dc871c943b54.
    
    This caused some conflicts vs the audit tree, and the audit maintainers
    would prefer we postpone this to the next merge window so we have more
    time for testing.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/unistd32.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-#ifndef _ASM_POWERPC_UNISTD32_H_
-#define _ASM_POWERPC_UNISTD32_H_
-
-#include <asm/unistd_32.h>
-
-#endif /* _ASM_POWERPC_UNISTD32_H_ */
