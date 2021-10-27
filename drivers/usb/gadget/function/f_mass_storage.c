9fff139aeb11186fd8e75860c959c86cb43ab2f6
Wesley Cheng <quic_wcheng@quicinc.com>
2021-10-27 13:10:07 -0700

    usb: gadget: f_mass_storage: Disable eps during disconnect
    When receiving a disconnect event from the UDC, the mass storage
    function driver currently runs the handle_exception() routine
    asynchronously.  For UDCs that support runtime PM, there is a
    possibility the UDC is already suspended by the time the
    do_set_interface() is executed.  This can lead to HW register access
    while the UDC is already suspended.
    
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Wesley Cheng <quic_wcheng@quicinc.com>
    Link: https://lore.kernel.org/r/1635365407-31337-3-git-send-email-quic_wcheng@quicinc.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/gadget/function/f_mass_storage.c
+++ drivers/usb/gadget/function/f_mass_storage.c
@@ -2342,6 +2342,16 @@ static void fsg_disable(struct usb_function *f)
 {
 	struct fsg_dev *fsg = fsg_from_func(f);
 
+	/* Disable the endpoints */
+	if (fsg->bulk_in_enabled) {
+		usb_ep_disable(fsg->bulk_in);
+		fsg->bulk_in_enabled = 0;
+	}
+	if (fsg->bulk_out_enabled) {
+		usb_ep_disable(fsg->bulk_out);
+		fsg->bulk_out_enabled = 0;
+	}
+
 	__raise_exception(fsg->common, FSG_STATE_CONFIG_CHANGE, NULL);
 }
 
