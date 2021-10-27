cc45b96e2de7ada26520f101dada0abafa4ba997
Subbaraya Sundeep <sbhatta@marvell.com>
2021-10-27 23:02:32 +0530

    octeontx2-af: Check whether ipolicers exists
    While displaying ingress policers information in
    debugfs check whether ingress policers exist in
    the hardware or not because some platforms(CN9XXX)
    do not have this feature.
    
    Fixes: e7d8971763f3 ("octeontx2-af: cn10k: Debugfs support for bandwidth")
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Signed-off-by: Rakesh Babu <rsaladi2@marvell.com>
    Signed-off-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_debugfs.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_debugfs.c
@@ -1719,6 +1719,10 @@ static int rvu_dbg_nix_band_prof_ctx_display(struct seq_file *m, void *unused)
 	u16 pcifunc;
 	char *str;
 
+	/* Ingress policers do not exist on all platforms */
+	if (!nix_hw->ipolicer)
+		return 0;
+
 	for (layer = 0; layer < BAND_PROF_NUM_LAYERS; layer++) {
 		if (layer == BAND_PROF_INVAL_LAYER)
 			continue;
@@ -1767,6 +1771,10 @@ static int rvu_dbg_nix_band_prof_rsrc_display(struct seq_file *m, void *unused)
 	struct nix_ipolicer *ipolicer;
 	int layer;
 	char *str;
+
+	/* Ingress policers do not exist on all platforms */
+	if (!nix_hw->ipolicer)
+		return 0;
 
 	seq_puts(m, "\nBandwidth profile resource free count\n");
 	seq_puts(m, "=====================================\n");
