e9f7832a4f141376559402fc4114a236a4cad416
Yinjun Zhang <yinjun.zhang@corigine.com>
2021-10-29 13:29:02 +0200

    nfp: fix NULL pointer access when scheduling dim work
    [ Upstream commit f8d384a640dd32aaf0a05fec137ccbf0e986b09f ]
    
    Each rx/tx ring has a related dim work, when rx/tx ring number is
    decreased by `ethtool -L`, the corresponding rx_ring or tx_ring is
    assigned NULL, while its related work is not destroyed. When scheduled,
    the work will access NULL pointer.
    
    Fixes: 9d32e4e7e9e1 ("nfp: add support for coalesce adaptive feature")
    Signed-off-by: Yinjun Zhang <yinjun.zhang@corigine.com>
    Signed-off-by: Louis Peens <louis.peens@corigine.com>
    Signed-off-by: Simon Horman <simon.horman@corigine.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/netronome/nfp/nfp_net_common.c
+++ drivers/net/ethernet/netronome/nfp/nfp_net_common.c
@@ -2067,7 +2067,7 @@ static int nfp_net_poll(struct napi_struct *napi, int budget)
 		if (napi_complete_done(napi, pkts_polled))
 			nfp_net_irq_unmask(r_vec->nfp_net, r_vec->irq_entry);
 
-	if (r_vec->nfp_net->rx_coalesce_adapt_on) {
+	if (r_vec->nfp_net->rx_coalesce_adapt_on && r_vec->rx_ring) {
 		struct dim_sample dim_sample = {};
 		unsigned int start;
 		u64 pkts, bytes;
@@ -2082,7 +2082,7 @@ static int nfp_net_poll(struct napi_struct *napi, int budget)
 		net_dim(&r_vec->rx_dim, dim_sample);
 	}
 
-	if (r_vec->nfp_net->tx_coalesce_adapt_on) {
+	if (r_vec->nfp_net->tx_coalesce_adapt_on && r_vec->tx_ring) {
 		struct dim_sample dim_sample = {};
 		unsigned int start;
 		u64 pkts, bytes;
