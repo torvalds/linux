d396e735ba0c91911aac5d696b5da090e38e919b
Tudor Ambarus <tudor.ambarus@microchip.com>
2021-10-29 20:26:19 +0300

    mtd: spi-nor: spansion: Use manufacturer late_init()
    spansion_post_sfdp_fixups() was called regardless if the flash defined
    SFDP tables or not. A better place for this kind of parameters init is
    in manufacturer's late_init() hook. post_sfdp() should be called only
    when SFDP is defined. No functional change in this patch.
    
    Instead of doing the 4b opcodes settings at manufacturer level, thus
    also for every flash that will be introduced, this should be done
    just where it is needed, per flash. I'll let this for other patch.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Reviewed-by: Michael Walle <michael@walle.cc>
    Reviewed-by: Pratyush Yadav <p.yadav@ti.com>
    Link: https://lore.kernel.org/r/20211029172633.886453-12-tudor.ambarus@microchip.com

--- drivers/mtd/spi-nor/spansion.c
+++ drivers/mtd/spi-nor/spansion.c
@@ -276,7 +276,7 @@ static const struct flash_info spansion_parts[] = {
 	},
 };
 
-static void spansion_post_sfdp_fixups(struct spi_nor *nor)
+static void spansion_late_init(struct spi_nor *nor)
 {
 	if (nor->params->size <= SZ_16M)
 		return;
@@ -288,7 +288,7 @@ static void spansion_post_sfdp_fixups(struct spi_nor *nor)
 }
 
 static const struct spi_nor_fixups spansion_fixups = {
-	.post_sfdp = spansion_post_sfdp_fixups,
+	.late_init = spansion_late_init,
 };
 
 const struct spi_nor_manufacturer spi_nor_spansion = {
