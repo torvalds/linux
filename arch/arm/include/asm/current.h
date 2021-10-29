c1e42efacb9bb4ae873e9b1cf249fa4fb6ef7f84
Ard Biesheuvel <ardb@kernel.org>
2021-10-29 17:49:37 +0100

    ARM: 9151/1: Thumb2: avoid __builtin_thread_pointer() on Clang
    If available, we use the __builtin_thread_pointer() helper to get the
    value of the TLS register, to help the compiler understand that it
    doesn't need to reload it every time we access 'current'.
    
    Unfortunately, Clang fails to emit the MRC system register read
    directly when building for Thumb2, and instead, it issues a call to the
    __aeabi_read_tp helper, which the kernel does not provide, and so this
    result in link failures at build time.
    
    So create a special case for this, and emit the MRC directly using an
    asm() block, just like we do when the helper is not available to begin
    with.
    
    Link: https://github.com/ClangBuiltLinux/linux/issues/1485
    
    Reviewed-by: Nick Desaulniers <ndesaulniers@google.com>
    Reviewed-by: Nathan Chancellor <nathan@kernel.org>
    Tested-by: Nathan Chancellor <nathan@kernel.org>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>

--- arch/arm/include/asm/current.h
+++ arch/arm/include/asm/current.h
@@ -26,12 +26,17 @@ static inline struct task_struct *get_current(void)
 {
 	struct task_struct *cur;
 
-#if __has_builtin(__builtin_thread_pointer)
+#if __has_builtin(__builtin_thread_pointer) && \
+    !(defined(CONFIG_THUMB2_KERNEL) && \
+      defined(CONFIG_CC_IS_CLANG) && CONFIG_CLANG_VERSION < 130001)
 	/*
 	 * Use the __builtin helper when available - this results in better
 	 * code, especially when using GCC in combination with the per-task
 	 * stack protector, as the compiler will recognize that it needs to
 	 * load the TLS register only once in every function.
+	 *
+	 * Clang < 13.0.1 gets this wrong for Thumb2 builds:
+	 * https://github.com/ClangBuiltLinux/linux/issues/1485
 	 */
 	cur = __builtin_thread_pointer();
 #else
