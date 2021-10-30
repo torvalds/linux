f99413e4e1ceb23976207a8ad33a4fb65055092f
Paul Cercueil <paul@crapouillou.net>
2021-10-30 11:00:32 +0100

    drm/ingenic: Remove bogus register write
    Commit 1bdb542da736 ("drm/ingenic: Simplify code by using hwdescs
    array") caused the dma_hwdesc_phys_f{0,1} variables to be used while
    uninitialized in a mmio register write, which most certainly broke the
    ingenic-drm driver.
    
    However, the very same patchset also submitted commit 6055466203df
    ("drm/ingenic: Upload palette before frame"), which restored a correct
    behaviour by doing the register writes in a different place in the code.
    
    What's left of this, is just to remove the bogus register writes in the
    probe function.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>
    Reported-by: kernel test robot <lkp@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211030100032.42066-1-paul@crapouillou.net
    Acked-by: Sam Ravnborg <sam@ravnborg.org>

--- drivers/gpu/drm/ingenic/ingenic-drm-drv.c
+++ drivers/gpu/drm/ingenic/ingenic-drm-drv.c
@@ -1013,7 +1013,6 @@ static int ingenic_drm_bind(struct device *dev, bool has_components)
 	void __iomem *base;
 	long parent_rate;
 	unsigned int i, clone_mask = 0;
-	dma_addr_t dma_hwdesc_phys_f0, dma_hwdesc_phys_f1;
 	int ret, irq;
 
 	soc_info = of_device_get_match_data(dev);
@@ -1267,10 +1266,6 @@ static int ingenic_drm_bind(struct device *dev, bool has_components)
 			goto err_pixclk_disable;
 		}
 	}
-
-	/* Set address of our DMA descriptor chain */
-	regmap_write(priv->map, JZ_REG_LCD_DA0, dma_hwdesc_phys_f0);
-	regmap_write(priv->map, JZ_REG_LCD_DA1, dma_hwdesc_phys_f1);
 
 	/* Enable OSD if available */
 	if (soc_info->has_osd)
