5bfa57795dc13b28375a5780914c8ae34dab44cc
Namjae Jeon <linkinjeon@kernel.org>
2021-10-31 09:53:50 +0900

    ksmbd: set unique value to volume serial field in FS_VOLUME_INFORMATION
    commit 5d2f0b1083eb158bdff01dd557e2c25046c0a7d2 upstream.
    
    Steve French reported ksmbd set fixed value to volume serial field in
    FS_VOLUME_INFORMATION. Volume serial value needs to be set to a unique
    value for client fscache. This patch set crc value that is generated
    with share name, path name and netbios name to volume serial.
    
    Fixes: e2f34481b24d ("cifsd: add server-side procedures for SMB3")
    Cc: stable@vger.kernel.org # v5.15
    Reported-by: Steve French <smfrench@gmail.com>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/ksmbd/server.c
+++ fs/ksmbd/server.c
@@ -632,5 +632,6 @@ MODULE_SOFTDEP("pre: sha512");
 MODULE_SOFTDEP("pre: aead2");
 MODULE_SOFTDEP("pre: ccm");
 MODULE_SOFTDEP("pre: gcm");
+MODULE_SOFTDEP("pre: crc32");
 module_init(ksmbd_server_init)
 module_exit(ksmbd_server_exit)
