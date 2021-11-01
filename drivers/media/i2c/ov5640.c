e738f5dd67eb8098d75345908a5e73782d0569a5
Mirela Rabulea <mirela.rabulea@nxp.com>
2021-11-01 21:52:51 +0200

    media: ov5640: Fix set format, v4l2_mbus_pixelcode not updated
    In ov5640_set_fmt, pending_fmt_change will always be false, because the
    sensor format is saved before comparing it with the previous format:
    	fmt = &sensor->fmt;...
    	*fmt = *mbus_fmt;...
    	if (mbus_fmt->code != sensor->fmt.code)
    		sensor->pending_fmt_change = true;
    This causes the sensor to capture with the previous pixelcode.
    
    Also, changes might happen even for V4L2_SUBDEV_FORMAT_TRY, so fix that.
    
    Basically, revert back to the state before
    commit 071154499193 ("media: ov5640: Fix set format regression")
    as it was more clear, and then update format even when pixelcode does
    not change, as resolution might change.
    
    Fixes: 071154499193 ("media: ov5640: Fix set format regression")
    Fixes: 6949d864776e ("media: ov5640: do not change mode if format or frame interval is unchanged")
    Fixes: fb98e29ff1ea5 ("media: ov5640: fix mode change regression")
    
    Signed-off-by: Mirela Rabulea <mirela.rabulea@nxp.com>
    Reviewed-by: Jacopo Mondi <jacopo+renesas@jmondi.org>
    Acked-by: Hugues Fruchet <hugues.fruchet@st.com>
    Tested-by: Hugues Fruchet <hugues.fruchet@st.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- drivers/media/i2c/ov5640.c
+++ drivers/media/i2c/ov5640.c
@@ -2293,7 +2293,6 @@ static int ov5640_set_fmt(struct v4l2_subdev *sd,
 	struct ov5640_dev *sensor = to_ov5640_dev(sd);
 	const struct ov5640_mode_info *new_mode;
 	struct v4l2_mbus_framefmt *mbus_fmt = &format->format;
-	struct v4l2_mbus_framefmt *fmt;
 	int ret;
 
 	if (format->pad != 0)
@@ -2311,12 +2310,10 @@ static int ov5640_set_fmt(struct v4l2_subdev *sd,
 	if (ret)
 		goto out;
 
-	if (format->which == V4L2_SUBDEV_FORMAT_TRY)
-		fmt = v4l2_subdev_get_try_format(sd, sd_state, 0);
-	else
-		fmt = &sensor->fmt;
-
-	*fmt = *mbus_fmt;
+	if (format->which == V4L2_SUBDEV_FORMAT_TRY) {
+		*v4l2_subdev_get_try_format(sd, sd_state, 0) = *mbus_fmt;
+		goto out;
+	}
 
 	if (new_mode != sensor->current_mode) {
 		sensor->current_mode = new_mode;
@@ -2324,6 +2321,9 @@ static int ov5640_set_fmt(struct v4l2_subdev *sd,
 	}
 	if (mbus_fmt->code != sensor->fmt.code)
 		sensor->pending_fmt_change = true;
+
+	/* update format even if code is unchanged, resolution might change */
+	sensor->fmt = *mbus_fmt;
 
 	__v4l2_ctrl_s_ctrl_int64(sensor->ctrls.pixel_rate,
 				 ov5640_calc_pixel_rate(sensor));
