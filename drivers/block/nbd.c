64742cf82dec2d72d260e6ffdfceaee739353d3f
Ye Bin <yebin10@huawei.com>
2021-11-02 09:52:37 +0800

    nbd: Fix hungtask when nbd_config_put
    [ Upstream commit e2daec488c57069a4a431d5b752f50294c4bf273 ]
    
    I got follow issue:
    [  247.381177] INFO: task kworker/u10:0:47 blocked for more than 120 seconds.
    [  247.382644]       Not tainted 4.19.90-dirty #140
    [  247.383502] "echo 0 > /proc/sys/kernel/hung_task_timeout_secs" disables this message.
    [  247.385027] Call Trace:
    [  247.388384]  schedule+0xb8/0x3c0
    [  247.388966]  schedule_timeout+0x2b4/0x380
    [  247.392815]  wait_for_completion+0x367/0x510
    [  247.397713]  flush_workqueue+0x32b/0x1340
    [  247.402700]  drain_workqueue+0xda/0x3c0
    [  247.403442]  destroy_workqueue+0x7b/0x690
    [  247.405014]  nbd_config_put.cold+0x2f9/0x5b6
    [  247.405823]  recv_work+0x1fd/0x2b0
    [  247.406485]  process_one_work+0x70b/0x1610
    [  247.407262]  worker_thread+0x5a9/0x1060
    [  247.408699]  kthread+0x35e/0x430
    [  247.410918]  ret_from_fork+0x1f/0x30
    
    We can reproduce issue as follows:
    1. Inject memory fault in nbd_start_device
    -1244,10 +1248,18 @@ static int nbd_start_device(struct nbd_device *nbd)
            nbd_dev_dbg_init(nbd);
            for (i = 0; i < num_connections; i++) {
                    struct recv_thread_args *args;
    -
    -               args = kzalloc(sizeof(*args), GFP_KERNEL);
    +
    +               if (i == 1) {
    +                       args = NULL;
    +                       printk("%s: inject malloc error\n", __func__);
    +               }
    +               else
    +                       args = kzalloc(sizeof(*args), GFP_KERNEL);
    2. Inject delay in recv_work
    -757,6 +760,8 @@ static void recv_work(struct work_struct *work)
    
                    blk_mq_complete_request(blk_mq_rq_from_pdu(cmd));
            }
    +       printk("%s: comm=%s pid=%d\n", __func__, current->comm, current->pid);
    +       mdelay(5 * 1000);
            nbd_config_put(nbd);
            atomic_dec(&config->recv_threads);
            wake_up(&config->recv_wq);
    3. Create nbd server
    nbd-server 8000 /tmp/disk
    4. Create nbd client
    nbd-client localhost 8000 /dev/nbd1
    Then will trigger above issue.
    
    Reason is when add delay in recv_work, lead to release the last reference
    of 'nbd->config_refs'. nbd_config_put will call flush_workqueue to make
    all work finish. Obviously, it will lead to deadloop.
    To solve this issue, according to Josef's suggestion move 'recv_work'
    init from start device to nbd_dev_add, then destroy 'recv_work'when
    nbd device teardown.
    
    Signed-off-by: Ye Bin <yebin10@huawei.com>
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Link: https://lore.kernel.org/r/20211102015237.2309763-5-yebin10@huawei.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/block/nbd.c
+++ drivers/block/nbd.c
@@ -254,7 +254,7 @@ static void nbd_dev_remove(struct nbd_device *nbd)
 	mutex_lock(&nbd_index_mutex);
 	idr_remove(&nbd_index_idr, nbd->index);
 	mutex_unlock(&nbd_index_mutex);
-
+	destroy_workqueue(nbd->recv_workq);
 	kfree(nbd);
 }
 
@@ -1260,10 +1260,6 @@ static void nbd_config_put(struct nbd_device *nbd)
 		kfree(nbd->config);
 		nbd->config = NULL;
 
-		if (nbd->recv_workq)
-			destroy_workqueue(nbd->recv_workq);
-		nbd->recv_workq = NULL;
-
 		nbd->tag_set.timeout = 0;
 		nbd->disk->queue->limits.discard_granularity = 0;
 		nbd->disk->queue->limits.discard_alignment = 0;
@@ -1290,14 +1286,6 @@ static int nbd_start_device(struct nbd_device *nbd)
 	    !(config->flags & NBD_FLAG_CAN_MULTI_CONN)) {
 		dev_err(disk_to_dev(nbd->disk), "server does not support multiple connections per device.\n");
 		return -EINVAL;
-	}
-
-	nbd->recv_workq = alloc_workqueue("knbd%d-recv",
-					  WQ_MEM_RECLAIM | WQ_HIGHPRI |
-					  WQ_UNBOUND, 0, nbd->index);
-	if (!nbd->recv_workq) {
-		dev_err(disk_to_dev(nbd->disk), "Could not allocate knbd recv work queue.\n");
-		return -ENOMEM;
 	}
 
 	blk_mq_update_nr_hw_queues(&nbd->tag_set, config->num_connections);
@@ -1725,6 +1713,15 @@ static struct nbd_device *nbd_dev_add(int index, unsigned int refs)
 	}
 	nbd->disk = disk;
 
+	nbd->recv_workq = alloc_workqueue("nbd%d-recv",
+					  WQ_MEM_RECLAIM | WQ_HIGHPRI |
+					  WQ_UNBOUND, 0, nbd->index);
+	if (!nbd->recv_workq) {
+		dev_err(disk_to_dev(nbd->disk), "Could not allocate knbd recv work queue.\n");
+		err = -ENOMEM;
+		goto out_err_disk;
+	}
+
 	/*
 	 * Tell the block layer that we are not a rotational device
 	 */
@@ -1755,7 +1752,7 @@ static struct nbd_device *nbd_dev_add(int index, unsigned int refs)
 	disk->first_minor = index << part_shift;
 	if (disk->first_minor < index || disk->first_minor > MINORMASK) {
 		err = -EINVAL;
-		goto out_err_disk;
+		goto out_free_work;
 	}
 
 	disk->minors = 1 << part_shift;
@@ -1764,7 +1761,7 @@ static struct nbd_device *nbd_dev_add(int index, unsigned int refs)
 	sprintf(disk->disk_name, "nbd%d", index);
 	err = add_disk(disk);
 	if (err)
-		goto out_err_disk;
+		goto out_free_work;
 
 	/*
 	 * Now publish the device.
@@ -1773,6 +1770,8 @@ static struct nbd_device *nbd_dev_add(int index, unsigned int refs)
 	nbd_total_devices++;
 	return nbd;
 
+out_free_work:
+	destroy_workqueue(nbd->recv_workq);
 out_err_disk:
 	blk_cleanup_disk(disk);
 out_free_idr:
@@ -2028,13 +2027,10 @@ static void nbd_disconnect_and_put(struct nbd_device *nbd)
 	nbd_disconnect(nbd);
 	sock_shutdown(nbd);
 	/*
-	 * Make sure recv thread has finished, so it does not drop the last
-	 * config ref and try to destroy the workqueue from inside the work
-	 * queue. And this also ensure that we can safely call nbd_clear_que()
+	 * Make sure recv thread has finished, we can safely call nbd_clear_que()
 	 * to cancel the inflight I/Os.
 	 */
-	if (nbd->recv_workq)
-		flush_workqueue(nbd->recv_workq);
+	flush_workqueue(nbd->recv_workq);
 	nbd_clear_que(nbd);
 	nbd->task_setup = NULL;
 	mutex_unlock(&nbd->config_lock);
