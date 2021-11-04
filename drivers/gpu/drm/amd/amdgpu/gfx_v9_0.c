e6ef9b396b6354b33373c62f0f47edf8702f12e5
Evan Quan <evan.quan@amd.com>
2021-11-04 11:11:26 +0800

    drm/amdgpu: correctly toggle gfx on/off around RLC_SPM_* register access
    As part of the ib padding process, accessing the RLC_SPM_* register may
    trigger gfx hang. Since gfxoff may be already kicked during the whole period.
    To address that, we manually toggle gfx on/off around the RLC_SPM_*
    register access.
    
    This can resolve the gfx hang issue observed on running Talos with RDP launched
    in parallel.
    
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Acked-by: Guchun Chen <guchun.chen@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gfx_v9_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v9_0.c
@@ -5104,6 +5104,8 @@ static void gfx_v9_0_update_spm_vmid(struct amdgpu_device *adev, unsigned vmid)
 {
 	u32 reg, data;
 
+	amdgpu_gfx_off_ctrl(adev, false);
+
 	reg = SOC15_REG_OFFSET(GC, 0, mmRLC_SPM_MC_CNTL);
 	if (amdgpu_sriov_is_pp_one_vf(adev))
 		data = RREG32_NO_KIQ(reg);
@@ -5117,6 +5119,8 @@ static void gfx_v9_0_update_spm_vmid(struct amdgpu_device *adev, unsigned vmid)
 		WREG32_SOC15_NO_KIQ(GC, 0, mmRLC_SPM_MC_CNTL, data);
 	else
 		WREG32_SOC15(GC, 0, mmRLC_SPM_MC_CNTL, data);
+
+	amdgpu_gfx_off_ctrl(adev, true);
 }
 
 static bool gfx_v9_0_check_rlcg_range(struct amdgpu_device *adev,
