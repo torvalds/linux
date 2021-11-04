64266f0a45c8323861d3756cbe3dc419e973917c
Huang, ChiaWen <ChiaWen.Huang@amd.com>
2021-11-04 16:52:03 -0400

    drm/amd/display: use link_rate_set above DPCD 1.3 (#1527)
    [Why & How]
    According to eDP spec, DPCD 1.3 is only for eDP DPCD v1.4
    In dpcd_set_link_settings function, the driver is just above v1.3
    
    Reviewed-by: Wenjing Liu <Wenjing.Liu@amd.com>
    Acked-by: Anson Jacob <Anson.Jacob@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: ChiawenHuang <chiawen.huang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_link_dp.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_link_dp.c
@@ -430,7 +430,7 @@ enum dc_status dpcd_set_link_settings(
 	status = core_link_write_dpcd(link, DP_LANE_COUNT_SET,
 		&lane_count_set.raw, 1);
 
-	if (link->dpcd_caps.dpcd_rev.raw >= DPCD_REV_14 &&
+	if (link->dpcd_caps.dpcd_rev.raw >= DPCD_REV_13 &&
 			lt_settings->link_settings.use_link_rate_set == true) {
 		rate = 0;
 		/* WA for some MUX chips that will power down with eDP and lose supported
