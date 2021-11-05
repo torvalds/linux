8446b59baaf45e83e1187cdb174ac78ac5d7d0ae
Eric Dumazet <edumazet@google.com>
2021-11-05 13:40:31 -0700

    mm/page_alloc.c: do not acquire zone lock in is_free_buddy_page()
    Grabbing zone lock in is_free_buddy_page() gives a wrong sense of
    safety, and has potential performance implications when zone is
    experiencing lock contention.
    
    In any case, if a caller needs a stable result, it should grab zone lock
    before calling this function.
    
    Link: https://lkml.kernel.org/r/20210922152833.4023972-1-eric.dumazet@gmail.com
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Hugh Dickins <hughd@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -9356,21 +9356,21 @@ void __offline_isolated_pages(unsigned long start_pfn, unsigned long end_pfn)
 }
 #endif
 
+/*
+ * This function returns a stable result only if called under zone lock.
+ */
 bool is_free_buddy_page(struct page *page)
 {
-	struct zone *zone = page_zone(page);
 	unsigned long pfn = page_to_pfn(page);
-	unsigned long flags;
 	unsigned int order;
 
-	spin_lock_irqsave(&zone->lock, flags);
 	for (order = 0; order < MAX_ORDER; order++) {
 		struct page *page_head = page - (pfn & ((1 << order) - 1));
 
-		if (PageBuddy(page_head) && buddy_order(page_head) >= order)
+		if (PageBuddy(page_head) &&
+		    buddy_order_unsafe(page_head) >= order)
 			break;
 	}
-	spin_unlock_irqrestore(&zone->lock, flags);
 
 	return order < MAX_ORDER;
 }
