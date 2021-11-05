21f45a2363bb0c96ca3e942d7207e7172514b5d2
hvanzyll <hvanzyll@amd.com>
2021-11-05 10:08:25 -0400

    drm/amd/display: Visual Confirm Bar Height Adjust
    [What]
    This change allows adjustment to the Visual Confirm
    height border.
    
    [Why]
    Aids debugging and testing
    
    [How]
    Use the existing infrastructure to implement logic to
    draw borders
    
    Reviewed-by: Anthony Koo <Anthony.Koo@amd.com>
    Acked-by: Wayne Lin <wayne.lin@amd.com>
    Signed-off-by: Harry VanZyllDeJong <hvanzyll@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dc.h
+++ drivers/gpu/drm/amd/display/dc/dc.h
@@ -574,6 +574,8 @@ struct dc_debug_options {
 	bool native422_support;
 	bool disable_dsc;
 	enum visual_confirm visual_confirm;
+	int visual_confirm_rect_height;
+
 	bool sanity_checks;
 	bool max_disp_clk;
 	bool surface_trace;
