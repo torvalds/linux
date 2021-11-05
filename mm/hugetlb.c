0739eb437f3d397eb39b5dc653aa250ee7b453f0
Baolin Wang <baolin.wang@linux.alibaba.com>
2021-11-05 13:41:58 -0700

    hugetlb: remove redundant validation in has_same_uncharge_info()
    The callers of has_same_uncharge_info() has accessed the original
    file_region and new file_region, and they are impossible to be NULL now.
    
    So we can remove the file_region validation in has_same_uncharge_info()
    to simplify the code.
    
    Link: https://lkml.kernel.org/r/97fc68d3f8d34f63c204645e10d7a718997e50b7.1634797639.git.baolin.wang@linux.alibaba.com
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -332,8 +332,7 @@ static bool has_same_uncharge_info(struct file_region *rg,
 				   struct file_region *org)
 {
 #ifdef CONFIG_CGROUP_HUGETLB
-	return rg && org &&
-	       rg->reservation_counter == org->reservation_counter &&
+	return rg->reservation_counter == org->reservation_counter &&
 	       rg->css == org->css;
 
 #else
