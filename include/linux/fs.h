0b3ea0926afb8dde70cfab00316ae0a70b93a7cc
Christoph Hellwig <hch@lst.de>
2021-11-05 13:36:58 -0700

    fs: explicitly unregister per-superblock BDIs
    Add a new SB_I_ flag to mark superblocks that have an ephemeral bdi
    associated with them, and unregister it when the superblock is shut
    down.
    
    Link: https://lkml.kernel.org/r/20211021124441.668816-4-hch@lst.de
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Cc: Miquel Raynal <miquel.raynal@bootlin.com>
    Cc: Richard Weinberger <richard@nod.at>
    Cc: Vignesh Raghavendra <vigneshr@ti.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1443,6 +1443,7 @@ extern int send_sigurg(struct fown_struct *fown);
 #define SB_I_UNTRUSTED_MOUNTER		0x00000040
 
 #define SB_I_SKIP_SYNC	0x00000100	/* Skip superblock at global sync */
+#define SB_I_PERSB_BDI	0x00000200	/* has a per-sb bdi */
 
 /* Possible states of 'frozen' field */
 enum {
