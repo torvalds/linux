b0d61266f56ae394b697dcc05698b46be6b1a194
Larry Finger <Larry.Finger@lwfinger.net>
2021-11-07 11:35:43 -0600

    staging: r8188eu: Fix breakage introduced when 5G code was removed
    commit d5f0b804368951b6b4a77d2f14b5bb6a04b0e011 upstream.
    
    In commit 221abd4d478a ("staging: r8188eu: Remove no more necessary definitions
    and code"), two entries were removed from RTW_ChannelPlanMap[], but not replaced
    with zeros. The position within this table is important, thus the patch broke
    systems operating in regulatory domains osted later than entry 0x13 in the table.
    Unfortunately, the FCC entry comes before that point and most testers did not see
    this problem.
    
    Fixes: 221abd4d478a ("staging: r8188eu: Remove no more necessary definitions and code")
    Cc: Stable <stable@vger.kernel.org> # v5.5+
    Reported-and-tested-by: Zameer Manji <zmanji@gmail.com>
    Reported-by: kernel test robot <lkp@intel.com>
    Reviewed-by: Phillip Potter <phil@philpotter.co.uk>
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Link: https://lore.kernel.org/r/20211107173543.7486-1-Larry.Finger@lwfinger.net
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/r8188eu/core/rtw_mlme_ext.c
+++ drivers/staging/r8188eu/core/rtw_mlme_ext.c
@@ -104,6 +104,7 @@ static struct rt_channel_plan_map	RTW_ChannelPlanMap[RT_CHANNEL_DOMAIN_MAX] = {
 	{0x01},	/* 0x10, RT_CHANNEL_DOMAIN_JAPAN */
 	{0x02},	/* 0x11, RT_CHANNEL_DOMAIN_FCC_NO_DFS */
 	{0x01},	/* 0x12, RT_CHANNEL_DOMAIN_JAPAN_NO_DFS */
+	{0x00}, /* 0x13 */
 	{0x02},	/* 0x14, RT_CHANNEL_DOMAIN_TAIWAN_NO_DFS */
 	{0x00},	/* 0x15, RT_CHANNEL_DOMAIN_ETSI_NO_DFS */
 	{0x00},	/* 0x16, RT_CHANNEL_DOMAIN_KOREA_NO_DFS */
@@ -115,6 +116,7 @@ static struct rt_channel_plan_map	RTW_ChannelPlanMap[RT_CHANNEL_DOMAIN_MAX] = {
 	{0x00},	/* 0x1C, */
 	{0x00},	/* 0x1D, */
 	{0x00},	/* 0x1E, */
+	{0x00},	/* 0x1F, */
 	/*  0x20 ~ 0x7F , New Define ===== */
 	{0x00},	/* 0x20, RT_CHANNEL_DOMAIN_WORLD_NULL */
 	{0x01},	/* 0x21, RT_CHANNEL_DOMAIN_ETSI1_NULL */
