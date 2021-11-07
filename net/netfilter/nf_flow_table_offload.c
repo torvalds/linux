b7ef25e8c27183bb24975a0f35fa2c3a0dd5fe81
Will Mortensen <willmo@gmail.com>
2021-11-06 18:28:21 -0700

    netfilter: flowtable: fix IPv6 tunnel addr match
    [ Upstream commit 39f6eed4cb209643f3f8633291854ed7375d7264 ]
    
    Previously the IPv6 addresses in the key were clobbered and the mask was
    left unset.
    
    I haven't tested this; I noticed it while skimming the code to
    understand an unrelated issue.
    
    Fixes: cfab6dbd0ecf ("netfilter: flowtable: add tunnel match offload support")
    Cc: wenxu <wenxu@ucloud.cn>
    Signed-off-by: Will Mortensen <willmo@gmail.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/netfilter/nf_flow_table_offload.c
+++ net/netfilter/nf_flow_table_offload.c
@@ -63,11 +63,11 @@ static void nf_flow_rule_lwt_match(struct nf_flow_match *match,
 		       sizeof(struct in6_addr));
 		if (memcmp(&key->enc_ipv6.src, &in6addr_any,
 			   sizeof(struct in6_addr)))
-			memset(&key->enc_ipv6.src, 0xff,
+			memset(&mask->enc_ipv6.src, 0xff,
 			       sizeof(struct in6_addr));
 		if (memcmp(&key->enc_ipv6.dst, &in6addr_any,
 			   sizeof(struct in6_addr)))
-			memset(&key->enc_ipv6.dst, 0xff,
+			memset(&mask->enc_ipv6.dst, 0xff,
 			       sizeof(struct in6_addr));
 		enc_keys |= BIT(FLOW_DISSECTOR_KEY_ENC_IPV6_ADDRS);
 		key->enc_control.addr_type = FLOW_DISSECTOR_KEY_IPV6_ADDRS;
