4da564004a73601e67ea609fed760f00d0682210
Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
2021-11-08 16:49:48 -0500

    drm/amd/display: Fix DPIA outbox timeout after GPU reset
    [ Upstream commit 6eff272dbee7ad444c491c9a96d49e78e91e2161 ]
    
    [Why]
    The HW interrupt gets disabled after GPU reset so we don't receive
    notifications for HPD or AUX from DMUB - leading to timeout and
    black screen with (or without) DPIA links connected.
    
    [How]
    Re-enable the interrupt after GPU reset like we do for the other
    DC interrupts.
    
    Fixes: 81927e2808be ("drm/amd/display: Support for DMUB AUX")
    
    Reviewed-by: Jude Shih <Jude.Shih@amd.com>
    Acked-by: Qingqing Zhuo <qingqing.zhuo@amd.com>
    Signed-off-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -2213,6 +2213,8 @@ static int dm_resume(void *handle)
 	if (amdgpu_in_reset(adev)) {
 		dc_state = dm->cached_dc_state;
 
+		amdgpu_dm_outbox_init(adev);
+
 		r = dm_dmub_hw_init(adev);
 		if (r)
 			DRM_ERROR("DMUB interface failed to initialize: status=%d\n", r);
