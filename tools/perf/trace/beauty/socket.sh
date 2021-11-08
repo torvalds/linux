1a1edf33206c30b51638effa72a6940b7bc7618f
Arnaldo Carvalho de Melo <acme@redhat.com>
2021-11-08 10:34:06 -0300

    perf beauty socket: Prep to receive more input header files
    Move from ternary like expression to an if block, this way we'll
    have just the extra lines for new files in the following patches.
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/trace/beauty/socket.sh
+++ tools/perf/trace/beauty/socket.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 # SPDX-License-Identifier: LGPL-2.1
 
-[ $# -eq 1 ] && uapi_header_dir=$1 || uapi_header_dir=tools/include/uapi/linux/
+if [ $# -gt 0 ] ; then
+	uapi_header_dir=$1
+else
+	uapi_header_dir=tools/include/uapi/linux/
+fi
 
 printf "static const char *socket_ipproto[] = {\n"
 regex='^[[:space:]]+IPPROTO_(\w+)[[:space:]]+=[[:space:]]+([[:digit:]]+),.*'
