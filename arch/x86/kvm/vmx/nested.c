3883bc9d28ed348d419d2e405d11f0924783f721
Lai Jiangshan <laijs@linux.alibaba.com>
2021-11-08 20:44:02 +0800

    KVM: X86: Mark CR3 dirty when vcpu->arch.cr3 is changed
    When vcpu->arch.cr3 is changed, it should be marked dirty unless it
    is being updated to the value of the architecture guest CR3 (i.e.
    VMX.GUEST_CR3 or vmcb->save.cr3 when tdp is enabled).
    
    This patch has no functionality changed because
    kvm_register_mark_dirty(vcpu, VCPU_EXREG_CR3) is superset of
    kvm_register_mark_available(vcpu, VCPU_EXREG_CR3) with additional
    change to vcpu->arch.regs_dirty, but no code uses regs_dirty for
    VCPU_EXREG_CR3.  (vmx_load_mmu_pgd() uses vcpu->arch.regs_avail instead
    to test if VCPU_EXREG_CR3 dirty which means current code (ab)uses
    regs_avail for VCPU_EXREG_CR3 dirty information.)
    
    Signed-off-by: Lai Jiangshan <laijs@linux.alibaba.com>
    Message-Id: <20211108124407.12187-11-jiangshanlai@gmail.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx/nested.c
+++ arch/x86/kvm/vmx/nested.c
@@ -1104,7 +1104,7 @@ static int nested_vmx_load_cr3(struct kvm_vcpu *vcpu, unsigned long cr3,
 		kvm_mmu_new_pgd(vcpu, cr3);
 
 	vcpu->arch.cr3 = cr3;
-	kvm_register_mark_available(vcpu, VCPU_EXREG_CR3);
+	kvm_register_mark_dirty(vcpu, VCPU_EXREG_CR3);
 
 	/* Re-initialize the MMU, e.g. to pick up CR4 MMU role changes. */
 	kvm_init_mmu(vcpu);
--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -1158,7 +1158,7 @@ int kvm_set_cr3(struct kvm_vcpu *vcpu, unsigned long cr3)
 		kvm_mmu_new_pgd(vcpu, cr3);
 
 	vcpu->arch.cr3 = cr3;
-	kvm_register_mark_available(vcpu, VCPU_EXREG_CR3);
+	kvm_register_mark_dirty(vcpu, VCPU_EXREG_CR3);
 
 handle_tlb_flush:
 	/*
@@ -10567,7 +10567,7 @@ static int __set_sregs_common(struct kvm_vcpu *vcpu, struct kvm_sregs *sregs,
 	vcpu->arch.cr2 = sregs->cr2;
 	*mmu_reset_needed |= kvm_read_cr3(vcpu) != sregs->cr3;
 	vcpu->arch.cr3 = sregs->cr3;
-	kvm_register_mark_available(vcpu, VCPU_EXREG_CR3);
+	kvm_register_mark_dirty(vcpu, VCPU_EXREG_CR3);
 
 	kvm_set_cr8(vcpu, sregs->cr8);
 
