98d5b61ef5fae7681df27065ad95ee6e30c42243
Jan Harkes <jaharkes@cs.cmu.edu>
2021-11-08 18:34:55 -0800

    coda: bump module version to 7.2
    Helps with tracking which patches have been propagated upstream and if
    users are running the latest known version.
    
    Link: https://lkml.kernel.org/r/20210908140308.18491-10-jaharkes@cs.cmu.edu
    Signed-off-by: Jan Harkes <jaharkes@cs.cmu.edu>
    Cc: Alex Shi <alex.shi@linux.alibaba.com>
    Cc: Jing Yangyang <jing.yangyang@zte.com.cn>
    Cc: Xin Tan <tanxin.ctf@gmail.com>
    Cc: Xiyu Yang <xiyuyang19@fudan.edu.cn>
    Cc: Zeal Robot <zealci@zte.com.cn>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/coda/psdev.c
+++ fs/coda/psdev.c
@@ -384,7 +384,7 @@ MODULE_AUTHOR("Jan Harkes, Peter J. Braam");
 MODULE_DESCRIPTION("Coda Distributed File System VFS interface");
 MODULE_ALIAS_CHARDEV_MAJOR(CODA_PSDEV_MAJOR);
 MODULE_LICENSE("GPL");
-MODULE_VERSION("7.0");
+MODULE_VERSION("7.2");
 
 static int __init init_coda(void)
 {
