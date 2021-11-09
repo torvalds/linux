e1edc277e6f6dfb372216522dfc57f9381c39e35
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2021-11-08 18:32:46 -0800

    linux/container_of.h: switch to static_assert
    _Static_assert() is evaluated already in the compiler's frontend, and
    gives a somehat more to-the-point error, compared to the BUILD_BUG_ON
    macro, which only fires after the optimizer has had a chance to
    eliminate calls to functions marked with __attribute__((error)).  In
    theory, this might make builds a tiny bit faster.
    
    There's also a little less gunk in the error message emitted:
    
      lib/sort.c: In function `foo':
      include/linux/build_bug.h:78:41: error: static assertion failed: "pointer type mismatch in container_of()"
         78 | #define __static_assert(expr, msg, ...) _Static_assert(expr, msg)
    
    compared to
    
      lib/sort.c: In function `foo':
      include/linux/compiler_types.h:322:38: error: call to `__compiletime_assert_2' declared with attribute error: pointer type mismatch in container_of()
        322 |  _compiletime_assert(condition, msg, __compiletime_assert_, __COUNTER__)
    
    While at it, fix the copy-pasto in container_of_safe().
    
    Link: https://lkml.kernel.org/r/20211015090530.2774079-1-linux@rasmusvillemoes.dk
    Link: https://lore.kernel.org/lkml/20211014132331.GA4811@kernel.org/T/
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Reviewed-by: Miguel Ojeda <ojeda@kernel.org>
    Acked-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/container_of.h
+++ include/linux/container_of.h
@@ -16,9 +16,9 @@
  */
 #define container_of(ptr, type, member) ({				\
 	void *__mptr = (void *)(ptr);					\
-	BUILD_BUG_ON_MSG(!__same_type(*(ptr), ((type *)0)->member) &&	\
-			 !__same_type(*(ptr), void),			\
-			 "pointer type mismatch in container_of()");	\
+	static_assert(__same_type(*(ptr), ((type *)0)->member) ||	\
+		      __same_type(*(ptr), void),			\
+		      "pointer type mismatch in container_of()");	\
 	((type *)(__mptr - offsetof(type, member))); })
 
 /**
@@ -31,9 +31,9 @@
  */
 #define container_of_safe(ptr, type, member) ({				\
 	void *__mptr = (void *)(ptr);					\
-	BUILD_BUG_ON_MSG(!__same_type(*(ptr), ((type *)0)->member) &&	\
-			 !__same_type(*(ptr), void),			\
-			 "pointer type mismatch in container_of()");	\
+	static_assert(__same_type(*(ptr), ((type *)0)->member) ||	\
+		      __same_type(*(ptr), void),			\
+		      "pointer type mismatch in container_of_safe()");	\
 	IS_ERR_OR_NULL(__mptr) ? ERR_CAST(__mptr) :			\
 		((type *)(__mptr - offsetof(type, member))); })
 
