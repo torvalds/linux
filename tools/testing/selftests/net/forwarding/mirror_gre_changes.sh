af0a51113cb7445435488e5f9514598f2b52d7a7
Petr Machata <petrm@nvidia.com>
2021-11-09 17:17:34 +0100

    selftests: forwarding: Fix packet matching in mirroring selftests
    In commit 6de6e46d27ef ("cls_flower: Fix inability to match GRE/IPIP
    packets"), cls_flower was fixed to match an outer packet of a tunneled
    packet as would be expected, rather than dissecting to the inner packet and
    matching on that.
    
    This fix uncovered several issues in packet matching in mirroring
    selftests:
    
    - in mirror_gre_bridge_1d_vlan.sh and mirror_gre_vlan_bridge_1q.sh, the
      vlan_ethtype match is copied around as "ip", even as some of the tests
      are running over ip6gretap. This is fixed by using an "ipv6" for
      vlan_ethtype in the ip6gretap tests.
    
    - in mirror_gre_changes.sh, a filter to count GRE packets is set up to
      match TTL of 50. This used to trigger in the offloaded datapath, where
      the envelope TTL was matched, but not in the software datapath, which
      considered TTL of the inner packet. Now that both match consistently, all
      the packets were double-counted. This is fixed by marking the filter as
      skip_hw, leaving only the SW datapath component active.
    
    Fixes: 6de6e46d27ef ("cls_flower: Fix inability to match GRE/IPIP packets")
    Signed-off-by: Petr Machata <petrm@nvidia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- tools/testing/selftests/net/forwarding/mirror_gre_changes.sh
+++ tools/testing/selftests/net/forwarding/mirror_gre_changes.sh
@@ -74,7 +74,7 @@ test_span_gre_ttl()
 
 	mirror_install $swp1 ingress $tundev "matchall $tcflags"
 	tc filter add dev $h3 ingress pref 77 prot $prot \
-		flower ip_ttl 50 action pass
+		flower skip_hw ip_ttl 50 action pass
 
 	mirror_test v$h1 192.0.2.1 192.0.2.2 $h3 77 0
 
