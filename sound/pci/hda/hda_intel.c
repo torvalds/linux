39173303c83859723dab32c2abfb97296d6af3bf
Takashi Iwai <tiwai@suse.de>
2021-11-10 20:46:33 +0100

    ALSA: hda: Free card instance properly at probe errors
    The recent change in hda-intel driver to allow repeated probes
    surfaced a problem that has been hidden until; the probe process in
    the work calls azx_free() at the error path, and this skips the card
    free process that eventually releases codec instances.  As a result,
    we get a kernel WARNING like:
    
      snd_hda_intel 0000:00:1f.3: Cannot probe codecs, giving up
      ------------[ cut here ]------------
      WARNING: CPU: 14 PID: 186 at sound/hda/hdac_bus.c:73
      ....
    
    For fixing this, we need to call snd_card_free() instead of
    azx_free().  Additionally, the device drvdata has to be cleared, as
    the driver binding itself is still active.  Then the PM and other
    driver callbacks will ignore the procedure.
    
    Fixes: c0f1886de7e1 ("ALSA: hda: intel: Allow repeatedly probing on codec configuration errors")
    Reported-and-tested-by: Scott Branden <scott.branden@broadcom.com>
    Cc: <stable@vger.kernel.org>
    Link: https://lore.kernel.org/r/063e2397-7edb-5f48-7b0d-618b938d9dd8@broadcom.com
    Link: https://lore.kernel.org/r/20211110194633.19098-1-tiwai@suse.de
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -2327,7 +2327,8 @@ static int azx_probe_continue(struct azx *chip)
 
 out_free:
 	if (err < 0) {
-		azx_free(chip);
+		pci_set_drvdata(pci, NULL);
+		snd_card_free(chip->card);
 		return err;
 	}
 
