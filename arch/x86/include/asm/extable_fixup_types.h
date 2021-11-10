f637fbc7bda60c0dca47925d5e2da948598230b6
Peter Zijlstra <peterz@infradead.org>
2021-11-10 11:01:09 +0100

    x86/extable: Extend extable functionality
    [ Upstream commit 4b5305decc8436bfe363d1c1773e8fa1c828b14d ]
    
    In order to remove further .fixup usage, extend the extable
    infrastructure to take additional information from the extable entry
    sites.
    
    Specifically add _ASM_EXTABLE_TYPE_REG() and EX_TYPE_IMM_REG that
    extend the existing _ASM_EXTABLE_TYPE() by taking an additional
    register argument and encoding that and an s16 immediate into the
    existing s32 type field. This limits the actual types to the first
    byte, 255 seem plenty.
    
    Also add a few flags into the type word, specifically CLEAR_AX and
    CLEAR_DX which clear the return and extended return register.
    
    Notes:
     - due to the % in our register names it's hard to make it more
       generally usable as arm64 did.
     - the s16 is far larger than used in these patches, future extentions
       can easily shrink this to get more bits.
     - without the bitfield fix this will not compile, because: 0xFF > -1
       and we can't even extract the TYPE field.
    
    [nathanchance: Build fix for clang-lto builds:
     https://lkml.kernel.org/r/20211210234953.3420108-1-nathan@kernel.org
    ]
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Reviewed-by: Nick Desaulniers <ndesaulniers@google.com>
    Tested-by: Nick Desaulniers <ndesaulniers@google.com>
    Link: https://lore.kernel.org/r/20211110101325.303890153@infradead.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/x86/include/asm/extable_fixup_types.h
+++ arch/x86/include/asm/extable_fixup_types.h
@@ -2,6 +2,29 @@
 #ifndef _ASM_X86_EXTABLE_FIXUP_TYPES_H
 #define _ASM_X86_EXTABLE_FIXUP_TYPES_H
 
+/*
+ * Our IMM is signed, as such it must live at the top end of the word. Also,
+ * since C99 hex constants are of ambigious type, force cast the mask to 'int'
+ * so that FIELD_GET() will DTRT and sign extend the value when it extracts it.
+ */
+#define EX_DATA_TYPE_MASK		((int)0x000000FF)
+#define EX_DATA_REG_MASK		((int)0x00000F00)
+#define EX_DATA_FLAG_MASK		((int)0x0000F000)
+#define EX_DATA_IMM_MASK		((int)0xFFFF0000)
+
+#define EX_DATA_REG_SHIFT		8
+#define EX_DATA_FLAG_SHIFT		12
+#define EX_DATA_IMM_SHIFT		16
+
+#define EX_DATA_FLAG(flag)		((flag) << EX_DATA_FLAG_SHIFT)
+#define EX_DATA_IMM(imm)		((imm) << EX_DATA_IMM_SHIFT)
+
+/* flags */
+#define EX_FLAG_CLEAR_AX		EX_DATA_FLAG(1)
+#define EX_FLAG_CLEAR_DX		EX_DATA_FLAG(2)
+#define EX_FLAG_CLEAR_AX_DX		EX_DATA_FLAG(3)
+
+/* types */
 #define	EX_TYPE_NONE			 0
 #define	EX_TYPE_DEFAULT			 1
 #define	EX_TYPE_FAULT			 2
@@ -20,5 +43,6 @@
 #define	EX_TYPE_FAULT_MCE_SAFE		13
 
 #define	EX_TYPE_POP_ZERO		14
+#define	EX_TYPE_IMM_REG			15 /* reg := (long)imm */
 
 #endif
