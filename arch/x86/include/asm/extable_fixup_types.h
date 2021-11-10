c9a34c3f4ece192f6d804039fe6aac9618f0d236
Peter Zijlstra <peterz@infradead.org>
2021-11-10 11:01:16 +0100

    x86/kvm: Remove .fixup usage
    KVM instruction emulation has a gnarly hack where the .fixup does a
    return, however there's already a ret right after the 10b label, so
    mark that as 11 and have the exception clear %esi to remove the
    .fixup.
    
    Suggested-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Link: https://lore.kernel.org/r/20211110101325.722157053@infradead.org

--- arch/x86/include/asm/extable_fixup_types.h
+++ arch/x86/include/asm/extable_fixup_types.h
@@ -46,5 +46,6 @@
 #define	EX_TYPE_IMM_REG			17 /* reg := (long)imm */
 #define	EX_TYPE_EFAULT_REG		(EX_TYPE_IMM_REG | EX_DATA_IMM(-EFAULT))
 #define	EX_TYPE_ZERO_REG		(EX_TYPE_IMM_REG | EX_DATA_IMM(0))
+#define	EX_TYPE_ONE_REG			(EX_TYPE_IMM_REG | EX_DATA_IMM(1))
 
 #endif
