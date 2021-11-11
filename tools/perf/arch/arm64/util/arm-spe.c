455c988225c7eee84c7a2f86984404825a1830bb
German Gomez <german.gomez@arm.com>
2021-11-11 13:36:23 +0000

    perf arm-spe: Update --switch-events docs in 'perf record'
    Update 'perf record' docs and ARM SPE recording options so that they are
    consistent. This includes supporting the --no-switch-events flag in ARM
    SPE as well.
    
    Reviewed-by: Leo Yan <leo.yan@linaro.org>
    Signed-off-by: German Gomez <german.gomez@arm.com>
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: John Garry <john.garry@huawei.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Mathieu Poirier <mathieu.poirier@linaro.org>
    Cc: Will Deacon <will@kernel.org>
    Cc: linux-arm-kernel@lists.infradead.org
    Link: https://lore.kernel.org/r/20211111133625.193568-3-german.gomez@arm.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/arch/arm64/util/arm-spe.c
+++ tools/perf/arch/arm64/util/arm-spe.c
@@ -254,8 +254,10 @@ static int arm_spe_recording_options(struct auxtrace_record *itr,
 	if (!perf_cpu_map__empty(cpus)) {
 		evsel__set_sample_bit(tracking_evsel, TIME);
 		evsel__set_sample_bit(tracking_evsel, CPU);
+
 		/* also track task context switch */
-		tracking_evsel->core.attr.context_switch = 1;
+		if (!record_opts__no_switch_events(opts))
+			tracking_evsel->core.attr.context_switch = 1;
 	}
 
 	return 0;
