26c79fcbfa64b18ca1407a3be7ac3442aef51073
Yonghong Song <yhs@fb.com>
2021-11-11 17:26:41 -0800

    selftests/bpf: Rename progs/tag.c to progs/btf_decl_tag.c
    Rename progs/tag.c to progs/btf_decl_tag.c so we can introduce
    progs/btf_type_tag.c in the next patch.
    
    Also create a subtest for btf_decl_tag in prog_tests/btf_tag.c
    so we can introduce btf_type_tag subtest in the next patch.
    
    I also took opportunity to remove the check whether __has_attribute
    is defined or not in progs/btf_decl_tag.c since all recent
    clangs should already support this macro.
    
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20211112012641.1507144-1-yhs@fb.com

--- tools/testing/selftests/bpf/prog_tests/btf_tag.c
+++ tools/testing/selftests/bpf/prog_tests/btf_tag.c
@@ -1,20 +1,26 @@
 // SPDX-License-Identifier: GPL-2.0
 /* Copyright (c) 2021 Facebook */
 #include <test_progs.h>
-#include "tag.skel.h"
+#include "btf_decl_tag.skel.h"
 
-void test_btf_tag(void)
+static void test_btf_decl_tag(void)
 {
-	struct tag *skel;
+	struct btf_decl_tag *skel;
 
-	skel = tag__open_and_load();
-	if (!ASSERT_OK_PTR(skel, "btf_tag"))
+	skel = btf_decl_tag__open_and_load();
+	if (!ASSERT_OK_PTR(skel, "btf_decl_tag"))
 		return;
 
 	if (skel->rodata->skip_tests) {
-		printf("%s:SKIP: btf_tag attribute not supported", __func__);
+		printf("%s:SKIP: btf_decl_tag attribute not supported", __func__);
 		test__skip();
 	}
 
-	tag__destroy(skel);
+	btf_decl_tag__destroy(skel);
+}
+
+void test_btf_tag(void)
+{
+	if (test__start_subtest("btf_decl_tag"))
+		test_btf_decl_tag();
 }
