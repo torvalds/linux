4a8980cb2a7cb48371af67df4e6d70352b991ab0
Tony Luck <tony.luck@intel.com>
2021-11-12 10:28:35 -0800

    x86/cpu: Add Raptor Lake to Intel family
    [ Upstream commit fbdb5e8f2926ae9636c9fa6f42c7426132ddeeb2 ]
    
    Add model ID for Raptor Lake.
    
    [ dhansen: These get added as soon as possible so that folks doing
      development can leverage them. ]
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Link: https://lkml.kernel.org/r/20211112182835.924977-1-tony.luck@intel.com
    Stable-dep-of: ce0b15d11ad8 ("x86/mm: Avoid incomplete Global INVLPG flushes")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/x86/include/asm/intel-family.h
+++ arch/x86/include/asm/intel-family.h
@@ -104,6 +104,8 @@
 #define INTEL_FAM6_RAPTORLAKE_P		0xBA
 #define INTEL_FAM6_RAPTORLAKE_S		0xBF
 
+#define INTEL_FAM6_RAPTOR_LAKE		0xB7
+
 /* "Small Core" Processors (Atom) */
 
 #define INTEL_FAM6_ATOM_BONNELL		0x1C /* Diamondville, Pineview */
