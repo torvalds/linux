a7a427d1543f89ad57fda5d6b9bb70f4cecb2fba
Zhang Mingyu <zhang.mingyu@zte.com.cn>
2021-11-12 09:25:47 +0000

    9p: Use BUG_ON instead of if condition followed by BUG.
    This issue was detected with the help of Coccinelle.
    
    Link: https://lkml.kernel.org/r/20211112092547.9153-1-zhang.mingyu@zte.com.cn
    Reported-by: Zeal Robot <zealci@zte.com.cn>
    Signed-off-by: Zhang Mingyu <zhang.mingyu@zte.com.cn>
    Signed-off-by: Dominique Martinet <asmadeus@codewreck.org>

--- fs/9p/vfs_file.c
+++ fs/9p/vfs_file.c
@@ -138,8 +138,7 @@ static int v9fs_file_do_lock(struct file *filp, int cmd, struct file_lock *fl)
 	fid = filp->private_data;
 	BUG_ON(fid == NULL);
 
-	if ((fl->fl_flags & FL_POSIX) != FL_POSIX)
-		BUG();
+	BUG_ON((fl->fl_flags & FL_POSIX) != FL_POSIX);
 
 	res = locks_lock_file_wait(filp, fl);
 	if (res < 0)
