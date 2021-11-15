385315decf65902768a0062b4e594f59f013025e
Jesse Melhuish <melhuishj@chromium.org>
2021-11-15 22:00:52 +0000

    Bluetooth: Don't initialize msft/aosp when using user channel
    A race condition is triggered when usermode control is given to
    userspace before the kernel's MSFT query responds, resulting in an
    unexpected response to userspace's reset command.
    
    Issue can be observed in btmon:
    
    < HCI Command: Vendor (0x3f|0x001e) plen 2                    #3 [hci0]
            05 01                                            ..
    @ USER Open: bt_stack_manage (privileged) version 2.22  {0x0002} [hci0]
    < HCI Command: Reset (0x03|0x0003) plen 0                     #4 [hci0]
    > HCI Event: Command Complete (0x0e) plen 5                   #5 [hci0]
          Vendor (0x3f|0x001e) ncmd 1
    	Status: Command Disallowed (0x0c)
    	05                                               .
    > HCI Event: Command Complete (0x0e) plen 4                   #6 [hci0]
          Reset (0x03|0x0003) ncmd 2
    	Status: Success (0x00)
    
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Reviewed-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Signed-off-by: Jesse Melhuish <melhuishj@chromium.org>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/hci_sync.c
+++ net/bluetooth/hci_sync.c
@@ -3887,8 +3887,10 @@ setup_failed:
 	    hci_dev_test_flag(hdev, HCI_VENDOR_DIAG) && hdev->set_diag)
 		ret = hdev->set_diag(hdev, true);
 
-	msft_do_open(hdev);
-	aosp_do_open(hdev);
+	if (!hci_dev_test_flag(hdev, HCI_USER_CHANNEL)) {
+		msft_do_open(hdev);
+		aosp_do_open(hdev);
+	}
 
 	clear_bit(HCI_INIT, &hdev->flags);
 
@@ -4031,8 +4033,10 @@ int hci_dev_close_sync(struct hci_dev *hdev)
 
 	hci_sock_dev_event(hdev, HCI_DEV_DOWN);
 
-	aosp_do_close(hdev);
-	msft_do_close(hdev);
+	if (!hci_dev_test_flag(hdev, HCI_USER_CHANNEL)) {
+		aosp_do_close(hdev);
+		msft_do_close(hdev);
+	}
 
 	if (hdev->flush)
 		hdev->flush(hdev);
