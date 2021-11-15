890e3dc8bb6ee630870560c34054692f7a45da42
Heiko Carstens <hca@linux.ibm.com>
2021-11-15 20:56:14 +0100

    ftrace/samples: add s390 support for ftrace direct multi sample
    Add s390 architecture support for the ftrace direct multi sample.
    See commit 5fae941b9a6f ("ftrace/samples: Add multi direct interface
    test module") for further details.
    
    Link: https://lore.kernel.org/r/20211115195614.3173346-3-hca@linux.ibm.com
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- samples/ftrace/ftrace-direct-multi.c
+++ samples/ftrace/ftrace-direct-multi.c
@@ -4,6 +4,7 @@
 #include <linux/mm.h> /* for handle_mm_fault() */
 #include <linux/ftrace.h>
 #include <linux/sched/stat.h>
+#include <asm/asm-offsets.h>
 
 extern void my_direct_func(unsigned long ip);
 
@@ -13,6 +14,8 @@ void my_direct_func(unsigned long ip)
 }
 
 extern void my_tramp(void *);
+
+#ifdef CONFIG_X86_64
 
 asm (
 "	.pushsection    .text, \"ax\", @progbits\n"
@@ -30,6 +33,33 @@ asm (
 "	.size		my_tramp, .-my_tramp\n"
 "	.popsection\n"
 );
+
+#endif /* CONFIG_X86_64 */
+
+#ifdef CONFIG_S390
+
+asm (
+"	.pushsection	.text, \"ax\", @progbits\n"
+"	.type		my_tramp, @function\n"
+"	.globl		my_tramp\n"
+"   my_tramp:"
+"	lgr		%r1,%r15\n"
+"	stmg		%r0,%r5,"__stringify(__SF_GPRS)"(%r15)\n"
+"	stg		%r14,"__stringify(__SF_GPRS+8*8)"(%r15)\n"
+"	aghi		%r15,"__stringify(-STACK_FRAME_OVERHEAD)"\n"
+"	stg		%r1,"__stringify(__SF_BACKCHAIN)"(%r15)\n"
+"	lgr		%r2,%r0\n"
+"	brasl		%r14,my_direct_func\n"
+"	aghi		%r15,"__stringify(STACK_FRAME_OVERHEAD)"\n"
+"	lmg		%r0,%r5,"__stringify(__SF_GPRS)"(%r15)\n"
+"	lg		%r14,"__stringify(__SF_GPRS+8*8)"(%r15)\n"
+"	lgr		%r1,%r0\n"
+"	br		%r1\n"
+"	.size		my_tramp, .-my_tramp\n"
+"	.popsection\n"
+);
+
+#endif /* CONFIG_S390 */
 
 static struct ftrace_ops direct;
 
