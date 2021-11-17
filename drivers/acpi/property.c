dbd961095ed44585fc628962d7c7da866852f9a5
Sakari Ailus <sakari.ailus@linux.intel.com>
2021-11-17 20:07:34 +0200

    ACPI: Get acpi_device's parent from the parent field
    commit 9054fc6d57e80c27c0b0632966416144f2092c2b upstream.
    
    Printk modifier %pfw is used to print the full path of the device name.
    This is obtained device by device until a device no longer has a parent.
    
    On ACPI getting the parent fwnode is done by calling acpi_get_parent()
    which tries to down() a semaphore. But local IRQs are now disabled in
    vprintk_store() before the mutex is acquired. This is obviously a problem.
    
    Luckily struct device, embedded in struct acpi_device, has a parent field
    already. Use that field to get the parent instead of relying on
    acpi_get_parent().
    
    Fixes: 3bd32d6a2ee6 ("lib/vsprintf: Add %pfw conversion specifier for printing fwnode names")
    Cc: 5.5+ <stable@vger.kernel.org> # 5.5+
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/acpi/property.c
+++ drivers/acpi/property.c
@@ -1090,15 +1090,10 @@ struct fwnode_handle *acpi_node_get_parent(const struct fwnode_handle *fwnode)
 		/* All data nodes have parent pointer so just return that */
 		return to_acpi_data_node(fwnode)->parent;
 	} else if (is_acpi_device_node(fwnode)) {
-		acpi_handle handle, parent_handle;
+		struct device *dev = to_acpi_device_node(fwnode)->dev.parent;
 
-		handle = to_acpi_device_node(fwnode)->handle;
-		if (ACPI_SUCCESS(acpi_get_parent(handle, &parent_handle))) {
-			struct acpi_device *adev;
-
-			if (!acpi_bus_get_device(parent_handle, &adev))
-				return acpi_fwnode_handle(adev);
-		}
+		if (dev)
+			return acpi_fwnode_handle(to_acpi_device(dev));
 	}
 
 	return NULL;
