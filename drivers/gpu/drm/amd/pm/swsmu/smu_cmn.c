e771d71d8d588aa0c0f9ba3e1b81ac5989ffcccf
Luben Tuikov <luben.tuikov@amd.com>
2021-11-17 13:39:26 -0500

    drm/amd/pm: Print the error on command submission
    Print the error on command submission immediately after submitting to
    the SMU. This is rate-limited. It helps to immediately know there was an
    error on command submission, rather than leave it up to clients to report
    the error, as sometimes they do not.
    
    Cc: Alex Deucher <Alexander.Deucher@amd.com>
    Signed-off-by: Luben Tuikov <luben.tuikov@amd.com>
    Acked-by: Alex Deucher <Alexander.Deucher@amd.com>
    Reviewed-by: Alex Deucher <Alexander.Deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/swsmu/smu_cmn.c
+++ drivers/gpu/drm/amd/pm/swsmu/smu_cmn.c
@@ -352,7 +352,7 @@ int smu_cmn_send_smc_msg_with_param(struct smu_context *smu,
 	__smu_cmn_send_msg(smu, (uint16_t) index, param);
 	reg = __smu_cmn_poll_stat(smu);
 	res = __smu_cmn_reg2errno(smu, reg);
-	if (res == -EREMOTEIO)
+	if (res != 0)
 		__smu_cmn_reg_print_error(smu, reg, index, param, msg);
 	if (read_arg)
 		smu_cmn_read_arg(smu, read_arg);
