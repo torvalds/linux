c7785d85b6c6cc9f3d0f1a8cab128f4062b30abb
Hou Wenlong <houwenlong93@linux.alibaba.com>
2021-11-17 17:20:39 +0800

    KVM: x86/mmu: Skip tlb flush if it has been done in zap_gfn_range()
    If the parameter flush is set, zap_gfn_range() would flush remote tlb
    when yield, then tlb flush is not needed outside. So use the return
    value of zap_gfn_range() directly instead of OR on it in
    kvm_unmap_gfn_range() and kvm_tdp_mmu_unmap_gfn_range().
    
    Fixes: 3039bcc744980 ("KVM: Move x86's MMU notifier memslot walkers to generic code")
    Signed-off-by: Hou Wenlong <houwenlong93@linux.alibaba.com>
    Message-Id: <5e16546e228877a4d974f8c0e448a93d52c7a5a9.1637140154.git.houwenlong93@linux.alibaba.com>
    Reviewed-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -1582,7 +1582,7 @@ bool kvm_unmap_gfn_range(struct kvm *kvm, struct kvm_gfn_range *range)
 		flush = kvm_handle_gfn_range(kvm, range, kvm_unmap_rmapp);
 
 	if (is_tdp_mmu_enabled(kvm))
-		flush |= kvm_tdp_mmu_unmap_gfn_range(kvm, range, flush);
+		flush = kvm_tdp_mmu_unmap_gfn_range(kvm, range, flush);
 
 	return flush;
 }
--- arch/x86/kvm/mmu/tdp_mmu.c
+++ arch/x86/kvm/mmu/tdp_mmu.c
@@ -1032,8 +1032,8 @@ bool kvm_tdp_mmu_unmap_gfn_range(struct kvm *kvm, struct kvm_gfn_range *range,
 	struct kvm_mmu_page *root;
 
 	for_each_tdp_mmu_root(kvm, root, range->slot->as_id)
-		flush |= zap_gfn_range(kvm, root, range->start, range->end,
-				       range->may_block, flush, false);
+		flush = zap_gfn_range(kvm, root, range->start, range->end,
+				      range->may_block, flush, false);
 
 	return flush;
 }
