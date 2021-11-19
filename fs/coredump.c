6ac79ec5378b675f91021c8073cde0eea59f81ad
Eric W. Biederman <ebiederm@xmission.com>
2021-11-19 12:11:54 -0600

    coredump: Stop setting signal->group_exit_task
    Currently the coredump code sets group_exit_task so that
    signal_group_exit() will return true during a coredump.  Now that the
    coredump code always sets SIGNAL_GROUP_EXIT there is no longer a need
    to set signal->group_exit_task.
    
    Link: https://lkml.kernel.org/r/20211213225350.27481-6-ebiederm@xmission.com
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- fs/coredump.c
+++ fs/coredump.c
@@ -377,7 +377,6 @@ static int zap_threads(struct task_struct *tsk,
 	spin_lock_irq(&tsk->sighand->siglock);
 	if (!signal_group_exit(tsk->signal)) {
 		tsk->signal->core_state = core_state;
-		tsk->signal->group_exit_task = tsk;
 		nr = zap_process(tsk, exit_code);
 		clear_tsk_thread_flag(tsk, TIF_SIGPENDING);
 		tsk->flags |= PF_DUMPCORE;
@@ -426,7 +425,6 @@ static void coredump_finish(bool core_dumped)
 	spin_lock_irq(&current->sighand->siglock);
 	if (core_dumped && !__fatal_signal_pending(current))
 		current->signal->group_exit_code |= 0x80;
-	current->signal->group_exit_task = NULL;
 	next = current->signal->core_state->dumper.next;
 	current->signal->core_state = NULL;
 	spin_unlock_irq(&current->sighand->siglock);
