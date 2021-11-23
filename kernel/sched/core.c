e6ee7abd6bfe559ad9989004b34c320fd638c526
Mark Rutland <mark.rutland@arm.com>
2021-11-23 11:40:47 +0000

    sched/scs: Reset task stack state in bringup_cpu()
    [ Upstream commit dce1ca0525bfdc8a69a9343bc714fbc19a2f04b3 ]
    
    To hot unplug a CPU, the idle task on that CPU calls a few layers of C
    code before finally leaving the kernel. When KASAN is in use, poisoned
    shadow is left around for each of the active stack frames, and when
    shadow call stacks are in use. When shadow call stacks (SCS) are in use
    the task's saved SCS SP is left pointing at an arbitrary point within
    the task's shadow call stack.
    
    When a CPU is offlined than onlined back into the kernel, this stale
    state can adversely affect execution. Stale KASAN shadow can alias new
    stackframes and result in bogus KASAN warnings. A stale SCS SP is
    effectively a memory leak, and prevents a portion of the shadow call
    stack being used. Across a number of hotplug cycles the idle task's
    entire shadow call stack can become unusable.
    
    We previously fixed the KASAN issue in commit:
    
      e1b77c92981a5222 ("sched/kasan: remove stale KASAN poison after hotplug")
    
    ... by removing any stale KASAN stack poison immediately prior to
    onlining a CPU.
    
    Subsequently in commit:
    
      f1a0a376ca0c4ef1 ("sched/core: Initialize the idle task with preemption disabled")
    
    ... the refactoring left the KASAN and SCS cleanup in one-time idle
    thread initialization code rather than something invoked prior to each
    CPU being onlined, breaking both as above.
    
    We fixed SCS (but not KASAN) in commit:
    
      63acd42c0d4942f7 ("sched/scs: Reset the shadow stack when idle_task_exit")
    
    ... but as this runs in the context of the idle task being offlined it's
    potentially fragile.
    
    To fix these consistently and more robustly, reset the SCS SP and KASAN
    shadow of a CPU's idle task immediately before we online that CPU in
    bringup_cpu(). This ensures the idle task always has a consistent state
    when it is running, and removes the need to so so when exiting an idle
    task.
    
    Whenever any thread is created, dup_task_struct() will give the task a
    stack which is free of KASAN shadow, and initialize the task's SCS SP,
    so there's no need to specially initialize either for idle thread within
    init_idle(), as this was only necessary to handle hotplug cycles.
    
    I've tested this on arm64 with:
    
    * gcc 11.1.0, defconfig +KASAN_INLINE, KASAN_STACK
    * clang 12.0.0, defconfig +KASAN_INLINE, KASAN_STACK, SHADOW_CALL_STACK
    
    ... offlining and onlining CPUS with:
    
    | while true; do
    |   for C in /sys/devices/system/cpu/cpu*/online; do
    |     echo 0 > $C;
    |     echo 1 > $C;
    |   done
    | done
    
    Fixes: f1a0a376ca0c4ef1 ("sched/core: Initialize the idle task with preemption disabled")
    Reported-by: Qian Cai <quic_qiancai@quicinc.com>
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <valentin.schneider@arm.com>
    Tested-by: Qian Cai <quic_qiancai@quicinc.com>
    Link: https://lore.kernel.org/lkml/20211115113310.35693-1-mark.rutland@arm.com/
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6523,9 +6523,6 @@ void __init init_idle(struct task_struct *idle, int cpu)
 	idle->se.exec_start = sched_clock();
 	idle->flags |= PF_IDLE;
 
-	scs_task_reset(idle);
-	kasan_unpoison_task_stack(idle);
-
 #ifdef CONFIG_SMP
 	/*
 	 * Its possible that init_idle() gets called multiple times on a task,
@@ -6681,7 +6678,6 @@ void idle_task_exit(void)
 		finish_arch_post_lock_switch();
 	}
 
-	scs_task_reset(current);
 	/* finish_cpu(), as ran on the BP, will clean up the active_mm state */
 }
 
