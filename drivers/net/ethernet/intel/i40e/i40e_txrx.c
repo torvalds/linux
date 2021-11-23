6e19cf7d3815c009bfd213832a6feac4bafc37ae
Alexander Lobakin <alexandr.lobakin@intel.com>
2021-11-23 18:18:34 +0100

    i40e: switch to napi_build_skb()
    napi_build_skb() reuses per-cpu NAPI skbuff_head cache in order
    to save some cycles on freeing/allocating skbuff_heads on every
    new Rx or completed Tx.
    i40e driver runs Tx completion polling cycle right before the Rx
    one and uses napi_consume_skb() to feed the cache with skbuff_heads
    of completed entries, so it's never empty and always warm at that
    moment. Switch to the napi_build_skb() to relax mm pressure on
    heavy Rx.
    
    Signed-off-by: Alexander Lobakin <alexandr.lobakin@intel.com>
    Reviewed-by: Michal Swiatkowski <michal.swiatkowski@linux.intel.com>
    Tested-by: Gurucharan G <gurucharanx.g@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_txrx.c
+++ drivers/net/ethernet/intel/i40e/i40e_txrx.c
@@ -2204,7 +2204,7 @@ static struct sk_buff *i40e_build_skb(struct i40e_ring *rx_ring,
 	net_prefetch(xdp->data_meta);
 
 	/* build an skb around the page buffer */
-	skb = build_skb(xdp->data_hard_start, truesize);
+	skb = napi_build_skb(xdp->data_hard_start, truesize);
 	if (unlikely(!skb))
 		return NULL;
 
