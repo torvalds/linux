f7b8afb8b4f141576ad3ffbe8f4db634f1cb3baf
Zefa Chen <zefa.chen@rock-chips.com>
2021-11-23 16:31:19 +0800

    include: uapi: rk-camera-module.h: struct channel_info add data type
    Signed-off-by: Zefa Chen <zefa.chen@rock-chips.com>
    Change-Id: I64ca7e16e0048c88938ce4cc77e96c81d14b7601

--- include/uapi/linux/rk-camera-module.h
+++ include/uapi/linux/rk-camera-module.h
@@ -116,7 +116,7 @@
 	_IOR('V', BASE_VIDIOC_PRIVATE + 19, __u32)
 
 #define RKMODULE_GET_CHANNEL_INFO	\
-	_IOR('V', BASE_VIDIOC_PRIVATE + 20, struct rkmodule_channel_info)
+	_IOWR('V', BASE_VIDIOC_PRIVATE + 20, struct rkmodule_channel_info)
 
 #define RKMODULE_GET_SYNC_MODE       \
 	_IOR('V', BASE_VIDIOC_PRIVATE + 21, __u32)
@@ -521,6 +521,8 @@ struct rkmodule_channel_info {
 	__u32 width;
 	__u32 height;
 	__u32 bus_fmt;
+	__u32 data_type;
+	__u32 data_bit;
 } __attribute__ ((packed));
 
 /*
