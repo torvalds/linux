0ec0eda3f3c3483872157a7780cebdf9bf3a11c4
Jeya R <jeyr@codeaurora.org>
2021-11-24 22:01:21 +0530

    misc: fastrpc: fix improper packet size calculation
    commit 3a1bf591e9a410f220b7405a142a47407394a1d5 upstream.
    
    The buffer list is sorted and this is not being considered while
    calculating packet size. This would lead to improper copy length
    calculation for non-dmaheap buffers which would eventually cause
    sending improper buffers to DSP.
    
    Fixes: c68cfb718c8f ("misc: fastrpc: Add support for context Invoke method")
    Reviewed-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Signed-off-by: Jeya R <jeyr@codeaurora.org>
    Link: https://lore.kernel.org/r/1637771481-4299-1-git-send-email-jeyr@codeaurora.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/fastrpc.c
+++ drivers/misc/fastrpc.c
@@ -717,16 +717,18 @@ static int fastrpc_get_meta_size(struct fastrpc_invoke_ctx *ctx)
 static u64 fastrpc_get_payload_size(struct fastrpc_invoke_ctx *ctx, int metalen)
 {
 	u64 size = 0;
-	int i;
+	int oix;
 
 	size = ALIGN(metalen, FASTRPC_ALIGN);
-	for (i = 0; i < ctx->nscalars; i++) {
+	for (oix = 0; oix < ctx->nbufs; oix++) {
+		int i = ctx->olaps[oix].raix;
+
 		if (ctx->args[i].fd == 0 || ctx->args[i].fd == -1) {
 
-			if (ctx->olaps[i].offset == 0)
+			if (ctx->olaps[oix].offset == 0)
 				size = ALIGN(size, FASTRPC_ALIGN);
 
-			size += (ctx->olaps[i].mend - ctx->olaps[i].mstart);
+			size += (ctx->olaps[oix].mend - ctx->olaps[oix].mstart);
 		}
 	}
 
