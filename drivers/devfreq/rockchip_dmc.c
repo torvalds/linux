5c4f1d6e3d69521cbd6eb8b5d9d412651971080f
YouMin Chen <cym@rock-chips.com>
2021-11-25 20:13:31 +0800

    PM / devfreq: rockchip_dmc: add support for rk3588
    Signed-off-by: YouMin Chen <cym@rock-chips.com>
    Change-Id: I2869e9a9afa3531d8300ef04393f0e6ea61626d2

--- drivers/devfreq/rockchip_dmc.c
+++ drivers/devfreq/rockchip_dmc.c
@@ -1800,6 +1800,74 @@ static __maybe_unused int rk3568_dmc_init(struct platform_device *pdev,
 	return 0;
 }
 
+static __maybe_unused int rk3588_dmc_init(struct platform_device *pdev,
+					  struct rockchip_dmcfreq *dmcfreq)
+{
+	struct arm_smccc_res res;
+	int ret;
+	int complt_irq;
+
+	res = sip_smc_dram(0, 0, ROCKCHIP_SIP_CONFIG_DRAM_GET_VERSION);
+	dev_notice(&pdev->dev, "current ATF version 0x%lx\n", res.a1);
+	if (res.a0) {
+		dev_err(&pdev->dev, "trusted firmware unsupported, please update.\n");
+		return -ENXIO;
+	}
+
+	/*
+	 * first 4KB is used for interface parameters
+	 * after 4KB is dts parameters
+	 * request share memory size 4KB * 2
+	 */
+	res = sip_smc_request_share_mem(2, SHARE_PAGE_TYPE_DDR);
+	if (res.a0 != 0) {
+		dev_err(&pdev->dev, "no ATF memory for init\n");
+		return -ENOMEM;
+	}
+	ddr_psci_param = (struct share_params *)res.a1;
+	/* Clear ddr_psci_param, size is 4KB * 2 */
+	memset_io(ddr_psci_param, 0x0, 4096 * 2);
+
+	/* start mcu with sip_smc_dram */
+	wait_ctrl.dcf_en = 2;
+
+	init_waitqueue_head(&wait_ctrl.wait_wq);
+	wait_ctrl.wait_en = 1;
+	wait_ctrl.wait_time_out_ms = 17 * 5;
+
+	complt_irq = platform_get_irq_byname(pdev, "complete");
+	if (complt_irq < 0) {
+		dev_err(&pdev->dev, "no IRQ for complt_irq: %d\n", complt_irq);
+		return complt_irq;
+	}
+	wait_ctrl.complt_irq = complt_irq;
+
+	ret = devm_request_irq(&pdev->dev, complt_irq, wait_dcf_complete_irq,
+			       0, dev_name(&pdev->dev), &wait_ctrl);
+	if (ret < 0) {
+		dev_err(&pdev->dev, "cannot request complt_irq\n");
+		return ret;
+	}
+	disable_irq(complt_irq);
+
+	res = sip_smc_dram(SHARE_PAGE_TYPE_DDR, 0, ROCKCHIP_SIP_CONFIG_DRAM_INIT);
+	if (res.a0) {
+		dev_err(&pdev->dev, "rockchip_sip_config_dram_init error:%lx\n", res.a0);
+		return -ENOMEM;
+	}
+
+	ret = rockchip_get_freq_info(dmcfreq);
+	if (ret < 0) {
+		dev_err(&pdev->dev, "cannot get frequency info\n");
+		return ret;
+	}
+	dmcfreq->is_set_rate_direct = true;
+
+	dmcfreq->set_auto_self_refresh = rockchip_ddr_set_auto_self_refresh;
+
+	return 0;
+}
+
 static __maybe_unused int rv1126_dmc_init(struct platform_device *pdev,
 					  struct rockchip_dmcfreq *dmcfreq)
 {
@@ -1915,6 +1983,9 @@ static const struct of_device_id rockchip_dmcfreq_of_match[] = {
 #endif
 #if IS_ENABLED(CONFIG_CPU_RK3568)
 	{ .compatible = "rockchip,rk3568-dmc", .data = rk3568_dmc_init },
+#endif
+#if IS_ENABLED(CONFIG_CPU_RK3588)
+	{ .compatible = "rockchip,rk3588-dmc", .data = rk3588_dmc_init },
 #endif
 #if IS_ENABLED(CONFIG_CPU_RV1126)
 	{ .compatible = "rockchip,rv1126-dmc", .data = rv1126_dmc_init },
@@ -2160,6 +2231,8 @@ static int rockchip_get_system_status_level(struct device_node *np,
 	} else {
 		return -EINVAL;
 	}
+
+	dmcfreq->auto_min_rate = dmcfreq->rate_low;
 
 	for (i = 0; i < count / 2; i++) {
 		of_property_read_u32_index(np, porp_name, 2 * i,
@@ -2733,9 +2806,11 @@ static void rockchip_dmcfreq_parse_dt(struct rockchip_dmcfreq *dmcfreq)
 	if (dmcfreq->info.auto_freq_en)
 		of_property_read_u32(np, "auto-freq-en",
 				     &dmcfreq->info.auto_freq_en);
-	of_property_read_u32(np, "auto-min-freq",
-			     (u32 *)&dmcfreq->auto_min_rate);
-	dmcfreq->auto_min_rate *= 1000;
+	if (!dmcfreq->auto_min_rate) {
+		of_property_read_u32(np, "auto-min-freq",
+				     (u32 *)&dmcfreq->auto_min_rate);
+		dmcfreq->auto_min_rate *= 1000;
+	}
 
 	if (rockchip_get_freq_map_talbe(np, "cpu-bw-dmc-freq",
 					&dmcfreq->cpu_bw_tbl))
