13117fe7af4edffdfcbb38a0c95d309541560747
Jeremy Kerr <jk@codeconstruct.com.au>
2021-11-25 14:07:37 +0800

    mctp: serial: cancel tx work on ldisc close
    We want to ensure that the tx work has finished before returning from
    the ldisc close op, so do a synchronous cancel.
    
    OpenBMC-Staging-Count: 1
    Reported-by: Jiri Slaby <jirislaby@kernel.org>
    Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 7bd9890f3d74e96f0e1a898f68decfc711de3001)
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- drivers/net/mctp/mctp-serial.c
+++ drivers/net/mctp/mctp-serial.c
@@ -479,6 +479,7 @@ static void mctp_serial_close(struct tty_struct *tty)
 	int idx = dev->idx;
 
 	unregister_netdev(dev->netdev);
+	cancel_work_sync(&dev->tx_work);
 	ida_free(&mctp_serial_ida, idx);
 }
 
