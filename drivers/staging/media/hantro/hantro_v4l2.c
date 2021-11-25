d912740881d5f7c13247ec714e9bc59b7cc1aace
Hans Verkuil <hverkuil-cisco@xs4all.nl>
2021-11-25 16:42:10 +0100

    media: hantro: drop unused vb2 headers
    hantro_v4l2.c includes media/videobuf2-dma-sg.h, which isn't used.
    And in fact, hantro doesn't use the vb2_dma_sg_memops at all, so this
    is confusing. Just drop both videobuf2 headers, they are not needed here.
    
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/staging/media/hantro/hantro_v4l2.c
+++ drivers/staging/media/hantro/hantro_v4l2.c
@@ -23,8 +23,6 @@
 #include <media/v4l2-ctrls.h>
 #include <media/v4l2-event.h>
 #include <media/v4l2-mem2mem.h>
-#include <media/videobuf2-core.h>
-#include <media/videobuf2-dma-sg.h>
 
 #include "hantro.h"
 #include "hantro_hw.h"
