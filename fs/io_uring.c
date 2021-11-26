3d2a1e68fd9904fdc1b02f2e7d40ca47df7ba39f
Pavel Begunkov <asml.silence@gmail.com>
2021-11-26 14:38:14 +0000

    io_uring: fail cancellation for EXITING tasks
    commit 617a89484debcd4e7999796d693cf0b77d2519de upstream.
    
    WARNING: CPU: 1 PID: 20 at fs/io_uring.c:6269 io_try_cancel_userdata+0x3c5/0x640 fs/io_uring.c:6269
    CPU: 1 PID: 20 Comm: kworker/1:0 Not tainted 5.16.0-rc1-syzkaller #0
    Workqueue: events io_fallback_req_func
    RIP: 0010:io_try_cancel_userdata+0x3c5/0x640 fs/io_uring.c:6269
    Call Trace:
     <TASK>
     io_req_task_link_timeout+0x6b/0x1e0 fs/io_uring.c:6886
     io_fallback_req_func+0xf9/0x1ae fs/io_uring.c:1334
     process_one_work+0x9b2/0x1690 kernel/workqueue.c:2298
     worker_thread+0x658/0x11f0 kernel/workqueue.c:2445
     kthread+0x405/0x4f0 kernel/kthread.c:327
     ret_from_fork+0x1f/0x30 arch/x86/entry/entry_64.S:295
     </TASK>
    
    We need original task's context to do cancellations, so if it's dying
    and the callback is executed in a fallback mode, fail the cancellation
    attempt.
    
    Fixes: 89b263f6d56e6 ("io_uring: run linked timeouts from task_work")
    Cc: stable@kernel.org # 5.15+
    Reported-by: syzbot+ab0cfe96c2b3cd1c1153@syzkaller.appspotmail.com
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/4c41c5f379c6941ad5a07cd48cb66ed62199cf7e.1637937097.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -6886,10 +6886,11 @@ static inline struct file *io_file_get(struct io_ring_ctx *ctx,
 static void io_req_task_link_timeout(struct io_kiocb *req, bool *locked)
 {
 	struct io_kiocb *prev = req->timeout.prev;
-	int ret;
+	int ret = -ENOENT;
 
 	if (prev) {
-		ret = io_try_cancel_userdata(req, prev->user_data);
+		if (!(req->task->flags & PF_EXITING))
+			ret = io_try_cancel_userdata(req, prev->user_data);
 		io_req_complete_post(req, ret ?: -ETIME, 0);
 		io_put_req(prev);
 	} else {
