5d4be19cbe6aadfad0a5f40df91bd478cedd8344
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2021-11-29 16:35:35 +0530

    bus: mhi: ep: Add uevent support for module autoloading
    Add uevent support to MHI endpoint bus so that the client drivers can be
    autoloaded by udev when the MHI endpoint devices gets created. The client
    drivers are expected to provide MODULE_DEVICE_TABLE with the MHI id_table
    struct so that the alias can be exported.
    
    The MHI endpoint reused the mhi_device_id structure of the MHI bus.
    
    Reviewed-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

--- drivers/bus/mhi/ep/main.c
+++ drivers/bus/mhi/ep/main.c
@@ -1536,6 +1536,14 @@ void mhi_ep_driver_unregister(struct mhi_ep_driver *mhi_drv)
 }
 EXPORT_SYMBOL_GPL(mhi_ep_driver_unregister);
 
+static int mhi_ep_uevent(struct device *dev, struct kobj_uevent_env *env)
+{
+	struct mhi_ep_device *mhi_dev = to_mhi_ep_device(dev);
+
+	return add_uevent_var(env, "MODALIAS=" MHI_EP_DEVICE_MODALIAS_FMT,
+					mhi_dev->name);
+}
+
 static int mhi_ep_match(struct device *dev, struct device_driver *drv)
 {
 	struct mhi_ep_device *mhi_dev = to_mhi_ep_device(dev);
@@ -1562,6 +1570,7 @@ struct bus_type mhi_ep_bus_type = {
 	.name = "mhi_ep",
 	.dev_name = "mhi_ep",
 	.match = mhi_ep_match,
+	.uevent = mhi_ep_uevent,
 };
 
 static int __init mhi_ep_init(void)
