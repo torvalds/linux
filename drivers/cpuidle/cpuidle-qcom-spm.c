0ee30ace67e425ab83a1673bf51f50b577328cf9
Stephan Gerhold <stephan@gerhold.net>
2021-12-01 14:05:02 +0100

    cpuidle: qcom-spm: Check if any CPU is managed by SPM
    At the moment, the "qcom-spm-cpuidle" platform device is always created,
    even if none of the CPUs is actually managed by the SPM. On non-qcom
    platforms this will result in infinite probe-deferral due to the
    failing qcom_scm_is_available() call.
    
    To avoid this, look through the CPU DT nodes and check if there is
    actually any CPU managed by a SPM (as indicated by the qcom,saw property).
    It should also be available because e.g. MSM8916 has qcom,saw defined
    but it's typically not enabled with ARM64/PSCI firmwares.
    
    This is needed in preparation of a follow-up change that calls
    qcom_scm_set_warm_boot_addr() a single time before registering any
    cpuidle drivers. Otherwise this call might be made even on devices
    that have this driver enabled but actually make use of PSCI.
    
    Fixes: 60f3692b5f0b ("cpuidle: qcom_spm: Detach state machine from main SPM handling")
    Reported-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Link: https://lore.kernel.org/r/86e3e09f-a8d7-3dff-3fc6-ddd7d30c5d78@samsung.com/
    Signed-off-by: Stephan Gerhold <stephan@gerhold.net>
    Tested-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Link: https://lore.kernel.org/r/20211201130505.257379-2-stephan@gerhold.net

--- drivers/cpuidle/cpuidle-qcom-spm.c
+++ drivers/cpuidle/cpuidle-qcom-spm.c
@@ -155,6 +155,22 @@ static struct platform_driver spm_cpuidle_driver = {
 	},
 };
 
+static bool __init qcom_spm_find_any_cpu(void)
+{
+	struct device_node *cpu_node, *saw_node;
+
+	for_each_of_cpu_node(cpu_node) {
+		saw_node = of_parse_phandle(cpu_node, "qcom,saw", 0);
+		if (of_device_is_available(saw_node)) {
+			of_node_put(saw_node);
+			of_node_put(cpu_node);
+			return true;
+		}
+		of_node_put(saw_node);
+	}
+	return false;
+}
+
 static int __init qcom_spm_cpuidle_init(void)
 {
 	struct platform_device *pdev;
@@ -163,6 +179,10 @@ static int __init qcom_spm_cpuidle_init(void)
 	ret = platform_driver_register(&spm_cpuidle_driver);
 	if (ret)
 		return ret;
+
+	/* Make sure there is actually any CPU managed by the SPM */
+	if (!qcom_spm_find_any_cpu())
+		return 0;
 
 	pdev = platform_device_register_simple("qcom-spm-cpuidle",
 					       -1, NULL, 0);
