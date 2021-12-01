24aaff6a6ce4c4defd18147f5078223a96283fd7
Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
2021-12-01 16:33:03 +0900

    clk: renesas: cpg-mssr: Add support for R-Car S4-8
    Initial CPG support for R-Car S4-8 (r8a779f0).
    
    Inspired by patches in the BSP by LUU HOAI.
    
    Signed-off-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Link: https://lore.kernel.org/r/20211201073308.1003945-10-yoshihiro.shimoda.uh@renesas.com
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- drivers/clk/renesas/renesas-cpg-mssr.c
+++ drivers/clk/renesas/renesas-cpg-mssr.c
@@ -848,6 +848,12 @@ static const struct of_device_id cpg_mssr_match[] = {
 		.data = &r8a779a0_cpg_mssr_info,
 	},
 #endif
+#ifdef CONFIG_CLK_R8A779F0
+	{
+		.compatible = "renesas,r8a779f0-cpg-mssr",
+		.data = &r8a779f0_cpg_mssr_info,
+	},
+#endif
 	{ /* sentinel */ }
 };
 
