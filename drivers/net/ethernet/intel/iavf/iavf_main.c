d784113217f95df777e32847d0f206d25325b334
Stefan Assmann <sassmann@kpanic.de>
2021-12-01 09:14:34 +0100

    iavf: do not override the adapter state in the watchdog task (again)
    commit fe523d7c9a8332855376ad5eb1aa301091129ba4 upstream.
    
    The watchdog task incorrectly changes the state to __IAVF_RESETTING,
    instead of letting the reset task take care of that. This was already
    resolved by commit 22c8fd71d3a5 ("iavf: do not override the adapter
    state in the watchdog task") but the problem was reintroduced by the
    recent code refactoring in commit 45eebd62999d ("iavf: Refactor iavf
    state machine tracking").
    
    Fixes: 45eebd62999d ("iavf: Refactor iavf state machine tracking")
    Signed-off-by: Stefan Assmann <sassmann@kpanic.de>
    Tested-by: Konrad Jankowski <konrad0.jankowski@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/intel/iavf/iavf_main.c
+++ drivers/net/ethernet/intel/iavf/iavf_main.c
@@ -2110,7 +2110,6 @@ static void iavf_watchdog_task(struct work_struct *work)
 	/* check for hw reset */
 	reg_val = rd32(hw, IAVF_VF_ARQLEN1) & IAVF_VF_ARQLEN1_ARQENABLE_MASK;
 	if (!reg_val) {
-		iavf_change_state(adapter, __IAVF_RESETTING);
 		adapter->flags |= IAVF_FLAG_RESET_PENDING;
 		adapter->aq_required = 0;
 		adapter->current_op = VIRTCHNL_OP_UNKNOWN;
