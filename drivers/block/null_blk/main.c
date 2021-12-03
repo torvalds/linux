2385ebf38f94d4f7761b1e9a4973d04753da02c2
Ming Lei <ming.lei@redhat.com>
2021-12-03 16:17:03 +0800

    block: null_blk: batched complete poll requests
    Complete poll requests via blk_mq_add_to_batch() and
    blk_mq_end_request_batch(), so that we can cover batched complete
    code path by running null_blk test.
    
    Meantime this way shows ~14% IOPS boost on 't/io_uring /dev/nullb0'
    in my test.
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Link: https://lore.kernel.org/r/20211203081703.3506020-1-ming.lei@redhat.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/null_blk/main.c
+++ drivers/block/null_blk/main.c
@@ -1574,7 +1574,9 @@ static int null_poll(struct blk_mq_hw_ctx *hctx, struct io_comp_batch *iob)
 		cmd = blk_mq_rq_to_pdu(req);
 		cmd->error = null_process_cmd(cmd, req_op(req), blk_rq_pos(req),
 						blk_rq_sectors(req));
-		end_cmd(cmd);
+		if (!blk_mq_add_to_batch(req, iob, cmd->error,
+					blk_mq_end_request_batch))
+			end_cmd(cmd);
 		nr++;
 	}
 
