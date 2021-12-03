9d9bcae47fd5a0b827521f65ab7d10a218eacc37
Hans de Goede <hdegoede@redhat.com>
2021-12-03 11:28:44 +0100

    ACPI: delay enumeration of devices with a _DEP pointing to an INT3472 device
    The clk and regulator frameworks expect clk/regulator consumer-devices
    to have info about the consumed clks/regulators described in the device's
    fw_node.
    
    To work around cases where this info is not present in the firmware tables,
    which is often the case on x86/ACPI devices, both frameworks allow the
    provider-driver to attach info about consumers to the clks/regulators
    when registering these.
    
    This causes problems with the probe ordering wrt drivers for consumers
    of these clks/regulators. Since the lookups are only registered when the
    provider-driver binds, trying to get these clks/regulators before then
    results in a -ENOENT error for clks and a dummy regulator for regulators.
    
    One case where we hit this issue is camera sensors such as e.g. the OV8865
    sensor found on the Microsoft Surface Go. The sensor uses clks, regulators
    and GPIOs provided by a TPS68470 PMIC which is described in an INT3472
    ACPI device. There is special platform code handling this and setting
    platform_data with the necessary consumer info on the MFD cells
    instantiated for the PMIC under: drivers/platform/x86/intel/int3472.
    
    For this to work properly the ov8865 driver must not bind to the I2C-client
    for the OV8865 sensor until after the TPS68470 PMIC gpio, regulator and
    clk MFD cells have all been fully setup.
    
    The OV8865 on the Microsoft Surface Go is just one example, all X86
    devices using the Intel IPU3 camera block found on recent Intel SoCs
    have similar issues where there is an INT3472 HID ACPI-device, which
    describes the clks and regulators, and the driver for this INT3472 device
    must be fully initialized before the sensor driver (any sensor driver)
    binds for things to work properly.
    
    On these devices the ACPI nodes describing the sensors all have a _DEP
    dependency on the matching INT3472 ACPI device (there is one per sensor).
    
    This allows solving the probe-ordering problem by delaying the enumeration
    (instantiation of the I2C-client in the ov8865 example) of ACPI-devices
    which have a _DEP dependency on an INT3472 device.
    
    The new acpi_dev_ready_for_enumeration() helper used for this is also
    exported because for devices, which have the enumeration_by_parent flag
    set, the parent-driver will do its own scan of child ACPI devices and
    it will try to enumerate those during its probe(). Code doing this such
    as e.g. the i2c-core-acpi.c code must call this new helper to ensure
    that it too delays the enumeration until all the _DEP dependencies are
    met on devices which have the new honor_deps flag set.
    
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://lore.kernel.org/r/20211203102857.44539-2-hdegoede@redhat.com

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -797,6 +797,12 @@ static const char * const acpi_ignore_dep_ids[] = {
 	NULL
 };
 
+/* List of HIDs for which we honor deps of matching ACPI devs, when checking _DEP lists. */
+static const char * const acpi_honor_dep_ids[] = {
+	"INT3472", /* Camera sensor PMIC / clk and regulator info */
+	NULL
+};
+
 static struct acpi_device *acpi_bus_get_parent(acpi_handle handle)
 {
 	struct acpi_device *device = NULL;
@@ -1762,8 +1768,12 @@ static void acpi_scan_dep_init(struct acpi_device *adev)
 	struct acpi_dep_data *dep;
 
 	list_for_each_entry(dep, &acpi_dep_list, node) {
-		if (dep->consumer == adev->handle)
+		if (dep->consumer == adev->handle) {
+			if (dep->honor_dep)
+				adev->flags.honor_deps = 1;
+
 			adev->dep_unmet++;
+		}
 	}
 }
 
@@ -1967,7 +1977,7 @@ static u32 acpi_scan_check_dep(acpi_handle handle, bool check_dep)
 	for (count = 0, i = 0; i < dep_devices.count; i++) {
 		struct acpi_device_info *info;
 		struct acpi_dep_data *dep;
-		bool skip;
+		bool skip, honor_dep;
 
 		status = acpi_get_object_info(dep_devices.handles[i], &info);
 		if (ACPI_FAILURE(status)) {
@@ -1976,6 +1986,7 @@ static u32 acpi_scan_check_dep(acpi_handle handle, bool check_dep)
 		}
 
 		skip = acpi_info_matches_ids(info, acpi_ignore_dep_ids);
+		honor_dep = acpi_info_matches_ids(info, acpi_honor_dep_ids);
 		kfree(info);
 
 		if (skip)
@@ -1989,6 +2000,7 @@ static u32 acpi_scan_check_dep(acpi_handle handle, bool check_dep)
 
 		dep->supplier = dep_devices.handles[i];
 		dep->consumer = handle;
+		dep->honor_dep = honor_dep;
 
 		mutex_lock(&acpi_dep_list_lock);
 		list_add_tail(&dep->node , &acpi_dep_list);
@@ -2155,8 +2167,8 @@ static void acpi_bus_attach(struct acpi_device *device, bool first_pass)
 		register_dock_dependent_device(device, ejd);
 
 	acpi_bus_get_status(device);
-	/* Skip devices that are not present. */
-	if (!acpi_device_is_present(device)) {
+	/* Skip devices that are not ready for enumeration (e.g. not present) */
+	if (!acpi_dev_ready_for_enumeration(device)) {
 		device->flags.initialized = false;
 		acpi_device_clear_enumerated(device);
 		device->flags.power_manageable = 0;
@@ -2317,6 +2329,23 @@ void acpi_dev_clear_dependencies(struct acpi_device *supplier)
 	acpi_walk_dep_device_list(supplier->handle, acpi_scan_clear_dep, NULL);
 }
 EXPORT_SYMBOL_GPL(acpi_dev_clear_dependencies);
+
+/**
+ * acpi_dev_ready_for_enumeration - Check if the ACPI device is ready for enumeration
+ * @device: Pointer to the &struct acpi_device to check
+ *
+ * Check if the device is present and has no unmet dependencies.
+ *
+ * Return true if the device is ready for enumeratino. Otherwise, return false.
+ */
+bool acpi_dev_ready_for_enumeration(const struct acpi_device *device)
+{
+	if (device->flags.honor_deps && device->dep_unmet)
+		return false;
+
+	return acpi_device_is_present(device);
+}
+EXPORT_SYMBOL_GPL(acpi_dev_ready_for_enumeration);
 
 /**
  * acpi_dev_get_first_consumer_dev - Return ACPI device dependent on @supplier
