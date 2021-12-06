21a6732f464894fa43fa1d43fdc7570b454b970c
Christian König <christian.koenig@amd.com>
2021-12-06 09:42:02 +0100

    drm/amdgpu: don't skip runtime pm get on A+A config
    The runtime PM get was incorrectly added after the check.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Acked-by: Evan Quan <evan.quan@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211206084551.92502-1-christian.koenig@amd.com

--- drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.c
@@ -61,9 +61,6 @@ static int amdgpu_dma_buf_attach(struct dma_buf *dmabuf,
 	if (pci_p2pdma_distance_many(adev->pdev, &attach->dev, 1, true) < 0)
 		attach->peer2peer = false;
 
-	if (attach->dev->driver == adev->dev->driver)
-		return 0;
-
 	r = pm_runtime_get_sync(adev_to_drm(adev)->dev);
 	if (r < 0)
 		goto out;
