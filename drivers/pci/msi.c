7112158d97a1539b217c360d379724ac3ee99fa3
Thomas Gleixner <tglx@linutronix.de>
2021-12-06 23:27:46 +0100

    PCI/MSI: Make msix_update_entries() smarter
    No need to walk the descriptors and check for each one whether the entries
    pointer function argument is NULL. Do it once.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Juergen Gross <jgross@suse.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Acked-by: Bjorn Helgaas <bhelgaas@google.com>
    Link: https://lore.kernel.org/r/20211206210224.600351129@linutronix.de

--- drivers/pci/msi.c
+++ drivers/pci/msi.c
@@ -642,8 +642,8 @@ static void msix_update_entries(struct pci_dev *dev, struct msix_entry *entries)
 {
 	struct msi_desc *entry;
 
-	for_each_pci_msi_entry(entry, dev) {
-		if (entries) {
+	if (entries) {
+		for_each_pci_msi_entry(entry, dev) {
 			entries->vector = entry->irq;
 			entries++;
 		}
