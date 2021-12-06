73f3760eddc9bc32c207fff06537f98f94bef451
Ming Lei <ming.lei@redhat.com>
2021-12-06 11:33:50 +0800

    blk-mq: don't use plug->mq_list->q directly in blk_mq_run_dispatch_ops()
    blk_mq_run_dispatch_ops() is defined as one macro, and plug->mq_list
    will be changed when running 'dispatch_ops', so add one local variable
    for holding request queue.
    
    Reported-and-tested-by: Yi Zhang <yi.zhang@redhat.com>
    Fixes: 4cafe86c9267 ("blk-mq: run dispatch lock once in case of issuing from list")
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -2521,7 +2521,9 @@ void blk_mq_flush_plug_list(struct blk_plug *plug, bool from_schedule)
 	plug->rq_count = 0;
 
 	if (!plug->multiple_queues && !plug->has_elevator && !from_schedule) {
-		blk_mq_run_dispatch_ops(plug->mq_list->q,
+		struct request_queue *q = rq_list_peek(&plug->mq_list)->q;
+
+		blk_mq_run_dispatch_ops(q,
 				blk_mq_plug_issue_direct(plug, false));
 		if (rq_list_empty(plug->mq_list))
 			return;
