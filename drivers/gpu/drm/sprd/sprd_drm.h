43531edd53f07cbe977a0b33dea6dd6c29b21fea
Kevin Tang <kevin3.tang@gmail.com>
2021-12-07 22:27:13 +0800

    drm/sprd: add Unisoc's drm kms master
    Adds drm support for the Unisoc's display subsystem.
    
    This is drm kms driver, this driver provides support for the
    application framework in Android, Yocto and more.
    
    Application framework can access Unisoc's display internal
    peripherals through libdrm or libkms, it's test ok by modetest
    (DRM/KMS test tool) and Android HWComposer.
    
    v4:
      - Move the devm_drm_dev_alloc to master_ops->bind function.
      - The managed drmm_mode_config_init() it is no longer necessary for drivers to explicitly call drm_mode_config_cleanup, so delete it.
    
    v5:
      - Remove subdir-ccflgas-y for Makefile.
      - Keep the selects sorted by alphabet for Kconfig.
    
    Cc: Orson Zhai <orsonzhai@gmail.com>
    Cc: Chunyan Zhang <zhang.lyra@gmail.com>
    Signed-off-by: Kevin Tang <kevin.tang@unisoc.com>
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211207142717.30296-3-kevin3.tang@gmail.com

---
+++ drivers/gpu/drm/sprd/sprd_drm.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2020 Unisoc Inc.
+ */
+
+#ifndef _SPRD_DRM_H_
+#define _SPRD_DRM_H_
+
+#include <drm/drm_atomic.h>
+#include <drm/drm_print.h>
+
+struct sprd_drm {
+	struct drm_device drm;
+};
+
+#endif /* _SPRD_DRM_H_ */
