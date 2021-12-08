0ef42fb749b17f7e49adef047ece5bebac5d6795
Chris Wilson <chris@chris-wilson.co.uk>
2021-12-08 21:04:02 +0530

    drm/i915: Exclude reserved stolen from driver use
    Remove the portion of stolen memory reserved for private use from driver
    access.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    cc: Matthew Auld <matthew.auld@intel.com>
    Signed-off-by: Ramalingam C <ramalingam.c@intel.com>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Reviewed-by: Andi Shyti <andi.shyti@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211208153404.27546-2-ramalingam.c@intel.com

--- drivers/gpu/drm/i915/gem/i915_gem_stolen.c
+++ drivers/gpu/drm/i915/gem/i915_gem_stolen.c
@@ -488,6 +488,9 @@ static int i915_gem_init_stolen(struct intel_memory_region *mem)
 		return 0;
 	}
 
+	/* Exclude the reserved region from driver use */
+	mem->region.end = reserved_base - 1;
+
 	/* It is possible for the reserved area to end before the end of stolen
 	 * memory, so just consider the start. */
 	reserved_total = stolen_top - reserved_base;
