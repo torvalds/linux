d7678a7a55d063a67b5b3432f9ca97c7785bc255
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2021-12-08 13:17:56 +0100

    media: i2c: max9286: Use dev_err_probe() helper
    [ Upstream commit df78b858e773967112b4444400fb7bb5bd7a9d8a ]
    
    Use the dev_err_probe() helper, instead of open-coding the same
    operation. While at it retrieve the error once and use it from
    'ret' instead of retrieving it twice.
    
    Link: https://lore.kernel.org/linux-media/20211208121756.3051565-1-niklas.soderlund+renesas@ragnatech.se
    Suggested-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Niklas S\xF6derlund <niklas.soderlund+renesas@ragnatech.se>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/media/i2c/max9286.c
+++ drivers/media/i2c/max9286.c
@@ -1296,11 +1296,9 @@ static int max9286_probe(struct i2c_client *client)
 
 	priv->regulator = devm_regulator_get(&client->dev, "poc");
 	if (IS_ERR(priv->regulator)) {
-		if (PTR_ERR(priv->regulator) != -EPROBE_DEFER)
-			dev_err(&client->dev,
-				"Unable to get PoC regulator (%ld)\n",
-				PTR_ERR(priv->regulator));
 		ret = PTR_ERR(priv->regulator);
+		dev_err_probe(&client->dev, ret,
+			      "Unable to get PoC regulator\n");
 		goto err_powerdown;
 	}
 
