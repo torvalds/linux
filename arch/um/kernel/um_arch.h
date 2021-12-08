361640b4fdc86167b0c25d8e73c08dcaa4ecd28a
Vincent Whitchurch <vincent.whitchurch@axis.com>
2021-12-08 16:11:22 +0100

    um: Extract load file helper from initrd.c
    The file loading support in initrd.c can be re-used for
    loading devicetrees.  Move it out of initrd.c.
    
    Signed-off-by: Vincent Whitchurch <vincent.whitchurch@axis.com>
    Signed-off-by: Richard Weinberger <richard@nod.at>

---
+++ arch/um/kernel/um_arch.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef __UML_ARCH_H__
+#define __UML_ARCH_H__
+
+extern void * __init uml_load_file(const char *filename, unsigned long long *size);
+
+#endif
