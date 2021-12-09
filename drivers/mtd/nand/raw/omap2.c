0137c74ad87316305599df8ada54de5273d868ec
Roger Quadros <rogerq@kernel.org>
2021-12-09 11:04:56 +0200

    mtd: rawnand: omap2: Add compatible for AM64 SoC
    AM64 SoC contains the GPMC NAND controller. Add compatible for it.
    
    Signed-off-by: Roger Quadros <rogerq@kernel.org>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Link: https://lore.kernel.org/linux-mtd/20211209090458.24830-5-rogerq@kernel.org

--- drivers/mtd/nand/raw/omap2.c
+++ drivers/mtd/nand/raw/omap2.c
@@ -2286,6 +2286,7 @@ static int omap_nand_remove(struct platform_device *pdev)
 
 static const struct of_device_id omap_nand_ids[] = {
 	{ .compatible = "ti,omap2-nand", },
+	{ .compatible = "ti,am64-nand", },
 	{},
 };
 MODULE_DEVICE_TABLE(of, omap_nand_ids);
