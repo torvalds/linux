dc1b242478f47d04a41f319a14f163711c52646a
Linus Walleij <linus.walleij@linaro.org>
2021-12-09 14:45:13 +0100

    pinctrl: bcm2835: Silence uninit warning
    The uninitialized variable would be caught by the BUG_ON()
    logic below, but the kernel test robot cannot see that.
    Silence the warning by initializing the variable.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Link: https://lore.kernel.org/r/20211209134513.306212-1-linus.walleij@linaro.org
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/bcm/pinctrl-bcm2835.c
+++ drivers/pinctrl/bcm/pinctrl-bcm2835.c
@@ -407,7 +407,7 @@ static void bcm2835_gpio_irq_handler(struct irq_desc *desc)
 	struct bcm2835_pinctrl *pc = gpiochip_get_data(chip);
 	struct irq_chip *host_chip = irq_desc_get_chip(desc);
 	int irq = irq_desc_get_irq(desc);
-	int group;
+	int group = 0;
 	int i;
 
 	for (i = 0; i < BCM2835_NUM_IRQS; i++) {
