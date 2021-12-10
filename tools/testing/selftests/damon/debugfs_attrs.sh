c6980e30af356e85699f37142ae435a6aa483ceb
SeongJae Park <sj@kernel.org>
2021-12-10 14:46:49 -0800

    selftests/damon: test DAMON enabling with empty target_ids case
    DAMON debugfs didn't check empty targets when starting monitoring, and
    the issue is fixed with commit b5ca3e83ddb0 ("mm/damon/dbgfs: add
    adaptive_targets list check before enable monitor_on").  To avoid future
    regression, this commit adds a test case for that in DAMON selftests.
    
    Link: https://lkml.kernel.org/r/20211201150440.1088-9-sj@kernel.org
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Cc: Brendan Higgins <brendanhiggins@google.com>
    Cc: Shuah Khan <shuah@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- tools/testing/selftests/damon/debugfs_attrs.sh
+++ tools/testing/selftests/damon/debugfs_attrs.sh
@@ -94,4 +94,13 @@ test_write_succ "$file" "" "$orig_content" "empty input"
 test_content "$file" "$orig_content" "" "empty input written"
 echo "$orig_content" > "$file"
 
+# Test empty targets case
+# =======================
+
+orig_target_ids=$(cat "$DBGFS/target_ids")
+echo "" > "$DBGFS/target_ids"
+orig_monitor_on=$(cat "$DBGFS/monitor_on")
+test_write_fail "$DBGFS/monitor_on" "on" "orig_monitor_on" "empty target ids"
+echo "$orig_target_ids" > "$DBGFS/target_ids"
+
 echo "PASS"
