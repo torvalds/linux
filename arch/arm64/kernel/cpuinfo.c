18c4e4fa56e55a5a8cf0c479011525cf35e3a644
Joey Gouly <joey.gouly@arm.com>
2021-12-10 16:54:30 +0000

    UPSTREAM: arm64: cpufeature: add HWCAP for FEAT_AFP
    commit 5c13f042e73200b50573ace63e1a6b94e2917616 upstream.
    
    Add a new HWCAP to detect the Alternate Floating-point Behaviour
    feature (FEAT_AFP), introduced in Armv8.7.
    
    Also expose this to userspace in the ID_AA64MMFR1_EL1 feature register.
    
    Bug: 215557547
    Signed-off-by: Joey Gouly <joey.gouly@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Acked-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20211210165432.8106-2-joey.gouly@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Ifb7685f6d97cb469262a3766daab71d09f293b1b

--- arch/arm64/kernel/cpuinfo.c
+++ arch/arm64/kernel/cpuinfo.c
@@ -95,6 +95,7 @@ static const char *const hwcap_str[] = {
 	[KERNEL_HWCAP_BTI]		= "bti",
 	[KERNEL_HWCAP_MTE]		= "mte",
 	[KERNEL_HWCAP_ECV]		= "ecv",
+	[KERNEL_HWCAP_AFP]		= "afp",
 };
 
 #ifdef CONFIG_COMPAT
