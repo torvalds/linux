6180c780e64ceb8dd23345b586dfa70c0a1536ee
Xin Long <lucien.xin@gmail.com>
2021-12-10 13:50:40 -0500

    tipc: discard MSG_CRYPTO msgs when key_exchange_enabled is not set
    When key_exchange is disabled, there is no reason to accept MSG_CRYPTO
    msgs if it doesn't send MSG_CRYPTO msgs.
    
    Signed-off-by: Xin Long <lucien.xin@gmail.com>
    Acked-by: Jon Maloy <jmaloy@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/tipc/link.c
+++ net/tipc/link.c
@@ -1298,7 +1298,8 @@ static bool tipc_data_input(struct tipc_link *l, struct sk_buff *skb,
 		return false;
 #ifdef CONFIG_TIPC_CRYPTO
 	case MSG_CRYPTO:
-		if (TIPC_SKB_CB(skb)->decrypted) {
+		if (sysctl_tipc_key_exchange_enabled &&
+		    TIPC_SKB_CB(skb)->decrypted) {
 			tipc_crypto_msg_rcv(l->net, skb);
 			return true;
 		}
