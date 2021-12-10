d85570c655cc2c257b7da37a6d1fa4c59443c055
SeongJae Park <sj@kernel.org>
2021-12-10 14:46:52 -0800

    selftests/damon: test wrong DAMOS condition ranges input
    A patch titled "mm/damon/schemes: add the validity judgment of
    thresholds"[1] makes DAMON debugfs interface to validate DAMON scheme
    inputs.  This commit adds a test case for the validation logic in DAMON
    selftests.
    
    [1] https://lore.kernel.org/linux-mm/d78360e52158d786fcbf20bc62c96785742e76d3.1637239568.git.xhao@linux.alibaba.com/
    
    Link: https://lkml.kernel.org/r/20211201150440.1088-10-sj@kernel.org
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Cc: Brendan Higgins <brendanhiggins@google.com>
    Cc: Shuah Khan <shuah@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- tools/testing/selftests/damon/debugfs_attrs.sh
+++ tools/testing/selftests/damon/debugfs_attrs.sh
@@ -77,6 +77,8 @@ test_write_succ "$file" "1 2 3 4 5 6 4 0 0 0 1 2 3 1 100 3 2 1" \
 test_write_fail "$file" "1 2
 3 4 5 6 3 0 0 0 1 2 3 1 100 3 2 1" "$orig_content" "multi lines"
 test_write_succ "$file" "" "$orig_content" "disabling"
+test_write_fail "$file" "2 1 2 1 10 1 3 10 1 1 1 1 1 1 1 1 2 3" \
+	"$orig_content" "wrong condition ranges"
 echo "$orig_content" > "$file"
 
 # Test target_ids file
