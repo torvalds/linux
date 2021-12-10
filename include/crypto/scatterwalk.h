244d22ffd656bc8e5c49a2cd2fdfeb0e52a9730f
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2021-12-10 16:30:09 +0200

    crypto: api - Replace kernel.h with the necessary inclusions
    When kernel.h is used in the headers it adds a lot into dependency hell,
    especially when there are circular dependencies are involved.
    
    Replace kernel.h inclusion with the list of what is really being used.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/scatterwalk.h
+++ include/crypto/scatterwalk.h
@@ -12,8 +12,9 @@
 #define _CRYPTO_SCATTERWALK_H
 
 #include <crypto/algapi.h>
+
 #include <linux/highmem.h>
-#include <linux/kernel.h>
+#include <linux/mm.h>
 #include <linux/scatterlist.h>
 
 static inline void scatterwalk_crypto_chain(struct scatterlist *head,
