fc6c01f0cd10b89c4b01dd2940e0b0cda1bd82fb
Yang Shen <shenyang39@huawei.com>
2021-12-11 16:56:55 +0800

    crypto: hisilicon/qm - fix deadlock for remove driver
    When remove the driver and executing the task occur at the same time,
    the following deadlock will be triggered:
    
    Chain exists of:
        sva_lock --> uacce_mutex --> &qm->qps_lock
        Possible unsafe locking scenario:
    		CPU0                    CPU1
    		----                    ----
    	lock(&qm->qps_lock);
    					lock(uacce_mutex);
    					lock(&qm->qps_lock);
    	lock(sva_lock);
    
    And the lock 'qps_lock' is used to protect qp. Therefore, it's reasonable
    cycle is to continue until the qp memory is released. So move the release
    lock infront of 'uacce_remove'.
    
    Signed-off-by: Yang Shen <shenyang39@huawei.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/hisilicon/qm.c
+++ drivers/crypto/hisilicon/qm.c
@@ -3425,6 +3425,7 @@ void hisi_qm_uninit(struct hisi_qm *qm)
 		dma_free_coherent(dev, qm->qdma.size,
 				  qm->qdma.va, qm->qdma.dma);
 	}
+	up_write(&qm->qps_lock);
 
 	qm_irq_unregister(qm);
 	hisi_qm_pci_uninit(qm);
@@ -3432,8 +3433,6 @@ void hisi_qm_uninit(struct hisi_qm *qm)
 		uacce_remove(qm->uacce);
 		qm->uacce = NULL;
 	}
-
-	up_write(&qm->qps_lock);
 }
 EXPORT_SYMBOL_GPL(hisi_qm_uninit);
 
