62cc9a7387f11193ab70a5f99aa8b2206817ce00
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2021-12-12 13:01:15 +0000

    net: axienet: mark as a legacy_pre_march2020 driver
    axienet has a PCS, but does not make use of the phylink PCS support.
    Mark it was a pre-March 2020 driver.
    
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/xilinx/xilinx_axienet_main.c
+++ drivers/net/ethernet/xilinx/xilinx_axienet_main.c
@@ -2051,6 +2051,7 @@ static int axienet_probe(struct platform_device *pdev)
 
 	lp->phylink_config.dev = &ndev->dev;
 	lp->phylink_config.type = PHYLINK_NETDEV;
+	lp->phylink_config.legacy_pre_march2020 = true;
 	lp->phylink_config.mac_capabilities = MAC_SYM_PAUSE | MAC_ASYM_PAUSE |
 		MAC_10FD | MAC_100FD | MAC_1000FD;
 
