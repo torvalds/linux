23686f5ee87076058cb4cc82c5059f4007fc6073
Jaegeuk Kim <jaegeuk@kernel.org>
2021-12-13 14:16:32 -0800

    UPSTREAM: f2fs: do not bother checkpoint by f2fs_get_node_info
    This patch tries to mitigate lock contention between f2fs_write_checkpoint and
    f2fs_get_node_info along with nat_tree_lock.
    
    The idea is, if checkpoint is currently running, other threads that try to grab
    nat_tree_lock would be better to wait for checkpoint.
    
    Bug: 214413989
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit a9419b63bf414775e8aeee95d8c4a5e0df690748
        git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git master)
    Change-Id: I7e253e8bf26012e451330ba6ce00bfc3839bdda3

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1356,7 +1356,7 @@ static int __allocate_data_block(struct dnode_of_data *dn, int seg_type)
 	if (unlikely(is_inode_flag_set(dn->inode, FI_NO_ALLOC)))
 		return -EPERM;
 
-	err = f2fs_get_node_info(sbi, dn->nid, &ni);
+	err = f2fs_get_node_info(sbi, dn->nid, &ni, false);
 	if (err)
 		return err;
 
@@ -1791,7 +1791,7 @@ static int f2fs_xattr_fiemap(struct inode *inode,
 		if (!page)
 			return -ENOMEM;
 
-		err = f2fs_get_node_info(sbi, inode->i_ino, &ni);
+		err = f2fs_get_node_info(sbi, inode->i_ino, &ni, false);
 		if (err) {
 			f2fs_put_page(page, 1);
 			return err;
@@ -1823,7 +1823,7 @@ static int f2fs_xattr_fiemap(struct inode *inode,
 		if (!page)
 			return -ENOMEM;
 
-		err = f2fs_get_node_info(sbi, xnid, &ni);
+		err = f2fs_get_node_info(sbi, xnid, &ni, false);
 		if (err) {
 			f2fs_put_page(page, 1);
 			return err;
@@ -2655,7 +2655,7 @@ got_it:
 		fio->need_lock = LOCK_REQ;
 	}
 
-	err = f2fs_get_node_info(fio->sbi, dn.nid, &ni);
+	err = f2fs_get_node_info(fio->sbi, dn.nid, &ni, false);
 	if (err)
 		goto out_writepage;
 
