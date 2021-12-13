f296a0bcc9611ba96ca3abb3332e21ee600d153f
Prike Liang <Prike.Liang@amd.com>
2021-12-13 16:17:02 +0800

    drm/amd/pm: skip setting gfx cgpg in the s0ix suspend-resume
    In the s0ix entry need retain gfx in the gfxoff state,so here need't
    set gfx cgpg in the S0ix suspend-resume process. Moreover move the S0ix
    check into SMU12 can simplify the code condition check.
    
    Signed-off-by: Prike Liang <Prike.Liang@amd.com>
    Reviewed-by: Evan Quan <evan.quan@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/swsmu/smu12/smu_v12_0.c
+++ drivers/gpu/drm/amd/pm/swsmu/smu12/smu_v12_0.c
@@ -120,7 +120,8 @@ int smu_v12_0_powergate_sdma(struct smu_context *smu, bool gate)
 
 int smu_v12_0_set_gfx_cgpg(struct smu_context *smu, bool enable)
 {
-	if (!(smu->adev->pg_flags & AMD_PG_SUPPORT_GFX_PG))
+	/* Until now the SMU12 only implemented for Renoir series so here neen't do APU check. */
+	if (!(smu->adev->pg_flags & AMD_PG_SUPPORT_GFX_PG) || smu->adev->in_s0ix)
 		return 0;
 
 	return smu_cmn_send_smc_msg_with_param(smu,
