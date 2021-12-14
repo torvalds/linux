1aebf115afd730d1e0f773b8273b04b8681e1128
Sergey Shtylyov <s.shtylyov@omp.ru>
2021-12-14 23:42:46 +0300

    usb: host: ehci-sh: propagate errors from platform_get_irq()
    The driver overrides the error codes and IRQ0 returned by platform_get_irq()
    to -ENODEV.  Switch to propagating the error codes upstream.  IRQ0 is no
    longer returned by platform_get_irq(), so we now can safely ignore it...
    
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Sergey Shtylyov <s.shtylyov@omp.ru>
    Link: https://lore.kernel.org/r/20211214204247.7172-4-s.shtylyov@omp.ru
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/ehci-sh.c
+++ drivers/usb/host/ehci-sh.c
@@ -82,8 +82,8 @@ static int ehci_hcd_sh_probe(struct platform_device *pdev)
 		return -ENODEV;
 
 	irq = platform_get_irq(pdev, 0);
-	if (irq <= 0) {
-		ret = -ENODEV;
+	if (irq < 0) {
+		ret = irq;
 		goto fail_create_hcd;
 	}
 
