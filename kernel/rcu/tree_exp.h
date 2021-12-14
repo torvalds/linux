63c564da11cbed96ec6cf0b5faf6af0b7e3624d2
Paul E. McKenney <paulmck@kernel.org>
2021-12-13 21:00:02 -0800

    rcu: Mark ->expmask access in synchronize_rcu_expedited_wait()
    This commit adds a READ_ONCE() to an access to the rcu_node structure's
    ->expmask field to prevent compiler mischief.  Detected by KCSAN.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/tree_exp.h
+++ kernel/rcu/tree_exp.h
@@ -502,7 +502,8 @@ static void synchronize_rcu_expedited_wait(void)
 		if (synchronize_rcu_expedited_wait_once(1))
 			return;
 		rcu_for_each_leaf_node(rnp) {
-			for_each_leaf_node_cpu_mask(rnp, cpu, rnp->expmask) {
+			mask = READ_ONCE(rnp->expmask);
+			for_each_leaf_node_cpu_mask(rnp, cpu, mask) {
 				rdp = per_cpu_ptr(&rcu_data, cpu);
 				if (rdp->rcu_forced_tick_exp)
 					continue;
