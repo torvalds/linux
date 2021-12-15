5471fe8b383f82d9a20f8ac577c1f1cc3b9dbe5c
Yann Gautier <yann.gautier@foss.st.com>
2021-12-15 15:17:24 +0100

    mmc: mmci: Add support for sdmmc variant revision v2.2
    The change is only hardware, and does not need driver change:
    Added hardware flow control during transmit packet with variable delay.
    The new id is then added to the ids list structure.
    
    Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://lore.kernel.org/r/20211215141727.4901-2-yann.gautier@foss.st.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/mmci.c
+++ drivers/mmc/host/mmci.c
@@ -2435,6 +2435,11 @@ static const struct amba_id mmci_ids[] = {
 		.mask	= 0xf0ffffff,
 		.data	= &variant_stm32_sdmmcv2,
 	},
+	{
+		.id     = 0x20253180,
+		.mask	= 0xf0ffffff,
+		.data	= &variant_stm32_sdmmcv2,
+	},
 	/* Qualcomm variants */
 	{
 		.id     = 0x00051180,
