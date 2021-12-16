2075d9f4fdc5dd39780cc21a01943db2d141357a
Loic Poulain <loic.poulain@linaro.org>
2021-12-16 13:42:19 +0530

    bus: mhi: pci_generic: Graceful shutdown on freeze
    commit f77097ec8c0141a4b5cf3722a246be0cb5677e29 upstream.
    
    There is no reason for shutting down MHI ungracefully on freeze,
    this causes the MHI host stack & device stack to not be aligned
    anymore since the proper MHI reset sequence is not performed for
    ungraceful shutdown.
    
    Link: https://lore.kernel.org/r/1635268180-13699-1-git-send-email-loic.poulain@linaro.org
    Fixes: 5f0c2ee1fe8d ("bus: mhi: pci-generic: Fix hibernation")
    Cc: stable@vger.kernel.org
    Suggested-by: Bhaumik Bhatt <bbhatt@codeaurora.org>
    Reviewed-by: Bhaumik Bhatt <bbhatt@codeaurora.org>
    Reviewed-by: Hemant Kumar <hemantk@codeaurora.org>
    Reviewed-by: Manivannan Sadhasivam <mani@kernel.org>
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Link: https://lore.kernel.org/r/20211216081227.237749-3-manivannan.sadhasivam@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/bus/mhi/pci_generic.c
+++ drivers/bus/mhi/pci_generic.c
@@ -1018,7 +1018,7 @@ static int __maybe_unused mhi_pci_freeze(struct device *dev)
 	 * context.
 	 */
 	if (test_and_clear_bit(MHI_PCI_DEV_STARTED, &mhi_pdev->status)) {
-		mhi_power_down(mhi_cntrl, false);
+		mhi_power_down(mhi_cntrl, true);
 		mhi_unprepare_after_power_down(mhi_cntrl);
 	}
 
