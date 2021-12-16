d2c741290f7e2f48709b7f67c862b3f708998824
Qais Yousef <qais.yousef@arm.com>
2021-12-16 22:53:20 +0000

    sched/uclamp: Fix iowait boost escaping uclamp restriction
    [ Upstream commit d37aee9018e68b0d356195caefbb651910e0bbfa ]
    
    iowait_boost signal is applied independently of util and doesn't take
    into account uclamp settings of the rq. An io heavy task that is capped
    by uclamp_max could still request higher frequency because
    sugov_iowait_apply() doesn't clamp the boost via uclamp_rq_util_with()
    like effective_cpu_util() does.
    
    Make sure that iowait_boost honours uclamp requests by calling
    uclamp_rq_util_with() when applying the boost.
    
    Fixes: 982d9cdc22c9 ("sched/cpufreq, sched/uclamp: Add clamps for FAIR and RT tasks")
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://lore.kernel.org/r/20211216225320.2957053-3-qais.yousef@arm.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -289,6 +289,7 @@ static void sugov_iowait_apply(struct sugov_cpu *sg_cpu, u64 time)
 	 * into the same scale so we can compare.
 	 */
 	boost = (sg_cpu->iowait_boost * sg_cpu->max) >> SCHED_CAPACITY_SHIFT;
+	boost = uclamp_rq_util_with(cpu_rq(sg_cpu->cpu), boost, NULL);
 	if (sg_cpu->util < boost)
 		sg_cpu->util = boost;
 }
