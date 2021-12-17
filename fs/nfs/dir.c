98ca3ee60b9e4b039cc3ef21a169e775afa9bd0c
Trond Myklebust <trond.myklebust@hammerspace.com>
2021-12-17 15:36:55 -0500

    NFSv4: Just don't cache negative dentries on case insensitive servers
    If the directory contents change, we cannot rely on the negative dentry
    being cacheable.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>

--- fs/nfs/dir.c
+++ fs/nfs/dir.c
@@ -1436,6 +1436,9 @@ int nfs_neg_need_reval(struct inode *dir, struct dentry *dentry,
 		return 0;
 	if (NFS_SERVER(dir)->flags & NFS_MOUNT_LOOKUP_CACHE_NONEG)
 		return 1;
+	/* Case insensitive server? Revalidate negative dentries */
+	if (nfs_server_capable(dir, NFS_CAP_CASE_INSENSITIVE))
+		return 1;
 	return !nfs_check_verifier(dir, dentry, flags & LOOKUP_RCU);
 }
 
