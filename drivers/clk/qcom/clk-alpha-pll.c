a5273ed2fed221317203c120670fc9a09488be3b
Taniya Das <tdas@codeaurora.org>
2021-12-20 22:13:54 +0530

    clk: qcom: clk-alpha-pll: Increase PLL lock detect poll time
    PLL poll for lock detection can take more than 100us for certain type
    of Lucid PLLs and also the new PLLs types(Lucid EVO), thus update to 200us.
    
    Signed-off-by: Taniya Das <tdas@codeaurora.org>
    Link: https://lore.kernel.org/r/1640018638-19436-2-git-send-email-tdas@codeaurora.org
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/qcom/clk-alpha-pll.c
+++ drivers/clk/qcom/clk-alpha-pll.c
@@ -204,7 +204,7 @@ static int wait_for_pll(struct clk_alpha_pll *pll, u32 mask, bool inverse,
 	if (ret)
 		return ret;
 
-	for (count = 100; count > 0; count--) {
+	for (count = 200; count > 0; count--) {
 		ret = regmap_read(pll->clkr.regmap, PLL_MODE(pll), &val);
 		if (ret)
 			return ret;
