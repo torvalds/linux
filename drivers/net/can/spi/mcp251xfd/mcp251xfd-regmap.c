0cab3fb917c579aa2c4b99df25b97bd76eb187fa
Thomas Kopp <thomas.kopp@microchip.com>
2021-12-21 22:24:52 +0000

    can: mcp251xfd: mcp251xfd_regmap_crc_read(): improve workaround handling for mcp2517fd
    commit 406cc9cdb3e8d644b15e8028948f091b82abdbca upstream.
    
    The mcp251xfd compatible chips have an erratum ([1], [2]), where the
    received CRC doesn't match the calculated CRC. In commit
    c7eb923c3caf ("can: mcp251xfd: mcp251xfd_regmap_crc_read(): work
    around broken CRC on TBC register") the following workaround was
    implementierend.
    
    - If a CRC read error on the TBC register is detected and the first
      byte is 0x00 or 0x80, the most significant bit of the first byte is
      flipped and the CRC is calculated again.
    - If the CRC now matches, the _original_ data is passed to the reader.
      For now we assume transferred data was OK.
    
    Measurements on the mcp2517fd show that the workaround is applicable
    not only of the lowest byte is 0x00 or 0x80, but also if 3 least
    significant bits are set.
    
    Update check on 1st data byte and workaround description accordingly.
    
    [1] mcp2517fd: DS80000792C: "Incorrect CRC for certain READ_CRC commands"
    [2] mcp2518fd: DS80000789C: "Incorrect CRC for certain READ_CRC commands"
    
    Link: https://lore.kernel.org/all/DM4PR11MB53901D49578FE265B239E55AFB7C9@DM4PR11MB5390.namprd11.prod.outlook.com
    Fixes: c7eb923c3caf ("can: mcp251xfd: mcp251xfd_regmap_crc_read(): work around broken CRC on TBC register")
    Cc: stable@vger.kernel.org
    Reported-by: Pavel Modilaynen <pavel.modilaynen@volvocars.com>
    Signed-off-by: Thomas Kopp <thomas.kopp@microchip.com>
    [mkl: split into 2 patches, update patch description and documentation]
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/can/spi/mcp251xfd/mcp251xfd-regmap.c
+++ drivers/net/can/spi/mcp251xfd/mcp251xfd-regmap.c
@@ -325,10 +325,12 @@ mcp251xfd_regmap_crc_read(void *context,
 		 * register. It increments once per SYS clock tick,
 		 * which is 20 or 40 MHz.
 		 *
-		 * Observation shows that if the lowest byte (which is
-		 * transferred first on the SPI bus) of that register
-		 * is 0x00 or 0x80 the calculated CRC doesn't always
-		 * match the transferred one.
+		 * Observation on the mcp2518fd shows that if the
+		 * lowest byte (which is transferred first on the SPI
+		 * bus) of that register is 0x00 or 0x80 the
+		 * calculated CRC doesn't always match the transferred
+		 * one. On the mcp2517fd this problem is not limited
+		 * to the first byte being 0x00 or 0x80.
 		 *
 		 * If the highest bit in the lowest byte is flipped
 		 * the transferred CRC matches the calculated one. We
@@ -337,7 +339,8 @@ mcp251xfd_regmap_crc_read(void *context,
 		 * correct.
 		 */
 		if (reg == MCP251XFD_REG_TBC &&
-		    (buf_rx->data[0] == 0x0 || buf_rx->data[0] == 0x80)) {
+		    ((buf_rx->data[0] & 0xf8) == 0x0 ||
+		     (buf_rx->data[0] & 0xf8) == 0x80)) {
 			/* Flip highest bit in lowest byte of le32 */
 			buf_rx->data[0] ^= 0x80;
 
