08d92c7a47375ac98aa5b9cdeb3a8736b37cf6d4
Colin Ian King <colin.i.king@gmail.com>
2021-12-21 00:37:50 +0000

    clk: socfpga: remove redundant assignment on division
    The variable parent_rate is being divided by div and the result
    is re-assigned to parent_rate before being returned. The assignment
    is redundant, replace /= operator with just / operator.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>
    Link: https://lore.kernel.org/r/20211221003750.212780-1-colin.i.king@gmail.com
    Acked-by: Dinh Nguyen <dinguyen@kernel.org>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/socfpga/clk-pll-s10.c
+++ drivers/clk/socfpga/clk-pll-s10.c
@@ -113,7 +113,7 @@ static unsigned long clk_boot_clk_recalc_rate(struct clk_hw *hwclk,
 		SWCTRLBTCLKSEL_MASK) >>
 		SWCTRLBTCLKSEL_SHIFT);
 	div += 1;
-	return parent_rate /= div;
+	return parent_rate / div;
 }
 
 
