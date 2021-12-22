5579649e7eb756a4e3d5784b6958374e5bfc41de
Ilkka Koskinen <ilkka@os.amperecomputing.com>
2021-12-22 17:37:21 +0100

    ACPICA: iASL: Add suppport for AGDI table
    ACPICA commit cf36a6d658ca5aa8c329c2edfc3322c095ffd844
    
    Add support for Arm Generic Diagnostic Dump and Reset Interface, which is
    described by "ACPI for Arm Components 1.1 Platform Design Document"
    ARM DEN0093.
    
    Add the necessary types in the ACPICA header files and support for
    compiling and decompiling the table.
    
    Link: https://github.com/acpica/acpica/commit/cf36a6d6
    Signed-off-by: Ilkka Koskinen <ilkka@os.amperecomputing.com>
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/actbl2.h
+++ include/acpi/actbl2.h
@@ -24,6 +24,7 @@
  * file. Useful because they make it more difficult to inadvertently type in
  * the wrong signature.
  */
+#define ACPI_SIG_AGDI           "AGDI"	/* Arm Generic Diagnostic Dump and Reset Device Interface */
 #define ACPI_SIG_BDAT           "BDAT"	/* BIOS Data ACPI Table */
 #define ACPI_SIG_IORT           "IORT"	/* IO Remapping Table */
 #define ACPI_SIG_IVRS           "IVRS"	/* I/O Virtualization Reporting Structure */
@@ -237,6 +238,25 @@ typedef struct acpi_aest_node_interrupt {
 #define ACPI_AEST_NODE_FAULT_HANDLING       0
 #define ACPI_AEST_NODE_ERROR_RECOVERY       1
 #define ACPI_AEST_XRUPT_RESERVED            2	/* 2 and above are reserved */
+
+/*******************************************************************************
+ * AGDI - Arm Generic Diagnostic Dump and Reset Device Interface
+ *
+ * Conforms to "ACPI for Arm Components 1.1, Platform Design Document"
+ * ARM DEN0093 v1.1
+ *
+ ******************************************************************************/
+struct acpi_table_agdi {
+	struct acpi_table_header header;	/* Common ACPI table header */
+	u8 flags;
+	u8 reserved[3];
+	u32 sdei_event;
+	u32 gsiv;
+};
+
+/* Mask for Flags field above */
+
+#define ACPI_AGDI_SIGNALING_MODE (1)
 
 /*******************************************************************************
  *
