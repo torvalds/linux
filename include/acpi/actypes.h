339651be3704f336634d90c000a7778b86e1be99
Jessica Clarke <jrtc27@jrtc27.com>
2021-12-22 17:23:28 +0100

    ACPICA: Macros: Remove ACPI_PHYSADDR_TO_PTR
    ACPICA commit 52abebd410945ec55afb4dd8b7150e8a39b5c960
    
    This macro was only ever used when stuffing pointers into physical
    addresses and trying to later reconstruct the pointer, which is
    implementation-defined as to whether that can be done. Now that all such
    operations are gone, the macro is unused, and should be removed to avoid
    such practices being reintroduced.
    
    Link: https://github.com/acpica/acpica/commit/52abebd4
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/actypes.h
+++ include/acpi/actypes.h
@@ -509,7 +509,6 @@ typedef u64 acpi_integer;
 #define ACPI_TO_POINTER(i)              ACPI_CAST_PTR (void, (acpi_size) (i))
 #define ACPI_TO_INTEGER(p)              ACPI_PTR_DIFF (p, (void *) 0)
 #define ACPI_OFFSET(d, f)               ACPI_PTR_DIFF (&(((d *) 0)->f), (void *) 0)
-#define ACPI_PHYSADDR_TO_PTR(i)         ACPI_TO_POINTER(i)
 #define ACPI_PTR_TO_PHYSADDR(i)         ACPI_TO_INTEGER(i)
 
 /* Optimizations for 4-character (32-bit) acpi_name manipulation */
