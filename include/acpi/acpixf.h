c95545a036705e1a78b40a83701c8d3868898114
Bob Moore <robert.moore@intel.com>
2021-12-22 17:38:29 +0100

    ACPICA: Update version to 20211217
    ACPICA commit 90088defcb99e122edf41038ae5c901206c86dc9
    
    Version 20211217.
    
    Link: https://github.com/acpica/acpica/commit/90088def
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/acpixf.h
+++ include/acpi/acpixf.h
@@ -12,7 +12,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20210930
+#define ACPI_CA_VERSION                 0x20211217
 
 #include <acpi/acconfig.h>
 #include <acpi/actypes.h>
