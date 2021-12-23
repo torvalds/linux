8fdaa9a6ace20a226793069da3e495dd0408ed1f
Sumeet Pawnikar <sumeet.r.pawnikar@intel.com>
2021-12-23 15:12:36 +0530

    thermal/drivers/int340x: Fix RFIM mailbox write commands
    commit 2685c77b80a80c57e2a25a726b82fb31e6e212ab upstream.
    
    The existing mail mechanism only supports writing of workload types.
    
    However, mailbox command for RFIM (cmd = 0x08) also requires write
    operation which is ignored. This results in failing to store RFI
    restriction.
    
    Fixint this requires enhancing mailbox writes for non workload
    commands too, so remove the check for MBOX_CMD_WORKLOAD_TYPE_WRITE
    in mailbox write to allow this other write commands to be supoorted.
    
    At the same time, however, we have to make sure that there is no
    impact on read commands, by avoiding to write anything into the
    mailbox data register.
    
    To properly implement that, add two separate functions for mbox read
    and write commands for the processor thermal workload command type.
    This helps to distinguish the read and write workload command types
    from each other while sending mbox commands.
    
    Fixes: 5d6fbc96bd36 ("thermal/drivers/int340x: processor_thermal: Export additional attributes")
    Signed-off-by: Sumeet Pawnikar <sumeet.r.pawnikar@intel.com>
    Cc: 5.14+ <stable@vger.kernel.org> # 5.14+
    Acked-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    [ rjw: Changelog edits ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/thermal/intel/int340x_thermal/processor_thermal_device.h
+++ drivers/thermal/intel/int340x_thermal/processor_thermal_device.h
@@ -80,7 +80,8 @@ void proc_thermal_rfim_remove(struct pci_dev *pdev);
 int proc_thermal_mbox_add(struct pci_dev *pdev, struct proc_thermal_device *proc_priv);
 void proc_thermal_mbox_remove(struct pci_dev *pdev);
 
-int processor_thermal_send_mbox_cmd(struct pci_dev *pdev, u16 cmd_id, u32 cmd_data, u64 *cmd_resp);
+int processor_thermal_send_mbox_read_cmd(struct pci_dev *pdev, u16 id, u64 *resp);
+int processor_thermal_send_mbox_write_cmd(struct pci_dev *pdev, u16 id, u32 data);
 int proc_thermal_add(struct device *dev, struct proc_thermal_device *priv);
 void proc_thermal_remove(struct proc_thermal_device *proc_priv);
 int proc_thermal_suspend(struct device *dev);
