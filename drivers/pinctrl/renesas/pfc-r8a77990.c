53565413604d1203e7944ef196510243a60bb491
Geert Uytterhoeven <geert+renesas@glider.be>
2021-12-23 15:42:10 +0100

    pinctrl: renesas: r8a77990: Share more VIN pin group data
    Pin group vin4_g8 is a subset of vin4_data24_a.
    Pin group vin5_high8 is a subset of vin5_data16_a.
    
    This reduces kernel size by 128 bytes.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://lore.kernel.org/r/cfb71c90c19723ba2770f7512f138e4b17857141.1640269757.git.geert+renesas@glider.be

--- drivers/pinctrl/renesas/pfc-r8a77990.c
+++ drivers/pinctrl/renesas/pfc-r8a77990.c
@@ -3646,20 +3646,6 @@ static const unsigned int vin4_data_b_mux[] = {
 	VI4_DATA22_MARK,  VI4_DATA23_MARK,
 };
 
-static const unsigned int vin4_g8_pins[] = {
-	RCAR_GP_PIN(1, 4),  RCAR_GP_PIN(1, 5),
-	RCAR_GP_PIN(1, 6),  RCAR_GP_PIN(1, 7),
-	RCAR_GP_PIN(1, 3),  RCAR_GP_PIN(1, 10),
-	RCAR_GP_PIN(1, 13), RCAR_GP_PIN(1, 14),
-};
-
-static const unsigned int vin4_g8_mux[] = {
-	VI4_DATA8_MARK,  VI4_DATA9_MARK,
-	VI4_DATA10_MARK, VI4_DATA11_MARK,
-	VI4_DATA12_MARK, VI4_DATA13_MARK,
-	VI4_DATA14_MARK, VI4_DATA15_MARK,
-};
-
 static const unsigned int vin4_sync_pins[] = {
 	/* HSYNC, VSYNC */
 	RCAR_GP_PIN(2, 25), RCAR_GP_PIN(2, 24),
@@ -3728,20 +3714,6 @@ static const unsigned int vin5_data8_b_mux[] = {
 	VI5_DATA2_B_MARK,  VI5_DATA3_B_MARK,
 	VI5_DATA4_B_MARK,  VI5_DATA5_B_MARK,
 	VI5_DATA6_B_MARK,  VI5_DATA7_B_MARK,
-};
-
-static const unsigned int vin5_high8_pins[] = {
-	RCAR_GP_PIN(0, 12), RCAR_GP_PIN(0, 13),
-	RCAR_GP_PIN(0, 9),  RCAR_GP_PIN(0, 11),
-	RCAR_GP_PIN(0, 8),  RCAR_GP_PIN(0, 10),
-	RCAR_GP_PIN(0, 2),  RCAR_GP_PIN(0, 3),
-};
-
-static const unsigned int vin5_high8_mux[] = {
-	VI5_DATA8_A_MARK,  VI5_DATA9_A_MARK,
-	VI5_DATA10_A_MARK, VI5_DATA11_A_MARK,
-	VI5_DATA12_A_MARK, VI5_DATA13_A_MARK,
-	VI5_DATA14_A_MARK, VI5_DATA15_A_MARK,
 };
 
 static const unsigned int vin5_sync_a_pins[] = {
@@ -4031,7 +4003,7 @@ static const struct {
 		SH_PFC_PIN_GROUP(vin4_data18_b),
 		BUS_DATA_PIN_GROUP(vin4_data, 20, _b),
 		BUS_DATA_PIN_GROUP(vin4_data, 24, _b),
-		SH_PFC_PIN_GROUP(vin4_g8),
+		SH_PFC_PIN_GROUP_SUBSET(vin4_g8, vin4_data_a, 8, 8),
 		SH_PFC_PIN_GROUP(vin4_sync),
 		SH_PFC_PIN_GROUP(vin4_field),
 		SH_PFC_PIN_GROUP(vin4_clkenb),
@@ -4041,7 +4013,7 @@ static const struct {
 		BUS_DATA_PIN_GROUP(vin5_data, 12, _a),
 		BUS_DATA_PIN_GROUP(vin5_data, 16, _a),
 		SH_PFC_PIN_GROUP(vin5_data8_b),
-		SH_PFC_PIN_GROUP(vin5_high8),
+		SH_PFC_PIN_GROUP_SUBSET(vin5_high8, vin5_data_a, 8, 8),
 		SH_PFC_PIN_GROUP(vin5_sync_a),
 		SH_PFC_PIN_GROUP(vin5_field_a),
 		SH_PFC_PIN_GROUP(vin5_clkenb_a),
