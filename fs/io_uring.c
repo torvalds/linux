20fb0dc35bf9f2f3ff6336bc0dd21e22bbc6d51a
Jens Axboe <axboe@kernel.dk>
2021-12-22 20:26:56 -0700

    io_uring: zero iocb->ki_pos for stream file types
    [ Upstream commit 7b9762a5e8837b92a027d58d396a9d27f6440c36 ]
    
    io_uring supports using offset == -1 for using the current file position,
    and we read that in as part of read/write command setup. For the non-iter
    read/write types we pass in NULL for the position pointer, but for the
    iter types we should not be passing any anything but 0 for the position
    for a stream.
    
    Clear kiocb->ki_pos if the file is a stream, don't leave it as -1. If we
    do, then the request will error with -ESPIPE.
    
    Fixes: ba04291eb66e ("io_uring: allow use of offset == -1 to mean file position")
    Link: https://github.com/axboe/liburing/discussions/501
    Reported-by: Samuel Williams <samuel.williams@oriontransfer.co.nz>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -2879,9 +2879,13 @@ static int io_prep_rw(struct io_kiocb *req, const struct io_uring_sqe *sqe,
 		req->flags |= REQ_F_ISREG;
 
 	kiocb->ki_pos = READ_ONCE(sqe->off);
-	if (kiocb->ki_pos == -1 && !(file->f_mode & FMODE_STREAM)) {
-		req->flags |= REQ_F_CUR_POS;
-		kiocb->ki_pos = file->f_pos;
+	if (kiocb->ki_pos == -1) {
+		if (!(file->f_mode & FMODE_STREAM)) {
+			req->flags |= REQ_F_CUR_POS;
+			kiocb->ki_pos = file->f_pos;
+		} else {
+			kiocb->ki_pos = 0;
+		}
 	}
 	kiocb->ki_hint = ki_hint_validate(file_write_hint(kiocb->ki_filp));
 	kiocb->ki_flags = iocb_flags(kiocb->ki_filp);
