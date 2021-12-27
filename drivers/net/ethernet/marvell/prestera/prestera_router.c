bca5859bc6c60956585b57220b6ed619e23a6a7b
Yevhen Orlov <yevhen.orlov@plvision.eu>
2021-12-27 23:52:29 +0200

    net: marvell: prestera: add hardware router objects accounting
    Add prestera_router_hw.c. This file contains functions, which track HW
    objects relations and links. This include implicity creation of objects,
    that needed by requested one and implicity removing of objects, which
    reference counter is became zero.
    
    We need this layer, because kernel callbacks not always mapped to
    creation of single HW object. So let it be two different layers - one
    for subscribing and parsing kernel structures, and another
    (prestera_router_hw.c) for HW objects relations tracking.
    
    There is two types of objects on router_hw layer:
     - Explicit objects (rif_entry) : created by higher layer.
     - Implicit objects (vr) : created on demand by explicit objects.
    
    Co-developed-by: Taras Chornyi <tchornyi@marvell.com>
    Signed-off-by: Taras Chornyi <tchornyi@marvell.com>
    Co-developed-by: Oleksandr Mazur <oleksandr.mazur@plvision.eu>
    Signed-off-by: Oleksandr Mazur <oleksandr.mazur@plvision.eu>
    Signed-off-by: Yevhen Orlov <yevhen.orlov@plvision.eu>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/prestera/prestera_router.c
+++ drivers/net/ethernet/marvell/prestera/prestera_router.c
@@ -5,10 +5,12 @@
 #include <linux/types.h>
 
 #include "prestera.h"
+#include "prestera_router_hw.h"
 
 int prestera_router_init(struct prestera_switch *sw)
 {
 	struct prestera_router *router;
+	int err;
 
 	router = kzalloc(sizeof(*sw->router), GFP_KERNEL);
 	if (!router)
@@ -17,7 +19,15 @@ int prestera_router_init(struct prestera_switch *sw)
 	sw->router = router;
 	router->sw = sw;
 
+	err = prestera_router_hw_init(sw);
+	if (err)
+		goto err_router_lib_init;
+
 	return 0;
+
+err_router_lib_init:
+	kfree(sw->router);
+	return err;
 }
 
 void prestera_router_fini(struct prestera_switch *sw)
