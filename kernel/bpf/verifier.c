e8efe8369944c6199f124e3b50662ad05a048b60
Daniel Borkmann <daniel@iogearbox.net>
2022-01-04 14:16:03 +0000

    bpf: Fix out of bounds access from invalid *_or_null type verification
    [ no upstream commit given implicitly fixed through the larger refactoring
      in c25b2ae136039ffa820c26138ed4a5e5f3ab3841 ]
    
    While auditing some other code, I noticed missing checks inside the pointer
    arithmetic simulation, more specifically, adjust_ptr_min_max_vals(). Several
    *_OR_NULL types are not rejected whereas they are _required_ to be rejected
    given the expectation is that they get promoted into a 'real' pointer type
    for the success case, that is, after an explicit != NULL check.
    
    One case which stands out and is accessible from unprivileged (iff enabled
    given disabled by default) is BPF ring buffer. From crafting a PoC, the NULL
    check can be bypassed through an offset, and its id marking will then lead
    to promotion of mem_or_null to a mem type.
    
    bpf_ringbuf_reserve() helper can trigger this case through passing of reserved
    flags, for example.
    
      func#0 @0
      0: R1=ctx(id=0,off=0,imm=0) R10=fp0
      0: (7a) *(u64 *)(r10 -8) = 0
      1: R1=ctx(id=0,off=0,imm=0) R10=fp0 fp-8_w=mmmmmmmm
      1: (18) r1 = 0x0
      3: R1_w=map_ptr(id=0,off=0,ks=0,vs=0,imm=0) R10=fp0 fp-8_w=mmmmmmmm
      3: (b7) r2 = 8
      4: R1_w=map_ptr(id=0,off=0,ks=0,vs=0,imm=0) R2_w=invP8 R10=fp0 fp-8_w=mmmmmmmm
      4: (b7) r3 = 0
      5: R1_w=map_ptr(id=0,off=0,ks=0,vs=0,imm=0) R2_w=invP8 R3_w=invP0 R10=fp0 fp-8_w=mmmmmmmm
      5: (85) call bpf_ringbuf_reserve#131
      6: R0_w=mem_or_null(id=2,ref_obj_id=2,off=0,imm=0) R10=fp0 fp-8_w=mmmmmmmm refs=2
      6: (bf) r6 = r0
      7: R0_w=mem_or_null(id=2,ref_obj_id=2,off=0,imm=0) R6_w=mem_or_null(id=2,ref_obj_id=2,off=0,imm=0) R10=fp0 fp-8_w=mmmmmmmm refs=2
      7: (07) r0 += 1
      8: R0_w=mem_or_null(id=2,ref_obj_id=2,off=1,imm=0) R6_w=mem_or_null(id=2,ref_obj_id=2,off=0,imm=0) R10=fp0 fp-8_w=mmmmmmmm refs=2
      8: (15) if r0 == 0x0 goto pc+4
       R0_w=mem(id=0,ref_obj_id=0,off=0,imm=0) R6_w=mem(id=0,ref_obj_id=2,off=0,imm=0) R10=fp0 fp-8_w=mmmmmmmm refs=2
      9: R0_w=mem(id=0,ref_obj_id=0,off=0,imm=0) R6_w=mem(id=0,ref_obj_id=2,off=0,imm=0) R10=fp0 fp-8_w=mmmmmmmm refs=2
      9: (62) *(u32 *)(r6 +0) = 0
       R0_w=mem(id=0,ref_obj_id=0,off=0,imm=0) R6_w=mem(id=0,ref_obj_id=2,off=0,imm=0) R10=fp0 fp-8_w=mmmmmmmm refs=2
      10: R0_w=mem(id=0,ref_obj_id=0,off=0,imm=0) R6_w=mem(id=0,ref_obj_id=2,off=0,imm=0) R10=fp0 fp-8_w=mmmmmmmm refs=2
      10: (bf) r1 = r6
      11: R0_w=mem(id=0,ref_obj_id=0,off=0,imm=0) R1_w=mem(id=0,ref_obj_id=2,off=0,imm=0) R6_w=mem(id=0,ref_obj_id=2,off=0,imm=0) R10=fp0 fp-8_w=mmmmmmmm refs=2
      11: (b7) r2 = 0
      12: R0_w=mem(id=0,ref_obj_id=0,off=0,imm=0) R1_w=mem(id=0,ref_obj_id=2,off=0,imm=0) R2_w=invP0 R6_w=mem(id=0,ref_obj_id=2,off=0,imm=0) R10=fp0 fp-8_w=mmmmmmmm refs=2
      12: (85) call bpf_ringbuf_submit#132
      13: R6=invP(id=0) R10=fp0 fp-8=mmmmmmmm
      13: (b7) r0 = 0
      14: R0_w=invP0 R6=invP(id=0) R10=fp0 fp-8=mmmmmmmm
      14: (95) exit
    
      from 8 to 13: safe
      processed 15 insns (limit 1000000) max_states_per_insn 0 total_states 1 peak_states 1 mark_read 0
      OK
    
    All three commits, that is b121b341e598 ("bpf: Add PTR_TO_BTF_ID_OR_NULL support"),
    457f44363a88 ("bpf: Implement BPF ring buffer and verifier support for it"), and the
    afbf21dce668 ("bpf: Support readonly/readwrite buffers in verifier") suffer the same
    cause and their *_OR_NULL type pendants must be rejected in adjust_ptr_min_max_vals().
    
    Make the test more robust by reusing reg_type_may_be_null() helper such that we catch
    all *_OR_NULL types we have today and in future.
    
    Note that pointer arithmetic on PTR_TO_BTF_ID, PTR_TO_RDONLY_BUF, and PTR_TO_RDWR_BUF
    is generally allowed.
    
    Fixes: b121b341e598 ("bpf: Add PTR_TO_BTF_ID_OR_NULL support")
    Fixes: 457f44363a88 ("bpf: Implement BPF ring buffer and verifier support for it")
    Fixes: afbf21dce668 ("bpf: Support readonly/readwrite buffers in verifier")
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -7039,16 +7039,16 @@ static int adjust_ptr_min_max_vals(struct bpf_verifier_env *env,
 		fallthrough;
 	case PTR_TO_PACKET_END:
 	case PTR_TO_SOCKET:
-	case PTR_TO_SOCKET_OR_NULL:
 	case PTR_TO_SOCK_COMMON:
-	case PTR_TO_SOCK_COMMON_OR_NULL:
 	case PTR_TO_TCP_SOCK:
-	case PTR_TO_TCP_SOCK_OR_NULL:
 	case PTR_TO_XDP_SOCK:
+reject:
 		verbose(env, "R%d pointer arithmetic on %s prohibited\n",
 			dst, reg_type_str[ptr_reg->type]);
 		return -EACCES;
 	default:
+		if (reg_type_may_be_null(ptr_reg->type))
+			goto reject;
 		break;
 	}
 
