00f6e68b8d59bf006db54e3e257684f44d26195c
GuoYong Zheng <zhenggy@chinatelecom.cn>
2022-01-05 18:12:02 +0800

    io_uring: remove unused function parameter
    Parameter res2 is not used in __io_complete_rw, remove it.
    
    Fixes: 6b19b766e8f0 ("fs: get rid of the res2 iocb->ki_complete argument")
    Signed-off-by: GuoYong Zheng <zhenggy@chinatelecom.cn>
    Link: https://lore.kernel.org/r/1641377522-1851-1-git-send-email-zhenggy@chinatelecom.cn
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -2791,7 +2791,7 @@ static inline void io_req_task_complete(struct io_kiocb *req, bool *locked)
 	}
 }
 
-static void __io_complete_rw(struct io_kiocb *req, long res, long res2,
+static void __io_complete_rw(struct io_kiocb *req, long res,
 			     unsigned int issue_flags)
 {
 	if (__io_complete_rw_common(req, res))
@@ -3037,7 +3037,7 @@ static void kiocb_done(struct io_kiocb *req, ssize_t ret,
 	if (req->flags & REQ_F_CUR_POS)
 		req->file->f_pos = req->rw.kiocb.ki_pos;
 	if (ret >= 0 && (req->rw.kiocb.ki_complete == io_complete_rw))
-		__io_complete_rw(req, ret, 0, issue_flags);
+		__io_complete_rw(req, ret, issue_flags);
 	else
 		io_rw_done(&req->rw.kiocb, ret);
 
