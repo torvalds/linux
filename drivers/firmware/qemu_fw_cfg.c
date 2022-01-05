ad8a5d1d2f577843dba14e4727c86e952ffbd5f1
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2022-01-05 19:31:33 +0100

    qemu_fw_cfg: use default_groups in kobj_type
    There are currently 2 ways to create a set of sysfs files for a
    kobj_type, through the default_attrs field, and the default_groups
    field.  Move the firmware qemu_fw_cfg sysfs code to use default_groups
    field which has been the preferred way since aa30f47cf666 ("kobject: Add
    support for default attribute groups to kobj_type") so that we can soon
    get rid of the obsolete default_attrs field.
    
    Cc: Gabriel Somlo <somlo@cmu.edu>
    Cc: "Michael S. Tsirkin" <mst@redhat.com>
    Cc: qemu-devel@nongnu.org
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Link: https://lore.kernel.org/r/20220105183133.2812848-1-gregkh@linuxfoundation.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/firmware/qemu_fw_cfg.c
+++ drivers/firmware/qemu_fw_cfg.c
@@ -395,7 +395,7 @@ static void fw_cfg_sysfs_cache_cleanup(void)
 	}
 }
 
-/* default_attrs: per-entry attributes and show methods */
+/* per-entry attributes and show methods */
 
 #define FW_CFG_SYSFS_ATTR(_attr) \
 struct fw_cfg_sysfs_attribute fw_cfg_sysfs_attr_##_attr = { \
@@ -428,6 +428,7 @@ static struct attribute *fw_cfg_sysfs_entry_attrs[] = {
 	&fw_cfg_sysfs_attr_name.attr,
 	NULL,
 };
+ATTRIBUTE_GROUPS(fw_cfg_sysfs_entry);
 
 /* sysfs_ops: find fw_cfg_[entry, attribute] and call appropriate show method */
 static ssize_t fw_cfg_sysfs_attr_show(struct kobject *kobj, struct attribute *a,
@@ -454,7 +455,7 @@ static void fw_cfg_sysfs_release_entry(struct kobject *kobj)
 
 /* kobj_type: ties together all properties required to register an entry */
 static struct kobj_type fw_cfg_sysfs_entry_ktype = {
-	.default_attrs = fw_cfg_sysfs_entry_attrs,
+	.default_groups = fw_cfg_sysfs_entry_groups,
 	.sysfs_ops = &fw_cfg_sysfs_attr_ops,
 	.release = fw_cfg_sysfs_release_entry,
 };
