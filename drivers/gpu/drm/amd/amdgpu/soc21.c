71dae221432cbf2dbd77325c90b9abecccb7b29c
James Zhu <James.Zhu@amd.com>
2022-01-05 20:36:08 -0500

    drm/amdgpu/jpeg: enable JPEG CG for VCN4_0_4
    Enable JPEG CG for VCN4_0_4.
    
    Signed-off-by: James Zhu <James.Zhu@amd.com>
    Reviewed-by: Sonny Jiang <sonny.jiang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/soc21.c
+++ drivers/gpu/drm/amd/amdgpu/soc21.c
@@ -548,7 +548,8 @@ static int soc21_common_early_init(void *handle)
 		adev->external_rev_id = adev->rev_id + 0x1; // TODO: need update
 		break;
 	case IP_VERSION(11, 0, 2):
-		adev->cg_flags = 0;
+		adev->cg_flags =
+			AMD_CG_SUPPORT_JPEG_MGCG;
 		adev->pg_flags = 0;
 		adev->external_rev_id = adev->rev_id + 0x10;
 		break;
