a74313481ae24cd301b79b5fca3161079e739a21
Kent Overstreet <kent.overstreet@gmail.com>
2022-01-06 00:04:56 -0500

    bcachefs: Fix check_pos_snapshot_overwritten for !snapshots
    It shouldn't run if the btree being checked doesn't have snapshots.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/btree_update_leaf.c
+++ fs/bcachefs/btree_update_leaf.c
@@ -1088,6 +1088,9 @@ static int check_pos_snapshot_overwritten(struct btree_trans *trans,
 	struct bkey_s_c k;
 	int ret;
 
+	if (!btree_type_has_snapshots(id))
+		return 0;
+
 	if (!snapshot_t(c, pos.snapshot)->children[0])
 		return 0;
 
