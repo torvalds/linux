b4784a45ea69577f21f89898c71127774a090a2a
Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
2022-01-07 20:06:58 +0800

    crypto: sm3 - make dependent on sm3 library
    SM3 generic library is stand-alone implementation, it is necessary
    making the sm3-generic implementation to depends on SM3 library.
    The functions crypto_sm3_*() provided by sm3_generic is no longer
    exported.
    
    Signed-off-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/sm3.h
+++ include/crypto/sm3.h
@@ -35,16 +35,6 @@ struct sm3_state {
 	u8 buffer[SM3_BLOCK_SIZE];
 };
 
-struct shash_desc;
-
-extern int crypto_sm3_update(struct shash_desc *desc, const u8 *data,
-			      unsigned int len);
-
-extern int crypto_sm3_final(struct shash_desc *desc, u8 *out);
-
-extern int crypto_sm3_finup(struct shash_desc *desc, const u8 *data,
-			     unsigned int len, u8 *hash);
-
 /*
  * Stand-alone implementation of the SM3 algorithm. It is designed to
  * have as little dependencies as possible so it can be used in the
