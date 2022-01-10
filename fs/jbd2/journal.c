715a67f11d6755e0cc853ff4ef539f362b566096
Yang Li <yang.lee@linux.alibaba.com>
2022-01-10 21:28:41 +0800

    jbd2: fix kernel-doc descriptions for jbd2_journal_shrink_{scan,count}()
    Add the description of @shrink and @sc in jbd2_journal_shrink_scan() and
    jbd2_journal_shrink_count() kernel-doc comment to remove warnings found
    by running scripts/kernel-doc, which is caused by using 'make W=1'.
    fs/jbd2/journal.c:1296: warning: Function parameter or member 'shrink'
    not described in 'jbd2_journal_shrink_scan'
    fs/jbd2/journal.c:1296: warning: Function parameter or member 'sc' not
    described in 'jbd2_journal_shrink_scan'
    fs/jbd2/journal.c:1320: warning: Function parameter or member 'shrink'
    not described in 'jbd2_journal_shrink_count'
    fs/jbd2/journal.c:1320: warning: Function parameter or member 'sc' not
    described in 'jbd2_journal_shrink_count'
    
    Reported-by: Abaci Robot <abaci@linux.alibaba.com>
    Signed-off-by: Yang Li <yang.lee@linux.alibaba.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Link: https://lore.kernel.org/r/20220110132841.34531-1-yang.lee@linux.alibaba.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/jbd2/journal.c
+++ fs/jbd2/journal.c
@@ -1287,6 +1287,8 @@ static int jbd2_min_tag_size(void)
 
 /**
  * jbd2_journal_shrink_scan()
+ * @shrink: shrinker to work on
+ * @sc: reclaim request to process
  *
  * Scan the checkpointed buffer on the checkpoint list and release the
  * journal_head.
@@ -1312,6 +1314,8 @@ static unsigned long jbd2_journal_shrink_scan(struct shrinker *shrink,
 
 /**
  * jbd2_journal_shrink_count()
+ * @shrink: shrinker to work on
+ * @sc: reclaim request to process
  *
  * Count the number of checkpoint buffers on the checkpoint list.
  */
