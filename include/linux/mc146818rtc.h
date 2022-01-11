cb283cca1ddc23297f61366a4363ce2b9e6f6331
Dan Carpenter <dan.carpenter@oracle.com>
2022-01-11 10:19:22 +0300

    rtc: mc146818-lib: fix signedness bug in mc146818_get_time()
    [ Upstream commit 7372971c1be5b7d4fdd8ad237798bdc1d1d54162 ]
    
    The mc146818_get_time() function returns zero on success or negative
    a error code on failure.  It needs to be type int.
    
    Fixes: d35786b3a28d ("rtc: mc146818-lib: change return values of mc146818_get_time()")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Reviewed-by: Mateusz Jończyk <mat.jonczyk@o2.pl>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Link: https://lore.kernel.org/r/20220111071922.GE11243@kili
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/linux/mc146818rtc.h
+++ include/linux/mc146818rtc.h
@@ -126,7 +126,7 @@ struct cmos_rtc_board_info {
 #endif /* ARCH_RTC_LOCATION */
 
 bool mc146818_does_rtc_work(void);
-unsigned int mc146818_get_time(struct rtc_time *time);
+int mc146818_get_time(struct rtc_time *time);
 int mc146818_set_time(struct rtc_time *time);
 
 bool mc146818_avoid_UIP(void (*callback)(unsigned char seconds, void *param),
