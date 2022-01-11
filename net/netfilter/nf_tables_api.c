cf46eacbc156a82d6643eb10afe8969abad5a35f
Pablo Neira Ayuso <pablo@netfilter.org>
2022-01-11 10:40:30 +0100

    netfilter: nf_tables: remove unused variable
    > Remove unused variable and fix missing initialization.
    >
    > >> net/netfilter/nf_tables_api.c:8266:6: warning: variable 'i' set but not used [-Wunused-but-set-variable]
    >            int i;
    >                ^
    
    Fixes: 2c865a8a28a1 ("netfilter: nf_tables: add rule blob layout")
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -8264,14 +8264,12 @@ static int nf_tables_commit_chain_prepare(struct net *net, struct nft_chain *cha
 	void *data, *data_boundary;
 	struct nft_rule_dp *prule;
 	struct nft_rule *rule;
-	int i;
 
 	/* already handled or inactive chain? */
 	if (chain->blob_next || !nft_is_active_next(net, chain))
 		return 0;
 
 	rule = list_entry(&chain->rules, struct nft_rule, list);
-	i = 0;
 
 	data_size = 0;
 	list_for_each_entry_continue(rule, &chain->rules, list) {
