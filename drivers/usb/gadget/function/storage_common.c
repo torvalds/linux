c5145ffb4a2b532a1f23c341ab67f289465e6c13
Neal Liu <neal_liu@aspeedtech.com>
2022-01-11 17:22:43 +0800

    usb gadget: remove size limitation for storage cdrom
    Remove size limitation for storage cdrom.
    
    Signed-off-by: Neal Liu <neal_liu@aspeedtech.com>
    Change-Id: I2a5e926bdf22bfb4c2ce8fca47bb16a5da20fc8d
    (cherry picked from commit 2f4ef0fe23ebee74cf4265d49f171cbe1f228947)

--- drivers/usb/gadget/function/storage_common.c
+++ drivers/usb/gadget/function/storage_common.c
@@ -242,15 +242,6 @@ int fsg_lun_open(struct fsg_lun *curlun, const char *filename)
 
 	num_sectors = size >> blkbits; /* File size in logic-block-size blocks */
 	min_sectors = 1;
-	if (curlun->cdrom) {
-		min_sectors = 300;	/* Smallest track is 300 frames */
-		if (num_sectors >= 256*60*75) {
-			num_sectors = 256*60*75 - 1;
-			LINFO(curlun, "file too big: %s\n", filename);
-			LINFO(curlun, "using only first %d blocks\n",
-					(int) num_sectors);
-		}
-	}
 	if (num_sectors < min_sectors) {
 		LINFO(curlun, "file too small: %s\n", filename);
 		rc = -ETOOSMALL;
