0d6ec07a9555759fa9e1bb88661f282435ea13de
Xiaojian Du <Xiaojian.Du@amd.com>
2022-01-11 15:48:45 +0800

    drm/amdgpu/discovery: add SMU v13.0.4 into the IP discovery list
    This patch will add SMU v13.0.4 into the IP discovery list.
    
    Signed-off-by: Xiaojian Du <Xiaojian.Du@amd.com>
    Reviewed-by: Huang Rui <ray.huang@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
@@ -1656,6 +1656,7 @@ static int amdgpu_discovery_set_smu_ip_blocks(struct amdgpu_device *adev)
 	case IP_VERSION(13, 0, 1):
 	case IP_VERSION(13, 0, 2):
 	case IP_VERSION(13, 0, 3):
+	case IP_VERSION(13, 0, 4):
 	case IP_VERSION(13, 0, 5):
 	case IP_VERSION(13, 0, 7):
 	case IP_VERSION(13, 0, 8):
