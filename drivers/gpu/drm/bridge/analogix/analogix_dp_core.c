6b9c58b8802c766ddad41afaa3c57541c849813e
Wyon bi <bivvy.bi@rock-chips.com>
2022-01-12 11:28:19 +0800

    Revert "drm/bridge: analogix_dp: Don't handle adjust request if clock recovery is already ok"
    This reverts commit 5639c343b07883a97762643dd25b75f4b4d1f445.
    
    Change-Id: Ib9391e3ae7ef1d22219c9b559789757416be81eb
    Signed-off-by: Wyon bi <bivvy.bi@rock-chips.com>

--- drivers/gpu/drm/bridge/analogix/analogix_dp_core.c
+++ drivers/gpu/drm/bridge/analogix/analogix_dp_core.c
@@ -539,16 +539,15 @@ static int analogix_dp_process_clock_recovery(struct analogix_dp_device *dp)
 				return -EIO;
 			}
 		}
-
-		analogix_dp_get_adjust_training_lane(dp, adjust_request);
-		analogix_dp_set_lane_link_training(dp);
-
-		retval = drm_dp_dpcd_write(&dp->aux, DP_TRAINING_LANE0_SET,
-					   dp->link_train.training_lane,
-					   lane_count);
-		if (retval < 0)
-			return retval;
 	}
+
+	analogix_dp_get_adjust_training_lane(dp, adjust_request);
+	analogix_dp_set_lane_link_training(dp);
+
+	retval = drm_dp_dpcd_write(&dp->aux, DP_TRAINING_LANE0_SET,
+				   dp->link_train.training_lane, lane_count);
+	if (retval < 0)
+		return retval;
 
 	return 0;
 }
