724c3be3db3413b930320cb716214e37cf0a40e2
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2022-01-13 11:14:30 +0100

    mfd: twl4030: Make twl4030_exit_irq() return void
    If twl4030_exit_irq() returns an error, the effect is that the caller
    (twl_remove()) forwards the error to the i2c core without unregistering
    its dummy slave devices. This only makes the i2c core emit another
    error message and then it still removes the device.
    
    In this situation it doesn't make sense to abort the remove cleanup and not
    unregister the slave devices. So do that. Then return value is actually
    unused and twl4030_exit_irq() can better be changed to return no value at
    all.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Link: https://lore.kernel.org/r/20220113101430.12869-3-u.kleine-koenig@pengutronix.de

--- drivers/mfd/twl-core.h
+++ drivers/mfd/twl-core.h
@@ -5,7 +5,7 @@
 extern int twl6030_init_irq(struct device *dev, int irq_num);
 extern void twl6030_exit_irq(void);
 extern int twl4030_init_irq(struct device *dev, int irq_num);
-extern int twl4030_exit_irq(void);
+extern void twl4030_exit_irq(void);
 extern int twl4030_init_chip_irq(const char *chip);
 
 #endif /*  __TWL_CORE_H__ */
