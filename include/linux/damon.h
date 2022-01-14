f4c6d22c6cf282ef7d24a724b9bd978ee2b74fc6
SeongJae Park <sj@kernel.org>
2022-01-14 14:10:14 -0800

    mm/damon: remove a mistakenly added comment for a future feature
    Due to a mistake in patches reordering, a comment for a future feature
    called 'arbitrary monitoring target support'[1], which is still under
    development, has added.  Because it only introduces confusion and we
    don't have a plan to post the patches soon, this commit removes the
    mistakenly added part.
    
    [1] https://lore.kernel.org/linux-mm/20201215115448.25633-3-sjpark@amazon.com/
    
    Link: https://lkml.kernel.org/r/20211209131806.19317-7-sj@kernel.org
    Fixes: 1f366e421c8f ("mm/damon/core: implement DAMON-based Operation Schemes (DAMOS)")
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/damon.h
+++ include/linux/damon.h
@@ -281,7 +281,7 @@ struct damon_ctx;
  * as an integer in [0, &DAMOS_MAX_SCORE].
  * @apply_scheme is called from @kdamond when a region for user provided
  * DAMON-based operation scheme is found.  It should apply the scheme's action
- * to the region.  This is not used for &DAMON_ARBITRARY_TARGET case.
+ * to the region.
  * @target_valid should check whether the target is still valid for the
  * monitoring.
  * @cleanup is called from @kdamond just before its termination.
