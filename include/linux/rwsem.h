d72d84aea4d57a735d8cfbade32ed323f47a5941
Guchun Chen <guchun.chen@amd.com>
2022-01-14 16:37:42 +0800

    locking/rwsem: drop redundant semicolon of down_write_nest_lock
    Otherwise, braces are needed when using it.
    
    Signed-off-by: Guchun Chen <guchun.chen@amd.com>
    Acked-by: Christian König <christian.koenig@amd.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220114083742.6219-1-guchun.chen@amd.com
    Signed-off-by: Christian König <christian.koenig@amd.com>

--- include/linux/rwsem.h
+++ include/linux/rwsem.h
@@ -230,7 +230,7 @@ extern void _down_write_nest_lock(struct rw_semaphore *sem, struct lockdep_map *
 do {								\
 	typecheck(struct lockdep_map *, &(nest_lock)->dep_map);	\
 	_down_write_nest_lock(sem, &(nest_lock)->dep_map);	\
-} while (0);
+} while (0)
 
 /*
  * Take/release a lock when not the owner will release it.
