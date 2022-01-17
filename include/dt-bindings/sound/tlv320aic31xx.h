6045ffd366283236f0de79c8a0e98ae766e9a8f9
Ariel D'Alessandro <ariel.dalessandro@collabora.com>
2022-01-17 10:21:06 -0300

    ASoC: tlv320aic31xx: Define PLL clock inputs
    Add constants for the different PLL clock inputs in tlv320aic31xx.
    
    Signed-off-by: Ariel D'Alessandro <ariel.dalessandro@collabora.com>
    Link: https://lore.kernel.org/r/20220117132109.283365-3-ariel.dalessandro@collabora.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/dt-bindings/sound/tlv320aic31xx.h
+++ include/dt-bindings/sound/tlv320aic31xx.h
@@ -6,4 +6,9 @@
 #define MICBIAS_2_5V		2
 #define MICBIAS_AVDDV		3
 
+#define PLL_CLKIN_MCLK		0x00
+#define PLL_CLKIN_BCLK		0x01
+#define PLL_CLKIN_GPIO1		0x02
+#define PLL_CLKIN_DIN		0x03
+
 #endif /* __DT_TLV320AIC31XX_H */
