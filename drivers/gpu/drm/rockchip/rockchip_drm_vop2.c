torvalds/linux

commit 8684b991450307081e62b93b3117399f43b7d490

drm/rockchip: vop2: power off all vop pd when enter suspend mode
Signed-off-by: Sandy Huang <hjc@rock-chips.com>
Change-Id: I992bfdec00e96c9d46681423de44fb512830bb92

drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
@@ -3187,6 +3187,7 @@ static void vop2_initial(struct drm_crtc *crtc)
 		else
 			memcpy(vop2->regsbak, vop2->regs, vop2->len);
 
+		VOP_CTRL_SET(vop2, pd_off_imd, 0);
 		VOP_MODULE_SET(vop2, wb, axi_yrgb_id, 0xd);
 		VOP_MODULE_SET(vop2, wb, axi_uv_id, 0xe);
 		vop2_wb_cfg_done(vp);
@@ -3224,6 +3225,19 @@ static void vop2_initial(struct drm_crtc *crtc)
 			      vp->id, ret);
 }
 
+static void vop2_power_off_all_pd(struct vop2 *vop2)
+{
+	struct vop2_power_domain *pd, *n;
+
+	VOP_CTRL_SET(vop2, pd_off_imd, 1);
+	list_for_each_entry_safe(pd, n, &vop2->pd_list_head, list) {
+		VOP_MODULE_SET(vop2, pd->data, pd, 1);
+		vop2_wait_power_domain_off(pd);
+		pd->on = false;
+		pd->module_on = false;
+	}
+}
+
 static void vop2_disable(struct drm_crtc *crtc)
 {
 	struct vop2_video_port *vp = to_vop2_video_port(crtc);
@@ -3243,6 +3257,8 @@ static void vop2_disable(struct drm_crtc *crtc)
 		rockchip_drm_dma_detach_device(vop2->drm_dev, vop2->dev);
 		vop2->is_iommu_enabled = false;
 	}
+	if (vop2->version == VOP_VERSION_RK3588)
+		vop2_power_off_all_pd(vop2);
 
 	pm_runtime_put_sync(vop2->dev);
 

drivers/gpu/drm/rockchip/rockchip_drm_vop.h
@@ -1033,6 +1033,7 @@ struct vop2_ctrl {
 	struct vop_reg bt1120_yc_swap;
 	struct vop_reg bt656_yc_swap;
 	struct vop_reg gamma_port_sel;
+	struct vop_reg pd_off_imd;
 
 	struct vop_reg otp_en;
 	struct vop_reg reg_done_frm;

drivers/gpu/drm/rockchip/rockchip_vop2_reg.c
@@ -2681,6 +2681,7 @@ static const struct vop2_ctrl rk3588_vop_ctrl = {
 	.dp0_pin_pol = VOP_REG(RK3568_DSP_IF_POL, 0x7, 8),
 	.dp1_pin_pol = VOP_REG(RK3568_DSP_IF_POL, 0x7, 12),
 	.gamma_port_sel = VOP_REG(RK3568_LUT_PORT_SEL, 0x3, 12),
+	.pd_off_imd = VOP_REG(RK3568_SYS_PD_CTRL, 0x1, 31),
 	.win_vp_id[ROCKCHIP_VOP2_CLUSTER0] = VOP_REG(RK3568_OVL_PORT_SEL, 0x3, 16),
 	.win_vp_id[ROCKCHIP_VOP2_CLUSTER1] = VOP_REG(RK3568_OVL_PORT_SEL, 0x3, 18),
 	.win_vp_id[ROCKCHIP_VOP2_CLUSTER2] = VOP_REG(RK3568_OVL_PORT_SEL, 0x3, 20),

commit f71b1f1b7052fb045f56976920e1a919b74cd6cb

drm/rockchip: vop2: Reset axi clk in vop2_initial
We notice a pd0 off timeout at system resume.
This because we set pd_off_imd before vop suspend.
but this will case VOP POWER_CTRL regisert record some
wrong state, and will lead a POWER_CTRL register value
change unexpectedly.

So we reset axi clk to clear this state at vop2_inital
to avoid the wrong register state.

[35.874922] [drm:vop2_crtc_atomic_enable] Update mode to 1080x1920p60, type: 16 for vp3 dclk: 132000000
[35.875412] [drm:vop2_crtc_atomic_enable] dclk_out3 div: 0 dclk_core3 div: 0
[35.875424] [drm:vop2_crtc_atomic_enable] set dclk_vop3 to 33000000, get 33000000
[35.885838] <<GTP-INF>>[gt1x_request_event_handler:1093] Request Reset.
[35.885866] <<GTP-INF>>[gt1x_reset_guitar:784] GTP RESET!
[36.073639] [drm:dw_mipi_dsi2_encoder_enable] final DSI-Link bandwidth: 880 x 4 Mbps
[36.141834] [drm:vop2_wait_power_domain_off] *ERROR* wait pd0 off timeout

Fixes: 8684b9914503("drm/rockchip: vop2: power off all vop pd when enter
suspend mode")

Signed-off-by: Andy Yan <andy.yan@rock-chips.com>
Change-Id: I974573163e35e10dc0748aadc4966219465ed603

drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
@@ -3226,6 +3226,18 @@ static void vop2_initial(struct drm_crtc *crtc)
 			return;
 		}
 
+		/*
+		 * we should rest axi to clear register state
+		 * when set pd_off_imd in vop2_power_off_all_pd,
+		 * otherwise the bit0/1/2 of POWER_CTRL register
+		 * will auto cleared after we clear pd_off_imd
+		 * in the following.
+		 */
+		if (vop2->version == VOP_VERSION_RK3588) {
+			if (!vp->loader_protect)
+				vop2_clk_reset(vop2->axi_rst);
+		}
+
 		if (vop2_soc_is_rk3566())
 			VOP_CTRL_SET(vop2, otp_en, 1);
 