d9505958e1eecaa264c3bb352080d885dbbb031f
Ard Biesheuvel <ardb@kernel.org>
2022-01-18 19:32:17 +0100

    ARM: 9180/1: Thumb2: align ALT_UP() sections in modules sufficiently
    commit 9f80ccda53b9417236945bc7ece4b519037df74d upstream.
    
    When building for Thumb2, the .alt.smp.init sections that are emitted by
    the ALT_UP() patching code may not be 32-bit aligned, even though the
    fixup_smp_on_up() routine expects that. This results in alignment faults
    at module load time, which need to be fixed up by the fault handler.
    
    So let's align those sections explicitly, and prevent this from occurring.
    
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm/include/asm/assembler.h
+++ arch/arm/include/asm/assembler.h
@@ -259,6 +259,7 @@
  */
 #define ALT_UP(instr...)					\
 	.pushsection ".alt.smp.init", "a"			;\
+	.align	2						;\
 	.long	9998b - .					;\
 9997:	instr							;\
 	.if . - 9997b == 2					;\
@@ -270,6 +271,7 @@
 	.popsection
 #define ALT_UP_B(label)					\
 	.pushsection ".alt.smp.init", "a"			;\
+	.align	2						;\
 	.long	9998b - .					;\
 	W(b)	. + (label - 9998b)					;\
 	.popsection
