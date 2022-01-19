20aa49541a2ea2cb767ada04cbcaf12fe3ca1275
kernel test robot <lkp@intel.com>
2022-01-19 11:38:36 +0800

    riscv: fix boolconv.cocci warnings
    arch/riscv/mm/init.c:48:11-16: WARNING: conversion to bool not needed here
    
     Remove unneeded conversion to bool
    
    Semantic patch information:
     Relational and logical operators evaluate to bool,
     explicit conversion is overly verbose and unneeded.
    
    Generated by: scripts/coccinelle/misc/boolconv.cocci
    
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>

--- arch/riscv/mm/init.c
+++ arch/riscv/mm/init.c
@@ -44,8 +44,7 @@ u64 satp_mode = SATP_MODE_32;
 #endif
 EXPORT_SYMBOL(satp_mode);
 
-bool pgtable_l4_enabled = IS_ENABLED(CONFIG_64BIT) && !IS_ENABLED(CONFIG_XIP_KERNEL) ?
-				true : false;
+bool pgtable_l4_enabled = IS_ENABLED(CONFIG_64BIT) && !IS_ENABLED(CONFIG_XIP_KERNEL);
 EXPORT_SYMBOL(pgtable_l4_enabled);
 
 phys_addr_t phys_ram_base __ro_after_init;
