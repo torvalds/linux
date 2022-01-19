a81399a5735de2f36ecebf2eed866902d88d7e02
David Matlack <dmatlack@google.com>
2022-01-19 23:07:33 +0000

    KVM: x86/mmu: Remove redundant role overrides for TDP MMU shadow pages
    The vCPU's mmu_role already has the correct values for direct,
    has_4_byte_gpte, access, and ad_disabled. Remove the code that was
    redundantly overwriting these fields with the same values.
    
    No functional change intended.
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: David Matlack <dmatlack@google.com>
    Message-Id: <20220119230739.2234394-13-dmatlack@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/mmu/tdp_mmu.c
+++ arch/x86/kvm/mmu/tdp_mmu.c
@@ -178,10 +178,6 @@ static union kvm_mmu_page_role page_role_for_level(struct kvm_vcpu *vcpu,
 
 	role = vcpu->arch.mmu->mmu_role.base;
 	role.level = level;
-	role.direct = true;
-	role.has_4_byte_gpte = false;
-	role.access = ACC_ALL;
-	role.ad_disabled = !shadow_accessed_mask;
 
 	return role;
 }
