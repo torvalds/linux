06c5088aeedafc06f8b33074d67e30077ba71b8b
Yafang Shao <laoar.shao@gmail.com>
2022-01-19 18:08:19 -0800

    fs/exec: replace strlcpy with strscpy_pad in __set_task_comm
    Patch series "task comm cleanups", v2.
    
    This patchset is part of the patchset "extend task comm from 16 to
    24"[1].  Now we have different opinion that dynamically allocates memory
    to store kthread's long name into a separate pointer, so I decide to
    take the useful cleanups apart from the original patchset and send it
    separately[2].
    
    These useful cleanups can make the usage around task comm less
    error-prone.  Furthermore, it will be useful if we want to extend task
    comm in the future.
    
    [1]. https://lore.kernel.org/lkml/20211101060419.4682-1-laoar.shao@gmail.com/
    [2]. https://lore.kernel.org/lkml/CALOAHbAx55AUo3bm8ZepZSZnw7A08cvKPdPyNTf=E_tPqmw5hw@mail.gmail.com/
    
    This patch (of 7):
    
    strlcpy() can trigger out-of-bound reads on the source string[1], we'd
    better use strscpy() instead.  To make it be robust against full tsk->comm
    copies that got noticed in other places, we should make sure it's zero
    padded.
    
    [1] https://github.com/KSPP/linux/issues/89
    
    Link: https://lkml.kernel.org/r/20211120112738.45980-1-laoar.shao@gmail.com
    Link: https://lkml.kernel.org/r/20211120112738.45980-2-laoar.shao@gmail.com
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Arnaldo Carvalho de Melo <arnaldo.melo@gmail.com>
    Cc: Alexei Starovoitov <alexei.starovoitov@gmail.com>
    Cc: Andrii Nakryiko <andrii.nakryiko@gmail.com>
    Cc: Michal Miroslaw <mirq-linux@rere.qmqm.pl>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Petr Mladek <pmladek@suse.com>
    Cc: Andrii Nakryiko <andrii@kernel.org>
    Cc: Dennis Dalessandro <dennis.dalessandro@cornelisnetworks.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/exec.c
+++ fs/exec.c
@@ -1222,7 +1222,7 @@ void __set_task_comm(struct task_struct *tsk, const char *buf, bool exec)
 {
 	task_lock(tsk);
 	trace_task_rename(tsk, buf);
-	strlcpy(tsk->comm, buf, sizeof(tsk->comm));
+	strscpy_pad(tsk->comm, buf, sizeof(tsk->comm));
 	task_unlock(tsk);
 	perf_event_comm(tsk, exec);
 }
