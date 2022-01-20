0fc2549d55a238da5e4c1b1ae20ebc3856922334
Alex Deucher <alexander.deucher@amd.com>
2022-01-20 12:57:33 -0500

    drm/amdgpu/display: use msleep rather than udelay for long delays
    Some architectures (e.g., ARM) throw an compilation error if the
    udelay is too long.  In general udelays of longer than 2000us are
    not recommended on any architecture.  Switch to msleep in these
    cases.
    
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_link_dp.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_link_dp.c
@@ -6935,7 +6935,7 @@ bool dpcd_write_128b_132b_sst_payload_allocation_table(
 			}
 		}
 		retries++;
-		udelay(5000);
+		msleep(5);
 	}
 
 	if (!result && retries == max_retries) {
@@ -6987,7 +6987,7 @@ bool dpcd_poll_for_allocation_change_trigger(struct dc_link *link)
 			break;
 		}
 
-		udelay(5000);
+		msleep(5);
 	}
 
 	if (result == ACT_FAILED) {
