ff9134882dfafcab4cd3204a4c901f95544d66a9
Evan Quan <evan.quan@amd.com>
2022-01-20 16:15:52 +0800

    drm/amdgpu: disable MMHUB PG for Picasso
    commit f626dd0ff05043e5a7154770cc7cda66acee33a3 upstream.
    
    MMHUB PG needs to be disabled for Picasso for stability reasons.
    
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/amd/amdgpu/soc15.c
+++ drivers/gpu/drm/amd/amdgpu/soc15.c
@@ -1194,8 +1194,11 @@ static int soc15_common_early_init(void *handle)
 				AMD_CG_SUPPORT_SDMA_MGCG |
 				AMD_CG_SUPPORT_SDMA_LS;
 
+			/*
+			 * MMHUB PG needs to be disabled for Picasso for
+			 * stability reasons.
+			 */
 			adev->pg_flags = AMD_PG_SUPPORT_SDMA |
-				AMD_PG_SUPPORT_MMHUB |
 				AMD_PG_SUPPORT_VCN;
 		} else {
 			adev->cg_flags = AMD_CG_SUPPORT_GFX_MGCG |
