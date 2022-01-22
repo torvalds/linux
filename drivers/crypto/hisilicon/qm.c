ca97dfbda5105f9b6220ff286c07367633ff2b55
Kai Ye <yekai13@huawei.com>
2022-01-22 17:30:42 +0800

    crypto: hisilicon/qm - cleanup warning in qm_vf_read_qos
    [ Upstream commit 05b3bade290d6c940701f97f3233c07cfe27205d ]
    
    The kernel test rebot report this warning: Uninitialized variable: ret.
    The code flow may return value of ret directly. This value is an
    uninitialized variable, here is fix it.
    
    Signed-off-by: Kai Ye <yekai13@huawei.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/crypto/hisilicon/qm.c
+++ drivers/crypto/hisilicon/qm.c
@@ -4107,7 +4107,7 @@ static void qm_vf_get_qos(struct hisi_qm *qm, u32 fun_num)
 static int qm_vf_read_qos(struct hisi_qm *qm)
 {
 	int cnt = 0;
-	int ret;
+	int ret = -EINVAL;
 
 	/* reset mailbox qos val */
 	qm->mb_qos = 0;
