50806fd91428a28d5daa649310dd0ca05b39c118
Mark Brown <broonie@kernel.org>
2022-01-24 17:55:26 +0000

    kselftest/arm64: Skip VL_INHERIT tests for unsupported vector types
    Currently we unconditionally test the ability to set the vector length
    inheritance flag via ptrace meaning that we generate false failures on
    systems that don't support SVE when we attempt to set the vector length
    there. Check the hwcap and mark the tests as skipped when it's not present.
    
    Fixes: 0ba1ce1e8605 ("selftests: arm64: Add coverage of ptrace flags for SVE VL inheritance")
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Reviewed-by: Shuah Khan <skhan@linuxfoundation.org>
    Link: https://lore.kernel.org/r/20220124175527.3260234-2-broonie@kernel.org
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- tools/testing/selftests/arm64/fp/sve-ptrace.c
+++ tools/testing/selftests/arm64/fp/sve-ptrace.c
@@ -557,7 +557,14 @@ static int do_parent(pid_t child)
 		}
 
 		/* prctl() flags */
-		ptrace_set_get_inherit(child, &vec_types[i]);
+		if (getauxval(vec_types[i].hwcap_type) & vec_types[i].hwcap) {
+			ptrace_set_get_inherit(child, &vec_types[i]);
+		} else {
+			ksft_test_result_skip("%s SVE_PT_VL_INHERIT set\n",
+					      vec_types[i].name);
+			ksft_test_result_skip("%s SVE_PT_VL_INHERIT cleared\n",
+					      vec_types[i].name);
+		}
 
 		/* Step through every possible VQ */
 		for (vq = SVE_VQ_MIN; vq <= SVE_VQ_MAX; vq++) {
