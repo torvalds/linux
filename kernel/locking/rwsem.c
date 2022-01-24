c23da05eac0d35441695caff0b7d0220f92ca8a0
Sangmoon Kim <sangmoon.kim@samsung.com>
2022-01-24 22:40:40 +0900

    ANDROID: Re-apply vendor hooks for information of blocked tasks
    This reverts commit 31c9ccb1389d (Revert "ANDROID: vendor_hooks: add
    waiting information for blocked tasks")
    
    And also revert portions of 396a501b1743 (Revert "ANDROID: rwsem: Add
    vendor hook to the rw-semaphore")
    
    The original patch has been reverted to resolve merge issues.
    This patch adds again the vendor hooks for the original purpose.
    
    Bug: 216016261
    
    Signed-off-by: Sangmoon Kim <sangmoon.kim@samsung.com>
    Change-Id: I04ed7b055eee40f7975bd5d74fb73dd080cd76bf

--- kernel/locking/rwsem.c
+++ kernel/locking/rwsem.c
@@ -30,6 +30,7 @@
 
 #ifndef CONFIG_PREEMPT_RT
 #include "lock_events.h"
+#include <trace/hooks/dtask.h>
 
 /*
  * The least significant 2 bits of the owner value has the following
@@ -1015,6 +1016,7 @@ queue:
 	wake_up_q(&wake_q);
 
 	/* wait to be given the lock */
+	trace_android_vh_rwsem_read_wait_start(sem);
 	for (;;) {
 		set_current_state(state);
 		if (!smp_load_acquire(&waiter.task)) {
@@ -1034,6 +1036,7 @@ queue:
 	}
 
 	__set_current_state(TASK_RUNNING);
+	trace_android_vh_rwsem_read_wait_finish(sem);
 	lockevent_inc(rwsem_rlock);
 	return sem;
 
@@ -1041,6 +1044,7 @@ out_nolock:
 	rwsem_del_waiter(sem, &waiter);
 	raw_spin_unlock_irq(&sem->wait_lock);
 	__set_current_state(TASK_RUNNING);
+	trace_android_vh_rwsem_read_wait_finish(sem);
 	lockevent_inc(rwsem_rlock_fail);
 	return ERR_PTR(-EINTR);
 }
@@ -1108,6 +1112,7 @@ rwsem_down_write_slowpath(struct rw_semaphore *sem, int state)
 
 wait:
 	/* wait until we successfully acquire the lock */
+	trace_android_vh_rwsem_write_wait_start(sem);
 	set_current_state(state);
 	for (;;) {
 		if (rwsem_try_write_lock(sem, &waiter)) {
@@ -1146,12 +1151,14 @@ trylock_again:
 		raw_spin_lock_irq(&sem->wait_lock);
 	}
 	__set_current_state(TASK_RUNNING);
+	trace_android_vh_rwsem_write_wait_finish(sem);
 	raw_spin_unlock_irq(&sem->wait_lock);
 	lockevent_inc(rwsem_wlock);
 	return sem;
 
 out_nolock:
 	__set_current_state(TASK_RUNNING);
+	trace_android_vh_rwsem_write_wait_finish(sem);
 	raw_spin_lock_irq(&sem->wait_lock);
 	rwsem_del_waiter(sem, &waiter);
 	if (!list_empty(&sem->wait_list))
