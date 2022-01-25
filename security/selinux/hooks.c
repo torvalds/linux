b084e189b01a7614d3098aca4f2381a759460d88
Christian Göttsche <cgzones@googlemail.com>
2022-01-25 15:14:18 +0100

    selinux: simplify cred_init_security
    The parameter of selinux_cred() is declared const, so an explicit cast
    dropping the const qualifier is not necessary. Without the cast the
    local variable cred serves no purpose.
    
    Reported by clang [-Wcast-qual]
    
    Signed-off-by: Christian Göttsche <cgzones@googlemail.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -211,10 +211,9 @@ static int selinux_lsm_notifier_avc_callback(u32 event)
  */
 static void cred_init_security(void)
 {
-	struct cred *cred = (struct cred *) current->real_cred;
 	struct task_security_struct *tsec;
 
-	tsec = selinux_cred(cred);
+	tsec = selinux_cred(current->real_cred);
 	tsec->osid = tsec->sid = SECINITSID_KERNEL;
 }
 
