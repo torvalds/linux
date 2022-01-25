3fd92f5be6c48cce92c90ecf71719582645da888
Mario Limonciello <mario.limonciello@amd.com>
2022-01-25 15:49:47 -0600

    display/amd: decrease message verbosity about watermarks table failure
    [ Upstream commit 03ad3093c7c069d6ab4403730009ebafeea9ee37 ]
    
    A number of BIOS versions have a problem with the watermarks table not
    being configured properly.  This manifests as a very scary looking warning
    during resume from s0i3.  This should be harmless in most cases and is well
    understood, so decrease the assertion to a clearer warning about the problem.
    
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/display/dc/clk_mgr/dcn31/dcn31_smu.c
+++ drivers/gpu/drm/amd/display/dc/clk_mgr/dcn31/dcn31_smu.c
@@ -120,7 +120,11 @@ int dcn31_smu_send_msg_with_param(
 	result = dcn31_smu_wait_for_response(clk_mgr, 10, 200000);
 
 	if (result == VBIOSSMC_Result_Failed) {
-		ASSERT(0);
+		if (msg_id == VBIOSSMC_MSG_TransferTableDram2Smu &&
+		    param == TABLE_WATERMARKS)
+			DC_LOG_WARNING("Watermarks table not configured properly by SMU");
+		else
+			ASSERT(0);
 		REG_WRITE(MP1_SMN_C2PMSG_91, VBIOSSMC_Result_OK);
 		return -1;
 	}
