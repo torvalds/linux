63fadaa2c7190ad6fc546a8b95e044bd75422dec
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2022-01-26 09:17:15 +0800

    soundwire: stream: make enable/disable/deprepare idempotent
    The stream management currently flags an 'inconsistent state' error
    when a change is requested multiple times. This was added on purpose
    to identify programming mistakes.
    
    In hindsight, there was no real reason to fail if the logic at the
    ASoC-DPCM level invokes the same callback multiple times. It's
    perfectly acceptable to just return and not flag an error when there
    is nothing to do. The main concern with the state management is to
    trap errors such as trying to enable a stream that was not prepared
    first.
    
    This patch suggests allowing the stream functions to be idempotent,
    i.e. they can be called multiple times.
    
    Note that the prepare case was already handling multiple calls, this
    was added in commit c32464c9393d ("soundwire: stream: only prepare
    stream when it is configured.")
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Rander Wang <rander.wang@intel.com>
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Link: https://lore.kernel.org/r/20220126011715.28204-20-yung-chuan.liao@linux.intel.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/soundwire/stream.c
+++ drivers/soundwire/stream.c
@@ -1505,6 +1505,11 @@ int sdw_enable_stream(struct sdw_stream_runtime *stream)
 
 	sdw_acquire_bus_lock(stream);
 
+	if (stream->state == SDW_STREAM_ENABLED) {
+		ret = 0;
+		goto state_err;
+	}
+
 	if (stream->state != SDW_STREAM_PREPARED &&
 	    stream->state != SDW_STREAM_DISABLED) {
 		pr_err("%s: %s: inconsistent state state %d\n",
@@ -1588,6 +1593,11 @@ int sdw_disable_stream(struct sdw_stream_runtime *stream)
 
 	sdw_acquire_bus_lock(stream);
 
+	if (stream->state == SDW_STREAM_DISABLED) {
+		ret = 0;
+		goto state_err;
+	}
+
 	if (stream->state != SDW_STREAM_ENABLED) {
 		pr_err("%s: %s: inconsistent state state %d\n",
 		       __func__, stream->name, stream->state);
@@ -1662,6 +1672,11 @@ int sdw_deprepare_stream(struct sdw_stream_runtime *stream)
 	}
 
 	sdw_acquire_bus_lock(stream);
+
+	if (stream->state == SDW_STREAM_DEPREPARED) {
+		ret = 0;
+		goto state_err;
+	}
 
 	if (stream->state != SDW_STREAM_PREPARED &&
 	    stream->state != SDW_STREAM_DISABLED) {
