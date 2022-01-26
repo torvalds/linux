c50df701d49e78bea6410b4b111c7be71e2a7c2b
Ville Syrjälä <ville.syrjala@linux.intel.com>
2022-01-26 10:15:39 +0200

    drm/i915: Enable rpm wakeref tracking whether runtime pm is enabled or not
    Don't see why we should skip the wakeref tracking when the
    platform doesn't support runtime pm. We still want all the
    code to be 100% leak free so let's track this unconditionally.
    
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Marco Elver <elver@google.com> # stackdepot
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Imre Deak <imre.deak@intel.com>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220126081539.23227-2-ville.syrjala@linux.intel.com
    Reviewed-by: Imre Deak <imre.deak@intel.com>

--- drivers/gpu/drm/i915/intel_runtime_pm.c
+++ drivers/gpu/drm/i915/intel_runtime_pm.c
@@ -77,9 +77,6 @@ track_intel_runtime_pm_wakeref(struct intel_runtime_pm *rpm)
 	depot_stack_handle_t stack, *stacks;
 	unsigned long flags;
 
-	if (!rpm->available)
-		return -1;
-
 	stack = __save_depot_stack();
 	if (!stack)
 		return -1;
