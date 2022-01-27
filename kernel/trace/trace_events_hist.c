78c28fdf1685e5884963018fd3692b0ff6820805
Tom Zanussi <zanussi@kernel.org>
2022-01-27 15:44:17 -0600

    tracing: Propagate is_signed to expression
    commit 097f1eefedeab528cecbd35586dfe293853ffb17 upstream.
    
    During expression parsing, a new expression field is created which
    should inherit the properties of the operands, such as size and
    is_signed.
    
    is_signed propagation was missing, causing spurious errors with signed
    operands.  Add it in parse_expr() and parse_unary() to fix the problem.
    
    Link: https://lkml.kernel.org/r/f4dac08742fd7a0920bf80a73c6c44042f5eaa40.1643319703.git.zanussi@kernel.org
    
    Cc: stable@vger.kernel.org
    Fixes: 100719dcef447 ("tracing: Add simple expression support to hist triggers")
    Reported-by: Yordan Karadzhov <ykaradzhov@vmware.com>
    BugLink: https://bugzilla.kernel.org/show_bug.cgi?id=215513
    Signed-off-by: Tom Zanussi <zanussi@kernel.org>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    [sudip: adjust context]
    Signed-off-by: Sudip Mukherjee <sudipm.mukherjee@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/trace/trace_events_hist.c
+++ kernel/trace/trace_events_hist.c
@@ -2220,6 +2220,8 @@ static struct hist_field *parse_unary(struct hist_trigger_data *hist_data,
 		(HIST_FIELD_FL_TIMESTAMP | HIST_FIELD_FL_TIMESTAMP_USECS);
 	expr->fn = hist_field_unary_minus;
 	expr->operands[0] = operand1;
+	expr->size = operand1->size;
+	expr->is_signed = operand1->is_signed;
 	expr->operator = FIELD_OP_UNARY_MINUS;
 	expr->name = expr_str(expr, 0);
 	expr->type = kstrdup_const(operand1->type, GFP_KERNEL);
@@ -2359,6 +2361,7 @@ static struct hist_field *parse_expr(struct hist_trigger_data *hist_data,
 
 	/* The operand sizes should be the same, so just pick one */
 	expr->size = operand1->size;
+	expr->is_signed = operand1->is_signed;
 
 	expr->operator = field_op;
 	expr->name = expr_str(expr, 0);
