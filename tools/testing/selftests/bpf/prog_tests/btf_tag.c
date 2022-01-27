571d01a9d06f984ce51bd7625036e67f7f1f886c
Yonghong Song <yhs@fb.com>
2022-01-27 07:46:11 -0800

    selftests/bpf: rename btf_decl_tag.c to test_btf_decl_tag.c
    The uapi btf.h contains the following declaration:
      struct btf_decl_tag {
           __s32   component_idx;
      };
    
    The skeleton will also generate a struct with name
    "btf_decl_tag" for bpf program btf_decl_tag.c.
    
    Rename btf_decl_tag.c to test_btf_decl_tag.c so
    the corresponding skeleton struct name becomes
    "test_btf_decl_tag". This way, we could include
    uapi btf.h in prog_tests/btf_tag.c.
    There is no functionality change for this patch.
    
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Link: https://lore.kernel.org/r/20220127154611.656699-1-yhs@fb.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/testing/selftests/bpf/prog_tests/btf_tag.c
+++ tools/testing/selftests/bpf/prog_tests/btf_tag.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /* Copyright (c) 2021 Facebook */
 #include <test_progs.h>
-#include "btf_decl_tag.skel.h"
+#include "test_btf_decl_tag.skel.h"
 
 /* struct btf_type_tag_test is referenced in btf_type_tag.skel.h */
 struct btf_type_tag_test {
@@ -11,9 +11,9 @@ struct btf_type_tag_test {
 
 static void test_btf_decl_tag(void)
 {
-	struct btf_decl_tag *skel;
+	struct test_btf_decl_tag *skel;
 
-	skel = btf_decl_tag__open_and_load();
+	skel = test_btf_decl_tag__open_and_load();
 	if (!ASSERT_OK_PTR(skel, "btf_decl_tag"))
 		return;
 
@@ -22,7 +22,7 @@ static void test_btf_decl_tag(void)
 		test__skip();
 	}
 
-	btf_decl_tag__destroy(skel);
+	test_btf_decl_tag__destroy(skel);
 }
 
 static void test_btf_type_tag(void)
