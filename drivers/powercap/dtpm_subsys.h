e446556173170e675a7a321e76ce5fa3587de724
Daniel Lezcano <daniel.lezcano@linaro.org>
2022-01-28 17:35:36 +0100

    powercap/drivers/dtpm: Add dtpm devfreq with energy model support
    Currently the dtpm supports the CPUs via cpufreq and the energy
    model. This change provides the same for the device which supports
    devfreq.
    
    Each device supporting devfreq and having an energy model can be added
    to the hierarchy.
    
    The concept is the same as the cpufreq DTPM support: the QoS is used
    to aggregate the requests and the energy model gives the value of the
    instantaneous power consumption ponderated by the load of the device.
    
    Cc: Chanwoo Choi <cwchoi00@gmail.com>
    Cc: Lukasz Luba <lukasz.luba@arm.com>
    Cc: Kyungmin Park <kyungmin.park@samsung.com>
    Cc: MyungJoo Ham <myungjoo.ham@samsung.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/20220128163537.212248-5-daniel.lezcano@linaro.org

--- drivers/powercap/dtpm_subsys.h
+++ drivers/powercap/dtpm_subsys.h
@@ -8,10 +8,14 @@
 #define ___DTPM_SUBSYS_H__
 
 extern struct dtpm_subsys_ops dtpm_cpu_ops;
+extern struct dtpm_subsys_ops dtpm_devfreq_ops;
 
 struct dtpm_subsys_ops *dtpm_subsys[] = {
 #ifdef CONFIG_DTPM_CPU
 	&dtpm_cpu_ops,
+#endif
+#ifdef CONFIG_DTPM_DEVFREQ
+	&dtpm_devfreq_ops,
 #endif
 };
 
