ab54b196c98f18b6fa3dfe074c8377d07e08202c
Abdun Nihaal <abdun.nihaal@gmail.com>
2022-01-29 21:57:56 +0530

    staging: r8188eu: remove unneeded variable in rtw_p2p_get_wps_configmethod
    Remove unneeded return variable that is initialized to 0 and not
    assigned after.
    
    Found using Coccinelle
    
    Signed-off-by: Abdun Nihaal <abdun.nihaal@gmail.com>
    Link: https://lore.kernel.org/r/ae35e6f6f603ef83c2bad685ac247273041bd138.1643466748.git.abdun.nihaal@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/r8188eu/os_dep/ioctl_linux.c
+++ drivers/staging/r8188eu/os_dep/ioctl_linux.c
@@ -2620,7 +2620,6 @@ static int rtw_p2p_get_wps_configmethod(struct net_device *dev,
 			       struct iw_request_info *info,
 			       union iwreq_data *wrqu, char *extra)
 {
-	int ret = 0;
 	struct adapter *padapter = (struct adapter *)rtw_netdev_priv(dev);
 	u8 peerMAC[ETH_ALEN] = {0x00};
 	int jj, kk;
@@ -2681,7 +2680,7 @@ static int rtw_p2p_get_wps_configmethod(struct net_device *dev,
 
 	if (copy_to_user(wrqu->data.pointer, attr_content_str, 6 + 17))
 		return -EFAULT;
-	return ret;
+	return 0;
 }
 
 static int rtw_p2p_get_go_device_address(struct net_device *dev,
