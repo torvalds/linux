c4c8451147da569a79eff5edcd2864e8ee21d7aa
Al Viro <viro@zeniv.linux.org.uk>
2022-01-30 21:12:26 -0500

    d_path.c: typo fix...
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/d_path.c
+++ fs/d_path.c
@@ -34,7 +34,7 @@ static bool prepend_char(struct prepend_buffer *p, unsigned char c)
 }
 
 /*
- * The source of the prepend data can be an optimistoc load
+ * The source of the prepend data can be an optimistic load
  * of a dentry name and length. And because we don't hold any
  * locks, the length and the pointer to the name may not be
  * in sync if a concurrent rename happens, and the kernel
