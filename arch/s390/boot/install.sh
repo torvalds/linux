d24e18ef7e13c9cd580ebee771f7ccb9d3f6ee42
Marc Hartmayer <mhartmay@linux.ibm.com>
2022-01-31 13:22:31 +0000

    s390/boot: improve install.sh script
    Use proper quoting for the variables and explicitly distinguish between
    command options and positional arguments.
    
    Acked-by: Heiko Carstens <hca@linux.ibm.com>
    Signed-off-by: Marc Hartmayer <mhartmay@linux.ibm.com>
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>

--- arch/s390/boot/install.sh
+++ arch/s390/boot/install.sh
@@ -17,8 +17,8 @@
 
 echo "Warning: '${INSTALLKERNEL}' command not available - additional " \
      "bootloader config required" >&2
-if [ -f $4/vmlinuz-$1 ]; then mv $4/vmlinuz-$1 $4/vmlinuz-$1.old; fi
-if [ -f $4/System.map-$1 ]; then mv $4/System.map-$1 $4/System.map-$1.old; fi
+if [ -f "$4/vmlinuz-$1" ]; then mv -- "$4/vmlinuz-$1" "$4/vmlinuz-$1.old"; fi
+if [ -f "$4/System.map-$1" ]; then mv -- "$4/System.map-$1" "$4/System.map-$1.old"; fi
 
-cat $2 > $4/vmlinuz-$1
-cp $3 $4/System.map-$1
+cat -- "$2" > "$4/vmlinuz-$1"
+cp -- "$3" "$4/System.map-$1"
