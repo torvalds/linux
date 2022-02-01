2d6c9810eb8915c4ddede707b8e167a1d919e1ca
Nathan Chancellor <nathan@kernel.org>
2022-02-01 13:56:22 -0700

    scripts/pahole-flags.sh: Use pahole-version.sh
    Use pahole-version.sh to get pahole's version code to reduce the amount
    of duplication across the tree.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20220201205624.652313-4-nathan@kernel.org

--- scripts/pahole-flags.sh
+++ scripts/pahole-flags.sh
@@ -7,7 +7,7 @@ if ! [ -x "$(command -v ${PAHOLE})" ]; then
 	exit 0
 fi
 
-pahole_ver=$(${PAHOLE} --version | sed -E 's/v([0-9]+)\.([0-9]+)/\1\2/')
+pahole_ver=$($(dirname $0)/pahole-version.sh ${PAHOLE})
 
 if [ "${pahole_ver}" -ge "118" ] && [ "${pahole_ver}" -le "121" ]; then
 	# pahole 1.18 through 1.21 can't handle zero-sized per-CPU vars
