df507a7f8675c4b597664745283ee6eb67e172e9
Yang Li <yang.lee@linux.alibaba.com>
2022-02-01 12:15:48 +0800

    wcn36xx: clean up some inconsistent indenting
    Eliminate the follow smatch warnings:
    drivers/net/wireless/ath/wcn36xx/main.c:1394 wcn36xx_get_survey() warn:
    inconsistent indenting
    drivers/net/wireless/ath/wcn36xx/txrx.c:379 wcn36xx_rx_skb() warn:
    inconsistent indenting
    
    Reported-by: Abaci Robot <abaci@linux.alibaba.com>
    Signed-off-by: Yang Li <yang.lee@linux.alibaba.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://lore.kernel.org/r/20220201041548.18464-1-yang.lee@linux.alibaba.com

--- drivers/net/wireless/ath/wcn36xx/main.c
+++ drivers/net/wireless/ath/wcn36xx/main.c
@@ -1391,11 +1391,11 @@ static int wcn36xx_get_survey(struct ieee80211_hw *hw, int idx,
 
 	spin_unlock_irqrestore(&wcn->survey_lock, flags);
 
-	 wcn36xx_dbg(WCN36XX_DBG_MAC,
-		     "ch %d rssi %d snr %d noise %d filled %x freq %d\n",
-		     HW_VALUE_CHANNEL(survey->channel->hw_value),
-		     chan_survey->rssi, chan_survey->snr, survey->noise,
-		     survey->filled, survey->channel->center_freq);
+	wcn36xx_dbg(WCN36XX_DBG_MAC,
+		    "ch %d rssi %d snr %d noise %d filled %x freq %d\n",
+		    HW_VALUE_CHANNEL(survey->channel->hw_value),
+		    chan_survey->rssi, chan_survey->snr, survey->noise,
+		    survey->filled, survey->channel->center_freq);
 
 	return 0;
 }
