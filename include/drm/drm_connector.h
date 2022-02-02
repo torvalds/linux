b9cf1208af36f6552e6f3b8fdf364983e3cc8e60
Maxime Ripard <maxime@cerno.tech>
2022-02-02 10:43:40 +0100

    drm/connector: Fix typo in documentation
    commit dca384a3bf5af1c781cfa6aec63904bdb5018c36 upstream.
    
    Commit 4adc33f36d80 ("drm/edid: Split deep color modes between RGB and
    YUV444") introduced two new variables in struct drm_display_info and
    their documentation, but the documentation part had a typo resulting in
    a doc build warning.
    
    Fixes: 4adc33f36d80 ("drm/edid: Split deep color modes between RGB and YUV444")
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>
    Reviewed-by: Simon Ser <contact@emersion.fr>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220202094340.875190-1-maxime@cerno.tech
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/drm/drm_connector.h
+++ include/drm/drm_connector.h
@@ -566,13 +566,13 @@ struct drm_display_info {
 	bool rgb_quant_range_selectable;
 
 	/**
-	 * @edid_hdmi_dc_rgb444_modes: Mask of supported hdmi deep color modes
+	 * @edid_hdmi_rgb444_dc_modes: Mask of supported hdmi deep color modes
 	 * in RGB 4:4:4. Even more stuff redundant with @bus_formats.
 	 */
 	u8 edid_hdmi_rgb444_dc_modes;
 
 	/**
-	 * @edid_hdmi_dc_ycbcr444_modes: Mask of supported hdmi deep color
+	 * @edid_hdmi_ycbcr444_dc_modes: Mask of supported hdmi deep color
 	 * modes in YCbCr 4:4:4. Even more stuff redundant with @bus_formats.
 	 */
 	u8 edid_hdmi_ycbcr444_dc_modes;
