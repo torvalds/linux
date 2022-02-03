080822563b35033595575ebf84568263ce8f6d61
Tobias Waldekranz <tobias@waldekranz.com>
2022-02-03 11:16:55 +0100

    net: dsa: mv88e6xxx: Enable port policy support on 6097
    [ Upstream commit 585d42bb57bb358d48906660a8de273b078810b1 ]
    
    This chip has support for the same per-port policy actions found in
    later versions of LinkStreet devices.
    
    Fixes: f3a2cd326e44 ("net: dsa: mv88e6xxx: introduce .port_set_policy")
    Signed-off-by: Tobias Waldekranz <tobias@waldekranz.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/dsa/mv88e6xxx/chip.c
+++ drivers/net/dsa/mv88e6xxx/chip.c
@@ -3297,6 +3297,7 @@ static const struct mv88e6xxx_ops mv88e6097_ops = {
 	.port_set_link = mv88e6xxx_port_set_link,
 	.port_set_speed_duplex = mv88e6185_port_set_speed_duplex,
 	.port_tag_remap = mv88e6095_port_tag_remap,
+	.port_set_policy = mv88e6352_port_set_policy,
 	.port_set_frame_mode = mv88e6351_port_set_frame_mode,
 	.port_set_egress_floods = mv88e6352_port_set_egress_floods,
 	.port_set_ether_type = mv88e6351_port_set_ether_type,
