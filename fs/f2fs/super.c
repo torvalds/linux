594835143a7065dd474688640c06b4179a647826
Chao Yu <chao@kernel.org>
2022-02-04 15:19:46 +0800

    BACKPORT: f2fs: introduce F2FS_IPU_HONOR_OPU_WRITE ipu policy
    Once F2FS_IPU_FORCE policy is enabled in some cases:
    a) f2fs forces to use F2FS_IPU_FORCE in a small-sized volume
    b) user sets F2FS_IPU_FORCE policy via sysfs
    
    Then we may fail to defragment file due to IPU policy check, it doesn't
    make sense, let's introduce a new IPU policy to allow OPU during file
    defragmentation.
    
    In small-sized volume, let's enable F2FS_IPU_HONOR_OPU_WRITE policy
    by default.
    
    Bug: 244657983
    
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit 1018a5463a063715365784704c4e8cdf2eec4b04)
    Change-Id: I05dfa5a07a6a17dcda68f50a8f4a8260c2612dcc

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -3771,7 +3771,8 @@ static void f2fs_tuning_parameters(struct f2fs_sb_info *sbi)
 	if (sm_i->main_segments <= SMALL_VOLUME_SEGMENTS) {
 		F2FS_OPTION(sbi).alloc_mode = ALLOC_MODE_REUSE;
 		sm_i->dcc_info->discard_granularity = 1;
-		sm_i->ipu_policy = 1 << F2FS_IPU_FORCE;
+		sm_i->ipu_policy = 1 << F2FS_IPU_FORCE |
+					1 << F2FS_IPU_HONOR_OPU_WRITE;
 	}
 
 	sbi->readdir_ra = 1;
