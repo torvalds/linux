a410a0cf98854a698a519bfbeb604145da384c0e
Guillaume Nault <gnault@redhat.com>
2022-02-04 14:58:11 +0100

    ipv6: Define dscp_t and stop taking ECN bits into account in fib6-rules
    Define a dscp_t type and its appropriate helpers that ensure ECN bits
    are not taken into account when handling DSCP.
    
    Use this new type to replace the tclass field of struct fib6_rule, so
    that fib6-rules don't get influenced by ECN bits anymore.
    
    Before this patch, fib6-rules didn't make any distinction between the
    DSCP and ECN bits. Therefore, rules specifying a DSCP (tos or dsfield
    options in iproute2) stopped working as soon a packets had at least one
    of its ECN bits set (as a work around one could create four rules for
    each DSCP value to match, one for each possible ECN value).
    
    After this patch fib6-rules only compare the DSCP bits. ECN doesn't
    influence the result anymore. Also, fib6-rules now must have the ECN
    bits cleared or they will be rejected.
    
    Signed-off-by: Guillaume Nault <gnault@redhat.com>
    Acked-by: David Ahern <dsahern@kernel.org>
    Reviewed-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/net/ipv6.h
+++ include/net/ipv6.h
@@ -17,6 +17,7 @@
 #include <net/if_inet6.h>
 #include <net/flow.h>
 #include <net/flow_dissector.h>
+#include <net/inet_dscp.h>
 #include <net/snmp.h>
 #include <net/netns/hash.h>
 
@@ -972,6 +973,11 @@ static inline __be32 ip6_flowlabel(const struct ipv6hdr *hdr)
 static inline u8 ip6_tclass(__be32 flowinfo)
 {
 	return ntohl(flowinfo & IPV6_TCLASS_MASK) >> IPV6_TCLASS_SHIFT;
+}
+
+static inline dscp_t ip6_dscp(__be32 flowinfo)
+{
+	return inet_dsfield_to_dscp(ip6_tclass(flowinfo));
 }
 
 static inline __be32 ip6_make_flowinfo(unsigned int tclass, __be32 flowlabel)
