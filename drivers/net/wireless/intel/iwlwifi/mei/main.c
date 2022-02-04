cdf3949f24f7beefbcced7fb8c06aa22d54279d1
Johannes Berg <johannes.berg@intel.com>
2022-02-04 12:25:10 +0200

    iwlwifi: mei: use C99 initializer for device IDs
    There's another field and so W=2 warns about this code, but
    in general it's nicer to use C99 initializers anyway to make
    the code easier to read.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Link: https://lore.kernel.org/r/iwlwifi.20220204122220.b5efbbd83c9c.I908128aa8249565ff1c5496ca5c5597efea7cb39@changeid
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/mei/main.c
+++ drivers/net/wireless/intel/iwlwifi/mei/main.c
@@ -1980,7 +1980,11 @@ static void iwl_mei_remove(struct mei_cl_device *cldev)
 }
 
 static const struct mei_cl_device_id iwl_mei_tbl[] = {
-	{ KBUILD_MODNAME, MEI_WLAN_UUID, MEI_CL_VERSION_ANY},
+	{
+		.name = KBUILD_MODNAME,
+		.uuid = MEI_WLAN_UUID,
+		.version = MEI_CL_VERSION_ANY,
+	},
 
 	/* required last entry */
 	{ }
