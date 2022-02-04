cf1c7fee7ef37cfc09b5e704eb52d9466ca49012
Tomohito Esaki <etom@igel.co.jp>
2022-02-04 11:36:35 +0900

    drm/sprd: remove allow_fb_modifiers setting
    Remove allow_fb_modifiers setting in this driver. The allow_fb_modifiers
    flag was removed.
    
    Signed-off-by: Tomohito Esaki <etom@igel.co.jp>
    Fixes: 3d082157a242 ("drm: remove allow_fb_modifiers")
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220204023635.15496-1-etom@igel.co.jp

--- drivers/gpu/drm/sprd/sprd_drm.c
+++ drivers/gpu/drm/sprd/sprd_drm.c
@@ -43,7 +43,6 @@ static void sprd_drm_mode_config_init(struct drm_device *drm)
 	drm->mode_config.min_height = 0;
 	drm->mode_config.max_width = 8192;
 	drm->mode_config.max_height = 8192;
-	drm->mode_config.allow_fb_modifiers = true;
 
 	drm->mode_config.funcs = &sprd_drm_mode_config_funcs;
 	drm->mode_config.helper_private = &sprd_drm_mode_config_helper;
