2b2f53331b3ae717875cd74b35149c0e5a6726a5
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2022-02-05 19:01:29 +0100

    Revert "drm/vc4: hdmi: Make sure the device is powered with CEC" again
    This reverts commit fbe1e801bc1fa0a4c34a5284abf6ad0ef887cdfc which is
    commit 20b0dfa86bef0e80b41b0e5ac38b92f23b6f27f9 upstream.
    
    It wasn't applied correctly, something went wrong with an attempt to fix
    it up again, so just revert the whole thing to be back at a clean state.
    
    Reported-by: Guenter Roeck <linux@roeck-us.net>
    Link: https://lore.kernel.org/r/20220205171238.GA3073350@roeck-us.net
    Reported-by: Alexey Khoroshilov <khoroshilov@ispras.ru>
    Link: https://lore.kernel.org/r/Yf5lNIJnvhP4ajam@kroah.com
    Cc: Dave Stevenson <dave.stevenson@raspberrypi.com>
    Cc: Maxime Ripard <maxime@cerno.tech>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/vc4/vc4_hdmi.c
+++ drivers/gpu/drm/vc4/vc4_hdmi.c
@@ -1735,14 +1735,8 @@ static int vc4_hdmi_cec_adap_enable(struct cec_adapter *adap, bool enable)
 	struct vc4_hdmi *vc4_hdmi = cec_get_drvdata(adap);
 	/* clock period in microseconds */
 	const u32 usecs = 1000000 / CEC_CLOCK_FREQ;
-	u32 val;
-	int ret;
+	u32 val = HDMI_READ(HDMI_CEC_CNTRL_5);
 
-	ret = pm_runtime_resume_and_get(&vc4_hdmi->pdev->dev);
-	if (ret)
-		return ret;
-
-	val = HDMI_READ(HDMI_CEC_CNTRL_5);
 	val &= ~(VC4_HDMI_CEC_TX_SW_RESET | VC4_HDMI_CEC_RX_SW_RESET |
 		 VC4_HDMI_CEC_CNT_TO_4700_US_MASK |
 		 VC4_HDMI_CEC_CNT_TO_4500_US_MASK);
@@ -1887,8 +1881,6 @@ static int vc4_hdmi_cec_init(struct vc4_hdmi *vc4_hdmi)
 	ret = cec_register_adapter(vc4_hdmi->cec_adap, &pdev->dev);
 	if (ret < 0)
 		goto err_remove_handlers;
-
-	pm_runtime_put(&vc4_hdmi->pdev->dev);
 
 	return 0;
 
