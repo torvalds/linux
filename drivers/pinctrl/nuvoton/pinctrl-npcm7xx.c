ecfc3f8a635054e05dd0a73ca3ad7d46330bbcec
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2022-02-05 16:53:30 +0100

    pinctrl: nuvoton: npcm7xx: Use %zu printk format for ARRAY_SIZE()
    commit 9d0f18bca3b557ae5d2128661ac06d33b3f45c0a upstream.
    
    When compile-testing on 64-bit architectures, GCC complains about the
    mismatch of types between the %d format specifier and value returned by
    ARRAY_LENGTH(). Use %zu, which is correct everywhere.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Fixes: 3b588e43ee5c7 ("pinctrl: nuvoton: add NPCM7xx pinctrl and GPIO driver")
    Signed-off-by: Jonathan Neuschäfer <j.neuschaefer@gmx.net>
    Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Link: https://lore.kernel.org/r/20220205155332.1308899-2-j.neuschaefer@gmx.net
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/pinctrl/nuvoton/pinctrl-npcm7xx.c
+++ drivers/pinctrl/nuvoton/pinctrl-npcm7xx.c
@@ -1560,7 +1560,7 @@ static int npcm7xx_get_groups_count(struct pinctrl_dev *pctldev)
 {
 	struct npcm7xx_pinctrl *npcm = pinctrl_dev_get_drvdata(pctldev);
 
-	dev_dbg(npcm->dev, "group size: %d\n", ARRAY_SIZE(npcm7xx_groups));
+	dev_dbg(npcm->dev, "group size: %zu\n", ARRAY_SIZE(npcm7xx_groups));
 	return ARRAY_SIZE(npcm7xx_groups);
 }
 
