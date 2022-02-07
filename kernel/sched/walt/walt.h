87952a7cb165c593d4aba26a870f9d5645a1c2ae
Ashay Jaiswal <quic_ashayj@quicinc.com>
2022-02-07 14:32:27 +0530

    sched: walt: update the copyright
    Update copyright of "walt.h".
    
    Change-Id: I2755f597f16904e54579c7e3a24917f41d7a35f7
    Signed-off-by: Ashay Jaiswal <quic_ashayj@quicinc.com>

--- kernel/sched/walt/walt.h
+++ kernel/sched/walt/walt.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2019-2022, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _WALT_H
