930c4acc064edacd2c9d5bec1a66acacb2fb2589
Willy Tarreau <w@1wt.eu>
2022-02-07 17:23:14 +0100

    tools/nolibc: guard the main file against multiple inclusion
    Including nolibc.h multiple times results in build errors due to multiple
    definitions. Let's add a guard against multiple inclusions.
    
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- tools/include/nolibc/nolibc.h
+++ tools/include/nolibc/nolibc.h
@@ -80,6 +80,8 @@
  *      https://w3challs.com/syscalls/
  *
  */
+#ifndef _NOLIBC_H
+#define _NOLIBC_H
 
 #include <asm/unistd.h>
 #include <asm/ioctls.h>
@@ -2582,3 +2584,5 @@ dev_t makedev(unsigned int major, unsigned int minor)
 {
 	return ((major & 0xfff) << 8) | (minor & 0xff);
 }
+
+#endif /* _NOLIBC_H */
