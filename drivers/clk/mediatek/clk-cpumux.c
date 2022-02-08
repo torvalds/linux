4e94ea5432f58d1454d4ac9478a6dec951c077d2
Chen-Yu Tsai <wenst@chromium.org>
2022-02-08 20:40:24 +0800

    clk: mediatek: cpumux: Implement error handling in register API
    The cpumux clk type registration function does not stop or return errors
    if any clk failed to be registered, nor does it implement an error
    handling path. This may result in a partially working device if any
    step failed.
    
    Make the register function return proper error codes, and bail out if
    errors occur. Proper cleanup, i.e. unregister any clks that were
    successfully registered, is done in the new error path.
    
    Signed-off-by: Chen-Yu Tsai <wenst@chromium.org>
    Reviewed-by: Miles Chen <miles.chen@mediatek.com>
    Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Link: https://lore.kernel.org/r/20220208124034.414635-22-wenst@chromium.org
    Reviewed-by: Chun-Jie Chen <chun-jie.chen@mediatek.com>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/mediatek/clk-cpumux.c
+++ drivers/clk/mediatek/clk-cpumux.c
@@ -123,13 +123,26 @@ int mtk_clk_register_cpumuxes(struct device_node *node,
 		clk = mtk_clk_register_cpumux(mux, regmap);
 		if (IS_ERR(clk)) {
 			pr_err("Failed to register clk %s: %pe\n", mux->name, clk);
-			continue;
+			goto err;
 		}
 
 		clk_data->clks[mux->id] = clk;
 	}
 
 	return 0;
+
+err:
+	while (--i >= 0) {
+		const struct mtk_composite *mux = &clks[i];
+
+		if (IS_ERR_OR_NULL(clk_data->clks[mux->id]))
+			continue;
+
+		mtk_clk_unregister_cpumux(clk_data->clks[mux->id]);
+		clk_data->clks[mux->id] = ERR_PTR(-ENOENT);
+	}
+
+	return PTR_ERR(clk);
 }
 
 void mtk_clk_unregister_cpumuxes(const struct mtk_composite *clks, int num,
