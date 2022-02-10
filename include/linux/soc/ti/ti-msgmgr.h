df227dc8a68d81b7fe3416eca16d1f21d0b537f0
Dave Gerlach <d-gerlach@ti.com>
2022-02-09 22:16:31 -0600

    mailbox: ti-msgmgr: Operate mailbox in polled mode during system suspend
    During the system suspend path we must set all queues to operate in
    polled mode as it is possible for any protocol built using this mailbox,
    such as TISCI, to require communication during the no irq phase of suspend,
    and we cannot rely on interrupts there.
    
    Polled mode is implemented by allowing the mailbox user to define an
    RX channel as part of the message that is sent which is what gets polled
    for a response. If polled mode is enabled, this will immediately be
    polled for a response at the end of the mailbox send_data op before
    returning success for the data send or timing out if no response is
    received.
    
    Finally, to ensure polled mode is always enabled during system suspend,
    iterate through all queues to set RX queues to polled mode during system
    suspend and disable polled mode for all in the resume handler.
    
    Signed-off-by: Dave Gerlach <d-gerlach@ti.com>
    Signed-off-by: Jassi Brar <jaswinder.singh@linaro.org>

--- include/linux/soc/ti/ti-msgmgr.h
+++ include/linux/soc/ti/ti-msgmgr.h
@@ -1,7 +1,7 @@
 /*
  * Texas Instruments' Message Manager
  *
- * Copyright (C) 2015-2016 Texas Instruments Incorporated - https://www.ti.com/
+ * Copyright (C) 2015-2022 Texas Instruments Incorporated - https://www.ti.com/
  *	Nishanth Menon
  *
  * This program is free software; you can redistribute it and/or modify
@@ -17,10 +17,14 @@
 #ifndef TI_MSGMGR_H
 #define TI_MSGMGR_H
 
+struct mbox_chan;
+
 /**
  * struct ti_msgmgr_message - Message Manager structure
  * @len: Length of data in the Buffer
  * @buf: Buffer pointer
+ * @chan_rx: Expected channel for response, must be provided to use polled rx
+ * @timeout_rx_ms: Timeout value to use if polling for response
  *
  * This is the structure for data used in mbox_send_message
  * the length of data buffer used depends on the SoC integration
@@ -30,6 +34,8 @@
 struct ti_msgmgr_message {
 	size_t len;
 	u8 *buf;
+	struct mbox_chan *chan_rx;
+	int timeout_rx_ms;
 };
 
 #endif /* TI_MSGMGR_H */
