149d7ba1f8fe515a2a36ff95fa659720e72fe4ed
yipechai <YiPeng.Chai@amd.com>
2022-02-14 16:15:19 +0800

    drm/amdgpu: Remove redundant calls of amdgpu_ras_block_late_fini in sdma ras block
    Remove redundant calls of amdgpu_ras_block_late_fini in sdma ras block.
    
    Signed-off-by: yipechai <YiPeng.Chai@amd.com>
    Reviewed-by: Tao Zhou <tao.zhou1@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/sdma_v4_0.c
+++ drivers/gpu/drm/amd/amdgpu/sdma_v4_0.c
@@ -2824,7 +2824,7 @@ static void sdma_v4_0_set_ras_funcs(struct amdgpu_device *adev)
 
 		/* If don't define special ras_fini function, use default ras_fini */
 		if (!adev->sdma.ras->ras_block.ras_fini)
-			adev->sdma.ras->ras_block.ras_fini = amdgpu_sdma_ras_fini;
+			adev->sdma.ras->ras_block.ras_fini = amdgpu_ras_block_late_fini;
 
 		/* If not defined special ras_cb function, use default ras_cb */
 		if (!adev->sdma.ras->ras_block.ras_cb)
