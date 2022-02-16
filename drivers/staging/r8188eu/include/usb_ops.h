475dae07f1cd67ff0be30b2d42fc381d4cadfe18
Phillip Potter <phil@philpotter.co.uk>
2022-02-16 01:07:02 +0000

    staging: r8188eu: remove remaining DBG_88E call from include/usb_ops.h
    Remove the one remaining DBG_88E call from include/usb_ops.h. After
    some thought, it makes more sense to just entirely strip all of these
    calls, so that debugging code in the driver can be more consistent and
    useful going forwards.
    
    Signed-off-by: Phillip Potter <phil@philpotter.co.uk>
    Link: https://lore.kernel.org/r/20220216010709.791-9-phil@philpotter.co.uk
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/r8188eu/include/usb_ops.h
+++ drivers/staging/r8188eu/include/usb_ops.h
@@ -31,8 +31,6 @@ static inline int rtw_inc_and_chk_continual_urb_error(struct dvobj_priv *dvobj)
 	int value;
 	value = atomic_inc_return(&dvobj->continual_urb_error);
 	if (value > MAX_CONTINUAL_URB_ERR) {
-		DBG_88E("[dvobj:%p][ERROR] continual_urb_error:%d > %d\n",
-			dvobj, value, MAX_CONTINUAL_URB_ERR);
 		ret = true;
 	}
 	return ret;
