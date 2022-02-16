60f8ad2392d03d250f6d381e382f7c838cd5ec11
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2022-02-16 21:16:16 +0100

    net: ll_temac: Use GFP_KERNEL instead of GFP_ATOMIC when possible
    XTE_MAX_JUMBO_FRAME_SIZE is over 9000 bytes and the default value for
    'rx_bd_num' is RX_BD_NUM_DEFAULT	(i.e. 1024)
    
    So this loop allocates more than 9 Mo of memory.
    
    Previous memory allocations in this function already use GFP_KERNEL, so
    use __netdev_alloc_skb_ip_align() and an explicit GFP_KERNEL instead of a
    implicit GFP_ATOMIC.
    
    This gives more opportunities of successful allocation.
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Link: https://lore.kernel.org/r/694abd65418b2b3974106a82d758e3474c65ae8f.1645042560.git.christophe.jaillet@wanadoo.fr
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/xilinx/ll_temac_main.c
+++ drivers/net/ethernet/xilinx/ll_temac_main.c
@@ -361,8 +361,9 @@ static int temac_dma_bd_init(struct net_device *ndev)
 		lp->rx_bd_v[i].next = cpu_to_be32(lp->rx_bd_p
 			+ sizeof(*lp->rx_bd_v) * ((i + 1) % lp->rx_bd_num));
 
-		skb = netdev_alloc_skb_ip_align(ndev,
-						XTE_MAX_JUMBO_FRAME_SIZE);
+		skb = __netdev_alloc_skb_ip_align(ndev,
+						  XTE_MAX_JUMBO_FRAME_SIZE,
+						  GFP_KERNEL);
 		if (!skb)
 			goto out;
 
