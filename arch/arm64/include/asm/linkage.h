0f61f6be1f7f44edfab0cb731c0a2340a838956f
Mark Rutland <mark.rutland@arm.com>
2022-02-16 16:22:27 +0000

    arm64: clean up symbol aliasing
    Now that we have SYM_FUNC_ALIAS() and SYM_FUNC_ALIAS_WEAK(), use those
    to simplify and more consistently define function aliases across
    arch/arm64.
    
    Aliases are now defined in terms of a canonical function name. For
    position-independent functions I've made the __pi_<func> name the
    canonical name, and defined other alises in terms of this.
    
    The SYM_FUNC_{START,END}_PI(func) macros obscure the __pi_<func> name,
    and make this hard to seatch for. The SYM_FUNC_START_WEAK_PI() macro
    also obscures the fact that the __pi_<func> fymbol is global and the
    <func> symbol is weak. For clarity, I have removed these macros and used
    SYM_FUNC_{START,END}() directly with the __pi_<func> name.
    
    For example:
    
    	SYM_FUNC_START_WEAK_PI(func)
    	... asm insns ...
    	SYM_FUNC_END_PI(func)
    	EXPORT_SYMBOL(func)
    
    ... becomes:
    
    	SYM_FUNC_START(__pi_func)
    	... asm insns ...
    	SYM_FUNC_END(__pi_func)
    
    	SYM_FUNC_ALIAS_WEAK(func, __pi_func)
    	EXPORT_SYMBOL(func)
    
    For clarity, where there are multiple annotations such as
    EXPORT_SYMBOL(), I've tried to keep annotations grouped by symbol. For
    example, where a function has a name and an alias which are both
    exported, this is organised as:
    
    	SYM_FUNC_START(func)
    	... asm insns ...
    	SYM_FUNC_END(func)
    	EXPORT_SYMBOL(func)
    
    	SYM_FUNC_ALIAS(alias, func)
    	EXPORT_SYMBOL(alias)
    
    For consistency with the other string functions, I've defined strrchr as
    a position-independent function, as it can safely be used as such even
    though we have no users today.
    
    As we no longer use SYM_FUNC_{START,END}_ALIAS(), our local copies are
    removed. The common versions will be removed by a subsequent patch.
    
    There should be no functional change as a result of this patch.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Acked-by: Mark Brown <broonie@kernel.org>
    Cc: Joey Gouly <joey.gouly@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20220216162229.1076788-3-mark.rutland@arm.com
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/linkage.h
+++ arch/arm64/include/asm/linkage.h
@@ -39,28 +39,4 @@
 	SYM_START(name, SYM_L_WEAK, SYM_A_NONE)		\
 	bti c ;
 
-/*
- * Annotate a function as position independent, i.e., safe to be called before
- * the kernel virtual mapping is activated.
- */
-#define SYM_FUNC_START_PI(x)			\
-		SYM_FUNC_START_ALIAS(__pi_##x);	\
-		SYM_FUNC_START(x)
-
-#define SYM_FUNC_START_WEAK_PI(x)		\
-		SYM_FUNC_START_ALIAS(__pi_##x);	\
-		SYM_FUNC_START_WEAK(x)
-
-#define SYM_FUNC_START_WEAK_ALIAS_PI(x)		\
-		SYM_FUNC_START_ALIAS(__pi_##x);	\
-		SYM_START(x, SYM_L_WEAK, SYM_A_ALIGN)
-
-#define SYM_FUNC_END_PI(x)			\
-		SYM_FUNC_END(x);		\
-		SYM_FUNC_END_ALIAS(__pi_##x)
-
-#define SYM_FUNC_END_ALIAS_PI(x)		\
-		SYM_FUNC_END_ALIAS(x);		\
-		SYM_FUNC_END_ALIAS(__pi_##x)
-
 #endif
