1af0e4a0233fea7e8226cb977d379dc20f9bbe11
Christian Göttsche <cgzones@googlemail.com>
2022-02-17 15:18:57 +0100

    security: declare member holding string literal const
    The struct security_hook_list member lsm is assigned in
    security_add_hooks() with string literals passed from the individual
    security modules.  Declare the function parameter and the struct member
    const to signal their immutability.
    
    Reported by Clang [-Wwrite-strings]:
    
        security/selinux/hooks.c:7388:63: error: passing 'const char [8]'
          to parameter of type 'char *' discards qualifiers
          [-Werror,-Wincompatible-pointer-types-discards-qualifiers]
                security_add_hooks(selinux_hooks,
                                   ARRAY_SIZE(selinux_hooks), selinux);
                                                              ^~~~~~~~~
        ./include/linux/lsm_hooks.h:1629:11: note: passing argument to
          parameter 'lsm' here
                                        char *lsm);
                                              ^
    
    Signed-off-by: Christian Göttsche <cgzones@googlemail.com>
    Reviewed-by: Paul Moore <paul@paul-moore.com>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- include/linux/lsm_hooks.h
+++ include/linux/lsm_hooks.h
@@ -1595,7 +1595,7 @@ struct security_hook_list {
 	struct hlist_node		list;
 	struct hlist_head		*head;
 	union security_list_options	hook;
-	char				*lsm;
+	const char			*lsm;
 } __randomize_layout;
 
 /*
@@ -1630,7 +1630,7 @@ extern struct security_hook_heads security_hook_heads;
 extern char *lsm_names;
 
 extern void security_add_hooks(struct security_hook_list *hooks, int count,
-				char *lsm);
+				const char *lsm);
 
 #define LSM_FLAG_LEGACY_MAJOR	BIT(0)
 #define LSM_FLAG_EXCLUSIVE	BIT(1)
