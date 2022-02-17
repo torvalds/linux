546c42b2c5c161619736dd730d3df709181999d0
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2022-02-17 11:24:00 +0100

    fork: Duplicate task_struct before stack allocation
    alloc_thread_stack_node() already populates the task_struct::stack
    member except on IA64. The stack pointer is saved and populated again
    because IA64 needs it and arch_dup_task_struct() overwrites it.
    
    Allocate thread's stack after task_struct has been duplicated as a
    preparation for further changes.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Andy Lutomirski <luto@kernel.org>
    Link: https://lore.kernel.org/r/20220217102406.3697941-3-bigeasy@linutronix.de

--- kernel/fork.c
+++ kernel/fork.c
@@ -888,6 +888,10 @@ static struct task_struct *dup_task_struct(struct task_struct *orig, int node)
 	if (!tsk)
 		return NULL;
 
+	err = arch_dup_task_struct(tsk, orig);
+	if (err)
+		goto free_tsk;
+
 	stack = alloc_thread_stack_node(tsk, node);
 	if (!stack)
 		goto free_tsk;
@@ -896,8 +900,6 @@ static struct task_struct *dup_task_struct(struct task_struct *orig, int node)
 		goto free_stack;
 
 	stack_vm_area = task_stack_vm_area(tsk);
-
-	err = arch_dup_task_struct(tsk, orig);
 
 	/*
 	 * arch_dup_task_struct() clobbers the stack-related fields.  Make
@@ -911,9 +913,6 @@ static struct task_struct *dup_task_struct(struct task_struct *orig, int node)
 #ifdef CONFIG_THREAD_INFO_IN_TASK
 	refcount_set(&tsk->stack_refcount, 1);
 #endif
-
-	if (err)
-		goto free_stack;
 
 	err = scs_prepare(tsk, node);
 	if (err)
