ef34c63346d600e9e6da2a3d9f46b4bfe1f0d74e
Jianqun Xu <jay.xu@rock-chips.com>
2022-02-18 08:34:07 +0800

    media: videobuf2: add vb2_cma_sg_memops
    Currently videobuf2 supports videobuf2-dma-sg, videobuf2-dma-contig and
    videobuf2-vmalloc.
    
    - vb2_dma_sg_memops
      created by videobuf2-dma-sg driver,
      allocates memory through 'alloc_pages' and manager them with scatter
      gather list (sgt).
    
    - vb2_dma_contig_memops
      created by videobuf2-dma-contig driver,
      allocates memory through dma_alloc_attrs, may result in many kinds of
      memory, including 'cma_alloc'.
    
    - vb2_vmalloc_memops
      created by videobuf2-vmalloc driver,
      allocates memory through vmalloc_user.
    
    In some case device requires allocate memory through cma_alloc and manager
    them by sgt, the new driver is named videobuf2-cma-sg.
    
    - vb2_cma_sg_memops
      created by videobuf2-cma-sg driver,
      allocates memory through 'cma_alloc' and manager them with sgt.
    
    Signed-off-by: Jianqun Xu <jay.xu@rock-chips.com>
    Change-Id: Ic974dd41aa3dcf9588311ad8ed6c166f3064d6fd
    Signed-off-by: Cai YiWei <cyw@rock-chips.com>

---
+++ include/media/videobuf2-cma-sg.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef _MEDIA_VIDEOBUF2_CMA_SG_H
+#define _MEDIA_VIDEOBUF2_CMA_SG_H
+
+#include <media/videobuf2-v4l2.h>
+
+extern const struct vb2_mem_ops vb2_cma_sg_memops;
+
+#endif
