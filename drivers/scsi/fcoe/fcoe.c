5d21aa3636fa8c7d3a2a69be93287da164054a3a
Bart Van Assche <bvanassche@acm.org>
2022-02-18 11:50:55 -0800

    scsi: libfc: Stop using the SCSI pointer
    Move the fc_fcp_pkt pointer, the residual length and the SCSI status into
    the new data structure libfc_cmd_priv. This patch prepares for removal of
    the SCSI pointer from struct scsi_cmnd.
    
    The user of the libfc data path functions have been identified as follows:
    $ git grep -lw fc_queuecommand | grep -v scsi/libfc/
    drivers/scsi/fcoe/fcoe.c
    
    Link: https://lore.kernel.org/r/20220218195117.25689-28-bvanassche@acm.org
    Cc: Saurav Kashyap <skashyap@marvell.com>
    Cc: Javed Hasan <jhasan@marvell.com>
    Reviewed-by: Himanshu Madhani <himanshu.madhani@oracle.com>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/fcoe/fcoe.c
+++ drivers/scsi/fcoe/fcoe.c
@@ -277,6 +277,7 @@ static struct scsi_host_template fcoe_shost_template = {
 	.sg_tablesize = SG_ALL,
 	.max_sectors = 0xffff,
 	.track_queue_depth = 1,
+	.cmd_size = sizeof(struct libfc_cmd_priv),
 };
 
 /**
