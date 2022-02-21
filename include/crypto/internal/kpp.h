1038fd78a1b8269b642ce09600242682186a78e2
Nicolai Stange <nstange@suse.de>
2022-02-21 13:10:47 +0100

    crypto: kpp - provide support for KPP template instances
    The upcoming support for the RFC 7919 ffdhe group parameters will be
    made available in the form of templates like "ffdhe2048(dh)",
    "ffdhe3072(dh)" and so on. Template instantiations thereof would wrap the
    inner "dh" kpp_alg and also provide kpp_alg services to the outside again.
    Furthermore, it might be perhaps be desirable to provide KDF templates in
    the future, which would similarly wrap an inner kpp_alg and present
    themselves to the outside as another kpp_alg, transforming the shared
    secret on its way out.
    
    Introduce the bits needed for supporting KPP template instances. Everything
    related to inner kpp_alg spawns potentially being held by such template
    instances will be deferred to a subsequent patch in order to facilitate
    review.
    
    Define struct struct kpp_instance in close analogy to the already existing
    skcipher_instance, shash_instance and alike, but wrapping a struct kpp_alg.
    Implement the new kpp_register_instance() template instance registration
    primitive. Provide some helper functions for
    - going back and forth between a generic struct crypto_instance and the new
      struct kpp_instance,
    - obtaining the instantiating kpp_instance from a crypto_kpp transform and
    - for accessing a given kpp_instance's implementation specific context
      data.
    
    Annotate everything with proper kernel-doc comments, even though
    include/crypto/internal/kpp.h is not considered for the generated docs.
    
    Signed-off-by: Nicolai Stange <nstange@suse.de>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/internal/kpp.h
+++ include/crypto/internal/kpp.h
@@ -10,6 +10,24 @@
 #include <crypto/kpp.h>
 #include <crypto/algapi.h>
 
+/**
+ * struct kpp_instance - KPP template instance
+ * @free: Callback getting invoked upon instance destruction. Must be set.
+ * @s: Internal. Generic crypto core instance state properly layout
+ *     to alias with @alg as needed.
+ * @alg: The &struct kpp_alg implementation provided by the instance.
+ */
+struct kpp_instance {
+	void (*free)(struct kpp_instance *inst);
+	union {
+		struct {
+			char head[offsetof(struct kpp_alg, base)];
+			struct crypto_instance base;
+		} s;
+		struct kpp_alg alg;
+	};
+};
+
 /*
  * Transform internal helpers.
  */
@@ -33,6 +51,62 @@ static inline const char *kpp_alg_name(struct crypto_kpp *tfm)
 	return crypto_kpp_tfm(tfm)->__crt_alg->cra_name;
 }
 
+/*
+ * Template instance internal helpers.
+ */
+/**
+ * kpp_crypto_instance() - Cast a &struct kpp_instance to the corresponding
+ *                         generic &struct crypto_instance.
+ * @inst: Pointer to the &struct kpp_instance to be cast.
+ * Return: A pointer to the &struct crypto_instance embedded in @inst.
+ */
+static inline struct crypto_instance *kpp_crypto_instance(
+	struct kpp_instance *inst)
+{
+	return &inst->s.base;
+}
+
+/**
+ * kpp_instance() - Cast a generic &struct crypto_instance to the corresponding
+ *                  &struct kpp_instance.
+ * @inst: Pointer to the &struct crypto_instance to be cast.
+ * Return: A pointer to the &struct kpp_instance @inst is embedded in.
+ */
+static inline struct kpp_instance *kpp_instance(struct crypto_instance *inst)
+{
+	return container_of(inst, struct kpp_instance, s.base);
+}
+
+/**
+ * kpp_alg_instance() - Get the &struct kpp_instance a given KPP transform has
+ *                      been instantiated from.
+ * @kpp: The KPP transform instantiated from some &struct kpp_instance.
+ * Return: The &struct kpp_instance associated with @kpp.
+ */
+static inline struct kpp_instance *kpp_alg_instance(struct crypto_kpp *kpp)
+{
+	return kpp_instance(crypto_tfm_alg_instance(&kpp->base));
+}
+
+/**
+ * kpp_instance_ctx() - Get a pointer to a &struct kpp_instance's implementation
+ *                      specific context data.
+ * @inst: The &struct kpp_instance whose context data to access.
+ *
+ * A KPP template implementation may allocate extra memory beyond the
+ * end of a &struct kpp_instance instantiated from &crypto_template.create().
+ * This function provides a means to obtain a pointer to this area.
+ *
+ * Return: A pointer to the implementation specific context data.
+ */
+static inline void *kpp_instance_ctx(struct kpp_instance *inst)
+{
+	return crypto_instance_ctx(kpp_crypto_instance(inst));
+}
+
+/*
+ * KPP algorithm (un)registration functions.
+ */
 /**
  * crypto_register_kpp() -- Register key-agreement protocol primitives algorithm
  *
@@ -55,5 +129,14 @@ int crypto_register_kpp(struct kpp_alg *alg);
  * @alg:	algorithm definition
  */
 void crypto_unregister_kpp(struct kpp_alg *alg);
+
+/**
+ * kpp_register_instance() - Register a KPP template instance.
+ * @tmpl: The instantiating template.
+ * @inst: The KPP template instance to be registered.
+ * Return: %0 on success, negative error code otherwise.
+ */
+int kpp_register_instance(struct crypto_template *tmpl,
+			  struct kpp_instance *inst);
 
 #endif
