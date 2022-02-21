1929be8c45a21954d3f328cd4c9e9b671e7ac688
Philipp Hortmann <philipp.g.hortmann@gmail.com>
2022-02-21 22:25:23 +0100

    staging: vt6656: Remove ftrace-like logging
    This patch fixes the checkpatch.pl warning like:
    WARNING: Unnecessary ftrace-like logging - prefer using ftrace
    
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Philipp Hortmann <philipp.g.hortmann@gmail.com>
    Link: https://lore.kernel.org/r/084064a5c94aad940600ae62d6c21bb26629cb2c.1645477326.git.philipp.g.hortmann@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/rxtx.c
+++ drivers/staging/vt6656/rxtx.c
@@ -58,8 +58,6 @@ static struct vnt_usb_send_context
 	struct vnt_usb_send_context *context = NULL;
 	int ii;
 
-	dev_dbg(&priv->usb->dev, "%s\n", __func__);
-
 	for (ii = 0; ii < priv->num_tx_context; ii++) {
 		if (!priv->tx_context[ii])
 			return NULL;
