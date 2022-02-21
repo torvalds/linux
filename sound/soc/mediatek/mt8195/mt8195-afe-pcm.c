ff5a90173d981934e1134d28af3625acaab01d80
Trevor Wu <trevor.wu@mediatek.com>
2022-02-21 13:57:16 +0800

    ASoC: mediatek: mt8195: enable apll tuner
    Normally, the clock source of audio module is either 26M or APLL1/APLL2,
    but APLL1/APLL2 are not the multiple of 26M.
    
    In the patch, APLL1 and APLL2 tuners are enabled to handle sample rate
    mismatch when the data path crosses two different clock domains.
    
    Signed-off-by: Trevor Wu <trevor.wu@mediatek.com>
    Link: https://lore.kernel.org/r/20220221055716.18580-1-trevor.wu@mediatek.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/mediatek/mt8195/mt8195-afe-pcm.c
+++ sound/soc/mediatek/mt8195/mt8195-afe-pcm.c
@@ -2583,8 +2583,6 @@ static bool mt8195_is_volatile_reg(struct device *dev, unsigned int reg)
 	case AFE_IRQ3_CON_MON:
 	case AFE_IRQ_MCU_MON2:
 	case ADSP_IRQ_STATUS:
-	case AFE_APLL_TUNER_CFG:
-	case AFE_APLL_TUNER_CFG1:
 	case AUDIO_TOP_STA0:
 	case AUDIO_TOP_STA1:
 	case AFE_GAIN1_CUR:
@@ -2623,7 +2621,6 @@ static bool mt8195_is_volatile_reg(struct device *dev, unsigned int reg)
 	case SPDIFIN_USERCODE10:
 	case SPDIFIN_USERCODE11:
 	case SPDIFIN_USERCODE12:
-	case AFE_SPDIFIN_APLL_TUNER_CFG:
 	case AFE_LINEIN_APLL_TUNER_MON:
 	case AFE_EARC_APLL_TUNER_MON:
 	case AFE_CM0_MON:
