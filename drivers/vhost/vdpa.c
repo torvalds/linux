5a4cbcb3df45be164bdf9257ea7e0dd89e447cf3
Zhu Lingshan <lingshan.zhu@intel.com>
2022-02-22 19:54:25 +0800

    vhost_vdpa: don't setup irq offloading when irq_num < 0
    [ Upstream commit cce0ab2b2a39072d81f98017f7b076f3410ef740 ]
    
    When irq number is negative(e.g., -EINVAL), the virtqueue
    may be disabled or the virtqueues are sharing a device irq.
    In such case, we should not setup irq offloading for a virtqueue.
    
    Signed-off-by: Zhu Lingshan <lingshan.zhu@intel.com>
    Link: https://lore.kernel.org/r/20220222115428.998334-3-lingshan.zhu@intel.com
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/vhost/vdpa.c
+++ drivers/vhost/vdpa.c
@@ -97,8 +97,11 @@ static void vhost_vdpa_setup_vq_irq(struct vhost_vdpa *v, u16 qid)
 		return;
 
 	irq = ops->get_vq_irq(vdpa, qid);
+	if (irq < 0)
+		return;
+
 	irq_bypass_unregister_producer(&vq->call_ctx.producer);
-	if (!vq->call_ctx.ctx || irq < 0)
+	if (!vq->call_ctx.ctx)
 		return;
 
 	vq->call_ctx.producer.token = vq->call_ctx.ctx;
