db749b769ff61a42480c377c30df70b8b722041e
Prike Liang <Prike.Liang@amd.com>
2022-02-23 14:21:42 +0800

    drm/amdgpu/nv: set mode2 reset for MP1 13.0.8
    Set mode2 reset support for MP1 13.0.8.
    
    Signed-off-by: Prike Liang <Prike.Liang@amd.com>
    Reviewed-by: Huang Rui <ray.huang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/nv.c
+++ drivers/gpu/drm/amd/amdgpu/nv.c
@@ -455,6 +455,7 @@ nv_asic_reset_method(struct amdgpu_device *adev)
 	case IP_VERSION(13, 0, 1):
 	case IP_VERSION(13, 0, 3):
 	case IP_VERSION(13, 0, 5):
+	case IP_VERSION(13, 0, 8):
 		return AMD_RESET_METHOD_MODE2;
 	case IP_VERSION(11, 0, 7):
 	case IP_VERSION(11, 0, 11):
