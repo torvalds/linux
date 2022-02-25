e5c894791eabf13f3754ee0b28a3a8c1c5660d41
Mario Limonciello <mario.limonciello@amd.com>
2022-02-25 11:23:18 -0600

    ata: ahci: Rename `AHCI_HFLAG_IS_MOBILE`
    `AHCI_HFLAG_IS_MOBILE` designates that a chipset should be using the
    default link power management policy from a kernel configuration item.
    
    As desktop chipsets may also be interested in this default policy
    configuration, rename the flag to `AHCI_HFLAG_USE_LPM_POLICY` to more
    accurately reflect that a chipset doesn't have to be mobile to adopt it.
    
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Reviewed-by: Paul Menzel <pmenzel@molgen.mpg.de>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>

--- drivers/ata/ahci.c
+++ drivers/ata/ahci.c
@@ -136,7 +136,7 @@ static const struct ata_port_info ahci_port_info[] = {
 		.port_ops	= &ahci_ops,
 	},
 	[board_ahci_low_power] = {
-		AHCI_HFLAGS	(AHCI_HFLAG_IS_MOBILE),
+		AHCI_HFLAGS	(AHCI_HFLAG_USE_LPM_POLICY),
 		.flags		= AHCI_FLAG_COMMON,
 		.pio_mask	= ATA_PIO4,
 		.udma_mask	= ATA_UDMA6,
@@ -1598,8 +1598,8 @@ static void ahci_update_initial_lpm_policy(struct ata_port *ap,
 	int policy = CONFIG_SATA_MOBILE_LPM_POLICY;
 
 
-	/* Ignore processing for non mobile platforms */
-	if (!(hpriv->flags & AHCI_HFLAG_IS_MOBILE))
+	/* Ignore processing for chipsets that don't use policy */
+	if (!(hpriv->flags & AHCI_HFLAG_USE_LPM_POLICY))
 		return;
 
 	/* user modified policy via module param */
