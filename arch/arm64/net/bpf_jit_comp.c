c08f23081c9bddc4a53e14481d3cd273fa16fe88
Hou Tao <houtao1@huawei.com>
2022-02-26 20:19:06 +0800

    bpf, arm64: Feed byte-offset into bpf line info
    [ Upstream commit dda7596c109fc382876118627e29db7607cde35d ]
    
    insn_to_jit_off passed to bpf_prog_fill_jited_linfo() is calculated in
    instruction granularity instead of bytes granularity, but BPF line info
    requires byte offset.
    
    bpf_prog_fill_jited_linfo() will be the last user of ctx.offset before
    it is freed, so convert the offset into byte-offset before calling into
    bpf_prog_fill_jited_linfo() in order to fix the line info dump on arm64.
    
    Fixes: 37ab566c178d ("bpf: arm64: Enable arm64 jit to provide bpf_line_info")
    Suggested-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Hou Tao <houtao1@huawei.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/bpf/20220226121906.5709-3-houtao1@huawei.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/arm64/net/bpf_jit_comp.c
+++ arch/arm64/net/bpf_jit_comp.c
@@ -1126,6 +1126,11 @@ skip_init_ctx:
 	prog->jited_len = prog_size;
 
 	if (!prog->is_func || extra_pass) {
+		int i;
+
+		/* offset[prog->len] is the size of program */
+		for (i = 0; i <= prog->len; i++)
+			ctx.offset[i] *= AARCH64_INSN_SIZE;
 		bpf_prog_fill_jited_linfo(prog, ctx.offset + 1);
 out_off:
 		kfree(ctx.offset);
