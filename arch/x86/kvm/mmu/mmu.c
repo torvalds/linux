cf3e26427c08ad9015956293ab389004ac6a338e
Sean Christopherson <seanjc@google.com>
2022-02-26 00:15:35 +0000

    KVM: x86/mmu: Zap only TDP MMU leafs in kvm_zap_gfn_range()
    Zap only leaf SPTEs in the TDP MMU's zap_gfn_range(), and rename various
    functions accordingly.  When removing mappings for functional correctness
    (except for the stupid VFIO GPU passthrough memslots bug), zapping the
    leaf SPTEs is sufficient as the paging structures themselves do not point
    at guest memory and do not directly impact the final translation (in the
    TDP MMU).
    
    Note, this aligns the TDP MMU with the legacy/full MMU, which zaps only
    the rmaps, a.k.a. leaf SPTEs, in kvm_zap_gfn_range() and
    kvm_unmap_gfn_range().
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Reviewed-by: Ben Gardon <bgardon@google.com>
    Message-Id: <20220226001546.360188-18-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -5845,8 +5845,8 @@ void kvm_zap_gfn_range(struct kvm *kvm, gfn_t gfn_start, gfn_t gfn_end)
 
 	if (is_tdp_mmu_enabled(kvm)) {
 		for (i = 0; i < KVM_ADDRESS_SPACE_NUM; i++)
-			flush = kvm_tdp_mmu_zap_gfn_range(kvm, i, gfn_start,
-							  gfn_end, flush);
+			flush = kvm_tdp_mmu_zap_leafs(kvm, i, gfn_start,
+						      gfn_end, true, flush);
 	}
 
 	if (flush)
