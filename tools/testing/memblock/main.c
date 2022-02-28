142eac65f3e04577451a40b3cc57e708311eed01
Karolina Drobnik <karolinadrobnik@gmail.com>
2022-02-28 15:46:45 +0100

    memblock tests: Add memblock_alloc tests for top down
    Add checks for memblock_alloc for top down allocation direction.
    The tested scenarios are:
      - Region can be allocated on the first fit (with and without
        region merging)
      - Region can be allocated on the second fit (with and without
        region merging)
    
    Add checks for both allocation directions:
      - Region can be allocated between two already existing entries
      - Limited memory available
      - All memory is reserved
      - No available memory registered with memblock
    
    Signed-off-by: Karolina Drobnik <karolinadrobnik@gmail.com>
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>
    Link: https://lore.kernel.org/r/26ccf409b8ff0394559d38d792b2afb24b55887c.1646055639.git.karolinadrobnik@gmail.com

--- tools/testing/memblock/main.c
+++ tools/testing/memblock/main.c
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include "tests/basic_api.h"
+#include "tests/alloc_api.h"
 
 int main(int argc, char **argv)
 {
 	memblock_basic_checks();
+	memblock_alloc_checks();
+
 	return 0;
 }
---
+++ tools/testing/memblock/tests/alloc_api.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _MEMBLOCK_ALLOCS_H
+#define _MEMBLOCK_ALLOCS_H
+
+#include "common.h"
+
+int memblock_alloc_checks(void);
+
+#endif
