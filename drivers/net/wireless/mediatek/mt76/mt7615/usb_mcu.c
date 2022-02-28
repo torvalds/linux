e809904f9bd2db92c1c7e403aa77ab49b8a9b266
Lorenzo Bianconi <lorenzo@kernel.org>
2022-02-28 20:39:28 +0100

    mt76: mt7615: honor ret from mt7615_mcu_restart in mt7663u_mcu_init
    Check return value of mt7615_mcu_restart routine in mt7663u_mcu_init().
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7615/usb_mcu.c
+++ drivers/net/wireless/mediatek/mt76/mt7615/usb_mcu.c
@@ -57,7 +57,10 @@ int mt7663u_mcu_init(struct mt7615_dev *dev)
 
 	mt76_set(dev, MT_UDMA_TX_QSEL, MT_FW_DL_EN);
 	if (test_and_clear_bit(MT76_STATE_POWER_OFF, &dev->mphy.state)) {
-		mt7615_mcu_restart(&dev->mt76);
+		ret = mt7615_mcu_restart(&dev->mt76);
+		if (ret)
+			return ret;
+
 		if (!mt76_poll_msec(dev, MT_CONN_ON_MISC,
 				    MT_TOP_MISC2_FW_PWR_ON, 0, 500))
 			return -EIO;
