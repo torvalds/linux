42f0c1934c7cb3e94c2fe8f5771245fa5631d0e7
Tao Chen <chentao3@hotmail.com>
2022-03-01 06:35:42 -0800

    tcp: Remove the unused api
    Last tcp_write_queue_head() use was removed in commit
    114f39feab36 ("tcp: restore autocorking"), so remove it.
    
    Signed-off-by: Tao Chen <chentao3@hotmail.com>
    Link: https://lore.kernel.org/r/SYZP282MB33317DEE1253B37C0F57231E86029@SYZP282MB3331.AUSP282.PROD.OUTLOOK.COM
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -1831,11 +1831,6 @@ static inline struct sk_buff *tcp_rtx_queue_tail(const struct sock *sk)
 	return skb_rb_last(&sk->tcp_rtx_queue);
 }
 
-static inline struct sk_buff *tcp_write_queue_head(const struct sock *sk)
-{
-	return skb_peek(&sk->sk_write_queue);
-}
-
 static inline struct sk_buff *tcp_write_queue_tail(const struct sock *sk)
 {
 	return skb_peek_tail(&sk->sk_write_queue);
