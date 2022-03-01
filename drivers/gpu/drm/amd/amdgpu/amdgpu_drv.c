5aa061474b1e0cc9605877190e23a880cf1551e4
Andrey Grodzovsky <andrey.grodzovsky@amd.com>
2022-03-01 12:58:23 -0500

    drm/amdgpu: Bump minor version for hot plug tests enabling.
    This will allow to enable the tests only after latest fix
    after which the tests passed on my system.
    
    I tested on NV21 standalone and Vega 10 and Polaris as
    pair with DRI_PRIME.
    
    It's possible there might be still issues on ASICs i don't
    have at my posession but that that the point of enbling
    the tests finally - if other people during testing will
    encounter errors they will report and I will be able to fix.
    
    The releated merge request for enabling libdrm tests suite  is in
    https://gitlab.freedesktop.org/mesa/drm/-/merge_requests/227
    
    Signed-off-by: Andrey Grodzovsky <andrey.grodzovsky@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -100,9 +100,10 @@
  * - 3.43.0 - Add device hot plug/unplug support
  * - 3.44.0 - DCN3 supports DCC independent block settings: !64B && 128B, 64B && 128B
  * - 3.45.0 - Add context ioctl stable pstate interface
+ * * 3.46.0 - To enable hot plug amdgpu tests in libdrm
  */
 #define KMS_DRIVER_MAJOR	3
-#define KMS_DRIVER_MINOR	45
+#define KMS_DRIVER_MINOR	46
 #define KMS_DRIVER_PATCHLEVEL	0
 
 int amdgpu_vram_limit;
