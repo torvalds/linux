b66fbf33425f30aacbbb95182c22d2df5b1d3b12
Kent Overstreet <kent.overstreet@gmail.com>
2022-03-01 15:31:20 -0500

    bcachefs: Drop unneeded journal pin in bch2_btree_update_start()
    When we do an interior btree update, we create new btree nodes and link
    them into the btree in memory, but they don't become reachable on disk
    until later, when btree_update_nodes_written_trans() runs.
    
    Updates to the new nodes can thus happen before they're reachable on
    disk, and if the updates to those new nodes are written before the nodes
    become reachable, we would then drop the journal pin for those updates
    before the btree has them.
    
    This is what the journal pin in bch2_btree_update_start() was protecting
    against. However, it's not actually needed because we don't allow
    subsequent append writes to btree nodes until the node is reachable on
    disk.
    
    Dropping this unneeded pin also fixes a bug introduced by "bcachefs:
    Journal seq now incremented at entry open, not close" - in the new code,
    if the journal is completely empty a journal pin list for
    journal_cur_seq() won't exist.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/btree_update_interior.c
+++ fs/bcachefs/btree_update_interior.c
@@ -544,8 +544,6 @@ static void btree_update_nodes_written(struct btree_update *as)
 	if (ret)
 		goto err;
 
-	BUG_ON(!journal_pin_active(&as->journal));
-
 	/*
 	 * Wait for any in flight writes to finish before we free the old nodes
 	 * on disk:
@@ -1044,10 +1042,6 @@ retry:
 	ret = bch2_btree_reserve_get(as, nr_nodes, flags, &cl);
 	if (ret)
 		goto err;
-
-	bch2_journal_pin_add(&c->journal,
-			     atomic64_read(&c->journal.seq),
-			     &as->journal, NULL);
 
 	return as;
 err:
