81d9d7f8bb8bacd96b2e0e5779e3fba9e86da73f
Tom Rix <trix@redhat.com>
2022-03-02 04:45:35 -0800

    drm/panfrost: cleanup comments
    For spdx
    change tab to space delimiter
    Use // for *.c
    
    Replacements
    commited to committed
    regsiters to registers
    initialze to initialize
    
    Signed-off-by: Tom Rix <trix@redhat.com>
    Reviewed-by: Steven Price <steven.price@arm.com>
    Signed-off-by: Steven Price <steven.price@arm.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220302124535.358060-1-trix@redhat.com

--- drivers/gpu/drm/panfrost/panfrost_regs.h
+++ drivers/gpu/drm/panfrost/panfrost_regs.h
@@ -293,7 +293,7 @@
 #define AS_FAULTADDRESS_LO(as)		(MMU_AS(as) + 0x20) /* (RO) Fault Address for address space n, low word */
 #define AS_FAULTADDRESS_HI(as)		(MMU_AS(as) + 0x24) /* (RO) Fault Address for address space n, high word */
 #define AS_STATUS(as)			(MMU_AS(as) + 0x28) /* (RO) Status flags for address space n */
-/* Additional Bifrost AS regsiters */
+/* Additional Bifrost AS registers */
 #define AS_TRANSCFG_LO(as)		(MMU_AS(as) + 0x30) /* (RW) Translation table configuration for address space n, low word */
 #define AS_TRANSCFG_HI(as)		(MMU_AS(as) + 0x34) /* (RW) Translation table configuration for address space n, high word */
 #define AS_FAULTEXTRA_LO(as)		(MMU_AS(as) + 0x38) /* (RO) Secondary fault address for address space n, low word */
