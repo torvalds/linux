cbc0a40e17da361a2ada8d669413ccfbd2028f2d
Keith Busch <kbusch@kernel.org>
2022-03-03 12:13:09 -0800

    lib: add rocksoft model crc64
    The NVM Express specification extended data integrity fields to 64 bits
    using the Rocksoft parameters. Add the poly to the crc64 table
    generation, and provide a generic library routine implementing the
    algorithm.
    
    The Rocksoft 64-bit CRC model parameters are as follows:
        Poly: 0xAD93D23594C93659
        Initial value: 0xFFFFFFFFFFFFFFFF
        Reflected Input: True
        Reflected Output: True
        Xor Final: 0xFFFFFFFFFFFFFFFF
    
    Since this model used reflected bits, the implementation generates the
    reflected table so the result is ordered consistently.
    
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Hannes Reinecke <hare@suse.de>
    Cc: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Reviewed-by: Martin K. Petersen <martin.petersen@oracle.com>
    Link: https://lore.kernel.org/r/20220303201312.3255347-6-kbusch@kernel.org
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/linux/crc64.h
+++ include/linux/crc64.h
@@ -8,4 +8,6 @@
 #include <linux/types.h>
 
 u64 __pure crc64_be(u64 crc, const void *p, size_t len);
+u64 __pure crc64_rocksoft_generic(u64 crc, const void *p, size_t len);
+
 #endif /* _LINUX_CRC64_H */
