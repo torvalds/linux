3e200d6cdb7d5c892fb4819be66c65385673a980
Nikolai Kondrashov <spbnick@gmail.com>
2022-03-03 08:47:33 +0100

    HID: uclogic: Allow three frame parameter sets
    Allow three frame parameter sets per each UC-Logic tablet interface.
    Bump the number of supported subreports to three as well to accommodate
    ID routing. This allows supporting the dial on Huion Q620M.
    
    Signed-off-by: Nikolai Kondrashov <spbnick@gmail.com>
    Signed-off-by: José Expósito <jose.exposito89@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-uclogic-params.h
+++ drivers/hid/hid-uclogic-params.h
@@ -73,8 +73,8 @@ struct uclogic_params_pen {
 	unsigned int desc_size;
 	/* Report ID, if reports should be tweaked, zero if not */
 	unsigned int id;
-	/* The list of subreports */
-	struct uclogic_params_pen_subreport subreport_list[1];
+	/* The list of subreports, only valid if "id" is not zero */
+	struct uclogic_params_pen_subreport subreport_list[3];
 	/* Type of in-range reporting, only valid if "id" is not zero */
 	enum uclogic_params_pen_inrange inrange;
 	/*
@@ -194,7 +194,7 @@ struct uclogic_params {
 	 * The list of frame control parameters and optional report descriptor
 	 * parts. Only valid, if "invalid" is false.
 	 */
-	struct uclogic_params_frame frame_list[1];
+	struct uclogic_params_frame frame_list[3];
 };
 
 /* Initialize a tablet interface and discover its parameters */
@@ -210,6 +210,8 @@ extern int uclogic_params_init(struct uclogic_params *params,
 		".pen.desc_size = %u\n"                         \
 		".pen.id = %u\n"                                \
 		".pen.subreport_list[0] = {0x%02hhx, %hhu}\n"   \
+		".pen.subreport_list[1] = {0x%02hhx, %hhu}\n"   \
+		".pen.subreport_list[2] = {0x%02hhx, %hhu}\n"   \
 		".pen.inrange = %s\n"                           \
 		".pen.fragmented_hires = %s\n"                  \
 		".pen.tilt_y_flipped = %s\n"                    \
@@ -221,7 +223,25 @@ extern int uclogic_params_init(struct uclogic_params *params,
 		".frame_list[0].dev_id_byte = %u\n"             \
 		".frame_list[0].touch_ring_byte = %u\n"         \
 		".frame_list[0].touch_ring_max = %hhd\n"        \
-		".frame_list[0].touch_ring_flip_at = %hhd\n"
+		".frame_list[0].touch_ring_flip_at = %hhd\n"    \
+		".frame_list[1].desc_ptr = %p\n"                \
+		".frame_list[1].desc_size = %u\n"               \
+		".frame_list[1].id = %u\n"                      \
+		".frame_list[1].suffix = %s\n"                  \
+		".frame_list[1].re_lsb = %u\n"                  \
+		".frame_list[1].dev_id_byte = %u\n"             \
+		".frame_list[1].touch_ring_byte = %u\n"         \
+		".frame_list[1].touch_ring_max = %hhd\n"        \
+		".frame_list[1].touch_ring_flip_at = %hhd\n"    \
+		".frame_list[2].desc_ptr = %p\n"                \
+		".frame_list[2].desc_size = %u\n"               \
+		".frame_list[2].id = %u\n"                      \
+		".frame_list[2].suffix = %s\n"                  \
+		".frame_list[2].re_lsb = %u\n"                  \
+		".frame_list[2].dev_id_byte = %u\n"             \
+		".frame_list[2].touch_ring_byte = %u\n"         \
+		".frame_list[2].touch_ring_max = %hhd\n"        \
+		".frame_list[2].touch_ring_flip_at = %hhd\n"
 
 /* Tablet interface parameters *printf format arguments */
 #define UCLOGIC_PARAMS_FMT_ARGS(_params) \
@@ -233,6 +253,10 @@ extern int uclogic_params_init(struct uclogic_params *params,
 		(_params)->pen.id,                                          \
 		(_params)->pen.subreport_list[0].value,                     \
 		(_params)->pen.subreport_list[0].id,                        \
+		(_params)->pen.subreport_list[1].value,                     \
+		(_params)->pen.subreport_list[1].id,                        \
+		(_params)->pen.subreport_list[2].value,                     \
+		(_params)->pen.subreport_list[2].id,                        \
 		uclogic_params_pen_inrange_to_str((_params)->pen.inrange),  \
 		((_params)->pen.fragmented_hires ? "true" : "false"),       \
 		((_params)->pen.tilt_y_flipped ? "true" : "false"),         \
@@ -244,7 +268,25 @@ extern int uclogic_params_init(struct uclogic_params *params,
 		(_params)->frame_list[0].dev_id_byte,                       \
 		(_params)->frame_list[0].touch_ring_byte,                   \
 		(_params)->frame_list[0].touch_ring_max,                    \
-		(_params)->frame_list[0].touch_ring_flip_at
+		(_params)->frame_list[0].touch_ring_flip_at,                \
+		(_params)->frame_list[1].desc_ptr,                          \
+		(_params)->frame_list[1].desc_size,                         \
+		(_params)->frame_list[1].id,                                \
+		(_params)->frame_list[1].suffix,                            \
+		(_params)->frame_list[1].re_lsb,                            \
+		(_params)->frame_list[1].dev_id_byte,                       \
+		(_params)->frame_list[1].touch_ring_byte,                   \
+		(_params)->frame_list[1].touch_ring_max,                    \
+		(_params)->frame_list[1].touch_ring_flip_at,                \
+		(_params)->frame_list[2].desc_ptr,                          \
+		(_params)->frame_list[2].desc_size,                         \
+		(_params)->frame_list[2].id,                                \
+		(_params)->frame_list[2].suffix,                            \
+		(_params)->frame_list[2].re_lsb,                            \
+		(_params)->frame_list[2].dev_id_byte,                       \
+		(_params)->frame_list[2].touch_ring_byte,                   \
+		(_params)->frame_list[2].touch_ring_max,                    \
+		(_params)->frame_list[2].touch_ring_flip_at
 
 /* Get a replacement report descriptor for a tablet's interface. */
 extern int uclogic_params_get_desc(const struct uclogic_params *params,
