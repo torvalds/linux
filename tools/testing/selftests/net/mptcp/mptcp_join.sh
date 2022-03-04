7d9bf018f907bccd04ada1ad9c613a79b07526cd
Geliang Tang <geliang.tang@suse.com>
2022-03-04 11:36:36 -0800

    selftests: mptcp: update output info of chk_rm_nr
    This patch updated the output info of chk_rm_nr. Renamed 'sf' to 'rmsf',
    which means 'remove subflow'. Added the display of whether the inverted
    namespaces has been used to check the mib counters.
    
    The new output looks like this:
    
     002 remove multiple subflows          syn[ ok ] - synack[ ok ] - ack[ ok ]
                                           rm [ ok ] - rmsf  [ ok ]
     003 remove single address             syn[ ok ] - synack[ ok ] - ack[ ok ]
                                           add[ ok ] - echo  [ ok ]
                                           rm [ ok ] - rmsf  [ ok ]   invert
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- tools/testing/selftests/net/mptcp/mptcp_join.sh
+++ tools/testing/selftests/net/mptcp/mptcp_join.sh
@@ -1153,15 +1153,14 @@ chk_rm_nr()
 	local invert=${3:-""}
 	local count
 	local dump_stats
-	local addr_ns
-	local subflow_ns
+	local addr_ns=$ns1
+	local subflow_ns=$ns2
+	local extra_msg=""
 
-	if [ -z $invert ]; then
-		addr_ns=$ns1
-		subflow_ns=$ns2
-	elif [ $invert = "invert" ]; then
+	if [[ $invert = "invert" ]]; then
 		addr_ns=$ns2
 		subflow_ns=$ns1
+		extra_msg="   invert"
 	fi
 
 	printf "%-${nr_blank}s %s" " " "rm "
@@ -1175,7 +1174,7 @@ chk_rm_nr()
 		echo -n "[ ok ]"
 	fi
 
-	echo -n " - sf    "
+	echo -n " - rmsf  "
 	count=`ip netns exec $subflow_ns nstat -as | grep MPTcpExtRmSubflow | awk '{print $2}'`
 	[ -z "$count" ] && count=0
 	if [ "$count" != "$rm_subflow_nr" ]; then
@@ -1183,10 +1182,12 @@ chk_rm_nr()
 		ret=1
 		dump_stats=1
 	else
-		echo "[ ok ]"
+		echo -n "[ ok ]"
 	fi
 
 	[ "${dump_stats}" = 1 ] && dump_stats
+
+	echo "$extra_msg"
 }
 
 chk_prio_nr()
