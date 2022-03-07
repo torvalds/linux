0eaecfb2e4814d51ab172df3823e35d7c488b6d2
Ismael Ferreras Morezuelas <swyterzone@gmail.com>
2022-03-07 21:04:44 +0100

    Bluetooth: hci_sync: Add a new quirk to skip HCI_FLT_CLEAR_ALL
    Some controllers have problems with being sent a command to clear
    all filtering. While the HCI code does not unconditionally
    send a clear-all anymore at BR/EDR setup (after the state machine
    refactor), there might be more ways of hitting these codepaths
    in the future as the kernel develops.
    
    Cc: stable@vger.kernel.org
    Cc: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Ismael Ferreras Morezuelas <swyterzone@gmail.com>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/hci_sync.c
+++ net/bluetooth/hci_sync.c
@@ -2809,6 +2809,9 @@ static int hci_set_event_filter_sync(struct hci_dev *hdev, u8 flt_type,
 	if (!hci_dev_test_flag(hdev, HCI_BREDR_ENABLED))
 		return 0;
 
+	if (test_bit(HCI_QUIRK_BROKEN_FILTER_CLEAR_ALL, &hdev->quirks))
+		return 0;
+
 	memset(&cp, 0, sizeof(cp));
 	cp.flt_type = flt_type;
 
@@ -2827,6 +2830,13 @@ static int hci_set_event_filter_sync(struct hci_dev *hdev, u8 flt_type,
 static int hci_clear_event_filter_sync(struct hci_dev *hdev)
 {
 	if (!hci_dev_test_flag(hdev, HCI_EVENT_FILTER_CONFIGURED))
+		return 0;
+
+	/* In theory the state machine should not reach here unless
+	 * a hci_set_event_filter_sync() call succeeds, but we do
+	 * the check both for parity and as a future reminder.
+	 */
+	if (test_bit(HCI_QUIRK_BROKEN_FILTER_CLEAR_ALL, &hdev->quirks))
 		return 0;
 
 	return hci_set_event_filter_sync(hdev, HCI_FLT_CLEAR_ALL, 0x00,
@@ -4826,6 +4836,12 @@ static int hci_update_event_filter_sync(struct hci_dev *hdev)
 	int err;
 
 	if (!hci_dev_test_flag(hdev, HCI_BREDR_ENABLED))
+		return 0;
+
+	/* Some fake CSR controllers lock up after setting this type of
+	 * filter, so avoid sending the request altogether.
+	 */
+	if (test_bit(HCI_QUIRK_BROKEN_FILTER_CLEAR_ALL, &hdev->quirks))
 		return 0;
 
 	/* Always clear event filter when starting */
