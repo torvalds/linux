b65700d046a60d0a29f6289e13c62d0c93689f11
Suman Anna <s-anna@ti.com>
2022-03-08 18:25:15 +0100

    remoteproc: move rproc_da_to_va declaration to remoteproc.h
    The rproc_da_to_va() API is an exported function, so move its
    declaration from the remoteproc local remoteproc_internal.h
    to the public remoteproc.h file.
    
    This will allow drivers outside of the remoteproc folder to be
    able to use this API.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Dave Gerlach <d-gerlach@ti.com>
    [adjusted line numbers to apply]
    Signed-off-by: Drew Fustini <dfustini@baylibre.com>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Link: https://lore.kernel.org/r/20220308172515.29556-1-dfustini@baylibre.com

--- include/linux/remoteproc.h
+++ include/linux/remoteproc.h
@@ -675,6 +675,7 @@ void rproc_shutdown(struct rproc *rproc);
 int rproc_detach(struct rproc *rproc);
 int rproc_set_firmware(struct rproc *rproc, const char *fw_name);
 void rproc_report_crash(struct rproc *rproc, enum rproc_crash_type type);
+void *rproc_da_to_va(struct rproc *rproc, u64 da, size_t len, bool *is_iomem);
 void rproc_coredump_using_sections(struct rproc *rproc);
 int rproc_coredump_add_segment(struct rproc *rproc, dma_addr_t da, size_t size);
 int rproc_coredump_add_custom_segment(struct rproc *rproc,
