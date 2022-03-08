cded07a2dccd5493696a3adce175f01e413423c6
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2022-03-08 13:25:59 -0600

    ALSA: intel-dspconfig: add ES8336 support for CNL
    We're missing this check for the CNL PCI id
    
    Reported-by: Nikolai Kostrigin <nickel@altlinux.org>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Reviewed-by: Péter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Acked-by: Takashi Iwai <tiwai@suse.de>
    Link: https://lore.kernel.org/r/20220308192610.392950-10-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/hda/intel-dsp-config.c
+++ sound/hda/intel-dsp-config.c
@@ -200,6 +200,11 @@ static const struct config_entry config_table[] = {
 		}
 	},
 	{
+		.flags = FLAG_SOF,
+		.device = 0x09dc8,
+		.codec_hid =  &essx_83x6,
+	},
+	{
 		.flags = FLAG_SOF | FLAG_SOF_ONLY_IF_DMIC_OR_SOUNDWIRE,
 		.device = 0x9dc8,
 	},
