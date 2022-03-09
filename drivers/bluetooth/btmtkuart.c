6ac034a76aa230c1acb5bce4442d47310b5aa2f3
Sean Wang <sean.wang@mediatek.com>
2022-03-09 08:03:54 +0800

    Bluetooth: btmtkuart: fix the conflict between mtk and msft vendor event
    There is a conflict between MediaTek wmt event and msft vendor extension
    logic in the core layer since 145373cb1b1f ("Bluetooth: Add framework for
    Microsoft vendor extension") was introduced because we changed the type of
    mediatek wmt event to the type of msft vendor event in the driver.
    
    But the purpose we reported mediatek event to the core layer is for the
    diagnostic purpose with that we are able to see the full packet trace via
    monitoring socket with btmon. Thus, it is harmless we keep the original
    type of mediatek vendor event here to avoid breaking the msft extension
    function especially they can be supported by Mediatek future devices.
    
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/btmtkuart.c
+++ drivers/bluetooth/btmtkuart.c
@@ -192,13 +192,6 @@ static int btmtkuart_recv_event(struct hci_dev *hdev, struct sk_buff *skb)
 	struct hci_event_hdr *hdr = (void *)skb->data;
 	int err;
 
-	/* Fix up the vendor event id with 0xff for vendor specific instead
-	 * of 0xe4 so that event send via monitoring socket can be parsed
-	 * properly.
-	 */
-	if (hdr->evt == 0xe4)
-		hdr->evt = HCI_EV_VENDOR;
-
 	/* When someone waits for the WMT event, the skb is being cloned
 	 * and being processed the events from there then.
 	 */
@@ -214,7 +207,7 @@ static int btmtkuart_recv_event(struct hci_dev *hdev, struct sk_buff *skb)
 	if (err < 0)
 		goto err_free_skb;
 
-	if (hdr->evt == HCI_EV_VENDOR) {
+	if (hdr->evt == HCI_EV_WMT) {
 		if (test_and_clear_bit(BTMTKUART_TX_WAIT_VND_EVT,
 				       &bdev->tx_state)) {
 			/* Barrier to sync with other CPUs */
