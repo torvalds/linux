bda1027358e72e6255f11a4b59c375f138a2dd10
Alexandre Belloni <alexandre.belloni@bootlin.com>
2022-03-09 17:22:42 +0100

    rtc: pcf2127: set RTC_FEATURE_ALARM_RES_2S
    The PCF2127 doesn't support UIE because setting an alarm to fire every
    second confuses the chip and the fastest we can go is an alarm every 2
    seconds.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Link: https://lore.kernel.org/r/20220309162301.61679-11-alexandre.belloni@bootlin.com

--- drivers/rtc/rtc-pcf2127.c
+++ drivers/rtc/rtc-pcf2127.c
@@ -656,6 +656,7 @@ static int pcf2127_probe(struct device *dev, struct regmap *regmap,
 	pcf2127->rtc->range_min = RTC_TIMESTAMP_BEGIN_2000;
 	pcf2127->rtc->range_max = RTC_TIMESTAMP_END_2099;
 	pcf2127->rtc->set_start_time = true; /* Sets actual start to 1970 */
+	set_bit(RTC_FEATURE_ALARM_RES_2S, pcf2127->rtc->features);
 	clear_bit(RTC_FEATURE_UPDATE_INTERRUPT, pcf2127->rtc->features);
 	clear_bit(RTC_FEATURE_ALARM, pcf2127->rtc->features);
 
