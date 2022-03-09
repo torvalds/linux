d9f50991592513cc7633684cbaff65022cfa6816
Dust Li <dust.li@linux.alibaba.com>
2022-03-09 11:30:51 +0800

    net/smc: fix -Wmissing-prototypes warning when CONFIG_SYSCTL not set
    when CONFIG_SYSCTL not set, smc_sysctl_net_init/exit
    need to be static inline to avoid missing-prototypes
    if compile with W=1.
    
    Since __net_exit has noinline annotation when CONFIG_NET_NS
    not set, it should not be used with static inline.
    So remove the __net_init/exit when CONFIG_SYSCTL not set.
    
    Fixes: 7de8eb0d9039 ("net/smc: fix compile warning for smc_sysctl")
    Signed-off-by: Dust Li <dust.li@linux.alibaba.com>
    Link: https://lore.kernel.org/r/20220309033051.41893-1-dust.li@linux.alibaba.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/smc/smc_sysctl.h
+++ net/smc/smc_sysctl.h
@@ -20,13 +20,13 @@ void __net_exit smc_sysctl_net_exit(struct net *net);
 
 #else
 
-int __net_init smc_sysctl_net_init(struct net *net)
+static inline int smc_sysctl_net_init(struct net *net)
 {
 	net->smc.sysctl_autocorking_size = SMC_AUTOCORKING_DEFAULT_SIZE;
 	return 0;
 }
 
-void __net_exit smc_sysctl_net_exit(struct net *net) { }
+static inline void smc_sysctl_net_exit(struct net *net) { }
 
 #endif /* CONFIG_SYSCTL */
 
