7968778914e53788a01c2dee2692cab157de9ac0
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2022-03-09 20:50:39 +0100

    PCI: Remove the deprecated "pci-dma-compat.h" API
    Now that all usages of the functions defined in "pci-dma-compat.h" have
    been removed, it is time to remove this file as well.
    
    In order not to break builds, move the "#include <linux/dma-mapping.h>"
    that was in "pci-dma-compat.h" into "include/linux/pci.h"
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Acked-by: Bjorn Helgaas <bhelgaas@google.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -2473,8 +2473,7 @@ static inline bool pci_is_thunderbolt_attached(struct pci_dev *pdev)
 void pci_uevent_ers(struct pci_dev *pdev, enum  pci_ers_result err_type);
 #endif
 
-/* Provide the legacy pci_dma_* API */
-#include <linux/pci-dma-compat.h>
+#include <linux/dma-mapping.h>
 
 #define pci_printk(level, pdev, fmt, arg...) \
 	dev_printk(level, &(pdev)->dev, fmt, ##arg)
