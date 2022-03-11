4335edbbc1284259b17590f76a21fd4a162b4305
Zhenzhong Duan <zhenzhong.duan@intel.com>
2022-03-11 18:26:43 +0800

    KVM: x86: Remove redundant vm_entry_controls_clearbit() call
    When emulating exit from long mode, EFER_LMA is cleared with
    vmx_set_efer().  This will already unset the VM_ENTRY_IA32E_MODE control
    bit as requested by SDM, so there is no need to unset VM_ENTRY_IA32E_MODE
    again in exit_lmode() explicitly.  In case EFER isn't supported by
    hardware, long mode isn't supported, so exit_lmode() cannot be reached.
    
    Note that, thanks to the shadow controls mechanism, this change doesn't
    eliminate vmread or vmwrite.
    
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>
    Message-Id: <20220311102643.807507-3-zhenzhong.duan@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -2902,7 +2902,6 @@ static void enter_lmode(struct kvm_vcpu *vcpu)
 
 static void exit_lmode(struct kvm_vcpu *vcpu)
 {
-	vm_entry_controls_clearbit(to_vmx(vcpu), VM_ENTRY_IA32E_MODE);
 	vmx_set_efer(vcpu, vcpu->arch.efer & ~EFER_LMA);
 }
 
