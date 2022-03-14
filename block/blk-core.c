daaca3522a8e67c46e39ef09c1d542e866f85f3b
Ming Lei <ming.lei@redhat.com>
2022-03-14 12:30:18 +0800

    block: release rq qos structures for queue without disk
    blkcg_init_queue() may add rq qos structures to request queue, previously
    blk_cleanup_queue() calls rq_qos_exit() to release them, but commit
    8e141f9eb803 ("block: drain file system I/O on del_gendisk")
    moves rq_qos_exit() into del_gendisk(), so memory leak is caused
    because queues may not have disk, such as un-present scsi luns, nvme
    admin queue, ...
    
    Fixes the issue by adding rq_qos_exit() to blk_cleanup_queue() back.
    
    BTW, v5.18 won't need this patch any more since we move
    blkcg_init_queue()/blkcg_exit_queue() into disk allocation/release
    handler, and patches have been in for-5.18/block.
    
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: stable@vger.kernel.org
    Fixes: 8e141f9eb803 ("block: drain file system I/O on del_gendisk")
    Reported-by: syzbot+b42749a851a47a0f581b@syzkaller.appspotmail.com
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20220314043018.177141-1-ming.lei@redhat.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-core.c
+++ block/blk-core.c
@@ -50,6 +50,7 @@
 #include "blk-mq-sched.h"
 #include "blk-pm.h"
 #include "blk-throttle.h"
+#include "blk-rq-qos.h"
 
 struct dentry *blk_debugfs_root;
 
@@ -313,6 +314,9 @@ void blk_cleanup_queue(struct request_queue *q)
 	 * after draining finished.
 	 */
 	blk_freeze_queue(q);
+
+	/* cleanup rq qos structures for queue without disk */
+	rq_qos_exit(q);
 
 	blk_queue_flag_set(QUEUE_FLAG_DEAD, q);
 
