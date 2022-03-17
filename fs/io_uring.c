3b2b78a8eb7cc38d207c5ee516769bc3f44d19ea
Pavel Begunkov <asml.silence@gmail.com>
2022-03-17 02:03:38 +0000

    io_uring: extend provided buf return to fails
    It's never a good idea to put provided buffers without notifying the
    userspace, it'll lead to userspace leaks, so add io_put_kbuf() in
    io_req_complete_failed(). The fail helper is called by all sorts of
    requests, but it's still safe to do as io_put_kbuf() will return 0 in
    for all requests that don't support and so don't expect provided buffers.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/a4880106fcf199d5810707fe2d17126fcdf18bc4.1647481208.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -2124,7 +2124,7 @@ static inline void io_req_complete(struct io_kiocb *req, s32 res)
 static void io_req_complete_failed(struct io_kiocb *req, s32 res)
 {
 	req_set_fail(req);
-	io_req_complete_post(req, res, 0);
+	io_req_complete_post(req, res, io_put_kbuf(req, 0));
 }
 
 static void io_req_complete_fail_submit(struct io_kiocb *req)
