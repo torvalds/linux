0978e5919c2853c31971ccdf4b31eb690cc45fb5
Colin Ian King <colin.i.king@gmail.com>
2022-03-18 00:50:21 +0000

    atl1c: remove redundant assignment to variable size
    Variable sie is being assigned a value that is never read. The
    The assignment is redundant and can be removed.
    
    Cleans up clang scan build warning:
    drivers/net/ethernet/atheros/atl1c/atl1c_main.c:1054:22: warning:
    Although the value stored to 'size' is used in the enclosing
    expression, the value is never actually read from 'size'
    [deadcode.DeadStores]
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>
    Link: https://lore.kernel.org/r/20220318005021.82073-1-colin.i.king@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/atheros/atl1c/atl1c_main.c
+++ drivers/net/ethernet/atheros/atl1c/atl1c_main.c
@@ -1051,7 +1051,7 @@ static int atl1c_setup_ring_resources(struct atl1c_adapter *adapter)
 	 * each ring/block may need up to 8 bytes for alignment, hence the
 	 * additional bytes tacked onto the end.
 	 */
-	ring_header->size = size =
+	ring_header->size =
 		sizeof(struct atl1c_tpd_desc) * tpd_ring->count * tqc +
 		sizeof(struct atl1c_rx_free_desc) * rfd_ring->count * rqc +
 		sizeof(struct atl1c_recv_ret_status) * rfd_ring->count * rqc +
