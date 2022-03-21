f705ec764b34323412f14b9bd95412e9bcb8770b
Jiri Olsa <jolsa@kernel.org>
2022-03-21 08:01:12 +0100

    Revert "bpf: Add support to inline bpf_get_func_ip helper on x86"
    This reverts commit 97ee4d20ee67eb462581a7af01442de6586e390b.
    
    Following change is adding more complexity to bpf_get_func_ip
    helper for kprobe_multi programs, which can't be inlined easily.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/bpf/20220321070113.1449167-2-jolsa@kernel.org

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -1042,7 +1042,6 @@ static const struct bpf_func_proto bpf_get_func_ip_proto_kprobe = {
 
 BPF_CALL_1(bpf_get_func_ip_kprobe_multi, struct pt_regs *, regs)
 {
-	/* This helper call is inlined by verifier on x86. */
 	return instruction_pointer(regs);
 }
 
