5ae75b4ed30322b42abaa75ef1b784addfdb7dc9
David Howells <dhowells@redhat.com>
2022-03-21 08:11:52 +0000

    watch_queue: Fix NULL dereference in error cleanup
    [ Upstream commit a635415a064e77bcfbf43da413fd9dfe0bbed9cb ]
    
    In watch_queue_set_size(), the error cleanup code doesn't take account of
    the fact that __free_page() can't handle a NULL pointer when trying to free
    up buffer pages that did get allocated.
    
    Fix this by only calling __free_page() on the pages actually allocated.
    
    Without the fix, this can lead to something like the following:
    
    BUG: KASAN: null-ptr-deref in __free_pages+0x1f/0x1b0 mm/page_alloc.c:5473
    Read of size 4 at addr 0000000000000034 by task syz-executor168/3599
    ...
    Call Trace:
     <TASK>
     __dump_stack lib/dump_stack.c:88 [inline]
     dump_stack_lvl+0xcd/0x134 lib/dump_stack.c:106
     __kasan_report mm/kasan/report.c:446 [inline]
     kasan_report.cold+0x66/0xdf mm/kasan/report.c:459
     check_region_inline mm/kasan/generic.c:183 [inline]
     kasan_check_range+0x13d/0x180 mm/kasan/generic.c:189
     instrument_atomic_read include/linux/instrumented.h:71 [inline]
     atomic_read include/linux/atomic/atomic-instrumented.h:27 [inline]
     page_ref_count include/linux/page_ref.h:67 [inline]
     put_page_testzero include/linux/mm.h:717 [inline]
     __free_pages+0x1f/0x1b0 mm/page_alloc.c:5473
     watch_queue_set_size+0x499/0x630 kernel/watch_queue.c:275
     pipe_ioctl+0xac/0x2b0 fs/pipe.c:632
     vfs_ioctl fs/ioctl.c:51 [inline]
     __do_sys_ioctl fs/ioctl.c:874 [inline]
     __se_sys_ioctl fs/ioctl.c:860 [inline]
     __x64_sys_ioctl+0x193/0x200 fs/ioctl.c:860
     do_syscall_x64 arch/x86/entry/common.c:50 [inline]
     do_syscall_64+0x35/0xb0 arch/x86/entry/common.c:80
     entry_SYSCALL_64_after_hwframe+0x44/0xae
    
    Fixes: c73be61cede5 ("pipe: Add general notification queue support")
    Reported-and-tested-by: syzbot+d55757faa9b80590767b@syzkaller.appspotmail.com
    Signed-off-by: David Howells <dhowells@redhat.com>
    Reviewed-by: Fabio M. De Francesco <fmdefrancesco@gmail.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/watch_queue.c
+++ kernel/watch_queue.c
@@ -274,7 +274,7 @@ long watch_queue_set_size(struct pipe_inode_info *pipe, unsigned int nr_notes)
 	return 0;
 
 error_p:
-	for (i = 0; i < nr_pages; i++)
+	while (--i >= 0)
 		__free_page(pages[i]);
 	kfree(pages);
 error:
