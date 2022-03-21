10d6a439615bacd60a63f58299fb823da04e6464
Cai YiWei <cyw@rock-chips.com>
2022-03-21 20:42:30 +0800

    media: rockchip: isp: isp32 fix nv12 error
    Change-Id: I90c563f0120b9000b45d68a526c74d99f3913f01
    Signed-off-by: Cai YiWei <cyw@rock-chips.com>

--- drivers/media/platform/rockchip/isp/hw.c
+++ drivers/media/platform/rockchip/isp/hw.c
@@ -773,7 +773,7 @@ static void isp_config_clk(struct rkisp_hw_dev *dev, int on)
 		CIF_ICCL_SRSZ_CLK | CIF_ICCL_JPEG_CLK | CIF_ICCL_MI_CLK |
 		CIF_ICCL_IE_CLK | CIF_ICCL_MIPI_CLK | CIF_ICCL_DCROP_CLK;
 
-	if ((dev->isp_ver == ISP_V20 || dev->isp_ver == ISP_V30) && on)
+	if ((dev->isp_ver == ISP_V20 || dev->isp_ver == ISP_V30 || dev->isp_ver == ISP_V32) && on)
 		val |= ICCL_MPFBC_CLK;
 	if (dev->isp_ver == ISP_V32 && on)
 		val |= ISP32_BRSZ_CLK_ENABLE;
