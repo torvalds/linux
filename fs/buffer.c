c0226eb8bde854e016a594a16f5c0d98aca426fa
Minchan Kim <minchan@kernel.org>
2022-03-22 14:39:34 -0700

    mm: fs: fix lru_cache_disabled race in bh_lru
    Check lru_cache_disabled under bh_lru_lock.  Otherwise, it could introduce
    race below and it fails to migrate pages containing buffer_head.
    
       CPU 0					CPU 1
    
    bh_lru_install
                                           lru_cache_disable
      lru_cache_disabled = false
                                           atomic_inc(&lru_disable_count);
    				       invalidate_bh_lrus_cpu of CPU 0
    				       bh_lru_lock
    				       __invalidate_bh_lrus
    				       bh_lru_unlock
      bh_lru_lock
      install the bh
      bh_lru_unlock
    
    WHen this race happens a CMA allocation fails, which is critical for
    the workload which depends on CMA.
    
    Link: https://lkml.kernel.org/r/20220308180709.2017638-1-minchan@kernel.org
    Fixes: 8cc621d2f45d ("mm: fs: invalidate BH LRU during page migration")
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Cc: Chris Goldsworthy <cgoldswo@codeaurora.org>
    Cc: Marcelo Tosatti <mtosatti@redhat.com>
    Cc: John Dias <joaodias@google.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/buffer.c
+++ fs/buffer.c
@@ -1235,16 +1235,18 @@ static void bh_lru_install(struct buffer_head *bh)
 	int i;
 
 	check_irqs_on();
+	bh_lru_lock();
+
 	/*
 	 * the refcount of buffer_head in bh_lru prevents dropping the
 	 * attached page(i.e., try_to_free_buffers) so it could cause
 	 * failing page migration.
 	 * Skip putting upcoming bh into bh_lru until migration is done.
 	 */
-	if (lru_cache_disabled())
+	if (lru_cache_disabled()) {
+		bh_lru_unlock();
 		return;
-
-	bh_lru_lock();
+	}
 
 	b = this_cpu_ptr(&bh_lrus);
 	for (i = 0; i < BH_LRU_SIZE; i++) {
