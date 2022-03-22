7e55b95651d88e60368087c243525a0d97d43d3d
Steinar H. Gunderson <sesse@google.com>
2022-03-22 09:24:52 +0100

    perf intel-pt: Synthesize cycle events
    There is no good reason why we cannot synthesize "cycle" events from
    Intel PT just as we can synthesize "instruction" events, in particular
    when CYC packets are available. This enables using PT to getting much
    more accurate cycle profiles than regular sampling (record -e cycles)
    when the work last for very short periods (<10 ms).  Thus, add support
    for this, based off of the existing IPC calculation framework. The new
    option to --itrace is "y" (for cYcles), as c was taken for calls. Cycle
    and instruction events can be synthesized together, and are by default.
    
    The only real caveat is that CYC packets are only emitted whenever some
    other packet is, which in practice is when a branch instruction is
    encountered (and not even all branches). Thus, even at no subsampling
    (e.g. --itrace=y0ns), it is impossible to get more accuracy than a
    single basic block, and all cycles spent executing that block will get
    attributed to the branch instruction that ends the packet.  Thus, one
    cannot know whether the cycles came from e.g. a specific load, a
    mispredicted branch, or something else. When subsampling (which is the
    default), the cycle events will get smeared out even more, but will
    still be generally useful to attribute cycle counts to functions.
    
    Reviewed-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Steinar H. Gunderson <sesse@google.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20220322082452.1429091-1-sesse@google.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/auxtrace.c
+++ tools/perf/util/auxtrace.c
@@ -1394,6 +1394,7 @@ void itrace_synth_opts__set_default(struct itrace_synth_opts *synth_opts,
 		synth_opts->calls = true;
 	} else {
 		synth_opts->instructions = true;
+		synth_opts->cycles = true;
 		synth_opts->period_type = PERF_ITRACE_DEFAULT_PERIOD_TYPE;
 		synth_opts->period = PERF_ITRACE_DEFAULT_PERIOD;
 	}
@@ -1482,7 +1483,11 @@ int itrace_do_parse_synth_opts(struct itrace_synth_opts *synth_opts,
 	for (p = str; *p;) {
 		switch (*p++) {
 		case 'i':
-			synth_opts->instructions = true;
+		case 'y':
+			if (p[-1] == 'y')
+				synth_opts->cycles = true;
+			else
+				synth_opts->instructions = true;
 			while (*p == ' ' || *p == ',')
 				p += 1;
 			if (isdigit(*p)) {
@@ -1641,7 +1646,7 @@ int itrace_do_parse_synth_opts(struct itrace_synth_opts *synth_opts,
 		}
 	}
 out:
-	if (synth_opts->instructions) {
+	if (synth_opts->instructions || synth_opts->cycles) {
 		if (!period_type_set)
 			synth_opts->period_type =
 					PERF_ITRACE_DEFAULT_PERIOD_TYPE;
