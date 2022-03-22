2c8c86040fcfd1b7573228740fdc879b344d7e3d
Quentin Perret <qperret@google.com>
2022-03-22 14:31:37 +0000

    ANDROID: KVM: arm64: Add is_pkvm_initialized() helper
    Add a helper allowing to check when the pkvm static key is enabled to
    ease the introduction of pkvm hooks in other parts of the code.
    
    Signed-off-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Bug: 233587962
    Change-Id: I8995021768def73bd7636a84059bdc43fa7ab2fc
    Signed-off-by: Quentin Perret <qperret@google.com>

--- arch/arm64/include/asm/virt.h
+++ arch/arm64/include/asm/virt.h
@@ -81,6 +81,12 @@ void __hyp_reset_vectors(void);
 
 DECLARE_STATIC_KEY_FALSE(kvm_protected_mode_initialized);
 
+static inline bool is_pkvm_initialized(void)
+{
+	return IS_ENABLED(CONFIG_KVM) &&
+	       static_branch_likely(&kvm_protected_mode_initialized);
+}
+
 /* Reports the availability of HYP mode */
 static inline bool is_hyp_mode_available(void)
 {
@@ -88,8 +94,7 @@ static inline bool is_hyp_mode_available(void)
 	 * If KVM protected mode is initialized, all CPUs must have been booted
 	 * in EL2. Avoid checking __boot_cpu_mode as CPUs now come up in EL1.
 	 */
-	if (IS_ENABLED(CONFIG_KVM) &&
-	    static_branch_likely(&kvm_protected_mode_initialized))
+	if (is_pkvm_initialized())
 		return true;
 
 	return (__boot_cpu_mode[0] == BOOT_CPU_MODE_EL2 &&
@@ -103,8 +108,7 @@ static inline bool is_hyp_mode_mismatched(void)
 	 * If KVM protected mode is initialized, all CPUs must have been booted
 	 * in EL2. Avoid checking __boot_cpu_mode as CPUs now come up in EL1.
 	 */
-	if (IS_ENABLED(CONFIG_KVM) &&
-	    static_branch_likely(&kvm_protected_mode_initialized))
+	if (is_pkvm_initialized())
 		return false;
 
 	return __boot_cpu_mode[0] != __boot_cpu_mode[1];
