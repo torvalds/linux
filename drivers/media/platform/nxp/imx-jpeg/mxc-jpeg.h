bec0a3a67389ede106d0661a007edf832878d8b2
Ming Qian <ming.qian@nxp.com>
2022-03-23 10:05:55 +0100

    media: imx-jpeg: Identify and handle precision correctly
    The decoder will save the precision that was detected from jpeg header
    and use it later, when choosing the pixel format and also calculate
    bytesperline according to precision.
    
    The 12bit jpeg is not supported yet,
    but driver shouldn't led to serious problem if user enqueue a 12 bit jpeg.
    And the 12bit jpeg is supported by hardware, driver may support it later.
    
    [hverkuil: document the new precision field]
    
    Signed-off-by: Ming Qian <ming.qian@nxp.com>
    Reviewed-by: Mirela Rabulea <mirela.rabulea@nxp.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>

--- drivers/media/platform/nxp/imx-jpeg/mxc-jpeg.h
+++ drivers/media/platform/nxp/imx-jpeg/mxc-jpeg.h
@@ -49,6 +49,7 @@ enum mxc_jpeg_mode {
  * @h_align:	horizontal alignment order (align to 2^h_align)
  * @v_align:	vertical alignment order (align to 2^v_align)
  * @flags:	flags describing format applicability
+ * @precision:  jpeg sample precision
  */
 struct mxc_jpeg_fmt {
 	const char				*name;
@@ -60,6 +61,7 @@ struct mxc_jpeg_fmt {
 	int					h_align;
 	int					v_align;
 	u32					flags;
+	u8					precision;
 };
 
 struct mxc_jpeg_desc {
