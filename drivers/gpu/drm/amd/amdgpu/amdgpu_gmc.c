5f3854f1f4e211f494018160b348a1c16e58013f
Alex Deucher <alexander.deucher@amd.com>
2022-03-24 18:04:00 -0400

    drm/amdgpu: add more cases to noretry=1
    Port current list from amd-staging-drm-next.
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_gmc.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_gmc.c
@@ -564,6 +564,9 @@ void amdgpu_gmc_noretry_set(struct amdgpu_device *adev)
 	case IP_VERSION(9, 4, 0):
 	case IP_VERSION(9, 4, 1):
 	case IP_VERSION(9, 4, 2):
+	case IP_VERSION(10, 3, 5):
+	case IP_VERSION(10, 3, 3):
+	case IP_VERSION(9, 3, 0):
 		/*
 		 * noretry = 0 will cause kfd page fault tests fail
 		 * for some ASICs, so set default to 1 for these ASICs.
