ec13aa4e0085eba99fab07f9842a27b727cf50cd
Nathan Chancellor <nathan@kernel.org>
2022-03-24 08:36:45 -0700

    btrfs: remove unused variable in btrfs_{start,write}_dirty_block_groups()
    commit 6d4a6b515c39f1f8763093e0f828959b2fbc2f45 upstream.
    
    Clang's version of -Wunused-but-set-variable recently gained support for
    unary operations, which reveals two unused variables:
    
      fs/btrfs/block-group.c:2949:6: error: variable 'num_started' set but not used [-Werror,-Wunused-but-set-variable]
              int num_started = 0;
                  ^
      fs/btrfs/block-group.c:3116:6: error: variable 'num_started' set but not used [-Werror,-Wunused-but-set-variable]
              int num_started = 0;
                  ^
      2 errors generated.
    
    These variables appear to be unused from their introduction, so just
    remove them to silence the warnings.
    
    Fixes: c9dc4c657850 ("Btrfs: two stage dirty block group writeout")
    Fixes: 1bbc621ef284 ("Btrfs: allow block group cache writeout outside critical section in commit")
    CC: stable@vger.kernel.org # 5.4+
    Link: https://github.com/ClangBuiltLinux/linux/issues/1614
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/btrfs/block-group.c
+++ fs/btrfs/block-group.c
@@ -2882,7 +2882,6 @@ int btrfs_start_dirty_block_groups(struct btrfs_trans_handle *trans)
 	struct btrfs_path *path = NULL;
 	LIST_HEAD(dirty);
 	struct list_head *io = &cur_trans->io_bgs;
-	int num_started = 0;
 	int loops = 0;
 
 	spin_lock(&cur_trans->dirty_bgs_lock);
@@ -2948,7 +2947,6 @@ again:
 			cache->io_ctl.inode = NULL;
 			ret = btrfs_write_out_cache(trans, cache, path);
 			if (ret == 0 && cache->io_ctl.inode) {
-				num_started++;
 				should_put = 0;
 
 				/*
@@ -3049,7 +3047,6 @@ int btrfs_write_dirty_block_groups(struct btrfs_trans_handle *trans)
 	int should_put;
 	struct btrfs_path *path;
 	struct list_head *io = &cur_trans->io_bgs;
-	int num_started = 0;
 
 	path = btrfs_alloc_path();
 	if (!path)
@@ -3107,7 +3104,6 @@ int btrfs_write_dirty_block_groups(struct btrfs_trans_handle *trans)
 			cache->io_ctl.inode = NULL;
 			ret = btrfs_write_out_cache(trans, cache, path);
 			if (ret == 0 && cache->io_ctl.inode) {
-				num_started++;
 				should_put = 0;
 				list_add_tail(&cache->io_list, io);
 			} else {
