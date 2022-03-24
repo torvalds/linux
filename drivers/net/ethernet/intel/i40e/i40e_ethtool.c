c8631e61f4d459ce9259ffddbb23baafd514c98d
Nabil S. Alramli <dev@nalramli.com>
2022-03-24 20:03:38 +0000

    i40e: Add vsi.tx_restart to i40e ethtool stats
    Add vsi.tx_restart to the i40e driver ethtool statistics
    
    Signed-off-by: Nabil S. Alramli <dev@nalramli.com>
    Tested-by: Gurucharan <gurucharanx.g@intel.com> (A Contingent worker at Intel)
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_ethtool.c
+++ drivers/net/ethernet/intel/i40e/i40e_ethtool.c
@@ -300,6 +300,7 @@ static const struct i40e_stats i40e_gstrings_misc_stats[] = {
 	I40E_VSI_STAT("rx_cache_alloc", rx_page_alloc),
 	I40E_VSI_STAT("rx_cache_waive", rx_page_waive),
 	I40E_VSI_STAT("rx_cache_busy", rx_page_busy),
+	I40E_VSI_STAT("tx_restart", tx_restart),
 };
 
 /* These PF_STATs might look like duplicates of some NETDEV_STATs,
