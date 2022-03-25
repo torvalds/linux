01251dd004d8e106295c3aa8e3ba890f0dd55e02
Mark Brown <broonie@kernel.org>
2022-03-25 15:42:41 +0000

    ASoC: atmel: Don't squash error codes from atmel_ssc_set_audio()
    The AT91SAM9G20-EK audio driver is replacing any error code returned by
    atmel_ssc_set_audio() with -EINVAL which could be unhelpful for debugging.
    Pass through the error code, and include it in the log message we print for
    good measure.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Reviewed-by: Codrin Ciubotariu <codrin.ciubotariu@microchip.com>
    Link: https://lore.kernel.org/r/20220325154241.1600757-4-broonie@kernel.org

--- sound/soc/atmel/sam9g20_wm8731.c
+++ sound/soc/atmel/sam9g20_wm8731.c
@@ -127,8 +127,8 @@ static int at91sam9g20ek_audio_probe(struct platform_device *pdev)
 
 	ret = atmel_ssc_set_audio(0);
 	if (ret) {
-		dev_err(&pdev->dev, "ssc channel is not valid\n");
-		return -EINVAL;
+		dev_err(&pdev->dev, "ssc channel is not valid: %d\n", ret);
+		return ret;
 	}
 
 	card->dev = &pdev->dev;
