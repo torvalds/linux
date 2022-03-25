e5cd0623d7879eb98059223b9eb233a686d58b40
Stephen Kitt <steve@sk2.org>
2022-03-25 17:54:52 +0100

    ASoC: ad1*, ada*, ssm*: use i2c_match_id and simple i2c probe
    As part of the ongoing i2c transition to the simple probe
    ("probe_new"), this patch uses i2c_match_id to retrieve the
    driver_data for the probed device. The id parameter is thus no longer
    necessary and the simple probe can be used instead.
    
    Signed-off-by: Stephen Kitt <steve@sk2.org>
    Reviewed-by: Nuno Sá <nuno.sa@analog.com>
    Link: https://lore.kernel.org/r/20220325165452.1212975-1-steve@sk2.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/ssm2602-i2c.c
+++ sound/soc/codecs/ssm2602-i2c.c
@@ -13,15 +13,17 @@
 
 #include "ssm2602.h"
 
+static const struct i2c_device_id ssm2602_i2c_id[];
+
 /*
  * ssm2602 2 wire address is determined by GPIO5
  * state during powerup.
  *    low  = 0x1a
  *    high = 0x1b
  */
-static int ssm2602_i2c_probe(struct i2c_client *client,
-			     const struct i2c_device_id *id)
+static int ssm2602_i2c_probe(struct i2c_client *client)
 {
+	const struct i2c_device_id *id = i2c_match_id(ssm2602_i2c_id, client);
 	return ssm2602_probe(&client->dev, id->driver_data,
 		devm_regmap_init_i2c(client, &ssm2602_regmap_config));
 }
@@ -47,7 +49,7 @@ static struct i2c_driver ssm2602_i2c_driver = {
 		.name = "ssm2602",
 		.of_match_table = ssm2602_of_match,
 	},
-	.probe = ssm2602_i2c_probe,
+	.probe_new = ssm2602_i2c_probe,
 	.id_table = ssm2602_i2c_id,
 };
 module_i2c_driver(ssm2602_i2c_driver);
