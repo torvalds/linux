c95578235888431e61d36f212ad2236e0ff8b90f
Trond Myklebust <trond.myklebust@hammerspace.com>
2022-03-25 21:51:03 -0400

    NFS: Don't loop forever in nfs_do_recoalesce()
    [ Upstream commit d02d81efc7564b4d5446a02e0214a164cf00b1f3 ]
    
    If __nfs_pageio_add_request() fails to add the request, it will return
    with either desc->pg_error < 0, or mirror->pg_recoalesce will be set, so
    we are guaranteed either to exit the function altogether, or to loop.
    
    However if there is nothing left in mirror->pg_list to coalesce, we must
    exit, so make sure that we clear mirror->pg_recoalesce every time we
    loop.
    
    Reported-by: Olga Kornievskaia <aglo@umich.edu>
    Fixes: 70536bf4eb07 ("NFS: Clean up reset of the mirror accounting variables")
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/nfs/pagelist.c
+++ fs/nfs/pagelist.c
@@ -1227,6 +1227,7 @@ static int nfs_do_recoalesce(struct nfs_pageio_descriptor *desc)
 
 	do {
 		list_splice_init(&mirror->pg_list, &head);
+		mirror->pg_recoalesce = 0;
 
 		while (!list_empty(&head)) {
 			struct nfs_page *req;
