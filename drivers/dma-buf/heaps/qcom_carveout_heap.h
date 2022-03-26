2ac7e03c0bc07cbf33887c574f1b0c4640f3ebb3
Patrick Daly <quic_pdaly@quicinc.com>
2022-03-25 20:53:11 -0700

    dma-heaps: Fix compiler warning with function stubs
    Fixes:
    error: unused function 'qcom_carveout_heap_create'.
    
    Change-Id: I1525a3455b045c2e527847b26bfa7df5b6cf19dc
    Signed-off-by: Patrick Daly <quic_pdaly@quicinc.com>

--- drivers/dma-buf/heaps/qcom_carveout_heap.h
+++ drivers/dma-buf/heaps/qcom_carveout_heap.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _QCOM_CARVEOUT_HEAP_H
@@ -12,13 +13,13 @@
 int qcom_secure_carveout_heap_create(struct platform_heap *heap_data);
 int qcom_carveout_heap_create(struct platform_heap *heap_data);
 #else
-static int qcom_secure_carveout_heap_create(struct platform_heap *heap_data)
+static inline int qcom_secure_carveout_heap_create(struct platform_heap *heap_data)
 {
-	return 1;
+	return -EINVAL;
 }
-static int qcom_carveout_heap_create(struct platform_heap *heap_data)
+static inline int qcom_carveout_heap_create(struct platform_heap *heap_data)
 {
-	return 1;
+	return -EINVAL;
 }
 #endif
 
