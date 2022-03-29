5a18d07ce3006dbcb3c4cfc7bf1c094a5da19540
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2022-03-29 17:17:34 +0700

    tools/nolibc/types: Implement `offsetof()` and `container_of()` macro
    Implement `offsetof()` and `container_of()` macro. The first use case
    of these macros is for `malloc()`, `realloc()` and `free()`.
    
    Acked-by: Willy Tarreau <w@1wt.eu>
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- tools/include/nolibc/types.h
+++ tools/include/nolibc/types.h
@@ -191,4 +191,15 @@ struct stat {
 #define major(dev) ((unsigned int)(((dev) >> 8) & 0xfff))
 #define minor(dev) ((unsigned int)(((dev) & 0xff))
 
+#ifndef offsetof
+#define offsetof(TYPE, FIELD) ((size_t) &((TYPE *)0)->FIELD)
+#endif
+
+#ifndef container_of
+#define container_of(PTR, TYPE, FIELD) ({			\
+	__typeof__(((TYPE *)0)->FIELD) *__FIELD_PTR = (PTR);	\
+	(TYPE *)((char *) __FIELD_PTR - offsetof(TYPE, FIELD));	\
+})
+#endif
+
 #endif /* _NOLIBC_TYPES_H */
