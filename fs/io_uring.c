056e1e7c17136cea36267383a43d586509566931
Jens Axboe <axboe@kernel.dk>
2022-03-30 11:06:02 -0600

    io_uring: don't check req->file in io_fsync_prep()
    commit ec858afda857e361182ceafc3d2ba2b164b8e889 upstream.
    
    This is a leftover from the really old days where we weren't able to
    track and error early if we need a file and it wasn't assigned. Kill
    the check.
    
    Cc: stable@vger.kernel.org # v5.15+
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -4128,9 +4128,6 @@ static int io_fsync_prep(struct io_kiocb *req, const struct io_uring_sqe *sqe)
 {
 	struct io_ring_ctx *ctx = req->ctx;
 
-	if (!req->file)
-		return -EBADF;
-
 	if (unlikely(ctx->flags & IORING_SETUP_IOPOLL))
 		return -EINVAL;
 	if (unlikely(sqe->addr || sqe->ioprio || sqe->buf_index ||
