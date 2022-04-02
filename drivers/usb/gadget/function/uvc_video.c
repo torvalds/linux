aee340dccf5a09a4a346db7a46cef6cf26cb48fa
Michael Grzeschik <m.grzeschik@pengutronix.de>
2022-04-03 01:27:42 +0200

    usb: gadget: uvc: rework uvcg_queue_next_buffer to uvcg_complete_buffer
    commit 61aa709ca58a0dbeeb817bfa9230c1a92979f2c6 upstream.
    
    The function uvcg_queue_next_buffer is used different than its name
    suggests. The return value nextbuf is never used by any caller. This
    patch reworks the function to its actual purpose, by removing the unused
    code and renaming it. The function name uvcg_complete_buffer makes it
    more clear that it is actually marking the current video buffer as
    complete.
    
    Signed-off-by: Michael Grzeschik <m.grzeschik@pengutronix.de>
    Link: https://lore.kernel.org/r/20220402232744.3622565-2-m.grzeschik@pengutronix.de
    Cc: Dan Vacura <w36195@motorola.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/gadget/function/uvc_video.c
+++ drivers/usb/gadget/function/uvc_video.c
@@ -83,7 +83,7 @@ uvc_video_encode_bulk(struct usb_request *req, struct uvc_video *video,
 	if (buf->bytesused == video->queue.buf_used) {
 		video->queue.buf_used = 0;
 		buf->state = UVC_BUF_STATE_DONE;
-		uvcg_queue_next_buffer(&video->queue, buf);
+		uvcg_complete_buffer(&video->queue, buf);
 		video->fid ^= UVC_STREAM_FID;
 
 		video->payload_size = 0;
@@ -154,7 +154,7 @@ uvc_video_encode_isoc_sg(struct usb_request *req, struct uvc_video *video,
 		video->queue.buf_used = 0;
 		buf->state = UVC_BUF_STATE_DONE;
 		buf->offset = 0;
-		uvcg_queue_next_buffer(&video->queue, buf);
+		uvcg_complete_buffer(&video->queue, buf);
 		video->fid ^= UVC_STREAM_FID;
 	}
 }
@@ -181,7 +181,7 @@ uvc_video_encode_isoc(struct usb_request *req, struct uvc_video *video,
 	if (buf->bytesused == video->queue.buf_used) {
 		video->queue.buf_used = 0;
 		buf->state = UVC_BUF_STATE_DONE;
-		uvcg_queue_next_buffer(&video->queue, buf);
+		uvcg_complete_buffer(&video->queue, buf);
 		video->fid ^= UVC_STREAM_FID;
 	}
 }
