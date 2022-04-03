12b635b4e349e6d03a2ec698f946073f62770892
Tao Jin <tao-j@outlook.com>
2022-04-03 12:57:44 -0400

    HID: multitouch: add quirks to enable Lenovo X12 trackpoint
    commit 95cd2cdc88c755dcd0a58b951faeb77742c733a4 upstream.
    
    This applies the similar quirks used by previous generation devices
    such as X1 tablet for X12 tablet, so that the trackpoint and buttons
    can work.
    
    This patch was applied and tested working on 5.17.1 .
    
    Cc: stable@vger.kernel.org # 5.8+ given that it relies on 40d5bb87377a
    Signed-off-by: Tao Jin <tao-j@outlook.com>
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Link: https://lore.kernel.org/r/CO6PR03MB6241CB276FCDC7F4CEDC34F6E1E29@CO6PR03MB6241.namprd03.prod.outlook.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -2032,6 +2032,12 @@ static const struct hid_device_id mt_devices[] = {
 			   USB_VENDOR_ID_LENOVO,
 			   USB_DEVICE_ID_LENOVO_X1_TAB3) },
 
+	/* Lenovo X12 TAB Gen 1 */
+	{ .driver_data = MT_CLS_WIN_8_FORCE_MULTI_INPUT,
+		HID_DEVICE(BUS_USB, HID_GROUP_MULTITOUCH_WIN_8,
+			   USB_VENDOR_ID_LENOVO,
+			   USB_DEVICE_ID_LENOVO_X12_TAB) },
+
 	/* MosArt panels */
 	{ .driver_data = MT_CLS_CONFIDENCE_MINUS_ONE,
 		MT_USB_DEVICE(USB_VENDOR_ID_ASUS,
