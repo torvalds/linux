4056a947ac99226c40e35aa2b491028f6e379f5b
Bhaumik Bhatt <quic_bbhatt@quicinc.com>
2022-04-04 10:23:50 -0700

    bus: mhi: host: Fix DTR MHI dev pointer and circular dependency
    There is a typo and a wrong struct type was used for the DTR
    driver features. Update it and use the correct mhi_device type in
    place of the dtr_device pointer. Also fix a circular dependency on
    DTR and MHI misc by using the traditional status callback route to
    notify the DTR client driver.
    
    Changes for misc.c,mhi_misc.h are already present in commit f1f4164c4f15
    ("bus: mhi: host: Add snapshot of MHI bus misc driver"), and
    for mhi_dtr.c in commit 0ff9eadd9cb2 ("bus: mhi: devices:
    Add snapshot of MHI devices driver").
    
    Change-Id: I877d0ed009bbde30605bbd306deaf53588f1611f
    Signed-off-by: Bhaumik Bhatt <quic_bbhatt@quicinc.com>
    Signed-off-by: Lazarus Motha <quic_lmotha@quicinc.com>

--- include/linux/mhi.h
+++ include/linux/mhi.h
@@ -39,6 +39,7 @@ struct mhi_buf_info;
  * @MHI_CB_BW_REQ: Received a bandwidth switch request from device
  * @MHI_CB_FALLBACK_IMG: MHI device was loaded with the provided fallback image
  * @MHI_CB_DTR_SIGNAL: DTR signaling update
+ * @MHI_CB_DTR_START_CHANNELS: DTR signal for client driver to start channels
  */
 enum mhi_callback {
 	MHI_CB_IDLE,
@@ -52,6 +53,7 @@ enum mhi_callback {
 	MHI_CB_BW_REQ,
 	MHI_CB_FALLBACK_IMG,
 	MHI_CB_DTR_SIGNAL,
+	MHI_CB_DTR_START_CHANNELS,
 };
 
 /**
