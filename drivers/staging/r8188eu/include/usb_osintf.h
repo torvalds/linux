0f0360ca067cdab8eb9a33247f27cd99dba24d96
Michael Straube <straube.linux@gmail.com>
2022-04-07 21:28:17 +0200

    staging: r8188eu: remove unused prototype usbvendorrequest()
    Remove unused prototype usbvendorrequest().
    
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Link: https://lore.kernel.org/r/20220407192819.10661-7-straube.linux@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/r8188eu/include/usb_osintf.h
+++ drivers/staging/r8188eu/include/usb_osintf.h
@@ -13,9 +13,6 @@ extern int rtw_mc2u_disable;
 
 #define USBD_HALTED(Status) ((u32)(Status) >> 30 == 3)
 
-u8 usbvendorrequest(struct dvobj_priv *pdvobjpriv, enum bt_usb_request brequest,
-		    enum rt_usb_wvalue wvalue, u8 windex, void *data,
-		    u8 datalen, u8 isdirectionin);
 void netdev_br_init(struct net_device *netdev);
 void dhcp_flag_bcast(struct adapter *priv, struct sk_buff *skb);
 void *scdb_findEntry(struct adapter *priv, unsigned char *ipAddr);
