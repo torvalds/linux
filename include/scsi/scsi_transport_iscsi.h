740411ee2f94632127338decbdd4e9994778573b
Mike Christie <michael.christie@oracle.com>
2022-04-07 19:13:07 -0500

    scsi: iscsi: Release endpoint ID when its freed
    [ Upstream commit 3c6ae371b8a1ffba1fc415989fd581ebf841ed0a ]
    
    We can't release the endpoint ID until all references to the endpoint have
    been dropped or it could be allocated while in use. This has us use an idr
    instead of looping over all conns to find a free ID and then free the ID
    when all references have been dropped instead of when the device is only
    deleted.
    
    Link: https://lore.kernel.org/r/20220408001314.5014-4-michael.christie@oracle.com
    Tested-by: Manish Rangankar <mrangankar@marvell.com>
    Reviewed-by: Lee Duncan <lduncan@suse.com>
    Reviewed-by: Chris Leech <cleech@redhat.com>
    Reviewed-by: Wu Bo <wubo40@huawei.com>
    Signed-off-by: Mike Christie <michael.christie@oracle.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/scsi/scsi_transport_iscsi.h
+++ include/scsi/scsi_transport_iscsi.h
@@ -296,7 +296,7 @@ extern void iscsi_host_for_each_session(struct Scsi_Host *shost,
 struct iscsi_endpoint {
 	void *dd_data;			/* LLD private data */
 	struct device dev;
-	uint64_t id;
+	int id;
 	struct iscsi_cls_conn *conn;
 };
 
