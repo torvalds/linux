27c08f751cb1fc874562e9b18d70ea2af33ca889
Jiyoup Kim <lakroforce@gmail.com>
2022-04-10 00:05:37 +0900

    mm/slub: remove duplicate flag in allocate_slab()
    In allocate_slab(), __GFP_NOFAIL flag is removed twice when trying
    higher-order allocation. Remove it.
    
    Signed-off-by: Jiyoup Kim <lakroforce@gmail.com>
    Reviewed-by: Hyeonggon Yoo <42.hyeyoo@gmail.com>
    Reviewed-by: Muchun Song <songmuchun@bytedance.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    Link: https://lore.kernel.org/r/20220409150538.1264-1-lakroforce@gmail.com

--- mm/slub.c
+++ mm/slub.c
@@ -1939,7 +1939,7 @@ static struct slab *allocate_slab(struct kmem_cache *s, gfp_t flags, int node)
 	 */
 	alloc_gfp = (flags | __GFP_NOWARN | __GFP_NORETRY) & ~__GFP_NOFAIL;
 	if ((alloc_gfp & __GFP_DIRECT_RECLAIM) && oo_order(oo) > oo_order(s->min))
-		alloc_gfp = (alloc_gfp | __GFP_NOMEMALLOC) & ~(__GFP_RECLAIM|__GFP_NOFAIL);
+		alloc_gfp = (alloc_gfp | __GFP_NOMEMALLOC) & ~__GFP_RECLAIM;
 
 	slab = alloc_slab_page(alloc_gfp, node, oo);
 	if (unlikely(!slab)) {
