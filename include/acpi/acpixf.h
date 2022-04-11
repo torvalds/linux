0076ca940e3f1a1eae6b5e39b6594c96934713a2
Bob Moore <robert.moore@intel.com>
2022-04-11 21:04:03 +0200

    ACPICA: Update version to 20220331
    ACPICA commit ada5b805eaa7480930082af9bc3d689c6f181329
    
    Version 20220331.
    
    Link: https://github.com/acpica/acpica/commit/ada5b805
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/acpixf.h
+++ include/acpi/acpixf.h
@@ -12,7 +12,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20211217
+#define ACPI_CA_VERSION                 0x20220331
 
 #include <acpi/acconfig.h>
 #include <acpi/actypes.h>
