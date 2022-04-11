cc994bb97587787b8f0c094a9bc6945d82075b1d
Tony Lindgren <tony@atomide.com>
2022-04-11 14:16:57 +0300

    serial: 8250: Fix runtime PM for start_tx() for empty buffer
    Commit 932d596378b0 ("serial: 8250: Return early in .start_tx() if there
    are no chars to send") caused a regression where the drivers implementing
    runtime PM stopped idling. This is because serial8250_rpm_put_tx() is now
    unbalanced on early return, it normally gets called at __stop_tx().
    
    Fixes: 932d596378b0 ("serial: 8250: Return early in .start_tx() if there are no chars to send")
    Cc: Steffen Trumtrar <s.trumtrar@pengutronix.de>
    Cc: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Reviewed-by: Johan Hovold <johan@kernel.org>
    Reviewed-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Link: https://lore.kernel.org/r/20220411111657.16744-1-tony@atomide.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/8250/8250_port.c
+++ drivers/tty/serial/8250/8250_port.c
@@ -1675,10 +1675,10 @@ static void serial8250_start_tx(struct uart_port *port)
 	struct uart_8250_port *up = up_to_u8250p(port);
 	struct uart_8250_em485 *em485 = up->em485;
 
-	serial8250_rpm_get_tx(up);
-
 	if (!port->x_char && uart_circ_empty(&port->state->xmit))
 		return;
+
+	serial8250_rpm_get_tx(up);
 
 	if (em485 &&
 	    em485->active_timer == &em485->start_tx_timer)
