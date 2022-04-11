bdc3ad925131d9587e64e21ce5e2346f855f21eb
Wang Qing <wangqing@vivo.com>
2022-04-10 19:36:19 -0700

    arch_topology: Do not set llc_sibling if llc_id is invalid
    commit 1dc9f1a66e1718479e1c4f95514e1750602a3cb9 upstream.
    
    When ACPI is not enabled, cpuid_topo->llc_id = cpu_topo->llc_id = -1, which
    will set llc_sibling 0xff(...), this is misleading.
    
    Don't set llc_sibling(default 0) if we don't know the cache topology.
    
    Reviewed-by: Sudeep Holla <sudeep.holla@arm.com>
    Signed-off-by: Wang Qing <wangqing@vivo.com>
    Fixes: 37c3ec2d810f ("arm64: topology: divorce MC scheduling domain from core_siblings")
    Cc: stable <stable@vger.kernel.org>
    Link: https://lore.kernel.org/r/1649644580-54626-1-git-send-email-wangqing@vivo.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/arch_topology.c
+++ drivers/base/arch_topology.c
@@ -515,7 +515,7 @@ void update_siblings_masks(unsigned int cpuid)
 	for_each_online_cpu(cpu) {
 		cpu_topo = &cpu_topology[cpu];
 
-		if (cpuid_topo->llc_id == cpu_topo->llc_id) {
+		if (cpu_topo->llc_id != -1 && cpuid_topo->llc_id == cpu_topo->llc_id) {
 			cpumask_set_cpu(cpu, &cpuid_topo->llc_sibling);
 			cpumask_set_cpu(cpuid, &cpu_topo->llc_sibling);
 		}
