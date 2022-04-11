d78eaf06b5d9f4686846ca4e35207025c415ba85
Martin Habets <habetsm.xilinx@gmail.com>
2022-04-11 12:27:20 +0100

    sfc: Remove global definition of efx_reset_type_names
    The strings are only used in efx_common.c so the definitions
    can be static in there.
    
    Signed-off-by: Martin Habets <habetsm.xilinx@gmail.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- drivers/net/ethernet/sfc/efx_common.c
+++ drivers/net/ethernet/sfc/efx_common.c
@@ -51,8 +51,8 @@ static unsigned int efx_monitor_interval = 1 * HZ;
 /* Default stats update time */
 #define STATS_PERIOD_MS_DEFAULT 1000
 
-const unsigned int efx_reset_type_max = RESET_TYPE_MAX;
-const char *const efx_reset_type_names[] = {
+static const unsigned int efx_reset_type_max = RESET_TYPE_MAX;
+static const char *const efx_reset_type_names[] = {
 	[RESET_TYPE_INVISIBLE]          = "INVISIBLE",
 	[RESET_TYPE_ALL]                = "ALL",
 	[RESET_TYPE_RECOVER_OR_ALL]     = "RECOVER_OR_ALL",
