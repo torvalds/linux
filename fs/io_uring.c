e126391c092014af7ea6786648c9f1d3fce512a3
Pavel Begunkov <asml.silence@gmail.com>
2022-04-12 15:09:45 +0100

    io_uring: shrink final link flush
    All good users should not set IOSQE_IO_*LINK flags for the last request
    of a link. io_uring flushes collected links at the end of submission,
    but it's not the optimal way and so we don't care too much about it.
    Replace io_queue_sqe() call with io_queue_sqe_fallback() as the former
    one is inlined and will generate a bunch of extra code. This will also
    help compilers with the submission path inlining.
    
    > size ./fs/io_uring.o
       text    data     bss     dec     hex filename
      87265   13734       8  101007   18a8f ./fs/io_uring.o
    > size ./fs/io_uring.o
       text    data     bss     dec     hex filename
      87073   13734       8  100815   189cf ./fs/io_uring.o
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/01fb5e417ef49925d544a0b0bae30409845ed2b4.1649771823.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -7785,8 +7785,8 @@ static void io_submit_state_end(struct io_ring_ctx *ctx)
 {
 	struct io_submit_state *state = &ctx->submit_state;
 
-	if (state->link.head)
-		io_queue_sqe(state->link.head);
+	if (unlikely(state->link.head))
+		io_queue_sqe_fallback(state->link.head);
 	/* flush only after queuing links as they can generate completions */
 	io_submit_flush_completions(ctx);
 	if (state->plug_started)
