16bf5a5e1ec56474ed2a19d72f272ed09a5d3ea1
Thomas Gleixner <tglx@linutronix.de>
2022-04-13 15:31:03 +0200

    smp: Rename flush_smp_call_function_from_idle()
    This is invoked from the stopper thread too, which is definitely not idle.
    Rename it to flush_smp_call_function_queue() and fixup the callers.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20220413133024.305001096@linutronix.de

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2411,7 +2411,7 @@ static int migration_cpu_stop(void *data)
 	 * __migrate_task() such that we will not miss enforcing cpus_ptr
 	 * during wakeups, see set_cpus_allowed_ptr()'s TASK_WAKING test.
 	 */
-	flush_smp_call_function_from_idle();
+	flush_smp_call_function_queue();
 
 	raw_spin_lock(&p->pi_lock);
 	rq_lock(rq, &rf);
