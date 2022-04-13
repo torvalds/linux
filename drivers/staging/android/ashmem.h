f5eed40f62780363fe5e5b7bd399f65fec6d9427
Greg Kroah-Hartman <gregkh@google.com>
2022-04-13 13:43:59 +0200

    Revert "staging: remove ashmem"
    This reverts commit 721412ed3d819e767cac2b06646bf03aa158aaec.
    
    Android just isn't ready yet to give it up :(
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Ie0a443d55e6e72df97c3d3eabb7358ae3a12548a

---
+++ drivers/staging/android/ashmem.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0 OR Apache-2.0 */
+/*
+ * include/linux/ashmem.h
+ *
+ * Copyright 2008 Google Inc.
+ * Author: Robert Love
+ */
+
+#ifndef _LINUX_ASHMEM_H
+#define _LINUX_ASHMEM_H
+
+#include <linux/limits.h>
+#include <linux/ioctl.h>
+#include <linux/compat.h>
+
+#include "uapi/ashmem.h"
+
+/* support of 32bit userspace on 64bit platforms */
+#ifdef CONFIG_COMPAT
+#define COMPAT_ASHMEM_SET_SIZE		_IOW(__ASHMEMIOC, 3, compat_size_t)
+#define COMPAT_ASHMEM_SET_PROT_MASK	_IOW(__ASHMEMIOC, 5, unsigned int)
+#endif
+
+#endif	/* _LINUX_ASHMEM_H */
