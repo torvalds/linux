bba98083499f63f62419edf42022275d34a72470
Menglong Dong <imagedong@tencent.com>
2022-04-13 16:15:57 +0800

    net: ipv6: remove redundant statistics in ipv6_hop_jumbo()
    There are two call chains for ipv6_hop_jumbo(). The first one is:
    
    ipv6_destopt_rcv() -> ip6_parse_tlv() -> ipv6_hop_jumbo()
    
    On this call chain, the drop statistics will be done in
    ipv6_destopt_rcv() with 'IPSTATS_MIB_INHDRERRORS' if ipv6_hop_jumbo()
    returns false.
    
    The second call chain is:
    
    ip6_rcv_core() -> ipv6_parse_hopopts() -> ip6_parse_tlv()
    
    And the drop statistics will also be done in ip6_rcv_core() with
    'IPSTATS_MIB_INHDRERRORS' if ipv6_hop_jumbo() returns false.
    
    Therefore, the statistics in ipv6_hop_jumbo() is redundant, which
    means the drop is counted twice. The statistics in ipv6_hop_jumbo()
    is almost the same as the outside, except the
    'IPSTATS_MIB_INTRUNCATEDPKTS', which seems that we have to ignore it.
    
    Signed-off-by: Menglong Dong <imagedong@tencent.com>
    Reviewed-by: Jiang Biao <benbjiang@tencent.com>
    Reviewed-by: Hao Peng <flyingpeng@tencent.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/exthdrs.c
+++ net/ipv6/exthdrs.c
@@ -994,33 +994,26 @@ drop:
 static bool ipv6_hop_jumbo(struct sk_buff *skb, int optoff)
 {
 	const unsigned char *nh = skb_network_header(skb);
-	struct inet6_dev *idev = __in6_dev_get_safely(skb->dev);
-	struct net *net = ipv6_skb_net(skb);
 	u32 pkt_len;
 
 	if (nh[optoff + 1] != 4 || (optoff & 3) != 2) {
 		net_dbg_ratelimited("ipv6_hop_jumbo: wrong jumbo opt length/alignment %d\n",
 				    nh[optoff+1]);
-		__IP6_INC_STATS(net, idev, IPSTATS_MIB_INHDRERRORS);
 		goto drop;
 	}
 
 	pkt_len = ntohl(*(__be32 *)(nh + optoff + 2));
 	if (pkt_len <= IPV6_MAXPLEN) {
-		__IP6_INC_STATS(net, idev, IPSTATS_MIB_INHDRERRORS);
 		icmpv6_param_prob(skb, ICMPV6_HDR_FIELD, optoff+2);
 		return false;
 	}
 	if (ipv6_hdr(skb)->payload_len) {
-		__IP6_INC_STATS(net, idev, IPSTATS_MIB_INHDRERRORS);
 		icmpv6_param_prob(skb, ICMPV6_HDR_FIELD, optoff);
 		return false;
 	}
 
-	if (pkt_len > skb->len - sizeof(struct ipv6hdr)) {
-		__IP6_INC_STATS(net, idev, IPSTATS_MIB_INTRUNCATEDPKTS);
+	if (pkt_len > skb->len - sizeof(struct ipv6hdr))
 		goto drop;
-	}
 
 	if (pskb_trim_rcsum(skb, pkt_len + sizeof(struct ipv6hdr)))
 		goto drop;
