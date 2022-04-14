11451693e4081d32ef65147c6ca08cd0094ae252
Daniel Starke <daniel.starke@siemens.com>
2022-04-14 02:42:06 -0700

    tty: n_gsm: fix missing mux reset on config change at responder
    Currently, only the initiator resets the mux protocol if the user requests
    new parameters that are incompatible to those of the current connection.
    The responder also needs to reset the multiplexer if the new parameter set
    requires this. Otherwise, we end up with an inconsistent parameter set
    between initiator and responder.
    Revert the old behavior to inform the peer upon an incompatible parameter
    set change from the user on the responder side by re-establishing the mux
    protocol in such case.
    
    Fixes: 509067bbd264 ("tty: n_gsm: Delete gsm_disconnect when config requester")
    Cc: stable@vger.kernel.org
    Signed-off-by: Daniel Starke <daniel.starke@siemens.com>
    Link: https://lore.kernel.org/r/20220414094225.4527-1-daniel.starke@siemens.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/n_gsm.c
+++ drivers/tty/n_gsm.c
@@ -2373,7 +2373,7 @@ static int gsm_config(struct gsm_mux *gsm, struct gsm_config *c)
 	 * configuration
 	 */
 
-	if (gsm->initiator && (need_close || need_restart)) {
+	if (need_close || need_restart) {
 		int ret;
 
 		ret = gsm_disconnect(gsm);
