15527fee767f592d894d954d78cf8af3685cda6d
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2022-04-14 13:48:03 -0500

    ASoC: SOF: add definitions for different IPC types
    Add enum type to allow for different types of IPCs. The IPCv4 is
    intended for Intel only as a convergence path with firmware used in
    Windows. Follow-up patches will introduce different abstractions with
    .ops and different search paths for firmware and topology files.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Reviewed-by: Rander Wang <rander.wang@intel.com>
    Reviewed-by: Péter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Link: https://lore.kernel.org/r/20220414184817.362215-2-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/sound/sof.h
+++ include/sound/sof.h
@@ -47,6 +47,13 @@ enum sof_dsp_power_states {
 	SOF_DSP_PM_D3,
 };
 
+/* Definitions for multiple IPCs */
+enum sof_ipc_type {
+	SOF_IPC,
+	SOF_INTEL_IPC4,
+	SOF_IPC_TYPE_COUNT
+};
+
 /*
  * SOF Platform data.
  */
@@ -83,6 +90,8 @@ struct snd_sof_pdata {
 	const struct snd_soc_acpi_mach *machine;
 
 	void *hw_pdata;
+
+	enum sof_ipc_type ipc_type;
 };
 
 /*
