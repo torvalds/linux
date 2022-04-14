7c507d35a53214ea366f2aeda7bebbc94909491b
James Zhu <James.Zhu@amd.com>
2022-04-14 10:19:13 -0400

    drm/amdgpu/jpeg: enable JPEG PG and CG for VCN4_0_0
    Enable JPEG PG and CG for VCN4_0_0.
    
    Reviewed-by: Leo Liu <leo.liu@amd.com>
    Reviewed-by: Sonny Jiang <sonny.jiang@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: James Zhu <James.Zhu@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/soc21.c
+++ drivers/gpu/drm/amd/amdgpu/soc21.c
@@ -483,8 +483,10 @@ static int soc21_common_early_init(void *handle)
 		adev->cg_flags = AMD_CG_SUPPORT_GFX_CGCG |
 			AMD_CG_SUPPORT_GFX_CGLS |
 			AMD_CG_SUPPORT_REPEATER_FGCG |
-			AMD_CG_SUPPORT_VCN_MGCG;
+			AMD_CG_SUPPORT_VCN_MGCG |
+			AMD_CG_SUPPORT_JPEG_MGCG;
 		adev->pg_flags = AMD_PG_SUPPORT_VCN |
+			AMD_PG_SUPPORT_JPEG |
 			AMD_PG_SUPPORT_ATHUB |
 			AMD_PG_SUPPORT_MMHUB;
 		adev->external_rev_id = adev->rev_id + 0x1; // TODO: need update
