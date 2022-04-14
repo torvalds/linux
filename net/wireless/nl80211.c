1b550a0bebfc0b69d6ec08fe6eb58953a8aec48a
Johannes Berg <johannes.berg@intel.com>
2022-04-14 14:04:02 +0200

    nl80211: don't hold RTNL in color change request
    It's not necessary to hold the RTNL across color change
    requests, since all the inner locking needs only the
    wiphy mutex which we already hold as well.
    
    Fixes: 0d2ab3aea50b ("nl80211: add support for BSS coloring")
    Link: https://lore.kernel.org/r/20220414140402.32e03e8c261b.I5e7dc6bc563a129b938c43298da6bb4e812400a5@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -16273,8 +16273,7 @@ static const struct genl_small_ops nl80211_small_ops[] = {
 		.validate = GENL_DONT_VALIDATE_STRICT | GENL_DONT_VALIDATE_DUMP,
 		.doit = nl80211_color_change,
 		.flags = GENL_UNS_ADMIN_PERM,
-		.internal_flags = NL80211_FLAG_NEED_NETDEV_UP |
-				  NL80211_FLAG_NEED_RTNL,
+		.internal_flags = NL80211_FLAG_NEED_NETDEV_UP,
 	},
 	{
 		.cmd = NL80211_CMD_SET_FILS_AAD,
