63b170806bd8f62d15cb86e1a532185aacfd3a96
Flora Cui <flora.cui@amd.com>
2022-04-15 10:25:29 -0400

    drm/amdgpu/discovery: add sdma6 support for SDMA 6.0.2
    Enable sdma6 support.
    
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
@@ -1784,6 +1784,7 @@ static int amdgpu_discovery_set_sdma_ip_blocks(struct amdgpu_device *adev)
 		amdgpu_device_ip_block_add(adev, &sdma_v5_2_ip_block);
 		break;
 	case IP_VERSION(6, 0, 0):
+	case IP_VERSION(6, 0, 2):
 		amdgpu_device_ip_block_add(adev, &sdma_v6_0_ip_block);
 		break;
 	default:
