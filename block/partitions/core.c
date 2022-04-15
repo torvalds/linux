f0f975a4dde890bfe25ce17bf07a6495453988a4
Christoph Hellwig <hch@lst.de>
2022-04-15 06:52:51 +0200

    block: use bdev_discard_alignment in part_discard_alignment_show
    Use the bdev based alignment helper instead of open coding it.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Martin K. Petersen <martin.petersen@oracle.com>
    Link: https://lore.kernel.org/r/20220415045258.199825-21-hch@lst.de
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/partitions/core.c
+++ block/partitions/core.c
@@ -206,11 +206,7 @@ static ssize_t part_alignment_offset_show(struct device *dev,
 static ssize_t part_discard_alignment_show(struct device *dev,
 					   struct device_attribute *attr, char *buf)
 {
-	struct block_device *bdev = dev_to_bdev(dev);
-
-	return sprintf(buf, "%u\n",
-		queue_limit_discard_alignment(&bdev_get_queue(bdev)->limits,
-				bdev->bd_start_sect));
+	return sprintf(buf, "%u\n", bdev_discard_alignment(dev_to_bdev(dev)));
 }
 
 static DEVICE_ATTR(partition, 0444, part_partition_show, NULL);
