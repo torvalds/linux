96b6efb72d1413fab4d9e7be9a87f7d2b191a3fb
Ivan Safonov <insafonov@gmail.com>
2022-04-17 19:36:08 +0300

    r8188eu: remove unused urbs from struct xmit_buf
    Driver allocates eighth urbs per xmit_buf, but uses only first urb.
    Allocation of seven remaining urbs wastes memory for nothing.
    
    Reviewed-by: Pavel Skripkin <paskripkin@gmail.com>
    Signed-off-by: Ivan Safonov <insafonov@gmail.com>
    Link: https://lore.kernel.org/r/20220417163608.42917-1-insafonov@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/r8188eu/include/rtw_xmit.h
+++ drivers/staging/r8188eu/include/rtw_xmit.h
@@ -198,7 +198,7 @@ struct xmit_buf {
 	u32  len;
 	struct submit_ctx *sctx;
 	u32	ff_hwaddr;
-	struct urb *pxmit_urb[8];
+	struct urb *pxmit_urb;
 	dma_addr_t dma_transfer_addr;	/* (in) dma addr for transfer_buffer */
 	u8 bpending[8];
 	int last[8];
