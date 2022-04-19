f08a06c9a35706349f74b7a18deefe3f89f73e8e
Zeng Guang <guang.zeng@intel.com>
2022-04-19 23:36:04 +0800

    KVM: VMX: Clean up vmx_refresh_apicv_exec_ctrl()
    Remove the condition check cpu_has_secondary_exec_ctrls(). Calling
    vmx_refresh_apicv_exec_ctrl() premises secondary controls activated
    and VMCS fields related to APICv valid as well. If it's invoked in
    wrong circumstance at the worst case, VMX operation will report
    VMfailValid error without further harmful impact and just functions
    as if all the secondary controls were 0.
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Zeng Guang <guang.zeng@intel.com>
    Message-Id: <20220419153604.11786-1-guang.zeng@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -4201,16 +4201,15 @@ static void vmx_refresh_apicv_exec_ctrl(struct kvm_vcpu *vcpu)
 	}
 
 	pin_controls_set(vmx, vmx_pin_based_exec_ctrl(vmx));
-	if (cpu_has_secondary_exec_ctrls()) {
-		if (kvm_vcpu_apicv_active(vcpu))
-			secondary_exec_controls_setbit(vmx,
-				      SECONDARY_EXEC_APIC_REGISTER_VIRT |
-				      SECONDARY_EXEC_VIRTUAL_INTR_DELIVERY);
-		else
-			secondary_exec_controls_clearbit(vmx,
-					SECONDARY_EXEC_APIC_REGISTER_VIRT |
-					SECONDARY_EXEC_VIRTUAL_INTR_DELIVERY);
-	}
+
+	if (kvm_vcpu_apicv_active(vcpu))
+		secondary_exec_controls_setbit(vmx,
+					       SECONDARY_EXEC_APIC_REGISTER_VIRT |
+					       SECONDARY_EXEC_VIRTUAL_INTR_DELIVERY);
+	else
+		secondary_exec_controls_clearbit(vmx,
+						 SECONDARY_EXEC_APIC_REGISTER_VIRT |
+						 SECONDARY_EXEC_VIRTUAL_INTR_DELIVERY);
 
 	vmx_update_msr_bitmap_x2apic(vcpu);
 }
