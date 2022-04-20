67e1e2f4854bb2c0dd2b8440cf090016a0e1a091
Guillaume Nault <gnault@redhat.com>
2022-04-21 01:21:33 +0200

    ipv4: Avoid using RTO_ONLINK with ip_route_connect().
    Now that ip_rt_fix_tos() doesn't reset ->flowi4_scope unconditionally,
    we don't have to rely on the RTO_ONLINK bit to properly set the scope
    of a flowi4 structure. We can just set ->flowi4_scope explicitly and
    avoid using RTO_ONLINK in ->flowi4_tos.
    
    This patch converts callers of ip_route_connect(). Instead of setting
    the tos parameter with RT_CONN_FLAGS(sk), as all callers do, we can:
    
      1- Drop the tos parameter from ip_route_connect(): its value was
         entirely based on sk, which is also passed as parameter.
    
      2- Set ->flowi4_scope depending on the SOCK_LOCALROUTE socket option
         instead of always initialising it with RT_SCOPE_UNIVERSE (let's
         define ip_sock_rt_scope() for this purpose).
    
      3- Avoid overloading ->flowi4_tos with RTO_ONLINK: since the scope is
         now properly initialised, we don't need to tell ip_rt_fix_tos() to
         adjust ->flowi4_scope for us. So let's define ip_sock_rt_tos(),
         which is the same as RT_CONN_FLAGS() but without the RTO_ONLINK
         bit overload.
    
    Note:
      In the original ip_route_connect() code, __ip_route_output_key()
      might clear the RTO_ONLINK bit of fl4->flowi4_tos (because of
      ip_rt_fix_tos()). Therefore flowi4_update_output() had to reuse the
      original tos variable. Now that we don't set RTO_ONLINK any more,
      this is not a problem and we can use fl4->flowi4_tos in
      flowi4_update_output().
    
    Signed-off-by: Guillaume Nault <gnault@redhat.com>
    Reviewed-by: David Ahern <dsahern@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dccp/ipv4.c
+++ net/dccp/ipv4.c
@@ -76,9 +76,8 @@ int dccp_v4_connect(struct sock *sk, struct sockaddr *uaddr, int addr_len)
 	orig_dport = usin->sin_port;
 	fl4 = &inet->cork.fl.u.ip4;
 	rt = ip_route_connect(fl4, nexthop, inet->inet_saddr,
-			      RT_CONN_FLAGS(sk), sk->sk_bound_dev_if,
-			      IPPROTO_DCCP,
-			      orig_sport, orig_dport, sk);
+			      sk->sk_bound_dev_if, IPPROTO_DCCP, orig_sport,
+			      orig_dport, sk);
 	if (IS_ERR(rt))
 		return PTR_ERR(rt);
 
