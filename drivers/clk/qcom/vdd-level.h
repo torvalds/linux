bbf27d45d38a815c4c56d23f812a38e6e044b269
Xubin Bai <quic_xubibai@quicinc.com>
2022-04-20 05:21:25 -0700

    clk: qcom: vdd-level: Add required vdd level 'TURBO_L1'
    Add required vdd level 'TURBO_L1' for Pineapple gcc.
    
    Change-Id: Ib7b4cd22b0e90ca4729a739273ca5d62a6695f63
    Signed-off-by: Xubin Bai <quic_xubibai@quicinc.com>

--- drivers/clk/qcom/vdd-level.h
+++ drivers/clk/qcom/vdd-level.h
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /* Copyright (c) 2019-2021, The Linux Foundation. All rights reserved. */
+/* Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved. */
 
 #ifndef __DRIVERS_CLK_QCOM_VDD_LEVEL_H
 #define __DRIVERS_CLK_QCOM_VDD_LEVEL_H
@@ -17,6 +18,7 @@ enum vdd_levels {
 	VDD_LOW_L1,		/* SVS_L1 */
 	VDD_NOMINAL,		/* NOM */
 	VDD_HIGH,		/* TURBO */
+	VDD_HIGH_L1,		/* TURBO_L1 */
 	VDD_NUM,
 };
 
@@ -30,6 +32,7 @@ static int vdd_corner[] = {
 	[VDD_LOW_L1]	= RPMH_REGULATOR_LEVEL_SVS_L1,
 	[VDD_NOMINAL]	= RPMH_REGULATOR_LEVEL_NOM,
 	[VDD_HIGH]	= RPMH_REGULATOR_LEVEL_TURBO,
+	[VDD_HIGH_L1]	= RPMH_REGULATOR_LEVEL_TURBO_L1,
 };
 
 #endif
