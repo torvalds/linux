f7a2d0688a3b2bb4769402b4c962f54f7b0fc23c
Xiubo Li <xiubli@redhat.com>
2022-04-20 13:13:02 +0800

    ceph: disable updating the atime since cephfs won't maintain it
    Since CephFS makes no attempt to maintain atime, we shouldn't
    try to update it in mmap and generic read cases and ignore updating
    it in direct and sync read cases.
    
    And even we update it in mmap and generic read cases we will drop
    it and won't sync it to MDS. And we are seeing the atime will be
    updated and then dropped to the floor again and again.
    
    URL: https://lists.ceph.io/hyperkitty/list/ceph-users@ceph.io/thread/VSJM7T4CS5TDRFF6XFPIYMHP75K73PZ6/
    Signed-off-by: Xiubo Li <xiubli@redhat.com>
    Acked-by: Ilya Dryomov <idryomov@gmail.com>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- fs/ceph/addr.c
+++ fs/ceph/addr.c
@@ -1777,7 +1777,6 @@ int ceph_mmap(struct file *file, struct vm_area_struct *vma)
 
 	if (!mapping->a_ops->readpage)
 		return -ENOEXEC;
-	file_accessed(file);
 	vma->vm_ops = &ceph_vmops;
 	return 0;
 }
