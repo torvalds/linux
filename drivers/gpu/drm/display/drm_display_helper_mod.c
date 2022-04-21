1e0f66420b13fdd45754be0f15220a9185d59aa4
Thomas Zimmermann <tzimmermann@suse.de>
2022-04-21 09:31:03 +0200

    drm/display: Introduce a DRM display-helper module
    Replace the DP-helper module with a display-helper module. The
    support for DisplayPort becomes an internal option that drivers
    have to select. Update all related Kconfig and Makefile rules.
    
    Besides the existing code for DisplayPort, the new module will
    contain helpers for other video-output standards, such as HDMI.
    Drivers will have to select their required video-output helpers.
    
    Linking all display-related code into a single module avoids the
    proliferation of small kernel modules.
    
    The module parameters drm_dp_cec_unregister_delay, dp_aux_i2c_speed_khz,
    and dp_aux_i2c_transfer_size are moving from the drm_dp_helper namespace
    to drm_display_helper.
    
    v2:
    	* mention module parameters in commit message (Javier)
    	* distiguish between display module and DP support in Kconfig
    	* update Makefile rules for DP helpers
    	* move Kconfig rules into separate file under display/
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Lyude Paul <lyude@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220421073108.19226-4-tzimmermann@suse.de

--- drivers/gpu/drm/display/drm_display_helper_mod.c
+++ drivers/gpu/drm/display/drm_display_helper_mod.c
@@ -4,19 +4,19 @@
 
 #include "drm_dp_helper_internal.h"
 
-MODULE_DESCRIPTION("DRM DisplayPort helper");
+MODULE_DESCRIPTION("DRM display adapter helper");
 MODULE_LICENSE("GPL and additional rights");
 
-static int __init drm_dp_helper_module_init(void)
+static int __init drm_display_helper_module_init(void)
 {
 	return drm_dp_aux_dev_init();
 }
 
-static void __exit drm_dp_helper_module_exit(void)
+static void __exit drm_display_helper_module_exit(void)
 {
 	/* Call exit functions from specific dp helpers here */
 	drm_dp_aux_dev_exit();
 }
 
-module_init(drm_dp_helper_module_init);
-module_exit(drm_dp_helper_module_exit);
+module_init(drm_display_helper_module_init);
+module_exit(drm_display_helper_module_exit);
