c7399e6d3b18dc17974bc44e2e178664f1bd1bec
Vinod Koul <vkoul@kernel.org>
2022-04-21 17:47:33 +0530

    dmaengine: qcom: gpi: Add support for sc7280
    Add compatible and driver_data for GPI DMA engines found in Qualcomm
    SC7280. The driver_data contains ee_offset of 0x10000.
    
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Link: https://lore.kernel.org/r/20220421121733.1829350-1-vkoul@kernel.org
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/qcom/gpi.c
+++ drivers/dma/qcom/gpi.c
@@ -2286,6 +2286,7 @@ static int gpi_probe(struct platform_device *pdev)
 }
 
 static const struct of_device_id gpi_of_match[] = {
+	{ .compatible = "qcom,sc7280-gpi-dma", .data = (void *)0x10000 },
 	{ .compatible = "qcom,sdm845-gpi-dma", .data = (void *)0x0 },
 	{ .compatible = "qcom,sm8150-gpi-dma", .data = (void *)0x0 },
 	{ .compatible = "qcom,sm8250-gpi-dma", .data = (void *)0x0 },
