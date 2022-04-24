f7e974a31881957148110d3cc52774a4e279ef84
Jernej Skrabec <jernej.skrabec@gmail.com>
2022-04-24 11:26:24 -0500

    sun4i/drm: engine: Add mode_set callback
    This optional callback is useful for setting properties which depends
    only on current mode. Such properties are width, height and interlaced
    output.
    
    These properties are currently set in update layer callback for primary
    plane which is less than ideal. More about that in follow up patches,
    which will migrate that code to this newly defined callback.
    
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>
    Signed-off-by: Samuel Holland <samuel@sholland.org>
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220424162633.12369-7-samuel@sholland.org

--- drivers/gpu/drm/sun4i/sun4i_crtc.c
+++ drivers/gpu/drm/sun4i/sun4i_crtc.c
@@ -146,6 +146,7 @@ static void sun4i_crtc_mode_set_nofb(struct drm_crtc *crtc)
 	struct sun4i_crtc *scrtc = drm_crtc_to_sun4i_crtc(crtc);
 
 	sun4i_tcon_mode_set(scrtc->tcon, encoder, mode);
+	sunxi_engine_mode_set(scrtc->engine, mode);
 }
 
 static const struct drm_crtc_helper_funcs sun4i_crtc_helper_funcs = {
