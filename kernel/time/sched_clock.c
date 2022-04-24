92067440f1311dfa4d77b57a9da6b3706f5da32e
Maciej W. Rozycki <macro@orcam.me.uk>
2022-04-24 12:47:20 +0100

    time/sched_clock: Round the frequency reported to nearest rather than down
    The frequency reported for clock sources are rounded down, which gives
    misleading figures, e.g.:
    
     I/O ASIC clock frequency 24999480Hz
     sched_clock: 32 bits at 24MHz, resolution 40ns, wraps every 85901132779ns
     MIPS counter frequency 59998512Hz
     sched_clock: 32 bits at 59MHz, resolution 16ns, wraps every 35792281591ns
    
    Rounding to nearest is more adequate:
    
     I/O ASIC clock frequency 24999664Hz
     sched_clock: 32 bits at 25MHz, resolution 40ns, wraps every 85900499947ns
     MIPS counter frequency 59999728Hz
     sched_clock: 32 bits at 60MHz, resolution 16ns, wraps every 35791556599ns
    
    Signed-off-by: Maciej W. Rozycki <macro@orcam.me.uk>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: John Stultz <jstultz@google.com>
    Link: https://lore.kernel.org/r/alpine.DEB.2.21.2204240055590.9383@angie.orcam.me.uk

--- kernel/time/sched_clock.c
+++ kernel/time/sched_clock.c
@@ -8,6 +8,7 @@
 #include <linux/jiffies.h>
 #include <linux/ktime.h>
 #include <linux/kernel.h>
+#include <linux/math.h>
 #include <linux/moduleparam.h>
 #include <linux/sched.h>
 #include <linux/sched/clock.h>
@@ -199,11 +200,11 @@ sched_clock_register(u64 (*read)(void), int bits, unsigned long rate)
 
 	r = rate;
 	if (r >= 4000000) {
-		r /= 1000000;
+		r = DIV_ROUND_CLOSEST(r, 1000000);
 		r_unit = 'M';
 	} else {
 		if (r >= 1000) {
-			r /= 1000;
+			r = DIV_ROUND_CLOSEST(r, 1000);
 			r_unit = 'k';
 		} else {
 			r_unit = ' ';
