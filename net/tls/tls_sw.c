c706b2b5ed74d30436b85cbd8e63e969f6b5873a
Jakub Kicinski <kuba@kernel.org>
2022-04-25 16:33:09 -0700

    net: tls: fix async vs NIC crypto offload
    When NIC takes care of crypto (or the record has already
    been decrypted) we forget to update darg->async. ->async
    is supposed to mean whether record is async capable on
    input and whether record has been queued for async crypto
    on output.
    
    Reported-by: Gal Pressman <gal@nvidia.com>
    Fixes: 3547a1f9d988 ("tls: rx: use async as an in-out argument")
    Tested-by: Gal Pressman <gal@nvidia.com>
    Link: https://lore.kernel.org/r/20220425233309.344858-1-kuba@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/tls/tls_sw.c
+++ net/tls/tls_sw.c
@@ -1562,6 +1562,7 @@ static int decrypt_skb_update(struct sock *sk, struct sk_buff *skb,
 
 	if (tlm->decrypted) {
 		darg->zc = false;
+		darg->async = false;
 		return 0;
 	}
 
@@ -1572,6 +1573,7 @@ static int decrypt_skb_update(struct sock *sk, struct sk_buff *skb,
 		if (err > 0) {
 			tlm->decrypted = 1;
 			darg->zc = false;
+			darg->async = false;
 			goto decrypt_done;
 		}
 	}
