f3a13716921fcfca0506cc40a32b2352c8ba557b
Fuad Tabba <tabba@google.com>
2022-04-25 10:31:08 +0000

    ANDROID: KVM: arm64: Refactor reset_mpidr to extract its computation
    Move the computation of the mpidr to its own function in a shared
    header, as the computation will be used by hyp in protected mode.
    
    No functional change intended.
    
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Bug: 233587962
    Change-Id: I48c36ebb430c3322a6991eeb391d617903525304
    Signed-off-by: Quentin Perret <qperret@google.com>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -558,19 +558,7 @@ static void reset_actlr(struct kvm_vcpu *vcpu, const struct sys_reg_desc *r)
 
 static void reset_mpidr(struct kvm_vcpu *vcpu, const struct sys_reg_desc *r)
 {
-	u64 mpidr;
-
-	/*
-	 * Map the vcpu_id into the first three affinity level fields of
-	 * the MPIDR. We limit the number of VCPUs in level 0 due to a
-	 * limitation to 16 CPUs in that level in the ICC_SGIxR registers
-	 * of the GICv3 to be able to address each CPU directly when
-	 * sending IPIs.
-	 */
-	mpidr = (vcpu->vcpu_id & 0x0f) << MPIDR_LEVEL_SHIFT(0);
-	mpidr |= ((vcpu->vcpu_id >> 4) & 0xff) << MPIDR_LEVEL_SHIFT(1);
-	mpidr |= ((vcpu->vcpu_id >> 12) & 0xff) << MPIDR_LEVEL_SHIFT(2);
-	vcpu_write_sys_reg(vcpu, (1ULL << 31) | mpidr, MPIDR_EL1);
+	vcpu_write_sys_reg(vcpu, calculate_mpidr(vcpu), MPIDR_EL1);
 }
 
 static unsigned int pmu_visibility(const struct kvm_vcpu *vcpu,
