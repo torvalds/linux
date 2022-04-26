3d2a02ffa2cd8af72360c5ce1280f8d0bbb2cb7c
Marc Zyngier <maz@kernel.org>
2022-04-26 09:05:46 +0000

    ANDROID: KVM: arm64: Force injection of a data abort on NISV MMIO exit
    If a vcpu exits for a data abort with an invalid syndrome, the
    expectations are that userspace has a chance to save the day if
    it has requested to see such exits.
    
    However, this is completely futile in the case of a protected VM,
    as none of the state is available. In this particular case, inject
    a data abort directly into the vcpu, consistent with what userspace
    could do.
    
    This also helps with pKVM, which discards all syndrome information when
    forwarding data aborts that are not known to be MMIO.
    
    Finally, hide the RETURN_NISV_IO_ABORT_TO_USER cap from userspace on
    protected VMs, and document this tweak to the API.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Bug: 233587962
    Change-Id: Ie081cf0b2fdd1ab374d479e3e355ab3cb536c960
    Signed-off-by: Quentin Perret <qperret@google.com>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -84,9 +84,13 @@ int kvm_vm_ioctl_enable_cap(struct kvm *kvm,
 
 	switch (cap->cap) {
 	case KVM_CAP_ARM_NISV_TO_USER:
-		r = 0;
-		set_bit(KVM_ARCH_FLAG_RETURN_NISV_IO_ABORT_TO_USER,
-			&kvm->arch.flags);
+		if (kvm_vm_is_protected(kvm)) {
+			r = -EINVAL;
+		} else {
+			r = 0;
+			set_bit(KVM_ARCH_FLAG_RETURN_NISV_IO_ABORT_TO_USER,
+				&kvm->arch.flags);
+		}
 		break;
 	case KVM_CAP_ARM_MTE:
 		mutex_lock(&kvm->lock);
@@ -222,13 +226,15 @@ static int kvm_check_extension(struct kvm *kvm, long ext)
 	case KVM_CAP_IMMEDIATE_EXIT:
 	case KVM_CAP_VCPU_EVENTS:
 	case KVM_CAP_ARM_IRQ_LINE_LAYOUT_2:
-	case KVM_CAP_ARM_NISV_TO_USER:
 	case KVM_CAP_ARM_INJECT_EXT_DABT:
 	case KVM_CAP_SET_GUEST_DEBUG:
 	case KVM_CAP_VCPU_ATTRIBUTES:
 	case KVM_CAP_PTP_KVM:
 	case KVM_CAP_ARM_SYSTEM_SUSPEND:
 		r = 1;
+		break;
+	case KVM_CAP_ARM_NISV_TO_USER:
+		r = !kvm || !kvm_vm_is_protected(kvm);
 		break;
 	case KVM_CAP_SET_GUEST_DEBUG2:
 		return KVM_GUESTDBG_VALID_MASK;
