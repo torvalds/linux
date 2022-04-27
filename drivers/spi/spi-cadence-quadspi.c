f724c296f2f2cc3f9342b0fc26239635cbed856e
Ian Abbott <abbotti@mev.co.uk>
2022-04-27 16:34:46 +0100

    spi: cadence-quadspi: fix Direct Access Mode disable for SoCFPGA
    The Cadence QSPI compatible string required for the SoCFPGA platform
    changed from the default "cdns,qspi-nor" to "intel,socfpga-qspi" with
    the introduction of an additional quirk in
    commit 98d948eb8331 ("spi: cadence-quadspi: fix write completion support").
    However, that change did not preserve the previously used
    quirk for this platform.  Reinstate the `CQSPI_DISABLE_DAC_MODE` quirk
    for the SoCFPGA platform.
    
    Fixes: 98d948eb8331 ("spi: cadence-quadspi: fix write completion support")
    Cc: Dinh Nguyen <dinguyen@kernel.org>
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Link: https://lore.kernel.org/r/20220427153446.10113-1-abbotti@mev.co.uk
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-cadence-quadspi.c
+++ drivers/spi/spi-cadence-quadspi.c
@@ -1865,7 +1865,7 @@ static const struct cqspi_driver_platdata intel_lgm_qspi = {
 };
 
 static const struct cqspi_driver_platdata socfpga_qspi = {
-	.quirks = CQSPI_NO_SUPPORT_WR_COMPLETION,
+	.quirks = CQSPI_DISABLE_DAC_MODE | CQSPI_NO_SUPPORT_WR_COMPLETION,
 };
 
 static const struct cqspi_driver_platdata versal_ospi = {
