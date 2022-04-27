9fe1bb29ea0ab231aa916dad4bcf0c435beb5869
Hans de Goede <hdegoede@redhat.com>
2022-04-27 13:49:56 +0200

    platform/x86: asus-wmi: Fix driver not binding when fan curve control probe fails
    Before this commit fan_curve_check_present() was trying to not cause
    the probe to fail on devices without fan curve control by testing for
    known error codes returned by asus_wmi_evaluate_method_buf().
    
    Checking for ENODATA or ENODEV, with the latter being returned by this
    function when an ACPI integer with a value of ASUS_WMI_UNSUPPORTED_METHOD
    is returned. But for other ACPI integer returns this function just returns
    them as is, including the ASUS_WMI_DSTS_UNKNOWN_BIT value of 2.
    
    On the Asus U36SD ASUS_WMI_DSTS_UNKNOWN_BIT gets returned, leading to:
    
      asus-nb-wmi: probe of asus-nb-wmi failed with error 2
    
    Instead of playing whack a mole with error codes here, simply treat all
    errors as there not being any fan curves, fixing the driver no longer
    loading on the Asus U36SD laptop.
    
    Fixes: e3d13da7f77d ("platform/x86: asus-wmi: Fix regression when probing for fan curve control")
    BugLink: https://bugzilla.redhat.com/show_bug.cgi?id=2079125
    Cc: Luke D. Jones <luke@ljones.dev>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://lore.kernel.org/r/20220427114956.332919-1-hdegoede@redhat.com

--- drivers/platform/x86/asus-wmi.c
+++ drivers/platform/x86/asus-wmi.c
@@ -2227,9 +2227,10 @@ static int fan_curve_check_present(struct asus_wmi *asus, bool *available,
 
 	err = fan_curve_get_factory_default(asus, fan_dev);
 	if (err) {
-		if (err == -ENODEV || err == -ENODATA)
-			return 0;
-		return err;
+		pr_debug("fan_curve_get_factory_default(0x%08x) failed: %d\n",
+			 fan_dev, err);
+		/* Don't cause probe to fail on devices without fan-curves */
+		return 0;
 	}
 
 	*available = true;
