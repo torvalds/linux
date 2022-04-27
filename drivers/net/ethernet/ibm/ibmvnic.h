8de15dad3c77c6f17302f91855bd6dc0f80aa113
Dany Madden <drt@linux.ibm.com>
2022-04-27 18:51:46 -0500

    Revert "ibmvnic: Add ethtool private flag for driver-defined queue limits"
    [ Upstream commit aeaf59b78712c7a1827c76f086acff4f586e072f ]
    
    This reverts commit 723ad916134784b317b72f3f6cf0f7ba774e5dae
    
    When client requests channel or ring size larger than what the server
    can support the server will cap the request to the supported max. So,
    the client would not be able to successfully request resources that
    exceed the server limit.
    
    Fixes: 723ad9161347 ("ibmvnic: Add ethtool private flag for driver-defined queue limits")
    Signed-off-by: Dany Madden <drt@linux.ibm.com>
    Link: https://lore.kernel.org/r/20220427235146.23189-1-drt@linux.ibm.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/ibm/ibmvnic.h
+++ drivers/net/ethernet/ibm/ibmvnic.h
@@ -43,11 +43,6 @@
 
 #define IBMVNIC_RESET_DELAY 100
 
-static const char ibmvnic_priv_flags[][ETH_GSTRING_LEN] = {
-#define IBMVNIC_USE_SERVER_MAXES 0x1
-	"use-server-maxes"
-};
-
 struct ibmvnic_login_buffer {
 	__be32 len;
 	__be32 version;
@@ -885,7 +880,6 @@ struct ibmvnic_adapter {
 	struct ibmvnic_control_ip_offload_buffer ip_offload_ctrl;
 	dma_addr_t ip_offload_ctrl_tok;
 	u32 msg_enable;
-	u32 priv_flags;
 
 	/* Vital Product Data (VPD) */
 	struct ibmvnic_vpd *vpd;
