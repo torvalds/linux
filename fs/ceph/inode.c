55ab5520802016b13098e0ea3794480289659aab
Luís Henriques <lhenriques@suse.de>
2022-04-27 16:57:04 +0100

    ceph: fix statfs for subdir mounts
    When doing a mount using as base a directory that has 'max_bytes' quotas
    statfs uses that value as the total; if a subdirectory is used instead,
    the same 'max_bytes' too in statfs, unless there is another quota set.
    
    Unfortunately, if this subdirectory only has the 'max_files' quota set,
    then statfs uses the filesystem total.  Fix this by making sure we only
    lookup realms that contain the 'max_bytes' quota.
    
    Cc: Ryan Taylor <rptaylor@uvic.ca>
    URL: https://tracker.ceph.com/issues/55090
    Signed-off-by: Luís Henriques <lhenriques@suse.de>
    Reviewed-by: Jeff Layton <jlayton@kernel.org>
    Reviewed-by: Xiubo Li <xiubli@redhat.com>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- fs/ceph/inode.c
+++ fs/ceph/inode.c
@@ -578,7 +578,7 @@ void ceph_evict_inode(struct inode *inode)
 
 	__ceph_remove_caps(ci);
 
-	if (__ceph_has_any_quota(ci))
+	if (__ceph_has_quota(ci, QUOTA_GET_ANY))
 		ceph_adjust_quota_realms_count(inode, false);
 
 	/*
