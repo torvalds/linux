e2adc33a171db2784b7369ce4ba05c6ba21684c8
Mario Tesi <mario.tesi@st.com>
2022-04-27 15:06:14 +0200

    drivers:iio:stm:accel:Add support to LIS2HH12 STMEMS accel
    Added support to LIS2HH12 STMEMS accel.
    This device was missing from the list of supported devices for
    kernel 5.4 despite being supported in older version kernels.
    This commit aligns device supported to previous kernel versions.
    
    Signed-off-by: Mario Tesi <mario.tesi@st.com>
    Change-Id: I3508361e425a74a8f3fa6073feeae82b5c0dadaf
    Reviewed-on: https://sczcxd1104.scz.st.com/gerrit/c/linux/stm-ldd-iio/+/629
    Tested-by: CI STM MSD <aosp-ger@st.com>
    Reviewed-by: Denis CIOCCA <denis.ciocca@st.com>

---
+++ drivers/iio/stm/accel/st_lis2hh12_i2c.c
@@ -0,0 +1,155 @@
+/*
+ * STMicroelectronics lis2hh12 driver
+ *
+ * Copyright 2016 STMicroelectronics Inc.
+ *
+ * Armando Visconti <armando.visconti@st.com>
+ *
+ * Licensed under the GPL-2.
+ */
+
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/i2c.h>
+#include <linux/types.h>
+
+#include "st_lis2hh12.h"
+
+static int lis2hh12_i2c_read(struct lis2hh12_data *cdata, u8 reg_addr, int len,
+								u8 * data)
+{
+	struct i2c_msg msg[2];
+	struct i2c_client *client = to_i2c_client(cdata->dev);
+
+	msg[0].addr = client->addr;
+	msg[0].flags = client->flags;
+	msg[0].len = 1;
+	msg[0].buf = &reg_addr;
+
+	msg[1].addr = client->addr;
+	msg[1].flags = client->flags | I2C_M_RD;
+	msg[1].len = len;
+	msg[1].buf = data;
+
+	return(i2c_transfer(client->adapter, msg, 2));
+}
+
+static int lis2hh12_i2c_write(struct lis2hh12_data *cdata, u8 reg_addr, int len,
+								u8 * data)
+{
+	u8 send[len + 1];
+	struct i2c_msg msg;
+	struct i2c_client *client = to_i2c_client(cdata->dev);
+
+	send[0] = reg_addr;
+	memcpy(&send[1], data, len * sizeof(u8));
+	len++;
+
+	msg.addr = client->addr;
+	msg.flags = client->flags;
+	msg.len = len;
+	msg.buf = send;
+
+	return(i2c_transfer(client->adapter, &msg, 1));
+}
+
+static const struct lis2hh12_transfer_function lis2hh12_tf_i2c = {
+	.write = lis2hh12_i2c_write,
+	.read = lis2hh12_i2c_read,
+};
+
+static int lis2hh12_i2c_probe(struct i2c_client *client,
+			    const struct i2c_device_id *id)
+{
+	int err;
+	struct lis2hh12_data *cdata;
+
+	cdata = kmalloc(sizeof(*cdata), GFP_KERNEL);
+	if (!cdata)
+		return -ENOMEM;
+
+	cdata->dev = &client->dev;
+	cdata->name = client->name;
+	cdata->tf = &lis2hh12_tf_i2c;
+	i2c_set_clientdata(client, cdata);
+
+	err = lis2hh12_common_probe(cdata, client->irq);
+	if (err < 0)
+		goto free_data;
+
+	return 0;
+
+free_data:
+	kfree(cdata);
+	return err;
+}
+
+static int lis2hh12_i2c_remove(struct i2c_client *client)
+{
+	struct lis2hh12_data *cdata = i2c_get_clientdata(client);
+
+	lis2hh12_common_remove(cdata, client->irq);
+	dev_info(cdata->dev, "%s: removed\n", LIS2HH12_DEV_NAME);
+	kfree(cdata);
+	return 0;
+}
+
+#ifdef CONFIG_PM
+static int lis2hh12_suspend(struct device *dev)
+{
+	struct lis2hh12_data *cdata = i2c_get_clientdata(to_i2c_client(dev));
+
+	return lis2hh12_common_suspend(cdata);
+}
+
+static int lis2hh12_resume(struct device *dev)
+{
+	struct lis2hh12_data *cdata = i2c_get_clientdata(to_i2c_client(dev));
+
+	return lis2hh12_common_resume(cdata);
+}
+
+static const struct dev_pm_ops lis2hh12_pm_ops = {
+	SET_SYSTEM_SLEEP_PM_OPS(lis2hh12_suspend, lis2hh12_resume)
+};
+
+#define LIS2HH12_PM_OPS		(&lis2hh12_pm_ops)
+#else /* CONFIG_PM */
+#define LIS2HH12_PM_OPS		NULL
+#endif /* CONFIG_PM */
+
+static const struct i2c_device_id lis2hh12_ids[] = {
+	{"lis2hh12", 0},
+	{}
+};
+
+MODULE_DEVICE_TABLE(i2c, lis2hh12_ids);
+
+#ifdef CONFIG_OF
+static const struct of_device_id lis2hh12_id_table[] = {
+	{.compatible = "st,lis2hh12",},
+	{},
+};
+
+MODULE_DEVICE_TABLE(of, lis2hh12_id_table);
+#endif /* CONFIG_OF */
+
+static struct i2c_driver lis2hh12_i2c_driver = {
+	.driver = {
+		   .owner = THIS_MODULE,
+		   .name = LIS2HH12_DEV_NAME,
+		   .pm = LIS2HH12_PM_OPS,
+#ifdef CONFIG_OF
+		   .of_match_table = lis2hh12_id_table,
+#endif /* CONFIG_OF */
+		   },
+	.probe = lis2hh12_i2c_probe,
+	.remove = lis2hh12_i2c_remove,
+	.id_table = lis2hh12_ids,
+};
+
+module_i2c_driver(lis2hh12_i2c_driver);
+
+MODULE_DESCRIPTION("STMicroelectronics lis2hh12 i2c driver");
+MODULE_AUTHOR("Armando Visconti <armando.visconti@st.com>");
+MODULE_LICENSE("GPL v2");
