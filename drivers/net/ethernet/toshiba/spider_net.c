26450aa7ca4266444e89d065766f8d8b98e277ff
Jakub Kicinski <kuba@kernel.org>
2022-04-28 14:23:21 -0700

    eth: spider: remove a copy of the NAPI_POLL_WEIGHT define
    Defining local versions of NAPI_POLL_WEIGHT with the same
    values in the drivers just makes refactoring harder.
    
    Acked-by: Geoff Levand <geoff@infradead.org>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/toshiba/spider_net.c
+++ drivers/net/ethernet/toshiba/spider_net.c
@@ -2270,7 +2270,7 @@ spider_net_setup_netdev(struct spider_net_card *card)
 	timer_setup(&card->aneg_timer, spider_net_link_phy, 0);
 
 	netif_napi_add(netdev, &card->napi,
-		       spider_net_poll, SPIDER_NET_NAPI_WEIGHT);
+		       spider_net_poll, NAPI_POLL_WEIGHT);
 
 	spider_net_setup_netdev_ops(netdev);
 
