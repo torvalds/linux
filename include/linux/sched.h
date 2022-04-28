50e7b416d2ab10b9771bd00a4d85df90ad2e4b37
Dietmar Eggemann <dietmar.eggemann@arm.com>
2022-04-28 15:43:37 +0100

    sched/fair: Remove sched_trace_*() helper functions
    We no longer need them as we can use DWARF debug info or BTF + pahole to
    re-generate the required structs to compile against them for a given
    kernel.
    
    This moves the burden of maintaining these helper functions to the
    module.
    
    	https://github.com/qais-yousef/sched_tp
    
    Note that pahole v1.15 is required at least for using DWARF. And for BTF
    v1.23 which is not yet released will be required. There's alignment
    problem that will lead to crashes in earlier versions when used with
    BTF.
    
    We should have enough infrastructure to make these helper functions now
    obsolete, so remove them.
    
    [Rewrote commit message to reflect the new alternative]
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20220428144338.479094-2-qais.yousef@arm.com

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2378,20 +2378,6 @@ static inline void rseq_syscall(struct pt_regs *regs)
 
 #endif
 
-const struct sched_avg *sched_trace_cfs_rq_avg(struct cfs_rq *cfs_rq);
-char *sched_trace_cfs_rq_path(struct cfs_rq *cfs_rq, char *str, int len);
-int sched_trace_cfs_rq_cpu(struct cfs_rq *cfs_rq);
-
-const struct sched_avg *sched_trace_rq_avg_rt(struct rq *rq);
-const struct sched_avg *sched_trace_rq_avg_dl(struct rq *rq);
-const struct sched_avg *sched_trace_rq_avg_irq(struct rq *rq);
-
-int sched_trace_rq_cpu(struct rq *rq);
-int sched_trace_rq_cpu_capacity(struct rq *rq);
-int sched_trace_rq_nr_running(struct rq *rq);
-
-const struct cpumask *sched_trace_rd_span(struct root_domain *rd);
-
 #ifdef CONFIG_SCHED_CORE
 extern void sched_core_free(struct task_struct *tsk);
 extern void sched_core_fork(struct task_struct *p);
