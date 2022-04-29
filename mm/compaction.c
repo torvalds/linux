024c61eaff17f6a8c73cb8b25137251f0c3ef039
Miaohe Lin <linmiaohe@huawei.com>
2022-04-28 23:16:17 -0700

    mm: compaction: remove unneeded return value of kcompactd_run
    Patch series "A few cleanup and fixup patches for compaction".
    
    This series contains a few patches to clean up some obsolete comment,
    remove unneeded return value and so on.  Also we fix the possible NULL
    pointer dereference.  More details can be found in the respective
    changelogs.
    
    
    This patch (of 12):
    
    The return value of kcompactd_run() is unused now.  Clean it up.
    
    Link: https://lkml.kernel.org/r/20220418141253.24298-1-linmiaohe@huawei.com
    Link: https://lkml.kernel.org/r/20220418141253.24298-2-linmiaohe@huawei.com
    Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
    Cc; Mel Gorman <mgorman@techsingularity.net>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Pintu Kumar <pintu@codeaurora.org>
    Cc: Charan Teja Kalla <charante@codeaurora.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/compaction.c
+++ mm/compaction.c
@@ -3016,21 +3016,18 @@ static int kcompactd(void *p)
  * This kcompactd start function will be called by init and node-hot-add.
  * On node-hot-add, kcompactd will moved to proper cpus if cpus are hot-added.
  */
-int kcompactd_run(int nid)
+void kcompactd_run(int nid)
 {
 	pg_data_t *pgdat = NODE_DATA(nid);
-	int ret = 0;
 
 	if (pgdat->kcompactd)
-		return 0;
+		return;
 
 	pgdat->kcompactd = kthread_run(kcompactd, pgdat, "kcompactd%d", nid);
 	if (IS_ERR(pgdat->kcompactd)) {
 		pr_err("Failed to start kcompactd on node %d\n", nid);
-		ret = PTR_ERR(pgdat->kcompactd);
 		pgdat->kcompactd = NULL;
 	}
-	return ret;
 }
 
 /*
