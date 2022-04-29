b499914eb83765a27e3b43f216e9d1bdf4265418
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2022-04-29 11:04:14 +0100

    drm/i915: Only setup private tmpfs mount when needed and fix logging
    If i915 does not want to use huge pages there is a) no point in setting up
    the private mount and b) should former fail, it is misleading to log THP
    support is disabled in the caller, which does not even know if callee
    tried to enable it.
    
    Fix both by restructuring the flow in i915_gemfs_init and at the same time
    note the failure to set it up in all cases.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Cc: Matthew Auld <matthew.auld@intel.com>
    Cc: Eero Tamminen <eero.t.tamminen@intel.com>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220429100414.647857-2-tvrtko.ursulin@linux.intel.com

--- drivers/gpu/drm/i915/gem/i915_gemfs.c
+++ drivers/gpu/drm/i915/gem/i915_gemfs.c
@@ -11,16 +11,11 @@
 #include "i915_gemfs.h"
 #include "i915_utils.h"
 
-int i915_gemfs_init(struct drm_i915_private *i915)
+void i915_gemfs_init(struct drm_i915_private *i915)
 {
 	char huge_opt[] = "huge=within_size"; /* r/w */
 	struct file_system_type *type;
 	struct vfsmount *gemfs;
-	char *opts;
-
-	type = get_fs_type("tmpfs");
-	if (!type)
-		return -ENODEV;
 
 	/*
 	 * By creating our own shmemfs mountpoint, we can pass in
@@ -34,29 +29,29 @@ int i915_gemfs_init(struct drm_i915_private *i915)
 	 * regressions such a slow reads issue on Broadwell and Skylake.
 	 */
 
-	opts = NULL;
-	if (GRAPHICS_VER(i915) >= 11 || i915_vtd_active(i915)) {
-		if (IS_ENABLED(CONFIG_TRANSPARENT_HUGEPAGE)) {
-			opts = huge_opt;
-			drm_info(&i915->drm,
-				 "Transparent Hugepage mode '%s'\n",
-				 opts);
-		} else {
-			drm_notice(&i915->drm,
-				   "Transparent Hugepage support is recommended for optimal performance%s\n",
-				   GRAPHICS_VER(i915) >= 11 ?
-				   " on this platform!" :
-				   " when IOMMU is enabled!");
-		}
-	}
+	if (GRAPHICS_VER(i915) < 11 && !i915_vtd_active(i915))
+		return;
 
-	gemfs = vfs_kern_mount(type, SB_KERNMOUNT, type->name, opts);
+	if (!IS_ENABLED(CONFIG_TRANSPARENT_HUGEPAGE))
+		goto err;
+
+	type = get_fs_type("tmpfs");
+	if (!type)
+		goto err;
+
+	gemfs = vfs_kern_mount(type, SB_KERNMOUNT, type->name, huge_opt);
 	if (IS_ERR(gemfs))
-		return PTR_ERR(gemfs);
+		goto err;
 
 	i915->mm.gemfs = gemfs;
+	drm_info(&i915->drm, "Using Transparent Hugepages\n");
+	return;
 
-	return 0;
+err:
+	drm_notice(&i915->drm,
+		   "Transparent Hugepage support is recommended for optimal performance%s\n",
+		   GRAPHICS_VER(i915) >= 11 ? " on this platform!" :
+					      " when IOMMU is enabled!");
 }
 
 void i915_gemfs_fini(struct drm_i915_private *i915)
