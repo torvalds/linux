f304d35e59958ea4be399f78e5be08d4a0c4db75
Sumit Saxena <sumit.saxena@broadcom.com>
2022-04-29 17:16:41 -0400

    scsi: mpi3mr: Update driver version to 8.0.0.69.0
    Link: https://lore.kernel.org/r/20220429211641.642010-9-sumit.saxena@broadcom.com
    Signed-off-by: Sumit Saxena <sumit.saxena@broadcom.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/mpi3mr/mpi3mr.h
+++ drivers/scsi/mpi3mr/mpi3mr.h
@@ -55,8 +55,8 @@ extern struct list_head mrioc_list;
 extern int prot_mask;
 extern atomic64_t event_counter;
 
-#define MPI3MR_DRIVER_VERSION	"8.0.0.68.0"
-#define MPI3MR_DRIVER_RELDATE	"10-February-2022"
+#define MPI3MR_DRIVER_VERSION	"8.0.0.69.0"
+#define MPI3MR_DRIVER_RELDATE	"16-March-2022"
 
 #define MPI3MR_DRIVER_NAME	"mpi3mr"
 #define MPI3MR_DRIVER_LICENSE	"GPL"
