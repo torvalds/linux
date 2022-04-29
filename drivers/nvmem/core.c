dbc2f62061c6bfba0aee93161ee3194dcee84bd0
Rafał Miłecki <rafal@milecki.pl>
2022-04-29 17:26:46 +0100

    nvmem: core: support passing DT node in cell info
    Some hardware may have NVMEM cells described in Device Tree using
    individual nodes. Let drivers pass such nodes to the NVMEM subsystem so
    they can be later used by NVMEM consumers.
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Link: https://lore.kernel.org/r/20220429162701.2222-2-srinivas.kandagatla@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/nvmem/core.c
+++ drivers/nvmem/core.c
@@ -467,6 +467,7 @@ static int nvmem_cell_info_to_nvmem_cell_entry_nodup(struct nvmem_device *nvmem,
 
 	cell->bit_offset = info->bit_offset;
 	cell->nbits = info->nbits;
+	cell->np = info->np;
 
 	if (cell->nbits)
 		cell->bytes = DIV_ROUND_UP(cell->nbits + cell->bit_offset,
