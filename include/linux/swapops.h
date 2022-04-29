ba91fb7dd03c4f463453f28af24851097a6547fb
Peter Xu <peterx@redhat.com>
2022-04-28 23:16:16 -0700

    include/linux/swapops.h: remove stub for non_swap_entry()
    The stub for non_swap_entry() may not help much, because MAX_SWAPFILES has
    already contained all the information to decide whether a swap entry is
    real swap entry or pesudo ones (migrations, ...).
    
    There can be some performance influences on non_swap_entry() with below
    conditions all met:
    
      !CONFIG_MIGRATION && !CONFIG_MEMORY_FAILURE && !CONFIG_DEVICE_PRIVATE
    
    But that's definitely not the major config most machines will use, at the
    meantime it's already in a slow path of swap entry (being parsed from a
    swap pte), so IMHO it shouldn't be a major issue.  Also according to the
    analysis from Alistair, somehow the stub didn't do the job right [1].
    
    To make the code cleaner, let's drop the stub.
    
    [1] https://lore.kernel.org/lkml/8735ihbw6g.fsf@nvdebian.thelocal/
    
    Note: the uffd-wp shmem & hugetlbfs series will need this patch to make
    sure swap entries work as expected with below config as spotted by
    Alistair:
    
      !CONFIG_MIGRATION &&
      !CONFIG_MEMORY_FAILURE &&
      !CONFIG_DEVICE_PRIVATE &&
      CONFIG_PTE_MARKER
    
    (PS: this config should mostly never gonna happen, though, afaict..)
    
    Link: https://lkml.kernel.org/r/20220413191147.66645-1-peterx@redhat.com
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Reviewed-by: Alistair Popple <apopple@nvidia.com>
    Cc: Hugh Dickins <hughd@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/swapops.h
+++ include/linux/swapops.h
@@ -387,18 +387,10 @@ static inline void num_poisoned_pages_inc(void)
 }
 #endif
 
-#if defined(CONFIG_MEMORY_FAILURE) || defined(CONFIG_MIGRATION) || \
-    defined(CONFIG_DEVICE_PRIVATE)
 static inline int non_swap_entry(swp_entry_t entry)
 {
 	return swp_type(entry) >= MAX_SWAPFILES;
 }
-#else
-static inline int non_swap_entry(swp_entry_t entry)
-{
-	return 0;
-}
-#endif
 
 #endif /* CONFIG_MMU */
 #endif /* _LINUX_SWAPOPS_H */
