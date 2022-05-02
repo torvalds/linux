aafa025c76dcc7d1a8c8f0bdefcbe4eb480b2f6a
Javier Martinez Canillas <javierm@redhat.com>
2022-05-02 15:50:14 +0200

    fbdev: Make fb_release() return -ENODEV if fbdev was unregistered
    A reference to the framebuffer device struct fb_info is stored in the file
    private data, but this reference could no longer be valid and must not be
    accessed directly. Instead, the file_fb_info() accessor function must be
    used since it does sanity checking to make sure that the fb_info is valid.
    
    This can happen for example if the registered framebuffer device is for a
    driver that just uses a framebuffer provided by the system firmware. In
    that case, the fbdev core would unregister the framebuffer device when a
    real video driver is probed and ask to remove conflicting framebuffers.
    
    The bug has been present for a long time but commit 27599aacbaef ("fbdev:
    Hot-unplug firmware fb devices on forced removal") unmasked it since the
    fbdev core started unregistering the framebuffers' devices associated.
    
    Fixes: 27599aacbaef ("fbdev: Hot-unplug firmware fb devices on forced removal")
    Reported-by: Maxime Ripard <maxime@cerno.tech>
    Reported-by: Junxiao Chang <junxiao.chang@intel.com>
    Signed-off-by: Javier Martinez Canillas <javierm@redhat.com>
    Reviewed-by: Thomas Zimmermann <tzimmermann@suse.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220502135014.377945-1-javierm@redhat.com

--- drivers/video/fbdev/core/fbmem.c
+++ drivers/video/fbdev/core/fbmem.c
@@ -1434,7 +1434,10 @@ fb_release(struct inode *inode, struct file *file)
 __acquires(&info->lock)
 __releases(&info->lock)
 {
-	struct fb_info * const info = file->private_data;
+	struct fb_info * const info = file_fb_info(file);
+
+	if (!info)
+		return -ENODEV;
 
 	lock_fb_info(info);
 	if (info->fbops->fb_release)
