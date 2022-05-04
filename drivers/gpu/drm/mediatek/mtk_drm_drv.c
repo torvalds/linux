5bb4f80716e2f0b2a059f8273acd6c69f3bce239
Rex-BC Chen <rex-bc.chen@mediatek.com>
2022-05-04 17:19:23 +0800

    drm/mediatek: Add MT8186 DSI compatible for mtk_drm_drv.c
    The compatible "mediatek,mt8186-dsi" is used by MT8186 DSI, so
    add it to mtk_ddp_comp_dt_ids in mtk_drm_drv.c.
    
    Link: https://patchwork.kernel.org/project/linux-mediatek/patch/20220504091923.2219-5-rex-bc.chen@mediatek.com/
    Signed-off-by: Rex-BC Chen <rex-bc.chen@mediatek.com>
    Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Signed-off-by: Chun-Kuang Hu <chunkuang.hu@kernel.org>

--- drivers/gpu/drm/mediatek/mtk_drm_drv.c
+++ drivers/gpu/drm/mediatek/mtk_drm_drv.c
@@ -641,6 +641,8 @@ static const struct of_device_id mtk_ddp_comp_dt_ids[] = {
 	  .data = (void *)MTK_DSI },
 	{ .compatible = "mediatek,mt8183-dsi",
 	  .data = (void *)MTK_DSI },
+	{ .compatible = "mediatek,mt8186-dsi",
+	  .data = (void *)MTK_DSI },
 	{ }
 };
 
