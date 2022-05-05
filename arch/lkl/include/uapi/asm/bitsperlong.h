0d680a3d5886e653ca3528c48171ff929e5e2a5e
Octavian Purdila <tavip@google.com>
2022-05-05 18:48:58 +0000

    lkl: remove CONFIG_ leaks in uapi headers
    Upstream has introduce checks against leaking CONFIG_ leaks in uapi
    headers. We currently have the following sources for such leaks:
    
    1. The kernel config.h we pull in syscalls.h
    2. CONFIG_UID16 we use in syscalls.h
    3. CONFIG_64BIT we use in syscalls.h and bitsperlong.h
    4. CONFIG_BIG_ENDIAN we use in byteorder.h
    
    We can remove the config.h as we only use the above mentioned other 3
    above CONFIG_. We can also remove CONFIG_UID16 and stick to providing
    uid32 APIs only (which it was the default anyways).
    
    The last two configs are not easy to deal with since we have targets
    where these are different so they need to be configurable.
    
    The approach taken for these are to create to let the host create a
    kernel config.h based on the current host target. We can then use that
    config.h to set the endianness and bits per long values, without
    relying on CONFIG_ symbols.
    
    Signed-off-by: Octavian Purdila <tavip@google.com>

--- arch/lkl/include/uapi/asm/bitsperlong.h
+++ arch/lkl/include/uapi/asm/bitsperlong.h
@@ -1,7 +1,9 @@
 #ifndef _ASM_UAPI_LKL_BITSPERLONG_H
 #define _ASM_UAPI_LKL_BITSPERLONG_H
 
-#ifdef CONFIG_64BIT
+#include <asm/config.h>
+
+#if defined(LKL_CONFIG_64BIT)
 #define __BITS_PER_LONG 64
 #else
 #define __BITS_PER_LONG 32
