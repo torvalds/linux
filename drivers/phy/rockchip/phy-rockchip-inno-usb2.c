631d008d1c6221c49dc47d36d1badc37581065b4
William Wu <william.wu@rock-chips.com>
2022-05-05 15:00:30 +0800

    phy: rockchip: inno-usb2: update phy tuning for rv1106 and rv1103
    According to the new simulation result, we need to update the
    phy configuration to cover different corner of rv1106 and rv1103.
    
    1. Always enable pre-emphasis in SOF & EOP & chirp & non-chirp state;
    2. Set Tx HS pre_emphasize strength to 3'b010;
    3. Set 45ohm HS ODT value to 5'b10111 for better Rx ODT resistance.
    
    Signed-off-by: William Wu <william.wu@rock-chips.com>
    Change-Id: I9faca9d35124122faf5a35c78f9ee13fd9c24bba

--- drivers/phy/rockchip/phy-rockchip-inno-usb2.c
+++ drivers/phy/rockchip/phy-rockchip-inno-usb2.c
@@ -2581,11 +2581,11 @@ static int rk3568_usb2phy_tuning(struct rockchip_usb2phy *rphy)
 
 static int rv1106_usb2phy_tuning(struct rockchip_usb2phy *rphy)
 {
-	/* Enable pre-emphasis during SOF and EOP, non-chirp state */
-	phy_update_bits(rphy->phy_base + 0x30, GENMASK(2, 0), 0x05);
+	/* Always enable pre-emphasis in SOF & EOP & chirp & non-chirp state */
+	phy_update_bits(rphy->phy_base + 0x30, GENMASK(2, 0), 0x07);
 
-	/* Set Tx HS pre_emphasize strength to 3'b011 */
-	phy_update_bits(rphy->phy_base + 0x40, GENMASK(5, 3), (0x03 << 3));
+	/* Set Tx HS pre_emphasize strength to 3'b010 */
+	phy_update_bits(rphy->phy_base + 0x40, GENMASK(5, 3), (0x02 << 3));
 
 	/* Set RX Squelch trigger point configure to 4'b0000(112.5 mV) */
 	phy_update_bits(rphy->phy_base + 0x64, GENMASK(6, 3), (0x00 << 3));
@@ -2593,10 +2593,10 @@ static int rv1106_usb2phy_tuning(struct rockchip_usb2phy *rphy)
 	/* Turn off differential receiver by default to save power */
 	phy_clear_bits(rphy->phy_base + 0x100, BIT(6));
 
-	/* Set 45ohm HS ODT value to 5'b11101 to increase driver strength */
-	phy_update_bits(rphy->phy_base + 0x11c, GENMASK(4, 0), 0x1d);
+	/* Set 45ohm HS ODT value to 5'b10111 to increase driver strength */
+	phy_update_bits(rphy->phy_base + 0x11c, GENMASK(4, 0), 0x17);
 
-	/* Set Tx HS eye height tuning to 3'b011(437.5 mV)*/
+	/* Set Tx HS eye height tuning to 3'b011(462 mV)*/
 	phy_update_bits(rphy->phy_base + 0x124, GENMASK(4, 2), (0x03 << 2));
 
 	return 0;
