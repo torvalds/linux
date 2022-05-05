a7da2a864a4fa63fe39c808eec8abfb76abf1dbd
Louis Peens <louis.peens@corigine.com>
2022-05-05 14:43:48 +0900

    nfp: flower: enable decap_v2 bit
    Finally enable the decap_v2 feature bit now that all the
    other bits are in place to configure it correctly.
    
    Signed-off-by: Louis Peens <louis.peens@corigine.com>
    Signed-off-by: Yinjun Zhang <yinjun.zhang@corigine.com>
    Signed-off-by: Simon Horman <simon.horman@corigine.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/netronome/nfp/flower/main.h
+++ drivers/net/ethernet/netronome/nfp/flower/main.h
@@ -68,7 +68,8 @@ struct nfp_app;
 	NFP_FL_FEATS_IPV6_TUN | \
 	NFP_FL_FEATS_VLAN_QINQ | \
 	NFP_FL_FEATS_QOS_PPS | \
-	NFP_FL_FEATS_QOS_METER)
+	NFP_FL_FEATS_QOS_METER | \
+	NFP_FL_FEATS_DECAP_V2)
 
 struct nfp_fl_mask_id {
 	struct circ_buf mask_id_free_list;
