fcb9e738667c874cbff0b240b96a895901f09ee0
James Smart <jsmart2021@gmail.com>
2022-05-05 20:55:19 -0700

    scsi: lpfc: Update lpfc version to 14.2.0.3
    Update lpfc version to 14.2.0.3
    
    Link: https://lore.kernel.org/r/20220506035519.50908-13-jsmart2021@gmail.com
    Co-developed-by: Justin Tee <justin.tee@broadcom.com>
    Signed-off-by: Justin Tee <justin.tee@broadcom.com>
    Signed-off-by: James Smart <jsmart2021@gmail.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/lpfc/lpfc_version.h
+++ drivers/scsi/lpfc/lpfc_version.h
@@ -20,7 +20,7 @@
  * included with this package.                                     *
  *******************************************************************/
 
-#define LPFC_DRIVER_VERSION "14.2.0.2"
+#define LPFC_DRIVER_VERSION "14.2.0.3"
 #define LPFC_DRIVER_NAME		"lpfc"
 
 /* Used for SLI 2/3 */
