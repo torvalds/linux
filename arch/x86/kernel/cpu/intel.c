d3287fb0d3c8afdfd4870a6cd4a852abc9008b3b
Jithu Joseph <jithu.joseph@intel.com>
2022-05-06 15:53:59 -0700

    x86/microcode/intel: Expose collect_cpu_info_early() for IFS
    IFS is a CPU feature that allows a binary blob, similar to microcode,
    to be loaded and consumed to perform low level validation of CPU
    circuitry. In fact, it carries the same Processor Signature
    (family/model/stepping) details that are contained in Intel microcode
    blobs.
    
    In support of an IFS driver to trigger loading, validation, and running
    of these tests blobs, make the functionality of cpu_signatures_match()
    and collect_cpu_info_early() available outside of the microcode driver.
    
    Add an "intel_" prefix and drop the "_early" suffix from
    collect_cpu_info_early() and EXPORT_SYMBOL_GPL() it. Add
    declaration to x86 <asm/cpu.h>
    
    Make cpu_signatures_match() an inline function in x86 <asm/cpu.h>,
    and also give it an "intel_" prefix.
    
    No functional change intended.
    
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    Co-developed-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://lore.kernel.org/r/20220506225410.1652287-2-tony.luck@intel.com
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- arch/x86/kernel/cpu/intel.c
+++ arch/x86/kernel/cpu/intel.c
@@ -181,6 +181,38 @@ static bool bad_spectre_microcode(struct cpuinfo_x86 *c)
 	return false;
 }
 
+int intel_cpu_collect_info(struct ucode_cpu_info *uci)
+{
+	unsigned int val[2];
+	unsigned int family, model;
+	struct cpu_signature csig = { 0 };
+	unsigned int eax, ebx, ecx, edx;
+
+	memset(uci, 0, sizeof(*uci));
+
+	eax = 0x00000001;
+	ecx = 0;
+	native_cpuid(&eax, &ebx, &ecx, &edx);
+	csig.sig = eax;
+
+	family = x86_family(eax);
+	model  = x86_model(eax);
+
+	if (model >= 5 || family > 6) {
+		/* get processor flags from MSR 0x17 */
+		native_rdmsr(MSR_IA32_PLATFORM_ID, val[0], val[1]);
+		csig.pf = 1 << ((val[1] >> 18) & 7);
+	}
+
+	csig.rev = intel_get_microcode_revision();
+
+	uci->cpu_sig = csig;
+	uci->valid = 1;
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(intel_cpu_collect_info);
+
 static void early_init_intel(struct cpuinfo_x86 *c)
 {
 	u64 misc_enable;
