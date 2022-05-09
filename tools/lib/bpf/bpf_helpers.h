8e2f618e8be66f74e6e088281ca72deb5c87cf04
Andrii Nakryiko <andrii@kernel.org>
2022-05-08 17:41:41 -0700

    libbpf: Make __kptr and __kptr_ref unconditionally use btf_type_tag() attr
    It will be annoying and surprising for users of __kptr and __kptr_ref if
    libbpf silently ignores them just because Clang used for compilation
    didn't support btf_type_tag(). It's much better to get clear compiler
    error than debug BPF verifier failures later on.
    
    Fixes: ef89654f2bc7 ("libbpf: Add kptr type tag macros to bpf_helpers.h")
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/bpf/20220509004148.1801791-3-andrii@kernel.org

--- tools/lib/bpf/bpf_helpers.h
+++ tools/lib/bpf/bpf_helpers.h
@@ -149,13 +149,8 @@ enum libbpf_tristate {
 
 #define __kconfig __attribute__((section(".kconfig")))
 #define __ksym __attribute__((section(".ksyms")))
-#if __has_attribute(btf_type_tag)
 #define __kptr __attribute__((btf_type_tag("kptr")))
 #define __kptr_ref __attribute__((btf_type_tag("kptr_ref")))
-#else
-#define __kptr
-#define __kptr_ref
-#endif
 
 #ifndef ___bpf_concat
 #define ___bpf_concat(a, b) a ## b
