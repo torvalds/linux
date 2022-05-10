84db2ee2370332240a5e562779862b564fb9d49a
Piyush Mehta <piyush.mehta@xilinx.com>
2022-05-10 18:52:52 +0530

    usb: host: ehci-xilinx: Fix quoted string split across lines
    This patch resolves checkpatch warnings for xilinx EHCI driver.
    
    Addressed warnings:
    
    WARNING: quoted string split across lines
    50: FILE: drivers/usb/host/ehci-xilinx-of.c:50:
    +			"The USB host controller does not support full speed "
    +			"nor low speed devices\n");
    
    WARNING: quoted string split across lines
    53: FILE: drivers/usb/host/ehci-xilinx-of.c:53:
    +			"You can reconfigure the host controller to have "
    +			"full speed support\n");
    
    Signed-off-by: Piyush Mehta <piyush.mehta@xilinx.com>
    Link: https://lore.kernel.org/r/20220510132252.26001-1-piyush.mehta@xilinx.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/ehci-xilinx-of.c
+++ drivers/usb/host/ehci-xilinx-of.c
@@ -46,11 +46,9 @@ static int ehci_xilinx_port_handed_over(struct usb_hcd *hcd, int portnum)
 		dev_warn(hcd->self.controller,
 			"Maybe your device is not a high speed device?\n");
 		dev_warn(hcd->self.controller,
-			"The USB host controller does not support full speed "
-			"nor low speed devices\n");
+			"The USB host controller does not support full speed nor low speed devices\n");
 		dev_warn(hcd->self.controller,
-			"You can reconfigure the host controller to have "
-			"full speed support\n");
+			"You can reconfigure the host controller to have full speed support\n");
 	}
 
 	return 0;
