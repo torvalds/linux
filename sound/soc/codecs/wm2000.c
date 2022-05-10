69b53a097d9984d93448b021e0d422f2803310bd
Charles Keepax <ckeepax@opensource.cirrus.com>
2022-05-10 16:38:42 +0100

    ASoC: wm2000: Remove redundant endianness flag
    The endianness flag is used on the CODEC side to specify an
    ambivalence to endian, typically because it is lost over the hardware
    link. This device has no DAI links and as such the flag would have
    no effect, remove the redundant flag.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>
    Link: https://lore.kernel.org/r/20220510153843.1029540-1-ckeepax@opensource.cirrus.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/wm2000.c
+++ sound/soc/codecs/wm2000.c
@@ -803,7 +803,6 @@ static const struct snd_soc_component_driver soc_component_dev_wm2000 = {
 	.num_dapm_routes	= ARRAY_SIZE(wm2000_audio_map),
 	.idle_bias_on		= 1,
 	.use_pmdown_time	= 1,
-	.endianness		= 1,
 	.non_legacy_dai_naming	= 1,
 };
 
