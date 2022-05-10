9f88361273082825d9f0d13a543d49f9fa0d44a8
Dmitrii Dolgov <9erthalion6@gmail.com>
2022-05-10 17:52:30 +0200

    bpf: Add bpf_link iterator
    Implement bpf_link iterator to traverse links via bpf_seq_file
    operations. The changeset is mostly shamelessly copied from
    commit a228a64fc1e4 ("bpf: Add bpf_prog iterator")
    
    Signed-off-by: Dmitrii Dolgov <9erthalion6@gmail.com>
    Acked-by: Yonghong Song <yhs@fb.com>
    Link: https://lore.kernel.org/r/20220510155233.9815-2-9erthalion6@gmail.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -1544,6 +1544,7 @@ void bpf_link_put(struct bpf_link *link);
 int bpf_link_new_fd(struct bpf_link *link);
 struct file *bpf_link_new_file(struct bpf_link *link, int *reserved_fd);
 struct bpf_link *bpf_link_get_from_fd(u32 ufd);
+struct bpf_link *bpf_link_get_curr_or_next(u32 *id);
 
 int bpf_obj_pin_user(u32 ufd, const char __user *pathname);
 int bpf_obj_get_user(const char __user *pathname, int flags);
