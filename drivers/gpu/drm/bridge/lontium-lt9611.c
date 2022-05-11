1ff72d434b40d221e2583fab5e1203031d05c059
John Stultz <jstultz@google.com>
2022-05-11 01:26:11 +0000

    drm/bridge: lt9611: Consolidate detection logic
    This patch simply consolidates the duplicated detection
    functionality in the driver.
    
    Cc: Yongqin Liu <yongqin.liu@linaro.org>
    Cc: Amit Pundir <amit.pundir@linaro.org>
    Cc: Peter Collingbourne <pcc@google.com>
    Cc: Vinod Koul <vkoul@kernel.org>
    Cc: Bjorn Andersson <bjorn.andersson@linaro.org>
    Cc: Robert Foss <robert.foss@linaro.org>
    Cc: kernel-team@android.com
    Reviewed-by: Robert Foss <robert.foss@linaro.org>
    Signed-off-by: John Stultz <jstultz@google.com>
    Reviewed-by: Robert Foss <robert.foss@linaro.org>
    Signed-off-by: Robert Foss <robert.foss@linaro.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220511012612.3297577-1-jstultz@google.com

--- drivers/gpu/drm/bridge/lontium-lt9611.c
+++ drivers/gpu/drm/bridge/lontium-lt9611.c
@@ -578,10 +578,8 @@ static struct lt9611_mode *lt9611_find_mode(const struct drm_display_mode *mode)
 }
 
 /* connector funcs */
-static enum drm_connector_status
-lt9611_connector_detect(struct drm_connector *connector, bool force)
+static enum drm_connector_status __lt9611_detect(struct lt9611 *lt9611)
 {
-	struct lt9611 *lt9611 = connector_to_lt9611(connector);
 	unsigned int reg_val = 0;
 	int connected = 0;
 
@@ -592,6 +590,12 @@ lt9611_connector_detect(struct drm_connector *connector, bool force)
 				connector_status_disconnected;
 
 	return lt9611->status;
+}
+
+static enum drm_connector_status
+lt9611_connector_detect(struct drm_connector *connector, bool force)
+{
+	return __lt9611_detect(connector_to_lt9611(connector));
 }
 
 static int lt9611_read_edid(struct lt9611 *lt9611)
@@ -893,17 +897,7 @@ static void lt9611_bridge_mode_set(struct drm_bridge *bridge,
 
 static enum drm_connector_status lt9611_bridge_detect(struct drm_bridge *bridge)
 {
-	struct lt9611 *lt9611 = bridge_to_lt9611(bridge);
-	unsigned int reg_val = 0;
-	int connected;
-
-	regmap_read(lt9611->regmap, 0x825e, &reg_val);
-	connected  = reg_val & BIT(0);
-
-	lt9611->status = connected ?  connector_status_connected :
-				connector_status_disconnected;
-
-	return lt9611->status;
+	return __lt9611_detect(bridge_to_lt9611(bridge));
 }
 
 static struct edid *lt9611_bridge_get_edid(struct drm_bridge *bridge,
