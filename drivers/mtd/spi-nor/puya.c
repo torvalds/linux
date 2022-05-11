d462575667051045b67c21e0f6cec68080706451
Jon Lin <jon.lin@rock-chips.com>
2022-05-11 11:19:49 +0800

    mtd: spi-nor: puya: Support new devices
    P25Q64H, P25Q128H
    
    Change-Id: I7871876bd17f5700310c429d3f4eaf75271a3d1e
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/spi-nor/puya.c
+++ drivers/mtd/spi-nor/puya.c
@@ -10,6 +10,10 @@
 static const struct flash_info puya_parts[] = {
 	{ "PY25Q128HA", INFO(0x852018, 0, 64 * 1024, 256,
 			     SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	{ "P25Q64H", INFO(0x856017, 0, 64 * 1024, 128,
+			  SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	{ "P25Q128H", INFO(0x856018, 0, 64 * 1024, 256,
+			   SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 };
 
 const struct spi_nor_manufacturer spi_nor_puya = {
