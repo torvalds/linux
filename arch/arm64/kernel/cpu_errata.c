149a25b82ca991c65d989a0148c3ab5d765e5272
Shreyas K K <quic_shrekk@quicinc.com>
2022-05-12 16:31:34 +0530

    arm64: Enable repeat tlbi workaround on KRYO4XX gold CPUs
    [ Upstream commit 51f559d66527e238f9a5f82027bff499784d4eac ]
    
    Add KRYO4XX gold/big cores to the list of CPUs that need the
    repeat TLBI workaround. Apply this to the affected
    KRYO4XX cores (rcpe to rfpe).
    
    The variant and revision bits are implementation defined and are
    different from the their Cortex CPU counterparts on which they are
    based on, i.e., (r0p0 to r3p0) is equivalent to (rcpe to rfpe).
    
    Signed-off-by: Shreyas K K <quic_shrekk@quicinc.com>
    Reviewed-by: Sai Prakash Ranjan <quic_saipraka@quicinc.com>
    Link: https://lore.kernel.org/r/20220512110134.12179-1-quic_shrekk@quicinc.com
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/arm64/kernel/cpu_errata.c
+++ arch/arm64/kernel/cpu_errata.c
@@ -208,6 +208,8 @@ static const struct arm64_cpu_capabilities arm64_repeat_tlbi_list[] = {
 #ifdef CONFIG_ARM64_ERRATUM_1286807
 	{
 		ERRATA_MIDR_RANGE(MIDR_CORTEX_A76, 0, 0, 3, 0),
+		/* Kryo4xx Gold (rcpe to rfpe) => (r0p0 to r3p0) */
+		ERRATA_MIDR_RANGE(MIDR_QCOM_KRYO_4XX_GOLD, 0xc, 0xe, 0xf, 0xe),
 	},
 #endif
 	{},
