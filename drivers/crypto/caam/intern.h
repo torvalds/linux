7a0e7d5265f58eab5983f6560817d4fe9943743b
Ahmad Fatoum <a.fatoum@pengutronix.de>
2022-05-13 16:57:01 +0200

    crypto: caam - determine whether CAAM supports blob encap/decap
    Depending on SoC variant, a CAAM may be available, but with some futures
    fused out. The LS1028A (non-E) SoC is one such SoC and while it
    indicates BLOB support, BLOB operations will ultimately fail, because
    there is no AES support. Add a new blob_present member to reflect
    whether both BLOB support and the AES support it depends on is
    available.
    
    These will be used in a follow-up commit to allow blob driver
    initialization to error out on SoCs without the necessary hardware
    support instead of failing at runtime with a cryptic
    
      caam_jr 8020000.jr: 20000b0f: CCB: desc idx 11: : Invalid CHA selected.
    
    Co-developed-by: Michael Walle <michael@walle.cc>
    Signed-off-by: Michael Walle <michael@walle.cc>
    Tested-by: Michael Walle <michael@walle.cc> # on ls1028a (non-E and E)
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Reviewed-by: Pankaj Gupta <pankaj.gupta@nxp.com>
    Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>

--- drivers/crypto/caam/intern.h
+++ drivers/crypto/caam/intern.h
@@ -92,6 +92,7 @@ struct caam_drv_private {
 	 */
 	u8 total_jobrs;		/* Total Job Rings in device */
 	u8 qi_present;		/* Nonzero if QI present in device */
+	u8 blob_present;	/* Nonzero if BLOB support present in device */
 	u8 mc_en;		/* Nonzero if MC f/w is active */
 	int secvio_irq;		/* Security violation interrupt number */
 	int virt_en;		/* Virtualization enabled in CAAM */
