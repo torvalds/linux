d91835655cc9047f8b5c40530791bd3bc020f1bd
YueHaibing <yuehaibing@huawei.com>
2022-05-14 10:38:06 +0800

    ASoC: amd: vangogh: Remove duplicate include files
    Remove duplicated includes.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Link: https://lore.kernel.org/r/20220514023806.34768-1-yuehaibing@huawei.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/vangogh/acp5x-mach.c
+++ sound/soc/amd/vangogh/acp5x-mach.c
@@ -17,10 +17,8 @@
 #include <linux/clk.h>
 #include <linux/gpio.h>
 #include <linux/gpio/consumer.h>
-#include <linux/module.h>
 #include <linux/i2c.h>
 #include <linux/input.h>
-#include <linux/io.h>
 #include <linux/acpi.h>
 #include <linux/dmi.h>
 
