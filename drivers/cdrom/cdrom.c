03fea699b050805ad6ee111f9db04f223f3e835e
Paul Gortmaker <paul.gortmaker@windriver.com>
2022-05-15 21:58:30 +0100

    cdrom: remove the unused driver specific disc change ioctl
    This was only used by the ide-cd driver, which went away in
    commit b7fb14d3ac63 ("ide: remove the legacy ide driver")
    so we might as well take advantage of that and get rid of
    this hook as well.
    
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Phillip Potter <phil@philpotter.co.uk>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Link: https://lore.kernel.org/all/20220427132436.12795-2-paul.gortmaker@windriver.com
    Signed-off-by: Phillip Potter <phil@philpotter.co.uk>
    Link: https://lore.kernel.org/r/20220515205833.944139-3-phil@philpotter.co.uk
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/cdrom/cdrom.c
+++ drivers/cdrom/cdrom.c
@@ -2452,14 +2452,6 @@ static int cdrom_ioctl_select_disc(struct cdrom_device_info *cdi,
 			return -EINVAL;
 	}
 
-	/*
-	 * ->select_disc is a hook to allow a driver-specific way of
-	 * seleting disc.  However, since there is no equivalent hook for
-	 * cdrom_slot_status this may not actually be useful...
-	 */
-	if (cdi->ops->select_disc)
-		return cdi->ops->select_disc(cdi, arg);
-
 	cd_dbg(CD_CHANGER, "Using generic cdrom_select_disc()\n");
 	return cdrom_select_disc(cdi, arg);
 }
