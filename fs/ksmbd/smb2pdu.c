3c8cfcaa2d9a2424005261bbcb5283831c38a05a
Namjae Jeon <linkinjeon@kernel.org>
2022-05-16 16:22:09 +0900

    ksmbd: handle smb2 query dir request for OutputBufferLength that is too small
    [ Upstream commit 65ca7a3ffff811d6c0d4342d467c381257d566d4 ]
    
    We found the issue that ksmbd return STATUS_NO_MORE_FILES response
    even though there are still dentries that needs to be read while
    file read/write test using framtest utils.
    windows client send smb2 query dir request included
    OutputBufferLength(128) that is too small to contain even one entry.
    This patch make ksmbd immediately returns OutputBufferLength of response
    as zero to client.
    
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>
    Reviewed-by: Hyunchul Lee <hyc.lee@gmail.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Stable-dep-of: 88541cb414b7 ("ksmbd: fix incorrect handling of iterate_dir")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/ksmbd/smb2pdu.c
+++ fs/ksmbd/smb2pdu.c
@@ -3962,6 +3962,12 @@ int smb2_query_dir(struct ksmbd_work *work)
 	set_ctx_actor(&dir_fp->readdir_data.ctx, __query_dir);
 
 	rc = iterate_dir(dir_fp->filp, &dir_fp->readdir_data.ctx);
+	/*
+	 * req->OutputBufferLength is too small to contain even one entry.
+	 * In this case, it immediately returns OutputBufferLength 0 to client.
+	 */
+	if (!d_info.out_buf_len && !d_info.num_entry)
+		goto no_buf_len;
 	if (rc == 0)
 		restart_ctx(&dir_fp->readdir_data.ctx);
 	if (rc == -ENOSPC)
@@ -3988,10 +3994,12 @@ int smb2_query_dir(struct ksmbd_work *work)
 		rsp->Buffer[0] = 0;
 		inc_rfc1001_len(rsp_org, 9);
 	} else {
+no_buf_len:
 		((struct file_directory_info *)
 		((char *)rsp->Buffer + d_info.last_entry_offset))
 		->NextEntryOffset = 0;
-		d_info.data_count -= d_info.last_entry_off_align;
+		if (d_info.data_count >= d_info.last_entry_off_align)
+			d_info.data_count -= d_info.last_entry_off_align;
 
 		rsp->StructureSize = cpu_to_le16(9);
 		rsp->OutputBufferOffset = cpu_to_le16(72);
