1bd8bb7d2dfc44509acf729e636523c3c2b729df
Mathias Nyman <mathias.nyman@linux.intel.com>
2022-05-16 12:48:50 +0300

    xhci: Don't defer primary roothub registration if there is only one roothub
    The support for xHCI controllers with only one roothub, and the code
    to defer primary roothub registation until second roothub got merged
    to usb-next for 5.19 at the same time.
    
    commit 873f323618c2 ("xhci: prepare for operation w/o shared hcd")
    commit b7a4f9b5d0e4 ("xhci: Set HCD flag to defer primary roothub
    registration")
    
    These got merged in such a way that the flag to defer primary roothub
    registration is set even for xHC controllers with just one roothub.
    
    Fix this by setting the defer flag in a codepath taken only if we have
    two roothubs
    
    Fixes: 873f323618c2 ("xhci: prepare for operation w/o shared hcd")
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Link: https://lore.kernel.org/r/20220516094850.19788-2-mathias.nyman@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -696,14 +696,14 @@ int xhci_run(struct usb_hcd *hcd)
 	xhci_dbg_trace(xhci, trace_xhci_dbg_init,
 			"Finished %s for main hcd", __func__);
 
-	set_bit(HCD_FLAG_DEFER_RH_REGISTER, &hcd->flags);
-
 	xhci_create_dbc_dev(xhci);
 
 	xhci_debugfs_init(xhci);
 
 	if (xhci_has_one_roothub(xhci))
 		return xhci_run_finished(xhci);
+
+	set_bit(HCD_FLAG_DEFER_RH_REGISTER, &hcd->flags);
 
 	return 0;
 }
