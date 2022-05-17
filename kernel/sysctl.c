7251ceb51af972603552fcea2db316ed2b9d95ba
Geert Uytterhoeven <geert+renesas@glider.be>
2022-05-17 17:07:31 +0200

    sysctl: Merge adjacent CONFIG_TREE_RCU blocks
    There are two adjacent sysctl entries protected by the same
    CONFIG_TREE_RCU config symbol.  Merge them into a single block to
    improve readability.
    
    Use the more common "#ifdef" form while at it.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -2061,7 +2061,7 @@ static struct ctl_table kern_table[] = {
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_ONE,
 	},
-#if defined(CONFIG_TREE_RCU)
+#ifdef CONFIG_TREE_RCU
 	{
 		.procname	= "panic_on_rcu_stall",
 		.data		= &sysctl_panic_on_rcu_stall,
@@ -2071,8 +2071,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_ONE,
 	},
-#endif
-#if defined(CONFIG_TREE_RCU)
 	{
 		.procname	= "max_rcu_stall_to_panic",
 		.data		= &sysctl_max_rcu_stall_to_panic,
