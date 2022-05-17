cdf87d2bd12cf3ea760a1fa35907a31e5177f425
YueHaibing <yuehaibing@huawei.com>
2022-05-17 17:49:00 +0800

    powerpc/kaslr_booke: Fix build error
    arch/powerpc/mm/nohash/kaslr_booke.c: In function ‘kaslr_get_cmdline’:
    arch/powerpc/mm/nohash/kaslr_booke.c:46:2: error: implicit declaration of function ‘early_init_dt_scan_chosen’
      early_init_dt_scan_chosen(boot_command_line);
      ^~~~~~~~~~~~~~~~~~~~~~~~~
    
    arch/powerpc/mm/nohash/kaslr_booke.c: In function ‘get_initrd_range’:
    arch/powerpc/mm/nohash/kaslr_booke.c:210:10: error: implicit declaration of function ‘of_read_number’
      start = of_read_number(prop, len / 4);
              ^~~~~~~~~~~~~~
    
    Add missing include files to fix this.
    
    Fixes: 86c38fec69a4 ("powerpc: Remove asm/prom.h from all files that don't need it")
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20220517094900.14900-1-yuehaibing@huawei.com

--- arch/powerpc/mm/nohash/kaslr_booke.c
+++ arch/powerpc/mm/nohash/kaslr_booke.c
@@ -14,6 +14,8 @@
 #include <linux/memblock.h>
 #include <linux/libfdt.h>
 #include <linux/crash_core.h>
+#include <linux/of.h>
+#include <linux/of_fdt.h>
 #include <asm/cacheflush.h>
 #include <asm/kdump.h>
 #include <mm/mmu_decl.h>
