ac2eab7de458f5e1210ce1237afab40a307075c8
Denis Efremov <denis.e.efremov@oracle.com>
2022-05-18 11:00:52 +0400

    staging: r8188eu: prevent ->Ssid overflow in rtw_wx_set_scan()
    commit bc10916e890948d8927a5c8c40fb5dc44be5e1b8 upstream.
    
    This code has a check to prevent read overflow but it needs another
    check to prevent writing beyond the end of the ->Ssid[] array.
    
    Fixes: 2b42bd58b321 ("staging: r8188eu: introduce new os_dep dir for RTL8188eu driver")
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Denis Efremov <denis.e.efremov@oracle.com>
    Link: https://lore.kernel.org/r/20220518070052.108287-1-denis.e.efremov@oracle.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/r8188eu/os_dep/ioctl_linux.c
+++ drivers/staging/r8188eu/os_dep/ioctl_linux.c
@@ -1249,9 +1249,11 @@ static int rtw_wx_set_scan(struct net_device *dev, struct iw_request_info *a,
 						break;
 					}
 					sec_len = *(pos++); len -= 1;
-					if (sec_len > 0 && sec_len <= len) {
+					if (sec_len > 0 &&
+					    sec_len <= len &&
+					    sec_len <= 32) {
 						ssid[ssid_index].SsidLength = sec_len;
-						memcpy(ssid[ssid_index].Ssid, pos, ssid[ssid_index].SsidLength);
+						memcpy(ssid[ssid_index].Ssid, pos, sec_len);
 						ssid_index++;
 					}
 					pos += sec_len;
