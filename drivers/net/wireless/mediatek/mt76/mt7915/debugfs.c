f572dc969a59a80baa22bf2f7c9af0064402652f
Dan Carpenter <dan.carpenter@oracle.com>
2022-05-19 16:57:22 +0300

    mt76: mt7915: fix endian bug in mt7915_rf_regval_set()
    This code is supposed to set a u32 value, but casting will not work on
    big endian systems.
    
    Fixes: 0a17329ae9c1 ("mt76: mt7915: add debugfs knob for RF registers read/write")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7915/debugfs.c
+++ drivers/net/wireless/mediatek/mt76/mt7915/debugfs.c
@@ -985,8 +985,9 @@ static int
 mt7915_rf_regval_set(void *data, u64 val)
 {
 	struct mt7915_dev *dev = data;
+	u32 val32 = val;
 
-	return mt7915_mcu_rf_regval(dev, dev->mt76.debugfs_reg, (u32 *)&val, true);
+	return mt7915_mcu_rf_regval(dev, dev->mt76.debugfs_reg, &val32, true);
 }
 
 DEFINE_DEBUGFS_ATTRIBUTE(fops_rf_regval, mt7915_rf_regval_get,
