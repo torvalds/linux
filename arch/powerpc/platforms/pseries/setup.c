61bdbca855024997fd8c82dc190f458aa81beca8
Laurent Dufour <ldufour@linux.ibm.com>
2022-05-23 18:43:53 +0200

    powerpc/64s: Don't read H_BLOCK_REMOVE characteristics in radix mode
    There is no need to read the H_BLOCK_REMOVE characteristics when running in
    Radix mode because this hcall is never called.
    
    Furthermore since the commit 387e220a2e5e ("powerpc/64s: Move hash MMU
    support code under CONFIG_PPC_64S_HASH_MMU") define
    pseries_lpar_read_hblkrm_characteristics as un empty function if
    CONFIG_PPC_64S_HASH_MMU is not set, the #ifdef block can be removed.
    
    Signed-off-by: Laurent Dufour <ldufour@linux.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20220523164353.26441-1-ldufour@linux.ibm.com

--- arch/powerpc/platforms/pseries/setup.c
+++ arch/powerpc/platforms/pseries/setup.c
@@ -803,9 +803,8 @@ static void __init pSeries_setup_arch(void)
 	fwnmi_init();
 
 	pseries_setup_security_mitigations();
-#ifdef CONFIG_PPC_64S_HASH_MMU
-	pseries_lpar_read_hblkrm_characteristics();
-#endif
+	if (!radix_enabled())
+		pseries_lpar_read_hblkrm_characteristics();
 
 	/* By default, only probe PCI (can be overridden by rtas_pci) */
 	pci_add_flags(PCI_PROBE_ONLY);
