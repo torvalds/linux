70ce3455345d056b5fc427c3bb4a3ff4d126b6d5
Christoph Hellwig <hch@lst.de>
2022-05-27 07:32:08 +0200

    nvme-pci: disable namespace identifiers for the MAXIO MAP1001
    The MAXIO MAP1001 controllers reports completely bogus Namespace
    identifiers that even change after suspend cycles.  Disable using
    the Identifiers entirely.
    
    Reported-by: Arman Hajishafieha <arman.hajishafieha@hotmail.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Chaitanya Kulkarni <kch@nvidia.com>
    Tested-by: Arman Hajishafieha <arman.hajishafieha@hotmail.com>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -3453,6 +3453,8 @@ static const struct pci_device_id nvme_id_table[] = {
 		.driver_data = NVME_QUIRK_NO_DEEPEST_PS, },
 	{ PCI_DEVICE(0x2646, 0x2263),   /* KINGSTON A2000 NVMe SSD  */
 		.driver_data = NVME_QUIRK_NO_DEEPEST_PS, },
+	{ PCI_DEVICE(0x1e4B, 0x1001),   /* MAXIO MAP1001 */
+		.driver_data = NVME_QUIRK_BOGUS_NID, },
 	{ PCI_DEVICE(0x1e4B, 0x1002),   /* MAXIO MAP1002 */
 		.driver_data = NVME_QUIRK_BOGUS_NID, },
 	{ PCI_DEVICE(0x1e4B, 0x1202),   /* MAXIO MAP1202 */
