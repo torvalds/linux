2cd008522707a59bf38c1f45d5c654eddbb86c20
Pu Lehui <pulehui@huawei.com>
2022-05-30 17:28:10 +0800

    bpf: Unify data extension operation of jited_ksyms and jited_linfo
    We found that 32-bit environment can not print BPF line info due to a data
    inconsistency between jited_ksyms[0] and jited_linfo[0].
    
    For example:
    
      jited_kyms[0] = 0xb800067c, jited_linfo[0] = 0xffffffffb800067c
    
    We know that both of them store BPF func address, but due to the different
    data extension operations when extended to u64, they may not be the same.
    We need to unify the data extension operations of them.
    
    Signed-off-by: Pu Lehui <pulehui@huawei.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/CAEf4BzZ-eDcdJZgJ+Np7Y=V-TVjDDvOMqPwzKjyWrh=i5juv4w@mail.gmail.com
    Link: https://lore.kernel.org/bpf/20220530092815.1112406-2-pulehui@huawei.com

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -4090,14 +4090,15 @@ static int bpf_prog_get_info_by_fd(struct file *file,
 		info.nr_jited_line_info = 0;
 	if (info.nr_jited_line_info && ulen) {
 		if (bpf_dump_raw_ok(file->f_cred)) {
+			unsigned long line_addr;
 			__u64 __user *user_linfo;
 			u32 i;
 
 			user_linfo = u64_to_user_ptr(info.jited_line_info);
 			ulen = min_t(u32, info.nr_jited_line_info, ulen);
 			for (i = 0; i < ulen; i++) {
-				if (put_user((__u64)(long)prog->aux->jited_linfo[i],
-					     &user_linfo[i]))
+				line_addr = (unsigned long)prog->aux->jited_linfo[i];
+				if (put_user((__u64)line_addr, &user_linfo[i]))
 					return -EFAULT;
 			}
 		} else {
