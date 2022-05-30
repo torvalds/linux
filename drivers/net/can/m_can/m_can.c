c7333f79888497bfd75dcd02a94eaf836dd1042c
Marc Kleine-Budde <mkl@pengutronix.de>
2022-05-30 19:30:28 +0200

    can: m_can: m_can_{read_fifo,echo_tx_event}(): shift timestamp to full 32 bits
    commit 4c3333693f07313f5f0145a922f14a7d3c0f4f21 upstream.
    
    In commit 1be37d3b0414 ("can: m_can: fix periph RX path: use
    rx-offload to ensure skbs are sent from softirq context") the RX path
    for peripheral devices was switched to RX-offload.
    
    Received CAN frames are pushed to RX-offload together with a
    timestamp. RX-offload is designed to handle overflows of the timestamp
    correctly, if 32 bit timestamps are provided.
    
    The timestamps of m_can core are only 16 bits wide. So this patch
    shifts them to full 32 bit before passing them to RX-offload.
    
    Link: https://lore.kernel.org/all/20220612211410.4081390-1-mkl@pengutronix.de
    Fixes: 1be37d3b0414 ("can: m_can: fix periph RX path: use rx-offload to ensure skbs are sent from softirq context")
    Cc: <stable@vger.kernel.org> # 5.13
    Cc: Torin Cooper-Bennun <torin@maxiluxsystems.com>
    Reviewed-by: Chandrasekar Ramakrishnan <rcsekar@samsung.com>
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/can/m_can/m_can.c
+++ drivers/net/can/m_can/m_can.c
@@ -532,7 +532,7 @@ static int m_can_read_fifo(struct net_device *dev, u32 rxfs)
 	stats->rx_packets++;
 	stats->rx_bytes += cf->len;
 
-	timestamp = FIELD_GET(RX_BUF_RXTS_MASK, fifo_header.dlc);
+	timestamp = FIELD_GET(RX_BUF_RXTS_MASK, fifo_header.dlc) << 16;
 
 	m_can_receive_skb(cdev, skb, timestamp);
 
@@ -1043,7 +1043,7 @@ static int m_can_echo_tx_event(struct net_device *dev)
 		}
 
 		msg_mark = FIELD_GET(TX_EVENT_MM_MASK, txe);
-		timestamp = FIELD_GET(TX_EVENT_TXTS_MASK, txe);
+		timestamp = FIELD_GET(TX_EVENT_TXTS_MASK, txe) << 16;
 
 		/* ack txe element */
 		m_can_write(cdev, M_CAN_TXEFA, FIELD_PREP(TXEFA_EFAI_MASK,
