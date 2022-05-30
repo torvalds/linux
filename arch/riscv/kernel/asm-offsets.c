f10ae751930efcc553d74fb80054dfb63ed899ff
Atish Patra <atishp@rivosinc.com>
2022-05-30 12:33:31 +0800

    RISC-V: Avoid using per cpu array for ordered booting
    Currently both order booting and spinwait approach uses a per cpu
    array to update stack & task pointer. This approach will not work for the
    following cases.
    1. If NR_CPUs are configured to be less than highest hart id.
    2. A platform has sparse hartid.
    
    This issue can be fixed for ordered booting as the booting cpu brings up
    one cpu at a time using SBI HSM extension which has opaque parameter
    that is unused until now.
    
    Introduce a common secondary boot data structure that can store the stack
    and task pointer. Secondary harts will use this data while booting up
    to setup the sp & tp.
    
    Reviewed-by: Anup Patel <anup@brainfault.org>
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    
    From 9a2451f1866344d38b4a1dc20396e3a03954fcd7
    Resolved merge conflict.
    
    Signed-off-by: <jeeheng.sia@starfivetech.com>
    Signed-off-by: minda.chen <minda.chen@starfivetech.com>

--- arch/riscv/kernel/asm-offsets.c
+++ arch/riscv/kernel/asm-offsets.c
@@ -11,6 +11,7 @@
 #include <asm/thread_info.h>
 #include <asm/ptrace.h>
 #include <asm/suspend.h>
+#include <asm/cpu_ops_sbi.h>
 
 void asm_offsets(void);
 
@@ -316,4 +317,6 @@ void asm_offsets(void)
 	DEFINE(PT_SIZE_ON_STACK, ALIGN(sizeof(struct pt_regs), STACK_ALIGN));
 
 	OFFSET(KERNEL_MAP_VIRT_ADDR, kernel_mapping, virt_addr);
+	OFFSET(SBI_HART_BOOT_TASK_PTR_OFFSET, sbi_hart_boot_data, task_ptr);
+	OFFSET(SBI_HART_BOOT_STACK_PTR_OFFSET, sbi_hart_boot_data, stack_ptr);
 }
