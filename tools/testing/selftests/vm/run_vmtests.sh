30f6f8614a1d1f8ccae6642eaad629997aa33b3b
Kalpana Shetty <kalpana.shetty@amd.com>
2022-05-31 15:55:56 +0530

    selftests/vm: add protection_keys tests to run_vmtests
    Add "protected_keys" tests to "run_vmtests.sh" would help run all VM
    related tests from a single shell script.
    
    [kalpana.shetty@amd.com: Shuah Khan's review comments incorporated, added -x executable check]
      Link: https://lkml.kernel.org/r/20220617202931.357-1-kalpana.shetty@amd.com
    Link: https://lkml.kernel.org/r/20220610090704.296-1-kalpana.shetty@amd.com
    Link: https://lkml.kernel.org/r/20220531102556.388-1-kalpana.shetty@amd.com
    Signed-off-by: Kalpana Shetty <kalpana.shetty@amd.com>
    Cc: Shuah Khan <shuah@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- tools/testing/selftests/vm/run_vmtests.sh
+++ tools/testing/selftests/vm/run_vmtests.sh
@@ -179,4 +179,15 @@ run_test ./ksm_tests -N -m 1
 # KSM test with 2 NUMA nodes and merge_across_nodes = 0
 run_test ./ksm_tests -N -m 0
 
+# protection_keys tests
+if [ -x ./protection_keys_32 ]
+then
+	run_test ./protection_keys_32
+fi
+
+if [ -x ./protection_keys_64 ]
+then
+	run_test ./protection_keys_64
+fi
+
 exit $exitcode
