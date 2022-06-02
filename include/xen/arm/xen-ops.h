9bf22421dc8a69cade3c994771637e9693ff0216
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2022-06-02 22:23:46 +0300

    arm/xen: Introduce xen_setup_dma_ops()
    This patch introduces new helper and places it in new header.
    The helper's purpose is to assign any Xen specific DMA ops in
    a single place. For now, we deal with xen-swiotlb DMA ops only.
    The one of the subsequent commits in current series will add
    xen-grant DMA ops case.
    
    Also re-use the xen_swiotlb_detect() check on Arm32.
    
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>
    [For arm64]
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/1654197833-25362-2-git-send-email-olekstysh@gmail.com
    Signed-off-by: Juergen Gross <jgross@suse.com>

---
+++ include/xen/arm/xen-ops.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_ARM_XEN_OPS_H
+#define _ASM_ARM_XEN_OPS_H
+
+#include <xen/swiotlb-xen.h>
+
+static inline void xen_setup_dma_ops(struct device *dev)
+{
+#ifdef CONFIG_XEN
+	if (xen_swiotlb_detect())
+		dev->dma_ops = &xen_swiotlb_dma_ops;
+#endif
+}
+
+#endif /* _ASM_ARM_XEN_OPS_H */
