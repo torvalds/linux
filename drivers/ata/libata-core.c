c745dfc541e78428ba3986f1d17fe1dfdaca8184
Tyler Erickson <tyler.erickson@seagate.com>
2022-06-02 16:51:11 -0600

    libata: fix reading concurrent positioning ranges log
    The concurrent positioning ranges log is not a fixed size and may depend
    on how many ranges are supported by the device. This patch uses the size
    reported in the GPL directory to determine the number of pages supported
    by the device before attempting to read this log page.
    
    This resolves this error from the dmesg output:
        ata6.00: Read log 0x47 page 0x00 failed, Emask 0x1
    
    Cc: stable@vger.kernel.org
    Fixes: fe22e1c2f705 ("libata: support concurrent positioning ranges log")
    Signed-off-by: Tyler Erickson <tyler.erickson@seagate.com>
    Reviewed-by: Muhammad Ahmad <muhammad.ahmad@seagate.com>
    Tested-by: Michael English <michael.english@seagate.com>
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -2010,16 +2010,16 @@ retry:
 	return err_mask;
 }
 
-static bool ata_log_supported(struct ata_device *dev, u8 log)
+static int ata_log_supported(struct ata_device *dev, u8 log)
 {
 	struct ata_port *ap = dev->link->ap;
 
 	if (dev->horkage & ATA_HORKAGE_NO_LOG_DIR)
-		return false;
+		return 0;
 
 	if (ata_read_log_page(dev, ATA_LOG_DIRECTORY, 0, ap->sector_buf, 1))
-		return false;
-	return get_unaligned_le16(&ap->sector_buf[log * 2]) ? true : false;
+		return 0;
+	return get_unaligned_le16(&ap->sector_buf[log * 2]);
 }
 
 static bool ata_identify_page_supported(struct ata_device *dev, u8 page)
@@ -2455,15 +2455,20 @@ static void ata_dev_config_cpr(struct ata_device *dev)
 	struct ata_cpr_log *cpr_log = NULL;
 	u8 *desc, *buf = NULL;
 
-	if (ata_id_major_version(dev->id) < 11 ||
-	    !ata_log_supported(dev, ATA_LOG_CONCURRENT_POSITIONING_RANGES))
+	if (ata_id_major_version(dev->id) < 11)
+		goto out;
+
+	buf_len = ata_log_supported(dev, ATA_LOG_CONCURRENT_POSITIONING_RANGES);
+	if (buf_len == 0)
 		goto out;
 
 	/*
 	 * Read the concurrent positioning ranges log (0x47). We can have at
-	 * most 255 32B range descriptors plus a 64B header.
+	 * most 255 32B range descriptors plus a 64B header. This log varies in
+	 * size, so use the size reported in the GPL directory. Reading beyond
+	 * the supported length will result in an error.
 	 */
-	buf_len = (64 + 255 * 32 + 511) & ~511;
+	buf_len <<= 9;
 	buf = kzalloc(buf_len, GFP_KERNEL);
 	if (!buf)
 		goto out;
