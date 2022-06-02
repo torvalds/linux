f8a4018c826fde6137425bbdbe524d5973feb173
Mark Brown <broonie@kernel.org>
2022-06-02 15:53:04 +0200

    ASoC: tas2770: Use modern ASoC DAI format terminology
    As part of moving to remove the old style defines for the bus clocks update
    the tas2770 driver to use more modern terminology for clocking.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20220602135316.3554400-9-broonie@kernel.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/tas2770.c
+++ sound/soc/codecs/tas2770.c
@@ -340,11 +340,11 @@ static int tas2770_set_fmt(struct snd_soc_dai *dai, unsigned int fmt)
 	u8 tdm_rx_start_slot = 0, asi_cfg_1 = 0;
 	int ret;
 
-	switch (fmt & SND_SOC_DAIFMT_MASTER_MASK) {
-	case SND_SOC_DAIFMT_CBS_CFS:
+	switch (fmt & SND_SOC_DAIFMT_CLOCK_PROVIDER_MASK) {
+	case SND_SOC_DAIFMT_CBC_CFC:
 		break;
 	default:
-		dev_err(tas2770->dev, "ASI format master is not found\n");
+		dev_err(tas2770->dev, "ASI invalid DAI clocking\n");
 		return -EINVAL;
 	}
 
