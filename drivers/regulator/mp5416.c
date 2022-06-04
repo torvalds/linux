b9dea0184b2641fb3937162a617289b23d52a587
Robert Marko <robimarko@gmail.com>
2022-06-04 16:58:14 +0200

    regulator: mp5416: alphabetically sort header includes
    Sort the header include list alphabetically.
    
    Signed-off-by: Robert Marko <robimarko@gmail.com>
    Link: https://lore.kernel.org/r/20220604145816.47576-2-robimarko@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/mp5416.c
+++ drivers/regulator/mp5416.c
@@ -6,14 +6,14 @@
 //
 // Author: Saravanan Sekar <sravanhome@gmail.com>
 
+#include <linux/err.h>
+#include <linux/i2c.h>
+#include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/init.h>
-#include <linux/err.h>
 #include <linux/platform_device.h>
 #include <linux/regmap.h>
 #include <linux/regulator/driver.h>
-#include <linux/i2c.h>
 
 #define MP5416_REG_CTL0			0x00
 #define MP5416_REG_CTL1			0x01
