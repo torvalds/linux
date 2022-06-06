650c9caba32a0167a018cca0fab32a2965d23513
Filipe Manana <fdmanana@suse.com>
2022-06-06 10:41:19 +0100

    btrfs: do not BUG_ON() on failure to migrate space when replacing extents
    At btrfs_replace_file_extents(), if we fail to migrate reserved metadata
    space from the transaction block reserve into the local block reserve,
    we trigger a BUG_ON(). This is because it should not be possible to have
    a failure here, as we reserved more space when we started the transaction
    than the space we want to migrate. However having a BUG_ON() is way too
    drastic, we can perfectly handle the failure and return the error to the
    caller. So just do that instead, and add a WARN_ON() to make it easier
    to notice the failure if it ever happens (which is particularly useful
    for fstests, and the warning will trigger a failure of a test case).
    
    Reviewed-by: Boris Burkov <boris@bur.io>
    Signed-off-by: Filipe Manana <fdmanana@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/file.c
+++ fs/btrfs/file.c
@@ -2718,7 +2718,8 @@ int btrfs_replace_file_extents(struct btrfs_inode *inode,
 
 	ret = btrfs_block_rsv_migrate(&fs_info->trans_block_rsv, rsv,
 				      min_size, false);
-	BUG_ON(ret);
+	if (WARN_ON(ret))
+		goto out_trans;
 	trans->block_rsv = rsv;
 
 	cur_offset = start;
@@ -2837,7 +2838,8 @@ int btrfs_replace_file_extents(struct btrfs_inode *inode,
 
 		ret = btrfs_block_rsv_migrate(&fs_info->trans_block_rsv,
 					      rsv, min_size, false);
-		BUG_ON(ret);	/* shouldn't happen */
+		if (WARN_ON(ret))
+			break;
 		trans->block_rsv = rsv;
 
 		cur_offset = drop_args.drop_end;
