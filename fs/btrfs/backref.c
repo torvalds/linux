d5b7a34379faf42cf8ef63fb520f05e5d3d218d4
David Sterba <dsterba@suse.com>
2022-06-06 19:32:59 +0200

    btrfs: sink iterator parameter to btrfs_ioctl_logical_to_ino
    [ Upstream commit e3059ec06b9f1a96826cc2bb6ed131aac0942446 ]
    
    There's only one function we pass to iterate_inodes_from_logical as
    iterator, so we can drop the indirection and call it directly, after
    moving the function to backref.c
    
    Signed-off-by: David Sterba <dsterba@suse.com>
    Stable-dep-of: 418ffb9e3cf6 ("btrfs: free btrfs_path before copying inodes to userspace")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/btrfs/backref.c
+++ fs/btrfs/backref.c
@@ -2060,10 +2060,29 @@ out:
 	return ret;
 }
 
+static int build_ino_list(u64 inum, u64 offset, u64 root, void *ctx)
+{
+	struct btrfs_data_container *inodes = ctx;
+	const size_t c = 3 * sizeof(u64);
+
+	if (inodes->bytes_left >= c) {
+		inodes->bytes_left -= c;
+		inodes->val[inodes->elem_cnt] = inum;
+		inodes->val[inodes->elem_cnt + 1] = offset;
+		inodes->val[inodes->elem_cnt + 2] = root;
+		inodes->elem_cnt += 3;
+	} else {
+		inodes->bytes_missing += c - inodes->bytes_left;
+		inodes->bytes_left = 0;
+		inodes->elem_missed += 3;
+	}
+
+	return 0;
+}
+
 int iterate_inodes_from_logical(u64 logical, struct btrfs_fs_info *fs_info,
 				struct btrfs_path *path,
-				iterate_extent_inodes_t *iterate, void *ctx,
-				bool ignore_offset)
+				void *ctx, bool ignore_offset)
 {
 	int ret;
 	u64 extent_item_pos;
@@ -2081,7 +2100,7 @@ int iterate_inodes_from_logical(u64 logical, struct btrfs_fs_info *fs_info,
 	extent_item_pos = logical - found_key.objectid;
 	ret = iterate_extent_inodes(fs_info, found_key.objectid,
 					extent_item_pos, search_commit_root,
-					iterate, ctx, ignore_offset);
+					build_ino_list, ctx, ignore_offset);
 
 	return ret;
 }
