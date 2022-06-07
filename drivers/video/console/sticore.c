1d0811b03eb30b2f0793acaa96c6ce90b8b9c87a
Helge Deller <deller@gmx.de>
2022-06-07 12:57:58 +0200

    parisc/stifb: Fix fb_is_primary_device() only available with CONFIG_FB_STI
    Fix this build error noticed by the kernel test robot:
    
    drivers/video/console/sticore.c:1132:5: error: redefinition of 'fb_is_primary_device'
     arch/parisc/include/asm/fb.h:18:19: note: previous definition of 'fb_is_primary_device'
    
    Signed-off-by: Helge Deller <deller@gmx.de>
    Reported-by: kernel test robot <lkp@intel.com>
    Cc: stable@vger.kernel.org   # v5.10+

--- drivers/video/console/sticore.c
+++ drivers/video/console/sticore.c
@@ -1148,6 +1148,7 @@ int sti_call(const struct sti_struct *sti, unsigned long func,
 	return ret;
 }
 
+#if defined(CONFIG_FB_STI)
 /* check if given fb_info is the primary device */
 int fb_is_primary_device(struct fb_info *info)
 {
@@ -1163,6 +1164,7 @@ int fb_is_primary_device(struct fb_info *info)
 	return (sti->info == info);
 }
 EXPORT_SYMBOL(fb_is_primary_device);
+#endif
 
 MODULE_AUTHOR("Philipp Rumpf, Helge Deller, Thomas Bogendoerfer");
 MODULE_DESCRIPTION("Core STI driver for HP's NGLE series graphics cards in HP PARISC machines");
