f16d89e083fccb465b12d4c055cb31a0b1506f8f
Greg Kroah-Hartman <gregkh@google.com>
2022-06-07 17:21:44 +0200

    ANDROID: scsi: ufs: sysfs: fix up scsi/ufs merge problems.
    When merging, the ufs_sysfs_add_nodes() function got merged incorrectly.
    Pick the upstream version which fixes the build problems.
    
    This moves the tracepoint out of the sysfs create function and puts it
    where the call originated, as the hba structure is not known in the
    sysfs create call.
    
    Bug: 158050260
    Cc: Jaegeuk Kim <jaegeuk@google.com>
    Fixes: 0b0468f649d2 ("ANDROID: scsi: ufs: add a hook to add sysfs entries")
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I875944bb95597f1df63f109684d053903b2282a0

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -9793,7 +9793,8 @@ int ufshcd_init(struct ufs_hba *hba, void __iomem *mmio_base, unsigned int irq)
 	ufshcd_set_ufs_dev_active(hba);
 
 	async_schedule(ufshcd_async_scan, hba);
-	ufs_sysfs_add_nodes(hba);
+	ufs_sysfs_add_nodes(dev);
+	trace_android_vh_ufs_update_sysfs(hba);
 
 	device_enable_async_suspend(dev);
 	return 0;
