e75e5db8f8ac5b9d4e8968060822bed4671f22ec
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2022-06-08 20:26:31 -0700

    ASoC: SOF: IPC4: Add pcm ops
    Define and set the PCM ops for IPC4.
    
    Co-developed-by: Rander Wang <rander.wang@linux.intel.com>
    Signed-off-by: Rander Wang <rander.wang@linux.intel.com>
    Co-developed-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Reviewed-by: Péter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Reviewed-by: Yaochun Hung <yc.hung@mediatek.com>
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Link: https://lore.kernel.org/r/20220609032643.916882-12-ranjani.sridharan@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sof/ipc4-priv.h
+++ sound/soc/sof/ipc4-priv.h
@@ -42,5 +42,6 @@ struct sof_ipc4_fw_module {
 extern const struct sof_ipc_fw_loader_ops ipc4_loader_ops;
 extern const struct sof_ipc_tplg_ops ipc4_tplg_ops;
 extern const struct sof_ipc_tplg_control_ops tplg_ipc4_control_ops;
+extern const struct sof_ipc_pcm_ops ipc4_pcm_ops;
 
 #endif
--- sound/soc/sof/ipc4.c
+++ sound/soc/sof/ipc4.c
@@ -604,4 +604,5 @@ const struct sof_ipc_ops ipc4_ops = {
 	.get_reply = sof_ipc4_get_reply,
 	.fw_loader = &ipc4_loader_ops,
 	.tplg = &ipc4_tplg_ops,
+	.pcm = &ipc4_pcm_ops,
 };
