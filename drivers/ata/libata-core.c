5eb8deb4af56b120c152066ff43a0867267b264b
Sergey Shtylyov <s.shtylyov@omp.ru>
2022-06-10 00:06:54 +0300

    ata: libata-core: get rid of *else* branches in ata_id_n_sectors()
    Using *else* after *return* doesn't make much sense -- getting rid of such
    *else* branches reduces the indentation levels and thus reduces # of LoC...
    
    Signed-off-by: Sergey Shtylyov <s.shtylyov@omp.ru>
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -1103,18 +1103,16 @@ static u64 ata_id_n_sectors(const u16 *id)
 	if (ata_id_has_lba(id)) {
 		if (ata_id_has_lba48(id))
 			return ata_id_u64(id, ATA_ID_LBA_CAPACITY_2);
-		else
-			return ata_id_u32(id, ATA_ID_LBA_CAPACITY);
-	} else {
-		if (ata_id_current_chs_valid(id))
-			return (u32)id[ATA_ID_CUR_CYLS] *
-			       (u32)id[ATA_ID_CUR_HEADS] *
-			       (u32)id[ATA_ID_CUR_SECTORS];
-		else
-			return (u32)id[ATA_ID_CYLS] *
-			       (u32)id[ATA_ID_HEADS] *
-			       (u32)id[ATA_ID_SECTORS];
+
+		return ata_id_u32(id, ATA_ID_LBA_CAPACITY);
 	}
+
+	if (ata_id_current_chs_valid(id))
+		return (u32)id[ATA_ID_CUR_CYLS] * (u32)id[ATA_ID_CUR_HEADS] *
+		       (u32)id[ATA_ID_CUR_SECTORS];
+
+	return (u32)id[ATA_ID_CYLS] * (u32)id[ATA_ID_HEADS] *
+	       (u32)id[ATA_ID_SECTORS];
 }
 
 u64 ata_tf_to_lba48(const struct ata_taskfile *tf)
