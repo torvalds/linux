8e28e53f13562e3fba4c64d01dfb66a9c34167da
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2022-06-11 10:37:40 +0200

    perf/marvell_cn10k: Remove useless license text when SPDX-License-Identifier is already used
    An SPDX-License-Identifier is already in place. There is no need to
    duplicate part of the corresponding license.
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Link: https://lore.kernel.org/r/4a8016a6da9cc6815cfa0f97ae8d3dd862797bda.1654936653.git.christophe.jaillet@wanadoo.fr
    Signed-off-by: Will Deacon <will@kernel.org>

--- drivers/perf/marvell_cn10k_tad_pmu.c
+++ drivers/perf/marvell_cn10k_tad_pmu.c
@@ -2,10 +2,6 @@
 /* Marvell CN10K LLC-TAD perf driver
  *
  * Copyright (C) 2021 Marvell
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  */
 
 #define pr_fmt(fmt) "tad_pmu: " fmt
