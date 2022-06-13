5731f7abf32281fc55a089cce255f6121b1596fb
Dom Cobley <popcornmix@gmail.com>
2022-06-13 16:47:56 +0200

    drm/vc4: hdmi: Report that 3d/stereo is allowed
    Our HDMI controllers supports Stereo output so let's enable it.
    
    Signed-off-by: Dom Cobley <popcornmix@gmail.com>
    Link: https://lore.kernel.org/r/20220613144800.326124-30-maxime@cerno.tech
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/gpu/drm/vc4/vc4_hdmi.c
+++ drivers/gpu/drm/vc4/vc4_hdmi.c
@@ -427,6 +427,7 @@ static int vc4_hdmi_connector_init(struct drm_device *dev,
 
 	connector->interlace_allowed = 1;
 	connector->doublescan_allowed = 0;
+	connector->stereo_allowed = 1;
 
 	if (vc4_hdmi->variant->supports_hdr)
 		drm_connector_attach_hdr_output_metadata_property(connector);
