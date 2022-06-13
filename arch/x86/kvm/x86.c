1c4dc57328bf218e999951824dce75c6125c4f3c
Ben Gardon <bgardon@google.com>
2022-06-13 21:25:20 +0000

    KVM: x86: Fix errant brace in KVM capability handling
    The braces around the KVM_CAP_XSAVE2 block also surround the
    KVM_CAP_PMU_CAPABILITY block, likely the result of a merge issue. Simply
    move the curly brace back to where it belongs.
    
    Fixes: ba7bb663f5547 ("KVM: x86: Provide per VM capability for disabling PMU virtualization")
    
    Reviewed-by: David Matlack <dmatlack@google.com>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Ben Gardon <bgardon@google.com>
    Message-Id: <20220613212523.3436117-8-bgardon@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4420,10 +4420,10 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 		if (r < sizeof(struct kvm_xsave))
 			r = sizeof(struct kvm_xsave);
 		break;
+	}
 	case KVM_CAP_PMU_CAPABILITY:
 		r = enable_pmu ? KVM_CAP_PMU_VALID_MASK : 0;
 		break;
-	}
 	case KVM_CAP_DISABLE_QUIRKS2:
 		r = KVM_X86_VALID_QUIRKS;
 		break;
