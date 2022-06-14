3d5f8d03786fee6aa7a4c59446c5356775aeb4d9
Sean Christopherson <seanjc@google.com>
2022-06-14 20:07:07 +0000

    KVM: selftests: Drop unused SVM_CPUID_FUNC macro
    Drop SVM_CPUID_FUNC to reduce the probability of tests open coding CPUID
    checks instead of using kvm_cpu_has() or this_cpu_has().
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Link: https://lore.kernel.org/r/20220614200707.3315957-43-seanjc@google.com

--- tools/testing/selftests/kvm/include/x86_64/svm.h
+++ tools/testing/selftests/kvm/include/x86_64/svm.h
@@ -218,8 +218,6 @@ struct __attribute__ ((__packed__)) vmcb {
 	struct vmcb_save_area save;
 };
 
-#define SVM_CPUID_FUNC 0x8000000a
-
 #define SVM_VM_CR_SVM_DISABLE 4
 
 #define SVM_SELECTOR_S_SHIFT 4
