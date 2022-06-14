363c82a634781ae299adf4698ecdbffb7e1fdd7a
Stephen Boyd <swboyd@chromium.org>
2022-06-14 00:57:26 -0700

    platform/chrome: cros_ec: Always expose last resume result
    [ Upstream commit 74bb746407bf0d7c7d126c7731dbcd66d467619b ]
    
    The last resume result exposing logic in cros_ec_sleep_event()
    incorrectly requires S0ix support, which doesn't work on ARM based
    systems where S0ix doesn't exist. That's because cros_ec_sleep_event()
    only reports the last resume result when the EC indicates the last sleep
    event was an S0ix resume. On ARM systems, the last sleep event is always
    S3 resume, but the EC can still detect sleep hang events in case some
    other part of the AP is blocking sleep.
    
    Always expose the last resume result if the EC supports it so that this
    works on all devices regardless of S0ix support. This fixes sleep hang
    detection on ARM based chromebooks like Trogdor.
    
    Cc: Rajat Jain <rajatja@chromium.org>
    Cc: Matthias Kaehlcke <mka@chromium.org>
    Cc: Hsin-Yi Wang <hsinyi@chromium.org>
    Cc: Tzung-Bi Shih <tzungbi@kernel.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-by: Evan Green <evgreen@chromium.org>
    Fixes: 7235560ac77a ("platform/chrome: Add support for v1 of host sleep event")
    Signed-off-by: Stephen Boyd <swboyd@chromium.org>
    Signed-off-by: Tzung-Bi Shih <tzungbi@kernel.org>
    Link: https://lore.kernel.org/r/20220614075726.2729987-1-swboyd@chromium.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/platform/chrome/cros_ec.c
+++ drivers/platform/chrome/cros_ec.c
@@ -135,16 +135,16 @@ static int cros_ec_sleep_event(struct cros_ec_device *ec_dev, u8 sleep_event)
 	buf.msg.command = EC_CMD_HOST_SLEEP_EVENT;
 
 	ret = cros_ec_cmd_xfer_status(ec_dev, &buf.msg);
-
-	/* For now, report failure to transition to S0ix with a warning. */
+	/* Report failure to transition to system wide suspend with a warning. */
 	if (ret >= 0 && ec_dev->host_sleep_v1 &&
-	    (sleep_event == HOST_SLEEP_EVENT_S0IX_RESUME)) {
+	    (sleep_event == HOST_SLEEP_EVENT_S0IX_RESUME ||
+	     sleep_event == HOST_SLEEP_EVENT_S3_RESUME)) {
 		ec_dev->last_resume_result =
 			buf.u.resp1.resume_response.sleep_transitions;
 
 		WARN_ONCE(buf.u.resp1.resume_response.sleep_transitions &
 			  EC_HOST_RESUME_SLEEP_TIMEOUT,
-			  "EC detected sleep transition timeout. Total slp_s0 transitions: %d",
+			  "EC detected sleep transition timeout. Total sleep transitions: %d",
 			  buf.u.resp1.resume_response.sleep_transitions &
 			  EC_HOST_RESUME_SLEEP_TRANSITIONS_MASK);
 	}
