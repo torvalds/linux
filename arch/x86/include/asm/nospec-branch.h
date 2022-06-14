0b53c374b9eff2255a386f1f1cfb9a928e52a5ae
Peter Zijlstra <peterz@infradead.org>
2022-06-14 23:15:36 +0200

    x86/retpoline: Use -mfunction-return
    Utilize -mfunction-return=thunk-extern when available to have the
    compiler replace RET instructions with direct JMPs to the symbol
    __x86_return_thunk. This does not affect assembler (.S) sources, only C
    sources.
    
    -mfunction-return=thunk-extern has been available since gcc 7.3 and
    clang 15.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Nick Desaulniers <ndesaulniers@google.com>
    Reviewed-by: Josh Poimboeuf <jpoimboe@kernel.org>
    Tested-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>

--- arch/x86/include/asm/nospec-branch.h
+++ arch/x86/include/asm/nospec-branch.h
@@ -123,6 +123,8 @@
 typedef u8 retpoline_thunk_t[RETPOLINE_THUNK_SIZE];
 extern retpoline_thunk_t __x86_indirect_thunk_array[];
 
+extern void __x86_return_thunk(void);
+
 #ifdef CONFIG_RETPOLINE
 
 #define GEN(reg) \
