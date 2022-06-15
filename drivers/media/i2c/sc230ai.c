aabfa89452dd05870e701551b911708ff76e8aa7
Ziyuan Xu <xzy.xu@rock-chips.com>
2022-06-15 22:56:10 +0800

    media: i2c: initcall sc230ai early when ROCKCHIP_THUNDER_BOOT_ISP=y
    Signed-off-by: Ziyuan Xu <xzy.xu@rock-chips.com>
    Change-Id: Ie4311886a46f070703ceb613bef0a8e3f85b5076

--- drivers/media/i2c/sc230ai.c
+++ drivers/media/i2c/sc230ai.c
@@ -1810,7 +1810,11 @@ static void __exit sensor_mod_exit(void)
 	i2c_del_driver(&sc230ai_i2c_driver);
 }
 
+#if defined(CONFIG_VIDEO_ROCKCHIP_THUNDER_BOOT_ISP) && !defined(CONFIG_INITCALL_ASYNC)
+subsys_initcall(sensor_mod_init);
+#else
 device_initcall_sync(sensor_mod_init);
+#endif
 module_exit(sensor_mod_exit);
 
 MODULE_DESCRIPTION("smartsens sc230ai sensor driver");
