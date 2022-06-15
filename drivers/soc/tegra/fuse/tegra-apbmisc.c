0a3c2dbec425b82d9af7aeb5dc9b18da398992a7
Liang He <windhl@126.com>
2022-06-15 20:32:32 +0800

    soc/tegra: fuse: Add missing of_node_put()
    In tegra_init_apbmisc(), of_find_matching_node() will return a node
    pointer with refcount incremented. We should use of_node_put() in each
    failure path or when it is not used anymore.
    
    Signed-off-by: Liang He <windhl@126.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/soc/tegra/fuse/tegra-apbmisc.c
+++ drivers/soc/tegra/fuse/tegra-apbmisc.c
@@ -182,12 +182,12 @@ void __init tegra_init_apbmisc(void)
 		 */
 		if (of_address_to_resource(np, 0, &apbmisc) < 0) {
 			pr_err("failed to get APBMISC registers\n");
-			return;
+			goto put;
 		}
 
 		if (of_address_to_resource(np, 1, &straps) < 0) {
 			pr_err("failed to get strapping options registers\n");
-			return;
+			goto put;
 		}
 	}
 
@@ -208,4 +208,7 @@ void __init tegra_init_apbmisc(void)
 	}
 
 	long_ram_code = of_property_read_bool(np, "nvidia,long-ram-code");
+
+put:
+	of_node_put(np);
 }
