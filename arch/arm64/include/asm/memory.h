450d0e74d886c172ac2f72518b797a18ee8d1327
Zhou Guanghui <zhouguanghui1@huawei.com>
2022-06-15 10:27:42 +0000

    memblock,arm64: expand the static memblock memory table
    In a system(Huawei Ascend ARM64 SoC) using HBM, a multi-bit ECC error
    occurs, and the BIOS will mark the corresponding area (for example, 2 MB)
    as unusable.  When the system restarts next time, these areas are not
    reported or reported as EFI_UNUSABLE_MEMORY.  Both cases lead to an
    increase in the number of memblocks, whereas EFI_UNUSABLE_MEMORY leads to
    a larger number of memblocks.
    
    For example, if the EFI_UNUSABLE_MEMORY type is reported:
    ...
    memory[0x92]    [0x0000200834a00000-0x0000200835bfffff], 0x0000000001200000 bytes on node 7 flags: 0x0
    memory[0x93]    [0x0000200835c00000-0x0000200835dfffff], 0x0000000000200000 bytes on node 7 flags: 0x4
    memory[0x94]    [0x0000200835e00000-0x00002008367fffff], 0x0000000000a00000 bytes on node 7 flags: 0x0
    memory[0x95]    [0x0000200836800000-0x00002008369fffff], 0x0000000000200000 bytes on node 7 flags: 0x4
    memory[0x96]    [0x0000200836a00000-0x0000200837bfffff], 0x0000000001200000 bytes on node 7 flags: 0x0
    memory[0x97]    [0x0000200837c00000-0x0000200837dfffff], 0x0000000000200000 bytes on node 7 flags: 0x4
    memory[0x98]    [0x0000200837e00000-0x000020087fffffff], 0x0000000048200000 bytes on node 7 flags: 0x0
    memory[0x99]    [0x0000200880000000-0x0000200bcfffffff], 0x0000000350000000 bytes on node 6 flags: 0x0
    memory[0x9a]    [0x0000200bd0000000-0x0000200bd01fffff], 0x0000000000200000 bytes on node 6 flags: 0x4
    memory[0x9b]    [0x0000200bd0200000-0x0000200bd07fffff], 0x0000000000600000 bytes on node 6 flags: 0x0
    memory[0x9c]    [0x0000200bd0800000-0x0000200bd09fffff], 0x0000000000200000 bytes on node 6 flags: 0x4
    memory[0x9d]    [0x0000200bd0a00000-0x0000200fcfffffff], 0x00000003ff600000 bytes on node 6 flags: 0x0
    memory[0x9e]    [0x0000200fd0000000-0x0000200fd01fffff], 0x0000000000200000 bytes on node 6 flags: 0x4
    memory[0x9f]    [0x0000200fd0200000-0x0000200fffffffff], 0x000000002fe00000 bytes on node 6 flags: 0x0
    ...
    
    The EFI memory map is parsed to construct the memblock arrays before the
    memblock arrays can be resized.  As the result, memory regions beyond
    INIT_MEMBLOCK_REGIONS are lost.
    
    Add a new macro INIT_MEMBLOCK_MEMORY_REGIONS to replace
    INIT_MEMBLOCK_REGTIONS to define the size of the static memblock.memory
    array.
    
    Allow overriding memblock.memory array size with architecture defined
    INIT_MEMBLOCK_MEMORY_REGIONS and make arm64 to set
    INIT_MEMBLOCK_MEMORY_REGIONS to 1024 when CONFIG_EFI is enabled.
    
    Link: https://lkml.kernel.org/r/20220615102742.96450-1-zhouguanghui1@huawei.com
    Signed-off-by: Zhou Guanghui <zhouguanghui1@huawei.com>
    Acked-by: Mike Rapoport <rppt@linux.ibm.com>
    Tested-by: Darren Hart <darren@os.amperecomputing.com>
    Acked-by: Will Deacon <will@kernel.org>		[arm64]
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Cc: Xu Qiang <xuqiang36@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/arm64/include/asm/memory.h
+++ arch/arm64/include/asm/memory.h
@@ -364,6 +364,15 @@ void dump_mem_limit(void);
 # define INIT_MEMBLOCK_RESERVED_REGIONS	(INIT_MEMBLOCK_REGIONS + NR_CPUS + 1)
 #endif
 
+/*
+ * memory regions which marked with flag MEMBLOCK_NOMAP(for example, the memory
+ * of the EFI_UNUSABLE_MEMORY type) may divide a continuous memory block into
+ * multiple parts. As a result, the number of memory regions is large.
+ */
+#ifdef CONFIG_EFI
+#define INIT_MEMBLOCK_MEMORY_REGIONS	(INIT_MEMBLOCK_REGIONS * 8)
+#endif
+
 #include <asm-generic/memory_model.h>
 
 #endif /* __ASM_MEMORY_H */
