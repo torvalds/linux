372a2eaf9bed96395699eb4c2d32e3fe48830e8c
hongao <hongao@uniontech.com>
2022-06-16 15:22:33 +0800

    drm/exynos: replace drm_detect_hdmi_monitor() with drm_display_info.is_hdmi
    Once EDID is parsed, the monitor HDMI support information is available
    through drm_display_info.is_hdmi.
    
    This driver calls drm_detect_hdmi_monitor() to receive the same
    information, which is less efficient.
    
    Avoid calling drm_detect_hdmi_monitor() and use drm_display_info.is_hdmi
    instead.
    
    Signed-off-by: hongao <hongao@uniontech.com>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_hdmi.c
+++ drivers/gpu/drm/exynos/exynos_hdmi.c
@@ -893,7 +893,7 @@ static int hdmi_get_modes(struct drm_connector *connector)
 	if (!edid)
 		return -ENODEV;
 
-	hdata->dvi_mode = !drm_detect_hdmi_monitor(edid);
+	hdata->dvi_mode = !connector->display_info.is_hdmi;
 	DRM_DEV_DEBUG_KMS(hdata->dev, "%s : width[%d] x height[%d]\n",
 			  (hdata->dvi_mode ? "dvi monitor" : "hdmi monitor"),
 			  edid->width_cm, edid->height_cm);
