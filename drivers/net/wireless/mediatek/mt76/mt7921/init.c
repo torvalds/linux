d5a50e6bd1972c481f82befa846dce0b9866f025
Deren Wu <deren.wu@mediatek.com>
2022-06-16 23:57:43 +0800

    mt76: mt7921: fix aggregation subframes setting to HE max
    mt7921/mt7922 support HE max aggregation subframes 256 for both tx/rx.
    Get better throughput then before.
    
    Fixes: 94bb18b03d43 ("mt76: mt7921: fix max aggregation subframes setting")
    Tested-by: Ming Yen Hsieh <mingyen.hsieh@mediatek.com>
    Reviewed-by: Sean Wang <sean.wang@mediatek.com>
    Signed-off-by: Deren Wu <deren.wu@mediatek.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7921/init.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/init.c
@@ -52,8 +52,8 @@ mt7921_init_wiphy(struct ieee80211_hw *hw)
 	struct wiphy *wiphy = hw->wiphy;
 
 	hw->queues = 4;
-	hw->max_rx_aggregation_subframes = 64;
-	hw->max_tx_aggregation_subframes = 128;
+	hw->max_rx_aggregation_subframes = IEEE80211_MAX_AMPDU_BUF_HE;
+	hw->max_tx_aggregation_subframes = IEEE80211_MAX_AMPDU_BUF_HE;
 	hw->netdev_features = NETIF_F_RXCSUM;
 
 	hw->radiotap_timestamp.units_pos =
