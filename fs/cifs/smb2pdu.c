7c05eae8db9296e28b5dd34deec1ca5ef96d0f08
Steve French <stfrench@microsoft.com>
2022-06-15 22:40:23 -0500

    smb3: add trace point for SMB2_set_eof
    In order to debug problems with file size being reported incorrectly
    temporarily (in this case xfstest generic/584 intermittent failure)
    we need to add trace point for the non-compounded code path where
    we set the file size (SMB2_set_eof).  The new trace point is:
       "smb3_set_eof"
    
    Here is sample output from the tracepoint:
    
                TASK-PID     CPU#  |||||  TIMESTAMP  FUNCTION
                  | |         |   |||||     |         |
              xfs_io-75403   [002] ..... 95219.189835: smb3_set_eof: xid=221 sid=0xeef1cbd2 tid=0x27079ee6 fid=0x52edb58c offset=0x100000
     aio-dio-append--75418   [010] ..... 95219.242402: smb3_set_eof: xid=226 sid=0xeef1cbd2 tid=0x27079ee6 fid=0xae89852d offset=0x0
    
    Reviewed-by: Paulo Alcantara (SUSE) <pc@cjr.nz>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/cifs/smb2pdu.c
+++ fs/cifs/smb2pdu.c
@@ -5154,6 +5154,8 @@ SMB2_set_eof(const unsigned int xid, struct cifs_tcon *tcon, u64 persistent_fid,
 	data = &info;
 	size = sizeof(struct smb2_file_eof_info);
 
+	trace_smb3_set_eof(xid, persistent_fid, tcon->tid, tcon->ses->Suid, le64_to_cpu(*eof));
+
 	return send_set_info(xid, tcon, persistent_fid, volatile_fid,
 			pid, FILE_END_OF_FILE_INFORMATION, SMB2_O_INFO_FILE,
 			0, 1, &data, &size);
