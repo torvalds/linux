626213d61b1b5b7668095afaa1968ccad8e1c197
Elliot Berman <quic_eberman@quicinc.com>
2022-06-15 18:31:48 -0700

    treewide: Fixup copyright attribution
    Clean up missing LF copyright attribution.
    
    Change-Id: Ic8c44271696144adc4555b5c7ff6573c166de5c3
    Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>
    Signed-off-by: Sai Harshini Nimmala <quic_snimmala@quicinc.com>

--- kernel/sched/walt/fixup.c
+++ kernel/sched/walt/fixup.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2016-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2016-2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <trace/hooks/cpufreq.h>
