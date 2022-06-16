7cf2b0f9611b9971d663e1fc3206eeda3b902922
Dave Chinner <dchinner@redhat.com>
2022-06-16 07:44:31 -0700

    xfs: bound maximum wait time for inodegc work
    Currently inodegc work can sit queued on the per-cpu queue until
    the workqueue is either flushed of the queue reaches a depth that
    triggers work queuing (and later throttling). This means that we
    could queue work that waits for a long time for some other event to
    trigger flushing.
    
    Hence instead of just queueing work at a specific depth, use a
    delayed work that queues the work at a bound time. We can still
    schedule the work immediately at a given depth, but we no long need
    to worry about leaving a number of items on the list that won't get
    processed until external events prevail.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -1074,7 +1074,7 @@ xfs_inodegc_init_percpu(
 		gc = per_cpu_ptr(mp->m_inodegc, cpu);
 		init_llist_head(&gc->list);
 		gc->items = 0;
-		INIT_WORK(&gc->work, xfs_inodegc_worker);
+		INIT_DELAYED_WORK(&gc->work, xfs_inodegc_worker);
 	}
 	return 0;
 }
