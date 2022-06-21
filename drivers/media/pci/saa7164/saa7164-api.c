4502aae07b3b84cb3f3da6444ff8a1d412f9c7e1
Jiang Jian <jiangjian@cdjrlc.com>
2022-06-21 16:05:28 +0100

    media: saa7164: Remove duplicate 'on' in two places.
    file: drivers/media/pci/saa7164/saa7164-api.c
    line: 804
    /* Assumption: Hauppauge eeprom is at 0xa0 on on bus 0 */
    changed to
    /* Assumption: Hauppauge eeprom is at 0xa0 on bus 0 */
    
    Signed-off-by: Jiang Jian <jiangjian@cdjrlc.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>

--- drivers/media/pci/saa7164/saa7164-api.c
+++ drivers/media/pci/saa7164/saa7164-api.c
@@ -801,7 +801,7 @@ int saa7164_api_read_eeprom(struct saa7164_dev *dev, u8 *buf, int buflen)
 	if (buflen < 128)
 		return -ENOMEM;
 
-	/* Assumption: Hauppauge eeprom is at 0xa0 on on bus 0 */
+	/* Assumption: Hauppauge eeprom is at 0xa0 on bus 0 */
 	/* TODO: Pull the details from the boards struct */
 	return saa7164_api_i2c_read(&dev->i2c_bus[0], 0xa0 >> 1, sizeof(reg),
 		&reg[0], 128, buf);
