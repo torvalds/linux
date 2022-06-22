bb924ca69f71b4f54c8f07be28e2b76f5ad1d2ac
David Matlack <dmatlack@google.com>
2022-06-22 15:26:48 -0400

    KVM: x86/mmu: Optimize MMU page cache lookup for all direct SPs
    Commit fb58a9c345f6 ("KVM: x86/mmu: Optimize MMU page cache lookup for
    fully direct MMUs") skipped the unsync checks and write flood clearing
    for full direct MMUs. We can extend this further to skip the checks for
    all direct shadow pages. Direct shadow pages in indirect MMUs (i.e.
    shadow paging) are used when shadowing a guest huge page with smaller
    pages. Such direct shadow pages, like their counterparts in fully direct
    MMUs, are never marked unsynced or have a non-zero write-flooding count.
    
    Checking sp->role.direct also generates better code than checking
    direct_map because, due to register pressure, direct_map has to get
    shoved onto the stack and then pulled back off.
    
    No functional change intended.
    
    Reviewed-by: Lai Jiangshan <jiangshanlai@gmail.com>
    Reviewed-by: Sean Christopherson <seanjc@google.com>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: David Matlack <dmatlack@google.com>
    Message-Id: <20220516232138.1783324-2-dmatlack@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -2000,7 +2000,6 @@ static struct kvm_mmu_page *kvm_mmu_get_page(struct kvm_vcpu *vcpu,
 					     int direct,
 					     unsigned int access)
 {
-	bool direct_mmu = vcpu->arch.mmu->root_role.direct;
 	union kvm_mmu_page_role role;
 	struct hlist_head *sp_list;
 	unsigned quadrant;
@@ -2060,7 +2059,8 @@ static struct kvm_mmu_page *kvm_mmu_get_page(struct kvm_vcpu *vcpu,
 			continue;
 		}
 
-		if (direct_mmu)
+		/* unsync and write-flooding only apply to indirect SPs. */
+		if (sp->role.direct)
 			goto trace_get_page;
 
 		if (sp->unsync) {
