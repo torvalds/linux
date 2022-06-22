62c0aff64c8d6594357b6217db019552ea664b90
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2022-06-22 20:11:47 +0300

    clk: Remove never used devm_clk_*unregister()
    For the entire history of the devm_clk_*unregister() existence they were
    used only once (*) in 2015. Remove them.
    
    *) The commit 264e3b75de4e ("clk: s2mps11: Simplify s2mps11_clk_probe unwind
       paths") exactly supports the point of the change proposed here.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Link: https://lore.kernel.org/r/20220622171147.85603-1-andriy.shevchenko@linux.intel.com
    Acked-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- include/linux/clk-provider.h
+++ include/linux/clk-provider.h
@@ -1176,10 +1176,8 @@ int __must_check devm_clk_hw_register(struct device *dev, struct clk_hw *hw);
 int __must_check of_clk_hw_register(struct device_node *node, struct clk_hw *hw);
 
 void clk_unregister(struct clk *clk);
-void devm_clk_unregister(struct device *dev, struct clk *clk);
 
 void clk_hw_unregister(struct clk_hw *hw);
-void devm_clk_hw_unregister(struct device *dev, struct clk_hw *hw);
 
 /* helper functions */
 const char *__clk_get_name(const struct clk *clk);
