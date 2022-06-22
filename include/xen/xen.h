251e90e7e346a23742b90e2c4db19d322e071d99
Juergen Gross <jgross@suse.com>
2022-06-22 08:38:38 +0200

    xen: don't require virtio with grants for non-PV guests
    Commit fa1f57421e0b ("xen/virtio: Enable restricted memory access using
    Xen grant mappings") introduced a new requirement for using virtio
    devices: the backend now needs to support the VIRTIO_F_ACCESS_PLATFORM
    feature.
    
    This is an undue requirement for non-PV guests, as those can be operated
    with existing backends without any problem, as long as those backends
    are running in dom0.
    
    Per default allow virtio devices without grant support for non-PV
    guests.
    
    On Arm require VIRTIO_F_ACCESS_PLATFORM for devices having been listed
    in the device tree to use grants.
    
    Add a new config item to always force use of grants for virtio.
    
    Fixes: fa1f57421e0b ("xen/virtio: Enable restricted memory access using Xen grant mappings")
    Reported-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Reviewed-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
    Tested-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com> # Arm64 guest using Xen
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>
    Link: https://lore.kernel.org/r/20220622063838.8854-4-jgross@suse.com
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- include/xen/xen.h
+++ include/xen/xen.h
@@ -52,14 +52,6 @@ bool xen_biovec_phys_mergeable(const struct bio_vec *vec1,
 extern u64 xen_saved_max_mem_size;
 #endif
 
-#include <linux/virtio_anchor.h>
-
-static inline void xen_set_restricted_virtio_memory_access(void)
-{
-	if (IS_ENABLED(CONFIG_XEN_VIRTIO) && xen_domain())
-		virtio_set_mem_acc_cb(virtio_require_restricted_mem_acc);
-}
-
 #ifdef CONFIG_XEN_UNPOPULATED_ALLOC
 int xen_alloc_unpopulated_pages(unsigned int nr_pages, struct page **pages);
 void xen_free_unpopulated_pages(unsigned int nr_pages, struct page **pages);
