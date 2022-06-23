20fb0c8272bbb102d15bdd11aa64f828619dd7cc
Petr Mladek <pmladek@suse.com>
2022-06-23 16:51:52 +0200

    Revert "printk: Wait for the global console lock when the system is going down"
    This reverts commit b87f02307d3cfbda768520f0687c51ca77e14fc3.
    
    The testing of 5.19 release candidates revealed missing synchronization
    between early and regular console functionality.
    
    It would be possible to start the console kthreads later as a workaround.
    But it is clear that console lock serialized console drivers between
    each other. It opens a big area of possible problems that were not
    considered by people involved in the development and review.
    
    printk() is crucial for debugging kernel issues and console output is
    very important part of it. The number of consoles is huge and a proper
    review would take some time. As a result it need to be reverted for 5.19.
    
    Link: https://lore.kernel.org/r/YrBdjVwBOVgLfHyb@alley
    Signed-off-by: Petr Mladek <pmladek@suse.com>
    Link: https://lore.kernel.org/r/20220623145157.21938-2-pmladek@suse.com

--- include/linux/printk.h
+++ include/linux/printk.h
@@ -174,7 +174,6 @@ extern void printk_prefer_direct_enter(void);
 extern void printk_prefer_direct_exit(void);
 
 extern bool pr_flush(int timeout_ms, bool reset_on_progress);
-extern void try_block_console_kthreads(int timeout_ms);
 
 /*
  * Please don't use printk_ratelimit(), because it shares ratelimiting state
@@ -237,10 +236,6 @@ static inline void printk_prefer_direct_exit(void)
 static inline bool pr_flush(int timeout_ms, bool reset_on_progress)
 {
 	return true;
-}
-
-static inline void try_block_console_kthreads(int timeout_ms)
-{
 }
 
 static inline int printk_ratelimit(void)
