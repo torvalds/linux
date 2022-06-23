e77804158b30b5c7a0638062ab8adde625104d3b
Nicolas Dichtel <nicolas.dichtel@6wind.com>
2022-06-23 14:00:15 +0200

    ipv6: take care of disable_policy when restoring routes
    commit 3b0dc529f56b5f2328244130683210be98f16f7f upstream.
    
    When routes corresponding to addresses are restored by
    fixup_permanent_addr(), the dst_nopolicy parameter was not set.
    The typical use case is a user that configures an address on a down
    interface and then put this interface up.
    
    Let's take care of this flag in addrconf_f6i_alloc(), so that every callers
    benefit ont it.
    
    CC: stable@kernel.org
    CC: David Forster <dforster@brocade.com>
    Fixes: df789fe75206 ("ipv6: Provide ipv6 version of "disable_policy" sysctl")
    Reported-by: Siwar Zitouni <siwar.zitouni@6wind.com>
    Signed-off-by: Nicolas Dichtel <nicolas.dichtel@6wind.com>
    Reviewed-by: David Ahern <dsahern@kernel.org>
    Link: https://lore.kernel.org/r/20220623120015.32640-1-nicolas.dichtel@6wind.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -1102,10 +1102,6 @@ ipv6_add_addr(struct inet6_dev *idev, struct ifa6_config *cfg,
 		goto out;
 	}
 
-	if (net->ipv6.devconf_all->disable_policy ||
-	    idev->cnf.disable_policy)
-		f6i->dst_nopolicy = true;
-
 	neigh_parms_data_state_setall(idev->nd_parms);
 
 	ifa->addr = *cfg->pfx;
