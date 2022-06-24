475031b6ed43d208925c81bea612f48c3259c3c8
Ard Biesheuvel <ardb@kernel.org>
2022-06-24 17:06:31 +0200

    arm64: head: move kimage_vaddr variable into C file
    This variable definition does not need to be in head.S so move it out.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Link: https://lore.kernel.org/r/20220624150651.1358849-2-ardb@kernel.org
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -49,6 +49,9 @@ u64 idmap_ptrs_per_pgd = PTRS_PER_PGD;
 u64 __section(".mmuoff.data.write") vabits_actual;
 EXPORT_SYMBOL(vabits_actual);
 
+u64 kimage_vaddr __ro_after_init = (u64)&_text;
+EXPORT_SYMBOL(kimage_vaddr);
+
 u64 kimage_voffset __ro_after_init;
 EXPORT_SYMBOL(kimage_voffset);
 
