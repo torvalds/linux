ad8b261d837400cb7ccc339e81d7c35ab018acd8
Pavel Begunkov <asml.silence@gmail.com>
2022-06-25 11:53:00 +0100

    io_uring: remove extra TIF_NOTIFY_SIGNAL check
    io_run_task_work() accounts for TIF_NOTIFY_SIGNAL, so no need to have an
    second check in io_run_task_work_sig().
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/52ce41a592ad904511697f432141e5690fd4b968.1656153285.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -2205,8 +2205,6 @@ int io_run_task_work_sig(void)
 {
 	if (io_run_task_work())
 		return 1;
-	if (test_thread_flag(TIF_NOTIFY_SIGNAL))
-		return -ERESTARTSYS;
 	if (task_sigpending(current))
 		return -EINTR;
 	return 0;
