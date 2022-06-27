e09db97889ec647ad373f7a7422c83099c6120c5
Mikko Perttunen <mperttunen@nvidia.com>
2022-06-27 17:19:53 +0300

    drm/tegra: Support context isolation
    For engines that support context isolation, allocate a context when
    opening a channel, and set up stream ID offset and context fields
    when submitting a job.
    
    As of this commit, the stream ID offset and fallback stream ID
    are not used when context isolation is disabled. However, with
    upcoming patches that enable a full featured job opcode sequence,
    these will be necessary.
    
    Signed-off-by: Mikko Perttunen <mperttunen@nvidia.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/drm.h
+++ drivers/gpu/drm/tegra/drm.h
@@ -80,6 +80,7 @@ struct tegra_drm_context {
 
 	/* Only used by new UAPI. */
 	struct xarray mappings;
+	struct host1x_memory_context *memory_context;
 };
 
 struct tegra_drm_client_ops {
@@ -91,6 +92,8 @@ struct tegra_drm_client_ops {
 	int (*submit)(struct tegra_drm_context *context,
 		      struct drm_tegra_submit *args, struct drm_device *drm,
 		      struct drm_file *file);
+	int (*get_streamid_offset)(struct tegra_drm_client *client, u32 *offset);
+	int (*can_use_memory_ctx)(struct tegra_drm_client *client, bool *supported);
 };
 
 int tegra_drm_submit(struct tegra_drm_context *context,
