8f2e02394dc907f5e0140bfab80a9aa11e3449ed
Michael Ellerman <mpe@ellerman.id.au>
2022-06-28 00:02:30 +1000

    selftests/powerpc: Don't save CR by default in asm helpers
    Thare are some asm helpers for creating/popping stack frames in
    basic_asm.h. They always save/restore CR, but none of the selftests
    tests touch non-volatile CR fields, so it's unnecessary to save them by
    default.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20220627140239.2464900-4-mpe@ellerman.id.au

--- tools/testing/selftests/powerpc/include/basic_asm.h
+++ tools/testing/selftests/powerpc/include/basic_asm.h
@@ -59,14 +59,10 @@
 	mflr	r0; \
 	std	r0, STACK_FRAME_LR_POS(%r1); \
 	stdu	%r1, -(((_extra + 15) & ~15) + STACK_FRAME_MIN_SIZE)(%r1); \
-	mfcr	r0; \
-	stw	r0, STACK_FRAME_CR_POS(%r1); \
 	std	%r2, STACK_FRAME_TOC_POS(%r1);
 
 #define POP_BASIC_STACK(_extra) \
 	ld	%r2, STACK_FRAME_TOC_POS(%r1); \
-	lwz	r0, STACK_FRAME_CR_POS(%r1); \
-	mtcr	r0; \
 	addi	%r1, %r1, (((_extra + 15) & ~15) + STACK_FRAME_MIN_SIZE); \
 	ld	r0, STACK_FRAME_LR_POS(%r1); \
 	mtlr	r0;
