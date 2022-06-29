4e3299eaddffd9d7d5b8bae28ad700bb775f02d0
Jason A. Donenfeld <Jason@zx2c4.com>
2022-06-29 15:06:59 +0200

    fs: do not compare against ->llseek
    Now vfs_llseek() can simply check for FMODE_LSEEK; if it's set,
    we know that ->llseek() won't be NULL and if it's not we should
    just fail with -ESPIPE.
    
    A couple of other places where we used to check for special
    values of ->llseek() (somewhat inconsistently) switched to
    checking FMODE_LSEEK.
    
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/read_write.c
+++ fs/read_write.c
@@ -290,14 +290,9 @@ EXPORT_SYMBOL(default_llseek);
 
 loff_t vfs_llseek(struct file *file, loff_t offset, int whence)
 {
-	loff_t (*fn)(struct file *, loff_t, int);
-
-	fn = no_llseek;
-	if (file->f_mode & FMODE_LSEEK) {
-		if (file->f_op->llseek)
-			fn = file->f_op->llseek;
-	}
-	return fn(file, offset, whence);
+	if (!(file->f_mode & FMODE_LSEEK))
+		return -ESPIPE;
+	return file->f_op->llseek(file, offset, whence);
 }
 EXPORT_SYMBOL(vfs_llseek);
 
