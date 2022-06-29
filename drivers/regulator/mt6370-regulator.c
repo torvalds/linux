46ae6fb58fd49c4cb2d1ca34e87d603b7c96ba97
ChiYuan Huang <cy_huang@richtek.com>
2022-06-29 09:41:01 +0800

    regulator: mt6370: Use the correct header for platform_device_id
    'platform_device_id' struct is defined in 'mod_devicetable.h'.
    Even 'of.h' also includes this header usage. The 'of.h' cannot be removed
    due to 'of_match_ptr' function.
    
    Signed-off-by: ChiYuan Huang <cy_huang@richtek.com>
    Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Link: https://lore.kernel.org/r/1656466861-7737-2-git-send-email-u0084500@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/mt6370-regulator.c
+++ drivers/regulator/mt6370-regulator.c
@@ -4,6 +4,7 @@
 #include <linux/gpio/consumer.h>
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
+#include <linux/mod_devicetable.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
