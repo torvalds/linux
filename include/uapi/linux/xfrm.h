5e25c25aa2c08fb9a79476e029c0b1e3dcd70566
Petr Vaněk <arkamar@atlas.cz>
2022-06-30 16:27:20 +0200

    xfrm: improve wording of comment above XFRM_OFFLOAD flags
    I have noticed a few minor wording issues in a comment recently added
    above XFRM_OFFLOAD flags in 7c76ecd9c99b ("xfrm: enforce validity of
    offload input flags").
    
    Signed-off-by: Petr Vaněk <arkamar@atlas.cz>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- include/uapi/linux/xfrm.h
+++ include/uapi/linux/xfrm.h
@@ -511,9 +511,9 @@ struct xfrm_user_offload {
 	int				ifindex;
 	__u8				flags;
 };
-/* This flag was exposed without any kernel code that supporting it.
- * Unfortunately, strongswan has the code that uses sets this flag,
- * which makes impossible to reuse this bit.
+/* This flag was exposed without any kernel code that supports it.
+ * Unfortunately, strongswan has the code that sets this flag,
+ * which makes it impossible to reuse this bit.
  *
  * So leave it here to make sure that it won't be reused by mistake.
  */
