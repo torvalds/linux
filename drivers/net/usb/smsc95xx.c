03b3df43ce1f64b2ec6ef8cbcf2006f3938643b2
Lukas Wunner <lukas@wunner.de>
2022-07-01 22:47:53 +0200

    usbnet: smsc95xx: Clean up unnecessary BUG_ON() upon register access
    smsc95xx_read_reg() and smsc95xx_write_reg() call BUG_ON() if the
    struct usbnet pointer passed in is NULL.
    
    The functions have just been amended to dereference the pointer on
    entry.  So the kernel now oopses if the pointer is NULL, eliminating
    the need for an explicit BUG_ON().
    
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/smsc95xx.c
+++ drivers/net/usb/smsc95xx.c
@@ -86,8 +86,6 @@ static int __must_check smsc95xx_read_reg(struct usbnet *dev, u32 index,
 	int ret;
 	int (*fn)(struct usbnet *, u8, u8, u16, u16, void *, u16);
 
-	BUG_ON(!dev);
-
 	if (current != pdata->pm_task)
 		fn = usbnet_read_cmd;
 	else
@@ -116,8 +114,6 @@ static int __must_check smsc95xx_write_reg(struct usbnet *dev, u32 index,
 	u32 buf;
 	int ret;
 	int (*fn)(struct usbnet *, u8, u8, u16, u16, const void *, u16);
-
-	BUG_ON(!dev);
 
 	if (current != pdata->pm_task)
 		fn = usbnet_write_cmd;
