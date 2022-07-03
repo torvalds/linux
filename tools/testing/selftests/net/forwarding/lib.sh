8e5fcfecd99a1ce881aaeba031b1a7faccfefc7a
Vladimir Oltean <vladimir.oltean@nxp.com>
2022-07-03 10:36:24 +0300

    selftests: forwarding: fix flood_unicast_test when h2 supports IFF_UNICAST_FLT
    [ Upstream commit b8e629b05f5d23f9649c901bef09fab8b0c2e4b9 ]
    
    As mentioned in the blamed commit, flood_unicast_test() works by
    checking the match count on a tc filter placed on the receiving
    interface.
    
    But the second host interface (host2_if) has no interest in receiving a
    packet with MAC DA de:ad:be:ef:13:37, so its RX filter drops it even
    before the ingress tc filter gets to be executed. So we will incorrectly
    get the message "Packet was not flooded when should", when in fact, the
    packet was flooded as expected but dropped due to an unrelated reason,
    at some other layer on the receiving side.
    
    Force h2 to accept this packet by temporarily placing it in promiscuous
    mode. Alternatively we could either deliver to its MAC address or use
    tcpdump_start, but this has the fewest complications.
    
    This fixes the "flooding" test from bridge_vlan_aware.sh and
    bridge_vlan_unaware.sh, which calls flood_test from the lib.
    
    Fixes: 236dd50bf67a ("selftests: forwarding: Add a test for flooded traffic")
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Reviewed-by: Ido Schimmel <idosch@nvidia.com>
    Tested-by: Ido Schimmel <idosch@nvidia.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/testing/selftests/net/forwarding/lib.sh
+++ tools/testing/selftests/net/forwarding/lib.sh
@@ -1215,6 +1215,7 @@ flood_test_do()
 
 	# Add an ACL on `host2_if` which will tell us whether the packet
 	# was flooded to it or not.
+	ip link set $host2_if promisc on
 	tc qdisc add dev $host2_if ingress
 	tc filter add dev $host2_if ingress protocol ip pref 1 handle 101 \
 		flower dst_mac $mac action drop
@@ -1232,6 +1233,7 @@ flood_test_do()
 
 	tc filter del dev $host2_if ingress protocol ip pref 1 handle 101 flower
 	tc qdisc del dev $host2_if ingress
+	ip link set $host2_if promisc off
 
 	return $err
 }
