f874e16870cc59be2c6444ea3d02665f942e665a
Dave Chinner <dchinner@redhat.com>
2022-07-03 08:04:55 +0300

    xfs: update superblock counters correctly for !lazysbcount
    commit 6543990a168acf366f4b6174d7bd46ba15a8a2a6 upstream.
    
    Keep the mount superblock counters up to date for !lazysbcount
    filesystems so that when we log the superblock they do not need
    updating in any way because they are already correct.
    
    It's found by what Zorro reported:
    1. mkfs.xfs -f -l lazy-count=0 -m crc=0 $dev
    2. mount $dev $mnt
    3. fsstress -d $mnt -p 100 -n 1000 (maybe need more or less io load)
    4. umount $mnt
    5. xfs_repair -n $dev
    and I've seen no problem with this patch.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Reported-by: Zorro Lang <zlang@redhat.com>
    Reviewed-by: Gao Xiang <hsiangkao@redhat.com>
    Signed-off-by: Gao Xiang <hsiangkao@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Brian Foster <bfoster@redhat.com>
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Acked-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/xfs/xfs_trans.c
+++ fs/xfs/xfs_trans.c
@@ -615,6 +615,9 @@ xfs_trans_unreserve_and_mod_sb(
 
 	/* apply remaining deltas */
 	spin_lock(&mp->m_sb_lock);
+	mp->m_sb.sb_fdblocks += tp->t_fdblocks_delta + tp->t_res_fdblocks_delta;
+	mp->m_sb.sb_icount += idelta;
+	mp->m_sb.sb_ifree += ifreedelta;
 	mp->m_sb.sb_frextents += rtxdelta;
 	mp->m_sb.sb_dblocks += tp->t_dblocks_delta;
 	mp->m_sb.sb_agcount += tp->t_agcount_delta;
