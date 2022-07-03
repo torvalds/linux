2fcfa72fc13f0203bb676bd1ec30ec85e17855be
Peng Fan <peng.fan@nxp.com>
2022-07-03 17:11:25 +0800

    interconnect: add device managed bulk API
    Add device managed bulk API to simplify driver.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Link: https://lore.kernel.org/r/20220703091132.1412063-4-peng.fan@oss.nxp.com
    Signed-off-by: Georgi Djakov <djakov@kernel.org>

--- include/linux/interconnect.h
+++ include/linux/interconnect.h
@@ -44,6 +44,7 @@ struct icc_path *icc_get(struct device *dev, const int src_id,
 			 const int dst_id);
 struct icc_path *of_icc_get(struct device *dev, const char *name);
 struct icc_path *devm_of_icc_get(struct device *dev, const char *name);
+int devm_of_icc_bulk_get(struct device *dev, int num_paths, struct icc_bulk_data *paths);
 struct icc_path *of_icc_get_by_index(struct device *dev, int idx);
 void icc_put(struct icc_path *path);
 int icc_enable(struct icc_path *path);
@@ -112,6 +113,12 @@ static inline const char *icc_get_name(struct icc_path *path)
 }
 
 static inline int of_icc_bulk_get(struct device *dev, int num_paths, struct icc_bulk_data *paths)
+{
+	return 0;
+}
+
+static inline int devm_of_icc_bulk_get(struct device *dev, int num_paths,
+				       struct icc_bulk_data *paths)
 {
 	return 0;
 }
