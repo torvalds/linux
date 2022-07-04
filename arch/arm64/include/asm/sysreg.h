d1b60bed639bd5ea6c4a80d43816e05c16c0ec6c
Mark Brown <broonie@kernel.org>
2022-07-04 18:02:53 +0100

    arm64/sysreg: Convert GMID to automatic generation
    Automatically generate the register definitions for GMID as per DDI0487H.a,
    no functional change.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20220704170302.2609529-20-broonie@kernel.org
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/sysreg.h
+++ arch/arm64/include/asm/sysreg.h
@@ -454,7 +454,6 @@
 #define SYS_CNTKCTL_EL1			sys_reg(3, 0, 14, 1, 0)
 
 #define SYS_CCSIDR_EL1			sys_reg(3, 1, 0, 0, 0)
-#define SYS_GMID_EL1			sys_reg(3, 1, 0, 0, 4)
 #define SYS_AIDR_EL1			sys_reg(3, 1, 0, 0, 7)
 
 #define SMIDR_EL1_IMPLEMENTER_SHIFT	24
