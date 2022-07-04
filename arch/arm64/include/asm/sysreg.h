464ca8df6248eced7bce5309c344f62b08a1d5c0
Mark Brown <broonie@kernel.org>
2022-07-04 18:02:56 +0100

    arm64/sysreg: Convert LORSA_EL1 to automatic generation
    Convert LORSA_EL1 to automatic register generation as per DDI0487H.a, no
    functional changes.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20220704170302.2609529-23-broonie@kernel.org
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/sysreg.h
+++ arch/arm64/include/asm/sysreg.h
@@ -407,7 +407,6 @@
 #define SYS_MAIR_EL1			sys_reg(3, 0, 10, 2, 0)
 #define SYS_AMAIR_EL1			sys_reg(3, 0, 10, 3, 0)
 
-#define SYS_LORSA_EL1			sys_reg(3, 0, 10, 4, 0)
 #define SYS_LOREA_EL1			sys_reg(3, 0, 10, 4, 1)
 #define SYS_LORN_EL1			sys_reg(3, 0, 10, 4, 2)
 #define SYS_LORC_EL1			sys_reg(3, 0, 10, 4, 3)
