0316f99c4780b0a5fd60b7f136c64cb1af8d5fc3
Cristian Marussi <cristian.marussi@arm.com>
2022-07-04 11:22:36 +0100

    firmware: arm_scmi: Add SCMI v3.1 powercap protocol basic support
    Add support for SCMI v3.1 powercap protocol, with the exception of powercap
    fast channels, exposing all the new related powercap protocol operations as
    usual in include/linux/scmi_protocol.h.
    
    Link: https://lore.kernel.org/r/20220704102241.2988447-3-cristian.marussi@arm.com
    Signed-off-by: Cristian Marussi <cristian.marussi@arm.com>
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>

--- drivers/firmware/arm_scmi/driver.c
+++ drivers/firmware/arm_scmi/driver.c
@@ -2485,6 +2485,7 @@ static int __init scmi_driver_init(void)
 	scmi_sensors_register();
 	scmi_voltage_register();
 	scmi_system_register();
+	scmi_powercap_register();
 
 	return platform_driver_register(&scmi_driver);
 }
@@ -2501,6 +2502,7 @@ static void __exit scmi_driver_exit(void)
 	scmi_sensors_unregister();
 	scmi_voltage_unregister();
 	scmi_system_unregister();
+	scmi_powercap_unregister();
 
 	scmi_bus_exit();
 
