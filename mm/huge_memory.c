749290799e72f05f7311ec8e85a47664dd7be37e
Miaohe Lin <linmiaohe@huawei.com>
2022-07-04 21:21:54 +0800

    mm/huge_memory: fix comment in zap_huge_pud
    The comment about deposited pgtable is borrowed from zap_huge_pmd but
    there's no deposited pgtable stuff for huge pud in zap_huge_pud. Remove
    it to avoid confusion.
    
    Link: https://lkml.kernel.org/r/20220704132201.14611-10-linmiaohe@huawei.com
    Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Muchun Song <songmuchun@bytedance.com>
    Cc: Yang Shi <shy828301@gmail.com>
    Cc: Zach O'Keefe <zokeefe@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -1913,12 +1913,7 @@ int zap_huge_pud(struct mmu_gather *tlb, struct vm_area_struct *vma,
 	ptl = __pud_trans_huge_lock(pud, vma);
 	if (!ptl)
 		return 0;
-	/*
-	 * For architectures like ppc64 we look at deposited pgtable
-	 * when calling pudp_huge_get_and_clear. So do the
-	 * pgtable_trans_huge_withdraw after finishing pudp related
-	 * operations.
-	 */
+
 	pudp_huge_get_and_clear_full(tlb->mm, addr, pud, tlb->fullmm);
 	tlb_remove_pud_tlb_entry(tlb, pud, addr);
 	if (vma_is_special_huge(vma)) {
