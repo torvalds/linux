dc86c129b4fb5c387b0678cfb6081ef29809cc41
Siddharth Gupta <sidgup@codeaurora.org>
2022-07-05 17:38:16 +0530

    remoteproc: qcom: pas: Mark devices as wakeup capable
    device_wakeup_enable() on its own is not capable of setting
    device as wakeup capable, it needs to be used in conjunction
    with device_set_wakeup_capable(). The device_init_wakeup()
    calls both these functions on the device passed.
    
    Fixes: a781e5aa5911 ("remoteproc: core: Prevent system suspend during remoteproc recovery")
    Signed-off-by: Siddharth Gupta <sidgup@codeaurora.org>
    Signed-off-by: Sibi Sankar <quic_sibis@quicinc.com>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Link: https://lore.kernel.org/r/1657022900-2049-4-git-send-email-quic_sibis@quicinc.com

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -486,7 +486,9 @@ static int adsp_probe(struct platform_device *pdev)
 	adsp->decrypt_shutdown = desc->decrypt_shutdown;
 	platform_set_drvdata(pdev, adsp);
 
-	device_wakeup_enable(adsp->dev);
+	ret = device_init_wakeup(adsp->dev, true);
+	if (ret)
+		goto free_rproc;
 
 	ret = adsp_alloc_memory_region(adsp);
 	if (ret)
