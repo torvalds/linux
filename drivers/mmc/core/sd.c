0684658366527f206b0812305d94087e40404dca
ChanWoo Lee <cw9316.lee@samsung.com>
2022-07-06 09:48:40 +0900

    mmc: core: Replace with already defined values for readability
    [ Upstream commit e427266460826bea21b70f9b2bb29decfb2c2620 ]
    
    SD_ROCR_S18A is already defined and is used to check the rocr value, so
    let's replace with already defined values for readability.
    
    Signed-off-by: ChanWoo Lee <cw9316.lee@samsung.com>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://lore.kernel.org/r/20220706004840.24812-1-cw9316.lee@samsung.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Stable-dep-of: e9233917a7e5 ("mmc: core: Terminate infinite loop in SD-UHS voltage switch")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -853,7 +853,7 @@ try_again:
 	 * the CCS bit is set as well. We deliberately deviate from the spec in
 	 * regards to this, which allows UHS-I to be supported for SDSC cards.
 	 */
-	if (!mmc_host_is_spi(host) && rocr && (*rocr & 0x01000000)) {
+	if (!mmc_host_is_spi(host) && rocr && (*rocr & SD_ROCR_S18A)) {
 		err = mmc_set_uhs_voltage(host, pocr);
 		if (err == -EAGAIN) {
 			retries--;
