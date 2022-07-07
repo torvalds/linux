1a63d33006dc083be36efb4ddbca7a1e8e0fb6bf
Rob Clark <robdclark@chromium.org>
2022-07-07 14:20:00 -0700

    drm/msm/dpu: Fix for non-visible planes
    [ Upstream commit cb77085b1f0a86ef9dfba86b5f3ed6c3340c2ea3 ]
    
    Fixes `kms_cursor_crc --run-subtest cursor-offscreen`.. when the cursor
    moves offscreen the plane becomes non-visible, so we need to skip over
    it in crtc atomic test and mixer setup.
    
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Fixes: 25fdd5933e4c ("drm/msm: Add SDM845 DPU support")
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Reviewed-by: Abhinav Kumar <quic_abhinavk@quicinc.com>
    Patchwork: https://patchwork.freedesktop.org/patch/492819/
    Link: https://lore.kernel.org/r/20220707212003.1710163-1-robdclark@gmail.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c
@@ -230,6 +230,9 @@ static void _dpu_crtc_blend_setup_mixer(struct drm_crtc *crtc,
 		if (!state)
 			continue;
 
+		if (!state->visible)
+			continue;
+
 		pstate = to_dpu_plane_state(state);
 		fb = state->fb;
 
@@ -974,6 +977,9 @@ static int dpu_crtc_atomic_check(struct drm_crtc *crtc,
 			goto end;
 		}
 		if (cnt >= DPU_STAGE_MAX * 4)
+			continue;
+
+		if (!pstate->visible)
 			continue;
 
 		pstates[cnt].dpu_pstate = dpu_pstate;
