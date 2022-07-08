dc87dfd7070ec37f75db5aa73e99d12411cb42ee
Venkatakrishnaiah Pari <quic_vpari@quicinc.com>
2022-07-07 23:15:46 -0700

    iommu: arm-smmu: Add support for hibernation
    Add support for hibernation use case with hibernation
    freeze_late,thaw_early and  restore_early callbacks.
    
    Change-Id: I6074029bb778954d00b2a6ad136874a6846b4b10
    Signed-off-by: Venkatakrishnaiah Pari <quic_vpari@quicinc.com>
    Signed-off-by: Darshankumar Jagdishchandra Thakkar <quic_djagdish@quicinc.com>

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -3777,10 +3777,14 @@ static int arm_smmu_pm_prepare(struct device *dev)
 }
 
 static const struct dev_pm_ops arm_smmu_pm_ops = {
-	SET_SYSTEM_SLEEP_PM_OPS(arm_smmu_pm_suspend, arm_smmu_pm_resume)
 	SET_RUNTIME_PM_OPS(arm_smmu_runtime_suspend,
 			   arm_smmu_runtime_resume, NULL)
 	.prepare = arm_smmu_pm_prepare,
+	.suspend  = arm_smmu_pm_suspend,
+	.resume   = arm_smmu_pm_resume,
+	.thaw_early = arm_smmu_pm_resume,
+	.freeze_late = arm_smmu_pm_suspend,
+	.restore_early = arm_smmu_pm_resume,
 };
 
 static struct platform_driver arm_smmu_driver = {
