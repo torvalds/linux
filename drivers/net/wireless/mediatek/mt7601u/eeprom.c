07db88f11e63d78c5062447faf942745ce8959ff
Justin Stitt <justinstitt@google.com>
2022-07-08 17:15:27 -0700

    wifi: mt7601u: eeprom: fix clang -Wformat warning
    When building with Clang we encounter the following warning:
    | drivers/net/wireless/mediatek/mt7601u/eeprom.c:193:5: error: format
    | specifies type 'char' but the argument has type 'int' [-Werror,-Wformat]
    | chan_bounds[idx].start + chan_bounds[idx].num - 1);
    
    Variadic functions (printf-like) undergo default argument promotion.
    Documentation/core-api/printk-formats.rst specifically recommends using
    the promoted-to-type's format flag.
    
    Moreover, C11 6.3.1.1 states:
    (https://www.open-std.org/jtc1/sc22/wg14/www/docs/n1548.pdf) `If an int
    can represent all values of the original type ..., the value is
    converted to an int; otherwise, it is converted to an unsigned int.
    These are called the integer promotions.`
    
    With this information in hand, we really should stop using `%hh[dxu]` or
    `%h[dxu]` as they usually prompt Clang -Wformat warnings as well as go
    against documented standard recommendations.
    
    Link: https://github.com/ClangBuiltLinux/linux/issues/378
    Signed-off-by: Justin Stitt <justinstitt@google.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://lore.kernel.org/r/20220709001527.618593-1-justinstitt@google.com

--- drivers/net/wireless/mediatek/mt7601u/eeprom.c
+++ drivers/net/wireless/mediatek/mt7601u/eeprom.c
@@ -188,7 +188,7 @@ mt7601u_set_country_reg(struct mt7601u_dev *dev, u8 *eeprom)
 
 	if (idx != -1)
 		dev_info(dev->dev,
-			 "EEPROM country region %02hhx (channels %hhd-%hhd)\n",
+			 "EEPROM country region %02x (channels %d-%d)\n",
 			 val, chan_bounds[idx].start,
 			 chan_bounds[idx].start + chan_bounds[idx].num - 1);
 	else
