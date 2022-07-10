53a2a90d5271ed4718a70f8e13faaaf9f9ba9eb6
Liam Beguin <liambeguin@gmail.com>
2022-07-09 21:31:05 -0400

    iio: test: rescale: add MODULE_* information
    In preparation for module support, add missing MODULE_* information.
    
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>
    Link: https://lore.kernel.org/r/20220710013109.3349104-2-liambeguin@gmail.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/test/iio-test-rescale.c
+++ drivers/iio/test/iio-test-rescale.c
@@ -708,3 +708,7 @@ static struct kunit_suite iio_rescale_test_suite = {
 	.test_cases = iio_rescale_test_cases,
 };
 kunit_test_suite(iio_rescale_test_suite);
+
+MODULE_AUTHOR("Liam Beguin <liambeguin@gmail.com>");
+MODULE_DESCRIPTION("Test IIO rescale conversion functions");
+MODULE_LICENSE("GPL v2");
