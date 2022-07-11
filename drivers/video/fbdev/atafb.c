3ee5e2280343472de16a9048d6b9049c66d572e3
Geert Uytterhoeven <geert@linux-m68k.org>
2022-07-11 17:50:30 +0200

    video: fbdev: atari: Remove unneeded casts to void *
    Arbitrary pointers can be passed to functions accepting "void *" without
    casting.
    
    Remove the casts, as they make it impossible to validate types.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Tested-by: Michael Schmitz <schmitzmic@gmail.com>
    Signed-off-by: Helge Deller <deller@gmx.de>

--- drivers/video/fbdev/atafb.c
+++ drivers/video/fbdev/atafb.c
@@ -2599,14 +2599,14 @@ atafb_ioctl(struct fb_info *info, unsigned int cmd, unsigned long arg)
 	switch (cmd) {
 #ifdef FBCMD_GET_CURRENTPAR
 	case FBCMD_GET_CURRENTPAR:
-		if (copy_to_user((void *)arg, (void *)&current_par,
+		if (copy_to_user((void *)arg, &current_par,
 				 sizeof(struct atafb_par)))
 			return -EFAULT;
 		return 0;
 #endif
 #ifdef FBCMD_SET_CURRENTPAR
 	case FBCMD_SET_CURRENTPAR:
-		if (copy_from_user((void *)&current_par, (void *)arg,
+		if (copy_from_user(&current_par, (void *)arg,
 				   sizeof(struct atafb_par)))
 			return -EFAULT;
 		ata_set_par(&current_par);
