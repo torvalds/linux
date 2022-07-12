ab5f3404b7762b88403fbddbdda6b1b464bd6cbc
Guenter Roeck <linux@roeck-us.net>
2022-07-12 15:17:05 -0700

    HID: nintendo: Add missing array termination
    joycon_dpad_inputs_jc[] is unterminated. This may result in odd warnings
    such as
    
    input: input_set_capability: invalid code 3077588140 for type 1
    
    or in kernel crashes in nintendo_hid_probe(). Terminate the array to fix
    the problem.
    
    Fixes: 2af16c1f846bd ("HID: nintendo: add nintendo switch controller driver")
    Cc: Daniel J. Ogorchock <djogorchock@gmail.com>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Reviewed-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-nintendo.c
+++ drivers/hid/hid-nintendo.c
@@ -1585,6 +1585,7 @@ static const unsigned int joycon_button_inputs_r[] = {
 /* We report joy-con d-pad inputs as buttons and pro controller as a hat. */
 static const unsigned int joycon_dpad_inputs_jc[] = {
 	BTN_DPAD_UP, BTN_DPAD_DOWN, BTN_DPAD_LEFT, BTN_DPAD_RIGHT,
+	0 /* 0 signals end of array */
 };
 
 static int joycon_input_create(struct joycon_ctlr *ctlr)
