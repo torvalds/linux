680b86da10a23268b24d65b567830824075eb151
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2022-07-12 13:59:19 +0100

    clk: qcom: gcc-msm8939: Fix venus0_vcodec0_clk frequency definitions
    The Venus clock frequencies are a copy/paste error from msm8916. Looking
    at the original clock-gcc-8936.c ftbl_gcc_venus0_vcodec0_clk defines we
    have:
    
    - 133 MHz
    - 200 MHz
    - 266 MHz
    
    These values are born out by the relevant qualcomm documentation for the
    msm8936/msm8939 Venus core performance levels.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Reviewed-by: Konrad Dybcio <konrad.dybcio@somainline.org>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Link: https://lore.kernel.org/r/20220712125922.3461675-4-bryan.odonoghue@linaro.org

--- drivers/clk/qcom/gcc-msm8939.c
+++ drivers/clk/qcom/gcc-msm8939.c
@@ -1836,9 +1836,9 @@ static struct clk_branch gcc_ultaudio_pcnoc_sway_clk = {
 };
 
 static const struct freq_tbl ftbl_gcc_venus0_vcodec0_clk[] = {
-	F(100000000, P_GPLL0, 8, 0, 0),
-	F(160000000, P_GPLL0, 5, 0, 0),
-	F(228570000, P_GPLL0, 3.5, 0, 0),
+	F(133330000, P_GPLL0, 6, 0, 0),
+	F(200000000, P_GPLL0, 4, 0, 0),
+	F(266670000, P_GPLL0, 3, 0, 0),
 	{ }
 };
 
