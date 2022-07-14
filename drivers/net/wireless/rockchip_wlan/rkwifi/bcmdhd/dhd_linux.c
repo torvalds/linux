8106e54bdf979827c46f0dc4d4ca988277de64f9
Tao Huang <huangtao@rock-chips.com>
2022-07-14 19:41:09 +0800

    net: wireless: rockchip: rkwifi: bcmdhd: set vfs-only exports into their own namespace
    According to gki commit d483eed85ffb ("ANDROID: GKI: set vfs-only exports into their own namespace").
    
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>
    Change-Id: Ie75429695e0f23ada39d8677a918e5b1c0b8fd97

--- drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/dhd_linux.c
@@ -370,6 +370,7 @@ extern void dhd_enable_oob_intr(struct dhd_bus *bus, bool enable);
 static void dhd_hang_process(struct work_struct *work_data);
 #endif /* OEM_ANDROID */
 MODULE_LICENSE("GPL and additional rights");
+MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
 
 #if defined(MULTIPLE_SUPPLICANT)
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 25))
