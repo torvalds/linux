d48596f3ef2c0aa2ce7a106dd5ed99045d4ae377
Ville Syrjälä <ville.syrjala@linux.intel.com>
2022-07-15 23:20:36 +0300

    drm/i915: Define VBT eDP/DP max lane count bits
    Since version 244 the VBT can llimt the eDP/DP max lane count.
    Add the bits.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220715202044.11153-5-ville.syrjala@linux.intel.com
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/display/intel_vbt_defs.h
+++ drivers/gpu/drm/i915/display/intel_vbt_defs.h
@@ -438,7 +438,7 @@ struct child_device_config {
 			u8 iboost:1;				/* 196+ */
 			u8 hpd_invert:1;			/* 196+ */
 			u8 use_vbt_vswing:1;			/* 218+ */
-			u8 flag_reserved:2;
+			u8 dp_max_lane_count:2;			/* 244+ */
 			u8 hdmi_support:1;			/* 158+ */
 			u8 dp_support:1;			/* 158+ */
 			u8 tmds_support:1;			/* 158+ */
