ef434f08b0562069cf431873a052692357d325a1
Jae Hyun Yoo <quic_jaehyoo@quicinc.com>
2022-07-15 17:06:43 -0700

    mtd: spi-nor: winbond: add support for W25Q512NW-IQ
    Add support for Winbond W25Q512NW-IQ/IN
    
    Signed-off-by: Jae Hyun Yoo <quic_jaehyoo@quicinc.com>
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    Reviewed-by: Michael Walle <michael@walle.cc>
    Link: https://www.winbond.com/resource-files/W25Q512NW%20RevB%2007192021.pdf
    Link: https://lore.kernel.org/r/20220716000643.3541839-2-quic_jaehyoo@quicinc.com

--- drivers/mtd/spi-nor/winbond.c
+++ drivers/mtd/spi-nor/winbond.c
@@ -133,6 +133,9 @@ static const struct flash_info winbond_nor_parts[] = {
 	{ "w25m512jv", INFO(0xef7119, 0, 64 * 1024, 1024)
 		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_QUAD_READ |
 			      SPI_NOR_DUAL_READ) },
+	{ "w25q512nwq", INFO(0xef6020, 0, 0, 0)
+		PARSE_SFDP
+		OTP_INFO(256, 3, 0x1000, 0x1000) },
 	{ "w25q512nwm", INFO(0xef8020, 0, 64 * 1024, 1024)
 		PARSE_SFDP
 		OTP_INFO(256, 3, 0x1000, 0x1000) },
