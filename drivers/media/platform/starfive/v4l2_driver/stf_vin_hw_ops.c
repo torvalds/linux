1a3fa7dd11aece005d4ff9ae1f3aee6d3d3c8b6d
changhuang.liang <changhuang.liang@starfivetech.com>
2022-07-18 10:31:34 +0800

    v4l2: fixed close wr due to system hung
    Signed-off-by: changhuang.liang <changhuang.liang@starfivetech.com>

--- drivers/media/platform/starfive/v4l2_driver/stf_vin_hw_ops.c
+++ drivers/media/platform/starfive/v4l2_driver/stf_vin_hw_ops.c
@@ -291,16 +291,11 @@ static int stf_vin_wr_stream_set(struct stf_vin2_dev *vin_dev, int on)
 	struct stfcamss *stfcamss = vin_dev->stfcamss;
 	struct stf_vin_dev *vin = vin_dev->stfcamss->vin;
 
-	print_reg(ST_VIN, vin->sysctrl_base, SYSCONSAIF_SYSCFG_20);
+	//make the axiwr alway on
 	if (on) {
 		reset_control_deassert(stfcamss->sys_rst[STFRST_AXIWR].rstc);
 		reg_set(vin->sysctrl_base, SYSCONSAIF_SYSCFG_20, U0_VIN_CNFG_AXIWR0_EN);
-	} else {
-		reg_clear(vin->sysctrl_base, SYSCONSAIF_SYSCFG_20, U0_VIN_CNFG_AXIWR0_EN);
-		reset_control_assert(stfcamss->sys_rst[STFRST_AXIWR].rstc);
 	}
-
-	print_reg(ST_VIN, vin->sysctrl_base, SYSCONSAIF_SYSCFG_20);
 
 	return 0;
 }
