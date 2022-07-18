bc2373a58aacb241bf135b9a7452a1499d0d457e
Kan Liang <kan.liang@linux.intel.com>
2022-07-18 09:43:10 -0700

    perf tsc: Add arch TSC frequency information
    The TSC frequency information is required for the event metrics with the
    literal, system_tsc_freq. For the newer Intel platform, the TSC
    frequency information can be retrieved from the CPUID leaf 0x15.  If the
    TSC frequency information isn't present the /proc/cpuinfo approach is
    used.
    
    Refactor cpuid() for this use. Note, the previous stack pushing/popping
    approach was broken on x86-64 that has stack red zones that would be
    clobbered.
    
    Committer testing:
    
    Before:
    
      $ perf record sleep 0.0001
      [ perf record: Woken up 1 times to write data ]
      $ perf report --header-only |& grep cpuid
      # cpuid : AuthenticAMD,25,33,0
      $
    
    After the patch:
    
      $ perf record sleep 0.0001
      [ perf record: Woken up 1 times to write data ]
      [ perf record: Captured and wrote 0.002 MB perf.data (8 samples) ]
      $ perf report --header-only |& grep cpuid
      # cpuid : AuthenticAMD,25,33,0
      $
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Alexandre Torgue <alexandre.torgue@foss.st.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Caleb Biggers <caleb.biggers@intel.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: James Clark <james.clark@arm.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: John Garry <john.garry@huawei.com>
    Cc: Kshipra Bopardikar <kshipra.bopardikar@intel.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Maxime Coquelin <mcoquelin.stm32@gmail.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Perry Taylor <perry.taylor@intel.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Xing Zhengjun <zhengjun.xing@linux.intel.com>
    Link: https://lore.kernel.org/r/20220718164312.3994191-2-irogers@google.com
    Signed-off-by: Ian Rogers <irogers@google.com>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/arch/x86/util/tsc.c
+++ tools/perf/arch/x86/util/tsc.c
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/types.h>
+#include <string.h>
 
 #include "../../../util/tsc.h"
+#include "cpuid.h"
 
 u64 rdtsc(void)
 {
@@ -10,4 +12,35 @@ u64 rdtsc(void)
 	asm volatile("rdtsc" : "=a" (low), "=d" (high));
 
 	return low | ((u64)high) << 32;
+}
+
+double arch_get_tsc_freq(void)
+{
+	unsigned int a, b, c, d, lvl;
+	static bool cached;
+	static double tsc;
+	char vendor[16];
+
+	if (cached)
+		return tsc;
+
+	cached = true;
+	get_cpuid_0(vendor, &lvl);
+	if (!strstr(vendor, "Intel"))
+		return 0;
+
+	/*
+	 * Don't support Time Stamp Counter and
+	 * Nominal Core Crystal Clock Information Leaf.
+	 */
+	if (lvl < 0x15)
+		return 0;
+
+	cpuid(0x15, 0, &a, &b, &c, &d);
+	/* TSC frequency is not enumerated */
+	if (!a || !b || !c)
+		return 0;
+
+	tsc = (double)c * (double)b / (double)a;
+	return tsc;
 }
