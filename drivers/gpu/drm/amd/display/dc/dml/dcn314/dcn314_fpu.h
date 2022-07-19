4c3861f587400db00801810eb8034c7b480e21dd
Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>
2022-07-19 16:28:50 -0400

    drm/amd/display: Create FPU files for DCN314
    DCN314 has multiple references to FPU operations inside the resource
    files, and we need to move those codes to the DML folder. This commit
    creates the dcn314_fpu files and moves the bounding box operation to
    this file.
    
    Reviewed-by: Nicholas Kazlauskas <Nicholas.Kazlauskas@amd.com>
    Acked-by: Tom Chung <chiahsuan.chung@amd.com>
    Signed-off-by: Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dml/dcn314/dcn314_fpu.h
+++ drivers/gpu/drm/amd/display/dc/dml/dcn314/dcn314_fpu.h
@@ -24,23 +24,14 @@
  *
  */
 
-#ifndef _DCN314_RESOURCE_H_
-#define _DCN314_RESOURCE_H_
+#ifndef __DCN314_FPU_H__
+#define __DCN314_FPU_H__
 
-#include "core_types.h"
+#define DCN3_14_DEFAULT_DET_SIZE 384
+#define DCN3_14_MAX_DET_SIZE 384
+#define DCN3_14_MIN_COMPBUF_SIZE_KB 128
+#define DCN3_14_CRB_SEGMENT_SIZE_KB 64
 
-extern struct _vcs_dpi_ip_params_st dcn3_14_ip;
-extern struct _vcs_dpi_soc_bounding_box_st dcn3_14_soc;
+void dcn314_update_bw_bounding_box_fpu(struct dc *dc, struct clk_bw_params *bw_params);
 
-#define TO_DCN314_RES_POOL(pool)\
-	container_of(pool, struct dcn314_resource_pool, base)
-
-struct dcn314_resource_pool {
-	struct resource_pool base;
-};
-
-struct resource_pool *dcn314_create_resource_pool(
-		const struct dc_init_data *init_data,
-		struct dc *dc);
-
-#endif /* _DCN314_RESOURCE_H_ */
+#endif
