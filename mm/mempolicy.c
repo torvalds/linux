6d97cf88ddde9c976d04b886b10b464ec8006c85
Miaohe Lin <linmiaohe@huawei.com>
2022-07-19 19:52:33 +0800

    mm/mempolicy: remove unneeded out label
    We can use unlock label to unlock ptl and return ret directly to remove
    the unneeded out label and reduce the size of mempolicy.o.  No functional
    change intended.
    
    [Before]
       text	   data	    bss	    dec	    hex	filename
      26702	   3972	   6168	  36842	   8fea	mm/mempolicy.o
    
    [After]
       text	   data	    bss	    dec	    hex	filename
      26662	   3972	   6168	  36802	   8fc2	mm/mempolicy.o
    
    Link: https://lkml.kernel.org/r/20220719115233.6706-1-linmiaohe@huawei.com
    Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/mempolicy.c
+++ mm/mempolicy.c
@@ -465,9 +465,8 @@ static int queue_pages_pmd(pmd_t *pmd, spinlock_t *ptl, unsigned long addr,
 	}
 	page = pmd_page(*pmd);
 	if (is_huge_zero_page(page)) {
-		spin_unlock(ptl);
 		walk->action = ACTION_CONTINUE;
-		goto out;
+		goto unlock;
 	}
 	if (!queue_pages_required(page, qp))
 		goto unlock;
@@ -484,7 +483,6 @@ static int queue_pages_pmd(pmd_t *pmd, spinlock_t *ptl, unsigned long addr,
 		ret = -EIO;
 unlock:
 	spin_unlock(ptl);
-out:
 	return ret;
 }
 
