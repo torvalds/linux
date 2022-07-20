12198d9179aaa53d0a4026318d8b73b146d89729
Lukas Bulwahn <lukas.bulwahn@gmail.com>
2022-07-20 10:29:34 +0200

    clk: davinci: remove PLL and PSC clocks for DaVinci DM644x and DM646x
    Commit 7dd33764486d ("ARM: davinci: Delete DM644x board files") and commit
    b4aed01de486 ("ARM: davinci: Delete DM646x board files") removes the
    support for DaVinci DM644x and DM646x boards.
    
    Hence, remove the PLL and PSC clock descriptions for those boards as well.
    
    Signed-off-by: Lukas Bulwahn <lukas.bulwahn@gmail.com>
    Link: https://lore.kernel.org/r/20220720082934.17741-1-lukas.bulwahn@gmail.com
    Reviewed-by: David Lechner <david@lechnology.com>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/davinci/pll.c
+++ drivers/clk/davinci/pll.c
@@ -890,14 +890,6 @@ static const struct platform_device_id davinci_pll_id_table[] = {
 	{ .name = "dm365-pll1",  .driver_data = (kernel_ulong_t)dm365_pll1_init  },
 	{ .name = "dm365-pll2",  .driver_data = (kernel_ulong_t)dm365_pll2_init  },
 #endif
-#ifdef CONFIG_ARCH_DAVINCI_DM644x
-	{ .name = "dm644x-pll1", .driver_data = (kernel_ulong_t)dm644x_pll1_init },
-	{ .name = "dm644x-pll2", .driver_data = (kernel_ulong_t)dm644x_pll2_init },
-#endif
-#ifdef CONFIG_ARCH_DAVINCI_DM646x
-	{ .name = "dm646x-pll1", .driver_data = (kernel_ulong_t)dm646x_pll1_init },
-	{ .name = "dm646x-pll2", .driver_data = (kernel_ulong_t)dm646x_pll2_init },
-#endif
 	{ }
 };
 
