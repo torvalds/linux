1f340e1c1c74d11c45a6e32663829b26acd4f47b
Liang He <windhl@126.com>
2022-07-22 22:43:48 +0800

    drm/omap: dss: Fix refcount leak bugs
    [ Upstream commit 8b42057e62120813ebe9274f508fa785b7cab33a ]
    
    In dss_init_ports() and __dss_uninit_ports(), we should call
    of_node_put() for the reference returned by of_graph_get_port_by_id()
    in fail path or when it is not used anymore.
    
    Fixes: 09bffa6e5192 ("drm: omap: use common OF graph helpers")
    Signed-off-by: Liang He <windhl@126.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220722144348.1306569-1-windhl@126.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/omapdrm/dss/dss.c
+++ drivers/gpu/drm/omapdrm/dss/dss.c
@@ -1173,6 +1173,7 @@ static void __dss_uninit_ports(struct dss_device *dss, unsigned int num_ports)
 		default:
 			break;
 		}
+		of_node_put(port);
 	}
 }
 
@@ -1205,11 +1206,13 @@ static int dss_init_ports(struct dss_device *dss)
 		default:
 			break;
 		}
+		of_node_put(port);
 	}
 
 	return 0;
 
 error:
+	of_node_put(port);
 	__dss_uninit_ports(dss, i);
 	return r;
 }
