5606aeaad01e0b0ad09c021cfa44ee18fc4ae33b
Wataru Gohda <wataru.gohda@cypress.com>
2022-07-22 13:56:29 +0200

    wifi: brcmfmac: Fix to add brcmf_clear_assoc_ies when rmmod
    Conn_info->req_ie/resp_ie is used to indicate the assoc_req_ies /
    assoc_resp_ies to cfg80211 layer when connection is done. The buffer is
    freed and allocated again at next connection establishment. The buffers
    also needs to be freed at the timing of rmmod as well.
    
    Signed-off-by: Wataru Gohda <wataru.gohda@cypress.com>
    Signed-off-by: Chi-hsien Lin <chi-hsien.lin@cypress.com>
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Signed-off-by: Alvin Šipraga <alsi@bang-olufsen.dk>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://lore.kernel.org/r/20220722115632.620681-5-alvin@pqrs.dk

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
@@ -6431,6 +6431,7 @@ static void wl_deinit_priv(struct brcmf_cfg80211_info *cfg)
 	cfg->dongle_up = false;	/* dongle down */
 	brcmf_abort_scanning(cfg);
 	brcmf_deinit_priv_mem(cfg);
+	brcmf_clear_assoc_ies(cfg);
 }
 
 static void init_vif_event(struct brcmf_cfg80211_vif_event *event)
