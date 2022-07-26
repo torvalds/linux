dd0b15bda48f59eb7dee17fab91eda8389f0e98d
Andreas Schwab <schwab@suse.de>
2022-07-26 10:01:22 +0200

    rtla: Define syscall numbers for riscv
    RISC-V uses the same (generic) syscall numbers as ARM64.
    
    Link: https://lkml.kernel.org/r/mvma68wl2ul.fsf@suse.de
    
    Signed-off-by: Andreas Schwab <schwab@suse.de>
    Acked-by: Daniel Bristot de Oliveira <bristot@kernel.org>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- tools/tracing/rtla/src/utils.c
+++ tools/tracing/rtla/src/utils.c
@@ -225,7 +225,7 @@ long parse_ns_duration(char *val)
 #elif __arm__
 # define __NR_sched_setattr	380
 # define __NR_sched_getattr	381
-#elif __aarch64__
+#elif __aarch64__ || __riscv
 # define __NR_sched_setattr	274
 # define __NR_sched_getattr	275
 #elif __powerpc__
