879e5ac7b2e4db05799a905b5a07fc9e5dedf651
Kalesh Singh <kaleshsingh@google.com>
2022-07-26 00:37:42 -0700

    KVM: arm64: Prepare non-protected nVHE hypervisor stacktrace
    In non-protected nVHE mode (non-pKVM) the host can directly access
    hypervisor memory; and unwinding of the hypervisor stacktrace is
    done from EL1 to save on memory for shared buffers.
    
    To unwind the hypervisor stack from EL1 the host needs to know the
    starting point for the unwind and information that will allow it to
    translate hypervisor stack addresses to the corresponding kernel
    addresses. This patch sets up this book keeping. It is made use of
    later in the series.
    
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    Reviewed-by: Fuad Tabba <tabba@google.com>
    Tested-by: Fuad Tabba <tabba@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20220726073750.3219117-10-kaleshsingh@google.com

--- arch/arm64/kvm/hyp/nvhe/stacktrace.c
+++ arch/arm64/kvm/hyp/nvhe/stacktrace.c
@@ -4,8 +4,49 @@
  *
  * Copyright (C) 2022 Google LLC
  */
+#include <asm/kvm_asm.h>
+#include <asm/kvm_hyp.h>
 #include <asm/memory.h>
 #include <asm/percpu.h>
 
 DEFINE_PER_CPU(unsigned long [OVERFLOW_STACK_SIZE/sizeof(long)], overflow_stack)
 	__aligned(16);
+
+DEFINE_PER_CPU(struct kvm_nvhe_stacktrace_info, kvm_stacktrace_info);
+
+/*
+ * hyp_prepare_backtrace - Prepare non-protected nVHE backtrace.
+ *
+ * @fp : frame pointer at which to start the unwinding.
+ * @pc : program counter at which to start the unwinding.
+ *
+ * Save the information needed by the host to unwind the non-protected
+ * nVHE hypervisor stack in EL1.
+ */
+static void hyp_prepare_backtrace(unsigned long fp, unsigned long pc)
+{
+	struct kvm_nvhe_stacktrace_info *stacktrace_info = this_cpu_ptr(&kvm_stacktrace_info);
+	struct kvm_nvhe_init_params *params = this_cpu_ptr(&kvm_init_params);
+
+	stacktrace_info->stack_base = (unsigned long)(params->stack_hyp_va - PAGE_SIZE);
+	stacktrace_info->overflow_stack_base = (unsigned long)this_cpu_ptr(overflow_stack);
+	stacktrace_info->fp = fp;
+	stacktrace_info->pc = pc;
+}
+
+/*
+ * kvm_nvhe_prepare_backtrace - prepare to dump the nVHE backtrace
+ *
+ * @fp : frame pointer at which to start the unwinding.
+ * @pc : program counter at which to start the unwinding.
+ *
+ * Saves the information needed by the host to dump the nVHE hypervisor
+ * backtrace.
+ */
+void kvm_nvhe_prepare_backtrace(unsigned long fp, unsigned long pc)
+{
+	if (is_protected_kvm_enabled())
+		return;
+	else
+		hyp_prepare_backtrace(fp, pc);
+}
