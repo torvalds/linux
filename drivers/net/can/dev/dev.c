7fb48d25b5ce3bc488dbb019bf1736248181de9a
Vincent Mailhol <mailhol.vincent@wanadoo.fr>
2022-07-27 19:16:34 +0900

    can: dev: add generic function can_ethtool_op_get_ts_info_hwts()
    Add function can_ethtool_op_get_ts_info_hwts(). This function will be
    used by CAN devices with hardware TX/RX timestamping support to
    implement ethtool_ops::get_ts_info. This function does not offer
    support to activate/deactivate hardware timestamps at device level nor
    support the filter options (which is currently the case for all CAN
    devices with hardware timestamping support).
    
    The fact that hardware timestamp can not be deactivated at hardware
    level does not impact the userland. As long as the user do not set
    SO_TIMESTAMPING using a setsockopt() or ioctl(), the kernel will not
    emit TX timestamps (RX timestamps will still be reproted as it is the
    case currently).
    
    Drivers which need more fine grained control remains free to implement
    their own function, but we foresee that the generic function
    introduced here will be sufficient for the majority.
    
    Signed-off-by: Vincent Mailhol <mailhol.vincent@wanadoo.fr>
    Link: https://lore.kernel.org/all/20220727101641.198847-8-mailhol.vincent@wanadoo.fr
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/dev/dev.c
+++ drivers/net/can/dev/dev.c
@@ -322,6 +322,27 @@ int can_change_mtu(struct net_device *dev, int new_mtu)
 }
 EXPORT_SYMBOL_GPL(can_change_mtu);
 
+/* generic implementation of ethtool_ops::get_ts_info for CAN devices
+ * supporting hardware timestamps
+ */
+int can_ethtool_op_get_ts_info_hwts(struct net_device *dev,
+				    struct ethtool_ts_info *info)
+{
+	info->so_timestamping =
+		SOF_TIMESTAMPING_TX_SOFTWARE |
+		SOF_TIMESTAMPING_RX_SOFTWARE |
+		SOF_TIMESTAMPING_SOFTWARE |
+		SOF_TIMESTAMPING_TX_HARDWARE |
+		SOF_TIMESTAMPING_RX_HARDWARE |
+		SOF_TIMESTAMPING_RAW_HARDWARE;
+	info->phc_index = -1;
+	info->tx_types = BIT(HWTSTAMP_TX_ON);
+	info->rx_filters = BIT(HWTSTAMP_FILTER_ALL);
+
+	return 0;
+}
+EXPORT_SYMBOL(can_ethtool_op_get_ts_info_hwts);
+
 /* Common open function when the device gets opened.
  *
  * This function should be called in the open function of the device
