8ea6e2c90bb0eb74a595a12e23a1dff9abbc760a
Chuck Lever <chuck.lever@oracle.com>
2022-07-27 14:40:35 -0400

    NFSD: Make nfs4_put_copy() static
    Clean up: All call sites are in fs/nfsd/nfs4proc.c.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- fs/nfsd/nfs4proc.c
+++ fs/nfsd/nfs4proc.c
@@ -1285,7 +1285,7 @@ out:
 	return status;
 }
 
-void nfs4_put_copy(struct nfsd4_copy *copy)
+static void nfs4_put_copy(struct nfsd4_copy *copy)
 {
 	if (!refcount_dec_and_test(&copy->refcount))
 		return;
