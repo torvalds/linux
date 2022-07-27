7ef3d06f1bc4a5e62273726f3dc2bd258ae1c71f
Jason A. Donenfeld <Jason@zx2c4.com>
2022-07-28 00:32:18 +1000

    powerpc/powernv/kvm: Use darn for H_RANDOM on Power9
    The existing logic in KVM to support guests calling H_RANDOM only works
    on Power8, because it looks for an RNG in the device tree, but on Power9
    we just use darn.
    
    In addition the existing code needs to work in real mode, so we have the
    special cased powernv_get_random_real_mode() to deal with that.
    
    Instead just have KVM call ppc_md.get_random_seed(), and do the real
    mode check inside of there, that way we use whatever RNG is available,
    including darn on Power9.
    
    Fixes: e928e9cb3601 ("KVM: PPC: Book3S HV: Add fast real-mode H_RANDOM implementation.")
    Cc: stable@vger.kernel.org # v4.1+
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Tested-by: Sachin Sant <sachinp@linux.ibm.com>
    [mpe: Rebase on previous commit, update change log appropriately]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20220727143219.2684192-2-mpe@ellerman.id.au

--- arch/powerpc/include/asm/archrandom.h
+++ arch/powerpc/include/asm/archrandom.h
@@ -38,12 +38,7 @@ static inline bool __must_check arch_get_random_seed_int(unsigned int *v)
 #endif /* CONFIG_ARCH_RANDOM */
 
 #ifdef CONFIG_PPC_POWERNV
-int powernv_hwrng_present(void);
 int powernv_get_random_long(unsigned long *v);
-int powernv_get_random_real_mode(unsigned long *v);
-#else
-static inline int powernv_hwrng_present(void) { return 0; }
-static inline int powernv_get_random_real_mode(unsigned long *v) { return 0; }
 #endif
 
 #endif /* _ASM_POWERPC_ARCHRANDOM_H */
