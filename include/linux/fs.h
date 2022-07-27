04b9407197789c81fffac52921e703cb47967d6a
Daniil Lunev <dlunev@chromium.org>
2022-07-27 16:44:24 +1000

    vfs: function to prevent re-use of block-device-based superblocks
    The function is to be called from filesystem-specific code to mark a
    superblock to be ignored by superblock test and thus never re-used.  The
    function also unregisters bdi if the bdi is per-superblock to avoid
    collision if a new superblock is created to represent the filesystem.
    generic_shutdown_super() skips unregistering bdi for a retired superlock as
    it assumes retire function has already done it.
    
    This patch adds the functionality only for the block-device-based supers,
    since the primary use case of the feature is to gracefully handle force
    unmount of external devices, mounted with FUSE.  This can be further
    extended to cover all superblocks, if the need arises.
    
    Signed-off-by: Daniil Lunev <dlunev@chromium.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1412,6 +1412,7 @@ extern int send_sigurg(struct fown_struct *fown);
 #define SB_I_SKIP_SYNC	0x00000100	/* Skip superblock at global sync */
 #define SB_I_PERSB_BDI	0x00000200	/* has a per-sb bdi */
 #define SB_I_TS_EXPIRY_WARNED 0x00000400 /* warned about timestamp range expiry */
+#define SB_I_RETIRED	0x00000800	/* superblock shouldn't be reused */
 
 /* Possible states of 'frozen' field */
 enum {
@@ -2432,6 +2433,7 @@ extern struct dentry *mount_nodev(struct file_system_type *fs_type,
 	int flags, void *data,
 	int (*fill_super)(struct super_block *, void *, int));
 extern struct dentry *mount_subtree(struct vfsmount *mnt, const char *path);
+void retire_super(struct super_block *sb);
 void generic_shutdown_super(struct super_block *sb);
 void kill_block_super(struct super_block *sb);
 void kill_anon_super(struct super_block *sb);
