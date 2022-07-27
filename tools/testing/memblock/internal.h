0a7e91528202bec8e8dc5787c7568e6c60ce7f43
Liu Xinpeng <liuxp11@chinatelecom.cn>
2022-07-27 18:07:33 +0800

    memblock tests: fix compilation errors
    Do 'make -C tools/testing/memblock', get the following errors:
    
    memblock.o: In function `memblock_find_in_range.constprop.9':
    memblock.c:(.text+0x4651): undefined reference to `pr_warn_ratelimited'
    memblock.o: In function `memblock_mark_mirror':
    memblock.c:(.text+0x7171): undefined reference to `mirrored_kernelcore'
    
    Fixes: 902c2d91582c ("memblock: Disable mirror feature if kernelcore is not specified")
    Fixes: 14d9a675fd0d ("mm: Ratelimited mirrored memory related warning messages")
    
    Signed-off-by: Liu Xinpeng <liuxp11@chinatelecom.cn>
    Tested-by: Ma Wupeng <mawupeng1@huawei.com>
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>
    Link: https://lore.kernel.org/r/1658916453-26312-1-git-send-email-liuxp11@chinatelecom.cn

--- tools/testing/memblock/internal.h
+++ tools/testing/memblock/internal.h
@@ -9,6 +9,10 @@
 static int memblock_debug = 1;
 #endif
 
+#define pr_warn_ratelimited(fmt, ...)    printf(fmt, ##__VA_ARGS__)
+
+bool mirrored_kernelcore = false;
+
 struct page {};
 
 void memblock_free_pages(struct page *page, unsigned long pfn,
