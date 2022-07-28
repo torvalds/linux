26e506a63e1b28beb833d3d7fda9b489b013f4ff
Greg Kroah-Hartman <gregkh@google.com>
2022-07-28 09:39:19 +0200

    Revert "Bluetooth: Interleave with allowlist scan"
    This reverts commit 5702c3c6576d753f405aa5863fdc079e7f6fb779 which is
    commit c4f1f408168cd6a83d973e98e1cd1888e4d3d907 upstream.
    
    It breaks the Android kernel ABI and is not needed for Android devices,
    so it is safe to revert for now.  If it is determined that it is needed
    in the future, it can be brought back in an abi-preserving way.
    
    Bug: 161946584
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I0931559c61d70d9750eae7c12cdcfe4485163a39

--- net/bluetooth/mgmt_config.c
+++ net/bluetooth/mgmt_config.c
@@ -67,8 +67,6 @@ int read_def_system_config(struct sock *sk, struct hci_dev *hdev, void *data,
 		HDEV_PARAM_U16(0x001a, le_supv_timeout),
 		HDEV_PARAM_U16_JIFFIES_TO_MSECS(0x001b,
 						def_le_autoconnect_timeout),
-		HDEV_PARAM_U16(0x001d, advmon_allowlist_duration),
-		HDEV_PARAM_U16(0x001e, advmon_no_filter_duration),
 	};
 	struct mgmt_rp_read_def_system_config *rp = (void *)params;
 
@@ -140,8 +138,6 @@ int set_def_system_config(struct sock *sk, struct hci_dev *hdev, void *data,
 		case 0x0019:
 		case 0x001a:
 		case 0x001b:
-		case 0x001d:
-		case 0x001e:
 			if (len != sizeof(u16)) {
 				bt_dev_warn(hdev, "invalid length %d, exp %zu for type %d",
 					    len, sizeof(u16), type);
@@ -254,12 +250,6 @@ int set_def_system_config(struct sock *sk, struct hci_dev *hdev, void *data,
 		case 0x0001b:
 			hdev->def_le_autoconnect_timeout =
 					msecs_to_jiffies(TLV_GET_LE16(buffer));
-			break;
-		case 0x0001d:
-			hdev->advmon_allowlist_duration = TLV_GET_LE16(buffer);
-			break;
-		case 0x0001e:
-			hdev->advmon_no_filter_duration = TLV_GET_LE16(buffer);
 			break;
 		default:
 			bt_dev_warn(hdev, "unsupported parameter %u", type);
