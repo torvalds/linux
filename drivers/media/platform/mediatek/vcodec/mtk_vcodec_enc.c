e7bfdf0a854037e8c0597f1f44f72651869c424d
Hirokazu Honda <hiroh@chromium.org>
2022-08-02 06:42:42 +0200

    media: mediatek: vcodec: Skip non CBR bitrate mode
    V4L2_MPEG_VIDEO_BITRATE_MODE_CBR is the only bitrate mode supported
    by the mediatek driver. The other bitrates must be skipped in
    QUERY_MENU.
    
    Fixes: d8e8aa866ed8 ("media: mediatek: vcodec: Report supported bitrate modes")
    Signed-off-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Chen-Yu Tsai <wenst@chromium.org>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>

--- drivers/media/platform/mediatek/vcodec/mtk_vcodec_enc.c
+++ drivers/media/platform/mediatek/vcodec/mtk_vcodec_enc.c
@@ -1405,7 +1405,8 @@ int mtk_vcodec_enc_ctrls_setup(struct mtk_vcodec_ctx *ctx)
 			       V4L2_MPEG_VIDEO_VP8_PROFILE_0, 0, V4L2_MPEG_VIDEO_VP8_PROFILE_0);
 	v4l2_ctrl_new_std_menu(handler, ops, V4L2_CID_MPEG_VIDEO_BITRATE_MODE,
 			       V4L2_MPEG_VIDEO_BITRATE_MODE_CBR,
-			       0, V4L2_MPEG_VIDEO_BITRATE_MODE_CBR);
+			       ~(1 << V4L2_MPEG_VIDEO_BITRATE_MODE_CBR),
+			       V4L2_MPEG_VIDEO_BITRATE_MODE_CBR);
 
 
 	if (handler->error) {
