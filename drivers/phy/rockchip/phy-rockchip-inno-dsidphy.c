3198142cf2499408b0f11129263218113fc98038
Nickey Yang <nickey.yang@rock-chips.com>
2022-08-03 20:53:41 +0800

    phy/rockchip: inno-dsidphy: support rv1126
    Signed-off-by: Nickey Yang <nickey.yang@rock-chips.com>
    Change-Id: I84fb7a922528894c7c14c95902f0e273640834d6

--- drivers/phy/rockchip/phy-rockchip-inno-dsidphy.c
+++ drivers/phy/rockchip/phy-rockchip-inno-dsidphy.c
@@ -926,6 +926,9 @@ static const struct of_device_id inno_dsidphy_of_match[] = {
 	}, {
 	  .compatible = "rockchip,rk3568-dsi-dphy",
 	  .data = &rk3568_plat_data,
+	}, {
+	  .compatible = "rockchip,rv1126-mipi-dphy",
+	  .data = &rk3568_plat_data,
 	},
 	{}
 };
