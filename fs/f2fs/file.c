a20fd832a4cc10940a9cc61a28a03f57a4342e6c
Jaewook Kim <jw5454.kim@samsung.com>
2022-08-03 17:53:58 +0900

    BACKPORT: f2fs: do not allow to decompress files have FI_COMPRESS_RELEASED
    If a file has FI_COMPRESS_RELEASED, all writes for it should not be
    allowed. However, as of now, in case of compress_mode=user, writes
    triggered by IOCTLs like F2FS_IOC_DE/COMPRESS_FILE are allowed unexpectly,
    which could crash that file.
    To fix it, let's do not allow F2FS_IOC_DE/COMPRESS_IOCTL if a file already
    has FI_COMPRESS_RELEASED flag.
    
    This is the reproduction process:
    1.  $ touch ./file
    2.  $ chattr +c ./file
    3.  $ dd if=/dev/random of=./file bs=4096 count=30 conv=notrunc
    4.  $ dd if=/dev/zero of=./file bs=4096 count=34 seek=30 conv=notrunc
    5.  $ sync
    6.  $ do_compress ./file      ; call F2FS_IOC_COMPRESS_FILE
    7.  $ get_compr_blocks ./file ; call F2FS_IOC_GET_COMPRESS_BLOCKS
    8.  $ release ./file          ; call F2FS_IOC_RELEASE_COMPRESS_BLOCKS
    9.  $ do_compress ./file      ; call F2FS_IOC_COMPRESS_FILE again
    10. $ get_compr_blocks ./file ; call F2FS_IOC_GET_COMPRESS_BLOCKS again
    
    This reproduction process is tested in 128kb cluster size.
    You can find compr_blocks has a negative value.
    
    Fixes: 5fdb322ff2c2b ("f2fs: add F2FS_IOC_DECOMPRESS_FILE and F2FS_IOC_COMPRESS_FILE")
    
    Bug: 228919347
    Bug: 267580491
    Signed-off-by: Junbeom Yeom <junbeom.yeom@samsung.com>
    Signed-off-by: Sungjong Seo <sj1557.seo@samsung.com>
    Signed-off-by: Youngjin Gil <youngjin.gil@samsung.com>
    Signed-off-by: Jaewook Kim <jw5454.kim@samsung.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit 854f8871ed48229d217edd7cb023f749e4a0817b)
    Change-Id: I086e4d88da68574ef793740d19ecbb2d4ad6ef1d

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -4121,6 +4121,11 @@ static int f2fs_ioc_decompress_file(struct file *filp, unsigned long arg)
 		goto out;
 	}
 
+	if (is_inode_flag_set(inode, FI_COMPRESS_RELEASED)) {
+		ret = -EINVAL;
+		goto out;
+	}
+
 	ret = filemap_write_and_wait_range(inode->i_mapping, 0, LLONG_MAX);
 	if (ret)
 		goto out;
@@ -4185,6 +4190,11 @@ static int f2fs_ioc_compress_file(struct file *filp, unsigned long arg)
 
 	if (!f2fs_is_compress_backend_ready(inode)) {
 		ret = -EOPNOTSUPP;
+		goto out;
+	}
+
+	if (is_inode_flag_set(inode, FI_COMPRESS_RELEASED)) {
+		ret = -EINVAL;
 		goto out;
 	}
 
