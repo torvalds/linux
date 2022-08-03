f9a36fa5367e055c9ba39d6d1f0852e040536304
Harman Kalra <hkalra@marvell.com>
2022-08-03 13:24:13 +0530

    octeontx2-af: suppress external profile loading warning
    commit cf2437626502b5271d19686b03dea306efe17ea0 upstream.
    
    The packet parser profile supplied as firmware may not
    be present all the time and default profile is used mostly.
    Hence suppress firmware loading warning from kernel due to
    absence of firmware in kernel image.
    
    Fixes: 3a7244152f9c ("octeontx2-af: add support for custom KPU entries")
    Signed-off-by: Harman Kalra <hkalra@marvell.com>
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_npc.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_npc.c
@@ -1650,7 +1650,7 @@ static void npc_load_kpu_profile(struct rvu *rvu)
 	 * Firmware database method.
 	 * Default KPU profile.
 	 */
-	if (!request_firmware(&fw, kpu_profile, rvu->dev)) {
+	if (!request_firmware_direct(&fw, kpu_profile, rvu->dev)) {
 		dev_info(rvu->dev, "Loading KPU profile from firmware: %s\n",
 			 kpu_profile);
 		rvu->kpu_fwdata = kzalloc(fw->size, GFP_KERNEL);
