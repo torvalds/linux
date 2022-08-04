2320be6032e1c1b17a3fcac98813947d1d28c32f
Daniel Lezcano <daniel.lezcano@linexp.org>
2022-08-05 00:43:31 +0200

    thermal/drivers/db8500: Switch to new of API
    The thermal OF code has a new API allowing to migrate the OF
    initialization to a simpler approach. The ops are no longer device
    tree specific and are the generic ones provided by the core code.
    
    Convert the ops to the thermal_zone_device_ops format and use the new
    API to register the thermal zone with these generic ops.
    
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linexp.org>
    Link: https://lore.kernel.org/r/20220804224349.1926752-16-daniel.lezcano@linexp.org
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- drivers/thermal/db8500_thermal.c
+++ drivers/thermal/db8500_thermal.c
@@ -58,9 +58,9 @@ struct db8500_thermal_zone {
 };
 
 /* Callback to get current temperature */
-static int db8500_thermal_get_temp(void *data, int *temp)
+static int db8500_thermal_get_temp(struct thermal_zone_device *tz, int *temp)
 {
-	struct db8500_thermal_zone *th = data;
+	struct db8500_thermal_zone *th = tz->devdata;
 
 	/*
 	 * TODO: There is no PRCMU interface to get temperature data currently,
@@ -72,7 +72,7 @@ static int db8500_thermal_get_temp(void *data, int *temp)
 	return 0;
 }
 
-static struct thermal_zone_of_device_ops thdev_ops = {
+static const struct thermal_zone_device_ops thdev_ops = {
 	.get_temp = db8500_thermal_get_temp,
 };
 
@@ -182,7 +182,7 @@ static int db8500_thermal_probe(struct platform_device *pdev)
 	}
 
 	/* register of thermal sensor and get info from DT */
-	th->tz = devm_thermal_zone_of_sensor_register(dev, 0, th, &thdev_ops);
+	th->tz = devm_thermal_of_zone_register(dev, 0, th, &thdev_ops);
 	if (IS_ERR(th->tz)) {
 		dev_err(dev, "register thermal zone sensor failed\n");
 		return PTR_ERR(th->tz);
