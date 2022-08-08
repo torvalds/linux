a722d70508d64e4800dbf7e9fbf132d186a6484a
Axel Rasmussen <axelrasmussen@google.com>
2022-08-08 10:56:10 -0700

    selftests: vm: add hugetlb_shared userfaultfd test to run_vmtests.sh
    Patch series "userfaultfd: add /dev/userfaultfd for fine grained access
    control", v7.
    
    Why not ...?
    ============
    
    - Why not /proc/[pid]/userfaultfd? Two main points (additional discussion [1]):
    
        - /proc/[pid]/* files are all owned by the user/group of the process, and
          they don't really support chmod/chown. So, without extending procfs it
          doesn't solve the problem this series is trying to solve.
    
        - The main argument *for* this was to support creating UFFDs for remote
          processes. But, that use case clearly calls for CAP_SYS_PTRACE, so to
          support this we could just use the UFFD syscall as-is.
    
    - Why not use a syscall? Access to syscalls is generally controlled by
      capabilities. We don't have a capability which is used for userfaultfd access
      without also granting more / other permissions as well, and adding a new
      capability was rejected [2].
    
        - It's possible a LSM could be used to control access instead, but I have
          some concerns. I don't think this approach would be as easy to use,
          particularly if we were to try to solve this with something heavyweight
          like SELinux. Maybe we could pursue adding a new LSM specifically for
          this user case, but it may be too narrow of a case to justify that.
    
    [1]: https://patchwork.kernel.org/project/linux-mm/cover/20220719195628.3415852-1-axelrasmussen@google.com/
    [2]: https://lore.kernel.org/lkml/686276b9-4530-2045-6bd8-170e5943abe4@schaufler-ca.com/T/
    
    
    This patch (of 5):
    
    This not being included was just a simple oversight.  There are certain
    features (like minor fault support) which are only enabled on shared
    mappings, so without including hugetlb_shared we actually lose a
    significant amount of test coverage.
    
    Link: https://lkml.kernel.org/r/20220808175614.3885028-1-axelrasmussen@google.com
    Link: https://lkml.kernel.org/r/20220808175614.3885028-2-axelrasmussen@google.com
    Signed-off-by: Axel Rasmussen <axelrasmussen@google.com>
    Reviewed-by: Shuah Khan <skhan@linuxfoundation.org>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Dmitry V. Levin <ldv@altlinux.org>
    Cc: Gleb Fotengauer-Malinovskiy <glebfm@altlinux.org>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Nadav Amit <namit@vmware.com>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Zhang Yi <yi.zhang@huawei.com>
    Cc: Mike Rapoport <rppt@linux.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- tools/testing/selftests/vm/run_vmtests.sh
+++ tools/testing/selftests/vm/run_vmtests.sh
@@ -121,9 +121,11 @@ run_test ./gup_test -a
 run_test ./gup_test -ct -F 0x1 0 19 0x1000
 
 run_test ./userfaultfd anon 20 16
-# Test requires source and destination huge pages.  Size of source
-# (half_ufd_size_MB) is passed as argument to test.
+# Hugetlb tests require source and destination huge pages. Pass in half the
+# size ($half_ufd_size_MB), which is used for *each*.
 run_test ./userfaultfd hugetlb "$half_ufd_size_MB" 32
+run_test ./userfaultfd hugetlb_shared "$half_ufd_size_MB" 32 "$mnt"/uffd-test
+rm -f "$mnt"/uffd-test
 run_test ./userfaultfd shmem 20 16
 
 #cleanup
