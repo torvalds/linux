04e520e6af4ffddc82b721fe738fa93212b88141
Hajime Tazaki <thehajime@gmail.com>
2022-08-08 15:57:20 +0900

    lkl: follow up fixes after v5.11 merge (missing switch_mm)
    Fixes: 94f89922e1e0 ("asm-generic: add generic MMU versions of mmu
    context functions")
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

---
+++ arch/lkl/include/asm/mmu_context.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_LKL_MMU_CONTEXT_H
+#define _ASM_LKL_MMU_CONTEXT_H
+
+#include <asm-generic/nommu_context.h>
+
+#endif /* _ASM_LKL_MMU_CONTEXT_H */
