2248ccd80124e61c2c84a22b22409bab452e1f0c
Sander Vanheule <sander@svanheule.net>
2022-08-09 19:36:34 +0200

    lib/cpumask: add inline cpumask_next_wrap() for UP
    In the uniprocessor case, cpumask_next_wrap() can be simplified, as the
    number of valid argument combinations is limited:
        - 'start' can only be 0
        - 'n' can only be -1 or 0
    
    The only valid CPU that can then be returned, if any, will be the first
    one set in the provided 'mask'.
    
    For NR_CPUS == 1, include/linux/cpumask.h now provides an inline
    definition of cpumask_next_wrap(), which will conflict with the one
    provided by lib/cpumask.c.  Make building of lib/cpumask.o again depend
    on CONFIG_SMP=y (i.e. NR_CPUS > 1) to avoid the re-definition.
    
    Suggested-by: Yury Norov <yury.norov@gmail.com>
    Signed-off-by: Sander Vanheule <sander@svanheule.net>
    Signed-off-by: Yury Norov <yury.norov@gmail.com>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -262,7 +262,26 @@ unsigned int cpumask_next_and(int n, const struct cpumask *src1p,
 		(cpu) = cpumask_next_zero((cpu), (mask)),	\
 		(cpu) < nr_cpu_ids;)
 
+#if NR_CPUS == 1
+static inline
+unsigned int cpumask_next_wrap(int n, const struct cpumask *mask, int start, bool wrap)
+{
+	cpumask_check(start);
+	if (n != -1)
+		cpumask_check(n);
+
+	/*
+	 * Return the first available CPU when wrapping, or when starting before cpu0,
+	 * since there is only one valid option.
+	 */
+	if (wrap && n >= 0)
+		return nr_cpumask_bits;
+
+	return cpumask_first(mask);
+}
+#else
 unsigned int __pure cpumask_next_wrap(int n, const struct cpumask *mask, int start, bool wrap);
+#endif
 
 /**
  * for_each_cpu_wrap - iterate over every cpu in a mask, starting at a specified location
