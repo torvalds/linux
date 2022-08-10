f002aa7c0ac5518a152ec24503c9f887031984a0
Justin Chen <justinpopo6@gmail.com>
2022-08-10 15:27:34 -0700

    usb: host: xhci-plat: suspend and resume clocks
    [ Upstream commit 8bd954c56197caf5e3a804d989094bc3fe6329aa ]
    
    Introduce XHCI_SUSPEND_RESUME_CLKS quirk as a means to suspend and resume
    clocks if the hardware is capable of doing so. We assume that clocks will
    be needed if the device may wake.
    
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Justin Chen <justinpopo6@gmail.com>
    Link: https://lore.kernel.org/r/1660170455-15781-2-git-send-email-justinpopo6@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1903,6 +1903,7 @@ struct xhci_hcd {
 #define XHCI_NO_SOFT_RETRY	BIT_ULL(40)
 #define XHCI_BROKEN_D3COLD	BIT_ULL(41)
 #define XHCI_EP_CTX_BROKEN_DCS	BIT_ULL(42)
+#define XHCI_SUSPEND_RESUME_CLKS	BIT_ULL(43)
 
 	unsigned int		num_active_eps;
 	unsigned int		limit_active_eps;
