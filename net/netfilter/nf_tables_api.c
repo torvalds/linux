0df32f45be40d0cf0e29a9e194d2f9af60242057
Pablo Neira Ayuso <pablo@netfilter.org>
2022-08-12 16:21:28 +0200

    netfilter: nf_tables: NFTA_SET_ELEM_KEY_END requires concat and interval flags
    commit 88cccd908d51397f9754f89a937cd13fa59dee37 upstream.
    
    If the NFT_SET_CONCAT|NFT_SET_INTERVAL flags are set on, then the
    netlink attribute NFTA_SET_ELEM_KEY_END must be specified. Otherwise,
    NFTA_SET_ELEM_KEY_END should not be present.
    
    For catch-all element, NFTA_SET_ELEM_KEY_END should not be present.
    The NFT_SET_ELEM_INTERVAL_END is never used with this set flags
    combination.
    
    Fixes: 7b225d0b5c6d ("netfilter: nf_tables: add NFTA_SET_ELEM_KEY_END attribute")
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -5711,6 +5711,24 @@ static void nft_setelem_remove(const struct net *net,
 		set->ops->remove(net, set, elem);
 }
 
+static bool nft_setelem_valid_key_end(const struct nft_set *set,
+				      struct nlattr **nla, u32 flags)
+{
+	if ((set->flags & (NFT_SET_CONCAT | NFT_SET_INTERVAL)) ==
+			  (NFT_SET_CONCAT | NFT_SET_INTERVAL)) {
+		if (flags & NFT_SET_ELEM_INTERVAL_END)
+			return false;
+		if (!nla[NFTA_SET_ELEM_KEY_END] &&
+		    !(flags & NFT_SET_ELEM_CATCHALL))
+			return false;
+	} else {
+		if (nla[NFTA_SET_ELEM_KEY_END])
+			return false;
+	}
+
+	return true;
+}
+
 static int nft_add_set_elem(struct nft_ctx *ctx, struct nft_set *set,
 			    const struct nlattr *attr, u32 nlmsg_flags)
 {
@@ -5769,6 +5787,9 @@ static int nft_add_set_elem(struct nft_ctx *ctx, struct nft_set *set,
 		if (nla[NFTA_SET_ELEM_OBJREF])
 			return -EINVAL;
 	}
+
+	if (!nft_setelem_valid_key_end(set, nla, flags))
+		return -EINVAL;
 
 	if ((flags & NFT_SET_ELEM_INTERVAL_END) &&
 	     (nla[NFTA_SET_ELEM_DATA] ||
@@ -6190,6 +6211,9 @@ static int nft_del_setelem(struct nft_ctx *ctx, struct nft_set *set,
 		return err;
 
 	if (!nla[NFTA_SET_ELEM_KEY] && !(flags & NFT_SET_ELEM_CATCHALL))
+		return -EINVAL;
+
+	if (!nft_setelem_valid_key_end(set, nla, flags))
 		return -EINVAL;
 
 	nft_set_ext_prepare(&tmpl);
