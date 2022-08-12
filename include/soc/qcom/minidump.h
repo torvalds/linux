314e434731f3f209b8f46649616b265c83b21d31
Cong Zhang <quic_congzhan@quicinc.com>
2022-08-12 11:48:16 +0800

    soc: qcom: Limit name length to 9 in SVM
    RM will add a prefix such as '45_' before the real entry name in SVM. So
    the real name length need limit to 9.
    
    Change-Id: I295074f34910946beb2c05828816ce6acbae5d59
    Signed-off-by: Cong Zhang <quic_congzhan@quicinc.com>

--- include/soc/qcom/minidump.h
+++ include/soc/qcom/minidump.h
@@ -9,7 +9,12 @@
 
 #include <linux/types.h>
 
+#if IS_ENABLED(CONFIG_ARCH_QTI_VM)
+#define MAX_NAME_LENGTH		9
+#else
 #define MAX_NAME_LENGTH		12
+#endif
+
 /* md_region -  Minidump table entry
  * @name:	Entry name, Minidump will dump binary with this name.
  * @id:		Entry ID, used only for SDI dumps.
