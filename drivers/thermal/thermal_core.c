2f9d142c93c293b2526d63e4e75716945edf0cd2
Daniel Lezcano <daniel.lezcano@linaro.org>
2022-08-12 15:07:43 +0200

    thermal/core: Fix lockdep_assert() warning
    The function thermal_zone_device_is_enabled() must be called with the
    thermal zone lock held. In the resume path, it is called without.
    
    As the thermal_zone_device_is_enabled() is also checked in
    thermal_zone_device_update(), do the check in resume() function is
    pointless, except for saving an extra initialization which does not
    hurt if it is done in all the cases.
    
    Fixes: ca48ad71717dd ("thermal/core: Move the mutex inside the thermal_zone_device_update() function")
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Reported-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Tested-by: Marek Szyprowski <m.szyprowski@samsung.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1438,9 +1438,6 @@ static int thermal_pm_notify(struct notifier_block *nb,
 	case PM_POST_SUSPEND:
 		atomic_set(&in_suspend, 0);
 		list_for_each_entry(tz, &thermal_tz_list, node) {
-			if (!thermal_zone_device_is_enabled(tz))
-				continue;
-
 			thermal_zone_device_init(tz);
 			thermal_zone_device_update(tz,
 						   THERMAL_EVENT_UNSPECIFIED);
