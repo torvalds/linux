2ee721536f4fdd4d9881020a7c541c104224a6b1
Tao Zhang <quic_taozha@quicinc.com>
2022-08-12 19:57:14 +0800

    coresight: byte-cntr: Add the length check in reading data
    Add the length check in the reading method to avoid data
    overwirtten.
    
    Change-Id: I79a467fe39d491dfdee267d8e6209c34a3b04269
    Signed-off-by: Tao Zhang <quic_taozha@quicinc.com>
    Signed-off-by: Mao Jinlong <quic_jinlmao@quicinc.com>

--- drivers/hwtracing/coresight/coresight-byte-cntr.c
+++ drivers/hwtracing/coresight/coresight-byte-cntr.c
@@ -57,7 +57,7 @@ static irqreturn_t etr_handler(int irq, void *data)
 
 
 static long tmc_etr_flush_remaining_bytes(struct tmc_drvdata *tmcdrvdata, long offset,
-			char **bufpp)
+			size_t len, char **bufpp)
 {
 	long rwp_offset, req_size, actual = 0;
 	struct etr_buf *etr_buf;
@@ -81,6 +81,9 @@ static long tmc_etr_flush_remaining_bytes(struct tmc_drvdata *tmcdrvdata, long o
 	req_size = ((rwp_offset < offset) ? tmcdrvdata->size : 0) +
 		rwp_offset - offset;
 
+	if (req_size > len)
+		req_size = len;
+
 	if (req_size > 0)
 		actual = tmc_etr_buf_get_data(etr_buf, offset, req_size, bufpp);
 
@@ -103,7 +106,7 @@ static ssize_t tmc_etr_byte_cntr_read(struct file *fp, char __user *data,
 	mutex_lock(&byte_cntr_data->byte_cntr_lock);
 	if (!byte_cntr_data->read_active) {
 		actual = tmc_etr_flush_remaining_bytes(tmcdrvdata,
-				byte_cntr_data->offset, &bufp);
+				byte_cntr_data->offset, len, &bufp);
 		if (actual > 0) {
 			len = actual;
 			goto copy;
@@ -123,7 +126,7 @@ static ssize_t tmc_etr_byte_cntr_read(struct file *fp, char __user *data,
 			mutex_lock(&byte_cntr_data->byte_cntr_lock);
 			if (!byte_cntr_data->read_active) {
 				actual = tmc_etr_flush_remaining_bytes(tmcdrvdata,
-						byte_cntr_data->offset, &bufp);
+						byte_cntr_data->offset, len, &bufp);
 				if (actual > 0) {
 					len = actual;
 					goto copy;
@@ -139,7 +142,7 @@ static ssize_t tmc_etr_byte_cntr_read(struct file *fp, char __user *data,
 
 	} else {
 		actual = tmc_etr_flush_remaining_bytes(tmcdrvdata,
-				byte_cntr_data->offset, &bufp);
+				byte_cntr_data->offset, len, &bufp);
 		if (actual > 0) {
 			len = actual;
 			goto copy;
