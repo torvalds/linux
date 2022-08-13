4b198c41d736c1c4c4974fd53cd03d632abdb6be
Alexander Gordeev <agordeev@linux.ibm.com>
2022-08-13 19:45:21 +0200

    s390/boot: fix absolute zero lowcore corruption on boot
    [ Upstream commit 12dd19c159659ec9050f45dc8a2ff3c3917f4be3 ]
    
    Crash dump always starts on CPU0. In case CPU0 is offline the
    prefix page is not installed and the absolute zero lowcore is
    used. However, struct lowcore::mcesad is never assigned and
    stays zero. That leads to __machine_kdump() -> save_vx_regs()
    call silently stores vector registers to the absolute lowcore
    at 0x11b0 offset.
    
    Fixes: a62bc0739253 ("s390/kdump: add support for vector extension")
    Reviewed-by: Heiko Carstens <hca@linux.ibm.com>
    Signed-off-by: Alexander Gordeev <agordeev@linux.ibm.com>
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/s390/kernel/nmi.c
+++ arch/s390/kernel/nmi.c
@@ -62,7 +62,7 @@ static inline unsigned long nmi_get_mcesa_size(void)
  * The structure is required for machine check happening early in
  * the boot process.
  */
-static struct mcesa boot_mcesa __initdata __aligned(MCESA_MAX_SIZE);
+static struct mcesa boot_mcesa __aligned(MCESA_MAX_SIZE);
 
 void __init nmi_alloc_boot_cpu(struct lowcore *lc)
 {
