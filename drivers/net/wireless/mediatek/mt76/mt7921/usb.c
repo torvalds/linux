b78f67bf59baaf22a90481dabbf3ee29dd96c970
Sean Wang <sean.wang@mediatek.com>
2022-08-15 07:37:14 +0800

    wifi: mt76: mt7921: get rid of the false positive reset
    False positive reset would be possibly triggered by those commands we
    applied in suspend with HZ MCU timeout, especially it happened when we
    enabled kernel log in pm core to diagnose how much time we spend in each
    driver during suspend procedure. So we enlarge the value and align the MCU
    timeout as other commands we did to reduce the false positive reset.
    
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Acked-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7921/usb.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/usb.c
@@ -106,12 +106,7 @@ mt7921u_mcu_send_message(struct mt76_dev *mdev, struct sk_buff *skb,
 	if (ret)
 		return ret;
 
-	if (cmd == MCU_UNI_CMD(HIF_CTRL) ||
-	    cmd == MCU_UNI_CMD(SUSPEND) ||
-	    cmd == MCU_UNI_CMD(OFFLOAD))
-		mdev->mcu.timeout = HZ;
-	else
-		mdev->mcu.timeout = 3 * HZ;
+	mdev->mcu.timeout = 3 * HZ;
 
 	if (cmd != MCU_CMD(FW_SCATTER))
 		ep = MT_EP_OUT_INBAND_CMD;
