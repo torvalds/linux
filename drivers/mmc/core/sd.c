cb27189360222112bb3b03c5dec1bda47f38f307
Adrian Hunter <adrian.hunter@intel.com>
2022-08-15 10:33:20 +0300

    mmc: core: Fix UHS-I SD 1.8V workaround branch
    commit 15c56208c79c340686869c31595c209d1431c5e8 upstream.
    
    When introduced, upon success, the 1.8V fixup workaround in
    mmc_sd_init_card() would branch to practically the end of the function, to
    a label named "done". Unfortunately, perhaps due to the label name, over
    time new code has been added that really should have come after "done" not
    before it. Let's fix the problem by moving the label to the correct place
    and rename it "cont".
    
    Fixes: 045d705dc1fb ("mmc: core: Enable the MMC host software queue for the SD card")
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Reviewed-by: Seunghui Lee <sh043.lee@samsung.com>
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/r/20220815073321.63382-2-adrian.hunter@intel.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    [Backport to 5.10]
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -1107,7 +1107,7 @@ retry:
 					mmc_remove_card(card);
 				goto retry;
 			}
-			goto done;
+			goto cont;
 		}
 	}
 
@@ -1143,7 +1143,7 @@ retry:
 			mmc_set_bus_width(host, MMC_BUS_WIDTH_4);
 		}
 	}
-
+cont:
 	if (host->cqe_ops && !host->cqe_enabled) {
 		err = host->cqe_ops->cqe_enable(host, card);
 		if (!err) {
@@ -1161,7 +1161,7 @@ retry:
 		err = -EINVAL;
 		goto free_card;
 	}
-done:
+
 	host->card = card;
 	return 0;
 
