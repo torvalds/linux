93b40b3ef5e1554e10891491542ef8dce0351af0
José Expósito <jose.exposito89@gmail.com>
2022-08-15 16:29:55 +0200

    HID: uclogic: Add support for XP-PEN Deco Pro S
    The XP-PEN Deco Pro S is a UGEE v2 device with a frame with 8 buttons,
    a bitmap dial and a mouse. Its pen has 2 buttons, supports tilt and
    pressure.
    
    All the pieces to support it are already in place. Add its ID in order
    to support the device.
    
    The required Wireshark traces were captured by Jouke Witteveen.
    For more information check [1].
    
    Link: https://gitlab.freedesktop.org/libinput/libinput/-/issues/738  [1]
    Tested-by: Jouke Witteveen <j.witteveen@gmail.com>
    Signed-off-by: José Expósito <jose.exposito89@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-uclogic-params.c
+++ drivers/hid/hid-uclogic-params.c
@@ -1582,6 +1582,8 @@ int uclogic_params_init(struct uclogic_params *params,
 		break;
 	case VID_PID(USB_VENDOR_ID_UGEE,
 		     USB_DEVICE_ID_UGEE_XPPEN_TABLET_DECO_L):
+	case VID_PID(USB_VENDOR_ID_UGEE,
+		     USB_DEVICE_ID_UGEE_XPPEN_TABLET_DECO_PRO_S):
 		rc = uclogic_params_ugee_v2_init(&p, hdev);
 		if (rc != 0)
 			goto cleanup;
