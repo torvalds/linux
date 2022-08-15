8884a192f95e6f845d11d039b1bc0ca0c368737f
Samuel Holland <samuel@sholland.org>
2022-08-14 23:12:43 -0500

    soc: sunxi: sram: Fix debugfs info for A64 SRAM C
    [ Upstream commit e3c95edb1bd8b9c2cb0caa6ae382fc8080f6a0ed ]
    
    The labels were backward with respect to the register values. The SRAM
    is mapped to the CPU when the register value is 1.
    
    Fixes: 5e4fb6429761 ("drivers: soc: sunxi: add support for A64 and its SRAM C")
    Acked-by: Jernej Skrabec <jernej.skrabec@gmail.com>
    Signed-off-by: Samuel Holland <samuel@sholland.org>
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>
    Link: https://lore.kernel.org/r/20220815041248.53268-7-samuel@sholland.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/soc/sunxi/sunxi_sram.c
+++ drivers/soc/sunxi/sunxi_sram.c
@@ -78,8 +78,8 @@ static struct sunxi_sram_desc sun4i_a10_sram_d = {
 
 static struct sunxi_sram_desc sun50i_a64_sram_c = {
 	.data	= SUNXI_SRAM_DATA("C", 0x4, 24, 1,
-				  SUNXI_SRAM_MAP(0, 1, "cpu"),
-				  SUNXI_SRAM_MAP(1, 0, "de2")),
+				  SUNXI_SRAM_MAP(1, 0, "cpu"),
+				  SUNXI_SRAM_MAP(0, 1, "de2")),
 };
 
 static const struct of_device_id sunxi_sram_dt_ids[] = {
