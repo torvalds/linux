f8e80792c1a88c521c3aa2c377b766826ae89b81
Dylan Yudaken <dylany@fb.com>
2022-08-16 06:59:59 -0700

    eventfd: guard wake_up in eventfd fs calls as well
    [ Upstream commit 9f0deaa12d832f488500a5afe9b912e9b3cfc432 ]
    
    Guard wakeups that the user can trigger, and that may end up triggering a
    call back into eventfd_signal. This is in addition to the current approach
    that only guards in eventfd_signal.
    
    Rename in_eventfd_signal -> in_eventfd at the same time to reflect this.
    
    Without this there would be a deadlock in the following code using libaio:
    
    int main()
    {
    	struct io_context *ctx = NULL;
    	struct iocb iocb;
    	struct iocb *iocbs[] = { &iocb };
    	int evfd;
            uint64_t val = 1;
    
    	evfd = eventfd(0, EFD_CLOEXEC);
    	assert(!io_setup(2, &ctx));
    	io_prep_poll(&iocb, evfd, POLLIN);
    	io_set_eventfd(&iocb, evfd);
    	assert(1 == io_submit(ctx, 1, iocbs));
            write(evfd, &val, 8);
    }
    
    Signed-off-by: Dylan Yudaken <dylany@fb.com>
    Reviewed-by: Jens Axboe <axboe@kernel.dk>
    Link: https://lore.kernel.org/r/20220816135959.1490641-1-dylany@fb.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -933,7 +933,7 @@ struct task_struct {
 #endif
 #ifdef CONFIG_EVENTFD
 	/* Recursion prevention for eventfd_signal() */
-	unsigned			in_eventfd_signal:1;
+	unsigned			in_eventfd:1;
 #endif
 
 	unsigned long			atomic_flags; /* Flags requiring atomic access. */
