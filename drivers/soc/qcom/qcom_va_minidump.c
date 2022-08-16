9d98b191601a89538c934f90974936f3e820c75d
Mukesh Ojha <quic_mojha@quicinc.com>
2022-08-16 10:12:01 +0530

    va_minidump: Fix the type 'name' in register api
    'name' passed to the qcom_va_md_register() api should be
    const char * type. Fix this with this change.
    
    Change-Id: Ibbe260b240989e35dbe9250c1db345f86b9ae410
    Signed-off-by: Mukesh Ojha <quic_mojha@quicinc.com>
    Signed-off-by: Guru Das Srinagesh <quic_gurus@quicinc.com>

--- drivers/soc/qcom/qcom_va_minidump.c
+++ drivers/soc/qcom/qcom_va_minidump.c
@@ -162,7 +162,7 @@ bool qcom_va_md_enabled(void)
 }
 EXPORT_SYMBOL(qcom_va_md_enabled);
 
-int qcom_va_md_register(char *name, struct notifier_block *nb)
+int qcom_va_md_register(const char *name, struct notifier_block *nb)
 {
 	int ret = 0;
 	struct va_md_s_data *va_md_s_data;
@@ -237,7 +237,7 @@ out:
 }
 EXPORT_SYMBOL(qcom_va_md_register);
 
-int qcom_va_md_unregister(char *name, struct notifier_block *nb)
+int qcom_va_md_unregister(const char *name, struct notifier_block *nb)
 {
 	struct va_md_s_data *va_md_s_data;
 	struct notifier_block_list *nbl, *tmpnbl;
