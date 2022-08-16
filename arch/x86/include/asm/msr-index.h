b8d1d163604bd1e600b062fb00de5dc42baa355f
Daniel Sneddon <daniel.sneddon@linux.intel.com>
2022-08-16 16:19:42 -0700

    x86/apic: Don't disable x2APIC if locked
    The APIC supports two modes, legacy APIC (or xAPIC), and Extended APIC
    (or x2APIC).  X2APIC mode is mostly compatible with legacy APIC, but
    it disables the memory-mapped APIC interface in favor of one that uses
    MSRs.  The APIC mode is controlled by the EXT bit in the APIC MSR.
    
    The MMIO/xAPIC interface has some problems, most notably the APIC LEAK
    [1].  This bug allows an attacker to use the APIC MMIO interface to
    extract data from the SGX enclave.
    
    Introduce support for a new feature that will allow the BIOS to lock
    the APIC in x2APIC mode.  If the APIC is locked in x2APIC mode and the
    kernel tries to disable the APIC or revert to legacy APIC mode a GP
    fault will occur.
    
    Introduce support for a new MSR (IA32_XAPIC_DISABLE_STATUS) and handle
    the new locked mode when the LEGACY_XAPIC_DISABLED bit is set by
    preventing the kernel from trying to disable the x2APIC.
    
    On platforms with the IA32_XAPIC_DISABLE_STATUS MSR, if SGX or TDX are
    enabled the LEGACY_XAPIC_DISABLED will be set by the BIOS.  If
    legacy APIC is required, then it SGX and TDX need to be disabled in the
    BIOS.
    
    [1]: https://aepicleak.com/aepicleak.pdf
    
    Signed-off-by: Daniel Sneddon <daniel.sneddon@linux.intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Acked-by: Dave Hansen <dave.hansen@linux.intel.com>
    Tested-by: Neelima Krishnan <neelima.krishnan@intel.com>
    Link: https://lkml.kernel.org/r/20220816231943.1152579-1-daniel.sneddon@linux.intel.com

--- arch/x86/include/asm/msr-index.h
+++ arch/x86/include/asm/msr-index.h
@@ -155,6 +155,11 @@
 						 * Return Stack Buffer Predictions.
 						 */
 
+#define ARCH_CAP_XAPIC_DISABLE		BIT(21)	/*
+						 * IA32_XAPIC_DISABLE_STATUS MSR
+						 * supported
+						 */
+
 #define MSR_IA32_FLUSH_CMD		0x0000010b
 #define L1D_FLUSH			BIT(0)	/*
 						 * Writeback and invalidate the
@@ -1053,5 +1058,13 @@
 /* Hardware Feedback Interface */
 #define MSR_IA32_HW_FEEDBACK_PTR        0x17d0
 #define MSR_IA32_HW_FEEDBACK_CONFIG     0x17d1
+
+/* x2APIC locked status */
+#define MSR_IA32_XAPIC_DISABLE_STATUS	0xBD
+#define LEGACY_XAPIC_DISABLED		BIT(0) /*
+						* x2APIC mode is locked and
+						* disabling x2APIC will cause
+						* a #GP
+						*/
 
 #endif /* _ASM_X86_MSR_INDEX_H */
