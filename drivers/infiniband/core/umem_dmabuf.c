b16de8b9e7d1aae169d059c3a0dd9a881a3c0d1d
Jason Gunthorpe <jgg@nvidia.com>
2022-08-16 11:03:20 -0300

    RDMA: Handle the return code from dma_resv_wait_timeout() properly
    ib_umem_dmabuf_map_pages() returns 0 on success and -ERRNO on failure.
    
    dma_resv_wait_timeout() uses a different scheme:
    
     * Returns -ERESTARTSYS if interrupted, 0 if the wait timed out, or
     * greater than zero on success.
    
    This results in ib_umem_dmabuf_map_pages() being non-functional as a
    positive return will be understood to be an error by drivers.
    
    Fixes: f30bceab16d1 ("RDMA: use dma_resv_wait() instead of extracting the fence")
    Cc: stable@kernel.org
    Link: https://lore.kernel.org/r/0-v1-d8f4e1fa84c8+17-rdma_dmabuf_fix_jgg@nvidia.com
    Tested-by: Maor Gottlieb <maorg@nvidia.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>

--- drivers/infiniband/core/umem_dmabuf.c
+++ drivers/infiniband/core/umem_dmabuf.c
@@ -18,6 +18,7 @@ int ib_umem_dmabuf_map_pages(struct ib_umem_dmabuf *umem_dmabuf)
 	struct scatterlist *sg;
 	unsigned long start, end, cur = 0;
 	unsigned int nmap = 0;
+	long ret;
 	int i;
 
 	dma_resv_assert_held(umem_dmabuf->attach->dmabuf->resv);
@@ -67,9 +68,14 @@ wait_fence:
 	 * may be not up-to-date. Wait for the exporter to finish
 	 * the migration.
 	 */
-	return dma_resv_wait_timeout(umem_dmabuf->attach->dmabuf->resv,
+	ret = dma_resv_wait_timeout(umem_dmabuf->attach->dmabuf->resv,
 				     DMA_RESV_USAGE_KERNEL,
 				     false, MAX_SCHEDULE_TIMEOUT);
+	if (ret < 0)
+		return ret;
+	if (ret == 0)
+		return -ETIMEDOUT;
+	return 0;
 }
 EXPORT_SYMBOL(ib_umem_dmabuf_map_pages);
 
