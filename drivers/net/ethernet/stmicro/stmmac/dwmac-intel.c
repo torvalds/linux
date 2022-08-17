e34cfee65ec891a319ce79797dda18083af33a76
Wong Vee Khee <veekhee@apple.com>
2022-08-17 14:43:24 +0800

    stmmac: intel: remove unused 'has_crossts' flag
    The 'has_crossts' flag was not used anywhere in the stmmac driver,
    removing it from both header file and dwmac-intel driver.
    
    Signed-off-by: Wong Vee Khee <veekhee@apple.com>
    Reviewed-by: Kurt Kanzenbach <kurt@linutronix.de>
    Link: https://lore.kernel.org/r/20220817064324.10025-1-veekhee@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-intel.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-intel.c
@@ -610,7 +610,6 @@ static int intel_mgbe_common_data(struct pci_dev *pdev,
 	plat->int_snapshot_num = AUX_SNAPSHOT1;
 	plat->ext_snapshot_num = AUX_SNAPSHOT0;
 
-	plat->has_crossts = true;
 	plat->crosststamp = intel_crosststamp;
 	plat->int_snapshot_en = 0;
 
