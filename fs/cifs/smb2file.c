98127f140bc4db0d19e6e32ed44ed09008a27df0
Enzo Matsumiya <ematsumiya@suse.de>
2022-08-17 16:08:34 -0300

    cifs: remove useless parameter 'is_fsctl' from SMB2_ioctl()
    [ Upstream commit 400d0ad63b190895e29f43bc75b1260111d3fd34 ]
    
    SMB2_ioctl() is always called with is_fsctl = true, so doesn't make any
    sense to have it at all.
    
    Thus, always set SMB2_0_IOCTL_IS_FSCTL flag on the request.
    
    Also, as per MS-SMB2 3.3.5.15 "Receiving an SMB2 IOCTL Request", servers
    must fail the request if the request flags is zero anyway.
    
    Signed-off-by: Enzo Matsumiya <ematsumiya@suse.de>
    Reviewed-by: Tom Talpey <tom@talpey.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/cifs/smb2file.c
+++ fs/cifs/smb2file.c
@@ -74,7 +74,6 @@ smb2_open_file(const unsigned int xid, struct cifs_open_parms *oparms,
 		nr_ioctl_req.Reserved = 0;
 		rc = SMB2_ioctl(xid, oparms->tcon, fid->persistent_fid,
 			fid->volatile_fid, FSCTL_LMR_REQUEST_RESILIENCY,
-			true /* is_fsctl */,
 			(char *)&nr_ioctl_req, sizeof(nr_ioctl_req),
 			CIFSMaxBufSize, NULL, NULL /* no return info */);
 		if (rc == -EOPNOTSUPP) {
