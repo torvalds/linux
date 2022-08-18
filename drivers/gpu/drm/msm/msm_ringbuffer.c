125e03b2b2bec7e749a3d5695febcfebc076481c
Akhil P Oommen <quic_akhilpo@quicinc.com>
2022-08-19 01:52:09 +0530

    drm/msm: Remove unnecessary pm_runtime_get/put
    We already enable gpu power from msm_gpu_submit(), so avoid a duplicate
    pm_runtime_get/put from msm_job_run().
    
    Signed-off-by: Akhil P Oommen <quic_akhilpo@quicinc.com>
    Patchwork: https://patchwork.freedesktop.org/patch/498390/
    Link: https://lore.kernel.org/r/20220819015030.v5.1.Icf1e8f0c9b3e7e9933c3b48c70477d0582f3243f@changeid
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/msm_ringbuffer.c
+++ drivers/gpu/drm/msm/msm_ringbuffer.c
@@ -29,16 +29,12 @@ static struct dma_fence *msm_job_run(struct drm_sched_job *job)
 		msm_gem_unlock(obj);
 	}
 
-	pm_runtime_get_sync(&gpu->pdev->dev);
-
 	/* TODO move submit path over to using a per-ring lock.. */
 	mutex_lock(&gpu->lock);
 
 	msm_gpu_submit(gpu, submit);
 
 	mutex_unlock(&gpu->lock);
-
-	pm_runtime_put(&gpu->pdev->dev);
 
 	return dma_fence_get(submit->hw_fence);
 }
