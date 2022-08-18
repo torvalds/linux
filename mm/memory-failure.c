6bbabd041dfd4823c752940286656d404621bf38
Miaohe Lin <linmiaohe@huawei.com>
2022-08-18 21:00:12 +0800

    mm, hwpoison: fix page refcnt leaking in unpoison_memory()
    When free_raw_hwp_pages() fails its work, the refcnt of the hugetlb page
    would have been incremented if ret > 0.  Using put_page() to fix refcnt
    leaking in this case.
    
    Link: https://lkml.kernel.org/r/20220818130016.45313-3-linmiaohe@huawei.com
    Fixes: debb6b9c3fdd ("mm, hwpoison: make unpoison aware of raw error info in hwpoisoned hugepage")
    Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
    Acked-by: Naoya Horiguchi <naoya.horiguchi@nec.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -2378,6 +2378,7 @@ int unpoison_memory(unsigned long pfn)
 			count = free_raw_hwp_pages(page, false);
 			if (count == 0) {
 				ret = -EBUSY;
+				put_page(page);
 				goto unlock_mutex;
 			}
 		}
