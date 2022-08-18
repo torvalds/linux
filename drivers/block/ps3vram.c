e55e1b4831563e5766f88fa821f5bfac0d6c298c
Wolfram Sang <wsa+renesas@sang-engineering.com>
2022-08-18 22:59:57 +0200

    block: move from strlcpy with unused retval to strscpy
    Follow the advice of the below link and prefer 'strscpy' in this
    subsystem. Conversion is 1:1 because the return value is not used.
    Generated by a coccinelle script.
    
    Link: https://lore.kernel.org/r/CAHk-=wgfRnXz0W3D37d01q3JFkr_i_uTL=V6A6G1oUZcprmknw@mail.gmail.com/
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Acked-by: Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
    Acked-by: Geoff Levand <geoff@infradead.org>
    Link: https://lore.kernel.org/r/20220818205958.6552-1-wsa+renesas@sang-engineering.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/ps3vram.c
+++ drivers/block/ps3vram.c
@@ -745,7 +745,7 @@ static int ps3vram_probe(struct ps3_system_bus_device *dev)
 	gendisk->flags |= GENHD_FL_NO_PART;
 	gendisk->fops = &ps3vram_fops;
 	gendisk->private_data = dev;
-	strlcpy(gendisk->disk_name, DEVICE_NAME, sizeof(gendisk->disk_name));
+	strscpy(gendisk->disk_name, DEVICE_NAME, sizeof(gendisk->disk_name));
 	set_capacity(gendisk, priv->size >> 9);
 	blk_queue_max_segments(gendisk->queue, BLK_MAX_SEGMENTS);
 	blk_queue_max_segment_size(gendisk->queue, BLK_MAX_SEGMENT_SIZE);
