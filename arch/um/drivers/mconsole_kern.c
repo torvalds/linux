758dfdb9185cf94160f20e85bbe05583e3cd4ff4
Guilherme G. Piccoli <gpiccoli@igalia.com>
2022-08-19 19:17:24 -0300

    um: Improve panic notifiers consistency and ordering
    Currently the panic notifiers from user mode linux don't follow
    the convention for most of the other notifiers present in the
    kernel (indentation, priority setting, numeric return).
    More important, the priorities could be improved, since it's a
    special case (userspace), hence we could run the notifiers earlier;
    user mode linux shouldn't care much with other panic notifiers but
    the ordering among the mconsole and arch notifier is important,
    given that the arch one effectively triggers a core dump.
    
    Fix that by running the mconsole notifier as the first panic
    notifier, followed by the architecture one (that coredumps).
    
    Cc: Anton Ivanov <anton.ivanov@cambridgegreys.com>
    Cc: Johannes Berg <johannes@sipsolutions.net>
    Cc: Richard Weinberger <richard@nod.at>
    Signed-off-by: Guilherme G. Piccoli <gpiccoli@igalia.com>
    
    V3:
    - No changes.
    
    V2:
    - Kept the notifier header to avoid implicit usage - thanks
    Johannes for the suggestion!
    
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/drivers/mconsole_kern.c
+++ arch/um/drivers/mconsole_kern.c
@@ -846,13 +846,12 @@ static int notify_panic(struct notifier_block *self, unsigned long unused1,
 
 	mconsole_notify(notify_socket, MCONSOLE_PANIC, message,
 			strlen(message) + 1);
-	return 0;
+	return NOTIFY_DONE;
 }
 
 static struct notifier_block panic_exit_notifier = {
-	.notifier_call 		= notify_panic,
-	.next 			= NULL,
-	.priority 		= 1
+	.notifier_call	= notify_panic,
+	.priority	= INT_MAX, /* run as soon as possible */
 };
 
 static int add_notifier(void)
--- arch/um/kernel/um_arch.c
+++ arch/um/kernel/um_arch.c
@@ -247,13 +247,13 @@ static int panic_exit(struct notifier_block *self, unsigned long unused1,
 	bust_spinlocks(0);
 	uml_exitcode = 1;
 	os_dump_core();
-	return 0;
+
+	return NOTIFY_DONE;
 }
 
 static struct notifier_block panic_exit_notifier = {
-	.notifier_call 		= panic_exit,
-	.next 			= NULL,
-	.priority 		= 0
+	.notifier_call	= panic_exit,
+	.priority	= INT_MAX - 1, /* run as 2nd notifier, won't return */
 };
 
 void uml_finishsetup(void)
