ea2aa97ca37a9044ade001aef71dbc06318e8d44
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-08-19 15:28:34 +0800

    drm/gem: Fix GEM handle release errors
    Currently we are assuming a one to one mapping between dmabuf and
    GEM handle when releasing GEM handles.
    
    But that is not always true, since we would create extra handles for the
    GEM obj in cases like gem_open() and getfb{,2}().
    
    A similar issue was reported at:
    https://lore.kernel.org/all/20211105083308.392156-1-jay.xu@rock-chips.com/
    
    Another problem is that the imported dmabuf might not always have
    gem_obj->dma_buf set, which would cause leaks in
    drm_gem_remove_prime_handles().
    
    Let's fix these for now by using handle to find the exact map to remove.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220819072834.17888-1-jeffy.chen@rock-chips.com

--- drivers/gpu/drm/drm_internal.h
+++ drivers/gpu/drm/drm_internal.h
@@ -74,8 +74,8 @@ int drm_prime_fd_to_handle_ioctl(struct drm_device *dev, void *data,
 
 void drm_prime_init_file_private(struct drm_prime_file_private *prime_fpriv);
 void drm_prime_destroy_file_private(struct drm_prime_file_private *prime_fpriv);
-void drm_prime_remove_buf_handle_locked(struct drm_prime_file_private *prime_fpriv,
-					struct dma_buf *dma_buf);
+void drm_prime_remove_buf_handle(struct drm_prime_file_private *prime_fpriv,
+				 uint32_t handle);
 
 /* drm_drv.c */
 struct drm_minor *drm_minor_acquire(unsigned int minor_id);
