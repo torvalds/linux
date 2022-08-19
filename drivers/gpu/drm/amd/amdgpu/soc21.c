828418259254863e0af5805bd712284e2bd88e3b
Yifan Zha <Yifan.Zha@amd.com>
2022-08-19 11:02:19 +0800

    drm/admgpu: Skip CG/PG on SOC21 under SRIOV VF
    [Why]
    There is no CG(Clock Gating)/PG(Power Gating) requirement on SRIOV VF.
    For multi VF, VF should not enable any CG/PG features.
    For one VF, PF will program CG/PG related registers.
    
    [How]
    Do not set any cg/pg flag bit at early init under sriov.
    
    Acked-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Yifan Zha <Yifan.Zha@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/soc21.c
+++ drivers/gpu/drm/amd/amdgpu/soc21.c
@@ -582,6 +582,10 @@ static int soc21_common_early_init(void *handle)
 			AMD_PG_SUPPORT_JPEG |
 			AMD_PG_SUPPORT_ATHUB |
 			AMD_PG_SUPPORT_MMHUB;
+		if (amdgpu_sriov_vf(adev)) {
+			adev->cg_flags = 0;
+			adev->pg_flags = 0;
+		}
 		adev->external_rev_id = adev->rev_id + 0x1; // TODO: need update
 		break;
 	case IP_VERSION(11, 0, 2):
