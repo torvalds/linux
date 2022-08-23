12014eaf1b3fa601ff544585180307cbf83a8ee8
Jaegeuk Kim <jaegeuk@kernel.org>
2022-08-23 10:18:42 -0700

    f2fs: increase the limit for reserve_root
    commit da35fe96d12d15779f3cb74929b7ed03941cf983 upstream.
    
    This patch increases the threshold that limits the reserved root space from 0.2%
    to 12.5% by using simple shift operation.
    
    Typically Android sets 128MB, but if the storage capacity is 32GB, 0.2% which is
    around 64MB becomes too small. Let's relax it.
    
    Cc: stable@vger.kernel.org
    Reported-by: Aran Dalton <arda@allwinnertech.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -267,10 +267,10 @@ static int f2fs_sb_read_encoding(const struct f2fs_super_block *sb,
 
 static inline void limit_reserve_root(struct f2fs_sb_info *sbi)
 {
-	block_t limit = min((sbi->user_block_count << 1) / 1000,
+	block_t limit = min((sbi->user_block_count >> 3),
 			sbi->user_block_count - sbi->reserved_blocks);
 
-	/* limit is 0.2% */
+	/* limit is 12.5% */
 	if (test_opt(sbi, RESERVE_ROOT) &&
 			F2FS_OPTION(sbi).root_reserved_blocks > limit) {
 		F2FS_OPTION(sbi).root_reserved_blocks = limit;
