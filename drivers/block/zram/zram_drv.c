641608f36244eb01028c2b30a961c7242144d96a
Alexey Romanov <avromanov@sberdevices.ru>
2022-08-24 14:31:17 +0300

    zram: don't retry compress incompressible page
    It doesn't make sense for us to retry to compress an uncompressible page
    (comp_len == PAGE_SIZE) in zsmalloc slowpath, because we will be storing
    it uncompressed anyway.  We can avoid wasting time on another compression
    attempt.  It is enough to take lock (zcomp_stream_get) and execute the
    code below.
    
    Link: https://lkml.kernel.org/r/20220824113117.78849-1-avromanov@sberdevices.ru
    Signed-off-by: Alexey Romanov <avromanov@sberdevices.ru>
    Signed-off-by: Dmitry Rokosov <ddrokosov@sberdevices.ru>
    Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Alexey Romanov <avromanov@sberdevices.ru>
    Cc: Dmitry Rokosov <DDRokosov@sberdevices.ru>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1410,9 +1410,19 @@ compress_again:
 		handle = zs_malloc(zram->mem_pool, comp_len,
 				GFP_NOIO | __GFP_HIGHMEM |
 				__GFP_MOVABLE);
-		if (!IS_ERR((void *)handle))
+		if (IS_ERR((void *)handle))
+			return PTR_ERR((void *)handle);
+
+		if (comp_len != PAGE_SIZE)
 			goto compress_again;
-		return PTR_ERR((void *)handle);
+		/*
+		 * If the page is not compressible, you need to acquire the lock and
+		 * execute the code below. The zcomp_stream_get() call is needed to
+		 * disable the cpu hotplug and grab the zstrm buffer back.
+		 * It is necessary that the dereferencing of the zstrm variable below
+		 * occurs correctly.
+		 */
+		zstrm = zcomp_stream_get(zram->comp);
 	}
 
 	alloced_pages = zs_get_total_pages(zram->mem_pool);
