025b5a487b16846233159ca02071b22de7e121e4
xiaofeng <xiaofeng5@xiaomi.com>
2022-08-24 19:34:06 +0800

    ANDROID: vendor_hooks:vendor hook for __alloc_pages_slowpath.
    add vendor hook in __alloc_pages_slowpath ahead of
    __alloc_pages_direct_reclaim and warn_alloc.
    
    Bug: 243629905
    Change-Id: Ieacc6cf79823c0bfacfdeec9afb55ed66f40d0b0
    Signed-off-by: xiaofeng <xiaofeng5@xiaomi.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -65,6 +65,16 @@ DECLARE_HOOK(android_vh_cache_show,
 	TP_PROTO(struct seq_file *m, struct slabinfo *sinfo, struct kmem_cache *s),
 	TP_ARGS(m, sinfo, s));
 
+DECLARE_HOOK(android_vh_alloc_pages_reclaim_bypass,
+    TP_PROTO(gfp_t gfp_mask, int order, int alloc_flags,
+	int migratetype, struct page **page),
+	TP_ARGS(gfp_mask, order, alloc_flags, migratetype, page));
+
+DECLARE_HOOK(android_vh_alloc_pages_failure_bypass,
+	TP_PROTO(gfp_t gfp_mask, int order, int alloc_flags,
+	int migratetype, struct page **page),
+	TP_ARGS(gfp_mask, order, alloc_flags, migratetype, page));
+
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
