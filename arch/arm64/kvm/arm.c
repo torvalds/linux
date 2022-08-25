34305ad4b1bbf0b1938712ef0070a78d8b2c3583
Vincent Donnefort <vdonnefort@google.com>
2022-08-25 11:14:12 +0100

    ANDROID: KVM: arm64: Add host support for the nVHE hyp tracing
    Now the nVHE hypervisor can write into a ring buffer. Add some support
    to start the tracing and stop it.
    
    This comes with a simple userspace interface tracing_on and per-CPU:
    
      - trace.X: non-consuming read of the ring buffer.
      - trace_pipe.X: consuming read of the ring buffer.
    
    The hypervisor support to declare and decode hyp events will be
    introduced later.
    
    Bug: 229972309
    Change-Id: I803b1fae8aaa5920d24adc547a9b5cc1c83d77f1
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -27,6 +27,8 @@
 #define CREATE_TRACE_POINTS
 #include "trace_arm.h"
 
+#include "hyp_trace.h"
+
 #include <linux/uaccess.h>
 #include <asm/ptrace.h>
 #include <asm/mman.h>
@@ -2391,6 +2393,10 @@ int kvm_arch_init(void *opaque)
 			kvm_err("Failed to finalize Hyp protection\n");
 			goto out_hyp;
 		}
+
+		err = init_hyp_tracefs();
+		if (err)
+			kvm_err("Failed to initialize Hyp tracing\n");
 	}
 
 	if (is_protected_kvm_enabled()) {
