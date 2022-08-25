d8daa3991652a5713061f23276dc91fb846dc0c0
Szabolcs Sipos <labuwx@balfug.com>
2022-08-26 00:42:07 +0200

    Bluetooth: btusb: RTL8761BUV consistent naming
    Based on photos available from the FCC, all currently supported RTL8761B
    series USB dongles have the same chip: RTL8761BUV.
    
    rtl8761bu is often used to refer to this chip.
    rtl8761b sometimes refers to this chip, and other times to its
    UART variant (RTL8761BTV).
    
    +----------------+---------+---------+---------------+
    |     Dongle     | USB VID | USB PID |    FCC ID     |
    +----------------+---------+---------+---------------+
    | ASUS USB-BT500 | 0x0b05  | 0x190e  | MSQ-USBBTJB00 |
    | TP-Link UB500  | 0x2357  | 0x0604  | 2AXJ4UB500    |
    | EDUP EP-B3519  |         |         | 2AHRD-EPB3519 |
    | EDUP EP-B3536  |         |         | 2AHRDEP-B3536 |
    | UGREEN CM390   |         |         | 2AQI5-CM390   |
    +----------------+---------+---------+---------------+
    
    Signed-off-by: Szabolcs Sipos <labuwx@balfug.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -521,17 +521,13 @@ static const struct usb_device_id blacklist_table[] = {
 	{ USB_DEVICE(0x0bda, 0xb009), .driver_info = BTUSB_REALTEK },
 	{ USB_DEVICE(0x2ff8, 0xb011), .driver_info = BTUSB_REALTEK },
 
-	/* Additional Realtek 8761B Bluetooth devices */
+	/* Additional Realtek 8761BUV Bluetooth devices */
 	{ USB_DEVICE(0x2357, 0x0604), .driver_info = BTUSB_REALTEK |
 						     BTUSB_WIDEBAND_SPEECH },
-
-	/* Additional Realtek 8761BU Bluetooth devices */
 	{ USB_DEVICE(0x0b05, 0x190e), .driver_info = BTUSB_REALTEK |
 	  					     BTUSB_WIDEBAND_SPEECH },
 	{ USB_DEVICE(0x2550, 0x8761), .driver_info = BTUSB_REALTEK |
 						     BTUSB_WIDEBAND_SPEECH },
-
-	/* Additional Realtek 8761BUV Bluetooth devices */
 	{ USB_DEVICE(0x0bda, 0x8771), .driver_info = BTUSB_REALTEK |
 						     BTUSB_WIDEBAND_SPEECH },
 
