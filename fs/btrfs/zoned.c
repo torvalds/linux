7da1afa444a19dec5ac49c2f8eae1838562a18f1
Shin'ichiro Kawasaki <shinichiro.kawasaki@wdc.com>
2022-08-26 16:42:15 +0900

    btrfs: zoned: set pseudo max append zone limit in zone emulation mode
    commit cac5c44c48c9fb9cc31bea15ebd9ef0c6462314f upstream.
    
    The commit 7d7672bc5d10 ("btrfs: convert count_max_extents() to use
    fs_info->max_extent_size") introduced a division by
    fs_info->max_extent_size. This max_extent_size is initialized with max
    zone append limit size of the device btrfs runs on. However, in zone
    emulation mode, the device is not zoned then its zone append limit is
    zero. This resulted in zero value of fs_info->max_extent_size and caused
    zero division error.
    
    Fix the error by setting non-zero pseudo value to max append zone limit
    in zone emulation mode. Set the pseudo value based on max_segments as
    suggested in the commit c2ae7b772ef4 ("btrfs: zoned: revive
    max_zone_append_bytes").
    
    Fixes: 7d7672bc5d10 ("btrfs: convert count_max_extents() to use fs_info->max_extent_size")
    CC: stable@vger.kernel.org # 5.12+
    Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Reviewed-by: Naohiro Aota <naohiro.aota@wdc.com>
    Signed-off-by: Shin'ichiro Kawasaki <shinichiro.kawasaki@wdc.com>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/btrfs/zoned.c
+++ fs/btrfs/zoned.c
@@ -392,10 +392,19 @@ int btrfs_get_dev_zone_info(struct btrfs_device *device, bool populate_cache)
 	 * since btrfs adds the pages one by one to a bio, and btrfs cannot
 	 * increase the metadata reservation even if it increases the number of
 	 * extents, it is safe to stick with the limit.
+	 *
+	 * With the zoned emulation, we can have non-zoned device on the zoned
+	 * mode. In this case, we don't have a valid max zone append size. So,
+	 * use max_segments * PAGE_SIZE as the pseudo max_zone_append_size.
 	 */
-	zone_info->max_zone_append_size =
-		min_t(u64, (u64)bdev_max_zone_append_sectors(bdev) << SECTOR_SHIFT,
-		      (u64)bdev_max_segments(bdev) << PAGE_SHIFT);
+	if (bdev_is_zoned(bdev)) {
+		zone_info->max_zone_append_size = min_t(u64,
+			(u64)bdev_max_zone_append_sectors(bdev) << SECTOR_SHIFT,
+			(u64)bdev_max_segments(bdev) << PAGE_SHIFT);
+	} else {
+		zone_info->max_zone_append_size =
+			(u64)bdev_max_segments(bdev) << PAGE_SHIFT;
+	}
 	if (!IS_ALIGNED(nr_sectors, zone_sectors))
 		zone_info->nr_zones++;
 
