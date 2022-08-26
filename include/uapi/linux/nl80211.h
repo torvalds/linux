a36c421690b3e5dee38fc12abfcabda742f00064
James Prestwood <prestwoj@gmail.com>
2022-08-26 10:00:31 -0700

    wifi: nl80211: Add POWERED_ADDR_CHANGE feature
    Add a new extended feature bit signifying that the wireless hardware
    supports changing the MAC address while the underlying net_device is
    powered. Note that this has a different meaning from
    IFF_LIVE_ADDR_CHANGE as additional restrictions might be imposed by
    the hardware, such as:
    
     - No connection is active on this interface, carrier is off
     - No scan is in progress
     - No offchannel operations are in progress
    
    Signed-off-by: James Prestwood <prestwoj@gmail.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- include/uapi/linux/nl80211.h
+++ include/uapi/linux/nl80211.h
@@ -6281,6 +6281,14 @@ enum nl80211_feature_flags {
  * @NL80211_EXT_FEATURE_RADAR_BACKGROUND: Device supports background radar/CAC
  *	detection.
  *
+ * @NL80211_EXT_FEATURE_POWERED_ADDR_CHANGE: Device can perform a MAC address
+ *	change without having to bring the underlying network device down
+ *	first. For example, in station mode this can be used to vary the
+ *	origin MAC address prior to a connection to a new AP for privacy
+ *	or other reasons. Note that certain driver specific restrictions
+ *	might apply, e.g. no scans in progress, no offchannel operations
+ *	in progress, and no active connections.
+ *
  * @NUM_NL80211_EXT_FEATURES: number of extended features.
  * @MAX_NL80211_EXT_FEATURES: highest extended feature index.
  */
@@ -6348,6 +6356,7 @@ enum nl80211_ext_feature_index {
 	NL80211_EXT_FEATURE_BSS_COLOR,
 	NL80211_EXT_FEATURE_FILS_CRYPTO_OFFLOAD,
 	NL80211_EXT_FEATURE_RADAR_BACKGROUND,
+	NL80211_EXT_FEATURE_POWERED_ADDR_CHANGE,
 
 	/* add new features before the definition below */
 	NUM_NL80211_EXT_FEATURES,
