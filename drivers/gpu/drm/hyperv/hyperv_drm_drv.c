8409fe92d88c332923130149fe209d1c882b286e
Vitaly Kuznetsov <vkuznets@redhat.com>
2022-08-27 15:03:43 +0200

    PCI: Move PCI_VENDOR_ID_MICROSOFT/PCI_DEVICE_ID_HYPERV_VIDEO definitions to pci_ids.h
    There are already three places in kernel which define
    PCI_VENDOR_ID_MICROSOFT and two for PCI_DEVICE_ID_HYPERV_VIDEO and
    there's a need to use these from core VMBus code. Move the defines where
    they belong.
    
    No functional change.
    
    Reviewed-by: Michael Kelley <mikelley@microsoft.com>
    Acked-by: Bjorn Helgaas <bhelgaas@google.com> # pci_ids.h
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Link: https://lore.kernel.org/r/20220827130345.1320254-2-vkuznets@redhat.com
    Signed-off-by: Wei Liu <wei.liu@kernel.org>

--- drivers/gpu/drm/hyperv/hyperv_drm_drv.c
+++ drivers/gpu/drm/hyperv/hyperv_drm_drv.c
@@ -23,9 +23,6 @@
 #define DRIVER_MAJOR 1
 #define DRIVER_MINOR 0
 
-#define PCI_VENDOR_ID_MICROSOFT 0x1414
-#define PCI_DEVICE_ID_HYPERV_VIDEO 0x5353
-
 DEFINE_DRM_GEM_FOPS(hv_fops);
 
 static struct drm_driver hyperv_driver = {
