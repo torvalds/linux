d3212c2dbababf849d940f5f7001f4fde222b888
Schspa Shi <schspa@gmail.com>
2022-08-29 01:03:02 +0800

    UPSTREAM: sched/rt: Fix bad task migration for rt tasks
    Commit 95158a89dd50 ("sched,rt: Use the full cpumask for balancing")
    allows find_lock_lowest_rq() to pick a task with migration disabled.
    The purpose of the commit is to push the current running task on the
    CPU that has the migrate_disable() task away.
    
    However, there is a race which allows a migrate_disable() task to be
    migrated. Consider:
    
      CPU0                                    CPU1
      push_rt_task
        check is_migration_disabled(next_task)
    
                                              task not running and
                                              migration_disabled == 0
    
        find_lock_lowest_rq(next_task, rq);
          _double_lock_balance(this_rq, busiest);
            raw_spin_rq_unlock(this_rq);
            double_rq_lock(this_rq, busiest);
              <<wait for busiest rq>>
                                                  <wakeup>
                                              task become running
                                              migrate_disable();
                                                <context out>
        deactivate_task(rq, next_task, 0);
        set_task_cpu(next_task, lowest_rq->cpu);
          WARN_ON_ONCE(is_migration_disabled(p));
    
    Fixes: 95158a89dd50 ("sched,rt: Use the full cpumask for balancing")
    Signed-off-by: Schspa Shi <schspa@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Tested-by: Dwaine Gonyier <dgonyier@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    
    Bug: 300418678
    (cherry picked from commit feffe5bb274dd3442080ef0e4053746091878799)
    [quic_dickey@quicinc.com: Port only]
    Change-Id: I3e7953aeb3edf3f1a5d03e355297d7b1541ff7c8
    Signed-off-by: Stephen Dickey <quic_dickey@quicinc.com>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -2246,6 +2246,7 @@ static struct rq *find_lock_later_rq(struct task_struct *task, struct rq *rq)
 				     !cpumask_test_cpu(later_rq->cpu, &task->cpus_mask) ||
 				     task_on_cpu(rq, task) ||
 				     !dl_task(task) ||
+				     is_migration_disabled(task) ||
 				     !task_on_rq_queued(task))) {
 				double_unlock_balance(rq, later_rq);
 				later_rq = NULL;
