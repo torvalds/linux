76ff33468beaabbd0fe141b3ea1c6f514a8ef7d4
Carlos Llamas <cmllamas@google.com>
2022-08-29 20:12:49 +0000

    binder: fix trivial kernel-doc typo
    Correct the misspelling of 'invariant' in kernel-doc section.
    
    No functional changes in this patch.
    
    Reviewed-by: Christian Brauner (Microsoft) <brauner@kernel.org>
    Acked-by: Todd Kjos <tkjos@google.com>
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    Link: https://lore.kernel.org/r/20220829201254.1814484-3-cmllamas@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/android/binder_alloc.h
+++ drivers/android/binder_alloc.h
@@ -75,10 +75,10 @@ struct binder_lru_page {
 /**
  * struct binder_alloc - per-binder proc state for binder allocator
  * @vma:                vm_area_struct passed to mmap_handler
- *                      (invarient after mmap)
+ *                      (invariant after mmap)
  * @tsk:                tid for task that called init for this proc
  *                      (invariant after init)
- * @vma_vm_mm:          copy of vma->vm_mm (invarient after mmap)
+ * @vma_vm_mm:          copy of vma->vm_mm (invariant after mmap)
  * @buffer:             base of per-proc address space mapped via mmap
  * @buffers:            list of all buffers for this proc
  * @free_buffers:       rb tree of buffers available for allocation
