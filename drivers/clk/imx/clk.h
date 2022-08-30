0836c8604a0bfaed2396d7e2aecb4146f8c07cca
Peng Fan <peng.fan@nxp.com>
2022-08-30 11:31:34 +0800

    clk: imx: add i.MX93 clk gate
    i.MX93 LPCG is different from i.MX8M CCGR. Although imx_clk_hw_gate4_flags
    is used here, it not strictly match i.MX93. i.MX93 has such design:
     - LPCG_DIRECT use BIT0 as on/off gate when LPCG_AUTHEN CPU_LPM is 0
     - LPCG_LPM_CUR use BIT[2:0] as on/off gate when LPCG_AUTHEN CPU_LPM is 1
    
    The current implementation suppose CPU_LPM is 0, and use LPCG_DIRECT
    BIT[1:0] as on/off gate. Although BIT1 is touched, actually BIT1 is
    reserved.
    
    And imx_clk_hw_gate4_flags use mask 0x3 to determine whether the clk
    is enabled or not, but i.MX93 LPCG only use BIT0 to control when CPU_LPM
    is 0. So clk disabled unused during kernel boot not able to gate off
    the unused clocks.
    
    To match i.MX93 LPCG, introduce imx93_clk_gate.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>
    Reviewed-by: Abel Vesa <abel.vesa@linaro.org>
    Signed-off-by: Abel Vesa <abel.vesa@linaro.org>
    Link: https://lore.kernel.org/r/20220830033137.4149542-6-peng.fan@oss.nxp.com

--- drivers/clk/imx/clk.h
+++ drivers/clk/imx/clk.h
@@ -451,6 +451,10 @@ struct clk_hw *imx93_clk_composite_flags(const char *name,
 	imx93_clk_composite_flags(name, parent_names, num_parents, reg, domain_id \
 				  CLK_SET_RATE_NO_REPARENT | CLK_OPS_PARENT_ENABLE)
 
+struct clk_hw *imx93_clk_gate(struct device *dev, const char *name, const char *parent_name,
+			      unsigned long flags, void __iomem *reg, u32 bit_idx, u32 val,
+			      u32 mask, u32 domain_id, unsigned int *share_count);
+
 struct clk_hw *imx_clk_hw_divider_gate(const char *name, const char *parent_name,
 		unsigned long flags, void __iomem *reg, u8 shift, u8 width,
 		u8 clk_divider_flags, const struct clk_div_table *table,
