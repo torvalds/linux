8770b9e575ce5727b4f95b6bcb086247dcce4eb3
Linus Walleij <linus.walleij@linaro.org>
2022-08-31 13:28:02 +0100

    ARM: 9240/1: dma-mapping: Pass (void *) to virt_to_page()
    Pointers to virtual memory functions are (void *) but the
    __dma_update_pte() function is passing an unsigned long.
    Fix this up by explicit cast.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -307,7 +307,7 @@ void __init dma_contiguous_remap(void)
 
 static int __dma_update_pte(pte_t *pte, unsigned long addr, void *data)
 {
-	struct page *page = virt_to_page(addr);
+	struct page *page = virt_to_page((void *)addr);
 	pgprot_t prot = *(pgprot_t *)data;
 
 	set_pte_ext(pte, mk_pte(page, prot), 0);
