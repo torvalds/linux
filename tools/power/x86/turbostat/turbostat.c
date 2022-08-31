9b1c2ecfa02bc2645e6e9d55f0f39bc191991270
Zhang Rui <rui.zhang@intel.com>
2022-08-31 14:49:57 +0800

    tools/power turbostat: Add support for RPL-S
    Add turbostat support for RAPTORLAKE_S platform, which behaves the same
    as RAPTORLAKE and RAPTORLAKE_P platforms.
    
    RPL-S 601/801 have different CPU ID than the Hybrid ADL-S platforms.
    
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- tools/power/x86/turbostat/turbostat.c
+++ tools/power/x86/turbostat/turbostat.c
@@ -5447,6 +5447,7 @@ unsigned int intel_model_duplicates(unsigned int model)
 	case INTEL_FAM6_ALDERLAKE_N:
 	case INTEL_FAM6_RAPTORLAKE:
 	case INTEL_FAM6_RAPTORLAKE_P:
+	case INTEL_FAM6_RAPTORLAKE_S:
 		return INTEL_FAM6_CANNONLAKE_L;
 
 	case INTEL_FAM6_ATOM_TREMONT_L:
