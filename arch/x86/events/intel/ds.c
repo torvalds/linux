24919fdea6f8b31d7cdf32ac291bc5dd0b023878
Kan Liang <kan.liang@linux.intel.com>
2022-08-31 07:27:02 -0700

    perf/x86/intel: Fix unchecked MSR access error for Alder Lake N
    For some Alder Lake N machine, the below unchecked MSR access error may be
    triggered.
    
    [ 0.088017] rcu: Hierarchical SRCU implementation.
    [ 0.088017] unchecked MSR access error: WRMSR to 0x38f (tried to write
    0x0001000f0000003f) at rIP: 0xffffffffb5684de8 (native_write_msr+0x8/0x30)
    [ 0.088017] Call Trace:
    [ 0.088017] <TASK>
    [ 0.088017] __intel_pmu_enable_all.constprop.46+0x4a/0xa0
    
    The Alder Lake N only has e-cores. The X86_FEATURE_HYBRID_CPU flag is
    not set. The perf cannot retrieve the correct CPU type via
    get_this_hybrid_cpu_type(). The model specific get_hybrid_cpu_type() is
    hardcode to p-core. The wrong CPU type is given to the PMU of the
    Alder Lake N.
    
    Since Alder Lake N isn't in fact a hybrid CPU, remove ALDERLAKE_N from
    the rest of {ALDER,RAPTOP}LAKE and create a non-hybrid PMU setup.
    
    The differences between Gracemont and the previous Tremont are,
    - Number of GP counters
    - Load and store latency Events
    - PEBS event_constraints
    - Instruction Latency support
    - Data source encoding
    - Memory access latency encoding
    
    Fixes: c2a960f7c574 ("perf/x86: Add new Alder Lake and Raptor Lake support")
    Reported-by: Jianfeng Gao <jianfeng.gao@intel.com>
    Suggested-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20220831142702.153110-1-kan.liang@linux.intel.com

--- arch/x86/events/intel/ds.c
+++ arch/x86/events/intel/ds.c
@@ -110,11 +110,16 @@ void __init intel_pmu_pebs_data_source_skl(bool pmem)
 	__intel_pmu_pebs_data_source_skl(pmem, pebs_data_source);
 }
 
-static void __init intel_pmu_pebs_data_source_grt(u64 *data_source)
+static void __init __intel_pmu_pebs_data_source_grt(u64 *data_source)
 {
 	data_source[0x05] = OP_LH | P(LVL, L3) | LEVEL(L3) | P(SNOOP, HIT);
 	data_source[0x06] = OP_LH | P(LVL, L3) | LEVEL(L3) | P(SNOOP, HITM);
 	data_source[0x08] = OP_LH | P(LVL, L3) | LEVEL(L3) | P(SNOOPX, FWD);
+}
+
+void __init intel_pmu_pebs_data_source_grt(void)
+{
+	__intel_pmu_pebs_data_source_grt(pebs_data_source);
 }
 
 void __init intel_pmu_pebs_data_source_adl(void)
@@ -127,7 +132,7 @@ void __init intel_pmu_pebs_data_source_adl(void)
 
 	data_source = x86_pmu.hybrid_pmu[X86_HYBRID_PMU_ATOM_IDX].pebs_data_source;
 	memcpy(data_source, pebs_data_source, sizeof(pebs_data_source));
-	intel_pmu_pebs_data_source_grt(data_source);
+	__intel_pmu_pebs_data_source_grt(data_source);
 }
 
 static u64 precise_store_data(u64 status)
