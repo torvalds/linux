d189614f24799ce150eaf3ecca8483c0a3697e9b
Takahiro Kuwano <Takahiro.Kuwano@infineon.com>
2022-08-31 13:59:03 +0900

    mtd: spi-nor: sfdp: Update params->hwcaps.mask at xSPI profile 1.0 table parse
    Existece of xSPI profile 1.0 table implies that the flash supports read
    and program in 8D-8D-8D mode. Update the params->hwcaps.mask in
    spi_nor_parase_profile1().
    
    Signed-off-by: Takahiro Kuwano <Takahiro.Kuwano@infineon.com>
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Link: https://lore.kernel.org/r/1b449bae6978f11f7636f2b5acb6435723963f59.1661915569.git.Takahiro.Kuwano@infineon.com

--- drivers/mtd/spi-nor/sfdp.c
+++ drivers/mtd/spi-nor/sfdp.c
@@ -1183,9 +1183,16 @@ static int spi_nor_parse_profile1(struct spi_nor *nor,
 	dummy = round_up(dummy, 2);
 
 	/* Update the fast read settings. */
+	nor->params->hwcaps.mask |= SNOR_HWCAPS_READ_8_8_8_DTR;
 	spi_nor_set_read_settings(&nor->params->reads[SNOR_CMD_READ_8_8_8_DTR],
 				  0, dummy, opcode,
 				  SNOR_PROTO_8_8_8_DTR);
+
+	/*
+	 * Page Program is "Required Command" in the xSPI Profile 1.0. Update
+	 * the params->hwcaps.mask here.
+	 */
+	nor->params->hwcaps.mask |= SNOR_HWCAPS_PP_8_8_8_DTR;
 
 out:
 	kfree(dwords);
