d0eb9d07b37231f2537cf87cbe3735a2786e95f1
shengyang.chen <shengyang.chen@starfivetech.com>
2022-08-31 16:17:32 +0800

    riscv:linux:drm
    replace driver compatible value for vout
    
    Signed-off-by: shengyang.chen<shengyang.chen@starfivetech.com>

--- drivers/gpu/drm/verisilicon/inno_hdmi.c
+++ drivers/gpu/drm/verisilicon/inno_hdmi.c
@@ -1199,7 +1199,7 @@ static int inno_hdmi_remove(struct platform_device *pdev)
 }
 
 static const struct of_device_id inno_hdmi_dt_ids[] = {
-	{ .compatible = "rockchip,rk3036-inno-hdmi",
+	{ .compatible = "inno,hdmi",
 	},
 	{},
 };
@@ -1209,7 +1209,7 @@ struct platform_driver inno_hdmi_driver = {
 	.probe  = inno_hdmi_probe,
 	.remove = inno_hdmi_remove,
 	.driver = {
-		.name = "innohdmi-rockchip",
+		.name = "innohdmi-starfive",
 		.of_match_table = inno_hdmi_dt_ids,
 	},
 };
