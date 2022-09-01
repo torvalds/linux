2e5fb3223261366d1673c3827190c85a74b1aa56
Zhengchao Shao <shaozhengchao@huawei.com>
2022-09-01 09:16:17 +0800

    net/sched: cls_api: remove redundant 0 check in tcf_qevent_init()
    tcf_qevent_parse_block_index() never returns a zero block_index.
    Therefore, it is unnecessary to check the value of block_index in
    tcf_qevent_init().
    
    Signed-off-by: Zhengchao Shao <shaozhengchao@huawei.com>
    Link: https://lore.kernel.org/r/20220901011617.14105-1-shaozhengchao@huawei.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/sched/cls_api.c
+++ net/sched/cls_api.c
@@ -3629,9 +3629,6 @@ int tcf_qevent_init(struct tcf_qevent *qe, struct Qdisc *sch,
 	if (err)
 		return err;
 
-	if (!block_index)
-		return 0;
-
 	qe->info.binder_type = binder_type;
 	qe->info.chain_head_change = tcf_chain_head_change_dflt;
 	qe->info.chain_head_change_priv = &qe->filter_chain;
