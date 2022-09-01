2a4209d6a9cb51082813e3081e1172ae68d27935
Jim Mattson <jmattson@google.com>
2022-09-01 14:18:07 -0700

    KVM: x86: Advertise fast REP string features inherent to the CPU
    Fast zero-length REP MOVSB, fast short REP STOSB, and fast short REP
    {CMPSB,SCASB} are inherent features of the processor that cannot be
    hidden by the hypervisor. When these features are present on the host,
    enumerate them in KVM_GET_SUPPORTED_CPUID.
    
    Signed-off-by: Jim Mattson <jmattson@google.com>
    Reviewed-by: Sean Christopherson <seanjc@google.com>
    Link: https://lore.kernel.org/r/20220901211811.2883855-2-jmattson@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -664,8 +664,9 @@ void kvm_set_cpu_caps(void)
 		kvm_cpu_cap_set(X86_FEATURE_SPEC_CTRL_SSBD);
 
 	kvm_cpu_cap_mask(CPUID_7_1_EAX,
-		F(AVX_VNNI) | F(AVX512_BF16) | F(CMPCCXADD) | F(AMX_FP16) |
-		F(AVX_IFMA)
+		F(AVX_VNNI) | F(AVX512_BF16) | F(CMPCCXADD) |
+		F(FZRM) | F(FSRS) | F(FSRC) |
+		F(AMX_FP16) | F(AVX_IFMA)
 	);
 
 	kvm_cpu_cap_init_kvm_defined(CPUID_7_1_EDX,
