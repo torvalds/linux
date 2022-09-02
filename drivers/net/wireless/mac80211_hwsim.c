b2c4aa35ebcc5c7e941a46e7b5c07587bd01b4e2
Johannes Berg <johannes.berg@intel.com>
2022-09-02 16:12:35 +0200

    wifi: mac80211_hwsim: check STA magic in change_sta_links
    Just as an additional check that mac80211 isn't doing
    anything strange, add a check of the STA magic (which
    gets assigned when the station is added, and cleared
    when the station is removed).
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/mac80211_hwsim.c
+++ drivers/net/wireless/mac80211_hwsim.c
@@ -3021,6 +3021,8 @@ static int mac80211_hwsim_change_sta_links(struct ieee80211_hw *hw,
 					   struct ieee80211_sta *sta,
 					   u16 old_links, u16 new_links)
 {
+	hwsim_check_sta_magic(sta);
+
 	return 0;
 }
 
