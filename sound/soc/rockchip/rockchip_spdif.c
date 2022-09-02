d818ae0e4c41001278dca55586b07ac9f72ddc00
Sugar Zhang <sugar.zhang@rock-chips.com>
2022-09-02 17:18:00 +0800

    ASoC: rockchip: spdif: Add support for full samplerate
    This patch add support for 8000 ~ 192000 samplerate.
    
    Signed-off-by: Sugar Zhang <sugar.zhang@rock-chips.com>
    Change-Id: If588138b186d446b3f86b4e244d71a1ca3715b2e

--- sound/soc/rockchip/rockchip_spdif.c
+++ sound/soc/rockchip/rockchip_spdif.c
@@ -261,11 +261,7 @@ static struct snd_soc_dai_driver rk_spdif_dai = {
 		.stream_name = "Playback",
 		.channels_min = 2,
 		.channels_max = 2,
-		.rates = (SNDRV_PCM_RATE_32000 |
-			  SNDRV_PCM_RATE_44100 |
-			  SNDRV_PCM_RATE_48000 |
-			  SNDRV_PCM_RATE_96000 |
-			  SNDRV_PCM_RATE_192000),
+		.rates = SNDRV_PCM_RATE_8000_192000,
 		.formats = (SNDRV_PCM_FMTBIT_S16_LE |
 			    SNDRV_PCM_FMTBIT_S20_3LE |
 			    SNDRV_PCM_FMTBIT_S24_LE |
