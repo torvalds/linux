77bfc8bdb5a1cdbcea21daccdf1686e1bfea3db6
Linus Walleij <linus.walleij@linaro.org>
2022-09-04 00:45:25 +0200

    parisc: Remove 64bit access on 32bit machines
    The parisc was using some readq/writeq accessors without special
    considerations as to what will happen on 32bit CPUs if you do
    this. Maybe we have been lucky that it "just worked" on 32bit
    due to the compiler behaviour, or the code paths were never
    executed.
    
    Fix the two offending code sites like this:
    
    arch/parisc/lib/iomap.c:
    
    - Put ifdefs around the 64bit accessors and make sure
      that ioread64, ioread64be, iowrite64 and iowrite64be
      are not available on 32bit builds.
    
    - Also fold in a bug fix where 64bit access was by
      mistake using 32bit writel() accessors rather
      than 64bit writeq().
    
    drivers/parisc/sba_iommu.c:
    
    - Access any 64bit registers using _lo_hi-semantics by way
      of the readq and writeq operations provided by
      <linux/io-64-nonatomic-lo-hi.h>
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Cc: James E.J. Bottomley <James.Bottomley@HansenPartnership.com>
    Cc: Helge Deller <deller@gmx.de>
    Cc: linux-parisc@vger.kernel.org
    Cc: linux-arch@vger.kernel.org
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: John David Anglin <dave.anglin@bell.net>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/parisc/sba_iommu.c
+++ drivers/parisc/sba_iommu.c
@@ -28,6 +28,12 @@
 #include <linux/dma-map-ops.h>
 #include <linux/scatterlist.h>
 #include <linux/iommu-helper.h>
+/*
+ * The semantics of 64 register access on 32bit systems can't be guaranteed
+ * by the C standard, we hope the _lo_hi() macros defining readq and writeq
+ * here will behave as expected.
+ */
+#include <linux/io-64-nonatomic-lo-hi.h>
 
 #include <asm/byteorder.h>
 #include <asm/io.h>
