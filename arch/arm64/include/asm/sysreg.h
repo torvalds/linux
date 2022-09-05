a04054362e4a9f48a51a742215c2ae1b8bfd04b5
Mark Brown <broonie@kernel.org>
2022-09-05 23:53:58 +0100

    arm64/sysreg: Remove stray SMIDR_EL1 defines
    SMIDR_EL1 was converted to automatic generation but some of the constants
    for fields in it were mistakenly left, remove them.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Reviewed-by: Kristina Martsenko <kristina.martsenko@arm.com>
    Link: https://lore.kernel.org/r/20220905225425.1871461-2-broonie@kernel.org
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/sysreg.h
+++ arch/arm64/include/asm/sysreg.h
@@ -445,10 +445,6 @@
 #define SYS_CCSIDR_EL1			sys_reg(3, 1, 0, 0, 0)
 #define SYS_AIDR_EL1			sys_reg(3, 1, 0, 0, 7)
 
-#define SMIDR_EL1_IMPLEMENTER_SHIFT	24
-#define SMIDR_EL1_SMPS_SHIFT	15
-#define SMIDR_EL1_AFFINITY_SHIFT	0
-
 #define SYS_RNDR_EL0			sys_reg(3, 3, 2, 4, 0)
 #define SYS_RNDRRS_EL0			sys_reg(3, 3, 2, 4, 1)
 
