245050af5d15a4f518c130e896cb597a263a070b
John Garry <john.garry@huawei.com>
2022-09-05 19:48:45 +0800

    scsi: hisi_sas: Revert change to limit max hw sectors for v3 HW
    Now that libsas and the SCSI core code limits the default sectors from
    commit 4cbfca5f7750 ("scsi: scsi_transport_sas: cap shost opt_sectors
    according to DMA optimal limit") and commit 608128d391fa ("scsi: sd: allow
    max_sectors be capped at DMA optimal size limit"), there is no need for
    the hack to limit the max HW sectors.
    
    Link: https://lore.kernel.org/r/1662378529-101489-2-git-send-email-john.garry@huawei.com
    Signed-off-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
+++ drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
@@ -2786,7 +2786,6 @@ static int slave_configure_v3_hw(struct scsi_device *sdev)
 	struct hisi_hba *hisi_hba = shost_priv(shost);
 	int ret = hisi_sas_slave_configure(sdev);
 	struct device *dev = hisi_hba->dev;
-	unsigned int max_sectors;
 
 	if (ret)
 		return ret;
@@ -2801,12 +2800,6 @@ static int slave_configure_v3_hw(struct scsi_device *sdev)
 			pm_runtime_disable(dev);
 		}
 	}
-
-	/* Set according to IOMMU IOVA caching limit */
-	max_sectors = min_t(size_t, queue_max_hw_sectors(sdev->request_queue),
-			    (PAGE_SIZE * 32) >> SECTOR_SHIFT);
-
-	blk_queue_max_hw_sectors(sdev->request_queue, max_sectors);
 
 	return 0;
 }
