98e8e67395cc6d0cdf3a771f86ea42d0ee6e59dd
Dongxiang Ke <kdx.glider@gmail.com>
2022-09-06 10:49:28 +0800

    ALSA: usb-audio: Fix an out-of-bounds bug in __snd_usb_parse_audio_interface()
    commit e53f47f6c1a56d2af728909f1cb894da6b43d9bf upstream.
    
    There may be a bad USB audio device with a USB ID of (0x04fa, 0x4201) and
    the number of it's interfaces less than 4, an out-of-bounds read bug occurs
    when parsing the interface descriptor for this device.
    
    Fix this by checking the number of interfaces.
    
    Signed-off-by: Dongxiang Ke <kdx.glider@gmail.com>
    Link: https://lore.kernel.org/r/20220906024928.10951-1-kdx.glider@gmail.com
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- sound/usb/stream.c
+++ sound/usb/stream.c
@@ -1105,7 +1105,7 @@ static int __snd_usb_parse_audio_interface(struct snd_usb_audio *chip,
 	 * Dallas DS4201 workaround: It presents 5 altsettings, but the last
 	 * one misses syncpipe, and does not produce any sound.
 	 */
-	if (chip->usb_id == USB_ID(0x04fa, 0x4201))
+	if (chip->usb_id == USB_ID(0x04fa, 0x4201) && num >= 4)
 		num = 4;
 
 	for (i = 0; i < num; i++) {
