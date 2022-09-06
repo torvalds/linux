06a84bda0a0876dc0ba358e47ff51e5e658ed374
Johan Hovold <johan+linaro@kernel.org>
2022-09-06 14:07:02 +0200

    usb: dwc3: disable USB core PHY management
    commit 6000b8d900cd5f52fbcd0776d0cc396e88c8c2ea upstream.
    
    The dwc3 driver manages its PHYs itself so the USB core PHY management
    needs to be disabled.
    
    Use the struct xhci_plat_priv hack added by commits 46034a999c07 ("usb:
    host: xhci-plat: add platform data support") and f768e718911e ("usb:
    host: xhci-plat: add priv quirk for skip PHY initialization") to
    propagate the setting for now.
    
    Fixes: 4e88d4c08301 ("usb: add a flag to skip PHY initialization to struct usb_hcd")
    Fixes: 178a0bce05cb ("usb: core: hcd: integrate the PHY wrapper into the HCD core")
    Tested-by: Matthias Kaehlcke <mka@chromium.org>
    Cc: stable <stable@kernel.org>
    Reviewed-by: Matthias Kaehlcke <mka@chromium.org>
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>
    Link: https://lore.kernel.org/r/20220825131836.19769-1-johan+linaro@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [ johan: adjust context to 5.15 ]
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/dwc3/host.c
+++ drivers/usb/dwc3/host.c
@@ -10,7 +10,12 @@
 #include <linux/acpi.h>
 #include <linux/platform_device.h>
 
+#include "../host/xhci-plat.h"
 #include "core.h"
+
+static const struct xhci_plat_priv dwc3_xhci_plat_priv = {
+	.quirks = XHCI_SKIP_PHY_INIT,
+};
 
 static int dwc3_host_get_irq(struct dwc3 *dwc)
 {
@@ -86,6 +91,11 @@ int dwc3_host_init(struct dwc3 *dwc)
 		dev_err(dwc->dev, "couldn't add resources to xHCI device\n");
 		goto err;
 	}
+
+	ret = platform_device_add_data(xhci, &dwc3_xhci_plat_priv,
+					sizeof(dwc3_xhci_plat_priv));
+	if (ret)
+		goto err;
 
 	memset(props, 0, sizeof(struct property_entry) * ARRAY_SIZE(props));
 
