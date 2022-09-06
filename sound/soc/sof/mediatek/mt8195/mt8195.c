404bec4c8f6c38ae5fa208344f1086d38026e93d
AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
2022-09-06 11:27:24 +0200

    ASoC: SOF: mediatek: mt8195: Import namespace SND_SOC_SOF_MTK_COMMON
    Here we're using function mtk_adsp_dump() from mtk-adsp-common:
    explicitly import its namespace.
    
    Fixes: 3a054f90e955 ("ASoC: SOF: mediatek: Add mt8195 debug dump")
    Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Link: https://lore.kernel.org/r/20220906092727.37324-3-angelogioacchino.delregno@collabora.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sof/mediatek/mt8195/mt8195.c
+++ sound/soc/sof/mediatek/mt8195/mt8195.c
@@ -652,4 +652,5 @@ static struct platform_driver snd_sof_of_mt8195_driver = {
 module_platform_driver(snd_sof_of_mt8195_driver);
 
 MODULE_IMPORT_NS(SND_SOC_SOF_XTENSA);
+MODULE_IMPORT_NS(SND_SOC_SOF_MTK_COMMON);
 MODULE_LICENSE("Dual BSD/GPL");
