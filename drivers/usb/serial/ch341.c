2a4c619a87ddf558082a1dd6f2b0cad3bd4623f5
Johan Hovold <johan@kernel.org>
2022-09-06 14:21:26 +0200

    USB: serial: ch341: fix lost character on LCR updates
    commit 8e83622ae7ca481c76c8fd9579877f6abae64ca2 upstream.
    
    Disable LCR updates for pre-0x30 devices which use a different (unknown)
    protocol for line control and where the current register write causes
    the next received character to be lost.
    
    Note that updating LCR using the INIT command has no effect on these
    devices either.
    
    Reported-by: Jonathan Woithe <jwoithe@just42.net>
    Tested-by: Jonathan Woithe <jwoithe@just42.net>
    Link: https://lore.kernel.org/r/Ys1iPTfiZRWj2gXs@marvin.atrad.com.au
    Fixes: 4e46c410e050 ("USB: serial: ch341: reinitialize chip on reconfiguration")
    Fixes: 55fa15b5987d ("USB: serial: ch341: fix baud rate and line-control handling")
    Cc: stable@vger.kernel.org      # 4.10
    Signed-off-by: Johan Hovold <johan@kernel.org>
    [ johan: adjust context to 5.15 ]
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/serial/ch341.c
+++ drivers/usb/serial/ch341.c
@@ -97,7 +97,10 @@ struct ch341_private {
 	u8 mcr;
 	u8 msr;
 	u8 lcr;
+
 	unsigned long quirks;
+	u8 version;
+
 	unsigned long break_end;
 };
 
@@ -271,6 +274,9 @@ static int ch341_set_baudrate_lcr(struct usb_device *dev,
 	 * (stop bits, parity and word length). Version 0x30 and above use
 	 * CH341_REG_LCR only and CH341_REG_LCR2 is always set to zero.
 	 */
+	if (priv->version < 0x30)
+		return 0;
+
 	r = ch341_control_out(dev, CH341_REQ_WRITE_REG,
 			      CH341_REG_LCR2 << 8 | CH341_REG_LCR, lcr);
 	if (r)
@@ -323,7 +329,9 @@ static int ch341_configure(struct usb_device *dev, struct ch341_private *priv)
 	r = ch341_control_in(dev, CH341_REQ_READ_VERSION, 0, 0, buffer, size);
 	if (r < 0)
 		goto out;
-	dev_dbg(&dev->dev, "Chip version: 0x%02x\n", buffer[0]);
+
+	priv->version = buffer[0];
+	dev_dbg(&dev->dev, "Chip version: 0x%02x\n", priv->version);
 
 	r = ch341_control_out(dev, CH341_REQ_SERIAL_INIT, 0, 0);
 	if (r < 0)
