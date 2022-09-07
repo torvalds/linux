fa313ede703115c9ebb31ce36a946aae23c14d61
Tomas Winkler <tomas.winkler@intel.com>
2022-09-08 00:51:07 +0300

    mei: mkhi: add memory ready command
    Add GSC memory ready command.
    The command indicates to the firmware that extend operation
    memory was setup and the firmware may enter PXP mode.
    
    CC: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>
    Reviewed-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220907215113.1596567-11-tomas.winkler@intel.com
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>

--- drivers/misc/mei/mkhi.h
+++ drivers/misc/mei/mkhi.h
@@ -16,6 +16,13 @@
 #define MKHI_GEN_GROUP_ID 0xFF
 #define MKHI_GEN_GET_FW_VERSION_CMD 0x2
 
+#define MKHI_GROUP_ID_GFX              0x30
+#define MKHI_GFX_RESET_WARN_CMD_REQ    0x0
+#define MKHI_GFX_MEMORY_READY_CMD_REQ  0x1
+
+/* Allow transition to PXP mode without approval */
+#define MKHI_GFX_MEM_READY_PXP_ALLOWED  0x1
+
 struct mkhi_rule_id {
 	__le16 rule_type;
 	u8 feature_id;
@@ -38,6 +45,11 @@ struct mkhi_msg_hdr {
 struct mkhi_msg {
 	struct mkhi_msg_hdr hdr;
 	u8 data[];
+} __packed;
+
+struct mkhi_gfx_mem_ready {
+	struct mkhi_msg_hdr hdr;
+	u32    flags;
 } __packed;
 
 #endif /* _MEI_MKHI_H_ */
