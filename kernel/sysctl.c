b13bc7cbb931727b1b0a63594cd734bfd979e985
Liu Shixin <liushixin2@huawei.com>
2022-09-08 16:29:46 +0800

    kernel/sysctl.c: move sysctl_vals and sysctl_long_vals to sysctl.c
    sysctl_vals and sysctl_long_vals are declared even if sysctl is disabled.
    Move its definition to sysctl.c to make sure their integrity in any case.
    
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -82,9 +82,16 @@
 #include <linux/rtmutex.h>
 #endif
 
+/* shared constants to be used in various sysctls */
+const int sysctl_vals[] = { 0, 1, 2, 3, 4, 100, 200, 1000, 3000, INT_MAX, 65535, -1 };
+EXPORT_SYMBOL(sysctl_vals);
+
+const unsigned long sysctl_long_vals[] = { 0, 1, LONG_MAX };
+EXPORT_SYMBOL_GPL(sysctl_long_vals);
+
 #if defined(CONFIG_SYSCTL)
 
-/* Constants used for minimum and  maximum */
+/* Constants used for minimum and maximum */
 
 #ifdef CONFIG_PERF_EVENTS
 static const int six_hundred_forty_kb = 640 * 1024;
