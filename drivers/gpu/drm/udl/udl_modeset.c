0a129790893b765bef4ba02fcdeb73cbc56d5b99
Takashi Iwai <tiwai@suse.de>
2022-09-08 11:51:04 +0200

    drm/udl: Restore display mode on resume
    commit 6d6e732835db92e66c28dbcf258a7e3d3c71420d upstream.
    
    Restore the display mode whne resuming from suspend. Currently, the
    display remains dark.
    
    On resume, the CRTC's mode does not change, but the 'active' flag
    changes to 'true'. Taking this into account when considering a mode
    switch restores the display mode.
    
    The bug is reproducable by using Gnome with udl and observing the
    adapter's suspend/resume behavior.
    
    Actually, the whole check added in udl_simple_display_pipe_enable()
    about the crtc_state->mode_changed was bogus.  We should drop the
    whole check and always apply the mode change in this function.
    
    [ tiwai -- Drop the mode_changed check entirely instead, per Daniel's
      suggestion ]
    
    Fixes: 997d33c35618 ("drm/udl: Inline DPMS code into CRTC enable and disable functions")
    Cc: <stable@vger.kernel.org>
    Suggested-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220908095115.23396-2-tiwai@suse.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/udl/udl_modeset.c
+++ drivers/gpu/drm/udl/udl_modeset.c
@@ -400,9 +400,6 @@ udl_simple_display_pipe_enable(struct drm_simple_display_pipe *pipe,
 
 	udl_handle_damage(fb, 0, 0, fb->width, fb->height);
 
-	if (!crtc_state->mode_changed)
-		return;
-
 	/* enable display */
 	udl_crtc_write_mode_to_hw(crtc);
 }
