9124dbcc2dd6c51e81f97f63f7807118c4eb140a
Oleksandr Mazur <oleksandr.mazur@plvision.eu>
2022-09-08 16:14:46 +0300

    net: marvell: prestera: add support for for Aldrin2
    Aldrin2 (98DX8525) is a Marvell Prestera PP, with 100G support.
    
    Signed-off-by: Oleksandr Mazur <oleksandr.mazur@plvision.eu>
    
    V2:
      - retarget to net tree instead of net-next;
      - fix missed colon in patch subject ('net marvell' vs 'net: mavell');
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/prestera/prestera_pci.c
+++ drivers/net/ethernet/marvell/prestera/prestera_pci.c
@@ -872,6 +872,7 @@ static void prestera_pci_remove(struct pci_dev *pdev)
 static const struct pci_device_id prestera_pci_devices[] = {
 	{ PCI_DEVICE(PCI_VENDOR_ID_MARVELL, 0xC804) },
 	{ PCI_DEVICE(PCI_VENDOR_ID_MARVELL, 0xC80C) },
+	{ PCI_DEVICE(PCI_VENDOR_ID_MARVELL, 0xCC1E) },
 	{ }
 };
 MODULE_DEVICE_TABLE(pci, prestera_pci_devices);
