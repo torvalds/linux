3705a6ef4068cbc9c2344561345d09630fd278ec
Yang Jihong <yangjihong1@huawei.com>
2022-09-08 09:48:54 +0800

    perf lock: Remove redundant word 'contention' in help message
    Before:
      # perf lock -h
    
       Usage: perf lock [<options>] {record|report|script|info|contention|contention}
    
          -D, --dump-raw-trace  dump raw trace in ASCII
          -f, --force           don't complain, do it
          -i, --input <file>    input file name
          -v, --verbose         be more verbose (show symbol address, etc)
              --kallsyms <file>
                                kallsyms pathname
              --vmlinux <file>  vmlinux pathname
    
    After:
      # perf lock -h
    
       Usage: perf lock [<options>] {record|report|script|info|contention}
    
          -D, --dump-raw-trace  dump raw trace in ASCII
          -f, --force           don't complain, do it
          -i, --input <file>    input file name
          -v, --verbose         be more verbose (show symbol address, etc)
              --kallsyms <file>
                                kallsyms pathname
              --vmlinux <file>  vmlinux pathname
    
    Fixes: 528b9cab3b813a3b ("perf lock: Add 'contention' subcommand")
    Signed-off-by: Yang Jihong <yangjihong1@huawei.com>
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20220908014854.151203-1-yangjihong1@huawei.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/builtin-lock.c
+++ tools/perf/builtin-lock.c
@@ -1874,8 +1874,7 @@ int cmd_lock(int argc, const char **argv)
 		NULL
 	};
 	const char *const lock_subcommands[] = { "record", "report", "script",
-						 "info", "contention",
-						 "contention", NULL };
+						 "info", "contention", NULL };
 	const char *lock_usage[] = {
 		NULL,
 		NULL
