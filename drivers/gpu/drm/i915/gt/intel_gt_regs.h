14f2f9bf34b180aa90b2088836f5153cb56db95e
Matt Roper <matthew.d.roper@intel.com>
2022-09-09 17:16:31 -0700

    drm/i915/mtl: Add MTL forcewake support
    MTL has separate forcewake tables for the primary/render GT and the
    media GT; each GT's intel_uncore will use a separate forcewake table and
    should only initialize the domains that are relevant to that GT.  The GT
    ack register also moves to a new location of (GSI base + 0xDFC) on this
    platform.
    
    Note that although our uncore handlers take care of transparently
    redirecting all register accesses in the media GT's GSI range to their
    new offset at 0x380000, the forcewake ranges listed in the table should
    use the final, post-translation offsets.
    
    NOTE:  There are two ranges in the media IP that have multicast
    registers where the two register instances reside in different power
    wells (either VD0 or VD2).  We don't have an easy way to deal with this
    today (and in fact we don't even access these register ranges in the
    driver today), so for now we just mark those ranges as FORCEWAKE_ALL
    which will cause all of the media power wells to be grabbed, ensuring
    proper operation.  If we start reading/writing in those ranges in the
    future, we can re-visit whether it's worth adding extra steering
    complexity into our forcewake support.
    
    Bspec: 67788, 67789, 52077
    Cc: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    Reviewed-by: Harish Chegondi <harish.chegondi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220910001631.1986601-1-matthew.d.roper@intel.com

--- drivers/gpu/drm/i915/gt/intel_gt_regs.h
+++ drivers/gpu/drm/i915/gt/intel_gt_regs.h
@@ -39,6 +39,9 @@
 #define FORCEWAKE_ACK_RENDER_GEN9		_MMIO(0xd84)
 #define FORCEWAKE_ACK_MEDIA_GEN9		_MMIO(0xd88)
 
+#define FORCEWAKE_ACK_GSC			_MMIO(0xdf8)
+#define FORCEWAKE_ACK_GT_MTL			_MMIO(0xdfc)
+
 #define MCFG_MCR_SELECTOR			_MMIO(0xfd0)
 #define SF_MCR_SELECTOR				_MMIO(0xfd8)
 #define GEN8_MCR_SELECTOR			_MMIO(0xfdc)
@@ -897,6 +900,8 @@
 
 #define FORCEWAKE_MEDIA_VDBOX_GEN11(n)		_MMIO(0xa540 + (n) * 4)
 #define FORCEWAKE_MEDIA_VEBOX_GEN11(n)		_MMIO(0xa560 + (n) * 4)
+
+#define FORCEWAKE_REQ_GSC			_MMIO(0xa618)
 
 #define CHV_POWER_SS0_SIG1			_MMIO(0xa720)
 #define CHV_POWER_SS0_SIG2			_MMIO(0xa724)
