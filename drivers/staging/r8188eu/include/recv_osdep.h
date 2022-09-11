a1974d31545c98e1fcc39c932482c45d6a331a4e
Michael Straube <straube.linux@gmail.com>
2022-09-11 19:49:32 +0200

    staging: r8188eu: remove unused prototypes from recv_osdep.h
    The function prototypes rtw_recv_returnpacket(), rtw_init_recv_priv()
    and rtw_free_recv_priv() are not used. Remove them.
    
    Tested-by: Philipp Hortmann <philipp.g.hortmann@gmail.com> # Edimax N150
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Link: https://lore.kernel.org/r/20220911174933.3784-3-straube.linux@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/r8188eu/include/recv_osdep.h
+++ drivers/staging/r8188eu/include/recv_osdep.h
@@ -11,10 +11,6 @@ int _rtw_init_recv_priv(struct recv_priv *precvpriv, struct adapter *padapter);
 void _rtw_free_recv_priv(struct recv_priv *precvpriv);
 
 s32  rtw_recv_entry(struct recv_frame *precv_frame);
-void rtw_recv_returnpacket(struct  net_device *cnxt, struct sk_buff *retpkt);
-
-int rtw_init_recv_priv(struct recv_priv *precvpriv, struct adapter *padapter);
-void rtw_free_recv_priv(struct recv_priv *precvpriv);
 
 int netdev_open(struct net_device *pnetdev);
 int netdev_close(struct net_device *pnetdev);
