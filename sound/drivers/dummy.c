7ae22bdf49d513b0555d25df4d361379fc8ad166
YJ Lee <yunjunlee@chromium.org>
2022-09-12 15:28:54 +0800

    ALSA: dummy: Fix trailing whitespaces.
    Fix checkpatch.pl ERROR: trailing whitespaces.
    
    Signed-off-by: YJ Lee <yunjunlee@chromium.org>
    Link: https://lore.kernel.org/r/20220912072854.760824-1-yunjunlee@chromium.org
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/drivers/dummy.c
+++ sound/drivers/dummy.c
@@ -296,7 +296,7 @@ static void dummy_systimer_callback(struct timer_list *t)
 	struct dummy_systimer_pcm *dpcm = from_timer(dpcm, t, timer);
 	unsigned long flags;
 	int elapsed = 0;
-	
+
 	spin_lock_irqsave(&dpcm->lock, flags);
 	dummy_systimer_update(dpcm);
 	dummy_systimer_rearm(dpcm);
@@ -717,7 +717,7 @@ static int snd_dummy_volume_info(struct snd_kcontrol *kcontrol,
 	uinfo->value.integer.max = 100;
 	return 0;
 }
- 
+
 static int snd_dummy_volume_get(struct snd_kcontrol *kcontrol,
 				struct snd_ctl_elem_value *ucontrol)
 {
@@ -766,7 +766,7 @@ static const DECLARE_TLV_DB_SCALE(db_scale_dummy, -4500, 30, 0);
   .private_value = addr }
 
 #define snd_dummy_capsrc_info	snd_ctl_boolean_stereo_info
- 
+
 static int snd_dummy_capsrc_get(struct snd_kcontrol *kcontrol,
 				struct snd_ctl_elem_value *ucontrol)
 {
@@ -1100,7 +1100,7 @@ static int snd_dummy_suspend(struct device *pdev)
 	snd_power_change_state(card, SNDRV_CTL_POWER_D3hot);
 	return 0;
 }
-	
+
 static int snd_dummy_resume(struct device *pdev)
 {
 	struct snd_card *card = dev_get_drvdata(pdev);
