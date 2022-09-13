3adbdf81038801ee84367845e78bb1a3b36bde39
Miaohe Lin <linmiaohe@huawei.com>
2022-09-13 16:54:52 +0800

    KVM: x86/mmu: use helper macro SPTE_ENT_PER_PAGE
    Use helper macro SPTE_ENT_PER_PAGE to get the number of spte entries
    per page. Minor readability improvement.
    
    Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
    Reviewed-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20220913085452.25561-1-linmiaohe@huawei.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -1645,7 +1645,7 @@ static int is_empty_shadow_page(u64 *spt)
 	u64 *pos;
 	u64 *end;
 
-	for (pos = spt, end = pos + PAGE_SIZE / sizeof(u64); pos != end; pos++)
+	for (pos = spt, end = pos + SPTE_ENT_PER_PAGE; pos != end; pos++)
 		if (is_shadow_present_pte(*pos)) {
 			printk(KERN_ERR "%s: %p %llx\n", __func__,
 			       pos, *pos);
