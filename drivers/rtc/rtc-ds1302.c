5dc8356830428656c3a00dd702fb9102fe43550f
Yang Yingliang <yangyingliang@huawei.com>
2022-09-13 22:49:05 +0800

    rtc: ds1302: remove unnecessary spi_set_drvdata()
    Remove unnecessary spi_set_drvdata() in ds1302_remove(), the driver_data
    will be set to NULL in device_unbind_cleanup() after calling ->remove().
    After this, ds1302_remove() is an empty function, so remove it too.
    
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Link: https://lore.kernel.org/r/20220913144905.2004924-1-yangyingliang@huawei.com
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/rtc/rtc-ds1302.c
+++ drivers/rtc/rtc-ds1302.c
@@ -185,11 +185,6 @@ static int ds1302_probe(struct spi_device *spi)
 	return 0;
 }
 
-static void ds1302_remove(struct spi_device *spi)
-{
-	spi_set_drvdata(spi, NULL);
-}
-
 #ifdef CONFIG_OF
 static const struct of_device_id ds1302_dt_ids[] = {
 	{ .compatible = "maxim,ds1302", },
@@ -208,7 +203,6 @@ static struct spi_driver ds1302_driver = {
 	.driver.name	= "rtc-ds1302",
 	.driver.of_match_table = of_match_ptr(ds1302_dt_ids),
 	.probe		= ds1302_probe,
-	.remove		= ds1302_remove,
 	.id_table	= ds1302_spi_ids,
 };
 
