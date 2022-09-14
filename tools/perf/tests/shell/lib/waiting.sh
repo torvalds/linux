84838712e92eab3dfa37b97100f32490507373b2
Adrian Hunter <adrian.hunter@intel.com>
2022-09-14 11:01:50 +0300

    perf test: waiting.sh: Parameterize timeouts
    Let helper functions accept a parameter to specify time out values in
    tenths of a second.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Link: https://lore.kernel.org/r/20220914080150.5888-3-adrian.hunter@intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/tests/shell/lib/waiting.sh
+++ tools/perf/tests/shell/lib/waiting.sh
@@ -3,16 +3,18 @@
 tenths=date\ +%s%1N
 
 # Wait for PID $1 to have $2 number of threads started
+# Time out after $3 tenths of a second or 5 seconds if $3 is ""
 wait_for_threads()
 {
+	tm_out=$3 ; [ -n "${tm_out}" ] || tm_out=50
 	start_time=$($tenths)
 	while [ -e "/proc/$1/task" ] ; do
 		th_cnt=$(find "/proc/$1/task" -mindepth 1 -maxdepth 1 -printf x | wc -c)
 		if [ "${th_cnt}" -ge "$2" ] ; then
 			return 0
 		fi
-		# Wait at most 5 seconds
-		if [ $(($($tenths) - start_time)) -ge 50 ] ; then
+		# Wait at most tm_out tenths of a second
+		if [ $(($($tenths) - start_time)) -ge $tm_out ] ; then
 			echo "PID $1 does not have $2 threads"
 			return 1
 		fi
@@ -22,8 +24,10 @@ wait_for_threads()
 
 # Wait for perf record -vvv 2>$2 with PID $1 to start by looking at file $2
 # It depends on capturing perf record debug message "perf record has started"
+# Time out after $3 tenths of a second or 5 seconds if $3 is ""
 wait_for_perf_to_start()
 {
+	tm_out=$3 ; [ -n "${tm_out}" ] || tm_out=50
 	echo "Waiting for \"perf record has started\" message"
 	start_time=$($tenths)
 	while [ -e "/proc/$1" ] ; do
@@ -31,8 +35,8 @@ wait_for_perf_to_start()
 			echo OK
 			break
 		fi
-		# Wait at most 5 seconds
-		if [ $(($($tenths) - start_time)) -ge 50 ] ; then
+		# Wait at most tm_out tenths of a second
+		if [ $(($($tenths) - start_time)) -ge $tm_out ] ; then
 			echo "perf recording did not start"
 			return 1
 		fi
@@ -41,12 +45,14 @@ wait_for_perf_to_start()
 }
 
 # Wait for process PID %1 to exit
+# Time out after $2 tenths of a second or 5 seconds if $2 is ""
 wait_for_process_to_exit()
 {
+	tm_out=$2 ; [ -n "${tm_out}" ] || tm_out=50
 	start_time=$($tenths)
 	while [ -e "/proc/$1" ] ; do
-		# Wait at most 5 seconds
-		if [ $(($($tenths) - start_time)) -ge 50 ] ; then
+		# Wait at most tm_out tenths of a second
+		if [ $(($($tenths) - start_time)) -ge $tm_out ] ; then
 			echo "PID $1 did not exit as expected"
 			return 1
 		fi
@@ -54,13 +60,15 @@ wait_for_process_to_exit()
 	return 0
 }
 
-# Check if PID $1 is still running after 0.3 seconds
+# Check if PID $1 is still running after $2 tenths of a second
+# or 0.3 seconds if $2 is ""
 is_running()
 {
+	tm_out=$2 ; [ -n "${tm_out}" ] || tm_out=3
 	start_time=$($tenths)
 	while [ -e "/proc/$1" ] ; do
-		# Check for at least 0.3s
-		if [ $(($($tenths) - start_time)) -gt 3 ] ; then
+		# Check for at least tm_out tenths of a second
+		if [ $(($($tenths) - start_time)) -gt $tm_out ] ; then
 			return 0
 		fi
 	done
