bf0197aea19562be03a5c179afd51adbb2a5bf80
Peter Collingbourne <pcc@google.com>
2022-09-13 19:00:01 -0700

    kasan: call kasan_malloc() from __kmalloc_*track_caller()
    commit 5373b8a09d6e037ee0587cb5d9fe4cc09077deeb upstream.
    
    We were failing to call kasan_malloc() from __kmalloc_*track_caller()
    which was causing us to sometimes fail to produce KASAN error reports
    for allocations made using e.g. devm_kcalloc(), as the KASAN poison was
    not being initialized. Fix it.
    
    Signed-off-by: Peter Collingbourne <pcc@google.com>
    Cc: <stable@vger.kernel.org> # 5.15
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/slub.c
+++ mm/slub.c
@@ -4920,6 +4920,8 @@ void *__kmalloc_track_caller(size_t size, gfp_t gfpflags, unsigned long caller)
 	/* Honor the call site pointer we received. */
 	trace_kmalloc(caller, ret, size, s->size, gfpflags);
 
+	ret = kasan_kmalloc(s, ret, size, gfpflags);
+
 	return ret;
 }
 EXPORT_SYMBOL(__kmalloc_track_caller);
@@ -4950,6 +4952,8 @@ void *__kmalloc_node_track_caller(size_t size, gfp_t gfpflags,
 
 	/* Honor the call site pointer we received. */
 	trace_kmalloc_node(caller, ret, size, s->size, gfpflags, node);
+
+	ret = kasan_kmalloc(s, ret, size, gfpflags);
 
 	return ret;
 }
