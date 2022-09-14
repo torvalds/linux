1ec5c0e1289c70c8e7dd93a988d945ad5bc3f4b1
Aric Cyr <aric.cyr@amd.com>
2022-09-14 12:54:52 -0400

    drm/amd/display: Fix audio on display after unplugging another
    Revert "dc: skip audio setup when audio stream is enabled"
    
    This reverts commit 65fbfb02c2734cacffec5e3f492e1b4f1dabcf98
    
    [why]
    We have minimal pipe split transition method to avoid pipe
    allocation outage.However, this method will invoke audio setup
    which cause audio output stuck once pipe reallocate.
    
    [how]
    skip audio setup for pipelines which audio stream has been enabled
    
    Reviewed-by: Martin Leung <Martin.Leung@amd.com>
    Acked-by: Jasdeep Dhillon <jdhillon@amd.com>
    Signed-off-by: Aric Cyr <aric.cyr@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dce110/dce110_hw_sequencer.c
+++ drivers/gpu/drm/amd/display/dc/dce110/dce110_hw_sequencer.c
@@ -2164,8 +2164,7 @@ static void dce110_setup_audio_dto(
 			continue;
 		if (pipe_ctx->stream->signal != SIGNAL_TYPE_HDMI_TYPE_A)
 			continue;
-		if (pipe_ctx->stream_res.audio != NULL &&
-			pipe_ctx->stream_res.audio->enabled == false) {
+		if (pipe_ctx->stream_res.audio != NULL) {
 			struct audio_output audio_output;
 
 			build_audio_output(context, pipe_ctx, &audio_output);
@@ -2205,8 +2204,7 @@ static void dce110_setup_audio_dto(
 			if (!dc_is_dp_signal(pipe_ctx->stream->signal))
 				continue;
 
-			if (pipe_ctx->stream_res.audio != NULL &&
-				pipe_ctx->stream_res.audio->enabled == false) {
+			if (pipe_ctx->stream_res.audio != NULL) {
 				struct audio_output audio_output;
 
 				build_audio_output(context, pipe_ctx, &audio_output);
