e47b082579f307d0367b1fb7ca3698fd9c73a88b
Yang Yingliang <yangyingliang@huawei.com>
2022-09-15 10:10:24 +0800

    mm/damon/lru_sort: change damon_lru_sort_wmarks to static
    damon_lru_sort_wmarks is only used in lru_sort.c now, change it to static.
    
    Link: https://lkml.kernel.org/r/20220915021024.4177940-2-yangyingliang@huawei.com
    Fixes: 189aa3d58206 ("mm/damon/lru_sort: use watermarks parameters generator macro")
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Reviewed-by: SeongJae Park <sj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/damon/lru_sort.c
+++ mm/damon/lru_sort.c
@@ -77,7 +77,7 @@ static struct damos_quota damon_lru_sort_quota = {
 };
 DEFINE_DAMON_MODULES_DAMOS_TIME_QUOTA(damon_lru_sort_quota);
 
-struct damos_watermarks damon_lru_sort_wmarks = {
+static struct damos_watermarks damon_lru_sort_wmarks = {
 	.metric = DAMOS_WMARK_FREE_MEM_RATE,
 	.interval = 5000000,	/* 5 seconds */
 	.high = 200,		/* 20 percent */
