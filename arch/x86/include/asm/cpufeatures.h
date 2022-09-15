80e4c1cd42fff110bfdae8fce7ac4f22465f9664
Thomas Gleixner <tglx@linutronix.de>
2022-09-15 13:11:19 +0200

    x86/retbleed: Add X86_FEATURE_CALL_DEPTH
    Intel SKL CPUs fall back to other predictors when the RSB underflows. The
    only microcode mitigation is IBRS which is insanely expensive. It comes
    with performance drops of up to 30% depending on the workload.
    
    A way less expensive, but nevertheless horrible mitigation is to track the
    call depth in software and overeagerly fill the RSB when returns underflow
    the software counter.
    
    Provide a configuration symbol and a CPU misfeature bit.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20220915111147.056176424@infradead.org

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -304,6 +304,7 @@
 #define X86_FEATURE_UNRET		(11*32+15) /* "" AMD BTB untrain return */
 #define X86_FEATURE_USE_IBPB_FW		(11*32+16) /* "" Use IBPB during runtime firmware calls */
 #define X86_FEATURE_RSB_VMEXIT_LITE	(11*32+17) /* "" Fill RSB on VM exit when EIBRS is enabled */
+#define X86_FEATURE_CALL_DEPTH		(11*32+18) /* "" Call depth tracking for RSB stuffing */
 
 /* Intel-defined CPU features, CPUID level 0x00000007:1 (EAX), word 12 */
 #define X86_FEATURE_AVX_VNNI		(12*32+ 4) /* AVX VNNI instructions */
