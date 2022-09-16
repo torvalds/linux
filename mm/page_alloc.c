dae37a5dccd104fc465241c42d9e17756ddebbc1
Miaohe Lin <linmiaohe@huawei.com>
2022-09-16 15:22:54 +0800

    mm/page_alloc: init local variable buddy_pfn
    The local variable buddy_pfn could be passed to buddy_merge_likely()
    without initialization if the passed in order is MAX_ORDER - 1.  This
    looks buggy but buddy_pfn won't be used in this case as there's a order >=
    MAX_ORDER - 2 check.  Init buddy_pfn to 0 anyway to avoid possible future
    misuse.
    
    Link: https://lkml.kernel.org/r/20220916072257.9639-14-linmiaohe@huawei.com
    Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Reviewed-by: Oscar Salvador <osalvador@suse.de>
    Cc: Matthew Wilcox <willy@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1113,7 +1113,7 @@ static inline void __free_one_page(struct page *page,
 		int migratetype, fpi_t fpi_flags)
 {
 	struct capture_control *capc = task_capc(zone);
-	unsigned long buddy_pfn;
+	unsigned long buddy_pfn = 0;
 	unsigned long combined_pfn;
 	struct page *buddy;
 	bool to_tail;
