f0911245782621ed79ae6cd4288f09658e2517d3
Sarannya S <quic_sarannya@quicinc.com>
2022-09-19 15:23:02 +0530

    net: qrtr: Fix duplicate server announcement in qrtr
    Currently, hello_sent flag is incrementing after successful
    transmit of first hello packet. By this time, another thread
    of ns hello packet is passing the check condition in qrtr_node_enqueue
    function.This patch will increment the hello_sent flag just after entering
    qrtr_node_enqueue function.
    By the time NS comes up, transport driver is already probed and
    hello packet is sent to the remote. While NS binding to the socket,
    resets hello_sent flag and again sends the hello packet to the remote.
    This patch also will reset the hello_sent and hello_rcvd flags when NS
    releases the socket. This will avoid sending duplicate Hello packets
    to the remote.
    
    Change-Id: Ibfaf8fae3cb50e100af37c600daa8b2d38e3d604
    Signed-off-by: Sivaji Boddupilli <quic_boddupil@quicinc.com>
    Signed-off-by: Sarannya S <quic_sarannya@quicinc.com>

--- net/qrtr/af_qrtr.c
+++ net/qrtr/af_qrtr.c
@@ -743,14 +743,22 @@ static int qrtr_node_enqueue(struct qrtr_node *node, struct sk_buff *skb,
 	size_t len = skb->len;
 	int rc, confirm_rx;
 
+	mutex_lock(&node->ep_lock);
 	if (!atomic_read(&node->hello_sent) && type != QRTR_TYPE_HELLO) {
 		kfree_skb(skb);
+		mutex_unlock(&node->ep_lock);
 		return 0;
 	}
 	if (atomic_read(&node->hello_sent) && type == QRTR_TYPE_HELLO) {
 		kfree_skb(skb);
+		mutex_unlock(&node->ep_lock);
 		return 0;
 	}
+
+	if (!atomic_read(&node->hello_sent) && type == QRTR_TYPE_HELLO)
+		atomic_inc(&node->hello_sent);
+
+	mutex_unlock(&node->ep_lock);
 
 	/* If sk is null, this is a forwarded packet and should not wait */
 	if (!skb->sk) {
@@ -788,9 +796,10 @@ static int qrtr_node_enqueue(struct qrtr_node *node, struct sk_buff *skb,
 	else
 		rc = skb_put_padto(skb, ALIGN(len, 4) + sizeof(*hdr));
 
-	if (rc)
+	if (rc) {
 		pr_err("%s: failed to pad size %lu to %lu rc:%d\n", __func__,
 		       skb->len, ALIGN(skb->len, 4), rc);
+	}
 
 	if (!rc) {
 		mutex_lock(&node->ep_lock);
@@ -805,11 +814,9 @@ static int qrtr_node_enqueue(struct qrtr_node *node, struct sk_buff *skb,
 	 * confirm_rx flag if we dropped this one */
 	if (rc && confirm_rx)
 		qrtr_tx_flow_failed(node, to->sq_node, to->sq_port);
-	if (type == QRTR_TYPE_HELLO) {
-		if (!rc)
-			atomic_inc(&node->hello_sent);
-		else
-			kthread_queue_work(&node->kworker, &node->say_hello);
+	if (rc && type == QRTR_TYPE_HELLO) {
+		atomic_dec(&node->hello_sent);
+		kthread_queue_work(&node->kworker, &node->say_hello);
 	}
 
 	return rc;
@@ -1639,17 +1646,6 @@ static int __qrtr_bind(struct socket *sock,
 		qrtr_reset_ports();
 	spin_unlock_irqrestore(&qrtr_port_lock, flags);
 
-	if (port == QRTR_PORT_CTRL) {
-		struct qrtr_node *node;
-
-		down_write(&qrtr_epts_lock);
-		list_for_each_entry(node, &qrtr_all_epts, item) {
-			atomic_set(&node->hello_sent, 0);
-			atomic_set(&node->hello_rcvd, 0);
-		}
-		up_write(&qrtr_epts_lock);
-	}
-
 	/* unbind previous, if any */
 	if (!zapped)
 		qrtr_port_remove(ipc);
@@ -2125,6 +2121,18 @@ static int qrtr_release(struct socket *sock)
 	lock_sock(sk);
 
 	ipc = qrtr_sk(sk);
+
+	if (ipc->us.sq_port == QRTR_PORT_CTRL) {
+		struct qrtr_node *node;
+
+		down_write(&qrtr_epts_lock);
+		list_for_each_entry(node, &qrtr_all_epts, item) {
+			atomic_set(&node->hello_sent, 0);
+			atomic_set(&node->hello_rcvd, 0);
+		}
+		up_write(&qrtr_epts_lock);
+	}
+
 	sk->sk_shutdown = SHUTDOWN_MASK;
 	if (!sock_flag(sk, SOCK_DEAD))
 		sk->sk_state_change(sk);
