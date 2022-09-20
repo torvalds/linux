b9926482ab91cd6dc19e28ae4f0ae98c6c1217d7
Wang Liang <wangliangzz@inspur.com>
2022-09-20 14:02:10 +0800

    kvm_host.h: fix spelling typo in function declaration
    Make parameters in function declaration consistent with
    those in function definition for better cscope-ability
    
    Signed-off-by: Wang Liang <wangliangzz@inspur.com>
    Reviewed-by: Sean Christopherson <seanjc@google.com>
    Link: https://lore.kernel.org/r/20220920060210.4842-1-wangliangzz@126.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -1353,7 +1353,7 @@ void kvm_arch_vcpu_unblocking(struct kvm_vcpu *vcpu);
 bool kvm_vcpu_wake_up(struct kvm_vcpu *vcpu);
 void kvm_vcpu_kick(struct kvm_vcpu *vcpu);
 int kvm_vcpu_yield_to(struct kvm_vcpu *target);
-void kvm_vcpu_on_spin(struct kvm_vcpu *vcpu, bool usermode_vcpu_not_eligible);
+void kvm_vcpu_on_spin(struct kvm_vcpu *vcpu, bool yield_to_kernel_mode);
 
 void kvm_flush_remote_tlbs(struct kvm *kvm);
 
