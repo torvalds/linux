176239a9a2e3c519923d7bd8bb205838d48c7a3c
Jun Yu <junyuu@chromium.org>
2022-09-20 00:31:17 +0000

    wifi: ath11k: retrieve MAC address from system firmware if provided
    Devices may provide their own MAC address via system firmware (e.g.,
    device tree), especially in the case where the device doesn't have a
    useful EEPROM on which to store its MAC address (e.g., for integrated
    ahb WCN6750).
    
    Use the generic device helper to retrieve the MAC address, and (if
    present) honor it above the MAC address advertised by the card.
    
    Tested-on: WCN6750 hw1.0 AHB WLAN.MSL.1.0.1-00887-QCAMSLSWPLZ-1
    
    Signed-off-by: Jun Yu <junyuu@chromium.org>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://lore.kernel.org/r/20220920003117.841442-1-junyuu@chromium.org

--- drivers/net/wireless/ath/ath11k/mac.c
+++ drivers/net/wireless/ath/ath11k/mac.c
@@ -9072,6 +9072,7 @@ int ath11k_mac_register(struct ath11k_base *ab)
 	struct ath11k_pdev *pdev;
 	int i;
 	int ret;
+	u8 mac_addr[ETH_ALEN] = {0};
 
 	if (test_bit(ATH11K_FLAG_REGISTERED, &ab->dev_flags))
 		return 0;
@@ -9084,13 +9085,18 @@ int ath11k_mac_register(struct ath11k_base *ab)
 	if (ret)
 		return ret;
 
+	device_get_mac_address(ab->dev, mac_addr);
+
 	for (i = 0; i < ab->num_radios; i++) {
 		pdev = &ab->pdevs[i];
 		ar = pdev->ar;
 		if (ab->pdevs_macaddr_valid) {
 			ether_addr_copy(ar->mac_addr, pdev->mac_addr);
 		} else {
-			ether_addr_copy(ar->mac_addr, ab->mac_addr);
+			if (is_zero_ether_addr(mac_addr))
+				ether_addr_copy(ar->mac_addr, ab->mac_addr);
+			else
+				ether_addr_copy(ar->mac_addr, mac_addr);
 			ar->mac_addr[4] += i;
 		}
 
