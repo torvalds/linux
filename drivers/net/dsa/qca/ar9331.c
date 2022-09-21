14b29ece30e53f9b380f146d5f4f9062131b0d65
Yang Yingliang <yangyingliang@huawei.com>
2022-09-21 22:05:18 +0800

    net: dsa: ar9331: remove unnecessary dev_set_drvdata()
    Remove unnecessary dev_set_drvdata() in ->remove(), the driver_data will
    be set to NULL in device_unbind_cleanup() after calling ->remove().
    
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/dsa/qca/ar9331.c
+++ drivers/net/dsa/qca/ar9331.c
@@ -1099,8 +1099,6 @@ static void ar9331_sw_remove(struct mdio_device *mdiodev)
 	dsa_unregister_switch(&priv->ds);
 
 	reset_control_assert(priv->sw_reset);
-
-	dev_set_drvdata(&mdiodev->dev, NULL);
 }
 
 static void ar9331_sw_shutdown(struct mdio_device *mdiodev)
