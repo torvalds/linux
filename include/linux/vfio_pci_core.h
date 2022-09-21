63d7c77989de98d3e92611dbb858028b74dca377
Yi Liu <yi.l.liu@intel.com>
2022-09-21 18:43:48 +0800

    vfio/pci: Use the new device life cycle helpers
    Also introduce two pci core helpers as @init/@release for pci drivers:
    
     - vfio_pci_core_init_dev()
     - vfio_pci_core_release_dev()
    
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>
    Signed-off-by: Kevin Tian <kevin.tian@intel.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/20220921104401.38898-3-kevin.tian@intel.com
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

--- include/linux/vfio_pci_core.h
+++ include/linux/vfio_pci_core.h
@@ -109,6 +109,8 @@ void vfio_pci_core_close_device(struct vfio_device *core_vdev);
 void vfio_pci_core_init_device(struct vfio_pci_core_device *vdev,
 			       struct pci_dev *pdev,
 			       const struct vfio_device_ops *vfio_pci_ops);
+int vfio_pci_core_init_dev(struct vfio_device *core_vdev);
+void vfio_pci_core_release_dev(struct vfio_device *core_vdev);
 int vfio_pci_core_register_device(struct vfio_pci_core_device *vdev);
 void vfio_pci_core_uninit_device(struct vfio_pci_core_device *vdev);
 void vfio_pci_core_unregister_device(struct vfio_pci_core_device *vdev);
