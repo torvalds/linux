da6f79164e98de4ab3f2fdeea4875207fe282014
ye xingchen <ye.xingchen@zte.com.cn>
2022-09-21 12:48:03 +0900

    nilfs2: remove the unneeded result variable
    Return the value nilfs_segctor_sync() directly instead of storing it in
    another redundant variable.
    
    Link: https://lkml.kernel.org/r/20220831033403.302184-1-ye.xingchen@zte.com.cn
    Link: https://lkml.kernel.org/r/20220921034803.2476-3-konishi.ryusuke@gmail.com
    Reported-by: Zeal Robot <zealci@zte.com.cn>
    Signed-off-by: ye xingchen <ye.xingchen@zte.com.cn>
    Signed-off-by: Ryusuke Konishi <konishi.ryusuke@gmail.com>
    Cc: Minghao Chi <chi.minghao@zte.com.cn>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/nilfs2/segment.c
+++ fs/nilfs2/segment.c
@@ -2235,7 +2235,6 @@ int nilfs_construct_segment(struct super_block *sb)
 	struct the_nilfs *nilfs = sb->s_fs_info;
 	struct nilfs_sc_info *sci = nilfs->ns_writer;
 	struct nilfs_transaction_info *ti;
-	int err;
 
 	if (!sci)
 		return -EROFS;
@@ -2243,8 +2242,7 @@ int nilfs_construct_segment(struct super_block *sb)
 	/* A call inside transactions causes a deadlock. */
 	BUG_ON((ti = current->journal_info) && ti->ti_magic == NILFS_TI_MAGIC);
 
-	err = nilfs_segctor_sync(sci);
-	return err;
+	return nilfs_segctor_sync(sci);
 }
 
 /**
