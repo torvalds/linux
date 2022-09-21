b9dd04a20f81333e4b99662f1bbaf7c9e3a1e137
Mike Rapoport <rppt@linux.ibm.com>
2022-09-21 10:48:41 +0300

    arm64/mm: fold check for KFENCE into can_set_direct_map()
    KFENCE requires linear map to be mapped at page granularity, so that it
    is possible to protect/unprotect single pages, just like with
    rodata_full and DEBUG_PAGEALLOC.
    
    Instead of repating
    
    	can_set_direct_map() || IS_ENABLED(CONFIG_KFENCE)
    
    make can_set_direct_map() handle the KFENCE case.
    
    This also prevents potential false positives in kernel_page_present()
    that may return true for non-present page if CONFIG_KFENCE is enabled.
    
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Link: https://lore.kernel.org/r/20220921074841.382615-1-rppt@kernel.org
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -535,7 +535,7 @@ static void __init map_mem(pgd_t *pgdp)
 	 */
 	BUILD_BUG_ON(pgd_index(direct_map_end - 1) == pgd_index(direct_map_end));
 
-	if (can_set_direct_map() || IS_ENABLED(CONFIG_KFENCE))
+	if (can_set_direct_map())
 		flags |= NO_BLOCK_MAPPINGS | NO_CONT_MAPPINGS;
 
 	/*
@@ -1542,11 +1542,7 @@ int arch_add_memory(int nid, u64 start, u64 size,
 
 	VM_BUG_ON(!mhp_range_allowed(start, size, true));
 
-	/*
-	 * KFENCE requires linear map to be mapped at page granularity, so that
-	 * it is possible to protect/unprotect single pages in the KFENCE pool.
-	 */
-	if (can_set_direct_map() || IS_ENABLED(CONFIG_KFENCE))
+	if (can_set_direct_map())
 		flags |= NO_BLOCK_MAPPINGS | NO_CONT_MAPPINGS;
 
 	__create_pgd_mapping(swapper_pg_dir, start, __phys_to_virt(start),
