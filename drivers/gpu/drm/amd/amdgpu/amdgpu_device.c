d0fa84f174770679ea22076f4f754bfde2f3f0e1
Lijo Lazar <lijo.lazar@amd.com>
2022-09-21 17:37:37 +0530

    drm/amdgpu: Disable verbose for p2p dist calc
    Disable verbose while getting p2p distance. With verbose, it shows
    warning if ACS redirect is set between the devices. Adds noise
    to dmesg logs when a few GPU devices are on the same platform.
    
    Example log:
    
    amdgpu 0000:34:00.0: ACS redirect is set between the client and provider (0000:31:00.0)
    amdgpu 0000:34:00.0: to disable ACS redirect for this path, add the kernel parameter:
    	pci=disable_acs_redir=0000:30:00.0;0000:2e:00.0;0000:33:00.0;0000:2e:10.0
    
    Signed-off-by: Lijo Lazar <lijo.lazar@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -5578,7 +5578,7 @@ bool amdgpu_device_is_peer_accessible(struct amdgpu_device *adev,
 		adev->gmc.aper_base + adev->gmc.aper_size - 1;
 	bool p2p_access = !adev->gmc.xgmi.connected_to_cpu &&
 			  !(pci_p2pdma_distance_many(adev->pdev,
-					&peer_adev->dev, 1, true) < 0);
+					&peer_adev->dev, 1, false) < 0);
 
 	return pcie_p2p && p2p_access && (adev->gmc.visible_vram_size &&
 		adev->gmc.real_vram_size == adev->gmc.visible_vram_size &&
