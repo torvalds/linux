fb7d959bc0091171015e78c7649b8274ba92cbe4
Muhammad Husaini Zulkifli <muhammad.husaini.zulkifli@intel.com>
2022-09-21 10:49:40 +0800

    igc: Correct the launchtime offset
    [ Upstream commit 790835fcc0cb9992349ae3c9010dbc7321aaa24d ]
    
    The launchtime offset should be corrected according to sections 7.5.2.6
    Transmit Scheduling Latency of the Intel Ethernet I225/I226 Software
    User Manual.
    
    Software can compensate the latency between the transmission scheduling
    and the time that packet is transmitted to the network by setting this
    GTxOffset register. Without setting this register, there may be a
    significant delay between the packet scheduling and the network point.
    
    This patch helps to reduce the latency for each of the link speed.
    
    Before:
    
    10Mbps   : 11000 - 13800 nanosecond
    100Mbps  : 1300 - 1700 nanosecond
    1000Mbps : 190 - 600 nanosecond
    2500Mbps : 1400 - 1700 nanosecond
    
    After:
    
    10Mbps   : less than 750 nanosecond
    100Mbps  : less than 192 nanosecond
    1000Mbps : less than 128 nanosecond
    2500Mbps : less than 128 nanosecond
    
    Test Setup:
    
    Talker : Use l2_tai.c to generate the launchtime into packet payload.
    Listener: Use timedump.c to compute the delta between packet arrival and
    LaunchTime packet payload.
    
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    Signed-off-by: Muhammad Husaini Zulkifli <muhammad.husaini.zulkifli@intel.com>
    Acked-by: Sasha Neftin <sasha.neftin@intel.com>
    Acked-by: Paul Menzel <pmenzel@molgen.mpg.de>
    Tested-by: Naama Meir <naamax.meir@linux.intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    Stable-dep-of: e037a26ead18 ("igc: Fix packet still tx after gate close by reducing i226 MAC retry buffer")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/intel/igc/igc_tsn.h
+++ drivers/net/ethernet/intel/igc/igc_tsn.h
@@ -6,5 +6,6 @@
 
 int igc_tsn_offload_apply(struct igc_adapter *adapter);
 int igc_tsn_reset(struct igc_adapter *adapter);
+void igc_tsn_adjust_txtime_offset(struct igc_adapter *adapter);
 
 #endif /* _IGC_BASE_H */
