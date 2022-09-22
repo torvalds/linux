bf3f11581893494a5fb01eb87b99627edc2a85ff
Vipin Sharma <vipinsh@google.com>
2022-09-21 23:24:51 -0700

    KVM: selftests: Check result in hyperv_features for successful hypercalls
    Commit cc5851c6be86 ("KVM: selftests: Use exception fixup for #UD/#GP
    Hyper-V MSR/hcall tests") introduced a wrong guest assert in guest_hcall().
    It is not checking the successful hypercall results and only checks the
    result when a fault happens.
    
      GUEST_ASSERT_2(!hcall->ud_expected || res == hcall->expect,
                     hcall->expect, res);
    
    Correct the assertion by only checking results of the successful
    hypercalls.
    
    This issue was observed when this test started failing after building it
    in Clang. Above guest assert statement fails because "res" is not equal
    to "hcall->expect" when "hcall->ud_expected" is true. "res" gets some
    garbage value in Clang from the RAX register. In GCC, RAX is 0 because
    it using RAX for @output_address in the asm statement and resetting it
    to 0 before using it as output operand in the same asm statement. Clang
    is not using RAX for @output_address.
    
    Fixes: cc5851c6be86 ("KVM: selftests: Use exception fixup for #UD/#GP Hyper-V MSR/hcall tests")
    Signed-off-by: Vipin Sharma <vipinsh@google.com>
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Reviewed-by: Jim Mattson <jmattson@google.com>
    Reviewed-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Link: https://lore.kernel.org/r/20220922062451.2927010-1-vipinsh@google.com
    [sean: wrap changelog at ~75 chars, move -EFAULT change to separate patch]
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- tools/testing/selftests/kvm/x86_64/hyperv_features.c
+++ tools/testing/selftests/kvm/x86_64/hyperv_features.c
@@ -81,13 +81,13 @@ static void guest_hcall(vm_vaddr_t pgs_gpa, struct hcall_data *hcall)
 	}
 
 	vector = hypercall(hcall->control, input, output, &res);
-	if (hcall->ud_expected)
+	if (hcall->ud_expected) {
 		GUEST_ASSERT_2(vector == UD_VECTOR, hcall->control, vector);
-	else
+	} else {
 		GUEST_ASSERT_2(!vector, hcall->control, vector);
+		GUEST_ASSERT_2(res == hcall->expect, hcall->expect, res);
+	}
 
-	GUEST_ASSERT_2(!hcall->ud_expected || res == hcall->expect,
-			hcall->expect, res);
 	GUEST_DONE();
 }
 
