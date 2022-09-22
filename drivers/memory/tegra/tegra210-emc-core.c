d4a5db55c7b8e1904a5fa4470f908cffe552ac04
Liu Shixin <liushixin2@huawei.com>
2022-09-22 22:33:43 +0800

    memory: tegra210-emc: Use DEFINE_SHOW_ATTRIBUTE to simplify code
    Use DEFINE_SHOW_ATTRIBUTE helper macro to simplify the code. No
    functional change.
    
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/memory/tegra/tegra210-emc-core.c
+++ drivers/memory/tegra/tegra210-emc-core.c
@@ -1621,20 +1621,7 @@ static int tegra210_emc_debug_available_rates_show(struct seq_file *s,
 
 	return 0;
 }
-
-static int tegra210_emc_debug_available_rates_open(struct inode *inode,
-						   struct file *file)
-{
-	return single_open(file, tegra210_emc_debug_available_rates_show,
-			   inode->i_private);
-}
-
-static const struct file_operations tegra210_emc_debug_available_rates_fops = {
-	.open = tegra210_emc_debug_available_rates_open,
-	.read = seq_read,
-	.llseek = seq_lseek,
-	.release = single_release,
-};
+DEFINE_SHOW_ATTRIBUTE(tegra210_emc_debug_available_rates);
 
 static int tegra210_emc_debug_min_rate_get(void *data, u64 *rate)
 {
