04af28faae6760514f0302aa9db561e60ee53a20
Christian Brauner <brauner@kernel.org>
2022-09-22 17:17:23 +0200

    ecryptfs: use stub posix acl handlers
    Now that ecryptfs supports the get and set acl inode operations and the
    vfs has been switched to the new posi api, ecryptfs can simply rely on
    the stub posix acl handlers.
    
    Signed-off-by: Christian Brauner (Microsoft) <brauner@kernel.org>

--- fs/ecryptfs/inode.c
+++ fs/ecryptfs/inode.c
@@ -1210,6 +1210,10 @@ static const struct xattr_handler ecryptfs_xattr_handler = {
 };
 
 const struct xattr_handler *ecryptfs_xattr_handlers[] = {
+#ifdef CONFIG_FS_POSIX_ACL
+	&posix_acl_access_xattr_handler,
+	&posix_acl_default_xattr_handler,
+#endif
 	&ecryptfs_xattr_handler,
 	NULL
 };
