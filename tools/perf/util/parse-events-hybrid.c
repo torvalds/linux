71c86cda750b001100e0d6dc04a88449b7381a59
Zhengjun Xing <zhengjun.xing@linux.intel.com>
2022-09-23 11:00:13 +0800

    perf parse-events: Remove "not supported" hybrid cache events
    By default, we create two hybrid cache events, one is for cpu_core, and
    another is for cpu_atom. But Some hybrid hardware cache events are only
    available on one CPU PMU. For example, the 'L1-dcache-load-misses' is only
    available on cpu_core, while the 'L1-icache-loads' is only available on
    cpu_atom. We need to remove "not supported" hybrid cache events. By
    extending is_event_supported() to global API and using it to check if the
    hybrid cache events are supported before being created, we can remove the
    "not supported" hybrid cache events.
    
    Before:
    
     # ./perf stat -e L1-dcache-load-misses,L1-icache-loads -a sleep 1
    
     Performance counter stats for 'system wide':
    
                52,570      cpu_core/L1-dcache-load-misses/
       <not supported>      cpu_atom/L1-dcache-load-misses/
       <not supported>      cpu_core/L1-icache-loads/
             1,471,817      cpu_atom/L1-icache-loads/
    
           1.004915229 seconds time elapsed
    
    After:
    
     # ./perf stat -e L1-dcache-load-misses,L1-icache-loads -a sleep 1
    
     Performance counter stats for 'system wide':
    
                54,510      cpu_core/L1-dcache-load-misses/
             1,441,286      cpu_atom/L1-icache-loads/
    
           1.005114281 seconds time elapsed
    
    Fixes: 30def61f64bac5f5 ("perf parse-events: Create two hybrid cache events")
    Reported-by: Yi Ammy <ammy.yi@intel.com>
    Reviewed-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Xing Zhengjun <zhengjun.xing@linux.intel.com>
    Acked-by: Ian Rogers <irogers@google.com>
    Cc: Alexander Shishkin <alexander.shishkin@intel.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jin Yao <yao.jin@linux.intel.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20220923030013.3726410-2-zhengjun.xing@linux.intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/parse-events-hybrid.c
+++ tools/perf/util/parse-events-hybrid.c
@@ -33,7 +33,8 @@ static void config_hybrid_attr(struct perf_event_attr *attr,
 	 * If the PMU type ID is 0, the PERF_TYPE_RAW will be applied.
 	 */
 	attr->type = type;
-	attr->config = attr->config | ((__u64)pmu_type << PERF_PMU_TYPE_SHIFT);
+	attr->config = (attr->config & PERF_HW_EVENT_MASK) |
+			((__u64)pmu_type << PERF_PMU_TYPE_SHIFT);
 }
 
 static int create_event_hybrid(__u32 config_type, int *idx,
@@ -48,13 +49,25 @@ static int create_event_hybrid(__u32 config_type, int *idx,
 	__u64 config = attr->config;
 
 	config_hybrid_attr(attr, config_type, pmu->type);
+
+	/*
+	 * Some hybrid hardware cache events are only available on one CPU
+	 * PMU. For example, the 'L1-dcache-load-misses' is only available
+	 * on cpu_core, while the 'L1-icache-loads' is only available on
+	 * cpu_atom. We need to remove "not supported" hybrid cache events.
+	 */
+	if (attr->type == PERF_TYPE_HW_CACHE
+	    && !is_event_supported(attr->type, attr->config))
+		return 0;
+
 	evsel = parse_events__add_event_hybrid(list, idx, attr, name, metric_id,
 					       pmu, config_terms);
-	if (evsel)
+	if (evsel) {
 		evsel->pmu_name = strdup(pmu->name);
-	else
+		if (!evsel->pmu_name)
+			return -ENOMEM;
+	} else
 		return -ENOMEM;
-
 	attr->type = type;
 	attr->config = config;
 	return 0;
