cddb8d09ff1e477de8236a061a5017b21bab3c14
Liu Shixin <liushixin2@huawei.com>
2022-09-23 11:33:43 +0800

    mm/mmap: use hotplug_memory_notifier() directly
    Commit 76ae847497bc52 ("Documentation: raise minimum supported version of
    GCC to 5.1") updated the minimum gcc version to 5.1.  So the problem
    mentioned in f02c69680088 ("include/linux/memory.h: implement
    register_hotmemory_notifier()") no longer exist.  So we can now switch to
    use hotplug_memory_notifier() directly rather than
    register_hotmemory_notifier().
    
    Link: https://lkml.kernel.org/r/20220923033347.3935160-5-liushixin2@huawei.com
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Kefeng Wang <wangkefeng.wang@huawei.com>
    Cc: Waiman Long <longman@redhat.com>
    Cc: zefan li <lizefan.x@bytedance.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/mmap.c
+++ mm/mmap.c
@@ -3749,13 +3749,9 @@ static int reserve_mem_notifier(struct notifier_block *nb,
 	return NOTIFY_OK;
 }
 
-static struct notifier_block reserve_mem_nb = {
-	.notifier_call = reserve_mem_notifier,
-};
-
 static int __meminit init_reserve_notifier(void)
 {
-	if (register_hotmemory_notifier(&reserve_mem_nb))
+	if (hotplug_memory_notifier(reserve_mem_notifier, 0))
 		pr_err("Failed registering memory add/remove notifier for admin reserve\n");
 
 	return 0;
