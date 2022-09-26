c09eb2e578eb1668bbc84dc07e8d8bd6f04b9a02
Jiri Olsa <jolsa@kernel.org>
2022-09-26 17:33:38 +0200

    bpf: Adjust kprobe_multi entry_ip for CONFIG_X86_KERNEL_IBT
    Martynas reported bpf_get_func_ip returning +4 address when
    CONFIG_X86_KERNEL_IBT option is enabled.
    
    When CONFIG_X86_KERNEL_IBT is enabled we'll have endbr instruction
    at the function entry, which screws return value of bpf_get_func_ip()
    helper that should return the function address.
    
    There's short term workaround for kprobe_multi bpf program made by
    Alexei [1], but we need this fixup also for bpf_get_attach_cookie,
    that returns cookie based on the entry_ip value.
    
    Moving the fixup in the fprobe handler, so both bpf_get_func_ip
    and bpf_get_attach_cookie get expected function address when
    CONFIG_X86_KERNEL_IBT option is enabled.
    
    Also renaming kprobe_multi_link_handler entry_ip argument to fentry_ip
    so it's clearer this is an ftrace __fentry__ ip.
    
    [1] commit 7f0059b58f02 ("selftests/bpf: Fix kprobe_multi test.")
    
    Cc: Peter Zijlstra <peterz@infradead.org>
    Reported-by: Martynas Pumputis <m@lambda.lt>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Link: https://lore.kernel.org/r/20220926153340.1621984-5-jolsa@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -1028,6 +1028,22 @@ static const struct bpf_func_proto bpf_get_func_ip_proto_tracing = {
 	.arg1_type	= ARG_PTR_TO_CTX,
 };
 
+#ifdef CONFIG_X86_KERNEL_IBT
+static unsigned long get_entry_ip(unsigned long fentry_ip)
+{
+	u32 instr;
+
+	/* Being extra safe in here in case entry ip is on the page-edge. */
+	if (get_kernel_nofault(instr, (u32 *) fentry_ip - 1))
+		return fentry_ip;
+	if (is_endbr(instr))
+		fentry_ip -= ENDBR_INSN_SIZE;
+	return fentry_ip;
+}
+#else
+#define get_entry_ip(fentry_ip) fentry_ip
+#endif
+
 BPF_CALL_1(bpf_get_func_ip_kprobe, struct pt_regs *, regs)
 {
 	struct kprobe *kp = kprobe_running();
@@ -2600,13 +2616,13 @@ kprobe_multi_link_prog_run(struct bpf_kprobe_multi_link *link,
 }
 
 static void
-kprobe_multi_link_handler(struct fprobe *fp, unsigned long entry_ip,
+kprobe_multi_link_handler(struct fprobe *fp, unsigned long fentry_ip,
 			  struct pt_regs *regs)
 {
 	struct bpf_kprobe_multi_link *link;
 
 	link = container_of(fp, struct bpf_kprobe_multi_link, fp);
-	kprobe_multi_link_prog_run(link, entry_ip, regs);
+	kprobe_multi_link_prog_run(link, get_entry_ip(fentry_ip), regs);
 }
 
 static int symbols_cmp_r(const void *a, const void *b, const void *priv)
