c91bdc9358992856721ff77887202a7e80b7ab22
Johannes Weiner <hannes@cmpxchg.org>
2022-09-26 09:57:01 -0400

    mm: memcontrol: don't allocate cgroup swap arrays when memcg is disabled
    Patch series "memcg swap fix & cleanups".
    
    
    This patch (of 4):
    
    Since commit 2d1c498072de ("mm: memcontrol: make swap tracking an integral
    part of memory control"), the cgroup swap arrays are used to track memory
    ownership at the time of swap readahead and swapoff, even if swap space
    *accounting* has been turned off by the user via swapaccount=0 (which sets
    cgroup_memory_noswap).
    
    However, the patch was overzealous: by simply dropping the
    cgroup_memory_noswap conditionals in the swapon, swapoff and uncharge
    path, it caused the cgroup arrays being allocated even when the memory
    controller as a whole is disabled.  This is a waste of that memory.
    
    Restore mem_cgroup_disabled() checks, implied previously by
    cgroup_memory_noswap, in the swapon, swapoff, and swap_entry_free
    callbacks.
    
    Link: https://lkml.kernel.org/r/20220926135704.400818-1-hannes@cmpxchg.org
    Link: https://lkml.kernel.org/r/20220926135704.400818-2-hannes@cmpxchg.org
    Fixes: 2d1c498072de ("mm: memcontrol: make swap tracking an integral part of memory control")
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Reported-by: Hugh Dickins <hughd@google.com>
    Reviewed-by: Shakeel Butt <shakeelb@google.com>
    Acked-by: Hugh Dickins <hughd@google.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -7459,6 +7459,9 @@ void __mem_cgroup_uncharge_swap(swp_entry_t entry, unsigned int nr_pages)
 	struct mem_cgroup *memcg;
 	unsigned short id;
 
+	if (mem_cgroup_disabled())
+		return;
+
 	id = swap_cgroup_record(entry, 0, nr_pages);
 	rcu_read_lock();
 	memcg = mem_cgroup_from_id(id);
