5d8c3e836fc224dfe633e41f7f2856753b39a905
Hamza Mahfooz <hamza.mahfooz@amd.com>
2022-09-27 15:01:46 -0400

    drm/amd/display: fix array-bounds error in dc_stream_remove_writeback()
    Address the following error:
    drivers/gpu/drm/amd/amdgpu/../display/dc/core/dc_stream.c: In function ‘dc_stream_remove_writeback’:
    drivers/gpu/drm/amd/amdgpu/../display/dc/core/dc_stream.c:527:55: error: array subscript [0, 0] is outside array bounds of ‘struct dc_writeback_info[1]’ [-Werror=array-bounds]
      527 |                                 stream->writeback_info[j] = stream->writeback_info[i];
          |                                 ~~~~~~~~~~~~~~~~~~~~~~^~~
    In file included from ./drivers/gpu/drm/amd/amdgpu/../display/dc/dc.h:1269,
                     from ./drivers/gpu/drm/amd/amdgpu/../display/dc/inc/core_types.h:29,
                     from ./drivers/gpu/drm/amd/amdgpu/../display/dc/basics/dc_common.h:29,
                     from drivers/gpu/drm/amd/amdgpu/../display/dc/core/dc_stream.c:27:
    ./drivers/gpu/drm/amd/amdgpu/../display/dc/dc_stream.h:241:34: note: while referencing ‘writeback_info’
      241 |         struct dc_writeback_info writeback_info[MAX_DWB_PIPES];
          |
    
    Currently, we aren't checking to see if j remains within
    writeback_info[]'s bounds. So, add a check to make sure that we aren't
    overflowing the buffer.
    
    Reviewed-by: Aurabindo Pillai <aurabindo.pillai@amd.com>
    Signed-off-by: Hamza Mahfooz <hamza.mahfooz@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_stream.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_stream.c
@@ -520,7 +520,7 @@ bool dc_stream_remove_writeback(struct dc *dc,
 	}
 
 	/* remove writeback info for disabled writeback pipes from stream */
-	for (i = 0, j = 0; i < stream->num_wb_info; i++) {
+	for (i = 0, j = 0; i < stream->num_wb_info && j < MAX_DWB_PIPES; i++) {
 		if (stream->writeback_info[i].wb_enabled) {
 			if (i != j)
 				/* trim the array */
