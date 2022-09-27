f3ddb74ad0790030c9592229fb14d8c451f4e9a8
Steven Rostedt (Google) <rostedt@goodmis.org>
2022-09-27 19:15:27 -0400

    tracing: Wake up ring buffer waiters on closing of the file
    When the file that represents the ring buffer is closed, there may be
    waiters waiting on more input from the ring buffer. Call
    ring_buffer_wake_waiters() to wake up any waiters when the file is
    closed.
    
    Link: https://lkml.kernel.org/r/20220927231825.182416969@goodmis.org
    
    Cc: stable@vger.kernel.org
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Fixes: e30f53aad2202 ("tracing: Do not busy wait in buffer splice")
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- include/linux/trace_events.h
+++ include/linux/trace_events.h
@@ -92,6 +92,7 @@ struct trace_iterator {
 	unsigned int		temp_size;
 	char			*fmt;	/* modified format holder */
 	unsigned int		fmt_size;
+	long			wait_index;
 
 	/* trace_seq for __print_flags() and __print_symbolic() etc. */
 	struct trace_seq	tmp_seq;
