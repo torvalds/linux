cce5dc03338e25e910fb5a2c4f2ce8a79644370f
Yu Kuai <yukuai3@huawei.com>
2022-09-27 21:01:16 +0800

    scsi: hisi_sas: Revert "scsi: hisi_sas: Limit max hw sectors for v3 HW"
    This reverts commit 24cd0b9bfdff126c066032b0d40ab0962d35e777.
    
    1) commit 4e89dce72521 ("iommu/iova: Retry from last rb tree node if
    iova search fails") tries to fix that iova allocation can fail while
    there are still free space available. This is not backported to 5.10
    stable.
    2) commit fce54ed02757 ("scsi: hisi_sas: Limit max hw sectors for v3
    HW") fix the performance regression introduced by 1), however, this
    is just a temporary solution and will cause io performance regression
    because it limit max io size to PAGE_SIZE * 32(128k for 4k page_size).
    3) John Garry posted a patchset to fix the problem.
    4) The temporary solution is reverted.
    
    It's weird that the patch in 2) is backported to 5.10 stable alone,
    while the right thing to do is to backport them all together.
    
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>
    Reviewed-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
+++ drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
@@ -2738,7 +2738,6 @@ static int slave_configure_v3_hw(struct scsi_device *sdev)
 	struct hisi_hba *hisi_hba = shost_priv(shost);
 	struct device *dev = hisi_hba->dev;
 	int ret = sas_slave_configure(sdev);
-	unsigned int max_sectors;
 
 	if (ret)
 		return ret;
@@ -2755,12 +2754,6 @@ static int slave_configure_v3_hw(struct scsi_device *sdev)
 			pm_runtime_disable(dev);
 		}
 	}
-
-	/* Set according to IOMMU IOVA caching limit */
-	max_sectors = min_t(size_t, queue_max_hw_sectors(sdev->request_queue),
-			    (PAGE_SIZE * 32) >> SECTOR_SHIFT);
-
-	blk_queue_max_hw_sectors(sdev->request_queue, max_sectors);
 
 	return 0;
 }
