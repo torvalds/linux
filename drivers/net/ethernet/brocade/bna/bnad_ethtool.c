3b882a7bf6cded3af1589c4eae9b6153d6767c26
Colin Ian King <colin.i.king@gmail.com>
2022-09-28 22:54:39 +0100

    net: bna: Fix spelling mistake "muliple" -> "multiple"
    There is a spelling mistake in a literal string in the array
    bnad_net_stats_strings. Fix it.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/brocade/bna/bnad_ethtool.c
+++ drivers/net/ethernet/brocade/bna/bnad_ethtool.c
@@ -114,7 +114,7 @@ static const char *bnad_net_stats_strings[] = {
 	"mac_tx_deferral",
 	"mac_tx_excessive_deferral",
 	"mac_tx_single_collision",
-	"mac_tx_muliple_collision",
+	"mac_tx_multiple_collision",
 	"mac_tx_late_collision",
 	"mac_tx_excessive_collision",
 	"mac_tx_total_collision",
