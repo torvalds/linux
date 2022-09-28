a56df5d5b7ca6d79c3cdef32401380e60c0928b1
Christian Brauner <brauner@kernel.org>
2022-09-28 13:34:00 +0200

    evm: add post set acl hook
    The security_inode_post_setxattr() hook is used by security modules to
    update their own security.* xattrs. Consequently none of the security
    modules operate on posix acls. So we don't need an additional security
    hook when post setting posix acls.
    
    However, the integrity subsystem wants to be informed about posix acl
    changes in order to reset the EVM status flag.
    
    -> evm_inode_post_setxattr()
       -> evm_update_evmxattr()
          -> evm_calc_hmac()
             -> evm_calc_hmac_or_hash()
    
    and evm_cacl_hmac_or_hash() walks the global list of protected xattr
    names evm_config_xattrnames. This global list can be modified via
    /sys/security/integrity/evm/evm_xattrs. The write to "evm_xattrs" is
    restricted to security.* xattrs and the default xattrs in
    evm_config_xattrnames only contains security.* xattrs as well.
    
    So the actual value for posix acls is currently completely irrelevant
    for evm during evm_inode_post_setxattr() and frankly it should stay that
    way in the future to not cause the vfs any more headaches. But if the
    actual posix acl values matter then evm shouldn't operate on the binary
    void blob and try to hack around in the uapi struct anyway. Instead it
    should then in the future add a dedicated hook which takes a struct
    posix_acl argument passing the posix acls in the proper vfs format.
    
    For now it is sufficient to make evm_inode_post_set_acl() a wrapper
    around evm_inode_post_setxattr() not passing any actual values down.
    This will cause the hashes to be updated as before.
    
    Reviewed-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Christian Brauner (Microsoft) <brauner@kernel.org>

--- include/linux/evm.h
+++ include/linux/evm.h
@@ -44,6 +44,12 @@ static inline int evm_inode_remove_acl(struct user_namespace *mnt_userns,
 {
 	return evm_inode_set_acl(mnt_userns, dentry, acl_name, NULL);
 }
+static inline void evm_inode_post_set_acl(struct dentry *dentry,
+					  const char *acl_name,
+					  struct posix_acl *kacl)
+{
+	return evm_inode_post_setxattr(dentry, acl_name, NULL, 0);
+}
 extern int evm_inode_init_security(struct inode *inode,
 				   const struct xattr *xattr_array,
 				   struct xattr *evm);
@@ -129,6 +135,13 @@ static inline int evm_inode_remove_acl(struct user_namespace *mnt_userns,
 				       const char *acl_name)
 {
 	return 0;
+}
+
+static inline void evm_inode_post_set_acl(struct dentry *dentry,
+					  const char *acl_name,
+					  struct posix_acl *kacl)
+{
+	return;
 }
 
 static inline int evm_inode_init_security(struct inode *inode,
