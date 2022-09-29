7860d720a84c74b2761c6b7995392a798ab0a3cb
Rob Clark <robdclark@chromium.org>
2022-09-29 09:14:04 -0700

    drm/msm: Fix build break with recent mm tree
    9178e3dcb121 ("mm: discard __GFP_ATOMIC") removed __GFP_ATOMIC,
    replacing it with a check for not __GFP_DIRECT_RECLAIM.
    
    Reported-by: Randy Dunlap <rdunlap@infradead.org>
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Acked-by: Randy Dunlap <rdunlap@infradead.org> # build-tested
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220929161404.2769414-1-robdclark@gmail.com

--- drivers/gpu/drm/msm/msm_gem_shrinker.c
+++ drivers/gpu/drm/msm/msm_gem_shrinker.c
@@ -26,7 +26,7 @@ static bool can_swap(void)
 
 static bool can_block(struct shrink_control *sc)
 {
-	if (sc->gfp_mask & __GFP_ATOMIC)
+	if (!(sc->gfp_mask & __GFP_DIRECT_RECLAIM))
 		return false;
 	return current_is_kswapd() || (sc->gfp_mask & __GFP_RECLAIM);
 }
