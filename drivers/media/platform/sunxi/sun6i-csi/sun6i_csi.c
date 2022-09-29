841af6202c58d4778a676b38a83a37f463ea4750
Yang Li <yang.lee@linux.alibaba.com>
2022-09-29 13:40:03 +0800

    media: sun6i-csi: Remove unnecessary print function dev_err()
    The print function dev_err() is redundant because platform_get_irq()
    already prints an error.
    
    Link: https://bugzilla.openanolis.cn/show_bug.cgi?id=2314
    Reported-by: Abaci Robot <abaci@linux.alibaba.com>
    Signed-off-by: Yang Li <yang.lee@linux.alibaba.com>
    Acked-by: Jernej Skrabec <jernej.skrabec@gmail.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

--- drivers/media/platform/sunxi/sun6i-csi/sun6i_csi.c
+++ drivers/media/platform/sunxi/sun6i-csi/sun6i_csi.c
@@ -913,7 +913,6 @@ static int sun6i_csi_resources_setup(struct sun6i_csi_device *csi_dev,
 
 	irq = platform_get_irq(platform_dev, 0);
 	if (irq < 0) {
-		dev_err(dev, "failed to get interrupt\n");
 		ret = -ENXIO;
 		goto error_clock_rate_exclusive;
 	}
