b6c1761721193c52234e3ed048e4d16ab527bb74
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2022-09-29 17:50:13 -0700

    spmi: pmic-arb: do not ack and clear peripheral interrupts in cleanup_irq
    Currently, cleanup_irq() is invoked when a peripheral's interrupt
    fires and there is no mapping present in the interrupt domain of
    spmi interrupt controller.
    
    The cleanup_irq clears the arbiter bit, clears the pmic interrupt
    and disables it at the pmic in that order. The last disable in
    cleanup_irq races with request_irq() in that it stomps over the
    enable issued by request_irq. Fix this by not writing to the pmic
    in cleanup_irq. The latched bit will be left set in the pmic,
    which will not send us more interrupts even if the enable bit
    stays enabled.
    
    When a client wants to request an interrupt, use the activate
    callback on the irq_domain to clear latched bit. This ensures
    that the latched, if set due to the above changes in cleanup_irq
    or when the bootloader leaves it set, gets cleaned up, paving way
    for upcoming interrupts to trigger.
    
    With this, there is a possibility of unwanted triggering of
    interrupt right after the latched bit is cleared - the interrupt
    may be left enabled too. To avoid that, clear the enable first
    followed by clearing the latched bit in the activate callback.
    
    Fixes: 6bc546e71e50 ("spmi: pmic-arb: cleanup unrequested irqs")
    Fixes: 02abec3616c1 ("spmi: pmic-arb: rename pa_xx to pmic_arb_xx and other cleanup")
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>
    [collinsd@codeaurora.org: fix merge conflict]
    Signed-off-by: David Collins <collinsd@codeaurora.org>
    Signed-off-by: Fenglin Wu <quic_fenglinw@quicinc.com>
    Link: https://lore.kernel.org/r/1655004286-11493-4-git-send-email-quic_fenglinw@quicinc.com
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>
    Link: https://lore.kernel.org/r/20220930005019.2663064-5-sboyd@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/spmi/spmi-pmic-arb.c
+++ drivers/spmi/spmi-pmic-arb.c
@@ -593,16 +593,6 @@ static void cleanup_irq(struct spmi_pmic_arb *pmic_arb, u16 apid, int id)
 	dev_err_ratelimited(&pmic_arb->spmic->dev, "%s apid=%d sid=0x%x per=0x%x irq=%d\n",
 			__func__, apid, sid, per, id);
 	writel_relaxed(irq_mask, pmic_arb->ver_ops->irq_clear(pmic_arb, apid));
-
-	if (pmic_arb_write_cmd(pmic_arb->spmic, SPMI_CMD_EXT_WRITEL, sid,
-			(per << 8) + QPNPINT_REG_LATCHED_CLR, &irq_mask, 1))
-		dev_err_ratelimited(&pmic_arb->spmic->dev, "failed to ack irq_mask = 0x%x for ppid = %x\n",
-				irq_mask, ppid);
-
-	if (pmic_arb_write_cmd(pmic_arb->spmic, SPMI_CMD_EXT_WRITEL, sid,
-			       (per << 8) + QPNPINT_REG_EN_CLR, &irq_mask, 1))
-		dev_err_ratelimited(&pmic_arb->spmic->dev, "failed to ack irq_mask = 0x%x for ppid = %x\n",
-				irq_mask, ppid);
 }
 
 static int periph_interrupt(struct spmi_pmic_arb *pmic_arb, u16 apid)
@@ -780,6 +770,7 @@ static int qpnpint_irq_domain_activate(struct irq_domain *domain,
 	u16 apid = hwirq_to_apid(d->hwirq);
 	u16 sid = hwirq_to_sid(d->hwirq);
 	u16 irq = hwirq_to_irq(d->hwirq);
+	u8 buf;
 
 	if (pmic_arb->apid_data[apid].irq_ee != pmic_arb->ee) {
 		dev_err(&pmic_arb->spmic->dev, "failed to xlate sid = %#x, periph = %#x, irq = %u: ee=%u but owner=%u\n",
@@ -787,6 +778,10 @@ static int qpnpint_irq_domain_activate(struct irq_domain *domain,
 			pmic_arb->apid_data[apid].irq_ee);
 		return -ENODEV;
 	}
+
+	buf = BIT(irq);
+	qpnpint_spmi_write(d, QPNPINT_REG_EN_CLR, &buf, 1);
+	qpnpint_spmi_write(d, QPNPINT_REG_LATCHED_CLR, &buf, 1);
 
 	return 0;
 }
