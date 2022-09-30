ba99b756da178aa8c608c4499a91074466050c10
Jianlong Huang <jianlong.huang@starfivetech.com>
2022-09-30 14:14:04 +0800

    pinctrl: starfive: Rename "pinctrl-starfive" to "pinctrl-starfive-jh7100"
    Add the SoC name to make it more clear. Also the next generation StarFive
    SoCs will use "pinctrl-starfive" as the core of StarFive pinctrl driver.
    No functional change.
    
    Signed-off-by: Jianlong Huang <jianlong.huang@starfivetech.com>
    Signed-off-by: Hal Feng <hal.feng@linux.starfivetech.com>
    Reviewed-by: Rob Herring <robh@kernel.org>
    Link: https://lore.kernel.org/r/20220930061404.5418-1-hal.feng@linux.starfivetech.com
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/starfive/pinctrl-starfive-jh7100.c
+++ drivers/pinctrl/starfive/pinctrl-starfive-jh7100.c
@@ -20,7 +20,7 @@
 #include <linux/pinctrl/pinctrl.h>
 #include <linux/pinctrl/pinmux.h>
 
-#include <dt-bindings/pinctrl/pinctrl-starfive.h>
+#include <dt-bindings/pinctrl/pinctrl-starfive-jh7100.h>
 
 #include "../core.h"
 #include "../pinctrl-utils.h"
