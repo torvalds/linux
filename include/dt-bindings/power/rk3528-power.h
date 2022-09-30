d486f0e97db59f4225ace6acbff83359c88b66b0
Finley Xiao <finley.xiao@rock-chips.com>
2022-09-30 19:54:21 +0800

    dt/bindings: power: add RK3528 SoCs header for idle-request
    According to a description from TRM, add all the idle request.
    
    Signed-off-by: Finley Xiao <finley.xiao@rock-chips.com>
    Change-Id: I3c58c73a61bf88d91930f9f3464207f820965b94

---
+++ include/dt-bindings/power/rk3528-power.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+#ifndef __DT_BINDINGS_POWER_RK3528_POWER_H__
+#define __DT_BINDINGS_POWER_RK3528_POWER_H__
+
+/*
+ * RK3528 idle id Summary.
+ */
+#define RK3528_PD_PMU		0
+#define RK3528_PD_BUS		1
+#define RK3528_PD_DDR		2
+#define RK3528_PD_MSCH		3
+#define RK3528_PD_GPU		4
+#define RK3528_PD_RKVDEC	5
+#define RK3528_PD_RKVENC	6
+#define RK3528_PD_VO		7
+#define RK3528_PD_VPU		8
+
+#endif
