d687772e6d2cbffd91fdda64812f79192c1e7ca0
William Zijl <postmaster@gusted.xyz>
2022-10-01 17:36:21 +0200

    random: fix typos in get_random_bytes() comment
    Remove extra whitespace and add a missing word to a sentence describing
    get_random_bytes().
    
    Signed-off-by: William Zijl <postmaster@gusted.xyz>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -382,11 +382,11 @@ static void _get_random_bytes(void *buf, size_t len)
 }
 
 /*
- * This function is the exported kernel interface.  It returns some
- * number of good random numbers, suitable for key generation, seeding
- * TCP sequence numbers, etc. In order to ensure that the randomness
- * by this function is okay, the function wait_for_random_bytes()
- * should be called and return 0 at least once at any point prior.
+ * This function is the exported kernel interface. It returns some number of
+ * good random numbers, suitable for key generation, seeding TCP sequence
+ * numbers, etc. In order to ensure that the randomness returned by this
+ * function is okay, the function wait_for_random_bytes() should be called and
+ * return 0 at least once at any point prior.
  */
 void get_random_bytes(void *buf, size_t len)
 {
