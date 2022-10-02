472a1482325b3a285e0bcf82c0b0edc689b7e8cd
William Breathitt Gray <william.gray@linaro.org>
2022-10-02 08:04:19 -0400

    counter: Reduce DEFINE_COUNTER_ARRAY_POLARITY() to defining counter_array
    A spare warning was reported for drivers/counter/ti-ecap-capture.c::
    
        sparse warnings: (new ones prefixed by >>)
        >> drivers/counter/ti-ecap-capture.c:380:8: sparse: sparse: symbol 'ecap_cnt_pol_array' was not declared. Should it be static?
    
        vim +/ecap_cnt_pol_array +380 drivers/counter/ti-ecap-capture.c
    
           379
         > 380	static DEFINE_COUNTER_ARRAY_POLARITY(ecap_cnt_pol_array, ecap_cnt_pol_avail, ECAP_NB_CEVT);
           381
    
    The first argument to the DEFINE_COUNTER_ARRAY_POLARITY() macro is a
    token serving as the symbol name in the definition of a new
    struct counter_array structure. However, this macro actually expands to
    two statements::
    
        #define DEFINE_COUNTER_ARRAY_POLARITY(_name, _enums, _length) \
                DEFINE_COUNTER_AVAILABLE(_name##_available, _enums); \
                struct counter_array _name = { \
                        .type = COUNTER_COMP_SIGNAL_POLARITY, \
                        .avail = &(_name##_available), \
                        .length = (_length), \
                }
    
    Because of this, the "static" on line 380 only applies to the first
    statement. This patch splits out the DEFINE_COUNTER_AVAILABLE() line
    and leaves DEFINE_COUNTER_ARRAY_POLARITY() as a simple structure
    definition to avoid issues like this.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Link: https://lore.kernel.org/all/202210020619.NQbyomII-lkp@intel.com/
    Cc: Julien Panis <jpanis@baylibre.com>
    Signed-off-by: William Breathitt Gray <william.gray@linaro.org>

--- drivers/counter/ti-ecap-capture.c
+++ drivers/counter/ti-ecap-capture.c
@@ -377,7 +377,8 @@ static const enum counter_signal_polarity ecap_cnt_pol_avail[] = {
 	COUNTER_SIGNAL_POLARITY_NEGATIVE,
 };
 
-static DEFINE_COUNTER_ARRAY_POLARITY(ecap_cnt_pol_array, ecap_cnt_pol_avail, ECAP_NB_CEVT);
+static DEFINE_COUNTER_AVAILABLE(ecap_cnt_pol_available, ecap_cnt_pol_avail);
+static DEFINE_COUNTER_ARRAY_POLARITY(ecap_cnt_pol_array, ecap_cnt_pol_available, ECAP_NB_CEVT);
 
 static struct counter_comp ecap_cnt_signal_ext[] = {
 	COUNTER_COMP_ARRAY_POLARITY(ecap_cnt_pol_read, ecap_cnt_pol_write, ecap_cnt_pol_array),
