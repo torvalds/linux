6cad1ecd4e3213d892b70afa999a81849d1f0206
Luis Chamberlain <mcgrof@kernel.org>
2022-10-03 09:58:49 -0700

    testing: use the copyleft-next-0.3.1 SPDX tag
    Two selftests drivers exist under the copyleft-next license.
    These drivers were added prior to SPDX practice taking full swing
    in the kernel. Now that we have an SPDX tag for copyleft-next-0.3.1
    documented, embrace it and remove the boiler plate.
    
    Cc: Goldwyn Rodrigues <rgoldwyn@suse.com>
    Cc: Kuno Woudt <kuno@frob.nl>
    Cc: Richard Fontana <fontana@sharpeleven.org>
    Cc: copyleft-next@lists.fedorahosted.org
    Cc: Ciaran Farrell <Ciaran.Farrell@suse.com>
    Cc: Christopher De Nicolo <Christopher.DeNicolo@suse.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Thorsten Leemhuis <linux@leemhuis.info>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>
    Reviewed-by: Tim Bird <tim.bird@sony.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- tools/testing/selftests/kmod/kmod.sh
+++ tools/testing/selftests/kmod/kmod.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-#
+# SPDX-License-Identifier: GPL-2.0-or-later OR copyleft-next-0.3.1
 # Copyright (C) 2017 Luis R. Rodriguez <mcgrof@kernel.org>
 #
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or at your option any
-# later version; or, when distributed separately from the Linux kernel or
-# when incorporated into other software packages, subject to the following
-# license:
-#
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of copyleft-next (version 0.3.1 or later) as published
-# at http://copyleft-next.org/.
-
 # This is a stress test script for kmod, the kernel module loader. It uses
 # test_kmod which exposes a series of knobs for the API for us so we can
 # tweak each test in userspace rather than in kernelspace.
--- tools/testing/selftests/sysctl/sysctl.sh
+++ tools/testing/selftests/sysctl/sysctl.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
+# SPDX-License-Identifier: GPL-2.0-or-later OR copyleft-next-0.3.1
 # Copyright (C) 2017 Luis R. Rodriguez <mcgrof@kernel.org>
-#
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or at your option any
-# later version; or, when distributed separately from the Linux kernel or
-# when incorporated into other software packages, subject to the following
-# license:
-#
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of copyleft-next (version 0.3.1 or later) as published
-# at http://copyleft-next.org/.
 
 # This performs a series tests against the proc sysctl interface.
 
