c24538f538ef2f70c10f4326c1c0efd6ec6561c9
Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>
2022-10-03 20:02:42 +0300

    drm/i915/gt: Remove unused function prototype
    Remove unused function prototype; intel_gt_create_kobj()
    
    Cc: Andi Shyti <andi.shyti@linux.intel.com>
    Signed-off-by: Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>
    Reviewed-by: Andi Shyti <andi.shyti@linux.intel.com>
    Signed-off-by: Andi Shyti <andi.shyti@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20221003170242.1246830-1-gwan-gyeong.mun@intel.com

--- drivers/gpu/drm/i915/gt/intel_gt_sysfs.h
+++ drivers/gpu/drm/i915/gt/intel_gt_sysfs.h
@@ -18,11 +18,6 @@ bool is_object_gt(struct kobject *kobj);
 
 struct drm_i915_private *kobj_to_i915(struct kobject *kobj);
 
-struct kobject *
-intel_gt_create_kobj(struct intel_gt *gt,
-		     struct kobject *dir,
-		     const char *name);
-
 static inline struct intel_gt *kobj_to_gt(struct kobject *kobj)
 {
 	return container_of(kobj, struct intel_gt, sysfs_gt);
