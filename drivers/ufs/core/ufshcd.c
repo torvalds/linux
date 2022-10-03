eef88095dcfbadcfa0fbfeeed505c3b3cfe2a40f
Konstantin Vyshetsky <vkon@google.com>
2022-10-03 15:07:14 -0700

    ANDROID: scsi: ufs: add complete init vendor hook
    Currently the core UFS driver does not have a vops to notify when the
    device is operational. This commit introduces a hook, which serves to
    notify device completing initialization and is ready to accept I/O.
    
    This is required by the FIPS140-2 [1] self integrity test of inline
    encryption engine, which must run whenever the host controller is reset.
    The code requires sleeping while waiting for I/O to complete and allocating
    some memory dynamically, which requires the vendor hook to be restricted.
    
    [1] https://csrc.nist.gov/publications/detail/fips/140/2/final
    
    Bug: 185809932
    Signed-off-by: Konstantin Vyshetsky <vkon@google.com>
    (cherry picked from commit e774e4eca69ce8ab60df04b27f524b586ab74f17)
    (cherry picked from https://android-review.googlesource.com/q/commit:c0f24579002c3fb0e404f223f8574c7f4fdac200)
    Merged-In: I6f476f9c2e2b50574d2898c3f1ef6b648d92df24
    Change-Id: I6f476f9c2e2b50574d2898c3f1ef6b648d92df24

--- drivers/ufs/core/ufshcd.c
+++ drivers/ufs/core/ufshcd.c
@@ -8882,6 +8882,8 @@ static int ufshcd_probe_hba(struct ufs_hba *hba, bool init_dev_params)
 	ufshcd_auto_hibern8_enable(hba);
 
 	ufshpb_toggle_state(hba, HPB_RESET, HPB_PRESENT);
+
+	trace_android_rvh_ufs_complete_init(hba);
 out:
 	spin_lock_irqsave(hba->host->host_lock, flags);
 	if (ret)
