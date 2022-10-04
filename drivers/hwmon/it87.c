12c44ab8b401c29d8d3569aaea34da662b8ece1d
Ahmad Khalifa <ahmad@khalifa.ws>
2022-10-04 22:01:01 +0100

    hwmon: (it87) Add param to ignore ACPI resource conflicts
    Add parameter to ignore ACPI resource conflicts as an alternate to using
    'acpi_enforce_resources=lax'.
    
    Some BIOSes reserve resources and don't use them and the system wide
    parameter may result in failures to certain drivers.
    
    Signed-off-by: Ahmad Khalifa <ahmad@khalifa.ws>
    Link: https://lore.kernel.org/r/20221004210100.540120-2-ahmad@khalifa.ws
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/it87.c
+++ drivers/hwmon/it87.c
@@ -69,6 +69,10 @@ static unsigned short force_id;
 module_param(force_id, ushort, 0);
 MODULE_PARM_DESC(force_id, "Override the detected device ID");
 
+static bool ignore_resource_conflict;
+module_param(ignore_resource_conflict, bool, 0);
+MODULE_PARM_DESC(ignore_resource_conflict, "Ignore ACPI resource conflict");
+
 static struct platform_device *it87_pdev[2];
 
 #define	REG_2E	0x2e	/* The register to read/write */
@@ -3261,8 +3265,10 @@ static int __init it87_device_add(int index, unsigned short address,
 	int err;
 
 	err = acpi_check_resource_conflict(&res);
-	if (err)
-		return err;
+	if (err) {
+		if (!ignore_resource_conflict)
+			return err;
+	}
 
 	pdev = platform_device_alloc(DRVNAME, address);
 	if (!pdev)
