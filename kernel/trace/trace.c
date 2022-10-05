7aeda81191fdd59e3e0375670ccc2c6856ba8b92
Steven Rostedt (Google) <rostedt@goodmis.org>
2022-10-05 11:37:57 -0400

    tracing: Do not free snapshot if tracer is on cmdline
    [ Upstream commit a541a9559bb0a8ecc434de01d3e4826c32e8bb53 ]
    
    The ftrace_boot_snapshot and alloc_snapshot cmdline options allocate the
    snapshot buffer at boot up for use later. The ftrace_boot_snapshot in
    particular requires the snapshot to be allocated because it will take a
    snapshot at the end of boot up allowing to see the traces that happened
    during boot so that it's not lost when user space takes over.
    
    When a tracer is registered (started) there's a path that checks if it
    requires the snapshot buffer or not, and if it does not and it was
    allocated it will do a synchronization and free the snapshot buffer.
    
    This is only required if the previous tracer was using it for "max
    latency" snapshots, as it needs to make sure all max snapshots are
    complete before freeing. But this is only needed if the previous tracer
    was using the snapshot buffer for latency (like irqoff tracer and
    friends). But it does not make sense to free it, if the previous tracer
    was not using it, and the snapshot was allocated by the cmdline
    parameters. This basically takes away the point of allocating it in the
    first place!
    
    Note, the allocated snapshot worked fine for just trace events, but fails
    when a tracer is enabled on the cmdline.
    
    Further investigation, this goes back even further and it does not require
    a tracer on the cmdline to fail. Simply enable snapshots and then enable a
    tracer, and it will remove the snapshot.
    
    Link: https://lkml.kernel.org/r/20221005113757.041df7fe@gandalf.local.home
    
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: stable@vger.kernel.org
    Fixes: 45ad21ca5530 ("tracing: Have trace_array keep track if snapshot buffer is allocated")
    Reported-by: Ross Zwisler <zwisler@kernel.org>
    Tested-by: Ross Zwisler <zwisler@kernel.org>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -6399,11 +6399,11 @@ int tracing_set_tracer(struct trace_array *tr, const char *buf)
 	if (tr->current_trace->reset)
 		tr->current_trace->reset(tr);
 
+#ifdef CONFIG_TRACER_MAX_TRACE
+	had_max_tr = tr->current_trace->use_max_tr;
+
 	/* Current trace needs to be nop_trace before synchronize_rcu */
 	tr->current_trace = &nop_trace;
-
-#ifdef CONFIG_TRACER_MAX_TRACE
-	had_max_tr = tr->allocated_snapshot;
 
 	if (had_max_tr && !t->use_max_tr) {
 		/*
@@ -6417,11 +6417,13 @@ int tracing_set_tracer(struct trace_array *tr, const char *buf)
 		free_snapshot(tr);
 	}
 
-	if (t->use_max_tr && !had_max_tr) {
+	if (t->use_max_tr && !tr->allocated_snapshot) {
 		ret = tracing_alloc_snapshot_instance(tr);
 		if (ret < 0)
 			goto out;
 	}
+#else
+	tr->current_trace = &nop_trace;
 #endif
 
 	if (t->init) {
