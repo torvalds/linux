df386f15b2fbe51863d0f076d5939d5cbdb36f0d
Nicholas Piggin <npiggin@gmail.com>
2022-10-06 20:56:35 +1000

    s390: remove the last remnants of cputime_t
    cputime_t was a core kernel type, removed by commits
    ed5c8c854f2b..b672592f0221. As explained in commit b672592f0221
    ("sched/cputime: Remove generic asm headers"), the final cleanup is for
    the arch to provide cputime_to_nsec[s](). Commit e53051e757d6
    ("s390/cputime: provide archicture specific cputime_to_nsecs") did that,
    but just didn't remove the then-unused cputime_to_usecs() and associated
    remnants.
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Reviewed-by: Sven Schnelle <svens@linux.ibm.com>
    Signed-off-by: Alexander Gordeev <agordeev@linux.ibm.com>
    Link: https://lore.kernel.org/r/20221006105635.115775-1-npiggin@gmail.com
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- arch/s390/include/asm/cputime.h
+++ arch/s390/include/asm/cputime.h
@@ -11,21 +11,6 @@
 #include <linux/types.h>
 #include <asm/timex.h>
 
-#define CPUTIME_PER_USEC 4096ULL
-#define CPUTIME_PER_SEC (CPUTIME_PER_USEC * USEC_PER_SEC)
-
-/* We want to use full resolution of the CPU timer: 2**-12 micro-seconds. */
-
-#define cmpxchg_cputime(ptr, old, new) cmpxchg64(ptr, old, new)
-
-/*
- * Convert cputime to microseconds.
- */
-static inline u64 cputime_to_usecs(const u64 cputime)
-{
-	return cputime >> 12;
-}
-
 /*
  * Convert cputime to nanoseconds.
  */
