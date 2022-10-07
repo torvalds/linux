cc994a0a76a87108ec3fac10bb906ff9d934c3c9
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2022-10-07 16:44:44 +0300

    pinctrl: merrifield: Add missing header(s)
    Do not imply that some of the generic headers may be always included.
    Instead, include explicitly what we are direct user of.
    
    While at it, sort headers alphabetically.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Mika Westerberg <mika.westerberg@linux.intel.com>

--- drivers/pinctrl/intel/pinctrl-merrifield.c
+++ drivers/pinctrl/intel/pinctrl-merrifield.c
@@ -12,8 +12,10 @@
 #include <linux/module.h>
 #include <linux/mod_devicetable.h>
 #include <linux/platform_device.h>
-#include <linux/pinctrl/pinconf.h>
+#include <linux/seq_file.h>
+
 #include <linux/pinctrl/pinconf-generic.h>
+#include <linux/pinctrl/pinconf.h>
 #include <linux/pinctrl/pinctrl.h>
 #include <linux/pinctrl/pinmux.h>
 
