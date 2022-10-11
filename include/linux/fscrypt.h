ccd30a476f8e864732de220bd50e6f372f5ebcab
Eric Biggers <ebiggers@google.com>
2022-10-11 14:38:38 -0700

    fscrypt: fix keyring memory leak on mount failure
    Commit d7e7b9af104c ("fscrypt: stop using keyrings subsystem for
    fscrypt_master_key") moved the keyring destruction from __put_super() to
    generic_shutdown_super() so that the filesystem's block device(s) are
    still available.  Unfortunately, this causes a memory leak in the case
    where a mount is attempted with the test_dummy_encryption mount option,
    but the mount fails after the option has already been processed.
    
    To fix this, attempt the keyring destruction in both places.
    
    Reported-by: syzbot+104c2a89561289cec13e@syzkaller.appspotmail.com
    Fixes: d7e7b9af104c ("fscrypt: stop using keyrings subsystem for fscrypt_master_key")
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Reviewed-by: Christian Brauner (Microsoft) <brauner@kernel.org>
    Link: https://lore.kernel.org/r/20221011213838.209879-1-ebiggers@kernel.org

--- include/linux/fscrypt.h
+++ include/linux/fscrypt.h
@@ -307,7 +307,7 @@ fscrypt_free_dummy_policy(struct fscrypt_dummy_policy *dummy_policy)
 }
 
 /* keyring.c */
-void fscrypt_sb_delete(struct super_block *sb);
+void fscrypt_destroy_keyring(struct super_block *sb);
 int fscrypt_ioctl_add_key(struct file *filp, void __user *arg);
 int fscrypt_add_test_dummy_key(struct super_block *sb,
 			       const struct fscrypt_dummy_policy *dummy_policy);
@@ -521,7 +521,7 @@ fscrypt_free_dummy_policy(struct fscrypt_dummy_policy *dummy_policy)
 }
 
 /* keyring.c */
-static inline void fscrypt_sb_delete(struct super_block *sb)
+static inline void fscrypt_destroy_keyring(struct super_block *sb)
 {
 }
 
