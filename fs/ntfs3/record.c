36963cf225f890f97fd84af0a82d323043edd0f1
Konstantin Komarov <almaz.alexandrovich@paragon-software.com>
2022-10-11 20:21:03 +0300

    fs/ntfs3: Make if more readable
    This way it looks better.
    
    Signed-off-by: Konstantin Komarov <almaz.alexandrovich@paragon-software.com>

--- fs/ntfs3/record.c
+++ fs/ntfs3/record.c
@@ -265,10 +265,9 @@ struct ATTRIB *mi_enum_attr(struct mft_inode *mi, struct ATTRIB *attr)
 		if (t16 + t32 > asize)
 			return NULL;
 
-		if (attr->name_len &&
-		    le16_to_cpu(attr->name_off) + sizeof(short) * attr->name_len > t16) {
+		t32 = sizeof(short) * attr->name_len;
+		if (t32 && le16_to_cpu(attr->name_off) + t32 > t16)
 			return NULL;
-		}
 
 		return attr;
 	}
