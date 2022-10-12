5fa993737b29bffe931cc5d0feb87ebc34cd5bb3
ZhenGuo Yin <zhenguo.yin@amd.com>
2022-10-12 16:54:38 +0800

    drm/amd/pm: Init pm_attr_list when dpm is disabled
    [Why]
    In SRIOV multi-vf, dpm is always disabled, and pm_attr_list won't
    be initialized. There will be a NULL pointer call trace after
    removing the dpm check condition in amdgpu_pm_sysfs_fini.
    BUG: kernel NULL pointer dereference, address: 0000000000000000
    RIP: 0010:amdgpu_device_attr_remove_groups+0x20/0x90 [amdgpu]
    Call Trace:
      <TASK>
      amdgpu_pm_sysfs_fini+0x2f/0x40 [amdgpu]
      amdgpu_device_fini_hw+0xdf/0x290 [amdgpu]
    
    [How]
    List pm_attr_list should be initialized when dpm is disabled.
    
    Fixes: a6ad27cec585fe ("drm/amd/pm: Remove redundant check condition")
    Signed-off-by: ZhenGuo Yin <zhenguo.yin@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/amdgpu_pm.c
+++ drivers/gpu/drm/amd/pm/amdgpu_pm.c
@@ -3362,10 +3362,10 @@ int amdgpu_pm_sysfs_init(struct amdgpu_device *adev)
 	if (adev->pm.sysfs_initialized)
 		return 0;
 
+	INIT_LIST_HEAD(&adev->pm.pm_attr_list);
+
 	if (adev->pm.dpm_enabled == 0)
 		return 0;
-
-	INIT_LIST_HEAD(&adev->pm.pm_attr_list);
 
 	adev->pm.int_hwmon_dev = hwmon_device_register_with_groups(adev->dev,
 								   DRIVER_NAME, adev,
