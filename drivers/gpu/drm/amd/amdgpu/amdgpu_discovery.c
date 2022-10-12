907d5b697d850a548e6a226fef6e84f1b665bbb3
Yifan Zhang <yifan1.zhang@amd.com>
2022-10-12 11:09:50 +0800

    drm/amdgpu/discovery: enable gmc v11 for GC 11.0.4
    commit d5fd8c89ed206b2df3933bc4ea129401b2b60869 upstream.
    
    Enable gmc (graphic memory controller) v11 for GC 11.0.4.
    
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Reviewed-by: Aaron Liu <aaron.liu@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: "Limonciello, Mario" <Mario.Limonciello@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
@@ -1552,6 +1552,7 @@ static int amdgpu_discovery_set_gmc_ip_blocks(struct amdgpu_device *adev)
 	case IP_VERSION(11, 0, 1):
 	case IP_VERSION(11, 0, 2):
 	case IP_VERSION(11, 0, 3):
+	case IP_VERSION(11, 0, 4):
 		amdgpu_device_ip_block_add(adev, &gmc_v11_0_ip_block);
 		break;
 	default:
