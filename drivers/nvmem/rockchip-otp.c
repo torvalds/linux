799f7c7d26ba036b14dc30bf9888ef1c9f33fbb8
Finley Xiao <finley.xiao@rock-chips.com>
2022-10-13 14:40:15 +0800

    nvmem: rockchip-otp: Add support for rk3528 otp
    This adds the necessary data for handling otp on the rk3528.
    
    Signed-off-by: Finley Xiao <finley.xiao@rock-chips.com>
    Change-Id: I1ffe83be9f5497e7503876b9b3916a880811ed9d

--- drivers/nvmem/rockchip-otp.c
+++ drivers/nvmem/rockchip-otp.c
@@ -761,6 +761,17 @@ static const struct rockchip_data px30s_data = {
 	.reg_read = px30s_otp_read,
 };
 
+static const char * const rk3528_otp_clocks[] = {
+	"usr", "sbpi", "apb",
+};
+
+static const struct rockchip_data rk3528_data = {
+	.size = 0x80,
+	.clocks = rk3528_otp_clocks,
+	.num_clks = ARRAY_SIZE(rk3528_otp_clocks),
+	.reg_read = rk3568_otp_read,
+};
+
 static const char * const rk3568_otp_clocks[] = {
 	"usr", "sbpi", "apb", "phy",
 };
@@ -826,6 +837,12 @@ static const struct of_device_id rockchip_otp_match[] = {
 	{
 		.compatible = "rockchip,rk3308bs-otp",
 		.data = (void *)&px30s_data,
+	},
+#endif
+#ifdef CONFIG_CPU_RK3528
+	{
+		.compatible = "rockchip,rk3528-otp",
+		.data = (void *)&rk3528_data,
 	},
 #endif
 #ifdef CONFIG_CPU_RK3568
