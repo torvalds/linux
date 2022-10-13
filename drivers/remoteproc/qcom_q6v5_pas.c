1cfaa13b82c73977e8ddaca63c791203addc39c1
Sireesh Kodali <sireeshkodali1@gmail.com>
2022-10-13 09:57:48 +0530

    remoteproc: qcom: pas: Add MSM8953 ADSP PIL support
    Add support for the Audio DSP PIL found on the Qualcomm MSM8953
    platform. The same configuration is used on all SoCs based on the
    MSM8953 platform (SDM450, SDA450, SDM625, SDM632, APQ8053).
    
    Signed-off-by: Sireesh Kodali <sireeshkodali1@gmail.com>
    Reviewed-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Reviewed-by: Neil Armstrong <neil.armstrong@linaro.org>
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    Link: https://lore.kernel.org/r/20221013042749.104668-2-sireeshkodali1@gmail.com

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -924,6 +924,7 @@ static const struct adsp_data sm8450_mpss_resource = {
 
 static const struct of_device_id adsp_of_match[] = {
 	{ .compatible = "qcom,msm8226-adsp-pil", .data = &adsp_resource_init},
+	{ .compatible = "qcom,msm8953-adsp-pil", .data = &msm8996_adsp_resource},
 	{ .compatible = "qcom,msm8974-adsp-pil", .data = &adsp_resource_init},
 	{ .compatible = "qcom,msm8996-adsp-pil", .data = &msm8996_adsp_resource},
 	{ .compatible = "qcom,msm8996-slpi-pil", .data = &slpi_resource_init},
