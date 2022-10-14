8230f27b1ccc4b8976c137e3d6d690f9d4ffca8d
Steven Rostedt (Google) <rostedt@goodmis.org>
2022-10-14 08:04:56 -0400

    tracing: Add __cpumask to denote a trace event field that is a cpumask_t
    The trace events have a __bitmask field that can be used for anything
    that requires bitmasks. Although currently it is only used for CPU
    masks, it could be used in the future for any type of bitmasks.
    
    There is some user space tooling that wants to know if a field is a CPU
    mask and not just some random unsigned long bitmask. Introduce
    "__cpumask()" helper functions that work the same as the current
    __bitmask() helpers but displays in the format file:
    
      field:__data_loc cpumask_t *[] mask;    offset:36;      size:4; signed:0;
    
    Instead of:
    
      field:__data_loc unsigned long[] mask;  offset:32;      size:4; signed:0;
    
    The main difference is the type. Instead of "unsigned long" it is
    "cpumask_t *". Note, this type field needs to be a real type in the
    __dynamic_array() logic that both __cpumask and__bitmask use, but the
    comparison field requires it to be a scalar type whereas cpumask_t is a
    structure (non-scalar). But everything works when making it a pointer.
    
    Valentin added changes to remove the need of passing in "nr_bits" and the
    __cpumask will always use nr_cpumask_bits as its size.
    
    Link: https://lkml.kernel.org/r/20221014080456.1d32b989@rorschach.local.home
    
    Requested-by: Valentin Schneider <vschneid@redhat.com>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Signed-off-by: Valentin Schneider <vschneid@redhat.com>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- include/trace/stages/stage4_event_fields.h
+++ include/trace/stages/stage4_event_fields.h
@@ -46,6 +46,9 @@
 #undef __bitmask
 #define __bitmask(item, nr_bits) __dynamic_array(unsigned long, item, -1)
 
+#undef __cpumask
+#define __cpumask(item) __dynamic_array(cpumask_t *, item, -1)
+
 #undef __sockaddr
 #define __sockaddr(field, len) __dynamic_array(u8, field, len)
 
@@ -63,6 +66,9 @@
 
 #undef __rel_bitmask
 #define __rel_bitmask(item, nr_bits) __rel_dynamic_array(unsigned long, item, -1)
+
+#undef __rel_cpumask
+#define __rel_cpumask(item) __rel_dynamic_array(cpumask_t *, item, -1)
 
 #undef __rel_sockaddr
 #define __rel_sockaddr(field, len) __rel_dynamic_array(u8, field, len)
