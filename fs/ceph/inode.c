f86a48667b91202d502d753c707e8576a6fe265b
Dan Carpenter <dan.carpenter@oracle.com>
2022-10-14 12:36:53 +0300

    ceph: fix a NULL vs IS_ERR() check when calling ceph_lookup_inode()
    The ceph_lookup_inode() function returns error pointers.  It never
    returns NULL.
    
    Fixes: aa87052dd965 ("ceph: fix incorrectly showing the .snap size for stat")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Reviewed-by: Jeff Layton <jlayton@kernel.org>
    Reviewed-by: Xiubo Li <xiubli@redhat.com>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- fs/ceph/inode.c
+++ fs/ceph/inode.c
@@ -2492,7 +2492,7 @@ int ceph_getattr(struct user_namespace *mnt_userns, const struct path *path,
 			struct inode *parent;
 
 			parent = ceph_lookup_inode(sb, ceph_ino(inode));
-			if (!parent)
+			if (IS_ERR(parent))
 				return PTR_ERR(parent);
 
 			pci = ceph_inode(parent);
