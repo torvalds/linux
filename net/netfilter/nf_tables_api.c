d77f6908f9cec12857075f138419b8cea359121d
Pablo Neira Ayuso <pablo@netfilter.org>
2022-10-17 14:12:58 +0200

    netfilter: nf_tables: relax NFTA_SET_ELEM_KEY_END set flags requirements
    [ Upstream commit 96df8360dbb435cc69f7c3c8db44bf8b1c24cd7b ]
    
    Otherwise EINVAL is bogusly reported to userspace when deleting a set
    element. NFTA_SET_ELEM_KEY_END does not need to be set in case of:
    
    - insertion: if not present, start key is used as end key.
    - deletion: only start key needs to be specified, end key is ignored.
    
    Hence, relax the sanity check.
    
    Fixes: 88cccd908d51 ("netfilter: nf_tables: NFTA_SET_ELEM_KEY_END requires concat and interval flags")
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -5720,8 +5720,9 @@ static bool nft_setelem_valid_key_end(const struct nft_set *set,
 			  (NFT_SET_CONCAT | NFT_SET_INTERVAL)) {
 		if (flags & NFT_SET_ELEM_INTERVAL_END)
 			return false;
-		if (!nla[NFTA_SET_ELEM_KEY_END] &&
-		    !(flags & NFT_SET_ELEM_CATCHALL))
+
+		if (nla[NFTA_SET_ELEM_KEY_END] &&
+		    flags & NFT_SET_ELEM_CATCHALL)
 			return false;
 	} else {
 		if (nla[NFTA_SET_ELEM_KEY_END])
