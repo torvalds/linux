4b549ccce941798703f159b227aa28c716aa78fa
Christian Langrock <christian.langrock@secunet.com>
2022-10-17 08:34:47 +0200

    xfrm: replay: Fix ESN wrap around for GSO
    When using GSO it can happen that the wrong seq_hi is used for the last
    packets before the wrap around. This can lead to double usage of a
    sequence number. To avoid this, we should serialize this last GSO
    packet.
    
    Fixes: d7dbefc45cf5 ("xfrm: Add xfrm_replay_overflow functions for offloading")
    Co-developed-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Christian Langrock <christian.langrock@secunet.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- net/xfrm/xfrm_device.c
+++ net/xfrm/xfrm_device.c
@@ -97,6 +97,18 @@ static void xfrm_outer_mode_prep(struct xfrm_state *x, struct sk_buff *skb)
 	}
 }
 
+static inline bool xmit_xfrm_check_overflow(struct sk_buff *skb)
+{
+	struct xfrm_offload *xo = xfrm_offload(skb);
+	__u32 seq = xo->seq.low;
+
+	seq += skb_shinfo(skb)->gso_segs;
+	if (unlikely(seq < xo->seq.low))
+		return true;
+
+	return false;
+}
+
 struct sk_buff *validate_xmit_xfrm(struct sk_buff *skb, netdev_features_t features, bool *again)
 {
 	int err;
@@ -134,7 +146,8 @@ struct sk_buff *validate_xmit_xfrm(struct sk_buff *skb, netdev_features_t featur
 		return skb;
 	}
 
-	if (skb_is_gso(skb) && unlikely(x->xso.dev != dev)) {
+	if (skb_is_gso(skb) && (unlikely(x->xso.dev != dev) ||
+				unlikely(xmit_xfrm_check_overflow(skb)))) {
 		struct sk_buff *segs;
 
 		/* Packet got rerouted, fixup features and segment it. */
