2004b7b1803719eaaaee5fa6b089b1699a65d31d
Elaine Zhang <zhangqing@rock-chips.com>
2022-10-18 17:14:04 +0200

    clk: rockchip: allow additional mux options for cpu-clock frequency changes
    In order to improve the main frequency of CPU, the clock path of CPU is
    simplified as follows:
                             |--\
                             |   \            |--\
     --apll--|\              |    \           |   \
             | |--apll_core--|     \          |    \
     --24M---|/              |mux1 |--[gate]--|mux2|---clk_core
                             |     /          |    /
     --gpll--|\              |    /    |------|   /
             | |--gpll_core--|   /     |      |--/
     --24M---|/              |--/      |
                                       |
     -------apll_directly--------------|
    
    When the CPU requests high frequency, we want to use MUX2 select the
    "apll_directly".
    At low frequencies use MUX1 to select “apll_core" and then MUX2 to
    select "apll_core_gate".
    
    However, in this way, the CPU frequency conversion needs to be
    in the following order:
    1. MUX2 select to "apll_core_gate", MUX1 select "gpll_core"
    2. Apll sets slow_mode, sets APLL parameters, locks APLL, and then APLL
    sets normal_mode
    3. MUX1 select "apll_core", MUX2 select "apll_directly"
    
    So add pre_mux and post_mux options to cover this special requirements.
    
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>
    [rebase]
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>
    Link: https://lore.kernel.org/r/20221018151407.63395-7-sebastian.reichel@collabora.com
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>

--- drivers/clk/rockchip/clk.h
+++ drivers/clk/rockchip/clk.h
@@ -399,6 +399,8 @@ struct rockchip_cpuclk_clksel {
 struct rockchip_cpuclk_rate_table {
 	unsigned long prate;
 	struct rockchip_cpuclk_clksel divs[ROCKCHIP_CPUCLK_NUM_DIVIDERS];
+	struct rockchip_cpuclk_clksel pre_muxs[ROCKCHIP_CPUCLK_NUM_DIVIDERS];
+	struct rockchip_cpuclk_clksel post_muxs[ROCKCHIP_CPUCLK_NUM_DIVIDERS];
 };
 
 /**
