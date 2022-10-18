b57b08e6f431348363adffa5b6643fe3ec9dc7fe
Jeff Vanhoof <qjv001@motorola.com>
2022-10-18 16:50:40 -0500

    usb: gadget: uvc: fix sg handling during video encode
    In uvc_video_encode_isoc_sg, the uvc_request's sg list is
    incorrectly being populated leading to corrupt video being
    received by the remote end. When building the sg list the
    usage of buf->sg's 'dma_length' field is not correct and
    instead its 'length' field should be used.
    
    Fixes: e81e7f9a0eb9 ("usb: gadget: uvc: add scatter gather support")
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Jeff Vanhoof <qjv001@motorola.com>
    Signed-off-by: Dan Vacura <w36195@motorola.com>
    Link: https://lore.kernel.org/r/20221018215044.765044-5-w36195@motorola.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/gadget/function/uvc_video.c
+++ drivers/usb/gadget/function/uvc_video.c
@@ -157,10 +157,10 @@ uvc_video_encode_isoc_sg(struct usb_request *req, struct uvc_video *video,
 	sg = sg_next(sg);
 
 	for_each_sg(sg, iter, ureq->sgt.nents - 1, i) {
-		if (!len || !buf->sg || !sg_dma_len(buf->sg))
+		if (!len || !buf->sg || !buf->sg->length)
 			break;
 
-		sg_left = sg_dma_len(buf->sg) - buf->offset;
+		sg_left = buf->sg->length - buf->offset;
 		part = min_t(unsigned int, len, sg_left);
 
 		sg_set_page(iter, sg_page(buf->sg), part, buf->offset);
