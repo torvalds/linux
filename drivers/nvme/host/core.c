66c56b2328393622f70e4e9960a4d62e1be25348
Christoph Hellwig <hch@lst.de>
2022-10-18 16:55:55 +0200

    nvme-hwmon: consistently ignore errors from nvme_hwmon_init
    [ Upstream commit 6b8cf94005187952f794c0c4ed3920a1e8accfa3 ]
    
    An NVMe controller works perfectly fine even when the hwmon
    initialization fails.  Stop returning errors that do not come from a
    controller reset from nvme_hwmon_init to handle this case consistently.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Reviewed-by: Serge Semin <fancer.lancer@gmail.com>
    Stable-dep-of: c94b7f9bab22 ("nvme-hwmon: kmalloc the NVME SMART log buffer")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -3088,8 +3088,12 @@ int nvme_init_ctrl_finish(struct nvme_ctrl *ctrl)
 		return ret;
 
 	if (!ctrl->identified && !nvme_discovery_ctrl(ctrl)) {
+		/*
+		 * Do not return errors unless we are in a controller reset,
+		 * the controller works perfectly fine without hwmon.
+		 */
 		ret = nvme_hwmon_init(ctrl);
-		if (ret < 0)
+		if (ret == -EINTR)
 			return ret;
 	}
 
