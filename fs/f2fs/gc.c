f1acf7e69355751bc09a70d9e5fefe3b4d1e02d2
Zhang Qilong <zhangqilong3@huawei.com>
2022-10-18 10:45:32 +0800

    f2fs: Fix the race condition of resize flag between resizefs
    [ Upstream commit 28fc4e9077ce59ab28c89c20dc6be5154473218f ]
    
    Because the set/clear SBI_IS_RESIZEFS flag not between any locks,
    In the following case:
      thread1			thread2
       ->ioctl(resizefs)
        ->set RESIZEFS flag		 ->ioctl(resizefs)
        ...                   	  ->set RESIZEFS flag
        ->clear RESIZEFS flag
        				  ->resizefs stream
    				    # No RESIZEFS flag in the stream
    
    Also before freeze_super, the resizefs not started, we should not set
    the SBI_IS_RESIZEFS flag.
    
    So move the set/clear SBI_IS_RESIZEFS flag between the cp_mutex and
    gc_lock.
    
    Fixes: b4b10061ef98 ("f2fs: refactor resize_fs to avoid meta updates in progress")
    Signed-off-by: Zhang Xiaoxu <zhangxiaoxu5@huawei.com>
    Signed-off-by: Zhang Qilong <zhangqilong3@huawei.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -2035,8 +2035,6 @@ out_unlock:
 	if (err)
 		return err;
 
-	set_sbi_flag(sbi, SBI_IS_RESIZEFS);
-
 	freeze_super(sbi->sb);
 	down_write(&sbi->gc_lock);
 	mutex_lock(&sbi->cp_mutex);
@@ -2052,6 +2050,7 @@ out_unlock:
 	if (err)
 		goto out_err;
 
+	set_sbi_flag(sbi, SBI_IS_RESIZEFS);
 	err = free_segment_range(sbi, secs, false);
 	if (err)
 		goto recover_out;
@@ -2075,6 +2074,7 @@ out_unlock:
 		f2fs_commit_super(sbi, false);
 	}
 recover_out:
+	clear_sbi_flag(sbi, SBI_IS_RESIZEFS);
 	if (err) {
 		set_sbi_flag(sbi, SBI_NEED_FSCK);
 		f2fs_err(sbi, "resize_fs failed, should run fsck to repair!");
@@ -2087,6 +2087,5 @@ out_err:
 	mutex_unlock(&sbi->cp_mutex);
 	up_write(&sbi->gc_lock);
 	thaw_super(sbi->sb);
-	clear_sbi_flag(sbi, SBI_IS_RESIZEFS);
 	return err;
 }
