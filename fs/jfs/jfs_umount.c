dee8744524096514109a9680a9c45a4259dae2a4
Colin Ian King <colin.i.king@gmail.com>
2022-10-18 16:23:10 +0100

    jfs: remove redundant assignments to ipaimap and ipaimap2
    The pointers ipaimap and ipaimap2 are re-assigned with values a second
    time with the same values when they were initialized. The re-assignments
    are redundant and can be removed.
    
    Cleans up two clang scan build warnings:
    fs/jfs/jfs_umount.c:42:16: warning: Value stored to 'ipaimap' during
    its initialization is never read [deadcode.DeadStores]
    
    fs/jfs/jfs_umount.c:43:16: warning: Value stored to 'ipaimap2' during
    its initialization is never read [deadcode.DeadStores]
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>

--- fs/jfs/jfs_umount.c
+++ fs/jfs/jfs_umount.c
@@ -68,7 +68,6 @@ int jfs_umount(struct super_block *sb)
 	/*
 	 * close secondary aggregate inode allocation map
 	 */
-	ipaimap2 = sbi->ipaimap2;
 	if (ipaimap2) {
 		diUnmount(ipaimap2, 0);
 		diFreeSpecial(ipaimap2);
@@ -78,7 +77,6 @@ int jfs_umount(struct super_block *sb)
 	/*
 	 * close aggregate inode allocation map
 	 */
-	ipaimap = sbi->ipaimap;
 	diUnmount(ipaimap, 0);
 	diFreeSpecial(ipaimap);
 	sbi->ipaimap = NULL;
