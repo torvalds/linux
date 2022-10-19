b7e2647671a2e7eb8d5dd1cb73464d7d760f6139
Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
2022-10-19 12:11:38 +0300

    serial: tegra: Use uart_xmit_advance()
    Take advantage of the new uart_xmit_advance() helper.
    
    Reviewed-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Link: https://lore.kernel.org/r/20221019091151.6692-32-ilpo.jarvinen@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/serial-tegra.c
+++ drivers/tty/serial/serial-tegra.c
@@ -496,8 +496,7 @@ static void tegra_uart_fill_tx_fifo(struct tegra_uart_port *tup, int max_bytes)
 				break;
 		}
 		tegra_uart_write(tup, xmit->buf[xmit->tail], UART_TX);
-		xmit->tail = (xmit->tail + 1) & (UART_XMIT_SIZE - 1);
-		tup->uport.icount.tx++;
+		uart_xmit_advance(&tup->uport, 1);
 	}
 }
 
