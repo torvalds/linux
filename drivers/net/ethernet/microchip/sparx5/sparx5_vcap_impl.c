8beef08f4618c9bfab9374fc72930fc6ed70fdc1
Steen Hegelund <steen.hegelund@microchip.com>
2022-10-20 15:08:56 +0200

    net: microchip: sparx5: Adding initial VCAP API support
    This provides the initial VCAP API framework and Sparx5 specific VCAP
    implementation.
    
    When the Sparx5 Switchdev driver is initialized it will also initialize its
    VCAP module, and this hooks up the concrete Sparx5 VCAP model to the VCAP
    API, so that the VCAP API knows what VCAP instances are available.
    
    Signed-off-by: Steen Hegelund <steen.hegelund@microchip.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ drivers/net/ethernet/microchip/sparx5/sparx5_vcap_impl.c
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-2.0+
+/* Microchip Sparx5 Switch driver VCAP implementation
+ *
+ * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
+ *
+ * The Sparx5 Chip Register Model can be browsed at this location:
+ * https://github.com/microchip-ung/sparx-5_reginfo
+ */
+
+#include <linux/types.h>
+#include <linux/list.h>
+
+#include "vcap_api.h"
+#include "sparx5_main_regs.h"
+#include "sparx5_main.h"
+
+/* Allocate a vcap control and vcap instances and configure the system */
+int sparx5_vcap_init(struct sparx5 *sparx5)
+{
+	struct vcap_control *ctrl;
+
+	/* Create a VCAP control instance that owns the platform specific VCAP
+	 * model with VCAP instances and information about keysets, keys,
+	 * actionsets and actions
+	 */
+	ctrl = kzalloc(sizeof(*ctrl), GFP_KERNEL);
+	if (!ctrl)
+		return -ENOMEM;
+
+	sparx5->vcap_ctrl = ctrl;
+
+	return 0;
+}
+
+void sparx5_vcap_destroy(struct sparx5 *sparx5)
+{
+	if (!sparx5->vcap_ctrl)
+		return;
+
+	kfree(sparx5->vcap_ctrl);
+}
