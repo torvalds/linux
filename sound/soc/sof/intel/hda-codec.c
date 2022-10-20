e9441675edc1bb8dbfadacf68aafacca60d65a25
Yang Yingliang <yangyingliang@huawei.com>
2022-10-20 19:01:57 +0800

    ASoC: SOF: Intel: hda-codec: fix possible memory leak in hda_codec_device_init()
    If snd_hdac_device_register() fails, 'codec' and name allocated in
    dev_set_name() called in snd_hdac_device_init() are leaked. Fix this
    by calling put_device(), so they can be freed in snd_hda_codec_dev_release()
    and kobject_cleanup().
    
    Fixes: 829c67319806 ("ASoC: SOF: Intel: Introduce HDA codec init and exit routines")
    Fixes: dfe66a18780d ("ALSA: hdac_ext: add extended HDA bus")
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Reviewed-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    Link: https://lore.kernel.org/r/20221020110157.1450191-1-yangyingliang@huawei.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sof/intel/hda-codec.c
+++ sound/soc/sof/intel/hda-codec.c
@@ -109,11 +109,6 @@ EXPORT_SYMBOL_NS(hda_codec_jack_check, SND_SOC_SOF_HDA_AUDIO_CODEC);
 #define is_generic_config(x)	0
 #endif
 
-static void hda_codec_device_exit(struct device *dev)
-{
-	snd_hdac_device_exit(dev_to_hdac_dev(dev));
-}
-
 static struct hda_codec *hda_codec_device_init(struct hdac_bus *bus, int addr, int type)
 {
 	struct hda_codec *codec;
@@ -126,12 +121,11 @@ static struct hda_codec *hda_codec_device_init(struct hdac_bus *bus, int addr, i
 	}
 
 	codec->core.type = type;
-	codec->core.dev.release = hda_codec_device_exit;
 
 	ret = snd_hdac_device_register(&codec->core);
 	if (ret) {
 		dev_err(bus->dev, "failed to register hdac device\n");
-		snd_hdac_device_exit(&codec->core);
+		put_device(&codec->core.dev);
 		return ERR_PTR(ret);
 	}
 
