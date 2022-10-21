00009406f0dbc53b95b9062c0cc297d6893ff394
Rafael Mendonca <rafaelmendsr@gmail.com>
2022-10-20 23:01:13 -0300

    x86/kvm: Remove unused virt to phys translation in kvm_guest_cpu_init()
    Presumably, this was introduced due to a conflict resolution with
    commit ef68017eb570 ("x86/kvm: Handle async page faults directly through
    do_page_fault()"), given that the last posted version [1] of the blamed
    commit was not based on the aforementioned commit.
    
    [1] https://lore.kernel.org/kvm/20200525144125.143875-9-vkuznets@redhat.com/
    
    Fixes: b1d405751cd5 ("KVM: x86: Switch KVM guest to using interrupts for page ready APF delivery")
    Signed-off-by: Rafael Mendonca <rafaelmendsr@gmail.com>
    Message-Id: <20221021020113.922027-1-rafaelmendsr@gmail.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kernel/kvm.c
+++ arch/x86/kernel/kvm.c
@@ -349,7 +349,7 @@ static notrace void kvm_guest_apic_eoi_write(u32 reg, u32 val)
 static void kvm_guest_cpu_init(void)
 {
 	if (kvm_para_has_feature(KVM_FEATURE_ASYNC_PF_INT) && kvmapf) {
-		u64 pa = slow_virt_to_phys(this_cpu_ptr(&apf_reason));
+		u64 pa;
 
 		WARN_ON_ONCE(!static_branch_likely(&kvm_async_pf_enabled));
 
