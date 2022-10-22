b3204cb3e0ad7e66db2477550a9b4b827841c3b1
Zack Rusin <zackr@vmware.com>
2022-10-22 00:02:33 -0400

    drm/vmwgfx: Remove explicit and broken vblank handling
    [ Upstream commit 2e10cdc6e85de5998b0b140deff01765ceb92f64 ]
    
    The explicit vblank handling was never finished. The driver never had
    the full implementation of vblank and what was there is emulated
    by DRM when the driver doesn't pretend to be implementing it itself.
    
    Let DRM handle the vblank emulation and stop pretending the driver is
    doing anything special with vblank. In the future it would make sense
    to implement helpers for full vblank handling because vkms and
    amdgpu_vkms already have that code. Exporting it to common helpers and
    having all three drivers share it would make sense (that would be largely
    just to allow more of igt to run).
    
    Signed-off-by: Zack Rusin <zackr@vmware.com>
    Reviewed-by: Maaz Mombasawala <mombasawalam@vmware.com>
    Reviewed-by: Martin Krastev <krastevm@vmware.com>
    Reviewed-by: Michael Banack <banackm@vmware.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20221022040236.616490-15-zack@kde.org
    Stable-dep-of: a37a512db3fa ("drm/vmwgfx: Fix Legacy Display Unit atomic drm support")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/vmwgfx/vmwgfx_ldu.c
+++ drivers/gpu/drm/vmwgfx/vmwgfx_ldu.c
@@ -28,7 +28,6 @@
 #include <drm/drm_atomic.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_fourcc.h>
-#include <drm/drm_vblank.h>
 
 #include "vmwgfx_kms.h"
 
@@ -235,9 +234,6 @@ static const struct drm_crtc_funcs vmw_legacy_crtc_funcs = {
 	.atomic_duplicate_state = vmw_du_crtc_duplicate_state,
 	.atomic_destroy_state = vmw_du_crtc_destroy_state,
 	.set_config = drm_atomic_helper_set_config,
-	.get_vblank_counter = vmw_get_vblank_counter,
-	.enable_vblank = vmw_enable_vblank,
-	.disable_vblank = vmw_disable_vblank,
 };
 
 
@@ -506,10 +502,6 @@ int vmw_kms_ldu_init_display(struct vmw_private *dev_priv)
 	dev_priv->ldu_priv->num_active = 0;
 	dev_priv->ldu_priv->last_num_active = 0;
 	dev_priv->ldu_priv->fb = NULL;
-
-	ret = drm_vblank_init(dev, num_display_units);
-	if (ret != 0)
-		goto err_free;
 
 	vmw_kms_create_implicit_placement_property(dev_priv);
 
