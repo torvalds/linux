270a7c3eba574364c43a6ec8583ec995122c255b
Paul Cercueil <paul@crapouillou.net>
2022-10-23 10:48:36 +0100

    mfd: sec: Remove #ifdef guards for PM related functions
    Use the new DEFINE_SIMPLE_DEV_PM_OPS() and pm_sleep_ptr() macros
    to handle the .suspend/.resume callbacks.
    
    These macros allow the suspend and resume functions to be automatically
    dropped by the compiler when CONFIG_SUSPEND is disabled, without having
    to use #ifdef guards.
    
    This has the advantage of always compiling these functions in,
    independently of any Kconfig option. Thanks to that, bugs and other
    regressions are subsequently easier to catch.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>
    Reviewed-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Signed-off-by: Lee Jones <lee@kernel.org>

--- drivers/mfd/sec-core.c
+++ drivers/mfd/sec-core.c
@@ -454,7 +454,6 @@ static void sec_pmic_shutdown(struct i2c_client *i2c)
 	regmap_update_bits(sec_pmic->regmap_pmic, reg, mask, 0);
 }
 
-#ifdef CONFIG_PM_SLEEP
 static int sec_pmic_suspend(struct device *dev)
 {
 	struct i2c_client *i2c = to_i2c_client(dev);
@@ -487,14 +486,14 @@ static int sec_pmic_resume(struct device *dev)
 
 	return 0;
 }
-#endif /* CONFIG_PM_SLEEP */
 
-static SIMPLE_DEV_PM_OPS(sec_pmic_pm_ops, sec_pmic_suspend, sec_pmic_resume);
+static DEFINE_SIMPLE_DEV_PM_OPS(sec_pmic_pm_ops,
+				sec_pmic_suspend, sec_pmic_resume);
 
 static struct i2c_driver sec_pmic_driver = {
 	.driver = {
 		   .name = "sec_pmic",
-		   .pm = &sec_pmic_pm_ops,
+		   .pm = pm_sleep_ptr(&sec_pmic_pm_ops),
 		   .of_match_table = sec_dt_match,
 	},
 	.probe_new = sec_pmic_probe,
