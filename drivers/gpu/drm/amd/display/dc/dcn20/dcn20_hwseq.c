00efc965a7b7990e63e3506e6d240e6271bd8571
Alvin Lee <Alvin.Lee2@amd.com>
2022-10-24 13:39:02 -0400

    drm/amd/display: Wait for VBLANK during pipe programming
    [Description]
    - Wait for vblank during front end programming
      for global sync to ensure all double buffer
      updates take.
    - This prevents underflow in some cases.
    
    Reviewed-by: Martin Leung <Martin.Leung@amd.com>
    Acked-by: Alan Liu <HaoPing.Liu@amd.com>
    Signed-off-by: Alvin Lee <Alvin.Lee2@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn20/dcn20_hwseq.c
+++ drivers/gpu/drm/amd/display/dc/dcn20/dcn20_hwseq.c
@@ -1663,6 +1663,7 @@ static void dcn20_program_pipe(
 				pipe_ctx->pipe_dlg_param.vupdate_width);
 
 		if (pipe_ctx->stream->mall_stream_config.type != SUBVP_PHANTOM) {
+			pipe_ctx->stream_res.tg->funcs->wait_for_state(pipe_ctx->stream_res.tg, CRTC_STATE_VBLANK);
 			pipe_ctx->stream_res.tg->funcs->wait_for_state(pipe_ctx->stream_res.tg, CRTC_STATE_VACTIVE);
 		}
 
