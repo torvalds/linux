af699fd6a21c46be1764ddf04c577e26fdba2687
Peifeng Li <lipeifeng@oppo.com>
2022-10-25 17:20:35 +0800

    ANDROID: vendor_hook: skip trace_android_vh_page_trylock_set when ignore_references is true
    Avoid async-reclaim to cause to reclaim-delay when ignore_references is true.
    
    Bug: 240003372
    Signed-off-by: Peifeng Li <lipeifeng@oppo.com>
    Change-Id: Iaf50bd4ac53f748da0dac93324c6d94de11e01e9

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1352,8 +1352,8 @@ static unsigned int shrink_page_list(struct list_head *page_list,
 
 			if (unlikely(PageTransHuge(page)))
 				flags |= TTU_SPLIT_HUGE_PMD;
-
-			trace_android_vh_page_trylock_set(page);
+			if (!ignore_references)
+				trace_android_vh_page_trylock_set(page);
 			if (!try_to_unmap(page, flags)) {
 				stat->nr_unmap_fail += nr_pages;
 				if (!was_swapbacked && PageSwapBacked(page))
