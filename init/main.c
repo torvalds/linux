051f5dcf144aa7659c4f4be04c66c3eda9b1bad3
Peter Zijlstra <peterz@infradead.org>
2022-10-25 21:38:25 +0200

    x86/mm: Initialize text poking earlier
    commit 5b93a83649c7cba3a15eb7e8959b250841acb1b1 upstream.
    
    Move poking_init() up a bunch; specifically move it right after
    mm_init() which is right before ftrace_init().
    
    This will allow simplifying ftrace text poking which currently has
    a bunch of exceptions for early boot.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20221025201057.881703081@infradead.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- init/main.c
+++ init/main.c
@@ -992,7 +992,7 @@ asmlinkage __visible void __init __no_sanitize_address start_kernel(void)
 	sort_main_extable();
 	trap_init();
 	mm_init();
-
+	poking_init();
 	ftrace_init();
 
 	/* trace_printk can be enabled here */
@@ -1125,8 +1125,6 @@ asmlinkage __visible void __init __no_sanitize_address start_kernel(void)
 	cgroup_init();
 	taskstats_init_early();
 	delayacct_init();
-
-	poking_init();
 
 	acpi_subsystem_init();
 	arch_post_acpi_subsys_init();
