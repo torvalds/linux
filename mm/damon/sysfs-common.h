67ef7b0f42a143e2cc471777397c2f6571e5dddd
SeongJae Park <sj@kernel.org>
2022-10-26 22:59:41 +0000

    UPSTREAM: mm/damon/sysfs: split out schemes directory implementation to separate file
    DAMON sysfs interface for 'schemes' directory is implemented using about
    one thousand lines of code.  It has no strong dependency with other
    parts of its file, so split it out to another file for better code
    management.
    
    Link: https://lkml.kernel.org/r/20221026225943.100429-11-sj@kernel.org
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (cherry picked from commit c8e7b4d0ba348a8ef14956a80c780f152f433764)
    
    Bug: 300502883
    Change-Id: I3a8f63dc4f6de6f4662b6f1a7d12afe1e25b36d2
    Signed-off-by: cui yangpei <cuiyangpei@xiaomi.com>

--- mm/damon/sysfs-common.h
+++ mm/damon/sysfs-common.h
@@ -22,3 +22,25 @@ struct damon_sysfs_ul_range *damon_sysfs_ul_range_alloc(
 void damon_sysfs_ul_range_release(struct kobject *kobj);
 
 extern struct kobj_type damon_sysfs_ul_range_ktype;
+
+/*
+ * schemes directory
+ */
+
+struct damon_sysfs_schemes {
+	struct kobject kobj;
+	struct damon_sysfs_scheme **schemes_arr;
+	int nr;
+};
+
+struct damon_sysfs_schemes *damon_sysfs_schemes_alloc(void);
+void damon_sysfs_schemes_rm_dirs(struct damon_sysfs_schemes *schemes);
+
+extern struct kobj_type damon_sysfs_schemes_ktype;
+
+int damon_sysfs_set_schemes(struct damon_ctx *ctx,
+		struct damon_sysfs_schemes *sysfs_schemes);
+
+void damon_sysfs_schemes_update_stats(
+		struct damon_sysfs_schemes *sysfs_schemes,
+		struct damon_ctx *ctx);
