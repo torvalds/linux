eb8be2dbfbb453a33f056df9e13fc878afa41c7d
James Smart <jsmart2021@gmail.com>
2022-10-28 14:08:24 -0700

    Revert "scsi: lpfc: Fix locking for lpfc_sli_iocbq_lookup()"
    This reverts commit 9a570069cdbbc28c4b5b4632d5c9369371ec739c.
    
    LTS 5.15 pulled in several lpfc "SLI Path split" patches.  The Path
    Split mods were a 14-patch set, which refactors the driver from
    to split the sli-3 hw (now eol) from the sli-4 hw and use sli4
    structures natively. The patches are highly inter-related.
    
    Given only some of the patches were included, it created a situation
    where FLOGI's fail, thus SLI Ports can't start communication.
    
    Reverting this patch as its a fix specific to the Path Split patches,
    which were partially included and now being pulled from 5.15.
    
    Signed-off-by: James Smart <jsmart2021@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/scsi/lpfc/lpfc_sli.c
+++ drivers/scsi/lpfc/lpfc_sli.c
@@ -3644,15 +3644,7 @@ lpfc_sli_process_sol_iocb(struct lpfc_hba *phba, struct lpfc_sli_ring *pring,
 	unsigned long iflag;
 	u32 ulp_command, ulp_status, ulp_word4, ulp_context, iotag;
 
-	if (phba->sli_rev == LPFC_SLI_REV4)
-		spin_lock_irqsave(&pring->ring_lock, iflag);
-	else
-		spin_lock_irqsave(&phba->hbalock, iflag);
 	cmdiocbp = lpfc_sli_iocbq_lookup(phba, pring, saveq);
-	if (phba->sli_rev == LPFC_SLI_REV4)
-		spin_unlock_irqrestore(&pring->ring_lock, iflag);
-	else
-		spin_unlock_irqrestore(&phba->hbalock, iflag);
 
 	ulp_command = get_job_cmnd(phba, saveq);
 	ulp_status = get_job_ulpstatus(phba, saveq);
@@ -3989,8 +3981,10 @@ lpfc_sli_handle_fast_ring_event(struct lpfc_hba *phba,
 				break;
 			}
 
+			spin_unlock_irqrestore(&phba->hbalock, iflag);
 			cmdiocbq = lpfc_sli_iocbq_lookup(phba, pring,
 							 &rspiocbq);
+			spin_lock_irqsave(&phba->hbalock, iflag);
 			if (unlikely(!cmdiocbq))
 				break;
 			if (cmdiocbq->cmd_flag & LPFC_DRIVER_ABORTED)
