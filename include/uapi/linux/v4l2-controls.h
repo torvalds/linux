867d3b275c385e80eaf7c46ab08674ae0af47bf3
Jammy Huang <jammy_huang@aspeedtech.com>
2022-10-28 10:35:51 +0800

    media: v4l2-ctrls: Reserve controls for ASPEED
    Reserve controls for ASPEED video family. Aspeed video engine contains a
    few features which improve video quality, reduce amount of compressed
    data, and etc. Hence, 16 controls are reserved for these aspeed
    proprietary features.
    
    Signed-off-by: Jammy Huang <jammy_huang@aspeedtech.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

--- include/uapi/linux/v4l2-controls.h
+++ include/uapi/linux/v4l2-controls.h
@@ -231,6 +231,12 @@ enum v4l2_colorfx {
  */
 #define V4L2_CID_USER_DW100_BASE		(V4L2_CID_USER_BASE + 0x1190)
 
+/*
+ * The base for Aspeed driver controls.
+ * We reserve 16 controls for this driver.
+ */
+#define V4L2_CID_USER_ASPEED_BASE		(V4L2_CID_USER_BASE + 0x11a0)
+
 /* MPEG-class control IDs */
 /* The MPEG controls are applicable to all codec controls
  * and the 'MPEG' part of the define is historical */
