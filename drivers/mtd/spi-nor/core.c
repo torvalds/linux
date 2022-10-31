c536932b6a2aa9fca61eaf422f287eaf5d54895a
Allen-KH Cheng <allen-kh.cheng@mediatek.com>
2022-10-31 20:46:33 +0800

    mtd: spi-nor: Fix the number of bytes for the dummy cycles
    [ Upstream commit fdc20370d93e8c6d2f448a539d08c2c064af7694 ]
    
    The number of bytes used by spi_nor_spimem_check_readop() may be
    incorrect for the dummy cycles. Since nor->read_dummy is not initialized
    before spi_nor_spimem_adjust_hwcaps().
    
    We use both mode and wait state clock cycles instead of nor->read_dummy.
    
    Fixes: 0e30f47232ab ("mtd: spi-nor: add support for DTR protocol")
    Co-developed-by: Bayi Cheng <bayi.cheng@mediatek.com>
    Signed-off-by: Bayi Cheng <bayi.cheng@mediatek.com>
    Signed-off-by: Allen-KH Cheng <allen-kh.cheng@mediatek.com>
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Tested-by: Dhruva Gole <d-gole@ti.com>
    Tested-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Reviewed-by: Pratyush Yadav <pratyush@kernel.org>
    Link: https://lore.kernel.org/r/20221031124633.13189-1-allen-kh.cheng@mediatek.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/mtd/spi-nor/core.c
+++ drivers/mtd/spi-nor/core.c
@@ -1914,7 +1914,8 @@ static int spi_nor_spimem_check_readop(struct spi_nor *nor,
 	spi_nor_spimem_setup_op(nor, &op, read->proto);
 
 	/* convert the dummy cycles to the number of bytes */
-	op.dummy.nbytes = (nor->read_dummy * op.dummy.buswidth) / 8;
+	op.dummy.nbytes = (read->num_mode_clocks + read->num_wait_states) *
+			  op.dummy.buswidth / 8;
 	if (spi_nor_protocol_is_dtr(nor->read_proto))
 		op.dummy.nbytes *= 2;
 
