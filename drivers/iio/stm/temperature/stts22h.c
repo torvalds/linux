d004087170d16a94e8ec08a261faebcdfcdafb21
Denis Ciocca <denis.ciocca@st.com>
2022-10-31 12:54:14 -0700

    drivers:iio:stm:temp:stts22h: remove VLA in the i2c write function
    Removing VLAs as per displeasure demonstrated by kernel
    developers. Additionally, Android kernels built with clang defaults
    forbids usage of it.
    
    Signed-off-by: Denis Ciocca <denis.ciocca@st.com>
    Change-Id: Ib01aa70f76712c02b2ae3b77691b4d01a0c57900
    Reviewed-on: https://gerrit.st.com/c/linuxandroidopen/stm-ldd-iio/+/273634
    Tested-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Mario TESI <mario.tesi@st.com>

--- drivers/iio/stm/temperature/stts22h.c
+++ drivers/iio/stm/temperature/stts22h.c
@@ -83,7 +83,10 @@ static int st_stts22h_write(struct device *dev, u8 addr, int len,
 {
 	struct i2c_client *client = to_i2c_client(dev);
 	struct i2c_msg msg;
-	u8 send[len + 1];
+	u8 send[4];
+
+	if (len > ARRAY_SIZE(send))
+		return -ENOMEM;
 
 	send[0] = addr;
 	memcpy(&send[1], data, len * sizeof(u8));
