34662cfed8aafb381fff85665b401c8384624249
Denis Ciocca <denis.ciocca@st.com>
2022-10-31 12:47:53 -0700

    drivers:iio:stm:imu:asm330lhhx: remove VLA usage in read_oneshot()
    Removing VLAs as per displeasure demonstrated by kernel
    developers. Additionally, Android kernels built with clang and by
    defaults forbids usage of it.
    
    Signed-off-by: Denis Ciocca <denis.ciocca@st.com>
    Change-Id: I08984fcbf4b1960f6f9cd8552995d80dbbe1904f
    Reviewed-on: https://gerrit.st.com/c/linuxandroidopen/stm-ldd-iio/+/273633
    Tested-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Mario TESI <mario.tesi@st.com>

--- drivers/iio/stm/imu/st_asm330lhhx/st_asm330lhhx_shub.c
+++ drivers/iio/stm/imu/st_asm330lhhx/st_asm330lhhx_shub.c
@@ -720,7 +720,10 @@ st_asm330lhhx_shub_read_oneshot(struct st_asm330lhhx_sensor *sensor,
 				int *val)
 {
 	int err, delay, len = ch->scan_type.realbits >> 3;
-	u8 data[len];
+	u8 data[4];
+
+	if (len > ARRAY_SIZE(data))
+		return -ENOMEM;
 
 	err = st_asm330lhhx_shub_set_enable(sensor, true);
 	if (err < 0)
