e66e20d71d79e66b2bd03b51964a5a7eddc5e55c
Dave Gerlach <d-gerlach@ti.com>
2022-11-01 13:09:32 -0500

    cpufreq: dt-platdev: Blacklist ti,am625 SoC
    Add ti,am625 SoC to the blacklist as the ti-cpufreq driver will handle
    creating the cpufreq-dt platform device after it completes so it is not
    created twice.
    
    Signed-off-by: Dave Gerlach <d-gerlach@ti.com>
    Signed-off-by: Vibhore Vardhan <vibhore@ti.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -160,6 +160,7 @@ static const struct of_device_id blocklist[] __initconst = {
 	{ .compatible = "ti,am43", },
 	{ .compatible = "ti,dra7", },
 	{ .compatible = "ti,omap3", },
+	{ .compatible = "ti,am625", },
 
 	{ .compatible = "qcom,ipq8064", },
 	{ .compatible = "qcom,apq8064", },
