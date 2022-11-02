a778f5d46b6287ebe26e24b48f3e8079c2db8ed2
Andrii Nakryiko <andrii@kernel.org>
2022-11-02 11:25:17 -0700

    tools/headers: Pull in stddef.h to uapi to fix BPF selftests build in CI
    With recent sync of linux/in.h tools/include headers are now relying on
    __DECLARE_FLEX_ARRAY macro, which isn't itself defined inside
    tools/include headers anywhere and is instead assumed to be present in
    system-wide UAPI header. This breaks isolated environments that don't
    have kernel UAPI headers installed system-wide, like BPF CI ([0]).
    
    To fix this, bring in include/uapi/linux/stddef.h into tools/include.
    We can't just copy/paste it, though, it has to be processed with
    scripts/headers_install.sh, which has a dependency on scripts/unifdef.
    So the full command to (re-)generate stddef.h for inclusion into
    tools/include directory is:
    
      $ make scripts_unifdef && \
        cp $KBUILD_OUTPUT/scripts/unifdef scripts/ && \
        scripts/headers_install.sh include/uapi/linux/stddef.h tools/include/uapi/linux/stddef.h
    
    This assumes KBUILD_OUTPUT envvar is set and used for out-of-tree builds.
    
      [0] https://github.com/kernel-patches/bpf/actions/runs/3379432493/jobs/5610982609
    
    Fixes: 036b8f5b8970 ("tools headers uapi: Update linux/in.h copy")
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Cc: Jakub Kicinski <kuba@kernel.org>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Link: https://lore.kernel.org/bpf/20221102182517.2675301-2-andrii@kernel.org

---
+++ tools/include/uapi/linux/stddef.h
@@ -0,0 +1,47 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _LINUX_STDDEF_H
+#define _LINUX_STDDEF_H
+
+
+
+#ifndef __always_inline
+#define __always_inline __inline__
+#endif
+
+/**
+ * __struct_group() - Create a mirrored named and anonyomous struct
+ *
+ * @TAG: The tag name for the named sub-struct (usually empty)
+ * @NAME: The identifier name of the mirrored sub-struct
+ * @ATTRS: Any struct attributes (usually empty)
+ * @MEMBERS: The member declarations for the mirrored structs
+ *
+ * Used to create an anonymous union of two structs with identical layout
+ * and size: one anonymous and one named. The former's members can be used
+ * normally without sub-struct naming, and the latter can be used to
+ * reason about the start, end, and size of the group of struct members.
+ * The named struct can also be explicitly tagged for layer reuse, as well
+ * as both having struct attributes appended.
+ */
+#define __struct_group(TAG, NAME, ATTRS, MEMBERS...) \
+	union { \
+		struct { MEMBERS } ATTRS; \
+		struct TAG { MEMBERS } ATTRS NAME; \
+	}
+
+/**
+ * __DECLARE_FLEX_ARRAY() - Declare a flexible array usable in a union
+ *
+ * @TYPE: The type of each flexible array element
+ * @NAME: The name of the flexible array member
+ *
+ * In order to have a flexible array member in a union or alone in a
+ * struct, it needs to be wrapped in an anonymous struct with at least 1
+ * named member, but that member can be empty.
+ */
+#define __DECLARE_FLEX_ARRAY(TYPE, NAME)	\
+	struct { \
+		struct { } __empty_ ## NAME; \
+		TYPE NAME[]; \
+	}
+#endif
