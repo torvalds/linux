5004d383fe8cdd20f0e2476f50323b03c5278bf0
Zhen Lei <thunder.leizhen@huawei.com>
2022-11-02 16:49:15 +0800

    kallsyms: Correctly sequence symbols when CONFIG_LTO_CLANG=y
    [ Upstream commit 010a0aad39fccceba4a07d30d163158a39c704f3 ]
    
    LLVM appends various suffixes for local functions and variables, suffixes
    observed:
     - foo.llvm.[0-9a-f]+
     - foo.[0-9a-f]+
    
    Therefore, when CONFIG_LTO_CLANG=y, kallsyms_lookup_name() needs to
    truncate the suffix of the symbol name before comparing the local function
    or variable name.
    
    Old implementation code:
    -	if (strcmp(namebuf, name) == 0)
    -		return kallsyms_sym_address(i);
    -	if (cleanup_symbol_name(namebuf) && strcmp(namebuf, name) == 0)
    -		return kallsyms_sym_address(i);
    
    The preceding process is traversed by address from low to high. That is,
    for those with the same name after the suffix is removed, the one with
    the smallest address is returned first. Therefore, when sorting in the
    tool, if the raw names are the same, they should be sorted by address in
    ascending order.
    
    ASCII[.]   = 2e
    ASCII[0-9] = 30,39
    ASCII[A-Z] = 41,5a
    ASCII[_]   = 5f
    ASCII[a-z] = 61,7a
    
    According to the preceding ASCII code values, the following sorting result
    is strictly followed.
     ---------------------------------
    |    main-key     |    sub-key    |
    |---------------------------------|
    |                 |  addr_lowest  |
    | <name>          |      ...      |
    | <name>.<suffix> |      ...      |
    |                 |  addr_highest |
    |---------------------------------|
    | <name>?<others> |               |   //? is [_A-Za-z0-9]
     ---------------------------------
    
    Signed-off-by: Zhen Lei <thunder.leizhen@huawei.com>
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>
    Stable-dep-of: 8cc32a9bbf29 ("kallsyms: strip LTO-only suffixes from promoted global functions")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- scripts/kallsyms.c
+++ scripts/kallsyms.c
@@ -78,6 +78,7 @@ static unsigned int table_size, table_cnt;
 static int all_symbols;
 static int absolute_percpu;
 static int base_relative;
+static int lto_clang;
 
 static int token_profit[0x10000];
 
@@ -89,7 +90,7 @@ static unsigned char best_table_len[256];
 static void usage(void)
 {
 	fprintf(stderr, "Usage: kallsyms [--all-symbols] [--absolute-percpu] "
-			"[--base-relative] in.map > out.S\n");
+			"[--base-relative] [--lto-clang] in.map > out.S\n");
 	exit(1);
 }
 
@@ -411,6 +412,34 @@ static int symbol_absolute(const struct sym_entry *s)
 	return s->percpu_absolute;
 }
 
+static char * s_name(char *buf)
+{
+	/* Skip the symbol type */
+	return buf + 1;
+}
+
+static void cleanup_symbol_name(char *s)
+{
+	char *p;
+
+	if (!lto_clang)
+		return;
+
+	/*
+	 * ASCII[.]   = 2e
+	 * ASCII[0-9] = 30,39
+	 * ASCII[A-Z] = 41,5a
+	 * ASCII[_]   = 5f
+	 * ASCII[a-z] = 61,7a
+	 *
+	 * As above, replacing '.' with '\0' does not affect the main sorting,
+	 * but it helps us with subsorting.
+	 */
+	p = strchr(s, '.');
+	if (p)
+		*p = '\0';
+}
+
 static int compare_names(const void *a, const void *b)
 {
 	int ret;
@@ -421,7 +450,9 @@ static int compare_names(const void *a, const void *b)
 
 	expand_symbol(sa->sym, sa->len, sa_namebuf);
 	expand_symbol(sb->sym, sb->len, sb_namebuf);
-	ret = strcmp(&sa_namebuf[1], &sb_namebuf[1]);
+	cleanup_symbol_name(s_name(sa_namebuf));
+	cleanup_symbol_name(s_name(sb_namebuf));
+	ret = strcmp(s_name(sa_namebuf), s_name(sb_namebuf));
 	if (!ret) {
 		if (sa->addr > sb->addr)
 			return 1;
@@ -855,6 +886,7 @@ int main(int argc, char **argv)
 			{"all-symbols",     no_argument, &all_symbols,     1},
 			{"absolute-percpu", no_argument, &absolute_percpu, 1},
 			{"base-relative",   no_argument, &base_relative,   1},
+			{"lto-clang",       no_argument, &lto_clang,       1},
 			{},
 		};
 
