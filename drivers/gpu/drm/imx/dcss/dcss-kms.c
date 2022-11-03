cbc5541f020ca178aa5cda4615ab36fba9109a9d
Thomas Zimmermann <tzimmermann@suse.de>
2022-11-03 16:14:28 +0100

    drm/imx/dcss: Don't set struct drm_driver.output_poll_changed
    Don't set struct drm_driver.output_poll_changed. It's used to restore
    the fbdev console. But as DCSS uses generic fbdev emulation, the
    console is being restored by the DRM client helpers already. See the
    functions drm_kms_helper_hotplug_event() and
    drm_kms_helper_connector_hotplug_event() in drm_probe_helper.c.
    
    v2:
    	* fix commit description (Christian)
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Javier Martinez Canillas <javierm@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20221103151446.2638-6-tzimmermann@suse.de

--- drivers/gpu/drm/imx/dcss/dcss-kms.c
+++ drivers/gpu/drm/imx/dcss/dcss-kms.c
@@ -21,7 +21,6 @@ DEFINE_DRM_GEM_DMA_FOPS(dcss_cma_fops);
 
 static const struct drm_mode_config_funcs dcss_drm_mode_config_funcs = {
 	.fb_create = drm_gem_fb_create,
-	.output_poll_changed = drm_fb_helper_output_poll_changed,
 	.atomic_check = drm_atomic_helper_check,
 	.atomic_commit = drm_atomic_helper_commit,
 };
