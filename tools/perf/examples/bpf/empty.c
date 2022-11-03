baddab891a21e5870723d182020fec445b0874b3
Ian Rogers <irogers@google.com>
2022-11-02 21:54:35 -0700

    perf trace: empty fix libbpf 1.0+ compatibility
    Avoid use of tools/perf/include/bpf/bpf.h and use the more regular BPF
    headers.  Add raw_syscalls:sys_enter to avoid the evlist being empty.
    
    Committer testing:
    
      # time perf trace -e ~acme/git/perf/tools/perf/examples/bpf/empty.c sleep 5
    
      real	0m5.697s
      user	0m0.217s
      sys	0m0.453s
      #
    
    I.e. it sets up everything successfully (use -v to see the details) and
    filters out all syscalls, then exits when the workload (sleep 5)
    finishes.
    
    Signed-off-by: Ian Rogers <irogers@google.com>
    Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Leo Yan <leo.yan@linaro.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: https://lore.kernel.org/r/20221103045437.163510-6-irogers@google.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/examples/bpf/empty.c
+++ tools/perf/examples/bpf/empty.c
@@ -1,3 +1,12 @@
-#include <bpf/bpf.h>
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
 
-license(GPL);
+struct syscall_enter_args;
+
+SEC("raw_syscalls:sys_enter")
+int sys_enter(struct syscall_enter_args *args)
+{
+	return 0;
+}
+char _license[] SEC("license") = "GPL";
