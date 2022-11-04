80edc49f6a7544fa0fac122e177ffb63af60651e
Vitaly Kuznetsov <vkuznets@redhat.com>
2022-11-04 15:47:08 +0100

    KVM: VMX: Resurrect vmcs_conf sanitization for KVM-on-Hyper-V
    Commit 9bcb90650e31 ("KVM: VMX: Get rid of eVMCS specific VMX controls
    sanitization") dropped 'vmcs_conf' sanitization for KVM-on-Hyper-V because
    there's no known Hyper-V version which would expose a feature
    unsupported in eVMCS in VMX feature MSRs. This works well for all
    currently existing Hyper-V version, however, future Hyper-V versions
    may add features which are supported by KVM and are currently missing
    in eVMCSv1 definition (e.g. APIC virtualization, PML,...). When this
    happens, existing KVMs will get broken. With the inverted 'unsupported
    by eVMCSv1' checks, we can resurrect vmcs_conf sanitization and make
    KVM future proof.
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Message-Id: <20221104144708.435865-5-vkuznets@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx/hyperv.c
+++ arch/x86/kvm/vmx/hyperv.c
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 
+#define pr_fmt(fmt) "kvm/hyper-v: " fmt
+
 #include <linux/errno.h>
 #include <linux/smp.h>
 
@@ -503,6 +505,38 @@ int nested_evmcs_check_controls(struct vmcs12 *vmcs12)
 
 	return 0;
 }
+
+#if IS_ENABLED(CONFIG_HYPERV)
+/*
+ * KVM on Hyper-V always uses the latest known eVMCSv1 revision, the assumption
+ * is: in case a feature has corresponding fields in eVMCS described and it was
+ * exposed in VMX feature MSRs, KVM is free to use it. Warn if KVM meets a
+ * feature which has no corresponding eVMCS field, this likely means that KVM
+ * needs to be updated.
+ */
+#define evmcs_check_vmcs_conf(field, ctrl)					\
+	do {									\
+		typeof(vmcs_conf->field) unsupported;				\
+										\
+		unsupported = vmcs_conf->field & ~EVMCS1_SUPPORTED_ ## ctrl;	\
+		if (unsupported) {						\
+			pr_warn_once(#field " unsupported with eVMCS: 0x%llx\n",\
+				     (u64)unsupported);				\
+			vmcs_conf->field &= EVMCS1_SUPPORTED_ ## ctrl;		\
+		}								\
+	}									\
+	while (0)
+
+__init void evmcs_sanitize_exec_ctrls(struct vmcs_config *vmcs_conf)
+{
+	evmcs_check_vmcs_conf(cpu_based_exec_ctrl, EXEC_CTRL);
+	evmcs_check_vmcs_conf(pin_based_exec_ctrl, PINCTRL);
+	evmcs_check_vmcs_conf(cpu_based_2nd_exec_ctrl, 2NDEXEC);
+	evmcs_check_vmcs_conf(cpu_based_3rd_exec_ctrl, 3RDEXEC);
+	evmcs_check_vmcs_conf(vmentry_ctrl, VMENTRY_CTRL);
+	evmcs_check_vmcs_conf(vmexit_ctrl, VMEXIT_CTRL);
+}
+#endif
 
 int nested_enable_evmcs(struct kvm_vcpu *vcpu,
 			uint16_t *vmcs_version)
