2f5d8cef45c30edcf3972d345f606df563d3a48e
Yishai Hadas <yishaih@nvidia.com>
2022-11-06 19:46:19 +0200

    vfio/mlx5: Fix a typo in mlx5vf_cmd_load_vhca_state()
    Fix a typo in mlx5vf_cmd_load_vhca_state() to use the 'load' memory
    layout.
    
    As in/out sizes are equal for save and load commands there wasn't any
    functional issue.
    
    Fixes: f1d98f346ee3 ("vfio/mlx5: Expose migration commands over mlx5 device")
    Signed-off-by: Yishai Hadas <yishaih@nvidia.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/20221106174630.25909-3-yishaih@nvidia.com
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

--- drivers/vfio/pci/mlx5/cmd.c
+++ drivers/vfio/pci/mlx5/cmd.c
@@ -378,8 +378,8 @@ int mlx5vf_cmd_load_vhca_state(struct mlx5vf_pci_core_device *mvdev,
 			       struct mlx5_vf_migration_file *migf)
 {
 	struct mlx5_core_dev *mdev;
-	u32 out[MLX5_ST_SZ_DW(save_vhca_state_out)] = {};
-	u32 in[MLX5_ST_SZ_DW(save_vhca_state_in)] = {};
+	u32 out[MLX5_ST_SZ_DW(load_vhca_state_out)] = {};
+	u32 in[MLX5_ST_SZ_DW(load_vhca_state_in)] = {};
 	u32 pdn, mkey;
 	int err;
 
