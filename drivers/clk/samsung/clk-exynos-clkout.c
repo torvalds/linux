2bc5febd05abe86c3e3d4b4f18dff4bc4316c1be
Marek Szyprowski <m.szyprowski@samsung.com>
2022-11-08 22:37:18 +0100

    clk: samsung: Revert "clk: samsung: exynos-clkout: Use of_device_get_match_data()"
    of_device_get_match_data() function should not be used on the device
    other than the one matched to the given driver, because it always returns
    the match_data of the matched driver. In case of exynos-clkout driver,
    the code matched the OF IDs on the PARENT device, so replacing it with
    of_device_get_match_data() broke the driver.
    
    This reverts commit 777aaf3d1daf793461269b49c063aca1cee06a44.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Fixes: 777aaf3d1daf ("clk: samsung: exynos-clkout: Use of_device_get_match_data()")
    Reviewed-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Link: https://lore.kernel.org/r/20221108213718.32076-1-m.szyprowski@samsung.com
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>

--- drivers/clk/samsung/clk-exynos-clkout.c
+++ drivers/clk/samsung/clk-exynos-clkout.c
@@ -81,17 +81,19 @@ MODULE_DEVICE_TABLE(of, exynos_clkout_ids);
 static int exynos_clkout_match_parent_dev(struct device *dev, u32 *mux_mask)
 {
 	const struct exynos_clkout_variant *variant;
+	const struct of_device_id *match;
 
 	if (!dev->parent) {
 		dev_err(dev, "not instantiated from MFD\n");
 		return -EINVAL;
 	}
 
-	variant = of_device_get_match_data(dev->parent);
-	if (!variant) {
+	match = of_match_device(exynos_clkout_ids, dev->parent);
+	if (!match) {
 		dev_err(dev, "cannot match parent device\n");
 		return -EINVAL;
 	}
+	variant = match->data;
 
 	*mux_mask = variant->mux_mask;
 
