c7a79f546100300d18585506f8fd0460a874df4a
Ajye Huang <ajye_huang@compal.corp-partner.google.com>
2022-11-08 12:27:16 +0800

    ASoC: Intel: sof rt5682: remove the duplicate codes
    Remove the redundant code to prevent user confuse.
    
    Signed-off-by: Ajye Huang <ajye_huang@compal.corp-partner.google.com>
    Acked-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20221108042716.2930255-3-ajye_huang@compal.corp-partner.google.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/boards/sof_rt5682.c
+++ sound/soc/intel/boards/sof_rt5682.c
@@ -855,10 +855,6 @@ static int sof_audio_probe(struct platform_device *pdev)
 	if (acpi_dev_present("RTL5682", NULL, -1))
 		sof_rt5682_quirk |= SOF_RT5682S_HEADPHONE_CODEC_PRESENT;
 
-	/* Detect the headset codec variant to support machines in DMI quirk */
-	if (acpi_dev_present("RTL5682", NULL, -1))
-		sof_rt5682_quirk |= SOF_RT5682S_HEADPHONE_CODEC_PRESENT;
-
 	if (soc_intel_is_byt() || soc_intel_is_cht()) {
 		is_legacy_cpu = 1;
 		dmic_be_num = 0;
