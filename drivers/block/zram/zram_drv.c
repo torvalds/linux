60e9b39ebec56467c36c3da76eee28083196cdf1
Sergey Senozhatsky <senozhatsky@chromium.org>
2022-11-09 20:50:39 +0900

    zram: add recompress flag to read_block_state()
    Add a new flag to zram block state that shows if the page was recompressed
    (using alternative compression algorithm).
    
    Link: https://lkml.kernel.org/r/20221109115047.2921851-6-senozhatsky@chromium.org
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Alexey Romanov <avromanov@sberdevices.ru>
    Cc: Nhat Pham <nphamcs@gmail.com>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Cc: Suleiman Souhlal <suleiman@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -936,13 +936,14 @@ static ssize_t read_block_state(struct file *file, char __user *buf,
 
 		ts = ktime_to_timespec64(zram->table[index].ac_time);
 		copied = snprintf(kbuf + written, count,
-			"%12zd %12lld.%06lu %c%c%c%c\n",
+			"%12zd %12lld.%06lu %c%c%c%c%c\n",
 			index, (s64)ts.tv_sec,
 			ts.tv_nsec / NSEC_PER_USEC,
 			zram_test_flag(zram, index, ZRAM_SAME) ? 's' : '.',
 			zram_test_flag(zram, index, ZRAM_WB) ? 'w' : '.',
 			zram_test_flag(zram, index, ZRAM_HUGE) ? 'h' : '.',
-			zram_test_flag(zram, index, ZRAM_IDLE) ? 'i' : '.');
+			zram_test_flag(zram, index, ZRAM_IDLE) ? 'i' : '.',
+			zram_get_priority(zram, index) ? 'r' : '.');
 
 		if (count <= copied) {
 			zram_slot_unlock(zram, index);
