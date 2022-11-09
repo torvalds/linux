f24ee92cbe13242758635e654b2422dbf4912e4b
Sergey Senozhatsky <senozhatsky@chromium.org>
2022-11-09 20:50:41 +0900

    zram: use IS_ERR_VALUE() to check for zs_malloc() errors
    Avoid typecasts that are needed for IS_ERR() and use IS_ERR_VALUE()
    instead.
    
    Link: https://lkml.kernel.org/r/20221109115047.2921851-8-senozhatsky@chromium.org
    Suggested-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Alexey Romanov <avromanov@sberdevices.ru>
    Cc: Nhat Pham <nphamcs@gmail.com>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Cc: Suleiman Souhlal <suleiman@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1542,19 +1542,19 @@ compress_again:
 	 * if we have a 'non-null' handle here then we are coming
 	 * from the slow path and handle has already been allocated.
 	 */
-	if (IS_ERR((void *)handle))
+	if (IS_ERR_VALUE(handle))
 		handle = zs_malloc(zram->mem_pool, comp_len,
 				__GFP_KSWAPD_RECLAIM |
 				__GFP_NOWARN |
 				__GFP_HIGHMEM |
 				__GFP_MOVABLE);
-	if (IS_ERR((void *)handle)) {
+	if (IS_ERR_VALUE(handle)) {
 		zcomp_stream_put(zram->comps[ZRAM_PRIMARY_COMP]);
 		atomic64_inc(&zram->stats.writestall);
 		handle = zs_malloc(zram->mem_pool, comp_len,
 				GFP_NOIO | __GFP_HIGHMEM |
 				__GFP_MOVABLE);
-		if (IS_ERR((void *)handle))
+		if (IS_ERR_VALUE(handle))
 			return PTR_ERR((void *)handle);
 
 		if (comp_len != PAGE_SIZE)
