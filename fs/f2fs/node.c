250f35d78bcc0e9291690ab3dc26917d0dc144a3
Jaegeuk Kim <jaegeuk@kernel.org>
2022-11-08 17:59:34 -0800

    FROMGIT: f2fs: allow to read node block after shutdown
    If block address is still alive, we should give a valid node block even after
    shutdown. Otherwise, we can see zero data when reading out a file.
    
    Bug: 257271565
    Cc: stable@vger.kernel.org
    Fixes: 83a3bfdb5a8 ("f2fs: indicate shutdown f2fs to allow unmount successfully")
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit 6953bf65286d git://git.kernel.org/pub/scm/linux/kernel/git/jaegeuk/f2fs.git dev)
    Change-Id: Ifb70f6c73bd67d5112ee9fa1a5e4ad8e10ae8517

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -1336,8 +1336,7 @@ static int read_node_page(struct page *page, int op_flags)
 		return err;
 
 	/* NEW_ADDR can be seen, after cp_error drops some dirty node pages */
-	if (unlikely(ni.blk_addr == NULL_ADDR || ni.blk_addr == NEW_ADDR) ||
-			is_sbi_flag_set(sbi, SBI_IS_SHUTDOWN)) {
+	if (unlikely(ni.blk_addr == NULL_ADDR || ni.blk_addr == NEW_ADDR)) {
 		ClearPageUptodate(page);
 		return -ENOENT;
 	}
