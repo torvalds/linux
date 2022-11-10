474be445555ba8f2e776b4b6458c310bc215f76b
Miguel Ojeda <ojeda@kernel.org>
2022-11-10 17:41:13 +0100

    rust: prelude: split re-exports into groups
    Split the prelude re-exports into groups: first the ones coming
    from the `core` crate, then `alloc`, then our own crates and
    finally the ones from modules from `kernel` itself (i.e. `super`).
    
    We are doing this manually for the moment, but ideally, long-term,
    this could be automated via `rustfmt` with options such as
    `group_imports` and `imports_granularity` (both currently unstable).
    
    Reviewed-by: Boqun Feng <boqun.feng@gmail.com>
    Reviewed-by: Wei Liu <wei.liu@kernel.org>
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- rust/kernel/prelude.rs
+++ rust/kernel/prelude.rs
@@ -11,10 +11,14 @@
 //! use kernel::prelude::*;
 //! ```
 
-pub use super::{
-    error::{Error, Result},
-    pr_emerg, pr_info, ThisModule,
-};
-pub use alloc::{boxed::Box, vec::Vec};
 pub use core::pin::Pin;
+
+pub use alloc::{boxed::Box, vec::Vec};
+
 pub use macros::module;
+
+pub use super::{pr_emerg, pr_info};
+
+pub use super::error::{Error, Result};
+
+pub use super::ThisModule;
