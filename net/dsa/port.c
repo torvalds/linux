97009f07f2176297c2fab340f51e4f8423dd3cb8
Vladimir Oltean <vladimir.oltean@nxp.com>
2022-11-11 23:10:20 +0200

    net: dsa: make dsa_master_ioctl() see through port_hwtstamp_get() shims
    [ Upstream commit ed1fe1bebe18884b11e5536b5ac42e3a48960835 ]
    
    There are multi-generational drivers like mv88e6xxx which have code like
    this:
    
    int mv88e6xxx_port_hwtstamp_get(struct dsa_switch *ds, int port,
    				struct ifreq *ifr)
    {
    	if (!chip->info->ptp_support)
    		return -EOPNOTSUPP;
    
    	...
    }
    
    DSA wants to deny PTP timestamping on the master if the switch supports
    timestamping too. However it currently relies on the presence of the
    port_hwtstamp_get() callback to determine PTP capability, and this
    clearly does not work in that case (method is present but returns
    -EOPNOTSUPP).
    
    We should not deny PTP on the DSA master for those switches which truly
    do not support hardware timestamping.
    
    Create a dsa_port_supports_hwtstamp() method which actually probes for
    support by calling port_hwtstamp_get() and seeing whether that returned
    -EOPNOTSUPP or not.
    
    Fixes: f685e609a301 ("net: dsa: Deny PTP on master if switch supports it")
    Link: https://patchwork.kernel.org/project/netdevbpf/patch/20221110124345.3901389-1-festevam@gmail.com/
    Reported-by: Fabio Estevam <festevam@gmail.com>
    Reported-by: Steffen Bätz <steffen@innosonix.de>
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Tested-by: Fabio Estevam <festevam@denx.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/dsa/port.c
+++ net/dsa/port.c
@@ -75,6 +75,22 @@ static bool dsa_port_can_configure_learning(struct dsa_port *dp)
 	return !err;
 }
 
+bool dsa_port_supports_hwtstamp(struct dsa_port *dp, struct ifreq *ifr)
+{
+	struct dsa_switch *ds = dp->ds;
+	int err;
+
+	if (!ds->ops->port_hwtstamp_get || !ds->ops->port_hwtstamp_set)
+		return false;
+
+	/* "See through" shim implementations of the "get" method.
+	 * This will clobber the ifreq structure, but we will either return an
+	 * error, or the master will overwrite it with proper values.
+	 */
+	err = ds->ops->port_hwtstamp_get(ds, dp->index, ifr);
+	return err != -EOPNOTSUPP;
+}
+
 int dsa_port_set_state(struct dsa_port *dp, u8 state, bool do_fast_age)
 {
 	struct dsa_switch *ds = dp->ds;
