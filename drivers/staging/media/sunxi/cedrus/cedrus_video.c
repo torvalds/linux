b15a92f054070f47aaa2658bb48cc1780b2e1481
Ian Cowan <ian@linux.cowan.aero>
2022-11-13 01:47:03 +0000

    media: staging: media: sunxi: cedrus: make vb2_ops struct definition const
    This cleans up a checkstyle warning where the vb2_ops struct definitions
    should be const.
    
    Signed-off-by: Ian Cowan <ian@linux.cowan.aero>
    Reviewed-by: Samuel Holland <samuel@sholland.org>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>

--- drivers/staging/media/sunxi/cedrus/cedrus_video.c
+++ drivers/staging/media/sunxi/cedrus/cedrus_video.c
@@ -562,7 +562,7 @@ static void cedrus_buf_request_complete(struct vb2_buffer *vb)
 	v4l2_ctrl_request_complete(vb->req_obj.req, &ctx->hdl);
 }
 
-static struct vb2_ops cedrus_qops = {
+static const struct vb2_ops cedrus_qops = {
 	.queue_setup		= cedrus_queue_setup,
 	.buf_prepare		= cedrus_buf_prepare,
 	.buf_queue		= cedrus_buf_queue,
