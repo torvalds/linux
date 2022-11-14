93e3f45a26310e3f3f8558be40df411e23ab742c
Sathvika Vasireddy <sv@linux.ibm.com>
2022-11-14 23:27:39 +0530

    powerpc: Fix __WARN_FLAGS() for use with Objtool
    Commit 1e688dd2a3d675 ("powerpc/bug: Provide better flexibility to
    WARN_ON/__WARN_FLAGS() with asm goto") updated __WARN_FLAGS() to use asm
    goto, and added a call to 'unreachable()' after the asm goto for optimal
    code generation. With CONFIG_OBJTOOL enabled, 'annotate_unreachable()'
    statement in 'unreachable()' tries to note down the location of the
    subsequent instruction in a separate elf section to aid code flow
    analysis. However, on powerpc, this results in gcc emitting a call to a
    symbol of size 0. This results in objtool complaining of "unannotated
    intra-function call" since the target symbol is not a valid function
    call destination.
    
    Objtool wants this annotation for code flow analysis, which we are not
    yet enabling on powerpc. As such, expand the call to 'unreachable()' in
    __WARN_FLAGS() without annotate_unreachable():
            barrier_before_unreachable();
            __builtin_unreachable();
    
    This still results in optimal code generation for __WARN_FLAGS(), while
    getting rid of the objtool warning.
    
    We still need barrier_before_unreachable() to work around gcc bugs 82365
    and 106751:
    - https://gcc.gnu.org/bugzilla/show_bug.cgi?id=82365
    - https://gcc.gnu.org/bugzilla/show_bug.cgi?id=106751
    
    Tested-by: Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
    Reviewed-by: Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
    Reviewed-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    Acked-by: Josh Poimboeuf <jpoimboe@kernel.org>
    Signed-off-by: Sathvika Vasireddy <sv@linux.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20221114175754.1131267-2-sv@linux.ibm.com

--- arch/powerpc/include/asm/bug.h
+++ arch/powerpc/include/asm/bug.h
@@ -99,7 +99,8 @@
 	__label__ __label_warn_on;				\
 								\
 	WARN_ENTRY("twi 31, 0, 0", BUGFLAG_WARNING | (flags), __label_warn_on); \
-	unreachable();						\
+	barrier_before_unreachable();				\
+	__builtin_unreachable();				\
 								\
 __label_warn_on:						\
 	break;							\
