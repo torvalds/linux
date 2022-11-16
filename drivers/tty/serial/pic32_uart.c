452b9b24754044eced1508f9090611f3d9aa4ca5
John Ogness <john.ogness@linutronix.de>
2022-11-16 17:27:34 +0106

    serial_core: replace uart_console_enabled() with uart_console_registered()
    All users of uart_console_enabled() really want to know if a console
    is registered. It is not reliable to check for CON_ENABLED in order
    to identify if a console is registered. Use console_is_registered()
    instead.
    
    A _locked() variant is provided because uart_set_options() is always
    called with the console_list_lock held and must check if a console
    is registered in order to synchronize with kgdboc.
    
    Signed-off-by: John Ogness <john.ogness@linutronix.de>
    Reviewed-by: Petr Mladek <pmladek@suse.com>
    Signed-off-by: Petr Mladek <pmladek@suse.com>
    Link: https://lore.kernel.org/r/20221116162152.193147-23-john.ogness@linutronix.de

--- drivers/tty/serial/pic32_uart.c
+++ drivers/tty/serial/pic32_uart.c
@@ -919,7 +919,7 @@ static int pic32_uart_probe(struct platform_device *pdev)
 	}
 
 #ifdef CONFIG_SERIAL_PIC32_CONSOLE
-	if (uart_console_enabled(port)) {
+	if (uart_console_registered(port)) {
 		/* The peripheral clock has been enabled by console_setup,
 		 * so disable it till the port is used.
 		 */
