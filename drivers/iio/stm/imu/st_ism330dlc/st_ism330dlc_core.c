a7cb978337076a98e15591b127905e514b180102
Mario Tesi <mario.tesi@st.com>
2022-11-17 17:10:47 +0100

    drivers:iio:stm:imu:st_ism330dlc: fix full scale to int plus nano
    compatibly with the write_raw_get_fmt and read_raw functions,
    the FS should be of the INT plus NANO type.
    
    Signed-off-by: Mario Tesi <mario.tesi@st.com>
    Change-Id: I69b918d884bac4e508fb134b435c61fe0873a6ec
    Reviewed-on: https://gerrit.st.com/c/linuxandroidopen/stm-ldd-iio/+/276302
    Tested-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Denis CIOCCA <denis.ciocca@st.com>

--- drivers/iio/stm/imu/st_ism330dlc/st_ism330dlc_core.c
+++ drivers/iio/stm/imu/st_ism330dlc/st_ism330dlc_core.c
@@ -1674,7 +1674,7 @@ static int st_ism330dlc_read_raw(struct iio_dev *indio_dev,
 	case IIO_CHAN_INFO_SCALE:
 		*val = 0;
 		*val2 = sdata->c_gain[0];
-		return IIO_VAL_INT_PLUS_MICRO;
+		return IIO_VAL_INT_PLUS_NANO;
 	default:
 		return -EINVAL;
 	}
