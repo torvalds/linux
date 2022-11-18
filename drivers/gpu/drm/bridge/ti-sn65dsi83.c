0f6548807fa77e87bbc37964c6b1ed9ba6e1155d
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2022-11-18 23:36:09 +0100

    drm/bridge: ti-sn65dsi83: Convert to i2c's .probe_new()
    .probe_new() doesn't get the i2c_device_id * parameter, so determine
    that explicitly in the probe function.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Link: https://lore.kernel.org/r/20221118224540.619276-36-uwe@kleine-koenig.org
    Signed-off-by: Robert Foss <robert.foss@linaro.org>

--- drivers/gpu/drm/bridge/ti-sn65dsi83.c
+++ drivers/gpu/drm/bridge/ti-sn65dsi83.c
@@ -653,9 +653,9 @@ static int sn65dsi83_host_attach(struct sn65dsi83 *ctx)
 	return 0;
 }
 
-static int sn65dsi83_probe(struct i2c_client *client,
-			   const struct i2c_device_id *id)
+static int sn65dsi83_probe(struct i2c_client *client)
 {
+	const struct i2c_device_id *id = i2c_client_get_device_id(client);
 	struct device *dev = &client->dev;
 	enum sn65dsi83_model model;
 	struct sn65dsi83 *ctx;
@@ -730,7 +730,7 @@ static const struct of_device_id sn65dsi83_match_table[] = {
 MODULE_DEVICE_TABLE(of, sn65dsi83_match_table);
 
 static struct i2c_driver sn65dsi83_driver = {
-	.probe = sn65dsi83_probe,
+	.probe_new = sn65dsi83_probe,
 	.remove = sn65dsi83_remove,
 	.id_table = sn65dsi83_id,
 	.driver = {
