b76f64caa42e9bb78e177ae94e1062d876a401b1
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2022-11-17 22:16:16 -0800

    Input: max8997 - convert to modern way to get a reference to a PWM
    pwm_request() isn't recommended to be used any more because it relies on
    global IDs for the PWM which comes with different difficulties.
    
    The new way to do things is to find the right PWM using a reference from
    the platform device. (This can be created either using a device-tree
    or a platform lookup table, see e.g. commit 5a4412d4a82f ("ARM: pxa:
    tavorevb: Use PWM lookup table") how to do this.)
    
    There are no in-tree users, so there are no other code locations that need
    adaption.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Acked-by: Lee Jones <lee@kernel.org>
    Link: https://lore.kernel.org/r/20221117073543.3790449-1-u.kleine-koenig@pengutronix.de
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/misc/max8997_haptic.c
+++ drivers/input/misc/max8997_haptic.c
@@ -278,8 +278,7 @@ static int max8997_haptic_probe(struct platform_device *pdev)
 		break;
 
 	case MAX8997_EXTERNAL_MODE:
-		chip->pwm = pwm_request(haptic_pdata->pwm_channel_id,
-					"max8997-haptic");
+		chip->pwm = pwm_get(&pdev->dev, NULL);
 		if (IS_ERR(chip->pwm)) {
 			error = PTR_ERR(chip->pwm);
 			dev_err(&pdev->dev,
@@ -344,7 +343,7 @@ err_put_regulator:
 	regulator_put(chip->regulator);
 err_free_pwm:
 	if (chip->mode == MAX8997_EXTERNAL_MODE)
-		pwm_free(chip->pwm);
+		pwm_put(chip->pwm);
 err_free_mem:
 	input_free_device(input_dev);
 	kfree(chip);
@@ -360,7 +359,7 @@ static int max8997_haptic_remove(struct platform_device *pdev)
 	regulator_put(chip->regulator);
 
 	if (chip->mode == MAX8997_EXTERNAL_MODE)
-		pwm_free(chip->pwm);
+		pwm_put(chip->pwm);
 
 	kfree(chip);
 
