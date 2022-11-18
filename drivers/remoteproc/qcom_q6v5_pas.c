4458ed2d9570d949a58665c6a94ebaadc44813db
Luca Weiss <luca.weiss@fairphone.com>
2022-11-18 10:08:15 +0100

    remoteproc: qcom_q6v5_pas: disable wakeup on probe fail or remove
    [ Upstream commit 9a70551996e699fda262e8d54bbd41739d7aad6d ]
    
    Leaving wakeup enabled during probe fail (-EPROBE_DEFER) or remove makes
    the subsequent probe fail.
    
    [    3.749454] remoteproc remoteproc0: releasing 3000000.remoteproc
    [    3.752949] qcom_q6v5_pas: probe of 3000000.remoteproc failed with error -17
    [    3.878935] remoteproc remoteproc0: releasing 4080000.remoteproc
    [    3.887602] qcom_q6v5_pas: probe of 4080000.remoteproc failed with error -17
    [    4.319552] remoteproc remoteproc0: releasing 8300000.remoteproc
    [    4.332716] qcom_q6v5_pas: probe of 8300000.remoteproc failed with error -17
    
    Fix this by disabling wakeup in both cases so the driver can properly
    probe on the next try.
    
    Fixes: a781e5aa5911 ("remoteproc: core: Prevent system suspend during remoteproc recovery")
    Fixes: dc86c129b4fb ("remoteproc: qcom: pas: Mark devices as wakeup capable")
    Reviewed-by: Mukesh Ojha <quic_mojha@quicinc.com>
    Signed-off-by: Luca Weiss <luca.weiss@fairphone.com>
    Reviewed-by: Caleb Connolly <caleb.connolly@linaro.org>
    Reviewed-by: Sibi Sankar <quic_sibis@quicinc.com>
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    Link: https://lore.kernel.org/r/20221118090816.100012-1-luca.weiss@fairphone.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -556,6 +556,7 @@ static int adsp_probe(struct platform_device *pdev)
 detach_proxy_pds:
 	adsp_pds_detach(adsp, adsp->proxy_pds, adsp->proxy_pd_count);
 free_rproc:
+	device_init_wakeup(adsp->dev, false);
 	rproc_free(rproc);
 
 	return ret;
@@ -572,6 +573,7 @@ static int adsp_remove(struct platform_device *pdev)
 	qcom_remove_sysmon_subdev(adsp->sysmon);
 	qcom_remove_smd_subdev(adsp->rproc, &adsp->smd_subdev);
 	qcom_remove_ssr_subdev(adsp->rproc, &adsp->ssr_subdev);
+	device_init_wakeup(adsp->dev, false);
 	rproc_free(adsp->rproc);
 
 	return 0;
