a3816a7d7c097c1da46aad5f5d1e229b607dce04
Patrick Delaunay <patrick.delaunay@foss.st.com>
2022-11-18 06:39:22 +0000

    nvmem: stm32: add nvmem type attribute
    Inform NVMEM framework of type attribute for stm32-romem as NVMEM_TYPE_OTP
    so userspace is able to know how the data is stored in BSEC.
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Link: https://lore.kernel.org/r/20221118063932.6418-4-srinivas.kandagatla@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/nvmem/stm32-romem.c
+++ drivers/nvmem/stm32-romem.c
@@ -160,6 +160,7 @@ static int stm32_romem_probe(struct platform_device *pdev)
 	priv->cfg.dev = dev;
 	priv->cfg.priv = priv;
 	priv->cfg.owner = THIS_MODULE;
+	priv->cfg.type = NVMEM_TYPE_OTP;
 
 	priv->lower = 0;
 
