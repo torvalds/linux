3cf4417385d0ac8f02f22888e12a6d21d97d89fc
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2022-11-18 23:44:42 +0100

    regulator: max8649: Convert to i2c's .probe_new()
    The probe function doesn't make use of the i2c_device_id * parameter so it
    can be trivially converted.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Acked-for-MFD-by: Lee Jones <lee@kernel.org>
    Acked-for-Backlight-by: Lee Jones <lee@kernel.org>
    Link: https://lore.kernel.org/r/20221118224540.619276-549-uwe@kleine-koenig.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/max8649.c
+++ drivers/regulator/max8649.c
@@ -145,8 +145,7 @@ static const struct regmap_config max8649_regmap_config = {
 	.val_bits = 8,
 };
 
-static int max8649_regulator_probe(struct i2c_client *client,
-					     const struct i2c_device_id *id)
+static int max8649_regulator_probe(struct i2c_client *client)
 {
 	struct max8649_platform_data *pdata = dev_get_platdata(&client->dev);
 	struct max8649_regulator_info *info = NULL;
@@ -247,7 +246,7 @@ static const struct i2c_device_id max8649_id[] = {
 MODULE_DEVICE_TABLE(i2c, max8649_id);
 
 static struct i2c_driver max8649_driver = {
-	.probe		= max8649_regulator_probe,
+	.probe_new	= max8649_regulator_probe,
 	.driver		= {
 		.name	= "max8649",
 	},
