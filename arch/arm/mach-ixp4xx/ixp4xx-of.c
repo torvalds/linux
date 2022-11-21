c93d8b457461d235ccff6c6864ead6a55ef1c18b
Linus Walleij <linus.walleij@linaro.org>
2022-11-21 23:10:15 +0100

    ARM: ixp4xx: Remove unused debug iomap
    If we don't provide the .map_io() callback, the LL debug
    defines will kick in anyway, so delete the UART debug map
    as well.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://lore.kernel.org/r/20221121221015.1454948-1-linus.walleij@linaro.org
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-ixp4xx/ixp4xx-of.c
+++ arch/arm/mach-ixp4xx/ixp4xx-of.c
@@ -2,31 +2,7 @@
 /*
  * IXP4xx Device Tree boot support
  */
-#include <linux/kernel.h>
-#include <linux/init.h>
-#include <linux/io.h>
-
 #include <asm/mach/arch.h>
-#include <asm/mach/map.h>
-
-#ifdef CONFIG_DEBUG_UART_8250
-static struct map_desc ixp4xx_of_io_desc[] __initdata = {
-	/* This is needed for LL-debug/earlyprintk/debug-macro.S */
-	{
-		.virtual = CONFIG_DEBUG_UART_VIRT,
-		.pfn = __phys_to_pfn(CONFIG_DEBUG_UART_PHYS),
-		.length = SZ_4K,
-		.type = MT_DEVICE,
-	},
-};
-
-static void __init ixp4xx_of_map_io(void)
-{
-	iotable_init(ixp4xx_of_io_desc, ARRAY_SIZE(ixp4xx_of_io_desc));
-}
-#else
-#define ixp4xx_of_map_io NULL
-#endif
 
 /*
  * We handle 4 different SoC families. These compatible strings are enough
@@ -42,6 +18,5 @@ static const char *ixp4xx_of_board_compat[] = {
 };
 
 DT_MACHINE_START(IXP4XX_DT, "IXP4xx (Device Tree)")
-	.map_io		= ixp4xx_of_map_io,
 	.dt_compat	= ixp4xx_of_board_compat,
 MACHINE_END
