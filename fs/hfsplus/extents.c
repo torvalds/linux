74b7046d329770ea9dd105ca321ad6abb91bdbbb
Alexander Potapenko <glider@google.com>
2022-11-21 12:21:34 +0100

    fs: hfsplus: initialize fsdata in hfsplus_file_truncate()
    When aops->write_begin() does not initialize fsdata, KMSAN may report an
    error passing the latter to aops->write_end().
    
    Fix this by unconditionally initializing fsdata.
    
    Link: https://lkml.kernel.org/r/20221121112134.407362-5-glider@google.com
    Signed-off-by: Alexander Potapenko <glider@google.com>
    Suggested-by: Eric Biggers <ebiggers@kernel.org>
    Cc: Andreas Dilger <adilger.kernel@dilger.ca>
    Cc: Chao Yu <chao@kernel.org>
    Cc: Jaegeuk Kim <jaegeuk@kernel.org>
    Cc: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/hfsplus/extents.c
+++ fs/hfsplus/extents.c
@@ -554,7 +554,7 @@ void hfsplus_file_truncate(struct inode *inode)
 	if (inode->i_size > hip->phys_size) {
 		struct address_space *mapping = inode->i_mapping;
 		struct page *page;
-		void *fsdata;
+		void *fsdata = NULL;
 		loff_t size = inode->i_size;
 
 		res = hfsplus_write_begin(NULL, mapping, size, 0,
