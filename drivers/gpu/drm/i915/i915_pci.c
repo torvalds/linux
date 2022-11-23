048ed577a327143f5bdb30299bffdafc0a6a34bd
Ville Syrjälä <ville.syrjala@linux.intel.com>
2022-11-23 17:26:36 +0200

    Revert "drm/i915: Disable DSB usage for now"
    This reverts commit 99510e1afb4863a225207146bd988064c5fd0629.
    
    DSB is now getting disabled locally in the color management
    code so we don't need to apply this big hammer via the device
    info (not that we have other DSB users at the moment).
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20221123152638.20622-12-ville.syrjala@linux.intel.com
    Reviewed-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>
    Reviewed-by: Uma Shankar <uma.shankar@intel.com>

--- drivers/gpu/drm/i915/i915_pci.c
+++ drivers/gpu/drm/i915/i915_pci.c
@@ -889,7 +889,7 @@ static const struct intel_device_info jsl_info = {
 	TGL_CURSOR_OFFSETS, \
 	.has_global_mocs = 1, \
 	.has_pxp = 1, \
-	.display.has_dsb = 0 /* FIXME: LUT load is broken with DSB */
+	.display.has_dsb = 1
 
 static const struct intel_device_info tgl_info = {
 	GEN12_FEATURES,
