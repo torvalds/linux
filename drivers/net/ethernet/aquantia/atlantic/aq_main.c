6922948c2ec1cc45b11acb8ec2eeb3a8353d572b
Izabela Bakollari <ibakolla@redhat.com>
2022-11-23 11:10:08 +0100

    aquantia: Do not purge addresses when setting the number of rings
    [ Upstream commit 2a83891130512dafb321418a8e7c9c09268d8c59 ]
    
    IPV6 addresses are purged when setting the number of rx/tx
    rings using ethtool -G. The function aq_set_ringparam
    calls dev_close, which removes the addresses. As a solution,
    call an internal function (aq_ndev_close).
    
    Fixes: c1af5427954b ("net: aquantia: Ethtool based ring size configuration")
    Signed-off-by: Izabela Bakollari <ibakolla@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/aquantia/atlantic/aq_main.c
+++ drivers/net/ethernet/aquantia/atlantic/aq_main.c
@@ -53,7 +53,7 @@ struct net_device *aq_ndev_alloc(void)
 	return ndev;
 }
 
-static int aq_ndev_open(struct net_device *ndev)
+int aq_ndev_open(struct net_device *ndev)
 {
 	struct aq_nic_s *aq_nic = netdev_priv(ndev);
 	int err = 0;
@@ -83,7 +83,7 @@ err_exit:
 	return err;
 }
 
-static int aq_ndev_close(struct net_device *ndev)
+int aq_ndev_close(struct net_device *ndev)
 {
 	struct aq_nic_s *aq_nic = netdev_priv(ndev);
 	int err = 0;
