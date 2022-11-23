d503d01e5016370be8473fc23d800c7ff37ab7f6
Mark Brown <broonie@kernel.org>
2022-11-23 18:02:09 +0000

    arm64/asm: Remove unused assembler DAIF save/restore macros
    There are no longer any users of the assembler macros for saving and
    restoring DAIF so remove them to prevent further users being added, there
    are C equivalents available.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20221123180209.634650-3-broonie@kernel.org
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/assembler.h
+++ arch/arm64/include/asm/assembler.h
@@ -34,21 +34,12 @@
 	wx\n	.req	w\n
 	.endr
 
-	.macro save_and_disable_daif, flags
-	mrs	\flags, daif
-	msr	daifset, #0xf
-	.endm
-
 	.macro disable_daif
 	msr	daifset, #0xf
 	.endm
 
 	.macro enable_daif
 	msr	daifclr, #0xf
-	.endm
-
-	.macro	restore_daif, flags:req
-	msr	daif, \flags
 	.endm
 
 /*
