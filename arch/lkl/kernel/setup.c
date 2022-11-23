1887555f1be375ddf0ad60ef95f313acdc696ccc
Octavian Purdila <tavip@google.com>
2022-11-23 18:33:15 +0000

    lkl: add BUILTIN_CMDLINE config option
    Add a config option for built-in command line parameters. The command
    line will be updated with lkl_start_kernel parameters and virtio
    devices.
    
    Signed-off-by: Octavian Purdila <tavip@google.com>

--- arch/lkl/kernel/setup.c
+++ arch/lkl/kernel/setup.c
@@ -48,18 +48,43 @@ static void __init lkl_run_kernel(void *arg)
 	start_kernel();
 }
 
-int __init lkl_start_kernel(const char *fmt, ...)
+static char *cmd_line_ptr __initdata = boot_command_line;
+static int cmd_line_len __initdata = COMMAND_LINE_SIZE;
+
+static __init void cmd_line_append_va(const char *fmt, va_list ap)
 {
-	va_list ap;
 	int ret;
 
+	ret = vsnprintf(cmd_line_ptr, cmd_line_len, fmt, ap);
+
+	if (ret > 0) {
+		cmd_line_ptr += ret;
+		cmd_line_len -= ret;
+	}
+}
+
+static inline __init void cmd_line_append(const char *fmt, ...)
+{
+	va_list ap;
+
 	va_start(ap, fmt);
-	ret = vsnprintf(boot_command_line, COMMAND_LINE_SIZE, fmt, ap);
+	cmd_line_append_va(fmt, ap);
+	va_end(ap);
+}
+
+int __init lkl_start_kernel(const char *fmt, ...)
+{
+	int ret;
+	va_list ap;
+
+	cmd_line_append("%s ", CONFIG_BUILTIN_CMDLINE);
+
+	va_start(ap, fmt);
+	cmd_line_append_va(fmt, ap);
 	va_end(ap);
 
 	if (lkl_ops->virtio_devices)
-		strscpy(boot_command_line + ret, lkl_ops->virtio_devices,
-			COMMAND_LINE_SIZE - ret);
+		cmd_line_append("%s", lkl_ops->virtio_devices);
 
 	memcpy(cmd_line, boot_command_line, COMMAND_LINE_SIZE);
 
