f4b09c295e54ef310085253f986f62999626865f
Guchun Chen <guchun.chen@amd.com>
2022-11-23 09:39:40 +0800

    drm/amdgpu: add printing to indicate rpm completeness
    Add an explicit printing to tell when finishing rpm execution
    in amdgpu.
    
    Signed-off-by: Guchun Chen <guchun.chen@amd.com>
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>
    Reviewed-by: Evan Quan <evan.quan@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -2569,6 +2569,8 @@ static int amdgpu_pmops_runtime_suspend(struct device *dev)
 		amdgpu_device_baco_enter(drm_dev);
 	}
 
+	dev_info(&pdev->dev, "asic/device is runtime suspended\n");
+
 	return 0;
 }
 
