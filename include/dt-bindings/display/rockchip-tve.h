4209529be51add566616b90e00f3fcacd447f4ba
Damon Ding <damon.ding@rock-chips.com>
2022-11-23 15:08:39 +0800

    dt-bindings: display: add rockchip tve dclk upsample mode macro
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>
    Change-Id: Ie887ab493f2a9c07d1552e3f400ba9ed55217029

---
+++ include/dt-bindings/display/rockchip-tve.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Copyright (c) 2022 Rockchip Electronics Co., Ltd.
+ */
+
+
+#ifndef _DT_BINDINGS_ROCKCHIP_TVE_H
+#define _DT_BINDINGS_ROCKCHIP_TVE_H
+
+/* tve dclk upsample mode */
+#define	DCLK_UPSAMPLEx1		0
+#define	DCLK_UPSAMPLEx2		1
+#define	DCLK_UPSAMPLEx4		2
+
+#endif
