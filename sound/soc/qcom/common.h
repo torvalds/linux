5827a5656d5a6bea313d58ab56b6d1923331e4f5
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2022-11-24 14:03:51 +0000

    ASoC: qcom: cleanup and fix dependency of QCOM_COMMON
    [ Upstream commit 8d89cf6ff229ff31cd4f73f5b3928564b81fc41e ]
    
    SND_SOC_QCOM_COMMON depends on SOUNDWIRE for some symbols but this
    is not explicitly specified using Kconfig depends. On the other hand
    SND_SOC_QCOM_COMMON is also directly selected by the sound card
    Kconfigs, this could result in various combinations and some symbols
    ending up in modules and soundcard that uses those symbols as in-build
    driver.
    
    Fix these issues by explicitly specifying the dependencies of
    SND_SOC_QCOM_COMMON and also use imply a to select SND_SOC_QCOM_COMMON
    so that the symbol is selected based on its dependencies.
    
    Also remove dummy stubs in common.c around CONFIG_SOUNDWIRE
    
    Fixes: 3bd975f3ae0a ("ASoC: qcom: sm8250: move some code to common")
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Link: https://lore.kernel.org/r/20221124140351.407506-1-srinivas.kandagatla@linaro.org
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- sound/soc/qcom/common.h
+++ sound/soc/qcom/common.h
@@ -11,7 +11,6 @@ int qcom_snd_parse_of(struct snd_soc_card *card);
 int qcom_snd_wcd_jack_setup(struct snd_soc_pcm_runtime *rtd,
 			    struct snd_soc_jack *jack, bool *jack_setup);
 
-#if IS_ENABLED(CONFIG_SOUNDWIRE)
 int qcom_snd_sdw_prepare(struct snd_pcm_substream *substream,
 			 struct sdw_stream_runtime *runtime,
 			 bool *stream_prepared);
@@ -21,26 +20,4 @@ int qcom_snd_sdw_hw_params(struct snd_pcm_substream *substream,
 int qcom_snd_sdw_hw_free(struct snd_pcm_substream *substream,
 			 struct sdw_stream_runtime *sruntime,
 			 bool *stream_prepared);
-#else
-static inline int qcom_snd_sdw_prepare(struct snd_pcm_substream *substream,
-				       struct sdw_stream_runtime *runtime,
-				       bool *stream_prepared)
-{
-	return -ENOTSUPP;
-}
-
-static inline int qcom_snd_sdw_hw_params(struct snd_pcm_substream *substream,
-					 struct snd_pcm_hw_params *params,
-					 struct sdw_stream_runtime **psruntime)
-{
-	return -ENOTSUPP;
-}
-
-static inline int qcom_snd_sdw_hw_free(struct snd_pcm_substream *substream,
-				       struct sdw_stream_runtime *sruntime,
-				       bool *stream_prepared)
-{
-	return -ENOTSUPP;
-}
-#endif
 #endif
