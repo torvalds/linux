ebca4396ee18521e9e5d435a15e5d0ab2eb6b009
Thomas Gleixner <tglx@linutronix.de>
2022-11-25 00:25:49 +0100

    genirq/msi: Provide data structs for per device domains
    Provide struct msi_domain_template which contains a bundle of struct
    irq_chip, struct msi_domain_ops and struct msi_domain_info and a name
    field.
    
    This template is used by MSI device domain implementations to provide the
    domain specific functionality, feature bits etc.
    
    When a MSI domain is created the template is duplicated in the core code
    so that it can be modified per instance. That means templates can be
    marked const at the MSI device domain code.
    
    The template is a bundle to avoid several allocations and duplications
    of the involved structures.
    
    The name field is used to construct the final domain and chip name via:
    
        $PREFIX$NAME-$DEVNAME
    
    where prefix is the optional prefix of the MSI parent domain, $NAME is the
    provided name in template::chip and the device name so that the domain
    is properly identified. On x86 this results for PCI/MSI in:
    
       PCI-MSI-0000:3d:00.1 or IR-PCI-MSIX-0000:3d:00.1
    
    depending on the domain type and the availability of remapping.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Acked-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20221124232325.442499757@linutronix.de

--- include/linux/msi.h
+++ include/linux/msi.h
@@ -24,6 +24,7 @@
 #include <linux/xarray.h>
 #include <linux/mutex.h>
 #include <linux/list.h>
+#include <linux/irq.h>
 #include <linux/bits.h>
 
 #include <asm/msi.h>
@@ -74,7 +75,6 @@ struct msi_msg {
 
 extern int pci_msi_ignore_mask;
 /* Helper functions */
-struct irq_data;
 struct msi_desc;
 struct pci_dev;
 struct platform_msi_priv_data;
@@ -440,6 +440,20 @@ struct msi_domain_info {
 	void				*handler_data;
 	const char			*handler_name;
 	void				*data;
+};
+
+/**
+ * struct msi_domain_template - Template for MSI device domains
+ * @name:	Storage for the resulting name. Filled in by the core.
+ * @chip:	Interrupt chip for this domain
+ * @ops:	MSI domain ops
+ * @info:	MSI domain info data
+ */
+struct msi_domain_template {
+	char			name[48];
+	struct irq_chip		chip;
+	struct msi_domain_ops	ops;
+	struct msi_domain_info	info;
 };
 
 /*
