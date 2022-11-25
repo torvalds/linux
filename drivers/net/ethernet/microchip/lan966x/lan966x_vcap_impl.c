39bedc169cff3f9320ba58339e82eec85cee140d
Horatiu Vultur <horatiu.vultur@microchip.com>
2022-11-25 10:50:05 +0100

    net: lan966x: Add is2 vcap model to vcap API.
    This provides the lan966x is2 model and adds it to the vcap control
    instance that will be provided to the vcap API.
    
    Signed-off-by: Horatiu Vultur <horatiu.vultur@microchip.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- drivers/net/ethernet/microchip/lan966x/lan966x_vcap_impl.c
+++ drivers/net/ethernet/microchip/lan966x/lan966x_vcap_impl.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0+
 
 #include "lan966x_main.h"
+#include "lan966x_vcap_ag_api.h"
 #include "vcap_api.h"
 
 int lan966x_vcap_init(struct lan966x *lan966x)
@@ -10,6 +11,9 @@ int lan966x_vcap_init(struct lan966x *lan966x)
 	ctrl = kzalloc(sizeof(*ctrl), GFP_KERNEL);
 	if (!ctrl)
 		return -ENOMEM;
+
+	ctrl->vcaps = lan966x_vcaps;
+	ctrl->stats = &lan966x_vcap_stats;
 
 	lan966x->vcap_ctrl = ctrl;
 
