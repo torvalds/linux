3741898b169476c17ece7962e5acf415c7041265
Damien Le Moal <damien.lemoal@opensource.wdc.com>
2022-11-25 11:06:20 +0900

    zonefs: Simplify IO error handling
    [ Upstream commit 46a9c526eef7fb68a00321e2a9591ce5276ae92b ]
    
    Simplify zonefs_check_zone_condition() by moving the code that changes
    an inode access rights to the new function zonefs_inode_update_mode().
    Furthermore, since on mount an inode wpoffset is always zero when
    zonefs_check_zone_condition() is called during an inode initialization,
    the "mount" boolean argument is not necessary for the readonly zone
    case. This argument is thus removed.
    
    zonefs_io_error_cb() is also modified to use the inode offline and
    zone state flags instead of checking the device zone condition. The
    multiple calls to zonefs_check_zone_condition() are reduced to the first
    call on entry, which allows removing the "warn" argument.
    zonefs_inode_update_mode() is also used to update an inode access rights
    as zonefs_io_error_cb() modifies the inode flags depending on the volume
    error handling mode (defined with a mount option). Since an inode mode
    change differs for read-only zones between mount time and IO error time,
    the flag ZONEFS_ZONE_INIT_MODE is used to differentiate both cases.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>
    Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Stable-dep-of: 88b170088ad2 ("zonefs: Fix error message in zonefs_file_dio_append()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/zonefs/zonefs.h
+++ fs/zonefs/zonefs.h
@@ -39,10 +39,11 @@ static inline enum zonefs_ztype zonefs_zone_type(struct blk_zone *zone)
 	return ZONEFS_ZTYPE_SEQ;
 }
 
-#define ZONEFS_ZONE_OPEN	(1U << 0)
-#define ZONEFS_ZONE_ACTIVE	(1U << 1)
-#define ZONEFS_ZONE_OFFLINE	(1U << 2)
-#define ZONEFS_ZONE_READONLY	(1U << 3)
+#define ZONEFS_ZONE_INIT_MODE	(1U << 0)
+#define ZONEFS_ZONE_OPEN	(1U << 1)
+#define ZONEFS_ZONE_ACTIVE	(1U << 2)
+#define ZONEFS_ZONE_OFFLINE	(1U << 3)
+#define ZONEFS_ZONE_READONLY	(1U << 4)
 
 /*
  * In-memory inode data.
