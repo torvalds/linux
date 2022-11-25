0a335db8c745204a69ad0a18ab8b92a46a6098ea
Horatiu Vultur <horatiu.vultur@microchip.com>
2022-11-25 10:50:02 +0100

    net: microchip: vcap: Merge the vcap_ag_api_kunit.h into vcap_ag_api.h
    Currently there are 2 files that contain the keyfields, keys,
    actionfields and actions. First file is used by the kunit while the
    second one is used by VCAP api.
    The header file that is used by kunit is just a super set of the of the
    header file used by VCAP api.
    Therefore not to have duplicate information in different files which is
    also harder to maintain, create a single file that is used both by API
    and by kunit.
    
    Signed-off-by: Horatiu Vultur <horatiu.vultur@microchip.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- drivers/net/ethernet/microchip/vcap/vcap_api.h
+++ drivers/net/ethernet/microchip/vcap/vcap_api.h
@@ -11,9 +11,6 @@
 #include <linux/netdevice.h>
 
 /* Use the generated API model */
-#ifdef CONFIG_VCAP_KUNIT_TEST
-#include "vcap_ag_api_kunit.h"
-#endif
 #include "vcap_ag_api.h"
 
 #define VCAP_CID_LOOKUP_SIZE          100000 /* Chains in a lookup */
