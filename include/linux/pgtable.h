d3a89233583bf8edab18ac09732759c71dbe0173
zhang songyi <zhang.songyi@zte.com.cn>
2022-11-28 21:07:43 +0800

    include/linux/pgtable.h: : remove redundant pte variable
    Return value from ptep_get_and_clear_full() directly instead of taking
    this in another redundant variable.
    
    Link: https://lkml.kernel.org/r/202211282107437343474@zte.com.cn
    Signed-off-by: zhang songyi <zhang.songyi@zte.com.cn>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/pgtable.h
+++ include/linux/pgtable.h
@@ -425,9 +425,7 @@ static inline pte_t ptep_get_and_clear_full(struct mm_struct *mm,
 					    unsigned long address, pte_t *ptep,
 					    int full)
 {
-	pte_t pte;
-	pte = ptep_get_and_clear(mm, address, ptep);
-	return pte;
+	return ptep_get_and_clear(mm, address, ptep);
 }
 #endif
 
