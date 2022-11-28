d889913205cf7ebda905b1e62c5867ed4e39f6c2
Kalle Valo <quic_kvalo@quicinc.com>
2022-11-28 17:09:53 +0200

    wifi: ath12k: driver for Qualcomm Wi-Fi 7 devices
    ath12k is a new mac80211 driver for Qualcomm Wi-Fi 7 devices, first
    supporting QCN9274 and WCN7850 PCI devices.  QCN9274 supports both AP
    and station; WCN7850 supports only station mode. Monitor mode is not
    (yet) supported. Only PCI bus devices are supported.
    
    ath12k is forked from an earlier version of ath11k. It was simpler to
    have a "clean start" for the new generation and not try to share the
    code with ath11k. This makes maintenance easier and avoids major
    changes in ath11k, which would have significantly increased the risk
    of regressions in existing setups.
    
    ath12k uses le32 and cpu_to_le32() macros to handle endian
    conversions, instead of using the firmware byte swap feature utilized
    by ath11k. There is only one kernel module, named ath12k.ko.
    
    Currently ath12k only supports HE mode (IEEE 802.11ax) or older, but
    work is ongoing to add EHT mode (IEEE 802.11be) support.
    
    The size of the driver is ~41 kLOC and 45 files. To make the review
    easier, this initial version of ath12k does not support Device Tree,
    debugfs or any other extra features. Those will be added later, after
    ath12k is accepted to upstream.
    
    The driver is build tested by Intel's kernel test robot with both GCC
    and Clang. Sparse reports no warnings. The driver is mostly free of
    checkpatch warnings, albeit few of the warnings are omitted on
    purpose, list of them here:
    
    https://github.com/qca/qca-swiss-army-knife/blob/master/tools/scripts/ath12k/ath12k-check#L52
    
    The driver has had multiple authors who are listed in alphabetical
    order below.
    
    Co-developed-by: Balamurugan Selvarajan <quic_bselvara@quicinc.com>
    Signed-off-by: Balamurugan Selvarajan <quic_bselvara@quicinc.com>
    Co-developed-by: Baochen Qiang <quic_bqiang@quicinc.com>
    Signed-off-by: Baochen Qiang <quic_bqiang@quicinc.com>
    Co-developed-by: Bhagavathi Perumal S <quic_bperumal@quicinc.com>
    Signed-off-by: Bhagavathi Perumal S <quic_bperumal@quicinc.com>
    Co-developed-by: Carl Huang <quic_cjhuang@quicinc.com>
    Signed-off-by: Carl Huang <quic_cjhuang@quicinc.com>
    Co-developed-by: Jeff Johnson <quic_jjohnson@quicinc.com>
    Signed-off-by: Jeff Johnson <quic_jjohnson@quicinc.com>
    Co-developed-by: Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
    Signed-off-by: Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
    Co-developed-by: P Praneesh <quic_ppranees@quicinc.com>
    Signed-off-by: P Praneesh <quic_ppranees@quicinc.com>
    Co-developed-by: Pradeep Kumar Chitrapu <quic_pradeepc@quicinc.com>
    Signed-off-by: Pradeep Kumar Chitrapu <quic_pradeepc@quicinc.com>
    Co-developed-by: Ramya Gnanasekar <quic_rgnanase@quicinc.com>
    Signed-off-by: Ramya Gnanasekar <quic_rgnanase@quicinc.com>
    Co-developed-by: Sriram R <quic_srirrama@quicinc.com>
    Signed-off-by: Sriram R <quic_srirrama@quicinc.com>
    Co-developed-by: Vasanthakumar Thiagarajan <quic_vthiagar@quicinc.com>
    Signed-off-by: Vasanthakumar Thiagarajan <quic_vthiagar@quicinc.com>
    Co-developed-by: Wen Gong <quic_wgong@quicinc.com>
    Signed-off-by: Wen Gong <quic_wgong@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>

---
+++ drivers/net/wireless/ath/ath12k/dp_tx.h
@@ -0,0 +1,41 @@
+/* SPDX-License-Identifier: BSD-3-Clause-Clear */
+/*
+ * Copyright (c) 2018-2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef ATH12K_DP_TX_H
+#define ATH12K_DP_TX_H
+
+#include "core.h"
+#include "hal_tx.h"
+
+struct ath12k_dp_htt_wbm_tx_status {
+	bool acked;
+	int ack_rssi;
+};
+
+int ath12k_dp_tx_htt_h2t_ver_req_msg(struct ath12k_base *ab);
+int ath12k_dp_tx(struct ath12k *ar, struct ath12k_vif *arvif,
+		 struct sk_buff *skb);
+void ath12k_dp_tx_completion_handler(struct ath12k_base *ab, int ring_id);
+
+int ath12k_dp_tx_htt_h2t_ppdu_stats_req(struct ath12k *ar, u32 mask);
+int
+ath12k_dp_tx_htt_h2t_ext_stats_req(struct ath12k *ar, u8 type,
+				   struct htt_ext_stats_cfg_params *cfg_params,
+				   u64 cookie);
+int ath12k_dp_tx_htt_rx_monitor_mode_ring_config(struct ath12k *ar, bool reset);
+
+int ath12k_dp_tx_htt_rx_filter_setup(struct ath12k_base *ab, u32 ring_id,
+				     int mac_id, enum hal_ring_type ring_type,
+				     int rx_buf_size,
+				     struct htt_rx_ring_tlv_filter *tlv_filter);
+void ath12k_dp_tx_put_bank_profile(struct ath12k_dp *dp, u8 bank_id);
+int ath12k_dp_tx_htt_tx_filter_setup(struct ath12k_base *ab, u32 ring_id,
+				     int mac_id, enum hal_ring_type ring_type,
+				     int tx_buf_size,
+				     struct htt_tx_ring_tlv_filter *htt_tlv_filter);
+int ath12k_dp_tx_htt_tx_monitor_mode_ring_config(struct ath12k *ar, bool reset);
+int ath12k_dp_tx_htt_monitor_mode_ring_config(struct ath12k *ar, bool reset);
+#endif
