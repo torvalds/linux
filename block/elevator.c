5998249e3238428156b09911f1606b41113443c5
Jinlong Chen <nickyc975@zju.edu.cn>
2022-11-29 23:46:35 +0800

    block: replace continue with else-if in elv_iosched_show
    else-if is more readable than continue here.
    
    Signed-off-by: Jinlong Chen <nickyc975@zju.edu.cn>
    Link: https://lore.kernel.org/r/77ac19ba556efd2c8639a6396eb4203c59bc13d6.1669736350.git.nickyc975@zju.edu.cn
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/elevator.c
+++ block/elevator.c
@@ -776,11 +776,9 @@ ssize_t elv_iosched_show(struct request_queue *q, char *name)
 
 	spin_lock(&elv_list_lock);
 	list_for_each_entry(e, &elv_list, list) {
-		if (e == cur) {
+		if (e == cur)
 			len += sprintf(name+len, "[%s] ", cur->elevator_name);
-			continue;
-		}
-		if (elv_support_features(q, e))
+		else if (elv_support_features(q, e))
 			len += sprintf(name+len, "%s ", e->elevator_name);
 	}
 	spin_unlock(&elv_list_lock);
