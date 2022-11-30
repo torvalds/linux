325fc9579c2e415e8ae3d1ab56a5157751157869
Sean Christopherson <seanjc@google.com>
2022-11-30 23:09:22 +0000

    KVM: SVM: Check for SVM support in CPU compatibility checks
    Check that SVM is supported and enabled in the processor compatibility
    checks.  SVM already checks for support during hardware enabling,
    i.e. this doesn't really add new functionality.  The net effect is that
    KVM will refuse to load if a CPU doesn't have SVM fully enabled, as
    opposed to failing KVM_CREATE_VM.
    
    Opportunistically move svm_check_processor_compat() up in svm.c so that
    it can be invoked during hardware enabling in a future patch.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20221130230934.1014142-39-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -543,6 +543,14 @@ static bool kvm_is_svm_supported(void)
 	return true;
 }
 
+static int __init svm_check_processor_compat(void)
+{
+	if (!kvm_is_svm_supported())
+		return -EIO;
+
+	return 0;
+}
+
 void __svm_write_tsc_multiplier(u64 multiplier)
 {
 	preempt_disable();
@@ -4091,11 +4099,6 @@ svm_patch_hypercall(struct kvm_vcpu *vcpu, unsigned char *hypercall)
 	hypercall[0] = 0x0f;
 	hypercall[1] = 0x01;
 	hypercall[2] = 0xd9;
-}
-
-static int __init svm_check_processor_compat(void)
-{
-	return 0;
 }
 
 /*
