a35b4bcb43895c6e57e09bdad1cf3191cb67c63c
Jan Kara <jack@suse.cz>
2022-11-30 18:56:53 +0100

    block: Do not reread partition table on exclusively open device
    commit 36369f46e91785688a5f39d7a5590e3f07981316 upstream.
    
    Since commit 10c70d95c0f2 ("block: remove the bd_openers checks in
    blk_drop_partitions") we allow rereading of partition table although
    there are users of the block device. This has an undesirable consequence
    that e.g. if sda and sdb are assembled to a RAID1 device md0 with
    partitions, BLKRRPART ioctl on sda will rescan partition table and
    create sda1 device. This partition device under a raid device confuses
    some programs (such as libstorage-ng used for initial partitioning for
    distribution installation) leading to failures.
    
    Fix the problem refusing to rescan partitions if there is another user
    that has the block device exclusively open.
    
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/all/20221130135344.2ul4cyfstfs3znxg@quack3
    Fixes: 10c70d95c0f2 ("block: remove the bd_openers checks in blk_drop_partitions")
    Signed-off-by: Jan Kara <jack@suse.cz>
    Link: https://lore.kernel.org/r/20221130175653.24299-1-jack@suse.cz
    [axboe: fold in followup fix]
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- block/genhd.c
+++ block/genhd.c
@@ -356,7 +356,7 @@ void disk_uevent(struct gendisk *disk, enum kobject_action action)
 }
 EXPORT_SYMBOL_GPL(disk_uevent);
 
-int disk_scan_partitions(struct gendisk *disk, fmode_t mode)
+int disk_scan_partitions(struct gendisk *disk, fmode_t mode, void *owner)
 {
 	struct block_device *bdev;
 
@@ -365,6 +365,9 @@ int disk_scan_partitions(struct gendisk *disk, fmode_t mode)
 	if (test_bit(GD_SUPPRESS_PART_SCAN, &disk->state))
 		return -EINVAL;
 	if (disk->open_partitions)
+		return -EBUSY;
+	/* Someone else has bdev exclusively open? */
+	if (disk->part0->bd_holder && disk->part0->bd_holder != owner)
 		return -EBUSY;
 
 	set_bit(GD_NEED_PART_SCAN, &disk->state);
@@ -500,7 +503,7 @@ int __must_check device_add_disk(struct device *parent, struct gendisk *disk,
 
 		bdev_add(disk->part0, ddev->devt);
 		if (get_capacity(disk))
-			disk_scan_partitions(disk, FMODE_READ);
+			disk_scan_partitions(disk, FMODE_READ, NULL);
 
 		/*
 		 * Announce the disk and partitions after all partitions are
