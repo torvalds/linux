d209ce353a324601f9d3c1eee43b1f0df53021b3
Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
2022-12-01 20:58:18 +0800

    blk-crypto: Add support for SM4-XTS blk crypto mode
    SM4 is a symmetric cipher algorithm widely used in China. The SM4-XTS
    variant is used to encrypt length-preserving data. This is the
    mandatory algorithm in some special scenarios.
    
    Add support for the algorithm to block inline encryption. This is needed
    for the inlinecrypt mount option to be supported via
    blk-crypto-fallback, as it is for the other fscrypt modes.
    
    Signed-off-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Link: https://lore.kernel.org/r/20221201125819.36932-2-tianjia.zhang@linux.alibaba.com

--- block/blk-crypto.c
+++ block/blk-crypto.c
@@ -36,6 +36,12 @@ const struct blk_crypto_mode blk_crypto_modes[] = {
 		.keysize = 32,
 		.ivsize = 32,
 	},
+	[BLK_ENCRYPTION_MODE_SM4_XTS] = {
+		.name = "SM4-XTS",
+		.cipher_str = "xts(sm4)",
+		.keysize = 32,
+		.ivsize = 16,
+	},
 };
 
 /*
