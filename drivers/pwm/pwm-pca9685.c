8fa22f4b88e877c0811d2a0e506cf56755add554
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2022-12-02 19:35:38 +0100

    pwm: pca9685: Convert to i2c's .probe_new()
    The probe function doesn't make use of the i2c_device_id * parameter so it
    can be trivially converted.
    
    Link: https://lore.kernel.org/r/20221118224540.619276-538-uwe@kleine-koenig.org
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- drivers/pwm/pwm-pca9685.c
+++ drivers/pwm/pwm-pca9685.c
@@ -515,8 +515,7 @@ static const struct regmap_config pca9685_regmap_i2c_config = {
 	.cache_type = REGCACHE_NONE,
 };
 
-static int pca9685_pwm_probe(struct i2c_client *client,
-				const struct i2c_device_id *id)
+static int pca9685_pwm_probe(struct i2c_client *client)
 {
 	struct pca9685 *pca;
 	unsigned int reg;
@@ -666,7 +665,7 @@ static struct i2c_driver pca9685_i2c_driver = {
 		.of_match_table = of_match_ptr(pca9685_dt_ids),
 		.pm = &pca9685_pwm_pm,
 	},
-	.probe = pca9685_pwm_probe,
+	.probe_new = pca9685_pwm_probe,
 	.remove = pca9685_pwm_remove,
 	.id_table = pca9685_id,
 };
