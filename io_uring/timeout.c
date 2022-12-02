9b5d3f21f1aba54064f8f56694d09ee8d91daec3
Pavel Begunkov <asml.silence@gmail.com>
2022-12-02 17:47:22 +0000

    io_uring: protect cq_timeouts with timeout_lock
    commit ea011ee10231f5fa6cbb415007048ca0bb948baf upstream.
    
    Read cq_timeouts in io_flush_timeouts() only after taking the
    timeout_lock, as it's protected by it. There are many places where we
    also grab ->completion_lock, but for instance io_timeout_fn() doesn't
    and still modifies cq_timeouts.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/9c79544dd6cf5c4018cb1bab99cf481a93ea46ef.1670002973.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- io_uring/timeout.c
+++ io_uring/timeout.c
@@ -72,10 +72,12 @@ static bool io_kill_timeout(struct io_kiocb *req, int status)
 __cold void io_flush_timeouts(struct io_ring_ctx *ctx)
 	__must_hold(&ctx->completion_lock)
 {
-	u32 seq = ctx->cached_cq_tail - atomic_read(&ctx->cq_timeouts);
+	u32 seq;
 	struct io_timeout *timeout, *tmp;
 
 	spin_lock_irq(&ctx->timeout_lock);
+	seq = ctx->cached_cq_tail - atomic_read(&ctx->cq_timeouts);
+
 	list_for_each_entry_safe(timeout, tmp, &ctx->timeout_list, list) {
 		struct io_kiocb *req = cmd_to_io_kiocb(timeout);
 		u32 events_needed, events_got;
