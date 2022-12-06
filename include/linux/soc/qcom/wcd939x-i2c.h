3db5852c624937b8e9117fdbb0f2fbef1cc1954f
Prasad Kumpatla <quic_pkumpatl@quicinc.com>
2022-12-06 22:17:56 +0530

    wcd939x-usbss: config to update power modes for wcd-usbss
    API to configure power mode for wcd-usbss.
    
    Change-Id: Id9722c8e73f4616525aef41c090c51a6c1695565
    Signed-off-by: Prasad Kumpatla <quic_pkumpatl@quicinc.com>

--- include/linux/soc/qcom/wcd939x-i2c.h
+++ include/linux/soc/qcom/wcd939x-i2c.h
@@ -9,6 +9,11 @@
 #include <linux/of.h>
 #include <linux/notifier.h>
 
+enum wcd_usbss_config_type {
+	WCD_USBSS_CONFIG_TYPE_POWER_MODE,
+	WCD_USBSS_CONFIG_TYPE_ZDET,
+};
+
 enum wcd_usbss_cable_types {
 	WCD_USBSS_USB,
 	WCD_USBSS_DP_AUX_CC1,
@@ -31,6 +36,8 @@ int wcd_usbss_reg_notifier(struct notifier_block *nb,
 int wcd_usbss_unreg_notifier(struct notifier_block *nb,
 			   struct device_node *node);
 int wcd_usbss_dpdm_switch_update(bool enable, bool eq_en);
+int wcd_usbss_audio_config(bool enable, enum wcd_usbss_config_type config_type,
+		unsigned int power_mode);
 #else
 static inline int wcd_usbss_switch_update(enum wcd_usbss_cable_types ctype,
 		enum wcd_usbss_cable_status status)
@@ -51,6 +58,12 @@ static inline int wcd_usbss_unreg_notifier(struct notifier_block *nb,
 }
 
 static inline int wcd_usbss_dpdm_switch_update(bool enable, bool eq_en)
+{
+	return 0;
+}
+
+int wcd_usbss_audio_config(bool enable, enum wcd_usbss_config_type config_type,
+		unsigned int power_mode)
 {
 	return 0;
 }
