ade23d7b7ec5c38bd43ec44ccb753cb7ea8ac08a
Quentin Schulz <quentin.schulz@theobroma-systems.com>
2022-12-07 14:19:17 +0100

    usb: dwc2: power on/off phy for peripheral mode in dual-role mode
    The PHY power is handled for peripheral mode but only when the device is
    forced into this peripheral mode. It is missing when the device is
    operating in peripheral mode when dual-role mode is enabled, so let's
    update the condition to match this scenario.
    
    Signed-off-by: Quentin Schulz <quentin.schulz@theobroma-systems.com>
    Link: https://lore.kernel.org/r/20221206-dwc2-gadget-dual-role-v1-2-36515e1092cd@theobroma-systems.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/dwc2/platform.c
+++ drivers/usb/dwc2/platform.c
@@ -576,7 +576,8 @@ static int dwc2_driver_probe(struct platform_device *dev)
 	dwc2_debugfs_init(hsotg);
 
 	/* Gadget code manages lowlevel hw on its own */
-	if (hsotg->dr_mode == USB_DR_MODE_PERIPHERAL)
+	if (hsotg->dr_mode == USB_DR_MODE_PERIPHERAL ||
+	    (hsotg->dr_mode == USB_DR_MODE_OTG && dwc2_is_device_mode(hsotg)))
 		dwc2_lowlevel_hw_disable(hsotg);
 
 #if IS_ENABLED(CONFIG_USB_DWC2_PERIPHERAL) || \
