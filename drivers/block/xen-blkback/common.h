656f3c1d7966ea65f9b51d17db3008ce5dbb4090
Juergen Gross <jgross@suse.com>
2022-12-12 09:43:28 +0100

    xen/blkback: remove stale prototype
    There is no function xen_blkif_purge_persistent(), so remove its
    prototype from common.h.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Acked-by: Roger Pau Monné <roger.pau@citrix.com>
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- drivers/block/xen-blkback/common.h
+++ drivers/block/xen-blkback/common.h
@@ -384,7 +384,6 @@ void xen_blkif_xenbus_fini(void);
 
 irqreturn_t xen_blkif_be_int(int irq, void *dev_id);
 int xen_blkif_schedule(void *arg);
-int xen_blkif_purge_persistent(void *arg);
 void xen_blkbk_free_caches(struct xen_blkif_ring *ring);
 
 int xen_blkbk_flush_diskcache(struct xenbus_transaction xbt,
