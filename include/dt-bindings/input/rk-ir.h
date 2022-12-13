462aebc05c2a68ccd8c6308f622cd4c8a61c85a8
Alex Wang <alex.wang@rock-chips.com>
2022-12-13 10:36:41 +0800

    dt-bindings: input: add rockchip ir key defines
    Change-Id: Ib2badc1396ec6b7e34b12ba77900d26a06a19694
    Signed-off-by: Alex Wang <alex.wang@rock-chips.com>

---
+++ include/dt-bindings/input/rk-ir.h
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Copyright (c) 2022 Rockchip Electronics Co., Ltd.
+ */
+#ifndef _DT_BINDINGS_RK_IR_H
+#define _DT_BINDINGS_RK_IR_H
+
+/* STB KEYS */
+#define KEY_CHANNEL_UP          249
+#define KEY_CHANNEL_DN          250
+#define KEY_HOME_PAGE           251
+#define KEY_CH_CUT_BACK         252
+#define KEY_DIRECT_SEEDING      253
+#define KEY_REVIEW              254
+#define KEY_ON_DEMAND           255
+#define KEY_INFO1               256
+#define KEY_SOUND1              257
+#define KEY_X1                  258
+#define KEY_X2                  259
+#define KEY_LOCAL               260
+#define KEY_APPLICATION         261
+#define KEY_POS                 262
+#define KEY_GO_BEGINNING        263
+#define KEY_INTERX              264
+#define KEY_FAVORITE            265
+#define KEY_CHANNEL_POS         266
+#define KEY_EVENT               267
+#define KEY_COMM                268
+#define KEY_LAUNCH              269
+#define KEY_TRACK               270
+#define KEY_PORTAL              271
+
+#endif
