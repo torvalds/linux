1d1b4c63ba739c6ca695cb2ea13fefa9dfbff60d
Muhammad Husaini Zulkifli <muhammad.husaini.zulkifli@intel.com>
2022-12-15 00:29:09 +0800

    igc: Remove reset adapter task for i226 during disable tsn config
    I225 have limitation when programming the BaseTime register which required
    a power cycle of the controller. This limitation already lifted in I226.
    This patch removes the restriction so that when user configure/remove any
    TSN mode, it would not go into power cycle reset adapter.
    
    How to test:
    
    Schedule any gate control list configuration or delete it.
    
    Example:
    
    1)
    
    BASE_TIME=$(date +%s%N)
    tc qdisc replace dev $interface_name parent root handle 100 taprio \
        num_tc 4 \
        map 3 1 0 2 3 3 3 3 3 3 3 3 3 3 3 3 \
        queues 1@0 1@1 1@2 1@3 \
        base-time $BASE_TIME \
        sched-entry S 0F 1000000 \
        flags 0x2
    
    2) tc qdisc del dev $intername_name root
    
    Signed-off-by: Muhammad Husaini Zulkifli <muhammad.husaini.zulkifli@intel.com>
    Tested-by: Naama Meir <naamax.meir@linux.intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>

--- drivers/net/ethernet/intel/igc/igc_main.c
+++ drivers/net/ethernet/intel/igc/igc_main.c
@@ -6020,7 +6020,7 @@ static int igc_tsn_enable_launchtime(struct igc_adapter *adapter,
 	if (err)
 		return err;
 
-	return igc_tsn_offload_apply(adapter, qopt->enable);
+	return igc_tsn_offload_apply(adapter);
 }
 
 static int igc_tsn_clear_schedule(struct igc_adapter *adapter)
@@ -6134,7 +6134,7 @@ static int igc_tsn_enable_qbv_scheduling(struct igc_adapter *adapter,
 	if (err)
 		return err;
 
-	return igc_tsn_offload_apply(adapter, qopt->enable);
+	return igc_tsn_offload_apply(adapter);
 }
 
 static int igc_save_cbs_params(struct igc_adapter *adapter, int queue,
@@ -6202,7 +6202,7 @@ static int igc_tsn_enable_cbs(struct igc_adapter *adapter,
 	if (err)
 		return err;
 
-	return igc_tsn_offload_apply(adapter, qopt->enable);
+	return igc_tsn_offload_apply(adapter);
 }
 
 static int igc_setup_tc(struct net_device *dev, enum tc_setup_type type,
