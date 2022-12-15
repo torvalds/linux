2cec280c491547e5d15a741aa66128650b97242f
Arnd Bergmann <arnd@arndb.de>
2022-12-15 17:30:17 +0100

    kmsan: include linux/vmalloc.h
    commit aaa746ad8b30f38ef89a301faf339ef1c19cf33a upstream.
    
    This is needed for the vmap/vunmap declarations:
    
    mm/kmsan/kmsan_test.c:316:9: error: implicit declaration of function 'vmap' is invalid in C99 [-Werror,-Wimplicit-function-declaration]
            vbuf = vmap(pages, npages, VM_MAP, PAGE_KERNEL);
                   ^
    mm/kmsan/kmsan_test.c:316:29: error: use of undeclared identifier 'VM_MAP'
            vbuf = vmap(pages, npages, VM_MAP, PAGE_KERNEL);
                                       ^
    mm/kmsan/kmsan_test.c:322:3: error: implicit declaration of function 'vunmap' is invalid in C99 [-Werror,-Wimplicit-function-declaration]
                    vunmap(vbuf);
                    ^
    
    Link: https://lkml.kernel.org/r/20221215163046.4079767-1-arnd@kernel.org
    Fixes: 8ed691b02ade ("kmsan: add tests for KMSAN")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Alexander Potapenko <glider@google.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Marco Elver <elver@google.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/kmsan/kmsan_test.c
+++ mm/kmsan/kmsan_test.c
@@ -22,6 +22,7 @@
 #include <linux/spinlock.h>
 #include <linux/string.h>
 #include <linux/tracepoint.h>
+#include <linux/vmalloc.h>
 #include <trace/events/printk.h>
 
 static DEFINE_PER_CPU(int, per_cpu_var);
