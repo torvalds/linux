55ab834a86a9934c4f17825c115f7dc16a89aae7
Michal Hocko <mhocko@suse.com>
2022-12-16 10:46:33 +0100

    Revert "mm: add nodes= arg to memory.reclaim"
    This reverts commit 12a5d3955227b0d7e04fb793ccceeb2a1dd275c5.
    
    Although it is recognized that a finer grained pro-active reclaim is
    something we need and want the semantic of this implementation is really
    ambiguous.
    
    In a follow up discussion it became clear that there are two essential
    usecases here.  One is to use memory.reclaim to pro-actively reclaim
    memory and expectation is that the requested and reported amount of memory
    is uncharged from the memcg.  Another usecase focuses on pro-active
    demotion when the memory is merely shuffled around to demotion targets
    while the overall charged memory stays unchanged.
    
    The current implementation considers demoted pages as reclaimed and that
    break both usecases.  [1] has tried to address the reporting part but
    there are more issues with that summarized in [2] and follow up emails.
    
    Let's revert the nodemask based extension of the memcg pro-active
    reclaim for now until we settle with a more robust semantic.
    
    [1] http://lkml.kernel.org/r/http://lkml.kernel.org/r/20221206023406.3182800-1-almasrymina@google.com
    [2] http://lkml.kernel.org/r/Y5bsmpCyeryu3Zz1@dhcp22.suse.cz
    
    Link: https://lkml.kernel.org/r/Y5xASNe1x8cusiTx@dhcp22.suse.cz
    Fixes: 12a5d3955227b0d ("mm: add nodes= arg to memory.reclaim")
    Signed-off-by: Michal Hocko <mhocko@suse.com>
    Cc: Bagas Sanjaya <bagasdotme@gmail.com>
    Cc: Huang Ying <ying.huang@intel.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Mina Almasry <almasrymina@google.com>
    Cc: Muchun Song <songmuchun@bytedance.com>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Shakeel Butt <shakeelb@google.com>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Wei Xu <weixugc@google.com>
    Cc: Yang Shi <yang.shi@linux.alibaba.com>
    Cc: Yosry Ahmed <yosryahmed@google.com>
    Cc: zefan li <lizefan.x@bytedance.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -6754,8 +6754,7 @@ unsigned long mem_cgroup_shrink_node(struct mem_cgroup *memcg,
 unsigned long try_to_free_mem_cgroup_pages(struct mem_cgroup *memcg,
 					   unsigned long nr_pages,
 					   gfp_t gfp_mask,
-					   unsigned int reclaim_options,
-					   nodemask_t *nodemask)
+					   unsigned int reclaim_options)
 {
 	unsigned long nr_reclaimed;
 	unsigned int noreclaim_flag;
@@ -6770,7 +6769,6 @@ unsigned long try_to_free_mem_cgroup_pages(struct mem_cgroup *memcg,
 		.may_unmap = 1,
 		.may_swap = !!(reclaim_options & MEMCG_RECLAIM_MAY_SWAP),
 		.proactive = !!(reclaim_options & MEMCG_RECLAIM_PROACTIVE),
-		.nodemask = nodemask,
 	};
 	/*
 	 * Traverse the ZONELIST_FALLBACK zonelist of the current node to put
