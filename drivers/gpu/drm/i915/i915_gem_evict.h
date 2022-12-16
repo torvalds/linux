801fa7a81f6da533cc5442fc40e32c72b76cd42a
Matthew Auld <matthew.auld@intel.com>
2022-12-16 11:34:56 +0000

    drm/i915: improve the catch-all evict to handle lock contention
    The catch-all evict can fail due to object lock contention, since it
    only goes as far as trylocking the object, due to us already holding the
    vm->mutex. Doing a full object lock here can deadlock, since the
    vm->mutex is always our inner lock. Add another execbuf pass which drops
    the vm->mutex and then tries to grab the object will the full lock,
    before then retrying the eviction. This should be good enough for now to
    fix the immediate regression with userspace seeing -ENOSPC from execbuf
    due to contended object locks during GTT eviction.
    
    v2 (Mani)
      - Also revamp the docs for the different passes.
    
    Testcase: igt@gem_ppgtt@shrink-vs-evict-*
    Fixes: 7e00897be8bf ("drm/i915: Add object locking to i915_gem_evict_for_node and i915_gem_evict_something, v2.")
    References: https://gitlab.freedesktop.org/drm/intel/-/issues/7627
    References: https://gitlab.freedesktop.org/drm/intel/-/issues/7570
    References: https://bugzilla.mozilla.org/show_bug.cgi?id=1779558
    Signed-off-by: Matthew Auld <matthew.auld@intel.com>
    Cc: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Cc: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Cc: Tvrtko Ursulin <tvrtko.ursulin@linux.intel.com>
    Cc: Andrzej Hajda <andrzej.hajda@intel.com>
    Cc: Mani Milani <mani@chromium.org>
    Cc: <stable@vger.kernel.org> # v5.18+
    Reviewed-by: Mani Milani <mani@chromium.org>
    Tested-by: Mani Milani <mani@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20221216113456.414183-1-matthew.auld@intel.com

--- drivers/gpu/drm/i915/i915_gem_evict.h
+++ drivers/gpu/drm/i915/i915_gem_evict.h
@@ -11,6 +11,7 @@
 struct drm_mm_node;
 struct i915_address_space;
 struct i915_gem_ww_ctx;
+struct drm_i915_gem_object;
 
 int __must_check i915_gem_evict_something(struct i915_address_space *vm,
 					  struct i915_gem_ww_ctx *ww,
@@ -23,6 +24,7 @@ int __must_check i915_gem_evict_for_node(struct i915_address_space *vm,
 					 struct drm_mm_node *node,
 					 unsigned int flags);
 int i915_gem_evict_vm(struct i915_address_space *vm,
-		      struct i915_gem_ww_ctx *ww);
+		      struct i915_gem_ww_ctx *ww,
+		      struct drm_i915_gem_object **busy_bo);
 
 #endif /* __I915_GEM_EVICT_H__ */
