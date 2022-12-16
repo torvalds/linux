1c8c09576dbdedfc6f5fa766b18fb8c5fdbe7037
shengyang.chen <shengyang.chen@starfivetech.com>
2022-12-16 16:15:39 +0800

    riscv: linux: vout: fix cusor problem
    fix cusor bluring problem of debian
    
    Signed-off-by: keith <keith.zhao@starfivetech.com>

--- drivers/gpu/drm/verisilicon/vs_plane.c
+++ drivers/gpu/drm/verisilicon/vs_plane.c
@@ -9,6 +9,7 @@
 #include <drm/drm_fb_cma_helper.h>
 #include <drm/drm_gem_cma_helper.h>
 #include <drm/vs_drm.h>
+#include <soc/starfive/vic7100.h>
 
 #include "vs_type.h"
 #include "vs_crtc.h"
@@ -289,6 +290,7 @@ static void vs_plane_atomic_update(struct drm_plane *plane,
 
 		vs_obj = vs_fb_get_gem_obj(fb, i);
 		vs_plane->dma_addr[i] = vs_obj->iova + fb->offsets[i];
+		starfive_flush_dcache(vs_plane->dma_addr[i], vs_obj->size);
 	}
 
 	plane_state->status.src = drm_plane_state_src(new_state);
