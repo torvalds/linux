92987fe8bdd1cbec61919a394bb11316c5d860f4
Paul E. McKenney <paulmck@kernel.org>
2022-12-19 18:02:20 -0800

    rcu: Allow expedited RCU CPU stall warnings to dump task stacks
    This commit introduces the rcupdate.rcu_exp_stall_task_details kernel
    boot parameter, which cause expedited RCU CPU stall warnings to dump
    the stacks of any tasks blocking the current expedited grace period.
    
    Reported-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/rcu.h
+++ kernel/rcu/rcu.h
@@ -224,6 +224,7 @@ extern int rcu_cpu_stall_ftrace_dump;
 extern int rcu_cpu_stall_suppress;
 extern int rcu_cpu_stall_timeout;
 extern int rcu_exp_cpu_stall_timeout;
+extern bool rcu_exp_stall_task_details __read_mostly;
 int rcu_jiffies_till_stall_check(void);
 int rcu_exp_jiffies_till_stall_check(void);
 
