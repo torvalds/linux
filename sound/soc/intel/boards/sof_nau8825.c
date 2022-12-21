3e78986a840d59dd27e636eae3f52dc11125c835
Arnd Bergmann <arnd@arndb.de>
2022-12-21 14:24:56 +0100

    ASoC: Intel: sof-nau8825: fix module alias overflow
    The maximum name length for a platform_device_id entry is 20 characters
    including the trailing NUL byte. The sof_nau8825.c file exceeds that,
    which causes an obscure error message:
    
    sound/soc/intel/boards/snd-soc-sof_nau8825.mod.c:35:45: error: illegal character encoding in string literal [-Werror,-Winvalid-source-encoding]
    MODULE_ALIAS("platform:adl_max98373_nau8825<U+0018><AA>");
                                                       ^~~~
    include/linux/module.h:168:49: note: expanded from macro 'MODULE_ALIAS'
                                                    ^~~~~~
    include/linux/module.h:165:56: note: expanded from macro 'MODULE_INFO'
                                                           ^~~~
    include/linux/moduleparam.h:26:47: note: expanded from macro '__MODULE_INFO'
                    = __MODULE_INFO_PREFIX __stringify(tag) "=" info
    
    I could not figure out how to make the module handling robust enough
    to handle this better, but as a quick fix, using slightly shorter
    names that are still unique avoids the build issue.
    
    Fixes: 8d0872f6239f ("ASoC: Intel: add sof-nau8825 machine driver")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20221221132515.2363276-1-arnd@kernel.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/boards/sof_nau8825.c
+++ sound/soc/intel/boards/sof_nau8825.c
@@ -631,7 +631,7 @@ static const struct platform_device_id board_ids[] = {
 
 	},
 	{
-		.name = "adl_rt1019p_nau8825",
+		.name = "adl_rt1019p_8825",
 		.driver_data = (kernel_ulong_t)(SOF_NAU8825_SSP_CODEC(0) |
 					SOF_SPEAKER_AMP_PRESENT |
 					SOF_RT1019P_SPEAKER_AMP_PRESENT |
@@ -639,7 +639,7 @@ static const struct platform_device_id board_ids[] = {
 					SOF_NAU8825_NUM_HDMIDEV(4)),
 	},
 	{
-		.name = "adl_max98373_nau8825",
+		.name = "adl_max98373_8825",
 		.driver_data = (kernel_ulong_t)(SOF_NAU8825_SSP_CODEC(0) |
 					SOF_SPEAKER_AMP_PRESENT |
 					SOF_MAX98373_SPEAKER_AMP_PRESENT |
@@ -650,7 +650,7 @@ static const struct platform_device_id board_ids[] = {
 	},
 	{
 		/* The limitation of length of char array, shorten the name */
-		.name = "adl_mx98360a_nau8825",
+		.name = "adl_mx98360a_8825",
 		.driver_data = (kernel_ulong_t)(SOF_NAU8825_SSP_CODEC(0) |
 					SOF_SPEAKER_AMP_PRESENT |
 					SOF_MAX98360A_SPEAKER_AMP_PRESENT |
@@ -661,7 +661,7 @@ static const struct platform_device_id board_ids[] = {
 
 	},
 	{
-		.name = "adl_rt1015p_nau8825",
+		.name = "adl_rt1015p_8825",
 		.driver_data = (kernel_ulong_t)(SOF_NAU8825_SSP_CODEC(0) |
 					SOF_SPEAKER_AMP_PRESENT |
 					SOF_RT1015P_SPEAKER_AMP_PRESENT |
