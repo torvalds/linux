4091dff1ffbca714aae6e96ae9503fd44ae83bb9
Krishna Kurapati <quic_kriskura@quicinc.com>
2022-12-21 23:32:01 +0530

    UPSTREAM: usb: dwc3: Remove DWC3 locking during gadget suspend/resume
    Remove the need for making dwc3_gadget_suspend() and dwc3_gadget_resume()
    to be called in a spinlock, as dwc3_gadget_run_stop() could potentially
    take some time to complete.
    
    Signed-off-by: Wesley Cheng <quic_wcheng@quicinc.com>
    Link: https://lore.kernel.org/r/20220901193625.8727-3-quic_wcheng@quicinc.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (cherry picked from commit 5265397f94424eaea596026fd34dc7acf474dcec)
    
    Bug: 263189538
    Change-Id: Ia28cf2f5dd1929a83812d685eec9a9008f8bc81d
    Signed-off-by: Krishna Kurapati <quic_kriskura@quicinc.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1756,9 +1756,7 @@ static int dwc3_suspend_common(struct dwc3 *dwc, pm_message_t msg)
 	case DWC3_GCTL_PRTCAP_DEVICE:
 		if (pm_runtime_suspended(dwc->dev))
 			break;
-		spin_lock_irqsave(&dwc->lock, flags);
 		dwc3_gadget_suspend(dwc);
-		spin_unlock_irqrestore(&dwc->lock, flags);
 		synchronize_irq(dwc->irq_gadget);
 		dwc3_core_exit(dwc);
 		break;
@@ -1819,9 +1817,7 @@ static int dwc3_resume_common(struct dwc3 *dwc, pm_message_t msg)
 			return ret;
 
 		dwc3_set_prtcap(dwc, DWC3_GCTL_PRTCAP_DEVICE);
-		spin_lock_irqsave(&dwc->lock, flags);
 		dwc3_gadget_resume(dwc);
-		spin_unlock_irqrestore(&dwc->lock, flags);
 		break;
 	case DWC3_GCTL_PRTCAP_HOST:
 		if (!PMSG_IS_AUTO(msg)) {
