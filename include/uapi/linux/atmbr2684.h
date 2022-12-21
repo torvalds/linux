7fac54b93ad13e5e7ac237af33eb2a0940eaeea0
Rong Tao <rongtao@cestc.cn>
2022-12-21 20:36:27 +0800

    atm: uapi: fix spelling typos in comments
    Fix the typo of 'Unsuported' in atmbr2684.h
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Link: https://lore.kernel.org/r/tencent_F1354BEC925C65EA357E741E91DF2044E805@qq.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/uapi/linux/atmbr2684.h
+++ include/uapi/linux/atmbr2684.h
@@ -38,7 +38,7 @@
  */
 #define BR2684_ENCAPS_VC	(0)	/* VC-mux */
 #define BR2684_ENCAPS_LLC	(1)
-#define BR2684_ENCAPS_AUTODETECT (2)	/* Unsuported */
+#define BR2684_ENCAPS_AUTODETECT (2)	/* Unsupported */
 
 /*
  * Is this VC bridged or routed?
