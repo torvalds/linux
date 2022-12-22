ba7523bb0f494fc440d3a9bb0b665cfcaa192d0c
Ajye Huang <ajye_huang@compal.corp-partner.google.com>
2022-12-22 12:26:24 +0800

    ASoC: Intel: sof_nau8825: add variant with nau8318 amplifier.
    This patch adds the driver data for two nau8318 speaker amplifiers on
    SSP1 and nau8825 on SSP0 for ADL platform.
    
    The nau8315 and nau8318 are both Nuvoton Amp chips. They use the same
    Amp driver nau8315.c. The acpi_device_id for nau8315 is "NVTN2010",
    for nau8318 is "NVTN2012".
    The nau8825 is one of Nuvoton headset codec, and its acpi_device_id is
    "10508825".
    
    Signed-off-by: Ajye Huang <ajye_huang@compal.corp-partner.google.com>
    Acked-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20221222042624.557869-1-ajye_huang@compal.corp-partner.google.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/common/soc-acpi-intel-adl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-adl-match.c
@@ -450,6 +450,11 @@ static const struct snd_soc_acpi_codecs adl_lt6911_hdmi = {
 	.codecs = {"INTC10B0"}
 };
 
+static const struct snd_soc_acpi_codecs adl_nau8318_amp = {
+	.num_codecs = 1,
+	.codecs = {"NVTN2012"}
+};
+
 struct snd_soc_acpi_mach snd_soc_acpi_intel_adl_machines[] = {
 	{
 		.comp_ids = &adl_rt5682_rt5682s_hp,
@@ -506,6 +511,13 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_adl_machines[] = {
 		.machine_quirk = snd_soc_acpi_codec_list,
 		.quirk_data = &adl_rt1015p_amp,
 		.sof_tplg_filename = "sof-adl-rt1015-nau8825.tplg",
+	},
+	{
+		.id = "10508825",
+		.drv_name = "adl_nau8318_8825",
+		.machine_quirk = snd_soc_acpi_codec_list,
+		.quirk_data = &adl_nau8318_amp,
+		.sof_tplg_filename = "sof-adl-nau8318-nau8825.tplg",
 	},
 	{
 		.id = "10508825",
