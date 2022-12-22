39b2e121c5f93104ff1c0ee4750d64f88238a713
Cai YiWei <cyw@rock-chips.com>
2022-12-22 15:30:41 +0800

    media: rockchip: isp: wait isp0 end for unite mode
    Change-Id: Iab845b2439df202d3543883975c13aa9f27fee8e
    Signed-off-by: Cai YiWei <cyw@rock-chips.com>

--- drivers/media/platform/rockchip/isp/rkisp.c
+++ drivers/media/platform/rockchip/isp/rkisp.c
@@ -3941,6 +3941,16 @@ void rkisp_isp_isr(unsigned int isp_mis,
 			rkisp_write(dev, ISP3X_ISP_ICR, val, true);
 			v4l2_dbg(3, rkisp_debug, &dev->v4l2_dev,
 				 "left isp isr:0x%x\n", val);
+			if (isp_mis & CIF_ISP_FRAME && !(val & CIF_ISP_FRAME)) {
+				/* wait isp0 frame end */
+				int timeout = read_poll_timeout_atomic(rkisp_read,
+					val, val & CIF_ISP_FRAME, 20, 20 * 50, true, dev, ISP3X_ISP_RIS, true);
+
+				if (val)
+					rkisp_write(dev, ISP3X_ISP_ICR, val, true);
+				if (timeout)
+					dev_err(dev->dev, "wait isp end timeout\n");
+			}
 		}
 	}
 	v4l2_dbg(3, rkisp_debug, &dev->v4l2_dev,
