4fa6b43ba44bd26004b558549f7ad448a6255a64
Mark Brown <broonie@kernel.org>
2022-12-27 17:12:06 +0000

    arm64/signal: Always allocate SVE signal frames on SME only systems
    commit f26cd7372160da2eba31061d7943348ab9f2c01d upstream.
    
    Currently we only allocate space for SVE signal frames on systems that
    support SVE, meaning that SME only systems do not allocate a signal frame
    for streaming mode SVE state. Change the check so space is allocated if
    either feature is supported.
    
    Fixes: 85ed24dad290 ("arm64/sme: Implement streaming SVE signal handling")
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20221223-arm64-fix-sme-only-v1-3-938d663f69e5@kernel.org
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/kernel/signal.c
+++ arch/arm64/kernel/signal.c
@@ -729,7 +729,7 @@ static int setup_sigframe_layout(struct rt_sigframe_user_layout *user,
 			return err;
 	}
 
-	if (system_supports_sve()) {
+	if (system_supports_sve() || system_supports_sme()) {
 		unsigned int vq = 0;
 
 		if (add_all || test_thread_flag(TIF_SVE) ||
