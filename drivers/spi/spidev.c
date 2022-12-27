6c9d1fd52956c3148e847a214bae9102b1811de5
Vincent Tremblay <vincent@vtremblay.dev>
2022-12-27 09:10:08 -0500

    spidev: Add Silicon Labs SI3210 device compatible
    Add compatible string for Silicon Labs SI3210 device.
    
    Signed-off-by: Vincent Tremblay <vincent@vtremblay.dev>
    Link: https://lore.kernel.org/r/20221227141011.111410-2-vincent@vtremblay.dev
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -703,6 +703,7 @@ static const struct spi_device_id spidev_spi_ids[] = {
 	{ .name = "spi-petra" },
 	{ .name = "spi-authenta" },
 	{ .name = "em3581" },
+	{ .name = "si3210" },
 	{},
 };
 MODULE_DEVICE_TABLE(spi, spidev_spi_ids);
@@ -730,6 +731,7 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "cisco,spi-petra", .data = &spidev_of_check },
 	{ .compatible = "micron,spi-authenta", .data = &spidev_of_check },
 	{ .compatible = "silabs,em3581", .data = &spidev_of_check },
+	{ .compatible = "silabs,si3210", .data = &spidev_of_check },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
