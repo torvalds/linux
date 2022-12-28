6949aa0eadafec9f4e83920a798330025c049ce5
Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>
2022-12-28 21:22:49 +0200

    drm/i915: Check for integer truncation on the configuration of ttm place
    There is an impedance mismatch between the first/last valid page
    frame number of ttm place in unsigned and our memory/page accounting in
    unsigned long.
    As the object size is under the control of userspace, we have to be prudent
    and catch the conversion errors.
    To catch the implicit truncation as we switch from unsigned long to
    unsigned, we use overflows_type check and report E2BIG or overflow_type
    prior to the operation.
    
    v3: Not to change execution inside a macro. (Mauro)
        Add safe_conversion_gem_bug_on() macro and remove temporal
        SAFE_CONVERSION() macro.
    v4: Fix unhandled GEM_BUG_ON() macro call from safe_conversion_gem_bug_on()
    v6: Fix to follow general use case for GEM_BUG_ON(). (Jani)
    v7: Fix to use WARN_ON() macro where GEM_BUG_ON() macro was used. (Jani)
    v8: Replace safe_conversion() with check_assign() (Kees)
    v14: Split one macro of assignment with checking of overflow to two steps,
         first overflow check, and second assignment.
    
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Matthew Auld <matthew.auld@intel.com>
    Cc: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>
    Reviewed-by: Nirmoy Das <nirmoy.das@intel.com> (v2)
    Reviewed-by: Mauro Carvalho Chehab <mchehab@kernel.org> (v3)
    Reported-by: kernel test robot <lkp@intel.com>
    Reviewed-by: Andrzej Hajda <andrzej.hajda@intel.com> (v5)
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20221228192252.917299-4-gwan-gyeong.mun@intel.com

--- drivers/gpu/drm/i915/gem/i915_gem_ttm.c
+++ drivers/gpu/drm/i915/gem/i915_gem_ttm.c
@@ -140,13 +140,16 @@ i915_ttm_place_from_region(const struct intel_memory_region *mr,
 	if (flags & I915_BO_ALLOC_CONTIGUOUS)
 		place->flags |= TTM_PL_FLAG_CONTIGUOUS;
 	if (offset != I915_BO_INVALID_OFFSET) {
+		WARN_ON(overflows_type(offset >> PAGE_SHIFT, place->fpfn));
 		place->fpfn = offset >> PAGE_SHIFT;
+		WARN_ON(overflows_type(place->fpfn + (size >> PAGE_SHIFT), place->lpfn));
 		place->lpfn = place->fpfn + (size >> PAGE_SHIFT);
 	} else if (mr->io_size && mr->io_size < mr->total) {
 		if (flags & I915_BO_ALLOC_GPU_ONLY) {
 			place->flags |= TTM_PL_FLAG_TOPDOWN;
 		} else {
 			place->fpfn = 0;
+			WARN_ON(overflows_type(mr->io_size >> PAGE_SHIFT, place->lpfn));
 			place->lpfn = mr->io_size >> PAGE_SHIFT;
 		}
 	}
