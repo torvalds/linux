3366823ee075453225909d6c413d71ddb2f0c359
Jon Lin <jon.lin@rock-chips.com>
2023-01-03 14:43:44 +0800

    mtd: spi-nor: xtx: Support xt25q64d xt25q128d
    Change-Id: Ic41720b2602447e871ed5fffe39883662ba8a6c2
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/spi-nor/xtx.c
+++ drivers/mtd/spi-nor/xtx.c
@@ -8,10 +8,10 @@
 #include "core.h"
 
 static const struct flash_info xtx_parts[] = {
-	{ "XT25F64F", INFO(0x0b4017, 0, 64 * 1024, 128,
-			    SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
-	{ "XT25F128B", INFO(0x0b4018, 0, 64 * 1024, 256,
-			     SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	{ "XT25F64F", INFO(0x0b4017, 0, 64 * 1024, 128, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	{ "XT25F128B", INFO(0x0b4018, 0, 64 * 1024, 256, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	{ "xt25q64d", INFO(0x0b6017, 0, 64 * 1024, 128, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	{ "xt25q128d", INFO(0x0b6018, 0, 64 * 1024, 256, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 };
 
 const struct spi_nor_manufacturer spi_nor_xtx = {
