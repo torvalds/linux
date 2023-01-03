68714ad80829dc34b7db71819bd8b908ecdd7222
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2023-01-03 16:55:15 +0200

    clk: qcom: videocc-sm8250: switch to devm_pm_runtime_enable
    Switch to using the devm_pm_runtime_enable() instead of hand-coding
    corresponding action to call pm_runtime_disable().
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    Link: https://lore.kernel.org/r/20230103145515.1164020-22-dmitry.baryshkov@linaro.org

--- drivers/clk/qcom/videocc-sm8250.c
+++ drivers/clk/qcom/videocc-sm8250.c
@@ -361,19 +361,12 @@ static const struct of_device_id video_cc_sm8250_match_table[] = {
 };
 MODULE_DEVICE_TABLE(of, video_cc_sm8250_match_table);
 
-static void video_cc_sm8250_pm_runtime_disable(void *data)
-{
-	pm_runtime_disable(data);
-}
-
 static int video_cc_sm8250_probe(struct platform_device *pdev)
 {
 	struct regmap *regmap;
 	int ret;
 
-	pm_runtime_enable(&pdev->dev);
-
-	ret = devm_add_action_or_reset(&pdev->dev, video_cc_sm8250_pm_runtime_disable, &pdev->dev);
+	ret = devm_pm_runtime_enable(&pdev->dev);
 	if (ret)
 		return ret;
 
