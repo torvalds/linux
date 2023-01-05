82f76fcf5b08fdf9a3e5f9bbdea199a3d5b4d458
Jingyi Wang <quic_jingyw@quicinc.com>
2023-01-05 10:13:27 +0800

    soc: qcom: rq_stats: update the copyright
    Update copyright of rq_stats.c
    
    Change-Id: I8d2dace39548235a9e088b64c0d3e5155adb10bc
    Signed-off-by: Jingyi Wang <quic_jingyw@quicinc.com>

--- drivers/soc/qcom/rq_stats.c
+++ drivers/soc/qcom/rq_stats.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2010-2015, 2017, 2019-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/init.h>
