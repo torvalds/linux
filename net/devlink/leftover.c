e50ef40f9a9a35d8526d38e6b5a165178fa1857f
Jakub Kicinski <kuba@kernel.org>
2023-01-04 20:05:18 -0800

    devlink: rename devlink_netdevice_event -> devlink_port_netdevice_event
    To make the upcoming change a pure(er?) code move rename
    devlink_netdevice_event -> devlink_port_netdevice_event.
    This makes it clear that it only touches ports and doesn't
    belong cleanly in the core.
    
    Reviewed-by: Jiri Pirko <jiri@nvidia.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/devlink/leftover.c
+++ net/devlink/leftover.c
@@ -9942,8 +9942,8 @@ void devlink_set_features(struct devlink *devlink, u64 features)
 }
 EXPORT_SYMBOL_GPL(devlink_set_features);
 
-static int devlink_netdevice_event(struct notifier_block *nb,
-				   unsigned long event, void *ptr);
+static int devlink_port_netdevice_event(struct notifier_block *nb,
+					unsigned long event, void *ptr);
 
 /**
  *	devlink_alloc_ns - Allocate new devlink instance resources
@@ -9978,7 +9978,7 @@ struct devlink *devlink_alloc_ns(const struct devlink_ops *ops,
 	if (ret < 0)
 		goto err_xa_alloc;
 
-	devlink->netdevice_nb.notifier_call = devlink_netdevice_event;
+	devlink->netdevice_nb.notifier_call = devlink_port_netdevice_event;
 	ret = register_netdevice_notifier_net(net, &devlink->netdevice_nb);
 	if (ret)
 		goto err_register_netdevice_notifier;
@@ -10480,8 +10480,8 @@ void devlink_port_type_clear(struct devlink_port *devlink_port)
 }
 EXPORT_SYMBOL_GPL(devlink_port_type_clear);
 
-static int devlink_netdevice_event(struct notifier_block *nb,
-				   unsigned long event, void *ptr)
+static int devlink_port_netdevice_event(struct notifier_block *nb,
+					unsigned long event, void *ptr)
 {
 	struct net_device *netdev = netdev_notifier_info_to_dev(ptr);
 	struct devlink_port *devlink_port = netdev->devlink_port;
