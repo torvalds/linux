887975834dea744dde636dd73bbd1e597f211d7d
Benedict Wong <benedictwong@google.com>
2023-01-05 21:28:12 +0000

    Fix XFRM-I support for nested ESP tunnels
    [ Upstream commit b0355dbbf13c0052931dd14c38c789efed64d3de ]
    
    This change adds support for nested IPsec tunnels by ensuring that
    XFRM-I verifies existing policies before decapsulating a subsequent
    policies. Addtionally, this clears the secpath entries after policies
    are verified, ensuring that previous tunnels with no-longer-valid
    do not pollute subsequent policy checks.
    
    This is necessary especially for nested tunnels, as the IP addresses,
    protocol and ports may all change, thus not matching the previous
    policies. In order to ensure that packets match the relevant inbound
    templates, the xfrm_policy_check should be done before handing off to
    the inner XFRM protocol to decrypt and decapsulate.
    
    Notably, raw ESP/AH packets did not perform policy checks inherently,
    whereas all other encapsulated packets (UDP, TCP encapsulated) do policy
    checks after calling xfrm_input handling in the respective encapsulation
    layer.
    
    Test: Verified with additional Android Kernel Unit tests
    Signed-off-by: Benedict Wong <benedictwong@google.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/xfrm/xfrm_policy.c
+++ net/xfrm/xfrm_policy.c
@@ -3710,6 +3710,9 @@ int __xfrm_policy_check(struct sock *sk, int dir, struct sk_buff *skb,
 			goto reject;
 		}
 
+		if (if_id)
+			secpath_reset(skb);
+
 		xfrm_pols_put(pols, npols);
 		return 1;
 	}
