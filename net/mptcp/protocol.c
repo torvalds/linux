fbe9fa195ef2d3b93c4e11c6386174b880e27b74
Menglong Dong <imagedong@tencent.com>
2023-01-06 10:57:20 -0800

    mptcp: introduce 'sk' to replace 'sock->sk' in mptcp_listen()
    [ Upstream commit cfdcfeed6449d702825d249cb85346ecf56236fc ]
    
    'sock->sk' is used frequently in mptcp_listen(). Therefore, we can
    introduce the 'sk' and replace 'sock->sk' with it.
    
    Acked-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Menglong Dong <imagedong@tencent.com>
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Stable-dep-of: 0226436acf24 ("mptcp: do not rely on implicit state check in mptcp_listen()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -3746,12 +3746,13 @@ unlock:
 static int mptcp_listen(struct socket *sock, int backlog)
 {
 	struct mptcp_sock *msk = mptcp_sk(sock->sk);
+	struct sock *sk = sock->sk;
 	struct socket *ssock;
 	int err;
 
 	pr_debug("msk=%p", msk);
 
-	lock_sock(sock->sk);
+	lock_sock(sk);
 	ssock = __mptcp_nmpc_socket(msk);
 	if (!ssock) {
 		err = -EINVAL;
@@ -3759,16 +3760,16 @@ static int mptcp_listen(struct socket *sock, int backlog)
 	}
 
 	mptcp_token_destroy(msk);
-	inet_sk_state_store(sock->sk, TCP_LISTEN);
-	sock_set_flag(sock->sk, SOCK_RCU_FREE);
+	inet_sk_state_store(sk, TCP_LISTEN);
+	sock_set_flag(sk, SOCK_RCU_FREE);
 
 	err = ssock->ops->listen(ssock, backlog);
-	inet_sk_state_store(sock->sk, inet_sk_state_load(ssock->sk));
+	inet_sk_state_store(sk, inet_sk_state_load(ssock->sk));
 	if (!err)
-		mptcp_copy_inaddrs(sock->sk, ssock->sk);
+		mptcp_copy_inaddrs(sk, ssock->sk);
 
 unlock:
-	release_sock(sock->sk);
+	release_sock(sk);
 	return err;
 }
 
