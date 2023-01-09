d972c8c08f96518ff02efd87c4fef594a833f6ea
Juergen Gross <jgross@suse.com>
2023-01-09 16:09:22 +0100

    x86/mm: fix poking_init() for Xen PV guests
    commit 26ce6ec364f18d2915923bc05784084e54a5c4cc upstream.
    
    Commit 3f4c8211d982 ("x86/mm: Use mm_alloc() in poking_init()") broke
    the kernel for running as Xen PV guest.
    
    It seems as if the new address space is never activated before being
    used, resulting in Xen rejecting to accept the new CR3 value (the PGD
    isn't pinned).
    
    Fix that by adding the now missing call of paravirt_arch_dup_mmap() to
    poking_init(). That call was previously done by dup_mm()->dup_mmap() and
    it is a NOP for all cases but for Xen PV, where it is just doing the
    pinning of the PGD.
    
    Fixes: 3f4c8211d982 ("x86/mm: Use mm_alloc() in poking_init()")
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20230109150922.10578-1-jgross@suse.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/mm/init.c
+++ arch/x86/mm/init.c
@@ -27,6 +27,7 @@
 #include <asm/pti.h>
 #include <asm/text-patching.h>
 #include <asm/memtype.h>
+#include <asm/paravirt.h>
 
 /*
  * We need to define the tracepoints somewhere, and tlb.c
@@ -828,6 +829,9 @@ void __init poking_init(void)
 
 	poking_mm = copy_init_mm();
 	BUG_ON(!poking_mm);
+
+	/* Xen PV guests need the PGD to be pinned. */
+	paravirt_arch_dup_mmap(NULL, poking_mm);
 
 	/*
 	 * Randomize the poking address, but make sure that the following page
