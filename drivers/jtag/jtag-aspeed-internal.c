af8e8c164ed65395700e7e30afdbec3293d87877
Billy Tsai <billy_tsai@aspeedtech.com>
2023-01-10 14:38:09 +0800

    jtag: aspeed: Add the dummy read to ensure the write complete.
    The dummy read can ensure the register write has been completed to avoid
    bus delays affecting the real TCK frequecy.
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: Ibc1f12b624b450052c91ce6151ebaa6b60a46ef5

--- drivers/jtag/jtag-aspeed-internal.c
+++ drivers/jtag/jtag-aspeed-internal.c
@@ -245,6 +245,7 @@ static u8 TCK_Cycle(struct aspeed_jtag_info *aspeed_jtag, u8 TMS, u8 TDI)
 			  JTAG_SW_MODE_EN | (TMS * JTAG_SW_MODE_TMS) |
 				  (TDI * JTAG_SW_MODE_TDIO),
 			  ASPEED_JTAG_SW);
+	aspeed_jtag_read(aspeed_jtag, ASPEED_JTAG_SW);
 
 	/* Target device have their operating frequency*/
 	ndelay(aspeed_jtag->sw_delay);
@@ -255,6 +256,7 @@ static u8 TCK_Cycle(struct aspeed_jtag_info *aspeed_jtag, u8 TMS, u8 TDI)
 				  (TMS * JTAG_SW_MODE_TMS) |
 				  (TDI * JTAG_SW_MODE_TDIO),
 			  ASPEED_JTAG_SW);
+	aspeed_jtag_read(aspeed_jtag, ASPEED_JTAG_SW);
 
 	ndelay(aspeed_jtag->sw_delay);
 	/* Sampled TDI(slave, master's TDO) on the rising edge */
