86bd0c4a2a5dc4265884648cb92c681646509692
Bart Van Assche <bvanassche@acm.org>
2023-01-12 15:42:13 -0800

    scsi: ufs: exynos: Fix DMA alignment for PAGE_SIZE != 4096
    The Exynos UFS controller only supports scatter/gather list elements that
    are aligned on a 4 KiB boundary. Fix DMA alignment in case PAGE_SIZE !=
    4096. Rename UFSHCD_QUIRK_ALIGN_SG_WITH_PAGE_SIZE into
    UFSHCD_QUIRK_4KB_DMA_ALIGNMENT.
    
    Cc: Kiwoong Kim <kwmad.kim@samsung.com>
    Fixes: 2b2bfc8aa519 ("scsi: ufs: Introduce a quirk to allow only page-aligned sg entries")
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Reviewed-by: Alim Akhtar <alim.akhtar@samsung.com>
    Tested-by: Alim Akhtar <alim.akhtar@samsung.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/ufs/core/ufshcd.c
+++ drivers/ufs/core/ufshcd.c
@@ -5107,8 +5107,8 @@ static int ufshcd_slave_configure(struct scsi_device *sdev)
 	ufshcd_hpb_configure(hba, sdev);
 
 	blk_queue_update_dma_pad(q, PRDT_DATA_BYTE_COUNT_PAD - 1);
-	if (hba->quirks & UFSHCD_QUIRK_ALIGN_SG_WITH_PAGE_SIZE)
-		blk_queue_update_dma_alignment(q, PAGE_SIZE - 1);
+	if (hba->quirks & UFSHCD_QUIRK_4KB_DMA_ALIGNMENT)
+		blk_queue_update_dma_alignment(q, 4096 - 1);
 	/*
 	 * Block runtime-pm until all consumers are added.
 	 * Refer ufshcd_setup_links().
