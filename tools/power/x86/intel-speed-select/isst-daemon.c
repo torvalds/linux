364ba3b7115087ab8960473a94cedc7336f345e2
Zhang Rui <rui.zhang@intel.com>
2023-01-12 14:43:35 +0800

    tools/power/x86/intel-speed-select: Handle open() failure case
    Add handling for open() failure case to make sure a valid file
    descriptor is passed to dup().
    
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- tools/power/x86/intel-speed-select/isst-daemon.c
+++ tools/power/x86/intel-speed-select/isst-daemon.c
@@ -174,6 +174,8 @@ static void daemonize(char *rundir, char *pidfile)
 		close(i);
 
 	i = open("/dev/null", O_RDWR);
+	if (i < 0)
+		exit(EXIT_FAILURE);
 	ret = dup(i);
 	if (ret == -1)
 		exit(EXIT_FAILURE);
