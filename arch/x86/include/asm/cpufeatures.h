78335aac6156eadad0025ab34469e2adcc60218b
Babu Moger <babu.moger@amd.com>
2023-01-13 09:20:30 -0600

    x86/cpufeatures: Add Bandwidth Monitoring Event Configuration feature flag
    Newer AMD processors support the new feature Bandwidth Monitoring Event
    Configuration (BMEC).
    
    The feature support is identified via CPUID Fn8000_0020_EBX_x0[3]: EVT_CFG -
    Bandwidth Monitoring Event Configuration (BMEC)
    
    The bandwidth monitoring events mbm_total_bytes and mbm_local_bytes are set to
    count all the total and local reads/writes, respectively. With the introduction
    of slow memory, the two counters are not enough to count all the different types
    of memory events. Therefore, BMEC provides the option to configure
    mbm_total_bytes and mbm_local_bytes to count the specific type of events.
    
    Each BMEC event has a configuration MSR which contains one field for each
    bandwidth type that can be used to configure the bandwidth event to track any
    combination of supported bandwidth types. The event will count requests from
    every bandwidth type bit that is set in the corresponding configuration
    register.
    
    Following are the types of events supported:
    
      ====    ========================================================
      Bits    Description
      ====    ========================================================
      6       Dirty Victims from the QOS domain to all types of memory
      5       Reads to slow memory in the non-local NUMA domain
      4       Reads to slow memory in the local NUMA domain
      3       Non-temporal writes to non-local NUMA domain
      2       Non-temporal writes to local NUMA domain
      1       Reads to memory in the non-local NUMA domain
      0       Reads to memory in the local NUMA domain
      ====    ========================================================
    
    By default, the mbm_total_bytes configuration is set to 0x7F to count
    all the event types and the mbm_local_bytes configuration is set to 0x15 to
    count all the local memory events.
    
    Feature description is available in the specification, "AMD64 Technology
    Platform Quality of Service Extensions, Revision: 1.03 Publication" at
    https://bugzilla.kernel.org/attachment.cgi?id=301365
    
    Signed-off-by: Babu Moger <babu.moger@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>
    Link: https://lore.kernel.org/r/20230113152039.770054-5-babu.moger@amd.com

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -308,6 +308,7 @@
 #define X86_FEATURE_CALL_DEPTH		(11*32+19) /* "" Call depth tracking for RSB stuffing */
 #define X86_FEATURE_MSR_TSX_CTRL	(11*32+20) /* "" MSR IA32_TSX_CTRL (Intel) implemented */
 #define X86_FEATURE_SMBA		(11*32+21) /* "" Slow Memory Bandwidth Allocation */
+#define X86_FEATURE_BMEC		(11*32+22) /* "" Bandwidth Monitoring Event Configuration */
 
 /* Intel-defined CPU features, CPUID level 0x00000007:1 (EAX), word 12 */
 #define X86_FEATURE_AVX_VNNI		(12*32+ 4) /* AVX VNNI instructions */
