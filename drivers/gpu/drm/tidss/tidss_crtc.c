a391a98989610ac0d59acb33200f666dc5dda1b4
Thomas Zimmermann <tzimmermann@suse.de>
2023-01-16 14:12:32 +0100

    drm/tidss: Remove unnecessary include statements for drm_crtc_helper.h
    Several source files include drm_crtc_helper.h without needing it or
    only to get its transitive include statements; leading to unnecessary
    compile-time dependencies.
    
    Directly include required headers and drop drm_crtc_helper.h where
    possible.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Sam Ravnborg <sam@ravnborg.org>
    Acked-by: Jani Nikula <jani.nikula@intel.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230116131235.18917-20-tzimmermann@suse.de

--- drivers/gpu/drm/tidss/tidss_crtc.c
+++ drivers/gpu/drm/tidss/tidss_crtc.c
@@ -7,7 +7,6 @@
 #include <drm/drm_atomic.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_crtc.h>
-#include <drm/drm_crtc_helper.h>
 #include <drm/drm_gem_dma_helper.h>
 #include <drm/drm_vblank.h>
 
