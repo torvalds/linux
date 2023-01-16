cde3d37b19dfddc2dd1bb238a5eaaae09a07b5c5
Thomas Zimmermann <tzimmermann@suse.de>
2023-01-16 14:12:15 +0100

    drm: Remove unnecessary include statements for drm_crtc_helper.h
    Several DRM core and helper source files include drm_crtc_helper.h
    without needing it or only to get its transitive include statements;
    leading to unnecessary compile-time dependencies.
    
    Directly include required headers and drop drm_crtc_helper.h where
    possible. The header file, drm_fixed.h, includes <linux/kernel.h>
    for lower_32_bits().
    
    v2:
    	* include drm_crtc_helper.h in drm_crtc_helper.c (Sam)
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Sam Ravnborg <sam@ravnborg.org>
    Acked-by: Jani Nikula <jani.nikula@intel.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230116131235.18917-3-tzimmermann@suse.de

--- drivers/gpu/drm/drm_plane_helper.c
+++ drivers/gpu/drm/drm_plane_helper.c
@@ -28,7 +28,6 @@
 #include <drm/drm_atomic.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_atomic_uapi.h>
-#include <drm/drm_crtc_helper.h>
 #include <drm/drm_device.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_encoder.h>
