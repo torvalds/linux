7fe3ead722a0d9823c2393a535b787bb268bb94a
Thomas Zimmermann <tzimmermann@suse.de>
2023-01-16 14:12:31 +0100

    drm/sun4i: Remove unnecessary include statements for drm_crtc_helper.h
    Several source files include drm_crtc_helper.h without needing it or
    only to get its transitive include statements; leading to unnecessary
    compile-time dependencies.
    
    Directly include required headers and drop drm_crtc_helper.h where
    possible.
    
    v2:
    	* keep includes sorted (Sam)
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Sam Ravnborg <sam@ravnborg.org>
    Acked-by: Jani Nikula <jani.nikula@intel.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230116131235.18917-19-tzimmermann@suse.de

--- drivers/gpu/drm/sun4i/sun8i_dw_hdmi.c
+++ drivers/gpu/drm/sun4i/sun8i_dw_hdmi.c
@@ -8,7 +8,7 @@
 #include <linux/of_device.h>
 #include <linux/platform_device.h>
 
-#include <drm/drm_crtc_helper.h>
+#include <drm/drm_modeset_helper_vtables.h>
 #include <drm/drm_of.h>
 #include <drm/drm_simple_kms_helper.h>
 
