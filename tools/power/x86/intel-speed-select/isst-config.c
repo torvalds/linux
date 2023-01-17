d1fcb7493fc36a192b19fb8b046c416d05b6d6fe
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2023-01-17 14:40:34 -0800

    tools/power/x86/intel-speed-select: v1.14 release
    This release adds following change:
    - Minor fixes for coverity static analysis
    - Don't read cpufreq on offline CPUs
    - SST turbo-freq enable on auto mode when user disables SMT from
    kernel command line
    - Fix uncore frequency display
    - Set uncore frequency max/min limits on perf level change
    
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- tools/power/x86/intel-speed-select/isst-config.c
+++ tools/power/x86/intel-speed-select/isst-config.c
@@ -15,7 +15,7 @@ struct process_cmd_struct {
 	int arg;
 };
 
-static const char *version_str = "v1.13";
+static const char *version_str = "v1.14";
 
 static const int supported_api_ver = 1;
 static struct isst_if_platform_info isst_platform_info;
