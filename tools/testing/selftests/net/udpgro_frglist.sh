3c107f36db061603bee7564fbd6388b1f1879fd3
Hangbin Liu <liuhangbin@gmail.com>
2023-01-18 10:09:27 +0800

    selftests/net: mv bpf/nat6to4.c to net folder
    There are some issues with the bpf/nat6to4.c building.
    
    1. It use TEST_CUSTOM_PROGS, which will add the nat6to4.o to
       kselftest-list file and run by common run_tests.
    2. When building the test via `make -C tools/testing/selftests/
       TARGETS="net"`, the nat6to4.o will be build in selftests/net/bpf/
       folder. But in test udpgro_frglist.sh it refers to ../bpf/nat6to4.o.
       The correct path should be ./bpf/nat6to4.o.
    3. If building the test via `make -C tools/testing/selftests/ TARGETS="net"
       install`. The nat6to4.o will be installed to kselftest_install/net/
       folder. Then the udpgro_frglist.sh should refer to ./nat6to4.o.
    
    To fix the confusing test path, let's just move the nat6to4.c to net folder
    and build it as TEST_GEN_FILES.
    
    Fixes: edae34a3ed92 ("selftests net: add UDP GRO fraglist + bpf self-tests")
    Tested-by: Björn Töpel <bjorn@kernel.org>
    Signed-off-by: Hangbin Liu <liuhangbin@gmail.com>
    Link: https://lore.kernel.org/r/20230118020927.3971864-1-liuhangbin@gmail.com
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- tools/testing/selftests/net/udpgro_frglist.sh
+++ tools/testing/selftests/net/udpgro_frglist.sh
@@ -40,8 +40,8 @@ run_one() {
 
 	ip -n "${PEER_NS}" link set veth1 xdp object ${BPF_FILE} section xdp
 	tc -n "${PEER_NS}" qdisc add dev veth1 clsact
-	tc -n "${PEER_NS}" filter add dev veth1 ingress prio 4 protocol ipv6 bpf object-file ../bpf/nat6to4.o section schedcls/ingress6/nat_6  direct-action
-	tc -n "${PEER_NS}" filter add dev veth1 egress prio 4 protocol ip bpf object-file ../bpf/nat6to4.o section schedcls/egress4/snat4 direct-action
+	tc -n "${PEER_NS}" filter add dev veth1 ingress prio 4 protocol ipv6 bpf object-file nat6to4.o section schedcls/ingress6/nat_6  direct-action
+	tc -n "${PEER_NS}" filter add dev veth1 egress prio 4 protocol ip bpf object-file nat6to4.o section schedcls/egress4/snat4 direct-action
         echo ${rx_args}
 	ip netns exec "${PEER_NS}" ./udpgso_bench_rx ${rx_args} -r &
 
@@ -88,8 +88,8 @@ if [ ! -f ${BPF_FILE} ]; then
 	exit -1
 fi
 
-if [ ! -f bpf/nat6to4.o ]; then
-	echo "Missing nat6to4 helper. Build bpfnat6to4.o selftest first"
+if [ ! -f nat6to4.o ]; then
+	echo "Missing nat6to4 helper. Build bpf nat6to4.o selftest first"
 	exit -1
 fi
 
