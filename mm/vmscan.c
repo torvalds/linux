014c372cc30eb17dbf213d9d56317e4988976599
T.J. Alumbaugh <talumbau@google.com>
2023-01-18 00:18:21 +0000

    UPSTREAM: mm: multi-gen LRU: section for working set protection
    Patch series "mm: multi-gen LRU: improve".
    
    This patch series improves a few MGLRU functions, collects related
    functions, and adds additional documentation.
    
    This patch (of 7):
    
    Add a section for working set protection in the code and the design doc.
    The admin doc already contains its usage.
    
    Link: https://lkml.kernel.org/r/20230118001827.1040870-1-talumbau@google.com
    Link: https://lkml.kernel.org/r/20230118001827.1040870-2-talumbau@google.com
    Change-Id: I65599075fd42951db7739a2ab7cee78516e157b3
    Signed-off-by: T.J. Alumbaugh <talumbau@google.com>
    Cc: Yu Zhao <yuzhao@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 7b8144e63d84716f16a1b929e0c7e03ae5c4d5c1)
    Bug: 274865848
    Signed-off-by: T.J. Mercier <tjmercier@google.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -4457,6 +4457,10 @@ done:
 	return true;
 }
 
+/******************************************************************************
+ *                          working set protection
+ ******************************************************************************/
+
 static bool lruvec_is_sizable(struct lruvec *lruvec, struct scan_control *sc)
 {
 	int gen, type, zone;
