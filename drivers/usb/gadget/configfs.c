1f6f0571ac7254e785f47058ad1df75ce7b0d0e5
Lee Jones <joneslee@google.com>
2023-01-18 15:49:24 +0000

    ANDROID: usb: f_accessory: Check buffer size when initialised via composite
    When communicating with accessory devices via USBFS, the initialisation
    call-stack looks like:
    
      ConfigFS > Gadget ConfigFS > UDC > Gadget ConfigFS > Composite
    
    Eventually ending up in composite_dev_prepare() where memory for the
    data buffer is allocated and initialised.  The default size used for the
    allocation is USB_COMP_EP0_BUFSIZ (4k).  When handling bulk transfers,
    acc_ctrlrequest() needs to be able to handle buffers up to
    BULK_BUFFER_SIZE (16k).  Instead of adding new generic attributes to
    'struct usb_request' to track the size of the allocated buffer, we can
    simply split off the affected thread of execution to travel via a
    knowledgeable abstracted function acc_ctrlrequest_composite() where we
    can complete the necessary specific checks.
    
    Bug: 264029575
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: Ia1280f85499621d3fa57f7262b4a2c80f4be7773

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -16,7 +16,7 @@
 #include <linux/usb/ch9.h>
 
 #ifdef CONFIG_USB_CONFIGFS_F_ACC
-extern int acc_ctrlrequest(struct usb_composite_dev *cdev,
+extern int acc_ctrlrequest_composite(struct usb_composite_dev *cdev,
 				const struct usb_ctrlrequest *ctrl);
 void acc_disconnect(void);
 #endif
@@ -1560,7 +1560,7 @@ static int android_setup(struct usb_gadget *gadget,
 
 #ifdef CONFIG_USB_CONFIGFS_F_ACC
 	if (value < 0)
-		value = acc_ctrlrequest(cdev, c);
+		value = acc_ctrlrequest_composite(cdev, c);
 #endif
 
 	if (value < 0)
