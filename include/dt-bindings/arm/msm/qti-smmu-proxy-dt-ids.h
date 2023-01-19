044cc7a25ffb58bf134efa95cf16186888c42c9a
Chris Goldsworthy <quic_cgoldswo@quicinc.com>
2023-01-19 15:37:07 -0800

    qti_smmu_proxy: Add proxy DT IDs
    Add the proxy context bank IDs that clients will use in their DT
    nodes.
    
    Change-Id: Iad1e29c47ec8d465eff75abf9761d0e0b74ff7b7
    Signed-off-by: Chris Goldsworthy <quic_cgoldswo@quicinc.com>

---
+++ include/dt-bindings/arm/msm/qti-smmu-proxy-dt-ids.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef _QTI_SMMU_PROXY_DT_IDS_H
+#define _QTI_SMMU_PROXY_DT_IDS_H
+
+#define QTI_SMMU_PROXY_CAMERA_CB 0
+#define QTI_SMMU_PROXY_DISPLAY_CB 1
+#define QTI_SMMU_PROXY_EVA_CB 2
+#define QTI_SMMU_PROXY_CB_IDS_LEN 3
+
+#endif /* _QTI_SMMU_PROXY_DT_IDS_H */
