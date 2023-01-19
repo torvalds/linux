fa4a15c85c849e92257da6dbffeb1e3a6399fd7b
Jacob Keller <jacob.e.keller@intel.com>
2023-01-18 17:16:51 -0800

    ice: introduce clear_reset_state operation
    When hardware is reset, the VF relies on the VFGEN_RSTAT register to detect
    when the VF is finished resetting. This is a tri-state register where 0
    indicates a reset is in progress, 1 indicates the hardware is done
    resetting, and 2 indicates that the software is done resetting.
    
    Currently the PF driver relies on the device hardware resetting VFGEN_RSTAT
    when a global reset occurs. This works ok, but it does mean that the VF
    might not immediately notice a reset when the driver first detects that the
    global reset is occurring.
    
    This is also problematic for Scalable IOV, because there is no read/write
    equivalent VFGEN_RSTAT register for the Scalable VSI type. Instead, the
    Scalable IOV VFs will need to emulate this register.
    
    To support this, introduce a new VF operation, clear_reset_state, which is
    called when the PF driver first detects a global reset. The Single Root IOV
    implementation can just write to VFGEN_RSTAT to ensure it's cleared
    immediately, without waiting for the actual hardware reset to begin. The
    Scalable IOV implementation will use this as part of its tracking of the
    reset status to allow properly reporting the emulated VFGEN_RSTAT to the VF
    driver.
    
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    Reviewed-by: Paul Menzel <pmenzel@molgen.mpg.de>
    Tested-by: Marek Szlosek <marek.szlosek@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>

--- drivers/net/ethernet/intel/ice/ice_vf_lib.c
+++ drivers/net/ethernet/intel/ice/ice_vf_lib.c
@@ -717,12 +717,22 @@ out_unlock:
  * ice_set_vf_state_qs_dis - Set VF queues state to disabled
  * @vf: pointer to the VF structure
  */
-void ice_set_vf_state_qs_dis(struct ice_vf *vf)
+static void ice_set_vf_state_qs_dis(struct ice_vf *vf)
 {
 	/* Clear Rx/Tx enabled queues flag */
 	bitmap_zero(vf->txq_ena, ICE_MAX_RSS_QS_PER_VF);
 	bitmap_zero(vf->rxq_ena, ICE_MAX_RSS_QS_PER_VF);
 	clear_bit(ICE_VF_STATE_QS_ENA, vf->vf_states);
+}
+
+/**
+ * ice_set_vf_state_dis - Set VF state to disabled
+ * @vf: pointer to the VF structure
+ */
+void ice_set_vf_state_dis(struct ice_vf *vf)
+{
+	ice_set_vf_state_qs_dis(vf);
+	vf->vf_ops->clear_reset_state(vf);
 }
 
 /* Private functions only accessed from other virtualization files */
