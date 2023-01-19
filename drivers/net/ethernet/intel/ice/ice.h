c7ef8221ca7d8dfdf679a68b2fc3187f42a652b0
Arkadiusz Kubalewski <arkadiusz.kubalewski@intel.com>
2023-01-18 16:58:36 -0800

    ice: use GNSS subsystem instead of TTY
    Previously support for GNSS was implemented as a TTY driver, it allowed
    to access GNSS receiver on /dev/ttyGNSS_<bus><func>.
    
    Use generic GNSS subsystem API instead of implementing own TTY driver.
    The receiver is accessible on /dev/gnss<id>. In case of multiple receivers
    in the OS, correct device can be found by enumerating either:
    - /sys/class/net/<eth port>/device/gnss/
    - /sys/class/gnss/gnss<id>/device/
    
    Using GNSS subsystem is superior to implementing own TTY driver, as the
    GNSS subsystem was designed solely for this purpose. It also implements
    TTY driver but in a common and defined way.
    
    From user perspective, there is no difference in communicating with a
    device, except new path to the device shall be used. The device will
    provide same information to the userspace as the old one, and can be used
    in the same way, i.e.:
    old # gpsmon /dev/ttyGNSS_2100_0
    new # gpsmon /dev/gnss0
    There is no other impact on userspace tools.
    
    User expecting onboard GNSS receiver support is required to enable
    CONFIG_GNSS=y/m in kernel config.
    
    Reviewed-by: Alexander Lobakin <alexandr.lobakin@intel.com>
    Signed-off-by: Karol Kolacinski <karol.kolacinski@intel.com>
    Signed-off-by: Michal Michalik <michal.michalik@intel.com>
    Signed-off-by: Arkadiusz Kubalewski <arkadiusz.kubalewski@intel.com>
    Tested-by: Gurucharan G <gurucharanx.g@intel.com> (A Contingent worker at Intel)
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/intel/ice/ice.h
+++ drivers/net/ethernet/intel/ice/ice.h
@@ -39,6 +39,7 @@
 #include <linux/avf/virtchnl.h>
 #include <linux/cpu_rmap.h>
 #include <linux/dim.h>
+#include <linux/gnss.h>
 #include <net/pkt_cls.h>
 #include <net/tc_act/tc_mirred.h>
 #include <net/tc_act/tc_gact.h>
@@ -565,9 +566,8 @@ struct ice_pf {
 	struct mutex adev_mutex;	/* lock to protect aux device access */
 	u32 msg_enable;
 	struct ice_ptp ptp;
-	struct tty_driver *ice_gnss_tty_driver;
-	struct tty_port *gnss_tty_port[ICE_GNSS_TTY_MINOR_DEVICES];
-	struct gnss_serial *gnss_serial[ICE_GNSS_TTY_MINOR_DEVICES];
+	struct gnss_serial *gnss_serial;
+	struct gnss_device *gnss_dev;
 	u16 num_rdma_msix;		/* Total MSIX vectors for RDMA driver */
 	u16 rdma_base_vector;
 
