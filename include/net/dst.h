c35badfe0d91b25312d195422c6525c89faf925e
Greg Kroah-Hartman <gregkh@google.com>
2023-01-19 11:36:11 +0000

    Revert "net: add atomic_long_t to net_device_stats fields"
    This reverts commit 037db10e3f93f0d51ab3f52fbdaa7e67a7edd0b8 which is
    commit 6c1c5097781f563b70a81683ea6fdac21637573b upstream.
    
    It breaks the CRC checking for the Android KABI (doesn't really break
    any actual ABI, but it isn't nice.)  As this isn't needed for Android
    systems at this point in time, just revert it to keep the CRC abi
    stable.
    
    If it is needed to come back, it can be reworked to be a CRC-abi neutral
    change if so desired.
    
    Bug: 161946584
    Fixes: 037db10e3f93 ("net: add atomic_long_t to net_device_stats fields")
    Cc: Eric Dumazet <edumazet@google.com>
    Change-Id: I9d3d9fb55ab5482a4ea92fcc4a2fb70c4471edec
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- include/net/dst.h
+++ include/net/dst.h
@@ -356,8 +356,9 @@ static inline void __skb_tunnel_rx(struct sk_buff *skb, struct net_device *dev,
 static inline void skb_tunnel_rx(struct sk_buff *skb, struct net_device *dev,
 				 struct net *net)
 {
-	DEV_STATS_INC(dev, rx_packets);
-	DEV_STATS_ADD(dev, rx_bytes, skb->len);
+	/* TODO : stats should be SMP safe */
+	dev->stats.rx_packets++;
+	dev->stats.rx_bytes += skb->len;
 	__skb_tunnel_rx(skb, dev, net);
 }
 
