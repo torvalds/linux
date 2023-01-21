dcff8b7ca92d724bdaf474a3fa37a7748377813a
fengwk <fengwk94@gmail.com>
2023-01-22 01:51:06 +0800

    ASoC: amd: yc: Add Xiaomi Redmi Book Pro 15 2022 into DMI table
    This model requires an additional detection quirk to enable the
    internal microphone - BIOS doesn't seem to support AcpDmicConnected
    (nothing in acpidump output).
    
    Signed-off-by: fengwk <fengwk94@gmail.com>
    Link: https://lore.kernel.org/r/Y8wmCutc74j/tyHP@arch
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/yc/acp6x-mach.c
+++ sound/soc/amd/yc/acp6x-mach.c
@@ -230,6 +230,13 @@ static const struct dmi_system_id yc_acp_quirk_table[] = {
 	{
 		.driver_data = &acp6x_card,
 		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "TIMI"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "Redmi Book Pro 15 2022"),
+		}
+	},
+	{
+		.driver_data = &acp6x_card,
+		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "Razer"),
 			DMI_MATCH(DMI_PRODUCT_NAME, "Blade 14 (2022) - RZ09-0427"),
 		}
