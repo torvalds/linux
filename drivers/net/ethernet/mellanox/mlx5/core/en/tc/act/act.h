d0cc0853640d81397572d8152d068cd673a3e739
Paul Blakey <paulb@nvidia.com>
2023-01-23 10:46:10 +0200

    net/mlx5e: TC, Remove multiple ct actions limitation
    Multiple ct actions was not supported when only chain was
    restored on misses, as CT is a modifying action which could
    modify the packet and cause the sw ct rule to not match again
    and continue processing.
    
    Now that the misses continue from the relevant tc ct action
    in software, this case is supported.
    
    Remove this limitation.
    
    Signed-off-by: Paul Blakey <paulb@nvidia.com>
    Reviewed-by: Roi Dayan <roid@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en/tc/act/act.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en/tc/act/act.h
@@ -17,7 +17,6 @@ struct mlx5e_tc_act_parse_state {
 	struct mlx5e_tc_flow *flow;
 	struct netlink_ext_ack *extack;
 	u32 actions;
-	bool ct;
 	bool encap;
 	bool decap;
 	bool mpls_push;
--- drivers/net/ethernet/mellanox/mlx5/core/en/tc/act/ct.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en/tc/act/ct.c
@@ -5,23 +5,6 @@
 #include "en/tc_priv.h"
 #include "en/tc_ct.h"
 
-static bool
-tc_act_can_offload_ct(struct mlx5e_tc_act_parse_state *parse_state,
-		      const struct flow_action_entry *act,
-		      int act_index,
-		      struct mlx5_flow_attr *attr)
-{
-	bool clear_action = act->ct.action & TCA_CT_ACT_CLEAR;
-	struct netlink_ext_ack *extack = parse_state->extack;
-
-	if (parse_state->ct && !clear_action) {
-		NL_SET_ERR_MSG_MOD(extack, "Multiple CT actions are not supported");
-		return false;
-	}
-
-	return true;
-}
-
 static int
 tc_act_parse_ct(struct mlx5e_tc_act_parse_state *parse_state,
 		const struct flow_action_entry *act,
@@ -71,7 +54,6 @@ tc_act_is_missable_ct(const struct flow_action_entry *act)
 }
 
 struct mlx5e_tc_act mlx5e_tc_act_ct = {
-	.can_offload = tc_act_can_offload_ct,
 	.parse_action = tc_act_parse_ct,
 	.post_parse = tc_act_post_parse_ct,
 	.is_multi_table_act = tc_act_is_multi_table_act_ct,
