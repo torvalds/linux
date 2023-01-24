876ab6c0157ac343e86957c3298a6b6179549a52
Mario Tesi <mario.tesi@st.com>
2023-01-24 15:40:53 +0100

    drivers:iio:stm:imu: add support to lsm6dsv16bx STMEMS IMU sensor
    Added support for the imu lsm6dsv16bx sensor. The current driver
    version supports the following features:
     - Acc and Gyro in continuous mode supported by the internal HW FIFO
     - QVAR sensor for presence and touch detection, data of QVAR are
       managed internally by HW FIFO.
     - SFLP Embedded Sensor Fusion Function Game Rotation vector sensor.
     - TDM slave interface configuration for accelerometer data.
     - MLC / FSM programmable functions.
     - Free fall, step counter, step detection, wake-up, significant
       motion, tilt, tap, double tap, 6D and sleep change embedded
       functions event detection.
    
    Signed-off-by: Mario Tesi <mario.tesi@st.com>
    Change-Id: I1f2ad688261cba90b5c3633ccd9fc6fb0301d771

---
+++ drivers/iio/stm/imu/st_lsm6dsv16bx/st_lsm6dsv16bx_embfunc.c
@@ -0,0 +1,579 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * STMicroelectronics st_lsm6dsv16bx embedded function sensor driver
+ *
+ * MEMS Software Solutions Team
+ *
+ * Copyright 2023 STMicroelectronics Inc.
+ */
+
+#include <linux/iio/buffer.h>
+#include <linux/iio/events.h>
+#include <linux/iio/iio.h>
+#include <linux/iio/sysfs.h>
+#include <linux/iio/trigger.h>
+#include <linux/iio/triggered_buffer.h>
+#include <linux/iio/trigger_consumer.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+#include "st_lsm6dsv16bx.h"
+
+/**
+ * Step Counter IIO channels description
+ *
+ * Step Counter exports to IIO framework the following data channels:
+ * Step Counters (16 bit unsigned in little endian)
+ * Timestamp (64 bit signed in little endian)
+ * Step Counter exports to IIO framework the following event channels:
+ * Flush event done
+ */
+static const struct iio_chan_spec st_lsm6dsv16bx_step_counter_channels[] = {
+	{
+		.type = STM_IIO_STEP_COUNTER,
+		.scan_index = 0,
+		.scan_type = {
+			.sign = 'u',
+			.realbits = 16,
+			.storagebits = 16,
+			.endianness = IIO_LE,
+		},
+	},
+	ST_LSM6DSV16BX_EVENT_CHANNEL(STM_IIO_STEP_COUNTER, flush),
+	IIO_CHAN_SOFT_TIMESTAMP(1),
+};
+
+/**
+ * Step Detector IIO channels description
+ *
+ * Step Detector exports to IIO framework the following event channels:
+ * Step detection event detection
+ */
+static const struct iio_chan_spec st_lsm6dsv16bx_step_detector_channels[] = {
+	ST_LSM6DSV16BX_EVENT_CHANNEL(IIO_STEPS, thr),
+};
+
+/**
+ * Significant Motion IIO channels description
+ *
+ * Significant Motion exports to IIO framework the following event
+ * channels:
+ * Significant Motion event detection
+ */
+static const struct iio_chan_spec st_lsm6dsv16bx_sign_motion_channels[] = {
+	ST_LSM6DSV16BX_EVENT_CHANNEL(STM_IIO_SIGN_MOTION, thr),
+};
+
+/**
+ * Tilt IIO channels description
+ *
+ * Tilt exports to IIO framework the following event channels:
+ * Tilt event detection
+ */
+static const struct iio_chan_spec st_lsm6dsv16bx_tilt_channels[] = {
+	ST_LSM6DSV16BX_EVENT_CHANNEL(STM_IIO_TILT, thr),
+};
+
+static const unsigned long st_lsm6dsv16bx_embfunc_available_scan_masks[] = {
+	BIT(0), 0x0
+};
+
+static int
+st_lsm6dsv16bx_embfunc_set_enable(struct st_lsm6dsv16bx_sensor *sensor,
+				  u8 mask, u8 irq_mask, bool enable)
+{
+	struct st_lsm6dsv16bx_hw *hw = sensor->hw;
+	u8 int_reg = hw->int_pin == 1 ? ST_LSM6DSV16BX_REG_EMB_FUNC_INT1_ADDR :
+					ST_LSM6DSV16BX_REG_EMB_FUNC_INT2_ADDR;
+	int err;
+
+	err = st_lsm6dsv16bx_sensor_set_enable(sensor, enable);
+	if (err < 0)
+		return err;
+
+	mutex_lock(&hw->page_lock);
+	err = st_lsm6dsv16bx_set_page_access(hw,
+					ST_LSM6DSV16BX_EMB_FUNC_REG_ACCESS_MASK,
+					1);
+	if (err < 0)
+		goto unlock;
+
+	err = __st_lsm6dsv16bx_write_with_mask(hw,
+					  ST_LSM6DSV16BX_REG_EMB_FUNC_EN_A_ADDR,
+					  mask,
+					  enable ? 1 : 0);
+	if (err < 0)
+		goto reset_page;
+
+	err = __st_lsm6dsv16bx_write_with_mask(hw, int_reg, irq_mask,
+					       enable ? 1 : 0);
+
+reset_page:
+	st_lsm6dsv16bx_set_page_access(hw,
+				       ST_LSM6DSV16BX_EMB_FUNC_REG_ACCESS_MASK,
+				       0);
+
+unlock:
+	mutex_unlock(&hw->page_lock);
+
+	return err;
+}
+
+/**
+ * st_lsm6dsv16bx_embfunc_sensor_set_enable() - Enable Embedded Function
+ *						sensor [EMB_FUN]
+ *
+ * @sensor: ST IMU sensor instance
+ * @enable: Enable/Disable sensor
+ *
+ * return < 0 if error, 0 otherwise
+ */
+static int
+st_lsm6dsv16bx_embfunc_sensor_set_enable(struct st_lsm6dsv16bx_sensor *sensor,
+					 bool enable)
+{
+	int err;
+
+	switch (sensor->id) {
+	case ST_LSM6DSV16BX_ID_STEP_DETECTOR:
+		err = st_lsm6dsv16bx_embfunc_set_enable(sensor,
+					ST_LSM6DSV16BX_REG_PEDO_EN_MASK,
+					ST_LSM6DSV16BX_INT_STEP_DETECTOR_MASK,
+					enable);
+		break;
+	case ST_LSM6DSV16BX_ID_SIGN_MOTION:
+		err = st_lsm6dsv16bx_embfunc_set_enable(sensor,
+					ST_LSM6DSV16BX_REG_SIGN_MOTION_EN_MASK,
+					ST_LSM6DSV16BX_INT_SIG_MOT_MASK,
+					enable);
+		break;
+	case ST_LSM6DSV16BX_ID_TILT:
+		err = st_lsm6dsv16bx_embfunc_set_enable(sensor,
+						ST_LSM6DSV16BX_REG_TILT_EN_MASK,
+						ST_LSM6DSV16BX_INT_TILT_MASK,
+						enable);
+		break;
+	default:
+		err = -EINVAL;
+		break;
+	}
+
+	return err;
+}
+
+/**
+ * st_lsm6dsv16bx_reset_step_counter() - Reset Step Counter value [EMB_FUN]
+ *
+ * @iio_dev: IIO device
+ *
+ * return < 0 if error, 0 otherwise
+ */
+static int st_lsm6dsv16bx_reset_step_counter(struct iio_dev *iio_dev)
+{
+	struct st_lsm6dsv16bx_sensor *sensor = iio_priv(iio_dev);
+	struct st_lsm6dsv16bx_hw *hw = sensor->hw;
+	int err;
+
+	mutex_lock(&hw->page_lock);
+	err = st_lsm6dsv16bx_set_page_access(hw,
+					ST_LSM6DSV16BX_EMB_FUNC_REG_ACCESS_MASK,
+					1);
+	if (err < 0)
+		goto unlock_page;
+
+	err = __st_lsm6dsv16bx_write_with_mask(hw,
+				     ST_LSM6DSV16BX_REG_EMB_FUNC_SRC_ADDR,
+				     ST_LSM6DSV16BX_PEDO_RST_STEP_MASK, 1);
+	st_lsm6dsv16bx_set_page_access(hw,
+				       ST_LSM6DSV16BX_EMB_FUNC_REG_ACCESS_MASK,
+				       0);
+
+unlock_page:
+	mutex_unlock(&hw->page_lock);
+
+	return err;
+}
+
+/**
+ * st_lsm6dsv16bx_read_embfunc_config() - Read embedded function sensor
+ *					  event configuration
+ *
+ * @iio_dev: IIO Device.
+ * @chan: IIO Channel.
+ * @type: Event Type.
+ * @dir: Event Direction.
+ *
+ * return 1 if Enabled, 0 Disabled
+ */
+static int
+st_lsm6dsv16bx_read_embfunc_config(struct iio_dev *iio_dev,
+				   const struct iio_chan_spec *chan,
+				   enum iio_event_type type,
+				   enum iio_event_direction dir)
+{
+	struct st_lsm6dsv16bx_sensor *sensor = iio_priv(iio_dev);
+	struct st_lsm6dsv16bx_hw *hw = sensor->hw;
+
+	return !!(hw->enable_mask & BIT(sensor->id));
+}
+
+/**
+ * st_lsm6dsv16bx_write_embfunc_config() - Write embedded function
+ *					sensor event configuration
+ *
+ * @iio_dev: IIO Device.
+ * @chan: IIO Channel.
+ * @type: Event Type.
+ * @dir: Event Direction.
+ * @state: New event state.
+ *
+ * return 0 if OK, negative for ERROR
+ */
+static int
+st_lsm6dsv16bx_write_embfunc_config(struct iio_dev *iio_dev,
+				    const struct iio_chan_spec *chan,
+				    enum iio_event_type type,
+				    enum iio_event_direction dir,
+				    int state)
+{
+	struct st_lsm6dsv16bx_sensor *sensor = iio_priv(iio_dev);
+	int err;
+
+	err = iio_device_claim_direct_mode(iio_dev);
+	if (err)
+		return err;
+
+	err = st_lsm6dsv16bx_embfunc_sensor_set_enable(sensor, state);
+	iio_device_release_direct_mode(iio_dev);
+
+	return err;
+}
+
+/**
+ * st_lsm6dsv16bx_sysfs_reset_step_counter() - Reset step counter value
+ *
+ * @dev: IIO Device.
+ * @attr: IIO Channel attribute.
+ * @buf: User buffer.
+ * @size: User buffer size.
+ *
+ * return buffer len, negative for ERROR
+ */
+static ssize_t
+st_lsm6dsv16bx_sysfs_reset_step_counter(struct device *dev,
+					struct device_attribute *attr,
+					const char *buf, size_t size)
+{
+	struct iio_dev *iio_dev = dev_get_drvdata(dev);
+	int err;
+
+	err = iio_device_claim_direct_mode(iio_dev);
+	if (err)
+		return err;
+
+	err = st_lsm6dsv16bx_reset_step_counter(iio_dev);
+	iio_device_release_direct_mode(iio_dev);
+
+	return err < 0 ? err : size;
+}
+
+static IIO_DEVICE_ATTR(reset_stepc, 0200, NULL,
+		       st_lsm6dsv16bx_sysfs_reset_step_counter, 0);
+
+static IIO_DEVICE_ATTR(hwfifo_stepc_watermark_max, 0444,
+		       st_lsm6dsv16bx_get_max_watermark, NULL, 0);
+static IIO_DEVICE_ATTR(hwfifo_stepc_flush, 0200, NULL,
+		       st_lsm6dsv16bx_flush_fifo, 0);
+static IIO_DEVICE_ATTR(hwfifo_stepc_watermark, 0644,
+		       st_lsm6dsv16bx_get_watermark,
+		       st_lsm6dsv16bx_set_watermark, 0);
+static IIO_DEVICE_ATTR(module_id, 0444, st_lsm6dsv16bx_get_module_id, NULL, 0);
+
+static struct attribute *st_lsm6dsv16bx_step_counter_attributes[] = {
+	&iio_dev_attr_hwfifo_stepc_watermark_max.dev_attr.attr,
+	&iio_dev_attr_hwfifo_stepc_watermark.dev_attr.attr,
+	&iio_dev_attr_reset_stepc.dev_attr.attr,
+	&iio_dev_attr_hwfifo_stepc_flush.dev_attr.attr,
+	&iio_dev_attr_module_id.dev_attr.attr,
+	NULL,
+};
+
+static const struct
+attribute_group st_lsm6dsv16bx_step_counter_attribute_group = {
+	.attrs = st_lsm6dsv16bx_step_counter_attributes,
+};
+
+static const struct iio_info st_lsm6dsv16bx_step_counter_info = {
+	.attrs = &st_lsm6dsv16bx_step_counter_attribute_group,
+};
+
+static struct attribute *st_lsm6dsv16bx_step_detector_attributes[] = {
+	&iio_dev_attr_module_id.dev_attr.attr,
+	NULL,
+};
+
+static const struct
+attribute_group st_lsm6dsv16bx_step_detector_attribute_group = {
+	.attrs = st_lsm6dsv16bx_step_detector_attributes,
+};
+
+static const struct iio_info st_lsm6dsv16bx_step_detector_info = {
+	.attrs = &st_lsm6dsv16bx_step_detector_attribute_group,
+	.read_event_config = st_lsm6dsv16bx_read_embfunc_config,
+	.write_event_config = st_lsm6dsv16bx_write_embfunc_config,
+};
+
+static struct attribute *st_lsm6dsv16bx_sign_motion_attributes[] = {
+	&iio_dev_attr_module_id.dev_attr.attr,
+	NULL,
+};
+
+static const struct
+attribute_group st_lsm6dsv16bx_sign_motion_attribute_group = {
+	.attrs = st_lsm6dsv16bx_sign_motion_attributes,
+};
+
+static const struct iio_info st_lsm6dsv16bx_sign_motion_info = {
+	.attrs = &st_lsm6dsv16bx_sign_motion_attribute_group,
+	.read_event_config = st_lsm6dsv16bx_read_embfunc_config,
+	.write_event_config = st_lsm6dsv16bx_write_embfunc_config,
+};
+
+static struct attribute *st_lsm6dsv16bx_tilt_attributes[] = {
+	&iio_dev_attr_module_id.dev_attr.attr,
+	NULL,
+};
+
+static const struct attribute_group st_lsm6dsv16bx_tilt_attribute_group = {
+	.attrs = st_lsm6dsv16bx_tilt_attributes,
+};
+
+static const struct iio_info st_lsm6dsv16bx_tilt_info = {
+	.attrs = &st_lsm6dsv16bx_tilt_attribute_group,
+	.read_event_config = st_lsm6dsv16bx_read_embfunc_config,
+	.write_event_config = st_lsm6dsv16bx_write_embfunc_config,
+};
+
+static int st_lsm6dsv16bx_embfunc_init(struct st_lsm6dsv16bx_hw *hw)
+{
+	u8 int_reg = hw->int_pin == 1 ? ST_LSM6DSV16BX_REG_MD1_CFG_ADDR :
+					ST_LSM6DSV16BX_REG_MD2_CFG_ADDR;
+	int err;
+
+	mutex_lock(&hw->page_lock);
+	err = st_lsm6dsv16bx_set_page_access(hw,
+					ST_LSM6DSV16BX_EMB_FUNC_REG_ACCESS_MASK,
+					1);
+	if (err < 0)
+		goto unlock_page;
+
+	/* enable embedded function latched interrupt */
+	err = __st_lsm6dsv16bx_write_with_mask(hw,
+					   ST_LSM6DSV16BX_REG_PAGE_RW_ADDR,
+					   ST_LSM6DSV16BX_EMB_FUNC_LIR_MASK, 1);
+	if (err < 0)
+		goto unlock_page;
+
+	st_lsm6dsv16bx_set_page_access(hw,
+				       ST_LSM6DSV16BX_EMB_FUNC_REG_ACCESS_MASK,
+				       0);
+
+	/* enable embedded function interrupt by default */
+	err = __st_lsm6dsv16bx_write_with_mask(hw, int_reg,
+					   ST_LSM6DSV16BX_REG_INT_EMB_FUNC_MASK,
+					   1);
+unlock_page:
+	mutex_unlock(&hw->page_lock);
+
+	return err;
+}
+
+static struct iio_dev *
+st_lsm6dsv16bx_alloc_embfunc_iiodev(struct st_lsm6dsv16bx_hw *hw,
+				    enum st_lsm6dsv16bx_sensor_id id)
+{
+	struct st_lsm6dsv16bx_sensor *sensor;
+	struct iio_dev *iio_dev;
+
+	iio_dev = devm_iio_device_alloc(hw->dev, sizeof(*sensor));
+	if (!iio_dev)
+		return NULL;
+
+	iio_dev->modes = INDIO_DIRECT_MODE;
+	iio_dev->dev.parent = hw->dev;
+
+	sensor = iio_priv(iio_dev);
+	sensor->id = id;
+	sensor->hw = hw;
+	sensor->watermark = 1;
+	iio_dev->available_scan_masks =
+				    st_lsm6dsv16bx_embfunc_available_scan_masks;
+
+	/* set main sensor odr to 26 Hz */
+	sensor->odr = hw->odr_table[ST_LSM6DSV16BX_ID_ACC].odr_avl[2].hz;
+	switch (id) {
+	case ST_LSM6DSV16BX_ID_STEP_COUNTER:
+		iio_dev->channels = st_lsm6dsv16bx_step_counter_channels;
+		iio_dev->num_channels = ARRAY_SIZE(st_lsm6dsv16bx_step_counter_channels);
+		scnprintf(sensor->name, sizeof(sensor->name),
+			 "%s_stepc", hw->settings->id.name);
+		iio_dev->info = &st_lsm6dsv16bx_step_counter_info;
+		break;
+	case ST_LSM6DSV16BX_ID_STEP_DETECTOR:
+		iio_dev->channels = st_lsm6dsv16bx_step_detector_channels;
+		iio_dev->num_channels = ARRAY_SIZE(st_lsm6dsv16bx_step_detector_channels);
+		scnprintf(sensor->name, sizeof(sensor->name),
+			 "%s_stepd", hw->settings->id.name);
+		iio_dev->info = &st_lsm6dsv16bx_step_detector_info;
+		break;
+	case ST_LSM6DSV16BX_ID_SIGN_MOTION:
+		iio_dev->channels = st_lsm6dsv16bx_sign_motion_channels;
+		iio_dev->num_channels = ARRAY_SIZE(st_lsm6dsv16bx_sign_motion_channels);
+		scnprintf(sensor->name, sizeof(sensor->name),
+			 "%s_sigmot", hw->settings->id.name);
+		iio_dev->info = &st_lsm6dsv16bx_sign_motion_info;
+		break;
+	case ST_LSM6DSV16BX_ID_TILT:
+		iio_dev->channels = st_lsm6dsv16bx_tilt_channels;
+		iio_dev->num_channels = ARRAY_SIZE(st_lsm6dsv16bx_tilt_channels);
+		scnprintf(sensor->name, sizeof(sensor->name),
+			 "%s_tilt", hw->settings->id.name);
+		iio_dev->info = &st_lsm6dsv16bx_tilt_info;
+		break;
+	default:
+		return NULL;
+	}
+
+	iio_dev->name = sensor->name;
+
+	return iio_dev;
+}
+
+/**
+ * st_lsm6dsv16bx_step_counter_set_enable() - Enable Step Counter
+ *					      Sensor [EMB_FUN]
+ *
+ * @sensor: ST IMU sensor instance
+ * @enable: Enable/Disable sensor
+ *
+ * return < 0 if error, 0 otherwise
+ */
+int st_lsm6dsv16bx_step_counter_set_enable(struct st_lsm6dsv16bx_sensor *sensor,
+					   bool enable)
+{
+	struct st_lsm6dsv16bx_hw *hw = sensor->hw;
+	int err;
+
+	err = st_lsm6dsv16bx_sensor_set_enable(sensor, enable);
+	if (err < 0)
+		return err;
+
+	mutex_lock(&hw->page_lock);
+	err = st_lsm6dsv16bx_set_page_access(hw,
+					ST_LSM6DSV16BX_EMB_FUNC_REG_ACCESS_MASK,
+					1);
+	if (err < 0)
+		goto unlock;
+
+	err = __st_lsm6dsv16bx_write_with_mask(hw,
+				     ST_LSM6DSV16BX_REG_EMB_FUNC_EN_A_ADDR,
+				     ST_LSM6DSV16BX_REG_PEDO_EN_MASK,
+				     enable);
+	if (err < 0)
+		goto reset_page;
+
+	/* enable step counter batching in fifo */
+	err = __st_lsm6dsv16bx_write_with_mask(hw,
+				 ST_LSM6DSV16BX_REG_EMB_FUNC_FIFO_EN_A_ADDR,
+				 ST_LSM6DSV16BX_STEP_COUNTER_FIFO_EN_MASK,
+				 enable);
+
+reset_page:
+	st_lsm6dsv16bx_set_page_access(hw,
+				       ST_LSM6DSV16BX_EMB_FUNC_REG_ACCESS_MASK,
+				       0);
+unlock:
+	mutex_unlock(&hw->page_lock);
+
+	return err;
+}
+
+/**
+ * st_lsm6dsv16bx_embfunc_handler_thread() - Bottom handler for embedded
+ *					     function event detection
+ *
+ * @hw: ST IMU MEMS hw instance.
+ *
+ * return IRQ_HANDLED or < 0 for error
+ */
+int st_lsm6dsv16bx_embfunc_handler_thread(struct st_lsm6dsv16bx_hw *hw)
+{
+	if (hw->enable_mask & (BIT(ST_LSM6DSV16BX_ID_STEP_DETECTOR) |
+			       BIT(ST_LSM6DSV16BX_ID_SIGN_MOTION) |
+			       BIT(ST_LSM6DSV16BX_ID_TILT))) {
+		struct iio_dev *iio_dev;
+		u8 status;
+		s64 event;
+		int err;
+
+		err = st_lsm6dsv16bx_read_locked(hw,
+			       ST_LSM6DSV16BX_REG_EMB_FUNC_STATUS_MAINPAGE_ADDR,
+			       &status, sizeof(status));
+		if (err < 0)
+			return IRQ_HANDLED;
+
+		/* embedded function sensors */
+		if (status & ST_LSM6DSV16BX_IS_STEP_DET_MASK) {
+			iio_dev = hw->iio_devs[ST_LSM6DSV16BX_ID_STEP_DETECTOR];
+			event = IIO_UNMOD_EVENT_CODE(IIO_STEPS, -1,
+						     IIO_EV_TYPE_THRESH,
+						     IIO_EV_DIR_RISING);
+			iio_push_event(iio_dev, event,
+				       iio_get_time_ns(iio_dev));
+		}
+
+		if (status & ST_LSM6DSV16BX_IS_SIGMOT_MASK) {
+			iio_dev = hw->iio_devs[ST_LSM6DSV16BX_ID_SIGN_MOTION];
+			event = IIO_UNMOD_EVENT_CODE(STM_IIO_SIGN_MOTION, -1,
+						     IIO_EV_TYPE_THRESH,
+						     IIO_EV_DIR_RISING);
+			iio_push_event(iio_dev, event,
+				       iio_get_time_ns(iio_dev));
+		}
+
+		if (status & ST_LSM6DSV16BX_IS_TILT_MASK) {
+			iio_dev = hw->iio_devs[ST_LSM6DSV16BX_ID_TILT];
+			event = IIO_UNMOD_EVENT_CODE(STM_IIO_TILT, -1,
+						     IIO_EV_TYPE_THRESH,
+						     IIO_EV_DIR_RISING);
+			iio_push_event(iio_dev, event,
+				       iio_get_time_ns(iio_dev));
+		}
+	}
+
+	return IRQ_HANDLED;
+}
+
+/**
+ * st_lsm6dsv16bx_probe_embfunc() - Allocate IIO embedded function device
+ *
+ * @hw: ST IMU MEMS hw instance.
+ *
+ * return 0 or < 0 for error
+ */
+int st_lsm6dsv16bx_probe_embfunc(struct st_lsm6dsv16bx_hw *hw)
+{
+	enum st_lsm6dsv16bx_sensor_id id;
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(st_lsm6dsv16bx_embfunc_sensor_list);
+	     i++) {
+
+		id = st_lsm6dsv16bx_embfunc_sensor_list[i];
+		hw->iio_devs[id] = st_lsm6dsv16bx_alloc_embfunc_iiodev(hw, id);
+		if (!hw->iio_devs[id])
+			return -ENOMEM;
+	}
+
+	return st_lsm6dsv16bx_embfunc_init(hw);
+}
---
+++ drivers/iio/stm/imu/st_lsm6dsv16bx/st_lsm6dsv16bx_hwtimestamp.c
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * STMicroelectronics st_lsm6dsv16bx hwtimestamp library driver
+ *
+ * MEMS Software Solutions Team
+ *
+ * Copyright 2023 STMicroelectronics Inc.
+ */
+#include <asm/unaligned.h>
+#include <linux/iio/buffer.h>
+#include <linux/iio/events.h>
+#include <linux/iio/kfifo_buf.h>
+#include <linux/iio/iio.h>
+#include <linux/module.h>
+
+#include "st_lsm6dsv16bx.h"
+
+#define ST_LSM6DSV16BX_TSYNC_OFFSET_NS		(300 * 1000LL)
+
+static void st_lsm6dsv16bx_read_hw_timestamp(struct st_lsm6dsv16bx_hw *hw)
+{
+	s64 timestamp_hw_global;
+	s64 eventLSB, eventMSB;
+	__le32 timestamp_hw;
+	s64 timestamp_cpu;
+	__le32 tmp;
+	int err;
+
+	err = st_lsm6dsv16bx_read_locked(hw, ST_LSM6DSV16BX_REG_TIMESTAMP0_ADDR,
+				      (u8 *)&timestamp_hw,
+				      sizeof(timestamp_hw));
+	if (err < 0)
+		return;
+
+	timestamp_cpu = iio_get_time_ns(hw->iio_devs[0]) -
+					ST_LSM6DSV16BX_TSYNC_OFFSET_NS;
+
+	eventLSB = IIO_EVENT_CODE(IIO_COUNT, 0, 0, 0,
+				  STM_IIO_EV_TYPE_TIME_SYNC, 0, 0, 0);
+	eventMSB = IIO_EVENT_CODE(IIO_COUNT, 0, 0, 1,
+				  STM_IIO_EV_TYPE_TIME_SYNC, 0, 0, 0);
+
+	spin_lock_irq(&hw->hwtimestamp_lock);
+	timestamp_hw_global = (hw->hw_timestamp_global & GENMASK_ULL(63, 32)) |
+			      (u32)le32_to_cpu(timestamp_hw);
+	spin_unlock_irq(&hw->hwtimestamp_lock);
+
+	tmp = cpu_to_le32((u32)timestamp_hw_global);
+	memcpy(&((int8_t *)&eventLSB)[0], &tmp, sizeof(tmp));
+
+	tmp = cpu_to_le32((u32)(timestamp_hw_global >> 32));
+	memcpy(&((int8_t *)&eventMSB)[0], &tmp, sizeof(tmp));
+
+	if (hw->enable_mask & BIT_ULL(ST_LSM6DSV16BX_ID_GYRO)) {
+		iio_push_event(hw->iio_devs[ST_LSM6DSV16BX_ID_GYRO], eventLSB,
+			       timestamp_cpu);
+		iio_push_event(hw->iio_devs[ST_LSM6DSV16BX_ID_GYRO], eventMSB,
+			       timestamp_cpu);
+	}
+	if (hw->enable_mask & BIT_ULL(ST_LSM6DSV16BX_ID_ACC)) {
+		iio_push_event(hw->iio_devs[ST_LSM6DSV16BX_ID_ACC], eventLSB,
+			       timestamp_cpu);
+		iio_push_event(hw->iio_devs[ST_LSM6DSV16BX_ID_ACC], eventMSB,
+			       timestamp_cpu);
+	}
+	if (hw->enable_mask & BIT_ULL(ST_LSM6DSV16BX_ID_TEMP)) {
+		iio_push_event(hw->iio_devs[ST_LSM6DSV16BX_ID_TEMP], eventLSB,
+			       timestamp_cpu);
+		iio_push_event(hw->iio_devs[ST_LSM6DSV16BX_ID_TEMP], eventMSB,
+			       timestamp_cpu);
+	}
+
+	if (hw->timesync_c < 6)
+		hw->timesync_c++;
+	else
+		hw->timesync_ktime = ktime_set(0, ST_LSM6DSV16BX_DEFAULT_KTIME);
+}
+
+static void st_lsm6dsv16bx_timesync_fn(struct work_struct *work)
+{
+	struct st_lsm6dsv16bx_hw *hw = container_of(work, struct st_lsm6dsv16bx_hw,
+						 timesync_work);
+
+	st_lsm6dsv16bx_read_hw_timestamp(hw);
+}
+
+static enum hrtimer_restart st_lsm6dsv16bx_timer_fn(struct hrtimer *timer)
+{
+	struct st_lsm6dsv16bx_hw *hw;
+
+	hw = container_of(timer, struct st_lsm6dsv16bx_hw, timesync_timer);
+	hrtimer_forward(timer, hrtimer_cb_get_time(timer), hw->timesync_ktime);
+	queue_work(hw->timesync_workqueue, &hw->timesync_work);
+
+	return HRTIMER_RESTART;
+}
+
+int st_lsm6dsv16bx_hwtimesync_init(struct st_lsm6dsv16bx_hw *hw)
+{
+	hw->timesync_c = 0;
+	hw->timesync_ktime = ktime_set(0, ST_LSM6DSV16BX_DEFAULT_KTIME);
+	hrtimer_init(&hw->timesync_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+	hw->timesync_timer.function = st_lsm6dsv16bx_timer_fn;
+
+	spin_lock_init(&hw->hwtimestamp_lock);
+	hw->hw_timestamp_global = 0;
+
+	hw->timesync_workqueue = create_singlethread_workqueue("st_lsm6dsv16bx_workqueue");
+	if (!hw->timesync_workqueue)
+		return -ENOMEM;
+
+	INIT_WORK(&hw->timesync_work, st_lsm6dsv16bx_timesync_fn);
+
+	return 0;
+}
+EXPORT_SYMBOL(st_lsm6dsv16bx_hwtimesync_init);
