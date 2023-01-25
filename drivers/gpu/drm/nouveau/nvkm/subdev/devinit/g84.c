da2b1a0a40d8ea8f23f9d13e10ec3160ef92178d
Deepak R Varma <drv@mailo.com>
2023-01-25 20:37:14 +0530

    drm/nouveau/devinit: Convert function disable() to be void
    The current design of callback function disable() of struct
    nvkm_devinit_func is defined to return a u64 value. In its implementation
    in the driver modules, the function always returns a fixed value 0. Hence
    the design and implementation of this function should be enhanced to return
    void instead of a fixed value. This change also eliminates untouched
    return variables.
    
    The change is identified using the returnvar.cocci Coccinelle semantic
    patch script.
    
    Signed-off-by: Deepak R Varma <drv@mailo.com>
    Reviewed-by: Lyude Paul <lyude@redhat.com>
    Signed-off-by: Lyude Paul <lyude@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/Y9FFoooIXjlr+UP1@ubun2204.myguest.virtualbox.org

--- drivers/gpu/drm/nouveau/nvkm/subdev/devinit/g84.c
+++ drivers/gpu/drm/nouveau/nvkm/subdev/devinit/g84.c
@@ -26,13 +26,12 @@
 #include <subdev/bios.h>
 #include <subdev/bios/init.h>
 
-static u64
+static void
 g84_devinit_disable(struct nvkm_devinit *init)
 {
 	struct nvkm_device *device = init->subdev.device;
 	u32 r001540 = nvkm_rd32(device, 0x001540);
 	u32 r00154c = nvkm_rd32(device, 0x00154c);
-	u64 disable = 0ULL;
 
 	if (!(r001540 & 0x40000000)) {
 		nvkm_subdev_disable(device, NVKM_ENGINE_MPEG, 0);
@@ -47,8 +46,6 @@ g84_devinit_disable(struct nvkm_devinit *init)
 		nvkm_subdev_disable(device, NVKM_ENGINE_BSP, 0);
 	if (!(r00154c & 0x00000040))
 		nvkm_subdev_disable(device, NVKM_ENGINE_CIPHER, 0);
-
-	return disable;
 }
 
 static const struct nvkm_devinit_func
