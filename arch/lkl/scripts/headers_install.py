63fc53a34fcfab8da9b13afd7f499ce2eebbbb3c
Xuan Xing <hclxing@gmail.com>
2023-01-25 01:38:28 +0000

    Add LKL fuzzer for HID.
    This fuzzer fuzzes Linux kernel HID subsystem via /dev/uhid device.
    
    To build the fuzzer:
    
    make -C tools/lkl LKL_FUZZING=1 fuzzers
    
    Signed-off-by: Xuan Xing <hclxing@gmail.com>

--- arch/lkl/scripts/headers_install.py
+++ arch/lkl/scripts/headers_install.py
@@ -128,6 +128,8 @@ args = parser.parse_args()
 
 find_headers("arch/lkl/include/uapi/asm/syscalls.h")
 headers.add("arch/lkl/include/uapi/asm/host_ops.h")
+find_headers("include/uapi/linux/uhid.h")
+find_headers("include/uapi/linux/input-event-codes.h")
 
 if 'LKL_INSTALL_ADDITIONAL_HEADERS' in os.environ:
     with open(os.environ['LKL_INSTALL_ADDITIONAL_HEADERS'], 'rU') as f:
