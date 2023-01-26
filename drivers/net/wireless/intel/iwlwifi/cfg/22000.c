9ad1e7e5b0c488e4518edd698238a87ac6a73caa
Golan Ben Ami <golan.ben.ami@intel.com>
2023-01-27 00:28:19 +0200

    wifi: iwlwifi: bump FW API to 74 for AX devices
    Start supporting API version 74 for AX devices.
    
    Signed-off-by: Golan Ben Ami <golan.ben.ami@intel.com>
    Link: https://lore.kernel.org/r/20230127002430.80012ee4c5d6.I45ba1f8bf923d242ef2ffeb160d736120c8add65@changeid
    Signed-off-by: Gregory Greenman <gregory.greenman@intel.com>

--- drivers/net/wireless/intel/iwlwifi/cfg/22000.c
+++ drivers/net/wireless/intel/iwlwifi/cfg/22000.c
@@ -10,7 +10,7 @@
 #include "fw/api/txq.h"
 
 /* Highest firmware API version supported */
-#define IWL_22000_UCODE_API_MAX	72
+#define IWL_22000_UCODE_API_MAX	74
 
 /* Lowest firmware API version supported */
 #define IWL_22000_UCODE_API_MIN	39
