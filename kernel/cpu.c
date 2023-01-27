6bd9415d98f3c44a820c398ed5cbccecce07f331
Srinivasarao Pathipati <quic_spathi@quicinc.com>
2023-01-27 11:34:03 +0530

    ANDROID: cpu: correct dl_cpu_busy() calls
    The patch 0039189a3b15 ("sched/deadline: Merge
    dl_task_can_attach() and dl_cpu_busy()") which is picked from
    upstream modifies declaration of function dl_cpu_busy().
    But it won't update function usage from android specific code
    that introduced with patch 683010f555d8 ("ANDROID: cpu/hotplug:
    add pause/resume_cpus interface").
    
    Bug: 266874695
    Fixes: 0039189a3b15 ("sched/deadline: Merge dl_task_can_attach() and dl_cpu_busy()"
    Change-Id: I40c12f912b7fe854b1e2e13f75c727c3c9a2435c
    Signed-off-by: Srinivasarao Pathipati <quic_spathi@quicinc.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1159,7 +1159,7 @@ int remove_cpu(unsigned int cpu)
 }
 EXPORT_SYMBOL_GPL(remove_cpu);
 
-extern bool dl_cpu_busy(unsigned int cpu);
+extern int  dl_cpu_busy(int cpu, struct task_struct *p);
 
 int __pause_drain_rq(struct cpumask *cpus)
 {
@@ -1234,7 +1234,7 @@ int pause_cpus(struct cpumask *cpus)
 	cpumask_and(cpus, cpus, cpu_active_mask);
 
 	for_each_cpu(cpu, cpus) {
-		if (!cpu_online(cpu) || dl_cpu_busy(cpu) ||
+		if (!cpu_online(cpu) || dl_cpu_busy(cpu, NULL) ||
 			get_cpu_device(cpu)->offline_disabled == true) {
 			err = -EBUSY;
 			goto err_cpu_maps_update;
