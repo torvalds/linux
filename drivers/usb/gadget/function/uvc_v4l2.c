7a4ae778bdf2124023c65c0789b88b6bb8faf6e0
William Wu <william.wu@rock-chips.com>
2023-01-30 20:45:18 +0800

    usb: gadget: uvc: fix uvc state for bulk transfer
    Set uvc state to UVC_STATE_STREAMING when stream on uvc
    for both isoc and bulk transfer, then it can schedule
    video pump work in uvc_v4l2_qbuf.
    
    Fixes: c15a7cecb83b ("usb: gadget: uvc: support streaming bulk transfer")
    Change-Id: Ie6fab6529cd83d59b34c6dd0028d6ad5b5eed010
    Signed-off-by: William Wu <william.wu@rock-chips.com>

--- drivers/usb/gadget/function/uvc_v4l2.c
+++ drivers/usb/gadget/function/uvc_v4l2.c
@@ -214,16 +214,15 @@ uvc_v4l2_streamon(struct file *file, void *fh, enum v4l2_buf_type type)
 	 * settings for zero-bandwidth and full-bandwidth
 	 * cases, but the same is not true for BULK endpoints,
 	 * as they have a single alt-setting.
+	 *
+	 * For ISOC endpoints, Complete the alternate setting
+	 * selection setup phase now that userspace is ready
+	 * to provide video frames.
 	 */
-	if (!usb_endpoint_xfer_bulk(video->ep->desc)) {
-		/*
-		 * Complete the alternate setting selection
-		 * setup phase now that userspace is ready
-		 * to provide video frames.
-		 */
+	if (!usb_endpoint_xfer_bulk(video->ep->desc))
 		uvc_function_setup_continue(uvc);
-		uvc->state = UVC_STATE_STREAMING;
-	}
+
+	uvc->state = UVC_STATE_STREAMING;
 
 	return 0;
 }
