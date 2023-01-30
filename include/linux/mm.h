fa4e3f5ffa5e6e22f751d289c9afa502dda30b8d
Vishal Moola (Oracle) <vishal.moola@gmail.com>
2023-01-30 12:18:28 -0800

    mm: add folio_estimated_sharers()
    Patch series "Convert various mempolicy.c functions to use folios", v4.
    
    This patch series converts migrate_page_add() and queue_pages_required()
    to migrate_folio_add() and queue_page_required().  It also converts the
    callers of the functions to use folios as well, and introduces a helper
    function to estimate the number of sharers of a folio.
    
    
    This patch (of 6):
    
    folio_estimated_sharers() takes in a folio and returns the precise number
    of times the first subpage of the folio is mapped.
    
    This function aims to provide an estimate for the number of sharers of a
    folio.  This is necessary for folio conversions where we care about the
    number of processes that share a folio, but don't necessarily want to
    check every single page within that folio.
    
    This is in contrast to folio_mapcount() which calculates the total number
    of the times a folio and all its subpages are mapped.
    
    Link: https://lkml.kernel.org/r/20230130201833.27042-1-vishal.moola@gmail.com
    Link: https://lkml.kernel.org/r/20230130201833.27042-2-vishal.moola@gmail.com
    Signed-off-by: Vishal Moola (Oracle) <vishal.moola@gmail.com>
    Reviewed-by: Yin Fengwei <fengwei.yin@intel.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Cc: Jane Chu <jane.chu@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1916,6 +1916,24 @@ static inline size_t folio_size(struct folio *folio)
 	return PAGE_SIZE << folio_order(folio);
 }
 
+/**
+ * folio_estimated_sharers - Estimate the number of sharers of a folio.
+ * @folio: The folio.
+ *
+ * folio_estimated_sharers() aims to serve as a function to efficiently
+ * estimate the number of processes sharing a folio. This is done by
+ * looking at the precise mapcount of the first subpage in the folio, and
+ * assuming the other subpages are the same. This may not be true for large
+ * folios. If you want exact mapcounts for exact calculations, look at
+ * page_mapcount() or folio_total_mapcount().
+ *
+ * Return: The estimated number of processes sharing a folio.
+ */
+static inline int folio_estimated_sharers(struct folio *folio)
+{
+	return page_mapcount(folio_page(folio, 0));
+}
+
 #ifndef HAVE_ARCH_MAKE_PAGE_ACCESSIBLE
 static inline int arch_make_page_accessible(struct page *page)
 {
