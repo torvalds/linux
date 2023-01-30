004fc58f917cfea5d7190139e3ed1b7a13e39c25
Anshuman Khandual <anshuman.khandual@arm.com>
2023-01-30 17:44:57 +0530

    arm64/mm: Intercept pfn changes in set_pte_at()
    Changing pfn on a user page table mapped entry, without first going through
    break-before-make (BBM) procedure is unsafe. This just updates set_pte_at()
    to intercept such changes, via an updated pgattr_change_is_safe(). This new
    check happens via __check_racy_pte_update(), which has now been renamed as
    __check_safe_pte_update().
    
    Cc: Will Deacon <will@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-kernel@vger.kernel.org
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Link: https://lore.kernel.org/r/20230130121457.1607675-1-anshuman.khandual@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -133,7 +133,7 @@ static phys_addr_t __init early_pgtable_alloc(int shift)
 	return phys;
 }
 
-static bool pgattr_change_is_safe(u64 old, u64 new)
+bool pgattr_change_is_safe(u64 old, u64 new)
 {
 	/*
 	 * The following mapping attributes may be updated in live
@@ -142,8 +142,12 @@ static bool pgattr_change_is_safe(u64 old, u64 new)
 	pteval_t mask = PTE_PXN | PTE_RDONLY | PTE_WRITE | PTE_NG;
 
 	/* creating or taking down mappings is always safe */
-	if (old == 0 || new == 0)
+	if (!pte_valid(__pte(old)) || !pte_valid(__pte(new)))
 		return true;
+
+	/* A live entry's pfn should not change */
+	if (pte_pfn(__pte(old)) != pte_pfn(__pte(new)))
+		return false;
 
 	/* live contiguous mappings may not be manipulated at all */
 	if ((old | new) & PTE_CONT)
