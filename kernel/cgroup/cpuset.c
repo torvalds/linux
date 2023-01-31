a2ab7f2cf5ef8f0c6212a246e681d1fe358cec1f
Waiman Long <longman@redhat.com>
2023-01-31 10:48:03 -0500

    cgroup/cpuset: Fix wrong check in update_parent_subparts_cpumask()
    commit e5ae8803847b80fe9d744a3174abe2b7bfed222a upstream.
    
    It was found that the check to see if a partition could use up all
    the cpus from the parent cpuset in update_parent_subparts_cpumask()
    was incorrect. As a result, it is possible to leave parent with no
    effective cpu left even if there are tasks in the parent cpuset. This
    can lead to system panic as reported in [1].
    
    Fix this probem by updating the check to fail the enabling the partition
    if parent's effective_cpus is a subset of the child's cpus_allowed.
    
    Also record the error code when an error happens in update_prstate()
    and add a test case where parent partition and child have the same cpu
    list and parent has task. Enabling partition in the child will fail in
    this case.
    
    [1] https://www.spinics.net/lists/cgroups/msg36254.html
    
    Fixes: f0af1bfc27b5 ("cgroup/cpuset: Relax constraints to partition & cpus changes")
    Cc: stable@vger.kernel.org # v6.1
    Reported-by: Srinivas Pandruvada <srinivas.pandruvada@intel.com>
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -1342,7 +1342,7 @@ static int update_parent_subparts_cpumask(struct cpuset *cs, int cmd,
 		 * A parent can be left with no CPU as long as there is no
 		 * task directly associated with the parent partition.
 		 */
-		if (!cpumask_intersects(cs->cpus_allowed, parent->effective_cpus) &&
+		if (cpumask_subset(parent->effective_cpus, cs->cpus_allowed) &&
 		    partition_is_populated(parent, cs))
 			return PERR_NOCPUS;
 
@@ -2320,6 +2320,7 @@ out:
 		new_prs = -new_prs;
 	spin_lock_irq(&callback_lock);
 	cs->partition_root_state = new_prs;
+	WRITE_ONCE(cs->prs_err, err);
 	spin_unlock_irq(&callback_lock);
 	/*
 	 * Update child cpusets, if present.
