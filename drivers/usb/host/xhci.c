fa89210a0e2d6626d912862f3ae19e8dfed8417d
Greg Kroah-Hartman <gregkh@google.com>
2023-02-01 13:31:38 +0000

    Revert "xhci: Add a flag to disable USB3 lpm on a xhci root port level."
    This reverts commit 4d70a8a9ab8edfa589cc13ad6e8bde3e26dfcdcf which is
    commit 0522b9a1653048440da5f21747f21e498b9220d1 upstream.
    
    It breaks the android kernel abi and is not needed for android at this
    point in time as the USB3 issues it resolves are not in Android devices.
    
    If this is needed in the future, it can be brought back in an ABI-safe way.
    
    Bug: 161946584
    Change-Id: I06a9e667eb91d6502d040b7ba5db7c43f9ccdcb5
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -5003,7 +5003,6 @@ static int xhci_enable_usb3_lpm_timeout(struct usb_hcd *hcd,
 			struct usb_device *udev, enum usb3_link_state state)
 {
 	struct xhci_hcd	*xhci;
-	struct xhci_port *port;
 	u16 hub_encoded_timeout;
 	int mel;
 	int ret;
@@ -5016,13 +5015,6 @@ static int xhci_enable_usb3_lpm_timeout(struct usb_hcd *hcd,
 	if (!xhci || !(xhci->quirks & XHCI_LPM_SUPPORT) ||
 			!xhci->devs[udev->slot_id])
 		return USB3_LPM_DISABLED;
-
-	/* If connected to root port then check port can handle lpm */
-	if (udev->parent && !udev->parent->parent) {
-		port = xhci->usb3_rhub.ports[udev->portnum - 1];
-		if (port->lpm_incapable)
-			return USB3_LPM_DISABLED;
-	}
 
 	hub_encoded_timeout = xhci_calculate_lpm_timeout(hcd, udev, state);
 	mel = calculate_max_exit_latency(udev, state, hub_encoded_timeout);
--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1742,7 +1742,6 @@ struct xhci_port {
 	int			hcd_portnum;
 	struct xhci_hub		*rhub;
 	struct xhci_port_cap	*port_cap;
-	unsigned int		lpm_incapable:1;
 };
 
 struct xhci_hub {
