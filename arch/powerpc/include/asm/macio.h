9db2235326c4b868b6e065dfa3a69011ee570848
Dawei Li <set_pte_at@outlook.com>
2023-02-01 22:36:19 +0800

    powerpc/macio: Make remove callback of macio driver void returned
    Commit fc7a6209d571 ("bus: Make remove callback return void") forces
    bus_type::remove be void-returned, it doesn't make much sense for any
    bus based driver implementing remove callbalk to return non-void to
    its caller.
    
    This change is for macio bus based drivers.
    
    Signed-off-by: Dawei Li <set_pte_at@outlook.com>
    Acked-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>
    Acked-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://msgid.link/TYCP286MB232391520CB471E7C8D6EA84CAD19@TYCP286MB2323.JPNP286.PROD.OUTLOOK.COM

--- arch/powerpc/include/asm/macio.h
+++ arch/powerpc/include/asm/macio.h
@@ -126,7 +126,7 @@ static inline struct pci_dev *macio_get_pci_dev(struct macio_dev *mdev)
 struct macio_driver
 {
 	int	(*probe)(struct macio_dev* dev, const struct of_device_id *match);
-	int	(*remove)(struct macio_dev* dev);
+	void	(*remove)(struct macio_dev *dev);
 
 	int	(*suspend)(struct macio_dev* dev, pm_message_t state);
 	int	(*resume)(struct macio_dev* dev);
