fbe7e38f3e57e38916ae7f248fd6efafeb9ecdd3
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2023-02-02 11:45:01 +0100

    serial: 8250: Fix mismerge regarding serial_lsr_in()
    The relevant history introducing serial_lsr_in() looks as follows:
    
    	$ git log --graph --oneline --boundary 9fafe733514b..df36f3e3fbb7 -- drivers/tty/serial/8250/8250_port.c
    	*   df36f3e3fbb7 Merge tag 'v5.19-rc3' into tty-next
    	|\
    	| * be03b0651ffd serial: 8250: Store to lsr_save_flags after lsr read
    	* | ...
    	* | bdb70c424df1 serial: 8250: Create serial_lsr_in()
    	* | ce338e4477cf serial: 8250: Store to lsr_save_flags after lsr read
    	* | ...
    	|/
    	o 9fafe733514b tty: remove CMSPAR ifdefs
    
    So the patch "serial: 8250: Store to lsr_save_flags after lsr read" was
    introduced twice and in one branch it was followed up by commit
    bdb70c424df1 ("serial: 8250: Create serial_lsr_in()") which moved
    explicit lsr_saved_flags handling into a new function serial_lsr_in().
    When the two branches were merged in commit df36f3e3fbb7, we got both,
    serial_lsr_in() and the explicit lsr_saved_flags handling.
    
    So drop the explicit lsr_saved_flags handling.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Link: https://lore.kernel.org/r/20230202104501.264686-1-u.kleine-koenig@pengutronix.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/8250/8250_port.c
+++ drivers/tty/serial/8250/8250_port.c
@@ -1512,8 +1512,6 @@ static inline void __stop_tx(struct uart_8250_port *p)
 		u16 lsr = serial_lsr_in(p);
 		u64 stop_delay = 0;
 
-		p->lsr_saved_flags |= lsr & LSR_SAVE_FLAGS;
-
 		if (!(lsr & UART_LSR_THRE))
 			return;
 		/*
