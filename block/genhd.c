178fa7d49815ea8001f43ade37a22072829fd8ab
Christoph Hellwig <hch@lst.de>
2023-02-03 16:03:43 +0100

    blk-cgroup: delay blk-cgroup initialization until add_disk
    There is no need to initialize the cgroup code before the disk is marked
    live.  Moving the cgroup initialization earlier will help to have a
    fully initialized struct device in the gendisk for the cgroup code to
    use in the future.  Similarly tear the cgroup information down in
    del_gendisk to be symmetric and because none of the cgroup tracking is
    needed once non-passthrough I/O stops.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Andreas Herrmann <aherrmann@suse.de>
    Acked-by: Tejun Heo <tj@kernel.org>
    Link: https://lore.kernel.org/r/20230203150400.3199230-3-hch@lst.de
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/genhd.c
+++ block/genhd.c
@@ -466,9 +466,13 @@ int __must_check device_add_disk(struct device *parent, struct gendisk *disk,
 	 */
 	pm_runtime_set_memalloc_noio(ddev, true);
 
-	ret = blk_integrity_add(disk);
+	ret = blkcg_init_disk(disk);
 	if (ret)
 		goto out_del_block_link;
+
+	ret = blk_integrity_add(disk);
+	if (ret)
+		goto out_blkcg_exit;
 
 	disk->part0->bd_holder_dir =
 		kobject_create_and_add("holders", &ddev->kobj);
@@ -534,6 +538,8 @@ out_put_holder_dir:
 	kobject_put(disk->part0->bd_holder_dir);
 out_del_integrity:
 	blk_integrity_del(disk);
+out_blkcg_exit:
+	blkcg_exit_disk(disk);
 out_del_block_link:
 	if (!sysfs_deprecated)
 		sysfs_remove_link(block_depr, dev_name(ddev));
@@ -661,6 +667,8 @@ void del_gendisk(struct gendisk *disk)
 	}
 	rq_qos_exit(q);
 	blk_mq_unquiesce_queue(q);
+
+	blkcg_exit_disk(disk);
 
 	/*
 	 * If the disk does not own the queue, allow using passthrough requests
@@ -1171,8 +1179,6 @@ static void disk_release(struct device *dev)
 	    !test_bit(GD_ADDED, &disk->state))
 		blk_mq_exit_queue(disk->queue);
 
-	blkcg_exit_disk(disk);
-
 	bioset_exit(&disk->bio_split);
 
 	disk_release_events(disk);
@@ -1385,9 +1391,6 @@ struct gendisk *__alloc_disk_node(struct request_queue *q, int node_id,
 	if (xa_insert(&disk->part_tbl, 0, disk->part0, GFP_KERNEL))
 		goto out_destroy_part_tbl;
 
-	if (blkcg_init_disk(disk))
-		goto out_erase_part0;
-
 	rand_initialize_disk(disk);
 	disk_to_dev(disk)->class = &block_class;
 	disk_to_dev(disk)->type = &disk_type;
@@ -1400,8 +1403,6 @@ struct gendisk *__alloc_disk_node(struct request_queue *q, int node_id,
 #endif
 	return disk;
 
-out_erase_part0:
-	xa_erase(&disk->part_tbl, 0);
 out_destroy_part_tbl:
 	xa_destroy(&disk->part_tbl);
 	disk->part0->bd_disk = NULL;
