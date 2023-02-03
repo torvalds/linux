3361eb2256f9e427c03a32f887519f06d192289a
Bjorn Helgaas <bhelgaas@google.com>
2023-02-03 13:57:29 -0600

    Revert "PCI/ASPM: Save L1 PM Substates Capability for suspend/resume"
    commit a7152be79b627428c628da2a887ca4b2512a78fd upstream.
    
    This reverts commit 4ff116d0d5fd8a025604b0802d93a2d5f4e465d1.
    
    Tasev Nikola and Mark Enriquez reported that resume from suspend was broken
    in v6.1-rc1.  Tasev bisected to a47126ec29f5 ("PCI/PTM: Cache PTM
    Capability offset"), but we can't figure out how that could be related.
    
    Mark saw the same symptoms and bisected to 4ff116d0d5fd ("PCI/ASPM: Save L1
    PM Substates Capability for suspend/resume"), which does have a connection:
    it restores L1 Substates configuration while ASPM L1 may be enabled:
    
      pci_restore_state
        pci_restore_aspm_l1ss_state
          aspm_program_l1ss
            pci_write_config_dword(PCI_L1SS_CTL1, ctl1)         # L1SS restore
        pci_restore_pcie_state
          pcie_capability_write_word(PCI_EXP_LNKCTL, cap[i++])  # L1 restore
    
    which is a problem because PCIe r6.0, sec 5.5.4, requires that:
    
      If setting either or both of the enable bits for ASPM L1 PM
      Substates, both ports must be configured as described in this
      section while ASPM L1 is disabled.
    
    Separately, Thomas Witt reported that 5e85eba6f50d ("PCI/ASPM: Refactor L1
    PM Substates Control Register programming") broke suspend/resume, and it
    depends on 4ff116d0d5fd.
    
    Revert 4ff116d0d5fd ("PCI/ASPM: Save L1 PM Substates Capability for
    suspend/resume") to fix the resume issue and enable revert of 5e85eba6f50d
    to fix the issue Thomas reported.
    
    Note that reverting 4ff116d0d5fd means L1 Substates config may be lost on
    suspend/resume.  As far as we know the system will use more power but will
    still *work* correctly.
    
    Fixes: 4ff116d0d5fd ("PCI/ASPM: Save L1 PM Substates Capability for suspend/resume")
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=216782
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=216877
    Reported-by: Tasev Nikola <tasev.stefanoska@skynet.be>
    Reported-by: Mark Enriquez <enriquezmark36@gmail.com>
    Reported-by: Thomas Witt <kernel@witt.link>
    Tested-by: Mark Enriquez <enriquezmark36@gmail.com>
    Tested-by: Thomas Witt <kernel@witt.link>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Cc: stable@vger.kernel.org	# v6.1+
    Cc: Vidya Sagar <vidyas@nvidia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -565,14 +565,10 @@ bool pcie_wait_for_link(struct pci_dev *pdev, bool active);
 void pcie_aspm_init_link_state(struct pci_dev *pdev);
 void pcie_aspm_exit_link_state(struct pci_dev *pdev);
 void pcie_aspm_powersave_config_link(struct pci_dev *pdev);
-void pci_save_aspm_l1ss_state(struct pci_dev *dev);
-void pci_restore_aspm_l1ss_state(struct pci_dev *dev);
 #else
 static inline void pcie_aspm_init_link_state(struct pci_dev *pdev) { }
 static inline void pcie_aspm_exit_link_state(struct pci_dev *pdev) { }
 static inline void pcie_aspm_powersave_config_link(struct pci_dev *pdev) { }
-static inline void pci_save_aspm_l1ss_state(struct pci_dev *dev) { }
-static inline void pci_restore_aspm_l1ss_state(struct pci_dev *dev) { }
 #endif
 
 #ifdef CONFIG_PCIE_ECRC
