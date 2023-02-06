234faf40fadd97dcf2f59985f43f01dc20e97d34
Huang Yiwei <quic_hyiwei@quicinc.com>
2023-02-06 17:44:15 +0800

    ANDROID: irq: Add vendor data field to struct irq_desc
    Add vendor data field to struct irq_desc to track the irq count
    growth, vendor module can store the current count value and
    calculate the delta in next scan cycle, helpful to enhance irq
    performance and debug irq related issues.
    
    Bug: 267983545
    Change-Id: I6b4c12aeaa09ee133918870772833a777190dccc
    Signed-off-by: Huang Yiwei <quic_hyiwei@quicinc.com>

--- include/linux/irqdesc.h
+++ include/linux/irqdesc.h
@@ -5,6 +5,7 @@
 #include <linux/rcupdate.h>
 #include <linux/kobject.h>
 #include <linux/mutex.h>
+#include <linux/android_vendor.h>
 
 /*
  * Core internal functions to deal with irq descriptors
@@ -102,6 +103,7 @@ struct irq_desc {
 	int			parent_irq;
 	struct module		*owner;
 	const char		*name;
+	ANDROID_VENDOR_DATA(1);
 } ____cacheline_internodealigned_in_smp;
 
 #ifdef CONFIG_SPARSE_IRQ
