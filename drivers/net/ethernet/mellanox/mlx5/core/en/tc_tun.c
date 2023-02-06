197c00029294528e35ba97e4b8b54155c6654261
Roi Dayan <roid@nvidia.com>
2023-02-06 14:40:24 +0200

    net/mlx5e: Use a simpler comparison for uplink rep
    get_route_and_out_devs()  is uses the following condition
    mlx5e_eswitch_rep() && mlx5e_is_uplink_rep() to check if a given netdev is the
    uplink rep.
    
    Alternatively we can just use the straight forward version
    mlx5e_eswitch_uplink_rep() that only checks if a given netdev is uplink rep.
    
    Signed-off-by: Roi Dayan <roid@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en/tc_tun.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en/tc_tun.c
@@ -93,8 +93,7 @@ static int get_route_and_out_devs(struct mlx5e_priv *priv,
 	else
 		return -EOPNOTSUPP;
 
-	if (!(mlx5e_eswitch_rep(*out_dev) &&
-	      mlx5e_is_uplink_rep(netdev_priv(*out_dev))))
+	if (!mlx5e_eswitch_uplink_rep(*out_dev))
 		return -EOPNOTSUPP;
 
 	if (mlx5e_eswitch_uplink_rep(priv->netdev) && *out_dev != priv->netdev &&
