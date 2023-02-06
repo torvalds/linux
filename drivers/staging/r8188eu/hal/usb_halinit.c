a0b3a1c269d32a0d96b50de4b2a301add847b3cf
Martin Kaiser <martin@kaiser.cx>
2023-02-06 21:17:57 +0100

    staging: r8188eu: use standard error codes in rtl8188eu_inirp_init
    Replace the driver-specific _SUCCESS and _FAIL error codes in
    rtl8188eu_inirp_init. Return 0 for success or a negative error
    code as we do elsewhere in the kernel.
    
    Signed-off-by: Martin Kaiser <martin@kaiser.cx>
    Tested-by: Philipp Hortmann <philipp.g.hortmann@gmail.com> # Edimax N150
    Link: https://lore.kernel.org/r/20230206201800.139195-4-martin@kaiser.cx
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/r8188eu/hal/usb_halinit.c
+++ drivers/staging/r8188eu/hal/usb_halinit.c
@@ -848,29 +848,25 @@ u32 rtl8188eu_hal_deinit(struct adapter *Adapter)
 	return _SUCCESS;
  }
 
-unsigned int rtl8188eu_inirp_init(struct adapter *Adapter)
+int rtl8188eu_inirp_init(struct adapter *Adapter)
 {
 	u8 i;
 	struct recv_buf *precvbuf;
-	uint	status;
 	struct recv_priv *precvpriv = &Adapter->recvpriv;
-
-	status = _SUCCESS;
+	int ret;
 
 	/* issue Rx irp to receive data */
 	precvbuf = (struct recv_buf *)precvpriv->precv_buf;
 	for (i = 0; i < NR_RECVBUFF; i++) {
-		if (!rtw_read_port(Adapter, precvbuf)) {
-			status = _FAIL;
-			goto exit;
-		}
+		ret = rtw_read_port(Adapter, precvbuf);
+		if (ret)
+			return ret;
 
 		precvbuf++;
 		precvpriv->free_recv_buf_queue_cnt--;
 	}
 
-exit:
-	return status;
+	return 0;
 }
 
 /*  */
