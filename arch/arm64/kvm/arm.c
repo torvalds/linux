67d953d4d7bec5333c1036258753573d9904753b
Marc Zyngier <maz@kernel.org>
2023-02-07 09:43:21 +0000

    KVM: arm64: Fix non-kerneldoc comments
    The robots amongts us have started spitting out irritating emails about
    random errors such as:
    
    <quote>
    arch/arm64/kvm/arm.c:2207: warning: expecting prototype for Initialize Hyp().
    Prototype was for kvm_arm_init() instead
    </quote>
    
    which makes little sense until you finally grok what they are on about:
    comments that look like a kerneldoc, but that aren't.
    
    Let's address this before I get even more irritated... ;-)
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/63e139e1.J5AHO6vmxaALh7xv%25lkp@intel.com
    Link: https://lore.kernel.org/r/20230207094321.1238600-1-maz@kernel.org
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -1909,9 +1909,7 @@ static int kvm_hyp_init_protection(u32 hyp_va_bits)
 	return 0;
 }
 
-/**
- * Inits Hyp-mode on all online CPUs
- */
+/* Inits Hyp-mode on all online CPUs */
 static int init_hyp_mode(void)
 {
 	u32 hyp_va_bits;
@@ -2187,9 +2185,7 @@ void kvm_arch_irq_bypass_start(struct irq_bypass_consumer *cons)
 	kvm_arm_resume_guest(irqfd->kvm);
 }
 
-/**
- * Initialize Hyp-mode and memory mappings on all CPUs.
- */
+/* Initialize Hyp-mode and memory mappings on all CPUs */
 int kvm_arch_init(void *opaque)
 {
 	int err;
