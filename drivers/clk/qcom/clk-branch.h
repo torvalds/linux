0932e565ba7c828d5ec59f872cb3ad6b070dc003
Konrad Dybcio <konrad.dybcio@linaro.org>
2023-02-08 10:13:32 +0100

    clk: qcom: branch: Add helper functions for setting SLEEP/WAKE bits
    HLOS-controlled branch clocks on non-ancient Qualcomm platforms
    feature SLEEP and WAKE fields which can be written to to configure
    how long the clock hardware should wait internally before being
    (un)gated. Some very sensitive clocks need to have these values
    programmed to prevent putting the hardware in a not-exactly-good
    state. Add definitions of these fields and introduce helpers for
    setting them inside clock drivers.
    
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Konrad Dybcio <konrad.dybcio@linaro.org>
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    Link: https://lore.kernel.org/r/20230208091340.124641-3-konrad.dybcio@linaro.org

--- drivers/clk/qcom/clk-branch.h
+++ drivers/clk/qcom/clk-branch.h
@@ -4,6 +4,7 @@
 #ifndef __QCOM_CLK_BRANCH_H__
 #define __QCOM_CLK_BRANCH_H__
 
+#include <linux/bitfield.h>
 #include <linux/clk-provider.h>
 
 #include "clk-regmap.h"
@@ -41,6 +42,8 @@ struct clk_branch {
 #define CBCR_FORCE_MEM_CORE_ON		BIT(14)
 #define CBCR_FORCE_MEM_PERIPH_ON	BIT(13)
 #define CBCR_FORCE_MEM_PERIPH_OFF	BIT(12)
+#define CBCR_WAKEUP			GENMASK(11, 8)
+#define CBCR_SLEEP			GENMASK(7, 4)
 
 static inline void qcom_branch_set_force_mem_core(struct regmap *regmap,
 						  struct clk_branch clk, bool on)
@@ -61,6 +64,18 @@ static inline void qcom_branch_set_force_periph_off(struct regmap *regmap,
 {
 	regmap_update_bits(regmap, clk.halt_reg, CBCR_FORCE_MEM_PERIPH_OFF,
 			   on ? CBCR_FORCE_MEM_PERIPH_OFF : 0);
+}
+
+static inline void qcom_branch_set_wakeup(struct regmap *regmap, struct clk_branch clk, u32 val)
+{
+	regmap_update_bits(regmap, clk.halt_reg, CBCR_WAKEUP,
+			   FIELD_PREP(CBCR_WAKEUP, val));
+}
+
+static inline void qcom_branch_set_sleep(struct regmap *regmap, struct clk_branch clk, u32 val)
+{
+	regmap_update_bits(regmap, clk.halt_reg, CBCR_SLEEP,
+			   FIELD_PREP(CBCR_SLEEP, val));
 }
 
 extern const struct clk_ops clk_branch_ops;
