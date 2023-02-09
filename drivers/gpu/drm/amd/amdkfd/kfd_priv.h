9becfff9f91e350fd4d6f16e9f117f9227258fb0
Philip Yang <Philip.Yang@amd.com>
2023-02-09 18:23:16 -0500

    drm/amdkfd: Flush TLB after unmapping for GFX v9.4.3
    [ Upstream commit 75dda67c7213c3e0d17244a8c42547c27ee746f8 ]
    
    kfd_flush_tlb_after_unmap should return true for GFX v9.4.3, to do TLB
    heavyweight flush after unmapping from GPU to guarantee that the GPU
    will not access pages after they have been unmapped. This also helps
    improve the mapping to GPU performance.
    
    Without this, KFD accidently flush TLB after mapping to GPU because the
    vm update sequence number is increased by previous unmapping.
    
    Signed-off-by: Philip Yang <Philip.Yang@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Stable-dep-of: edcfe22985d0 ("drm/amdkfd: Insert missing TLB flush on GFX10 and later")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/amdkfd/kfd_priv.h
+++ drivers/gpu/drm/amd/amdkfd/kfd_priv.h
@@ -1349,9 +1349,9 @@ void kfd_flush_tlb(struct kfd_process_device *pdd, enum TLB_FLUSH_TYPE type);
 
 static inline bool kfd_flush_tlb_after_unmap(struct kfd_dev *dev)
 {
-	return KFD_GC_VERSION(dev) == IP_VERSION(9, 4, 2) ||
-	       (KFD_GC_VERSION(dev) == IP_VERSION(9, 4, 1) &&
-	       dev->adev->sdma.instance[0].fw_version >= 18) ||
+	return KFD_GC_VERSION(dev) == IP_VERSION(9, 4, 3) ||
+	       KFD_GC_VERSION(dev) == IP_VERSION(9, 4, 2) ||
+	       (KFD_GC_VERSION(dev) == IP_VERSION(9, 4, 1) && dev->sdma_fw_version >= 18) ||
 	       KFD_GC_VERSION(dev) == IP_VERSION(9, 4, 0);
 }
 
