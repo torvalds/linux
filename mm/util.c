6bdfc60cf0f9771d592a006dcd2cf6e40e1ccd79
Jakub Wilk <jwilk@jwilk.net>
2023-02-10 21:33:16 +0100

    mm: fix typo in __vm_enough_memory warning
    Link: https://lkml.kernel.org/r/20230210203316.5613-1-jwilk@jwilk.net
    Signed-off-by: Jakub Wilk <jwilk@jwilk.net>
    Acked-by: Mike Rapoport (IBM) <rppt@kernel.org>
    Cc: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/util.c
+++ mm/util.c
@@ -967,7 +967,7 @@ int __vm_enough_memory(struct mm_struct *mm, long pages, int cap_sys_admin)
 	if (percpu_counter_read_positive(&vm_committed_as) < allowed)
 		return 0;
 error:
-	pr_warn_ratelimited("%s: pid: %d, comm: %s, no enough memory for the allocation\n",
+	pr_warn_ratelimited("%s: pid: %d, comm: %s, not enough memory for the allocation\n",
 			    __func__, current->pid, current->comm);
 	vm_unacct_memory(pages);
 
