b87c52e431adfe2dfe8634216b317b4a952aa9fc
Alexander Gordeev <agordeev@linux.ibm.com>
2023-02-10 01:02:52 +0100

    s390/dasd: sort out physical vs virtual pointers usage
    This does not fix a real bug, since virtual addresses
    are currently indentical to physical ones.
    
    Signed-off-by: Alexander Gordeev <agordeev@linux.ibm.com>
    Signed-off-by: Stefan Haberland <sth@linux.ibm.com>
    Link: https://lore.kernel.org/r/20230210000253.1644903-2-sth@linux.ibm.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/s390/block/dasd_eer.c
+++ drivers/s390/block/dasd_eer.c
@@ -491,7 +491,7 @@ int dasd_eer_enable(struct dasd_device *device)
 	ccw->cmd_code = DASD_ECKD_CCW_SNSS;
 	ccw->count = SNSS_DATA_SIZE;
 	ccw->flags = 0;
-	ccw->cda = (__u32)(addr_t) cqr->data;
+	ccw->cda = (__u32)virt_to_phys(cqr->data);
 
 	cqr->buildclk = get_tod_clock();
 	cqr->status = DASD_CQR_FILLED;
