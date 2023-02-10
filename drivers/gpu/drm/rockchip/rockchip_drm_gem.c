e4d1514867ef75ed0a24a0948f81ba4803fceae9
Jianqun Xu <jay.xu@rock-chips.com>
2023-02-10 17:59:05 +0800

    drm/rockchip: gem: fix for VVOP enabled but VOP and VOP2 not
    In some case we need to select the CONFIG_ROCKCHIP_VVOP but unselect the
    CONFIG_ROCKCHIP_VOP and CONFIG_ROCKCHIP_VOP2, this patch can fix it to
    not do vop iommu map/unmap.
    
    Change-Id: Ib48a11fd1dc6c0230701edb94c19c9f2a1a08a0a
    Signed-off-by: Jianqun Xu <jay.xu@rock-chips.com>

--- drivers/gpu/drm/rockchip/rockchip_drm_gem.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_gem.c
@@ -414,6 +414,11 @@ static void rockchip_gem_free_secure(struct rockchip_gem_object *rk_obj)
 		      rk_obj->base.size);
 }
 
+static inline bool is_vop_enabled(void)
+{
+	return (IS_ENABLED(CONFIG_ROCKCHIP_VOP) || IS_ENABLED(CONFIG_ROCKCHIP_VOP2));
+}
+
 static int rockchip_gem_alloc_buf(struct rockchip_gem_object *rk_obj,
 				  bool alloc_kmap)
 {
@@ -422,7 +427,7 @@ static int rockchip_gem_alloc_buf(struct rockchip_gem_object *rk_obj,
 	struct rockchip_drm_private *private = drm->dev_private;
 	int ret = 0;
 
-	if (!private->domain)
+	if (!private->domain && is_vop_enabled())
 		rk_obj->flags |= ROCKCHIP_BO_CONTIG;
 
 	if (rk_obj->flags & ROCKCHIP_BO_SECURE) {
@@ -462,7 +467,7 @@ static int rockchip_gem_alloc_buf(struct rockchip_gem_object *rk_obj,
 		ret = rockchip_gem_iommu_map(rk_obj);
 		if (ret < 0)
 			goto err_free;
-	} else {
+	} else if (is_vop_enabled()) {
 		WARN_ON(!rk_obj->dma_handle);
 		rk_obj->dma_addr = rk_obj->dma_handle;
 	}
