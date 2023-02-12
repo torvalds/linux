0e4f2c4567953a230b420f2c4460c3368d6509db
Mike Rapoport <rppt@kernel.org>
2023-02-12 10:46:10 +0200

    char/agp: consolidate {alloc,free}_gatt_pages()
    There is a copy of alloc_gatt_pages() and free_gatt_pages in several
    architectures in arch/$ARCH/include/asm/agp.h. All the copies do exactly
    the same: alias alloc_gatt_pages() to __get_free_pages(GFP_KERNEL) and
    alias free_gatt_pages() to free_pages().
    
    Define alloc_gatt_pages() and free_gatt_pages() in drivers/char/agp/agp.h
    and drop per-architecture definitions.
    
    Signed-off-by: Mike Rapoport (IBM) <rppt@kernel.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/powerpc/include/asm/agp.h
+++ arch/powerpc/include/asm/agp.h
@@ -9,11 +9,5 @@
 #define unmap_page_from_agp(page) do {} while (0)
 #define flush_agp_cache() mb()
 
-/* GATT allocation. Returns/accepts GATT kernel virtual address. */
-#define alloc_gatt_pages(order)		\
-	((char *)__get_free_pages(GFP_KERNEL, (order)))
-#define free_gatt_pages(table, order)	\
-	free_pages((unsigned long)(table), (order))
-
 #endif /* __KERNEL__ */
 #endif	/* _ASM_POWERPC_AGP_H */
--- arch/sparc/include/asm/agp.h
+++ arch/sparc/include/asm/agp.h
@@ -8,10 +8,4 @@
 #define unmap_page_from_agp(page)	do { } while (0)
 #define flush_agp_cache()		mb()
 
-/* GATT allocation. Returns/accepts GATT kernel virtual address. */
-#define alloc_gatt_pages(order)		\
-	((char *)__get_free_pages(GFP_KERNEL, (order)))
-#define free_gatt_pages(table, order)	\
-	free_pages((unsigned long)(table), (order))
-
 #endif
