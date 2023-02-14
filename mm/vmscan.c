cdded861182142ac4488a4d64c571107aeb77f53
Kalesh Singh <kaleshsingh@google.com>
2023-02-14 14:17:57 -0800

    ANDROID: MGLRU: Don't skip anon reclaim if swap low
    MGLRU tries to avoid doing unnecessary anon reclaim work if swap is
    low by checking if the available swap is less than the MIN_BATCH_SIZE
    (256kB). This can lead to unintened consequences where PSI pressure is
    less and LMKD doesn't wake up in time to avoid file cache thrashing.
    
    Remove this check to preserve the old bahavior. This can be improved
    later on once we have a low swap notification event from the kernel.
    
    Bug: 268574308
    Change-Id: Id381316931a9cf6e7ea8b1ea7800c77f176c9892
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3174,8 +3174,7 @@ static int get_swappiness(struct lruvec *lruvec, struct scan_control *sc)
 	struct mem_cgroup *memcg = lruvec_memcg(lruvec);
 	struct pglist_data *pgdat = lruvec_pgdat(lruvec);
 
-	if (!can_demote(pgdat->node_id, sc) &&
-	    mem_cgroup_get_nr_swap_pages(memcg) < MIN_LRU_BATCH)
+	if (!can_demote(pgdat->node_id, sc))
 		return 0;
 
 	return mem_cgroup_swappiness(memcg);
