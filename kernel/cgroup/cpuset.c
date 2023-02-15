a42f6e7d0aa09de1ccd2e8094fb069ae7860b9fe
Greg Kroah-Hartman <gregkh@google.com>
2023-02-15 09:25:47 +0000

    Revert "ANDROID: sched/cpuset: Add vendor hook to change tasks affinity"
    This reverts commit 8ecd88d9d38f849d5aac59596ebc7a8679405f5b as it is
    broken with regards to upstream changes made in 6.1.12.
    
    If this is still needed, it can be brought back in a way that works
    properly based on the changes made upstream.
    
    Bug: 174125747
    Cc: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
    Cc: Sai Harshini Nimmala <quic_snimmala@quicinc.com>
    Change-Id: Ic3163351faabbecbce688a87215f79ca3b5d6188
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -67,8 +67,6 @@
 #include <linux/cgroup.h>
 #include <linux/wait.h>
 
-#include <trace/hooks/sched.h>
-
 DEFINE_STATIC_KEY_FALSE(cpusets_pre_enable_key);
 DEFINE_STATIC_KEY_FALSE(cpusets_enabled_key);
 
@@ -1198,18 +1196,6 @@ void rebuild_sched_domains(void)
 }
 EXPORT_SYMBOL_GPL(rebuild_sched_domains);
 
-static int update_cpus_allowed(struct cpuset *cs, struct task_struct *p,
-				const struct cpumask *new_mask)
-{
-	int ret = -EINVAL;
-
-	trace_android_rvh_update_cpus_allowed(p, cs->cpus_requested, new_mask, &ret);
-	if (!ret)
-		return ret;
-
-	return set_cpus_allowed_ptr(p, new_mask);
-}
-
 /**
  * update_tasks_cpumask - Update the cpumasks of tasks in the cpuset.
  * @cs: the cpuset in which each task's cpus_allowed mask needs to be changed
@@ -1232,7 +1218,7 @@ static void update_tasks_cpumask(struct cpuset *cs)
 		if (top_cs && (task->flags & PF_KTHREAD) &&
 		    kthread_is_per_cpu(task))
 			continue;
-		update_cpus_allowed(cs, task, cs->effective_cpus);
+		set_cpus_allowed_ptr(task, cs->effective_cpus);
 	}
 	css_task_iter_end(&it);
 }
@@ -2542,7 +2528,7 @@ static void cpuset_attach(struct cgroup_taskset *tset)
 		 * can_attach beforehand should guarantee that this doesn't
 		 * fail.  TODO: have a better way to handle failure here
 		 */
-		WARN_ON_ONCE(update_cpus_allowed(cs, task, cpus_attach));
+		WARN_ON_ONCE(set_cpus_allowed_ptr(task, cpus_attach));
 
 		cpuset_change_task_nodemask(task, &cpuset_attach_nodemask_to);
 		cpuset_update_task_spread_flag(cs, task);
