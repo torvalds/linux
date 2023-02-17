5519fea915effccf57fa1369669f4a6bc6c44a07
Christian König <christian.koenig@amd.com>
2023-02-17 13:54:27 +0100

    drm: remove drm_dev_set_unique
    Not used by any drivers any more, the only use case in drm_dev_init()
    can be inlined now.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Jeffrey Hugo <quic_jhugo@quicinc.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230316082035.567520-2-christian.koenig@amd.com

--- include/drm/drm_drv.h
+++ include/drm/drm_drv.h
@@ -584,8 +584,6 @@ static inline bool drm_drv_uses_atomic_modeset(struct drm_device *dev)
 }
 
 
-int drm_dev_set_unique(struct drm_device *dev, const char *name);
-
 /* TODO: Inline drm_firmware_drivers_only() in all its callers. */
 static inline bool drm_firmware_drivers_only(void)
 {
