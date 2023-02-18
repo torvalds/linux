96c79da2e4d1a25e73916c656fe4ccf7fc8176ae
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2023-02-18 16:29:40 +0100

    wifi: rtw88: mac: Add support for the SDIO HCI in rtw_pwr_seq_parser()
    rtw_pwr_seq_parser() needs to know about the HCI bus interface mask for
    the SDIO bus so it can parse the chip state change sequences.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Reviewed-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://lore.kernel.org/r/20230218152944.48842-2-martin.blumenstingl@googlemail.com

--- drivers/net/wireless/realtek/rtw88/mac.c
+++ drivers/net/wireless/realtek/rtw88/mac.c
@@ -222,6 +222,9 @@ static int rtw_pwr_seq_parser(struct rtw_dev *rtwdev,
 	case RTW_HCI_TYPE_USB:
 		intf_mask = RTW_PWR_INTF_USB_MSK;
 		break;
+	case RTW_HCI_TYPE_SDIO:
+		intf_mask = RTW_PWR_INTF_SDIO_MSK;
+		break;
 	default:
 		return -EINVAL;
 	}
