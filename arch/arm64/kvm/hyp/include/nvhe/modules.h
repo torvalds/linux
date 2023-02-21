bf019165fec551395cb4391fe4df0a783bc258ec
Vincent Donnefort <vdonnefort@google.com>
2023-02-21 16:38:39 +0000

    ANDROID: KVM: arm64: Cleanup useless declarations
    While introducing ("KVM: arm64: Rework pKVM module locking") some bits
    have been missed. Remove them.
    
    Bug: 254835242
    Change-Id: Ib2b5c84c6d900cfa452232c9ec62483dd230af0c
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/modules.h
+++ arch/arm64/kvm/hyp/include/nvhe/modules.h
@@ -17,9 +17,6 @@ int reset_pkvm_priv_hcall_limit(void);
 int __pkvm_init_module(void *module_init);
 int __pkvm_register_hcall(unsigned long hfn_hyp_va);
 int handle_host_dynamic_hcall(struct kvm_cpu_context *host_ctxt);
-void pkvm_modules_lock(void);
-void pkvm_modules_unlock(void);
-bool pkvm_modules_enabled(void);
 int __pkvm_close_module_registration(void);
 #else
 static inline int __pkvm_init_module(void *module_init) { return -EOPNOTSUPP; }
@@ -29,8 +26,5 @@ static inline int handle_host_dynamic_hcall(struct kvm_cpu_context *host_ctxt)
 {
 	return HCALL_UNHANDLED;
 }
-static inline void pkvm_modules_lock(void) { }
-static inline void pkvm_modules_unlock(void) { }
-static inline bool pkvm_modules_enabled(void) { return false; }
 static inline int __pkvm_close_module_registration(void) { return -EOPNOTSUPP; }
 #endif
