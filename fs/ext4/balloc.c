ad3f09be6cfe332be8ff46c78e6ec0f8839107aa
Kemeng Shi <shikemeng@huaweicloud.com>
2023-02-21 19:59:16 +0800

    ext4: remove unnecessary check in ext4_bg_num_gdb_nometa
    We only call ext4_bg_num_gdb_nometa if there is no meta_bg feature or group
    does not reach first_meta_bg, so group must not reside in meta group. Then
    group has a global gdt if superblock exists. Just remove confusing branch
    that meta_bg feature exists.
    
    Signed-off-by: Kemeng Shi <shikemeng@huaweicloud.com>
    Link: https://lore.kernel.org/r/20230221115919.1918161-5-shikemeng@huaweicloud.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/balloc.c
+++ fs/ext4/balloc.c
@@ -844,10 +844,7 @@ static unsigned long ext4_bg_num_gdb_nometa(struct super_block *sb,
 	if (!ext4_bg_has_super(sb, group))
 		return 0;
 
-	if (ext4_has_feature_meta_bg(sb))
-		return le32_to_cpu(EXT4_SB(sb)->s_es->s_first_meta_bg);
-	else
-		return EXT4_SB(sb)->s_gdb_count;
+	return EXT4_SB(sb)->s_gdb_count;
 }
 
 /**
