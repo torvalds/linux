28013c35629647679e10cb1bbf36c3feec2af756
Kalle Valo <quic_kvalo@quicinc.com>
2023-02-22 18:40:14 +0200

    wifi: ath12k: remove memset with byte count of 278528
    Sparse warns:
    
    drivers/net/wireless/ath/ath12k/dp.c:1471:15: warning: memset with byte count of 278528
    
    There's no need to use memset() here, instead call dma_alloc_coherent() with __GFP_ZERO.
    
    While at it, remove an extra line before the error handler.
    
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://lore.kernel.org/r/20230222164014.860-1-kvalo@kernel.org

--- drivers/net/wireless/ath/ath12k/dp.c
+++ drivers/net/wireless/ath/ath12k/dp.c
@@ -1461,14 +1461,11 @@ static int ath12k_dp_reoq_lut_setup(struct ath12k_base *ab)
 	dp->reoq_lut.vaddr = dma_alloc_coherent(ab->dev,
 						DP_REOQ_LUT_SIZE,
 						&dp->reoq_lut.paddr,
-						GFP_KERNEL);
-
+						GFP_KERNEL | __GFP_ZERO);
 	if (!dp->reoq_lut.vaddr) {
 		ath12k_warn(ab, "failed to allocate memory for reoq table");
 		return -ENOMEM;
 	}
-
-	memset(dp->reoq_lut.vaddr, 0, DP_REOQ_LUT_SIZE);
 
 	ath12k_hif_write32(ab, HAL_SEQ_WCSS_UMAC_REO_REG + HAL_REO1_QDESC_LUT_BASE0(ab),
 			   dp->reoq_lut.paddr);
