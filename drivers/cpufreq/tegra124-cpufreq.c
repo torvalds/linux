f35512777e31085f1ade7febed6c2038adb1fb07
Nick Alcock <nick.alcock@oracle.com>
2023-02-22 12:14:40 +0000

    kbuild, cpufreq: tegra124: remove MODULE_LICENSE in non-modules
    Since commit 8b41fc4454e ("kbuild: create modules.builtin without
    Makefile.modbuiltin or tristate.conf"), MODULE_LICENSE declarations
    are used to identify modules. As a consequence, uses of the macro
    in non-modules will cause modprobe to misidentify their containing
    object file as a module when it is not (false positives), and modprobe
    might succeed rather than failing with a suitable error message.
    
    So remove it in the files in this commit, none of which can be built as
    modules.
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    Suggested-by: Luis Chamberlain <mcgrof@kernel.org>
    Cc: Luis Chamberlain <mcgrof@kernel.org>
    Cc: linux-modules@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Cc: Hitomi Hasegawa <hasegawa-hitomi@fujitsu.com>
    Cc: "Rafael J. Wysocki" <rafael@kernel.org>
    Cc: Viresh Kumar <viresh.kumar@linaro.org>
    Cc: Thierry Reding <thierry.reding@gmail.com>
    Cc: Jonathan Hunter <jonathanh@nvidia.com>
    Cc: linux-pm@vger.kernel.org
    Cc: linux-tegra@vger.kernel.org
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/cpufreq/tegra124-cpufreq.c
+++ drivers/cpufreq/tegra124-cpufreq.c
@@ -221,4 +221,3 @@ module_init(tegra_cpufreq_init);
 
 MODULE_AUTHOR("Tuomas Tynkkynen <ttynkkynen@nvidia.com>");
 MODULE_DESCRIPTION("cpufreq driver for NVIDIA Tegra124");
-MODULE_LICENSE("GPL v2");
