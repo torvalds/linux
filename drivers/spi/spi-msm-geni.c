arter97/android_kernel_oneplus_sm8350

spi: spi-msm-geni: Add support for SPI Master cross connect
Configure the GENI_CFG_REG80 to change the functionality of
MISO and MOSI GPIO of SPI master when it is cross connected.

Change-Id: I141bf91b34a2b68c348dc07de8ae4a9818d8cb37
Signed-off-by: Aniket Randive <arandive@codeaurora.org>

drivers/spi/spi-msm-geni.c
@@ -185,11 +185,13 @@ struct spi_geni_master {
 	bool slave_cross_connected;
 	bool use_fixed_timeout;
 	struct spi_geni_ssr spi_ssr;
+	bool master_cross_connect;
 };
 
 static void spi_slv_setup(struct spi_geni_master *mas);
 static void ssr_spi_force_suspend(struct device *dev);
 static void ssr_spi_force_resume(struct device *dev);
+static void spi_master_setup(struct spi_geni_master *mas);
 
 static ssize_t spi_slave_state_show(struct device *dev,
 		struct device_attribute *attr, char *buf)
@@ -216,6 +218,17 @@ static ssize_t spi_slave_state_store(struct device *dev,
 }
 
 static DEVICE_ATTR_RW(spi_slave_state);
+
+static void spi_master_setup(struct spi_geni_master *mas)
+{
+	geni_write_reg(OTHER_IO_OE | IO2_DATA_IN_SEL | RX_DATA_IN_SEL |
+		IO_MACRO_IO3_SEL | IO_MACRO_IO2_SEL | IO_MACRO_IO0_SEL,
+					mas->base, GENI_CFG_REG80);
+	geni_write_reg(START_TRIGGER, mas->base, SE_GENI_CFG_SEQ_START);
+
+	/* ensure data is written to hardware register */
+	wmb();
+}
 
 static void spi_slv_setup(struct spi_geni_master *mas)
 {
@@ -1202,6 +1215,9 @@ static int spi_geni_mas_setup(struct spi_master *spi)
 
 		if (spi->slave)
 			spi_slv_setup(mas);
+
+		if (mas->master_cross_connect)
+			spi_master_setup(mas);
 	}
 
 	geni_se_init(mas->base, 0x0, (mas->tx_fifo_depth - 2));
@@ -2210,6 +2226,9 @@ static int spi_geni_probe(struct platform_device *pdev)
 
 	geni_mas->disable_dma = of_property_read_bool(pdev->dev.of_node,
 		"qcom,disable-dma");
+
+	if (of_property_read_bool(pdev->dev.of_node, "qcom,master-cross-connect"))
+		geni_mas->master_cross_connect = true;
 
 	slave_en  = of_property_read_bool(pdev->dev.of_node,
 			 "qcom,slv-ctrl");

torvalds/linux

Signed-off-by: Aniket Randive <quic_arandive@quicinc.com>

drivers/spi/spi-msm-geni.c
@@ -221,6 +221,7 @@ struct spi_geni_master {
 	bool slave_setup;
 	bool slave_state;
 	bool slave_cross_connected;
+	bool master_cross_connect;
 };
 
 /**
@@ -299,6 +300,7 @@ void geni_spi_se_dump_dbg_regs(struct geni_se *se, void __iomem *base,
 }
 
 static void spi_slv_setup(struct spi_geni_master *mas);
+static void spi_master_setup(struct spi_geni_master *mas);
 
 static ssize_t spi_slave_state_show(struct device *dev,
 		struct device_attribute *attr, char *buf)
@@ -325,6 +327,17 @@ static ssize_t spi_slave_state_store(struct device *dev,
 }
 
 static DEVICE_ATTR_RW(spi_slave_state);
+
+static void spi_master_setup(struct spi_geni_master *mas)
+{
+	geni_write_reg(OTHER_IO_OE | IO2_DATA_IN_SEL | RX_DATA_IN_SEL |
+		IO_MACRO_IO3_SEL | IO_MACRO_IO2_SEL | IO_MACRO_IO0_SEL_BIT,
+					mas->base, GENI_CFG_REG80);
+	geni_write_reg(START_TRIGGER, mas->base, SE_GENI_CFG_SEQ_START);
+
+	/* ensure data is written to hardware register */
+	wmb();
+}
 
 static void spi_slv_setup(struct spi_geni_master *mas)
 {
@@ -1284,6 +1297,9 @@ static int spi_geni_mas_setup(struct spi_master *spi)
 
 		if (spi->slave)
 			spi_slv_setup(mas);
+
+		if (mas->master_cross_connect)
+			spi_master_setup(mas);
 	}
 	geni_se_init(&mas->spi_rsc, 0x0, (mas->tx_fifo_depth - 2));
 	mas->tx_fifo_depth = geni_se_get_tx_fifo_depth(&mas->spi_rsc);
@@ -2229,6 +2245,9 @@ static int spi_geni_probe(struct platform_device *pdev)
 		spi->slave = true;
 		spi->slave_abort = spi_slv_abort;
 	}
+
+	if (of_property_read_bool(pdev->dev.of_node, "qcom,master-cross-connect"))
+		geni_mas->master_cross_connect = true;
 
 	geni_mas->slave_cross_connected =
 		of_property_read_bool(pdev->dev.of_node, "slv-cross-connected");