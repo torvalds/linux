65b571afdfdcefa09508274488086b218e34906d
Asahi Lina <lina@asahilina.net>
2023-02-24 18:11:47 +0900

    rust: Import upstream `alloc::vec::set_len_on_drop` module
    This is a subset of the Rust standard library `alloc` crate,
    version 1.62.0, licensed under "Apache-2.0 OR MIT", from:
    
        https://github.com/rust-lang/rust/tree/1.62.0/library/alloc/src
    
    The file is copied as-is, with no modifications whatsoever
    (not even adding the SPDX identifiers).
    
    For copyright details, please see:
    
        https://github.com/rust-lang/rust/blob/1.62.0/COPYRIGHT
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>
    Link: https://lore.kernel.org/r/20230224-rust-vec-v1-1-733b5b5a57c5@asahilina.net
    [ Import version 1.62.0 instead, to match the one in mainline. ]
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

---
+++ rust/alloc/vec/set_len_on_drop.rs
@@ -0,0 +1,28 @@
+// Set the length of the vec when the `SetLenOnDrop` value goes out of scope.
+//
+// The idea is: The length field in SetLenOnDrop is a local variable
+// that the optimizer will see does not alias with any stores through the Vec's data
+// pointer. This is a workaround for alias analysis issue #32155
+pub(super) struct SetLenOnDrop<'a> {
+    len: &'a mut usize,
+    local_len: usize,
+}
+
+impl<'a> SetLenOnDrop<'a> {
+    #[inline]
+    pub(super) fn new(len: &'a mut usize) -> Self {
+        SetLenOnDrop { local_len: *len, len }
+    }
+
+    #[inline]
+    pub(super) fn increment_len(&mut self, increment: usize) {
+        self.local_len += increment;
+    }
+}
+
+impl Drop for SetLenOnDrop<'_> {
+    #[inline]
+    fn drop(&mut self) {
+        *self.len = self.local_len;
+    }
+}
