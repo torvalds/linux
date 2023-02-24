6269d536a36a8c54c83e982616659b0eeeaf7baf
Tao Huang <huangtao@rock-chips.com>
2023-02-24 16:56:55 +0800

    Revert "ALSA: usb-audio: add USB ID for nanoc audio codec"
    This reverts commit 45c518d6ca307e0c415b61db47d02b9f809f6c3f.
    
    Remove unused RKVR driver.
    
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>
    Change-Id: I1220abd811b4ec8c416758ca3ae00b438ff215c0

--- sound/usb/quirks.c
+++ sound/usb/quirks.c
@@ -1523,9 +1523,6 @@ bool snd_usb_get_sample_rate_quirk(struct snd_usb_audio *chip)
 	case USB_ID(0x0556, 0x0014): /* Phoenix Audio TMX320VC */
 	case USB_ID(0x05a3, 0x9420): /* ELP HD USB Camera */
 	case USB_ID(0x05a7, 0x1020): /* Bose Companion 5 */
-#ifdef CONFIG_HID_RKVR
-	case USB_ID(0x071B, 0x3205): /* RockChip NanoC VR */
-#endif
 	case USB_ID(0x074d, 0x3553): /* Outlaw RR2150 (Micronas UAC3553B) */
 	case USB_ID(0x1395, 0x740a): /* Sennheiser DECT */
 	case USB_ID(0x1901, 0x0191): /* GE B850V3 CP2114 audio interface */
