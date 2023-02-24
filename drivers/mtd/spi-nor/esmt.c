efc029bba1d1e3f5d5066e5493e9e29ba952259f
Jon Lin <jon.lin@rock-chips.com>
2023-02-24 20:25:51 +0800

    mtd: spi-nor: esmt: Support new devices
    en25qh128, en25s64, en25qh256a, en25qx256a
    
    Change-Id: I168292cab499644970822f18daf11c0edc51d6a8
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/spi-nor/esmt.c
+++ drivers/mtd/spi-nor/esmt.c
@@ -16,6 +16,14 @@ static const struct flash_info esmt_parts[] = {
 			   SECT_4K | SPI_NOR_HAS_LOCK) },
 	{ "f25l64qa", INFO(0x8c4117, 0, 64 * 1024, 128,
 			   SECT_4K | SPI_NOR_HAS_LOCK) },
+	{ "en25qh128",
+		INFO(0x1c7018, 0, 64 * 1024, 256, SECT_4K) },
+	{ "en25s64",
+		INFO(0x1c3817, 0, 64 * 1024, 128, SECT_4K) },
+	{ "en25qh256a",
+		INFO(0x1c7019, 0, 64 * 1024, 512, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ | SPI_NOR_4B_OPCODES) },
+	{ "en25qx256a",
+		INFO(0x1c7119, 0, 64 * 1024, 512, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ | SPI_NOR_4B_OPCODES) },
 };
 
 const struct spi_nor_manufacturer spi_nor_esmt = {
