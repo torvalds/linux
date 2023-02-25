8f0e056068f26c0f1121f7f96a6b4515f59160f2
Nikita Romanyuk <ufh8945@gmail.com>
2023-02-25 10:12:29 +0300

    drivers: video: logo: add SPDX comment, remove GPL notice in pnmtologo.c
    Signed-off-by: Nikita Romanyuk <ufh8945@gmail.com>
    Reviewed-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Helge Deller <deller@gmx.de>

--- drivers/video/logo/pnmtologo.c
+++ drivers/video/logo/pnmtologo.c
@@ -1,14 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  *  Convert a logo in ASCII PNM format to C source suitable for inclusion in
  *  the Linux kernel
  *
  *  (C) Copyright 2001-2003 by Geert Uytterhoeven <geert@linux-m68k.org>
- *
- *  --------------------------------------------------------------------------
- *
- *  This file is subject to the terms and conditions of the GNU General Public
- *  License. See the file COPYING in the main directory of the Linux
- *  distribution for more details.
  */
 
 #include <ctype.h>
