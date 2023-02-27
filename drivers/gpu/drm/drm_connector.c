c5dc1756dc98e730ebcaed156a05b961086dc2f0
Simon Ser <contact@emersion.fr>
2023-02-27 12:25:26 +0000

    drm: remove outdated doc TODO for subconnector property
    This is already documented under "standard connector properties".
    
    Signed-off-by: Simon Ser <contact@emersion.fr>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: Pekka Paalanen <ppaalanen@gmail.com>
    Cc: Noralf Trønnes <noralf@tronnes.org>
    Cc: Mateusz Kwiatkowski <kfyatek+publicgit@gmail.com>
    Acked-by: Maxime Ripard <maxime@cerno.tech>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230227122522.117580-1-contact@emersion.fr

--- drivers/gpu/drm/drm_connector.c
+++ drivers/gpu/drm/drm_connector.c
@@ -1615,7 +1615,6 @@ EXPORT_SYMBOL(drm_connector_attach_dp_subconnector_property);
  *   - overscan
  *   - saturation
  *   - select subconnector
- *   - subconnector
  */
 /**
  * DOC: Analog TV Connector Properties
