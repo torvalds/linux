3c6748cd516ff009a3d8dac1d4cdc2ade6d218a7
Suren Baghdasaryan <surenb@google.com>
2023-02-27 09:36:20 -0800

    UPSTREAM: mm/mmap: prevent pagefault handler from racing with mmu_notifier registration
    Page fault handlers might need to fire MMU notifications while a new
    notifier is being registered.  Modify mm_take_all_locks to write-lock all
    VMAs and prevent this race with page fault handlers that would hold VMA
    locks.  VMAs are locked before i_mmap_rwsem and anon_vma to keep the same
    locking order as in page fault handlers.
    
    Link: https://lkml.kernel.org/r/20230227173632.3292573-22-surenb@google.com
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (cherry picked from commit eeff9a5d47f89bc641034fea05501c8a6de131cb)
    
    Bug: 161210518
    Change-Id: I4176bf0e1b07f03dfc1ac7dd37d7941d5a1dbc02
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -3592,6 +3592,7 @@ static void vm_lock_mapping(struct mm_struct *mm, struct address_space *mapping)
  * of mm/rmap.c:
  *   - all hugetlbfs_i_mmap_rwsem_key locks (aka mapping->i_mmap_rwsem for
  *     hugetlb mapping);
+ *   - all vmas marked locked
  *   - all i_mmap_rwsem locks;
  *   - all anon_vma->rwseml
  *
@@ -3614,6 +3615,13 @@ int mm_take_all_locks(struct mm_struct *mm)
 
 	mutex_lock(&mm_all_locks_mutex);
 
+	mas_for_each(&mas, vma, ULONG_MAX) {
+		if (signal_pending(current))
+			goto out_unlock;
+		vma_start_write(vma);
+	}
+
+	mas_set(&mas, 0);
 	mas_for_each(&mas, vma, ULONG_MAX) {
 		if (signal_pending(current))
 			goto out_unlock;
@@ -3703,6 +3711,7 @@ void mm_drop_all_locks(struct mm_struct *mm)
 		if (vma->vm_file && vma->vm_file->f_mapping)
 			vm_unlock_mapping(vma->vm_file->f_mapping);
 	}
+	vma_end_write_all(mm);
 
 	mutex_unlock(&mm_all_locks_mutex);
 }
