a3a087edd0497e9768c5948bdc11c1b3448a9721
Jakub Kicinski <kuba@kernel.org>
2023-02-27 10:12:01 -0800

    tls: rx: fix return value for async crypto
    [ Upstream commit 4d42cd6bc2ac1b9be50ade13771daec90c9d18b1 ]
    
    Gaurav reports that TLS Rx is broken with async crypto
    accelerators. The commit under fixes missed updating
    the retval byte counting logic when updating how records
    are stored. Even tho both before and after the change
    'decrypted' was updated inside the main loop, it was
    completely overwritten when processing the async
    completions. Now that the rx_list only holds
    non-zero-copy records we need to add, not overwrite.
    
    Reported-and-bisected-by: Gaurav Jain <gaurav.jain@nxp.com>
    Fixes: cbbdee9918a2 ("tls: rx: async: don't put async zc on the list")
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=217064
    Tested-by: Gaurav Jain <gaurav.jain@nxp.com>
    Reviewed-by: Simon Horman <simon.horman@corigine.com>
    Link: https://lore.kernel.org/r/20230227181201.1793772-1-kuba@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/tls/tls_sw.c
+++ net/tls/tls_sw.c
@@ -2114,7 +2114,7 @@ recv_end:
 		else
 			err = process_rx_list(ctx, msg, &control, 0,
 					      async_copy_bytes, is_peek);
-		decrypted = max(err, 0);
+		decrypted += max(err, 0);
 	}
 
 	copied += decrypted;
