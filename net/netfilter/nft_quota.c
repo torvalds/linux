34869f8e713e7235fce22ba7ac47d975fb81dd60
Pablo Neira Ayuso <pablo@netfilter.org>
2023-02-28 20:43:02 +0100

    netfilter: nft_quota: copy content when cloning expression
    [ Upstream commit aabef97a35160461e9c576848ded737558d89055 ]
    
    If the ruleset contains consumed quota, restore them accordingly.
    Otherwise, listing after restoration shows never used items.
    
    Restore the user-defined quota and flags too.
    
    Fixes: ed0a0c60f0e5 ("netfilter: nft_quota: move stateful fields out of expression data")
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/netfilter/nft_quota.c
+++ net/netfilter/nft_quota.c
@@ -235,12 +235,16 @@ static void nft_quota_destroy(const struct nft_ctx *ctx,
 static int nft_quota_clone(struct nft_expr *dst, const struct nft_expr *src)
 {
 	struct nft_quota *priv_dst = nft_expr_priv(dst);
+	struct nft_quota *priv_src = nft_expr_priv(src);
+
+	priv_dst->quota = priv_src->quota;
+	priv_dst->flags = priv_src->flags;
 
 	priv_dst->consumed = kmalloc(sizeof(*priv_dst->consumed), GFP_ATOMIC);
 	if (!priv_dst->consumed)
 		return -ENOMEM;
 
-	atomic64_set(priv_dst->consumed, 0);
+	*priv_dst->consumed = *priv_src->consumed;
 
 	return 0;
 }
