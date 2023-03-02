656e9007ef5862746cdf7ac16267c8e06e7b0989
Arnd Bergmann <arnd@arndb.de>
2023-03-02 09:53:31 +0100

    asm-generic: avoid __generic_cmpxchg_local warnings
    Code that passes a 32-bit constant into cmpxchg() produces a harmless
    sparse warning because of the truncation in the branch that is not taken:
    
    fs/erofs/zdata.c: note: in included file (through /home/arnd/arm-soc/arch/arm/include/asm/cmpxchg.h, /home/arnd/arm-soc/arch/arm/include/asm/atomic.h, /home/arnd/arm-soc/include/linux/atomic.h, ...):
    include/asm-generic/cmpxchg-local.h:29:33: warning: cast truncates bits from constant value (5f0ecafe becomes fe)
    include/asm-generic/cmpxchg-local.h:33:34: warning: cast truncates bits from constant value (5f0ecafe becomes cafe)
    include/asm-generic/cmpxchg-local.h:29:33: warning: cast truncates bits from constant value (5f0ecafe becomes fe)
    include/asm-generic/cmpxchg-local.h:30:42: warning: cast truncates bits from constant value (5f0edead becomes ad)
    include/asm-generic/cmpxchg-local.h:33:34: warning: cast truncates bits from constant value (5f0ecafe becomes cafe)
    include/asm-generic/cmpxchg-local.h:34:44: warning: cast truncates bits from constant value (5f0edead becomes dead)
    
    This was reported as a regression to Matt's recent __generic_cmpxchg_local
    patch, though this patch only added more warnings on top of the ones
    that were already there.
    
    Rewording the truncation to use an explicit bitmask instead of a cast
    to a smaller type avoids the warning but otherwise leaves the code
    unchanged.
    
    I had another look at why the cast is even needed for atomic_cmpxchg(),
    and as Matt describes the problem here is that atomic_t contains a
    signed 'int', but cmpxchg() takes an 'unsigned long' argument, and
    converting between the two leads to a 64-bit sign-extension of
    negative 32-bit atomics.
    
    I checked the other implementations of arch_cmpxchg() and did not find
    any others that run into the same problem as __generic_cmpxchg_local(),
    but it's easy to be on the safe side here and always convert the
    signed int into an unsigned int when calling arch_cmpxchg(), as this
    will work even when any of the arch_cmpxchg() implementations run
    into the same problem.
    
    Fixes: 624654152284 ("locking/atomic: cmpxchg: Make __generic_cmpxchg_local compare against zero-extended 'old' value")
    Reviewed-by: Matt Evans <mev@rivosinc.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/asm-generic/cmpxchg-local.h
+++ include/asm-generic/cmpxchg-local.h
@@ -26,16 +26,16 @@ static inline unsigned long __generic_cmpxchg_local(volatile void *ptr,
 	raw_local_irq_save(flags);
 	switch (size) {
 	case 1: prev = *(u8 *)ptr;
-		if (prev == (u8)old)
-			*(u8 *)ptr = (u8)new;
+		if (prev == (old & 0xffu))
+			*(u8 *)ptr = (new & 0xffu);
 		break;
 	case 2: prev = *(u16 *)ptr;
-		if (prev == (u16)old)
-			*(u16 *)ptr = (u16)new;
+		if (prev == (old & 0xffffu))
+			*(u16 *)ptr = (new & 0xffffu);
 		break;
 	case 4: prev = *(u32 *)ptr;
-		if (prev == (u32)old)
-			*(u32 *)ptr = (u32)new;
+		if (prev == (old & 0xffffffffffu))
+			*(u32 *)ptr = (new & 0xffffffffu);
 		break;
 	case 8: prev = *(u64 *)ptr;
 		if (prev == old)
