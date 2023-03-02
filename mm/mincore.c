63cf584203f3367c8b073d417c8e5cbbfc450506
James Houghton <jthoughton@google.com>
2023-03-02 22:24:04 +0000

    mm: teach mincore_hugetlb about pte markers
    By checking huge_pte_none(), we incorrectly classify PTE markers as
    "present".  Instead, check huge_pte_none_mostly(), classifying PTE markers
    the same as if the PTE were completely blank.
    
    PTE markers, unlike other kinds of swap entries, don't reference any
    physical page and don't indicate that a physical page was mapped
    previously.  As such, treat them as non-present for the sake of mincore().
    
    Link: https://lkml.kernel.org/r/20230302222404.175303-1-jthoughton@google.com
    Fixes: 5c041f5d1f23 ("mm: teach core mm about pte markers")
    Signed-off-by: James Houghton <jthoughton@google.com>
    Acked-by: Peter Xu <peterx@redhat.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Cc: Axel Rasmussen <axelrasmussen@google.com>
    Cc: James Houghton <jthoughton@google.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/mincore.c
+++ mm/mincore.c
@@ -33,7 +33,7 @@ static int mincore_hugetlb(pte_t *pte, unsigned long hmask, unsigned long addr,
 	 * Hugepages under user process are always in RAM and never
 	 * swapped out, but theoretically it needs to be checked.
 	 */
-	present = pte && !huge_pte_none(huge_ptep_get(pte));
+	present = pte && !huge_pte_none_mostly(huge_ptep_get(pte));
 	for (; addr != end; vec++, addr += PAGE_SIZE)
 		*vec = present;
 	walk->private = vec;
