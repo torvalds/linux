4bdfe20d85b32274234bbcd705d5547fd70a4ea1
Saaem Rizvi <SyedSaaem.Rizvi@amd.com>
2023-03-06 15:10:13 -0500

    drm/amd/display: Implement workaround for writing to OTG_PIXEL_RATE_DIV register
    commit 74fa4c81aadf418341f0d073c864ea7dca730a2e upstream.
    
    [Why and How]
    Current implementation requires FPGA builds to take a different
    code path from DCN32 to write to OTG_PIXEL_RATE_DIV. Now that
    we have a workaround to write to OTG_PIXEL_RATE_DIV register without
    blanking display on hotplug on DCN32, we can allow the code paths for
    FPGA to be exactly the same allowing for more consistent
    testing.
    
    Reviewed-by: Alvin Lee <Alvin.Lee2@amd.com>
    Acked-by: Qingqing Zhuo <qingqing.zhuo@amd.com>
    Signed-off-by: Saaem Rizvi <SyedSaaem.Rizvi@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: "Limonciello, Mario" <mario.limonciello@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/amd/display/dc/dcn32/dcn32_hwseq.c
+++ drivers/gpu/drm/amd/display/dc/dcn32/dcn32_hwseq.c
@@ -1177,7 +1177,7 @@ unsigned int dcn32_calculate_dccg_k1_k2_values(struct pipe_ctx *pipe_ctx, unsign
 			*k2_div = PIXEL_RATE_DIV_BY_2;
 		else
 			*k2_div = PIXEL_RATE_DIV_BY_4;
-	} else if (dc_is_dp_signal(stream->signal)) {
+	} else if (dc_is_dp_signal(stream->signal) || dc_is_virtual_signal(stream->signal)) {
 		if (two_pix_per_container) {
 			*k1_div = PIXEL_RATE_DIV_BY_1;
 			*k2_div = PIXEL_RATE_DIV_BY_2;
