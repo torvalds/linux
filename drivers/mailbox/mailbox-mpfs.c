75dfbcbfd781f826c73ebe42b78d49c23179d082
Conor Dooley <conor.dooley@microchip.com>
2023-03-07 20:22:51 +0000

    mailbox: mpfs: fix an incorrect mask width
    The system controller registers on PolarFire SoC are 32 bits wide, so
    16 + 16 as the first input to GENMASK_ULL() gives a 33 bit wide mask.
    It probably should have been immediately obvious when it was pointed
    out during review that the width required using GENMASK_ULL() - but I
    scarcely knew what I was doing at the time and missed it.
    The mistake ends up being moot as it is a mask after all, but it is
    incorrect and should be fixed.
    
    No functional change intended.
    
    Acked-by: Jassi Brar <jaswinder.singh@linaro.org>
    Tested-by: Valentina Fernandez <valentina.fernandezalanis@microchip.com>
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- drivers/mailbox/mailbox-mpfs.c
+++ drivers/mailbox/mailbox-mpfs.c
@@ -39,7 +39,7 @@
 #define SCB_CTRL_NOTIFY_MASK BIT(SCB_CTRL_NOTIFY)
 
 #define SCB_CTRL_POS (16)
-#define SCB_CTRL_MASK GENMASK_ULL(SCB_CTRL_POS + SCB_MASK_WIDTH, SCB_CTRL_POS)
+#define SCB_CTRL_MASK GENMASK(SCB_CTRL_POS + SCB_MASK_WIDTH - 1, SCB_CTRL_POS)
 
 /* SCBCTRL service status register */
 
@@ -118,6 +118,7 @@ static int mpfs_mbox_send_data(struct mbox_chan *chan, void *data)
 	}
 
 	opt_sel = ((msg->mbox_offset << 7u) | (msg->cmd_opcode & 0x7fu));
+
 	tx_trigger = (opt_sel << SCB_CTRL_POS) & SCB_CTRL_MASK;
 	tx_trigger |= SCB_CTRL_REQ_MASK | SCB_STATUS_NOTIFY_MASK;
 	writel_relaxed(tx_trigger, mbox->ctrl_base + SERVICES_CR_OFFSET);
