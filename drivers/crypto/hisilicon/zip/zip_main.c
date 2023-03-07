f69ef19f106da84502b400788c1661d5fda86a73
Bjorn Helgaas <bhelgaas@google.com>
2023-03-07 10:19:47 -0600

    crypto: hisilicon/zip - remove unnecessary aer.h include
    <linux/aer.h> is unused, so remove it.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Cc: Yang Shen <shenyang39@huawei.com>
    Cc: Zhou Wang <wangzhou1@hisilicon.com>
    Cc: Nick Terrell <terrelln@fb.com>
    Acked-by: Longfang Liu <liulongfang@huawei.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/hisilicon/zip/zip_main.c
+++ drivers/crypto/hisilicon/zip/zip_main.c
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /* Copyright (c) 2019 HiSilicon Limited. */
 #include <linux/acpi.h>
-#include <linux/aer.h>
 #include <linux/bitops.h>
 #include <linux/debugfs.h>
 #include <linux/init.h>
