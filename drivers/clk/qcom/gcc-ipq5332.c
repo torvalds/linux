377c0b46d8794ab4441c8c079e079dc2e367c1e1
Kathiravan T <quic_kathirav@quicinc.com>
2023-03-07 11:52:28 +0530

    clk: qcom: ipq5332: mark GPLL4 as ignore unused temporarily
    Clock framework disables the GPLL4 source since there are no active users
    for this source currently. Some of the clocks initialized by the
    bootloaders uses the GPLL4 as the source. Due to this, when the GPLL4 is
    disabled by the clock framework, system is going for the reboot.
    
    To avoid this, mark the GPLL4 as ignore unused so that clock framework
    doesn't disable it. Once the users of this source is enabled, we can get
    rid of this flag.
    
    Signed-off-by: Kathiravan T <quic_kathirav@quicinc.com>
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    Link: https://lore.kernel.org/r/20230307062232.4889-6-quic_kathirav@quicinc.com

--- drivers/clk/qcom/gcc-ipq5332.c
+++ drivers/clk/qcom/gcc-ipq5332.c
@@ -128,6 +128,17 @@ static struct clk_alpha_pll gpll4_main = {
 			.parent_data = &gcc_parent_data_xo,
 			.num_parents = 1,
 			.ops = &clk_alpha_pll_stromer_ops,
+			/*
+			 * There are no consumers for this GPLL in kernel yet,
+			 * (will be added soon), so the clock framework
+			 * disables this source. But some of the clocks
+			 * initialized by boot loaders uses this source. So we
+			 * need to keep this clock ON. Add the
+			 * CLK_IGNORE_UNUSED flag so the clock will not be
+			 * disabled. Once the consumer in kernel is added, we
+			 * can get rid of this flag.
+			 */
+			.flags = CLK_IGNORE_UNUSED,
 		},
 	},
 };
