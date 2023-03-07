4ae47770d57bff0193fbbf48d56c18759cad5f6e
oushixiong <oushixiong@kylinos.cn>
2023-03-07 06:57:48 +0100

    media: mtk-jpegenc: Fix a compilation issue
    ‘mtk8195_jpegenc_drvdata’ defined but not used [-Werror=unused-variable]
    
    Signed-off-by: oushixiong <oushixiong@kylinos.cn>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>

--- drivers/media/platform/mediatek/jpeg/mtk_jpeg_core.c
+++ drivers/media/platform/mediatek/jpeg/mtk_jpeg_core.c
@@ -1846,6 +1846,7 @@ static const struct dev_pm_ops mtk_jpeg_pm_ops = {
 	SET_RUNTIME_PM_OPS(mtk_jpeg_pm_suspend, mtk_jpeg_pm_resume, NULL)
 };
 
+#if defined(CONFIG_OF)
 static const struct mtk_jpeg_variant mt8173_jpeg_drvdata = {
 	.clks = mt8173_jpeg_dec_clocks,
 	.num_clks = ARRAY_SIZE(mt8173_jpeg_dec_clocks),
@@ -1898,7 +1899,6 @@ static const struct mtk_jpeg_variant mtk8195_jpegdec_drvdata = {
 	.cap_q_default_fourcc = V4L2_PIX_FMT_YUV420M,
 };
 
-#if defined(CONFIG_OF)
 static const struct of_device_id mtk_jpeg_match[] = {
 	{
 		.compatible = "mediatek,mt8173-jpgdec",
