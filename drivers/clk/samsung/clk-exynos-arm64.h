f05dc20243163d0218bbb258b6461681865cff5c
Sam Protsenko <semen.protsenko@linaro.org>
2023-03-06 18:24:23 -0600

    clk: samsung: exynos5433: Extract PM support to common ARM64 layer
    Exynos5433 clock driver implements PM support internally, which might be
    also useful for other Exynos clock drivers. Extract all PM related code
    from clk-exynos5433 to common ARM64 functions.
    
    Tested-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>
    Link: https://lore.kernel.org/r/20230307002423.24454-4-semen.protsenko@linaro.org
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>

--- drivers/clk/samsung/clk-exynos-arm64.h
+++ drivers/clk/samsung/clk-exynos-arm64.h
@@ -16,5 +16,8 @@
 
 void exynos_arm64_register_cmu(struct device *dev,
 		struct device_node *np, const struct samsung_cmu_info *cmu);
+int exynos_arm64_register_cmu_pm(struct platform_device *pdev, bool set_manual);
+int exynos_arm64_cmu_suspend(struct device *dev);
+int exynos_arm64_cmu_resume(struct device *dev);
 
 #endif /* __CLK_EXYNOS_ARM64_H */
