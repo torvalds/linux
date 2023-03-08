48a1084a8b7423642b5f17ca6202f6f277c5392b
Andrei Vagin <avagin@google.com>
2023-03-07 23:31:59 -0800

    seccomp: add the synchronous mode for seccomp_unotify
    seccomp_unotify allows more privileged processes do actions on behalf
    of less privileged processes.
    
    In many cases, the workflow is fully synchronous. It means a target
    process triggers a system call and passes controls to a supervisor
    process that handles the system call and returns controls to the target
    process. In this context, "synchronous" means that only one process is
    running and another one is waiting.
    
    There is the WF_CURRENT_CPU flag that is used to advise the scheduler to
    move the wakee to the current CPU. For such synchronous workflows, it
    makes context switches a few times faster.
    
    Right now, each interaction takes 12µs. With this patch, it takes about
    3µs.
    
    This change introduce the SECCOMP_USER_NOTIF_FD_SYNC_WAKE_UP flag that
    it used to enable the sync mode.
    
    Signed-off-by: Andrei Vagin <avagin@google.com>
    Acked-by: "Peter Zijlstra (Intel)" <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20230308073201.3102738-5-avagin@google.com
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- kernel/seccomp.c
+++ kernel/seccomp.c
@@ -143,9 +143,12 @@ struct seccomp_kaddfd {
  *           filter->notify_lock.
  * @next_id: The id of the next request.
  * @notifications: A list of struct seccomp_knotif elements.
+ * @flags: A set of SECCOMP_USER_NOTIF_FD_* flags.
  */
+
 struct notification {
 	atomic_t requests;
+	u32 flags;
 	u64 next_id;
 	struct list_head notifications;
 };
@@ -1117,7 +1120,10 @@ static int seccomp_do_user_notification(int this_syscall,
 	INIT_LIST_HEAD(&n.addfd);
 
 	atomic_inc(&match->notif->requests);
-	wake_up_poll(&match->wqh, EPOLLIN | EPOLLRDNORM);
+	if (match->notif->flags & SECCOMP_USER_NOTIF_FD_SYNC_WAKE_UP)
+		wake_up_poll_on_current_cpu(&match->wqh, EPOLLIN | EPOLLRDNORM);
+	else
+		wake_up_poll(&match->wqh, EPOLLIN | EPOLLRDNORM);
 
 	/*
 	 * This is where we wait for a reply from userspace.
@@ -1593,7 +1599,10 @@ static long seccomp_notify_send(struct seccomp_filter *filter,
 	knotif->error = resp.error;
 	knotif->val = resp.val;
 	knotif->flags = resp.flags;
-	complete(&knotif->ready);
+	if (filter->notif->flags & SECCOMP_USER_NOTIF_FD_SYNC_WAKE_UP)
+		complete_on_current_cpu(&knotif->ready);
+	else
+		complete(&knotif->ready);
 out:
 	mutex_unlock(&filter->notify_lock);
 	return ret;
@@ -1621,6 +1630,22 @@ static long seccomp_notify_id_valid(struct seccomp_filter *filter,
 
 	mutex_unlock(&filter->notify_lock);
 	return ret;
+}
+
+static long seccomp_notify_set_flags(struct seccomp_filter *filter,
+				    unsigned long flags)
+{
+	long ret;
+
+	if (flags & ~SECCOMP_USER_NOTIF_FD_SYNC_WAKE_UP)
+		return -EINVAL;
+
+	ret = mutex_lock_interruptible(&filter->notify_lock);
+	if (ret < 0)
+		return ret;
+	filter->notif->flags = flags;
+	mutex_unlock(&filter->notify_lock);
+	return 0;
 }
 
 static long seccomp_notify_addfd(struct seccomp_filter *filter,
@@ -1752,6 +1777,8 @@ static long seccomp_notify_ioctl(struct file *file, unsigned int cmd,
 	case SECCOMP_IOCTL_NOTIF_ID_VALID_WRONG_DIR:
 	case SECCOMP_IOCTL_NOTIF_ID_VALID:
 		return seccomp_notify_id_valid(filter, buf);
+	case SECCOMP_IOCTL_NOTIF_SET_FLAGS:
+		return seccomp_notify_set_flags(filter, arg);
 	}
 
 	/* Extensible Argument ioctls */
