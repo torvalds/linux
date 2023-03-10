c570f861fa059ea653599415a7c8cc1dfaf16763
Oleksij Rempel <o.rempel@pengutronix.de>
2023-03-10 10:08:09 +0100

    net: dsa: microchip: add ETS Qdisc support for KSZ9477 series
    Add ETS Qdisc support for KSZ9477 of switches. Current implementation is
    limited to strict priority mode.
    
    Tested on KSZ8563R with following configuration:
    tc qdisc replace dev lan2 root handle 1: ets strict 4 \
      priomap 3 3 2 2 1 1 0 0
    ip link add link lan2 name v1 type vlan id 1 \
      egress-qos-map 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7
    
    and patched iperf3 version:
    https://github.com/esnet/iperf/pull/1476
    iperf3 -c 172.17.0.1 -b100M  -l1472 -t100 -u -R --sock-prio 2
    
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>
    Acked-by: Arun Ramadoss <arun.ramadoss@microchip.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/microchip/ksz_common.h
+++ drivers/net/dsa/microchip/ksz_common.h
@@ -51,6 +51,7 @@ struct ksz_chip_data {
 	u8 port_nirqs;
 	u8 num_tx_queues;
 	bool tc_cbs_supported;
+	bool tc_ets_supported;
 	const struct ksz_dev_ops *ops;
 	bool phy_errata_9477;
 	bool ksz87xx_eee_link_erratum;
@@ -649,6 +650,14 @@ static inline int is_lan937x(struct ksz_device *dev)
 #define KSZ8_LEGAL_PACKET_SIZE		1518
 #define KSZ9477_MAX_FRAME_SIZE		9000
 
+#define KSZ9477_REG_PORT_OUT_RATE_0	0x0420
+#define KSZ9477_OUT_RATE_NO_LIMIT	0
+
+#define KSZ9477_PORT_MRI_TC_MAP__4	0x0808
+
+#define KSZ9477_PORT_TC_MAP_S		4
+#define KSZ9477_MAX_TC_PRIO		7
+
 /* CBS related registers */
 #define REG_PORT_MTI_QUEUE_INDEX__4	0x0900
 
@@ -661,6 +670,9 @@ static inline int is_lan937x(struct ksz_device *dev)
 #define MTI_SHAPING_OFF			0
 #define MTI_SHAPING_SRP			1
 #define MTI_SHAPING_TIME_AWARE		2
+
+#define KSZ9477_PORT_MTI_QUEUE_CTRL_1	0x0915
+#define KSZ9477_DEFAULT_WRR_WEIGHT	1
 
 #define REG_PORT_MTI_HI_WATER_MARK	0x0916
 #define REG_PORT_MTI_LO_WATER_MARK	0x0918
