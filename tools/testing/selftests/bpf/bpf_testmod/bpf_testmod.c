aa3d65de4b9004d799f97700751a86d3ebd7d5f9
Viktor Malik <vmalik@redhat.com>
2023-03-10 08:41:00 +0100

    bpf/selftests: Test fentry attachment to shadowed functions
    Adds a new test that tries to attach a program to fentry of two
    functions of the same name, one located in vmlinux and the other in
    bpf_testmod.
    
    To avoid conflicts with existing tests, a new function
    "bpf_fentry_shadow_test" was created both in vmlinux and in bpf_testmod.
    
    The previous commit fixed a bug which caused this test to fail. The
    verifier would always use the vmlinux function's address as the target
    trampoline address, hence trying to create two trampolines for a single
    address, which is forbidden.
    
    The test (similarly to other fentry/fexit tests) is not working on arm64
    at the moment.
    
    Signed-off-by: Viktor Malik <vmalik@redhat.com>
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Link: https://lore.kernel.org/r/5fe2f364190b6f79b085066ed7c5989c5bc475fa.1678432753.git.vmalik@redhat.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/testing/selftests/bpf/bpf_testmod/bpf_testmod.c
+++ tools/testing/selftests/bpf/bpf_testmod/bpf_testmod.c
@@ -268,6 +268,12 @@ static const struct btf_kfunc_id_set bpf_testmod_kfunc_set = {
 	.set   = &bpf_testmod_check_kfunc_ids,
 };
 
+noinline int bpf_fentry_shadow_test(int a)
+{
+	return a + 2;
+}
+EXPORT_SYMBOL_GPL(bpf_fentry_shadow_test);
+
 extern int bpf_fentry_test1(int a);
 
 static int bpf_testmod_init(void)
