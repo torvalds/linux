b39a3be50a56891f7538cfcbe729b5434b8890c5
Andrey Konovalov <andreyknvl@google.com>
2023-03-11 00:43:29 +0100

    UPSTREAM: kasan: drop empty tagging-related defines
    mm/kasan/kasan.h provides a number of empty defines for a few
    arch-specific tagging-related routines, in case the architecture code
    didn't define them.
    
    The original idea was to simplify integration in case another architecture
    starts supporting memory tagging.  However, right now, if any of those
    routines are not provided by an architecture, Hardware Tag-Based KASAN
    won't work.
    
    Drop the empty defines, as it would be better to get compiler errors
    rather than runtime crashes when adding support for a new architecture.
    
    Also drop empty hw_enable_tagging_sync/async/asymm defines for
    !CONFIG_KASAN_HW_TAGS case, as those are only used in mm/kasan/hw_tags.c.
    
    Link: https://lkml.kernel.org/r/bc919c144f8684a7fd9ba70c356ac2a75e775e29.1678491668.git.andreyknvl@google.com
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Andrey Ryabinin <ryabinin.a.a@gmail.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com> [arm64]
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Evgenii Stepanov <eugenis@google.com>
    Cc: Marco Elver <elver@google.com>
    Cc: Peter Collingbourne <pcc@google.com>
    Cc: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Cc: Weizhao Ouyang <ouyangweizhao@zeku.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Bug: 254721825
    (cherry picked from commit e34f1e2ee0982a0ebed9e71f18d1b9529df43cd5)
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Change-Id: I22911baac04cc56db2a08250e8eb47a0d4ee6128

--- mm/kasan/kasan.h
+++ mm/kasan/kasan.h
@@ -401,28 +401,6 @@ static inline const void *arch_kasan_set_tag(const void *addr, u8 tag)
 
 #ifdef CONFIG_KASAN_HW_TAGS
 
-#ifndef arch_enable_tagging_sync
-#define arch_enable_tagging_sync()
-#endif
-#ifndef arch_enable_tagging_async
-#define arch_enable_tagging_async()
-#endif
-#ifndef arch_enable_tagging_asymm
-#define arch_enable_tagging_asymm()
-#endif
-#ifndef arch_force_async_tag_fault
-#define arch_force_async_tag_fault()
-#endif
-#ifndef arch_get_random_tag
-#define arch_get_random_tag()	(0xFF)
-#endif
-#ifndef arch_get_mem_tag
-#define arch_get_mem_tag(addr)	(0xFF)
-#endif
-#ifndef arch_set_mem_tag_range
-#define arch_set_mem_tag_range(addr, size, tag, init) ((void *)(addr))
-#endif
-
 #define hw_enable_tagging_sync()		arch_enable_tagging_sync()
 #define hw_enable_tagging_async()		arch_enable_tagging_async()
 #define hw_enable_tagging_asymm()		arch_enable_tagging_asymm()
@@ -435,10 +413,6 @@ static inline const void *arch_kasan_set_tag(const void *addr, u8 tag)
 void kasan_enable_tagging(void);
 
 #else /* CONFIG_KASAN_HW_TAGS */
-
-#define hw_enable_tagging_sync()
-#define hw_enable_tagging_async()
-#define hw_enable_tagging_asymm()
 
 static inline void kasan_enable_tagging(void) { }
 
