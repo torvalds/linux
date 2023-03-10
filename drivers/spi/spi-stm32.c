1e4929112507f145951f4c356161ab80ad9c1f0e
Leonard Göhrs <l.goehrs@pengutronix.de>
2023-03-10 10:20:53 +0100

    spi: stm32: split large transfers based on word size instead of bytes
    The TSIZE register in CR2, to which the number of words to transfer
    is written, is only 16 Bit. This limits transfers to 65535 SPI
    _words_ at a time. The existing code uses spi_split_transfers_maxsize
    to limit transfers to 65535 _bytes_ at a time.
    
    This breaks large transfers with bits_per_word > 8, as they are
    split inside of a word boundary by the odd size limit.
    
    Split transfers based on the number of words instead.
    This has the added benefit of not artificially limiting the maximum
    length of bpw > 8 transfers to half or a quarter of the actual limit.
    
    The combination of very large transfers and bits_per_word = 16 is triggered
    e.g. by MIPI DBI displays when updating large parts of the screen.
    
    Signed-off-by: Leonard Göhrs <l.goehrs@pengutronix.de>
    Acked-by: Alain Volmat <alain.volmat@foss.st.com>
    Link: https://lore.kernel.org/r/20230310092053.1006459-2-l.goehrs@pengutronix.de
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-stm32.c
+++ drivers/spi/spi-stm32.c
@@ -984,9 +984,9 @@ static int stm32_spi_prepare_msg(struct spi_master *master,
 	if (spi->cfg->set_number_of_data) {
 		int ret;
 
-		ret = spi_split_transfers_maxsize(master, msg,
-						  STM32H7_SPI_TSIZE_MAX,
-						  GFP_KERNEL | GFP_DMA);
+		ret = spi_split_transfers_maxwords(master, msg,
+						   STM32H7_SPI_TSIZE_MAX,
+						   GFP_KERNEL | GFP_DMA);
 		if (ret)
 			return ret;
 	}
