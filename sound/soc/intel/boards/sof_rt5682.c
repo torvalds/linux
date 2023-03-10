12e3b2848bfa8197062b7989eefb7c081d372fb8
Ajye Huang <ajye_huang@compal.corp-partner.google.com>
2023-03-11 02:42:01 +0800

    ASoC: Intel: sof_rt5682: Enable Bluetooth offload on adl_rt1019_rt5682
    Enable Bluetooth audio offload for drv_name "adl_rt1019_rt5682" with
    following board configuration specifically:
    
    SSP0 - rt5682 Headset
    SSP1 - alc1019p speaker amp
    SSP2 - Bluetooth audio
    
    Signed-off-by: Ajye Huang <ajye_huang@compal.corp-partner.google.com>
    Signed-off-by: Mac Chiang <mac.chiang@intel.com>
    Acked-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20230310184201.1302232-1-ajye_huang@compal.corp-partner.google.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/boards/sof_rt5682.c
+++ sound/soc/intel/boards/sof_rt5682.c
@@ -1109,7 +1109,9 @@ static const struct platform_device_id board_ids[] = {
 					SOF_SPEAKER_AMP_PRESENT |
 					SOF_RT1019_SPEAKER_AMP_PRESENT |
 					SOF_RT5682_SSP_AMP(1) |
-					SOF_RT5682_NUM_HDMIDEV(4)),
+					SOF_RT5682_NUM_HDMIDEV(4) |
+					SOF_BT_OFFLOAD_SSP(2) |
+					SOF_SSP_BT_OFFLOAD_PRESENT),
 	},
 	{
 		.name = "mtl_mx98357_rt5682",
