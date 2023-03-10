553be6e70d08d8b88a7c3e694e8a562f8ba2cbc8
Andrey Konovalov <andreyknvl@google.com>
2023-03-11 00:43:30 +0100

    BACKPORT: kasan, arm64: rename tagging-related routines
    (Backport: no real conflicts, neighboring lines changed.)
    
    Rename arch_enable_tagging_sync/async/asymm to
    arch_enable_tag_checks_sync/async/asymm, as the new name better reflects
    their function.
    
    Also rename kasan_enable_tagging to kasan_enable_hw_tags for the same
    reason.
    
    Link: https://lkml.kernel.org/r/069ef5b77715c1ac8d69b186725576c32b149491.1678491668.git.andreyknvl@google.com
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Andrey Ryabinin <ryabinin.a.a@gmail.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Evgenii Stepanov <eugenis@google.com>
    Cc: Marco Elver <elver@google.com>
    Cc: Peter Collingbourne <pcc@google.com>
    Cc: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Cc: Weizhao Ouyang <ouyangweizhao@zeku.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Bug: 254721825
    (cherry picked from commit 0eafff1c5a56893ca9708fb8ff1327a738b242e9)
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Change-Id: Idbb7c99b798843dd8909528cc29d2fe883108b9f

--- mm/kasan/kasan.h
+++ mm/kasan/kasan.h
@@ -401,20 +401,20 @@ static inline const void *arch_kasan_set_tag(const void *addr, u8 tag)
 
 #ifdef CONFIG_KASAN_HW_TAGS
 
-#define hw_enable_tagging_sync()		arch_enable_tagging_sync()
-#define hw_enable_tagging_async()		arch_enable_tagging_async()
-#define hw_enable_tagging_asymm()		arch_enable_tagging_asymm()
+#define hw_enable_tag_checks_sync()		arch_enable_tag_checks_sync()
+#define hw_enable_tag_checks_async()		arch_enable_tag_checks_async()
+#define hw_enable_tag_checks_asymm()		arch_enable_tag_checks_asymm()
 #define hw_force_async_tag_fault()		arch_force_async_tag_fault()
 #define hw_get_random_tag()			arch_get_random_tag()
 #define hw_get_mem_tag(addr)			arch_get_mem_tag(addr)
 #define hw_set_mem_tag_range(addr, size, tag, init) \
 			arch_set_mem_tag_range((addr), (size), (tag), (init))
 
-void kasan_enable_tagging(void);
+void kasan_enable_hw_tags(void);
 
 #else /* CONFIG_KASAN_HW_TAGS */
 
-static inline void kasan_enable_tagging(void) { }
+static inline void kasan_enable_hw_tags(void) { }
 
 #endif /* CONFIG_KASAN_HW_TAGS */
 
