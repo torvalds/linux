b6f228e800ccf285906bb1c4c366ce3848a5443e
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2023-03-11 18:38:03 +0100

    clocksource/drivers/stm32-lp: Drop of_match_ptr for ID table
    The driver can match only via the DT table so the table should be always
    used and the of_match_ptr does not have any sense (this also allows ACPI
    matching via PRP0001, even though it might not be relevant here).
    
      drivers/clocksource/timer-stm32-lp.c:203:34: error: ‘stm32_clkevent_lp_of_match’ defined but not used [-Werror=unused-const-variable=]
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/20230311173803.263446-1-krzysztof.kozlowski@linaro.org

--- drivers/clocksource/timer-stm32-lp.c
+++ drivers/clocksource/timer-stm32-lp.c
@@ -205,7 +205,7 @@ static struct platform_driver stm32_clkevent_lp_driver = {
 	.probe  = stm32_clkevent_lp_probe,
 	.driver	= {
 		.name = "stm32-lptimer-timer",
-		.of_match_table = of_match_ptr(stm32_clkevent_lp_of_match),
+		.of_match_table = stm32_clkevent_lp_of_match,
 		.suppress_bind_attrs = true,
 	},
 };
