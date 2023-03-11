0f17b42827ae63aaf08cad875d9575b1bca1e066
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2023-03-11 18:32:56 +0100

    net: dsa: ocelot: drop of_match_ptr for ID table
    The driver can match only via the DT table so the table should be always
    used and the of_match_ptr does not have any sense (this also allows ACPI
    matching via PRP0001, even though it might not be relevant here).
    
      drivers/net/dsa/ocelot/ocelot_ext.c:143:34: error: ‘ocelot_ext_switch_of_match’ defined but not used [-Werror=unused-const-variable=]
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Acked-by: Colin Foster <colin.foster@in-advantage.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/ocelot/ocelot_ext.c
+++ drivers/net/dsa/ocelot/ocelot_ext.c
@@ -149,7 +149,7 @@ MODULE_DEVICE_TABLE(of, ocelot_ext_switch_of_match);
 static struct platform_driver ocelot_ext_switch_driver = {
 	.driver = {
 		.name = "ocelot-ext-switch",
-		.of_match_table = of_match_ptr(ocelot_ext_switch_of_match),
+		.of_match_table = ocelot_ext_switch_of_match,
 	},
 	.probe = ocelot_ext_probe,
 	.remove = ocelot_ext_remove,
