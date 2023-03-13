d4e492338d11937c55841b1279287280d6e35894
Alexander Lobakin <aleksander.lobakin@intel.com>
2023-03-13 22:55:53 +0100

    xdp: remove unused {__,}xdp_release_frame()
    __xdp_build_skb_from_frame() was the last user of
    {__,}xdp_release_frame(), which detaches pages from the page_pool.
    All the consumers now recycle Page Pool skbs and page, except mlx5,
    stmmac and tsnep drivers, which use page_pool_release_page() directly
    (might change one day). It's safe to assume this functionality is not
    needed anymore and can be removed (in favor of recycling).
    
    Signed-off-by: Alexander Lobakin <aleksander.lobakin@intel.com>
    Link: https://lore.kernel.org/r/20230313215553.1045175-5-aleksander.lobakin@intel.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- net/core/xdp.c
+++ net/core/xdp.c
@@ -531,21 +531,6 @@ out:
 }
 EXPORT_SYMBOL_GPL(xdp_return_buff);
 
-/* Only called for MEM_TYPE_PAGE_POOL see xdp.h */
-void __xdp_release_frame(void *data, struct xdp_mem_info *mem)
-{
-	struct xdp_mem_allocator *xa;
-	struct page *page;
-
-	rcu_read_lock();
-	xa = rhashtable_lookup(mem_id_ht, &mem->id, mem_id_rht_params);
-	page = virt_to_head_page(data);
-	if (xa)
-		page_pool_release_page(xa->page_pool, page);
-	rcu_read_unlock();
-}
-EXPORT_SYMBOL_GPL(__xdp_release_frame);
-
 void xdp_attachment_setup(struct xdp_attachment_info *info,
 			  struct netdev_bpf *bpf)
 {
