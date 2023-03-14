721d3e91bfc93975c5e1a76c7d588dd8df5d82da
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2023-03-14 13:34:42 +0530

    qcom: llcc/edac: Support polling mode for ECC handling
    Not all Qcom platforms support IRQ mode for ECC handling. For those
    platforms, the current EDAC driver will not be probed due to missing ECC
    IRQ in devicetree.
    
    So add support for polling mode so that the EDAC driver can be used on all
    Qcom platforms supporting LLCC.
    
    The polling delay of 5000ms is chosen based on Qcom downstream/vendor
    driver.
    
    Reported-by: Luca Weiss <luca.weiss@fairphone.com>
    Tested-by: Luca Weiss <luca.weiss@fairphone.com>
    Tested-by: Steev Klimaszewski <steev@kali.org> # Thinkpad X13s
    Tested-by: Andrew Halaney <ahalaney@redhat.com> # sa8540p-ride
    Reviewed-by: Borislav Petkov (AMD) <bp@alien8.de>
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    Link: https://lore.kernel.org/r/20230314080443.64635-14-manivannan.sadhasivam@linaro.org

--- drivers/soc/qcom/llcc-qcom.c
+++ drivers/soc/qcom/llcc-qcom.c
@@ -1011,13 +1011,12 @@ static int qcom_llcc_probe(struct platform_device *pdev)
 		goto err;
 
 	drv_data->ecc_irq = platform_get_irq_optional(pdev, 0);
-	if (drv_data->ecc_irq >= 0) {
-		llcc_edac = platform_device_register_data(&pdev->dev,
-						"qcom_llcc_edac", -1, drv_data,
-						sizeof(*drv_data));
-		if (IS_ERR(llcc_edac))
-			dev_err(dev, "Failed to register llcc edac driver\n");
-	}
+
+	llcc_edac = platform_device_register_data(&pdev->dev,
+					"qcom_llcc_edac", -1, drv_data,
+					sizeof(*drv_data));
+	if (IS_ERR(llcc_edac))
+		dev_err(dev, "Failed to register llcc edac driver\n");
 
 	return 0;
 err:
