dabe5e1c316971fa6cd0ab42a098a58679f9ecc9
Billy Tsai <billy_tsai@aspeedtech.com>
2023-03-14 14:57:51 +0800

    i3c: ibi-mqueue: Create predefined mdb table to check the pec.
    When an IBI status is received with the PEC error flag set, further
    matching with the mdb table to ensure the pec error actually occurred.
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: I1052fe864480e1f58389015c4b414a773f56f82a

--- drivers/i3c/i3c-ibi-mqueue.c
+++ drivers/i3c/i3c-ibi-mqueue.c
@@ -18,6 +18,7 @@
 #define MQ_QUEUE_SIZE		4
 #define MQ_QUEUE_NEXT(x)	(((x) + 1) & (MQ_QUEUE_SIZE - 1))
 
+#define IBI_QUEUE_STATUS_PEC_ERR	BIT(30)
 #define IBI_STATUS_LAST_FRAG	BIT(24)
 #define PID_MANUF_ID_ASPEED	0x03f6
 
@@ -39,6 +40,11 @@ struct mq_queue {
 	struct mq_msg queue[MQ_QUEUE_SIZE];
 };
 
+static u8 mdb_table[] = {
+	0xbf, /* Aspeed BIC */
+	0,
+};
+
 static void i3c_ibi_mqueue_callback(struct i3c_device *dev,
 				    const struct i3c_ibi_payload *payload)
 {
@@ -47,6 +53,7 @@ static void i3c_ibi_mqueue_callback(struct i3c_device *dev,
 	u8 *buf = (u8 *)payload->data;
 	struct i3c_device_info info;
 	u32 status;
+	const u8 *mdb;
 
 	i3c_device_get_info(dev, &info);
 	/* first DW is IBI status */
@@ -56,7 +63,15 @@ static void i3c_ibi_mqueue_callback(struct i3c_device *dev,
 	buf += sizeof(status);
 	memcpy(&msg->buf[msg->len], buf, payload->len - sizeof(status));
 	msg->len += payload->len - sizeof(status);
-
+	if (status & IBI_QUEUE_STATUS_PEC_ERR) {
+		for (mdb = mdb_table; *mdb != 0; mdb++)
+			if (buf[0] == *mdb)
+				break;
+		if (!(*mdb)) {
+			dev_err(&dev->dev, "ibi crc/pec error: mdb = %x", buf[0]);
+			return;
+		}
+	}
 	/* if last fragment, notify and update pointers */
 	if (status & IBI_STATUS_LAST_FRAG) {
 		/* check pending-read-notification */
