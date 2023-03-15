288b7baf090b7ea2ac30c09293df00b03e874458
Yang Li <yang.lee@linux.alibaba.com>
2023-03-15 13:50:23 +0800

    mmc: sdhci-of-aspeed: Use devm_platform_get_and_ioremap_resource()
    According to commit 890cc39a8799 ("drivers: provide
    devm_platform_get_and_ioremap_resource()"), convert
    platform_get_resource(), devm_ioremap_resource() to a single
    call to devm_platform_get_and_ioremap_resource(), as this is exactly
    what this function does.
    
    Signed-off-by: Yang Li <yang.lee@linux.alibaba.com>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Link: https://lore.kernel.org/r/20230315055023.61779-1-yang.lee@linux.alibaba.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/sdhci-of-aspeed.c
+++ drivers/mmc/host/sdhci-of-aspeed.c
@@ -547,8 +547,7 @@ static int aspeed_sdc_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	sdc->res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	sdc->regs = devm_ioremap_resource(&pdev->dev, sdc->res);
+	sdc->regs = devm_platform_get_and_ioremap_resource(pdev, 0, &sdc->res);
 	if (IS_ERR(sdc->regs)) {
 		ret = PTR_ERR(sdc->regs);
 		goto err_clk;
