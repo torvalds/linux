b2ed4eb35260ea0826ebf0a17acc4be31afa5a3a
Matteo Croce <mcroce@microsoft.com>
2023-03-15 11:03:26 +0800

    riscv: optimized memcpy Write a C version of memcpy() which uses the biggest data size allowed, without generating unaligned accesses.
    The procedure is made of three steps:
    First copy data one byte at time until the destination buffer is aligned
    to a long boundary.
    Then copy the data one long at time shifting the current and the next u8
    to compose a long at every cycle.
    Finally, copy the remainder one byte at time.
    
    On a BeagleV, the TCP RX throughput increased by 45%
    
    Signed-off-by: Matteo Croce <mcroce@microsoft.com>
    Signed-off-by: Emil Renner Berthing <kernel@esmil.dk>
    Signed-off-by: Samin Guo <samin.guo@starfivetech.com>

--- arch/riscv/include/asm/string.h
+++ arch/riscv/include/asm/string.h
@@ -12,9 +12,11 @@
 #define __HAVE_ARCH_MEMSET
 extern asmlinkage void *memset(void *, int, size_t);
 extern asmlinkage void *__memset(void *, int, size_t);
+
 #define __HAVE_ARCH_MEMCPY
-extern asmlinkage void *memcpy(void *, const void *, size_t);
-extern asmlinkage void *__memcpy(void *, const void *, size_t);
+extern void *memcpy(void *dest, const void *src, size_t count);
+extern void *__memcpy(void *dest, const void *src, size_t count);
+
 #define __HAVE_ARCH_MEMMOVE
 extern asmlinkage void *memmove(void *, const void *, size_t);
 extern asmlinkage void *__memmove(void *, const void *, size_t);
