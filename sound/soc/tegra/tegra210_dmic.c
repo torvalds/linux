7ffba01bcd3e5e4e6ebb13348b6d9975015ab9e2
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2023-03-15 16:07:25 +0100

    ASoC: tegra: tegra210_dmic: Convert to platform remove callback returning void
    The .remove() callback for a platform driver returns an int which makes
    many driver authors wrongly assume it's possible to do error handling by
    returning an error code. However the value returned is (mostly) ignored
    and this typically results in resource leaks. To improve here there is a
    quest to make the remove callback return void. In the first step of this
    quest all drivers are converted to .remove_new() which already returns
    void.
    
    Trivially convert this driver from always returning zero in the remove
    callback to the void returning variant.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Acked-by: Takashi Iwai <tiwai@suse.de>
    Acked-by: Nicolas Ferre <nicolas.ferre@microchip.com>
    Link: https://lore.kernel.org/r/20230315150745.67084-154-u.kleine-koenig@pengutronix.de
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/tegra/tegra210_dmic.c
+++ sound/soc/tegra/tegra210_dmic.c
@@ -534,11 +534,9 @@ static int tegra210_dmic_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static int tegra210_dmic_remove(struct platform_device *pdev)
+static void tegra210_dmic_remove(struct platform_device *pdev)
 {
 	pm_runtime_disable(&pdev->dev);
-
-	return 0;
 }
 
 static const struct dev_pm_ops tegra210_dmic_pm_ops = {
@@ -561,7 +559,7 @@ static struct platform_driver tegra210_dmic_driver = {
 		.pm = &tegra210_dmic_pm_ops,
 	},
 	.probe = tegra210_dmic_probe,
-	.remove = tegra210_dmic_remove,
+	.remove_new = tegra210_dmic_remove,
 };
 module_platform_driver(tegra210_dmic_driver)
 
