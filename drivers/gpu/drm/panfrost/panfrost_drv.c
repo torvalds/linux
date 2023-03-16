ab1a072c05a8f4f2ce3292140b30ea0b64ad260c
AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
2023-03-16 11:20:40 +0100

    drm/panfrost: Add new compatible for Mali on the MT8183 SoC
    The "mediatek,mt8183-mali" compatible uses platform data that calls for
    getting (and managing) two regulators ("mali" and "sram") but devfreq
    does not support this usecase, resulting in DVFS not working.
    
    Since a lot of MediaTek SoCs need to set the voltages for the GPU SRAM
    regulator in a specific relation to the GPU VCORE regulator, a MediaTek
    SoC specific driver was introduced to automatically satisfy, through
    coupling, these constraints: this means that there is at all no need to
    manage both regulators in panfrost but to otherwise just manage the main
    "mali" (-> gpu vcore) regulator instead.
    
    Keeping in mind that we cannot break the ABI, the most sensible route
    (avoiding hacks and uselessly overcomplicated code) to get a MT8183
    node with one power supply was to add a new "mediatek,mt8183b-mali"
    compatible, which effectively deprecates the former.
    
    Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Reviewed-by: Steven Price <steven.price@arm.com>
    Reviewed-by: Chen-Yu Tsai <wenst@chromium.org>
    Tested-by: Chen-Yu Tsai <wenst@chromium.org>
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230316102041.210269-12-angelogioacchino.delregno@collabora.com

--- drivers/gpu/drm/panfrost/panfrost_drv.c
+++ drivers/gpu/drm/panfrost/panfrost_drv.c
@@ -647,11 +647,27 @@ static const struct panfrost_compatible amlogic_data = {
 	.vendor_quirk = panfrost_gpu_amlogic_quirk,
 };
 
+/*
+ * The old data with two power supplies for MT8183 is here only to
+ * keep retro-compatibility with older devicetrees, as DVFS will
+ * not work with this one.
+ *
+ * On new devicetrees please use the _b variant with a single and
+ * coupled regulators instead.
+ */
 static const char * const mediatek_mt8183_supplies[] = { "mali", "sram", NULL };
 static const char * const mediatek_mt8183_pm_domains[] = { "core0", "core1", "core2" };
 static const struct panfrost_compatible mediatek_mt8183_data = {
 	.num_supplies = ARRAY_SIZE(mediatek_mt8183_supplies) - 1,
 	.supply_names = mediatek_mt8183_supplies,
+	.num_pm_domains = ARRAY_SIZE(mediatek_mt8183_pm_domains),
+	.pm_domain_names = mediatek_mt8183_pm_domains,
+};
+
+static const char * const mediatek_mt8183_b_supplies[] = { "mali", NULL };
+static const struct panfrost_compatible mediatek_mt8183_b_data = {
+	.num_supplies = ARRAY_SIZE(mediatek_mt8183_b_supplies) - 1,
+	.supply_names = mediatek_mt8183_b_supplies,
 	.num_pm_domains = ARRAY_SIZE(mediatek_mt8183_pm_domains),
 	.pm_domain_names = mediatek_mt8183_pm_domains,
 };
@@ -684,6 +700,7 @@ static const struct of_device_id dt_match[] = {
 	{ .compatible = "arm,mali-bifrost", .data = &default_data, },
 	{ .compatible = "arm,mali-valhall-jm", .data = &default_data, },
 	{ .compatible = "mediatek,mt8183-mali", .data = &mediatek_mt8183_data },
+	{ .compatible = "mediatek,mt8183b-mali", .data = &mediatek_mt8183_b_data },
 	{ .compatible = "mediatek,mt8192-mali", .data = &mediatek_mt8192_data },
 	{}
 };
