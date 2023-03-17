f596005d6fa9e15044706295ebb358acf53495f2
Chia-Wei Wang <chiawei_wang@aspeedtech.com>
2023-03-17 18:24:56 +0800

    serial: 8250_aspeed_vuart: Add compatible string for AST2600
    Add the compatible string for AST2600 SoCs.
    
    Signed-off-by: Chia-Wei Wang <chiawei_wang@aspeedtech.com>
    Change-Id: I4bf0f4bec5c828b4aa7ff28bbbdb9d6250cb5815

--- drivers/tty/serial/8250/8250_aspeed_vuart.c
+++ drivers/tty/serial/8250/8250_aspeed_vuart.c
@@ -588,6 +588,7 @@ static int aspeed_vuart_remove(struct platform_device *pdev)
 static const struct of_device_id aspeed_vuart_table[] = {
 	{ .compatible = "aspeed,ast2400-vuart" },
 	{ .compatible = "aspeed,ast2500-vuart" },
+	{ .compatible = "aspeed,ast2600-vuart" },
 	{ },
 };
 
