c7154ca8e075cc456fe773879263b33ae307a59e
Eric Dumazet <edumazet@google.com>
2023-03-17 15:55:37 +0000

    x25: preserve const qualifier in [a]x25_sk()
    We can change [a]x25_sk() to propagate their argument const qualifier,
    thanks to container_of_const().
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Simon Horman <simon.horman@corigine.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/ax25.h
+++ include/net/ax25.h
@@ -260,10 +260,7 @@ struct ax25_sock {
 	struct ax25_cb		*cb;
 };
 
-static inline struct ax25_sock *ax25_sk(const struct sock *sk)
-{
-	return (struct ax25_sock *) sk;
-}
+#define ax25_sk(ptr) container_of_const(ptr, struct ax25_sock, sk)
 
 static inline struct ax25_cb *sk_to_ax25(const struct sock *sk)
 {
