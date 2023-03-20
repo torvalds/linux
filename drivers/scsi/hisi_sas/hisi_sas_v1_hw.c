bb544224da77b96b2c11a13872bf91ede1e015be
Xingui Yang <yangxingui@huawei.com>
2023-03-20 11:34:23 +0800

    scsi: hisi_sas: Handle NCQ error when IPTT is valid
    If an NCQ error occurs when the IPTT is valid and slot->abort flag is set
    in completion path, sas_task_abort() will be called to abort only one NCQ
    command now, and the host would be set to SHOST_RECOVERY state. But this
    may not kick-off EH Immediately until other outstanding QCs timeouts. As a
    result, the host may remain in the SHOST_RECOVERY state for up to 30
    seconds, such as follows:
    
    [7972317.645234] hisi_sas_v3_hw 0000:74:04.0: erroneous completion iptt=3264 task=00000000466116b8 dev id=2 sas_addr=0x5000000000000502 CQ hdr: 0x1883 0x20cc0 0x40000 0x20420000 Error info: 0x0 0x0 0x200000 0x0
    [7972341.508264] sas: Enter sas_scsi_recover_host busy: 32 failed: 32
    [7972341.984731] sas: --- Exit sas_scsi_recover_host: busy: 0 failed: 32 tries: 1
    
    All NCQ commands that are in the queue should be aborted when an NCQ error
    occurs in this scenario.
    
    Fixes: 05d91b557af9 ("scsi: hisi_sas: Directly trigger SCSI error handling for completion errors")
    Signed-off-by: Xingui Yang <yangxingui@huawei.com>
    Signed-off-by: Xiang Chen <chenxiang66@hisilicon.com>
    Link: https://lore.kernel.org/r/1679283265-115066-3-git-send-email-chenxiang66@hisilicon.com
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/hisi_sas/hisi_sas_v1_hw.c
+++ drivers/scsi/hisi_sas/hisi_sas_v1_hw.c
@@ -1258,7 +1258,11 @@ static void slot_complete_v1_hw(struct hisi_hba *hisi_hba,
 
 		slot_err_v1_hw(hisi_hba, task, slot);
 		if (unlikely(slot->abort)) {
-			sas_task_abort(task);
+			if (dev_is_sata(device) && task->ata_task.use_ncq)
+				sas_ata_device_link_abort(device, true);
+			else
+				sas_task_abort(task);
+
 			return;
 		}
 		goto out;
