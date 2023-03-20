877d5cd2aeed8fddeb3afe174c6b9eb77cd56f8b
qinyu <qinyu32@huawei.com>
2023-03-20 16:17:02 +0800

    cpufreq: warn about invalid vals to scaling_max/min_freq interfaces
    When echo an invalid val to scaling_min_freq:
    > echo 123abc123 > scaling_min_freq
    It looks weird to have a return val of 0:
    > echo $?
    > 0
    
    Sane people won't echo strings like that into these interfaces but fuzz
    tests may do. Also, maybe it's better to inform people if input is
    invalid.
    
    After this:
    > echo 123abc123 > scaling_min_freq
    > -bash: echo: write error: Invalid argument
    
    Signed-off-by: qinyu <qinyu32@huawei.com>
    Tested-by: zhangxiaofeng <zhangxiaofeng46@huawei.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -725,9 +725,9 @@ static ssize_t store_##file_name					\
 	unsigned long val;						\
 	int ret;							\
 									\
-	ret = sscanf(buf, "%lu", &val);					\
-	if (ret != 1)							\
-		return -EINVAL;						\
+	ret = kstrtoul(buf, 0, &val);					\
+	if (ret)							\
+		return ret;						\
 									\
 	ret = freq_qos_update_request(policy->object##_freq_req, val);\
 	return ret >= 0 ? count : ret;					\
