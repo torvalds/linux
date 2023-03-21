186016da9cca040ae535de09ab1be67e32373443
Siddharth Vadapalli <s-vadapalli@ti.com>
2023-03-21 16:49:58 +0530

    net: ethernet: ti: am65-cpsw: Enable SGMII mode for J721E
    TI's J721E SoC supports SGMII mode with the CPSW9G instance of the CPSW
    Ethernet Switch. Thus, enable it by adding SGMII mode to the extra_modes
    member of the "j721e_cpswxg_pdata" SoC data.
    
    Signed-off-by: Siddharth Vadapalli <s-vadapalli@ti.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/ti/am65-cpsw-nuss.c
+++ drivers/net/ethernet/ti/am65-cpsw-nuss.c
@@ -2784,7 +2784,7 @@ static const struct am65_cpsw_pdata j721e_cpswxg_pdata = {
 	.quirks = 0,
 	.ale_dev_id = "am64-cpswxg",
 	.fdqring_mode = K3_RINGACC_RING_MODE_MESSAGE,
-	.extra_modes = BIT(PHY_INTERFACE_MODE_QSGMII),
+	.extra_modes = BIT(PHY_INTERFACE_MODE_QSGMII) | BIT(PHY_INTERFACE_MODE_SGMII),
 };
 
 static const struct of_device_id am65_cpsw_nuss_of_mtable[] = {
