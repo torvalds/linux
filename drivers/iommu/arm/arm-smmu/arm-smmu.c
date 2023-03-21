a2972cb89935160bfe515b15d28a77694723ac06
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2023-03-21 09:41:16 +0100

    iommu/arm-smmu: Drop if with an always false condition
    The remove and shutdown callback are only called after probe completed
    successfully. In this case platform_set_drvdata() was called with a
    non-NULL argument and so smmu is never NULL. Other functions in this
    driver also don't check for smmu being non-NULL before using it.
    
    Also note that returning an error code from a remove callback doesn't
    result in the device staying bound. It's still removed and devm allocated
    resources are freed (among others *smmu and the register mapping). So
    after an early exit to iommu device stayed around and using it probably
    oopses.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>
    Link: https://lore.kernel.org/r/20230321084125.337021-2-u.kleine-koenig@pengutronix.de
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -2195,9 +2195,6 @@ static void arm_smmu_device_shutdown(struct platform_device *pdev)
 {
 	struct arm_smmu_device *smmu = platform_get_drvdata(pdev);
 
-	if (!smmu)
-		return;
-
 	if (!bitmap_empty(smmu->context_map, ARM_SMMU_MAX_CBS))
 		dev_notice(&pdev->dev, "disabling translation\n");
 
@@ -2217,9 +2214,6 @@ static void arm_smmu_device_shutdown(struct platform_device *pdev)
 static int arm_smmu_device_remove(struct platform_device *pdev)
 {
 	struct arm_smmu_device *smmu = platform_get_drvdata(pdev);
-
-	if (!smmu)
-		return -ENODEV;
 
 	iommu_device_unregister(&smmu->iommu);
 	iommu_device_sysfs_remove(&smmu->iommu);
