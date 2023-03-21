453c3364632a1f305db7d425dc9f8d6711cd3491
Nancy.Lin <nancy.lin@mediatek.com>
2023-03-21 20:18:55 +0800

    drm/mediatek: Add ovl_adaptor support for MT8195
    Add ovl_adaptor driver for MT8195.
    Ovl_adaptor is an encapsulated module and designed for simplified
    DRM control flow. This module is composed of 8 RDMAs, 4 MERGEs and
    an ETHDR. Two RDMAs merge into one layer, so this module support 4
    layers.
    
    Signed-off-by: Nancy.Lin <nancy.lin@mediatek.com>
    Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Tested-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Tested-by: Bo-Chen Chen <rex-bc.chen@mediatek.com>
    Link: https://patchwork.kernel.org/project/linux-mediatek/patch/20230321121859.2355-4-nancy.lin@mediatek.com/
    Signed-off-by: Chun-Kuang Hu <chunkuang.hu@kernel.org>

--- drivers/gpu/drm/mediatek/mtk_drm_drv.h
+++ drivers/gpu/drm/mediatek/mtk_drm_drv.h
@@ -51,6 +51,7 @@ extern struct platform_driver mtk_disp_ccorr_driver;
 extern struct platform_driver mtk_disp_color_driver;
 extern struct platform_driver mtk_disp_gamma_driver;
 extern struct platform_driver mtk_disp_merge_driver;
+extern struct platform_driver mtk_disp_ovl_adaptor_driver;
 extern struct platform_driver mtk_disp_ovl_driver;
 extern struct platform_driver mtk_disp_rdma_driver;
 extern struct platform_driver mtk_dpi_driver;
