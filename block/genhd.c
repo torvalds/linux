a2eb778aaceafca546cd2e9b4da59a628f68c344
Yu Kuai <yukuai3@huawei.com>
2023-03-22 11:59:26 +0800

    block: don't set GD_NEED_PART_SCAN if scan partition failed
    [ Upstream commit 3723091ea1884d599cc8b8bf719d6f42e8d4d8b1 ]
    
    Currently if disk_scan_partitions() failed, GD_NEED_PART_SCAN will still
    set, and partition scan will be proceed again when blkdev_get_by_dev()
    is called. However, this will cause a problem that re-assemble partitioned
    raid device will creat partition for underlying disk.
    
    Test procedure:
    
    mdadm -CR /dev/md0 -l 1 -n 2 /dev/sda /dev/sdb -e 1.0
    sgdisk -n 0:0:+100MiB /dev/md0
    blockdev --rereadpt /dev/sda
    blockdev --rereadpt /dev/sdb
    mdadm -S /dev/md0
    mdadm -A /dev/md0 /dev/sda /dev/sdb
    
    Test result: underlying disk partition and raid partition can be
    observed at the same time
    
    Note that this can still happen in come corner cases that
    GD_NEED_PART_SCAN can be set for underlying disk while re-assemble raid
    device.
    
    Fixes: e5cfefa97bcc ("block: fix scan partition for exclusively open device again")
    Reviewed-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- block/genhd.c
+++ block/genhd.c
@@ -368,7 +368,6 @@ int disk_scan_partitions(struct gendisk *disk, fmode_t mode)
 	if (disk->open_partitions)
 		return -EBUSY;
 
-	set_bit(GD_NEED_PART_SCAN, &disk->state);
 	/*
 	 * If the device is opened exclusively by current thread already, it's
 	 * safe to scan partitons, otherwise, use bd_prepare_to_claim() to
@@ -381,12 +380,19 @@ int disk_scan_partitions(struct gendisk *disk, fmode_t mode)
 			return ret;
 	}
 
+	set_bit(GD_NEED_PART_SCAN, &disk->state);
 	bdev = blkdev_get_by_dev(disk_devt(disk), mode & ~FMODE_EXCL, NULL);
 	if (IS_ERR(bdev))
 		ret =  PTR_ERR(bdev);
 	else
 		blkdev_put(bdev, mode & ~FMODE_EXCL);
 
+	/*
+	 * If blkdev_get_by_dev() failed early, GD_NEED_PART_SCAN is still set,
+	 * and this will cause that re-assemble partitioned raid device will
+	 * creat partition for underlying disk.
+	 */
+	clear_bit(GD_NEED_PART_SCAN, &disk->state);
 	if (!(mode & FMODE_EXCL))
 		bd_abort_claiming(disk->part0, disk_scan_partitions);
 	return ret;
