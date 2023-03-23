188f46cac267b9b8f3201ed21d719a51946e1e34
Yang Li <yang.lee@linux.alibaba.com>
2023-03-23 15:45:53 +0800

    PCI: imx6: Use devm_platform_get_and_ioremap_resource()
    Convert calls to platform_get_resource() and devm_ioremap_resource()
    into a single call to devm_platform_get_and_ioremap_resource(), to
    simplify the code.
    
    This new helper added in commit 890cc39a8799 ("drivers: provide
    devm_platform_get_and_ioremap_resource()") does what both of the
    other separate functions do.
    
    [kwilczynski: commit log]
    Link: https://lore.kernel.org/linux-pci/20230323074553.90372-1-yang.lee@linux.alibaba.com
    Signed-off-by: Yang Li <yang.lee@linux.alibaba.com>
    Signed-off-by: Krzysztof Wilczyński <kwilczynski@kernel.org>
    Reviewed-by: Richard Zhu <hongxing.zhu@nxp.com>

--- drivers/pci/controller/dwc/pci-imx6.c
+++ drivers/pci/controller/dwc/pci-imx6.c
@@ -1282,8 +1282,7 @@ static int imx6_pcie_probe(struct platform_device *pdev)
 			return PTR_ERR(imx6_pcie->phy_base);
 	}
 
-	dbi_base = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	pci->dbi_base = devm_ioremap_resource(dev, dbi_base);
+	pci->dbi_base = devm_platform_get_and_ioremap_resource(pdev, 0, &dbi_base);
 	if (IS_ERR(pci->dbi_base))
 		return PTR_ERR(pci->dbi_base);
 
