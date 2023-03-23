f812e2a9f85d6bea78957ccb5197e4491316848b
Cai Huoqing <cai.huoqing@linux.dev>
2023-03-23 19:26:09 +0800

    wifi: ath11k: Remove redundant pci_clear_master
    Remove pci_clear_master to simplify the code,
    the bus-mastering is also cleared in do_pci_disable_device,
    like this:
    ./drivers/pci/pci.c:2197
    static void do_pci_disable_device(struct pci_dev *dev)
    {
    	u16 pci_command;
    
    	pci_read_config_word(dev, PCI_COMMAND, &pci_command);
    	if (pci_command & PCI_COMMAND_MASTER) {
    		pci_command &= ~PCI_COMMAND_MASTER;
    		pci_write_config_word(dev, PCI_COMMAND, pci_command);
    	}
    
    	pcibios_disable_device(dev);
    }.
    And dev->is_busmaster is set to 0 in pci_disable_device.
    
    Signed-off-by: Cai Huoqing <cai.huoqing@linux.dev>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://lore.kernel.org/r/20230323112613.7550-1-cai.huoqing@linux.dev

--- drivers/net/wireless/ath/ath11k/pci.c
+++ drivers/net/wireless/ath/ath11k/pci.c
@@ -540,7 +540,7 @@ static int ath11k_pci_claim(struct ath11k_pci *ab_pci, struct pci_dev *pdev)
 	if (!ab->mem) {
 		ath11k_err(ab, "failed to map pci bar %d\n", ATH11K_PCI_BAR_NUM);
 		ret = -EIO;
-		goto clear_master;
+		goto release_region;
 	}
 
 	ab->mem_ce = ab->mem;
@@ -548,8 +548,6 @@ static int ath11k_pci_claim(struct ath11k_pci *ab_pci, struct pci_dev *pdev)
 	ath11k_dbg(ab, ATH11K_DBG_BOOT, "boot pci_mem 0x%pK\n", ab->mem);
 	return 0;
 
-clear_master:
-	pci_clear_master(pdev);
 release_region:
 	pci_release_region(pdev, ATH11K_PCI_BAR_NUM);
 disable_device:
@@ -565,7 +563,6 @@ static void ath11k_pci_free_region(struct ath11k_pci *ab_pci)
 
 	pci_iounmap(pci_dev, ab->mem);
 	ab->mem = NULL;
-	pci_clear_master(pci_dev);
 	pci_release_region(pci_dev, ATH11K_PCI_BAR_NUM);
 	if (pci_is_enabled(pci_dev))
 		pci_disable_device(pci_dev);
