3228150ba688b8cddc7459e5ae9a09bc2d2e47be
Cai Huoqing <cai.huoqing@linux.dev>
2023-03-23 17:03:07 +0800

    ethernet: ec_bhf: Remove redundant pci_clear_master
    Remove pci_clear_master to simplify the code,
    the bus-mastering is also cleared in do_pci_disable_device,
    like this:
    ./drivers/pci/pci.c:2197
    static void do_pci_disable_device(struct pci_dev *dev)
    {
    	u16 pci_command;
    
    	pci_read_config_word(dev, PCI_COMMAND, &pci_command);
    	if (pci_command & PCI_COMMAND_MASTER) {
    		pci_command &= ~PCI_COMMAND_MASTER;
    		pci_write_config_word(dev, PCI_COMMAND, pci_command);
    	}
    
    	pcibios_disable_device(dev);
    }.
    And dev->is_busmaster is set to 0 in pci_disable_device.
    
    Signed-off-by: Cai Huoqing <cai.huoqing@linux.dev>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/ec_bhf.c
+++ drivers/net/ethernet/ec_bhf.c
@@ -558,7 +558,6 @@ err_unmap:
 err_release_regions:
 	pci_release_regions(dev);
 err_disable_dev:
-	pci_clear_master(dev);
 	pci_disable_device(dev);
 
 	return err;
@@ -577,7 +576,6 @@ static void ec_bhf_remove(struct pci_dev *dev)
 	free_netdev(net_dev);
 
 	pci_release_regions(dev);
-	pci_clear_master(dev);
 	pci_disable_device(dev);
 }
 
