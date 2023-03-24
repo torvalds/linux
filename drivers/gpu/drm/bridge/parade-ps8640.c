14aed8ea48e2f5c710ab678e1ba32b9f398d40b2
Pin-yen Lin <treapking@chromium.org>
2023-03-24 11:59:09 +0800

    drm/bridge: ps8640: Return NULL immediately when EDID read fail
    drm_edid_read returns NULL on error, so feeding it directly into
    drm_edid_duplicate may lead to NULL pointer dereference. Add a check to
    guard this.
    
    Fixes: 6a17b4d1b52f ("drm/bridge: ps8640: Add a cache for EDID")
    Signed-off-by: Pin-yen Lin <treapking@chromium.org>
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230324035909.1727356-1-treapking@chromium.org

--- drivers/gpu/drm/bridge/parade-ps8640.c
+++ drivers/gpu/drm/bridge/parade-ps8640.c
@@ -543,6 +543,7 @@ static struct edid *ps8640_bridge_get_edid(struct drm_bridge *bridge,
 					   struct drm_connector *connector)
 {
 	struct ps8640 *ps_bridge = bridge_to_ps8640(bridge);
+	struct device *dev = &ps_bridge->page[PAGE0_DP_CNTL]->dev;
 	bool poweroff = !ps_bridge->pre_enabled;
 
 	if (!ps_bridge->edid) {
@@ -572,6 +573,11 @@ static struct edid *ps8640_bridge_get_edid(struct drm_bridge *bridge,
 		if (poweroff)
 			drm_atomic_bridge_chain_post_disable(bridge,
 							     connector->state->state);
+	}
+
+	if (!ps_bridge->edid) {
+		dev_err(dev, "Failed to get EDID\n");
+		return NULL;
 	}
 
 	return drm_edid_duplicate(ps_bridge->edid);
