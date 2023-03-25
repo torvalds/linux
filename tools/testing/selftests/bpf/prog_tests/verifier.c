caf345cf12073eb5905b03c3c5f905cc0964dc6c
Eduard Zingerman <eddyz87@gmail.com>
2023-03-25 04:55:08 +0200

    selftests/bpf: verifier/map_ptr.c converted to inline assembly
    Test verifier/map_ptr.c automatically converted to use inline assembly.
    
    Signed-off-by: Eduard Zingerman <eddyz87@gmail.com>
    Link: https://lore.kernel.org/r/20230325025524.144043-28-eddyz87@gmail.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/testing/selftests/bpf/prog_tests/verifier.c
+++ tools/testing/selftests/bpf/prog_tests/verifier.c
@@ -24,6 +24,7 @@
 #include "verifier_int_ptr.skel.h"
 #include "verifier_ld_ind.skel.h"
 #include "verifier_leak_ptr.skel.h"
+#include "verifier_map_ptr.skel.h"
 
 __maybe_unused
 static void run_tests_aux(const char *skel_name, skel_elf_bytes_fn elf_bytes_factory)
@@ -70,3 +71,4 @@ void test_verifier_helper_value_access(void)  { RUN(verifier_helper_value_access
 void test_verifier_int_ptr(void)              { RUN(verifier_int_ptr); }
 void test_verifier_ld_ind(void)               { RUN(verifier_ld_ind); }
 void test_verifier_leak_ptr(void)             { RUN(verifier_leak_ptr); }
+void test_verifier_map_ptr(void)              { RUN(verifier_map_ptr); }
