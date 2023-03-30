95f0e3a209b0045a56a06987d85981280f523270
Jiapeng Chong <jiapeng.chong@linux.alibaba.com>
2023-03-30 10:00:14 +0800

    x86/unwind/orc: Use swap() instead of open coding it
    Swap is a function interface that provides exchange function. To avoid
    code duplication, we can use swap function.
    
    ./arch/x86/kernel/unwind_orc.c:235:16-17: WARNING opportunity for swap().
    
    Reported-by: Abaci Robot <abaci@linux.alibaba.com>
    Link: https://bugzilla.openanolis.cn/show_bug.cgi?id=4641
    Signed-off-by: Jiapeng Chong <jiapeng.chong@linux.alibaba.com>
    Link: https://lore.kernel.org/r/20230330020014.40489-1-jiapeng.chong@linux.alibaba.com
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>

--- arch/x86/kernel/unwind_orc.c
+++ arch/x86/kernel/unwind_orc.c
@@ -219,7 +219,6 @@ static struct orc_entry *cur_orc_table = __start_orc_unwind;
 static void orc_sort_swap(void *_a, void *_b, int size)
 {
 	struct orc_entry *orc_a, *orc_b;
-	struct orc_entry orc_tmp;
 	int *a = _a, *b = _b, tmp;
 	int delta = _b - _a;
 
@@ -231,9 +230,7 @@ static void orc_sort_swap(void *_a, void *_b, int size)
 	/* Swap the corresponding .orc_unwind entries: */
 	orc_a = cur_orc_table + (a - cur_orc_ip_table);
 	orc_b = cur_orc_table + (b - cur_orc_ip_table);
-	orc_tmp = *orc_a;
-	*orc_a = *orc_b;
-	*orc_b = orc_tmp;
+	swap(*orc_a, *orc_b);
 }
 
 static int orc_sort_cmp(const void *_a, const void *_b)
