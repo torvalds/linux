dbc84c4ed36abf75953659a2c9ae80227b2338be
Oreoluwa Babatunde <quic_obabatun@quicinc.com>
2023-03-31 14:03:46 -0700

    dma-heap: Add the qcom,ubwcp-movable heap
    Add code to implement a new dmabuf heap called qcom,ubwcp-movable
    that is used to support lending memory from the movable zone for
    ubwcp use cases when the system is in low memory situations.
    
    Change-Id: I8e77b4142769b6ca2a5e8bce5969c9b4912c3c51
    Signed-off-by: Oreoluwa Babatunde <quic_obabatun@quicinc.com>

--- drivers/dma-buf/heaps/qcom_ubwcp_heap.h
+++ drivers/dma-buf/heaps/qcom_ubwcp_heap.h
@@ -1,15 +1,15 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _QCOM_UBWCP_HEAP_H
 #define _QCOM_UBWCP_HEAP_H
 
 #ifdef CONFIG_QCOM_DMABUF_HEAPS_UBWCP
-int qcom_ubwcp_heap_create(void);
+int qcom_ubwcp_heap_create(char *name, bool movable);
 #else
-int qcom_ubwcp_heap_create(void)
+int qcom_ubwcp_heap_create(char *name, bool movable)
 {
 	return 0;
 }
