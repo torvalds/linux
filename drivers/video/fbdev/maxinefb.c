8c13c9de80b516d2a168e18744c9b7f586135ea4
Thomas Zimmermann <tzimmermann@suse.de>
2023-03-31 11:23:08 +0200

    fbdev: maxinefb: Remove trailing whitespaces
    Fix coding style. No functional changes.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Signed-off-by: Helge Deller <deller@gmx.de>

--- drivers/video/fbdev/maxinefb.c
+++ drivers/video/fbdev/maxinefb.c
@@ -138,7 +138,7 @@ int __init maxinefb_init(void)
 		*(volatile unsigned char *)fboff = 0x0;
 
 	maxinefb_fix.smem_start = fb_start;
-	
+
 	/* erase hardware cursor */
 	for (i = 0; i < 512; i++) {
 		maxinefb_ims332_write_register(IMS332_REG_CURSOR_RAM + i,
