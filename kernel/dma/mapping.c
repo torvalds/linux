1d3f56b295302fdb4ac9caf6ce09f5ae7d2e651a
Jiaxun Yang <jiaxun.yang@flygoat.com>
2023-04-01 10:15:30 +0100

    dma-mapping: provide CONFIG_ARCH_DMA_DEFAULT_COHERENT
    Provide a kconfig option to allow arches to manipulate default
    value of dma_default_coherent in Kconfig.
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- kernel/dma/mapping.c
+++ kernel/dma/mapping.c
@@ -20,7 +20,7 @@
 #if defined(CONFIG_ARCH_HAS_SYNC_DMA_FOR_DEVICE) || \
 	defined(CONFIG_ARCH_HAS_SYNC_DMA_FOR_CPU) || \
 	defined(CONFIG_ARCH_HAS_SYNC_DMA_FOR_CPU_ALL)
-bool dma_default_coherent;
+bool dma_default_coherent = IS_ENABLED(CONFIG_ARCH_DMA_DEFAULT_COHERENT);
 #endif
 
 /*
