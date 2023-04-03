d3205ab75e99a47539ec91ef85ba488f4ddfeaa9
Keith Busch <kbusch@kernel.org>
2023-04-03 13:09:25 -0700

    nvme: fix discard support without oncs
    The device can report discard support without setting the ONCS DSM bit.
    When not set, the driver clears max_discard_size expecting it to be set
    later. We don't know the size until we have the namespace format,
    though, so setting it is deferred until configuring one, but the driver
    was abandoning the discard settings due to that initial clearing.
    
    Move the max_discard_size calculation above the check for a '0' discard
    size.
    
    Fixes: 1a86924e4f46475 ("nvme: fix interpretation of DMRSL")
    Reported-by: Laurence Oberman <loberman@redhat.com>
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Reviewed-by: Niklas Cassel <niklas.cassel@wdc.com>
    Reviewed-by: Sagi Grimberg <sagi@grimberg.me>
    Tested-by: Laurence Oberman <loberman@redhat.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -1674,6 +1674,9 @@ static void nvme_config_discard(struct gendisk *disk, struct nvme_ns *ns)
 	struct request_queue *queue = disk->queue;
 	u32 size = queue_logical_block_size(queue);
 
+	if (ctrl->dmrsl && ctrl->dmrsl <= nvme_sect_to_lba(ns, UINT_MAX))
+		ctrl->max_discard_sectors = nvme_lba_to_sect(ns, ctrl->dmrsl);
+
 	if (ctrl->max_discard_sectors == 0) {
 		blk_queue_max_discard_sectors(queue, 0);
 		return;
@@ -1687,9 +1690,6 @@ static void nvme_config_discard(struct gendisk *disk, struct nvme_ns *ns)
 	/* If discard is already enabled, don't reset queue limits */
 	if (queue->limits.max_discard_sectors)
 		return;
-
-	if (ctrl->dmrsl && ctrl->dmrsl <= nvme_sect_to_lba(ns, UINT_MAX))
-		ctrl->max_discard_sectors = nvme_lba_to_sect(ns, ctrl->dmrsl);
 
 	blk_queue_max_discard_sectors(queue, ctrl->max_discard_sectors);
 	blk_queue_max_discard_segments(queue, ctrl->max_discard_segments);
