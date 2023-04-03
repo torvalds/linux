46384d0990bf99ed8b597e8794ea581e2a647710
Asahi Lina <lina@asahilina.net>
2023-04-03 18:48:10 +0900

    rust: error: Rename to_kernel_errno() -> to_errno()
    This is kernel code, so specifying "kernel" is redundant. Let's simplify
    things and just call it to_errno().
    
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Reviewed-by: Martin Rodriguez Reboredo <yakoyoku@gmail.com>
    Signed-off-by: Asahi Lina <lina@asahilina.net>
    Link: https://lore.kernel.org/r/20230224-rust-error-v3-1-03779bddc02b@asahilina.net
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- rust/macros/module.rs
+++ rust/macros/module.rs
@@ -278,7 +278,7 @@ pub(crate) fn module(ts: TokenStream) -> TokenStream {
                         return 0;
                     }}
                     Err(e) => {{
-                        return e.to_kernel_errno();
+                        return e.to_errno();
                     }}
                 }}
             }}
