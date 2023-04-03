f0417e72add5aed5997092b6eb76298290d866c9
Eli Cohen <elic@nvidia.com>
2023-04-03 14:40:39 +0300

    vdpa/mlx5: Add and remove debugfs in setup/teardown driver
    The right place to add the debugfs create is in
    setup_driver() and remove it in teardown_driver().
    
    Current code adds the debugfs when creating the device but resetting a
    device will remove the debugfs subtree and subsequent set_driver will
    not be able to create the files since the debugfs pointer is NULL.
    
    Fixes: 294221004322 ("vdpa/mlx5: Add debugfs subtree")
    Signed-off-by: Eli Cohen <elic@nvidia.com>
    
    v3 -> v4:
    Fix error flow in setup_driver()
    Message-Id: <20230403114039.11102-1-elic@nvidia.com>
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Jason Wang <jasowang@redhat.com>

--- drivers/vdpa/mlx5/net/mlx5_vnet.c
+++ drivers/vdpa/mlx5/net/mlx5_vnet.c
@@ -2467,10 +2467,11 @@ static int setup_driver(struct mlx5_vdpa_dev *mvdev)
 		err = 0;
 		goto out;
 	}
+	mlx5_vdpa_add_debugfs(ndev);
 	err = setup_virtqueues(mvdev);
 	if (err) {
 		mlx5_vdpa_warn(mvdev, "setup_virtqueues\n");
-		goto out;
+		goto err_setup;
 	}
 
 	err = create_rqt(ndev);
@@ -2500,6 +2501,8 @@ err_tir:
 	destroy_rqt(ndev);
 err_rqt:
 	teardown_virtqueues(ndev);
+err_setup:
+	mlx5_vdpa_remove_debugfs(ndev->debugfs);
 out:
 	return err;
 }
@@ -2513,6 +2516,8 @@ static void teardown_driver(struct mlx5_vdpa_net *ndev)
 	if (!ndev->setup)
 		return;
 
+	mlx5_vdpa_remove_debugfs(ndev->debugfs);
+	ndev->debugfs = NULL;
 	teardown_steering(ndev);
 	destroy_tir(ndev);
 	destroy_rqt(ndev);
@@ -3261,7 +3266,6 @@ static int mlx5_vdpa_dev_add(struct vdpa_mgmt_dev *v_mdev, const char *name,
 	if (err)
 		goto err_reg;
 
-	mlx5_vdpa_add_debugfs(ndev);
 	mgtdev->ndev = ndev;
 	return 0;
 
