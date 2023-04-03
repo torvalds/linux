6551a7fe0acbbc816802350414b37d9b42daccc8
Miguel Ojeda <ojeda@kernel.org>
2023-04-03 18:48:12 +0900

    rust: error: Add Error::from_errno{_unchecked}()
    Add a function to create `Error` values out of a kernel error return,
    which safely upholds the invariant that the error code is well-formed
    (negative and greater than -MAX_ERRNO). If a malformed code is passed
    in, it will be converted to EINVAL.
    
    Lina: Imported from rust-for-linux/rust as authored by Miguel and Fox
    with refactoring from Wedson, renamed from_kernel_errno() to
    from_errno().
    
    Co-developed-by: Fox Chen <foxhlchen@gmail.com>
    Signed-off-by: Fox Chen <foxhlchen@gmail.com>
    Co-developed-by: Wedson Almeida Filho <wedsonaf@gmail.com>
    Signed-off-by: Wedson Almeida Filho <wedsonaf@gmail.com>
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>
    Reviewed-by: Andreas Hindborg <a.hindborg@samsung.com>
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Reviewed-by: Martin Rodriguez Reboredo <yakoyoku@gmail.com>
    Signed-off-by: Asahi Lina <lina@asahilina.net>
    Link: https://lore.kernel.org/r/20230224-rust-error-v3-3-03779bddc02b@asahilina.net
    [ Mark the new associated functions as `#[allow(dead_code)]`. ]
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- rust/kernel/error.rs
+++ rust/kernel/error.rs
@@ -72,6 +72,38 @@ pub mod code {
 pub struct Error(core::ffi::c_int);
 
 impl Error {
+    /// Creates an [`Error`] from a kernel error code.
+    ///
+    /// It is a bug to pass an out-of-range `errno`. `EINVAL` would
+    /// be returned in such a case.
+    #[allow(dead_code)]
+    pub(crate) fn from_errno(errno: core::ffi::c_int) -> Error {
+        if errno < -(bindings::MAX_ERRNO as i32) || errno >= 0 {
+            // TODO: Make it a `WARN_ONCE` once available.
+            crate::pr_warn!(
+                "attempted to create `Error` with out of range `errno`: {}",
+                errno
+            );
+            return code::EINVAL;
+        }
+
+        // INVARIANT: The check above ensures the type invariant
+        // will hold.
+        Error(errno)
+    }
+
+    /// Creates an [`Error`] from a kernel error code.
+    ///
+    /// # Safety
+    ///
+    /// `errno` must be within error code range (i.e. `>= -MAX_ERRNO && < 0`).
+    #[allow(dead_code)]
+    unsafe fn from_errno_unchecked(errno: core::ffi::c_int) -> Error {
+        // INVARIANT: The contract ensures the type invariant
+        // will hold.
+        Error(errno)
+    }
+
     /// Returns the kernel error code.
     pub fn to_errno(self) -> core::ffi::c_int {
         self.0
