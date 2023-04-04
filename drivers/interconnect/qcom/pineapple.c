b2ca9b6350f0edb10b9ae17f209a072eb3efe158
Vivek Aknurwar <quic_viveka@quicinc.com>
2023-04-03 23:23:42 -0700

    interconnect: qcom: pineapple: fix UAF under remove function
    UAF is observed while unloading the interconnect driver.
    Interconnect is core to the system and should not
    be unloaded once it is probed. Marking the driver as
    permanent by removing the module_exit function.
    
    Change-Id: Ibb8372c01ccb45079e554b357e292a34447745c8
    Signed-off-by: Vivek Aknurwar <quic_viveka@quicinc.com>

--- drivers/interconnect/qcom/pineapple.c
+++ drivers/interconnect/qcom/pineapple.c
@@ -3326,11 +3326,5 @@ static int __init qnoc_driver_init(void)
 }
 core_initcall(qnoc_driver_init);
 
-static void __exit qnoc_driver_exit(void)
-{
-	platform_driver_unregister(&qnoc_driver);
-}
-module_exit(qnoc_driver_exit);
-
 MODULE_DESCRIPTION("Pineapple NoC driver");
 MODULE_LICENSE("GPL v2");
