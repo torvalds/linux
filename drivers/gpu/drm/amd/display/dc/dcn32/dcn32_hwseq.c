e0a77e09c707cf89317de00f87b94b1168f27acd
Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>
2023-04-04 14:54:05 -0600

    drm/amd/display: Add missing WA and MCLK validation
    When the commit fff7eb56b376 ("drm/amd/display: Don't set dram clock
    change requirement for SubVP") was merged, we missed some parts
    associated with the MCLK switch. This commit adds all the missing parts.
    
    Fixes: fff7eb56b376 ("drm/amd/display: Don't set dram clock change requirement for SubVP")
    Reviewed-by: Aurabindo Pillai <aurabindo.pillai@amd.com>
    Signed-off-by: Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn32/dcn32_hwseq.c
+++ drivers/gpu/drm/amd/display/dc/dcn32/dcn32_hwseq.c
@@ -948,6 +948,7 @@ void dcn32_init_hw(struct dc *dc)
 	if (dc->ctx->dmub_srv) {
 		dc_dmub_srv_query_caps_cmd(dc->ctx->dmub_srv->dmub);
 		dc->caps.dmub_caps.psr = dc->ctx->dmub_srv->dmub->feature_caps.psr;
+		dc->caps.dmub_caps.mclk_sw = dc->ctx->dmub_srv->dmub->feature_caps.fw_assisted_mclk_switch;
 	}
 }
 
