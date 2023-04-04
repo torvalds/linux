5212a53c0e698ee5c8c84fd896646d916173aaaa
Mika Westerberg <mika.westerberg@linux.intel.com>
2023-04-04 15:32:55 -0500

    PCI/PM: Drop pci_bridge_wait_for_secondary_bus() timeout parameter
    [ Upstream commit e74b2b58ff715ca17bb49c3ad89f665a7150e14b ]
    
    All callers of pci_bridge_wait_for_secondary_bus() supply a timeout of
    PCIE_RESET_READY_POLL_MS, so drop the parameter.  Move the definition of
    PCIE_RESET_READY_POLL_MS into pci.c, the only user.
    
    [bhelgaas: extracted from
    https: //lore.kernel.org/r/20230404052714.51315-3-mika.westerberg@linux.intel.com]
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Stable-dep-of: 3e40aa29d47e ("PCI: Wait for Link before restoring Downstream Buses")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -69,12 +69,6 @@ struct pci_cap_saved_state *pci_find_saved_ext_cap(struct pci_dev *dev,
  * Reset (PCIe r6.0 sec 5.8).
  */
 #define PCI_RESET_WAIT		1000	/* msec */
-/*
- * Devices may extend the 1 sec period through Request Retry Status completions
- * (PCIe r6.0 sec 2.3.1).  The spec does not provide an upper limit, but 60 sec
- * ought to be enough for any device to become responsive.
- */
-#define PCIE_RESET_READY_POLL_MS 60000	/* msec */
 
 void pci_update_current_state(struct pci_dev *dev, pci_power_t state);
 void pci_refresh_power_state(struct pci_dev *dev);
@@ -99,8 +93,7 @@ void pci_msix_init(struct pci_dev *dev);
 bool pci_bridge_d3_possible(struct pci_dev *dev);
 void pci_bridge_d3_update(struct pci_dev *dev);
 void pci_bridge_reconfigure_ltr(struct pci_dev *dev);
-int pci_bridge_wait_for_secondary_bus(struct pci_dev *dev, char *reset_type,
-				      int timeout);
+int pci_bridge_wait_for_secondary_bus(struct pci_dev *dev, char *reset_type);
 
 static inline void pci_wakeup_event(struct pci_dev *dev)
 {
