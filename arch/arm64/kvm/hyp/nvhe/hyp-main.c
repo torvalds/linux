3e3a126ea3cffc1ce5c1caa3774f90317405bf70
Fuad Tabba <tabba@google.com>
2023-04-06 12:46:33 +0100

    ANDROID: KVM: arm64: Factor out logic for setting SVE vector length at hyp
    Factor out the logic for setting the SVE vector length at the
    hypervisor in pKVM to the maximum hardware supported value when
    saving and restoring host SVE state.
    
    This shares common code, and elides an unused variable warning
    when SVE is not configured.
    
    Change-Id: Ibaa58e5bb44e4f85b9548a6abb498f0a769f6a4b
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Bug: 267291591

--- arch/arm64/kvm/hyp/nvhe/hyp-main.c
+++ arch/arm64/kvm/hyp/nvhe/hyp-main.c
@@ -688,10 +688,9 @@ static void fpsimd_host_restore(void)
 
 		if (system_supports_sve()) {
 			struct kvm_host_sve_state *sve_state = get_host_sve_state(vcpu);
-			u64 vq_len = sve_vq_from_vl(kvm_host_sve_max_vl) - 1;
 
 			write_sysreg_el1(sve_state->zcr_el1, SYS_ZCR);
-			sve_cond_update_zcr_vq(vq_len, SYS_ZCR_EL2);
+			pkvm_set_max_sve_vq();
 			__sve_restore_state(sve_state->sve_regs +
 					    sve_ffr_offset(kvm_host_sve_max_vl),
 					    &sve_state->fpsr);
