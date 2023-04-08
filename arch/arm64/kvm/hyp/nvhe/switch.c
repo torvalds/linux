55b5bac15939dec3cbcbee1f6271bc3a4afd4534
Marc Zyngier <maz@kernel.org>
2023-04-08 17:04:23 +0100

    KVM: arm64: nvhe: Synchronise with page table walker on vcpu run
    When taking an exception between the EL1&0 translation regime and
    the EL2 translation regime, the page table walker is allowed to
    complete the walks started from EL0 or EL1 while running at EL2.
    
    It means that altering the system registers that define the EL1&0
    translation regime is fraught with danger *unless* we wait for
    the completion of such walk with a DSB (R_LFHQG and subsequent
    statements in the ARM ARM). We already did the right thing for
    other external agents (SPE, TRBE), but not the PTW.
    
    Rework the existing SPE/TRBE synchronisation to include the PTW,
    and add the missing DSB on guest exit.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Reviewed-by: Oliver Upton <oliver.upton@linux.dev>

--- arch/arm64/kvm/hyp/nvhe/switch.c
+++ arch/arm64/kvm/hyp/nvhe/switch.c
@@ -272,6 +272,17 @@ int __kvm_vcpu_run(struct kvm_vcpu *vcpu)
 	 */
 	__debug_save_host_buffers_nvhe(vcpu);
 
+	/*
+	 * We're about to restore some new MMU state. Make sure
+	 * ongoing page-table walks that have started before we
+	 * trapped to EL2 have completed. This also synchronises the
+	 * above disabling of SPE and TRBE.
+	 *
+	 * See DDI0487I.a D8.1.5 "Out-of-context translation regimes",
+	 * rule R_LFHQG and subsequent information statements.
+	 */
+	dsb(nsh);
+
 	__kvm_adjust_pc(vcpu);
 
 	/*
@@ -305,6 +316,13 @@ int __kvm_vcpu_run(struct kvm_vcpu *vcpu)
 	__sysreg32_save_state(vcpu);
 	__timer_disable_traps(vcpu);
 	__hyp_vgic_save_state(vcpu);
+
+	/*
+	 * Same thing as before the guest run: we're about to switch
+	 * the MMU context, so let's make sure we don't have any
+	 * ongoing EL1&0 translations.
+	 */
+	dsb(nsh);
 
 	__deactivate_traps(vcpu);
 	__load_host_stage2();
