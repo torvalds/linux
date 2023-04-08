dfe09eba62c97aeaca2e7dbd743fcf959bc9e825
simoes <trintaeoitogc@gmail.com>
2023-04-08 14:57:54 -0300

    feat: initial create a lib to printk show fp

---
+++ lib/fp_printk/fp_printk.c
@@ -0,0 +1,14 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <asm/fpu/api.h>
+
+MODULE_AUTHOR("Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>");
+MODULE_DESCRIPTION("This lib will ");
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_VERSION("0.1");
+
+static char* __init fp_printk(float number) 
+{
+
+}
