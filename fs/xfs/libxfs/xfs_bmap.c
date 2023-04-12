774a99b47b588bf0bd9f65d3b241d5bba0b2fcb0
Darrick J. Wong <djwong@kernel.org>
2023-04-11 18:59:53 -0700

    xfs: give xfs_bmap_intent its own perag reference
    Give the xfs_bmap_intent an active reference to the perag structure
    data.  This reference will be used to enable scrub intent draining
    functionality in subsequent patches.  Later, shrink will use these
    passive references to know if an AG is quiesced or not.
    
    The reason why we take a passive ref for a file mapping operation is
    simple: we're committing to some sort of action involving space in an
    AG, so we want to indicate our interest in that AG.  The space is
    already allocated, so we need to be able to operate on AGs that are
    offline or being shrunk.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Dave Chinner <dchinner@redhat.com>

--- fs/xfs/libxfs/xfs_bmap.c
+++ fs/xfs/libxfs/xfs_bmap.c
@@ -6075,6 +6075,7 @@ __xfs_bmap_add(
 	bi->bi_whichfork = whichfork;
 	bi->bi_bmap = *bmap;
 
+	xfs_bmap_update_get_group(tp->t_mountp, bi);
 	xfs_defer_add(tp, XFS_DEFER_OPS_TYPE_BMAP, &bi->bi_list);
 	return 0;
 }
