db76f19a607d6cf4a9d5868d05675c355e99809c
Thomas Zimmermann <tzimmermann@suse.de>
2023-04-17 14:56:50 +0200

    arch/sparc: Implement <asm/fb.h> with generic helpers
    Replace sparc64's fb_pgprotect() with the generic one
    from <asm-generic/fb.h>. On sparc, pgprot_writecombine() and
    pgprot_noncached() are the same; hence no functional changes
    
    v3:
    	* use default implementation for fb_pgprotect() on
    	  sparc64 (Arnd)
    v2:
    	* restore the original fb_pgprotect()
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Cc: "David S. Miller" <davem@davemloft.net>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Helge Deller <deller@gmx.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230417125651.25126-19-tzimmermann@suse.de

--- arch/sparc/include/asm/fb.h
+++ arch/sparc/include/asm/fb.h
@@ -2,20 +2,20 @@
 #ifndef _SPARC_FB_H_
 #define _SPARC_FB_H_
 
-#include <linux/fs.h>
-
-#include <asm/page.h>
-
 struct fb_info;
+struct file;
+struct vm_area_struct;
 
+#ifdef CONFIG_SPARC32
 static inline void fb_pgprotect(struct file *file, struct vm_area_struct *vma,
 				unsigned long off)
-{
-#ifdef CONFIG_SPARC64
-	vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
+{ }
+#define fb_pgprotect fb_pgprotect
 #endif
-}
 
 int fb_is_primary_device(struct fb_info *info);
+#define fb_is_primary_device fb_is_primary_device
+
+#include <asm-generic/fb.h>
 
 #endif /* _SPARC_FB_H_ */
