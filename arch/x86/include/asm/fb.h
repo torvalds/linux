91c249b2b9f6a80c744387b6713adf275ffd296b
Thomas Zimmermann <tzimmermann@suse.de>
2023-04-17 14:56:51 +0200

    arch/x86: Implement <asm/fb.h> with generic helpers
    Include <asm-generic/fb.h> and set the required preprocessor tokens
    correctly. x86 now implements its own set of fb helpers, but still
    follows the overall pattern of the other <asm/fb.h> files.
    
    v3:
    	* clarified commit message
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Helge Deller <deller@gmx.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230417125651.25126-20-tzimmermann@suse.de

--- arch/x86/include/asm/fb.h
+++ arch/x86/include/asm/fb.h
@@ -2,9 +2,10 @@
 #ifndef _ASM_X86_FB_H
 #define _ASM_X86_FB_H
 
-#include <linux/fb.h>
-#include <linux/fs.h>
 #include <asm/page.h>
+
+struct fb_info;
+struct file;
 
 static inline void fb_pgprotect(struct file *file, struct vm_area_struct *vma,
 				unsigned long off)
@@ -16,7 +17,11 @@ static inline void fb_pgprotect(struct file *file, struct vm_area_struct *vma,
 		pgprot_val(vma->vm_page_prot) =
 			prot | cachemode2protval(_PAGE_CACHE_MODE_UC_MINUS);
 }
+#define fb_pgprotect fb_pgprotect
 
-extern int fb_is_primary_device(struct fb_info *info);
+int fb_is_primary_device(struct fb_info *info);
+#define fb_is_primary_device fb_is_primary_device
+
+#include <asm-generic/fb.h>
 
 #endif /* _ASM_X86_FB_H */
