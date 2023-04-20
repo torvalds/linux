1023de27cd1d0d692e70fe6d6d5cee9fff9b9c84
Archie Pusaka <apusaka@chromium.org>
2023-04-20 20:23:36 +0800

    Bluetooth: Cancel sync command before suspend and power off
    [ Upstream commit f419863588217f76eaf754e1dfce21ea7fcb026d ]
    
    Some of the sync commands might take a long time to complete, e.g.
    LE Create Connection when the peer device isn't responding might take
    20 seconds before it times out. If suspend command is issued during
    this time, it will need to wait for completion since both commands are
    using the same sync lock.
    
    This patch cancel any running sync commands before attempting to
    suspend or adapter power off.
    
    Signed-off-by: Archie Pusaka <apusaka@chromium.org>
    Reviewed-by: Ying Hsu <yinghsu@chromium.org>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Stable-dep-of: 2615fd9a7c25 ("Bluetooth: hci_sync: Fix overwriting request callback")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -2838,6 +2838,9 @@ int hci_suspend_dev(struct hci_dev *hdev)
 	if (mgmt_powering_down(hdev))
 		return 0;
 
+	/* Cancel potentially blocking sync operation before suspend */
+	__hci_cmd_sync_cancel(hdev, -EHOSTDOWN);
+
 	hci_req_sync_lock(hdev);
 	ret = hci_suspend_sync(hdev);
 	hci_req_sync_unlock(hdev);
--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -1401,6 +1401,10 @@ static int set_powered(struct sock *sk, struct hci_dev *hdev, void *data,
 		goto failed;
 	}
 
+	/* Cancel potentially blocking sync operation before power off */
+	if (cp->val == 0x00)
+		__hci_cmd_sync_cancel(hdev, -EHOSTDOWN);
+
 	err = hci_cmd_sync_queue(hdev, set_powered_sync, cmd,
 				 mgmt_set_powered_complete);
 
