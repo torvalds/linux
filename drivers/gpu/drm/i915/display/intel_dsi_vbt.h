aa0f98c5d1962b4dedec00067fc1b28a6d4f7d65
Hans de Goede <hdegoede@redhat.com>
2023-04-25 21:44:41 +0200

    drm/i915/dsi: Use unconditional msleep() instead of intel_dsi_msleep()
    commit c8c2969bfcba5fcba3a5b078315c1b586d927d9f upstream.
    
    The intel_dsi_msleep() helper skips sleeping if the MIPI-sequences have
    a version of 3 or newer and the panel is in vid-mode.
    
    This is based on the big comment around line 730 which starts with
    "Panel enable/disable sequences from the VBT spec.", where
    the "v3 video mode seq" column does not have any wait t# entries.
    
    Checking the Windows driver shows that it does always honor
    the VBT delays independent of the version of the VBT sequences.
    
    Commit 6fdb335f1c9c ("drm/i915/dsi: Use unconditional msleep for
    the panel_on_delay when there is no reset-deassert MIPI-sequence")
    switched to a direct msleep() instead of intel_dsi_msleep()
    when there is no MIPI_SEQ_DEASSERT_RESET sequence, to fix
    the panel on an Acer Aspire Switch 10 E SW3-016 not turning on.
    
    And now testing on a Nextbook Ares 8A shows that panel_on_delay
    must always be honored otherwise the panel will not turn on.
    
    Instead of only always using regular msleep() for panel_on_delay
    do as Windows does and always use regular msleep() everywhere
    were intel_dsi_msleep() is used and drop the intel_dsi_msleep()
    helper.
    
    Changes in v2:
    - Replace all intel_dsi_msleep() calls instead of just
      the intel_dsi_msleep(panel_on_delay) call
    
    Cc: stable@vger.kernel.org
    Fixes: 6fdb335f1c9c ("drm/i915/dsi: Use unconditional msleep for the panel_on_delay when there is no reset-deassert MIPI-sequence")
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230425194441.68086-1-hdegoede@redhat.com
    (cherry picked from commit fa83c12132f71302f7d4b02758dc0d46048d3f5f)
    Signed-off-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/i915/display/intel_dsi_vbt.h
+++ drivers/gpu/drm/i915/display/intel_dsi_vbt.h
@@ -16,7 +16,6 @@ void intel_dsi_vbt_gpio_init(struct intel_dsi *intel_dsi, bool panel_is_on);
 void intel_dsi_vbt_gpio_cleanup(struct intel_dsi *intel_dsi);
 void intel_dsi_vbt_exec_sequence(struct intel_dsi *intel_dsi,
 				 enum mipi_seq seq_id);
-void intel_dsi_msleep(struct intel_dsi *intel_dsi, int msec);
 void intel_dsi_log_params(struct intel_dsi *intel_dsi);
 
 #endif /* __INTEL_DSI_VBT_H__ */
