11fbe6cd41210c7b5173257158a22e11e225622d
Thomas Gleixner <tglx@linutronix.de>
2023-04-25 20:49:08 +0200

    posix-timers: Remove pointless irqsafe from hash_lock
    All usage of hash_lock is in thread context. No point in using
    spin_lock_irqsave()/irqrestore() for a single usage site.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Frederic Weisbecker <frederic@kernel.org>
    Link: https://lore.kernel.org/r/20230425183313.249063953@linutronix.de

--- kernel/time/posix-timers.c
+++ kernel/time/posix-timers.c
@@ -471,10 +471,9 @@ static void k_itimer_rcu_free(struct rcu_head *head)
 static void release_posix_timer(struct k_itimer *tmr, int it_id_set)
 {
 	if (it_id_set) {
-		unsigned long flags;
-		spin_lock_irqsave(&hash_lock, flags);
+		spin_lock(&hash_lock, flags);
 		hlist_del_rcu(&tmr->t_hash);
-		spin_unlock_irqrestore(&hash_lock, flags);
+		spin_unlock(&hash_lock, flags);
 	}
 	put_pid(tmr->it_pid);
 	sigqueue_free(tmr->sigq);
