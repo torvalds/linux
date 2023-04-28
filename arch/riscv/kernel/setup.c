17509e73ac8b08b677b6d704b7a7d7e822dce20f
Alexandre Ghiti <alexghiti@rivosinc.com>
2023-04-28 12:29:27 +0200

    riscv: Do not set initial_boot_params to the linear address of the dtb
    commit f1581626071c8e37c58c5e8f0b4126b17172a211 upstream.
    
    early_init_dt_verify() is already called in parse_dtb() and since the dtb
    address does not change anymore (it is now in the fixmap region), no need
    to reset initial_boot_params by calling early_init_dt_verify() again.
    
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Link: https://lore.kernel.org/r/20230329081932.79831-3-alexghiti@rivosinc.com
    Cc: stable@vger.kernel.org # 6.1.x
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/riscv/kernel/setup.c
+++ arch/riscv/kernel/setup.c
@@ -278,10 +278,7 @@ void __init setup_arch(char **cmdline_p)
 #if IS_ENABLED(CONFIG_BUILTIN_DTB)
 	unflatten_and_copy_device_tree();
 #else
-	if (early_init_dt_verify(__va(XIP_FIXUP(dtb_early_pa))))
-		unflatten_device_tree();
-	else
-		pr_err("No DTB found in kernel mappings\n");
+	unflatten_device_tree();
 #endif
 	misc_mem_init();
 
