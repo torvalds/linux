57bb11ee2612ecc71073db375d67b64d23f0d3a8
Veera Vegivada <quic_vvegivad@quicinc.com>
2023-05-03 12:22:18 +0530

    dt-bindings: clock: Add clock-ids for SM6150
    Add the clock-ids for all clock controller drivers for
    clients to be able to request for the SM6150 clocks.
    
    Change-Id: Iab46d027300127185bc7035e8afff35fd97c2e09
    Signed-off-by: Veera Vegivada <quic_vvegivad@quicinc.com>

---
+++ include/dt-bindings/clock/qcom,videocc-sm6150.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_VIDEO_CC_SM6150_H
+#define _DT_BINDINGS_CLK_QCOM_VIDEO_CC_SM6150_H
+
+/* VIDEO_CC clocks */
+#define VIDEO_PLL0						0
+#define VIDEO_CC_SLEEP_CLK					1
+#define VIDEO_CC_SLEEP_CLK_SRC					2
+#define VIDEO_CC_VCODEC0_AXI_CLK				3
+#define VIDEO_CC_VCODEC0_CORE_CLK				4
+#define VIDEO_CC_VENUS_AHB_CLK					5
+#define VIDEO_CC_VENUS_CLK_SRC					6
+#define VIDEO_CC_VENUS_CTL_AXI_CLK				7
+#define VIDEO_CC_VENUS_CTL_CORE_CLK				8
+#define VIDEO_CC_XO_CLK						9
+
+#endif
