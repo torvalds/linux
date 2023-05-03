2f6386623f817c07e173987747e1cb34fc409ca3
Billy Tsai <billy_tsai@aspeedtech.com>
2023-05-03 19:02:50 +0800

    drivers: jtag: Add debug message
    Adding debug messages at the common API layer can be a useful tool for
    analyzing application behavior during debugging
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: Ideffd340405de2101b83980b02a663bd3ee7baaf

--- drivers/jtag/jtag.c
+++ drivers/jtag/jtag.c
@@ -14,6 +14,11 @@
 #include <linux/spinlock.h>
 #include <uapi/linux/jtag.h>
 
+static char *end_status_str[] = { "tlr",   "idle",   "selDR", "capDR", "sDR",
+				  "ex1DR", "pDR",    "ex2DR", "updDR", "selIR",
+				  "capIR", "sIR",    "ex1IR", "pIR",   "ex2IR",
+				  "updIR", "current" };
+
 struct jtag {
 	struct miscdevice miscdev;
 	const struct jtag_ops *ops;
@@ -53,6 +58,8 @@ static long jtag_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 		err = jtag->ops->freq_get(jtag, &value);
 		if (err)
 			break;
+		dev_dbg(jtag->miscdev.parent, "JTAG_GIOCFREQ: freq get = %d",
+			value);
 
 		if (put_user(value, (__u32 __user *)arg))
 			err = -EFAULT;
@@ -68,6 +75,8 @@ static long jtag_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 			return -EINVAL;
 
 		err = jtag->ops->freq_set(jtag, value);
+		dev_dbg(jtag->miscdev.parent, "JTAG_SIOCFREQ: freq set = %d",
+			value);
 		break;
 
 	case JTAG_SIOCSTATE:
@@ -84,6 +93,12 @@ static long jtag_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 		if (tapstate.reset > JTAG_FORCE_RESET)
 			return -EINVAL;
 
+		dev_dbg(jtag->miscdev.parent,
+			"JTAG_SIOCSTATE: status set from %s to %s reset %d tck %d",
+			end_status_str[tapstate.from],
+			end_status_str[tapstate.endstate], tapstate.reset,
+			tapstate.tck);
+
 		err = jtag->ops->status_set(jtag, &tapstate);
 		break;
 
@@ -91,6 +106,7 @@ static long jtag_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	{
 		u8 ubit_mask = GENMASK(7, 0);
 		u8 remaining_bits = 0x0;
+		union pad_config padding;
 
 		if (copy_from_user(&xfer, (const void __user *)arg,
 				   sizeof(struct jtag_xfer)))
@@ -123,12 +139,25 @@ static long jtag_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 
 		if (IS_ERR(xfer_data))
 			return -EFAULT;
+		padding.int_value = xfer.padding;
+		dev_dbg(jtag->miscdev.parent,
+			"JTAG_IOCXFER: type: %s direction: %d, END : %s, padding: (value: %d) pre_pad: %d post_pad: %d, len: %d\n",
+			xfer.type ? "DR" : "IR", xfer.direction,
+			end_status_str[xfer.endstate], padding.pad_data,
+			padding.pre_pad_number, padding.post_pad_number,
+			xfer.length);
+
+		print_hex_dump_debug("I:", DUMP_PREFIX_NONE, 16, 1, xfer_data,
+				     data_size, false);
 
 		err = jtag->ops->xfer(jtag, &xfer, xfer_data);
 		if (err) {
 			kfree(xfer_data);
 			return err;
 		}
+
+		print_hex_dump_debug("O:", DUMP_PREFIX_NONE, 16, 1, xfer_data,
+				     data_size, false);
 
 		/* Restore unused remaining bits in this transfer */
 		xfer_data[data_size - 1] = (xfer_data[data_size - 1]
@@ -150,6 +179,8 @@ static long jtag_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 		err = jtag->ops->status_get(jtag, &value);
 		if (err)
 			break;
+		dev_dbg(jtag->miscdev.parent, "JTAG_GIOCSTATUS: status get %s",
+			end_status_str[value]);
 
 		err = put_user(value, (__u32 __user *)arg);
 		break;
@@ -186,6 +217,9 @@ static long jtag_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 				   sizeof(struct jtag_mode)))
 			return -EFAULT;
 
+		dev_dbg(jtag->miscdev.parent,
+			"JTAG_SIOCMODE: mode set feature %d mode %d",
+			mode.feature, mode.mode);
 		err = jtag->ops->mode_set(jtag, &mode);
 		break;
 
