0199849acd07d07e2a8e42757653ca8b14a122f5
Luis Chamberlain <mcgrof@kernel.org>
2023-05-02 18:30:04 -0700

    sysctl: remove register_sysctl_paths()
    The deprecation for register_sysctl_paths() is over. We can rejoice as
    we nuke register_sysctl_paths(). The routine register_sysctl_table()
    was the only user left of register_sysctl_paths(), so we can now just
    open code and move the implementation over to what used to be
    to __register_sysctl_paths().
    
    The old dynamic struct ctl_table_set *set is now the point to
    sysctl_table_root.default_set.
    
    The old dynamic const struct ctl_path *path was being used in the
    routine register_sysctl_paths() with a static:
    
    static const struct ctl_path null_path[] = { {} };
    
    Since this is a null path we can now just simplfy the old routine
    and remove its use as its always empty.
    
    This saves us a total of 230 bytes.
    
    $ ./scripts/bloat-o-meter vmlinux.old vmlinux
    add/remove: 2/7 grow/shrink: 1/1 up/down: 1015/-1245 (-230)
    Function                                     old     new   delta
    register_leaf_sysctl_tables.constprop          -     524    +524
    register_sysctl_table                         22     497    +475
    __pfx_register_leaf_sysctl_tables.constprop       -      16     +16
    null_path                                      8       -      -8
    __pfx_register_sysctl_paths                   16       -     -16
    __pfx_register_leaf_sysctl_tables             16       -     -16
    __pfx___register_sysctl_paths                 16       -     -16
    __register_sysctl_base                        29      12     -17
    register_sysctl_paths                         18       -     -18
    register_leaf_sysctl_tables                  534       -    -534
    __register_sysctl_paths                      620       -    -620
    Total: Before=21259666, After=21259436, chg -0.00%
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- include/linux/sysctl.h
+++ include/linux/sysctl.h
@@ -221,14 +221,8 @@ extern void retire_sysctl_set(struct ctl_table_set *set);
 struct ctl_table_header *__register_sysctl_table(
 	struct ctl_table_set *set,
 	const char *path, struct ctl_table *table);
-struct ctl_table_header *__register_sysctl_paths(
-	struct ctl_table_set *set,
-	const struct ctl_path *path, struct ctl_table *table);
 struct ctl_table_header *register_sysctl(const char *path, struct ctl_table *table);
 struct ctl_table_header *register_sysctl_table(struct ctl_table * table);
-struct ctl_table_header *register_sysctl_paths(const struct ctl_path *path,
-						struct ctl_table *table);
-
 void unregister_sysctl_table(struct ctl_table_header * table);
 
 extern int sysctl_init_bases(void);
@@ -273,12 +267,6 @@ static inline void register_sysctl_init(const char *path, struct ctl_table *tabl
 }
 
 static inline struct ctl_table_header *register_sysctl_mount_point(const char *path)
-{
-	return NULL;
-}
-
-static inline struct ctl_table_header *register_sysctl_paths(
-			const struct ctl_path *path, struct ctl_table *table)
 {
 	return NULL;
 }
