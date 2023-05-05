dcba8da50b05f36437ff5ee8e37a32bdecb4de8a
Maxime Ripard <maxime@cerno.tech>
2023-05-05 13:25:23 +0200

    clk: vc5: mux: Add a determine_rate hook
    The Versaclock5 mux clock implements a mux with a set_parent hook, but
    doesn't provide a determine_rate implementation.
    
    This is a bit odd, since set_parent() is there to, as its name implies,
    change the parent of a clock. However, the most likely candidates to
    trigger that parent change are either the assigned-clock-parents device
    tree property or a call to clk_set_rate(), with determine_rate()
    figuring out which parent is the best suited for a given rate.
    
    The other trigger would be a call to clk_set_parent(), but it's far less
    used, and it doesn't look like there's any obvious user for that clock.
    
    Similarly, it doesn't look like the device tree using that clock driver
    uses any of the assigned-clock properties on that clock.
    
    So, the set_parent hook is effectively unused, possibly because of an
    oversight. However, it could also be an explicit decision by the
    original author to avoid any reparenting but through an explicit call to
    clk_set_parent().
    
    The latter case would be equivalent to setting the determine_rate
    implementation to clk_hw_determine_rate_no_reparent(). Indeed, if no
    determine_rate implementation is provided, clk_round_rate() (through
    clk_core_round_rate_nolock()) will call itself on the parent if
    CLK_SET_RATE_PARENT is set, and will not change the clock rate
    otherwise.
    
    And if it was an oversight, then we are at least explicit about our
    behavior now and it can be further refined down the line.
    
    Cc: Luca Ceresoli <luca.ceresoli@bootlin.com>
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>
    Link: https://lore.kernel.org/r/20221018-clk-range-checks-fixes-v4-21-971d5077e7d2@cerno.tech
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/clk-versaclock5.c
+++ drivers/clk/clk-versaclock5.c
@@ -281,6 +281,7 @@ static int vc5_mux_set_parent(struct clk_hw *hw, u8 index)
 }
 
 static const struct clk_ops vc5_mux_ops = {
+	.determine_rate	= clk_hw_determine_rate_no_reparent,
 	.set_parent	= vc5_mux_set_parent,
 	.get_parent	= vc5_mux_get_parent,
 };
