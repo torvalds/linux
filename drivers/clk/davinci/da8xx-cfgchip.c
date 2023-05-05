de9271f24efc30db84ff514387bba897274291b2
Maxime Ripard <maxime@cerno.tech>
2023-05-05 13:25:26 +0200

    clk: davinci: da8xx-cfgchip: Add a determine_rate hook
    The Davinci DA8xxx cfgchip mux clock implements a mux with a set_parent
    hook, but doesn't provide a determine_rate implementation.
    
    This is a bit odd, since set_parent() is there to, as its name implies,
    change the parent of a clock. However, the most likely candidates to
    trigger that parent change are either the assigned-clock-parents device
    tree property or a call to clk_set_rate(), with determine_rate()
    figuring out which parent is the best suited for a given rate.
    
    The other trigger would be a call to clk_set_parent(), but it's far less
    used, and it doesn't look like there's any obvious user for that clock.
    
    However, the upstream device trees seem to use assigned-clock-parents on
    that clock to force the parent at boot time, so it's likely that the
    author intent was to force the parent through the device tree and
    prevent any reparenting but through an explicit call to
    clk_set_parent().
    
    This case would be equivalent to setting the determine_rate
    implementation to clk_hw_determine_rate_no_reparent(). Indeed, if no
    determine_rate implementation is provided, clk_round_rate() (through
    clk_core_round_rate_nolock()) will call itself on the parent if
    CLK_SET_RATE_PARENT is set, and will not change the clock rate
    otherwise.
    
    Cc: David Lechner <david@lechnology.com>
    Cc: Sekhar Nori <nsekhar@ti.com>
    Acked-by: David Lechner <david@lechnology.com>
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>
    Link: https://lore.kernel.org/r/20221018-clk-range-checks-fixes-v4-24-971d5077e7d2@cerno.tech
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/davinci/da8xx-cfgchip.c
+++ drivers/clk/davinci/da8xx-cfgchip.c
@@ -229,6 +229,7 @@ static u8 da8xx_cfgchip_mux_clk_get_parent(struct clk_hw *hw)
 }
 
 static const struct clk_ops da8xx_cfgchip_mux_clk_ops = {
+	.determine_rate	= clk_hw_determine_rate_no_reparent,
 	.set_parent	= da8xx_cfgchip_mux_clk_set_parent,
 	.get_parent	= da8xx_cfgchip_mux_clk_get_parent,
 };
