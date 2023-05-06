1ce33b6c846fbe0439eeee477b767de4bc3ad35f
Daniel Rosenberg <drosen@google.com>
2023-05-05 18:31:31 -0700

    selftests/bpf: Test allowing NULL buffer in dynptr slice
    bpf_dynptr_slice(_rw) no longer requires a buffer for verification. If the
    buffer is needed, but not present, the function will return NULL.
    
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Link: https://lore.kernel.org/r/20230506013134.2492210-3-drosen@google.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/testing/selftests/bpf/progs/dynptr_success.c
+++ tools/testing/selftests/bpf/progs/dynptr_success.c
@@ -505,3 +505,20 @@ int test_dynptr_clone(struct __sk_buff *skb)
 
 	return 0;
 }
+
+SEC("?cgroup_skb/egress")
+int test_dynptr_skb_no_buff(struct __sk_buff *skb)
+{
+	struct bpf_dynptr ptr;
+	__u64 *data;
+
+	if (bpf_dynptr_from_skb(skb, 0, &ptr)) {
+		err = 1;
+		return 1;
+	}
+
+	/* This may return NULL. SKB may require a buffer */
+	data = bpf_dynptr_slice(&ptr, 0, NULL, 1);
+
+	return !!data;
+}
