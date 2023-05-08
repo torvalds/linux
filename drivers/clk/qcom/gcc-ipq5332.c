a30e62bf6bf4d3230fa9164c7e174e32b9be7ba5
Kathiravan T <quic_kathirav@quicinc.com>
2023-05-08 22:01:45 +0530

    clk: qcom: gcc: ipq5332: Use floor ops for SDCC clocks
    SDCC clocks must be rounded down to avoid overclocking the controller.
    
    Fixes: 3d89d52970fd ("clk: qcom: add Global Clock controller (GCC) driver for IPQ5332 SoC")
    Signed-off-by: Kathiravan T <quic_kathirav@quicinc.com>
    Reviewed-by: Konrad Dybcio <konrad.dybcio@linaro.org>
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    Link: https://lore.kernel.org/r/20230508163145.9678-1-quic_kathirav@quicinc.com

--- drivers/clk/qcom/gcc-ipq5332.c
+++ drivers/clk/qcom/gcc-ipq5332.c
@@ -963,7 +963,7 @@ static struct clk_rcg2 gcc_sdcc1_apps_clk_src = {
 		.name = "gcc_sdcc1_apps_clk_src",
 		.parent_data = gcc_parent_data_9,
 		.num_parents = ARRAY_SIZE(gcc_parent_data_9),
-		.ops = &clk_rcg2_ops,
+		.ops = &clk_rcg2_floor_ops,
 	},
 };
 
