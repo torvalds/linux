062896b7fda2bd677ece9a6292105c755d6ea019
Naman Jain <quic_namajain@quicinc.com>
2023-05-09 11:15:00 +0530

    remoteproc: qcom_q6v5_pas: Add support for mpss, cdsp, adsp in Cliffs
    Add configuration structure and compatible string for mpss, adsp,
    cdsp remoteproc subsystems for Cliffs SoC.
    
    Change-Id: I275c04293479246011a6dc872eedf0b3c5ae3773
    Signed-off-by: Naman Jain <quic_namajain@quicinc.com>

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -1593,6 +1593,22 @@ static const struct adsp_data pineapple_adsp_resource = {
 	.ssctl_id = 0x14,
 };
 
+static const struct adsp_data cliffs_adsp_resource = {
+	.crash_reason_smem = 423,
+	.firmware_name = "adsp.mdt",
+	.dtb_firmware_name = "adsp_dtb.mdt",
+	.pas_id = 1,
+	.dtb_pas_id = 0x24,
+	.minidump_id = 5,
+	.uses_elf64 = true,
+	.has_aggre2_clk = false,
+	.auto_boot = false,
+	.ssr_name = "lpass",
+	.sysmon_name = "adsp",
+	.qmp_name = "adsp",
+	.ssctl_id = 0x14,
+};
+
 static const struct adsp_data khaje_adsp_resource = {
 	.crash_reason_smem = 423,
 	.firmware_name = "adsp.mdt",
@@ -1788,6 +1804,23 @@ static const struct adsp_data pineapple_cdsp_resource = {
 	.ssctl_id = 0x17,
 };
 
+static const struct adsp_data cliffs_cdsp_resource = {
+	.crash_reason_smem = 601,
+	.firmware_name = "cdsp.mdt",
+	.dtb_firmware_name = "cdsp_dtb.mdt",
+	.pas_id = 18,
+	.dtb_pas_id = 0x25,
+	.minidump_id = 7,
+	.uses_elf64 = true,
+	.has_aggre2_clk = false,
+	.auto_boot = false,
+	.hyp_assign_mem = true,
+	.ssr_name = "cdsp",
+	.sysmon_name = "cdsp",
+	.qmp_name = "cdsp",
+	.ssctl_id = 0x17,
+};
+
 static const struct adsp_data khaje_cdsp_resource = {
 	.crash_reason_smem = 601,
 	.firmware_name = "cdsp.mdt",
@@ -1893,6 +1926,26 @@ static const struct adsp_data pineapple_mpss_resource = {
 	.has_aggre2_clk = false,
 	.auto_boot = false,
 	.ssr_hyp_assign_mem = true,
+	.ssr_name = "mpss",
+	.sysmon_name = "modem",
+	.qmp_name = "modem",
+	.ssctl_id = 0x12,
+	.dma_phys_below_32b = true,
+	.both_dumps = true,
+};
+
+static const struct adsp_data cliffs_mpss_resource = {
+	.crash_reason_smem = 421,
+	.firmware_name = "modem.mdt",
+	.dtb_firmware_name = "modem_dtb.mdt",
+	.pas_id = 4,
+	.dtb_pas_id = 0x26,
+	.free_after_auth_reset = true,
+	.minidump_id = 3,
+	.uses_elf64 = true,
+	.has_aggre2_clk = false,
+	.auto_boot = false,
+	.hyp_assign_mem = true,
 	.ssr_name = "mpss",
 	.sysmon_name = "modem",
 	.qmp_name = "modem",
@@ -2228,6 +2281,9 @@ static const struct of_device_id adsp_of_match[] = {
 	{ .compatible = "qcom,holi-adsp-pas", .data = &holi_adsp_resource},
 	{ .compatible = "qcom,holi-cdsp-pas", .data = &holi_cdsp_resource},
 	{ .compatible = "qcom,holi-modem-pas", .data = &holi_mpss_resource},
+	{ .compatible = "qcom,cliffs-adsp-pas", .data = &cliffs_adsp_resource},
+	{ .compatible = "qcom,cliffs-modem-pas", .data = &cliffs_mpss_resource},
+	{ .compatible = "qcom,cliffs-cdsp-pas", .data = &cliffs_cdsp_resource},
 	{ },
 };
 MODULE_DEVICE_TABLE(of, adsp_of_match);
