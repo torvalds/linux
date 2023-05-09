af94aad4c9150cca6781ad134c950fb05dff43f9
Kristina Martsenko <kristina.martsenko@arm.com>
2023-05-09 15:22:25 +0100

    KVM: arm64: initialize HCRX_EL2
    ARMv8.7/9.2 adds a new hypervisor configuration register HCRX_EL2.
    Initialize the register to a safe value (all fields 0), to be robust
    against firmware that has not initialized it. This is also needed to
    ensure that the register is reinitialized after a kexec by a future
    kernel.
    
    In addition, move SMPME setup over to the new flags, as it would
    otherwise get overridden. It is safe to set the bit even if SME is not
    (uniformly) supported, as it will write to a RES0 bit (having no
    effect), and SME will be disabled by the cpufeature framework.
    (Similar to how e.g. the API bit is handled in HCR_HOST_NVHE_FLAGS.)
    
    Signed-off-by: Kristina Martsenko <kristina.martsenko@arm.com>
    Acked-by: Marc Zyngier <maz@kernel.org>
    Acked-by: Oliver Upton <oliver.upton@linux.dev>
    Link: https://lore.kernel.org/r/20230509142235.3284028-2-kristina.martsenko@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/el2_setup.h
+++ arch/arm64/include/asm/el2_setup.h
@@ -22,6 +22,15 @@
 	isb
 .endm
 
+.macro __init_el2_hcrx
+	mrs	x0, id_aa64mmfr1_el1
+	ubfx	x0, x0, #ID_AA64MMFR1_EL1_HCX_SHIFT, #4
+	cbz	x0, .Lskip_hcrx_\@
+	mov_q	x0, HCRX_HOST_FLAGS
+	msr_s	SYS_HCRX_EL2, x0
+.Lskip_hcrx_\@:
+.endm
+
 /*
  * Allow Non-secure EL1 and EL0 to access physical timer and counter.
  * This is not necessary for VHE, since the host kernel runs in EL2,
@@ -184,6 +193,7 @@
  */
 .macro init_el2_state
 	__init_el2_sctlr
+	__init_el2_hcrx
 	__init_el2_timers
 	__init_el2_debug
 	__init_el2_lor
@@ -284,14 +294,6 @@
 	cbz     x1, .Lskip_sme_\@
 
 	msr_s	SYS_SMPRIMAP_EL2, xzr		// Make all priorities equal
-
-	mrs	x1, id_aa64mmfr1_el1		// HCRX_EL2 present?
-	ubfx	x1, x1, #ID_AA64MMFR1_EL1_HCX_SHIFT, #4
-	cbz	x1, .Lskip_sme_\@
-
-	mrs_s	x1, SYS_HCRX_EL2
-	orr	x1, x1, #HCRX_EL2_SMPME_MASK	// Enable priority mapping
-	msr_s	SYS_HCRX_EL2, x1
 .Lskip_sme_\@:
 .endm
 
