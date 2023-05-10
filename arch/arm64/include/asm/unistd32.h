946e697c69ffeeefdd84dad90eac307284df46be
Nhat Pham <nphamcs@gmail.com>
2023-05-10 12:58:06 -0700

    cachestat: wire up cachestat for other architectures
    cachestat is previously only wired in for x86 (and architectures using
    the generic unistd.h table):
    
    https://lore.kernel.org/lkml/20230503013608.2431726-1-nphamcs@gmail.com/
    
    This patch wires cachestat in for all the other architectures.
    
    [nphamcs@gmail.com: wire up cachestat for arm64]
      Link: https://lkml.kernel.org/r/20230511092843.3896327-1-nphamcs@gmail.com
    Link: https://lkml.kernel.org/r/20230510195806.2902878-1-nphamcs@gmail.com
    Signed-off-by: Nhat Pham <nphamcs@gmail.com>
    Tested-by: Michael Ellerman <mpe@ellerman.id.au>	[powerpc]
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>	[m68k]
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Heiko Carstens <hca@linux.ibm.com>		[s390]
    Cc: Alexander Gordeev <agordeev@linux.ibm.com>
    Cc: Christian Borntraeger <borntraeger@linux.ibm.com>
    Cc: Christophe Leroy <christophe.leroy@csgroup.eu>
    Cc: Chris Zankel <chris@zankel.net>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Helge Deller <deller@gmx.de>
    Cc: Ivan Kokshaysky <ink@jurassic.park.msu.ru>
    Cc: "James E.J. Bottomley" <James.Bottomley@HansenPartnership.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
    Cc: Matt Turner <mattst88@gmail.com>
    Cc: Max Filippov <jcmvbkbc@gmail.com>
    Cc: Michal Simek <monstr@monstr.eu>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Cc: Richard Henderson <richard.henderson@linaro.org>
    Cc: Rich Felker <dalias@libc.org>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: Sven Schnelle <svens@linux.ibm.com>
    Cc: Thomas Bogendoerfer <tsbogend@alpha.franken.de>
    Cc: Vasily Gorbik <gor@linux.ibm.com>
    Cc: Yoshinori Sato <ysato@users.sourceforge.jp>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -907,6 +907,8 @@ __SYSCALL(__NR_process_mrelease, sys_process_mrelease)
 __SYSCALL(__NR_futex_waitv, sys_futex_waitv)
 #define __NR_set_mempolicy_home_node 450
 __SYSCALL(__NR_set_mempolicy_home_node, sys_set_mempolicy_home_node)
+#define __NR_cachestat 451
+__SYSCALL(__NR_cachestat, sys_cachestat)
 
 /*
  * Please add new compat syscalls above this comment and update
