3d0204d64adbd8fddf02a4b00226e383ef91a1dd
Jyothi Kumar Seerapu <quic_jseerapu@quicinc.com>
2023-05-10 13:00:05 +0530

    i2c: i2c-msm-geni: Update I2C_SCL_COUNTERS configuration settings
    For I2C fast plus mode, which is of support of 1Mhz,
    updated the I2C_SCL_COUNTERS configuration settings
    for better I2C performance.
    Updated t_high to 2, t_low to 8 and t_cycle to 18.
    
    Change-Id: I8886e0db10ebd53e54f662107e89faf8765f560e
    Signed-off-by: Jyothi Kumar Seerapu <quic_jseerapu@quicinc.com>

--- drivers/i2c/busses/i2c-msm-geni.c
+++ drivers/i2c/busses/i2c-msm-geni.c
@@ -244,7 +244,7 @@ struct geni_i2c_clk_fld {
 static struct geni_i2c_clk_fld geni_i2c_clk_map[] = {
 	{KHz(100), 7, 10, 11, 26},
 	{KHz(400), 2,  7, 10, 24},
-	{KHz(1000), 1, 3,  9, 18},
+	{KHz(1000), 1, 2,  8, 18},
 };
 
 static int geni_i2c_clk_map_idx(struct geni_i2c_dev *gi2c)
