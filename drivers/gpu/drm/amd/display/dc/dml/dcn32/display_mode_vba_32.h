7e60ab4eb3e4ba2adac46d737fdbbc5732bebd58
Alvin Lee <alvin.lee2@amd.com>
2023-05-11 15:25:01 -0400

    drm/amd/display: Apply 60us prefetch for DCFCLK <= 300Mhz
    [Description]
    - Previously we wanted to apply extra 60us of prefetch for min DCFCLK
      (200Mhz), but DCFCLK can be calculated to be 201Mhz which underflows
      also without the extra prefetch
    - Instead, apply the the extra 60us prefetch for any DCFCLK freq <=
      300Mhz
    
    Reviewed-by: Nevenko Stupar <nevenko.stupar@amd.com>
    Reviewed-by: Jun Lei <jun.lei@amd.com>
    Acked-by: Tom Chung <chiahsuan.chung@amd.com>
    Signed-off-by: Alvin Lee <alvin.lee2@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dml/dcn32/display_mode_vba_32.h
+++ drivers/gpu/drm/amd/display/dc/dml/dcn32/display_mode_vba_32.h
@@ -53,7 +53,7 @@
 #define BPP_BLENDED_PIPE 0xffffffff
 
 #define MEM_STROBE_FREQ_MHZ 1600
-#define MIN_DCFCLK_FREQ_MHZ 200
+#define DCFCLK_FREQ_EXTRA_PREFETCH_REQ_MHZ 300
 #define MEM_STROBE_MAX_DELIVERY_TIME_US 60.0
 
 struct display_mode_lib;
