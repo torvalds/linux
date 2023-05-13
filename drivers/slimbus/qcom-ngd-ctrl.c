224fd8eff54f7969f5ee3ace6152620267540910
Chandana Kishori Chiluveru <quic_cchiluve@quicinc.com>
2023-05-13 17:27:29 +0530

    slimbus: qcom-ngd-ctrl: Add timeout for wait operation
    In current driver qcom_slim_ngd_up_worker indefinitely waiting for
    ctrl->qmi_up completion object. This is resulting in
    workqueue lockup on Kthread.
    
    Added wait_for_completion_interruptible_timeout to
    allows the thread to wait for a specified timeout period.
    
    Change-Id: Id6cf3df41c923f05e12448f58e244e007c1de99f
    Signed-off-by: Chandana Kishori Chiluveru <quic_cchiluve@quicinc.com>

--- drivers/slimbus/qcom-ngd-ctrl.c
+++ drivers/slimbus/qcom-ngd-ctrl.c
@@ -85,6 +85,7 @@
 
 #define QCOM_SLIM_NGD_AUTOSUSPEND	(MSEC_PER_SEC / 10)
 #define SLIM_RX_MSGQ_TIMEOUT_VAL	0x10000
+#define SLIM_QMI_TIMEOUT_MS		1000
 
 #define SLIM_LA_MGR	0xFF
 #define SLIM_ROOT_FREQ	24576000
@@ -1801,7 +1802,11 @@ static void qcom_slim_ngd_up_worker(struct work_struct *work)
 	ctrl = container_of(work, struct qcom_slim_ngd_ctrl, ngd_up_work);
 
 	/* Make sure qmi service is up before continuing */
-	wait_for_completion_interruptible(&ctrl->qmi_up);
+	if (!wait_for_completion_interruptible_timeout(&ctrl->qmi_up,
+		msecs_to_jiffies(SLIM_QMI_TIMEOUT_MS))) {
+		SLIM_INFO(ctrl, "QMI wait timeout\n");
+		return;
+	}
 
 	mutex_lock(&ctrl->ssr_lock);
 	qcom_slim_ngd_enable(ctrl, true);
