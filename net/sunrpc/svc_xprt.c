cce4ee9c7834f68cf6a221d61d614c2748611c1c
Chuck Lever <chuck.lever@oracle.com>
2023-05-15 09:32:47 -0400

    SUNRPC: Remove dprintk() in svc_handle_xprt()
    When enabled, this dprintk() fires for every incoming RPC, which is
    an enormous amount of log traffic. These days, after the first few
    hundred log messages, the system journald is just going to mute it,
    along with all other NFSD debug output.
    
    Let's rely on trace points for this high-traffic information
    instead.
    
    Reviewed-by: Jeff Layton <jlayton@kernel.org>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- net/sunrpc/svc_xprt.c
+++ net/sunrpc/svc_xprt.c
@@ -843,9 +843,6 @@ static int svc_handle_xprt(struct svc_rqst *rqstp, struct svc_xprt *xprt)
 		svc_xprt_received(xprt);
 	} else if (svc_xprt_reserve_slot(rqstp, xprt)) {
 		/* XPT_DATA|XPT_DEFERRED case: */
-		dprintk("svc: server %p, pool %u, transport %p, inuse=%d\n",
-			rqstp, rqstp->rq_pool->sp_id, xprt,
-			kref_read(&xprt->xpt_ref));
 		rqstp->rq_deferred = svc_deferred_dequeue(xprt);
 		if (rqstp->rq_deferred)
 			len = svc_deferred_recv(rqstp);
