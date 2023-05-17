e1e80e7ff2b0d2781e13663a736e3c30bb31e033
Paolo Abeni <pabeni@redhat.com>
2023-05-17 12:16:14 -0700

    inet: factor out locked section of inet_accept() in a new helper
    [ Upstream commit 711bdd5141d81ab21dbe0a533024d594210d5ba4 ]
    
    No functional changes intended. The new helper will be used
    by the MPTCP protocol in the next patch to avoid duplicating
    a few LoC.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Reviewed-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Mat Martineau <martineau@kernel.org>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: 26afda78cda3 ("net: relax socket state check at accept time.")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/net/inet_common.h
+++ include/net/inet_common.h
@@ -31,6 +31,8 @@ int inet_dgram_connect(struct socket *sock, struct sockaddr *uaddr,
 		       int addr_len, int flags);
 int inet_accept(struct socket *sock, struct socket *newsock, int flags,
 		bool kern);
+void __inet_accept(struct socket *sock, struct socket *newsock,
+		   struct sock *newsk);
 int inet_send_prepare(struct sock *sk);
 int inet_sendmsg(struct socket *sock, struct msghdr *msg, size_t size);
 void inet_splice_eof(struct socket *sock);
