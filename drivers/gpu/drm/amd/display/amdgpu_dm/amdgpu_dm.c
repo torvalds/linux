8789989b476b5f3bb0bf1a63b5223f6e76cfd13d
Hamza Mahfooz <hamza.mahfooz@amd.com>
2023-05-17 14:09:39 -0400

    drm/amd/display: drop unused count variable in create_eml_sink()
    Since, we are only interested in having
    drm_edid_override_connector_update(), update the value of
    connector->edid_blob_ptr. We don't care about the return value of
    drm_edid_override_connector_update() here. So, drop count.
    
    Fixes: 550e5d23f147 ("drm/amd/display: assign edid_blob_ptr with edid from debugfs")
    Reported-by: kernel test robot <lkp@intel.com>
    Reviewed-by: Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>
    Signed-off-by: Hamza Mahfooz <hamza.mahfooz@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -6397,9 +6397,8 @@ static void create_eml_sink(struct amdgpu_dm_connector *aconnector)
 		/* if connector->edid_override valid, pass
 		 * it to edid_override to edid_blob_ptr
 		 */
-		int count;
 
-		count = drm_edid_override_connector_update(&aconnector->base);
+		drm_edid_override_connector_update(&aconnector->base);
 
 		if (!aconnector->base.edid_blob_ptr) {
 			DRM_ERROR("No EDID firmware found on connector: %s ,forcing to OFF!\n",
