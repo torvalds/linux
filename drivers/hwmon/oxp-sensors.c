7d0c2c61b1a4d1cf5641b35b491fe58ffafe26bc
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2023-05-17 15:35:41 -0300

    hwmon: (oxp-sensors) Add new DMI match for OXP Mini
    A newer BIOS version for the OneXPlayer Mini AMD modified vendor and
    board name strings. Add those to the match table and set the same board
    model as older OXP Mini AMD.
    
    Signed-off-by: Joaquín Ignacio Aramendía <samsagax@gmail.com>
    Link: https://lore.kernel.org/r/20230517183540.187383-1-samsagax@gmail.com
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/oxp-sensors.c
+++ drivers/hwmon/oxp-sensors.c
@@ -102,6 +102,13 @@ static const struct dmi_system_id dmi_table[] = {
 	{
 		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "ONE-NETBOOK"),
+			DMI_EXACT_MATCH(DMI_BOARD_NAME, "ONEXPLAYER mini A07"),
+		},
+		.driver_data = (void *)oxp_mini_amd,
+	},
+	{
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "ONE-NETBOOK"),
 			DMI_EXACT_MATCH(DMI_BOARD_NAME, "ONEXPLAYER Mini Pro"),
 		},
 		.driver_data = (void *)oxp_mini_amd_pro,
