70dee7045bdc57da24088cf1ee3f2e6a9e6999f5
Shaoxing Chen <csx@rock-chips.com>
2023-05-18 18:46:00 +0800

    net: stmmac: Add 100Base-T1 and 1000Base-T1 support
    Change-Id: I854394757b30042da0c448451affd616278846ce
    Signed-off-by: Shaoxing Chen <csx@rock-chips.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -847,6 +847,8 @@ static void stmmac_validate(struct phylink_config *config,
 	phylink_set(mac_supported, 1000baseT_Half);
 	phylink_set(mac_supported, 1000baseT_Full);
 	phylink_set(mac_supported, 1000baseKX_Full);
+	phylink_set(mac_supported, 100baseT1_Full);
+	phylink_set(mac_supported, 1000baseT1_Full);
 
 	phylink_set(mac_supported, Autoneg);
 	phylink_set(mac_supported, Pause);
