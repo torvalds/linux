157f7226b080b292f0aaddb42d40017a731c8ddf
Neal Liu <neal_liu@aspeedtech.com>
2023-05-18 17:53:11 +0800

    usb: gadget: aspeed: add dma set mask for 64-bit DMA addressing
    Add dma_set_mask_and_coherent() to set 64-bit DMA addressing
    capability.
    
    Signed-off-by: Neal Liu <neal_liu@aspeedtech.com>
    Change-Id: Iec7cd150fd1e821ad68914daa823a19863569cb8

--- drivers/usb/gadget/udc/aspeed-vhub/core.c
+++ drivers/usb/gadget/udc/aspeed-vhub/core.c
@@ -370,6 +370,13 @@ static int ast_vhub_probe(struct platform_device *pdev)
 		goto err;
 	}
 
+#ifdef CONFIG_MACH_ASPEED_G7
+	rc = dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(64));
+	if (rc) {
+		dev_warn(&pdev->dev, "No suitable DMA available\n");
+		goto err;
+	}
+#endif
 	/*
 	 * Allocate DMA buffers for all EP0s in one chunk,
 	 * one per port and one for the vHub itself
