cc3ff544a296b5b4bb021f4dc415b53a6955b980
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2023-05-18 09:27:42 +0200

    ASoC: codecs: rt1308: do not store status in state container
    Driver in its update status callback stores Soundwire device status in
    state container but it never uses it later.  Simplify the code a bit.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com
    Link: https://lore.kernel.org/r/20230518072753.7361-2-krzysztof.kozlowski@linaro.org
    Signed-off-by: Mark Brown <broonie@kernel.org

--- sound/soc/codecs/rt1308-sdw.c
+++ sound/soc/codecs/rt1308-sdw.c
@@ -304,9 +304,6 @@ static int rt1308_update_status(struct sdw_slave *slave,
 {
 	struct  rt1308_sdw_priv *rt1308 = dev_get_drvdata(&slave->dev);
 
-	/* Update the status */
-	rt1308->status = status;
-
 	if (status == SDW_SLAVE_UNATTACHED)
 		rt1308->hw_init = false;
 
@@ -314,7 +311,7 @@ static int rt1308_update_status(struct sdw_slave *slave,
 	 * Perform initialization only if slave status is present and
 	 * hw_init flag is false
 	 */
-	if (rt1308->hw_init || rt1308->status != SDW_SLAVE_ATTACHED)
+	if (rt1308->hw_init || status != SDW_SLAVE_ATTACHED)
 		return 0;
 
 	/* perform I/O transfers required for Slave initialization */
