ddd8d552a8f8d6e15bcdcdd294fb3ee897d4e221
Alexandre Ghiti <alexghiti@rivosinc.com>
2023-05-19 15:13:11 +0200

    riscv: Fix unused variable warning when BUILTIN_DTB is set
    [ Upstream commit 33d418da6f476b15e4510e0a590062583f63cd36 ]
    
    commit ef69d2559fe9 ("riscv: Move early dtb mapping into the fixmap
    region") wrongly moved the #ifndef CONFIG_BUILTIN_DTB surrounding the pa
    variable definition in create_fdt_early_page_table(), so move it back to
    its right place to quiet the following warning:
    
    ../arch/riscv/mm/init.c: In function ‘create_fdt_early_page_table’:
    ../arch/riscv/mm/init.c:925:12: warning: unused variable ‘pa’ [-Wunused-variable]
      925 |  uintptr_t pa = dtb_pa & ~(PMD_SIZE - 1);
    
    Fixes: ef69d2559fe9 ("riscv: Move early dtb mapping into the fixmap region")
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Reviewed-by: Conor Dooley <conor.dooley@microchip.com>
    Link: https://lore.kernel.org/r/20230519131311.391960-1-alexghiti@rivosinc.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/riscv/mm/init.c
+++ arch/riscv/mm/init.c
@@ -845,9 +845,9 @@ static void __init create_kernel_page_table(pgd_t *pgdir, bool early)
 static void __init create_fdt_early_page_table(uintptr_t fix_fdt_va,
 					       uintptr_t dtb_pa)
 {
+#ifndef CONFIG_BUILTIN_DTB
 	uintptr_t pa = dtb_pa & ~(PMD_SIZE - 1);
 
-#ifndef CONFIG_BUILTIN_DTB
 	/* Make sure the fdt fixmap address is always aligned on PMD size */
 	BUILD_BUG_ON(FIX_FDT % (PMD_SIZE / PAGE_SIZE));
 
