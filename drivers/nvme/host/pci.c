22efb27a21b6ebe6be0760949d877141077de701
Tatsuki Sugiura <sugi@nemui.org>
2023-05-20 21:23:50 +0900

    NVMe: Add MAXIO 1602 to bogus nid list.
    [ Upstream commit a3a9d63dcd15535e7fdf4c7c1b32bfaed762973a ]
    
    HIKSEMI FUTURE M.2 SSD uses the same dummy nguid and eui64.
    I confirmed it with my two devices.
    
    This patch marks the controller as NVME_QUIRK_BOGUS_NID.
    
    ---------------------------------------------------------
    sugi@tempest:~% sudo nvme id-ctrl /dev/nvme0
    NVME Identify Controller:
    vid       : 0x1e4b
    ssvid     : 0x1e4b
    sn        : 30096022612
    mn        : HS-SSD-FUTURE 2048G
    fr        : SN10542
    rab       : 0
    ieee      : 000000
    cmic      : 0
    mdts      : 7
    cntlid    : 0
    ver       : 0x10400
    rtd3r     : 0x7a120
    rtd3e     : 0x1e8480
    oaes      : 0x200
    ctratt    : 0x2
    rrls      : 0
    cntrltype : 1
    fguid     : 00000000-0000-0000-0000-000000000000
    <snip...>
    ---------------------------------------------------------
    
    ---------------------------------------------------------
    sugi@tempest:~% sudo nvme id-ns /dev/nvme0n1
    NVME Identify Namespace 1:
    <snip...>
    nguid   : 00000000000000000000000000000000
    eui64   : 0000000000000002
    lbaf  0 : ms:0   lbads:9  rp:0 (in use)
    ---------------------------------------------------------
    
    Signed-off-by: Tatsuki Sugiura <sugi@nemui.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -3537,6 +3537,8 @@ static const struct pci_device_id nvme_id_table[] = {
 		.driver_data = NVME_QUIRK_BOGUS_NID, },
 	{ PCI_DEVICE(0x1e4B, 0x1202),   /* MAXIO MAP1202 */
 		.driver_data = NVME_QUIRK_BOGUS_NID, },
+	{ PCI_DEVICE(0x1e4B, 0x1602),   /* MAXIO MAP1602 */
+		.driver_data = NVME_QUIRK_BOGUS_NID, },
 	{ PCI_DEVICE(0x1cc1, 0x5350),   /* ADATA XPG GAMMIX S50 */
 		.driver_data = NVME_QUIRK_BOGUS_NID, },
 	{ PCI_DEVICE(0x1dbe, 0x5236),   /* ADATA XPG GAMMIX S70 */
