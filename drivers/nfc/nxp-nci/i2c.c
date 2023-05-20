efc3001f8b44bf5da0f178bd726a73c73f370707
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2023-05-20 19:21:04 +0200

    nfc: Switch i2c drivers back to use .probe()
    After commit b8a1a4cd5a98 ("i2c: Provide a temporary .probe_new()
    call-back type"), all drivers being converted to .probe_new() and then
    03c835f498b5 ("i2c: Switch .probe() to not take an id parameter")
    convert back to (the new) .probe() to be able to eventually drop
    .probe_new() from struct i2c_driver.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Reviewed-by: Luca Ceresoli <luca.ceresoli@bootlin.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/nfc/nxp-nci/i2c.c
+++ drivers/nfc/nxp-nci/i2c.c
@@ -348,7 +348,7 @@ static struct i2c_driver nxp_nci_i2c_driver = {
 		   .acpi_match_table = ACPI_PTR(acpi_id),
 		   .of_match_table = of_nxp_nci_i2c_match,
 		  },
-	.probe_new = nxp_nci_i2c_probe,
+	.probe = nxp_nci_i2c_probe,
 	.id_table = nxp_nci_i2c_id_table,
 	.remove = nxp_nci_i2c_remove,
 };
