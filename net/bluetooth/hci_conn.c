31c5f9164949347c9cb34f041a7e04fdc08b1b85
Pauli Virtanen <pav@iki.fi>
2023-05-21 15:48:28 +0000

    Bluetooth: ISO: consider right CIS when removing CIG at cleanup
    When looking for CIS blocking CIG removal, consider only the CIS with
    the right CIG ID. Don't try to remove CIG with unset CIG ID.
    
    Fixes: 26afbd826ee3 ("Bluetooth: Add initial implementation of CIS connections")
    Signed-off-by: Pauli Virtanen <pav@iki.fi>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- net/bluetooth/hci_conn.c
+++ net/bluetooth/hci_conn.c
@@ -947,8 +947,8 @@ static void find_cis(struct hci_conn *conn, void *data)
 {
 	struct iso_list_data *d = data;
 
-	/* Ignore broadcast */
-	if (!bacmp(&conn->dst, BDADDR_ANY))
+	/* Ignore broadcast or if CIG don't match */
+	if (!bacmp(&conn->dst, BDADDR_ANY) || d->cig != conn->iso_qos.ucast.cig)
 		return;
 
 	d->count++;
@@ -962,6 +962,9 @@ static void cis_cleanup(struct hci_conn *conn)
 {
 	struct hci_dev *hdev = conn->hdev;
 	struct iso_list_data d;
+
+	if (conn->iso_qos.ucast.cig == BT_ISO_QOS_CIG_UNSET)
+		return;
 
 	memset(&d, 0, sizeof(d));
 	d.cig = conn->iso_qos.ucast.cig;
