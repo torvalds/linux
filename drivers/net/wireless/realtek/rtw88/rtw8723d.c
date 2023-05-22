9be20a82232779c59979527dfc8febca3182fee2
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2023-05-22 22:24:23 +0200

    wifi: rtw88: rtw8723d: Implement RTL8723DS (SDIO) efuse parsing
    The efuse of the SDIO RTL8723DS chip has only one known member: the mac
    address is at offset 0x11a. Add a struct rtw8723ds_efuse describing this
    and use it for copying the mac address when the SDIO bus is used.
    
    Reviewed-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://lore.kernel.org/r/20230522202425.1827005-3-martin.blumenstingl@googlemail.com

--- drivers/net/wireless/realtek/rtw88/rtw8723d.c
+++ drivers/net/wireless/realtek/rtw88/rtw8723d.c
@@ -216,6 +216,12 @@ static void rtw8723du_efuse_parsing(struct rtw_efuse *efuse,
 	ether_addr_copy(efuse->addr, map->u.mac_addr);
 }
 
+static void rtw8723ds_efuse_parsing(struct rtw_efuse *efuse,
+				    struct rtw8723d_efuse *map)
+{
+	ether_addr_copy(efuse->addr, map->s.mac_addr);
+}
+
 static int rtw8723d_read_efuse(struct rtw_dev *rtwdev, u8 *log_map)
 {
 	struct rtw_efuse *efuse = &rtwdev->efuse;
@@ -247,6 +253,9 @@ static int rtw8723d_read_efuse(struct rtw_dev *rtwdev, u8 *log_map)
 		break;
 	case RTW_HCI_TYPE_USB:
 		rtw8723du_efuse_parsing(efuse, map);
+		break;
+	case RTW_HCI_TYPE_SDIO:
+		rtw8723ds_efuse_parsing(efuse, map);
 		break;
 	default:
 		/* unsupported now */
