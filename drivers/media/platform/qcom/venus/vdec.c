47f867cb1b6364be6b3d50a333370d6f66a7e085
Dikshita Agarwal <quic_dikshita@quicinc.com>
2023-05-22 07:02:52 +0100

    media: venus: fix EOS handling in decoder stop command
    Use firmware version based check to assign correct
    device address for EOS buffer to fix the EOS handling
    with different firmware version.
    
    Reviewed-by: Konrad Dybcio <konrad.dybcio@linaro.org>
    Tested-by: Nathan Hebert <nhebert@chromium.org>
    Signed-off-by: Vikash Garodia <quic_vgarodia@quicinc.com>
    Signed-off-by: Viswanath Boma <quic_vboma@quicinc.com>
    Signed-off-by: Dikshita Agarwal <quic_dikshita@quicinc.com>
    Signed-off-by: Stanimir Varbanov <stanimir.k.varbanov@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>

--- drivers/media/platform/qcom/venus/vdec.c
+++ drivers/media/platform/qcom/venus/vdec.c
@@ -551,7 +551,7 @@ vdec_decoder_cmd(struct file *file, void *fh, struct v4l2_decoder_cmd *cmd)
 
 		fdata.buffer_type = HFI_BUFFER_INPUT;
 		fdata.flags |= HFI_BUFFERFLAG_EOS;
-		if (IS_V6(inst->core))
+		if (IS_V6(inst->core) && is_fw_rev_or_older(inst->core, 1, 0, 87))
 			fdata.device_addr = 0;
 		else
 			fdata.device_addr = 0xdeadb000;
