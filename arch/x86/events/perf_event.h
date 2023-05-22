a430021faad6b4fa86c820fc3e7f8dbfc2f14fb4
Kan Liang <kan.liang@linux.intel.com>
2023-05-22 04:30:35 -0700

    perf/x86/intel: Add Crestmont PMU
    The Grand Ridge and Sierra Forest are successors to Snow Ridge. They
    both have Crestmont core. From the core PMU's perspective, they are
    similar to the e-core of MTL. The only difference is the LBR event
    logging feature, which will be implemented in the following patches.
    
    Create a non-hybrid PMU setup for Grand Ridge and Sierra Forest.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Link: https://lore.kernel.org/r/20230522113040.2329924-1-kan.liang@linux.intel.com

--- arch/x86/events/perf_event.h
+++ arch/x86/events/perf_event.h
@@ -1606,6 +1606,8 @@ void intel_pmu_pebs_data_source_grt(void);
 
 void intel_pmu_pebs_data_source_mtl(void);
 
+void intel_pmu_pebs_data_source_cmt(void);
+
 int intel_pmu_setup_lbr_filter(struct perf_event *event);
 
 void intel_pt_interrupt(void);
