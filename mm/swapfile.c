b53e24c4f6bcc5f38b79c1c344e643bd54b69964
Peter Collingbourne <pcc@google.com>
2023-05-22 17:43:09 -0700

    mm: call arch_swap_restore() from unuse_pte()
    We would like to move away from requiring architectures to restore
    metadata from swap in the set_pte_at() implementation, as this is not only
    error-prone but adds complexity to the arch-specific code.  This requires
    us to call arch_swap_restore() before calling swap_free() whenever pages
    are restored from swap.  We are currently doing so everywhere except in
    unuse_pte(); do so there as well.
    
    Link: https://lkml.kernel.org/r/20230523004312.1807357-3-pcc@google.com
    Link: https://linux-review.googlesource.com/id/I68276653e612d64cde271ce1b5a99ae05d6bbc4f
    Signed-off-by: Peter Collingbourne <pcc@google.com>
    Suggested-by: David Hildenbrand <david@redhat.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Acked-by: "Huang, Ying" <ying.huang@intel.com>
    Reviewed-by: Steven Price <steven.price@arm.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Alexandru Elisei <alexandru.elisei@arm.com>
    Cc: Chinwen Chang <chinwen.chang@mediatek.com>
    Cc: Evgenii Stepanov <eugenis@google.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: kasan-dev <kasan-dev@googlegroups.com>
    Cc: "Kuan-Ying Lee (李冠穎)" <Kuan-Ying.Lee@mediatek.com>
    Cc: Qun-Wei Lin <qun-wei.lin@mediatek.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -1778,6 +1778,13 @@ static int unuse_pte(struct vm_area_struct *vma, pmd_t *pmd,
 		goto setpte;
 	}
 
+	/*
+	 * Some architectures may have to restore extra metadata to the page
+	 * when reading from swap. This metadata may be indexed by swap entry
+	 * so this must be called before swap_free().
+	 */
+	arch_swap_restore(entry, page_folio(page));
+
 	/* See do_swap_page() */
 	BUG_ON(!PageAnon(page) && PageMappedToDisk(page));
 	BUG_ON(PageAnon(page) && PageAnonExclusive(page));
