365af746f5ad32f183a9959c9ae2b2338717d3da
Jisheng Zhang <jszhang@kernel.org>
2023-05-24 00:59:42 +0800

    BACKPORT: riscv: mm: try VMA lock-based page fault handling first
    Attempt VMA lock-based page fault handling first, and fall back to the
    existing mmap_lock-based handling if that fails.
    
    A simple running the ebizzy benchmark on Lichee Pi 4A shows that
    PER_VMA_LOCK can improve the ebizzy benchmark by about 32.68%. In
    theory, the more CPUs, the bigger improvement, but I don't have any
    HW platform which has more than 4 CPUs.
    
    This is the riscv variant of "x86/mm: try VMA lock-based page fault
    handling first".
    
    Signed-off-by: Jisheng Zhang <jszhang@kernel.org>
    Reviewed-by: Guo Ren <guoren@kernel.org>
    Reviewed-by: Suren Baghdasaryan <surenb@google.com>
    Link: https://lore.kernel.org/r/20230523165942.2630-1-jszhang@kernel.org
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    
    (cherry picked from commit 648321fa0d970c04b4327ac1a053abf43d285931)
    
    Bug: 293665307
    Change-Id: I59b63add96645d2483f87c2b680d4a7afa86f7b6
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- arch/riscv/mm/fault.c
+++ arch/riscv/mm/fault.c
@@ -289,6 +289,36 @@ asmlinkage void do_page_fault(struct pt_regs *regs)
 		flags |= FAULT_FLAG_WRITE;
 	else if (cause == EXC_INST_PAGE_FAULT)
 		flags |= FAULT_FLAG_INSTRUCTION;
+#ifdef CONFIG_PER_VMA_LOCK
+	if (!(flags & FAULT_FLAG_USER))
+		goto lock_mmap;
+
+	vma = lock_vma_under_rcu(mm, addr);
+	if (!vma)
+		goto lock_mmap;
+
+	if (unlikely(access_error(cause, vma))) {
+		vma_end_read(vma);
+		goto lock_mmap;
+	}
+
+	fault = handle_mm_fault(vma, addr, flags | FAULT_FLAG_VMA_LOCK, regs);
+	vma_end_read(vma);
+
+	if (!(fault & VM_FAULT_RETRY)) {
+		count_vm_vma_lock_event(VMA_LOCK_SUCCESS);
+		goto done;
+	}
+	count_vm_vma_lock_event(VMA_LOCK_RETRY);
+
+	if (fault_signal_pending(fault, regs)) {
+		if (!user_mode(regs))
+			no_context(regs, addr);
+		return;
+	}
+lock_mmap:
+#endif /* CONFIG_PER_VMA_LOCK */
+
 retry:
 	vma = lock_mm_and_find_vma(mm, addr, regs);
 	if (unlikely(!vma)) {
@@ -341,6 +371,9 @@ retry:
 
 	mmap_read_unlock(mm);
 
+#ifdef CONFIG_PER_VMA_LOCK
+done:
+#endif
 	if (unlikely(fault & VM_FAULT_ERROR)) {
 		tsk->thread.bad_cause = cause;
 		mm_fault_error(regs, addr, fault);
