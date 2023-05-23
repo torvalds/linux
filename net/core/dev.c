c857946a4e262e0f798fe7625fadf85bf1190fc4
Kurt Kanzenbach <kurt@linutronix.de>
2023-05-23 13:15:18 +0200

    net/core: Enable socket busy polling on -RT
    Busy polling is currently not allowed on PREEMPT_RT, because it disables
    preemption while invoking the NAPI callback. It is not possible to acquire
    sleeping locks with disabled preemption. For details see commit
    20ab39d13e2e ("net/core: disable NET_RX_BUSY_POLL on PREEMPT_RT").
    
    However, strict cyclic and/or low latency network applications may prefer busy
    polling e.g., using AF_XDP instead of interrupt driven communication.
    
    The preempt_disable() is used in order to prevent the poll_owner and NAPI owner
    to be preempted while owning the resource to ensure progress. Netpoll performs
    busy polling in order to acquire the lock. NAPI is locked by setting the
    NAPIF_STATE_SCHED flag. There is no busy polling if the flag is set and the
    "owner" is preempted. Worst case is that the task owning NAPI gets preempted and
    NAPI processing stalls.  This is can be prevented by properly prioritising the
    tasks within the system.
    
    Allow RX_BUSY_POLL on PREEMPT_RT if NETPOLL is disabled. Don't disable
    preemption on PREEMPT_RT within the busy poll loop.
    
    Tested on x86 hardware with v6.1-RT and v6.3-RT on Intel i225 (igc) with
    AF_XDP/ZC sockets configured to run in busy polling mode.
    
    Suggested-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Kurt Kanzenbach <kurt@linutronix.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -6197,7 +6197,8 @@ restart:
 	if (!napi)
 		goto out;
 
-	preempt_disable();
+	if (!IS_ENABLED(CONFIG_PREEMPT_RT))
+		preempt_disable();
 	for (;;) {
 		int work = 0;
 
@@ -6239,7 +6240,8 @@ count:
 		if (unlikely(need_resched())) {
 			if (napi_poll)
 				busy_poll_stop(napi, have_poll_lock, prefer_busy_poll, budget);
-			preempt_enable();
+			if (!IS_ENABLED(CONFIG_PREEMPT_RT))
+				preempt_enable();
 			rcu_read_unlock();
 			cond_resched();
 			if (loop_end(loop_end_arg, start_time))
@@ -6250,7 +6252,8 @@ count:
 	}
 	if (napi_poll)
 		busy_poll_stop(napi, have_poll_lock, prefer_busy_poll, budget);
-	preempt_enable();
+	if (!IS_ENABLED(CONFIG_PREEMPT_RT))
+		preempt_enable();
 out:
 	rcu_read_unlock();
 }
