02f1b0b736606f9870595b3089d9c124f9da8be9
Chao Gao <chao.gao@intel.com>
2023-05-24 14:16:32 +0800

    KVM: x86: Correct the name for skipping VMENTER l1d flush
    There is no VMENTER_L1D_FLUSH_NESTED_VM. It should be
    ARCH_CAP_SKIP_VMENTRY_L1DFLUSH.
    
    Signed-off-by: Chao Gao <chao.gao@intel.com>
    Reviewed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Link: https://lore.kernel.org/r/20230524061634.54141-3-chao.gao@intel.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -1631,7 +1631,7 @@ static u64 kvm_get_arch_capabilities(void)
 	 * If we're doing cache flushes (either "always" or "cond")
 	 * we will do one whenever the guest does a vmlaunch/vmresume.
 	 * If an outer hypervisor is doing the cache flush for us
-	 * (VMENTER_L1D_FLUSH_NESTED_VM), we can safely pass that
+	 * (ARCH_CAP_SKIP_VMENTRY_L1DFLUSH), we can safely pass that
 	 * capability to the guest too, and if EPT is disabled we're not
 	 * vulnerable.  Overall, only VMENTER_L1D_FLUSH_NEVER will
 	 * require a nested hypervisor to do a flush of its own.
