cf650342f83ae655c6d05a1a74ae1672459973d0
Baolin Wang <baolin.wang@linux.alibaba.com>
2023-05-25 20:53:58 +0800

    mm: compaction: skip more fully scanned pageblock
    In fast_isolate_around(), it assumes the pageblock is fully scanned if
    cc->nr_freepages < cc->nr_migratepages after trying to isolate some free
    pages, and will set skip flag to avoid scanning in future.  However this
    can miss setting the skip flag for a fully scanned pageblock (returned
    'start_pfn' is equal to 'end_pfn') in the case where cc->nr_freepages is
    larger than cc->nr_migratepages.
    
    So using the returned 'start_pfn' from isolate_freepages_block() and
    'end_pfn' to decide if a pageblock is fully scanned makes more sense.  It
    can also cover the case where cc->nr_freepages < cc->nr_migratepages,
    which means the 'start_pfn' is usually equal to 'end_pfn' except some
    uncommon fatal error occurs after non-strict mode isolation.
    
    Link: https://lkml.kernel.org/r/f4efd2fa08735794a6d809da3249b6715ba6ad38.1685018752.git.baolin.wang@linux.alibaba.com
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/compaction.c
+++ mm/compaction.c
@@ -1389,7 +1389,7 @@ fast_isolate_around(struct compact_control *cc, unsigned long pfn)
 	isolate_freepages_block(cc, &start_pfn, end_pfn, &cc->freepages, 1, false);
 
 	/* Skip this pageblock in the future as it's full or nearly full */
-	if (cc->nr_freepages < cc->nr_migratepages)
+	if (start_pfn == end_pfn)
 		set_pageblock_skip(page);
 
 	return;
