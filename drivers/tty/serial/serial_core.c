51e45fba14bf08b66bca764a083c7f2e2ff62f01
John Ogness <john.ogness@linutronix.de>
2023-05-25 11:37:55 +0206

    serial: core: lock port for start_rx() in uart_resume_port()
    The only user of the start_rx() callback (qcom_geni) directly calls
    its own stop_rx() callback. Since stop_rx() requires that the
    port->lock is taken and interrupts are disabled, the start_rx()
    callback has the same requirement.
    
    Fixes: cfab87c2c271 ("serial: core: Introduce callback for start_rx and do stop_rx in suspend only if this callback implementation is present.")
    Signed-off-by: John Ogness <john.ogness@linutronix.de>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Link: https://lore.kernel.org/r/20230525093159.223817-5-john.ogness@linutronix.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/serial_core.c
+++ drivers/tty/serial/serial_core.c
@@ -2430,8 +2430,11 @@ int uart_resume_port(struct uart_driver *drv, struct uart_port *uport)
 		if (console_suspend_enabled)
 			uart_change_pm(state, UART_PM_STATE_ON);
 		uport->ops->set_termios(uport, &termios, NULL);
-		if (!console_suspend_enabled && uport->ops->start_rx)
+		if (!console_suspend_enabled && uport->ops->start_rx) {
+			spin_lock_irq(&uport->lock);
 			uport->ops->start_rx(uport);
+			spin_unlock_irq(&uport->lock);
+		}
 		if (console_suspend_enabled)
 			console_start(uport->cons);
 	}
