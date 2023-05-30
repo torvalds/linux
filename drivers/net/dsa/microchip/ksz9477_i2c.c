3ea903e2a523e9655e425ba8eae13733e9e80ac9
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2023-05-30 08:39:36 +0200

    net: dsa: Switch i2c drivers back to use .probe()
    After commit b8a1a4cd5a98 ("i2c: Provide a temporary .probe_new()
    call-back type"), all drivers being converted to .probe_new() and then
    03c835f498b5 ("i2c: Switch .probe() to not take an id parameter") convert
    back to (the new) .probe() to be able to eventually drop .probe_new() from
    struct i2c_driver.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/microchip/ksz9477_i2c.c
+++ drivers/net/dsa/microchip/ksz9477_i2c.c
@@ -119,7 +119,7 @@ static struct i2c_driver ksz9477_i2c_driver = {
 		.name	= "ksz9477-switch",
 		.of_match_table = ksz9477_dt_ids,
 	},
-	.probe_new = ksz9477_i2c_probe,
+	.probe = ksz9477_i2c_probe,
 	.remove	= ksz9477_i2c_remove,
 	.shutdown = ksz9477_i2c_shutdown,
 	.id_table = ksz9477_i2c_id,
