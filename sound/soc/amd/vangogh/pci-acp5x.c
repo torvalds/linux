e89f45edb747ed88e97a5771dd6d3dd1eb517873
Venkata Prasad Potturu <venkataprasad.potturu@amd.com>
2023-05-30 16:37:58 +0530

    ASoC: amd: vangogh: Add check for acp config flags in vangogh platform
    We have SOF and generic ACP support enabled for Vangogh platform
    on some machines. Since we have same PCI id used for probing,
    add check for machine configuration flag to avoid conflict with
    newer pci drivers. Such machine flag has been initialized via
    dmi match on few Vangogh based machines. If no flag is
    specified probe and register older platform device.
    
    Signed-off-by: Venkata Prasad Potturu <venkataprasad.potturu@amd.com>
    Link: https://lore.kernel.org/r/20230530110802.674939-1-venkataprasad.potturu@amd.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/vangogh/pci-acp5x.c
+++ sound/soc/amd/vangogh/pci-acp5x.c
@@ -125,9 +125,14 @@ static int snd_acp5x_probe(struct pci_dev *pci,
 {
 	struct acp5x_dev_data *adata;
 	struct platform_device_info pdevinfo[ACP5x_DEVS];
-	unsigned int irqflags;
+	unsigned int irqflags, flag;
 	int ret, i;
 	u32 addr, val;
+
+	/* Return if acp config flag is defined */
+	flag = snd_amd_acp_find_config(pci);
+	if (flag)
+		return -ENODEV;
 
 	irqflags = IRQF_SHARED;
 	if (pci->revision != 0x50)
