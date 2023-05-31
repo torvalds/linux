41cec40bc9baba83d36a0718ea94bfe63189274a
Wenyou Yang <WenYou.Yang@amd.com>
2023-05-31 10:16:47 +0800

    drm/amd/pm: Vangogh: Add new gpu_metrics_v2_4 to acquire gpu_metrics
    To acquire the voltage and current info from gpu_metrics interface,
    but gpu_metrics_v2_3 doesn't contain them, and to be backward compatible,
    add new gpu_metrics_v2_4 structure.
    
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>
    Acked-by: Evan Quan <evan.quan@amd.com>
    Signed-off-by: Wenyou Yang <WenYou.Yang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/swsmu/smu_cmn.c
+++ drivers/gpu/drm/amd/pm/swsmu/smu_cmn.c
@@ -996,6 +996,9 @@ void smu_cmn_init_soft_gpu_metrics(void *table, uint8_t frev, uint8_t crev)
 	case METRICS_VERSION(2, 3):
 		structure_size = sizeof(struct gpu_metrics_v2_3);
 		break;
+	case METRICS_VERSION(2, 4):
+		structure_size = sizeof(struct gpu_metrics_v2_4);
+		break;
 	default:
 		return;
 	}
