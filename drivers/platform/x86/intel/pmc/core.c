416a87c972b978d71ab828442d1d48e3bd194855
Xi Pardee <xi.pardee@intel.com>
2023-05-31 17:47:06 -0700

    platform/x86:intel/pmc: Remove Meteor Lake S platform support
    commit c5ad454a12c6 ("platform/x86: intel/pmc/core: Add Meteor Lake
    support to pmc core driver") was supposed to add support for Meter
    Lake P/M and mistakenly added support for Meteor Lake S instead. Meteor
    Lake P/M support was added later and MTL-S support needs to be removed
    since its currently assigned to the wrong register maps.
    
    Fixes: c5ad454a12c6 ("platform/x86: intel/pmc/core: Add Meteor Lake support to pmc core driver")
    Signed-off-by: Xi Pardee <xi.pardee@intel.com>
    Signed-off-by: David E. Box <david.e.box@linux.intel.com>
    Link: https://lore.kernel.org/r/20230601004706.871528-1-xi.pardee@intel.com
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/platform/x86/intel/pmc/core.c
+++ drivers/platform/x86/intel/pmc/core.c
@@ -1039,7 +1039,6 @@ static const struct x86_cpu_id intel_pmc_core_ids[] = {
 	X86_MATCH_INTEL_FAM6_MODEL(RAPTORLAKE_P,        tgl_core_init),
 	X86_MATCH_INTEL_FAM6_MODEL(RAPTORLAKE,		adl_core_init),
 	X86_MATCH_INTEL_FAM6_MODEL(RAPTORLAKE_S,	adl_core_init),
-	X86_MATCH_INTEL_FAM6_MODEL(METEORLAKE,          mtl_core_init),
 	X86_MATCH_INTEL_FAM6_MODEL(METEORLAKE_L,	mtl_core_init),
 	{}
 };
