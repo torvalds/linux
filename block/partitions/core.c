69f90b70bdb62e1a930239d33579e04884cd0b9a
Christoph Hellwig <hch@lst.de>
2023-06-01 11:44:49 +0200

    block: unhash the inode earlier in delete_partition
    Move the call to remove_inode_hash to the beginning of delete_partition,
    as we want to prevent opening a block_device that is about to be removed
    ASAP.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Acked-by: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Dave Chinner <dchinner@redhat.com>
    Link: https://lore.kernel.org/r/20230601094459.1350643-7-hch@lst.de
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/partitions/core.c
+++ block/partitions/core.c
@@ -267,18 +267,18 @@ static void delete_partition(struct block_device *part)
 {
 	lockdep_assert_held(&part->bd_disk->open_mutex);
 
+	/*
+	 * Remove the block device from the inode hash, so that it cannot be
+	 * looked up any more even when openers still hold references.
+	 */
+	remove_inode_hash(part->bd_inode);
+
 	fsync_bdev(part);
 	__invalidate_device(part, true);
 
 	xa_erase(&part->bd_disk->part_tbl, part->bd_partno);
 	kobject_put(part->bd_holder_dir);
 	device_del(&part->bd_device);
-
-	/*
-	 * Remove the block device from the inode hash, so that it cannot be
-	 * looked up any more even when openers still hold references.
-	 */
-	remove_inode_hash(part->bd_inode);
 
 	put_device(&part->bd_device);
 }
