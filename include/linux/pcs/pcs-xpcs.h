4739b9f3d211b3c4ce9353fbfd9f22e2bcb64c17
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2023-06-02 14:58:45 +0100

    net: pcs: xpcs: remove xpcs_create() from public view
    There are now no callers of xpcs_create(), so let's remove it from
    public view to discourage future direct usage.
    
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Reviewed-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/pcs/pcs-xpcs.h
+++ include/linux/pcs/pcs-xpcs.h
@@ -35,8 +35,6 @@ int xpcs_do_config(struct dw_xpcs *xpcs, phy_interface_t interface,
 void xpcs_get_interfaces(struct dw_xpcs *xpcs, unsigned long *interfaces);
 int xpcs_config_eee(struct dw_xpcs *xpcs, int mult_fact_100ns,
 		    int enable);
-struct dw_xpcs *xpcs_create(struct mdio_device *mdiodev,
-			    phy_interface_t interface);
 struct dw_xpcs *xpcs_create_mdiodev(struct mii_bus *bus, int addr,
 				    phy_interface_t interface);
 void xpcs_destroy(struct dw_xpcs *xpcs);
