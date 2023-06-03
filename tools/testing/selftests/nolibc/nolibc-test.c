4cbab2eca009ca022ae17596b5f3d4024c52a65b
Zhangjin Wu <falcon@tinylab.org>
2023-06-03 16:14:55 +0800

    selftests/nolibc: remove gettimeofday_bad1/2 completely
    In the clock_gettime / clock_gettime64 syscalls based gettimeofday(),
    there is no way to let kernel space 'fixup' the invalid data pointer of
    'struct timeval' and 'struct timezone' for us for we need to read
    timespec from kernel space and then convert to timeval in user-space
    ourselves and also we need to simply ignore and reset timezone in
    user-space.
    
    Without this removal, the invalid (void *)1 address will trigger a
    sigsegv (signum = 11) signal and stop the whole test.
    
    Suggested-by: Willy Tarreau <w@1wt.eu>
    Link: https://lore.kernel.org/linux-riscv/20230528113325.GJ1956@1wt.eu/
    Signed-off-by: Zhangjin Wu <falcon@tinylab.org>
    Reviewed-by: Thomas Weißschuh <linux@weissschuh.net>
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- tools/testing/selftests/nolibc/nolibc-test.c
+++ tools/testing/selftests/nolibc/nolibc-test.c
@@ -593,10 +593,6 @@ int run_syscall(int min, int max)
 		CASE_TEST(fork);              EXPECT_SYSZR(1, test_fork()); break;
 		CASE_TEST(getdents64_root);   EXPECT_SYSNE(1, test_getdents64("/"), -1); break;
 		CASE_TEST(getdents64_null);   EXPECT_SYSER(1, test_getdents64("/dev/null"), -1, ENOTDIR); break;
-#ifdef NOLIBC
-		CASE_TEST(gettimeofday_bad1); EXPECT_SYSER(1, gettimeofday((void *)1, NULL), -1, EFAULT); break;
-		CASE_TEST(gettimeofday_bad2); EXPECT_SYSER(1, gettimeofday(NULL, (void *)1), -1, EFAULT); break;
-#endif
 		CASE_TEST(getpagesize);       EXPECT_SYSZR(1, test_getpagesize()); break;
 		CASE_TEST(ioctl_tiocinq);     EXPECT_SYSZR(1, ioctl(0, TIOCINQ, &tmp)); break;
 		CASE_TEST(ioctl_tiocinq);     EXPECT_SYSZR(1, ioctl(0, TIOCINQ, &tmp)); break;
