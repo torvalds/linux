7e521093113b3920aff2f932221c87b6e910f33a
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2023-06-05 18:45:23 +0300

    pinctrl: moorefield: Use BUFCFG_PINMODE_GPIO in ->pin_dbg_show()
    Use explicit comparison to BUFCFG_PINMODE_GPIO instead of implying it.
    
    Acked-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/pinctrl/intel/pinctrl-moorefield.c
+++ drivers/pinctrl/intel/pinctrl-moorefield.c
@@ -504,7 +504,7 @@ static void mofld_pin_dbg_show(struct pinctrl_dev *pctldev, struct seq_file *s,
 	}
 
 	mode = (value & BUFCFG_PINMODE_MASK) >> BUFCFG_PINMODE_SHIFT;
-	if (!mode)
+	if (mode == BUFCFG_PINMODE_GPIO)
 		seq_puts(s, "GPIO ");
 	else
 		seq_printf(s, "mode %d ", mode);
