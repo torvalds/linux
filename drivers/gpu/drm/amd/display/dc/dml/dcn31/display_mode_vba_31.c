caf0f98dc280b5426b1858d4e70130aa3dd9679c
Hamza Mahfooz <hamza.mahfooz@amd.com>
2023-06-05 13:59:08 -0400

    drm/amd/display: mark dml31's UseMinimumDCFCLK() as noinline_for_stack
    clang reports:
    drivers/gpu/drm/amd/amdgpu/../display/dc/dml/dcn31/display_mode_vba_31.c:3797:6: error: stack frame size (2632) exceeds limit (2048) in 'dml31_ModeSupportAndSystemConfigurationFull' [-Werror,-Wframe-larger-than]
     3797 | void dml31_ModeSupportAndSystemConfigurationFull(struct display_mode_lib *mode_lib)
          |      ^
    1 error generated.
    
    So, since UseMinimumDCFCLK() consumes a lot of stack space, mark it as
    noinline_for_stack to prevent it from blowing up
    dml31_ModeSupportAndSystemConfigurationFull()'s stack size.
    
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Hamza Mahfooz <hamza.mahfooz@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dml/dcn31/display_mode_vba_31.c
+++ drivers/gpu/drm/amd/display/dc/dml/dcn31/display_mode_vba_31.c
@@ -7032,7 +7032,7 @@ static double CalculateUrgentLatency(
 	return ret;
 }
 
-static void UseMinimumDCFCLK(
+static noinline_for_stack void UseMinimumDCFCLK(
 		struct display_mode_lib *mode_lib,
 		int MaxPrefetchMode,
 		int ReorderingBytes)
