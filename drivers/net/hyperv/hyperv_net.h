4cab498f33f7adaa01ad15909c0f34a81e5a0b0a
Shradha Gupta <shradhagupta@linux.microsoft.com>
2023-06-05 04:30:06 -0700

    hv_netvsc: Allocate rx indirection table size dynamically
    Allocate the size of rx indirection table dynamically in netvsc
    from the value of size provided by OID_GEN_RECEIVE_SCALE_CAPABILITIES
    query instead of using a constant value of ITAB_NUM.
    
    Signed-off-by: Shradha Gupta <shradhagupta@linux.microsoft.com>
    Reviewed-by: Haiyang Zhang <haiyangz@microsoft.com>
    Tested-on: Ubuntu22 (azure VM, SKU size: Standard_F72s_v2)
    Testcases:
    1. ethtool -x eth0 output
    2. LISA testcase:PERF-NETWORK-TCP-THROUGHPUT-MULTICONNECTION-NTTTCP-Synthetic
    3. LISA testcase:PERF-NETWORK-TCP-THROUGHPUT-MULTICONNECTION-NTTTCP-SRIOV
    Reviewed-by: Simon Horman <simon.horman@corigine.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/hyperv/hyperv_net.h
+++ drivers/net/hyperv/hyperv_net.h
@@ -74,6 +74,7 @@ struct ndis_recv_scale_cap { /* NDIS_RECEIVE_SCALE_CAPABILITIES */
 #define NDIS_RSS_HASH_SECRET_KEY_MAX_SIZE_REVISION_2   40
 
 #define ITAB_NUM 128
+#define ITAB_NUM_MAX 256
 
 struct ndis_recv_scale_param { /* NDIS_RECEIVE_SCALE_PARAMETERS */
 	struct ndis_obj_header hdr;
@@ -1034,7 +1035,9 @@ struct net_device_context {
 
 	u32 tx_table[VRSS_SEND_TAB_SIZE];
 
-	u16 rx_table[ITAB_NUM];
+	u16 *rx_table;
+
+	u32 rx_table_sz;
 
 	/* Ethtool settings */
 	u8 duplex;
