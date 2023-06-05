3536a204ce05752b93f13b4d818a3d17acb60ee2
Wyon Bi <bivvy.bi@rock-chips.com>
2023-06-05 13:31:35 +0000

    drm/panel: maxim-max96752f: Fix pin assignment
    Fixes: 375777288b3d ("drm/panel: maxim-max96752f: Add HannStar HSD123JPW3-A15 panel")
    Signed-off-by: Wyon Bi <bivvy.bi@rock-chips.com>
    Change-Id: I73ae495870af9d230bd6a16bed8205a03841bac0

--- drivers/gpu/drm/panel/panel-maxim-max96752f.c
+++ drivers/gpu/drm/panel/panel-maxim-max96752f.c
@@ -408,12 +408,11 @@ static int hannstar_hsd123jpw3_a15_prepare(struct max96752f *max96752f)
 {
 	maxim_deserializer_write(max96752f, 0x0002, 0x43);
 	maxim_deserializer_write(max96752f, 0x0140, 0x20);
+	maxim_deserializer_write(max96752f, 0x01ce, 0x5e);
 
-	maxim_deserializer_write(max96752f, 0x01ce, 0x5e);	/* oldi */
-	maxim_deserializer_write(max96752f, 0x0226, 0x40);	/* bl_pwm */
-	maxim_deserializer_write(max96752f, 0x0224, 0x84);
-	maxim_deserializer_write(max96752f, 0x0204, 0xa1);	/* tp_int */
-	maxim_deserializer_write(max96752f, 0x0203, 0x83);
+	maxim_deserializer_write(max96752f, 0x0203, 0x83);	/* GPIO1  <- TP_INT */
+	maxim_deserializer_write(max96752f, 0x0206, 0x84);      /* GPIO2  -> TP_RST */
+	maxim_deserializer_write(max96752f, 0x0224, 0x84);	/* GPIO12 -> LCD_BL_PWM */
 
 	return 0;
 }
@@ -425,8 +424,7 @@ static int hannstar_hsd123jpw3_a15_unprepare(struct max96752f *max96752f)
 
 static int hannstar_hsd123jpw3_a15_enable(struct max96752f *max96752f)
 {
-	maxim_deserializer_write(max96752f, 0x0221, 0x90);	/* lcd_rst */
-	maxim_deserializer_write(max96752f, 0x0206, 0x90);	/* tp_rst */
+	maxim_deserializer_write(max96752f, 0x0221, 0x90);	/* GPIO11 -> LCD_RESET */
 	msleep(20);
 
 	return 0;
@@ -434,8 +432,7 @@ static int hannstar_hsd123jpw3_a15_enable(struct max96752f *max96752f)
 
 static int hannstar_hsd123jpw3_a15_disable(struct max96752f *max96752f)
 {
-	maxim_deserializer_write(max96752f, 0x0206, 0x80);      /* tp_rst */
-	maxim_deserializer_write(max96752f, 0x0227, 0x80);	/* lcd_rst */
+	maxim_deserializer_write(max96752f, 0x0221, 0x80);	/* GPIO11 -> LCD_RESET */
 	msleep(20);
 
 	return 0;
