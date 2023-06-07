ed83fe2abcace898fdec5c2ba0455703178ac9a3
Ilya Bakoulin <ilya.bakoulin@amd.com>
2023-06-07 16:49:45 -0400

    drm/amd/display: Fix 128b132b link loss handling
    [Why]
    We don't check 128b132b-specific bits in LANE_ALIGN_STATUS_UPDATED DPCD
    registers when parsing link loss status, which can cause us to miss a
    link loss notification from some sinks.
    
    [How]
    Add a 128b132b-specific status bit check.
    
    Cc: stable@vger.kernel.org # 6.3+
    Reviewed-by: Wenjing Liu <wenjing.liu@amd.com>
    Acked-by: Hamza Mahfooz <hamza.mahfooz@amd.com>
    Signed-off-by: Ilya Bakoulin <ilya.bakoulin@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/link/protocols/link_dp_irq_handler.c
+++ drivers/gpu/drm/amd/display/dc/link/protocols/link_dp_irq_handler.c
@@ -82,8 +82,15 @@ bool dp_parse_link_loss_status(
 	}
 
 	/* Check interlane align.*/
-	if (sink_status_changed ||
-		!hpd_irq_dpcd_data->bytes.lane_status_updated.bits.INTERLANE_ALIGN_DONE) {
+	if (link_dp_get_encoding_format(&link->cur_link_settings) == DP_128b_132b_ENCODING &&
+			(!hpd_irq_dpcd_data->bytes.lane_status_updated.bits.EQ_INTERLANE_ALIGN_DONE_128b_132b ||
+			 !hpd_irq_dpcd_data->bytes.lane_status_updated.bits.CDS_INTERLANE_ALIGN_DONE_128b_132b)) {
+		sink_status_changed = true;
+	} else if (!hpd_irq_dpcd_data->bytes.lane_status_updated.bits.INTERLANE_ALIGN_DONE) {
+		sink_status_changed = true;
+	}
+
+	if (sink_status_changed) {
 
 		DC_LOG_HW_HPD_IRQ("%s: Link Status changed.\n", __func__);
 
