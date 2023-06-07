564d73c4d9201526bd976b9379d2aaf1a7133e84
Biju Das <biju.das.jz@bp.renesas.com>
2023-06-07 17:57:38 +0100

    i2c: Add i2c_get_match_data()
    Add i2c_get_match_data() to get match data for I2C, ACPI and
    DT-based matching, so that we can optimize the driver code.
    
    Suggested-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>
    [wsa: simplified var initialization]
    Signed-off-by: Wolfram Sang <wsa@kernel.org>

--- include/linux/i2c.h
+++ include/linux/i2c.h
@@ -367,6 +367,8 @@ struct i2c_adapter *i2c_verify_adapter(struct device *dev);
 const struct i2c_device_id *i2c_match_id(const struct i2c_device_id *id,
 					 const struct i2c_client *client);
 
+const void *i2c_get_match_data(const struct i2c_client *client);
+
 static inline struct i2c_client *kobj_to_i2c_client(struct kobject *kobj)
 {
 	struct device * const dev = kobj_to_dev(kobj);
