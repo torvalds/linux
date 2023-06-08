bfc43a9c0ce5f3fcf98c972d7b7f21a742b7c957
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2023-06-08 13:53:10 +0100

    dt-bindings: clock: Add LPASSCC and reset controller for SC8280XP
    The LPASS (Low Power Audio Subsystem) clock controller provides reset
    support when it is under the control of Q6DSP.
    
    Add support for those resets and adds IDs for clients to request the reset.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Reviewed-by: Johan Hovold <johan+linaro@kernel.org>
    Reviewed-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    Link: https://lore.kernel.org/r/20230608125315.11454-2-srinivas.kandagatla@linaro.org

---
+++ include/dt-bindings/clock/qcom,sc8280xp-lpasscc.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright (c) 2023, Linaro Ltd.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_LPASSCC_SC8280XP_H
+#define _DT_BINDINGS_CLK_QCOM_LPASSCC_SC8280XP_H
+
+/* LPASS TCSR */
+#define LPASS_AUDIO_SWR_TX_CGCR				0
+
+#endif
