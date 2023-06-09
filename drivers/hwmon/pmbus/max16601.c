450d1a8ce1e5f1cadd402ae62013afff50a5cc9c
Guenter Roeck <linux@roeck-us.net>
2023-06-09 11:24:47 -0700

    hwmon: (pmbus/max16601) Add support for new revisions of MAX16508
    New revisions of MAX16508 report MAX16508.xx or MAX16508y.xx as device ID,
    but are functionally similar to MAX16508. Add support for those chip
    variants.
    
    Cc: Vlad Sytchenko <vsytch@google.com>
    Cc: Steve Foreman <foremans@google.com>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/pmbus/max16601.c
+++ drivers/hwmon/pmbus/max16601.c
@@ -283,10 +283,10 @@ static int max16601_get_id(struct i2c_client *client)
 		return -ENODEV;
 
 	/*
-	 * PMBUS_IC_DEVICE_ID is expected to return MAX1660[012]y.xx" or
-	 * "MAX16500y.xx".cdxxcccccccccc
+	 * PMBUS_IC_DEVICE_ID is expected to return MAX1660[012]y.xx",
+	 * "MAX16500y.xx".cdxxcccccccccc, or "MAX16508y.xx".
 	 */
-	if (!strncmp(buf, "MAX16500", 8)) {
+	if (!strncmp(buf, "MAX16500", 8) || !strncmp(buf, "MAX16508", 8)) {
 		id = max16508;
 	} else if (!strncmp(buf, "MAX16600", 8)) {
 		id = max16600;
