4389fb6b6a9d31704cc576b7671b6dfe9ac9f966
Qu Wenruo <wqu@suse.com>
2023-06-11 08:09:13 +0800

    btrfs: do not ASSERT() on duplicated global roots
    commit 745806fb4554f334e6406fa82b328562aa48f08f upstream.
    
    [BUG]
    Syzbot reports a reproducible ASSERT() when using rescue=usebackuproot
    mount option on a corrupted fs.
    
    The full report can be found here:
    https://syzkaller.appspot.com/bug?extid=c4614eae20a166c25bf0
    
      BTRFS error (device loop0: state C): failed to load root csum
      assertion failed: !tmp, in fs/btrfs/disk-io.c:1103
      ------------[ cut here ]------------
      kernel BUG at fs/btrfs/ctree.h:3664!
      invalid opcode: 0000 [#1] PREEMPT SMP KASAN
      CPU: 1 PID: 3608 Comm: syz-executor356 Not tainted 6.0.0-rc7-syzkaller-00029-g3800a713b607 #0
      Hardware name: Google Google Compute Engine/Google Compute Engine, BIOS Google 08/26/2022
      RIP: 0010:assertfail+0x1a/0x1c fs/btrfs/ctree.h:3663
      RSP: 0018:ffffc90003aaf250 EFLAGS: 00010246
      RAX: 0000000000000032 RBX: 0000000000000000 RCX: f21c13f886638400
      RDX: 0000000000000000 RSI: 0000000080000000 RDI: 0000000000000000
      RBP: ffff888021c640a0 R08: ffffffff816bd38d R09: ffffed10173667f1
      R10: ffffed10173667f1 R11: 1ffff110173667f0 R12: dffffc0000000000
      R13: ffff8880229c21f7 R14: ffff888021c64060 R15: ffff8880226c0000
      FS:  0000555556a73300(0000) GS:ffff8880b9b00000(0000) knlGS:0000000000000000
      CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
      CR2: 000055a2637d7a00 CR3: 00000000709c4000 CR4: 00000000003506e0
      DR0: 0000000000000000 DR1: 0000000000000000 DR2: 0000000000000000
      DR3: 0000000000000000 DR6: 00000000fffe0ff0 DR7: 0000000000000400
      Call Trace:
       <TASK>
       btrfs_global_root_insert+0x1a7/0x1b0 fs/btrfs/disk-io.c:1103
       load_global_roots_objectid+0x482/0x8c0 fs/btrfs/disk-io.c:2467
       load_global_roots fs/btrfs/disk-io.c:2501 [inline]
       btrfs_read_roots fs/btrfs/disk-io.c:2528 [inline]
       init_tree_roots+0xccb/0x203c fs/btrfs/disk-io.c:2939
       open_ctree+0x1e53/0x33df fs/btrfs/disk-io.c:3574
       btrfs_fill_super+0x1c6/0x2d0 fs/btrfs/super.c:1456
       btrfs_mount_root+0x885/0x9a0 fs/btrfs/super.c:1824
       legacy_get_tree+0xea/0x180 fs/fs_context.c:610
       vfs_get_tree+0x88/0x270 fs/super.c:1530
       fc_mount fs/namespace.c:1043 [inline]
       vfs_kern_mount+0xc9/0x160 fs/namespace.c:1073
       btrfs_mount+0x3d3/0xbb0 fs/btrfs/super.c:1884
    
    [CAUSE]
    Since the introduction of global roots, we handle
    csum/extent/free-space-tree roots as global roots, even if no
    extent-tree-v2 feature is enabled.
    
    So for regular csum/extent/fst roots, we load them into
    fs_info::global_root_tree rb tree.
    
    And we should not expect any conflicts in that rb tree, thus we have an
    ASSERT() inside btrfs_global_root_insert().
    
    But rescue=usebackuproot can break the assumption, as we will try to
    load those trees again and again as long as we have bad roots and have
    backup roots slot remaining.
    
    So in that case we can have conflicting roots in the rb tree, and
    triggering the ASSERT() crash.
    
    [FIX]
    We can safely remove that ASSERT(), as the caller will properly put the
    offending root.
    
    To make further debugging easier, also add two explicit error messages:
    
    - Error message for conflicting global roots
    - Error message when using backup roots slot
    
    Reported-by: syzbot+a694851c6ab28cbcfb9c@syzkaller.appspotmail.com
    Fixes: abed4aaae4f7 ("btrfs: track the csum, extent, and free space trees in a rb tree")
    CC: stable@vger.kernel.org # 6.1+
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -1098,13 +1098,18 @@ int btrfs_global_root_insert(struct btrfs_root *root)
 {
 	struct btrfs_fs_info *fs_info = root->fs_info;
 	struct rb_node *tmp;
+	int ret = 0;
 
 	write_lock(&fs_info->global_root_lock);
 	tmp = rb_find_add(&root->rb_node, &fs_info->global_root_tree, global_root_cmp);
 	write_unlock(&fs_info->global_root_lock);
-	ASSERT(!tmp);
 
-	return tmp ? -EEXIST : 0;
+	if (tmp) {
+		ret = -EEXIST;
+		btrfs_warn(fs_info, "global root %llu %llu already exists",
+				root->root_key.objectid, root->root_key.offset);
+	}
+	return ret;
 }
 
 void btrfs_global_root_delete(struct btrfs_root *root)
@@ -2934,6 +2939,7 @@ static int __cold init_tree_roots(struct btrfs_fs_info *fs_info)
 			/* We can't trust the free space cache either */
 			btrfs_set_opt(fs_info->mount_opt, CLEAR_CACHE);
 
+			btrfs_warn(fs_info, "try to load backup roots slot %d", i);
 			ret = read_backup_root(fs_info, i);
 			backup_index = ret;
 			if (ret < 0)
