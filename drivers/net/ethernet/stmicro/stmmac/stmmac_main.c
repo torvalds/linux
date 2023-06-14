33965ac3409153d3f1af8039ad671b6b47dd0b2d
Christian Marangi <ansuelsmth@gmail.com>
2023-06-14 11:17:14 +0200

    net: ethernet: stmicro: stmmac: fix possible memory leak in __stmmac_open
    commit 30134b7c47bd28fdb4db4d12aef824e0579cfee4 upstream.
    
    Fix a possible memory leak in __stmmac_open when stmmac_init_phy fails.
    It's also needed to free everything allocated by stmmac_setup_dma_desc
    and not just the dma_conf struct.
    
    Drop free_dma_desc_resources from __stmmac_open and correctly call
    free_dma_desc_resources on each user of __stmmac_open on error.
    
    Reported-by: Jose Abreu <Jose.Abreu@synopsys.com>
    Fixes: ba39b344e924 ("net: ethernet: stmicro: stmmac: generate stmmac dma conf before open")
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>
    Cc: stable@vger.kernel.org
    Reviewed-by: Simon Horman <simon.horman@corigine.com>
    Reviewed-by: Jose Abreu <Jose.Abreu@synopsys.com>
    Link: https://lore.kernel.org/r/20230614091714.15912-1-ansuelsmth@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -3865,7 +3865,6 @@ irq_error:
 
 	stmmac_hw_teardown(dev);
 init_error:
-	free_dma_desc_resources(priv, &priv->dma_conf);
 	phylink_disconnect_phy(priv->phylink);
 init_phy_error:
 	pm_runtime_put(priv->device);
@@ -3883,6 +3882,9 @@ static int stmmac_open(struct net_device *dev)
 		return PTR_ERR(dma_conf);
 
 	ret = __stmmac_open(dev, dma_conf);
+	if (ret)
+		free_dma_desc_resources(priv, dma_conf);
+
 	kfree(dma_conf);
 	return ret;
 }
@@ -5607,11 +5609,14 @@ static int stmmac_change_mtu(struct net_device *dev, int new_mtu)
 		stmmac_release(dev);
 
 		ret = __stmmac_open(dev, dma_conf);
-		kfree(dma_conf);
 		if (ret) {
+			free_dma_desc_resources(priv, dma_conf);
+			kfree(dma_conf);
 			netdev_err(priv->dev, "failed reopening the interface after MTU change\n");
 			return ret;
 		}
+
+		kfree(dma_conf);
 
 		stmmac_set_rx_mode(dev);
 	}
