efed5b50f3b835b7e12459f5a77b99931215596c
Fabio M. De Francesco <fmdefrancesco@gmail.com>
2023-06-14 12:02:55 +0200

    ext4: drop the call to ext4_error() from ext4_get_group_info()
    [ Upstream commit f451fd97dd2b78f286379203a47d9d295c467255 ]
    
    A recent patch added a call to ext4_error() which is problematic since
    some callers of the ext4_get_group_info() function may be holding a
    spinlock, whereas ext4_error() must never be called in atomic context.
    
    This triggered a report from Syzbot: "BUG: sleeping function called from
    invalid context in ext4_update_super" (see the link below).
    
    Therefore, drop the call to ext4_error() from ext4_get_group_info(). In
    the meantime use eight characters tabs instead of nine characters ones.
    
    Reported-by: syzbot+4acc7d910e617b360859@syzkaller.appspotmail.com
    Closes: https://lore.kernel.org/all/00000000000070575805fdc6cdb2@google.com/
    Fixes: 5354b2af3406 ("ext4: allow ext4_get_group_info() to fail")
    Suggested-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Fabio M. De Francesco <fmdefrancesco@gmail.com>
    Link: https://lore.kernel.org/r/20230614100446.14337-1-fmdefrancesco@gmail.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/ext4/balloc.c
+++ fs/ext4/balloc.c
@@ -322,17 +322,15 @@ static ext4_fsblk_t ext4_valid_block_bitmap_padding(struct super_block *sb,
 struct ext4_group_info *ext4_get_group_info(struct super_block *sb,
 					    ext4_group_t group)
 {
-	 struct ext4_group_info **grp_info;
-	 long indexv, indexh;
+	struct ext4_group_info **grp_info;
+	long indexv, indexh;
 
-	 if (unlikely(group >= EXT4_SB(sb)->s_groups_count)) {
-		 ext4_error(sb, "invalid group %u", group);
-		 return NULL;
-	 }
-	 indexv = group >> (EXT4_DESC_PER_BLOCK_BITS(sb));
-	 indexh = group & ((EXT4_DESC_PER_BLOCK(sb)) - 1);
-	 grp_info = sbi_array_rcu_deref(EXT4_SB(sb), s_group_info, indexv);
-	 return grp_info[indexh];
+	if (unlikely(group >= EXT4_SB(sb)->s_groups_count))
+		return NULL;
+	indexv = group >> (EXT4_DESC_PER_BLOCK_BITS(sb));
+	indexh = group & ((EXT4_DESC_PER_BLOCK(sb)) - 1);
+	grp_info = sbi_array_rcu_deref(EXT4_SB(sb), s_group_info, indexv);
+	return grp_info[indexh];
 }
 
 /*
