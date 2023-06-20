c3018c0e878413e0fb2af414bb61e8f14fea42f2
Hajime Tazaki <thehajime@gmail.com>
2023-06-20 22:24:24 +0900

    lkl: follow up fix for v6.0 merge (pci.h)
    Fixes: a2912b45b082 ("asm-generic: Add new pci.h and use it")
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- arch/lkl/include/asm/pci.h
+++ arch/lkl/include/asm/pci.h
@@ -2,10 +2,10 @@
 #ifndef _ASM_LKL_PCI_H
 #define _ASM_LKL_PCI_H
 
-#include <asm-generic/pci.h>
-
 #define pcibios_assign_all_busses() 0
 #define PCIBIOS_MIN_IO 0x1000
 #define PCIBIOS_MIN_MEM 0x10000000
+
+#include <asm-generic/pci.h>
 
 #endif /* _ASM_LKL_PCI_H */
