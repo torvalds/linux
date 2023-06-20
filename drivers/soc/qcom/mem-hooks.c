ecc206cbbb7f47bfdf24f46d6130c3e388814fdd
Chris Goldsworthy <quic_cgoldswo@quicinc.com>
2023-06-20 11:22:52 -0700

    Revert "cma: Register hooks for ZONE_MOVABLE allocs"
    This reverts commit d446075558c85ad9e7d76ccd4dd6f88cf1ff99c9.
    
    Change-Id: I11bc0c6020f68b9965d3a979eaac348ea96598e0
    Signed-off-by: Chris Goldsworthy <quic_cgoldswo@quicinc.com>

--- drivers/soc/qcom/mem-hooks.c
+++ drivers/soc/qcom/mem-hooks.c
@@ -13,27 +13,10 @@
 #include <linux/nodemask.h>
 #include <linux/kthread.h>
 #include <linux/swap.h>
-#include <linux/gfp_types.h>
 
 static void balance_reclaim(void *unused, bool *balance_anon_file_reclaim)
 {
 	*balance_anon_file_reclaim = true;
-}
-
-static void readahead_set(void *data, gfp_t *flag)
-{
-	if (*flag & __GFP_MOVABLE) {
-		*flag |= __GFP_CMA;
-		*flag &= ~__GFP_HIGHMEM;
-	}
-}
-
-static void gfp_zone_set(void *data, gfp_t *flag)
-{
-	if (!IS_ENABLED(CONFIG_HIGHMEM)) {
-		if ((*flag & __GFP_MOVABLE) && !(*flag & __GFP_CMA))
-			*flag &= ~__GFP_HIGHMEM;
-	}
 }
 
 static int __init init_mem_hooks(void)
@@ -48,19 +31,6 @@ static int __init init_mem_hooks(void)
 			pr_err("Failed to register balance_anon_file_reclaim hooks\n");
 			return ret;
 		}
-	}
-
-	ret = register_trace_android_rvh_set_readahead_gfp_mask(readahead_set,
-								NULL);
-	if (ret) {
-		pr_err("Failed to register readahead_gfp_mask hooks\n");
-		return ret;
-	}
-
-	ret = register_trace_android_rvh_set_gfp_zone_flags(gfp_zone_set, NULL);
-	if (ret) {
-		pr_err("Failed to register gfp_zone_flags hooks\n");
-		return ret;
 	}
 
 	return 0;
