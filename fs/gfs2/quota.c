dec64ae37bf90c2ae3fba2089f5bd1d025c57995
Bob Peterson <rpeterso@redhat.com>
2023-06-21 12:31:15 -0500

    gfs2: Remove useless err set
    Function gfs2_adjust_quota set variable err, then set it again to a
    different value. This patch removes the redundant set.
    
    Signed-off-by: Bob Peterson <rpeterso@redhat.com>
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- fs/gfs2/quota.c
+++ fs/gfs2/quota.c
@@ -859,7 +859,6 @@ static int gfs2_adjust_quota(struct gfs2_sbd *sdp, loff_t loc,
 		return err;
 
 	loc -= sizeof(q); /* gfs2_internal_read would've advanced the loc ptr */
-	err = -EIO;
 	be64_add_cpu(&q.qu_value, change);
 	if (((s64)be64_to_cpu(q.qu_value)) < 0)
 		q.qu_value = 0; /* Never go negative on quota usage */
