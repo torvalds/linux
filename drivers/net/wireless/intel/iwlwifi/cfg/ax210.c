a7de384c93996f8cdf8e7b5304bf7963161241a6
Johannes Berg <johannes.berg@intel.com>
2023-06-21 13:12:11 +0300

    wifi: iwlwifi: don't load old firmware for ax210
    The earliest firmware released for these products is with
    API version 59 (for 'ty' only), so no point trying to go
    back in time even further than that.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Gregory Greenman <gregory.greenman@intel.com>
    Link: https://lore.kernel.org/r/20230621130443.ebe02b5dbddb.I51484ebb6c89256b0e6e7f9bb24f597c4ebead67@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/intel/iwlwifi/cfg/ax210.c
+++ drivers/net/wireless/intel/iwlwifi/cfg/ax210.c
@@ -13,7 +13,7 @@
 #define IWL_AX210_UCODE_API_MAX	82
 
 /* Lowest firmware API version supported */
-#define IWL_AX210_UCODE_API_MIN	39
+#define IWL_AX210_UCODE_API_MIN	59
 
 /* NVM versions */
 #define IWL_AX210_NVM_VERSION		0x0a1d
