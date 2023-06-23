6dac1507a654f897ae98d7ec1a12b712c3ec4d47
Jim Quinlan <james.quinlan@broadcom.com>
2023-06-23 10:40:58 -0400

    PCI: brcmstb: Remove stale comment
    A comment says that Multi-MSI is not supported by the driver.
    A past commit [1] added this feature, so the comment is
    incorrect and is removed.
    
    [1] commit 198acab1772f22f2 ("PCI: brcmstb: Enable Multi-MSI")
    
    Link: https://lore.kernel.org/r/20230623144100.34196-6-james.quinlan@broadcom.com
    Signed-off-by: Jim Quinlan <james.quinlan@broadcom.com>
    Signed-off-by: Lorenzo Pieralisi <lpieralisi@kernel.org>
    Acked-by: Florian Fainelli <f.fainelli@gmail.com>

--- drivers/pci/controller/pcie-brcmstb.c
+++ drivers/pci/controller/pcie-brcmstb.c
@@ -439,7 +439,6 @@ static struct irq_chip brcm_msi_irq_chip = {
 };
 
 static struct msi_domain_info brcm_msi_domain_info = {
-	/* Multi MSI is supported by the controller, but not by this driver */
 	.flags	= (MSI_FLAG_USE_DEF_DOM_OPS | MSI_FLAG_USE_DEF_CHIP_OPS |
 		   MSI_FLAG_MULTI_PCI_MSI),
 	.chip	= &brcm_msi_irq_chip,
