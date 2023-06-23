da1a055d01ed0c18402dd1f1934096ac4bb36ada
Sumitra Sharma <sumitraartsy@gmail.com>
2023-06-23 08:16:44 -0700

    lib/test_bpf: Call page_address() on page acquired with GFP_KERNEL flag
    generate_test_data() acquires a page with alloc_page(GFP_KERNEL).
    The GFP_KERNEL is typical for kernel-internal allocations. The
    caller requires ZONE_NORMAL or a lower zone for direct access.
    
    Therefore the page cannot come from ZONE_HIGHMEM. Thus there's no
    need to map it with kmap().
    
    Also, the kmap() is being deprecated in favor of kmap_local_page() [1].
    
    Hence, use a plain page_address() directly.
    
    Since the page passed to the page_address() is not from the highmem
    zone, the page_address() function will always return a valid kernel
    virtual address and will not return NULL. Hence, remove the check
    'if (!ptr)'.
    
    Remove the unused variable 'ptr' and label 'err_free_page'.
    
      [1] https://lore.kernel.org/all/20220813220034.806698-1-ira.weiny@intel.com/
    
    Reported-by: kernel test robot <lkp@intel.com>
    Suggested-by: Fabio M. De Francesco <fmdefrancesco@gmail.com>
    Signed-off-by: Sumitra Sharma <sumitraartsy@gmail.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Reviewed-by: Fabio M. De Francesco <fmdefrancesco@gmail.com>
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Link: https://lore.kernel.org/bpf/20230623151644.GA434468@sumitra.com

--- lib/test_bpf.c
+++ lib/test_bpf.c
@@ -14381,25 +14381,15 @@ static void *generate_test_data(struct bpf_test *test, int sub)
 		 * single fragment to the skb, filled with
 		 * test->frag_data.
 		 */
-		void *ptr;
-
 		page = alloc_page(GFP_KERNEL);
-
 		if (!page)
 			goto err_kfree_skb;
 
-		ptr = kmap(page);
-		if (!ptr)
-			goto err_free_page;
-		memcpy(ptr, test->frag_data, MAX_DATA);
-		kunmap(page);
+		memcpy(page_address(page), test->frag_data, MAX_DATA);
 		skb_add_rx_frag(skb, 0, page, 0, MAX_DATA, MAX_DATA);
 	}
 
 	return skb;
-
-err_free_page:
-	__free_page(page);
 err_kfree_skb:
 	kfree_skb(skb);
 	return NULL;
