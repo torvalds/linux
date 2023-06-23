a11e097504ac1889b35b6858f495565838325f88
Nikolay Borisov <nik.borisov@suse.com>
2023-06-23 14:14:09 +0300

    x86: Make IA32_EMULATION boot time configurable
    Distributions would like to reduce their attack surface as much as
    possible but at the same time they'd want to retain flexibility to cater
    to a variety of legacy software. This stems from the conjecture that
    compat layer is likely rarely tested and could have latent security
    bugs. Ideally distributions will set their default policy and also
    give users the ability to override it as appropriate.
    
    To enable this use case, introduce CONFIG_IA32_EMULATION_DEFAULT_DISABLED
    compile time option, which controls whether 32bit processes/syscalls
    should be allowed or not. This option is aimed mainly at distributions
    to set their preferred default behavior in their kernels.
    
    To allow users to override the distro's policy, introduce the 'ia32_emulation'
    parameter which allows overriding CONFIG_IA32_EMULATION_DEFAULT_DISABLED
    state at boot time.
    
    Signed-off-by: Nikolay Borisov <nik.borisov@suse.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20230623111409.3047467-7-nik.borisov@suse.com

--- arch/x86/entry/common.c
+++ arch/x86/entry/common.c
@@ -19,6 +19,7 @@
 #include <linux/nospec.h>
 #include <linux/syscalls.h>
 #include <linux/uaccess.h>
+#include <linux/init.h>
 
 #ifdef CONFIG_XEN_PV
 #include <xen/xen-ops.h>
@@ -97,7 +98,13 @@ static __always_inline int syscall_32_enter(struct pt_regs *regs)
 }
 
 #ifdef CONFIG_IA32_EMULATION
-bool __ia32_enabled __ro_after_init = true;
+bool __ia32_enabled __ro_after_init = !IS_ENABLED(CONFIG_IA32_EMULATION_DEFAULT_DISABLED);
+
+static int ia32_emulation_override_cmdline(char *arg)
+{
+	return kstrtobool(arg, &__ia32_enabled);
+}
+early_param("ia32_emulation", ia32_emulation_override_cmdline);
 #endif
 
 /*
