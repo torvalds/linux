3867caee497edf6ce6b6117aac1c0b87c0a2cb5f
Herbert Xu <herbert@gondor.apana.org.au>
2023-06-24 13:19:56 +0800

    crypto: sm2 - Provide sm2_compute_z_digest when sm2 is disabled
    When sm2 is disabled we need to provide an implementation of
    sm2_compute_z_digest.
    
    Fixes: e5221fa6a355 ("KEYS: asymmetric: Move sm2 code into x509_public_key")
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202306231917.utO12sx8-lkp@intel.com/
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/sm2.h
+++ include/crypto/sm2.h
@@ -13,7 +13,16 @@
 
 struct shash_desc;
 
+#if IS_REACHABLE(CONFIG_CRYPTO_SM2)
 int sm2_compute_z_digest(struct shash_desc *desc,
 			 const void *key, unsigned int keylen, void *dgst);
+#else
+static inline int sm2_compute_z_digest(struct shash_desc *desc,
+				       const void *key, unsigned int keylen,
+				       void *dgst)
+{
+	return -ENOTSUPP;
+}
+#endif
 
 #endif /* _CRYPTO_SM2_H */
