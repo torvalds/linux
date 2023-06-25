26c38cd802c947401cfbcc285b7d841256b5f17f
Andy Chiu <andy.chiu@sifive.com>
2023-06-25 15:54:15 +0000

    riscv: vector: only enable interrupts in the first-use trap
    The function irqentry_exit_to_user_mode() must be called with interrupt
    disabled. The caller of do_trap_insn_illegal() also assumes running
    without interrupts. So, we should turn off interrupts after
    riscv_v_first_use_handler() returns.
    
    Fixes: cd054837243b ("riscv: Allocate user's vector context in the first-use trap")
    Signed-off-by: Andy Chiu <andy.chiu@sifive.com>
    Reviewed-by: Björn Töpel <bjorn@rivosinc.com>
    Link: https://lore.kernel.org/r/20230625155416.18629-1-andy.chiu@sifive.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>

--- arch/riscv/kernel/traps.c
+++ arch/riscv/kernel/traps.c
@@ -150,12 +150,18 @@ DO_ERROR_INFO(do_trap_insn_fault,
 
 asmlinkage __visible __trap_section void do_trap_insn_illegal(struct pt_regs *regs)
 {
+	bool handled;
+
 	if (user_mode(regs)) {
 		irqentry_enter_from_user_mode(regs);
 
 		local_irq_enable();
 
-		if (!riscv_v_first_use_handler(regs))
+		handled = riscv_v_first_use_handler(regs);
+
+		local_irq_disable();
+
+		if (!handled)
 			do_trap_error(regs, SIGILL, ILL_ILLOPC, regs->epc,
 				      "Oops - illegal instruction");
 
