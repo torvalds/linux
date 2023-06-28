0c0856714ed742acf40d30b422eba3d7a5e74a91
Greg Kroah-Hartman <gregkh@google.com>
2023-06-28 06:29:42 +0000

    Revert "tcp: deny tcp_disconnect() when threads are waiting"
    This reverts commit cccc6209708f59b0d5f0b4853588a720c829a111 which is
    commit 4faeee0cf8a5d88d63cdbc3bab124fb0e6aed08c upstream.
    
    It breaks the Android kernel abi, and if it is needed in the future can
    come back in an abi-safe way.
    
    Bug: 161946584
    Change-Id: If50dd244848a8aa70b08be347020b263c71f4a61
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -328,7 +328,6 @@ struct bpf_local_storage;
   *	@sk_cgrp_data: cgroup data for this cgroup
   *	@sk_memcg: this socket's memory cgroup association
   *	@sk_write_pending: a write to stream socket waits to start
-  *	@sk_wait_pending: number of threads blocked on this socket
   *	@sk_state_change: callback to indicate change in the state of the sock
   *	@sk_data_ready: callback to indicate there is data to be processed
   *	@sk_write_space: callback to indicate there is bf sending space available
@@ -413,7 +412,6 @@ struct sock {
 	unsigned int		sk_napi_id;
 #endif
 	int			sk_rcvbuf;
-	int			sk_wait_pending;
 
 	struct sk_filter __rcu	*sk_filter;
 	union {
@@ -1118,7 +1116,6 @@ static inline void sock_rps_reset_rxhash(struct sock *sk)
 
 #define sk_wait_event(__sk, __timeo, __condition, __wait)		\
 	({	int __rc;						\
-		__sk->sk_wait_pending++;				\
 		release_sock(__sk);					\
 		__rc = __condition;					\
 		if (!__rc) {						\
@@ -1128,7 +1125,6 @@ static inline void sock_rps_reset_rxhash(struct sock *sk)
 		}							\
 		sched_annotate_sleep();					\
 		lock_sock(__sk);					\
-		__sk->sk_wait_pending--;				\
 		__rc = __condition;					\
 		__rc;							\
 	})
