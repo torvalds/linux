70a6404ff610aa4889d98977da131c37f9ff9d1f
Sameer Pujar <spujar@nvidia.com>
2023-06-29 10:42:15 +0530

    ASoC: rt5640: Fix sleep in atomic context
    Following prints are observed while testing audio on Jetson AGX Orin which
    has onboard RT5640 audio codec:
    
      BUG: sleeping function called from invalid context at kernel/workqueue.c:3027
      in_atomic(): 1, irqs_disabled(): 128, non_block: 0, pid: 0, name: swapper/0
      preempt_count: 10001, expected: 0
      RCU nest depth: 0, expected: 0
      ------------[ cut here ]------------
      WARNING: CPU: 0 PID: 0 at kernel/irq/handle.c:159 __handle_irq_event_percpu+0x1e0/0x270
      ---[ end trace ad1c64905aac14a6 ]-
    
    The IRQ handler rt5640_irq() runs in interrupt context and can sleep
    during cancel_delayed_work_sync().
    
    Fix this by running IRQ handler, rt5640_irq(), in thread context.
    Hence replace request_irq() calls with devm_request_threaded_irq().
    
    Fixes: 051dade34695 ("ASoC: rt5640: Fix the wrong state of JD1 and JD2")
    Cc: stable@vger.kernel.org
    Cc: Oder Chiou <oder_chiou@realtek.com>
    Signed-off-by: Sameer Pujar <spujar@nvidia.com>
    Link: https://lore.kernel.org/r/1688015537-31682-4-git-send-email-spujar@nvidia.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/rt5640.c
+++ sound/soc/codecs/rt5640.c
@@ -2567,9 +2567,10 @@ static void rt5640_enable_jack_detect(struct snd_soc_component *component,
 	if (jack_data && jack_data->use_platform_clock)
 		rt5640->use_platform_clock = jack_data->use_platform_clock;
 
-	ret = request_irq(rt5640->irq, rt5640_irq,
-			  IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING | IRQF_ONESHOT,
-			  "rt5640", rt5640);
+	ret = devm_request_threaded_irq(component->dev, rt5640->irq,
+					NULL, rt5640_irq,
+					IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING | IRQF_ONESHOT,
+					"rt5640", rt5640);
 	if (ret) {
 		dev_warn(component->dev, "Failed to reguest IRQ %d: %d\n", rt5640->irq, ret);
 		rt5640_disable_jack_detect(component);
@@ -2622,8 +2623,9 @@ static void rt5640_enable_hda_jack_detect(
 
 	rt5640->jack = jack;
 
-	ret = request_irq(rt5640->irq, rt5640_irq,
-			  IRQF_TRIGGER_RISING | IRQF_ONESHOT, "rt5640", rt5640);
+	ret = devm_request_threaded_irq(component->dev, rt5640->irq,
+					NULL, rt5640_irq, IRQF_TRIGGER_RISING | IRQF_ONESHOT,
+					"rt5640", rt5640);
 	if (ret) {
 		dev_warn(component->dev, "Failed to reguest IRQ %d: %d\n", rt5640->irq, ret);
 		rt5640->irq = -ENXIO;
