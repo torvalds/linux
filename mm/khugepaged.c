e8c716bc6812202ccf4ce0f0bad3428b794fb39c
Hugh Dickins <hughd@google.com>
2023-06-28 21:31:35 -0700

    mm/khugepaged: fix regression in collapse_file()
    There is no xas_pause(&xas) in collapse_file()'s main loop, at the points
    where it does xas_unlock_irq(&xas) and then continues.
    
    That would explain why, once two weeks ago and twice yesterday, I have
    hit the VM_BUG_ON_PAGE(page != xas_load(&xas), page) since "mm/khugepaged:
    fix iteration in collapse_file" removed the xas_set(&xas, index) just
    before it: xas.xa_node could be left pointing to a stale node, if there
    was concurrent activity on the file which transformed its xarray.
    
    I tried inserting xas_pause()s, but then even bootup crashed on that
    VM_BUG_ON_PAGE(): there appears to be a subtle "nextness" implicit in
    xas_pause().
    
    xas_next() and xas_pause() are good for use in simple loops, but not in
    this one: xas_set() worked well until now, so use xas_set(&xas, index)
    explicitly at the head of the loop; and change that VM_BUG_ON_PAGE() not
    to need its own xas_set(), and not to interfere with the xa_state (which
    would probably stop the crashes from xas_pause(), but I trust that less).
    
    The user-visible effects of this bug (if VM_BUG_ONs are configured out)
    would be data loss and data leak - potentially - though in practice I
    expect it is more likely that a subsequent check (e.g. on mapping or on
    nr_none) would notice an inconsistency, and just abandon the collapse.
    
    Link: https://lore.kernel.org/linux-mm/f18e4b64-3f88-a8ab-56cc-d1f5f9c58d4@google.com/
    Fixes: c8a8f3b4a95a ("mm/khugepaged: fix iteration in collapse_file")
    Signed-off-by: Hugh Dickins <hughd@google.com>
    Cc: stable@kernel.org
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: David Stevens <stevensd@chromium.org>
    Cc: Peter Xu <peterx@redhat.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -1937,9 +1937,9 @@ static int collapse_file(struct mm_struct *mm, unsigned long addr,
 		}
 	} while (1);
 
-	xas_set(&xas, start);
 	for (index = start; index < end; index++) {
-		page = xas_next(&xas);
+		xas_set(&xas, index);
+		page = xas_load(&xas);
 
 		VM_BUG_ON(index != xas.xa_index);
 		if (is_shmem) {
@@ -1954,7 +1954,6 @@ static int collapse_file(struct mm_struct *mm, unsigned long addr,
 						result = SCAN_TRUNCATED;
 						goto xa_locked;
 					}
-					xas_set(&xas, index + 1);
 				}
 				if (!shmem_charge(mapping->host, 1)) {
 					result = SCAN_FAIL;
@@ -2090,7 +2089,7 @@ static int collapse_file(struct mm_struct *mm, unsigned long addr,
 
 		xas_lock_irq(&xas);
 
-		VM_BUG_ON_PAGE(page != xas_load(&xas), page);
+		VM_BUG_ON_PAGE(page != xa_load(xas.xa, index), page);
 
 		/*
 		 * We control three references to the page:
