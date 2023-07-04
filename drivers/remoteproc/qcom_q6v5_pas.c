d045841d299831280051484d5a23374c7e9dbc8f
Yadu MG <quic_ymg@quicinc.com>
2023-07-04 10:36:12 +0530

    drivers: remoteproc: Add support for CDSP remoteproc
    Add CDSP Remoteproc changes for MonacoAU target.
    
    Change-Id: I19b2c2c4c695d505e7b652605466c3f09622b2cd
    Signed-off-by: Yadu MG <quic_ymg@quicinc.com>

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -1922,6 +1922,19 @@ static const struct adsp_data monaco_auto_adsp_resource = {
 	.ssctl_id = 0x14,
 };
 
+static const struct adsp_data monaco_auto_cdsp_resource = {
+	.crash_reason_smem = 601,
+	.firmware_name = "cdsp0.mdt",
+	.pas_id = 18,
+	.uses_elf64 = true,
+	.has_aggre2_clk = false,
+	.auto_boot = false,
+	.ssr_name = "cdsp",
+	.sysmon_name = "cdsp",
+	.qmp_name = "cdsp",
+	.ssctl_id = 0x17,
+};
+
 static const struct of_device_id adsp_of_match[] = {
 	{ .compatible = "qcom,msm8226-adsp-pil", .data = &adsp_resource_init},
 	{ .compatible = "qcom,msm8974-adsp-pil", .data = &adsp_resource_init},
@@ -1975,6 +1988,7 @@ static const struct of_device_id adsp_of_match[] = {
 	{ .compatible = "qcom,blair-cdsp-pas", .data = &blair_cdsp_resource},
 	{ .compatible = "qcom,blair-modem-pas", .data = &blair_mpss_resource},
 	{ .compatible = "qcom,monaco_auto-adsp-pas", .data = &monaco_auto_adsp_resource},
+	{ .compatible = "qcom,monaco_auto-cdsp-pas", .data = &monaco_auto_cdsp_resource},
 	{ },
 };
 MODULE_DEVICE_TABLE(of, adsp_of_match);
