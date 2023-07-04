840f68226fcbca709ef2241490632f43e1c27fc4
Yangtao Li <frank.li@vivo.com>
2023-07-04 21:37:25 +0800

    mailbox: rockchip: Use devm_platform_get_and_ioremap_resource()
    Convert platform_get_resource(), devm_ioremap_resource() to a single
    call to devm_platform_get_and_ioremap_resource(), as this is exactly
    what this function does.
    
    Signed-off-by: Yangtao Li <frank.li@vivo.com>
    Signed-off-by: Jassi Brar <jaswinder.singh@linaro.org>

--- drivers/mailbox/rockchip-mailbox.c
+++ drivers/mailbox/rockchip-mailbox.c
@@ -194,11 +194,7 @@ static int rockchip_mbox_probe(struct platform_device *pdev)
 	mb->mbox.ops = &rockchip_mbox_chan_ops;
 	mb->mbox.txdone_irq = true;
 
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	if (!res)
-		return -ENODEV;
-
-	mb->mbox_base = devm_ioremap_resource(&pdev->dev, res);
+	mb->mbox_base = devm_platform_get_and_ioremap_resource(pdev, 0, &res);
 	if (IS_ERR(mb->mbox_base))
 		return PTR_ERR(mb->mbox_base);
 
