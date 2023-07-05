6ac5422617eda03b95c00367d0e59fcb4d7ca3cc
Jeff Layton <jlayton@kernel.org>
2023-07-05 15:01:54 -0400

    apparmor: convert to ctime accessor functions
    In later patches, we're going to change how the inode's ctime field is
    used. Switch to using accessor functions instead of raw accesses of
    inode->i_ctime.
    
    Signed-off-by: Jeff Layton <jlayton@kernel.org>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Message-Id: <20230705190309.579783-87-jlayton@kernel.org>
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- security/apparmor/policy_unpack.c
+++ security/apparmor/policy_unpack.c
@@ -89,10 +89,10 @@ void __aa_loaddata_update(struct aa_loaddata *data, long revision)
 		struct inode *inode;
 
 		inode = d_inode(data->dents[AAFS_LOADDATA_DIR]);
-		inode->i_mtime = inode->i_ctime = current_time(inode);
+		inode->i_mtime = inode_set_ctime_current(inode);
 
 		inode = d_inode(data->dents[AAFS_LOADDATA_REVISION]);
-		inode->i_mtime = inode->i_ctime = current_time(inode);
+		inode->i_mtime = inode_set_ctime_current(inode);
 	}
 }
 
