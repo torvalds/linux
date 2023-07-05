14626246efb529ebe04812cb4cc08f78be12be9a
Yangtao Li <frank.li@vivo.com>
2023-07-05 17:01:23 +0800

    phy: marvell: phy-mvebu-cp110-comphy: Use devm_platform_get_and_ioremap_resource()
    Convert platform_get_resource(), devm_ioremap_resource() to a single
    call to devm_platform_get_and_ioremap_resource(), as this is exactly
    what this function does.
    
    Signed-off-by: Yangtao Li <frank.li@vivo.com>
    Link: https://lore.kernel.org/r/20230705090126.26854-1-frank.li@vivo.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/phy/marvell/phy-mvebu-cp110-comphy.c
+++ drivers/phy/marvell/phy-mvebu-cp110-comphy.c
@@ -1011,8 +1011,7 @@ static int mvebu_comphy_probe(struct platform_device *pdev)
 						"marvell,system-controller");
 	if (IS_ERR(priv->regmap))
 		return PTR_ERR(priv->regmap);
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	priv->base = devm_ioremap_resource(&pdev->dev, res);
+	priv->base = devm_platform_get_and_ioremap_resource(pdev, 0, &res);
 	if (IS_ERR(priv->base))
 		return PTR_ERR(priv->base);
 
