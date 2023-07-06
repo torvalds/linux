07141cfedfe7fbbf2a32b6bb73f53cdc15dcc588
Yangtao Li <frank.li@vivo.com>
2023-07-06 19:39:35 +0800

    mfd: ti_am335x_tscadc: Use devm_platform_get_and_ioremap_resource()
    Convert platform_get_resource(), devm_ioremap_resource() to a single
    call to devm_platform_get_and_ioremap_resource(), as this is exactly
    what this function does.
    
    Signed-off-by: Yangtao Li <frank.li@vivo.com>
    Link: https://lore.kernel.org/r/20230706113939.1178-3-frank.li@vivo.com
    Signed-off-by: Lee Jones <lee@kernel.org>

--- drivers/mfd/ti_am335x_tscadc.c
+++ drivers/mfd/ti_am335x_tscadc.c
@@ -201,8 +201,7 @@ static	int ti_tscadc_probe(struct platform_device *pdev)
 	else
 		tscadc->irq = err;
 
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	tscadc->tscadc_base = devm_ioremap_resource(&pdev->dev, res);
+	tscadc->tscadc_base = devm_platform_get_and_ioremap_resource(pdev, 0, &res);
 	if (IS_ERR(tscadc->tscadc_base))
 		return PTR_ERR(tscadc->tscadc_base);
 
