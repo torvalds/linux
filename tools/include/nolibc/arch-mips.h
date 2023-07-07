6d1970e1efc8696bc8c4b8d36c9680882cb2c849
Zhangjin Wu <falcon@tinylab.org>
2023-07-07 22:55:35 +0800

    tools/nolibc: add missing my_syscall6() for mips
    It is able to pass the 6th argument like the 5th argument via the stack
    for mips, let's add a new my_syscall6() now, see [1] for details:
    
      The mips/o32 system call convention passes arguments 5 through 8 on
      the user stack.
    
    Both mmap() and pselect6() require my_syscall6().
    
    [1]: https://man7.org/linux/man-pages/man2/syscall.2.html
    
    Signed-off-by: Zhangjin Wu <falcon@tinylab.org>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- tools/include/nolibc/arch-mips.h
+++ tools/include/nolibc/arch-mips.h
@@ -166,11 +166,36 @@ struct sys_stat_struct {
 	__asm__ volatile (                                                    \
 		"addiu $sp, $sp, -32\n"                                       \
 		"sw %7, 16($sp)\n"                                            \
-		"syscall\n  "                                                 \
+		"syscall\n"                                                   \
 		"addiu $sp, $sp, 32\n"                                        \
 		: "=r" (_num), "=r"(_arg4)                                    \
 		: "0"(_num),                                                  \
 		  "r"(_arg1), "r"(_arg2), "r"(_arg3), "r"(_arg4), "r"(_arg5)  \
+		: _NOLIBC_SYSCALL_CLOBBERLIST                                 \
+	);                                                                    \
+	_arg4 ? -_num : _num;                                                 \
+})
+
+#define my_syscall6(num, arg1, arg2, arg3, arg4, arg5, arg6)                  \
+({                                                                            \
+	register long _num __asm__ ("v0")  = (num);                           \
+	register long _arg1 __asm__ ("a0") = (long)(arg1);                    \
+	register long _arg2 __asm__ ("a1") = (long)(arg2);                    \
+	register long _arg3 __asm__ ("a2") = (long)(arg3);                    \
+	register long _arg4 __asm__ ("a3") = (long)(arg4);                    \
+	register long _arg5 = (long)(arg5);                                   \
+	register long _arg6 = (long)(arg6);                                   \
+									      \
+	__asm__ volatile (                                                    \
+		"addiu $sp, $sp, -32\n"                                       \
+		"sw %7, 16($sp)\n"                                            \
+		"sw %8, 20($sp)\n"                                            \
+		"syscall\n"                                                   \
+		"addiu $sp, $sp, 32\n"                                        \
+		: "=r" (_num), "=r"(_arg4)                                    \
+		: "0"(_num),                                                  \
+		  "r"(_arg1), "r"(_arg2), "r"(_arg3), "r"(_arg4), "r"(_arg5), \
+		  "r"(_arg6)                                                  \
 		: _NOLIBC_SYSCALL_CLOBBERLIST                                 \
 	);                                                                    \
 	_arg4 ? -_num : _num;                                                 \
