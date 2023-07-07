ef28231b5716ad8882e7f12f23beb21c031a34a0
Thomas Zimmermann <tzimmermann@suse.de>
2023-07-07 10:31:56 +0200

    drm/tegra: Store pointer to vmap'ed framebuffer in screen_buffer
    Tegra uses DMA-able memory, which has to be acessed with CPU ops
    for system-memory. Store the framebuffer's vmap address in struct
    fb_info.screen_buffer. The currently used field 'screen_base' is
    for I/O memory.
    
    Suggested-by: Thierry Reding <thierry.reding@gmail.com>
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Cc: Thierry Reding <thierry.reding@gmail.com>
    Cc: Mikko Perttunen <mperttunen@nvidia.com>
    Reviewed-by: Javier Martinez Canillas <javierm@redhat.com>
    Acked-by: Thierry Reding <treding@nvidia.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230707083422.18691-6-tzimmermann@suse.de

--- drivers/gpu/drm/tegra/fbdev.c
+++ drivers/gpu/drm/tegra/fbdev.c
@@ -133,7 +133,7 @@ static int tegra_fbdev_probe(struct drm_fb_helper *helper,
 	}
 
 	info->flags |= FBINFO_VIRTFB;
-	info->screen_base = (void __iomem *)bo->vaddr + offset;
+	info->screen_buffer = bo->vaddr + offset;
 	info->screen_size = size;
 	info->fix.smem_start = (unsigned long)(bo->iova + offset);
 	info->fix.smem_len = size;
