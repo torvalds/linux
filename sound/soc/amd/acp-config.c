ef51cddf014b3e4909e9656025d1f7c2b4cc4117
Venkata Prasad Potturu <venkataprasad.potturu@amd.com>
2023-07-07 17:37:29 +0530

    ASoC: amd: Add acpi machine id's for vangogh platform
    Add acpi machine id's for vangogh platform and configure
    driver data to enable SOF sound card support on newer boards.
    
    Signed-off-by: Venkata Prasad Potturu <venkataprasad.potturu@amd.com>
    Link: https://lore.kernel.org/r/20230707120730.1948445-4-venkataprasad.potturu@amd.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/acp-config.c
+++ sound/soc/amd/acp-config.c
@@ -82,6 +82,11 @@ static struct snd_soc_acpi_codecs amp_max = {
 	.codecs = {"MX98360A"}
 };
 
+static struct snd_soc_acpi_codecs amp_max98388 = {
+	.num_codecs = 1,
+	.codecs = {"ADS8388"}
+};
+
 struct snd_soc_acpi_mach snd_soc_acpi_amd_sof_machines[] = {
 	{
 		.id = "10EC5682",
@@ -129,6 +134,20 @@ struct snd_soc_acpi_mach snd_soc_acpi_amd_sof_machines[] = {
 	{},
 };
 EXPORT_SYMBOL(snd_soc_acpi_amd_sof_machines);
+
+struct snd_soc_acpi_mach snd_soc_acpi_amd_vangogh_sof_machines[] = {
+	{
+		.id = "NVTN2020",
+		.drv_name = "nau8821-max",
+		.pdata = &acp_quirk_data,
+		.machine_quirk = snd_soc_acpi_codec_list,
+		.quirk_data = &amp_max98388,
+		.fw_filename = "sof-vangogh.ri",
+		.sof_tplg_filename = "sof-vangogh-nau8821-max.tplg",
+	},
+	{},
+};
+EXPORT_SYMBOL(snd_soc_acpi_amd_vangogh_sof_machines);
 
 struct snd_soc_acpi_mach snd_soc_acpi_amd_rmb_sof_machines[] = {
 	{
