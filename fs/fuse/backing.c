6c695fad68dc262b1d1d2f37d3f443f0a4038605
Daniel Rosenberg <drosen@google.com>
2023-07-07 16:07:09 -0700

    ANDROID: fuse-bpf: Add partial flock support
    This adds passthrough support for flock on fuse-bpf files. It does not
    give any control via a bpf filter. The flock will act as though it was
    taken on the lower file.
    
    Bug: 289882899
    Test: fuse_test -t32 (flock_test)
    Change-Id: Iba0b9630766cedbd3195532c5e929891593cfe30
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- fs/fuse/backing.c
+++ fs/fuse/backing.c
@@ -966,6 +966,20 @@ void *fuse_file_write_iter_finalize(struct fuse_bpf_args *fa,
 	return ERR_PTR(fwio->ret);
 }
 
+int fuse_file_flock_backing(struct file *file, int cmd, struct file_lock *fl)
+{
+	struct fuse_file *ff = file->private_data;
+	struct file *backing_file = ff->backing_file;
+	int error;
+
+	fl->fl_file = backing_file;
+	if (backing_file->f_op->flock)
+		error = backing_file->f_op->flock(backing_file, cmd, fl);
+	else
+		error = locks_lock_file_wait(backing_file, fl);
+	return error;
+}
+
 ssize_t fuse_backing_mmap(struct file *file, struct vm_area_struct *vma)
 {
 	int ret;
