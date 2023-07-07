8139dccd464aaee4a2c351506ff883733c6ca5a3
Ivan Babrou <ivan@cloudflare.com>
2023-07-06 21:39:20 -0700

    udp6: add a missing call into udp_fail_queue_rcv_skb tracepoint
    The tracepoint has existed for 12 years, but it only covered udp
    over the legacy IPv4 protocol. Having it enabled for udp6 removes
    the unnecessary difference in error visibility.
    
    Signed-off-by: Ivan Babrou <ivan@cloudflare.com>
    Fixes: 296f7ea75b45 ("udp: add tracepoints for queueing skb to rcvbuf")
    Acked-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/udp.c
+++ net/ipv6/udp.c
@@ -45,6 +45,7 @@
 #include <net/tcp_states.h>
 #include <net/ip6_checksum.h>
 #include <net/ip6_tunnel.h>
+#include <trace/events/udp.h>
 #include <net/xfrm.h>
 #include <net/inet_hashtables.h>
 #include <net/inet6_hashtables.h>
@@ -680,6 +681,7 @@ static int __udpv6_queue_rcv_skb(struct sock *sk, struct sk_buff *skb)
 		}
 		UDP6_INC_STATS(sock_net(sk), UDP_MIB_INERRORS, is_udplite);
 		kfree_skb_reason(skb, drop_reason);
+		trace_udp_fail_queue_rcv_skb(rc, sk);
 		return -1;
 	}
 
