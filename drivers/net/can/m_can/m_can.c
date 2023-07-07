b382380c0d2da427aaffca931e4a3b831048cace
Judith Mendez <jm@ti.com>
2023-07-07 15:47:14 -0500

    can: m_can: Add hrtimer to generate software interrupt
    Introduce timer polling method to MCAN since some SoCs may not
    have M_CAN interrupt routed to A53 Linux and do not have
    interrupt property in device tree M_CAN node.
    
    On AM62x SoC, MCANs on MCU domain do not have hardware interrupt
    routed to A53 Linux, instead they will use timer polling method.
    
    Add an hrtimer to MCAN class device. Each MCAN will have its own
    hrtimer instantiated if there is no hardware interrupt found in
    device tree M_CAN node. The timer will generate a software
    interrupt every 1 ms. In hrtimer callback, we check if there is
    a transaction pending by reading a register, then process by
    calling the isr if there is.
    
    Tested-by: Hiago De Franco <hiago.franco@toradex.com> # Toradex Verdin AM62
    Reviewed-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Judith Mendez <jm@ti.com>
    Link: https://lore.kernel.org/all/20230707204714.62964-3-jm@ti.com
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/m_can/m_can.c
+++ drivers/net/can/m_can/m_can.c
@@ -11,6 +11,7 @@
 #include <linux/bitfield.h>
 #include <linux/can/dev.h>
 #include <linux/ethtool.h>
+#include <linux/hrtimer.h>
 #include <linux/interrupt.h>
 #include <linux/io.h>
 #include <linux/iopoll.h>
@@ -307,6 +308,9 @@ enum m_can_reg {
 /* E1 */
 #define TX_EVENT_MM_MASK	GENMASK(31, 24)
 #define TX_EVENT_TXTS_MASK	GENMASK(15, 0)
+
+/* Hrtimer polling interval */
+#define HRTIMER_POLL_INTERVAL_MS		1
 
 /* The ID and DLC registers are adjacent in M_CAN FIFO memory,
  * and we can save a (potentially slow) bus round trip by combining
@@ -1414,6 +1418,12 @@ static int m_can_start(struct net_device *dev)
 
 	m_can_enable_all_interrupts(cdev);
 
+	if (!dev->irq) {
+		dev_dbg(cdev->dev, "Start hrtimer\n");
+		hrtimer_start(&cdev->hrtimer, ms_to_ktime(HRTIMER_POLL_INTERVAL_MS),
+			      HRTIMER_MODE_REL_PINNED);
+	}
+
 	return 0;
 }
 
@@ -1567,6 +1577,11 @@ static int m_can_dev_setup(struct m_can_classdev *cdev)
 static void m_can_stop(struct net_device *dev)
 {
 	struct m_can_classdev *cdev = netdev_priv(dev);
+
+	if (!dev->irq) {
+		dev_dbg(cdev->dev, "Stop hrtimer\n");
+		hrtimer_cancel(&cdev->hrtimer);
+	}
 
 	/* disable all interrupts */
 	m_can_disable_all_interrupts(cdev);
@@ -1793,6 +1808,18 @@ static netdev_tx_t m_can_start_xmit(struct sk_buff *skb,
 	return NETDEV_TX_OK;
 }
 
+static enum hrtimer_restart hrtimer_callback(struct hrtimer *timer)
+{
+	struct m_can_classdev *cdev = container_of(timer, struct
+						   m_can_classdev, hrtimer);
+
+	m_can_isr(0, cdev->net);
+
+	hrtimer_forward_now(timer, ms_to_ktime(HRTIMER_POLL_INTERVAL_MS));
+
+	return HRTIMER_RESTART;
+}
+
 static int m_can_open(struct net_device *dev)
 {
 	struct m_can_classdev *cdev = netdev_priv(dev);
@@ -1831,7 +1858,7 @@ static int m_can_open(struct net_device *dev)
 		err = request_threaded_irq(dev->irq, NULL, m_can_isr,
 					   IRQF_ONESHOT,
 					   dev->name, dev);
-	} else {
+	} else if (dev->irq) {
 		err = request_irq(dev->irq, m_can_isr, IRQF_SHARED, dev->name,
 				  dev);
 	}
@@ -2026,6 +2053,9 @@ int m_can_class_register(struct m_can_classdev *cdev)
 		if (ret)
 			goto clk_disable;
 	}
+
+	if (!cdev->net->irq)
+		cdev->hrtimer.function = &hrtimer_callback;
 
 	ret = m_can_dev_setup(cdev);
 	if (ret)
