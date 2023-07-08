c137381f71aec755fbf47cd4e9bd4dce752c054c
Suren Baghdasaryan <surenb@google.com>
2023-07-08 12:12:10 -0700

    mm: lock a vma before stack expansion
    With recent changes necessitating mmap_lock to be held for write while
    expanding a stack, per-VMA locks should follow the same rules and be
    write-locked to prevent page faults into the VMA being expanded. Add
    the necessary locking.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/mmap.c
+++ mm/mmap.c
@@ -1977,6 +1977,8 @@ static int expand_upwards(struct vm_area_struct *vma, unsigned long address)
 		return -ENOMEM;
 	}
 
+	/* Lock the VMA before expanding to prevent concurrent page faults */
+	vma_start_write(vma);
 	/*
 	 * vma->vm_start/vm_end cannot change under us because the caller
 	 * is required to hold the mmap_lock in read mode.  We need the
@@ -2064,6 +2066,8 @@ int expand_downwards(struct vm_area_struct *vma, unsigned long address)
 		return -ENOMEM;
 	}
 
+	/* Lock the VMA before expanding to prevent concurrent page faults */
+	vma_start_write(vma);
 	/*
 	 * vma->vm_start/vm_end cannot change under us because the caller
 	 * is required to hold the mmap_lock in read mode.  We need the
