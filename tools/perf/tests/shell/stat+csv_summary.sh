faae152aa6d237001e55bfcb9026cadf9531fc3c
Athira Rajeev <atrajeev@linux.vnet.ibm.com>
2023-07-09 23:57:41 +0530

    perf tests stat+csv_summary: Fix unused variable references detected via shellcheck
    Running shellcheck on stat+csv_summary.sh throws below warnings:
    
       In tests/shell/stat+csv_summary.sh line 26:
       while read num event run pct
                  ^-^ SC2034 (warning): num appears unused. Verify use (or export if used externally).
                      ^---^ SC2034 (warning): event appears unused. Verify use (or export if used externally).
                            ^-^ SC2034 (warning): run appears unused. Verify use (or export if used externally).
                                ^-^ SC2034 (warning): pct appears unused. Verify use (or export if used externally).
    
    These variables are intentionally unused since they are needed to parse
    through the output. Use "_" as a prefix for these throw away variables.
    
    Signed-off-by: Athira Rajeev <atrajeev@linux.vnet.ibm.com>
    Acked-by: Ian Rogers <irogers@google.com>
    Cc: Disha Goel <disgoel@linux.vnet.ibm.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Kajol Jain <kjain@linux.ibm.com>
    Cc: Madhavan Srinivasan <maddy@linux.ibm.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: linuxppc-dev@lists.ozlabs.org
    Link: https://lore.kernel.org/r/20230709182800.53002-8-atrajeev@linux.vnet.ibm.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/tests/shell/stat+csv_summary.sh
+++ tools/perf/tests/shell/stat+csv_summary.sh
@@ -10,7 +10,7 @@ set -e
 #
 perf stat -e cycles  -x' ' -I1000 --interval-count 1 --summary 2>&1 | \
 grep -e summary | \
-while read summary num event run pct
+while read summary _num _event _run _pct
 do
 	if [ $summary != "summary" ]; then
 		exit 1
@@ -23,7 +23,7 @@ done
 #
 perf stat -e cycles  -x' ' -I1000 --interval-count 1 --summary --no-csv-summary 2>&1 | \
 grep -e summary | \
-while read num event run pct
+while read _num _event _run _pct
 do
 	exit 1
 done
