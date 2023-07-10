b8e39b38487e68c6503419db6e4a851a0ef56de7
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2023-07-10 13:08:30 +0300

    netlink: Make use of __assign_bit() API
    We have for some time the __assign_bit() API to replace open coded
    
    	if (foo)
    		__set_bit(n, bar);
    	else
    		__clear_bit(n, bar);
    
    Use this API in the code. No functional change intended.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Alexander Lobakin <aleksander.lobakin@intel.com>
    Message-ID: <20230710100830.89936-2-andriy.shevchenko@linux.intel.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- net/netlink/af_netlink.c
+++ net/netlink/af_netlink.c
@@ -1629,10 +1629,7 @@ static void netlink_update_socket_mc(struct netlink_sock *nlk,
 
 	old = test_bit(group - 1, nlk->groups);
 	subscriptions = nlk->subscriptions - old + new;
-	if (new)
-		__set_bit(group - 1, nlk->groups);
-	else
-		__clear_bit(group - 1, nlk->groups);
+	__assign_bit(group - 1, nlk->groups, new);
 	netlink_update_subscriptions(&nlk->sk, subscriptions);
 	netlink_update_listeners(&nlk->sk);
 }
