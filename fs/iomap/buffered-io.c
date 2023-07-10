a01b8f225248e86f3328a48c3311882148a8c5d3
Ritesh Harjani (IBM) <ritesh.list@gmail.com>
2023-07-10 14:12:24 -0700

    iomap: Allocate ifs in ->write_begin() early
    We dont need to allocate an ifs in ->write_begin() for writes where the
    position and length completely overlap with the given folio.
    Therefore, such cases are skipped.
    
    Currently when the folio is uptodate, we only allocate ifs at writeback
    time (in iomap_writepage_map()). This is ok until now, but when we are
    going to add support for per-block dirty state bitmap in ifs, this
    could cause some performance degradation. The reason is that if we don't
    allocate ifs during ->write_begin(), then we will never mark the
    necessary dirty bits in ->write_end() call. And we will have to mark all
    the bits as dirty at the writeback time, that could cause the same write
    amplification and performance problems as it is now.
    
    Signed-off-by: Ritesh Harjani (IBM) <ritesh.list@gmail.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- fs/iomap/buffered-io.c
+++ fs/iomap/buffered-io.c
@@ -557,13 +557,22 @@ static int __iomap_write_begin(const struct iomap_iter *iter, loff_t pos,
 	size_t from = offset_in_folio(folio, pos), to = from + len;
 	size_t poff, plen;
 
-	if (folio_test_uptodate(folio))
+	/*
+	 * If the write completely overlaps the current folio, then
+	 * entire folio will be dirtied so there is no need for
+	 * per-block state tracking structures to be attached to this folio.
+	 */
+	if (pos <= folio_pos(folio) &&
+	    pos + len >= folio_pos(folio) + folio_size(folio))
 		return 0;
-	folio_clear_error(folio);
 
 	ifs = ifs_alloc(iter->inode, folio, iter->flags);
 	if ((iter->flags & IOMAP_NOWAIT) && !ifs && nr_blocks > 1)
 		return -EAGAIN;
+
+	if (folio_test_uptodate(folio))
+		return 0;
+	folio_clear_error(folio);
 
 	do {
 		iomap_adjust_read_range(iter->inode, folio, &block_start,
