08b0af4478bacb8bb701c172c99a34ea32da89f5
Christian Hesse <mail@eworm.de>
2023-07-10 23:16:09 +0200

    tpm/tpm_tis: Disable interrupts for Framework Laptop Intel 12th gen
    This device suffer an irq storm, so add it in tpm_tis_dmi_table to
    force polling.
    
    Cc: stable@vger.kernel.org # v6.4+
    Link: https://community.frame.work/t/boot-and-shutdown-hangs-with-arch-linux-kernel-6-4-1-mainline-and-arch/33118
    Fixes: e644b2f498d2 ("tpm, tpm_tis: Enable interrupt test")
    Reported-by: <roubro1991@gmail.com>
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=217631
    Signed-off-by: Christian Hesse <mail@eworm.de>
    Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>

--- drivers/char/tpm/tpm_tis.c
+++ drivers/char/tpm/tpm_tis.c
@@ -116,6 +116,14 @@ static int tpm_tis_disable_irq(const struct dmi_system_id *d)
 static const struct dmi_system_id tpm_tis_dmi_table[] = {
 	{
 		.callback = tpm_tis_disable_irq,
+		.ident = "Framework Laptop (12th Gen Intel Core)",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Framework"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "Laptop (12th Gen Intel Core)"),
+		},
+	},
+	{
+		.callback = tpm_tis_disable_irq,
 		.ident = "ThinkPad T490s",
 		.matches = {
 			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
