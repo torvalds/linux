b821a3c8fc1efe12782061993b4feef026f8ce58
Qais Yousef <qyousef@google.com>
2023-07-11 22:37:40 +0000

    ANDROID: fs/proc: Perform priority inheritance around access_remote_vm()
    It holds mmap_sem lock which is a hot path. Some debug daemons can end
    up holding this lock to get the cmdline of a process, which can result
    in slowing down this process.
    
    Add hooks around the calls to allow vendors to implement a simple prio
    inheritance scheme to deal with this.
    
    Bug: 289412815
    Signed-off-by: Qais Yousef <qyousef@google.com>
    Change-Id: I160637b30e5bd58d5978b25be8a21ce025175ec3

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -98,6 +98,7 @@
 #include <linux/cn_proc.h>
 #include <linux/cpufreq_times.h>
 #include <trace/events/oom.h>
+#include <trace/hooks/sched.h>
 #include "internal.h"
 #include "fd.h"
 
@@ -344,13 +345,24 @@ static ssize_t get_task_cmdline(struct task_struct *tsk, char __user *buf,
 				size_t count, loff_t *pos)
 {
 	struct mm_struct *mm;
+	bool prio_inherited = false;
+	int saved_prio;
 	ssize_t ret;
 
 	mm = get_task_mm(tsk);
 	if (!mm)
 		return 0;
 
+	/*
+	 * access_remote_vm() holds the hot mmap_sem lock which can cause the
+	 * task for which we read cmdline etc for by some debug deamon to slow
+	 * down and suffer a performance hit. Especially if the reader task has
+	 * a low nice value.
+	 */
+	trace_android_vh_prio_inheritance(tsk, &saved_prio, &prio_inherited);
 	ret = get_mm_cmdline(mm, buf, count, pos);
+	if (prio_inherited)
+		trace_android_vh_prio_restore(saved_prio);
 	mmput(mm);
 	return ret;
 }
--- kernel/sched/vendor_hooks.c
+++ kernel/sched/vendor_hooks.c
@@ -108,3 +108,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_update_load_avg);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_remove_entity_load_avg);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_update_blocked_fair);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_update_rt_rq_load_avg);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_prio_inheritance);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_prio_restore);
