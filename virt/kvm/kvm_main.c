ed8bbe6627cf3cbe57e0b731cb8a7643496cdb99
Sean Christopherson <seanjc@google.com>
2023-07-11 16:01:25 -0700

    KVM: Grab a reference to KVM for VM and vCPU stats file descriptors
    commit eed3013faa401aae662398709410a59bb0646e32 upstream.
    
    Grab a reference to KVM prior to installing VM and vCPU stats file
    descriptors to ensure the underlying VM and vCPU objects are not freed
    until the last reference to any and all stats fds are dropped.
    
    Note, the stats paths manually invoke fd_install() and so don't need to
    grab a reference before creating the file.
    
    Fixes: ce55c049459c ("KVM: stats: Support binary stats retrieval for a VCPU")
    Fixes: fcfe1baeddbf ("KVM: stats: Support binary stats retrieval for a VM")
    Reported-by: Zheng Zhang <zheng.zhang@email.ucr.edu>
    Closes: https://lore.kernel.org/all/CAC_GQSr3xzZaeZt85k_RCBd5kfiOve8qXo7a81Cq53LuVQ5r=Q@mail.gmail.com
    Cc: stable@vger.kernel.org
    Cc: Kees Cook <keescook@chromium.org>
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Message-Id: <20230711230131.648752-2-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -4032,8 +4032,17 @@ static ssize_t kvm_vcpu_stats_read(struct file *file, char __user *user_buffer,
 			sizeof(vcpu->stat), user_buffer, size, offset);
 }
 
+static int kvm_vcpu_stats_release(struct inode *inode, struct file *file)
+{
+	struct kvm_vcpu *vcpu = file->private_data;
+
+	kvm_put_kvm(vcpu->kvm);
+	return 0;
+}
+
 static const struct file_operations kvm_vcpu_stats_fops = {
 	.read = kvm_vcpu_stats_read,
+	.release = kvm_vcpu_stats_release,
 	.llseek = noop_llseek,
 };
 
@@ -4054,6 +4063,9 @@ static int kvm_vcpu_ioctl_get_stats_fd(struct kvm_vcpu *vcpu)
 		put_unused_fd(fd);
 		return PTR_ERR(file);
 	}
+
+	kvm_get_kvm(vcpu->kvm);
+
 	file->f_mode |= FMODE_PREAD;
 	fd_install(fd, file);
 
@@ -4658,8 +4670,17 @@ static ssize_t kvm_vm_stats_read(struct file *file, char __user *user_buffer,
 				sizeof(kvm->stat), user_buffer, size, offset);
 }
 
+static int kvm_vm_stats_release(struct inode *inode, struct file *file)
+{
+	struct kvm *kvm = file->private_data;
+
+	kvm_put_kvm(kvm);
+	return 0;
+}
+
 static const struct file_operations kvm_vm_stats_fops = {
 	.read = kvm_vm_stats_read,
+	.release = kvm_vm_stats_release,
 	.llseek = noop_llseek,
 };
 
@@ -4678,6 +4699,9 @@ static int kvm_vm_ioctl_get_stats_fd(struct kvm *kvm)
 		put_unused_fd(fd);
 		return PTR_ERR(file);
 	}
+
+	kvm_get_kvm(kvm);
+
 	file->f_mode |= FMODE_PREAD;
 	fd_install(fd, file);
 
