939902913a25a0feaa9ca34969dd7e5b43fc2502
Matthew Auld <matthew.auld@intel.com>
2023-07-12 16:27:21 +0100

    drm/xe/selftests: hold rpm for ccs_test_migrate()
    The GPU job will keep the device awake, however assumption here is that
    caller of xe_migrate_clear() is also holding mem_access.ref otherwise we
    hit the asserts in xe_sa_bo_flush_write() prior to the job construction.
    
    Signed-off-by: Matthew Auld <matthew.auld@intel.com>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Reviewed-by: Nirmoy Das <nirmoy.das@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/xe/tests/xe_bo.c
+++ drivers/gpu/drm/xe/tests/xe_bo.c
@@ -158,8 +158,12 @@ static int ccs_test_run_device(struct xe_device *xe)
 		return 0;
 	}
 
+	xe_device_mem_access_get(xe);
+
 	for_each_gt(gt, xe, id)
 		ccs_test_run_gt(xe, gt, test);
+
+	xe_device_mem_access_put(xe);
 
 	return 0;
 }
