af8e2ccfa6f101f505add076c1a4d56c718e0d50
Takahiro Itazuri <itazur@amazon.com>
2023-07-12 19:31:36 +0100

    KVM: x86: Advertise host CPUID 0x80000005 in KVM_GET_SUPPORTED_CPUID
    Advertise CPUID 0x80000005 (L1 cache and TLB info) to userspace so that
    VMMs that reflect KVM_GET_SUPPORTED_CPUID into KVM_SET_CPUID2 will
    enumerate sane cache/TLB information to the guest.
    
    CPUID 0x80000006 (L2 cache and TLB and L3 cache info) has been returned
    since commit 43d05de2bee7 ("KVM: pass through CPUID(0x80000006)").
    Enumerating both 0x80000005 and 0x80000006 with KVM_GET_SUPPORTED_CPUID
    is better than reporting one or the other, and 0x80000005 could be helpful
    for VMM to pass it to KVM_SET_CPUID{,2} for the same reason with
    0x80000006.
    
    Signed-off-by: Takahiro Itazuri <itazur@amazon.com>
    Link: https://lore.kernel.org/all/ZK7NmfKI9xur%2FMop@google.com
    Link: https://lore.kernel.org/r/20230712183136.85561-1-itazur@amazon.com
    [sean: add link, massage changelog]
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -1151,6 +1151,9 @@ static inline int __do_cpuid_func(struct kvm_cpuid_array *array, u32 function)
 		cpuid_entry_override(entry, CPUID_8000_0001_EDX);
 		cpuid_entry_override(entry, CPUID_8000_0001_ECX);
 		break;
+	case 0x80000005:
+		/*  Pass host L1 cache and TLB info. */
+		break;
 	case 0x80000006:
 		/* Drop reserved bits, pass host L2 cache and TLB info. */
 		entry->edx &= ~GENMASK(17, 16);
