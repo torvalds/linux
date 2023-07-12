b49e578b9314db051da0ad72bba24094193f9bd0
Christophe Leroy <christophe.leroy@csgroup.eu>
2023-07-12 23:45:02 +1000

    Revert "powerpc/bug: Provide better flexibility to WARN_ON/__WARN_FLAGS() with asm goto"
    This partly reverts commit 1e688dd2a3d6759d416616ff07afc4bb836c4213.
    
    That commit aimed at optimising the code around generation of
    WARN_ON/BUG_ON but this leads to a lot of dead code erroneously
    generated by GCC.
    
    That dead code becomes a problem when we start using objtool validation
    because objtool will abort validation with a warning as soon as it
    detects unreachable code. This is because unreachable code might
    be the indication that objtool doesn't properly decode object text.
    
         text	   data	    bss	    dec	    hex	filename
      9551585	3627834	 224376	13403795	 cc8693	vmlinux.before
      9535281	3628358	 224376	13388015	 cc48ef	vmlinux.after
    
    Once this change is reverted, in a standard configuration (pmac32 +
    function tracer) the text is reduced by 16k which is around 1.7%
    
    We already had problem with it when starting to use objtool on powerpc
    as a replacement for recordmcount, see commit 93e3f45a2631 ("powerpc:
    Fix __WARN_FLAGS() for use with Objtool")
    
    There is also a problem with at least GCC 12, on ppc64_defconfig +
    CONFIG_CC_OPTIMIZE_FOR_SIZE=y + CONFIG_DEBUG_SECTION_MISMATCH=y :
    
        LD      .tmp_vmlinux.kallsyms1
      powerpc64-linux-ld: net/ipv4/tcp_input.o:(__ex_table+0xc4): undefined reference to `.L2136'
      make[2]: *** [scripts/Makefile.vmlinux:36: vmlinux] Error 1
      make[1]: *** [/home/chleroy/linux-powerpc/Makefile:1238: vmlinux] Error 2
    
    Taking into account that other problems are encountered with that
    'asm goto' in WARN_ON(), including build failures, keeping that
    change is not worth it allthough it is primarily a compiler bug.
    
    Revert it for now.
    
    mpe: Retain EMIT_WARN_ENTRY as a synonym for EMIT_BUG_ENTRY to reduce
    churn, as there are now nearly as many uses of EMIT_WARN_ENTRY as
    EMIT_BUG_ENTRY.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    Acked-by: Naveen N Rao <naveen@kernel.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://msgid.link/20230712134552.534955-1-mpe@ellerman.id.au

--- arch/powerpc/kernel/traps.c
+++ arch/powerpc/kernel/traps.c
@@ -1508,13 +1508,8 @@ static void do_program_check(struct pt_regs *regs)
 
 		if (!(regs->msr & MSR_PR) &&  /* not user-mode */
 		    report_bug(bugaddr, regs) == BUG_TRAP_TYPE_WARN) {
-			const struct exception_table_entry *entry;
-
-			entry = search_exception_tables(bugaddr);
-			if (entry) {
-				regs_set_return_ip(regs, extable_fixup(entry) + regs->nip - bugaddr);
-				return;
-			}
+			regs_add_return_ip(regs, 4);
+			return;
 		}
 
 		if (cpu_has_feature(CPU_FTR_DEXCR_NPHIE) && user_mode(regs)) {
