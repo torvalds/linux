6806494ed4a0b71c5c44c583b3d1b7705bc60006
Breno Leitao <leitao@debian.org>
2023-07-14 02:31:46 -0700

    cxl/acpi: Return 'rc' instead of '0' in cxl_parse_cfmws()
    [ Upstream commit 91019b5bc7c2c5e6f676cce80ee6d12b2753d018 ]
    
    Driver initialization returned success (return 0) even if the
    initialization (cxl_decoder_add() or acpi_table_parse_cedt()) failed.
    
    Return the error instead of swallowing it.
    
    Fixes: f4ce1f766f1e ("cxl/acpi: Convert CFMWS parsing to ACPI sub-table helpers")
    Signed-off-by: Breno Leitao <leitao@debian.org>
    Link: https://lore.kernel.org/r/20230714093146.2253438-2-leitao@debian.org
    Reviewed-by: Alison Schofield <alison.schofield@intel.com>
    Signed-off-by: Vishal Verma <vishal.l.verma@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/cxl/acpi.c
+++ drivers/cxl/acpi.c
@@ -155,7 +155,7 @@ static int cxl_parse_cfmws(union acpi_subtable_headers *header, void *arg,
 		rc = cxl_decoder_autoremove(dev, cxld);
 	if (rc) {
 		dev_err(dev, "Failed to add decode range: %pr", res);
-		return 0;
+		return rc;
 	}
 	dev_dbg(dev, "add: %s node: %d range [%#llx - %#llx]\n",
 		dev_name(&cxld->dev),
