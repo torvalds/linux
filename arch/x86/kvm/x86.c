362ff6dca5416e20646badf09394b9a8bfc000db
Yan Zhao <yan.y.zhao@intel.com>
2023-07-14 14:52:23 +0800

    KVM: x86/mmu: Zap KVM TDP when noncoherent DMA assignment starts/stops
    Zap KVM TDP when noncoherent DMA assignment starts (noncoherent dma count
    transitions from 0 to 1) or stops (noncoherent dma count transitions
    from 1 to 0). Before the zap, test if guest MTRR is to be honored after
    the assignment starts or was honored before the assignment stops.
    
    When there's no noncoherent DMA device, EPT memory type is
    ((MTRR_TYPE_WRBACK << VMX_EPT_MT_EPTE_SHIFT) | VMX_EPT_IPAT_BIT)
    
    When there're noncoherent DMA devices, EPT memory type needs to honor
    guest CR0.CD and MTRR settings.
    
    So, if noncoherent DMA count transitions between 0 and 1, EPT leaf entries
    need to be zapped to clear stale memory type.
    
    This issue might be hidden when the device is statically assigned with
    VFIO adding/removing MMIO regions of the noncoherent DMA devices for
    several times during guest boot, and current KVM MMU will call
    kvm_mmu_zap_all_fast() on the memslot removal.
    
    But if the device is hot-plugged, or if the guest has mmio_always_on for
    the device, the MMIO regions of it may only be added for once, then there's
    no path to do the EPT entries zapping to clear stale memory type.
    
    Therefore do the EPT zapping when noncoherent assignment starts/stops to
    ensure stale entries cleaned away.
    
    Signed-off-by: Yan Zhao <yan.y.zhao@intel.com>
    Link: https://lore.kernel.org/r/20230714065223.20432-1-yan.y.zhao@intel.com
    [sean: fix misspelled words in comment and changelog]
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -13188,15 +13188,30 @@ bool noinstr kvm_arch_has_assigned_device(struct kvm *kvm)
 }
 EXPORT_SYMBOL_GPL(kvm_arch_has_assigned_device);
 
+static void kvm_noncoherent_dma_assignment_start_or_stop(struct kvm *kvm)
+{
+	/*
+	 * Non-coherent DMA assignment and de-assignment will affect
+	 * whether KVM honors guest MTRRs and cause changes in memtypes
+	 * in TDP.
+	 * So, pass %true unconditionally to indicate non-coherent DMA was,
+	 * or will be involved, and that zapping SPTEs might be necessary.
+	 */
+	if (__kvm_mmu_honors_guest_mtrrs(true))
+		kvm_zap_gfn_range(kvm, gpa_to_gfn(0), gpa_to_gfn(~0ULL));
+}
+
 void kvm_arch_register_noncoherent_dma(struct kvm *kvm)
 {
-	atomic_inc(&kvm->arch.noncoherent_dma_count);
+	if (atomic_inc_return(&kvm->arch.noncoherent_dma_count) == 1)
+		kvm_noncoherent_dma_assignment_start_or_stop(kvm);
 }
 EXPORT_SYMBOL_GPL(kvm_arch_register_noncoherent_dma);
 
 void kvm_arch_unregister_noncoherent_dma(struct kvm *kvm)
 {
-	atomic_dec(&kvm->arch.noncoherent_dma_count);
+	if (!atomic_dec_return(&kvm->arch.noncoherent_dma_count))
+		kvm_noncoherent_dma_assignment_start_or_stop(kvm);
 }
 EXPORT_SYMBOL_GPL(kvm_arch_unregister_noncoherent_dma);
 
