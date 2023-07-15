751f9a8b10cd7ea934cd0fc3b732debc5b4343b2
Thomas Zimmermann <tzimmermann@suse.de>
2023-07-15 20:51:58 +0200

    fbdev/pxafb: Remove flag FBINFO_FLAG_DEFAULT
    The flag FBINFO_FLAG_DEFAULT is 0 and has no effect, as struct
    fbinfo.flags has been allocated to zero by devm_kzalloc(). So do not
    set it.
    
    Flags should signal differences from the default values. After cleaning
    up all occurrences of FBINFO_DEFAULT, the token will be removed.
    
    v2:
    	* fix commit message (Miguel)
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Acked-by: Sam Ravnborg <sam@ravnborg.org>
    Cc: Helge Deller <deller@gmx.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230715185343.7193-17-tzimmermann@suse.de

--- drivers/video/fbdev/pxafb.c
+++ drivers/video/fbdev/pxafb.c
@@ -888,7 +888,6 @@ static void init_pxafb_overlay(struct pxafb_info *fbi, struct pxafb_layer *ofb,
 	ofb->fb.var.vmode		= FB_VMODE_NONINTERLACED;
 
 	ofb->fb.fbops			= &overlay_fb_ops;
-	ofb->fb.flags			= FBINFO_FLAG_DEFAULT;
 	ofb->fb.node			= -1;
 	ofb->fb.pseudo_palette		= NULL;
 
