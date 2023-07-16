9d1bd9346241cd6963b58da7ffb7ed303285f684
Mavroudis Chatzilazaridis <mavchatz@protonmail.com>
2023-07-16 18:23:44 +0000

    HID: logitech-dj: Add support for a new lightspeed receiver iteration
    The lightspeed receiver for the Pro X Superlight uses 13 byte mouse reports
    without a report id. The workaround for such cases has been adjusted to
    handle these larger packets.
    
    The device now reports the status of its battery in wireless mode and
    libratbag now recognizes the device and it can be configured with Piper.
    
    https://github.com/libratbag/libratbag/pull/1122
    
    Co-developed-by: Filipe Laíns <lains@riseup.net>
    Signed-off-by: Filipe Laíns <lains@riseup.net>
    Signed-off-by: Mavroudis Chatzilazaridis <mavchatz@protonmail.com>
    Reviewed-by: Bastien Nocera <hadess@hadess.net>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-logitech-dj.c
+++ drivers/hid/hid-logitech-dj.c
@@ -1692,11 +1692,12 @@ static int logi_dj_raw_event(struct hid_device *hdev,
 		}
 		/*
 		 * Mouse-only receivers send unnumbered mouse data. The 27 MHz
-		 * receiver uses 6 byte packets, the nano receiver 8 bytes.
+		 * receiver uses 6 byte packets, the nano receiver 8 bytes,
+		 * the lightspeed receiver (Pro X Superlight) 13 bytes.
 		 */
 		if (djrcv_dev->unnumbered_application == HID_GD_MOUSE &&
-		    size <= 8) {
-			u8 mouse_report[9];
+		    size <= 13){
+			u8 mouse_report[14];
 
 			/* Prepend report id */
 			mouse_report[0] = REPORT_TYPE_MOUSE;
@@ -1979,6 +1980,10 @@ static const struct hid_device_id logi_dj_receivers[] = {
 	{ /* Logitech lightspeed receiver (0xc53f) */
 	  HID_USB_DEVICE(USB_VENDOR_ID_LOGITECH,
 		USB_DEVICE_ID_LOGITECH_NANO_RECEIVER_LIGHTSPEED_1_1),
+	 .driver_data = recvr_type_gaming_hidpp},
+	{ /* Logitech lightspeed receiver (0xc547) */
+	  HID_USB_DEVICE(USB_VENDOR_ID_LOGITECH,
+		USB_DEVICE_ID_LOGITECH_NANO_RECEIVER_LIGHTSPEED_1_2),
 	 .driver_data = recvr_type_gaming_hidpp},
 
 	{ /* Logitech 27 MHz HID++ 1.0 receiver (0xc513) */
