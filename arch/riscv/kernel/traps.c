8d0be64154cf24660a947b84340e8d5bb1af855a
Guo Ren <guoren@linux.alibaba.com>
2023-07-15 20:15:05 -0400

    riscv: stack: Fixup independent irq stack for CONFIG_FRAME_POINTER=n
    The independent irq stack uses s0 to save & restore sp, but s0 would be
    corrupted when CONFIG_FRAME_POINTER=n. So add s0 in the clobber list to
    fix the problem.
    
    Fixes: 163e76cc6ef4 ("riscv: stack: Support HAVE_IRQ_EXIT_ON_IRQ_STACK")
    Cc: stable@vger.kernel.org
    Reported-by: Zhangjin Wu <falcon@tinylab.org>
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>
    Signed-off-by: Guo Ren <guoren@kernel.org>
    Tested-by: Drew Fustini <dfustini@baylibre.com>
    Link: https://lore.kernel.org/r/20230716001506.3506041-2-guoren@kernel.org
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>

--- arch/riscv/kernel/traps.c
+++ arch/riscv/kernel/traps.c
@@ -372,6 +372,9 @@ asmlinkage void noinstr do_irq(struct pt_regs *regs)
 		: [sp] "r" (sp), [regs] "r" (regs)
 		: "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7",
 		  "t0", "t1", "t2", "t3", "t4", "t5", "t6",
+#ifndef CONFIG_FRAME_POINTER
+		  "s0",
+#endif
 		  "memory");
 	} else
 #endif
