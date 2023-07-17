06b3f0bf418a7b6c266a589ef0516e6a99bc4fab
Zhihao Cheng <chengzhihao1@huawei.com>
2023-07-17 11:09:03 +0800

    ovl: let helper ovl_i_path_real() return the realinode
    [ Upstream commit b2dd05f107b11966e26fe52a313b418364cf497b ]
    
    Let helper ovl_i_path_real() return the realinode to prepare for
    checking non-null realinode in RCU walking path.
    
    [msz] Use d_inode_rcu() since we are depending on the consitency
    between dentry and inode being non-NULL in an RCU setting.
    
    There are some changes from upstream commit:
    1. Context conflicts caused by 73db6a063c785bc ("ovl: port to
       vfs{g,u}id_t and associated helpers") is handled.
    
    Signed-off-by: Zhihao Cheng <chengzhihao1@huawei.com>
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Fixes: ffa5723c6d25 ("ovl: store lower path in ovl_inode")
    Cc: <stable@vger.kernel.org> # v5.19
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/overlayfs/overlayfs.h
+++ fs/overlayfs/overlayfs.h
@@ -369,7 +369,7 @@ enum ovl_path_type ovl_path_type(struct dentry *dentry);
 void ovl_path_upper(struct dentry *dentry, struct path *path);
 void ovl_path_lower(struct dentry *dentry, struct path *path);
 void ovl_path_lowerdata(struct dentry *dentry, struct path *path);
-void ovl_i_path_real(struct inode *inode, struct path *path);
+struct inode *ovl_i_path_real(struct inode *inode, struct path *path);
 enum ovl_path_type ovl_path_real(struct dentry *dentry, struct path *path);
 enum ovl_path_type ovl_path_realdata(struct dentry *dentry, struct path *path);
 struct dentry *ovl_dentry_upper(struct dentry *dentry);
