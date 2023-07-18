288f1acf51d9844ce130d69233c2aea7999f69db
Rob Herring <robh@kernel.org>
2023-07-18 14:55:08 -0600

    fsi: Explicitly include correct DT includes
    The DT of_device.h and of_platform.h date back to the separate
    of_platform_bus_type before it as merged into the regular platform bus.
    As part of that merge prepping Arm DT support 13 years ago, they
    "temporarily" include each other. They also include platform_device.h
    and of.h. As a result, there's a pretty much random mix of those include
    files used throughout the tree. In order to detangle these headers and
    replace the implicit includes with struct declarations, users need to
    explicitly include the correct includes.
    
    Signed-off-by: Rob Herring <robh@kernel.org>
    Reviewed-by: Eddie James <eajames@linux.ibm.com>
    Link: https://lore.kernel.org/r/20230718205508.1790932-1-robh@kernel.org
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- drivers/fsi/fsi-sbefifo.c
+++ drivers/fsi/fsi-sbefifo.c
@@ -22,8 +22,8 @@
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/of.h>
-#include <linux/of_device.h>
 #include <linux/of_platform.h>
+#include <linux/platform_device.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
