0480e07330814dfbee98b4709e7e5bba7e5fb59f
Chao Yu <chao@kernel.org>
2023-07-19 21:50:46 +0800

    f2fs: remove unneeded check condition in __f2fs_setxattr()
    [ Upstream commit bc3994ffa4cf23f55171943c713366132c3ff45d ]
    
    It has checked return value of write_all_xattrs(), remove unneeded
    following check condition.
    
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Stable-dep-of: aaf8c0b9ae04 ("f2fs: reduce expensive checkpoint trigger frequency")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/f2fs/xattr.c
+++ fs/f2fs/xattr.c
@@ -772,7 +772,7 @@ retry:
 	if (index == F2FS_XATTR_INDEX_ENCRYPTION &&
 			!strcmp(name, F2FS_XATTR_NAME_ENCRYPTION_CONTEXT))
 		f2fs_set_encrypted_inode(inode);
-	if (!error && S_ISDIR(inode->i_mode))
+	if (S_ISDIR(inode->i_mode))
 		set_sbi_flag(F2FS_I_SB(inode), SBI_NEED_CP);
 
 same:
