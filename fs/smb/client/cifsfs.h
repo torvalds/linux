ba61a03af29a5f305cb301eb83b42578474508f7
Steve French <stfrench@microsoft.com>
2023-07-20 08:30:32 -0500

    cifs: update internal module version number for cifs.ko
    From 2.43 to 2.44
    
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/smb/client/cifsfs.h
+++ fs/smb/client/cifsfs.h
@@ -159,6 +159,6 @@ extern const struct export_operations cifs_export_ops;
 #endif /* CONFIG_CIFS_NFSD_EXPORT */
 
 /* when changing internal version - update following two lines at same time */
-#define SMB3_PRODUCT_BUILD 43
-#define CIFS_VERSION   "2.43"
+#define SMB3_PRODUCT_BUILD 44
+#define CIFS_VERSION   "2.44"
 #endif				/* _CIFSFS_H */
