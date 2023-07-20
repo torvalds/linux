51a5acce7162185a9b9d695b314a12fc067562b4
YueHaibing <yuehaibing@huawei.com>
2023-07-20 22:36:25 +0800

    genirq: Remove unused extern declaration
    commit 3795de236d67 ("genirq: Distangle kernel/irq/handle.c")
    left behind this.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20230720143625.29176-1-yuehaibing@huawei.com

--- kernel/irq/internals.h
+++ kernel/irq/internals.h
@@ -108,8 +108,6 @@ extern int __irq_get_irqchip_state(struct irq_data *data,
 				   enum irqchip_irq_state which,
 				   bool *state);
 
-extern void init_kstat_irqs(struct irq_desc *desc, int node, int nr);
-
 irqreturn_t __handle_irq_event_percpu(struct irq_desc *desc);
 irqreturn_t handle_irq_event_percpu(struct irq_desc *desc);
 irqreturn_t handle_irq_event(struct irq_desc *desc);
