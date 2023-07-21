b439eb8ab578557263815ba8581d02c1b730e348
Sean Christopherson <seanjc@google.com>
2023-07-21 15:43:37 -0700

    Revert "KVM: SVM: Skip WRMSR fastpath on VM-Exit if next RIP isn't valid"
    Now that handle_fastpath_set_msr_irqoff() acquires kvm->srcu, i.e. allows
    dereferencing memslots during WRMSR emulation, drop the requirement that
    "next RIP" is valid.  In hindsight, acquiring kvm->srcu would have been a
    better fix than avoiding the pastpath, but at the time it was thought that
    accessing SRCU-protected data in the fastpath was a one-off edge case.
    
    This reverts commit 5c30e8101e8d5d020b1d7119117889756a6ed713.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20230721224337.2335137-3-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -3986,14 +3986,8 @@ static int svm_vcpu_pre_run(struct kvm_vcpu *vcpu)
 
 static fastpath_t svm_exit_handlers_fastpath(struct kvm_vcpu *vcpu)
 {
-	struct vmcb_control_area *control = &to_svm(vcpu)->vmcb->control;
-
-	/*
-	 * Note, the next RIP must be provided as SRCU isn't held, i.e. KVM
-	 * can't read guest memory (dereference memslots) to decode the WRMSR.
-	 */
-	if (control->exit_code == SVM_EXIT_MSR && control->exit_info_1 &&
-	    nrips && control->next_rip)
+	if (to_svm(vcpu)->vmcb->control.exit_code == SVM_EXIT_MSR &&
+	    to_svm(vcpu)->vmcb->control.exit_info_1)
 		return handle_fastpath_set_msr_irqoff(vcpu);
 
 	return EXIT_FASTPATH_NONE;
