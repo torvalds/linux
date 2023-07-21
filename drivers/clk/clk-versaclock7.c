fccd617f65e587e9a255538d5d0c662e489d9fb2
Biju Das <biju.das.jz@bp.renesas.com>
2023-07-21 08:00:19 +0100

    clk: vc7: Use i2c_get_match_data() instead of device_get_match_data()
    The device_get_match_data(), is to get match data for firmware interfaces
    such as just OF/ACPI. This driver has I2C matching table as well. Use
    i2c_get_match_data() to get match data for I2C, ACPI and DT-based
    matching.
    
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>
    Reviewed-by: Marek Vasut <marek.vasut+renesas@mailbox.org>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://lore.kernel.org/r/20230721070019.96627-3-biju.das.jz@bp.renesas.com
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/clk-versaclock7.c
+++ drivers/clk/clk-versaclock7.c
@@ -1109,7 +1109,7 @@ static int vc7_probe(struct i2c_client *client)
 
 	i2c_set_clientdata(client, vc7);
 	vc7->client = client;
-	vc7->chip_info = device_get_match_data(&client->dev);
+	vc7->chip_info = i2c_get_match_data(client);
 
 	vc7->pin_xin = devm_clk_get(&client->dev, "xin");
 	if (PTR_ERR(vc7->pin_xin) == -EPROBE_DEFER) {
