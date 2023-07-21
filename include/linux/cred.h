ca22eca6e2ad7eaed1c791628ef7cb4c739e3da6
YueHaibing <yuehaibing@huawei.com>
2023-07-21 20:28:40 +0800

    cred: remove unsued extern declaration change_create_files_as()
    Since commit 3a3b7ce93369 ("CRED: Allow kernel services to override LSM settings for task actions")
    this is never used, so can be removed.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Fixes: 3a3b7ce93369 ("CRED: Allow kernel services to override LSM settings for task actions")
    [PM: subject tweak, fixes tag]
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- include/linux/cred.h
+++ include/linux/cred.h
@@ -164,7 +164,6 @@ extern void abort_creds(struct cred *);
 extern const struct cred *override_creds(const struct cred *);
 extern void revert_creds(const struct cred *);
 extern struct cred *prepare_kernel_cred(struct task_struct *);
-extern int change_create_files_as(struct cred *, struct inode *);
 extern int set_security_override(struct cred *, u32);
 extern int set_security_override_from_ctx(struct cred *, const char *);
 extern int set_create_files_as(struct cred *, struct inode *);
