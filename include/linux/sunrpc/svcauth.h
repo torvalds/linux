f80774787aa2b719d9c5f2d67a5901b59f219ce7
YueHaibing <yuehaibing@huawei.com>
2023-07-22 11:31:16 +0800

    sunrpc: Remove unused extern declarations
    Since commit 49b28684fdba ("nfsd: Remove deprecated nfsctl system call and related code.")
    these declarations are unused, so can remove it.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- include/linux/sunrpc/svcauth.h
+++ include/linux/sunrpc/svcauth.h
@@ -157,11 +157,8 @@ extern void	svc_auth_unregister(rpc_authflavor_t flavor);
 
 extern struct auth_domain *unix_domain_find(char *name);
 extern void auth_domain_put(struct auth_domain *item);
-extern int auth_unix_add_addr(struct net *net, struct in6_addr *addr, struct auth_domain *dom);
 extern struct auth_domain *auth_domain_lookup(char *name, struct auth_domain *new);
 extern struct auth_domain *auth_domain_find(char *name);
-extern struct auth_domain *auth_unix_lookup(struct net *net, struct in6_addr *addr);
-extern int auth_unix_forget_old(struct auth_domain *dom);
 extern void svcauth_unix_purge(struct net *net);
 extern void svcauth_unix_info_release(struct svc_xprt *xpt);
 extern int svcauth_unix_set_client(struct svc_rqst *rqstp);
