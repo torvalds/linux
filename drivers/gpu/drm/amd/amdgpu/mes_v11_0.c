8a92e8676cfbe4de018a60a2870de37188fc75c6
Bob Zhou <bob.zhou@amd.com>
2023-07-24 10:46:35 +0800

    drm/amdgpu: remove repeat code for mes_add_queue_pkt
    The setting of mes_add_queue_pkt is repeated, so remove it.
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/mes_v11_0.c
+++ drivers/gpu/drm/amd/amdgpu/mes_v11_0.c
@@ -210,10 +210,6 @@ static int mes_v11_0_add_hw_queue(struct amdgpu_mes *mes,
 	mes_add_queue_pkt.is_aql_queue = input->is_aql_queue;
 	mes_add_queue_pkt.gds_size = input->queue_size;
 
-	/* For KFD, gds_size is re-used for queue size (needed in MES for AQL queues) */
-	mes_add_queue_pkt.is_aql_queue = input->is_aql_queue;
-	mes_add_queue_pkt.gds_size = input->queue_size;
-
 	mes_add_queue_pkt.exclusively_scheduled = input->exclusively_scheduled;
 
 	return mes_v11_0_submit_pkt_and_poll_completion(mes,
