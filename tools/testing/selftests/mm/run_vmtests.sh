f6dd4e223d8798319d0e2815a468b9fb0a276446
Ryan Roberts <ryan.roberts@arm.com>
2023-07-24 09:25:16 +0100

    selftests/mm: skip soft-dirty tests on arm64
    arm64 does not support the soft-dirty PTE bit.  However, the `soft-dirty`
    test suite is currently run unconditionally and therefore generates
    spurious test failures on arm64.  There are also some tests in
    `madv_populate` which assume it is supported.
    
    For `soft-dirty` lets disable the whole suite for arm64; it is no longer
    built and run_vmtests.sh will skip it if its not present.
    
    For `madv_populate`, we need a runtime mechanism so that the remaining
    tests continue to be run.  Unfortunately, the only way to determine if the
    soft-dirty dirty bit is supported is to write to a page, then see if the
    bit is set in /proc/self/pagemap.  But the tests that we want to
    conditionally execute are testing precicesly this.  So if we introduced
    this feature check, we could accedentally turn a real failure (on a system
    that claims to support soft-dirty) into a skip.  So instead, do the check
    based on architecture; for arm64, we report that soft-dirty is not
    supported.
    
    Link: https://lkml.kernel.org/r/20230724082522.1202616-3-ryan.roberts@arm.com
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Cc: Florent Revest <revest@chromium.org>
    Cc: Jérôme Glisse <jglisse@redhat.com>
    Cc: John Hubbard <jhubbard@nvidia.com>
    Cc: Mark Brown <broonie@kernel.org>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Shuah Khan <shuah@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- tools/testing/selftests/mm/run_vmtests.sh
+++ tools/testing/selftests/mm/run_vmtests.sh
@@ -322,7 +322,10 @@ then
 	CATEGORY="pkey" run_test ./protection_keys_64
 fi
 
-CATEGORY="soft_dirty" run_test ./soft-dirty
+if [ -x ./soft-dirty ]
+then
+	CATEGORY="soft_dirty" run_test ./soft-dirty
+fi
 
 # COW tests
 CATEGORY="cow" run_test ./cow
