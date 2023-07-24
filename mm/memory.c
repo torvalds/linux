ffcebdef16579f8957e116726ab5425bb22fbede
Matthew Wilcox (Oracle) <willy@infradead.org>
2023-07-24 19:54:08 +0100

    FROMGIT: mm: run the fault-around code under the VMA lock
    The map_pages fs method should be safe to run under the VMA lock instead
    of the mmap lock.  This should have a measurable reduction in contention
    on the mmap lock.
    
    Link: https://lkml.kernel.org/r/20230724185410.1124082-9-willy@infradead.org
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Reviewed-by: Suren Baghdasaryan <surenb@google.com>
    Cc: Arjun Roy <arjunroy@google.com>
    Cc: Eric Dumazet <edumazet@google.com>
    Cc: Punit Agrawal <punit.agrawal@bytedance.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (cherry picked from commit 7456c15600264d635293c91df1e0c0b5a1e73578
    https: //git.kernel.org/pub/scm/linux/kernel/git/akpm/mm.git mm-unstable)
    
    Bug: 293665307
    Change-Id: Iaa1b0c2deeade361b34118f41b5deb591268a269
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- mm/memory.c
+++ mm/memory.c
@@ -4585,11 +4585,6 @@ static vm_fault_t do_read_fault(struct vm_fault *vmf)
 {
 	vm_fault_t ret = 0;
 
-	if (vmf->flags & FAULT_FLAG_VMA_LOCK) {
-		vma_end_read(vmf->vma);
-		return VM_FAULT_RETRY;
-	}
-
 	/*
 	 * Let's call ->map_pages() first and use ->fault() as fallback
 	 * if page by the offset is not ready to be mapped (cold cache or
@@ -4599,6 +4594,11 @@ static vm_fault_t do_read_fault(struct vm_fault *vmf)
 		ret = do_fault_around(vmf);
 		if (ret)
 			return ret;
+	}
+
+	if (vmf->flags & FAULT_FLAG_VMA_LOCK) {
+		vma_end_read(vmf->vma);
+		return VM_FAULT_RETRY;
 	}
 
 	ret = __do_fault(vmf);
