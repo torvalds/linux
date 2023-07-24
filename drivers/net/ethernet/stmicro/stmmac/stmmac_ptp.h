20bf98c94146eb6fe62177817cb32f53e72dd2e8
Johannes Zink <j.zink@pengutronix.de>
2023-07-24 12:01:31 +0200

    net: stmmac: correct MAC propagation delay
    The IEEE1588 Standard specifies that the timestamps of Packets must be
    captured when the PTP message timestamp point (leading edge of first
    octet after the start of frame delimiter) crosses the boundary between
    the node and the network. As the MAC latches the timestamp at an
    internal point, the captured timestamp must be corrected for the
    additional path latency, as described in the publicly available
    datasheet [1].
    
    This patch only corrects for the MAC-Internal delay, which can be read
    out from the MAC_Ingress_Timestamp_Latency register, since the Phy
    framework currently does not support querying the Phy ingress and egress
    latency. The Closs Domain Crossing Circuits errors as indicated in [1]
    are already being accounted in the stmmac_get_tx_hwtstamp() function and
    are not corrected here.
    
    As the Latency varies for different link speeds and MII
    modes of operation, the correction value needs to be updated on each
    link state change.
    
    As the delay also causes a phase shift in the timestamp counter compared
    to the rest of the network, this correction will also reduce phase error
    when generating PPS outputs from the timestamp counter.
    
    [1] i.MX8MP Reference Manual, rev.1 Section 11.7.2.5.3 "Timestamp
    correction"
    
    Signed-off-by: Johannes Zink <j.zink@pengutronix.de>
    Link: https://lore.kernel.org/r/20230719-stmmac_correct_mac_delay-v2-1-3366f38ee9a6@pengutronix.de
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_ptp.h
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_ptp.h
@@ -26,6 +26,12 @@
 #define	PTP_ACR		0x40	/* Auxiliary Control Reg */
 #define	PTP_ATNR	0x48	/* Auxiliary Timestamp - Nanoseconds Reg */
 #define	PTP_ATSR	0x4c	/* Auxiliary Timestamp - Seconds Reg */
+#define	PTP_TS_INGR_CORR_NS	0x58	/* Ingress timestamp correction nanoseconds */
+#define	PTP_TS_EGR_CORR_NS	0x5C	/* Egress timestamp correction nanoseconds*/
+#define	PTP_TS_INGR_CORR_SNS	0x60	/* Ingress timestamp correction subnanoseconds */
+#define	PTP_TS_EGR_CORR_SNS	0x64	/* Egress timestamp correction subnanoseconds */
+#define	PTP_TS_INGR_LAT	0x68	/* MAC internal Ingress Latency */
+#define	PTP_TS_EGR_LAT	0x6c	/* MAC internal Egress Latency */
 
 #define	PTP_STNSUR_ADDSUB_SHIFT	31
 #define	PTP_DIGITAL_ROLLOVER_MODE	0x3B9ACA00	/* 10e9-1 ns */
