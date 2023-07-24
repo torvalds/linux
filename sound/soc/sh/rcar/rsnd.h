547b02f74e4ac1e7d295a6266d5bc93a647cd4ac
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2023-07-24 00:21:39 +0000

    ASoC: rsnd: enable multi Component support for Audio Graph Card/Card2
    	+-- Basic Board ---------+
    	|+--------+      +------+|
    	|| CPU ch0| <--> |CodecA||
    	||     ch1| <-+  +------+|
    	|+--------+   |          |
    	+-------------|----------+
    	+-- expansion board -----+
    	|             |  +------+|
    	|             +->|CodecB||
    	|                +------+|
    	+------------------------+
    
    In above HW connection case, we intuitively think we want to handle these
    as "2 Sound Cards".
    
    	card0,0: CPU-ch0 - CodecA
    	card1,0: CPU-ch1 - CodecB
    
    But, we needed to handle it as "1 big Sound Card", because of
    Component vs Card limitation.
    
    	card0,0: CPU-ch0 - CodecA
    	card0,1: CPU-ch1 - CodecB
    
    This patch enables multi Component to handle multi Cards.
    To support it, it needs
    
    	- Fill dai_args for each DAI on snd_soc_dai_driver
    	- Parse DT for each Component (Simple Card/Audio Graph Card)
    
    Ex) Simple Card
    
    	rcar_sound {
    		...
    
    		/* Component0 */
    		rcar_sound,dai@0 {
    			...
    		};
    
    		/* Component1 */
    		rcar_sound,dai@1 {
    			...
    		};
    	};
    
    Ex) Audio Graph Card/Card2
    
    	rcar_sound {
    		/* Component0 */
    		ports@0 {
    			...
    		};
    
    		/* Component1 */
    		ports@1 {
    			...
    		};
    	};
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://lore.kernel.org/r/87tttub1m4.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sh/rcar/rsnd.h
+++ sound/soc/sh/rcar/rsnd.h
@@ -545,6 +545,7 @@ struct rsnd_dai {
 	struct rsnd_dai_stream capture;
 	struct rsnd_priv *priv;
 	struct snd_pcm_hw_constraint_list constraint;
+	struct of_phandle_args dai_args;
 
 	int max_channels;	/* 2ch - 16ch */
 	int ssi_lane;		/* 1lane - 4lane */
@@ -702,6 +703,9 @@ struct rsnd_priv {
 	struct snd_soc_dai_driver *daidrv;
 	struct rsnd_dai *rdai;
 	int rdai_nr;
+
+#define RSND_MAX_COMPONENT 3
+	int component_dais[RSND_MAX_COMPONENT];
 };
 
 #define rsnd_priv_to_pdev(priv)	((priv)->pdev)
