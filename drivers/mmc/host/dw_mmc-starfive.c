c8b7f3c2395cc8f96baeda0574da7d07973464bf
William Qiu <william.qiu@starfivetech.com>
2023-07-26 16:26:42 +0800

    mmc: starfive: change the tuning method
    Previously, the system controller register was used to do tuning, but it
    was only useful for HS200, so now change the tuning mode to use the
    UHS_REG_EXT register, and delete the previous code about syscon
    
    Signed-off-by: William Qiu <william.qiu@starfivetech.com>

--- drivers/mmc/host/dw_mmc-starfive.c
+++ drivers/mmc/host/dw_mmc-starfive.c
@@ -52,67 +52,60 @@ static void dw_mci_starfive_set_ios(struct dw_mci *host, struct mmc_ios *ios)
 	}
 }
 
+static void dw_mci_starfive_hs_set_bits(struct dw_mci *host, u32 smpl_phase)
+{
+	/* change driver phase and sample phase */
+	u32 mask = 0x1f;
+	u32 reg_value;
+
+	reg_value = mci_readl(host, UHS_REG_EXT);
+
+	/* In UHS_REG_EXT, only 5 bits valid in DRV_PHASE and SMPL_PHASE */
+	reg_value &= ~(mask << 16);
+	reg_value |= (smpl_phase << 16);
+	mci_writel(host, UHS_REG_EXT, reg_value);
+
+	/* We should delay 1ms wait for timing setting finished. */
+	udelay(1000);
+}
+
 static int dw_mci_starfive_execute_tuning(struct dw_mci_slot *slot,
 					     u32 opcode)
 {
 	static const int grade  = MAX_DELAY_CHAIN;
 	struct dw_mci *host = slot->host;
-	struct starfive_priv *priv = host->priv;
-	int raise_point = -1, fall_point = -1;
-	int err, prev_err = -1;
-	int found = 0;
+	int err = -1;
+	int smpl_phase, smpl_raise = -1, smpl_fall = -1;
 	int i;
-	u32 regval;
 
 	for (i = 0; i < grade; i++) {
-		regval = i << priv->syscon_shift;
-		err = regmap_update_bits(priv->reg_syscon, priv->syscon_offset, priv->syscon_mask, regval);
-		if (err)
-			return err;
+		smpl_phase = i;
+		dw_mci_starfive_hs_set_bits(host, smpl_phase);
 		mci_writel(host, RINTSTS, ALL_INT_CLR);
 
 		err = mmc_send_tuning(slot->mmc, opcode, NULL);
-		if (!err)
-			found = 1;
 
-		if (i > 0) {
-			if (err && !prev_err)
-				fall_point = i - 1;
-			if (!err && prev_err)
-				raise_point = i;
+		if (!err && smpl_raise < 0)
+			smpl_raise = i;
+		else if (err && smpl_raise >= 0) {
+			smpl_fall = i - 1;
+			break;
 		}
-
-		if (raise_point != -1 && fall_point != -1)
-			goto tuning_out;
-
-		prev_err = err;
-		err = 0;
 	}
 
-tuning_out:
-	if (found) {
-		if (raise_point == -1)
-			raise_point = 0;
-		if (fall_point == -1)
-			fall_point = grade - 1;
-		if (fall_point < raise_point) {
-			if ((raise_point + fall_point) >
-			    (grade - 1))
-				i = fall_point / 2;
-			else
-				i = (raise_point + grade - 1) / 2;
-		} else {
-			i = (raise_point + fall_point) / 2;
-		}
+	if (i >= grade && smpl_raise >= 0)
+		smpl_fall = grade - 1 ;
 
-		regval = i << priv->syscon_shift;
-		err = regmap_update_bits(priv->reg_syscon, priv->syscon_offset, priv->syscon_mask, regval);
-		if (err)
-			return err;
-		dev_dbg(host->dev, "Found valid delay chain! use it [delay=%d]\n", i);
-	} else {
+	if (smpl_raise < 0) {
 		dev_err(host->dev, "No valid delay chain! use default\n");
+		dw_mci_starfive_hs_set_bits(host, 0);
 		err = -EINVAL;
+	}
+	else {
+		smpl_phase = (smpl_raise + smpl_fall) / 2;
+		dw_mci_starfive_hs_set_bits(host, smpl_phase);
+		dev_dbg(host->dev, "Found valid delay chain! use it [delay=%d]\n", smpl_phase);
+		err = 0;
 	}
 
 	mci_writel(host, RINTSTS, ALL_INT_CLR);
@@ -146,42 +139,10 @@ static int dw_mci_starfive_switch_voltage(struct mmc_host *mmc, struct mmc_ios *
 	return 0;
 }
 
-static int dw_mci_starfive_parse_dt(struct dw_mci *host)
-{
-	struct of_phandle_args args;
-	struct starfive_priv *priv;
-	int ret;
-
-	priv = devm_kzalloc(host->dev, sizeof(*priv), GFP_KERNEL);
-	if (!priv)
-		return -ENOMEM;
-
-	ret = of_parse_phandle_with_fixed_args(host->dev->of_node,
-						"starfive,sys-syscon", 3, 0, &args);
-	if (ret) {
-		dev_err(host->dev, "Failed to parse starfive,sys-syscon\n");
-		return -EINVAL;
-	}
-
-	priv->reg_syscon = syscon_node_to_regmap(args.np);
-	of_node_put(args.np);
-	if (IS_ERR(priv->reg_syscon))
-		return PTR_ERR(priv->reg_syscon);
-
-	priv->syscon_offset = args.args[0];
-	priv->syscon_shift  = args.args[1];
-	priv->syscon_mask   = args.args[2];
-
-	host->priv = priv;
-
-	return 0;
-}
-
 static const struct dw_mci_drv_data starfive_data = {
 	.caps = dw_mci_starfive_caps,
 	.num_caps = ARRAY_SIZE(dw_mci_starfive_caps),
 	.set_ios = dw_mci_starfive_set_ios,
-	.parse_dt = dw_mci_starfive_parse_dt,
 	.execute_tuning = dw_mci_starfive_execute_tuning,
 	.switch_voltage  = dw_mci_starfive_switch_voltage,
 };
