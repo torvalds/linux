823de40c94d680ec8d57a660ac96a653acadb0a5
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2023-07-26 20:59:31 +0530

    PCI: qcom-ep: Treat unknown IRQ events as an error
    Sometimes, the Qcom PCIe EP controller can receive some interrupts
    unknown to the driver, like safety interrupts in newer SoCs. In those
    cases, if the driver doesn't clear the interrupts, it will end up in an
    interrupt storm. However, the users will not know about it because the
    log is treated as a debug message.
    
    So let's treat the unknown event log as an error so that it at least
    makes the user aware, thereby getting fixed eventually.
    
    [kwilczynski: commit log]
    Link: https://lore.kernel.org/linux-pci/20230726152931.18134-1-manivannan.sadhasivam@linaro.org
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Signed-off-by: Krzysztof Wilczyński <kwilczynski@kernel.org>

--- drivers/pci/controller/dwc/pcie-qcom-ep.c
+++ drivers/pci/controller/dwc/pcie-qcom-ep.c
@@ -593,7 +593,7 @@ static irqreturn_t qcom_pcie_ep_global_irq_thread(int irq, void *data)
 		dw_pcie_ep_linkup(&pci->ep);
 		pcie_ep->link_status = QCOM_PCIE_EP_LINK_UP;
 	} else {
-		dev_dbg(dev, "Received unknown event: %d\n", status);
+		dev_err(dev, "Received unknown event: %d\n", status);
 	}
 
 	return IRQ_HANDLED;
