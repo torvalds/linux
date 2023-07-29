b21f187ff1d032d7541fe441587da1e650b5907e
Thomas Zimmermann <tzimmermann@suse.de>
2023-07-29 21:26:48 +0200

    fbdev: Use _DMAMEM_ infix for DMA-memory helpers
    Change the infix for fbdev's DMA-memory helpers from _DMA_ to
    _DMAMEM_. The helpers perform operations within DMA-able memory,
    but they don't perform DMA operations. Naming should make this
    clear. Adapt all users. No functional changes.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Sam Ravnborg <sam@ravnborg.org>
    Acked-by: Helge Deller <deller@gmx.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230729193157.15446-4-tzimmermann@suse.de

--- include/linux/fb.h
+++ include/linux/fb.h
@@ -579,11 +579,11 @@ extern ssize_t fb_sys_write(struct fb_info *info, const char __user *buf,
  * Helpers for framebuffers in DMA-able memory
  */
 
-#define __FB_DEFAULT_DMA_OPS_RDWR \
+#define __FB_DEFAULT_DMAMEM_OPS_RDWR \
 	.fb_read	= fb_sys_read, \
 	.fb_write	= fb_sys_write
 
-#define __FB_DEFAULT_DMA_OPS_DRAW \
+#define __FB_DEFAULT_DMAMEM_OPS_DRAW \
 	.fb_fillrect	= sys_fillrect, \
 	.fb_copyarea	= sys_copyarea, \
 	.fb_imageblit	= sys_imageblit
