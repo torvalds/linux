a32fa4359a7b7766631549db427a183c5c6d032e
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2023-07-28 17:51:15 -0700

    Input: ad7879 - use device core to create driver-specific device attributes
    Instead of creating driver-specific device attributes with
    devm_device_add_group() have device core do this by setting up dev_groups
    pointer in the driver structure.
    
    Acked-by: Michael Hennerich <michael.hennerich@analog.com>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://lore.kernel.org/r/20230729005133.1095051-6-dmitry.torokhov@gmail.com
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/touchscreen/ad7879-i2c.c
+++ drivers/input/touchscreen/ad7879-i2c.c
@@ -58,9 +58,10 @@ MODULE_DEVICE_TABLE(of, ad7879_i2c_dt_ids);
 
 static struct i2c_driver ad7879_i2c_driver = {
 	.driver = {
-		.name	= "ad7879",
-		.pm	= &ad7879_pm_ops,
-		.of_match_table = of_match_ptr(ad7879_i2c_dt_ids),
+		.name		= "ad7879",
+		.dev_groups	= ad7879_groups,
+		.pm		= &ad7879_pm_ops,
+		.of_match_table	= of_match_ptr(ad7879_i2c_dt_ids),
 	},
 	.probe		= ad7879_i2c_probe,
 	.id_table	= ad7879_id,
