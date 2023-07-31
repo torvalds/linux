b2fd72aafb13118503df6798b18ed28f83dda2c2
Ruan Jinjie <ruanjinjie@huawei.com>
2023-07-31 20:44:55 +0800

    wifi: ath9k: Remove unnecessary ternary operators
    Ther are a little ternary operators, the true or false judgement
    of which is unnecessary in C language semantics.
    
    Signed-off-by: Ruan Jinjie <ruanjinjie@huawei.com>
    Acked-by: Toke Høiland-Jørgensen <toke@toke.dk>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://lore.kernel.org/r/20230731124455.2039184-1-ruanjinjie@huawei.com

--- drivers/net/wireless/ath/ath9k/htc_drv_main.c
+++ drivers/net/wireless/ath/ath9k/htc_drv_main.c
@@ -719,7 +719,7 @@ static int ath9k_htc_tx_aggr_oper(struct ath9k_htc_priv *priv,
 
 	aggr.sta_index = ista->index;
 	aggr.tidno = tid & 0xf;
-	aggr.aggr_enable = (action == IEEE80211_AMPDU_TX_START) ? true : false;
+	aggr.aggr_enable = action == IEEE80211_AMPDU_TX_START;
 
 	WMI_CMD_BUF(WMI_TX_AGGR_ENABLE_CMDID, &aggr);
 	if (ret)
