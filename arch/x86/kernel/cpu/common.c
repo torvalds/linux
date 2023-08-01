c956807d8462e94a1450dc0737728c25917b1d67
Thomas Gleixner <tglx@linutronix.de>
2023-08-01 16:36:25 +0200

    x86/init: Initialize signal frame size late
    commit 54d9a91a3d6713d1332e93be13b4eaf0fa54349d upstream
    
    No point in doing this during really early boot. Move it to an early
    initcall so that it is set up before possible user mode helpers are started
    during device initialization.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20230613224545.727330699@linutronix.de
    Signed-off-by: Daniel Sneddon <daniel.sneddon@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -63,7 +63,6 @@
 #include <asm/cpu_device_id.h>
 #include <asm/uv/uv.h>
 #include <asm/set_memory.h>
-#include <asm/sigframe.h>
 #include <asm/traps.h>
 #include <asm/sev.h>
 
@@ -1577,8 +1576,6 @@ static void __init early_identify_cpu(struct cpuinfo_x86 *c)
 	sld_setup(c);
 
 	fpu__init_system(c);
-
-	init_sigframe_size();
 
 #ifdef CONFIG_X86_32
 	/*
