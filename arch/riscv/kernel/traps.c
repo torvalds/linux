52449c17bdd1540940e21511612b58acebc49c06
Celeste Liu <coelacanthushex@gmail.com>
2023-08-01 22:15:16 +0800

    riscv: entry: set a0 = -ENOSYS only when syscall != -1
    When we test seccomp with 6.4 kernel, we found errno has wrong value.
    If we deny NETLINK_AUDIT with EAFNOSUPPORT, after f0bddf50586d, we will
    get ENOSYS instead. We got same result with commit 9c2598d43510 ("riscv:
    entry: Save a0 prior syscall_enter_from_user_mode()").
    
    After analysing code, we think that regs->a0 = -ENOSYS should only be
    executed when syscall != -1. In __seccomp_filter, when seccomp rejected
    this syscall with specified errno, they will set a0 to return number as
    syscall ABI, and then return -1. This return number is finally pass as
    return number of syscall_enter_from_user_mode, and then is compared with
    NR_syscalls after converted to ulong (so it will be ULONG_MAX). The
    condition syscall < NR_syscalls will always be false, so regs->a0 = -ENOSYS
    is always executed. It covered a0 set by seccomp, so we always get
    ENOSYS when match seccomp RET_ERRNO rule.
    
    Fixes: f0bddf50586d ("riscv: entry: Convert to generic entry")
    Reported-by: Felix Yan <felixonmars@archlinux.org>
    Co-developed-by: Ruizhe Pan <c141028@gmail.com>
    Signed-off-by: Ruizhe Pan <c141028@gmail.com>
    Co-developed-by: Shiqi Zhang <shiqi@isrc.iscas.ac.cn>
    Signed-off-by: Shiqi Zhang <shiqi@isrc.iscas.ac.cn>
    Signed-off-by: Celeste Liu <CoelacanthusHex@gmail.com>
    Tested-by: Felix Yan <felixonmars@archlinux.org>
    Tested-by: Emil Renner Berthing <emil.renner.berthing@canonical.com>
    Reviewed-by: Björn Töpel <bjorn@rivosinc.com>
    Reviewed-by: Guo Ren <guoren@kernel.org>
    Link: https://lore.kernel.org/r/20230801141607.435192-1-CoelacanthusHex@gmail.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>

--- arch/riscv/kernel/traps.c
+++ arch/riscv/kernel/traps.c
@@ -297,7 +297,7 @@ asmlinkage __visible __trap_section void do_trap_break(struct pt_regs *regs)
 asmlinkage __visible __trap_section void do_trap_ecall_u(struct pt_regs *regs)
 {
 	if (user_mode(regs)) {
-		ulong syscall = regs->a7;
+		long syscall = regs->a7;
 
 		regs->epc += 4;
 		regs->orig_a0 = regs->a0;
@@ -306,9 +306,9 @@ asmlinkage __visible __trap_section void do_trap_ecall_u(struct pt_regs *regs)
 
 		syscall = syscall_enter_from_user_mode(regs, syscall);
 
-		if (syscall < NR_syscalls)
+		if (syscall >= 0 && syscall < NR_syscalls)
 			syscall_handler(regs, syscall);
-		else
+		else if (syscall != -1)
 			regs->a0 = -ENOSYS;
 
 		syscall_exit_to_user_mode(regs);
