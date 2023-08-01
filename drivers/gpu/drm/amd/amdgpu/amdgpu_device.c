2e9b152325f649923b9324fa8ea5f1a5289145bb
Perry Yuan <perry.yuan@amd.com>
2023-08-01 10:37:41 -0400

    drm/amdgpu: optimize RLC powerdown notification on Vangogh
    The smu needs to get the rlc power down message to sync the rlc state
    with smu, the rlc state updating message need to be sent at while smu
    begin suspend sequence , otherwise SMU will crash while RLC state is not
    notified by driver, and rlc state probally changed after that
    notification, so it needs to notify rlc state to smu at the end of the
    suspend sequence in amdgpu_device_suspend() that can make sure the rlc
    state  is correctly set to SMU.
    
    [  101.000590] amdgpu 0000:03:00.0: amdgpu: SMU: I'm not done with your previous command: SMN_C2PMSG_66:0x0000001E SMN_C2PMSG_82:0x00000000
    [  101.000598] amdgpu 0000:03:00.0: amdgpu: Failed to disable gfxoff!
    [  110.838026] amdgpu 0000:03:00.0: amdgpu: SMU: I'm not done with your previous command: SMN_C2PMSG_66:0x0000001E SMN_C2PMSG_82:0x00000000
    [  110.838035] amdgpu 0000:03:00.0: amdgpu: Failed to disable smu features.
    [  110.838039] amdgpu 0000:03:00.0: amdgpu: Fail to disable dpm features!
    [  110.838040] [drm:amdgpu_device_ip_suspend_phase2 [amdgpu]] *ERROR* suspend of IP block <smu> failed -62
    [  110.884394] PM: suspend of devices aborted after 21213.620 msecs
    [  110.884402] PM: start suspend of devices aborted after 21213.882 msecs
    [  110.884405] PM: Some devices failed to suspend, or early wake event detected
    
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Perry Yuan <perry.yuan@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -4549,6 +4549,10 @@ int amdgpu_device_suspend(struct drm_device *dev, bool fbcon)
 	if (amdgpu_sriov_vf(adev))
 		amdgpu_virt_release_full_gpu(adev, false);
 
+	r = amdgpu_dpm_notify_rlc_state(adev, false);
+	if (r)
+		return r;
+
 	return 0;
 }
 
