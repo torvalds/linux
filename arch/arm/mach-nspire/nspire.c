e4011d83f2c72a5ce94d0d89e47d9e597884f6e1
Andrew Davis <afd@ti.com>
2023-08-02 10:33:33 -0500

    ARM: nspire: Remove unused header file mmio.h
    The Nspire boardfile platform drivers have all been converted. None
    of the definitions in this header are used anymore. Remove it.
    
    While here lets remove all the other unused headers and the file
    name from in the file itself.
    
    Signed-off-by: Andrew Davis <afd@ti.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-nspire/nspire.c
+++ arch/arm/mach-nspire/nspire.c
@@ -1,16 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- *	linux/arch/arm/mach-nspire/nspire.c
- *
  *	Copyright (C) 2013 Daniel Tang <tangrs@tangrs.id.au>
  */
-#include <linux/init.h>
-#include <linux/io.h>
 
 #include <asm/mach/arch.h>
-#include <asm/mach-types.h>
-
-#include "mmio.h"
 
 static const char *const nspire_dt_match[] __initconst = {
 	"ti,nspire",
