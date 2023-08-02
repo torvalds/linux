876397837d582ce72f977ac3e635ce74eebcecc9
Matthew Wilcox (Oracle) <willy@infradead.org>
2023-08-02 16:13:41 +0100

    ia64: implement the new page table range API
    Add PFN_PTE_SHIFT, update_mmu_cache_range() and flush_dcache_folio(). 
    Change the PG_arch_1 (aka PG_dcache_clean) flag from being per-page to
    per-folio, which makes arch_dma_mark_clean() and mark_clean() a little
    more exciting.
    
    [willy@infradead.org: fix folio_size() handling]
      Link: https://lkml.kernel.org/r/ZNPlOCe8F+nrzPxr@casper.infradead.org
    Link: https://lkml.kernel.org/r/20230802151406.3735276-14-willy@infradead.org
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Acked-by: Mike Rapoport (IBM) <rppt@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/ia64/include/asm/pgtable.h
+++ arch/ia64/include/asm/pgtable.h
@@ -206,6 +206,7 @@ ia64_phys_addr_valid (unsigned long addr)
 #define RGN_MAP_SHIFT (PGDIR_SHIFT + PTRS_PER_PGD_SHIFT - 3)
 #define RGN_MAP_LIMIT	((1UL << RGN_MAP_SHIFT) - PAGE_SIZE)	/* per region addr limit */
 
+#define PFN_PTE_SHIFT	PAGE_SHIFT
 /*
  * Conversion functions: convert page frame number (pfn) and a protection value to a page
  * table entry (pte).
@@ -303,8 +304,6 @@ static inline void set_pte(pte_t *ptep, pte_t pteval)
 	*ptep = pteval;
 }
 
-#define set_pte_at(mm,addr,ptep,pteval) set_pte(ptep,pteval)
-
 /*
  * Make page protection values cacheable, uncacheable, or write-
  * combining.  Note that "protection" is really a misnomer here as the
@@ -396,6 +395,7 @@ pte_same (pte_t a, pte_t b)
 	return pte_val(a) == pte_val(b);
 }
 
+#define update_mmu_cache_range(vmf, vma, address, ptep, nr) do { } while (0)
 #define update_mmu_cache(vma, address, ptep) do { } while (0)
 
 extern pgd_t swapper_pg_dir[PTRS_PER_PGD];
