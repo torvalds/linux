bb0173a1daaef0d6afa1ddaa193add4b9f93ac2f
Linyu Yuan <quic_linyyuan@quicinc.com>
2023-08-03 17:10:50 +0800

    BACKPORT: usb: gadget: unconditionally allocate hs/ss descriptor in bind operation
    Take f_midi_bind() for example,  when composite layer call it, it will
    allocate hs descriptor by calling gadget_is_dualspeed() API to check
    gadget max support speed capability, but most other gadget function didn't
    do like this.
    
    To follow other function drivers, it is safe to remove the check which
    mean support all possible link speed by default in function driver.
    
    Similar change apply to midi2 and uvc.
    
    Also in midi and midi2, as there is no descriptor difference between
    super speed and super speed plus, follow other gadget function drivers,
    do not allocate descriptor for super speed plus, composite layer will
    handle it properly.
    
    Signed-off-by: Linyu Yuan <quic_linyyuan@quicinc.com>
    Link: https://lore.kernel.org/r/20230803091053.9714-5-quic_linyyuan@quicinc.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Bug: 301887900
    Change-Id: I8b287266b3973c66e559bd3eea9adbe95e051be7
    (cherry picked from commit 46decc82ffd54212cc2c600031daec6e835a6503
     https://kernel.googlesource.com/pub/scm/linux/kernel/git/gregkh/usb usb-next)
    [arakesh: f_midi2.c doesn't exist, so dropped those changes]
    Signed-off-by: Avichal Rakesh <arakesh@google.com>

--- drivers/usb/gadget/function/f_uvc.c
+++ drivers/usb/gadget/function/f_uvc.c
@@ -718,21 +718,19 @@ uvc_function_bind(struct usb_configuration *c, struct usb_function *f)
 		f->fs_descriptors = NULL;
 		goto error;
 	}
-	if (gadget_is_dualspeed(cdev->gadget)) {
-		f->hs_descriptors = uvc_copy_descriptors(uvc, USB_SPEED_HIGH);
-		if (IS_ERR(f->hs_descriptors)) {
-			ret = PTR_ERR(f->hs_descriptors);
-			f->hs_descriptors = NULL;
-			goto error;
-		}
+
+	f->hs_descriptors = uvc_copy_descriptors(uvc, USB_SPEED_HIGH);
+	if (IS_ERR(f->hs_descriptors)) {
+		ret = PTR_ERR(f->hs_descriptors);
+		f->hs_descriptors = NULL;
+		goto error;
 	}
-	if (gadget_is_superspeed(c->cdev->gadget)) {
-		f->ss_descriptors = uvc_copy_descriptors(uvc, USB_SPEED_SUPER);
-		if (IS_ERR(f->ss_descriptors)) {
-			ret = PTR_ERR(f->ss_descriptors);
-			f->ss_descriptors = NULL;
-			goto error;
-		}
+
+	f->ss_descriptors = uvc_copy_descriptors(uvc, USB_SPEED_SUPER);
+	if (IS_ERR(f->ss_descriptors)) {
+		ret = PTR_ERR(f->ss_descriptors);
+		f->ss_descriptors = NULL;
+		goto error;
 	}
 
 	/* Preallocate control endpoint request. */
