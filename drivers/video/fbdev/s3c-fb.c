588a25e016e25deb0247f19f10dffe73f24f3fff
Thomas Zimmermann <tzimmermann@suse.de>
2023-08-03 20:36:02 +0200

    fbdev/s3cfb: Use fbdev I/O helpers
    Set struct fb_ops and with FB_DEFAULT_IOMEM_OPS, fbdev's initializer
    for I/O memory. Sets the callbacks to the cfb_ and fb_io_ functions.
    Select the correct modules with Kconfig's FB_IOMEM_HELPERS token.
    
    The macro and token set the currently selected values, so there is
    no functional change.
    
    v3:
    	* use _IOMEM_ in commit message
    v2:
    	* updated to use _IOMEM_ tokens
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Sam Ravnborg <sam@ravnborg.org>
    Acked-by: Helge Deller <deller@gmx.de>
    Cc: Jingoo Han <jingoohan1@gmail.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230803184034.6456-38-tzimmermann@suse.de

--- drivers/video/fbdev/s3c-fb.c
+++ drivers/video/fbdev/s3c-fb.c
@@ -1038,13 +1038,11 @@ static int s3c_fb_ioctl(struct fb_info *info, unsigned int cmd,
 
 static const struct fb_ops s3c_fb_ops = {
 	.owner		= THIS_MODULE,
+	FB_DEFAULT_IOMEM_OPS,
 	.fb_check_var	= s3c_fb_check_var,
 	.fb_set_par	= s3c_fb_set_par,
 	.fb_blank	= s3c_fb_blank,
 	.fb_setcolreg	= s3c_fb_setcolreg,
-	.fb_fillrect	= cfb_fillrect,
-	.fb_copyarea	= cfb_copyarea,
-	.fb_imageblit	= cfb_imageblit,
 	.fb_pan_display	= s3c_fb_pan_display,
 	.fb_ioctl	= s3c_fb_ioctl,
 };
