220965d15cef417a6d6ecfbd41c64d582522f48c
Li Zetao <lizetao1@huawei.com>
2023-08-03 11:23:53 +0800

    tty: serial: Remove redundant initialization for ma35d1serial_driver
    There is a warning reported by coccinelle:
    
    ./drivers/tty/serial/ma35d1_serial.c:791:3-8:
    	No need to set .owner here. The core will do it.
    
    The module_platform_driver() will set "THIS_MODULE" to driver.owner
    when register a driver for platform-level devices, so it is redundant
    initialization to set driver.owner in ma35d1serial_driver statement.
    Remove it to silence the warning.
    
    Signed-off-by: Li Zetao <lizetao1@huawei.com>
    Link: https://lore.kernel.org/r/20230803032353.3045221-1-lizetao1@huawei.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/ma35d1_serial.c
+++ drivers/tty/serial/ma35d1_serial.c
@@ -788,7 +788,6 @@ static struct platform_driver ma35d1serial_driver = {
 	.resume     = ma35d1serial_resume,
 	.driver     = {
 		.name   = "ma35d1-uart",
-		.owner  = THIS_MODULE,
 		.of_match_table = of_match_ptr(ma35d1_serial_of_match),
 	},
 };
