b9a281f1f72b9786a4cb04b4105c121f3333aabf
Rob Herring <robh@kernel.org>
2023-08-04 08:48:44 -0600

    hwrng: xgene: Add explicit io.h include
    Commit 0788257aeebe ("hwrng: Explicitly include correct DT includes")
    removed an implicit include of io.h. On most architectures, there's
    still an implicit include of it, but not on s390. Enabling COMPILE_TEST
    in commit 1ce1cd8208ad ("hwrng: Enable COMPILE_TEST for more drivers")
    exposed this.
    
    Fixes: 0788257aeebe ("hwrng: Explicitly include correct DT includes")
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202308042049.8R2tNRoo-lkp@intel.com/
    Signed-off-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/char/hw_random/xgene-rng.c
+++ drivers/char/hw_random/xgene-rng.c
@@ -14,6 +14,7 @@
 #include <linux/hw_random.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
+#include <linux/io.h>
 #include <linux/module.h>
 #include <linux/mod_devicetable.h>
 #include <linux/platform_device.h>
