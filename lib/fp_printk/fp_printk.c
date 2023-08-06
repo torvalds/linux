751d3a4cf84b68db61491399e3561e2a863eb4c1
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2023-08-06 15:31:42 -0300

    refactor: in module init test of fp_printk()

--- lib/fp_printk/fp_printk.c
+++ lib/fp_printk/fp_printk.c
@@ -44,6 +44,13 @@ EXPORT_SYMBOL(fp_printk);
 static int __init fp_printk_init(void)
 {
 	pr_info("%s: initial execute module", OURMODNAME);
+	int n = 123334;
+	char *s = fp_printk(n, 4);
+	if(s){
+		pr_info("%s: test for fp_printk: %s", OURMODNAME, s);
+		kfree(s);
+	}
+
 	return 0;
 }
 
