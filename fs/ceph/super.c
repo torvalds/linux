56d2e2cfa21315c12945c22e141c7e7ec8b0a630
Christian Brauner <brauner@kernel.org>
2023-08-07 15:26:26 +0200

    ceph: allow idmapped mounts
    Now that we converted cephfs internally to account for idmapped mounts
    allow the creation of idmapped mounts on by setting the FS_ALLOW_IDMAP
    flag.
    
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>
    Reviewed-by: Xiubo Li <xiubli@redhat.com>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- fs/ceph/super.c
+++ fs/ceph/super.c
@@ -1582,7 +1582,7 @@ static struct file_system_type ceph_fs_type = {
 	.name		= "ceph",
 	.init_fs_context = ceph_init_fs_context,
 	.kill_sb	= ceph_kill_sb,
-	.fs_flags	= FS_RENAME_DOES_D_MOVE,
+	.fs_flags	= FS_RENAME_DOES_D_MOVE | FS_ALLOW_IDMAP,
 };
 MODULE_ALIAS_FS("ceph");
 
