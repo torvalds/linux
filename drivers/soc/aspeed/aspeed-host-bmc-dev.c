adcf900672314b5616636364a13e13a5c43be210
Ryan Chen <ryan_chen@aspeedtech.com>
2023-08-08 13:44:15 +0800

    soc:aspeed: update sync with ast2700 host-bmc-dev
    Signed-off-by: Ryan Chen <ryan_chen@aspeedtech.com>
    Change-Id: I408fde7b9d257d4a99ef341451e4271dedb6f496

--- drivers/soc/aspeed/aspeed-host-bmc-dev.c
+++ drivers/soc/aspeed/aspeed-host-bmc-dev.c
@@ -6,49 +6,55 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/errno.h>
-
 #include <linux/pci.h>
 #include <linux/file.h>
 #include <linux/fs.h>
 #include <linux/interrupt.h>
 #include <linux/wait.h>
 #include <linux/workqueue.h>
-
-
 #include <linux/miscdevice.h>
 #include <linux/module.h>
 #include <linux/serial_core.h>
 #include <linux/serial_8250.h>
 
-#define ASPEED_PCI_BMC_HOST2BMC_Q1		0x30000
-#define ASPEED_PCI_BMC_HOST2BMC_Q2		0x30010
-#define ASPEED_PCI_BMC_BMC2HOST_Q1		0x30020
-#define ASPEED_PCI_BMC_BMC2HOST_Q2		0x30030
-#define ASPEED_PCI_BMC_BMC2HOST_STS		0x30040
-#define	 BMC2HOST_INT_STS_DOORBELL		BIT(31)
-#define	 BMC2HOST_ENABLE_INTB			BIT(30)
+#define PCI_BMC_HOST2BMC_Q1		0x30000
+#define PCI_BMC_HOST2BMC_Q2		0x30010
+#define PCI_BMC_BMC2HOST_Q1		0x30020
+#define PCI_BMC_BMC2HOST_Q2		0x30030
+#define PCI_BMC_BMC2HOST_STS		0x30040
+#define	 BMC2HOST_INT_STS_DOORBELL	BIT(31)
+#define	 BMC2HOST_ENABLE_INTB		BIT(30)
 /* */
-#define	 BMC2HOST_Q1_FULL				BIT(27)
-#define	 BMC2HOST_Q1_EMPTY				BIT(26)
-#define	 BMC2HOST_Q2_FULL				BIT(25)
-#define	 BMC2HOST_Q2_EMPTY				BIT(24)
-#define	 BMC2HOST_Q1_FULL_UNMASK		BIT(23)
-#define	 BMC2HOST_Q1_EMPTY_UNMASK		BIT(22)
-#define	 BMC2HOST_Q2_FULL_UNMASK		BIT(21)
-#define	 BMC2HOST_Q2_EMPTY_UNMASK		BIT(20)
+#define	 BMC2HOST_Q1_FULL		BIT(27)
+#define	 BMC2HOST_Q1_EMPTY		BIT(26)
+#define	 BMC2HOST_Q2_FULL		BIT(25)
+#define	 BMC2HOST_Q2_EMPTY		BIT(24)
+#define	 BMC2HOST_Q1_FULL_UNMASK	BIT(23)
+#define	 BMC2HOST_Q1_EMPTY_UNMASK	BIT(22)
+#define	 BMC2HOST_Q2_FULL_UNMASK	BIT(21)
+#define	 BMC2HOST_Q2_EMPTY_UNMASK	BIT(20)
 
-#define ASPEED_PCI_BMC_HOST2BMC_STS		0x30044
-#define	 HOST2BMC_INT_STS_DOORBELL		BIT(31)
-#define	 HOST2BMC_ENABLE_INTB			BIT(30)
+#define PCI_BMC_HOST2BMC_STS		0x30044
+#define	 HOST2BMC_INT_STS_DOORBELL	BIT(31)
+#define	 HOST2BMC_ENABLE_INTB		BIT(30)
 /* */
-#define	 HOST2BMC_Q1_FULL				BIT(27)
-#define	 HOST2BMC_Q1_EMPTY				BIT(26)
-#define	 HOST2BMC_Q2_FULL				BIT(25)
-#define	 HOST2BMC_Q2_EMPTY				BIT(24)
-#define	 HOST2BMC_Q1_FULL_UNMASK		BIT(23)
-#define	 HOST2BMC_Q1_EMPTY_UNMASK		BIT(22)
-#define	 HOST2BMC_Q2_FULL_UNMASK		BIT(21)
-#define	 HOST2BMC_Q2_EMPTY_UNMASK		BIT(20)
+#define	 HOST2BMC_Q1_FULL		BIT(27)
+#define	 HOST2BMC_Q1_EMPTY		BIT(26)
+#define	 HOST2BMC_Q2_FULL		BIT(25)
+#define	 HOST2BMC_Q2_EMPTY		BIT(24)
+#define	 HOST2BMC_Q1_FULL_UNMASK	BIT(23)
+#define	 HOST2BMC_Q1_EMPTY_UNMASK	BIT(22)
+#define	 HOST2BMC_Q2_FULL_UNMASK	BIT(21)
+#define	 HOST2BMC_Q2_EMPTY_UNMASK	BIT(20)
+
+enum msi_index {
+	BMC_MSI,
+	MBX_MSI,
+	VUART_MSI,
+};
+
+#define MSI_INDX 3
+#define VUART_MAX_PARMS		2
 
 struct aspeed_pci_bmc_dev {
 	struct device *dev;
@@ -61,6 +67,8 @@ struct aspeed_pci_bmc_dev {
 	unsigned long message_bar_base;
 	unsigned long message_bar_size;
 	void __iomem *msg_bar_reg;
+
+	void __iomem *pcie_sio_decode_addr;
 
 	struct bin_attribute	bin0;
 	struct bin_attribute	bin1;
@@ -76,8 +84,8 @@ struct aspeed_pci_bmc_dev {
 
 	void __iomem *sio_mbox_reg;
 	int sio_mbox_irq;
-
-	u8 IntLine;
+	struct uart_8250_port uart[VUART_MAX_PARMS];
+	int ast2600_msi_idx[MSI_INDX];
 	int legency_irq;
 };
 
@@ -87,9 +95,8 @@ struct aspeed_pci_bmc_dev {
 
 #define DRIVER_NAME "ASPEED BMC DEVICE"
 
-#define VUART_MAX_PARMS		2
-static uint16_t vuart_ioport[VUART_MAX_PARMS];
-static uint16_t vuart_sirq[VUART_MAX_PARMS];
+static u16 vuart_ioport[VUART_MAX_PARMS];
+static u16 vuart_sirq[VUART_MAX_PARMS];
 
 static struct aspeed_pci_bmc_dev *file_aspeed_bmc_device(struct file *file)
 {
@@ -109,8 +116,8 @@ static int aspeed_pci_bmc_dev_mmap(struct file *file, struct vm_area_struct *vma
 	prot = pgprot_noncached(prot);
 
 	if (remap_pfn_range(vma, vma->vm_start,
-		(pci_bmc_dev->mem_bar_base >> PAGE_SHIFT) + vma->vm_pgoff,
-		vsize, prot))
+			    (pci_bmc_dev->mem_bar_base >> PAGE_SHIFT) + vma->vm_pgoff,
+			    vsize, prot))
 		return -EAGAIN;
 
 	return 0;
@@ -121,44 +128,56 @@ static const struct file_operations aspeed_pci_bmc_dev_fops = {
 	.mmap		= aspeed_pci_bmc_dev_mmap,
 };
 
-static ssize_t aspeed_pci_bmc_dev_queue1_rx(struct file *filp, struct kobject *kobj,
-		struct bin_attribute *attr, char *buf, loff_t off, size_t count)
+static ssize_t
+aspeed_pci_bmc_dev_queue1_rx(struct file *filp, struct kobject *kobj,
+			     struct bin_attribute *attr, char *buf, loff_t off, size_t count)
 {
-	struct aspeed_pci_bmc_dev *pci_bmc_device = dev_get_drvdata(container_of(kobj, struct device, kobj));
-	u32 *data = (u32 *) buf;
+	struct aspeed_pci_bmc_dev *pci_bmc_device = dev_get_drvdata(container_of(kobj,
+								    struct device, kobj));
+	u32 *data = (u32 *)buf;
 	int ret;
 
 	ret = wait_event_interruptible(pci_bmc_device->rx_wait0,
-		!(readl(pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_BMC2HOST_STS) & BMC2HOST_Q1_EMPTY));
+				       !(readl(pci_bmc_device->msg_bar_reg + PCI_BMC_BMC2HOST_STS)
+					       & BMC2HOST_Q1_EMPTY));
 	if (ret)
 		return -EINTR;
 
-	data[0] = readl(pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_BMC2HOST_Q1);
-	writel(HOST2BMC_INT_STS_DOORBELL | HOST2BMC_ENABLE_INTB, pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_HOST2BMC_STS);
+	data[0] = readl(pci_bmc_device->msg_bar_reg + PCI_BMC_BMC2HOST_Q1);
+	writel(HOST2BMC_INT_STS_DOORBELL | HOST2BMC_ENABLE_INTB,
+	       pci_bmc_device->msg_bar_reg + PCI_BMC_HOST2BMC_STS);
+
 	return sizeof(u32);
 }
 
-static ssize_t aspeed_pci_bmc_dev_queue2_rx(struct file *filp, struct kobject *kobj,
-		struct bin_attribute *attr, char *buf, loff_t off, size_t count)
+static ssize_t
+aspeed_pci_bmc_dev_queue2_rx(struct file *filp, struct kobject *kobj,
+			     struct bin_attribute *attr, char *buf, loff_t off, size_t count)
 {
-	struct aspeed_pci_bmc_dev *pci_bmc_device = dev_get_drvdata(container_of(kobj, struct device, kobj));
-	u32 *data = (u32 *) buf;
+	struct aspeed_pci_bmc_dev *pci_bmc_device = dev_get_drvdata(container_of(kobj,
+								    struct device, kobj));
+	u32 *data = (u32 *)buf;
 	int ret;
 
 	ret = wait_event_interruptible(pci_bmc_device->rx_wait1,
-		!(readl(pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_BMC2HOST_STS) & BMC2HOST_Q2_EMPTY));
+				       !(readl(pci_bmc_device->msg_bar_reg + PCI_BMC_BMC2HOST_STS)
+					       & BMC2HOST_Q2_EMPTY));
 	if (ret)
 		return -EINTR;
 
-	data[0] = readl(pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_BMC2HOST_Q2);
-	writel(HOST2BMC_INT_STS_DOORBELL | HOST2BMC_ENABLE_INTB, pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_HOST2BMC_STS);
+	data[0] = readl(pci_bmc_device->msg_bar_reg + PCI_BMC_BMC2HOST_Q2);
+	writel(HOST2BMC_INT_STS_DOORBELL | HOST2BMC_ENABLE_INTB,
+	       pci_bmc_device->msg_bar_reg + PCI_BMC_HOST2BMC_STS);
+
 	return sizeof(u32);
 }
 
-static ssize_t aspeed_pci_bmc_dev_queue1_tx(struct file *filp, struct kobject *kobj,
-		struct bin_attribute *attr, char *buf, loff_t off, size_t count)
+static ssize_t
+aspeed_pci_bmc_dev_queue1_tx(struct file *filp, struct kobject *kobj,
+			     struct bin_attribute *attr, char *buf, loff_t off, size_t count)
 {
-	struct aspeed_pci_bmc_dev *pci_bmc_device = dev_get_drvdata(container_of(kobj, struct device, kobj));
+	struct aspeed_pci_bmc_dev *pci_bmc_device = dev_get_drvdata(container_of(kobj,
+								    struct device, kobj));
 	u32 tx_buff;
 	int ret;
 
@@ -166,22 +185,26 @@ static ssize_t aspeed_pci_bmc_dev_queue1_tx(struct file *filp, struct kobject *k
 		return -EINVAL;
 
 	ret = wait_event_interruptible(pci_bmc_device->tx_wait0,
-		!(readl(pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_HOST2BMC_STS) & HOST2BMC_Q1_FULL));
+				       !(readl(pci_bmc_device->msg_bar_reg + PCI_BMC_HOST2BMC_STS)
+					       & HOST2BMC_Q1_FULL));
 	if (ret)
 		return -EINTR;
 
 	memcpy(&tx_buff, buf, 4);
-	writel(tx_buff, pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_HOST2BMC_Q1);
+	writel(tx_buff, pci_bmc_device->msg_bar_reg + PCI_BMC_HOST2BMC_Q1);
 	//trigger to host
-	writel(HOST2BMC_INT_STS_DOORBELL | HOST2BMC_ENABLE_INTB, pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_HOST2BMC_STS);
+	writel(HOST2BMC_INT_STS_DOORBELL | HOST2BMC_ENABLE_INTB,
+	       pci_bmc_device->msg_bar_reg + PCI_BMC_HOST2BMC_STS);
 
 	return sizeof(u32);
 }
 
-static ssize_t aspeed_pci_bmc_dev_queue2_tx(struct file *filp, struct kobject *kobj,
-		struct bin_attribute *attr, char *buf, loff_t off, size_t count)
+static ssize_t
+aspeed_pci_bmc_dev_queue2_tx(struct file *filp, struct kobject *kobj,
+			     struct bin_attribute *attr, char *buf, loff_t off, size_t count)
 {
-	struct aspeed_pci_bmc_dev *pci_bmc_device = dev_get_drvdata(container_of(kobj, struct device, kobj));
+	struct aspeed_pci_bmc_dev *pci_bmc_device = dev_get_drvdata(container_of(kobj,
+								    struct device, kobj));
 	u32 tx_buff = 0;
 	int ret;
 
@@ -189,14 +212,16 @@ static ssize_t aspeed_pci_bmc_dev_queue2_tx(struct file *filp, struct kobject *k
 		return -EINVAL;
 
 	ret = wait_event_interruptible(pci_bmc_device->tx_wait0,
-		!(readl(pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_HOST2BMC_STS) & HOST2BMC_Q2_FULL));
+				       !(readl(pci_bmc_device->msg_bar_reg + PCI_BMC_HOST2BMC_STS)
+					       & HOST2BMC_Q2_FULL));
 	if (ret)
 		return -EINTR;
 
 	memcpy(&tx_buff, buf, 4);
-	writel(tx_buff, pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_HOST2BMC_Q2);
+	writel(tx_buff, pci_bmc_device->msg_bar_reg + PCI_BMC_HOST2BMC_Q2);
 	//trigger to host
-	writel(HOST2BMC_INT_STS_DOORBELL | HOST2BMC_ENABLE_INTB, pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_HOST2BMC_STS);
+	writel(HOST2BMC_INT_STS_DOORBELL | HOST2BMC_ENABLE_INTB,
+	       pci_bmc_device->msg_bar_reg + PCI_BMC_HOST2BMC_STS);
 
 	return sizeof(u32);
 }
@@ -204,13 +229,14 @@ static ssize_t aspeed_pci_bmc_dev_queue2_tx(struct file *filp, struct kobject *k
 irqreturn_t aspeed_pci_host_bmc_device_interrupt(int irq, void *dev_id)
 {
 	struct aspeed_pci_bmc_dev *pci_bmc_device = dev_id;
-	u32 bmc2host_q_sts = readl(pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_BMC2HOST_STS);
+	u32 bmc2host_q_sts = readl(pci_bmc_device->msg_bar_reg + PCI_BMC_BMC2HOST_STS);
 
 	if (bmc2host_q_sts & BMC2HOST_INT_STS_DOORBELL)
-		writel(BMC2HOST_INT_STS_DOORBELL, pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_BMC2HOST_STS);
+		writel(BMC2HOST_INT_STS_DOORBELL,
+		       pci_bmc_device->msg_bar_reg + PCI_BMC_BMC2HOST_STS);
 
 	if (bmc2host_q_sts & BMC2HOST_ENABLE_INTB)
-		writel(BMC2HOST_ENABLE_INTB, pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_BMC2HOST_STS);
+		writel(BMC2HOST_ENABLE_INTB, pci_bmc_device->msg_bar_reg + PCI_BMC_BMC2HOST_STS);
 
 	if (bmc2host_q_sts & BMC2HOST_Q1_FULL)
 		dev_info(pci_bmc_device->dev, "Q1 Full\n");
@@ -218,22 +244,20 @@ irqreturn_t aspeed_pci_host_bmc_device_interrupt(int irq, void *dev_id)
 	if (bmc2host_q_sts & BMC2HOST_Q2_FULL)
 		dev_info(pci_bmc_device->dev, "Q2 Full\n");
 
-
 	//check q1
-	if (!(readl(pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_HOST2BMC_STS) & HOST2BMC_Q1_FULL))
+	if (!(readl(pci_bmc_device->msg_bar_reg + PCI_BMC_HOST2BMC_STS) & HOST2BMC_Q1_FULL))
 		wake_up_interruptible(&pci_bmc_device->tx_wait0);
 
-	if (!(readl(pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_BMC2HOST_STS) & BMC2HOST_Q1_EMPTY))
+	if (!(readl(pci_bmc_device->msg_bar_reg + PCI_BMC_BMC2HOST_STS) & BMC2HOST_Q1_EMPTY))
 		wake_up_interruptible(&pci_bmc_device->rx_wait0);
 	//chech q2
-	if (!(readl(pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_HOST2BMC_STS) & HOST2BMC_Q2_FULL))
+	if (!(readl(pci_bmc_device->msg_bar_reg + PCI_BMC_HOST2BMC_STS) & HOST2BMC_Q2_FULL))
 		wake_up_interruptible(&pci_bmc_device->tx_wait1);
 
-	if (!(readl(pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_BMC2HOST_STS) & BMC2HOST_Q2_EMPTY))
+	if (!(readl(pci_bmc_device->msg_bar_reg + PCI_BMC_BMC2HOST_STS) & BMC2HOST_Q2_EMPTY))
 		wake_up_interruptible(&pci_bmc_device->rx_wait1);
 
 	return IRQ_HANDLED;
-
 }
 
 irqreturn_t aspeed_pci_host_mbox_interrupt(int irq, void *dev_id)
@@ -245,24 +269,12 @@ irqreturn_t aspeed_pci_host_mbox_interrupt(int irq, void *dev_id)
 		writel(BIT(7), pci_bmc_device->sio_mbox_reg + 0x94);
 
 	return IRQ_HANDLED;
-
 }
-
-enum msi_index {
-	BMC_MSI,
-	MBX_MSI,
-	VUART_MSI,
-};
-
-#define MSI_INDX 3
 
 static int aspeed_pci_host_bmc_device_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 {
-	struct uart_8250_port uart[VUART_MAX_PARMS];
 	struct aspeed_pci_bmc_dev *pci_bmc_dev;
 	struct device *dev = &pdev->dev;
-	int ast2600_msi_idx[MSI_INDX];
-	void __iomem *pcie_sio_decode_addr;
 	u16 config_cmd_val;
 	int nr_entries;
 	int rc = 0;
@@ -287,17 +299,17 @@ static int aspeed_pci_host_bmc_device_probe(struct pci_dev *pdev, const struct p
 	pci_set_master(pdev);
 
 	if (pdev->revision == 0x27) {
-		ast2600_msi_idx[BMC_MSI] = 0;
-		ast2600_msi_idx[MBX_MSI] = 11;
-		ast2600_msi_idx[VUART_MSI] = 6;
+		pci_bmc_dev->ast2600_msi_idx[BMC_MSI] = 0;
+		pci_bmc_dev->ast2600_msi_idx[MBX_MSI] = 11;
+		pci_bmc_dev->ast2600_msi_idx[VUART_MSI] = 6;
 	} else {
-		ast2600_msi_idx[BMC_MSI] = 4;
-		ast2600_msi_idx[MBX_MSI] = 21;
-		ast2600_msi_idx[VUART_MSI] = 16;
+		pci_bmc_dev->ast2600_msi_idx[BMC_MSI] = 4;
+		pci_bmc_dev->ast2600_msi_idx[MBX_MSI] = 21;
+		pci_bmc_dev->ast2600_msi_idx[VUART_MSI] = 16;
 	}
 
 	nr_entries = pci_alloc_irq_vectors(pdev, 1, BMC_MULTI_MSI,
-				PCI_IRQ_MSIX | PCI_IRQ_MSI);
+					   PCI_IRQ_MSIX | PCI_IRQ_MSI);
 	if (nr_entries < 0) {
 		pci_bmc_dev->legency_irq = 1;
 		pci_read_config_word(pdev, PCI_COMMAND, &config_cmd_val);
@@ -309,7 +321,7 @@ static int aspeed_pci_host_bmc_device_probe(struct pci_dev *pdev, const struct p
 		pci_read_config_word(pdev, PCI_COMMAND, &config_cmd_val);
 		config_cmd_val |= PCI_COMMAND_INTX_DISABLE;
 		pci_write_config_word((struct pci_dev *)pdev, PCI_COMMAND, config_cmd_val);
-		pdev->irq = pci_irq_vector(pdev, ast2600_msi_idx[BMC_MSI]);
+		pdev->irq = pci_irq_vector(pdev, pci_bmc_dev->ast2600_msi_idx[BMC_MSI]);
 	}
 
 	pr_info("ASPEED BMC PCI ID %04x:%04x, IRQ=%u\n", pdev->vendor, pdev->device, pdev->irq);
@@ -323,7 +335,8 @@ static int aspeed_pci_host_bmc_device_probe(struct pci_dev *pdev, const struct p
 	pci_bmc_dev->mem_bar_base = pci_resource_start(pdev, 0);
 	pci_bmc_dev->mem_bar_size = pci_resource_len(pdev, 0);
 
-	pr_info("BAR0 I/O Mapped Base Address is: %08lx End %08lx\n", pci_bmc_dev->mem_bar_base, pci_bmc_dev->mem_bar_size);
+	pr_info("BAR0 I/O Mapped Base Address is: %08lx End %08lx\n",
+		pci_bmc_dev->mem_bar_base, pci_bmc_dev->mem_bar_size);
 
 	pci_bmc_dev->mem_bar_reg = pci_ioremap_bar(pdev, 0);
 	if (!pci_bmc_dev->mem_bar_reg) {
@@ -335,7 +348,8 @@ static int aspeed_pci_host_bmc_device_probe(struct pci_dev *pdev, const struct p
 	pci_bmc_dev->message_bar_base = pci_resource_start(pdev, 1);
 	pci_bmc_dev->message_bar_size = pci_resource_len(pdev, 1);
 
-	pr_info("MSG BAR1 Memory Mapped Base Address is: %08lx End %08lx\n", pci_bmc_dev->message_bar_base, pci_bmc_dev->message_bar_size);
+	pr_info("MSG BAR1 Memory Mapped Base Address is: %08lx End %08lx\n",
+		pci_bmc_dev->message_bar_base, pci_bmc_dev->message_bar_size);
 
 	pci_bmc_dev->msg_bar_reg = pci_ioremap_bar(pdev, 1);
 	if (!pci_bmc_dev->msg_bar_reg) {
@@ -398,66 +412,68 @@ static int aspeed_pci_host_bmc_device_probe(struct pci_dev *pdev, const struct p
 
 	pci_set_drvdata(pdev, pci_bmc_dev);
 
-	rc = request_irq(pdev->irq, aspeed_pci_host_bmc_device_interrupt, IRQF_SHARED, "ASPEED BMC DEVICE", pci_bmc_dev);
+	rc = request_irq(pdev->irq, aspeed_pci_host_bmc_device_interrupt,
+			 IRQF_SHARED, "ASPEED BMC DEVICE", pci_bmc_dev);
 	if (rc) {
 		pr_err("host bmc device Unable to get IRQ %d\n", rc);
 		goto out_unreg;
 	}
 
 	/* setup mbox */
-	pcie_sio_decode_addr = pci_bmc_dev->msg_bar_reg + PCIE_DEVICE_SIO_ADDR;
-	writel(0xaa, pcie_sio_decode_addr);
-	writel(0xa5, pcie_sio_decode_addr);
-	writel(0xa5, pcie_sio_decode_addr);
-	writel(0x07, pcie_sio_decode_addr);
-	writel(0x0e, pcie_sio_decode_addr + 0x04);
+	pci_bmc_dev->pcie_sio_decode_addr = pci_bmc_dev->msg_bar_reg + PCIE_DEVICE_SIO_ADDR;
+	writel(0xaa, pci_bmc_dev->pcie_sio_decode_addr);
+	writel(0xa5, pci_bmc_dev->pcie_sio_decode_addr);
+	writel(0xa5, pci_bmc_dev->pcie_sio_decode_addr);
+	writel(0x07, pci_bmc_dev->pcie_sio_decode_addr);
+	writel(0x0e, pci_bmc_dev->pcie_sio_decode_addr + 0x04);
 	/* disable */
-	writel(0x30, pcie_sio_decode_addr);
-	writel(0x00, pcie_sio_decode_addr + 0x04);
+	writel(0x30, pci_bmc_dev->pcie_sio_decode_addr);
+	writel(0x00, pci_bmc_dev->pcie_sio_decode_addr + 0x04);
 	/* set decode address 0x100 */
-	writel(0x60, pcie_sio_decode_addr);
-	writel(0x01, pcie_sio_decode_addr + 0x04);
-	writel(0x61, pcie_sio_decode_addr);
-	writel(0x00, pcie_sio_decode_addr + 0x04);
+	writel(0x60, pci_bmc_dev->pcie_sio_decode_addr);
+	writel(0x01, pci_bmc_dev->pcie_sio_decode_addr + 0x04);
+	writel(0x61, pci_bmc_dev->pcie_sio_decode_addr);
+	writel(0x00, pci_bmc_dev->pcie_sio_decode_addr + 0x04);
 	/* enable */
-	writel(0x30, pcie_sio_decode_addr);
-	writel(0x01, pcie_sio_decode_addr + 0x04);
+	writel(0x30, pci_bmc_dev->pcie_sio_decode_addr);
+	writel(0x01, pci_bmc_dev->pcie_sio_decode_addr + 0x04);
 	pci_bmc_dev->sio_mbox_reg = pci_bmc_dev->msg_bar_reg + 0x400;
 
 	if (pci_bmc_dev->legency_irq)
 		pci_bmc_dev->sio_mbox_irq = pdev->irq;
 	else
-		pci_bmc_dev->sio_mbox_irq = pci_irq_vector(pdev, ast2600_msi_idx[MBX_MSI]);
+		pci_bmc_dev->sio_mbox_irq = pci_irq_vector(pdev,
+							   pci_bmc_dev->ast2600_msi_idx[MBX_MSI]);
 
-	rc = request_irq(pci_bmc_dev->sio_mbox_irq, aspeed_pci_host_mbox_interrupt, IRQF_SHARED, "ASPEED SIO MBOX", pci_bmc_dev);
+	rc = request_irq(pci_bmc_dev->sio_mbox_irq,
+			 aspeed_pci_host_mbox_interrupt,
+			 IRQF_SHARED, "ASPEED SIO MBOX", pci_bmc_dev);
 	if (rc)
 		pr_err("host bmc device Unable to get IRQ %d\n", rc);
 
-	/* setup VUART */
-	memset(uart, 0, sizeof(uart));
-
 	for (i = 0; i < VUART_MAX_PARMS; i++) {
 		vuart_ioport[i] = 0x3F8 - (i * 0x100);
-		vuart_sirq[i] = ast2600_msi_idx[VUART_MSI] - i;
-		uart[i].port.flags = UPF_SKIP_TEST | UPF_BOOT_AUTOCONF | UPF_SHARE_IRQ;
-		uart[i].port.uartclk = 115200 * 16;
+		vuart_sirq[i] = pci_bmc_dev->ast2600_msi_idx[VUART_MSI] - i;
+		pci_bmc_dev->uart[i].port.flags = UPF_SKIP_TEST | UPF_BOOT_AUTOCONF | UPF_SHARE_IRQ;
+		pci_bmc_dev->uart[i].port.uartclk = 115200 * 16;
 
 		if (pci_bmc_dev->legency_irq)
-			uart[i].port.irq = pdev->irq;
+			pci_bmc_dev->uart[i].port.irq = pdev->irq;
 		else
-			uart[i].port.irq = pci_irq_vector(pdev, vuart_sirq[i]);
-		uart[i].port.dev = &pdev->dev;
-		uart[i].port.iotype = UPIO_MEM32;
-		uart[i].port.iobase = 0;
-		uart[i].port.mapbase = pci_bmc_dev->message_bar_base + (vuart_ioport[i] << 2);
-		uart[i].port.membase = 0;
-		uart[i].port.type = PORT_16550A;
-		uart[i].port.flags |= (UPF_IOREMAP | UPF_FIXED_PORT | UPF_FIXED_TYPE);
-		uart[i].port.regshift = 2;
+			pci_bmc_dev->uart[i].port.irq = pci_irq_vector(pdev, vuart_sirq[i]);
 
-		rc = serial8250_register_8250_port(&uart[i]);
+		pci_bmc_dev->uart[i].port.dev = &pdev->dev;
+		pci_bmc_dev->uart[i].port.iotype = UPIO_MEM32;
+		pci_bmc_dev->uart[i].port.iobase = 0;
+		pci_bmc_dev->uart[i].port.mapbase =
+					pci_bmc_dev->message_bar_base + (vuart_ioport[i] << 2);
+		pci_bmc_dev->uart[i].port.membase = 0;
+		pci_bmc_dev->uart[i].port.type = PORT_16550A;
+		pci_bmc_dev->uart[i].port.flags |= (UPF_IOREMAP | UPF_FIXED_PORT | UPF_FIXED_TYPE);
+		pci_bmc_dev->uart[i].port.regshift = 2;
+		rc = serial8250_register_8250_port(&pci_bmc_dev->uart[i]);
 		if (rc < 0) {
-			dev_err(dev, "cannot setup VUART@%xh over PCIe, rc=%d\n", vuart_ioport[i], rc);
+			dev_err_probe(dev, rc, "Can't setup PCIe VUART\n");
 			goto out_unreg;
 		}
 	}
@@ -476,7 +492,6 @@ out_err:
 	pci_disable_device(pdev);
 
 	return rc;
-
 }
 
 static void aspeed_pci_host_bmc_device_remove(struct pci_dev *pdev)
@@ -520,7 +535,6 @@ static int __init aspeed_host_bmc_device_init(void)
 	}
 
 	return 0;
-
 }
 
 static void aspeed_host_bmc_device_exit(void)
