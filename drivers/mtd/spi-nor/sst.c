9d0164c6500e3b78b329c5eca7909eba61e972d3
Miquel Raynal <miquel.raynal@bootlin.com>
2023-08-08 09:50:01 +0200

    mtd: spi-nor: Add support for sst26vf032b flash
    Describe this new part. The datasheet is public.
    
    Link: https://ww1.microchip.com/downloads/aemDocuments/documents/MPD/ProductDocuments/DataSheets/SST26VF032B-SST26VF032BA-2.5V-3.0V-32-Mbit-Serial-Quad-IO-%28SQI%29-Flash-Memory-20005218K.pdf
    
    Here are the sfdp tables plus base testing to show it works.
    
    $ cat /sys/bus/spi/devices/spi0.0/spi-nor/partname
    sst26vf032b
    $ cat /sys/bus/spi/devices/spi0.0/spi-nor/jedec_id
    bf2642
    $ cat /sys/bus/spi/devices/spi0.0/spi-nor/manufacturer
    sst
    $ xxd -p /sys/bus/spi/devices/spi0.0/spi-nor/sfdp
    53464450060102ff00060110300000ff81000106000100ffbf0001180002
    0001fffffffffffffffffffffffffffffffffd20f1ffffffff0144eb086b
    083b80bbfeffffffffff00ffffff440b0c200dd80fd810d820914824806f
    1d81ed0f773830b030b0f7ffffff29c25cfff030c080ffffffffffffffff
    ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
    ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
    ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
    ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
    ffffffffffffffffffffffffffffffffff0004fff37f0000f57f0000f9ff
    3d00f57f0000f37f0000ffffffffffffffffffffffffffffffffffffffff
    ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
    ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
    ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
    ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
    ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
    ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
    ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
    ffffbf2642ffb95ffdff30f260f332ff0a122346ff0f19320f1919ffffff
    ffffffff00669938ff05013506040232b03072428de89888a585c09faf5a
    ffff06ec060c0003080bffffffffff07ffff0202ff060300fdfd040600fc
    0300fefe0202070e
    $ md5sum /sys/bus/spi/devices/spi0.0/spi-nor/sfdp
    e7efddddb3d5ee89ca37bf6b6e789645  /sys/bus/spi/devices/spi0.0/spi-nor/sfdp
    
    $ dd if=/dev/urandom of=./qspi_test bs=1M count=1
    1+0 records in
    1+0 records out
    $ mtd_debug write /dev/mtd0 0 1048576 qspi_test
    Copied 1048576 bytes from qspi_test to address 0x00000000 in flash
    $ mtd_debug erase /dev/mtd0 0 1048576
    Erased 1048576 bytes from address 0x00000000 in flash
    $ mtd_debug read /dev/mtd0 0 1048576 qspi_read
    Copied 1048576 bytes from address 0x00000000 in flash to qspi_read
    $ hexdump qspi_read
    0000000 ffff ffff ffff ffff ffff ffff ffff ffff
    *
    0100000
    $ mtd_debug write /dev/mtd0 0 1048576 qspi_test
    Copied 1048576 bytes from qspi_test to address 0x00000000 in flash
    $ mtd_debug read /dev/mtd0 0 1048576 qspi_read
    Copied 1048576 bytes from address 0x00000000 in flash to qspi_read
    $ sha1sum qspi_test qspi_read
    2f2f191c7a937eca5db21a1c39e79e7327587cc1  qspi_test
    2f2f191c7a937eca5db21a1c39e79e7327587cc1  qspi_read
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Link: https://lore.kernel.org/r/20230808075001.223150-1-miquel.raynal@bootlin.com
    Signed-off-by: Tudor Ambarus <tudor.ambarus@linaro.org>

--- drivers/mtd/spi-nor/sst.c
+++ drivers/mtd/spi-nor/sst.c
@@ -113,6 +113,10 @@ static const struct flash_info sst_nor_parts[] = {
 			      SPI_NOR_QUAD_READ) },
 	{ "sst26vf016b", INFO(0xbf2641, 0, 64 * 1024, 32)
 		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ) },
+	{ "sst26vf032b", INFO(0xbf2642, 0, 0, 0)
+		FLAGS(SPI_NOR_HAS_LOCK | SPI_NOR_SWP_IS_VOLATILE)
+		PARSE_SFDP
+		.fixups = &sst26vf_nor_fixups },
 	{ "sst26vf064b", INFO(0xbf2643, 0, 64 * 1024, 128)
 		FLAGS(SPI_NOR_HAS_LOCK | SPI_NOR_SWP_IS_VOLATILE)
 		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ)
