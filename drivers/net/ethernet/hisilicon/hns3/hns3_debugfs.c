c4f7de3e8ce1799b12bdd1520e3ebd49d2e12f42
Hao Chen <chenhao418@huawei.com>
2023-08-09 10:09:02 +0800

    net: hns3: fix strscpy causing content truncation issue
    commit 5e3d20617b055e725e785e0058426368269949f3 upstream.
    
    hns3_dbg_fill_content()/hclge_dbg_fill_content() is aim to integrate some
    items to a string for content, and we add '\n' and '\0' in the last
    two bytes of content.
    
    strscpy() will add '\0' in the last byte of destination buffer(one of
    items), it result in finishing content print ahead of schedule and some
    dump content truncation.
    
    One Error log shows as below:
    cat mac_list/uc
    UC MAC_LIST:
    
    Expected:
    UC MAC_LIST:
    FUNC_ID  MAC_ADDR            STATE
    pf       00:2b:19:05:03:00   ACTIVE
    
    The destination buffer is length-bounded and not required to be
    NUL-terminated, so just change strscpy() to memcpy() to fix it.
    
    Fixes: 1cf3d5567f27 ("net: hns3: fix strncpy() not using dest-buf length as length issue")
    Signed-off-by: Hao Chen <chenhao418@huawei.com>
    Signed-off-by: Jijie Shao <shaojijie@huawei.com>
    Link: https://lore.kernel.org/r/20230809020902.1941471-1-shaojijie@huawei.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/hisilicon/hns3/hns3_debugfs.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3_debugfs.c
@@ -458,9 +458,9 @@ static void hns3_dbg_fill_content(char *content, u16 len,
 		if (result) {
 			if (item_len < strlen(result[i]))
 				break;
-			strscpy(pos, result[i], strlen(result[i]));
+			memcpy(pos, result[i], strlen(result[i]));
 		} else {
-			strscpy(pos, items[i].name, strlen(items[i].name));
+			memcpy(pos, items[i].name, strlen(items[i].name));
 		}
 		pos += item_len;
 		len -= item_len;
