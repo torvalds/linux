5ac8480ae4d01f0ca5dfd561884424046df2478a
Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
2023-08-09 21:44:36 -0700

    RDMA/bnxt_re: Fix error handling in probe failure path
    During bnxt_re_dev_init(), when bnxt_re_setup_chip_ctx() fails unregister
    with L2 first before bailing out probe.
    
    Fixes: ae8637e13185 ("RDMA/bnxt_re: Add chip context to identify 57500 series")
    Link: https://lore.kernel.org/r/1691642677-21369-3-git-send-email-selvin.xavier@broadcom.com
    Signed-off-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
    Signed-off-by: Selvin Xavier <selvin.xavier@broadcom.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/infiniband/hw/bnxt_re/main.c
+++ drivers/infiniband/hw/bnxt_re/main.c
@@ -1253,6 +1253,8 @@ static int bnxt_re_dev_init(struct bnxt_re_dev *rdev, u8 wqe_mode)
 
 	rc = bnxt_re_setup_chip_ctx(rdev, wqe_mode);
 	if (rc) {
+		bnxt_unregister_dev(rdev->en_dev);
+		clear_bit(BNXT_RE_FLAG_NETDEV_REGISTERED, &rdev->flags);
 		ibdev_err(&rdev->ibdev, "Failed to get chip context\n");
 		return -EINVAL;
 	}
