b1da99664b51c0538bec271e9d9924a3d92e3bd4
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2023-08-10 11:58:43 +0200

    mfd: max77541: Fix Wvoid-pointer-to-enum-cast warning
    'id' is an enum, thus cast of pointer on 64-bit compile test with W=1
    causes:
    
      max77541.c:176:18: error: cast to smaller integer type 'enum max7754x_ids' from 'const void *' [-Werror,-Wvoid-pointer-to-enum-cast]
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Link: https://lore.kernel.org/r/20230810095849.123321-3-krzysztof.kozlowski@linaro.org
    Signed-off-by: Lee Jones <lee@kernel.org>

--- drivers/mfd/max77541.c
+++ drivers/mfd/max77541.c
@@ -173,7 +173,7 @@ static int max77541_probe(struct i2c_client *client)
 	i2c_set_clientdata(client, max77541);
 	max77541->i2c = client;
 
-	max77541->id  = (enum max7754x_ids)device_get_match_data(dev);
+	max77541->id = (uintptr_t)device_get_match_data(dev);
 	if (!max77541->id)
 		max77541->id  = (enum max7754x_ids)id->driver_data;
 
