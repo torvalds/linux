9793a5a9c493a66871de4c6f27e26e9ca90699cc
Ido Schimmel <idosch@nvidia.com>
2023-08-11 17:13:57 +0200

    mlxsw: spectrum: Stop ignoring learning notifications from redirected traffic
    As explained in the previous patch, with the ignore action prepended to
    the redirect action, it is not longer possible for redirected traffic to
    generate learning notifications.
    
    Therefore, remove the workaround that was added in commit 577fa14d2100
    ("mlxsw: spectrum: Do not process learned records with a dummy FID") as
    it is no longer needed.
    
    Signed-off-by: Ido Schimmel <idosch@nvidia.com>
    Reviewed-by: Petr Machata <petrm@nvidia.com>
    Signed-off-by: Petr Machata <petrm@nvidia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum.h
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum.h
@@ -1271,7 +1271,6 @@ int mlxsw_sp_setup_tc_block_qevent_mark(struct mlxsw_sp_port *mlxsw_sp_port,
 					struct flow_block_offload *f);
 
 /* spectrum_fid.c */
-bool mlxsw_sp_fid_is_dummy(struct mlxsw_sp *mlxsw_sp, u16 fid_index);
 struct mlxsw_sp_fid *mlxsw_sp_fid_lookup_by_index(struct mlxsw_sp *mlxsw_sp,
 						  u16 fid_index);
 int mlxsw_sp_fid_nve_ifindex(const struct mlxsw_sp_fid *fid, int *nve_ifindex);
