d95773a6202c714d1acc7cacc4781bb494a939a1
Dylan Hung <dylan_hung@aspeedtech.com>
2023-08-11 11:32:17 +0800

    dt-bindings: i3c: Add bus-context definitions
    Introduce a header file containing bus-context definitions, enabling
    users to utilize these definitions in their DTS files for declaring the
    I3C bus context. Additionally, add the missing type ref of the
    bus-context.
    
    Fixes: 57128da01c6d ("dt-bindings: i3c: Add bus-context property")
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: Icb4a68b02d046ae2a93c1b4d924ec80fa7897209

---
+++ include/dt-bindings/i3c/i3c.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * This header provides constants for I3C bindings
+ *
+ * Copyright 2023 Aspeed Technology Inc.
+ */
+#ifndef _DT_BINDINGS_I3C_I3C_H
+#define _DT_BINDINGS_I3C_I3C_H
+
+#define I3C_BUS_CONTEXT_MIPI_BASIC_V1_0_0	0x10
+#define I3C_BUS_CONTEXT_MIPI_BASIC_V1_1_0	0x11
+#define I3C_BUS_CONTEXT_MIPI_BASIC_V1_1_1	0x31
+#define I3C_BUS_CONTEXT_MIPI_BASIC_V1_2_0	0x12
+#define I3C_BUS_CONTEXT_JESD403			0x80
+#define I3C_BUS_CONTEXT_MCTP			0x81
+
+#endif
