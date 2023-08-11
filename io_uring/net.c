b2e74db55dd93d6db22a813c9a775b5dbf87c560
Pavel Begunkov <asml.silence@gmail.com>
2023-08-11 13:53:42 +0100

    io_uring/net: don't overflow multishot recv
    Don't allow overflowing multishot recv CQEs, it might get out of
    hand, hurt performance, and in the worst case scenario OOM the task.
    
    Cc: stable@vger.kernel.org
    Fixes: b3fdea6ecb55c ("io_uring: multishot recv")
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/0b295634e8f1b71aa764c984608c22d85f88f75c.1691757663.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- io_uring/net.c
+++ io_uring/net.c
@@ -642,7 +642,7 @@ static inline bool io_recv_finish(struct io_kiocb *req, int *ret,
 
 	if (!mshot_finished) {
 		if (io_aux_cqe(req, issue_flags & IO_URING_F_COMPLETE_DEFER,
-			       *ret, cflags | IORING_CQE_F_MORE, true)) {
+			       *ret, cflags | IORING_CQE_F_MORE, false)) {
 			io_recv_prep_retry(req);
 			/* Known not-empty or unknown state, retry */
 			if (cflags & IORING_CQE_F_SOCK_NONEMPTY ||
