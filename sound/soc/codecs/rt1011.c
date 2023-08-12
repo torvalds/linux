a5c8e75b34fa0d399e8e6b304de3bad4d9e41a3a
Linus Walleij <linus.walleij@linaro.org>
2023-08-12 21:56:39 +0200

    ASoC: rt1011: Drop GPIO includes
    This driver include two GPIO legacy headers yet doesn't use
    symbols from any of them. Drop the includes.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org
    Link: https://lore.kernel.org/r/20230812-descriptors-asoc-v1-1-eb4dca1f68af@linaro.org
    Signed-off-by: Mark Brown <broonie@kernel.org

--- sound/soc/codecs/rt1011.c
+++ sound/soc/codecs/rt1011.c
@@ -13,11 +13,9 @@
 #include <linux/init.h>
 #include <linux/delay.h>
 #include <linux/pm.h>
-#include <linux/gpio.h>
 #include <linux/i2c.h>
 #include <linux/acpi.h>
 #include <linux/regmap.h>
-#include <linux/of_gpio.h>
 #include <linux/platform_device.h>
 #include <linux/firmware.h>
 #include <sound/core.h>
