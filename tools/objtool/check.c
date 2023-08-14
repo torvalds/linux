4ae68b26c3ab5a82aa271e6e9fc9b1a06e1d6b40
Peter Zijlstra <peterz@infradead.org>
2023-08-14 13:44:29 +0200

    objtool/x86: Fix SRSO mess
    Objtool --rethunk does two things:
    
     - it collects all (tail) call's of __x86_return_thunk and places them
       into .return_sites. These are typically compiler generated, but
       RET also emits this same.
    
     - it fudges the validation of the __x86_return_thunk symbol; because
       this symbol is inside another instruction, it can't actually find
       the instruction pointed to by the symbol offset and gets upset.
    
    Because these two things pertained to the same symbol, there was no
    pressing need to separate these two separate things.
    
    However, alas, along comes SRSO and more crazy things to deal with
    appeared.
    
    The SRSO patch itself added the following symbol names to identify as
    rethunk:
    
      'srso_untrain_ret', 'srso_safe_ret' and '__ret'
    
    Where '__ret' is the old retbleed return thunk, 'srso_safe_ret' is a
    new similarly embedded return thunk, and 'srso_untrain_ret' is
    completely unrelated to anything the above does (and was only included
    because of that INT3 vs UD2 issue fixed previous).
    
    Clear things up by adding a second category for the embedded instruction
    thing.
    
    Fixes: fb3bd914b3ec ("x86/srso: Add a Speculative RAS Overflow mitigation")
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://lore.kernel.org/r/20230814121148.704502245@infradead.org

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -455,7 +455,7 @@ static int decode_instructions(struct objtool_file *file)
 				return -1;
 			}
 
-			if (func->return_thunk || func->alias != func)
+			if (func->embedded_insn || func->alias != func)
 				continue;
 
 			if (!find_insn(file, sec, func->offset)) {
@@ -1288,12 +1288,29 @@ static int add_ignore_alternatives(struct objtool_file *file)
 	return 0;
 }
 
+/*
+ * Symbols that replace INSN_CALL_DYNAMIC, every (tail) call to such a symbol
+ * will be added to the .retpoline_sites section.
+ */
 __weak bool arch_is_retpoline(struct symbol *sym)
 {
 	return false;
 }
 
+/*
+ * Symbols that replace INSN_RETURN, every (tail) call to such a symbol
+ * will be added to the .return_sites section.
+ */
 __weak bool arch_is_rethunk(struct symbol *sym)
+{
+	return false;
+}
+
+/*
+ * Symbols that are embedded inside other instructions, because sometimes crazy
+ * code exists. These are mostly ignored for validation purposes.
+ */
+__weak bool arch_is_embedded_insn(struct symbol *sym)
 {
 	return false;
 }
@@ -1583,7 +1600,7 @@ static int add_jump_destinations(struct objtool_file *file)
 			 * middle of another instruction.  Objtool only
 			 * knows about the outer instruction.
 			 */
-			if (sym && sym->return_thunk) {
+			if (sym && sym->embedded_insn) {
 				add_return_call(file, insn, false);
 				continue;
 			}
@@ -2501,6 +2518,9 @@ static int classify_symbols(struct objtool_file *file)
 
 		if (arch_is_rethunk(func))
 			func->return_thunk = true;
+
+		if (arch_is_embedded_insn(func))
+			func->embedded_insn = true;
 
 		if (arch_ftrace_match(func->name))
 			func->fentry = true;
