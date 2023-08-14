65aaca1134025d437882535c9eb54903b9cd09c9
Jason Gunthorpe <jgg@nvidia.com>
2023-08-14 20:24:34 -0300

    iommufd: Remove iommufd_ref_to_users()
    This no longer has any callers, remove the function
    
    Kevin noticed that after commit 99f98a7c0d69 ("iommufd: IOMMUFD_DESTROY
    should not increase the refcount") there was only one other user and it
    turns out the rework in commit 9227da7816dd ("iommufd: Add
    iommufd_access_change_ioas(_id) helpers") got rid of the last one.
    
    Link: https://lore.kernel.org/r/0-v1-abb31bedd888+c1-iommufd_ref_to_users_jgg@nvidia.com
    Suggested-by: Kevin Tian <kevin.tian@intel.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/iommu/iommufd/iommufd_private.h
+++ drivers/iommu/iommufd/iommufd_private.h
@@ -149,29 +149,6 @@ static inline void iommufd_put_object(struct iommufd_object *obj)
 	up_read(&obj->destroy_rwsem);
 }
 
-/**
- * iommufd_ref_to_users() - Switch from destroy_rwsem to users refcount
- *        protection
- * @obj - Object to release
- *
- * Objects have two refcount protections (destroy_rwsem and the refcount_t
- * users). Holding either of these will prevent the object from being destroyed.
- *
- * Depending on the use case, one protection or the other is appropriate.  In
- * most cases references are being protected by the destroy_rwsem. This allows
- * orderly destruction of the object because iommufd_object_destroy_user() will
- * wait for it to become unlocked. However, as a rwsem, it cannot be held across
- * a system call return. So cases that have longer term needs must switch
- * to the weaker users refcount_t.
- *
- * With users protection iommufd_object_destroy_user() will return false,
- * refusing to destroy the object, causing -EBUSY to userspace.
- */
-static inline void iommufd_ref_to_users(struct iommufd_object *obj)
-{
-	up_read(&obj->destroy_rwsem);
-	/* iommufd_lock_obj() obtains users as well */
-}
 void iommufd_object_abort(struct iommufd_ctx *ictx, struct iommufd_object *obj);
 void iommufd_object_abort_and_destroy(struct iommufd_ctx *ictx,
 				      struct iommufd_object *obj);
