8b0ff83e8ad3daaeab583189ff7504383c854f24
Peter Zijlstra <peterz@infradead.org>
2023-08-14 13:44:31 +0200

    x86/cpu: Clean up SRSO return thunk mess
    commit d43490d0ab824023e11d0b57d0aeec17a6e0ca13 upstream.
    
    Use the existing configurable return thunk. There is absolute no
    justification for having created this __x86_return_thunk alternative.
    
    To clarify, the whole thing looks like:
    
    Zen3/4 does:
    
      srso_alias_untrain_ret:
    	  nop2
    	  lfence
    	  jmp srso_alias_return_thunk
    	  int3
    
      srso_alias_safe_ret: // aliasses srso_alias_untrain_ret just so
    	  add $8, %rsp
    	  ret
    	  int3
    
      srso_alias_return_thunk:
    	  call srso_alias_safe_ret
    	  ud2
    
    While Zen1/2 does:
    
      srso_untrain_ret:
    	  movabs $foo, %rax
    	  lfence
    	  call srso_safe_ret           (jmp srso_return_thunk ?)
    	  int3
    
      srso_safe_ret: // embedded in movabs instruction
    	  add $8,%rsp
              ret
              int3
    
      srso_return_thunk:
    	  call srso_safe_ret
    	  ud2
    
    While retbleed does:
    
      zen_untrain_ret:
    	  test $0xcc, %bl
    	  lfence
    	  jmp zen_return_thunk
              int3
    
      zen_return_thunk: // embedded in the test instruction
    	  ret
              int3
    
    Where Zen1/2 flush the BTB entry using the instruction decoder trick
    (test,movabs) Zen3/4 use BTB aliasing. SRSO adds a return sequence
    (srso_safe_ret()) which forces the function return instruction to
    speculate into a trap (UD2).  This RET will then mispredict and
    execution will continue at the return site read from the top of the
    stack.
    
    Pick one of three options at boot (evey function can only ever return
    once).
    
      [ bp: Fixup commit message uarch details and add them in a comment in
        the code too. Add a comment about the srso_select_mitigation()
        dependency on retbleed_select_mitigation(). Add moar ifdeffery for
        32-bit builds. Add a dummy srso_untrain_ret_alias() definition for
        32-bit alternatives needing the symbol. ]
    
    Fixes: fb3bd914b3ec ("x86/srso: Add a Speculative RAS Overflow mitigation")
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://lore.kernel.org/r/20230814121148.842775684@infradead.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/include/asm/nospec-branch.h
+++ arch/x86/include/asm/nospec-branch.h
@@ -201,9 +201,14 @@ extern void __x86_return_thunk(void);
 static inline void __x86_return_thunk(void) {}
 #endif
 
+extern void zen_return_thunk(void);
+extern void srso_return_thunk(void);
+extern void srso_alias_return_thunk(void);
+
 extern void zen_untrain_ret(void);
 extern void srso_untrain_ret(void);
 extern void srso_untrain_ret_alias(void);
+
 extern void entry_ibpb(void);
 
 #ifdef CONFIG_RETPOLINE
