fb5040ef7f707525d0681cf6bfe424ccd1aadab7
Tony Krowiak <akrowiak@linux.ibm.com>
2023-08-15 14:43:32 -0400

    KVM: s390: export kvm_s390_pv*_is_protected functions
    Export the kvm_s390_pv_is_protected and kvm_s390_pv_cpu_is_protected
    functions so that they can be called from other modules that carry a
    GPL-compatible license.
    
    Signed-off-by: Janosch Frank <frankja@linux.ibm.com>
    Signed-off-by: Tony Krowiak <akrowiak@linux.ibm.com>
    Tested-by: Viktor Mihajlovski <mihajlov@linux.ibm.com>
    Link: https://lore.kernel.org/r/20230815184333.6554-12-akrowiak@linux.ibm.com
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- arch/s390/include/asm/kvm_host.h
+++ arch/s390/include/asm/kvm_host.h
@@ -1028,6 +1028,9 @@ static inline int sie64a(struct kvm_s390_sie_block *sie_block, u64 *rsa)
 
 extern char sie_exit;
 
+bool kvm_s390_pv_is_protected(struct kvm *kvm);
+bool kvm_s390_pv_cpu_is_protected(struct kvm_vcpu *vcpu);
+
 extern int kvm_s390_gisc_register(struct kvm *kvm, u32 gisc);
 extern int kvm_s390_gisc_unregister(struct kvm *kvm, u32 gisc);
 
