5bc2de5f517d561a5b8dc472d3ba17d0f32b122e
Stefan Adolfsson <sadolfsson@chromium.org>
2023-08-16 12:41:25 +0200

    media: cros-ec-cec: Add Constitution to the match table
    Constitution has two HDMI ports which support CEC:
        Port B is EC port 0
        Port A is EC port 1
    
    This patch depends on "media: cros-ec-cec: Add Dibbi to the match
    table".
    
    Signed-off-by: Stefan Adolfsson <sadolfsson@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    [hverkuil: updated to the new multi-port datastructures]

--- drivers/media/cec/platform/cros-ec/cros-ec-cec.c
+++ drivers/media/cec/platform/cros-ec/cros-ec-cec.c
@@ -296,6 +296,7 @@ struct cec_dmi_match {
 
 static const char *const fizz_conns[] = { "Port B", NULL };
 static const char *const dibbi_conns[] = { "Port D", "Port B", NULL };
+static const char *const constitution_conns[] = { "Port B", "Port A", NULL };
 
 static const struct cec_dmi_match cec_dmi_match_table[] = {
 	/* Google Fizz */
@@ -316,6 +317,8 @@ static const struct cec_dmi_match cec_dmi_match_table[] = {
 	{ "Google", "Lisbon", "0000:00:02.0", fizz_conns },
 	/* Google Dibbi */
 	{ "Google", "Dibbi", "0000:00:02.0", dibbi_conns },
+	/* Google Constitution */
+	{ "Google", "Constitution", "0000:00:02.0", constitution_conns },
 };
 
 static struct device *cros_ec_cec_find_hdmi_dev(struct device *dev,
