ec2ea550b89a266ce36a223453097c12e106d42f
Yadu MG <quic_ymg@quicinc.com>
2023-08-17 00:14:08 +0530

    drivers: remoteproc: Enable Auto Boot for ADSP Rproc
    Enable Auto Boot for ADSP Remoteproc on MonacoAU target.
    
    Change-Id: I8e072b02a55a189f8e712345df45441e87e087a5
    Signed-off-by: Yadu MG <quic_ymg@quicinc.com>

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -1975,6 +1975,7 @@ static const struct adsp_data monaco_auto_adsp_resource = {
 	.sysmon_name = "adsp",
 	.qmp_name = "adsp",
 	.ssctl_id = 0x14,
+	.auto_boot = true,
 };
 
 static const struct adsp_data monaco_auto_cdsp_resource = {
