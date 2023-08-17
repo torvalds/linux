5496eac6ad7428fa06811a8c34b3a15beb93b86d
Miquel Raynal <miquel.raynal@bootlin.com>
2023-08-17 12:18:53 +0200

    i3c: master: svc: Describe member 'saved_regs'
    The 'saved_regs' member of the 'svc_i3c_master'	structure is not
    described in the kernel doc, which produces the following warning:
    
        Function parameter or member 'saved_regs' not described in 'svc_i3c_master'
    
    Add the missing line in the kernel documentation of the parent
    structure.
    
    Fixes: 1c5ee2a77b1b ("i3c: master: svc: fix i3c suspend/resume issue")
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202308171435.0xQ82lvu-lkp@intel.com/
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Link: https://lore.kernel.org/r/20230817101853.16805-1-miquel.raynal@bootlin.com
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/i3c/master/svc-i3c-master.c
+++ drivers/i3c/master/svc-i3c-master.c
@@ -156,6 +156,7 @@ struct svc_i3c_regs_save {
  * @base: I3C master controller
  * @dev: Corresponding device
  * @regs: Memory mapping
+ * @saved_regs: Volatile values for PM operations
  * @free_slots: Bit array of available slots
  * @addrs: Array containing the dynamic addresses of each attached device
  * @descs: Array of descriptors, one per attached device
