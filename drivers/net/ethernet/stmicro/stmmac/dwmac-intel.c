58f2ffdedf7b05b318f2cdafaca69e4435c8478e
Kurt Kanzenbach <kurt@linutronix.de>
2023-08-18 13:14:01 +0200

    stmmac: intel: Enable correction of MAC propagation delay
    All captured timestamps should be corrected by PHY, MAC and CDC introduced
    latency/errors. The CDC correction is already used. Enable MAC propagation delay
    correction as well which is available since commit 26cfb838aa00 ("net: stmmac:
    correct MAC propagation delay").
    
    Before:
    |ptp4l[390.458]: rms    7 max   21 freq   +177 +/-  14 delay   357 +/-   1
    
    After:
    |ptp4l[620.012]: rms    7 max   20 freq   +195 +/-  14 delay   345 +/-   1
    
    Tested on Intel Elkhart Lake.
    
    Signed-off-by: Kurt Kanzenbach <kurt@linutronix.de>
    Reviewed-by: Johannes Zink <j.zink@pengutronix.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-intel.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-intel.c
@@ -627,6 +627,7 @@ static int ehl_common_data(struct pci_dev *pdev,
 	plat->rx_queues_to_use = 8;
 	plat->tx_queues_to_use = 8;
 	plat->flags |= STMMAC_FLAG_USE_PHY_WOL;
+	plat->flags |= STMMAC_FLAG_HWTSTAMP_CORRECT_LATENCY;
 
 	plat->safety_feat_cfg->tsoee = 1;
 	plat->safety_feat_cfg->mrxpee = 1;
