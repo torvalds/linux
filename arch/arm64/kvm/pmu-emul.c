8c694f557fd80ca9815ddb1cf5de10d8bf168110
Reiji Watanabe <reijiw@google.com>
2023-08-18 21:39:46 -0700

    KVM: arm64: PMU: Don't advertise the STALL_SLOT event
    Currently, KVM hides the STALL_SLOT event for guests if the
    host PMU version is PMUv3p4 or newer, as PMMIR_EL1 is handled
    as RAZ for the guests. But, this should be based on the guests'
    PMU version (instead of the host PMU version), as an older PMU
    that doesn't support PMMIR_EL1 could support the STALL_SLOT
    event, according to the Arm ARM. Exposing the STALL_SLOT event
    without PMMIR_EL1 won't be very useful anyway though.
    
    Stop advertising the STALL_SLOT event for guests unconditionally,
    rather than fixing or keeping the inaccurate checking to
    advertise the event for the case, where it is not very useful.
    
    Suggested-by: Oliver Upton <oliver.upton@linux.dev>
    Signed-off-by: Reiji Watanabe <reijiw@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20230819043947.4100985-4-reijiw@google.com

--- arch/arm64/kvm/pmu-emul.c
+++ arch/arm64/kvm/pmu-emul.c
@@ -761,8 +761,7 @@ u64 kvm_pmu_get_pmceid(struct kvm_vcpu *vcpu, bool pmceid1)
 		 * Don't advertise STALL_SLOT, as PMMIR_EL0 is handled
 		 * as RAZ
 		 */
-		if (vcpu->kvm->arch.arm_pmu->pmuver >= ID_AA64DFR0_EL1_PMUVer_V3P4)
-			val &= ~BIT_ULL(ARMV8_PMUV3_PERFCTR_STALL_SLOT - 32);
+		val &= ~BIT_ULL(ARMV8_PMUV3_PERFCTR_STALL_SLOT - 32);
 		base = 32;
 	}
 
