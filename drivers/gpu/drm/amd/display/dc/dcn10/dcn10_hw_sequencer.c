cbb4c9bc55427774ca4d819933e1b5fa38a6fb44
Alvin Lee <alvin.lee2@amd.com>
2023-08-23 10:18:36 -0400

    drm/amd/display: Don't lock phantom pipe on disabling
    [Description]
    - When disabling a phantom pipe, we first enable the phantom
      OTG so the double buffer update can successfully take place
    - However, want to avoid locking the phantom otherwise setting
      DPG_EN=1 for the phantom pipe is blocked (without this we could
      hit underflow due to phantom HUBP being blanked by default)
    
    Reviewed-by: Samson Tam <samson.tam@amd.com>
    Acked-by: Stylon Wang <stylon.wang@amd.com>
    Signed-off-by: Alvin Lee <alvin.lee2@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn10/dcn10_hw_sequencer.c
+++ drivers/gpu/drm/amd/display/dc/dcn10/dcn10_hw_sequencer.c
@@ -111,7 +111,8 @@ void dcn10_lock_all_pipes(struct dc *dc,
 		if (pipe_ctx->top_pipe ||
 		    !pipe_ctx->stream ||
 		    (!pipe_ctx->plane_state && !old_pipe_ctx->plane_state) ||
-		    !tg->funcs->is_tg_enabled(tg))
+		    !tg->funcs->is_tg_enabled(tg) ||
+			pipe_ctx->stream->mall_stream_config.type == SUBVP_PHANTOM)
 			continue;
 
 		if (lock)
