3ec648c631d27a61d8069f610251d9a752cfc6bd
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2023-08-23 11:29:12 +0200

    IB: Use capital "OR" for multiple licenses in SPDX
    Documentation/process/license-rules.rst and checkpatch expect the SPDX
    identifier syntax for multiple licenses to use capital "OR".  Correct it
    to keep consistent format and avoid copy-paste issues.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Link: https://lore.kernel.org/r/20230823092912.122674-1-krzysztof.kozlowski@linaro.org
    Signed-off-by: Leon Romanovsky <leon@kernel.org>

--- drivers/infiniband/hw/irdma/osdep.h
+++ drivers/infiniband/hw/irdma/osdep.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0 or Linux-OpenIB */
+/* SPDX-License-Identifier: GPL-2.0 OR Linux-OpenIB */
 /* Copyright (c) 2015 - 2021 Intel Corporation */
 #ifndef IRDMA_OSDEP_H
 #define IRDMA_OSDEP_H
--- drivers/infiniband/hw/irdma/ws.h
+++ drivers/infiniband/hw/irdma/ws.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0 or Linux-OpenIB */
+/* SPDX-License-Identifier: GPL-2.0 OR Linux-OpenIB */
 /* Copyright (c) 2015 - 2020 Intel Corporation */
 #ifndef IRDMA_WS_H
 #define IRDMA_WS_H
