09a24be312b5ac7a1db0ddfcf03616bff7f19bfa
Vladimir Lypak <vladimir.lypak@gmail.com>
2023-08-24 23:51:15 +0300

    dt-bindings: clock: add apcs-msm8953 bindings
    Signed-off-by: Vladimir Lypak <vladimir.lypak@gmail.com>

---
+++ include/dt-bindings/clock/qcom,apcs-msm8953.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef _DT_BINDINGS_CLOCK_QCOM_APCS_MSM8953_H
+#define _DT_BINDINGS_CLOCK_QCOM_APCS_MSM8953_H
+
+/* Clocks */
+#define APCS_CPU0_CLK		0
+#define APCS_CPU0_CLK_SRC	1
+#define APCS_CPU0_PLL		2
+#define APCS_CPU4_CLK		3
+#define APCS_CPU4_CLK_SRC	4
+#define APCS_CPU4_PLL		5
+#define APCS_CCI_CLK		6
+#define APCS_CCI_CLK_SRC	7
+#define APCS_CCI_PLL		8
+#define APCS_NUM_CLOCKS		9
+
+/* Interconnect nodes */
+#define APCS_MAS_CCI		0
+#define APCS_SLV_BIMC		1
+
+#endif
