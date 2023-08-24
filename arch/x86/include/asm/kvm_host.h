f10a570b093e60c6bd3f210ae909f014f421352a
Kyle Meyer <kyle.meyer@hpe.com>
2023-08-24 16:52:46 -0500

    KVM: x86: Add CONFIG_KVM_MAX_NR_VCPUS to allow up to 4096 vCPUs
    Add a Kconfig entry to set the maximum number of vCPUs per KVM guest and
    set the default value to 4096 when MAXSMP is enabled, as there are use
    cases that want to create more than the currently allowed 1024 vCPUs and
    are more than happy to eat the memory overhead.
    
    The Hyper-V TLFS doesn't allow more than 64 sparse banks, i.e. allows a
    maximum of 4096 virtual CPUs. Cap KVM's maximum number of virtual CPUs
    to 4096 to avoid exceeding Hyper-V's limit as KVM support for Hyper-V is
    unconditional, and alternatives like dynamically disabling Hyper-V
    enlightenments that rely on sparse banks would require non-trivial code
    changes.
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Kyle Meyer <kyle.meyer@hpe.com>
    Link: https://lore.kernel.org/r/20230824215244.3897419-1-kyle.meyer@hpe.com
    [sean: massage changelog with --verbose, document #ifdef mess]
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -39,7 +39,15 @@
 
 #define __KVM_HAVE_ARCH_VCPU_DEBUGFS
 
+/*
+ * CONFIG_KVM_MAX_NR_VCPUS is defined iff CONFIG_KVM!=n, provide a dummy max if
+ * KVM is disabled (arbitrarily use the default from CONFIG_KVM_MAX_NR_VCPUS).
+ */
+#ifdef CONFIG_KVM_MAX_NR_VCPUS
+#define KVM_MAX_VCPUS CONFIG_KVM_MAX_NR_VCPUS
+#else
 #define KVM_MAX_VCPUS 1024
+#endif
 
 /*
  * In x86, the VCPU ID corresponds to the APIC ID, and APIC IDs
