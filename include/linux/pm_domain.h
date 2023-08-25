3dd91515ef43dd43e32e2a84e4bd881b64fb33ae
Ulf Hansson <ulf.hansson@linaro.org>
2023-08-25 13:26:32 +0200

    PM: domains: Allow genpd providers to manage OPP tables directly by its FW
    In some cases the OPP tables aren't specified in device tree, but rather
    encoded in the FW. To allow a genpd provider to specify them dynamically
    instead, let's add a new genpd flag, GENPD_FLAG_OPP_TABLE_FW.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Link: https://lore.kernel.org/r/20230825112633.236607-13-ulf.hansson@linaro.org
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>

--- include/linux/pm_domain.h
+++ include/linux/pm_domain.h
@@ -61,6 +61,10 @@
  * GENPD_FLAG_MIN_RESIDENCY:	Enable the genpd governor to consider its
  *				components' next wakeup when determining the
  *				optimal idle state.
+ *
+ * GENPD_FLAG_OPP_TABLE_FW:	The genpd provider supports performance states,
+ *				but its corresponding OPP tables are not
+ *				described in DT, but are given directly by FW.
  */
 #define GENPD_FLAG_PM_CLK	 (1U << 0)
 #define GENPD_FLAG_IRQ_SAFE	 (1U << 1)
@@ -69,6 +73,7 @@
 #define GENPD_FLAG_CPU_DOMAIN	 (1U << 4)
 #define GENPD_FLAG_RPM_ALWAYS_ON (1U << 5)
 #define GENPD_FLAG_MIN_RESIDENCY (1U << 6)
+#define GENPD_FLAG_OPP_TABLE_FW	 (1U << 7)
 
 enum gpd_status {
 	GENPD_STATE_ON = 0,	/* PM domain is on */
