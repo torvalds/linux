bac806830fde94ccf41482535fc442c02656febc
Wenchao Chen <wenchao.chen@unisoc.com>
2023-08-25 17:17:42 +0800

    mmc: core: Add host specific tuning support for SD HS mode
    To support the need for host specific tuning for SD high-speed mode, let's
    add two new optional callbacks, ->prepare|execute_sd_hs_tuning() and let's
    call them when switching into the SD high-speed mode.
    
    Note that, during the tuning process it's also needed for host drivers to
    send commands to the SD card to verify that the tuning process succeeds.
    Therefore, let's also share the corresponding functions from the core to
    allow this.
    
    Signed-off-by: Wenchao Chen <wenchao.chen@unisoc.com>
    Link: https://lore.kernel.org/r/20230825091743.15613-2-wenchao.chen@unisoc.com
    [Ulf: Dropped unnecessary function declarations and updated the commit msg]
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -1518,6 +1518,13 @@ retry:
 		 */
 		mmc_set_clock(host, mmc_sd_get_max_clock(card));
 
+		if (host->ios.timing == MMC_TIMING_SD_HS &&
+			host->ops->prepare_sd_hs_tuning) {
+			err = host->ops->prepare_sd_hs_tuning(host, card);
+			if (err)
+				goto free_card;
+		}
+
 		/*
 		 * Switch to wider bus (if supported).
 		 */
@@ -1528,6 +1535,13 @@ retry:
 				goto free_card;
 
 			mmc_set_bus_width(host, MMC_BUS_WIDTH_4);
+		}
+
+		if (host->ios.timing == MMC_TIMING_SD_HS &&
+			host->ops->execute_sd_hs_tuning) {
+			err = host->ops->execute_sd_hs_tuning(host, card);
+			if (err)
+				goto free_card;
 		}
 	}
 cont:
