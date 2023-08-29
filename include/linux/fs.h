69881be3d9a00cca770886af40913cfc5274b2d0
Christian Brauner <brauner@kernel.org>
2023-08-29 17:23:56 +0200

    fs: export sget_dev()
    They will be used for mtd devices as well.
    
    Acked-by: Richard Weinberger <richard@nod.at>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Message-Id: <20230829-vfs-super-mtd-v1-1-fecb572e5df3@kernel.org>
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2397,6 +2397,7 @@ struct super_block *sget(struct file_system_type *type,
 			int (*test)(struct super_block *,void *),
 			int (*set)(struct super_block *,void *),
 			int flags, void *data);
+struct super_block *sget_dev(struct fs_context *fc, dev_t dev);
 
 /* Alas, no aliases. Too much hassle with bringing module.h everywhere */
 #define fops_get(fops) \
