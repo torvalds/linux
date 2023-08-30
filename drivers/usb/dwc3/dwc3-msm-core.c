cbd62a9e555c40e8e399adc03f30f106399d6c04
Udipto Goswami <quic_ugoswami@quicinc.com>
2023-08-30 13:24:20 +0530

    usb: dwc3: msm: Avoid Noc error after of_platform_depopulate
    Currently if dwc3_probe didn't complete before the dwc3_msm_core_init
    is called, then the of_platform_depopulate will be called which makes
    sure that the child driver is teared down.
    However, if within this time the usespace writes the UDC in turn calls
    gadget_pullup will let the driver trigger gadget_start. At this point
    the trbs can be allocated from an  unknown or stale dma pool which when
    is tried to access from the controller will lead to a NOc error stating
    that the Stream id is known.
    
    Fix this by introducing wait_for_device_probe which would make sure
    that all the probes complete before even driver start the process of
    getting the drvdata.
    
    Change-Id: I241329c42fcdba3dc25d2765d5bd01ae2ce2d35d
    Signed-off-by: Udipto Goswami <quic_ugoswami@quicinc.com>

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -5751,6 +5751,12 @@ static int dwc3_msm_core_init(struct dwc3_msm *mdwc)
 		goto err;
 	}
 
+	/*
+	 * Wait for child's probe completion before retrieving the
+	 * driver data to make sure its populated properly.
+	 */
+	wait_for_device_probe();
+
 	mdwc->dwc3 = of_find_device_by_node(dwc3_node);
 	of_node_put(dwc3_node);
 	if (!mdwc->dwc3) {
