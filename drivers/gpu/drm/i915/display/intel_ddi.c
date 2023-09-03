183670347b060521920a81f84ff7f10e227ebe05
Jim Cromie <jim.cromie@gmail.com>
2023-09-03 12:46:00 -0600

    drm/i915: add trailing newlines to msgs
    By at least strong convention, a print-buffer's trailing newline says
    "message complete, send it".  The exception (no TNL, followed by a call
    to pr_cont) proves the general rule.
    
    Most DRM.debug calls already comport with this: 207 DRM_DEV_DEBUG,
    1288 drm_dbg.  Clean up the remainders, in maintainer sized chunks.
    
    No functional changes.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    Reviewed-by: Andi Shyti <andi.shyti@linux.intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    [Rodrigo changed subject while pushing]
    Link: https://patchwork.freedesktop.org/patch/msgid/20230903184607.272198-4-jim.cromie@gmail.com

--- drivers/gpu/drm/i915/display/intel_ddi.c
+++ drivers/gpu/drm/i915/display/intel_ddi.c
@@ -4173,7 +4173,7 @@ static int intel_ddi_compute_config_late(struct intel_encoder *encoder,
 	struct drm_connector *connector = conn_state->connector;
 	u8 port_sync_transcoders = 0;
 
-	drm_dbg_kms(&i915->drm, "[ENCODER:%d:%s] [CRTC:%d:%s]",
+	drm_dbg_kms(&i915->drm, "[ENCODER:%d:%s] [CRTC:%d:%s]\n",
 		    encoder->base.base.id, encoder->base.name,
 		    crtc_state->uapi.crtc->base.id, crtc_state->uapi.crtc->name);
 
