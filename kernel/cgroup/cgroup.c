ffed79e366a08959be9ff47c838d5e42d059c188
Greg Kroah-Hartman <gregkh@google.com>
2023-09-07 10:48:30 +0000

    Revert "sched/psi: use kernfs polling functions for PSI trigger polling"
    This reverts commit 92cc0153324b6ae8577a39f5bf2cd83c9a34ea6a which is
    commit aff037078ecaecf34a7c2afab1341815f90fba5e upstream.
    
    It is part of a patch series that breaks the Android API.  If this
    series is needed in Android devices in the future, it can come back in
    an ABI-safe manner.
    
    Bug: 161946584
    Change-Id: I2c941ec44d5261a914d95591b7918e46f7db825a
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -3777,7 +3777,7 @@ static ssize_t pressure_write(struct kernfs_open_file *of, char *buf,
 	}
 
 	psi = cgroup_psi(cgrp);
-	new = psi_trigger_create(psi, buf, res, of->file, of);
+	new = psi_trigger_create(psi, buf, res, of->file);
 	if (IS_ERR(new)) {
 		cgroup_put(cgrp);
 		return PTR_ERR(new);
