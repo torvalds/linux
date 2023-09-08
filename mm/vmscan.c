bbc9d3bc0b4866a857f0a65f72c4056a2cdb1dd4
xiaosong.ma <xiaosong.ma@unisoc.com>
2023-09-08 10:58:08 +0800

    ANDROID: vendor_hooks: mm: Add tune_swappiness vendor hook in get_swappiness()
    Add hook in get_swappiness() for customized swappiness when lru_gen is enabled.
    
    Bug: 299548382
    Test: buid pass
    Change-Id: If15cb4f71fda6c0b24359f8dc439a090a5434dc9
    Signed-off-by: xiaosong.ma <xiaosong.ma@unisoc.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3221,6 +3221,7 @@ static struct lruvec *get_lruvec(struct mem_cgroup *memcg, int nid)
 
 static int get_swappiness(struct lruvec *lruvec, struct scan_control *sc)
 {
+	int swappiness;
 	struct mem_cgroup *memcg = lruvec_memcg(lruvec);
 	struct pglist_data *pgdat = lruvec_pgdat(lruvec);
 
@@ -3231,7 +3232,10 @@ static int get_swappiness(struct lruvec *lruvec, struct scan_control *sc)
 		mem_cgroup_get_nr_swap_pages(memcg) <= 0)
 		return 0;
 
-	return mem_cgroup_swappiness(memcg);
+	swappiness = mem_cgroup_swappiness(memcg);
+	trace_android_vh_tune_swappiness(&swappiness);
+
+	return swappiness;
 }
 
 static int get_nr_gens(struct lruvec *lruvec, int type)
