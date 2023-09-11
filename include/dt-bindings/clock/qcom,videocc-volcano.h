7555920df598a928da6d492bce10a60d6dd0f1af
Ajit Pandey <quic_ajipan@quicinc.com>
2023-09-12 00:09:25 +0530

    bindings: clock: qcom: Add support for clock IDs for volcano
    Add the clock handles for CAMCC/DISPCC/GCC/GPUCC/VIDEOCC, so
    that the clients can request on the clock ids.
    
    Change-Id: Id9f72c77d34e4aa48394c087282b748573cdc131
    Signed-off-by: Ajit Pandey <quic_ajipan@quicinc.com>

---
+++ include/dt-bindings/clock/qcom,videocc-volcano.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_VIDEO_CC_VOLCANO_H
+#define _DT_BINDINGS_CLK_QCOM_VIDEO_CC_VOLCANO_H
+
+/* VIDEO_CC clocks */
+#define VIDEO_CC_PLL0						0
+#define VIDEO_CC_AHB_CLK					1
+#define VIDEO_CC_AHB_CLK_SRC					2
+#define VIDEO_CC_MVS0_CLK					3
+#define VIDEO_CC_MVS0_CLK_SRC					4
+#define VIDEO_CC_MVS0_DIV_CLK_SRC				5
+#define VIDEO_CC_MVS0_SHIFT_CLK					6
+#define VIDEO_CC_MVS0C_CLK					7
+#define VIDEO_CC_MVS0C_DIV2_DIV_CLK_SRC				8
+#define VIDEO_CC_MVS0C_SHIFT_CLK				9
+#define VIDEO_CC_SLEEP_CLK					10
+#define VIDEO_CC_SLEEP_CLK_SRC					11
+#define VIDEO_CC_XO_CLK						12
+#define VIDEO_CC_XO_CLK_SRC					13
+
+/* VIDEO_CC resets */
+#define VIDEO_CC_INTERFACE_BCR					0
+#define VIDEO_CC_MVS0_BCR					1
+#define VIDEO_CC_MVS0C_CLK_ARES					2
+#define VIDEO_CC_MVS0C_BCR					3
+
+#endif
