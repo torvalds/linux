32a4ec211d4164e667d9d0b807fadf02053cd2e9
Alexey Dobriyan <adobriyan@gmail.com>
2023-09-12 19:22:24 +0300

    uapi: stddef.h: Fix __DECLARE_FLEX_ARRAY for C++
    __DECLARE_FLEX_ARRAY(T, member) macro expands to
    
    	struct {
    		struct {} __empty_member;
    		T member[];
    	};
    
    which is subtly wrong in C++ because sizeof(struct{}) is 1 not 0,
    changing UAPI structures layouts.
    
    This can be fixed by expanding to
    
    	T member[];
    
    Now g++ doesn't like "T member[]" either, throwing errors on
    the following code:
    
    	struct S {
    		union {
    			T1 member1[];
    			T2 member2[];
    		};
    	};
    
    or
    
    	struct S {
    		T member[];
    	};
    
    Use "T member[0];" which seems to work and does the right thing wrt
    structure layout.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Fixes: 3080ea5553cc ("stddef: Introduce DECLARE_FLEX_ARRAY() helper")
    Link: https://lore.kernel.org/r/97242381-f1ec-4a4a-9472-1a464f575657@p183
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- include/uapi/linux/stddef.h
+++ include/uapi/linux/stddef.h
@@ -29,6 +29,11 @@
 		struct TAG { MEMBERS } ATTRS NAME; \
 	}
 
+#ifdef __cplusplus
+/* sizeof(struct{}) is 1 in C++, not 0, can't use C version of the macro. */
+#define __DECLARE_FLEX_ARRAY(T, member)	\
+	T member[0]
+#else
 /**
  * __DECLARE_FLEX_ARRAY() - Declare a flexible array usable in a union
  *
@@ -44,6 +49,7 @@
 		struct { } __empty_ ## NAME; \
 		TYPE NAME[]; \
 	}
+#endif
 
 #ifndef __counted_by
 #define __counted_by(m)
