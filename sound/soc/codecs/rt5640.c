1698854b03a5190a44712ca827ed9c5b5793d090
Hans de Goede <hdegoede@redhat.com>
2023-09-12 13:32:44 +0200

    ASoC: rt5640: Fix IRQ not being free-ed for HDA jack detect mode
    [ Upstream commit 8c8bf3df6b7c0ed1c4dd373b23eb0ce13a63f452 ]
    
    Set "rt5640->irq_requested = true" after a successful request_irq()
    in rt5640_enable_hda_jack_detect(), so that rt5640_disable_jack_detect()
    properly frees the IRQ.
    
    This fixes the IRQ not being freed on rmmod / driver unbind.
    
    Fixes: 2b9c8d2b3c89 ("ASoC: rt5640: Add the HDA header support")
    Cc: Oder Chiou <oder_chiou@realtek.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://lore.kernel.org/r/20230912113245.320159-6-hdegoede@redhat.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- sound/soc/codecs/rt5640.c
+++ sound/soc/codecs/rt5640.c
@@ -2624,6 +2624,7 @@ static void rt5640_enable_hda_jack_detect(
 		rt5640->irq = -ENXIO;
 		return;
 	}
+	rt5640->irq_requested = true;
 
 	/* sync initial jack state */
 	queue_delayed_work(system_long_wq, &rt5640->jack_work, 0);
