82f575a7e81192588a02eafc5837820a0f661e6b
Michal Grzedzicki <mge@meta.com>
2023-09-13 08:56:10 -0700

    scsi: pm80xx: Use phy-specific SAS address when sending PHY_START command
    [ Upstream commit 71996bb835aed58c7ec4967be1d05190a27339ec ]
    
    Some cards have more than one SAS address. Using an incorrect address
    causes communication issues with some devices like expanders.
    
    Closes: https://lore.kernel.org/linux-kernel/A57AEA84-5CA0-403E-8053-106033C73C70@fb.com/
    Signed-off-by: Michal Grzedzicki <mge@meta.com>
    Link: https://lore.kernel.org/r/20230913155611.3183612-1-mge@meta.com
    Acked-by: Jack Wang <jinpu.wang@ionos.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/scsi/pm8001/pm80xx_hwi.c
+++ drivers/scsi/pm8001/pm80xx_hwi.c
@@ -4803,7 +4803,7 @@ pm80xx_chip_phy_start_req(struct pm8001_hba_info *pm8001_ha, u8 phy_id)
 	payload.sas_identify.dev_type = SAS_END_DEVICE;
 	payload.sas_identify.initiator_bits = SAS_PROTOCOL_ALL;
 	memcpy(payload.sas_identify.sas_addr,
-	  &pm8001_ha->sas_addr, SAS_ADDR_SIZE);
+		&pm8001_ha->phy[phy_id].dev_sas_addr, SAS_ADDR_SIZE);
 	payload.sas_identify.phy_id = phy_id;
 
 	return pm8001_mpi_build_cmd(pm8001_ha, 0, opcode, &payload,
