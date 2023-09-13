e925bcee7d8ddd6e8990c81c89d90a06014b8ba5
Rob Herring <robh@kernel.org>
2023-09-13 14:54:24 +0100

    pmdomain: starfive: Explicitly include correct DT includes
    The DT of_device.h and of_platform.h date back to the separate
    of_platform_bus_type before it as merged into the regular platform bus.
    As part of that merge prepping Arm DT support 13 years ago, they
    "temporarily" include each other. They also include platform_device.h
    and of.h. As a result, there's a pretty much random mix of those include
    files used throughout the tree. In order to detangle these headers and
    replace the implicit includes with struct declarations, users need to
    explicitly include the correct includes.
    
    Signed-off-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>
    Link: https://lore.kernel.org/r/20230913-swooned-ecosphere-aff4a05c1556@spud
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/pmdomain/starfive/jh71xx-pmu.c
+++ drivers/pmdomain/starfive/jh71xx-pmu.c
@@ -10,7 +10,6 @@
 #include <linux/iopoll.h>
 #include <linux/module.h>
 #include <linux/of.h>
-#include <linux/of_device.h>
 #include <linux/platform_device.h>
 #include <linux/pm_domain.h>
 #include <dt-bindings/power/starfive,jh7110-pmu.h>
