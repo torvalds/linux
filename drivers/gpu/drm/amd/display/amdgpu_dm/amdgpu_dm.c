2de19022c5d7ff519dd5b9690f7713267bd1abfe
Hamza Mahfooz <hamza.mahfooz@amd.com>
2023-09-13 14:48:08 -0400

    drm/amd/display: fix the ability to use lower resolution modes on eDP
    On eDP we can receive invalid modes from dm_update_crtc_state() for
    entirely new streams for which drm_mode_set_crtcinfo() shouldn't be
    called on. So, instead of calling drm_mode_set_crtcinfo() from within
    create_stream_for_sink() we can instead call it from
    amdgpu_dm_connector_mode_valid(). Since, we are guaranteed to only call
    drm_mode_set_crtcinfo() for valid modes from that function (invalid
    modes are rejected by that callback) and that is the only user
    of create_validate_stream_for_sink() that we need to call
    drm_mode_set_crtcinfo() for (as before commit cb841d27b876
    ("drm/amd/display: Always pass connector_state to stream validation"),
    that is the only place where create_validate_stream_for_sink()'s
    dm_state was NULL).
    
    Cc: stable@vger.kernel.org
    Link: https://gitlab.freedesktop.org/drm/amd/-/issues/2693
    Fixes: cb841d27b876 ("drm/amd/display: Always pass connector_state to stream validation")
    Tested-by: Mark Broadworth <mark.broadworth@amd.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Hamza Mahfooz <hamza.mahfooz@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -6098,8 +6098,6 @@ create_stream_for_sink(struct amdgpu_dm_connector *aconnector,
 
 	if (recalculate_timing)
 		drm_mode_set_crtcinfo(&saved_mode, 0);
-	else if (!old_stream)
-		drm_mode_set_crtcinfo(&mode, 0);
 
 	/*
 	 * If scaling is enabled and refresh rate didn't change
@@ -6660,6 +6658,8 @@ enum drm_mode_status amdgpu_dm_connector_mode_valid(struct drm_connector *connec
 		DRM_ERROR("dc_sink is NULL!\n");
 		goto fail;
 	}
+
+	drm_mode_set_crtcinfo(mode, 0);
 
 	stream = create_validate_stream_for_sink(aconnector, mode,
 						 to_dm_connector_state(connector->state),
