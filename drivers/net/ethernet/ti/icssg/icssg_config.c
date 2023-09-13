0a205f0fe8dd2ab80531b54f01d6483f37120c0e
MD Danish Anwar <danishanwar@ti.com>
2023-09-13 14:40:11 +0530

    net: ti: icssg-prueth: Add support for half duplex operation
    This patch adds support for half duplex operation at 10M and 100M link
    speeds for AM654x/AM64x devices.
    - Driver configures rand_seed, a random number, in DMEM HD_RAND_SEED_OFFSET
    field, which will be used by firmware for Back off time calculation.
    - Driver informs FW about half duplex link operation in DMEM
    PORT_LINK_SPEED_OFFSET field by setting bit 7 for 10/100M HD.
    
    Hence, the half duplex operation depends on board design the
    "ti,half-duplex-capable" property has to be enabled for ICSS-G ports if HW
    is capable to perform half duplex.
    
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Reviewed-by: Roger Quadros <rogerq@kernel.org>
    Signed-off-by: MD Danish Anwar <danishanwar@ti.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/ti/icssg/icssg_config.c
+++ drivers/net/ethernet/ti/icssg/icssg_config.c
@@ -433,6 +433,17 @@ int emac_set_port_state(struct prueth_emac *emac,
 	return ret;
 }
 
+void icssg_config_half_duplex(struct prueth_emac *emac)
+{
+	u32 val;
+
+	if (!emac->half_duplex)
+		return;
+
+	val = get_random_u32();
+	writel(val, emac->dram.va + HD_RAND_SEED_OFFSET);
+}
+
 void icssg_config_set_speed(struct prueth_emac *emac)
 {
 	u8 fw_speed;
@@ -452,6 +463,9 @@ void icssg_config_set_speed(struct prueth_emac *emac)
 		netdev_err(emac->ndev, "Unsupported link speed\n");
 		return;
 	}
+
+	if (emac->duplex == DUPLEX_HALF)
+		fw_speed |= FW_LINK_SPEED_HD;
 
 	writeb(fw_speed, emac->dram.va + PORT_LINK_SPEED_OFFSET);
 }
