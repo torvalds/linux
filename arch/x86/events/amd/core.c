599522d9d2e19d6240e4312577f1c5f3ffca22f6
Breno Leitao <leitao@debian.org>
2023-09-14 19:58:40 +0530

    perf/x86/amd: Do not WARN() on every IRQ
    Zen 4 systems running buggy microcode can hit a WARN_ON() in the PMI
    handler, as shown below, several times while perf runs. A simple
    `perf top` run is enough to render the system unusable:
    
      WARNING: CPU: 18 PID: 20608 at arch/x86/events/amd/core.c:944 amd_pmu_v2_handle_irq+0x1be/0x2b0
    
    This happens because the Performance Counter Global Status Register
    (PerfCntGlobalStatus) has one or more bits set which are considered
    reserved according to the "AMD64 Architecture Programmer’s Manual,
    Volume 2: System Programming, 24593":
    
      https://www.amd.com/system/files/TechDocs/24593.pdf
    
    To make this less intrusive, warn just once if any reserved bit is set
    and prompt the user to update the microcode. Also sanitize the value to
    what the code is handling, so that the overflow events continue to be
    handled for the number of counters that are known to be sane.
    
    Going forward, the following microcode patch levels are recommended
    for Zen 4 processors in order to avoid such issues with reserved bits:
    
      Family=0x19 Model=0x11 Stepping=0x01: Patch=0x0a10113e
      Family=0x19 Model=0x11 Stepping=0x02: Patch=0x0a10123e
      Family=0x19 Model=0xa0 Stepping=0x01: Patch=0x0aa00116
      Family=0x19 Model=0xa0 Stepping=0x02: Patch=0x0aa00212
    
    Commit f2eb058afc57 ("linux-firmware: Update AMD cpu microcode") from
    the linux-firmware tree has binaries that meet the minimum required
    patch levels.
    
      [ sandipan: - add message to prompt users to update microcode
                  - rework commit message and call out required microcode levels ]
    
    Fixes: 7685665c390d ("perf/x86/amd/core: Add PerfMonV2 overflow handling")
    Reported-by: Jirka Hladky <jhladky@redhat.com>
    Signed-off-by: Breno Leitao <leitao@debian.org>
    Signed-off-by: Sandipan Das <sandipan.das@amd.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/all/3540f985652f41041e54ee82aa53e7dbd55739ae.1694696888.git.sandipan.das@amd.com/

--- arch/x86/events/amd/core.c
+++ arch/x86/events/amd/core.c
@@ -886,7 +886,7 @@ static int amd_pmu_v2_handle_irq(struct pt_regs *regs)
 	struct hw_perf_event *hwc;
 	struct perf_event *event;
 	int handled = 0, idx;
-	u64 status, mask;
+	u64 reserved, status, mask;
 	bool pmu_enabled;
 
 	/*
@@ -910,6 +910,14 @@ static int amd_pmu_v2_handle_irq(struct pt_regs *regs)
 		amd_pmu_lbr_read();
 		status &= ~GLOBAL_STATUS_LBRS_FROZEN;
 	}
+
+	reserved = status & ~amd_pmu_global_cntr_mask;
+	if (reserved)
+		pr_warn_once("Reserved PerfCntrGlobalStatus bits are set (0x%llx), please consider updating microcode\n",
+			     reserved);
+
+	/* Clear any reserved bits set by buggy microcode */
+	status &= amd_pmu_global_cntr_mask;
 
 	for (idx = 0; idx < x86_pmu.num_counters; idx++) {
 		if (!test_bit(idx, cpuc->active_mask))
