972ba0bcac67397a71205e60c18d358efc7ec748
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2023-09-14 21:16:18 +0300

    media: i2c: imx355: Drop check for reentrant .s_stream()
    The subdev .s_stream() operation shall not be called to start streaming
    on an already started subdev, or stop streaming on a stopped subdev.
    Remove the check that guards against that condition.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

--- drivers/media/i2c/imx355.c
+++ drivers/media/i2c/imx355.c
@@ -1436,10 +1436,6 @@ static int imx355_set_stream(struct v4l2_subdev *sd, int enable)
 	int ret = 0;
 
 	mutex_lock(&imx355->mutex);
-	if (imx355->streaming == enable) {
-		mutex_unlock(&imx355->mutex);
-		return 0;
-	}
 
 	if (enable) {
 		ret = pm_runtime_resume_and_get(&client->dev);
