55ec67f78c2ce48980ffc3eb03259fa684513928
Giovanni Cabiddu <giovanni.cabiddu@intel.com>
2023-09-14 10:55:49 +0100

    crypto: qat - fix unregistration of compression algorithms
    [ Upstream commit 11af152f784d9eca54e193140891ac52de36e9a9 ]
    
    The function adf_dev_init(), through the subsystem qat_compression,
    populates the list of list of compression instances
    accel_dev->compression_list. If the list of instances is not empty,
    the function adf_dev_start() will then call qat_compression_registers()
    register the compression algorithms into the crypto framework.
    
    If any of the functions in adf_dev_start() fail, the caller of such
    function, in the error path calls adf_dev_down() which in turn call
    adf_dev_stop() and adf_dev_shutdown(), see for example the function
    state_store in adf_sriov.c.
    However, if the registration of compression algorithms is not done,
    adf_dev_stop() will try to unregister the algorithms regardless.
    This might cause the counter active_devs in qat_compression.c to get
    to a negative value.
    
    Add a new state, ADF_STATUS_COMPRESSION_ALGS_REGISTERED, which tracks
    if the compression algorithms are registered into the crypto framework.
    Then use this to unregister the algorithms if such flag is set. This
    ensures that the compression algorithms are only unregistered if
    previously registered.
    
    Fixes: 1198ae56c9a5 ("crypto: qat - expose deflate through acomp api for QAT GEN2")
    Signed-off-by: Giovanni Cabiddu <giovanni.cabiddu@intel.com>
    Reviewed-by: Adam Guerin <adam.guerin@intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/crypto/intel/qat/qat_common/adf_init.c
+++ drivers/crypto/intel/qat/qat_common/adf_init.c
@@ -240,6 +240,7 @@ static int adf_dev_start(struct adf_accel_dev *accel_dev)
 		clear_bit(ADF_STATUS_STARTED, &accel_dev->status);
 		return -EFAULT;
 	}
+	set_bit(ADF_STATUS_COMP_ALGS_REGISTERED, &accel_dev->status);
 
 	adf_dbgfs_add(accel_dev);
 
@@ -280,8 +281,10 @@ static void adf_dev_stop(struct adf_accel_dev *accel_dev)
 	}
 	clear_bit(ADF_STATUS_CRYPTO_ALGS_REGISTERED, &accel_dev->status);
 
-	if (!list_empty(&accel_dev->compression_list))
+	if (!list_empty(&accel_dev->compression_list) &&
+	    test_bit(ADF_STATUS_COMP_ALGS_REGISTERED, &accel_dev->status))
 		qat_comp_algs_unregister();
+	clear_bit(ADF_STATUS_COMP_ALGS_REGISTERED, &accel_dev->status);
 
 	list_for_each(list_itr, &service_table) {
 		service = list_entry(list_itr, struct service_hndl, list);
