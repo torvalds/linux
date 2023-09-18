c17195c3972b42745460f09b9778e8d9061762a4
Chuck Lever <chuck.lever@oracle.com>
2023-09-18 09:59:39 -0400

    NFSD: Add nfsd4_encode_fattr4_maxread()
    Refactor the encoder for FATTR4_MAXREAD into a helper. In a
    subsequent patch, this helper will be called from a bitmask loop.
    
    Reviewed-by: Jeff Layton <jlayton@kernel.org>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- fs/nfsd/nfs4xdr.c
+++ fs/nfsd/nfs4xdr.c
@@ -3171,6 +3171,12 @@ static __be32 nfsd4_encode_fattr4_maxname(struct xdr_stream *xdr,
 	return nfsd4_encode_uint32_t(xdr, args->statfs.f_namelen);
 }
 
+static __be32 nfsd4_encode_fattr4_maxread(struct xdr_stream *xdr,
+					  const struct nfsd4_fattr_args *args)
+{
+	return nfsd4_encode_uint64_t(xdr, svc_max_payload(args->rqstp));
+}
+
 /*
  * Note: @fhp can be NULL; in this case, we might have to compose the filehandle
  * ourselves.
@@ -3440,10 +3446,9 @@ nfsd4_encode_fattr(struct xdr_stream *xdr, struct svc_fh *fhp,
 			goto out;
 	}
 	if (bmval0 & FATTR4_WORD0_MAXREAD) {
-		p = xdr_reserve_space(xdr, 8);
-		if (!p)
-			goto out_resource;
-		p = xdr_encode_hyper(p, (u64) svc_max_payload(rqstp));
+		status = nfsd4_encode_fattr4_maxread(xdr, &args);
+		if (status != nfs_ok)
+			goto out;
 	}
 	if (bmval0 & FATTR4_WORD0_MAXWRITE) {
 		p = xdr_reserve_space(xdr, 8);
