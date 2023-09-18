ec2926f5f6221b550fbed6912dafd6ef72b29131
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2023-09-18 04:54:31 -0700

    HID: nvidia-shield: Fix some missing function calls() in the probe error handling path
    The commit in Fixes updated the error handling path of
    thunderstrike_create() and the remove function but not the error handling
    path of shield_probe(), should an error occur after a successful
    thunderstrike_create() call.
    
    Add the missing calls.
    
    Fixes: 3ab196f88237 ("HID: nvidia-shield: Add battery support for Thunderstrike")
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Reviewed-by: Rahul Rameshbabu <rrameshbabu@nvidia.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-nvidia-shield.c
+++ drivers/hid/hid-nvidia-shield.c
@@ -1058,7 +1058,7 @@ static int shield_probe(struct hid_device *hdev, const struct hid_device_id *id)
 	ret = hid_hw_start(hdev, HID_CONNECT_HIDINPUT);
 	if (ret) {
 		hid_err(hdev, "Failed to start HID device\n");
-		goto err_haptics;
+		goto err_ts_create;
 	}
 
 	ret = hid_hw_open(hdev);
@@ -1073,10 +1073,12 @@ static int shield_probe(struct hid_device *hdev, const struct hid_device_id *id)
 
 err_stop:
 	hid_hw_stop(hdev);
-err_haptics:
+err_ts_create:
+	power_supply_unregister(ts->base.battery_dev.psy);
 	if (ts->haptics_dev)
 		input_unregister_device(ts->haptics_dev);
 	led_classdev_unregister(&ts->led_dev);
+	ida_free(&thunderstrike_ida, ts->id);
 	return ret;
 }
 
