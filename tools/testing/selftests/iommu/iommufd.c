bb812e0069ce5de9af2a7910951c8c95632cebe3
Nicolin Chen <nicolinc@nvidia.com>
2023-09-18 18:16:37 -0700

    iommufd/selftest: Iterate idev_ids in mock_domain's alloc_hwpt test
    The point in iterating variant->mock_domains is to test the idev_ids[0]
    and idev_ids[1]. So use it instead of keeping testing idev_ids[0] only.
    
    Link: https://lore.kernel.org/r/20230919011637.16483-1-nicolinc@nvidia.com
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- tools/testing/selftests/iommu/iommufd.c
+++ tools/testing/selftests/iommu/iommufd.c
@@ -1407,7 +1407,7 @@ TEST_F(iommufd_mock_domain, alloc_hwpt)
 		uint32_t stddev_id;
 		uint32_t hwpt_id;
 
-		test_cmd_hwpt_alloc(self->idev_ids[0], self->ioas_id, &hwpt_id);
+		test_cmd_hwpt_alloc(self->idev_ids[i], self->ioas_id, &hwpt_id);
 		test_cmd_mock_domain(hwpt_id, &stddev_id, NULL, NULL);
 		test_ioctl_destroy(stddev_id);
 		test_ioctl_destroy(hwpt_id);
