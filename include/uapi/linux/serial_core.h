00c77bcf2a624e6863121ea0eeb37e35c58ca5d0
Wolfram Sang <wsa+renesas@sang-engineering.com>
2023-09-22 08:36:42 +0200

    serial: core: add comment about definitely used port types
    When port type 18 was removed, it was deduced that the code could go but
    its define has to stay because it is used in userspace. Share that
    knowledge by adding a comment about it.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Link: https://lore.kernel.org/r/20230922063642.4120-3-wsa+renesas@sang-engineering.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/uapi/linux/serial_core.h
+++ include/uapi/linux/serial_core.h
@@ -11,6 +11,8 @@
  * The type definitions.  These are from Ted Ts'o's serial.h
  * By historical reasons the values from 0 to 13 are defined
  * in the include/uapi/linux/serial.h, do not define them here.
+ * Values 0 to 19 are used by setserial from busybox and must never
+ * be modified.
  */
 #define PORT_NS16550A	14
 #define PORT_XSCALE	15
