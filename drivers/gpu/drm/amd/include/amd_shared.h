e1133ac8117672c22c904db52ad2f9995da044b9
Juntong Deng <juntong.deng@outlook.com>
2023-09-24 21:00:26 +0100

    drm/amdgpu: Add description for AMD_IP_BLOCK_TYPE_VPE
    AMD_IP_BLOCK_TYPE_VPE is a new IP BLOCK type for Video Processing Engine,
    but currently lacks description.
    
    Fix the documentation warning:
    warning: Enum value 'AMD_IP_BLOCK_TYPE_VPE' not described in
    enum 'amd_ip_block_type'
    
    Signed-off-by: Juntong Deng <juntong.deng@outlook.com>
    Reviewed-by: Lang Yu <lang.yu@amd.com>
    Acked-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/include/amd_shared.h
+++ drivers/gpu/drm/amd/include/amd_shared.h
@@ -83,6 +83,7 @@ enum amd_apu_flags {
 * @AMD_IP_BLOCK_TYPE_VCN: Video Core/Codec Next
 * @AMD_IP_BLOCK_TYPE_MES: Micro-Engine Scheduler
 * @AMD_IP_BLOCK_TYPE_JPEG: JPEG Engine
+* @AMD_IP_BLOCK_TYPE_VPE: Video Processing Engine
 * @AMD_IP_BLOCK_TYPE_UMSCH_MM: User Mode Schduler for Multimedia
 * @AMD_IP_BLOCK_TYPE_NUM: Total number of IP block types
 */
