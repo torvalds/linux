adf41f47375e07a1c8c96bc53945ae6e30d6615d
Ulf Hansson <ulf.hansson@linaro.org>
2023-09-25 15:17:09 +0200

    BACKPORT: OPP: Add dev_pm_opp_add_dynamic() to allow more flexibility
    The dev_pm_opp_add() API is limited to add dynamic OPPs with a frequency
    and a voltage level. To enable more flexibility, let's add a new API,
    dev_pm_opp_add_dynamic() that's takes a struct dev_pm_opp_data* instead of
    a list of in-parameters.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    
    Bug: 323966425
    Change-Id: I37e08f4b4c9f42e1f8c42a876d46d89447fda7a1
    (cherry picked from commit 248a38d5cc3f3505e6cfbbc0514435c9f1ba00af)
    [anantg: Kept dev_pm_opp_add() as an exported API to maintain the ABI]
    Signed-off-by: Nikunj Kela <quic_nkela@quicinc.com>
    Signed-off-by: Anant Goel <quic_anantg@quicinc.com>

--- include/linux/pm_opp.h
+++ include/linux/pm_opp.h
@@ -92,6 +92,16 @@ struct dev_pm_opp_config {
 	struct device ***virt_devs;
 };
 
+/**
+ * struct dev_pm_opp_data - The data to use to initialize an OPP.
+ * @freq: The clock rate in Hz for the OPP.
+ * @u_volt: The voltage in uV for the OPP.
+ */
+struct dev_pm_opp_data {
+	unsigned long freq;
+	unsigned long u_volt;
+};
+
 #if defined(CONFIG_PM_OPP)
 
 struct opp_table *dev_pm_opp_get_opp_table(struct device *dev);
@@ -140,8 +150,9 @@ struct dev_pm_opp *dev_pm_opp_find_bw_floor(struct device *dev,
 
 void dev_pm_opp_put(struct dev_pm_opp *opp);
 
-int dev_pm_opp_add(struct device *dev, unsigned long freq,
-		   unsigned long u_volt);
+int dev_pm_opp_add_dynamic(struct device *dev, struct dev_pm_opp_data *opp);
+int dev_pm_opp_add(struct device *dev, unsigned long freq, unsigned long u_volt);
+
 void dev_pm_opp_remove(struct device *dev, unsigned long freq);
 void dev_pm_opp_remove_all_dynamic(struct device *dev);
 
@@ -291,8 +302,14 @@ static inline struct dev_pm_opp *dev_pm_opp_find_bw_floor(struct device *dev,
 
 static inline void dev_pm_opp_put(struct dev_pm_opp *opp) {}
 
-static inline int dev_pm_opp_add(struct device *dev, unsigned long freq,
-					unsigned long u_volt)
+static inline int
+dev_pm_opp_add_dynamic(struct device *dev, struct dev_pm_opp_data *opp)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int
+dev_pm_opp_add(struct device *dev, struct dev_pm_opp_data *opp, unsigned long u_volt)
 {
 	return -EOPNOTSUPP;
 }
