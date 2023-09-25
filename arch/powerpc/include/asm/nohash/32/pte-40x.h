ed815bd3fe9b14a742e2ae094f7f55f70918dbbc
Christophe Leroy <christophe.leroy@csgroup.eu>
2023-09-25 20:31:46 +0200

    powerpc/40x: Introduce _PAGE_READ and remove _PAGE_USER
    _PAGE_USER is used to select the zone. Today zone 0 is kernel
    and zone 1 is user.
    
    To implement _PAGE_NONE, _PAGE_USER is cleared, leading to no access
    for user but kernel still has access to the page so it's possible for
    a user application to write in that page by using a kernel function
    as trampoline.
    
    What is really wanted is to have user rights on pages below TASK_SIZE
    and no user rights on pages above TASK_SIZE. Use zones for that.
    There are 16 zones so lets use the 4 upper address bits to set the
    zone and declare zone rights based on TASK_SIZE.
    
    Then drop _PAGE_USER and reuse it as _PAGE_READ that will be checked
    in Data TLB miss handler. That will properly handle PAGE_NONE for
    both kernel and user.
    
    In addition, it partially implements execute-only right. The
    implementation won't be complete because once a TLB has been loaded
    via the Instruction TLB miss handler, it will be possible to read
    the page. But at least it can't be read unless it is executed first.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://msgid.link/2a13e3ba8a5dec43143cc1f9a91ec71ea1529f3c.1695659959.git.christophe.leroy@csgroup.eu

--- arch/powerpc/include/asm/nohash/32/pte-40x.h
+++ arch/powerpc/include/asm/nohash/32/pte-40x.h
@@ -42,25 +42,18 @@
 #define _PAGE_PRESENT	0x002	/* software: PTE contains a translation */
 #define	_PAGE_NO_CACHE	0x004	/* I: caching is inhibited */
 #define	_PAGE_WRITETHRU	0x008	/* W: caching is write-through */
-#define	_PAGE_USER	0x010	/* matches one of the zone permission bits */
+#define	_PAGE_READ	0x010	/* software: read permission */
 #define	_PAGE_SPECIAL	0x020	/* software: Special page */
 #define	_PAGE_DIRTY	0x080	/* software: dirty page */
-#define _PAGE_RW	0x100	/* hardware: WR, anded with dirty in exception */
+#define _PAGE_WRITE	0x100	/* hardware: WR, anded with dirty in exception */
 #define _PAGE_EXEC	0x200	/* hardware: EX permission */
 #define _PAGE_ACCESSED	0x400	/* software: R: page referenced */
-
-#define _PAGE_WRITE	_PAGE_RW
 
 /* No page size encoding in the linux PTE */
 #define _PAGE_PSIZE		0
 
 /* cache related flags non existing on 40x */
 #define _PAGE_COHERENT	0
-
-#define _PAGE_KERNEL_RO		0
-#define _PAGE_KERNEL_ROX	_PAGE_EXEC
-#define _PAGE_KERNEL_RW		(_PAGE_DIRTY | _PAGE_RW)
-#define _PAGE_KERNEL_RWX	(_PAGE_DIRTY | _PAGE_RW | _PAGE_EXEC)
 
 #define _PMD_PRESENT	0x400	/* PMD points to page of PTEs */
 #define _PMD_PRESENT_MASK	_PMD_PRESENT
@@ -74,14 +67,7 @@
 #define _PAGE_BASE_NC	(_PAGE_PRESENT | _PAGE_ACCESSED)
 #define _PAGE_BASE	(_PAGE_BASE_NC)
 
-/* Permission masks used to generate the __P and __S table */
-#define PAGE_NONE	__pgprot(_PAGE_BASE)
-#define PAGE_SHARED	__pgprot(_PAGE_BASE | _PAGE_USER | _PAGE_RW)
-#define PAGE_SHARED_X	__pgprot(_PAGE_BASE | _PAGE_USER | _PAGE_RW | _PAGE_EXEC)
-#define PAGE_COPY	__pgprot(_PAGE_BASE | _PAGE_USER)
-#define PAGE_COPY_X	__pgprot(_PAGE_BASE | _PAGE_USER | _PAGE_EXEC)
-#define PAGE_READONLY	__pgprot(_PAGE_BASE | _PAGE_USER)
-#define PAGE_READONLY_X	__pgprot(_PAGE_BASE | _PAGE_USER | _PAGE_EXEC)
+#include <asm/pgtable-masks.h>
 
 #endif /* __KERNEL__ */
 #endif /*  _ASM_POWERPC_NOHASH_32_PTE_40x_H */
