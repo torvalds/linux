270a1470408e44619a55be1079254bf2ba0567fb
Richard Acayan <mailingradian@gmail.com>
2023-09-25 19:42:48 -0400

    iommu/arm-smmu-qcom: Add SDM670 MDSS compatible
    Add the compatible for the MDSS client on the Snapdragon 670 so it can
    be properly configured by the IOMMU driver.
    
    Otherwise, there is an unhandled context fault.
    
    Signed-off-by: Richard Acayan <mailingradian@gmail.com>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Link: https://lore.kernel.org/r/20230925234246.900351-3-mailingradian@gmail.com
    Signed-off-by: Will Deacon <will@kernel.org>

--- drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
@@ -251,6 +251,7 @@ static const struct of_device_id qcom_smmu_client_of_match[] __maybe_unused = {
 	{ .compatible = "qcom,sc7280-mss-pil" },
 	{ .compatible = "qcom,sc8180x-mdss" },
 	{ .compatible = "qcom,sc8280xp-mdss" },
+	{ .compatible = "qcom,sdm670-mdss" },
 	{ .compatible = "qcom,sdm845-mdss" },
 	{ .compatible = "qcom,sdm845-mss-pil" },
 	{ .compatible = "qcom,sm6350-mdss" },
