3568cb6556695af163e930a75b1ed8f6dfa848ba
Irui Wang <irui.wang@mediatek.com>
2023-09-25 12:02:00 +0800

    media: mediatek: vcodec: add encoder power management helper functions
    Remove PM functions at start/stop streaming, add PM helper functions
    to get PM before encoding frame start and put PM after encoding frame
    done. Meanwhile, remove unnecessary clock operations.
    
    Signed-off-by: Irui Wang <irui.wang@mediatek.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

--- drivers/media/platform/mediatek/vcodec/encoder/mtk_vcodec_enc_pm.h
+++ drivers/media/platform/mediatek/vcodec/encoder/mtk_vcodec_enc_pm.h
@@ -10,7 +10,8 @@
 #include "mtk_vcodec_enc_drv.h"
 
 int mtk_vcodec_init_enc_clk(struct mtk_vcodec_enc_dev *dev);
-
+void mtk_vcodec_enc_pw_on(struct mtk_vcodec_pm *pm);
+void mtk_vcodec_enc_pw_off(struct mtk_vcodec_pm *pm);
 void mtk_vcodec_enc_clock_on(struct mtk_vcodec_pm *pm);
 void mtk_vcodec_enc_clock_off(struct mtk_vcodec_pm *pm);
 
