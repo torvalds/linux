7dbbf557b77946de4aa341a1cf24c1ebe7ec681a
Gregory Greenman <gregory.greenman@intel.com>
2023-09-26 11:07:21 +0300

    wifi: iwlwifi: bump FW API to 84 for AX/BZ/SC devices
    Start supporting API version 84 for new devices.
    
    Signed-off-by: Gregory Greenman <gregory.greenman@intel.com>
    Link: https://lore.kernel.org/r/20230926110319.eae20f9fdc06.Ifa9be6482121ea6df364bddc96ea6a7d101366b6@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/intel/iwlwifi/cfg/sc.c
+++ drivers/net/wireless/intel/iwlwifi/cfg/sc.c
@@ -10,7 +10,7 @@
 #include "fw/api/txq.h"
 
 /* Highest firmware API version supported */
-#define IWL_SC_UCODE_API_MAX	83
+#define IWL_SC_UCODE_API_MAX	84
 
 /* Lowest firmware API version supported */
 #define IWL_SC_UCODE_API_MIN	82
