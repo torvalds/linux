00f3696f7555d0890ae07b635e6ccbf39fd2eb3a
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2023-09-27 11:00:30 +0200

    net: appletalk: remove cops support
    The COPS Appletalk support is very old, never said to actually work
    properly, and the firmware code for the devices are under a very suspect
    license.  Remove it all to clear up the license issue, if it is still
    needed and actually used by anyone, we can add it back later once the
    license is cleared up.
    
    Reported-by: Prarit Bhargava <prarit@redhat.com>
    Cc: jschlst@samba.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Acked-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Prarit Bhargava <prarit@redhat.com>
    Reviewed-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Link: https://lore.kernel.org/r/20230927090029.44704-2-gregkh@linuxfoundation.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- Documentation/networking/device_drivers/index.rst
+++ Documentation/networking/device_drivers/index.rst
@@ -8,7 +8,6 @@ Contents:
 .. toctree::
    :maxdepth: 2
 
-   appletalk/index
    atm/index
    cable/index
    can/index
--- include/net/Space.h
+++ include/net/Space.h
@@ -10,4 +10,3 @@ struct net_device *smc_init(int unit);
 struct net_device *cs89x0_probe(int unit);
 struct net_device *tc515_probe(int unit);
 struct net_device *lance_probe(int unit);
-struct net_device *cops_probe(int unit);
