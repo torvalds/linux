61e21cf2d2c3cc5e60e8d0a62a77e250fccda62c
Kemeng Shi <shikemeng@huaweicloud.com>
2023-09-27 17:44:01 +0800

    mm/page_alloc: correct start page when guard page debug is enabled
    When guard page debug is enabled and set_page_guard returns success, we
    miss to forward page to point to start of next split range and we will do
    split unexpectedly in page range without target page.  Move start page
    update before set_page_guard to fix this.
    
    As we split to wrong target page, then splited pages are not able to merge
    back to original order when target page is put back and splited pages
    except target page is not usable.  To be specific:
    
    Consider target page is the third page in buddy page with order 2.
    | buddy-2 | Page | Target | Page |
    
    After break down to target page, we will only set first page to Guard
    because of bug.
    | Guard   | Page | Target | Page |
    
    When we try put_page_back_buddy with target page, the buddy page of target
    if neither guard nor buddy, Then it's not able to construct original page
    with order 2
    | Guard | Page | buddy-0 | Page |
    
    All pages except target page is not in free list and is not usable.
    
    Link: https://lkml.kernel.org/r/20230927094401.68205-1-shikemeng@huaweicloud.com
    Fixes: 06be6ff3d2ec ("mm,hwpoison: rework soft offline for free pages")
    Signed-off-by: Kemeng Shi <shikemeng@huaweicloud.com>
    Acked-by: Naoya Horiguchi <naoya.horiguchi@nec.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6475,6 +6475,7 @@ static void break_down_buddy_pages(struct zone *zone, struct page *page,
 			next_page = page;
 			current_buddy = page + size;
 		}
+		page = next_page;
 
 		if (set_page_guard(zone, current_buddy, high, migratetype))
 			continue;
@@ -6482,7 +6483,6 @@ static void break_down_buddy_pages(struct zone *zone, struct page *page,
 		if (current_buddy != target) {
 			add_to_free_list(current_buddy, zone, high, migratetype);
 			set_buddy_order(current_buddy, high);
-			page = next_page;
 		}
 	}
 }
