7214141067922836b48157e8266335096a0ea4ea
Linus Walleij <linus.walleij@linaro.org>
2023-09-29 00:12:56 +0200

    ASoC: rockchip: Drop includes from Rockchip MAX98090
    The Rockchip MAX98090 ASoC driver includes two legacy GPIO
    headers but doesn't use symbols from any of them. Delete
    the includes.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://lore.kernel.org/r/20230929-descriptors-asoc-rockchip-v2-3-2d2c0e043aab@linaro.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/rockchip/rockchip_max98090.c
+++ sound/soc/rockchip/rockchip_max98090.c
@@ -9,8 +9,6 @@
 #include <linux/of_device.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
-#include <linux/gpio.h>
-#include <linux/of_gpio.h>
 #include <sound/core.h>
 #include <sound/jack.h>
 #include <sound/pcm.h>
