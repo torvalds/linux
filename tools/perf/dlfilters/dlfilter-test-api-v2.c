f38f547314b858b94d36aeb9a4401f0aade7d1af
Adrian Hunter <adrian.hunter@intel.com>
2023-09-28 12:10:33 +0300

    perf dlfilter: Add a test for object_code()
    Extend the "dlfilter C API" test to test
    perf_dlfilter_fns.object_code().
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Link: https://lore.kernel.org/r/20230928091033.33998-1-adrian.hunter@intel.com
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/dlfilters/dlfilter-test-api-v2.c
+++ tools/perf/dlfilters/dlfilter-test-api-v2.c
@@ -308,6 +308,15 @@ static int check_attr(void *ctx)
 	return 0;
 }
 
+static int check_object_code(void *ctx, const struct perf_dlfilter_sample *sample)
+{
+	__u8 buf[15];
+
+	CHECK(perf_dlfilter_fns.object_code(ctx, sample->ip, buf, sizeof(buf)) > 0);
+
+	return 0;
+}
+
 static int do_checks(void *data, const struct perf_dlfilter_sample *sample, void *ctx, bool early)
 {
 	struct filter_data *d = data;
@@ -333,7 +342,8 @@ static int do_checks(void *data, const struct perf_dlfilter_sample *sample, void
 	if (early && !d->do_early)
 		return 0;
 
-	if (check_al(ctx) || check_addr_al(ctx) || check_address_al(ctx, sample))
+	if (check_al(ctx) || check_addr_al(ctx) || check_address_al(ctx, sample) ||
+	    check_object_code(ctx, sample))
 		return -1;
 
 	if (early)
