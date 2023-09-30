2b40885cdc4129c5add80598f51392ab06429f95
Greg Kroah-Hartman <gregkh@google.com>
2023-09-30 12:24:12 +0000

    Revert "crypto: api - Use work queue in crypto_destroy_instance"
    This reverts commit 625bf86bf53eb7a8ee60fb9dc45b272b77e5ce1c which is
    commit 9ae4577bc077a7e32c3c7d442c95bc76865c0f17 upstream.
    
    It breaks the android KABI so revert it.  If it needs to come back in
    the future, it can be modified to do so in an abi-safe way.
    
    Bug: 161946584
    Change-Id: I99e23f307011ff3a7e4b0dfbd6f341512caa3a3d
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- include/crypto/algapi.h
+++ include/crypto/algapi.h
@@ -10,7 +10,6 @@
 #include <linux/crypto.h>
 #include <linux/list.h>
 #include <linux/kernel.h>
-#include <linux/workqueue.h>
 
 /*
  * Maximum values for blocksize and alignmask, used to allocate
@@ -55,8 +54,6 @@ struct crypto_instance {
 		/* List of attached spawns before registration. */
 		struct crypto_spawn *spawns;
 	};
-
-	struct work_struct free_work;
 
 	void *__ctx[] CRYPTO_MINALIGN_ATTR;
 };
