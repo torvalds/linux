c6801e23322d03e57c299ff16d013bd37a5d9360
Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
2023-10-03 10:53:52 +0200

    pinctrl: st: use new pinctrl GPIO helpers
    Replace the pinctrl helpers taking the global GPIO number as argument
    with the improved variants that instead take a pointer to the GPIO chip
    and the controller-relative offset.
    
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
    Reviewed-by: Patrice Chotard <patrice.chotard@foss.st.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-st.c
+++ drivers/pinctrl/pinctrl-st.c
@@ -719,7 +719,7 @@ static void st_gpio_set(struct gpio_chip *chip, unsigned offset, int value)
 
 static int st_gpio_direction_input(struct gpio_chip *chip, unsigned offset)
 {
-	pinctrl_gpio_direction_input(chip->base + offset);
+	pinctrl_gpio_direction_input_new(chip, offset);
 
 	return 0;
 }
@@ -730,7 +730,7 @@ static int st_gpio_direction_output(struct gpio_chip *chip,
 	struct st_gpio_bank *bank = gpiochip_get_data(chip);
 
 	__st_gpio_set(bank, offset, value);
-	pinctrl_gpio_direction_output(chip->base + offset);
+	pinctrl_gpio_direction_output_new(chip, offset);
 
 	return 0;
 }
