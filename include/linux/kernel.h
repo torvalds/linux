3cd39bc3b11b8d34b7d7c961a35fdfd18b0ebf75
Alejandro Colomar <alx@kernel.org>
2023-10-03 14:59:53 +0300

    kernel.h: Move ARRAY_SIZE() to a separate header
    Touching files so used for the kernel,
    forces 'make' to recompile most of the kernel.
    
    Having those definitions in more granular files
    helps avoid recompiling so much of the kernel.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>
    Reviewed-by: Giovanni Cabiddu <giovanni.cabiddu@intel.com>
    Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Link: https://lore.kernel.org/r/20230817143352.132583-2-lucas.segarra.fernandez@intel.com
    [andy: reduced to cover only string.h for now]
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -13,6 +13,7 @@
 
 #include <linux/stdarg.h>
 #include <linux/align.h>
+#include <linux/array_size.h>
 #include <linux/limits.h>
 #include <linux/linkage.h>
 #include <linux/stddef.h>
@@ -49,12 +50,6 @@
 /* generic data direction definitions */
 #define READ			0
 #define WRITE			1
-
-/**
- * ARRAY_SIZE - get the number of elements in array @arr
- * @arr: array to be sized
- */
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]) + __must_be_array(arr))
 
 #define PTR_IF(cond, ptr)	((cond) ? (ptr) : NULL)
 
