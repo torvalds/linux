db23405211ed245c2c3fb077459e8d71f6b024c0
Peter Wang <peter.wang@mediatek.com>
2023-10-04 14:24:54 +0800

    UPSTREAM: scsi: ufs: core: Remove dev cmd clock scaling busy
    If a dev command times out, clk_scaling.active_reqs is not decreased which
    causes abnormal clock scaling.
    
    It is complicated to handle different dev command timeout cases in both
    legacy mode and MCQ mode. Besides, dev cmds are rarely used and the busy
    time is short.
    
    Remove clock scaling busy window for dev cmds like we do for UIC or TM cmds
    which don't update busy window either.
    
    Change-Id: I2f25dc5032126778ac99b6a79bd78f9578dd730c
    (cherry picked from commit e66413faa5b55800ad42a570aad674aed634c5e6)
    Signed-off-by: Peter Wang <peter.wang@mediatek.com>
    Link: https://lore.kernel.org/r/20231004062454.29165-1-peter.wang@mediatek.com
    Reviewed-by: Stanley Chu <stanley.chu@mediatek.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/ufs/core/ufshcd.c
+++ drivers/ufs/core/ufshcd.c
@@ -2248,7 +2248,8 @@ void ufshcd_send_command(struct ufs_hba *hba, unsigned int task_tag,
 	lrbp->compl_time_stamp_local_clock = 0;
 	trace_android_vh_ufs_send_command(hba, lrbp);
 	ufshcd_add_command_trace(hba, task_tag, UFS_CMD_SEND);
-	ufshcd_clk_scaling_start_busy(hba);
+	if (lrbp->cmd)
+		ufshcd_clk_scaling_start_busy(hba);
 	if (unlikely(ufshcd_should_inform_monitor(hba, lrbp)))
 		ufshcd_start_monitor(hba, lrbp);
 
@@ -5544,7 +5545,6 @@ void ufshcd_compl_one_cqe(struct ufs_hba *hba, int task_tag,
 			}
 			ufshcd_add_command_trace(hba, task_tag, UFS_DEV_COMP);
 			complete(hba->dev_cmd.complete);
-			ufshcd_clk_scaling_update_busy(hba);
 		}
 	}
 }
