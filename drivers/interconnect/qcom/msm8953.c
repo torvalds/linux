7faf0be4970ab350a1ede26ab32529b094436dce
Vladimir Lypak <vladimir.lypak@gmail.com>
2023-10-04 22:37:18 +0300

    interconnect: qcom: msm8953: enable sync callback

--- drivers/interconnect/qcom/msm8953.c
+++ drivers/interconnect/qcom/msm8953.c
@@ -1314,6 +1314,7 @@ static struct platform_driver msm8953_noc_driver = {
 	.driver = {
 		.name = "qnoc-msm8953",
 		.of_match_table = msm8953_noc_of_match,
+		.sync_state = icc_sync_state,
 	},
 };
 
