cd5c11d5aacdb7eda2acc6ee57d442b48f300858
Ken Lin <ken_lin5@hotmail.com>
2023-10-05 09:38:41 +0800

    media: platform: cros-ec: Add Taranza to the match table
    Taranza has two HDMI ports which support CEC:
    Port D is EC port 0
    Port B is EC port 1
    
    Signed-off-by: Ken Lin <ken_lin5@hotmail.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

--- drivers/media/cec/platform/cros-ec/cros-ec-cec.c
+++ drivers/media/cec/platform/cros-ec/cros-ec-cec.c
@@ -322,6 +322,8 @@ static const struct cec_dmi_match cec_dmi_match_table[] = {
 	{ "Google", "Constitution", "0000:00:02.0", port_ba_conns },
 	/* Google Boxy */
 	{ "Google", "Boxy", "0000:00:02.0", port_d_conns },
+	/* Google Taranza */
+	{ "Google", "Taranza", "0000:00:02.0", port_db_conns },
 };
 
 static struct device *cros_ec_cec_find_hdmi_dev(struct device *dev,
