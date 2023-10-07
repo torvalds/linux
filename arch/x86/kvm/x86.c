ef8d89033c3f1f6a64757f066b2c17e76d1189f8
Like Xu <likexu@tencent.com>
2023-10-07 14:40:19 +0800

    KVM: x86: Remove 'return void' expression for 'void function'
    The requested info will be stored in 'guest_xsave->region' referenced by
    the incoming pointer "struct kvm_xsave *guest_xsave", thus there is no need
    to explicitly use return void expression for a void function "static void
    kvm_vcpu_ioctl_x86_get_xsave(...)". The issue is caught with [-Wpedantic].
    
    Fixes: 2d287ec65e79 ("x86/fpu: Allow caller to constrain xfeatures when copying to uabi buffer")
    Signed-off-by: Like Xu <likexu@tencent.com>
    Reviewed-by: Maxim Levitsky <mlevitsk@redhat.com>
    Link: https://lore.kernel.org/r/20231007064019.17472-1-likexu@tencent.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -5411,8 +5411,8 @@ static void kvm_vcpu_ioctl_x86_get_xsave2(struct kvm_vcpu *vcpu,
 static void kvm_vcpu_ioctl_x86_get_xsave(struct kvm_vcpu *vcpu,
 					 struct kvm_xsave *guest_xsave)
 {
-	return kvm_vcpu_ioctl_x86_get_xsave2(vcpu, (void *)guest_xsave->region,
-					     sizeof(guest_xsave->region));
+	kvm_vcpu_ioctl_x86_get_xsave2(vcpu, (void *)guest_xsave->region,
+				      sizeof(guest_xsave->region));
 }
 
 static int kvm_vcpu_ioctl_x86_set_xsave(struct kvm_vcpu *vcpu,
