c42c2b8224c40f91f5f4984cc721d33ab10c7d43
Wen Gong <quic_wgong@quicinc.com>
2023-10-10 10:27:21 +0300

    wifi: ath12k: fix invalid m3 buffer address
    This is to fix m3 buffer reuse issue as m3_mem->size isn't set to
    zero in the free function, which leads invalid m3 downloading to
    firmware and firmware crashing.
    
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0-03427-QCAHMTSWPL_V1.0_V2.0_SILICONZ-1.15378.4
    
    Signed-off-by: Wen Gong <quic_wgong@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://lore.kernel.org/r/20230721055305.20420-4-quic_wgong@quicinc.com

--- drivers/net/wireless/ath/ath12k/qmi.c
+++ drivers/net/wireless/ath/ath12k/qmi.c
@@ -2540,6 +2540,7 @@ static void ath12k_qmi_m3_free(struct ath12k_base *ab)
 	dma_free_coherent(ab->dev, m3_mem->size,
 			  m3_mem->vaddr, m3_mem->paddr);
 	m3_mem->vaddr = NULL;
+	m3_mem->size = 0;
 }
 
 static int ath12k_qmi_wlanfw_m3_info_send(struct ath12k_base *ab)
