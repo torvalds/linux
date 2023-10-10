dddb91cde52b4a57fa06a332b230fca3b11b885f
Heikki Krogerus <heikki.krogerus@linux.intel.com>
2023-10-10 17:17:49 +0300

    usb: typec: ucsi: Fix missing link removal
    The link between the partner device and its USB Power
    Delivery instance was never removed which prevented the
    device from being released. Removing the link always when
    the partner is unregistered.
    
    Fixes: b04e1747fbcc ("usb: typec: ucsi: Register USB Power Delivery Capabilities")
    Cc: stable <stable@kernel.org>
    Reported-by: Douglas Gilbert <dgilbert@interlog.com>
    Closes: https://lore.kernel.org/linux-usb/ZSUMXdw9nanHtnw2@kuha.fi.intel.com/
    Signed-off-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Link: https://lore.kernel.org/r/20231010141749.3912016-1-heikki.krogerus@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/typec/ucsi/ucsi.c
+++ drivers/usb/typec/ucsi/ucsi.c
@@ -787,6 +787,7 @@ static void ucsi_unregister_partner(struct ucsi_connector *con)
 
 	typec_set_mode(con->port, TYPEC_STATE_SAFE);
 
+	typec_partner_set_usb_power_delivery(con->partner, NULL);
 	ucsi_unregister_partner_pdos(con);
 	ucsi_unregister_altmodes(con, UCSI_RECIPIENT_SOP);
 	typec_unregister_partner(con->partner);
