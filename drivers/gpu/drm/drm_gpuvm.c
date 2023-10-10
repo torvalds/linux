f7749a549b4f4db0c02e6b3d3800ea400dd76c12
Thomas Hellström <thomas.hellstrom@linux.intel.com>
2023-10-10 16:27:24 +0200

    drm/gpuvm: Dual-licence the drm_gpuvm code GPL-2.0 OR MIT
    Dual-licence in order to make it possible for other non-GPL os'es
    to re-implement the code. The use of EXPORT_SYMBOL_GPL() is intentionally
    left untouched to prevent use of drm_gpuvm as a proxy for non-GPL drivers
    to access GPL-only kernel symbols.
    
    Much of the ideas and algorithms used in the drm_gpuvm code is already
    present in one way or another in MIT-licensed code.
    
    Cc: Danilo Krummrich <dakr@redhat.com>
    Cc: airlied@gmail.com
    Cc: daniel@ffwll.ch
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Acked-by: Danilo Krummrich <dakr@redhat.com>
    Reviewed-by: Francois Dugast <francois.dugast@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20231010142725.8920-1-thomas.hellstrom@linux.intel.com

--- drivers/gpu/drm/drm_gpuvm.c
+++ drivers/gpu/drm/drm_gpuvm.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-License-Identifier: GPL-2.0 OR MIT
 /*
  * Copyright (c) 2022 Red Hat.
  *
--- include/drm/drm_gpuvm.h
+++ include/drm/drm_gpuvm.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
+/* SPDX-License-Identifier: GPL-2.0 OR MIT */
 
 #ifndef __DRM_GPUVM_H__
 #define __DRM_GPUVM_H__
