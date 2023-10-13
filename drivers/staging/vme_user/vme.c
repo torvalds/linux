4d8cc30a9b49781089c082cd858a1c0fc882f480
Soumya Negi <soumya.negi97@gmail.com>
2023-10-13 16:47:13 -0700

    staging: vme_user: Correct spelling mistakes in comments
    Fix misspelled words in vme.c. Issue found by checkpatch.pl
    
    Signed-off-by: Soumya Negi <soumya.negi97@gmail.com>
    Link: https://lore.kernel.org/r/f41caa8e7c987fdc010942838117fece8151affe.1697240381.git.soumya.negi97@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vme_user/vme.c
+++ drivers/staging/vme_user/vme.c
@@ -1603,7 +1603,7 @@ EXPORT_SYMBOL(vme_lm_get);
  * @callback: Pointer to callback function called when triggered.
  * @data: Generic pointer that will be passed to the callback function.
  *
- * Attach a callback to the specificed offset into the location monitors
+ * Attach a callback to the specified offset into the location monitors
  * monitored addresses. A generic pointer is provided to allow data to be
  * passed to the callback when called.
  *
@@ -1638,7 +1638,7 @@ EXPORT_SYMBOL(vme_lm_attach);
  * @resource: Pointer to VME location monitor resource.
  * @monitor: Offset to which callback should be removed.
  *
- * Remove the callback associated with the specificed offset into the
+ * Remove the callback associated with the specified offset into the
  * location monitors monitored addresses.
  *
  * Return: Zero on success, -EINVAL when provided with an invalid location
