221917420f12be0bef8a2b40827468de87365191
Kalpak Kawadkar <quic_kkawadka@quicinc.com>
2023-10-13 15:22:10 +0530

    clk: qcom: Add support of DISPCC on NIOBE
    Add support for display clock controller, for clients
    to be able to request these clocks on NIOBE platform.
    
    Change-Id: Idac16da52b33d59f649919a7b95a3416f3212f15
    Signed-off-by: Kalpak Kawadkar <quic_kkawadka@quicinc.com>

--- drivers/clk/qcom/vdd-level.h
+++ drivers/clk/qcom/vdd-level.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /* Copyright (c) 2019-2021, The Linux Foundation. All rights reserved. */
-/* Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved. */
+/* Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved. */
 
 #ifndef __DRIVERS_CLK_QCOM_VDD_LEVEL_H
 #define __DRIVERS_CLK_QCOM_VDD_LEVEL_H
@@ -17,6 +17,7 @@ enum vdd_levels {
 	VDD_LOW_L0,		/* SVS_L0 */
 	VDD_LOW_L1,		/* SVS_L1 */
 	VDD_NOMINAL,		/* NOM */
+	VDD_NOMINAL_L1,		/* NOM_L1 */
 	VDD_HIGH,		/* TURBO */
 	VDD_HIGH_L1,		/* TURBO_L1 */
 	VDD_NUM,
@@ -31,6 +32,7 @@ static int vdd_corner[] = {
 	[VDD_LOW_L0]	= RPMH_REGULATOR_LEVEL_SVS_L0,
 	[VDD_LOW_L1]	= RPMH_REGULATOR_LEVEL_SVS_L1,
 	[VDD_NOMINAL]	= RPMH_REGULATOR_LEVEL_NOM,
+	[VDD_NOMINAL_L1] = RPMH_REGULATOR_LEVEL_NOM_L1,
 	[VDD_HIGH]	= RPMH_REGULATOR_LEVEL_TURBO,
 	[VDD_HIGH_L1]	= RPMH_REGULATOR_LEVEL_TURBO_L1,
 };
