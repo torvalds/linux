f19cfce87d9416d050399ae56a00f2060eab85f4
Sonny Jiang <sonny.jiang@amd.com>
2023-10-16 16:49:53 -0400

    drm/amdgpu/jpeg5: enable power gating
    Enable PG on JPEG5
    
    Signed-off-by: Sonny Jiang <sonny.jiang@amd.com>
    Reviewed-by: Leo Liu <leo.liu@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/soc24.c
+++ drivers/gpu/drm/amd/amdgpu/soc24.c
@@ -359,7 +359,8 @@ static int soc24_common_early_init(void *handle)
 		break;
 	case IP_VERSION(12, 0, 1):
 		adev->cg_flags = 0;
-		adev->pg_flags = AMD_PG_SUPPORT_VCN;
+		adev->pg_flags = AMD_PG_SUPPORT_VCN |
+			AMD_PG_SUPPORT_JPEG;
 		adev->external_rev_id = adev->rev_id + 0x50;
 		adev->cg_flags = AMD_CG_SUPPORT_GFX_CGCG |
 				AMD_CG_SUPPORT_GFX_CGLS;
