95ab70f134d837a566f2d998b3090f40227a1b60
Suraj Kandpal <suraj.kandpal@intel.com>
2023-10-16 14:31:41 +0530

    drm/xe/hdcp: Define intel_hdcp_gsc_check_status in Xe
    Define intel_hdcp_gsc_check_status in Xe to account
    for changes in i915 and Xe.
    intel_hdcp_check_status always returns false as gsc cs
    interface is not yet ported.
    intel_hdcp_gsc_cs_required always returns true as going
    forward gsc cs will always be required by upcoming
    platforms
    
    --v5
    -Define intel_hdcp_gsc_cs_required()
    
    --v6
    -Explain reasons for the return values [Chaitanya]
    
    Signed-off-by: Suraj Kandpal <suraj.kandpal@intel.com>
    Reviewed-by: Chaitanya Kumar Borah <chaitanya.kumar.borah@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/xe/display/xe_hdcp_gsc.c
+++ drivers/gpu/drm/xe/display/xe_hdcp_gsc.c
@@ -6,6 +6,16 @@
 #include "i915_drv.h"
 #include "intel_hdcp_gsc.h"
 
+bool intel_hdcp_gsc_cs_required(struct drm_i915_private *i915)
+{
+	return true;
+}
+
+bool intel_hdcp_gsc_check_status(struct drm_i915_private *i915)
+{
+	return false;
+}
+
 int intel_hdcp_gsc_init(struct drm_i915_private *i915)
 {
 	drm_info(&i915->drm, "HDCP support not yet implemented\n");
