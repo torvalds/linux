a614f95797055dd96802202af542b04fead7274f
Justin Stitt <justinstitt@google.com>
2023-10-17 20:11:29 +0000

    wifi: brcmsmac: replace deprecated strncpy with memcpy
    Let's move away from using strncpy and instead use the more obvious
    interface for this context.
    
    For wlc->pub->srom_ccode, we're just copying two bytes from ccode into
    wlc->pub->srom_ccode with no expectation that srom_ccode be
    NUL-terminated:
    wlc->pub->srom_ccode is only used in regulatory_hint():
    1193 |       if (wl->pub->srom_ccode[0] &&
    1194 |           regulatory_hint(wl->wiphy, wl->pub->srom_ccode))
    1195 |               wiphy_err(wl->wiphy, "%s: regulatory hint failed\n", __func__);
    
    We can see that only index 0 and index 1 are accessed.
    3307 |       int regulatory_hint(struct wiphy *wiphy, const char *alpha2)
    3308 |       {
    ...  |          ...
    3322 |          request->alpha2[0] = alpha2[0];
    3323 |          request->alpha2[1] = alpha2[1];
    ...  |          ...
    3332 |       }
    
    Since this is just a simple byte copy with correct lengths, let's use
    memcpy(). There should be no functional change.
    
    In a similar boat, both wlc->country_default and
    wlc->autocountry_default are just simple byte copies so let's use
    memcpy. However, FWICT they aren't used anywhere. (they should be
    used or removed -- not in scope of my patch, though).
    
    Signed-off-by: Justin Stitt <justinstitt@google.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://lore.kernel.org/r/20231017-strncpy-drivers-net-wireless-broadcom-brcm80211-brcmfmac-cfg80211-c-v3-2-af780d74ae38@google.com

--- drivers/net/wireless/broadcom/brcm80211/brcmsmac/channel.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmsmac/channel.c
@@ -341,7 +341,7 @@ struct brcms_cm_info *brcms_c_channel_mgr_attach(struct brcms_c_info *wlc)
 	/* store the country code for passing up as a regulatory hint */
 	wlc_cm->world_regd = brcms_world_regd(ccode, ccode_len);
 	if (brcms_c_country_valid(ccode))
-		strncpy(wlc->pub->srom_ccode, ccode, ccode_len);
+		memcpy(wlc->pub->srom_ccode, ccode, ccode_len);
 
 	/*
 	 * If no custom world domain is found in the SROM, use the
@@ -354,10 +354,10 @@ struct brcms_cm_info *brcms_c_channel_mgr_attach(struct brcms_c_info *wlc)
 	}
 
 	/* save default country for exiting 11d regulatory mode */
-	strncpy(wlc->country_default, ccode, ccode_len);
+	memcpy(wlc->country_default, ccode, ccode_len);
 
 	/* initialize autocountry_default to driver default */
-	strncpy(wlc->autocountry_default, ccode, ccode_len);
+	memcpy(wlc->autocountry_default, ccode, ccode_len);
 
 	brcms_c_set_country(wlc_cm, wlc_cm->world_regd);
 
