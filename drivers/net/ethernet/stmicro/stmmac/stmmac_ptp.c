3fba8234395576b5b7160e06f23e1446f92973e6
Johannes Zink <j.zink@pengutronix.de>
2023-10-18 09:09:53 +0200

    net: stmmac: simplify debug message on stmmac_enable()
    Simplify the netdev_dbg() call in stmmac_enable() in order to reduce code
    duplication. No functional change.
    
    Signed-off-by: Johannes Zink <j.zink@pengutronix.de>
    Reviewed-by: Andrew Halaney <ahalaney@redhat.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_ptp.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_ptp.c
@@ -203,14 +203,10 @@ static int stmmac_enable(struct ptp_clock_info *ptp,
 			/* Enable External snapshot trigger */
 			acr_value |= priv->plat->ext_snapshot_num;
 			acr_value |= PTP_ACR_ATSFC;
-			netdev_dbg(priv->dev, "Auxiliary Snapshot %d enabled.\n",
-				   priv->plat->ext_snapshot_num >>
-				   PTP_ACR_ATSEN_SHIFT);
-		} else {
-			netdev_dbg(priv->dev, "Auxiliary Snapshot %d disabled.\n",
-				   priv->plat->ext_snapshot_num >>
-				   PTP_ACR_ATSEN_SHIFT);
 		}
+		netdev_dbg(priv->dev, "Auxiliary Snapshot %d %s.\n",
+			   priv->plat->ext_snapshot_num >> PTP_ACR_ATSEN_SHIFT,
+			   on ? "enabled" : "disabled");
 		writel(acr_value, ptpaddr + PTP_ACR);
 		mutex_unlock(&priv->aux_ts_lock);
 		/* wait for auxts fifo clear to finish */
