5329aa5101f73c451bcd48deaf3f296685849d9c
Jiao Zhou <jiaozhou@google.com>
2023-10-20 00:46:39 +0200

    efivarfs: Add uid/gid mount options
    Allow UEFI variables to be modified by non-root processes in order to
    run sandboxed code. This doesn't change the behavior of mounting
    efivarfs unless uid/gid are specified; by default both are set to root.
    
    Signed-off-by: Jiao Zhou <jiaozhou@google.com>
    Acked-by: Matthew Garrett <mgarrett@aurora.tech>
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- fs/efivarfs/internal.h
+++ fs/efivarfs/internal.h
@@ -9,6 +9,15 @@
 #include <linux/list.h>
 #include <linux/efi.h>
 
+struct efivarfs_mount_opts {
+	kuid_t uid;
+	kgid_t gid;
+};
+
+struct efivarfs_fs_info {
+	struct efivarfs_mount_opts mount_opts;
+};
+
 struct efi_variable {
 	efi_char16_t  VariableName[EFI_VAR_NAME_LEN/sizeof(efi_char16_t)];
 	efi_guid_t    VendorGuid;
