9d536f9372ad051c2db81cae54d858c7a7aecea1
Phil Sutter <phil@nwl.cc>
2023-10-20 19:34:28 +0200

    netfilter: nf_tables: Drop pointless memset in nf_tables_dump_obj
    [ Upstream commit ff16111cc10c82ee065ffbd9fa8d6210394ff8c6 ]
    
    The code does not make use of cb->args fields past the first one, no
    need to zero them.
    
    Signed-off-by: Phil Sutter <phil@nwl.cc>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Stable-dep-of: bd662c4218f9 ("netfilter: nf_tables: Add locking for NFT_MSG_GETOBJ_RESET requests")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -7452,9 +7452,6 @@ static int nf_tables_dump_obj(struct sk_buff *skb, struct netlink_callback *cb)
 				goto cont;
 			if (idx < s_idx)
 				goto cont;
-			if (idx > s_idx)
-				memset(&cb->args[1], 0,
-				       sizeof(cb->args) - sizeof(cb->args[0]));
 			if (filter && filter->table &&
 			    strcmp(filter->table, table->name))
 				goto cont;
