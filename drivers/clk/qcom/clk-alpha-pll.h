84da48921a97cee3dd1391659e93ee01d122b78b
Varadarajan Narayanan <quic_varada@quicinc.com>
2023-10-20 11:49:32 +0530

    clk: qcom: clk-alpha-pll: introduce stromer plus ops
    Stromer plus APSS PLL does not support dynamic frequency scaling.
    To switch between frequencies, we have to shut down the PLL,
    configure the L and ALPHA values and turn on again. So introduce the
    separate set of ops for Stromer Plus PLL.
    
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Kathiravan T <quic_kathirav@quicinc.com>
    Signed-off-by: Varadarajan Narayanan <quic_varada@quicinc.com>
    Link: https://lore.kernel.org/r/2affa6c63ff0c4342230623a7d4eef02ec7c02d4.1697781921.git.quic_varada@quicinc.com
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>

--- drivers/clk/qcom/clk-alpha-pll.h
+++ drivers/clk/qcom/clk-alpha-pll.h
@@ -152,6 +152,7 @@ extern const struct clk_ops clk_alpha_pll_postdiv_ops;
 extern const struct clk_ops clk_alpha_pll_huayra_ops;
 extern const struct clk_ops clk_alpha_pll_postdiv_ro_ops;
 extern const struct clk_ops clk_alpha_pll_stromer_ops;
+extern const struct clk_ops clk_alpha_pll_stromer_plus_ops;
 
 extern const struct clk_ops clk_alpha_pll_fabia_ops;
 extern const struct clk_ops clk_alpha_pll_fixed_fabia_ops;
