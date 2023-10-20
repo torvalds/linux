d99317f214ca3d381f7b17a75c41263885664e06
Namhyung Kim <namhyung@kernel.org>
2023-10-20 13:47:39 -0700

    perf lock contention: Clear lock addr after use
    It checks the current lock to calculated the delta of contention time.
    The address is saved in the tstamp map which is allocated at begining of
    contention and released at end of contention.
    
    But it's possible for bpf_map_delete_elem() to fail.  In that case, the
    element in the tstamp map kept for the current lock and it makes the
    next contention for the same lock tracked incorrectly.  Specificially
    the next contention begin will see the existing element for the task and
    it'd just return.  Then the next contention end will see the element and
    calculate the time using the timestamp for the previous begin.
    
    This can result in a large value for two small contentions happened from
    time to time.  Let's clear the lock address so that it can be updated
    next time even if the bpf_map_delete_elem() failed.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Acked-by: Ian Rogers <irogers@google.com>
    Cc: Hao Luo <haoluo@google.com>
    Cc: Song Liu <song@kernel.org>
    Cc: bpf@vger.kernel.org
    Link: https://lore.kernel.org/r/20231020204741.1869520-1-namhyung@kernel.org

--- tools/perf/util/bpf_skel/lock_contention.bpf.c
+++ tools/perf/util/bpf_skel/lock_contention.bpf.c
@@ -389,6 +389,7 @@ int contention_end(u64 *ctx)
 
 	duration = bpf_ktime_get_ns() - pelem->timestamp;
 	if ((__s64)duration < 0) {
+		pelem->lock = 0;
 		bpf_map_delete_elem(&tstamp, &pid);
 		__sync_fetch_and_add(&time_fail, 1);
 		return 0;
@@ -422,6 +423,7 @@ int contention_end(u64 *ctx)
 	data = bpf_map_lookup_elem(&lock_stat, &key);
 	if (!data) {
 		if (data_map_full) {
+			pelem->lock = 0;
 			bpf_map_delete_elem(&tstamp, &pid);
 			__sync_fetch_and_add(&data_fail, 1);
 			return 0;
@@ -445,6 +447,7 @@ int contention_end(u64 *ctx)
 				data_map_full = 1;
 			__sync_fetch_and_add(&data_fail, 1);
 		}
+		pelem->lock = 0;
 		bpf_map_delete_elem(&tstamp, &pid);
 		return 0;
 	}
@@ -458,6 +461,7 @@ int contention_end(u64 *ctx)
 	if (data->min_time > duration)
 		data->min_time = duration;
 
+	pelem->lock = 0;
 	bpf_map_delete_elem(&tstamp, &pid);
 	return 0;
 }
