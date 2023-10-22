koverstreet/bcachefs

bcachefs: Rename lock_held_stats -> btree_transaction_stats
Going to be adding more things to this in the next patch.

Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

fs/bcachefs/bcachefs.h
@@ -319,8 +319,6 @@ BCH_DEBUG_PARAMS_DEBUG()
 #undef BCH_DEBUG_PARAM
 #endif
 
-#define BCH_LOCK_TIME_NR 128
-
 #define BCH_TIME_STATS()			\
 	x(btree_node_mem_alloc)			\
 	x(btree_node_split)			\
@@ -531,9 +529,10 @@ struct btree_debug {
 	unsigned		id;
 };
 
-struct lock_held_stats {
-	struct time_stats       times[BCH_LOCK_TIME_NR];
-	const char              *names[BCH_LOCK_TIME_NR];
+#define BCH_TRANSACTIONS_NR 128
+
+struct btree_transaction_stats {
+	struct time_stats       lock_hold_times;
 };
 
 struct bch_fs_pcpu {
@@ -930,7 +929,8 @@ struct bch_fs {
 
 	struct time_stats	times[BCH_TIME_STAT_NR];
 
-	struct lock_held_stats lock_held_stats;
+	const char              *btree_transaction_fns[BCH_TRANSACTIONS_NR];
+	struct btree_transaction_stats btree_transaction_stats[BCH_TRANSACTIONS_NR];
 };
 
 static inline void bch2_set_ra_pages(struct bch_fs *c, unsigned ra_pages)

torvalds/linux

Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

fs/bcachefs/bcachefs.h
@@ -319,8 +319,6 @@ BCH_DEBUG_PARAMS_DEBUG()
 #undef BCH_DEBUG_PARAM
 #endif
 
-#define BCH_LOCK_TIME_NR 128
-
 #define BCH_TIME_STATS()			\
 	x(btree_node_mem_alloc)			\
 	x(btree_node_split)			\
@@ -529,9 +527,10 @@ struct btree_debug {
 	unsigned		id;
 };
 
-struct lock_held_stats {
-	struct bch2_time_stats	times[BCH_LOCK_TIME_NR];
-	const char		*names[BCH_LOCK_TIME_NR];
+#define BCH_TRANSACTIONS_NR 128
+
+struct btree_transaction_stats {
+	struct bch2_time_stats	lock_hold_times;
 };
 
 struct bch_fs_pcpu {
@@ -928,7 +927,8 @@ mempool_t		bio_bounce_pages;
 
 	struct bch2_time_stats	times[BCH_TIME_STAT_NR];
 
-	struct lock_held_stats	lock_held_stats;
+	const char              *btree_transaction_fns[BCH_TRANSACTIONS_NR];
+	struct btree_transaction_stats btree_transaction_stats[BCH_TRANSACTIONS_NR];
 };
 
 static inline void bch2_set_ra_pages(struct bch_fs *c, unsigned ra_pages)