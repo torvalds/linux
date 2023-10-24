53f0b020218fcc0a56a11df39630dbd379e4d9a6
Joao Martins <joao.m.martins@oracle.com>
2023-10-24 14:50:52 +0100

    vfio/iova_bitmap: Export more API symbols
    In preparation to move iova_bitmap into iommufd, export the rest of API
    symbols that will be used in what could be used by modules, namely:
    
    	iova_bitmap_alloc
    	iova_bitmap_free
    	iova_bitmap_for_each
    
    Link: https://lore.kernel.org/r/20231024135109.73787-2-joao.m.martins@oracle.com
    Suggested-by: Alex Williamson <alex.williamson@redhat.com>
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Reviewed-by: Alex Williamson <alex.williamson@redhat.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/vfio/iova_bitmap.c
+++ drivers/vfio/iova_bitmap.c
@@ -268,6 +268,7 @@ err:
 	iova_bitmap_free(bitmap);
 	return ERR_PTR(rc);
 }
+EXPORT_SYMBOL_GPL(iova_bitmap_alloc);
 
 /**
  * iova_bitmap_free() - Frees an IOVA bitmap object
@@ -289,6 +290,7 @@ void iova_bitmap_free(struct iova_bitmap *bitmap)
 
 	kfree(bitmap);
 }
+EXPORT_SYMBOL_GPL(iova_bitmap_free);
 
 /*
  * Returns the remaining bitmap indexes from mapped_total_index to process for
@@ -387,6 +389,7 @@ int iova_bitmap_for_each(struct iova_bitmap *bitmap, void *opaque,
 
 	return ret;
 }
+EXPORT_SYMBOL_GPL(iova_bitmap_for_each);
 
 /**
  * iova_bitmap_set() - Records an IOVA range in bitmap
