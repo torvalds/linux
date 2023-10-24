e7c5f137e5aa8081b2d2c608da521b2f48a6dbc1
Maria Yu <quic_aiquny@quicinc.com>
2023-10-24 09:09:54 +0800

    arm64: module: Fix PLT counting when CONFIG_RANDOMIZE_BASE=n
    commit d35686444fc80950c731e33a2f6ad4a55822be9b upstream.
    
    The counting of module PLTs has been broken when CONFIG_RANDOMIZE_BASE=n
    since commit:
    
      3e35d303ab7d22c4 ("arm64: module: rework module VA range selection")
    
    Prior to that commit, when CONFIG_RANDOMIZE_BASE=n, the kernel image and
    all modules were placed within a 128M region, and no PLTs were necessary
    for B or BL. Hence count_plts() and partition_branch_plt_relas() skipped
    handling B and BL when CONFIG_RANDOMIZE_BASE=n.
    
    After that commit, modules can be placed anywhere within a 2G window
    regardless of CONFIG_RANDOMIZE_BASE, and hence PLTs may be necessary for
    B and BL even when CONFIG_RANDOMIZE_BASE=n. Unfortunately that commit
    failed to update count_plts() and partition_branch_plt_relas()
    accordingly.
    
    Due to this, module_emit_plt_entry() may fail if an insufficient number
    of PLT entries have been reserved, resulting in modules failing to load
    with -ENOEXEC.
    
    Fix this by counting PLTs regardless of CONFIG_RANDOMIZE_BASE in
    count_plts() and partition_branch_plt_relas().
    
    Fixes: 3e35d303ab7d ("arm64: module: rework module VA range selection")
    Signed-off-by: Maria Yu <quic_aiquny@quicinc.com>
    Cc: <stable@vger.kernel.org> # 6.5.x
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Fixes: 3e35d303ab7d ("arm64: module: rework module VA range selection")
    Reviewed-by: Mark Rutland <mark.rutland@arm.com>
    Link: https://lore.kernel.org/r/20231024010954.6768-1-quic_aiquny@quicinc.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/kernel/module-plts.c
+++ arch/arm64/kernel/module-plts.c
@@ -167,9 +167,6 @@ static unsigned int count_plts(Elf64_Sym *syms, Elf64_Rela *rela, int num,
 		switch (ELF64_R_TYPE(rela[i].r_info)) {
 		case R_AARCH64_JUMP26:
 		case R_AARCH64_CALL26:
-			if (!IS_ENABLED(CONFIG_RANDOMIZE_BASE))
-				break;
-
 			/*
 			 * We only have to consider branch targets that resolve
 			 * to symbols that are defined in a different section.
@@ -268,9 +265,6 @@ static int partition_branch_plt_relas(Elf64_Sym *syms, Elf64_Rela *rela,
 				      int numrels, Elf64_Word dstidx)
 {
 	int i = 0, j = numrels - 1;
-
-	if (!IS_ENABLED(CONFIG_RANDOMIZE_BASE))
-		return 0;
 
 	while (i < j) {
 		if (branch_rela_needs_plt(syms, &rela[i], dstidx))
