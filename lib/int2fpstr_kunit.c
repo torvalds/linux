69ca390c73bbee72306ffe6d8296ae9b5163c0fe
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2023-10-24 11:21:02 -0300

    test: add cast in call int2fpstr

--- lib/int2fpstr_kunit.c
+++ lib/int2fpstr_kunit.c
@@ -7,7 +7,7 @@
 
 static void should_return_2345_without_errors(struct kunit *test)
 {
-	char *destination = int2fpstr(2345, 2);
+	char *destination = (char *) int2fpstr(2345, 2);
 	int ret = strncmp(destination, "23.45", 5);
 	KUNIT_EXPECT_EQ(test, 0, ret);
 	kfree(destination);
@@ -15,7 +15,7 @@ static void should_return_2345_without_errors(struct kunit *test)
 
 static void should_return_2_without_errors_and_float_point(struct kunit *test)
 {
-	char *destination = int2fpstr(2, 0);
+	char *destination = (char *) int2fpstr(2, 0);
 	int ret = strncmp(destination, "2", 1);
 	KUNIT_EXPECT_EQ(test, 0, ret);
 	kfree(destination);
@@ -23,7 +23,7 @@ static void should_return_2_without_errors_and_float_point(struct kunit *test)
 
 static void should_return_0_without_errors(struct kunit *test)
 { 
-	char *destination = int2fpstr(0, 0);
+	char *destination = (char *) int2fpstr(0, 0);
 	int ret = strncmp(destination, "0", 1);
 	KUNIT_EXPECT_EQ(test, 0, ret);
 	kfree(destination);
@@ -31,7 +31,7 @@ static void should_return_0_without_errors(struct kunit *test)
 
 static void should_return_12_without_errors_and_float_point(struct kunit *test)
 {
-	char *destination = int2fpstr(12, 0);
+	char *destination = (char *) int2fpstr(12, 0);
 	int ret = strncmp(destination, "12", 2);
 	KUNIT_EXPECT_EQ(test, 0, ret);
 	kfree(destination);
@@ -39,7 +39,7 @@ static void should_return_12_without_errors_and_float_point(struct kunit *test)
 
 static void should_return_12_without_errors(struct kunit *test)
 {
-	char *destination = int2fpstr(-12, 0);
+	char *destination = (char *) int2fpstr(-12, 0);
 	int ret = strncmp(destination, "-12", 2);
 	KUNIT_EXPECT_EQ(test, 0, ret);
 	kfree(destination);
@@ -47,7 +47,7 @@ static void should_return_12_without_errors(struct kunit *test)
 
 static void should_return_125_without_errors(struct kunit *test)
 {
-	char *destination = int2fpstr(-125, 1);
+	char *destination = (char *) int2fpstr(-125, 1);
 	int ret = strncmp(destination, "-12.5", 2);
 	KUNIT_EXPECT_EQ(test, 0, ret);
 	kfree(destination);
