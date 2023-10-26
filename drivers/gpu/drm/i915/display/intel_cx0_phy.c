10184a8a7f70d28ba6aae22142a7375a8c8c1924
Lucas De Marchi <lucas.demarchi@intel.com>
2023-10-26 11:40:44 -0700

    drm/i915/lnl: Extend C10/C20 phy
    For Lunar Lake, DDI-A is connected to C10 PHY, while TC1-TC3 are connected
    to C20 phy, like in Meteor Lake. Update the check in intel_is_c10phy()
    accordingly.
    
    This reverts the change in commit e388ae97e225 ("drm/i915/display:
    Eliminate IS_METEORLAKE checks") that turned that into a display engine
    version check. The phy <-> port connection is very SoC-specific and not
    related to that version.
    
    IS_LUNARLAKE() is defined to 0 in i915 as it's expected that the
    (upcoming) xe driver is the one defining the platform, with i915 only
    driving the display side.
    
    Bspec: 70818
    Reviewed-by: Gustavo Sousa <gustavo.sousa@intel.com>
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20231026184045.1015655-2-lucas.demarchi@intel.com

--- drivers/gpu/drm/i915/display/intel_cx0_phy.c
+++ drivers/gpu/drm/i915/display/intel_cx0_phy.c
@@ -31,7 +31,7 @@
 
 bool intel_is_c10phy(struct drm_i915_private *i915, enum phy phy)
 {
-	if (DISPLAY_VER_FULL(i915) == IP_VER(14, 0) && phy < PHY_C)
+	if ((IS_LUNARLAKE(i915) || IS_METEORLAKE(i915)) && phy < PHY_C)
 		return true;
 
 	return false;
