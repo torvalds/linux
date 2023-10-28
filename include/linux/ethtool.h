eef592e71ae12f50c461c7cd47a1c8dc15ef8ef1
Andrew Lunn <andrew@lunn.ch>
2023-10-28 21:25:11 +0200

    net: ethtool: Fix documentation of ethtool_sprintf()
    commit f55d8e60f10909dbc5524e261041e1d28d7d20d8 upstream.
    
    This function takes a pointer to a pointer, unlike sprintf() which is
    passed a plain pointer. Fix up the documentation to make this clear.
    
    Fixes: 7888fe53b706 ("ethtool: Add common function for filling out strings")
    Cc: Alexander Duyck <alexanderduyck@fb.com>
    Cc: Justin Stitt <justinstitt@google.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Reviewed-by: Justin Stitt <justinstitt@google.com>
    Link: https://lore.kernel.org/r/20231028192511.100001-1-andrew@lunn.ch
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/ethtool.h
+++ include/linux/ethtool.h
@@ -836,10 +836,10 @@ int ethtool_get_phc_vclocks(struct net_device *dev, int **vclock_index);
 
 /**
  * ethtool_sprintf - Write formatted string to ethtool string data
- * @data: Pointer to start of string to update
+ * @data: Pointer to a pointer to the start of string to update
  * @fmt: Format of string to write
  *
- * Write formatted string to data. Update data to point at start of
+ * Write formatted string to *data. Update *data to point at start of
  * next string.
  */
 extern __printf(2, 3) void ethtool_sprintf(u8 **data, const char *fmt, ...);
