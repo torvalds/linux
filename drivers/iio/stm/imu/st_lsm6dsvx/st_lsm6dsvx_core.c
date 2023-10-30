c6c879865444b547e20117fdaaa4055920aae58f
mariotesi <mario.tesi@st.com>
2023-10-30 14:43:33 +0100

    drivers:iio:stm:imu:st_lsm6dsvx: rework self-test procedure
    After enabling the sensor discard the first sample
    because it could be prior to the activation of the
    self-test procedure.
    With this reworking the procedure is now faithful
    to the application note.
    
    Signed-off-by: mariotesi <mario.tesi@st.com>
    Change-Id: I2a590df2b22fdd3f8ab73558901c513b3d960a50
    Reviewed-on: https://gerrit.st.com/c/linuxandroidopen/stm-ldd-iio/+/336873
    Reviewed-by: Matteo DAMENO <matteo.dameno@st.com>
    Tested-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>

--- drivers/iio/stm/imu/st_lsm6dsvx/st_lsm6dsvx_core.c
+++ drivers/iio/stm/imu/st_lsm6dsvx/st_lsm6dsvx_core.c
@@ -1023,6 +1023,9 @@ static int st_lsm6dsvx_selftest_sensor(struct st_lsm6dsvx_sensor *sensor,
 		return -EINVAL;
 	}
 
+	/* reset selftest_status */
+	sensor->selftest_status = -1;
+
 	/* set selftest normal mode */
 	ret = st_lsm6dsvx_set_selftest(sensor, 0);
 	if (ret < 0)
@@ -1035,14 +1038,36 @@ static int st_lsm6dsvx_selftest_sensor(struct st_lsm6dsvx_sensor *sensor,
 	/* calculate delay time because self test is running in polling mode */
 	delay = 1100000 / sensor->odr;
 
-	/* power up, wait 100 ms for stable output */
-	msleep(100);
+	/* power up, wait at least 100 ms for stable output */
+	usleep_range(100000, 110000);
+
+	/* after enabled the sensor discard first sample */
+	while (try_count < 3) {
+		usleep_range(delay, delay + delay / 10);
+		ret = st_lsm6dsvx_read_locked(sensor->hw,
+					      ST_LSM6DSVX_REG_STATUS_REG_ADDR,
+					      &status, sizeof(status));
+		if (ret < 0)
+			goto selftest_failure;
+
+		if (status & bitmask) {
+			st_lsm6dsvx_read_locked(sensor->hw, reg,
+						raw_data,
+						sizeof(raw_data));
+			break;
+		}
+
+		try_count++;
+	}
+
+	if (try_count == 3)
+		goto selftest_failure;
 
 	/* for 5 times, after checking status bit, read the output registers */
 	for (i = 0; i < 5; i++) {
 		try_count = 0;
 		while (try_count < 3) {
-			usleep_range(delay, delay + 1);
+			usleep_range(delay, delay + delay / 10);
 			ret = st_lsm6dsvx_read_locked(sensor->hw,
 						ST_LSM6DSVX_REG_STATUS_REG_ADDR,
 						&status, sizeof(status));
@@ -1084,14 +1109,37 @@ static int st_lsm6dsvx_selftest_sensor(struct st_lsm6dsvx_sensor *sensor,
 	/* set selftest mode */
 	st_lsm6dsvx_set_selftest(sensor, test);
 
-	/* wait 100 ms for stable output */
-	msleep(100);
+	/* power up, wait at least 100 ms for stable output */
+	usleep_range(100000, 110000);
+
+	/* after enabled the sensor trash first sample */
+	try_count = 0;
+	while (try_count < 3) {
+		usleep_range(delay, delay + delay / 10);
+		ret = st_lsm6dsvx_read_locked(sensor->hw,
+					      ST_LSM6DSVX_REG_STATUS_REG_ADDR,
+					      &status, sizeof(status));
+		if (ret < 0)
+			goto selftest_failure;
+
+		if (status & bitmask) {
+			st_lsm6dsvx_read_locked(sensor->hw, reg,
+						raw_data,
+						sizeof(raw_data));
+			break;
+		}
+
+		try_count++;
+	}
+
+	if (try_count == 3)
+		goto selftest_failure;
 
 	/* for 5 times, after checking status bit, read the output registers */
 	for (i = 0; i < 5; i++) {
 		try_count = 0;
 		while (try_count < 3) {
-			usleep_range(delay, delay + 1);
+			usleep_range(delay, delay + delay / 10);
 			ret = st_lsm6dsvx_read_locked(sensor->hw,
 						ST_LSM6DSVX_REG_STATUS_REG_ADDR,
 						&status, sizeof(status));
@@ -1127,18 +1175,27 @@ static int st_lsm6dsvx_selftest_sensor(struct st_lsm6dsvx_sensor *sensor,
 	if ((abs(x_selftest - x) < sensor->min_st) ||
 	    (abs(x_selftest - x) > sensor->max_st)) {
 		sensor->selftest_status = -1;
+		dev_err(sensor->hw->dev,
+			"st: failure on x: non-st(%d), st(%d)\n",
+			x, x_selftest);
 		goto selftest_failure;
 	}
 
 	if ((abs(y_selftest - y) < sensor->min_st) ||
 	    (abs(y_selftest - y) > sensor->max_st)) {
 		sensor->selftest_status = -1;
+		dev_err(sensor->hw->dev,
+			"st: failure on y: non-st(%d), st(%d)\n",
+			y, y_selftest);
 		goto selftest_failure;
 	}
 
 	if ((abs(z_selftest - z) < sensor->min_st) ||
 	    (abs(z_selftest - z) > sensor->max_st)) {
 		sensor->selftest_status = -1;
+		dev_err(sensor->hw->dev,
+			"st: failure on z: non-st(%d), st(%d)\n",
+			z, z_selftest);
 		goto selftest_failure;
 	}
 
