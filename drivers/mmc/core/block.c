ed9009ad300c0f15a3ecfe9613547b1962bde02c
Bean Huo <beanhuo@micron.com>
2023-10-30 23:48:09 +0100

    mmc: Add quirk MMC_QUIRK_BROKEN_CACHE_FLUSH for Micron eMMC Q2J54A
    Micron MTFC4GACAJCN eMMC supports cache but requires that flush cache
    operation be allowed only after a write has occurred. Otherwise, the
    cache flush command or subsequent commands will time out.
    
    Signed-off-by: Bean Huo <beanhuo@micron.com>
    Signed-off-by: Rafael Beims <rafael.beims@toradex.com>
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/r/20231030224809.59245-1-beanhuo@iokpp.de
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/core/block.c
+++ drivers/mmc/core/block.c
@@ -2381,8 +2381,10 @@ enum mmc_issued mmc_blk_mq_issue_rq(struct mmc_queue *mq, struct request *req)
 			}
 			ret = mmc_blk_cqe_issue_flush(mq, req);
 			break;
-		case REQ_OP_READ:
 		case REQ_OP_WRITE:
+			card->written_flag = true;
+			fallthrough;
+		case REQ_OP_READ:
 			if (host->cqe_enabled)
 				ret = mmc_blk_cqe_issue_rw_rq(mq, req);
 			else
