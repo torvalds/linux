955997e880175b00161efb83b1b282849ff412d3
Nikolay Borisov <nik.borisov@suse.com>
2023-10-30 16:17:28 +0200

    KVM: x86: Use mutex guards to eliminate __kvm_x86_vendor_init()
    Use the recently introduced guard(mutex) infrastructure acquire and
    automatically release vendor_module_lock when the guard goes out of scope.
    Drop the inner __kvm_x86_vendor_init(), its sole purpose was to simplify
    releasing vendor_module_lock in error paths.
    
    No functional change intended.
    
    Signed-off-by: Nikolay Borisov <nik.borisov@suse.com>
    Reviewed-by: Kai Huang <kai.huang@intel.com>
    Link: https://lore.kernel.org/r/20231030141728.1406118-1-nik.borisov@suse.com
    [sean: rewrite changelog]
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -9632,10 +9632,12 @@ static void kvm_x86_check_cpu_compat(void *ret)
 	*(int *)ret = kvm_x86_check_processor_compatibility();
 }
 
-static int __kvm_x86_vendor_init(struct kvm_x86_init_ops *ops)
+int kvm_x86_vendor_init(struct kvm_x86_init_ops *ops)
 {
 	u64 host_pat;
 	int r, cpu;
+
+	guard(mutex)(&vendor_module_lock);
 
 	if (kvm_x86_ops.hardware_enable) {
 		pr_err("already loaded vendor module '%s'\n", kvm_x86_ops.name);
@@ -9764,17 +9766,6 @@ out_free_percpu:
 	free_percpu(user_return_msrs);
 out_free_x86_emulator_cache:
 	kmem_cache_destroy(x86_emulator_cache);
-	return r;
-}
-
-int kvm_x86_vendor_init(struct kvm_x86_init_ops *ops)
-{
-	int r;
-
-	mutex_lock(&vendor_module_lock);
-	r = __kvm_x86_vendor_init(ops);
-	mutex_unlock(&vendor_module_lock);
-
 	return r;
 }
 EXPORT_SYMBOL_GPL(kvm_x86_vendor_init);
