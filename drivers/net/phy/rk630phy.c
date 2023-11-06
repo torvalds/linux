torvalds/linux

commit ee144563a528bc2558c4a34e9be0368f49dae9ae

net: phy: rk630: Enable aps && uaps to save power consumption
aps and uaps can save power consumption when RJ45 plug out.

Signed-off-by: David Wu <david.wu@rock-chips.com>
Change-Id: Iec36feb6ba1d734408efa9430e7889015c9d3fd1

drivers/net/phy/rk630phy.c
@@ -160,14 +160,33 @@ static void rk630_phy_ieee_set(struct phy_device *phydev, bool enable)
 	phy_write(phydev, REG_PAGE_SEL, 0x0000);
 }
 
-static void rk630_phy_set_uaps(struct phy_device *phydev)
+static void rk630_phy_set_aps(struct phy_device *phydev, bool enable)
+{
+	u32 value;
+
+	/* Switch to page 1 */
+	phy_write(phydev, REG_PAGE_SEL, 0x0100);
+	value = phy_read(phydev, REG_PAGE1_APS_CTRL);
+	if (enable)
+		value |= BIT(15);
+	else
+		value &= ~BIT(15);
+	phy_write(phydev, REG_PAGE1_APS_CTRL, value);
+	/* Switch to page 0 */
+	phy_write(phydev, REG_PAGE_SEL, 0x0000);
+}
+
+static void rk630_phy_set_uaps(struct phy_device *phydev, bool enable)
 {
 	u32 value;
 
 	/* Switch to page 1 */
 	phy_write(phydev, REG_PAGE_SEL, 0x0100);
 	value = phy_read(phydev, REG_PAGE1_UAPS_CONFIGURE);
-	value |= BIT(15);
+	if (enable)
+		value |= BIT(15);
+	else
+		value &= ~BIT(15);
 	phy_write(phydev, REG_PAGE1_UAPS_CONFIGURE, value);
 	/* Switch to page 0 */
 	phy_write(phydev, REG_PAGE_SEL, 0x0000);
@@ -268,10 +287,12 @@ static int rk630_phy_config_init(struct phy_device *phydev)
 		 * Ultra Auto-Power Saving Mode (UAPS) is designed to
 		 * save power when cable is not plugged into PHY.
 		 */
-		rk630_phy_set_uaps(phydev);
+		rk630_phy_set_uaps(phydev, true);
 		break;
 	case PHY_ADDR_T22:
 		rk630_phy_t22_config_init(phydev);
+		rk630_phy_set_aps(phydev, true);
+		rk630_phy_set_uaps(phydev, true);
 		break;
 	default:
 		phydev_err(phydev, "Unsupported address for current phy: %d\n",

commit 6a5f6ee5bfe4a51426e67d81b0d3106d6a3f0b83

net: phy: rk630: Fixed for reboot && ping issue
Fixes: ee144563a528 ("net: phy: rk630: Enable aps && uaps to save power consumption")
Fixes: b67f47f2429e ("net: phy: rk630phy: Add adc performance")

Signed-off-by: David Wu <david.wu@rock-chips.com>
Change-Id: I9519024f839e1648570ea98c77d2c22e58b9bc76

drivers/net/phy/rk630phy.c
@@ -238,7 +238,7 @@ static void rk630_phy_t22_config_init(struct phy_device *phydev)
 	/* Switch to page 6 */
 	phy_write(phydev, REG_PAGE_SEL, 0x0600);
 	/* PHYAFE ADC optimization */
-	phy_write(phydev, REG_PAGE6_ADC_ANONTROL, 0x555e);
+	phy_write(phydev, REG_PAGE6_ADC_ANONTROL, 0x5540);
 	/* PHYAFE Gain optimization */
 	phy_write(phydev, REG_PAGE6_GAIN_ANONTROL, 0x0400);
 	/* PHYAFE EQ optimization */
@@ -291,8 +291,8 @@ static int rk630_phy_config_init(struct phy_device *phydev)
 		break;
 	case PHY_ADDR_T22:
 		rk630_phy_t22_config_init(phydev);
-		rk630_phy_set_aps(phydev, true);
-		rk630_phy_set_uaps(phydev, true);
+		rk630_phy_set_aps(phydev, false);
+		rk630_phy_set_uaps(phydev, false);
 		break;
 	default:
 		phydev_err(phydev, "Unsupported address for current phy: %d\n",