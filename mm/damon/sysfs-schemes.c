31c59d59c7a7963e0ceec6c35f166d19ebabf8fd
SeongJae Park <sj@kernel.org>
2023-11-06 23:34:08 +0000

    UPSTREAM: mm/damon/sysfs-schemes: handle tried region directory allocation failure
    DAMON sysfs interface's before_damos_apply callback
    (damon_sysfs_before_damos_apply()), which creates the DAMOS tried regions
    for each DAMOS action applied region, is not handling the allocation
    failure for the sysfs directory data.  As a result, NULL pointer
    derefeence is possible.  Fix it by handling the case.
    
    Link: https://lkml.kernel.org/r/20231106233408.51159-4-sj@kernel.org
    Fixes: f1d13cacabe1 ("mm/damon/sysfs: implement DAMOS tried regions update command")
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Cc: <stable@vger.kernel.org>	[6.2+]
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (cherry picked from commit ae636ae2bbfd9279f5681dbf320d1da817e52b68)
    
    Bug: 300502883
    Change-Id: I98568f4b0cee9fea82f4fe6d3e7a505370c3c304
    Signed-off-by: cui yangpei <cuiyangpei@xiaomi.com>

--- mm/damon/sysfs-schemes.c
+++ mm/damon/sysfs-schemes.c
@@ -1344,6 +1344,8 @@ static int damon_sysfs_before_damos_apply(struct damon_ctx *ctx,
 		return 0;
 
 	region = damon_sysfs_scheme_region_alloc(r);
+	if (!region)
+		return 0;
 	list_add_tail(&region->list, &sysfs_regions->regions_list);
 	sysfs_regions->nr_regions++;
 	if (kobject_init_and_add(&region->kobj,
