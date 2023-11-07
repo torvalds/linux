6e8ff2d813a1e7863d784d10f525e0df739c906d
Shubham Diwane <quic_sanantad@quicinc.com>
2023-11-07 13:36:33 +0530

    dt-bindings: clock: Add support for clock ids for SDXLEMUR
    Add clock IDs for GCC and APSSCC clients to be able to request
    for clocks from these clock controllers.
    
    Change-Id: Iecb46c09728de1b6304021599368323c7313aed6
    Signed-off-by: Shubham Diwane <quic_sanantad@quicinc.com>

---
+++ include/dt-bindings/clock/qcom,apsscc-sdxlemur.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_APSS_CC_SDXLEMUR_H
+#define _DT_BINDINGS_CLK_QCOM_APSS_CC_SDXLEMUR_H
+
+/* APSS_CC clocks */
+#define APCS_CPU_PLL						0
+#define APCS_MUX_CLK						1
+
+#endif
