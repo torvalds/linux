bb619539629cee523df886705d6ef866e099640a
Hunter Chasens <hunter.chasens18@ncf.edu>
2023-11-07 11:28:30 -0500

    drm: amd: Resolve Sphinx unexpected indentation warning
    Resolves Sphinx unexpected indentation warning when compiling
    documentation (e.g. `make htmldocs`). Replaces tabs with spaces and adds
    a literal block to keep vertical formatting of the
    example power state list.
    
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>
    Reviewed-by: Bagas Sanjaya <bagasdotme@gmail.com> (v2)
    Acked-by: Randy Dunlap <rdunlap@infradead.org> (v2)
    Signed-off-by: Hunter Chasens <hunter.chasens18@ncf.edu>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/amdgpu_pm.c
+++ drivers/gpu/drm/amd/pm/amdgpu_pm.c
@@ -989,12 +989,13 @@ static ssize_t amdgpu_get_pp_features(struct device *dev,
  * Reading back the files will show you the available power levels within
  * the power state and the clock information for those levels. If deep sleep is
  * applied to a clock, the level will be denoted by a special level 'S:'
- * E.g.,
- *	S: 19Mhz *
- *	0: 615Mhz
- *	1: 800Mhz
- *	2: 888Mhz
- *	3: 1000Mhz
+ * E.g., ::
+ *
+ *  S: 19Mhz *
+ *  0: 615Mhz
+ *  1: 800Mhz
+ *  2: 888Mhz
+ *  3: 1000Mhz
  *
  *
  * To manually adjust these states, first select manual using
