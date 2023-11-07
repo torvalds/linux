ec5e692cbad49a2a31b49b1d153e41f31bb74a37
Hsin-Yi Wang <hsinyi@chromium.org>
2023-11-07 12:41:51 -0800

    drm/panel-edp: drm/panel-edp: Fix AUO B116XAK01 name and timing
    [ Upstream commit fc6e7679296530106ee0954e8ddef1aa58b2e0b5 ]
    
    Rename AUO 0x405c B116XAK01 to B116XAK01.0 and adjust the timing of
    auo_b116xak01: T3=200, T12=500, T7_max = 50 according to decoding edid
    and datasheet.
    
    Fixes: da458286a5e2 ("drm/panel: Add support for AUO B116XAK01 panel")
    Cc: stable@vger.kernel.org
    Signed-off-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Acked-by: Maxime Ripard <mripard@kernel.org>
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20231107204611.3082200-2-hsinyi@chromium.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/panel/panel-edp.c
+++ drivers/gpu/drm/panel/panel-edp.c
@@ -975,6 +975,8 @@ static const struct panel_desc auo_b116xak01 = {
 	},
 	.delay = {
 		.hpd_absent = 200,
+		.unprepare = 500,
+		.enable = 50,
 	},
 };
 
@@ -1870,7 +1872,7 @@ static const struct edp_panel_entry edp_panels[] = {
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x1062, &delay_200_500_e50, "B120XAN01.0"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x1e9b, &delay_200_500_e50, "B133UAN02.1"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x1ea5, &delay_200_500_e50, "B116XAK01.6"),
-	EDP_PANEL_ENTRY('A', 'U', 'O', 0x405c, &auo_b116xak01.delay, "B116XAK01"),
+	EDP_PANEL_ENTRY('A', 'U', 'O', 0x405c, &auo_b116xak01.delay, "B116XAK01.0"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x615c, &delay_200_500_e50, "B116XAN06.1"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x8594, &delay_200_500_e50, "B133UAN01.0"),
 
