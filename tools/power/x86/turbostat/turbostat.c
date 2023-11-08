b8337e6a780dad9505f9d44da07c0a5c52fa0a04
Len Brown <len.brown@intel.com>
2023-11-07 23:28:30 -0500

    tools/power turbostat: version 2023.11.07
    Turbostat features are now table-driven (Rui Zhang)
    Add support for some new platforms (Sumeet Pawnikar, Rui Zhang)
    Gracefully run in configs when CPUs are limited (Rui Zhang, Srinivas Pandruvada)
    misc minor fixes.
    
    Signed-off-by: Len Brown <len.brown@intel.com>

--- tools/power/x86/turbostat/turbostat.c
+++ tools/power/x86/turbostat/turbostat.c
@@ -6259,7 +6259,7 @@ int get_and_dump_counters(void)
 
 void print_version()
 {
-	fprintf(outf, "turbostat version 2023.03.17 - Len Brown <lenb@kernel.org>\n");
+	fprintf(outf, "turbostat version 2023.11.07 - Len Brown <lenb@kernel.org>\n");
 }
 
 #define COMMAND_LINE_SIZE 2048
