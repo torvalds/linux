4ddda80541a292c9dde742b5d6ee7b7400cc3d84
Yu Qiaowei <cerf.yu@rock-chips.com>
2023-11-09 10:15:05 +0800

    video: rockchip: rga3: fix the error report of Coverity
    1. Fix the issue of scheduler and hardware maximum count mismatch.
    2. Fix the issue of potential GCD returning 0.
    
    Change-Id: I083553e69ab60549b26465b5d1437e817575856f
    Signed-off-by: Yu Qiaowei <cerf.yu@rock-chips.com>

--- drivers/video/rockchip/rga3/rga_policy.c
+++ drivers/video/rockchip/rga3/rga_policy.c
@@ -15,7 +15,7 @@
 #define GET_GCD(n1, n2) \
 	({ \
 		int i; \
-		int gcd = 0; \
+		int gcd = 1; \
 		for (i = 1; i <= (n1) && i <= (n2); i++) { \
 			if ((n1) % i == 0 && (n2) % i == 0) \
 				gcd = i; \
