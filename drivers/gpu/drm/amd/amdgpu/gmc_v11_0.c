0db062eac3e0846c6f120867a79df83b4c3db46f
Alex Deucher <alexander.deucher@amd.com>
2023-11-09 15:34:19 -0500

    drm/amdgpu/gmc11: disable AGP aperture
    We've had misc reports of random IOMMU page faults when
    this is used.  It's just a rarely used optimization anyway, so
    let's just disable it.  It can still be toggled via the
    module parameter for testing.
    
    v2: leave it configurable via module parameter
    
    Fixes: 67318cb84341 ("drm/amdgpu/gmc11: set gart placement GC11")
    Reviewed-by: Yang Wang <kevinyang.wang@amd.com> (v1)
    Acked-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>
    Tested-by: Mario Limonciello <mario.limonciello@amd.com> # PHX & Navi33
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gmc_v11_0.c
+++ drivers/gpu/drm/amd/amdgpu/gmc_v11_0.c
@@ -642,7 +642,7 @@ static void gmc_v11_0_vram_gtt_location(struct amdgpu_device *adev,
 	amdgpu_gmc_gart_location(adev, mc, AMDGPU_GART_PLACEMENT_HIGH);
 	if (!amdgpu_sriov_vf(adev) &&
 	    (amdgpu_ip_version(adev, GC_HWIP, 0) < IP_VERSION(11, 5, 0)) &&
-	    (amdgpu_agp != 0))
+	    (amdgpu_agp == 1))
 		amdgpu_gmc_agp_location(adev, mc);
 
 	/* base offset of vram pages */
