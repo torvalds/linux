1777bb5c740d6d8837dbae61b4694ef90f363221
Hyeongtak Ji <hyeongtak.ji@sk.com>
2023-11-10 14:37:09 +0900

    mm/damon/core.c: avoid unintentional filtering out of schemes
    commit 13b2a4b22e98ff80b888a160a2acd92d81b05925 upstream.
    
    The function '__damos_filter_out()' causes DAMON to always filter out
    schemes whose filter type is anon or memcg if its matching value is set
    to false.
    
    This commit addresses the issue by ensuring that '__damos_filter_out()'
    no longer applies to filters whose type is 'anon' or 'memcg'.
    
    Link: https://lkml.kernel.org/r/1699594629-3816-1-git-send-email-hyeongtak.ji@gmail.com
    Fixes: ab9bda001b681 ("mm/damon/core: introduce address range type damos filter")
    Signed-off-by: Hyeongtak Ji <hyeongtak.ji@sk.com>
    Reviewed-by: SeongJae Park <sj@kernel.org>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/damon/core.c
+++ mm/damon/core.c
@@ -914,7 +914,7 @@ static bool __damos_filter_out(struct damon_ctx *ctx, struct damon_target *t,
 		matched = true;
 		break;
 	default:
-		break;
+		return false;
 	}
 
 	return matched == filter->matching;
