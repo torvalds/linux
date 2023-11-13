a7d5a955bfa854ac6b0c53aaf933394b4e6139e4
Pablo Neira Ayuso <pablo@netfilter.org>
2023-11-13 20:34:56 +0100

    netfilter: nf_tables: bogus ENOENT when destroying element which does not exist
    destroy element command bogusly reports ENOENT in case a set element
    does not exist. ENOENT errors are skipped, however, err is still set
    and propagated to userspace.
    
     # nft destroy element ip raw BLACKLIST { 1.2.3.4 }
     Error: Could not process rule: No such file or directory
     destroy element ip raw BLACKLIST { 1.2.3.4 }
     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    Fixes: f80a612dd77c ("netfilter: nf_tables: add support to destroy operation")
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -7263,10 +7263,11 @@ static int nf_tables_delsetelem(struct sk_buff *skb,
 
 		if (err < 0) {
 			NL_SET_BAD_ATTR(extack, attr);
-			break;
+			return err;
 		}
 	}
-	return err;
+
+	return 0;
 }
 
 /*
