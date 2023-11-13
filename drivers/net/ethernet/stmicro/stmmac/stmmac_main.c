7414a28de1b3b028714859078c00a874f9feff52
Baruch Siach <baruch@tkos.co.il>
2023-11-13 19:42:50 +0200

    net: stmmac: avoid rx queue overrun
    [ Upstream commit b6cb4541853c7ee512111b0e7ddf3cb66c99c137 ]
    
    dma_rx_size can be set as low as 64. Rx budget might be higher than
    that. Make sure to not overrun allocated rx buffers when budget is
    larger.
    
    Leave one descriptor unused to avoid wrap around of 'dirty_rx' vs
    'cur_rx'.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Reviewed-by: Serge Semin <fancer.lancer@gmail.com>
    Fixes: 47dd7a540b8a ("net: add support for STMicroelectronics Ethernet controllers.")
    Link: https://lore.kernel.org/r/d95413e44c97d4692e72cec13a75f894abeb6998.1699897370.git.baruch@tkos.co.il
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -5198,6 +5198,7 @@ static int stmmac_rx(struct stmmac_priv *priv, int limit, u32 queue)
 
 	dma_dir = page_pool_get_dma_dir(rx_q->page_pool);
 	buf_sz = DIV_ROUND_UP(priv->dma_conf.dma_buf_sz, PAGE_SIZE) * PAGE_SIZE;
+	limit = min(priv->dma_conf.dma_rx_size - 1, (unsigned int)limit);
 
 	if (netif_msg_rx_status(priv)) {
 		void *rx_head;
