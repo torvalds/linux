a9dc6f2e27baf909a61c428b7d03fd1312703230
Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
2023-11-13 12:07:37 -0800

    platform/x86: hp-bioscfg: Simplify return check in hp_add_other_attributes()
    commit c5dbf04160005e07e8ca7232a7faa77ab1547ae0 upstream.
    
    All cases in switch-case have a same goto on error, move the return
    check out of the switch. This is a cleanup.
    
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Link: https://lore.kernel.org/r/20231113200742.3593548-1-harshit.m.mogalapalli@oracle.com
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/platform/x86/hp/hp-bioscfg/bioscfg.c
+++ drivers/platform/x86/hp/hp-bioscfg/bioscfg.c
@@ -630,20 +630,18 @@ static int hp_add_other_attributes(int attr_type)
 	switch (attr_type) {
 	case HPWMI_SECURE_PLATFORM_TYPE:
 		ret = hp_populate_secure_platform_data(attr_name_kobj);
-		if (ret)
-			goto err_other_attr_init;
 		break;
 
 	case HPWMI_SURE_START_TYPE:
 		ret = hp_populate_sure_start_data(attr_name_kobj);
-		if (ret)
-			goto err_other_attr_init;
 		break;
 
 	default:
 		ret = -EINVAL;
-		goto err_other_attr_init;
 	}
+
+	if (ret)
+		goto err_other_attr_init;
 
 	mutex_unlock(&bioscfg_drv.mutex);
 	return 0;
