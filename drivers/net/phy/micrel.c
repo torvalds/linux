d06b88b01fc1e446ae5dd76e06b944a26bc1a245
Kory Maincent <kory.maincent@bootlin.com>
2023-11-14 12:28:34 +0100

    net: phy: micrel: fix ts_info value in case of no phc
    [ Upstream commit 915d25a9d69be969c1cc6c1dd0c3861f6da7b55e ]
    
    In case of no phc we should not return SOFTWARE TIMESTAMPING flags as we do
    not know whether the netdev supports of timestamping.
    Remove it from the lan8841_ts_info and simply return 0.
    
    Signed-off-by: Kory Maincent <kory.maincent@bootlin.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/phy/micrel.c
+++ drivers/net/phy/micrel.c
@@ -3628,12 +3628,8 @@ static int lan8841_ts_info(struct mii_timestamper *mii_ts,
 
 	info->phc_index = ptp_priv->ptp_clock ?
 				ptp_clock_index(ptp_priv->ptp_clock) : -1;
-	if (info->phc_index == -1) {
-		info->so_timestamping |= SOF_TIMESTAMPING_TX_SOFTWARE |
-					 SOF_TIMESTAMPING_RX_SOFTWARE |
-					 SOF_TIMESTAMPING_SOFTWARE;
+	if (info->phc_index == -1)
 		return 0;
-	}
 
 	info->so_timestamping = SOF_TIMESTAMPING_TX_HARDWARE |
 				SOF_TIMESTAMPING_RX_HARDWARE |
