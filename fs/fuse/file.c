cfdfc17a46d0310b2cbe3e654bd8796bf4c75ba2
Paul Lawrence <paullawrence@google.com>
2023-11-15 14:29:23 -0800

    ANDROID: fuse-bpf: Ignore readaheads unless they go to the daemon
    readpages will be triggered on the fuse fs in passthrough mode though
    system calls like fadvise. If the daemon isn't aware of the file, this
    will likely cause a hang.
    
    For the moment, simply ignore fadvise in this situation
    
    Bug: 301201239
    Test: fuse_test, atest ScopedStorageDeviceTest both pass
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: I524a84aeeb1b1593e51264fcc37f7cfa66757168

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1026,6 +1026,16 @@ static void fuse_readahead(struct readahead_control *rac)
 	struct fuse_conn *fc = get_fuse_conn(inode);
 	unsigned int i, max_pages, nr_pages = 0;
 
+#ifdef CONFIG_FUSE_BPF
+	/*
+	 * Currently no meaningful readahead is possible with fuse-bpf within
+	 * the kernel, so unless the daemon is aware of this file, ignore this
+	 * call.
+	 */
+	if (!get_fuse_inode(inode)->nodeid)
+		return;
+#endif
+
 	if (fuse_is_bad(inode))
 		return;
 
