080990aa3344123673f686cda2df0d1b0deee046
Borislav Petkov (AMD) <bp@alien8.de>
2023-11-15 22:02:12 +0100

    x86/microcode: Rework early revisions reporting
    The AMD side of the loader issues the microcode revision for each
    logical thread on the system, which can become really noisy on huge
    machines. And doing that doesn't make a whole lot of sense - the
    microcode revision is already in /proc/cpuinfo.
    
    So in case one is interested in the theoretical support of mixed silicon
    steppings on AMD, one can check there.
    
    What is also missing on the AMD side - something which people have
    requested before - is showing the microcode revision the CPU had
    *before* the early update.
    
    So abstract that up in the main code and have the BSP on each vendor
    provide those revision numbers.
    
    Then, dump them only once on driver init.
    
    On Intel, do not dump the patch date - it is not needed.
    
    Reported-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/CAHk-=wg=%2B8rceshMkB4VnKxmRccVLtBLPBawnewZuuqyx5U=3A@mail.gmail.com

--- arch/x86/kernel/cpu/microcode/core.c
+++ arch/x86/kernel/cpu/microcode/core.c
@@ -75,6 +75,8 @@ static u32 final_levels[] = {
 	0, /* T-101 terminator */
 };
 
+struct early_load_data early_data;
+
 /*
  * Check the current patch level on this CPU.
  *
@@ -153,9 +155,9 @@ void __init load_ucode_bsp(void)
 		return;
 
 	if (intel)
-		load_ucode_intel_bsp();
+		load_ucode_intel_bsp(&early_data);
 	else
-		load_ucode_amd_bsp(cpuid_1_eax);
+		load_ucode_amd_bsp(&early_data, cpuid_1_eax);
 }
 
 void load_ucode_ap(void)
@@ -825,6 +827,11 @@ static int __init microcode_init(void)
 
 	if (!microcode_ops)
 		return -ENODEV;
+
+	pr_info_once("Current revision: 0x%08x\n", (early_data.new_rev ?: early_data.old_rev));
+
+	if (early_data.new_rev)
+		pr_info_once("Updated early from: 0x%08x\n", early_data.old_rev);
 
 	microcode_pdev = platform_device_register_simple("microcode", -1, NULL, 0);
 	if (IS_ERR(microcode_pdev))
