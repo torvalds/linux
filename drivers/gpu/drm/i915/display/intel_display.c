1aba67132cbc46856dfa8f904cd7021a75b1806d
Juha-Pekka Heikkila <juhapekka.heikkila@gmail.com>
2023-11-16 17:02:23 +0200

    drm/i915/display: Separate xe and i915 common dpt code into own file
    Here created intel_dpt_common.c to hold intel_dpt_configure which is
    needed for both xe and i915.
    
    Signed-off-by: Juha-Pekka Heikkila <juhapekka.heikkila@gmail.com>
    Reviewed-by: Jouni Högander <jouni.hogander@intel.com>
    Signed-off-by: Jouni Högander <jouni.hogander@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20231116150225.204233-1-juhapekka.heikkila@gmail.com

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -76,6 +76,7 @@
 #include "intel_dpll.h"
 #include "intel_dpll_mgr.h"
 #include "intel_dpt.h"
+#include "intel_dpt_common.h"
 #include "intel_drrs.h"
 #include "intel_dsb.h"
 #include "intel_dsi.h"
