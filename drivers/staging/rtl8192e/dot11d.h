cea57157fa7af3eba8f386642937f4964df4ce90
Philipp Hortmann <philipp.g.hortmann@gmail.com>
2023-11-19 23:15:26 +0100

    staging: rtl8192e: Remove unused variables from struct rt_dot11d_info
    Remove unused variables country_len, country_buffer, country_src_addr and
    max_tx_power_list. Remove comments about usage as well.
    
    Signed-off-by: Philipp Hortmann <philipp.g.hortmann@gmail.com>
    Reviewed-by: Dan Carpenter <dan.carpenter@linaro.org>
    Link: https://lore.kernel.org/r/eeb9915d68c353554973e1056893371d15381f88.1700431464.git.philipp.g.hortmann@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/dot11d.h
+++ drivers/staging/rtl8192e/dot11d.h
@@ -17,23 +17,16 @@ struct chnl_txpow_triple {
 };
 
 /**
- * struct rt_dot11d_info * @country_len: value greater than 0 if
- *		  @country_buffer contains valid country information element.
+ * struct rt_dot11d_info
  * @channel_map: holds channel values
  *		0 - invalid,
  *		1 - valid (active scan),
  *		2 - valid (passive scan)
- * @country_src_addr - Source AP of the country IE
  */
 
 struct rt_dot11d_info {
-	u16 country_len;
-	u8  country_buffer[MAX_IE_LEN];
-	u8  country_src_addr[6];
 	u8  country_watchdog;
-
 	u8  channel_map[MAX_CHANNEL_NUMBER + 1];
-	u8  max_tx_power_list[MAX_CHANNEL_NUMBER + 1];
 };
 
 #define GET_DOT11D_INFO(__ieee_dev)			\
