30614ed295bc55295ebe0a60abb484cc8cee0cd0
Jack Wang <jinpu.wang@ionos.com>
2023-11-20 16:41:39 +0100

    RDMA/rtrs-clt: Start hb after path_up
    [ Upstream commit 3e44a61b5db873612e20e7b7922468d7d1ac2d22 ]
    
    If we start hb too early, it will confuse server side to close
    the session.
    
    Fixes: 6a98d71daea1 ("RDMA/rtrs: client: main functionality")
    Signed-off-by: Jack Wang <jinpu.wang@ionos.com>
    Reviewed-by: Md Haris Iqbal <haris.iqbal@ionos.com>
    Signed-off-by: Grzegorz Prajsner <grzegorz.prajsner@ionos.com>
    Link: https://lore.kernel.org/r/20231120154146.920486-3-haris.iqbal@ionos.com
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/infiniband/ulp/rtrs/rtrs-clt.c
+++ drivers/infiniband/ulp/rtrs/rtrs-clt.c
@@ -2345,8 +2345,6 @@ static int init_conns(struct rtrs_clt_path *clt_path)
 	if (err)
 		goto destroy;
 
-	rtrs_start_hb(&clt_path->s);
-
 	return 0;
 
 destroy:
@@ -2620,6 +2618,7 @@ static int init_path(struct rtrs_clt_path *clt_path)
 		goto out;
 	}
 	rtrs_clt_path_up(clt_path);
+	rtrs_start_hb(&clt_path->s);
 out:
 	mutex_unlock(&clt_path->init_mutex);
 
