d75f7c1bd8fd0144b766239838e250e5aee9e375
Cong Yang <yangcong5@huaqin.corp-partner.google.com>
2023-11-20 10:01:09 +0800

    drm/panel: boe-tv101wum-nl6: Fine tune Himax83102-j02 panel HFP and HBP
    [ Upstream commit cea7008190ad65b4aaae6e94667a358d2c10a696 ]
    
    The refresh reported by modetest is 60.46Hz, and the actual measurement
    is 60.01Hz, which is outside the expected tolerance. Adjust hporch and
    pixel clock to fix it. After repair, modetest and actual measurement were
    all 60.01Hz.
    
    Modetest refresh = Pixel CLK/ htotal* vtotal, but measurement frame rate
    is HS->LP cycle time(Vblanking). Measured frame rate is not only affecte
    by Htotal/Vtotal/pixel clock, also affected by Lane-num/PixelBit/LineTime
    /DSI CLK. Assume that the DSI controller could not make the mode that we
    requested(presumably it's PLL couldn't generate the exact pixel clock?).
    If you use a different DSI controller, you may need to readjust these
    parameters. Now this panel looks like it's only used by me on the MTK
    platform, so let's change this set of parameters.
    
    Fixes: 1bc2ef065f13 ("drm/panel: Support for Starry-himax83102-j02 TDDI MIPI-DSI panel")
    Signed-off-by: Cong Yang <yangcong5@huaqin.corp-partner.google.com>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20231120020109.3216343-1-yangcong5@huaqin.corp-partner.google.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/panel/panel-boe-tv101wum-nl6.c
+++ drivers/gpu/drm/panel/panel-boe-tv101wum-nl6.c
@@ -2107,11 +2107,11 @@ static const struct panel_desc starry_qfh032011_53g_desc = {
 };
 
 static const struct drm_display_mode starry_himax83102_j02_default_mode = {
-	.clock = 161600,
+	.clock = 162850,
 	.hdisplay = 1200,
-	.hsync_start = 1200 + 40,
-	.hsync_end = 1200 + 40 + 20,
-	.htotal = 1200 + 40 + 20 + 40,
+	.hsync_start = 1200 + 50,
+	.hsync_end = 1200 + 50 + 20,
+	.htotal = 1200 + 50 + 20 + 50,
 	.vdisplay = 1920,
 	.vsync_start = 1920 + 116,
 	.vsync_end = 1920 + 116 + 8,
