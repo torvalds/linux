d73dcff9eb0d30956e9caa86d0d94d4c8e402037
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2023-11-21 16:53:02 +0100

    net: ethernet: renesas: rcar_gen4_ptp: Remove incorrect comment
    The comments intent was to indicates which function uses the enum. While
    upstreaming rcar_gen4_ptp the function was renamed but this comment was
    left with the old function name.
    
    Instead of correcting the comment remove it, it adds little value.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Reviewed-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- drivers/net/ethernet/renesas/rcar_gen4_ptp.h
+++ drivers/net/ethernet/renesas/rcar_gen4_ptp.h
@@ -13,7 +13,6 @@
 #define RCAR_GEN4_PTP_CLOCK_S4		PTPTIVC_INIT
 #define RCAR_GEN4_GPTP_OFFSET_S4	0x00018000
 
-/* for rcar_gen4_ptp_init */
 enum rcar_gen4_ptp_reg_layout {
 	RCAR_GEN4_PTP_REG_LAYOUT_S4
 };
