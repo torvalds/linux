ab3f300524697919f64ae920e904d0836b4057b0
WANG Rui <wangrui@loongson.cn>
2023-11-21 15:03:25 +0800

    LoongArch: Record pc instead of offset in la_abs relocation
    [ Upstream commit aa0cbc1b506b090c3a775b547c693ada108cc0d7 ]
    
    To clarify, the previous version functioned flawlessly. However, it's
    worth noting that the LLVM's LoongArch backend currently lacks support
    for cross-section label calculations. With this patch, we enable the use
    of clang to compile relocatable kernels.
    
    Tested-by: Nathan Chancellor <nathan@kernel.org>
    Signed-off-by: WANG Rui <wangrui@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/loongarch/kernel/relocate.c
+++ arch/loongarch/kernel/relocate.c
@@ -52,7 +52,7 @@ static inline void __init relocate_absolute(long random_offset)
 	for (p = begin; (void *)p < end; p++) {
 		long v = p->symvalue;
 		uint32_t lu12iw, ori, lu32id, lu52id;
-		union loongarch_instruction *insn = (void *)p - p->offset;
+		union loongarch_instruction *insn = (void *)p->pc;
 
 		lu12iw = (v >> 12) & 0xfffff;
 		ori    = v & 0xfff;
