ecf5dd1ffe84ede52d8a6c9fb72d8aad04ff8160
Serge Semin <fancer.lancer@gmail.com>
2023-11-22 21:24:03 +0300

    mm/mm_init.c: extend init unavailable range doc info
    Besides of the already described reasons the pages backended memory holes
    might be persistent due to having memory mapped IO spaces behind those
    ranges in the framework of flatmem kernel config.  Add such note to the
    init_unavailable_range() method kdoc in order to point out to one more
    reason of having the function executed for such regions.
    
    [fancer.lancer@gmail.com: update per Mike]
      Link: https://lkml.kernel.org/r/20231202111855.18392-1-fancer.lancer@gmail.com
    Link: https://lkml.kernel.org/r/20231122182419.30633-6-fancer.lancer@gmail.com
    Signed-off-by: Serge Semin <fancer.lancer@gmail.com>
    Reviewed-by: Mike Rapoport (IBM) <rppt@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/mm_init.c
+++ mm/mm_init.c
@@ -796,6 +796,7 @@ overlap_memmap_init(unsigned long zone, unsigned long *pfn)
  * - physical memory bank size is not necessarily the exact multiple of the
  *   arbitrary section size
  * - early reserved memory may not be listed in memblock.memory
+ * - non-memory regions covered by the contigious flatmem mapping
  * - memory layouts defined with memmap= kernel parameter may not align
  *   nicely with memmap sections
  *
