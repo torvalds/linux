d0a2bc5fe712217d2c73822ae75fd4e69a15cb2c
Ming Yen Hsieh <mingyen.hsieh@mediatek.com>
2023-11-22 11:06:45 +0800

    wifi: mt76: mt7921: fix CLC command timeout when suspend/resume
    When enter suspend/resume while in a connected state, the upper layer
    will trigger disconnection before entering suspend, and at the same time,
    it will trigger regd_notifier() and update CLC, causing the CLC event to
    not be received due to suspend, resulting in a command timeout.
    
    Therefore, the update of CLC is postponed until resume, to ensure data
    consistency and avoid the occurrence of command timeout.
    
    Fixes: 4fc8df50fd41 ("wifi: mt76: mt7921: get regulatory information from the clc event")
    Signed-off-by: Ming Yen Hsieh <mingyen.hsieh@mediatek.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7921/pci.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/pci.c
@@ -507,6 +507,9 @@ static int mt7921_pci_resume(struct device *device)
 		mt76_connac_mcu_set_deep_sleep(&dev->mt76, false);
 
 	err = mt76_connac_mcu_set_hif_suspend(mdev, false);
+
+	mt7921_regd_update(dev);
+
 failed:
 	pm->suspended = false;
 
