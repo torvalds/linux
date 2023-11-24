88ead68e764cd164abb965e258c4e18841433ecf
Atish Patra <atishp@rivosinc.com>
2023-11-24 12:39:04 +0530

    tty: Add SBI debug console support to HVC SBI driver
    RISC-V SBI specification supports advanced debug console
    support via SBI DBCN extension.
    
    Extend the HVC SBI driver to support it.
    
    Signed-off-by: Atish Patra <atishp@rivosinc.com>
    Signed-off-by: Anup Patel <apatel@ventanamicro.com>
    Reviewed-by: Andrew Jones <ajones@ventanamicro.com>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://lore.kernel.org/r/20231124070905.1043092-5-apatel@ventanamicro.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>

--- drivers/tty/hvc/hvc_riscv_sbi.c
+++ drivers/tty/hvc/hvc_riscv_sbi.c
@@ -39,21 +39,44 @@ static int hvc_sbi_tty_get(uint32_t vtermno, char *buf, int count)
 	return i;
 }
 
-static const struct hv_ops hvc_sbi_ops = {
+static const struct hv_ops hvc_sbi_v01_ops = {
 	.get_chars = hvc_sbi_tty_get,
 	.put_chars = hvc_sbi_tty_put,
 };
 
+static int hvc_sbi_dbcn_tty_put(uint32_t vtermno, const char *buf, int count)
+{
+	return sbi_debug_console_write(buf, count);
+}
+
+static int hvc_sbi_dbcn_tty_get(uint32_t vtermno, char *buf, int count)
+{
+	return sbi_debug_console_read(buf, count);
+}
+
+static const struct hv_ops hvc_sbi_dbcn_ops = {
+	.put_chars = hvc_sbi_dbcn_tty_put,
+	.get_chars = hvc_sbi_dbcn_tty_get,
+};
+
 static int __init hvc_sbi_init(void)
 {
-	return PTR_ERR_OR_ZERO(hvc_alloc(0, 0, &hvc_sbi_ops, 16));
-}
-device_initcall(hvc_sbi_init);
+	int err;
 
-static int __init hvc_sbi_console_init(void)
-{
-	hvc_instantiate(0, 0, &hvc_sbi_ops);
+	if (sbi_debug_console_available) {
+		err = PTR_ERR_OR_ZERO(hvc_alloc(0, 0, &hvc_sbi_dbcn_ops, 256));
+		if (err)
+			return err;
+		hvc_instantiate(0, 0, &hvc_sbi_dbcn_ops);
+	} else if (IS_ENABLED(CONFIG_RISCV_SBI_V01)) {
+		err = PTR_ERR_OR_ZERO(hvc_alloc(0, 0, &hvc_sbi_v01_ops, 256));
+		if (err)
+			return err;
+		hvc_instantiate(0, 0, &hvc_sbi_v01_ops);
+	} else {
+		return -ENODEV;
+	}
 
 	return 0;
 }
-console_initcall(hvc_sbi_console_init);
+device_initcall(hvc_sbi_init);
