bd029a02ce46e77e4d553140b039f93cf78ee8c1
Jim.Lin <jim.chihjung.lin@gmail.com>
2023-11-28 10:57:37 +0800

    nvme-pci: disable write zeroes for SK Hynix BC901
    SK Hynix BC901 drive write zero will cause Chromebook takes more than 20 mins to switch to developer mode
    "disable write zeroes" can fix this issue and Sk Hynix has been verified.
    
    Signed-off-by: Jim.Lin <jim.lin@siliconmotion.com>
    Signed-off-by: Keith Busch <kbusch@kernel.org>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -3394,6 +3394,8 @@ static const struct pci_device_id nvme_id_table[] = {
 		.driver_data = NVME_QUIRK_DISABLE_WRITE_ZEROES, },
 	{ PCI_DEVICE(0x1c5c, 0x174a),   /* SK Hynix P31 SSD */
 		.driver_data = NVME_QUIRK_BOGUS_NID, },
+	{ PCI_DEVICE(0x1c5c, 0x1D59),   /* SK Hynix BC901 */
+		.driver_data = NVME_QUIRK_DISABLE_WRITE_ZEROES, },
 	{ PCI_DEVICE(0x15b7, 0x2001),   /*  Sandisk Skyhawk */
 		.driver_data = NVME_QUIRK_DISABLE_WRITE_ZEROES, },
 	{ PCI_DEVICE(0x1d97, 0x2263),   /* SPCC */
