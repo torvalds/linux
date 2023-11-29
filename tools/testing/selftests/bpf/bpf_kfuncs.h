341f06fdddf72cd60a10945152f69f0f1d614519
Song Liu <song@kernel.org>
2023-11-29 15:44:16 -0800

    selftests/bpf: Add tests for filesystem kfuncs
    Add selftests for two new filesystem kfuncs:
      1. bpf_get_file_xattr
      2. bpf_get_fsverity_digest
    
    These tests simply make sure the two kfuncs work. Another selftest will be
    added to demonstrate how to use these kfuncs to verify file signature.
    
    CONFIG_FS_VERITY is added to selftests config. However, this is not
    sufficient to guarantee bpf_get_fsverity_digest works. This is because
    fsverity need to be enabled at file system level (for example, with tune2fs
    on ext4). If local file system doesn't have this feature enabled, just skip
    the test.
    
    Signed-off-by: Song Liu <song@kernel.org>
    Link: https://lore.kernel.org/r/20231129234417.856536-6-song@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/testing/selftests/bpf/bpf_kfuncs.h
+++ tools/testing/selftests/bpf/bpf_kfuncs.h
@@ -55,4 +55,7 @@ void *bpf_cast_to_kern_ctx(void *) __ksym;
 
 void *bpf_rdonly_cast(void *obj, __u32 btf_id) __ksym;
 
+extern int bpf_get_file_xattr(struct file *file, const char *name,
+			      struct bpf_dynptr *value_ptr) __ksym;
+extern int bpf_get_fsverity_digest(struct file *file, struct bpf_dynptr *digest_ptr) __ksym;
 #endif
