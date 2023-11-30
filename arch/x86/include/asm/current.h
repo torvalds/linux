4604c052b84d66407f5e68045a1939685eac401e
Uros Bizjak <ubizjak@gmail.com>
2023-11-30 17:27:35 +0100

    x86/percpu: Declare const_pcpu_hot as extern const variable
    const_pcpu_hot is aliased by linker to pcpu_hot, so there is no need
    to use the DECLARE_PER_CPU_ALIGNED() macro. Also, declare const_pcpu_hot
    as extern to avoid allocating storage space for the aliased structure.
    
    Fixes: ed2f752e0e0a ("x86/percpu: Introduce const-qualified const_pcpu_hot to micro-optimize code generation")
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Uros Bizjak <ubizjak@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20231130162949.83518-1-ubizjak@gmail.com
    Closes: https://lore.kernel.org/oe-kbuild-all/202311302257.tSFtZnly-lkp@intel.com/

--- arch/x86/include/asm/current.h
+++ arch/x86/include/asm/current.h
@@ -37,8 +37,7 @@ static_assert(sizeof(struct pcpu_hot) == 64);
 DECLARE_PER_CPU_ALIGNED(struct pcpu_hot, pcpu_hot);
 
 /* const-qualified alias to pcpu_hot, aliased by linker. */
-DECLARE_PER_CPU_ALIGNED(const struct pcpu_hot __percpu_seg_override,
-			const_pcpu_hot);
+extern const struct pcpu_hot __percpu_seg_override const_pcpu_hot;
 
 static __always_inline struct task_struct *get_current(void)
 {
