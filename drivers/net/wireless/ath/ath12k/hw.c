ed7e818a7b501012038d6bc6fedadaf7375a380a
Kang Yang <quic_kangyang@quicinc.com>
2023-12-01 18:09:48 +0200

    wifi: ath12k: fix and enable AP mode for WCN7850
    For AP mode, the peer is created earlier in ath12k_mac_op_add_interface() but
    ath12k_mac_op_assign_vif_chanctx() will try to create peer again.  Then an
    error will return which makes AP mode startup fail.
    
    Kernel log:
    
    [ 5017.665006] ath12k_pci 0000:04:00.0: failed to create peer after vdev start delay: -22
    
    wpa_supplicant log:
    
    Failed to set beacon parameters
    Interface initialization failed
    wls1: interface state UNINITIALIZED->DISABLED
    wls1: AP-DISABLED
    wls1: Unable to setup interface.
    Failed to initialize AP interface
    wls1: interface state DISABLED->DISABLED
    wls1: AP-DISABLED
    
    So fix this check and enable AP mode for WCN7850, as now AP mode works normally.
    
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0-03427-QCAHMTSWPL_V1.0_V2.0_SILICONZ-1.15378.4
    
    Signed-off-by: Kang Yang <quic_kangyang@quicinc.com>
    Acked-by: Jeff Johnson <quic_jjohnson@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://lore.kernel.org/r/20231121022459.17209-1-quic_kangyang@quicinc.com

--- drivers/net/wireless/ath/ath12k/hw.c
+++ drivers/net/wireless/ath/ath12k/hw.c
@@ -949,7 +949,8 @@ static const struct ath12k_hw_params ath12k_hw_params[] = {
 		.rx_mac_buf_ring = true,
 		.vdev_start_delay = true,
 
-		.interface_modes = BIT(NL80211_IFTYPE_STATION),
+		.interface_modes = BIT(NL80211_IFTYPE_STATION) |
+				   BIT(NL80211_IFTYPE_AP),
 		.supports_monitor = false,
 
 		.idle_ps = true,
