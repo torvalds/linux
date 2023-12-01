3c323796951a8bed0eb4e950e4a5f6203c31202a
Kunwu Chan <chentao@kylinos.cn>
2023-12-01 13:54:47 +0800

    platform/mellanox: Check devm_hwmon_device_register_with_groups() return value
    [ Upstream commit 3494a594315b56516988afb6854d75dee5b501db ]
    
    devm_hwmon_device_register_with_groups() returns an error pointer upon
    failure. Check its return value for errors.
    
    Compile-tested only.
    
    Fixes: 1a218d312e65 ("platform/mellanox: mlxbf-pmc: Add Mellanox BlueField PMC driver")
    Suggested-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Suggested-by: Vadim Pasternak <vadimp@nvidia.com>
    Signed-off-by: Kunwu Chan <chentao@kylinos.cn>
    Reviewed-by: Vadim Pasternak <vadimp@nvidia.com>
    Link: https://lore.kernel.org/r/20231201055447.2356001-1-chentao@kylinos.cn
    [ij: split the change into two]
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/platform/mellanox/mlxbf-pmc.c
+++ drivers/platform/mellanox/mlxbf-pmc.c
@@ -1454,6 +1454,8 @@ static int mlxbf_pmc_probe(struct platform_device *pdev)
 
 	pmc->hwmon_dev = devm_hwmon_device_register_with_groups(
 		dev, "bfperf", pmc, pmc->groups);
+	if (IS_ERR(pmc->hwmon_dev))
+		return PTR_ERR(pmc->hwmon_dev);
 	platform_set_drvdata(pdev, pmc);
 
 	return 0;
