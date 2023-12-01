d6a57588666301acd9d42d3b00d74240964f07f6
Jiadong Zhu <Jiadong.Zhu@amd.com>
2023-12-01 08:38:15 +0800

    drm/amdgpu: disable MCBP by default
    Disable MCBP(mid command buffer preemption) by default as old Mesa
    hangs with it. We shall not enable the feature that breaks old usermode
    driver.
    
    Fixes: 50a7c8765ca6 ("drm/amdgpu: enable mcbp by default on gfx9")
    Signed-off-by: Jiadong Zhu <Jiadong.Zhu@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: stable@vger.kernel.org

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -3791,10 +3791,6 @@ static void amdgpu_device_set_mcbp(struct amdgpu_device *adev)
 		adev->gfx.mcbp = true;
 	else if (amdgpu_mcbp == 0)
 		adev->gfx.mcbp = false;
-	else if ((amdgpu_ip_version(adev, GC_HWIP, 0) >= IP_VERSION(9, 0, 0)) &&
-		 (amdgpu_ip_version(adev, GC_HWIP, 0) < IP_VERSION(10, 0, 0)) &&
-		 adev->gfx.num_gfx_rings)
-		adev->gfx.mcbp = true;
 
 	if (amdgpu_sriov_vf(adev))
 		adev->gfx.mcbp = true;
