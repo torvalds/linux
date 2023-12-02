aca90eea8a90b1abc844504109b6f919dfbd4259
Jaegeuk Kim <jaegeuk@kernel.org>
2023-12-02 11:15:41 -0800

    f2fs: check write pointers when checkpoint=disable
    Even if f2fs was rebooted as staying checkpoint=disable, let's match the write
    pointers all the time.
    
    Reviewed-by: Daeho Jeong <daehojeong@google.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -4741,7 +4741,7 @@ try_onemore:
 #ifdef CONFIG_QUOTA
 	f2fs_recover_quota_end(sbi, quota_enabled);
 #endif
-
+reset_checkpoint:
 	/*
 	 * If the f2fs is not readonly and fsync data recovery succeeds,
 	 * check zoned block devices' write pointer consistency.
@@ -4752,7 +4752,6 @@ try_onemore:
 			goto free_meta;
 	}
 
-reset_checkpoint:
 	f2fs_init_inmem_curseg(sbi);
 
 	/* f2fs_recover_fsync_data() cleared this already */
