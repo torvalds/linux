49e27d3c9cd67fd5851f8b5518645b9bf3d2c6c0
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2023-12-03 03:05:29 +0300

    drm/msm/dpu: finalise global state object
    Add calls to finalise global state object and corresponding lock.
    
    Fixes: de3916c70a24 ("drm/msm/dpu: Track resources in global state")
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Reviewed-by: Abhinav Kumar <quic_abhinavk@quicinc.com>
    Patchwork: https://patchwork.freedesktop.org/patch/570175/
    Link: https://lore.kernel.org/r/20231203000532.1290480-3-dmitry.baryshkov@linaro.org

--- drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
@@ -374,6 +374,12 @@ static int dpu_kms_global_obj_init(struct dpu_kms *dpu_kms)
 	return 0;
 }
 
+static void dpu_kms_global_obj_fini(struct dpu_kms *dpu_kms)
+{
+	drm_atomic_private_obj_fini(&dpu_kms->global_state);
+	drm_modeset_lock_fini(&dpu_kms->global_state_lock);
+}
+
 static int dpu_kms_parse_data_bus_icc_path(struct dpu_kms *dpu_kms)
 {
 	struct icc_path *path0;
@@ -803,6 +809,8 @@ static void _dpu_kms_hw_destroy(struct dpu_kms *dpu_kms)
 	for (i = 0; i < ARRAY_SIZE(dpu_kms->hw_vbif); i++) {
 		dpu_kms->hw_vbif[i] = NULL;
 	}
+
+	dpu_kms_global_obj_fini(dpu_kms);
 
 	dpu_kms->catalog = NULL;
 
