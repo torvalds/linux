2a04739139b2b2761571e18937e2400e71eff664
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2023-12-03 14:43:28 +0300

    drm/bridge: add transparent bridge helper
    Define a helper for creating simple transparent bridges which serve the
    only purpose of linking devices into the bridge chain up to the last
    bridge representing the connector. This is especially useful for
    DP/USB-C bridge chains, which can span across several devices, but do
    not require any additional functionality from the intermediate bridges.
    
    Reviewed-by: Neil Armstrong <neil.armstrong@linaro.org>
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20231203114333.1305826-2-dmitry.baryshkov@linaro.org

---
+++ include/drm/bridge/aux-bridge.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2023 Linaro Ltd.
+ *
+ * Author: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
+ */
+#ifndef DRM_AUX_BRIDGE_H
+#define DRM_AUX_BRIDGE_H
+
+#if IS_ENABLED(CONFIG_DRM_AUX_BRIDGE)
+int drm_aux_bridge_register(struct device *parent);
+#else
+static inline int drm_aux_bridge_register(struct device *parent)
+{
+	return 0;
+}
+#endif
+
+#endif
