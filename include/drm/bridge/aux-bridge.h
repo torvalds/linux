e560518a6c2e60f1566473c146fddcff3281f617
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2023-12-03 14:43:31 +0300

    drm/bridge: implement generic DP HPD bridge
    Several USB-C controllers implement a pretty simple DRM bridge which
    implements just the HPD notification operations. Add special helper
    for creating such simple bridges.
    
    Acked-by: Neil Armstrong <neil.armstrong@linaro.org>
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20231203114333.1305826-5-dmitry.baryshkov@linaro.org

--- include/drm/bridge/aux-bridge.h
+++ include/drm/bridge/aux-bridge.h
@@ -7,12 +7,30 @@
 #ifndef DRM_AUX_BRIDGE_H
 #define DRM_AUX_BRIDGE_H
 
+#include <drm/drm_connector.h>
+
 #if IS_ENABLED(CONFIG_DRM_AUX_BRIDGE)
 int drm_aux_bridge_register(struct device *parent);
 #else
 static inline int drm_aux_bridge_register(struct device *parent)
 {
 	return 0;
+}
+#endif
+
+#if IS_ENABLED(CONFIG_DRM_AUX_HPD_BRIDGE)
+struct device *drm_dp_hpd_bridge_register(struct device *parent,
+					  struct device_node *np);
+void drm_aux_hpd_bridge_notify(struct device *dev, enum drm_connector_status status);
+#else
+static inline struct device *drm_dp_hpd_bridge_register(struct device *parent,
+							struct device_node *np)
+{
+	return 0;
+}
+
+static inline void drm_aux_hpd_bridge_notify(struct device *dev, enum drm_connector_status status)
+{
 }
 #endif
 
