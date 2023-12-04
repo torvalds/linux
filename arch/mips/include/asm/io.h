235a59c373b15e84df1a254c96066fc5fc47ae41
Arnd Bergmann <arnd@arndb.de>
2023-12-04 12:57:06 +0100

    mips: remove extraneous asm-generic/iomap.h include
    When this file is included before defining readq(), it misses the
    declarations for a couple of functions that now become unusable:
    
    lib/iomap.c:156:5: warning: no previous prototype for 'ioread64_lo_hi' [-Wmissing-prototypes]
    lib/iomap.c:163:5: warning: no previous prototype for 'ioread64_hi_lo' [-Wmissing-prototypes]
    lib/iomap.c:170:5: warning: no previous prototype for 'ioread64be_lo_hi' [-Wmissing-prototypes]
    lib/iomap.c:178:5: warning: no previous prototype for 'ioread64be_hi_lo' [-Wmissing-prototypes]
    lib/iomap.c:264:6: warning: no previous prototype for 'iowrite64_lo_hi' [-Wmissing-prototypes]
    lib/iomap.c:272:6: warning: no previous prototype for 'iowrite64_hi_lo' [-Wmissing-prototypes]
    lib/iomap.c:280:6: warning: no previous prototype for 'iowrite64be_lo_hi' [-Wmissing-prototypes]
    lib/iomap.c:288:6: warning: no previous prototype for 'iowrite64be_hi_lo' [-Wmissing-prototypes]
    
    The file is included again later from asm-generic/io.h, so dropping the initial
    include statement makes it do the right thing, both for avoiding the warning
    and for actually providing these functions.
    
    Link: https://lkml.kernel.org/r/20231204115710.2247097-17-arnd@kernel.org
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Stephen Rothwell <sfr@rothwell.id.au>
    Cc: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/include/asm/io.h
+++ arch/mips/include/asm/io.h
@@ -187,8 +187,6 @@ void iounmap(const volatile void __iomem *addr);
 #define ioremap_wc(offset, size)					\
 	ioremap_prot((offset), (size), boot_cpu_data.writecombine)
 
-#include <asm-generic/iomap.h>
-
 #if defined(CONFIG_CPU_CAVIUM_OCTEON)
 #define war_io_reorder_wmb()		wmb()
 #else
