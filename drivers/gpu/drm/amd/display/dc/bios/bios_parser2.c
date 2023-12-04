669080888691c312cc926322a7b24600121c90fb
Alvin Lee <alvin.lee2@amd.com>
2023-12-04 15:19:34 -0500

    drm/amd/display: Revert " drm/amd/display: Use channel_width = 2 for vram table 3.0"
    [Description]
    Revert commit fec05adc40c2 ("drm/amd/display: Use channel_width = 2 for vram table 3.0")
    Because the issue is being fixed from VBIOS side.
    
    Reviewed-by: Samson Tam <samson.tam@amd.com>
    Acked-by: Wayne Lin <wayne.lin@amd.com>
    Signed-off-by: Alvin Lee <alvin.lee2@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/bios/bios_parser2.c
+++ drivers/gpu/drm/amd/display/dc/bios/bios_parser2.c
@@ -2386,13 +2386,7 @@ static enum bp_result get_vram_info_v30(
 		return BP_RESULT_BADBIOSTABLE;
 
 	info->num_chans = info_v30->channel_num;
-	/* As suggested by VBIOS we should always use
-	 * dram_channel_width_bytes = 2 when using VRAM
-	 * table version 3.0. This is because the channel_width
-	 * param in the VRAM info table is changed in 7000 series and
-	 * no longer represents the memory channel width.
-	 */
-	info->dram_channel_width_bytes = 2;
+	info->dram_channel_width_bytes = (1 << info_v30->channel_width) / 8;
 
 	return result;
 }
