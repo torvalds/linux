d7271ba40155d5e5c28e13516df5d9c69f40ef20
Heiko Carstens <hca@linux.ibm.com>
2023-12-04 15:46:41 +0100

    KVM: s390: remove superfluous save_fpu_regs() call
    The save_fpu_regs() call in kvm_arch_vcpu_ioctl_get_fpu() is pointless: it
    will save the current user space fpu context to the thread's save area. But
    the code is accessing only the vcpu's save are / mapped register area,
    which in this case are not the same.
    
    Therefore remove the confusing call.
    
    Reviewed-by: Claudio Imbrenda <imbrenda@linux.ibm.com>
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>
    Signed-off-by: Alexander Gordeev <agordeev@linux.ibm.com>

--- arch/s390/kvm/kvm-s390.c
+++ arch/s390/kvm/kvm-s390.c
@@ -4331,8 +4331,6 @@ int kvm_arch_vcpu_ioctl_get_fpu(struct kvm_vcpu *vcpu, struct kvm_fpu *fpu)
 {
 	vcpu_load(vcpu);
 
-	/* make sure we have the latest values */
-	save_fpu_regs();
 	if (MACHINE_HAS_VX)
 		convert_vx_to_fp((freg_t *) fpu->fprs,
 				 (__vector128 *) vcpu->run->s.regs.vrs);
