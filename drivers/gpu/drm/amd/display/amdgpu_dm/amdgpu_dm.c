9cdef4f720376ef0fb0febce1ed2377c19e531f9
Wayne Lin <wayne.lin@amd.com>
2023-12-04 10:09:33 +0800

    drm/amd/display: pbn_div need be updated for hotplug event
    link_rate sometime will be changed when DP MST connector hotplug, so
    pbn_div also need be updated; otherwise, it will mismatch with
    link_rate, causes no output in external monitor.
    
    This is a backport to 6.7 and older.
    
    Cc: stable@vger.kernel.org
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Reviewed-by: Jerry Zuo <jerry.zuo@amd.com>
    Acked-by: Rodrigo Siqueira <rodrigo.siqueira@amd.com>
    Signed-off-by: Wade Wang <wade.wang@hp.com>
    Signed-off-by: Wayne Lin <wayne.lin@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -6914,8 +6914,7 @@ static int dm_encoder_helper_atomic_check(struct drm_encoder *encoder,
 	if (IS_ERR(mst_state))
 		return PTR_ERR(mst_state);
 
-	if (!mst_state->pbn_div)
-		mst_state->pbn_div = dm_mst_get_pbn_divider(aconnector->mst_root->dc_link);
+	mst_state->pbn_div = dm_mst_get_pbn_divider(aconnector->mst_root->dc_link);
 
 	if (!state->duplicated) {
 		int max_bpc = conn_state->max_requested_bpc;
