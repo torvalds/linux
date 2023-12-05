4618d0a66b505a81cc39b17935118227a7fc24f8
Qu Wenruo <wqu@suse.com>
2023-12-05 18:21:29 +1030

    btrfs: fix mismatching parameter names for btrfs_get_extent()
    The definition for btrfs_get_extent() is using "u64 end" as the last
    parameter, but in implementation we go "u64 len", and all call sites
    follows the implementation.
    
    This can be very confusing during development, as most developers
    including me, would just use the snippet returned by LSP (clangd in my
    case), which would only check the definition.
    
    Unfortunately this mismatch is introduced from the very beginning of
    btrfs.
    
    Fix it to prevent further confusion.
    
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/btrfs_inode.h
+++ fs/btrfs/btrfs_inode.h
@@ -491,7 +491,7 @@ struct inode *btrfs_iget_path(struct super_block *s, u64 ino,
 struct inode *btrfs_iget(struct super_block *s, u64 ino, struct btrfs_root *root);
 struct extent_map *btrfs_get_extent(struct btrfs_inode *inode,
 				    struct page *page, size_t pg_offset,
-				    u64 start, u64 end);
+				    u64 start, u64 len);
 int btrfs_update_inode(struct btrfs_trans_handle *trans,
 		       struct btrfs_inode *inode);
 int btrfs_update_inode_fallback(struct btrfs_trans_handle *trans,
