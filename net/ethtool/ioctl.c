2a48c635fd9a48699805bbfeee1e4b94b8fe819d
justinstitt@google.com <justinstitt@google.com>
2023-12-06 23:16:10 +0000

    ethtool: Implement ethtool_puts()
    Use strscpy() to implement ethtool_puts().
    
    Functionally the same as ethtool_sprintf() when it's used with two
    arguments or with just "%s" format specifier.
    
    Signed-off-by: Justin Stitt <justinstitt@google.com>
    Reviewed-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Reviewed-by: Madhuri Sripada <madhuri.sripada@microchip.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ethtool/ioctl.c
+++ net/ethtool/ioctl.c
@@ -1994,6 +1994,13 @@ __printf(2, 3) void ethtool_sprintf(u8 **data, const char *fmt, ...)
 }
 EXPORT_SYMBOL(ethtool_sprintf);
 
+void ethtool_puts(u8 **data, const char *str)
+{
+	strscpy(*data, str, ETH_GSTRING_LEN);
+	*data += ETH_GSTRING_LEN;
+}
+EXPORT_SYMBOL(ethtool_puts);
+
 static int ethtool_phys_id(struct net_device *dev, void __user *useraddr)
 {
 	struct ethtool_value id;
