9b7f18042d4c12ecc5fc797c0a31b0ff0bcef3cc
Jesse Brandeburg <jesse.brandeburg@intel.com>
2023-12-05 17:01:04 -0800

    iavf: field prep conversion
    Refactor iavf driver to use FIELD_PREP(), which reduces lines of code
    and adds clarity of intent.
    
    This code was generated by the following coccinelle/spatch script and
    then manually repaired.
    
    Clean up a couple spots in the code that had repetitive
    y = cpu_to_*((blah << blah_blah) & blat)
    y |= cpu_to_*((blahs << blahs_blahs) & blats)
    to
    x = FIELD_PREP(blat blah)
    x |= FIELD_PREP(blats, blahs)
    y = cpu_to_*(x);
    
    @prep2@
    constant shift,mask;
    type T;
    expression a;
    @@
    -(((T)(a) << shift) & mask)
    +FIELD_PREP(mask, a)
    
    @prep@
    constant shift,mask;
    type T;
    expression a;
    @@
    -((T)((a) << shift) & mask)
    +FIELD_PREP(mask, a)
    
    Cc: Julia Lawall <Julia.Lawall@inria.fr>
    Cc: Ahmed Zaki <ahmed.zaki@intel.com>
    Reviewed-by: Marcin Szycik <marcin.szycik@linux.intel.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: Jesse Brandeburg <jesse.brandeburg@intel.com>
    Tested-by: Rafal Romanowski <rafal.romanowski@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>

--- drivers/net/ethernet/intel/iavf/iavf_fdir.c
+++ drivers/net/ethernet/intel/iavf/iavf_fdir.c
@@ -358,7 +358,7 @@ iavf_fill_fdir_ip6_hdr(struct iavf_fdir_fltr *fltr,
 
 	if (fltr->ip_mask.tclass == U8_MAX) {
 		iph->priority = (fltr->ip_data.tclass >> 4) & 0xF;
-		iph->flow_lbl[0] = (fltr->ip_data.tclass << 4) & 0xF0;
+		iph->flow_lbl[0] = FIELD_PREP(0xF0, fltr->ip_data.tclass);
 		VIRTCHNL_ADD_PROTO_HDR_FIELD_BIT(hdr, IPV6, TC);
 	}
 
