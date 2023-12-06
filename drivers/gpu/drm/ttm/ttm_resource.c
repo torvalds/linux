cc941c70df3927be89dfb875a9b3cec3ef5cf2c8
Christian König <christian.koenig@amd.com>
2023-12-06 16:37:29 +0100

    drm/ttm: improve idle/busy handling v5
    Previously we would never try to move a BO into the preferred placements
    when it ever landed in a busy placement since those were considered
    compatible.
    
    Rework the whole handling and finally unify the idle and busy handling.
    ttm_bo_validate() is now responsible to try idle placement first and then
    use the busy placement if that didn't worked.
    
    Drawback is that we now always try the idle placement first for each
    validation which might cause some additional CPU overhead on overcommit.
    
    v2: fix kerneldoc warning and coding style
    v3: take care of XE as well
    v4: keep the ttm_bo_mem_space functionality as it is for now, only add
        new handling for ttm_bo_validate as suggested by Thomas
    v5: fix bug pointed out by Matthew
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Zack Rusin <zack.rusin@broadcom.com> v3
    Link: https://patchwork.freedesktop.org/patch/msgid/20240229134003.3688-1-christian.koenig@amd.com
    Reviewed-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>

--- drivers/gpu/drm/ttm/ttm_resource.c
+++ drivers/gpu/drm/ttm/ttm_resource.c
@@ -295,11 +295,13 @@ bool ttm_resource_intersects(struct ttm_device *bdev,
  *
  * @res: the resource to check
  * @placement: the placement to check against
+ * @evicting: true if the caller is doing evictions
  *
  * Returns true if the placement is compatible.
  */
 bool ttm_resource_compatible(struct ttm_resource *res,
-			     struct ttm_placement *placement)
+			     struct ttm_placement *placement,
+			     bool evicting)
 {
 	struct ttm_buffer_object *bo = res->bo;
 	struct ttm_device *bdev = bo->bdev;
@@ -315,14 +317,20 @@ bool ttm_resource_compatible(struct ttm_resource *res,
 		if (res->mem_type != place->mem_type)
 			continue;
 
+		if (place->flags & (evicting ? TTM_PL_FLAG_DESIRED :
+				    TTM_PL_FLAG_FALLBACK))
+			continue;
+
+		if (place->flags & TTM_PL_FLAG_CONTIGUOUS &&
+		    !(res->placement & TTM_PL_FLAG_CONTIGUOUS))
+			continue;
+
 		man = ttm_manager_type(bdev, res->mem_type);
 		if (man->func->compatible &&
 		    !man->func->compatible(man, res, place, bo->base.size))
 			continue;
 
-		if ((!(place->flags & TTM_PL_FLAG_CONTIGUOUS) ||
-		     (res->placement & TTM_PL_FLAG_CONTIGUOUS)))
-			return true;
+		return true;
 	}
 	return false;
 }
--- include/drm/ttm/ttm_resource.h
+++ include/drm/ttm/ttm_resource.h
@@ -366,7 +366,8 @@ bool ttm_resource_intersects(struct ttm_device *bdev,
 			     const struct ttm_place *place,
 			     size_t size);
 bool ttm_resource_compatible(struct ttm_resource *res,
-			     struct ttm_placement *placement);
+			     struct ttm_placement *placement,
+			     bool evicting);
 void ttm_resource_set_bo(struct ttm_resource *res,
 			 struct ttm_buffer_object *bo);
 
