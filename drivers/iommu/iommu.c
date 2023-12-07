64945d1b0ed169aeffa59020941e4ac45ebc315a
Jason Gunthorpe <jgg@nvidia.com>
2023-12-07 14:03:11 -0400

    iommu: Mark dev_iommu_get() with lockdep
    Allocation of dev->iommu must be done under the
    iommu_probe_device_lock. Mark this with lockdep to discourage future
    mistakes.
    
    Reviewed-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Tested-by: Hector Martin <marcan@marcan.st>
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Moritz Fischer <moritzf@google.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/4-v2-16e4def25ebb+820-iommu_fwspec_p1_jgg@nvidia.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -334,6 +334,8 @@ static struct dev_iommu *dev_iommu_get(struct device *dev)
 {
 	struct dev_iommu *param = dev->iommu;
 
+	lockdep_assert_held(&iommu_probe_device_lock);
+
 	if (param)
 		return param;
 
