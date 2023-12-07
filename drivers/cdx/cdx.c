aaee477e3e2c7305a95ffc528bf831a13da3dacb
Rob Herring <robh@kernel.org>
2023-12-07 10:31:27 -0600

    cdx: Explicitly include correct DT includes, again
    The DT of_device.h and of_platform.h date back to the separate
    of_platform_bus_type before it was merged into the regular platform bus.
    As part of that merge prepping Arm DT support 13 years ago, they
    "temporarily" include each other. They also include platform_device.h
    and of.h. As a result, there's a pretty much random mix of those include
    files used throughout the tree. In order to detangle these headers and
    replace the implicit includes with struct declarations, users need to
    explicitly include the correct includes.
    
    CDX was fixed once, but commit ("cdx: Remove cdx controller list from cdx
    bus system") added another occurrence.
    
    Fixes: 54b406e10f03 ("cdx: Remove cdx controller list from cdx bus system")
    Signed-off-by: Rob Herring <robh@kernel.org>
    Acked-by: Nikhil Agarwal <Nikhil.agarwal@amd.com>
    Link: https://lore.kernel.org/r/20231207163128.2707993-2-robh@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/cdx/cdx.c
+++ drivers/cdx/cdx.c
@@ -57,7 +57,10 @@
 
 #include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/of.h>
 #include <linux/of_device.h>
+#include <linux/of_platform.h>
+#include <linux/platform_device.h>
 #include <linux/slab.h>
 #include <linux/mm.h>
 #include <linux/idr.h>
