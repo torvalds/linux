ea6b597fcaca99562fa56a473bcbbbd79b40af03
Ard Biesheuvel <ardb@kernel.org>
2023-12-08 17:39:28 +0100

    efivarfs: Free s_fs_info on unmount
    [ Upstream commit 547713d502f7b4b8efccd409cff84d731a23853b ]
    
    Now that we allocate a s_fs_info struct on fs context creation, we
    should ensure that we free it again when the superblock goes away.
    
    Fixes: 5329aa5101f7 ("efivarfs: Add uid/gid mount options")
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/efivarfs/super.c
+++ fs/efivarfs/super.c
@@ -250,10 +250,13 @@ static int efivarfs_init_fs_context(struct fs_context *fc)
 
 static void efivarfs_kill_sb(struct super_block *sb)
 {
+	struct efivarfs_fs_info *sfi = sb->s_fs_info;
+
 	kill_litter_super(sb);
 
 	/* Remove all entries and destroy */
 	efivar_entry_iter(efivarfs_destroy, &efivarfs_list, NULL);
+	kfree(sfi);
 }
 
 static struct file_system_type efivarfs_type = {
