576f3aef47f42f368db08fd5e3f49880c4493bf5
Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
2023-12-09 22:32:23 +0200

    ASoC: amd: acp: Add missing MODULE_DESCRIPTION in mach-common
    Add a MODULE_DESCRIPTION() in the generic ACP machine driver to avoid
    the following warning when building with W=1:
    
    WARNING: modpost: missing MODULE_DESCRIPTION() in sound/soc/amd/acp/snd-acp-mach.o
    
    Fixes: d4c750f2c7d4 ("ASoC: amd: acp: Add generic machine driver support for ACP cards")
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
    Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
    Link: https://msgid.link/r/20231209203229.878730-6-cristian.ciocaltea@collabora.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/acp/acp-mach-common.c
+++ sound/soc/amd/acp/acp-mach-common.c
@@ -1773,4 +1773,5 @@ int acp_legacy_dai_links_create(struct snd_soc_card *card)
 }
 EXPORT_SYMBOL_NS_GPL(acp_legacy_dai_links_create, SND_SOC_AMD_MACH);
 
+MODULE_DESCRIPTION("AMD ACP Common Machine driver");
 MODULE_LICENSE("GPL v2");
