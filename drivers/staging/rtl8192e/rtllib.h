0c5e85dd078a2e57ff93a6607071f049ca919b58
Tree Davies <tdavies@darkphysics.net>
2023-12-10 11:37:04 -0800

    Staging: rtl8192e: Remove variable bEncrypt
    Remove variable bEncrypt as it is defined but never used.
    
    Signed-off-by: Tree Davies <tdavies@darkphysics.net>
    Link: https://lore.kernel.org/r/20231210193705.2131807-4-tdavies@darkphysics.net
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtllib.h
+++ drivers/staging/rtl8192e/rtllib.h
@@ -103,7 +103,6 @@ struct cb_desc {
 	/* Tx Desc Related flags (8-9) */
 	u8 bLastIniPkt:1;
 	u8 bCmdOrInit:1;
-	u8 bEncrypt:1;
 	u8 tx_dis_rate_fallback:1;
 	u8 tx_use_drv_assinged_rate:1;
 	u8 bHwSec:1;
