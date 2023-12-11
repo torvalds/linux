2ca8718be0c469a99435f6330904364f8dc5a094
Miquel Raynal <miquel.raynal@bootlin.com>
2023-12-11 16:07:03 +0100

    mtd: rawnand: rockchip: Rename a structure
    Robots are unhappy with the ecc_cnt_status structure because the kernel
    doc says it should be called rk_ecc_cnt_status. In general, it is
    considered a better practice to prefix all symbols in a file with the
    same prexif, and thus it seems more relevant to rename the structure
    rather than changing the kernel doc header.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202312102130.geZ4dqyN-lkp@intel.com/
    Fixes: 058e0e847d54 ("mtd: rawnand: rockchip: NFC driver for RK3308, RK2928 and others")
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Link: https://lore.kernel.org/linux-mtd/20231211150704.109138-1-miquel.raynal@bootlin.com

--- drivers/mtd/nand/raw/rockchip-nand-controller.c
+++ drivers/mtd/nand/raw/rockchip-nand-controller.c
@@ -98,7 +98,7 @@ enum nfc_type {
  * @high: ECC count high bit index at register.
  * @high_mask: mask bit
  */
-struct ecc_cnt_status {
+struct rk_ecc_cnt_status {
 	u8 err_flag_bit;
 	u8 low;
 	u8 low_mask;
@@ -144,8 +144,8 @@ struct nfc_cfg {
 	u32 int_st_off;
 	u32 oob0_off;
 	u32 oob1_off;
-	struct ecc_cnt_status ecc0;
-	struct ecc_cnt_status ecc1;
+	struct rk_ecc_cnt_status ecc0;
+	struct rk_ecc_cnt_status ecc1;
 };
 
 struct rk_nfc_nand_chip {
