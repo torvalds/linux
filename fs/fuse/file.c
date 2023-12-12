9511176bbaee0ac60ecc84e7b01cf5972a59ea17
Bernd Schubert <bschubert@ddn.com>
2023-12-12 14:33:23 +0100

    fuse: fix VM_MAYSHARE and direct_io_allow_mmap
    There were multiple issues with direct_io_allow_mmap:
    
     - fuse_link_write_file() was missing, resulting in warnings in
       fuse_write_file_get() and EIO from msync()
    
     - "vma->vm_ops = &fuse_file_vm_ops" was not set, but especially
       fuse_page_mkwrite is needed.
    
    The semantics of invalidate_inode_pages2() is so far not clearly defined in
    fuse_file_mmap.  It dates back to commit 3121bfe76311 ("fuse: fix
    "direct_io" private mmap") Though, as direct_io_allow_mmap is a new
    feature, that was for MAP_PRIVATE only.  As invalidate_inode_pages2() is
    calling into fuse_launder_folio() and writes out dirty pages, it should be
    safe to call invalidate_inode_pages2 for MAP_PRIVATE and MAP_SHARED as
    well.
    
    Cc: Hao Xu <howeyxu@tencent.com>
    Cc: stable@vger.kernel.org
    Fixes: e78662e818f9 ("fuse: add a new fuse init flag to relax restrictions in no cache mode")
    Signed-off-by: Bernd Schubert <bschubert@ddn.com>
    Reviewed-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -2468,7 +2468,8 @@ static int fuse_file_mmap(struct file *file, struct vm_area_struct *vma)
 		return fuse_dax_mmap(file, vma);
 
 	if (ff->open_flags & FOPEN_DIRECT_IO) {
-		/* Can't provide the coherency needed for MAP_SHARED
+		/*
+		 * Can't provide the coherency needed for MAP_SHARED
 		 * if FUSE_DIRECT_IO_ALLOW_MMAP isn't set.
 		 */
 		if ((vma->vm_flags & VM_MAYSHARE) && !fc->direct_io_allow_mmap)
@@ -2476,7 +2477,10 @@ static int fuse_file_mmap(struct file *file, struct vm_area_struct *vma)
 
 		invalidate_inode_pages2(file->f_mapping);
 
-		return generic_file_mmap(file, vma);
+		if (!(vma->vm_flags & VM_MAYSHARE)) {
+			/* MAP_PRIVATE */
+			return generic_file_mmap(file, vma);
+		}
 	}
 
 	if ((vma->vm_flags & VM_SHARED) && (vma->vm_flags & VM_MAYWRITE))
