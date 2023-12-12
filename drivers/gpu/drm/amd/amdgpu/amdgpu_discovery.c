f1c40b6ea40b765dadfea5d27fb02aa4b10c14c6
Yifan Zhang <yifan1.zhang@amd.com>
2023-12-12 17:11:04 +0800

    drm/amdgpu: enable soc21 discovery support for GC 11.5.1
    This patch to enable soc21 support for GC 11.5.1
    
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
@@ -1683,6 +1683,7 @@ static int amdgpu_discovery_set_common_ip_blocks(struct amdgpu_device *adev)
 	case IP_VERSION(11, 0, 3):
 	case IP_VERSION(11, 0, 4):
 	case IP_VERSION(11, 5, 0):
+	case IP_VERSION(11, 5, 1):
 		amdgpu_device_ip_block_add(adev, &soc21_common_ip_block);
 		break;
 	default:
