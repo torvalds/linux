d6a76c0a5a75b519ce81cd472077f9e76db5d6c3
Yifan Zhang <yifan1.zhang@amd.com>
2023-12-12 17:18:18 +0800

    drm/amdgpu: enable MES discovery for GC 11.5.1
    This patch to enable MES for GC 11.5.1
    
    Reviewed-by: shaoyun.liu <Shaoyun.liu@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
@@ -2185,6 +2185,7 @@ static int amdgpu_discovery_set_mes_ip_blocks(struct amdgpu_device *adev)
 	case IP_VERSION(11, 0, 3):
 	case IP_VERSION(11, 0, 4):
 	case IP_VERSION(11, 5, 0):
+	case IP_VERSION(11, 5, 1):
 		amdgpu_device_ip_block_add(adev, &mes_v11_0_ip_block);
 		adev->enable_mes = true;
 		adev->enable_mes_kiq = true;
