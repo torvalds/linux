031e011d8b22346a6513c7879cbecd7105b4c11d
Ard Biesheuvel <ardb@kernel.org>
2023-12-13 09:40:26 +0100

    arm64: mm: Move PCI I/O emulation region above the vmemmap region
    Move the PCI I/O region above the vmemmap region in the kernel's VA
    space. This will permit us to reclaim the lower part of the vmemmap
    region for vmalloc/vmap allocations when running a 52-bit VA capable
    build on a 48-bit VA capable system.
    
    Also, given that PCI_IO_START is derived from VMEMMAP_END, use that
    symbolic constant directly in ptdump rather than deriving it from
    VMEMMAP_START and VMEMMAP_SIZE, as those definitions will change in
    subsequent patches.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Link: https://lore.kernel.org/r/20231213084024.2367360-10-ardb@google.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Mark Rutland <mark.rutland@arm.com>

--- arch/arm64/mm/ptdump.c
+++ arch/arm64/mm/ptdump.c
@@ -47,10 +47,10 @@ static struct addr_marker address_markers[] = {
 	{ VMALLOC_END,			"vmalloc() end" },
 	{ FIXADDR_TOT_START,		"Fixmap start" },
 	{ FIXADDR_TOP,			"Fixmap end" },
+	{ VMEMMAP_START,		"vmemmap start" },
+	{ VMEMMAP_END,			"vmemmap end" },
 	{ PCI_IO_START,			"PCI I/O start" },
 	{ PCI_IO_END,			"PCI I/O end" },
-	{ VMEMMAP_START,		"vmemmap start" },
-	{ VMEMMAP_START + VMEMMAP_SIZE,	"vmemmap end" },
 	{ -1,				NULL },
 };
 
