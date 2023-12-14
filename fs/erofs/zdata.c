a18efa4e4aa95a7348469db998411322ac8193be
Gao Xiang <hsiangkao@linux.alibaba.com>
2023-12-15 00:13:37 +0800

    FROMGIT: erofs: fix ztailpacking for subpage compressed blocks
    `pageofs_in` should be the compressed data offset of the page rather
    than of the block.
    
    Acked-by: Chao Yu <chao@kernel.org>
    Reviewed-by: Yue Hu <huyue2@coolpad.com>
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Link: https://lore.kernel.org/r/20231214161337.753049-1-hsiangkao@linux.alibaba.com
    
    Bug: 318378021
    Change-Id: I0997a69b22b0f42c327c810359f55f5fa6a76275
    (cherry picked from commit e5aba911dee5e20fa82efbe13e0af8f38ea459e7
     https://git.kernel.org/pub/scm/linux/kernel/git/xiang/erofs.git dev)
    Signed-off-by: Sandeep Dhavale <dhavale@google.com>

--- fs/erofs/zdata.c
+++ fs/erofs/zdata.c
@@ -820,7 +820,6 @@ static int z_erofs_register_pcluster(struct z_erofs_decompress_frontend *fe)
 
 	if (ztailpacking) {
 		pcl->obj.index = 0;	/* which indicates ztailpacking */
-		pcl->pageofs_in = erofs_blkoff(fe->inode->i_sb, map->m_pa);
 	} else {
 		pcl->obj.index = erofs_blknr(sb, map->m_pa);
 
@@ -897,6 +896,7 @@ static int z_erofs_pcluster_begin(struct z_erofs_decompress_frontend *fe)
 		}
 		get_page(map->buf.page);
 		WRITE_ONCE(fe->pcl->compressed_bvecs[0].page, map->buf.page);
+		fe->pcl->pageofs_in = map->m_pa & ~PAGE_MASK;
 		fe->mode = Z_EROFS_PCLUSTER_FOLLOWED_NOINPLACE;
 	}
 	/* file-backed inplace I/O pages are traversed in reverse order */
