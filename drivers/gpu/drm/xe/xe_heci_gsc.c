48e70d2a1a9c8d58c48b2840feda3aa3bc330a94
Matt Roper <matthew.d.roper@intel.com>
2023-12-14 10:47:05 -0800

    drm/xe: Move GSC HECI base offsets out of register header
    These offsets are only used to setup the auxiliary device BAR
    information and are never used for driver read/write operations.  Move
    them to the GSC HECI file where they're actually used.
    
    Reviewed-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Link: https://lore.kernel.org/r/20231214184659.2249559-15-matthew.d.roper@intel.com
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/xe/xe_heci_gsc.c
+++ drivers/gpu/drm/xe/xe_heci_gsc.c
@@ -16,6 +16,10 @@
 
 #define GSC_BAR_LENGTH  0x00000FFC
 
+#define DG1_GSC_HECI2_BASE			0x259000
+#define PVC_GSC_HECI2_BASE			0x285000
+#define DG2_GSC_HECI2_BASE			0x374000
+
 static void heci_gsc_irq_mask(struct irq_data *d)
 {
 	/* generic irq handling */
