56925f389e152dcb8d093435d43b78a310539c23
Yonghong Song <yonghong.song@linux.dev>
2023-12-14 12:38:20 -0800

    selftests/bpf: Remove flaky test_btf_id test
    With previous patch, one of subtests in test_btf_id becomes
    flaky and may fail. The following is a failing example:
    
      Error: #26 btf
      Error: #26/174 btf/BTF ID
        Error: #26/174 btf/BTF ID
        btf_raw_create:PASS:check 0 nsec
        btf_raw_create:PASS:check 0 nsec
        test_btf_id:PASS:check 0 nsec
        ...
        test_btf_id:PASS:check 0 nsec
        test_btf_id:FAIL:check BTF lingersdo_test_get_info:FAIL:check failed: -1
    
    The test tries to prove a btf_id not available after the map is closed.
    But btf_id is freed only after workqueue and a rcu grace period, compared
    to previous case just after a rcu grade period.
    Depending on system workload, workqueue could take quite some time
    to execute function bpf_map_free_deferred() which may cause the test failure.
    Instead of adding arbitrary delays, let us remove the logic to
    check btf_id availability after map is closed.
    
    Signed-off-by: Yonghong Song <yonghong.song@linux.dev>
    Link: https://lore.kernel.org/r/20231214203820.1469402-1-yonghong.song@linux.dev
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/testing/selftests/bpf/prog_tests/btf.c
+++ tools/testing/selftests/bpf/prog_tests/btf.c
@@ -4630,11 +4630,6 @@ static int test_btf_id(unsigned int test_num)
 	/* The map holds the last ref to BTF and its btf_id */
 	close(map_fd);
 	map_fd = -1;
-	btf_fd[0] = bpf_btf_get_fd_by_id(map_info.btf_id);
-	if (CHECK(btf_fd[0] >= 0, "BTF lingers")) {
-		err = -1;
-		goto done;
-	}
 
 	fprintf(stderr, "OK");
 
