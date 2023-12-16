b8a5d1f4a08751031a18621ce1b59fc49d042494
Simon Ser <contact@emersion.fr>
2023-12-16 14:15:27 +0000

    drm/vc4: plane: check drm_gem_plane_helper_prepare_fb() return value
    Bubble up any error to the caller.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>
    Cc: Maxime Ripard <mripard@kernel.org>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Dave Stevenson <dave.stevenson@raspberrypi.com>
    Reviewed-by: Maíra Canal <mcanal@igalia.com>
    Signed-off-by: Maxime Ripard <mripard@kernel.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20231216141518.242811-1-contact@emersion.fr

--- drivers/gpu/drm/vc4/vc4_plane.c
+++ drivers/gpu/drm/vc4/vc4_plane.c
@@ -1497,13 +1497,16 @@ static int vc4_prepare_fb(struct drm_plane *plane,
 			  struct drm_plane_state *state)
 {
 	struct vc4_bo *bo;
+	int ret;
 
 	if (!state->fb)
 		return 0;
 
 	bo = to_vc4_bo(&drm_fb_dma_get_gem_obj(state->fb, 0)->base);
 
-	drm_gem_plane_helper_prepare_fb(plane, state);
+	ret = drm_gem_plane_helper_prepare_fb(plane, state);
+	if (ret)
+		return ret;
 
 	if (plane->state->fb == state->fb)
 		return 0;
