144377c340f292c5d8c039eb790318754eec557e
Jonathan Corbet <corbet@lwn.net>
2023-12-19 16:51:12 -0700

    net: sock: remove excess structure-member documentation
    Remove a couple of kerneldoc entries for struct members that do not exist,
    addressing these warnings:
    
      ./include/net/sock.h:548: warning: Excess struct member '__sk_flags_offset' description in 'sock'
      ./include/net/sock.h:548: warning: Excess struct member 'sk_padding' description in 'sock'
    
    Signed-off-by: Jonathan Corbet <corbet@lwn.net>
    Reviewed-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/sock.h
+++ include/net/sock.h
@@ -264,8 +264,6 @@ struct sk_filter;
   *	@sk_pacing_status: Pacing status (requested, handled by sch_fq)
   *	@sk_max_pacing_rate: Maximum pacing rate (%SO_MAX_PACING_RATE)
   *	@sk_sndbuf: size of send buffer in bytes
-  *	@__sk_flags_offset: empty field used to determine location of bitfield
-  *	@sk_padding: unused element for alignment
   *	@sk_no_check_tx: %SO_NO_CHECK setting, set checksum in TX packets
   *	@sk_no_check_rx: allow zero checksum in RX packets
   *	@sk_route_caps: route capabilities (e.g. %NETIF_F_TSO)
