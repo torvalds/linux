c43203154d8ac579537aa0c7802b77d463b1f53a
Jens Axboe <axboe@kernel.dk>
2023-12-19 08:54:20 -0700

    io_uring/register: move io_uring_register(2) related code to register.c
    Most of this code is basically self contained, move it out of the core
    io_uring file to bring a bit more separation to the registration related
    bits. This moves another ~10% of the code into register.c.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- io_uring/io_uring.h
+++ io_uring/io_uring.h
@@ -79,6 +79,14 @@ bool io_match_task_safe(struct io_kiocb *head, struct task_struct *task,
 void *io_mem_alloc(size_t size);
 void io_mem_free(void *ptr);
 
+enum {
+	IO_EVENTFD_OP_SIGNAL_BIT,
+	IO_EVENTFD_OP_FREE_BIT,
+};
+
+void io_eventfd_ops(struct rcu_head *rcu);
+void io_activate_pollwq(struct io_ring_ctx *ctx);
+
 #if defined(CONFIG_PROVE_LOCKING)
 static inline void io_lockdep_assert_cq_locked(struct io_ring_ctx *ctx)
 {
