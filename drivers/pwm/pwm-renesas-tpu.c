752193da3f8b0aa819a27fc741d46ab046be315e
Sean Young <sean@mess.org>
2023-12-19 16:30:26 +0000

    pwm: renesas: Remove unused include
    No mutex is used in this driver.
    
    Reviewed-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- drivers/pwm/pwm-renesas-tpu.c
+++ drivers/pwm/pwm-renesas-tpu.c
@@ -11,7 +11,6 @@
 #include <linux/init.h>
 #include <linux/ioport.h>
 #include <linux/module.h>
-#include <linux/mutex.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
