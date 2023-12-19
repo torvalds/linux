f6da276479c63ca29774bc331a537b92f0550c45
Ioana Ciornei <ioana.ciornei@nxp.com>
2023-12-19 13:59:30 +0200

    dpaa2-switch: do not clear any interrupts automatically
    The DPSW object has multiple event sources multiplexed over the same
    IRQ. The driver has the capability to configure only some of these
    events to trigger the IRQ.
    
    The dpsw_get_irq_status() can clear events automatically based on the
    value stored in the 'status' variable passed to it. We don't want that
    to happen because we could get into a situation when we are clearing
    more events than we actually handled.
    
    Just resort to manually clearing the events that we handled. Also, since
    status is not used on the out path we remove its initialization to zero.
    
    This change does not have a user-visible effect because the dpaa2-switch
    driver enables and handles all the DPSW events which exist at the
    moment.
    
    Signed-off-by: Ioana Ciornei <ioana.ciornei@nxp.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/dpaa2/dpaa2-switch.c
+++ drivers/net/ethernet/freescale/dpaa2/dpaa2-switch.c
@@ -1509,9 +1509,9 @@ static irqreturn_t dpaa2_switch_irq0_handler_thread(int irq_num, void *arg)
 	struct device *dev = (struct device *)arg;
 	struct ethsw_core *ethsw = dev_get_drvdata(dev);
 	struct ethsw_port_priv *port_priv;
-	u32 status = ~0;
 	int err, if_id;
 	bool had_mac;
+	u32 status;
 
 	err = dpsw_get_irq_status(ethsw->mc_io, 0, ethsw->dpsw_handle,
 				  DPSW_IRQ_INDEX_IF, &status);
@@ -1539,12 +1539,12 @@ static irqreturn_t dpaa2_switch_irq0_handler_thread(int irq_num, void *arg)
 			dpaa2_switch_port_connect_mac(port_priv);
 	}
 
-out:
 	err = dpsw_clear_irq_status(ethsw->mc_io, 0, ethsw->dpsw_handle,
 				    DPSW_IRQ_INDEX_IF, status);
 	if (err)
 		dev_err(dev, "Can't clear irq status (err %d)\n", err);
 
+out:
 	return IRQ_HANDLED;
 }
 
