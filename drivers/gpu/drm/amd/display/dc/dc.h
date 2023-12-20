a465536ebff88fcc42e131a1b09bbe3df829117b
Martin Leung <martin.leung@amd.com>
2023-12-20 16:56:11 -0500

    drm/amd/display: revert "Optimize VRR updates to only necessary ones"
    This reverts commit 6e4337f695c25162f0296934152506ad596fcebf.
    
    The original commit causes regression in corner case with HDMI at
    specific timings. Reverting from staging to get the full suite to
    retest.
    
    Reviewed-by: Rodrigo Siqueira <rodrigo.siqueira@amd.com>
    Signed-off-by: Martin Leung <martin.leung@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dc.h
+++ drivers/gpu/drm/amd/display/dc/dc.h
@@ -1036,6 +1036,7 @@ struct dc {
 
 	/* Require to optimize clocks and bandwidth for added/removed planes */
 	bool optimized_required;
+	bool wm_optimized_required;
 	bool idle_optimizations_allowed;
 	bool enable_c20_dtm_b0;
 
