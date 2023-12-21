torvalds/linux

commit 0c2c6c94303c37f148a90a974041b4524ba26e37

drm/bridge: synopsys: Fix wrong FIFO reset on AUD_CONF0
Should use update_bits instead of write directly which
will override the other configs on AUD_CONF0.

Fixes: 19820cc6531a ("drm/bridge: synopsys: Recover sound for XRUN routine")

Signed-off-by: Sugar Zhang <sugar.zhang@rock-chips.com>
Change-Id: Ie64cdce700a30f1b4a64fc9d437d34bd0496aa30

drivers/gpu/drm/bridge/synopsys/dw-hdmi-i2s-audio.c
@@ -34,6 +34,14 @@ static inline u8 hdmi_read(struct dw_hdmi_i2s_audio_data *audio, int offset)
 	return audio->read(hdmi, offset);
 }
 
+static inline void hdmi_update_bits(struct dw_hdmi_i2s_audio_data *audio,
+				    u8 data, u8 mask, unsigned int reg)
+{
+	struct dw_hdmi *hdmi = audio->hdmi;
+
+	audio->mod(hdmi, data, mask, reg);
+}
+
 static int dw_hdmi_i2s_hw_params(struct device *dev, void *data,
 				 struct hdmi_codec_daifmt *fmt,
 				 struct hdmi_codec_params *hparms)
@@ -52,7 +60,8 @@ static int dw_hdmi_i2s_hw_params(struct device *dev, void *data,
 	}
 
 	/* Reset the FIFOs before applying new params */
-	hdmi_write(audio, HDMI_AUD_CONF0_SW_RESET, HDMI_AUD_CONF0);
+	hdmi_update_bits(audio, HDMI_AUD_CONF0_SW_RESET,
+			 HDMI_AUD_CONF0_SW_RESET, HDMI_AUD_CONF0);
 	hdmi_write(audio, (u8)~HDMI_MC_SWRSTZ_I2SSWRST_REQ, HDMI_MC_SWRSTZ);
 
 	inputclkfs	= HDMI_AUD_INPUTCLKFS_64FS;
@@ -141,7 +150,8 @@ static int dw_hdmi_i2s_prepare(struct device *dev, void *data,
 
 	dw_hdmi_audio_disable(hdmi);
 
-	hdmi_write(audio, HDMI_AUD_CONF0_SW_RESET, HDMI_AUD_CONF0);
+	hdmi_update_bits(audio, HDMI_AUD_CONF0_SW_RESET,
+			 HDMI_AUD_CONF0_SW_RESET, HDMI_AUD_CONF0);
 	hdmi_write(audio, (u8)~HDMI_MC_SWRSTZ_I2SSWRST_REQ, HDMI_MC_SWRSTZ);
 
 	dw_hdmi_audio_enable(hdmi);

drivers/gpu/drm/bridge/synopsys/dw-hdmi-audio.h
@@ -17,6 +17,7 @@ struct dw_hdmi_i2s_audio_data {
 
 	void (*write)(struct dw_hdmi *hdmi, u8 val, int offset);
 	u8 (*read)(struct dw_hdmi *hdmi, int offset);
+	void (*mod)(struct dw_hdmi *hdmi, u8 data, u8 mask, unsigned int reg);
 	u8 *(*get_eld)(struct dw_hdmi *hdmi);
 };
 

drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
@@ -5008,6 +5008,7 @@ struct dw_hdmi *dw_hdmi_probe(struct platform_device *pdev,
 		audio.get_eld	= hdmi_audio_get_eld;
 		audio.write	= hdmi_writeb;
 		audio.read	= hdmi_readb;
+		audio.mod	= hdmi_modb;
 		hdmi->enable_audio = dw_hdmi_i2s_audio_enable;
 		hdmi->disable_audio = dw_hdmi_i2s_audio_disable;
 

commit 87d068845824b89f96208feb628be75bdd2b1f1e

drm/bridge: synopsys: Fix no sound on some TVs
This patch do full hw params re-configuation instead of the reset one.

Fixes: 0c2c6c94303c ("drm/bridge: synopsys: Fix wrong FIFO reset on AUD_CONF0")

Signed-off-by: Sugar Zhang <sugar.zhang@rock-chips.com>
Change-Id: I1e259797ce22cd9a3be7086e1f21bd02c440ac57

drivers/gpu/drm/bridge/synopsys/dw-hdmi-i2s-audio.c
@@ -145,18 +145,7 @@ static int dw_hdmi_i2s_prepare(struct device *dev, void *data,
 			       struct hdmi_codec_daifmt *fmt,
 			       struct hdmi_codec_params *hparms)
 {
-	struct dw_hdmi_i2s_audio_data *audio = data;
-	struct dw_hdmi *hdmi = audio->hdmi;
-
-	dw_hdmi_audio_disable(hdmi);
-
-	hdmi_update_bits(audio, HDMI_AUD_CONF0_SW_RESET,
-			 HDMI_AUD_CONF0_SW_RESET, HDMI_AUD_CONF0);
-	hdmi_write(audio, (u8)~HDMI_MC_SWRSTZ_I2SSWRST_REQ, HDMI_MC_SWRSTZ);
-
-	dw_hdmi_audio_enable(hdmi);
-
-	return 0;
+	return dw_hdmi_i2s_hw_params(dev, data, fmt, hparms);
 }
 
 static int dw_hdmi_i2s_audio_startup(struct device *dev, void *data)