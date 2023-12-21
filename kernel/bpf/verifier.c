5abde62465222edd3080b70099bd809f166d5d7d
Simon Horman <horms@kernel.org>
2023-12-21 18:03:52 +0100

    bpf: Avoid unnecessary use of comma operator in verifier
    Although it does not seem to have any untoward side-effects, the use
    of ';' to separate to assignments seems more appropriate than ','.
    
    Flagged by clang-17 -Wcomma
    
    No functional change intended. Compile tested only.
    
    Signed-off-by: Simon Horman <horms@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Reviewed-by: Dave Marchevsky <davemarchevsky@fb.com>
    Link: https://lore.kernel.org/bpf/20231221-bpf-verifier-comma-v1-1-cde2530912e9@kernel.org

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -9616,7 +9616,7 @@ static int set_find_vma_callback_state(struct bpf_verifier_env *env,
 	callee->regs[BPF_REG_2].type = PTR_TO_BTF_ID;
 	__mark_reg_known_zero(&callee->regs[BPF_REG_2]);
 	callee->regs[BPF_REG_2].btf =  btf_vmlinux;
-	callee->regs[BPF_REG_2].btf_id = btf_tracing_ids[BTF_TRACING_TYPE_VMA],
+	callee->regs[BPF_REG_2].btf_id = btf_tracing_ids[BTF_TRACING_TYPE_VMA];
 
 	/* pointer to stack or null */
 	callee->regs[BPF_REG_3] = caller->regs[BPF_REG_4];
