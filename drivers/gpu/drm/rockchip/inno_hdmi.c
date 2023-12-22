f68a68fe9d9197ea6aa9cb461270685f370b165e
Alex Bee <knaerzche@gmail.com>
2023-12-22 18:42:09 +0100

    drm/rockchip: inno_hdmi: Remove useless include
    The inclusion syscon.h isn't used anywhere. Remove it.
    
    Signed-off-by: Alex Bee <knaerzche@gmail.com>
    Reviewed-by: Maxime Ripard <mripard@kernel.org>
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20231222174220.55249-19-knaerzche@gmail.com

--- drivers/gpu/drm/rockchip/inno_hdmi.c
+++ drivers/gpu/drm/rockchip/inno_hdmi.c
@@ -10,7 +10,6 @@
 #include <linux/delay.h>
 #include <linux/err.h>
 #include <linux/hdmi.h>
-#include <linux/mfd/syscon.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
