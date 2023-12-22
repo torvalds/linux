c39aa3b289e9c10d0d246cd919b06809f13b72b8
Yonghong Song <yonghong.song@linux.dev>
2023-12-21 19:17:45 -0800

    bpf: Allow per unit prefill for non-fix-size percpu memory allocator
    Commit 41a5db8d8161 ("Add support for non-fix-size percpu mem allocation")
    added support for non-fix-size percpu memory allocation.
    Such allocation will allocate percpu memory for all buckets on all
    cpus and the memory consumption is in the order to quadratic.
    For example, let us say, 4 cpus, unit size 16 bytes, so each
    cpu has 16 * 4 = 64 bytes, with 4 cpus, total will be 64 * 4 = 256 bytes.
    Then let us say, 8 cpus with the same unit size, each cpu
    has 16 * 8 = 128 bytes, with 8 cpus, total will be 128 * 8 = 1024 bytes.
    So if the number of cpus doubles, the number of memory consumption
    will be 4 times. So for a system with large number of cpus, the
    memory consumption goes up quickly with quadratic order.
    For example, for 4KB percpu allocation, 128 cpus. The total memory
    consumption will 4KB * 128 * 128 = 64MB. Things will become
    worse if the number of cpus is bigger (e.g., 512, 1024, etc.)
    
    In Commit 41a5db8d8161, the non-fix-size percpu memory allocation is
    done in boot time, so for system with large number of cpus, the initial
    percpu memory consumption is very visible. For example, for 128 cpu
    system, the total percpu memory allocation will be at least
    (16 + 32 + 64 + 96 + 128 + 196 + 256 + 512 + 1024 + 2048 + 4096)
      * 128 * 128 = ~138MB.
    which is pretty big. It will be even bigger for larger number of cpus.
    
    Note that the current prefill also allocates 4 entries if the unit size
    is less than 256. So on top of 138MB memory consumption, this will
    add more consumption with
    3 * (16 + 32 + 64 + 96 + 128 + 196 + 256) * 128 * 128 = ~38MB.
    Next patch will try to reduce this memory consumption.
    
    Later on, Commit 1fda5bb66ad8 ("bpf: Do not allocate percpu memory
    at init stage") moved the non-fix-size percpu memory allocation
    to bpf verificaiton stage. Once a particular bpf_percpu_obj_new()
    is called by bpf program, the memory allocator will try to fill in
    the cache with all sizes, causing the same amount of percpu memory
    consumption as in the boot stage.
    
    To reduce the initial percpu memory consumption for non-fix-size
    percpu memory allocation, instead of filling the cache with all
    supported allocation sizes, this patch intends to fill the cache
    only for the requested size. As typically users will not use large
    percpu data structure, this can save memory significantly.
    For example, the allocation size is 64 bytes with 128 cpus.
    Then total percpu memory amount will be 64 * 128 * 128 = 1MB,
    much less than previous 138MB.
    
    Signed-off-by: Yonghong Song <yonghong.song@linux.dev>
    Acked-by: Hou Tao <houtao1@huawei.com>
    Link: https://lore.kernel.org/r/20231222031745.1289082-1-yonghong.song@linux.dev
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- kernel/bpf/memalloc.c
+++ kernel/bpf/memalloc.c
@@ -121,6 +121,8 @@ struct bpf_mem_caches {
 	struct bpf_mem_cache cache[NUM_CACHES];
 };
 
+static const u16 sizes[NUM_CACHES] = {96, 192, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096};
+
 static struct llist_node notrace *__llist_del_first(struct llist_head *head)
 {
 	struct llist_node *entry, *next;
@@ -499,11 +501,13 @@ static void prefill_mem_cache(struct bpf_mem_cache *c, int cpu)
  */
 int bpf_mem_alloc_init(struct bpf_mem_alloc *ma, int size, bool percpu)
 {
-	static u16 sizes[NUM_CACHES] = {96, 192, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096};
 	struct bpf_mem_caches *cc, __percpu *pcc;
 	struct bpf_mem_cache *c, __percpu *pc;
 	struct obj_cgroup *objcg = NULL;
 	int cpu, i, unit_size, percpu_size = 0;
+
+	if (percpu && size == 0)
+		return -EINVAL;
 
 	/* room for llist_node and per-cpu pointer */
 	if (percpu)
@@ -524,6 +528,7 @@ int bpf_mem_alloc_init(struct bpf_mem_alloc *ma, int size, bool percpu)
 			objcg = get_obj_cgroup_from_current();
 #endif
 		ma->objcg = objcg;
+
 		for_each_possible_cpu(cpu) {
 			c = per_cpu_ptr(pc, cpu);
 			c->unit_size = unit_size;
@@ -559,6 +564,56 @@ int bpf_mem_alloc_init(struct bpf_mem_alloc *ma, int size, bool percpu)
 	}
 
 	ma->caches = pcc;
+	return 0;
+}
+
+int bpf_mem_alloc_percpu_init(struct bpf_mem_alloc *ma, struct obj_cgroup *objcg)
+{
+	struct bpf_mem_caches __percpu *pcc;
+
+	pcc = __alloc_percpu_gfp(sizeof(struct bpf_mem_caches), 8, GFP_KERNEL);
+	if (!pcc)
+		return -ENOMEM;
+
+	ma->caches = pcc;
+	ma->objcg = objcg;
+	ma->percpu = true;
+	return 0;
+}
+
+int bpf_mem_alloc_percpu_unit_init(struct bpf_mem_alloc *ma, int size)
+{
+	struct bpf_mem_caches *cc, __percpu *pcc;
+	int cpu, i, unit_size, percpu_size;
+	struct obj_cgroup *objcg;
+	struct bpf_mem_cache *c;
+
+	i = bpf_mem_cache_idx(size);
+	if (i < 0)
+		return -EINVAL;
+
+	/* room for llist_node and per-cpu pointer */
+	percpu_size = LLIST_NODE_SZ + sizeof(void *);
+
+	unit_size = sizes[i];
+	objcg = ma->objcg;
+	pcc = ma->caches;
+
+	for_each_possible_cpu(cpu) {
+		cc = per_cpu_ptr(pcc, cpu);
+		c = &cc->cache[i];
+		if (cpu == 0 && c->unit_size)
+			break;
+
+		c->unit_size = unit_size;
+		c->objcg = objcg;
+		c->percpu_size = percpu_size;
+		c->tgt = c;
+
+		init_refill_work(c);
+		prefill_mem_cache(c, cpu);
+	}
+
 	return 0;
 }
 
