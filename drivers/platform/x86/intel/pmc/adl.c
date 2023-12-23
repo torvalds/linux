6f9cc5c1f94daa98846b2073733d03ced709704b
David E. Box <david.e.box@linux.intel.com>
2023-12-22 19:25:44 -0800

    platform/x86/intel/pmc: Allow reenabling LTRs
    Commit 804951203aa5 ("platform/x86:intel/pmc: Combine core_init() and
    core_configure()") caused a network performance regression due to the GBE
    LTR ignore that it added during probe. The fix will move the ignore to
    occur at suspend-time (so as to not affect suspend power). This will
    require the ability to enable the LTR again on resume. Modify
    pmc_core_send_ltr_ignore() to allow enabling an LTR.
    
    Fixes: 804951203aa5 ("platform/x86:intel/pmc: Combine core_init() and core_configure()")
    Signed-off-by: "David E. Box" <david.e.box@linux.intel.com>
    Link: https://lore.kernel.org/r/20231223032548.1680738-5-david.e.box@linux.intel.com
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>

--- drivers/platform/x86/intel/pmc/adl.c
+++ drivers/platform/x86/intel/pmc/adl.c
@@ -323,7 +323,7 @@ int adl_core_init(struct pmc_dev *pmcdev)
 	 * when a cable is attached. Tell the PMC to ignore it.
 	 */
 	dev_dbg(&pmcdev->pdev->dev, "ignoring GBE LTR\n");
-	pmc_core_send_ltr_ignore(pmcdev, 3);
+	pmc_core_send_ltr_ignore(pmcdev, 3, 1);
 
 	return 0;
 }
