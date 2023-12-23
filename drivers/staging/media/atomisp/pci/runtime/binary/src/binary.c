3b63b7db1d9298847c90905a426f589a7f9beaae
Dipendra Khadka <kdipendra88@gmail.com>
2023-12-23 10:15:21 +0100

    media: atomisp: Fix spelling mistake in binary.c
    codespell reported following spelling mistake
    in runtime/binary/src as below:
    
    '''
    ./runtime/binary/src/binary.c:537: spcification ==> specification
    '''
    This patch fixes thisspelling mistake.
    
    Link: https://lore.kernel.org/r/20231223091521.85467-1-kdipendra88@gmail.com
    
    Signed-off-by: Dipendra Khadka <kdipendra88@gmail.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>

--- drivers/staging/media/atomisp/pci/runtime/binary/src/binary.c
+++ drivers/staging/media/atomisp/pci/runtime/binary/src/binary.c
@@ -534,7 +534,7 @@ ia_css_binary_uninit(void) {
 static int
 binary_grid_deci_factor_log2(int width, int height)
 {
-	/* 3A/Shading decimation factor spcification (at August 2008)
+	/* 3A/Shading decimation factor specification (at August 2008)
 	 * ------------------------------------------------------------------
 	 * [Image Width (BQ)] [Decimation Factor (BQ)] [Resulting grid cells]
 	 * 1280 ?c             32                       40 ?c
