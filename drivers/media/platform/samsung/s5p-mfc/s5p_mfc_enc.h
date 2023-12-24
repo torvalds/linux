e12dcb89e8aa27ed3330ee78e64e9ab1242fe07b
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2023-12-24 16:44:02 +0100

    media: s5p-mfc: drop unused get_*_def_fmt declarations
    get_dec_def_fmt() and get_enc_def_fmt() do not have definitions, so
    their declarations are pointless.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Reviewed-by: Aakarsh Jain <aakarsh.jain@samsung.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

--- drivers/media/platform/samsung/s5p-mfc/s5p_mfc_enc.h
+++ drivers/media/platform/samsung/s5p-mfc/s5p_mfc_enc.h
@@ -12,7 +12,6 @@
 const struct s5p_mfc_codec_ops *get_enc_codec_ops(void);
 struct vb2_ops *get_enc_queue_ops(void);
 const struct v4l2_ioctl_ops *get_enc_v4l2_ioctl_ops(void);
-struct s5p_mfc_fmt *get_enc_def_fmt(bool src);
 int s5p_mfc_enc_ctrls_setup(struct s5p_mfc_ctx *ctx);
 void s5p_mfc_enc_ctrls_delete(struct s5p_mfc_ctx *ctx);
 void s5p_mfc_enc_init(struct s5p_mfc_ctx *ctx);
