51fb591edc867a11c7fcebfa88298e7ccdb61265
Andrey Konovalov <andreyknvl@google.com>
2023-12-25 16:19:24 +0100

    xtensa, kasan: define KASAN_SHADOW_END
    Common KASAN code might rely on the definitions of the shadow mapping
    start, end, and size.  Define KASAN_SHADOW_END in addition to
    KASAN_SHADOW_START and KASAN_SHADOW_SIZE.
    
    Link: https://lkml.kernel.org/r/20231225151924.5422-1-andrey.konovalov@linux.dev
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202312240755.MqsWuTno-lkp@intel.com/
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Andrey Ryabinin <ryabinin.a.a@gmail.com>
    Cc: Chris Zankel <chris@zankel.net>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Marco Elver <elver@google.com>
    Cc: Max Filippov <jcmvbkbc@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/xtensa/include/asm/kasan.h
+++ arch/xtensa/include/asm/kasan.h
@@ -18,6 +18,8 @@
 #define KASAN_SHADOW_START (XCHAL_PAGE_TABLE_VADDR + XCHAL_PAGE_TABLE_SIZE)
 /* Size of the shadow map */
 #define KASAN_SHADOW_SIZE (-KASAN_START_VADDR >> KASAN_SHADOW_SCALE_SHIFT)
+/* End of the shadow map */
+#define KASAN_SHADOW_END (KASAN_SHADOW_START + KASAN_SHADOW_SIZE)
 /* Offset for mem to shadow address transformation */
 #define KASAN_SHADOW_OFFSET __XTENSA_UL_CONST(CONFIG_KASAN_SHADOW_OFFSET)
 
