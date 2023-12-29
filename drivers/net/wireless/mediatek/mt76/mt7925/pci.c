9300ae0cd9e8f2407b20e0e67ee3ea03dc8b06af
Quan Zhou <quan.zhou@mediatek.com>
2023-12-29 11:09:35 +0800

    wifi: mt76: mt7925: add flow to avoid chip bt function fail
    A sub-process of Wifi L0.5 reset will make chip common partition
    enter low power, and have chance lead to Bluetooth host-to-chip
    command timeout, modify the software flow according to the chip's
    design to solve the problem.
    
    Fixes: c948b5da6bbe ("wifi: mt76: mt7925: add Mediatek Wi-Fi7 driver for mt7925 chips")
    Signed-off-by: Quan Zhou <quan.zhou@mediatek.com>
    Signed-off-by: Ming Yen Hsieh <mingyen.hsieh@mediatek.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7925/pci.c
+++ drivers/net/wireless/mediatek/mt76/mt7925/pci.c
@@ -386,6 +386,8 @@ static int mt7925_pci_probe(struct pci_dev *pdev,
 
 	dev_info(mdev->dev, "ASIC revision: %04x\n", mdev->rev);
 
+	mt76_rmw_field(dev, MT_HW_EMI_CTL, MT_HW_EMI_CTL_SLPPROT_EN, 1);
+
 	ret = mt792x_wfsys_reset(dev);
 	if (ret)
 		goto err_free_dev;
