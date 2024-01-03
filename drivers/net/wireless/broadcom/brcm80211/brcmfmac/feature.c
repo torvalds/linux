ba4d4726335c77bb0ac17840b2c5f764f3d40b99
Arend van Spriel <arend.vanspriel@broadcom.com>
2024-01-03 10:57:03 +0100

    wifi: brcmfmac: move feature overrides before feature_disable
    The feature_disable option in brcmf_feat_attach() is a debugging or
    workaround option to disable features that cause driver issues. Hence
    they should be done as last step after all feature detection mechanisms
    have been completed.
    
    Signed-off-by: Arend van Spriel <arend.vanspriel@broadcom.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://msgid.link/20240103095704.135651-4-arend.vanspriel@broadcom.com

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.c
@@ -340,6 +340,9 @@ void brcmf_feat_attach(struct brcmf_pub *drvr)
 	brcmf_feat_iovar_int_get(ifp, BRCMF_FEAT_FWSUP, "sup_wpa");
 	brcmf_feat_iovar_int_get(ifp, BRCMF_FEAT_SCAN_V2, "scan_ver");
 
+	brcmf_feat_wlc_version_overrides(drvr);
+	brcmf_feat_firmware_overrides(drvr);
+
 	brcmf_fwvid_feat_attach(ifp);
 
 	if (drvr->settings->feature_disable) {
@@ -348,9 +351,6 @@ void brcmf_feat_attach(struct brcmf_pub *drvr)
 			  drvr->settings->feature_disable);
 		ifp->drvr->feat_flags &= ~drvr->settings->feature_disable;
 	}
-
-	brcmf_feat_wlc_version_overrides(drvr);
-	brcmf_feat_firmware_overrides(drvr);
 
 	/* set chip related quirks */
 	switch (drvr->bus_if->chip) {
