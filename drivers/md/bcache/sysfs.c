b20a229c28bee1aef063d1abc3e1fc036609b396
Pierre Gondois <pierre.gondois@arm.com>
2024-01-04 17:49:35 +0100

    bcache: use of hlist_count_nodes()
    Make use of the newly added hlist_count_nodes().
    
    Link: https://lkml.kernel.org/r/20240104164937.424320-4-pierre.gondois@arm.com
    Signed-off-by: Pierre Gondois <pierre.gondois@arm.com>
    Acked-by: Coly Li <colyli@suse.de>
    Acked-by: Marco Elver <elver@google.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Cc: Arve Hjønnevåg <arve@android.com>
    Cc: Carlos Llamas <cmllamas@google.com>
    Cc: Christian Brauner <brauner@kernel.org>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Cc: Joel Fernandes (Google) <joel@joelfernandes.org>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Kent Overstreet <kent.overstreet@gmail.com>
    Cc: Martijn Coenen <maco@android.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Todd Kjos <tkjos@android.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/md/bcache/sysfs.c
+++ drivers/md/bcache/sysfs.c
@@ -702,13 +702,7 @@ static unsigned int bch_cache_max_chain(struct cache_set *c)
 	for (h = c->bucket_hash;
 	     h < c->bucket_hash + (1 << BUCKET_HASH_BITS);
 	     h++) {
-		unsigned int i = 0;
-		struct hlist_node *p;
-
-		hlist_for_each(p, h)
-			i++;
-
-		ret = max(ret, i);
+		ret = max(ret, hlist_count_nodes(h));
 	}
 
 	mutex_unlock(&c->bucket_lock);
