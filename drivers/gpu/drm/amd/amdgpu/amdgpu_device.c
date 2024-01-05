9749c868434596315231c8ab0c818d87b31641df
Ma Jun <Jun.Ma2@amd.com>
2024-01-05 14:05:25 +0800

    drm/amdgpu: Fix the warning info in mode1 reset
    Fix the warning info below during mode1 reset.
    [  +0.000004] Call Trace:
    [  +0.000004]  <TASK>
    [  +0.000006]  ? show_regs+0x6e/0x80
    [  +0.000011]  ? __flush_work.isra.0+0x2e8/0x390
    [  +0.000005]  ? __warn+0x91/0x150
    [  +0.000009]  ? __flush_work.isra.0+0x2e8/0x390
    [  +0.000006]  ? report_bug+0x19d/0x1b0
    [  +0.000013]  ? handle_bug+0x46/0x80
    [  +0.000012]  ? exc_invalid_op+0x1d/0x80
    [  +0.000011]  ? asm_exc_invalid_op+0x1f/0x30
    [  +0.000014]  ? __flush_work.isra.0+0x2e8/0x390
    [  +0.000007]  ? __flush_work.isra.0+0x208/0x390
    [  +0.000007]  ? _prb_read_valid+0x216/0x290
    [  +0.000008]  __cancel_work_timer+0x11d/0x1a0
    [  +0.000007]  ? try_to_grab_pending+0xe8/0x190
    [  +0.000012]  cancel_work_sync+0x14/0x20
    [  +0.000008]  amddrm_sched_stop+0x3c/0x1d0 [amd_sched]
    [  +0.000032]  amdgpu_device_gpu_recover+0x29a/0xe90 [amdgpu]
    
    This warning info was printed after applying the patch
    "drm/sched: Convert drm scheduler to use a work queue rather than kthread".
    The root cause is that amdgpu driver tries to use the uninitialized
    work_struct in the struct drm_gpu_scheduler
    
    v2:
     - Rename the function to amdgpu_ring_sched_ready and move it to
    amdgpu_ring.c (Alex)
    v3:
    - Fix a few more checks based on Vitaly's patch (Alex)
    v4:
    - squash in fix noticed by Bert in
    https://gitlab.freedesktop.org/drm/amd/-/issues/3139
    
    Fixes: 11b3b9f461c5 ("drm/sched: Check scheduler ready before calling timeout handling")
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Vitaly Prosyak <vitaly.prosyak@amd.com>
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -5036,7 +5036,7 @@ bool amdgpu_device_has_job_running(struct amdgpu_device *adev)
 	for (i = 0; i < AMDGPU_MAX_RINGS; ++i) {
 		struct amdgpu_ring *ring = adev->rings[i];
 
-		if (!ring || !drm_sched_wqueue_ready(&ring->sched))
+		if (!amdgpu_ring_sched_ready(ring))
 			continue;
 
 		spin_lock(&ring->sched.job_list_lock);
@@ -5175,7 +5175,7 @@ int amdgpu_device_pre_asic_reset(struct amdgpu_device *adev,
 	for (i = 0; i < AMDGPU_MAX_RINGS; ++i) {
 		struct amdgpu_ring *ring = adev->rings[i];
 
-		if (!ring || !drm_sched_wqueue_ready(&ring->sched))
+		if (!amdgpu_ring_sched_ready(ring))
 			continue;
 
 		/* Clear job fence from fence drv to avoid force_completion
@@ -5642,7 +5642,7 @@ int amdgpu_device_gpu_recover(struct amdgpu_device *adev,
 		for (i = 0; i < AMDGPU_MAX_RINGS; ++i) {
 			struct amdgpu_ring *ring = tmp_adev->rings[i];
 
-			if (!ring || !drm_sched_wqueue_ready(&ring->sched))
+			if (!amdgpu_ring_sched_ready(ring))
 				continue;
 
 			drm_sched_stop(&ring->sched, job ? &job->base : NULL);
@@ -5711,7 +5711,7 @@ skip_hw_reset:
 		for (i = 0; i < AMDGPU_MAX_RINGS; ++i) {
 			struct amdgpu_ring *ring = tmp_adev->rings[i];
 
-			if (!ring || !drm_sched_wqueue_ready(&ring->sched))
+			if (!amdgpu_ring_sched_ready(ring))
 				continue;
 
 			drm_sched_start(&ring->sched, true);
@@ -6066,7 +6066,7 @@ pci_ers_result_t amdgpu_pci_error_detected(struct pci_dev *pdev, pci_channel_sta
 		for (i = 0; i < AMDGPU_MAX_RINGS; ++i) {
 			struct amdgpu_ring *ring = adev->rings[i];
 
-			if (!ring || !drm_sched_wqueue_ready(&ring->sched))
+			if (!amdgpu_ring_sched_ready(ring))
 				continue;
 
 			drm_sched_stop(&ring->sched, NULL);
@@ -6208,7 +6208,7 @@ void amdgpu_pci_resume(struct pci_dev *pdev)
 	for (i = 0; i < AMDGPU_MAX_RINGS; ++i) {
 		struct amdgpu_ring *ring = adev->rings[i];
 
-		if (!ring || !drm_sched_wqueue_ready(&ring->sched))
+		if (!amdgpu_ring_sched_ready(ring))
 			continue;
 
 		drm_sched_start(&ring->sched, true);
