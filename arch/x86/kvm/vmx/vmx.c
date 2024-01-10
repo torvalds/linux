e6b5d16bbd2d4c8259ad76aa33de80d561aba5f9
Sean Christopherson <seanjc@google.com>
2024-01-09 17:27:01 -0800

    KVM: VMX: Re-enter guest in fastpath for "spurious" preemption timer exits
    Re-enter the guest in the fast path if VMX preeemption timer VM-Exit was
    "spurious", i.e. if KVM "soft disabled" the timer by writing -1u and by
    some miracle the timer expired before any other VM-Exit occurred.  This is
    just an intermediate step to cleaning up the preemption timer handling,
    optimizing these types of spurious VM-Exits is not interesting as they are
    extremely rare/infrequent.
    
    Link: https://lore.kernel.org/r/20240110012705.506918-3-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -5992,8 +5992,15 @@ static fastpath_t handle_fastpath_preemption_timer(struct kvm_vcpu *vcpu)
 {
 	struct vcpu_vmx *vmx = to_vmx(vcpu);
 
-	if (!vmx->req_immediate_exit &&
-	    !unlikely(vmx->loaded_vmcs->hv_timer_soft_disabled)) {
+	/*
+	 * In the *extremely* unlikely scenario that this is a spurious VM-Exit
+	 * due to the timer expiring while it was "soft" disabled, just eat the
+	 * exit and re-enter the guest.
+	 */
+	if (unlikely(vmx->loaded_vmcs->hv_timer_soft_disabled))
+		return EXIT_FASTPATH_REENTER_GUEST;
+
+	if (!vmx->req_immediate_exit) {
 		kvm_lapic_expired_hv_timer(vcpu);
 		return EXIT_FASTPATH_REENTER_GUEST;
 	}
