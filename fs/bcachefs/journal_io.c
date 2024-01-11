3e44f325f6f75078cdcd44cd337f517ba3650d05
Christoph Hellwig <hch@lst.de>
2024-01-11 08:36:55 +0100

    bcachefs: fix incorrect usage of REQ_OP_FLUSH
    REQ_OP_FLUSH is only for internal use in the blk-mq and request based
    drivers. File systems and other block layer consumers must use
    REQ_OP_WRITE | REQ_PREFLUSH as documented in
    Documentation/block/writeback_cache_control.rst.
    
    While REQ_OP_FLUSH appears to work for blk-mq drivers it does not
    get the proper flush state machine handling, and completely fails
    for any bio based drivers, including all the stacking drivers.  The
    block layer will also get a check in 6.8 to reject this use case
    entirely.
    
    [Note: completely untested, but as this never got fixed since the
    original bug report in November:
    
       https://bugzilla.kernel.org/show_bug.cgi?id=218184
    
    and the the discussion in December:
    
        https://lore.kernel.org/all/20231221053016.72cqcfg46vxwohcj@moria.home.lan/T/
    
    this seems to be best way to force it]
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/journal_io.c
+++ fs/bcachefs/journal_io.c
@@ -1988,7 +1988,8 @@ CLOSURE_CALLBACK(bch2_journal_write)
 			percpu_ref_get(&ca->io_ref);
 
 			bio = ca->journal.bio;
-			bio_reset(bio, ca->disk_sb.bdev, REQ_OP_FLUSH);
+			bio_reset(bio, ca->disk_sb.bdev,
+				  REQ_OP_WRITE|REQ_PREFLUSH);
 			bio->bi_end_io		= journal_write_endio;
 			bio->bi_private		= ca;
 			closure_bio_submit(bio, cl);
