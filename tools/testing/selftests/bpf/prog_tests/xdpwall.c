f067074bafd5060428211ee7bb8a3f86ff6bc58d
Nathan Chancellor <nathan@kernel.org>
2024-01-11 13:16:48 -0700

    selftests/bpf: Update LLVM Phabricator links
    reviews.llvm.org was LLVM's Phabricator instances for code review. It
    has been abandoned in favor of GitHub pull requests. While the majority
    of links in the kernel sources still work because of the work Fangrui
    has done turning the dynamic Phabricator instance into a static archive,
    there are some issues with that work, so preemptively convert all the
    links in the kernel sources to point to the commit on GitHub.
    
    Most of the commits have the corresponding differential review link in
    the commit message itself so there should not be any loss of fidelity in
    the relevant information.
    
    Additionally, fix a typo in the xdpwall.c print ("LLMV" -> "LLVM") while
    in the area.
    
    Link: https://discourse.llvm.org/t/update-on-github-pull-requests/71540/172
    Acked-by: Yonghong Song <yonghong.song@linux.dev>
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>
    Link: https://lore.kernel.org/r/20240111-bpf-update-llvm-phabricator-links-v2-1-9a7ae976bd64@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/testing/selftests/bpf/prog_tests/xdpwall.c
+++ tools/testing/selftests/bpf/prog_tests/xdpwall.c
@@ -9,7 +9,7 @@ void test_xdpwall(void)
 	struct xdpwall *skel;
 
 	skel = xdpwall__open_and_load();
-	ASSERT_OK_PTR(skel, "Does LLMV have https://reviews.llvm.org/D109073?");
+	ASSERT_OK_PTR(skel, "Does LLVM have https://github.com/llvm/llvm-project/commit/ea72b0319d7b0f0c2fcf41d121afa5d031b319d5?");
 
 	xdpwall__destroy(skel);
 }
