f9c29137392e77319f9974c2cdf27d087f05abee
Li RongQing <lirongqing@baidu.com>
2024-01-15 11:09:14 +0800

    virtio_fs: remove duplicate check if queue is broken
    virtqueue_enable_cb() will call virtqueue_poll() which will check if
    queue is broken at beginning, so remove the virtqueue_is_broken() call
    
    Signed-off-by: Li RongQing <lirongqing@baidu.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/virtio_fs.c
+++ fs/fuse/virtio_fs.c
@@ -401,7 +401,7 @@ static void virtio_fs_hiprio_done_work(struct work_struct *work)
 			kfree(req);
 			dec_in_flight_req(fsvq);
 		}
-	} while (!virtqueue_enable_cb(vq) && likely(!virtqueue_is_broken(vq)));
+	} while (!virtqueue_enable_cb(vq));
 	spin_unlock(&fsvq->lock);
 }
 
@@ -683,7 +683,7 @@ static void virtio_fs_requests_done_work(struct work_struct *work)
 			list_move_tail(&req->list, &reqs);
 			spin_unlock(&fpq->lock);
 		}
-	} while (!virtqueue_enable_cb(vq) && likely(!virtqueue_is_broken(vq)));
+	} while (!virtqueue_enable_cb(vq));
 	spin_unlock(&fsvq->lock);
 
 	/* End requests */
