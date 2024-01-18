557f8c582a9ba8abe6aa0fd734b6f342af106b26
Kees Cook <keescook@chromium.org>
2024-01-18 15:06:05 -0800

    ubsan: Reintroduce signed overflow sanitizer
    In order to mitigate unexpected signed wrap-around[1], bring back the
    signed integer overflow sanitizer. It was removed in commit 6aaa31aeb9cf
    ("ubsan: remove overflow checks") because it was effectively a no-op
    when combined with -fno-strict-overflow (which correctly changes signed
    overflow from being "undefined" to being explicitly "wrap around").
    
    Compilers are adjusting their sanitizers to trap wrap-around and to
    detecting common code patterns that should not be instrumented
    (e.g. "var + offset < var"). Prepare for this and explicitly rename
    the option from "OVERFLOW" to "WRAP" to more accurately describe the
    behavior.
    
    To annotate intentional wrap-around arithmetic, the helpers
    wrapping_add/sub/mul_wrap() can be used for individual statements. At
    the function level, the __signed_wrap attribute can be used to mark an
    entire function as expecting its signed arithmetic to wrap around. For a
    single object file the Makefile can use "UBSAN_SIGNED_WRAP_target.o := n"
    to mark it as wrapping, and for an entire directory, "UBSAN_SIGNED_WRAP :=
    n" can be used.
    
    Additionally keep these disabled under CONFIG_COMPILE_TEST for now.
    
    Link: https://github.com/KSPP/linux/issues/26 [1]
    Cc: Miguel Ojeda <ojeda@kernel.org>
    Cc: Nathan Chancellor <nathan@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Hao Luo <haoluo@google.com>
    Reviewed-by: Marco Elver <elver@google.com>
    Reviewed-by: Justin Stitt <justinstitt@google.com>
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- include/linux/compiler_types.h
+++ include/linux/compiler_types.h
@@ -282,11 +282,18 @@ struct ftrace_likely_data {
 #define __no_sanitize_or_inline __always_inline
 #endif
 
+/* Do not trap wrapping arithmetic within an annotated function. */
+#ifdef CONFIG_UBSAN_SIGNED_WRAP
+# define __signed_wrap __attribute__((no_sanitize("signed-integer-overflow")))
+#else
+# define __signed_wrap
+#endif
+
 /* Section for code which can't be instrumented at all */
 #define __noinstr_section(section)					\
 	noinline notrace __attribute((__section__(section)))		\
 	__no_kcsan __no_sanitize_address __no_profile __no_sanitize_coverage \
-	__no_sanitize_memory
+	__no_sanitize_memory __signed_wrap
 
 #define noinstr __noinstr_section(".noinstr.text")
 
