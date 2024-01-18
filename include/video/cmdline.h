a3b6792e990d63d47c4d2622a9704a01db50c3a2
Thomas Zimmermann <tzimmermann@suse.de>
2024-01-18 10:05:26 +0100

    video/cmdline: Introduce CONFIG_VIDEO for video= parameter
    Add CONFIG_VIDEO for common code in drivers/video/. Use the option to
    select helpers for the video= parameter. Replaces CONFIG_VIDEO_CMDLINE.
    
    Other common code in drivers/video/ can be moved behind CONFIG_VIDEO,
    which will simplify the Kconfig rules.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240118090721.7995-2-tzimmermann@suse.de

--- include/video/cmdline.h
+++ include/video/cmdline.h
@@ -5,16 +5,9 @@
 
 #include <linux/types.h>
 
-#if defined(CONFIG_VIDEO_CMDLINE)
 const char *video_get_options(const char *name);
 
 /* exported for compatibility with fbdev; don't use in new code */
 bool __video_get_options(const char *name, const char **option, bool is_of);
-#else
-static inline const char *video_get_options(const char *name)
-{
-	return NULL;
-}
-#endif
 
 #endif
