6cea614ba78d6b78e64e39f5ccdfad4aac1d0110
Lukasz Majczak <lma@chromium.org>
2024-01-19 08:43:27 +0000

    mfd: cros_ec: Register EC-based watchdog subdevice
    Add ChromeOS EC-based watchdog as EC subdevice.
    
    Signed-off-by: Lukasz Majczak <lma@chromium.org>
    Link: https://lore.kernel.org/r/20240119084328.3135503-4-lma@chromium.org
    Signed-off-by: Lee Jones <lee@kernel.org>

--- drivers/mfd/cros_ec_dev.c
+++ drivers/mfd/cros_ec_dev.c
@@ -91,6 +91,10 @@ static const struct mfd_cell cros_usbpd_notify_cells[] = {
 	{ .name = "cros-usbpd-notify", },
 };
 
+static const struct mfd_cell cros_ec_wdt_cells[] = {
+	{ .name = "cros-ec-wdt", }
+};
+
 static const struct cros_feature_to_cells cros_subdevices[] = {
 	{
 		.id		= EC_FEATURE_CEC,
@@ -106,6 +110,11 @@ static const struct cros_feature_to_cells cros_subdevices[] = {
 		.id		= EC_FEATURE_USB_PD,
 		.mfd_cells	= cros_usbpd_charger_cells,
 		.num_cells	= ARRAY_SIZE(cros_usbpd_charger_cells),
+	},
+	{
+		.id		= EC_FEATURE_HANG_DETECT,
+		.mfd_cells	= cros_ec_wdt_cells,
+		.num_cells	= ARRAY_SIZE(cros_ec_wdt_cells),
 	},
 };
 
