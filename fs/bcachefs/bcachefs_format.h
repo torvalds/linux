c6c4ff6507c4e1d32f9c2019795d4b7aa6eb559f
Kent Overstreet <kent.overstreet@linux.dev>
2024-01-21 02:42:53 -0500

    bcachefs: subvolume_format.h
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/bcachefs_format.h
+++ fs/bcachefs/bcachefs_format.h
@@ -773,38 +773,6 @@ struct bch_inline_data {
 	u8			data[];
 };
 
-/* Subvolumes: */
-
-#define SUBVOL_POS_MIN		POS(0, 1)
-#define SUBVOL_POS_MAX		POS(0, S32_MAX)
-#define BCACHEFS_ROOT_SUBVOL	1
-
-struct bch_subvolume {
-	struct bch_val		v;
-	__le32			flags;
-	__le32			snapshot;
-	__le64			inode;
-	/*
-	 * Snapshot subvolumes form a tree, separate from the snapshot nodes
-	 * tree - if this subvolume is a snapshot, this is the ID of the
-	 * subvolume it was created from:
-	 *
-	 * This is _not_ necessarily the subvolume of the directory containing
-	 * this subvolume:
-	 */
-	__le32			parent;
-	__le32			pad;
-	bch_le128		otime;
-};
-
-LE32_BITMASK(BCH_SUBVOLUME_RO,		struct bch_subvolume, flags,  0,  1)
-/*
- * We need to know whether a subvolume is a snapshot so we can know whether we
- * can delete it (or whether it should just be rm -rf'd)
- */
-LE32_BITMASK(BCH_SUBVOLUME_SNAP,	struct bch_subvolume, flags,  1,  2)
-LE32_BITMASK(BCH_SUBVOLUME_UNLINKED,	struct bch_subvolume, flags,  2,  3)
-
 /* LRU btree: */
 
 struct bch_lru {
@@ -872,6 +840,7 @@ struct bch_sb_field {
 #include "xattr_format.h"
 #include "quota_format.h"
 #include "snapshot_format.h"
+#include "subvolume_format.h"
 #include "sb-counters_format.h"
 
 enum bch_sb_field_type {
