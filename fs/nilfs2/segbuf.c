f7aeb97a584f5d80b22eafc822e9fb3714110b94
Ryusuke Konishi <konishi.ryusuke@gmail.com>
2024-01-22 23:01:49 +0900

    nilfs2: convert segment buffer to use kmap_local
    In the segment buffer code used for log writing, a CRC calculation routine
    uses the deprecated kmap_atomic(), so convert it to use kmap_local.
    
    Link: https://lkml.kernel.org/r/20240122140202.6950-3-konishi.ryusuke@gmail.com
    Signed-off-by: Ryusuke Konishi <konishi.ryusuke@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/nilfs2/segbuf.c
+++ fs/nilfs2/segbuf.c
@@ -220,9 +220,9 @@ static void nilfs_segbuf_fill_in_data_crc(struct nilfs_segment_buffer *segbuf,
 		crc = crc32_le(crc, bh->b_data, bh->b_size);
 	}
 	list_for_each_entry(bh, &segbuf->sb_payload_buffers, b_assoc_buffers) {
-		kaddr = kmap_atomic(bh->b_page);
+		kaddr = kmap_local_page(bh->b_page);
 		crc = crc32_le(crc, kaddr + bh_offset(bh), bh->b_size);
-		kunmap_atomic(kaddr);
+		kunmap_local(kaddr);
 	}
 	raw_sum->ss_datasum = cpu_to_le32(crc);
 }
