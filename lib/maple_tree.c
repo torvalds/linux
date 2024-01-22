8689d750006bbd811423dd41ed5efcd8a029862c
Lukas Bulwahn <lukas.bulwahn@gmail.com>
2024-01-22 11:20:00 +0100

    maple_tree: avoid duplicate variable init in mast_spanning_rebalance()
    The local variables r_tmp and l_tmp in mast_spanning_rebalance() are
    already initialized at its declaration; there is no need to assign the
    value again.
    
    Remove the duplicate initialization of {r,l}_tmp.  No functional change. 
    Due to common compiler optimizations, also no change to object code.
    
    This issue was identified with clang-analyzer's dead stores analysis.
    
    Link: https://lkml.kernel.org/r/20240122102000.29558-1-lukas.bulwahn@gmail.com
    Signed-off-by: Lukas Bulwahn <lukas.bulwahn@gmail.com>
    Reviewed-by: Liam R. Howlett <Liam.Howlett@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- lib/maple_tree.c
+++ lib/maple_tree.c
@@ -2271,8 +2271,6 @@ bool mast_spanning_rebalance(struct maple_subtree_state *mast)
 	struct ma_state l_tmp = *mast->orig_l;
 	unsigned char depth = 0;
 
-	r_tmp = *mast->orig_r;
-	l_tmp = *mast->orig_l;
 	do {
 		mas_ascend(mast->orig_r);
 		mas_ascend(mast->orig_l);
