08701813a1b439f01e52c10e66ee2e99d9d23c6c
Oleg Nesterov <oleg@redhat.com>
2024-01-22 18:16:31 +0100

    ptrace_attach: shift send(SIGSTOP) into ptrace_set_stopped()
    Turn send_sig_info(SIGSTOP) into send_signal_locked(SIGSTOP) and move it
    from ptrace_attach() to ptrace_set_stopped().
    
    This looks more logical and avoids lock(siglock) right after unlock().
    
    Link: https://lkml.kernel.org/r/20240122171631.GA29844@redhat.com
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -375,10 +375,13 @@ static int check_ptrace_options(unsigned long data)
 	return 0;
 }
 
-static inline void ptrace_set_stopped(struct task_struct *task)
+static inline void ptrace_set_stopped(struct task_struct *task, bool seize)
 {
 	guard(spinlock)(&task->sighand->siglock);
 
+	/* SEIZE doesn't trap tracee on attach */
+	if (!seize)
+		send_signal_locked(SIGSTOP, SEND_SIG_PRIV, task, PIDTYPE_PID);
 	/*
 	 * If the task is already STOPPED, set JOBCTL_TRAP_STOP and
 	 * TRAPPING, and kick it so that it transits to TRACED.  TRAPPING
@@ -457,14 +460,8 @@ static int ptrace_attach(struct task_struct *task, long request,
 				return -EPERM;
 
 			task->ptrace = flags;
-
 			ptrace_link(task, current);
-
-			/* SEIZE doesn't trap tracee on attach */
-			if (!seize)
-				send_sig_info(SIGSTOP, SEND_SIG_PRIV, task);
-
-			ptrace_set_stopped(task);
+			ptrace_set_stopped(task, seize);
 		}
 	}
 
