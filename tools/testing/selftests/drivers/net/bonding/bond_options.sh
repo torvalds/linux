09d60387d8c191bfc487317bf544b515f7d85956
Hangbin Liu <liuhangbin@gmail.com>
2024-01-23 15:59:17 +0800

    selftests: bonding: do not test arp/ns target with mode balance-alb/tlb
    [ Upstream commit a2933a8759a62269754e54733d993b19de870e84 ]
    
    The prio_arp/ns tests hard code the mode to active-backup. At the same
    time, The balance-alb/tlb modes do not support arp/ns target. So remove
    the prio_arp/ns tests from the loop and only test active-backup mode.
    
    Fixes: 481b56e0391e ("selftests: bonding: re-format bond option tests")
    Reported-by: Jay Vosburgh <jay.vosburgh@canonical.com>
    Closes: https://lore.kernel.org/netdev/17415.1705965957@famine/
    Signed-off-by: Hangbin Liu <liuhangbin@gmail.com>
    Acked-by: Jay Vosburgh <jay.vosburgh@canonical.com>
    Link: https://lore.kernel.org/r/20240123075917.1576360-1-liuhangbin@gmail.com
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/testing/selftests/drivers/net/bonding/bond_options.sh
+++ tools/testing/selftests/drivers/net/bonding/bond_options.sh
@@ -162,7 +162,7 @@ prio_arp()
 	local mode=$1
 
 	for primary_reselect in 0 1 2; do
-		prio_test "mode active-backup arp_interval 100 arp_ip_target ${g_ip4} primary eth1 primary_reselect $primary_reselect"
+		prio_test "mode $mode arp_interval 100 arp_ip_target ${g_ip4} primary eth1 primary_reselect $primary_reselect"
 		log_test "prio" "$mode arp_ip_target primary_reselect $primary_reselect"
 	done
 }
@@ -178,7 +178,7 @@ prio_ns()
 	fi
 
 	for primary_reselect in 0 1 2; do
-		prio_test "mode active-backup arp_interval 100 ns_ip6_target ${g_ip6} primary eth1 primary_reselect $primary_reselect"
+		prio_test "mode $mode arp_interval 100 ns_ip6_target ${g_ip6} primary eth1 primary_reselect $primary_reselect"
 		log_test "prio" "$mode ns_ip6_target primary_reselect $primary_reselect"
 	done
 }
@@ -194,9 +194,9 @@ prio()
 
 	for mode in $modes; do
 		prio_miimon $mode
-		prio_arp $mode
-		prio_ns $mode
 	done
+	prio_arp "active-backup"
+	prio_ns "active-backup"
 }
 
 arp_validate_test()
