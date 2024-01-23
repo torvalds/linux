143667ee9a9cb88b6da7fe6a3d0f32bc33d75d71
Max Gurtovoy <mgurtovoy@nvidia.com>
2024-01-23 16:40:26 +0200

    nvmet: compare mqes and sqsize only for IO SQ
    According to the NVMe Spec:
    "
    MQES: This field indicates the maximum individual queue size that the
    controller supports. For NVMe over PCIe implementations, this value
    applies to the I/O Submission Queues and I/O Completion Queues that the
    host creates. For NVMe over Fabrics implementations, this value applies
    to only the I/O Submission Queues that the host creates.
    "
    
    Align the target code to compare mqes and sqsize as mentioned in the
    NVMe Spec.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Sagi Grimberg <sagi@grimberg.me>
    Signed-off-by: Max Gurtovoy <mgurtovoy@nvidia.com>
    Signed-off-by: Keith Busch <kbusch@kernel.org>

--- drivers/nvme/target/fabrics-cmd.c
+++ drivers/nvme/target/fabrics-cmd.c
@@ -157,7 +157,8 @@ static u16 nvmet_install_queue(struct nvmet_ctrl *ctrl, struct nvmet_req *req)
 		return NVME_SC_CMD_SEQ_ERROR | NVME_SC_DNR;
 	}
 
-	if (sqsize > mqes) {
+	/* for fabrics, this value applies to only the I/O Submission Queues */
+	if (qid && sqsize > mqes) {
 		pr_warn("sqsize %u is larger than MQES supported %u cntlid %d\n",
 				sqsize, mqes, ctrl->cntlid);
 		req->error_loc = offsetof(struct nvmf_connect_command, sqsize);
