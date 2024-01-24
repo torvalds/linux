0f299da55ac3d28bc9de23a84fae01a85c4e253a
Christoph Hellwig <hch@lst.de>
2024-01-24 10:26:56 +0100

    blk-mq: move blk_mq_attempt_bio_merge out blk_mq_get_new_requests
    blk_mq_attempt_bio_merge has nothing to do with allocating a new
    request, it avoids allocating a new request.  Move the call out of
    blk_mq_get_new_requests and into the only caller.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Damien Le Moal <dlemoal@kernel.org>
    Tested-by: Damien Le Moal <dlemoal@kernel.org>
    Link: https://lore.kernel.org/r/20240124092658.2258309-2-hch@lst.de
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -2891,9 +2891,6 @@ static struct request *blk_mq_get_new_requests(struct request_queue *q,
 	};
 	struct request *rq;
 
-	if (blk_mq_attempt_bio_merge(q, bio, nsegs))
-		return NULL;
-
 	rq_qos_throttle(q, bio);
 
 	if (plug) {
@@ -2992,18 +2989,18 @@ void blk_mq_submit_bio(struct bio *bio)
 		if (unlikely(bio_may_exceed_limits(bio, &q->limits))) {
 			bio = __bio_split_to_limits(bio, &q->limits, &nr_segs);
 			if (!bio)
-				goto fail;
+				goto queue_exit;
 		}
 		if (!bio_integrity_prep(bio))
-			goto fail;
+			goto queue_exit;
 	}
 
+	if (blk_mq_attempt_bio_merge(q, bio, nr_segs))
+		goto queue_exit;
+
 	rq = blk_mq_get_new_requests(q, plug, bio, nr_segs);
-	if (unlikely(!rq)) {
-fail:
-		blk_queue_exit(q);
-		return;
-	}
+	if (unlikely(!rq))
+		goto queue_exit;
 
 done:
 	trace_block_getrq(bio);
@@ -3036,6 +3033,10 @@ done:
 	} else {
 		blk_mq_run_dispatch_ops(q, blk_mq_try_issue_directly(hctx, rq));
 	}
+	return;
+
+queue_exit:
+	blk_queue_exit(q);
 }
 
 #ifdef CONFIG_BLK_MQ_STACKING
