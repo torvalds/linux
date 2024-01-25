ad979679d64befabd9bdfa0756aefd8c4a34fd91
Breno Leitao <leitao@debian.org>
2024-01-25 11:34:17 -0800

    net: fill in MODULE_DESCRIPTION()s for ec_bhf
    W=1 builds now warn if module is built without a MODULE_DESCRIPTION().
    Add descriptions to the Beckhoff CX5020 EtherCAT Ethernet driver.
    
    Signed-off-by: Breno Leitao <leitao@debian.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/ec_bhf.c
+++ drivers/net/ethernet/ec_bhf.c
@@ -590,5 +590,6 @@ module_pci_driver(pci_driver);
 module_param(polling_frequency, long, 0444);
 MODULE_PARM_DESC(polling_frequency, "Polling timer frequency in ns");
 
+MODULE_DESCRIPTION("Beckhoff CX5020 EtherCAT Ethernet driver");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Dariusz Marcinkiewicz <reksio@newterm.pl>");
