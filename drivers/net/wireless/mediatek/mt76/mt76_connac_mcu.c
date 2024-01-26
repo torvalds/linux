098428c400ff2d0f32b7cc0dc003c8da4b69908d
Shayne Chen <shayne.chen@mediatek.com>
2024-01-26 17:09:22 +0800

    wifi: mt76: connac: set correct muar_idx for mt799x chipsets
    The MUAR (multicast unicast address) is an address mapping table that
    participates in the process of searching WTBL entries. For mt799x
    chipsets, the default muar index of BMC WTBL is 0xe.
    
    Signed-off-by: Shayne Chen <shayne.chen@mediatek.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt76_connac_mcu.c
+++ drivers/net/wireless/mediatek/mt76/mt76_connac_mcu.c
@@ -283,6 +283,9 @@ __mt76_connac_mcu_alloc_sta_req(struct mt76_dev *dev, struct mt76_vif *mvif,
 	};
 	struct sk_buff *skb;
 
+	if (is_mt799x(dev) && !wcid->sta)
+		hdr.muar_idx = 0xe;
+
 	mt76_connac_mcu_get_wlan_idx(dev, wcid, &hdr.wlan_idx_lo,
 				     &hdr.wlan_idx_hi);
 	skb = mt76_mcu_msg_alloc(dev, NULL, len);
