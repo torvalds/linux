0c10a15d21222ef22b41d11e423d7b17ed3d4a5d
Maaz Mombasawala <maaz.mombasawala@broadcom.com>
2024-01-26 15:08:02 -0500

    drm/vmwgfx: Add SPDX header to vmwgfx_drm.h
    Update vmwgfx_drm.h with SPDX-License-Identifier:
    (GPL-2.0 WITH Linux-syscall-note) OR MIT
    
    Signed-off-by: Maaz Mombasawala <maaz.mombasawala@broadcom.com>
    Reviewed-by: Martin Krastev <martin.krastev@broadcom.com>
    Signed-off-by: Zack Rusin <zack.rusin@broadcom.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240126200804.732454-4-zack.rusin@broadcom.com

--- include/uapi/drm/vmwgfx_drm.h
+++ include/uapi/drm/vmwgfx_drm.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: (GPL-2.0 WITH Linux-syscall-note) OR MIT */
 /**************************************************************************
  *
  * Copyright © 2009-2023 VMware, Inc., Palo Alto, CA., USA
