733c498a80853acbafe284a40468b91f4d41f0b4
Johannes Berg <johannes.berg@intel.com>
2024-01-29 15:54:02 +0100

    wifi: mac80211: fix driver debugfs for vif type change
    If a driver implements the change_interface() method, we switch
    interface type without taking the interface down, but still will
    recreate the debugfs for it since it's a new type. As such, we
    should use the ieee80211_debugfs_recreate_netdev() function here
    to also recreate the driver's files, if it is indeed from a type
    change while up.
    
    Link: https://msgid.link/20240129155402.7311a36ffeeb.I18df02bbeb685d4250911de5ffbaf090f60c3803@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/debugfs_netdev.c
+++ net/mac80211/debugfs_netdev.c
@@ -997,8 +997,8 @@ static void add_link_files(struct ieee80211_link_data *link,
 	}
 }
 
-void ieee80211_debugfs_add_netdev(struct ieee80211_sub_if_data *sdata,
-				  bool mld_vif)
+static void ieee80211_debugfs_add_netdev(struct ieee80211_sub_if_data *sdata,
+					 bool mld_vif)
 {
 	char buf[10+IFNAMSIZ];
 
