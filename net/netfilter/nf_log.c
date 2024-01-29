79578be4d35c842a802487e2f31c2aed80cc005f
Pablo Neira Ayuso <pablo@netfilter.org>
2024-01-29 20:24:24 +0100

    netfilter: nf_log: consolidate check for NULL logger in lookup function
    Consolidate pointer fetch to logger and check for NULL in
    __find_logger().
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Florian Westphal <fw@strlen.de>

--- net/netfilter/nf_log.c
+++ net/netfilter/nf_log.c
@@ -31,10 +31,10 @@ static struct nf_logger *__find_logger(int pf, const char *str_logger)
 	int i;
 
 	for (i = 0; i < NF_LOG_TYPE_MAX; i++) {
-		if (loggers[pf][i] == NULL)
+		log = nft_log_dereference(loggers[pf][i]);
+		if (!log)
 			continue;
 
-		log = nft_log_dereference(loggers[pf][i]);
 		if (!strncasecmp(str_logger, log->name, strlen(log->name)))
 			return log;
 	}
