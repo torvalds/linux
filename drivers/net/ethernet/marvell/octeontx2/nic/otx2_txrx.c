04f647c8e456fcfabe9c252a4dcaee03b586fa4f
Geetha sowjanya <gakula@marvell.com>
2024-01-30 17:36:10 +0530

    octeontx2-pf: Remove xdp queues on program detach
    XDP queues are created/destroyed when a XDP program
    is attached/detached. In current driver xdp_queues are not
    getting destroyed on program exit due to incorrect xdp_queue
    and tot_tx_queue count values.
    
    This patch fixes the issue by setting tot_tx_queue and xdp_queue
    count to correct values. It also fixes xdp.data_hard_start address.
    
    Fixes: 06059a1a9a4a ("octeontx2-pf: Add XDP support to netdev PF")
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Link: https://lore.kernel.org/r/20240130120610.16673-1-gakula@marvell.com
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_txrx.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_txrx.c
@@ -1403,7 +1403,7 @@ static bool otx2_xdp_rcv_pkt_handler(struct otx2_nic *pfvf,
 				     struct otx2_cq_queue *cq,
 				     bool *need_xdp_flush)
 {
-	unsigned char *hard_start, *data;
+	unsigned char *hard_start;
 	int qidx = cq->cq_idx;
 	struct xdp_buff xdp;
 	struct page *page;
@@ -1417,9 +1417,8 @@ static bool otx2_xdp_rcv_pkt_handler(struct otx2_nic *pfvf,
 
 	xdp_init_buff(&xdp, pfvf->rbsize, &cq->xdp_rxq);
 
-	data = (unsigned char *)phys_to_virt(pa);
-	hard_start = page_address(page);
-	xdp_prepare_buff(&xdp, hard_start, data - hard_start,
+	hard_start = (unsigned char *)phys_to_virt(pa);
+	xdp_prepare_buff(&xdp, hard_start, OTX2_HEAD_ROOM,
 			 cqe->sg.seg_size, false);
 
 	act = bpf_prog_run_xdp(prog, &xdp);
