4e73826089ce899357580bbf6e0afe4e6f9900b7
Lewis Huang <lewis.huang@amd.com>
2024-01-31 17:20:17 +0800

    drm/amd/display: Only allow dig mapping to pwrseq in new asic
    [Why]
    The old asic only have 1 pwrseq hw.
    We don't need to map the diginst to pwrseq inst in old asic.
    
    [How]
    1. Only mapping dig to pwrseq for new asic.
    2. Move mapping function into dcn specific panel control component
    
    Cc: Stable <stable@vger.kernel.org> # v6.6+
    Cc: Mario Limonciello <mario.limonciello@amd.com>
    Link: https://gitlab.freedesktop.org/drm/amd/-/issues/3122
    Reviewed-by: Anthony Koo <anthony.koo@amd.com>
    Acked-by: Rodrigo Siqueira <rodrigo.siqueira@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Lewis Huang <lewis.huang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn31/dcn31_panel_cntl.c
+++ drivers/gpu/drm/amd/display/dc/dcn31/dcn31_panel_cntl.c
@@ -154,8 +154,24 @@ void dcn31_panel_cntl_construct(
 	struct dcn31_panel_cntl *dcn31_panel_cntl,
 	const struct panel_cntl_init_data *init_data)
 {
+	uint8_t pwrseq_inst = 0xF;
+
 	dcn31_panel_cntl->base.funcs = &dcn31_link_panel_cntl_funcs;
 	dcn31_panel_cntl->base.ctx = init_data->ctx;
 	dcn31_panel_cntl->base.inst = init_data->inst;
-	dcn31_panel_cntl->base.pwrseq_inst = init_data->pwrseq_inst;
+
+	switch (init_data->eng_id) {
+	case ENGINE_ID_DIGA:
+		pwrseq_inst = 0;
+		break;
+	case ENGINE_ID_DIGB:
+		pwrseq_inst = 1;
+		break;
+	default:
+		DC_LOG_WARNING("Unsupported pwrseq engine id: %d!\n", init_data->eng_id);
+		ASSERT(false);
+		break;
+	}
+
+	dcn31_panel_cntl->base.pwrseq_inst = pwrseq_inst;
 }
