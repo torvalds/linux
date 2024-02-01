bed41a344426a407ba5e103b2877a2e560e72229
David Lechner <dlechner@baylibre.com>
2024-02-01 14:12:47 -0600

    wifi: wilc1000: remove setting msg.spi
    Calling spi_sync() unconditionally sets the spi field of struct
    spi_message. Therefore setting msg.spi = spi before calling spi_sync()
    has no effect and can be removed.
    
    (spi_message_add_tail() does not access this field.)
    
    Signed-off-by: David Lechner <dlechner@baylibre.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://msgid.link/20240201201248.2334798-2-dlechner@baylibre.com

--- drivers/net/wireless/microchip/wilc1000/spi.c
+++ drivers/net/wireless/microchip/wilc1000/spi.c
@@ -300,7 +300,6 @@ static int wilc_spi_tx(struct wilc *wilc, u8 *b, u32 len)
 
 		memset(&msg, 0, sizeof(msg));
 		spi_message_init(&msg);
-		msg.spi = spi;
 		spi_message_add_tail(&tr, &msg);
 
 		ret = spi_sync(spi, &msg);
@@ -343,7 +342,6 @@ static int wilc_spi_rx(struct wilc *wilc, u8 *rb, u32 rlen)
 
 		memset(&msg, 0, sizeof(msg));
 		spi_message_init(&msg);
-		msg.spi = spi;
 		spi_message_add_tail(&tr, &msg);
 
 		ret = spi_sync(spi, &msg);
@@ -381,8 +379,6 @@ static int wilc_spi_tx_rx(struct wilc *wilc, u8 *wb, u8 *rb, u32 rlen)
 
 		memset(&msg, 0, sizeof(msg));
 		spi_message_init(&msg);
-		msg.spi = spi;
-
 		spi_message_add_tail(&tr, &msg);
 		ret = spi_sync(spi, &msg);
 		if (ret < 0)
