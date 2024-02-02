9c440cf0c68fa30dc3072b8f1755f2d91863d7d6
Bo Liu <liubo03@inspur.com>
2024-02-02 02:14:52 -0500

    hwmon: (emc1403) convert to use maple tree register cache
    The maple tree register cache is based on a much more modern data structure
    than the rbtree cache and makes optimisation choices which are probably
    more appropriate for modern systems than those made by the rbtree cache.
    
    Signed-off-by: Bo Liu <liubo03@inspur.com>
    Link: https://lore.kernel.org/r/20240202071452.40778-1-liubo03@inspur.com
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/emc1403.c
+++ drivers/hwmon/emc1403.c
@@ -385,7 +385,7 @@ static bool emc1403_regmap_is_volatile(struct device *dev, unsigned int reg)
 static const struct regmap_config emc1403_regmap_config = {
 	.reg_bits = 8,
 	.val_bits = 8,
-	.cache_type = REGCACHE_RBTREE,
+	.cache_type = REGCACHE_MAPLE,
 	.volatile_reg = emc1403_regmap_is_volatile,
 };
 
