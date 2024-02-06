fd4f101edbd9f99567ab2adb1f2169579ede7c13
Eric Dumazet <edumazet@google.com>
2024-02-06 14:42:57 +0000

    net: add exit_batch_rtnl() method
    Many (struct pernet_operations)->exit_batch() methods have
    to acquire rtnl.
    
    In presence of rtnl mutex pressure, this makes cleanup_net()
    very slow.
    
    This patch adds a new exit_batch_rtnl() method to reduce
    number of rtnl acquisitions from cleanup_net().
    
    exit_batch_rtnl() handlers are called while rtnl is locked,
    and devices to be killed can be queued in a list provided
    as their second argument.
    
    A single unregister_netdevice_many() is called right
    before rtnl is released.
    
    exit_batch_rtnl() handlers are called before ->exit() and
    ->exit_batch() handlers.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Antoine Tenart <atenart@kernel.org>
    Link: https://lore.kernel.org/r/20240206144313.2050392-2-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/net/net_namespace.h
+++ include/net/net_namespace.h
@@ -448,6 +448,9 @@ struct pernet_operations {
 	void (*pre_exit)(struct net *net);
 	void (*exit)(struct net *net);
 	void (*exit_batch)(struct list_head *net_exit_list);
+	/* Following method is called with RTNL held. */
+	void (*exit_batch_rtnl)(struct list_head *net_exit_list,
+				struct list_head *dev_kill_list);
 	unsigned int *id;
 	size_t size;
 };
