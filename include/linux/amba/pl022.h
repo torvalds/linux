c42d9bead493854507e1a180942ebe33c9180598
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2024-02-08 21:21:53 +0100

    spi: pl022: Add missing dma_filter field kerneldoc
    Add kerneldoc for dma_filter field in struct pl022_ssp_controller.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Link: https://lore.kernel.org/r/20240208202154.630336-2-krzysztof.kozlowski@linaro.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/linux/amba/pl022.h
+++ include/linux/amba/pl022.h
@@ -225,6 +225,7 @@ struct dma_chan;
  * struct pl022_ssp_master - device.platform_data for SPI controller devices.
  * @bus_id: identifier for this bus
  * @enable_dma: if true enables DMA driven transfers.
+ * @dma_filter: callback filter for dma_request_channel.
  * @dma_rx_param: parameter to locate an RX DMA channel.
  * @dma_tx_param: parameter to locate a TX DMA channel.
  * @autosuspend_delay: delay in ms following transfer completion before the
