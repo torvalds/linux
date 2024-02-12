a6ba1e453174f11e25dd3cdcf289b4b34ddfa28d
Frank Li <Frank.Li@nxp.com>
2024-02-12 11:19:08 -0500

    usb: dwc3: apply snps,host-vbus-glitches workaround unconditionally
    Remove 'snps,host-vbus-glitches-quirk' and apply workaround
    unconditionally.
    
    It is safer to keep vbus disabled before handing over to xhci driver. So
    needn't 'snps,host-vbus-glitches' property to enable it and apply it
    unconditionally.
    
    Remove all host_vbus_glitches variable and call
    dwc3_power_off_all_roothub_ports() directly.
    
    Signed-off-by: Frank Li <Frank.Li@nxp.com>
    Acked-by: Thinh Nguyen <Thinh.Nguyen@synopsys.com>
    Acked-by: Conor Dooley <conor.dooley@microchip.com>
    Link: https://lore.kernel.org/r/20240212-vbus-glitch-v2-2-d71b73a82de1@nxp.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -1133,7 +1133,6 @@ struct dwc3_scratchpad_array {
  *	2	- No de-emphasis
  *	3	- Reserved
  * @dis_metastability_quirk: set to disable metastability quirk.
- * @host_vbus_glitches_quirk: set to avoid vbus glitch during xhci reset.
  * @dis_split_quirk: set to disable split boundary.
  * @wakeup_configured: set if the device is configured for remote wakeup.
  * @suspended: set to track suspend event due to U3/L2.
@@ -1355,7 +1354,6 @@ struct dwc3 {
 	unsigned		tx_de_emphasis:2;
 
 	unsigned		dis_metastability_quirk:1;
-	unsigned		host_vbus_glitches_quirk:1;
 
 	unsigned		dis_split_quirk:1;
 	unsigned		async_callbacks:1;
--- drivers/usb/dwc3/host.c
+++ drivers/usb/dwc3/host.c
@@ -114,8 +114,7 @@ int dwc3_host_init(struct dwc3 *dwc)
 	 * Some platforms need to power off all Root hub ports immediately after DWC3 set to host
 	 * mode to avoid VBUS glitch happen when xhci get reset later.
 	 */
-	if (dwc->host_vbus_glitches_quirk)
-		dwc3_power_off_all_roothub_ports(dwc);
+	dwc3_power_off_all_roothub_ports(dwc);
 
 	irq = dwc3_host_get_irq(dwc);
 	if (irq < 0)
