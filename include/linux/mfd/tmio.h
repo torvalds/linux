ef6dfcbcbbf7d9a414feb44aa093d2d8592a2e20
Wolfram Sang <wsa+renesas@sang-engineering.com>
2024-02-13 23:02:20 +0100

    mfd: tmio: Remove obsolete platform_data
    With commit 8971bb812e3c ("mfd: remove toshiba tmio drivers"), all users
    of platform data for NAND and framebuffers are gone. So, remove
    definitions from the header, too.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://lore.kernel.org/r/20240213220221.2380-9-wsa+renesas@sang-engineering.com
    Signed-off-by: Lee Jones <lee@kernel.org>

--- include/linux/mfd/tmio.h
+++ include/linux/mfd/tmio.h
@@ -103,31 +103,4 @@ struct tmio_mmc_data {
 	void (*set_pwr)(struct platform_device *host, int state);
 	void (*set_clk_div)(struct platform_device *host, int state);
 };
-
-/*
- * data for the NAND controller
- */
-struct tmio_nand_data {
-	struct nand_bbt_descr	*badblock_pattern;
-	struct mtd_partition	*partition;
-	unsigned int		num_partitions;
-	const char *const	*part_parsers;
-};
-
-#define FBIO_TMIO_ACC_WRITE	0x7C639300
-#define FBIO_TMIO_ACC_SYNC	0x7C639301
-
-struct tmio_fb_data {
-	int			(*lcd_set_power)(struct platform_device *fb_dev,
-						 bool on);
-	int			(*lcd_mode)(struct platform_device *fb_dev,
-					    const struct fb_videomode *mode);
-	int			num_modes;
-	struct fb_videomode	*modes;
-
-	/* in mm: size of screen */
-	int			height;
-	int			width;
-};
-
 #endif
