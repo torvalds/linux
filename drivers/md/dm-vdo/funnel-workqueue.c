a958c53af7a5b9297ec0dcaf3689172c34485e35
Mike Snitzer <snitzer@kernel.org>
2024-02-13 13:50:41 -0600

    dm-vdo funnel-workqueue: return VDO_SUCCESS from make_simple_work_queue
    Signed-off-by: Mike Snitzer <snitzer@kernel.org>
    Signed-off-by: Matthew Sakai <msakai@redhat.com>

--- drivers/md/dm-vdo/funnel-workqueue.c
+++ drivers/md/dm-vdo/funnel-workqueue.c
@@ -367,7 +367,7 @@ static int make_simple_work_queue(const char *thread_name_prefix, const char *na
 	wait_for_completion(&started);
 
 	*queue_ptr = queue;
-	return UDS_SUCCESS;
+	return VDO_SUCCESS;
 }
 
 /**
