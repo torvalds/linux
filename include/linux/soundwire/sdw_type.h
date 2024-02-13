81a7d0c4d059cb5c122110acbeec7bedfb91a741
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-13 11:36:56 -0300

    soundwire: bus_type: make sdw_bus_type const
    Since commit d492cc2573a0 ("driver core: device.h: make struct
    bus_type a const *"), the driver core can properly handle constant
    struct bus_type, move the sdw_bus_type variable to be a constant
    structure as well, placing it into read-only memory which can not be
    modified at runtime.
    
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Suggested-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: "Ricardo B. Marliere" <ricardo@marliere.net>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://lore.kernel.org/r/20240213-bus_cleanup-soundwire-v1-1-3878b00f6f57@marliere.net
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- include/linux/soundwire/sdw_type.h
+++ include/linux/soundwire/sdw_type.h
@@ -4,7 +4,7 @@
 #ifndef __SOUNDWIRE_TYPES_H
 #define __SOUNDWIRE_TYPES_H
 
-extern struct bus_type sdw_bus_type;
+extern const struct bus_type sdw_bus_type;
 extern struct device_type sdw_slave_type;
 extern struct device_type sdw_master_type;
 
