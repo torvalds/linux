69b6d71052b54fb10feba68564ccb41c0f0ce1e9
Casey Schaufler <casey@schaufler-ca.com>
2024-02-14 10:47:06 -0800

    Smack: use init_task_smack() in smack_cred_transfer()
    smack_cred_transfer() open codes the same initialization
    as init_task_smack(). Remove the open coding and replace it
    with a call to init_task_smack().
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>

--- security/smack/smack_lsm.c
+++ security/smack/smack_lsm.c
@@ -2101,12 +2101,7 @@ static void smack_cred_transfer(struct cred *new, const struct cred *old)
 	struct task_smack *old_tsp = smack_cred(old);
 	struct task_smack *new_tsp = smack_cred(new);
 
-	new_tsp->smk_task = old_tsp->smk_task;
-	new_tsp->smk_forked = old_tsp->smk_task;
-	mutex_init(&new_tsp->smk_rules_lock);
-	INIT_LIST_HEAD(&new_tsp->smk_rules);
-
-	/* cbs copy rule list */
+	init_task_smack(new_tsp, old_tsp->smk_task, old_tsp->smk_task);
 }
 
 /**
