9f9cd26aec8406bbae42d7d2afe23a5d368b7b9a
Paul Elder <paul.elder@ideasonboard.com>
2024-02-16 18:54:58 +0900

    media: rkisp1: Add match data for i.MX8MP ISP
    Add match data to the rkisp1 driver to match the i.MX8MP ISP.
    
    Signed-off-by: Paul Elder <paul.elder@ideasonboard.com>
    Reviewed-by: Rob Herring <robh@kernel.org>
    Tested-by: Alexander Stein <alexander.stein@ew.tq-group.com>
    Tested-by: Adam Ford <aford173@gmail.com>
    Reviewed-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/media/platform/rockchip/rkisp1/rkisp1-dev.c
+++ drivers/media/platform/rockchip/rkisp1/rkisp1-dev.c
@@ -511,6 +511,26 @@ static const struct rkisp1_info rk3399_isp_info = {
 		  | RKISP1_FEATURE_DUAL_CROP,
 };
 
+static const char * const imx8mp_isp_clks[] = {
+	"isp",
+	"hclk",
+	"aclk",
+};
+
+static const struct rkisp1_isr_data imx8mp_isp_isrs[] = {
+	{ NULL, rkisp1_isr, BIT(RKISP1_IRQ_ISP) | BIT(RKISP1_IRQ_MI) },
+};
+
+static const struct rkisp1_info imx8mp_isp_info = {
+	.clks = imx8mp_isp_clks,
+	.clk_size = ARRAY_SIZE(imx8mp_isp_clks),
+	.isrs = imx8mp_isp_isrs,
+	.isr_size = ARRAY_SIZE(imx8mp_isp_isrs),
+	.isp_ver = RKISP1_V_IMX8MP,
+	.features = RKISP1_FEATURE_MAIN_STRIDE
+		  | RKISP1_FEATURE_DMA_34BIT,
+};
+
 static const struct of_device_id rkisp1_of_match[] = {
 	{
 		.compatible = "rockchip,px30-cif-isp",
@@ -519,6 +539,10 @@ static const struct of_device_id rkisp1_of_match[] = {
 	{
 		.compatible = "rockchip,rk3399-cif-isp",
 		.data = &rk3399_isp_info,
+	},
+	{
+		.compatible = "fsl,imx8mp-isp",
+		.data = &imx8mp_isp_info,
 	},
 	{},
 };
