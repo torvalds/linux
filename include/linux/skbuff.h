219eee9c0d16f1b754a8b85275854ab17df0850a
Florian Westphal <fw@strlen.de>
2024-02-16 12:36:57 +0100

    net: skbuff: add overflow debug check to pull/push helpers
    syzbot managed to trigger following splat:
    BUG: KASAN: use-after-free in __skb_flow_dissect+0x4a3b/0x5e50
    Read of size 1 at addr ffff888208a4000e by task a.out/2313
    [..]
      __skb_flow_dissect+0x4a3b/0x5e50
      __skb_get_hash+0xb4/0x400
      ip_tunnel_xmit+0x77e/0x26f0
      ipip_tunnel_xmit+0x298/0x410
      ..
    
    Analysis shows that the skb has a valid ->head, but bogus ->data
    pointer.
    
    skb->data gets its bogus value via the neigh layer, which does:
    
    1556    __skb_pull(skb, skb_network_offset(skb));
    
    ... and the skb was already dodgy at this point:
    
    skb_network_offset(skb) returns a negative value due to an
    earlier overflow of skb->network_header (u16).  __skb_pull thus
    "adjusts" skb->data by a huge offset, pointing outside skb->head
    area.
    
    Allow debug builds to splat when we try to pull/push more than
    INT_MAX bytes.
    
    After this, the syzkaller reproducer yields a more precise splat
    before the flow dissector attempts to read off skb->data memory:
    
    WARNING: CPU: 5 PID: 2313 at include/linux/skbuff.h:2653 neigh_connected_output+0x28e/0x400
      ip_finish_output2+0xb25/0xed0
      iptunnel_xmit+0x4ff/0x870
      ipgre_xmit+0x78e/0xbb0
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://lore.kernel.org/r/20240216113700.23013-1-fw@strlen.de
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -2684,6 +2684,8 @@ static inline void skb_put_u8(struct sk_buff *skb, u8 val)
 void *skb_push(struct sk_buff *skb, unsigned int len);
 static inline void *__skb_push(struct sk_buff *skb, unsigned int len)
 {
+	DEBUG_NET_WARN_ON_ONCE(len > INT_MAX);
+
 	skb->data -= len;
 	skb->len  += len;
 	return skb->data;
@@ -2692,6 +2694,8 @@ static inline void *__skb_push(struct sk_buff *skb, unsigned int len)
 void *skb_pull(struct sk_buff *skb, unsigned int len);
 static inline void *__skb_pull(struct sk_buff *skb, unsigned int len)
 {
+	DEBUG_NET_WARN_ON_ONCE(len > INT_MAX);
+
 	skb->len -= len;
 	if (unlikely(skb->len < skb->data_len)) {
 #if defined(CONFIG_DEBUG_NET)
@@ -2716,6 +2720,8 @@ void *__pskb_pull_tail(struct sk_buff *skb, int delta);
 static inline enum skb_drop_reason
 pskb_may_pull_reason(struct sk_buff *skb, unsigned int len)
 {
+	DEBUG_NET_WARN_ON_ONCE(len > INT_MAX);
+
 	if (likely(len <= skb_headlen(skb)))
 		return SKB_NOT_DROPPED_YET;
 
