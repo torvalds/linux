30e920e1debb437e5aea7a4ccdab61634354297a
Ankit Agrawal <ankita@nvidia.com>
2024-02-20 17:20:54 +0530

    vfio/pci: rename and export range_intersect_range
    range_intersect_range determines an overlap between two ranges. If an
    overlap, the helper function returns the overlapping offset and size.
    
    The VFIO PCI variant driver emulates the PCI config space BAR offset
    registers. These offset may be accessed for read/write with a variety
    of lengths including sub-word sizes from sub-word offsets. The driver
    makes use of this helper function to read/write the targeted part of
    the emulated register.
    
    Make this a vfio_pci_core function, rename and export as GPL. Also
    update references in virtio driver.
    
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Reviewed-by: Yishai Hadas <yishaih@nvidia.com>
    Signed-off-by: Ankit Agrawal <ankita@nvidia.com>
    Link: https://lore.kernel.org/r/20240220115055.23546-3-ankita@nvidia.com
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

--- include/linux/vfio_pci_core.h
+++ include/linux/vfio_pci_core.h
@@ -134,6 +134,11 @@ ssize_t vfio_pci_core_do_io_rw(struct vfio_pci_core_device *vdev, bool test_mem,
 			       void __iomem *io, char __user *buf,
 			       loff_t off, size_t count, size_t x_start,
 			       size_t x_end, bool iswrite);
+bool vfio_pci_core_range_intersect_range(loff_t buf_start, size_t buf_cnt,
+					 loff_t reg_start, size_t reg_cnt,
+					 loff_t *buf_offset,
+					 size_t *intersect_count,
+					 size_t *register_offset);
 #define VFIO_IOWRITE_DECLATION(size) \
 int vfio_pci_core_iowrite##size(struct vfio_pci_core_device *vdev,	\
 			bool test_mem, u##size val, void __iomem *io);
