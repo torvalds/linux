4bd3e126b2793313a5b85af5aefe51fefb1e205e
Kunwu Chan <chentao@kylinos.cn>
2024-02-20 17:06:41 +0800

    btrfs: use KMEM_CACHE() to create btrfs_ordered_extent cache
    Use the KMEM_CACHE() macro instead of kmem_cache_create() to simplify
    the creation of SLAB caches when the default values are used.
    
    Signed-off-by: Kunwu Chan <chentao@kylinos.cn>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/ordered-data.c
+++ fs/btrfs/ordered-data.c
@@ -1235,10 +1235,7 @@ struct btrfs_ordered_extent *btrfs_split_ordered_extent(
 
 int __init ordered_data_init(void)
 {
-	btrfs_ordered_extent_cache = kmem_cache_create("btrfs_ordered_extent",
-				     sizeof(struct btrfs_ordered_extent), 0,
-				     SLAB_MEM_SPREAD,
-				     NULL);
+	btrfs_ordered_extent_cache = KMEM_CACHE(btrfs_ordered_extent, SLAB_MEM_SPREAD);
 	if (!btrfs_ordered_extent_cache)
 		return -ENOMEM;
 
