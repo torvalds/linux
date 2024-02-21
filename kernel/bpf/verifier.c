55bad79e33aeb670317290158a4b2ff71cdc8380
Benjamin Tissoires <bentiss@kernel.org>
2024-02-21 17:25:17 +0100

    bpf: allow more maps in sleepable bpf programs
    These 2 maps types are required for HID-BPF when a user wants to do
    IO with a device from a sleepable tracing point.
    
    Allowing BPF_MAP_TYPE_QUEUE (and therefore BPF_MAP_TYPE_STACK) allows
    for a BPF program to prepare from an IRQ the list of HID commands to send
    back to the device and then these commands can be retrieved from the
    sleepable trace point.
    
    Signed-off-by: Benjamin Tissoires <bentiss@kernel.org>
    Link: https://lore.kernel.org/r/20240221-hid-bpf-sleepable-v3-1-1fb378ca6301@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -18022,6 +18022,8 @@ static int check_map_prog_compatibility(struct bpf_verifier_env *env,
 		case BPF_MAP_TYPE_SK_STORAGE:
 		case BPF_MAP_TYPE_TASK_STORAGE:
 		case BPF_MAP_TYPE_CGRP_STORAGE:
+		case BPF_MAP_TYPE_QUEUE:
+		case BPF_MAP_TYPE_STACK:
 			break;
 		default:
 			verbose(env,
