bd5ed02e23958cb56d0f5a90ebe620c8b47dab47
Gang Li <gang.li@linux.dev>
2024-02-22 22:04:18 +0800

    padata: downgrade padata_do_multithreaded to serial execution for non-SMP
    hugetlb parallelization depends on PADATA, and PADATA depends on SMP.
    
    PADATA consists of two distinct functionality: One part is
    padata_do_multithreaded which disregards order and simply divides tasks
    into several groups for parallel execution.  Hugetlb init parallelization
    depends on padata_do_multithreaded.
    
    The other part is composed of a set of APIs that, while handling data in
    an out-of-order parallel manner, can eventually return the data with
    ordered sequence.  Currently Only `crypto/pcrypt.c` use them.
    
    All users of PADATA of non-SMP case currently only use
    padata_do_multithreaded.  It is easy to implement a serial one in
    include/linux/padata.h.  And it is not necessary to implement another
    functionality unless the only user of crypto/pcrypt.c does not depend on
    SMP in the future.
    
    Link: https://lkml.kernel.org/r/20240222140422.393911-6-gang.li@linux.dev
    Signed-off-by: Gang Li <ligang.bdlg@bytedance.com>
    Tested-by: Paul E. McKenney <paulmck@kernel.org>
    Acked-by: Daniel Jordan <daniel.m.jordan@oracle.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Jane Chu <jane.chu@oracle.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Steffen Klassert <steffen.klassert@secunet.com>
    Cc: Tim Chen <tim.c.chen@linux.intel.com>
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/padata.h
+++ include/linux/padata.h
@@ -180,10 +180,6 @@ struct padata_instance {
 
 #ifdef CONFIG_PADATA
 extern void __init padata_init(void);
-#else
-static inline void __init padata_init(void) {}
-#endif
-
 extern struct padata_instance *padata_alloc(const char *name);
 extern void padata_free(struct padata_instance *pinst);
 extern struct padata_shell *padata_alloc_shell(struct padata_instance *pinst);
@@ -194,4 +190,12 @@ extern void padata_do_serial(struct padata_priv *padata);
 extern void __init padata_do_multithreaded(struct padata_mt_job *job);
 extern int padata_set_cpumask(struct padata_instance *pinst, int cpumask_type,
 			      cpumask_var_t cpumask);
+#else
+static inline void __init padata_init(void) {}
+static inline void __init padata_do_multithreaded(struct padata_mt_job *job)
+{
+	job->thread_fn(job->start, job->start + job->size, job->fn_arg);
+}
+#endif
+
 #endif
