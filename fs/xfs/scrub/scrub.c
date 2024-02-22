4ed080cd7cb077bbb4b64f0712be1618c9d55a0d
Darrick J. Wong <djwong@kernel.org>
2024-02-22 12:33:05 -0800

    xfs: repair summary counters
    Use the same summary counter calculation infrastructure to generate new
    values for the in-core summary counters.   The difference between the
    scrubber and the repairer is that the repairer will freeze the fs during
    setup, which means that the values should match exactly.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- fs/xfs/scrub/scrub.c
+++ fs/xfs/scrub/scrub.c
@@ -364,7 +364,7 @@ static const struct xchk_meta_ops meta_scrub_ops[] = {
 		.type	= ST_FS,
 		.setup	= xchk_setup_fscounters,
 		.scrub	= xchk_fscounters,
-		.repair	= xrep_notsupported,
+		.repair	= xrep_fscounters,
 	},
 	[XFS_SCRUB_TYPE_QUOTACHECK] = {	/* quota counters */
 		.type	= ST_FS,
