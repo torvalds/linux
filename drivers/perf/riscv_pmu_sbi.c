ea0e0178e101c8d4662a0db7424df057b88e2712
Yu Chien Peter Lin <peterlin@andestech.com>
2024-02-22 16:39:42 +0800

    perf: RISC-V: Eliminate redundant interrupt enable/disable operations
    The interrupt enable/disable operations are already performed by the
    IRQ chip functions riscv_intc_irq_unmask()/riscv_intc_irq_mask() during
    enable_percpu_irq()/disable_percpu_irq(). It can be done only once.
    
    Signed-off-by: Yu Chien Peter Lin <peterlin@andestech.com>
    Reviewed-by: Atish Patra <atishp@rivosinc.com>
    Link: https://lore.kernel.org/r/20240222083946.3977135-7-peterlin@andestech.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>

--- drivers/perf/riscv_pmu_sbi.c
+++ drivers/perf/riscv_pmu_sbi.c
@@ -781,7 +781,6 @@ static int pmu_sbi_starting_cpu(unsigned int cpu, struct hlist_node *node)
 	if (riscv_pmu_use_irq) {
 		cpu_hw_evt->irq = riscv_pmu_irq;
 		csr_clear(CSR_IP, BIT(riscv_pmu_irq_num));
-		csr_set(CSR_IE, BIT(riscv_pmu_irq_num));
 		enable_percpu_irq(riscv_pmu_irq, IRQ_TYPE_NONE);
 	}
 
@@ -792,7 +791,6 @@ static int pmu_sbi_dying_cpu(unsigned int cpu, struct hlist_node *node)
 {
 	if (riscv_pmu_use_irq) {
 		disable_percpu_irq(riscv_pmu_irq);
-		csr_clear(CSR_IE, BIT(riscv_pmu_irq_num));
 	}
 
 	/* Disable all counters access for user mode now */
