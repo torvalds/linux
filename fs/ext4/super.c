708623737b0a28aff33bff0237862a2e08bc5c97
Chengming Zhou <zhouchengming@bytedance.com>
2024-02-24 13:48:22 +0000

    ext4: remove SLAB_MEM_SPREAD flag usage
    The SLAB_MEM_SPREAD flag used to be implemented in SLAB, which was
    removed as of v6.8-rc1, so it became a dead flag since the commit
    16a1d968358a ("mm/slab: remove mm/slab.c and slab_def.h"). And the
    series[1] went on to mark it obsolete to avoid confusion for users.
    Here we can just remove all its users, which has no functional change.
    
    [1] https://lore.kernel.org/all/20240223-slab-cleanup-flags-v2-1-02f1753e8303@suse.cz/
    
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>
    Link: https://lore.kernel.org/r/20240224134822.829456-1-chengming.zhou@linux.dev
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -1500,8 +1500,7 @@ static int __init init_inodecache(void)
 {
 	ext4_inode_cachep = kmem_cache_create_usercopy("ext4_inode_cache",
 				sizeof(struct ext4_inode_info), 0,
-				(SLAB_RECLAIM_ACCOUNT|SLAB_MEM_SPREAD|
-					SLAB_ACCOUNT),
+				SLAB_RECLAIM_ACCOUNT|SLAB_ACCOUNT,
 				offsetof(struct ext4_inode_info, i_data),
 				sizeof_field(struct ext4_inode_info, i_data),
 				init_once);
