2a95fd3cc23d06bf2a86f30c3ca983b170dcc665
Chengming Zhou <zhouchengming@bytedance.com>
2024-02-24 13:49:25 +0000

    jfs: remove SLAB_MEM_SPREAD flag usage
    The SLAB_MEM_SPREAD flag used to be implemented in SLAB, which was
    removed as of v6.8-rc1 (see [1]), so it became a dead flag since the
    commit 16a1d968358a ("mm/slab: remove mm/slab.c and slab_def.h"). And
    the series[1] went on to mark it obsolete explicitly to avoid confusion
    for users. Here we can just remove all its users, which has no any
    functional change.
    
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>
    Link: https://lore.kernel.org/all/20240223-slab-cleanup-flags-v2-1-02f1753e8303@suse.cz [1]
    Link: https://lore.kernel.org/r/20240224134925.829677-1-chengming.zhou@linux.dev
    Acked-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- fs/jfs/super.c
+++ fs/jfs/super.c
@@ -932,7 +932,7 @@ static int __init init_jfs_fs(void)
 
 	jfs_inode_cachep =
 	    kmem_cache_create_usercopy("jfs_ip", sizeof(struct jfs_inode_info),
-			0, SLAB_RECLAIM_ACCOUNT|SLAB_MEM_SPREAD|SLAB_ACCOUNT,
+			0, SLAB_RECLAIM_ACCOUNT|SLAB_ACCOUNT,
 			offsetof(struct jfs_inode_info, i_inline_all),
 			sizeof_field(struct jfs_inode_info, i_inline_all),
 			init_once);
