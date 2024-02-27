4c8a49854130da0117a0fdb858551824919a2389
Ingo Molnar <mingo@kernel.org>
2024-02-27 09:58:15 +0100

    smp: Avoid 'setup_max_cpus' namespace collision/shadowing
    bringup_nonboot_cpus() gets passed the 'setup_max_cpus'
    variable in init/main.c - which is also the name of the parameter,
    shadowing the name.
    
    To reduce confusion and to allow the 'setup_max_cpus' value
    to be #defined in the <linux/smp.h> header, use the 'max_cpus'
    name for the function parameter name.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -112,7 +112,7 @@ void notify_cpu_starting(unsigned int cpu);
 extern void cpu_maps_update_begin(void);
 extern void cpu_maps_update_done(void);
 int bringup_hibernate_cpu(unsigned int sleep_cpu);
-void bringup_nonboot_cpus(unsigned int setup_max_cpus);
+void bringup_nonboot_cpus(unsigned int max_cpus);
 
 #else	/* CONFIG_SMP */
 #define cpuhp_tasks_frozen	0
