4b8d18c0c986877fe89ade2214e1e81ad817cef1
Lu Baolu <baolu.lu@linux.intel.com>
2024-02-27 10:14:34 +0800

    iommu/vt-d: Remove INTEL_IOMMU_BROKEN_GFX_WA
    Commit 62edf5dc4a524 ("intel-iommu: Restore DMAR_BROKEN_GFX_WA option for
    broken graphics drivers") was introduced 24 years ago as a temporary
    workaround for graphics drivers that used physical addresses for DMA and
    avoided DMA APIs. This workaround was disabled by default.
    
    As 24 years have passed, it is expected that graphics driver developers
    have migrated their drivers to use kernel DMA APIs. Therefore, this
    workaround is no longer required and could been removed.
    
    The Intel iommu driver also provides a "igfx_off" option to turn off
    the DMA translation for the graphic dedicated IOMMU. Hence, there is
    really no good reason to keep this config option.
    
    Suggested-by: Kevin Tian <kevin.tian@intel.com>
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Link: https://lore.kernel.org/r/20240130060823.57990-1-baolu.lu@linux.intel.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -2660,10 +2660,6 @@ static int __init init_dmars(void)
 		iommu_set_root_entry(iommu);
 	}
 
-#ifdef CONFIG_INTEL_IOMMU_BROKEN_GFX_WA
-	dmar_map_gfx = 0;
-#endif
-
 	if (!dmar_map_gfx)
 		iommu_identity_mapping |= IDENTMAP_GFX;
 
