a83a7728e4871b69e40ccc255e075700820c87bf
Takashi Iwai <tiwai@suse.de>
2024-02-27 11:29:14 +0100

    Bluetooth: btmtk: Add MODULE_FIRMWARE() for MT7922
    [ Upstream commit 3e465a07cdf444140f16bc57025c23fcafdde997 ]
    
    Since dracut refers to the module info for defining the required
    firmware files and btmtk driver doesn't provide the firmware info for
    MT7922, the generate initrd misses the firmware, resulting in the
    broken Bluetooth.
    
    This patch simply adds the MODULE_FIRMWARE() for the missing entry
    for covering that.
    
    Link: https://bugzilla.suse.com/show_bug.cgi?id=1214133
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Reviewed-by: Paul Menzel <pmenzel@molgen.mpg.de>
    Reviewed-by: Matthias Brugger <matthias.bgg@gmail.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/bluetooth/btmtk.h
+++ drivers/bluetooth/btmtk.h
@@ -4,6 +4,7 @@
 #define FIRMWARE_MT7622		"mediatek/mt7622pr2h.bin"
 #define FIRMWARE_MT7663		"mediatek/mt7663pr2h.bin"
 #define FIRMWARE_MT7668		"mediatek/mt7668pr2h.bin"
+#define FIRMWARE_MT7922		"mediatek/BT_RAM_CODE_MT7922_1_1_hdr.bin"
 #define FIRMWARE_MT7961		"mediatek/BT_RAM_CODE_MT7961_1_2_hdr.bin"
 
 #define HCI_EV_WMT 0xe4
