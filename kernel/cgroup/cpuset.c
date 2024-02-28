66f40b926dd249f74334a22162c09e7ec1ec5b07
Waiman Long <longman@redhat.com>
2024-02-27 19:58:01 -0500

    cgroup/cpuset: Fix a memory leak in update_exclusive_cpumask()
    Fix a possible memory leak in update_exclusive_cpumask() by moving the
    alloc_cpumasks() down after the validate_change() check which can fail
    and still before the temporary cpumasks are needed.
    
    Fixes: e2ffe502ba45 ("cgroup/cpuset: Add cpuset.cpus.exclusive for v2")
    Reported-and-tested-by: Mirsad Todorovac <mirsad.todorovac@alu.hr>
    Closes: https://lore.kernel.org/lkml/14915689-27a3-4cd8-80d2-9c30d0c768b6@alu.unizg.hr
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: stable@vger.kernel.org # v6.7+

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2598,9 +2598,6 @@ static int update_exclusive_cpumask(struct cpuset *cs, struct cpuset *trialcs,
 	if (cpumask_equal(cs->exclusive_cpus, trialcs->exclusive_cpus))
 		return 0;
 
-	if (alloc_cpumasks(NULL, &tmp))
-		return -ENOMEM;
-
 	if (*buf)
 		compute_effective_exclusive_cpumask(trialcs, NULL);
 
@@ -2614,6 +2611,9 @@ static int update_exclusive_cpumask(struct cpuset *cs, struct cpuset *trialcs,
 	retval = validate_change(cs, trialcs);
 	if (retval)
 		return retval;
+
+	if (alloc_cpumasks(NULL, &tmp))
+		return -ENOMEM;
 
 	if (old_prs) {
 		if (cpumask_empty(trialcs->effective_xcpus)) {
