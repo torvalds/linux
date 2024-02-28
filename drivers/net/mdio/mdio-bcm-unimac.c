9112fc0109fc0037ac3b8b633a169e78b4e23ca1
Justin Chen <justin.chen@broadcom.com>
2024-02-28 14:53:58 -0800

    net: phy: mdio-bcm-unimac: Add asp v2.2 support
    Add mdio compat string for ASP 2.0 ethernet driver.
    
    Signed-off-by: Justin Chen <justin.chen@broadcom.com>
    Acked-by: Florian Fainelli <florian.fainelli@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/mdio/mdio-bcm-unimac.c
+++ drivers/net/mdio/mdio-bcm-unimac.c
@@ -334,6 +334,7 @@ static SIMPLE_DEV_PM_OPS(unimac_mdio_pm_ops,
 			 NULL, unimac_mdio_resume);
 
 static const struct of_device_id unimac_mdio_ids[] = {
+	{ .compatible = "brcm,asp-v2.2-mdio", },
 	{ .compatible = "brcm,asp-v2.1-mdio", },
 	{ .compatible = "brcm,asp-v2.0-mdio", },
 	{ .compatible = "brcm,genet-mdio-v5", },
