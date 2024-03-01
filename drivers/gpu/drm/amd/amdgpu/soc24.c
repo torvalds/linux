81b09cedb37bc7aa80712361a60f1396c8c3d0a0
Kenneth Feng <kenneth.feng@amd.com>
2024-03-01 11:09:22 +0800

    drm/amd/amdgpu: enable cgcg and cgls
    enable cgcg and cgls on gc 12.0.1
    
    Signed-off-by: Kenneth Feng <kenneth.feng@amd.com>
    Reviewed-by: Likun Gao <Likun.Gao@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/soc24.c
+++ drivers/gpu/drm/amd/amdgpu/soc24.c
@@ -401,7 +401,8 @@ static int soc24_common_early_init(void *handle)
 		adev->external_rev_id = adev->rev_id + 0x40;
 		break;
 	case IP_VERSION(12, 0, 1):
-		adev->cg_flags = 0;
+		adev->cg_flags = AMD_CG_SUPPORT_GFX_CGCG |
+			AMD_CG_SUPPORT_GFX_CGLS;
 		adev->pg_flags = AMD_PG_SUPPORT_VCN |
 			AMD_PG_SUPPORT_JPEG |
 			AMD_PG_SUPPORT_VCN_DPG;
