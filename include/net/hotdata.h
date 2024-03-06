6a55ca6b0122d4678e3ab54a8553361aae5082f1
Eric Dumazet <edumazet@google.com>
2024-03-06 16:00:24 +0000

    udp: move udpv4_offload and udpv6_offload to net_hotdata
    These structures are used in GRO and GSO paths.
    Move them to net_hodata for better cache locality.
    
    v2: udpv6_offload definition depends on CONFIG_INET=y
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Soheil Hassas Yeganeh <soheil@google.com>
    Reviewed-by: David Ahern <dsahern@kernel.org>
    Link: https://lore.kernel.org/r/20240306160031.874438-12-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/net/hotdata.h
+++ include/net/hotdata.h
@@ -11,8 +11,10 @@ struct net_hotdata {
 #if IS_ENABLED(CONFIG_INET)
 	struct packet_offload	ip_packet_offload;
 	struct net_offload	tcpv4_offload;
+	struct net_offload 	udpv4_offload;
 	struct packet_offload	ipv6_packet_offload;
 	struct net_offload	tcpv6_offload;
+	struct net_offload	udpv6_offload;
 #endif
 	struct list_head	offload_base;
 	struct list_head	ptype_all;
