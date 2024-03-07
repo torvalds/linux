1bcf7f48b7d40547eda6bfd1ea0bfed9cf9ec54f
Heiko Carstens <hca@linux.ibm.com>
2024-03-07 13:28:06 +0100

    s390/cio: use bitwise types to allow for type checking
    Change types of I/O structure members which contain physical addresses to
    dma32_t and dma64_t bitwise types.
    
    This allows to make use of sparse (aka "make C=1") to find incorrect usage
    of physical addresses.
    
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- drivers/s390/cio/chsc.h
+++ drivers/s390/cio/chsc.h
@@ -91,8 +91,8 @@ struct chsc_scssc_area {
 	u16:16;
 	u32:32;
 	u32:32;
-	u64 summary_indicator_addr;
-	u64 subchannel_indicator_addr;
+	dma64_t summary_indicator_addr;
+	dma64_t subchannel_indicator_addr;
 	u32 ks:4;
 	u32 kc:4;
 	u32:21;
