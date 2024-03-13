0b1d327a1aef446af24d44b99475a4c0135c9431
Madhu Ananthula <quic_mananthu@quicinc.com>
2024-03-13 11:05:13 +0530

    arm-smmu: change supend retry option in prepare callback
    In case of pm runtime suspend is in progress, then kgsl smmu
    system suspend would go for retry by returing -EINPROGRESS.
    but this is aborting the complete suspend for a client,
    which doesn't want to change their user space to handle reattempt.
    
    to fix this by calling pm_runtime_put_sync_suspend in pm_prepare,
    we ensure that rpm_suspend had been executed before suspend.
    since call is synchronous, frozen workqueue is not used
    and does not prevent execution. changed this return value to -EAGAIN
    to retry the prepare again to make pm runtime suspended correctly.
    
    Change-Id: Id602051aaebcc3ddbd16a3e496e8b7f512679f3a
    Signed-off-by: Madhu Ananthula <quic_mananthu@quicinc.com>
    Signed-off-by: Srinivasarao Pathipati <quic_c_spathi@quicinc.com>

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -14,7 +14,7 @@
  *	- Context fault reporting
  *	- Extended Stream ID (16 bit)
  *
- * Copyright (c) 2021-2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #define pr_fmt(fmt) "arm-smmu: " fmt
@@ -3807,6 +3807,8 @@ static int __maybe_unused arm_smmu_pm_resume_common(struct device *dev)
 
 static int arm_smmu_pm_prepare(struct device *dev)
 {
+	int ret = 0;
+
 	if (!of_device_is_compatible(dev->of_node, "qcom,adreno-smmu"))
 		return 0;
 
@@ -3815,14 +3817,25 @@ static int arm_smmu_pm_prepare(struct device *dev)
 	 * cause a deadlock where cx vote is never put down causing timeout. So,
 	 * abort system suspend here if dev->power.usage_count is 1 as this indicates
 	 * rpm_suspend is in progress and prepare is the one incrementing this counter.
-	 * Now rpm_suspend can continue and put down cx vote. System suspend will resume
-	 * later and complete.
+	 * Now pm runtime put sync suspend will complete the rpm suspend and system
+	 * suspend will resume later and complete.
+	 * in case if runtime still not suspended after sync suspend also then will
+	 * retry with EGAIN by incrementing the usage count to avoid the under flow.
 	 */
 	if (pm_runtime_suspended(dev))
 		return 0;
 
-	return (atomic_read(&dev->power.usage_count) == 1) ? -EINPROGRESS : 0;
+	if (atomic_read(&dev->power.usage_count) == 1) {
+		ret = pm_runtime_put_sync_suspend(dev);
+		if (ret < 0) {
+			dev_err(dev, "sync supend failed to suspend the rpm\n");
+			pm_runtime_get_noresume(dev);
+			return -EAGAIN;
+		}
+	}
+	return 0;
 }
+
 static int __maybe_unused arm_smmu_pm_restore_early(struct device *dev)
 {
 	struct arm_smmu_device *smmu = dev_get_drvdata(dev);
