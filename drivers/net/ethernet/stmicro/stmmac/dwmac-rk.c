19c5c04d3178a8a4ca803369aa4948dad1ece902
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2024-03-13 16:00:57 +0200

    net: stmmac: dwmac-rk: Remove unused of_gpio.h
    of_gpio.h is deprecated and subject to remove.
    The driver doesn't use it, simply remove the unused header.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Sebastian Reichel <sebastian.reichel@collabora.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-rk.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-rk.c
@@ -12,10 +12,8 @@
 #include <linux/clk.h>
 #include <linux/phy.h>
 #include <linux/of_net.h>
-#include <linux/gpio.h>
 #include <linux/module.h>
 #include <linux/of.h>
-#include <linux/of_gpio.h>
 #include <linux/platform_device.h>
 #include <linux/regulator/consumer.h>
 #include <linux/delay.h>
