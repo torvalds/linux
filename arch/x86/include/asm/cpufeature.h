d689863c1a60b9936b47a34fa5c3330de374f4fc
Uros Bizjak <ubizjak@gmail.com>
2024-03-19 11:40:14 +0100

    x86/asm: Use %a instead of %P operand modifier in asm templates
    The "P" asm operand modifier is a x86 target-specific modifier.
    
    For x86_64, when used with a symbol reference, the "%P" modifier
    emits "sym" instead of "sym(%rip)". This property is currently
    used to issue bare symbol reference.
    
    The generic "a" operand modifier should be used instead. The "a"
    asm operand modifier substitutes a memory reference, with the
    actual operand treated as address.  For x86_64, when a symbol is
    provided, the "a" modifier emits "sym(%rip)" instead of "sym",
    enabling shorter %rip-relative addressing.
    
    Also note that unlike GCC, clang emits %rip-relative symbol
    reference with "P" asm operand modifier, so the patch also unifies
    symbol handling with both compilers.
    
    No functional changes intended.
    
    Signed-off-by: Uros Bizjak <ubizjak@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Link: https://lore.kernel.org/r/20240319104418.284519-4-ubizjak@gmail.com

--- arch/x86/include/asm/cpufeature.h
+++ arch/x86/include/asm/cpufeature.h
@@ -172,7 +172,7 @@ static __always_inline bool _static_cpu_has(u16 bit)
 		ALTERNATIVE_TERNARY("jmp 6f", %c[feature], "", "jmp %l[t_no]")
 		".pushsection .altinstr_aux,\"ax\"\n"
 		"6:\n"
-		" testb %[bitnum]," _ASM_RIP(%P[cap_byte]) "\n"
+		" testb %[bitnum], %a[cap_byte]\n"
 		" jnz %l[t_yes]\n"
 		" jmp %l[t_no]\n"
 		".popsection\n"
