9a7bb61ffe467034571959ce90509158d4bd00bd
Thomas Zimmermann <tzimmermann@suse.de>
2024-03-19 10:37:24 +0100

    staging: fbtft: Remove reference to fb_blank
    The field fb_blank in struct backlight_properties is deprecated and
    should not be used. Don't output its value in the driver's debug print.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Dan Carpenter <dan.carpenter@linaro.org>
    Link: https://lore.kernel.org/r/20240319093915.31778-6-tzimmermann@suse.de
    Signed-off-by: Lee Jones <lee@kernel.org>

--- drivers/staging/fbtft/fb_ssd1351.c
+++ drivers/staging/fbtft/fb_ssd1351.c
@@ -194,9 +194,7 @@ static int update_onboard_backlight(struct backlight_device *bd)
 	struct fbtft_par *par = bl_get_data(bd);
 	bool on;
 
-	fbtft_par_dbg(DEBUG_BACKLIGHT, par,
-		      "%s: power=%d, fb_blank=%d\n",
-		      __func__, bd->props.power, bd->props.fb_blank);
+	fbtft_par_dbg(DEBUG_BACKLIGHT, par, "%s: power=%d\n", __func__, bd->props.power);
 
 	on = !backlight_is_blank(bd);
 	/* Onboard backlight connected to GPIO0 on SSD1351, GPIO1 unused */
