c2c0de23f91b2cad0f1e35cbd8f3d5322e0f39fb
Ching-Te Ku <ku920601@realtek.com>
2024-03-20 15:50:47 +0800

    wifi: rtw88: coex: Prevent doing I/O during Wi-Fi power saving
    Fix Wi-Fi 2.4Ghz throughput drop over than 40% when Bluetooh is idle.
    The code flow will read registers during Wi-Fi power saving, and be
    returned, which results in incorrect counters to do mechanism judgment.
    Adjust the code flow. Will leave Wi-Fi power save mode first then update
    counters.
    
    Signed-off-by: Ching-Te Ku <ku920601@realtek.com>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://msgid.link/20240320075047.31810-1-pkshih@realtek.com

--- drivers/net/wireless/realtek/rtw88/main.c
+++ drivers/net/wireless/realtek/rtw88/main.c
@@ -227,9 +227,6 @@ static void rtw_watch_dog_work(struct work_struct *work)
 	else
 		clear_bit(RTW_FLAG_BUSY_TRAFFIC, rtwdev->flags);
 
-	rtw_coex_wl_status_check(rtwdev);
-	rtw_coex_query_bt_hid_list(rtwdev);
-
 	if (busy_traffic != test_bit(RTW_FLAG_BUSY_TRAFFIC, rtwdev->flags))
 		rtw_coex_wl_status_change_notify(rtwdev, 0);
 
@@ -257,6 +254,8 @@ static void rtw_watch_dog_work(struct work_struct *work)
 
 	/* make sure BB/RF is working for dynamic mech */
 	rtw_leave_lps(rtwdev);
+	rtw_coex_wl_status_check(rtwdev);
+	rtw_coex_query_bt_hid_list(rtwdev);
 
 	rtw_phy_dynamic_mechanism(rtwdev);
 
