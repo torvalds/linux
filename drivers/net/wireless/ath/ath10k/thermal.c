fa1a4f15bdcabb9ec8fd7d78259ef06fbb5390bc
Li Zhijian <lizhijian@fujitsu.com>
2024-03-21 13:42:47 +0200

    wifi: ath: Convert sprintf/snprintf to sysfs_emit
    Per filesystems/sysfs.rst, show() should only use sysfs_emit()
    or sysfs_emit_at() when formatting the value to be returned to user space.
    
    coccinelle complains that there are still a couple of functions that use
    snprintf(). Convert them to sysfs_emit().
    
    sprintf() will be converted as weel if they have.
    
    Generally, this patch is generated by
    make coccicheck M=<path/to/file> MODE=patch \
    COCCI=scripts/coccinelle/api/device_attr_show.cocci
    
    No functional change intended
    
    CC: Kalle Valo <kvalo@kernel.org>
    CC: Jeff Johnson <jjohnson@kernel.org>
    CC: linux-wireless@vger.kernel.org
    CC: ath11k@lists.infradead.org
    CC: ath10k@lists.infradead.org
    Signed-off-by: Li Zhijian <lizhijian@fujitsu.com>
    
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://msgid.link/20240315055211.1347548-1-lizhijian@fujitsu.com

--- drivers/net/wireless/ath/ath10k/thermal.c
+++ drivers/net/wireless/ath/ath10k/thermal.c
@@ -100,7 +100,7 @@ static ssize_t ath10k_thermal_show_temp(struct device *dev,
 	spin_unlock_bh(&ar->data_lock);
 
 	/* display in millidegree celsius */
-	ret = snprintf(buf, PAGE_SIZE, "%d\n", temperature * 1000);
+	ret = sysfs_emit(buf, "%d\n", temperature * 1000);
 out:
 	mutex_unlock(&ar->conf_mutex);
 	return ret;
