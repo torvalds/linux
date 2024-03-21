9ea9cd8e61a1f8dd304a3c8531b20f27ed5446d5
Kent Overstreet <kent.overstreet@linux.dev>
2024-03-21 09:36:25 -0700

    mm/slub: mark slab_free_freelist_hook() __always_inline
    It seems we need to be more forceful with the compiler on this one.  This
    is done for performance reasons only.
    
    Link: https://lkml.kernel.org/r/20240321163705.3067592-4-surenb@google.com
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Reviewed-by: Vlastimil Babka <vbabka@suse.cz>
    Tested-by: Kees Cook <keescook@chromium.org>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Alex Gaynor <alex.gaynor@gmail.com>
    Cc: Alice Ryhl <aliceryhl@google.com>
    Cc: Andreas Hindborg <a.hindborg@samsung.com>
    Cc: Benno Lossin <benno.lossin@proton.me>
    Cc: "Björn Roy Baron" <bjorn3_gh@protonmail.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Dennis Zhou <dennis@kernel.org>
    Cc: Gary Guo <gary@garyguo.net>
    Cc: Miguel Ojeda <ojeda@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Wedson Almeida Filho <wedsonaf@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/slub.c
+++ mm/slub.c
@@ -2106,9 +2106,9 @@ bool slab_free_hook(struct kmem_cache *s, void *x, bool init)
 	return !kasan_slab_free(s, x, init);
 }
 
-static inline bool slab_free_freelist_hook(struct kmem_cache *s,
-					   void **head, void **tail,
-					   int *cnt)
+static __fastpath_inline
+bool slab_free_freelist_hook(struct kmem_cache *s, void **head, void **tail,
+			     int *cnt)
 {
 
 	void *object;
