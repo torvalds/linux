0f63c95aebf11d87b166a5dfd389957c67fef9c0
Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
2024-03-22 15:12:19 +0000

    clocksource/drivers/renesas-ostm: Allow OSTM driver to reprobe for RZ/V2H(P) SoC
    The RZ/V2H(P) (R9A09G057) SoC is equipped with the Generic Timer Module,
    also known as OSTM. Similar to the RZ/G2L SoC, the OSTM on the RZ/V2H(P)
    SoC requires the reset line to be deasserted before accessing any
    registers.
    
    Early call to ostm_init() happens through TIMER_OF_DECLARE() which always
    fails with -EPROBE_DEFER, as resets are not available that early in the
    boot process.  To address this issue on the RZ/V2H(P) SoC, enable the OSTM
    driver to be reprobed through the platform driver probe mechanism.
    
    Signed-off-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/20240322151219.885832-3-prabhakar.mahadev-lad.rj@bp.renesas.com

--- drivers/clocksource/renesas-ostm.c
+++ drivers/clocksource/renesas-ostm.c
@@ -224,7 +224,7 @@ err_free:
 
 TIMER_OF_DECLARE(ostm, "renesas,ostm", ostm_init);
 
-#ifdef CONFIG_ARCH_RZG2L
+#if defined(CONFIG_ARCH_RZG2L) || defined(CONFIG_ARCH_R9A09G057)
 static int __init ostm_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
