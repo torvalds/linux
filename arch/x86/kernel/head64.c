9843231c97267d72be38a0409f5097987bc2cfa4
Tom Lendacky <thomas.lendacky@amd.com>
2024-03-22 10:41:07 -0500

    x86/boot/64: Move 5-level paging global variable assignments back
    Commit 63bed9660420 ("x86/startup_64: Defer assignment of 5-level paging
    global variables") moved assignment of 5-level global variables to later
    in the boot in order to avoid having to use RIP relative addressing in
    order to set them. However, when running with 5-level paging and SME
    active (mem_encrypt=on), the variables are needed as part of the page
    table setup needed to encrypt the kernel (using pgd_none(), p4d_offset(),
    etc.). Since the variables haven't been set, the page table manipulation
    is done as if 4-level paging is active, causing the system to crash on
    boot.
    
    While only a subset of the assignments that were moved need to be set
    early, move all of the assignments back into check_la57_support() so that
    these assignments aren't spread between two locations. Instead of just
    reverting the fix, this uses the new RIP_REL_REF() macro when assigning
    the variables.
    
    Fixes: 63bed9660420 ("x86/startup_64: Defer assignment of 5-level paging global variables")
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>
    Link: https://lore.kernel.org/r/2ca419f4d0de719926fd82353f6751f717590a86.1711122067.git.thomas.lendacky@amd.com

--- arch/x86/kernel/head64.c
+++ arch/x86/kernel/head64.c
@@ -81,6 +81,13 @@ static inline bool check_la57_support(void)
 	if (!(native_read_cr4() & X86_CR4_LA57))
 		return false;
 
+	RIP_REL_REF(__pgtable_l5_enabled)	= 1;
+	RIP_REL_REF(pgdir_shift)		= 48;
+	RIP_REL_REF(ptrs_per_p4d)		= 512;
+	RIP_REL_REF(page_offset_base)		= __PAGE_OFFSET_BASE_L5;
+	RIP_REL_REF(vmalloc_base)		= __VMALLOC_BASE_L5;
+	RIP_REL_REF(vmemmap_base)		= __VMEMMAP_BASE_L5;
+
 	return true;
 }
 
@@ -430,15 +437,6 @@ asmlinkage __visible void __init __noreturn x86_64_start_kernel(char * real_mode
 	MAYBE_BUILD_BUG_ON(!(((MODULES_END - 1) & PGDIR_MASK) ==
 				(__START_KERNEL & PGDIR_MASK)));
 	BUILD_BUG_ON(__fix_to_virt(__end_of_fixed_addresses) <= MODULES_END);
-
-	if (check_la57_support()) {
-		__pgtable_l5_enabled	= 1;
-		pgdir_shift		= 48;
-		ptrs_per_p4d		= 512;
-		page_offset_base	= __PAGE_OFFSET_BASE_L5;
-		vmalloc_base		= __VMALLOC_BASE_L5;
-		vmemmap_base		= __VMEMMAP_BASE_L5;
-	}
 
 	cr4_init_shadow();
 
