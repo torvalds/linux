4de34b04783628f14614badb0a1aa67ce3fcef5d
Alex Deucher <alexander.deucher@amd.com>
2024-03-26 11:28:29 -0400

    drm/amdgpu: always allocate cleared VRAM for GEM allocations
    This adds allocation latency, but aligns better with user
    expectations.  The latency should improve with the drm buddy
    clearing patches that Arun has been working on.
    
    In addition this fixes the high CPU spikes seen when doing
    wipe on release.
    
    v2: always set AMDGPU_GEM_CREATE_VRAM_CLEARED (Christian)
    
    Closes: https://gitlab.freedesktop.org/drm/amd/-/issues/3528
    Fixes: a68c7eaa7a8f ("drm/amdgpu: Enable clear page functionality")
    Acked-by: Arunpravin Paneer Selvam <Arunpravin.PaneerSelvam@amd.com>
    Reviewed-by: Michel Dänzer <mdaenzer@redhat.com> (v1)
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: Arunpravin Paneer Selvam <Arunpravin.PaneerSelvam@amd.com>
    Cc: Christian König <christian.koenig@amd.com>
    (cherry picked from commit 6c0a7c3c693ac84f8b50269a9088af8f37446863)
    Cc: stable@vger.kernel.org # 6.10.x

--- drivers/gpu/drm/amd/amdgpu/amdgpu_gem.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_gem.c
@@ -348,6 +348,9 @@ int amdgpu_gem_create_ioctl(struct drm_device *dev, void *data,
 		return -EINVAL;
 	}
 
+	/* always clear VRAM */
+	flags |= AMDGPU_GEM_CREATE_VRAM_CLEARED;
+
 	/* create a gem object to contain this object in */
 	if (args->in.domains & (AMDGPU_GEM_DOMAIN_GDS |
 	    AMDGPU_GEM_DOMAIN_GWS | AMDGPU_GEM_DOMAIN_OA)) {
