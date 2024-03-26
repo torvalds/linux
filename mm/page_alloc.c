bb8ea62daa7cfd634d1a43815666948311074d0c
Baoquan He <bhe@redhat.com>
2024-03-26 14:11:30 +0800

    mm/page_alloc.c: remove unneeded codes in !NUMA version of build_zonelists()
    When CONFIG_NUMA=n, MAX_NUMNODES is always 1 because Kconfig item
    NODES_SHIFT depends on NUMA.  So in !NUMA version of build_zonelists(), no
    need to bother with the two for loop because code execution won't enter
    them ever.
    
    Here, remove those unneeded codes in !NUMA version of build_zonelists().
    
    [bhe@redhat.com: remove unused locals]
      Link: https://lkml.kernel.org/r/ZgQL1WOf9K88nLpQ@MiWiFi-R3L-srv
    Link: https://lkml.kernel.org/r/20240326061134.1055295-5-bhe@redhat.com
    Signed-off-by: Baoquan He <bhe@redhat.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: "Mike Rapoport (IBM)" <rppt@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5280,36 +5280,12 @@ static void setup_min_slab_ratio(void);
 
 static void build_zonelists(pg_data_t *pgdat)
 {
-	int node, local_node;
 	struct zoneref *zonerefs;
 	int nr_zones;
-
-	local_node = pgdat->node_id;
 
 	zonerefs = pgdat->node_zonelists[ZONELIST_FALLBACK]._zonerefs;
 	nr_zones = build_zonerefs_node(pgdat, zonerefs);
 	zonerefs += nr_zones;
-
-	/*
-	 * Now we build the zonelist so that it contains the zones
-	 * of all the other nodes.
-	 * We don't want to pressure a particular node, so when
-	 * building the zones for node N, we make sure that the
-	 * zones coming right after the local ones are those from
-	 * node N+1 (modulo N)
-	 */
-	for (node = local_node + 1; node < MAX_NUMNODES; node++) {
-		if (!node_online(node))
-			continue;
-		nr_zones = build_zonerefs_node(NODE_DATA(node), zonerefs);
-		zonerefs += nr_zones;
-	}
-	for (node = 0; node < local_node; node++) {
-		if (!node_online(node))
-			continue;
-		nr_zones = build_zonerefs_node(NODE_DATA(node), zonerefs);
-		zonerefs += nr_zones;
-	}
 
 	zonerefs->zone = NULL;
 	zonerefs->zone_idx = 0;
