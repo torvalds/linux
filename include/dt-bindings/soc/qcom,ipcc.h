e1b6a795b5bed0d7a345c50c23c1a7afeebf7170
Anvesh Jain P <quic_ajainp@quicinc.com>
2024-03-27 18:01:49 +0530

    dt-bindings: Add IPCC client ID for NSP1
    Add IPCC client ID for NSP1.
    
    Change-Id: I8ed4d672511241136c15ed178004d4ae7a8974fe
    Signed-off-by: Anvesh Jain P <quic_ajainp@quicinc.com>

--- include/dt-bindings/soc/qcom,ipcc.h
+++ include/dt-bindings/soc/qcom,ipcc.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef __DT_BINDINGS_QCOM_IPCC_H
@@ -35,6 +35,7 @@
 #define IPCC_CLIENT_PCIE1		14
 #define IPCC_CLIENT_PCIE2		15
 #define IPCC_CLIENT_SPSS		16
+#define IPCC_CLIENT_NSP1                18
 #define IPCC_CLIENT_TME			23
 #define IPCC_CLIENT_WPSS		24
 #define IPCC_CLIENT_SAIL0               27
