99bb8600413b2eb8d2a271c282e8bd43eb9a416d
Zahir Shabbir Khan <quic_zshabbir@quicinc.com>
2024-03-27 17:24:22 +0530

    spi: spi-msm-geni: Add fix for system suspend failure when xfer timeout
    System suspend failure issue is observed if there is xfer timeout before
    pm suspend call. xfer in progress variable is not set to false if
    unprepeare transfer hardware cb is not called.
    
    So driver will always be transfer in progress state & causing suspend
    failure hence clear the flag in timeout error path.
    
    Change-Id: I45b50fa615741357c84d754cbaf2ef2e150fc493
    Signed-off-by: Zahir Shabbir Khan <quic_zshabbir@quicinc.com>

--- drivers/spi/spi-msm-geni.c
+++ drivers/spi/spi-msm-geni.c
@@ -1955,6 +1955,8 @@ dma_unprep:
 	if (spi->slave && !mas->dis_autosuspend)
 		pm_runtime_put_sync_suspend(mas->dev);
 
+	mas->is_xfer_in_progress = false;
+
 }
 
 static int spi_geni_transfer_one(struct spi_master *spi,
@@ -2124,6 +2126,7 @@ static int spi_geni_transfer_one(struct spi_master *spi,
 err_gsi_geni_transfer_one:
 	geni_spi_se_dump_dbg_regs(&mas->spi_rsc, mas->base, mas->ipc);
 	mutex_unlock(&mas->spi_ssr.ssr_lock);
+	mas->is_xfer_in_progress = false;
 	dmaengine_terminate_all(mas->tx);
 	if (mas->is_le_vm)
 		mas->le_gpi_reset_done = true;
