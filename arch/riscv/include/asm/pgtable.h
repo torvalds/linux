9ad6bb3298d1c008bb36b1f4fa9ea896d2904b5a
Samuel Holland <samuel.holland@sifive.com>
2024-03-27 07:38:12 -0700

    riscv: Remove PGDIR_SIZE_L3 and TASK_SIZE_MIN
    TASK_SIZE_MIN is unused since commit 085e2ff9aeb0 ("efi: libstub: Drop
    randomization of runtime memory map"). PGDIR_SIZE_L3 is only used in the
    definition of TASK_SIZE_MIN.
    
    Signed-off-by: Samuel Holland <samuel.holland@sifive.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Link: https://lore.kernel.org/r/20240327143858.711792-2-samuel.holland@sifive.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>

--- arch/riscv/include/asm/pgtable.h
+++ arch/riscv/include/asm/pgtable.h
@@ -870,7 +870,6 @@ static inline pte_t pte_swp_clear_exclusive(pte_t pte)
  */
 #ifdef CONFIG_64BIT
 #define TASK_SIZE_64	(PGDIR_SIZE * PTRS_PER_PGD / 2)
-#define TASK_SIZE_MIN	(PGDIR_SIZE_L3 * PTRS_PER_PGD / 2)
 
 #ifdef CONFIG_COMPAT
 #define TASK_SIZE_32	(_AC(0x80000000, UL) - PAGE_SIZE)
@@ -882,7 +881,6 @@ static inline pte_t pte_swp_clear_exclusive(pte_t pte)
 
 #else
 #define TASK_SIZE	FIXADDR_START
-#define TASK_SIZE_MIN	TASK_SIZE
 #endif
 
 #else /* CONFIG_MMU */
