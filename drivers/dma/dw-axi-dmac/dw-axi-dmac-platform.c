559a6690187ee0ab7875f7c560d3d19e35423fb3
Tan Chun Hau <chunhau.tan@starfivetech.com>
2024-03-26 19:51:26 -0700

    dmaengine: dw-axi-dmac: Add support for StarFive JH8100 DMA
    JH8100 requires reset operation only in device probe.
    
    Signed-off-by: Tan Chun Hau <chunhau.tan@starfivetech.com>
    Link: https://lore.kernel.org/r/20240327025126.229475-3-chunhau.tan@starfivetech.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/dw-axi-dmac/dw-axi-dmac-platform.c
+++ drivers/dma/dw-axi-dmac/dw-axi-dmac-platform.c
@@ -1653,6 +1653,9 @@ static const struct of_device_id dw_dma_of_id_table[] = {
 	}, {
 		.compatible = "starfive,jh7110-axi-dma",
 		.data = (void *)(AXI_DMA_FLAG_HAS_RESETS | AXI_DMA_FLAG_USE_CFG2),
+	}, {
+		.compatible = "starfive,jh8100-axi-dma",
+		.data = (void *)AXI_DMA_FLAG_HAS_RESETS,
 	},
 	{}
 };
