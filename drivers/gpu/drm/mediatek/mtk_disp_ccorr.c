4c591e97bf3e6691174551a570a11a4d01c50b4e
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2024-03-30 21:43:13 +0100

    drm/mediatek: ccorr: drop driver owner initialization
    Core in platform_driver_register() already sets the .owner, so driver
    does not need to.  Whatever is set here will be anyway overwritten by
    main driver calling platform_driver_register().
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Link: https://patchwork.kernel.org/project/dri-devel/patch/20240330-b4-module-owner-drm-mediatek-v1-2-fd5c4b8d633e@linaro.org/
    Signed-off-by: Chun-Kuang Hu <chunkuang.hu@kernel.org>

--- drivers/gpu/drm/mediatek/mtk_disp_ccorr.c
+++ drivers/gpu/drm/mediatek/mtk_disp_ccorr.c
@@ -214,7 +214,6 @@ struct platform_driver mtk_disp_ccorr_driver = {
 	.remove_new	= mtk_disp_ccorr_remove,
 	.driver		= {
 		.name	= "mediatek-disp-ccorr",
-		.owner	= THIS_MODULE,
 		.of_match_table = mtk_disp_ccorr_driver_dt_match,
 	},
 };
