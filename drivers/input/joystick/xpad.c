3d432a840339777e9063fcf0fecbb1e853456ade
Ilia Baryshnikov <qwelias@gmail.com>
2024-04-03 02:00:48 +0200

    Input: xpad - add support for Gamesir T4 Cyclone
    Add Gamesir T4 Cyclone to the list of supported devices.

--- drivers/input/joystick/xpad.c
+++ drivers/input/joystick/xpad.c
@@ -379,6 +379,7 @@ static const struct xpad_device {
 	{ 0x31e3, 0x1310, "Wooting 60HE (ARM)", 0, XTYPE_XBOX360 },
 	{ 0x3285, 0x0607, "Nacon GC-100", 0, XTYPE_XBOX360 },
 	{ 0x3537, 0x1004, "GameSir T4 Kaleid", 0, XTYPE_XBOX360 },
+	{ 0x3537, 0x1014, "GameSir T4 Cyclone", 0, XTYPE_XBOX360 },
 	{ 0x3767, 0x0101, "Fanatec Speedster 3 Forceshock Wheel", 0, XTYPE_XBOX },
 	{ 0xffff, 0xffff, "Chinese-made Xbox Controller", 0, XTYPE_XBOX },
 	{ 0x0000, 0x0000, "Generic X-Box pad", 0, XTYPE_UNKNOWN }
