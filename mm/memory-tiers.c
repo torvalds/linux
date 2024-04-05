a72a30af550c08423de1b9feecb6ceeddc434889
Ho-Ren (Jack) Chuang <horenchuang@bytedance.com>
2024-04-05 00:07:05 +0000

    memory tier: dax/kmem: introduce an abstract layer for finding, allocating, and putting memory types
    Patch series "Improved Memory Tier Creation for CPUless NUMA Nodes", v11.
    
    When a memory device, such as CXL1.1 type3 memory, is emulated as normal
    memory (E820_TYPE_RAM), the memory device is indistinguishable from normal
    DRAM in terms of memory tiering with the current implementation.  The
    current memory tiering assigns all detected normal memory nodes to the
    same DRAM tier.  This results in normal memory devices with different
    attributions being unable to be assigned to the correct memory tier,
    leading to the inability to migrate pages between different types of
    memory. 
    https://lore.kernel.org/linux-mm/PH0PR08MB7955E9F08CCB64F23963B5C3A860A@PH0PR08MB7955.namprd08.prod.outlook.com/T/
    
    This patchset automatically resolves the issues.  It delays the
    initialization of memory tiers for CPUless NUMA nodes until they obtain
    HMAT information and after all devices are initialized at boot time,
    eliminating the need for user intervention.  If no HMAT is specified, it
    falls back to using `default_dram_type`.
    
    Example usecase:
    We have CXL memory on the host, and we create VMs with a new system memory
    device backed by host CXL memory.  We inject CXL memory performance
    attributes through QEMU, and the guest now sees memory nodes with
    performance attributes in HMAT.  With this change, we enable the guest
    kernel to construct the correct memory tiering for the memory nodes.
    
    
    This patch (of 2):
    
    Since different memory devices require finding, allocating, and putting
    memory types, these common steps are abstracted in this patch, enhancing
    the scalability and conciseness of the code.
    
    Link: https://lkml.kernel.org/r/20240405000707.2670063-1-horenchuang@bytedance.com
    Link: https://lkml.kernel.org/r/20240405000707.2670063-2-horenchuang@bytedance.com
    Signed-off-by: Ho-Ren (Jack) Chuang <horenchuang@bytedance.com>
    Reviewed-by: "Huang, Ying" <ying.huang@intel.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawie.com>
    Cc: Alistair Popple <apopple@nvidia.com>
    Cc: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Dave Jiang <dave.jiang@intel.com>
    Cc: Gregory Price <gourry.memverge@gmail.com>
    Cc: Hao Xiang <hao.xiang@bytedance.com>
    Cc: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Ravi Jonnalagadda <ravis.opensrc@micron.com>
    Cc: SeongJae Park <sj@kernel.org>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Vishal Verma <vishal.l.verma@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memory-tiers.c
+++ mm/memory-tiers.c
@@ -623,6 +623,35 @@ void clear_node_memory_type(int node, struct memory_dev_type *memtype)
 }
 EXPORT_SYMBOL_GPL(clear_node_memory_type);
 
+struct memory_dev_type *mt_find_alloc_memory_type(int adist, struct list_head *memory_types)
+{
+	struct memory_dev_type *mtype;
+
+	list_for_each_entry(mtype, memory_types, list)
+		if (mtype->adistance == adist)
+			return mtype;
+
+	mtype = alloc_memory_type(adist);
+	if (IS_ERR(mtype))
+		return mtype;
+
+	list_add(&mtype->list, memory_types);
+
+	return mtype;
+}
+EXPORT_SYMBOL_GPL(mt_find_alloc_memory_type);
+
+void mt_put_memory_types(struct list_head *memory_types)
+{
+	struct memory_dev_type *mtype, *mtn;
+
+	list_for_each_entry_safe(mtype, mtn, memory_types, list) {
+		list_del(&mtype->list);
+		put_memory_type(mtype);
+	}
+}
+EXPORT_SYMBOL_GPL(mt_put_memory_types);
+
 static void dump_hmem_attrs(struct access_coordinate *coord, const char *prefix)
 {
 	pr_info(
