ed6d22f5d8672f38d6020c719d5d658a4e3e6be5
Jiri Slaby (SUSE) <jirislaby@kernel.org>
2024-04-05 08:08:16 +0200

    kfifo: rename l to len_to_end in setup_sgl()
    So that one can make any sense of the name.
    
    Signed-off-by: Jiri Slaby (SUSE) <jirislaby@kernel.org>
    Cc: Stefani Seibold <stefani@seibold.net>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Link: https://lore.kernel.org/r/20240405060826.2521-6-jirislaby@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- lib/kfifo.c
+++ lib/kfifo.c
@@ -321,7 +321,7 @@ static unsigned int setup_sgl(struct __kfifo *fifo, struct scatterlist *sgl,
 {
 	unsigned int size = fifo->mask + 1;
 	unsigned int esize = fifo->esize;
-	unsigned int l;
+	unsigned int len_to_end;
 	unsigned int n;
 
 	off &= fifo->mask;
@@ -330,10 +330,10 @@ static unsigned int setup_sgl(struct __kfifo *fifo, struct scatterlist *sgl,
 		size *= esize;
 		len *= esize;
 	}
-	l = min(len, size - off);
+	len_to_end = min(len, size - off);
 
-	n = setup_sgl_buf(sgl, fifo->data + off, nents, l);
-	n += setup_sgl_buf(sgl + n, fifo->data, nents - n, len - l);
+	n = setup_sgl_buf(sgl, fifo->data + off, nents, len_to_end);
+	n += setup_sgl_buf(sgl + n, fifo->data, nents - n, len - len_to_end);
 
 	return n;
 }
