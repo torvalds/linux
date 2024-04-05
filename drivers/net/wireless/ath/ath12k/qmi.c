b34389c3978c3679954bb272ecec9365665365aa
Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
2024-04-05 15:22:09 +0300

    wifi: ath12k: fix link capable flags
    By default driver supports intra-device SLO/MLO, the link capability flags
    reflect this. When the QMI PHY capability learning fails driver not enable
    the MLO parameter in the host capability QMI request message. In this case,
    reset the device link capability flags to zero (SLO/MLO not support) to
    accurately represent the capabilities.
    
    Found in code review.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.1.1-00188-QCAHKSWPL_SILICONZ-1
    
    Signed-off-by: Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
    Acked-by: Jeff Johnson <quic_jjohnson@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://msgid.link/20240403042056.1504209-3-quic_periyasa@quicinc.com

--- drivers/net/wireless/ath/ath12k/qmi.c
+++ drivers/net/wireless/ath/ath12k/qmi.c
@@ -2006,6 +2006,8 @@ static void ath12k_host_cap_parse_mlo(struct ath12k_base *ab,
 	int i;
 
 	if (!ab->qmi.num_radios || ab->qmi.num_radios == U8_MAX) {
+		ab->mlo_capable_flags = 0;
+
 		ath12k_dbg(ab, ATH12K_DBG_QMI,
 			   "skip QMI MLO cap due to invalid num_radio %d\n",
 			   ab->qmi.num_radios);
