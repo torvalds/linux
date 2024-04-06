244b6e92bd1a0b6ff1f6253b10a96dc208a315f2
Marc Dietrich <marvin24@gmx.de>
2024-04-06 14:31:19 +0200

    staging: nvec: add ability to ignore EC responses in sync writes
    In case we just want to submit a message to the EC but are not
    interested in its response, we can free the response buffer early.
    
    Signed-off-by: Marc Dietrich <marvin24@gmx.de>
    Link: https://lore.kernel.org/r/20240406123123.37148-2-marvin24@gmx.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/nvec/nvec.c
+++ drivers/staging/nvec/nvec.c
@@ -300,7 +300,9 @@ int nvec_write_sync(struct nvec_chip *nvec,
 {
 	mutex_lock(&nvec->sync_write_mutex);
 
-	*msg = NULL;
+	if (msg != NULL)
+		*msg = NULL;
+
 	nvec->sync_write_pending = (data[1] << 8) + data[0];
 
 	if (nvec_write_async(nvec, data, size) < 0) {
@@ -320,7 +322,10 @@ int nvec_write_sync(struct nvec_chip *nvec,
 
 	dev_dbg(nvec->dev, "nvec_sync_write: pong!\n");
 
-	*msg = nvec->last_sync_msg;
+	if (msg != NULL)
+		*msg = nvec->last_sync_msg;
+	else
+		nvec_msg_free(nvec, nvec->last_sync_msg);
 
 	mutex_unlock(&nvec->sync_write_mutex);
 
