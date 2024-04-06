d8f20383a2fc3a3844b08a4999cf0e81164a0e56
Hans de Goede <hdegoede@redhat.com>
2024-04-06 15:56:25 +0200

    ACPI: x86: Add PNP_UART1_SKIP quirk for Lenovo Blade2 tablets
    The x86 Android tablets on which quirks to skip looking for a matching
    UartSerialBus resource and instead unconditionally create a serial bus
    device (serdev) are necessary there are 2 sorts of serialports:
    
    ACPI enumerated highspeed designware UARTs, these are the ones which
    typcially need to be skipped since they need a serdev for the attached
    BT HCI.
    
    A PNP enumerated UART which is part of the PCU. So far the existing
    quirks have ignored this. But on the Lenovo Yoga Tablet 2 Pro 1380
    models this is used for a custom fastcharging protocol. There is
    a Micro USB switch which can switch the USB data lines to this uart
    and then a 600 baud protocol is used to configure the charger for
    a voltage higher then 5V.
    
    Add a new ACPI_QUIRK_PNP_UART1_SKIP quirk type and set this for
    the existing entry for the Lenovo Yoga Tablet 2 830 / 1050 models.
    Note this will lead to unnecessarily also creating a serdev for
    the PCU UART on the 830 / 1050 which don't need this, but the UART
    is not used otherwise there so that is not a problem.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/x86/utils.c
+++ drivers/acpi/x86/utils.c
@@ -269,9 +269,10 @@ bool force_storage_d3(void)
 #define ACPI_QUIRK_SKIP_I2C_CLIENTS				BIT(0)
 #define ACPI_QUIRK_UART1_SKIP					BIT(1)
 #define ACPI_QUIRK_UART1_TTY_UART2_SKIP				BIT(2)
-#define ACPI_QUIRK_SKIP_ACPI_AC_AND_BATTERY			BIT(3)
-#define ACPI_QUIRK_USE_ACPI_AC_AND_BATTERY			BIT(4)
-#define ACPI_QUIRK_SKIP_GPIO_EVENT_HANDLERS			BIT(5)
+#define ACPI_QUIRK_PNP_UART1_SKIP				BIT(3)
+#define ACPI_QUIRK_SKIP_ACPI_AC_AND_BATTERY			BIT(4)
+#define ACPI_QUIRK_USE_ACPI_AC_AND_BATTERY			BIT(5)
+#define ACPI_QUIRK_SKIP_GPIO_EVENT_HANDLERS			BIT(6)
 
 static const struct dmi_system_id acpi_quirk_skip_dmi_ids[] = {
 	/*
@@ -351,6 +352,7 @@ static const struct dmi_system_id acpi_quirk_skip_dmi_ids[] = {
 			DMI_MATCH(DMI_BIOS_VERSION, "BLADE_21"),
 		},
 		.driver_data = (void *)(ACPI_QUIRK_SKIP_I2C_CLIENTS |
+					ACPI_QUIRK_PNP_UART1_SKIP |
 					ACPI_QUIRK_SKIP_ACPI_AC_AND_BATTERY),
 	},
 	{
@@ -449,13 +451,17 @@ static int acpi_dmi_skip_serdev_enumeration(struct device *controller_parent, bo
 	if (ret)
 		return 0;
 
-	/* to not match on PNP enumerated debug UARTs */
-	if (!dev_is_platform(controller_parent))
-		return 0;
-
 	dmi_id = dmi_first_match(acpi_quirk_skip_dmi_ids);
 	if (dmi_id)
 		quirks = (unsigned long)dmi_id->driver_data;
+
+	if (!dev_is_platform(controller_parent)) {
+		/* PNP enumerated UARTs */
+		if ((quirks & ACPI_QUIRK_PNP_UART1_SKIP) && uid == 1)
+			*skip = true;
+
+		return 0;
+	}
 
 	if ((quirks & ACPI_QUIRK_UART1_SKIP) && uid == 1)
 		*skip = true;
