2ac72cbc7e61ef24e8ee2f39d02b3b8b59c14a41
Likun Gao <Likun.Gao@amd.com>
2024-04-08 03:38:10 +0800

    drm/amdgpu: enable some cg feature for gc 12.0.0
    Enable 3D cgcg, 3D cgls, sram fgcg, perfcounter mgcg,
    repeater fgcg for gc v12.0.0.
    
    Signed-off-by: Likun Gao <Likun.Gao@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/soc24.c
+++ drivers/gpu/drm/amd/amdgpu/soc24.c
@@ -398,7 +398,13 @@ static int soc24_common_early_init(void *handle)
 	switch (amdgpu_ip_version(adev, GC_HWIP, 0)) {
 	case IP_VERSION(12, 0, 0):
 		adev->cg_flags = AMD_CG_SUPPORT_GFX_CGCG |
-			AMD_CG_SUPPORT_GFX_CGLS;
+			AMD_CG_SUPPORT_GFX_CGLS |
+			AMD_CG_SUPPORT_GFX_MGCG |
+			AMD_CG_SUPPORT_GFX_3D_CGCG |
+			AMD_CG_SUPPORT_GFX_3D_CGLS |
+			AMD_CG_SUPPORT_REPEATER_FGCG |
+			AMD_CG_SUPPORT_GFX_FGCG |
+			AMD_CG_SUPPORT_GFX_PERF_CLK;
 		adev->pg_flags = AMD_PG_SUPPORT_VCN |
 			AMD_PG_SUPPORT_JPEG;
 		adev->external_rev_id = adev->rev_id + 0x40;
