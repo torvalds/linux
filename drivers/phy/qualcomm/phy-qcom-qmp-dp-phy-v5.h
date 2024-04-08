025a6f7448f7bb5f4fceb62498ee33d89ae266bb
Johan Hovold <johan+linaro@kernel.org>
2024-04-08 11:30:23 +0200

    phy: qcom: qmp-combo: fix VCO div offset on v5_5nm and v6
    Commit 5abed58a8bde ("phy: qcom: qmp-combo: Fix VCO div offset on v3")
    fixed a regression introduced in 6.5 by making sure that the correct
    offset is used for the DP_PHY_VCO_DIV register on v3 hardware.
    
    Unfortunately, that fix instead broke DisplayPort on v5_5nm and v6
    hardware as it failed to add the corresponding offsets also to those
    register tables.
    
    Fixes: 815891eee668 ("phy: qcom-qmp-combo: Introduce orientation variable")
    Fixes: 5abed58a8bde ("phy: qcom: qmp-combo: Fix VCO div offset on v3")
    Cc: stable@vger.kernel.org	# 6.5: 5abed58a8bde
    Cc: Stephen Boyd <swboyd@chromium.org>
    Cc: Abhinav Kumar <quic_abhinavk@quicinc.com>
    Cc: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Reviewed-by: Stephen Boyd <swboyd@chromium.org>
    Reviewed-by: Abhinav Kumar <quic_abhinavk@quicinc.com>
    Link: https://lore.kernel.org/r/20240408093023.506-1-johan+linaro@kernel.org
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/phy/qualcomm/phy-qcom-qmp-dp-phy-v5.h
+++ drivers/phy/qualcomm/phy-qcom-qmp-dp-phy-v5.h
@@ -7,6 +7,7 @@
 #define QCOM_PHY_QMP_DP_PHY_V5_H_
 
 /* Only for QMP V5 PHY - DP PHY registers */
+#define QSERDES_V5_DP_PHY_VCO_DIV			0x070
 #define QSERDES_V5_DP_PHY_AUX_INTERRUPT_STATUS		0x0d8
 #define QSERDES_V5_DP_PHY_STATUS			0x0dc
 
