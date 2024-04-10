1d428f5d3827fd9826abe3b14609aaa2037bc00c
Wolfram Sang <wsa+renesas@sang-engineering.com>
2024-04-10 13:24:32 +0200

    i2c: uniphier: remove printout on handled timeouts
    I2C and SMBus timeouts are not something the user needs to be informed
    about on controller level. The client driver may know if that really is
    a problem and give more detailed information to the user. The controller
    should just pass this information upwards. Remove the printout.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Andi Shyti <andi.shyti@kernel.org>

--- drivers/i2c/busses/i2c-uniphier.c
+++ drivers/i2c/busses/i2c-uniphier.c
@@ -71,10 +71,8 @@ static int uniphier_i2c_xfer_byte(struct i2c_adapter *adap, u32 txdata,
 	writel(txdata, priv->membase + UNIPHIER_I2C_DTRM);
 
 	time_left = wait_for_completion_timeout(&priv->comp, adap->timeout);
-	if (unlikely(!time_left)) {
-		dev_err(&adap->dev, "transaction timeout\n");
+	if (unlikely(!time_left))
 		return -ETIMEDOUT;
-	}
 
 	rxdata = readl(priv->membase + UNIPHIER_I2C_DREC);
 	if (rxdatap)
