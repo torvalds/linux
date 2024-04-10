6802f9347993a534797847f627c27f4334067945
Kyle Meyer <kyle.meyer@hpe.com>
2024-04-10 16:33:10 -0500

    cpumask: Add for_each_cpu_from()
    Add for_each_cpu_from() as a generic cpumask macro.
    
    for_each_cpu_from() is the same as for_each_cpu(), except it starts at
    @cpu instead of zero.
    
    Signed-off-by: Kyle Meyer <kyle.meyer@hpe.com>
    Acked-by: Yury Norov <yury.norov@gmail.com>
    Signed-off-by: Yury Norov <yury.norov@gmail.com>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -369,6 +369,16 @@ unsigned int __pure cpumask_next_wrap(int n, const struct cpumask *mask, int sta
 	for_each_or_bit(cpu, cpumask_bits(mask1), cpumask_bits(mask2), small_cpumask_bits)
 
 /**
+ * for_each_cpu_from - iterate over CPUs present in @mask, from @cpu to the end of @mask.
+ * @cpu: the (optionally unsigned) integer iterator
+ * @mask: the cpumask pointer
+ *
+ * After the loop, cpu is >= nr_cpu_ids.
+ */
+#define for_each_cpu_from(cpu, mask)				\
+	for_each_set_bit_from(cpu, cpumask_bits(mask), small_cpumask_bits)
+
+/**
  * cpumask_any_but - return a "random" in a cpumask, but not this one.
  * @mask: the cpumask to search
  * @cpu: the cpu to ignore.
