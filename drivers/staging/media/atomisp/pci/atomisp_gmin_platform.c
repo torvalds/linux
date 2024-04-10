ca88632506f40033366ec65213676fffd0661632
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2024-04-10 11:32:11 +0100

    media: atomisp: Replace open-coded i2c_find_device_by_fwnode()
    gmin_i2c_dev_exists() is using open-coded variant of
    i2c_find_device_by_fwnode(). Replace it with a corresponding call.
    
    Link: https://lore.kernel.org/r/20240326202813.1425431-3-andriy.shevchenko@linux.intel.com
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>

--- drivers/staging/media/atomisp/pci/atomisp_gmin_platform.c
+++ drivers/staging/media/atomisp/pci/atomisp_gmin_platform.c
@@ -388,19 +388,15 @@ static struct i2c_client *gmin_i2c_dev_exists(struct device *dev, char *name,
 					      struct i2c_client **client)
 {
 	struct acpi_device *adev;
-	struct device *d;
 
 	adev = acpi_dev_get_first_match_dev(name, NULL, -1);
 	if (!adev)
 		return NULL;
 
-	d = bus_find_device_by_acpi_dev(&i2c_bus_type, adev);
+	*client = i2c_find_device_by_fwnode(acpi_fwnode_handle(adev));
 	acpi_dev_put(adev);
-	if (!d)
+	if (!*client)
 		return NULL;
-
-	*client = i2c_verify_client(d);
-	put_device(d);
 
 	dev_dbg(dev, "found '%s' at address 0x%02x, adapter %d\n",
 		(*client)->name, (*client)->addr, (*client)->adapter->nr);
