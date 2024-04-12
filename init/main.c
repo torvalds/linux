ddd53363f875eb23b6754362ce0a43f2214f0a83
Yuntao Wang <ytcoode@gmail.com>
2024-04-12 16:17:33 +0800

    init/main.c: Minor cleanup for the setup_command_line() function
    This is just a minor cleanup to make the code look a bit cleaner.
    
    Link: https://lore.kernel.org/all/20240412081733.35925-3-ytcoode@gmail.com/
    
    Signed-off-by: Yuntao Wang <ytcoode@gmail.com>
    Signed-off-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>

--- init/main.c
+++ init/main.c
@@ -633,11 +633,11 @@ static void __init setup_command_line(char *command_line)
 		ilen = strlen(extra_init_args) + 4; /* for " -- " */
 	}
 
-	len = xlen + strlen(boot_command_line) + 1;
+	len = xlen + strlen(boot_command_line) + ilen + 1;
 
-	saved_command_line = memblock_alloc(len + ilen, SMP_CACHE_BYTES);
+	saved_command_line = memblock_alloc(len, SMP_CACHE_BYTES);
 	if (!saved_command_line)
-		panic("%s: Failed to allocate %zu bytes\n", __func__, len + ilen);
+		panic("%s: Failed to allocate %zu bytes\n", __func__, len);
 
 	len = xlen + strlen(command_line) + 1;
 
