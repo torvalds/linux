de8f4d97bb4af59b15a807401427d25aa830fe53
Matti Vaittinen <mazziesaccount@gmail.com>
2024-04-12 09:02:55 +0300

    mfd: bd71828: Remove commented code lines
    Some commented out code was accidentally left in the header.
    
    Clean up commented out macros.
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>
    Link: https://lore.kernel.org/r/ZhjOj_4AUgC4Iwh_@drtxq0yyyyyyyyyyyyyby-3.rev.dnainternet.fi
    Signed-off-by: Lee Jones <lee@kernel.org>

--- include/linux/mfd/rohm-bd71828.h
+++ include/linux/mfd/rohm-bd71828.h
@@ -44,7 +44,6 @@ enum {
 
 #define BD71828_MASK_STATE_HBNT		BIT(1)
 
-//#define BD71828_REG_SWRESET		0x06
 #define BD71828_MASK_RUN_LVL_CTRL	0x30
 
 /* Regulator control masks */
@@ -136,7 +135,6 @@ enum {
 #define BD71828_REG_LDO5_VOLT		0x43
 #define BD71828_REG_LDO5_VOLT_OPT	0x42
 #define BD71828_REG_LDO6_EN		0x44
-//#define BD71828_REG_LDO6_VOLT		0x4
 #define BD71828_REG_LDO7_EN		0x45
 #define BD71828_REG_LDO7_VOLT		0x46
 
