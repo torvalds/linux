21e1fe9e84f4f2fa672add77a9f9dae7e9d18b6e
Asbjørn Sloth Tønnesen <ast@fiberby.net>
2024-04-16 14:43:30 +0000

    ice: flower: validate control flags
    This driver currently doesn't support any control flags.
    
    Use flow_rule_has_control_flags() to check for control flags,
    such as can be set through `tc flower ... ip_flags frag`.
    
    In case any control flags are masked, flow_rule_has_control_flags()
    sets a NL extended error message, and we return -EOPNOTSUPP.
    
    Only compile-tested.
    
    Signed-off-by: Asbjørn Sloth Tønnesen <ast@fiberby.net>
    Reviewed-by: Michal Swiatkowski <michal.swiatkowski@linux.intel.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Tested-by: Sujai Buvaneswaran <sujai.buvaneswaran@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>

--- drivers/net/ethernet/intel/ice/ice_tc_lib.c
+++ drivers/net/ethernet/intel/ice/ice_tc_lib.c
@@ -1658,6 +1658,10 @@ ice_parse_cls_flower(struct net_device *filter_dev, struct ice_vsi *vsi,
 		flow_rule_match_control(rule, &match);
 
 		addr_type = match.key->addr_type;
+
+		if (flow_rule_has_control_flags(match.mask->flags,
+						fltr->extack))
+			return -EOPNOTSUPP;
 	}
 
 	if (addr_type == FLOW_DISSECTOR_KEY_IPV4_ADDRS) {
