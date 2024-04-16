bce93a162505f0bc0499888ace2f66bd56fb94b3
AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
2024-04-16 09:13:53 +0200

    ASoC: mediatek: Add common machine soundcard driver probe mechanism
    Add a common machine soundcard driver probe function that supports both
    DSP and AFE-direct usecases and also provides a hook for legacy machine
    soundcard driver probe mechanisms.
    
    Note that the hook is there because, even for legacy probe, a lot of the
    actual code can still be commonized, hence still reducing duplication
    for the legacy devicetree retrocompatibility cases.
    
    This common probe function deprecates all of the inconsistent previous
    probe mechanisms and aims to settle all of the MediaTek card drivers on
    consistent and common devicetree properties describing wanted DAIs,
    device specific DAI configuration and DAI links to codecs found on
    each device/board.
    
    Reviewed-by: Alexandre Mergnat <amergnat@baylibre.com>
    Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Link: https://lore.kernel.org/r/20240416071410.75620-2-angelogioacchino.delregno@collabora.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/mediatek/common/mtk-soundcard-driver.h
+++ sound/soc/mediatek/common/mtk-soundcard-driver.h
@@ -9,6 +9,24 @@
 #ifndef _MTK_SOUNDCARD_DRIVER_H_
 #define _MTK_SOUNDCARD_DRIVER_H_
 
+struct mtk_sof_priv;
+struct mtk_soc_card_data;
+
+struct mtk_platform_card_data {
+	struct snd_soc_card *card;
+	struct snd_soc_jack *jacks;
+	u8 num_jacks;
+	u8 flags;
+};
+
+struct mtk_soundcard_pdata {
+	const char *card_name;
+	struct mtk_platform_card_data *card_data;
+	struct mtk_sof_priv *sof_priv;
+	int (*soc_probe)(struct mtk_soc_card_data *card_data, bool legacy);
+};
+
 int parse_dai_link_info(struct snd_soc_card *card);
 void clean_card_reference(struct snd_soc_card *card);
+int mtk_soundcard_common_probe(struct platform_device *pdev);
 #endif
