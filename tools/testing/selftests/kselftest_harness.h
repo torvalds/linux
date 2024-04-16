caed8eba221533123192d39cc947f45cbb1e1db5
Jakub Kicinski <kuba@kernel.org>
2024-04-16 08:10:48 -0700

    selftests: kselftest_harness: fix Clang warning about zero-length format
    Apparently it's more legal to pass the format as NULL, than
    it is to use an empty string. Clang complains about empty
    formats:
    
    ./../kselftest_harness.h:1207:30: warning: format string is empty
    [-Wformat-zero-length]
     1207 |            diagnostic ? "%s" : "", diagnostic);
          |                                 ^~
    1 warning generated.
    
    Reported-by: Sean Christopherson <seanjc@google.com>
    Link: https://lore.kernel.org/all/20240409224256.1581292-1-seanjc@google.com
    Fixes: 378193eff339 ("selftests: kselftest_harness: let PASS / FAIL provide diagnostic")
    Tested-by: Sean Christopherson <seanjc@google.com>
    Reviewed-by: Muhammad Usama Anjum <usama.anjum@collabora.com>
    Link: https://lore.kernel.org/r/20240416151048.1682352-1-kuba@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- tools/testing/selftests/kselftest_harness.h
+++ tools/testing/selftests/kselftest_harness.h
@@ -1202,7 +1202,7 @@ void __run_test(struct __fixture_metadata *f,
 		diagnostic = "unknown";
 
 	ksft_test_result_code(t->exit_code, test_name,
-			      diagnostic ? "%s" : "", diagnostic);
+			      diagnostic ? "%s" : NULL, diagnostic);
 }
 
 static int test_harness_run(int argc, char **argv)
