5bfa66bf86d792bbcc76bc09cf99a2ae9d6e0eec
Benjamin Gray <bgray@linux.ibm.com>
2024-04-17 21:23:21 +1000

    selftests/powerpc/dexcr: Add DEXCR prctl interface test
    Some basic tests of the prctl interface of the DEXCR.
    
    Signed-off-by: Benjamin Gray <bgray@linux.ibm.com>
    [mpe: Add missing SPDX tag]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://msgid.link/20240417112325.728010-6-bgray@linux.ibm.com

--- tools/testing/selftests/powerpc/dexcr/dexcr.h
+++ tools/testing/selftests/powerpc/dexcr/dexcr.h
@@ -28,6 +28,16 @@
 
 bool dexcr_exists(void);
 
+bool pr_dexcr_aspect_supported(unsigned long which);
+
+bool pr_dexcr_aspect_editable(unsigned long which);
+
+int pr_get_dexcr(unsigned long pr_aspect);
+
+int pr_set_dexcr(unsigned long pr_aspect, unsigned long ctrl);
+
+unsigned int pr_which_to_aspect(unsigned long which);
+
 bool hashchk_triggers(void);
 
 enum dexcr_source {
