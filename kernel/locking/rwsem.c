1df05952a145791a588f39b6d0e4fa7fde7b9aa9
zhujingpeng <zhujingpeng@vivo.com>
2024-04-18 00:57:11 +0800

    ANDROID: vendor_hooks: add hooks in rwsem
    these hooks are required by the following features:
    1.For rwsem readers, currently only the latest reader will be
    recorded in sem->owner. We add hooks to record all readers which
    have acquired the lock, once there are UX threads blocked in the
    rwsem waiting list, these read_owners will be given high
    priority in scheduling.
    2.For rwsem writer, when a writer acquires the lock, we check
    whether there are UX threads blocked in the rwsem wait list. If
    so, we give this writer a high priority in scheduling so that it
    can release the lock as soon as possible.
    
    Both of these features can optimize the priority inversion
    problem caused by rwsem and improve system responsiveness and
    performance.
    
    Bug: 335408185
    Change-Id: I82a6fbb6acd2ce05d049e686b61e34e4d3b39a5e
    Signed-off-by: zhujingpeng <zhujingpeng@vivo.com>
    [jstultz: Rebased and resolved minor conflict]
    Signed-off-by: John Stultz <jstultz@google.com>
    (cherry picked from commit 188c41744ddcccef6daf6dcd4d6a444dabdc2f94)

--- kernel/locking/rwsem.c
+++ kernel/locking/rwsem.c
@@ -143,12 +143,14 @@ static inline void rwsem_set_owner(struct rw_semaphore *sem)
 {
 	lockdep_assert_preemption_disabled();
 	atomic_long_set(&sem->owner, (long)current);
+	trace_android_vh_record_rwsem_writer_owned(sem);
 }
 
 static inline void rwsem_clear_owner(struct rw_semaphore *sem)
 {
 	lockdep_assert_preemption_disabled();
 	atomic_long_set(&sem->owner, 0);
+	trace_android_vh_clear_rwsem_writer_owned(sem);
 }
 
 /*
@@ -181,6 +183,7 @@ static inline void __rwsem_set_reader_owned(struct rw_semaphore *sem,
 static inline void rwsem_set_reader_owned(struct rw_semaphore *sem)
 {
 	__rwsem_set_reader_owned(sem, current);
+	trace_android_vh_record_rwsem_reader_owned(sem, NULL);
 }
 
 /*
@@ -211,6 +214,7 @@ static inline void rwsem_clear_reader_owned(struct rw_semaphore *sem)
 {
 	unsigned long val = atomic_long_read(&sem->owner);
 
+	trace_android_vh_clear_rwsem_reader_owned(sem);
 	while ((val & ~RWSEM_OWNER_FLAGS_MASK) == (unsigned long)current) {
 		if (atomic_long_try_cmpxchg(&sem->owner, &val,
 					    val & RWSEM_OWNER_FLAGS_MASK))
@@ -220,6 +224,7 @@ static inline void rwsem_clear_reader_owned(struct rw_semaphore *sem)
 #else
 static inline void rwsem_clear_reader_owned(struct rw_semaphore *sem)
 {
+	trace_android_vh_clear_rwsem_reader_owned(sem);
 }
 #endif
 
@@ -561,6 +566,7 @@ static void rwsem_mark_wake(struct rw_semaphore *sem,
 
 	if (adjustment)
 		atomic_long_add(adjustment, &sem->count);
+	trace_android_vh_record_rwsem_reader_owned(sem, &wlist);
 
 	/* 2nd pass */
 	list_for_each_entry_safe(waiter, tmp, &wlist, list) {
