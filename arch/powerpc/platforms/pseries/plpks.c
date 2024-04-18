15e1f8425f1a7c2db18ed07959c38c8ef28e09d1
Nayna Jain <nayna@linux.ibm.com>
2024-04-17 23:12:30 -0400

    powerpc/pseries: make max polling consistent for longer H_CALLs
    [ Upstream commit 784354349d2c988590c63a5a001ca37b2a6d4da1 ]
    
    Currently, plpks_confirm_object_flushed() function polls for 5msec in
    total instead of 5sec.
    
    Keep max polling time consistent for all the H_CALLs, which take longer
    than expected, to be 5sec. Also, make use of fsleep() everywhere to
    insert delay.
    
    Reported-by: Nageswara R Sastry <rnsastry@linux.ibm.com>
    Fixes: 2454a7af0f2a ("powerpc/pseries: define driver for Platform KeyStore")
    Signed-off-by: Nayna Jain <nayna@linux.ibm.com>
    Tested-by: Nageswara R Sastry <rnsastry@linux.ibm.com>
    Reviewed-by: Andrew Donnellan <ajd@linux.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://msgid.link/20240418031230.170954-1-nayna@linux.ibm.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/powerpc/platforms/pseries/plpks.c
+++ arch/powerpc/platforms/pseries/plpks.c
@@ -254,8 +254,7 @@ static int plpks_confirm_object_flushed(struct label *label,
 		if (!rc && status == 1)
 			break;
 
-		usleep_range(PLPKS_FLUSH_SLEEP,
-			     PLPKS_FLUSH_SLEEP + PLPKS_FLUSH_SLEEP_RANGE);
+		fsleep(PLPKS_FLUSH_SLEEP);
 		timeout = timeout + PLPKS_FLUSH_SLEEP;
 	} while (timeout < PLPKS_MAX_TIMEOUT);
 
