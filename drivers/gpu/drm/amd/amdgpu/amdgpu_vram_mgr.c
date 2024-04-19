a68c7eaa7a8ffdec9287ba1561a668d674c20a13
Arunpravin Paneer Selvam <Arunpravin.PaneerSelvam@amd.com>
2024-04-19 12:05:37 +0530

    drm/amdgpu: Enable clear page functionality
    Add clear page support in vram memory region.
    
    v1(Christian):
      - Dont handle clear page as TTM flag since when moving the BO back
        in from GTT again we don't need that.
      - Make a specialized version of amdgpu_fill_buffer() which only
        clears the VRAM areas which are not already cleared
      - Drop the TTM_PL_FLAG_WIPE_ON_RELEASE check in
        amdgpu_object.c
    
    v2:
      - Modify the function name amdgpu_ttm_* (Alex)
      - Drop the delayed parameter (Christian)
      - handle amdgpu_res_cleared(&cursor) just above the size
        calculation (Christian)
      - Use AMDGPU_GEM_CREATE_VRAM_WIPE_ON_RELEASE for clearing the buffers
        in the free path to properly wait for fences etc.. (Christian)
    
    v3(Christian):
      - Remove buffer clear code in VRAM manager instead change the
        AMDGPU_GEM_CREATE_VRAM_WIPE_ON_RELEASE handling to set
        the DRM_BUDDY_CLEARED flag.
      - Remove ! from amdgpu_res_cleared(&cursor) check.
    
    v4(Christian):
      - vres flag setting move to vram manager file
      - use dma_fence_get_stub in amdgpu_ttm_clear_buffer function
      - make fence a mandatory parameter and drop the if and the get/put dance
    
    Signed-off-by: Arunpravin Paneer Selvam <Arunpravin.PaneerSelvam@amd.com>
    Suggested-by: Christian König <christian.koenig@amd.com>
    Acked-by: Felix Kuehling <felix.kuehling@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240419063538.11957-2-Arunpravin.PaneerSelvam@amd.com
    Signed-off-by: Christian König <christian.koenig@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_vram_mgr.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_vram_mgr.c
@@ -450,6 +450,7 @@ static int amdgpu_vram_mgr_new(struct ttm_resource_manager *man,
 {
 	struct amdgpu_vram_mgr *mgr = to_vram_mgr(man);
 	struct amdgpu_device *adev = to_amdgpu_device(mgr);
+	struct amdgpu_bo *bo = ttm_to_amdgpu_bo(tbo);
 	u64 vis_usage = 0, max_bytes, min_block_size;
 	struct amdgpu_vram_mgr_resource *vres;
 	u64 size, remaining_size, lpfn, fpfn;
@@ -500,6 +501,9 @@ static int amdgpu_vram_mgr_new(struct ttm_resource_manager *man,
 
 	if (place->flags & TTM_PL_FLAG_CONTIGUOUS)
 		vres->flags |= DRM_BUDDY_CONTIGUOUS_ALLOCATION;
+
+	if (bo->flags & AMDGPU_GEM_CREATE_VRAM_CLEARED)
+		vres->flags |= DRM_BUDDY_CLEAR_ALLOCATION;
 
 	if (fpfn || lpfn != mgr->mm.size)
 		/* Allocate blocks in desired range */
@@ -604,7 +608,7 @@ static void amdgpu_vram_mgr_del(struct ttm_resource_manager *man,
 
 	amdgpu_vram_mgr_do_reserve(man);
 
-	drm_buddy_free_list(mm, &vres->blocks, 0);
+	drm_buddy_free_list(mm, &vres->blocks, vres->flags);
 	mutex_unlock(&mgr->lock);
 
 	atomic64_sub(vis_usage, &mgr->vis_usage);
