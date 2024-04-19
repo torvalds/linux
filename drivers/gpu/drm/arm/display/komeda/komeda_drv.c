386030e519cec7be426918b9c4220275c8240f19
Thomas Zimmermann <tzimmermann@suse.de>
2024-04-19 10:29:15 +0200

    drm/arm/komeda: Use fbdev-dma
    Implement fbdev emulation with fbdev-dma. Fbdev-dma now supports
    damage handling, which is required by komeda. Avoids the overhead of
    fbdev-generic's additional shadow buffering. No functional changes.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Cc: Liviu Dudau <liviu.dudau@arm.com>
    Acked-by: Liviu Dudau <liviu.dudau@arm.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240419083331.7761-23-tzimmermann@suse.de

--- drivers/gpu/drm/arm/display/komeda/komeda_drv.c
+++ drivers/gpu/drm/arm/display/komeda/komeda_drv.c
@@ -9,7 +9,7 @@
 #include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
-#include <drm/drm_fbdev_generic.h>
+#include <drm/drm_fbdev_dma.h>
 #include <drm/drm_module.h>
 #include <drm/drm_of.h>
 #include "komeda_dev.h"
@@ -80,7 +80,7 @@ static int komeda_platform_probe(struct platform_device *pdev)
 	}
 
 	dev_set_drvdata(dev, mdrv);
-	drm_fbdev_generic_setup(&mdrv->kms->base, 32);
+	drm_fbdev_dma_setup(&mdrv->kms->base, 32);
 
 	return 0;
 
