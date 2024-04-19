5ab91447aa13b8b98bc11f5326f33500b0ee2c48
Thomas Zimmermann <tzimmermann@suse.de>
2024-04-19 10:29:27 +0200

    drm/tiny/ili9225: Use fbdev-dma
    Implement fbdev emulation with fbdev-dma. Fbdev-dma now supports
    damage handling, which is required by ili9225. Avoids the overhead of
    fbdev-generic's additional shadow buffering. No functional changes.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Cc: David Lechner <david@lechnology.com>
    Acked-by: David Lechner <david@lechnology.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240419083331.7761-35-tzimmermann@suse.de

--- drivers/gpu/drm/tiny/ili9225.c
+++ drivers/gpu/drm/tiny/ili9225.c
@@ -20,7 +20,7 @@
 #include <drm/drm_damage_helper.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_fb_dma_helper.h>
-#include <drm/drm_fbdev_generic.h>
+#include <drm/drm_fbdev_dma.h>
 #include <drm/drm_fourcc.h>
 #include <drm/drm_framebuffer.h>
 #include <drm/drm_gem_atomic_helper.h>
@@ -426,7 +426,7 @@ static int ili9225_probe(struct spi_device *spi)
 
 	spi_set_drvdata(spi, drm);
 
-	drm_fbdev_generic_setup(drm, 0);
+	drm_fbdev_dma_setup(drm, 0);
 
 	return 0;
 }
