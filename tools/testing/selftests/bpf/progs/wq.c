8290dba51910d36721ced6ccf03049ed6b7ea2ce
Benjamin Tissoires <bentiss@kernel.org>
2024-04-20 11:09:16 +0200

    selftests/bpf: wq: add bpf_wq_start() checks
    Allows to test if allocation/free works
    
    Signed-off-by: Benjamin Tissoires <bentiss@kernel.org>
    Link: https://lore.kernel.org/r/20240420-bpf_wq-v2-16-6c986a5a741f@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/testing/selftests/bpf/progs/wq.c
+++ tools/testing/selftests/bpf/progs/wq.c
@@ -49,11 +49,18 @@ struct {
 	__type(value, struct elem);
 } lru SEC(".maps");
 
+__u32 ok;
+__u32 ok_sleepable;
+
 static int test_elem_callback(void *map, int *key,
 		int (callback_fn)(void *map, int *key, struct bpf_wq *wq))
 {
 	struct elem init = {}, *val;
 	struct bpf_wq *wq;
+
+	if ((ok & (1 << *key) ||
+	    (ok_sleepable & (1 << *key))))
+		return -22;
 
 	if (map == &lru &&
 	    bpf_map_update_elem(map, key, &init, 0))
@@ -70,6 +77,9 @@ static int test_elem_callback(void *map, int *key,
 	if (bpf_wq_set_callback(wq, callback_fn, 0))
 		return -4;
 
+	if (bpf_wq_start(wq, 0))
+		return -5;
+
 	return 0;
 }
 
@@ -78,6 +88,10 @@ static int test_hmap_elem_callback(void *map, int *key,
 {
 	struct hmap_elem init = {}, *val;
 	struct bpf_wq *wq;
+
+	if ((ok & (1 << *key) ||
+	    (ok_sleepable & (1 << *key))))
+		return -22;
 
 	if (bpf_map_update_elem(map, key, &init, 0))
 		return -1;
@@ -93,11 +107,11 @@ static int test_hmap_elem_callback(void *map, int *key,
 	if (bpf_wq_set_callback(wq, callback_fn, 0))
 		return -4;
 
+	if (bpf_wq_start(wq, 0))
+		return -5;
+
 	return 0;
 }
-
-__u32 ok;
-__u32 ok_sleepable;
 
 /* callback for non sleepable workqueue */
 static int wq_callback(void *map, int *key, struct bpf_wq *work)
