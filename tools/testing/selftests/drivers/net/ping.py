a48a87c0866444343354f211d30b5acab54bb800
Jakub Kicinski <kuba@kernel.org>
2024-04-19 19:52:34 -0700

    selftests: drv-net: add a trivial ping test
    Add a very simple test for testing with a remote system.
    Both IPv4 and IPv6 connectivity is optional, later change
    will add checks to skip tests based on available addresses.
    
    Using netdevsim:
    
     $ ./run_kselftest.sh -t drivers/net:ping.py
     TAP version 13
     1..1
     # timeout set to 45
     # selftests: drivers/net: ping.py
     # KTAP version 1
     # 1..2
     # ok 1 ping.test_v4
     # ok 2 ping.test_v6
     # # Totals: pass:2 fail:0 xfail:0 xpass:0 skip:0 error:0
     ok 1 selftests: drivers/net: ping.py
    
    Command line SSH:
    
     $ NETIF=virbr0 REMOTE_TYPE=ssh REMOTE_ARGS=root@192.168.122.123 \
        LOCAL_V4=192.168.122.1 REMOTE_V4=192.168.122.123 \
        ./tools/testing/selftests/drivers/net/ping.py
     KTAP version 1
     1..2
     ok 1 ping.test_v4
     ok 2 ping.test_v6 # SKIP Test requires IPv6 connectivity
     # Totals: pass:1 fail:0 xfail:1 xpass:0 skip:0 error:0
    
    Existing devices placed in netns (and using net.config):
    
     $ cat drivers/net/net.config
     NETIF=veth0
     REMOTE_TYPE=netns
     REMOTE_ARGS=red
     LOCAL_V4="192.168.1.1"
     REMOTE_V4="192.168.1.2"
    
     $ ./run_kselftest.sh -t drivers/net:ping.py
     TAP version 13
     1..1
     # timeout set to 45
     # selftests: drivers/net: ping.py
     # KTAP version 1
     # 1..2
     # ok 1 ping.test_v4
     # ok 2 ping.test_v6 # SKIP Test requires IPv6 connectivity
     # # Totals: pass:1 fail:0 xfail:1 xpass:0 skip:0 error:0
    
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Link: https://lore.kernel.org/r/20240420025237.3309296-5-kuba@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

---
+++ tools/testing/selftests/drivers/net/ping.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+
+from lib.py import ksft_run, ksft_exit
+from lib.py import NetDrvEpEnv
+from lib.py import cmd
+
+
+def test_v4(cfg) -> None:
+    cmd(f"ping -c 1 -W0.5 {cfg.remote_v4}")
+    cmd(f"ping -c 1 -W0.5 {cfg.v4}", host=cfg.remote)
+
+
+def test_v6(cfg) -> None:
+    cmd(f"ping -c 1 -W0.5 {cfg.remote_v6}")
+    cmd(f"ping -c 1 -W0.5 {cfg.v6}", host=cfg.remote)
+
+
+def main() -> None:
+    with NetDrvEpEnv(__file__) as cfg:
+        ksft_run([test_v4, test_v6],
+                 args=(cfg, ))
+    ksft_exit()
+
+
+if __name__ == "__main__":
+    main()
