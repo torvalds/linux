77122bf9e3dfd927de4bf4a75b6297f928313e7e
Arnd Bergmann <arnd@arndb.de>
2024-04-23 23:14:14 +0200

    openrisc: convert to generic syscall table
    The uapi/asm/unistd_32.h and asm/syscall_table_32.h headers can now be
    generated from scripts/syscall.tbl, which makes this consistent with
    the other architectures that have their own syscall.tbl.
    
    openrisc has one extra system call that gets added to scripts/syscall.tbl.
    
    The time32, stat64, rlimit and renameat entries in the syscall_abis_32
    line are for system calls that were part of the generic ABI when
    arch/nios2 got added but are no longer enabled by default for new
    architectures.
    
    Both the user visible side of asm/unistd.h and the internal syscall
    table in the kernel should have the same effective contents after this.
    
    When asm/syscalls.h is included in kernel/fork.c for the purpose of
    type checking, the redirection macros cause problems.  Move these so
    only the references get redirected.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/openrisc/include/uapi/asm/unistd.h
+++ arch/openrisc/include/uapi/asm/unistd.h
@@ -17,16 +17,4 @@
  * (at your option) any later version.
  */
 
-#define sys_mmap2 sys_mmap_pgoff
-
-#define __ARCH_WANT_RENAMEAT
-#define __ARCH_WANT_STAT64
-#define __ARCH_WANT_SET_GET_RLIMIT
-#define __ARCH_WANT_SYS_FORK
-#define __ARCH_WANT_SYS_CLONE
-#define __ARCH_WANT_TIME32_SYSCALLS
-
-#include <asm-generic/unistd.h>
-
-#define __NR_or1k_atomic __NR_arch_specific_syscall
-__SYSCALL(__NR_or1k_atomic, sys_or1k_atomic)
+#include <asm/unistd_32.h>
