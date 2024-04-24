e7af4014b4a2ea1fc95724d733de996a32b4b1dd
Maninder Singh <maninder1.s@samsung.com>
2024-04-24 16:48:37 +0530

    mm: page_owner: fix wrong information in dump_page_owner
    With commit ea4b5b33bf8a ("mm,page_owner: update metadata for tail
    pages"), new API __update_page_owner_handle was introduced and arguemnt
    was passed in wrong order from __set_page_owner and thus page_owner is
    giving wrong data.
    
    [   15.982420] page last allocated via order 0, migratetype Unmovable, gfp_mask 0xcc0(GFP_KERNEL), pid 80, tgid -1210279584 (insmod), ts 80, free_ts 0
    
    Fixing the same.
    Correct output:
    [   14.556482] page last allocated via order 0, migratetype Unmovable, gfp_mask 0xcc0(GFP_KERNEL), pid 80, tgid 80 (insmod), ts 14552004992, free_ts 0
    
    Link: https://lkml.kernel.org/r/20240424111838.3782931-1-hariom1.p@samsung.com
    Fixes: ea4b5b33bf8a ("mm,page_owner: update metadata for tail pages")
    Signed-off-by: Maninder Singh <maninder1.s@samsung.com>
    Signed-off-by: Hariom Panthi <hariom1.p@samsung.com>
    Acked-by: Oscar Salvador <osalvador@suse.de>
    Cc: Christoph Hellwig <hch@infradead.org>
    Cc: Lorenzo Stoakes <lstoakes@gmail.com>
    Cc: Rohit Thapliyal <r.thapliyal@samsung.com>
    Cc: Uladzislau Rezki (Sony) <urezki@gmail.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_owner.c
+++ mm/page_owner.c
@@ -328,7 +328,7 @@ noinline void __set_page_owner(struct page *page, unsigned short order,
 	if (unlikely(!page_ext))
 		return;
 	__update_page_owner_handle(page_ext, handle, order, gfp_mask, -1,
-				   current->pid, current->tgid, ts_nsec,
+				   ts_nsec, current->pid, current->tgid,
 				   current->comm);
 	page_ext_put(page_ext);
 	inc_stack_record_count(handle, gfp_mask, 1 << order);
