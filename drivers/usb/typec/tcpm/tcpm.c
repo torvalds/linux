cfcd544a9974c6b6fb37ca385146e4796dcaf66d
Amit Sunil Dhamne <amitsd@google.com>
2024-04-24 15:32:16 -0700

    usb: typec: tcpm: unregister existing source caps before re-registration
    commit 230ecdf71a644c9c73e0e6735b33173074ae3f94 upstream.
    
    Check and unregister existing source caps in tcpm_register_source_caps
    function before registering new ones. This change fixes following
    warning when port partner resends source caps after negotiating PD contract
    for the purpose of re-negotiation.
    
    [  343.135030][  T151] sysfs: cannot create duplicate filename '/devices/virtual/usb_power_delivery/pd1/source-capabilities'
    [  343.135071][  T151] Call trace:
    [  343.135076][  T151]  dump_backtrace+0xe8/0x108
    [  343.135099][  T151]  show_stack+0x18/0x24
    [  343.135106][  T151]  dump_stack_lvl+0x50/0x6c
    [  343.135119][  T151]  dump_stack+0x18/0x24
    [  343.135126][  T151]  sysfs_create_dir_ns+0xe0/0x140
    [  343.135137][  T151]  kobject_add_internal+0x228/0x424
    [  343.135146][  T151]  kobject_add+0x94/0x10c
    [  343.135152][  T151]  device_add+0x1b0/0x4c0
    [  343.135187][  T151]  device_register+0x20/0x34
    [  343.135195][  T151]  usb_power_delivery_register_capabilities+0x90/0x20c
    [  343.135209][  T151]  tcpm_pd_rx_handler+0x9f0/0x15b8
    [  343.135216][  T151]  kthread_worker_fn+0x11c/0x260
    [  343.135227][  T151]  kthread+0x114/0x1bc
    [  343.135235][  T151]  ret_from_fork+0x10/0x20
    [  343.135265][  T151] kobject: kobject_add_internal failed for source-capabilities with -EEXIST, don't try to register things with the same name in the same directory.
    
    Fixes: 8203d26905ee ("usb: typec: tcpm: Register USB Power Delivery Capabilities")
    Cc: linux-usb@vger.kernel.org
    Cc: stable@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Cc: Mark Brown <broonie@kernel.org>
    Signed-off-by: Amit Sunil Dhamne <amitsd@google.com>
    Reviewed-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Link: https://lore.kernel.org/r/20240424223227.1807844-1-amitsd@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/typec/tcpm/tcpm.c
+++ drivers/usb/typec/tcpm/tcpm.c
@@ -2416,7 +2416,7 @@ static int tcpm_register_sink_caps(struct tcpm_port *port)
 {
 	struct usb_power_delivery_desc desc = { port->negotiated_rev };
 	struct usb_power_delivery_capabilities_desc caps = { };
-	struct usb_power_delivery_capabilities *cap;
+	struct usb_power_delivery_capabilities *cap = port->partner_source_caps;
 
 	if (!port->partner_pd)
 		port->partner_pd = usb_power_delivery_register(NULL, &desc);
@@ -2425,6 +2425,9 @@ static int tcpm_register_sink_caps(struct tcpm_port *port)
 
 	memcpy(caps.pdo, port->sink_caps, sizeof(u32) * port->nr_sink_caps);
 	caps.role = TYPEC_SINK;
+
+	if (cap)
+		usb_power_delivery_unregister_capabilities(cap);
 
 	cap = usb_power_delivery_register_capabilities(port->partner_pd, &caps);
 	if (IS_ERR(cap))
