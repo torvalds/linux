2a8f7464d33c759c3848737399d155a6c83c1ffa
Sunil Khatri <sunil.khatri@amd.com>
2024-04-25 15:45:56 +0530

    drm/amdgpu: skip ip dump if devcoredump flag is set
    Do not dump the ip registers during driver reload
    in passthrough environment.
    
    Signed-off-by: Sunil Khatri <sunil.khatri@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -5368,14 +5368,15 @@ int amdgpu_do_asic_reset(struct list_head *device_list_handle,
 	tmp_adev = list_first_entry(device_list_handle, struct amdgpu_device,
 				    reset_list);
 
-	if (!test_bit(AMDGPU_SKIP_COREDUMP, &reset_context->flags))
+	if (!test_bit(AMDGPU_SKIP_COREDUMP, &reset_context->flags)) {
 		amdgpu_reset_reg_dumps(tmp_adev);
 
-	/* Trigger ip dump before we reset the asic */
-	for (i = 0; i < tmp_adev->num_ip_blocks; i++)
-		if (tmp_adev->ip_blocks[i].version->funcs->dump_ip_state)
-			tmp_adev->ip_blocks[i].version->funcs->dump_ip_state(
-				(void *)tmp_adev);
+		/* Trigger ip dump before we reset the asic */
+		for (i = 0; i < tmp_adev->num_ip_blocks; i++)
+			if (tmp_adev->ip_blocks[i].version->funcs->dump_ip_state)
+				tmp_adev->ip_blocks[i].version->funcs
+				->dump_ip_state((void *)tmp_adev);
+	}
 
 	reset_context->reset_device_list = device_list_handle;
 	r = amdgpu_reset_perform_reset(tmp_adev, reset_context);
