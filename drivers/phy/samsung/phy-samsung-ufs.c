8b19c45ae4c0480ea07ab9f33c43d0fa6ea43aa1
Peter Griffin <peter.griffin@linaro.org>
2024-04-26 11:41:28 +0100

    phy: samsung-ufs: ufs: remove superfluous mfd/syscon.h header
    mfd/syscon.h header is no longer required since the update to
    exynos_get_pmu_regmap_by_phandle() api.
    
    Fixes: f2c6d0fa197a ("phy: samsung-ufs: use exynos_get_pmu_regmap_by_phandle() to obtain PMU regmap")
    Signed-off-by: Peter Griffin <peter.griffin@linaro.org>
    Link: https://lore.kernel.org/r/20240426104129.2211949-2-peter.griffin@linaro.org
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/phy/samsung/phy-samsung-ufs.c
+++ drivers/phy/samsung/phy-samsung-ufs.c
@@ -13,7 +13,6 @@
 #include <linux/of.h>
 #include <linux/io.h>
 #include <linux/iopoll.h>
-#include <linux/mfd/syscon.h>
 #include <linux/module.h>
 #include <linux/phy/phy.h>
 #include <linux/platform_device.h>
