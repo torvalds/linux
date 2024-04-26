c9deb9a4f574b49bfd00be88aba470659d2db671
Peter Griffin <peter.griffin@linaro.org>
2024-04-26 13:20:02 +0100

    scsi: ufs: exynos: Allow max frequencies up to 267Mhz
    Platforms such as Tensor gs101 the pclk frequency is 267Mhz.  Increase
    PCLK_AVAIL_MAX so we don't fail the frequency check.
    
    Signed-off-by: Peter Griffin <peter.griffin@linaro.org>
    Link: https://lore.kernel.org/r/20240426122004.2249178-5-peter.griffin@linaro.org
    Acked-by: Krzysztof Kozlowski <krzk@kernel.org>
    Tested-by: Will McVicker <willmcvicker@google.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/ufs/host/ufs-exynos.h
+++ drivers/ufs/host/ufs-exynos.h
@@ -116,7 +116,7 @@ struct exynos_ufs;
 #define PA_HIBERN8TIME_VAL	0x20
 
 #define PCLK_AVAIL_MIN	70000000
-#define PCLK_AVAIL_MAX	167000000
+#define PCLK_AVAIL_MAX	267000000
 
 struct exynos_ufs_uic_attr {
 	/* TX Attributes */
