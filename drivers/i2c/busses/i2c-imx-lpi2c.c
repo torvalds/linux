8dacd79fec3cd86ddf6b8c87b0179c5b13250a9c
Wolfram Sang <wsa+renesas@sang-engineering.com>
2024-04-27 22:35:57 +0200

    i2c: imx-lpi2c: use 'time_left' variable with wait_for_completion_timeout()
    There is a confusing pattern in the kernel to use a variable named 'timeout' to
    store the result of wait_for_completion_timeout() causing patterns like:
    
    	timeout = wait_for_completion_timeout(...)
    	if (!timeout) return -ETIMEDOUT;
    
    with all kinds of permutations. Use 'time_left' as a variable to make the code
    self explaining.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Andi Shyti <andi.shyti@kernel.org>

--- drivers/i2c/busses/i2c-imx-lpi2c.c
+++ drivers/i2c/busses/i2c-imx-lpi2c.c
@@ -307,11 +307,11 @@ static int lpi2c_imx_master_disable(struct lpi2c_imx_struct *lpi2c_imx)
 
 static int lpi2c_imx_msg_complete(struct lpi2c_imx_struct *lpi2c_imx)
 {
-	unsigned long timeout;
+	unsigned long time_left;
 
-	timeout = wait_for_completion_timeout(&lpi2c_imx->complete, HZ);
+	time_left = wait_for_completion_timeout(&lpi2c_imx->complete, HZ);
 
-	return timeout ? 0 : -ETIMEDOUT;
+	return time_left ? 0 : -ETIMEDOUT;
 }
 
 static int lpi2c_imx_txfifo_empty(struct lpi2c_imx_struct *lpi2c_imx)
