567173472aaea47d92b03ef6f1874f79bfdfc94c
richter <80592076+fantik86@users.noreply.github.com>
2024-04-29 02:17:41 +0300

    Tiny update mem.c

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -533,7 +533,7 @@ static unsigned long get_unmapped_area_zero(struct file *file,
 				unsigned long pgoff, unsigned long flags)
 {
 #ifdef CONFIG_MMU
-	if (flags & MAP_SHARED) {
+	if (flags & MAP_SHARED)
 		/*
 		 * mmap_zero() will call shmem_zero_setup() to create a file,
 		 * so use shmem's get_unmapped_area in case it can be huge;
@@ -541,7 +541,6 @@ static unsigned long get_unmapped_area_zero(struct file *file,
 		 * so as not to confuse shmem with our handle on "/dev/zero".
 		 */
 		return shmem_get_unmapped_area(NULL, addr, len, pgoff, flags);
-	}
 
 	/* Otherwise flags & MAP_PRIVATE: with no shmem object beneath it */
 	return current->mm->get_unmapped_area(file, addr, len, pgoff, flags);
