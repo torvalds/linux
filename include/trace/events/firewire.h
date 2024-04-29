57614c28843d4ec34c12ca67dd11411396aab55d
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2024-04-29 13:32:14 +0900

    firewire: core: add support for Linux kernel tracepoints
    The Linux Kernel Tracepoints framework is enough useful to trace
    packet data inbound to and outbound from core.
    
    This commit adds firewire subsystem to use the framework.
    
    Link: https://lore.kernel.org/r/20240429043218.609398-2-o-takashi@sakamocchi.jp
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ include/trace/events/firewire.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright (c) 2024 Takashi Sakamoto
+
+#define TRACE_SYSTEM	firewire
+
+#if !defined(_FIREWIRE_TRACE_EVENT_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _FIREWIRE_TRACE_EVENT_H
+
+#include <linux/tracepoint.h>
+
+// Placeholder for future use.
+
+#endif // _FIREWIRE_TRACE_EVENT_H
+
+#include <trace/define_trace.h>
