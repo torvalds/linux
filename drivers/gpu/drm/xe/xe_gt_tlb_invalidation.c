e29a7a34c3cf140ceb2f994a8eae0b68d21e357e
Nirmoy Das <nirmoy.das@intel.com>
2024-04-29 22:30:39 +0200

    drm/xe: Remove uninitialized end var from xe_gt_tlb_invalidation_range()
    This fixes commit c4f18703629d ("drm/xe: Add
    xe_gt_tlb_invalidation_range and convert PT layer to use this")
    which added the end variable as part of the function param.
    
    v2: Add fixes tag(Matt)
    
    Fixes: c4f18703629d ("drm/xe: Add xe_gt_tlb_invalidation_range and convert PT layer to use this")
    Cc: Matthew Brost <matthew.brost@intel.com>
    Signed-off-by: Nirmoy Das <nirmoy.das@intel.com>
    Reviewed-by: Matthew Brost <matthew.brost@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240429203039.26918-1-nirmoy.das@intel.com
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- drivers/gpu/drm/xe/xe_gt_tlb_invalidation.c
+++ drivers/gpu/drm/xe/xe_gt_tlb_invalidation.c
@@ -305,7 +305,7 @@ int xe_gt_tlb_invalidation_range(struct xe_gt *gt,
 	} else {
 		u64 orig_start = start;
 		u64 length = end - start;
-		u64 align, end;
+		u64 align;
 
 		if (length < SZ_4K)
 			length = SZ_4K;
