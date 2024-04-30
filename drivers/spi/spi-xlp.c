594aa75d6bdda85b5fd027a5056d8cd1345c1db3
Wolfram Sang <wsa+renesas@sang-engineering.com>
2024-04-30 13:41:40 +0200

    spi: xlp: use 'time_left' variable with wait_for_completion_timeout()
    There is a confusing pattern in the kernel to use a variable named 'timeout' to
    store the result of wait_for_completion_timeout() causing patterns like:
    
    	timeout = wait_for_completion_timeout(...)
    	if (!timeout) return -ETIMEDOUT;
    
    with all kinds of permutations. Use 'time_left' as a variable to make the code
    self explaining.
    
    Fix to the proper variable type 'unsigned long' while here.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Link: https://lore.kernel.org/r/20240430114142.28551-9-wsa+renesas@sang-engineering.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-xlp.c
+++ drivers/spi/spi-xlp.c
@@ -270,7 +270,7 @@ static int xlp_spi_xfer_block(struct  xlp_spi_priv *xs,
 		const unsigned char *tx_buf,
 		unsigned char *rx_buf, int xfer_len, int cmd_cont)
 {
-	int timeout;
+	unsigned long time_left;
 	u32 intr_mask = 0;
 
 	xs->tx_buf = tx_buf;
@@ -299,11 +299,11 @@ static int xlp_spi_xfer_block(struct  xlp_spi_priv *xs,
 	intr_mask |= XLP_SPI_INTR_DONE;
 	xlp_spi_reg_write(xs, xs->cs, XLP_SPI_INTR_EN, intr_mask);
 
-	timeout = wait_for_completion_timeout(&xs->done,
-				msecs_to_jiffies(1000));
+	time_left = wait_for_completion_timeout(&xs->done,
+						msecs_to_jiffies(1000));
 	/* Disable interrupts */
 	xlp_spi_reg_write(xs, xs->cs, XLP_SPI_INTR_EN, 0x0);
-	if (!timeout) {
+	if (!time_left) {
 		dev_err(&xs->dev, "xfer timedout!\n");
 		goto out;
 	}
