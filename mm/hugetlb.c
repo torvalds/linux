b8a2528835b31718286e7436529917e1f521bf6f
David Hildenbrand <david@redhat.com>
2024-05-02 10:52:59 +0200

    mm/hugetlb: document why hugetlb uses folio_mapcount() for COW reuse decisions
    Let's document why hugetlb still uses folio_mapcount() and is prone to
    leaking memory between processes, for example using vmsplice() that still
    uses FOLL_GET.
    
    More details can be found in [1], especially around how hugetlb pages
    cannot really be overcommitted, and why we don't particularly care about
    these vmsplice() leaks for hugetlb -- in contrast to ordinary memory.
    
    [1] https://lore.kernel.org/all/8b42a24d-caf0-46ef-9e15-0f88d47d2f21@redhat.com/
    
    Link: https://lkml.kernel.org/r/20240502085259.103784-3-david@redhat.com
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Suggested-by: Peter Xu <peterx@redhat.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Shuah Khan <shuah@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -5963,6 +5963,13 @@ retry_avoidcopy:
 	/*
 	 * If no-one else is actually using this page, we're the exclusive
 	 * owner and can reuse this page.
+	 *
+	 * Note that we don't rely on the (safer) folio refcount here, because
+	 * copying the hugetlb folio when there are unexpected (temporary)
+	 * folio references could harm simple fork()+exit() users when
+	 * we run out of free hugetlb folios: we would have to kill processes
+	 * in scenarios that used to work. As a side effect, there can still
+	 * be leaks between processes, for example, with FOLL_GET users.
 	 */
 	if (folio_mapcount(old_folio) == 1 && folio_test_anon(old_folio)) {
 		if (!PageAnonExclusive(&old_folio->page)) {
