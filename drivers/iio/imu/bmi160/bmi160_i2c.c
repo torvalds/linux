cedb9bd816694736dc27a02b123b62395205181d
Danila Tikhonov <danila@jiaxyga.com>
2024-05-05 07:36:54 +0200

    iio: imu: bmi160: add support for bmi120
    Add support for bmi120 low power variant of bmi160.
    Relax failure to match ID to a warning rather than probe fail.
    This allows for fallback compatibles, whilst retaining a useful
    debugging message if they turn out not to be so compatible due to
    badly behaved firmware.
    
    Signed-off-by: Danila Tikhonov <danila@jiaxyga.com>
    Co-developed-by: Barnabás Czémán <trabarni@gmail.com>
    Signed-off-by: Barnabás Czémán <trabarni@gmail.com>
    Link: https://lore.kernel.org/r/20240505-bmi120-v3-1-15cee3d0b2ef@gmail.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/imu/bmi160/bmi160_i2c.c
+++ drivers/iio/imu/bmi160/bmi160_i2c.c
@@ -37,6 +37,7 @@ static int bmi160_i2c_probe(struct i2c_client *client)
 }
 
 static const struct i2c_device_id bmi160_i2c_id[] = {
+	{"bmi120", 0},
 	{"bmi160", 0},
 	{}
 };
@@ -52,12 +53,14 @@ static const struct acpi_device_id bmi160_acpi_match[] = {
 	 * the affected devices are from 2021/2022.
 	 */
 	{"10EC5280", 0},
+	{"BMI0120", 0},
 	{"BMI0160", 0},
 	{ },
 };
 MODULE_DEVICE_TABLE(acpi, bmi160_acpi_match);
 
 static const struct of_device_id bmi160_of_match[] = {
+	{ .compatible = "bosch,bmi120" },
 	{ .compatible = "bosch,bmi160" },
 	{ },
 };
