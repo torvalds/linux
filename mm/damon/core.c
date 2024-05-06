3b15f9d1c22dfe82efd03cb7acc2eeb557c735b5
Alex Rusuf <yorha.op@gmail.com>
2024-05-06 11:02:38 -0700

    mm/damon/core: fix return value from damos_wmark_metric_value
    damos_wmark_metric_value's return value is 'unsigned long', so returning
    -EINVAL as 'unsigned long' may turn out to be very different from the
    expected one (using 2's complement) and treat as usual matric's value. 
    So, fix that, checking if returned value is not 0.
    
    Link: https://lkml.kernel.org/r/20240506180238.53842-1-sj@kernel.org
    Fixes: ee801b7dd782 ("mm/damon/schemes: activate schemes based on a watermarks mechanism")
    Signed-off-by: Alex Rusuf <yorha.op@gmail.com>
    Reviewed-by: SeongJae Park <sj@kernel.org>
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/damon/core.c
+++ mm/damon/core.c
@@ -1481,12 +1481,14 @@ static bool kdamond_need_stop(struct damon_ctx *ctx)
 	return true;
 }
 
-static unsigned long damos_wmark_metric_value(enum damos_wmark_metric metric)
+static int damos_get_wmark_metric_value(enum damos_wmark_metric metric,
+					unsigned long *metric_value)
 {
 	switch (metric) {
 	case DAMOS_WMARK_FREE_MEM_RATE:
-		return global_zone_page_state(NR_FREE_PAGES) * 1000 /
+		*metric_value = global_zone_page_state(NR_FREE_PAGES) * 1000 /
 		       totalram_pages();
+		return 0;
 	default:
 		break;
 	}
@@ -1501,10 +1503,9 @@ static unsigned long damos_wmark_wait_us(struct damos *scheme)
 {
 	unsigned long metric;
 
-	if (scheme->wmarks.metric == DAMOS_WMARK_NONE)
+	if (damos_get_wmark_metric_value(scheme->wmarks.metric, &metric))
 		return 0;
 
-	metric = damos_wmark_metric_value(scheme->wmarks.metric);
 	/* higher than high watermark or lower than low watermark */
 	if (metric > scheme->wmarks.high || scheme->wmarks.low > metric) {
 		if (scheme->wmarks.activated)
