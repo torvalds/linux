6a100f53d1c9f7a0f0d603a707de94edbae98af6
Chaitanya Kumar Borah <chaitanya.kumar.borah@intel.com>
2024-05-06 10:55:31 +0530

    ALSA: hda: Add Intel BMG PCI ID and HDMI codec vid
    Add HD Audio PCI ID and HDMI codec vendor ID for Intel Battlemage.
    
    Signed-off-by: Chaitanya Kumar Borah <chaitanya.kumar.borah@intel.com>
    Message-ID: <20240506052531.1150062-1-chaitanya.kumar.borah@intel.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/sound/hdaudio.h
+++ include/sound/hdaudio.h
@@ -732,6 +732,7 @@ static inline unsigned int snd_array_index(struct snd_array *array, void *ptr)
 			{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_HDA_DG2_0) }, \
 			{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_HDA_DG2_1) }, \
 			{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_HDA_DG2_2) }, \
+			{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_HDA_BMG) }, \
 			{ } \
 		}, pci) || HDA_CONTROLLER_IS_HSW(pci))
 
