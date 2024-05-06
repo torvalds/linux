719c15a75ebf3bda3ca718fe8e0ce63d262ec7ae
Christoph Hellwig <hch@lst.de>
2024-05-06 06:20:27 +0200

    blk-lib: check for kill signal in ioctl BLKDISCARD
    Discards can access a significant capacity and take longer than the user
    expected.  A user may change their mind about wanting to run that command
    and attempt to kill the process and do something else with their device.
    But since the task is uninterruptable, they have to wait for it to
    finish, which could be many hours.
    
    Open code blkdev_issue_discard in the BLKDISCARD ioctl handler and check
    for a fatal signal at each iteration so the user doesn't have to wait
    for their regretted operation to complete naturally.
    
    Heavily based on an earlier patch from Keith Busch.
    
    Reported-by: Conrad Meyer <conradmeyer@meta.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20240506042027.2289826-7-hch@lst.de
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/ioctl.c
+++ block/ioctl.c
@@ -96,9 +96,11 @@ static int blk_ioctl_discard(struct block_device *bdev, blk_mode_t mode,
 		unsigned long arg)
 {
 	unsigned int bs_mask = bdev_logical_block_size(bdev) - 1;
-	uint64_t range[2];
-	uint64_t start, len;
 	struct inode *inode = bdev->bd_inode;
+	uint64_t range[2], start, len;
+	struct bio *prev = NULL, *bio;
+	sector_t sector, nr_sects;
+	struct blk_plug plug;
 	int err;
 
 	if (!(mode & BLK_OPEN_WRITE))
@@ -127,7 +129,32 @@ static int blk_ioctl_discard(struct block_device *bdev, blk_mode_t mode,
 	err = truncate_bdev_range(bdev, mode, start, start + len - 1);
 	if (err)
 		goto fail;
-	err = blkdev_issue_discard(bdev, start >> 9, len >> 9, GFP_KERNEL);
+
+	sector = start >> SECTOR_SHIFT;
+	nr_sects = len >> SECTOR_SHIFT;
+
+	blk_start_plug(&plug);
+	while (1) {
+		if (fatal_signal_pending(current)) {
+			if (prev)
+				bio_await_chain(prev);
+			err = -EINTR;
+			goto out_unplug;
+		}
+		bio = blk_alloc_discard_bio(bdev, &sector, &nr_sects,
+				GFP_KERNEL);
+		if (!bio)
+			break;
+		prev = bio_chain_and_submit(prev, bio);
+	}
+	if (prev) {
+		err = submit_bio_wait(prev);
+		if (err == -EOPNOTSUPP)
+			err = 0;
+		bio_put(prev);
+	}
+out_unplug:
+	blk_finish_plug(&plug);
 fail:
 	filemap_invalidate_unlock(inode->i_mapping);
 	return err;
