5b882c1e5a355d034c0e08fba2402b4451765ab2
Michal Wajdeczko <michal.wajdeczko@intel.com>
2024-05-06 22:52:54 +0200

    drm/xe: Fix xe_mocs.h
    We don't need to include <linux/types.h>.
    We don't use struct xe_exec_queue here.
    We should sort forward declarations.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240506205254.2659-1-michal.wajdeczko@intel.com

--- drivers/gpu/drm/xe/xe_mocs.h
+++ drivers/gpu/drm/xe/xe_mocs.h
@@ -6,11 +6,8 @@
 #ifndef _XE_MOCS_H_
 #define _XE_MOCS_H_
 
-#include <linux/types.h>
-
-struct xe_exec_queue;
-struct xe_gt;
 struct drm_printer;
+struct xe_gt;
 
 void xe_mocs_init_early(struct xe_gt *gt);
 void xe_mocs_init(struct xe_gt *gt);
