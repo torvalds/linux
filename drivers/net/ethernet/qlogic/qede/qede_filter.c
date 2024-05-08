f84d52776ccf4d72a1936940019937303ef5dfcd
Asbjørn Sloth Tønnesen <ast@fiberby.net>
2024-05-08 14:33:55 +0000

    net: qede: use extack in qede_flow_parse_tcp_v4()
    Convert qede_flow_parse_tcp_v4() to take extack,
    and drop the edev argument.
    
    Pass extack in call to qede_flow_parse_v4_common().
    
    In call to qede_flow_parse_tcp_v4(), use NULL as extack
    for now, until a subsequent patch makes extack available.
    
    Only compile tested.
    
    Signed-off-by: Asbjørn Sloth Tønnesen <ast@fiberby.net>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://lore.kernel.org/r/20240508143404.95901-8-ast@fiberby.net
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/qlogic/qede/qede_filter.c
+++ drivers/net/ethernet/qlogic/qede/qede_filter.c
@@ -1796,13 +1796,13 @@ qede_flow_parse_tcp_v6(struct flow_rule *rule, struct qede_arfs_tuple *tuple,
 }
 
 static int
-qede_flow_parse_tcp_v4(struct qede_dev *edev, struct flow_rule *rule,
-		     struct qede_arfs_tuple *tuple)
+qede_flow_parse_tcp_v4(struct flow_rule *rule, struct qede_arfs_tuple *tuple,
+		       struct netlink_ext_ack *extack)
 {
 	tuple->ip_proto = IPPROTO_TCP;
 	tuple->eth_proto = htons(ETH_P_IP);
 
-	return qede_flow_parse_v4_common(rule, tuple, NULL);
+	return qede_flow_parse_v4_common(rule, tuple, extack);
 }
 
 static int
@@ -1860,7 +1860,7 @@ qede_parse_flow_attr(struct qede_dev *edev, __be16 proto,
 	}
 
 	if (ip_proto == IPPROTO_TCP && proto == htons(ETH_P_IP))
-		rc = qede_flow_parse_tcp_v4(edev, rule, tuple);
+		rc = qede_flow_parse_tcp_v4(rule, tuple, NULL);
 	else if (ip_proto == IPPROTO_TCP && proto == htons(ETH_P_IPV6))
 		rc = qede_flow_parse_tcp_v6(rule, tuple, NULL);
 	else if (ip_proto == IPPROTO_UDP && proto == htons(ETH_P_IP))
