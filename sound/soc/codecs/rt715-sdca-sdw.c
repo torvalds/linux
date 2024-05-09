c8bdf9e727acb6e1b37febf422ef1751e5a2c7d1
Bard Liao <yung-chuan.liao@linux.intel.com>
2024-05-09 11:36:58 -0500

    ASoC: rt715-sdca-sdw: Fix wrong complete waiting in rt715_dev_resume()
    enumeration_complete will be completed when a peripheral is attached.
    And initialization_complete will be completed when a peripheral is
    initialized. rt715_dev_resume() should wait for initialization_complete
    instead of enumeration_complete.
    
    the issue exists since commit 20d17057f0a8 ("ASoC: rt715-sdca: Add RT715
    sdca vendor-specific driver"), but the commit can only apply to
    commit f892e66fcabc ("ASoC: rt-sdw*: add __func__ to all error logs").
    
    Fixes: f892e66fcabc ("ASoC: rt-sdw*: add __func__ to all error logs")
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20240509163658.68062-1-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/rt715-sdca-sdw.c
+++ sound/soc/codecs/rt715-sdca-sdw.c
@@ -234,10 +234,10 @@ static int __maybe_unused rt715_dev_resume(struct device *dev)
 	if (!slave->unattach_request)
 		goto regmap_sync;
 
-	time = wait_for_completion_timeout(&slave->enumeration_complete,
+	time = wait_for_completion_timeout(&slave->initialization_complete,
 					   msecs_to_jiffies(RT715_PROBE_TIMEOUT));
 	if (!time) {
-		dev_err(&slave->dev, "%s: Enumeration not complete, timed out\n", __func__);
+		dev_err(&slave->dev, "%s: Initialization not complete, timed out\n", __func__);
 		sdw_show_ping_status(slave->bus, true);
 
 		return -ETIMEDOUT;
