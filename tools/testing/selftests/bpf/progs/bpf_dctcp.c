6ad4e6e94697e960630594907666bc09e78a3b8a
Martin KaFai Lau <martin.lau@kernel.org>
2024-05-09 10:50:23 -0700

    selftests/bpf: Use bpf_tracing_net.h in bpf_dctcp
    This patch uses bpf_tracing_net.h (i.e. vmlinux.h) in bpf_dctcp.
    This will allow to retire the bpf_tcp_helpers.h and consolidate
    tcp-cc tests to vmlinux.h.
    
    It will have a dup on min/max macros with the bpf_cubic. It could
    be further refactored in the future.
    
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    Link: https://lore.kernel.org/r/20240509175026.3423614-8-martin.lau@linux.dev
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/testing/selftests/bpf/progs/bpf_dctcp.c
+++ tools/testing/selftests/bpf/progs/bpf_dctcp.c
@@ -6,15 +6,23 @@
  * the kernel BPF logic.
  */
 
-#include <stddef.h>
-#include <linux/bpf.h>
-#include <linux/types.h>
-#include <linux/stddef.h>
-#include <linux/tcp.h>
-#include <errno.h>
+#include "bpf_tracing_net.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
-#include "bpf_tcp_helpers.h"
+
+#ifndef EBUSY
+#define EBUSY 16
+#endif
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min_not_zero(x, y) ({			\
+	typeof(x) __x = (x);			\
+	typeof(y) __y = (y);			\
+	__x == 0 ? __y : ((__y == 0) ? __x : min(__x, __y)); })
+static bool before(__u32 seq1, __u32 seq2)
+{
+	return (__s32)(seq1-seq2) < 0;
+}
 
 char _license[] SEC("license") = "GPL";
 
