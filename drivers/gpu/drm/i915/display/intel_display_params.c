328add8892c261f28dbf2ef060e8a0707f795c43
Jouni Högander <jouni.hogander@intel.com>
2024-05-10 12:38:21 +0300

    drm/i915/psr: Update PSR module parameter descriptions
    We are re-using PSR module parameters for panel replay. Update module
    parameter descriptions with panel replay information:
    
    enable_psr:
    
    -1 (default) == follow what is in VBT
    0 == disable PSR/PR
    1 == Allow PSR1 and PR full frame update
    2 == allow PSR1/PSR2 and PR Selective Update
    
    enable_psr2_sel_fetch
    
    0 == disable selective fetch for PSR and PR
    1 (default) == allow selective fetch for PSR PR
    
    Signed-off-by: Jouni Högander <jouni.hogander@intel.com>
    Reviewed-by: Animesh Manna <animesh.manna@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240510093823.3146455-11-jouni.hogander@intel.com

--- drivers/gpu/drm/i915/display/intel_display_params.c
+++ drivers/gpu/drm/i915/display/intel_display_params.c
@@ -106,7 +106,8 @@ intel_display_param_named_unsafe(enable_fbc, int, 0400,
 
 intel_display_param_named_unsafe(enable_psr, int, 0400,
 	"Enable PSR "
-	"(0=disabled, 1=enable up to PSR1, 2=enable up to PSR2) "
+	"(0=disabled, 1=enable up to PSR1 and Panel Replay full frame update, "
+	"2=enable up to PSR2 and Panel Replay Selective Update) "
 	"Default: -1 (use per-chip default)");
 
 intel_display_param_named(psr_safest_params, bool, 0400,
@@ -116,7 +117,7 @@ intel_display_param_named(psr_safest_params, bool, 0400,
 	"Default: 0");
 
 intel_display_param_named_unsafe(enable_psr2_sel_fetch, bool, 0400,
-	"Enable PSR2 selective fetch "
+	"Enable PSR2 and Panel Replay selective fetch "
 	"(0=disabled, 1=enabled) "
 	"Default: 1");
 
