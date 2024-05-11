ba39486d2c43ba7c103c438540aa56c8bde3b6c7
Jose E. Marchesi <jose.marchesi@oracle.com>
2024-05-11 23:22:43 +0200

    bpf: make list_for_each_entry portable
    [Changes from V1:
    - The __compat_break has been abandoned in favor of
      a more readable can_loop macro that can be used anywhere, including
      loop conditions.]
    
    The macro list_for_each_entry is defined in bpf_arena_list.h as
    follows:
    
      #define list_for_each_entry(pos, head, member)				\
    	for (void * ___tmp = (pos = list_entry_safe((head)->first,		\
    						    typeof(*(pos)), member),	\
    			      (void *)0);					\
    	     pos && ({ ___tmp = (void *)pos->member.next; 1; });		\
    	     cond_break,							\
    	     pos = list_entry_safe((void __arena *)___tmp, typeof(*(pos)), member))
    
    The macro cond_break, in turn, expands to a statement expression that
    contains a `break' statement.  Compound statement expressions, and the
    subsequent ability of placing statements in the header of a `for'
    loop, are GNU extensions.
    
    Unfortunately, clang implements this GNU extension differently than
    GCC:
    
    - In GCC the `break' statement is bound to the containing "breakable"
      context in which the defining `for' appears.  If there is no such
      context, GCC emits a warning: break statement without enclosing `for'
      o `switch' statement.
    
    - In clang the `break' statement is bound to the defining `for'.  If
      the defining `for' is itself inside some breakable construct, then
      clang emits a -Wgcc-compat warning.
    
    This patch adds a new macro can_loop to bpf_experimental, that
    implements the same logic than cond_break but evaluates to a boolean
    expression.  The patch also changes all the current instances of usage
    of cond_break withing the header of loop accordingly.
    
    Tested in bpf-next master.
    No regressions.
    
    Signed-off-by: Jose E. Marchesi <jose.marchesi@oracle.com>
    Cc: david.faust@oracle.com
    Cc: cupertino.miranda@oracle.com
    Cc: Alexei Starovoitov <alexei.starovoitov@gmail.com>
    Link: https://lore.kernel.org/r/20240511212243.23477-1-jose.marchesi@oracle.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/testing/selftests/bpf/bpf_experimental.h
+++ tools/testing/selftests/bpf/bpf_experimental.h
@@ -326,19 +326,48 @@ l_true:												\
        })
 #endif
 
+/*
+ * Note that cond_break can only be portably used in the body of a breakable
+ * construct, whereas can_loop can be used anywhere.
+ */
 #ifdef __BPF_FEATURE_MAY_GOTO
+#define can_loop					\
+	({ __label__ l_break, l_continue;		\
+	bool ret = true;				\
+	asm volatile goto("may_goto %l[l_break]"	\
+		      :::: l_break);			\
+	goto l_continue;				\
+	l_break: ret = false;				\
+	l_continue:;					\
+	ret;						\
+	})
+
 #define cond_break					\
 	({ __label__ l_break, l_continue;		\
-	 asm volatile goto("may_goto %l[l_break]"	\
+	asm volatile goto("may_goto %l[l_break]"	\
 		      :::: l_break);			\
 	goto l_continue;				\
 	l_break: break;					\
 	l_continue:;					\
 	})
 #else
+#define can_loop					\
+	({ __label__ l_break, l_continue;		\
+	bool ret = true;				\
+	asm volatile goto("1:.byte 0xe5;		\
+		      .byte 0;				\
+		      .long ((%l[l_break] - 1b - 8) / 8) & 0xffff;	\
+		      .short 0"				\
+		      :::: l_break);			\
+	goto l_continue;				\
+	l_break: ret = false;				\
+	l_continue:;					\
+	ret;						\
+	})
+
 #define cond_break					\
 	({ __label__ l_break, l_continue;		\
-	 asm volatile goto("1:.byte 0xe5;			\
+	asm volatile goto("1:.byte 0xe5;		\
 		      .byte 0;				\
 		      .long ((%l[l_break] - 1b - 8) / 8) & 0xffff;	\
 		      .short 0"				\
