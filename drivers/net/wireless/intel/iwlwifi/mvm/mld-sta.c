ca88709d10b1c21aca2bf58fa528723a81322b03
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2024-05-12 07:30:56 +0300

    wifi: iwlwifi: mvm: Use the SMPS cfg of the correct link
    The exiting code is checking the SMPS mode of deflink.
    Use the SMPS mode of the link_sta function parameter.
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Link: https://msgid.link/20240512072733.f3e2834e78f5.Ie24b431a6ad0dbf977afbbd4d2116bfe2426a0d5@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/intel/iwlwifi/mvm/mld-sta.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/mld-sta.c
@@ -470,7 +470,7 @@ static int iwl_mvm_mld_cfg_sta(struct iwl_mvm *mvm, struct ieee80211_sta *sta,
 		break;
 	}
 
-	switch (sta->deflink.smps_mode) {
+	switch (link_sta->smps_mode) {
 	case IEEE80211_SMPS_AUTOMATIC:
 	case IEEE80211_SMPS_NUM_MODES:
 		WARN_ON(1);
