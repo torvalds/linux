e213eb95a49724a8168e6ef48a62fa06a785fa41
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2024-05-12 07:30:58 +0300

    wifi: iwlwifi: bump FW API to 91 for BZ/SC devices
    Start supporting API version 91 for new devices.
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Link: https://msgid.link/20240512072733.27c00099e8b7.I99b4f24bba073414dd04a6e04a359c7fbba52990@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/intel/iwlwifi/cfg/sc.c
+++ drivers/net/wireless/intel/iwlwifi/cfg/sc.c
@@ -10,7 +10,7 @@
 #include "fw/api/txq.h"
 
 /* Highest firmware API version supported */
-#define IWL_SC_UCODE_API_MAX	90
+#define IWL_SC_UCODE_API_MAX	91
 
 /* Lowest firmware API version supported */
 #define IWL_SC_UCODE_API_MIN	82
