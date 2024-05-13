304aa805ee8e82adc30159ff43038cd96eb69eb9
Michal Wajdeczko <michal.wajdeczko@intel.com>
2024-05-13 10:42:15 +0200

    drm/xe: Fix xe_gt_throttle_sysfs.h
    We don't need to include drm/drm_managed.h here.
    We don't need to comment final #endif.
    Also remove empty line at the end.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240513084218.2084-2-michal.wajdeczko@intel.com

--- drivers/gpu/drm/xe/xe_gt_throttle_sysfs.h
+++ drivers/gpu/drm/xe/xe_gt_throttle_sysfs.h
@@ -6,11 +6,8 @@
 #ifndef _XE_GT_THROTTLE_SYSFS_H_
 #define _XE_GT_THROTTLE_SYSFS_H_
 
-#include <drm/drm_managed.h>
-
 struct xe_gt;
 
 int xe_gt_throttle_sysfs_init(struct xe_gt *gt);
 
-#endif /* _XE_GT_THROTTLE_SYSFS_H_ */
-
+#endif
