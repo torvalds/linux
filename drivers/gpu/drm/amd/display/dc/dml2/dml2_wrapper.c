7da55c27e76749b98401fe307d3e243fe6ceb53d
Aurabindo Pillai <aurabindo.pillai@amd.com>
2024-05-13 20:08:58 -0400

    drm/amd/display: Remove incorrect FP context start
    All the DC_FP_START/END should be used before call anything from DML2,
    for this reason, the use of those guards inside DML it is not correct.
    This commit removes two unnecessary DC_FP_START/END from a dml2
    function.
    
    Reviewed-by: Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>
    Signed-off-by: Aurabindo Pillai <aurabindo.pillai@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dml2/dml2_wrapper.c
+++ drivers/gpu/drm/amd/display/dc/dml2/dml2_wrapper.c
@@ -759,7 +759,6 @@ static void dml2_init(const struct dc *in_dc, const struct dml2_configuration_op
 
 bool dml2_create(const struct dc *in_dc, const struct dml2_configuration_options *config, struct dml2_context **dml2)
 {
-	DC_FP_START();
 	// TODO : Temporarily add DCN_VERSION_3_2 for N-1 validation. Remove DCN_VERSION_3_2 after N-1 validation phase is complete.
 	if ((in_dc->debug.using_dml21) && (in_dc->ctx->dce_version == DCN_VERSION_4_01 || in_dc->ctx->dce_version == DCN_VERSION_3_2)) {
 		return dml21_create(in_dc, dml2, config);
@@ -773,7 +772,6 @@ bool dml2_create(const struct dc *in_dc, const struct dml2_configuration_options
 
 	dml2_init(in_dc, config, dml2);
 
-	DC_FP_END();
 	return true;
 }
 
