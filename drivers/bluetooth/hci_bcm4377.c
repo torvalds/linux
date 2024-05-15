56c695a823e4ee1e5294a8340d5afe5de73828ec
Sven Peter <sven@svenpeter.dev>
2024-05-15 18:15:02 +0000

    Bluetooth: hci_bcm4377: Use correct unit for timeouts
    BCM4377_TIMEOUT is always used to wait for completitions and their API
    expects a timeout in jiffies instead of msecs.
    
    Fixes: 8a06127602de ("Bluetooth: hci_bcm4377: Add new driver for BCM4377 PCIe boards")
    Signed-off-by: Sven Peter <sven@svenpeter.dev>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- drivers/bluetooth/hci_bcm4377.c
+++ drivers/bluetooth/hci_bcm4377.c
@@ -32,7 +32,7 @@ enum bcm4377_chip {
 #define BCM4378_DEVICE_ID 0x5f69
 #define BCM4387_DEVICE_ID 0x5f71
 
-#define BCM4377_TIMEOUT 1000
+#define BCM4377_TIMEOUT msecs_to_jiffies(1000)
 
 /*
  * These devices only support DMA transactions inside a 32bit window
