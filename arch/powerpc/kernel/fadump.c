61700f816e6f58f6b1aaa881a69a784d146e30f0
Michael Ellerman <mpe@ellerman.id.au>
2024-05-16 22:14:44 +1000

    powerpc/fadump: Fix section mismatch warning
    With some compilers/configs fadump_setup_param_area() isn't inlined into
    its caller (which is __init), leading to a section mismatch warning:
    
      WARNING: modpost: vmlinux: section mismatch in reference:
      fadump_setup_param_area+0x200 (section: .text.fadump_setup_param_area)
      -> memblock_phys_alloc_range (section: .init.text)
    
    Fix it by adding an __init annotation.
    
    Fixes: 683eab94da75 ("powerpc/fadump: setup additional parameters for dump capture kernel")
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Closes: https://lore.kernel.org/all/20240515163708.3380c4d1@canb.auug.org.au/
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/all/202405140922.oucLOx4Y-lkp@intel.com/
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://msgid.link/20240516132631.347956-1-mpe@ellerman.id.au

--- arch/powerpc/kernel/fadump.c
+++ arch/powerpc/kernel/fadump.c
@@ -1740,7 +1740,7 @@ err_out:
  * Reserve memory to store additional parameters to be passed
  * for fadump/capture kernel.
  */
-static void fadump_setup_param_area(void)
+static void __init fadump_setup_param_area(void)
 {
 	phys_addr_t range_start, range_end;
 
