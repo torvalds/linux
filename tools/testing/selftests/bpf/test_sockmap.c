9de00d5c3d8b949878c62dc95ab4d8f0ae7d84a7
Geliang Tang <tanggeliang@kylinos.cn>
2024-05-17 14:21:46 +0800

    selftests/bpf: Fix prog numbers in test_sockmap
    [ Upstream commit 6c8d7598dfed759bf1d9d0322b4c2b42eb7252d8 ]
    
    bpf_prog5 and bpf_prog7 are removed from progs/test_sockmap_kern.h in
    commit d79a32129b21 ("bpf: Selftests, remove prints from sockmap tests"),
    now there are only 9 progs in it, not 11:
    
    	SEC("sk_skb1")
    	int bpf_prog1(struct __sk_buff *skb)
    	SEC("sk_skb2")
    	int bpf_prog2(struct __sk_buff *skb)
    	SEC("sk_skb3")
    	int bpf_prog3(struct __sk_buff *skb)
    	SEC("sockops")
    	int bpf_sockmap(struct bpf_sock_ops *skops)
    	SEC("sk_msg1")
    	int bpf_prog4(struct sk_msg_md *msg)
    	SEC("sk_msg2")
    	int bpf_prog6(struct sk_msg_md *msg)
    	SEC("sk_msg3")
    	int bpf_prog8(struct sk_msg_md *msg)
    	SEC("sk_msg4")
    	int bpf_prog9(struct sk_msg_md *msg)
    	SEC("sk_msg5")
    	int bpf_prog10(struct sk_msg_md *msg)
    
    This patch updates the array sizes of prog_fd[], prog_attach_type[] and
    prog_type[] from 11 to 9 accordingly.
    
    Fixes: d79a32129b21 ("bpf: Selftests, remove prints from sockmap tests")
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/9c10d9f974f07fcb354a43a8eca67acb2fafc587.1715926605.git.tanggeliang@kylinos.cn
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/testing/selftests/bpf/test_sockmap.c
+++ tools/testing/selftests/bpf/test_sockmap.c
@@ -63,7 +63,7 @@ int passed;
 int failed;
 int map_fd[9];
 struct bpf_map *maps[9];
-int prog_fd[11];
+int prog_fd[9];
 
 int txmsg_pass;
 int txmsg_redir;
@@ -1775,8 +1775,6 @@ int prog_attach_type[] = {
 	BPF_SK_MSG_VERDICT,
 	BPF_SK_MSG_VERDICT,
 	BPF_SK_MSG_VERDICT,
-	BPF_SK_MSG_VERDICT,
-	BPF_SK_MSG_VERDICT,
 };
 
 int prog_type[] = {
@@ -1784,8 +1782,6 @@ int prog_type[] = {
 	BPF_PROG_TYPE_SK_SKB,
 	BPF_PROG_TYPE_SK_SKB,
 	BPF_PROG_TYPE_SOCK_OPS,
-	BPF_PROG_TYPE_SK_MSG,
-	BPF_PROG_TYPE_SK_MSG,
 	BPF_PROG_TYPE_SK_MSG,
 	BPF_PROG_TYPE_SK_MSG,
 	BPF_PROG_TYPE_SK_MSG,
