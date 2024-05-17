2a38e4ca302280fdcce370ba2bee79bac16c4587
Dave Hansen <dave.hansen@linux.intel.com>
2024-05-17 13:05:34 -0700

    x86/cpu: Provide default cache line size if not enumerated
    tl;dr: CPUs with CPUID.80000008H but without CPUID.01H:EDX[CLFSH]
    will end up reporting cache_line_size()==0 and bad things happen.
    Fill in a default on those to avoid the problem.
    
    Long Story:
    
    The kernel dies a horrible death if c->x86_cache_alignment (aka.
    cache_line_size() is 0.  Normally, this value is populated from
    c->x86_clflush_size.
    
    Right now the code is set up to get c->x86_clflush_size from two
    places.  First, modern CPUs get it from CPUID.  Old CPUs that don't
    have leaf 0x80000008 (or CPUID at all) just get some sane defaults
    from the kernel in get_cpu_address_sizes().
    
    The vast majority of CPUs that have leaf 0x80000008 also get
    ->x86_clflush_size from CPUID.  But there are oddballs.
    
    Intel Quark CPUs[1] and others[2] have leaf 0x80000008 but don't set
    CPUID.01H:EDX[CLFSH], so they skip over filling in ->x86_clflush_size:
    
    	cpuid(0x00000001, &tfms, &misc, &junk, &cap0);
    	if (cap0 & (1<<19))
    		c->x86_clflush_size = ((misc >> 8) & 0xff) * 8;
    
    So they: land in get_cpu_address_sizes() and see that CPUID has level
    0x80000008 and jump into the side of the if() that does not fill in
    c->x86_clflush_size.  That assigns a 0 to c->x86_cache_alignment, and
    hilarity ensues in code like:
    
            buffer = kzalloc(ALIGN(sizeof(*buffer), cache_line_size()),
                             GFP_KERNEL);
    
    To fix this, always provide a sane value for ->x86_clflush_size.
    
    Big thanks to Andy Shevchenko for finding and reporting this and also
    providing a first pass at a fix. But his fix was only partial and only
    worked on the Quark CPUs.  It would not, for instance, have worked on
    the QEMU config.
    
    1. https://raw.githubusercontent.com/InstLatx64/InstLatx64/master/GenuineIntel/GenuineIntel0000590_Clanton_03_CPUID.txt
    2. You can also get this behavior if you use "-cpu 486,+clzero"
       in QEMU.
    
    [ dhansen: remove 'vp_bits_from_cpuid' reference in changelog
    	   because bpetkov brutally murdered it recently. ]
    
    Fixes: fbf6449f84bf ("x86/sev-es: Set x86_virt_bits to the correct value straight away, instead of a two-phase approach")
    Reported-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Tested-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Tested-by: Jörn Heusipp <osmanx@heusipp.de>
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/all/20240516173928.3960193-1-andriy.shevchenko@linux.intel.com/
    Link: https://lore.kernel.org/lkml/5e31cad3-ad4d-493e-ab07-724cfbfaba44@heusipp.de/
    Link: https://lore.kernel.org/all/20240517200534.8EC5F33E%40davehans-spike.ostc.intel.com

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -1075,6 +1075,10 @@ void get_cpu_address_sizes(struct cpuinfo_x86 *c)
 
 		c->x86_virt_bits = (eax >> 8) & 0xff;
 		c->x86_phys_bits = eax & 0xff;
+
+		/* Provide a sane default if not enumerated: */
+		if (!c->x86_clflush_size)
+			c->x86_clflush_size = 32;
 	}
 
 	c->x86_cache_bits = c->x86_phys_bits;
