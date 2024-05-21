4a8f635a60540888dab3804992e86410360339c8
Andrii Nakryiko <andrii@kernel.org>
2024-05-21 09:33:58 -0700

    bpf: remove unnecessary rcu_read_{lock,unlock}() in multi-uprobe attach logic
    get_pid_task() internally already calls rcu_read_lock() and
    rcu_read_unlock(), so there is no point to do this one extra time.
    
    This is a drive-by improvement and has no correctness implications.
    
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/r/20240521163401.3005045-3-andrii@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -3423,9 +3423,7 @@ int bpf_uprobe_multi_link_attach(const union bpf_attr *attr, struct bpf_prog *pr
 	}
 
 	if (pid) {
-		rcu_read_lock();
 		task = get_pid_task(find_vpid(pid), PIDTYPE_TGID);
-		rcu_read_unlock();
 		if (!task) {
 			err = -ESRCH;
 			goto error_path_put;
