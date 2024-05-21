fcf6a49d79923a234844b8efe830a61f3f0584e4
Wayne Lin <wayne.lin@amd.com>
2024-05-21 11:54:26 +0800

    drm/amd/display: Don't refer to dc_sink in is_dsc_need_re_compute
    [Why]
    When unplug one of monitors connected after mst hub, encounter null pointer dereference.
    
    It's due to dc_sink get released immediately in early_unregister() or detect_ctx(). When
    commit new state which directly referring to info stored in dc_sink will cause null pointer
    dereference.
    
    [how]
    Remove redundant checking condition. Relevant condition should already be covered by checking
    if dsc_aux is null or not. Also reset dsc_aux to NULL when the connector is disconnected.
    
    Reviewed-by: Jerry Zuo <jerry.zuo@amd.com>
    Acked-by: Zaeem Mohamed <zaeem.mohamed@amd.com>
    Signed-off-by: Wayne Lin <wayne.lin@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
@@ -182,6 +182,8 @@ amdgpu_dm_mst_connector_early_unregister(struct drm_connector *connector)
 		dc_sink_release(dc_sink);
 		aconnector->dc_sink = NULL;
 		aconnector->edid = NULL;
+		aconnector->dsc_aux = NULL;
+		port->passthrough_aux = NULL;
 	}
 
 	aconnector->mst_status = MST_STATUS_DEFAULT;
@@ -498,6 +500,8 @@ dm_dp_mst_detect(struct drm_connector *connector,
 		dc_sink_release(aconnector->dc_sink);
 		aconnector->dc_sink = NULL;
 		aconnector->edid = NULL;
+		aconnector->dsc_aux = NULL;
+		port->passthrough_aux = NULL;
 
 		amdgpu_dm_set_mst_status(&aconnector->mst_status,
 			MST_REMOTE_EDID | MST_ALLOCATE_NEW_PAYLOAD | MST_CLEAR_ALLOCATED_PAYLOAD,
@@ -1236,14 +1240,6 @@ static bool is_dsc_need_re_compute(
 
 		aconnector = (struct amdgpu_dm_connector *) stream->dm_stream_context;
 		if (!aconnector || !aconnector->dsc_aux)
-			continue;
-
-		/*
-		 *	check if cached virtual MST DSC caps are available and DSC is supported
-		 *	as per specifications in their Virtual DPCD registers.
-		*/
-		if (!(aconnector->dc_sink->dsc_caps.dsc_dec_caps.is_dsc_supported ||
-			aconnector->dc_link->dpcd_caps.dsc_caps.dsc_basic_caps.fields.dsc_support.DSC_PASSTHROUGH_SUPPORT))
 			continue;
 
 		stream_on_link[new_stream_on_link_num] = aconnector;
