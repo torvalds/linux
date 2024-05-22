7978c4d41496becda1c1cbd5b783c60e9b64d618
Alex Deucher <alexander.deucher@amd.com>
2024-05-22 15:26:50 -0400

    drm/amdkfd: simplify APU VRAM handling
    With commit 89773b85599a
    ("drm/amdkfd: Let VRAM allocations go to GTT domain on small APUs")
    big and small APU "VRAM" handling in KFD was unified.  Since AMD_IS_APU
    is set for both big and small APUs, we can simplify the checks in
    the code.
    
    v2: clean up a few more places (Lang)
    
    Acked-by: Felix Kuehling <felix.kuehling@amd.com>
    Reviewed-by: Lang Yu <Lang.Yu@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_svm.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_svm.c
@@ -2634,8 +2634,7 @@ svm_range_best_restore_location(struct svm_range *prange,
 		return -1;
 	}
 
-	if (node->adev->gmc.is_app_apu ||
-	    node->adev->flags & AMD_IS_APU)
+	if (node->adev->flags & AMD_IS_APU)
 		return 0;
 
 	if (prange->preferred_loc == gpuid ||
@@ -3353,8 +3352,7 @@ svm_range_best_prefetch_location(struct svm_range *prange)
 		goto out;
 	}
 
-	if (bo_node->adev->gmc.is_app_apu ||
-	    bo_node->adev->flags & AMD_IS_APU) {
+	if (bo_node->adev->flags & AMD_IS_APU) {
 		best_loc = 0;
 		goto out;
 	}
