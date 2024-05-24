5714e25f1d1875b300fb337dadfaa75324c1161a
Baochen Qiang <quic_bqiang@quicinc.com>
2024-05-24 10:15:58 +0800

    wifi: ath11k: fix wrong definition of CE ring's base address
    Base address of CE ring is defined as u32, currently this works
    because coherent DMA mask configured as 32 bit:
    
    	#define ATH11K_PCI_COHERENT_DMA_MASK	32
    
    However this mask could be changed once firmware bugs are fixed
    to fully support 36 bit DMA addressing. So to protect against any
    future changes to the DMA mask, change the type of the fields that
    are dependent upon it.
    
    This is found during code review. Compile tested only.
    
    Fixes: d5c65159f289 ("ath11k: driver for Qualcomm IEEE 802.11ax devices")
    Signed-off-by: Baochen Qiang <quic_bqiang@quicinc.com>
    Acked-by: Jeff Johnson <quic_jjohnson@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://msgid.link/20240524021558.34452-1-quic_bqiang@quicinc.com

--- drivers/net/wireless/ath/ath11k/ce.h
+++ drivers/net/wireless/ath/ath11k/ce.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: BSD-3-Clause-Clear */
 /*
  * Copyright (c) 2018-2019 The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022, 2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef ATH11K_CE_H
@@ -146,7 +146,7 @@ struct ath11k_ce_ring {
 	/* Host address space */
 	void *base_addr_owner_space_unaligned;
 	/* CE address space */
-	u32 base_addr_ce_space_unaligned;
+	dma_addr_t base_addr_ce_space_unaligned;
 
 	/* Actual start of descriptors.
 	 * Aligned to descriptor-size boundary.
@@ -156,7 +156,7 @@ struct ath11k_ce_ring {
 	void *base_addr_owner_space;
 
 	/* CE address space */
-	u32 base_addr_ce_space;
+	dma_addr_t base_addr_ce_space;
 
 	/* HAL ring id */
 	u32 hal_ring_id;
