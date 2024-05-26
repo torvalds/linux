970c3a6b7aa3c68ccdf5af2562c3d39533dd62a9
Thomas Weißschuh <linux@weissschuh.net>
2024-05-26 20:17:18 +0200

    mfd: cros_ec: Register keyboard backlight subdevice
    Load cros_kbd_led_backlight when the EC reports EC_FEATURE_PWM_KEYB.
    This makes cros_kbd_led_backlight work on machines without specific
    ACPI or OF support for the keyboard backlight.
    
    Signed-off-by: Thomas Weißschuh <linux@weissschuh.net>
    Link: https://lore.kernel.org/r/20240526-cros_ec-kbd-led-framework-v3-4-ee577415a521@weissschuh.net
    Signed-off-by: Lee Jones <lee@kernel.org>

--- drivers/mfd/cros_ec_dev.c
+++ drivers/mfd/cros_ec_dev.c
@@ -99,6 +99,10 @@ static const struct mfd_cell cros_ec_wdt_cells[] = {
 	{ .name = "cros-ec-wdt", }
 };
 
+static const struct mfd_cell cros_ec_keyboard_leds_cells[] = {
+	{ .name = "cros-keyboard-leds", },
+};
+
 static const struct cros_feature_to_cells cros_subdevices[] = {
 	{
 		.id		= EC_FEATURE_CEC,
@@ -124,6 +128,11 @@ static const struct cros_feature_to_cells cros_subdevices[] = {
 		.id		= EC_FEATURE_HANG_DETECT,
 		.mfd_cells	= cros_ec_wdt_cells,
 		.num_cells	= ARRAY_SIZE(cros_ec_wdt_cells),
+	},
+	{
+		.id		= EC_FEATURE_PWM_KEYB,
+		.mfd_cells	= cros_ec_keyboard_leds_cells,
+		.num_cells	= ARRAY_SIZE(cros_ec_keyboard_leds_cells),
 	},
 };
 
