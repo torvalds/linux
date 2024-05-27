6a6118336270f67174fb8c799c8262bfa88e97e0
Dr. David Alan Gilbert <linux@treblig.org>
2024-05-27 17:01:18 +0100

    PCI: tegra: Remove unused struct 'tegra_pcie_soc'
    'tegra_pcie_soc' has been unused since 56e15a238d92 ("PCI: tegra: Add
    Tegra194 PCIe support").  Remove it.
    
    Link: https://lore.kernel.org/r/20240527160118.37069-1-linux@treblig.org
    Signed-off-by: Dr. David Alan Gilbert <linux@treblig.org>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Niklas Cassel <cassel@kernel.org>

--- drivers/pci/controller/dwc/pcie-tegra194.c
+++ drivers/pci/controller/dwc/pcie-tegra194.c
@@ -308,10 +308,6 @@ static inline u32 appl_readl(struct tegra_pcie_dw *pcie, const u32 reg)
 	return readl_relaxed(pcie->appl_base + reg);
 }
 
-struct tegra_pcie_soc {
-	enum dw_pcie_device_mode mode;
-};
-
 static void tegra_pcie_icc_set(struct tegra_pcie_dw *pcie)
 {
 	struct dw_pcie *pci = &pcie->pci;
