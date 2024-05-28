64ff013ce0986952fad1678d7faf8998584fc500
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2024-05-28 10:17:42 +0100

    wifi: wlcore: correctness fix fwlog reading
    Fix the calculation of clear_offset, which may overflow the end of
    the buffer. However, this is harmless if it does because in that case
    it will be recalculated when we copy the chunk of messages at the
    start of the buffer.
    
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://msgid.link/E1sBsxi-00E8vQ-5r@rmk-PC.armlinux.org.uk

--- drivers/net/wireless/ti/wlcore/event.c
+++ drivers/net/wireless/ti/wlcore/event.c
@@ -83,7 +83,7 @@ int wlcore_event_fw_logger(struct wl1271 *wl)
 	/* Copy initial part up to the end of ring buffer */
 	len = min(actual_len, available_len);
 	wl12xx_copy_fwlog(wl, &buffer[start_loc], len);
-	clear_ptr = addr_ptr + start_loc + actual_len;
+	clear_ptr = addr_ptr + start_loc + len;
 	if (clear_ptr == buff_end_ptr)
 		clear_ptr = buff_start_ptr;
 
