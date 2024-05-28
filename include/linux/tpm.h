f09fc6cee0dcfc38148ee6b6dd04f93e353d22f2
Jarkko Sakkinen <jarkko@kernel.org>
2024-05-28 12:52:21 +0300

    tpm: Rename TPM2_OA_TMPL to TPM2_OA_NULL_KEY and make it local
    Rename and document TPM2_OA_TMPL, as originally requested in the patch
    set review, but left unaddressed without any appropriate reasoning. The
    new name is TPM2_OA_NULL_KEY, has a documentation and is local only to
    tpm2-sessions.c.
    
    Link: https://lore.kernel.org/linux-integrity/ddbeb8111f48a8ddb0b8fca248dff6cc9d7079b2.camel@HansenPartnership.com/
    Link: https://lore.kernel.org/linux-integrity/CZCKTWU6ZCC9.2UTEQPEVICYHL@suppilovahvero/
    Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>

--- include/linux/tpm.h
+++ include/linux/tpm.h
@@ -394,21 +394,6 @@ enum tpm2_object_attributes {
 	TPM2_OA_SIGN			= BIT(18),
 };
 
-/*
- * definitions for the canonical template.  These are mandated
- * by the TCG key template documents
- */
-
-#define AES_KEY_BYTES	AES_KEYSIZE_128
-#define AES_KEY_BITS	(AES_KEY_BYTES*8)
-#define TPM2_OA_TMPL	(TPM2_OA_NO_DA |			\
-			 TPM2_OA_FIXED_TPM |			\
-			 TPM2_OA_FIXED_PARENT |			\
-			 TPM2_OA_SENSITIVE_DATA_ORIGIN |	\
-			 TPM2_OA_USER_WITH_AUTH |		\
-			 TPM2_OA_DECRYPT |			\
-			 TPM2_OA_RESTRICTED)
-
 enum tpm2_session_attributes {
 	TPM2_SA_CONTINUE_SESSION	= BIT(0),
 	TPM2_SA_AUDIT_EXCLUSIVE		= BIT(1),
