b67be58fafb21b6b75f792674ce8f9f5635694d1
Jani Nikula <jani.nikula@intel.com>
2024-05-29 20:48:11 +0300

    drm/xe/display: move compat uncore stubs to the correct file
    Move things that belong to intel_uncore.h to the correct place.
    
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Acked-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/3f3903c7c5e34aefac0f6d06e433710bc782c97e.1717004739.git.jani.nikula@intel.com

--- drivers/gpu/drm/xe/compat-i915-headers/i915_drv.h
+++ drivers/gpu/drm/xe/compat-i915-headers/i915_drv.h
@@ -154,11 +154,6 @@ static inline void intel_runtime_pm_put(struct xe_runtime_pm *pm, intel_wakeref_
 #define assert_rpm_wakelock_held(x) do { } while (0)
 #define assert_rpm_raw_wakeref_held(x) do { } while (0)
 
-#define intel_uncore_forcewake_get(x, y) do { } while (0)
-#define intel_uncore_forcewake_put(x, y) do { } while (0)
-
-#define intel_uncore_arm_unclaimed_mmio_detection(x) do { } while (0)
-
 #define I915_PRIORITY_DISPLAY 0
 struct i915_sched_attr {
 	int priority;
