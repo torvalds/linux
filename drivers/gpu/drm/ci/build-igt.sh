0493be3ba118ecb928afd9fe807ef77aa42755ae
Vignesh Raman <vignesh.raman@collabora.com>
2024-05-29 08:10:46 +0530

    drm/ci: generate testlist from build
    Stop vendoring the testlist into the kernel. Instead, use the
    testlist from the IGT build to ensure we do not miss renamed
    or newly added tests.
    
    Signed-off-by: Vignesh Raman <vignesh.raman@collabora.com>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Reviewed-by: Jessica Zhang <quic_jesszhan@quicinc.com>
    Acked-by: Helen Koike <helen.koike@collabora.com>
    Signed-off-by: Helen Koike <helen.koike@collabora.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240529024049.356327-4-vignesh.raman@collabora.com

--- drivers/gpu/drm/ci/build-igt.sh
+++ drivers/gpu/drm/ci/build-igt.sh
@@ -3,6 +3,30 @@
 
 set -ex
 
+function generate_testlist {
+    set +x
+    while read -r line; do
+        if [ "$line" = "TESTLIST" ] || [ "$line" = "END TESTLIST" ]; then
+            continue
+        fi
+
+        tests=$(echo "$line" | tr ' ' '\n')
+
+        for test in $tests; do
+            output=$(/igt/libexec/igt-gpu-tools/"$test" --list-subtests || true)
+
+            if [ -z "$output" ]; then
+                echo "$test"
+            else
+                echo "$output" | while read -r subtest; do
+                    echo "$test@$subtest"
+                done
+            fi
+        done
+    done < /igt/libexec/igt-gpu-tools/test-list.txt > /igt/libexec/igt-gpu-tools/ci-testlist.txt
+    set -x
+}
+
 git clone https://gitlab.freedesktop.org/drm/igt-gpu-tools.git --single-branch --no-checkout
 cd igt-gpu-tools
 git checkout $IGT_VERSION
@@ -25,6 +49,17 @@ mkdir -p /igt
 meson build $MESON_OPTIONS $EXTRA_MESON_ARGS
 ninja -C build -j${FDO_CI_CONCURRENT:-4} || ninja -C build -j 1
 ninja -C build install
+
+if [[ "$KERNEL_ARCH" = "arm64" ]]; then
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/igt/lib/aarch64-linux-gnu
+elif [[ "$KERNEL_ARCH" = "arm" ]]; then
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/igt/lib
+else
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/igt/lib64
+fi
+
+echo "Generating ci-testlist.txt"
+generate_testlist
 
 mkdir -p artifacts/
 tar -cf artifacts/igt.tar /igt
