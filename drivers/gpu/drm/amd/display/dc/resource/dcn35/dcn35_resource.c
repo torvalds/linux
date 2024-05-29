7c70e60fbf4bff1123f0e8d5cb1ae71df6164d7f
Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
2024-05-29 12:12:09 -0400

    drm/amd/display: Disable DMCUB timeout for DCN35
    [Why]
    DMCUB can intermittently take longer than expected to process commands.
    
    Old ASIC policy was to continue while logging a diagnostic error - which
    works fine for ASIC without IPS, but with IPS this could lead to a race
    condition where we attempt to access DCN state while it's inaccessible,
    leading to a system hang when the NIU port is not disabled or register
    accesses that timeout and the display configuration in an undefined
    state.
    
    [How]
    We need to investigate why these accesses take longer than expected, but
    for now we should disable the timeout on DCN35 to avoid this race
    condition. Since the waits happen only at lower interrupt levels the
    risk of taking too long at higher IRQ and causing a system watchdog
    timeout are minimal.
    
    Reviewed-by: Ovidiu Bunea <ovidiu.bunea@amd.com>
    Acked-by: Zaeem Mohamed <zaeem.mohamed@amd.com>
    Signed-off-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/resource/dcn35/dcn35_resource.c
+++ drivers/gpu/drm/amd/display/dc/resource/dcn35/dcn35_resource.c
@@ -785,6 +785,7 @@ static const struct dc_debug_options debug_defaults_drv = {
 	.ips2_entry_delay_us = 800,
 	.disable_dmub_reallow_idle = false,
 	.static_screen_wait_frames = 2,
+	.disable_timeout = true,
 };
 
 static const struct dc_panel_config panel_config_defaults = {
