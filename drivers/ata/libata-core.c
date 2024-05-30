473880369304cfd4445720cdd8bae4c6f1e16e60
Niklas Cassel <cassel@kernel.org>
2024-05-30 23:32:44 +0200

    ata: libata-core: Add ATA_HORKAGE_NOLPM for AMD Radeon S3 SSD
    Commit 7627a0edef54 ("ata: ahci: Drop low power policy board type")
    dropped the board_ahci_low_power board type, and instead enables LPM if:
    -The AHCI controller reports that it supports LPM (Partial/Slumber), and
    -CONFIG_SATA_MOBILE_LPM_POLICY != 0, and
    -The port is not defined as external in the per port PxCMD register, and
    -The port is not defined as hotplug capable in the per port PxCMD
     register.
    
    Partial and Slumber LPM states can either be initiated by HIPM or DIPM.
    
    For HIPM (host initiated power management) to get enabled, both the AHCI
    controller and the drive have to report that they support HIPM.
    
    For DIPM (device initiated power management) to get enabled, only the
    drive has to report that it supports DIPM. However, the HBA will reject
    device requests to enter LPM states which the HBA does not support.
    
    The problem is that AMD Radeon S3 SSD drives do not handle low power modes
    correctly. The problem was most likely not seen before because no one
    had used this drive with a AHCI controller with LPM enabled.
    
    Add a quirk so that we do not enable LPM for this drive, since we see
    command timeouts if we do (even though the drive claims to support both
    HIPM and DIPM).
    
    Fixes: 7627a0edef54 ("ata: ahci: Drop low power policy board type")
    Cc: stable@vger.kernel.org
    Reported-by: Doru Iorgulescu <doru.iorgulescu1@gmail.com>
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=218832
    Reviewed-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Reviewed-by: Damien Le Moal <dlemoal@kernel.org>
    Signed-off-by: Niklas Cassel <cassel@kernel.org>

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -4156,6 +4156,9 @@ static const struct ata_blacklist_entry ata_device_blacklist [] = {
 						ATA_HORKAGE_ZERO_AFTER_TRIM |
 						ATA_HORKAGE_NOLPM },
 
+	/* AMD Radeon devices with broken LPM support */
+	{ "R3SL240G",			NULL,	ATA_HORKAGE_NOLPM },
+
 	/* These specific Samsung models/firmware-revs do not handle LPM well */
 	{ "SAMSUNG MZMPC128HBFU-000MV", "CXM14M1Q", ATA_HORKAGE_NOLPM },
 	{ "SAMSUNG SSD PM830 mSATA *",  "CXM13D1Q", ATA_HORKAGE_NOLPM },
