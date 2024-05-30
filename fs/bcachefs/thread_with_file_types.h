7ed122aea2e26686467c6ec63dc4a1c060e0ff44
Kent Overstreet <kent.overstreet@linux.dev>
2024-05-30 15:54:08 -0400

    bcachefs: twf: delete dead struct fields
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/thread_with_file_types.h
+++ fs/bcachefs/thread_with_file_types.h
@@ -14,10 +14,6 @@ struct stdio_buf {
 struct stdio_redirect {
 	struct stdio_buf	input;
 	struct stdio_buf	output;
-
-	spinlock_t		input_lock;
-	wait_queue_head_t	input_wait;
-	darray_char		input_buf;
 	bool			done;
 };
 
