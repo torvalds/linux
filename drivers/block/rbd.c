a00d4bfce7c6d7fa4712b8133ec195c9bd142ae6
Christoph Hellwig <hch@lst.de>
2024-05-31 09:47:58 +0200

    rbd: increase io_opt again
    Commit 16d80c54ad42 ("rbd: set io_min, io_opt and discard_granularity to
    alloc_size") lowered the io_opt size for rbd from objset_bytes which is
    4MB for typical setup to alloc_size which is typically 64KB.
    
    The commit mostly talks about discard behavior and does mention io_min
    in passing.  Reducing io_opt means reducing the readahead size, which
    seems counter-intuitive given that rbd currently abuses the user
    max_sectors setting to actually increase the I/O size.  Switch back
    to the old setting to allow larger reads (the readahead size despite it's
    name actually limits the size of any buffered read) and to prepare
    for using io_opt in the max_sectors calculation and getting drivers out
    of the business of overriding the max_user_sectors value.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Ilya Dryomov <idryomov@gmail.com>
    Reviewed-by: Martin K. Petersen <martin.petersen@oracle.com>
    Link: https://lore.kernel.org/r/20240531074837.1648501-4-hch@lst.de
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/rbd.c
+++ drivers/block/rbd.c
@@ -4955,8 +4955,8 @@ static int rbd_init_disk(struct rbd_device *rbd_dev)
 	struct queue_limits lim = {
 		.max_hw_sectors		= objset_bytes >> SECTOR_SHIFT,
 		.max_user_sectors	= objset_bytes >> SECTOR_SHIFT,
+		.io_opt			= objset_bytes,
 		.io_min			= rbd_dev->opts->alloc_size,
-		.io_opt			= rbd_dev->opts->alloc_size,
 		.max_segments		= USHRT_MAX,
 		.max_segment_size	= UINT_MAX,
 	};
