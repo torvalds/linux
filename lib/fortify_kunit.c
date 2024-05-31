e471831be2bea5f253396796c7691209c0065c65
Jeff Johnson <quic_jjohnson@quicinc.com>
2024-05-31 15:45:22 -0700

    kunit/fortify: add missing MODULE_DESCRIPTION() macros
    make allmodconfig && make W=1 C=1 reports:
    WARNING: modpost: missing MODULE_DESCRIPTION() in lib/memcpy_kunit.o
    WARNING: modpost: missing MODULE_DESCRIPTION() in lib/fortify_kunit.o
    
    Add the missing invocations of the MODULE_DESCRIPTION() macro.
    
    Link: https://lkml.kernel.org/r/20240531-md-lib-fortify_source-v1-1-2c37f7fbaafc@quicinc.com
    Signed-off-by: Jeff Johnson <quic_jjohnson@quicinc.com>
    Cc: Kees Cook <keescook@chromium.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- lib/fortify_kunit.c
+++ lib/fortify_kunit.c
@@ -1096,4 +1096,5 @@ static struct kunit_suite fortify_test_suite = {
 
 kunit_test_suite(fortify_test_suite);
 
+MODULE_DESCRIPTION("Runtime test cases for CONFIG_FORTIFY_SOURCE");
 MODULE_LICENSE("GPL");
