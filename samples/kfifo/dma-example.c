9059044b6717b0c100e3ad63c5bad3ec13df0fc4
Jeff Johnson <quic_jjohnson@quicinc.com>
2024-06-01 17:50:27 -0700

    kfifo: add missing MODULE_DESCRIPTION() macros
    make allmodconfig && make W=1 C=1 reports:
    WARNING: modpost: missing MODULE_DESCRIPTION() in samples/kfifo/bytestream-example.o
    WARNING: modpost: missing MODULE_DESCRIPTION() in samples/kfifo/dma-example.o
    WARNING: modpost: missing MODULE_DESCRIPTION() in samples/kfifo/inttype-example.o
    WARNING: modpost: missing MODULE_DESCRIPTION() in samples/kfifo/record-example.o
    
    Add the missing invocations of the MODULE_DESCRIPTION() macro.
    
    Link: https://lkml.kernel.org/r/20240601-md-samples-kfifo-v1-1-de34345c5fd8@quicinc.com
    Signed-off-by: Jeff Johnson <quic_jjohnson@quicinc.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Stefani Seibold <stefani@seibold.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- samples/kfifo/dma-example.c
+++ samples/kfifo/dma-example.c
@@ -138,5 +138,6 @@ static void __exit example_exit(void)
 
 module_init(example_init);
 module_exit(example_exit);
+MODULE_DESCRIPTION("Sample fifo dma implementation");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Stefani Seibold <stefani@seibold.net>");
