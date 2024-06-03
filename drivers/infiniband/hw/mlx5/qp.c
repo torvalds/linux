d98995b4bf981519dde4af0a081c393d62474039
Jianbo Liu <jianbol@nvidia.com>
2024-06-03 13:26:37 +0300

    net/mlx5: Reimplement write combining test
    The test of write combining was added before in mlx5_ib driver. It
    opens UD QP and posts NOP WQEs, and uses BlueFlame doorbell. When
    BlueFlame is used, WQEs get written directly to a PCI BAR of the
    device (in addition to memory) so that the device handles them without
    having to access memory.
    
    In this test, the WQEs written in memory are different from the ones
    written to the BlueFlame which request CQE update. By checking the
    completion reports posted on CQ, we can know if BlueFlame succeeds or
    not. The write combining must be supported if BlueFlame succeeds as
    its register is written using write combining.
    
    This patch reimplements the test in the same way, but using a pair of
    SQ and CQ only. It is moved to mlx5_core as a general feature used by
    both mlx5_core and mlx5_ib.
    
    Besides, save write combine test result of the PCI function, so that
    its thousands of child functions such as SF can query without paying
    the time and resource penalty by itself. The test function is called
    only after failing to get the cached result. With this enhancement,
    all thousands of SFs of the PF attached to same driver no longer need
    to perform WC check explicitly, which is already done in the system.
    This saves several commands per SF, thereby speeds up SF creation and
    also saves completion EQ creation.
    
    Signed-off-by: Jianbo Liu <jianbol@nvidia.com>
    Reviewed-by: Tariq Toukan <tariqt@nvidia.com>
    Link: https://lore.kernel.org/r/4ff5a8cc4c5b5b0d98397baa45a5019bcdbf096e.1717409369.git.leon@kernel.org
    Signed-off-by: Leon Romanovsky <leon@kernel.org>

--- drivers/infiniband/hw/mlx5/qp.c
+++ drivers/infiniband/hw/mlx5/qp.c
@@ -1107,8 +1107,6 @@ static int _create_kernel_qp(struct mlx5_ib_dev *dev,
 
 	if (init_attr->qp_type == MLX5_IB_QPT_REG_UMR)
 		qp->bf.bfreg = &dev->fp_bfreg;
-	else if (qp->flags & MLX5_IB_QP_CREATE_WC_TEST)
-		qp->bf.bfreg = &dev->wc_bfreg;
 	else
 		qp->bf.bfreg = &dev->bfreg;
 
@@ -2959,14 +2957,6 @@ static void process_create_flag(struct mlx5_ib_dev *dev, int *flags, int flag,
 		return;
 	}
 
-	if (flag == MLX5_IB_QP_CREATE_WC_TEST) {
-		/*
-		 * Special case, if condition didn't meet, it won't be error,
-		 * just different in-kernel flow.
-		 */
-		*flags &= ~MLX5_IB_QP_CREATE_WC_TEST;
-		return;
-	}
 	mlx5_ib_dbg(dev, "Verbs create QP flag 0x%X is not supported\n", flag);
 }
 
@@ -3027,8 +3017,6 @@ static int process_create_flags(struct mlx5_ib_dev *dev, struct mlx5_ib_qp *qp,
 			    IB_QP_CREATE_PCI_WRITE_END_PADDING,
 			    MLX5_CAP_GEN(mdev, end_pad), qp);
 
-	process_create_flag(dev, &create_flags, MLX5_IB_QP_CREATE_WC_TEST,
-			    qp_type != MLX5_IB_QPT_REG_UMR, qp);
 	process_create_flag(dev, &create_flags, MLX5_IB_QP_CREATE_SQPN_QP1,
 			    true, qp);
 
@@ -4607,10 +4595,6 @@ static bool mlx5_ib_modify_qp_allowed(struct mlx5_ib_dev *dev,
 		return true;
 
 	if (qp->type == IB_QPT_RAW_PACKET || qp->type == MLX5_IB_QPT_REG_UMR)
-		return true;
-
-	/* Internal QP used for wc testing, with NOPs in wq */
-	if (qp->flags & MLX5_IB_QP_CREATE_WC_TEST)
 		return true;
 
 	return false;
