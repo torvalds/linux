b907194a5d5bcd3b01d28d095f6a4d9fcd9c5354
Anshumali Gaur <agaur@marvell.com>
2024-06-03 16:52:48 +0530

    octeontx2-af: Add debugfs support to dump NIX TM topology
    This patch adds support to dump NIX transmit queue topology.
    There are multiple levels of scheduling/shaping supported by
    NIX and a packet traverses through multiple levels before sending
    the packet out. At each level, there are set of scheduling/shaping
    rules applied to a packet flow.
    
    Each packet traverses through multiple levels
    SQ->SMQ->TL4->TL3->TL2->TL1 and these levels are mapped in a parent-child
    relationship.
    
    This patch dumps the debug information related to all TM Levels in
    the following way.
    
    Example:
    $ echo <nixlf> > /sys/kernel/debug/octeontx2/nix/tm_tree
    $ cat /sys/kernel/debug/octeontx2/nix/tm_tree
    
    A more desriptive set of registers at each level can be dumped
    in the following way.
    
    Example:
    $ echo <nixlf> > /sys/kernel/debug/octeontx2/nix/tm_topo
    $ cat /sys/kernel/debug/octeontx2/nix/tm_topo
    
    Signed-off-by: Anshumali Gaur <agaur@marvell.com>
    Reviewed-by: Wojciech Drewek <wojciech.drewek@intel.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -76,6 +76,7 @@ struct rvu_debugfs {
 	struct dump_ctx nix_cq_ctx;
 	struct dump_ctx nix_rq_ctx;
 	struct dump_ctx nix_sq_ctx;
+	struct dump_ctx nix_tm_ctx;
 	struct cpt_ctx cpt_ctx[MAX_CPT_BLKS];
 	int npa_qsize_id;
 	int nix_qsize_id;
