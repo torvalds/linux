0f9579d9e0331b6255132ac06bdf2c0a01cceb90
Konstantin Komarov <almaz.alexandrovich@paragon-software.com>
2024-06-03 09:58:13 +0300

    fs/ntfs3: Add missing .dirty_folio in address_space_operations
    After switching from pages to folio [1], it became evident that
    the initialization of .dirty_folio for page cache operations was missed for
    compressed files.
    
    [1] https://lore.kernel.org/ntfs3/20240422193203.3534108-1-willy@infradead.org
    
    Fixes: 82cae269cfa95 ("fs/ntfs3: Add initialization of super block")
    Signed-off-by: Konstantin Komarov <almaz.alexandrovich@paragon-software.com>

--- fs/ntfs3/inode.c
+++ fs/ntfs3/inode.c
@@ -2131,5 +2131,6 @@ const struct address_space_operations ntfs_aops = {
 const struct address_space_operations ntfs_aops_cmpr = {
 	.read_folio	= ntfs_read_folio,
 	.readahead	= ntfs_readahead,
+	.dirty_folio	= block_dirty_folio,
 };
 // clang-format on
