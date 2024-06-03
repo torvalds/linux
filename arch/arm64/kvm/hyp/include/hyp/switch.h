6d8fb3cbf7e06431a607c30c1bc4cd53a62c220a
Fuad Tabba <tabba@google.com>
2024-06-03 13:28:45 +0100

    KVM: arm64: Abstract set/clear of CPTR_EL2 bits behind helper
    The same traps controlled by CPTR_EL2 or CPACR_EL1 need to be
    toggled in different parts of the code, but the exact bits and
    their polarity differ between these two formats and the mode
    (vhe/nvhe/hvhe).
    
    To reduce the amount of duplicated code and the chance of getting
    the wrong bit/polarity or missing a field, abstract the set/clear
    of CPTR_EL2 bits behind a helper.
    
    Since (h)VHE is the way of the future, use the CPACR_EL1 format,
    which is a subset of the VHE CPTR_EL2, as a reference.
    
    No functional change intended.
    
    Suggested-by: Oliver Upton <oliver.upton@linux.dev>
    Reviewed-by: Oliver Upton <oliver.upton@linux.dev>
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Link: https://lore.kernel.org/r/20240603122852.3923848-4-tabba@google.com
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/hyp/include/hyp/switch.h
+++ arch/arm64/kvm/hyp/include/hyp/switch.h
@@ -331,7 +331,6 @@ static bool kvm_hyp_handle_fpsimd(struct kvm_vcpu *vcpu, u64 *exit_code)
 {
 	bool sve_guest;
 	u8 esr_ec;
-	u64 reg;
 
 	if (!system_supports_fpsimd())
 		return false;
@@ -354,19 +353,10 @@ static bool kvm_hyp_handle_fpsimd(struct kvm_vcpu *vcpu, u64 *exit_code)
 	/* Valid trap.  Switch the context: */
 
 	/* First disable enough traps to allow us to update the registers */
-	if (has_vhe() || has_hvhe()) {
-		reg = CPACR_EL1_FPEN_EL0EN | CPACR_EL1_FPEN_EL1EN;
-		if (sve_guest)
-			reg |= CPACR_EL1_ZEN_EL0EN | CPACR_EL1_ZEN_EL1EN;
-
-		sysreg_clear_set(cpacr_el1, 0, reg);
-	} else {
-		reg = CPTR_EL2_TFP;
-		if (sve_guest)
-			reg |= CPTR_EL2_TZ;
-
-		sysreg_clear_set(cptr_el2, reg, 0);
-	}
+	if (sve_guest)
+		cpacr_clear_set(0, CPACR_ELx_FPEN | CPACR_ELx_ZEN);
+	else
+		cpacr_clear_set(0, CPACR_ELx_FPEN);
 	isb();
 
 	/* Write out the host state if it's in the registers */
