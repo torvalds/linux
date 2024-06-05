93389767da62142f4fa16733ddbc258d315180e0
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2024-06-06 08:51:46 +0900

    firewire: core: add helper function to handle port status from self ID sequence and its KUnit test
    The self ID sequence delivers the information about the state of port.
    
    This commit adds some enumerations to express the state of port, and
    some helper functions to handle the state. It adds a KUnit test for them,
    too.
    
    Link: https://lore.kernel.org/r/20240605235155.116468-3-o-takashi@sakamocchi.jp
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- drivers/firewire/phy-packet-definitions.h
+++ drivers/firewire/phy-packet-definitions.h
@@ -15,6 +15,8 @@
 #define SELF_ID_EXTENDED_SEQUENCE_MASK			0x00700000
 #define SELF_ID_EXTENDED_SEQUENCE_SHIFT			20
 
+#define SELF_ID_PORT_STATUS_MASK			0x3
+
 #define SELF_ID_SEQUENCE_MAXIMUM_QUADLET_COUNT		4
 
 static inline bool phy_packet_self_id_get_extended(u32 quadlet)
@@ -73,6 +75,47 @@ static inline const u32 *self_id_sequence_enumerator_next(
 	enumerator->quadlet_count -= count;
 
 	return self_id_sequence;
+}
+
+enum phy_packet_self_id_port_status {
+	PHY_PACKET_SELF_ID_PORT_STATUS_NONE = 0,
+	PHY_PACKET_SELF_ID_PORT_STATUS_NCONN = 1,
+	PHY_PACKET_SELF_ID_PORT_STATUS_PARENT = 2,
+	PHY_PACKET_SELF_ID_PORT_STATUS_CHILD = 3,
+};
+
+static inline unsigned int self_id_sequence_get_port_capacity(unsigned int quadlet_count)
+{
+	return quadlet_count * 8 - 5;
+}
+
+static inline enum phy_packet_self_id_port_status self_id_sequence_get_port_status(
+		const u32 *self_id_sequence, unsigned int quadlet_count, unsigned int port_index)
+{
+	unsigned int index, shift;
+
+	index = (port_index + 5) / 8;
+	shift = 16 - ((port_index + 5) % 8) * 2;
+
+	if (index < quadlet_count && index < SELF_ID_SEQUENCE_MAXIMUM_QUADLET_COUNT)
+		return (self_id_sequence[index] >> shift) & SELF_ID_PORT_STATUS_MASK;
+
+	return PHY_PACKET_SELF_ID_PORT_STATUS_NONE;
+}
+
+static inline void self_id_sequence_set_port_status(u32 *self_id_sequence, unsigned int quadlet_count,
+						    unsigned int port_index,
+						    enum phy_packet_self_id_port_status status)
+{
+	unsigned int index, shift;
+
+	index = (port_index + 5) / 8;
+	shift = 16 - ((port_index + 5) % 8) * 2;
+
+	if (index < quadlet_count) {
+		self_id_sequence[index] &= ~(SELF_ID_PORT_STATUS_MASK << shift);
+		self_id_sequence[index] |= status << shift;
+	}
 }
 
 #endif // _FIREWIRE_PHY_PACKET_DEFINITIONS_H
