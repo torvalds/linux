9ac3e660cac3e29cfc817b6a23735b70f12bd16a
Kent Overstreet <kent.overstreet@linux.dev>
2024-06-06 19:30:41 -0400

    bcachefs: set sb->s_shrinker->seeks = 0
    inodes and dentries are still present in the btree node cache, in much
    more compact form
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/fs.c
+++ fs/bcachefs/fs.c
@@ -1967,6 +1967,7 @@ got_sb:
 	sb->s_time_min		= div_s64(S64_MIN, c->sb.time_units_per_sec) + 1;
 	sb->s_time_max		= div_s64(S64_MAX, c->sb.time_units_per_sec);
 	sb->s_uuid		= c->sb.user_uuid;
+	sb->s_shrink->seeks	= 0;
 	c->vfs_sb		= sb;
 	strscpy(sb->s_id, c->name, sizeof(sb->s_id));
 
