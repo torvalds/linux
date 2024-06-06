5071010ac3aa32a1b0f0b4c14d3ea6b217ba21ba
Kemeng Shi <shikemeng@huaweicloud.com>
2024-06-06 20:55:08 +0800

    ext4: correct comment of h_checksum
    Checksum of xattr block is always crc32c(uuid+blknum+xattrblock), see
    ext4_xattr_block_csum_set for detail. Remove incorrect comment that
    "id = inum if refcount=1".
    
    Signed-off-by: Kemeng Shi <shikemeng@huaweicloud.com>
    Link: https://patch.msgid.link/20240606125508.1459893-4-shikemeng@huaweicloud.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/xattr.h
+++ fs/ext4/xattr.h
@@ -32,8 +32,7 @@ struct ext4_xattr_header {
 	__le32	h_refcount;	/* reference count */
 	__le32	h_blocks;	/* number of disk blocks used */
 	__le32	h_hash;		/* hash value of all attributes */
-	__le32	h_checksum;	/* crc32c(uuid+id+xattrblock) */
-				/* id = inum if refcount=1, blknum otherwise */
+	__le32	h_checksum;	/* crc32c(uuid+blknum+xattrblock) */
 	__u32	h_reserved[3];	/* zero right now */
 };
 
