efa6b0a1d66e189fa13e75f704dd8ee5948a3cd3
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2024-06-06 21:48:41 +0300

    serial: 8250_platform: Explicitly show we initialise ISA ports only once
    serial8250_isa_init_ports() uses home grown approach to make itself
    a singleton. Instead, explicitly show that we initialise ISA ports
    once by providing a helper function which calls the original function
    via DO_ONCE() macro.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Link: https://lore.kernel.org/r/20240606184841.3102545-1-andriy.shevchenko@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/8250/8250_platform.c
+++ drivers/tty/serial/8250/8250_platform.c
@@ -9,6 +9,7 @@
 #include <linux/array_size.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
+#include <linux/once.h>
 #include <linux/platform_device.h>
 
 #include <linux/serial_8250.h>
@@ -53,14 +54,9 @@ void serial8250_set_isa_configurator(serial8250_isa_config_fn v)
 }
 EXPORT_SYMBOL(serial8250_set_isa_configurator);
 
-void __init serial8250_isa_init_ports(void)
+static void __init __serial8250_isa_init_ports(void)
 {
-	static int first = 1;
 	int i, irqflag = 0;
-
-	if (!first)
-		return;
-	first = 0;
 
 	if (nr_uarts > UART_NR)
 		nr_uarts = UART_NR;
@@ -100,6 +96,11 @@ void __init serial8250_isa_init_ports(void)
 
 		serial_base_add_isa_preferred_console(serial8250_reg.dev_name, i);
 	}
+}
+
+void __init serial8250_isa_init_ports(void)
+{
+	DO_ONCE(__serial8250_isa_init_ports);
 }
 
 /*
