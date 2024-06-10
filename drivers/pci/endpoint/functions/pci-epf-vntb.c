b6b0fa9e320d5d9386d4a9513ad40409d974ff8e
Dan Carpenter <dan.carpenter@linaro.org>
2024-06-10 12:33:49 +0300

    PCI: endpoint: Fix error handling in epf_ntb_epc_cleanup()
    [ Upstream commit 6bba3c0ac5dc54737998a0982b2e272242c87e0f ]
    
    There are two issues related to epf_ntb_epc_cleanup():
    
      1) It should call epf_ntb_config_sspad_bar_clear()
      2) The epf_ntb_bind() function should call epf_ntb_epc_cleanup()
         to cleanup.
    
    I also changed the ordering a bit.  Unwinding should be done in the
    mirror order from how they are allocated.
    
    Fixes: e35f56bb0330 ("PCI: endpoint: Support NTB transfer between RC and EP")
    Link: https://lore.kernel.org/linux-pci/aaffbe8d-7094-4083-8146-185f4a84e8a1@moroto.mountain
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>
    Signed-off-by: Krzysztof Wilczyński <kwilczynski@kernel.org>
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/pci/endpoint/functions/pci-epf-vntb.c
+++ drivers/pci/endpoint/functions/pci-epf-vntb.c
@@ -813,8 +813,9 @@ err_config_interrupt:
  */
 static void epf_ntb_epc_cleanup(struct epf_ntb *ntb)
 {
-	epf_ntb_db_bar_clear(ntb);
 	epf_ntb_mw_bar_clear(ntb, ntb->num_mws);
+	epf_ntb_db_bar_clear(ntb);
+	epf_ntb_config_sspad_bar_clear(ntb);
 }
 
 #define EPF_NTB_R(_name)						\
@@ -1354,7 +1355,7 @@ static int epf_ntb_bind(struct pci_epf *epf)
 	ret = pci_register_driver(&vntb_pci_driver);
 	if (ret) {
 		dev_err(dev, "failure register vntb pci driver\n");
-		goto err_bar_alloc;
+		goto err_epc_cleanup;
 	}
 
 	ret = vpci_scan_bus(ntb);
@@ -1365,6 +1366,8 @@ static int epf_ntb_bind(struct pci_epf *epf)
 
 err_unregister:
 	pci_unregister_driver(&vntb_pci_driver);
+err_epc_cleanup:
+	epf_ntb_epc_cleanup(ntb);
 err_bar_alloc:
 	epf_ntb_config_spad_bar_free(ntb);
 
