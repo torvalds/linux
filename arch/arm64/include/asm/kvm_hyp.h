ea9d7c83d14e332db9ae25eb2872b90a06ebc9e6
Pierre-Clément Tosi <ptosi@google.com>
2024-06-10 07:32:31 +0100

    KVM: arm64: Fix __pkvm_init_switch_pgd call ABI
    Fix the mismatch between the (incorrect) C signature, C call site, and
    asm implementation by aligning all three on an API passing the
    parameters (pgd and SP) separately, instead of as a bundled struct.
    
    Remove the now unnecessary memory accesses while the MMU is off from the
    asm, which simplifies the C caller (as it does not need to convert a VA
    struct pointer to PA) and makes the code slightly more robust by
    offsetting the struct fields from C and properly expressing the call to
    the C compiler (e.g. type checker and kCFI).
    
    Fixes: f320bc742bc2 ("KVM: arm64: Prepare the creation of s1 mappings at EL2")
    Signed-off-by: Pierre-Clément Tosi <ptosi@google.com>
    Acked-by: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20240610063244.2828978-3-ptosi@google.com
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>

--- arch/arm64/include/asm/kvm_hyp.h
+++ arch/arm64/include/asm/kvm_hyp.h
@@ -124,8 +124,8 @@ void __noreturn __hyp_do_panic(struct kvm_cpu_context *host_ctxt, u64 spsr,
 #endif
 
 #ifdef __KVM_NVHE_HYPERVISOR__
-void __pkvm_init_switch_pgd(phys_addr_t phys, unsigned long size,
-			    phys_addr_t pgd, void *sp, void *cont_fn);
+void __pkvm_init_switch_pgd(phys_addr_t pgd, unsigned long sp,
+		void (*fn)(void));
 int __pkvm_init(phys_addr_t phys, unsigned long size, unsigned long nr_cpus,
 		unsigned long *per_cpu_base, u32 hyp_va_bits);
 void __noreturn __host_enter(struct kvm_cpu_context *host_ctxt);
