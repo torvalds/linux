dfe8e56fc604a6000550174d1002c6076efd8003
Masahiro Yamada <masahiroy@kernel.org>
2024-06-12 02:55:11 +0900

    kconfig: add fallthrough comments to expr_compare_type()
    Clarify the missing 'break' is intentional.
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>

--- scripts/kconfig/expr.c
+++ scripts/kconfig/expr.c
@@ -1083,19 +1083,24 @@ static int expr_compare_type(enum expr_type t1, enum expr_type t2)
 	case E_GTH:
 		if (t2 == E_EQUAL || t2 == E_UNEQUAL)
 			return 1;
+		/* fallthrough */
 	case E_EQUAL:
 	case E_UNEQUAL:
 		if (t2 == E_NOT)
 			return 1;
+		/* fallthrough */
 	case E_NOT:
 		if (t2 == E_AND)
 			return 1;
+		/* fallthrough */
 	case E_AND:
 		if (t2 == E_OR)
 			return 1;
+		/* fallthrough */
 	case E_OR:
 		if (t2 == E_LIST)
 			return 1;
+		/* fallthrough */
 	default:
 		break;
 	}
