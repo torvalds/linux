e19fbe3996aae35a467ebad35ff2b8d84975a65c
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2024-06-11 15:54:33 +0200

    net: dsa: lantiq_gswip: Update comments in gswip_port_vlan_filtering()
    Update the comments in gswip_port_vlan_filtering() so it's clear that
    there are two separate cases, one for "tag based VLAN" and another one
    for "port based VLAN".
    
    Suggested-by: Martin Schiller <ms@dev.tdt.de>
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Acked-by: Hauke Mehrtens <hauke@hauke-m.de>
    Reviewed-by: Vladimir Oltean <olteanv@gmail.com>
    Signed-off-by: Martin Schiller <ms@dev.tdt.de>
    Link: https://lore.kernel.org/r/20240611135434.3180973-12-ms@dev.tdt.de
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/dsa/lantiq_gswip.c
+++ drivers/net/dsa/lantiq_gswip.c
@@ -780,7 +780,7 @@ static int gswip_port_vlan_filtering(struct dsa_switch *ds, int port,
 	}
 
 	if (vlan_filtering) {
-		/* Use port based VLAN tag */
+		/* Use tag based VLAN */
 		gswip_switch_mask(priv,
 				  GSWIP_PCE_VCTRL_VSR,
 				  GSWIP_PCE_VCTRL_UVR | GSWIP_PCE_VCTRL_VIMR |
@@ -789,7 +789,7 @@ static int gswip_port_vlan_filtering(struct dsa_switch *ds, int port,
 		gswip_switch_mask(priv, GSWIP_PCE_PCTRL_0_TVM, 0,
 				  GSWIP_PCE_PCTRL_0p(port));
 	} else {
-		/* Use port based VLAN tag */
+		/* Use port based VLAN */
 		gswip_switch_mask(priv,
 				  GSWIP_PCE_VCTRL_UVR | GSWIP_PCE_VCTRL_VIMR |
 				  GSWIP_PCE_VCTRL_VEMR,
