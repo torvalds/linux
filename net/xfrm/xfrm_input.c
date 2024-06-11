15f5fe9e84839dcc9eaa69b08ced9d24cb464369
Antony Antony <antony.antony@secunet.com>
2024-06-11 08:32:15 +0200

    xfrm: Log input direction mismatch error in one place
    Previously, the offload data path decrypted the packet before checking
    the direction, leading to error logging and packet dropping. However,
    dropped packets wouldn't be visible in tcpdump or audit log.
    
    With this fix, the offload path, upon noticing SA direction mismatch,
    will pass the packet to the stack without decrypting it. The L3 layer
    will then log the error, audit, and drop ESP without decrypting or
    decapsulating it.
    
    This also ensures that the slow path records the error and audit log,
    making dropped packets visible in tcpdump.
    
    Fixes: 304b44f0d5a4 ("xfrm: Add dir validation to "in" data path lookup")
    Signed-off-by: Antony Antony <antony.antony@secunet.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- net/xfrm/xfrm_input.c
+++ net/xfrm/xfrm_input.c
@@ -474,11 +474,6 @@ int xfrm_input(struct sk_buff *skb, int nexthdr, __be32 spi, int encap_type)
 	if (encap_type < 0 || (xo && xo->flags & XFRM_GRO)) {
 		x = xfrm_input_state(skb);
 
-		if (unlikely(x->dir && x->dir != XFRM_SA_DIR_IN)) {
-			XFRM_INC_STATS(net, LINUX_MIB_XFRMINSTATEDIRERROR);
-			goto drop;
-		}
-
 		if (unlikely(x->km.state != XFRM_STATE_VALID)) {
 			if (x->km.state == XFRM_STATE_ACQ)
 				XFRM_INC_STATS(net, LINUX_MIB_XFRMACQUIREERROR);
