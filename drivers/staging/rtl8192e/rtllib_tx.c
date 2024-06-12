23c48df4087aebed875ba18857fd245b98e1603f
Tree Davies <tdavies@darkphysics.net>
2024-06-11 20:22:23 -0700

    Staging: rtl8192e: Rename function rtllib_query_ShortPreambleMode
    Rename function rtllib_query_ShortPreambleMode to
    rtllib_query_short_preamble_mode to fix checkpatch warning Avoid
    CamelCase.
    
    Signed-off-by: Tree Davies <tdavies@darkphysics.net>
    Tested-by: Philipp Hortmann <philipp.g.hortmann@gmail.com>
    Reviewed-by: Dan Carpenter <dan.carpenter@linaro.org>
    Link: https://lore.kernel.org/r/20240612032230.9738-18-tdavies@darkphysics.net
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtllib_tx.c
+++ drivers/staging/rtl8192e/rtllib_tx.c
@@ -319,7 +319,7 @@ static void rtllib_tx_query_agg_cap(struct rtllib_device *ieee,
 	}
 }
 
-static void rtllib_query_ShortPreambleMode(struct rtllib_device *ieee,
+static void rtllib_query_short_preamble_mode(struct rtllib_device *ieee,
 					   struct cb_desc *tcb_desc)
 {
 	tcb_desc->bUseShortPreamble = false;
@@ -868,7 +868,7 @@ static int rtllib_xmit_inter(struct sk_buff *skb, struct net_device *dev)
 				tcb_desc->bdhcp = 1;
 			}
 
-			rtllib_query_ShortPreambleMode(ieee, tcb_desc);
+			rtllib_query_short_preamble_mode(ieee, tcb_desc);
 			rtllib_tx_query_agg_cap(ieee, txb->fragments[0],
 						tcb_desc);
 			rtllib_query_HTCapShortGI(ieee, tcb_desc);
