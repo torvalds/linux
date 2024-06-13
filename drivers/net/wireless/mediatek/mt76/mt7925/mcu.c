523f5d9c256079fe21087b3dfc4c3c0c1fffab53
Sean Wang <sean.wang@mediatek.com>
2024-06-12 20:02:34 -0700

    wifi: mt76: mt7925: extend mt7925_mcu_bss_mld_tlv for per-link STA
    Extend mt7925_mcu_bss_mld_tlv with the per-link STA configuration.
    
    Co-developed-by: Ming Yen Hsieh <mingyen.hsieh@mediatek.com>
    Signed-off-by: Ming Yen Hsieh <mingyen.hsieh@mediatek.com>
    Co-developed-by: Deren Wu <deren.wu@mediatek.com>
    Signed-off-by: Deren Wu <deren.wu@mediatek.com>
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Link: https://patch.msgid.link/20240613030241.5771-41-sean.wang@kernel.org
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7925/mcu.c
+++ drivers/net/wireless/mediatek/mt76/mt7925/mcu.c
@@ -2228,7 +2228,7 @@ mt7925_mcu_bss_bmc_tlv(struct sk_buff *skb, struct mt792x_phy *phy,
 static void
 mt7925_mcu_bss_mld_tlv(struct sk_buff *skb,
 		       struct ieee80211_bss_conf *link_conf,
-		       struct ieee80211_sta *sta)
+		       struct ieee80211_link_sta *link_sta)
 {
 	struct mt792x_bss_conf *mconf = mt792x_link_conf_to_mconf(link_conf);
 	bool is_mld = ieee80211_vif_is_mld(link_conf->vif);
@@ -2238,13 +2238,13 @@ mt7925_mcu_bss_mld_tlv(struct sk_buff *skb,
 	tlv = mt76_connac_mcu_add_tlv(skb, UNI_BSS_INFO_MLD, sizeof(*mld));
 	mld = (struct bss_mld_tlv *)tlv;
 
-	mld->link_id = sta ? (is_mld ? link_conf->link_id : 0) : 0xff;
+	mld->link_id = link_sta ? (is_mld ? link_conf->link_id : 0) : 0xff;
 	mld->group_mld_id = is_mld ? mconf->mt76.idx : 0xff;
 	mld->own_mld_id = mconf->mt76.idx + 32;
 	mld->remap_idx = 0xff;
 
-	if (sta)
-		memcpy(mld->mac_addr, sta->addr, ETH_ALEN);
+	if (link_sta)
+		memcpy(mld->mac_addr, link_sta->addr, ETH_ALEN);
 }
 
 static void
@@ -2359,7 +2359,7 @@ int mt7925_mcu_add_bss_info(struct mt792x_phy *phy,
 
 	mt7925_mcu_bss_bmc_tlv(skb, phy, ctx, link_conf);
 	mt7925_mcu_bss_qos_tlv(skb, link_conf);
-	mt7925_mcu_bss_mld_tlv(skb, link_conf, link_sta->sta);
+	mt7925_mcu_bss_mld_tlv(skb, link_conf, link_sta);
 	mt7925_mcu_bss_ifs_tlv(skb, link_conf);
 
 	if (link_conf->he_support) {
