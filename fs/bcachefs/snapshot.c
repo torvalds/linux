1ba44217f8258f92c56644ca4fad4462f1941e33
Kent Overstreet <kent.overstreet@linux.dev>
2024-06-17 09:51:01 -0400

    bcachefs: delete_dead_snapshots() doesn't need to go RW
    We've been moving away from going RW lazily; if we want to go RW we do
    that in set_may_go_rw(), and if we didn't go RW we don't need to delete
    dead snapshots.
    
    Reported-by: syzbot+4366624c0b5aac4906cf@syzkaller.appspotmail.com
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/snapshot.c
+++ fs/bcachefs/snapshot.c
@@ -1565,13 +1565,6 @@ int bch2_delete_dead_snapshots(struct bch_fs *c)
 	if (!test_and_clear_bit(BCH_FS_need_delete_dead_snapshots, &c->flags))
 		return 0;
 
-	if (!test_bit(BCH_FS_started, &c->flags)) {
-		ret = bch2_fs_read_write_early(c);
-		bch_err_msg(c, ret, "deleting dead snapshots: error going rw");
-		if (ret)
-			return ret;
-	}
-
 	trans = bch2_trans_get(c);
 
 	/*
