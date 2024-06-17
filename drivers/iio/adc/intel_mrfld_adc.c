0544a8c2fb32c9e7534b787f4888ccd75990e67e
Trevor Gamblin <tgamblin@baylibre.com>
2024-06-17 09:49:52 -0400

    iio: adc: intel_mrfld_adc: make use of regmap_clear_bits()
    Instead of using regmap_update_bits() and passing val = 0, use
    regmap_clear_bits().
    
    Suggested-by: Uwe Kleine-König <u.kleine-koenig@baylibre.com>
    Signed-off-by: Trevor Gamblin <tgamblin@baylibre.com>
    Acked-by: Uwe Kleine-König <u.kleine-koenig@baylibre.com>
    Link: https://patch.msgid.link/20240617-review-v3-12-88d1338c4cca@baylibre.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/adc/intel_mrfld_adc.c
+++ drivers/iio/adc/intel_mrfld_adc.c
@@ -81,8 +81,8 @@ static int mrfld_adc_single_conv(struct iio_dev *indio_dev,
 
 	reinit_completion(&adc->completion);
 
-	regmap_update_bits(regmap, BCOVE_MADCIRQ, BCOVE_ADCIRQ_ALL, 0);
-	regmap_update_bits(regmap, BCOVE_MIRQLVL1, BCOVE_LVL1_ADC, 0);
+	regmap_clear_bits(regmap, BCOVE_MADCIRQ, BCOVE_ADCIRQ_ALL);
+	regmap_clear_bits(regmap, BCOVE_MIRQLVL1, BCOVE_LVL1_ADC);
 
 	ret = regmap_read_poll_timeout(regmap, BCOVE_GPADCREQ, req,
 				       !(req & BCOVE_GPADCREQ_BUSY),
