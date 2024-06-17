ea04ef19ebdcd22e8a21054a19c2c8fefae011ce
Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
2024-06-17 15:45:51 -0400

    drm/amd/display: Add debug option for disabling SLDO optimizations
    [Why]
    DM can hook this up to disable SLDO optimizations in firmware during
    DMCUB initialization for debug purposes.
    
    [How]
    Add the option and pass it through dmub_srv.
    
    Reviewed-by: Hansen Dsouza <hansen.dsouza@amd.com>
    Signed-off-by: Jerry Zuo <jerry.zuo@amd.com>
    Signed-off-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dmub/src/dmub_dcn35.c
+++ drivers/gpu/drm/amd/display/dmub/src/dmub_dcn35.c
@@ -424,6 +424,7 @@ void dmub_dcn35_enable_dmub_boot_options(struct dmub_srv *dmub, const struct dmu
 	boot_options.bits.disable_clk_gate = params->disable_clock_gate;
 	boot_options.bits.ips_disable = params->disable_ips;
 	boot_options.bits.ips_sequential_ono = params->ips_sequential_ono;
+	boot_options.bits.disable_sldo_opt = params->disable_sldo_opt;
 
 	REG_WRITE(DMCUB_SCRATCH14, boot_options.all);
 }
