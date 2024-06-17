5a820b13db3988122080f8de2920721f770c37a0
Iulia Tanasescu <iulia.tanasescu@nxp.com>
2024-06-17 11:52:06 +0300

    Bluetooth: hci_event: Set QoS encryption from BIGInfo report
    On a Broadcast Sink, after synchronizing to the PA transimitted by a
    Broadcast Source, the BIGInfo advertising reports emitted by the
    Controller hold the encryption field, which indicates whether the
    Broadcast Source is transmitting encrypted streams.
    
    This updates the PA sync hcon QoS with the encryption value reported
    in the BIGInfo report, so that this information is accurate if the
    userspace tries to access the QoS struct via getsockopt.
    
    Fixes: 1d11d70d1f6b ("Bluetooth: ISO: Pass BIG encryption info through QoS")
    Signed-off-by: Iulia Tanasescu <iulia.tanasescu@nxp.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -6988,6 +6988,8 @@ static void hci_le_big_info_adv_report_evt(struct hci_dev *hdev, void *data,
 	if (!pa_sync)
 		goto unlock;
 
+	pa_sync->iso_qos.bcast.encryption = ev->encryption;
+
 	/* Notify iso layer */
 	hci_connect_cfm(pa_sync, 0);
 
