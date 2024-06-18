119474bdbac0858053cf367c8c932dd5c4bb4e85
Filipe Manana <fdmanana@suse.com>
2024-06-18 08:28:30 +0100

    btrfs: remove NULL transaction support for btrfs_lookup_extent_info()
    There are no callers of btrfs_lookup_extent_info() that pass a NULL value
    for the transaction handle argument, so there's no point in having special
    logic to deal with the NULL. The last caller that passed a NULL value was
    removed in commit 19b546d7a1b2 ("btrfs: relocation:
    Use btrfs_find_all_leafs to locate data extent parent tree leaves").
    
    So remove the NULL handling from btrfs_lookup_extent_info().
    
    Reviewed-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: Filipe Manana <fdmanana@suse.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/extent-tree.c
+++ fs/btrfs/extent-tree.c
@@ -126,11 +126,6 @@ int btrfs_lookup_extent_info(struct btrfs_trans_handle *trans,
 	if (!path)
 		return -ENOMEM;
 
-	if (!trans) {
-		path->skip_locking = 1;
-		path->search_commit_root = 1;
-	}
-
 search_again:
 	key.objectid = bytenr;
 	key.offset = offset;
@@ -171,11 +166,7 @@ search_again:
 			btrfs_err(fs_info,
 			"unexpected extent item size, has %u expect >= %zu",
 				  item_size, sizeof(*ei));
-			if (trans)
-				btrfs_abort_transaction(trans, ret);
-			else
-				btrfs_handle_fs_error(fs_info, ret, NULL);
-
+			btrfs_abort_transaction(trans, ret);
 			goto out_free;
 		}
 
@@ -185,9 +176,6 @@ search_again:
 		extent_flags = 0;
 		ret = 0;
 	}
-
-	if (!trans)
-		goto out;
 
 	delayed_refs = &trans->transaction->delayed_refs;
 	spin_lock(&delayed_refs->lock);
@@ -219,7 +207,7 @@ search_again:
 		mutex_unlock(&head->mutex);
 	}
 	spin_unlock(&delayed_refs->lock);
-out:
+
 	WARN_ON(num_refs == 0);
 	if (refs)
 		*refs = num_refs;
