541fe90ee699658134c26539fdc0ea31c8f47fa9
Frank Min <Frank.Min@amd.com>
2024-06-18 14:34:24 +0800

    drm/amdgpu: update MTYPE mapping for gfx12
    gfx12 only support MTYPE UC and NC, so update it accordingly.
    
    Signed-off-by: Frank Min <Frank.Min@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gmc_v12_0.c
+++ drivers/gpu/drm/amd/amdgpu/gmc_v12_0.c
@@ -464,10 +464,6 @@ static uint64_t gmc_v12_0_map_mtype(struct amdgpu_device *adev, uint32_t flags)
 		return AMDGPU_PTE_MTYPE_GFX12(0ULL, MTYPE_NC);
 	case AMDGPU_VM_MTYPE_NC:
 		return AMDGPU_PTE_MTYPE_GFX12(0ULL, MTYPE_NC);
-	case AMDGPU_VM_MTYPE_WC:
-		return AMDGPU_PTE_MTYPE_GFX12(0ULL, MTYPE_WC);
-	case AMDGPU_VM_MTYPE_CC:
-		return AMDGPU_PTE_MTYPE_GFX12(0ULL, MTYPE_CC);
 	case AMDGPU_VM_MTYPE_UC:
 		return AMDGPU_PTE_MTYPE_GFX12(0ULL, MTYPE_UC);
 	default:
