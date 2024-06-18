fe8929bdf83512ed7e413a28e543c725bf536354
Ashutosh Dixit <ashutosh.dixit@intel.com>
2024-06-17 18:45:54 -0700

    drm/xe/perf/uapi: Add perf_stream_paranoid sysctl
    Normally only superuser/root can access perf counter data. However,
    superuser can set perf_stream_paranoid sysctl to 0 to allow non-privileged
    users to also access perf data. perf_stream_paranoid is introduced at the
    perf layer to allow different perf stream types to share this access
    mechanism.
    
    v2: Add kernel doc for non-static functions (Michal)
    
    Acked-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Acked-by: José Roberto de Souza <jose.souza@intel.com>
    Reviewed-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
    Signed-off-by: Ashutosh Dixit <ashutosh.dixit@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240618014609.3233427-3-ashutosh.dixit@intel.com

--- drivers/gpu/drm/xe/xe_perf.c
+++ drivers/gpu/drm/xe/xe_perf.c
@@ -4,10 +4,14 @@
  */
 
 #include <linux/errno.h>
+#include <linux/sysctl.h>
 
 #include <drm/xe_drm.h>
 
 #include "xe_perf.h"
+
+u32 xe_perf_stream_paranoid = true;
+static struct ctl_table_header *sysctl_header;
 
 /**
  * xe_perf_ioctl - The top level perf layer ioctl
@@ -31,4 +35,40 @@ int xe_perf_ioctl(struct drm_device *dev, void *data, struct drm_file *file)
 	default:
 		return -EINVAL;
 	}
+}
+
+static struct ctl_table perf_ctl_table[] = {
+	{
+	 .procname = "perf_stream_paranoid",
+	 .data = &xe_perf_stream_paranoid,
+	 .maxlen = sizeof(xe_perf_stream_paranoid),
+	 .mode = 0644,
+	 .proc_handler = proc_dointvec_minmax,
+	 .extra1 = SYSCTL_ZERO,
+	 .extra2 = SYSCTL_ONE,
+	 },
+	{}
+};
+
+/**
+ * xe_perf_sysctl_register - Register "perf_stream_paranoid" sysctl
+ *
+ * Normally only superuser/root can access perf counter data. However,
+ * superuser can set perf_stream_paranoid sysctl to 0 to allow non-privileged
+ * users to also access perf data.
+ *
+ * Return: always returns 0
+ */
+int xe_perf_sysctl_register(void)
+{
+	sysctl_header = register_sysctl("dev/xe", perf_ctl_table);
+	return 0;
+}
+
+/**
+ * xe_perf_sysctl_unregister - Unregister "perf_stream_paranoid" sysctl
+ */
+void xe_perf_sysctl_unregister(void)
+{
+	unregister_sysctl_table(sysctl_header);
 }
--- drivers/gpu/drm/xe/xe_perf.h
+++ drivers/gpu/drm/xe/xe_perf.h
@@ -6,9 +6,15 @@
 #ifndef _XE_PERF_H_
 #define _XE_PERF_H_
 
+#include <linux/types.h>
+
 struct drm_device;
 struct drm_file;
 
+extern u32 xe_perf_stream_paranoid;
+
 int xe_perf_ioctl(struct drm_device *dev, void *data, struct drm_file *file);
+int xe_perf_sysctl_register(void);
+void xe_perf_sysctl_unregister(void);
 
 #endif
