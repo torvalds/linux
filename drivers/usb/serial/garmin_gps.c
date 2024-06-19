55a15b3a713a3f24360cf9d8dcfd2a3e337321d6
Javier Carrasco <javier.carrasco.cruz@gmail.com>
2024-06-19 21:42:44 +0200

    USB: serial: garmin_gps: annotate struct garmin_packet with __counted_by
    Use the __counted_by compiler attribute for the data[] flexible array
    member to improve the results of array bound sanitizers.
    
    Reviewed-by: Nathan Chancellor <nathan@kernel.org>
    Signed-off-by: Javier Carrasco <javier.carrasco.cruz@gmail.com>
    Reviewed-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Reviewed-by: Kees Cook <kees@kernel.org>
    Signed-off-by: Johan Hovold <johan@kernel.org>

--- drivers/usb/serial/garmin_gps.c
+++ drivers/usb/serial/garmin_gps.c
@@ -104,7 +104,7 @@ struct garmin_packet {
 	int               seq;
 	/* the real size of the data array, always > 0 */
 	int               size;
-	__u8              data[];
+	__u8              data[] __counted_by(size);
 };
 
 /* structure used to keep the current state of the driver */
