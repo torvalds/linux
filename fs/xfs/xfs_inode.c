673cd885bbbfd873aa6983ce2363a813b7826425
Darrick J. Wong <djwong@kernel.org>
2024-06-19 10:32:46 -0700

    xfs: honor init_xattrs in xfs_init_new_inode for !ATTR fs
    xfs_init_new_inode ignores the init_xattrs parameter for filesystems
    that do not have ATTR enabled.  As a result, the first init_xattrs file
    to be created by the kernel will not have an attr fork created to store
    acls.  Storing that first acl will add ATTR to the superblock flags, so
    subsequent files will be created with attr forks.  The overhead of this
    is so small that chances are that nobody has noticed this behavior.
    
    However, this is disastrous on a filesystem with parent pointers because
    it requires that a new linkable file /must/ have a pre-existing attr
    fork, and the parent pointers code uses init_xattrs to create that fork.
    The preproduction version of mkfs.xfs used to set this, but the V5 sb
    verifier only requires ATTR2, not ATTR.  There is no guard for
    filesystems with (PARENT && !ATTR).
    
    It turns out that I misunderstood the two flags -- ATTR means that we at
    some point created an attr fork to store xattrs in a file; ATTR2
    apparently means only that inodes have dynamic fork offsets or that the
    filesystem was mounted with the "attr2" option.
    
    Fixes: 2442ee15bb1e ("xfs: eager inode attr fork init needs attr feature awareness")
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Chandan Babu R <chandanbabu@kernel.org>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -42,6 +42,7 @@
 #include "xfs_pnfs.h"
 #include "xfs_parent.h"
 #include "xfs_xattr.h"
+#include "xfs_sb.h"
 
 struct kmem_cache *xfs_inode_cache;
 
@@ -870,9 +871,16 @@ xfs_init_new_inode(
 	 * this saves us from needing to run a separate transaction to set the
 	 * fork offset in the immediate future.
 	 */
-	if (init_xattrs && xfs_has_attr(mp)) {
+	if (init_xattrs) {
 		ip->i_forkoff = xfs_default_attroffset(ip) >> 3;
 		xfs_ifork_init_attr(ip, XFS_DINODE_FMT_EXTENTS, 0);
+
+		if (!xfs_has_attr(mp)) {
+			spin_lock(&mp->m_sb_lock);
+			xfs_add_attr(mp);
+			spin_unlock(&mp->m_sb_lock);
+			xfs_log_sb(tp);
+		}
 	}
 
 	/*
