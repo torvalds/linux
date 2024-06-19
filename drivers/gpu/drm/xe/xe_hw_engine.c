7875fe7c2495884dd08d4a23fdc44147225542da
Michal Wajdeczko <michal.wajdeczko@intel.com>
2024-06-19 23:45:56 +0200

    drm/xe/vf: Skip engine ring enabling if VF
    All engines will be correctly initialized by the PF driver.
    Moreover, VF drivers can't access related engine registers.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Reviewed-by: Piotr Piórkowski <piotr.piorkowski@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240619214557.905-9-michal.wajdeczko@intel.com

--- drivers/gpu/drm/xe/xe_hw_engine.c
+++ drivers/gpu/drm/xe/xe_hw_engine.c
@@ -547,7 +547,8 @@ static int hw_engine_init(struct xe_gt *gt, struct xe_hw_engine *hwe,
 		if (hwe->class == XE_ENGINE_CLASS_OTHER)
 			hwe->irq_handler = xe_gsc_hwe_irq_handler;
 
-		xe_hw_engine_enable_ring(hwe);
+		if (!IS_SRIOV_VF(xe))
+			xe_hw_engine_enable_ring(hwe);
 	}
 
 	/* We reserve the highest BCS instance for USM */
