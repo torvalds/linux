89f415b99050fbf7bcf3fca0cde30c09b62265eb
Geert Uytterhoeven <geert+renesas@glider.be>
2024-06-20 17:39:45 +0200

    mfd: tmio: Remove obsolete .set_clk_div() callback
    Commit bef64d2908e825c5 ("mmc: remove tmio_mmc driver") removed the last
    user of the .set_clk_div() callback in the tmio_mmc_data structure.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Tested-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Reviewed-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
    Acked-by: Lee Jones <lee@kernel.org>
    Link: https://lore.kernel.org/r/e0fa98f138a7b2836128178f8b3a757978517307.1718897545.git.geert+renesas@glider.be
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- include/linux/mfd/tmio.h
+++ include/linux/mfd/tmio.h
@@ -101,7 +101,6 @@ struct tmio_mmc_data {
 	unsigned int			max_blk_count;
 	unsigned short			max_segs;
 	void (*set_pwr)(struct platform_device *host, int state);
-	void (*set_clk_div)(struct platform_device *host, int state);
 };
 
 /*
