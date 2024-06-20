43d7ca2907cb0e8a23d73934a5cf000e24b0aa22
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2024-06-20 15:21:53 +0200

    net: Use __napi_alloc_frag_align() instead of open coding it.
    The else condition within __netdev_alloc_frag_align() is an open coded
    __napi_alloc_frag_align().
    
    Use __napi_alloc_frag_align() instead of open coding it.
    Move fragsz assignment before page_frag_alloc_align() invocation because
    __napi_alloc_frag_align() also contains this statement.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Link: https://patch.msgid.link/20240620132727.660738-4-bigeasy@linutronix.de
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -318,19 +318,15 @@ void *__netdev_alloc_frag_align(unsigned int fragsz, unsigned int align_mask)
 {
 	void *data;
 
-	fragsz = SKB_DATA_ALIGN(fragsz);
 	if (in_hardirq() || irqs_disabled()) {
 		struct page_frag_cache *nc = this_cpu_ptr(&netdev_alloc_cache);
 
+		fragsz = SKB_DATA_ALIGN(fragsz);
 		data = __page_frag_alloc_align(nc, fragsz, GFP_ATOMIC,
 					       align_mask);
 	} else {
-		struct napi_alloc_cache *nc;
-
 		local_bh_disable();
-		nc = this_cpu_ptr(&napi_alloc_cache);
-		data = __page_frag_alloc_align(&nc->page, fragsz, GFP_ATOMIC,
-					       align_mask);
+		data = __napi_alloc_frag_align(fragsz, align_mask);
 		local_bh_enable();
 	}
 	return data;
