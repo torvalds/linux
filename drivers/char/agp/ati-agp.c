e4066d17b92ea7930fbef990912c49b07430a6e0
Jeff Johnson <quic_jjohnson@quicinc.com>
2024-06-21 22:14:29 -0700

    video: agp: add remaining missing MODULE_DESCRIPTION() macros
    With ARCH=i386, make allmodconfig && make W=1 C=1 reports:
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/char/agp/ali-agp.o
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/char/agp/ati-agp.o
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/char/agp/amd-k7-agp.o
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/char/agp/efficeon-agp.o
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/char/agp/nvidia-agp.o
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/char/agp/sworks-agp.o
    
    Add the missing invocation of the MODULE_DESCRIPTION() macro to all
    files which have a MODULE_LICENSE().
    
    This includes alpha-agp.c and parisc-agp.c which, although they did
    not produce a warning with the i386 allmodconfig configuration, may
    cause this warning with other configurations.
    
    Signed-off-by: Jeff Johnson <quic_jjohnson@quicinc.com>
    Signed-off-by: Helge Deller <deller@gmx.de>

--- drivers/char/agp/ati-agp.c
+++ drivers/char/agp/ati-agp.c
@@ -572,5 +572,6 @@ module_init(agp_ati_init);
 module_exit(agp_ati_cleanup);
 
 MODULE_AUTHOR("Dave Jones");
+MODULE_DESCRIPTION("ATi AGPGART routines");
 MODULE_LICENSE("GPL and additional rights");
 
