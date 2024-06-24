1f5ed3ae020a0624c9f51ed9720f8279bb0cae60
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2024-06-24 00:16:17 +0000

    clk: renesas: r8a779h0: Add Audio clocks
    Add module clocks for the Audio (SSI/SSIU) blocks on the Renesas R-Car
    V4M (R8A779H0) SoC.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://lore.kernel.org/87h6djkxf2.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- drivers/clk/renesas/r8a779h0-cpg-mssr.c
+++ drivers/clk/renesas/r8a779h0-cpg-mssr.c
@@ -233,6 +233,8 @@ static const struct mssr_mod_clk r8a779h0_mod_clks[] = {
 	DEF_MOD("pfc1",		916,	R8A779H0_CLK_CP),
 	DEF_MOD("pfc2",		917,	R8A779H0_CLK_CP),
 	DEF_MOD("tsc2:tsc1",	919,	R8A779H0_CLK_CL16M),
+	DEF_MOD("ssiu",		2926,	R8A779H0_CLK_S0D6_PER),
+	DEF_MOD("ssi",		2927,	R8A779H0_CLK_S0D6_PER),
 };
 
 /*
