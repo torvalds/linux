7c877586da3178974a8a94577b6045a48377ff25
Jan Kara <jack@suse.cz>
2024-06-25 12:18:53 +0200

    readahead: properly shorten readahead when falling back to do_page_cache_ra()
    When we succeed in creating some folios in page_cache_ra_order() but then
    need to fallback to single page folios, we don't shorten the amount to
    read passed to do_page_cache_ra() by the amount we've already read.  This
    then results in reading more and also in placing another readahead mark in
    the middle of the readahead window which confuses readahead code.  Fix the
    problem by properly reducing number of pages to read.
    
    Link: https://lkml.kernel.org/r/20240625101909.12234-3-jack@suse.cz
    Signed-off-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Tested-by: Zhang Peng <zhangpengpeng0808@gmail.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/readahead.c
+++ mm/readahead.c
@@ -491,7 +491,8 @@ void page_cache_ra_order(struct readahead_control *ractl,
 		struct file_ra_state *ra, unsigned int new_order)
 {
 	struct address_space *mapping = ractl->mapping;
-	pgoff_t index = readahead_index(ractl);
+	pgoff_t start = readahead_index(ractl);
+	pgoff_t index = start;
 	pgoff_t limit = (i_size_read(mapping->host) - 1) >> PAGE_SHIFT;
 	pgoff_t mark = index + ra->size - ra->async_size;
 	unsigned int nofs;
@@ -544,7 +545,7 @@ void page_cache_ra_order(struct readahead_control *ractl,
 	if (!err)
 		return;
 fallback:
-	do_page_cache_ra(ractl, ra->size, ra->async_size);
+	do_page_cache_ra(ractl, ra->size - (index - start), ra->async_size);
 }
 
 /*
