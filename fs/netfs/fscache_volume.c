85b08b31a22b481ec6528130daf94eee4452e23f
Baokun Li <libaokun1@huawei.com>
2024-06-28 14:29:22 +0800

    netfs, fscache: export fscache_put_volume() and add fscache_try_get_volume()
    Export fscache_put_volume() and add fscache_try_get_volume()
    helper function to allow cachefiles to get/put fscache_volume
    via linux/fscache-cache.h.
    
    Signed-off-by: Baokun Li <libaokun1@huawei.com>
    Link: https://lore.kernel.org/r/20240628062930.2467993-2-libaokun@huaweicloud.com
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- fs/netfs/fscache_volume.c
+++ fs/netfs/fscache_volume.c
@@ -27,6 +27,19 @@ struct fscache_volume *fscache_get_volume(struct fscache_volume *volume,
 	return volume;
 }
 
+struct fscache_volume *fscache_try_get_volume(struct fscache_volume *volume,
+					      enum fscache_volume_trace where)
+{
+	int ref;
+
+	if (!__refcount_inc_not_zero(&volume->ref, &ref))
+		return NULL;
+
+	trace_fscache_volume(volume->debug_id, ref + 1, where);
+	return volume;
+}
+EXPORT_SYMBOL(fscache_try_get_volume);
+
 static void fscache_see_volume(struct fscache_volume *volume,
 			       enum fscache_volume_trace where)
 {
@@ -420,6 +433,7 @@ void fscache_put_volume(struct fscache_volume *volume,
 			fscache_free_volume(volume);
 	}
 }
+EXPORT_SYMBOL(fscache_put_volume);
 
 /*
  * Relinquish a volume representation cookie.
