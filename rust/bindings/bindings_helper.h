5b026e34120766408e76ba19a0e33a9dc996f9f0
Andreas Hindborg <a.hindborg@samsung.com>
2024-06-28 11:11:52 +0200

    rust: block: fix generated bindings after refactoring of features
    Block device features and flags were refactored from `enum` to `#define`.
    This broke Rust binding generation. This patch fixes the binding
    generation.
    
    Fixes: fcf865e357f8 ("block: convert features and flags to __bitwise types")
    Signed-off-by: Andreas Hindborg <a.hindborg@samsung.com>
    Acked-by: Miguel Ojeda <ojeda@kernel.org>
    Link: https://lore.kernel.org/r/20240628091152.2185241-1-nmi@metaspace.dk
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -9,6 +9,7 @@
 #include <kunit/test.h>
 #include <linux/blk_types.h>
 #include <linux/blk-mq.h>
+#include <linux/blkdev.h>
 #include <linux/errname.h>
 #include <linux/ethtool.h>
 #include <linux/jiffies.h>
@@ -28,3 +29,4 @@ const gfp_t RUST_CONST_HELPER_GFP_KERNEL = GFP_KERNEL;
 const gfp_t RUST_CONST_HELPER_GFP_KERNEL_ACCOUNT = GFP_KERNEL_ACCOUNT;
 const gfp_t RUST_CONST_HELPER_GFP_NOWAIT = GFP_NOWAIT;
 const gfp_t RUST_CONST_HELPER___GFP_ZERO = __GFP_ZERO;
+const blk_features_t RUST_CONST_HELPER_BLK_FEAT_ROTATIONAL = BLK_FEAT_ROTATIONAL;
