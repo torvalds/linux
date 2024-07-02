6b0e82791bd03b2326c7f7d8c1124c825742f2a4
Christophe Leroy <christophe.leroy@csgroup.eu>
2024-07-02 15:51:28 +0200

    powerpc/e500: switch to 64 bits PGD on 85xx (32 bits)
    At the time being when CONFIG_PTE_64BIT is selected, PTE entries are 64
    bits but PGD entries are still 32 bits.
    
    In order to allow leaf PMD entries, switch the PGD to 64 bits entries.
    
    Link: https://lkml.kernel.org/r/ca85397df02564e5edc3a3c27b55cf43af3e4ef3.1719928057.git.christophe.leroy@csgroup.eu
    Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    Cc: Jason Gunthorpe <jgg@nvidia.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Peter Xu <peterx@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/powerpc/include/asm/pgtable-types.h
+++ arch/powerpc/include/asm/pgtable-types.h
@@ -49,7 +49,11 @@ static inline unsigned long pud_val(pud_t x)
 #endif /* CONFIG_PPC64 */
 
 /* PGD level */
+#if defined(CONFIG_PPC_E500) && defined(CONFIG_PTE_64BIT)
+typedef struct { unsigned long long pgd; } pgd_t;
+#else
 typedef struct { unsigned long pgd; } pgd_t;
+#endif
 #define __pgd(x)	((pgd_t) { (x) })
 static inline unsigned long pgd_val(pgd_t x)
 {
