5b674baa596e624fde8bf62b9a3d8a26eef399b2
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2024-07-02 16:44:27 +0200

    thermal: trip: Fold __thermal_zone_get_trip() into its caller
    Because __thermal_zone_get_trip() is only called by thermal_zone_get_trip()
    now, fold the former into the latter.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://patch.msgid.link/22339769.EfDdHjke4D@rjwysocki.net

--- include/linux/thermal.h
+++ include/linux/thermal.h
@@ -202,8 +202,6 @@ static inline void devm_thermal_of_zone_unregister(struct device *dev,
 }
 #endif
 
-int __thermal_zone_get_trip(struct thermal_zone_device *tz, int trip_id,
-			    struct thermal_trip *trip);
 int thermal_zone_get_trip(struct thermal_zone_device *tz, int trip_id,
 			  struct thermal_trip *trip);
 int for_each_thermal_trip(struct thermal_zone_device *tz,
