b6ad1091666732e8750357f9d2fb599f81bf6bff
Saleemkhan Jamadar <saleemkhan.jamadar@amd.com>
2024-07-04 15:19:35 +0530

    drm/amdgpu: enable dpg for vcn and jpeg on GC 11_5_2
    DPG mode is enabled for vcn and jpeg on VCN v4_0_5
    
    Signed-off-by: Saleemkhan Jamadar <saleemkhan.jamadar@amd.com>
    Reviewed-by: Tim Huang <tim.huang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/soc21.c
+++ drivers/gpu/drm/amd/amdgpu/soc21.c
@@ -774,7 +774,9 @@ static int soc21_common_early_init(void *handle)
 			AMD_CG_SUPPORT_IH_CG |
 			AMD_CG_SUPPORT_BIF_MGCG |
 			AMD_CG_SUPPORT_BIF_LS;
-		adev->pg_flags = AMD_PG_SUPPORT_VCN |
+		adev->pg_flags = AMD_PG_SUPPORT_VCN_DPG |
+			AMD_PG_SUPPORT_VCN |
+			AMD_PG_SUPPORT_JPEG_DPG |
 			AMD_PG_SUPPORT_JPEG |
 			AMD_PG_SUPPORT_GFX_PG;
 		adev->external_rev_id = adev->rev_id + 0x40;
