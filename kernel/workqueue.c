c5178e6ca6c8063edc103b75f410add7e4565e63
Lai Jiangshan <jiangshan.ljs@antgroup.com>
2024-07-04 11:49:11 +0800

    workqueue: Make rescuer initialization as the last step of the creation of a new wq
    For early wq allocation, rescuer initialization is the last step of the
    creation of a new wq.  Make the behavior the same for all allocations.
    
    Prepare for initializing rescuer's affinities with the default pwq's
    affinities.
    
    Prepare for moving the whole workqueue initializing procedure into
    wq_pool_mutex and cpu hotplug locks.
    
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Waiman Long <longman@redhat.com>
    Signed-off-by: Lai Jiangshan <jiangshan.ljs@antgroup.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -5681,9 +5681,6 @@ struct workqueue_struct *alloc_workqueue(const char *fmt,
 	if (alloc_and_link_pwqs(wq) < 0)
 		goto err_free_node_nr_active;
 
-	if (wq_online && init_rescuer(wq) < 0)
-		goto err_destroy;
-
 	/*
 	 * wq_pool_mutex protects global freeze state and workqueues list.
 	 * Grab it, adjust max_active and add the new @wq to workqueues
@@ -5698,6 +5695,9 @@ struct workqueue_struct *alloc_workqueue(const char *fmt,
 	list_add_tail_rcu(&wq->list, &workqueues);
 
 	mutex_unlock(&wq_pool_mutex);
+
+	if (wq_online && init_rescuer(wq) < 0)
+		goto err_destroy;
 
 	if ((wq->flags & WQ_SYSFS) && workqueue_sysfs_register(wq))
 		goto err_destroy;
