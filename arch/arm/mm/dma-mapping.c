MotorolaMobilityLLC/kernel-msm

arm: mm: dma-mapping: export arch_setup_dma_ops
Export the arch_setup_dma_ops symbol that can be used by the dynamically
lodable kernel module.

Change-Id: Icf84c7653743748060d39d08e91175fb4dad3f93
Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>

arch/arm/mm/dma-mapping.c
@@ -2672,6 +2672,7 @@ void arch_setup_dma_ops(struct device *dev, u64 dma_base, u64 size,
 #endif
 	dev->archdata.dma_ops_setup = true;
 }
+EXPORT_SYMBOL(arch_setup_dma_ops);
 
 void arch_teardown_dma_ops(struct device *dev)
 {

torvalds/linux

Signed-off-by: Roja Rani Yarubandi <quic_ryaruban@quicinc.com>

arch/arm/mm/dma-mapping.c
@@ -1792,6 +1792,7 @@ void arch_setup_dma_ops(struct device *dev, u64 dma_base, u64 size,
 	xen_setup_dma_ops(dev);
 	dev->archdata.dma_ops_setup = true;
 }
+EXPORT_SYMBOL_GPL(arch_setup_dma_ops);
 
 void arch_teardown_dma_ops(struct device *dev)
 {