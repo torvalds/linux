2ab9c93d6104e36110754d0778103f7f8719d969
John Hubbard <jhubbard@nvidia.com>
2024-07-04 00:24:28 -0700

    selftests/x86: build sysret_rip.c with clang
    When building with clang, via:
    
        make LLVM=1 -C tools/testing/selftests
    
    ...the build fails because clang's inline asm doesn't support all of the
    features that are used in the asm() snippet in sysret_rip.c.
    
    Fix this by moving the asm code into the clang_helpers_64.S file, where
    it can be built with the assembler's full set of features.
    
    Acked-by: Muhammad Usama Anjum <usama.anjum@collabora.com>
    Signed-off-by: John Hubbard <jhubbard@nvidia.com>
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>

--- tools/testing/selftests/x86/sysret_rip.c
+++ tools/testing/selftests/x86/sysret_rip.c
@@ -22,21 +22,13 @@
 #include <sys/mman.h>
 #include <assert.h>
 
-
-asm (
-	".pushsection \".text\", \"ax\"\n\t"
-	".balign 4096\n\t"
-	"test_page: .globl test_page\n\t"
-	".fill 4094,1,0xcc\n\t"
-	"test_syscall_insn:\n\t"
-	"syscall\n\t"
-	".ifne . - test_page - 4096\n\t"
-	".error \"test page is not one page long\"\n\t"
-	".endif\n\t"
-	".popsection"
-    );
-
+/*
+ * These items are in clang_helpers_64.S, in order to avoid clang inline asm
+ * limitations:
+ */
+void test_syscall_ins(void);
 extern const char test_page[];
+
 static void const *current_test_page_addr = test_page;
 
 static void sethandler(int sig, void (*handler)(int, siginfo_t *, void *),
