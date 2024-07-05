0e7b7bde460304f44e8c6b212c3195ac2f69f6fe
Nícolas F. R. A. Prado <nfraprado@collabora.com>
2024-07-05 19:29:55 -0400

    kselftest: Move ksft helper module to common directory
    Move the ksft python module, which provides generic helpers for
    kselftests, to a common directory so it can be more easily shared
    between different tests.
    
    Signed-off-by: Nícolas F. R. A. Prado <nfraprado@collabora.com>
    Acked-by: Shuah Khan <skhan@linuxfoundation.org>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://lore.kernel.org/r/20240705-dev-err-log-selftest-v2-2-163b9cd7b3c1@collabora.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- tools/testing/selftests/devices/probe/test_discoverable_devices.py
+++ tools/testing/selftests/devices/probe/test_discoverable_devices.py
@@ -16,11 +16,16 @@
 
 import argparse
 import glob
-import ksft
 import os
 import re
 import sys
 import yaml
+
+# Allow ksft module to be imported from different directory
+this_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(this_dir, "../../kselftest/"))
+
+import ksft
 
 pci_controllers = []
 usb_controllers = []
