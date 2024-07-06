b00f427a07a3ccc56ed2b1140b47474ae948efd5
Wolfram Sang <wsa+renesas@sang-engineering.com>
2024-07-06 13:20:50 +0200

    i2c: tegra-bpmp: reword according to newest specification
    Change the wording of this driver wrt. the newest I2C v7 and SMBus 3.2
    specifications and replace "master/slave" with more appropriate terms.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Reviewed-by: Andi Shyti <andi.shyti@kernel.org>
    Signed-off-by: Andi Shyti <andi.shyti@kernel.org>

--- drivers/i2c/busses/i2c-tegra-bpmp.c
+++ drivers/i2c/busses/i2c-tegra-bpmp.c
@@ -275,8 +275,8 @@ static u32 tegra_bpmp_i2c_func(struct i2c_adapter *adapter)
 }
 
 static const struct i2c_algorithm tegra_bpmp_i2c_algo = {
-	.master_xfer = tegra_bpmp_i2c_xfer,
-	.master_xfer_atomic = tegra_bpmp_i2c_xfer_atomic,
+	.xfer = tegra_bpmp_i2c_xfer,
+	.xfer_atomic = tegra_bpmp_i2c_xfer_atomic,
 	.functionality = tegra_bpmp_i2c_func,
 };
 
