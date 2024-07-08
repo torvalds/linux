992648f5a61f1c9fab800fbc42dea31727cd8a55
Jiri Pirko <jiri@nvidia.com>
2024-07-08 09:48:02 +0200

    virtio: call virtio_find_vqs_info() from virtio_find_single_vq() directly
    Since there are no more implementations of find_vqs() op, call
    virtio_find_vqs_info() from virtio_find_single_vq() directly.
    
    Signed-off-by: Jiri Pirko <jiri@nvidia.com>
    Message-Id: <20240708074814.1739223-8-jiri@resnulli.us>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- include/linux/virtio_config.h
+++ include/linux/virtio_config.h
@@ -283,10 +283,11 @@ static inline
 struct virtqueue *virtio_find_single_vq(struct virtio_device *vdev,
 					vq_callback_t *c, const char *n)
 {
-	vq_callback_t *callbacks[] = { c };
-	const char *names[] = { n };
+	struct virtqueue_info vqs_info[] = {
+		{ n, c },
+	};
 	struct virtqueue *vq;
-	int err = virtio_find_vqs(vdev, 1, &vq, callbacks, names, NULL);
+	int err = virtio_find_vqs_info(vdev, 1, &vq, vqs_info, NULL);
 
 	if (err < 0)
 		return ERR_PTR(err);
