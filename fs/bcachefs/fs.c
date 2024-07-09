dc003efbc7769f22919e1d7d924bf8fc4d2ff841
Kent Overstreet <kent.overstreet@linux.dev>
2024-07-09 09:11:34 +0800

    bcachefs: Add support for FS_IOC_GETFSSYSFSPATH
    [TEST]:
    ```
    $ cat ioctl_getsysfspath.c
     #include <stdio.h>
     #include <stdlib.h>
     #include <fcntl.h>
     #include <sys/ioctl.h>
     #include <linux/fs.h>
     #include <unistd.h>
    
     int main(int argc, char *argv[]) {
         int fd;
         struct fs_sysfs_path sysfs_path = {};
    
         if (argc != 2) {
             fprintf(stderr, "Usage: %s <path_to_file_or_directory>\n", argv[0]);
             exit(EXIT_FAILURE);
         }
    
         fd = open(argv[1], O_RDONLY);
         if (fd == -1) {
             perror("open");
             exit(EXIT_FAILURE);
         }
    
         if (ioctl(fd, FS_IOC_GETFSSYSFSPATH, &sysfs_path) == -1) {
             perror("ioctl FS_IOC_GETFSSYSFSPATH");
             close(fd);
             exit(EXIT_FAILURE);
         }
    
         printf("FS_IOC_GETFSSYSFSPATH: %s\n", sysfs_path.name);
         close(fd);
         return 0;
     }
    
    $ gcc ioctl_getsysfspath.c
    $ sudo bcachefs format /dev/sda
    $ sudo mount.bcachefs /dev/sda /mnt
    $ sudo ./a.out /mnt
      FS_IOC_GETFSSYSFSPATH: bcachefs/c380b4ab-fbb6-41d2-b805-7a89cae9cadb
    ```
    
    Original patch link:
    [1]: https://lore.kernel.org/all/20240207025624.1019754-8-kent.overstreet@linux.dev/
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>
    Signed-off-by: Youling Tang <youling.tang@linux.dev>
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/fs.c
+++ fs/bcachefs/fs.c
@@ -2217,6 +2217,7 @@ got_sb:
 	sb->s_time_min		= div_s64(S64_MIN, c->sb.time_units_per_sec) + 1;
 	sb->s_time_max		= div_s64(S64_MAX, c->sb.time_units_per_sec);
 	super_set_uuid(sb, c->sb.user_uuid.b, sizeof(c->sb.user_uuid));
+	super_set_sysfs_name_uuid(sb);
 	sb->s_shrink->seeks	= 0;
 	c->vfs_sb		= sb;
 	strscpy(sb->s_id, c->name, sizeof(sb->s_id));
