41bb96296f9d3a92f3b52bcf502d6c0e86fa84a0
Kees Cook <kees@kernel.org>
2024-07-11 08:56:35 -0700

    scsi: mpi3mr: struct mpi3_sas_io_unit_page0: Replace 1-element array with flexible array
    Replace the deprecated[1] use of a 1-element array in struct
    mpi3_sas_io_unit_page0 with a modern flexible array.
    
    No binary differences are present after this conversion.
    
    Link: https://github.com/KSPP/linux/issues/79 [1]
    Signed-off-by: Kees Cook <kees@kernel.org>
    Link: https://lore.kernel.org/r/20240711155637.3757036-3-kees@kernel.org
    Reviewed-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/mpi3mr/mpi/mpi30_cnfg.h
+++ drivers/scsi/mpi3mr/mpi/mpi30_cnfg.h
@@ -1565,16 +1565,13 @@ struct mpi3_sas_io_unit0_phy_data {
 	__le32             reserved10;
 };
 
-#ifndef MPI3_SAS_IO_UNIT0_PHY_MAX
-#define MPI3_SAS_IO_UNIT0_PHY_MAX           (1)
-#endif
 struct mpi3_sas_io_unit_page0 {
 	struct mpi3_config_page_header         header;
 	__le32                             reserved08;
 	u8                                 num_phys;
 	u8                                 init_status;
 	__le16                             reserved0e;
-	struct mpi3_sas_io_unit0_phy_data      phy_data[MPI3_SAS_IO_UNIT0_PHY_MAX];
+	struct mpi3_sas_io_unit0_phy_data      phy_data[];
 };
 
 #define MPI3_SASIOUNIT0_PAGEVERSION                          (0x00)
