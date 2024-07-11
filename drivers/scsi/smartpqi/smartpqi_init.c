5b4ded3f35d5b2c077e4035b66af2d04668d7ee5
Don Brace <don.brace@microchip.com>
2024-07-11 14:47:04 -0500

    scsi: smartpqi: Update driver version to 2.1.28-025
    Update driver version to 2.1.28-025.
    
    Reviewed-by: Mike Tran <mike.tran@microchip.com>
    Reviewed-by: Gerry Morong <gerry.morong@microchip.com>
    Reviewed-by: Scott Benesh <scott.benesh@microchip.com>
    Reviewed-by: Scott Teel <scott.teel@microchip.com>
    Signed-off-by: Don Brace <don.brace@microchip.com>
    Link: https://lore.kernel.org/r/20240711194704.982400-6-don.brace@microchip.com
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/smartpqi/smartpqi_init.c
+++ drivers/scsi/smartpqi/smartpqi_init.c
@@ -33,11 +33,11 @@
 #define BUILD_TIMESTAMP
 #endif
 
-#define DRIVER_VERSION		"2.1.26-030"
+#define DRIVER_VERSION		"2.1.28-025"
 #define DRIVER_MAJOR		2
 #define DRIVER_MINOR		1
-#define DRIVER_RELEASE		26
-#define DRIVER_REVISION		30
+#define DRIVER_RELEASE		28
+#define DRIVER_REVISION		25
 
 #define DRIVER_NAME		"Microchip SmartPQI Driver (v" \
 				DRIVER_VERSION BUILD_TIMESTAMP ")"
