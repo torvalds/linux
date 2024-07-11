d06a26d24db090d8be0ea4c9bfa1457e334940b7
Kent Overstreet <kent.overstreet@linux.dev>
2024-07-11 16:11:45 -0400

    bcachefs: sysfs trigger_freelist_wakeup
    another debugging knob
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/sysfs.c
+++ fs/bcachefs/sysfs.c
@@ -143,6 +143,7 @@ write_attribute(trigger_journal_flush);
 write_attribute(trigger_journal_writes);
 write_attribute(trigger_btree_cache_shrink);
 write_attribute(trigger_btree_key_cache_shrink);
+write_attribute(trigger_freelist_wakeup);
 rw_attribute(gc_gens_pos);
 
 read_attribute(uuid);
@@ -501,6 +502,9 @@ STORE(bch2_fs)
 	if (attr == &sysfs_trigger_journal_writes)
 		bch2_journal_do_writes(&c->journal);
 
+	if (attr == &sysfs_trigger_freelist_wakeup)
+		closure_wake_up(&c->freelist_wait);
+
 #ifdef CONFIG_BCACHEFS_TESTS
 	if (attr == &sysfs_perf_test) {
 		char *tmp = kstrdup(buf, GFP_KERNEL), *p = tmp;
@@ -622,6 +626,7 @@ struct attribute *bch2_fs_internal_files[] = {
 	&sysfs_trigger_journal_writes,
 	&sysfs_trigger_btree_cache_shrink,
 	&sysfs_trigger_btree_key_cache_shrink,
+	&sysfs_trigger_freelist_wakeup,
 
 	&sysfs_gc_gens_pos,
 
