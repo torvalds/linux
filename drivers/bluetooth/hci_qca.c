84f9288208dfcd955446060a53add15b9e01af4e
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2024-07-10 22:30:57 -0400

    Bluetooth: hci_qca: Fix build error
    This fixes the following build error introduced by b80f4e3b8cb8
    ("Bluetooth: hci_qca: schedule a devm action for disabling the clock"):
    
    drivers/bluetooth/hci_qca.c: In function ‘qca_serdev_remove’:
    drivers/bluetooth/hci_qca.c:2501:2: error: label at end of compound statement
     2501 |  default:
          |  ^~~~~~~
    
    Fixes: b80f4e3b8cb8 ("Bluetooth: hci_qca: schedule a devm action for disabling the clock")
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- drivers/bluetooth/hci_qca.c
+++ drivers/bluetooth/hci_qca.c
@@ -2499,6 +2499,7 @@ static void qca_serdev_remove(struct serdev_device *serdev)
 			qca_power_shutdown(&qcadev->serdev_hu);
 		break;
 	default:
+		break;
 	}
 
 	hci_uart_unregister_device(&qcadev->serdev_hu);
