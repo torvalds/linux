0e11f97bfddc608fd31d4fb7b8feaf74755d6372
Kees Cook <kees@kernel.org>
2024-07-11 08:56:34 -0700

    scsi: mpi3mr: struct mpi3_event_data_pcie_topology_change_list: Replace 1-element array with flexible array
    Replace the deprecated[1] use of a 1-element array in struct
    mpi3_event_data_pcie_topology_change_list with a modern flexible array.
    
    Additionally add __counted_by annotation since port_entry is only ever
    accessed in loops controlled by num_entries. For example:
    
            for (i = 0; i < event_data->num_entries; i++) {
                    handle =
                        le16_to_cpu(event_data->port_entry[i].attached_dev_handle);
    
    No binary differences are present after this conversion.
    
    Link: https://github.com/KSPP/linux/issues/79 [1]
    Signed-off-by: Kees Cook <kees@kernel.org>
    Link: https://lore.kernel.org/r/20240711155637.3757036-2-kees@kernel.org
    Reviewed-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/mpi3mr/mpi/mpi30_ioc.h
+++ drivers/scsi/mpi3mr/mpi/mpi30_ioc.h
@@ -542,9 +542,6 @@ struct mpi3_event_data_pcie_enumeration {
 #define MPI3_EVENT_PCIE_ENUM_ES_MAX_SWITCHES_EXCEED         (0x40000000)
 #define MPI3_EVENT_PCIE_ENUM_ES_MAX_DEVICES_EXCEED          (0x20000000)
 #define MPI3_EVENT_PCIE_ENUM_ES_RESOURCES_EXHAUSTED         (0x10000000)
-#ifndef MPI3_EVENT_PCIE_TOPO_PORT_COUNT
-#define MPI3_EVENT_PCIE_TOPO_PORT_COUNT         (1)
-#endif
 struct mpi3_event_pcie_topo_port_entry {
 	__le16             attached_dev_handle;
 	u8                 port_status;
@@ -585,7 +582,7 @@ struct mpi3_event_data_pcie_topology_change_list {
 	u8                                     switch_status;
 	u8                                     io_unit_port;
 	__le32                                 reserved0c;
-	struct mpi3_event_pcie_topo_port_entry     port_entry[MPI3_EVENT_PCIE_TOPO_PORT_COUNT];
+	struct mpi3_event_pcie_topo_port_entry     port_entry[] __counted_by(num_entries);
 };
 
 #define MPI3_EVENT_PCIE_TOPO_SS_NO_PCIE_SWITCH          (0x00)
