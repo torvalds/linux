274aff8711b2e77c27bbda0ddc24caa39f154bfa
Stephen Boyd <sboyd@kernel.org>
2024-07-18 14:05:07 -0700

    clk: Add KUnit tests for clks registered with struct clk_parent_data
    Test that clks registered with 'struct clk_parent_data' work as
    intended and can find their parents.
    
    Cc: Christian Marangi <ansuelsmth@gmail.com>
    Cc: Brendan Higgins <brendan.higgins@linux.dev>
    Reviewed-by: David Gow <davidgow@google.com>
    Cc: Rae Moar <rmoar@google.com>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>
    Link: https://lore.kernel.org/r/20240718210513.3801024-9-sboyd@kernel.org

---
+++ drivers/clk/clk_parent_data_test.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _CLK_PARENT_DATA_TEST_H
+#define _CLK_PARENT_DATA_TEST_H
+
+#define CLK_PARENT_DATA_1MHZ_NAME	"1mhz_fixed_legacy"
+#define CLK_PARENT_DATA_PARENT1		"parent_fwname"
+#define CLK_PARENT_DATA_PARENT2		"50"
+#define CLK_PARENT_DATA_50MHZ_NAME	"50_clk"
+
+#endif
