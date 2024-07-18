e173211a6890755a190365d28c9c399c96480da1
Fei Yang <quic_feiyan@quicinc.com>
2024-07-18 17:20:42 +0800

    soc: qcom: minidump: Use queue work to avoid scheduling while atomic
    Use queue work to wait backend respond to avoid kernel bug or warning
    that scheduling while atomic or RCU read-side critical section
    for VirtIO minidump.
    
    Change-Id: I5c705a8cbcd8e14f0d5fec3149883969bbf05228
    Signed-off-by: Fei Yang <quic_feiyan@quicinc.com>

--- drivers/soc/qcom/minidump_virtio.c
+++ drivers/soc/qcom/minidump_virtio.c
@@ -47,13 +47,20 @@ struct virtio_minidump_msg {
 	__virtio32 result;
 };
 
+struct md_request {
+	struct md_region	entry;
+	struct work_struct	work;
+	enum minidump_entry_cmd	minidump_cmd;
+};
+
 /* Protect elfheader from deferred calls contention */
 static DEFINE_RWLOCK(mdt_remove_lock);
+static struct workqueue_struct *minidump_wq;
 
 /* Set as globle variable */
 static struct virtio_minidump *vmd;
 
-static int virtio_add_region(const struct md_region *entry)
+static void md_virtio_add_work(struct md_region *entry)
 {
 	struct virtio_minidump_msg *req, *rsp;
 	struct scatterlist sg[1];
@@ -62,12 +69,14 @@ static int virtio_add_region(const struct md_region *entry)
 
 	if (!pfn_is_map_memory(virt_to_pfn(entry->virt_addr))) {
 		pr_err("%s: Invalid Phy address %llu\n", entry->name, entry->phys_addr);
-		return -EINVAL;
+		return;
 	}
 
 	req = kzalloc(sizeof(struct virtio_minidump_msg), GFP_KERNEL);
-	if (!req)
-		return -ENOMEM;
+	if (!req) {
+		pr_err("%s: Alloc message memory fail\n", entry->name);
+		return;
+	}
 
 	strscpy(req->name, entry->name, sizeof(req->name));
 	req->phy_addr = cpu_to_virtio64(vmd->vdev, entry->phys_addr);
@@ -101,10 +110,9 @@ out:
 
 	pr_debug("%s: %s return %d\n", __func__, entry->name, ret);
 
-	return ret;
 }
 
-static int virtio_remove_region(const struct md_region *entry)
+static void md_virtio_remove_work(struct md_region *entry)
 {
 	struct virtio_minidump_msg *req, *rsp;
 	struct scatterlist sg[1];
@@ -112,8 +120,10 @@ static int virtio_remove_region(const struct md_region *entry)
 	int ret = 0;
 
 	req = kzalloc(sizeof(struct virtio_minidump_msg), GFP_KERNEL);
-	if (!req)
-		return -ENOMEM;
+	if (!req) {
+		pr_err("%s: Alloc message memory fail\n", entry->name);
+		return;
+	}
 
 	strscpy(req->name, entry->name, sizeof(req->name));
 	req->type = cpu_to_virtio32(vmd->vdev, MD_SS_REMOVE_REGION);
@@ -144,11 +154,9 @@ out:
 	kfree(req);
 
 	pr_debug("%s: %s return %d\n", __func__, entry->name, ret);
-
-	return ret;
 }
 
-static int virtio_update_region(const struct md_region *entry)
+static void md_virtio_update_work(struct md_region *entry)
 {
 	struct virtio_minidump_msg *req, *rsp;
 	struct scatterlist sg[1];
@@ -156,8 +164,10 @@ static int virtio_update_region(const struct md_region *entry)
 	int ret = 0;
 
 	req = kzalloc(sizeof(struct virtio_minidump_msg), GFP_KERNEL);
-	if (!req)
-		return -ENOMEM;
+	if (!req) {
+		pr_err("%s: Alloc message memory fail\n", entry->name);
+		return;
+	}
 
 	strscpy(req->name, entry->name, sizeof(req->name));
 	req->phy_addr = cpu_to_virtio64(vmd->vdev, entry->phys_addr);
@@ -190,8 +200,79 @@ out:
 	kfree(req);
 
 	pr_debug("%s: %s return %d\n", __func__, entry->name, ret);
+}
 
-	return ret;
+static void minidump_work(struct work_struct *work)
+{
+	struct md_request *vm_work =
+		container_of(work, struct md_request, work);
+	enum minidump_entry_cmd cmd;
+
+	cmd = vm_work->minidump_cmd;
+
+	switch (cmd) {
+	case MINIDUMP_UPDATE:
+		md_virtio_update_work(&vm_work->entry);
+		break;
+	case MINIDUMP_ADD:
+		md_virtio_add_work(&vm_work->entry);
+		break;
+	case MINIDUMP_REMOVE:
+		md_virtio_remove_work(&vm_work->entry);
+		break;
+	default:
+		pr_debug("No command for virtio minidump work\n");
+		break;
+	}
+	kfree(vm_work);
+}
+
+static int virtio_add_region(const struct md_region *entry)
+{
+	struct md_request *vm_work;
+
+	/* alloc an entry for workqueue, need free in work */
+	vm_work = kzalloc(sizeof(*vm_work), GFP_ATOMIC);
+	if (!vm_work)
+		return -ENOMEM;
+	vm_work->entry = *entry;
+	vm_work->minidump_cmd = MINIDUMP_ADD;
+	INIT_WORK(&vm_work->work, minidump_work);
+	queue_work(minidump_wq, &vm_work->work);
+
+	return 0;
+}
+
+static int virtio_remove_region(const struct md_region *entry)
+{
+	struct md_request *vm_work;
+
+	/* alloc an entry for workqueue, need free in work */
+	vm_work = kzalloc(sizeof(*vm_work), GFP_ATOMIC);
+	if (!vm_work)
+		return -ENOMEM;
+	vm_work->entry = *entry;
+	vm_work->minidump_cmd = MINIDUMP_REMOVE;
+	INIT_WORK(&vm_work->work, minidump_work);
+	queue_work(minidump_wq, &vm_work->work);
+
+	return 0;
+}
+
+static int virtio_update_region(const struct md_region *entry)
+{
+	struct md_request *vm_work;
+
+	/* alloc an entry for workqueue, need free in work */
+	vm_work = kzalloc(sizeof(*vm_work), GFP_ATOMIC);
+	if (!vm_work)
+		return -ENOMEM;
+	vm_work->entry = *entry;
+	vm_work->minidump_cmd = MINIDUMP_UPDATE;
+	INIT_WORK(&vm_work->work, minidump_work);
+	queue_work(minidump_wq, &vm_work->work);
+
+	return 0;
 }
 
 static int virtio_get_available_region(void)
@@ -211,6 +292,12 @@ static int md_virtio_init_md_table(void)
 	}
 
 	pr_debug("Get available region count: %d\n", res);
+
+	minidump_wq = create_singlethread_workqueue("minidump_wq");
+	if (!minidump_wq) {
+		pr_err("Unable to initialize workqueue\n");
+		return -EINVAL;
+	}
 
 	return 0;
 }
@@ -290,6 +377,7 @@ static int md_virtio_add_region(const struct md_region *entry)
 
 	spin_lock_irqsave(&mdt_lock, flags);
 	md_add_elf_header(entry);
+	ret = md_num_regions;
 	md_num_regions++;
 	spin_unlock_irqrestore(&mdt_lock, flags);
 
