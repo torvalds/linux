5a6a25ea5bcd5bdf80fb13acd65a03fc6b8794b1
Dan Carpenter <dan.carpenter@linaro.org>
2024-07-17 21:25:53 -0500

    clk: sophgo: clk-sg2042-pll: Fix uninitialized variable in debug output
    If sg2042_get_pll_ctl_setting() fails then "value" isn't initialized and
    it is printed in the debug output.  Initialize it to zero.
    
    Fixes: 48cf7e01386e ("clk: sophgo: Add SG2042 clock driver")
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>
    Link: https://lore.kernel.org/r/baf0a490-d5ba-4528-90ba-80399684692d@stanley.mountain
    Reviewed-by: Chen Wang <unicorn_wang@outlook.com>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/sophgo/clk-sg2042-pll.c
+++ drivers/clk/sophgo/clk-sg2042-pll.c
@@ -387,7 +387,7 @@ static int sg2042_clk_pll_set_rate(struct clk_hw *hw,
 	struct sg2042_pll_clock *pll = to_sg2042_pll_clk(hw);
 	struct sg2042_pll_ctrl pctrl_table;
 	unsigned long flags;
-	u32 value;
+	u32 value = 0;
 	int ret;
 
 	spin_lock_irqsave(pll->lock, flags);
