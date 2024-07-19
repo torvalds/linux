d4f1531d4262ff309920ea82fcec2d19fc8cb176
Peng Fan <peng.fan@nxp.com>
2024-07-19 16:36:13 +0800

    remoteproc: imx_rproc: Initialize workqueue earlier
    [ Upstream commit 858e57c1d3dd7b92cc0fa692ba130a0a5d57e49d ]
    
    Initialize workqueue before requesting mailbox channel, otherwise if
    mailbox interrupt comes before workqueue ready, the imx_rproc_rx_callback
    will trigger issue.
    
    Fixes: 2df7062002d0 ("remoteproc: imx_proc: enable virtio/mailbox")
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Daniel Baluta <daniel.baluta@nxp.com>
    Link: https://lore.kernel.org/r/20240719-imx_rproc-v2-3-10d0268c7eb1@nxp.com
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/remoteproc/imx_rproc.c
+++ drivers/remoteproc/imx_rproc.c
@@ -796,6 +796,8 @@ static int imx_rproc_probe(struct platform_device *pdev)
 		goto err_put_rproc;
 	}
 
+	INIT_WORK(&priv->rproc_work, imx_rproc_vq_work);
+
 	ret = imx_rproc_xtr_mbox_init(rproc);
 	if (ret)
 		goto err_put_wkq;
@@ -813,8 +815,6 @@ static int imx_rproc_probe(struct platform_device *pdev)
 	ret = imx_rproc_clk_enable(priv);
 	if (ret)
 		goto err_put_mbox;
-
-	INIT_WORK(&priv->rproc_work, imx_rproc_vq_work);
 
 	if (rproc->state != RPROC_DETACHED)
 		rproc->auto_boot = of_property_read_bool(np, "fsl,auto-boot");
