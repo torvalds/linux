e05d4cd9b895c503dcf19c0ed9ebb8d393b220ec
Xi Ruoyao <xry111@xry111.site>
2024-07-20 22:41:07 +0800

    LoongArch: Add support for relocating the kernel with RELR relocation
    RELR as a relocation packing format for relative relocations for
    reducing the size of relative relocation records.  In a position
    independent executable there are often many relative relocation
    records, and our vmlinux is a PIE.
    
    The LLD linker (since 17.0.0) and the BFD linker (since 2.43) supports
    packing the relocations in the RELR format for LoongArch, with the flag
    -z pack-relative-relocs.
    
    Commits 5cf896fb6be3eff ("arm64: Add support for relocating the kernel
    with RELR relocations") and ccb2d173b983984bfa ("Makefile: use -z
    pack-relative-relocs") have already added the framework to use RELR.
    We just need to wire it up and process the RELR relocation records in
    relocate_relative() in addition to the RELA relocation records.
    
    A ".p2align 3" directive is added to la_abs macro or the BFD linker
    cannot pack the relocation records against the .la_abs section (the
    ". = ALIGN(8);" directive in vmlinux.lds.S is too late in the linking
    process).
    
    With defconfig and CONFIG_RELR vmlinux.efi is 2.1 MiB (6%) smaller, and
    vmlinuz.efi (using gzip compression) is 384 KiB (2.8%) smaller.
    
    Link: https://groups.google.com/d/topic/generic-abi/bX460iggiKg
    Link: https://reviews.llvm.org/D138135#4531389
    Link: https://sourceware.org/git/?p=binutils-gdb.git;a=commit;h=d89ecf33ab6d
    Signed-off-by: Xi Ruoyao <xry111@xry111.site>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- arch/loongarch/kernel/relocate.c
+++ arch/loongarch/kernel/relocate.c
@@ -38,6 +38,24 @@ static inline void __init relocate_relative(void)
 		relocated_addr = (Elf64_Addr)RELOCATED(relocated_addr);
 		*(Elf64_Addr *)RELOCATED(addr) = relocated_addr;
 	}
+
+#ifdef CONFIG_RELR
+	u64 *addr = NULL;
+	u64 *relr = (u64 *)&__relr_dyn_begin;
+	u64 *relr_end = (u64 *)&__relr_dyn_end;
+
+	for ( ; relr < relr_end; relr++) {
+		if ((*relr & 1) == 0) {
+			addr = (u64 *)(*relr + reloc_offset);
+			*addr++ += reloc_offset;
+		} else {
+			for (u64 *p = addr, r = *relr >> 1; r; p++, r >>= 1)
+				if (r & 1)
+					*p += reloc_offset;
+			addr += 63;
+		}
+	}
+#endif
 }
 
 static inline void __init relocate_absolute(long random_offset)
