4009c95fede6b783802ad01f264a7a0541f5ea60
Martin KaFai Lau <martin.lau@kernel.org>
2024-07-22 11:30:47 -0700

    selftests/bpf: Ensure the unsupported struct_ops prog cannot be loaded
    There is an existing "bpf_tcp_ca/unsupp_cong_op" test to ensure
    the unsupported tcp-cc "get_info" struct_ops prog cannot be loaded.
    
    This patch adds a new test in the bpf_testmod such that the
    unsupported ops test does not depend on other kernel subsystem
    where its supporting ops may be changed in the future.
    
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    Link: https://lore.kernel.org/r/20240722183049.2254692-4-martin.lau@linux.dev
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>

--- tools/testing/selftests/bpf/bpf_testmod/bpf_testmod.h
+++ tools/testing/selftests/bpf/bpf_testmod/bpf_testmod.h
@@ -35,6 +35,7 @@ struct bpf_testmod_ops {
 	void (*test_2)(int a, int b);
 	/* Used to test nullable arguments. */
 	int (*test_maybe_null)(int dummy, struct task_struct *task);
+	int (*unsupported_ops)(void);
 
 	/* The following fields are used to test shadow copies. */
 	char onebyte;
