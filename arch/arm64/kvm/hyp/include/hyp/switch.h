7e814a20f6da2bd2044b1a4682dd92a6f0df5a92
Fuad Tabba <tabba@google.com>
2024-07-22 17:33:11 +0100

    KVM: arm64: Tidying up PAuth code in KVM
    Tidy up some of the PAuth trapping code to clear up some comments
    and avoid clang/checkpatch warnings. Also, don't bother setting
    PAuth HCR_EL2 bits in pKVM, since it's handled by the hypervisor.
    
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Link: https://lore.kernel.org/r/20240722163311.1493879-1-tabba@google.com
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>

--- arch/arm64/kvm/hyp/include/hyp/switch.h
+++ arch/arm64/kvm/hyp/include/hyp/switch.h
@@ -27,7 +27,6 @@
 #include <asm/kvm_hyp.h>
 #include <asm/kvm_mmu.h>
 #include <asm/kvm_nested.h>
-#include <asm/kvm_ptrauth.h>
 #include <asm/fpsimd.h>
 #include <asm/debug-monitors.h>
 #include <asm/processor.h>
