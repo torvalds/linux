01cd1f2ce6662aaa9b734fe9012e4740aaae8505
Bao D. Nguyen <quic_nguyenb@quicinc.com>
2024-07-23 20:49:32 -0700

    FROMGIT: scsi: ufs: core: Support Updating UIC Command Timeout
    The default UIC command timeout still remains 500ms.
    Allow platform drivers to override the UIC command
    timeout if desired.
    
    In a real product, the 500ms timeout value is probably good enough.
    However, during the product development where there are a lot of
    logging and debug messages being printed to the uart console,
    interrupt starvations happen occasionally because the uart may
    print long debug messages from different modules in the system.
    While printing, the uart may have interrupts disabled for more
    than 500ms, causing UIC command timeout.
    The UIC command timeout would trigger more printing from
    the UFS driver, and eventually a watchdog timeout may
    occur unnecessarily.
    
    Add support for overriding the UIC command timeout value
    with the newly created uic_cmd_timeout kernel module parameter.
    Default value is 500ms. Supported values range from 500ms
    to 2 seconds.
    
    Signed-off-by: Bao D. Nguyen <quic_nguyenb@quicinc.com>
    Link: https://lore.kernel.org/r/e4e1c87f3f867f270a3d4b5d57a00139ff0e9741.1721792309.git.quic_nguyenb@quicinc.com
    Suggested-by: Bart Van Assche <bvanassche@acm.org>
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Reviewed-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Reviewed-by: Peter Wang <peter.wang@mediatek.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    Bug: 358022094
    Bug: 361419350
    Bug: 374645970
    Change-Id: I3778a26aa82805591454f3771f727d30f92286d9
    (cherry picked from commit 88e6804fb323d1dbe477f002a89755f8b034e890
     https://git.kernel.org/pub/scm/linux/kernel/git/mkp/scsi.git 6.12/scsi-staging)
    Signed-off-by: Ziqi Chen <quic_ziqichen@quicinc.com>
    (cherry picked from commit 0c95462c9112873df5ceeeaff13cbfcf05136480)
    Signed-off-by: Blagovest Kolenichev <quic_c_bkolen@quicinc.com>
    (cherry picked from commit 5414728d5721e0ad3921e45083ebf8cff8d757ac)
    Signed-off-by: Shreyas K K <quic_shrekk@quicinc.com>
    Signed-off-by: Srinivasarao Pathipati <quic_c_spathi@quicinc.com>

--- drivers/ufs/core/ufshcd.c
+++ drivers/ufs/core/ufshcd.c
@@ -54,8 +54,10 @@
 
 
 /* UIC command timeout, unit: ms */
-#define UIC_CMD_TIMEOUT	500
-
+enum {
+	UIC_CMD_TIMEOUT_DEFAULT	= 500,
+	UIC_CMD_TIMEOUT_MAX	= 2000,
+};
 /* NOP OUT retries waiting for NOP IN response */
 #define NOP_OUT_RETRIES    10
 /* Timeout after 50 msecs if NOP OUT hangs without response */
@@ -128,6 +130,23 @@ static const struct kernel_param_ops mcq_mode_ops = {
 
 module_param_cb(use_mcq_mode, &mcq_mode_ops, &use_mcq_mode, 0644);
 MODULE_PARM_DESC(use_mcq_mode, "Control MCQ mode for controllers starting from UFSHCI 4.0. 1 - enable MCQ, 0 - disable MCQ. MCQ is enabled by default");
+
+static unsigned int uic_cmd_timeout = UIC_CMD_TIMEOUT_DEFAULT;
+
+static int uic_cmd_timeout_set(const char *val, const struct kernel_param *kp)
+{
+	return param_set_uint_minmax(val, kp, UIC_CMD_TIMEOUT_DEFAULT,
+				     UIC_CMD_TIMEOUT_MAX);
+}
+
+static const struct kernel_param_ops uic_cmd_timeout_ops = {
+	.set = uic_cmd_timeout_set,
+	.get = param_get_uint,
+};
+
+module_param_cb(uic_cmd_timeout, &uic_cmd_timeout_ops, &uic_cmd_timeout, 0644);
+MODULE_PARM_DESC(uic_cmd_timeout,
+		 "UFS UIC command timeout in milliseconds. Defaults to 500ms. Supported values range from 500ms to 2 seconds inclusively");
 
 #define ufshcd_toggle_vreg(_dev, _vreg, _on)				\
 	({                                                              \
@@ -2380,7 +2399,7 @@ static inline bool ufshcd_ready_for_uic_cmd(struct ufs_hba *hba)
 {
 	u32 val;
 	int ret = read_poll_timeout(ufshcd_readl, val, val & UIC_COMMAND_READY,
-				    500, UIC_CMD_TIMEOUT * 1000, false, hba,
+				    500, uic_cmd_timeout * 1000, false, hba,
 				    REG_CONTROLLER_STATUS);
 	return ret == 0 ? true : false;
 }
@@ -2439,7 +2458,7 @@ ufshcd_wait_for_uic_cmd(struct ufs_hba *hba, struct uic_command *uic_cmd)
 	lockdep_assert_held(&hba->uic_cmd_mutex);
 
 	if (wait_for_completion_timeout(&uic_cmd->done,
-					msecs_to_jiffies(UIC_CMD_TIMEOUT))) {
+					msecs_to_jiffies(uic_cmd_timeout))) {
 		ret = uic_cmd->argument2 & MASK_UIC_COMMAND_RESULT;
 	} else {
 		ret = -ETIMEDOUT;
@@ -4252,7 +4271,7 @@ static int ufshcd_uic_pwr_ctrl(struct ufs_hba *hba, struct uic_command *cmd)
 	}
 
 	if (!wait_for_completion_timeout(hba->uic_async_done,
-					 msecs_to_jiffies(UIC_CMD_TIMEOUT))) {
+					 msecs_to_jiffies(uic_cmd_timeout))) {
 		dev_err(hba->dev,
 			"pwr ctrl cmd 0x%x with mode 0x%x completion timeout\n",
 			cmd->command, cmd->argument3);
