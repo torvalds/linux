728db2cc9ae7bab15a1e404c7496c2ccc63408c9
Kalpak Kawadkar <quic_kkawadka@quicinc.com>
2024-07-24 15:23:15 +0530

    bindings: clock: qcom: Add support of clock ids for SERAPH
    Add the clock handles for GCC/CAMCC/GPUCC/VIDEOCC/LSRCC/EVACC/TCSRCC,
    so that the clients can request on the clock ids for SERAPH platform.
    
    Change-Id: I87d32ada7cfe017c8f971e1df7fe255c84b5d86e
    Signed-off-by: Kalpak Kawadkar <quic_kkawadka@quicinc.com>

---
+++ include/dt-bindings/clock/qcom,lsrcc-seraph.h
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright (c) 2024, Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_LSR_CC_SERAPH_H
+#define _DT_BINDINGS_CLK_QCOM_LSR_CC_SERAPH_H
+
+/* LSR_CC clocks */
+#define LSR_CC_PLL0						0
+#define LSR_CC_PLL1						1
+#define LSR_CC_AHB_CLK						2
+#define LSR_CC_AHB_CLK_SRC					3
+#define LSR_CC_MVS0_CLK						4
+#define LSR_CC_MVS0_CLK_SRC					5
+#define LSR_CC_MVS0_FREERUN_CLK					6
+#define LSR_CC_MVS0_SHIFT_CLK					7
+#define LSR_CC_MVS0C_CLK					8
+#define LSR_CC_MVS0C_CLK_SRC					9
+#define LSR_CC_MVS0C_FREERUN_CLK				10
+#define LSR_CC_MVS0C_SHIFT_CLK					11
+#define LSR_CC_SLEEP_CLK					12
+#define LSR_CC_SLEEP_CLK_SRC					13
+#define LSR_CC_XO_CLK						14
+#define LSR_CC_XO_CLK_SRC					15
+
+/* LSR_CC resets */
+#define LSR_CC_INTERFACE_BCR					0
+#define LSR_CC_LSR_NOC_BCR					1
+#define LSR_CC_MVS0_BCR						2
+#define LSR_CC_MVS0C_BCR					3
+
+#endif
---
+++ include/dt-bindings/clock/qcom,tcsrcc-seraph.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright (c) 2024, Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_TCSR_CC_SERAPH_H
+#define _DT_BINDINGS_CLK_QCOM_TCSR_CC_SERAPH_H
+
+/* TCSR_CC clocks */
+#define TCSR_PCIE_0_CLKREF_EN				0
+#define TCSR_PCIE_1_CLKREF_EN				1
+#define TCSR_USB3_CLKREF_EN				2
+
+#endif
