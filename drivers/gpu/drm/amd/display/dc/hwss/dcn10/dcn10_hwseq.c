e8097cf1ce9e7ad8516ee95f06f7baaa31506035
Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>
2024-07-25 16:41:38 -0600

    drm/amd/display: Add missing program DET segment call to pipe init
    Add a callback that program the DET segment when initializing pipes.
    
    Acked-by: Tom Chung <chiahsuan.chung@amd.com>
    Signed-off-by: Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>
    Signed-off-by: Wayne Lin <wayne.lin@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit e1dbe625d6ac2821eb29e087db46cb539d8079f0)

--- drivers/gpu/drm/amd/display/dc/hwss/dcn10/dcn10_hwseq.c
+++ drivers/gpu/drm/amd/display/dc/hwss/dcn10/dcn10_hwseq.c
@@ -1402,6 +1402,8 @@ void dcn10_init_pipes(struct dc *dc, struct dc_state *context)
 		if (hubbub && hubp) {
 			if (hubbub->funcs->program_det_size)
 				hubbub->funcs->program_det_size(hubbub, hubp->inst, 0);
+			if (hubbub->funcs->program_det_segments)
+				hubbub->funcs->program_det_segments(hubbub, hubp->inst, 0);
 		}
 	}
 
