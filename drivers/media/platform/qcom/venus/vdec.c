8e172e38a623ce284baf2514f963b29e4d47c62e
Nas Chung <nas.chung@chipsnmedia.com>
2024-07-25 15:10:33 +0900

    media: qcom: venus: Fix uninitialized variable warning
    Avoid uninitialized variable when both V4L2_TYPE_IS_OUTPUT() and
    V4L2_TYPE_IS_CAPTURE() return false.
    
    Signed-off-by: Nas Chung <nas.chung@chipsnmedia.com>
    Signed-off-by: Sebastian Fricke <sebastian.fricke@collabora.com>
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>

--- drivers/media/platform/qcom/venus/vdec.c
+++ drivers/media/platform/qcom/venus/vdec.c
@@ -154,14 +154,14 @@ find_format_by_index(struct venus_inst *inst, unsigned int index, u32 type)
 		return NULL;
 
 	for (i = 0; i < size; i++) {
-		bool valid;
+		bool valid = false;
 
 		if (fmt[i].type != type)
 			continue;
 
 		if (V4L2_TYPE_IS_OUTPUT(type)) {
 			valid = venus_helper_check_codec(inst, fmt[i].pixfmt);
-		} else if (V4L2_TYPE_IS_CAPTURE(type)) {
+		} else {
 			valid = venus_helper_check_format(inst, fmt[i].pixfmt);
 
 			if (fmt[i].pixfmt == V4L2_PIX_FMT_QC10C &&
