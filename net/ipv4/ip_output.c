4ff8863419cdc40f2c6e1ad99436e375b9b86b68
James Chapman <jchapman@katalix.com>
2024-07-29 16:38:01 +0100

    ipv4: export ip_flush_pending_frames
    To avoid protocol modules implementing their own, export
    ip_flush_pending_frames.
    
    Signed-off-by: James Chapman <jchapman@katalix.com>
    Signed-off-by: Tom Parkin <tparkin@katalix.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/ip_output.c
+++ net/ipv4/ip_output.c
@@ -1534,6 +1534,7 @@ void ip_flush_pending_frames(struct sock *sk)
 {
 	__ip_flush_pending_frames(sk, &sk->sk_write_queue, &inet_sk(sk)->cork.base);
 }
+EXPORT_SYMBOL_GPL(ip_flush_pending_frames);
 
 struct sk_buff *ip_make_skb(struct sock *sk,
 			    struct flowi4 *fl4,
