7537f0e7b2cd0faa4f58e3e9e64da0f95ade7a77
Sai Sree Kartheek Adivi <sskartheekadivi@gmail.com>
2024-07-30 06:59:01 +0000

    staging: rtl8723bs: style fix open brace on new line
    It fixes the following checkpatch.pl error
    "ERROR: that open brace { should be on the previous line" and avoids the
    warning "WARNING: braces {} are not necessary for single statement blocks".
    
    Signed-off-by: Sai Sree Kartheek Adivi <sskartheekadivi@gmail.com>
    Link: https://lore.kernel.org/r/20240730065901.35254-1-sskartheekadivi@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/include/osdep_service.h
+++ drivers/staging/rtl8723bs/include/osdep_service.h
@@ -81,9 +81,7 @@ static inline void thread_enter(char *name)
 static inline void flush_signals_thread(void)
 {
 	if (signal_pending(current))
-	{
 		flush_signals(current);
-	}
 }
 
 #define rtw_warn_on(condition) WARN_ON(condition)
