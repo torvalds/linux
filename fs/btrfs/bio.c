f4d39cf1cebfb83f76ffdd632958248aff364e33
Johannes Thumshirn <johannes.thumshirn@wdc.com>
2024-07-31 22:43:05 +0200

    btrfs: set search_commit_root on stripe io in case of relocation
    Set rst_search_commit_root in the btrfs_io_stripe we're passing to
    btrfs_map_block() in case we're doing data relocation.
    
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Reviewed-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/bio.c
+++ fs/btrfs/bio.c
@@ -678,7 +678,10 @@ static bool btrfs_submit_chunk(struct btrfs_bio *bbio, int mirror_num)
 	blk_status_t ret;
 	int error;
 
-	smap.rst_search_commit_root = !bbio->inode;
+	if (!bbio->inode || btrfs_is_data_reloc_root(inode->root))
+		smap.rst_search_commit_root = true;
+	else
+		smap.rst_search_commit_root = false;
 
 	btrfs_bio_counter_inc_blocked(fs_info);
 	error = btrfs_map_block(fs_info, btrfs_op(bio), logical, &map_length,
