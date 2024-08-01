1fa4ffd8e6f6d001da27f00382af79bad0336091
Al Viro <viro@zeniv.linux.org.uk>
2024-08-01 16:03:19 -0400

    close_files(): don't bother with xchg()
    At that point nobody else has references to the victim files_struct;
    as the matter of fact, the caller will free it immediately after
    close_files() returns, with no RCU delays or anything of that sort.
    
    That's why we are not protecting against fdtable reallocation on
    expansion, not cleaning the bitmaps, etc.  There's no point
    zeroing the pointers in ->fd[] either, let alone make that an
    atomic operation.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/file.c
+++ fs/file.c
@@ -413,7 +413,7 @@ static struct fdtable *close_files(struct files_struct * files)
 		set = fdt->open_fds[j++];
 		while (set) {
 			if (set & 1) {
-				struct file * file = xchg(&fdt->fd[i], NULL);
+				struct file *file = fdt->fd[i];
 				if (file) {
 					filp_close(file, files);
 					cond_resched();
