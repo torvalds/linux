6cf9ef23d9428ca4950a818e3e70bd818748815a
Sean Christopherson <seanjc@google.com>
2024-08-02 13:01:35 -0700

    KVM: Return '0' directly when there's no task to yield to
    Do "return 0" instead of initializing and returning a local variable in
    kvm_vcpu_yield_to(), e.g. so that it's more obvious what the function
    returns if there is no task.
    
    No functional change intended.
    
    Acked-by: Oliver Upton <oliver.upton@linux.dev>
    Link: https://lore.kernel.org/r/20240802200136.329973-2-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -3772,7 +3772,7 @@ int kvm_vcpu_yield_to(struct kvm_vcpu *target)
 {
 	struct pid *pid;
 	struct task_struct *task = NULL;
-	int ret = 0;
+	int ret;
 
 	rcu_read_lock();
 	pid = rcu_dereference(target->pid);
@@ -3780,7 +3780,7 @@ int kvm_vcpu_yield_to(struct kvm_vcpu *target)
 		task = get_pid_task(pid, PIDTYPE_PID);
 	rcu_read_unlock();
 	if (!task)
-		return ret;
+		return 0;
 	ret = yield_to(task, 1);
 	put_task_struct(task);
 
