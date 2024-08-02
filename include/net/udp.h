87d973e8ddeeddf1777e6689df86d5d369cbcbb3
Eric Dumazet <edumazet@google.com>
2024-08-02 13:40:29 +0000

    ipv6: udp: constify 'struct net' parameter of socket lookups
    Following helpers do not touch their 'struct net' argument.
    
    - udp6_lib_lookup()
    - __udp6_lib_lookup()
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://patch.msgid.link/20240802134029.3748005-6-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/net/udp.h
+++ include/net/udp.h
@@ -305,11 +305,11 @@ struct sock *__udp4_lib_lookup(const struct net *net, __be32 saddr,
 			       struct udp_table *tbl, struct sk_buff *skb);
 struct sock *udp4_lib_lookup_skb(const struct sk_buff *skb,
 				 __be16 sport, __be16 dport);
-struct sock *udp6_lib_lookup(struct net *net,
+struct sock *udp6_lib_lookup(const struct net *net,
 			     const struct in6_addr *saddr, __be16 sport,
 			     const struct in6_addr *daddr, __be16 dport,
 			     int dif);
-struct sock *__udp6_lib_lookup(struct net *net,
+struct sock *__udp6_lib_lookup(const struct net *net,
 			       const struct in6_addr *saddr, __be16 sport,
 			       const struct in6_addr *daddr, __be16 dport,
 			       int dif, int sdif, struct udp_table *tbl,
