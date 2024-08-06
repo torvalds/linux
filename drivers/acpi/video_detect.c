1989feaa278c332c733a439772e902af3a1dc742
Esther Shimanovich <eshimanovich@chromium.org>
2024-08-06 20:08:47 +0000

    ACPI: video: force native for Apple MacbookPro9,2
    [ Upstream commit 7dc918daaf2994963690171584ba423f28724df5 ]
    
    It used to be that the MacbookPro9,2 used its native intel backlight
    device until the following commit was introduced:
    
    commit b1d36e73cc1c ("drm/i915: Don't register backlight when another
    backlight should be used (v2)")
    
    This commit forced this model to use its firmware acpi_video backlight
    device instead.
    
    That worked fine until an additional commit was added:
    
    commit 92714006eb4d ("drm/i915/backlight: Do not bump min brightness
    to max on enable")
    
    That commit uncovered a bug in the MacbookPro 9,2's acpi_video
    backlight firmware; the backlight does not come back up after resume.
    
    Add DMI quirk to select the working native intel interface instead
    so that the backlight successfully comes back up after resume.
    
    Fixes: 92714006eb4d ("drm/i915/backlight: Do not bump min brightness to max on enable")
    Signed-off-by: Esther Shimanovich <eshimanovich@chromium.org>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://patch.msgid.link/20240806-acpi-video-quirk-v1-1-369d8f7abc59@chromium.org
    [ rjw: Subject and changelog edits ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/acpi/video_detect.c
+++ drivers/acpi/video_detect.c
@@ -550,6 +550,14 @@ static const struct dmi_system_id video_detect_dmi_table[] = {
 		},
 	},
 	{
+	 .callback = video_detect_force_native,
+	 /* Apple MacBook Pro 9,2 */
+	 .matches = {
+		DMI_MATCH(DMI_SYS_VENDOR, "Apple Inc."),
+		DMI_MATCH(DMI_PRODUCT_NAME, "MacBookPro9,2"),
+		},
+	},
+	{
 	 /* https://bugzilla.redhat.com/show_bug.cgi?id=1217249 */
 	 .callback = video_detect_force_native,
 	 /* Apple MacBook Pro 12,1 */
