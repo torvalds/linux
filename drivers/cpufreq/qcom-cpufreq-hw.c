5c326d24c1b31a275afc05b4fbb7292eabd4d70a
Rob Herring (Arm) <robh@kernel.org>
2024-08-06 07:58:22 -0600

    cpufreq: qcom: Add explicit io.h include for readl/writel_relaxed
    The qcom-cpufreq-hw driver is relying on an implicit include of io.h
    which doesn't get included on some architectures.
    
    Signed-off-by: Rob Herring (Arm) <robh@kernel.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -9,6 +9,7 @@
 #include <linux/init.h>
 #include <linux/interconnect.h>
 #include <linux/interrupt.h>
+#include <linux/io.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/of.h>
