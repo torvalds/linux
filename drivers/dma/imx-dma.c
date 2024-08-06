39dc2a4929f7be748b37a5070f41afe7bcb60706
Fabio Estevam <festevam@denx.de>
2024-08-05 23:17:44 -0300

    dmaengine: imx-dma: Remove i.MX21 support
    i.MX21 support has been removed since commit 4b563a066611 ("ARM: imx:
    Remove imx21 support").
    
    Remove the i.MX21 support inside the imx-dma driver.
    
    Signed-off-by: Fabio Estevam <festevam@denx.de>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Link: https://lore.kernel.org/r/20240806021744.2524233-1-festevam@gmail.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/imx-dma.c
+++ drivers/dma/imx-dma.c
@@ -167,7 +167,6 @@ struct imxdma_channel {
 
 enum imx_dma_type {
 	IMX1_DMA,
-	IMX21_DMA,
 	IMX27_DMA,
 };
 
@@ -194,8 +193,6 @@ struct imxdma_filter_data {
 static const struct of_device_id imx_dma_of_dev_id[] = {
 	{
 		.compatible = "fsl,imx1-dma", .data = (const void *)IMX1_DMA,
-	}, {
-		.compatible = "fsl,imx21-dma", .data = (const void *)IMX21_DMA,
 	}, {
 		.compatible = "fsl,imx27-dma", .data = (const void *)IMX27_DMA,
 	}, {
