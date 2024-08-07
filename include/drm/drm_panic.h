969135862e731620b9e03bb0c21179ff1cccfd0e
Jocelyn Falempe <jfalempe@redhat.com>
2024-08-07 15:36:13 +0200

    drm/panic: Move copyright notice to the top
    Move the copyright notice to the top of drm_panic.h, and add the
    missing Red Hat copyright notice.
    
    Suggested-by: Thomas Zimmermann <tzimmermann@suse.de>
    Signed-off-by: Jocelyn Falempe <jfalempe@redhat.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Reviewed-by: Thomas Zimmermann <tzimmermann@suse.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240807134902.458669-5-jfalempe@redhat.com

--- include/drm/drm_panic.h
+++ include/drm/drm_panic.h
@@ -1,4 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0 or MIT */
+
+/*
+ * Copyright (c) 2024 Intel
+ * Copyright (c) 2024 Red Hat
+ */
+
 #ifndef __DRM_PANIC_H__
 #define __DRM_PANIC_H__
 
@@ -8,9 +14,6 @@
 
 #include <drm/drm_device.h>
 #include <drm/drm_fourcc.h>
-/*
- * Copyright (c) 2024 Intel
- */
 
 /**
  * struct drm_scanout_buffer - DRM scanout buffer
