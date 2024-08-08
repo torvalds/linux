6d6c05b90f5ac321ecc712dd359713ac48c4b3b2
Geliang Tang <geliang.tang@suse.com>
2024-08-08 17:35:47 +0200

    mptcp: export local_address
    commit dc886bce753cc2cf3c88ec5c7a6880a4e17d65ba upstream.
    
    Rename local_address() with "mptcp_" prefix and export it in protocol.h.
    
    This function will be re-used in the common PM code (pm.c) in the
    following commit.
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Reviewed-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Reviewed-by: Larysa Zaremba <larysa.zaremba@intel.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: 6834097fc38c ("mptcp: pm: fix backup support in signal endpoints")
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -618,6 +618,7 @@ void __mptcp_unaccepted_force_close(struct sock *sk);
 
 bool mptcp_addresses_equal(const struct mptcp_addr_info *a,
 			   const struct mptcp_addr_info *b, bool use_port);
+void mptcp_local_address(const struct sock_common *skc, struct mptcp_addr_info *addr);
 
 /* called with sk socket lock held */
 int __mptcp_subflow_connect(struct sock *sk, const struct mptcp_addr_info *loc,
