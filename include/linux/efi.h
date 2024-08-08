21b91d40575fb64f3f280f6c3af586e32a704a92
Yue Haibing <yuehaibing@huawei.com>
2024-08-08 22:05:54 +0800

    efi: Remove unused declaration efi_initialize_iomem_resources()
    Since commit cf8e8658100d ("arch: Remove Itanium (IA-64) architecture"),
    this is not used anymore.
    
    Signed-off-by: Yue Haibing <yuehaibing@huawei.com>
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- include/linux/efi.h
+++ include/linux/efi.h
@@ -764,8 +764,6 @@ extern int efi_mem_desc_lookup(u64 phys_addr, efi_memory_desc_t *out_md);
 extern int __efi_mem_desc_lookup(u64 phys_addr, efi_memory_desc_t *out_md);
 extern void efi_mem_reserve(phys_addr_t addr, u64 size);
 extern int efi_mem_reserve_persistent(phys_addr_t addr, u64 size);
-extern void efi_initialize_iomem_resources(struct resource *code_resource,
-		struct resource *data_resource, struct resource *bss_resource);
 extern u64 efi_get_fdt_params(struct efi_memory_map_data *data);
 extern struct kobject *efi_kobj;
 
