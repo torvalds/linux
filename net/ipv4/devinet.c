ecdae5168460f7260c0f8dad069aa06f9ba0706e
Kuniyuki Iwashima <kuniyu@amazon.com>
2024-08-09 16:54:04 -0700

    ipv4: Remove redundant !ifa->ifa_dev check.
    Now, ifa_dev is only set in inet_alloc_ifa() and never
    NULL after ifa gets visible.
    
    Let's remove the unneeded NULL check for ifa->ifa_dev.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    Link: https://patch.msgid.link/20240809235406.50187-4-kuniyu@amazon.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/ipv4/devinet.c
+++ net/ipv4/devinet.c
@@ -234,8 +234,7 @@ static void inet_rcu_free_ifa(struct rcu_head *head)
 {
 	struct in_ifaddr *ifa = container_of(head, struct in_ifaddr, rcu_head);
 
-	if (ifa->ifa_dev)
-		in_dev_put(ifa->ifa_dev);
+	in_dev_put(ifa->ifa_dev);
 	kfree(ifa);
 }
 
