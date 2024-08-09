76c510617502a9d423b05f246dba8eb9a38a7b21
Dhananjay Ugwekar <Dhananjay.Ugwekar@amd.com>
2024-08-09 06:08:16 +0000

    cpufreq/amd-pstate: Add the missing cpufreq_cpu_put()
    [ Upstream commit 49243adc715e6ae34d6cc003827e63bcf5b3a21d ]
    
    Fix the reference counting of cpufreq_policy object in amd_pstate_update()
    function by adding the missing cpufreq_cpu_put().
    
    Fixes: e8f555daacd3 ("cpufreq/amd-pstate: fix setting policy current frequency value")
    Signed-off-by: Dhananjay Ugwekar <Dhananjay.Ugwekar@amd.com>
    Reviewed-by: Perry Yuan <perry.yuan@amd.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/cpufreq/amd-pstate.c
+++ drivers/cpufreq/amd-pstate.c
@@ -554,12 +554,15 @@ static void amd_pstate_update(struct amd_cpudata *cpudata, u32 min_perf,
 	}
 
 	if (value == prev)
-		return;
+		goto cpufreq_policy_put;
 
 	WRITE_ONCE(cpudata->cppc_req_cached, value);
 
 	amd_pstate_update_perf(cpudata, min_perf, des_perf,
 			       max_perf, fast_switch);
+
+cpufreq_policy_put:
+	cpufreq_cpu_put(policy);
 }
 
 static int amd_pstate_verify(struct cpufreq_policy_data *policy)
