f203fd85e6669d72fb3bfa3ce485a9642a4a9583
Jakub Kicinski <kuba@kernel.org>
2024-08-09 22:37:18 -0700

    eth: mvpp2: implement new RSS context API
    Implement the separate create/modify/delete ops for RSS.
    
    No problems with IDs - even tho RSS tables are per device
    the driver already seems to allocate IDs linearly per port.
    There's a translation table from per-port context ID
    to device context ID.
    
    mvpp2 doesn't have a key for the hash, it defaults to
    an empty/previous indir table.
    
    Note that there is no key at all, so we don't have to be
    concerned with reporting the wrong one (which is addressed
    by a patch later in the series).
    
    Compile-tested only.
    
    Reviewed-by: Edward Cree <ecree.xilinx@gmail.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/mvpp2/mvpp2_cls.h
+++ drivers/net/ethernet/marvell/mvpp2/mvpp2_cls.h
@@ -264,7 +264,7 @@ int mvpp22_port_rss_init(struct mvpp2_port *port);
 int mvpp22_port_rss_enable(struct mvpp2_port *port);
 int mvpp22_port_rss_disable(struct mvpp2_port *port);
 
-int mvpp22_port_rss_ctx_create(struct mvpp2_port *port, u32 *rss_ctx);
+int mvpp22_port_rss_ctx_create(struct mvpp2_port *port, u32 rss_ctx);
 int mvpp22_port_rss_ctx_delete(struct mvpp2_port *port, u32 rss_ctx);
 
 int mvpp22_port_rss_ctx_indir_set(struct mvpp2_port *port, u32 rss_ctx,
