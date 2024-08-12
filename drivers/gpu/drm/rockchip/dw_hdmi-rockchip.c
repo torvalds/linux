ee689a9500efb4daeab45f44d43c782fc214dada
Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
2024-08-13 00:15:11 +0300

    drm/rockchip: dw_hdmi: Drop superfluous assignments of mpll_cfg, cur_ctr and phy_config
    The mpll_cfg, cur_ctr and phy_config members in struct dw_hdmi_plat_data
    are only used to configure the Synopsys PHYs supported internally by DW
    HDMI transmitter driver (gpu/drm/bridge/synopsys/dw-hdmi.c), via
    hdmi_phy_configure_dwc_hdmi_3d_tx(), which is further invoked from
    dw_hdmi_phy_init().  This is part of the internal
    dw_hdmi_synopsys_phy_ops struct, managed within dw_hdmi_detect_phy().
    
    To handle vendor PHYs, DW HDMI driver doesn't make use of the internal
    PHY ops and, instead, relies on the glue layer to provide the phy_ops
    and phy_name members of struct dw_hdmi_plat_data.
    
    Drop the unnecessary assignments of DW internal PHY related members from
    structs rk3228_hdmi_drv_data and rk3328_hdmi_drv_data, since both set
    the phy_force_vendor flag and correctly provide the expected vendor PHY
    data.
    
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240813-dw-hdmi-rockchip-cleanup-v1-4-b3e73b5f4fd6@collabora.com

--- drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
+++ drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
@@ -444,9 +444,6 @@ static struct rockchip_hdmi_chip_data rk3228_chip_data = {
 
 static const struct dw_hdmi_plat_data rk3228_hdmi_drv_data = {
 	.mode_valid = dw_hdmi_rockchip_mode_valid,
-	.mpll_cfg = rockchip_mpll_cfg,
-	.cur_ctr = rockchip_cur_ctr,
-	.phy_config = rockchip_phy_config,
 	.phy_data = &rk3228_chip_data,
 	.phy_ops = &rk3228_hdmi_phy_ops,
 	.phy_name = "inno_dw_hdmi_phy2",
@@ -481,9 +478,6 @@ static struct rockchip_hdmi_chip_data rk3328_chip_data = {
 
 static const struct dw_hdmi_plat_data rk3328_hdmi_drv_data = {
 	.mode_valid = dw_hdmi_rockchip_mode_valid,
-	.mpll_cfg = rockchip_mpll_cfg,
-	.cur_ctr = rockchip_cur_ctr,
-	.phy_config = rockchip_phy_config,
 	.phy_data = &rk3328_chip_data,
 	.phy_ops = &rk3328_hdmi_phy_ops,
 	.phy_name = "inno_dw_hdmi_phy2",
