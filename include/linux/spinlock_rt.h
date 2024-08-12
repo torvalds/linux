b1f01f9e54b1aaadb6740f86017e8fabdee77fe2
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2024-08-12 12:39:03 +0200

    locking/rt: Remove one __cond_lock() in RT's spin_trylock_irqsave()
    spin_trylock_irqsave() has a __cond_lock() wrapper which points to
    __spin_trylock_irqsave(). The function then invokes spin_trylock() which
    has another __cond_lock() finally pointing to rt_spin_trylock().
    
    The compiler has no problem to parse this but sparse does not recognise
    that users of spin_trylock_irqsave() acquire a conditional lock and
    complains.
    
    Remove one layer of __cond_lock() so that sparse recognises conditional
    locking.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/all/20240812104200.2239232-3-bigeasy@linutronix.de

--- include/linux/spinlock_rt.h
+++ include/linux/spinlock_rt.h
@@ -132,7 +132,7 @@ static __always_inline void spin_unlock_irqrestore(spinlock_t *lock,
 #define spin_trylock_irq(lock)				\
 	__cond_lock(lock, rt_spin_trylock(lock))
 
-#define __spin_trylock_irqsave(lock, flags)		\
+#define spin_trylock_irqsave(lock, flags)		\
 ({							\
 	int __locked;					\
 							\
@@ -141,9 +141,6 @@ static __always_inline void spin_unlock_irqrestore(spinlock_t *lock,
 	__locked = spin_trylock(lock);			\
 	__locked;					\
 })
-
-#define spin_trylock_irqsave(lock, flags)		\
-	__cond_lock(lock, __spin_trylock_irqsave(lock, flags))
 
 #define spin_is_contended(lock)		(((void)(lock), 0))
 
