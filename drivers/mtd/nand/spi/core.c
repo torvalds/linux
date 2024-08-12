1824520e7477bedf76bd08c32261c755e6405cd9
Daniel Golle <daniel@makrotopia.org>
2024-08-12 02:56:41 +0100

    mtd: spinand: set bitflip_threshold to 75% of ECC strength
    Reporting an unclean read from SPI-NAND only when the maximum number
    of correctable bitflip errors has been hit seems a bit late.
    UBI LEB scrubbing, which depends on the lower MTD device reporting
    correctable bitflips, then only kicks in when it's almost too late.
    
    Set bitflip_threshold to 75% of the ECC strength, which is also the
    default for raw NAND.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>
    Reviewed-by: Frieder Schrempf <frieder.schrempf@kontron.de>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Link: https://lore.kernel.org/linux-mtd/2117e387260b0a96f95b8e1652ff79e0e2d71d53.1723427450.git.daniel@makrotopia.org

--- drivers/mtd/nand/spi/core.c
+++ drivers/mtd/nand/spi/core.c
@@ -1287,6 +1287,7 @@ static int spinand_init(struct spinand_device *spinand)
 	/* Propagate ECC information to mtd_info */
 	mtd->ecc_strength = nanddev_get_ecc_conf(nand)->strength;
 	mtd->ecc_step_size = nanddev_get_ecc_conf(nand)->step_size;
+	mtd->bitflip_threshold = DIV_ROUND_UP(mtd->ecc_strength * 3, 4);
 
 	ret = spinand_create_dirmaps(spinand);
 	if (ret) {
