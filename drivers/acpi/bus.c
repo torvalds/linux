21734d29f84ad610dbafdca5f4c1bf7ecceeb2fb
Sunil V L <sunilvl@ventanamicro.com>
2024-08-12 06:29:19 +0530

    ACPI: bus: Add RINTC IRQ model for RISC-V
    Add the IRQ model for RISC-V INTC so that acpi_set_irq_model can use this
    for RISC-V.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>
    Tested-by: Björn Töpel <bjorn@rivosinc.com>
    Link: https://patch.msgid.link/20240812005929.113499-8-sunilvl@ventanamicro.com
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/bus.c
+++ drivers/acpi/bus.c
@@ -1203,6 +1203,9 @@ static int __init acpi_bus_init_irq(void)
 	case ACPI_IRQ_MODEL_LPIC:
 		message = "LPIC";
 		break;
+	case ACPI_IRQ_MODEL_RINTC:
+		message = "RINTC";
+		break;
 	default:
 		pr_info("Unknown interrupt routing model\n");
 		return -ENODEV;
