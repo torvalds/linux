9ee2578c343a2c8b6cf6c43f2f385cdb56a25250
Miaoqing Pan <quic_miaoqing@quicinc.com>
2024-08-13 09:30:27 +0800

    wifi: ath11k: use union for vaddr and iaddr in target_mem_chunk
    The value of 'ab->hw_params.fixed_mem_region' determines that
    only one variable 'vaddr' or 'iaddr' is used in target_mem_chunk.
    So use an anonymous union instead, easy to check whether the
    memory is set or not.
    
    Tested-on: WCN6855 hw2.1 PCI WLAN.HSP.1.1-04358-QCAHSPSWPL_V1_V2_SILICONZ_LITE-1
    
    Signed-off-by: Miaoqing Pan <quic_miaoqing@quicinc.com>
    Acked-by: Jeff Johnson <quic_jjohnson@quicinc.com>
    Link: https://patch.msgid.link/20240813013028.2708111-1-quic_miaoqing@quicinc.com
    Signed-off-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>

--- drivers/net/wireless/ath/ath11k/qmi.h
+++ drivers/net/wireless/ath/ath11k/qmi.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: BSD-3-Clause-Clear */
 /*
  * Copyright (c) 2018-2019 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef ATH11K_QMI_H
@@ -102,8 +102,11 @@ struct target_mem_chunk {
 	u32 prev_size;
 	u32 prev_type;
 	dma_addr_t paddr;
-	u32 *vaddr;
-	void __iomem *iaddr;
+	union {
+		u32 *vaddr;
+		void __iomem *iaddr;
+		void *anyaddr;
+	};
 };
 
 struct target_info {
