ec9e2e7acc6dabb8f00c2c60785931310caaa883
Ilya Bakoulin <Ilya.Bakoulin@amd.com>
2024-08-15 18:45:20 -0400

    drm/amd/display: Fix construct_phy with MXM connector
    [Why/How]
    The call to construct_phy will fail in cases where connector type is
    MXM, and the dc_link won't be properly created/initialized.
    
    Reviewed-by: Wenjing Liu <wenjing.liu@amd.com>
    Signed-off-by: Ilya Bakoulin <Ilya.Bakoulin@amd.com>
    Signed-off-by: Roman Li <roman.li@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/link/link_factory.c
+++ drivers/gpu/drm/amd/display/dc/link/link_factory.c
@@ -524,6 +524,7 @@ static bool construct_phy(struct dc_link *link,
 		link->connector_signal = SIGNAL_TYPE_DVI_DUAL_LINK;
 		break;
 	case CONNECTOR_ID_DISPLAY_PORT:
+	case CONNECTOR_ID_MXM:
 	case CONNECTOR_ID_USBC:
 		link->connector_signal = SIGNAL_TYPE_DISPLAY_PORT;
 
