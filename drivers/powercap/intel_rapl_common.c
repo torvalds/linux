eca0f1b0bbf9f1082993206e3430d9da391be366
Sumeet Pawnikar <sumeet.r.pawnikar@intel.com>
2024-08-16 17:03:32 +0530

    powercap: intel_rapl: Add support for ArrowLake-U platform
    Add support for ArrowLake-U platform to the RAPL common driver.
    
    Signed-off-by: Sumeet Pawnikar <sumeet.r.pawnikar@intel.com>
    Acked-by: Zhang Rui <rui.zhang@intel.com>
    Link: https://patch.msgid.link/20240816113332.7408-1-sumeet.r.pawnikar@intel.com
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/powercap/intel_rapl_common.c
+++ drivers/powercap/intel_rapl_common.c
@@ -1267,6 +1267,7 @@ static const struct x86_cpu_id rapl_ids[] __initconst = {
 	X86_MATCH_VFM(INTEL_LUNARLAKE_M,	&rapl_defaults_core),
 	X86_MATCH_VFM(INTEL_ARROWLAKE_H,	&rapl_defaults_core),
 	X86_MATCH_VFM(INTEL_ARROWLAKE,		&rapl_defaults_core),
+	X86_MATCH_VFM(INTEL_ARROWLAKE_U,	&rapl_defaults_core),
 	X86_MATCH_VFM(INTEL_LAKEFIELD,		&rapl_defaults_core),
 
 	X86_MATCH_VFM(INTEL_ATOM_SILVERMONT,	&rapl_defaults_byt),
