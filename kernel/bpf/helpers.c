67666479edf1e2b732f4d0ac797885e859a78de4
Matteo Croce <teknoraver@meta.com>
2024-08-19 18:28:04 +0200

    bpf: Enable generic kfuncs for BPF_CGROUP_* programs
    These kfuncs are enabled even in BPF_PROG_TYPE_TRACING, so they
    should be safe also in BPF_CGROUP_* programs.
    Since all BPF_CGROUP_* programs share the same hook,
    call register_btf_kfunc_id_set() only once.
    
    In enum btf_kfunc_hook, rename BTF_KFUNC_HOOK_CGROUP_SKB to a more
    generic BTF_KFUNC_HOOK_CGROUP, since it's used for all the cgroup
    related program types.
    
    Signed-off-by: Matteo Croce <teknoraver@meta.com>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20240819162805.78235-2-technoboy85@gmail.com

--- kernel/bpf/helpers.c
+++ kernel/bpf/helpers.c
@@ -3052,6 +3052,7 @@ static int __init kfunc_init(void)
 	ret = ret ?: register_btf_kfunc_id_set(BPF_PROG_TYPE_XDP, &generic_kfunc_set);
 	ret = ret ?: register_btf_kfunc_id_set(BPF_PROG_TYPE_STRUCT_OPS, &generic_kfunc_set);
 	ret = ret ?: register_btf_kfunc_id_set(BPF_PROG_TYPE_SYSCALL, &generic_kfunc_set);
+	ret = ret ?: register_btf_kfunc_id_set(BPF_PROG_TYPE_CGROUP_SKB, &generic_kfunc_set);
 	ret = ret ?: register_btf_id_dtor_kfuncs(generic_dtors,
 						  ARRAY_SIZE(generic_dtors),
 						  THIS_MODULE);
