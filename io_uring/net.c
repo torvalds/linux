641a6816795b208aa7ccac751acaae580897db10
Jens Axboe <axboe@kernel.dk>
2024-08-20 07:22:53 -0600

    Revert "io_uring: Require zeroed sqe->len on provided-buffers send"
    This reverts commit 79996b45f7b28c0e3e08a95bab80119e95317e28.
    
    Revert the change that restricts a send provided buffer to be zero, so
    it will always consume the whole buffer. This is strictly needed for
    partial consumption, as the send may very well be a subset of the
    current buffer. In fact, that's the intended use case.
    
    For non-incremental provided buffer rings, an application should set
    sqe->len carefully to avoid the potential issue described in the
    reverted commit. It is recommended that '0' still be set for len for
    that case, if the application is set on maintaining more than 1 send
    inflight for the same socket. This is somewhat of a nonsensical thing
    to do.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- io_uring/net.c
+++ io_uring/net.c
@@ -434,8 +434,6 @@ int io_sendmsg_prep(struct io_kiocb *req, const struct io_uring_sqe *sqe)
 		sr->buf_group = req->buf_index;
 		req->buf_list = NULL;
 	}
-	if (req->flags & REQ_F_BUFFER_SELECT && sr->len)
-		return -EINVAL;
 
 #ifdef CONFIG_COMPAT
 	if (req->ctx->compat)
@@ -599,7 +597,7 @@ retry_bundle:
 	if (io_do_buffer_select(req)) {
 		struct buf_sel_arg arg = {
 			.iovs = &kmsg->fast_iov,
-			.max_len = INT_MAX,
+			.max_len = min_not_zero(sr->len, INT_MAX),
 			.nr_iovs = 1,
 		};
 
