efe508816d2caf83536ff2f308e09043380fb2b7
Andrew Cooper <andrew.cooper3@citrix.com>
2024-08-22 00:39:05 -0700

    x86/msr: Switch between WRMSRNS and WRMSR with the alternatives mechanism
    Per the discussion about FRED MSR writes with WRMSRNS instruction [1],
    use the alternatives mechanism to choose WRMSRNS when it's available,
    otherwise fallback to WRMSR.
    
    Remove the dependency on X86_FEATURE_WRMSRNS as WRMSRNS is no longer
    dependent on FRED.
    
    [1] https://lore.kernel.org/lkml/15f56e6a-6edd-43d0-8e83-bb6430096514@citrix.com/
    
    Use DS prefix to pad WRMSR instead of a NOP. The prefix is ignored. At
    least that's the current information from the hardware folks.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Signed-off-by: Xin Li (Intel) <xin@zytor.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/all/20240822073906.2176342-3-xin@zytor.com

--- arch/x86/include/asm/switch_to.h
+++ arch/x86/include/asm/switch_to.h
@@ -71,7 +71,6 @@ static inline void update_task_stack(struct task_struct *task)
 	this_cpu_write(cpu_tss_rw.x86_tss.sp1, task->thread.sp0);
 #else
 	if (cpu_feature_enabled(X86_FEATURE_FRED)) {
-		/* WRMSRNS is a baseline feature for FRED. */
 		wrmsrns(MSR_IA32_FRED_RSP0, (unsigned long)task_stack_page(task) + THREAD_SIZE);
 	} else if (cpu_feature_enabled(X86_FEATURE_XENPV)) {
 		/* Xen PV enters the kernel on the thread stack. */
