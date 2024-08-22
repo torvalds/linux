1a9e3b0af301413210319e6946fb4b0b1ad71ccc
Shenghao Ding <shenghao-ding@ti.com>
2024-08-22 14:32:02 +0800

    ASoC: tas2781: mark const variables tas2563_dvc_table as __maybe_unused
    In case of tas2781, tas2563_dvc_table will be unused,
    so mark it as __maybe_unused.
    
    Signed-off-by: Shenghao Ding <shenghao-ding@ti.com>
    Link: https://patch.msgid.link/20240822063205.662-1-shenghao-ding@ti.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/sound/tas2563-tlv.h
+++ include/sound/tas2563-tlv.h
@@ -18,7 +18,7 @@
 static const __maybe_unused DECLARE_TLV_DB_SCALE(tas2563_dvc_tlv, -12150, 50, 1);
 
 /* pow(10, db/20) * pow(2,30) */
-static const unsigned char tas2563_dvc_table[][4] = {
+static const __maybe_unused unsigned char tas2563_dvc_table[][4] = {
 	{ 0X00, 0X00, 0X00, 0X00 }, /* -121.5db */
 	{ 0X00, 0X00, 0X03, 0XBC }, /* -121.0db */
 	{ 0X00, 0X00, 0X03, 0XF5 }, /* -120.5db */
