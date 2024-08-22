6673f43370a79e40f14b035f9eceaa89faf2eaf8
Túlio Fernandes <tuliomf09@gmail.com>
2024-08-21 21:45:58 -0300

    staging: rtl8192e: Fix parenthesis alignment
    Fix parenthesis alignment in rtl92e_set_rf_state in order to rid
    the following warning:
    
    CHECK: Alignment should match open parenthesis
    
    Signed-off-by: Túlio Fernandes <tuliomf09@gmail.com>
    Link: https://lore.kernel.org/r/20240822004558.14638-1-tuliomf09@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/rtl_core.c
+++ drivers/staging/rtl8192e/rtl8192e/rtl_core.c
@@ -173,7 +173,7 @@ bool rtl92e_set_rf_state(struct net_device *dev,
 				else
 					priv->blinked_ingpio = false;
 				rtllib_mgnt_disconnect(priv->rtllib,
-						      WLAN_REASON_DISASSOC_STA_HAS_LEFT);
+						       WLAN_REASON_DISASSOC_STA_HAS_LEFT);
 			}
 		}
 		if ((change_source == RF_CHANGE_BY_HW) && !priv->hw_radio_off)
