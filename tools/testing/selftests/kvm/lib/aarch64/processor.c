ca1a18368d764f9b29ab0c79b3ddd712f5511855
Colton Lewis <coltonlewis@google.com>
2024-08-23 17:58:35 +0000

    KVM: arm64: selftests: Ensure pending interrupts are handled in arch_timer test
    Break up the asm instructions poking daifclr and daifset to handle
    interrupts. R_RBZYL specifies pending interrupts will be handle after
    context synchronization events such as an ISB.
    
    Introduce a function wrapper for the WFI instruction.
    
    Signed-off-by: Colton Lewis <coltonlewis@google.com>
    Link: https://lore.kernel.org/r/20240823175836.2798235-2-coltonlewis@google.com
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- tools/testing/selftests/kvm/lib/aarch64/processor.c
+++ tools/testing/selftests/kvm/lib/aarch64/processor.c
@@ -639,3 +639,9 @@ void vm_vaddr_populate_bitmap(struct kvm_vm *vm)
 	sparsebit_set_num(vm->vpages_valid, 0,
 			  (1ULL << vm->va_bits) >> vm->page_shift);
 }
+
+/* Helper to call wfi instruction. */
+void wfi(void)
+{
+	asm volatile("wfi");
+}
