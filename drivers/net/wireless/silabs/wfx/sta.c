2926be1d0fc3b69c5d2b1b708f1f9fa0db511a1c
Alexander Sverdlin <alexander.sverdlin@siemens.com>
2024-08-23 15:15:20 +0200

    wifi: wfx: repair open network AP mode
    commit 6d30bb88f623526197c0e18a366e68a4254a2c83 upstream.
    
    RSN IE missing in beacon is normal in open networks.
    Avoid returning -EINVAL in this case.
    
    Steps to reproduce:
    
    $ cat /etc/wpa_supplicant.conf
    network={
    	ssid="testNet"
    	mode=2
    	key_mgmt=NONE
    }
    
    $ wpa_supplicant -iwlan0 -c /etc/wpa_supplicant.conf
    nl80211: Beacon set failed: -22 (Invalid argument)
    Failed to set beacon parameters
    Interface initialization failed
    wlan0: interface state UNINITIALIZED->DISABLED
    wlan0: AP-DISABLED
    wlan0: Unable to setup interface.
    Failed to initialize AP interface
    
    After the change:
    
    $ wpa_supplicant -iwlan0 -c /etc/wpa_supplicant.conf
    Successfully initialized wpa_supplicant
    wlan0: interface state UNINITIALIZED->ENABLED
    wlan0: AP-ENABLED
    
    Cc: stable@vger.kernel.org
    Fixes: fe0a7776d4d1 ("wifi: wfx: fix possible NULL pointer dereference in wfx_set_mfp_ap()")
    Signed-off-by: Alexander Sverdlin <alexander.sverdlin@siemens.com>
    Reviewed-by: Jérôme Pouiller <jerome.pouiller@silabs.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://patch.msgid.link/20240823131521.3309073-1-alexander.sverdlin@siemens.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/wireless/silabs/wfx/sta.c
+++ drivers/net/wireless/silabs/wfx/sta.c
@@ -370,8 +370,11 @@ static int wfx_set_mfp_ap(struct wfx_vif *wvif)
 
 	ptr = (u16 *)cfg80211_find_ie(WLAN_EID_RSN, skb->data + ieoffset,
 				      skb->len - ieoffset);
-	if (unlikely(!ptr))
+	if (!ptr) {
+		/* No RSN IE is fine in open networks */
+		ret = 0;
 		goto free_skb;
+	}
 
 	ptr += pairwise_cipher_suite_count_offset;
 	if (WARN_ON(ptr > (u16 *)skb_tail_pointer(skb)))
