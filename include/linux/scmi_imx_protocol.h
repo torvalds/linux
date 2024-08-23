61c9f03e22fc57fe61726c513b1f92c0ed1ef00f
Peng Fan <peng.fan@nxp.com>
2024-08-23 17:05:19 +0800

    firmware: arm_scmi: Add initial support for i.MX MISC protocol
    i.MX95 System Manager(SM) firmware includes a SCMI vendor protocol, SCMI
    MISC protocol which includes controls that are misc settings/actions that
    must be exposed from the SM to agents. They are device specific and are
    usually define to access bit fields in various mix block control modules,
    IOMUX_GPR, and other General Purpose registers, Control Status Registers
    owned by the SM.
    
    Reviewed-by: Cristian Marussi <cristian.marussi@arm.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Message-Id: <20240823-imx95-bbm-misc-v2-v8-3-e600ed9e9271@nxp.com>
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>

--- include/linux/scmi_imx_protocol.h
+++ include/linux/scmi_imx_protocol.h
@@ -15,6 +15,7 @@
 
 enum scmi_nxp_protocol {
 	SCMI_PROTOCOL_IMX_BBM = 0x81,
+	SCMI_PROTOCOL_IMX_MISC = 0x84,
 };
 
 struct scmi_imx_bbm_proto_ops {
@@ -30,6 +31,7 @@ struct scmi_imx_bbm_proto_ops {
 enum scmi_nxp_notification_events {
 	SCMI_EVENT_IMX_BBM_RTC = 0x0,
 	SCMI_EVENT_IMX_BBM_BUTTON = 0x1,
+	SCMI_EVENT_IMX_MISC_CONTROL = 0x0,
 };
 
 struct scmi_imx_bbm_notif_report {
@@ -38,5 +40,20 @@ struct scmi_imx_bbm_notif_report {
 	ktime_t			timestamp;
 	unsigned int		rtc_id;
 	unsigned int		rtc_evt;
+};
+
+struct scmi_imx_misc_ctrl_notify_report {
+	ktime_t			timestamp;
+	unsigned int		ctrl_id;
+	unsigned int		flags;
+};
+
+struct scmi_imx_misc_proto_ops {
+	int (*misc_ctrl_set)(const struct scmi_protocol_handle *ph, u32 id,
+			     u32 num, u32 *val);
+	int (*misc_ctrl_get)(const struct scmi_protocol_handle *ph, u32 id,
+			     u32 *num, u32 *val);
+	int (*misc_ctrl_req_notify)(const struct scmi_protocol_handle *ph,
+				    u32 ctrl_id, u32 evt_id, u32 flags);
 };
 #endif
