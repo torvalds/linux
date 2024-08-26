58487cc9f93e116562d24c5a62278dba32214068
Yihan Zhu <Yihan.Zhu@amd.com>
2024-08-26 14:44:04 -0400

    drm/amd/display: Enable DML2 override_det_buffer_size_kbytes
    commit f57b77d667dc6bd2b114d08d04b03869539209f6 upstream.
    
    [WHY]
    Corrupted screen will be observed when 4k144 DP/HDMI display and
    4k144 eDP are connected, changing eDP refresh rate from 60Hz to 144Hz.
    
    [HOW]
    override_det_buffer_size_kbytes should be true for DCN35/DCN351.
    
    Cc: Mario Limonciello <mario.limonciello@amd.com>
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: stable@vger.kernel.org
    Reviewed-by: Roman Li <roman.li@amd.com>
    Reviewed-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Yihan Zhu <Yihan.Zhu@amd.com>
    Signed-off-by: Alex Hung <alex.hung@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/amd/display/dc/resource/dcn35/dcn35_resource.c
+++ drivers/gpu/drm/amd/display/dc/resource/dcn35/dcn35_resource.c
@@ -2153,6 +2153,7 @@ static bool dcn35_resource_construct(
 
 	dc->dml2_options.max_segments_per_hubp = 24;
 	dc->dml2_options.det_segment_size = DCN3_2_DET_SEG_SIZE;/*todo*/
+	dc->dml2_options.override_det_buffer_size_kbytes = true;
 
 	if (dc->config.sdpif_request_limit_words_per_umc == 0)
 		dc->config.sdpif_request_limit_words_per_umc = 16;/*todo*/
