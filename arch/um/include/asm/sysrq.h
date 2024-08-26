ae0dc67c2512e09fee26226e1b2d78b82ebebf66
Tiwei Bie <tiwei.btw@antgroup.com>
2024-08-26 18:08:15 +0800

    um: Remove outdated asm/sysrq.h header
    This header no longer serves a purpose after show_trace was removed
    by commit 9d1ee8ce92e1 ("um: Rewrite show_stack()").
    
    Signed-off-by: Tiwei Bie <tiwei.btw@antgroup.com>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/include/asm/sysrq.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef __UM_SYSRQ_H
-#define __UM_SYSRQ_H
-
-struct task_struct;
-extern void show_trace(struct task_struct* task, unsigned long *stack);
-
-#endif
