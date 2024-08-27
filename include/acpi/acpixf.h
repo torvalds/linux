9af32b4a25f59e3a2423a5f87e738a29487242ce
Saket Dumbre <saket.dumbre@intel.com>
2024-08-27 11:24:10 -0700

    ACPICA: Setup for ACPICA release 20240827
    ACPICA commit 86c762afe5bf915e8101a0455513368e2a60dd80
    
    Link: https://github.com/acpica/acpica/commit/86c762af
    Signed-off-by: Saket Dumbre <saket.dumbre@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/acpixf.h
+++ include/acpi/acpixf.h
@@ -12,7 +12,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20240322
+#define ACPI_CA_VERSION                 0x20240827
 
 #include <acpi/acconfig.h>
 #include <acpi/actypes.h>
