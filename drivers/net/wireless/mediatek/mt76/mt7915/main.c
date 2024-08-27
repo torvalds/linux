0137ca96e613b720898e812e7d9408deb4fa0fb9
Howard Hsu <howard-yh.hsu@mediatek.com>
2024-08-27 11:30:08 +0200

    wifi: mt76: mt7915: fix rx filter setting for bfee functionality
    [ Upstream commit 6ac80fce713e875a316a58975b830720a3e27721 ]
    
    Fix rx filter setting to prevent dropping NDPA frames. Without this
    change, bfee functionality may behave abnormally.
    
    Fixes: e57b7901469f ("mt76: add mac80211 driver for MT7915 PCIe-based chipsets")
    Signed-off-by: Howard Hsu <howard-yh.hsu@mediatek.com>
    Link: https://patch.msgid.link/20240827093011.18621-21-nbd@nbd.name
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/wireless/mediatek/mt76/mt7915/main.c
+++ drivers/net/wireless/mediatek/mt76/mt7915/main.c
@@ -564,8 +564,7 @@ static void mt7915_configure_filter(struct ieee80211_hw *hw,
 
 	MT76_FILTER(CONTROL, MT_WF_RFCR_DROP_CTS |
 			     MT_WF_RFCR_DROP_RTS |
-			     MT_WF_RFCR_DROP_CTL_RSV |
-			     MT_WF_RFCR_DROP_NDPA);
+			     MT_WF_RFCR_DROP_CTL_RSV);
 
 	*total_flags = flags;
 	rxfilter = phy->rxfilter;
