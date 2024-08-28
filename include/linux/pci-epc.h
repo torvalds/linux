0328947c50324cf4b2d8b181bf948edb8101f59f
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2024-08-28 21:16:15 +0530

    PCI: endpoint: Assign PCI domain number for endpoint controllers
    Right now, PCI endpoint subsystem doesn't assign PCI domain number for the
    PCI endpoint controllers. But this domain number could be useful to the EPC
    drivers to uniquely identify each controller based on the hardware instance
    when there are multiple ones present in an SoC (even multiple RC/EP).
    
    So let's make use of the existing pci_bus_find_domain_nr() API to allocate
    domain numbers based on either devicetree (linux,pci-domain) property or
    dynamic domain number allocation scheme.
    
    It should be noted that the domain number allocated by this API will be
    based on both RC and EP controllers in a SoC. If the 'linux,pci-domain' DT
    property is present, then the domain number represents the actual hardware
    instance of the PCI endpoint controller. If not, then the domain number
    will be allocated based on the PCI EP/RC controller probe order.
    
    If the architecture doesn't support CONFIG_PCI_DOMAINS_GENERIC (rare), then
    currently a warning is thrown to indicate that the architecture specific
    implementation is needed.
    
    Link: https://lore.kernel.org/linux-pci/20240828-pci-qcom-hotplug-v4-5-263a385fbbcb@linaro.org
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Signed-off-by: Krzysztof Wilczyński <kwilczynski@kernel.org>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>

--- include/linux/pci-epc.h
+++ include/linux/pci-epc.h
@@ -128,6 +128,7 @@ struct pci_epc_mem {
  * @group: configfs group representing the PCI EPC device
  * @lock: mutex to protect pci_epc ops
  * @function_num_map: bitmap to manage physical function number
+ * @domain_nr: PCI domain number of the endpoint controller
  * @init_complete: flag to indicate whether the EPC initialization is complete
  *                 or not
  */
@@ -145,6 +146,7 @@ struct pci_epc {
 	/* mutex to protect against concurrent access of EP controller */
 	struct mutex			lock;
 	unsigned long			function_num_map;
+	int				domain_nr;
 	bool				init_complete;
 };
 
