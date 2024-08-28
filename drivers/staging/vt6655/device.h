7b6ff621e8bd18773728791f727246dc9b2e6fb5
Dominik Karol Piątkowski <dominik.karol.piatkowski@protonmail.com>
2024-08-28 15:00:09 +0000

    staging: vt6655: Fix block comment alignment
    This patch fixes the "Block comments should align the * on each line"
    warning detected by checkpatch.pl.
    
    Signed-off-by: Dominik Karol Piątkowski <dominik.karol.piatkowski@protonmail.com>
    Link: https://lore.kernel.org/r/20240828145923.78004-1-dominik.karol.piatkowski@protonmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/device.h
+++ drivers/staging/vt6655/device.h
@@ -189,10 +189,10 @@ struct vnt_private {
 
 	u8		byBBType; /* 0:11A, 1:11B, 2:11G */
 	u8		packet_type; /*
-				       * 0:11a,1:11b,2:11gb (only CCK
-				       * in BasicRate), 3:11ga (OFDM in
-				       * Basic Rate)
-				       */
+				      * 0:11a,1:11b,2:11gb (only CCK
+				      * in BasicRate), 3:11ga (OFDM in
+				      * Basic Rate)
+				      */
 	unsigned short wBasicRate;
 	unsigned char byACKRate;
 	unsigned char byTopOFDMBasicRate;
