459ca85ae1feff78d1518344df88bb79a092780c
Julian Sun <sunjunchao2870@gmail.com>
2024-08-28 16:13:59 +0800

    writeback: Refine the show_inode_state() macro definition
    Currently, the show_inode_state() macro only prints
    part of the state of inode->i_state. Let’s improve it
    to display more of its state.
    
    Signed-off-by: Julian Sun <sunjunchao2870@gmail.com>
    Link: https://lore.kernel.org/r/20240828081359.62429-1-sunjunchao2870@gmail.com
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- include/trace/events/writeback.h
+++ include/trace/events/writeback.h
@@ -20,7 +20,15 @@
 		{I_CLEAR,		"I_CLEAR"},		\
 		{I_SYNC,		"I_SYNC"},		\
 		{I_DIRTY_TIME,		"I_DIRTY_TIME"},	\
-		{I_REFERENCED,		"I_REFERENCED"}		\
+		{I_REFERENCED,		"I_REFERENCED"},	\
+		{I_LINKABLE,		"I_LINKABLE"},		\
+		{I_WB_SWITCH,		"I_WB_SWITCH"},		\
+		{I_OVL_INUSE,		"I_OVL_INUSE"},		\
+		{I_CREATING,		"I_CREATING"},		\
+		{I_DONTCACHE,		"I_DONTCACHE"},		\
+		{I_SYNC_QUEUED,		"I_SYNC_QUEUED"},	\
+		{I_PINNING_NETFS_WB,	"I_PINNING_NETFS_WB"},	\
+		{I_LRU_ISOLATING,	"I_LRU_ISOLATING"}	\
 	)
 
 /* enums need to be exported to user space */
