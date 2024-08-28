f633919d132cf1755d62278c989124c0ce23950f
Juntong Deng <juntong.deng@outlook.com>
2024-08-28 20:48:11 +0100

    bpf: Relax KF_ACQUIRE kfuncs strict type matching constraint
    Currently we cannot pass zero offset (implicit cast) or non-zero offset
    pointers to KF_ACQUIRE kfuncs. This is because KF_ACQUIRE kfuncs
    requires strict type matching, but zero offset or non-zero offset does
    not change the type of pointer, which causes the ebpf program to be
    rejected by the verifier.
    
    This can cause some problems, one example is that bpf_skb_peek_tail
    kfunc [0] cannot be implemented by just passing in non-zero offset
    pointers. We cannot pass pointers like &sk->sk_write_queue (non-zero
    offset) or &sk->__sk_common (zero offset) to KF_ACQUIRE kfuncs.
    
    This patch makes KF_ACQUIRE kfuncs not require strict type matching.
    
    [0]: https://lore.kernel.org/bpf/AM6PR03MB5848CA39CB4B7A4397D380B099B12@AM6PR03MB5848.eurprd03.prod.outlook.com/
    
    Signed-off-by: Juntong Deng <juntong.deng@outlook.com>
    Link: https://lore.kernel.org/r/AM6PR03MB5848FD2BD89BF0B6B5AA3B4C99952@AM6PR03MB5848.eurprd03.prod.outlook.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -11513,8 +11513,7 @@ static int process_kf_arg_ptr_to_btf_id(struct bpf_verifier_env *env,
 	 * btf_struct_ids_match() to walk the struct at the 0th offset, and
 	 * resolve types.
 	 */
-	if (is_kfunc_acquire(meta) ||
-	    (is_kfunc_release(meta) && reg->ref_obj_id) ||
+	if ((is_kfunc_release(meta) && reg->ref_obj_id) ||
 	    btf_type_ids_nocast_alias(&env->log, reg_btf, reg_ref_id, meta->btf, ref_id))
 		strict_type_match = true;
 
