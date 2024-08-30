f68f72d04b60d3af36b63b888fe084966bca07b9
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2024-08-30 17:29:27 -0400

    Bluetooth: hci_core: Fix sending MGMT_EV_CONNECT_FAILED
    [ Upstream commit d47da6bd4cfa982fe903f33423b9e2ec541e9496 ]
    
    If HCI_CONN_MGMT_CONNECTED has been set then the event shall be
    HCI_CONN_MGMT_DISCONNECTED.
    
    Fixes: b644ba336997 ("Bluetooth: Update device_connected and device_found events to latest API")
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/bluetooth/hci_conn.c
+++ net/bluetooth/hci_conn.c
@@ -106,8 +106,7 @@ void hci_connect_le_scan_cleanup(struct hci_conn *conn, u8 status)
 	 * where a timeout + cancel does indicate an actual failure.
 	 */
 	if (status && status != HCI_ERROR_UNKNOWN_CONN_ID)
-		mgmt_connect_failed(hdev, &conn->dst, conn->type,
-				    conn->dst_type, status);
+		mgmt_connect_failed(hdev, conn, status);
 
 	/* The connection attempt was doing scan for new RPA, and is
 	 * in scan phase. If params are not associated with any other
@@ -1250,8 +1249,7 @@ void hci_conn_failed(struct hci_conn *conn, u8 status)
 		hci_le_conn_failed(conn, status);
 		break;
 	case ACL_LINK:
-		mgmt_connect_failed(hdev, &conn->dst, conn->type,
-				    conn->dst_type, status);
+		mgmt_connect_failed(hdev, conn, status);
 		break;
 	}
 
