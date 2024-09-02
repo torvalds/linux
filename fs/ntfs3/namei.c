589996bf8c459deb5bbc9747d8f1c51658608103
Diogo Jahchan Koike <djahchankoike@gmail.com>
2024-09-02 14:19:32 -0300

    ntfs3: Change to non-blocking allocation in ntfs_d_hash
    d_hash is done while under "rcu-walk" and should not sleep.
    __get_name() allocates using GFP_KERNEL, having the possibility
    to sleep when under memory pressure. Change the allocation to
    GFP_NOWAIT.
    
    Reported-by: syzbot+7f71f79bbfb4427b00e1@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=7f71f79bbfb4427b00e1
    Fixes: d392e85fd1e8 ("fs/ntfs3: Fix the format of the "nocase" mount option")
    Signed-off-by: Diogo Jahchan Koike <djahchankoike@gmail.com>
    Signed-off-by: Konstantin Komarov <almaz.alexandrovich@paragon-software.com>

--- fs/ntfs3/namei.c
+++ fs/ntfs3/namei.c
@@ -395,7 +395,7 @@ static int ntfs_d_hash(const struct dentry *dentry, struct qstr *name)
 	/*
 	 * Try slow way with current upcase table
 	 */
-	uni = __getname();
+	uni = kmem_cache_alloc(names_cachep, GFP_NOWAIT);
 	if (!uni)
 		return -ENOMEM;
 
@@ -417,7 +417,7 @@ static int ntfs_d_hash(const struct dentry *dentry, struct qstr *name)
 	err = 0;
 
 out:
-	__putname(uni);
+	kmem_cache_free(names_cachep, uni);
 	return err;
 }
 
