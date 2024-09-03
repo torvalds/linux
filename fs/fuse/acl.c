4d833befa20253049e4f48ece9cb82f12800405d
Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>
2024-09-03 17:16:20 +0200

    fuse: drop idmap argument from __fuse_get_acl
    We don't need to have idmap in the __fuse_get_acl as we don't
    have any use for it.
    
    In the current POSIX ACL implementation, idmapped mounts are
    taken into account on the userspace/kernel border
    (see vfs_set_acl_idmapped_mnt() and vfs_posix_acl_to_xattr()).
    
    Signed-off-by: Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>
    Reviewed-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/acl.c
+++ fs/fuse/acl.c
@@ -12,7 +12,6 @@
 #include <linux/posix_acl_xattr.h>
 
 static struct posix_acl *__fuse_get_acl(struct fuse_conn *fc,
-					struct mnt_idmap *idmap,
 					struct inode *inode, int type, bool rcu)
 {
 	int size;
@@ -74,7 +73,7 @@ struct posix_acl *fuse_get_acl(struct mnt_idmap *idmap,
 	if (fuse_no_acl(fc, inode))
 		return ERR_PTR(-EOPNOTSUPP);
 
-	return __fuse_get_acl(fc, idmap, inode, type, false);
+	return __fuse_get_acl(fc, inode, type, false);
 }
 
 struct posix_acl *fuse_get_inode_acl(struct inode *inode, int type, bool rcu)
@@ -90,8 +89,7 @@ struct posix_acl *fuse_get_inode_acl(struct inode *inode, int type, bool rcu)
 	 */
 	if (!fc->posix_acl)
 		return NULL;
-
-	return __fuse_get_acl(fc, &nop_mnt_idmap, inode, type, rcu);
+	return __fuse_get_acl(fc,  inode, type, rcu);
 }
 
 int fuse_set_acl(struct mnt_idmap *idmap, struct dentry *dentry,
