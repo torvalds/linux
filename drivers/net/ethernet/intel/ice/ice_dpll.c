ccca30a18e36a742e606d5bf0630e75be7711d0a
Gui-Dong Han <hanguidong02@outlook.com>
2024-09-03 11:48:43 +0000

    ice: Fix improper handling of refcount in ice_dpll_init_rclk_pins()
    This patch addresses a reference count handling issue in the
    ice_dpll_init_rclk_pins() function. The function calls ice_dpll_get_pins(),
    which increments the reference count of the relevant resources. However,
    if the condition WARN_ON((!vsi || !vsi->netdev)) is met, the function
    currently returns an error without properly releasing the resources
    acquired by ice_dpll_get_pins(), leading to a reference count leak.
    
    To resolve this, the check has been moved to the top of the function. This
    ensures that the function verifies the state before any resources are
    acquired, avoiding the need for additional resource management in the
    error path.
    
    This bug was identified by an experimental static analysis tool developed
    by our team. The tool specializes in analyzing reference count operations
    and detecting potential issues where resources are not properly managed.
    In this case, the tool flagged the missing release operation as a
    potential problem, which led to the development of this patch.
    
    Fixes: d7999f5ea64b ("ice: implement dpll interface to control cgu")
    Cc: stable@vger.kernel.org
    Signed-off-by: Gui-Dong Han <hanguidong02@outlook.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Tested-by: Pucha Himasekhar Reddy <himasekharx.reddy.pucha@intel.com> (A Contingent worker at Intel)
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>

--- drivers/net/ethernet/intel/ice/ice_dpll.c
+++ drivers/net/ethernet/intel/ice/ice_dpll.c
@@ -1843,6 +1843,8 @@ ice_dpll_init_rclk_pins(struct ice_pf *pf, struct ice_dpll_pin *pin,
 	struct dpll_pin *parent;
 	int ret, i;
 
+	if (WARN_ON((!vsi || !vsi->netdev)))
+		return -EINVAL;
 	ret = ice_dpll_get_pins(pf, pin, start_idx, ICE_DPLL_RCLK_NUM_PER_PF,
 				pf->dplls.clock_id);
 	if (ret)
@@ -1858,8 +1860,6 @@ ice_dpll_init_rclk_pins(struct ice_pf *pf, struct ice_dpll_pin *pin,
 		if (ret)
 			goto unregister_pins;
 	}
-	if (WARN_ON((!vsi || !vsi->netdev)))
-		return -EINVAL;
 	dpll_netdev_pin_set(vsi->netdev, pf->dplls.rclk.pin);
 
 	return 0;
