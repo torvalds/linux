9e8354b399e99cec3e3546035bee3347a6df2f24
Jason Gunthorpe <jgg@ziepe.ca>
2024-09-04 13:36:02 +0100

    ARM: 9417/1: dma-mapping: Pass device to arm_iommu_create_mapping()
    All users of ARM IOMMU mappings create them for a particular device, so
    change the interface to accept the device rather than forcing a vague
    indirection through a bus type. This prepares for making a similar
    change to iommu_domain_alloc() itself.
    
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Vasant Hegde <vasant.hegde@amd.com>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Jeff Johnson <quic_jjohnson@quicinc.com>
    Signed-off-by: Jason Gunthorpe <jgg@ziepe.ca>
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>

--- drivers/iommu/ipmmu-vmsa.c
+++ drivers/iommu/ipmmu-vmsa.c
@@ -804,8 +804,7 @@ static int ipmmu_init_arm_mapping(struct device *dev)
 	if (!mmu->mapping) {
 		struct dma_iommu_mapping *mapping;
 
-		mapping = arm_iommu_create_mapping(&platform_bus_type,
-						   SZ_1G, SZ_2G);
+		mapping = arm_iommu_create_mapping(dev, SZ_1G, SZ_2G);
 		if (IS_ERR(mapping)) {
 			dev_err(mmu->dev, "failed to create ARM IOMMU mapping\n");
 			ret = PTR_ERR(mapping);
