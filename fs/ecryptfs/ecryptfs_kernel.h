fba133a3411847db49297c965218400c49571ebd
Zhang Zekun <zhangzekun11@huawei.com>
2024-09-06 14:12:41 +0800

    ecryptfs: Remove unused declartion ecryptfs_fill_zeros()
    The definition of ecryptfs_fill_zeros() has been removed since
    commit b6c1d8fcbade ("eCryptfs: remove unused functions and kmem_cache")
    So, Remove the empty declartion in header files.
    
    Signed-off-by: Zhang Zekun <zhangzekun11@huawei.com>
    Link: https://lore.kernel.org/r/20240906061241.20010-1-zhangzekun11@huawei.com
    Signed-off-by: Tyler Hicks <code@tyhicks.com>

--- fs/ecryptfs/ecryptfs_kernel.h
+++ fs/ecryptfs/ecryptfs_kernel.h
@@ -551,7 +551,6 @@ int ecryptfs_decode_and_decrypt_filename(char **decrypted_name,
 					 size_t *decrypted_name_size,
 					 struct super_block *sb,
 					 const char *name, size_t name_size);
-int ecryptfs_fill_zeros(struct file *file, loff_t new_length);
 int ecryptfs_encrypt_and_encode_filename(
 	char **encoded_name,
 	size_t *encoded_name_size,
