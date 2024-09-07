a3ed1cc4136df01e74a8b6df8c562db69e752bad
Alyssa Ross <hi@alyssa.is>
2024-09-07 18:00:26 +0200

    bcachefs: Fix negative timespecs
    This fixes two problems in the handling of negative times:
    
     • rem is signed, but the rem * c->sb.nsec_per_time_unit operation
       produced a bogus unsigned result, because s32 * u32 = u32.
    
     • The timespec was not normalized (it could contain more than a
       billion nanoseconds).
    
    For example, { .tv_sec = -14245441, .tv_nsec = 750000000 }, after
    being round tripped through timespec_to_bch2_time and then
    bch2_time_to_timespec would come back as
    { .tv_sec = -14245440, .tv_nsec = 4044967296 } (more than 4 billion
    nanoseconds).
    
    Cc: stable@vger.kernel.org
    Fixes: 595c1e9bab7f ("bcachefs: Fix time handling")
    Closes: https://github.com/koverstreet/bcachefs/issues/743
    Co-developed-by: Erin Shepherd <erin.shepherd@e43.eu>
    Signed-off-by: Erin Shepherd <erin.shepherd@e43.eu>
    Co-developed-by: Ryan Lahfa <ryan@lahfa.xyz>
    Signed-off-by: Ryan Lahfa <ryan@lahfa.xyz>
    Signed-off-by: Alyssa Ross <hi@alyssa.is>
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/bcachefs.h
+++ fs/bcachefs/bcachefs.h
@@ -1195,12 +1195,15 @@ static inline bool btree_id_cached(const struct bch_fs *c, enum btree_id btree)
 static inline struct timespec64 bch2_time_to_timespec(const struct bch_fs *c, s64 time)
 {
 	struct timespec64 t;
+	s64 sec;
 	s32 rem;
 
 	time += c->sb.time_base_lo;
 
-	t.tv_sec = div_s64_rem(time, c->sb.time_units_per_sec, &rem);
-	t.tv_nsec = rem * c->sb.nsec_per_time_unit;
+	sec = div_s64_rem(time, c->sb.time_units_per_sec, &rem);
+
+	set_normalized_timespec64(&t, sec, rem * (s64)c->sb.nsec_per_time_unit);
+
 	return t;
 }
 
