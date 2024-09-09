ede5bbe488d162bcd572880e58f9044c9df84050
Nikita Shubin <nikita.shubin@maquefel.me>
2024-09-09 11:10:27 +0300

    ARM: ep93xx: add regmap aux_dev
    The following driver's should be instantiated by ep93xx syscon driver:
    
    - reboot
    - pinctrl
    - clock
    
    They all require access to DEVCFG register with a shared lock held, to
    avoid conflict writing to swlocked parts of DEVCFG.
    
    Provide common resources such as base, regmap and spinlock via auxiliary
    bus framework.
    
    Signed-off-by: Nikita Shubin <nikita.shubin@maquefel.me>
    Tested-by: Alexander Sverdlin <alexander.sverdlin@gmail.com>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Reviewed-by: Stephen Boyd <sboyd@kernel.org>
    Acked-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/soc/cirrus/ep93xx.h
+++ include/linux/soc/cirrus/ep93xx.h
@@ -3,12 +3,38 @@
 #define _SOC_EP93XX_H
 
 struct platform_device;
+struct regmap;
+struct spinlock_t;
+
+enum ep93xx_soc_model {
+	EP93XX_9301_SOC,
+	EP93XX_9307_SOC,
+	EP93XX_9312_SOC,
+};
+
+#include <linux/auxiliary_bus.h>
+#include <linux/compiler_types.h>
+#include <linux/container_of.h>
 
 #define EP93XX_CHIP_REV_D0	3
 #define EP93XX_CHIP_REV_D1	4
 #define EP93XX_CHIP_REV_E0	5
 #define EP93XX_CHIP_REV_E1	6
 #define EP93XX_CHIP_REV_E2	7
+
+struct ep93xx_regmap_adev {
+	struct auxiliary_device adev;
+	struct regmap *map;
+	void __iomem *base;
+	spinlock_t *lock;
+	void (*write)(struct regmap *map, spinlock_t *lock, unsigned int reg,
+		      unsigned int val);
+	void (*update_bits)(struct regmap *map, spinlock_t *lock,
+			    unsigned int reg, unsigned int mask, unsigned int val);
+};
+
+#define to_ep93xx_regmap_adev(_adev) \
+	container_of((_adev), struct ep93xx_regmap_adev, adev)
 
 #ifdef CONFIG_ARCH_EP93XX
 int ep93xx_pwm_acquire_gpio(struct platform_device *pdev);
