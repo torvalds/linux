17a1d7c5a49cae46b46a36eb4c4ee0add1565488
Philipp Hortmann <philipp.g.hortmann@gmail.com>
2024-09-10 07:58:04 +0200

    staging: rtl8723bs: Remove unused file rtw_rf.c
    Remove unused file rtw_rf.c
    
    Signed-off-by: Philipp Hortmann <philipp.g.hortmann@gmail.com>
    Link: https://lore.kernel.org/r/2a31866e3e6f65e90d612f7473501066f6bc5937.1725826273.git.philipp.g.hortmann@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/core/rtw_rf.c
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0
-/******************************************************************************
- *
- * Copyright(c) 2007 - 2011 Realtek Corporation. All rights reserved.
- *
- ******************************************************************************/
-
-#include <drv_types.h>
-#include <linux/kernel.h>
