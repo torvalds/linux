558e8b82a52c5ca9f54f4470cc67d8ec0b9e9b1c
Nikunj Kela <quic_nkela@quicinc.com>
2024-09-10 10:15:33 -0700

    soc: qcom: socinfo: add support for SA8255P
    Add SocInfo support for SA8255P.
    
    Reviewed-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Signed-off-by: Nikunj Kela <quic_nkela@quicinc.com>
    Link: https://lore.kernel.org/r/20240910171534.2412263-3-quic_nkela@quicinc.com
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -425,6 +425,7 @@ static const struct soc_id soc_id[] = {
 	{ qcom_board_id(SAR2130P) },
 	{ qcom_board_id(SM8475) },
 	{ qcom_board_id(SM8475P) },
+	{ qcom_board_id(SA8255P) },
 	{ qcom_board_id(SA8775P) },
 	{ qcom_board_id(QRU1000) },
 	{ qcom_board_id(SM8475_2) },
