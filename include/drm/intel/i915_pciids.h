66b281fd8e599ddd7a00a89dc0dcfb7a13411441
Dnyaneshwar Bhadane <dnyaneshwar.bhadane@intel.com>
2024-09-10 11:53:01 +0530

    drm/i915/pciid: Add new PCI id for ARL
    Add new PCI id for ARL platform.
    
    Signed-off-by: Dnyaneshwar Bhadane <dnyaneshwar.bhadane@intel.com>
    Reviewed-by: Nemesa Garg <nemesa.garg@intel.com>
    Reviewed-by: Sai Teja Pottumuttu <sai.teja.pottumuttu@intel.com>
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240910062301.2006782-1-dnyaneshwar.bhadane@intel.com

--- include/drm/intel/i915_pciids.h
+++ include/drm/intel/i915_pciids.h
@@ -769,7 +769,8 @@
 	MACRO__(0x7D41, ## __VA_ARGS__), \
 	MACRO__(0x7D51, ## __VA_ARGS__), \
 	MACRO__(0x7D67, ## __VA_ARGS__), \
-	MACRO__(0x7DD1, ## __VA_ARGS__)
+	MACRO__(0x7DD1, ## __VA_ARGS__), \
+	MACRO__(0xB640, ## __VA_ARGS__)
 
 /* MTL */
 #define INTEL_MTL_IDS(MACRO__, ...) \
