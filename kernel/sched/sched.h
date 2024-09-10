d62f06180937529282e4f8f6a3ef4e2f48cdb18e
Greg Kroah-Hartman <gregkh@google.com>
2024-09-10 16:44:03 +0000

    Revert "sched: Move psi_account_irqtime() out of update_rq_clock_task() hotpath"
    This reverts commit bfaf0990f14e2b14f4b2761e0b76544d310d21ab which is
    commit ddae0ca2a8fe12d0e24ab10ba759c3fbd755ada8 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I8853ca9becfb9c32a846a41d5866dc4070a15b90
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1112,7 +1112,6 @@ struct rq {
 
 #ifdef CONFIG_IRQ_TIME_ACCOUNTING
 	u64			prev_irq_time;
-	u64			psi_irq_time;
 #endif
 #ifdef CONFIG_PARAVIRT
 	u64			prev_steal_time;
--- kernel/sched/stats.h
+++ kernel/sched/stats.h
@@ -110,12 +110,8 @@ __schedstats_from_se(struct sched_entity *se)
 void psi_task_change(struct task_struct *task, int clear, int set);
 void psi_task_switch(struct task_struct *prev, struct task_struct *next,
 		     bool sleep);
-#ifdef CONFIG_IRQ_TIME_ACCOUNTING
-void psi_account_irqtime(struct rq *rq, struct task_struct *curr, struct task_struct *prev);
-#else
-static inline void psi_account_irqtime(struct rq *rq, struct task_struct *curr,
-				       struct task_struct *prev) {}
-#endif /*CONFIG_IRQ_TIME_ACCOUNTING */
+void psi_account_irqtime(struct task_struct *task, u32 delta);
+
 /*
  * PSI tracks state that persists across sleeps, such as iowaits and
  * memory stalls. As a result, it has to distinguish between sleeps,
@@ -210,8 +206,7 @@ static inline void psi_ttwu_dequeue(struct task_struct *p) {}
 static inline void psi_sched_switch(struct task_struct *prev,
 				    struct task_struct *next,
 				    bool sleep) {}
-static inline void psi_account_irqtime(struct rq *rq, struct task_struct *curr,
-				       struct task_struct *prev) {}
+static inline void psi_account_irqtime(struct task_struct *task, u32 delta) {}
 #endif /* CONFIG_PSI */
 
 #ifdef CONFIG_SCHED_INFO
