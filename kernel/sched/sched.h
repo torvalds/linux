902d67a2d40f5b0815f4f627b26d91f96cc51fb3
Tejun Heo <tj@kernel.org>
2024-09-11 09:36:43 -1000

    sched: Move update_other_load_avgs() to kernel/sched/pelt.c
    96fd6c65efc6 ("sched: Factor out update_other_load_avgs() from
    __update_blocked_others()") added update_other_load_avgs() in
    kernel/sched/syscalls.c right above effective_cpu_util(). This location
    didn't fit that well in the first place, and with 5d871a63997f ("sched/fair:
    Move effective_cpu_util() and effective_cpu_util() in fair.c") moving
    effective_cpu_util() to kernel/sched/fair.c, it looks even more out of
    place.
    
    Relocate the function to kernel/sched/pelt.c where all its callees are.
    
    No functional changes.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@redhat.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -3245,8 +3245,6 @@ static inline void cpufreq_update_util(struct rq *rq, unsigned int flags) { }
 
 #ifdef CONFIG_SMP
 
-bool update_other_load_avgs(struct rq *rq);
-
 unsigned long effective_cpu_util(int cpu, unsigned long util_cfs,
 				 unsigned long *min,
 				 unsigned long *max);
