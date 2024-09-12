056cd1481b281e36f8ec3ac055bdb94df9832541
Sayyad Abid <sayyad.abid16@gmail.com>
2024-09-12 09:34:08 +0530

    staging: rtl8723bs: include: Fix trailing */ position in rtw_security.h
    This change enhances readability and conforms to the standard commenting
    style in the kernel.
    
    Reported by `checkpatch.pl`:
    WARNING: trailing `*/` should be on a separate line.
    
    Signed-off-by: Sayyad Abid <sayyad.abid16@gmail.com>
    Tested-by: Philipp Hortmann <philipp.g.hortmann@gmail.com>
    Link: https://lore.kernel.org/r/20240912040409.3315067-8-sayyad.abid16@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/include/rtw_security.h
+++ drivers/staging/rtl8723bs/include/rtw_security.h
@@ -240,7 +240,8 @@ struct mic_data {
 /* ===== start - public domain SHA256 implementation ===== */
 
 /* This is based on SHA256 implementation in LibTomCrypt that was released into
- * public domain by Tom St Denis. */
+ * public domain by Tom St Denis.
+ */
 
 int omac1_aes_128(u8 *key, u8 *data, size_t data_len, u8 *mac);
 void rtw_secmicsetkey(struct mic_data *pmicdata, u8 *key);
