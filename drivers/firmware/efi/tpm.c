a066397e8ed1036e8b959050ab6e830ee90d9f58
Gregory Price <gourry@gourry.net>
2024-09-13 19:19:53 -0400

    tpm: fix unsigned/signed mismatch errors related to __calc_tpm2_event_size
    __calc_tpm2_event_size returns 0 or a positive length, but return values
    are often interpreted as ints.  Convert everything over to u32 to avoid
    signed/unsigned logic errors.
    
    Signed-off-by: Gregory Price <gourry@gourry.net>
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- drivers/firmware/efi/tpm.c
+++ drivers/firmware/efi/tpm.c
@@ -19,7 +19,7 @@ EXPORT_SYMBOL(efi_tpm_final_log_size);
 static int __init tpm2_calc_event_log_size(void *data, int count, void *size_info)
 {
 	struct tcg_pcr_event2_head *header;
-	int event_size, size = 0;
+	u32 event_size, size = 0;
 
 	while (count > 0) {
 		header = data + size;
