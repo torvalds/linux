3b5edc0421e2598a0ae7f0adcd592017f37e3cdf
Dapeng Mi <dapeng1.mi@linux.intel.com>
2024-09-13 08:47:09 +0000

    perf x86/topdown: Don't move topdown metric events in group
    when running below perf command, we say error is reported.
    
    perf record -e "{slots,instructions,topdown-retiring}:S" -vv -C0 sleep 1
    
    ------------------------------------------------------------
    perf_event_attr:
      type                             4 (cpu)
      size                             168
      config                           0x400 (slots)
      sample_type                      IP|TID|TIME|READ|CPU|PERIOD|IDENTIFIER
      read_format                      ID|GROUP|LOST
      disabled                         1
      sample_id_all                    1
      exclude_guest                    1
    ------------------------------------------------------------
    sys_perf_event_open: pid -1  cpu 0  group_fd -1  flags 0x8 = 5
    ------------------------------------------------------------
    perf_event_attr:
      type                             4 (cpu)
      size                             168
      config                           0x8000 (topdown-retiring)
      { sample_period, sample_freq }   4000
      sample_type                      IP|TID|TIME|READ|CPU|PERIOD|IDENTIFIER
      read_format                      ID|GROUP|LOST
      freq                             1
      sample_id_all                    1
      exclude_guest                    1
    ------------------------------------------------------------
    sys_perf_event_open: pid -1  cpu 0  group_fd 5  flags 0x8
    sys_perf_event_open failed, error -22
    
    Error:
    The sys_perf_event_open() syscall returned with 22 (Invalid argument) for
    event (topdown-retiring).
    
    The reason of error is that the events are regrouped and
    topdown-retiring event is moved to closely after the slots event and
    topdown-retiring event needs to do the sampling, but Intel PMU driver
    doesn't support to sample topdown metrics events.
    
    For topdown metrics events, it just requires to be in a group which has
    slots event as leader. It doesn't require topdown metrics event must be
    closely after slots event. Thus it's a overkill to move topdown metrics
    event closely after slots event in events regrouping and furtherly cause
    the above issue.
    
    Thus don't move topdown metrics events forward if they are already in a
    group.
    
    Reviewed-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Cc: Yongwei Ma <yongwei.ma@intel.com>
    Link: https://lore.kernel.org/r/20240913084712.13861-4-dapeng1.mi@linux.intel.com
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/arch/x86/util/evlist.c
+++ tools/perf/arch/x86/util/evlist.c
@@ -7,6 +7,61 @@
 
 int arch_evlist__cmp(const struct evsel *lhs, const struct evsel *rhs)
 {
+	/*
+	 * Currently the following topdown events sequence are supported to
+	 * move and regroup correctly.
+	 *
+	 * a. all events in a group
+	 *    perf stat -e "{instructions,topdown-retiring,slots}" -C0 sleep 1
+	 *    WARNING: events were regrouped to match PMUs
+	 *     Performance counter stats for 'CPU(s) 0':
+	 *          15,066,240     slots
+	 *          1,899,760      instructions
+	 *          2,126,998      topdown-retiring
+	 * b. all events not in a group
+	 *    perf stat -e "instructions,topdown-retiring,slots" -C0 sleep 1
+	 *    WARNING: events were regrouped to match PMUs
+	 *     Performance counter stats for 'CPU(s) 0':
+	 *          2,045,561      instructions
+	 *          17,108,370     slots
+	 *          2,281,116      topdown-retiring
+	 * c. slots event in a group but topdown metrics events outside the group
+	 *    perf stat -e "{instructions,slots},topdown-retiring" -C0 sleep 1
+	 *    WARNING: events were regrouped to match PMUs
+	 *     Performance counter stats for 'CPU(s) 0':
+	 *         20,323,878      slots
+	 *          2,634,884      instructions
+	 *          3,028,656      topdown-retiring
+	 * d. slots event and topdown metrics events in two groups
+	 *    perf stat -e "{instructions,slots},{topdown-retiring}" -C0 sleep 1
+	 *    WARNING: events were regrouped to match PMUs
+	 *     Performance counter stats for 'CPU(s) 0':
+	 *         26,319,024      slots
+	 *          2,427,791      instructions
+	 *          2,683,508      topdown-retiring
+	 *
+	 * If slots event and topdown metrics events are not in same group, the
+	 * topdown metrics events must be first event after the slots event group,
+	 * otherwise topdown metrics events can't be regrouped correctly, e.g.
+	 *
+	 * a. perf stat -e "{instructions,slots},cycles,topdown-retiring" -C0 sleep 1
+	 *    WARNING: events were regrouped to match PMUs
+	 *     Performance counter stats for 'CPU(s) 0':
+	 *         17,923,134      slots
+	 *          2,154,855      instructions
+	 *          3,015,058      cycles
+	 *    <not supported>      topdown-retiring
+	 *
+	 * if slots event and topdown metrics events are in two groups, the group which
+	 * has topdown metrics events must contain only the topdown metrics event,
+	 * otherwise topdown metrics event can't be regrouped correctly as well, e.g.
+	 *
+	 * a. perf stat -e "{instructions,slots},{topdown-retiring,cycles}" -C0 sleep 1
+	 *    WARNING: events were regrouped to match PMUs
+	 *    Error:
+	 *    The sys_perf_event_open() syscall returned with 22 (Invalid argument) for
+	 *    event (topdown-retiring)
+	 */
 	if (topdown_sys_has_perf_metrics() &&
 	    (arch_evsel__must_be_in_group(lhs) || arch_evsel__must_be_in_group(rhs))) {
 		/* Ensure the topdown slots comes first. */
@@ -17,7 +72,12 @@ int arch_evlist__cmp(const struct evsel *lhs, const struct evsel *rhs)
 		/* Followed by topdown events. */
 		if (arch_is_topdown_metrics(lhs) && !arch_is_topdown_metrics(rhs))
 			return -1;
-		if (!arch_is_topdown_metrics(lhs) && arch_is_topdown_metrics(rhs))
+		/*
+		 * Move topdown events forward only when topdown events
+		 * are not in same group with previous event.
+		 */
+		if (!arch_is_topdown_metrics(lhs) && arch_is_topdown_metrics(rhs) &&
+		    lhs->core.leader != rhs->core.leader)
 			return 1;
 	}
 
