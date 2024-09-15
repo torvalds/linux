ac681835b6747fc5a0cd40398d4c28210318df32
Alice Ryhl <aliceryhl@google.com>
2024-09-15 14:31:34 +0000

    rust: file: add abstraction for `poll_table`
    The existing `CondVar` abstraction is a wrapper around
    `wait_queue_head`, but it does not support all use-cases of the C
    `wait_queue_head` type. To be specific, a `CondVar` cannot be registered
    with a `struct poll_table`. This limitation has the advantage that you
    do not need to call `synchronize_rcu` when destroying a `CondVar`.
    
    However, we need the ability to register a `poll_table` with a
    `wait_queue_head` in Rust Binder. To enable this, introduce a type
    called `PollCondVar`, which is like `CondVar` except that you can
    register a `poll_table`. We also introduce `PollTable`, which is a safe
    wrapper around `poll_table` that is intended to be used with
    `PollCondVar`.
    
    The destructor of `PollCondVar` unconditionally calls `synchronize_rcu`
    to ensure that the removal of epoll waiters has fully completed before
    the `wait_queue_head` is destroyed.
    
    That said, `synchronize_rcu` is rather expensive and is not needed in
    all cases: If we have never registered a `poll_table` with the
    `wait_queue_head`, then we don't need to call `synchronize_rcu`. (And
    this is a common case in Binder - not all processes use Binder with
    epoll.) The current implementation does not account for this, but if we
    find that it is necessary to improve this, a future patch could store a
    boolean next to the `wait_queue_head` to keep track of whether a
    `poll_table` has ever been registered.
    
    Reviewed-by: Benno Lossin <benno.lossin@proton.me>
    Reviewed-by: Martin Rodriguez Reboredo <yakoyoku@gmail.com>
    Reviewed-by: Trevor Gross <tmgross@umich.edu>
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Link: https://lore.kernel.org/r/20240915-alice-file-v10-8-88484f7a3dcf@google.com
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -20,6 +20,7 @@
 #include <linux/mdio.h>
 #include <linux/phy.h>
 #include <linux/pid_namespace.h>
+#include <linux/poll.h>
 #include <linux/refcount.h>
 #include <linux/sched.h>
 #include <linux/security.h>
