0ee4dcafda9576910559f0471a3d6891daf9ab92
Zijun Hu <quic_zijuhu@quicinc.com>
2024-09-18 22:48:13 +0800

    lib: devres: Simplify API devm_iounmap() implementation
    Simplify devm_iounmap() implementation by dedicated API devres_release()
    compared with current solution, namely, devres_destroy() + iounmap()
    devres_release() has the following advantages:
    
    - it is simpler if devm_iounmap()'s parameter @addr is valid, namely
      @addr was ever returned by one of devm_ioremap() variants.
    
    - it can avoid unnecessary iounmap(@addr) if @addr is not valid.
    
    Signed-off-by: Zijun Hu <quic_zijuhu@quicinc.com>
    Link: https://lore.kernel.org/r/20240918-fix_lib_devres-v1-1-e696ab5486e6@quicinc.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- lib/devres.c
+++ lib/devres.c
@@ -115,9 +115,8 @@ EXPORT_SYMBOL(devm_ioremap_wc);
  */
 void devm_iounmap(struct device *dev, void __iomem *addr)
 {
-	WARN_ON(devres_destroy(dev, devm_ioremap_release, devm_ioremap_match,
+	WARN_ON(devres_release(dev, devm_ioremap_release, devm_ioremap_match,
 			       (__force void *)addr));
-	iounmap(addr);
 }
 EXPORT_SYMBOL(devm_iounmap);
 
