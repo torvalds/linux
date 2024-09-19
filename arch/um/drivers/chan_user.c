fdb2ecd35d327a1fc6bba69b97f85b494e1f4b6b
Benjamin Berg <benjamin.berg@intel.com>
2024-09-19 14:45:05 +0200

    um: Set parent death signal for winch thread/process
    The winch "thread" is really a separate process. Using prctl to set
    PR_SET_PDEATHSIG ensures that this separate thread will be killed if the
    UML kernel itself dies unexpectedly and does not perform proper cleanup.
    
    Signed-off-by: Benjamin Berg <benjamin.berg@intel.com>
    Link: https://patch.msgid.link/20240919124511.282088-5-benjamin@sipsolutions.net
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- arch/um/drivers/chan_user.c
+++ arch/um/drivers/chan_user.c
@@ -10,6 +10,7 @@
 #include <signal.h>
 #include <termios.h>
 #include <sys/ioctl.h>
+#include <sys/prctl.h>
 #include "chan_user.h"
 #include <os.h>
 #include <um_malloc.h>
@@ -160,6 +161,8 @@ static __noreturn int winch_thread(void *arg)
 	int pty_fd, pipe_fd;
 	int count;
 	char c = 1;
+
+	prctl(PR_SET_PDEATHSIG, SIGKILL);
 
 	pty_fd = data->pty_fd;
 	pipe_fd = data->pipe_fd;
