a70ef3d4d7d00cc65232417db2635bda165b6589
Namjae Jeon <linkinjeon@kernel.org>
2024-09-19 09:22:57 +0900

    ksmbd: fix warning: comparison of distinct pointer types lacks a cast
    [ Upstream commit 289ebd9afeb94862d96c89217068943f1937df5b ]
    
    smb2pdu.c: In function ‘smb2_open’:
    ./include/linux/minmax.h:20:28: warning: comparison of distinct
    pointer types lacks a cast
       20 |  (!!(sizeof((typeof(x) *)1 == (typeof(y) *)1)))
          |                            ^~
    ./include/linux/minmax.h:26:4: note: in expansion of macro ‘__typecheck’
       26 |   (__typecheck(x, y) && __no_side_effects(x, y))
          |    ^~~~~~~~~~~
    ./include/linux/minmax.h:36:24: note: in expansion of macro ‘__safe_cmp’
       36 |  __builtin_choose_expr(__safe_cmp(x, y), \
          |                        ^~~~~~~~~~
    ./include/linux/minmax.h:45:19: note: in expansion of macro ‘__careful_cmp’
       45 | #define min(x, y) __careful_cmp(x, y, <)
          |                   ^~~~~~~~~~~~~
    /home/linkinjeon/git/smbd_work/ksmbd/smb2pdu.c:3713:27: note: in
    expansion of macro ‘min’
     3713 |     fp->durable_timeout = min(dh_info.timeout,
    
    Fixes: c8efcc786146 ("ksmbd: add support for durable handles v1/v2")
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/smb/server/smb2pdu.c
+++ fs/smb/server/smb2pdu.c
@@ -3531,8 +3531,9 @@ int smb2_open(struct ksmbd_work *work)
 			memcpy(fp->create_guid, dh_info.CreateGuid,
 					SMB2_CREATE_GUID_SIZE);
 			if (dh_info.timeout)
-				fp->durable_timeout = min(dh_info.timeout,
-						DURABLE_HANDLE_MAX_TIMEOUT);
+				fp->durable_timeout =
+					min_t(unsigned int, dh_info.timeout,
+					      DURABLE_HANDLE_MAX_TIMEOUT);
 			else
 				fp->durable_timeout = 60;
 		}
