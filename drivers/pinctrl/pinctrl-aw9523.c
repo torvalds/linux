af8e4812288181f9a9af27b1df5da366bdcce740
Uwe Kleine-König <u.kleine-koenig@baylibre.com>
2024-09-20 17:34:31 +0200

    pinctrl: aw9523: Drop explicit initialization of struct i2c_device_id::driver_data to 0
    These drivers don't use the driver_data member of struct i2c_device_id,
    so don't explicitly initialize this member.
    
    This prepares putting driver_data in an anonymous union which requires
    either no initialization or named designators. But it's also a nice
    cleanup on its own.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@baylibre.com>
    Link: https://lore.kernel.org/20240920153430.503212-13-u.kleine-koenig@baylibre.com
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-aw9523.c
+++ drivers/pinctrl/pinctrl-aw9523.c
@@ -1053,7 +1053,7 @@ static void aw9523_remove(struct i2c_client *client)
 }
 
 static const struct i2c_device_id aw9523_i2c_id_table[] = {
-	{ "aw9523_i2c", 0 },
+	{ "aw9523_i2c" },
 	{ }
 };
 MODULE_DEVICE_TABLE(i2c, aw9523_i2c_id_table);
