7eb4a319db65566005989121563ead344ca79140
Kent Overstreet <kent.overstreet@linux.dev>
2024-09-23 16:39:49 -0400

    bcachefs: Fix infinite loop in propagate_key_to_snapshot_leaves()
    As we iterate we need to mark that we no longer need iterators -
    otherwise we'll infinite loop via the "too many iters" check when
    there's many snapshots.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/snapshot.c
+++ fs/bcachefs/snapshot.c
@@ -1784,6 +1784,7 @@ static int bch2_propagate_key_to_snapshot_leaf(struct btree_trans *trans,
 	new->k.p.snapshot = leaf_id;
 	ret = bch2_trans_update(trans, &iter, new, 0);
 out:
+	bch2_set_btree_iter_dontneed(&iter);
 	bch2_trans_iter_exit(trans, &iter);
 	return ret;
 }
