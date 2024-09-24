df0cc9eaff170edf294cc6c6c46caf9e0946b515
Thomas Zimmermann <tzimmermann@suse.de>
2024-09-24 09:12:15 +0200

    drm/ili9341: Run DRM default client setup
    Call drm_client_setup() to run the kernel's default client setup
    for DRM. Set fbdev_probe in struct drm_driver, so that the client
    setup can start the common fbdev client.
    
    v5:
    - select DRM_CLIENT_SELECTION
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Acked-by: Javier Martinez Canillas <javierm@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240924071734.98201-18-tzimmermann@suse.de

--- drivers/gpu/drm/tiny/ili9341.c
+++ drivers/gpu/drm/tiny/ili9341.c
@@ -16,6 +16,7 @@
 #include <linux/spi/spi.h>
 
 #include <drm/drm_atomic_helper.h>
+#include <drm/drm_client_setup.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_fbdev_dma.h>
 #include <drm/drm_gem_atomic_helper.h>
@@ -150,6 +151,7 @@ static const struct drm_driver ili9341_driver = {
 	.driver_features	= DRIVER_GEM | DRIVER_MODESET | DRIVER_ATOMIC,
 	.fops			= &ili9341_fops,
 	DRM_GEM_DMA_DRIVER_OPS_VMAP,
+	DRM_FBDEV_DMA_DRIVER_OPS,
 	.debugfs_init		= mipi_dbi_debugfs_init,
 	.name			= "ili9341",
 	.desc			= "Ilitek ILI9341",
@@ -218,7 +220,7 @@ static int ili9341_probe(struct spi_device *spi)
 
 	spi_set_drvdata(spi, drm);
 
-	drm_fbdev_dma_setup(drm, 0);
+	drm_client_setup(drm, NULL);
 
 	return 0;
 }
