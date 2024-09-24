9027204d5a7ef055af2e415ad58fd2774f343617
Kalesh Singh <kaleshsingh@google.com>
2024-09-24 23:28:56 +0000

    ANDROID: 16K: Introduce pgsize_migration_inline.h
    Introduce inline header to avoid circular dependency. This
    will be used in a subsequent patch.
    
    Also take opportunity to do some small noop refactor in
    vma_pad_pages() and split_pad_vma() for more robust code.
    
    Bug: 357901498
    Change-Id: Ia5f447758d0d07ed3e1429ca1e35dcc0741cc22a
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- include/linux/pgsize_migration.h
+++ include/linux/pgsize_migration.h
@@ -13,35 +13,9 @@
  * page size in Android.
  */
 
-#include <linux/mm.h>
+#include <linux/pgsize_migration_inline.h>
 #include <linux/seq_file.h>
-#include <linux/sizes.h>
-
-/*
- * vm_flags representation of VMA padding pages.
- *
- * This allows the kernel to identify the portion of an ELF LOAD segment VMA
- * that is padding.
- *
- * 4 high bits of vm_flags [63,60] are used to represent ELF segment padding
- * up to 60kB, which is sufficient for ELFs of both 16kB and 64kB segment
- * alignment (p_align).
- *
- * The representation is illustrated below.
- *
- *                    63        62        61        60
- *                _________ _________ _________ _________
- *               |  Bit 3  |  Bit 2  |  Bit 1  |  Bit 0  |
- *               | of  4kB | of  4kB | of  4kB | of  4kB |
- *               |  chunks |  chunks |  chunks |  chunks |
- *               |_________|_________|_________|_________|
- */
-
-#define VM_PAD_WIDTH		4
-#define VM_PAD_SHIFT		(BITS_PER_LONG - VM_PAD_WIDTH)
-#define VM_TOTAL_PAD_PAGES	((1ULL << VM_PAD_WIDTH) - 1)
-#define VM_PAD_MASK		(VM_TOTAL_PAD_PAGES << VM_PAD_SHIFT)
-#define VMA_PAD_START(vma)	(vma->vm_end - (vma_pad_pages(vma) << PAGE_SHIFT))
+#include <linux/mm.h>
 
 #if PAGE_SIZE == SZ_4K && defined(CONFIG_64BIT)
 extern void vma_set_pad_pages(struct vm_area_struct *vma,
@@ -62,9 +36,6 @@ extern void show_map_pad_vma(struct vm_area_struct *vma,
 
 extern void split_pad_vma(struct vm_area_struct *vma, struct vm_area_struct *new,
 			  unsigned long addr, int new_below);
-
-extern unsigned long vma_pad_fixup_flags(struct vm_area_struct *vma,
-					 unsigned long newflags);
 
 extern bool is_mergable_pad_vma(struct vm_area_struct *vma,
 				unsigned long vm_flags);
@@ -105,12 +76,6 @@ static inline void show_map_pad_vma(struct vm_area_struct *vma,
 static inline void split_pad_vma(struct vm_area_struct *vma, struct vm_area_struct *new,
 				 unsigned long addr, int new_below)
 {
-}
-
-static inline unsigned long vma_pad_fixup_flags(struct vm_area_struct *vma,
-						unsigned long newflags)
-{
-	return newflags;
 }
 
 static inline bool is_mergable_pad_vma(struct vm_area_struct *vma,
