38fbefeb2c140b581ed7de8117a5c90d6dd89c22
Carlos Llamas <cmllamas@google.com>
2024-09-26 23:36:12 +0000

    binder: fix node UAF in binder_add_freeze_work()
    commit dc8aea47b928cc153b591b3558829ce42f685074 upstream.
    
    In binder_add_freeze_work() we iterate over the proc->nodes with the
    proc->inner_lock held. However, this lock is temporarily dropped in
    order to acquire the node->lock first (lock nesting order). This can
    race with binder_node_release() and trigger a use-after-free:
    
      ==================================================================
      BUG: KASAN: slab-use-after-free in _raw_spin_lock+0xe4/0x19c
      Write of size 4 at addr ffff53c04c29dd04 by task freeze/640
    
      CPU: 5 UID: 0 PID: 640 Comm: freeze Not tainted 6.11.0-07343-ga727812a8d45 #17
      Hardware name: linux,dummy-virt (DT)
      Call trace:
       _raw_spin_lock+0xe4/0x19c
       binder_add_freeze_work+0x148/0x478
       binder_ioctl+0x1e70/0x25ac
       __arm64_sys_ioctl+0x124/0x190
    
      Allocated by task 637:
       __kmalloc_cache_noprof+0x12c/0x27c
       binder_new_node+0x50/0x700
       binder_transaction+0x35ac/0x6f74
       binder_thread_write+0xfb8/0x42a0
       binder_ioctl+0x18f0/0x25ac
       __arm64_sys_ioctl+0x124/0x190
    
      Freed by task 637:
       kfree+0xf0/0x330
       binder_thread_read+0x1e88/0x3a68
       binder_ioctl+0x16d8/0x25ac
       __arm64_sys_ioctl+0x124/0x190
      ==================================================================
    
    Fix the race by taking a temporary reference on the node before
    releasing the proc->inner lock. This ensures the node remains alive
    while in use.
    
    Fixes: d579b04a52a1 ("binder: frozen notification")
    Cc: stable@vger.kernel.org
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Acked-by: Todd Kjos <tkjos@google.com>
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    Link: https://lore.kernel.org/r/20240926233632.821189-2-cmllamas@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -5552,6 +5552,7 @@ static bool binder_txns_pending_ilocked(struct binder_proc *proc)
 
 static void binder_add_freeze_work(struct binder_proc *proc, bool is_frozen)
 {
+	struct binder_node *prev = NULL;
 	struct rb_node *n;
 	struct binder_ref *ref;
 
@@ -5560,7 +5561,10 @@ static void binder_add_freeze_work(struct binder_proc *proc, bool is_frozen)
 		struct binder_node *node;
 
 		node = rb_entry(n, struct binder_node, rb_node);
+		binder_inc_node_tmpref_ilocked(node);
 		binder_inner_proc_unlock(proc);
+		if (prev)
+			binder_put_node(prev);
 		binder_node_lock(node);
 		hlist_for_each_entry(ref, &node->refs, node_entry) {
 			/*
@@ -5586,10 +5590,13 @@ static void binder_add_freeze_work(struct binder_proc *proc, bool is_frozen)
 			}
 			binder_inner_proc_unlock(ref->proc);
 		}
+		prev = node;
 		binder_node_unlock(node);
 		binder_inner_proc_lock(proc);
 	}
 	binder_inner_proc_unlock(proc);
+	if (prev)
+		binder_put_node(prev);
 }
 
 static int binder_ioctl_freeze(struct binder_freeze_info *info,
