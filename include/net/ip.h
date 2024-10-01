997ba8889611891f91e8ad83583466aeab6239a3
Eric Dumazet <edumazet@google.com>
2024-10-01 15:04:04 +0000

    icmp: change the order of rate limits
    commit 8c2bd38b95f75f3d2a08c93e35303e26d480d24e upstream.
    
    ICMP messages are ratelimited :
    
    After the blamed commits, the two rate limiters are applied in this order:
    
    1) host wide ratelimit (icmp_global_allow())
    
    2) Per destination ratelimit (inetpeer based)
    
    In order to avoid side-channels attacks, we need to apply
    the per destination check first.
    
    This patch makes the following change :
    
    1) icmp_global_allow() checks if the host wide limit is reached.
       But credits are not yet consumed. This is deferred to 3)
    
    2) The per destination limit is checked/updated.
       This might add a new node in inetpeer tree.
    
    3) icmp_global_consume() consumes tokens if prior operations succeeded.
    
    This means that host wide ratelimit is still effective
    in keeping inetpeer tree small even under DDOS.
    
    As a bonus, I removed icmp_global.lock as the fast path
    can use a lock-free operation.
    
    Fixes: c0303efeab73 ("net: reduce cycles spend on ICMP replies that gets rate limited")
    Fixes: 4cdf507d5452 ("icmp: add a global rate limitation")
    Reported-by: Keyu Man <keyu.man@email.ucr.edu>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: David Ahern <dsahern@kernel.org>
    Cc: Jesper Dangaard Brouer <hawk@kernel.org>
    Cc: stable@vger.kernel.org
    Link: https://patch.msgid.link/20240829144641.3880376-2-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/net/ip.h
+++ include/net/ip.h
@@ -781,6 +781,8 @@ static inline void ip_cmsg_recv(struct msghdr *msg, struct sk_buff *skb)
 }
 
 bool icmp_global_allow(void);
+void icmp_global_consume(void);
+
 extern int sysctl_icmp_msgs_per_sec;
 extern int sysctl_icmp_msgs_burst;
 
