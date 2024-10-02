d18dba06f9f334a7e6a33f239a4f98e8f66208b5
Alex Deucher <alexander.deucher@amd.com>
2024-10-02 10:22:30 -0400

    drm/amdgpu/swsmu: Only force workload setup on init
    commit cb07c8338fc2b9d5f949a19d4a07ee4d5ecf8793 upstream.
    
    Needed to set the workload type at init time so that
    we can apply the navi3x margin optimization.
    
    Closes: https://gitlab.freedesktop.org/drm/amd/-/issues/3618
    Link: https://gitlab.freedesktop.org/drm/amd/-/issues/3131
    Fixes: c50fe289ed72 ("drm/amdgpu/swsmu: always force a state reprogram on init")
    Reviewed-by: Kenneth Feng <kenneth.feng@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 580ad7cbd4b7be8d2cb5ab5c1fca6bb76045eb0e)
    Cc: stable@vger.kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
+++ drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
@@ -1831,7 +1831,7 @@ static int smu_bump_power_profile_mode(struct smu_context *smu,
 static int smu_adjust_power_state_dynamic(struct smu_context *smu,
 					  enum amd_dpm_forced_level level,
 					  bool skip_display_settings,
-					  bool force_update)
+					  bool init)
 {
 	int ret = 0;
 	int index = 0;
@@ -1860,7 +1860,7 @@ static int smu_adjust_power_state_dynamic(struct smu_context *smu,
 		}
 	}
 
-	if (force_update || smu_dpm_ctx->dpm_level != level) {
+	if (smu_dpm_ctx->dpm_level != level) {
 		ret = smu_asic_set_performance_level(smu, level);
 		if (ret) {
 			dev_err(smu->adev->dev, "Failed to set performance level!");
@@ -1877,7 +1877,7 @@ static int smu_adjust_power_state_dynamic(struct smu_context *smu,
 		index = index > 0 && index <= WORKLOAD_POLICY_MAX ? index - 1 : 0;
 		workload[0] = smu->workload_setting[index];
 
-		if (force_update || smu->power_profile_mode != workload[0])
+		if (init || smu->power_profile_mode != workload[0])
 			smu_bump_power_profile_mode(smu, workload, 0);
 	}
 
