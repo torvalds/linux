ee3283c608dfa21251b0821d7bb198c7ae3189f6
Jeff Layton <jlayton@kernel.org>
2024-10-02 17:27:16 -0400

    timekeeping: Add interfaces for handling timestamps with a floor value
    Multigrain timestamps allow the kernel to use fine-grained timestamps when
    an inode's attributes is being actively observed via ->getattr().  With
    this support, it's possible for a file to get a fine-grained timestamp, and
    another modified after it to get a coarse-grained stamp that is earlier
    than the fine-grained time.  If this happens then the files can appear to
    have been modified in reverse order, which breaks VFS ordering guarantees
    [1].
    
    To prevent this, maintain a floor value for multigrain timestamps.
    Whenever a fine-grained timestamp is handed out, record it, and when later
    coarse-grained stamps are handed out, ensure they are not earlier than that
    value. If the coarse-grained timestamp is earlier than the fine-grained
    floor, return the floor value instead.
    
    Add a static singleton atomic64_t into timekeeper.c that is used to keep
    track of the latest fine-grained time ever handed out. This is tracked as a
    monotonic ktime_t value to ensure that it isn't affected by clock
    jumps. Because it is updated at different times than the rest of the
    timekeeper object, the floor value is managed independently of the
    timekeeper via a cmpxchg() operation, and sits on its own cacheline.
    
    Add two new public interfaces:
    
    - ktime_get_coarse_real_ts64_mg() fills a timespec64 with the later of the
      coarse-grained clock and the floor time
    
    - ktime_get_real_ts64_mg() gets the fine-grained clock value, and tries
      to swap it into the floor. A timespec64 is filled with the result.
    
    The floor value is global and updated via a single try_cmpxchg(). If
    that fails then the operation raced with a concurrent update. Any
    concurrent update must be later than the existing floor value, so any
    racing tasks can accept any resulting floor value without retrying.
    
    [1]: POSIX requires that files be stamped with realtime clock values, and
         makes no provision for dealing with backward clock jumps. If a backward
         realtime clock jump occurs, then files can appear to have been modified
         in reverse order.
    
    Signed-off-by: Jeff Layton <jlayton@kernel.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Randy Dunlap <rdunlap@infradead.org> # documentation bits
    Acked-by: John Stultz <jstultz@google.com>
    Link: https://lore.kernel.org/all/20241002-mgtime-v10-1-d1c4717f5284@kernel.org
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- include/linux/timekeeping.h
+++ include/linux/timekeeping.h
@@ -45,6 +45,10 @@ extern void ktime_get_real_ts64(struct timespec64 *tv);
 extern void ktime_get_coarse_ts64(struct timespec64 *ts);
 extern void ktime_get_coarse_real_ts64(struct timespec64 *ts);
 
+/* Multigrain timestamp interfaces */
+extern void ktime_get_coarse_real_ts64_mg(struct timespec64 *ts);
+extern void ktime_get_real_ts64_mg(struct timespec64 *ts);
+
 void getboottime64(struct timespec64 *ts);
 
 /*
