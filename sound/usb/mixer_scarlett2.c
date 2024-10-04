5c293089af7de1393af9efd2eb3354b10703670e
Geoffrey D. Bennett <g@b4.vu>
2024-10-04 23:58:12 +0930

    ALSA: scarlett2: Fix mixed declarations and code warning
    In scarlett2_compressor_ctl_put(), move the declaration of param to
    the beginning of the function to avoid mixed declarations and code.
    
    Signed-off-by: Geoffrey D. Bennett <g@b4.vu>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Link: https://patch.msgid.link/ede84092737fa2ca7e94371135cc1e42f1731e84.1727971672.git.g@b4.vu

--- sound/usb/mixer_scarlett2.c
+++ sound/usb/mixer_scarlett2.c
@@ -5385,6 +5385,8 @@ static int scarlett2_compressor_ctl_put(
 	int index = elem->control;
 	int channel = index / SCARLETT2_COMPRESSOR_PARAM_COUNT;
 	int param_index = index % SCARLETT2_COMPRESSOR_PARAM_COUNT;
+	const struct compressor_param *param = &compressor_params[param_index];
+
 	int oval, val, err;
 	s32 scaled_val;
 
@@ -5405,8 +5407,6 @@ static int scarlett2_compressor_ctl_put(
 		goto unlock;
 
 	private->compressor_values[index] = val;
-
-	const struct compressor_param *param = &compressor_params[param_index];
 
 	scaled_val = val << param->scale_bits;
 
