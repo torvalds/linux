435f2d87579e2408ab6502248f2270fc3c9e636e
Hans de Goede <hdegoede@redhat.com>
2024-10-05 23:28:17 +0200

    ACPI: resource: Make Asus ExpertBook B2502 matches cover more models
    Like the various 14" Asus ExpertBook B2 B2402* models there are also
    4 variants of the 15" Asus ExpertBook B2 B2502* models:
    
    B2502CBA: 12th gen Intel CPU, non flip
    B2502FBA: 12th gen Intel CPU, flip
    B2502CVA: 13th gen Intel CPU, non flip
    B2502FVA: 13th gen Intel CPU, flip
    
    Currently there already are DMI quirks for the B2502CBA, B2502FBA and
    B2502CVA models. Asus website shows that there also is a B2502FVA.
    
    Rather then adding a 4th quirk fold the 3 existing quirks into a single
    quirk covering B2502* to also cover the last model while at the same time
    reducing the number of quirks.
    
    Cc: All applicable <stable@vger.kernel.org>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://patch.msgid.link/20241005212819.354681-3-hdegoede@redhat.com
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/resource.c
+++ drivers/acpi/resource.c
@@ -490,24 +490,10 @@ static const struct dmi_system_id irq1_level_low_skip_override[] = {
 		},
 	},
 	{
-		/* Asus ExpertBook B2502 */
+		/* Asus ExpertBook B2502 (B2502CBA / B2502FBA / B2502CVA / B2502FVA) */
 		.matches = {
 			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
-			DMI_MATCH(DMI_BOARD_NAME, "B2502CBA"),
-		},
-	},
-	{
-		/* Asus ExpertBook B2502FBA */
-		.matches = {
-			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
-			DMI_MATCH(DMI_BOARD_NAME, "B2502FBA"),
-		},
-	},
-	{
-		/* Asus ExpertBook B2502CVA */
-		.matches = {
-			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
-			DMI_MATCH(DMI_BOARD_NAME, "B2502CVA"),
+			DMI_MATCH(DMI_BOARD_NAME, "B2502"),
 		},
 	},
 	{
