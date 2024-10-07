7bd8870af8dde527e0e8e83838de8966418c58f3
Mark Rutland <mark.rutland@arm.com>
2024-10-07 13:39:16 +0100

    arm64: asm-offsets: remove VMA_VM_*
    The VMA_VM_MM definition is only used by the vma_vm_mm macro, which
    itself is unused. The VMA_VM_FLAGS definition isn't used anywhere.
    
    Remove them all.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20241007123921.549340-3-mark.rutland@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/kernel/asm-offsets.c
+++ arch/arm64/kernel/asm-offsets.c
@@ -104,9 +104,6 @@ int main(void)
 #endif
   DEFINE(MM_CONTEXT_ID,		offsetof(struct mm_struct, context.id.counter));
   BLANK();
-  DEFINE(VMA_VM_MM,		offsetof(struct vm_area_struct, vm_mm));
-  DEFINE(VMA_VM_FLAGS,		offsetof(struct vm_area_struct, vm_flags));
-  BLANK();
   DEFINE(VM_EXEC,	       	VM_EXEC);
   BLANK();
   DEFINE(PAGE_SZ,	       	PAGE_SIZE);
