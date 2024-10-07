69a5d2d0d913cc0190899be7a835a48b0808a5ec
Dharageswari R <dharageswari.r@intel.com>
2024-10-07 15:59:53 +0800

    ASoC: intel: sof_sdw: add RT722 SDCA card for PTL platform
    Enable on-board rt722 based sound card for PTL RVP.
    
    Signed-off-by: Dharageswari R <dharageswari.r@intel.com>
    Reviewed-by: Péter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Link: https://patch.msgid.link/20241007075955.12575-2-yung-chuan.liao@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/common/soc-acpi-intel-ptl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-ptl-match.c
@@ -78,6 +78,15 @@ static const struct snd_soc_acpi_adr_device rt722_0_single_adr[] = {
 	}
 };
 
+static const struct snd_soc_acpi_adr_device rt722_1_single_adr[] = {
+	{
+		.adr = 0x000130025d072201ull,
+		.num_endpoints = ARRAY_SIZE(rt722_endpoints),
+		.endpoints = rt722_endpoints,
+		.name_prefix = "rt722"
+	}
+};
+
 static const struct snd_soc_acpi_adr_device rt722_3_single_adr[] = {
 	{
 		.adr = 0x000330025d072201ull,
@@ -92,6 +101,15 @@ static const struct snd_soc_acpi_link_adr ptl_rt722_only[] = {
 		.mask = BIT(0),
 		.num_adr = ARRAY_SIZE(rt722_0_single_adr),
 		.adr_d = rt722_0_single_adr,
+	},
+	{}
+};
+
+static const struct snd_soc_acpi_link_adr ptl_rt722_l1[] = {
+	{
+		.mask = BIT(1),
+		.num_adr = ARRAY_SIZE(rt722_1_single_adr),
+		.adr_d = rt722_1_single_adr,
 	},
 	{}
 };
@@ -144,6 +162,12 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_ptl_sdw_machines[] = {
 	{
 		.link_mask = BIT(0),
 		.links = ptl_rt722_only,
+		.drv_name = "sof_sdw",
+		.sof_tplg_filename = "sof-ptl-rt722.tplg",
+	},
+	{
+		.link_mask = BIT(1),
+		.links = ptl_rt722_l1,
 		.drv_name = "sof_sdw",
 		.sof_tplg_filename = "sof-ptl-rt722.tplg",
 	},
