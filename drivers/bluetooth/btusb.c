7c217a4ef1bc334b4acfc1ee1e1102d06d1a7640
Garrett Wilke <garrett@system76.com>
2024-10-07 10:14:10 -0600

    realtek 8851be bluetooth device added to btusb.c quirk table

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -509,6 +509,9 @@ static const struct usb_device_id quirks_table[] = {
 	{ USB_DEVICE(0x13d3, 0x3549), .driver_info = BTUSB_REALTEK |
 						     BTUSB_WIDEBAND_SPEECH },
 
+	/* Realtek 8851BE Bluetooth devices */
+	{ USB_DEVICE(0x13d3, 0x3600), .driver_info = BTUSB_REALTEK },
+
 	/* Realtek 8852AE Bluetooth devices */
 	{ USB_DEVICE(0x0bda, 0x2852), .driver_info = BTUSB_REALTEK |
 						     BTUSB_WIDEBAND_SPEECH },
