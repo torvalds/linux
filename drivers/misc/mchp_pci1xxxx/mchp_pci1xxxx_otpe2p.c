2471787c1f0dae6721f60ab44be37460635d3732
Heiko Thiery <heiko.thiery@gmail.com>
2024-10-07 09:11:22 +0200

    misc: microchip: pci1xxxx: add support for NVMEM_DEVID_AUTO for OTP device
    By using NVMEM_DEVID_AUTO we support more than 1 device and
    automatically enumerate.
    
    Fixes: 0969001569e4 ("misc: microchip: pci1xxxx: Add support to read and write into PCI1XXXX OTP via NVMEM sysfs")
    Cc: stable@vger.kernel.org
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>
    Reviewed-by: Michael Walle <mwalle@kernel.org>
    Link: https://lore.kernel.org/r/20241007071120.9522-2-heiko.thiery@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/mchp_pci1xxxx/mchp_pci1xxxx_otpe2p.c
+++ drivers/misc/mchp_pci1xxxx/mchp_pci1xxxx_otpe2p.c
@@ -384,6 +384,7 @@ static int pci1xxxx_otp_eeprom_probe(struct auxiliary_device *aux_dev,
 
 	priv->nvmem_config_otp.type = NVMEM_TYPE_OTP;
 	priv->nvmem_config_otp.name = OTP_NAME;
+	priv->nvmem_config_otp.id = NVMEM_DEVID_AUTO;
 	priv->nvmem_config_otp.dev = &aux_dev->dev;
 	priv->nvmem_config_otp.owner = THIS_MODULE;
 	priv->nvmem_config_otp.reg_read = pci1xxxx_otp_read;
