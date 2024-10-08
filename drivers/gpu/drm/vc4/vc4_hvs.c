9b432bbf76ed8cfabe23c938d6334092badd40cb
Dave Stevenson <dave.stevenson@raspberrypi.com>
2024-10-08 17:44:35 +0100

    drm/vc4: Match drm_dev_enter and exit calls in vc4_hvs_atomic_flush
    [ Upstream commit 6b0bd1b02ea24b10522c92b2503981970b26d1a2 ]
    
    Commit 92c17d16476c ("drm/vc4: hvs: Ignore atomic_flush if we're disabled")
    added a path which returned early without having called drm_dev_exit.
    
    Ensure all paths call drm_dev_exit.
    
    Fixes: 92c17d16476c ("drm/vc4: hvs: Ignore atomic_flush if we're disabled")
    Tested-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Reviewed-by: Maíra Canal <mcanal@igalia.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241008-drm-vc4-fixes-v1-2-9d0396ca9f42@raspberrypi.com
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/vc4/vc4_hvs.c
+++ drivers/gpu/drm/vc4/vc4_hvs.c
@@ -584,7 +584,7 @@ void vc4_hvs_atomic_flush(struct drm_crtc *crtc,
 	}
 
 	if (vc4_state->assigned_channel == VC4_HVS_CHANNEL_DISABLED)
-		return;
+		goto exit;
 
 	if (debug_dump_regs) {
 		DRM_INFO("CRTC %d HVS before:\n", drm_crtc_index(crtc));
@@ -667,6 +667,7 @@ void vc4_hvs_atomic_flush(struct drm_crtc *crtc,
 		vc4_hvs_dump_state(hvs);
 	}
 
+exit:
 	drm_dev_exit(idx);
 }
 
