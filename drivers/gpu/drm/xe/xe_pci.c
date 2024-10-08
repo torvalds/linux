800d75bf20ae429698ecf7a8a392b638f86f9642
Haridhar Kalvala <haridhar.kalvala@intel.com>
2024-10-07 18:35:05 -0700

    drm/xe/xe3: Define Xe3 feature flags
    Define a common set of Xe3 feature flags and definitions that will be
    used for all platforms in this family.
    
    The feature flags are inherited unchanged from the Xe2 (XE2_FEATURES)
    platform.
    
    Following B-spec details inherited from Xe2 feature flag definition
    commit.
    
    v2: reuse graphics_xe2 definition
    
    Bspec: 58695
     - dma_mask_size remains 46   (not documented in bspec)
     - supports_usm=1             (Bspec 59651)
     - has_flatccs=1              (Bspec 58797)
     - has_4tile=1                (Bspec 58788)
     - has_asid=1                 (Bspec 59654, 59265, 60288)
     - has_range_tlb_invalidate=1 (Bspec 71126)
     - five-level page table      (Bspec 59505)
     - 1 VD + 1 VE + 1 SFC        (Bspec 67103, 70819)
     - platform engine mask       (Bspec 60149)
    
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Signed-off-by: Haridhar Kalvala <haridhar.kalvala@intel.com>
    Signed-off-by: Matt Atwood <matthew.s.atwood@intel.com>
    Reviewed-by: Shekhar Chauhan <shekhar.chauhan@intel.com>
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241008013509.61233-3-matthew.s.atwood@intel.com

--- drivers/gpu/drm/xe/xe_pci.c
+++ drivers/gpu/drm/xe/xe_pci.c
@@ -208,7 +208,7 @@ static const struct xe_media_desc media_xelpmp = {
 };
 
 static const struct xe_media_desc media_xe2 = {
-	.name = "Xe2_LPM / Xe2_HPM",
+	.name = "Xe2_LPM / Xe2_HPM / Xe3_LPM",
 	.hw_engine_mask =
 		GENMASK(XE_HW_ENGINE_VCS7, XE_HW_ENGINE_VCS0) |
 		GENMASK(XE_HW_ENGINE_VECS3, XE_HW_ENGINE_VECS0) |
@@ -356,6 +356,8 @@ static const struct gmdid_map graphics_ip_map[] = {
 	{ 1274, &graphics_xelpg },	/* Xe_LPG+ */
 	{ 2001, &graphics_xe2 },
 	{ 2004, &graphics_xe2 },
+	{ 3000, &graphics_xe2 },
+	{ 3001, &graphics_xe2 },
 };
 
 /* Map of GMD_ID values to media IP */
@@ -363,6 +365,7 @@ static const struct gmdid_map media_ip_map[] = {
 	{ 1300, &media_xelpmp },
 	{ 1301, &media_xe2 },
 	{ 2000, &media_xe2 },
+	{ 3000, &media_xe2 },
 };
 
 #define INTEL_VGA_DEVICE(id, info) {			\
