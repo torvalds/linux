9c4a25140dee5356254ddb921086c49fb93ba952
Joey Gouly <joey.gouly@arm.com>
2024-10-08 15:01:21 +0100

    arm64: cpufeature: add POE to cpucap_is_possible()
    Since de66cb37ab6 ("arm64: Add cpucap_is_possible()"),
    alternative_has_cap_unlikely() includes the IS_ENABLED() check.
    
    Add CONFIG_ARM64_POE to cpucap_is_possible() to avoid the explicit check.
    
    Signed-off-by: Joey Gouly <joey.gouly@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Acked-by: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20241008140121.2774348-1-joey.gouly@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -834,8 +834,7 @@ static inline bool system_supports_lpa2(void)
 
 static inline bool system_supports_poe(void)
 {
-	return IS_ENABLED(CONFIG_ARM64_POE) &&
-		alternative_has_cap_unlikely(ARM64_HAS_S1POE);
+	return alternative_has_cap_unlikely(ARM64_HAS_S1POE);
 }
 
 int do_emulate_mrs(struct pt_regs *regs, u32 sys_reg, u32 rt);
