54c5dc52143e5b335b698f4e8810efa1f00787bf
Tamir Duberstein <tamird@gmail.com>
2024-10-09 12:23:58 -0400

    rust: exports: improve grammar in commentary
    Commit e26fa546042a ("rust: kbuild: auto generate helper exports")
    added an errant "the" where one was not needed; remove it.
    
    Signed-off-by: Tamir Duberstein <tamird@gmail.com>
    Reviewed-by: Dirk Behme <dirk.behme@gmail.com>
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Andreas Hindborg <a.hindborg@kernel.org>
    Link: https://lore.kernel.org/r/20241009162358.27735-1-tamird@gmail.com
    [ Reworded title. - Miguel ]
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- rust/exports.c
+++ rust/exports.c
@@ -3,9 +3,9 @@
  * A hack to export Rust symbols for loadable modules without having to redo
  * the entire `include/linux/export.h` logic in Rust.
  *
- * This requires the Rust's new/future `v0` mangling scheme because the default
- * one ("legacy") uses invalid characters for C identifiers (thus we cannot use
- * the `EXPORT_SYMBOL_*` macros).
+ * This requires Rust's new/future `v0` mangling scheme because the default one
+ * ("legacy") uses invalid characters for C identifiers (thus we cannot use the
+ * `EXPORT_SYMBOL_*` macros).
  *
  * All symbols are exported as GPL-only to guarantee no GPL-only feature is
  * accidentally exposed.
