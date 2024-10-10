f87f3b80abaf7949e638dd17dfdc267066eb52d5
Jessica Zhang <quic_jesszhan@quicinc.com>
2024-10-09 20:46:19 -0700

    drm/msm/dpu: don't always program merge_3d block
    Only program the merge_3d block for the video phys encoder when the 3d
    blend mode is not NONE
    
    Fixes: 3e79527a33a8 ("drm/msm/dpu: enable merge_3d support on sm8150/sm8250")
    Suggested-by: Abhinav Kumar <quic_abhinavk@quicinc.com>
    Signed-off-by: Jessica Zhang <quic_jesszhan@quicinc.com>
    Patchwork: https://patchwork.freedesktop.org/patch/619095/
    Link: https://lore.kernel.org/r/20241009-merge3d-fix-v1-1-0d0b6f5c244e@quicinc.com
    Signed-off-by: Abhinav Kumar <quic_abhinavk@quicinc.com>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_encoder_phys_vid.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_encoder_phys_vid.c
@@ -302,7 +302,7 @@ static void dpu_encoder_phys_vid_setup_timing_engine(
 	intf_cfg.stream_sel = 0; /* Don't care value for video mode */
 	intf_cfg.mode_3d = dpu_encoder_helper_get_3d_blend_mode(phys_enc);
 	intf_cfg.dsc = dpu_encoder_helper_get_dsc(phys_enc);
-	if (phys_enc->hw_pp->merge_3d)
+	if (intf_cfg.mode_3d && phys_enc->hw_pp->merge_3d)
 		intf_cfg.merge_3d = phys_enc->hw_pp->merge_3d->idx;
 
 	spin_lock_irqsave(phys_enc->enc_spinlock, lock_flags);
