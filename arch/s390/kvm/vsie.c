d0ef8d9fbebea0d23d80fbf6734c2b4f8bebc144
Sean Christopherson <seanjc@google.com>
2024-10-10 11:24:22 -0700

    KVM: s390: Use kvm_release_page_dirty() to unpin "struct page" memory
    Use kvm_release_page_dirty() when unpinning guest pages, as the pfn was
    retrieved via pin_guest_page(), i.e. is guaranteed to be backed by struct
    page memory.  This will allow dropping kvm_release_pfn_dirty() and
    friends.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Tested-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-ID: <20241010182427.1434605-81-seanjc@google.com>

--- arch/s390/kvm/vsie.c
+++ arch/s390/kvm/vsie.c
@@ -670,7 +670,7 @@ static int pin_guest_page(struct kvm *kvm, gpa_t gpa, hpa_t *hpa)
 /* Unpins a page previously pinned via pin_guest_page, marking it as dirty. */
 static void unpin_guest_page(struct kvm *kvm, gpa_t gpa, hpa_t hpa)
 {
-	kvm_release_pfn_dirty(hpa >> PAGE_SHIFT);
+	kvm_release_page_dirty(pfn_to_page(hpa >> PAGE_SHIFT));
 	/* mark the page always as dirty for migration */
 	mark_page_dirty(kvm, gpa_to_gfn(gpa));
 }
