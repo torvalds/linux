0b139b877b1462ccdecb5146493c68a0b94ccdc8
Sean Christopherson <seanjc@google.com>
2024-10-10 11:23:22 -0700

    KVM: Remove pointless sanity check on @map param to kvm_vcpu_(un)map()
    Drop kvm_vcpu_{,un}map()'s useless checks on @map being non-NULL.  The map
    is 100% kernel controlled, any caller that passes a NULL pointer is broken
    and needs to be fixed, i.e. a crash due to a NULL pointer dereference is
    desirable (though obviously not as desirable as not having a bug in the
    first place).
    
    Tested-by: Alex Bennée <alex.bennee@linaro.org>
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Tested-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-ID: <20241010182427.1434605-21-seanjc@google.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -3065,9 +3065,6 @@ int kvm_vcpu_map(struct kvm_vcpu *vcpu, gfn_t gfn, struct kvm_host_map *map)
 	void *hva = NULL;
 	struct page *page = KVM_UNMAPPED_PAGE;
 
-	if (!map)
-		return -EINVAL;
-
 	pfn = gfn_to_pfn(vcpu->kvm, gfn);
 	if (is_error_noslot_pfn(pfn))
 		return -EINVAL;
@@ -3095,9 +3092,6 @@ EXPORT_SYMBOL_GPL(kvm_vcpu_map);
 
 void kvm_vcpu_unmap(struct kvm_vcpu *vcpu, struct kvm_host_map *map, bool dirty)
 {
-	if (!map)
-		return;
-
 	if (!map->hva)
 		return;
 
