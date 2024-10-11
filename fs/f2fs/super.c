9e11b1d5fda972f6be60ab732976a7c8e064cd56
Ye Bin <yebin10@huawei.com>
2024-10-12 00:44:50 +0800

    f2fs: fix null-ptr-deref in f2fs_submit_page_bio()
    [ Upstream commit b7d0a97b28083084ebdd8e5c6bccd12e6ec18faa ]
    
    There's issue as follows when concurrently installing the f2fs.ko
    module and mounting the f2fs file system:
    KASAN: null-ptr-deref in range [0x0000000000000020-0x0000000000000027]
    RIP: 0010:__bio_alloc+0x2fb/0x6c0 [f2fs]
    Call Trace:
     <TASK>
     f2fs_submit_page_bio+0x126/0x8b0 [f2fs]
     __get_meta_page+0x1d4/0x920 [f2fs]
     get_checkpoint_version.constprop.0+0x2b/0x3c0 [f2fs]
     validate_checkpoint+0xac/0x290 [f2fs]
     f2fs_get_valid_checkpoint+0x207/0x950 [f2fs]
     f2fs_fill_super+0x1007/0x39b0 [f2fs]
     mount_bdev+0x183/0x250
     legacy_get_tree+0xf4/0x1e0
     vfs_get_tree+0x88/0x340
     do_new_mount+0x283/0x5e0
     path_mount+0x2b2/0x15b0
     __x64_sys_mount+0x1fe/0x270
     do_syscall_64+0x5f/0x170
     entry_SYSCALL_64_after_hwframe+0x76/0x7e
    
    Above issue happens as the biset of the f2fs file system is not
    initialized before register "f2fs_fs_type".
    To address above issue just register "f2fs_fs_type" at the last in
    init_f2fs_fs(). Ensure that all f2fs file system resources are
    initialized.
    
    Fixes: f543805fcd60 ("f2fs: introduce private bioset")
    Signed-off-by: Ye Bin <yebin10@huawei.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -4991,9 +4991,6 @@ static int __init init_f2fs_fs(void)
 	err = f2fs_init_shrinker();
 	if (err)
 		goto free_sysfs;
-	err = register_filesystem(&f2fs_fs_type);
-	if (err)
-		goto free_shrinker;
 	f2fs_create_root_stats();
 	err = f2fs_init_post_read_processing();
 	if (err)
@@ -5016,7 +5013,12 @@ static int __init init_f2fs_fs(void)
 	err = f2fs_create_casefold_cache();
 	if (err)
 		goto free_compress_cache;
+	err = register_filesystem(&f2fs_fs_type);
+	if (err)
+		goto free_casefold_cache;
 	return 0;
+free_casefold_cache:
+	f2fs_destroy_casefold_cache();
 free_compress_cache:
 	f2fs_destroy_compress_cache();
 free_compress_mempool:
@@ -5031,8 +5033,6 @@ free_post_read:
 	f2fs_destroy_post_read_processing();
 free_root_stats:
 	f2fs_destroy_root_stats();
-	unregister_filesystem(&f2fs_fs_type);
-free_shrinker:
 	f2fs_exit_shrinker();
 free_sysfs:
 	f2fs_exit_sysfs();
@@ -5056,6 +5056,7 @@ fail:
 
 static void __exit exit_f2fs_fs(void)
 {
+	unregister_filesystem(&f2fs_fs_type);
 	f2fs_destroy_casefold_cache();
 	f2fs_destroy_compress_cache();
 	f2fs_destroy_compress_mempool();
@@ -5064,7 +5065,6 @@ static void __exit exit_f2fs_fs(void)
 	f2fs_destroy_iostat_processing();
 	f2fs_destroy_post_read_processing();
 	f2fs_destroy_root_stats();
-	unregister_filesystem(&f2fs_fs_type);
 	f2fs_exit_shrinker();
 	f2fs_exit_sysfs();
 	f2fs_destroy_garbage_collection_cache();
