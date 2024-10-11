a6021aa24f6417416d93318bbfa022ab229c33c8
Arnd Bergmann <arnd@arndb.de>
2024-10-11 06:18:17 +0000

    ACPI: EC: make EC support compile-time conditional
    The embedded controller code is mainly used on x86 laptops and cannot
    work without PC style I/O port access.
    
    Make this a user-visible configuration option that is default enabled
    on x86 but otherwise disabled, and that can never be enabled unless
    CONFIG_HAS_IOPORT is also available.
    
    The empty stubs in internal.h help ignore the EC code in configurations
    that don't support it. In order to see those stubs, the sbshc code also
    has to include this header and drop duplicate declarations.
    
    All the direct callers of ec_read/ec_write already had an x86
    dependency and now also need to depend on APCI_EC.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Guenter Roeck <linux@roeck-us.net>
    Acked-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://patch.msgid.link/20241011061948.3211423-1-arnd@kernel.org
    [ rjw: Subject edits ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/sbshc.c
+++ drivers/acpi/sbshc.c
@@ -14,6 +14,7 @@
 #include <linux/module.h>
 #include <linux/interrupt.h>
 #include "sbshc.h"
+#include "internal.h"
 
 #define ACPI_SMB_HC_CLASS	"smbus_host_ctl"
 #define ACPI_SMB_HC_DEVICE_NAME	"ACPI SMBus HC"
@@ -236,12 +237,6 @@ static int smbus_alarm(void *context)
 	return 0;
 }
 
-typedef int (*acpi_ec_query_func) (void *data);
-
-extern int acpi_ec_add_query_handler(struct acpi_ec *ec, u8 query_bit,
-			      acpi_handle handle, acpi_ec_query_func func,
-			      void *data);
-
 static int acpi_smbus_hc_add(struct acpi_device *device)
 {
 	int status;
@@ -277,8 +272,6 @@ static int acpi_smbus_hc_add(struct acpi_device *device)
 
 	return 0;
 }
-
-extern void acpi_ec_remove_query_handler(struct acpi_ec *ec, u8 query_bit);
 
 static void acpi_smbus_hc_remove(struct acpi_device *device)
 {
