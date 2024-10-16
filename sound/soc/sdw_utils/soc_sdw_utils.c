7d6f2d5254b1786c8b3bd64c6295b14e1607965d
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2024-10-16 18:23:31 +0800

    ASoC: sdw_utils: add SmartMic DAI for RT713 VB
    In theory the dailinks are created based on the number of endpoints
    reported in ACPI match tables, so it should harmless to add a new
    dailink: RT713 VA would not use it since it has only 2 endpoints.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Péter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Link: https://patch.msgid.link/20241016102333.294448-10-yung-chuan.liao@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sdw_utils/soc_sdw_utils.c
+++ sound/soc/sdw_utils/soc_sdw_utils.c
@@ -185,8 +185,15 @@ struct asoc_sdw_codec_info codec_info_list[] = {
 				.widgets = generic_jack_widgets,
 				.num_widgets = ARRAY_SIZE(generic_jack_widgets),
 			},
+			{
+				.direction = {false, true},
+				.dai_name = "rt712-sdca-aif3",
+				.dai_type = SOC_SDW_DAI_TYPE_MIC,
+				.dailink = {SOC_SDW_UNUSED_DAI_ID, SOC_SDW_DMIC_DAI_ID},
+				.rtd_init = asoc_sdw_rt_dmic_rtd_init,
+			},
 		},
-		.dai_num = 1,
+		.dai_num = 2,
 	},
 	{
 		.part_id = 0x1713,
