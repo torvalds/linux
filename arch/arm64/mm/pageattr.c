0e9cb5995b2539a332fe65ada6a28a6be55f6e40
Steven Price <steven.price@arm.com>
2024-10-17 14:14:31 +0100

    arm64: mm: Avoid TLBI when marking pages as valid
    When __change_memory_common() is purely setting the valid bit on a PTE
    (e.g. via the set_memory_valid() call) there is no need for a TLBI as
    either the entry isn't changing (the valid bit was already set) or the
    entry was invalid and so should not have been cached in the TLB.
    
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Reviewed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Steven Price <steven.price@arm.com>
    Link: https://lore.kernel.org/r/20241017131434.40935-9-steven.price@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/mm/pageattr.c
+++ arch/arm64/mm/pageattr.c
@@ -60,7 +60,13 @@ static int __change_memory_common(unsigned long start, unsigned long size,
 	ret = apply_to_page_range(&init_mm, start, size, change_page_range,
 					&data);
 
-	flush_tlb_kernel_range(start, start + size);
+	/*
+	 * If the memory is being made valid without changing any other bits
+	 * then a TLBI isn't required as a non-valid entry cannot be cached in
+	 * the TLB.
+	 */
+	if (pgprot_val(set_mask) != PTE_VALID || pgprot_val(clear_mask))
+		flush_tlb_kernel_range(start, start + size);
 	return ret;
 }
 
