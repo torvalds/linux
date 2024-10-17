0e07d5dbfbd9b0441ae4ec07a2a72738121356e2
Petr Machata <petrm@nvidia.com>
2024-10-17 11:45:45 +0200

    selftests: forwarding: lib: Allow passing PID to stop_traffic()
    Now that it is possible to schedule a deferral of stop_traffic() right
    after the traffic is started, we do not have to rely on the %% magic to
    kill the background process that was started last. Instead we can just give
    the PID explicitly. This makes it possible to start other background
    processes after the traffic is started without confusing the cleanup.
    
    Signed-off-by: Petr Machata <petrm@nvidia.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- tools/testing/selftests/net/forwarding/lib.sh
+++ tools/testing/selftests/net/forwarding/lib.sh
@@ -1768,8 +1768,10 @@ start_tcp_traffic()
 
 stop_traffic()
 {
+	local pid=${1-%%}; shift
+
 	# Suppress noise from killing mausezahn.
-	{ kill %% && wait %%; } 2>/dev/null
+	{ kill $pid && wait $pid; } 2>/dev/null
 }
 
 declare -A cappid
