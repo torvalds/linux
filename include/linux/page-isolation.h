b7f058f827392022d8c689329f88c7b324d71dad
Luoxi Li <kaixa@kiloview.com>
2024-10-18 17:22:35 +0800

    mm: remove unused has_isolate_pageblock
    has_isolate_pageblock() has been unused since commit 55612e80e722 ("mm:
    page_alloc: close migratetype race between freeing and stealing")
    
    Remove it.
    
    Link: https://lkml.kernel.org/r/20241018092235.2764859-1-kaixa@kiloview.com
    Signed-off-by: Luoxi Li <kaixa@kiloview.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Muhammad Usama Anjum <usama.anjum@collabora.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/page-isolation.h
+++ include/linux/page-isolation.h
@@ -3,10 +3,6 @@
 #define __LINUX_PAGEISOLATION_H
 
 #ifdef CONFIG_MEMORY_ISOLATION
-static inline bool has_isolate_pageblock(struct zone *zone)
-{
-	return zone->nr_isolate_pageblock;
-}
 static inline bool is_migrate_isolate_page(struct page *page)
 {
 	return get_pageblock_migratetype(page) == MIGRATE_ISOLATE;
@@ -16,10 +12,6 @@ static inline bool is_migrate_isolate(int migratetype)
 	return migratetype == MIGRATE_ISOLATE;
 }
 #else
-static inline bool has_isolate_pageblock(struct zone *zone)
-{
-	return false;
-}
 static inline bool is_migrate_isolate_page(struct page *page)
 {
 	return false;
