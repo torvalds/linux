d26aed5eba16bf5a4aa86bc717edf0b5ed192b93
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2024-10-20 23:58:55 +0000

    ASoC: intel: remove dpcm_xxx flags
    dpcm_xxx flags are no longer needed. It converts dpcm_xxx flag to
    xxx_only if needed.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://patch.msgid.link/87froq9vi9.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/avs/boards/rt298.c
+++ sound/soc/intel/avs/boards/rt298.c
@@ -173,8 +173,6 @@ static int avs_create_dai_link(struct device *dev, const char *platform_name, in
 	dl->ops = &avs_rt298_ops;
 	dl->nonatomic = 1;
 	dl->no_pcm = 1;
-	dl->dpcm_capture = 1;
-	dl->dpcm_playback = 1;
 
 	*dai_link = dl;
 
