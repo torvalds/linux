6ba3df714723563a62e5068b15305a5670cad08d
Vasileios Amoiridis <vassilisamir@gmail.com>
2024-10-21 21:53:05 +0200

    iio: chemical: bme680: optimize startup time
    According to datasheet's Section 1.1, Table 1, the startup time for the
    device is 2ms and not 5ms.
    
    Signed-off-by: Vasileios Amoiridis <vassilisamir@gmail.com>
    Link: https://patch.msgid.link/20241021195316.58911-3-vassilisamir@gmail.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/chemical/bme680.h
+++ drivers/iio/chemical/bme680.h
@@ -65,7 +65,8 @@
 
 #define BME680_MEAS_TRIM_MASK			GENMASK(24, 4)
 
-#define BME680_STARTUP_TIME_US			5000
+/* Datasheet Section 1.1, Table 1 */
+#define BME680_STARTUP_TIME_US			2000
 
 /* Calibration Parameters */
 #define BME680_T2_LSB_REG	0x8A
