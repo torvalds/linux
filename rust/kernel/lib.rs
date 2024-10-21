b26e36b3e2cbca4d064f297b094c88d4879bdb06
Yutaro Ohno <yutaro.ono.418@gmail.com>
2024-10-21 11:58:47 +0900

    rust: kernel: fix THIS_MODULE header path in ThisModule doc comment
    [ Upstream commit 8b55dc8610acf816a66373be53ca6e3bbe2d313a ]
    
    The doc comment for `ThisModule` incorrectly states the C header file
    for `THIS_MODULE` as `include/linux/export.h`, while the correct path is
    `include/linux/init.h`. This is because `THIS_MODULE` was moved in
    commit 5b20755b7780 ("init: move THIS_MODULE from <linux/export.h> to
    <linux/init.h>").
    
    Update the doc comment for `ThisModule` to reflect the correct header
    file path for `THIS_MODULE`.
    
    Fixes: 5b20755b7780 ("init: move THIS_MODULE from <linux/export.h> to <linux/init.h>")
    Signed-off-by: Yutaro Ohno <yutaro.ono.418@gmail.com>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://lore.kernel.org/r/ZxXDZwxWgoEiIYkj@ohnotp
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -83,7 +83,7 @@ pub trait Module: Sized + Sync + Send {
 
 /// Equivalent to `THIS_MODULE` in the C API.
 ///
-/// C header: [`include/linux/export.h`](srctree/include/linux/export.h)
+/// C header: [`include/linux/init.h`](srctree/include/linux/init.h)
 pub struct ThisModule(*mut bindings::module);
 
 // SAFETY: `THIS_MODULE` may be used from all threads within a module.
