1d81d85d1a19e50d5237dc67d6b825c34ae13de8
Borislav Petkov (AMD) <bp@alien8.de>
2024-10-21 10:38:21 +0200

    x86/microcode/AMD: Split load_microcode_amd()
    This function should've been split a long time ago because it is used in
    two paths:
    
    1) On the late loading path, when the microcode is loaded through the
       request_firmware interface
    
    2) In the save_microcode_in_initrd() path which collects all the
       microcode patches which are relevant for the current system before
       the initrd with the microcode container has been jettisoned.
    
       In that path, it is not really necessary to iterate over the nodes on
       a system and match a patch however it didn't cause any trouble so it
       was left for a later cleanup
    
    However, that later cleanup was expedited by the fact that Jens was
    enabling "Use L3 as a NUMA node" in the BIOS setting in his machine and
    so this causes the NUMA CPU masks used in cpumask_of_node() to be
    generated *after* 2) above happened on the first node. Which means, all
    those masks were funky, wrong, uninitialized and whatnot, leading to
    explosions when dereffing c->microcode in load_microcode_amd().
    
    So split that function and do only the necessary work needed at each
    stage.
    
    Fixes: 94838d230a6c ("x86/microcode/AMD: Use the family,model,stepping encoded in the patch ID")
    Reported-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Tested-by: Jens Axboe <axboe@kernel.dk>
    Link: https://lore.kernel.org/r/91194406-3fdf-4e38-9838-d334af538f74@kernel.dk

--- arch/x86/kernel/cpu/microcode/amd.c
+++ arch/x86/kernel/cpu/microcode/amd.c
@@ -584,7 +584,7 @@ void __init load_ucode_amd_bsp(struct early_load_data *ed, unsigned int cpuid_1_
 		native_rdmsr(MSR_AMD64_PATCH_LEVEL, ed->new_rev, dummy);
 }
 
-static enum ucode_state load_microcode_amd(u8 family, const u8 *data, size_t size);
+static enum ucode_state _load_microcode_amd(u8 family, const u8 *data, size_t size);
 
 static int __init save_microcode_in_initrd(void)
 {
@@ -605,7 +605,7 @@ static int __init save_microcode_in_initrd(void)
 	if (!desc.mc)
 		return -EINVAL;
 
-	ret = load_microcode_amd(x86_family(cpuid_1_eax), desc.data, desc.size);
+	ret = _load_microcode_amd(x86_family(cpuid_1_eax), desc.data, desc.size);
 	if (ret > UCODE_UPDATED)
 		return -EINVAL;
 
@@ -954,6 +954,20 @@ static enum ucode_state __load_microcode_amd(u8 family, const u8 *data,
 	return UCODE_OK;
 }
 
+static enum ucode_state _load_microcode_amd(u8 family, const u8 *data, size_t size)
+{
+	enum ucode_state ret;
+
+	/* free old equiv table */
+	free_equiv_cpu_table();
+
+	ret = __load_microcode_amd(family, data, size);
+	if (ret != UCODE_OK)
+		cleanup();
+
+	return ret;
+}
+
 static enum ucode_state load_microcode_amd(u8 family, const u8 *data, size_t size)
 {
 	struct cpuinfo_x86 *c;
@@ -961,14 +975,9 @@ static enum ucode_state load_microcode_amd(u8 family, const u8 *data, size_t siz
 	struct ucode_patch *p;
 	enum ucode_state ret;
 
-	/* free old equiv table */
-	free_equiv_cpu_table();
-
-	ret = __load_microcode_amd(family, data, size);
-	if (ret != UCODE_OK) {
-		cleanup();
+	ret = _load_microcode_amd(family, data, size);
+	if (ret != UCODE_OK)
 		return ret;
-	}
 
 	for_each_node(nid) {
 		cpu = cpumask_first(cpumask_of_node(nid));
