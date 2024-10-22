44b682694a0ca366bf15c26c3c3c16d26c9e9f6d
Liam Zuiderhoek <zuiderhoekl@gmail.com>
2024-10-22 20:46:59 +0200

    i2c: Fix whitespace style issue
    This patch fixes a coding style issue in the alignment of parameters
    in the function i2c_smbus_write_bytes(). It replaces spaces with tabs for
    alignment,  as per the coding style guidelines.
    
    Signed-off-by: Liam Zuiderhoek <zuiderhoekl@gmail.com>
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>

--- drivers/i2c/i2c-core-smbus.c
+++ drivers/i2c/i2c-core-smbus.c
@@ -122,7 +122,7 @@ EXPORT_SYMBOL(i2c_smbus_read_byte);
 s32 i2c_smbus_write_byte(const struct i2c_client *client, u8 value)
 {
 	return i2c_smbus_xfer(client->adapter, client->addr, client->flags,
-	                      I2C_SMBUS_WRITE, value, I2C_SMBUS_BYTE, NULL);
+			      I2C_SMBUS_WRITE, value, I2C_SMBUS_BYTE, NULL);
 }
 EXPORT_SYMBOL(i2c_smbus_write_byte);
 
