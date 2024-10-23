f247fd22e9f27d919d81861e4dcc438e0b6d179b
Sven Schnelle <svens@linux.ibm.com>
2024-10-23 08:56:00 +0200

    s390/time: Add clocksource id to TOD clock
    To allow specifying the clock source in the upcoming PtP driver,
    add a clocksource ID to the s390 TOD clock.
    
    Acked-by: Heiko Carstens <hca@linux.ibm.com>
    Acked-by: Richard Cochran <richardcochran@gmail.com>
    Signed-off-by: Sven Schnelle <svens@linux.ibm.com>
    Link: https://patch.msgid.link/20241023065601.449586-2-svens@linux.ibm.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- arch/s390/kernel/time.c
+++ arch/s390/kernel/time.c
@@ -255,6 +255,7 @@ static struct clocksource clocksource_tod = {
 	.shift		= 24,
 	.flags		= CLOCK_SOURCE_IS_CONTINUOUS,
 	.vdso_clock_mode = VDSO_CLOCKMODE_TOD,
+	.id		= CSID_S390_TOD,
 };
 
 struct clocksource * __init clocksource_default_clock(void)
