75837f0b2bc61c62c8326fe3a1eb9a100714cfd7
Sibi Sankar <quic_sibis@quicinc.com>
2024-10-25 18:02:26 +0530

    firmware: qcom: uefisecapp: Allow X1E Devkit devices
    Add X1E Devkit to the allowlist.
    
    Signed-off-by: Sibi Sankar <quic_sibis@quicinc.com>
    Acked-by: Marc Zyngier <maz@kernel.org>
    Tested-by: Marc Zyngier <maz@kernel.org>
    Tested-by: Jens Glathe <jens.glathe@oldschoolsolutions.biz>
    Link: https://lore.kernel.org/r/20241025123227.3527720-3-quic_sibis@quicinc.com
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -1740,6 +1740,7 @@ static const struct of_device_id qcom_scm_qseecom_allowlist[] __maybe_unused = {
 	{ .compatible = "microsoft,romulus13", },
 	{ .compatible = "microsoft,romulus15", },
 	{ .compatible = "qcom,sc8180x-primus" },
+	{ .compatible = "qcom,x1e001de-devkit" },
 	{ .compatible = "qcom,x1e80100-crd" },
 	{ .compatible = "qcom,x1e80100-qcp" },
 	{ }
