4c9397cf292f357fe13f3c7cd9f53240d062c18e
Julian Vetter <jvetter@kalrayinc.com>
2024-10-28 14:42:26 +0100

    csky: Use new fallback IO memcpy/memset
    Use the new fallback memcpy_{from,to}io and memset_io functions from
    lib/iomem_copy.c on the csky processor architecture.
    
    Acked-by: Guo Ren <guoren@kernel.org>
    Reviewed-by: Yann Sionneau <ysionneau@kalrayinc.com>
    Signed-off-by: Julian Vetter <jvetter@kalrayinc.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/csky/include/asm/io.h
+++ arch/csky/include/asm/io.h
@@ -32,17 +32,6 @@
 #endif
 
 /*
- * String version of I/O memory access operations.
- */
-extern void __memcpy_fromio(void *, const volatile void __iomem *, size_t);
-extern void __memcpy_toio(volatile void __iomem *, const void *, size_t);
-extern void __memset_io(volatile void __iomem *, int, size_t);
-
-#define memset_io(c,v,l)        __memset_io((c),(v),(l))
-#define memcpy_fromio(a,c,l)    __memcpy_fromio((a),(c),(l))
-#define memcpy_toio(c,a,l)      __memcpy_toio((c),(a),(l))
-
-/*
  * I/O memory mapping functions.
  */
 #define ioremap_wc(addr, size) \
