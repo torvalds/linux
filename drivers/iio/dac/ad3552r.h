0b4d9fe58be8260819c453fb4717f23bdafd3ba3
Angelo Dureghello <adureghello@baylibre.com>
2024-10-28 22:45:34 +0100

    iio: dac: ad3552r: add high-speed platform driver
    Add High Speed ad3552r platform driver.
    
    The ad3552r DAC is controlled by a custom (fpga-based) DAC IP
    through the current AXI backend, or similar alternative IIO backend.
    
    Compared to the existing driver (ad3552r.c), that is a simple SPI
    driver, this driver is coupled with a DAC IIO backend that finally
    controls the ad3552r by a fpga-based "QSPI+DDR" interface, to reach
    maximum transfer rate of 33MUPS using dma stream capabilities.
    
    All commands involving QSPI bus read/write are delegated to the backend
    through the provided APIs for bus read/write.
    
    Reviewed-by: Nuno Sa <nuno.sa@analog.com>
    Signed-off-by: Angelo Dureghello <adureghello@baylibre.com>
    Reviewed-by: David Lechner <dlechner@baylibre.com>
    Link: https://patch.msgid.link/20241028-wip-bl-ad3552r-axi-v0-iio-testing-v9-7-f6960b4f9719@kernel-space.org
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/dac/ad3552r.h
+++ drivers/iio/dac/ad3552r.h
@@ -127,8 +127,12 @@
 #define AD3552R_GAIN_SCALE				1000
 #define AD3552R_LDAC_PULSE_US				100
 
+#define AD3552R_CH0_ACTIVE				BIT(0)
+#define AD3552R_CH1_ACTIVE				BIT(1)
+
 #define AD3552R_MAX_RANGES	5
 #define AD3542R_MAX_RANGES	6
+#define AD3552R_QUAD_SPI	2
 
 extern const s32 ad3552r_ch_ranges[AD3552R_MAX_RANGES][2];
 extern const s32 ad3542r_ch_ranges[AD3542R_MAX_RANGES][2];
