3e8b7238b427e05498034c240451af5f5495afda
Johan Hovold <johan+linaro@kernel.org>
2024-10-28 13:49:58 +0100

    gpiolib: fix debugfs newline separators
    The gpiolib debugfs interface exports a list of all gpio chips in a
    system and the state of their pins.
    
    The gpio chip sections are supposed to be separated by a newline
    character, but a long-standing bug prevents the separator from
    being included when output is generated in multiple sessions, making the
    output inconsistent and hard to read.
    
    Make sure to only suppress the newline separator at the beginning of the
    file as intended.
    
    Fixes: f9c4a31f6150 ("gpiolib: Use seq_file's iterator interface")
    Cc: stable@vger.kernel.org	# 3.7
    Cc: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>
    Link: https://lore.kernel.org/r/20241028125000.24051-2-johan+linaro@kernel.org
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -4926,6 +4926,8 @@ static void *gpiolib_seq_start(struct seq_file *s, loff_t *pos)
 		return NULL;
 
 	s->private = priv;
+	if (*pos > 0)
+		priv->newline = true;
 	priv->idx = srcu_read_lock(&gpio_devices_srcu);
 
 	list_for_each_entry_srcu(gdev, &gpio_devices, list,
