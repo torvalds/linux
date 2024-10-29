e1f6fa55664a0eeb0a641f497e1adfcf6672e995
Nirmoy Das <nirmoy.das@intel.com>
2024-10-29 13:01:17 +0100

    drm/xe/guc/tlb: Flush g2h worker in case of tlb timeout
    Flush the g2h worker explicitly if TLB timeout happens which is
    observed on LNL and that points to the recent scheduling issue with
    E-cores on LNL.
    
    This is similar to the recent fix:
    commit e51527233804 ("drm/xe/guc/ct: Flush g2h worker in case of g2h
    response timeout") and should be removed once there is E core
    scheduling fix.
    
    v2: Add platform check(Himal)
    v3: Remove gfx platform check as the issue related to cpu
        platform(John)
        Use the common WA macro(John) and print when the flush
        resolves timeout(Matt B)
    v4: Remove the resolves log and do the flush before taking
        pending_lock(Matt A)
    
    Cc: Badal Nilawar <badal.nilawar@intel.com>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Cc: Matthew Auld <matthew.auld@intel.com>
    Cc: John Harrison <John.C.Harrison@Intel.com>
    Cc: Himal Prasad Ghimiray <himal.prasad.ghimiray@intel.com>
    Cc: Lucas De Marchi <lucas.demarchi@intel.com>
    Cc: stable@vger.kernel.org # v6.11+
    Link: https://gitlab.freedesktop.org/drm/xe/kernel/-/issues/2687
    Signed-off-by: Nirmoy Das <nirmoy.das@intel.com>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241029120117.449694-3-nirmoy.das@intel.com
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- drivers/gpu/drm/xe/xe_gt_tlb_invalidation.c
+++ drivers/gpu/drm/xe/xe_gt_tlb_invalidation.c
@@ -72,6 +72,8 @@ static void xe_gt_tlb_fence_timeout(struct work_struct *work)
 	struct xe_device *xe = gt_to_xe(gt);
 	struct xe_gt_tlb_invalidation_fence *fence, *next;
 
+	LNL_FLUSH_WORK(&gt->uc.guc.ct.g2h_worker);
+
 	spin_lock_irq(&gt->tlb_invalidation.pending_lock);
 	list_for_each_entry_safe(fence, next,
 				 &gt->tlb_invalidation.pending_fences, link) {
