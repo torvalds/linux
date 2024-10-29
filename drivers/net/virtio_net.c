47008bb51c3e11c187dacc17af334bdca97c2dca
Xuan Zhuo <xuanzhuo@linux.alibaba.com>
2024-10-29 16:46:14 +0800

    virtio_net: enable premapped mode for merge and small by default
    Currently, the virtio core will perform a dma operation for each
    buffer. Although, the same page may be operated multiple times.
    
    In premapped mod, we can perform only one dma operation for the pages of
    the alloc frag. This is beneficial for the iommu device.
    
    kernel command line: intel_iommu=on iommu.passthrough=0
    
           |  strict=0  | strict=1
    Before |  775496pps | 428614pps
    After  | 1109316pps | 742853pps
    
    In the 6.11, we disabled this feature because a regress [1].
    
    Now, we fix the problem and re-enable it.
    
    [1]: http://lore.kernel.org/all/8b20cc28-45a9-4643-8e87-ba164a540c0a@oracle.com
    
    Tested-by: Darren Kenny <darren.kenny@oracle.com>
    Signed-off-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -6107,6 +6107,17 @@ err_ctrl:
 	return -ENOMEM;
 }
 
+static void virtnet_rq_set_premapped(struct virtnet_info *vi)
+{
+	int i;
+
+	for (i = 0; i < vi->max_queue_pairs; i++) {
+		/* error should never happen */
+		BUG_ON(virtqueue_set_dma_premapped(vi->rq[i].vq));
+		vi->rq[i].do_dma = true;
+	}
+}
+
 static int init_vqs(struct virtnet_info *vi)
 {
 	int ret;
@@ -6119,6 +6130,10 @@ static int init_vqs(struct virtnet_info *vi)
 	ret = virtnet_find_vqs(vi);
 	if (ret)
 		goto err_free;
+
+	/* disable for big mode */
+	if (!vi->big_packets || vi->mergeable_rx_bufs)
+		virtnet_rq_set_premapped(vi);
 
 	cpus_read_lock();
 	virtnet_set_affinity(vi);
