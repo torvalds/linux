78b5c2ca5f27534dc04fbbe0b491dd3bd4ec814b
Frederic Weisbecker <frederic@kernel.org>
2024-10-29 13:54:46 +0100

    clocksource/drivers/arm_arch_timer: Remove clockevents shutdown call on offlining
    The clockevents core already detached and unregistered it at this stage.
    
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/all/20241029125451.54574-6-frederic@kernel.org

--- drivers/clocksource/arm_arch_timer.c
+++ drivers/clocksource/arm_arch_timer.c
@@ -1179,8 +1179,6 @@ static void arch_timer_stop(struct clock_event_device *clk)
 	disable_percpu_irq(arch_timer_ppi[arch_timer_uses_ppi]);
 	if (arch_timer_has_nonsecure_ppi())
 		disable_percpu_irq(arch_timer_ppi[ARCH_TIMER_PHYS_NONSECURE_PPI]);
-
-	clk->set_state_shutdown(clk);
 }
 
 static int arch_timer_dying_cpu(unsigned int cpu)
