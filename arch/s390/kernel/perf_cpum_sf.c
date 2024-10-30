f55bd479d8663a4a4e403b3d308d3d1aa33d92df
Thomas Richter <tmricht@linux.ibm.com>
2024-10-30 12:37:18 +0100

    s390/cpum_sf: Fix and protect memory allocation of SDBs with mutex
    Reservation of the PMU hardware is done at first event creation
    and is protected by a pair of mutex_lock() and mutex_unlock().
    After reservation of the PMU hardware the memory
    required for the PMUs the event is to be installed on is
    allocated by allocate_buffers() and alloc_sampling_buffer().
    This done outside of the mutex protection.
    Without mutex protection two or more concurrent invocations of
    perf_event_init() may run in parallel.
    This can lead to allocation of Sample Data Blocks (SDBs)
    multiple times for the same PMU.
    Prevent this and protect memory allocation of SDBs by
    mutex.
    
    Fixes: 8a6fe8f21ec4 ("s390/cpum_sf: Use refcount_t instead of atomic_t")
    Signed-off-by: Thomas Richter <tmricht@linux.ibm.com>
    Reviewed-by: Sumanth Korikkar <sumanthk@linux.ibm.com>
    Reviewed-by: Hendrik Brueckner <brueckner@linux.ibm.com>
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- arch/s390/kernel/perf_cpum_sf.c
+++ arch/s390/kernel/perf_cpum_sf.c
@@ -758,7 +758,6 @@ static int __hw_perf_event_init(struct perf_event *event)
 		reserve_pmc_hardware();
 		refcount_set(&num_events, 1);
 	}
-	mutex_unlock(&pmc_reserve_mutex);
 	event->destroy = hw_perf_event_destroy;
 
 	/* Access per-CPU sampling information (query sampling info) */
@@ -847,6 +846,7 @@ static int __hw_perf_event_init(struct perf_event *event)
 		if (is_default_overflow_handler(event))
 			event->overflow_handler = cpumsf_output_event_pid;
 out:
+	mutex_unlock(&pmc_reserve_mutex);
 	return err;
 }
 
