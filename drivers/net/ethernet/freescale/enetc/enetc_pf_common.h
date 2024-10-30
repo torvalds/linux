80c8c852615e97fdefc250be38ae1740b183eb0b
Wei Fang <wei.fang@nxp.com>
2024-10-30 17:39:16 +0800

    net: enetc: extract common ENETC PF parts for LS1028A and i.MX95 platforms
    The ENETC PF driver of LS1028A (rev 1.0) is incompatible with the version
    used on the i.MX95 platform (rev 4.1), except for the station interface
    (SI) part. To reduce code redundancy and prepare for a new driver for rev
    4.1 and later, extract shared interfaces from enetc_pf.c and move them to
    enetc_pf_common.c. This refactoring lays the groundwork for compiling
    enetc_pf_common.c into a shared driver for both platforms' PF drivers.
    
    Signed-off-by: Wei Fang <wei.fang@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ drivers/net/ethernet/freescale/enetc/enetc_pf_common.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR BSD-3-Clause) */
+/* Copyright 2024 NXP */
+
+#include "enetc_pf.h"
+
+void enetc_pf_get_primary_mac_addr(struct enetc_hw *hw, int si, u8 *addr);
+void enetc_pf_set_primary_mac_addr(struct enetc_hw *hw, int si,
+				   const u8 *addr);
+int enetc_pf_set_mac_addr(struct net_device *ndev, void *addr);
+int enetc_setup_mac_addresses(struct device_node *np, struct enetc_pf *pf);
+void enetc_pf_netdev_setup(struct enetc_si *si, struct net_device *ndev,
+			   const struct net_device_ops *ndev_ops);
+int enetc_mdiobus_create(struct enetc_pf *pf, struct device_node *node);
+void enetc_mdiobus_destroy(struct enetc_pf *pf);
+int enetc_phylink_create(struct enetc_ndev_priv *priv, struct device_node *node,
+			 const struct phylink_mac_ops *ops);
+void enetc_phylink_destroy(struct enetc_ndev_priv *priv);
