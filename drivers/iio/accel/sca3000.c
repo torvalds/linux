4880978294a2a79bfe0fdb23353c4499ebe39211
Julien Stephan <jstephan@baylibre.com>
2024-10-31 16:27:04 +0100

    iio: accel: sca3000: use bool for event state
    Since the write_event_config callback now uses a bool for the state
    parameter, update the signatures of the functions it calls accordingly.
    
    Signed-off-by: Julien Stephan <jstephan@baylibre.com>
    Link: https://patch.msgid.link/20241031-iio-fix-write-event-config-signature-v2-9-2bcacbb517a2@baylibre.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/accel/sca3000.c
+++ drivers/iio/accel/sca3000.c
@@ -1158,7 +1158,7 @@ error_ret:
 	return ret;
 }
 
-static int sca3000_freefall_set_state(struct iio_dev *indio_dev, int state)
+static int sca3000_freefall_set_state(struct iio_dev *indio_dev, bool state)
 {
 	struct sca3000_state *st = iio_priv(indio_dev);
 	int ret;
@@ -1181,7 +1181,7 @@ static int sca3000_freefall_set_state(struct iio_dev *indio_dev, int state)
 }
 
 static int sca3000_motion_detect_set_state(struct iio_dev *indio_dev, int axis,
-					   int state)
+					   bool state)
 {
 	struct sca3000_state *st = iio_priv(indio_dev);
 	int ret, ctrlval;
