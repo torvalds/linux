e26cbab9821a6e511ec839884517838ec596751e
Thomas Gleixner <tglx@linutronix.de>
2024-10-31 13:04:08 +0100

    timekeeping: Always check for negative motion
    [ Upstream commit c163e40af9b2331b2c629fd4ec8b703ed4d4ae39 ]
    
    clocksource_delta() has two variants. One with a check for negative motion,
    which is only selected by x86. This is a historic leftover as this function
    was previously used in the time getter hot paths.
    
    Since 135225a363ae timekeeping_cycles_to_ns() has unconditional protection
    against this as a by-product of the protection against 64bit math overflow.
    
    clocksource_delta() is only used in the clocksource watchdog and in
    timekeeping_advance(). The extra conditional there is not hurting anyone.
    
    Remove the config option and unconditionally prevent negative motion of the
    readout.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: John Stultz <jstultz@google.com>
    Link: https://lore.kernel.org/all/20241031120328.599430157@linutronix.de
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/time/timekeeping_internal.h
+++ kernel/time/timekeeping_internal.h
@@ -15,7 +15,6 @@ extern void tk_debug_account_sleep_time(const struct timespec64 *t);
 #define tk_debug_account_sleep_time(x)
 #endif
 
-#ifdef CONFIG_CLOCKSOURCE_VALIDATE_LAST_CYCLE
 static inline u64 clocksource_delta(u64 now, u64 last, u64 mask)
 {
 	u64 ret = (now - last) & mask;
@@ -26,12 +25,6 @@ static inline u64 clocksource_delta(u64 now, u64 last, u64 mask)
 	 */
 	return ret & ~(mask >> 1) ? 0 : ret;
 }
-#else
-static inline u64 clocksource_delta(u64 now, u64 last, u64 mask)
-{
-	return (now - last) & mask;
-}
-#endif
 
 /* Semi public for serialization of non timekeeper VDSO updates. */
 extern raw_spinlock_t timekeeper_lock;
