8f5fab5329b7e966344d59fd1c17adbf9f025c52
Zhang Yi <zhangyi@everest-semi.com>
2024-10-31 14:02:53 +0800

    ASoC: codecs: ES8326: Reduce pop noise
    We modify the value of ES8326_ANA_MICBIAS to reduce the pop noise
    
    Signed-off-by: Zhang Yi <zhangyi@everest-semi.com>
    Link: https://patch.msgid.link/20241031060253.21001-1-zhangyi@everest-semi.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/es8326.c
+++ sound/soc/codecs/es8326.c
@@ -616,7 +616,7 @@ static int es8326_mute(struct snd_soc_dai *dai, int mute, int direction)
 					0x0F, 0x0F);
 			if (es8326->version > ES8326_VERSION_B) {
 				regmap_update_bits(es8326->regmap, ES8326_VMIDSEL, 0x40, 0x40);
-				regmap_update_bits(es8326->regmap, ES8326_ANA_MICBIAS, 0x70, 0x00);
+				regmap_update_bits(es8326->regmap, ES8326_ANA_MICBIAS, 0x70, 0x10);
 			}
 		}
 	} else {
@@ -1082,7 +1082,7 @@ static void es8326_init(struct snd_soc_component *component)
 	regmap_write(es8326->regmap, ES8326_ADC2_SRC, 0x66);
 	es8326_disable_micbias(es8326->component);
 	if (es8326->version > ES8326_VERSION_B) {
-		regmap_update_bits(es8326->regmap, ES8326_ANA_MICBIAS, 0x73, 0x03);
+		regmap_update_bits(es8326->regmap, ES8326_ANA_MICBIAS, 0x73, 0x13);
 		regmap_update_bits(es8326->regmap, ES8326_VMIDSEL, 0x40, 0x40);
 	}
 
