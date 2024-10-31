6cea47849d6e8c143192797893759fc185eee332
Ming Lei <ming.lei@redhat.com>
2024-10-31 21:37:20 +0800

    block: don't verify IO lock for freeze/unfreeze in elevator_init_mq()
    commit 357e1b7f730bd85a383e7afa75a3caba329c5707 upstream.
    
    elevator_init_mq() is only called at the entry of add_disk_fwnode() when
    disk IO isn't allowed yet.
    
    So not verify io lock(q->io_lockdep_map) for freeze & unfreeze in
    elevator_init_mq().
    
    Reported-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Reported-by: Lai Yi <yi1.lai@linux.intel.com>
    Fixes: f1be1788a32e ("block: model freeze & enter queue as lock for supporting lockdep")
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Link: https://lore.kernel.org/r/20241031133723.303835-5-ming.lei@redhat.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- block/elevator.c
+++ block/elevator.c
@@ -598,13 +598,19 @@ void elevator_init_mq(struct request_queue *q)
 	 * drain any dispatch activities originated from passthrough
 	 * requests, then no need to quiesce queue which may add long boot
 	 * latency, especially when lots of disks are involved.
+	 *
+	 * Disk isn't added yet, so verifying queue lock only manually.
 	 */
-	blk_mq_freeze_queue(q);
+	blk_freeze_queue_start_non_owner(q);
+	blk_freeze_acquire_lock(q, true, false);
+	blk_mq_freeze_queue_wait(q);
+
 	blk_mq_cancel_work_sync(q);
 
 	err = blk_mq_init_sched(q, e);
 
-	blk_mq_unfreeze_queue(q);
+	blk_unfreeze_release_lock(q, true, false);
+	blk_mq_unfreeze_queue_non_owner(q);
 
 	if (err) {
 		pr_warn("\"%s\" elevator initialization failed, "
