e89204070dac190bb38d4e4e87c9a0c88664f385
Heiko Carstens <hca@linux.ibm.com>
2024-11-04 11:03:41 +0100

    s390/sparsemem: Provide phys_to_target_node() with CONFIG_NUMA
    Add a trival phys_to_target_node() implementation which always returns 0 if
    CONFIG_NUMA is enabled, since the s390 NUMA implementation only supports
    node 0.
    This is similar to memory_add_physaddr_to_nid() in order to avoid runtime
    warnings.
    
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- arch/s390/include/asm/sparsemem.h
+++ arch/s390/include/asm/sparsemem.h
@@ -6,11 +6,19 @@
 #define MAX_PHYSMEM_BITS	CONFIG_MAX_PHYSMEM_BITS
 
 #ifdef CONFIG_NUMA
+
 static inline int memory_add_physaddr_to_nid(u64 addr)
 {
 	return 0;
 }
 #define memory_add_physaddr_to_nid memory_add_physaddr_to_nid
+
+static inline int phys_to_target_node(u64 start)
+{
+	return 0;
+}
+#define phys_to_target_node phys_to_target_node
+
 #endif /* CONFIG_NUMA */
 
 #endif /* _ASM_S390_SPARSEMEM_H */
