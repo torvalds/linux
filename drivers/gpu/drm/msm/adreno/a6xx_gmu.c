b34a7401ffaee45354e81b38a4d072794079cfd6
Sui Jingfeng <sui.jingfeng@linux.dev>
2024-11-04 17:07:38 +0800

    drm/msm: Check return value of of_dma_configure()
    Because the of_dma_configure() will returns '-EPROBE_DEFER' if the probe
    procedure of the specific platform IOMMU driver is not finished yet. It
    can also return other error code for various reasons.
    
    Stop pretending that it will always suceess, quit if it fail.
    
    Signed-off-by: Sui Jingfeng <sui.jingfeng@linux.dev>
    Reviewed-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
    Fixes: 29ac8979cdf7 ("drm/msm/a6xx: use msm_gem for GMU memory objects")
    Fixes: 5a903a44a984 ("drm/msm/a6xx: Introduce GMU wrapper support")
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Patchwork: https://patchwork.freedesktop.org/patch/622782/
    Link: https://lore.kernel.org/r/20241104090738.529848-1-sui.jingfeng@linux.dev
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- drivers/gpu/drm/msm/adreno/a6xx_gmu.c
+++ drivers/gpu/drm/msm/adreno/a6xx_gmu.c
@@ -1603,7 +1603,9 @@ int a6xx_gmu_wrapper_init(struct a6xx_gpu *a6xx_gpu, struct device_node *node)
 
 	gmu->dev = &pdev->dev;
 
-	of_dma_configure(gmu->dev, node, true);
+	ret = of_dma_configure(gmu->dev, node, true);
+	if (ret)
+		return ret;
 
 	pm_runtime_enable(gmu->dev);
 
@@ -1668,7 +1670,9 @@ int a6xx_gmu_init(struct a6xx_gpu *a6xx_gpu, struct device_node *node)
 
 	gmu->dev = &pdev->dev;
 
-	of_dma_configure(gmu->dev, node, true);
+	ret = of_dma_configure(gmu->dev, node, true);
+	if (ret)
+		return ret;
 
 	/* Fow now, don't do anything fancy until we get our feet under us */
 	gmu->idle_level = GMU_IDLE_STATE_ACTIVE;
