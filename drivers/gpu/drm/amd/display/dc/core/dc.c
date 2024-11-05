c3ea03c2a1557644386e38aaf2b5a9c261e0be1a
Austin Zheng <Austin.Zheng@amd.com>
2024-11-05 10:22:02 -0500

    drm/amd/display: Populate Power Profile In Case of Early Return
    Early return possible if context has no clk_mgr.
    This will lead to an invalid power profile being returned
    which looks identical to a profile with the lowest power level.
    Add back logic that populated the power profile and overwrite
    the value if needed.
    
    Cc: stable@vger.kernel.org
    Fixes: d016d0dd5a57 ("drm/amd/display: Update Interface to Check UCLK DPM")
    Reviewed-by: Dillon Varone <dillon.varone@amd.com>
    Signed-off-by: Austin Zheng <Austin.Zheng@amd.com>
    Signed-off-by: Hamza Mahfooz <hamza.mahfooz@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc.c
+++ drivers/gpu/drm/amd/display/dc/core/dc.c
@@ -6100,10 +6100,10 @@ struct dc_power_profile dc_get_power_profile_for_dc_state(const struct dc_state 
 {
 	struct dc_power_profile profile = { 0 };
 
-	if (!context || !context->clk_mgr || !context->clk_mgr->ctx || !context->clk_mgr->ctx->dc)
+	profile.power_level = !context->bw_ctx.bw.dcn.clk.p_state_change_support;
+	if (!context->clk_mgr || !context->clk_mgr->ctx || !context->clk_mgr->ctx->dc)
 		return profile;
 	struct dc *dc = context->clk_mgr->ctx->dc;
-
 
 	if (dc->res_pool->funcs->get_power_profile)
 		profile.power_level = dc->res_pool->funcs->get_power_profile(context);
