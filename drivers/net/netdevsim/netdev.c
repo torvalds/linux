494bd83bb5193dbd56a3dac3965b0bb6c312c930
Sabrina Dubroca <sd@queasysnail.net>
2024-11-07 00:13:27 +0100

    netdevsim: add more hw_features
    netdevsim currently only set HW_TC in its hw_features, but other
    features should also be present to better reflect the behavior of real
    HW.
    
    In my macsec offload testing, this ends up as HW_CSUM being missing
    from hw_features, so it doesn't stick in wanted_features when offload
    is turned off. Then HW_CSUM (and thus TSO, thanks to
    netdev_fix_features) is not automatically turned back on when offload
    is re-enabled.
    
    Signed-off-by: Sabrina Dubroca <sd@queasysnail.net>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://patch.msgid.link/b918dc4dd76410a57f7516a855f66b0a2bd58326.1730929545.git.sd@queasysnail.net
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/netdevsim/netdev.c
+++ drivers/net/netdevsim/netdev.c
@@ -663,7 +663,11 @@ static void nsim_setup(struct net_device *dev)
 			 NETIF_F_FRAGLIST |
 			 NETIF_F_HW_CSUM |
 			 NETIF_F_TSO;
-	dev->hw_features |= NETIF_F_HW_TC;
+	dev->hw_features |= NETIF_F_HW_TC |
+			    NETIF_F_SG |
+			    NETIF_F_FRAGLIST |
+			    NETIF_F_HW_CSUM |
+			    NETIF_F_TSO;
 	dev->max_mtu = ETH_MAX_MTU;
 	dev->xdp_features = NETDEV_XDP_ACT_HW_OFFLOAD;
 }
