cc7d10339d95bf6bdff1a56666fce6a556b0d500
Masahiro Yamada <masahiroy@kernel.org>
2024-11-07 01:14:40 +0900

    Rename .data.unlikely to .data..unlikely
    [ Upstream commit bb43a59944f45e89aa158740b8a16ba8f0b0fa2b ]
    
    Commit 7ccaba5314ca ("consolidate WARN_...ONCE() static variables")
    was intended to collect all .data.unlikely sections into one chunk.
    However, this has not worked when CONFIG_LD_DEAD_CODE_DATA_ELIMINATION
    or CONFIG_LTO_CLANG is enabled, because .data.unlikely matches the
    .data.[0-9a-zA-Z_]* pattern in the DATA_MAIN macro.
    
    Commit cb87481ee89d ("kbuild: linker script do not match C names unless
    LD_DEAD_CODE_DATA_ELIMINATION is configured") was introduced to suppress
    the issue for the default CONFIG_LD_DEAD_CODE_DATA_ELIMINATION=n case,
    providing a minimal fix for stable backporting. We were aware this did
    not address the issue for CONFIG_LD_DEAD_CODE_DATA_ELIMINATION=y. The
    plan was to apply correct fixes and then revert cb87481ee89d. [1]
    
    Seven years have passed since then, yet the #ifdef workaround remains in
    place.
    
    Using a ".." separator in the section name fixes the issue for
    CONFIG_LD_DEAD_CODE_DATA_ELIMINATION and CONFIG_LTO_CLANG.
    
    [1]: https://lore.kernel.org/linux-kbuild/CAK7LNASck6BfdLnESxXUeECYL26yUDm0cwRZuM4gmaWUkxjL5g@mail.gmail.com/
    
    Fixes: cb87481ee89d ("kbuild: linker script do not match C names unless LD_DEAD_CODE_DATA_ELIMINATION is configured")
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -350,7 +350,7 @@
 	*(.data..decrypted)						\
 	*(.ref.data)							\
 	*(.data..shared_aligned) /* percpu related */			\
-	*(.data.unlikely)						\
+	*(.data..unlikely)						\
 	__start_once = .;						\
 	*(.data.once)							\
 	__end_once = .;							\
