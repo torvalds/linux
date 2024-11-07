torvalds/linux

commit ad59cf382cd5c0548b3aeb80cb1e34ebac40ade6

staging: gpib: add module descriptions
Every loadable module should have a description, and not having one
causes a W=1 build warning, so add these to the newly added modules.

Signed-off-by: Arnd Bergmann <arnd@arndb.de>
Link: https://lore.kernel.org/r/20241016111521.1143191-2-arnd@kernel.org
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

drivers/staging/gpib/agilent_82357a/agilent_82357a.c
@@ -15,6 +15,7 @@
 #include "tms9914.h"
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("GPIB driver for Agilent 82357A/B usb adapter");
 
 #define MAX_NUM_82357A_INTERFACES 128
 static struct usb_interface *agilent_82357a_driver_interfaces[MAX_NUM_82357A_INTERFACES];

drivers/staging/gpib/agilent_82350b/agilent_82350b.c
@@ -18,6 +18,7 @@
 #include <linux/wait.h>
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("GPIB driver for Agilent 82350b");
 
 int agilent_82350b_accel_read(gpib_board_t *board, uint8_t *buffer, size_t length, int *end,
 			      size_t *bytes_read)

drivers/staging/gpib/cb7210/cb7210.c
@@ -21,6 +21,7 @@
 #include "quancom_pci.h"
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("GPIB driver Measurement Computing boards using cb7210.2 and cbi488.2");
 
 static inline int have_fifo_word(const struct cb7210_priv *cb_priv)
 {

drivers/staging/gpib/cec/cec_gpib.c
@@ -13,6 +13,7 @@
 #include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("GPIB driver for CEC PCI and PCMCIA boards");
 
 /*
  * GPIB interrupt service routines

drivers/staging/gpib/common/gpib_os.c
@@ -20,6 +20,7 @@
 #include <linux/uaccess.h>
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("GPIB base support");
 MODULE_ALIAS_CHARDEV_MAJOR(GPIB_CODE);
 
 static int board_type_ioctl(gpib_file_private_t *file_priv, gpib_board_t *board, unsigned long arg);

drivers/staging/gpib/eastwood/fluke_gpib.c
@@ -18,6 +18,7 @@
 #include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("GPIB Driver for Fluke cda devices");
 
 static int fluke_attach_holdoff_all(gpib_board_t *board, const gpib_board_config_t *config);
 static int fluke_attach_holdoff_end(gpib_board_t *board, const gpib_board_config_t *config);

drivers/staging/gpib/gpio/gpib_bitbang.c
@@ -368,6 +368,7 @@ static inline void SET_DIR_READ(struct bb_priv *priv);
 #define DIR_WRITE 1
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("GPIB helper functions for bitbanging I/O");
 
 /****  global variables	 ****/
 #ifdef CONFIG_GPIB_DEBUG

drivers/staging/gpib/hp_82335/hp82335.c
@@ -17,6 +17,7 @@
 #include <linux/init.h>
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("GPIB driver for HP 82335 interface cards");
 
 static int hp82335_attach(gpib_board_t *board, const gpib_board_config_t *config);
 

commit 7fa4e5bc10557fbdf459b66ce445c74c35b203a4

staging: gpib: Fix MODULES_DESCRIPTION
Use plural for adapters

Fixes: ad59cf382cd5 ("staging: gpib: add module descriptions")
Signed-off-by: Dave Penkler <dpenkler@gmail.com>
Reviewed-by: Dan Carpenter <dan.carpenter@linaro.org>
Link: https://lore.kernel.org/r/20241104175014.12317-6-dpenkler@gmail.com
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

drivers/staging/gpib/agilent_82357a/agilent_82357a.c
@@ -15,7 +15,7 @@
 #include "tms9914.h"
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("GPIB driver for Agilent 82357A/B usb adapter");
+MODULE_DESCRIPTION("GPIB driver for Agilent 82357A/B usb adapters");
 
 #define MAX_NUM_82357A_INTERFACES 128
 static struct usb_interface *agilent_82357a_driver_interfaces[MAX_NUM_82357A_INTERFACES];
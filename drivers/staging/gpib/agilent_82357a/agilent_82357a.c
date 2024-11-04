7fa4e5bc10557fbdf459b66ce445c74c35b203a4
Dave Penkler <dpenkler@gmail.com>
2024-11-04 18:50:06 +0100

    staging: gpib: Fix MODULES_DESCRIPTION
    Use plural for adapters
    
    Fixes: ad59cf382cd5 ("staging: gpib: add module descriptions")
    Signed-off-by: Dave Penkler <dpenkler@gmail.com>
    Reviewed-by: Dan Carpenter <dan.carpenter@linaro.org>
    Link: https://lore.kernel.org/r/20241104175014.12317-6-dpenkler@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/gpib/agilent_82357a/agilent_82357a.c
+++ drivers/staging/gpib/agilent_82357a/agilent_82357a.c
@@ -15,7 +15,7 @@
 #include "tms9914.h"
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("GPIB driver for Agilent 82357A/B usb adapter");
+MODULE_DESCRIPTION("GPIB driver for Agilent 82357A/B usb adapters");
 
 #define MAX_NUM_82357A_INTERFACES 128
 static struct usb_interface *agilent_82357a_driver_interfaces[MAX_NUM_82357A_INTERFACES];
