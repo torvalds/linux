cec6b33a6786e3ecad4b1d4bb7843d122ffa1912
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2024-11-06 23:15:30 -0800

    Input: ff-memless - convert locking to guard notation
    Use guard() notation instead of explicitly acquiring and releasing
    spinlocks to simplify the code and ensure that all locks are released.
    
    Link: https://lore.kernel.org/r/20241107071538.195340-4-dmitry.torokhov@gmail.com
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/ff-memless.c
+++ drivers/input/ff-memless.c
@@ -401,13 +401,11 @@ static void ml_effect_timer(struct timer_list *t)
 {
 	struct ml_device *ml = from_timer(ml, t, timer);
 	struct input_dev *dev = ml->dev;
-	unsigned long flags;
 
 	pr_debug("timer: updating effects\n");
 
-	spin_lock_irqsave(&dev->event_lock, flags);
+	guard(spinlock_irqsave)(&dev->event_lock);
 	ml_play_effects(ml);
-	spin_unlock_irqrestore(&dev->event_lock, flags);
 }
 
 /*
@@ -465,7 +463,7 @@ static int ml_ff_upload(struct input_dev *dev,
 	struct ml_device *ml = dev->ff->private;
 	struct ml_effect_state *state = &ml->states[effect->id];
 
-	spin_lock_irq(&dev->event_lock);
+	guard(spinlock_irq)(&dev->event_lock);
 
 	if (test_bit(FF_EFFECT_STARTED, &state->flags)) {
 		__clear_bit(FF_EFFECT_PLAYING, &state->flags);
@@ -476,8 +474,6 @@ static int ml_ff_upload(struct input_dev *dev,
 		state->adj_at = state->play_at;
 		ml_schedule_timer(ml);
 	}
-
-	spin_unlock_irq(&dev->event_lock);
 
 	return 0;
 }
