449b1a71788af14c6ecebf1eab4f004b86b281b8
Xu Kuohai <xukuohai@huawei.com>
2024-11-12 22:58:49 +0800

    bpf: Add kernel symbol for struct_ops trampoline
    [ Upstream commit 7c8ce4ffb684676039b1ff9ff81c126794e8d88e ]
    
    Without kernel symbols for struct_ops trampoline, the unwinder may
    produce unexpected stacktraces.
    
    For example, the x86 ORC and FP unwinders check if an IP is in kernel
    text by verifying the presence of the IP's kernel symbol. When a
    struct_ops trampoline address is encountered, the unwinder stops due
    to the absence of symbol, resulting in an incomplete stacktrace that
    consists only of direct and indirect child functions called from the
    trampoline.
    
    The arm64 unwinder is another example. While the arm64 unwinder can
    proceed across a struct_ops trampoline address, the corresponding
    symbol name is displayed as "unknown", which is confusing.
    
    Thus, add kernel symbol for struct_ops trampoline. The name is
    bpf__<struct_ops_name>_<member_name>, where <struct_ops_name> is the
    type name of the struct_ops, and <member_name> is the name of
    the member that the trampoline is linked to.
    
    Below is a comparison of stacktraces captured on x86 by perf record,
    before and after this patch.
    
    Before:
    ffffffff8116545d __lock_acquire+0xad ([kernel.kallsyms])
    ffffffff81167fcc lock_acquire+0xcc ([kernel.kallsyms])
    ffffffff813088f4 __bpf_prog_enter+0x34 ([kernel.kallsyms])
    
    After:
    ffffffff811656bd __lock_acquire+0x30d ([kernel.kallsyms])
    ffffffff81167fcc lock_acquire+0xcc ([kernel.kallsyms])
    ffffffff81309024 __bpf_prog_enter+0x34 ([kernel.kallsyms])
    ffffffffc000d7e9 bpf__tcp_congestion_ops_cong_avoid+0x3e ([kernel.kallsyms])
    ffffffff81f250a5 tcp_ack+0x10d5 ([kernel.kallsyms])
    ffffffff81f27c66 tcp_rcv_established+0x3b6 ([kernel.kallsyms])
    ffffffff81f3ad03 tcp_v4_do_rcv+0x193 ([kernel.kallsyms])
    ffffffff81d65a18 __release_sock+0xd8 ([kernel.kallsyms])
    ffffffff81d65af4 release_sock+0x34 ([kernel.kallsyms])
    ffffffff81f15c4b tcp_sendmsg+0x3b ([kernel.kallsyms])
    ffffffff81f663d7 inet_sendmsg+0x47 ([kernel.kallsyms])
    ffffffff81d5ab40 sock_write_iter+0x160 ([kernel.kallsyms])
    ffffffff8149c67b vfs_write+0x3fb ([kernel.kallsyms])
    ffffffff8149caf6 ksys_write+0xc6 ([kernel.kallsyms])
    ffffffff8149cb5d __x64_sys_write+0x1d ([kernel.kallsyms])
    ffffffff81009200 x64_sys_call+0x1d30 ([kernel.kallsyms])
    ffffffff82232d28 do_syscall_64+0x68 ([kernel.kallsyms])
    ffffffff8240012f entry_SYSCALL_64_after_hwframe+0x76 ([kernel.kallsyms])
    
    Fixes: 85d33df357b6 ("bpf: Introduce BPF_MAP_TYPE_STRUCT_OPS")
    Signed-off-by: Xu Kuohai <xukuohai@huawei.com>
    Acked-by: Yonghong Song <yonghong.song@linux.dev>
    Link: https://lore.kernel.org/r/20241112145849.3436772-4-xukuohai@huaweicloud.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -1373,7 +1373,8 @@ int arch_prepare_bpf_dispatcher(void *image, void *buf, s64 *funcs, int num_func
 void bpf_dispatcher_change_prog(struct bpf_dispatcher *d, struct bpf_prog *from,
 				struct bpf_prog *to);
 /* Called only from JIT-enabled code, so there's no need for stubs. */
-void bpf_image_ksym_add(void *data, unsigned int size, struct bpf_ksym *ksym);
+void bpf_image_ksym_init(void *data, unsigned int size, struct bpf_ksym *ksym);
+void bpf_image_ksym_add(struct bpf_ksym *ksym);
 void bpf_image_ksym_del(struct bpf_ksym *ksym);
 void bpf_ksym_add(struct bpf_ksym *ksym);
 void bpf_ksym_del(struct bpf_ksym *ksym);
