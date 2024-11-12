bfe52e8019efca7353368ee60a469dbaa0eced3b
Robert Beckett <bob.beckett@collabora.com>
2024-11-12 19:50:00 +0000

    nvme-pci: 512 byte aligned dma pool segment quirk
    [ Upstream commit ebefac5647968679f6ef5803e5d35a71997d20fa ]
    
    We initially introduced a quick fix limiting the queue depth to 1 as
    experimentation showed that it fixed data corruption on 64GB steamdecks.
    
    Further experimentation revealed corruption only happens when the last
    PRP data element aligns to the end of the page boundary. The device
    appears to treat this as a PRP chain to a new list instead of the data
    element that it actually is. This implementation is in violation of the
    spec. Encountering this errata with the Linux driver requires the host
    request a 128k transfer and coincidently be handed the last small pool
    dma buffer within a page.
    
    The QD1 quirk effectly works around this because the last data PRP
    always was at a 248 byte offset from the page start, so it never
    appeared at the end of the page, but comes at the expense of throttling
    IO and wasting the remainder of the PRP page beyond 256 bytes. Also to
    note, the MDTS on these devices is small enough that the "large" prp
    pool can hold enough PRP elements to never reach the end, so that pool
    is not a problem either.
    
    Introduce a new quirk to ensure the small pool is always aligned such
    that the last PRP element can't appear a the end of the page. This comes
    at the expense of wasting 256 bytes per small pool page allocated.
    
    Link: https://lore.kernel.org/linux-nvme/20241113043151.GA20077@lst.de/T/#u
    Fixes: 83bdfcbdbe5d ("nvme-pci: qdepth 1 quirk")
    Cc: Paweł Anikiel <panikiel@google.com>
    Signed-off-by: Robert Beckett <bob.beckett@collabora.com>
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -2690,15 +2690,20 @@ static int nvme_disable_prepare_reset(struct nvme_dev *dev, bool shutdown)
 
 static int nvme_setup_prp_pools(struct nvme_dev *dev)
 {
+	size_t small_align = 256;
+
 	dev->prp_page_pool = dma_pool_create("prp list page", dev->dev,
 						NVME_CTRL_PAGE_SIZE,
 						NVME_CTRL_PAGE_SIZE, 0);
 	if (!dev->prp_page_pool)
 		return -ENOMEM;
 
+	if (dev->ctrl.quirks & NVME_QUIRK_DMAPOOL_ALIGN_512)
+		small_align = 512;
+
 	/* Optimisation for I/Os between 4k and 128k */
 	dev->prp_small_pool = dma_pool_create("prp list 256", dev->dev,
-						256, 256, 0);
+						256, small_align, 0);
 	if (!dev->prp_small_pool) {
 		dma_pool_destroy(dev->prp_page_pool);
 		return -ENOMEM;
@@ -3446,7 +3451,7 @@ static const struct pci_device_id nvme_id_table[] = {
 	{ PCI_VDEVICE(REDHAT, 0x0010),	/* Qemu emulated controller */
 		.driver_data = NVME_QUIRK_BOGUS_NID, },
 	{ PCI_DEVICE(0x1217, 0x8760), /* O2 Micro 64GB Steam Deck */
-		.driver_data = NVME_QUIRK_QDEPTH_ONE },
+		.driver_data = NVME_QUIRK_DMAPOOL_ALIGN_512, },
 	{ PCI_DEVICE(0x126f, 0x2262),	/* Silicon Motion generic */
 		.driver_data = NVME_QUIRK_NO_DEEPEST_PS |
 				NVME_QUIRK_BOGUS_NID, },
