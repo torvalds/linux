1133853bd68762a0440a2382e93502bd5e827aca
Nirmoy Das <nirmoy.das@intel.com>
2024-11-14 16:05:37 +0100

    drm/xe/ufence: Wake up waiters after setting ufence->signalled
    [ Upstream commit 37a1cf288e4538eb39b38dbc745fe0da7ae53d94 ]
    
    If a previous ufence is not signalled, vm_bind will return -EBUSY.
    Delaying the modification of ufence->signalled can cause issues if the
    UMD reuses the same ufence so update ufence->signalled before waking up
    waiters.
    
    Cc: Matthew Brost <matthew.brost@intel.com>
    Link: https://gitlab.freedesktop.org/drm/xe/kernel/-/issues/3233
    Fixes: 977e5b82e090 ("drm/xe: Expose user fence from xe_sync_entry")
    Reviewed-by: Matthew Brost <matthew.brost@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241114150537.4161573-1-nirmoy.das@intel.com
    Signed-off-by: Nirmoy Das <nirmoy.das@intel.com>
    (cherry picked from commit 553a5d14fcd927194c409b10faced6a6dbc678d1)
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/xe/xe_sync.c
+++ drivers/gpu/drm/xe/xe_sync.c
@@ -85,8 +85,12 @@ static void user_fence_worker(struct work_struct *w)
 		mmput(ufence->mm);
 	}
 
-	wake_up_all(&ufence->xe->ufence_wq);
+	/*
+	 * Wake up waiters only after updating the ufence state, allowing the UMD
+	 * to safely reuse the same ufence without encountering -EBUSY errors.
+	 */
 	WRITE_ONCE(ufence->signalled, 1);
+	wake_up_all(&ufence->xe->ufence_wq);
 	user_fence_put(ufence);
 }
 
