7a91aee8ef811d3770c8ea30bd80e1d8d719f757
Han Xu <han.xu@nxp.com>
2024-11-15 17:37:23 -0500

    iio: gyro: Add support for iam20380 sensor
    Add support for the Invensense IAM20380 sensor to the MPU6050 driver. It is
    similar to the IAM20680. But IAM20380 only supports gyro and WHOAMI
    register data is difference.
    
    Signed-off-by: Han Xu <han.xu@nxp.com>
    Signed-off-by: Frank Li <Frank.Li@nxp.com>
    Acked-by: Jean-Baptiste Maneyrol <jean-baptiste.maneyrol@tdk.com>
    Link: https://patch.msgid.link/20241115-iam20380-v2-2-d8d9dc6891f5@nxp.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/imu/inv_mpu6050/inv_mpu_i2c.c
+++ drivers/iio/imu/inv_mpu6050/inv_mpu_i2c.c
@@ -34,6 +34,7 @@ static bool inv_mpu_i2c_aux_bus(struct device *dev)
 	case INV_ICM20689:
 	case INV_ICM20600:
 	case INV_ICM20602:
+	case INV_IAM20380:
 	case INV_IAM20680:
 		/* no i2c auxiliary bus on the chip */
 		return false;
@@ -187,6 +188,7 @@ static const struct i2c_device_id inv_mpu_id[] = {
 	{"icm20600", INV_ICM20600},
 	{"icm20602", INV_ICM20602},
 	{"icm20690", INV_ICM20690},
+	{"iam20380", INV_IAM20380},
 	{"iam20680", INV_IAM20680},
 	{"iam20680hp", INV_IAM20680HP},
 	{"iam20680ht", INV_IAM20680HT},
@@ -251,6 +253,10 @@ static const struct of_device_id inv_of_match[] = {
 	{
 		.compatible = "invensense,icm20690",
 		.data = (void *)INV_ICM20690
+	},
+	{
+		.compatible = "invensense,iam20380",
+		.data = (void *)INV_IAM20380
 	},
 	{
 		.compatible = "invensense,iam20680",
