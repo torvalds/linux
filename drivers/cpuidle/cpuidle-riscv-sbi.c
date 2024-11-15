0ea7a3e44255860d7424caebd9be0b0b2146eb97
Javier Carrasco <javier.carrasco.cruz@gmail.com>
2024-11-16 00:32:39 +0100

    cpuidle: riscv-sbi: fix device node release in early exit of for_each_possible_cpu
    [ Upstream commit 7e25044b804581b9c029d5a28d8800aebde18043 ]
    
    The 'np' device_node is initialized via of_cpu_device_node_get(), which
    requires explicit calls to of_node_put() when it is no longer required
    to avoid leaking the resource.
    
    Instead of adding the missing calls to of_node_put() in all execution
    paths, use the cleanup attribute for 'np' by means of the __free()
    macro, which automatically calls of_node_put() when the variable goes
    out of scope. Given that 'np' is only used within the
    for_each_possible_cpu(), reduce its scope to release the nood after
    every iteration of the loop.
    
    Fixes: 6abf32f1d9c5 ("cpuidle: Add RISC-V SBI CPU idle driver")
    Reviewed-by: Andrew Jones <ajones@ventanamicro.com>
    Signed-off-by: Javier Carrasco <javier.carrasco.cruz@gmail.com>
    Link: https://lore.kernel.org/r/20241116-cpuidle-riscv-sbi-cleanup-v3-1-a3a46372ce08@gmail.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/cpuidle/cpuidle-riscv-sbi.c
+++ drivers/cpuidle/cpuidle-riscv-sbi.c
@@ -500,12 +500,12 @@ static int sbi_cpuidle_probe(struct platform_device *pdev)
 	int cpu, ret;
 	struct cpuidle_driver *drv;
 	struct cpuidle_device *dev;
-	struct device_node *np, *pds_node;
+	struct device_node *pds_node;
 
 	/* Detect OSI support based on CPU DT nodes */
 	sbi_cpuidle_use_osi = true;
 	for_each_possible_cpu(cpu) {
-		np = of_cpu_device_node_get(cpu);
+		struct device_node *np __free(device_node) = of_cpu_device_node_get(cpu);
 		if (np &&
 		    of_property_present(np, "power-domains") &&
 		    of_property_present(np, "power-domain-names")) {
