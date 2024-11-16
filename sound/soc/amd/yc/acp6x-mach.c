e451d7ca7870fe3c970e91532ffeb7272f36f894
Alex Far <anf1980@gmail.com>
2024-11-16 21:58:45 +0300

    ASoC: amd: yc: fix internal mic on Redmi G 2022
    [ Upstream commit 67a0463d339059eeeead9cd015afa594659cfdaf ]
    
    This laptop model requires an additional detection quirk to enable the
    internal microphone
    
    Signed-off-by: Alex Far <anf1980@gmail.com>
    Link: https://patch.msgid.link/ZzjrZY3sImcqTtGx@RedmiG
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- sound/soc/amd/yc/acp6x-mach.c
+++ sound/soc/amd/yc/acp6x-mach.c
@@ -419,6 +419,13 @@ static const struct dmi_system_id yc_acp_quirk_table[] = {
 	{
 		.driver_data = &acp6x_card,
 		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "TIMI"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "Redmi G 2022"),
+		}
+	},
+	{
+		.driver_data = &acp6x_card,
+		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "Razer"),
 			DMI_MATCH(DMI_PRODUCT_NAME, "Blade 14 (2022) - RZ09-0427"),
 		}
