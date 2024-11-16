bfff915c97d548cdaba471eb0c87ffad42ad0e21
Greg Kroah-Hartman <gregkh@google.com>
2024-11-16 10:14:15 +0000

    Revert "arm64: probes: Fix uprobes for big-endian kernels"
    This reverts commit 14841bb7a531b96e2dde37423a3b33e75147c60d which is
    commit 13f8f1e05f1dc36dbba6cba0ae03354c0dafcde7 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I3e6141d5184c37814b1ea62f3e2966e97e3e3932
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- arch/arm64/include/asm/uprobes.h
+++ arch/arm64/include/asm/uprobes.h
@@ -10,9 +10,11 @@
 #include <asm/insn.h>
 #include <asm/probes.h>
 
+#define MAX_UINSN_BYTES		AARCH64_INSN_SIZE
+
 #define UPROBE_SWBP_INSN	cpu_to_le32(BRK64_OPCODE_UPROBES)
 #define UPROBE_SWBP_INSN_SIZE	AARCH64_INSN_SIZE
-#define UPROBE_XOL_SLOT_BYTES	AARCH64_INSN_SIZE
+#define UPROBE_XOL_SLOT_BYTES	MAX_UINSN_BYTES
 
 typedef u32 uprobe_opcode_t;
 
@@ -21,8 +23,8 @@ struct arch_uprobe_task {
 
 struct arch_uprobe {
 	union {
-		__le32 insn;
-		__le32 ixol;
+		u8 insn[MAX_UINSN_BYTES];
+		u8 ixol[MAX_UINSN_BYTES];
 	};
 	struct arch_probe_insn api;
 	bool simulate;
