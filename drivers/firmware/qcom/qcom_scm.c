9b01fc6bb1fca6ab2b7edb04b55bf106cf489118
Maud Spierings <maud_spierings@hotmail.com>
2024-11-16 14:44:39 +0100

    firmware: qcom: scm: Allow QSEECOM on the asus vivobook s15
    Add the asus vivobook s15 to the compatible list to allow access to
    efivars
    
    Signed-off-by: Maud Spierings <maud_spierings@hotmail.com>
    Link: https://lore.kernel.org/r/20241116-add_asus_qcom_scm-v1-1-5aa2b0fb52bd@hotmail.com
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -1768,6 +1768,7 @@ EXPORT_SYMBOL_GPL(qcom_scm_qseecom_app_send);
  + any potential issues with this, only allow validated machines for now.
  */
 static const struct of_device_id qcom_scm_qseecom_allowlist[] __maybe_unused = {
+	{ .compatible = "asus,vivobook-s15" },
 	{ .compatible = "dell,xps13-9345" },
 	{ .compatible = "lenovo,flex-5g" },
 	{ .compatible = "lenovo,thinkpad-t14s" },
