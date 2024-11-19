49ccf2c3cafb5774a4562d61294afcf492bed487
Yang Shi <yang@os.amperecomputing.com>
2024-11-19 12:09:14 -0800

    arm64: mte: set VM_MTE_ALLOWED for hugetlbfs at correct place
    The commit 5de195060b2e ("mm: resolve faulty mmap_region() error path
    behaviour") moved vm flags validation before fop->mmap for file
    mappings.  But when commit 25c17c4b55de ("hugetlb: arm64: add mte support")
    was rebased on top of it, the hugetlbfs part was missed.  Mmapping
    hugetlbfs file may not have MAP_HUGETLB set.
    
    Fixes: 25c17c4b55de ("hugetlb: arm64: add mte support")
    Signed-off-by: Yang Shi <yang@os.amperecomputing.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/20241119200914.1145249-1-yang@os.amperecomputing.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- fs/hugetlbfs/inode.c
+++ fs/hugetlbfs/inode.c
@@ -113,7 +113,7 @@ static int hugetlbfs_file_mmap(struct file *file, struct vm_area_struct *vma)
 	 * way when do_mmap unwinds (may be important on powerpc
 	 * and ia64).
 	 */
-	vm_flags_set(vma, VM_HUGETLB | VM_DONTEXPAND | VM_MTE_ALLOWED);
+	vm_flags_set(vma, VM_HUGETLB | VM_DONTEXPAND);
 	vma->vm_ops = &hugetlb_vm_ops;
 
 	ret = seal_check_write(info->seals, vma);
