e7bb49e3f6435ff3611b83f78a61d387f24d80f8
Ian Rogers <irogers@google.com>
2024-11-18 17:16:34 -0800

    perf x86: Define arch_fetch_insn in NO_AUXTRACE builds
    archinsn.c containing arch_fetch_insn was only enabled with
    CONFIG_AUXTRACE, but this meant that a NO_AUXTRACE build on x86 would
    use the empty weak version of arch_fetch_insn - weak symbols are a
    frequent source of errors like this and are outside of the C
    specification. Change it so that archinsn.c is always built on x86 and
    make the weak symbol empty version of arch_fetch_insn a strong one
    guarded by ifdefs.
    
    arch_fetch_insn on x86 depends on insn_decode which is a function
    included then built into intel-pt-insn-decoder.c.
    intel-pt-insn-decoder.c isn't built in a NO_AUXTRACE=1 build. Separate
    the insn_decode function from intel-pt-insn-decoder.c by just directly
    compiling the relevant file. Guard this compilation to be for either
    always on x86 (because of the use in arch_fetch_insn) or when auxtrace
    is enabled. Apply the CFLAGS overrides as necessary, reducing the amount
    of code where warnings are disabled.
    
    Signed-off-by: Ian Rogers <irogers@google.com>
    Tested-by: Adrian Hunter <adrian.hunter@intel.com>
    Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Athira Rajeev <atrajeev@linux.vnet.ibm.com>
    Cc: Colin Ian King <colin.i.king@gmail.com>
    Cc: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Cc: Howard Chu <howardchu95@gmail.com>
    Cc: Ilya Leoshkevich <iii@linux.ibm.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: James Clark <james.clark@linaro.org>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Kan Liang <kan.liang@linux.intel.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Michael Petlan <mpetlan@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Richter <tmricht@linux.ibm.com>
    Cc: Veronika Molnarova <vmolnaro@redhat.com>
    Cc: Weilin Wang <weilin.wang@intel.com>
    Link: https://lore.kernel.org/r/20241119011644.971342-13-irogers@google.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/trace-event-scripting.c
+++ tools/perf/util/trace-event-scripting.c
@@ -274,11 +274,13 @@ void setup_perl_scripting(void)
 #endif
 #endif
 
-__weak void arch_fetch_insn(struct perf_sample *sample __maybe_unused,
+#if !defined(__i386__) && !defined(__x86_64__)
+void arch_fetch_insn(struct perf_sample *sample __maybe_unused,
 		     struct thread *thread __maybe_unused,
 		     struct machine *machine __maybe_unused)
 {
 }
+#endif
 
 void script_fetch_insn(struct perf_sample *sample, struct thread *thread,
 		       struct machine *machine, bool native_arch)
