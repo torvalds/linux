6a1853bdf17874392476b552398df261f75503e0
Aruna Ramakrishna <aruna.ramakrishna@oracle.com>
2024-11-19 17:45:19 +0000

    x86/pkeys: Change caller of update_pkru_in_sigframe()
    update_pkru_in_sigframe() will shortly need some information which
    is only available inside xsave_to_user_sigframe(). Move
    update_pkru_in_sigframe() inside the other function to make it
    easier to provide it that information.
    
    No functional changes.
    
    Signed-off-by: Aruna Ramakrishna <aruna.ramakrishna@oracle.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Link: https://lore.kernel.org/all/20241119174520.3987538-2-aruna.ramakrishna%40oracle.com

--- arch/x86/kernel/fpu/xstate.h
+++ arch/x86/kernel/fpu/xstate.h
@@ -69,6 +69,16 @@ static inline u64 xfeatures_mask_independent(void)
 	return fpu_kernel_cfg.independent_features;
 }
 
+/*
+ * Update the value of PKRU register that was already pushed onto the signal frame.
+ */
+static inline int update_pkru_in_sigframe(struct xregs_state __user *buf, u32 pkru)
+{
+	if (unlikely(!cpu_feature_enabled(X86_FEATURE_OSPKE)))
+		return 0;
+	return __put_user(pkru, (unsigned int __user *)get_xsave_addr_user(buf, XFEATURE_PKRU));
+}
+
 /* XSAVE/XRSTOR wrapper functions */
 
 #ifdef CONFIG_X86_64
@@ -256,7 +266,7 @@ static inline u64 xfeatures_need_sigframe_write(void)
  * The caller has to zero buf::header before calling this because XSAVE*
  * does not touch the reserved fields in the header.
  */
-static inline int xsave_to_user_sigframe(struct xregs_state __user *buf)
+static inline int xsave_to_user_sigframe(struct xregs_state __user *buf, u32 pkru)
 {
 	/*
 	 * Include the features which are not xsaved/rstored by the kernel
@@ -280,6 +290,9 @@ static inline int xsave_to_user_sigframe(struct xregs_state __user *buf)
 	stac();
 	XSTATE_OP(XSAVE, buf, lmask, hmask, err);
 	clac();
+
+	if (!err)
+		err = update_pkru_in_sigframe(buf, pkru);
 
 	return err;
 }
