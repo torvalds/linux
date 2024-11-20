37624dde4768ec25d2f9798aa75bf32e18c0eae2
Lyude Paul <lyude@redhat.com>
2024-11-20 17:26:28 -0500

    rust: sync: Add MutexGuard type alias
    A simple helper alias for code that needs to deal with Guard types returned
    from Mutexes.
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>
    Link: https://lore.kernel.org/r/20241120222742.2490495-2-lyude@redhat.com

--- rust/kernel/sync/lock/mutex.rs
+++ rust/kernel/sync/lock/mutex.rs
@@ -86,6 +86,14 @@ pub use new_mutex;
 /// [`struct mutex`]: srctree/include/linux/mutex.h
 pub type Mutex<T> = super::Lock<T, MutexBackend>;
 
+/// A [`Guard`] acquired from locking a [`Mutex`].
+///
+/// This is simply a type alias for a [`Guard`] returned from locking a [`Mutex`]. It will unlock
+/// the [`Mutex`] upon being dropped.
+///
+/// [`Guard`]: super::Guard
+pub type MutexGuard<'a, T> = super::Guard<'a, T, MutexBackend>;
+
 /// A kernel `struct mutex` lock backend.
 pub struct MutexBackend;
 
