adceb416888daf0c53048d1cc5411527dcc8d24c
Jani Nikula <jani.nikula@intel.com>
2024-11-20 14:03:34 +0200

    drm/i915/display: add intel_display_conversion.c to hide stuff better
    The __to_intel_display() generics require the definition of struct
    drm_i915_private i.e. inclusion of i915_drv.h. Add
    intel_display_conversion.c with a __i915_to_display() function to do the
    conversion without the intel_display_conversion.h having an implicit
    dependency on i915_drv.h.
    
    The long term goal is to remove __to_intel_display() and the
    intel_display_conversion.[ch] files altoghether, and this is merely a
    transitional step to make the dependencies on i915_drv.h explicit.
    
    Reviewed-by: Luca Coelho <luciano.coelho@intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/39e99b765b8c1a05d001659c39686a661ac268e2.1732104170.git.jani.nikula@intel.com

---
+++ drivers/gpu/drm/i915/display/intel_display_conversion.c
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+/* Copyright © 2024 Intel Corporation */
+
+#include "i915_drv.h"
+
+struct intel_display *__i915_to_display(struct drm_i915_private *i915)
+{
+	return &i915->display;
+}
