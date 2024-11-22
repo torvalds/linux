cbc86dd0a4fe9f8c41075328c2e740b68419d639
Uwe Kleine-König <ukleinek@debian.org>
2024-11-22 08:56:05 +0100

    ASoC: amd: yc: Add quirk for microphone on Lenovo Thinkpad T14s Gen 6 21M1CTO1WW
    Add a quirk for Tova's Lenovo Thinkpad T14s with product name 21M1.
    
    Suggested-by: Tova <blueaddagio@laposte.net>
    Link: https://bugs.debian.org/1087673
    Signed-off-by: Uwe Kleine-König <ukleinek@debian.org>
    Link: https://patch.msgid.link/20241122075606.213132-2-ukleinek@debian.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/yc/acp6x-mach.c
+++ sound/soc/amd/yc/acp6x-mach.c
@@ -224,6 +224,13 @@ static const struct dmi_system_id yc_acp_quirk_table[] = {
 		.driver_data = &acp6x_card,
 		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "LENOVO"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "21M1"),
+		}
+	},
+	{
+		.driver_data = &acp6x_card,
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "LENOVO"),
 			DMI_MATCH(DMI_PRODUCT_NAME, "21M3"),
 		}
 	},
