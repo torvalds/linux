d727935cad9f6f52c8d184968f9720fdc966c669
Jinliang Zheng <alexjlzheng@gmail.com>
2024-11-24 11:46:36 +0800

    fs: fix proc_handler for sysctl_nr_open
    Use proc_douintvec_minmax() instead of proc_dointvec_minmax() to handle
    sysctl_nr_open, because its data type is unsigned int, not int.
    
    Fixes: 9b80a184eaad ("fs/file: more unsigned file descriptors")
    Signed-off-by: Jinliang Zheng <alexjlzheng@tencent.com>
    Link: https://lore.kernel.org/r/20241124034636.325337-1-alexjlzheng@tencent.com
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- fs/file_table.c
+++ fs/file_table.c
@@ -128,7 +128,7 @@ static struct ctl_table fs_stat_sysctls[] = {
 		.data		= &sysctl_nr_open,
 		.maxlen		= sizeof(unsigned int),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec_minmax,
+		.proc_handler	= proc_douintvec_minmax,
 		.extra1		= &sysctl_nr_open_min,
 		.extra2		= &sysctl_nr_open_max,
 	},
