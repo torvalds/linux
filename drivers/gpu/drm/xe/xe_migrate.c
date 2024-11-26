1c052c66461f0bf6a6fcc6273999e9b93db54856
Matthew Auld <matthew.auld@intel.com>
2024-11-26 18:13:01 +0000

    drm/xe/migrate: use XE_BO_FLAG_PAGETABLE
    commit c78f4399188369a55eed69cbf19a8aad2a65ac75 upstream.
    
    On some HW we want to avoid the host caching PTEs, since access from GPU
    side can be incoherent. However here the special migrate object is
    mapping PTEs which are written from the host and potentially cached. Use
    XE_BO_FLAG_PAGETABLE to ensure that non-cached mapping is used, on
    platforms where this matters.
    
    Fixes: 7a060d786cc1 ("drm/xe/mtl: Map PPGTT as CPU:WC")
    Signed-off-by: Matthew Auld <matthew.auld@intel.com>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Cc: Nirmoy Das <nirmoy.das@intel.com>
    Cc: <stable@vger.kernel.org> # v6.8+
    Reviewed-by: Nirmoy Das <nirmoy.das@intel.com>
    Reviewed-by: Matthew Brost <matthew.brost@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241126181259.159713-4-matthew.auld@intel.com
    (cherry picked from commit febc689b27d28973cd02f667548a5dca383d859a)
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/xe/xe_migrate.c
+++ drivers/gpu/drm/xe/xe_migrate.c
@@ -209,7 +209,8 @@ static int xe_migrate_prepare_vm(struct xe_tile *tile, struct xe_migrate *m,
 				  num_entries * XE_PAGE_SIZE,
 				  ttm_bo_type_kernel,
 				  XE_BO_FLAG_VRAM_IF_DGFX(tile) |
-				  XE_BO_FLAG_PINNED);
+				  XE_BO_FLAG_PINNED |
+				  XE_BO_FLAG_PAGETABLE);
 	if (IS_ERR(bo))
 		return PTR_ERR(bo);
 
