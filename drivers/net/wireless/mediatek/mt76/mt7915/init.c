64d571742b0ae44eee5efd51e2d4a09d7f6782fc
xueqin Luo <luoxueqin@kylinos.cn>
2024-12-02 11:19:17 +0800

    wifi: mt76: mt7915: fix overflows seen when writing limit attributes
    DIV_ROUND_CLOSEST() after kstrtoul() results in an overflow if a large
    number such as 18446744073709551615 is provided by the user.
    Fix it by reordering clamp_val() and DIV_ROUND_CLOSEST() operations.
    This commit was inspired by commit: 57ee12b6c514.
    
    Fixes: 02ee68b95d81 ("mt76: mt7915: add control knobs for thermal throttling")
    Signed-off-by: xueqin Luo <luoxueqin@kylinos.cn>
    Link: https://patch.msgid.link/20241202031917.23741-3-luoxueqin@kylinos.cn
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7915/init.c
+++ drivers/net/wireless/mediatek/mt76/mt7915/init.c
@@ -82,7 +82,7 @@ static ssize_t mt7915_thermal_temp_store(struct device *dev,
 		return ret;
 
 	mutex_lock(&phy->dev->mt76.mutex);
-	val = clamp_val(DIV_ROUND_CLOSEST(val, 1000), 60, 130);
+	val = DIV_ROUND_CLOSEST(clamp_val(val, 60 * 1000, 130 * 1000), 1000);
 
 	if ((i - 1 == MT7915_CRIT_TEMP_IDX &&
 	     val > phy->throttle_temp[MT7915_MAX_TEMP_IDX]) ||
