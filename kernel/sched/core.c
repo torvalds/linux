0ee98301f1f026e9f0abc60bbf91aadb738456a2
Vincent Guittot <vincent.guittot@linaro.org>
2024-12-02 18:45:56 +0100

    sched/fair: Fix sched_can_stop_tick() for fair tasks
    [ Upstream commit c1f43c342e1f2e32f0620bf2e972e2a9ea0a1e60 ]
    
    We can't stop the tick of a rq if there are at least 2 tasks enqueued in
    the whole hierarchy and not only at the root cfs rq.
    
    rq->cfs.nr_running tracks the number of sched_entity at one level
    whereas rq->cfs.h_nr_running tracks all queued tasks in the
    hierarchy.
    
    Fixes: 11cc374f4643b ("sched_ext: Simplify scx_can_stop_tick() invocation in sched_can_stop_tick()")
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Link: https://lore.kernel.org/r/20241202174606.4074512-2-vincent.guittot@linaro.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1300,7 +1300,7 @@ bool sched_can_stop_tick(struct rq *rq)
 	if (scx_enabled() && !scx_can_stop_tick(rq))
 		return false;
 
-	if (rq->cfs.nr_running > 1)
+	if (rq->cfs.h_nr_running > 1)
 		return false;
 
 	/*
