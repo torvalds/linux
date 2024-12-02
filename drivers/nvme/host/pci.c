b0de5456e201c475d6a860ceeb3ed8ee2923695a
Keith Busch <kbusch@kernel.org>
2024-12-02 09:45:48 -0800

    nvme-pci: remove two deallocate zeroes quirks
    The quirk was initially used as a signal to set the discard_zeroes_data
    queue limit because there were some use cases that relied on that
    behavior. The queue limit no longer exists as every user of it has been
    converted to use the write zeroes operation instead.
    
    The quirk now means to use a discard command as an alias to a write
    zeroes request. Two of the devices previously using the quirk support
    the write zeroes command directly, so these don't need or want to use
    discard when the desired operation is to write zeroes.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Keith Busch <kbusch@kernel.org>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -3588,12 +3588,10 @@ static const struct pci_device_id nvme_id_table[] = {
 				NVME_QUIRK_DEALLOCATE_ZEROES, },
 	{ PCI_VDEVICE(INTEL, 0x0a54),	/* Intel P4500/P4600 */
 		.driver_data = NVME_QUIRK_STRIPE_SIZE |
-				NVME_QUIRK_DEALLOCATE_ZEROES |
 				NVME_QUIRK_IGNORE_DEV_SUBNQN |
 				NVME_QUIRK_BOGUS_NID, },
 	{ PCI_VDEVICE(INTEL, 0x0a55),	/* Dell Express Flash P4600 */
-		.driver_data = NVME_QUIRK_STRIPE_SIZE |
-				NVME_QUIRK_DEALLOCATE_ZEROES, },
+		.driver_data = NVME_QUIRK_STRIPE_SIZE, },
 	{ PCI_VDEVICE(INTEL, 0xf1a5),	/* Intel 600P/P3100 */
 		.driver_data = NVME_QUIRK_NO_DEEPEST_PS |
 				NVME_QUIRK_MEDIUM_PRIO_SQ |
