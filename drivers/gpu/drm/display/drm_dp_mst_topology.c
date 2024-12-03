59ca0e1c97c5d752f85ca2922ff258ff5e62bc73
Imre Deak <imre.deak@intel.com>
2024-12-03 18:02:23 +0200

    drm/dp_mst: Use reset_msg_rx_state() instead of open coding it
    Use reset_msg_rx_state() in drm_dp_mst_handle_up_req() instead of
    open-coding it.
    
    Cc: Lyude Paul <lyude@redhat.com>
    Reviewed-by: Lyude Paul <lyude@redhat.com>
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241203160223.2926014-8-imre.deak@intel.com

--- drivers/gpu/drm/display/drm_dp_mst_topology.c
+++ drivers/gpu/drm/display/drm_dp_mst_topology.c
@@ -4188,7 +4188,7 @@ static int drm_dp_mst_handle_up_req(struct drm_dp_mst_topology_mgr *mgr)
 out_put_primary:
 	drm_dp_mst_topology_put_mstb(mst_primary);
 out_clear_reply:
-	memset(&mgr->up_req_recv, 0, sizeof(struct drm_dp_sideband_msg_rx));
+	reset_msg_rx_state(&mgr->up_req_recv);
 	return ret;
 }
 
