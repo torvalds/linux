d5ea5e5e50dffd13fccedb690a0a1f27be56191a
Jan Kara <jack@suse.cz>
2024-12-04 19:10:16 +0100

    readahead: properly shorten readahead when falling back to do_page_cache_ra()
    When we succeed in creating some folios in page_cache_ra_order() but then
    need to fallback to single page folios, we don't shorten the amount to
    read passed to do_page_cache_ra() by the amount we've already read.  This
    then results in reading more and also in placing another readahead mark in
    the middle of the readahead window which confuses readahead code.  Fix the
    problem by properly reducing number of pages to read.  Unlike previous
    attempt at this fix (commit 7c877586da31) which had to be reverted, we are
    now careful to check there is indeed something to read so that we don't
    submit negative-sized readahead.
    
    Link: https://lkml.kernel.org/r/20241204181016.15273-3-jack@suse.cz
    Signed-off-by: Jan Kara <jack@suse.cz>
    Cc: Matthew Wilcox <willy@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/readahead.c
+++ mm/readahead.c
@@ -448,7 +448,8 @@ void page_cache_ra_order(struct readahead_control *ractl,
 		struct file_ra_state *ra, unsigned int new_order)
 {
 	struct address_space *mapping = ractl->mapping;
-	pgoff_t index = readahead_index(ractl);
+	pgoff_t start = readahead_index(ractl);
+	pgoff_t index = start;
 	unsigned int min_order = mapping_min_folio_order(mapping);
 	pgoff_t limit = (i_size_read(mapping->host) - 1) >> PAGE_SHIFT;
 	pgoff_t mark = index + ra->size - ra->async_size;
@@ -506,12 +507,18 @@ void page_cache_ra_order(struct readahead_control *ractl,
 	/*
 	 * If there were already pages in the page cache, then we may have
 	 * left some gaps.  Let the regular readahead code take care of this
-	 * situation.
+	 * situation below.
 	 */
 	if (!err)
 		return;
 fallback:
-	do_page_cache_ra(ractl, ra->size, ra->async_size);
+	/*
+	 * ->readahead() may have updated readahead window size so we have to
+	 * check there's still something to read.
+	 */
+	if (ra->size > index - start)
+		do_page_cache_ra(ractl, ra->size - (index - start),
+				 ra->async_size);
 }
 
 static unsigned long ractl_max_pages(struct readahead_control *ractl,
