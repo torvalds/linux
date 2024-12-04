2d06f1929f5b96eedd3bc4175093b89ba58e8832
Noface <166717111+noface1200@users.noreply.github.com>
2024-12-04 20:18:30 +0000

    Update cmpdi2.c

--- lib/cmpdi2.c
+++ lib/cmpdi2.c
@@ -1,30 +1,37 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
+ * Function for comparing two 64-bit signed integers.
  */
 
 #include <linux/export.h>
-
 #include <linux/libgcc.h>
 
+// Compare two 64-bit signed integers
 word_type notrace __cmpdi2(long long a, long long b)
 {
-	const DWunion au = {
-		.ll = a
-	};
-	const DWunion bu = {
-		.ll = b
-	};
+    // Union to split 64-bit values into high and low 32-bit parts
+    const DWunion au = {
+        .ll = a
+    };
+    const DWunion bu = {
+        .ll = b
+    };
 
-	if (au.s.high < bu.s.high)
-		return 0;
-	else if (au.s.high > bu.s.high)
-		return 2;
+    // Compare the high 32 bits
+    if (au.s.high < bu.s.high)
+        return 0;  // a < b
+    else if (au.s.high > bu.s.high)
+        return 2;  // a > b
 
-	if ((unsigned int) au.s.low < (unsigned int) bu.s.low)
-		return 0;
-	else if ((unsigned int) au.s.low > (unsigned int) bu.s.low)
-		return 2;
+    // If high parts are equal, compare the low 32 bits
+    if ((unsigned int) au.s.low < (unsigned int) bu.s.low)
+        return 0;  // a < b
+    else if ((unsigned int) au.s.low > (unsigned int) bu.s.low)
+        return 2;  // a > b
 
-	return 1;
+    // Return 1 if both parts are equal (a == b)
+    return 1;  // a == b
 }
+
+// Make the function available for use in other modules
 EXPORT_SYMBOL(__cmpdi2);
