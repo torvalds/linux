09471d8f5b390883eaf21b917c4bf3ced1b8a1df
Ming Wang <wangming01@loongson.cn>
2024-12-05 19:43:07 +0800

    rtc: loongson: clear TOY_MATCH0_REG in loongson_rtc_isr()
    The TOY_MATCH0_REG should be cleared to 0 in the RTC interrupt handler,
    otherwise the interrupt cannot be cleared, which will cause the
    loongson_rtc_isr() to be triggered multiple times.
    
    The previous code cleared TOY_MATCH0_REG in the loongson_rtc_handler(),
    which is an ACPI interrupt. This did not prevent loongson_rtc_isr()
    from being triggered multiple times.
    
    This commit moves the clearing of TOY_MATCH0_REG to the
    loongson_rtc_isr() to ensure that the interrupt is properly cleared.
    
    Fixes: 1b733a9ebc3d ("rtc: Add rtc driver for the Loongson family chips")
    Signed-off-by: Ming Wang <wangming01@loongson.cn>
    Reviewed-by: Huacai Chen <chenhuacai@loongson.cn>
    Reviewed-by: Keguang Zhang <keguang.zhang@gmail.com> # on LS1B
    Tested-by: Keguang Zhang <keguang.zhang@gmail.com>
    Link: https://lore.kernel.org/r/20241205114307.1891418-1-wangming01@loongson.cn
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/rtc/rtc-loongson.c
+++ drivers/rtc/rtc-loongson.c
@@ -114,6 +114,13 @@ static irqreturn_t loongson_rtc_isr(int irq, void *id)
 	struct loongson_rtc_priv *priv = (struct loongson_rtc_priv *)id;
 
 	rtc_update_irq(priv->rtcdev, 1, RTC_AF | RTC_IRQF);
+
+	/*
+	 * The TOY_MATCH0_REG should be cleared 0 here,
+	 * otherwise the interrupt cannot be cleared.
+	 */
+	regmap_write(priv->regmap, TOY_MATCH0_REG, 0);
+
 	return IRQ_HANDLED;
 }
 
@@ -131,11 +138,7 @@ static u32 loongson_rtc_handler(void *id)
 	writel(RTC_STS, priv->pm_base + PM1_STS_REG);
 	spin_unlock(&priv->lock);
 
-	/*
-	 * The TOY_MATCH0_REG should be cleared 0 here,
-	 * otherwise the interrupt cannot be cleared.
-	 */
-	return regmap_write(priv->regmap, TOY_MATCH0_REG, 0);
+	return ACPI_INTERRUPT_HANDLED;
 }
 
 static int loongson_rtc_set_enabled(struct device *dev)
