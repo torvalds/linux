c1043cdb019ed4d053d673e62b553a5cea1a287d
Kurt Borja <kuurtb@gmail.com>
2024-12-07 21:26:55 -0300

    alienware-wmi: Fix X Series and G Series quirks
    Devices that are known to support the WMI thermal interface do not
    support the legacy LED control interface. Make `.num_zones = 0` and
    avoid calling alienware_zone_init() if that's the case.
    
    Fixes: 9f6c43041552 ("alienware-wmi: added platform profile support")
    Fixes: 1c1eb70e7d23 ("alienware-wmi: extends the list of supported models")
    Suggested-by: Armin Wolf <W_Armin@gmx.de>
    Reviewed-by: Armin Wolf <W_Armin@gmx.de>
    Signed-off-by: Kurt Borja <kuurtb@gmail.com>
    Link: https://lore.kernel.org/r/20241208002652.5885-4-kuurtb@gmail.com
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>

--- drivers/platform/x86/dell/alienware-wmi.c
+++ drivers/platform/x86/dell/alienware-wmi.c
@@ -190,7 +190,7 @@ static struct quirk_entry quirk_asm201 = {
 };
 
 static struct quirk_entry quirk_g_series = {
-	.num_zones = 2,
+	.num_zones = 0,
 	.hdmi_mux = 0,
 	.amplifier = 0,
 	.deepslp = 0,
@@ -199,7 +199,7 @@ static struct quirk_entry quirk_g_series = {
 };
 
 static struct quirk_entry quirk_x_series = {
-	.num_zones = 2,
+	.num_zones = 0,
 	.hdmi_mux = 0,
 	.amplifier = 0,
 	.deepslp = 0,
@@ -686,6 +686,9 @@ static int alienware_zone_init(struct platform_device *dev)
 static void alienware_zone_exit(struct platform_device *dev)
 {
 	u8 zone;
+
+	if (!quirks->num_zones)
+		return;
 
 	sysfs_remove_group(&dev->dev.kobj, &zone_attribute_group);
 	led_classdev_unregister(&global_led);
@@ -1229,9 +1232,11 @@ static int __init alienware_wmi_init(void)
 			goto fail_prep_thermal_profile;
 	}
 
-	ret = alienware_zone_init(platform_device);
-	if (ret)
-		goto fail_prep_zones;
+	if (quirks->num_zones > 0) {
+		ret = alienware_zone_init(platform_device);
+		if (ret)
+			goto fail_prep_zones;
+	}
 
 	return 0;
 
