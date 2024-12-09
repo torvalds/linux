25a3123a67d98d4560ec9ee2286f4f7cffe4c22c
Kent Overstreet <kent.overstreet@linux.dev>
2024-12-08 21:10:27 -0500

    bcachefs: check_indirect_extents can run online
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/recovery_passes_types.h
+++ fs/bcachefs/recovery_passes_types.h
@@ -43,7 +43,7 @@
 	x(fs_upgrade_for_subvolumes,		22, 0)				\
 	x(check_inodes,				24, PASS_FSCK)			\
 	x(check_extents,			25, PASS_FSCK)			\
-	x(check_indirect_extents,		26, PASS_FSCK)			\
+	x(check_indirect_extents,		26, PASS_ONLINE|PASS_FSCK)	\
 	x(check_dirents,			27, PASS_FSCK)			\
 	x(check_xattrs,				28, PASS_FSCK)			\
 	x(check_root,				29, PASS_ONLINE|PASS_FSCK)	\
