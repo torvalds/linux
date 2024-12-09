9f468bfed1fba8da7db13f2c55f42b7be23a9b56
Maxim Levitsky <mlevitsk@redhat.com>
2024-12-09 12:57:51 -0500

    net: mana: Fix irq_contexts memory leak in mana_gd_setup_irqs
    [ Upstream commit 9a5beb6ca6305de5c5210efab0702ea79b62eb39 ]
    
    gc->irq_contexts is not freeded if one of the later operations
    fail.
    
    Suggested-by: Michael Kelley <mhklinux@outlook.com>
    Fixes: 8afefc361209 ("net: mana: Assigning IRQ affinity on HT cores")
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>
    Reviewed-by: Michal Swiatkowski <michal.swiatkowski@linux.intel.com>
    Reviewed-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
    Reviewed-by: Saurabh Sengar <ssengar@linux.microsoft.com>
    Reviewed-by: Yury Norov <yury.norov@gmail.com>
    Link: https://patch.msgid.link/20241209175751.287738-3-mlevitsk@redhat.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/microsoft/mana/gdma_main.c
+++ drivers/net/ethernet/microsoft/mana/gdma_main.c
@@ -1315,7 +1315,7 @@ static int mana_gd_setup_irqs(struct pci_dev *pdev)
 				   GFP_KERNEL);
 	if (!gc->irq_contexts) {
 		err = -ENOMEM;
-		goto free_irq_vector;
+		goto free_irq_array;
 	}
 
 	for (i = 0; i < nvec; i++) {
@@ -1385,8 +1385,9 @@ free_irq:
 	}
 
 	kfree(gc->irq_contexts);
-	kfree(irqs);
 	gc->irq_contexts = NULL;
+free_irq_array:
+	kfree(irqs);
 free_irq_vector:
 	cpus_read_unlock();
 	pci_free_irq_vectors(pdev);
