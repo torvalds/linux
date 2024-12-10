2218704997979fbf11765281ef752f07c5cf25bb
Maarten Lankhorst <dev@lankhorst.se>
2024-12-10 09:31:02 +0100

    drm/xe: Remove double pageflip
    This is already handled below in the code by fixup_initial_plane_config.
    
    Fixes: a8153627520a ("drm/i915: Try to relocate the BIOS fb to the start of ggtt")
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Vinod Govindapillai <vinod.govindapillai@intel.com>
    Reviewed-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241210083111.230484-3-dev@lankhorst.se
    Signed-off-by: Maarten Lankhorst <dev@lankhorst.se>

--- drivers/gpu/drm/xe/display/xe_plane_initial.c
+++ drivers/gpu/drm/xe/display/xe_plane_initial.c
@@ -194,8 +194,6 @@ intel_find_initial_plane_obj(struct intel_crtc *crtc,
 		to_intel_plane(crtc->base.primary);
 	struct intel_plane_state *plane_state =
 		to_intel_plane_state(plane->base.state);
-	struct intel_crtc_state *crtc_state =
-		to_intel_crtc_state(crtc->base.state);
 	struct drm_framebuffer *fb;
 	struct i915_vma *vma;
 
@@ -241,14 +239,6 @@ intel_find_initial_plane_obj(struct intel_crtc *crtc,
 	atomic_or(plane->frontbuffer_bit, &to_intel_frontbuffer(fb)->bits);
 
 	plane_config->vma = vma;
-
-	/*
-	 * Flip to the newly created mapping ASAP, so we can re-use the
-	 * first part of GGTT for WOPCM, prevent flickering, and prevent
-	 * the lookup of sysmem scratch pages.
-	 */
-	plane->check_plane(crtc_state, plane_state);
-	plane->async_flip(NULL, plane, crtc_state, plane_state, true);
 	return;
 
 nofb:
