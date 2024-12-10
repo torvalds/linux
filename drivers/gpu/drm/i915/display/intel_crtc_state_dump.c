ab684293168c7c817fc6089f290496cd6bfeec0b
Ville Syrjälä <ville.syrjala@linux.intel.com>
2024-12-10 23:09:57 +0200

    drm/i915: Include the scanline offset in the state dump
    When looking at raw hardware scanline numbers it's helpful to
    remember what the offset between the hardware values and our
    more human readable numbers should be. Include that in the state dump.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241210211007.5976-9-ville.syrjala@linux.intel.com
    Reviewed-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>

--- drivers/gpu/drm/i915/display/intel_crtc_state_dump.c
+++ drivers/gpu/drm/i915/display/intel_crtc_state_dump.c
@@ -10,6 +10,7 @@
 #include "intel_crtc_state_dump.h"
 #include "intel_display_types.h"
 #include "intel_hdmi.h"
+#include "intel_vblank.h"
 #include "intel_vdsc.h"
 #include "intel_vrr.h"
 
@@ -282,6 +283,9 @@ void intel_crtc_state_dump(const struct intel_crtc_state *pipe_config,
 	if (pipe_config->has_audio)
 		drm_print_hex_dump(&p, "ELD: ", pipe_config->eld,
 				   drm_eld_size(pipe_config->eld));
+
+	drm_printf(&p, "scanline offset: %d\n",
+		   intel_crtc_scanline_offset(pipe_config));
 
 	drm_printf(&p, "vblank delay: %d, framestart delay: %d, MSA timing delay: %d\n",
 		   pipe_config->hw.adjusted_mode.crtc_vblank_start -
