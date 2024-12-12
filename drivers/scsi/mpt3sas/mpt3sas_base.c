09ecc187ebde77ebbfc444b2c08192024e2dc6a4
Paul Menzel <pmenzel@molgen.mpg.de>
2024-12-12 23:18:11 +0100

    scsi: mpt3sas: Add details to EEDPTagMode error message
    Linux 5.15 logs the error below
    
        mpt3sas_cm0: overriding NVDATA EEDPTagMode setting
    
    on a Dell PowerEdge T440 with the card below.
    
        5e:00.0 Serial Attached SCSI controller [0107]: Broadcom / LSI SAS3008 PCI-Express Fusion-MPT SAS-3 [1000:0097] (rev 02)
    
    It’s not clear to a user what this error is about. As a first step to
    improve this, add the values to the error message.
    
    Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
    Link: https://lore.kernel.org/r/20241212221817.78940-1-pmenzel@molgen.mpg.de
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/mpt3sas/mpt3sas_base.c
+++ drivers/scsi/mpt3sas/mpt3sas_base.c
@@ -5627,7 +5627,7 @@ _base_static_config_pages(struct MPT3SAS_ADAPTER *ioc)
 	if (rc)
 		return rc;
 	if (!ioc->is_gen35_ioc && ioc->manu_pg11.EEDPTagMode == 0) {
-		pr_err("%s: overriding NVDATA EEDPTagMode setting\n",
+		pr_err("%s: overriding NVDATA EEDPTagMode setting from 0 to 1\n",
 		    ioc->name);
 		ioc->manu_pg11.EEDPTagMode &= ~0x3;
 		ioc->manu_pg11.EEDPTagMode |= 0x1;
