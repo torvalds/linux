7e320a4063a81508e171012e0f75ec4a111850d4
Kent Overstreet <kent.overstreet@linux.dev>
2024-12-12 02:26:15 -0500

    bcachefs: Fix btree_trans_peek_key_cache() BTREE_ITER_all_snapshots
    In BTREE_ITER_all_snapshots mode, we're required to only return keys
    where the snapshot field matches the iterator position -
    BTREE_ITER_filter_snapshots requires pulling keys into the key cache
    from ancestor snapshots, so we have to check for that.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/btree_iter.c
+++ fs/bcachefs/btree_iter.c
@@ -2230,6 +2230,10 @@ struct bkey_s_c btree_trans_peek_key_cache(struct btree_iter *iter, struct bpos 
 
 	k = bch2_btree_path_peek_slot(trans->paths + iter->key_cache_path, &u);
 	if (k.k && !bkey_err(k)) {
+		if ((iter->flags & BTREE_ITER_all_snapshots) &&
+		    !bpos_eq(pos, k.k->p))
+			return bkey_s_c_null;
+
 		iter->k = u;
 		k.k = &iter->k;
 	}
