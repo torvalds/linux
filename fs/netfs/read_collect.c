43b8d3249b0b71bad239d42dbe08ce6c938ba000
David Howells <dhowells@redhat.com>
2024-12-13 13:50:09 +0000

    netfs: Fix ceph copy to cache on write-begin
    [ Upstream commit 38cf8e945721ffe708fa675507465da7f4f2a9f7 ]
    
    At the end of netfs_unlock_read_folio() in which folios are marked
    appropriately for copying to the cache (either with by being marked dirty
    and having their private data set or by having PG_private_2 set) and then
    unlocked, the folio_queue struct has the entry pointing to the folio
    cleared.  This presents a problem for netfs_pgpriv2_write_to_the_cache(),
    which is used to write folios marked with PG_private_2 to the cache as it
    expects to be able to trawl the folio_queue list thereafter to find the
    relevant folios, leading to a hang.
    
    Fix this by not clearing the folio_queue entry if we're going to do the
    deprecated copy-to-cache.  The clearance will be done instead as the folios
    are written to the cache.
    
    This can be reproduced by starting cachefiles, mounting a ceph filesystem
    with "-o fsc" and writing to it.
    
    Fixes: 796a4049640b ("netfs: In readahead, put the folio refs as soon extracted")
    Reported-by: Max Kellermann <max.kellermann@ionos.com>
    Closes: https://lore.kernel.org/r/CAKPOu+_4m80thNy5_fvROoxBm689YtA0dZ-=gcmkzwYSY4syqw@mail.gmail.com/
    Signed-off-by: David Howells <dhowells@redhat.com>
    Link: https://lore.kernel.org/r/20241213135013.2964079-10-dhowells@redhat.com
    Fixes: ee4cdf7ba857 ("netfs: Speed up buffered reading")
    cc: Jeff Layton <jlayton@kernel.org>
    cc: Ilya Dryomov <idryomov@gmail.com>
    cc: Xiubo Li <xiubli@redhat.com>
    cc: netfs@lists.linux.dev
    cc: ceph-devel@vger.kernel.org
    cc: linux-fsdevel@vger.kernel.org
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/netfs/read_collect.c
+++ fs/netfs/read_collect.c
@@ -62,10 +62,14 @@ static void netfs_unlock_read_folio(struct netfs_io_subrequest *subreq,
 		} else {
 			trace_netfs_folio(folio, netfs_folio_trace_read_done);
 		}
+
+		folioq_clear(folioq, slot);
 	} else {
 		// TODO: Use of PG_private_2 is deprecated.
 		if (test_bit(NETFS_SREQ_COPY_TO_CACHE, &subreq->flags))
 			netfs_pgpriv2_mark_copy_to_cache(subreq, rreq, folioq, slot);
+		else
+			folioq_clear(folioq, slot);
 	}
 
 	if (!test_bit(NETFS_RREQ_DONT_UNLOCK_FOLIOS, &rreq->flags)) {
@@ -77,8 +81,6 @@ static void netfs_unlock_read_folio(struct netfs_io_subrequest *subreq,
 			folio_unlock(folio);
 		}
 	}
-
-	folioq_clear(folioq, slot);
 }
 
 /*
