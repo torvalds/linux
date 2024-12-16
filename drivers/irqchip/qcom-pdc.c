9e301b325d9d17a53a97b7283b6fe16688a44a48
Neil Armstrong <neil.armstrong@linaro.org>
2024-12-16 12:20:25 +0530

    BACKPORT: UPSTREAM: irqchip/qcom-pdc: Add support for v3.2 HW
    Starting from HW version 3.2 the IRQ_ENABLE bit has moved to the
    IRQ_i_CFG register and requires a change of the driver to avoid
    writing into an undefined register address.
    
    Get the HW version from registers and set the IRQ_ENABLE bit to the
    correct register depending on the HW version.
    
    Link: https://lore.kernel.org/r/20230905-topic-sm8x50-upstream-pdc-ver-v4-1-fc633c7df84b@linaro.org
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>
    Git-commit: 5873d380f4c0ff23ec7d0d1780107e46a4637c0e
    Git-repo: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Change-Id: I245cd96a5e2ccdd0be6784a44ad07de493a8c3f2
    Signed-off-by: Chintan Kothari <quic_ckothari@quicinc.com>

--- drivers/irqchip/qcom-pdc.c
+++ drivers/irqchip/qcom-pdc.c
@@ -128,26 +128,30 @@ static u32 pdc_reg_read(int reg, u32 i)
 	return readl_relaxed(pdc_base + reg + i * sizeof(u32));
 }
 
-static void pdc_enable_intr(struct irq_data *d, bool on)
+static void __pdc_enable_intr(int pin_out, bool on)
 {
-	int pin_out = d->hwirq;
 	unsigned long enable;
-	unsigned long flags;
-	u32 index, mask;
 
-	raw_spin_lock_irqsave(&pdc_lock, flags);
 	if (!enable_in_cfg) {
+		u32 index, mask;
 		index = pin_out / 32;
 		mask = pin_out % 32;
 		enable = pdc_reg_read(IRQ_ENABLE_BANK, index);
 		__assign_bit(mask, &enable, on);
 		pdc_reg_write(IRQ_ENABLE_BANK, index, enable);
 	} else {
-		index = d->hwirq;
-		enable = pdc_reg_read(IRQ_i_CFG, index);
+		enable = pdc_reg_read(IRQ_i_CFG, pin_out);
 		__assign_bit(IRQ_i_CFG_IRQ_ENABLE, &enable, on);
-		pdc_reg_write(IRQ_i_CFG, index, enable);
+		pdc_reg_write(IRQ_i_CFG, pin_out, enable);
 	}
+}
+
+static void pdc_enable_intr(struct irq_data *d, bool on)
+{
+	unsigned long flags;
+
+	raw_spin_lock_irqsave(&pdc_lock, flags);
+	__pdc_enable_intr(d->hwirq, on);
 	raw_spin_unlock_irqrestore(&pdc_lock, flags);
 	ipc_log_string(pdc_ipc_log, "PIN=%lu enable=%d", d->hwirq, on);
 }
@@ -342,8 +346,7 @@ static const struct irq_domain_ops qcom_pdc_ops = {
 
 static int pdc_setup_pin_mapping(struct device_node *np)
 {
-	int ret, n, i, last_region;
-	u32 irq_index, reg_index, val, max_irq;
+	int ret, n, i;
 
 	n = of_property_count_elems_of_size(np, "qcom,pdc-ranges", sizeof(u32));
 	if (n <= 0 || n % 3)
@@ -372,25 +375,9 @@ static int pdc_setup_pin_mapping(struct device_node *np)
 						 &pdc_region[n].cnt);
 		if (ret)
 			return ret;
-	}
 
-	last_region = pdc_region_cnt - 1;
-	max_irq = pdc_region[last_region].pin_base + pdc_region[last_region].cnt;
-
-	if (!enable_in_cfg) {
-		for (i = 0; i < max_irq; i++) {
-			reg_index = (i + pdc_region[n].pin_base) >> 5;
-			irq_index = (i + pdc_region[n].pin_base) & 0x1f;
-			val = pdc_reg_read(IRQ_ENABLE_BANK, reg_index);
-			val &= ~BIT(irq_index);
-			pdc_reg_write(IRQ_ENABLE_BANK, reg_index, val);
-		}
-	} else {
-		for (i = 0; i < max_irq; i++) {
-			val = pdc_reg_read(IRQ_i_CFG, i);
-			val &= ~BIT(IRQ_i_CFG_IRQ_ENABLE);
-			pdc_reg_write(IRQ_i_CFG, i, val);
-		}
+		for (i = 0; i < pdc_region[n].cnt; i++)
+			__pdc_enable_intr(i + pdc_region[n].pin_base, 0);
 	}
 
 	return 0;
