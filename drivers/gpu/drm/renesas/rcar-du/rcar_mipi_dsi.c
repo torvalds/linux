6389e616fae8a101ce00068f7690461ab57b29d8
Tomi Valkeinen <tomi.valkeinen+renesas@ideasonboard.com>
2024-12-17 07:31:35 +0200

    drm/rcar-du: dsi: Fix PHY lock bit check
    The driver checks for bit 16 (using CLOCKSET1_LOCK define) in CLOCKSET1
    register when waiting for the PPI clock. However, the right bit to check
    is bit 17 (CLOCKSET1_LOCK_PHY define). Not only that, but there's
    nothing in the documents for bit 16 for V3U nor V4H.
    
    So, fix the check to use bit 17, and drop the define for bit 16.
    
    Fixes: 155358310f01 ("drm: rcar-du: Add R-Car DSI driver")
    Fixes: 11696c5e8924 ("drm: Place Renesas drivers in a separate dir")
    Cc: stable@vger.kernel.org
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen+renesas@ideasonboard.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Tested-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241217-rcar-gh-dsi-v5-1-e77421093c05@ideasonboard.com

--- drivers/gpu/drm/renesas/rcar-du/rcar_mipi_dsi.c
+++ drivers/gpu/drm/renesas/rcar-du/rcar_mipi_dsi.c
@@ -587,7 +587,7 @@ static int rcar_mipi_dsi_startup(struct rcar_mipi_dsi *dsi,
 	for (timeout = 10; timeout > 0; --timeout) {
 		if ((rcar_mipi_dsi_read(dsi, PPICLSR) & PPICLSR_STPST) &&
 		    (rcar_mipi_dsi_read(dsi, PPIDLSR) & PPIDLSR_STPST) &&
-		    (rcar_mipi_dsi_read(dsi, CLOCKSET1) & CLOCKSET1_LOCK))
+		    (rcar_mipi_dsi_read(dsi, CLOCKSET1) & CLOCKSET1_LOCK_PHY))
 			break;
 
 		usleep_range(1000, 2000);
