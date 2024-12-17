46e6075287e68e1d3d0ea8ecda610064636e0854
Manikanta Mylavarapu <quic_mmanikan@quicinc.com>
2024-12-17 17:09:09 +0530

    dt-bindings: clock: qcom: gcc-ipq5424: remove apss_dbg clock macro
    The gcc_apss_dbg clk is access protected by trust zone, and accessing
    it results in a kernel crash. Therefore remove the gcc_apss_dbg_clk macro.
    
    Acked-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Signed-off-by: Manikanta Mylavarapu <quic_mmanikan@quicinc.com>
    Link: https://lore.kernel.org/r/20241217113909.3522305-3-quic_mmanikan@quicinc.com
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>

--- include/dt-bindings/clock/qcom,ipq5424-gcc.h
+++ include/dt-bindings/clock/qcom,ipq5424-gcc.h
@@ -12,7 +12,6 @@
 #define GPLL2					2
 #define GPLL2_OUT_MAIN                          3
 #define GCC_SLEEP_CLK_SRC			4
-#define GCC_APSS_DBG_CLK                        5
 #define GCC_USB0_EUD_AT_CLK			6
 #define GCC_PCIE0_AXI_M_CLK_SRC			7
 #define GCC_PCIE0_AXI_M_CLK			8
