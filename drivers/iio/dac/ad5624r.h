25d4851dda719ceb26514b73b48fc6319c0432b7
Matti Vaittinen <mazziesaccount@gmail.com>
2024-12-18 10:17:15 +0200

    iio: dac: ad5624r: Drop docs of missing members
    The documentation for struct ad5624r_state contains members that are (no
    longer?) part of the structure.
    
    Remove unnecessary docs.
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>
    Link: https://patch.msgid.link/Z2KFC7ZBwmM69Qb4@mva-rohm
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/dac/ad5624r.h
+++ drivers/iio/dac/ad5624r.h
@@ -42,10 +42,8 @@ struct ad5624r_chip_info {
 
 /**
  * struct ad5624r_state - driver instance specific data
- * @indio_dev:		the industrial I/O device
  * @us:			spi_device
  * @chip_info:		chip model specific constants, available modes etc
- * @reg:		supply regulator
  * @vref_mv:		actual reference voltage used
  * @pwr_down_mask	power down mask
  * @pwr_down_mode	current power down mode
