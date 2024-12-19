adba0929736a6a2d2780e8e6e4082e42e5ba025c
Arvind Yadav <Arvind.Yadav@amd.com>
2024-12-19 19:43:54 +0530

    drm/amdgpu: Fix Illegal opcode in command stream Error
    When applications closes, it triggers the drm_file_free
    function which subsequently releases all allocated buffer
    objects. Concurrently, the resume_worker thread will attempt
    to map the usermode queue. However, since the wptr buffer
    object has already been deallocated, this will result in
    an Illegal opcode error being raised in the command stream.
    
    Now replacing drm_release() with a new function
    amdgpu_drm_release(). This function will set the flag to
    prevent the scheduling of any new queue resume/map, stop
    all queues and then call drm_release().
    
    V2:
      - Replace drm_release with amdgpu_drm_release(Christian).
    
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: Christian Koenig <christian.koenig@amd.com>
    Reviewed-by: Shashank Sharma <shashank.sharma@amd.com>
    Signed-off-by: Arvind Yadav <arvind.yadav@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -2851,6 +2851,20 @@ static int amdgpu_pmops_runtime_idle(struct device *dev)
 	return ret;
 }
 
+static int amdgpu_drm_release(struct inode *inode, struct file *filp)
+{
+	struct drm_file *file_priv = filp->private_data;
+	struct amdgpu_fpriv *fpriv = file_priv->driver_priv;
+
+	if (fpriv) {
+		fpriv->evf_mgr.fd_closing = true;
+		amdgpu_userq_mgr_fini(&fpriv->userq_mgr);
+		amdgpu_eviction_fence_destroy(&fpriv->evf_mgr);
+	}
+
+	return drm_release(inode, filp);
+}
+
 long amdgpu_drm_ioctl(struct file *filp,
 		      unsigned int cmd, unsigned long arg)
 {
@@ -2902,7 +2916,7 @@ static const struct file_operations amdgpu_driver_kms_fops = {
 	.owner = THIS_MODULE,
 	.open = drm_open,
 	.flush = amdgpu_flush,
-	.release = drm_release,
+	.release = amdgpu_drm_release,
 	.unlocked_ioctl = amdgpu_drm_ioctl,
 	.mmap = drm_gem_mmap,
 	.poll = drm_poll,
