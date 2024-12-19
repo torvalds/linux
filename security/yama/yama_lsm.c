7c58ed44bd399277c4a14fcb4e447e1da39870d3
Yafang Shao <laoar.shao@gmail.com>
2024-12-19 10:34:51 +0800

    security: remove get_task_comm() and print task comm directly
    Since task->comm is guaranteed to be NUL-terminated, we can print it
    directly without the need to copy it into a separate buffer.  This
    simplifies the code and avoids unnecessary operations.
    
    Link: https://lkml.kernel.org/r/20241219023452.69907-5-laoar.shao@gmail.com
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
    Reviewed-by: Paul Moore <paul@paul-moore.com>
    Acked-by: Kees Cook <kees@kernel.org>
    Cc: James Morris <jmorris@namei.org>
    Cc: "Serge E. Hallyn" <serge@hallyn.com>
    Cc: "André Almeida" <andrealmeid@igalia.com>
    Cc: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Cc: Borislav Petkov (AMD) <bp@alien8.de>
    Cc: Danilo Krummrich <dakr@redhat.com>
    Cc: Darren Hart <dvhart@infradead.org>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: David Airlie <airlied@gmail.com>
    Cc: Davidlohr Bueso <dave@stgolabs.net>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jani Nikula <jani.nikula@linux.intel.com>
    Cc: Jiri Slaby <jirislaby@kernel.org>
    Cc: Johannes Berg <johannes@sipsolutions.net>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Cc: Kalle Valo <kvalo@kernel.org>
    Cc: Karol Herbst <kherbst@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Lyude Paul <lyude@redhat.com>
    Cc: Oded Gabbay <ogabbay@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Petr Mladek <pmladek@suse.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Cc: Simona Vetter <simona@ffwll.ch>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tvrtko Ursulin <tursulin@ursulin.net>
    Cc: Vineet Gupta <vgupta@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- security/yama/yama_lsm.c
+++ security/yama/yama_lsm.c
@@ -76,7 +76,6 @@ static void report_access(const char *access, struct task_struct *target,
 				struct task_struct *agent)
 {
 	struct access_report_info *info;
-	char agent_comm[sizeof(agent->comm)];
 
 	assert_spin_locked(&target->alloc_lock); /* for target->comm */
 
@@ -86,8 +85,7 @@ static void report_access(const char *access, struct task_struct *target,
 		 */
 		pr_notice_ratelimited(
 		    "ptrace %s of \"%s\"[%d] was attempted by \"%s\"[%d]\n",
-		    access, target->comm, target->pid,
-		    get_task_comm(agent_comm, agent), agent->pid);
+		    access, target->comm, target->pid, agent->comm, agent->pid);
 		return;
 	}
 
