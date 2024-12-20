05bda8a1bdedd5dfc21522d732c3bf9413d70eb3
Daniel Machon <daniel.machon@microchip.com>
2024-12-20 14:48:42 +0100

    net: sparx5: use is_port_rgmii() throughout
    Now that we can check if a given port is an RGMII port, use it in the
    following cases:
    
     - To set RGMII PHY modes for RGMII port devices.
    
     - To avoid checking for a SerDes node in the devicetree, when the port
       is an RGMII port.
    
     - To bail out of sparx5_port_init() when the common configuration is
       done.
    
    Reviewed-by: Steen Hegelund <Steen.Hegelund@microchip.com>
    Reviewed-by: Horatiu Vultur <horatiu.vultur@microchip.com>
    Tested-by: Robert Marko <robert.marko@sartura.hr>
    Signed-off-by: Daniel Machon <daniel.machon@microchip.com>
    Link: https://patch.msgid.link/20241220-sparx5-lan969x-switch-driver-4-v5-3-fa8ba5dff732@microchip.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/microchip/sparx5/sparx5_port.c
+++ drivers/net/ethernet/microchip/sparx5/sparx5_port.c
@@ -1090,6 +1090,9 @@ int sparx5_port_init(struct sparx5 *sparx5,
 		 ANA_CL_FILTER_CTRL_FILTER_SMAC_MC_DIS,
 		 sparx5, ANA_CL_FILTER_CTRL(port->portno));
 
+	if (ops->is_port_rgmii(port->portno))
+		return 0; /* RGMII device - nothing more to configure */
+
 	/* Configure MAC vlan awareness */
 	err = sparx5_port_max_tags_set(sparx5, port);
 	if (err)
