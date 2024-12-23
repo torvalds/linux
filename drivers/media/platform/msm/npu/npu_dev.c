eb0390d13671db90ae8796166bff7b979ee19581
Gao Wang <quic_gaowang@quicinc.com>
2024-12-23 22:11:13 +0800

    msm: npu: Fix use after free issue
    There is possibility that network will be used after free
    due to wait_event_interruptible & mutex drop.
    Add an is_executing flag to avoid unloading the network
    during executing thread is waiting for interrupt.
    
    Change-Id: Ia5a5d68aa4f322ecf9998f641e6c4089a1f76a3b
    Signed-off-by: Gao Wang <quic_gaowang@quicinc.com>

--- drivers/media/platform/msm/npu/npu_dev.c
+++ drivers/media/platform/msm/npu/npu_dev.c
@@ -1567,8 +1567,6 @@ static int npu_set_fw_state(struct npu_client *client, uint32_t enable)
 	int rc = 0;
 
 	if (host_ctx->network_num > 0) {
-		pr_err("Need to unload network first\n");
-		mutex_unlock(&npu_dev->dev_lock);
 		return -EINVAL;
 	}
 
