695df3e833c04aa48e57c28a25311dfdde241ce3
Dr. David Alan Gilbert <linux@treblig.org>
2024-12-23 00:16:13 +0000

    RDMA/irdma: Remove unused irdma_cqp_*_fpm_val_cmd functions
    irdma_cqp_commit_fpm_val_cmd() and irdma_cqp_query_fpm_val_cmd()
    were added in 2021 by
    commit 915cc7ac0f8e ("RDMA/irdma: Add miscellaneous utility definitions")
    but haven't been used.
    
    Remove them.
    
    Signed-off-by: Dr. David Alan Gilbert <linux@treblig.org>
    Link: https://patch.msgid.link/20241223001613.307138-1-linux@treblig.org
    Signed-off-by: Leon Romanovsky <leon@kernel.org>

--- drivers/infiniband/hw/irdma/osdep.h
+++ drivers/infiniband/hw/irdma/osdep.h
@@ -59,10 +59,6 @@ int irdma_cqp_sds_cmd(struct irdma_sc_dev *dev,
 int irdma_cqp_manage_hmc_fcn_cmd(struct irdma_sc_dev *dev,
 				 struct irdma_hmc_fcn_info *hmcfcninfo,
 				 u16 *pmf_idx);
-int irdma_cqp_query_fpm_val_cmd(struct irdma_sc_dev *dev,
-				struct irdma_dma_mem *val_mem, u8 hmc_fn_id);
-int irdma_cqp_commit_fpm_val_cmd(struct irdma_sc_dev *dev,
-				 struct irdma_dma_mem *val_mem, u8 hmc_fn_id);
 int irdma_alloc_query_fpm_buf(struct irdma_sc_dev *dev,
 			      struct irdma_dma_mem *mem);
 void *irdma_remove_cqp_head(struct irdma_sc_dev *dev);
