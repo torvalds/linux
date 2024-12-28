d7bde4f27ceef3dc6d72010a20d4da23db835a32
Chuck Lever <chuck.lever@oracle.com>
2024-12-28 12:55:18 -0500

    Revert "libfs: Add simple_offset_empty()"
    simple_empty() and simple_offset_empty() perform the same task.
    The latter's use as a canary to find bugs has not found any new
    issues. A subsequent patch will remove the use of the mtree for
    iterating directory contents, so revert back to using a similar
    mechanism for determining whether a directory is indeed empty.
    
    Only one such mechanism is ever needed.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Link: https://lore.kernel.org/r/20241228175522.1854234-3-cel@kernel.org
    Reviewed-by: Yang Erkun <yangerkun@huawei.com>
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -3468,7 +3468,6 @@ struct offset_ctx {
 void simple_offset_init(struct offset_ctx *octx);
 int simple_offset_add(struct offset_ctx *octx, struct dentry *dentry);
 void simple_offset_remove(struct offset_ctx *octx, struct dentry *dentry);
-int simple_offset_empty(struct dentry *dentry);
 int simple_offset_rename(struct inode *old_dir, struct dentry *old_dentry,
 			 struct inode *new_dir, struct dentry *new_dentry);
 int simple_offset_rename_exchange(struct inode *old_dir,
