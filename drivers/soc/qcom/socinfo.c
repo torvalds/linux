22cf4fae6660b6e1a583a41cbf84e3046ca9ccd0
Stephan Gerhold <stephan.gerhold@linaro.org>
2024-12-30 20:59:35 +0100

    soc: qcom: socinfo: Avoid out of bounds read of serial number
    On MSM8916 devices, the serial number exposed in sysfs is constant and does
    not change across individual devices. It's always:
    
      db410c:/sys/devices/soc0$ cat serial_number
      2644893864
    
    The firmware used on MSM8916 exposes SOCINFO_VERSION(0, 8), which does not
    have support for the serial_num field in the socinfo struct. There is an
    existing check to avoid exposing the serial number in that case, but it's
    not correct: When checking the item_size returned by SMEM, we need to make
    sure the *end* of the serial_num is within bounds, instead of comparing
    with the *start* offset. The serial_number currently exposed on MSM8916
    devices is just an out of bounds read of whatever comes after the socinfo
    struct in SMEM.
    
    Fix this by changing offsetof() to offsetofend(), so that the size of the
    field is also taken into account.
    
    Cc: stable@vger.kernel.org
    Fixes: efb448d0a3fc ("soc: qcom: Add socinfo driver")
    Signed-off-by: Stephan Gerhold <stephan.gerhold@linaro.org>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Link: https://lore.kernel.org/r/20241230-qcom-socinfo-serialno-oob-v1-1-9b7a890da3da@linaro.org
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -796,7 +796,7 @@ static int qcom_socinfo_probe(struct platform_device *pdev)
 	if (!qs->attr.soc_id || !qs->attr.revision)
 		return -ENOMEM;
 
-	if (offsetof(struct socinfo, serial_num) <= item_size) {
+	if (offsetofend(struct socinfo, serial_num) <= item_size) {
 		qs->attr.serial_number = devm_kasprintf(&pdev->dev, GFP_KERNEL,
 							"%u",
 							le32_to_cpu(info->serial_num));
