058b38ccd2af9e5c95590b018e8425fa148d7aca
Arnaldo Carvalho de Melo <acme@redhat.com>
2025-01-02 16:50:39 -0300

    perf top: Don't complain about lack of vmlinux when not resolving some kernel samples
    Recently we got a case where a kernel sample wasn't being resolved due
    to a bug that was not setting the end address on kernel functions
    implemented in assembly (see Link: tag), and then those were not being
    found by machine__resolve() -> map__find_symbol().
    
    So we ended up with:
    
      # perf top --stdio
      PerfTop: 0 irqs/s  kernel: 0%  exact: 0% lost: 0/0 drop: 0/0 [cycles/P]
      -----------------------------------------------------------------------
    
      Warning:
      A vmlinux file was not found.
      Kernel samples will not be resolved.
      ^Z
      [1]+  Stopped                 perf top --stdio
      #
    
    But then resolving all other kernel symbols.
    
    So just fixup the logic to only print that warning when there are no
    symbols in the kernel map.
    
    Fixes: d88205db9caa0e9d ("perf dso: Add dso__has_symbols() method")
    Reviewed-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Christophe Leroy <christophe.leroy@csgroup.eu>
    Cc: James Clark <james.clark@linaro.org>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Kan Liang <kan.liang@linux.intel.com>
    Link: https://lore.kernel.org/lkml/Z3buKhcCsZi3_aGb@x1
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/builtin-top.c
+++ tools/perf/builtin-top.c
@@ -809,7 +809,7 @@ static void perf_event__process_sample(const struct perf_tool *tool,
 		 * invalid --vmlinux ;-)
 		 */
 		if (!machine->kptr_restrict_warned && !top->vmlinux_warned &&
-		    __map__is_kernel(al.map) && map__has_symbols(al.map)) {
+		    __map__is_kernel(al.map) && !map__has_symbols(al.map)) {
 			if (symbol_conf.vmlinux_name) {
 				char serr[256];
 
