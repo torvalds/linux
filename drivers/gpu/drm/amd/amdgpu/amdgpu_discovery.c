76e0410fe07ea236477adf7468636a9def0769bd
Tim Huang <tim.huang@amd.com>
2025-01-02 14:30:35 +0800

    drm/amdgpu: add discovery support for DCN IP version 3.6.0
    Add discovery entry for DCN IP version 3.6.0.
    
    Signed-off-by: Tim Huang <tim.huang@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
@@ -2162,6 +2162,7 @@ static int amdgpu_discovery_set_display_ip_blocks(struct amdgpu_device *adev)
 		case IP_VERSION(3, 2, 1):
 		case IP_VERSION(3, 5, 0):
 		case IP_VERSION(3, 5, 1):
+		case IP_VERSION(3, 6, 0):
 		case IP_VERSION(4, 1, 0):
 			/* TODO: Fix IP version. DC code expects version 4.0.1 */
 			if (adev->ip_versions[DCE_HWIP][0] == IP_VERSION(4, 1, 0))
