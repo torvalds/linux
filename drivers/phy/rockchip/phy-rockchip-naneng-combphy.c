3126ea9be66b53e607f87f067641ba724be24181
Chukun Pan <amadeus@jmu.edu.cn>
2025-01-06 18:00:01 +0800

    phy: rockchip: naneng-combphy: compatible reset with old DT
    The device tree of RK3568 did not specify reset-names before.
    So add fallback to old behaviour to be compatible with old DT.
    
    Fixes: fbcbffbac994 ("phy: rockchip: naneng-combphy: fix phy reset")
    Cc: Jianfeng Liu <liujianfeng1994@gmail.com>
    Signed-off-by: Chukun Pan <amadeus@jmu.edu.cn>
    Reviewed-by: Jonas Karlman <jonas@kwiboo.se>
    Link: https://lore.kernel.org/r/20250106100001.1344418-2-amadeus@jmu.edu.cn
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/phy/rockchip/phy-rockchip-naneng-combphy.c
+++ drivers/phy/rockchip/phy-rockchip-naneng-combphy.c
@@ -324,7 +324,10 @@ static int rockchip_combphy_parse_dt(struct device *dev, struct rockchip_combphy
 
 	priv->ext_refclk = device_property_present(dev, "rockchip,ext-refclk");
 
-	priv->phy_rst = devm_reset_control_get(dev, "phy");
+	priv->phy_rst = devm_reset_control_get_exclusive(dev, "phy");
+	/* fallback to old behaviour */
+	if (PTR_ERR(priv->phy_rst) == -ENOENT)
+		priv->phy_rst = devm_reset_control_array_get_exclusive(dev);
 	if (IS_ERR(priv->phy_rst))
 		return dev_err_probe(dev, PTR_ERR(priv->phy_rst), "failed to get phy reset\n");
 
