cf5817ce66f6c75452027af243689da780dbddb4
Jaegeuk Kim <jaegeuk@kernel.org>
2025-01-07 02:26:34 +0000

    f2fs: don't call block truncation for aliased file
    This patch should avoid the below warning which does not corrupt the metadata
    tho.
    
    [   51.508120][  T253] F2FS-fs (dm-59): access invalid blkaddr:36
    [   51.508156][  T253]  __f2fs_is_valid_blkaddr+0x330/0x384
    [   51.508162][  T253]  f2fs_is_valid_blkaddr_raw+0x10/0x24
    [   51.508163][  T253]  f2fs_truncate_data_blocks_range+0x1ec/0x438
    [   51.508177][  T253]  f2fs_remove_inode_page+0x8c/0x148
    [   51.508194][  T253]  f2fs_evict_inode+0x230/0x76c
    
    Fixes: 128d333f0dff ("f2fs: introduce device aliasing file")
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -1275,8 +1275,9 @@ int f2fs_remove_inode_page(struct inode *inode)
 	}
 
 	/* remove potential inline_data blocks */
-	if (S_ISREG(inode->i_mode) || S_ISDIR(inode->i_mode) ||
-				S_ISLNK(inode->i_mode))
+	if (!IS_DEVICE_ALIASING(inode) &&
+	    (S_ISREG(inode->i_mode) || S_ISDIR(inode->i_mode) ||
+	     S_ISLNK(inode->i_mode)))
 		f2fs_truncate_data_blocks_range(&dn, 1);
 
 	/* 0 is possible, after f2fs_new_inode() has failed */
