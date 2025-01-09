bc87b75847d86f073a3df56e4ad44265b3d64e44
Phil Sutter <phil@nwl.cc>
2025-01-09 18:31:35 +0100

    netfilter: nf_tables: Compare netdev hooks based on stored name
    The 1:1 relationship between nft_hook and nf_hook_ops is about to break,
    so choose the stored ifname to uniquely identify hooks.
    
    Signed-off-by: Phil Sutter <phil@nwl.cc>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -2317,7 +2317,7 @@ static struct nft_hook *nft_hook_list_find(struct list_head *hook_list,
 	struct nft_hook *hook;
 
 	list_for_each_entry(hook, hook_list, list) {
-		if (this->ops.dev == hook->ops.dev)
+		if (!strcmp(hook->ifname, this->ifname))
 			return hook;
 	}
 
