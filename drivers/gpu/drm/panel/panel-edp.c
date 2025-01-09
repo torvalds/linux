749b5b279e5636cdcef51e15d67b77162cca6caa
Douglas Anderson <dianders@chromium.org>
2025-01-09 14:28:53 -0800

    drm/panel-edp: Add Starry 116KHD024006
    We have a few reports of sc7180-trogdor-pompom devices that have a
    panel in them that IDs as STA 0x0004 and has the following raw EDID:
    
      00 ff ff ff ff ff ff 00  4e 81 04 00 00 00 00 00
      10 20 01 04 a5 1a 0e 78  0a dc dd 96 5b 5b 91 28
      1f 52 54 00 00 00 01 01  01 01 01 01 01 01 01 01
      01 01 01 01 01 01 8e 1c  56 a0 50 00 1e 30 28 20
      55 00 00 90 10 00 00 18  00 00 00 00 00 00 00 00
      00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
      00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 fe
      00 31 31 36 4b 48 44 30  32 34 30 30 36 0a 00 e6
    
    We've been unable to locate a datasheet for this panel and our partner
    has not been responsive, but all Starry eDP datasheets that we can
    find agree on the same timing (delay_100_500_e200) so it should be
    safe to use that here instead of the super conservative timings. We'll
    still go a little extra conservative and allow `hpd_absent` of 200
    instead of 100 because that won't add any real-world delay in most
    cases.
    
    We'll associate the string from the EDID ("116KHD024006") with this
    panel. Given that the ID is the suspicious value of 0x0004 it seems
    likely that Starry doesn't always update their IDs but the string will
    still work to differentiate if we ever need to in the future.
    
    Reviewed-by: Neil Armstrong <neil.armstrong@linaro.org>
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20250109142853.1.Ibcc3009933fd19507cc9c713ad0c99c7a9e4fe17@changeid

--- drivers/gpu/drm/panel/panel-edp.c
+++ drivers/gpu/drm/panel/panel-edp.c
@@ -1996,6 +1996,7 @@ static const struct edp_panel_entry edp_panels[] = {
 	EDP_PANEL_ENTRY('S', 'H', 'P', 0x154c, &delay_200_500_p2e100, "LQ116M1JW10"),
 	EDP_PANEL_ENTRY('S', 'H', 'P', 0x1593, &delay_200_500_p2e100, "LQ134N1"),
 
+	EDP_PANEL_ENTRY('S', 'T', 'A', 0x0004, &delay_200_500_e200, "116KHD024006"),
 	EDP_PANEL_ENTRY('S', 'T', 'A', 0x0100, &delay_100_500_e200, "2081116HHD028001-51D"),
 
 	{ /* sentinal */ }
