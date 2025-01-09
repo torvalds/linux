965e9bbe025e02ddea3f34bdcb5757411593f43e
Thorsten Blum <thorsten.blum@linux.dev>
2025-01-09 11:46:34 +0100

    arm64: Remove duplicate included header
    The header asm/unistd_compat_32.h is included whether CONFIG_COMPAT is
    defined or not.
    
    Include it only once and remove the following make includecheck warning:
    
      asm/unistd_compat_32.h is included more than once
    
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Link: https://lore.kernel.org/r/20250109104636.124507-2-thorsten.blum@linux.dev
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/seccomp.h
+++ arch/arm64/include/asm/seccomp.h
@@ -23,7 +23,6 @@
 #define SECCOMP_ARCH_NATIVE_NR		NR_syscalls
 #define SECCOMP_ARCH_NATIVE_NAME	"aarch64"
 #ifdef CONFIG_COMPAT
-#include <asm/unistd_compat_32.h>
 # define SECCOMP_ARCH_COMPAT		AUDIT_ARCH_ARM
 # define SECCOMP_ARCH_COMPAT_NR		__NR_compat32_syscalls
 # define SECCOMP_ARCH_COMPAT_NAME	"arm"
