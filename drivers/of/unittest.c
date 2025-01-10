44748065ed321041db6e18cdcaa8c2a9554768ac
Rob Herring (Arm) <robh@kernel.org>
2025-01-10 15:50:28 -0600

    of/unittest: Add test that of_address_to_resource() fails on non-translatable address
    of_address_to_resource() on a non-translatable address should return an
    error. Additionally, this case also triggers a spurious WARN for
    missing #address-cells/#size-cells.
    
    Link: https://lore.kernel.org/r/20250110215030.3637845-1-robh@kernel.org
    Signed-off-by: Rob Herring (Arm) <robh@kernel.org>

--- drivers/of/unittest.c
+++ drivers/of/unittest.c
@@ -1380,6 +1380,7 @@ static void __init of_unittest_bus_3cell_ranges(void)
 static void __init of_unittest_reg(void)
 {
 	struct device_node *np;
+	struct resource res;
 	int ret;
 	u64 addr, size;
 
@@ -1396,6 +1397,19 @@ static void __init of_unittest_reg(void)
 		np, addr);
 
 	of_node_put(np);
+
+	np = of_find_node_by_path("/testcase-data/platform-tests-2/node/test-device@100");
+	if (!np) {
+		pr_err("missing testcase data\n");
+		return;
+	}
+
+	ret = of_address_to_resource(np, 0, &res);
+	unittest(ret == -EINVAL, "of_address_to_resource(%pOF) expected error on untranslatable address\n",
+		 np);
+
+	of_node_put(np);
+
 }
 
 struct of_unittest_expected_res {
