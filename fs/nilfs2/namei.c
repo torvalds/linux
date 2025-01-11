ee70999a988b8abc3490609142f50ebaa8344432
Ryusuke Konishi <konishi.ryusuke@gmail.com>
2025-01-11 23:26:35 +0900

    nilfs2: handle errors that nilfs_prepare_chunk() may return
    Patch series "nilfs2: fix issues with rename operations".
    
    This series fixes BUG_ON check failures reported by syzbot around rename
    operations, and a minor behavioral issue where the mtime of a child
    directory changes when it is renamed instead of moved.
    
    
    This patch (of 2):
    
    The directory manipulation routines nilfs_set_link() and
    nilfs_delete_entry() rewrite the directory entry in the folio/page
    previously read by nilfs_find_entry(), so error handling is omitted on the
    assumption that nilfs_prepare_chunk(), which prepares the buffer for
    rewriting, will always succeed for these.  And if an error is returned, it
    triggers the legacy BUG_ON() checks in each routine.
    
    This assumption is wrong, as proven by syzbot: the buffer layer called by
    nilfs_prepare_chunk() may call nilfs_get_block() if necessary, which may
    fail due to metadata corruption or other reasons.  This has been there all
    along, but improved sanity checks and error handling may have made it more
    reproducible in fuzzing tests.
    
    Fix this issue by adding missing error paths in nilfs_set_link(),
    nilfs_delete_entry(), and their caller nilfs_rename().
    
    Link: https://lkml.kernel.org/r/20250111143518.7901-1-konishi.ryusuke@gmail.com
    Link: https://lkml.kernel.org/r/20250111143518.7901-2-konishi.ryusuke@gmail.com
    Signed-off-by: Ryusuke Konishi <konishi.ryusuke@gmail.com>
    Reported-by: syzbot+32c3706ebf5d95046ea1@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=32c3706ebf5d95046ea1
    Reported-by: syzbot+1097e95f134f37d9395c@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=1097e95f134f37d9395c
    Fixes: 2ba466d74ed7 ("nilfs2: directory entry operations")
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/nilfs2/namei.c
+++ fs/nilfs2/namei.c
@@ -406,8 +406,10 @@ static int nilfs_rename(struct mnt_idmap *idmap,
 			err = PTR_ERR(new_de);
 			goto out_dir;
 		}
-		nilfs_set_link(new_dir, new_de, new_folio, old_inode);
+		err = nilfs_set_link(new_dir, new_de, new_folio, old_inode);
 		folio_release_kmap(new_folio, new_de);
+		if (unlikely(err))
+			goto out_dir;
 		nilfs_mark_inode_dirty(new_dir);
 		inode_set_ctime_current(new_inode);
 		if (dir_de)
@@ -430,20 +432,16 @@ static int nilfs_rename(struct mnt_idmap *idmap,
 	 */
 	inode_set_ctime_current(old_inode);
 
-	nilfs_delete_entry(old_de, old_folio);
-
-	if (dir_de) {
-		nilfs_set_link(old_inode, dir_de, dir_folio, new_dir);
-		folio_release_kmap(dir_folio, dir_de);
-		drop_nlink(old_dir);
+	err = nilfs_delete_entry(old_de, old_folio);
+	if (likely(!err)) {
+		if (dir_de) {
+			err = nilfs_set_link(old_inode, dir_de, dir_folio,
+					     new_dir);
+			drop_nlink(old_dir);
+		}
+		nilfs_mark_inode_dirty(old_dir);
 	}
-	folio_release_kmap(old_folio, old_de);
-
-	nilfs_mark_inode_dirty(old_dir);
 	nilfs_mark_inode_dirty(old_inode);
-
-	err = nilfs_transaction_commit(old_dir->i_sb);
-	return err;
 
 out_dir:
 	if (dir_de)
@@ -451,7 +449,10 @@ out_dir:
 out_old:
 	folio_release_kmap(old_folio, old_de);
 out:
-	nilfs_transaction_abort(old_dir->i_sb);
+	if (likely(!err))
+		err = nilfs_transaction_commit(old_dir->i_sb);
+	else
+		nilfs_transaction_abort(old_dir->i_sb);
 	return err;
 }
 
