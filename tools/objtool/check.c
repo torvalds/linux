cee6f9a9c87b6ecfb51845950c28216b231c3610
Miguel Ojeda <ojeda@kernel.org>
2025-01-12 15:39:51 +0100

    objtool/rust: add one more `noreturn` Rust function
    Starting with Rust 1.85.0 (currently in beta, to be released 2025-02-20),
    under some kernel configurations with `CONFIG_RUST_DEBUG_ASSERTIONS=y`,
    one may trigger a new `objtool` warning:
    
        rust/kernel.o: warning: objtool: _R...securityNtB2_11SecurityCtx8as_bytes()
        falls through to next function _R...core3ops4drop4Drop4drop()
    
    due to a call to the `noreturn` symbol:
    
        core::panicking::assert_failed::<usize, usize>
    
    Thus add it to the list so that `objtool` knows it is actually `noreturn`.
    Do so matching with `strstr` since it is a generic.
    
    See commit 56d680dd23c3 ("objtool/rust: list `noreturn` Rust functions")
    for more details.
    
    Cc: stable@vger.kernel.org # Needed in 6.12.y and 6.13.y only (Rust is pinned in older LTSs).
    Fixes: 56d680dd23c3 ("objtool/rust: list `noreturn` Rust functions")
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Link: https://lore.kernel.org/r/20250112143951.751139-1-ojeda@kernel.org
    [ Updated Cc: stable@ to include 6.13.y. - Miguel ]
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -227,6 +227,7 @@ static bool is_rust_noreturn(const struct symbol *func)
 	       str_ends_with(func->name, "_4core9panicking18panic_bounds_check")			||
 	       str_ends_with(func->name, "_4core9panicking19assert_failed_inner")			||
 	       str_ends_with(func->name, "_4core9panicking36panic_misaligned_pointer_dereference")	||
+	       strstr(func->name, "_4core9panicking13assert_failed")					||
 	       strstr(func->name, "_4core9panicking11panic_const24panic_const_")			||
 	       (strstr(func->name, "_4core5slice5index24slice_") &&
 		str_ends_with(func->name, "_fail"));
