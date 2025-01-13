eac5b138814a69435dae04c41591023477b3a18d
Zixian Zeng <sycamoremoon376@gmail.com>
2025-01-14 00:30:20 +0800

    riscv: remove redundant CMDLINE_FORCE check
    Drop redundant CMDLINE_FORCE check as it's already done in
    function early_init_dt_scan_chosen().
    
    Signed-off-by: Zixian Zeng <sycamoremoon376@gmail.com>
    Reviewed-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Link: https://lore.kernel.org/r/20250114-rebund-v1-1-5632b2d54d6c@gmail.com
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>

--- arch/riscv/kernel/setup.c
+++ arch/riscv/kernel/setup.c
@@ -235,11 +235,6 @@ static void __init parse_dtb(void)
 	} else {
 		pr_err("No DTB passed to the kernel\n");
 	}
-
-#ifdef CONFIG_CMDLINE_FORCE
-	strscpy(boot_command_line, CONFIG_CMDLINE, COMMAND_LINE_SIZE);
-	pr_info("Forcing kernel command line to: %s\n", boot_command_line);
-#endif
 }
 
 #if defined(CONFIG_RISCV_COMBO_SPINLOCKS)
