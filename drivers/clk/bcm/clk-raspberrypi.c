e7d0b023955ae3cb25796e19d44a4d55b5ad58de
Maxime Ripard <mripard@kernel.org>
2025-01-16 16:24:12 +0000

    clk: bcm: rpi: Add disp clock
    BCM2712 has an extra clock exposed by the firmware called DISP, and used
    by (at least) the HVS. Let's add it to the list of clocks to register in
    Linux.
    
    Acked-by: Stephen Boyd <sboyd@kernel.org>
    Signed-off-by: Maxime Ripard <mripard@kernel.org>
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>
    Link: https://lore.kernel.org/r/20250116-bcm2712-clk-updates-v1-5-10bc92ffbf41@raspberrypi.com
    Reviewed-by: Florian Fainelli <florian.fainelli@broadcom.com>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/bcm/clk-raspberrypi.c
+++ drivers/clk/bcm/clk-raspberrypi.c
@@ -34,6 +34,7 @@ static char *rpi_firmware_clk_names[] = {
 	[RPI_FIRMWARE_M2MC_CLK_ID]	= "m2mc",
 	[RPI_FIRMWARE_PIXEL_BVB_CLK_ID]	= "pixel-bvb",
 	[RPI_FIRMWARE_VEC_CLK_ID]	= "vec",
+	[RPI_FIRMWARE_DISP_CLK_ID]	= "disp",
 };
 
 #define RPI_FIRMWARE_STATE_ENABLE_BIT	BIT(0)
@@ -136,6 +137,10 @@ raspberrypi_clk_variants[RPI_FIRMWARE_NUM_CLK_ID] = {
 		.minimize = true,
 	},
 	[RPI_FIRMWARE_VEC_CLK_ID] = {
+		.export = true,
+		.minimize = true,
+	},
+	[RPI_FIRMWARE_DISP_CLK_ID] = {
 		.export = true,
 		.minimize = true,
 	},
