8e622fb1b91d8f2e390d4331b0d33ff3dfb59b72
Mandar Mahesh Kamble <quic_maheshka@quicinc.com>
2025-01-17 08:56:05 +0530

    soc: qcom: Add imem support for reboot reason driver
    Add imem support for reboot reason driver.
    
    Change-Id: Ie94b0fd2737e4fe0aca7d8d512d8b231bb931701
    Signed-off-by: Mandar Mahesh Kamble <quic_maheshka@quicinc.com>

--- drivers/power/reset/qcom-reboot-reason.c
+++ drivers/power/reset/qcom-reboot-reason.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /* Copyright (c) 2019, 2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2024-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 #define pr_fmt(fmt)     "qcom-reboot-reason: %s: " fmt, __func__
 
@@ -15,27 +15,48 @@
 #include <linux/pm.h>
 #include <linux/of_address.h>
 #include <linux/nvmem-consumer.h>
+#include <linux/of_device.h>
+#include <linux/of_platform.h>
 
 struct qcom_reboot_reason {
 	struct device *dev;
 	struct notifier_block reboot_nb;
 	struct nvmem_cell *nvmem_cell;
+	struct poweroff_reason *reasons;
 };
 
 struct poweroff_reason {
 	const char *cmd;
-	unsigned char pon_reason;
+	unsigned int pon_reason;
+	unsigned int size;
 };
 
-static struct poweroff_reason reasons[] = {
-	{ "recovery",			0x01 },
-	{ "bootloader",			0x02 },
-	{ "rtc",			0x03 },
-	{ "dm-verity device corrupted",	0x04 },
-	{ "dm-verity enforcing",	0x05 },
-	{ "keys clear",			0x06 },
+static struct poweroff_reason pon_reasons[] = {
+	{ "recovery",			0x01, 0x1 },
+	{ "bootloader",			0x02, 0x1 },
+	{ "rtc",			0x03, 0x1 },
+	{ "dm-verity device corrupted",	0x04, 0x1 },
+	{ "dm-verity enforcing",	0x05, 0x1 },
+	{ "keys clear",			0x06, 0x1 },
 	{}
 };
+
+static  struct poweroff_reason imem_pon_reasons[] = {
+	{ "recovery",                   0x77665502, 0x4 },
+	{ "bootloader",                 0x77665500, 0x4 },
+	{ "rtc",                        0x77665503, 0x4 },
+	{ "dm-verity device corrupted", 0x77665508, 0x4 },
+	{ "dm-verity enforcing",        0x77665509, 0x4 },
+	{ "keys clear",                 0x7766550a, 0x4 },
+	{}
+};
+
+static const struct of_device_id of_qcom_reboot_reason_match[] = {
+	{ .compatible = "qcom,reboot-reason", .data = pon_reasons },
+	{ .compatible = "qcom,imem-reboot-reason", .data = imem_pon_reasons },
+	{},
+};
+MODULE_DEVICE_TABLE(of, of_qcom_reboot_reason_match);
 
 static int qcom_reboot_reason_reboot(struct notifier_block *this,
 				     unsigned long event, void *ptr)
@@ -48,11 +69,11 @@ static int qcom_reboot_reason_reboot(struct notifier_block *this,
 
 	if (!cmd)
 		return NOTIFY_OK;
-	for (reason = reasons; reason->cmd; reason++) {
+	for (reason = reboot->reasons; reason->cmd; reason++) {
 		if (!strcmp(cmd, reason->cmd)) {
 			rc = nvmem_cell_write(reboot->nvmem_cell,
 					 &reason->pon_reason,
-					 sizeof(reason->pon_reason));
+					 reason->size);
 			if (rc < 0)
 				pr_err("PON reason store failed, rc=%d\n", rc);
 			break;
@@ -65,6 +86,7 @@ static int qcom_reboot_reason_reboot(struct notifier_block *this,
 static int qcom_reboot_reason_probe(struct platform_device *pdev)
 {
 	struct qcom_reboot_reason *reboot;
+	const struct of_device_id *match;
 
 	reboot = devm_kzalloc(&pdev->dev, sizeof(*reboot), GFP_KERNEL);
 	if (!reboot)
@@ -72,7 +94,18 @@ static int qcom_reboot_reason_probe(struct platform_device *pdev)
 
 	reboot->dev = &pdev->dev;
 
-	reboot->nvmem_cell = nvmem_cell_get(reboot->dev, "restart_reason");
+	/*
+	 * can't use of_device_get_match_data() because it returns
+	 * const data. nvmem_cell_write() doesn't have signature to accept const data.
+	 * Hence used of_match_device().
+	 */
+
+	match = of_match_device(of_match_ptr(of_qcom_reboot_reason_match), &pdev->dev);
+	if (!match)
+		return -ENODEV;
+	reboot->reasons = (struct poweroff_reason *)match->data;
+
+	reboot->nvmem_cell = devm_nvmem_cell_get(reboot->dev, "restart_reason");
 
 	if (IS_ERR(reboot->nvmem_cell))
 		return PTR_ERR(reboot->nvmem_cell);
@@ -95,11 +128,6 @@ static int qcom_reboot_reason_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static const struct of_device_id of_qcom_reboot_reason_match[] = {
-	{ .compatible = "qcom,reboot-reason", },
-	{},
-};
-MODULE_DEVICE_TABLE(of, of_qcom_reboot_reason_match);
 
 static struct platform_driver qcom_reboot_reason_driver = {
 	.probe = qcom_reboot_reason_probe,
