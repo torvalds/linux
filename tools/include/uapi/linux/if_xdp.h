01f3ce5328c405179b2c69ea047c423dad2bfa6d
Vishal Chourasia <vishalc@linux.ibm.com>
2025-01-17 15:42:59 +0100

    tools: Sync if_xdp.h uapi tooling header
    Sync if_xdp.h uapi header to remove following warning:
    
      Warning: Kernel ABI header at 'tools/include/uapi/linux/if_xdp.h'
      differs from latest version at 'include/uapi/linux/if_xdp.h'
    
    Fixes: 48eb03dd2630 ("xsk: Add TX timestamp and TX checksum offload support")
    Signed-off-by: Vishal Chourasia <vishalc@linux.ibm.com>
    Signed-off-by: Song Yoong Siang <yoong.siang.song@intel.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/bpf/20250115032248.125742-1-yoong.siang.song@intel.com

--- tools/include/uapi/linux/if_xdp.h
+++ tools/include/uapi/linux/if_xdp.h
@@ -117,12 +117,12 @@ struct xdp_options {
 	((1ULL << XSK_UNALIGNED_BUF_OFFSET_SHIFT) - 1)
 
 /* Request transmit timestamp. Upon completion, put it into tx_timestamp
- * field of union xsk_tx_metadata.
+ * field of struct xsk_tx_metadata.
  */
 #define XDP_TXMD_FLAGS_TIMESTAMP		(1 << 0)
 
 /* Request transmit checksum offload. Checksum start position and offset
- * are communicated via csum_start and csum_offset fields of union
+ * are communicated via csum_start and csum_offset fields of struct
  * xsk_tx_metadata.
  */
 #define XDP_TXMD_FLAGS_CHECKSUM			(1 << 1)
