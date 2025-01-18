14a627fe794a10cf507861e63cf9d46077699337
Yonghong Song <yonghong.song@linux.dev>
2025-01-18 11:20:34 -0800

    selftests/bpf: Add some tests related to 'may_goto 0' insns
    Add both asm-based and C-based tests which have 'may_goto 0' insns.
    
    For the following code in C-based test,
       int i, tmp[3];
       for (i = 0; i < 3 && can_loop; i++)
           tmp[i] = 0;
    
    The clang compiler (clang 19 and 20) generates
       may_goto 2
       may_goto 1
       may_goto 0
       r1 = 0
       r2 = 0
       r3 = 0
    
    The above asm codes are due to llvm pass SROAPass. This ensures the
    successful verification since tmp[0-2] are initialized.  Otherwise,
    the code without SROAPass like
       may_goto 5
       r1 = 0
       may_goto 3
       r2 = 0
       may_goto 1
       r3 = 0
    will have verification failure.
    
    Although from the source code C-based test should have verification
    failure, clang compiler optimization generates code with successful
    verification. If gcc generates different asm codes than clang, the
    following code can be used for gcc:
       int i, tmp[3];
       for (i = 0; i < 3; i++)
           tmp[i] = 0;
    
    Acked-by: Eduard Zingerman <eddyz87@gmail.com>
    Signed-off-by: Yonghong Song <yonghong.song@linux.dev>
    Link: https://lore.kernel.org/r/20250118192034.2124952-1-yonghong.song@linux.dev
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/testing/selftests/bpf/prog_tests/verifier.c
+++ tools/testing/selftests/bpf/prog_tests/verifier.c
@@ -52,6 +52,8 @@
 #include "verifier_map_ptr_mixing.skel.h"
 #include "verifier_map_ret_val.skel.h"
 #include "verifier_masking.skel.h"
+#include "verifier_may_goto_1.skel.h"
+#include "verifier_may_goto_2.skel.h"
 #include "verifier_meta_access.skel.h"
 #include "verifier_movsx.skel.h"
 #include "verifier_mtu.skel.h"
@@ -182,6 +184,8 @@ void test_verifier_map_ptr(void)              { RUN(verifier_map_ptr); }
 void test_verifier_map_ptr_mixing(void)       { RUN(verifier_map_ptr_mixing); }
 void test_verifier_map_ret_val(void)          { RUN(verifier_map_ret_val); }
 void test_verifier_masking(void)              { RUN(verifier_masking); }
+void test_verifier_may_goto_1(void)           { RUN(verifier_may_goto_1); }
+void test_verifier_may_goto_2(void)           { RUN(verifier_may_goto_2); }
 void test_verifier_meta_access(void)          { RUN(verifier_meta_access); }
 void test_verifier_movsx(void)                 { RUN(verifier_movsx); }
 void test_verifier_netfilter_ctx(void)        { RUN(verifier_netfilter_ctx); }
