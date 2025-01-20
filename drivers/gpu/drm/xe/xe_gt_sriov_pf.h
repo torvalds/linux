9ebb5846e1a3b1705f8a7cbc528888a1aa0b163e
Michal Wajdeczko <michal.wajdeczko@intel.com>
2025-01-21 00:24:43 +0100

    drm/xe/pf: Fix migration initialization
    The migration support only needs to be initialized once, but it
    was incorrectly called from the xe_gt_sriov_pf_init_hw(), which
    is part of the reset flow and may be called multiple times.
    
    Fixes: d86e3737c7ab ("drm/xe/pf: Add functions to save and restore VF GuC state")
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Michał Winiarski <michal.winiarski@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20250120232443.544-1-michal.wajdeczko@intel.com

--- drivers/gpu/drm/xe/xe_gt_sriov_pf.h
+++ drivers/gpu/drm/xe/xe_gt_sriov_pf.h
@@ -10,11 +10,17 @@ struct xe_gt;
 
 #ifdef CONFIG_PCI_IOV
 int xe_gt_sriov_pf_init_early(struct xe_gt *gt);
+int xe_gt_sriov_pf_init(struct xe_gt *gt);
 void xe_gt_sriov_pf_init_hw(struct xe_gt *gt);
 void xe_gt_sriov_pf_sanitize_hw(struct xe_gt *gt, unsigned int vfid);
 void xe_gt_sriov_pf_restart(struct xe_gt *gt);
 #else
 static inline int xe_gt_sriov_pf_init_early(struct xe_gt *gt)
+{
+	return 0;
+}
+
+static inline int xe_gt_sriov_pf_init(struct xe_gt *gt)
 {
 	return 0;
 }
