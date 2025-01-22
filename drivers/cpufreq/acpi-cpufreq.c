5c840223abc58dcc143713384e5efbae0a6dc050
Viresh Kumar <viresh.kumar@linaro.org>
2025-01-22 16:23:51 +0530

    cpufreq: acpi: Stop setting common freq attributes
    The core handles this now, the driver can skip setting it.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Rafael J. Wysocki <rafael@kernel.org>

--- drivers/cpufreq/acpi-cpufreq.c
+++ drivers/cpufreq/acpi-cpufreq.c
@@ -949,7 +949,6 @@ static int acpi_cpufreq_resume(struct cpufreq_policy *policy)
 }
 
 static struct freq_attr *acpi_cpufreq_attr[] = {
-	&cpufreq_freq_attr_scaling_available_freqs,
 	&freqdomain_cpus,
 #ifdef CONFIG_X86_ACPI_CPUFREQ_CPB
 	&cpb,
