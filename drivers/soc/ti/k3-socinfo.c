a5caf03188e44388e8c618dcbe5fffad1a249385
Andrew Davis <afd@ti.com>
2025-01-23 12:17:26 -0600

    soc: ti: k3-socinfo: Do not use syscon helper to build regmap
    The syscon helper device_node_to_regmap() is used to fetch a regmap
    registered to a device node. It also currently creates this regmap
    if the node did not already have a regmap associated with it. This
    should only be used on "syscon" nodes. This driver is not such a
    device and instead uses device_node_to_regmap() on its own node as
    a hacky way to create a regmap for itself.
    
    This will not work going forward and so we should create our regmap
    the normal way by defining our regmap_config, fetching our memory
    resource, then using the normal regmap_init_mmio() function.
    
    Signed-off-by: Andrew Davis <afd@ti.com>
    Link: https://lore.kernel.org/r/20250123181726.597144-1-afd@ti.com
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- drivers/soc/ti/k3-socinfo.c
+++ drivers/soc/ti/k3-socinfo.c
@@ -105,6 +105,12 @@ err_unknown_variant:
 	return -ENODEV;
 }
 
+static const struct regmap_config k3_chipinfo_regmap_cfg = {
+	.reg_bits = 32,
+	.val_bits = 32,
+	.reg_stride = 4,
+};
+
 static int k3_chipinfo_probe(struct platform_device *pdev)
 {
 	struct device_node *node = pdev->dev.of_node;
@@ -112,13 +118,18 @@ static int k3_chipinfo_probe(struct platform_device *pdev)
 	struct device *dev = &pdev->dev;
 	struct soc_device *soc_dev;
 	struct regmap *regmap;
+	void __iomem *base;
 	u32 partno_id;
 	u32 variant;
 	u32 jtag_id;
 	u32 mfg;
 	int ret;
 
-	regmap = device_node_to_regmap(node);
+	base = devm_platform_ioremap_resource(pdev, 0);
+	if (IS_ERR(base))
+		return PTR_ERR(base);
+
+	regmap = regmap_init_mmio(dev, base, &k3_chipinfo_regmap_cfg);
 	if (IS_ERR(regmap))
 		return PTR_ERR(regmap);
 
