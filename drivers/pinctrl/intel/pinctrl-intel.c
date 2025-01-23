c42a407beeb91dd5e8cf1d537a7291a5d6415192
Uwe Kleine-König <u.kleine-koenig@baylibre.com>
2025-01-23 11:11:10 +0100

    pinctrl: intel: Import PWM_LPSS namespace for devm_pwm_lpss_probe()
    The Intel pinctrl driver can provide a PWM device and for that needs to
    call the function devm_pwm_lpss_probe(). That function is provided by
    the pwm-lpss driver which intends to export it in the "PWM_LPSS"
    namespace. To prepare fixing the pwm-lpss driver to indeed use the
    "PWM_LPSS" namespace, import that namespace when used.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@baylibre.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/pinctrl/intel/pinctrl-intel.c
+++ drivers/pinctrl/intel/pinctrl-intel.c
@@ -1941,3 +1941,4 @@ MODULE_AUTHOR("Mathias Nyman <mathias.nyman@linux.intel.com>");
 MODULE_AUTHOR("Mika Westerberg <mika.westerberg@linux.intel.com>");
 MODULE_DESCRIPTION("Intel pinctrl/GPIO core driver");
 MODULE_LICENSE("GPL v2");
+MODULE_IMPORT_NS("PWM_LPSS");
