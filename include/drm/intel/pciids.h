16016ade13f691da315fac7b23ebf1ab7b28b7ab
Matt Atwood <matthew.s.atwood@intel.com>
2025-01-28 09:51:02 -0800

    drm/xe/ptl: Update the PTL pci id table
    Update to current bspec table.
    
    Bspec: 72574
    
    Signed-off-by: Matt Atwood <matthew.s.atwood@intel.com>
    Reviewed-by: Clint Taylor <Clinton.A.Taylor@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20250128175102.45797-1-matthew.s.atwood@intel.com
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- include/drm/intel/pciids.h
+++ include/drm/intel/pciids.h
@@ -827,12 +827,10 @@
 	MACRO__(0xB080, ## __VA_ARGS__), \
 	MACRO__(0xB081, ## __VA_ARGS__), \
 	MACRO__(0xB082, ## __VA_ARGS__), \
+	MACRO__(0xB083, ## __VA_ARGS__), \
+	MACRO__(0xB08F, ## __VA_ARGS__), \
 	MACRO__(0xB090, ## __VA_ARGS__), \
-	MACRO__(0xB091, ## __VA_ARGS__), \
-	MACRO__(0xB092, ## __VA_ARGS__), \
 	MACRO__(0xB0A0, ## __VA_ARGS__), \
-	MACRO__(0xB0A1, ## __VA_ARGS__), \
-	MACRO__(0xB0A2, ## __VA_ARGS__), \
 	MACRO__(0xB0B0, ## __VA_ARGS__)
 
 #endif /* __PCIIDS_H__ */
