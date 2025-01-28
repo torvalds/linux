3775fc538f535a7c5adaf11990c7932a0bd1f9eb
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2025-01-28 20:24:41 +0100

    PM: sleep: core: Synchronize runtime PM status of parents and children
    Commit 6e176bf8d461 ("PM: sleep: core: Do not skip callbacks in the
    resume phase") overlooked the case in which the parent of a device with
    DPM_FLAG_SMART_SUSPEND set did not use that flag and could be runtime-
    suspended before a transition into a system-wide sleep state.  In that
    case, if the child is resumed during the subsequent transition from
    that state into the working state, its runtime PM status will be set to
    RPM_ACTIVE, but the runtime PM status of the parent will not be updated
    accordingly, even though the parent will be resumed too, because of the
    dev_pm_skip_suspend() check in device_resume_noirq().
    
    Address this problem by tracking the need to set the runtime PM status
    to RPM_ACTIVE during system-wide resume transitions for devices with
    DPM_FLAG_SMART_SUSPEND set and all of the devices depended on by them.
    
    Fixes: 6e176bf8d461 ("PM: sleep: core: Do not skip callbacks in the resume phase")
    Closes: https://lore.kernel.org/linux-pm/Z30p2Etwf3F2AUvD@hovoldconsulting.com/
    Reported-by: Johan Hovold <johan@kernel.org>
    Tested-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Johan Hovold <johan+linaro@kernel.org>
    Tested-by: Johan Hovold <johan+linaro@kernel.org>
    Link: https://patch.msgid.link/12619233.O9o76ZdvQC@rjwysocki.net

--- include/linux/pm.h
+++ include/linux/pm.h
@@ -679,6 +679,7 @@ struct dev_pm_info {
 	bool			no_pm_callbacks:1;	/* Owned by the PM core */
 	bool			async_in_progress:1;	/* Owned by the PM core */
 	bool			must_resume:1;		/* Owned by the PM core */
+	bool			set_active:1;		/* Owned by the PM core */
 	bool			may_skip_resume:1;	/* Set by subsystems */
 #else
 	bool			should_wakeup:1;
