5c2c07a18c7d9763bcc6e47466c6bbd40fcd61fc
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2025-01-29 17:24:42 +0200

    iio: pressure: zpa2326: Drop unneeded assignment for cache_type
    REGCACHE_NONE is the default type of the cache when not provided.
    Drop unneeded explicit assignment to it.
    
    Note, it's defined to 0, and if ever be redefined, it will break
    literally a lot of the drivers, so it very unlikely to happen.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Nuno Sá <nuno.sa@analog.com>
    Link: https://patch.msgid.link/20250129152546.1798306-4-andriy.shevchenko@linux.intel.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/pressure/zpa2326_spi.c
+++ drivers/iio/pressure/zpa2326_spi.c
@@ -26,7 +26,6 @@ static const struct regmap_config zpa2326_regmap_spi_config = {
 	.precious_reg   = zpa2326_isreg_precious,
 	.max_register   = ZPA2326_TEMP_OUT_H_REG,
 	.read_flag_mask = BIT(7) | BIT(6),
-	.cache_type     = REGCACHE_NONE,
 };
 
 static int zpa2326_probe_spi(struct spi_device *spi)
