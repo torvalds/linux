ba69e0750b0362870294adab09339a0c39c3beaf
Ard Biesheuvel <ardb@kernel.org>
2025-02-01 18:21:35 +0100

    efi: Avoid cold plugged memory for placing the kernel
    UEFI 2.11 introduced EFI_MEMORY_HOT_PLUGGABLE to annotate system memory
    regions that are 'cold plugged' at boot, i.e., hot pluggable memory that
    is available from early boot, and described as system RAM by the
    firmware.
    
    Existing loaders and EFI applications running in the boot context will
    happily use this memory for allocating data structures that cannot be
    freed or moved at runtime, and this prevents the memory from being
    unplugged. Going forward, the new EFI_MEMORY_HOT_PLUGGABLE attribute
    should be tested, and memory annotated as such should be avoided for
    such allocations.
    
    In the EFI stub, there are a couple of occurrences where, instead of the
    high-level AllocatePages() UEFI boot service, a low-level code sequence
    is used that traverses the EFI memory map and carves out the requested
    number of pages from a free region. This is needed, e.g., for allocating
    as low as possible, or for allocating pages at random.
    
    While AllocatePages() should presumably avoid special purpose memory and
    cold plugged regions, this manual approach needs to incorporate this
    logic itself, in order to prevent the kernel itself from ending up in a
    hot unpluggable region, preventing it from being unplugged.
    
    So add the EFI_MEMORY_HOTPLUGGABLE macro definition, and check for it
    where appropriate.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- include/linux/efi.h
+++ include/linux/efi.h
@@ -128,6 +128,7 @@ typedef	struct {
 #define EFI_MEMORY_RO		((u64)0x0000000000020000ULL)	/* read-only */
 #define EFI_MEMORY_SP		((u64)0x0000000000040000ULL)	/* soft reserved */
 #define EFI_MEMORY_CPU_CRYPTO	((u64)0x0000000000080000ULL)	/* supports encryption */
+#define EFI_MEMORY_HOT_PLUGGABLE	BIT_ULL(20)	/* supports unplugging at runtime */
 #define EFI_MEMORY_RUNTIME	((u64)0x8000000000000000ULL)	/* range requires runtime mapping */
 #define EFI_MEMORY_DESCRIPTOR_VERSION	1
 
