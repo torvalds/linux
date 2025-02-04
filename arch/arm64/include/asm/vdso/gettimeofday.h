0b3bc3354eb9ad36719a044726092750a2ba01ff
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2025-02-04 13:05:40 +0100

    arm64: vdso: Switch to generic storage implementation
    The generic storage implementation provides the same features as the
    custom one. However it can be shared between architectures, making
    maintenance easier.
    
    This switch also moves the random state data out of the time data page.
    The currently used hardcoded __VDSO_RND_DATA_OFFSET does not take into
    account changes to the time data page layout.
    
    Co-developed-by: Nam Cao <namcao@linutronix.de>
    Signed-off-by: Nam Cao <namcao@linutronix.de>
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/all/20250204-vdso-store-rng-v3-8-13a4669dfc8c@linutronix.de

--- arch/arm64/include/asm/vdso/gettimeofday.h
+++ arch/arm64/include/asm/vdso/gettimeofday.h
@@ -67,7 +67,7 @@ int clock_getres_fallback(clockid_t _clkid, struct __kernel_timespec *_ts)
 }
 
 static __always_inline u64 __arch_get_hw_counter(s32 clock_mode,
-						 const struct vdso_data *vd)
+						 const struct vdso_time_data *vd)
 {
 	u64 res;
 
@@ -98,20 +98,6 @@ static __always_inline u64 __arch_get_hw_counter(s32 clock_mode,
 
 	return res;
 }
-
-static __always_inline
-const struct vdso_data *__arch_get_vdso_data(void)
-{
-	return _vdso_data;
-}
-
-#ifdef CONFIG_TIME_NS
-static __always_inline
-const struct vdso_data *__arch_get_timens_vdso_data(const struct vdso_data *vd)
-{
-	return _timens_data;
-}
-#endif
 
 #endif /* !__ASSEMBLY__ */
 
