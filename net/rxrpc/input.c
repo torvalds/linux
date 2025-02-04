2d7b30aef34dae942e9ab7812b288ce14658ae66
David Howells <dhowells@redhat.com>
2025-02-04 23:05:54 +0000

    rxrpc: Fix race in call state changing vs recvmsg()
    There's a race in between the rxrpc I/O thread recording the end of the
    receive phase of a call and recvmsg() examining the state of the call to
    determine whether it has completed.
    
    The problem is that call->_state records the I/O thread's view of the call,
    not the application's view (which may lag), so that alone is not
    sufficient.  To this end, the application also checks whether there is
    anything left in call->recvmsg_queue for it to pick up.  The call must be
    in state RXRPC_CALL_COMPLETE and the recvmsg_queue empty for the call to be
    considered fully complete.
    
    In rxrpc_input_queue_data(), the latest skbuff is added to the queue and
    then, if it was marked as LAST_PACKET, the state is advanced...  But this
    is two separate operations with no locking around them.
    
    As a consequence, the lack of locking means that sendmsg() can jump into
    the gap on a service call and attempt to send the reply - but then get
    rejected because the I/O thread hasn't advanced the state yet.
    
    Simply flipping the order in which things are done isn't an option as that
    impacts the client side, causing the checks in rxrpc_kernel_check_life() as
    to whether the call is still alive to race instead.
    
    Fix this by moving the update of call->_state inside the skb queue
    spinlocked section where the packet is queued on the I/O thread side.
    
    rxrpc's recvmsg() will then automatically sync against this because it has
    to take the call->recvmsg_queue spinlock in order to dequeue the last
    packet.
    
    rxrpc's sendmsg() doesn't need amending as the app shouldn't be calling it
    to send a reply until recvmsg() indicates it has returned all of the
    request.
    
    Fixes: 93368b6bd58a ("rxrpc: Move call state changes from recvmsg to I/O thread")
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: Marc Dionne <marc.dionne@auristor.com>
    cc: Simon Horman <horms@kernel.org>
    cc: linux-afs@lists.infradead.org
    Link: https://patch.msgid.link/20250204230558.712536-3-dhowells@redhat.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/rxrpc/input.c
+++ net/rxrpc/input.c
@@ -448,11 +448,19 @@ static void rxrpc_input_queue_data(struct rxrpc_call *call, struct sk_buff *skb,
 	struct rxrpc_skb_priv *sp = rxrpc_skb(skb);
 	bool last = sp->hdr.flags & RXRPC_LAST_PACKET;
 
-	skb_queue_tail(&call->recvmsg_queue, skb);
+	spin_lock_irq(&call->recvmsg_queue.lock);
+
+	__skb_queue_tail(&call->recvmsg_queue, skb);
 	rxrpc_input_update_ack_window(call, window, wtop);
 	trace_rxrpc_receive(call, last ? why + 1 : why, sp->hdr.serial, sp->hdr.seq);
 	if (last)
+		/* Change the state inside the lock so that recvmsg syncs
+		 * correctly with it and using sendmsg() to send a reply
+		 * doesn't race.
+		 */
 		rxrpc_end_rx_phase(call, sp->hdr.serial);
+
+	spin_unlock_irq(&call->recvmsg_queue.lock);
 }
 
 /*
