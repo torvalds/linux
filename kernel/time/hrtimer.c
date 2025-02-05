1cc24f2e766c5a6606b834a677bd58991c1b9781
Nam Cao <namcao@linutronix.de>
2025-02-05 11:55:17 +0100

    hrtimers: Remove unnecessary NULL check in hrtimer_start_range_ns()
    The struct hrtimer::function field can only be changed using
    hrtimer_setup*() or hrtimer_update_function(), and both already null-check
    'function'. Therefore, null-checking 'function' in hrtimer_start_range_ns()
    is not necessary.
    
    Signed-off-by: Nam Cao <namcao@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/all/4661c571ee87980c340ccc318fc1a473c0c8f6bc.1738746927.git.namcao@linutronix.de

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -1316,8 +1316,6 @@ void hrtimer_start_range_ns(struct hrtimer *timer, ktime_t tim,
 	struct hrtimer_clock_base *base;
 	unsigned long flags;
 
-	if (WARN_ON_ONCE(!ACCESS_PRIVATE(timer, function)))
-		return;
 	/*
 	 * Check whether the HRTIMER_MODE_SOFT bit and hrtimer.is_soft
 	 * match on CONFIG_PREEMPT_RT = n. With PREEMPT_RT check the hard
