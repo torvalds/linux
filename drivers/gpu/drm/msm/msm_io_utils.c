58ac3c93306ec588a6dbb8d3e02ac1109b32df6b
Nam Cao <namcao@linutronix.de>
2025-02-05 11:46:25 +0100

    drm/msm: Switch to use hrtimer_setup()
    hrtimer_setup() takes the callback function pointer as argument and
    initializes the timer completely.
    
    Replace hrtimer_init() and the open coded initialization of
    hrtimer::function with the new setup mechanism.
    
    Patch was created by using Coccinelle.
    
    Signed-off-by: Nam Cao <namcao@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Zack Rusin <zack.rusin@broadcom.com>
    Link: https://lore.kernel.org/all/2689e1ad4105f415ce8cd9e426873d9ac479dc36.1738746904.git.namcao@linutronix.de

--- drivers/gpu/drm/msm/msm_io_utils.c
+++ drivers/gpu/drm/msm/msm_io_utils.c
@@ -135,8 +135,7 @@ void msm_hrtimer_work_init(struct msm_hrtimer_work *work,
 			   clockid_t clock_id,
 			   enum hrtimer_mode mode)
 {
-	hrtimer_init(&work->timer, clock_id, mode);
-	work->timer.function = msm_hrtimer_worktimer;
+	hrtimer_setup(&work->timer, msm_hrtimer_worktimer, clock_id, mode);
 	work->worker = worker;
 	kthread_init_work(&work->work, fn);
 }
