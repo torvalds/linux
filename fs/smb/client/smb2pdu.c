773dc23ff81838b6f74d7fabba5a441cc6a93982
Paulo Alcantara <pc@manguebit.com>
2025-02-05 13:22:11 -0300

    smb: client: fix noisy when tree connecting to DFS interlink targets
    When the client attempts to tree connect to a domain-based DFS
    namespace from a DFS interlink target, the server will return
    STATUS_BAD_NETWORK_NAME and the following will appear on dmesg:
    
    	CIFS: VFS:  BAD_NETWORK_NAME: \\dom\dfs
    
    Since a DFS share might contain several DFS interlinks and they expire
    after 10 minutes, the above message might end up being flooded on
    dmesg when mounting or accessing them.
    
    Print this only once per share.
    
    Signed-off-by: Paulo Alcantara (Red Hat) <pc@manguebit.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/smb/client/smb2pdu.c
+++ fs/smb/client/smb2pdu.c
@@ -2169,7 +2169,7 @@ tcon_exit:
 
 tcon_error_exit:
 	if (rsp && rsp->hdr.Status == STATUS_BAD_NETWORK_NAME)
-		cifs_tcon_dbg(VFS, "BAD_NETWORK_NAME: %s\n", tree);
+		cifs_dbg(VFS | ONCE, "BAD_NETWORK_NAME: %s\n", tree);
 	goto tcon_exit;
 }
 
