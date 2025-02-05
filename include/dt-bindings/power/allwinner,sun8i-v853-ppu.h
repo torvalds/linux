0104f4daa339bdd2649ff7277a30ab3b859677bf
Andras Szemzo <szemzo.andras@gmail.com>
2025-02-05 13:52:20 +0100

    dt-bindings: power: add V853 ppu bindings
    Document V853 PPU compatible.
    
    Signed-off-by: Andras Szemzo <szemzo.andras@gmail.com>
    Reviewed-by: Rob Herring (Arm) <robh@kernel.org>
    Link: https://lore.kernel.org/r/20250205125225.1152849-6-szemzo.andras@gmail.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

---
+++ include/dt-bindings/power/allwinner,sun8i-v853-ppu.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+
+#ifndef _DT_BINDINGS_POWER_SUN8I_V853_PPU_H_
+#define _DT_BINDINGS_POWER_SUN8I_V853_PPU_H_
+
+#define PD_RISCV		0
+#define PD_NPU			1
+#define PD_VE			2
+
+#endif
