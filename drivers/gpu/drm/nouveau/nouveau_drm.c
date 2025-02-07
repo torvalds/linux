b1ca384772b657df433acf0c36f0771d5ebe1138
Ben Skeggs <bskeggs@nvidia.com>
2025-02-07 14:16:11 +1000

    drm/nouveau/gv100-: switch to volta semaphore methods
    HOPPER_CHANNEL_GPFIFO_A removes the SEMAPHORE[A-D] methods that are
    currently used by nouveau to implement fences on GF100 and newer.
    
    Switch to the newer SEM methods available from VOLTA_CHANNEL_GPFIFO,
    which are also available on the Hopper/Blackwell host classes.
    
    Signed-off-by: Ben Skeggs <bskeggs@nvidia.com>
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Reviewed-by: Timur Tabi <ttabi@nvidia.com>
    Tested-by: Timur Tabi <ttabi@nvidia.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/nouveau/nouveau_drm.c
+++ drivers/gpu/drm/nouveau/nouveau_drm.c
@@ -503,11 +503,13 @@ nouveau_accel_init(struct nouveau_drm *drm)
 		case KEPLER_CHANNEL_GPFIFO_B:
 		case MAXWELL_CHANNEL_GPFIFO_A:
 		case PASCAL_CHANNEL_GPFIFO_A:
+			ret = nvc0_fence_create(drm);
+			break;
 		case VOLTA_CHANNEL_GPFIFO_A:
 		case TURING_CHANNEL_GPFIFO_A:
 		case AMPERE_CHANNEL_GPFIFO_A:
 		case AMPERE_CHANNEL_GPFIFO_B:
-			ret = nvc0_fence_create(drm);
+			ret = gv100_fence_create(drm);
 			break;
 		default:
 			break;
