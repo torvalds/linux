fc2ef5b6e1ab9717b1a0b588a39b410864091fa9
luoqing <luoqing@kylinos.cn>
2025-02-08 09:35:39 +0800

    selftests: i915: Use struct_size() helper in kmalloc()
    Make use of the struct_size() helper instead of an open-coded version,
    in order to avoid any potential type mistakes or integer overflows that,
    in the worst scenario, could lead to heap overflows.
    
    Signed-off-by: luoqing <luoqing@kylinos.cn>
    Reviewed-by: Andi Shyti <andi.shyti@linux.intel.com>
    Signed-off-by: Andi Shyti <andi.shyti@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20250208013539.3586855-1-l1138897701@163.com

--- drivers/gpu/drm/i915/gem/selftests/mock_dmabuf.c
+++ drivers/gpu/drm/i915/gem/selftests/mock_dmabuf.c
@@ -103,8 +103,7 @@ static struct dma_buf *mock_dmabuf(int npages)
 	struct dma_buf *dmabuf;
 	int i;
 
-	mock = kmalloc(sizeof(*mock) + npages * sizeof(struct page *),
-		       GFP_KERNEL);
+	mock = kmalloc(struct_size(mock, pages, npages), GFP_KERNEL);
 	if (!mock)
 		return ERR_PTR(-ENOMEM);
 
