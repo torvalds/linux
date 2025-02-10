84a6fc378471fbeaf48f8604566a5a33a3d63c18
Benjamin Berg <benjamin.berg@intel.com>
2025-02-10 17:09:26 +0100

    um: remove copy_from_kernel_nofault_allowed
    There is no need to override the default version of this function
    anymore as UML now has proper _nofault memory access functions.
    
    Doing this also fixes the fact that the implementation was incorrect as
    using mincore() will incorrectly flag pages as inaccessible if they were
    swapped out by the host.
    
    Fixes: f75b1b1bedfb ("um: Implement probe_kernel_read()")
    Signed-off-by: Benjamin Berg <benjamin.berg@intel.com>
    Link: https://patch.msgid.link/20250210160926.420133-3-benjamin@sipsolutions.net
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- arch/um/include/shared/os.h
+++ arch/um/include/shared/os.h
@@ -213,7 +213,6 @@ extern int os_protect_memory(void *addr, unsigned long len,
 extern int os_unmap_memory(void *addr, int len);
 extern int os_drop_memory(void *addr, int length);
 extern int can_drop_memory(void);
-extern int os_mincore(void *addr, unsigned long len);
 
 void os_set_pdeathsig(void);
 
