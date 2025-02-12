1e9f7d9169c55c06e84cdd33bc1107e873910d94
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2025-02-12 21:01:37 +0100

    clk: qcom: Add missing header includes
    Include mod_devicetable.h for the 'struct of_device_id' and
    clk-provider.h for the 'struct clk_hw'.
    
    Reviewed-by: Taniya Das <quic_tdas@quicinc.com>
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Link: https://lore.kernel.org/r/20250212-b4-clk-qcom-clean-v3-3-499f37444f5d@linaro.org
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>

--- drivers/clk/qcom/gpucc-sm8350.c
+++ drivers/clk/qcom/gpucc-sm8350.c
@@ -8,6 +8,7 @@
 #include <linux/clk.h>
 #include <linux/err.h>
 #include <linux/kernel.h>
+#include <linux/mod_devicetable.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/regmap.h>
--- drivers/clk/qcom/mmcc-msm8998.c
+++ drivers/clk/qcom/mmcc-msm8998.c
@@ -7,6 +7,7 @@
 #include <linux/bitops.h>
 #include <linux/err.h>
 #include <linux/platform_device.h>
+#include <linux/mod_devicetable.h>
 #include <linux/module.h>
 #include <linux/clk-provider.h>
 #include <linux/regmap.h>
