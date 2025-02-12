2e2006c91c842c551521434466f9b4324719c9a7
Chuyi Zhou <zhouchuyi@bytedance.com>
2025-02-12 15:19:36 +0800

    sched_ext: Fix the incorrect bpf_list kfunc API in common.bpf.h.
    Now BPF only supports bpf_list_push_{front,back}_impl kfunc, not bpf_list_
    push_{front,back}.
    
    This patch fix this issue. Without this patch, if we use bpf_list kfunc
    in scx, the BPF verifier would complain:
    
    libbpf: extern (func ksym) 'bpf_list_push_back': not found in kernel or
    module BTFs
    libbpf: failed to load object 'scx_foo'
    libbpf: failed to load BPF skeleton 'scx_foo': -EINVAL
    
    With this patch, the bpf list kfunc will work as expected.
    
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>
    Fixes: 2a52ca7c98960 ("sched_ext: Add scx_simple and scx_example_qmap example schedulers")
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- tools/sched_ext/include/scx/common.bpf.h
+++ tools/sched_ext/include/scx/common.bpf.h
@@ -270,8 +270,16 @@ void bpf_obj_drop_impl(void *kptr, void *meta) __ksym;
 #define bpf_obj_new(type) ((type *)bpf_obj_new_impl(bpf_core_type_id_local(type), NULL))
 #define bpf_obj_drop(kptr) bpf_obj_drop_impl(kptr, NULL)
 
-void bpf_list_push_front(struct bpf_list_head *head, struct bpf_list_node *node) __ksym;
-void bpf_list_push_back(struct bpf_list_head *head, struct bpf_list_node *node) __ksym;
+int bpf_list_push_front_impl(struct bpf_list_head *head,
+				    struct bpf_list_node *node,
+				    void *meta, __u64 off) __ksym;
+#define bpf_list_push_front(head, node) bpf_list_push_front_impl(head, node, NULL, 0)
+
+int bpf_list_push_back_impl(struct bpf_list_head *head,
+				   struct bpf_list_node *node,
+				   void *meta, __u64 off) __ksym;
+#define bpf_list_push_back(head, node) bpf_list_push_back_impl(head, node, NULL, 0)
+
 struct bpf_list_node *bpf_list_pop_front(struct bpf_list_head *head) __ksym;
 struct bpf_list_node *bpf_list_pop_back(struct bpf_list_head *head) __ksym;
 struct bpf_rb_node *bpf_rbtree_remove(struct bpf_rb_root *root,
