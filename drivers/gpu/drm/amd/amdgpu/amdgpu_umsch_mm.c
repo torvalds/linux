56763be4009f3be178e534f9b3c10594abec5b6e
Alex Deucher <alexander.deucher@amd.com>
2025-02-12 16:43:13 -0500

    drm/amdgpu/umsch: tidy up the ucode name string handling
    Make the constant parts of the name part of the string
    we pass to amdgpu_ucode_request().  Only the version
    number varies from IP to IP.
    
    Reviewed-by: Saleemkhan Jamadar <saleemkhan.jamadar@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: Lang Yu <Lang.Yu@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_umsch_mm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_umsch_mm.c
@@ -126,14 +126,14 @@ int amdgpu_umsch_mm_init_microcode(struct amdgpu_umsch_mm *umsch)
 	switch (amdgpu_ip_version(adev, VCN_HWIP, 0)) {
 	case IP_VERSION(4, 0, 5):
 	case IP_VERSION(4, 0, 6):
-		fw_name = "amdgpu/umsch_mm_4_0_0.bin";
+		fw_name = "4_0_0";
 		break;
 	default:
 		return -EINVAL;
 	}
 
 	r = amdgpu_ucode_request(adev, &adev->umsch_mm.fw, AMDGPU_UCODE_REQUIRED,
-				 "%s", fw_name);
+				 "amdgpu/umsch_mm_%s.bin", fw_name);
 	if (r) {
 		release_firmware(adev->umsch_mm.fw);
 		adev->umsch_mm.fw = NULL;
