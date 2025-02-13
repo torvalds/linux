60b8f711143de7cd9c0f55be0fe7eb94b19eb5c7
Steven Rostedt <rostedt@goodmis.org>
2025-02-13 13:41:32 -0500

    tracing: Have the error of __tracing_resize_ring_buffer() passed to user
    Currently if __tracing_resize_ring_buffer() returns an error, the
    tracing_resize_ringbuffer() returns -ENOMEM. But it may not be a memory
    issue that caused the function to fail. If the ring buffer is memory
    mapped, then the resizing of the ring buffer will be disabled. But if the
    user tries to resize the buffer, it will get an -ENOMEM returned, which is
    confusing because there is plenty of memory. The actual error returned was
    -EBUSY, which would make much more sense to the user.
    
    Cc: stable@vger.kernel.org
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Vincent Donnefort <vdonnefort@google.com>
    Link: https://lore.kernel.org/20250213134132.7e4505d7@gandalf.local.home
    Fixes: 117c39200d9d7 ("ring-buffer: Introducing ring-buffer mapping functions")
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Reviewed-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -5977,8 +5977,6 @@ static int __tracing_resize_ring_buffer(struct trace_array *tr,
 ssize_t tracing_resize_ring_buffer(struct trace_array *tr,
 				  unsigned long size, int cpu_id)
 {
-	int ret;
-
 	guard(mutex)(&trace_types_lock);
 
 	if (cpu_id != RING_BUFFER_ALL_CPUS) {
@@ -5987,11 +5985,7 @@ ssize_t tracing_resize_ring_buffer(struct trace_array *tr,
 			return -EINVAL;
 	}
 
-	ret = __tracing_resize_ring_buffer(tr, size, cpu_id);
-	if (ret < 0)
-		ret = -ENOMEM;
-
-	return ret;
+	return __tracing_resize_ring_buffer(tr, size, cpu_id);
 }
 
 static void update_last_data(struct trace_array *tr)
