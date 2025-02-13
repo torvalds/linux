3dd98c5c442358c5aefa13e2c91ee9dee32d776e
Suren Baghdasaryan <surenb@google.com>
2025-02-13 14:46:51 -0800

    mm/debug: print vm_refcnt state when dumping the vma
    vm_refcnt encodes a number of useful states:
    - whether vma is attached or detached
    - the number of current vma readers
    - presence of a vma writer
    Let's include it in the vma dump.
    
    Link: https://lkml.kernel.org/r/20250213224655.1680278-15-surenb@google.com
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Tested-by: Shivank Garg <shivankg@amd.com>
      Link: https://lkml.kernel.org/r/5e19ec93-8307-47c2-bb13-3ddf7150624e@amd.com
    Cc: Christian Brauner <brauner@kernel.org>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: David Howells <dhowells@redhat.com>
    Cc: Davidlohr Bueso <dave@stgolabs.net>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Klara Modin <klarasmodin@gmail.com>
    Cc: Liam R. Howlett <Liam.Howlett@Oracle.com>
    Cc: Lokesh Gidra <lokeshgidra@google.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Mateusz Guzik <mjguzik@gmail.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Minchan Kim <minchan@google.com>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Pasha Tatashin <pasha.tatashin@soleen.com>
    Cc: "Paul E . McKenney" <paulmck@kernel.org>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Shakeel Butt <shakeel.butt@linux.dev>
    Cc: Sourav Panda <souravpanda@google.com>
    Cc: Wei Yang <richard.weiyang@gmail.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Heiko Carstens <hca@linux.ibm.com>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/debug.c
+++ mm/debug.c
@@ -181,11 +181,17 @@ void dump_vma(const struct vm_area_struct *vma)
 	pr_emerg("vma %px start %px end %px mm %px\n"
 		"prot %lx anon_vma %px vm_ops %px\n"
 		"pgoff %lx file %px private_data %px\n"
+#ifdef CONFIG_PER_VMA_LOCK
+		"refcnt %x\n"
+#endif
 		"flags: %#lx(%pGv)\n",
 		vma, (void *)vma->vm_start, (void *)vma->vm_end, vma->vm_mm,
 		(unsigned long)pgprot_val(vma->vm_page_prot),
 		vma->anon_vma, vma->vm_ops, vma->vm_pgoff,
 		vma->vm_file, vma->vm_private_data,
+#ifdef CONFIG_PER_VMA_LOCK
+		refcount_read(&vma->vm_refcnt),
+#endif
 		vma->vm_flags, &vma->vm_flags);
 }
 EXPORT_SYMBOL(dump_vma);
