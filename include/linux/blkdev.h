a6aa36e957a1bfb5341986dec32d013d23228fe1
Damien Le Moal <dlemoal@kernel.org>
2025-02-14 13:14:34 +0900

    block: Remove zone write plugs when handling native zone append writes
    For devices that natively support zone append operations,
    REQ_OP_ZONE_APPEND BIOs are not processed through zone write plugging
    and are immediately issued to the zoned device. This means that there is
    no write pointer offset tracking done for these operations and that a
    zone write plug is not necessary.
    
    However, when receiving a zone append BIO, we may already have a zone
    write plug for the target zone if that zone was previously partially
    written using regular write operations. In such case, since the write
    pointer offset of the zone write plug is not incremented by the amount
    of sectors appended to the zone, 2 issues arise:
    1) we risk leaving the plug in the disk hash table if the zone is fully
       written using zone append or regular write operations, because the
       write pointer offset will never reach the "zone full" state.
    2) Regular write operations that are issued after zone append operations
       will always be failed by blk_zone_wplug_prepare_bio() as the write
       pointer alignment check will fail, even if the user correctly
       accounted for the zone append operations and issued the regular
       writes with a correct sector.
    
    Avoid these issues by immediately removing the zone write plug of zones
    that are the target of zone append operations when blk_zone_plug_bio()
    is called. The new function blk_zone_wplug_handle_native_zone_append()
    implements this for devices that natively support zone append. The
    removal of the zone write plug using disk_remove_zone_wplug() requires
    aborting all plugged regular write using disk_zone_wplug_abort() as
    otherwise the plugged write BIOs would never be executed (with the plug
    removed, the completion path will never see again the zone write plug as
    disk_get_zone_wplug() will return NULL). Rate-limited warnings are added
    to blk_zone_wplug_handle_native_zone_append() and to
    disk_zone_wplug_abort() to signal this.
    
    Since blk_zone_wplug_handle_native_zone_append() is called in the hot
    path for operations that will not be plugged, disk_get_zone_wplug() is
    optimized under the assumption that a user issuing zone append
    operations is not at the same time issuing regular writes and that there
    are no hashed zone write plugs. The struct gendisk atomic counter
    nr_zone_wplugs is added to check this, with this counter incremented in
    disk_insert_zone_wplug() and decremented in disk_remove_zone_wplug().
    
    To be consistent with this fix, we do not need to fill the zone write
    plug hash table with zone write plugs for zones that are partially
    written for a device that supports native zone append operations.
    So modify blk_revalidate_seq_zone() to return early to avoid allocating
    and inserting a zone write plug for partially written sequential zones
    if the device natively supports zone append.
    
    Reported-by: Jorgen Hansen <Jorgen.Hansen@wdc.com>
    Fixes: 9b1ce7f0c6f8 ("block: Implement zone append emulation")
    Cc: stable@vger.kernel.org
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>
    Tested-by: Jorgen Hansen <Jorgen.Hansen@wdc.com>
    Link: https://lore.kernel.org/r/20250214041434.82564-1-dlemoal@kernel.org
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -196,10 +196,11 @@ struct gendisk {
 	unsigned int		zone_capacity;
 	unsigned int		last_zone_capacity;
 	unsigned long __rcu	*conv_zones_bitmap;
-	unsigned int            zone_wplugs_hash_bits;
-	spinlock_t              zone_wplugs_lock;
+	unsigned int		zone_wplugs_hash_bits;
+	atomic_t		nr_zone_wplugs;
+	spinlock_t		zone_wplugs_lock;
 	struct mempool_s	*zone_wplugs_pool;
-	struct hlist_head       *zone_wplugs_hash;
+	struct hlist_head	*zone_wplugs_hash;
 	struct workqueue_struct *zone_wplugs_wq;
 #endif /* CONFIG_BLK_DEV_ZONED */
 
