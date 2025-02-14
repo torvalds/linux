b38c72ab808c5657b5411a47cc6eb4912e8b4824
Jiayuan Chen <mrpre@163.com>
2025-02-14 17:18:22 +0800

    selftests/bpf: Introduce __load_if_JITed annotation for tests
    In some cases, the verification logic under the interpreter and JIT
    differs, such as may_goto, and the test program behaves differently under
    different runtime modes, requiring separate verification logic for each
    result.
    
    Introduce __load_if_JITed and __load_if_no_JITed annotation for tests.
    
    Signed-off-by: Jiayuan Chen <mrpre@163.com>
    Link: https://lore.kernel.org/r/20250214091823.46042-3-mrpre@163.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/testing/selftests/bpf/progs/bpf_misc.h
+++ tools/testing/selftests/bpf/progs/bpf_misc.h
@@ -135,6 +135,8 @@
 #define __arch_arm64		__arch("ARM64")
 #define __arch_riscv64		__arch("RISCV64")
 #define __caps_unpriv(caps)	__attribute__((btf_decl_tag("comment:test_caps_unpriv=" EXPAND_QUOTE(caps))))
+#define __load_if_JITed()	__attribute__((btf_decl_tag("comment:load_mode=jited")))
+#define __load_if_no_JITed()	__attribute__((btf_decl_tag("comment:load_mode=no_jited")))
 
 /* Define common capabilities tested using __caps_unpriv */
 #define CAP_NET_ADMIN		12
