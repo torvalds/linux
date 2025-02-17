e76750f50aaae66817ffc92417a0ac11c57dede6
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2025-02-17 14:16:13 +0000

    iio: adc: ad7768-1: Switch to sparse friendly iio_device_claim/release_direct()
    These new functions allow sparse to find failures to release
    direct mode reducing chances of bugs over the claim_direct_mode()
    functions that are deprecated.
    
    Reviewed-by: Nuno Sá <nuno.sa@analog.com>
    Link: https://patch.msgid.link/20250217141630.897334-14-jic23@kernel.org
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/adc/ad7768-1.c
+++ drivers/iio/adc/ad7768-1.c
@@ -365,13 +365,12 @@ static int ad7768_read_raw(struct iio_dev *indio_dev,
 
 	switch (info) {
 	case IIO_CHAN_INFO_RAW:
-		ret = iio_device_claim_direct_mode(indio_dev);
-		if (ret)
-			return ret;
+		if (!iio_device_claim_direct(indio_dev))
+			return -EBUSY;
 
 		ret = ad7768_scan_direct(indio_dev);
 
-		iio_device_release_direct_mode(indio_dev);
+		iio_device_release_direct(indio_dev);
 		if (ret < 0)
 			return ret;
 		*val = ret;
