5f7a3ab50706c14714bc97847b57fb73294a8009
Michael Aaron Murphy <michael@mmurphy.dev>
2025-02-17 13:48:54 +0100

    disable enable_virt_at_load to fix VirtualBox

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -5563,7 +5563,7 @@ static struct miscdevice kvm_dev = {
 };
 
 #ifdef CONFIG_KVM_GENERIC_HARDWARE_ENABLING
-static bool enable_virt_at_load = true;
+static bool enable_virt_at_load = false;
 module_param(enable_virt_at_load, bool, 0444);
 
 __visible bool kvm_rebooting;
