d40f4b15c07782699b0764509225156c202fd482
Dr. David Alan Gilbert <linux@treblig.org>
2025-02-20 16:55:16 +0000

    media: cx23885: Remove unused netup_eeprom_write
    netup_eeprom_write() was added in 2009 by
    commit b45c0551f94d ("V4L/DVB (10797): Add EEPROM code for NetUP Dual
    DVB-S2 CI card.")
    but has remained unused.
    
    Remove it.
    
    Signed-off-by: Dr. David Alan Gilbert <linux@treblig.org>
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>

--- drivers/media/pci/cx23885/netup-eeprom.h
+++ drivers/media/pci/cx23885/netup-eeprom.h
@@ -21,7 +21,6 @@ struct netup_card_info {
 };
 
 extern int netup_eeprom_read(struct i2c_adapter *i2c_adap, u8 addr);
-extern int netup_eeprom_write(struct i2c_adapter *i2c_adap, u8 addr, u8 data);
 extern void netup_get_card_info(struct i2c_adapter *i2c_adap,
 				struct netup_card_info *cinfo);
 
