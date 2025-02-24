4de14ec76b5e67d824896f774b3a23d86a2ebc87
Tudor Ambarus <tudor.ambarus@linaro.org>
2025-02-24 08:27:17 +0000

    mailbox: remove unused header files
    There's nothing used from these header files, remove their inclusion.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@linaro.org>
    Signed-off-by: Jassi Brar <jassisinghbrar@gmail.com>

--- drivers/mailbox/mailbox.c
+++ drivers/mailbox/mailbox.c
@@ -6,17 +6,14 @@
  * Author: Jassi Brar <jassisinghbrar@gmail.com>
  */
 
-#include <linux/bitops.h>
 #include <linux/delay.h>
 #include <linux/device.h>
 #include <linux/err.h>
-#include <linux/interrupt.h>
 #include <linux/mailbox_client.h>
 #include <linux/mailbox_controller.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/of.h>
-#include <linux/slab.h>
 #include <linux/spinlock.h>
 
 #include "mailbox.h"
