72e1c440c848624ad4cfac93d69d8a999a20355b
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2025-02-24 14:36:25 +0200

    auxdisplay: panel: Fix an API misuse in panel.c
    Variable allocated by charlcd_alloc() should be released
    by charlcd_free(). The following patch changed kfree() to
    charlcd_free() to fix an API misuse.
    
    Reviewed-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Fixes: 718e05ed92ec ("auxdisplay: Introduce hd44780_common.[ch]")
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/auxdisplay/panel.c
+++ drivers/auxdisplay/panel.c
@@ -1664,7 +1664,7 @@ err_lcd_unreg:
 	if (lcd.enabled)
 		charlcd_unregister(lcd.charlcd);
 err_unreg_device:
-	kfree(lcd.charlcd);
+	charlcd_free(lcd.charlcd);
 	lcd.charlcd = NULL;
 	parport_unregister_device(pprt);
 	pprt = NULL;
@@ -1692,7 +1692,7 @@ static void panel_detach(struct parport *port)
 		charlcd_unregister(lcd.charlcd);
 		lcd.initialized = false;
 		kfree(lcd.charlcd->drvdata);
-		kfree(lcd.charlcd);
+		charlcd_free(lcd.charlcd);
 		lcd.charlcd = NULL;
 	}
 
