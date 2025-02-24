2294059118c550464dd8906286324d90c33b152b
Stanimir Varbanov <svarbanov@suse.de>
2025-02-24 10:35:56 +0200

    PCI: brcmstb: Add a softdep to MIP MSI-X driver
    Then the brcmstb PCIe driver and MIP MSI-X interrupt controller
    drivers are built as modules there could be a race in probing.
    
    To avoid this, add a softdep to MIP driver to guarantee that
    MIP driver will be load first.
    
    Signed-off-by: Stanimir Varbanov <svarbanov@suse.de>
    Reviewed-by: Florian Fainelli <florian.fainelli@broadcom.com>
    Tested-by: Ivan T. Ivanov <iivanov@suse.de>
    Link: https://lore.kernel.org/r/20250224083559.47645-5-svarbanov@suse.de
    [kwilczynski: commit log]
    Signed-off-by: Krzysztof Wilczyński <kwilczynski@kernel.org>

--- drivers/pci/controller/pcie-brcmstb.c
+++ drivers/pci/controller/pcie-brcmstb.c
@@ -1945,3 +1945,4 @@ module_platform_driver(brcm_pcie_driver);
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Broadcom STB PCIe RC driver");
 MODULE_AUTHOR("Broadcom");
+MODULE_SOFTDEP("pre: irq_bcm2712_mip");
