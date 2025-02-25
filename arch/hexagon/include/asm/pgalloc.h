e3ecf7c7d0829a00a4fb02531338ab9e7e75ea0d
Qi Zheng <zhengqi.arch@bytedance.com>
2025-02-25 11:45:53 +0800

    mm: pgtable: convert some architectures to use tlb_remove_ptdesc()
    Now, the nine architectures of csky, hexagon, loongarch, m68k, mips,
    nios2, openrisc, sh and um do not select CONFIG_MMU_GATHER_RCU_TABLE_FREE,
    and just call pagetable_dtor() + tlb_remove_page_ptdesc() (the wrapper of
    tlb_remove_page()).  This is the same as the implementation of
    tlb_remove_{ptdesc|table}() under !CONFIG_MMU_GATHER_TABLE_FREE, so
    convert these architectures to use tlb_remove_ptdesc().
    
    The ultimate goal is to make the architecture only use tlb_remove_ptdesc()
    or tlb_remove_table() for page table pages.
    
    [zhengqi.arch@bytedance.com: v2]
      Link: https://lkml.kernel.org/r/20250303072603.45423-1-zhengqi.arch@bytedance.com
    [akpm@linux-foundation.org: remove trailing semi in arch/loongarch/include/asm/pgalloc.h]
    Link: https://lkml.kernel.org/r/19db3e8673b67bad2f1df1ab37f1c89d99eacfea.1740454179.git.zhengqi.arch@bytedance.com
    Signed-off-by: Qi Zheng <zhengqi.arch@bytedance.com>
    Suggested-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Kevin Brodsky <kevin.brodsky@arm.com>
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>	[m68k]
    Cc: Alexandre Ghiti <alexghiti@rivosinc.com>
    Cc: "Aneesh Kumar K.V" <aneesh.kumar@kernel.org>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Hugh Dickens <hughd@google.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Matthew Wilcow (Oracle) <willy@infradead.org>
    Cc: "Mike Rapoport (IBM)" <rppt@kernel.org>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: Vishal Moola (Oracle) <vishal.moola@gmail.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Yu Zhao <yuzhao@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/hexagon/include/asm/pgalloc.h
+++ arch/hexagon/include/asm/pgalloc.h
@@ -87,10 +87,7 @@ static inline void pmd_populate_kernel(struct mm_struct *mm, pmd_t *pmd,
 		max_kernel_seg = pmdindex;
 }
 
-#define __pte_free_tlb(tlb, pte, addr)				\
-do {								\
-	pagetable_dtor((page_ptdesc(pte)));			\
-	tlb_remove_page_ptdesc((tlb), (page_ptdesc(pte)));	\
-} while (0)
+#define __pte_free_tlb(tlb, pte, addr)	\
+	tlb_remove_ptdesc((tlb), page_ptdesc(pte))
 
 #endif
