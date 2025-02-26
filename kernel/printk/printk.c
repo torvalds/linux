72c96a2dacc0fb056d13a5f02b0845c4c910fe54
Marcos Paulo de Souza <mpdesouza@suse.com>
2025-02-26 16:59:05 -0300

    printk: Check CON_SUSPEND when unblanking a console
    The commit 9e70a5e109a4 ("printk: Add per-console suspended state")
    introduced the CON_SUSPENDED flag for consoles. The suspended consoles
    will stop receiving messages, so don't unblank suspended consoles
    because it won't be showing anything either way.
    
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>
    Reviewed-by: Petr Mladek <pmladek@suse.com>
    Reviewed-by: John Ogness <john.ogness@linutronix.de>
    Link: https://lore.kernel.org/r/20250226-printk-renaming-v1-5-0b878577f2e6@suse.com
    Signed-off-by: Petr Mladek <pmladek@suse.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -3342,7 +3342,12 @@ void console_unblank(void)
 	 */
 	cookie = console_srcu_read_lock();
 	for_each_console_srcu(c) {
-		if ((console_srcu_read_flags(c) & CON_ENABLED) && c->unblank) {
+		short flags = console_srcu_read_flags(c);
+
+		if (flags & CON_SUSPENDED)
+			continue;
+
+		if ((flags & CON_ENABLED) && c->unblank) {
 			found_unblank = true;
 			break;
 		}
@@ -3379,7 +3384,12 @@ void console_unblank(void)
 
 	cookie = console_srcu_read_lock();
 	for_each_console_srcu(c) {
-		if ((console_srcu_read_flags(c) & CON_ENABLED) && c->unblank)
+		short flags = console_srcu_read_flags(c);
+
+		if (flags & CON_SUSPENDED)
+			continue;
+
+		if ((flags & CON_ENABLED) && c->unblank)
 			c->unblank();
 	}
 	console_srcu_read_unlock(cookie);
