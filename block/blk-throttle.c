29cb955934302a5da525db6b327c795572538426
Yu Kuai <yukuai3@huawei.com>
2025-02-27 20:06:45 +0800

    blk-throttle: fix lower bps rate by throtl_trim_slice()
    The bio submission time may be a few jiffies more than the expected
    waiting time, due to 'extra_bytes' can't be divided in
    tg_within_bps_limit(), and also due to timer wakeup delay.
    In this case, adjust slice_start to jiffies will discard the extra wait time,
    causing lower rate than expected.
    
    Current in-tree code already covers deviation by rounddown(), but turns
    out it is not enough, because jiffies - slice_start can be a multiple of
    throtl_slice.
    
    For example, assume bps_limit is 1000bytes, 1 jiffes is 10ms, and
    slice is 20ms(2 jiffies), expected rate is 1000 / 1000 * 20 = 20 bytes
    per slice.
    
    If user issues two 21 bytes IO, then wait time will be 30ms for the
    first IO:
    
    bytes_allowed = 20, extra_bytes = 1;
    jiffy_wait = 1 + 2 = 3 jiffies
    
    and consider
    extra 1 jiffies by timer, throtl_trim_slice() will be called at:
    
    jiffies = 40ms
    slice_start = 0ms, slice_end= 40ms
    bytes_disp = 21
    
    In this case, before the patch, real rate in the first two slices is
    10.5 bytes per slice, and slice will be updated to:
    
    jiffies = 40ms
    slice_start = 40ms, slice_end = 60ms,
    bytes_disp = 0;
    
    Hence the second IO will have to wait another 30ms;
    
    With the patch, the real rate in the first slice is 20 bytes per slice,
    which is the same as expected, and slice will be updated:
    
    jiffies=40ms,
    slice_start = 20ms, slice_end = 60ms,
    bytes_disp = 1;
    
    And now, there is still 19 bytes allowed in the second slice, and the
    second IO will only have to wait 10ms;
    
    This problem will cause blktests throtl/001 failure in case of
    CONFIG_HZ_100=y, fix it by preserving one extra finished slice in
    throtl_trim_slice().
    
    Fixes: e43473b7f223 ("blkio: Core implementation of throttle policy")
    Reported-by: Ming Lei <ming.lei@redhat.com>
    Closes: https://lore.kernel.org/linux-block/20250222092823.210318-3-yukuai1@huaweicloud.com/
    Reviewed-by: Ming Lei <ming.lei@redhat.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>
    Link: https://lore.kernel.org/r/20250227120645.812815-1-yukuai1@huaweicloud.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-throttle.c
+++ block/blk-throttle.c
@@ -599,14 +599,23 @@ static inline void throtl_trim_slice(struct throtl_grp *tg, bool rw)
 	 * sooner, then we need to reduce slice_end. A high bogus slice_end
 	 * is bad because it does not allow new slice to start.
 	 */
-
 	throtl_set_slice_end(tg, rw, jiffies + tg->td->throtl_slice);
 
 	time_elapsed = rounddown(jiffies - tg->slice_start[rw],
 				 tg->td->throtl_slice);
-	if (!time_elapsed)
+	/* Don't trim slice until at least 2 slices are used */
+	if (time_elapsed < tg->td->throtl_slice * 2)
 		return;
 
+	/*
+	 * The bio submission time may be a few jiffies more than the expected
+	 * waiting time, due to 'extra_bytes' can't be divided in
+	 * tg_within_bps_limit(), and also due to timer wakeup delay. In this
+	 * case, adjust slice_start will discard the extra wait time, causing
+	 * lower rate than expected. Therefore, other than the above rounddown,
+	 * one extra slice is preserved for deviation.
+	 */
+	time_elapsed -= tg->td->throtl_slice;
 	bytes_trim = calculate_bytes_allowed(tg_bps_limit(tg, rw),
 					     time_elapsed) +
 		     tg->carryover_bytes[rw];
