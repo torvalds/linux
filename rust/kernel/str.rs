5928642b11cb6aee8f6250c762857e713e7112da
Andreas Hindborg <a.hindborg@kernel.org>
2025-02-27 15:38:10 +0100

    rust: str: implement `strip_prefix` for `BStr`
    Implement `strip_prefix` for `BStr` by deferring to `slice::strip_prefix`
    on the underlying `&[u8]`.
    
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Daniel Almeida <daniel.almeida@collabora.com>
    Tested-by: Daniel Almeida <daniel.almeida@collabora.com>
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>
    Tested-by: Daniel Gomez <da.gomez@samsung.com>
    Link: https://lore.kernel.org/r/20250227-module-params-v3-v8-4-ceeee85d9347@kernel.org
    [ Pluralized section name. Hid `use`. - Miguel ]
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- rust/kernel/str.rs
+++ rust/kernel/str.rs
@@ -31,6 +31,23 @@ impl BStr {
         // SAFETY: `BStr` is transparent to `[u8]`.
         unsafe { &*(bytes as *const [u8] as *const BStr) }
     }
+
+    /// Strip a prefix from `self`. Delegates to [`slice::strip_prefix`].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use kernel::b_str;
+    /// assert_eq!(Some(b_str!("bar")), b_str!("foobar").strip_prefix(b_str!("foo")));
+    /// assert_eq!(None, b_str!("foobar").strip_prefix(b_str!("bar")));
+    /// assert_eq!(Some(b_str!("foobar")), b_str!("foobar").strip_prefix(b_str!("")));
+    /// assert_eq!(Some(b_str!("")), b_str!("foobar").strip_prefix(b_str!("foobar")));
+    /// ```
+    pub fn strip_prefix(&self, pattern: impl AsRef<Self>) -> Option<&BStr> {
+        self.deref()
+            .strip_prefix(pattern.as_ref().deref())
+            .map(Self::from_bytes)
+    }
 }
 
 impl fmt::Display for BStr {
