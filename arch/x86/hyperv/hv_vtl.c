ced518ad55b4118dd77f8a455a06801b89f2f877
Roman Kisel <romank@linux.microsoft.com>
2025-02-27 13:47:27 -0800

    x86/hyperv: Add VTL mode emergency restart callback
    By default, X86(-64) systems use the emergecy restart routine
    in the course of which the code unconditionally writes to
    the physical address of 0x472 to indicate the boot mode
    to the firmware (BIOS or UEFI).
    
    When the kernel itself runs as a firmware in the VTL mode,
    that write corrupts the memory of the guest upon emergency
    restarting. Preserving the state intact in that situation
    is important for debugging, at least.
    
    Define the specialized machine callback to avoid that write
    and use the triple fault to perform emergency restart.
    
    Signed-off-by: Roman Kisel <romank@linux.microsoft.com>
    Link: https://lore.kernel.org/r/20250227214728.15672-2-romank@linux.microsoft.com
    Signed-off-by: Wei Liu <wei.liu@kernel.org>
    Message-ID: <20250227214728.15672-2-romank@linux.microsoft.com>

--- arch/x86/hyperv/hv_vtl.c
+++ arch/x86/hyperv/hv_vtl.c
@@ -12,6 +12,7 @@
 #include <asm/i8259.h>
 #include <asm/mshyperv.h>
 #include <asm/realmode.h>
+#include <asm/reboot.h>
 #include <../kernel/smpboot.h>
 
 extern struct boot_params boot_params;
@@ -20,6 +21,27 @@ static struct real_mode_header hv_vtl_real_mode_header;
 static bool __init hv_vtl_msi_ext_dest_id(void)
 {
 	return true;
+}
+
+/*
+ * The `native_machine_emergency_restart` function from `reboot.c` writes
+ * to the physical address 0x472 to indicate the type of reboot for the
+ * firmware. We cannot have that in VSM as the memory composition might
+ * be more generic, and such write effectively corrupts the memory thus
+ * making diagnostics harder at the very least.
+ */
+static void  __noreturn hv_vtl_emergency_restart(void)
+{
+	/*
+	 * Cause a triple fault and the immediate reset. Here the code does not run
+	 * on the top of any firmware, whereby cannot reach out to its services.
+	 * The inifinite loop is for the improbable case that the triple fault does
+	 * not work and have to preserve the state intact for debugging.
+	 */
+	for (;;) {
+		idt_invalidate();
+		__asm__ __volatile__("int3");
+	}
 }
 
 void __init hv_vtl_init_platform(void)
@@ -235,6 +257,7 @@ static int hv_vtl_wakeup_secondary_cpu(u32 apicid, unsigned long start_eip)
 
 int __init hv_vtl_early_init(void)
 {
+	machine_ops.emergency_restart = hv_vtl_emergency_restart;
 	/*
 	 * `boot_cpu_has` returns the runtime feature support,
 	 * and here is the earliest it can be used.
