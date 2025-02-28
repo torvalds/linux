eb7e83a7ca2dba01671c711e1711705e1a15626d
Ian Rogers <irogers@google.com>
2025-02-28 14:23:00 -0800

    perf evsel: tp_format accessing improvements
    Ensure evsel__clone copies the tp_sys and tp_name variables.
    In evsel__tp_format, if tp_sys isn't set, use the config value to find
    the tp_format. This succeeds in python code where pyrf__tracepoint has
    already found the format.
    
    Reviewed-by: Howard Chu <howardchu95@gmail.com>
    Signed-off-by: Ian Rogers <irogers@google.com>
    Reviewed-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Link: https://lore.kernel.org/r/20250228222308.626803-4-irogers@google.com
    Fixes: 6c8310e8380d472c ("perf evsel: Allow evsel__newtp without libtraceevent")
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -521,6 +521,16 @@ struct evsel *evsel__clone(struct evsel *dest, struct evsel *orig)
 	}
 	evsel->cgrp = cgroup__get(orig->cgrp);
 #ifdef HAVE_LIBTRACEEVENT
+	if (orig->tp_sys) {
+		evsel->tp_sys = strdup(orig->tp_sys);
+		if (evsel->tp_sys == NULL)
+			goto out_err;
+	}
+	if (orig->tp_name) {
+		evsel->tp_name = strdup(orig->tp_name);
+		if (evsel->tp_name == NULL)
+			goto out_err;
+	}
 	evsel->tp_format = orig->tp_format;
 #endif
 	evsel->handler = orig->handler;
@@ -644,7 +654,11 @@ struct tep_event *evsel__tp_format(struct evsel *evsel)
 	if (evsel->core.attr.type != PERF_TYPE_TRACEPOINT)
 		return NULL;
 
-	tp_format = trace_event__tp_format(evsel->tp_sys, evsel->tp_name);
+	if (!evsel->tp_sys)
+		tp_format = trace_event__tp_format_id(evsel->core.attr.config);
+	else
+		tp_format = trace_event__tp_format(evsel->tp_sys, evsel->tp_name);
+
 	if (IS_ERR(tp_format)) {
 		int err = -PTR_ERR(evsel->tp_format);
 
