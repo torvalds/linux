cba3b86974a3388b12130654809e50cd19294849
Miguel Ojeda <ojeda@kernel.org>
2025-03-02 00:16:02 +0100

    drm/panic: fix overindented list items in documentation
    Starting with the upcoming Rust 1.86.0 (to be released 2025-04-03),
    Clippy warns:
    
        error: doc list item overindented
           --> drivers/gpu/drm/drm_panic_qr.rs:914:5
            |
        914 | ///    will be encoded as binary segment, otherwise it will be encoded
            |     ^^^ help: try using `  ` (2 spaces)
            |
            = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#doc_overindented_list_items
    
    The overindentation is slightly hard to notice, since all the items
    start with a backquote that makes it look OK, but it is there.
    
    Thus fix it.
    
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>
    Fixes: cb5164ac43d0 ("drm/panic: Add a QR code panic screen")
    Cc: stable@vger.kernel.org # Needed in 6.12.y and 6.13.y only (Rust is pinned in older LTSs).
    Reviewed-by: Jocelyn Falempe <jfalempe@redhat.com>
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Signed-off-by: Jocelyn Falempe <jfalempe@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20250301231602.917580-2-ojeda@kernel.org

--- drivers/gpu/drm/drm_panic_qr.rs
+++ drivers/gpu/drm/drm_panic_qr.rs
@@ -911,16 +911,16 @@ impl QrImage<'_> {
 ///
 /// * `url`: The base URL of the QR code. It will be encoded as Binary segment.
 /// * `data`: A pointer to the binary data, to be encoded. if URL is NULL, it
-///    will be encoded as binary segment, otherwise it will be encoded
-///    efficiently as a numeric segment, and appended to the URL.
+///   will be encoded as binary segment, otherwise it will be encoded
+///   efficiently as a numeric segment, and appended to the URL.
 /// * `data_len`: Length of the data, that needs to be encoded, must be less
-///    than data_size.
+///   than data_size.
 /// * `data_size`: Size of data buffer, it should be at least 4071 bytes to hold
-///    a V40 QR code. It will then be overwritten with the QR code image.
+///   a V40 QR code. It will then be overwritten with the QR code image.
 /// * `tmp`: A temporary buffer that the QR code encoder will use, to write the
-///    segments and ECC.
+///   segments and ECC.
 /// * `tmp_size`: Size of the temporary buffer, it must be at least 3706 bytes
-///    long for V40.
+///   long for V40.
 ///
 /// # Safety
 ///
