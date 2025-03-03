0a22454ab2eca530702b2689858909b608953703
Linus Walleij <linus.walleij@linaro.org>
2025-03-03 09:41:46 +0100

    ASoC: samsung: tm2_wm5110: Drop unused include
    This file does not use the symbols from the legacy
    <linux/gpio.h> header, so let's drop it.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://patch.msgid.link/20250303-samsung-codecs-v1-6-56a2299dd928@linaro.org
    Reviewed-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/samsung/tm2_wm5110.c
+++ sound/soc/samsung/tm2_wm5110.c
@@ -6,7 +6,6 @@
 //          Sylwester Nawrocki <s.nawrocki@samsung.com>
 
 #include <linux/clk.h>
-#include <linux/gpio.h>
 #include <linux/gpio/consumer.h>
 #include <linux/module.h>
 #include <linux/of.h>
