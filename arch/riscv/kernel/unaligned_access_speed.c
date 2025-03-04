5af72a818612332a11171b16f27a62ec0a0f91d7
Andrew Jones <ajones@ventanamicro.com>
2025-03-04 13:00:17 +0100

    riscv: Fix riscv_online_cpu_vec
    We shouldn't probe when we already know vector is unsupported and
    we should probe when we see we don't yet know whether it's supported.
    Furthermore, we should ensure we've set the access type to
    unsupported when we don't have vector at all.
    
    Fixes: e7c9d66e313b ("RISC-V: Report vector unaligned access speed hwprobe")
    Reviewed-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Signed-off-by: Andrew Jones <ajones@ventanamicro.com>
    Link: https://lore.kernel.org/r/20250304120014.143628-12-ajones@ventanamicro.com
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>

--- arch/riscv/kernel/unaligned_access_speed.c
+++ arch/riscv/kernel/unaligned_access_speed.c
@@ -367,10 +367,12 @@ static void check_vector_unaligned_access(struct work_struct *work __always_unus
 
 static int riscv_online_cpu_vec(unsigned int cpu)
 {
-	if (!has_vector())
+	if (!has_vector()) {
+		per_cpu(vector_misaligned_access, cpu) = RISCV_HWPROBE_MISALIGNED_VECTOR_UNSUPPORTED;
 		return 0;
+	}
 
-	if (per_cpu(vector_misaligned_access, cpu) != RISCV_HWPROBE_MISALIGNED_VECTOR_UNSUPPORTED)
+	if (per_cpu(vector_misaligned_access, cpu) != RISCV_HWPROBE_MISALIGNED_VECTOR_UNKNOWN)
 		return 0;
 
 	check_vector_unaligned_access_emulated(NULL);
