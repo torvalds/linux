2ea8f6a1cda773bdc7dd6aec458217ac69b6fcd9
Eduard Zingerman <eddyz87@gmail.com>
2025-03-04 11:50:24 -0800

    selftests/bpf: test cases for compute_live_registers()
    Cover instructions from each kind:
    - assignment
    - arithmetic
    - store/load
    - endian conversion
    - atomics
    - branches, conditional branches, may_goto, calls
    - LD_ABS/LD_IND
    - address_space_cast
    
    Signed-off-by: Eduard Zingerman <eddyz87@gmail.com>
    Link: https://lore.kernel.org/r/20250304195024.2478889-6-eddyz87@gmail.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

---
+++ tools/testing/selftests/bpf/prog_tests/compute_live_registers.c
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include "compute_live_registers.skel.h"
+#include "test_progs.h"
+
+void test_compute_live_registers(void)
+{
+	RUN_TESTS(compute_live_registers);
+}
