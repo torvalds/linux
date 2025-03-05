2fff55d499eea7c61a2671c5d7394e9c62995d9f
Jani Nikula <jani.nikula@intel.com>
2025-03-05 18:38:22 +0200

    drm/xe/compat: refactor compat i915_drv.h
    The compat i915_drv.h contains things that aren't there in the original
    i915_drv.h. Split out gem/i915_gem_object.h and i915_scheduler_types.h,
    moving the corresponding pieces out, including FORCEWAKE_ALL to
    intel_uncore.h.
    
    Technically I915_PRIORITY_DISPLAY should be in i915_priolist_types.h,
    but it's a bit overkill to split out another file just for
    that. i915_scheduler_types.h shall do.
    
    With this, the compat i915_drv.h becomes a strict subset of the
    original.
    
    Reviewed-by: Nemesa Garg <nemesa.garg@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/d6bd95bf52aa37f48ddec3e675b7a3cc66829eef.1741192597.git.jani.nikula@intel.com
    [Jani: fix i915_gem_object.h header guard while applying]
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/display/intel_atomic_plane.c
+++ drivers/gpu/drm/i915/display/intel_atomic_plane.c
@@ -41,8 +41,10 @@
 #include <drm/drm_gem.h>
 #include <drm/drm_gem_atomic_helper.h>
 
+#include "gem/i915_gem_object.h"
 #include "i915_config.h"
 #include "i915_drv.h"
+#include "i915_scheduler_types.h"
 #include "i915_vma.h"
 #include "i9xx_plane_regs.h"
 #include "intel_atomic_plane.h"
