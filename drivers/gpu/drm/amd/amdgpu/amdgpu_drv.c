68bfdc8dc0a1a7fdd9ab61e69907ae71a6fd3d91
Mario Limonciello <mario.limonciello@amd.com>
2025-03-06 12:51:24 -0600

    drm/amd: Keep display off while going into S4
    When userspace invokes S4 the flow is:
    
    1) amdgpu_pmops_prepare()
    2) amdgpu_pmops_freeze()
    3) Create hibernation image
    4) amdgpu_pmops_thaw()
    5) Write out image to disk
    6) Turn off system
    
    Then on resume amdgpu_pmops_restore() is called.
    
    This flow has a problem that because amdgpu_pmops_thaw() is called
    it will call amdgpu_device_resume() which will resume all of the GPU.
    
    This includes turning the display hardware back on and discovering
    connectors again.
    
    This is an unexpected experience for the display to turn back on.
    Adjust the flow so that during the S4 sequence display hardware is
    not turned back on.
    
    Reported-by: Xaver Hugl <xaver.hugl@gmail.com>
    Closes: https://gitlab.freedesktop.org/drm/amd/-/issues/2038
    Cc: Muhammad Usama Anjum <usama.anjum@collabora.com>
    Tested-by: Muhammad Usama Anjum <usama.anjum@collabora.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>
    Link: https://lore.kernel.org/r/20250306185124.44780-1-mario.limonciello@amd.com
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -2563,7 +2563,6 @@ static int amdgpu_pmops_freeze(struct device *dev)
 	int r;
 
 	r = amdgpu_device_suspend(drm_dev, true);
-	adev->in_s4 = false;
 	if (r)
 		return r;
 
@@ -2575,8 +2574,13 @@ static int amdgpu_pmops_freeze(struct device *dev)
 static int amdgpu_pmops_thaw(struct device *dev)
 {
 	struct drm_device *drm_dev = dev_get_drvdata(dev);
+	struct amdgpu_device *adev = drm_to_adev(drm_dev);
+	int r;
 
-	return amdgpu_device_resume(drm_dev, true);
+	r = amdgpu_device_resume(drm_dev, true);
+	adev->in_s4 = false;
+
+	return r;
 }
 
 static int amdgpu_pmops_poweroff(struct device *dev)
@@ -2589,6 +2593,9 @@ static int amdgpu_pmops_poweroff(struct device *dev)
 static int amdgpu_pmops_restore(struct device *dev)
 {
 	struct drm_device *drm_dev = dev_get_drvdata(dev);
+	struct amdgpu_device *adev = drm_to_adev(drm_dev);
+
+	adev->in_s4 = false;
 
 	return amdgpu_device_resume(drm_dev, true);
 }
