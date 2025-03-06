55c85fa7579dc2e3f5399ef5bad67a44257c1a48
Yi Liu <yi.l.liu@intel.com>
2025-03-05 19:48:42 -0800

    iommufd: Fail replace if device has not been attached
    The current implementation of iommufd_device_do_replace() implicitly
    assumes that the input device has already been attached. However, there
    is no explicit check to verify this assumption. If another device within
    the same group has been attached, the replace operation might succeed,
    but the input device itself may not have been attached yet.
    
    As a result, the input device might not be tracked in the
    igroup->device_list, and its reserved IOVA might not be added. Despite
    this, the caller might incorrectly assume that the device has been
    successfully replaced, which could lead to unexpected behavior or errors.
    
    To address this issue, add a check to ensure that the input device has
    been attached before proceeding with the replace operation. This check
    will help maintain the integrity of the device tracking system and prevent
    potential issues arising from incorrect assumptions about the device's
    attachment status.
    
    Fixes: e88d4ec154a8 ("iommufd: Add iommufd_device_replace()")
    Link: https://patch.msgid.link/r/20250306034842.5950-1-yi.l.liu@intel.com
    Cc: stable@vger.kernel.org
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/iommu/iommufd/device.c
+++ drivers/iommu/iommufd/device.c
@@ -471,6 +471,17 @@ iommufd_device_attach_reserved_iova(struct iommufd_device *idev,
 
 /* The device attach/detach/replace helpers for attach_handle */
 
+/* Check if idev is attached to igroup->hwpt */
+static bool iommufd_device_is_attached(struct iommufd_device *idev)
+{
+	struct iommufd_device *cur;
+
+	list_for_each_entry(cur, &idev->igroup->device_list, group_item)
+		if (cur == idev)
+			return true;
+	return false;
+}
+
 static int iommufd_hwpt_attach_device(struct iommufd_hw_pagetable *hwpt,
 				      struct iommufd_device *idev)
 {
@@ -706,6 +717,11 @@ iommufd_device_do_replace(struct iommufd_device *idev,
 	mutex_lock(&idev->igroup->lock);
 
 	if (igroup->hwpt == NULL) {
+		rc = -EINVAL;
+		goto err_unlock;
+	}
+
+	if (!iommufd_device_is_attached(idev)) {
 		rc = -EINVAL;
 		goto err_unlock;
 	}
