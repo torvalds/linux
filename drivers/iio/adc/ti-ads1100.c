3ba3800286bb8881d323659fd019cf6b746580d9
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2025-03-09 16:58:15 +0000

    iio: adc: ti-ads1100: Switch to sparse friendly iio_device_claim/release_direct()
    These new functions allow sparse to find failures to release
    direct mode reducing chances of bugs over the claim_direct_mode()
    functions that are deprecated.
    
    Cc: Mike Looijmans <mike.looijmans@topic.nl>
    Reviewed-by: David Lechner <dlechner@baylibre.com>
    Link: https://patch.msgid.link/20250309165819.1346684-5-jic23@kernel.org
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/adc/ti-ads1100.c
+++ drivers/iio/adc/ti-ads1100.c
@@ -223,12 +223,11 @@ static int ads1100_read_raw(struct iio_dev *indio_dev,
 	guard(mutex)(&data->lock);
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
-		ret = iio_device_claim_direct_mode(indio_dev);
-		if (ret)
-			return ret;
+		if (!iio_device_claim_direct(indio_dev))
+			return -EBUSY;
 
 		ret = ads1100_get_adc_result(data, chan->address, val);
-		iio_device_release_direct_mode(indio_dev);
+		iio_device_release_direct(indio_dev);
 		if (ret < 0)
 			return ret;
 
