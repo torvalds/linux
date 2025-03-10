8a141be3233af7d4f7014ebc44d5452d46b2b1be
Thomas Huth <thuth@redhat.com>
2025-03-10 11:42:56 +0100

    x86/headers: Replace __ASSEMBLY__ with __ASSEMBLER__ in UAPI headers
    __ASSEMBLY__ is only defined by the Makefile of the kernel, so
    this is not really useful for UAPI headers (unless the userspace
    Makefile defines it, too). Let's switch to __ASSEMBLER__ which
    gets set automatically by the compiler when compiling assembly
    code.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Brian Gerst <brgerst@gmail.com>
    Link: https://lore.kernel.org/r/20250310104256.123527-1-thuth@redhat.com

--- arch/x86/include/uapi/asm/ldt.h
+++ arch/x86/include/uapi/asm/ldt.h
@@ -12,7 +12,7 @@
 /* The size of each LDT entry. */
 #define LDT_ENTRY_SIZE	8
 
-#ifndef __ASSEMBLY__
+#ifndef __ASSEMBLER__
 /*
  * Note on 64bit base and limit is ignored and you cannot set DS/ES/CS
  * not to the default values if you still want to do syscalls. This
@@ -44,5 +44,5 @@ struct user_desc {
 #define MODIFY_LDT_CONTENTS_STACK	1
 #define MODIFY_LDT_CONTENTS_CODE	2
 
-#endif /* !__ASSEMBLY__ */
+#endif /* !__ASSEMBLER__ */
 #endif /* _ASM_X86_LDT_H */
