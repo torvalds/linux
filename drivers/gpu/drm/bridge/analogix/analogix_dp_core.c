0e8b86b6df143662c631dee8bb3b1fff368aa18a
Damon Ding <damon.ding@rock-chips.com>
2025-03-10 18:41:11 +0800

    drm/bridge: analogix_dp: Add support for RK3588
    Expand enum analogix_dp_devtype with RK3588_EDP, and add max_link_rate
    and max_lane_count configs for it.
    
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>
    Link: https://lore.kernel.org/r/20250310104114.2608063-11-damon.ding@rock-chips.com
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>

--- drivers/gpu/drm/bridge/analogix/analogix_dp_core.c
+++ drivers/gpu/drm/bridge/analogix/analogix_dp_core.c
@@ -1505,6 +1505,10 @@ static int analogix_dp_dt_parse_pdata(struct analogix_dp_device *dp)
 		video_info->max_link_rate = 0x0A;
 		video_info->max_lane_count = 0x04;
 		break;
+	case RK3588_EDP:
+		video_info->max_link_rate = 0x14;
+		video_info->max_lane_count = 0x04;
+		break;
 	case EXYNOS_DP:
 		/*
 		 * NOTE: those property parseing code is used for
