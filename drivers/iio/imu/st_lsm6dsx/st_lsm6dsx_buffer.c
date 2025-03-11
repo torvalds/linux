159ca7f18129834b6f4c7eae67de48e96c752fc9
Silvano Seva <s.seva@4sigma.it>
2025-03-11 09:49:47 +0100

    iio: imu: st_lsm6dsx: fix possible lockup in st_lsm6dsx_read_fifo
    Prevent st_lsm6dsx_read_fifo from falling in an infinite loop in case
    pattern_len is equal to zero and the device FIFO is not empty.
    
    Fixes: 290a6ce11d93 ("iio: imu: add support to lsm6dsx driver")
    Signed-off-by: Silvano Seva <s.seva@4sigma.it>
    Acked-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Link: https://patch.msgid.link/20250311085030.3593-2-s.seva@4sigma.it
    Cc: <Stable@vger.kernel.org>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/imu/st_lsm6dsx/st_lsm6dsx_buffer.c
+++ drivers/iio/imu/st_lsm6dsx/st_lsm6dsx_buffer.c
@@ -392,6 +392,9 @@ int st_lsm6dsx_read_fifo(struct st_lsm6dsx_hw *hw)
 	if (fifo_status & cpu_to_le16(ST_LSM6DSX_FIFO_EMPTY_MASK))
 		return 0;
 
+	if (!pattern_len)
+		pattern_len = ST_LSM6DSX_SAMPLE_SIZE;
+
 	fifo_len = (le16_to_cpu(fifo_status) & fifo_diff_mask) *
 		   ST_LSM6DSX_CHAN_SIZE;
 	fifo_len = (fifo_len / pattern_len) * pattern_len;
