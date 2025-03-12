194a613088a8c9dae300dfb08433287cee803e8d
Uros Bizjak <ubizjak@gmail.com>
2025-03-12 13:38:44 +0100

    x86/hweight: Use ASM_CALL_CONSTRAINT in inline asm()
    Use ASM_CALL_CONSTRAINT to prevent inline asm() that includes call
    instruction from being scheduled before the frame pointer gets set
    up by the containing function. This unconstrained scheduling might
    cause objtool to print a "call without frame pointer save/setup"
    warning. Current versions of compilers don't seem to trigger this
    condition, but without this constraint there's nothing to prevent
    the compiler from scheduling the insn in front of frame creation.
    
    Signed-off-by: Uros Bizjak <ubizjak@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Nathan Chancellor <nathan@kernel.org>
    Cc: Nick Desaulniers <ndesaulniers@google.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Link: https://lore.kernel.org/r/20250312123905.149298-2-ubizjak@gmail.com

--- arch/x86/include/asm/arch_hweight.h
+++ arch/x86/include/asm/arch_hweight.h
@@ -17,7 +17,7 @@ static __always_inline unsigned int __arch_hweight32(unsigned int w)
 	unsigned int res;
 
 	asm (ALTERNATIVE("call __sw_hweight32", "popcntl %[val], %[cnt]", X86_FEATURE_POPCNT)
-			 : [cnt] "=" REG_OUT (res)
+			 : [cnt] "=" REG_OUT (res), ASM_CALL_CONSTRAINT
 			 : [val] REG_IN (w));
 
 	return res;
@@ -45,7 +45,7 @@ static __always_inline unsigned long __arch_hweight64(__u64 w)
 	unsigned long res;
 
 	asm (ALTERNATIVE("call __sw_hweight64", "popcntq %[val], %[cnt]", X86_FEATURE_POPCNT)
-			 : [cnt] "=" REG_OUT (res)
+			 : [cnt] "=" REG_OUT (res), ASM_CALL_CONSTRAINT
 			 : [val] REG_IN (w));
 
 	return res;
