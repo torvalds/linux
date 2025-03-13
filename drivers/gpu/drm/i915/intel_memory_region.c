9e2434f32d2894e726c091f89ce0fd05e415d354
Ville Syrjälä <ville.syrjala@linux.intel.com>
2025-03-13 16:08:30 +0200

    drm/i915: Expose intel_memory_type_str()
    Rename region_type_str() into intel_memory_type_str() and
    expose it outside intel_memory_region.c. I'll have another
    use for this in the BIOS FB takeover code.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20250313140838.29742-3-ville.syrjala@linux.intel.com
    Reviewed-by: Jouni Högander <jouni.hogander@intel.com>

--- drivers/gpu/drm/i915/intel_memory_region.c
+++ drivers/gpu/drm/i915/intel_memory_region.c
@@ -227,7 +227,7 @@ static int intel_memory_region_memtest(struct intel_memory_region *mem,
 	return err;
 }
 
-static const char *region_type_str(u16 type)
+const char *intel_memory_type_str(enum intel_memory_type type)
 {
 	switch (type) {
 	case INTEL_MEMORY_SYSTEM:
@@ -271,7 +271,7 @@ intel_memory_region_create(struct drm_i915_private *i915,
 	mem->instance = instance;
 
 	snprintf(mem->uabi_name, sizeof(mem->uabi_name), "%s%u",
-		 region_type_str(type), instance);
+		 intel_memory_type_str(type), instance);
 
 	mutex_init(&mem->objects.lock);
 	INIT_LIST_HEAD(&mem->objects.list);
