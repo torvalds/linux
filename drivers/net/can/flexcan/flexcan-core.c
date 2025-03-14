5a19143124be42900b3fbc9ada3c919632eb45eb
Haibo Chen <haibo.chen@nxp.com>
2025-03-14 19:01:45 +0800

    can: flexcan: disable transceiver during system PM
    During system PM, if no wakeup requirement, disable transceiver to
    save power.
    
    Fixes: 4de349e786a3 ("can: flexcan: fix resume function")
    Cc: stable@vger.kernel.org
    Reviewed-by: Frank Li <frank.li@nxp.com>
    Signed-off-by: Haibo Chen <haibo.chen@nxp.com>
    Link: https://patch.msgid.link/20250314110145.899179-2-haibo.chen@nxp.com
    [mkl: add newlines]
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/flexcan/flexcan-core.c
+++ drivers/net/can/flexcan/flexcan-core.c
@@ -2260,6 +2260,10 @@ static int __maybe_unused flexcan_suspend(struct device *device)
 
 			flexcan_chip_interrupts_disable(dev);
 
+			err = flexcan_transceiver_disable(priv);
+			if (err)
+				return err;
+
 			err = pinctrl_pm_select_sleep_state(device);
 			if (err)
 				return err;
@@ -2292,9 +2296,15 @@ static int __maybe_unused flexcan_resume(struct device *device)
 			if (err)
 				return err;
 
-			err = flexcan_chip_start(dev);
+			err = flexcan_transceiver_enable(priv);
 			if (err)
 				return err;
+
+			err = flexcan_chip_start(dev);
+			if (err) {
+				flexcan_transceiver_disable(priv);
+				return err;
+			}
 
 			flexcan_chip_interrupts_enable(dev);
 		}
