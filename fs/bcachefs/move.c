6aa446c05a44ddc46b0aea510a95d288e993daec
Kent Overstreet <kent.overstreet@linux.dev>
2025-03-15 17:27:27 -0400

    bcachefs: Fix offset_into_extent in data move path
    Fixes the following:
    
    [   17.607394] kernel BUG at fs/bcachefs/reflink.c:261!
    [   17.608316] Oops: invalid opcode: 0000 [#1] PREEMPT SMP NOPTI
    [   17.608485] CPU: 0 UID: 0 PID: 564 Comm: bch-rebalance/3 Tainted: G           OE      6.14.0-rc6-arch1-gfcb0bd9609d2 #7 0efd7a8f4a00afeb2c5fb6e7ecb1aec8ddcbb1e1
    [   17.608616] Tainted: [O]=OOT_MODULE, [E]=UNSIGNED_MODULE
    [   17.608736] Hardware name: Micro-Star International Co., Ltd. MS-7D75/MAG B650 TOMAHAWK WIFI (MS-7D75), BIOS 1.74 08/01/2023
    [   17.608855] RIP: 0010:bch2_lookup_indirect_extent+0x252/0x290 [bcachefs]
    [   17.609006] Code: 00 00 00 00 e8 7f 51 f5 ff 89 c3 85 c0 74 52 48 8b 7d b0 4c 89 ee e8 4d 4b f4 ff 48 63 d3 48 89 d0 31 d2 e9 2e ff ff ff 0f 0b <0f> 0b 48 8b 7d b0 4c 89 ee 48 89 55 a8 e8 2c 4b f4 ff 4c 8b 55 a8
    [   17.609136] RSP: 0018:ffffa3714455f850 EFLAGS: 00010246
    [   17.609261] RAX: 0000000000000080 RBX: ffff895891098790 RCX: 0000000000000000
    [   17.609387] RDX: 0000000000000080 RSI: ffffa3714455fa90 RDI: ffff895889550000
    [   17.609511] RBP: ffffa3714455f8c0 R08: ffff895891098790 R09: 0000000000000001
    [   17.609637] R10: ffffa3714455f8d8 R11: ffffa3714455f950 R12: ffffa3714455fa58
    [   17.609763] R13: ffff895891098790 R14: ffffa3714455fa58 R15: ffff895889550000
    [   17.609888] FS:  0000000000000000(0000) GS:ffff896757c00000(0000) knlGS:0000000000000000
    [   17.610015] CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
    [   17.610143] CR2: 0000716b8cda2750 CR3: 0000000914e22000 CR4: 0000000000f50ef0
    [   17.610272] PKRU: 55555554
    [   17.610403] Call Trace:
    [   17.610535]  <TASK>
    [   17.610662]  ? __die_body.cold+0x19/0x27
    [   17.610791]  ? die+0x2e/0x50
    [   17.610918]  ? do_trap+0xca/0x110
    [   17.611049]  ? do_error_trap+0x6a/0x90
    [   17.611178]  ? bch2_lookup_indirect_extent+0x252/0x290 [bcachefs c42b95c23facdfe11d39755520127cd771dddec2]
    [   17.611331]  ? exc_invalid_op+0x50/0x70
    [   17.611468]  ? bch2_lookup_indirect_extent+0x252/0x290 [bcachefs c42b95c23facdfe11d39755520127cd771dddec2]
    [   17.611620]  ? asm_exc_invalid_op+0x1a/0x20
    [   17.611757]  ? bch2_lookup_indirect_extent+0x252/0x290 [bcachefs c42b95c23facdfe11d39755520127cd771dddec2]
    [   17.611911]  ? bch2_move_data_btree+0x58a/0x6c0 [bcachefs c42b95c23facdfe11d39755520127cd771dddec2]
    [   17.612084]  bch2_move_data_btree+0x58a/0x6c0 [bcachefs c42b95c23facdfe11d39755520127cd771dddec2]
    [   17.612256]  ? __pfx_rebalance_pred+0x10/0x10 [bcachefs c42b95c23facdfe11d39755520127cd771dddec2]
    [   17.612431]  ? bch2_move_extent+0x3d7/0x6e0 [bcachefs c42b95c23facdfe11d39755520127cd771dddec2]
    [   17.612607]  ? __bch2_move_data+0xea/0x200 [bcachefs c42b95c23facdfe11d39755520127cd771dddec2]
    [   17.612782]  __bch2_move_data+0xea/0x200 [bcachefs c42b95c23facdfe11d39755520127cd771dddec2]
    [   17.612959]  ? __pfx_rebalance_pred+0x10/0x10 [bcachefs c42b95c23facdfe11d39755520127cd771dddec2]
    [   17.613149]  do_rebalance+0x517/0x8d0 [bcachefs c42b95c23facdfe11d39755520127cd771dddec2]
    [   17.613342]  ? local_clock_noinstr+0xd/0xd0
    [   17.613518]  ? local_clock+0x15/0x30
    [   17.613693]  ? __bch2_trans_get+0x152/0x300 [bcachefs c42b95c23facdfe11d39755520127cd771dddec2]
    [   17.613890]  ? __pfx_bch2_rebalance_thread+0x10/0x10 [bcachefs c42b95c23facdfe11d39755520127cd771dddec2]
    [   17.614090]  bch2_rebalance_thread+0x66/0xb0 [bcachefs c42b95c23facdfe11d39755520127cd771dddec2]
    
    The offset_into_extent bit was copied from the read path, but it's
    unnecessary here, where we always want to read and move the entire
    indirect extent, and it causes the assertion pop - because we're using a
    non-extents iterator, which always points to the end of the reflink
    pointer.
    
    Reported-by: Maël Kerbiriou <mael.kerbiriou@free.fr>
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/move.c
+++ fs/bcachefs/move.c
@@ -591,7 +591,7 @@ static int bch2_move_data_btree(struct moving_context *ctxt,
 		    k.k->type == KEY_TYPE_reflink_p &&
 		    REFLINK_P_MAY_UPDATE_OPTIONS(bkey_s_c_to_reflink_p(k).v)) {
 			struct bkey_s_c_reflink_p p = bkey_s_c_to_reflink_p(k);
-			s64 offset_into_extent	= iter.pos.offset - bkey_start_offset(k.k);
+			s64 offset_into_extent	= 0;
 
 			bch2_trans_iter_exit(trans, &reflink_iter);
 			k = bch2_lookup_indirect_extent(trans, &reflink_iter, &offset_into_extent, p, true, 0);
@@ -610,6 +610,7 @@ static int bch2_move_data_btree(struct moving_context *ctxt,
 			 * pointer - need to fixup iter->k
 			 */
 			extent_iter = &reflink_iter;
+			offset_into_extent = 0;
 		}
 
 		if (!bkey_extent_is_direct_data(k.k))
