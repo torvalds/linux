a024843d92cca491034dac648bcfec967199a4a4
Mykyta Yatsenko <yatsenko@meta.com>
2025-03-17 17:40:39 +0000

    selftests/bpf: Test freplace from user namespace
    Add selftests to verify that it is possible to load freplace program
    from user namespace if BPF token is initialized by bpf_object__prepare
    before calling bpf_program__set_attach_target.
    Negative test is added as well.
    
    Modified type of the priv_prog to xdp, as kprobe did not work on aarch64
    and s390x.
    
    Signed-off-by: Mykyta Yatsenko <yatsenko@meta.com>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Acked-by: Yonghong Song <yonghong.song@linux.dev>
    Link: https://lore.kernel.org/bpf/20250317174039.161275-5-mykyta.yatsenko5@gmail.com

--- tools/testing/selftests/bpf/progs/priv_freplace_prog.c
+++ tools/testing/selftests/bpf/progs/priv_freplace_prog.c
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0
-/* Copyright (c) 2023 Meta Platforms, Inc. and affiliates. */
+/* Copyright (c) 2025 Meta Platforms, Inc. and affiliates. */
 
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 
 char _license[] SEC("license") = "GPL";
 
-SEC("xdp")
-int xdp_prog1(struct xdp_md *xdp)
+SEC("freplace/xdp_prog1")
+int new_xdp_prog2(struct xdp_md *xd)
 {
 	return XDP_DROP;
 }
