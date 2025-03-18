3bb9286f4ece6acbc1fbaa9f192a82645d30efbf
Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
2025-03-18 14:35:36 +0200

    phy: hdmi: Add color depth configuration
    Extend the HDMI configuration options to allow managing bits per color
    channel.  This is required by some PHY drivers such as
    rockchip-samsung-hdptx.
    
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
    Link: https://lore.kernel.org/r/20250318-phy-sam-hdptx-bpc-v6-2-8cb1678e7663@collabora.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- include/linux/phy/phy-hdmi.h
+++ include/linux/phy/phy-hdmi.h
@@ -9,11 +9,13 @@
 /**
  * struct phy_configure_opts_hdmi - HDMI configuration set
  * @tmds_char_rate: HDMI TMDS Character Rate in Hertz.
+ * @bpc: Bits per color channel.
  *
  * This structure is used to represent the configuration state of a HDMI phy.
  */
 struct phy_configure_opts_hdmi {
 	unsigned long long tmds_char_rate;
+	unsigned int bpc;
 };
 
 #endif /* __PHY_HDMI_H_ */
