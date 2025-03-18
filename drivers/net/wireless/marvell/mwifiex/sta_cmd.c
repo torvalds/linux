9868c4ce9481043d6c11f7421fe2b9637aa0feee
Jeff Chen <jeff.chen_1@nxp.com>
2025-03-18 13:07:39 +0800

    wifi: mwifiex: Fix RF calibration data download from file
    This patch resolves an issue where RF calibration data from a
    file could not be downloaded to the firmware. The feature to
    download calibration data from a file was broken by the commit:
    d39fbc88956e.
    
    The issue arose because the function `mwifiex_cmd_cfg_data()`
    was modified in a way that prevented proper handling of
    file-based calibration data. While this patch restores the ability
    to download RF calibration data from a file, it may inadvertently
    break the feature to download calibration data from the device
    tree. This is because the function `mwifiex_dnld_dt_cfgdata()`,
    which also relies on `mwifiex_cmd_cfg_data()`, is still used for
    device tree-based calibration data downloads.
    
    Fixes: d39fbc88956e ("mwifiex: remove cfg_data construction")
    Signed-off-by: Jeff Chen <jeff.chen_1@nxp.com>
    Link: https://patch.msgid.link/20250318050739.2239376-3-jeff.chen_1@nxp.com
    [add newline for shorter lines]
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/marvell/mwifiex/sta_cmd.c
+++ drivers/net/wireless/marvell/mwifiex/sta_cmd.c
@@ -1507,6 +1507,7 @@ static int mwifiex_cmd_cfg_data(struct mwifiex_private *priv,
 	u32 len;
 	u8 *data = (u8 *)cmd + S_DS_GEN;
 	int ret;
+	struct host_cmd_ds_802_11_cfg_data *pcfg_data;
 
 	if (prop) {
 		len = prop->length;
@@ -1514,12 +1515,20 @@ static int mwifiex_cmd_cfg_data(struct mwifiex_private *priv,
 						data, len);
 		if (ret)
 			return ret;
+
+		cmd->size = cpu_to_le16(S_DS_GEN + len);
 		mwifiex_dbg(adapter, INFO,
 			    "download cfg_data from device tree: %s\n",
 			    prop->name);
 	} else if (adapter->cal_data->data && adapter->cal_data->size > 0) {
 		len = mwifiex_parse_cal_cfg((u8 *)adapter->cal_data->data,
-					    adapter->cal_data->size, data);
+					    adapter->cal_data->size,
+					    data + sizeof(*pcfg_data));
+		pcfg_data = &cmd->params.cfg_data;
+		pcfg_data->action = cpu_to_le16(HOST_CMD_ACT_GEN_SET);
+		pcfg_data->type = cpu_to_le16(MWIFIEX_CFG_TYPE_CAL);
+		pcfg_data->data_len = cpu_to_le16(len);
+		cmd->size = cpu_to_le16(S_DS_GEN + sizeof(*pcfg_data) + len);
 		mwifiex_dbg(adapter, INFO,
 			    "download cfg_data from config file\n");
 	} else {
@@ -1527,7 +1536,6 @@ static int mwifiex_cmd_cfg_data(struct mwifiex_private *priv,
 	}
 
 	cmd->command = cpu_to_le16(HostCmd_CMD_CFG_DATA);
-	cmd->size = cpu_to_le16(S_DS_GEN + len);
 
 	return 0;
 }
