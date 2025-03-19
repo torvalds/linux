874fa827df50a9e1d258c7539e51cd945cc4a34d
Ian Rogers <irogers@google.com>
2025-03-18 22:07:40 -0700

    perf trace: Fix BTF memory leak
    Add missing btf__free in trace__exit.
    
    Signed-off-by: Ian Rogers <irogers@google.com>
    Reviewed-by: Namhyung Kim <namhyung@kernel.org>
    Acked-by: Arnaldo Carvalho de Melo <acme@kernel.org>
    Link: https://lore.kernel.org/r/20250319050741.269828-14-irogers@google.com
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/builtin-trace.c
+++ tools/perf/builtin-trace.c
@@ -5339,6 +5339,10 @@ static void trace__exit(struct trace *trace)
 		zfree(&trace->syscalls.table);
 	}
 	zfree(&trace->perfconfig_events);
+#ifdef HAVE_LIBBPF_SUPPORT
+	btf__free(trace->btf);
+	trace->btf = NULL;
+#endif
 }
 
 #ifdef HAVE_BPF_SKEL
