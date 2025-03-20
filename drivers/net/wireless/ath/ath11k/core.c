31e98e277ae47f56632e4d663b1d4fd12ba33ea8
Stone Zhang <quic_stonez@quicinc.com>
2025-03-20 13:31:45 +0800

    wifi: ath11k: fix node corruption in ar->arvifs list
    In current WLAN recovery code flow, ath11k_core_halt() only
    reinitializes the "arvifs" list head. This will cause the
    list node immediately following the list head to become an
    invalid list node. Because the prev of that node still points
    to the list head "arvifs", but the next of the list head "arvifs"
    no longer points to that list node.
    
    When a WLAN recovery occurs during the execution of a vif
    removal, and it happens before the spin_lock_bh(&ar->data_lock)
    in ath11k_mac_op_remove_interface(), list_del() will detect the
    previously mentioned situation, thereby triggering a kernel panic.
    
    The fix is to remove and reinitialize all vif list nodes from the
    list head "arvifs" during WLAN halt. The reinitialization is to make
    the list nodes valid, ensuring that the list_del() in
    ath11k_mac_op_remove_interface() can execute normally.
    
    Call trace:
    __list_del_entry_valid_or_report+0xb8/0xd0
    ath11k_mac_op_remove_interface+0xb0/0x27c [ath11k]
    drv_remove_interface+0x48/0x194 [mac80211]
    ieee80211_do_stop+0x6e0/0x844 [mac80211]
    ieee80211_stop+0x44/0x17c [mac80211]
    __dev_close_many+0xac/0x150
    __dev_change_flags+0x194/0x234
    dev_change_flags+0x24/0x6c
    devinet_ioctl+0x3a0/0x670
    inet_ioctl+0x200/0x248
    sock_do_ioctl+0x60/0x118
    sock_ioctl+0x274/0x35c
    __arm64_sys_ioctl+0xac/0xf0
    invoke_syscall+0x48/0x114
    ...
    
    Tested-on: QCA6698AQ hw2.1 PCI WLAN.HSP.1.1-04591-QCAHSPSWPL_V1_V2_SILICONZ_IOE-1
    
    Fixes: d5c65159f289 ("ath11k: driver for Qualcomm IEEE 802.11ax devices")
    Signed-off-by: Stone Zhang <quic_stonez@quicinc.com>
    Link: https://patch.msgid.link/20250320053145.3445187-1-quic_stonez@quicinc.com
    Signed-off-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>

--- drivers/net/wireless/ath/ath11k/core.c
+++ drivers/net/wireless/ath/ath11k/core.c
@@ -2238,6 +2238,7 @@ err_hal_srng_deinit:
 void ath11k_core_halt(struct ath11k *ar)
 {
 	struct ath11k_base *ab = ar->ab;
+	struct list_head *pos, *n;
 
 	lockdep_assert_held(&ar->conf_mutex);
 
@@ -2253,7 +2254,12 @@ void ath11k_core_halt(struct ath11k *ar)
 
 	rcu_assign_pointer(ab->pdevs_active[ar->pdev_idx], NULL);
 	synchronize_rcu();
-	INIT_LIST_HEAD(&ar->arvifs);
+
+	spin_lock_bh(&ar->data_lock);
+	list_for_each_safe(pos, n, &ar->arvifs)
+		list_del_init(pos);
+	spin_unlock_bh(&ar->data_lock);
+
 	idr_init(&ar->txmgmt_idr);
 }
 
