cc9428d5336aec746ffeb3d13ee2cf49756b157d
Saleemkhan Jamadar <saleemkhan.jamadar@amd.com>
2025-03-21 17:00:09 +0530

    drm/amd/display: add proper error message for vblank init
    v1 - DRM_ERROR to drm_err (Mario)
    
    Update message to identifiy the vblank initialization fail case
    
    Signed-off-by: Saleemkhan Jamadar <saleemkhan.jamadar@amd.com>
    Reviewed-by: Rodrigo Siqueira <siqueira@igalia.com>
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -2241,7 +2241,7 @@ static int amdgpu_dm_init(struct amdgpu_device *adev)
 	adev_to_drm(adev)->mode_config.cursor_height = adev->dm.dc->caps.max_cursor_size;
 
 	if (drm_vblank_init(adev_to_drm(adev), adev->dm.display_indexes_num)) {
-		dev_err(adev->dev,
+		drm_err(adev_to_drm(adev),
 		"amdgpu: failed to initialize vblank sw for display support.\n");
 		goto error;
 	}
