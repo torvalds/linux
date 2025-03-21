5a4dd520ef8a94ecf81ac77b90d6a03e91c100a9
Syed Saba kareem <syed.sabakareem@amd.com>
2025-03-21 17:55:03 +0530

    ASoC: amd: yc: update quirk data for new Lenovo model
    Update Quirk data for new Lenovo model 83J2 for YC platform.
    
    Signed-off-by: Syed Saba kareem <syed.sabakareem@amd.com>
    Link: https://patch.msgid.link/20250321122507.190193-1-syed.sabakareem@amd.com
    Reported-by: Reiner <Reiner.Proels@gmail.com>
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=219887
    Tested-by: Reiner <Reiner.Proels@gmail.com>
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/yc/acp6x-mach.c
+++ sound/soc/amd/yc/acp6x-mach.c
@@ -342,6 +342,13 @@ static const struct dmi_system_id yc_acp_quirk_table[] = {
 	{
 		.driver_data = &acp6x_card,
 		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "LENOVO"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "83J2"),
+		}
+	},
+	{
+		.driver_data = &acp6x_card,
+		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "ASUSTeK COMPUTER INC."),
 			DMI_MATCH(DMI_PRODUCT_NAME, "UM5302TA"),
 		}
