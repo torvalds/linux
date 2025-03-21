0b4fd567261bc21ba1fd8636489396f0940b54f8
Kent Overstreet <kent.overstreet@linux.dev>
2025-03-21 14:22:39 -0400

    bcachefs: btree_trans_restart_foreign_task()
    In debug mode, we save the call stack on transaction restart - but
    there's no locking, so we can't touch it if we're issuing the restart
    from another thread.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/btree_locking.c
+++ fs/bcachefs/btree_locking.c
@@ -172,7 +172,9 @@ static int abort_lock(struct lock_graph *g, struct trans_waiting_for_lock *i)
 {
 	if (i == g->g) {
 		trace_would_deadlock(g, i->trans);
-		return btree_trans_restart(i->trans, BCH_ERR_transaction_restart_would_deadlock);
+		return btree_trans_restart_foreign_task(i->trans,
+					BCH_ERR_transaction_restart_would_deadlock,
+					_THIS_IP_);
 	} else {
 		i->trans->lock_must_abort = true;
 		wake_up_process(i->trans->locking_wait.task);
