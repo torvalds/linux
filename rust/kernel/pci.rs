1dbaf8b1bafb8557904eb54b98bb323a3061dd2c
Miguel Ojeda <ojeda@kernel.org>
2025-03-24 22:03:54 +0100

    rust: pci: fix docs related to missing Markdown code spans
    In particular:
    
      - Add missing Markdown code spans.
    
      - Improve title for `DeviceId`, adding a link to the struct in the
        C side, rather than referring to `bindings::`.
    
      - Convert `TODO` from documentation to a normal comment, and put code
        in block.
    
    This was found using the Clippy `doc_markdown` lint, which we may want
    to enable.
    
    Fixes: 1bd8b6b2c5d3 ("rust: pci: add basic PCI device / driver abstractions")
    Reviewed-by: Benno Lossin <benno.lossin@proton.me>
    Acked-by: Danilo Krummrich <dakr@kernel.org>
    Link: https://lore.kernel.org/r/20250324210359.1199574-8-ojeda@kernel.org
    [ Prefixed link text with `struct`. - Miguel ]
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- rust/kernel/pci.rs
+++ rust/kernel/pci.rs
@@ -118,7 +118,9 @@ macro_rules! module_pci_driver {
 };
 }
 
-/// Abstraction for bindings::pci_device_id.
+/// Abstraction for the PCI device ID structure ([`struct pci_device_id`]).
+///
+/// [`struct pci_device_id`]: https://docs.kernel.org/PCI/pci.html#c.pci_device_id
 #[repr(transparent)]
 #[derive(Clone, Copy)]
 pub struct DeviceId(bindings::pci_device_id);
@@ -173,7 +175,7 @@ unsafe impl RawDeviceId for DeviceId {
     }
 }
 
-/// IdTable type for PCI
+/// `IdTable` type for PCI.
 pub type IdTable<T> = &'static dyn kernel::device_id::IdTable<DeviceId, T>;
 
 /// Create a PCI `IdTable` with its alias for modpost.
@@ -224,10 +226,11 @@ macro_rules! pci_device_table {
 /// `Adapter` documentation for an example.
 pub trait Driver: Send {
     /// The type holding information about each device id supported by the driver.
-    ///
-    /// TODO: Use associated_type_defaults once stabilized:
-    ///
-    /// type IdInfo: 'static = ();
+    // TODO: Use `associated_type_defaults` once stabilized:
+    //
+    // ```
+    // type IdInfo: 'static = ();
+    // ```
     type IdInfo: 'static;
 
     /// The table of device ids supported by the driver.
