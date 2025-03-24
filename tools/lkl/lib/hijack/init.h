4abf48fdfff98d3c3238419d07ab9390a74e427a
David Disseldorp <ddiss@suse.de>
2025-03-24 21:46:30 +1100

    lkl: hijack: move dbg_handler out of liblkl
    dbg_handler exposes a very useful debug shell, but it's currently only
    used (within tools/lkl at least) by hijack and zpoline.
    Move the functionality into liblkl-hijack, to slightly trim down the
    liblkl core library.
    This may break external lkl_register_dbg_handler() callers.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- tools/lkl/lib/hijack/init.h
+++ tools/lkl/lib/hijack/init.h
@@ -7,4 +7,15 @@ extern int dual_fds[];
 void __hijack_init(void);
 void __hijack_fini(void);
 
+/*
+ * lkl_register_dbg_handler- register a signal handler that loads a debug lib.
+ *
+ * The signal handler is triggered by Ctrl-Z. It creates a new pthread which
+ * call dbg_entrance().
+ *
+ * If you run the program from shell script, make sure you ignore SIGTSTP by
+ * "trap '' TSTP" in the shell script.
+ */
+void lkl_register_dbg_handler(void);
+
 #endif /*_LKL_HIJACK_INIT_H */
