19408200c094858d952a90bf4977733dc89a4df5
Vincent Mailhol <mailhol.vincent@wanadoo.fr>
2025-03-26 00:59:57 +0900

    bits: introduce fixed-type GENMASK_U*()
    Add GENMASK_TYPE() which generalizes __GENMASK() to support different
    types, and implement fixed-types versions of GENMASK() based on it.
    The fixed-type version allows more strict checks to the min/max values
    accepted, which is useful for defining registers like implemented by
    i915 and xe drivers with their REG_GENMASK*() macros.
    
    The strict checks rely on shift-count-overflow compiler check to fail
    the build if a number outside of the range allowed is passed.
    Example:
    
      #define FOO_MASK GENMASK_U32(33, 4)
    
    will generate a warning like:
    
      include/linux/bits.h:51:27: error: right shift count >= width of type [-Werror=shift-count-overflow]
         51 |               type_max(t) >> (BITS_PER_TYPE(t) - 1 - (h)))))
            |                           ^~
    
    The result is casted to the corresponding fixed width type. For
    example, GENMASK_U8() returns an u8. Note that because of the C
    promotion rules, GENMASK_U8() and GENMASK_U16() will immediately be
    promoted to int if used in an expression. Regardless, the main goal is
    not to get the correct type, but rather to enforce more checks at
    compile time.
    
    While GENMASK_TYPE() is crafted to cover all variants, including the
    already existing GENMASK(), GENMASK_ULL() and GENMASK_U128(), for the
    moment, only use it for the newly introduced GENMASK_U*(). The
    consolidation will be done in a separate change.
    
    Co-developed-by: Yury Norov <yury.norov@gmail.com>
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Acked-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Vincent Mailhol <mailhol.vincent@wanadoo.fr>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Yury Norov <yury.norov@gmail.com>

--- include/linux/bits.h
+++ include/linux/bits.h
@@ -12,6 +12,7 @@
 #define BIT_ULL_MASK(nr)	(ULL(1) << ((nr) % BITS_PER_LONG_LONG))
 #define BIT_ULL_WORD(nr)	((nr) / BITS_PER_LONG_LONG)
 #define BITS_PER_BYTE		8
+#define BITS_PER_TYPE(type)	(sizeof(type) * BITS_PER_BYTE)
 
 /*
  * Create a contiguous bitmask starting at bit position @l and ending at
@@ -20,10 +21,39 @@
  */
 #if !defined(__ASSEMBLY__)
 
+/*
+ * Missing asm support
+ *
+ * GENMASK_U*() depend on BITS_PER_TYPE() which relies on sizeof(),
+ * something not available in asm. Nevertheless, fixed width integers is a C
+ * concept. Assembly code can rely on the long and long long versions instead.
+ */
+
 #include <linux/build_bug.h>
 #include <linux/compiler.h>
+#include <linux/overflow.h>
 
 #define GENMASK_INPUT_CHECK(h, l) BUILD_BUG_ON_ZERO(const_true((l) > (h)))
+
+/*
+ * Generate a mask for the specified type @t. Additional checks are made to
+ * guarantee the value returned fits in that type, relying on
+ * -Wshift-count-overflow compiler check to detect incompatible arguments.
+ * For example, all these create build errors or warnings:
+ *
+ * - GENMASK(15, 20): wrong argument order
+ * - GENMASK(72, 15): doesn't fit unsigned long
+ * - GENMASK_U32(33, 15): doesn't fit in a u32
+ */
+#define GENMASK_TYPE(t, h, l)					\
+	((t)(GENMASK_INPUT_CHECK(h, l) +			\
+	     (type_max(t) << (l) &				\
+	      type_max(t) >> (BITS_PER_TYPE(t) - 1 - (h)))))
+
+#define GENMASK_U8(h, l)	GENMASK_TYPE(u8, h, l)
+#define GENMASK_U16(h, l)	GENMASK_TYPE(u16, h, l)
+#define GENMASK_U32(h, l)	GENMASK_TYPE(u32, h, l)
+#define GENMASK_U64(h, l)	GENMASK_TYPE(u64, h, l)
 
 #else /* defined(__ASSEMBLY__) */
 
