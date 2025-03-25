7fc3a367b2f51004e4c0bd7b215502b7be6a042b
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2025-03-25 14:11:04 +0100

    staging: rtl8723bs: simplify if-else block by removing unnecessary braces
    The if-else block contained braces around single statements, which are
    unnecessary according to the Linux kernel coding style.
    
    Remove the redundant braces to improve code readability and maintain
    consistency with the rest of the codebase.
    
    Reported by checkpatch:
    
    WARNING: braces {} are not necessary for any arm of this statement
    
    Signed-off-by: Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
    Reviewed-by: Julia Lawall <julia.lawall@inria.fr>
    Link: https://lore.kernel.org/r/Z+KraOo2DfmH5zMX@HP-650
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/core/rtw_ieee80211.c
+++ drivers/staging/rtl8723bs/core/rtw_ieee80211.c
@@ -988,11 +988,10 @@ void rtw_macaddr_cfg(struct device *dev, u8 *mac_addr)
 	if (is_broadcast_ether_addr(mac) || is_zero_ether_addr(mac)) {
 		addr = of_get_property(np, "local-mac-address", &len);
 
-		if (addr && len == ETH_ALEN) {
+		if (addr && len == ETH_ALEN)
 			ether_addr_copy(mac_addr, addr);
-		} else {
+		else
 			eth_random_addr(mac_addr);
-		}
 	}
 }
 
