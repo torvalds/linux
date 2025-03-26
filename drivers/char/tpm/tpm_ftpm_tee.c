980a573621ea4b5032123937df0115bdbec6b2de
Jarkko Sakkinen <jarkko.sakkinen@opinsys.com>
2025-03-26 17:55:49 +0200

    tpm: Make chip->{status,cancel,req_canceled} opt
    tpm_ftpm_tee does not require chip->status, chip->cancel and
    chip->req_canceled. Make them optional.
    
    Reviewed-by: Stefano Garzarella <sgarzare@redhat.com>
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@opinsys.com>
    Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>

--- drivers/char/tpm/tpm_ftpm_tee.c
+++ drivers/char/tpm/tpm_ftpm_tee.c
@@ -164,30 +164,10 @@ static int ftpm_tee_tpm_op_send(struct tpm_chip *chip, u8 *buf, size_t len)
 	return 0;
 }
 
-static void ftpm_tee_tpm_op_cancel(struct tpm_chip *chip)
-{
-	/* not supported */
-}
-
-static u8 ftpm_tee_tpm_op_status(struct tpm_chip *chip)
-{
-	return 0;
-}
-
-static bool ftpm_tee_tpm_req_canceled(struct tpm_chip *chip, u8 status)
-{
-	return false;
-}
-
 static const struct tpm_class_ops ftpm_tee_tpm_ops = {
 	.flags = TPM_OPS_AUTO_STARTUP,
 	.recv = ftpm_tee_tpm_op_recv,
 	.send = ftpm_tee_tpm_op_send,
-	.cancel = ftpm_tee_tpm_op_cancel,
-	.status = ftpm_tee_tpm_op_status,
-	.req_complete_mask = 0,
-	.req_complete_val = 0,
-	.req_canceled = ftpm_tee_tpm_req_canceled,
 };
 
 /*
