4038005a226f7c9a3882730bf48596cecbfbf619
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2025-03-27 12:23:44 +0000

    Update version.c

--- init/version.c
+++ init/version.c
@@ -33,7 +33,7 @@ static int __init early_hostname(char *arg)
 early_param("hostname", early_hostname);
 
 const char linux_proc_banner[] =
-	"%s version %s"
+	"NeTT OS 7, using Linux: %s version %s"
 	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
 	" (" LINUX_COMPILER ") %s\n";
 
