b584ab12d59f646b9254b2b16ff197d612fd4935
Jerome Brunet <jbrunet@baylibre.com>
2025-03-28 15:30:44 +0100

    PCI: rcar-gen4: set ep BAR4 fixed size
    On rcar-gen4, the ep BAR4 has a fixed size of 256B. Document this
    constraint in the epc features of the platform.
    
    Fixes: e311b3834dfa ("PCI: rcar-gen4: Add endpoint mode support")
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Reviewed-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Link: https://patch.msgid.link/20250328-rcar-gen4-bar4-v1-1-10bb6ce9ee7f@baylibre.com

--- drivers/pci/controller/dwc/pcie-rcar-gen4.c
+++ drivers/pci/controller/dwc/pcie-rcar-gen4.c
@@ -403,6 +403,7 @@ static const struct pci_epc_features rcar_gen4_pcie_epc_features = {
 	.msix_capable = false,
 	.bar[BAR_1] = { .type = BAR_RESERVED, },
 	.bar[BAR_3] = { .type = BAR_RESERVED, },
+	.bar[BAR_4] = { .type = BAR_FIXED, .fixed_size = 256 },
 	.bar[BAR_5] = { .type = BAR_RESERVED, },
 	.align = SZ_1M,
 };
