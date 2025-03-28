ccad447a3d331a239477c281533bacb585b54a98
Ojaswin Mujoo <ojaswin@linux.ibm.com>
2025-03-28 11:54:52 +0530

    ext4: make block validity check resistent to sb bh corruption
    Block validity checks need to be skipped in case they are called
    for journal blocks since they are part of system's protected
    zone.
    
    Currently, this is done by checking inode->ino against
    sbi->s_es->s_journal_inum, which is a direct read from the ext4 sb
    buffer head. If someone modifies this underneath us then the
    s_journal_inum field might get corrupted. To prevent against this,
    change the check to directly compare the inode with journal->j_inode.
    
    **Slight change in behavior**: During journal init path,
    check_block_validity etc might be called for journal inode when
    sbi->s_journal is not set yet. In this case we now proceed with
    ext4_inode_block_valid() instead of returning early. Since systems zones
    have not been set yet, it is okay to proceed so we can perform basic
    checks on the blocks.
    
    Suggested-by: Baokun Li <libaokun1@huawei.com>
    Reviewed-by: Baokun Li <libaokun1@huawei.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Zhang Yi <yi.zhang@huawei.com>
    Signed-off-by: Ojaswin Mujoo <ojaswin@linux.ibm.com>
    Link: https://patch.msgid.link/0c06bc9ebfcd6ccfed84a36e79147bf45ff5adc1.1743142920.git.ojaswin@linux.ibm.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -384,10 +384,11 @@ static int __check_block_validity(struct inode *inode, const char *func,
 				unsigned int line,
 				struct ext4_map_blocks *map)
 {
-	if (ext4_has_feature_journal(inode->i_sb) &&
-	    (inode->i_ino ==
-	     le32_to_cpu(EXT4_SB(inode->i_sb)->s_es->s_journal_inum)))
+	journal_t *journal = EXT4_SB(inode->i_sb)->s_journal;
+
+	if (journal && inode == journal->j_inode)
 		return 0;
+
 	if (!ext4_inode_block_valid(inode, map->m_pblk, map->m_len)) {
 		ext4_error_inode(inode, func, line, map->m_pblk,
 				 "lblock %lu mapped to illegal pblock %llu "
