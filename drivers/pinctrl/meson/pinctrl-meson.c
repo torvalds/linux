e56088a13708757da68ad035269d69b93ac8c389
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2025-03-29 20:01:32 +0100

    pinctrl: meson: define the pull up/down resistor value as 60 kOhm
    The public datasheets of the following Amlogic SoCs describe a typical
    resistor value for the built-in pull up/down resistor:
    - Meson8/8b/8m2: not documented
    - GXBB (S905): 60 kOhm
    - GXL (S905X): 60 kOhm
    - GXM (S912): 60 kOhm
    - G12B (S922X): 60 kOhm
    - SM1 (S905D3): 60 kOhm
    
    The public G12B and SM1 datasheets additionally state min and max
    values:
    - min value: 50 kOhm for both, pull-up and pull-down
    - max value for the pull-up: 70 kOhm
    - max value for the pull-down: 130 kOhm
    
    Use 60 kOhm in the pinctrl-meson driver as well so it's shown in the
    debugfs output. It may not be accurate for Meson8/8b/8m2 but in reality
    60 kOhm is closer to the actual value than 1 Ohm.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Reviewed-by: Neil Armstrong <neil.armstrong@linaro.org>
    Link: https://lore.kernel.org/20250329190132.855196-1-martin.blumenstingl@googlemail.com
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/meson/pinctrl-meson.c
+++ drivers/pinctrl/meson/pinctrl-meson.c
@@ -487,7 +487,7 @@ static int meson_pinconf_get(struct pinctrl_dev *pcdev, unsigned int pin,
 	case PIN_CONFIG_BIAS_PULL_DOWN:
 	case PIN_CONFIG_BIAS_PULL_UP:
 		if (meson_pinconf_get_pull(pc, pin) == param)
-			arg = 1;
+			arg = 60000;
 		else
 			return -EINVAL;
 		break;
