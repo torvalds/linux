b8417283d98d425da0e4bfef9d9648363f7ce1ee
3 <69488850+mlodyskiny@users.noreply.github.com>
2025-03-30 12:53:49 +0200

    Remove useless directory

--- include/linux/mutex_api.h
+++
@@ -1,1 +0,0 @@
-#include <linux/mutex.h>
--- include/uapi/linux/ioctl.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
-#ifndef _LINUX_IOCTL_H
-#define _LINUX_IOCTL_H
-
-#include <asm/ioctl.h>
-
-#endif /* _LINUX_IOCTL_H */
-
