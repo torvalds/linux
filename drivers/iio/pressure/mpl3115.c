d2de91b3eded57bc02a59ce154080cade7e7917b
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2025-03-31 13:13:07 +0100

    iio: pressure: mpl3115: Switch to sparse friendly iio_device_claim/release_direct()
    These new functions allow sparse to find failures to release
    direct mode reducing chances of bugs over the claim_direct_mode()
    functions that are deprecated.
    
    Reviewed-by: David Lechner <dlechner@baylibre.com>
    Reviewed-by: Marcelo Schmitt <marcelo.schmitt1@gmail.com>
    Link: https://patch.msgid.link/20250331121317.1694135-28-jic23@kernel.org
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/pressure/mpl3115.c
+++ drivers/iio/pressure/mpl3115.c
@@ -124,12 +124,11 @@ static int mpl3115_read_raw(struct iio_dev *indio_dev,
 
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
-		ret = iio_device_claim_direct_mode(indio_dev);
-		if (ret)
-			return ret;
+		if (!iio_device_claim_direct(indio_dev))
+			return -EBUSY;
 
 		ret = mpl3115_read_info_raw(data, chan, val);
-		iio_device_release_direct_mode(indio_dev);
+		iio_device_release_direct(indio_dev);
 		return ret;
 
 	case IIO_CHAN_INFO_SCALE:
