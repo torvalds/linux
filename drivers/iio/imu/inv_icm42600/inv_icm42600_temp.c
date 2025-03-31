cbff19a382bbedd2a247ca9fc52fc1a884f13ac3
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2025-03-31 13:12:55 +0100

    iio: imu: inv_icm42600: Switch to sparse friendly iio_device_claim/release_direct()
    These new functions allow sparse to find failures to release
    direct mode reducing chances of bugs over the claim_direct_mode()
    functions that are deprecated.
    
    Acked-by: Jean-Baptiste Maneyrol <jean-baptiste.maneyrol@tdk.com>
    Reviewed-by: David Lechner <dlechner@baylibre.com>
    Reviewed-by: Marcelo Schmitt <marcelo.schmitt1@gmail.com>
    Link: https://patch.msgid.link/20250331121317.1694135-16-jic23@kernel.org
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/imu/inv_icm42600/inv_icm42600_temp.c
+++ drivers/iio/imu/inv_icm42600/inv_icm42600_temp.c
@@ -56,11 +56,10 @@ int inv_icm42600_temp_read_raw(struct iio_dev *indio_dev,
 
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
-		ret = iio_device_claim_direct_mode(indio_dev);
-		if (ret)
-			return ret;
+		if (!iio_device_claim_direct(indio_dev))
+			return -EBUSY;
 		ret = inv_icm42600_temp_read(st, &temp);
-		iio_device_release_direct_mode(indio_dev);
+		iio_device_release_direct(indio_dev);
 		if (ret)
 			return ret;
 		*val = temp;
