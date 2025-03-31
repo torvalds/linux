ecb75a8985244d5234dd890b98022a6e3fe71df4
Westagger <118185268+Westagger@users.noreply.github.com>
2025-03-31 09:20:26 -0400

    Fix msg_ring.h with header guards
    Added header guards to prevent duplicate inclusions, avoiding redefinition errors and difficulties in compilation and debugging.

--- io_uring/msg_ring.h
+++ io_uring/msg_ring.h
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: GPL-2.0
 
+#ifndef IO_URING_MSG_RING_H
+#define IO_URING_MSG_RING_H
+
+struct io_uring_sqe;
+struct io_kiocb;
+
 int io_uring_sync_msg_ring(struct io_uring_sqe *sqe);
 int io_msg_ring_prep(struct io_kiocb *req, const struct io_uring_sqe *sqe);
 int io_msg_ring(struct io_kiocb *req, unsigned int issue_flags);
 void io_msg_ring_cleanup(struct io_kiocb *req);
+
+#endif
