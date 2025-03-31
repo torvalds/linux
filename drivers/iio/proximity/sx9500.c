0ed4424478d36c8301dd4fbc18a49eafb5e67569
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2025-03-31 13:13:13 +0100

    iio: proximity: sx9500: Switch to sparse friendly iio_device_claim/release_direct()
    These new functions allow sparse to find failures to release
    direct mode reducing chances of bugs over the claim_direct_mode()
    functions that are deprecated.
    
    Reviewed-by: David Lechner <dlechner@baylibre.com>
    Reviewed-by: Marcelo Schmitt <marcelo.schmitt1@gmail.com>
    Link: https://patch.msgid.link/20250331121317.1694135-34-jic23@kernel.org
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/proximity/sx9500.c
+++ drivers/iio/proximity/sx9500.c
@@ -387,11 +387,10 @@ static int sx9500_read_raw(struct iio_dev *indio_dev,
 	case IIO_PROXIMITY:
 		switch (mask) {
 		case IIO_CHAN_INFO_RAW:
-			ret = iio_device_claim_direct_mode(indio_dev);
-			if (ret)
-				return ret;
+			if (!iio_device_claim_direct(indio_dev))
+				return -EBUSY;
 			ret = sx9500_read_proximity(data, chan, val);
-			iio_device_release_direct_mode(indio_dev);
+			iio_device_release_direct(indio_dev);
 			return ret;
 		case IIO_CHAN_INFO_SAMP_FREQ:
 			return sx9500_read_samp_freq(data, val, val2);
