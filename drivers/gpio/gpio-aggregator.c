83c8e3df642f5fde320278a5e6ce9e46f9689d1a
Koichiro Den <koichiro.den@canonical.com>
2025-04-07 13:30:16 +0900

    gpio: aggregator: expose aggregator created via legacy sysfs to configfs
    Expose settings for aggregators created using the sysfs 'new_device'
    interface to configfs. Once written to 'new_device', an "_sysfs.<N>" path
    appears in the configfs regardless of whether the probe succeeds.
    Consequently, users can no longer use that prefix for custom GPIO
    aggregator names. The 'live' attribute changes to 1 when the probe
    succeeds and the GPIO forwarder is instantiated.
    
    Note that the aggregator device created via sysfs is asynchronous, i.e.
    writing into 'new_device' returns without waiting for probe completion,
    and the probe may succeed, fail, or eventually succeed via deferred
    probe. Thus, the 'live' attribute may change from 0 to 1 asynchronously
    without notice. So, editing key/offset/name while it's waiting for
    deferred probe is prohibited.
    
    The configfs auto-generation relies on create_default_group(), which
    inherently prohibits rmdir(2). To align with the limitation, this commit
    also prohibits mkdir(2) for them. When users want to change the number
    of lines for an aggregator initialized via 'new_device', they need to
    tear down the device using 'delete_device' and reconfigure it from
    scratch. This does not break previous behavior; users of legacy sysfs
    interface simply gain additional almost read-only configfs exposure.
    
    Still, users can write to the 'live' attribute to toggle the device
    unless it's waiting for deferred probe. So once probe succeeds, they can
    deactivate it in the same manner as the devices initialized via
    configfs.
    
    Signed-off-by: Koichiro Den <koichiro.den@canonical.com>
    Link: https://lore.kernel.org/r/20250407043019.4105613-7-koichiro.den@canonical.com
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>

--- drivers/gpio/gpio-aggregator.c
+++ drivers/gpio/gpio-aggregator.c
@@ -33,6 +33,7 @@
 #include "dev-sync-probe.h"
 
 #define AGGREGATOR_MAX_GPIOS 512
+#define AGGREGATOR_LEGACY_PREFIX "_sysfs"
 
 /*
  * GPIO Aggregator sysfs interface
@@ -131,6 +132,14 @@ static bool gpio_aggregator_is_active(struct gpio_aggregator *aggr)
 	return aggr->probe_data.pdev && platform_get_drvdata(aggr->probe_data.pdev);
 }
 
+/* Only aggregators created via legacy sysfs can be "activating". */
+static bool gpio_aggregator_is_activating(struct gpio_aggregator *aggr)
+{
+	lockdep_assert_held(&aggr->lock);
+
+	return aggr->probe_data.pdev && !platform_get_drvdata(aggr->probe_data.pdev);
+}
+
 static size_t gpio_aggregator_count_lines(struct gpio_aggregator *aggr)
 {
 	lockdep_assert_held(&aggr->lock);
@@ -187,6 +196,30 @@ static void gpio_aggregator_line_del(struct gpio_aggregator *aggr,
 	lockdep_assert_held(&aggr->lock);
 
 	list_del(&line->entry);
+}
+
+static void gpio_aggregator_free_lines(struct gpio_aggregator *aggr)
+{
+	struct gpio_aggregator_line *line, *tmp;
+
+	list_for_each_entry_safe(line, tmp, &aggr->list_head, entry) {
+		configfs_unregister_group(&line->group);
+		/*
+		 * Normally, we acquire aggr->lock within the configfs
+		 * callback. However, in the legacy sysfs interface case,
+		 * calling configfs_(un)register_group while holding
+		 * aggr->lock could cause a deadlock. Fortunately, this is
+		 * unnecessary because the new_device/delete_device path
+		 * and the module unload path are mutually exclusive,
+		 * thanks to an explicit try_module_get. That's why this
+		 * minimal scoped_guard suffices.
+		 */
+		scoped_guard(mutex, &aggr->lock)
+			gpio_aggregator_line_del(aggr, line);
+		kfree(line->key);
+		kfree(line->name);
+		kfree(line);
+	}
 }
 
 
@@ -701,7 +734,8 @@ gpio_aggregator_line_key_store(struct config_item *item, const char *page,
 
 	guard(mutex)(&aggr->lock);
 
-	if (gpio_aggregator_is_active(aggr))
+	if (gpio_aggregator_is_activating(aggr) ||
+	    gpio_aggregator_is_active(aggr))
 		return -EBUSY;
 
 	kfree(line->key);
@@ -738,7 +772,8 @@ gpio_aggregator_line_name_store(struct config_item *item, const char *page,
 
 	guard(mutex)(&aggr->lock);
 
-	if (gpio_aggregator_is_active(aggr))
+	if (gpio_aggregator_is_activating(aggr) ||
+	    gpio_aggregator_is_active(aggr))
 		return -EBUSY;
 
 	kfree(line->name);
@@ -784,7 +819,8 @@ gpio_aggregator_line_offset_store(struct config_item *item, const char *page,
 
 	guard(mutex)(&aggr->lock);
 
-	if (gpio_aggregator_is_active(aggr))
+	if (gpio_aggregator_is_activating(aggr) ||
+	    gpio_aggregator_is_active(aggr))
 		return -EBUSY;
 
 	line->offset = offset;
@@ -842,11 +878,12 @@ gpio_aggregator_device_live_store(struct config_item *item, const char *page,
 	if (!try_module_get(THIS_MODULE))
 		return -ENOENT;
 
-	if (live)
+	if (live && !aggr->init_via_sysfs)
 		gpio_aggregator_lockup_configfs(aggr, true);
 
 	scoped_guard(mutex, &aggr->lock) {
-		if (live == gpio_aggregator_is_active(aggr))
+		if (gpio_aggregator_is_activating(aggr) ||
+		    (live == gpio_aggregator_is_active(aggr)))
 			ret = -EPERM;
 		else if (live)
 			ret = gpio_aggregator_activate(aggr);
@@ -858,7 +895,7 @@ gpio_aggregator_device_live_store(struct config_item *item, const char *page,
 	 * Undepend is required only if device disablement (live == 0)
 	 * succeeds or if device enablement (live == 1) fails.
 	 */
-	if (live == !!ret)
+	if (live == !!ret && !aggr->init_via_sysfs)
 		gpio_aggregator_lockup_configfs(aggr, false);
 
 	module_put(THIS_MODULE);
@@ -902,7 +939,7 @@ static void gpio_aggregator_device_release(struct config_item *item)
 	struct gpio_aggregator *aggr = to_gpio_aggregator(item);
 
 	/*
-	 * If the aggregator is active, this code wouldn't be reached,
+	 * At this point, aggr is neither active nor activating,
 	 * so calling gpio_aggregator_deactivate() is always unnecessary.
 	 */
 	gpio_aggregator_free(aggr);
@@ -923,6 +960,15 @@ gpio_aggregator_device_make_group(struct config_group *group, const char *name)
 	ret = sscanf(name, "line%u%n", &idx, &nchar);
 	if (ret != 1 || nchar != strlen(name))
 		return ERR_PTR(-EINVAL);
+
+	if (aggr->init_via_sysfs)
+		/*
+		 * Aggregators created via legacy sysfs interface are exposed as
+		 * default groups, which means rmdir(2) is prohibited for them.
+		 * For simplicity, and to avoid confusion, we also prohibit
+		 * mkdir(2).
+		 */
+		return ERR_PTR(-EPERM);
 
 	guard(mutex)(&aggr->lock);
 
@@ -961,6 +1007,14 @@ gpio_aggregator_make_group(struct config_group *group, const char *name)
 	struct gpio_aggregator *aggr;
 	int ret;
 
+	/*
+	 * "_sysfs" prefix is reserved for auto-generated config group
+	 * for devices create via legacy sysfs interface.
+	 */
+	if (strncmp(name, AGGREGATOR_LEGACY_PREFIX,
+		    sizeof(AGGREGATOR_LEGACY_PREFIX)) == 0)
+		return ERR_PTR(-EINVAL);
+
 	/* arg space is unneeded */
 	ret = gpio_aggregator_alloc(&aggr, 0);
 	if (ret)
@@ -996,6 +1050,8 @@ static struct configfs_subsystem gpio_aggregator_subsys = {
 static int gpio_aggregator_parse(struct gpio_aggregator *aggr)
 {
 	char *args = skip_spaces(aggr->args);
+	struct gpio_aggregator_line *line;
+	char name[CONFIGFS_ITEM_NAME_LEN];
 	char *key, *offsets, *p;
 	unsigned int i, n = 0;
 	int error = 0;
@@ -1012,9 +1068,24 @@ static int gpio_aggregator_parse(struct gpio_aggregator *aggr)
 		p = get_options(offsets, 0, &error);
 		if (error == 0 || *p) {
 			/* Named GPIO line */
+			scnprintf(name, sizeof(name), "line%u", n);
+			line = gpio_aggregator_line_alloc(aggr, n, key, -1);
+			if (!line) {
+				error = -ENOMEM;
+				goto err;
+			}
+			config_group_init_type_name(&line->group, name,
+						    &gpio_aggregator_line_type);
+			error = configfs_register_group(&aggr->group,
+							&line->group);
+			if (error)
+				goto err;
+			scoped_guard(mutex, &aggr->lock)
+				gpio_aggregator_line_add(aggr, line);
+
 			error = gpio_aggregator_add_gpio(aggr, key, U16_MAX, &n);
 			if (error)
-				return error;
+				goto err;
 
 			key = offsets;
 			continue;
@@ -1028,9 +1099,24 @@ static int gpio_aggregator_parse(struct gpio_aggregator *aggr)
 		}
 
 		for_each_set_bit(i, bitmap, AGGREGATOR_MAX_GPIOS) {
+			scnprintf(name, sizeof(name), "line%u", n);
+			line = gpio_aggregator_line_alloc(aggr, n, key, i);
+			if (!line) {
+				error = -ENOMEM;
+				goto err;
+			}
+			config_group_init_type_name(&line->group, name,
+						    &gpio_aggregator_line_type);
+			error = configfs_register_group(&aggr->group,
+							&line->group);
+			if (error)
+				goto err;
+			scoped_guard(mutex, &aggr->lock)
+				gpio_aggregator_line_add(aggr, line);
+
 			error = gpio_aggregator_add_gpio(aggr, key, i, &n);
 			if (error)
-				return error;
+				goto err;
 		}
 
 		args = next_arg(args, &key, &p);
@@ -1038,15 +1124,20 @@ static int gpio_aggregator_parse(struct gpio_aggregator *aggr)
 
 	if (!n) {
 		pr_err("No GPIOs specified\n");
-		return -EINVAL;
+		goto err;
 	}
 
 	return 0;
+
+err:
+	gpio_aggregator_free_lines(aggr);
+	return error;
 }
 
 static ssize_t gpio_aggregator_new_device_store(struct device_driver *driver,
 						const char *buf, size_t count)
 {
+	char name[CONFIGFS_ITEM_NAME_LEN];
 	struct gpio_aggregator *aggr;
 	struct platform_device *pdev;
 	int res;
@@ -1075,9 +1166,24 @@ static ssize_t gpio_aggregator_new_device_store(struct device_driver *driver,
 		goto free_table;
 	}
 
-	res = gpio_aggregator_parse(aggr);
+	scnprintf(name, sizeof(name), "%s.%d", AGGREGATOR_LEGACY_PREFIX, aggr->id);
+	config_group_init_type_name(&aggr->group, name, &gpio_aggregator_device_type);
+
+	/*
+	 * Since the device created by sysfs might be toggled via configfs
+	 * 'live' attribute later, this initialization is needed.
+	 */
+	dev_sync_probe_init(&aggr->probe_data);
+
+	/* Expose to configfs */
+	res = configfs_register_group(&gpio_aggregator_subsys.su_group,
+				      &aggr->group);
 	if (res)
 		goto free_dev_id;
+
+	res = gpio_aggregator_parse(aggr);
+	if (res)
+		goto unregister_group;
 
 	gpiod_add_lookup_table(aggr->lookups);
 
@@ -1093,6 +1199,8 @@ static ssize_t gpio_aggregator_new_device_store(struct device_driver *driver,
 
 remove_table:
 	gpiod_remove_lookup_table(aggr->lookups);
+unregister_group:
+	configfs_unregister_group(&aggr->group);
 free_dev_id:
 	kfree(aggr->lookups->dev_id);
 free_table:
@@ -1109,7 +1217,13 @@ static struct driver_attribute driver_attr_gpio_aggregator_new_device =
 
 static void gpio_aggregator_destroy(struct gpio_aggregator *aggr)
 {
-	gpio_aggregator_deactivate(aggr);
+	scoped_guard(mutex, &aggr->lock) {
+		if (gpio_aggregator_is_activating(aggr) ||
+		    gpio_aggregator_is_active(aggr))
+			gpio_aggregator_deactivate(aggr);
+	}
+	gpio_aggregator_free_lines(aggr);
+	configfs_unregister_group(&aggr->group);
 	kfree(aggr);
 }
 
