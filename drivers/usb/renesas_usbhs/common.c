b9cff71c509bd97880e277e798e7dbf8a853192b
Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
2025-04-07 11:50:00 +0100

    usb: renesas_usbhs: Correct function references in comment
    Update the comment to reference `usbhs_mod_probe()` instead of
    `usbhs_mod_init()`, and replace `dev_set_drvdata()` with
    `platform_set_drvdata()`, as these are the correct functions used
    in this context.
    
    Signed-off-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
    Acked-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Reviewed-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Link: https://lore.kernel.org/r/20250407105002.107181-2-prabhakar.mahadev-lad.rj@bp.renesas.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/renesas_usbhs/common.c
+++ drivers/usb/renesas_usbhs/common.c
@@ -698,7 +698,7 @@ static int usbhs_probe(struct platform_device *pdev)
 	if (ret < 0)
 		goto probe_end_fifo_exit;
 
-	/* dev_set_drvdata should be called after usbhs_mod_init */
+	/* platform_set_drvdata() should be called after usbhs_mod_probe() */
 	platform_set_drvdata(pdev, priv);
 
 	ret = reset_control_deassert(priv->rsts);
