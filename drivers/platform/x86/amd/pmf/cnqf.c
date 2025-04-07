fcf27a6a926fd9eeba39e9c3fde43c9298fe284e
Mario Limonciello <mario.limonciello@amd.com>
2025-04-07 13:18:21 -0500

    platform/x86: amd: pmf: Fix STT limits
    On some platforms it has been observed that STT limits are not being
    applied properly causing poor performance as power limits are set too low.
    
    STT limits that are sent to the platform are supposed to be in Q8.8
    format.  Convert them before sending.
    
    Reported-by: Yijun Shen <Yijun.Shen@dell.com>
    Fixes: 7c45534afa443 ("platform/x86/amd/pmf: Add support for PMF Policy Binary")
    Cc: stable@vger.kernel.org
    Tested-by: Yijun Shen <Yijun_Shen@Dell.com>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Acked-by: Shyam Sundar S K <Shyam-sundar.S-k@amd.com>
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Link: https://lore.kernel.org/r/20250407181915.1482450-1-superm1@kernel.org
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>

--- drivers/platform/x86/amd/pmf/cnqf.c
+++ drivers/platform/x86/amd/pmf/cnqf.c
@@ -81,10 +81,10 @@ static int amd_pmf_set_cnqf(struct amd_pmf_dev *dev, int src, int idx,
 	amd_pmf_send_cmd(dev, SET_SPPT, false, pc->sppt, NULL);
 	amd_pmf_send_cmd(dev, SET_SPPT_APU_ONLY, false, pc->sppt_apu_only, NULL);
 	amd_pmf_send_cmd(dev, SET_STT_MIN_LIMIT, false, pc->stt_min, NULL);
-	amd_pmf_send_cmd(dev, SET_STT_LIMIT_APU, false, pc->stt_skin_temp[STT_TEMP_APU],
-			 NULL);
-	amd_pmf_send_cmd(dev, SET_STT_LIMIT_HS2, false, pc->stt_skin_temp[STT_TEMP_HS2],
-			 NULL);
+	amd_pmf_send_cmd(dev, SET_STT_LIMIT_APU, false,
+			 fixp_q88_fromint(pc->stt_skin_temp[STT_TEMP_APU]), NULL);
+	amd_pmf_send_cmd(dev, SET_STT_LIMIT_HS2, false,
+			 fixp_q88_fromint(pc->stt_skin_temp[STT_TEMP_HS2]), NULL);
 
 	if (is_apmf_func_supported(dev, APMF_FUNC_SET_FAN_IDX))
 		apmf_update_fan_idx(dev,
