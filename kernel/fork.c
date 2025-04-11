17f1b08acf50c0bfb02e21623e53e7e575612b67
Christian Brauner <brauner@kernel.org>
2025-04-11 15:22:45 +0200

    pidfs: ensure consistent ENOENT/ESRCH reporting
    In a prior patch series we tried to cleanly differentiate between:
    
    (1) The task has already been reaped.
    (2) The caller requested a pidfd for a thread-group leader but the pid
        actually references a struct pid that isn't used as a thread-group
        leader.
    
    as this was causing issues for non-threaded workloads.
    
    But there's cases where the current simple logic is wrong. Specifically,
    if the pid was a leader pid and the check races with __unhash_process().
    Stabilize this by using the pidfd waitqueue lock.
    
    Link: https://lore.kernel.org/20250411-work-pidfs-enoent-v2-2-60b2d3bb545f@kernel.org
    Reviewed-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2108,27 +2108,25 @@ static int __pidfd_prepare(struct pid *pid, unsigned int flags, struct file **re
  */
 int pidfd_prepare(struct pid *pid, unsigned int flags, struct file **ret)
 {
-	int err = 0;
-
-	if (!(flags & PIDFD_THREAD)) {
-		/*
-		 * If this is struct pid isn't used as a thread-group
-		 * leader pid but the caller requested to create a
-		 * thread-group leader pidfd then report ENOENT to the
-		 * caller as a hint.
-		 */
-		if (!pid_has_task(pid, PIDTYPE_TGID))
-			err = -ENOENT;
-	}
-
 	/*
-	 * If this wasn't a thread-group leader struct pid or the task
-	 * got reaped in the meantime report -ESRCH to userspace.
+	 * While holding the pidfd waitqueue lock removing the task
+	 * linkage for the thread-group leader pid (PIDTYPE_TGID) isn't
+	 * possible. Thus, if there's still task linkage for PIDTYPE_PID
+	 * not having thread-group leader linkage for the pid means it
+	 * wasn't a thread-group leader in the first place.
 	 */
-	if (!pid_has_task(pid, PIDTYPE_PID))
-		err = -ESRCH;
-	if (err)
-		return err;
+	scoped_guard(spinlock_irq, &pid->wait_pidfd.lock) {
+		/* Task has already been reaped. */
+		if (!pid_has_task(pid, PIDTYPE_PID))
+			return -ESRCH;
+		/*
+		 * If this struct pid isn't used as a thread-group
+		 * leader but the caller requested to create a
+		 * thread-group leader pidfd then report ENOENT.
+		 */
+		if (!(flags & PIDFD_THREAD) && !pid_has_task(pid, PIDTYPE_TGID))
+			return -ENOENT;
+	}
 
 	return __pidfd_prepare(pid, flags, ret);
 }
