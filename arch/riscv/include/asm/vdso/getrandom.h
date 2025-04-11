89079520cef65d6da1e864eab4464effe5396e23
Xi Ruoyao <xry111@xry111.site>
2025-04-11 10:46:00 +0800

    RISC-V: vDSO: Wire up getrandom() vDSO implementation
    Hook up the generic vDSO implementation to the generic vDSO getrandom
    implementation by providing the required __arch_chacha20_blocks_nostack
    and getrandom_syscall implementations. Also wire up the selftests.
    
    The benchmark result:
    
    	vdso: 25000000 times in 2.466341333 seconds
    	libc: 25000000 times in 41.447720005 seconds
    	syscall: 25000000 times in 41.043926672 seconds
    
    	vdso: 25000000 x 256 times in 162.286219353 seconds
    	libc: 25000000 x 256 times in 2953.855018685 seconds
    	syscall: 25000000 x 256 times in 2796.268546000 seconds
    
    Signed-off-by: Xi Ruoyao <xry111@xry111.site>
    Link: https://lore.kernel.org/r/20250411024600.16045-1-xry111@xry111.site
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>

---
+++ arch/riscv/include/asm/vdso/getrandom.h
@@ -0,0 +1,30 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2025 Xi Ruoyao <xry111@xry111.site>. All Rights Reserved.
+ */
+#ifndef __ASM_VDSO_GETRANDOM_H
+#define __ASM_VDSO_GETRANDOM_H
+
+#ifndef __ASSEMBLY__
+
+#include <asm/unistd.h>
+
+static __always_inline ssize_t getrandom_syscall(void *_buffer, size_t _len, unsigned int _flags)
+{
+	register long ret asm("a0");
+	register long nr asm("a7") = __NR_getrandom;
+	register void *buffer asm("a0") = _buffer;
+	register size_t len asm("a1") = _len;
+	register unsigned int flags asm("a2") = _flags;
+
+	asm volatile ("ecall\n"
+		      : "+r" (ret)
+		      : "r" (nr), "r" (buffer), "r" (len), "r" (flags)
+		      : "memory");
+
+	return ret;
+}
+
+#endif /* !__ASSEMBLY__ */
+
+#endif /* __ASM_VDSO_GETRANDOM_H */
