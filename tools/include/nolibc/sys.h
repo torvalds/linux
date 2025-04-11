4de88a88bcbe4cf89477d8c6a9145fdfd929bc96
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2025-04-11 11:00:41 +0200

    tools/nolibc: use ppoll_time64 if available
    riscv32 does not have any of the older poll systemcalls.
    Use ppoll_time64 instead.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Acked-by: Willy Tarreau <w@1wt.eu>

--- tools/include/nolibc/sys.h
+++ tools/include/nolibc/sys.h
@@ -772,6 +772,14 @@ int sys_poll(struct pollfd *fds, int nfds, int timeout)
 		t.tv_nsec = (timeout % 1000) * 1000000;
 	}
 	return my_syscall5(__NR_ppoll, fds, nfds, (timeout >= 0) ? &t : NULL, NULL, 0);
+#elif defined(__NR_ppoll_time64)
+	struct __kernel_timespec t;
+
+	if (timeout >= 0) {
+		t.tv_sec  = timeout / 1000;
+		t.tv_nsec = (timeout % 1000) * 1000000;
+	}
+	return my_syscall5(__NR_ppoll_time64, fds, nfds, (timeout >= 0) ? &t : NULL, NULL, 0);
 #elif defined(__NR_poll)
 	return my_syscall3(__NR_poll, fds, nfds, timeout);
 #else
