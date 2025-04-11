34f170a590710737f9611231cd77b37ea41f707a
Herbert Xu <herbert@gondor.apana.org.au>
2025-04-11 15:38:50 +0800

    crypto: simd - Include asm/simd.h in internal/simd.h
    Now that the asm/simd.h files have been made safe against double
    inclusion, include it directly in internal/simd.h.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/internal/simd.h
+++ include/crypto/internal/simd.h
@@ -6,6 +6,7 @@
 #ifndef _CRYPTO_INTERNAL_SIMD_H
 #define _CRYPTO_INTERNAL_SIMD_H
 
+#include <asm/simd.h>
 #include <linux/percpu.h>
 #include <linux/types.h>
 
@@ -46,9 +47,6 @@ void simd_unregister_aeads(struct aead_alg *algs, int count,
  * self-tests, in order to test the no-SIMD fallback code.  This override is
  * currently limited to configurations where the extra self-tests are enabled,
  * because it might be a bit too invasive to be part of the regular self-tests.
- *
- * This is a macro so that <asm/simd.h>, which some architectures don't have,
- * doesn't have to be included directly here.
  */
 #ifdef CONFIG_CRYPTO_MANAGER_EXTRA_TESTS
 DECLARE_PER_CPU(bool, crypto_simd_disabled_for_test);
