8e786a85c0a3c0fffae6244733fb576eeabd9dec
Borislav Petkov (AMD) <bp@alien8.de>
2025-04-14 15:33:19 +0200

    x86/process: Move the buffer clearing before MONITOR
    Move the VERW clearing before the MONITOR so that VERW doesn't disarm it
    and the machine never enters C1.
    
    Original idea by Kim Phillips <kim.phillips@amd.com>.
    
    Suggested-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>

--- arch/x86/kernel/process.c
+++ arch/x86/kernel/process.c
@@ -907,16 +907,24 @@ static __init bool prefer_mwait_c1_over_halt(void)
  */
 static __cpuidle void mwait_idle(void)
 {
+	if (need_resched())
+		return;
+
+	x86_idle_clear_cpu_buffers();
+
 	if (!current_set_polling_and_test()) {
 		const void *addr = &current_thread_info()->flags;
 
 		alternative_input("", "clflush (%[addr])", X86_BUG_CLFLUSH_MONITOR, [addr] "a" (addr));
 		__monitor(addr, 0, 0);
-		if (!need_resched()) {
-			__sti_mwait(0, 0);
-			raw_local_irq_disable();
-		}
+		if (need_resched())
+			goto out;
+
+		__sti_mwait(0, 0);
+		raw_local_irq_disable();
 	}
+
+out:
 	__current_clr_polling();
 }
 
