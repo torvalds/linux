c9b19ea63036fc537a69265acea1b18dabd1cbd3
Marek Szyprowski <m.szyprowski@samsung.com>
2025-04-15 09:56:59 +0200

    dma-mapping: avoid potential unused data compilation warning
    When CONFIG_NEED_DMA_MAP_STATE is not defined, dma-mapping clients might
    report unused data compilation warnings for dma_unmap_*() calls
    arguments. Redefine macros for those calls to let compiler to notice that
    it is okay when the provided arguments are not used.
    
    Reported-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Suggested-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Tested-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Link: https://lore.kernel.org/r/20250415075659.428549-1-m.szyprowski@samsung.com

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -629,10 +629,14 @@ static inline int dma_mmap_wc(struct device *dev,
 #else
 #define DEFINE_DMA_UNMAP_ADDR(ADDR_NAME)
 #define DEFINE_DMA_UNMAP_LEN(LEN_NAME)
-#define dma_unmap_addr(PTR, ADDR_NAME)           (0)
-#define dma_unmap_addr_set(PTR, ADDR_NAME, VAL)  do { } while (0)
-#define dma_unmap_len(PTR, LEN_NAME)             (0)
-#define dma_unmap_len_set(PTR, LEN_NAME, VAL)    do { } while (0)
+#define dma_unmap_addr(PTR, ADDR_NAME)           \
+	({ typeof(PTR) __p __maybe_unused = PTR; 0; })
+#define dma_unmap_addr_set(PTR, ADDR_NAME, VAL)  \
+	do { typeof(PTR) __p __maybe_unused = PTR; } while (0)
+#define dma_unmap_len(PTR, LEN_NAME)             \
+	({ typeof(PTR) __p __maybe_unused = PTR; 0; })
+#define dma_unmap_len_set(PTR, LEN_NAME, VAL)    \
+	do { typeof(PTR) __p __maybe_unused = PTR; } while (0)
 #endif
 
 #endif /* _LINUX_DMA_MAPPING_H */
