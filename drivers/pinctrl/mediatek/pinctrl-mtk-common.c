fe412e3a6c97cfe49ecf4564a278122f7d78e3f0
Chen-Yu Tsai <wenst@chromium.org>
2025-04-15 19:23:37 +0800

    pinctrl: mediatek: common-v1: Fix EINT breakage on older controllers
    When EINT support for multiple addresses was introduced, the driver
    library for the older generations (pinctrl-mtk-common) was not fixed
    together. This resulted in invalid pointer accesses.
    
    Fix up the filled in |struct mtk_eint| in pinctrl-mtk-common to match
    what is now expected by the mtk-eint library.
    
    Reported-by: Uwe Kleine-König <u.kleine-koenig@baylibre.com>
    Tested-by: Uwe Kleine-König <u.kleine-koenig@baylibre.com>
    Closes: https://lore.kernel.org/all/43nd5jxpk7b7fv46frqlfjnqfh5jlpqsemeoakqzd4wdi3df6y@w7ycd3k5ezvn/
    Fixes: 3ef9f710efcb ("pinctrl: mediatek: Add EINT support for multiple addresses")
    Cc: Hao Chang <ot_chhao.chang@mediatek.com>
    Cc: Qingliang Li <qingliang.li@mediatek.com>
    Signed-off-by: Chen-Yu Tsai <wenst@chromium.org>
    Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Link: https://lore.kernel.org/20250415112339.2385454-1-wenst@chromium.org
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/mediatek/pinctrl-mtk-common.c
+++ drivers/pinctrl/mediatek/pinctrl-mtk-common.c
@@ -1015,7 +1015,13 @@ static int mtk_eint_init(struct mtk_pinctrl *pctl, struct platform_device *pdev)
 	if (!pctl->eint)
 		return -ENOMEM;
 
-	pctl->eint->base = devm_platform_ioremap_resource(pdev, 0);
+	pctl->eint->nbase = 1;
+	/* mtk-eint expects an array */
+	pctl->eint->base = devm_kzalloc(pctl->dev, sizeof(pctl->eint->base), GFP_KERNEL);
+	if (IS_ERR(pctl->eint->base))
+		return -ENOMEM;
+
+	pctl->eint->base[0] = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(pctl->eint->base))
 		return PTR_ERR(pctl->eint->base);
 
