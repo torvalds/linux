f406005e162b660dc405b4f18bf7bcb93a515608
Geoffrey D. Bennett <g@b4.vu>
2025-04-17 04:19:23 +0930

    ALSA: usb-audio: Add retry on -EPROTO from usb_set_interface()
    During initialisation of Focusrite USB audio interfaces, -EPROTO is
    sometimes returned from usb_set_interface(), which sometimes prevents
    the device from working: subsequent usb_set_interface() and
    uac_clock_source_is_valid() calls fail.
    
    This patch adds up to 5 retries in endpoint_set_interface(), with a
    delay starting at 5ms and doubling each time. 5 retries was chosen to
    allow for longer than expected waits for the interface to start
    responding correctly; in testing, a single 5ms delay was sufficient to
    fix the issue.
    
    Closes: https://github.com/geoffreybennett/fcp-support/issues/2
    Cc: stable@vger.kernel.org
    Signed-off-by: Geoffrey D. Bennett <g@b4.vu>
    Link: https://patch.msgid.link/Z//7s9dKsmVxHzY2@m.b4.vu
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/usb/endpoint.c
+++ sound/usb/endpoint.c
@@ -926,6 +926,8 @@ static int endpoint_set_interface(struct snd_usb_audio *chip,
 {
 	int altset = set ? ep->altsetting : 0;
 	int err;
+	int retries = 0;
+	const int max_retries = 5;
 
 	if (ep->iface_ref->altset == altset)
 		return 0;
@@ -935,8 +937,13 @@ static int endpoint_set_interface(struct snd_usb_audio *chip,
 
 	usb_audio_dbg(chip, "Setting usb interface %d:%d for EP 0x%x\n",
 		      ep->iface, altset, ep->ep_num);
+retry:
 	err = usb_set_interface(chip->dev, ep->iface, altset);
 	if (err < 0) {
+		if (err == -EPROTO && ++retries <= max_retries) {
+			msleep(5 * (1 << (retries - 1)));
+			goto retry;
+		}
 		usb_audio_err_ratelimited(
 			chip, "%d:%d: usb_set_interface failed (%d)\n",
 			ep->iface, altset, err);
