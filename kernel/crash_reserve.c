f0eba23cb70a945ea0947bdbf0706ca00da50d26
Colin Ian King <colin.i.king@gmail.com>
2025-04-18 13:03:31 +0100

    crash: fix spelling mistake "crahskernel" -> "crashkernel"
    There is a spelling mistake in a pr_warn message. Fix it.
    
    Link: https://lkml.kernel.org/r/20250418120331.535086-1-colin.i.king@gmail.com
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>
    Acked-by: Baoquan He <bhe@redhat.com>
    Cc: Dave Young <dyoung@redhat.com>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/crash_reserve.c
+++ kernel/crash_reserve.c
@@ -131,7 +131,7 @@ static int __init parse_crashkernel_mem(char *cmdline,
 			cur++;
 			*crash_base = memparse(cur, &tmp);
 			if (cur == tmp) {
-				pr_warn("crahskernel: Memory value expected after '@'\n");
+				pr_warn("crashkernel: Memory value expected after '@'\n");
 				return -EINVAL;
 			}
 		}
