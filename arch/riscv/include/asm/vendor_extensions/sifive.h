34e9b16b4b888988730ffab9a9039cfcf305942e
Cyan Yang <cyan.yang@sifive.com>
2025-04-18 13:32:37 +0800

    riscv: Add SiFive xsfvfwmaccqqq vendor extension
    Add SiFive vendor extension "xsfvfwmaccqqq" support to the kernel.
    
    Signed-off-by: Cyan Yang <cyan.yang@sifive.com>
    Link: https://lore.kernel.org/r/20250418053239.4351-11-cyan.yang@sifive.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>

--- arch/riscv/include/asm/vendor_extensions/sifive.h
+++ arch/riscv/include/asm/vendor_extensions/sifive.h
@@ -9,6 +9,7 @@
 #define RISCV_ISA_VENDOR_EXT_XSFVQMACCDOD		0
 #define RISCV_ISA_VENDOR_EXT_XSFVQMACCQOQ		1
 #define RISCV_ISA_VENDOR_EXT_XSFVFNRCLIPXFQF		2
+#define RISCV_ISA_VENDOR_EXT_XSFVFWMACCQQQ		3
 
 extern struct riscv_isa_vendor_ext_data_list riscv_isa_vendor_ext_list_sifive;
 
