45bd443bfd8697a7da308c16c3e75e2bb353b3d1
Dr. David Alan Gilbert <linux@treblig.org>
2025-04-18 02:15:19 +0100

    net: 802: Remove unused p8022 code
    p8022.c defines two external functions, register_8022_client()
    and unregister_8022_client(), the last use of which was removed in
    2018 by
    commit 7a2e838d28cf ("staging: ipx: delete it from the tree")
    
    Remove the p8022.c file, it's corresponding header, and glue
    surrounding it.  There was one place the header was included in vlan.c
    but it didn't use the functions it declared.
    
    There was a comment in net/802/Makefile about checking
    against net/core/Makefile, but that's at least 20 years old and
    there's no sign of net/core/Makefile mentioning it.
    
    Signed-off-by: Dr. David Alan Gilbert <linux@treblig.org>
    Link: https://patch.msgid.link/20250418011519.145320-1-linux@treblig.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/8021q/vlan.c
+++ net/8021q/vlan.c
@@ -23,7 +23,6 @@
 #include <linux/slab.h>
 #include <linux/init.h>
 #include <linux/rculist.h>
-#include <net/p8022.h>
 #include <net/arp.h>
 #include <linux/rtnetlink.h>
 #include <linux/notifier.h>
