a86ee2d74013e071efaa18631f9aa313262e5b1f
Herbert Xu <herbert@gondor.apana.org.au>
2025-04-18 10:59:38 +0800

    crypto: sha1_base - Remove partial block helpers
    Now that all sha1_base users have been converted to use the API
    partial block handling, remove the partial block helpers.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/sha1_base.h
+++ include/crypto/sha1_base.h
@@ -31,44 +31,6 @@ static inline int sha1_base_init(struct shash_desc *desc)
 	return 0;
 }
 
-static inline int sha1_base_do_update(struct shash_desc *desc,
-				      const u8 *data,
-				      unsigned int len,
-				      sha1_block_fn *block_fn)
-{
-	struct sha1_state *sctx = shash_desc_ctx(desc);
-	unsigned int partial = sctx->count % SHA1_BLOCK_SIZE;
-
-	sctx->count += len;
-
-	if (unlikely((partial + len) >= SHA1_BLOCK_SIZE)) {
-		int blocks;
-
-		if (partial) {
-			int p = SHA1_BLOCK_SIZE - partial;
-
-			memcpy(sctx->buffer + partial, data, p);
-			data += p;
-			len -= p;
-
-			block_fn(sctx, sctx->buffer, 1);
-		}
-
-		blocks = len / SHA1_BLOCK_SIZE;
-		len %= SHA1_BLOCK_SIZE;
-
-		if (blocks) {
-			block_fn(sctx, data, blocks);
-			data += blocks * SHA1_BLOCK_SIZE;
-		}
-		partial = 0;
-	}
-	if (len)
-		memcpy(sctx->buffer + partial, data, len);
-
-	return 0;
-}
-
 static inline int sha1_base_do_update_blocks(struct shash_desc *desc,
 					     const u8 *data,
 					     unsigned int len,
@@ -80,29 +42,6 @@ static inline int sha1_base_do_update_blocks(struct shash_desc *desc,
 	sctx->count += len - remain;
 	block_fn(sctx, data, len / SHA1_BLOCK_SIZE);
 	return remain;
-}
-
-static inline int sha1_base_do_finalize(struct shash_desc *desc,
-					sha1_block_fn *block_fn)
-{
-	const int bit_offset = SHA1_BLOCK_SIZE - sizeof(__be64);
-	struct sha1_state *sctx = shash_desc_ctx(desc);
-	__be64 *bits = (__be64 *)(sctx->buffer + bit_offset);
-	unsigned int partial = sctx->count % SHA1_BLOCK_SIZE;
-
-	sctx->buffer[partial++] = 0x80;
-	if (partial > bit_offset) {
-		memset(sctx->buffer + partial, 0x0, SHA1_BLOCK_SIZE - partial);
-		partial = 0;
-
-		block_fn(sctx, sctx->buffer, 1);
-	}
-
-	memset(sctx->buffer + partial, 0x0, bit_offset - partial);
-	*bits = cpu_to_be64(sctx->count << 3);
-	block_fn(sctx, sctx->buffer, 1);
-
-	return 0;
 }
 
 static inline int sha1_base_do_finup(struct shash_desc *desc,
