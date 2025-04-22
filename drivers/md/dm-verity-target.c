f9ed31214e2ac43cd38d1e517e774050b613b8da
Eric Biggers <ebiggers@google.com>
2025-04-22 13:22:05 -0700

    dm-verity: use softirq context only when !need_resched()
    Further limit verification in softirq (a.k.a. BH) context to cases where
    rescheduling of the interrupted task is not pending.
    
    This helps prevent the CPU from spending too long in softirq context.
    
    Note that handle_softirqs() in kernel/softirq.c already stops running
    softirqs in this same case.  However, that check is too coarse-grained,
    since many I/O requests can be processed in a single BLOCK_SOFTIRQ.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Mikulas Patocka <mpatocka@redhat.com>

--- drivers/md/dm-verity-target.c
+++ drivers/md/dm-verity-target.c
@@ -682,7 +682,8 @@ static void verity_bh_work(struct work_struct *w)
 static inline bool verity_use_bh(unsigned int bytes, unsigned short ioprio)
 {
 	return ioprio <= IOPRIO_CLASS_IDLE &&
-		bytes <= READ_ONCE(dm_verity_use_bh_bytes[ioprio]);
+		bytes <= READ_ONCE(dm_verity_use_bh_bytes[ioprio]) &&
+		!need_resched();
 }
 
 static void verity_end_io(struct bio *bio)
