2c809080d296500d6ceed516f86dfa5e4e8f25bc
Animesh Manna <animesh.manna@intel.com>
2025-04-23 14:53:28 +0530

    drm/i915/lobf: Add fixed refresh rate check in compute_config()
    LOBF can be enabled with vrr fixed rate mode, so add check
    if vmin = vmax = flipline in compute_config().
    
    Signed-off-by: Animesh Manna <animesh.manna@intel.com>
    Reviewed-by: Jouni Högander <jouni.hogander@intel.com>
    Link: https://lore.kernel.org/r/20250423092334.2294483-6-animesh.manna@intel.com

--- drivers/gpu/drm/i915/display/intel_alpm.c
+++ drivers/gpu/drm/i915/display/intel_alpm.c
@@ -290,6 +290,10 @@ void intel_alpm_lobf_compute_config(struct intel_dp *intel_dp,
 	if (crtc_state->has_psr)
 		return;
 
+	if (crtc_state->vrr.vmin != crtc_state->vrr.vmax ||
+	    crtc_state->vrr.vmin != crtc_state->vrr.flipline)
+		return;
+
 	if (!(intel_alpm_aux_wake_supported(intel_dp) ||
 	      intel_alpm_aux_less_wake_supported(intel_dp)))
 		return;
