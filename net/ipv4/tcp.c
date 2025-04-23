2b13042d3636327eb50c8a0ee06f629d52d1b8fb
Jeremy Harris <jgh@exim.org>
2025-04-23 13:43:34 +0100

    tcp: fastopen: pass TFO child indication through getsockopt
    tcp: fastopen: pass TFO child indication through getsockopt
    
    Note that this uses up the last bit of a field in struct tcp_info
    
    Signed-off-by: Jeremy Harris <jgh@exim.org>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Neal Cardwell <ncardwell@google.com>
    Link: https://patch.msgid.link/20250423124334.4916-3-jgh@exim.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -4165,6 +4165,8 @@ void tcp_get_info(struct sock *sk, struct tcp_info *info)
 		info->tcpi_options |= TCPI_OPT_SYN_DATA;
 	if (tp->tcp_usec_ts)
 		info->tcpi_options |= TCPI_OPT_USEC_TS;
+	if (tp->syn_fastopen_child)
+		info->tcpi_options |= TCPI_OPT_TFO_CHILD;
 
 	info->tcpi_rto = jiffies_to_usecs(icsk->icsk_rto);
 	info->tcpi_ato = jiffies_to_usecs(min_t(u32, icsk->icsk_ack.ato,
