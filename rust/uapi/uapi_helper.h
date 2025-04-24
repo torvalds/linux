cdeaeb9dd762d7711241a62459dfb730b2cd0281
Danilo Krummrich <dakr@kernel.org>
2025-04-24 18:02:50 +0200

    drm: nova-drm: add initial driver skeleton
    Add the initial nova-drm driver skeleton.
    
    nova-drm is connected to nova-core through the auxiliary bus and
    implements the DRM parts of the nova driver stack.
    
    For now, it implements the fundamental DRM abstractions, i.e. creates a
    DRM device and registers it, exposing a three sample IOCTLs.
    
      DRM_IOCTL_NOVA_GETPARAM
        - provides the PCI bar size from the bar that maps the GPUs VRAM
          from nova-core
    
      DRM_IOCTL_NOVA_GEM_CREATE
        - creates a new dummy DRM GEM object and returns a handle
    
      DRM_IOCTL_NOVA_GEM_INFO
        - provides metadata for the DRM GEM object behind a given handle
    
    I implemented a small userspace test suite [1] that utilizes this
    interface.
    
    Link: https://gitlab.freedesktop.org/dakr/drm-test [1]
    Reviewed-by: Maxime Ripard <mripard@kernel.org>
    Acked-by: Dave Airlie <airlied@redhat.com>
    Link: https://lore.kernel.org/r/20250424160452.8070-3-dakr@kernel.org
    [ Kconfig: depend on DRM=y rather than just DRM. - Danilo ]
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- rust/uapi/uapi_helper.h
+++ rust/uapi/uapi_helper.h
@@ -8,6 +8,7 @@
 
 #include <uapi/asm-generic/ioctl.h>
 #include <uapi/drm/drm.h>
+#include <uapi/drm/nova_drm.h>
 #include <uapi/linux/mdio.h>
 #include <uapi/linux/mii.h>
 #include <uapi/linux/ethtool.h>
