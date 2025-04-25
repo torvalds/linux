54ce9aa5829fe9b7a316cb4b56ecd2e86aedd2d9
Ulf Hansson <ulf.hansson@linaro.org>
2025-04-25 13:39:42 +0200

    pmdomain: core: Convert to device_awake_path()
    As device_wakeup_path() is intended to be removed, let's switch to use the
    device_awake_path() instead. No functional change.
    
    Acked-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Link: https://lore.kernel.org/r/20250425113942.134458-1-ulf.hansson@linaro.org

--- drivers/pmdomain/core.c
+++ drivers/pmdomain/core.c
@@ -1508,7 +1508,7 @@ static int genpd_finish_suspend(struct device *dev,
 	if (ret)
 		return ret;
 
-	if (device_wakeup_path(dev) && genpd_is_active_wakeup(genpd))
+	if (device_awake_path(dev) && genpd_is_active_wakeup(genpd))
 		return 0;
 
 	if (genpd->dev_ops.stop && genpd->dev_ops.start &&
@@ -1563,7 +1563,7 @@ static int genpd_finish_resume(struct device *dev,
 	if (IS_ERR(genpd))
 		return -EINVAL;
 
-	if (device_wakeup_path(dev) && genpd_is_active_wakeup(genpd))
+	if (device_awake_path(dev) && genpd_is_active_wakeup(genpd))
 		return resume_noirq(dev);
 
 	genpd_lock(genpd);
