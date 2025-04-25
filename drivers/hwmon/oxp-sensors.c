217d55ca13d22ba6af7e96ac2d28c2ef6927fc54
Antheas Kapenekakis <lkml@antheas.dev>
2025-04-25 13:18:06 +0200

    hwmon: (oxp-sensors) Distinguish the X1 variants
    Currently, the oxp-sensors driver fuzzy matches the X1 variants. Luckily,
    X1 and X1 mini share most hardware features so this works. However, they
    are completely different product lines, and there is an expectation that
    OneXPlayer will release more devices in the X1 line that may have
    differences.
    
    Therefore, distinguish the 3 devices that currently exist in the market.
    These are the OneXPlayer X1 AMD and Intel variants, and the X1 mini which
    only has an AMD variant. As far as registers go, all three support the
    current driver functionality.
    
    Reviewed-by: Derek J. Clark <derekjohn.clark@gmail.com>
    Acked-by: Guenter Roeck <linux@roeck-us.net>
    Reviewed-by: Thomas Weißschuh <linux@weissschuh.net>
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Antheas Kapenekakis <lkml@antheas.dev>
    Link: https://lore.kernel.org/r/20250425111821.88746-2-lkml@antheas.dev
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>

--- drivers/hwmon/oxp-sensors.c
+++ drivers/hwmon/oxp-sensors.c
@@ -205,7 +205,28 @@ static const struct dmi_system_id dmi_table[] = {
 	{
 		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "ONE-NETBOOK"),
-			DMI_MATCH(DMI_BOARD_NAME, "ONEXPLAYER X1"),
+			DMI_EXACT_MATCH(DMI_BOARD_NAME, "ONEXPLAYER X1 A"),
+		},
+		.driver_data = (void *)oxp_x1,
+	},
+	{
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "ONE-NETBOOK"),
+			DMI_EXACT_MATCH(DMI_BOARD_NAME, "ONEXPLAYER X1 i"),
+		},
+		.driver_data = (void *)oxp_x1,
+	},
+	{
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "ONE-NETBOOK"),
+			DMI_EXACT_MATCH(DMI_BOARD_NAME, "ONEXPLAYER X1 mini"),
+		},
+		.driver_data = (void *)oxp_x1,
+	},
+	{
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "ONE-NETBOOK"),
+			DMI_EXACT_MATCH(DMI_BOARD_NAME, "ONEXPLAYER X1Pro"),
 		},
 		.driver_data = (void *)oxp_x1,
 	},
