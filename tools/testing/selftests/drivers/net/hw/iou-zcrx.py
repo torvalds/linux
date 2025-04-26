187e0216366f3573c894514cf41267df843efd49
David Wei <dw@davidwei.uk>
2025-04-26 12:55:24 -0700

    io_uring/zcrx: selftests: use rand_port()
    Use rand_port() and stop hard coding port 9999.
    
    Signed-off-by: David Wei <dw@davidwei.uk>
    Reviewed-by: Joe Damato <jdamato@fastly.com>
    Link: https://patch.msgid.link/20250426195525.1906774-2-dw@davidwei.uk
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- tools/testing/selftests/drivers/net/hw/iou-zcrx.py
+++ tools/testing/selftests/drivers/net/hw/iou-zcrx.py
@@ -5,7 +5,7 @@ import re
 from os import path
 from lib.py import ksft_run, ksft_exit
 from lib.py import NetDrvEpEnv
-from lib.py import bkg, cmd, defer, ethtool, wait_port_listen
+from lib.py import bkg, cmd, defer, ethtool, rand_port, wait_port_listen
 
 
 def _get_current_settings(cfg):
@@ -28,14 +28,14 @@ def _create_rss_ctx(cfg, chans):
     return (ctx_id, defer(ethtool, f"-X {cfg.ifname} delete context {ctx_id}", host=cfg.remote))
 
 
-def _set_flow_rule(cfg, chan):
-    output = ethtool(f"-N {cfg.ifname} flow-type tcp6 dst-port 9999 action {chan}", host=cfg.remote).stdout
+def _set_flow_rule(cfg, port, chan):
+    output = ethtool(f"-N {cfg.ifname} flow-type tcp6 dst-port {port} action {chan}", host=cfg.remote).stdout
     values = re.search(r'ID (\d+)', output).group(1)
     return int(values)
 
 
-def _set_flow_rule_rss(cfg, chan):
-    output = ethtool(f"-N {cfg.ifname} flow-type tcp6 dst-port 9999 action {chan}", host=cfg.remote).stdout
+def _set_flow_rule_rss(cfg, port, chan):
+    output = ethtool(f"-N {cfg.ifname} flow-type tcp6 dst-port {port} action {chan}", host=cfg.remote).stdout
     values = re.search(r'ID (\d+)', output).group(1)
     return int(values)
 
@@ -47,22 +47,27 @@ def test_zcrx(cfg) -> None:
     if combined_chans < 2:
         raise KsftSkipEx('at least 2 combined channels required')
     (rx_ring, hds_thresh) = _get_current_settings(cfg)
+    port = rand_port()
 
     ethtool(f"-G {cfg.ifname} tcp-data-split on", host=cfg.remote)
     defer(ethtool, f"-G {cfg.ifname} tcp-data-split auto", host=cfg.remote)
+
     ethtool(f"-G {cfg.ifname} hds-thresh 0", host=cfg.remote)
     defer(ethtool, f"-G {cfg.ifname} hds-thresh {hds_thresh}", host=cfg.remote)
+
     ethtool(f"-G {cfg.ifname} rx 64", host=cfg.remote)
     defer(ethtool, f"-G {cfg.ifname} rx {rx_ring}", host=cfg.remote)
+
     ethtool(f"-X {cfg.ifname} equal {combined_chans - 1}", host=cfg.remote)
     defer(ethtool, f"-X {cfg.ifname} default", host=cfg.remote)
-    flow_rule_id = _set_flow_rule(cfg, combined_chans - 1)
+
+    flow_rule_id = _set_flow_rule(cfg, port, combined_chans - 1)
     defer(ethtool, f"-N {cfg.ifname} delete {flow_rule_id}", host=cfg.remote)
 
-    rx_cmd = f"{cfg.bin_remote} -s -p 9999 -i {cfg.ifname} -q {combined_chans - 1}"
-    tx_cmd = f"{cfg.bin_local} -c -h {cfg.remote_addr_v['6']} -p 9999 -l 12840"
+    rx_cmd = f"{cfg.bin_remote} -s -p {port} -i {cfg.ifname} -q {combined_chans - 1}"
+    tx_cmd = f"{cfg.bin_local} -c -h {cfg.remote_addr_v['6']} -p {port} -l 12840"
     with bkg(rx_cmd, host=cfg.remote, exit_wait=True):
-        wait_port_listen(9999, proto="tcp", host=cfg.remote)
+        wait_port_listen(port, proto="tcp", host=cfg.remote)
         cmd(tx_cmd)
 
 
@@ -73,22 +78,27 @@ def test_zcrx_oneshot(cfg) -> None:
     if combined_chans < 2:
         raise KsftSkipEx('at least 2 combined channels required')
     (rx_ring, hds_thresh) = _get_current_settings(cfg)
+    port = rand_port()
 
     ethtool(f"-G {cfg.ifname} tcp-data-split on", host=cfg.remote)
     defer(ethtool, f"-G {cfg.ifname} tcp-data-split auto", host=cfg.remote)
+
     ethtool(f"-G {cfg.ifname} hds-thresh 0", host=cfg.remote)
     defer(ethtool, f"-G {cfg.ifname} hds-thresh {hds_thresh}", host=cfg.remote)
+
     ethtool(f"-G {cfg.ifname} rx 64", host=cfg.remote)
     defer(ethtool, f"-G {cfg.ifname} rx {rx_ring}", host=cfg.remote)
+
     ethtool(f"-X {cfg.ifname} equal {combined_chans - 1}", host=cfg.remote)
     defer(ethtool, f"-X {cfg.ifname} default", host=cfg.remote)
-    flow_rule_id = _set_flow_rule(cfg, combined_chans - 1)
+
+    flow_rule_id = _set_flow_rule(cfg, port, combined_chans - 1)
     defer(ethtool, f"-N {cfg.ifname} delete {flow_rule_id}", host=cfg.remote)
 
-    rx_cmd = f"{cfg.bin_remote} -s -p 9999 -i {cfg.ifname} -q {combined_chans - 1} -o 4"
-    tx_cmd = f"{cfg.bin_local} -c -h {cfg.remote_addr_v['6']} -p 9999 -l 4096 -z 16384"
+    rx_cmd = f"{cfg.bin_remote} -s -p {port} -i {cfg.ifname} -q {combined_chans - 1} -o 4"
+    tx_cmd = f"{cfg.bin_local} -c -h {cfg.remote_addr_v['6']} -p {port} -l 4096 -z 16384"
     with bkg(rx_cmd, host=cfg.remote, exit_wait=True):
-        wait_port_listen(9999, proto="tcp", host=cfg.remote)
+        wait_port_listen(port, proto="tcp", host=cfg.remote)
         cmd(tx_cmd)
 
 
@@ -99,24 +109,28 @@ def test_zcrx_rss(cfg) -> None:
     if combined_chans < 2:
         raise KsftSkipEx('at least 2 combined channels required')
     (rx_ring, hds_thresh) = _get_current_settings(cfg)
+    port = rand_port()
 
     ethtool(f"-G {cfg.ifname} tcp-data-split on", host=cfg.remote)
     defer(ethtool, f"-G {cfg.ifname} tcp-data-split auto", host=cfg.remote)
+
     ethtool(f"-G {cfg.ifname} hds-thresh 0", host=cfg.remote)
     defer(ethtool, f"-G {cfg.ifname} hds-thresh {hds_thresh}", host=cfg.remote)
+
     ethtool(f"-G {cfg.ifname} rx 64", host=cfg.remote)
     defer(ethtool, f"-G {cfg.ifname} rx {rx_ring}", host=cfg.remote)
+
     ethtool(f"-X {cfg.ifname} equal {combined_chans - 1}", host=cfg.remote)
     defer(ethtool, f"-X {cfg.ifname} default", host=cfg.remote)
 
     (ctx_id, delete_ctx) = _create_rss_ctx(cfg, combined_chans)
-    flow_rule_id = _set_flow_rule_rss(cfg, ctx_id)
+    flow_rule_id = _set_flow_rule_rss(cfg, port, ctx_id)
     defer(ethtool, f"-N {cfg.ifname} delete {flow_rule_id}", host=cfg.remote)
 
-    rx_cmd = f"{cfg.bin_remote} -s -p 9999 -i {cfg.ifname} -q {combined_chans - 1}"
-    tx_cmd = f"{cfg.bin_local} -c -h {cfg.remote_addr_v['6']} -p 9999 -l 12840"
+    rx_cmd = f"{cfg.bin_remote} -s -p {port} -i {cfg.ifname} -q {combined_chans - 1}"
+    tx_cmd = f"{cfg.bin_local} -c -h {cfg.remote_addr_v['6']} -p {port} -l 12840"
     with bkg(rx_cmd, host=cfg.remote, exit_wait=True):
-        wait_port_listen(9999, proto="tcp", host=cfg.remote)
+        wait_port_listen(port, proto="tcp", host=cfg.remote)
         cmd(tx_cmd)
 
 
