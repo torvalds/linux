5e7392dc82ed7bf3e734cbca77a1c6fd044ec871
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2025-04-28 14:40:15 +0200

    tools/nolibc: fall back to sys_clock_gettime() in gettimeofday()
    Newer architectures (like riscv32) do not implement sys_gettimeofday().
    In those cases fall back to sys_clock_gettime().
    While that does not support the timezone argument of sys_gettimeofday(),
    specifying this argument invokes undefined behaviour, so it's safe to ignore.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Acked-by: Willy Tarreau <w@1wt.eu>
    Link: https://lore.kernel.org/r/20250428-nolibc-misc-v2-14-3c043eeab06c@linutronix.de
    Signed-off-by: Thomas Weißschuh <linux@weissschuh.net>

--- tools/include/nolibc/sys/time.h
+++ tools/include/nolibc/sys/time.h
@@ -13,6 +13,8 @@
 #include "../arch.h"
 #include "../sys.h"
 
+static int sys_clock_gettime(clockid_t clockid, struct timespec *tp);
+
 /*
  * int gettimeofday(struct timeval *tv, struct timezone *tz);
  */
@@ -23,7 +25,18 @@ int sys_gettimeofday(struct timeval *tv, struct timezone *tz)
 #ifdef __NR_gettimeofday
 	return my_syscall2(__NR_gettimeofday, tv, tz);
 #else
-	return __nolibc_enosys(__func__, tv, tz);
+	(void) tz; /* Non-NULL tz is undefined behaviour */
+
+	struct timespec tp;
+	int ret;
+
+	ret = sys_clock_gettime(CLOCK_REALTIME, &tp);
+	if (!ret && tv) {
+		tv->tv_sec = tp.tv_sec;
+		tv->tv_usec = tp.tv_nsec / 1000;
+	}
+
+	return ret;
 #endif
 }
 
