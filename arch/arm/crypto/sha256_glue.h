ca4477e41c68b58043e67bc78074cd6fcc59ee5e
Eric Biggers <ebiggers@google.com>
2025-04-28 10:00:27 -0700

    crypto: arm/sha256 - implement library instead of shash
    Instead of providing crypto_shash algorithms for the arch-optimized
    SHA-256 code, instead implement the SHA-256 library.  This is much
    simpler, it makes the SHA-256 library functions be arch-optimized, and
    it fixes the longstanding issue where the arch-optimized SHA-256 was
    disabled by default.  SHA-256 still remains available through
    crypto_shash, but individual architectures no longer need to handle it.
    
    To merge the scalar, NEON, and CE code all into one module cleanly, add
    !CPU_V7M as a direct dependency of the CE code.  Previously, !CPU_V7M
    was only a direct dependency of the scalar and NEON code.  The result is
    still the same because CPU_V7M implies !KERNEL_MODE_NEON, so !CPU_V7M
    was already an indirect dependency of the CE code.
    
    To match sha256_blocks_arch(), change the type of the nblocks parameter
    of the assembly functions from int to size_t.  The assembly functions
    actually already treated it as size_t.
    
    While renaming the assembly files, also fix the naming quirk where
    "sha2" meant sha256.  (SHA-512 is also part of SHA-2.)
    
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- arch/arm/crypto/sha256_glue.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _CRYPTO_SHA256_GLUE_H
-#define _CRYPTO_SHA256_GLUE_H
-
-#include <crypto/hash.h>
-
-extern struct shash_alg sha256_neon_algs[2];
-
-#endif /* _CRYPTO_SHA256_GLUE_H */
