d8ca84d48a2a2d4f6780980743c34b70c49f5844
Su Hui <suhui@nfschina.com>
2025-04-30 11:27:33 +0800

    alarmtimer: Remove dead return value in clock2alarm()
    'clockid' can only be ALARM_REALTIME and ALARM_BOOTTIME. It's impossible to
    return -1 and callers never check the return value.
    
    Only alarm_clock_get_timespec(), alarm_clock_get_ktime(),
    alarm_timer_create() and alarm_timer_nsleep() call clock2alarm(). These
    callers use clockid_to_kclock() to get 'struct k_clock', which ensures
    that clock2alarm() never returns -1.
    
    Remove the impossible -1 return value, and add a warning to notify about any
    future misuse of this function.
    
    Signed-off-by: Su Hui <suhui@nfschina.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/all/20250430032734.2079290-3-suhui@nfschina.com

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -515,9 +515,9 @@ static enum alarmtimer_type clock2alarm(clockid_t clockid)
 {
 	if (clockid == CLOCK_REALTIME_ALARM)
 		return ALARM_REALTIME;
-	if (clockid == CLOCK_BOOTTIME_ALARM)
-		return ALARM_BOOTTIME;
-	return -1;
+
+	WARN_ON_ONCE(clockid != CLOCK_BOOTTIME_ALARM);
+	return ALARM_BOOTTIME;
 }
 
 /**
