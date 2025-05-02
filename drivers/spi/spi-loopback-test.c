233d740e3a819829ccd6d21319015a94349d64eb
Geert Uytterhoeven <geert+renesas@glider.be>
2025-05-02 13:12:39 +0200

    spi: loopback-test: Simplify strange loopback value check
    Apply De Morgan's Theorem and drop superfluous parentheses to simplify
    the check for strange loopback values.
    While at it, add the missing zero in the related comment.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://patch.msgid.link/da612090f543c8c7cc99fb9dc6ef4abc9560abe4.1746184293.git.geert+renesas@glider.be
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-loopback-test.c
+++ drivers/spi/spi-loopback-test.c
@@ -635,8 +635,8 @@ static int spi_test_check_loopback_result(struct spi_device *spi,
 		} else {
 			/* first byte received */
 			txb = ((u8 *)xfer->rx_buf)[0];
-			/* first byte may be 0 or xff */
-			if (!((txb == 0) || (txb == 0xff))) {
+			/* first byte may be 0 or 0xff */
+			if (txb != 0 && txb != 0xff) {
 				dev_err(&spi->dev,
 					"loopback strangeness - we expect 0x00 or 0xff, but not 0x%02x\n",
 					txb);
