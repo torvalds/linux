0cecd37daef3d57e6656c0023978d5ec2d7409c1
Kees Cook <kees@kernel.org>
2025-05-03 11:46:18 -0700

    gcc-plugins: Force full rebuild when plugins change
    There was no dependency between the plugins changing and the rest of the
    kernel being built. This could cause strange behaviors as instrumentation
    could vary between targets depending on when they were built.
    
    Generate a new header file, gcc-plugins.h, any time the GCC plugins
    change. Include the header file in compiler-version.h when its associated
    feature name, GCC_PLUGINS, is defined. This will be picked up by fixdep
    and force rebuilds where needed.
    
    Add a generic "touch" kbuild command, which will be used again in
    a following patch. Add a "normalize_path" string helper to make the
    "TOUCH" output less ugly.
    
    Link: https://lore.kernel.org/r/20250503184623.2572355-1-kees@kernel.org
    Tested-by: Nicolas Schier <n.schier@avm.de>
    Reviewed-by: Nicolas Schier <n.schier@avm.de>
    Signed-off-by: Kees Cook <kees@kernel.org>

--- include/linux/compiler-version.h
+++ include/linux/compiler-version.h
@@ -12,3 +12,14 @@
  * and add dependency on include/config/CC_VERSION_TEXT, which is touched
  * by Kconfig when the version string from the compiler changes.
  */
+
+/* Additional tree-wide dependencies start here. */
+
+/*
+ * If any of the GCC plugins change, we need to rebuild everything that
+ * was built with them, as they may have changed their behavior and those
+ * behaviors may need to be synchronized across all translation units.
+ */
+#ifdef GCC_PLUGINS
+#include <generated/gcc-plugins.h>
+#endif
