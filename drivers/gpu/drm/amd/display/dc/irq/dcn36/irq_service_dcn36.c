7e340d3ceacf75e3e088fcc3dd4f8729d6c079c3
Sebastian Aguilera Novoa <saguileran@ime.usp.br>
2025-05-03 00:59:46 -0300

    drm/amd/display/dc/irq: Remove duplications of hpd_ack function from IRQ
    The major of dcn and dce irqs share a copy-pasted collection
    of copy-pasted function, which is: hpd_ack.
    
    This patch removes the multiple copy-pasted by moving them to
    the irq_service.c and make the irq_service's
    calls the functions implemented by the irq_service.c
    instead.
    
    The hpd_ack function is replaced by hpd0_ack and hpd1_ack, the
    required constants are also added.
    
    The changes were not tested on actual hardware. I am only able
    to verify that the changes keep the code compileable and do my
    best to look repeatedly if I am not actually changing any code.
    
    Signed-off-by: Sebastian Aguilera Novoa <saguileran@ime.usp.br>
    Reviewed-by: Alex Hung <alex.hung@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/irq/dcn36/irq_service_dcn36.c
+++ drivers/gpu/drm/amd/display/dc/irq/dcn36/irq_service_dcn36.c
@@ -105,36 +105,9 @@ static enum dc_irq_source to_dal_irq_source_dcn36(
 	}
 }
 
-static bool hpd_ack(
-	struct irq_service *irq_service,
-	const struct irq_source_info *info)
-{
-	uint32_t addr = info->status_reg;
-	uint32_t value = dm_read_reg(irq_service->ctx, addr);
-	uint32_t current_status =
-		get_reg_field_value(
-			value,
-			HPD0_DC_HPD_INT_STATUS,
-			DC_HPD_SENSE_DELAYED);
-
-	dal_irq_service_ack_generic(irq_service, info);
-
-	value = dm_read_reg(irq_service->ctx, info->enable_reg);
-
-	set_reg_field_value(
-		value,
-		current_status ? 0 : 1,
-		HPD0_DC_HPD_INT_CONTROL,
-		DC_HPD_INT_POLARITY);
-
-	dm_write_reg(irq_service->ctx, info->enable_reg, value);
-
-	return true;
-}
-
 static struct irq_source_info_funcs hpd_irq_info_funcs = {
 	.set = NULL,
-	.ack = hpd_ack
+	.ack = hpd0_ack
 };
 
 static struct irq_source_info_funcs hpd_rx_irq_info_funcs = {
