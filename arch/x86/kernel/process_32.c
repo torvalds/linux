730faa15a069f4025a0f8c2a5244c3067da7ecbe
Oleg Nesterov <oleg@redhat.com>
2025-05-03 16:38:30 +0200

    x86/fpu: Simplify the switch_fpu_prepare() + switch_fpu_finish() logic
    Now that switch_fpu_finish() doesn't load the FPU state, it makes more
    sense to fold it into switch_fpu_prepare() renamed to switch_fpu(), and
    more importantly, use the "prev_p" task as a target for TIF_NEED_FPU_LOAD.
    It doesn't make any sense to delay set_tsk_thread_flag(TIF_NEED_FPU_LOAD)
    until "prev_p" is scheduled again.
    
    There is no worry about the very first context switch, fpu_clone() must
    always set TIF_NEED_FPU_LOAD.
    
    Also, shift the test_tsk_thread_flag(TIF_NEED_FPU_LOAD) from the callers
    to switch_fpu().
    
    Note that the "PF_KTHREAD | PF_USER_WORKER" check can be removed but
    this deserves a separate patch which can change more functions, say,
    kernel_fpu_begin_mask().
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Cc: Chang S . Bae <chang.seok.bae@intel.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Brian Gerst <brgerst@gmail.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20250503143830.GA8982@redhat.com

--- arch/x86/kernel/process_32.c
+++ arch/x86/kernel/process_32.c
@@ -160,8 +160,7 @@ __switch_to(struct task_struct *prev_p, struct task_struct *next_p)
 
 	/* never put a printk in __switch_to... printk() calls wake_up*() indirectly */
 
-	if (!test_tsk_thread_flag(prev_p, TIF_NEED_FPU_LOAD))
-		switch_fpu_prepare(prev_p, cpu);
+	switch_fpu(prev_p, cpu);
 
 	/*
 	 * Save away %gs. No need to save %fs, as it was saved on the
@@ -207,8 +206,6 @@ __switch_to(struct task_struct *prev_p, struct task_struct *next_p)
 		loadsegment(gs, next->gs);
 
 	raw_cpu_write(current_task, next_p);
-
-	switch_fpu_finish(next_p);
 
 	/* Load the Intel cache allocation PQR MSR. */
 	resctrl_sched_in(next_p);
