3fea0641b06ff4e53d95d07a96764d8951d4ced6
Chao Yu <chao@kernel.org>
2025-05-06 15:47:25 +0800

    f2fs: sysfs: add encoding_flags entry
    This patch adds a new sysfs entry /sys/fs/f2fs/<disk>/encoding_flags,
    it is a read-only entry to show the value of sb.s_encoding_flags, the
    value is hexadecimal.
    
    ============================     ==========
    Flag_Name                        Flag_Value
    ============================     ==========
    SB_ENC_STRICT_MODE_FL            0x00000001
    SB_ENC_NO_COMPAT_FALLBACK_FL     0x00000002
    ============================     ==========
    
    case#1
    mkfs.f2fs -f -O casefold -C utf8:strict /dev/vda
    mount /dev/vda /mnt/f2fs
    cat /sys/fs/f2fs/vda/encoding_flags
    1
    
    case#2
    mkfs.f2fs -f -O casefold -C utf8 /dev/vda
    fsck.f2fs --nolinear-lookup=1 /dev/vda
    mount /dev/vda /mnt/f2fs
    cat /sys/fs/f2fs/vda/encoding_flags
    2
    
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/sysfs.c
+++ fs/f2fs/sysfs.c
@@ -274,6 +274,13 @@ static ssize_t encoding_show(struct f2fs_attr *a,
 	return sysfs_emit(buf, "(none)\n");
 }
 
+static ssize_t encoding_flags_show(struct f2fs_attr *a,
+		struct f2fs_sb_info *sbi, char *buf)
+{
+	return sysfs_emit(buf, "%x\n",
+		le16_to_cpu(F2FS_RAW_SUPER(sbi)->s_encoding_flags));
+}
+
 static ssize_t mounted_time_sec_show(struct f2fs_attr *a,
 		struct f2fs_sb_info *sbi, char *buf)
 {
@@ -1158,6 +1165,7 @@ F2FS_GENERAL_RO_ATTR(features);
 F2FS_GENERAL_RO_ATTR(current_reserved_blocks);
 F2FS_GENERAL_RO_ATTR(unusable);
 F2FS_GENERAL_RO_ATTR(encoding);
+F2FS_GENERAL_RO_ATTR(encoding_flags);
 F2FS_GENERAL_RO_ATTR(mounted_time_sec);
 F2FS_GENERAL_RO_ATTR(main_blkaddr);
 F2FS_GENERAL_RO_ATTR(pending_discard);
@@ -1270,6 +1278,7 @@ static struct attribute *f2fs_attrs[] = {
 	ATTR_LIST(reserved_blocks),
 	ATTR_LIST(current_reserved_blocks),
 	ATTR_LIST(encoding),
+	ATTR_LIST(encoding_flags),
 	ATTR_LIST(mounted_time_sec),
 #ifdef CONFIG_F2FS_STAT_FS
 	ATTR_LIST(cp_foreground_calls),
