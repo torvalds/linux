2abc698ac77314e0de5b33a6d96a39c5159d88e4
Aditya Garg <gargaditya08@live.com>
2025-05-07 12:09:00 -0700

    Input: synaptics - enable InterTouch on TUXEDO InfinityBook Pro 14 v5
    Enable InterTouch mode on TUXEDO InfinityBook Pro 14 v5 by adding
    "SYN1221" to the list of SMBus-enabled variants.
    
    Add support for InterTouch on SYN1221 by adding it to the list of
    SMBus-enabled variants.
    
    Reported-by: Matthias Eilert <kernel.hias@eilert.tech>
    Tested-by: Matthias Eilert <kernel.hias@eilert.tech>
    Signed-off-by: Aditya Garg <gargaditya08@live.com>
    Link: https://lore.kernel.org/r/PN3PR01MB9597C033C4BC20EE2A0C4543B888A@PN3PR01MB9597.INDPRD01.PROD.OUTLOOK.COM
    Cc: stable@vger.kernel.org
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/mouse/synaptics.c
+++ drivers/input/mouse/synaptics.c
@@ -190,6 +190,7 @@ static const char * const smbus_pnp_ids[] = {
 	"LEN2054", /* E480 */
 	"LEN2055", /* E580 */
 	"LEN2068", /* T14 Gen 1 */
+	"SYN1221", /* TUXEDO InfinityBook Pro 14 v5 */
 	"SYN3003", /* HP EliteBook 850 G1 */
 	"SYN3015", /* HP EliteBook 840 G2 */
 	"SYN3052", /* HP EliteBook 840 G4 */
