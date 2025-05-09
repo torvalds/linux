27cd1bf1240d482e4f02ca4f9812e748f3106e4f
Al Viro <viro@zeniv.linux.org.uk>
2025-05-08 23:38:01 -0400

    securityfs: don't pin dentries twice, once is enough...
    incidentally, securityfs_recursive_remove() is broken without that -
    it leaks dentries, since simple_recursive_removal() does not expect
    anything of that sort.  It could be worked around by dput() in
    remove_one() callback, but it's easier to just drop that double-get
    stuff.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- security/inode.c
+++ security/inode.c
@@ -159,7 +159,6 @@ static struct dentry *securityfs_create_dentry(const char *name, umode_t mode,
 		inode->i_fop = fops;
 	}
 	d_instantiate(dentry, inode);
-	dget(dentry);
 	inode_unlock(dir);
 	return dentry;
 
@@ -306,7 +305,6 @@ void securityfs_remove(struct dentry *dentry)
 			simple_rmdir(dir, dentry);
 		else
 			simple_unlink(dir, dentry);
-		dput(dentry);
 	}
 	inode_unlock(dir);
 	simple_release_fs(&mount, &mount_count);
