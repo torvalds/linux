63935e2ee1f2a371e511f853737b9efebc238bc7
John Allen <john.allen@amd.com>
2025-05-12 18:17:05 +0000

    crypto: ccp - Add support for PCI device 0x17D8
    Add a new CCP/PSP PCI device ID.
    
    Signed-off-by: John Allen <john.allen@amd.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/ccp/sp-pci.c
+++ drivers/crypto/ccp/sp-pci.c
@@ -535,6 +535,7 @@ static const struct pci_device_id sp_pci_table[] = {
 	{ PCI_VDEVICE(AMD, 0x1134), (kernel_ulong_t)&dev_vdata[7] },
 	{ PCI_VDEVICE(AMD, 0x17E0), (kernel_ulong_t)&dev_vdata[7] },
 	{ PCI_VDEVICE(AMD, 0x156E), (kernel_ulong_t)&dev_vdata[8] },
+	{ PCI_VDEVICE(AMD, 0x17D8), (kernel_ulong_t)&dev_vdata[8] },
 	/* Last entry must be zero */
 	{ 0, }
 };
