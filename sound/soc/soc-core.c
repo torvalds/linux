144d6dfc7482455eabf8e8caa974a6e8d9572705
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2025-05-12 02:33:16 +0000

    ASoC: soc-core: merge snd_soc_unregister_component() and snd_soc_unregister_component_by_driver()
    We have below 2 functions, but these are very similar
    
    (A)	snd_soc_unregister_component_by_driver()
    (B)	snd_soc_unregister_component()
    
    (A)	void snd_soc_unregister_component_by_driver(...)
    	{
    		...
    
    (a)		mutex_lock(&client_mutex);
     ^ (X)		component = snd_soc_lookup_component_nolocked(dev, component_driver->name);
     |		if (!component)					   ^^^^^^^^^^^^^^^^^^^^^^
     |			goto out;
    (b)
     |		snd_soc_del_component_unlocked(component);
     v
    	out:
    (c)		mutex_unlock(&client_mutex);
    	}
    
    (B)	void snd_soc_unregister_component_by_driver(...)
    	{
    (a)		mutex_lock(&client_mutex);
     ^		while (1) {
     | (X)			struct snd_soc_component *component = snd_soc_lookup_component_nolocked(dev, NULL);
     |												     ^^^^
    (b)			if (!component)
     |				break;
     |
     |			snd_soc_del_component_unlocked(component);
     v		}
    (c)		mutex_unlock(&client_mutex);
    	}
    
    Both are calling lock (a), find component and remove it (b), and
    unlock (c). The big diff is whether use driver name for lookup() or
    not (X).
    
    Merge these into snd_soc_unregister_component_by_driver() (B), and
    snd_soc_unregister_component_by_driver() (A) can be macro.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://patch.msgid.link/87h61qy2vn.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -2927,34 +2927,14 @@ EXPORT_SYMBOL_GPL(snd_soc_register_component);
 void snd_soc_unregister_component_by_driver(struct device *dev,
 					    const struct snd_soc_component_driver *component_driver)
 {
-	struct snd_soc_component *component;
+	const char *driver_name = NULL;
 
-	if (!component_driver)
-		return;
+	if (component_driver)
+		driver_name = component_driver->name;
 
-	mutex_lock(&client_mutex);
-	component = snd_soc_lookup_component_nolocked(dev, component_driver->name);
-	if (!component)
-		goto out;
-
-	snd_soc_del_component_unlocked(component);
-
-out:
-	mutex_unlock(&client_mutex);
-}
-EXPORT_SYMBOL_GPL(snd_soc_unregister_component_by_driver);
-
-/**
- * snd_soc_unregister_component - Unregister all related component
- * from the ASoC core
- *
- * @dev: The device to unregister
- */
-void snd_soc_unregister_component(struct device *dev)
-{
 	mutex_lock(&client_mutex);
 	while (1) {
-		struct snd_soc_component *component = snd_soc_lookup_component_nolocked(dev, NULL);
+		struct snd_soc_component *component = snd_soc_lookup_component_nolocked(dev, driver_name);
 
 		if (!component)
 			break;
@@ -2963,7 +2943,7 @@ void snd_soc_unregister_component(struct device *dev)
 	}
 	mutex_unlock(&client_mutex);
 }
-EXPORT_SYMBOL_GPL(snd_soc_unregister_component);
+EXPORT_SYMBOL_GPL(snd_soc_unregister_component_by_driver);
 
 /* Retrieve a card's name from device tree */
 int snd_soc_of_parse_card_name(struct snd_soc_card *card,
