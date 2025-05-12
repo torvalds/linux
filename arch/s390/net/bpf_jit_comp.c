5f55f2168432298f5a55294831ab6a76a10cb3c3
Ilya Leoshkevich <iii@linux.ibm.com>
2025-05-12 14:26:15 +0200

    s390/bpf: Store backchain even for leaf progs
    Currently a crash in a leaf prog (caused by a bug) produces the
    following call trace:
    
         [<000003ff600ebf00>] bpf_prog_6df0139e1fbf2789_fentry+0x20/0x78
         [<0000000000000000>] 0x0
    
    This is because leaf progs do not store backchain. Fix by making all
    progs do it. This is what GCC and Clang-generated code does as well.
    Now the call trace looks like this:
    
         [<000003ff600eb0f2>] bpf_prog_6df0139e1fbf2789_fentry+0x2a/0x80
         [<000003ff600ed096>] bpf_trampoline_201863462940+0x96/0xf4
         [<000003ff600e3a40>] bpf_prog_05f379658fdd72f2_classifier_0+0x58/0xc0
         [<000003ffe0aef070>] bpf_test_run+0x210/0x390
         [<000003ffe0af0dc2>] bpf_prog_test_run_skb+0x25a/0x668
         [<000003ffe038a90e>] __sys_bpf+0xa46/0xdb0
         [<000003ffe038ad0c>] __s390x_sys_bpf+0x44/0x50
         [<000003ffe0defea8>] __do_syscall+0x150/0x280
         [<000003ffe0e01d5c>] system_call+0x74/0x98
    
    Fixes: 054623105728 ("s390/bpf: Add s390x eBPF JIT compiler backend")
    Signed-off-by: Ilya Leoshkevich <iii@linux.ibm.com>
    Link: https://lore.kernel.org/r/20250512122717.54878-1-iii@linux.ibm.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- arch/s390/net/bpf_jit_comp.c
+++ arch/s390/net/bpf_jit_comp.c
@@ -605,17 +605,15 @@ static void bpf_jit_prologue(struct bpf_jit *jit, struct bpf_prog *fp,
 	}
 	/* Setup stack and backchain */
 	if (is_first_pass(jit) || (jit->seen & SEEN_STACK)) {
-		if (is_first_pass(jit) || (jit->seen & SEEN_FUNC))
-			/* lgr %w1,%r15 (backchain) */
-			EMIT4(0xb9040000, REG_W1, REG_15);
+		/* lgr %w1,%r15 (backchain) */
+		EMIT4(0xb9040000, REG_W1, REG_15);
 		/* la %bfp,STK_160_UNUSED(%r15) (BPF frame pointer) */
 		EMIT4_DISP(0x41000000, BPF_REG_FP, REG_15, STK_160_UNUSED);
 		/* aghi %r15,-STK_OFF */
 		EMIT4_IMM(0xa70b0000, REG_15, -(STK_OFF + stack_depth));
-		if (is_first_pass(jit) || (jit->seen & SEEN_FUNC))
-			/* stg %w1,152(%r15) (backchain) */
-			EMIT6_DISP_LH(0xe3000000, 0x0024, REG_W1, REG_0,
-				      REG_15, 152);
+		/* stg %w1,152(%r15) (backchain) */
+		EMIT6_DISP_LH(0xe3000000, 0x0024, REG_W1, REG_0,
+			      REG_15, 152);
 	}
 }
 
