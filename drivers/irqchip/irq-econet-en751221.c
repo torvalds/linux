96a8cb6d28cebd51a286fe4a8782dc8492813ac4
Jiri Slaby (SUSE) <jirislaby@kernel.org>
2025-05-13 10:47:39 +0200

    irqchip/econet-en751221: Switch to of_fwnode_handle()
    of_node_to_fwnode() is an irqdomain's reimplementation of the
    "officially" defined of_fwnode_handle(). The former is in the process of
    being removed, so use the latter instead.
    
    Signed-off-by: Jiri Slaby (SUSE) <jirislaby@kernel.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/all/20250513084739.2611747-1-jirislaby@kernel.org

--- drivers/irqchip/irq-econet-en751221.c
+++ drivers/irqchip/irq-econet-en751221.c
@@ -286,7 +286,7 @@ static int __init econet_intc_of_init(struct device_node *node, struct device_no
 
 	econet_mask_all();
 
-	domain = irq_domain_create_linear(of_node_to_fwnode(node), IRQ_COUNT,
+	domain = irq_domain_create_linear(of_fwnode_handle(node), IRQ_COUNT,
 					  &econet_domain_ops, NULL);
 	if (!domain) {
 		pr_err("%pOF: Failed to add irqdomain\n", node);
