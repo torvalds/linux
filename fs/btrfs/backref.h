d8f6cb2b28627fefa72e2ce1508db5781868a692
Daniel Vacek <neelx@suse.com>
2025-05-14 15:12:39 +0200

    btrfs: relocation: simplify unused logic related to LINK_LOWER
    btrfs_backref_link_edge() is always called with the LINK_LOWER argument.
    We can simplify it and remove the LINK_LOWER and LINK_UPPER macros
    completely.
    
    The last call with LINK_UPPER was removed with commit 0097422c0dfe0a
    ("btrfs: remove clone_backref_node() from relocation").
    
    Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Signed-off-by: Daniel Vacek <neelx@suse.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/backref.h
+++ fs/btrfs/backref.h
@@ -423,13 +423,6 @@ struct btrfs_backref_node *btrfs_backref_alloc_node(
 struct btrfs_backref_edge *btrfs_backref_alloc_edge(
 		struct btrfs_backref_cache *cache);
 
-#define		LINK_LOWER	(1U << 0)
-#define		LINK_UPPER	(1U << 1)
-
-void btrfs_backref_link_edge(struct btrfs_backref_edge *edge,
-			     struct btrfs_backref_node *lower,
-			     struct btrfs_backref_node *upper,
-			     int link_which);
 void btrfs_backref_free_node(struct btrfs_backref_cache *cache,
 			     struct btrfs_backref_node *node);
 void btrfs_backref_free_edge(struct btrfs_backref_cache *cache,
