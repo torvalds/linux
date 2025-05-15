622b91e0f94600d1c797b7c82ec67a6e221e74ec
Oleksij Rempel <o.rempel@pengutronix.de>
2025-05-15 10:20:51 +0200

    net: phy: microchip: document where the LAN88xx PHYs are used
    The driver uses the name LAN88xx for PHYs with phy_id = 0x0007c132. But
    with this placeholder name no documentation can be found on the net.
    
    Document the fact that these PHYs are build into the LAN7800 and LAN7850
    USB/Ethernet controllers.
    
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Link: https://patch.msgid.link/20250515082051.2644450-1-o.rempel@pengutronix.de
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/phy/microchip.c
+++ drivers/net/phy/microchip.c
@@ -474,6 +474,8 @@ static struct phy_driver microchip_phy_driver[] = {
 	/* This mask (0xfffffff2) is to differentiate from
 	 * LAN8742 (phy_id 0x0007c130 and 0x0007c131)
 	 * and allows future phy_id revisions.
+	 * These PHYs are integrated in LAN7800 and LAN7850 USB/Ethernet
+	 * controllers.
 	 */
 	.phy_id_mask	= 0xfffffff2,
 	.name		= "Microchip LAN88xx",
