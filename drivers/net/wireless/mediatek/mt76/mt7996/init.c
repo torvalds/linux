8b2f574845e33d02e7fbad2d3192a8b717567afa
Peter Chiu <chui-hao.chiu@mediatek.com>
2025-05-15 11:29:46 +0800

    wifi: mt76: mt7996: set EHT max ampdu length capability
    Set the max AMPDU length in the EHT MAC CAP. Without this patch, the
    peer station cannot obtain the correct capability, which prevents
    achieving peak throughput on the 2 GHz band.
    
    Fixes: 1816ad9381e0 ("wifi: mt76: mt7996: add max mpdu len capability")
    Signed-off-by: Peter Chiu <chui-hao.chiu@mediatek.com>
    Signed-off-by: Shayne Chen <shayne.chen@mediatek.com>
    Link: https://patch.msgid.link/20250515032952.1653494-3-shayne.chen@mediatek.com
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7996/init.c
+++ drivers/net/wireless/mediatek/mt76/mt7996/init.c
@@ -1326,6 +1326,9 @@ mt7996_init_eht_caps(struct mt7996_phy *phy, enum nl80211_band band,
 		u8_encode_bits(IEEE80211_EHT_MAC_CAP0_MAX_MPDU_LEN_11454,
 			       IEEE80211_EHT_MAC_CAP0_MAX_MPDU_LEN_MASK);
 
+	eht_cap_elem->mac_cap_info[1] |=
+		IEEE80211_EHT_MAC_CAP1_MAX_AMPDU_LEN_MASK;
+
 	eht_cap_elem->phy_cap_info[0] =
 		IEEE80211_EHT_PHY_CAP0_NDP_4_EHT_LFT_32_GI |
 		IEEE80211_EHT_PHY_CAP0_SU_BEAMFORMER |
