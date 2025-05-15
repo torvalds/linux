e43c0356e0dc3606bfd83616123688b60ef2e075
clywm520 <149147967@qq.com>
2025-05-15 20:03:50 +0800

    Create one.sh

---
+++ one.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+if command -v apt &> /dev/null; then
+sudo apt update
+sudo apt-get install build-essential ncurses-dev bison flex libssl-dev libelf-dev -y
+sudo apt-get install -y debhelper-compat=12
+fi
+
+if command -v yum &> /dev/null; then
+yum -y install  build-essential ncurses-dev bison flex libssl-dev libelf-dev
+yum -y install openssl-devel
+fi
+
+\cp /boot/$(ls /boot/ | grep config | head -1)  arch/x86/configs/my_defconfig
+sed -i 's/^CONFIG_MODULE_SIG=.*/CONFIG_MODULE_SIG=n/' arch/x86/configs/my_defconfig
+sed -i 's/^CONFIG_DEBUG_INFO=.*/CONFIG_DEBUG_INFO=n/' arch/x86/configs/my_defconfig
+sed -i 's/^CONFIG_MODULE_SIG_ALL=.*/CONFIG_MODULE_SIG_ALL=n/' arch/x86/configs/my_defconfig
+sed -i 's/^CONFIG_SYSTEM_REVOCATION_KEYS=.*/CONFIG_SYSTEM_REVOCATION_KEYS=""/' arch/x86/configs/my_defconfig
+sed -i 's/^CONFIG_MODULE_SIG_KEY=.*/CONFIG_MODULE_SIG_KEY=""/' arch/x86/configs/my_defconfig
+sed -i 's/^CONFIG_SYSTEM_TRUSTED_KEYS=.*/CONFIG_SYSTEM_TRUSTED_KEYS=""/' arch/x86/configs/my_defconfig
+
+sed -i 's/^CONFIG_ZRAM=.*/CONFIG_ZRAM=m/' arch/x86/configs/my_defconfig
+sed -i 's/^CONFIG_ZRAM_DEF_COMP=.*/CONFIG_ZRAM_DEF_COMP="zstd"/' arch/x86/configs/my_defconfig
+sed -i 's/^CONFIG_ZRAM_BACKEND_FORCE_LZO=.*/CONFIG_ZRAM_BACKEND_FORCE_LZO=n/' arch/x86/configs/my_defconfig
+sed -i 's/^CONFIG_ZRAM_BACKEND_LZO=.*/CONFIG_ZRAM_BACKEND_LZO=n/' arch/x86/configs/my_defconfig
+echo "CONFIG_ZRAM_BACKEND_ZSTD=y" >> arch/x86/configs/my_defconfig
+echo "CONFIG_ZRAM_DEF_COMP_ZSTD=y" >> arch/x86/configs/my_defconfig
+
+
+echo "CONFIG_MEMCG_V1=y" >> arch/x86/configs/my_defconfig
+echo "CONFIG_CPUSETS_V1=y" >> arch/x86/configs/my_defconfig
+
+make my_defconfig
+
+
+make -j$(nproc)
+
+make modules -j$(nproc)
+make bindeb-pkg -j$(nproc)
+
+#make modules_install
+#rm -rf /boot/*6.15*
+#make install
+
