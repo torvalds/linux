5a78977262f47dd52a4880689a2ca3d21dbf149a
Alice Ryhl <aliceryhl@google.com>
2025-05-16 19:32:19 +0000

    mm: rust: make CONFIG_MMU ifdefs more narrow
    Currently the entire kernel::mm module is ifdef'd out when CONFIG_MMU=n.
    However, there are some downstream users of the module in
    rust/kernel/task.rs and rust/kernel/miscdevice.rs. Thus, update the cfgs
    so that only MmWithUserAsync is removed with CONFIG_MMU=n.
    
    The code is moved into a new file, since the #[cfg()] annotation
    otherwise has to be duplicated several times.
    
    Link: https://lkml.kernel.org/r/20250516193219.2987032-1-aliceryhl@google.com
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202505071753.kldNHYVQ-lkp@intel.com/
    Closes: https://lore.kernel.org/oe-kbuild-all/202505072116.eSYC8igT-lkp@intel.com/
    Fixes: 5bb9ed6cdfeb ("mm: rust: add abstraction for struct mm_struct")
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Boqun Feng <boqun.feng@gmail.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- rust/kernel/mm.rs
+++ rust/kernel/mm.rs
@@ -10,7 +10,6 @@
 //! control what happens when userspace reads or writes to that region of memory.
 //!
 //! C header: [`include/linux/mm.h`](srctree/include/linux/mm.h)
-#![cfg(CONFIG_MMU)]
 
 use crate::{
     bindings,
@@ -20,6 +19,10 @@ use core::{ops::Deref, ptr::NonNull};
 
 pub mod virt;
 use virt::VmaRef;
+
+#[cfg(CONFIG_MMU)]
+pub use mmput_async::MmWithUserAsync;
+mod mmput_async;
 
 /// A wrapper for the kernel's `struct mm_struct`.
 ///
@@ -111,50 +114,6 @@ impl Deref for MmWithUser {
     }
 }
 
-/// A wrapper for the kernel's `struct mm_struct`.
-///
-/// This type is identical to `MmWithUser` except that it uses `mmput_async` when dropping a
-/// refcount. This means that the destructor of `ARef<MmWithUserAsync>` is safe to call in atomic
-/// context.
-///
-/// # Invariants
-///
-/// Values of this type are always refcounted using `mmget`. The value of `mm_users` is non-zero.
-#[repr(transparent)]
-pub struct MmWithUserAsync {
-    mm: MmWithUser,
-}
-
-// SAFETY: It is safe to call `mmput_async` on another thread than where `mmget` was called.
-unsafe impl Send for MmWithUserAsync {}
-// SAFETY: All methods on `MmWithUserAsync` can be called in parallel from several threads.
-unsafe impl Sync for MmWithUserAsync {}
-
-// SAFETY: By the type invariants, this type is always refcounted.
-unsafe impl AlwaysRefCounted for MmWithUserAsync {
-    #[inline]
-    fn inc_ref(&self) {
-        // SAFETY: The pointer is valid since self is a reference.
-        unsafe { bindings::mmget(self.as_raw()) };
-    }
-
-    #[inline]
-    unsafe fn dec_ref(obj: NonNull<Self>) {
-        // SAFETY: The caller is giving up their refcount.
-        unsafe { bindings::mmput_async(obj.cast().as_ptr()) };
-    }
-}
-
-// Make all `MmWithUser` methods available on `MmWithUserAsync`.
-impl Deref for MmWithUserAsync {
-    type Target = MmWithUser;
-
-    #[inline]
-    fn deref(&self) -> &MmWithUser {
-        &self.mm
-    }
-}
-
 // These methods are safe to call even if `mm_users` is zero.
 impl Mm {
     /// Returns a raw pointer to the inner `mm_struct`.
@@ -204,13 +163,6 @@ impl MmWithUser {
         // SAFETY: Caller promises that the pointer is valid for 'a. The layout is compatible due
         // to repr(transparent).
         unsafe { &*ptr.cast() }
-    }
-
-    /// Use `mmput_async` when dropping this refcount.
-    #[inline]
-    pub fn into_mmput_async(me: ARef<MmWithUser>) -> ARef<MmWithUserAsync> {
-        // SAFETY: The layouts and invariants are compatible.
-        unsafe { ARef::from_raw(ARef::into_raw(me).cast()) }
     }
 
     /// Attempt to access a vma using the vma read lock.
