16d693749866f2e4731f2aa1c553f185804dd392
Jiri Slaby (SUSE) <jirislaby@kernel.org>
2025-05-17 20:23:29 -0700

    irqdomain: spmi: Switch to irq_domain_create_tree()
    irq_domain_add_tree() is going away as being obsolete now. Switch to
    the preferred irq_domain_create_tree(). That differs in the first
    parameter: It takes more generic struct fwnode_handle instead of struct
    device_node. Therefore, of_fwnode_handle() is added around the
    parameter.
    
    Note some of the users can likely use dev->fwnode directly instead of
    indirect of_fwnode_handle(dev->of_node). But dev->fwnode is not
    guaranteed to be set for all, so this has to be investigated on case to
    case basis (by people who can actually test with the HW).
    
    Signed-off-by: "Jiri Slaby (SUSE)" <jirislaby@kernel.org>
    Cc: Stephen Boyd <sboyd@kernel.org>
    Link: https://lore.kernel.org/r/20250319092951.37667-37-jirislaby@kernel.org
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>
    Link: https://lore.kernel.org/r/20250518032330.2959766-5-sboyd@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/spmi/spmi-pmic-arb.c
+++ drivers/spmi/spmi-pmic-arb.c
@@ -1737,7 +1737,7 @@ static int spmi_pmic_arb_bus_init(struct platform_device *pdev,
 
 	dev_dbg(&pdev->dev, "adding irq domain for bus %d\n", bus_index);
 
-	bus->domain = irq_domain_add_tree(node, &pmic_arb_irq_domain_ops, bus);
+	bus->domain = irq_domain_create_tree(of_fwnode_handle(node), &pmic_arb_irq_domain_ops, bus);
 	if (!bus->domain) {
 		dev_err(&pdev->dev, "unable to create irq_domain\n");
 		return -ENOMEM;
