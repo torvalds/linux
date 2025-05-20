2b520c6104f34e3a548525173c38ebca4402cac3
Connor Abbott <cwabbott0@gmail.com>
2025-05-20 18:28:06 -0400

    drm/msm/a7xx: Call CP_RESET_CONTEXT_STATE
    Calling this packet is necessary when we switch contexts because there
    are various pieces of state used by userspace to synchronize between BR
    and BV that are persistent across submits and we need to make sure that
    they are in a "safe" state when switching contexts. Otherwise a
    userspace submission in one context could cause another context to
    function incorrectly and hang, effectively a denial of service (although
    without leaking data). This was missed during initial a7xx bringup.
    
    Fixes: af66706accdf ("drm/msm/a6xx: Add skeleton A7xx support")
    Signed-off-by: Connor Abbott <cwabbott0@gmail.com>
    Patchwork: https://patchwork.freedesktop.org/patch/654924/
    Signed-off-by: Rob Clark <robin.clark@oss.qualcomm.com>

--- drivers/gpu/drm/msm/adreno/a6xx_gpu.c
+++ drivers/gpu/drm/msm/adreno/a6xx_gpu.c
@@ -130,6 +130,20 @@ static void a6xx_set_pagetable(struct a6xx_gpu *a6xx_gpu,
 		OUT_RING(ring, lower_32_bits(rbmemptr(ring, fence)));
 		OUT_RING(ring, upper_32_bits(rbmemptr(ring, fence)));
 		OUT_RING(ring, submit->seqno - 1);
+
+		OUT_PKT7(ring, CP_THREAD_CONTROL, 1);
+		OUT_RING(ring, CP_SET_THREAD_BOTH);
+
+		/* Reset state used to synchronize BR and BV */
+		OUT_PKT7(ring, CP_RESET_CONTEXT_STATE, 1);
+		OUT_RING(ring,
+			 CP_RESET_CONTEXT_STATE_0_CLEAR_ON_CHIP_TS |
+			 CP_RESET_CONTEXT_STATE_0_CLEAR_RESOURCE_TABLE |
+			 CP_RESET_CONTEXT_STATE_0_CLEAR_BV_BR_COUNTER |
+			 CP_RESET_CONTEXT_STATE_0_RESET_GLOBAL_LOCAL_TS);
+
+		OUT_PKT7(ring, CP_THREAD_CONTROL, 1);
+		OUT_RING(ring, CP_SET_THREAD_BR);
 	}
 
 	if (!sysprof) {
