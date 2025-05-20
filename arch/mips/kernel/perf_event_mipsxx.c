b216af2eb4618caec6bef32d5886a9700b0ecfeb
Kan Liang <kan.liang@linux.intel.com>
2025-05-20 11:16:44 -0700

    mips/perf: Remove driver-specific throttle support
    The throttle support has been added in the generic code. Remove
    the driver-specific throttle support.
    
    Besides the throttle, perf_event_overflow may return true because of
    event_limit. It already does an inatomic event disable. The pmu->stop
    is not required either.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20250520181644.2673067-17-kan.liang@linux.intel.com

--- arch/mips/kernel/perf_event_mipsxx.c
+++ arch/mips/kernel/perf_event_mipsxx.c
@@ -791,8 +791,7 @@ static void handle_associated_event(struct cpu_hw_events *cpuc,
 	if (!mipspmu_event_set_period(event, hwc, idx))
 		return;
 
-	if (perf_event_overflow(event, data, regs))
-		mipsxx_pmu_disable_event(idx);
+	perf_event_overflow(event, data, regs);
 }
 
 
