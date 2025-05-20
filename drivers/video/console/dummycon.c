03bcbbb3995ba5df43af9aba45334e35f2dfe27b
Thomas Zimmermann <tzimmermann@suse.de>
2025-05-20 09:14:00 +0200

    dummycon: Trigger redraw when switching consoles with deferred takeover
    Signal vt subsystem to redraw console when switching to dummycon
    with deferred takeover enabled. Makes the console switch to fbcon
    and displays the available output.
    
    With deferred takeover enabled, dummycon acts as the placeholder
    until the first output to the console happens. At that point, fbcon
    takes over. If the output happens while dummycon is not active, it
    cannot inform fbcon. This is the case if the vt subsystem runs in
    graphics mode.
    
    A typical graphical boot starts plymouth, a display manager and a
    compositor; all while leaving out dummycon. Switching to a text-mode
    console leaves the console with dummycon even if a getty terminal
    has been started.
    
    Returning true from dummycon's con_switch helper signals the vt
    subsystem to redraw the screen. If there's output available dummycon's
    con_putc{s} helpers trigger deferred takeover of fbcon, which sets a
    display mode and displays the output. If no output is available,
    dummycon remains active.
    
    v2:
    - make the comment slightly more verbose (Javier)
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reported-by: Andrei Borzenkov <arvidjaar@gmail.com>
    Closes: https://bugzilla.suse.com/show_bug.cgi?id=1242191
    Tested-by: Andrei Borzenkov <arvidjaar@gmail.com>
    Acked-by: Javier Martinez Canillas <javierm@redhat.com>
    Fixes: 83d83bebf401 ("console/fbcon: Add support for deferred console takeover")
    Cc: Hans de Goede <hdegoede@redhat.com>
    Cc: linux-fbdev@vger.kernel.org
    Cc: dri-devel@lists.freedesktop.org
    Cc: <stable@vger.kernel.org> # v4.19+
    Link: https://lore.kernel.org/r/20250520071418.8462-1-tzimmermann@suse.de

--- drivers/video/console/dummycon.c
+++ drivers/video/console/dummycon.c
@@ -85,6 +85,15 @@ static bool dummycon_blank(struct vc_data *vc, enum vesa_blank_mode blank,
 	/* Redraw, so that we get putc(s) for output done while blanked */
 	return true;
 }
+
+static bool dummycon_switch(struct vc_data *vc)
+{
+	/*
+	 * Redraw, so that we get putc(s) for output done while switched
+	 * away. Informs deferred consoles to take over the display.
+	 */
+	return true;
+}
 #else
 static void dummycon_putc(struct vc_data *vc, u16 c, unsigned int y,
 			  unsigned int x) { }
@@ -92,6 +101,10 @@ static void dummycon_putcs(struct vc_data *vc, const u16 *s, unsigned int count,
 			   unsigned int ypos, unsigned int xpos) { }
 static bool dummycon_blank(struct vc_data *vc, enum vesa_blank_mode blank,
 			   bool mode_switch)
+{
+	return false;
+}
+static bool dummycon_switch(struct vc_data *vc)
 {
 	return false;
 }
@@ -120,11 +133,6 @@ static void dummycon_cursor(struct vc_data *vc, bool enable) { }
 static bool dummycon_scroll(struct vc_data *vc, unsigned int top,
 			    unsigned int bottom, enum con_scroll dir,
 			    unsigned int lines)
-{
-	return false;
-}
-
-static bool dummycon_switch(struct vc_data *vc)
 {
 	return false;
 }
