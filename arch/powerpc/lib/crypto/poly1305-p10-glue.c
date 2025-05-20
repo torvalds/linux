16766292c697f31ccfc338d4040f2d5f03df777c
Herbert Xu <herbert@gondor.apana.org.au>
2025-05-20 10:35:29 +0800

    Revert "crypto: powerpc/poly1305 - Add SIMD fallback"
    This reverts commit c66d7ebbe2fa14e41913adb421090a7426f59786.
    
    Link: https://lore.kernel.org/all/02c22854-eebf-4ad1-b89e-8c2b65ab8236@csgroup.eu/
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- arch/powerpc/lib/crypto/poly1305-p10-glue.c
+++ arch/powerpc/lib/crypto/poly1305-p10-glue.c
@@ -6,7 +6,6 @@
  */
 #include <asm/switch_to.h>
 #include <crypto/internal/poly1305.h>
-#include <crypto/internal/simd.h>
 #include <linux/cpufeature.h>
 #include <linux/jump_label.h>
 #include <linux/kernel.h>
@@ -18,11 +17,6 @@ asmlinkage void poly1305_64s(struct poly1305_block_state *state, const u8 *m, u3
 asmlinkage void poly1305_emit_64(const struct poly1305_state *state, const u32 nonce[4], u8 digest[POLY1305_DIGEST_SIZE]);
 
 static __ro_after_init DEFINE_STATIC_KEY_FALSE(have_p10);
-
-static inline bool is_state_base64(struct poly1305_block_state *state)
-{
-	return state->core_r.precomputed_s.r64[2];
-}
 
 static void vsx_begin(void)
 {
@@ -36,35 +30,12 @@ static void vsx_end(void)
 	preempt_enable();
 }
 
-static void convert_to_base2_44(struct poly1305_block_state *state)
-{
-	u8 raw_key[POLY1305_BLOCK_SIZE];
-	u64 h0, h1, h2;
-
-	if (!is_state_base64(state))
-		return;
-
-	state->core_r.precomputed_s.r64[2] = 0;
-	put_unaligned_le64(state->core_r.key.r64[0], raw_key + 0);
-	put_unaligned_le64(state->core_r.key.r64[1], raw_key + 8);
-	poly1305_core_setkey(&state->core_r, raw_key);
-
-	h0 = state->h.h64[0];
-	h1 = state->h.h64[1];
-	h2 = state->h.h64[2];
-	state->h.h64[0] = h0 & 0xfffffffffffULL;
-	state->h.h64[1] = h0 >> 44 | (h1 & 0xffffffULL) << 20;
-	state->h.h64[2] = h1 >> 24 | h2 << 40;
-}
-
 void poly1305_block_init_arch(struct poly1305_block_state *dctx,
 			      const u8 raw_key[POLY1305_BLOCK_SIZE])
 {
-	dctx->core_r.precomputed_s.r64[2] = 0;
-	if (!static_key_enabled(&have_p10) || !crypto_simd_usable())
+	if (!static_key_enabled(&have_p10))
 		return poly1305_block_init_generic(dctx, raw_key);
 
-	dctx->core_r.precomputed_s.r64[2] = 1;
 	dctx->h = (struct poly1305_state){};
 	dctx->core_r.key.r64[0] = get_unaligned_le64(raw_key + 0);
 	dctx->core_r.key.r64[1] = get_unaligned_le64(raw_key + 8);
@@ -74,11 +45,8 @@ EXPORT_SYMBOL_GPL(poly1305_block_init_arch);
 void poly1305_blocks_arch(struct poly1305_block_state *state, const u8 *src,
 			  unsigned int len, u32 padbit)
 {
-	if (!static_key_enabled(&have_p10) || !is_state_base64(state) ||
-	    !crypto_simd_usable()) {
-		convert_to_base2_44(state);
+	if (!static_key_enabled(&have_p10))
 		return poly1305_blocks_generic(state, src, len, padbit);
-	}
 	vsx_begin();
 	if (len >= POLY1305_BLOCK_SIZE * 4) {
 		poly1305_p10le_4blocks(state, src, len);
@@ -98,10 +66,7 @@ void poly1305_emit_arch(const struct poly1305_state *state,
 			u8 digest[POLY1305_DIGEST_SIZE],
 			const u32 nonce[4])
 {
-	struct poly1305_block_state *dctx =
-		container_of(state, struct poly1305_block_state, h);
-
-	if (!static_key_enabled(&have_p10) || !is_state_base64(dctx))
+	if (!static_key_enabled(&have_p10))
 		return poly1305_emit_generic(state, digest, nonce);
 	poly1305_emit_64(state, nonce, digest);
 }
