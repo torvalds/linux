24b2f5df86aaebbe7bac40304eaf5a146c02367c
Laurent Vivier <lvivier@redhat.com>
2025-05-21 11:22:36 +0200

    virtio_net: Enforce minimum TX ring size for reliability
    The `tx_may_stop()` logic stops TX queues if free descriptors
    (`sq->vq->num_free`) fall below the threshold of (`MAX_SKB_FRAGS` + 2).
    If the total ring size (`ring_num`) is not strictly greater than this
    value, queues can become persistently stopped or stop after minimal
    use, severely degrading performance.
    
    A single sk_buff transmission typically requires descriptors for:
    - The virtio_net_hdr (1 descriptor)
    - The sk_buff's linear data (head) (1 descriptor)
    - Paged fragments (up to MAX_SKB_FRAGS descriptors)
    
    This patch enforces that the TX ring size ('ring_num') must be strictly
    greater than (MAX_SKB_FRAGS + 2). This ensures that the ring is
    always large enough to hold at least one maximally-fragmented packet
    plus at least one additional slot.
    
    Reported-by: Lei Yang <leiyang@redhat.com>
    Signed-off-by: Laurent Vivier <lvivier@redhat.com>
    Reviewed-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Link: https://patch.msgid.link/20250521092236.661410-4-lvivier@redhat.com
    Tested-by: Lei Yang <leiyang@redhat.com>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -3504,6 +3504,12 @@ static int virtnet_tx_resize(struct virtnet_info *vi, struct send_queue *sq,
 {
 	int qindex, err;
 
+	if (ring_num <= MAX_SKB_FRAGS + 2) {
+		netdev_err(vi->dev, "tx size (%d) cannot be smaller than %d\n",
+			   ring_num, MAX_SKB_FRAGS + 2);
+		return -EINVAL;
+	}
+
 	qindex = sq - vi->sq;
 
 	virtnet_tx_pause(vi, sq);
