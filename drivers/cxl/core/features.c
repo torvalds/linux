f76e0bbc8bc34c55fd5da569b9e1126cf42b09ec
Shiju Jose <shiju.jose@huawei.com>
2025-05-21 13:47:40 +0100

    cxl: Update prototype of function get_support_feature_info()
    Add following changes to function get_support_feature_info()
    1. Make generic to share between cxl-fwctl and cxl-edac paths.
    2. Rename get_support_feature_info() to cxl_feature_info()
    3. Change parameter const struct fwctl_rpc_cxl *rpc_in to
       const uuid_t *uuid.
    
    Suggested-by: Dan Williams <dan.j.williams@intel.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Signed-off-by: Shiju Jose <shiju.jose@huawei.com>
    Reviewed-by: Fan Ni <fan.ni@samsung.com>
    Reviewed-by: Alison Schofield <alison.schofield@intel.com>
    Acked-by: Dan Williams <dan.j.williams@intel.com>
    Link: https://patch.msgid.link/20250521124749.817-3-shiju.jose@huawei.com
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>

--- drivers/cxl/core/features.c
+++ drivers/cxl/core/features.c
@@ -355,17 +355,11 @@ static void cxlctl_close_uctx(struct fwctl_uctx *uctx)
 {
 }
 
-static struct cxl_feat_entry *
-get_support_feature_info(struct cxl_features_state *cxlfs,
-			 const struct fwctl_rpc_cxl *rpc_in)
+struct cxl_feat_entry *
+cxl_feature_info(struct cxl_features_state *cxlfs,
+		 const uuid_t *uuid)
 {
 	struct cxl_feat_entry *feat;
-	const uuid_t *uuid;
-
-	if (rpc_in->op_size < sizeof(uuid))
-		return ERR_PTR(-EINVAL);
-
-	uuid = &rpc_in->set_feat_in.uuid;
 
 	for (int i = 0; i < cxlfs->entries->num_features; i++) {
 		feat = &cxlfs->entries->ent[i];
@@ -547,7 +541,10 @@ static bool cxlctl_validate_set_features(struct cxl_features_state *cxlfs,
 	struct cxl_feat_entry *feat;
 	u32 flags;
 
-	feat = get_support_feature_info(cxlfs, rpc_in);
+	if (rpc_in->op_size < sizeof(uuid_t))
+		return ERR_PTR(-EINVAL);
+
+	feat = cxl_feature_info(cxlfs, &rpc_in->set_feat_in.uuid);
 	if (IS_ERR(feat))
 		return false;
 
