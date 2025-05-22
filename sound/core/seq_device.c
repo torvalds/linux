62f134ab190c5fd5c9f68fe638ad8e13bb8a4cb4
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2025-05-22 12:08:05 +0200

    ALSA: core: fix up bus match const issues.
    In commit d69d80484598 ("driver core: have match() callback in struct
    bus_type take a const *"), the match bus callback was changed to have
    the driver be a const pointer.  Unfortunately that const attribute was
    thrown away when container_of() is called, which is not correct and was
    not caught by the compiler due to how container_of() is implemented.
    Fix this up by correctly preserving the const attribute of the driver
    passed to the bus match function which requires the hdac_driver match
    function to also take a const pointer for the driver structure.
    
    Cc: Jaroslav Kysela <perex@perex.cz>
    Cc: Takashi Iwai <tiwai@suse.com>
    Fixes: d69d80484598 ("driver core: have match() callback in struct bus_type take a const *")
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://patch.msgid.link/2025052204-hyphen-thermal-3e72@gregkh
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/seq_device.c
+++ sound/core/seq_device.c
@@ -43,7 +43,7 @@ MODULE_LICENSE("GPL");
 static int snd_seq_bus_match(struct device *dev, const struct device_driver *drv)
 {
 	struct snd_seq_device *sdev = to_seq_dev(dev);
-	struct snd_seq_driver *sdrv = to_seq_drv(drv);
+	const struct snd_seq_driver *sdrv = to_seq_drv(drv);
 
 	return strcmp(sdrv->id, sdev->id) == 0 &&
 		sdrv->argsize == sdev->argsize;
