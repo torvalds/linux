692c70f4d80247c622ce864672cbd0e3d8799189
Hawking Zhang <Hawking.Zhang@amd.com>
2025-05-27 21:02:22 +0800

    drm/amdgpu: Use ih v7_0 ip block for ih v7_1
    ih v7_1 and ih v7_0 share the same ip block implementation
    
    Signed-off-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Reviewed-by: Likun Gao <Likun.Gao@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
@@ -2088,6 +2088,7 @@ static int amdgpu_discovery_set_ih_ip_blocks(struct amdgpu_device *adev)
 		amdgpu_device_ip_block_add(adev, &ih_v6_1_ip_block);
 		break;
 	case IP_VERSION(7, 0, 0):
+	case IP_VERSION(7, 1, 0):
 		amdgpu_device_ip_block_add(adev, &ih_v7_0_ip_block);
 		break;
 	default:
