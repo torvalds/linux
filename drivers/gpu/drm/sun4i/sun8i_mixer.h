ef54f1dc246b72bac4dd222bd57054ba740fa207
Jernej Skrabec <jernej.skrabec@gmail.com>
2025-05-28 21:22:08 +1200

    drm: sun4i: de2/de3: add generic blender register reference function
    The DE2 and DE3 engines have a blender register range within the
    mixer engine register map, whereas the DE33 separates this out into
    a separate display group.
    
    Prepare for this by adding a function to look the blender reference up,
    with a subsequent patch to add a conditional based on the DE type.
    
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>
    Signed-off-by: Ryan Walklin <ryan@testtoast.com>
    Acked-by: Maxime Ripard <mripard@kernel.org>
    Reviewed-by: Chen-Yu Tsai <wens@csie.org>
    Link: https://lore.kernel.org/r/20250528092431.28825-4-ryan@testtoast.com
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- drivers/gpu/drm/sun4i/sun8i_mixer.h
+++ drivers/gpu/drm/sun4i/sun8i_mixer.h
@@ -222,6 +222,12 @@ sun8i_blender_base(struct sun8i_mixer *mixer)
 	return mixer->cfg->de_type == SUN8I_MIXER_DE3 ? DE3_BLD_BASE : DE2_BLD_BASE;
 }
 
+static inline struct regmap *
+sun8i_blender_regmap(struct sun8i_mixer *mixer)
+{
+	return mixer->engine.regs;
+}
+
 static inline u32
 sun8i_channel_base(struct sun8i_mixer *mixer, int channel)
 {
