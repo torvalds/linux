df7996076b1e1ba8a0690542d0e40f703f2f9eb7
meowmeowbeanz <meowmeowbeanz@gmx.com>
2025-05-28 10:56:58 -0700

    ASoC: amd: yc: Add support for Lenovo Yoga 7 16ARP8
    Add DMI quirk entry for Lenovo Yoga 7 16ARP8 (83BS) to enable
    digital microphone support via ACP driver.
    
    Fixes microphone detection on this specific model which was
    previously falling back to non-functional generic audio paths.
    
    Tested-by: meowmeowbeanz <meowmeowbeanz@gmx.com>
    Signed-off-by: meowmeowbeanz <meowmeowbeanz@gmx.com>
    Link: https://patch.msgid.link/20250528-yoga-7-16arp8-microphone-fix-v1-1-bfeed2ecd0c2@gmx.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/yc/acp6x-mach.c
+++ sound/soc/amd/yc/acp6x-mach.c
@@ -315,6 +315,13 @@ static const struct dmi_system_id yc_acp_quirk_table[] = {
 		.driver_data = &acp6x_card,
 		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "LENOVO"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "83BS"),
+		}
+	},
+	{
+		.driver_data = &acp6x_card,
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "LENOVO"),
 			DMI_MATCH(DMI_PRODUCT_NAME, "83HN"),
 		}
 	},
