dc0a083948040ff364d065da8bb50c29f77a39ad
Ard Biesheuvel <ardb@kernel.org>
2025-05-31 14:30:05 +0200

    arm64: Work around convergence issue with LLD linker
    LLD will occasionally error out with a '__init_end does not converge'
    error if INIT_IDMAP_DIR_SIZE is defined in terms of _end, as this
    results in a circular dependency.
    
    Counter this by dimensioning the initial IDMAP page tables based on a
    new boundary marker 'kimage_limit', and define it such that its value
    should not change as a result of the initdata segment being pushed over
    a 64k segment boundary due to changes in INIT_IDMAP_DIR_SIZE, provided
    that its value doesn't change by more than 2M between linker passes.
    
    Reported-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Link: https://lore.kernel.org/r/20250531123005.3866382-2-ardb+git@google.com
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/kernel/image-vars.h
+++ arch/arm64/kernel/image-vars.h
@@ -146,6 +146,17 @@ KVM_NVHE_ALIAS(kvm_protected_mode_initialized);
 _kernel_codesize = ABSOLUTE(__inittext_end - _text);
 #endif
 
+/*
+ * LLD will occasionally error out with a '__init_end does not converge' error
+ * if INIT_IDMAP_DIR_SIZE is defined in terms of _end, as this results in a
+ * circular dependency. Counter this by dimensioning the initial IDMAP page
+ * tables based on kimage_limit, which is defined such that its value should
+ * not change as a result of the initdata segment being pushed over a 64k
+ * segment boundary due to changes in INIT_IDMAP_DIR_SIZE, provided that its
+ * value doesn't change by more than 2M between linker passes.
+ */
+kimage_limit = ALIGN(ABSOLUTE(_end + SZ_64K), SZ_2M);
+
 #undef ASSERT
 
 #endif /* __ARM64_KERNEL_IMAGE_VARS_H */
