4b9f6d13e4c45f5f63b55a9930b2b86781630bda
Filipe Manana <fdmanana@suse.com>
2025-06-02 13:46:23 +0100

    btrfs: add comment for optimization in free_extent_buffer()
    There's this special atomic compare and exchange logic which serves to
    avoid locking the extent buffers refs_lock spinlock and therefore reduce
    lock contention, so add a comment to make it more obvious.
    
    Reviewed-by: Boris Burkov <boris@bur.io>
    Signed-off-by: Filipe Manana <fdmanana@suse.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -3494,6 +3494,7 @@ void free_extent_buffer(struct extent_buffer *eb)
 			break;
 		}
 
+		/* Optimization to avoid locking eb->refs_lock. */
 		if (atomic_try_cmpxchg(&eb->refs, &refs, refs - 1))
 			return;
 	}
