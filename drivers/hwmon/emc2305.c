7114b74d99a3cd588da4ecb6011858c06f8408a1
Florin Leotescu <florin.leotescu@nxp.com>
2025-06-03 14:31:22 +0300

    hwmon: (emc2305) Add support for PWM frequency, polarity and output
    Add three new attributes to the driver data structures to support
    configuration of PWM frequency, PWM polarity and PWM output config.
    
    Signed-off-by: Florin Leotescu <florin.leotescu@nxp.com>
    Link: https://lore.kernel.org/r/20250603113125.3175103-2-florin.leotescu@oss.nxp.com
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/emc2305.c
+++ drivers/hwmon/emc2305.c
@@ -89,8 +89,11 @@ struct emc2305_cdev_data {
  * @hwmon_dev: hwmon device
  * @max_state: maximum cooling state of the cooling device
  * @pwm_num: number of PWM channels
+ * @pwm_output_mask: PWM output mask
+ * @pwm_polarity_mask: PWM polarity mask
  * @pwm_separate: separate PWM settings for every channel
  * @pwm_min: array of minimum PWM per channel
+ * @pwm_freq: array of PWM frequency per channel
  * @cdev_data: array of cooling devices data
  */
 struct emc2305_data {
@@ -98,8 +101,11 @@ struct emc2305_data {
 	struct device *hwmon_dev;
 	u8 max_state;
 	u8 pwm_num;
+	u8 pwm_output_mask;
+	u8 pwm_polarity_mask;
 	bool pwm_separate;
 	u8 pwm_min[EMC2305_PWM_MAX];
+	u16 pwm_freq[EMC2305_PWM_MAX];
 	struct emc2305_cdev_data cdev_data[EMC2305_PWM_MAX];
 };
 
