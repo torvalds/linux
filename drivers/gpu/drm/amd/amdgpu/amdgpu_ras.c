3bdf8dd84e55e9ea0806eb62bd4437c8c7746521
Lijo Lazar <lijo.lazar@amd.com>
2025-06-04 09:36:09 +0530

    drm/amdgpu: Clear reset flags from ras context
    Once RAS errors are cleared with appropriate recovery mechanism, clear
    reset flags also from RAS context. Otherwise, stale flag values could
    affect the subsequent RAS reset handling on the device.
    
    Signed-off-by: Lijo Lazar <lijo.lazar@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ras.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ras.c
@@ -4414,8 +4414,10 @@ void amdgpu_ras_clear_err_state(struct amdgpu_device *adev)
 	struct amdgpu_ras *ras;
 
 	ras = amdgpu_ras_get_context(adev);
-	if (ras)
+	if (ras) {
 		ras->ras_err_state = 0;
+		ras->gpu_reset_flags = 0;
+	}
 }
 
 void amdgpu_ras_set_err_poison(struct amdgpu_device *adev,
