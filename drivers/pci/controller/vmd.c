255c891533d89f5d7339076468a98afc947c4a73
George D Sworo <george.d.sworo@intel.com>
2025-06-06 14:02:30 -0700

    PCI: vmd: Add VMD Device ID Support for Panther Lake (PTL)-H/P/U
    Add VMD Device ID Support for PTL-H/P/U processors.
    
    Signed-off-by: George D Sworo <george.d.sworo@intel.com>
    [mani: expanded PTL]
    Signed-off-by: Manivannan Sadhasivam <mani@kernel.org>
    Link: https://patch.msgid.link/20250606210230.340803-2-george.d.sworo@intel.com

--- drivers/pci/controller/vmd.c
+++ drivers/pci/controller/vmd.c
@@ -1129,6 +1129,8 @@ static const struct pci_device_id vmd_ids[] = {
                 .driver_data = VMD_FEATS_CLIENT,},
 	{PCI_VDEVICE(INTEL, 0xb06f),
                 .driver_data = VMD_FEATS_CLIENT,},
+	{PCI_VDEVICE(INTEL, 0xb07f),
+                .driver_data = VMD_FEATS_CLIENT,},
 	{0,}
 };
 MODULE_DEVICE_TABLE(pci, vmd_ids);
