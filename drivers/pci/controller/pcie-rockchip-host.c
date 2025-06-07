1fdb13f92388dfc936624b0a0d6abae362b0ace3
Hans Zhang <18255117159@163.com>
2025-06-08 00:02:01 +0800

    PCI: rockchip-host: Remove unused header includes
    Clean up the driver by removing unnecessary header includes that are no
    longer referenced after the recent refactoring.
    
    Signed-off-by: Hans Zhang <18255117159@163.com>
    [mani: commit message rewording]
    Signed-off-by: Manivannan Sadhasivam <mani@kernel.org>
    Reviewed-by: Manivannan Sadhasivam <mani@kernel.org>
    Acked-by: Shawn Lin <shawn.lin@rock-chips.com>
    Link: https://patch.msgid.link/20250607160201.807043-4-18255117159@163.com

--- drivers/pci/controller/pcie-rockchip-host.c
+++ drivers/pci/controller/pcie-rockchip-host.c
@@ -12,26 +12,17 @@
  */
 
 #include <linux/bitrev.h>
-#include <linux/clk.h>
-#include <linux/delay.h>
 #include <linux/gpio/consumer.h>
-#include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/iopoll.h>
 #include <linux/irq.h>
 #include <linux/irqchip/chained_irq.h>
 #include <linux/irqdomain.h>
-#include <linux/kernel.h>
-#include <linux/mfd/syscon.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_pci.h>
-#include <linux/pci.h>
-#include <linux/pci_ids.h>
 #include <linux/phy/phy.h>
 #include <linux/platform_device.h>
-#include <linux/reset.h>
-#include <linux/regmap.h>
 
 #include "../pci.h"
 #include "pcie-rockchip.h"
