080241946ec542ada349fced93936e2aebb51c75
Dr. David Alan Gilbert <linux@treblig.org>
2025-06-09 02:19:21 +0100

    utf8: Remove unused utf8_normalize
    utf8_normalize() was added in 2019 as part of
    commit 9d53690f0d4e ("unicode: implement higher level API for string
    handling")
    but has remained unused.
    
    (I think because the other higher level routines added by that patch
    normalise as part of their operations)
    
    Remove it.
    
    Signed-off-by: Dr. David Alan Gilbert <linux@treblig.org>
    Signed-off-by: Gabriel Krisman Bertazi <krisman@suse.de>

--- fs/unicode/utf8-core.c
+++ fs/unicode/utf8-core.c
@@ -138,28 +138,6 @@ int utf8_casefold_hash(const struct unicode_map *um, const void *salt,
 }
 EXPORT_SYMBOL(utf8_casefold_hash);
 
-int utf8_normalize(const struct unicode_map *um, const struct qstr *str,
-		   unsigned char *dest, size_t dlen)
-{
-	struct utf8cursor cur;
-	ssize_t nlen = 0;
-
-	if (utf8ncursor(&cur, um, UTF8_NFDI, str->name, str->len) < 0)
-		return -EINVAL;
-
-	for (nlen = 0; nlen < dlen; nlen++) {
-		int c = utf8byte(&cur);
-
-		dest[nlen] = c;
-		if (!c)
-			return nlen;
-		if (c == -1)
-			break;
-	}
-	return -EINVAL;
-}
-EXPORT_SYMBOL(utf8_normalize);
-
 static const struct utf8data *find_table_version(const struct utf8data *table,
 		size_t nr_entries, unsigned int version)
 {
