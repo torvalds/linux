5d939fbdd480cdf276eccc01eda3ed41e37d3f8a
Len Brown <len.brown@intel.com>
2025-06-09 23:34:04 -0400

    tools/power turbostat: regression fix: --show C1E%
    The new default idle counter groupings broke "--show C1E%" (or any other C-state %)
    
    Also delete a stray debug printf from the same offending commit.
    
    Reported-by: Zhang Rui <rui.zhang@intel.com>
    Fixes: ec4acd3166d8 ("tools/power turbostat: disable "cpuidle" invocation counters, by default")
    Signed-off-by: Len Brown <len.brown@intel.com>

--- tools/power/x86/turbostat/turbostat.c
+++ tools/power/x86/turbostat/turbostat.c
@@ -2429,7 +2429,6 @@ unsigned long long bic_lookup(char *name_list, enum show_hide_mode mode)
 
 		}
 		if (i == MAX_BIC) {
-			fprintf(stderr, "deferred %s\n", name_list);
 			if (mode == SHOW_LIST) {
 				deferred_add_names[deferred_add_index++] = name_list;
 				if (deferred_add_index >= MAX_DEFERRED) {
@@ -10536,9 +10535,6 @@ void probe_cpuidle_residency(void)
 	int state;
 	int min_state = 1024, max_state = 0;
 	char *sp;
-
-	if (!DO_BIC(BIC_pct_idle))
-		return;
 
 	for (state = 10; state >= 0; --state) {
 
