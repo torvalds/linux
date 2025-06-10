fbf355f32b98903c572544ead7fc0db79583c3a2
Srish Srinivasan <ssrish@linux.ibm.com>
2025-06-11 02:49:05 +0530

    powerpc/pseries: Correct secvar format representation for static key management
    On a PLPKS enabled PowerVM LPAR, the secvar format property for static
    key management is misrepresented as "ibm,plpks-sb-unknown", creating
    reason for confusion.
    
    Static key management mode uses fixed, built-in keys. Dynamic key
    management mode allows keys to be updated in production to handle
    security updates without firmware rebuilds.
    
    Define a function named plpks_get_sb_keymgmt_mode() to retrieve the
    key management mode based on the existence of the SB_VERSION property
    in the firmware.
    
    Set the secvar format property to either "ibm,plpks-sb-v<version>" or
    "ibm,plpks-sb-v0" based on the key management mode, and return the
    length of the secvar format property.
    
    Co-developed-by: Souradeep <soura@imap.linux.ibm.com>
    Signed-off-by: Souradeep <soura@imap.linux.ibm.com>
    Signed-off-by: Srish Srinivasan <ssrish@linux.ibm.com>
    Tested-by: R Nageswara Sastry <rnsastry@linux.ibm.com>
    Reviewed-by: Mimi Zohar <zohar@linux.ibm.com>
    Reviewed-by: Stefan Berger <stefanb@linux.ibm.com>
    Reviewed-by: Nayna Jain <nayna@linux.ibm.com>
    Reviewed-by: Andrew Donnellan <ajd@linux.ibm.com>
    Signed-off-by: Madhavan Srinivasan <maddy@linux.ibm.com>
    Link: https://patch.msgid.link/20250610211907.101384-2-ssrish@linux.ibm.com

--- arch/powerpc/platforms/pseries/plpks-secvar.c
+++ arch/powerpc/platforms/pseries/plpks-secvar.c
@@ -152,39 +152,55 @@ err:
 	return rc;
 }
 
-// PLPKS dynamic secure boot doesn't give us a format string in the same way OPAL does.
-// Instead, report the format using the SB_VERSION variable in the keystore.
-// The string is made up by us, and takes the form "ibm,plpks-sb-v<n>" (or "ibm,plpks-sb-unknown"
-// if the SB_VERSION variable doesn't exist). Hypervisor defines the SB_VERSION variable as a
-// "1 byte unsigned integer value".
+/*
+ * Return the key management mode.
+ *
+ * SB_VERSION is defined as a "1 byte unsigned integer value", taking values
+ * starting from 1. It is owned by the Partition Firmware and its presence
+ * indicates that the key management mode is dynamic. Any failure in
+ * reading SB_VERSION defaults the key management mode to static. The error
+ * codes -ENOENT or -EPERM are expected in static key management mode. An
+ * unexpected error code will have to be investigated. Only signed variables
+ * have null bytes in their names, SB_VERSION does not.
+ *
+ * Return 0 to indicate that the key management mode is static. Otherwise
+ * return the SB_VERSION value to indicate that the key management mode is
+ * dynamic.
+ */
+static u8 plpks_get_sb_keymgmt_mode(void)
+{
+	u8 mode;
+	ssize_t rc;
+	struct plpks_var var = {
+		.component = NULL,
+		.name = "SB_VERSION",
+		.namelen = 10,
+		.datalen = 1,
+		.data = &mode,
+	};
+
+	rc = plpks_read_fw_var(&var);
+	if (rc) {
+		if (rc != -ENOENT && rc != -EPERM)
+			pr_info("Error %ld reading SB_VERSION from firmware\n", rc);
+		mode = 0;
+	}
+	return mode;
+}
+
+/*
+ * PLPKS dynamic secure boot doesn't give us a format string in the same way
+ * OPAL does. Instead, report the format using the SB_VERSION variable in the
+ * keystore. The string, made up by us, takes the form of either
+ * "ibm,plpks-sb-v<n>" or "ibm,plpks-sb-v0", based on the key management mode,
+ * and return the length of the secvar format property.
+ */
 static ssize_t plpks_secvar_format(char *buf, size_t bufsize)
 {
-	struct plpks_var var = {0};
-	ssize_t ret;
-	u8 version;
+	u8 mode;
 
-	var.component = NULL;
-	// Only the signed variables have null bytes in their names, this one doesn't
-	var.name = "SB_VERSION";
-	var.namelen = strlen(var.name);
-	var.datalen = 1;
-	var.data = &version;
-
-	// Unlike the other vars, SB_VERSION is owned by firmware instead of the OS
-	ret = plpks_read_fw_var(&var);
-	if (ret) {
-		if (ret == -ENOENT) {
-			ret = snprintf(buf, bufsize, "ibm,plpks-sb-unknown");
-		} else {
-			pr_err("Error %ld reading SB_VERSION from firmware\n", ret);
-			ret = -EIO;
-		}
-		goto err;
-	}
-
-	ret = snprintf(buf, bufsize, "ibm,plpks-sb-v%hhu", version);
-err:
-	return ret;
+	mode = plpks_get_sb_keymgmt_mode();
+	return snprintf(buf, bufsize, "ibm,plpks-sb-v%hhu", mode);
 }
 
 static int plpks_max_size(u64 *max_size)
