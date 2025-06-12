934eee0ce35cde5ea8a570de94cf5903cb44518d
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2025-06-12 23:11:27 +0300

    ACPI: proc: Remove unused header
    With `make W=1` build we get a warning:
    
    drivers/acpi/proc.c: warning: EXPORT_SYMBOL() is not used, but #include <linux/export.h> is present
    
    Fix it by removing unused inclusion.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Link: https://patch.msgid.link/20250612201321.3536493-4-andriy.shevchenko@linux.intel.com
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/proc.c
+++ drivers/acpi/proc.c
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
-#include <linux/export.h>
 #include <linux/suspend.h>
 #include <linux/bcd.h>
 #include <linux/acpi.h>
