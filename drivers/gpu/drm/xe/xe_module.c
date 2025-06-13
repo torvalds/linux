a37128ba613ad6a5f81f382fa3cfe5c4a6527310
Lucas De Marchi <lucas.demarchi@intel.com>
2025-06-13 13:00:37 -0700

    drm/xe/guc: Default log level to non-verbose
    Currently xe sets the guc log level to a verbose level since it's useful
    to debug hangs and general development. However the verbose level may
    already be too much and affect performance.
    
    Michal Mrozek did some tests with the L0 compute stack for submission
    latency with ULLS disabled. Below are the normalized numbers with log
    level 3 (the current default) as baseline for each test:
    
                              Test \ Log Level                        3      0      1      2
     ----------------------------------------------------------- ------ ------ ------ ------
      BestWalkerNthCommandListSubmission(CmdListCount=2)           1.00   0.63   0.63   0.96
      BestWalkerNthSubmission(KernelCount=2)                       1.00   0.62   0.63   0.96
      BestWalkerNthSubmissionImmediate(KernelCount=2)              1.00   0.58   0.58   0.85
      BestWalkerSubmission                                         1.00   0.62   0.62   0.96
      BestWalkerSubmissionImmediate                                1.00   0.63   0.62   0.96
      BestWalkerSubmissionImmediateMultiCmdlists(cmdlistCount=2)   1.00   0.58   0.58   0.86
      BestWalkerSubmissionImmediateMultiCmdlists(cmdlistCount=4)   1.00   0.70   0.70   0.83
      BestWalkerSubmissionImmediateMultiCmdlists(cmdlistCount=8)   1.00   0.53   0.52   0.78
    
    Log level 2 is the first "verbose level" for GuC, where the biggest
    difference happens. Keep log level 3 for CONFIG_DRM_XE_DEBUG, but switch
    to 1, i.e.  GUC_LOG_LEVEL_NON_VERBOSE, for "normal" builds.
    
    Cc: Michal Mrozek <michal.mrozek@intel.com>
    Cc: John Harrison <John.C.Harrison@Intel.com>
    Reviewed-by: John Harrison <John.C.Harrison@Intel.com>
    Link: https://lore.kernel.org/r/20250613-guc-log-level-v2-1-cb84a63e49fe@intel.com
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- drivers/gpu/drm/xe/xe_module.c
+++ drivers/gpu/drm/xe/xe_module.c
@@ -20,7 +20,7 @@
 
 struct xe_modparam xe_modparam = {
 	.probe_display = true,
-	.guc_log_level = 3,
+	.guc_log_level = IS_ENABLED(CONFIG_DRM_XE_DEBUG) ? 3 : 1,
 	.force_probe = CONFIG_DRM_XE_FORCE_PROBE,
 	.wedged_mode = 1,
 	.svm_notifier_size = 512,
