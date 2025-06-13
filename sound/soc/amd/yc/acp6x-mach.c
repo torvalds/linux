13b86ea92ebf0fa587fbadfb8a60ca2e9993203f
Raven Black <ravenblack@gmail.com>
2025-06-13 07:51:25 -0400

    ASoC: amd: yc: update quirk data for HP Victus
    Make the internal microphone work on HP Victus laptops.
    
    Signed-off-by: Raven Black <ravenblack@gmail.com>
    Link: https://patch.msgid.link/20250613-support-hp-victus-microphone-v1-1-bebc4c3a2041@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/yc/acp6x-mach.c
+++ sound/soc/amd/yc/acp6x-mach.c
@@ -525,6 +525,13 @@ static const struct dmi_system_id yc_acp_quirk_table[] = {
 		.driver_data = &acp6x_card,
 		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "HP"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "Victus by HP Gaming Laptop 15-fb2xxx"),
+		}
+	},
+	{
+		.driver_data = &acp6x_card,
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "HP"),
 			DMI_MATCH(DMI_BOARD_NAME, "8A42"),
 		}
 	},
