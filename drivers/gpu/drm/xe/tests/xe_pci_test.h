33c77e00f2ee74406474c2b6e2adab45f25e4bf2
Michal Wajdeczko <michal.wajdeczko@intel.com>
2025-06-14 20:24:46 +0200

    drm/xe/tests: Convert xe_pci tests to parametrized tests
    Instead of looping over known IP descriptors within single test
    case, without any diagnostics which IP descriptor is eventually
    broken, define kunit parameter generators with IP descriptors,
    and make existing xe_pci tests fully parametrized:
    
     [ ] =================== xe_pci (2 subtests) ====================
     [ ] ==================== check_graphics_ip  ====================
     [ ] [PASSED] 12.70 Xe_LPG
     [ ] [PASSED] 12.71 Xe_LPG
     [ ] [PASSED] 12.74 Xe_LPG+
     [ ] [PASSED] 20.01 Xe2_HPG
     [ ] [PASSED] 20.04 Xe2_LPG
     [ ] [PASSED] 30.00 Xe3_LPG
     [ ] [PASSED] 30.01 Xe3_LPG
     [ ] ================ [PASSED] check_graphics_ip ================
     [ ] ===================== check_media_ip  ======================
     [ ] [PASSED] 13.00 Xe_LPM+
     [ ] [PASSED] 13.01 Xe2_HPM
     [ ] [PASSED] 20.00 Xe2_LPM
     [ ] [PASSED] 30.00 Xe3_LPM
     [ ] ================= [PASSED] check_media_ip ==================
     [ ] ===================== [PASSED] xe_pci ======================
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Lucas De Marchi <lucas.demarchi@intel.com>
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Reviewed-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>
    Link: https://lore.kernel.org/r/20250614182446.2024-1-michal.wajdeczko@intel.com
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/xe/tests/xe_pci_test.h
+++ drivers/gpu/drm/xe/tests/xe_pci_test.h
@@ -12,14 +12,6 @@
 #include "xe_sriov_types.h"
 
 struct xe_device;
-struct xe_graphics_desc;
-struct xe_media_desc;
-
-typedef void (*xe_graphics_fn)(const struct xe_graphics_desc *);
-typedef void (*xe_media_fn)(const struct xe_media_desc *);
-
-void xe_call_for_each_graphics_ip(xe_graphics_fn xe_fn);
-void xe_call_for_each_media_ip(xe_media_fn xe_fn);
 
 struct xe_pci_fake_data {
 	enum xe_sriov_mode sriov_mode;
@@ -33,6 +25,8 @@ struct xe_pci_fake_data {
 
 int xe_pci_fake_device_init(struct xe_device *xe);
 
+const void *xe_pci_graphics_ip_gen_param(const void *prev, char *desc);
+const void *xe_pci_media_ip_gen_param(const void *prev, char *desc);
 const void *xe_pci_live_device_gen_param(const void *prev, char *desc);
 
 #endif
