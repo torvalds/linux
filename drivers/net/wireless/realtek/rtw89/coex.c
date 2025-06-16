c5ef95e29166ac868a96ae6e2c5b6357e3495f10
Ching-Te Ku <ku920601@realtek.com>
2025-06-16 17:02:52 +0800

    wifi: rtw89: coex: Update Wi-Fi/Bluetooth coexistence version to 9.0.0
    The version 9 support the WiFi7 related functions.
    
    Signed-off-by: Ching-Te Ku <ku920601@realtek.com>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://patch.msgid.link/20250616090252.51098-12-pkshih@realtek.com

--- drivers/net/wireless/realtek/rtw89/coex.c
+++ drivers/net/wireless/realtek/rtw89/coex.c
@@ -11,7 +11,7 @@
 #include "ps.h"
 #include "reg.h"
 
-#define RTW89_COEX_VERSION 0x07000413
+#define RTW89_COEX_VERSION 0x09000013
 #define FCXDEF_STEP 50 /* MUST <= FCXMAX_STEP and match with wl fw*/
 #define BTC_E2G_LIMIT_DEF 80
 
