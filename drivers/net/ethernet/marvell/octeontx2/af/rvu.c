0289c51f889e435a7626931688023a80090d9c5e
Subbaraya Sundeep <sbhatta@marvell.com>
2025-06-18 19:27:16 +0530

    octeontx2-af: Fix rvu_mbox_init return path
    rvu_mbox_init function makes use of error path for
    freeing memory which are local to the function in
    both success and failure conditions. This is unusual hence
    fix it by returning zero on success. With new cn20k code this
    is freeing valid memory in success case also.
    
    Fixes: e53ee4acb220 ("octeontx2-af: CN20k basic mbox operations and structures")
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -2573,7 +2573,11 @@ static int rvu_mbox_init(struct rvu *rvu, struct mbox_wq_info *mw,
 		mwork->rvu = rvu;
 		INIT_WORK(&mwork->work, mbox_up_handler);
 	}
-	goto free_regions;
+
+	kfree(mbox_regions);
+	bitmap_free(pf_bmap);
+
+	return 0;
 
 exit:
 	destroy_workqueue(mw->mbox_wq);
