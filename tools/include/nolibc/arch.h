cc6dc5fbecfdf2de3ec02c15754d3fd4879e32ff
Willy Tarreau <w@1wt.eu>
2025-06-18 20:30:30 +0200

    tools/nolibc: merge i386 and x86_64 into a single x86 arch
    This remained the only exception to the kernel's architectures
    organization and it's always a bit cumbersome to deal with. Let's merge
    i386 and x86_64 into x86. This will result in a single arch-x86.h file
    by default, and we'll no longer need to merge the two manually during
    installation. Requesting either i386 or x86_64 will also result in
    installing x86.
    
    Acked-by: Thomas Weißschuh <linux@weissschuh.net>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- tools/include/nolibc/arch.h
+++ tools/include/nolibc/arch.h
@@ -15,10 +15,8 @@
 #ifndef _NOLIBC_ARCH_H
 #define _NOLIBC_ARCH_H
 
-#if defined(__x86_64__)
-#include "arch-x86_64.h"
-#elif defined(__i386__) || defined(__i486__) || defined(__i586__) || defined(__i686__)
-#include "arch-i386.h"
+#if defined(__x86_64__) || defined(__i386__) || defined(__i486__) || defined(__i586__) || defined(__i686__)
+#include "arch-x86.h"
 #elif defined(__ARM_EABI__)
 #include "arch-arm.h"
 #elif defined(__aarch64__)
