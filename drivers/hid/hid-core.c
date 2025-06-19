37a9acb971c2f338e7a1b602b0ee40ad70668e81
Chelsy Ratnawat <chelsyratnawat2001@gmail.com>
2025-06-19 08:46:27 -0700

    HID: replace scnprintf() with sysfs_emit()
    Documentation/filesystems/sysfs.rst mentions that show() should only
    use sysfs_emit() or sysfs_emit_at() when formating the value to be
    returned to user space. So replace scnprintf() with sysfs_emit().
    
    Signed-off-by: Chelsy Ratnawat <chelsyratnawat2001@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.com>

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -2797,7 +2797,7 @@ static ssize_t modalias_show(struct device *dev, struct device_attribute *a,
 {
 	struct hid_device *hdev = container_of(dev, struct hid_device, dev);
 
-	return scnprintf(buf, PAGE_SIZE, "hid:b%04Xg%04Xv%08Xp%08X\n",
+	return sysfs_emit(buf, "hid:b%04Xg%04Xv%08Xp%08X\n",
 			 hdev->bus, hdev->group, hdev->vendor, hdev->product);
 }
 static DEVICE_ATTR_RO(modalias);
