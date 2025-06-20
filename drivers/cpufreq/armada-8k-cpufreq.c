b1b41bc072baf7301b1ae95fe417de09a5ad47e2
Arnd Bergmann <arnd@arndb.de>
2025-06-20 13:14:53 +0200

    cpufreq: armada-8k: make both cpu masks static
    An earlier patch marked one of the two CPU masks as 'static' to reduce stack
    usage, but if CONFIG_NR_CPUS is large enough, the function still produces
    a warning for compile testing:
    
    drivers/cpufreq/armada-8k-cpufreq.c: In function 'armada_8k_cpufreq_init':
    drivers/cpufreq/armada-8k-cpufreq.c:203:1: error: the frame size of 1416 bytes is larger than 1408 bytes [-Werror=frame-larger-than=]
    
    Normally this should be done using alloc_cpumask_var(), but since the
    driver already has a static mask and the probe function is not called
    concurrently, use the same trick for both.
    
    Fixes: 1ffec650d07f ("cpufreq: armada-8k: Avoid excessive stack usage")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/cpufreq/armada-8k-cpufreq.c
+++ drivers/cpufreq/armada-8k-cpufreq.c
@@ -132,7 +132,7 @@ static int __init armada_8k_cpufreq_init(void)
 	int ret = 0, opps_index = 0, cpu, nb_cpus;
 	struct freq_table *freq_tables;
 	struct device_node *node;
-	static struct cpumask cpus;
+	static struct cpumask cpus, shared_cpus;
 
 	node = of_find_matching_node_and_match(NULL, armada_8k_cpufreq_of_match,
 					       NULL);
@@ -154,7 +154,6 @@ static int __init armada_8k_cpufreq_init(void)
 	 * divisions of it).
 	 */
 	for_each_cpu(cpu, &cpus) {
-		struct cpumask shared_cpus;
 		struct device *cpu_dev;
 		struct clk *clk;
 
