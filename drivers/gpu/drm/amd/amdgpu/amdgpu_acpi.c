e36519f5c8035f1685b39690ed330ac3b2c978a2
Pratap Nirujogi <pratap.nirujogi@amd.com>
2025-06-24 19:15:00 -0400

    drm/amd/amdgpu: Initialize swnode for ISP MFD device
    Create amd_isp_capture MFD device with swnode initialized to
    isp specific software_node part of fwnode graph in amd_isp4
    x86/platform driver. The isp driver use this swnode handle
    to retrieve the critical properties (data-lanes, mipi phyid,
    link-frequencies etc.) required for camera to work on AMD ISP4
    based targets.
    
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Pratap Nirujogi <pratap.nirujogi@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_acpi.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_acpi.c
@@ -1545,7 +1545,7 @@ static int isp_match_acpi_device_ids(struct device *dev, const void *data)
 	return acpi_match_device(data, dev) ? 1 : 0;
 }
 
-int amdgpu_acpi_get_isp4_dev_hid(u8 (*hid)[ACPI_ID_LEN])
+int amdgpu_acpi_get_isp4_dev(struct acpi_device **dev)
 {
 	struct device *pdev __free(put_device) = NULL;
 	struct acpi_device *acpi_pdev;
@@ -1559,7 +1559,7 @@ int amdgpu_acpi_get_isp4_dev_hid(u8 (*hid)[ACPI_ID_LEN])
 	if (!acpi_pdev)
 		return -ENODEV;
 
-	strscpy(*hid, acpi_device_hid(acpi_pdev));
+	*dev = acpi_pdev;
 
 	return 0;
 }
