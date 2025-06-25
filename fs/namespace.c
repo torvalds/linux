f6cc2f4e3d304c93b44c80f50430aa40e080cc3c
Al Viro <viro@zeniv.linux.org.uk>
2025-06-25 15:02:11 -0400

    invent_group_ids(): zero ->mnt_group_id always implies !IS_MNT_SHARED()
    All places where we call set_mnt_shared() are guaranteed to have
    non-zero ->mnt_group_id - either by explicit test, or by having
    done successful invent_group_ids() covering the same mount since
    we'd grabbed namespace_sem.
    
    The opposite combination (non-zero ->mnt_group_id and !IS_MNT_SHARED())
    *is* possible - it means that we have allocated group id, but didn't
    get around to set_mnt_shared() yet; such state is transient -
    by the time we do namespace_unlock(), we must either do set_mnt_shared()
    or unroll the group id allocations by cleanup_group_ids().
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/namespace.c
+++ fs/namespace.c
@@ -2516,7 +2516,7 @@ static int invent_group_ids(struct mount *mnt, bool recurse)
 	struct mount *p;
 
 	for (p = mnt; p; p = recurse ? next_mnt(p, mnt) : NULL) {
-		if (!p->mnt_group_id && !IS_MNT_SHARED(p)) {
+		if (!p->mnt_group_id) {
 			int err = mnt_alloc_group_id(p);
 			if (err) {
 				cleanup_group_ids(mnt, p);
