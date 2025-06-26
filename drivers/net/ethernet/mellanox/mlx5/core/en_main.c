7012d4f3c7a82008113974108bf0c9c0553b424a
Fushuai Wang <wangfushuai@baidu.com>
2025-06-26 13:30:03 +0800

    net/mlx5e: Fix error handling in RQ memory model registration
    Currently when xdp_rxq_info_reg_mem_model() fails in the XSK path, the
    error handling incorrectly jumps to err_destroy_page_pool. While this
    may not cause errors, we should make it jump to the correct location.
    
    Signed-off-by: Fushuai Wang <wangfushuai@baidu.com>
    Reviewed-by: Zhu Yanjun <yanjun.zhu@linux.dev>
    Acked-by: Dragos Tatulea <dtatulea@nvidia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlx5/core/en_main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -952,6 +952,8 @@ static int mlx5e_alloc_rq(struct mlx5e_params *params,
 	if (xsk) {
 		err = xdp_rxq_info_reg_mem_model(&rq->xdp_rxq,
 						 MEM_TYPE_XSK_BUFF_POOL, NULL);
+		if (err)
+			goto err_free_by_rq_type;
 		xsk_pool_set_rxq_info(rq->xsk_pool, &rq->xdp_rxq);
 	} else {
 		/* Create a page_pool and register it with rxq */
@@ -985,12 +987,13 @@ static int mlx5e_alloc_rq(struct mlx5e_params *params,
 		}
 		if (!rq->hd_page_pool)
 			rq->hd_page_pool = rq->page_pool;
-		if (xdp_rxq_info_is_reg(&rq->xdp_rxq))
+		if (xdp_rxq_info_is_reg(&rq->xdp_rxq)) {
 			err = xdp_rxq_info_reg_mem_model(&rq->xdp_rxq,
 							 MEM_TYPE_PAGE_POOL, rq->page_pool);
+			if (err)
+				goto err_destroy_page_pool;
+		}
 	}
-	if (err)
-		goto err_destroy_page_pool;
 
 	for (i = 0; i < wq_sz; i++) {
 		if (rq->wq_type == MLX5_WQ_TYPE_LINKED_LIST_STRIDING_RQ) {
