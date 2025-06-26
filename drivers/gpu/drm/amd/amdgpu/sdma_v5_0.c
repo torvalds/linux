4b1df3bad2e283dc299ac38fb94c379b35173a86
Alex Deucher <alexander.deucher@amd.com>
2025-06-26 09:52:55 -0400

    drm/amdgpu/sdma5: re-emit unprocessed state on ring reset
    Re-emit the unprocessed state after resetting the queue.
    
    Reviewed-by: Jesse Zhang <Jesse.Zhang@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/sdma_v5_0.c
+++ drivers/gpu/drm/amd/amdgpu/sdma_v5_0.c
@@ -1555,11 +1555,15 @@ static int sdma_v5_0_reset_queue(struct amdgpu_ring *ring,
 		return -EINVAL;
 	}
 
-	amdgpu_amdkfd_suspend(adev, true);
-	r = amdgpu_sdma_reset_engine(adev, ring->me, false);
-	amdgpu_amdkfd_resume(adev, true);
+	amdgpu_ring_reset_helper_begin(ring, timedout_fence);
 
-	return r;
+	amdgpu_amdkfd_suspend(adev, true);
+	r = amdgpu_sdma_reset_engine(adev, ring->me, true);
+	amdgpu_amdkfd_resume(adev, true);
+	if (r)
+		return r;
+
+	return amdgpu_ring_reset_helper_end(ring, timedout_fence);
 }
 
 static int sdma_v5_0_stop_queue(struct amdgpu_ring *ring)
