39dac316f09ae5a0930878d2cae8aea113648b5a
Joel Granados <joel.granados@kernel.org>
2025-06-27 09:00:05 +0200

    sysctl: Removed unused variable
    Remove unaligned_dump_stack from sysctl.h; it is no longer used or
    defined.
    
    Signed-off-by: Joel Granados <joel.granados@kernel.org>

--- include/linux/sysctl.h
+++ include/linux/sysctl.h
@@ -243,7 +243,6 @@ int do_proc_douintvec(const struct ctl_table *table, int write,
 		      void *data);
 
 extern int unaligned_enabled;
-extern int unaligned_dump_stack;
 extern int no_unaligned_warning;
 
 #else /* CONFIG_SYSCTL */
