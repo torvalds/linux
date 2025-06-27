c91e140c82eb58724c435f623702e51cc7896646
Ranjan Kumar <ranjan.kumar@broadcom.com>
2025-06-28 01:15:38 +0530

    scsi: mpi3mr: Serialize admin queue BAR writes on 32-bit systems
    On 32-bit systems, 64-bit BAR writes to admin queue registers are
    performed as two 32-bit writes. Without locking, this can cause partial
    writes when accessed concurrently.
    
    Updated per-queue spinlocks is used to serialize these writes and prevent
    race conditions.
    
    Fixes: 824a156633df ("scsi: mpi3mr: Base driver code")
    Cc: stable@vger.kernel.org
    Signed-off-by: Ranjan Kumar <ranjan.kumar@broadcom.com>
    Link: https://lore.kernel.org/r/20250627194539.48851-4-ranjan.kumar@broadcom.com
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/mpi3mr/mpi3mr.h
+++ drivers/scsi/mpi3mr/mpi3mr.h
@@ -1137,6 +1137,8 @@ struct scmd_priv {
  * @logdata_buf: Circular buffer to store log data entries
  * @logdata_buf_idx: Index of entry in buffer to store
  * @logdata_entry_sz: log data entry size
+ * @adm_req_q_bar_writeq_lock: Admin request queue lock
+ * @adm_reply_q_bar_writeq_lock: Admin reply queue lock
  * @pend_large_data_sz: Counter to track pending large data
  * @io_throttle_data_length: I/O size to track in 512b blocks
  * @io_throttle_high: I/O size to start throttle in 512b blocks
@@ -1339,6 +1341,8 @@ struct mpi3mr_ioc {
 	u8 *logdata_buf;
 	u16 logdata_buf_idx;
 	u16 logdata_entry_sz;
+	spinlock_t adm_req_q_bar_writeq_lock;
+	spinlock_t adm_reply_q_bar_writeq_lock;
 
 	atomic_t pend_large_data_sz;
 	u32 io_throttle_data_length;
