496deecb020d14ba89ba7084fbc3024f91687023
Hiago De Franco <hiago.franco@toradex.com>
2025-06-29 14:25:11 -0300

    remoteproc: imx_rproc: skip clock enable when M-core is managed by the SCU
    For the i.MX8X and i.MX8 family SoCs, when the Cortex-M core is powered
    up and started by the Cortex-A core using the bootloader (e.g., via the
    U-Boot bootaux command), both M-core and Linux run within the same SCFW
    (System Controller Firmware) partition. With that, Linux has permission
    to control the M-core.
    
    But once the M-core is started by the bootloader, the SCFW automatically
    enables its clock and sets the clock rate. If Linux later attempts to
    enable the same clock via clk_prepare_enable(), the SCFW returns a
    'LOCKED' error, as the clock is already configured by the SCFW. This
    causes the probe function in imx_rproc.c to fail, leading to the M-core
    power domain being shut down while the core is still running. This
    results in a fault from the SCU (System Controller Unit) and triggers a
    system reset.
    
    To address this issue, ignore handling the clk for i.MX8X and i.MX8
    M-core, as SCFW already takes care of enabling and configuring the
    clock.
    
    Suggested-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Hiago De Franco <hiago.franco@toradex.com>
    Acked-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Link: https://lore.kernel.org/r/20250629172512.14857-3-hiagofranco@gmail.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/remoteproc/imx_rproc.c
+++ drivers/remoteproc/imx_rproc.c
@@ -1029,8 +1029,8 @@ static int imx_rproc_clk_enable(struct imx_rproc *priv)
 	struct device *dev = priv->dev;
 	int ret;
 
-	/* Remote core is not under control of Linux */
-	if (dcfg->method == IMX_RPROC_NONE)
+	/* Remote core is not under control of Linux or it is managed by SCU API */
+	if (dcfg->method == IMX_RPROC_NONE || dcfg->method == IMX_RPROC_SCU_API)
 		return 0;
 
 	priv->clk = devm_clk_get(dev, NULL);
