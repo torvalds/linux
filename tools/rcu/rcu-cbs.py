a08591968ef36c0e8aeba061d3a94f9cd1426cbd
Dev. Muhannad Muhammad Gamal <vv3e@outlook.com>
2025-06-30 20:32:08 +0300

    Refactored the script into a modular structure for clarity and maintainability.
    I also introduced optional per-CPU callback count printing and added error handling when fetching RCU data symbols.

--- tools/rcu/rcu-cbs.py
+++ tools/rcu/rcu-cbs.py
@@ -1,46 +1,51 @@
 #!/usr/bin/env drgn
 # SPDX-License-Identifier: GPL-2.0+
 #
-# Dump out the number of RCU callbacks outstanding.
+# Enhanced version of the RCU callback counter script.
+# Originally by Paul E. McKenney <paulmck@kernel.org>
+# Enhanced by Muhannad - github.com/muhannad-iz-a-tech-nerd
 #
-# On older kernels having multiple flavors of RCU, this dumps out the
-# number of callbacks for the most heavily used flavor.
+# Description:
+#   This script sums up the number of outstanding RCU callbacks in
+#   the system. On kernels with multiple RCU flavors, it targets
+#   the most actively used one.
 #
-# Usage: sudo drgn rcu-cbs.py
+# Usage:
+#   sudo drgn rcu-cbs.py
 #
-# Copyright (C) 2021 Facebook, Inc.
-#
-# Authors: Paul E. McKenney <paulmck@kernel.org>
+# Dependencies:
+#   drgn - https://github.com/osandov/drgn
 
 import sys
 import drgn
-from drgn import NULL, Object
+from drgn import NULL
 from drgn.helpers.linux import *
 
 def get_rdp0(prog):
-	try:
-		rdp0 = prog.variable('rcu_preempt_data', 'kernel/rcu/tree.c');
-	except LookupError:
-		rdp0 = NULL;
+    """Get the base rcu_data pointer."""
+    for symbol in ("rcu_preempt_data", "rcu_sched_data", "rcu_data"):
+        try:
+            return prog.variable(symbol, "kernel/rcu/tree.c").address_of_()
+        except LookupError:
+            continue
+    print("Error: Could not find any RCU data symbol.")
+    sys.exit(1)
 
-	if rdp0 == NULL:
-		try:
-			rdp0 = prog.variable('rcu_sched_data',
-					     'kernel/rcu/tree.c');
-		except LookupError:
-			rdp0 = NULL;
+def main():
+    rdp0 = get_rdp0(prog)
 
-	if rdp0 == NULL:
-		rdp0 = prog.variable('rcu_data', 'kernel/rcu/tree.c');
-	return rdp0.address_of_();
+    total_callbacks = 0
+    print_per_cpu = False  # Change to True to enable per-CPU output
 
-rdp0 = get_rdp0(prog);
+    for cpu in for_each_possible_cpu(prog):
+        rdp = per_cpu_ptr(rdp0, cpu)
+        cb_count = rdp.cblist.len.value_()
+        if print_per_cpu:
+            print(f"CPU {cpu:2d}: {cb_count} RCU callbacks")
+        total_callbacks += cb_count
 
-# Sum up RCU callbacks.
-sum = 0;
-for cpu in for_each_possible_cpu(prog):
-	rdp = per_cpu_ptr(rdp0, cpu);
-	len = rdp.cblist.len.value_();
-	# print("CPU " + str(cpu) + " RCU callbacks: " + str(len));
-	sum += len;
-print("Number of RCU callbacks in flight: " + str(sum));
+    print(f"\nTotal RCU callbacks in flight: {total_callbacks}")
+
+if __name__ == "__main__":
+    main()
+
