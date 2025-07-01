7037d1d8979653e4da384b732d2f38d151b9f493
Vikas Gupta <vikas.gupta@broadcom.com>
2025-07-01 14:35:01 +0000

    bng_en: Add firmware communication mechanism
    Add support to communicate with the firmware.
    Future patches will use these functions to send the
    messages to the firmware.
    Functions support allocating request/response buffers
    to send a particular command. Each command has certain
    timeout value to which the driver waits for response from
    the firmware. In error case, commands may be either timed
    out waiting on response from the firmware or may return
    a specific error code.
    
    Signed-off-by: Vikas Gupta <vikas.gupta@broadcom.com>
    Reviewed-by: Bhargava Chenna Marreddy <bhargava.marreddy@broadcom.com>
    Reviewed-by: Rajashekar Hudumula <rajashekar.hudumula@broadcom.com>
    Link: https://patch.msgid.link/20250701143511.280702-4-vikas.gupta@broadcom.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/broadcom/bnge/bnge.h
+++ drivers/net/ethernet/broadcom/bnge/bnge.h
@@ -13,6 +13,8 @@ enum board_idx {
 	BCM57708,
 };
 
+#define INVALID_HW_RING_ID      ((u16)-1)
+
 struct bnge_dev {
 	struct device	*dev;
 	struct pci_dev	*pdev;
@@ -22,6 +24,17 @@ struct bnge_dev {
 	char		board_serialno[BNGE_VPD_FLD_LEN];
 
 	void __iomem	*bar0;
+
+	/* HWRM members */
+	u16			hwrm_cmd_seq;
+	u16			hwrm_cmd_kong_seq;
+	struct dma_pool		*hwrm_dma_pool;
+	struct hlist_head	hwrm_pending_list;
+	u16			hwrm_max_req_len;
+	u16			hwrm_max_ext_req_len;
+	unsigned int		hwrm_cmd_timeout;
+	unsigned int		hwrm_cmd_max_timeout;
+	struct mutex		hwrm_cmd_lock;	/* serialize hwrm messages */
 };
 
 #endif /* _BNGE_H_ */
