a3b0e80428c8e2aa50d7e3c75721dd7f5c76c4d9
Venkata Prasad Potturu <venkataprasad.potturu@amd.com>
2025-07-02 15:14:10 +0530

    ASoC: amd: acp: Add legacy driver support acp7.2 based platforms
    Add pci revision id 0x72 in pci and platform driver to support
    acp7.2 based platforms.
    
    Signed-off-by: Venkata Prasad Potturu <venkataprasad.potturu@amd.com>
    Link: https://patch.msgid.link/20250702094425.155185-2-venkataprasad.potturu@amd.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/acp/acp-pci.c
+++ sound/soc/amd/acp/acp-pci.c
@@ -153,6 +153,7 @@ static int acp_pci_probe(struct pci_dev *pci, const struct pci_device_id *pci_id
 		break;
 	case 0x70:
 	case 0x71:
+	case 0x72:
 		chip->name = "acp_asoc_acp70";
 		chip->rsrc = &acp70_rsrc;
 		chip->acp_hw_ops_init = acp70_hw_ops_init;
