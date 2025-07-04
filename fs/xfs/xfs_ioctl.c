f4a3f01e8e451fb3cb444a95a59964f4bc746902
Pranav Tyagi <pranav.tyagi03@gmail.com>
2025-07-04 15:42:50 +0530

    fs/xfs: replace strncpy with memtostr_pad()
    Replace the deprecated strncpy() with memtostr_pad(). This also avoids
    the need for separate zeroing using memset(). Mark sb_fname buffer with
    __nonstring as its size is XFSLABEL_MAX and so no terminating NULL for
    sb_fname.
    
    Signed-off-by: Pranav Tyagi <pranav.tyagi03@gmail.com>
    Reviewed-by: Carlos Maiolino <cmaiolino@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Carlos Maiolino <cem@kernel.org>

--- fs/xfs/xfs_ioctl.c
+++ fs/xfs/xfs_ioctl.c
@@ -990,9 +990,8 @@ xfs_ioc_getlabel(
 	BUILD_BUG_ON(sizeof(sbp->sb_fname) > FSLABEL_MAX);
 
 	/* 1 larger than sb_fname, so this ensures a trailing NUL char */
-	memset(label, 0, sizeof(label));
 	spin_lock(&mp->m_sb_lock);
-	strncpy(label, sbp->sb_fname, XFSLABEL_MAX);
+	memtostr_pad(label, sbp->sb_fname);
 	spin_unlock(&mp->m_sb_lock);
 
 	if (copy_to_user(user_label, label, sizeof(label)))
