2fa9c93035e17380cafa897ee1a4d503881a3770
Uday Shankar <ushankar@purestorage.com>
2025-07-03 23:41:07 -0600

    ublk: speed up ublk server exit handling
    Recently, we've observed a few cases where a ublk server is able to
    complete restart more quickly than the driver can process the exit of
    the previous ublk server. The new ublk server comes up, attempts
    recovery of the preexisting ublk devices, and observes them still in
    state UBLK_S_DEV_LIVE. While this is possible due to the asynchronous
    nature of io_uring cleanup and should therefore be handled properly in
    the ublk server, it is still preferable to make ublk server exit
    handling faster if possible, as we should strive for it to not be a
    limiting factor in how fast a ublk server can restart and provide
    service again.
    
    Analysis of the issue showed that the vast majority of the time spent in
    handling the ublk server exit was in calls to blk_mq_quiesce_queue,
    which is essentially just a (relatively expensive) call to
    synchronize_rcu. The ublk server exit path currently issues an
    unnecessarily large number of calls to blk_mq_quiesce_queue, for two
    reasons:
    
    1. It tries to call blk_mq_quiesce_queue once per ublk_queue. However,
       blk_mq_quiesce_queue targets the request_queue of the underlying ublk
       device, of which there is only one. So the number of calls is larger
       than necessary by a factor of nr_hw_queues.
    2. In practice, it calls blk_mq_quiesce_queue _more_ than once per
       ublk_queue. This is because of a data race where we read
       ubq->canceling without any locking when deciding if we should call
       ublk_start_cancel. It is thus possible for two calls to
       ublk_uring_cmd_cancel_fn against the same ublk_queue to both call
       ublk_start_cancel against the same ublk_queue.
    
    Fix this by making the "canceling" flag a per-device state. This
    actually matches the existing code better, as there are several places
    where the flag is set or cleared for all queues simultaneously, and
    there is the general expectation that cancellation corresponds with ublk
    server exit. This per-device canceling flag is then checked under a
    (new) lock (addressing the data race (2) above), and the queue is only
    quiesced if it is cleared (addressing (1) above). The result is just one
    call to blk_mq_quiesce_queue per ublk device.
    
    To minimize the number of cache lines that are accessed in the hot path,
    the per-queue canceling flag is kept. The values of the per-device
    canceling flag and all per-queue canceling flags should always match.
    
    In our setup, where one ublk server handles I/O for 128 ublk devices,
    each having 24 hardware queues of depth 4096, here are the results
    before and after this patch, where teardown time is measured from the
    first call to io_ring_ctx_wait_and_kill to the return from the last
    ublk_ch_release:
    
    						before		after
    number of calls to blk_mq_quiesce_queue:	6469		256
    teardown time:					11.14s		2.44s
    
    There are still some potential optimizations here, but this takes care
    of a big chunk of the ublk server exit handling delay.
    
    Signed-off-by: Uday Shankar <ushankar@purestorage.com>
    Reviewed-by: Caleb Sander Mateos <csander@purestorage.com>
    Reviewed-by: Ming Lei <ming.lei@redhat.com>
    Link: https://lore.kernel.org/r/20250703-ublk_too_many_quiesce-v2-1-3527b5339eeb@purestorage.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/ublk_drv.c
+++ drivers/block/ublk_drv.c
@@ -235,6 +235,8 @@ struct ublk_device {
 	struct completion	completion;
 	unsigned int		nr_queues_ready;
 	unsigned int		nr_privileged_daemon;
+	struct mutex cancel_mutex;
+	bool canceling;
 };
 
 /* header of ublk_params */
@@ -1589,6 +1591,7 @@ static int ublk_ch_release(struct inode *inode, struct file *filp)
 	 * All requests may be inflight, so ->canceling may not be set, set
 	 * it now.
 	 */
+	ub->canceling = true;
 	for (i = 0; i < ub->dev_info.nr_hw_queues; i++) {
 		struct ublk_queue *ubq = ublk_get_queue(ub, i);
 
@@ -1717,23 +1720,18 @@ static void ublk_abort_queue(struct ublk_device *ub, struct ublk_queue *ubq)
 	}
 }
 
-/* Must be called when queue is frozen */
-static void ublk_mark_queue_canceling(struct ublk_queue *ubq)
+static void ublk_start_cancel(struct ublk_device *ub)
 {
-	spin_lock(&ubq->cancel_lock);
-	if (!ubq->canceling)
-		ubq->canceling = true;
-	spin_unlock(&ubq->cancel_lock);
-}
-
-static void ublk_start_cancel(struct ublk_queue *ubq)
-{
-	struct ublk_device *ub = ubq->dev;
 	struct gendisk *disk = ublk_get_disk(ub);
+	int i;
 
 	/* Our disk has been dead */
 	if (!disk)
 		return;
+
+	mutex_lock(&ub->cancel_mutex);
+	if (ub->canceling)
+		goto out;
 	/*
 	 * Now we are serialized with ublk_queue_rq()
 	 *
@@ -1742,8 +1740,12 @@ static void ublk_start_cancel(struct ublk_queue *ubq)
 	 * touch completed uring_cmd
 	 */
 	blk_mq_quiesce_queue(disk->queue);
-	ublk_mark_queue_canceling(ubq);
+	ub->canceling = true;
+	for (i = 0; i < ub->dev_info.nr_hw_queues; i++)
+		ublk_get_queue(ub, i)->canceling = true;
 	blk_mq_unquiesce_queue(disk->queue);
+out:
+	mutex_unlock(&ub->cancel_mutex);
 	ublk_put_disk(disk);
 }
 
@@ -1816,8 +1818,7 @@ static void ublk_uring_cmd_cancel_fn(struct io_uring_cmd *cmd,
 	if (WARN_ON_ONCE(task && task != io->task))
 		return;
 
-	if (!ubq->canceling)
-		ublk_start_cancel(ubq);
+	ublk_start_cancel(ubq->dev);
 
 	WARN_ON_ONCE(io->cmd != cmd);
 	ublk_cancel_cmd(ubq, pdu->tag, issue_flags);
@@ -1944,6 +1945,7 @@ static void ublk_reset_io_flags(struct ublk_device *ub)
 		ubq->canceling = false;
 		ubq->fail_io = false;
 	}
+	ub->canceling = false;
 }
 
 /* device can only be started after all IOs are ready */
@@ -2652,6 +2654,7 @@ static void ublk_cdev_rel(struct device *dev)
 	ublk_deinit_queues(ub);
 	ublk_free_dev_number(ub);
 	mutex_destroy(&ub->mutex);
+	mutex_destroy(&ub->cancel_mutex);
 	kfree(ub);
 }
 
@@ -3004,6 +3007,7 @@ static int ublk_ctrl_add_dev(const struct ublksrv_ctrl_cmd *header)
 		goto out_unlock;
 	mutex_init(&ub->mutex);
 	spin_lock_init(&ub->lock);
+	mutex_init(&ub->cancel_mutex);
 
 	ret = ublk_alloc_dev_number(ub, header->dev_id);
 	if (ret < 0)
@@ -3075,6 +3079,7 @@ out_free_dev_number:
 	ublk_free_dev_number(ub);
 out_free_ub:
 	mutex_destroy(&ub->mutex);
+	mutex_destroy(&ub->cancel_mutex);
 	kfree(ub);
 out_unlock:
 	mutex_unlock(&ublk_ctl_mutex);
@@ -3429,8 +3434,9 @@ static int ublk_ctrl_quiesce_dev(struct ublk_device *ub,
 	if (ub->dev_info.state != UBLK_S_DEV_LIVE)
 		goto put_disk;
 
-	/* Mark all queues as canceling */
+	/* Mark the device as canceling */
 	blk_mq_quiesce_queue(disk->queue);
+	ub->canceling = true;
 	for (i = 0; i < ub->dev_info.nr_hw_queues; i++) {
 		struct ublk_queue *ubq = ublk_get_queue(ub, i);
 
