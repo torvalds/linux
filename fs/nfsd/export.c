9c71fe0abf324ab0ba77e3d161d16af3e0f9074b
Al Viro <viro@zeniv.linux.org.uk>
2025-07-06 18:53:37 -0400

    rqst_exp_get_by_name(): constify path argument
    Acked-by: Chuck Lever <chuck.lever@oracle.com>
    Reviewed-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/nfsd/export.c
+++ fs/nfsd/export.c
@@ -1181,7 +1181,7 @@ denied:
  * use exp_get_by_name() or exp_find().
  */
 struct svc_export *
-rqst_exp_get_by_name(struct svc_rqst *rqstp, struct path *path)
+rqst_exp_get_by_name(struct svc_rqst *rqstp, const struct path *path)
 {
 	struct svc_export *gssexp, *exp = ERR_PTR(-ENOENT);
 	struct nfsd_net *nn = net_generic(SVC_NET(rqstp), nfsd_net_id);
