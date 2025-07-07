f6bfc9afc7510cb5e6fbe0a17c507917b0120280
Thomas Zimmermann <tzimmermann@suse.de>
2025-07-07 15:11:55 +0200

    drm/framebuffer: Acquire internal references on GEM handles
    Acquire GEM handles in drm_framebuffer_init() and release them in
    the corresponding drm_framebuffer_cleanup(). Ties the handle's
    lifetime to the framebuffer. Not all GEM buffer objects have GEM
    handles. If not set, no refcounting takes place. This is the case
    for some fbdev emulation. This is not a problem as these GEM objects
    do not use dma-bufs and drivers will not release them while fbdev
    emulation is running. Framebuffer flags keep a bit per color plane
    of which the framebuffer holds a GEM handle reference.
    
    As all drivers use drm_framebuffer_init(), they will now all hold
    dma-buf references as fixed in commit 5307dce878d4 ("drm/gem: Acquire
    references on GEM handles for framebuffers").
    
    In the GEM framebuffer helpers, restore the original ref counting
    on buffer objects. As the helpers for handle refcounting are now
    no longer called from outside the DRM core, unexport the symbols.
    
    v3:
    - don't mix internal flags with mode flags (Christian)
    v2:
    - track framebuffer handle refs by flag
    - drop gma500 cleanup (Christian)
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Fixes: 5307dce878d4 ("drm/gem: Acquire references on GEM handles for framebuffers")
    Reported-by: Bert Karwatzki <spasswolf@web.de>
    Closes: https://lore.kernel.org/dri-devel/20250703115915.3096-1-spasswolf@web.de/
    Tested-by: Bert Karwatzki <spasswolf@web.de>
    Tested-by: Mario Limonciello <superm1@kernel.org>
    Tested-by: Borislav Petkov (AMD) <bp@alien8.de>
    Cc: Thomas Zimmermann <tzimmermann@suse.de>
    Cc: Anusha Srivatsa <asrivats@redhat.com>
    Cc: Christian König <christian.koenig@amd.com>
    Cc: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Cc: Maxime Ripard <mripard@kernel.org>
    Cc: Sumit Semwal <sumit.semwal@linaro.org>
    Cc: "Christian König" <christian.koenig@amd.com>
    Cc: linux-media@vger.kernel.org
    Cc: dri-devel@lists.freedesktop.org
    Cc: linaro-mm-sig@lists.linaro.org
    Cc: <stable@vger.kernel.org>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Link: https://lore.kernel.org/r/20250707131224.249496-1-tzimmermann@suse.de

--- drivers/gpu/drm/drm_gem.c
+++ drivers/gpu/drm/drm_gem.c
@@ -223,23 +223,34 @@ static void drm_gem_object_handle_get(struct drm_gem_object *obj)
 }
 
 /**
- * drm_gem_object_handle_get_unlocked - acquire reference on user-space handles
+ * drm_gem_object_handle_get_if_exists_unlocked - acquire reference on user-space handle, if any
  * @obj: GEM object
  *
- * Acquires a reference on the GEM buffer object's handle. Required
- * to keep the GEM object alive. Call drm_gem_object_handle_put_unlocked()
- * to release the reference.
+ * Acquires a reference on the GEM buffer object's handle. Required to keep
+ * the GEM object alive. Call drm_gem_object_handle_put_if_exists_unlocked()
+ * to release the reference. Does nothing if the buffer object has no handle.
+ *
+ * Returns:
+ * True if a handle exists, or false otherwise
  */
-void drm_gem_object_handle_get_unlocked(struct drm_gem_object *obj)
+bool drm_gem_object_handle_get_if_exists_unlocked(struct drm_gem_object *obj)
 {
 	struct drm_device *dev = obj->dev;
 
 	guard(mutex)(&dev->object_name_lock);
 
-	drm_WARN_ON(dev, !obj->handle_count); /* first ref taken in create-tail helper */
+	/*
+	 * First ref taken during GEM object creation, if any. Some
+	 * drivers set up internal framebuffers with GEM objects that
+	 * do not have a GEM handle. Hence, this counter can be zero.
+	 */
+	if (!obj->handle_count)
+		return false;
+
 	drm_gem_object_handle_get(obj);
+
+	return true;
 }
-EXPORT_SYMBOL(drm_gem_object_handle_get_unlocked);
 
 /**
  * drm_gem_object_handle_free - release resources bound to userspace handles
@@ -272,7 +283,7 @@ static void drm_gem_object_exported_dma_buf_free(struct drm_gem_object *obj)
 }
 
 /**
- * drm_gem_object_handle_put_unlocked - releases reference on user-space handles
+ * drm_gem_object_handle_put_unlocked - releases reference on user-space handle
  * @obj: GEM object
  *
  * Releases a reference on the GEM buffer object's handle. Possibly releases
@@ -283,14 +294,14 @@ void drm_gem_object_handle_put_unlocked(struct drm_gem_object *obj)
 	struct drm_device *dev = obj->dev;
 	bool final = false;
 
-	if (WARN_ON(READ_ONCE(obj->handle_count) == 0))
+	if (drm_WARN_ON(dev, READ_ONCE(obj->handle_count) == 0))
 		return;
 
 	/*
-	* Must bump handle count first as this may be the last
-	* ref, in which case the object would disappear before we
-	* checked for a name
-	*/
+	 * Must bump handle count first as this may be the last
+	 * ref, in which case the object would disappear before
+	 * we checked for a name.
+	 */
 
 	mutex_lock(&dev->object_name_lock);
 	if (--obj->handle_count == 0) {
@@ -303,7 +314,6 @@ void drm_gem_object_handle_put_unlocked(struct drm_gem_object *obj)
 	if (final)
 		drm_gem_object_put(obj);
 }
-EXPORT_SYMBOL(drm_gem_object_handle_put_unlocked);
 
 /*
  * Called at device or object close to release the file's
