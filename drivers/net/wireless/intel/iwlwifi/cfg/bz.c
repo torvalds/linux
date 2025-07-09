e9b7a0264baf9b4aa55ff3ddae275d26b56f476b
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2025-07-09 08:16:33 +0300

    wifi: iwlwifi: bump minimum API version in BZ
    Stop supporting older FWs
    
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Link: https://patch.msgid.link/20250709081300.71404c289481.Iea4f3d36e18029a817ec5d6641d08ac5ee025678@changeid

--- drivers/net/wireless/intel/iwlwifi/cfg/bz.c
+++ drivers/net/wireless/intel/iwlwifi/cfg/bz.c
@@ -13,7 +13,7 @@
 #define IWL_BZ_UCODE_API_MAX	102
 
 /* Lowest firmware API version supported */
-#define IWL_BZ_UCODE_API_MIN	94
+#define IWL_BZ_UCODE_API_MIN	98
 
 /* Memory offsets and lengths */
 #define IWL_BZ_SMEM_OFFSET		0x400000
