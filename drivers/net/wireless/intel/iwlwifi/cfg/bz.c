318f54a6486c769e796cf5bee1a822111b7fc98d
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2025-07-09 08:16:24 +0300

    wifi: iwlwifi: bump FW API to 102 for BZ/SC/DR
    Start supporting FW API version 102 on those devices.
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Link: https://patch.msgid.link/20250709081300.da98a7b6be42.I77150bbf55eb160dbe0ef75c3e28afc053f27ec3@changeid

--- drivers/net/wireless/intel/iwlwifi/cfg/bz.c
+++ drivers/net/wireless/intel/iwlwifi/cfg/bz.c
@@ -10,7 +10,7 @@
 #include "fw/api/txq.h"
 
 /* Highest firmware API version supported */
-#define IWL_BZ_UCODE_API_MAX	99
+#define IWL_BZ_UCODE_API_MAX	102
 
 /* Lowest firmware API version supported */
 #define IWL_BZ_UCODE_API_MIN	94
