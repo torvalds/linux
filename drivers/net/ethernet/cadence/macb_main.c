dce32ece3bb8f3768d04d01cbe146b7ac5ccdbde
Ryan Wanner <Ryan.Wanner@microchip.com>
2025-07-14 09:37:00 -0700

    net: cadence: macb: Expose REFCLK as a device tree property
    The RMII and RGMII can both support internal or external provided
    REFCLKs 50MHz and 125MHz respectively. Since this is dependent on
    the board that the SoC is on this needs to be set via the device tree.
    
    This property flag is checked in the MACB DT node so the REFCLK cap is
    configured the correct way for the RMII or RGMII is configured on the
    board.
    
    Signed-off-by: Ryan Wanner <Ryan.Wanner@microchip.com>
    Link: https://patch.msgid.link/7f9b65896d6b7b48275bc527b72a16347f8ce10a.1752510727.git.Ryan.Wanner@microchip.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/cadence/macb_main.c
+++ drivers/net/ethernet/cadence/macb_main.c
@@ -4109,7 +4109,11 @@ static const struct net_device_ops macb_netdev_ops = {
 static void macb_configure_caps(struct macb *bp,
 				const struct macb_config *dt_conf)
 {
+	struct device_node *np = bp->pdev->dev.of_node;
+	bool refclk_ext;
 	u32 dcfg;
+
+	refclk_ext = of_property_read_bool(np, "cdns,refclk-ext");
 
 	if (dt_conf)
 		bp->caps = dt_conf->caps;
@@ -4140,6 +4144,9 @@ static void macb_configure_caps(struct macb *bp,
 			}
 		}
 	}
+
+	if (refclk_ext)
+		bp->caps |= MACB_CAPS_USRIO_HAS_CLKEN;
 
 	dev_dbg(&bp->pdev->dev, "Cadence caps 0x%08x\n", bp->caps);
 }
