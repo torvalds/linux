8eb698f54736b44a398ca883d0a33f661c12da9d
Danilo Krummrich <dakr@kernel.org>
2025-07-16 17:02:48 +0200

    rust: pci: implement the `dma::Device` trait
    The PCI bus is potentially capable of performing DMA, hence implement
    the `dma:Device` trait for `pci::Device`.
    
    Reviewed-by: Daniel Almeida <daniel.almeida@collabora.com>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://lore.kernel.org/r/20250716150354.51081-4-dakr@kernel.org
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- rust/kernel/pci.rs
+++ rust/kernel/pci.rs
@@ -449,6 +449,8 @@ impl Device<device::Core> {
 kernel::impl_device_context_deref!(unsafe { Device });
 kernel::impl_device_context_into_aref!(Device);
 
+impl crate::dma::Device for Device<device::Core> {}
+
 // SAFETY: Instances of `Device` are always reference-counted.
 unsafe impl crate::types::AlwaysRefCounted for Device {
     fn inc_ref(&self) {
