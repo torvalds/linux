954c0d74129948eed5c8f4a6898d3d5b344c8b18
Paul E. McKenney <paulmck@kernel.org>
2025-07-17 11:55:41 -0700

    srcu: Add guards for SRCU-fast readers
    This adds the usual scoped_guard(srcu_fast, &my_srcu) and
    guard(srcu_fast)(&my_srcu).
    
    Suggested-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Reviewed-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Signed-off-by: Neeraj Upadhyay (AMD) <neeraj.upadhyay@kernel.org>

--- include/linux/srcu.h
+++ include/linux/srcu.h
@@ -481,4 +481,9 @@ DEFINE_LOCK_GUARD_1(srcu, struct srcu_struct,
 		    srcu_read_unlock(_T->lock, _T->idx),
 		    int idx)
 
+DEFINE_LOCK_GUARD_1(srcu_fast, struct srcu_struct,
+		    _T->scp = srcu_read_lock_fast(_T->lock),
+		    srcu_read_unlock_fast(_T->lock, _T->scp),
+		    struct srcu_ctr __percpu *scp)
+
 #endif
