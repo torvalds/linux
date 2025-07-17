8a6506e1ba0d2b831729808d958aae77604f12f9
Alexander Sverdlin <alexander.sverdlin@siemens.com>
2025-07-17 17:27:03 +0200

    ARM: AM33xx: Implement TI advisory 1.0.36 (EMU0/EMU1 pins state on reset)
    There is an issue possible where TI AM33xx SoCs do not boot properly after
    a reset if EMU0/EMU1 pins were used as GPIO and have been driving low level
    actively prior to reset [1].
    
    "Advisory 1.0.36 EMU0 and EMU1: Terminals Must be Pulled High Before
    ICEPick Samples
    
    The state of the EMU[1:0] terminals are latched during reset to determine
    ICEPick boot mode. For normal device operation, these terminals must be
    pulled up to a valid high logic level ( > VIH min) before ICEPick samples
    the state of these terminals, which occurs
    [five CLK_M_OSC clock cycles - 10 ns] after the falling edge of WARMRSTn.
    
    Many applications may not require the secondary GPIO function of the
    EMU[1:0] terminals. In this case, they would only be connected to pull-up
    resistors, which ensures they are always high when ICEPick samples.
    However, some applications may need to use these terminals as GPIO where
    they could be driven low before reset is asserted. This usage of the
    EMU[1:0] terminals may require special attention to ensure the terminals
    are allowed to return to a valid high-logic level before ICEPick samples
    the state of these terminals.
    
    When any device reset is asserted, the pin mux mode of EMU[1:0] terminals
    configured to operate as GPIO (mode 7) will change back to EMU input
    (mode 0) on the falling edge of WARMRSTn. This only provides a short period
    of time for the terminals to return high if driven low before reset is
    asserted...
    
    If the EMU[1:0] terminals are configured to operate as GPIO, the product
    should be designed such these terminals can be pulled to a valid high-logic
    level within 190 ns after the falling edge of WARMRSTn."
    
    We've noticed this problem with custom am335x hardware in combination with
    recently implemented cold reset method
    (commit 6521f6a195c70 ("ARM: AM33xx: PRM: Implement REBOOT_COLD")).
    It looks like the problem can affect other HW, for instance AM335x
    Chiliboard, because the latter has LEDs on GPIO3_7/GPIO3_8 as well.
    
    One option would be to check if the pins are in GPIO mode and either switch
    to output active high, or switch to input and poll until the external
    pull-ups have brought the pins to the desired high state. But fighting
    with GPIO driver for these pins is probably not the most straight forward
    approch in a reboot handler.
    
    Fortunately we can easily control pinmuxing here and rely on the external
    pull-ups. TI recommends 4k7 external pull up resistors [2] and even with
    quite conservative estimation for pin capacity (1 uF should never happen)
    the required delay shall not exceed 5ms.
    
    [1] Link: https://www.ti.com/lit/pdf/sprz360
    [2] Link: https://e2e.ti.com/support/processors-group/processors/f/processors-forum/866346/am3352-emu-1-0-questions
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Alexander Sverdlin <alexander.sverdlin@siemens.com>
    Link: https://lore.kernel.org/r/20250717152708.487891-1-alexander.sverdlin@siemens.com
    Signed-off-by: Kevin Hilman <khilman@baylibre.com>

--- arch/arm/mach-omap2/am33xx-restart.c
+++ arch/arm/mach-omap2/am33xx-restart.c
@@ -2,11 +2,45 @@
 /*
  * am33xx-restart.c - Code common to all AM33xx machines.
  */
+#include <dt-bindings/pinctrl/am33xx.h>
+#include <linux/delay.h>
 #include <linux/kernel.h>
 #include <linux/reboot.h>
 
 #include "common.h"
+#include "control.h"
 #include "prm.h"
+
+/*
+ * Advisory 1.0.36 EMU0 and EMU1: Terminals Must be Pulled High Before
+ * ICEPick Samples
+ *
+ * If EMU0/EMU1 pins have been used as GPIO outputs and actively driving low
+ * level, the device might not reboot in normal mode. We are in a bad position
+ * to override GPIO state here, so just switch the pins into EMU input mode
+ * (that's what reset will do anyway) and wait a bit, because the state will be
+ * latched 190 ns after reset.
+ */
+static void am33xx_advisory_1_0_36(void)
+{
+	u32 emu0 = omap_ctrl_readl(AM335X_PIN_EMU0);
+	u32 emu1 = omap_ctrl_readl(AM335X_PIN_EMU1);
+
+	/* If both pins are in EMU mode, nothing to do */
+	if (!(emu0 & 7) && !(emu1 & 7))
+		return;
+
+	/* Switch GPIO3_7/GPIO3_8 into EMU0/EMU1 modes respectively */
+	omap_ctrl_writel(emu0 & ~7, AM335X_PIN_EMU0);
+	omap_ctrl_writel(emu1 & ~7, AM335X_PIN_EMU1);
+
+	/*
+	 * Give pull-ups time to load the pin/PCB trace capacity.
+	 * 5 ms shall be enough to load 1 uF (would be huge capacity for these
+	 * pins) with TI-recommended 4k7 external pull-ups.
+	 */
+	mdelay(5);
+}
 
 /**
  * am33xx_restart - trigger a software restart of the SoC
@@ -18,6 +52,8 @@
  */
 void am33xx_restart(enum reboot_mode mode, const char *cmd)
 {
+	am33xx_advisory_1_0_36();
+
 	/* TODO: Handle cmd if necessary */
 	prm_reboot_mode = mode;
 
