77de19b6867f2740cdcb6c9c7e50d522b47847a4
Chao Yu <chao@kernel.org>
2025-07-17 21:26:33 +0800

    f2fs: fix to avoid out-of-boundary access in dnode page
    As Jiaming Zhang reported:
    
     <TASK>
     __dump_stack lib/dump_stack.c:94 [inline]
     dump_stack_lvl+0x1c1/0x2a0 lib/dump_stack.c:120
     print_address_description mm/kasan/report.c:378 [inline]
     print_report+0x17e/0x800 mm/kasan/report.c:480
     kasan_report+0x147/0x180 mm/kasan/report.c:593
     data_blkaddr fs/f2fs/f2fs.h:3053 [inline]
     f2fs_data_blkaddr fs/f2fs/f2fs.h:3058 [inline]
     f2fs_get_dnode_of_data+0x1a09/0x1c40 fs/f2fs/node.c:855
     f2fs_reserve_block+0x53/0x310 fs/f2fs/data.c:1195
     prepare_write_begin fs/f2fs/data.c:3395 [inline]
     f2fs_write_begin+0xf39/0x2190 fs/f2fs/data.c:3594
     generic_perform_write+0x2c7/0x910 mm/filemap.c:4112
     f2fs_buffered_write_iter fs/f2fs/file.c:4988 [inline]
     f2fs_file_write_iter+0x1ec8/0x2410 fs/f2fs/file.c:5216
     new_sync_write fs/read_write.c:593 [inline]
     vfs_write+0x546/0xa90 fs/read_write.c:686
     ksys_write+0x149/0x250 fs/read_write.c:738
     do_syscall_x64 arch/x86/entry/syscall_64.c:63 [inline]
     do_syscall_64+0xf3/0x3d0 arch/x86/entry/syscall_64.c:94
     entry_SYSCALL_64_after_hwframe+0x77/0x7f
    
    The root cause is in the corrupted image, there is a dnode has the same
    node id w/ its inode, so during f2fs_get_dnode_of_data(), it tries to
    access block address in dnode at offset 934, however it parses the dnode
    as inode node, so that get_dnode_addr() returns 360, then it tries to
    access page address from 360 + 934 * 4 = 4096 w/ 4 bytes.
    
    To fix this issue, let's add sanity check for node id of all direct nodes
    during f2fs_get_dnode_of_data().
    
    Cc: stable@kernel.org
    Reported-by: Jiaming Zhang <r772577952@gmail.com>
    Closes: https://groups.google.com/g/syzkaller/c/-ZnaaOOfO3M
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -815,6 +815,16 @@ int f2fs_get_dnode_of_data(struct dnode_of_data *dn, pgoff_t index, int mode)
 	for (i = 1; i <= level; i++) {
 		bool done = false;
 
+		if (nids[i] && nids[i] == dn->inode->i_ino) {
+			err = -EFSCORRUPTED;
+			f2fs_err_ratelimited(sbi,
+				"inode mapping table is corrupted, run fsck to fix it, "
+				"ino:%lu, nid:%u, level:%d, offset:%d",
+				dn->inode->i_ino, nids[i], level, offset[level]);
+			set_sbi_flag(sbi, SBI_NEED_FSCK);
+			goto release_pages;
+		}
+
 		if (!nids[i] && mode == ALLOC_NODE) {
 			/* alloc new node */
 			if (!f2fs_alloc_nid(sbi, &(nids[i]))) {
