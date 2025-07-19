cbbcfb94c55c02a8c4ce52b5da0770b5591a314c
Jeremy Linton <jeremy.linton@arm.com>
2025-07-18 23:37:33 -0500

    arm64/gcs: task_gcs_el0_enable() should use passed task
    Mark Rutland noticed that the task parameter is ignored and
    'current' is being used instead. Since this is usually
    what its passed, it hasn't yet been causing problems but likely
    will as the code gets more testing.
    
    But, once this is fixed, it creates a new bug in copy_thread_gcs()
    since the gcs_el_mode isn't yet set for the task before its being
    checked. Move gcs_alloc_thread_stack() after the new task's
    gcs_el0_mode initialization to avoid this.
    
    Fixes: fc84bc5378a8 ("arm64/gcs: Context switch GCS state for EL0")
    Signed-off-by: Jeremy Linton <jeremy.linton@arm.com>
    Reviewed-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20250719043740.4548-2-jeremy.linton@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -305,12 +305,12 @@ static int copy_thread_gcs(struct task_struct *p,
 	p->thread.gcs_base = 0;
 	p->thread.gcs_size = 0;
 
+	p->thread.gcs_el0_mode = current->thread.gcs_el0_mode;
+	p->thread.gcs_el0_locked = current->thread.gcs_el0_locked;
+
 	gcs = gcs_alloc_thread_stack(p, args);
 	if (IS_ERR_VALUE(gcs))
 		return PTR_ERR((void *)gcs);
-
-	p->thread.gcs_el0_mode = current->thread.gcs_el0_mode;
-	p->thread.gcs_el0_locked = current->thread.gcs_el0_locked;
 
 	return 0;
 }
