2d503aafdf70357e279ffef42ae18474328cc223
Akhilesh Patil <akhilesh@ee.iitb.ac.in>
2025-07-19 16:44:27 +0530

    drivers: firmware: bcm47xx_sprom: fix spelling
    Fix spelling in bcm47xx_sprom.c as follows,
    specififc --> specific
    
    Signed-off-by: Akhilesh Patil <akhilesh@ee.iitb.ac.in>
    Link: https://lore.kernel.org/r/aHt+Ey5AQ0dsCuB4@bhairav-test.ee.iitb.ac.in
    Signed-off-by: Florian Fainelli <florian.fainelli@broadcom.com>

--- drivers/firmware/broadcom/bcm47xx_sprom.c
+++ drivers/firmware/broadcom/bcm47xx_sprom.c
@@ -404,7 +404,7 @@ static void bcm47xx_sprom_fill_auto(struct ssb_sprom *sprom,
 	ENTRY(0x00000700, u8, pre, "noiselvl5gua1", noiselvl5gua[1], 0, fb);
 	ENTRY(0x00000700, u8, pre, "noiselvl5gua2", noiselvl5gua[2], 0, fb);
 }
-#undef ENTRY /* It's specififc, uses local variable, don't use it (again). */
+#undef ENTRY /* It's specific, uses local variable, don't use it (again). */
 
 static void bcm47xx_fill_sprom_path_r4589(struct ssb_sprom *sprom,
 					  const char *prefix, bool fallback)
