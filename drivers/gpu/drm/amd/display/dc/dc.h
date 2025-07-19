c5fc24f126786dddcfdb87ac24afbecb1e4dd30d
Taimur Hassan <Syed.Hassan@amd.com>
2025-07-18 19:26:12 -0500

    drm/amd/display: Promote DAL to 3.2.343
    Summary:
    * Fix caching streams for LT automation
    * Fix DMUB command alignment
    * Disabling DSC power gating on DCN314
    * Add debugfs for Replay
    * Add debug option for BW allocation mode
    * Removal of unnecessary includes for faster compilation
    * Refactor of code, including adding SPDX license to amdgpu_dm
    
    Acked-by: Sun peng (Leo) Li <sunpeng.li@amd.com>
    Signed-off-by: Taimur Hassan <Syed.Hassan@amd.com>
    Signed-off-by: Ivan Lipski <ivan.lipski@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dc.h
+++ drivers/gpu/drm/amd/display/dc/dc.h
@@ -55,7 +55,7 @@ struct aux_payload;
 struct set_config_cmd_payload;
 struct dmub_notification;
 
-#define DC_VER "3.2.342"
+#define DC_VER "3.2.343"
 
 /**
  * MAX_SURFACES - representative of the upper bound of surfaces that can be piped to a single CRTC
