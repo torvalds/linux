01fa9758c8498d8930df56eca36c88ba3e9493d4
Mangesh Gadre <Mangesh.Gadre@amd.com>
2025-07-22 00:27:52 +0800

    drm/amdgpu: Initialize jpeg v5_0_1 ras function
    Initialize jpeg v5_0_1 ras function
    
    Signed-off-by: Mangesh Gadre <Mangesh.Gadre@amd.com>
    Reviewed-by: Stanley.Yang <Stanley.Yang@amd.com>
    Reviewed-by: Tao Zhou <tao.zhou1@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/jpeg_v5_0_1.c
+++ drivers/gpu/drm/amd/amdgpu/jpeg_v5_0_1.c
@@ -196,6 +196,14 @@ static int jpeg_v5_0_1_sw_init(struct amdgpu_ip_block *ip_block)
 		}
 	}
 
+	if (amdgpu_ras_is_supported(adev, AMDGPU_RAS_BLOCK__JPEG)) {
+		r = amdgpu_jpeg_ras_sw_init(adev);
+		if (r) {
+			dev_err(adev->dev, "Failed to initialize jpeg ras block!\n");
+			return r;
+		}
+	}
+
 	r = amdgpu_jpeg_reg_dump_init(adev, jpeg_reg_list_5_0_1, ARRAY_SIZE(jpeg_reg_list_5_0_1));
 	if (r)
 		return r;
