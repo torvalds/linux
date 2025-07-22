b9adb86b9045e6e912035e5991d370ac769be0b8
Steven Rostedt <rostedt@goodmis.org>
2025-07-22 16:19:08 -0400

    xfs: remove unused trace event xfs_dqreclaim_dirty
    The tracepoint trace_xfs_dqreclaim_dirty was removed with other code
    removed from xfs_qm_dquot_isolate() but the defined tracepoint was not.
    
    Fixes: d62016b1a2df ("xfs: avoid dquot buffer pin deadlock")
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Carlos Maiolino <cem@kernel.org>

--- fs/xfs/xfs_trace.h
+++ fs/xfs/xfs_trace.h
@@ -1396,7 +1396,6 @@ DEFINE_EVENT(xfs_dquot_class, name, \
 	TP_ARGS(dqp))
 DEFINE_DQUOT_EVENT(xfs_dqadjust);
 DEFINE_DQUOT_EVENT(xfs_dqreclaim_want);
-DEFINE_DQUOT_EVENT(xfs_dqreclaim_dirty);
 DEFINE_DQUOT_EVENT(xfs_dqreclaim_busy);
 DEFINE_DQUOT_EVENT(xfs_dqreclaim_done);
 DEFINE_DQUOT_EVENT(xfs_dqattach_found);
