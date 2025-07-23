367f4b163a8cff20d0cb06eb265a1bf1c6652bd9
Ralf Lici <ralf@mandelbit.com>
2025-07-23 14:35:34 +0200

    selftests: ovpn: check asymmetric peer-id
    Extend the base test to verify that the correct peer-id is set in data
    packet headers. This is done by capturing ping packets with tcpdump during
    the initial exchange and matching the first portion of the header
    against the expected sequence for every connection.
    
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: linux-kselftest@vger.kernel.org
    Cc: horms@kernel.org
    Signed-off-by: Ralf Lici <ralf@mandelbit.com>
    Signed-off-by: Antonio Quartulli <antonio@openvpn.net>

---
+++ tools/testing/selftests/net/ovpn/test-symmetric-id.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2025 OpenVPN, Inc.
+#
+#	Author:	Ralf Lici <ralf@mandelbit.com>
+#		Antonio Quartulli <antonio@openvpn.net>
+
+SYMMETRIC_ID="1"
+
+source test.sh
