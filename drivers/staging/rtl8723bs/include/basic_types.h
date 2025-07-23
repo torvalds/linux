63533ba19d0f325f8e36cc5aae73281a93758618
Ali Nasrolahi <a.nasrolahi01@gmail.com>
2025-07-23 07:49:21 +0330

    staging: rtl8723bs: remove redundant semicolon in basic_types.h
    The macro SET_BITS_TO_LE_2BYTE ends with a semicolon inside its definition,
    which can lead to an extra semicolon when used, resulting in inconsistent
    formatting or potential warnings.
    
    This patch removes the redundant semicolon to comply with kernel macro
    style guidelines and improve readability.
    
    Detected using checkpatch.pl.
    
    Signed-off-by: Ali Nasrolahi <A.Nasrolahi01@gmail.com>
    Link: https://lore.kernel.org/r/20250723041920.9623-2-A.Nasrolahi01@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/include/basic_types.h
+++ drivers/staging/rtl8723bs/include/basic_types.h
@@ -163,7 +163,7 @@
 		(					\
 		LE_BITS_CLEARED_TO_2BYTE(__pstart, __bitoffset, __bitlen) | \
 		((((u16)__val) & BIT_LEN_MASK_16(__bitlen)) << (__bitoffset)) \
-		);
+		)
 
 #define SET_BITS_TO_LE_1BYTE(__pstart, __bitoffset, __bitlen, __val) \
 		*((u8 *)(__pstart)) = EF1BYTE			\
