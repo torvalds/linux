74e15ac34b098934895fd27655d098971d2b43d9
Tamir Duberstein <tamird@kernel.org>
2025-07-23 11:39:42 -0400

    scripts: generate_rust_analyzer: Add pin_init_internal deps
    Commit d7659acca7a3 ("rust: add pin-init crate build infrastructure")
    did not add dependencies to `pin_init_internal`, resulting in broken
    navigation. Thus add them now.
    
    [ Tamir elaborates:
    
      "before this series, go-to-symbol from pin_init_internal to e.g.
       proc_macro::TokenStream doesn't work."
    
         - Miguel ]
    
    Signed-off-by: Tamir Duberstein <tamird@kernel.org>
    Reviewed-by: Jesung Yang <y.j3ms.n@gmail.com>
    Acked-by: Benno Lossin <lossin@kernel.org>
    Fixes: d7659acca7a3 ("rust: add pin-init crate build infrastructure")
    Cc: stable@vger.kernel.org
    Link: https://patch.msgid.link/20250723-rust-analyzer-pin-init-v1-3-3c6956173c78@kernel.org
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- scripts/generate_rust_analyzer.py
+++ scripts/generate_rust_analyzer.py
@@ -123,7 +123,7 @@ def generate_crates(srctree, objtree, sysroot_src, external_src, cfgs, core_edit
     append_crate(
         "pin_init_internal",
         srctree / "rust" / "pin-init" / "internal" / "src" / "lib.rs",
-        [],
+        ["std", "proc_macro"],
         cfg=["kernel"],
         is_proc_macro=True,
     )
