bb4b0f8a1bcbf8f4e3a0841aaefb3fd580d12fc9
Aleksa Paunovic <aleksa.paunovic@htecgroup.com>
2025-07-24 17:23:28 +0200

    riscv: hwprobe: Add MIPS vendor extension probing
    Add a new hwprobe key "RISCV_HWPROBE_KEY_VENDOR_EXT_MIPS_0" which allows
    userspace to probe for the new xmipsexectl vendor extension.
    
    Signed-off-by: Aleksa Paunovic <aleksa.paunovic@htecgroup.com>
    Reviewed-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Link: https://lore.kernel.org/r/20250724-p8700-pause-v5-4-a6cbbe1c3412@htecgroup.com
    [pjw@kernel.org: fixed some checkpatch issues]
    Signed-off-by: Paul Walmsley <pjw@kernel.org>

--- arch/riscv/kernel/sys_hwprobe.c
+++ arch/riscv/kernel/sys_hwprobe.c
@@ -15,6 +15,7 @@
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
 #include <asm/vector.h>
+#include <asm/vendor_extensions/mips_hwprobe.h>
 #include <asm/vendor_extensions/sifive_hwprobe.h>
 #include <asm/vendor_extensions/thead_hwprobe.h>
 #include <vdso/vsyscall.h>
@@ -306,6 +307,9 @@ static void hwprobe_one_pair(struct riscv_hwprobe *pair,
 
 	case RISCV_HWPROBE_KEY_VENDOR_EXT_THEAD_0:
 		hwprobe_isa_vendor_ext_thead_0(pair, cpus);
+		break;
+	case RISCV_HWPROBE_KEY_VENDOR_EXT_MIPS_0:
+		hwprobe_isa_vendor_ext_mips_0(pair, cpus);
 		break;
 
 	/*
