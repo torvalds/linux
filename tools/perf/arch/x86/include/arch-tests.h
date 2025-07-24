a563c9f3bb8c23416f3e72edfbc75d1a4937f7e0
Ian Rogers <irogers@google.com>
2025-07-24 09:33:01 -0700

    perf test: Move PERF_SAMPLE_WEIGHT_STRUCT parsing to common test
    test__x86_sample_parsing is identical to test__sample_parsing except
    it explicitly tested PERF_SAMPLE_WEIGHT_STRUCT. Now the parsing code
    is common move the PERF_SAMPLE_WEIGHT_STRUCT to the common sample
    parsing test and remove the x86 version.
    
    Signed-off-by: Ian Rogers <irogers@google.com>
    Link: https://lore.kernel.org/r/20250724163302.596743-22-irogers@google.com
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/arch/x86/include/arch-tests.h
+++ tools/perf/arch/x86/include/arch-tests.h
@@ -14,7 +14,6 @@ int test__insn_x86(struct test_suite *test, int subtest);
 int test__intel_pt_pkt_decoder(struct test_suite *test, int subtest);
 int test__intel_pt_hybrid_compat(struct test_suite *test, int subtest);
 int test__bp_modify(struct test_suite *test, int subtest);
-int test__x86_sample_parsing(struct test_suite *test, int subtest);
 int test__amd_ibs_via_core_pmu(struct test_suite *test, int subtest);
 int test__amd_ibs_period(struct test_suite *test, int subtest);
 int test__hybrid(struct test_suite *test, int subtest);
