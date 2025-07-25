e87e953bb20629ca1f008f8146c38e313e5ed319
Filipe Manana <fdmanana@suse.com>
2025-07-25 16:54:49 +0100

    btrfs: fix inode leak on failure to add link to inode
    If we fail to update the inode or delete the orphan item we leak the inode
    since we update its refcount with the ihold() call to account for the
    d_instantiate() call which never happens in case we fail those steps. Fix
    this by setting 'drop_inode' to true in case we fail those steps.
    
    Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Signed-off-by: Filipe Manana <fdmanana@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -6854,6 +6854,7 @@ static int btrfs_link(struct dentry *old_dentry, struct inode *dir,
 		ret = btrfs_update_inode(trans, BTRFS_I(inode));
 		if (ret) {
 			btrfs_abort_transaction(trans, ret);
+			drop_inode = 1;
 			goto fail;
 		}
 		if (inode->i_nlink == 1) {
@@ -6864,6 +6865,7 @@ static int btrfs_link(struct dentry *old_dentry, struct inode *dir,
 			ret = btrfs_orphan_del(trans, BTRFS_I(inode));
 			if (ret) {
 				btrfs_abort_transaction(trans, ret);
+				drop_inode = 1;
 				goto fail;
 			}
 		}
