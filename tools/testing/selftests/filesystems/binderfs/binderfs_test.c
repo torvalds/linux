f37b55ded8ed35424ebb91a4d012527071e1f601
Li Li <dualli@google.com>
2025-07-27 18:29:07 +0000

    binder: add transaction_report feature entry
    Add "transaction_report" to the binderfs feature list, to help userspace
    determine if the "BINDER_CMD_REPORT" generic netlink api is supported by
    the binder driver.
    
    Signed-off-by: Li Li <dualli@google.com>
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    Link: https://lore.kernel.org/r/20250727182932.2499194-5-cmllamas@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- tools/testing/selftests/filesystems/binderfs/binderfs_test.c
+++ tools/testing/selftests/filesystems/binderfs/binderfs_test.c
@@ -65,6 +65,7 @@ static int __do_binderfs_test(struct __test_metadata *_metadata)
 		"oneway_spam_detection",
 		"extended_error",
 		"freeze_notification",
+		"transaction_report",
 	};
 
 	change_mountns(_metadata);
