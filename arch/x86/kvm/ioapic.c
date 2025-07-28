49be82d4ad2ea3329c77b9e6010507baf1c66134
Thomas Huth <thuth@redhat.com>
2025-07-28 17:28:43 +0200

    arch/x86/kvm/ioapic: Remove license boilerplate with bad FSF address
    The Free Software Foundation does not reside in "59 Temple Place"
    anymore, so we should not mention that address in the source code here.
    But instead of updating the address to their current location, let's
    rather drop the license boilerplate text here and use a proper SPDX
    license identifier instead. The text talks about the "GNU *Lesser*
    General Public License" and "any later version", so LGPL-2.1+ is the
    right choice here.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Link: https://lore.kernel.org/r/20250728152843.310260-1-thuth@redhat.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/ioapic.c
+++ arch/x86/kvm/ioapic.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
 /*
  *  Copyright (C) 2001  MandrakeSoft S.A.
  *  Copyright 2010 Red Hat, Inc. and/or its affiliates.
@@ -7,20 +8,6 @@
  *    75002 Paris - France
  *    http://www.linux-mandrake.com/
  *    http://www.mandrakesoft.com/
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2 of the License, or (at your option) any later version.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
  *  Yunhong Jiang <yunhong.jiang@intel.com>
  *  Yaozu (Eddie) Dong <eddie.dong@intel.com>
