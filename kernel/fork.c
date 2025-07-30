e703b7e247503b8bf87b62c02a4392749b09eca8
Thomas Gleixner <tglx@linutronix.de>
2025-07-30 21:44:55 +0200

    futex: Move futex cleanup to __mmdrop()
    Futex hash allocations are done in mm_init() and the cleanup happens in
    __mmput(). That works most of the time, but there are mm instances which
    are instantiated via mm_alloc() and freed via mmdrop(), which causes the
    futex hash to be leaked.
    
    Move the cleanup to __mmdrop().
    
    Fixes: 56180dd20c19 ("futex: Use RCU-based per-CPU reference counting instead of rcuref_t")
    Reported-by: André Draszik <andre.draszik@linaro.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: André Draszik <andre.draszik@linaro.org>
    Link: https://lore.kernel.org/all/87ldo5ihu0.ffs@tglx
    Closes: https://lore.kernel.org/all/0c8cc83bb73abf080faf584f319008b67d0931db.camel@linaro.org

--- kernel/fork.c
+++ kernel/fork.c
@@ -686,6 +686,7 @@ void __mmdrop(struct mm_struct *mm)
 	mm_pasid_drop(mm);
 	mm_destroy_cid(mm);
 	percpu_counter_destroy_many(mm->rss_stat, NR_MM_COUNTERS);
+	futex_hash_free(mm);
 
 	free_mm(mm);
 }
@@ -1133,7 +1134,6 @@ static inline void __mmput(struct mm_struct *mm)
 	if (mm->binfmt)
 		module_put(mm->binfmt->module);
 	lru_gen_del_mm(mm);
-	futex_hash_free(mm);
 	mmdrop(mm);
 }
 
