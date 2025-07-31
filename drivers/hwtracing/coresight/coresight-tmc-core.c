8a79026926b329d4ab0c6d0921373a80ec8aab6e
Leo Yan <leo.yan@arm.com>
2025-07-31 13:23:37 +0100

    coresight: tmc: Support atclk
    The atclk is an optional clock for the CoreSight TMC, but the driver
    misses to initialize it.  In most cases, TMC shares the atclk clock with
    other CoreSight components.  Since these components enable the clock
    before the TMC device is initialized, the TMC continues properly,
    which is why we don’t observe any lockup issues.
    
    This change enables atclk in probe of the TMC driver.  Given the clock
    is optional, it is possible to return NULL if the clock does not exist.
    IS_ERR() is tolerant for this case.
    
    Dynamically disable and enable atclk during suspend and resume.  The
    clock pointers will never be error values if the driver has successfully
    probed, and the case of a NULL pointer case will be handled by the clock
    core layer.  The driver data is always valid after probe. Therefore,
    remove the related checks.  Also in the resume flow adds error handling.
    
    Fixes: bc4bf7fe98da ("coresight-tmc: add CoreSight TMC driver")
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Reviewed-by: Yeoreum Yun <yeoreum.yun@arm.com>
    Tested-by: James Clark <james.clark@linaro.org>
    Signed-off-by: Leo Yan <leo.yan@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Link: https://lore.kernel.org/r/20250731-arm_cs_fix_clock_v4-v6-1-1dfe10bb3f6f@arm.com

--- drivers/hwtracing/coresight/coresight-tmc-core.c
+++ drivers/hwtracing/coresight/coresight-tmc-core.c
@@ -779,6 +779,10 @@ static int __tmc_probe(struct device *dev, struct resource *res)
 	struct coresight_desc desc = { 0 };
 	struct coresight_dev_list *dev_list = NULL;
 
+	drvdata->atclk = devm_clk_get_optional_enabled(dev, "atclk");
+	if (IS_ERR(drvdata->atclk))
+		return PTR_ERR(drvdata->atclk);
+
 	ret = -ENOMEM;
 
 	/* Validity for the resource is already checked by the AMBA core */
@@ -1010,18 +1014,26 @@ static int tmc_runtime_suspend(struct device *dev)
 {
 	struct tmc_drvdata *drvdata = dev_get_drvdata(dev);
 
-	if (drvdata && !IS_ERR_OR_NULL(drvdata->pclk))
-		clk_disable_unprepare(drvdata->pclk);
+	clk_disable_unprepare(drvdata->atclk);
+	clk_disable_unprepare(drvdata->pclk);
+
 	return 0;
 }
 
 static int tmc_runtime_resume(struct device *dev)
 {
 	struct tmc_drvdata *drvdata = dev_get_drvdata(dev);
+	int ret;
 
-	if (drvdata && !IS_ERR_OR_NULL(drvdata->pclk))
-		clk_prepare_enable(drvdata->pclk);
-	return 0;
+	ret = clk_prepare_enable(drvdata->pclk);
+	if (ret)
+		return ret;
+
+	ret = clk_prepare_enable(drvdata->atclk);
+	if (ret)
+		clk_disable_unprepare(drvdata->pclk);
+
+	return ret;
 }
 #endif
 
