d77ad5178e90f5aa4ce6085510b3b2f742abc5f0
Luca Ceresoli <luca.ceresoli@bootlin.com>
2025-08-01 19:05:24 +0200

    drm/bridge: add drm_bridge_chain_get_last_bridge()
    Add an equivalent of drm_bridge_chain_get_first_bridge() to get the last
    bridge.
    
    Reviewed-by: Maxime Ripard <mripard@kernel.org>
    Link: https://lore.kernel.org/r/20250801-drm-bridge-alloc-getput-drm_bridge_get_next_bridge-v2-2-888912b0be13@bootlin.com
    Signed-off-by: Luca Ceresoli <luca.ceresoli@bootlin.com>

--- include/drm/drm_bridge.h
+++ include/drm/drm_bridge.h
@@ -1411,6 +1411,24 @@ drm_bridge_chain_get_first_bridge(struct drm_encoder *encoder)
 }
 
 /**
+ * drm_bridge_chain_get_last_bridge() - Get the last bridge in the chain
+ * @encoder: encoder object
+ *
+ * The refcount of the returned bridge is incremented. Use drm_bridge_put()
+ * when done with it.
+ *
+ * RETURNS:
+ * the last bridge in the chain, or NULL if @encoder has no bridge attached
+ * to it.
+ */
+static inline struct drm_bridge *
+drm_bridge_chain_get_last_bridge(struct drm_encoder *encoder)
+{
+	return drm_bridge_get(list_last_entry_or_null(&encoder->bridge_chain,
+						      struct drm_bridge, chain_node));
+}
+
+/**
  * drm_for_each_bridge_in_chain() - Iterate over all bridges present in a chain
  * @encoder: the encoder to iterate bridges on
  * @bridge: a bridge pointer updated to point to the current bridge at each
