a424353937c24554bb242a6582ed8f018b4a411c
Michal Wajdeczko <michal.wajdeczko@intel.com>
2025-08-01 16:28:20 +0200

    drm/xe/pf: Disable PF restart worker on device removal
    We can't let restart worker run once device is removed, since other
    data that it might want to access could be already released.
    Explicitly disable worker as part of device cleanup action.
    
    Fixes: a4d1c5d0b99b ("drm/xe/pf: Move VFs reprovisioning to worker")
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Reviewed-by: Piotr Piórkowski <piotr.piorkowski@intel.com>
    Cc: Jonathan Cavitt <jonathan.cavitt@intel.com>
    Link: https://lore.kernel.org/r/20250801142822.180530-2-michal.wajdeczko@intel.com

--- drivers/gpu/drm/xe/xe_gt_sriov_pf.c
+++ drivers/gpu/drm/xe/xe_gt_sriov_pf.c
@@ -47,7 +47,14 @@ static int pf_alloc_metadata(struct xe_gt *gt)
 
 static void pf_init_workers(struct xe_gt *gt)
 {
+	xe_gt_assert(gt, IS_SRIOV_PF(gt_to_xe(gt)));
 	INIT_WORK(&gt->sriov.pf.workers.restart, pf_worker_restart_func);
+}
+
+static void pf_fini_workers(struct xe_gt *gt)
+{
+	xe_gt_assert(gt, IS_SRIOV_PF(gt_to_xe(gt)));
+	disable_work_sync(&gt->sriov.pf.workers.restart);
 }
 
 /**
@@ -79,6 +86,21 @@ int xe_gt_sriov_pf_init_early(struct xe_gt *gt)
 	return 0;
 }
 
+static void pf_fini_action(void *arg)
+{
+	struct xe_gt *gt = arg;
+
+	pf_fini_workers(gt);
+}
+
+static int pf_init_late(struct xe_gt *gt)
+{
+	struct xe_device *xe = gt_to_xe(gt);
+
+	xe_gt_assert(gt, IS_SRIOV_PF(xe));
+	return devm_add_action_or_reset(xe->drm.dev, pf_fini_action, gt);
+}
+
 /**
  * xe_gt_sriov_pf_init - Prepare SR-IOV PF data structures on PF.
  * @gt: the &xe_gt to initialize
@@ -95,7 +117,15 @@ int xe_gt_sriov_pf_init(struct xe_gt *gt)
 	if (err)
 		return err;
 
-	return xe_gt_sriov_pf_migration_init(gt);
+	err = xe_gt_sriov_pf_migration_init(gt);
+	if (err)
+		return err;
+
+	err = pf_init_late(gt);
+	if (err)
+		return err;
+
+	return 0;
 }
 
 static bool pf_needs_enable_ggtt_guest_update(struct xe_device *xe)
