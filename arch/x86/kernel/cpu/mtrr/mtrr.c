045f6a6e4dbaa0ecdee43bc2362676ff62ba5456
Thomas Huth <thuth@redhat.com>
2025-08-01 10:45:31 +0200

    x86/mtrr: Remove license boilerplate text with bad FSF address
    The Free Software Foundation does not reside in "675 Mass Ave, Cambridge"
    anymore, so no need to mention that address in the source code here.  But
    instead of updating the address to their current location, just drop the
    license boilerplate text and use a proper SPDX license identifier instead.  The
    text talks about the "GNU *Library* General Public License" and "any later
    version", so LGPL-2.0+ is the right choice here.
    
      [ bp: Massage commit message. ]
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://lore.kernel.org/20250801084531.34089-1-thuth@redhat.com

--- arch/x86/kernel/cpu/mtrr/mtrr.c
+++ arch/x86/kernel/cpu/mtrr/mtrr.c
@@ -1,21 +1,8 @@
+// SPDX-License-Identifier: LGPL-2.0+
 /*  Generic MTRR (Memory Type Range Register) driver.
 
     Copyright (C) 1997-2000  Richard Gooch
     Copyright (c) 2002	     Patrick Mochel
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public
-    License along with this library; if not, write to the Free
-    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
     Richard Gooch may be reached by email at  rgooch@atnf.csiro.au
     The postal address is:
