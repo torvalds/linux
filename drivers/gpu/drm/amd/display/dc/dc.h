8d6593c192de6e51102ca7d797756a5003f08cd3
Taimur Hassan <Syed.Hassan@amd.com>
2025-08-03 18:38:31 -0500

    drm/amd/display: Promote DC to 3.2.345
    This version brings along following update:
    -Fix close and open lid may cause eDP remaining blank
    -Fix frequently disabling/enabling OTG may cause incorrect
     configuration of OTG
    
    Acked-by: Wayne Lin <wayne.lin@amd.com>
    Signed-off-by: Taimur Hassan <Syed.Hassan@amd.com>
    Signed-off-by: Tom Chung <chiahsuan.chung@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dc.h
+++ drivers/gpu/drm/amd/display/dc/dc.h
@@ -55,7 +55,7 @@ struct aux_payload;
 struct set_config_cmd_payload;
 struct dmub_notification;
 
-#define DC_VER "3.2.344"
+#define DC_VER "3.2.345"
 
 /**
  * MAX_SURFACES - representative of the upper bound of surfaces that can be piped to a single CRTC
