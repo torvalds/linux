9863124e0bfb94a0c1e84186d2f56f400b2e0112
Qianfeng Rong <rongqianfeng@vivo.com>
2025-08-04 21:00:17 +0800

    xarray: remove redundant __GFP_NOWARN
    Commit 16f5dfbc851b ("gfp: include __GFP_NOWARN in GFP_NOWAIT") made
    GFP_NOWAIT implicitly include __GFP_NOWARN.
    
    Therefore, explicit __GFP_NOWARN combined with GFP_NOWAIT (e.g.,
    `GFP_NOWAIT | __GFP_NOWARN`) is now redundant.  Let's clean up these
    redundant flags across subsystems.
    
    No functional changes.
    
    Link: https://lkml.kernel.org/r/20250804130018.484321-1-rongqianfeng@vivo.com
    Signed-off-by: Qianfeng Rong <rongqianfeng@vivo.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- lib/xarray.c
+++ lib/xarray.c
@@ -370,7 +370,7 @@ static void *xas_alloc(struct xa_state *xas, unsigned int shift)
 	if (node) {
 		xas->xa_alloc = NULL;
 	} else {
-		gfp_t gfp = GFP_NOWAIT | __GFP_NOWARN;
+		gfp_t gfp = GFP_NOWAIT;
 
 		if (xas->xa->xa_flags & XA_FLAGS_ACCOUNT)
 			gfp |= __GFP_ACCOUNT;
