754dbf164acd4d22dd7a5241b1880f54546d68f2
Christopher Orr <chris.orr@gmail.com>
2025-08-05 21:27:55 -0400

    drm/panel-edp: Add SHP LQ134Z1 panel for Dell XPS 9345
    Introduce high-res OLED panel for the Dell XPS 9345
    
    These timings were selected based on Alex Vinarkskis' commit,
    (6b3815c6815f07acc7eeffa8ae734d1a1c0ee817) for the LQ134N1
    and seem to work fine for the high-res OLED panel on the 9345.
    
    The raw edid for this SHP panel is:
    
    00 ff ff ff ff ff ff 00 4d 10 8f 15 00 00 00 00
    2e 21 01 04 b5 1d 12 78 03 0f 95 ae 52 43 b0 26
    0f 50 54 00 00 00 01 01 01 01 01 01 01 01 01 01
    01 01 01 01 01 01 fd d7 00 a0 a0 40 fc 66 30 20
    36 00 20 b4 10 00 00 18 00 00 00 fd 00 1e 78 cc
    cc 38 01 0a 20 20 20 20 20 20 00 00 00 fe 00 43
    37 31 4d 31 81 4c 51 31 33 34 5a 31 00 00 00 00
    00 02 41 0c 32 01 01 00 00 0b 41 0a 20 20 01 ea
    
    70 20 79 02 00 20 00 13 8c 52 19 8f 15 00 00 00
    00 2e 17 07 4c 51 31 33 34 5a 31 21 00 1d 40 0b
    08 07 00 0a 40 06 88 e1 fa 51 3d a4 b0 66 62 0f
    02 45 54 d0 5f d0 5f 00 34 13 78 26 00 09 06 00
    00 00 00 00 41 00 00 22 00 14 d9 6f 08 05 ff 09
    9f 00 2f 00 1f 00 3f 06 5d 00 02 00 05 00 25 01
    09 d9 6f 08 d9 6f 08 1e 78 80 81 00 0b e3 05 80
    00 e6 06 05 01 6a 6a 39 00 00 00 00 00 00 58 90
    
    Signed-off-by: Christopher Orr <chris.orr@gmail.com>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Link: https://lore.kernel.org/r/aJKvm3SlhLGHW4qn@jander

--- drivers/gpu/drm/panel/panel-edp.c
+++ drivers/gpu/drm/panel/panel-edp.c
@@ -2055,6 +2055,7 @@ static const struct edp_panel_entry edp_panels[] = {
 	EDP_PANEL_ENTRY('S', 'H', 'P', 0x1523, &delay_80_500_e50, "LQ140M1JW46"),
 	EDP_PANEL_ENTRY('S', 'H', 'P', 0x153a, &delay_200_500_e50, "LQ140T1JH01"),
 	EDP_PANEL_ENTRY('S', 'H', 'P', 0x154c, &delay_200_500_p2e100, "LQ116M1JW10"),
+	EDP_PANEL_ENTRY('S', 'H', 'P', 0x158f, &delay_200_500_p2e100, "LQ134Z1"),
 	EDP_PANEL_ENTRY('S', 'H', 'P', 0x1593, &delay_200_500_p2e100, "LQ134N1"),
 
 	EDP_PANEL_ENTRY('S', 'T', 'A', 0x0004, &delay_200_500_e200, "116KHD024006"),
