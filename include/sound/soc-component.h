7b900b5878a910d60ecfa67448bbe81e4e2bb8b7
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2025-08-06 04:41:51 +0000

    ASoC: soc-component: unpack snd_soc_component_force_bias_level()
    Because struct snd_soc_dapm_context is soc-dapm framework specific, user
    driver don't need to access its member directly, we would like to hide
    them. struct snd_soc_dapm_context will be removed from header in the
    future.
    
    This patch unpack component wrapper to cleanup it.
    
    The function will be kept by using macro for a while, but will be
    replaced/cleanuped in the future.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://patch.msgid.link/871ppp83jk.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/sound/soc-component.h
+++ include/sound/soc-component.h
@@ -315,23 +315,6 @@ snd_soc_component_get_bias_level(struct snd_soc_component *component)
 }
 
 /**
- * snd_soc_component_force_bias_level() - Set the COMPONENT DAPM bias level
- * @component: The COMPONENT for which to set the level
- * @level: The level to set to
- *
- * Forces the COMPONENT bias level to a specific state. See
- * snd_soc_dapm_force_bias_level().
- */
-static inline int
-snd_soc_component_force_bias_level(struct snd_soc_component *component,
-				   enum snd_soc_bias_level level)
-{
-	return snd_soc_dapm_force_bias_level(
-		snd_soc_component_get_dapm(component),
-		level);
-}
-
-/**
  * snd_soc_dapm_kcontrol_component() - Returns the component associated to a
  * kcontrol
  * @kcontrol: The kcontrol
--- include/sound/soc-dapm.h
+++ include/sound/soc-dapm.h
@@ -725,6 +725,9 @@ int snd_soc_dapm_force_bias_level(struct snd_soc_dapm_context *dapm, enum snd_so
 enum snd_soc_bias_level snd_soc_dapm_get_bias_level(struct snd_soc_dapm_context *dapm);
 void snd_soc_dapm_init_bias_level(struct snd_soc_dapm_context *dapm, enum snd_soc_bias_level level);
 
+// REMOVE ME !!
+#define snd_soc_component_force_bias_level(c, l)	snd_soc_dapm_force_bias_level(&(c)->dapm, l)
+
 #define for_each_dapm_widgets(list, i, widget)				\
 	for ((i) = 0;							\
 	     (i) < list->num_widgets && (widget = list->widgets[i]);	\
