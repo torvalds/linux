806ecb209aa86fcc1d92bc9f10323cf773f64d6d
Jens Axboe <axboe@kernel.dk>
2025-08-07 14:22:16 -0600

    io_uring/nop: add support for IORING_SETUP_CQE_MIXED
    This adds support for setting IORING_NOP_CQE32 as a flag for a NOP
    command, in which case a 32b CQE will be posted rather than a regular
    one. This is the default if the ring has been setup with
    IORING_SETUP_CQE32. If the ring has been setup with
    IORING_SETUP_CQE_MIXED, then 16b CQEs will be posted without this flag
    set, and 32b CQEs if this flag is set. For the latter case, sqe->off is
    what will be posted as cqe->big_cqe[0] and sqe->addr is what will be
    posted as cqe->big_cqe[1].
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/uapi/linux/io_uring.h
+++ include/uapi/linux/io_uring.h
@@ -464,6 +464,7 @@ enum io_uring_msg_ring_flags {
 #define IORING_NOP_FIXED_FILE		(1U << 2)
 #define IORING_NOP_FIXED_BUFFER		(1U << 3)
 #define IORING_NOP_TW			(1U << 4)
+#define IORING_NOP_CQE32		(1U << 5)
 
 /*
  * IO completion data structure (Completion Queue Entry)
