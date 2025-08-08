c92f59bac078465425f1223b29a8d63c89027eba
Luca Ceresoli <luca.ceresoli@bootlin.com>
2025-08-08 16:49:08 +0200

    drm/display: bridge-connector: use scope-specific variable for the bridge pointer
    Currently drm_bridge_connector_init() reuses the 'bridge' variable, first
    as a loop variable in the long drm_for_each_bridge_in_chain() and then in
    the HDMI-specific code as a shortcut to bridge_connector->bridge_cec.
    
    We are about to remove the 'bridge' loop variable for
    drm_for_each_bridge_in_chain() by moving to a scoped version of the same
    macro, which implies removing the 'bridge' variable from the main function
    scope. Additionally reusing the variable can make such long function less
    readable.
    
    Similarly to what commit 6f727c838ea8 ("drm/bridge-connector: Fix bridge in
    drm_connector_hdmi_audio_init()") already did for the audio HDMI bridge,
    use n local variable inside the scopes where it is needed as a
    bridge_connector->bridge_hdmi_cec shortcut to make its scope clearer as
    well as to allow removing the 'bridge' variable in an upcoming commit.
    
    Reviewed-by: Maxime Ripard <mripard@kernel.org>
    Link: https://lore.kernel.org/r/20250808-drm-bridge-alloc-getput-for_each_bridge-v2-1-edb6ee81edf1@bootlin.com
    Signed-off-by: Luca Ceresoli <luca.ceresoli@bootlin.com>

--- drivers/gpu/drm/display/drm_bridge_connector.c
+++ drivers/gpu/drm/display/drm_bridge_connector.c
@@ -818,7 +818,7 @@ struct drm_connector *drm_bridge_connector_init(struct drm_device *drm,
 
 	if (bridge_connector->bridge_hdmi_cec &&
 	    bridge_connector->bridge_hdmi_cec->ops & DRM_BRIDGE_OP_HDMI_CEC_NOTIFIER) {
-		bridge = bridge_connector->bridge_hdmi_cec;
+		struct drm_bridge *bridge = bridge_connector->bridge_hdmi_cec;
 
 		ret = drmm_connector_hdmi_cec_notifier_register(connector,
 								NULL,
@@ -829,7 +829,7 @@ struct drm_connector *drm_bridge_connector_init(struct drm_device *drm,
 
 	if (bridge_connector->bridge_hdmi_cec &&
 	    bridge_connector->bridge_hdmi_cec->ops & DRM_BRIDGE_OP_HDMI_CEC_ADAPTER) {
-		bridge = bridge_connector->bridge_hdmi_cec;
+		struct drm_bridge *bridge = bridge_connector->bridge_hdmi_cec;
 
 		ret = drmm_connector_hdmi_cec_register(connector,
 						       &drm_bridge_connector_hdmi_cec_funcs,
