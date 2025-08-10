dc322d13cf417552b59e313e809a6da40b8b36ef
Jacopo Mondi <jacopo.mondi@ideasonboard.com>
2025-08-10 04:30:15 +0300

    media: zoran: Remove zoran_fh structure
    The zoran_fh structure is a wrapper around v4l2_fh. Its usage has been
    mostly removed by commit 83f89a8bcbc3 ("media: zoran: convert to vb2"),
    but the structure stayed by mistake. It is now used in a single
    location, assigned from a void pointer and then recast to a void
    pointer, without being every accessed. Drop it.
    
    Fixes: 83f89a8bcbc3 ("media: zoran: convert to vb2")
    Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Hans Verkuil <hverkuil+cisco@kernel.org>

--- drivers/media/pci/zoran/zoran_driver.c
+++ drivers/media/pci/zoran/zoran_driver.c
@@ -511,12 +511,11 @@ static int zoran_s_fmt_vid_cap(struct file *file, void *__fh,
 			       struct v4l2_format *fmt)
 {
 	struct zoran *zr = video_drvdata(file);
-	struct zoran_fh *fh = __fh;
 	int i;
 	int res = 0;
 
 	if (fmt->fmt.pix.pixelformat == V4L2_PIX_FMT_MJPEG)
-		return zoran_s_fmt_vid_out(file, fh, fmt);
+		return zoran_s_fmt_vid_out(file, __fh, fmt);
 
 	for (i = 0; i < NUM_FORMATS; i++)
 		if (fmt->fmt.pix.pixelformat == zoran_formats[i].fourcc)
