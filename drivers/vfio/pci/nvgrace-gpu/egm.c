0027e8ee56f440119ae4a145a2bbfe0cc755d872
Ankit Agrawal <ankita@nvidia.com>
2025-08-14 11:14:44 +0000

    vfio/nvgrace-egm: Introduce egm class and register char device numbers
    The EGM regions are exposed to the userspace as char devices. A unique
    char device with a different minor number is assigned to EGM region
    belonging to a different Grace socket.
    
    Add a new egm class and register a range of char device numbers for
    the same.
    
    Setting MAX_EGM_NODES as 4 as the 4-socket is the largest configuration
    on Grace based systems.
    
    Suggested-by: Aniket Agashe <aniketa@nvidia.com>
    Signed-off-by: Ankit Agrawal <ankita@nvidia.com>

--- drivers/vfio/pci/nvgrace-gpu/egm.c
+++ drivers/vfio/pci/nvgrace-gpu/egm.c
@@ -4,14 +4,50 @@
  */
 
 #include <linux/vfio_pci_core.h>
+#include <linux/nvgrace-egm.h>
+
+#define MAX_EGM_NODES 4
+
+static dev_t dev;
+static struct class *class;
+
+static char *egm_devnode(const struct device *device, umode_t *mode)
+{
+	if (mode)
+		*mode = 0600;
+
+	return NULL;
+}
 
 static int __init nvgrace_egm_init(void)
 {
+	int ret;
+
+	/*
+	 * Each EGM region on a system is represented with a unique
+	 * char device with a different minor number. Allow a range
+	 * of char device creation.
+	 */
+	ret = alloc_chrdev_region(&dev, 0, MAX_EGM_NODES,
+				  NVGRACE_EGM_DEV_NAME);
+	if (ret < 0)
+		return ret;
+
+	class = class_create(NVGRACE_EGM_DEV_NAME);
+	if (IS_ERR(class)) {
+		unregister_chrdev_region(dev, MAX_EGM_NODES);
+		return PTR_ERR(class);
+	}
+
+	class->devnode = egm_devnode;
+
 	return 0;
 }
 
 static void __exit nvgrace_egm_cleanup(void)
 {
+	class_destroy(class);
+	unregister_chrdev_region(dev, MAX_EGM_NODES);
 }
 
 module_init(nvgrace_egm_init);
