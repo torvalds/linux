292cb391479d50f4379a0abab34324de92c82a92
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2025-08-13 23:03:52 -0700

    software node: Constify node_group in registration functions
    The software_node_register_node_group() and
    software_node_unregister_node_group() functions take in essence an
    array of pointers to software_node structs. Since the functions do not
    modify the array declare the argument as constant, so that static
    arrays can be declared as const and annotated as __initconst.
    
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Link: https://lore.kernel.org/r/2zny5grbgtwbplynxffxg6dkgjgqf45aigwmgxio5stesdr3wi@gf2zamk5amic
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/property.h
+++ include/linux/property.h
@@ -574,8 +574,8 @@ const struct software_node *
 software_node_find_by_name(const struct software_node *parent,
 			   const char *name);
 
-int software_node_register_node_group(const struct software_node **node_group);
-void software_node_unregister_node_group(const struct software_node **node_group);
+int software_node_register_node_group(const struct software_node * const *node_group);
+void software_node_unregister_node_group(const struct software_node * const *node_group);
 
 int software_node_register(const struct software_node *node);
 void software_node_unregister(const struct software_node *node);
