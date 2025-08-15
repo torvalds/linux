f17bd297bb833e6f1cae9d3cadad8f334bc3cb8b
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2025-08-15 12:32:05 +0100

    net: stmmac: remove unnecessary "stmmac: wakeup enable" print
    Printing "stmmac: wakeup enable" to the kernel log isn't useful - it
    doesn't identify the adapter, and is effectively nothing more than a
    debugging print. This information can be discovered by looking at
    /sys/device.../power/wakeup as the device_set_wakeup_enable() call
    updates this sysfs file.
    
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Link: https://patch.msgid.link/E1umsfF-008vKc-Kt@rmk-PC.armlinux.org.uk
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
@@ -816,7 +816,6 @@ static int stmmac_set_wol(struct net_device *dev, struct ethtool_wolinfo *wol)
 	}
 
 	if (wol->wolopts) {
-		pr_info("stmmac: wakeup enable\n");
 		device_set_wakeup_enable(priv->device, 1);
 		/* Avoid unbalanced enable_irq_wake calls */
 		if (priv->wol_irq_disabled)
