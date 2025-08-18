7bf1158514e410310aec975e630cec99d4e4092f
Kuen-Han Tsai <khtsai@google.com>
2025-08-18 16:27:19 +0800

    usb: udc: Add trace event for usb_gadget_set_state
    While the userspace program can be notified of gadget state changes,
    timing issue can lead to missed transitions when reading the state
    value.
    
    Introduce a trace event for usb_gadget_set_state to reliably track state
    transitions.
    
    Signed-off-by: Kuen-Han Tsai <khtsai@google.com>
    Link: https://lore.kernel.org/r/20250818082722.2952867-1-khtsai@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/gadget/udc/core.c
+++ drivers/usb/gadget/udc/core.c
@@ -1125,6 +1125,7 @@ void usb_gadget_set_state(struct usb_gadget *gadget,
 {
 	gadget->state = state;
 	schedule_work(&gadget->work);
+	trace_usb_gadget_set_state(gadget, 0);
 }
 EXPORT_SYMBOL_GPL(usb_gadget_set_state);
 
