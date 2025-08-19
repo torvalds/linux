ffdd27d36265be108827c606c9fbe81a5947547e
Qianfeng Rong <rongqianfeng@vivo.com>
2025-08-19 21:12:33 +0800

    PCI: keystone: Use kcalloc() instead of kzalloc()
    Replace calls of devm_kzalloc() with devm_kcalloc() in ks_pcie_probe().
    As noted in the kernel documentation [1], open-coded multiplication in
    allocator arguments is discouraged because it can lead to integer
    overflow.
    
    Using devm_kcalloc() provides built-in overflow protection, making the
    memory allocation safer when calculating the allocation size compared
    to explicit multiplication.
    
    [1]: https://www.kernel.org/doc/html/next/process/deprecated.html#open-coded-arithmetic-in-allocator-arguments
    
    Signed-off-by: Qianfeng Rong <rongqianfeng@vivo.com>
    Signed-off-by: Manivannan Sadhasivam <mani@kernel.org>
    Reviewed-by: Siddharth Vadapalli <s-vadapalli@ti.com>
    Link: https://patch.msgid.link/20250819131235.152967-1-rongqianfeng@vivo.com

--- drivers/pci/controller/dwc/pci-keystone.c
+++ drivers/pci/controller/dwc/pci-keystone.c
@@ -1213,11 +1213,11 @@ static int ks_pcie_probe(struct platform_device *pdev)
 	if (ret)
 		num_lanes = 1;
 
-	phy = devm_kzalloc(dev, sizeof(*phy) * num_lanes, GFP_KERNEL);
+	phy = devm_kcalloc(dev, num_lanes, sizeof(*phy), GFP_KERNEL);
 	if (!phy)
 		return -ENOMEM;
 
-	link = devm_kzalloc(dev, sizeof(*link) * num_lanes, GFP_KERNEL);
+	link = devm_kcalloc(dev, num_lanes, sizeof(*link), GFP_KERNEL);
 	if (!link)
 		return -ENOMEM;
 
