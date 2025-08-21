b68b7f3e9b50747b88ba211080d27310430c928b
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2025-08-21 15:28:10 +0200

    sched/core: Avoid direct access to hrtimer clockbase
    The field timer->base->get_time is a private implementation detail and
    should not be accessed outside of the hrtimer core.
    
    Switch to the equivalent helper.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/all/20250821-hrtimer-cleanup-get_time-v2-3-3ae822e5bfbd@linutronix.de

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -917,7 +917,7 @@ void hrtick_start(struct rq *rq, u64 delay)
 	 * doesn't make sense and can cause timer DoS.
 	 */
 	delta = max_t(s64, delay, 10000LL);
-	rq->hrtick_time = ktime_add_ns(timer->base->get_time(), delta);
+	rq->hrtick_time = ktime_add_ns(hrtimer_cb_get_time(timer), delta);
 
 	if (rq == this_rq())
 		__hrtick_restart(rq);
