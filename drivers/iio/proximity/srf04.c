1f6fcf7fb4ca49c19960b9545f9cd352ac2b7a31
Dixit Parmar <dixitparmar19@gmail.com>
2025-08-22 09:19:57 +0530

    iio: proximity: Drop unnecessary -ENOMEM messages
    The drivers do not require their own error messages for error
    -ENOMEM, memory allocation failures. So remove the dev_err()
    messages from the probe().
    
    Signed-off-by: Dixit Parmar <dixitparmar19@gmail.com>
    Link: https://patch.msgid.link/20250822-enomam_logs-v1-9-db87f2974552@gmail.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/proximity/srf04.c
+++ drivers/iio/proximity/srf04.c
@@ -253,10 +253,8 @@ static int srf04_probe(struct platform_device *pdev)
 	int ret;
 
 	indio_dev = devm_iio_device_alloc(dev, sizeof(struct srf04_data));
-	if (!indio_dev) {
-		dev_err(dev, "failed to allocate IIO device\n");
+	if (!indio_dev)
 		return -ENOMEM;
-	}
 
 	data = iio_priv(indio_dev);
 	data->dev = dev;
