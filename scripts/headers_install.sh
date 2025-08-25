c2a756891bb428104fa8899998ba277042274cdb
Jakub Kicinski <kuba@kernel.org>
2025-08-25 13:18:28 -0700

    uapi: wrap compiler_types.h in an ifdef instead of the implicit strip
    The uAPI stddef header includes compiler_types.h, a kernel-only
    header, to make sure that kernel definitions of annotations
    like __counted_by() take precedence.
    
    There is a hack in scripts/headers_install.sh which strips includes
    of compiler.h and compiler_types.h when installing uAPI headers.
    While explicit handling makes sense for compiler.h, which is included
    all over the uAPI, compiler_types.h is only included by stddef.h
    (within the uAPI, obviously it's included in kernel code a lot).
    
    Remove the stripping from scripts/headers_install.sh and wrap
    the include of compiler_types.h in #ifdef __KERNEL__ instead.
    This should be equivalent functionally, but is easier to understand
    to a casual reader of the code. It also makes it easier to work
    with kernel headers directly from under tools/
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://patch.msgid.link/20250825201828.2370083-1-kuba@kernel.org
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- scripts/headers_install.sh
+++ scripts/headers_install.sh
@@ -32,7 +32,7 @@ fi
 sed -E -e '
 	s/([[:space:](])(__user|__force|__iomem)[[:space:]]/\1/g
 	s/__attribute_const__([[:space:]]|$)/\1/g
-	s@^#include <linux/compiler(|_types).h>@@
+	s@^#include <linux/compiler.h>@@
 	s/(^|[^a-zA-Z0-9])__packed([^a-zA-Z0-9_]|$)/\1__attribute__((packed))\2/g
 	s/(^|[[:space:](])(inline|asm|volatile)([[:space:](]|$)/\1__\2__\3/g
 	s@#(ifndef|define|endif[[:space:]]*/[*])[[:space:]]*_UAPI@#\1 @
