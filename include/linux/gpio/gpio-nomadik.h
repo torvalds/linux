ddeb66d2cb10f03a43d97a0ff2c3869d1951c87d
Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
2025-08-26 11:54:36 +0200

    gpio: nomadik: don't print out global GPIO numbers in debugfs callbacks
    In order to further limit the number of references to the GPIO base
    number stored in struct gpio_chip, replace the global GPIO numbers in
    the output of debugfs callbacks by hardware offsets.
    
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://lore.kernel.org/r/20250826-gpio-dbg-show-base-v1-2-7f27cd7f2256@linaro.org
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>

--- include/linux/gpio/gpio-nomadik.h
+++ include/linux/gpio/gpio-nomadik.h
@@ -261,8 +261,7 @@ struct platform_device;
  * true.
  */
 void nmk_gpio_dbg_show_one(struct seq_file *s, struct pinctrl_dev *pctldev,
-			   struct gpio_chip *chip, unsigned int offset,
-			   unsigned int gpio);
+			   struct gpio_chip *chip, unsigned int offset);
 
 #else
 
