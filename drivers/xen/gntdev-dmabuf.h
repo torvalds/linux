2ea7a5bcc4cfca817b3502b38f97885767730ed8
Juergen Gross <jgross@suse.com>
2025-08-26 16:56:08 +0200

    drivers/xen/gntdev: use xen_pv_domain() instead of cached value
    Eliminate the use_ptemod variable by replacing its use cases with
    xen_pv_domain().
    
    Instead of passing the xen_pv_domain() return value to
    gntdev_ioctl_dmabuf_exp_from_refs(), use xen_pv_domain() in that
    function.
    
    Reviewed-by: Jason Andryuk <jason.andryuk@amd.com>
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Message-ID: <20250826145608.10352-4-jgross@suse.com>

--- drivers/xen/gntdev-dmabuf.h
+++ drivers/xen/gntdev-dmabuf.h
@@ -18,7 +18,7 @@ struct gntdev_dmabuf_priv *gntdev_dmabuf_init(struct file *filp);
 
 void gntdev_dmabuf_fini(struct gntdev_dmabuf_priv *priv);
 
-long gntdev_ioctl_dmabuf_exp_from_refs(struct gntdev_priv *priv, int use_ptemod,
+long gntdev_ioctl_dmabuf_exp_from_refs(struct gntdev_priv *priv,
 				       struct ioctl_gntdev_dmabuf_exp_from_refs __user *u);
 
 long gntdev_ioctl_dmabuf_exp_wait_released(struct gntdev_priv *priv,
