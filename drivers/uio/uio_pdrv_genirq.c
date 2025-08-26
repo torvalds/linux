47625846727b82e4a05f14d988fc5efa2c1d882f
Vivian Wang <wangruikang@iscas.ac.cn>
2025-08-26 18:34:40 +0800

    uio: uio_pdrv_genirq: Remove MODULE_DEVICE_TABLE
    The struct of_device_id list here is filled in at load time since
    commit 05c3e0bb5629 ("UIO: allow binding uio_pdrv_genirq.c to devices using
    command line option").
    
    However, this does not work with MODULE_DEVICE_TABLE, which generates an
    alias at build time.  In this case, it generates the aliases "of:N*T*"
    and "of:N*T*C*", which matches *any* OF device node, which is obviously
    not intended. It confuses userspace into loading this module for any OF
    device, and confuses anyone trying to resolve an OF modalias.
    
    Remove this broken MODULE_DEVICE_TABLE.
    
    Suggested-by: Emily <hello@emily.moe>
    Fixes: 05c3e0bb5629 ("UIO: allow binding uio_pdrv_genirq.c to devices using command line option")
    Signed-off-by: Vivian Wang <wangruikang@iscas.ac.cn>
    Link: https://lore.kernel.org/r/20250826-uio-no-modalias-v2-1-495afa9e8a5b@iscas.ac.cn
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/uio/uio_pdrv_genirq.c
+++ drivers/uio/uio_pdrv_genirq.c
@@ -276,7 +276,6 @@ static struct of_device_id uio_of_genirq_match[] = {
 	{ /* This is filled with module_parm */ },
 	{ /* Sentinel */ },
 };
-MODULE_DEVICE_TABLE(of, uio_of_genirq_match);
 module_param_string(of_id, uio_of_genirq_match[0].compatible, 128, 0);
 MODULE_PARM_DESC(of_id, "Openfirmware id of the device to be handled by uio");
 #endif
