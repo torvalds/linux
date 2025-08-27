04d3cd43700a2d0fe4bfb1012a8ec7f2e34a3507
Breno Leitao <leitao@debian.org>
2025-08-27 03:42:21 -0700

    arm64: kexec: initialize kexec_buf struct in load_other_segments()
    Patch series "kexec: Fix invalid field access".
    
    The kexec_buf structure was previously declared without initialization. 
    commit bf454ec31add ("kexec_file: allow to place kexec_buf randomly")
    added a field that is always read but not consistently populated by all
    architectures.  This un-initialized field will contain garbage.
    
    This is also triggering a UBSAN warning when the uninitialized data was
    accessed:
    
    	------------[ cut here ]------------
    	UBSAN: invalid-load in ./include/linux/kexec.h:210:10
    	load of value 252 is not a valid value for type '_Bool'
    
    Zero-initializing kexec_buf at declaration ensures all fields are cleanly
    set, preventing future instances of uninitialized memory being used.
    
    An initial fix was already landed for arm64[0], and this patchset fixes
    the problem on the remaining arm64 code and on riscv, as raised by Mark.
    
    Discussions about this problem could be found at[1][2].
    
    
    This patch (of 3):
    
    The kexec_buf structure was previously declared without initialization.
    commit bf454ec31add ("kexec_file: allow to place kexec_buf randomly")
    added a field that is always read but not consistently populated by all
    architectures. This un-initialized field will contain garbage.
    
    This is also triggering a UBSAN warning when the uninitialized data was
    accessed:
    
    	------------[ cut here ]------------
    	UBSAN: invalid-load in ./include/linux/kexec.h:210:10
    	load of value 252 is not a valid value for type '_Bool'
    
    Zero-initializing kexec_buf at declaration ensures all fields are
    cleanly set, preventing future instances of uninitialized memory being
    used.
    
    Link: https://lkml.kernel.org/r/20250827-kbuf_all-v1-0-1df9882bb01a@debian.org
    Link: https://lkml.kernel.org/r/20250827-kbuf_all-v1-1-1df9882bb01a@debian.org
    Link: https://lore.kernel.org/all/20250826180742.f2471131255ec1c43683ea07@linux-foundation.org/ [0]
    Link: https://lore.kernel.org/all/oninomspajhxp4omtdapxnckxydbk2nzmrix7rggmpukpnzadw@c67o7njgdgm3/ [1]
    Link: https://lore.kernel.org/all/20250826-akpm-v1-1-3c831f0e3799@debian.org/ [2]
    Fixes: bf454ec31add ("kexec_file: allow to place kexec_buf randomly")
    Signed-off-by: Breno Leitao <leitao@debian.org>
    Acked-by: Baoquan He <bhe@redhat.com>
    Cc: Albert Ou <aou@eecs.berkeley.edu>
    Cc: Alexander Gordeev <agordeev@linux.ibm.com>
    Cc: Alexandre Ghiti <alex@ghiti.fr>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Christian Borntraeger <borntraeger@linux.ibm.com>
    Cc: Coiby Xu <coxu@redhat.com>
    Cc: Heiko Carstens <hca@linux.ibm.com>
    Cc: Palmer Dabbelt <palmer@dabbelt.com>
    Cc: Paul Walmsley <paul.walmsley@sifive.com>
    Cc: Sven Schnelle <svens@linux.ibm.com>
    Cc: Vasily Gorbik <gor@linux.ibm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/arm64/kernel/machine_kexec_file.c
+++ arch/arm64/kernel/machine_kexec_file.c
@@ -94,7 +94,7 @@ int load_other_segments(struct kimage *image,
 			char *initrd, unsigned long initrd_len,
 			char *cmdline)
 {
-	struct kexec_buf kbuf;
+	struct kexec_buf kbuf = {};
 	void *dtb = NULL;
 	unsigned long initrd_load_addr = 0, dtb_len,
 		      orig_segments = image->nr_segments;
