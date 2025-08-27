3712ce9fa501617cdc4466d30ae3894d50887743
Antheas Kapenekakis <lkml@antheas.dev>
2025-08-27 19:58:42 +0200

    gpiolib: acpi: Ignore touchpad wakeup on GPD G1619-05
    Same issue as G1619-04 in commit 805c74eac8cb ("gpiolib: acpi: Ignore
    touchpad wakeup on GPD G1619-04"), Strix Point lineup uses 05.
    
    Signed-off-by: Antheas Kapenekakis <lkml@antheas.dev>
    Reviewed-by: Mika Westerberg <westeri@kernel.org>
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/gpio/gpiolib-acpi-quirks.c
+++ drivers/gpio/gpiolib-acpi-quirks.c
@@ -319,6 +319,18 @@ static const struct dmi_system_id gpiolib_acpi_quirks[] __initconst = {
 	},
 	{
 		/*
+		 * Same as G1619-04. New model.
+		 */
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "GPD"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "G1619-05"),
+		},
+		.driver_data = &(struct acpi_gpiolib_dmi_quirk) {
+			.ignore_wake = "PNP0C50:00@8",
+		},
+	},
+	{
+		/*
 		 * Spurious wakeups from GPIO 11
 		 * Found in BIOS 1.04
 		 * https://gitlab.freedesktop.org/drm/amd/-/issues/3954
