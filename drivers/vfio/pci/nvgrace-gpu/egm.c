8a2554725edb86a7374fa8a09e761aefd1de6bf4
Ankit Agrawal <ankita@nvidia.com>
2025-08-27 05:55:39 +0000

    vfio/nvgrace-egm: Add chardev ops for EGM management
    EGM module implements the mmap file_ops to manage the usermode app's
    VMA mapping to the EGM region. The appropriate region is determined
    from the minor number.
    
    Note that the EGM memory region is invisible to the host kernel as it
    is not present in the host EFI map. The host Linux MM thus cannot manage
    the memory, even though it is accessible on the host SPA. The EGM module
    thus use remap_pfn_range() to perform the VMA mapping to the EGM region.
    
    Suggested-by: Aniket Agashe <aniketa@nvidia.com>
    Signed-off-by: Ankit Agrawal <ankita@nvidia.com>

--- drivers/vfio/pci/nvgrace-gpu/egm.c
+++ drivers/vfio/pci/nvgrace-gpu/egm.c
@@ -17,19 +17,46 @@ struct chardev {
 	struct cdev cdev;
 };
 
+static struct nvgrace_egm_dev *
+egm_chardev_to_nvgrace_egm_dev(struct chardev *egm_chardev)
+{
+	struct auxiliary_device *aux_dev =
+		container_of(egm_chardev->device.parent, struct auxiliary_device, dev);
+
+	return container_of(aux_dev, struct nvgrace_egm_dev, aux_dev);
+}
+
 static int nvgrace_egm_open(struct inode *inode, struct file *file)
 {
+	struct chardev *egm_chardev =
+		container_of(inode->i_cdev, struct chardev, cdev);
+
+	file->private_data = egm_chardev;
+
 	return 0;
 }
 
 static int nvgrace_egm_release(struct inode *inode, struct file *file)
 {
+	file->private_data = NULL;
+
 	return 0;
 }
 
 static int nvgrace_egm_mmap(struct file *file, struct vm_area_struct *vma)
 {
-	return 0;
+	struct chardev *egm_chardev = file->private_data;
+	struct nvgrace_egm_dev *egm_dev =
+		egm_chardev_to_nvgrace_egm_dev(egm_chardev);
+
+	/*
+	 * EGM memory is invisible to the host kernel and is not managed
+	 * by it. Map the usermode VMA to the EGM region.
+	 */
+	return remap_pfn_range(vma, vma->vm_start,
+			       PHYS_PFN(egm_dev->egmphys),
+			       (vma->vm_end - vma->vm_start),
+			       vma->vm_page_prot);
 }
 
 static const struct file_operations file_ops = {
