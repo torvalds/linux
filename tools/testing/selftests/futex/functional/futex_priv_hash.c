5fdb877b4916a1eb2b2c85018c2311855893405b
Gopi Krishna Menon <krishnagopi487@gmail.com>
2025-08-27 15:00:10 +0200

    selftests/futex: Fix typos and grammar in futex_priv_hash
    Fix multiple typos and small grammar issues in help text, comments and test
    messages in the futex_priv_hash test.
    
    Signed-off-by: Gopi Krishna Menon <krishnagopi487@gmail.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: André Almeida <andrealmeid@igalia.com>
    Link: https://lore.kernel.org/20250827130011.677600-5-bigeasy@linutronix.de

--- tools/testing/selftests/futex/functional/futex_priv_hash.c
+++ tools/testing/selftests/futex/functional/futex_priv_hash.c
@@ -188,7 +188,7 @@ int main(int argc, char *argv[])
 	if (ret != 0)
 		ksft_exit_fail_msg("pthread_join() failed: %d, %m\n", ret);
 
-	/* First thread, has to initialiaze private hash */
+	/* First thread, has to initialize private hash */
 	futex_slots1 = futex_hash_slots_get();
 	if (futex_slots1 <= 0) {
 		ksft_print_msg("Current hash buckets: %d\n", futex_slots1);
@@ -256,17 +256,17 @@ retry_getslots:
 
 	futex_hash_slots_set_verify(2);
 	join_max_threads();
-	ksft_test_result(counter == MAX_THREADS, "Created of waited for %d of %d threads\n",
+	ksft_test_result(counter == MAX_THREADS, "Created and waited for %d of %d threads\n",
 			 counter, MAX_THREADS);
 	counter = 0;
-	/* Once the user set something, auto reisze must be disabled */
+	/* Once the user set something, auto resize must be disabled */
 	ret = pthread_barrier_init(&barrier_main, NULL, MAX_THREADS);
 
 	create_max_threads(thread_lock_fn);
 	join_max_threads();
 
 	ret = futex_hash_slots_get();
-	ksft_test_result(ret == 2, "No more auto-resize after manaul setting, got %d\n",
+	ksft_test_result(ret == 2, "No more auto-resize after manual setting, got %d\n",
 			 ret);
 
 	futex_hash_slots_set_must_fail(1 << 29);
