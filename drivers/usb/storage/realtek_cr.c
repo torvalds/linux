5195edb359855d9a6460f19b7c3915a8611fceed
Thorsten Blum <thorsten.blum@linux.dev>
2025-08-28 18:26:24 +0200

    usb: storage: realtek_cr: Simplify residue calculation in rts51x_bulk_transport()
    Simplify the calculation of 'residue' in rts51x_bulk_transport() and
    avoid unnecessarily reassigning 'residue' to itself.
    
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>
    Link: https://lore.kernel.org/r/20250828162623.4840-3-thorsten.blum@linux.dev
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/storage/realtek_cr.c
+++ drivers/usb/storage/realtek_cr.c
@@ -260,8 +260,8 @@ static int rts51x_bulk_transport(struct us_data *us, u8 lun,
 	 * try to compute the actual residue, based on how much data
 	 * was really transferred and what the device tells us
 	 */
-	if (residue)
-		residue = residue < buf_len ? residue : buf_len;
+	if (residue > buf_len)
+		residue = buf_len;
 
 	if (act_len)
 		*act_len = buf_len - residue;
