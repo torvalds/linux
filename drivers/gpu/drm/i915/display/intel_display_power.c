c698a48e9f8019c01eab83a962289ad4e81db938
Jani Nikula <jani.nikula@intel.com>
2025-08-28 15:20:51 +0300

    drm/i915/power: use generic poll_timeout_us() instead of wait_for()
    Prefer generic poll helpers over i915 custom helpers.
    
    The functional change is losing the exponentially growing sleep of
    wait_for(), which used to be 10, 20, 40, ..., 640, and 1280 us.
    
    Use an arbitrary constant 100 us sleep instead. The timeout remains at 1
    ms.
    
    Reviewed-by: Jouni Högander <jouni.hogander@intel.com>
    Link: https://lore.kernel.org/r/900680516b047ae32e3298b5cdbcede0393e0466.1756383233.git.jani.nikula@intel.com
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/display/intel_display_power.c
+++ drivers/gpu/drm/i915/display/intel_display_power.c
@@ -3,6 +3,7 @@
  * Copyright © 2019 Intel Corporation
  */
 
+#include <linux/iopoll.h>
 #include <linux/string_helpers.h>
 
 #include "soc/intel_dram.h"
@@ -1310,8 +1311,10 @@ static void hsw_disable_lcpll(struct intel_display *display,
 	hsw_write_dcomp(display, val);
 	ndelay(100);
 
-	if (wait_for((hsw_read_dcomp(display) &
-		      D_COMP_RCOMP_IN_PROGRESS) == 0, 1))
+	ret = poll_timeout_us(val = hsw_read_dcomp(display),
+			      (val & D_COMP_RCOMP_IN_PROGRESS) == 0,
+			      100, 1000, false);
+	if (ret)
 		drm_err(display->drm, "D_COMP RCOMP still in progress\n");
 
 	if (allow_power_down) {
