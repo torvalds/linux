ec6ac1d5e15a14e403630c5fbf8e41daf184ec83
Takashi Iwai <tiwai@suse.de>
2025-08-29 17:00:15 +0200

    ALSA: opl3: Use guard() for spin locks
    Clean up the code using guard() for spin locks.
    
    Merely code refactoring, and no behavior change.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Link: https://patch.msgid.link/20250829150026.6379-5-tiwai@suse.de

--- sound/drivers/opl3/opl3_seq.c
+++ sound/drivers/opl3/opl3_seq.c
@@ -66,16 +66,15 @@ int snd_opl3_synth_setup(struct snd_opl3 * opl3)
 
 void snd_opl3_synth_cleanup(struct snd_opl3 * opl3)
 {
-	unsigned long flags;
 	struct snd_hwdep *hwdep;
 
 	/* Stop system timer */
-	spin_lock_irqsave(&opl3->sys_timer_lock, flags);
-	if (opl3->sys_timer_status) {
-		timer_delete(&opl3->tlist);
-		opl3->sys_timer_status = 0;
+	scoped_guard(spinlock_irq, &opl3->sys_timer_lock) {
+		if (opl3->sys_timer_status) {
+			timer_delete(&opl3->tlist);
+			opl3->sys_timer_status = 0;
+		}
 	}
-	spin_unlock_irqrestore(&opl3->sys_timer_lock, flags);
 
 	snd_opl3_reset(opl3);
 	hwdep = opl3->hwdep;
