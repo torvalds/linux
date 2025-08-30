358253fa8179ab4217ac283b56adde0174186f87
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2025-08-30 13:16:58 +0200

    pinctrl: samsung: Drop unused S3C24xx driver data
    Drop unused declarations after S3C24xx SoC family removal in the commit
    61b7f8920b17 ("ARM: s3c: remove all s3c24xx support").
    
    Fixes: 1ea35b355722 ("ARM: s3c: remove s3c24xx specific hacks")
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/r/20250830111657.126190-3-krzysztof.kozlowski@linaro.org
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>

--- drivers/pinctrl/samsung/pinctrl-samsung.h
+++ drivers/pinctrl/samsung/pinctrl-samsung.h
@@ -403,10 +403,6 @@ extern const struct samsung_pinctrl_of_match_data exynosautov920_of_data;
 extern const struct samsung_pinctrl_of_match_data fsd_of_data;
 extern const struct samsung_pinctrl_of_match_data gs101_of_data;
 extern const struct samsung_pinctrl_of_match_data s3c64xx_of_data;
-extern const struct samsung_pinctrl_of_match_data s3c2412_of_data;
-extern const struct samsung_pinctrl_of_match_data s3c2416_of_data;
-extern const struct samsung_pinctrl_of_match_data s3c2440_of_data;
-extern const struct samsung_pinctrl_of_match_data s3c2450_of_data;
 extern const struct samsung_pinctrl_of_match_data s5pv210_of_data;
 
 #endif /* __PINCTRL_SAMSUNG_H */
