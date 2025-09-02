bde50e28f7c5fe874112fe9d98e84873548fa8de
Andreas Hindborg <a.hindborg@kernel.org>
2025-09-02 11:55:09 +0200

    rust: block: mq: fix spelling in a safety comment
    Add code block quotes to a safety comment.
    
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Daniel Almeida <daniel.almeida@collabora.com>
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>
    Link: https://lore.kernel.org/r/20250902-rnull-up-v6-16-v7-15-b5212cc89b98@kernel.org
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- rust/kernel/block/mq/request.rs
+++ rust/kernel/block/mq/request.rs
@@ -148,7 +148,7 @@ impl<T: Operations> Request<T> {
         // valid allocation.
         let wrapper_ptr =
             unsafe { bindings::blk_mq_rq_to_pdu(request_ptr).cast::<RequestDataWrapper>() };
-        // SAFETY: By C API contract, wrapper_ptr points to a valid allocation
+        // SAFETY: By C API contract, `wrapper_ptr` points to a valid allocation
         // and is not null.
         unsafe { NonNull::new_unchecked(wrapper_ptr) }
     }
