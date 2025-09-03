086f66edf9244d8c05b0ddf24631fe7f3472ac45
Liao Yuanhong <liaoyuanhong@vivo.com>
2025-09-03 20:03:50 +0800

    drm/amdgpu/vcn: Remove redundant ternary operators
    For ternary operators in the form of "a ? true : false", if 'a' itself
    returns a boolean result, the ternary operator can be omitted. Remove
    redundant ternary operators to clean up the code.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Liao Yuanhong <liaoyuanhong@vivo.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/vcn_v4_0_5.c
+++ drivers/gpu/drm/amd/amdgpu/vcn_v4_0_5.c
@@ -1591,7 +1591,7 @@ static int vcn_v4_0_5_set_clockgating_state(struct amdgpu_ip_block *ip_block,
 					  enum amd_clockgating_state state)
 {
 	struct amdgpu_device *adev = ip_block->adev;
-	bool enable = (state == AMD_CG_STATE_GATE) ? true : false;
+	bool enable = state == AMD_CG_STATE_GATE;
 	int i;
 
 	for (i = 0; i < adev->vcn.num_vcn_inst; ++i) {
