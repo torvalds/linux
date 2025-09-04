5d0316e25defee47c8c7b1b6324244f630b6621f
Naveen N Rao <naveen@kernel.org>
2025-09-05 00:03:06 +0530

    x86/cpufeatures: Add X86_FEATURE_X2AVIC_EXT
    Add CPUID feature bit for x2AVIC extension that enables AMD SVM to
    support up to 4096 vCPUs in x2AVIC mode. The primary change is in the
    size of the AVIC Physical ID table, which can now go up to 8 contiguous
    4k pages. The number of pages allocated is controlled by the maximum
    APIC ID for a guest, and that controls the number of pages to allocate
    for the AVIC Physical ID table. AVIC hardware is enhanced to look up
    Physical ID table entries for vCPUs > 512 for locating the target APIC
    backing page and the host APIC ID of the physical core on which the
    guest vCPU is running.
    
    Signed-off-by: Naveen N Rao (AMD) <naveen@kernel.org>
    Acked-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://lore.kernel.org/r/e5c9c471ab99a130bf9b728b77050ab308cf8624.1757009416.git.naveen@kernel.org
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -500,6 +500,7 @@
 #define X86_FEATURE_IBPB_EXIT_TO_USER	(21*32+14) /* Use IBPB on exit-to-userspace, see VMSCAPE bug */
 #define X86_FEATURE_ABMC		(21*32+15) /* Assignable Bandwidth Monitoring Counters */
 #define X86_FEATURE_MSR_IMM		(21*32+16) /* MSR immediate form instructions */
+#define X86_FEATURE_X2AVIC_EXT		(21*32+17) /* AMD SVM x2AVIC support for 4k vCPUs */
 
 /*
  * BUG word(s)
