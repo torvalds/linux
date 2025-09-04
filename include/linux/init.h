0b815825b1b0bd6762ca028e9b6631b002efb7ca
Kees Cook <kees@kernel.org>
2025-09-03 20:46:45 -0700

    x86/cfi: Remove __noinitretpoline and __noretpoline
    Commit 66f793099a63 ("x86/retpoline: Avoid retpolines for built-in __init
    functions") disabled retpolines in __init sections (__noinitretpoline)
    as a precaution against potential issues with retpolines in early boot,
    but it has not been a problem in practice (i.e. see Clang below).
    
    Commit 87358710c1fb ("x86/retpoline: Support retpoline builds with Clang")
    narrowed this to only GCC, as Clang doesn't have per-function control
    over retpoline emission. As such, Clang has been booting with retpolines
    in __init since retpoline support was introduced.
    
    Clang KCFI has been instrumenting __init since CFI was introduced.
    
    With the introduction of KCFI for GCC, KCFI instrumentation with
    retpolines disabled means that objtool does not construct .retpoline_sites
    section entries for the non-retpoline KCFI calls. At boot, the KCFI
    rehashing code, via __apply_fineibt(), misses all __init KCFI calls
    (since they are not retpolines), resulting in immediate hash mismatches:
    all preambles are rehashed (via .cfi_sites) and none of the __init call
    sites are rehashed.
    
    Remove __noinitretpoline since it provides no meaningful utility and
    creates problems with CFI. Additionally remove __noretpoline since it
    is now unused.
    
    Alternatively, cfi_rand_callers() could walk the .kcfi_traps section which
    is exactly the list of KCFI instrumentation sites. But it seems better to
    have as few differences in common instruction sequences between compilers
    as possible, so better to remove the special handling of retpolines in
    __init for GCC.
    
    Signed-off-by: Kees Cook <kees@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20250904034656.3670313-6-kees@kernel.org

--- include/linux/init.h
+++ include/linux/init.h
@@ -7,13 +7,6 @@
 #include <linux/stringify.h>
 #include <linux/types.h>
 
-/* Built-in __init functions needn't be compiled with retpoline */
-#if defined(__noretpoline) && !defined(MODULE)
-#define __noinitretpoline __noretpoline
-#else
-#define __noinitretpoline
-#endif
-
 /* These macros are used to mark some functions or 
  * initialized data (doesn't apply to uninitialized data)
  * as `initialization' functions. The kernel can take this
@@ -50,7 +43,6 @@
 /* These are for everybody (although not all archs will actually
    discard it in modules) */
 #define __init		__section(".init.text") __cold __latent_entropy	\
-						__noinitretpoline	\
 						__no_kstack_erase
 #define __initdata	__section(".init.data")
 #define __initconst	__section(".init.rodata")
