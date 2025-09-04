50243079865ae7c150bc54ea3ed59077cdf3da03
Dave Airlie <airlied@redhat.com>
2025-09-04 12:16:39 +1000

    ttm/bo: add an API to populate a bo before exporting.
    While discussing cgroups we noticed a problem where you could export
    a BO to a dma-buf without having it ever being backed or accounted for.
    
    This meant in low memory situations or eventually with cgroups, a
    lower privledged process might cause the compositor to try and allocate
    a lot of memory on it's behalf and this could fail. At least make
    sure the exporter has managed to allocate the RAM at least once
    before exporting the object.
    
    This only applies currently to TTM_PL_SYSTEM objects, because
    GTT objects get populated on first validate, and VRAM doesn't
    use TT.
    
    Reviewed-by: Christian Koenig <christian.koenig@amd.com>
    Cc: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Cc: Simona Vetter <simona.vetter@ffwll.ch>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    Reviewed-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    Link: https://lore.kernel.org/r/20250904021643.2050497-1-airlied@gmail.com

--- drivers/gpu/drm/ttm/ttm_bo.c
+++ drivers/gpu/drm/ttm/ttm_bo.c
@@ -1283,3 +1283,18 @@ int ttm_bo_populate(struct ttm_buffer_object *bo,
 	return 0;
 }
 EXPORT_SYMBOL(ttm_bo_populate);
+
+int ttm_bo_setup_export(struct ttm_buffer_object *bo,
+			struct ttm_operation_ctx *ctx)
+{
+	int ret;
+
+	ret = ttm_bo_reserve(bo, false, false, NULL);
+	if (ret != 0)
+		return ret;
+
+	ret = ttm_bo_populate(bo, ctx);
+	ttm_bo_unreserve(bo);
+	return ret;
+}
+EXPORT_SYMBOL(ttm_bo_setup_export);
