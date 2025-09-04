fd5081f4ef3325b49d26e41b5976d1f34032ca9b
Zqiang <qiang.zhang@linux.dev>
2025-09-04 19:31:32 +0800

    workqueue: Remove redundant rcu_read_lock/unlock() in workqueue_congested()
    The preempt_disable/enable() has already formed RCU read crtical
    section, this commit therefore remove rcu_read_lock/unlock() in
    workqueue_congested().
    
    Signed-off-by: Zqiang <qiang.zhang@linux.dev>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -6046,7 +6046,6 @@ bool workqueue_congested(int cpu, struct workqueue_struct *wq)
 	struct pool_workqueue *pwq;
 	bool ret;
 
-	rcu_read_lock();
 	preempt_disable();
 
 	if (cpu == WORK_CPU_UNBOUND)
@@ -6056,7 +6055,6 @@ bool workqueue_congested(int cpu, struct workqueue_struct *wq)
 	ret = !list_empty(&pwq->inactive_works);
 
 	preempt_enable();
-	rcu_read_unlock();
 
 	return ret;
 }
