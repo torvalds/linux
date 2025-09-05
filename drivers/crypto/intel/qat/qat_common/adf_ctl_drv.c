1e26339703e2afd397037defa798682b2b93dcc0
Thorsten Blum <thorsten.blum@linux.dev>
2025-09-05 18:39:11 +0200

    crypto: qat - Replace kzalloc() + copy_from_user() with memdup_user()
    Replace kzalloc() followed by copy_from_user() with memdup_user() to
    improve and simplify adf_ctl_alloc_resources(). memdup_user() returns
    either -ENOMEM or -EFAULT (instead of -EIO) if an error occurs.
    
    Remove the unnecessary device id initialization, since memdup_user()
    (like copy_from_user()) immediately overwrites it.
    
    No functional changes intended other than returning the more idiomatic
    error code -EFAULT.
    
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/intel/qat/qat_common/adf_ctl_drv.c
+++ drivers/crypto/intel/qat/qat_common/adf_ctl_drv.c
@@ -94,17 +94,10 @@ static int adf_ctl_alloc_resources(struct adf_user_cfg_ctl_data **ctl_data,
 {
 	struct adf_user_cfg_ctl_data *cfg_data;
 
-	cfg_data = kzalloc(sizeof(*cfg_data), GFP_KERNEL);
-	if (!cfg_data)
-		return -ENOMEM;
-
-	/* Initialize device id to NO DEVICE as 0 is a valid device id */
-	cfg_data->device_id = ADF_CFG_NO_DEVICE;
-
-	if (copy_from_user(cfg_data, (void __user *)arg, sizeof(*cfg_data))) {
+	cfg_data = memdup_user((void __user *)arg, sizeof(*cfg_data));
+	if (IS_ERR(cfg_data)) {
 		pr_err("QAT: failed to copy from user cfg_data.\n");
-		kfree(cfg_data);
-		return -EIO;
+		return PTR_ERR(cfg_data);
 	}
 
 	*ctl_data = cfg_data;
