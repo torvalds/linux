3e9c967f30a6dc57e4386c85ee5b75caee7246e8
Guenter Roeck <linux@roeck-us.net>
2025-09-07 20:41:27 -0700

    hwmon: (mr75203) Drop unnecessary include file
    The driver does not perform any locking and thus does not need to
    include mutex.h. Drop the unnecessary include file.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/mr75203.c
+++ drivers/hwmon/mr75203.c
@@ -14,7 +14,6 @@
 #include <linux/kstrtox.h>
 #include <linux/module.h>
 #include <linux/mod_devicetable.h>
-#include <linux/mutex.h>
 #include <linux/platform_device.h>
 #include <linux/property.h>
 #include <linux/regmap.h>
