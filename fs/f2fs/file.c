9251a9e6e871cb03c4714a18efa8f5d4a8818450
Chao Yu <chao@kernel.org>
2025-09-10 16:40:24 +0800

    f2fs: fix to truncate first page in error path of f2fs_truncate()
    syzbot reports a bug as below:
    
    loop0: detected capacity change from 0 to 40427
    F2FS-fs (loop0): Wrong SSA boundary, start(3584) end(4096) blocks(3072)
    F2FS-fs (loop0): Can't find valid F2FS filesystem in 1th superblock
    F2FS-fs (loop0): invalid crc value
    F2FS-fs (loop0): f2fs_convert_inline_folio: corrupted inline inode ino=3, i_addr[0]:0x1601, run fsck to fix.
    ------------[ cut here ]------------
    kernel BUG at fs/inode.c:753!
    RIP: 0010:clear_inode+0x169/0x190 fs/inode.c:753
    Call Trace:
     <TASK>
     evict+0x504/0x9c0 fs/inode.c:810
     f2fs_fill_super+0x5612/0x6fa0 fs/f2fs/super.c:5047
     get_tree_bdev_flags+0x40e/0x4d0 fs/super.c:1692
     vfs_get_tree+0x8f/0x2b0 fs/super.c:1815
     do_new_mount+0x2a2/0x9e0 fs/namespace.c:3808
     do_mount fs/namespace.c:4136 [inline]
     __do_sys_mount fs/namespace.c:4347 [inline]
     __se_sys_mount+0x317/0x410 fs/namespace.c:4324
     do_syscall_x64 arch/x86/entry/syscall_64.c:63 [inline]
     do_syscall_64+0xfa/0x3b0 arch/x86/entry/syscall_64.c:94
     entry_SYSCALL_64_after_hwframe+0x77/0x7f
    
    During f2fs_evict_inode(), clear_inode() detects that we missed to truncate
    all page cache before destorying inode, that is because in below path, we
    will create page #0 in cache, but missed to drop it in error path, let's fix
    it.
    
    - evict
     - f2fs_evict_inode
      - f2fs_truncate
       - f2fs_convert_inline_inode
        - f2fs_grab_cache_folio
        : create page #0 in cache
        - f2fs_convert_inline_folio
        : sanity check failed, return -EFSCORRUPTED
      - clear_inode detects that inode->i_data.nrpages is not zero
    
    Fixes: 92dffd01790a ("f2fs: convert inline_data when i_size becomes large")
    Reported-by: syzbot+90266696fe5daacebd35@syzkaller.appspotmail.com
    Closes: https://lore.kernel.org/linux-f2fs-devel/68c09802.050a0220.3c6139.000e.GAE@google.com
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -904,8 +904,16 @@ int f2fs_truncate(struct inode *inode)
 	/* we should check inline_data size */
 	if (!f2fs_may_inline_data(inode)) {
 		err = f2fs_convert_inline_inode(inode);
-		if (err)
+		if (err) {
+			/*
+			 * Always truncate page #0 to avoid page cache
+			 * leak in evict() path.
+			 */
+			truncate_inode_pages_range(inode->i_mapping,
+					F2FS_BLK_TO_BYTES(0),
+					F2FS_BLK_END_BYTES(0));
 			return err;
+		}
 	}
 
 	err = f2fs_truncate_blocks(inode, i_size_read(inode), true);
