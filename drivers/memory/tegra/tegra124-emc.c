1c9cce8a0e0bd4f383a04655bd3a4f650fc1f20f
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2025-09-11 11:43:13 +0200

    memory: tegra124-emc: Do not print error on icc_node_create() failure
    icc_node_create() is alloc-like function, so no need to print error
    messages on its failure.  Dropping one label makes the code a bit
    simpler.
    
    Reviewed-by: Jon Hunter <jonathanh@nvidia.com>
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>

--- drivers/memory/tegra/tegra124-emc.c
+++ drivers/memory/tegra/tegra124-emc.c
@@ -1350,10 +1350,8 @@ static int tegra_emc_interconnect_init(struct tegra_emc *emc)
 
 	/* create External Memory Controller node */
 	node = icc_node_create(TEGRA_ICC_EMC);
-	if (IS_ERR(node)) {
-		err = PTR_ERR(node);
-		goto err_msg;
-	}
+	if (IS_ERR(node))
+		return PTR_ERR(node);
 
 	node->name = "External Memory Controller";
 	icc_node_add(node, &emc->provider);
@@ -1381,7 +1379,6 @@ static int tegra_emc_interconnect_init(struct tegra_emc *emc)
 
 remove_nodes:
 	icc_nodes_remove(&emc->provider);
-err_msg:
 	dev_err(emc->dev, "failed to initialize ICC: %d\n", err);
 
 	return err;
