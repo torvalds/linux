a4a66cbaa20f51cb953d09a95c67cb237a088ec9
Peter Chiu <chui-hao.chiu@mediatek.com>
2025-09-15 09:59:05 +0200

    wifi: mt76: mt7996: disable promiscuous mode by default
    Set MT_WF_RFCR_DROP_OTHER_UC by default and disable this flag in
    mt7996_set_monitor only if monitor mode is enabled.
    
    Without this patch, the MT_WF_RFCR_DROP_OTHER_UC would not be set so the
    driver would receive lots of packets meant for other devices.
    
    Signed-off-by: Peter Chiu <chui-hao.chiu@mediatek.com>
    Link: https://patch.msgid.link/20250915075910.47558-10-nbd@nbd.name
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7996/init.c
+++ drivers/net/wireless/mediatek/mt76/mt7996/init.c
@@ -410,6 +410,7 @@ mt7996_init_wiphy_band(struct ieee80211_hw *hw, struct mt7996_phy *phy)
 
 	phy->slottime = 9;
 	phy->beacon_rate = -1;
+	phy->rxfilter = MT_WF_RFCR_DROP_OTHER_UC;
 
 	if (phy->mt76->cap.has_2ghz) {
 		phy->mt76->sband_2g.sband.ht_cap.cap |=
