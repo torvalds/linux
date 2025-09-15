33eea63ff9c6abf26236dcad41165d3cc499d905
Thorsten Blum <thorsten.blum@linux.dev>
2025-09-16 00:12:45 +0200

    crypto: fips - replace simple_strtol with kstrtoint to improve fips_enable
    Replace simple_strtol() with the recommended kstrtoint() for parsing the
    'fips=' boot parameter. Unlike simple_strtol(), which returns a long,
    kstrtoint() converts the string directly to an integer and avoids
    implicit casting.
    
    Check the return value of kstrtoint() and reject invalid values. This
    adds error handling while preserving existing behavior for valid values,
    and removes use of the deprecated simple_strtol() helper.
    
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/fips.c
+++ crypto/fips.c
@@ -24,7 +24,10 @@ EXPORT_SYMBOL_GPL(fips_fail_notif_chain);
 /* Process kernel command-line parameter at boot time. fips=0 or fips=1 */
 static int fips_enable(char *str)
 {
-	fips_enabled = !!simple_strtol(str, NULL, 0);
+	if (kstrtoint(str, 0, &fips_enabled))
+		return 0;
+
+	fips_enabled = !!fips_enabled;
 	pr_info("fips mode: %s\n", str_enabled_disabled(fips_enabled));
 	return 1;
 }
