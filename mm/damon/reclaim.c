3f7a914ab9a5e46cf8aac7de270f02aa3f63de04
SeongJae Park <sj@kernel.org>
2025-09-15 20:35:07 -0700

    mm/damon/reclaim: use damon_initialized()
    DAMON_RECLAIM is assuming DAMON is ready to use in module_init time, and
    uses its own hack to see if it is the time.  Use damon_initialized(),
    which is a way for seeing if DAMON is ready to be used that is more
    reliable and better to maintain instead of the hack.
    
    Link: https://lkml.kernel.org/r/20250916033511.116366-4-sj@kernel.org
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/damon/reclaim.c
+++ mm/damon/reclaim.c
@@ -349,7 +349,7 @@ static int damon_reclaim_enabled_store(const char *val,
 		return 0;
 
 	/* Called before init function.  The function will handle this. */
-	if (!ctx)
+	if (!damon_initialized())
 		goto set_param_out;
 
 	err = damon_reclaim_turn(enable);
@@ -372,8 +372,13 @@ MODULE_PARM_DESC(enabled,
 
 static int __init damon_reclaim_init(void)
 {
-	int err = damon_modules_new_paddr_ctx_target(&ctx, &target);
+	int err;
 
+	if (!damon_initialized()) {
+		err = -ENOMEM;
+		goto out;
+	}
+	err = damon_modules_new_paddr_ctx_target(&ctx, &target);
 	if (err)
 		goto out;
 
