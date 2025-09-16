201c53c687f2b55a7cc6d9f4000af4797860174b
Kuen-Han Tsai <khtsai@google.com>
2025-09-16 16:21:33 +0800

    usb: gadget: Introduce free_usb_request helper
    Introduce the free_usb_request() function that frees both the request's
    buffer and the request itself.
    
    This function serves as the cleanup callback for DEFINE_FREE() to enable
    automatic, scope-based cleanup for usb_request pointers.
    
    Signed-off-by: Kuen-Han Tsai <khtsai@google.com>
    Link: https://lore.kernel.org/r/20250916-ready-v1-2-4997bf277548@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://lore.kernel.org/r/20250916-ready-v1-2-4997bf277548@google.com

--- include/linux/usb/gadget.h
+++ include/linux/usb/gadget.h
@@ -15,6 +15,7 @@
 #ifndef __LINUX_USB_GADGET_H
 #define __LINUX_USB_GADGET_H
 
+#include <linux/cleanup.h>
 #include <linux/configfs.h>
 #include <linux/device.h>
 #include <linux/errno.h>
@@ -290,6 +291,28 @@ static inline int usb_ep_fifo_status(struct usb_ep *ep)
 static inline void usb_ep_fifo_flush(struct usb_ep *ep)
 { }
 #endif /* USB_GADGET */
+
+/*-------------------------------------------------------------------------*/
+
+/**
+ * free_usb_request - frees a usb_request object and its buffer
+ * @req: the request being freed
+ *
+ * This helper function frees both the request's buffer and the request object
+ * itself by calling usb_ep_free_request(). Its signature is designed to be used
+ * with DEFINE_FREE() to enable automatic, scope-based cleanup for usb_request
+ * pointers.
+ */
+static inline void free_usb_request(struct usb_request *req)
+{
+	if (!req)
+		return;
+
+	kfree(req->buf);
+	usb_ep_free_request(req->ep, req);
+}
+
+DEFINE_FREE(free_usb_request, struct usb_request *, free_usb_request(_T))
 
 /*-------------------------------------------------------------------------*/
 
