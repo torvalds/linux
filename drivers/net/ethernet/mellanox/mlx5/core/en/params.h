913d28f8a71cd8e38d6d788c70643f5a71507400
Cosmin Ratiu <cratiu@nvidia.com>
2025-09-16 17:11:37 +0300

    net/mlx5e: Remove unused 'xsk' param of mlx5e_build_xdpsq_param
    This was added in commit [1], but its only use removed in commit [2].
    The parameter is unused, so remove it from the function parameter list.
    
    [1] commit 9ded70fa1d81 ("net/mlx5e: Don't prefill WQEs in XDP SQ in the
    multi buffer mode")
    [2] commit 1a9304859b3a ("net/mlx5: XDP, Enable TX side XDP multi-buffer
    support")
    Signed-off-by: Cosmin Ratiu <cratiu@nvidia.com>
    Reviewed-by: Dragos Tatulea <dtatulea@nvidia.com>
    Signed-off-by: Tariq Toukan <tariqt@nvidia.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/mellanox/mlx5/core/en/params.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en/params.h
@@ -132,7 +132,6 @@ void mlx5e_build_tx_cq_param(struct mlx5_core_dev *mdev,
 			     struct mlx5e_cq_param *param);
 void mlx5e_build_xdpsq_param(struct mlx5_core_dev *mdev,
 			     struct mlx5e_params *params,
-			     struct mlx5e_xsk_param *xsk,
 			     struct mlx5e_sq_param *param);
 int mlx5e_build_channel_param(struct mlx5_core_dev *mdev,
 			      struct mlx5e_params *params,
