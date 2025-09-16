1b881ee294b2d8929a77b0e489047765d55f0191
Jiri Olsa <jolsa@kernel.org>
2025-09-16 23:53:00 +0200

    selftests/bpf: Add kprobe write ctx attach test
    Adding test to check we can't attach standard kprobe program that
    writes to the context.
    
    It's x86_64 specific test.
    
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Link: https://lore.kernel.org/r/20250916215301.664963-6-jolsa@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

---
+++ tools/testing/selftests/bpf/progs/kprobe_write_ctx.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+char _license[] SEC("license") = "GPL";
+
+#if defined(__TARGET_ARCH_x86)
+SEC("kprobe")
+int kprobe_write_ctx(struct pt_regs *ctx)
+{
+	ctx->ax = 0;
+	return 0;
+}
+#endif
