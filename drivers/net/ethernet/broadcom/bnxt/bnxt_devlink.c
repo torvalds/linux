e23c40d41b88281b771920035d84050735aaf841
Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
2025-09-16 21:08:31 -0700

    bnxt_en: Remove unnecessary VF check in bnxt_hwrm_nvm_req()
    The driver registers the supported configuration parameters with the
    devlink stack only on the PF using devlink_params_register().
    Hence there is no need for a VF check inside bnxt_hwrm_nvm_req().
    
    Reviewed-by: Ajit Khaparde <ajit.khaparde@broadcom.com>
    Reviewed-by: Somnath Kotur <somnath.kotur@broadcom.com>
    Signed-off-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
    Signed-off-by: Michael Chan <michael.chan@broadcom.com>
    Link: https://patch.msgid.link/20250917040839.1924698-3-michael.chan@broadcom.com
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt_devlink.c
+++ drivers/net/ethernet/broadcom/bnxt/bnxt_devlink.c
@@ -1074,15 +1074,8 @@ static int __bnxt_hwrm_nvm_req(struct bnxt *bp,
 static int bnxt_hwrm_nvm_req(struct bnxt *bp, u32 param_id, void *msg,
 			     union devlink_param_value *val)
 {
-	struct hwrm_nvm_get_variable_input *req = msg;
 	const struct bnxt_dl_nvm_param *nvm_param;
 	int i;
-
-	/* Get/Set NVM CFG parameter is supported only on PFs */
-	if (BNXT_VF(bp)) {
-		hwrm_req_drop(bp, req);
-		return -EPERM;
-	}
 
 	for (i = 0; i < ARRAY_SIZE(nvm_params); i++) {
 		nvm_param = &nvm_params[i];
