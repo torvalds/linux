ac398f570724c41e5e039d54e4075519f6af7408
Guangshuo Li <202321181@mail.sdu.edu.cn>
2025-09-18 19:44:10 +0800

    LoongArch: vDSO: Check kcalloc() result in init_vdso()
    Add a NULL-pointer check after the kcalloc() call in init_vdso(). If
    allocation fails, return -ENOMEM to prevent a possible dereference of
    vdso_info.code_mapping.pages when it is NULL.
    
    Cc: stable@vger.kernel.org
    Fixes: 2ed119aef60d ("LoongArch: Set correct size for vDSO code mapping")
    Signed-off-by: Guangshuo Li <202321181@mail.sdu.edu.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- arch/loongarch/kernel/vdso.c
+++ arch/loongarch/kernel/vdso.c
@@ -54,6 +54,9 @@ static int __init init_vdso(void)
 	vdso_info.code_mapping.pages =
 		kcalloc(vdso_info.size / PAGE_SIZE, sizeof(struct page *), GFP_KERNEL);
 
+	if (!vdso_info.code_mapping.pages)
+		return -ENOMEM;
+
 	pfn = __phys_to_pfn(__pa_symbol(vdso_info.vdso));
 	for (i = 0; i < vdso_info.size / PAGE_SIZE; i++)
 		vdso_info.code_mapping.pages[i] = pfn_to_page(pfn + i);
