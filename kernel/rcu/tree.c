499d48f75b230522f4aa5aa4b9cc3c5b1594e1af
Marco Crivellari <marco.crivellari@suse.com>
2025-09-19 16:50:38 +0200

    rcu: WQ_PERCPU added to alloc_workqueue users
    Currently if a user enqueue a work item using schedule_delayed_work() the
    used wq is "system_wq" (per-cpu wq) while queue_delayed_work() use
    WORK_CPU_UNBOUND (used when a cpu is not specified). The same applies to
    schedule_work() that is using system_wq and queue_work(), that makes use
    again of WORK_CPU_UNBOUND.
    This lack of consistentcy cannot be addressed without refactoring the API.
    
    alloc_workqueue() treats all queues as per-CPU by default, while unbound
    workqueues must opt-in via WQ_UNBOUND.
    
    This default is suboptimal: most workloads benefit from unbound queues,
    allowing the scheduler to place worker threads where they’re needed and
    reducing noise when CPUs are isolated.
    
    This patch adds a new WQ_PERCPU flag to explicitly request the use of
    the per-CPU behavior. Both flags coexist for one release cycle to allow
    callers to transition their calls.
    
    Once migration is complete, WQ_UNBOUND can be removed and unbound will
    become the implicit default.
    
    With the introduction of the WQ_PERCPU flag (equivalent to !WQ_UNBOUND),
    any alloc_workqueue() caller that doesn’t explicitly specify WQ_UNBOUND
    must now use WQ_PERCPU.
    
    All existing users have been updated accordingly.
    
    Suggested-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Marco Crivellari <marco.crivellari@suse.com>
    Reviewed-by: Frederic Weisbecker <frederic@kernel.org>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -4890,7 +4890,7 @@ void __init rcu_init(void)
 	rcutree_online_cpu(cpu);
 
 	/* Create workqueue for Tree SRCU and for expedited GPs. */
-	rcu_gp_wq = alloc_workqueue("rcu_gp", WQ_MEM_RECLAIM, 0);
+	rcu_gp_wq = alloc_workqueue("rcu_gp", WQ_MEM_RECLAIM | WQ_PERCPU, 0);
 	WARN_ON(!rcu_gp_wq);
 
 	sync_wq = alloc_workqueue("sync_wq", WQ_MEM_RECLAIM, 0);
