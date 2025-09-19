32fdf8f418d74da3e22a2c1319285010192edc73
Ville Syrjälä <ville.syrjala@linux.intel.com>
2025-09-19 21:08:38 +0300

    drm/i915: s/intel_get_linetime_us()/skl_wm_linetime_us()/
    Rename intel_get_linetime_us() to skl_wm_linetime_us() to better
    reflect that it's not meant to be used for anything apart from
    the watermark calculations.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20250919180838.10498-4-ville.syrjala@linux.intel.com
    Reviewed-by: Vinod Govindapillai <vinod.govindapillai@intel.com>

--- drivers/gpu/drm/i915/display/skl_watermark.c
+++ drivers/gpu/drm/i915/display/skl_watermark.c
@@ -1636,9 +1636,8 @@ skl_wm_method2(u32 pixel_rate, u32 pipe_htotal, u32 latency,
 	return ret;
 }
 
-static int
-intel_get_linetime_us(const struct intel_crtc_state *crtc_state,
-		      int pixel_rate)
+static int skl_wm_linetime_us(const struct intel_crtc_state *crtc_state,
+			      int pixel_rate)
 {
 	return DIV_ROUND_UP(crtc_state->hw.pipe_mode.crtc_htotal * 1000,
 			    pixel_rate);
@@ -1729,7 +1728,7 @@ skl_compute_wm_params(const struct intel_crtc_state *crtc_state,
 	wp->y_tile_minimum = mul_u32_fixed16(wp->y_min_scanlines,
 					     wp->plane_blocks_per_line);
 
-	wp->linetime_us = intel_get_linetime_us(crtc_state, plane_pixel_rate);
+	wp->linetime_us = skl_wm_linetime_us(crtc_state, plane_pixel_rate);
 
 	return 0;
 }
