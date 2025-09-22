9ed94e0e66153a153b3da94db0f297081adf92f8
kittenzrulz123 <103671212+kittenzrulz123@users.noreply.github.com>
2025-09-21 21:20:44 -0400

    Removing bloat
    Audio is bloat

--- sound/soc/codecs/adau-utils.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef SOUND_SOC_CODECS_ADAU_PLL_H
-#define SOUND_SOC_CODECS_ADAU_PLL_H
-
-int adau_calc_pll_cfg(unsigned int freq_in, unsigned int freq_out,
-	uint8_t regs[5]);
-
-#endif
