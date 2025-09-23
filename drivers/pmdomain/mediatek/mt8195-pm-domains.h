5fc4ab3269dea6a0b00c7256cb6f6c0101b6a44b
Louis-Alexis Eyraud <louisalexis.eyraud@collabora.com>
2025-09-23 10:52:12 +0200

    pmdomain: mediatek: set default off flag for MT8195 AUDIO power domain
    In MT8195 power domain data array, set the KEEP_DEFAULT_OFF and
    ACTIVE_WAKEUP flags for the AUDIO power domain entry to avoid
    having this domain being on during boot sequence when unneeded.
    
    Fixes: 0e789b491ba0 ("pmdomain: core: Leave powered-on genpds on until sync_state")
    Fixes: 13a4b7fb6260 ("pmdomain: core: Leave powered-on genpds on until late_initcall_sync")
    Signed-off-by: Louis-Alexis Eyraud <louisalexis.eyraud@collabora.com>
    Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/pmdomain/mediatek/mt8195-pm-domains.h
+++ drivers/pmdomain/mediatek/mt8195-pm-domains.h
@@ -123,6 +123,7 @@ static const struct scpsys_domain_data scpsys_domain_data_mt8195[] = {
 				    MT8195_TOP_AXI_PROT_EN_2_CLR,
 				    MT8195_TOP_AXI_PROT_EN_2_STA1),
 		},
+		.caps = MTK_SCPD_KEEP_DEFAULT_OFF | MTK_SCPD_ACTIVE_WAKEUP,
 	},
 	[MT8195_POWER_DOMAIN_MFG0] = {
 		.name = "mfg0",
