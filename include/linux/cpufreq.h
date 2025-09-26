950c6451a5c38d375993c3b9da427e2e69b01c30
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2025-09-26 12:31:47 +0200

    cpufreq: Drop unused symbol CPUFREQ_ETERNAL
    Drop CPUFREQ_ETERNAL that has no users any more along with all
    references to it in the documentation.
    
    No functional impact.
    
    Reviewed-by: Mario Limonciello (AMD) <superm1@kernel.org>
    Reviewed-by: Jie Zhan <zhanjie9@hisilicon.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Qais Yousef <qyousef@layalina.io>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -26,12 +26,7 @@
  *********************************************************************/
 /*
  * Frequency values here are CPU kHz
- *
- * Maximum transition latency is in nanoseconds - if it's unknown,
- * CPUFREQ_ETERNAL shall be used.
  */
-
-#define CPUFREQ_ETERNAL			(-1)
 
 #define CPUFREQ_DEFAULT_TRANSITION_LATENCY_NS	NSEC_PER_MSEC
 
