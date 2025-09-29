bbb99668b5b1c2411de2683e89f018be60a75dff
Ian Rogers <irogers@google.com>
2025-09-29 12:07:52 -0700

    perf capstone: Move capstone functionality into its own file
    Capstone disassembly support was split between disasm.c and
    print_insn.c. Move support out of these files into capstone.[ch] and
    remove include capstone/capstone.h from those files. As disassembly
    routines can fail, make failure the only option without
    HAVE_LIBCAPSTONE_SUPPORT. For simplicity's sake, duplicate the
    read_symbol utility function.
    
    The intent with moving capstone support into a single file is that
    dynamic support, using dlopen for libcapstone, can be added in later
    patches. This can potentially always succeed or fail, so relying on
    ifdefs isn't sufficient. Using dlopen is a useful option to minimize
    the perf tools dependencies and potentially size.
    
    Signed-off-by: Ian Rogers <irogers@google.com>
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Alexandre Ghiti <alexghiti@rivosinc.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Athira Rajeev <atrajeev@linux.ibm.com>
    Cc: Bill Wendling <morbo@google.com>
    Cc: Charlie Jenkins <charlie@rivosinc.com>
    Cc: Collin Funk <collin.funk1@gmail.com>
    Cc: Dmitriy Vyukov <dvyukov@google.com>
    Cc: Dr. David Alan Gilbert <linux@treblig.org>
    Cc: Eric Biggers <ebiggers@kernel.org>
    Cc: Haibo Xu <haibo1.xu@intel.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: James Clark <james.clark@linaro.org>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Justin Stitt <justinstitt@google.com>
    Cc: Li Huafei <lihuafei1@huawei.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Nathan Chancellor <nathan@kernel.org>
    Cc: Nick Desaulniers <nick.desaulniers+lkml@gmail.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Song Liu <song@kernel.org>
    Cc: Stephen Brennan <stephen.s.brennan@oracle.com>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/builtin-script.c
+++ tools/perf/builtin-script.c
@@ -1225,7 +1225,6 @@ static int any_dump_insn(struct evsel *evsel __maybe_unused,
 			 u8 *inbuf, int inlen, int *lenp,
 			 FILE *fp)
 {
-#ifdef HAVE_LIBCAPSTONE_SUPPORT
 	if (PRINT_FIELD(BRSTACKDISASM)) {
 		int printed = fprintf_insn_asm(x->machine, x->thread, x->cpumode, x->is64bit,
 					       (uint8_t *)inbuf, inlen, ip, lenp,
@@ -1234,7 +1233,6 @@ static int any_dump_insn(struct evsel *evsel __maybe_unused,
 		if (printed > 0)
 			return printed;
 	}
-#endif
 	return fprintf(fp, "%s", dump_insn(x, ip, inbuf, inlen, lenp));
 }
 
