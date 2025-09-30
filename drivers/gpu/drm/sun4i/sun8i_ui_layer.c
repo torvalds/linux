9bb3ceb961bd0aa6eb9d2512b0242c9f63a1f59f
Maxime Ripard <mripard@kernel.org>
2025-09-30 12:59:43 +0200

    drm/sun4i: Switch to drm_atomic_get_new_crtc_state()
    The sun4i atomic_check implementation uses the deprecated
    drm_atomic_get_existing_crtc_state() helper.
    
    This hook is called as part of the global atomic_check, thus before the
    states are swapped. The existing state thus points to the new state, and
    we can use drm_atomic_get_new_crtc_state() instead.
    
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://lore.kernel.org/r/20250930-drm-no-more-existing-state-v5-28-eeb9e1287907@kernel.org
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- drivers/gpu/drm/sun4i/sun8i_ui_layer.c
+++ drivers/gpu/drm/sun4i/sun8i_ui_layer.c
@@ -206,8 +206,7 @@ static int sun8i_ui_layer_atomic_check(struct drm_plane *plane,
 	if (!crtc)
 		return 0;
 
-	crtc_state = drm_atomic_get_existing_crtc_state(state,
-							crtc);
+	crtc_state = drm_atomic_get_new_crtc_state(state, crtc);
 	if (WARN_ON(!crtc_state))
 		return -EINVAL;
 
