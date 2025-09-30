afea87890ddace6980fdff405f2ea4cacc5c9b22
Maxime Ripard <mripard@kernel.org>
2025-09-30 12:59:39 +0200

    drm/mediatek: Switch to drm_atomic_get_new_crtc_state()
    The mediatek atomic_check implementation uses the deprecated
    drm_atomic_get_existing_crtc_state() helper.
    
    This hook is called as part of the global atomic_check, thus before the
    states are swapped. The existing state thus points to the new state, and
    we can use drm_atomic_get_new_crtc_state() instead.
    
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://lore.kernel.org/r/20250930-drm-no-more-existing-state-v5-24-eeb9e1287907@kernel.org
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- drivers/gpu/drm/mediatek/mtk_plane.c
+++ drivers/gpu/drm/mediatek/mtk_plane.c
@@ -122,7 +122,8 @@ static int mtk_plane_atomic_async_check(struct drm_plane *plane,
 	if (ret)
 		return ret;
 
-	crtc_state = drm_atomic_get_existing_crtc_state(state, new_plane_state->crtc);
+	crtc_state = drm_atomic_get_new_crtc_state(state,
+						   new_plane_state->crtc);
 
 	return drm_atomic_helper_check_plane_state(plane->state, crtc_state,
 						   DRM_PLANE_NO_SCALING,
