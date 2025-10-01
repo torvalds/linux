814f5415d3e3084eeb0550acdee5eca8b4966055
Ard Biesheuvel <ardb@kernel.org>
2025-10-01 12:20:32 +0200

    ARM/simd: Add scoped guard API for kernel mode SIMD
    Implement the ksimd scoped guard API so that it can be used by code that
    supports both ARM and arm64.
    
    Reviewed-by: Kees Cook <kees@kernel.org>
    Reviewed-by: Eric Biggers <ebiggers@kernel.org>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- arch/arm/include/asm/simd.h
+++ arch/arm/include/asm/simd.h
@@ -2,14 +2,21 @@
 #ifndef _ASM_SIMD_H
 #define _ASM_SIMD_H
 
+#include <linux/cleanup.h>
 #include <linux/compiler_attributes.h>
 #include <linux/preempt.h>
 #include <linux/types.h>
+
+#include <asm/neon.h>
 
 static __must_check inline bool may_use_simd(void)
 {
 	return IS_ENABLED(CONFIG_KERNEL_MODE_NEON) && !in_hardirq()
 	       && !irqs_disabled();
 }
+
+DEFINE_LOCK_GUARD_0(ksimd, kernel_neon_begin(), kernel_neon_end())
+
+#define scoped_ksimd()	scoped_guard(ksimd)
 
 #endif	/* _ASM_SIMD_H */
