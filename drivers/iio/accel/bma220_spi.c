b4cecec7dc0b09b8e7260abd0d45ec58214dfa9a
Petre Rodan <petre.rodan@subdimension.ro>
2025-10-05 16:12:12 +0300

    iio: accel: bma220: cleanup license string
    Fix checkpatch warning about use of "GPL v2" license:
    
    Prefer "GPL" over "GPL v2" - see commit bf7fbeeae6db
    ("module: Cure the MODULE_LICENSE "GPL" vs. "GPL v2" bogosity")
    
    Signed-off-by: Petre Rodan <petre.rodan@subdimension.ro>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/accel/bma220_spi.c
+++ drivers/iio/accel/bma220_spi.c
@@ -332,4 +332,4 @@ module_spi_driver(bma220_driver);
 
 MODULE_AUTHOR("Tiberiu Breana <tiberiu.a.breana@intel.com>");
 MODULE_DESCRIPTION("BMA220 acceleration sensor driver");
-MODULE_LICENSE("GPL v2");
+MODULE_LICENSE("GPL");
