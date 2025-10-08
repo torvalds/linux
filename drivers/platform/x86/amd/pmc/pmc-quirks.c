f945afe01c6768dcfed7868c671a26e1164c2284
Antheas Kapenekakis <lkml@antheas.dev>
2025-10-08 15:50:57 +0200

    platform/x86/amd: pmc: Add Lenovo Legion Go 2 to pmc quirk list
    The Lenovo Legion Go 2 takes a long time to resume from suspend.
    This is due to it having an nvme resume handler that interferes
    with IOMMU mappings. It is a common issue with older Lenovo
    laptops. Adding it to that quirk list fixes this issue.
    
    Closes: https://gitlab.freedesktop.org/drm/amd/-/issues/4618
    Suggested-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Antheas Kapenekakis <lkml@antheas.dev>
    Reviewed-by: Mario Limonciello (AMD) <superm1@kernel.org>
    Link: https://patch.msgid.link/20251008135057.731928-1-lkml@antheas.dev
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>

--- drivers/platform/x86/amd/pmc/pmc-quirks.c
+++ drivers/platform/x86/amd/pmc/pmc-quirks.c
@@ -212,6 +212,23 @@ static const struct dmi_system_id fwbug_list[] = {
 			DMI_MATCH(DMI_PRODUCT_NAME, "82ND"),
 		}
 	},
+	/* https://gitlab.freedesktop.org/drm/amd/-/issues/4618 */
+	{
+		.ident = "Lenovo Legion Go 2",
+		.driver_data = &quirk_s2idle_bug,
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "LENOVO"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "83N0"),
+		}
+	},
+	{
+		.ident = "Lenovo Legion Go 2",
+		.driver_data = &quirk_s2idle_bug,
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "LENOVO"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "83N1"),
+		}
+	},
 	/* https://gitlab.freedesktop.org/drm/amd/-/issues/2684 */
 	{
 		.ident = "HP Laptop 15s-eq2xxx",
