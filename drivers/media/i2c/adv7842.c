ce5bf59e0eea1c14c918c44683cfae78c438b92d
Ricardo Ribalda <ribalda@chromium.org>
2025-10-13 14:14:44 +0000

    media: adv7842: Use %pe format specifier
    The %pe format specifier is designed to print error pointers. It prints
    a symbolic error name (eg. -EINVAL) and it makes the code simpler by
    omitting PTR_ERR()
    
    This patch fixes this cocci report:
    ./i2c/adv7842.c:3470:16-23: WARNING: Consider using %pe to print PTR_ERR()
    
    Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>
    Acked-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Hans Verkuil <hverkuil+cisco@kernel.org>

--- drivers/media/i2c/adv7842.c
+++ drivers/media/i2c/adv7842.c
@@ -3469,8 +3469,8 @@ static struct i2c_client *adv7842_dummy_client(struct v4l2_subdev *sd, const cha
 
 	cp = i2c_new_dummy_device(client->adapter, io_read(sd, io_reg) >> 1);
 	if (IS_ERR(cp)) {
-		v4l2_err(sd, "register %s on i2c addr 0x%x failed with %ld\n",
-			 desc, addr, PTR_ERR(cp));
+		v4l2_err(sd, "register %s on i2c addr 0x%x failed with %pe\n",
+			 desc, addr, cp);
 		cp = NULL;
 	}
 
