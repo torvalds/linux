9b041a4b66b3b62c30251e700b5688324cf66625
Nathan Chancellor <nathan@kernel.org>
2025-10-13 14:27:36 -0700

    x86/mm: Ensure clear_page() variants always have __kcfi_typeid_ symbols
    When building with CONFIG_CFI=y and CONFIG_LTO_CLANG_FULL=y, there is a series
    of errors from the various versions of clear_page() not having __kcfi_typeid_
    symbols.
    
      $ cat kernel/configs/repro.config
      CONFIG_CFI=y
      # CONFIG_LTO_NONE is not set
      CONFIG_LTO_CLANG_FULL=y
    
      $ make -skj"$(nproc)" ARCH=x86_64 LLVM=1 clean defconfig repro.config bzImage
      ld.lld: error: undefined symbol: __kcfi_typeid_clear_page_rep
      >>> referenced by ld-temp.o
      >>>               vmlinux.o:(__cfi_clear_page_rep)
    
      ld.lld: error: undefined symbol: __kcfi_typeid_clear_page_orig
      >>> referenced by ld-temp.o
      >>>               vmlinux.o:(__cfi_clear_page_orig)
    
      ld.lld: error: undefined symbol: __kcfi_typeid_clear_page_erms
      >>> referenced by ld-temp.o
      >>>               vmlinux.o:(__cfi_clear_page_erms)
    
    With full LTO, it is possible for LLVM to realize that these functions never
    have their address taken (as they are only used within an alternative, which
    will make them a direct call) across the whole kernel and either drop or skip
    generating their kCFI type identification symbols.
    
    clear_page_{rep,orig,erms}() are defined in clear_page_64.S with
    SYM_TYPED_FUNC_START as a result of
    
      2981557cb040 ("x86,kcfi: Fix EXPORT_SYMBOL vs kCFI"),
    
    as exported functions are free to be called indirectly thus need kCFI type
    identifiers.
    
    Use KCFI_REFERENCE with these clear_page() functions to force LLVM to see
    these functions as address-taken and generate then keep the kCFI type
    identifiers.
    
    Fixes: 2981557cb040 ("x86,kcfi: Fix EXPORT_SYMBOL vs kCFI")
    Closes: https://github.com/ClangBuiltLinux/linux/issues/2128
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Sami Tolvanen <samitolvanen@google.com>
    Link: https://patch.msgid.link/20251013-x86-fix-clear_page-cfi-full-lto-errors-v1-1-d69534c0be61@kernel.org

--- arch/x86/include/asm/page_64.h
+++ arch/x86/include/asm/page_64.h
@@ -43,6 +43,9 @@ extern unsigned long __phys_addr_symbol(unsigned long);
 void clear_page_orig(void *page);
 void clear_page_rep(void *page);
 void clear_page_erms(void *page);
+KCFI_REFERENCE(clear_page_orig);
+KCFI_REFERENCE(clear_page_rep);
+KCFI_REFERENCE(clear_page_erms);
 
 static inline void clear_page(void *page)
 {
