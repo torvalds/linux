a2e5a3cea4b18f6e2575acc444a5e8cce1fc8260
Zhang Yi <yi.zhang@huawei.com>
2025-10-13 09:51:17 +0800

    ext4: correct the checking of quota files before moving extents
    The move extent operation should return -EOPNOTSUPP if any of the inodes
    is a quota inode, rather than requiring both to be quota inodes.
    
    Fixes: 02749a4c2082 ("ext4: add ext4_is_quota_file()")
    Signed-off-by: Zhang Yi <yi.zhang@huawei.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Message-ID: <20251013015128.499308-2-yi.zhang@huaweicloud.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/move_extent.c
+++ fs/ext4/move_extent.c
@@ -485,7 +485,7 @@ mext_check_arguments(struct inode *orig_inode,
 		return -ETXTBSY;
 	}
 
-	if (ext4_is_quota_file(orig_inode) && ext4_is_quota_file(donor_inode)) {
+	if (ext4_is_quota_file(orig_inode) || ext4_is_quota_file(donor_inode)) {
 		ext4_debug("ext4 move extent: The argument files should not be quota files [ino:orig %lu, donor %lu]\n",
 			orig_inode->i_ino, donor_inode->i_ino);
 		return -EOPNOTSUPP;
