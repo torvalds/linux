41a6e1032c799184586d3c5ecb594cc05b844dc7
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2025-10-14 04:26:28 +0000

    ASoC: codecs: wm8960: use snd_kcontrol_chip() instead of snd_soc_kcontrol_component()
    We have very similar name functions (A)(B). Both gets component from
    snd_kcontrol, but (A) is used in callback functions which is registered
    through snd_soc_add_component_controls(), (B) is used through
    snd_soc_dapm_new_widgets().
    
    	(A) snd_soc_kcontrol_component()
    	(B) snd_soc_dapm_kcontrol_component()
    
    (B) is using very picky way to get component but using it is necessary in
    ASoC. But (A) is just wrapper function to snd_kcontrol_chip(), and directly
    using it without wrapper is very common way on ALSA.
    To reduce confusions of similar function, let's use common way on (A).
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://patch.msgid.link/87ecr6nm6j.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/wm8960.c
+++ sound/soc/codecs/wm8960.c
@@ -211,7 +211,7 @@ static int wm8960_set_deemph(struct snd_soc_component *component)
 static int wm8960_get_deemph(struct snd_kcontrol *kcontrol,
 			     struct snd_ctl_elem_value *ucontrol)
 {
-	struct snd_soc_component *component = snd_soc_kcontrol_component(kcontrol);
+	struct snd_soc_component *component = snd_kcontrol_chip(kcontrol);
 	struct wm8960_priv *wm8960 = snd_soc_component_get_drvdata(component);
 
 	ucontrol->value.integer.value[0] = wm8960->deemph;
@@ -221,7 +221,7 @@ static int wm8960_get_deemph(struct snd_kcontrol *kcontrol,
 static int wm8960_put_deemph(struct snd_kcontrol *kcontrol,
 			     struct snd_ctl_elem_value *ucontrol)
 {
-	struct snd_soc_component *component = snd_soc_kcontrol_component(kcontrol);
+	struct snd_soc_component *component = snd_kcontrol_chip(kcontrol);
 	struct wm8960_priv *wm8960 = snd_soc_component_get_drvdata(component);
 	unsigned int deemph = ucontrol->value.integer.value[0];
 
