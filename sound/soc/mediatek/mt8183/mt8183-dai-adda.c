b9b68f3a262bce63d4d363f08b345481e4f9d23b
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2025-10-14 04:27:33 +0000

    ASoC: mediatek: mt8183: use snd_kcontrol_chip() instead of snd_soc_kcontrol_component()
    We have very similar name functions (A)(B). Both gets component from
    snd_kcontrol, but (A) is used in callback functions which is registered
    through snd_soc_add_component_controls(), (B) is used through
    snd_soc_dapm_new_widgets().
    
    	(A) snd_soc_kcontrol_component()
    	(B) snd_soc_dapm_kcontrol_component()
    
    (B) is using very picky way to get component but using it is necessary in
    ASoC. But (A) is just wrapper function to snd_kcontrol_chip(), and directly
    using it without wrapper is very common way on ALSA.
    To reduce confusions of similar function, let's use common way on (A).
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://patch.msgid.link/87o6qam7kb.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/mediatek/mt8183/mt8183-dai-adda.c
+++ sound/soc/mediatek/mt8183/mt8183-dai-adda.c
@@ -113,7 +113,7 @@ static const struct soc_enum mt8183_adda_enum[] = {
 static int mt8183_adda_dmic_get(struct snd_kcontrol *kcontrol,
 				struct snd_ctl_elem_value *ucontrol)
 {
-	struct snd_soc_component *cmpnt = snd_soc_kcontrol_component(kcontrol);
+	struct snd_soc_component *cmpnt = snd_kcontrol_chip(kcontrol);
 	struct mtk_base_afe *afe = snd_soc_component_get_drvdata(cmpnt);
 	struct mt8183_afe_private *afe_priv = afe->platform_priv;
 
@@ -125,7 +125,7 @@ static int mt8183_adda_dmic_get(struct snd_kcontrol *kcontrol,
 static int mt8183_adda_dmic_set(struct snd_kcontrol *kcontrol,
 				struct snd_ctl_elem_value *ucontrol)
 {
-	struct snd_soc_component *cmpnt = snd_soc_kcontrol_component(kcontrol);
+	struct snd_soc_component *cmpnt = snd_kcontrol_chip(kcontrol);
 	struct mtk_base_afe *afe = snd_soc_component_get_drvdata(cmpnt);
 	struct mt8183_afe_private *afe_priv = afe->platform_priv;
 	struct soc_enum *e = (struct soc_enum *)kcontrol->private_value;
