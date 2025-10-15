ab4e41eb9fabd4607304fa7cfe8ec9c0bd8e1552
Maxim Levitsky <mlevitsk@redhat.com>
2025-10-14 23:32:58 -0400

    KVM: x86: Don't clear async #PF queue when CR0.PG is disabled (e.g. on #SMI)
    Fix an interaction between SMM and PV asynchronous #PFs where an #SMI can
    cause KVM to drop an async #PF ready event, and thus result in guest tasks
    becoming permanently stuck due to the task that encountered the #PF never
    being resumed.  Specifically, don't clear the completion queue when paging
    is disabled, and re-check for completed async #PFs if/when paging is
    enabled.
    
    Prior to commit 2635b5c4a0e4 ("KVM: x86: interrupt based APF 'page ready'
    event delivery"), flushing the APF queue without notifying the guest of
    completed APF requests when paging is disabled was "necessary", in that
    delivering a #PF to the guest when paging is disabled would likely confuse
    and/or crash the guest.  And presumably the original async #PF development
    assumed that a guest would only disable paging when there was no intent to
    ever re-enable paging.
    
    That assumption fails in several scenarios, most visibly on an emulated
    SMI, as entering SMM always disables CR0.PG (i.e. initially runs with
    paging disabled).  When the SMM handler eventually executes RSM, the
    interrupted paging-enabled is restored, and the async #PF event is lost.
    
    Similarly, invoking firmware, e.g. via EFI runtime calls, might require a
    transition through paging modes and thus also disable paging with valid
    entries in the competion queue.
    
    To avoid dropping completion events, drop the "clear" entirely, and handle
    paging-enable transitions in the same way KVM already handles APIC
    enable/disable events: if a vCPU's APIC is disabled, APF completion events
    are not kept pending and not injected while APIC is disabled.  Once a
    vCPU's APIC is re-enabled, KVM raises KVM_REQ_APF_READY so that the vCPU
    recognizes any pending pending #APF ready events.
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>
    Cc: stable@vger.kernel.org
    Link: https://patch.msgid.link/20251015033258.50974-4-mlevitsk@redhat.com
    [sean: rework changelog to call out #PF injection, drop "real mode"
           references, expand the code comment]
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -1045,6 +1045,13 @@ bool kvm_require_dr(struct kvm_vcpu *vcpu, int dr)
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_require_dr);
 
+static bool kvm_pv_async_pf_enabled(struct kvm_vcpu *vcpu)
+{
+	u64 mask = KVM_ASYNC_PF_ENABLED | KVM_ASYNC_PF_DELIVERY_AS_INT;
+
+	return (vcpu->arch.apf.msr_en_val & mask) == mask;
+}
+
 static inline u64 pdptr_rsvd_bits(struct kvm_vcpu *vcpu)
 {
 	return vcpu->arch.reserved_gpa_bits | rsvd_bits(5, 8) | rsvd_bits(1, 2);
@@ -1137,15 +1144,20 @@ void kvm_post_set_cr0(struct kvm_vcpu *vcpu, unsigned long old_cr0, unsigned lon
 	}
 
 	if ((cr0 ^ old_cr0) & X86_CR0_PG) {
-		kvm_clear_async_pf_completion_queue(vcpu);
-		kvm_async_pf_hash_reset(vcpu);
-
 		/*
 		 * Clearing CR0.PG is defined to flush the TLB from the guest's
 		 * perspective.
 		 */
 		if (!(cr0 & X86_CR0_PG))
 			kvm_make_request(KVM_REQ_TLB_FLUSH_GUEST, vcpu);
+		/*
+		 * Check for async #PF completion events when enabling paging,
+		 * as the vCPU may have previously encountered async #PFs (it's
+		 * entirely legal for the guest to toggle paging on/off without
+		 * waiting for the async #PF queue to drain).
+		 */
+		else if (kvm_pv_async_pf_enabled(vcpu))
+			kvm_make_request(KVM_REQ_APF_READY, vcpu);
 	}
 
 	if ((cr0 ^ old_cr0) & KVM_MMU_CR0_ROLE_BITS)
@@ -3648,13 +3660,6 @@ static int set_msr_mce(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 		return 1;
 	}
 	return 0;
-}
-
-static inline bool kvm_pv_async_pf_enabled(struct kvm_vcpu *vcpu)
-{
-	u64 mask = KVM_ASYNC_PF_ENABLED | KVM_ASYNC_PF_DELIVERY_AS_INT;
-
-	return (vcpu->arch.apf.msr_en_val & mask) == mask;
 }
 
 static int kvm_pv_enable_async_pf(struct kvm_vcpu *vcpu, u64 data)
