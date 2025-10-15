47955b58cf9b97fe4dc2b0d622b8ea3a2656bbf9
Borislav Petkov (AMD) <bp@alien8.de>
2025-10-15 12:35:48 +0200

    x86/cpufeatures: Correct LKGS feature flag description
    Quotation marks in cpufeatures.h comments are special and when the
    comment begins with a quoted string, that string lands in /proc/cpuinfo,
    turning it into a user-visible one.
    
    The LKGS comment doesn't begin with a quoted string but just in case
    drop the quoted "kernel" in there to avoid confusion. And while at it,
    simply change the description into what the LKGS instruction does for
    more clarity.
    
    No functional changes.
    
    Reviewed-by: Xin Li (Intel) <xin@zytor.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://lore.kernel.org/r/20251015103548.10194-1-bp@kernel.org

--- tools/arch/x86/include/asm/cpufeatures.h
+++ tools/arch/x86/include/asm/cpufeatures.h
@@ -320,7 +320,7 @@
 #define X86_FEATURE_FSRS		(12*32+11) /* Fast short REP STOSB */
 #define X86_FEATURE_FSRC		(12*32+12) /* Fast short REP {CMPSB,SCASB} */
 #define X86_FEATURE_FRED		(12*32+17) /* "fred" Flexible Return and Event Delivery */
-#define X86_FEATURE_LKGS		(12*32+18) /* Load "kernel" (userspace) GS */
+#define X86_FEATURE_LKGS		(12*32+18) /* Like MOV_GS except MSR_KERNEL_GS_BASE = GS.base */
 #define X86_FEATURE_WRMSRNS		(12*32+19) /* Non-serializing WRMSR */
 #define X86_FEATURE_AMX_FP16		(12*32+21) /* AMX fp16 Support */
 #define X86_FEATURE_AVX_IFMA            (12*32+23) /* Support for VPMADD52[H,L]UQ */
