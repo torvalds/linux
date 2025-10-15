b1fc226edf8680882a5bf89038bdc55afa2ac80d
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2025-10-15 10:53:45 +0300

    media: stm32: dma2d: Drop unneeded v4l2_m2m_get_vq() NULL check
    The v4l2_m2m_get_vq() function never returns NULL. The check was
    probably intended to catch invalid format types, but that's not needed
    as the V4L2 core picks the appropriate VIDIOC_G_FMT ioctl handler based
    on the format type, so the type can't be incorrect. Drop the unneeded
    return value check and, as the return value is not used for other
    purposes and the function has no side effect, the function call as well.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Hans Verkuil <hverkuil+cisco@kernel.org>

--- drivers/media/platform/st/stm32/dma2d/dma2d.c
+++ drivers/media/platform/st/stm32/dma2d/dma2d.c
@@ -355,12 +355,7 @@ static int vidioc_enum_fmt(struct file *file, void *priv, struct v4l2_fmtdesc *f
 static int vidioc_g_fmt(struct file *file, void *priv, struct v4l2_format *f)
 {
 	struct dma2d_ctx *ctx = file2ctx(file);
-	struct vb2_queue *vq;
 	struct dma2d_frame *frm;
-
-	vq = v4l2_m2m_get_vq(ctx->fh.m2m_ctx, f->type);
-	if (!vq)
-		return -EINVAL;
 
 	frm = get_frame(ctx, f->type);
 	f->fmt.pix.width		= frm->width;
