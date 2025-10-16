1d7cf255eefbb479d0eea9aa3b6372a1e52f8c62
Ian Rogers <irogers@google.com>
2025-10-16 13:51:25 -0700

    tools headers: Update the linux/unaligned.h copy with the kernel sources
    To pick up the changes in:
    
      vdso: Switch get/put_unaligned() from packed struct to memcpy
    
    As the code is dependent on __unqual_scalar_typeof, update also the tools
    version of compiler_types.h to include this.
    
    Signed-off-by: Ian Rogers <irogers@google.com>
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Link: https://patch.msgid.link/20251016205126.2882625-4-irogers@google.com

--- tools/include/linux/compiler_types.h
+++ tools/include/linux/compiler_types.h
@@ -40,4 +40,26 @@
 #define asm_goto_output(x...) asm goto(x)
 #endif
 
+/*
+ * __unqual_scalar_typeof(x) - Declare an unqualified scalar type, leaving
+ *			       non-scalar types unchanged.
+ */
+/*
+ * Prefer C11 _Generic for better compile-times and simpler code. Note: 'char'
+ * is not type-compatible with 'signed char', and we define a separate case.
+ */
+#define __scalar_type_to_expr_cases(type)				\
+		unsigned type:	(unsigned type)0,			\
+		signed type:	(signed type)0
+
+#define __unqual_scalar_typeof(x) typeof(				\
+		_Generic((x),						\
+			 char:	(char)0,				\
+			 __scalar_type_to_expr_cases(char),		\
+			 __scalar_type_to_expr_cases(short),		\
+			 __scalar_type_to_expr_cases(int),		\
+			 __scalar_type_to_expr_cases(long),		\
+			 __scalar_type_to_expr_cases(long long),	\
+			 default: (x)))
+
 #endif /* __LINUX_COMPILER_TYPES_H */
