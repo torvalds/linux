83be4bee57f0374ff751aaff3fef4af0af66ec81
Yazen Ghannam <yazen.ghannam@amd.com>
2025-10-17 13:26:28 +0000

    ACPI: PRM: Add acpi_prm_handler_available()
    Add a helper function to check if a PRM handler/module is present.
    
    This can be used during init time by code that depends on a particular
    handler. If the handler is not present, then the code does not need to
    be loaded.
    
    Signed-off-by: Yazen Ghannam <yazen.ghannam@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: "Mario Limonciello (AMD)" <superm1@kernel.org>
    Acked-by: "Rafael J. Wysocki (Intel)" <rafael@kernel.org>
    Link: https://patch.msgid.link/all/20251017-wip-atl-prm-v2-1-7ab1df4a5fbc@amd.com

--- include/linux/prmt.h
+++ include/linux/prmt.h
@@ -4,9 +4,11 @@
 
 #ifdef CONFIG_ACPI_PRMT
 void init_prmt(void);
+bool acpi_prm_handler_available(const guid_t *handler_guid);
 int acpi_call_prm_handler(guid_t handler_guid, void *param_buffer);
 #else
 static inline void init_prmt(void) { }
+static inline bool acpi_prm_handler_available(const guid_t *handler_guid) { return false; }
 static inline int acpi_call_prm_handler(guid_t handler_guid, void *param_buffer)
 {
 	return -EOPNOTSUPP;
