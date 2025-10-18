d9252f1be2f91c61e43e056ed1420ebdb68ca12f
Tamir Duberstein <tamird@gmail.com>
2025-10-18 15:16:23 -0400

    rust_binder: remove trailing comma
    This prepares for a later commit in which we introduce a custom
    formatting macro; that macro doesn't handle trailing commas so just
    remove this one.
    
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Signed-off-by: Tamir Duberstein <tamird@gmail.com>
    Link: https://patch.msgid.link/20251018-cstr-core-v18-2-9378a54385f8@gmail.com
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- drivers/android/binder/process.rs
+++ drivers/android/binder/process.rs
@@ -596,7 +596,7 @@ impl Process {
                     "  ref {}: desc {} {}node {debug_id} s {strong} w {weak}",
                     r.debug_id,
                     r.handle,
-                    if dead { "dead " } else { "" },
+                    if dead { "dead " } else { "" }
                 );
             }
         }
