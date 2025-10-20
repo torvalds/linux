5888533c6011de319c5f23ae147f1f291ce81582
Chen-Yu Tsai <wens@kernel.org>
2025-10-21 01:10:51 +0800

    clk: sunxi-ng: sun55i-a523-r-ccu: Mark bus-r-dma as critical
    The "bus-r-dma" clock in the A523's PRCM clock controller is also
    referred to as "DMA_CLKEN_SW" or "DMA ADB400 gating". It is unclear how
    this ties into the DMA controller MBUS clock gate; however if the clock
    is not enabled, the DMA controller in the MCU block will fail to access
    DRAM, even failing to retrieve the DMA descriptors.
    
    Mark this clock as critical. This sort of mirrors what is done for the
    main DMA controller's MBUS clock, which has a separate toggle that is
    currently left out of the main clock controller driver.
    
    Fixes: 8cea339cfb81 ("clk: sunxi-ng: add support for the A523/T527 PRCM CCU")
    Acked-by: Jernej Skrabec <jernej.skrabec@gmail.com>
    Link: https://patch.msgid.link/20251020171059.2786070-6-wens@kernel.org
    Signed-off-by: Chen-Yu Tsai <wens@kernel.org>

--- drivers/clk/sunxi-ng/ccu-sun55i-a523-r.c
+++ drivers/clk/sunxi-ng/ccu-sun55i-a523-r.c
@@ -121,7 +121,7 @@ static SUNXI_CCU_GATE_HW(bus_r_ir_rx_clk, "bus-r-ir-rx",
 			 &r_apb0_clk.common.hw, 0x1cc, BIT(0), 0);
 
 static SUNXI_CCU_GATE_HW(bus_r_dma_clk, "bus-r-dma",
-			 &r_apb0_clk.common.hw, 0x1dc, BIT(0), 0);
+			 &r_apb0_clk.common.hw, 0x1dc, BIT(0), CLK_IS_CRITICAL);
 static SUNXI_CCU_GATE_HW(bus_r_rtc_clk, "bus-r-rtc",
 			 &r_apb0_clk.common.hw, 0x20c, BIT(0), 0);
 static SUNXI_CCU_GATE_HW(bus_r_cpucfg_clk, "bus-r-cpucfg",
