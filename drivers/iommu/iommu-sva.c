72f98ef9a4be30d2a60136dd6faee376f780d06c
Lu Baolu <baolu.lu@linux.intel.com>
2025-10-22 16:26:27 +0800

    iommu: disable SVA when CONFIG_X86 is set
    Patch series "Fix stale IOTLB entries for kernel address space", v7.
    
    This proposes a fix for a security vulnerability related to IOMMU Shared
    Virtual Addressing (SVA).  In an SVA context, an IOMMU can cache kernel
    page table entries.  When a kernel page table page is freed and
    reallocated for another purpose, the IOMMU might still hold stale,
    incorrect entries.  This can be exploited to cause a use-after-free or
    write-after-free condition, potentially leading to privilege escalation or
    data corruption.
    
    This solution introduces a deferred freeing mechanism for kernel page
    table pages, which provides a safe window to notify the IOMMU to
    invalidate its caches before the page is reused.
    
    
    This patch (of 8):
    
    In the IOMMU Shared Virtual Addressing (SVA) context, the IOMMU hardware
    shares and walks the CPU's page tables.  The x86 architecture maps the
    kernel's virtual address space into the upper portion of every process's
    page table.  Consequently, in an SVA context, the IOMMU hardware can walk
    and cache kernel page table entries.
    
    The Linux kernel currently lacks a notification mechanism for kernel page
    table changes, specifically when page table pages are freed and reused. 
    The IOMMU driver is only notified of changes to user virtual address
    mappings.  This can cause the IOMMU's internal caches to retain stale
    entries for kernel VA.
    
    Use-After-Free (UAF) and Write-After-Free (WAF) conditions arise when
    kernel page table pages are freed and later reallocated.  The IOMMU could
    misinterpret the new data as valid page table entries.  The IOMMU might
    then walk into attacker-controlled memory, leading to arbitrary physical
    memory DMA access or privilege escalation.  This is also a
    Write-After-Free issue, as the IOMMU will potentially continue to write
    Accessed and Dirty bits to the freed memory while attempting to walk the
    stale page tables.
    
    Currently, SVA contexts are unprivileged and cannot access kernel
    mappings.  However, the IOMMU will still walk kernel-only page tables all
    the way down to the leaf entries, where it realizes the mapping is for the
    kernel and errors out.  This means the IOMMU still caches these
    intermediate page table entries, making the described vulnerability a real
    concern.
    
    Disable SVA on x86 architecture until the IOMMU can receive notification
    to flush the paging cache before freeing the CPU kernel page table pages.
    
    Link: https://lkml.kernel.org/r/20251022082635.2462433-1-baolu.lu@linux.intel.com
    Link: https://lkml.kernel.org/r/20251022082635.2462433-2-baolu.lu@linux.intel.com
    Fixes: 26b25a2b98e4 ("iommu: Bind process address spaces to devices")
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Suggested-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Cc: Alistair Popple <apopple@nvidia.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Borislav Betkov <bp@alien8.de>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Cc: Joerg Roedel <joro@8bytes.org>
    Cc: Kevin Tian <kevin.tian@intel.com>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Robin Murohy <robin.murphy@arm.com>
    Cc: Thomas Gleinxer <tglx@linutronix.de>
    Cc: "Uladzislau Rezki (Sony)" <urezki@gmail.com>
    Cc: Vasant Hegde <vasant.hegde@amd.com>
    Cc: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Will Deacon <will@kernel.org>
    Cc: Yi Lai <yi1.lai@intel.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/iommu/iommu-sva.c
+++ drivers/iommu/iommu-sva.c
@@ -77,6 +77,9 @@ struct iommu_sva *iommu_sva_bind_device(struct device *dev, struct mm_struct *mm
 	if (!group)
 		return ERR_PTR(-ENODEV);
 
+	if (IS_ENABLED(CONFIG_X86))
+		return ERR_PTR(-EOPNOTSUPP);
+
 	mutex_lock(&iommu_sva_lock);
 
 	/* Allocate mm->pasid if necessary. */
