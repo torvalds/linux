1ce120dcefc056ce8af2486cebbb77a458aad4c3
Bernd Schubert <bschubert@ddn.com>
2025-10-23 00:21:18 +0200

    fuse: Always flush the page cache before FOPEN_DIRECT_IO write
    This was done as condition on direct_io_allow_mmap, but I believe
    this is not right, as a file might be open two times - once with
    write-back enabled another time with FOPEN_DIRECT_IO.
    
    Signed-off-by: Bernd Schubert <bschubert@ddn.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1619,7 +1619,7 @@ ssize_t fuse_direct_io(struct fuse_io_priv *io, struct iov_iter *iter,
 	if (!ia)
 		return -ENOMEM;
 
-	if (fopen_direct_io && fc->direct_io_allow_mmap) {
+	if (fopen_direct_io) {
 		res = filemap_write_and_wait_range(mapping, pos, pos + count - 1);
 		if (res) {
 			fuse_io_free(ia);
