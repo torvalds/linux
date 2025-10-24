0a6428978df1c16971db0b0daae60b8fc0c4eb9b
Stefan Metzmacher <metze@samba.org>
2025-10-24 15:48:15 +0200

    smb: smbdirect: introduce smbdirect_all_c_files.c
    This is a very basic start in order to introduce
    common functions, which will be shared by client and server.
    
    As a start smbdirect_all_c_files.c will be included in
    fs/smb/client/smbdirect.c and fs/smb/server/transport_rdma.c
    in order to allow tiny steps in the direction of moving to
    a few exported functions from an smbdirect.ko.
    
    Step by step this will include individual c files
    with the real functions.
    
    Cc: Steve French <smfrench@gmail.com>
    Cc: Tom Talpey <tom@talpey.com>
    Cc: Long Li <longli@microsoft.com>
    Cc: Namjae Jeon <linkinjeon@kernel.org>
    Cc: linux-cifs@vger.kernel.org
    Cc: samba-technical@lists.samba.org
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Acked-by: Namjae Jeon <linkinjeon@kernel.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>

---
+++ fs/smb/common/smbdirect/smbdirect_all_c_files.c
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ *   Copyright (c) 2025, Stefan Metzmacher
+ */
+
+/*
+ * This is a temporary solution in order
+ * to include the common smbdirect functions
+ * into .c files in order to make a transformation
+ * in tiny bisectable steps possible.
+ *
+ * It will be replaced by a smbdirect.ko with
+ * exported public functions at the end.
+ */
+#ifndef SMBDIRECT_USE_INLINE_C_FILES
+#error SMBDIRECT_USE_INLINE_C_FILES define needed
+#endif
