7bb02a34c2ba69bbe0329fc3049c795f7ba66b9a
Sunil Khatri <sunil.khatri@amd.com>
2025-10-24 10:14:56 +0530

    drm/amdkfd: add missing return value check for range
    amdgpu_hmm_range_alloc could fails in case of low
    memory condition and hence we should have a check
    for the return value.
    
    Signed-off-by: Sunil Khatri <sunil.khatri@amd.com>
    Reviewed-by: Shirish S <shirish.s@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_svm.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_svm.c
@@ -1738,6 +1738,11 @@ static int svm_range_validate_and_map(struct mm_struct *mm,
 
 			WRITE_ONCE(p->svms.faulting_task, current);
 			range = amdgpu_hmm_range_alloc(NULL);
+			if (unlikely(!range)) {
+				r = -ENOMEM;
+				goto free_ctx;
+			}
+
 			r = amdgpu_hmm_range_get_pages(&prange->notifier, addr, npages,
 						       readonly, owner,
 						       range);
