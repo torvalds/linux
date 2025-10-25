ca459ca70f60ce05445845eca74c788b0d5ddb1b
Al Viro <viro@zeniv.linux.org.uk>
2025-10-25 18:34:49 -0400

    kill securityfs_recursive_remove()
    it's an unused alias for securityfs_remove()
    
    Acked-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/security.h
+++ include/linux/security.h
@@ -2258,8 +2258,6 @@ static inline void securityfs_remove(struct dentry *dentry)
 
 #endif
 
-#define securityfs_recursive_remove securityfs_remove
-
 #ifdef CONFIG_BPF_SYSCALL
 union bpf_attr;
 struct bpf_map;
