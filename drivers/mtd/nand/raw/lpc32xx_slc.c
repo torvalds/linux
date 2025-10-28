cdf44f1add4ec9ee80569d5a43e6e9bba0d74c7a
Haotian Zhang <vulab@iscas.ac.cn>
2025-10-28 17:47:47 +0800

    mtd: rawnand: lpc32xx_slc: fix GPIO descriptor leak on probe error and remove
    The driver calls gpiod_get_optional() in the probe function but
    never calls gpiod_put() in the remove function or in the probe
    error path. This leads to a GPIO descriptor resource leak.
    The lpc32xx_mlc.c driver in the same directory handles this
    correctly by calling gpiod_put() on both paths.
    
    Add gpiod_put() in the remove function and in the probe error path
    to fix the resource leak.
    
    Fixes: 6b923db2867c ("mtd: rawnand: lpc32xx_slc: switch to using gpiod API")
    Signed-off-by: Haotian Zhang <vulab@iscas.ac.cn>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- drivers/mtd/nand/raw/lpc32xx_slc.c
+++ drivers/mtd/nand/raw/lpc32xx_slc.c
@@ -937,6 +937,7 @@ release_dma:
 	dma_release_channel(host->dma_chan);
 enable_wp:
 	lpc32xx_wp_enable(host);
+	gpiod_put(host->wp_gpio);
 
 	return res;
 }
@@ -962,6 +963,7 @@ static void lpc32xx_nand_remove(struct platform_device *pdev)
 	writel(tmp, SLC_CTRL(host->io_base));
 
 	lpc32xx_wp_enable(host);
+	gpiod_put(host->wp_gpio);
 }
 
 static int lpc32xx_nand_resume(struct platform_device *pdev)
