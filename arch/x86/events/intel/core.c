5e4e355ae7cdeb0fef5dbe908866e1f895abfacc
Dapeng Mi <dapeng1.mi@linux.intel.com>
2025-10-29 18:21:28 +0800

    perf/x86/intel: Correct large PEBS flag check
    current large PEBS flag check only checks if sample_regs_user contains
    unsupported GPRs but doesn't check if sample_regs_intr contains
    unsupported GPRs.
    
    Of course, currently PEBS HW supports to sample all perf supported GPRs,
    the missed check doesn't cause real issue. But it won't be true any more
    after the subsequent patches support to sample SSP register. SSP
    sampling is not supported by adaptive PEBS HW and it would be supported
    until arch-PEBS HW. So correct this issue.
    
    Fixes: a47ba4d77e12 ("perf/x86: Enable free running PEBS for REGS_USER/INTR")
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://patch.msgid.link/20251029102136.61364-5-dapeng1.mi@linux.intel.com

--- arch/x86/events/intel/core.c
+++ arch/x86/events/intel/core.c
@@ -4029,7 +4029,9 @@ static unsigned long intel_pmu_large_pebs_flags(struct perf_event *event)
 	if (!event->attr.exclude_kernel)
 		flags &= ~PERF_SAMPLE_REGS_USER;
 	if (event->attr.sample_regs_user & ~PEBS_GP_REGS)
-		flags &= ~(PERF_SAMPLE_REGS_USER | PERF_SAMPLE_REGS_INTR);
+		flags &= ~PERF_SAMPLE_REGS_USER;
+	if (event->attr.sample_regs_intr & ~PEBS_GP_REGS)
+		flags &= ~PERF_SAMPLE_REGS_INTR;
 	return flags;
 }
 
