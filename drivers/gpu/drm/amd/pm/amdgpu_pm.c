37e3567dee273f68726373b342b38234bafe4cf9
Yang Wang <kevinyang.wang@amd.com>
2025-10-30 13:06:24 +0800

    drm/amd/pm: fix missing device_attr cleanup in amdgpu_pm_sysfs_init()
    Use the correct label to complete all cleanup work.
    
    Fixes: 4d154b1ca580 ("drm/amd/pm: Add support for DPM policies")
    Fixes: 25e82f2e2c59 ("drm/amd/pm: Add temperature metrics sysfs entry")
    Signed-off-by: Yang Wang <kevinyang.wang@amd.com>
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 4c4c138a1c86775c4d47e24f26357a1f8b64d0a3)

--- drivers/gpu/drm/amd/pm/amdgpu_pm.c
+++ drivers/gpu/drm/amd/pm/amdgpu_pm.c
@@ -4724,14 +4724,14 @@ int amdgpu_pm_sysfs_init(struct amdgpu_device *adev)
 		ret = devm_device_add_group(adev->dev,
 					    &amdgpu_pm_policy_attr_group);
 		if (ret)
-			goto err_out0;
+			goto err_out1;
 	}
 
 	if (amdgpu_dpm_is_temp_metrics_supported(adev, SMU_TEMP_METRIC_GPUBOARD)) {
 		ret = devm_device_add_group(adev->dev,
 					    &amdgpu_board_attr_group);
 		if (ret)
-			goto err_out0;
+			goto err_out1;
 		if (amdgpu_pm_get_sensor_generic(adev, AMDGPU_PP_SENSOR_MAXNODEPOWERLIMIT,
 						 (void *)&tmp) != -EOPNOTSUPP) {
 			sysfs_add_file_to_group(&adev->dev->kobj,
