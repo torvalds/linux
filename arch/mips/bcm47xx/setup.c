458639c42b7e6927a746bbbf0954ce3dd738c468
Heiner Kallweit <hkallweit1@gmail.com>
2025-10-30 22:45:30 +0100

    MIPS: BCM47XX: remove creating a fixed phy
    Now that b44 ethernet driver creates a fixed phy if needed, we can
    remove this here.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Link: https://patch.msgid.link/8983b705-6bca-4728-9283-7aa60f49340f@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- arch/mips/bcm47xx/setup.c
+++ arch/mips/bcm47xx/setup.c
@@ -256,12 +256,6 @@ static int __init bcm47xx_cpu_fixes(void)
 }
 arch_initcall(bcm47xx_cpu_fixes);
 
-static const struct fixed_phy_status bcm47xx_fixed_phy_status __initconst = {
-	.link	= 1,
-	.speed	= SPEED_100,
-	.duplex	= DUPLEX_FULL,
-};
-
 static int __init bcm47xx_register_bus_complete(void)
 {
 	switch (bcm47xx_bus_type) {
@@ -282,7 +276,6 @@ static int __init bcm47xx_register_bus_complete(void)
 	bcm47xx_leds_register();
 	bcm47xx_workarounds();
 
-	fixed_phy_add(&bcm47xx_fixed_phy_status);
 	return 0;
 }
 device_initcall(bcm47xx_register_bus_complete);
