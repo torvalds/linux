dc5742b6197f1cd41302b0cc4d66a830e2b4880a
Suraj Kandpal <suraj.kandpal@intel.com>
2025-11-01 08:54:56 +0530

    drm/i915/ltphy: Add LT Phy Programming recipe tables
    Add the LT Phy programming recipe tables for eDP, DP & HDMI and a
    function to use the correct table.
    
    Bspec: 74667
    Signed-off-by: Suraj Kandpal <suraj.kandpal@intel.com>
    Reviewed-by: Arun R Murthy <arun.r.murthy@intel.com>
    Link: https://patch.msgid.link/20251101032513.4171255-9-suraj.kandpal@intel.com

--- drivers/gpu/drm/i915/display/intel_lt_phy.h
+++ drivers/gpu/drm/i915/display/intel_lt_phy.h
@@ -13,5 +13,10 @@ struct intel_crtc_state;
 
 void intel_lt_phy_pll_enable(struct intel_encoder *encoder,
 			     const struct intel_crtc_state *crtc_state);
+int
+intel_lt_phy_pll_calc_state(struct intel_crtc_state *crtc_state,
+			    struct intel_encoder *encoder);
+
+#define HAS_LT_PHY(display) (DISPLAY_VER(display) >= 35)
 
 #endif /* __INTEL_LT_PHY_H__ */
