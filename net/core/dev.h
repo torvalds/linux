c07a491c1b735e0c27454ea5c27a446d43401b1e
David Wei <dw@davidwei.uk>
2025-10-31 19:24:48 -0700

    net: export netdev_get_by_index_lock()
    Need to call netdev_get_by_index_lock() from io_uring/zcrx.c, but it is
    currently private to net. Export the function in linux/netdevice.h.
    
    Signed-off-by: David Wei <dw@davidwei.uk>
    Acked-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- net/core/dev.h
+++ net/core/dev.h
@@ -29,7 +29,6 @@ struct napi_struct *
 netdev_napi_by_id_lock(struct net *net, unsigned int napi_id);
 struct net_device *dev_get_by_napi_id(unsigned int napi_id);
 
-struct net_device *netdev_get_by_index_lock(struct net *net, int ifindex);
 struct net_device *__netdev_put_lock(struct net_device *dev, struct net *net);
 struct net_device *
 netdev_xa_find_lock(struct net *net, struct net_device *dev,
