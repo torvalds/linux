2e1136acf8a8887c29f52e35a77b537309af321f
Haotian Zhang <vulab@iscas.ac.cn>
2025-11-03 15:30:18 +0800

    dmaengine: omap-dma: fix dma_pool resource leak in error paths
    The dma_pool created by dma_pool_create() is not destroyed when
    dma_async_device_register() or of_dma_controller_register() fails,
    causing a resource leak in the probe error paths.
    
    Add dma_pool_destroy() in both error paths to properly release the
    allocated dma_pool resource.
    
    Fixes: 7bedaa553760 ("dmaengine: add OMAP DMA engine driver")
    Signed-off-by: Haotian Zhang <vulab@iscas.ac.cn>
    Link: https://patch.msgid.link/20251103073018.643-1-vulab@iscas.ac.cn
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/ti/omap-dma.c
+++ drivers/dma/ti/omap-dma.c
@@ -1808,6 +1808,8 @@ static int omap_dma_probe(struct platform_device *pdev)
 	if (rc) {
 		pr_warn("OMAP-DMA: failed to register slave DMA engine device: %d\n",
 			rc);
+		if (od->ll123_supported)
+			dma_pool_destroy(od->desc_pool);
 		omap_dma_free(od);
 		return rc;
 	}
@@ -1823,6 +1825,8 @@ static int omap_dma_probe(struct platform_device *pdev)
 		if (rc) {
 			pr_warn("OMAP-DMA: failed to register DMA controller\n");
 			dma_async_device_unregister(&od->ddev);
+			if (od->ll123_supported)
+				dma_pool_destroy(od->desc_pool);
 			omap_dma_free(od);
 		}
 	}
