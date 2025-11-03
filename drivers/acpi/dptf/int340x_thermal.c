13a96342d59f4edbe237245e84c418c045b66188
Slawomir Rosek <srosek@google.com>
2025-11-03 16:25:15 +0000

    thermal: intel: Select INT340X_THERMAL from INTEL_SOC_DTS_THERMAL
    The IRQ used by the Intel SoC DTS thermal device for critical
    overheating notification is listed in _CRS of device INT3401 which
    therefore needs to be enumerated for Intel SoC DTS thermal to work.
    
    The enumeration happens by binding the int3401_thermal driver to the
    INT3401 platform device. Thus CONFIG_INT340X_THERMAL is in fact
    necessary for enumerating it, so checking CONFIG_INTEL_SOC_DTS_THERMAL
    in int340x_thermal_handler_attach() is pointless and INT340X_THERMAL
    may as well be selected by INTEL_SOC_DTS_THERMAL.
    
    Signed-off-by: Slawomir Rosek <srosek@google.com>
    [ rjw: New subject ]
    Link: https://patch.msgid.link/20251103162516.2606158-2-srosek@google.com
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/dptf/int340x_thermal.c
+++ drivers/acpi/dptf/int340x_thermal.c
@@ -11,10 +11,9 @@
 
 #include "../internal.h"
 
-#define INT3401_DEVICE 0X01
 static const struct acpi_device_id int340x_thermal_device_ids[] = {
 	{"INT3400"},
-	{"INT3401", INT3401_DEVICE},
+	{"INT3401"},
 	{"INT3402"},
 	{"INT3403"},
 	{"INT3404"},
@@ -75,10 +74,6 @@ static int int340x_thermal_handler_attach(struct acpi_device *adev,
 					const struct acpi_device_id *id)
 {
 	if (IS_ENABLED(CONFIG_INT340X_THERMAL))
-		acpi_create_platform_device(adev, NULL);
-	/* Intel SoC DTS thermal driver needs INT3401 to set IRQ descriptor */
-	else if (IS_ENABLED(CONFIG_INTEL_SOC_DTS_THERMAL) &&
-		 id->driver_data == INT3401_DEVICE)
 		acpi_create_platform_device(adev, NULL);
 	return 1;
 }
