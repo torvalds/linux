01fba45deaddcce0d0b01c411435d1acf6feab7b
Chao Yu <chao@kernel.org>
2025-11-05 14:50:22 +0800

    f2fs: fix return value of f2fs_recover_fsync_data()
    With below scripts, it will trigger panic in f2fs:
    
    mkfs.f2fs -f /dev/vdd
    mount /dev/vdd /mnt/f2fs
    touch /mnt/f2fs/foo
    sync
    echo 111 >> /mnt/f2fs/foo
    f2fs_io fsync /mnt/f2fs/foo
    f2fs_io shutdown 2 /mnt/f2fs
    umount /mnt/f2fs
    mount -o ro,norecovery /dev/vdd /mnt/f2fs
    or
    mount -o ro,disable_roll_forward /dev/vdd /mnt/f2fs
    
    F2FS-fs (vdd): f2fs_recover_fsync_data: recovery fsync data, check_only: 0
    F2FS-fs (vdd): Mounted with checkpoint version = 7f5c361f
    F2FS-fs (vdd): Stopped filesystem due to reason: 0
    F2FS-fs (vdd): f2fs_recover_fsync_data: recovery fsync data, check_only: 1
    Filesystem f2fs get_tree() didn't set fc->root, returned 1
    ------------[ cut here ]------------
    kernel BUG at fs/super.c:1761!
    Oops: invalid opcode: 0000 [#1] SMP PTI
    CPU: 3 UID: 0 PID: 722 Comm: mount Not tainted 6.18.0-rc2+ #721 PREEMPT(voluntary)
    Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS 1.16.3-debian-1.16.3-2 04/01/2014
    RIP: 0010:vfs_get_tree.cold+0x18/0x1a
    Call Trace:
     <TASK>
     fc_mount+0x13/0xa0
     path_mount+0x34e/0xc50
     __x64_sys_mount+0x121/0x150
     do_syscall_64+0x84/0x800
     entry_SYSCALL_64_after_hwframe+0x76/0x7e
    RIP: 0033:0x7fa6cc126cfe
    
    The root cause is we missed to handle error number returned from
    f2fs_recover_fsync_data() when mounting image w/ ro,norecovery or
    ro,disable_roll_forward mount option, result in returning a positive
    error number to vfs_get_tree(), fix it.
    
    Cc: stable@kernel.org
    Fixes: 6781eabba1bd ("f2fs: give -EINVAL for norecovery and rw mount")
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -5211,11 +5211,15 @@ try_onemore:
 		}
 	} else {
 		err = f2fs_recover_fsync_data(sbi, true);
-
-		if (!f2fs_readonly(sb) && err > 0) {
-			err = -EINVAL;
-			f2fs_err(sbi, "Need to recover fsync data");
-			goto free_meta;
+		if (err > 0) {
+			if (!f2fs_readonly(sb)) {
+				f2fs_err(sbi, "Need to recover fsync data");
+				err = -EINVAL;
+				goto free_meta;
+			} else {
+				f2fs_info(sbi, "drop all fsynced data");
+				err = 0;
+			}
 		}
 	}
 
