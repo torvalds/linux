7ab06ea41af53aa1713186ceaa154179e4b0d4c9
Yicong Yang <yangyccccc@gmail.com>
2025-11-05 18:38:49 +0800

    arch_topology: Provide a stub topology_core_has_smt() for !CONFIG_GENERIC_ARCH_TOPOLOGY
    The arm_pmu driver is using topology_core_has_smt() for retrieving
    the SMT implementation which depends on CONFIG_GENERIC_ARCH_TOPOLOGY.
    The config is optional on arm platforms so provide a
    !CONFIG_GENERIC_ARCH_TOPOLOGY stub for topology_core_has_smt().
    
    Fixes: c3d78c34ad00 ("perf: arm_pmuv3: Don't use PMCCNTR_EL0 on SMT cores")
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202511041757.vuCGOmFc-lkp@intel.com/
    Suggested-by: Will Deacon <will@kernel.org>
    Signed-off-by: Yicong Yang <yangyccccc@gmail.com>
    Reviewed-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Will Deacon <will@kernel.org>

--- include/linux/arch_topology.h
+++ include/linux/arch_topology.h
@@ -100,6 +100,10 @@ static inline bool topology_core_has_smt(int cpu)
 	return cpu_topology[cpu].thread_id != -1;
 }
 
-#endif
+#else
+
+static inline bool topology_core_has_smt(int cpu) { return false; }
+
+#endif /* CONFIG_GENERIC_ARCH_TOPOLOGY */
 
 #endif /* _LINUX_ARCH_TOPOLOGY_H_ */
