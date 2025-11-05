37cdb89c0a5940e41682e1f89a3ed88990a94887
Ahmad Rehman <Ahmad.Rehman@amd.com>
2025-11-05 09:48:08 -0500

    drm/amdkfd: Fixing the clang format
    This patch fixes the formatting in the patch
    "amdkfd: Do not wait for queue op response during reset"
    
    Signed-off-by: Ahmad Rehman <Ahmad.Rehman@amd.com>
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_device_queue_manager.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_device_queue_manager.c
@@ -2095,7 +2095,7 @@ int amdkfd_fence_wait_timeout(struct device_queue_manager *dqm,
 	while (*fence_addr != fence_value) {
 		/* Fatal err detected, this response won't come */
 		if (amdgpu_amdkfd_is_fed(dqm->dev->adev) ||
-				amdgpu_in_reset(dqm->dev->adev))
+		    amdgpu_in_reset(dqm->dev->adev))
 			return -EIO;
 
 		if (time_after(jiffies, end_jiffies)) {
