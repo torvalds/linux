98be155451eb2aa4b0413b85c3f95e239de3636f
Matthew Wilcox (Oracle) <willy@infradead.org>
2025-11-06 20:35:25 +0000

    mm: constify __dump_folio() arguments
    These arguments aren't modified by the function; mark them as const to
    help the compiler.
    
    Link: https://lkml.kernel.org/r/20251106203526.2368275-1-willy@infradead.org
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Oscar Salvador <osalvador@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/debug.c
+++ mm/debug.c
@@ -67,7 +67,7 @@ static const char *page_type_name(unsigned int page_type)
 	return page_type_names[i];
 }
 
-static void __dump_folio(struct folio *folio, struct page *page,
+static void __dump_folio(const struct folio *folio, const struct page *page,
 		unsigned long pfn, unsigned long idx)
 {
 	struct address_space *mapping = folio_mapping(folio);
