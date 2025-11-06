dbfe51513aae6bace00cc390e11cb486a64a63d2
Geert Uytterhoeven <geert+renesas@glider.be>
2025-11-06 14:33:49 +0100

    clk: at91: pmc: #undef field_{get,prep}() before definition
    Prepare for the advent of globally available common field_get() and
    field_prep() macros by undefining the symbols before defining local
    variants.  This prevents redefinition warnings from the C preprocessor
    when introducing the common macros later.
    
    Suggested-by: Yury Norov <yury.norov@gmail.com>
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Acked-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Acked-by: Stephen Boyd <sboyd@kernel.org>
    Acked-by: Claudiu Beznea <claudiu.beznea@tuxon.dev>
    Signed-off-by: Yury Norov (NVIDIA) <yury.norov@gmail.com>

--- drivers/clk/at91/pmc.h
+++ drivers/clk/at91/pmc.h
@@ -117,7 +117,9 @@ struct at91_clk_pms {
 	unsigned int parent;
 };
 
+#undef field_get
 #define field_get(_mask, _reg) (((_reg) & (_mask)) >> (ffs(_mask) - 1))
+#undef field_prep
 #define field_prep(_mask, _val) (((_val) << (ffs(_mask) - 1)) & (_mask))
 
 #define ndck(a, s) (a[s - 1].id + 1)
