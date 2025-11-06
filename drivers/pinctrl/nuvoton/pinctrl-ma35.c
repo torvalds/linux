bb0e7fda87753a973cb4a86c22905b1177f00d4e
Geert Uytterhoeven <geert+renesas@glider.be>
2025-11-06 14:34:08 +0100

    pinctrl: ma35: Convert to common field_{get,prep}() helpers
    Drop the driver-specific field_get() and field_prep() macros, in favor
    of the globally available variants from <linux/bitfield.h>.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Yury Norov (NVIDIA) <yury.norov@gmail.com>

--- drivers/pinctrl/nuvoton/pinctrl-ma35.c
+++ drivers/pinctrl/nuvoton/pinctrl-ma35.c
@@ -81,12 +81,6 @@
 #define MVOLT_1800			0
 #define MVOLT_3300			1
 
-/* Non-constant mask variant of FIELD_GET() and FIELD_PREP() */
-#undef field_get
-#define field_get(_mask, _reg)	(((_reg) & (_mask)) >> (ffs(_mask) - 1))
-#undef field_prep
-#define field_prep(_mask, _val)	(((_val) << (ffs(_mask) - 1)) & (_mask))
-
 static const char * const gpio_group_name[] = {
 	"gpioa", "gpiob", "gpioc", "gpiod", "gpioe", "gpiof", "gpiog",
 	"gpioh", "gpioi", "gpioj", "gpiok", "gpiol", "gpiom", "gpion",
