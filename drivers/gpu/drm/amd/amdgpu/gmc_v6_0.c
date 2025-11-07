3a4132e6cb4879764d886f11b9fd225197fb48dd
Timur Kristóf <timur.kristof@gmail.com>
2025-11-07 16:57:34 +0100

    drm/amdgpu/gmc6: Place gart at low address range
    Instead of using a best-fit algorithm to determine which part
    of the VMID 0 address space to use for GART, always use the low
    address range.
    
    A subsequent commit will use this to map the VCPU BO in GART
    for the VCE1 IP block.
    
    Split this into	a separate patch to make it easier to bisect,
    in case	there are any errors in	the future.
    
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gmc_v6_0.c
+++ drivers/gpu/drm/amd/amdgpu/gmc_v6_0.c
@@ -213,7 +213,7 @@ static void gmc_v6_0_vram_gtt_location(struct amdgpu_device *adev,
 
 	amdgpu_gmc_set_agp_default(adev, mc);
 	amdgpu_gmc_vram_location(adev, mc, base);
-	amdgpu_gmc_gart_location(adev, mc, AMDGPU_GART_PLACEMENT_BEST_FIT);
+	amdgpu_gmc_gart_location(adev, mc, AMDGPU_GART_PLACEMENT_LOW);
 }
 
 static void gmc_v6_0_mc_program(struct amdgpu_device *adev)
