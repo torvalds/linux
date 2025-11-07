aa4cd70b1b5e01b0344f8cd93a8e03798e88bc27
Ville Syrjälä <ville.syrjala@linux.intel.com>
2025-11-07 20:11:25 +0200

    drm/i915/cursor: Extract intel_cursor_mode_config_init()
    Move the max cursor size initialization into intel_cursor.c
    so that all the platform specific details about cursors are
    concentrated in one file.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patch.msgid.link/20251107181126.5743-9-ville.syrjala@linux.intel.com
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/display/intel_cursor.c
+++ drivers/gpu/drm/i915/display/intel_cursor.c
@@ -1092,3 +1092,20 @@ fail:
 
 	return ERR_PTR(ret);
 }
+
+void intel_cursor_mode_config_init(struct intel_display *display)
+{
+	struct drm_mode_config *mode_config = &display->drm->mode_config;
+
+	if (display->platform.i845g || display->platform.i865g) {
+		mode_config->cursor_width = display->platform.i845g ? 64 : 512;
+		mode_config->cursor_height = 1023;
+	} else if (display->platform.i830 || display->platform.i85x ||
+		   display->platform.i915g || display->platform.i915gm) {
+		mode_config->cursor_width = 64;
+		mode_config->cursor_height = 64;
+	} else {
+		mode_config->cursor_width = 256;
+		mode_config->cursor_height = 256;
+	}
+}
