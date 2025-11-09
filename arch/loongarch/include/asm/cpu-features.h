f28abb9f96e65a28d46885afd6b70cfc4d5df5a2
Huacai Chen <chenhuacai@loongson.cn>
2025-11-09 16:02:00 +0800

    LoongArch: Clarify 3 MSG interrupt features
    LoongArch's MSG interrupt features are used across multiple subsystems.
    Clarify these features to avoid misuse, existing users will be adjusted
    if necessary.
    
    MSGINT: Infrastructure, means the CPU core supports message interupts.
    Indicated by CPUCFG1.MSGINT.
    
    AVECINT: AVEC interrupt controller based on MSGINT, means the CPU chip
    supports direct message interrupts. Indicated by IOCSR.FEATURES.DMSI.
    
    REDIRECTINT: REDIRECT interrupt controller based on MSGINT and AVECINT,
    means the CPU chip supports redirect message interrupts. Indicated by
    IOCSR.FEATURES.RMSI.
    
    For example:
    Loongson-3A5000/3C5000 doesn't support MSGINT/AVECINT/REDIRECTINT;
    Loongson-3A6000 supports MSGINT but doesn't support AVECINT/REDIRECTINT;
    Loongson-3C6000 supports MSGINT/AVECINT/REDIRECTINT.
    
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- arch/loongarch/include/asm/cpu-features.h
+++ arch/loongarch/include/asm/cpu-features.h
@@ -67,6 +67,8 @@
 #define cpu_has_hypervisor	cpu_opt(LOONGARCH_CPU_HYPERVISOR)
 #define cpu_has_ptw		cpu_opt(LOONGARCH_CPU_PTW)
 #define cpu_has_lspw		cpu_opt(LOONGARCH_CPU_LSPW)
+#define cpu_has_msgint		cpu_opt(LOONGARCH_CPU_MSGINT)
 #define cpu_has_avecint		cpu_opt(LOONGARCH_CPU_AVECINT)
+#define cpu_has_redirectint	cpu_opt(LOONGARCH_CPU_REDIRECTINT)
 
 #endif /* __ASM_CPU_FEATURES_H */
