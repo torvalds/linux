a7ab96cf1366f2e35d05b1e773db2a4c1b5e4a45
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2025-11-11 00:23:37 +0000

    ASoC: codecs: max9850: convert to snd_soc_dapm_xxx()
    This patch converts below functions.
    
    dapm->dev					-> snd_soc_dapm_to_dev()
    dapm->card					-> snd_soc_dapm_to_card()
    dapm->component					-> snd_soc_dapm_to_component()
    
    dapm_kcontrol_get_value()			-> snd_soc_dapm_kcontrol_get_value()
    
    snd_soc_component_enable_pin()			-> snd_soc_dapm_enable_pin()
    snd_soc_component_enable_pin_unlocked()		-> snd_soc_dapm_enable_pin_unlocked()
    snd_soc_component_disable_pin()			-> snd_soc_dapm_disable_pin()
    snd_soc_component_disable_pin_unlocked()	-> snd_soc_dapm_disable_pin_unlocked()
    snd_soc_component_nc_pin()			-> snd_soc_dapm_nc_pin()
    snd_soc_component_nc_pin_unlocked()		-> snd_soc_dapm_nc_pin_unlocked()
    snd_soc_component_get_pin_status()		-> snd_soc_dapm_get_pin_status()
    snd_soc_component_force_enable_pin()		-> snd_soc_dapm_force_enable_pin()
    snd_soc_component_force_enable_pin_unlocked()	-> snd_soc_dapm_force_enable_pin_unlocked()
    snd_soc_component_force_bias_level()		-> snd_soc_dapm_force_bias_level()
    snd_soc_component_get_bias_level()		-> snd_soc_dapm_get_bias_level()
    snd_soc_component_init_bias_level()		-> snd_soc_dapm_init_bias_level()
    snd_soc_component_get_dapm()			-> snd_soc_component_to_dapm()
    
    snd_soc_dapm_kcontrol_component()		-> snd_soc_dapm_kcontrol_to_component()
    snd_soc_dapm_kcontrol_widget()			-> snd_soc_dapm_kcontrol_to_widget()
    snd_soc_dapm_kcontrol_dapm()			-> snd_soc_dapm_kcontrol_to_dapm()
    snd_soc_dapm_np_pin()				-> snd_soc_dapm_disable_pin()
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Reviewed-by: Charles Keepax <ckeepax@opensource.cirrus.com>
    Link: https://patch.msgid.link/87wm3x4dtj.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/max9850.c
+++ sound/soc/codecs/max9850.c
@@ -225,6 +225,7 @@ static int max9850_set_bias_level(struct snd_soc_component *component,
 				  enum snd_soc_bias_level level)
 {
 	struct max9850_priv *max9850 = snd_soc_component_get_drvdata(component);
+	struct snd_soc_dapm_context *dapm = snd_soc_component_to_dapm(component);
 	int ret;
 
 	switch (level) {
@@ -233,7 +234,7 @@ static int max9850_set_bias_level(struct snd_soc_component *component,
 	case SND_SOC_BIAS_PREPARE:
 		break;
 	case SND_SOC_BIAS_STANDBY:
-		if (snd_soc_component_get_bias_level(component) == SND_SOC_BIAS_OFF) {
+		if (snd_soc_dapm_get_bias_level(dapm) == SND_SOC_BIAS_OFF) {
 			ret = regcache_sync(max9850->regmap);
 			if (ret) {
 				dev_err(component->dev,
