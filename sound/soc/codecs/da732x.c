476fb171ed43ce544d54cd082ec56df2e8f8aa5d
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2025-11-11 00:22:02 +0000

    ASoC: codecs: da732x: convert to snd_soc_dapm_xxx()
    This patch converts below functions.
    
    dapm->dev					-> snd_soc_dapm_to_dev()
    dapm->card					-> snd_soc_dapm_to_card()
    dapm->component					-> snd_soc_dapm_to_component()
    
    dapm_kcontrol_get_value()			-> snd_soc_dapm_kcontrol_get_value()
    
    snd_soc_component_enable_pin()			-> snd_soc_dapm_enable_pin()
    snd_soc_component_enable_pin_unlocked()		-> snd_soc_dapm_enable_pin_unlocked()
    snd_soc_component_disable_pin()			-> snd_soc_dapm_disable_pin()
    snd_soc_component_disable_pin_unlocked()	-> snd_soc_dapm_disable_pin_unlocked()
    snd_soc_component_nc_pin()			-> snd_soc_dapm_nc_pin()
    snd_soc_component_nc_pin_unlocked()		-> snd_soc_dapm_nc_pin_unlocked()
    snd_soc_component_get_pin_status()		-> snd_soc_dapm_get_pin_status()
    snd_soc_component_force_enable_pin()		-> snd_soc_dapm_force_enable_pin()
    snd_soc_component_force_enable_pin_unlocked()	-> snd_soc_dapm_force_enable_pin_unlocked()
    snd_soc_component_force_bias_level()		-> snd_soc_dapm_force_bias_level()
    snd_soc_component_get_bias_level()		-> snd_soc_dapm_get_bias_level()
    snd_soc_component_init_bias_level()		-> snd_soc_dapm_init_bias_level()
    snd_soc_component_get_dapm()			-> snd_soc_component_to_dapm()
    
    snd_soc_dapm_kcontrol_component()		-> snd_soc_dapm_kcontrol_to_component()
    snd_soc_dapm_kcontrol_widget()			-> snd_soc_dapm_kcontrol_to_widget()
    snd_soc_dapm_kcontrol_dapm()			-> snd_soc_dapm_kcontrol_to_dapm()
    snd_soc_dapm_np_pin()				-> snd_soc_dapm_disable_pin()
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Reviewed-by: Charles Keepax <ckeepax@opensource.cirrus.com>
    Link: https://patch.msgid.link/87zf8t5sgm.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/da732x.c
+++ sound/soc/codecs/da732x.c
@@ -1408,6 +1408,7 @@ static int da732x_set_bias_level(struct snd_soc_component *component,
 				 enum snd_soc_bias_level level)
 {
 	struct da732x_priv *da732x = snd_soc_component_get_drvdata(component);
+	struct snd_soc_dapm_context *dapm = snd_soc_component_to_dapm(component);
 
 	switch (level) {
 	case SND_SOC_BIAS_ON:
@@ -1418,7 +1419,7 @@ static int da732x_set_bias_level(struct snd_soc_component *component,
 	case SND_SOC_BIAS_PREPARE:
 		break;
 	case SND_SOC_BIAS_STANDBY:
-		if (snd_soc_component_get_bias_level(component) == SND_SOC_BIAS_OFF) {
+		if (snd_soc_dapm_get_bias_level(dapm) == SND_SOC_BIAS_OFF) {
 			/* Init Codec */
 			snd_soc_component_write(component, DA732X_REG_REF1,
 				      DA732X_VMID_FASTCHG);
