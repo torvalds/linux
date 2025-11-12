aceccac58ad76305d147165788ea6b939bef179b
Mel Gorman <mgorman@techsingularity.net>
2025-11-12 12:25:20 +0000

    sched/fair: Enable scheduler feature NEXT_BUDDY
    The NEXT_BUDDY feature reinforces wakeup preemption to encourage the last
    wakee to be scheduled sooner on the assumption that the waker/wakee share
    cache-hot data. In CFS, it was paired with LAST_BUDDY to switch back on
    the assumption that the pair of tasks still share data but also relied
    on START_DEBIT and the exact WAKEUP_PREEMPTION implementation to get
    good results.
    
    NEXT_BUDDY has been disabled since commit 0ec9fab3d186 ("sched: Improve
    latencies and throughput") and LAST_BUDDY was removed in commit 5e963f2bd465
    ("sched/fair: Commit to EEVDF"). The reasoning is not clear but as vruntime
    spread is mentioned so the expectation is that NEXT_BUDDY had an impact
    on overall fairness. It was not noted why LAST_BUDDY was removed but it
    is assumed that it's very difficult to reason what LAST_BUDDY's correct
    and effective behaviour should be while still respecting EEVDFs goals.
    Peter Zijlstra noted during review;
    
    	I think I was just struggling to make sense of things and figured
    	less is more and axed it.
    
    	I have vague memories trying to work through the dynamics of
    	a wakeup-stack and the EEVDF latency requirements and getting
    	a head-ache.
    
    NEXT_BUDDY is easier to reason about given that it's a point-in-time
    decision on the wakees deadline and eligibilty relative to the waker. Enable
    NEXT_BUDDY as a preparation path to document that the decision to ignore
    the current implementation is deliberate. While not presented, the results
    were at best neutral and often much more variable.
    
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://patch.msgid.link/20251112122521.1331238-2-mgorman@techsingularity.net

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -29,7 +29,7 @@ SCHED_FEAT(PREEMPT_SHORT, true)
  * wakeup-preemption), since its likely going to consume data we
  * touched, increases cache locality.
  */
-SCHED_FEAT(NEXT_BUDDY, false)
+SCHED_FEAT(NEXT_BUDDY, true)
 
 /*
  * Allow completely ignoring cfs_rq->next; which can be set from various
