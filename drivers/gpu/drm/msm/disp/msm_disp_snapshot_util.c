9d2a48c3a7029532294d3c5d32a4791ea737aa0f
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2025-11-13 15:32:20 +0100

    drm/msm: Switch to use %ptSp
    Use %ptSp instead of open coded variants to print content of
    struct timespec64 in human readable format.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Acked-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Link: https://patch.msgid.link/20251113150217.3030010-7-andriy.shevchenko@linux.intel.com
    Signed-off-by: Petr Mladek <pmladek@suse.com>

--- drivers/gpu/drm/msm/disp/msm_disp_snapshot_util.c
+++ drivers/gpu/drm/msm/disp/msm_disp_snapshot_util.c
@@ -82,8 +82,7 @@ void msm_disp_state_print(struct msm_disp_state *state, struct drm_printer *p)
 	drm_printf(p, "kernel: " UTS_RELEASE "\n");
 	drm_printf(p, "module: " KBUILD_MODNAME "\n");
 	drm_printf(p, "dpu devcoredump\n");
-	drm_printf(p, "time: %lld.%09ld\n",
-		state->time.tv_sec, state->time.tv_nsec);
+	drm_printf(p, "time: %ptSp\n", &state->time);
 
 	list_for_each_entry_safe(block, tmp, &state->blocks, node) {
 		drm_printf(p, "====================%s================\n", block->name);
