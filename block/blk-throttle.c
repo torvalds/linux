f76581f9f1d29e32e120b0242974ba266e79de58
Guenter Roeck <linux@roeck-us.net>
2025-11-14 15:54:32 -0800

    block/blk-throttle: Fix throttle slice time for SSDs
    Commit d61fcfa4bb18 ("blk-throttle: choose a small throtl_slice for SSD")
    introduced device type specific throttle slices if BLK_DEV_THROTTLING_LOW
    was enabled. Commit bf20ab538c81 ("blk-throttle: remove
    CONFIG_BLK_DEV_THROTTLING_LOW") removed support for BLK_DEV_THROTTLING_LOW,
    but left the device type specific throttle slices in place. This
    effectively changed throttling behavior on systems with SSD which now use
    a different and non-configurable slice time compared to non-SSD devices.
    Practical impact is that throughput tests with low configured throttle
    values (65536 bps) experience less than expected throughput on SSDs,
    presumably due to rounding errors associated with the small throttle slice
    time used for those devices. The same tests pass when setting the throttle
    values to 65536 * 4 = 262144 bps.
    
    The original code sets the throttle slice time to DFL_THROTL_SLICE_HD if
    CONFIG_BLK_DEV_THROTTLING_LOW is disabled. Restore that code to fix the
    problem. With that, DFL_THROTL_SLICE_SSD is no longer necessary. Revert to
    the original code and re-introduce DFL_THROTL_SLICE to replace both
    DFL_THROTL_SLICE_HD and DFL_THROTL_SLICE_SSD. This effectively reverts
    commit d61fcfa4bb18 ("blk-throttle: choose a small throtl_slice for SSD").
    
    While at it, also remove MAX_THROTL_SLICE since it is not used anymore.
    
    Fixes: bf20ab538c81 ("blk-throttle: remove CONFIG_BLK_DEV_THROTTLING_LOW")
    Cc: Yu Kuai <yukuai@kernel.org>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Khazhismel Kumykov <khazhy@google.com>
    Reviewed-by: Yu Kuai <yukuai@fnnas.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-throttle.c
+++ block/blk-throttle.c
@@ -22,9 +22,7 @@
 #define THROTL_QUANTUM 32
 
 /* Throttling is performed over a slice and after that slice is renewed */
-#define DFL_THROTL_SLICE_HD (HZ / 10)
-#define DFL_THROTL_SLICE_SSD (HZ / 50)
-#define MAX_THROTL_SLICE (HZ)
+#define DFL_THROTL_SLICE (HZ / 10)
 
 /* A workqueue to queue throttle related work */
 static struct workqueue_struct *kthrotld_workqueue;
@@ -1341,10 +1339,7 @@ static int blk_throtl_init(struct gendisk *disk)
 		goto out;
 	}
 
-	if (blk_queue_nonrot(q))
-		td->throtl_slice = DFL_THROTL_SLICE_SSD;
-	else
-		td->throtl_slice = DFL_THROTL_SLICE_HD;
+	td->throtl_slice = DFL_THROTL_SLICE;
 	td->track_bio_latency = !queue_is_mq(q);
 	if (!td->track_bio_latency)
 		blk_stat_enable_accounting(q);
