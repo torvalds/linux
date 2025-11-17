dc3fdd4ade8763aac3546835909b70c298b8e8f4
Mika Kahola <mika.kahola@intel.com>
2025-11-17 12:45:52 +0200

    drm/i915/cx0: Add MTL+ .update_dpll_ref_clks hook
    Add .update_dpll_ref_clks function pointer to MTL+
    platforms to support dpll framework. Reuse ICL
    function pointer.
    
    Signed-off-by: Mika Kahola <mika.kahola@intel.com>
    Reviewed-by: Suraj Kandpal <suraj.kandpal@intel.com>
    Link: https://lore.kernel.org/r/20251117104602.2363671-23-mika.kahola@intel.com

--- drivers/gpu/drm/i915/display/intel_dpll_mgr.c
+++ drivers/gpu/drm/i915/display/intel_dpll_mgr.c
@@ -4450,6 +4450,7 @@ static const struct intel_dpll_mgr mtl_pll_mgr = {
 	.get_dplls = mtl_get_dplls,
 	.put_dplls = icl_put_dplls,
 	.update_active_dpll = icl_update_active_dpll,
+	.update_ref_clks = icl_update_dpll_ref_clks,
 };
 
 /**
