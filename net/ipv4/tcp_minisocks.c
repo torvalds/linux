932478bf9f6d3c7da9635c2fd0f87766c72ae683
Kuniyuki Iwashima <kuniyu@google.com>
2025-11-18 00:04:40 +0000

    tcp: Don't reinitialise tw->tw_transparent in tcp_time_wait().
    tw->tw_transparent is initialised twice in inet_twsk_alloc()
    and tcp_time_wait().
    
    Let's remove the latter.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@google.com>
    Reviewed-by: Jason Xing <kerneljasonxing@gmail.com>
    Link: https://patch.msgid.link/20251118000445.4091280-1-kuniyu@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/ipv4/tcp_minisocks.c
+++ net/ipv4/tcp_minisocks.c
@@ -337,7 +337,6 @@ void tcp_time_wait(struct sock *sk, int state, int timeo)
 		struct tcp_timewait_sock *tcptw = tcp_twsk((struct sock *)tw);
 		const int rto = (icsk->icsk_rto << 2) - (icsk->icsk_rto >> 1);
 
-		tw->tw_transparent	= inet_test_bit(TRANSPARENT, sk);
 		tw->tw_mark		= sk->sk_mark;
 		tw->tw_priority		= READ_ONCE(sk->sk_priority);
 		tw->tw_rcv_wscale	= tp->rx_opt.rcv_wscale;
