922a6f34c1756d2b0c35d9b2d915b8af19e85965
Ian Kent <raven@themaw.net>
2025-11-18 10:46:31 +0800

    autofs: dont trigger mount if it cant succeed
    If a mount namespace contains autofs mounts, and they are propagation
    private, and there is no namespace specific automount daemon to handle
    possible automounting then attempted path resolution will loop until
    MAXSYMLINKS is reached before failing causing quite a bit of noise in
    the log.
    
    Add a check for this in autofs ->d_automount() so that the VFS can
    immediately return an error in this case. Since the mount is propagation
    private an EPERM return seems most appropriate.
    
    Suggested by: Christian Brauner <brauner@kernel.org>
    
    Signed-off-by: Ian Kent <raven@themaw.net>
    Link: https://patch.msgid.link/20251118024631.10854-2-raven@themaw.net
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- fs/autofs/inode.c
+++ fs/autofs/inode.c
@@ -251,6 +251,7 @@ static struct autofs_sb_info *autofs_alloc_sbi(void)
 	sbi->min_proto = AUTOFS_MIN_PROTO_VERSION;
 	sbi->max_proto = AUTOFS_MAX_PROTO_VERSION;
 	sbi->pipefd = -1;
+	sbi->mnt_ns_id = to_ns_common(current->nsproxy->mnt_ns)->ns_id;
 
 	set_autofs_type_indirect(&sbi->type);
 	mutex_init(&sbi->wq_mutex);
