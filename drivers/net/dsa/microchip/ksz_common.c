0f80e21bf6229637e193248fbd284c0ec44bc0fd
Bastien Curutchet (Schneider Electric) <bastien.curutchet@bootlin.com>
2025-11-20 10:12:03 +0100

    net: dsa: microchip: Free previously initialized ports on init failures
    If a port interrupt setup fails after at least one port has already been
    successfully initialized, the gotos miss some resource releasing:
    - the already initialized PTP IRQs aren't released
    - the already initialized port IRQs aren't released if the failure
    occurs in ksz_pirq_setup().
    
    Merge 'out_girq' and 'out_ptpirq' into a single 'port_release' label.
    Behind this label, use the reverse loop to release all IRQ resources
    for all initialized ports.
    Jump in the middle of the reverse loop if an error occurs in
    ksz_ptp_irq_setup() to only release the port IRQ of the current
    iteration.
    
    Cc: stable@vger.kernel.org
    Fixes: c9cd961c0d43 ("net: dsa: microchip: lan937x: add interrupt support for port phy link")
    Signed-off-by: Bastien Curutchet (Schneider Electric) <bastien.curutchet@bootlin.com>
    Link: https://patch.msgid.link/20251120-ksz-fix-v6-4-891f80ae7f8f@bootlin.com
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- drivers/net/dsa/microchip/ksz_common.c
+++ drivers/net/dsa/microchip/ksz_common.c
@@ -3038,12 +3038,12 @@ static int ksz_setup(struct dsa_switch *ds)
 		dsa_switch_for_each_user_port(dp, dev->ds) {
 			ret = ksz_pirq_setup(dev, dp->index);
 			if (ret)
-				goto out_girq;
+				goto port_release;
 
 			if (dev->info->ptp_capable) {
 				ret = ksz_ptp_irq_setup(ds, dp->index);
 				if (ret)
-					goto out_pirq;
+					goto pirq_release;
 			}
 		}
 	}
@@ -3053,7 +3053,7 @@ static int ksz_setup(struct dsa_switch *ds)
 		if (ret) {
 			dev_err(dev->dev, "Failed to register PTP clock: %d\n",
 				ret);
-			goto out_ptpirq;
+			goto port_release;
 		}
 	}
 
@@ -3076,17 +3076,16 @@ static int ksz_setup(struct dsa_switch *ds)
 out_ptp_clock_unregister:
 	if (dev->info->ptp_capable)
 		ksz_ptp_clock_unregister(ds);
-out_ptpirq:
-	if (dev->irq > 0 && dev->info->ptp_capable)
-		dsa_switch_for_each_user_port(dp, dev->ds)
-			ksz_ptp_irq_free(ds, dp->index);
-out_pirq:
-	if (dev->irq > 0)
-		dsa_switch_for_each_user_port_continue_reverse(dp, dev->ds)
+port_release:
+	if (dev->irq > 0) {
+		dsa_switch_for_each_user_port_continue_reverse(dp, dev->ds) {
+			if (dev->info->ptp_capable)
+				ksz_ptp_irq_free(ds, dp->index);
+pirq_release:
 			ksz_irq_free(&dev->ports[dp->index].pirq);
-out_girq:
-	if (dev->irq > 0)
+		}
 		ksz_irq_free(&dev->girq);
+	}
 
 	return ret;
 }
