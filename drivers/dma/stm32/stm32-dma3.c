0d41ed4ea496fabbb4dc21171e32d9a924c2a661
Amelie Delaunay <amelie.delaunay@foss.st.com>
2025-11-21 14:36:56 +0100

    dmaengine: stm32-dma3: use module_platform_driver
    Without module_platform_driver(), stm32-dma3 doesn't have a
    module_exit procedure. Once stm32-dma3 module is inserted, it
    can't be removed, marked busy.
    Use module_platform_driver() instead of subsys_initcall() to register
    (insmod) and unregister (rmmod) stm32-dma3 driver.
    
    Reviewed-by: Eugen Hristev <eugen.hristev@linaro.org>
    Signed-off-by: Amelie Delaunay <amelie.delaunay@foss.st.com>
    Link: https://patch.msgid.link/20251121-dma3_improv-v2-1-76a207b13ea6@foss.st.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/stm32/stm32-dma3.c
+++ drivers/dma/stm32/stm32-dma3.c
@@ -1914,12 +1914,7 @@ static struct platform_driver stm32_dma3_driver = {
 	},
 };
 
-static int __init stm32_dma3_init(void)
-{
-	return platform_driver_register(&stm32_dma3_driver);
-}
-
-subsys_initcall(stm32_dma3_init);
+module_platform_driver(stm32_dma3_driver);
 
 MODULE_DESCRIPTION("STM32 DMA3 controller driver");
 MODULE_AUTHOR("Amelie Delaunay <amelie.delaunay@foss.st.com>");
