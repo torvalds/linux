87417cc95b0f1096cc27011ec750d9f988a63e83
Jesung Yang <y.j3ms.n@gmail.com>
2025-11-25 09:32:59 +0000

    scripts: generate_rust_analyzer: syn: treat `std` as a dependency
    Fix the `generate_rust_analyzer.py` script to ensure that the
    `rust-project.json` it produces includes `std` in the `deps` field for
    the `syn` crate.
    
    `syn` directly references items from `std`, so rust-analyzer should
    treat it as a dependency to provide correct IDE support.
    
    For example, `syn::Punctuated` contains fields of type `Vec<..>` and
    `Option<..>`, both of which come from the standard library prelude.
    With `std` listed in the `deps` field, rust-analyzer can infer the types
    of these fields instead of showing `{unknown}`.
    
    Verified the explicit uses of `std` using:
    
        grep -rn 'std::' rust/syn/
    
    Fixes: 737401751ace ("rust: syn: enable support in kbuild")
    Signed-off-by: Jesung Yang <y.j3ms.n@gmail.com>
    Reviewed-by: Tamir Duberstein <tamird@gmail.com>
    Tested-by: Tamir Duberstein <tamird@gmail.com>
    Link: https://patch.msgid.link/6dbdf6e1c1639ae381ca9ab7041f84728ffa2267.1764062688.git.y.j3ms.n@gmail.com
    [ Reworded title. - Miguel ]
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- scripts/generate_rust_analyzer.py
+++ scripts/generate_rust_analyzer.py
@@ -103,7 +103,7 @@ def generate_crates(srctree, objtree, sysroot_src, external_src, cfgs, core_edit
     append_crate(
         "syn",
         srctree / "rust" / "syn" / "lib.rs",
-        ["proc_macro", "proc_macro2", "quote"],
+        ["std", "proc_macro", "proc_macro2", "quote"],
         cfg=crates_cfgs["syn"],
     )
 
