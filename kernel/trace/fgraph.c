1650a1b6cb1ae6cb99bb4fce21b30ebdf9fc238e
Shengming Hu <hu.shengming@zte.com.cn>
2025-11-26 17:33:31 +0800

    fgraph: Check ftrace_pids_enabled on registration for early filtering
    When registering ftrace_graph, check if ftrace_pids_enabled is active.
    If enabled, assign entryfunc to fgraph_pid_func to ensure filtering
    is performed before executing the saved original entry function.
    
    Cc: stable@vger.kernel.org
    Cc: <wang.yaxin@zte.com.cn>
    Cc: <mhiramat@kernel.org>
    Cc: <mark.rutland@arm.com>
    Cc: <mathieu.desnoyers@efficios.com>
    Cc: <zhang.run@zte.com.cn>
    Cc: <yang.yang29@zte.com.cn>
    Link: https://patch.msgid.link/20251126173331679XGVF98NLhyLJRdtNkVZ6w@zte.com.cn
    Fixes: df3ec5da6a1e7 ("function_graph: Add pid tracing back to function graph tracer")
    Signed-off-by: Shengming Hu <hu.shengming@zte.com.cn>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- kernel/trace/fgraph.c
+++ kernel/trace/fgraph.c
@@ -1377,6 +1377,13 @@ int register_ftrace_graph(struct fgraph_ops *gops)
 
 	ftrace_graph_active++;
 
+	/* Always save the function, and reset at unregistering */
+	gops->saved_func = gops->entryfunc;
+#ifdef CONFIG_DYNAMIC_FTRACE
+	if (ftrace_pids_enabled(&gops->ops))
+		gops->entryfunc = fgraph_pid_func;
+#endif
+
 	if (ftrace_graph_active == 2)
 		ftrace_graph_disable_direct(true);
 
@@ -1396,8 +1403,6 @@ int register_ftrace_graph(struct fgraph_ops *gops)
 	} else {
 		init_task_vars(gops->idx);
 	}
-	/* Always save the function, and reset at unregistering */
-	gops->saved_func = gops->entryfunc;
 
 	gops->ops.flags |= FTRACE_OPS_FL_GRAPH;
 
