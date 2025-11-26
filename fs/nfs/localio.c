3af870aedbff10bfed220e280b57a405e972229f
Mike Snitzer <snitzer@kernel.org>
2025-11-26 01:01:25 -0500

    nfs/localio: fix regression due to out-of-order __put_cred
    Commit f2060bdc21d7 ("nfs/localio: add refcounting for each iocb IO
    associated with NFS pgio header") inadvertantly reintroduced the same
    potential for __put_cred() triggering BUG_ON(cred == current->cred) that
    commit 992203a1fba5 ("nfs/localio: restore creds before releasing pageio
    data") fixed.
    
    Fix this by saving and restoring the cred around each {read,write}_iter
    call within the respective for loop of nfs_local_call_{read,write} using
    scoped_with_creds().
    
    NOTE: this fix started by first reverting the following commits:
    
     94afb627dfc2 ("nfs: use credential guards in nfs_local_call_read()")
     bff3c841f7bd ("nfs: use credential guards in nfs_local_call_write()")
     1d18101a644e ("Merge tag 'kernel-6.19-rc1.cred' of git://git.kernel.org/pub/scm/linux/kernel/git/vfs/vfs")
    
    followed by narrowly fixing the cred lifetime issue by using
    scoped_with_creds().  In doing so, this commit's changes appear more
    extensive than they really are (as evidenced by comparing to v6.18's
    fs/nfs/localio.c).
    
    Reported-by: Zorro Lang <zlang@redhat.com>
    Signed-off-by: Mike Snitzer <snitzer@kernel.org>
    Acked-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    Reviewed-by: Christian Brauner <brauner@kernel.org>
    Link: https://lore.kernel.org/linux-next/20251205111942.4150b06f@canb.auug.org.au/
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/nfs/localio.c
+++ fs/nfs/localio.c
@@ -615,8 +615,11 @@ static void nfs_local_read_aio_complete(struct kiocb *kiocb, long ret)
 	nfs_local_pgio_aio_complete(iocb); /* Calls nfs_local_read_aio_complete_work */
 }
 
-static void do_nfs_local_call_read(struct nfs_local_kiocb *iocb, struct file *filp)
+static void nfs_local_call_read(struct work_struct *work)
 {
+	struct nfs_local_kiocb *iocb =
+		container_of(work, struct nfs_local_kiocb, work);
+	struct file *filp = iocb->kiocb.ki_filp;
 	bool force_done = false;
 	ssize_t status;
 	int n_iters;
@@ -633,7 +636,9 @@ static void do_nfs_local_call_read(struct nfs_local_kiocb *iocb, struct file *fi
 		} else
 			iocb->kiocb.ki_flags &= ~IOCB_DIRECT;
 
-		status = filp->f_op->read_iter(&iocb->kiocb, &iocb->iters[i]);
+		scoped_with_creds(filp->f_cred)
+			status = filp->f_op->read_iter(&iocb->kiocb, &iocb->iters[i]);
+
 		if (status != -EIOCBQUEUED) {
 			if (unlikely(status >= 0 && status < iocb->iters[i].count))
 				force_done = true; /* Partial read */
@@ -643,16 +648,6 @@ static void do_nfs_local_call_read(struct nfs_local_kiocb *iocb, struct file *fi
 			}
 		}
 	}
-}
-
-static void nfs_local_call_read(struct work_struct *work)
-{
-	struct nfs_local_kiocb *iocb =
-		container_of(work, struct nfs_local_kiocb, work);
-	struct file *filp = iocb->kiocb.ki_filp;
-
-	scoped_with_creds(filp->f_cred)
-		do_nfs_local_call_read(iocb, filp);
 }
 
 static int
@@ -822,12 +817,17 @@ static void nfs_local_write_aio_complete(struct kiocb *kiocb, long ret)
 	nfs_local_pgio_aio_complete(iocb); /* Calls nfs_local_write_aio_complete_work */
 }
 
-static ssize_t do_nfs_local_call_write(struct nfs_local_kiocb *iocb,
-				       struct file *filp)
+static void nfs_local_call_write(struct work_struct *work)
 {
+	struct nfs_local_kiocb *iocb =
+		container_of(work, struct nfs_local_kiocb, work);
+	struct file *filp = iocb->kiocb.ki_filp;
+	unsigned long old_flags = current->flags;
 	bool force_done = false;
 	ssize_t status;
 	int n_iters;
+
+	current->flags |= PF_LOCAL_THROTTLE | PF_MEMALLOC_NOIO;
 
 	file_start_write(filp);
 	n_iters = atomic_read(&iocb->n_iters);
@@ -842,7 +842,9 @@ static ssize_t do_nfs_local_call_write(struct nfs_local_kiocb *iocb,
 		} else
 			iocb->kiocb.ki_flags &= ~IOCB_DIRECT;
 
-		status = filp->f_op->write_iter(&iocb->kiocb, &iocb->iters[i]);
+		scoped_with_creds(filp->f_cred)
+			status = filp->f_op->write_iter(&iocb->kiocb, &iocb->iters[i]);
+
 		if (status != -EIOCBQUEUED) {
 			if (unlikely(status >= 0 && status < iocb->iters[i].count))
 				force_done = true; /* Partial write */
@@ -853,22 +855,6 @@ static ssize_t do_nfs_local_call_write(struct nfs_local_kiocb *iocb,
 		}
 	}
 	file_end_write(filp);
-
-	return status;
-}
-
-static void nfs_local_call_write(struct work_struct *work)
-{
-	struct nfs_local_kiocb *iocb =
-		container_of(work, struct nfs_local_kiocb, work);
-	struct file *filp = iocb->kiocb.ki_filp;
-	unsigned long old_flags = current->flags;
-	ssize_t status;
-
-	current->flags |= PF_LOCAL_THROTTLE | PF_MEMALLOC_NOIO;
-
-	scoped_with_creds(filp->f_cred)
-		status = do_nfs_local_call_write(iocb, filp);
 
 	current->flags = old_flags;
 }
