73b97d46dde64fa184d47865d4a532d818c3a007
Shengjiu Wang <shengjiu.wang@nxp.com>
2025-11-26 14:45:09 +0800

    ASoC: fsl_xcvr: clear the channel status control memory
    memset_io() writes memory byte by byte with __raw_writeb() on the arm
    platform if the size is word. but XCVR data RAM memory can't be accessed
    with byte address, so with memset_io() the channel status control memory
    is not really cleared, use writel_relaxed() instead.
    
    Fixes: 28564486866f ("ASoC: fsl_xcvr: Add XCVR ASoC CPU DAI driver")
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>
    Link: https://patch.msgid.link/20251126064509.1900974-1-shengjiu.wang@nxp.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/fsl/fsl_xcvr.c
+++ sound/soc/fsl/fsl_xcvr.c
@@ -1421,7 +1421,7 @@ static irqreturn_t irq0_isr(int irq, void *devid)
 						bitrev32(val);
 				}
 				/* clear CS control register */
-				memset_io(reg_ctrl, 0, sizeof(val));
+				writel_relaxed(0, reg_ctrl);
 			}
 		} else {
 			regmap_read(xcvr->regmap, FSL_XCVR_RX_CS_DATA_0,
