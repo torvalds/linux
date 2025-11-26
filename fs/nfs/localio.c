f50d0328d02fe38ba196a73c143e5d87e341d4f7
Mike Snitzer <snitzer@hammerspace.com>
2025-11-26 01:01:26 -0500

    nfs/localio: remove alignment size checking in nfs_is_local_dio_possible
    This check to ensure dio_offset_align isn't larger than PAGE_SIZE is
    no longer relevant (older iterations of NFS Direct was allocating
    misaligned head and tail pages but no longer does, so this check isn't
    needed).
    
    Fixes: c817248fc831 ("nfs/localio: add proper O_DIRECT support for READ and WRITE")
    Signed-off-by: Mike Snitzer <snitzer@kernel.org>
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfs/localio.c
+++ fs/nfs/localio.c
@@ -339,8 +339,6 @@ nfs_is_local_dio_possible(struct nfs_local_kiocb *iocb, int rw,
 
 	if (unlikely(!nf_dio_mem_align || !nf_dio_offset_align))
 		return false;
-	if (unlikely(nf_dio_offset_align > PAGE_SIZE))
-		return false;
 	if (unlikely(len < nf_dio_offset_align))
 		return false;
 
