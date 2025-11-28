9316012dd01952f75e37035360138ccc786ef727
Jonas Gorski <jonas.gorski@gmail.com>
2025-11-28 09:06:20 +0100

    net: dsa: b53: fix extracting VID from entry for BCM5325/65
    BCM5325/65's Entry register uses the highest three bits for
    VALID/STATIC/AGE, so shifting by 53 only will add these to
    b53_arl_entry::vid.
    
    So make sure to mask the vid value as well, to not get invalid VIDs.
    
    Fixes: c45655386e53 ("net: dsa: b53: add support for FDB operations on 5325/5365")
    Reviewed-by: Florian Fainelli <florian.fainelli@broadcom.com>
    Tested-by: Álvaro Fernández Rojas <noltari@gmail.com>
    Signed-off-by: Jonas Gorski <jonas.gorski@gmail.com>
    Link: https://patch.msgid.link/20251128080625.27181-3-jonas.gorski@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/dsa/b53/b53_priv.h
+++ drivers/net/dsa/b53/b53_priv.h
@@ -350,7 +350,7 @@ static inline void b53_arl_to_entry_25(struct b53_arl_entry *ent,
 	ent->is_age = !!(mac_vid & ARLTBL_AGE_25);
 	ent->is_static = !!(mac_vid & ARLTBL_STATIC_25);
 	u64_to_ether_addr(mac_vid, ent->mac);
-	ent->vid = mac_vid >> ARLTBL_VID_S_65;
+	ent->vid = (mac_vid >> ARLTBL_VID_S_65) & ARLTBL_VID_MASK_25;
 }
 
 static inline void b53_arl_to_entry_89(struct b53_arl_entry *ent,
