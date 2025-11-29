67b032daa288b70df2e6cb2c3a9cefcae0c6fa5f
Taimur Hassan <Syed.Hassan@amd.com>
2025-11-28 20:15:43 -0500

    drm/amd/display: Promote DC to 3.2.362
    This version brings along the following updates:
    
     - Defer transitions from minimal state to final state
     - Remove periodic detection callbacks from dcn35+
     - Fixes for S0i3 exit
     - Refactor dml_core_mode_support to reduce stack frame
     - Add additional info from DML for DMU
    
    Signed-off-by: Taimur Hassan <Syed.Hassan@amd.com>
    Signed-off-by: Chenyu Chen <chen-yu.chen@amd.com>
    Acked-by: Tom Chung <chiahsuan.chung@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dc.h
+++ drivers/gpu/drm/amd/display/dc/dc.h
@@ -63,7 +63,7 @@ struct dcn_dsc_reg_state;
 struct dcn_optc_reg_state;
 struct dcn_dccg_reg_state;
 
-#define DC_VER "3.2.361"
+#define DC_VER "3.2.362"
 
 /**
  * MAX_SURFACES - representative of the upper bound of surfaces that can be piped to a single CRTC
