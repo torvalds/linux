c7ff956344e4c4cc87de6c1a53a1fcbb5fd5fd78
Alice Ryhl <aliceryhl@google.com>
2025-12-02 19:37:44 +0000

    rust: irq: add __rust_helper to helpers
    This is needed to inline these helpers into Rust code.
    
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Reviewed-by: Boqun Feng <boqun.feng@gmail.com>
    Link: https://patch.msgid.link/20251202-define-rust-helper-v1-20-a2e13cbc17a6@google.com
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- rust/helpers/irq.c
+++ rust/helpers/irq.c
@@ -2,8 +2,10 @@
 
 #include <linux/interrupt.h>
 
-int rust_helper_request_irq(unsigned int irq, irq_handler_t handler,
-			    unsigned long flags, const char *name, void *dev)
+__rust_helper int rust_helper_request_irq(unsigned int irq,
+					  irq_handler_t handler,
+					  unsigned long flags, const char *name,
+					  void *dev)
 {
 	return request_irq(irq, handler, flags, name, dev);
 }
