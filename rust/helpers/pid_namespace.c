fc73d4b4d281c24fe8ffbde2919e039cf8162bef
Alice Ryhl <aliceryhl@google.com>
2025-12-02 19:37:51 +0000

    rust: pid_namespace: add __rust_helper to helpers
    This is needed to inline these helpers into Rust code.
    
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Link: https://patch.msgid.link/20251202-define-rust-helper-v1-27-a2e13cbc17a6@google.com
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Reviewed-by: Boqun Feng <boqun.feng@gmail.com>
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- rust/helpers/pid_namespace.c
+++ rust/helpers/pid_namespace.c
@@ -3,18 +3,20 @@
 #include <linux/pid_namespace.h>
 #include <linux/cleanup.h>
 
-struct pid_namespace *rust_helper_get_pid_ns(struct pid_namespace *ns)
+__rust_helper struct pid_namespace *
+rust_helper_get_pid_ns(struct pid_namespace *ns)
 {
 	return get_pid_ns(ns);
 }
 
-void rust_helper_put_pid_ns(struct pid_namespace *ns)
+__rust_helper void rust_helper_put_pid_ns(struct pid_namespace *ns)
 {
 	put_pid_ns(ns);
 }
 
 /* Get a reference on a task's pid namespace. */
-struct pid_namespace *rust_helper_task_get_pid_ns(struct task_struct *task)
+__rust_helper struct pid_namespace *
+rust_helper_task_get_pid_ns(struct task_struct *task)
 {
 	struct pid_namespace *pid_ns;
 
