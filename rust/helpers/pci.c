593e0b22340c3b6aa19c0fa3c466b0809a7ba0d1
Alice Ryhl <aliceryhl@google.com>
2025-12-02 19:37:50 +0000

    rust: pci: add __rust_helper to helpers
    This is needed to inline these helpers into Rust code.
    
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Reviewed-by: Boqun Feng <boqun.feng@gmail.com>
    Link: https://patch.msgid.link/20251202-define-rust-helper-v1-26-a2e13cbc17a6@google.com
    [ Consider latest helper additions. - Danilo ]
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- rust/helpers/pci.c
+++ rust/helpers/pci.c
@@ -2,41 +2,44 @@
 
 #include <linux/pci.h>
 
-u16 rust_helper_pci_dev_id(struct pci_dev *dev)
+__rust_helper u16 rust_helper_pci_dev_id(struct pci_dev *dev)
 {
 	return PCI_DEVID(dev->bus->number, dev->devfn);
 }
 
-resource_size_t rust_helper_pci_resource_start(struct pci_dev *pdev, int bar)
+__rust_helper resource_size_t
+rust_helper_pci_resource_start(struct pci_dev *pdev, int bar)
 {
 	return pci_resource_start(pdev, bar);
 }
 
-resource_size_t rust_helper_pci_resource_len(struct pci_dev *pdev, int bar)
+__rust_helper resource_size_t rust_helper_pci_resource_len(struct pci_dev *pdev,
+							   int bar)
 {
 	return pci_resource_len(pdev, bar);
 }
 
-bool rust_helper_dev_is_pci(const struct device *dev)
+__rust_helper bool rust_helper_dev_is_pci(const struct device *dev)
 {
 	return dev_is_pci(dev);
 }
 
 #ifndef CONFIG_PCI_MSI
-int rust_helper_pci_alloc_irq_vectors(struct pci_dev *dev,
-				      unsigned int min_vecs,
-				      unsigned int max_vecs,
-				      unsigned int flags)
+__rust_helper int rust_helper_pci_alloc_irq_vectors(struct pci_dev *dev,
+						    unsigned int min_vecs,
+						    unsigned int max_vecs,
+						    unsigned int flags)
 {
 	return pci_alloc_irq_vectors(dev, min_vecs, max_vecs, flags);
 }
 
-void rust_helper_pci_free_irq_vectors(struct pci_dev *dev)
+__rust_helper void rust_helper_pci_free_irq_vectors(struct pci_dev *dev)
 {
 	pci_free_irq_vectors(dev);
 }
 
-int rust_helper_pci_irq_vector(struct pci_dev *pdev, unsigned int nvec)
+__rust_helper int rust_helper_pci_irq_vector(struct pci_dev *pdev,
+					     unsigned int nvec)
 {
 	return pci_irq_vector(pdev, nvec);
 }
