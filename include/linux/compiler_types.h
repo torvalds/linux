81cee9166a9073b4da28e970e75d7f89c98ed966
Kees Cook <kees@kernel.org>
2025-12-03 15:30:33 -0800

    compiler_types: Introduce __flex_counter() and family
    Introduce __flex_counter() which wraps __builtin_counted_by_ref(),
    as newly introduced by GCC[1] and Clang[2]. Use of __flex_counter()
    allows access to the counter member of a struct's flexible array member
    when it has been annotated with __counted_by().
    
    Introduce typeof_flex_counter(), overflows_flex_counter_type(), and
    __set_flex_counter() to provide the needed _Generic() wrappers to get
    sane results out of __flex_counter().
    
    For example, with:
    
    	struct foo {
    		int counter;
    		short array[] __counted_by(counter);
    	} *p;
    
    __flex_counter(p->array) will resolve to: &p->counter
    
    typeof_flex_counter(p->array) will resolve to "int". (If p->array was not
    annotated, it would resolve to "size_t".)
    
    overflows_flex_counter_type(typeof(*p), array, COUNT) is the same as:
    
    	COUNT <= type_max(p->counter) && COUNT >= type_min(p->counter)
    
    (If p->array was not annotated it would return true since everything
    fits in size_t.)
    
    __set_flex_counter(p->array, COUNT) is the same as:
    
    	p->counter = COUNT;
    
    (It is a no-op if p->array is not annotated with __counted_by().)
    
    Link: https://patch.msgid.link/20251203233036.3212363-3-kees@kernel.org
    Signed-off-by: Kees Cook <kees@kernel.org>

--- include/linux/compiler_types.h
+++ include/linux/compiler_types.h
@@ -536,6 +536,37 @@ struct ftrace_likely_data {
 #endif
 
 /*
+ * Optional: only supported since gcc >= 15, clang >= 19
+ *
+ *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html#index-_005f_005fbuiltin_005fcounted_005fby_005fref
+ * clang: https://clang.llvm.org/docs/LanguageExtensions.html#builtin-counted-by-ref
+ */
+#if __has_builtin(__builtin_counted_by_ref)
+/**
+ * __flex_counter() - Get pointer to counter member for the given
+ *                    flexible array, if it was annotated with __counted_by()
+ * @FAM: Pointer to flexible array member of an addressable struct instance
+ *
+ * For example, with:
+ *
+ *	struct foo {
+ *		int counter;
+ *		short array[] __counted_by(counter);
+ *	} *p;
+ *
+ * __flex_counter(p->array) will resolve to &p->counter.
+ *
+ * Note that Clang may not allow this to be assigned to a separate
+ * variable; it must be used directly.
+ *
+ * If p->array is unannotated, this returns (void *)NULL.
+ */
+#define __flex_counter(FAM)	__builtin_counted_by_ref(FAM)
+#else
+#define __flex_counter(FAM)	((void *)NULL)
+#endif
+
+/*
  * Some versions of gcc do not mark 'asm goto' volatile:
  *
  *  https://gcc.gnu.org/bugzilla/show_bug.cgi?id=103979
