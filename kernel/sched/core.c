6c125b85f3c87b4bf7dba91af6f27d9600b9dba0
Gabriele Monaco <gmonaco@redhat.com>
2025-12-05 14:16:16 +0100

    sched: Export hidden tracepoints to modules
    The tracepoints sched_entry, sched_exit and sched_set_need_resched
    are not exported to tracefs as trace events, this allows only kernel
    code to access them. Helper modules like [1] can be used to still have
    the tracepoints available to ftrace for debugging purposes, but they do
    rely on the tracepoints being exported.
    
    Export the 3 not exported tracepoints.
    Note that sched_set_state is already exported as the macro is called
    from modules.
    
    [1] - https://github.com/qais-yousef/sched_tp.git
    
    Fixes: adcc3bfa8806 ("sched: Adapt sched tracepoints for RV task model")
    Signed-off-by: Gabriele Monaco <gmonaco@redhat.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Phil Auld <pauld@redhat.com>
    Link: https://patch.msgid.link/20251205131621.135513-9-gmonaco@redhat.com

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -119,6 +119,9 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(sched_util_est_cfs_tp);
 EXPORT_TRACEPOINT_SYMBOL_GPL(sched_util_est_se_tp);
 EXPORT_TRACEPOINT_SYMBOL_GPL(sched_update_nr_running_tp);
 EXPORT_TRACEPOINT_SYMBOL_GPL(sched_compute_energy_tp);
+EXPORT_TRACEPOINT_SYMBOL_GPL(sched_entry_tp);
+EXPORT_TRACEPOINT_SYMBOL_GPL(sched_exit_tp);
+EXPORT_TRACEPOINT_SYMBOL_GPL(sched_set_need_resched_tp);
 
 DEFINE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
 DEFINE_PER_CPU(struct rnd_state, sched_rnd_state);
