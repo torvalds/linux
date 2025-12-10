a1eaca410a3cf533f8005d2959a7a8d9d8979f3e
Marc Zyngier <maz@kernel.org>
2025-12-10 08:22:39 +0000

    MIPS: Move IP30 timer to request_percpu_irq()
    Teach the SGI IP30 timer about request_percpu_irq(), which ultimately will
    allow for the removal of the antiquated setup_percpu_irq() API.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://patch.msgid.link/20251210082242.360936-4-maz@kernel.org

--- arch/mips/include/asm/cevt-r4k.h
+++ arch/mips/include/asm/cevt-r4k.h
@@ -23,7 +23,6 @@ void mips_event_handler(struct clock_event_device *dev);
 int c0_compare_int_usable(void);
 irqreturn_t c0_compare_interrupt(int, void *);
 
-extern struct irqaction c0_compare_irqaction;
 extern int cp0_timer_irq_installed;
 
 #endif /* __ASM_CEVT_R4K_H */
