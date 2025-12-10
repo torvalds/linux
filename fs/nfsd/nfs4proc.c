c6c209ceb87f64a6ceebe61761951dcbbf4a0baa
Chuck Lever <chuck.lever@oracle.com>
2025-12-09 19:28:49 -0500

    NFSD: Remove NFSERR_EAGAIN
    I haven't found an NFSERR_EAGAIN in RFCs 1094, 1813, 7530, or 8881.
    None of these RFCs have an NFS status code that match the numeric
    value "11".
    
    Based on the meaning of the EAGAIN errno, I presume the use of this
    status in NFSD means NFS4ERR_DELAY. So replace the one usage of
    nfserr_eagain, and remove it from NFSD's NFS status conversion
    tables.
    
    As far as I can tell, NFSERR_EAGAIN has existed since the pre-git
    era, but was not actually used by any code until commit f4e44b393389
    ("NFSD: delay unmount source's export after inter-server copy
    completed."), at which time it become possible for NFSD to return
    a status code of 11 (which is not valid NFS protocol).
    
    Fixes: f4e44b393389 ("NFSD: delay unmount source's export after inter-server copy completed.")
    Cc: stable@vger.kernel.org
    Reviewed-by: NeilBrown <neil@brown.name>
    Reviewed-by: Jeff Layton <jlayton@kernel.org>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- fs/nfsd/nfs4proc.c
+++ fs/nfsd/nfs4proc.c
@@ -1506,7 +1506,7 @@ try_again:
 					(schedule_timeout(20*HZ) == 0)) {
 				finish_wait(&nn->nfsd_ssc_waitq, &wait);
 				kfree(work);
-				return nfserr_eagain;
+				return nfserr_jukebox;
 			}
 			finish_wait(&nn->nfsd_ssc_waitq, &wait);
 			goto try_again;
