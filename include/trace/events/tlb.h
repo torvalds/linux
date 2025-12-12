8b62e64e6d30fa047b3aefb1a36e1f80c8acb3d2
Tal Zussman <tz2294@columbia.edu>
2025-12-12 04:08:07 -0500

    x86/mm/tlb/trace: Export the TLB_REMOTE_WRONG_CPU enum in <trace/events/tlb.h>
    When the TLB_REMOTE_WRONG_CPU enum was introduced for the tlb_flush
    tracepoint, the enum was not exported to user-space. Add it to the
    appropriate macro definition to enable parsing by userspace tools, as
    per:
    
      Link: https://lore.kernel.org/all/20150403013802.220157513@goodmis.org
    
    [ mingo: Capitalize IPI, etc. ]
    
    Fixes: 2815a56e4b72 ("x86/mm/tlb: Add tracepoint for TLB flush IPI to stale CPU")
    Signed-off-by: Tal Zussman <tz2294@columbia.edu>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Rik van Riel <riel@surriel.com>
    Link: https://patch.msgid.link/20251212-tlb-trace-fix-v2-1-d322e0ad9b69@columbia.edu

--- include/trace/events/tlb.h
+++ include/trace/events/tlb.h
@@ -12,8 +12,9 @@
 	EM(  TLB_FLUSH_ON_TASK_SWITCH,	"flush on task switch" )	\
 	EM(  TLB_REMOTE_SHOOTDOWN,	"remote shootdown" )		\
 	EM(  TLB_LOCAL_SHOOTDOWN,	"local shootdown" )		\
-	EM(  TLB_LOCAL_MM_SHOOTDOWN,	"local mm shootdown" )		\
-	EMe( TLB_REMOTE_SEND_IPI,	"remote ipi send" )
+	EM(  TLB_LOCAL_MM_SHOOTDOWN,	"local MM shootdown" )		\
+	EM(  TLB_REMOTE_SEND_IPI,	"remote IPI send" )		\
+	EMe( TLB_REMOTE_WRONG_CPU,	"remote wrong CPU" )
 
 /*
  * First define the enums in TLB_FLUSH_REASON to be exported to userspace
