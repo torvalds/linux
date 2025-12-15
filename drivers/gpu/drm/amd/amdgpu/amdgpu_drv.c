8644084a74a4573278d6f454c6638ccd5965f4e2
Mario Limonciello (AMD) <superm1@kernel.org>
2025-12-14 19:12:19 -0600

    drm/amd: Drop "amdgpu kernel modesetting enabled" message
    The behavior for amdgpu was changed with commit e00e5c223878
    ("drm/amdgpu: adjust drm_firmware_drivers_only() handling") to
    potentially allow loading even if nomodeset was set, so the
    message is no longer accurate.
    
    Just drop it to avoid confusion.
    
    Fixes: e00e5c223878 ("drm/amdgpu: adjust drm_firmware_drivers_only() handling")
    Signed-off-by: Mario Limonciello (AMD) <superm1@kernel.org>
    Reviewed-by: Aurabindo Pillai <aurabindo.pillai@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -3164,7 +3164,6 @@ static int __init amdgpu_init(void)
 	if (r)
 		goto error_fence;
 
-	DRM_INFO("amdgpu kernel modesetting enabled.\n");
 	amdgpu_register_atpx_handler();
 	amdgpu_acpi_detect();
 
