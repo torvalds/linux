ee628d9cc8d5b96fdceeb270cf662efc4f85f2b6
Kevin Brodsky <kevin.brodsky@arm.com>
2025-12-15 15:03:23 +0000

    mm: add basic tests for lazy_mmu
    Add basic KUnit tests for the generic aspects of the lazy MMU mode: ensure
    that it appears active when it should, depending on how enable/disable and
    pause/resume pairs are nested.
    
    [akpm@linux-foundation.org: export ppc64_tlb_batch and __flush_tlb_pending to modules]
    [ritesh.list@gmail.com: use EXPORT_SYMBOL_IF_KUNIT()]
      Link: https://lkml.kernel.org/r/87a4zhkt6h.ritesh.list@gmail.com
    [kevin.brodsky@arm.com: move MODULE_IMPORT_NS(), add comment]
      Link: https://lkml.kernel.org/r/20251217163812.2633648-2-kevin.brodsky@arm.com
    Link: https://lkml.kernel.org/r/20251215150323.2218608-15-kevin.brodsky@arm.com
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>
    Reviewed-by: Yeoreum Yun <yeoreum.yun@arm.com>
    Signed-off-by: Ritesh Harjani (IBM) <ritesh.list@gmail.com>
    Acked-by: David Hildenbrand (Red Hat) <david@kernel.org>
    Cc: Alexander Gordeev <agordeev@linux.ibm.com>
    Cc: Andreas Larsson <andreas@gaisler.com>
    Cc: Anshuman Khandual <anshuman.khandual@arm.com>
    Cc: Borislav Betkov <bp@alien8.de>
    Cc: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Christophe Leroy <christophe.leroy@csgroup.eu>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: David Woodhouse <dwmw2@infradead.org>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Juegren Gross <jgross@suse.com>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Madhavan Srinivasan <maddy@linux.ibm.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ritesh Harjani (IBM) <ritesh.list@gmail.com>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Thomas Gleinxer <tglx@linutronix.de>
    Cc: Venkat Rao Bagalkote <venkat88@linux.ibm.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/powerpc/mm/book3s64/hash_tlb.c
+++ arch/powerpc/mm/book3s64/hash_tlb.c
@@ -25,11 +25,12 @@
 #include <asm/tlb.h>
 #include <asm/bug.h>
 #include <asm/pte-walk.h>
-
+#include <kunit/visibility.h>
 
 #include <trace/events/thp.h>
 
 DEFINE_PER_CPU(struct ppc64_tlb_batch, ppc64_tlb_batch);
+EXPORT_SYMBOL_IF_KUNIT(ppc64_tlb_batch);
 
 /*
  * A linux PTE was changed and the corresponding hash table entry
@@ -154,6 +155,7 @@ void __flush_tlb_pending(struct ppc64_tlb_batch *batch)
 		flush_hash_range(i, local);
 	batch->index = 0;
 }
+EXPORT_SYMBOL_IF_KUNIT(__flush_tlb_pending);
 
 void hash__tlb_flush(struct mmu_gather *tlb)
 {
