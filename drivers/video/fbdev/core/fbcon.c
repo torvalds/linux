011a0502801c8536f64141a2b61362c14f456544
Andrey Vatoropin <a.vatoropin@crpt.ru>
2025-12-17 09:11:05 +0000

    fbcon: check return value of con2fb_acquire_newinfo()
    If fbcon_open() fails when called from con2fb_acquire_newinfo() then
    info->fbcon_par pointer remains NULL which is later dereferenced.
    
    Add check for return value of the function con2fb_acquire_newinfo() to
    avoid it.
    
    Found by Linux Verification Center (linuxtesting.org) with SVACE.
    
    Fixes: d1baa4ffa677 ("fbcon: set_con2fb_map fixes")
    Cc: stable@vger.kernel.org
    Signed-off-by: Andrey Vatoropin <a.vatoropin@crpt.ru>
    Signed-off-by: Helge Deller <deller@gmx.de>

--- drivers/video/fbdev/core/fbcon.c
+++ drivers/video/fbdev/core/fbcon.c
@@ -1068,7 +1068,8 @@ static void fbcon_init(struct vc_data *vc, bool init)
 		return;
 
 	if (!info->fbcon_par)
-		con2fb_acquire_newinfo(vc, info, vc->vc_num);
+		if (con2fb_acquire_newinfo(vc, info, vc->vc_num))
+			return;
 
 	/* If we are not the first console on this
 	   fb, copy the font from that console */
