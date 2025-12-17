8618307b52ef2fa5caa87f1b7938125cb410e777
Alice Ryhl <aliceryhl@google.com>
2025-12-17 13:51:30 +0000

    rust: bitops: add __rust_helper to helpers
    This is needed to inline these helpers into Rust code.
    
    Reviewed-by: Boqun Feng <boqun.feng@gmail.com>
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Signed-off-by: Yury Norov (NVIDIA) <yury.norov@gmail.com>

--- rust/helpers/bitops.c
+++ rust/helpers/bitops.c
@@ -2,21 +2,25 @@
 
 #include <linux/bitops.h>
 
+__rust_helper
 void rust_helper___set_bit(unsigned long nr, unsigned long *addr)
 {
 	__set_bit(nr, addr);
 }
 
+__rust_helper
 void rust_helper___clear_bit(unsigned long nr, unsigned long *addr)
 {
 	__clear_bit(nr, addr);
 }
 
+__rust_helper
 void rust_helper_set_bit(unsigned long nr, volatile unsigned long *addr)
 {
 	set_bit(nr, addr);
 }
 
+__rust_helper
 void rust_helper_clear_bit(unsigned long nr, volatile unsigned long *addr)
 {
 	clear_bit(nr, addr);
