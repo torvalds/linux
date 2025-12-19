fbbc944b5945bfd2715a3d762784c318a040f36a
Thorsten Blum <thorsten.blum@linux.dev>
2025-12-19 15:51:17 +0100

    crypto: acomp - Use unregister_acomps in register_acomps
    Replace the for loop with a call to crypto_unregister_acomps(). Return
    'ret' immediately and remove the goto statement to simplify the error
    handling code.  No functional changes.
    
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/acompress.c
+++ crypto/acompress.c
@@ -337,17 +337,13 @@ int crypto_register_acomps(struct acomp_alg *algs, int count)
 
 	for (i = 0; i < count; i++) {
 		ret = crypto_register_acomp(&algs[i]);
-		if (ret)
-			goto err;
+		if (ret) {
+			crypto_unregister_acomps(algs, i);
+			return ret;
+		}
 	}
 
 	return 0;
-
-err:
-	for (--i; i >= 0; --i)
-		crypto_unregister_acomp(&algs[i]);
-
-	return ret;
 }
 EXPORT_SYMBOL_GPL(crypto_register_acomps);
 
