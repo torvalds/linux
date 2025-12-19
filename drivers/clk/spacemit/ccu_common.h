99669468d24ce21be12f3751e7381c47ab2c9ecd
Yixun Lan <dlan@gentoo.org>
2025-12-19 08:07:23 +0800

    clk: spacemit: extract common ccu functions
    Refactor the probe function of SpacemiT's clock, and extract a common ccu
    file, so new clock driver added in the future can share the same code,
    which would lower the burden of maintenance. Since this commit changes the
    module name from spacemit_ccu_k1 to spacemit_ccu where the auxiliary device
    registered, the auxiliary device id need to be adjusted. Idea of the patch
    comes from the review of K3 clock driver, please refer to this disucssion[1]
    for more detail.
    
    This change will introduce a runtime break to reset driver, and will be
    fixed in follow-up commit:
    ecff77f7c041 ("reset: spacemit: fix auxiliary device id")
    
    Link: https://lore.kernel.org/all/aTo8sCPpVM1o9PKX@pie/ [1]
    Link: https://lore.kernel.org/r/20260108-06-k1-clk-common-v4-2-badf635993d3@gentoo.org
    Suggested-by: Yao Zi <me@ziyao.cc>
    Reviewed-by: Alex Elder <elder@riscstar.com>
    Signed-off-by: Yixun Lan <dlan@gentoo.org>

--- drivers/clk/spacemit/ccu_common.h
+++ drivers/clk/spacemit/ccu_common.h
@@ -7,6 +7,8 @@
 #ifndef _CCU_COMMON_H_
 #define _CCU_COMMON_H_
 
+#include <linux/clk-provider.h>
+#include <linux/platform_device.h>
 #include <linux/regmap.h>
 
 struct ccu_common {
@@ -36,6 +38,12 @@ static inline struct ccu_common *hw_to_ccu_common(struct clk_hw *hw)
 	return container_of(hw, struct ccu_common, hw);
 }
 
+struct spacemit_ccu_data {
+	const char *reset_name;
+	struct clk_hw **hws;
+	size_t num;
+};
+
 #define ccu_read(c, reg)						\
 	({								\
 		u32 tmp;						\
@@ -44,5 +52,7 @@ static inline struct ccu_common *hw_to_ccu_common(struct clk_hw *hw)
 	 })
 #define ccu_update(c, reg, mask, val) \
 	regmap_update_bits((c)->regmap, (c)->reg_##reg, mask, val)
+
+int spacemit_ccu_probe(struct platform_device *pdev, const char *compat);
 
 #endif /* _CCU_COMMON_H_ */
