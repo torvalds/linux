e521dc1687834d0e8c7506f1fdf00daab4ebb51d
Tim Wassink <timwassink.dev@gmail.com>
2025-12-21 19:17:14 +0100

    platform/x86: asus-nb-wmi: Add keymap for display toggle
    On the Asus Zenbook 14 (UX3405MA), the Fn+F7 key combination emits
    WMI code 0x2d, which was previously unmapped.
    
    Map this code to KEY_DISPLAYTOGGLE. This matches the behavior of the
    display toggle/projector mode key found on other Asus laptops, allowing
    userspace to handle multi-monitor switching or screen toggling.
    
    Tested on ASUS Zenbook 14 UX3405MA.
    
    Signed-off-by: Tim Wassink <timwassink.dev@gmail.com>
    Reviewed-by: Denis Benato <benato.denis96@gmail.com>
    Link: https://patch.msgid.link/20251221181724.19927-1-timwassink.dev@gmail.com
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>

--- drivers/platform/x86/asus-nb-wmi.c
+++ drivers/platform/x86/asus-nb-wmi.c
@@ -580,6 +580,7 @@ static const struct key_entry asus_nb_wmi_keymap[] = {
 	{ KE_KEY, 0x2a, { KEY_SELECTIVE_SCREENSHOT } },
 	{ KE_IGNORE, 0x2b, }, /* PrintScreen (also send via PS/2) on newer models */
 	{ KE_IGNORE, 0x2c, }, /* CapsLock (also send via PS/2) on newer models */
+	{ KE_KEY, 0x2d, { KEY_DISPLAYTOGGLE } },
 	{ KE_KEY, 0x30, { KEY_VOLUMEUP } },
 	{ KE_KEY, 0x31, { KEY_VOLUMEDOWN } },
 	{ KE_KEY, 0x32, { KEY_MUTE } },
