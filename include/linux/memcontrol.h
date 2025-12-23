4221de8c410e7adb1114a4374c78fa4269175343
Roman Gushchin <roman.gushchin@linux.dev>
2025-12-22 20:41:51 -0800

    mm: declare memcg_page_state_output() in memcontrol.h
    To use memcg_page_state_output() in bpf_memcontrol.c move the
    declaration from v1-specific memcontrol-v1.h to memcontrol.h.
    
    Signed-off-by: Roman Gushchin <roman.gushchin@linux.dev>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Link: https://lore.kernel.org/r/20251223044156.208250-2-roman.gushchin@linux.dev
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -950,6 +950,7 @@ static inline void mod_memcg_page_state(struct page *page,
 }
 
 unsigned long memcg_page_state(struct mem_cgroup *memcg, int idx);
+unsigned long memcg_page_state_output(struct mem_cgroup *memcg, int item);
 unsigned long lruvec_page_state(struct lruvec *lruvec, enum node_stat_item idx);
 unsigned long lruvec_page_state_local(struct lruvec *lruvec,
 				      enum node_stat_item idx);
@@ -1369,6 +1370,11 @@ static inline void mod_memcg_page_state(struct page *page,
 }
 
 static inline unsigned long memcg_page_state(struct mem_cgroup *memcg, int idx)
+{
+	return 0;
+}
+
+static inline unsigned long memcg_page_state_output(struct mem_cgroup *memcg, int item)
 {
 	return 0;
 }
