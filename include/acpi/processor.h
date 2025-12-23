d8e43c89cf08ffea4825a24fb8dfd8bde4de9bb1
Huisong Li <lihuisong@huawei.com>
2025-12-23 18:09:13 +0800

    ACPI: processor: idle: Rearrange declarations in header file
    Group all of the declarations of functions that belong to the ACPI
    processor idle driver together in one place in processor.h.
    
    While at it, drop the unnecessary extern modifier from the declaraions
    of two functions.
    
    Signed-off-by: Huisong Li <lihuisong@huawei.com>
    Tested-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://patch.msgid.link/20251223100914.2407069-6-lihuisong@huawei.com
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/processor.h
+++ include/acpi/processor.h
@@ -425,6 +425,8 @@ int acpi_processor_power_state_has_changed(struct acpi_processor *pr);
 int acpi_processor_hotplug(struct acpi_processor *pr);
 void acpi_processor_register_idle_driver(void);
 void acpi_processor_unregister_idle_driver(void);
+int acpi_processor_ffh_lpi_probe(unsigned int cpu);
+int acpi_processor_ffh_lpi_enter(struct acpi_lpi_state *lpi);
 #endif /* CONFIG_ACPI_PROCESSOR_IDLE */
 
 /* in processor_thermal.c */
@@ -446,11 +448,6 @@ static inline void acpi_thermal_cpufreq_exit(struct cpufreq_policy *policy)
 	return;
 }
 #endif	/* CONFIG_CPU_FREQ */
-
-#ifdef CONFIG_ACPI_PROCESSOR_IDLE
-extern int acpi_processor_ffh_lpi_probe(unsigned int cpu);
-extern int acpi_processor_ffh_lpi_enter(struct acpi_lpi_state *lpi);
-#endif
 
 void acpi_processor_init_invariance_cppc(void);
 
