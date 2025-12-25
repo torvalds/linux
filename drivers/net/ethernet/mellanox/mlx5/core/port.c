144297e2a24e3e54aee1180ec21120ea38822b97
Gal Pressman <gal@nvidia.com>
2025-12-25 15:27:16 +0200

    net/mlx5e: Don't print error message due to invalid module
    Dumping module EEPROM on newer modules is supported through the netlink
    interface only.
    
    Querying with old userspace ethtool (or other tools, such as 'lshw')
    which still uses the ioctl interface results in an error message that
    could flood dmesg (in addition to the expected error return value).
    The original message was added under the assumption that the driver
    should be able to handle all module types, but now that such flows are
    easily triggered from userspace, it doesn't serve its purpose.
    
    Change the log level of the print in mlx5_query_module_eeprom() to
    debug.
    
    Fixes: bb64143eee8c ("net/mlx5e: Add ethtool support for dump module EEPROM")
    Signed-off-by: Gal Pressman <gal@nvidia.com>
    Reviewed-by: Tariq Toukan <tariqt@nvidia.com>
    Signed-off-by: Mark Bloch <mbloch@nvidia.com>
    Link: https://patch.msgid.link/20251225132717.358820-5-mbloch@nvidia.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/mellanox/mlx5/core/port.c
+++ drivers/net/ethernet/mellanox/mlx5/core/port.c
@@ -431,7 +431,8 @@ int mlx5_query_module_eeprom(struct mlx5_core_dev *dev,
 		mlx5_qsfp_eeprom_params_set(&query.i2c_address, &query.page, &offset);
 		break;
 	default:
-		mlx5_core_err(dev, "Module ID not recognized: 0x%x\n", module_id);
+		mlx5_core_dbg(dev, "Module ID not recognized: 0x%x\n",
+			      module_id);
 		return -EINVAL;
 	}
 
