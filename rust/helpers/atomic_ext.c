8de731a6c75547602601a5d219b5cf259ce2b38b
FUJITA Tomonori <fujita.tomonori@gmail.com>
2025-12-27 20:59:51 +0900

    rust: helpers: Add i8/i16 atomic try_cmpxchg_relaxed helpers
    Add i8/i16 atomic try_cmpxchg_relaxed helpers that call
    try_cmpxchg_relaxed() macro implementing atomic try_cmpxchg_relaxed
    using architecture-specific instructions.
    
    [boqun: Use try_cmpxchg_relaxed() instead of raw_try_cmpxchg_relaxed()]
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@gmail.com>
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>
    Link: https://patch.msgid.link/20251227115951.1424458-5-fujita.tomonori@gmail.com

--- rust/helpers/atomic_ext.c
+++ rust/helpers/atomic_ext.c
@@ -127,3 +127,13 @@ __rust_helper bool rust_helper_atomic_i16_try_cmpxchg_release(s16 *ptr, s16 *old
 {
 	return try_cmpxchg_release(ptr, old, new);
 }
+
+__rust_helper bool rust_helper_atomic_i8_try_cmpxchg_relaxed(s8 *ptr, s8 *old, s8 new)
+{
+	return try_cmpxchg_relaxed(ptr, old, new);
+}
+
+__rust_helper bool rust_helper_atomic_i16_try_cmpxchg_relaxed(s16 *ptr, s16 *old, s16 new)
+{
+	return try_cmpxchg_relaxed(ptr, old, new);
+}
