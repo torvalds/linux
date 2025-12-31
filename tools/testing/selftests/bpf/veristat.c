c286e7e9d1f1f3d90ad11c37e896f582b02d19c4
Puranjay Mohan <puranjay@kernel.org>
2025-12-31 14:10:50 -0800

    selftests/bpf: veristat: fix printing order in output_stats()
    The order of the variables in the printf() doesn't match the text and
    therefore veristat prints something like this:
    
    Done. Processed 24 files, 0 programs. Skipped 62 files, 0 programs.
    
    When it should print:
    
    Done. Processed 24 files, 62 programs. Skipped 0 files, 0 programs.
    
    Fix the order of variables in the printf() call.
    
    Fixes: 518fee8bfaf2 ("selftests/bpf: make veristat skip non-BPF and failing-to-open BPF objects")
    Tested-by: Eduard Zingerman <eddyz87@gmail.com>
    Signed-off-by: Puranjay Mohan <puranjay@kernel.org>
    Link: https://lore.kernel.org/r/20251231221052.759396-1-puranjay@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/testing/selftests/bpf/veristat.c
+++ tools/testing/selftests/bpf/veristat.c
@@ -2580,7 +2580,7 @@ static void output_stats(const struct verif_stats *s, enum resfmt fmt, bool last
 	if (last && fmt == RESFMT_TABLE) {
 		output_header_underlines();
 		printf("Done. Processed %d files, %d programs. Skipped %d files, %d programs.\n",
-		       env.files_processed, env.files_skipped, env.progs_processed, env.progs_skipped);
+		       env.files_processed, env.progs_processed, env.files_skipped, env.progs_skipped);
 	}
 }
 
