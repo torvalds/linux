f9b74c13b773b7c7e4920d7bc214ea3d5f37b422
Wei Yang <richard.weiyang@gmail.com>
2025-12-31 03:00:26 +0000

    mm/mmu_gather: remove @delay_remap of __tlb_remove_page_size()
    __tlb_remove_page_size() is only used in tlb_remove_page_size() with
    @delay_remap set to false and it is passed directly to
    __tlb_remove_folio_pages_size().
    
    Remove @delay_remap of __tlb_remove_page_size() and call
    __tlb_remove_folio_pages_size() with false @delay_remap.
    
    Link: https://lkml.kernel.org/r/20251231030026.15938-1-richard.weiyang@gmail.com
    Signed-off-by: Wei Yang <richard.weiyang@gmail.com>
    Acked-by: SeongJae Park <sj@kernel.org>
    Acked-by: David Hildenbrand (Red Hat) <david@kernel.org>
    Acked-by: Will Deacon <will@kernel.org>
    Acked-by: Heiko Carstens <hca@linux.ibm.com> # s390
    Cc: Alexander Gordeev <agordeev@linux.ibm.com>
    Cc: "Aneesh Kumar K.V" <aneesh.kumar@kernel.org>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Christian Borntraeger <borntraeger@linux.ibm.com>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Sven Schnelle <svens@linux.ibm.com>
    Cc: Vasily Gorbik <gor@linux.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/asm-generic/tlb.h
+++ include/asm-generic/tlb.h
@@ -287,8 +287,7 @@ struct mmu_gather_batch {
  */
 #define MAX_GATHER_BATCH_COUNT	(10000UL/MAX_GATHER_BATCH)
 
-extern bool __tlb_remove_page_size(struct mmu_gather *tlb, struct page *page,
-		bool delay_rmap, int page_size);
+extern bool __tlb_remove_page_size(struct mmu_gather *tlb, struct page *page, int page_size);
 bool __tlb_remove_folio_pages(struct mmu_gather *tlb, struct page *page,
 		unsigned int nr_pages, bool delay_rmap);
 
@@ -510,7 +509,7 @@ static inline void tlb_flush_mmu_tlbonly(struct mmu_gather *tlb)
 static inline void tlb_remove_page_size(struct mmu_gather *tlb,
 					struct page *page, int page_size)
 {
-	if (__tlb_remove_page_size(tlb, page, false, page_size))
+	if (__tlb_remove_page_size(tlb, page, page_size))
 		tlb_flush_mmu(tlb);
 }
 
