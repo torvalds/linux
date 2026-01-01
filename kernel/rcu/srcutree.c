cee2557ae3b19e0cdfa09695a4d6ba420cc1fd41
Zqiang <qiang.zhang@linux.dev>
2026-01-01 11:34:11 -0500

    srcu: Use suitable gfp_flags for the init_srcu_struct_nodes()
    For use the init_srcu_struct*() to initialized srcu structure,
    the srcu structure's->srcu_sup and sda use GFP_KERNEL flags to
    allocate memory. similarly, if set SRCU_SIZING_INIT, the
    srcu_sup's->node can still use GFP_KERNEL flags to allocate
    memory, not need to use GFP_ATOMIC flags all the time.
    
    Signed-off-by: Zqiang <qiang.zhang@linux.dev>
    Reviewed-by: Joel Fernandes <joelagnelf@nvidia.com>
    Tested-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Joel Fernandes <joelagnelf@nvidia.com>
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- kernel/rcu/srcutree.c
+++ kernel/rcu/srcutree.c
@@ -262,7 +262,7 @@ static int init_srcu_struct_fields(struct srcu_struct *ssp, bool is_static)
 	ssp->srcu_sup->srcu_gp_seq_needed_exp = SRCU_GP_SEQ_INITIAL_VAL;
 	ssp->srcu_sup->srcu_last_gp_end = ktime_get_mono_fast_ns();
 	if (READ_ONCE(ssp->srcu_sup->srcu_size_state) == SRCU_SIZE_SMALL && SRCU_SIZING_IS_INIT()) {
-		if (!init_srcu_struct_nodes(ssp, GFP_ATOMIC))
+		if (!init_srcu_struct_nodes(ssp, is_static ? GFP_ATOMIC : GFP_KERNEL))
 			goto err_free_sda;
 		WRITE_ONCE(ssp->srcu_sup->srcu_size_state, SRCU_SIZE_BIG);
 	}
