c4df070a57def243dcf5773428dd1b51bc8337ef
Jakub Kicinski <kuba@kernel.org>
2026-01-04 10:46:00 -0800

    selftests: hw-net: rss-input-xfrm: try to enable the xfrm at the start
    The test currently SKIPs if the symmetric RSS xfrm is not enabled
    by default. This leads to spurious SKIPs in the Intel CI reporting
    results to NIPA.
    
    Testing on CX7:
    
     # ./drivers/net/hw/rss_input_xfrm.py
      TAP version 13
      1..2
      ok 1 rss_input_xfrm.test_rss_input_xfrm_ipv4 # SKIP Test requires IPv4 connectivity
      # Sym input xfrm already enabled: {'sym-or-xor'}
      ok 2 rss_input_xfrm.test_rss_input_xfrm_ipv6
      # Totals: pass:1 fail:0 xfail:0 xpass:0 skip:1 error:0
    
     # ethtool -X eth0 xfrm none
    
     # ./drivers/net/hw/rss_input_xfrm.py
      TAP version 13
      1..2
      ok 1 rss_input_xfrm.test_rss_input_xfrm_ipv4 # SKIP Test requires IPv4 connectivity
      # Sym input xfrm configured: {'sym-or-xor'}
      ok 2 rss_input_xfrm.test_rss_input_xfrm_ipv6
      # Totals: pass:1 fail:0 xfail:0 xpass:0 skip:1 error:0
    
    Link: https://patch.msgid.link/20260104184600.795280-1-kuba@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- tools/testing/selftests/drivers/net/hw/rss_input_xfrm.py
+++ tools/testing/selftests/drivers/net/hw/rss_input_xfrm.py
@@ -5,9 +5,9 @@ import multiprocessing
 import socket
 from lib.py import ksft_run, ksft_exit, ksft_eq, ksft_ge, cmd, fd_read_timeout
 from lib.py import NetDrvEpEnv
-from lib.py import EthtoolFamily, NetdevFamily
+from lib.py import EthtoolFamily, NetdevFamily, NlError
 from lib.py import KsftSkipEx, KsftFailEx
-from lib.py import rand_port
+from lib.py import defer, ksft_pr, rand_port
 
 
 def traffic(cfg, local_port, remote_port, ipver):
@@ -19,6 +19,40 @@ def traffic(cfg, local_port, remote_port, ipver):
     cmd("echo a | socat - UDP" + tgt, host=cfg.remote)
     fd_read_timeout(sock.fileno(), 5)
     return sock.getsockopt(socket.SOL_SOCKET, socket.SO_INCOMING_CPU)
+
+
+def _rss_input_xfrm_try_enable(cfg):
+    """
+    Check if symmetric input-xfrm is already enabled, if not try to enable it
+    and register a cleanup.
+    """
+    rss = cfg.ethnl.rss_get({'header': {'dev-name': cfg.ifname}})
+    orig_xfrm = rss.get('input-xfrm', set())
+    sym_xfrm = set(filter(lambda x: 'sym' in x, orig_xfrm))
+
+    if sym_xfrm:
+        ksft_pr("Sym input xfrm already enabled:", sym_xfrm)
+        return sym_xfrm
+
+    for xfrm in cfg.ethnl.consts["input-xfrm"].entries:
+        # Skip non-symmetric transforms
+        if "sym" not in xfrm:
+            continue
+
+        try_xfrm = {xfrm} | orig_xfrm
+        try:
+            cfg.ethnl.rss_set({"header": {"dev-index": cfg.ifindex},
+                               "input-xfrm": try_xfrm})
+        except NlError:
+            continue
+
+        ksft_pr("Sym input xfrm configured:", try_xfrm)
+        defer(cfg.ethnl.rss_set,
+              {"header": {"dev-index": cfg.ifindex},
+               "input-xfrm": orig_xfrm})
+        return {xfrm}
+
+    return set()
 
 
 def test_rss_input_xfrm(cfg, ipver):
@@ -37,12 +71,10 @@ def test_rss_input_xfrm(cfg, ipver):
     if not hasattr(socket, "SO_INCOMING_CPU"):
         raise KsftSkipEx("socket.SO_INCOMING_CPU was added in Python 3.11")
 
-    rss = cfg.ethnl.rss_get({'header': {'dev-name': cfg.ifname}})
-    input_xfrm = set(filter(lambda x: 'sym' in x, rss.get('input-xfrm', {})))
-
     # Check for symmetric xor/or-xor
+    input_xfrm = _rss_input_xfrm_try_enable(cfg)
     if not input_xfrm:
-        raise KsftSkipEx("Symmetric RSS hash not requested")
+        raise KsftSkipEx("Symmetric RSS hash not supported by device")
 
     cpus = set()
     successful = 0
