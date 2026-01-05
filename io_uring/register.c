130a82760718997806a618490f5b7ab06932bd9c
Caleb Sander Mateos <csander@purestorage.com>
2026-01-05 14:05:42 -0700

    io_uring/register: drop io_register_enable_rings() submitter_task check
    io_register_enable_rings() checks that the io_ring_ctx is
    IORING_SETUP_R_DISABLED, which ensures submitter_task hasn't been
    assigned by io_uring_create() or a previous io_register_enable_rings()
    call. So drop the redundant check that submitter_task is NULL.
    
    Signed-off-by: Caleb Sander Mateos <csander@purestorage.com>
    Reviewed-by: Joanne Koong <joannelkoong@gmail.com>
    Reviewed-by: Gabriel Krisman Bertazi <krisman@suse.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- io_uring/register.c
+++ io_uring/register.c
@@ -180,7 +180,7 @@ static int io_register_enable_rings(struct io_ring_ctx *ctx)
 	if (!(ctx->flags & IORING_SETUP_R_DISABLED))
 		return -EBADFD;
 
-	if (ctx->flags & IORING_SETUP_SINGLE_ISSUER && !ctx->submitter_task) {
+	if (ctx->flags & IORING_SETUP_SINGLE_ISSUER) {
 		ctx->submitter_task = get_task_struct(current);
 		/*
 		 * Lazy activation attempts would fail if it was polled before
