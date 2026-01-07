eed444d02585b426645532166ede3e2427db901d
John Garry <john.g.garry@oracle.com>
2026-01-07 09:40:04 +0000

    include/asm-generic/topology.h: Remove unused definition of cpumask_of_node()
    The definition of cpumask_of_node() in question is guarded by conflicting
    CONFIG_NUMA and !CONFIG_NUMA checks, so remove it.
    
    Signed-off-by: John Garry <john.g.garry@oracle.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/asm-generic/topology.h
+++ include/asm-generic/topology.h
@@ -45,11 +45,7 @@
 #endif
 
 #ifndef cpumask_of_node
-  #ifdef CONFIG_NUMA
-    #define cpumask_of_node(node)	((node) == 0 ? cpu_online_mask : cpu_none_mask)
-  #else
-    #define cpumask_of_node(node)	((void)(node), cpu_online_mask)
-  #endif
+#define cpumask_of_node(node)	((void)(node), cpu_online_mask)
 #endif
 #ifndef pcibus_to_node
 #define pcibus_to_node(bus)	((void)(bus), -1)
@@ -61,7 +57,7 @@
 				 cpumask_of_node(pcibus_to_node(bus)))
 #endif
 
-#endif	/* CONFIG_NUMA */
+#endif	/* !CONFIG_NUMA */
 
 #if !defined(CONFIG_NUMA) || !defined(CONFIG_HAVE_MEMORYLESS_NODES)
 
