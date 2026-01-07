0483e5e1dc78f53c540b19231e9cf5571ce01d50
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2026-01-07 11:36:56 +0100

    hrtimer: Remove unused resolution constants
    These constants are never used, remove them.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Link: https://patch.msgid.link/20260107-hrtimer-header-cleanup-v1-1-1a698ef0ddae@linutronix.de

--- include/linux/hrtimer_defs.h
+++ include/linux/hrtimer_defs.h
@@ -15,14 +15,6 @@
  * this resolution values.
  */
 # define HIGH_RES_NSEC		1
-# define KTIME_HIGH_RES		(HIGH_RES_NSEC)
-# define MONOTONIC_RES_NSEC	HIGH_RES_NSEC
-# define KTIME_MONOTONIC_RES	KTIME_HIGH_RES
-
-#else
-
-# define MONOTONIC_RES_NSEC	LOW_RES_NSEC
-# define KTIME_MONOTONIC_RES	KTIME_LOW_RES
 
 #endif
 
