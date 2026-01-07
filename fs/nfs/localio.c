9bb0060f7860aa4561c5b21163dd45ceb66946a9
Mike Snitzer <snitzer@hammerspace.com>
2026-01-07 11:08:56 -0500

    NFS/localio: use GFP_NOIO and non-memreclaim workqueue in nfs_local_commit
    nfslocaliod_workqueue is a non-memreclaim workqueue (it isn't
    initialized with WQ_MEM_RECLAIM), see commit b9f5dd57f4a5
    ("nfs/localio: use dedicated workqueues for filesystem read and
    write").
    
    Use nfslocaliod_workqueue for LOCALIO's SYNC work.
    
    Also, set PF_LOCAL_THROTTLE | PF_MEMALLOC_NOIO in
    nfs_local_fsync_work.
    
    Fixes: b9f5dd57f4a5 ("nfs/localio: use dedicated workqueues for filesystem read and write")
    Signed-off-by: Mike Snitzer <snitzer@hammerspace.com>
    Signed-off-by: Anna Schumaker <anna.schumaker@oracle.com>

--- fs/nfs/localio.c
+++ fs/nfs/localio.c
@@ -1056,10 +1056,13 @@ nfs_local_fsync_ctx_free(struct nfs_local_fsync_ctx *ctx)
 static void
 nfs_local_fsync_work(struct work_struct *work)
 {
+	unsigned long old_flags = current->flags;
 	struct nfs_local_fsync_ctx *ctx;
 	int status;
 
 	ctx = container_of(work, struct nfs_local_fsync_ctx, work);
+
+	current->flags |= PF_LOCAL_THROTTLE | PF_MEMALLOC_NOIO;
 
 	status = nfs_local_run_commit(nfs_to->nfsd_file_file(ctx->localio),
 				      ctx->data);
@@ -1067,6 +1070,8 @@ nfs_local_fsync_work(struct work_struct *work)
 	if (ctx->done != NULL)
 		complete(ctx->done);
 	nfs_local_fsync_ctx_free(ctx);
+
+	current->flags = old_flags;
 }
 
 static struct nfs_local_fsync_ctx *
@@ -1090,7 +1095,7 @@ int nfs_local_commit(struct nfsd_file *localio,
 {
 	struct nfs_local_fsync_ctx *ctx;
 
-	ctx = nfs_local_fsync_ctx_alloc(data, localio, GFP_KERNEL);
+	ctx = nfs_local_fsync_ctx_alloc(data, localio, GFP_NOIO);
 	if (!ctx) {
 		nfs_local_commit_done(data, -ENOMEM);
 		nfs_local_release_commit_data(localio, data, call_ops);
@@ -1102,10 +1107,10 @@ int nfs_local_commit(struct nfsd_file *localio,
 	if (how & FLUSH_SYNC) {
 		DECLARE_COMPLETION_ONSTACK(done);
 		ctx->done = &done;
-		queue_work(nfsiod_workqueue, &ctx->work);
+		queue_work(nfslocaliod_workqueue, &ctx->work);
 		wait_for_completion(&done);
 	} else
-		queue_work(nfsiod_workqueue, &ctx->work);
+		queue_work(nfslocaliod_workqueue, &ctx->work);
 
 	return 0;
 }
