ec69daabe45256f98ac86c651b8ad1b2574489a7
Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
2026-01-08 21:36:48 +0900

    bpf: Fix reference count leak in bpf_prog_test_run_xdp()
    syzbot is reporting
    
      unregister_netdevice: waiting for sit0 to become free. Usage count = 2
    
    problem. A debug printk() patch found that a refcount is obtained at
    xdp_convert_md_to_buff() from bpf_prog_test_run_xdp().
    
    According to commit ec94670fcb3b ("bpf: Support specifying ingress via
    xdp_md context in BPF_PROG_TEST_RUN"), the refcount obtained by
    xdp_convert_md_to_buff() will be released by xdp_convert_buff_to_md().
    
    Therefore, we can consider that the error handling path introduced by
    commit 1c1949982524 ("bpf: introduce frags support to
    bpf_prog_test_run_xdp()") forgot to call xdp_convert_buff_to_md().
    
    Reported-by: syzbot+881d65229ca4f9ae8c84@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=881d65229ca4f9ae8c84
    Fixes: 1c1949982524 ("bpf: introduce frags support to bpf_prog_test_run_xdp()")
    Signed-off-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Reviewed-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Link: https://lore.kernel.org/r/af090e53-9d9b-4412-8acb-957733b3975c@I-love.SAKURA.ne.jp
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- net/bpf/test_run.c
+++ net/bpf/test_run.c
@@ -1363,13 +1363,13 @@ int bpf_prog_test_run_xdp(struct bpf_prog *prog, const union bpf_attr *kattr,
 
 			if (sinfo->nr_frags == MAX_SKB_FRAGS) {
 				ret = -ENOMEM;
-				goto out;
+				goto out_put_dev;
 			}
 
 			page = alloc_page(GFP_KERNEL);
 			if (!page) {
 				ret = -ENOMEM;
-				goto out;
+				goto out_put_dev;
 			}
 
 			frag = &sinfo->frags[sinfo->nr_frags++];
@@ -1381,7 +1381,7 @@ int bpf_prog_test_run_xdp(struct bpf_prog *prog, const union bpf_attr *kattr,
 			if (copy_from_user(page_address(page), data_in + size,
 					   data_len)) {
 				ret = -EFAULT;
-				goto out;
+				goto out_put_dev;
 			}
 			sinfo->xdp_frags_size += data_len;
 			size += data_len;
@@ -1396,6 +1396,7 @@ int bpf_prog_test_run_xdp(struct bpf_prog *prog, const union bpf_attr *kattr,
 		ret = bpf_test_run_xdp_live(prog, &xdp, repeat, batch_size, &duration);
 	else
 		ret = bpf_test_run(prog, &xdp, repeat, &retval, &duration, true);
+out_put_dev:
 	/* We convert the xdp_buff back to an xdp_md before checking the return
 	 * code so the reference count of any held netdevice will be decremented
 	 * even if the test run failed.
