fe7b87d908da33326fbf6fe2b3830426432ec66c
Yi Sun <yi.sun@intel.com>
2026-01-07 16:02:23 -0800

    dmaengine: idxd: Add Max SGL Size Support for DSA3.0
    Certain DSA 3.0 opcodes, such as Gather copy and Gather reduce, require max
    SGL configured for workqueues prior to supporting these opcodes.
    
    Configure the maximum scatter-gather list (SGL) size for workqueues during
    setup on the supported HW. Application can then properly handle the SGL
    size without explicitly setting it.
    
    Signed-off-by: Yi Sun <yi.sun@intel.com>
    Co-developed-by: Anil S Keshavamurthy <anil.s.keshavamurthy@intel.com>
    Signed-off-by: Anil S Keshavamurthy <anil.s.keshavamurthy@intel.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Tested-by: Yi Lai <yi1.lai@intel.com>
    Acked-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    Link: https://patch.msgid.link/20260107-idxd-yi-sun-dsa3-sgl-size-v2-2-dbef8f559e48@intel.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/idxd/idxd.h
+++ drivers/dma/idxd/idxd.h
@@ -227,6 +227,7 @@ struct idxd_wq {
 	char name[WQ_NAME_SIZE + 1];
 	u64 max_xfer_bytes;
 	u32 max_batch_size;
+	u32 max_sgl_size;
 
 	/* Lock to protect upasid_xa access. */
 	struct mutex uc_lock;
@@ -348,6 +349,7 @@ struct idxd_device {
 
 	u64 max_xfer_bytes;
 	u32 max_batch_size;
+	u32 max_sgl_size;
 	int max_groups;
 	int max_engines;
 	int max_rdbufs;
@@ -690,6 +692,20 @@ static inline void idxd_wq_set_max_batch_size(int idxd_type, struct idxd_wq *wq,
 		wq->max_batch_size = 0;
 	else
 		wq->max_batch_size = max_batch_size;
+}
+
+static bool idxd_sgl_supported(struct idxd_device *idxd)
+{
+	return idxd->data->type == IDXD_TYPE_DSA &&
+	       idxd->hw.version >= DEVICE_VERSION_3 &&
+	       idxd->hw.dsacap0.sgl_formats;
+}
+
+static inline void idxd_wq_set_init_max_sgl_size(struct idxd_device *idxd,
+						 struct idxd_wq *wq)
+{
+	if (idxd_sgl_supported(idxd))
+		wq->max_sgl_size = 1U << idxd->hw.dsacap0.max_sgl_shift;
 }
 
 static inline void idxd_wqcfg_set_max_batch_shift(int idxd_type, union wqcfg *wqcfg,
--- drivers/dma/idxd/registers.h
+++ drivers/dma/idxd/registers.h
@@ -390,7 +390,8 @@ union wqcfg {
 		/* bytes 12-15 */
 		u32 max_xfer_shift:5;
 		u32 max_batch_shift:4;
-		u32 rsvd4:23;
+		u32 max_sgl_shift:4;
+		u32 rsvd4:19;
 
 		/* bytes 16-19 */
 		u16 occupancy_inth;
