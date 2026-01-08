6aaa1d6337b5721ede04385444e5796392419241
Jeff Layton <jlayton@kernel.org>
2026-01-08 12:13:09 -0500

    ntfs3: add setlease file operation
    Add the setlease file_operation to ntfs_file_operations,
    ntfs_legacy_file_operations, ntfs_dir_operations, and
    ntfs_legacy_dir_operations, pointing to generic_setlease.  A future
    patch will change the default behavior to reject lease attempts with
    -EINVAL when there is no setlease file operation defined. Add
    generic_setlease to retain the ability to set leases on this
    filesystem.
    
    Signed-off-by: Jeff Layton <jlayton@kernel.org>
    Link: https://patch.msgid.link/20260108-setlease-6-20-v1-14-ea4dec9b67fa@kernel.org
    Acked-by: Al Viro <viro@zeniv.linux.org.uk>
    Acked-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- fs/ntfs3/file.c
+++ fs/ntfs3/file.c
@@ -14,6 +14,7 @@
 #include <linux/falloc.h>
 #include <linux/fiemap.h>
 #include <linux/fileattr.h>
+#include <linux/filelock.h>
 
 #include "debug.h"
 #include "ntfs.h"
@@ -1477,6 +1478,7 @@ const struct file_operations ntfs_file_operations = {
 	.fsync		= ntfs_file_fsync,
 	.fallocate	= ntfs_fallocate,
 	.release	= ntfs_file_release,
+	.setlease	= generic_setlease,
 };
 
 #if IS_ENABLED(CONFIG_NTFS_FS)
@@ -1486,6 +1488,7 @@ const struct file_operations ntfs_legacy_file_operations = {
 	.splice_read	= ntfs_file_splice_read,
 	.open		= ntfs_file_open,
 	.release	= ntfs_file_release,
+	.setlease	= generic_setlease,
 };
 #endif
 // clang-format on
