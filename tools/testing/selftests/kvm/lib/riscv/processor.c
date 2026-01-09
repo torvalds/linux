582b39463f1c0774e0b3cb5be2118e8564b7941e
Fuad Tabba <tabba@google.com>
2026-01-09 08:22:16 +0000

    KVM: riscv: selftests: Fix incorrect rounding in page_align()
    The implementation of `page_align()` in `processor.c` calculates
    alignment incorrectly for values that are already aligned. Specifically,
    `(v + vm->page_size) & ~(vm->page_size - 1)` aligns to the *next* page
    boundary even if `v` is already page-aligned, potentially wasting a page
    of memory.
    
    Fix the calculation to use standard alignment logic: `(v + vm->page_size
    - 1) & ~(vm->page_size - 1)`.
    
    Fixes: 3e06cdf10520 ("KVM: selftests: Add initial support for RISC-V 64-bit")
    Reviewed-by: Andrew Jones <andrew.jones@linux.dev>
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Link: https://patch.msgid.link/20260109082218.3236580-4-tabba@google.com
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- tools/testing/selftests/kvm/lib/riscv/processor.c
+++ tools/testing/selftests/kvm/lib/riscv/processor.c
@@ -28,7 +28,7 @@ bool __vcpu_has_ext(struct kvm_vcpu *vcpu, uint64_t ext)
 
 static uint64_t page_align(struct kvm_vm *vm, uint64_t v)
 {
-	return (v + vm->page_size) & ~(vm->page_size - 1);
+	return (v + vm->page_size - 1) & ~(vm->page_size - 1);
 }
 
 static uint64_t pte_addr(struct kvm_vm *vm, uint64_t entry)
