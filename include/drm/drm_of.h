a4b4385d0523e39a7c058cb5a6c8269e513126ca
Luca Ceresoli <luca.ceresoli@bootlin.com>
2026-01-09 08:31:32 +0100

    drm: of: drm_of_panel_bridge_remove(): fix device_node leak
    drm_of_panel_bridge_remove() uses of_graph_get_remote_node() to get a
    device_node but does not put the node reference.
    
    Fixes: c70087e8f16f ("drm/drm_of: add drm_of_panel_bridge_remove function")
    Cc: stable@vger.kernel.org # v4.15
    Acked-by: Maxime Ripard <mripard@kernel.org>
    Link: https://patch.msgid.link/20260109-drm-bridge-alloc-getput-drm_of_find_bridge-2-v2-1-8bad3ef90b9f@bootlin.com
    Signed-off-by: Luca Ceresoli <luca.ceresoli@bootlin.com>

--- include/drm/drm_of.h
+++ include/drm/drm_of.h
@@ -5,6 +5,7 @@
 #include <linux/err.h>
 #include <linux/of_graph.h>
 #if IS_ENABLED(CONFIG_OF) && IS_ENABLED(CONFIG_DRM_PANEL_BRIDGE)
+#include <linux/of.h>
 #include <drm/drm_bridge.h>
 #endif
 
@@ -172,6 +173,8 @@ static inline int drm_of_panel_bridge_remove(const struct device_node *np,
 
 	bridge = of_drm_find_bridge(remote);
 	drm_panel_bridge_remove(bridge);
+
+	of_node_put(remote);
 
 	return 0;
 #else
