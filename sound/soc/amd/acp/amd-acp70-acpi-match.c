e7c30ac379b429d439eb62ae1bb69720a6701e26
Vijendar Mukunda <Vijendar.Mukunda@amd.com>
2026-01-10 12:14:14 +0530

    ASoC: amd: acp: soc-acpi: add is_device_rt712_vb() helper
    Add a filter to skip the RT172 VB configuration if a SmartMic Function
    is not found in the SDCA descriptors.
    
    If the ACPI information is incorrect this can only be quirked further
    with DMI information.
    
    Signed-off-by: Vijendar Mukunda <Vijendar.Mukunda@amd.com>
    Link: https://patch.msgid.link/20260110064505.1485927-1-Vijendar.Mukunda@amd.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/acp/amd-acp70-acpi-match.c
+++ sound/soc/amd/acp/amd-acp70-acpi-match.c
@@ -7,6 +7,7 @@
  */
 
 #include <sound/soc-acpi.h>
+#include "soc-acpi-amd-sdca-quirks.h"
 #include "../mach-config.h"
 
 static const struct snd_soc_acpi_endpoint single_endpoint = {
@@ -42,6 +43,39 @@ static const struct snd_soc_acpi_endpoint spk_3_endpoint = {
 	.aggregated = 1,
 	.group_position = 3,
 	.group_id = 1
+};
+
+static const struct snd_soc_acpi_endpoint jack_amp_g1_dmic_endpoints[] = {
+	/* Jack Endpoint */
+	{
+		.num = 0,
+		.aggregated = 0,
+		.group_position = 0,
+		.group_id = 0,
+	},
+	/* Amp Endpoint, work as spk_l_endpoint */
+	{
+		.num = 1,
+		.aggregated = 1,
+		.group_position = 0,
+		.group_id = 1,
+	},
+	/* DMIC Endpoint */
+	{
+		.num = 2,
+		.aggregated = 0,
+		.group_position = 0,
+		.group_id = 0,
+	},
+};
+
+static const struct snd_soc_acpi_adr_device rt712_vb_1_group1_adr[] = {
+	{
+		.adr = 0x000130025D071201ull,
+		.num_endpoints = ARRAY_SIZE(jack_amp_g1_dmic_endpoints),
+		.endpoints = jack_amp_g1_dmic_endpoints,
+		.name_prefix = "rt712"
+	}
 };
 
 static const struct snd_soc_acpi_adr_device rt711_rt1316_group_adr[] = {
@@ -254,6 +288,15 @@ static const struct snd_soc_acpi_link_adr acp70_cs35l56x4_l1[] = {
 	{}
 };
 
+static const struct snd_soc_acpi_link_adr acp70_alc712_vb_l1[] = {
+	{
+		.mask = BIT(1),
+		.num_adr = ARRAY_SIZE(rt712_vb_1_group1_adr),
+		.adr_d = rt712_vb_1_group1_adr,
+	},
+	{}
+};
+
 static const struct snd_soc_acpi_link_adr acp70_rt722_only[] = {
 	{
 		.mask = BIT(0),
@@ -308,6 +351,12 @@ struct snd_soc_acpi_mach snd_soc_acpi_amd_acp70_sdw_machines[] = {
 		.links = acp70_cs35l56x4_l1,
 		.drv_name = "amd_sdw",
 	},
+	{
+		.link_mask = BIT(1),
+		.links = acp70_alc712_vb_l1,
+		.machine_check = snd_soc_acpi_amd_sdca_is_device_rt712_vb,
+		.drv_name = "amd_sdw",
+	},
 	{},
 };
 EXPORT_SYMBOL(snd_soc_acpi_amd_acp70_sdw_machines);
@@ -327,3 +376,4 @@ EXPORT_SYMBOL(snd_soc_acpi_amd_acp70_sof_sdw_machines);
 MODULE_DESCRIPTION("AMD ACP7.0 & ACP7.1 tables and support for ACPI enumeration");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Vijendar.Mukunda@amd.com");
+MODULE_IMPORT_NS("SND_SOC_ACPI_AMD_SDCA_QUIRKS");
