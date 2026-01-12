c8c6fb886f57d5bf71fb6de6334a143608d35707
Niklas Cassel <cassel@kernel.org>
2026-01-12 13:20:51 +0100

    ata: libata: Print features also for ATAPI devices
    Commit d633b8a702ab ("libata: print feature list on device scan")
    added a print of the features supported by the device for ATA_DEV_ATA and
    ATA_DEV_ZAC devices, but not for ATA_DEV_ATAPI devices.
    
    Fix this by printing the features also for ATAPI devices.
    
    Before changes:
    ata1.00: ATAPI: Slimtype DVD A  DU8AESH, 6C2M, max UDMA/133
    
    After changes:
    ata1.00: ATAPI: Slimtype DVD A  DU8AESH, 6C2M, max UDMA/133
    ata1.00: Features: Dev-Attention HIPM DIPM
    
    Fixes: d633b8a702ab ("libata: print feature list on device scan")
    Signed-off-by: Niklas Cassel <cassel@kernel.org>
    Tested-by: Wolf <wolf@yoxt.cc>
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -3119,6 +3119,9 @@ int ata_dev_configure(struct ata_device *dev)
 				     dma_dir_string);
 
 		ata_dev_config_lpm(dev);
+
+		if (print_info)
+			ata_dev_print_features(dev);
 	}
 
 	/* determine max_sectors */
