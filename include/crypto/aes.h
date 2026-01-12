a22fd0e3c495dd2d706c49c26663476e24d96e7d
Eric Biggers <ebiggers@kernel.org>
2026-01-12 11:20:00 -0800

    lib/crypto: aes: Introduce improved AES library
    The kernel's AES library currently has the following issues:
    
    - It doesn't take advantage of the architecture-optimized AES code,
      including the implementations using AES instructions.
    
    - It's much slower than even the other software AES implementations: 2-4
      times slower than "aes-generic", "aes-arm", and "aes-arm64".
    
    - It requires that both the encryption and decryption round keys be
      computed and cached.  This is wasteful for users that need only the
      forward (encryption) direction of the cipher: the key struct is 484
      bytes when only 244 are actually needed.  This missed optimization is
      very common, as many AES modes (e.g. GCM, CFB, CTR, CMAC, and even the
      tweak key in XTS) use the cipher only in the forward (encryption)
      direction even when doing decryption.
    
    - It doesn't provide the flexibility to customize the prepared key
      format.  The API is defined to do key expansion, and several callers
      in drivers/crypto/ use it specifically to expand the key.  This is an
      issue when integrating the existing powerpc, s390, and sparc code,
      which is necessary to provide full parity with the traditional API.
    
    To resolve these issues, I'm proposing the following changes:
    
    1. New structs 'aes_key' and 'aes_enckey' are introduced, with
       corresponding functions aes_preparekey() and aes_prepareenckey().
    
       Generally these structs will include the encryption+decryption round
       keys and the encryption round keys, respectively.  However, the exact
       format will be under control of the architecture-specific AES code.
    
       (The verb "prepare" is chosen over "expand" since key expansion isn't
       necessarily done.  It's also consistent with hmac*_preparekey().)
    
    2. aes_encrypt() and aes_decrypt() will be changed to operate on the new
       structs instead of struct crypto_aes_ctx.
    
    3. aes_encrypt() and aes_decrypt() will use architecture-optimized code
       when available, or else fall back to a new generic AES implementation
       that unifies the existing two fragmented generic AES implementations.
    
       The new generic AES implementation uses tables for both SubBytes and
       MixColumns, making it almost as fast as "aes-generic".  However,
       instead of aes-generic's huge 8192-byte tables per direction, it uses
       only 1024 bytes for encryption and 1280 bytes for decryption (similar
       to "aes-arm").  The cost is just some extra rotations.
    
       The new generic AES implementation also includes table prefetching,
       making it have some "constant-time hardening".  That's an improvement
       from aes-generic which has no constant-time hardening.
    
       It does slightly regress in constant-time hardening vs. the old
       lib/crypto/aes.c which had smaller tables, and from aes-fixed-time
       which disabled IRQs on top of that.  But I think this is tolerable.
       The real solutions for constant-time AES are AES instructions or
       bit-slicing.  The table-based code remains a best-effort fallback for
       the increasingly-rare case where a real solution is unavailable.
    
    4. crypto_aes_ctx and aes_expandkey() will remain for now, but only for
       callers that are using them specifically for the AES key expansion
       (as opposed to en/decrypting data with the AES library).
    
    This commit begins the migration process by introducing the new structs
    and functions, backed by the new generic AES implementation.
    
    To allow callers to be incrementally converted, aes_encrypt() and
    aes_decrypt() are temporarily changed into macros that use a _Generic
    expression to call either the old functions (which take crypto_aes_ctx)
    or the new functions (which take the new types).  Once all callers have
    been updated, these macros will go away, the old functions will be
    removed, and the "_new" suffix will be dropped from the new functions.
    
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Link: https://lore.kernel.org/r/20260112192035.10427-3-ebiggers@kernel.org
    Signed-off-by: Eric Biggers <ebiggers@kernel.org>

--- include/crypto/aes.h
+++ include/crypto/aes.h
@@ -18,6 +18,60 @@
 #define AES_MAX_KEYLENGTH	(15 * 16)
 #define AES_MAX_KEYLENGTH_U32	(AES_MAX_KEYLENGTH / sizeof(u32))
 
+union aes_enckey_arch {
+	u32 rndkeys[AES_MAX_KEYLENGTH_U32];
+};
+
+union aes_invkey_arch {
+	u32 inv_rndkeys[AES_MAX_KEYLENGTH_U32];
+};
+
+/**
+ * struct aes_enckey - An AES key prepared for encryption
+ * @len: Key length in bytes: 16 for AES-128, 24 for AES-192, 32 for AES-256.
+ * @nrounds: Number of rounds: 10 for AES-128, 12 for AES-192, 14 for AES-256.
+ *	     This is '6 + @len / 4' and is cached so that AES implementations
+ *	     that need it don't have to recompute it for each en/decryption.
+ * @padding: Padding to make offsetof(@k) be a multiple of 16, so that aligning
+ *	     this struct to a 16-byte boundary results in @k also being 16-byte
+ *	     aligned.  Users aren't required to align this struct to 16 bytes,
+ *	     but it may slightly improve performance.
+ * @k: This typically contains the AES round keys as an array of '@nrounds + 1'
+ *     groups of four u32 words.  However, architecture-specific implementations
+ *     of AES may store something else here, e.g. just the raw key if it's all
+ *     they need.
+ *
+ * Note that this struct is about half the size of struct aes_key.  This is
+ * separate from struct aes_key so that modes that need only AES encryption
+ * (e.g. AES-GCM, AES-CTR, AES-CMAC, tweak key in AES-XTS) don't incur the time
+ * and space overhead of computing and caching the decryption round keys.
+ *
+ * Note that there's no decryption-only equivalent (i.e. "struct aes_deckey"),
+ * since (a) it's rare that modes need decryption-only, and (b) some AES
+ * implementations use the same @k for both encryption and decryption, either
+ * always or conditionally; in the latter case both @k and @inv_k are needed.
+ */
+struct aes_enckey {
+	u32 len;
+	u32 nrounds;
+	u32 padding[2];
+	union aes_enckey_arch k;
+};
+
+/**
+ * struct aes_key - An AES key prepared for encryption and decryption
+ * @aes_enckey: Common fields and the key prepared for encryption
+ * @inv_k: This generally contains the round keys for the AES Equivalent
+ *	   Inverse Cipher, as an array of '@nrounds + 1' groups of four u32
+ *	   words.  However, architecture-specific implementations of AES may
+ *	   store something else here.  For example, they may leave this field
+ *	   uninitialized if they use @k for both encryption and decryption.
+ */
+struct aes_key {
+	struct aes_enckey; /* Include all fields of aes_enckey. */
+	union aes_invkey_arch inv_k;
+};
+
 /*
  * Please ensure that the first two fields are 16-byte aligned
  * relative to the start of the structure, i.e., don't move them!
@@ -34,7 +88,7 @@ extern const u32 crypto_it_tab[4][256] ____cacheline_aligned;
 /*
  * validate key length for AES algorithms
  */
-static inline int aes_check_keylen(unsigned int keylen)
+static inline int aes_check_keylen(size_t keylen)
 {
 	switch (keylen) {
 	case AES_KEYSIZE_128:
@@ -69,23 +123,104 @@ int aes_expandkey(struct crypto_aes_ctx *ctx, const u8 *in_key,
 		  unsigned int key_len);
 
 /**
- * aes_encrypt - Encrypt a single AES block
- * @ctx:	Context struct containing the key schedule
- * @out:	Buffer to store the ciphertext
- * @in:		Buffer containing the plaintext
+ * aes_preparekey() - Prepare an AES key for encryption and decryption
+ * @key: (output) The key structure to initialize
+ * @in_key: The raw AES key
+ * @key_len: Length of the raw key in bytes.  Should be either AES_KEYSIZE_128,
+ *	     AES_KEYSIZE_192, or AES_KEYSIZE_256.
+ *
+ * This prepares an AES key for both the encryption and decryption directions of
+ * the block cipher.  Typically this involves expanding the raw key into both
+ * the standard round keys and the Equivalent Inverse Cipher round keys, but
+ * some architecture-specific implementations don't do the full expansion here.
+ *
+ * The caller is responsible for zeroizing both the struct aes_key and the raw
+ * key once they are no longer needed.
+ *
+ * If you don't need decryption support, use aes_prepareenckey() instead.
+ *
+ * Return: 0 on success or -EINVAL if the given key length is invalid.  No other
+ *	   errors are possible, so callers that always pass a valid key length
+ *	   don't need to check for errors.
+ *
+ * Context: Any context.
  */
-void aes_encrypt(const struct crypto_aes_ctx *ctx, u8 *out, const u8 *in);
+int aes_preparekey(struct aes_key *key, const u8 *in_key, size_t key_len);
 
 /**
- * aes_decrypt - Decrypt a single AES block
- * @ctx:	Context struct containing the key schedule
- * @out:	Buffer to store the plaintext
- * @in:		Buffer containing the ciphertext
+ * aes_prepareenckey() - Prepare an AES key for encryption-only
+ * @key: (output) The key structure to initialize
+ * @in_key: The raw AES key
+ * @key_len: Length of the raw key in bytes.  Should be either AES_KEYSIZE_128,
+ *	     AES_KEYSIZE_192, or AES_KEYSIZE_256.
+ *
+ * This prepares an AES key for only the encryption direction of the block
+ * cipher.  Typically this involves expanding the raw key into only the standard
+ * round keys, resulting in a struct about half the size of struct aes_key.
+ *
+ * The caller is responsible for zeroizing both the struct aes_enckey and the
+ * raw key once they are no longer needed.
+ *
+ * Note that while the resulting prepared key supports only AES encryption, it
+ * can still be used for decrypting in a mode of operation that uses AES in only
+ * the encryption (forward) direction, for example counter mode.
+ *
+ * Return: 0 on success or -EINVAL if the given key length is invalid.  No other
+ *	   errors are possible, so callers that always pass a valid key length
+ *	   don't need to check for errors.
+ *
+ * Context: Any context.
  */
-void aes_decrypt(const struct crypto_aes_ctx *ctx, u8 *out, const u8 *in);
+int aes_prepareenckey(struct aes_enckey *key, const u8 *in_key, size_t key_len);
+
+typedef union {
+	const struct aes_enckey *enc_key;
+	const struct aes_key *full_key;
+} aes_encrypt_arg __attribute__ ((__transparent_union__));
+
+/**
+ * aes_encrypt() - Encrypt a single AES block
+ * @key: The AES key, as a pointer to either an encryption-only key
+ *	 (struct aes_enckey) or a full, bidirectional key (struct aes_key).
+ * @out: Buffer to store the ciphertext block
+ * @in: Buffer containing the plaintext block
+ *
+ * Context: Any context.
+ */
+#define aes_encrypt(key, out, in) \
+	_Generic((key), \
+		 struct crypto_aes_ctx *: aes_encrypt_old((const struct crypto_aes_ctx *)(key), (out), (in)), \
+		 const struct crypto_aes_ctx *: aes_encrypt_old((const struct crypto_aes_ctx *)(key), (out), (in)), \
+		 struct aes_enckey *: aes_encrypt_new((const struct aes_enckey *)(key), (out), (in)), \
+		 const struct aes_enckey *: aes_encrypt_new((const struct aes_enckey *)(key), (out), (in)), \
+		 struct aes_key *: aes_encrypt_new((const struct aes_key *)(key), (out), (in)), \
+		 const struct aes_key *: aes_encrypt_new((const struct aes_key *)(key), (out), (in)))
+void aes_encrypt_old(const struct crypto_aes_ctx *ctx, u8 *out, const u8 *in);
+void aes_encrypt_new(aes_encrypt_arg key, u8 out[at_least AES_BLOCK_SIZE],
+		     const u8 in[at_least AES_BLOCK_SIZE]);
+
+/**
+ * aes_decrypt() - Decrypt a single AES block
+ * @key: The AES key, previously initialized by aes_preparekey()
+ * @out: Buffer to store the plaintext block
+ * @in: Buffer containing the ciphertext block
+ *
+ * Context: Any context.
+ */
+#define aes_decrypt(key, out, in) \
+	_Generic((key), \
+		 struct crypto_aes_ctx *: aes_decrypt_old((const struct crypto_aes_ctx *)(key), (out), (in)), \
+		 const struct crypto_aes_ctx *: aes_decrypt_old((const struct crypto_aes_ctx *)(key), (out), (in)), \
+		 struct aes_key *: aes_decrypt_new((const struct aes_key *)(key), (out), (in)), \
+		 const struct aes_key *: aes_decrypt_new((const struct aes_key *)(key), (out), (in)))
+void aes_decrypt_old(const struct crypto_aes_ctx *ctx, u8 *out, const u8 *in);
+void aes_decrypt_new(const struct aes_key *key, u8 out[at_least AES_BLOCK_SIZE],
+		     const u8 in[at_least AES_BLOCK_SIZE]);
 
 extern const u8 crypto_aes_sbox[];
 extern const u8 crypto_aes_inv_sbox[];
+extern const u32 aes_enc_tab[256];
+extern const u32 aes_dec_tab[256];
 
 void aescfb_encrypt(const struct crypto_aes_ctx *ctx, u8 *dst, const u8 *src,
 		    int len, const u8 iv[AES_BLOCK_SIZE]);
