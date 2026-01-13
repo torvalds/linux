e428b013d9dff30f7a65509e33047ba975cce8ba
Finn Thain <fthain@linux-m68k.org>
2026-01-13 16:22:28 +1100

    atomic: specify alignment for atomic_t and atomic64_t
    Some recent commits incorrectly assumed 4-byte alignment of locks.  That
    assumption fails on Linux/m68k (and, interestingly, would have failed on
    Linux/cris also).  The jump label implementation makes a similar alignment
    assumption.
    
    The expectation that atomic_t and atomic64_t variables will be naturally
    aligned seems reasonable, as indeed they are on 64-bit architectures.  But
    atomic64_t isn't naturally aligned on csky, m68k, microblaze, nios2,
    openrisc and sh.  Neither atomic_t nor atomic64_t are naturally aligned on
    m68k.
    
    This patch brings a little uniformity by specifying natural alignment for
    atomic types.  One benefit is that atomic64_t variables do not get split
    across a page boundary.  The cost is that some structs grow which leads to
    cache misses and wasted memory.
    
    See also, commit bbf2a330d92c ("x86: atomic64: The atomic64_t data type
    should be 8 bytes aligned on 32-bit too").
    
    Link: https://lkml.kernel.org/r/a76bc24a4e7c1d8112d7d5fa8d14e4b694a0e90c.1768281748.git.fthain@linux-m68k.org
    Link: https://lore.kernel.org/lkml/CAFr9PX=MYUDGJS2kAvPMkkfvH+0-SwQB_kxE4ea0J_wZ_pk=7w@mail.gmail.com
    Link: https://lore.kernel.org/lkml/CAMuHMdW7Ab13DdGs2acMQcix5ObJK0O2dG_Fxzr8_g58Rc1_0g@mail.gmail.com/
    Signed-off-by: Finn Thain <fthain@linux-m68k.org>
    Acked-by: Guo Ren <guoren@kernel.org>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Guo Ren <guoren@kernel.org>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Dinh Nguyen <dinguyen@kernel.org>
    Cc: Jonas Bonn <jonas@southpole.se>
    Cc: Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
    Cc: Stafford Horne <shorne@gmail.com>
    Cc: Yoshinori Sato <ysato@users.sourceforge.jp>
    Cc: Rich Felker <dalias@libc.org>
    Cc: John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Andrii Nakryiko <andrii@kernel.org>
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Borislav Petkov (AMD)" <bp@alien8.de>
    Cc: Daniel Borkman <daniel@iogearbox.net>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Eduard Zingerman <eddyz87@gmail.com>
    Cc: Gary Guo <gary@garyguo.net>
    Cc: Hao Luo <haoluo@google.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: John Fastabend <john.fastabend@gmail.com>
    Cc: KP Singh <kpsingh@kernel.org>
    Cc: Marc Rutland <mark.rutland@arm.com>
    Cc: Martin KaFai Lau <martin.lau@linux.dev>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Sasha Levin (Microsoft) <sashal@kernel.org>
    Cc: Song Liu <song@kernel.org>
    Cc: Stanislav Fomichev <sdf@fomichev.me>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Will Deacon <will@kernel.org>
    Cc: Yonghong Song <yonghong.song@linux.dev>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/types.h
+++ include/linux/types.h
@@ -179,7 +179,7 @@ typedef phys_addr_t resource_size_t;
 typedef unsigned long irq_hw_number_t;
 
 typedef struct {
-	int counter;
+	int __aligned(sizeof(int)) counter;
 } atomic_t;
 
 #define ATOMIC_INIT(i) { (i) }
