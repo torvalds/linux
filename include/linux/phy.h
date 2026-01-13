b1b77c82cec16668571b0d2587d19d4204d432e0
Heiner Kallweit <hkallweit1@gmail.com>
2026-01-13 08:23:17 +0100

    net: phy: remove unused fixup unregistering functions
    No user of PHY fixups unregisters these. IOW: The fixup unregistering
    functions are unused and can be removed. Remove also documentation
    for these functions. Whilst at it, remove also mentioning of
    phy_register_fixup() from the Documentation, as this function has been
    static since ea47e70e476f ("net: phy: remove fixup-related definitions
    from phy.h which are not used outside phylib").
    
    Fixup unregistering functions were added with f38e7a32ee4f
    ("phy: add phy fixup unregister functions") in 2016, and last user
    was removed with 6782d06a47ad ("net: usb: lan78xx: Remove KSZ9031 PHY
    fixup") in 2024.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Link: https://patch.msgid.link/ff8ac321-435c-48d0-b376-fbca80c0c22e@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -2405,10 +2405,6 @@ int phy_register_fixup_for_id(const char *bus_id,
 int phy_register_fixup_for_uid(u32 phy_uid, u32 phy_uid_mask,
 			       int (*run)(struct phy_device *));
 
-int phy_unregister_fixup(const char *bus_id, u32 phy_uid, u32 phy_uid_mask);
-int phy_unregister_fixup_for_id(const char *bus_id);
-int phy_unregister_fixup_for_uid(u32 phy_uid, u32 phy_uid_mask);
-
 int phy_eee_tx_clock_stop_capable(struct phy_device *phydev);
 int phy_eee_rx_clock_stop(struct phy_device *phydev, bool clk_stop_enable);
 int phy_init_eee(struct phy_device *phydev, bool clk_stop_enable);
