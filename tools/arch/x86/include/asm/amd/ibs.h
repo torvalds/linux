73cee0aad1ee2479fde2c9b753a1b66acb7d1b9a
Yen-Hsiang Hsu <rrrrr4413@gmail.com>
2026-01-13 22:18:30 +0800

    perf/x86/amd/ibs: Fix comment typo in ibs_op_data
    The comment for tag_to_ret_ctr in ibs_op_data says "15-31"
    but it should be "16-31".
    
    Fix the misleading comment. No functional changes.
    
    Signed-off-by: Yen-Hsiang Hsu <rrrrr4413@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://patch.msgid.link/20260113141830.3204114-1-rrrrr4413@gmail.com

--- tools/arch/x86/include/asm/amd/ibs.h
+++ tools/arch/x86/include/asm/amd/ibs.h
@@ -77,7 +77,7 @@ union ibs_op_data {
 	__u64 val;
 	struct {
 		__u64	comp_to_ret_ctr:16,	/* 0-15: op completion to retire count */
-			tag_to_ret_ctr:16,	/* 15-31: op tag to retire count */
+			tag_to_ret_ctr:16,	/* 16-31: op tag to retire count */
 			reserved1:2,		/* 32-33: reserved */
 			op_return:1,		/* 34: return op */
 			op_brn_taken:1,		/* 35: taken branch op */
