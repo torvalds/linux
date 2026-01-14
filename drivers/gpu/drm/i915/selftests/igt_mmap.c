8103ecc1999eefc54b740d8d99f264ced1d9c54d
Krzysztof Karas <krzysztof.karas@intel.com>
2026-01-14 16:04:21 +0000

    drm/i915/selftests: Keep mock file open during unfaultable migrate with fill
    igt_mmap_migrate() tests migration with various parameters.
    In one of the cases, where FILL and UNFAULTABLE flags are set,
    during first stages of this test, a mock file is opened in
    igt_mmap_offset(), which results in allocating GEM objects for
    page table structures and scratch in GPU mappable memory.
    
    Then, also in igt_mmap_offset(), the file is closed (fput) and
    the cleanup of these objects is scheduled on a delayed worqueue,
    which is designed to execute after unspecified amount of time.
    
    Next, the test calls igt_fill_mappable() to fill mappable GPU
    memory. At this point, three scenarios are possible
    (N = max size of GPU memory for this test in MiB):
     1) the objects allocated for the mock file get cleaned up after
        crucial part of the test is over, so the memory is full with
        the 1 MiB they occupy and N - 1 MiB added by
        igt_fill_mappable(), so the migration fails properly;
     2) the object cleanup fires before igt_fill_mappable()
        completes, so the whole memory is populated with N MiB from
        igt_fill_mappable(), so migration fails as well;
     3) the object cleanup is performed right after fill is done,
        so only N - 1 MiB are in the mappable portion of GPU memory,
        allowing the migration to succeed - we'd expect no space
        left to perform migration, but an object was able to fit in
        the remaining 1 MiB, which caused get_user() to succeed, so
        a page fault did not fail.
    
    The test incorrectly assumes that the GPU mappable memory state
    is unchanging during the test. Amend this by keeping the mock
    file open until migration and page fault checking is complete.
    
    Closes: https://gitlab.freedesktop.org/drm/i915/kernel/-/issues/13929
    Signed-off-by: Krzysztof Karas <krzysztof.karas@intel.com>
    Reviewed-by: Sebastian Brzezinka <sebastian.brzezinka@intel.com>
    Acked-by: Andi Shyti <andi.shyti@linux.intel.com>
    Link: https://lore.kernel.org/r/6xc74s3mbmtliqxihtxbok32jobhc26vfm5mu6cod4ywov6utf@ujp3rmqcwmr3
    Signed-off-by: Andi Shyti <andi.shyti@linux.intel.com>

--- drivers/gpu/drm/i915/selftests/igt_mmap.c
+++ drivers/gpu/drm/i915/selftests/igt_mmap.c
@@ -9,14 +9,14 @@
 #include "i915_drv.h"
 #include "igt_mmap.h"
 
-unsigned long igt_mmap_offset(struct drm_i915_private *i915,
-			      u64 offset,
-			      unsigned long size,
-			      unsigned long prot,
-			      unsigned long flags)
+unsigned long igt_mmap_offset_with_file(struct drm_i915_private *i915,
+					u64 offset,
+					unsigned long size,
+					unsigned long prot,
+					unsigned long flags,
+					struct file *file)
 {
 	struct drm_vma_offset_node *node;
-	struct file *file;
 	unsigned long addr;
 	int err;
 
@@ -31,22 +31,35 @@ unsigned long igt_mmap_offset(struct drm_i915_private *i915,
 		return -ENOENT;
 	}
 
-	/* Pretend to open("/dev/dri/card0") */
-	file = mock_drm_getfile(i915->drm.primary, O_RDWR);
-	if (IS_ERR(file))
-		return PTR_ERR(file);
-
 	err = drm_vma_node_allow(node, file->private_data);
 	if (err) {
-		addr = err;
-		goto out_file;
+		return err;
 	}
 
 	addr = vm_mmap(file, 0, drm_vma_node_size(node) << PAGE_SHIFT,
 		       prot, flags, drm_vma_node_offset_addr(node));
 
 	drm_vma_node_revoke(node, file->private_data);
-out_file:
+
+	return addr;
+}
+
+unsigned long igt_mmap_offset(struct drm_i915_private *i915,
+			      u64 offset,
+			      unsigned long size,
+			      unsigned long prot,
+			      unsigned long flags)
+{
+	struct file *file;
+	unsigned long addr;
+
+	/* Pretend to open("/dev/dri/card0") */
+	file = mock_drm_getfile(i915->drm.primary, O_RDWR);
+	if (IS_ERR(file))
+		return PTR_ERR(file);
+
+	addr = igt_mmap_offset_with_file(i915, offset, size, prot, flags, file);
 	fput(file);
+
 	return addr;
 }
