d1c0978a7e7864fcaa7cdd562a8f938afcd2ad39
Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
2026-01-19 22:08:09 +0200

    drm/tests: hdmi: fix build failure
    The commit ca59e33f5a1f ("drm/atomic: add max_size check to
    drm_property_replace_blob_from_id()") added a new parameter to
    drm_property_replace_blob_from_id(), however commit 7436a87db99d
    ("drm/tests: hdmi: check the infoframes behaviour") was based on the
    older tree and used the old number of params (with me failing to run
    kunit tests when applying).
    
    Fix the build error by specifying -1 as the max_size (as expected).
    
    Fixes: 7436a87db99d ("drm/tests: hdmi: check the infoframes behaviour")
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202601200306.dgF5deFm-lkp@intel.com/
    Tested-by: Luca Ceresoli <luca.ceresoli@bootlin.com>
    Reviewed-by: Luca Ceresoli <luca.ceresoli@bootlin.com>
    Link: https://patch.msgid.link/20260119-fix-kunit-infoframe-v1-1-5f2f9b066594@oss.qualcomm.com
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>

--- drivers/gpu/drm/tests/drm_hdmi_state_helper_test.c
+++ drivers/gpu/drm/tests/drm_hdmi_state_helper_test.c
@@ -2779,7 +2779,7 @@ retry_conn_state:
 	ret = drm_property_replace_blob_from_id(drm,
 						&new_conn_state->hdr_output_metadata,
 						hdr_blob->base.id,
-						sizeof(struct hdr_output_metadata), -1,
+						-1, sizeof(struct hdr_output_metadata), -1,
 						&replaced);
 	KUNIT_ASSERT_EQ(test, ret, 0);
 	KUNIT_ASSERT_EQ(test, replaced, true);
