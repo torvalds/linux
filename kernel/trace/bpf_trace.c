802eef5afb1865bc5536a5302c068ba2215a1f72
Zesen Liu <ftyghome@gmail.com>
2026-01-20 16:28:46 +0800

    bpf: Fix memory access flags in helper prototypes
    After commit 37cce22dbd51 ("bpf: verifier: Refactor helper access type tracking"),
    the verifier started relying on the access type flags in helper
    function prototypes to perform memory access optimizations.
    
    Currently, several helper functions utilizing ARG_PTR_TO_MEM lack the
    corresponding MEM_RDONLY or MEM_WRITE flags. This omission causes the
    verifier to incorrectly assume that the buffer contents are unchanged
    across the helper call. Consequently, the verifier may optimize away
    subsequent reads based on this wrong assumption, leading to correctness
    issues.
    
    For bpf_get_stack_proto_raw_tp, the original MEM_RDONLY was incorrect
    since the helper writes to the buffer. Change it to ARG_PTR_TO_UNINIT_MEM
    which correctly indicates write access to potentially uninitialized memory.
    
    Similar issues were recently addressed for specific helpers in commit
    ac44dcc788b9 ("bpf: Fix verifier assumptions of bpf_d_path's output buffer")
    and commit 2eb7648558a7 ("bpf: Specify access type of bpf_sysctl_get_name args").
    
    Fix these prototypes by adding the correct memory access flags.
    
    Fixes: 37cce22dbd51 ("bpf: verifier: Refactor helper access type tracking")
    Co-developed-by: Shuran Liu <electronlsr@gmail.com>
    Signed-off-by: Shuran Liu <electronlsr@gmail.com>
    Co-developed-by: Peili Gao <gplhust955@gmail.com>
    Signed-off-by: Peili Gao <gplhust955@gmail.com>
    Co-developed-by: Haoran Ni <haoran.ni.cs@gmail.com>
    Signed-off-by: Haoran Ni <haoran.ni.cs@gmail.com>
    Signed-off-by: Zesen Liu <ftyghome@gmail.com>
    Acked-by: Eduard Zingerman <eddyz87@gmail.com>
    Link: https://lore.kernel.org/r/20260120-helper_proto-v3-1-27b0180b4e77@gmail.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -1022,7 +1022,7 @@ const struct bpf_func_proto bpf_snprintf_btf_proto = {
 	.func		= bpf_snprintf_btf,
 	.gpl_only	= false,
 	.ret_type	= RET_INTEGER,
-	.arg1_type	= ARG_PTR_TO_MEM,
+	.arg1_type	= ARG_PTR_TO_MEM | MEM_WRITE,
 	.arg2_type	= ARG_CONST_SIZE,
 	.arg3_type	= ARG_PTR_TO_MEM | MEM_RDONLY,
 	.arg4_type	= ARG_CONST_SIZE,
@@ -1526,7 +1526,7 @@ static const struct bpf_func_proto bpf_read_branch_records_proto = {
 	.gpl_only       = true,
 	.ret_type       = RET_INTEGER,
 	.arg1_type      = ARG_PTR_TO_CTX,
-	.arg2_type      = ARG_PTR_TO_MEM_OR_NULL,
+	.arg2_type      = ARG_PTR_TO_MEM_OR_NULL | MEM_WRITE,
 	.arg3_type      = ARG_CONST_SIZE_OR_ZERO,
 	.arg4_type      = ARG_ANYTHING,
 };
@@ -1661,7 +1661,7 @@ static const struct bpf_func_proto bpf_get_stack_proto_raw_tp = {
 	.gpl_only	= true,
 	.ret_type	= RET_INTEGER,
 	.arg1_type	= ARG_PTR_TO_CTX,
-	.arg2_type	= ARG_PTR_TO_MEM | MEM_RDONLY,
+	.arg2_type	= ARG_PTR_TO_UNINIT_MEM,
 	.arg3_type	= ARG_CONST_SIZE_OR_ZERO,
 	.arg4_type	= ARG_ANYTHING,
 };
