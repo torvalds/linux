945fc28a06a1d30315ca416167754e10208024a5
Dhruva Gole <d-gole@ti.com>
2026-01-20 17:17:30 +0530

    cpufreq: dt-platdev: Add ti,am62l3 to blocklist
    Add AM62L3 SoC to the dt-platdev blocklist to ensure proper handling
    of CPUFreq functionality. The AM62L3 will use its native TI CPUFreq
    driver implementation instead of the generic dt-platdev driver.
    
    This follows the same pattern as other TI SoCs like AM62A7, AM62D2,
    and AM62P5 which have been previously added to this blocklist.
    
    Reviewed-by: Kendall Willis <k-willis@ti.com>
    Signed-off-by: Dhruva Gole <d-gole@ti.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -196,6 +196,7 @@ static const struct of_device_id blocklist[] __initconst = {
 	{ .compatible = "ti,am625", },
 	{ .compatible = "ti,am62a7", },
 	{ .compatible = "ti,am62d2", },
+	{ .compatible = "ti,am62l3", },
 	{ .compatible = "ti,am62p5", },
 
 	{ .compatible = "qcom,ipq5332", },
