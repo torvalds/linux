85fc4be6d811372f8f9a2a131a092735418fdbf2
Menglong Dong <menglong8.dong@gmail.com>
2026-01-24 14:20:04 +0800

    bpftool: add fsession support
    Add BPF_TRACE_FSESSION to bpftool.
    
    Signed-off-by: Menglong Dong <dongml2@chinatelecom.cn>
    Link: https://lore.kernel.org/r/20260124062008.8657-10-dongml2@chinatelecom.cn
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/bpf/bpftool/common.c
+++ tools/bpf/bpftool/common.c
@@ -1191,6 +1191,7 @@ const char *bpf_attach_type_input_str(enum bpf_attach_type t)
 	case BPF_TRACE_FENTRY:			return "fentry";
 	case BPF_TRACE_FEXIT:			return "fexit";
 	case BPF_MODIFY_RETURN:			return "mod_ret";
+	case BPF_TRACE_FSESSION:		return "fsession";
 	case BPF_SK_REUSEPORT_SELECT:		return "sk_skb_reuseport_select";
 	case BPF_SK_REUSEPORT_SELECT_OR_MIGRATE:	return "sk_skb_reuseport_select_or_migrate";
 	default:	return libbpf_bpf_attach_type_str(t);
