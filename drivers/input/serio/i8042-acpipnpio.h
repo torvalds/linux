2934325f56150ad8dab8ab92cbe2997242831396
feng <alec.jiang@gmail.com>
2026-01-24 21:44:12 -0800

    Input: i8042 - add quirk for ASUS Zenbook UX425QA_UM425QA
    The ASUS Zenbook UX425QA_UM425QA fails to initialize the keyboard after
    a cold boot.
    
    A quirk already exists for "ZenBook UX425", but some Zenbooks report
    "Zenbook" with a lowercase 'b'. Since DMI matching is case-sensitive,
    the existing quirk is not applied to these "extra special" Zenbooks.
    
    Testing confirms that this model needs the same quirks as the ZenBook
    UX425 variants.
    
    Signed-off-by: feng <alec.jiang@gmail.com>
    Link: https://patch.msgid.link/20260122013957.11184-1-alec.jiang@gmail.com
    Cc: stable@vger.kernel.org
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/serio/i8042-acpipnpio.h
+++ drivers/input/serio/i8042-acpipnpio.h
@@ -116,6 +116,17 @@ static const struct dmi_system_id i8042_dmi_quirk_table[] __initconst = {
 		.driver_data = (void *)(SERIO_QUIRK_NOMUX | SERIO_QUIRK_RESET_NEVER)
 	},
 	{
+		/*
+		 * ASUS Zenbook UX425QA_UM425QA
+		 * Some Zenbooks report "Zenbook" with a lowercase b.
+		 */
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
+			DMI_MATCH(DMI_PRODUCT_NAME, "Zenbook UX425QA_UM425QA"),
+		},
+		.driver_data = (void *)(SERIO_QUIRK_PROBE_DEFER | SERIO_QUIRK_RESET_NEVER)
+	},
+	{
 		/* ASUS ZenBook UX425UA/QA */
 		.matches = {
 			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
