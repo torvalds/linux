0e6b7eae1fded85f94a357d6132f07d64c614cfa
Andrey Albershteyn <aalbersh@kernel.org>
2026-01-26 12:56:57 +0100

    fs: add FS_XFLAG_VERITY for fs-verity files
    fs-verity introduced inode flag for inodes with enabled fs-verity on
    them. This patch adds FS_XFLAG_VERITY file attribute which can be
    retrieved with FS_IOC_FSGETXATTR ioctl() and file_getattr() syscall.
    
    This flag is read-only and can not be set with corresponding set ioctl()
    and file_setattr(). The FS_IOC_SETFLAGS requires file to be opened for
    writing which is not allowed for verity files. The FS_IOC_FSSETXATTR and
    file_setattr() clears this flag from the user input.
    
    As this is now common flag for both flag interfaces (flags/xflags) add
    it to overlapping flags list to exclude it from overwrite.
    
    Signed-off-by: Andrey Albershteyn <aalbersh@kernel.org>
    Link: https://patch.msgid.link/20260126115658.27656-2-aalbersh@kernel.org
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- fs/file_attr.c
+++ fs/file_attr.c
@@ -36,6 +36,8 @@ void fileattr_fill_xflags(struct file_kattr *fa, u32 xflags)
 		fa->flags |= FS_DAX_FL;
 	if (fa->fsx_xflags & FS_XFLAG_PROJINHERIT)
 		fa->flags |= FS_PROJINHERIT_FL;
+	if (fa->fsx_xflags & FS_XFLAG_VERITY)
+		fa->flags |= FS_VERITY_FL;
 }
 EXPORT_SYMBOL(fileattr_fill_xflags);
 
@@ -66,6 +68,8 @@ void fileattr_fill_flags(struct file_kattr *fa, u32 flags)
 		fa->fsx_xflags |= FS_XFLAG_DAX;
 	if (fa->flags & FS_PROJINHERIT_FL)
 		fa->fsx_xflags |= FS_XFLAG_PROJINHERIT;
+	if (fa->flags & FS_VERITY_FL)
+		fa->fsx_xflags |= FS_XFLAG_VERITY;
 }
 EXPORT_SYMBOL(fileattr_fill_flags);
 
