ccad8c1336b6511e3c7ca5c02f797b1fd2cf67e1
Deepak Gupta <debug@rivosinc.com>
2026-01-25 21:09:56 -0700

    arch/riscv: add dual vdso creation logic and select vdso based on hw
    Shadow stack instructions are taken from the Zimop ISA extension,
    which is mandated on RVA23. Any userspace with shadow stack
    instructions in it will fault on hardware that doesn't have support
    for Zimop.  Thus, a shadow stack-enabled userspace can't be run on
    hardware that doesn't support Zimop.
    
    It's not known how Linux userspace providers will respond to this kind
    of binary fragmentation.  In order to keep kernel portable across
    different hardware, 'arch/riscv/kernel/vdso_cfi' is created which has
    Makefile logic to compile 'arch/riscv/kernel/vdso' sources with CFI
    flags, and 'arch/riscv/kernel/vdso.c' is modified to select the
    appropriate vdso depending on whether the underlying CPU implements
    the Zimop extension. Since the offset of vdso symbols will change due
    to having two different vdso binaries, there is added logic to include
    a new generated vdso offset header and dynamically select the offset
    (like for rt_sigreturn).
    
    Signed-off-by: Deepak Gupta <debug@rivosinc.com>
    Acked-by: Charles Mirabile <cmirabil@redhat.com>
    Tested-by: Andreas Korb <andreas.korb@aisec.fraunhofer.de> # QEMU, custom CVA6
    Tested-by: Valentin Haudiquet <valentin.haudiquet@canonical.com>
    Link: https://patch.msgid.link/20251112-v5_user_cfi_series-v23-24-b55691eacf4f@rivosinc.com
    [pjw@kernel.org: cleaned up patch description]
    Signed-off-by: Paul Walmsley <pjw@kernel.org>

--- arch/riscv/kernel/vdso/gen_vdso_offsets.sh
+++ arch/riscv/kernel/vdso/gen_vdso_offsets.sh
@@ -2,4 +2,6 @@
 # SPDX-License-Identifier: GPL-2.0
 
 LC_ALL=C
-sed -n -e 's/^[0]\+\(0[0-9a-fA-F]*\) . \(__vdso_[a-zA-Z0-9_]*\)$/\#define \2_offset\t0x\1/p'
+SUFFIX=${1:-""}
+sed -n -e \
+'s/^[0]\+\(0[0-9a-fA-F]*\) . \(__vdso_[a-zA-Z0-9_]*\)$/\#define \2'$SUFFIX'_offset\t0x\1/p'
