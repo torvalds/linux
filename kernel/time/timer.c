a554a25e66efea0b78fb3d24f4f19289e037c0dc
Frederic Weisbecker <frederic@kernel.org>
2026-01-28 17:05:27 +0100

    cpufreq: ondemand: Simplify idle cputime granularity test
    cpufreq calls get_cpu_idle_time_us() just to know if idle cputime
    accounting has a nanoseconds granularity.
    
    Use the appropriate indicator instead to make that deduction.
    
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>
    Link: https://patch.msgid.link/aXozx0PXutnm8ECX@localhost.localdomain
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -281,7 +281,7 @@ DEFINE_STATIC_KEY_FALSE(timers_migration_enabled);
 
 static void timers_update_migration(void)
 {
-	if (sysctl_timer_migration && tick_nohz_active)
+	if (sysctl_timer_migration && tick_nohz_is_active())
 		static_branch_enable(&timers_migration_enabled);
 	else
 		static_branch_disable(&timers_migration_enabled);
