7a7d24a14a009691a1af6bfe45b210521635db5b
Le Qi <le.qi@oss.qualcomm.com>
2026-01-30 14:12:31 +0800

    soc: qcom: pd-mapper: Add QCS615 power domain mappings
    Add the QCS615 domain table to the in-kernel pd-mapper so that audio
    subsystems no longer rely on the userspace pd-mapper daemon.
    This enables proper initialization of ADSP and CDSP domains directly
    from the kernel.
    
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Signed-off-by: Le Qi <le.qi@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20260130061231.310113-1-le.qi@oss.qualcomm.com
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>

--- drivers/soc/qcom/qcom_pd_mapper.c
+++ drivers/soc/qcom/qcom_pd_mapper.c
@@ -401,6 +401,16 @@ static const struct qcom_pdm_domain_data *qcs404_domains[] = {
 	NULL,
 };
 
+static const struct qcom_pdm_domain_data *qcs615_domains[] = {
+	&adsp_audio_pd,
+	&adsp_root_pd,
+	&adsp_sensor_pd,
+	&cdsp_root_pd,
+	&mpss_root_pd,
+	&mpss_wlan_pd,
+	NULL,
+};
+
 static const struct qcom_pdm_domain_data *sc7180_domains[] = {
 	&adsp_audio_pd,
 	&adsp_root_pd_pdr,
@@ -572,6 +582,7 @@ static const struct of_device_id qcom_pdm_domains[] __maybe_unused = {
 	{ .compatible = "qcom,qcm2290", .data = qcm2290_domains, },
 	{ .compatible = "qcom,qcm6490", .data = sc7280_domains, },
 	{ .compatible = "qcom,qcs404", .data = qcs404_domains, },
+	{ .compatible = "qcom,qcs615", .data = qcs615_domains, },
 	{ .compatible = "qcom,sc7180", .data = sc7180_domains, },
 	{ .compatible = "qcom,sc7280", .data = sc7280_domains, },
 	{ .compatible = "qcom,sc8180x", .data = sc8180x_domains, },
