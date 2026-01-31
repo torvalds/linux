c8f62f73bbced3a79894655bdb0b625462d956fc
Duoming Zhou <duoming@zju.edu.cn>
2026-01-31 10:47:31 +0800

    wifi: mt76: mt7996: fix use-after-free bugs in mt7996_mac_dump_work()
    When the mt7996 pci chip is detaching, the mt7996_crash_data is
    released in mt7996_coredump_unregister(). However, the work item
    dump_work may still be running or pending, leading to UAF bugs
    when the already freed crash_data is dereferenced again in
    mt7996_mac_dump_work().
    
    The race condition can occur as follows:
    
    CPU 0 (removal path)               | CPU 1 (workqueue)
    mt7996_pci_remove()                | mt7996_sys_recovery_set()
     mt7996_unregister_device()        |  mt7996_reset()
      mt7996_coredump_unregister()     |   queue_work()
       vfree(dev->coredump.crash_data) | mt7996_mac_dump_work()
                                       |  crash_data-> // UAF
    
    Fix this by ensuring dump_work is properly canceled before
    the crash_data is deallocated. Add cancel_work_sync() in
    mt7996_unregister_device() to synchronize with any pending
    or executing dump work.
    
    Fixes: 878161d5d4a4 ("wifi: mt76: mt7996: enable coredump support")
    Signed-off-by: Duoming Zhou <duoming@zju.edu.cn>
    Link: https://patch.msgid.link/20260131024731.18741-1-duoming@zju.edu.cn
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7996/init.c
+++ drivers/net/wireless/mediatek/mt76/mt7996/init.c
@@ -1773,6 +1773,7 @@ error:
 
 void mt7996_unregister_device(struct mt7996_dev *dev)
 {
+	cancel_work_sync(&dev->dump_work);
 	cancel_work_sync(&dev->wed_rro.work);
 	mt7996_unregister_phy(mt7996_phy3(dev));
 	mt7996_unregister_phy(mt7996_phy2(dev));
