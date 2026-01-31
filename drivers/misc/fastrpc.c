6a502776f4a4f80fb839b22f12aeaf0267fca344
Xingjing Deng <micro6947@gmail.com>
2026-01-31 14:55:39 +0800

    misc: fastrpc: check qcom_scm_assign_mem() return in rpmsg_probe
    In the SDSP probe path, qcom_scm_assign_mem() is used to assign the
    reserved memory to the configured VMIDs, but its return value was not checked.
    
    Fail the probe if the SCM call fails to avoid continuing with an
    unexpected/incorrect memory permission configuration.
    
    This issue was found by an in-house analysis workflow that extracts AST-based
    information and runs static checks, with LLM assistance for triage, and was
    confirmed by manual code review.
    No hardware testing was performed.
    
    Fixes: c3c0363bc72d4 ("misc: fastrpc: support complete DMA pool access to the DSP")
    Cc: stable@vger.kernel.org # 6.11-rc1
    Signed-off-by: Xingjing Deng <xjdeng@buaa.edu.cn>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Link: https://patch.msgid.link/20260131065539.2124047-1-xjdeng@buaa.edu.cn
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/fastrpc.c
+++ drivers/misc/fastrpc.c
@@ -2390,8 +2390,10 @@ static int fastrpc_rpmsg_probe(struct rpmsg_device *rpdev)
 		if (!err) {
 			src_perms = BIT(QCOM_SCM_VMID_HLOS);
 
-			qcom_scm_assign_mem(res.start, resource_size(&res), &src_perms,
+			err = qcom_scm_assign_mem(res.start, resource_size(&res), &src_perms,
 				    data->vmperms, data->vmcount);
+			if (err)
+				goto err_free_data;
 		}
 
 	}
