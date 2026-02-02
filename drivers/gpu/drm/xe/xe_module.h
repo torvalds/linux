1acec6ef0511b92e7974cc5a8768bfd3a659feaf
Shuicheng Lin <shuicheng.lin@intel.com>
2026-02-02 18:18:54 +0000

    drm/xe: Make xe_modparam.force_vram_bar_size signed
    vram_bar_size is registered as an int module parameter and is documented
    to accept negative values to disable BAR resizing.
    Store it as an int in xe_modparam as well, so negative values work as
    intended and the module_param type matches.
    
    Fixes: 80742a1aa26e ("drm/xe: Allow to drop vram resizing")
    Reviewed-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Signed-off-by: Shuicheng Lin <shuicheng.lin@intel.com>
    Link: https://patch.msgid.link/20260202181853.1095736-2-shuicheng.lin@intel.com
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    (cherry picked from commit 25c9aa4dcb5ef2ad9f354d19f8f1eeb690d1c161)
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/xe/xe_module.h
+++ drivers/gpu/drm/xe/xe_module.h
@@ -12,7 +12,7 @@
 struct xe_modparam {
 	bool force_execlist;
 	bool probe_display;
-	u32 force_vram_bar_size;
+	int force_vram_bar_size;
 	int guc_log_level;
 	char *guc_firmware_path;
 	char *huc_firmware_path;
