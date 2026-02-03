ff4a46c278ac6a4b3f39be1492a4568b6dcc6105
Linus Walleij <linusw@kernel.org>
2026-02-03 11:23:01 +0100

    ata: pata_ftide010: Fix some DMA timings
    The FTIDE010 has been missing some timing settings since its
    inception, since the upstream OpenWrt patch was missing these.
    
    The community has since come up with the appropriate timings.
    
    Fixes: be4e456ed3a5 ("ata: Add driver for Faraday Technology FTIDE010")
    Cc: stable@vger.kernel.org
    Signed-off-by: Linus Walleij <linusw@kernel.org>
    Signed-off-by: Niklas Cassel <cassel@kernel.org>

--- drivers/ata/pata_ftide010.c
+++ drivers/ata/pata_ftide010.c
@@ -122,10 +122,10 @@ static const u8 mwdma_50_active_time[3] = {6, 2, 2};
 static const u8 mwdma_50_recovery_time[3] = {6, 2, 1};
 static const u8 mwdma_66_active_time[3] = {8, 3, 3};
 static const u8 mwdma_66_recovery_time[3] = {8, 2, 1};
-static const u8 udma_50_setup_time[6] = {3, 3, 2, 2, 1, 1};
+static const u8 udma_50_setup_time[6] = {3, 3, 2, 2, 1, 9};
 static const u8 udma_50_hold_time[6] = {3, 1, 1, 1, 1, 1};
-static const u8 udma_66_setup_time[7] = {4, 4, 3, 2, };
-static const u8 udma_66_hold_time[7] = {};
+static const u8 udma_66_setup_time[7] = {4, 4, 3, 2, 1, 9, 9};
+static const u8 udma_66_hold_time[7] = {4, 2, 1, 1, 1, 1, 1};
 
 /*
  * We set 66 MHz for all MWDMA modes
