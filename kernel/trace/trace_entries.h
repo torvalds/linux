033c55fe2e326bea022c3cc5178ecf3e0e459b82
Steven Rostedt <rostedt@goodmis.org>
2026-02-04 11:36:28 -0500

    tracing: Fix ftrace event field alignments
    The fields of ftrace specific events (events used to save ftrace internal
    events like function traces and trace_printk) are generated similarly to
    how normal trace event fields are generated. That is, the fields are added
    to a trace_events_fields array that saves the name, offset, size,
    alignment and signness of the field. It is used to produce the output in
    the format file in tracefs so that tooling knows how to parse the binary
    data of the trace events.
    
    The issue is that some of the ftrace event structures are packed. The
    function graph exit event structures are one of them. The 64 bit calltime
    and rettime fields end up 4 byte aligned, but the algorithm to show to
    userspace shows them as 8 byte aligned.
    
    The macros that create the ftrace events has one for embedded structure
    fields. There's two macros for theses fields:
    
      __field_desc() and __field_packed()
    
    The difference of the latter macro is that it treats the field as packed.
    
    Rename that field to __field_desc_packed() and create replace the
    __field_packed() to be a normal field that is packed and have the calltime
    and rettime use those.
    
    This showed up on 32bit architectures for function graph time fields. It
    had:
    
     ~# cat /sys/kernel/tracing/events/ftrace/funcgraph_exit/format
    [..]
            field:unsigned long func;       offset:8;       size:4; signed:0;
            field:unsigned int depth;       offset:12;      size:4; signed:0;
            field:unsigned int overrun;     offset:16;      size:4; signed:0;
            field:unsigned long long calltime;      offset:24;      size:8; signed:0;
            field:unsigned long long rettime;       offset:32;      size:8; signed:0;
    
    Notice that overrun is at offset 16 with size 4, where in the structure
    calltime is at offset 20 (16 + 4), but it shows the offset at 24. That's
    because it used the alignment of unsigned long long when used as a
    declaration and not as a member of a structure where it would be aligned
    by word size (in this case 4).
    
    By using the proper structure alignment, the format has it at the correct
    offset:
    
     ~# cat /sys/kernel/tracing/events/ftrace/funcgraph_exit/format
    [..]
            field:unsigned long func;       offset:8;       size:4; signed:0;
            field:unsigned int depth;       offset:12;      size:4; signed:0;
            field:unsigned int overrun;     offset:16;      size:4; signed:0;
            field:unsigned long long calltime;      offset:20;      size:8; signed:0;
            field:unsigned long long rettime;       offset:28;      size:8; signed:0;
    
    Cc: stable@vger.kernel.org
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Reported-by: "jempty.liang" <imntjempty@163.com>
    Link: https://patch.msgid.link/20260204113628.53faec78@gandalf.local.home
    Fixes: 04ae87a52074e ("ftrace: Rework event_create_dir()")
    Closes: https://lore.kernel.org/all/20260130015740.212343-1-imntjempty@163.com/
    Closes: https://lore.kernel.org/all/20260202123342.2544795-1-imntjempty@163.com/
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- kernel/trace/trace_entries.h
+++ kernel/trace/trace_entries.h
@@ -79,8 +79,8 @@ FTRACE_ENTRY(funcgraph_entry, ftrace_graph_ent_entry,
 
 	F_STRUCT(
 		__field_struct(	struct ftrace_graph_ent,	graph_ent	)
-		__field_packed(	unsigned long,	graph_ent,	func		)
-		__field_packed(	unsigned long,	graph_ent,	depth		)
+		__field_desc_packed(unsigned long,	graph_ent,	func	)
+		__field_desc_packed(unsigned long,	graph_ent,	depth	)
 		__dynamic_array(unsigned long,	args				)
 	),
 
@@ -96,9 +96,9 @@ FTRACE_ENTRY_PACKED(fgraph_retaddr_entry, fgraph_retaddr_ent_entry,
 
 	F_STRUCT(
 		__field_struct(	struct fgraph_retaddr_ent,	graph_rent	)
-		__field_packed(	unsigned long,	graph_rent.ent,	func		)
-		__field_packed(	unsigned long,	graph_rent.ent,	depth		)
-		__field_packed(	unsigned long,	graph_rent,	retaddr		)
+		__field_desc_packed(	unsigned long,	graph_rent.ent,	func	)
+		__field_desc_packed(	unsigned long,	graph_rent.ent,	depth	)
+		__field_desc_packed(	unsigned long,	graph_rent,	retaddr	)
 		__dynamic_array(unsigned long,	args				)
 	),
 
@@ -123,12 +123,12 @@ FTRACE_ENTRY_PACKED(funcgraph_exit, ftrace_graph_ret_entry,
 
 	F_STRUCT(
 		__field_struct(	struct ftrace_graph_ret,	ret	)
-		__field_packed(	unsigned long,	ret,		func	)
-		__field_packed(	unsigned long,	ret,		retval	)
-		__field_packed(	unsigned int,	ret,		depth	)
-		__field_packed(	unsigned int,	ret,		overrun	)
-		__field(unsigned long long,	calltime		)
-		__field(unsigned long long,	rettime			)
+		__field_desc_packed(	unsigned long,	ret,	func	)
+		__field_desc_packed(	unsigned long,	ret,	retval	)
+		__field_desc_packed(	unsigned int,	ret,	depth	)
+		__field_desc_packed(	unsigned int,	ret,	overrun	)
+		__field_packed(unsigned long long,	calltime)
+		__field_packed(unsigned long long,	rettime	)
 	),
 
 	F_printk("<-- %ps (%u) (start: %llx  end: %llx) over: %u retval: %lx",
@@ -146,11 +146,11 @@ FTRACE_ENTRY_PACKED(funcgraph_exit, ftrace_graph_ret_entry,
 
 	F_STRUCT(
 		__field_struct(	struct ftrace_graph_ret,	ret	)
-		__field_packed(	unsigned long,	ret,		func	)
-		__field_packed(	unsigned int,	ret,		depth	)
-		__field_packed(	unsigned int,	ret,		overrun	)
-		__field(unsigned long long,	calltime		)
-		__field(unsigned long long,	rettime			)
+		__field_desc_packed(	unsigned long,	ret,	func	)
+		__field_desc_packed(	unsigned int,	ret,	depth	)
+		__field_desc_packed(	unsigned int,	ret,	overrun	)
+		__field_packed(unsigned long long,	calltime	)
+		__field_packed(unsigned long long,	rettime		)
 	),
 
 	F_printk("<-- %ps (%u) (start: %llx  end: %llx) over: %u",
