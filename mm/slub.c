144080a5823b2dbd635acb6decf7ab23182664f3
Harry Yoo <harry.yoo@oracle.com>
2026-02-10 17:18:59 +0900

    mm/slab: do not access current->mems_allowed_seq if !allow_spin
    Lockdep complains when get_from_any_partial() is called in an NMI
    context, because current->mems_allowed_seq is seqcount_spinlock_t and
    not NMI-safe:
    
      ================================
      WARNING: inconsistent lock state
      6.19.0-rc5-kfree-rcu+ #315 Tainted: G                 N
      --------------------------------
      inconsistent {INITIAL USE} -> {IN-NMI} usage.
      kunit_try_catch/9989 [HC1[1]:SC0[0]:HE0:SE1] takes:
      ffff889085799820 (&____s->seqcount#3){.-.-}-{0:0}, at: ___slab_alloc+0x58f/0xc00
      {INITIAL USE} state was registered at:
        lock_acquire+0x185/0x320
        kernel_init_freeable+0x391/0x1150
        kernel_init+0x1f/0x220
        ret_from_fork+0x736/0x8f0
        ret_from_fork_asm+0x1a/0x30
      irq event stamp: 56
      hardirqs last  enabled at (55): [<ffffffff850a68d7>] _raw_spin_unlock_irq+0x27/0x70
      hardirqs last disabled at (56): [<ffffffff850858ca>] __schedule+0x2a8a/0x6630
      softirqs last  enabled at (0): [<ffffffff81536711>] copy_process+0x1dc1/0x6a10
      softirqs last disabled at (0): [<0000000000000000>] 0x0
    
      other info that might help us debug this:
       Possible unsafe locking scenario:
    
             CPU0
             ----
        lock(&____s->seqcount#3);
        <Interrupt>
          lock(&____s->seqcount#3);
    
       *** DEADLOCK ***
    
    According to Documentation/locking/seqlock.rst, seqcount_t is not
    NMI-safe and seqcount_latch_t should be used when read path can interrupt
    the write-side critical section. In this case, do not access
    current->mems_allowed_seq and avoid retry.
    
    Fixes: af92793e52c3 ("slab: Introduce kmalloc_nolock() and kfree_nolock().")
    Cc: stable@vger.kernel.org
    Signed-off-by: Harry Yoo <harry.yoo@oracle.com>
    Link: https://patch.msgid.link/20260210081900.329447-2-harry.yoo@oracle.com
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>

--- mm/slub.c
+++ mm/slub.c
@@ -3791,6 +3791,7 @@ static void *get_from_any_partial(struct kmem_cache *s, struct partial_context *
 	struct zone *zone;
 	enum zone_type highest_zoneidx = gfp_zone(pc->flags);
 	unsigned int cpuset_mems_cookie;
+	bool allow_spin = gfpflags_allow_spinning(pc->flags);
 
 	/*
 	 * The defrag ratio allows a configuration of the tradeoffs between
@@ -3815,7 +3816,15 @@ static void *get_from_any_partial(struct kmem_cache *s, struct partial_context *
 		return NULL;
 
 	do {
-		cpuset_mems_cookie = read_mems_allowed_begin();
+		/*
+		 * read_mems_allowed_begin() accesses current->mems_allowed_seq,
+		 * a seqcount_spinlock_t that is not NMI-safe. Do not access
+		 * current->mems_allowed_seq and avoid retry when GFP flags
+		 * indicate spinning is not allowed.
+		 */
+		if (allow_spin)
+			cpuset_mems_cookie = read_mems_allowed_begin();
+
 		zonelist = node_zonelist(mempolicy_slab_node(), pc->flags);
 		for_each_zone_zonelist(zone, z, zonelist, highest_zoneidx) {
 			struct kmem_cache_node *n;
@@ -3839,7 +3848,7 @@ static void *get_from_any_partial(struct kmem_cache *s, struct partial_context *
 				}
 			}
 		}
-	} while (read_mems_allowed_retry(cpuset_mems_cookie));
+	} while (allow_spin && read_mems_allowed_retry(cpuset_mems_cookie));
 #endif	/* CONFIG_NUMA */
 	return NULL;
 }
