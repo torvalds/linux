2692c614f8f05929d692b3dbfd3faef1f00fbaf0
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2026-02-10 14:58:22 +0100

    device property: Allow secondary lookup in fwnode_get_next_child_node()
    When device_get_child_node_count() got split to the fwnode and device
    respective APIs, the fwnode didn't inherit the ability to traverse over
    the secondary fwnode. Hence any user, that switches from device to fwnode
    API misses this feature. In particular, this was revealed by the commit
    1490cbb9dbfd ("device property: Split fwnode_get_child_node_count()")
    that effectively broke the GPIO enumeration on Intel Galileo boards.
    Fix this by moving the secondary lookup from device to fwnode API.
    
    Note, in general no device_*() API should go into the depth of the fwnode
    implementation.
    
    Fixes: 114dbb4fa7c4 ("drivers property: When no children in primary, try secondary")
    Cc: stable@vger.kernel.org
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Rafael J. Wysocki (Intel) <rafael@kernel.org>
    Reviewed-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Link: https://patch.msgid.link/20260210135822.47335-1-andriy.shevchenko@linux.intel.com
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- drivers/base/property.c
+++ drivers/base/property.c
@@ -797,7 +797,18 @@ struct fwnode_handle *
 fwnode_get_next_child_node(const struct fwnode_handle *fwnode,
 			   struct fwnode_handle *child)
 {
-	return fwnode_call_ptr_op(fwnode, get_next_child_node, child);
+	struct fwnode_handle *next;
+
+	if (IS_ERR_OR_NULL(fwnode))
+		return NULL;
+
+	/* Try to find a child in primary fwnode */
+	next = fwnode_call_ptr_op(fwnode, get_next_child_node, child);
+	if (next)
+		return next;
+
+	/* When no more children in primary, continue with secondary */
+	return fwnode_call_ptr_op(fwnode->secondary, get_next_child_node, child);
 }
 EXPORT_SYMBOL_GPL(fwnode_get_next_child_node);
 
@@ -841,19 +852,7 @@ EXPORT_SYMBOL_GPL(fwnode_get_next_available_child_node);
 struct fwnode_handle *device_get_next_child_node(const struct device *dev,
 						 struct fwnode_handle *child)
 {
-	const struct fwnode_handle *fwnode = dev_fwnode(dev);
-	struct fwnode_handle *next;
-
-	if (IS_ERR_OR_NULL(fwnode))
-		return NULL;
-
-	/* Try to find a child in primary fwnode */
-	next = fwnode_get_next_child_node(fwnode, child);
-	if (next)
-		return next;
-
-	/* When no more children in primary, continue with secondary */
-	return fwnode_get_next_child_node(fwnode->secondary, child);
+	return fwnode_get_next_child_node(dev_fwnode(dev), child);
 }
 EXPORT_SYMBOL_GPL(device_get_next_child_node);
 
