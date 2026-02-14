3c75e6a5da3c0dfcd34e5f9df5390804179f2aeb
Sean Christopherson <seanjc@google.com>
2026-02-13 17:26:48 -0800

    KVM: VMX: Move architectural "vmcs" and "vmcs_hdr" structures to public vmx.h
    Move "struct vmcs" and "struct vmcs_hdr" to asm/vmx.h in anticipation of
    moving VMXON/VMXOFF to the core kernel (VMXON requires a "root" VMCS with
    the appropriate revision ID in its header).
    
    No functional change intended.
    
    Tested-by: Chao Gao <chao.gao@intel.com>
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Tested-by: Sagi Shahar <sagis@google.com>
    Link: https://patch.msgid.link/20260214012702.2368778-3-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/include/asm/vmx.h
+++ arch/x86/include/asm/vmx.h
@@ -20,6 +20,17 @@
 #include <asm/trapnr.h>
 #include <asm/vmxfeatures.h>
 
+struct vmcs_hdr {
+	u32 revision_id:31;
+	u32 shadow_vmcs:1;
+};
+
+struct vmcs {
+	struct vmcs_hdr hdr;
+	u32 abort;
+	char data[];
+};
+
 #define VMCS_CONTROL_BIT(x)	BIT(VMX_FEATURE_##x & 0x1f)
 
 /*
