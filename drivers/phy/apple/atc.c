290a35756aaef85bbe0527eaf451f533a61b5f6c
Janne Grunau <j@jannau.net>
2026-02-15 09:02:51 +0100

    phy: apple: apple: Use local variable for ioremap return value
    The indirection through the resources array is unnecessarily complicated
    and resuling in using IS_ERR() and PTR_ERR() on a valid address. A local
    variable for the devm_ioremap_resource() return value is both easier to
    read and matches expectations when reading code.
    
    Reported-by: Dan Carpenter <dan.carpenter@linaro.org>
    Closes: https://lore.kernel.org/asahi/aYXvX1bYOXtYCgfC@stanley.mountain/
    Suggested-by: Vladimir Oltean <olteanv@gmail.com>
    Fixes: 8e98ca1e74db ("phy: apple: Add Apple Type-C PHY")
    Signed-off-by: Janne Grunau <j@jannau.net>
    Reviewed-by: Sven Peter <sven@kernel.org>
    Reviewed-by: Vladimir Oltean <olteanv@gmail.com>
    Link: https://patch.msgid.link/20260215-phy-apple-resource-err-ptr-v2-1-e43c22453682@jannau.net
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/phy/apple/atc.c
+++ drivers/phy/apple/atc.c
@@ -2202,14 +2202,16 @@ static int atcphy_map_resources(struct platform_device *pdev, struct apple_atcph
 		{ "pipehandler", &atcphy->regs.pipehandler, NULL },
 	};
 	struct resource *res;
+	void __iomem *addr;
 
 	for (int i = 0; i < ARRAY_SIZE(resources); i++) {
 		res = platform_get_resource_byname(pdev, IORESOURCE_MEM, resources[i].name);
-		*resources[i].addr = devm_ioremap_resource(&pdev->dev, res);
-		if (IS_ERR(resources[i].addr))
-			return dev_err_probe(atcphy->dev, PTR_ERR(resources[i].addr),
+		addr = devm_ioremap_resource(&pdev->dev, res);
+		if (IS_ERR(addr))
+			return dev_err_probe(atcphy->dev, PTR_ERR(addr),
 					     "Unable to map %s regs", resources[i].name);
 
+		*resources[i].addr = addr;
 		if (resources[i].res)
 			*resources[i].res = res;
 	}
