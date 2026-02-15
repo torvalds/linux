38d581ba76545b4712ecdb63f2b2612d5b7601c9
Jaime Saguillo Revilla <jaime.saguillo@gmail.com>
2026-02-15 12:45:11 +0000

    proc: array: drop stale FIXME about RCU in task_sig()
    task_sig() already wraps the SigQ rlimit read in an explicit RCU read-side
    critical section.  Drop the stale FIXME comment and keep using
    task_ucounts() for the ucounts access.
    
    No functional change.
    
    Link: https://lkml.kernel.org/r/20260215124511.14227-1-jaime.saguillo@gmail.com
    Signed-off-by: Jaime Saguillo Revilla <jaime.saguillo@gmail.com>
    Acked-by: Oleg Nesterov <oleg@redhat.com>
    Cc: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/proc/array.c
+++ fs/proc/array.c
@@ -280,7 +280,7 @@ static inline void task_sig(struct seq_file *m, struct task_struct *p)
 		blocked = p->blocked;
 		collect_sigign_sigcatch(p, &ignored, &caught);
 		num_threads = get_nr_threads(p);
-		rcu_read_lock();  /* FIXME: is this correct? */
+		rcu_read_lock();
 		qsize = get_rlimit_value(task_ucounts(p), UCOUNT_RLIMIT_SIGPENDING);
 		rcu_read_unlock();
 		qlim = task_rlimit(p, RLIMIT_SIGPENDING);
