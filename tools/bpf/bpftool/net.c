3b39d73cc3379360a33eb583b17f21fe55e1288e
Jakub Kicinski <kuba@kernel.org>
2026-02-17 11:41:50 -0800

    bpftool: Fix truncated netlink dumps
    Netlink requires that the recv buffer used during dumps is at least
    min(PAGE_SIZE, 8k) (see the man page). Otherwise the messages will
    get truncated. Make sure bpftool follows this requirement, avoid
    missing information on systems with large pages.
    
    Acked-by: Quentin Monnet <qmo@kernel.org>
    Fixes: 7084566a236f ("tools/bpftool: Remove libbpf_internal.h usage in bpftool")
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Link: https://lore.kernel.org/r/20260217194150.734701-1-kuba@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/bpf/bpftool/net.c
+++ tools/bpf/bpftool/net.c
@@ -156,7 +156,7 @@ static int netlink_recv(int sock, __u32 nl_pid, __u32 seq,
 	bool multipart = true;
 	struct nlmsgerr *err;
 	struct nlmsghdr *nh;
-	char buf[4096];
+	char buf[8192];
 	int len, ret;
 
 	while (multipart) {
@@ -201,6 +201,9 @@ static int netlink_recv(int sock, __u32 nl_pid, __u32 seq,
 					return ret;
 			}
 		}
+
+		if (len)
+			p_err("Invalid message or trailing data in Netlink response: %d bytes left", len);
 	}
 	ret = 0;
 done:
