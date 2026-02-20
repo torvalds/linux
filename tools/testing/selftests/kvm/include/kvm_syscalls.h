58f5d8eebd5c6b0c9377391d6b7bf9d321e014cc
Ackerley Tng <ackerleytng@google.com>
2026-02-20 23:54:35 +0000

    KVM: selftests: Wrap madvise() to assert success
    Extend kvm_syscalls.h to wrap madvise() to assert success. This will be
    used in the next patch.
    
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Reviewed-by: David Hildenbrand (Arm) <david@kernel.org>
    Link: https://patch.msgid.link/455483ca29a3a3042efee0cf3bbd0e2548cbeb1c.1771630983.git.ackerleytng@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- tools/testing/selftests/kvm/include/kvm_syscalls.h
+++ tools/testing/selftests/kvm/include/kvm_syscalls.h
@@ -77,5 +77,6 @@ __KVM_SYSCALL_DEFINE(munmap, 2, void *, mem, size_t, size);
 __KVM_SYSCALL_DEFINE(close, 1, int, fd);
 __KVM_SYSCALL_DEFINE(fallocate, 4, int, fd, int, mode, loff_t, offset, loff_t, len);
 __KVM_SYSCALL_DEFINE(ftruncate, 2, unsigned int, fd, off_t, length);
+__KVM_SYSCALL_DEFINE(madvise, 3, void *, addr, size_t, length, int, advice);
 
 #endif /* SELFTEST_KVM_SYSCALLS_H */
