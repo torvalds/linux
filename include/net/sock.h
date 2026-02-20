983512f3a87fd8dc4c94dfa6b596b6e57df5aad7
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2026-02-20 19:38:58 +0100

    net: Drop the lock in skb_may_tx_timestamp()
    skb_may_tx_timestamp() may acquire sock::sk_callback_lock. The lock must
    not be taken in IRQ context, only softirq is okay. A few drivers receive
    the timestamp via a dedicated interrupt and complete the TX timestamp
    from that handler. This will lead to a deadlock if the lock is already
    write-locked on the same CPU.
    
    Taking the lock can be avoided. The socket (pointed by the skb) will
    remain valid until the skb is released. The ->sk_socket and ->file
    member will be set to NULL once the user closes the socket which may
    happen before the timestamp arrives.
    If we happen to observe the pointer while the socket is closing but
    before the pointer is set to NULL then we may use it because both
    pointer (and the file's cred member) are RCU freed.
    
    Drop the lock. Use READ_ONCE() to obtain the individual pointer. Add a
    matching WRITE_ONCE() where the pointer are cleared.
    
    Link: https://lore.kernel.org/all/20260205145104.iWinkXHv@linutronix.de
    Fixes: b245be1f4db1a ("net-timestamp: no-payload only sysctl")
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Reviewed-by: Jason Xing <kerneljasonxing@gmail.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Link: https://patch.msgid.link/20260220183858.N4ERjFW6@linutronix.de
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -2098,7 +2098,7 @@ static inline int sk_rx_queue_get(const struct sock *sk)
 
 static inline void sk_set_socket(struct sock *sk, struct socket *sock)
 {
-	sk->sk_socket = sock;
+	WRITE_ONCE(sk->sk_socket, sock);
 	if (sock) {
 		WRITE_ONCE(sk->sk_uid, SOCK_INODE(sock)->i_uid);
 		WRITE_ONCE(sk->sk_ino, SOCK_INODE(sock)->i_ino);
