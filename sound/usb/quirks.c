a8cc55bf81a45772cad44c83ea7bb0e98431094a
Geoffrey D. Bennett <g@b4.vu>
2026-02-21 02:34:48 +1030

    ALSA: usb-audio: Remove VALIDATE_RATES quirk for Focusrite devices
    Remove QUIRK_FLAG_VALIDATE_RATES for Focusrite. With the previous
    commit, focusrite_valid_sample_rate() produces correct rate tables
    without USB probing.
    
    QUIRK_FLAG_VALIDATE_RATES sends SET_CUR requests for each rate (~25ms
    each) and leaves the device at 192kHz. This is a problem because that
    rate: 1) disables the internal mixer, so outputs are silent until an
    application opens the PCM and sets a lower rate, and 2) the Air and
    Safe modes get disabled.
    
    Fixes: 5963e5262180 ("ALSA: usb-audio: Enable rate validation for Scarlett devices")
    Signed-off-by: Geoffrey D. Bennett <g@b4.vu>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Link: https://patch.msgid.link/09b9c012024c998c4ca14bd876ef0dce0d0b6101.1771594828.git.g@b4.vu

--- sound/usb/quirks.c
+++ sound/usb/quirks.c
@@ -2424,7 +2424,7 @@ static const struct usb_audio_quirk_flags_table quirk_flags_table[] = {
 	VENDOR_FLG(0x07fd, /* MOTU */
 		   QUIRK_FLAG_VALIDATE_RATES),
 	VENDOR_FLG(0x1235, /* Focusrite Novation */
-		   QUIRK_FLAG_VALIDATE_RATES),
+		   0),
 	VENDOR_FLG(0x1511, /* AURALiC */
 		   QUIRK_FLAG_DSD_RAW),
 	VENDOR_FLG(0x152a, /* Thesycon devices */
