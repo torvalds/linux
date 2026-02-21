800ca7b88a0c0eba6fa721e043eb3ed5b10ecc4f
Saket Dumbre <saket.dumbre@intel.com>
2026-02-21 12:36:39 +0100

    ACPICA: Update the _CPC definition to match ACPI 6.6
    Update the _CPC definition to also support return package sub-type of
    a Package (with Integer and Buffer) as per ACPI Spec 6.6.
    
    Link: https://github.com/acpica/acpica/commit/17a761944cc2
    Signed-off-by: Saket Dumbre <saket.dumbre@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://patch.msgid.link/2829238.mvXUDI8C0e@rafael.j.wysocki

--- drivers/acpi/acpica/acpredef.h
+++ drivers/acpi/acpica/acpredef.h
@@ -379,8 +379,9 @@ const union acpi_predefined_info acpi_gbl_predefined_methods[] = {
 
 	{{"_CPC", METHOD_0ARGS,
 	  METHOD_RETURNS(ACPI_RTYPE_PACKAGE)}},	/* Variable-length (Ints/Bufs) */
-	PACKAGE_INFO(ACPI_PTYPE1_VAR, ACPI_RTYPE_INTEGER | ACPI_RTYPE_BUFFER, 0,
-		     0, 0, 0),
+	PACKAGE_INFO(ACPI_PTYPE1_VAR,
+		     ACPI_RTYPE_INTEGER | ACPI_RTYPE_BUFFER |
+		     ACPI_RTYPE_PACKAGE, 0, 0, 0, 0),
 
 	{{"_CR3", METHOD_0ARGS,	/* ACPI 6.0 */
 	  METHOD_RETURNS(ACPI_RTYPE_INTEGER)}},
