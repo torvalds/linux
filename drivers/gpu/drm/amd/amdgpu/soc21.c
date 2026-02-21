389c2024cab817366e6b8345f679f41064fa94d6
sguttula <suresh.guttula@amd.com>
2026-02-21 10:47:59 +0530

    drm/amdgpu: Enable DPG support for VCN5
    This will set DPG flags for enabling power gating on GFX11_5_4
    
    Signed-off-by: sguttula <suresh.guttula@amd.com>
    Reviewed-by: Pratik Vishwakarma <Pratik.Vishwakarma@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit a503c266d70d3363ba6bffb883cd6ecdb092670c)

--- drivers/gpu/drm/amd/amdgpu/soc21.c
+++ drivers/gpu/drm/amd/amdgpu/soc21.c
@@ -858,7 +858,9 @@ static int soc21_common_early_init(struct amdgpu_ip_block *ip_block)
 			AMD_CG_SUPPORT_IH_CG |
 			AMD_CG_SUPPORT_BIF_MGCG |
 			AMD_CG_SUPPORT_BIF_LS;
-		adev->pg_flags = AMD_PG_SUPPORT_VCN |
+		adev->pg_flags = AMD_PG_SUPPORT_VCN_DPG |
+			AMD_PG_SUPPORT_VCN |
+			AMD_PG_SUPPORT_JPEG_DPG |
 			AMD_PG_SUPPORT_JPEG |
 			AMD_PG_SUPPORT_GFX_PG;
 		adev->external_rev_id = adev->rev_id + 0x1;
