2a3b4a8c10a64a62c4243007139d253dc1324dfd
Antheas Kapenekakis <lkml@antheas.dev>
2026-02-23 19:29:52 +0100

    platform/x86: oxpec: Add support for OneXPlayer X1 Air
    X1 Air is an X1 variant with a newer Intel chipset. It uses the same
    registers as the X1. Add a quirk for it to the oxpec driver.
    
    Signed-off-by: Antheas Kapenekakis <lkml@antheas.dev>
    Link: https://patch.msgid.link/20260223183004.2696892-4-lkml@antheas.dev
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>

--- drivers/platform/x86/oxpec.c
+++ drivers/platform/x86/oxpec.c
@@ -243,6 +243,13 @@ static const struct dmi_system_id dmi_table[] = {
 	{
 		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "ONE-NETBOOK"),
+			DMI_EXACT_MATCH(DMI_BOARD_NAME, "ONEXPLAYER X1Air"),
+		},
+		.driver_data = (void *)oxp_x1,
+	},
+	{
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "ONE-NETBOOK"),
 			DMI_EXACT_MATCH(DMI_BOARD_NAME, "ONEXPLAYER X1 mini"),
 		},
 		.driver_data = (void *)oxp_x1,
