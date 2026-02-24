671047943dce5af24e023bca3c5cc244d7565f5a
Thomas Gleixner <tglx@kernel.org>
2026-02-24 17:38:47 +0100

    rbtree: Provide rbtree with links
    Some RB tree users require quick access to the next and the previous node,
    e.g. to check whether a modification of the node results in a change of the
    nodes position in the tree. If the node position does not change, then the
    modification can happen in place without going through a full enqueue
    requeue cycle. A upcoming use case for this are the timer queues of the
    hrtimer subsystem as they can optimize for timers which are frequently
    rearmed while enqueued.
    
    This can be obviously achieved with rb_next() and rb_prev(), but those
    turned out to be quite expensive for hotpath operations depending on the
    tree depth.
    
    Add a linked RB tree variant where add() and erase() maintain the links
    between the nodes. Like the cached variant it provides a pointer to the
    left most node in the root.
    
    It intentionally does not use a [h]list head as there is no real need for
    true list operations as the list is strictly coupled to the tree and
    and cannot be manipulated independently.
    
    It sets the nodes previous pointer to NULL for the left most node and the
    next pointer to NULL for the right most node. This allows a quick check
    especially for the left most node without consulting the list head address,
    which creates better code.
    
    Aside of the rb_leftmost cached pointer this could trivially provide a
    rb_rightmost pointer as well, but there is no usage for that (yet).
    
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://patch.msgid.link/20260224163431.668401024@kernel.org

--- include/linux/rbtree_types.h
+++ include/linux/rbtree_types.h
@@ -9,6 +9,12 @@ struct rb_node {
 } __attribute__((aligned(sizeof(long))));
 /* The alignment might seem pointless, but allegedly CRIS needs it */
 
+struct rb_node_linked {
+	struct rb_node		node;
+	struct rb_node_linked	*prev;
+	struct rb_node_linked	*next;
+};
+
 struct rb_root {
 	struct rb_node *rb_node;
 };
@@ -28,7 +34,17 @@ struct rb_root_cached {
 	struct rb_node *rb_leftmost;
 };
 
+/*
+ * Leftmost tree with links. This would allow a trivial rb_rightmost update,
+ * but that has been omitted due to the lack of users.
+ */
+struct rb_root_linked {
+	struct rb_root		rb_root;
+	struct rb_node_linked	*rb_leftmost;
+};
+
 #define RB_ROOT (struct rb_root) { NULL, }
 #define RB_ROOT_CACHED (struct rb_root_cached) { {NULL, }, NULL }
+#define RB_ROOT_LINKED (struct rb_root_linked) { {NULL, }, NULL }
 
 #endif
