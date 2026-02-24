ab39cc4cb8ceecdc2b61747433e7237f1ac2b789
David Arcari <darcari@redhat.com>
2026-02-24 07:21:06 -0500

    cpufreq: intel_pstate: Fix NULL pointer dereference in update_cpu_qos_request()
    The update_cpu_qos_request() function attempts to initialize the 'freq'
    variable by dereferencing 'cpudata' before verifying if the 'policy'
    is valid.
    
    This issue occurs on systems booted with the "nosmt" parameter, where
    all_cpu_data[cpu] is NULL for the SMT sibling threads. As a result,
    any call to update_qos_requests() will result in a NULL pointer
    dereference as the code will attempt to access pstate.turbo_freq using
    the NULL cpudata pointer.
    
    Also, pstate.turbo_freq may be updated by intel_pstate_get_hwp_cap()
    after initializing the 'freq' variable, so it is better to defer the
    'freq' until intel_pstate_get_hwp_cap() has been called.
    
    Fix this by deferring the 'freq' assignment until after the policy and
    driver_data have been validated.
    
    Fixes: ae1bdd23b99f ("cpufreq: intel_pstate: Adjust frequency percentage computations")
    Reported-by: Jirka Hladky <jhladky@redhat.com>
    Closes: https://lore.kernel.org/all/CAE4VaGDfiPvz3AzrwrwM4kWB3SCkMci25nPO8W1JmTBd=xHzZg@mail.gmail.com/
    Signed-off-by: David Arcari <darcari@redhat.com>
    Cc: 6.18+ <stable@vger.kernel.org> # 6.18+
    [ rjw: Added one paragraph to the changelog ]
    Link: https://patch.msgid.link/20260224122106.228116-1-darcari@redhat.com
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -1647,8 +1647,8 @@ static ssize_t store_no_turbo(struct kobject *a, struct kobj_attribute *b,
 static void update_cpu_qos_request(int cpu, enum freq_qos_req_type type)
 {
 	struct cpudata *cpudata = all_cpu_data[cpu];
-	unsigned int freq = cpudata->pstate.turbo_freq;
 	struct freq_qos_request *req;
+	unsigned int freq;
 
 	struct cpufreq_policy *policy __free(put_cpufreq_policy) = cpufreq_cpu_get(cpu);
 	if (!policy)
@@ -1660,6 +1660,8 @@ static void update_cpu_qos_request(int cpu, enum freq_qos_req_type type)
 
 	if (hwp_active)
 		intel_pstate_get_hwp_cap(cpudata);
+
+	freq = cpudata->pstate.turbo_freq;
 
 	if (type == FREQ_QOS_MIN) {
 		freq = DIV_ROUND_UP(freq * global.min_perf_pct, 100);
