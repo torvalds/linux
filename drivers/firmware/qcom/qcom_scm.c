87a1698cb80650e47b41ca78a78ce06870cba631
Pankaj Patil <pankaj.patil@oss.qualcomm.com>
2026-02-25 17:37:24 +0530

    firmware: qcom: scm: Allow QSEECOM on Glymur CRD
    Add glymur-crd to QSEECOM allowlist for enabling access to efivars and
    uefi bootloader
    
    Signed-off-by: Pankaj Patil <pankaj.patil@oss.qualcomm.com>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20260225-qseecom_glymur-v1-1-0cafc709e2ef@oss.qualcomm.com
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -2310,6 +2310,7 @@ static const struct of_device_id qcom_scm_qseecom_allowlist[] __maybe_unused = {
 	{ .compatible = "microsoft,denali", },
 	{ .compatible = "microsoft,romulus13", },
 	{ .compatible = "microsoft,romulus15", },
+	{ .compatible = "qcom,glymur-crd" },
 	{ .compatible = "qcom,hamoa-iot-evk" },
 	{ .compatible = "qcom,purwa-iot-evk" },
 	{ .compatible = "qcom,sc8180x-primus" },
