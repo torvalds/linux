778d8c1b2a6ffe622ddcd3bb35b620e6e41f4da0
Yosry Ahmed <yosry@kernel.org>
2026-02-25 00:59:43 +0000

    KVM: nSVM: Sync NextRIP to cached vmcb12 after VMRUN of L2
    After VMRUN in guest mode, nested_sync_control_from_vmcb02() syncs
    fields written by the CPU from vmcb02 to the cached vmcb12. This is
    because the cached vmcb12 is used as the authoritative copy of some of
    the controls, and is the payload when saving/restoring nested state.
    
    NextRIP is also written by the CPU (in some cases) after VMRUN, but is
    not sync'd to the cached vmcb12. As a result, it is corrupted after
    save/restore (replaced by the original value written by L1 on nested
    VMRUN). This could cause problems for both KVM (e.g. when injecting a
    soft IRQ) or L1 (e.g. when using NextRIP to advance RIP after emulating
    an instruction).
    
    Fix this by sync'ing NextRIP to the cache after VMRUN of L2, but only
    after completing interrupts (not in nested_sync_control_from_vmcb02()),
    as KVM may update NextRIP (e.g. when re-injecting a soft IRQ).
    
    Fixes: cc440cdad5b7 ("KVM: nSVM: implement KVM_GET_NESTED_STATE and KVM_SET_NESTED_STATE")
    CC: stable@vger.kernel.org
    Co-developed-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Yosry Ahmed <yosry@kernel.org>
    Link: https://patch.msgid.link/20260225005950.3739782-2-yosry@kernel.org
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -4435,6 +4435,16 @@ static __no_kcsan fastpath_t svm_vcpu_run(struct kvm_vcpu *vcpu, u64 run_flags)
 
 	svm_complete_interrupts(vcpu);
 
+	/*
+	 * Update the cache after completing interrupts to get an accurate
+	 * NextRIP, e.g. when re-injecting a soft interrupt.
+	 *
+	 * FIXME: Rework svm_get_nested_state() to not pull data from the
+	 *        cache (except for maybe int_ctl).
+	 */
+	if (is_guest_mode(vcpu))
+		svm->nested.ctl.next_rip = svm->vmcb->control.next_rip;
+
 	return svm_exit_handlers_fastpath(vcpu);
 }
 
