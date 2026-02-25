03bee264f8ebfd39e0254c98e112d033a7aa9055
Yosry Ahmed <yosry@kernel.org>
2026-02-25 00:59:44 +0000

    KVM: nSVM: Sync interrupt shadow to cached vmcb12 after VMRUN of L2
    After VMRUN in guest mode, nested_sync_control_from_vmcb02() syncs
    fields written by the CPU from vmcb02 to the cached vmcb12. This is
    because the cached vmcb12 is used as the authoritative copy of some of
    the controls, and is the payload when saving/restoring nested state.
    
    int_state is also written by the CPU, specifically bit 0 (i.e.
    SVM_INTERRUPT_SHADOW_MASK) for nested VMs, but it is not sync'd to
    cached vmcb12. This does not cause a problem if KVM_SET_NESTED_STATE
    preceeds KVM_SET_VCPU_EVENTS in the restore path, as an interrupt shadow
    would be correctly restored to vmcb02 (KVM_SET_VCPU_EVENTS overwrites
    what KVM_SET_NESTED_STATE restored in int_state).
    
    However, if KVM_SET_VCPU_EVENTS preceeds KVM_SET_NESTED_STATE, an
    interrupt shadow would be restored into vmcb01 instead of vmcb02. This
    would mostly be benign for L1 (delays an interrupt), but not for L2. For
    L2, the vCPU could hang (e.g. if a wakeup interrupt is delivered before
    a HLT that should have been in an interrupt shadow).
    
    Sync int_state to the cached vmcb12 in nested_sync_control_from_vmcb02()
    to avoid this problem. With that, KVM_SET_NESTED_STATE restores the
    correct interrupt shadow state, and if KVM_SET_VCPU_EVENTS follows it
    would overwrite it with the same value.
    
    Fixes: cc440cdad5b7 ("KVM: nSVM: implement KVM_GET_NESTED_STATE and KVM_SET_NESTED_STATE")
    CC: stable@vger.kernel.org
    Signed-off-by: Yosry Ahmed <yosry@kernel.org>
    Link: https://patch.msgid.link/20260225005950.3739782-3-yosry@kernel.org
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/svm/nested.c
+++ arch/x86/kvm/svm/nested.c
@@ -521,6 +521,7 @@ void nested_sync_control_from_vmcb02(struct vcpu_svm *svm)
 	u32 mask;
 	svm->nested.ctl.event_inj      = svm->vmcb->control.event_inj;
 	svm->nested.ctl.event_inj_err  = svm->vmcb->control.event_inj_err;
+	svm->nested.ctl.int_state	= svm->vmcb->control.int_state;
 
 	/* Only a few fields of int_ctl are written by the processor.  */
 	mask = V_IRQ_MASK | V_TPR_MASK;
