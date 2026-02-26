d76e0c54ed8405469375177ddc7e4b863b59d0ae
Josua Mayer <josua@solid-run.com>
2026-02-26 15:21:12 +0200

    phy: can-transceiver: drop temporary helper getting optional mux-state
    Multiplexer subsystem has now added helpers for getting managed optional
    mux-state.
    
    Switch to the new devm_mux_state_get_optional helper.
    
    This change is only compile-tested.
    
    Acked-by: Vinod Koul <vkoul@kernel.org>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Josua Mayer <josua@solid-run.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/phy/phy-can-transceiver.c
+++ drivers/phy/phy-can-transceiver.c
@@ -126,16 +126,6 @@ static const struct of_device_id can_transceiver_phy_ids[] = {
 };
 MODULE_DEVICE_TABLE(of, can_transceiver_phy_ids);
 
-/* Temporary wrapper until the multiplexer subsystem supports optional muxes */
-static inline struct mux_state *
-can_transceiver_phy_mux_state_get_optional(struct device *dev, const char *mux_name)
-{
-	if (!of_property_present(dev->of_node, "mux-states"))
-		return NULL;
-
-	return devm_mux_state_get(dev, mux_name);
-}
-
 static struct phy *can_transceiver_phy_xlate(struct device *dev,
 					     const struct of_phandle_args *args)
 {
@@ -183,7 +173,7 @@ static int can_transceiver_phy_probe(struct platform_device *pdev)
 	priv->num_ch = num_ch;
 	platform_set_drvdata(pdev, priv);
 
-	mux_state = can_transceiver_phy_mux_state_get_optional(dev, NULL);
+	mux_state = devm_mux_state_get_optional(dev, NULL);
 	if (IS_ERR(mux_state))
 		return PTR_ERR(mux_state);
 
