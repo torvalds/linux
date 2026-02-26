bed80a08ff5e357ea722db10e2b552acf1ff7482
Kavita Kavita <kavita.kavita@oss.qualcomm.com>
2026-02-27 00:25:53 +0530

    wifi: mac80211: Advertise IEEE 802.1X authentication support
    Advertise support for IEEE 802.1X authentication protocol directly from
    mac80211, without depending on driver indication of (Re)Association
    frame encryption capability.
    
    As specified in "IEEE P802.11bi/D4.0, clauses 12.16.5 and 12.16.8.2",
    IEEE 802.1X authentication can operate with or without (Re)Association
    frame encryption support. Therefore, mac80211 can safely advertise
    802.1X support independently of driver capabilities.
    
    Signed-off-by: Kavita Kavita <kavita.kavita@oss.qualcomm.com>
    Link: https://patch.msgid.link/20260226185553.1516290-6-kavita.kavita@oss.qualcomm.com
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/main.c
+++ net/mac80211/main.c
@@ -915,6 +915,7 @@ struct ieee80211_hw *ieee80211_alloc_hw_nm(size_t priv_data_len,
 
 	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_TXQS);
 	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_RRM);
+	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_IEEE8021X_AUTH);
 
 	wiphy->bss_priv_size = sizeof(struct ieee80211_bss);
 
