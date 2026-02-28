29f0724c4592a5ab9076e1ff6e4e39f0de60cc9e
Alejandro Lucero <alucerop@amd.com>
2026-02-28 17:36:02 +0000

    cxl/region: Factor out interleave ways setup
    Region creation based on Type3 devices can be triggered from user space
    allowing memory combination through interleaving.
    
    In preparation for kernel driven region creation, that is Type2 drivers
    triggering region creation backed with its advertised CXL memory, factor
    out a common helper from the user-sysfs region setup for interleave ways.
    
    Signed-off-by: Alejandro Lucero <alucerop@amd.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Gregory Price <gourry@gourry.net>
    Tested-by: Gregory Price <gourry@gourry.net>
    Reviewed-by: Alison Schofield <alison.schofield@intel.com>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Link: https://patch.msgid.link/20260228173603.1125109-3-alejandro.lucero-palau@amd.com
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>

--- drivers/cxl/core/region.c
+++ drivers/cxl/core/region.c
@@ -485,21 +485,13 @@ static ssize_t interleave_ways_show(struct device *dev,
 
 static const struct attribute_group *get_cxl_region_target_group(void);
 
-static ssize_t interleave_ways_store(struct device *dev,
-				     struct device_attribute *attr,
-				     const char *buf, size_t len)
+static int set_interleave_ways(struct cxl_region *cxlr, int val)
 {
-	struct cxl_region *cxlr = to_cxl_region(dev);
 	struct cxl_root_decoder *cxlrd = cxlr->cxlrd;
 	struct cxl_decoder *cxld = &cxlrd->cxlsd.cxld;
 	struct cxl_region_params *p = &cxlr->params;
-	unsigned int val, save;
-	int rc;
+	int save, rc;
 	u8 iw;
-
-	rc = kstrtouint(buf, 0, &val);
-	if (rc)
-		return rc;
 
 	rc = ways_to_eiw(val, &iw);
 	if (rc)
@@ -515,9 +507,7 @@ static ssize_t interleave_ways_store(struct device *dev,
 		return -EINVAL;
 	}
 
-	ACQUIRE(rwsem_write_kill, rwsem)(&cxl_rwsem.region);
-	if ((rc = ACQUIRE_ERR(rwsem_write_kill, &rwsem)))
-		return rc;
+	lockdep_assert_held_write(&cxl_rwsem.region);
 
 	if (p->state >= CXL_CONFIG_INTERLEAVE_ACTIVE)
 		return -EBUSY;
@@ -525,10 +515,31 @@ static ssize_t interleave_ways_store(struct device *dev,
 	save = p->interleave_ways;
 	p->interleave_ways = val;
 	rc = sysfs_update_group(&cxlr->dev.kobj, get_cxl_region_target_group());
-	if (rc) {
+	if (rc)
 		p->interleave_ways = save;
+
+	return rc;
+}
+
+static ssize_t interleave_ways_store(struct device *dev,
+				     struct device_attribute *attr,
+				     const char *buf, size_t len)
+{
+	struct cxl_region *cxlr = to_cxl_region(dev);
+	int val;
+	int rc;
+
+	rc = kstrtoint(buf, 0, &val);
+	if (rc)
 		return rc;
-	}
+
+	ACQUIRE(rwsem_write_kill, rwsem)(&cxl_rwsem.region);
+	if ((rc = ACQUIRE_ERR(rwsem_write_kill, &rwsem)))
+		return rc;
+
+	rc = set_interleave_ways(cxlr, val);
+	if (rc)
+		return rc;
 
 	return len;
 }
