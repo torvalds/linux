afe376d2c1fa78c8a1063a357c6971bba3f6da91
Ritesh Harjani (IBM) <ritesh.list@gmail.com>
2026-03-01 21:44:26 +0530

    ext4: kunit: extents-test: lix percpu_counters list corruption
    commit 82f80e2e3b23 ("ext4: add extent status cache support to kunit tests"),
    added ext4_es_register_shrinker() in extents_kunit_init() function but
    failed to add the unregister shrinker routine in extents_kunit_exit().
    
    This could cause the following percpu_counters list corruption bug.
    
             ok 1 split unwrit extent to 2 extents and convert 1st half writ
      slab kmalloc-4k start c0000002007ff000 pointer offset 1448 size 4096
     list_add corruption. next->prev should be prev (c000000004bc9e60), but was 0000000000000000. (next=c0000002007ff5a8).
     ------------[ cut here ]------------
     kernel BUG at lib/list_debug.c:29!
    cpu 0x2: Vector: 700 (Program Check) at [c000000241927a30]
        pc: c000000000f26ed0: __list_add_valid_or_report+0x120/0x164
        lr: c000000000f26ecc: __list_add_valid_or_report+0x11c/0x164
        sp: c000000241927cd0
       msr: 800000000282b033
      current = 0xc000000241215200
      paca    = 0xc0000003fffff300   irqmask: 0x03   irq_happened: 0x09
        pid   = 258, comm = kunit_try_catch
    kernel BUG at lib/list_debug.c:29!
    enter ? for help
     __percpu_counter_init_many+0x148/0x184
     ext4_es_register_shrinker+0x74/0x23c
     extents_kunit_init+0x100/0x308
     kunit_try_run_case+0x78/0x1f8
     kunit_generic_run_threadfn_adapter+0x40/0x70
     kthread+0x190/0x1a0
     start_kernel_thread+0x14/0x18
    2:mon>
    
    This happens because:
    
    extents_kunit_init(test N):
      ext4_es_register_shrinker(sbi)
        percpu_counters_init() x 4; // this adds 4 list nodes to global percpu_counters list
          list_add(&fbc->list, &percpu_counters);
        shrinker_register();
    
    extents_kunit_exit(test N):
      kfree(sbi);			// frees sbi w/o removing those 4 list nodes.
      				// So, those list node now becomes dangling pointers
    
    extents_kunit_init(test N+1):
      kzalloc_obj(ext4_sb_info)	// allocator returns same page, but zeroed.
      ext4_es_register_shrinker(sbi)
        percpu_counters_init()
          list_add(&fbc->list, &percpu_counters);
            __list_add_valid(new, prev, next);
    	next->prev != prev 		// list corruption bug detected, since next->prev = NULL
    
    Fixes: 82f80e2e3b23 ("ext4: add extent status cache support to kunit tests")
    Signed-off-by: Ritesh Harjani (IBM) <ritesh.list@gmail.com>
    Reviewed-by: Ojaswin Mujoo <ojaswin@linux.ibm.com>
    Link: https://patch.msgid.link/5bb9041471dab8ce870c191c19cbe4df57473be8.1772381213.git.ritesh.list@gmail.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Cc: stable@kernel.org

--- fs/ext4/extents-test.c
+++ fs/ext4/extents-test.c
@@ -142,8 +142,10 @@ static struct file_system_type ext_fs_type = {
 
 static void extents_kunit_exit(struct kunit *test)
 {
-	struct ext4_sb_info *sbi = k_ctx.k_ei->vfs_inode.i_sb->s_fs_info;
+	struct super_block *sb = k_ctx.k_ei->vfs_inode.i_sb;
+	struct ext4_sb_info *sbi = sb->s_fs_info;
 
+	ext4_es_unregister_shrinker(sbi);
 	kfree(sbi);
 	kfree(k_ctx.k_ei);
 	kfree(k_ctx.k_data);
