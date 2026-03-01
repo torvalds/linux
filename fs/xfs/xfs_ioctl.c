c6ce65cb17aa9321687d1b8a842487f839e1a548
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2026-03-01 10:34:35 +1000

    xfs: add write pointer to xfs_rtgroup_geometry
    There is currently no XFS ioctl that allows userspace to retrieve the
    write pointer for a specific realtime group block for zoned XFS. On zoned
    block devices, userspace can obtain this information via zone reports from
    the underlying device. However, for zoned XFS operating on regular block
    devices, no equivalent mechanism exists.
    
    Access to the realtime group write pointer is useful to userspace
    development and analysis tools such as Zonar [1]. So extend the existing
    struct xfs_rtgroup_geometry to add a new rg_writepointer field. This field
    is valid if XFS_RTGROUP_GEOM_WRITEPOINTER flag is set. The rg_writepointer
    field specifies the location of the current writepointer as a block offset
    into the respective rtgroup.
    
    [1] https://lwn.net/Articles/1059364/
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Carlos Maiolino <cem@kernel.org>

--- fs/xfs/xfs_ioctl.c
+++ fs/xfs/xfs_ioctl.c
@@ -37,12 +37,15 @@
 #include "xfs_ioctl.h"
 #include "xfs_xattr.h"
 #include "xfs_rtbitmap.h"
+#include "xfs_rtrmap_btree.h"
 #include "xfs_file.h"
 #include "xfs_exchrange.h"
 #include "xfs_handle.h"
 #include "xfs_rtgroup.h"
 #include "xfs_healthmon.h"
 #include "xfs_verify_media.h"
+#include "xfs_zone_priv.h"
+#include "xfs_zone_alloc.h"
 
 #include <linux/mount.h>
 #include <linux/fileattr.h>
@@ -413,6 +416,7 @@ xfs_ioc_rtgroup_geometry(
 {
 	struct xfs_rtgroup	*rtg;
 	struct xfs_rtgroup_geometry rgeo;
+	xfs_rgblock_t		highest_rgbno;
 	int			error;
 
 	if (copy_from_user(&rgeo, arg, sizeof(rgeo)))
@@ -432,6 +436,21 @@ xfs_ioc_rtgroup_geometry(
 	xfs_rtgroup_put(rtg);
 	if (error)
 		return error;
+
+	if (xfs_has_zoned(mp)) {
+		xfs_rtgroup_lock(rtg, XFS_RTGLOCK_RMAP);
+		if (rtg->rtg_open_zone) {
+			rgeo.rg_writepointer = rtg->rtg_open_zone->oz_allocated;
+		} else {
+			highest_rgbno = xfs_rtrmap_highest_rgbno(rtg);
+			if (highest_rgbno == NULLRGBLOCK)
+				rgeo.rg_writepointer = 0;
+			else
+				rgeo.rg_writepointer = highest_rgbno + 1;
+		}
+		xfs_rtgroup_unlock(rtg, XFS_RTGLOCK_RMAP);
+		rgeo.rg_flags |= XFS_RTGROUP_GEOM_WRITEPOINTER;
+	}
 
 	if (copy_to_user(arg, &rgeo, sizeof(rgeo)))
 		return -EFAULT;
