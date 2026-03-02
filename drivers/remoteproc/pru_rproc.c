d1165ef7e9d2bc86a7efeac386435de104c0ba2f
Andrew Davis <afd@ti.com>
2026-03-02 14:27:28 -0600

    remoteproc: pru: Remove empty remove callback
    The .remove() callback only prints out a debug message, remove
    this otherwise unneeded callback.
    
    Signed-off-by: Andrew Davis <afd@ti.com>
    Link: https://lore.kernel.org/r/20260302202728.322073-2-afd@ti.com
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

--- drivers/remoteproc/pru_rproc.c
+++ drivers/remoteproc/pru_rproc.c
@@ -1078,14 +1078,6 @@ static int pru_rproc_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static void pru_rproc_remove(struct platform_device *pdev)
-{
-	struct device *dev = &pdev->dev;
-	struct rproc *rproc = platform_get_drvdata(pdev);
-
-	dev_dbg(dev, "%s: removing rproc %s\n", __func__, rproc->name);
-}
-
 static const struct pru_private_data pru_data = {
 	.type = PRU_TYPE_PRU,
 };
@@ -1131,7 +1123,6 @@ static struct platform_driver pru_rproc_driver = {
 		.suppress_bind_attrs = true,
 	},
 	.probe  = pru_rproc_probe,
-	.remove = pru_rproc_remove,
 };
 module_platform_driver(pru_rproc_driver);
 
