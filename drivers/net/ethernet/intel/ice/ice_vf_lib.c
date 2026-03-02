82e68aa4a6b16aa060d5044116dac957631cf6f0
Yury Norov <ynorov@nvidia.com>
2026-03-01 20:11:57 -0500

    ice: use bitmap_empty() in ice_vf_has_no_qs_ena
    bitmap_empty() is more verbose and efficient, as it stops traversing
    {r,t}xq_ena as soon as the 1st set bit found.
    
    Tested-by: Rafal Romanowski <rafal.romanowski@intel.com>
    Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Reviewed-by: Jacob Keller <jacob.e.keller@intel.com>
    Signed-off-by: Yury Norov <ynorov@nvidia.com>

--- drivers/net/ethernet/intel/ice/ice_vf_lib.c
+++ drivers/net/ethernet/intel/ice/ice_vf_lib.c
@@ -1210,8 +1210,8 @@ bool ice_is_vf_trusted(struct ice_vf *vf)
  */
 bool ice_vf_has_no_qs_ena(struct ice_vf *vf)
 {
-	return (!bitmap_weight(vf->rxq_ena, ICE_MAX_RSS_QS_PER_VF) &&
-		!bitmap_weight(vf->txq_ena, ICE_MAX_RSS_QS_PER_VF));
+	return bitmap_empty(vf->rxq_ena, ICE_MAX_RSS_QS_PER_VF) &&
+		bitmap_empty(vf->txq_ena, ICE_MAX_RSS_QS_PER_VF);
 }
 
 /**
