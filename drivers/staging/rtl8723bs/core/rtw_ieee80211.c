e65ea7fb205e1f9e3f7908569da1868e7a3fc2a6
Jose A. Perez de Azpillaga <azpijr@gmail.com>
2026-03-02 22:51:54 +0100

    staging: rtl8723bs: fix bitwise OR operator spacing
    Fix spaces between bitwise OR operations rtw_action_frame_parse() for
    better readability.
    
    Signed-off-by: Jose A. Perez de Azpillaga <azpijr@gmail.com>
    Reviewed-by: Ethan Tidmore <ethantidmore06@gmail.com>
    Link: https://patch.msgid.link/20260302215208.67045-1-azpijr@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/core/rtw_ieee80211.c
+++ drivers/staging/rtl8723bs/core/rtw_ieee80211.c
@@ -1123,11 +1123,9 @@ int rtw_action_frame_parse(const u8 *frame, u32 frame_len, u8 *category, u8 *act
 
 	fc = le16_to_cpu(((struct ieee80211_hdr_3addr *)frame)->frame_control);
 
-	if ((fc & (IEEE80211_FCTL_FTYPE|IEEE80211_FCTL_STYPE))
-		!= (IEEE80211_FTYPE_MGMT|IEEE80211_STYPE_ACTION)
-	) {
+	if ((fc & (IEEE80211_FCTL_FTYPE | IEEE80211_FCTL_STYPE)) !=
+	    (IEEE80211_FTYPE_MGMT | IEEE80211_STYPE_ACTION))
 		return false;
-	}
 
 	c = frame_body[0];
 
