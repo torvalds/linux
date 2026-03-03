417607de1f4e6280f646aa42cad5ed84e9228c01
Yuvraj Sakshith <yuvraj.sakshith@oss.qualcomm.com>
2026-03-03 03:30:28 -0800

    mm/page_reporting: add PAGE_REPORTING_ORDER_UNSPECIFIED
    Patch series "Allow order zero pages in page reporting", v4.
    
    Today, page reporting sets page_reporting_order in two ways:
    
    (1) page_reporting.page_reporting_order cmdline parameter
    (2) Driver can pass order while registering itself.
    
    In both cases, order zero is ignored by free page reporting because it is
    used to set page_reporting_order to a default value, like MAX_PAGE_ORDER.
    
    In some cases we might want page_reporting_order to be zero.
    
    For instance, when virtio-balloon runs inside a guest with tiny memory
    (say, 16MB), it might not be able to find a order 1 page (or in the worst
    case order MAX_PAGE_ORDER page) after some uptime.  Page reporting should
    be able to return order zero pages back for optimal memory relinquishment.
    
    This patch changes the default fallback value from '0' to '-1' in all
    possible clients of free page reporting (hv_balloon and virtio-balloon)
    together with allowing '0' as a valid order in page_reporting_register().
    
    
    This patch (of 5):
    
    Drivers can pass order of pages to be reported while registering itself. 
    Today, this is a magic number, 0.
    
    Label this with PAGE_REPORTING_ORDER_UNSPECIFIED and check for it when the
    driver is being registered.
    
    This macro will be used in relevant drivers next.
    
    [akpm@linux-foundation.org: tweak whitespace, per David]
    Link: https://lkml.kernel.org/r/20260303113032.3008371-1-yuvraj.sakshith@oss.qualcomm.com
    Link: https://lkml.kernel.org/r/20260303113032.3008371-2-yuvraj.sakshith@oss.qualcomm.com
    Signed-off-by: Yuvraj Sakshith <yuvraj.sakshith@oss.qualcomm.com>
    Acked-by: David Hildenbrand (Arm) <david@kernel.org>
    Reviewed-by: Michael Kelley <mhklinux@outlook.com>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Cc: Brendan Jackman <jackmanb@google.com>
    Cc: Dexuan Cui <decui@microsoft.com>
    Cc: Eugenio Pérez <eperezma@redhat.com>
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Cc: Jason Wang <jasowang@redhat.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: K. Y. Srinivasan <kys@microsoft.com>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Long Li <longli@microsoft.com>
    Cc: Lorenzo Stoakes (Oracle) <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Wei Liu <wei.liu@kernel.org>
    Cc: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_reporting.c
+++ mm/page_reporting.c
@@ -370,7 +370,8 @@ int page_reporting_register(struct page_reporting_dev_info *prdev)
 	 */
 
 	if (page_reporting_order == -1) {
-		if (prdev->order > 0 && prdev->order <= MAX_PAGE_ORDER)
+		if (prdev->order != PAGE_REPORTING_ORDER_UNSPECIFIED &&
+		    prdev->order <= MAX_PAGE_ORDER)
 			page_reporting_order = prdev->order;
 		else
 			page_reporting_order = pageblock_order;
