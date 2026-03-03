f3d8bb94255117aa7b405c949277c874c1c496f0
Yury Norov <ynorov@nvidia.com>
2026-03-03 15:08:38 -0500

    thermal: intel: switch cpumask_get() to using cpumask_print_to_pagebuf()
    The function opencodes cpumask_print_to_pagebuf() with more generic
    bitmap_print_to_pagebuf(). Switch to using the proper API.
    
    Signed-off-by: Yury Norov <ynorov@nvidia.com>

--- drivers/thermal/intel/intel_powerclamp.c
+++ drivers/thermal/intel/intel_powerclamp.c
@@ -200,8 +200,7 @@ static int cpumask_get(char *buf, const struct kernel_param *kp)
 	if (!cpumask_available(idle_injection_cpu_mask))
 		return -ENODEV;
 
-	return bitmap_print_to_pagebuf(false, buf, cpumask_bits(idle_injection_cpu_mask),
-				       nr_cpumask_bits);
+	return cpumap_print_to_pagebuf(false, buf, idle_injection_cpu_mask);
 }
 
 static const struct kernel_param_ops cpumask_ops = {
