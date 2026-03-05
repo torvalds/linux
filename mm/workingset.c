b3ca98297cd98a51ee9d6d491d0a4ee0ca79b515
Muchun Song <songmuchun@bytedance.com>
2026-03-05 19:52:34 +0800

    mm: workingset: prevent memory cgroup release in lru_gen_eviction()
    In the near future, a folio will no longer pin its corresponding memory
    cgroup.  To ensure safety, it will only be appropriate to hold the rcu
    read lock or acquire a reference to the memory cgroup returned by
    folio_memcg(), thereby preventing it from being released.
    
    In the current patch, the rcu read lock is employed to safeguard against
    the release of the memory cgroup in lru_gen_eviction().
    
    This serves as a preparatory measure for the reparenting of the LRU pages.
    
    Link: https://lore.kernel.org/f37e8ae2d84ddc690813d834cd75735d52d1bc78.1772711148.git.zhengqi.arch@bytedance.com
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Signed-off-by: Qi Zheng <zhengqi.arch@bytedance.com>
    Reviewed-by: Harry Yoo <harry.yoo@oracle.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Cc: Allen Pais <apais@linux.microsoft.com>
    Cc: Axel Rasmussen <axelrasmussen@google.com>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Chengming Zhou <chengming.zhou@linux.dev>
    Cc: Chen Ridong <chenridong@huawei.com>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Hamza Mahfooz <hamzamahfooz@linux.microsoft.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Imran Khan <imran.f.khan@oracle.com>
    Cc: Kamalesh Babulal <kamalesh.babulal@oracle.com>
    Cc: Lance Yang <lance.yang@linux.dev>
    Cc: Liam Howlett <Liam.Howlett@oracle.com>
    Cc: Lorenzo Stoakes (Oracle) <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Michal Koutný <mkoutny@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Nhat Pham <nphamcs@gmail.com>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Usama Arif <usamaarif642@gmail.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Cc: Wei Xu <weixugc@google.com>
    Cc: Yosry Ahmed <yosry@kernel.org>
    Cc: Yuanchu Xie <yuanchu@google.com>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/workingset.c
+++ mm/workingset.c
@@ -244,12 +244,15 @@ static void *lru_gen_eviction(struct folio *folio)
 	int refs = folio_lru_refs(folio);
 	bool workingset = folio_test_workingset(folio);
 	int tier = lru_tier_from_refs(refs, workingset);
-	struct mem_cgroup *memcg = folio_memcg(folio);
+	struct mem_cgroup *memcg;
 	struct pglist_data *pgdat = folio_pgdat(folio);
+	unsigned short memcg_id;
 
 	BUILD_BUG_ON(LRU_GEN_WIDTH + LRU_REFS_WIDTH >
 		     BITS_PER_LONG - max(EVICTION_SHIFT, EVICTION_SHIFT_ANON));
 
+	rcu_read_lock();
+	memcg = folio_memcg(folio);
 	lruvec = mem_cgroup_lruvec(memcg, pgdat);
 	lrugen = &lruvec->lrugen;
 	min_seq = READ_ONCE(lrugen->min_seq[type]);
@@ -257,8 +260,10 @@ static void *lru_gen_eviction(struct folio *folio)
 
 	hist = lru_hist_from_seq(min_seq);
 	atomic_long_add(delta, &lrugen->evicted[hist][type][tier]);
+	memcg_id = mem_cgroup_private_id(memcg);
+	rcu_read_unlock();
 
-	return pack_shadow(mem_cgroup_private_id(memcg), pgdat, token, workingset, type);
+	return pack_shadow(memcg_id, pgdat, token, workingset, type);
 }
 
 /*
