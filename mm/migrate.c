53050890802e25b6b04ab5b243c90e42d10ef777
Muchun Song <songmuchun@bytedance.com>
2026-03-05 19:52:31 +0800

    mm: migrate: prevent memory cgroup release in folio_migrate_mapping()
    In the near future, a folio will no longer pin its corresponding memory
    cgroup.  To ensure safety, it will only be appropriate to hold the rcu
    read lock or acquire a reference to the memory cgroup returned by
    folio_memcg(), thereby preventing it from being released.
    
    In __folio_migrate_mapping(), the rcu read lock is employed to safeguard
    against the release of the memory cgroup in folio_migrate_mapping().
    
    This serves as a preparatory measure for the reparenting of the LRU pages.
    
    Link: https://lore.kernel.org/0f156c2f1188f256855617953f8305f43e066065.1772711148.git.zhengqi.arch@bytedance.com
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Signed-off-by: Qi Zheng <zhengqi.arch@bytedance.com>
    Reviewed-by: Harry Yoo <harry.yoo@oracle.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Cc: Allen Pais <apais@linux.microsoft.com>
    Cc: Axel Rasmussen <axelrasmussen@google.com>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Chengming Zhou <chengming.zhou@linux.dev>
    Cc: Chen Ridong <chenridong@huawei.com>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Hamza Mahfooz <hamzamahfooz@linux.microsoft.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Imran Khan <imran.f.khan@oracle.com>
    Cc: Kamalesh Babulal <kamalesh.babulal@oracle.com>
    Cc: Lance Yang <lance.yang@linux.dev>
    Cc: Liam Howlett <Liam.Howlett@oracle.com>
    Cc: Lorenzo Stoakes (Oracle) <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Michal Koutný <mkoutny@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Nhat Pham <nphamcs@gmail.com>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Usama Arif <usamaarif642@gmail.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Cc: Wei Xu <weixugc@google.com>
    Cc: Yosry Ahmed <yosry@kernel.org>
    Cc: Yuanchu Xie <yuanchu@google.com>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/migrate.c
+++ mm/migrate.c
@@ -672,6 +672,7 @@ static int __folio_migrate_mapping(struct address_space *mapping,
 		struct lruvec *old_lruvec, *new_lruvec;
 		struct mem_cgroup *memcg;
 
+		rcu_read_lock();
 		memcg = folio_memcg(folio);
 		old_lruvec = mem_cgroup_lruvec(memcg, oldzone->zone_pgdat);
 		new_lruvec = mem_cgroup_lruvec(memcg, newzone->zone_pgdat);
@@ -699,6 +700,7 @@ static int __folio_migrate_mapping(struct address_space *mapping,
 			mod_lruvec_state(new_lruvec, NR_FILE_DIRTY, nr);
 			__mod_zone_page_state(newzone, NR_ZONE_WRITE_PENDING, nr);
 		}
+		rcu_read_unlock();
 	}
 	local_irq_enable();
 
