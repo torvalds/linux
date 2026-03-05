8988913aacee82e5401bf3b96839731982dcbde7
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2026-03-05 10:31:38 +0100

    module: Drop unused signature types
    Only PKCS#7 signatures are used today.
    
    Remove the unused enum values. As this enum is used in on-disk data,
    preserve the numeric value.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Reviewed-by: Petr Pavlu <petr.pavlu@suse.com>
    Reviewed-by: Nicolas Schier <nsc@kernel.org>
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- include/linux/module_signature.h
+++ include/linux/module_signature.h
@@ -15,9 +15,7 @@
 #define MODULE_SIG_STRING "~Module signature appended~\n"
 
 enum pkey_id_type {
-	PKEY_ID_PGP,		/* OpenPGP generated key ID */
-	PKEY_ID_X509,		/* X.509 arbitrary subjectKeyIdentifier */
-	PKEY_ID_PKCS7,		/* Signature in PKCS#7 message */
+	PKEY_ID_PKCS7 = 2,	/* Signature in PKCS#7 message */
 };
 
 /*
