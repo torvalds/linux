f287826fd7e406caa56692ecc39742bdb312b2e8
Yuanshen Cao <alex.caoys@gmail.com>
2026-03-05 03:34:07 +0000

    dt-bindings: power: Add Support for Allwinner A733 PCK600 Power Domain Controller
    The A733 PCK600, similar to A523 PCK600, is likely a customized version
    of ARM PCK-600 power controller. They share the same BSP drivers in the
    package provided by Radxa, with the only difference being the lack of
    resets.
    
    Therefore, document A733 compatible and make resets required only for
    the other models, as well as prepare the PD definitions for future
    device trees.
    
    Reviewed-by: Krzysztof Kozlowski <krzysztof.kozlowski@oss.qualcomm.com>
    Signed-off-by: Yuanshen Cao <alex.caoys@gmail.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

---
+++ include/dt-bindings/power/allwinner,sun60i-a733-pck-600.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+
+#ifndef _DT_BINDINGS_POWER_SUN60I_A733_PCK600_H_
+#define _DT_BINDINGS_POWER_SUN60I_A733_PCK600_H_
+
+#define PD_VI			0
+#define PD_DE_SYS		1
+#define PD_VE_DEC		2
+#define PD_VE_ENC		3
+#define PD_NPU			4
+#define PD_GPU_TOP		5
+#define PD_GPU_CORE		6
+#define PD_PCIE			7
+#define PD_USB2			8
+#define PD_VO			9
+#define PD_VO1			10
+
+#endif /* _DT_BINDINGS_POWER_SUN60I_A733_PCK600_H_ */
