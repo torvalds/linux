b45b4314d3e55be70b597baa1f0ab9283e68003b
Aleksander Jan Bajkowski <olek2@wp.pl>
2026-03-05 21:08:20 +0100

    crypto: testmgr - Add test vectors for authenc(hmac(sha1),rfc3686(ctr(aes)))
    Test vectors were generated starting from existing RFC3686(CTR(AES)) test
    vectors and adding HMAC(SHA1) computed with software implementation.
    Then, the results were double-checked on Mediatek MT7986 (safexcel).
    Platform pass self-tests.
    
    Signed-off-by: Aleksander Jan Bajkowski <olek2@wp.pl>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -4135,8 +4135,12 @@ static const struct alg_test_desc alg_test_descs[] = {
 		}
 	}, {
 		.alg = "authenc(hmac(sha1),rfc3686(ctr(aes)))",
-		.test = alg_test_null,
+		.generic_driver = "authenc(hmac-sha1-lib,rfc3686(ctr(aes-lib)))",
+		.test = alg_test_aead,
 		.fips_allowed = 1,
+		.suite = {
+			.aead = __VECS(hmac_sha1_aes_ctr_rfc3686_tv_temp)
+		}
 	}, {
 		.alg = "authenc(hmac(sha224),cbc(aes))",
 		.generic_driver = "authenc(hmac-sha224-lib,cbc(aes-generic))",
