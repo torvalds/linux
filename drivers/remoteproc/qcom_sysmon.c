95b6c029e56e4d75e2957ce7ac795da29415865b
Daniel Lezcano <daniel.lezcano@oss.qualcomm.com>
2026-03-10 00:03:33 +0100

    remoteproc: sysmon: Use the unified QMI service ID instead of defining it locally
    Instead of defining a local macro with a custom name for the QMI
    service identifier, use the one provided in qmi.h and remove the
    locally defined macro.
    
    Signed-off-by: Daniel Lezcano <daniel.lezcano@oss.qualcomm.com>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20260309230346.3584252-5-daniel.lezcano@oss.qualcomm.com
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>

--- drivers/remoteproc/qcom_sysmon.c
+++ drivers/remoteproc/qcom_sysmon.c
@@ -677,7 +677,7 @@ struct qcom_sysmon *qcom_add_sysmon_subdev(struct rproc *rproc,
 		return ERR_PTR(ret);
 	}
 
-	qmi_add_lookup(&sysmon->qmi, 43, 0, 0);
+	qmi_add_lookup(&sysmon->qmi, QMI_SERVICE_ID_SSCTL, 0, 0);
 
 	sysmon->subdev.prepare = sysmon_prepare;
 	sysmon->subdev.start = sysmon_start;
