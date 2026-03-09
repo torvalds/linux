4e57fdd11083d5cd44febc4b6613777291ec936e
Sohil Mehta <sohil.mehta@intel.com>
2026-03-09 11:10:26 -0700

    x86/traps: Consolidate user fixups in the #GP handler
    Move the UMIP exception fixup under the common "if (user_mode(regs))"
    condition where the rest of user mode fixups reside. Also, move the UMIP
    feature check into its fixup function to keep the calling code
    consistent and clean.
    
    No functional change intended.
    
    Suggested-by: Dave Hansen <dave.hansen@intel.com>
    Signed-off-by: Sohil Mehta <sohil.mehta@intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: H. Peter Anvin (Intel) <hpa@zytor.com>
    Acked-by: Dave Hansen <dave.hansen@linux.intel.com>
    Tested-by: Maciej Wieczor-Retman <maciej.wieczor-retman@intel.com>
    Link: https://patch.msgid.link/20260309181029.398498-3-sohil.mehta@intel.com

--- arch/x86/kernel/traps.c
+++ arch/x86/kernel/traps.c
@@ -921,11 +921,6 @@ DEFINE_IDTENTRY_ERRORCODE(exc_general_protection)
 
 	cond_local_irq_enable(regs);
 
-	if (static_cpu_has(X86_FEATURE_UMIP)) {
-		if (user_mode(regs) && fixup_umip_exception(regs))
-			goto exit;
-	}
-
 	if (v8086_mode(regs)) {
 		local_irq_enable();
 		handle_vm86_fault((struct kernel_vm86_regs *) regs, error_code);
@@ -938,6 +933,9 @@ DEFINE_IDTENTRY_ERRORCODE(exc_general_protection)
 			goto exit;
 
 		if (fixup_vdso_exception(regs, X86_TRAP_GP, error_code, 0))
+			goto exit;
+
+		if (fixup_umip_exception(regs))
 			goto exit;
 
 		gp_user_force_sig_segv(regs, X86_TRAP_GP, error_code, desc);
