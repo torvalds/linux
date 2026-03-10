87f7dff3ec75b91def0024ebaaf732457f47a63b
Alok Tiwari <alok.a.tiwari@oracle.com>
2026-03-10 11:48:17 -0700

    octeontx2-af: devlink: fix NIX RAS reporter to use RAS interrupt status
    The NIX RAS health report path uses nix_af_rvu_err when handling the
    NIX_AF_RVU_RAS case, so the report prints the ERR interrupt status rather
    than the RAS interrupt status.
    
    Use nix_af_rvu_ras for the NIX_AF_RVU_RAS report.
    
    Fixes: 5ed66306eab6 ("octeontx2-af: Add devlink health reporters for NIX")
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>
    Link: https://patch.msgid.link/20260310184824.1183651-2-alok.a.tiwari@oracle.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_devlink.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_devlink.c
@@ -327,10 +327,10 @@ static int rvu_nix_report_show(struct devlink_fmsg *fmsg, void *ctx,
 		rvu_report_pair_end(fmsg);
 		break;
 	case NIX_AF_RVU_RAS:
-		intr_val = nix_event_context->nix_af_rvu_err;
+		intr_val = nix_event_context->nix_af_rvu_ras;
 		rvu_report_pair_start(fmsg, "NIX_AF_RAS");
 		devlink_fmsg_u64_pair_put(fmsg, "\tNIX RAS Interrupt Reg ",
-					  nix_event_context->nix_af_rvu_err);
+					  nix_event_context->nix_af_rvu_ras);
 		devlink_fmsg_string_put(fmsg, "\n\tPoison Data on:");
 		if (intr_val & BIT_ULL(34))
 			devlink_fmsg_string_put(fmsg, "\n\tNIX_AQ_INST_S");
