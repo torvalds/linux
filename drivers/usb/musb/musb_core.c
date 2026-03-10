786bf7ef564e97f195a2fca379fa7866a4c3ea08
Ai Chao <aichao@kylinos.cn>
2026-03-10 17:44:30 +0800

    usb: musb: Use safer strscpy() instead of strcpy()
    Use a safer function strscpy() instead of strcpy() for copying to
    arrays.
    
    Only idiomatic code replacement, and no functional changes.
    
    Signed-off-by: Ai Chao <aichao@kylinos.cn>
    Link: https://patch.msgid.link/20260310094434.3639602-3-aichao@kylinos.cn
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -1600,7 +1600,7 @@ static int musb_core_init(u16 musb_type, struct musb *musb)
 	/* log core options (read using indexed model) */
 	reg = musb_read_configdata(mbase);
 
-	strcpy(aInfo, (reg & MUSB_CONFIGDATA_UTMIDW) ? "UTMI-16" : "UTMI-8");
+	strscpy(aInfo, (reg & MUSB_CONFIGDATA_UTMIDW) ? "UTMI-16" : "UTMI-8");
 	if (reg & MUSB_CONFIGDATA_DYNFIFO) {
 		strcat(aInfo, ", dyn FIFOs");
 		musb->dyn_fifo = true;
