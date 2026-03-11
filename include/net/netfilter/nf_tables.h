598adea720b97572c7028635cb1c59b3684e128c
Florian Westphal <fw@strlen.de>
2026-03-11 16:24:02 +0100

    netfilter: revert nft_set_rbtree: validate open interval overlap
    This reverts commit 648946966a08 ("netfilter: nft_set_rbtree: validate
    open interval overlap").
    
    There have been reports of nft failing to laod valid rulesets after this
    patch was merged into -stable.
    
    I can reproduce several such problem with recent nft versions, including
    nft 1.1.6 which is widely shipped by distributions.
    
    We currently have little choice here.
    This commit can be resurrected at some point once the nftables fix that
    triggers the false overlap positive has appeared in common distros
    (see e83e32c8d1cd ("mnl: restore create element command with large batches" in
     nftables.git).
    
    Fixes: 648946966a08 ("netfilter: nft_set_rbtree: validate open interval overlap")
    Acked-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Florian Westphal <fw@strlen.de>

--- include/net/netfilter/nf_tables.h
+++ include/net/netfilter/nf_tables.h
@@ -277,8 +277,6 @@ struct nft_userdata {
 	unsigned char		data[];
 };
 
-#define NFT_SET_ELEM_INTERNAL_LAST	0x1
-
 /* placeholder structure for opaque set element backend representation. */
 struct nft_elem_priv { };
 
@@ -288,7 +286,6 @@ struct nft_elem_priv { };
  *	@key: element key
  *	@key_end: closing element key
  *	@data: element data
- * 	@flags: flags
  *	@priv: element private data and extensions
  */
 struct nft_set_elem {
@@ -304,7 +301,6 @@ struct nft_set_elem {
 		u32		buf[NFT_DATA_VALUE_MAXLEN / sizeof(u32)];
 		struct nft_data val;
 	} data;
-	u32			flags;
 	struct nft_elem_priv	*priv;
 };
 
