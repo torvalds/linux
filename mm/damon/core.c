c63067e8b08495550be09afa646f9bf101762d0e
SeongJae Park <sj@kernel.org>
2026-03-10 22:29:22 -0700

    mm/damon/core: fix wrong end address assignment on walk_system_ram()
    Patch series "mm/damon: support addr_unit on default monitoring targets
    for modules".
    
    DAMON_RECLAIM and DAMON_LRU_SORT support 'addr_unit' parameters only when
    the monitoring target address range is explicitly set.  This was
    intentional for making the initial 'addr_unit' support change small.  Now
    'addr_unit' support is being quite stabilized.  Having the corner case of
    the support is only making the code inconsistent with implicit rules.  The
    inconsistency makes it easy to confuse [1] readers.  After all, there is
    no real reason to keep 'addr_unit' support incomplete.  Add the support
    for the case to improve the readability and more completely support
    'addr_unit'.
    
    This series is constructed with five patches.  The first one (patch 1)
    fixes a small bug that mistakenly assigns inclusive end address to open
    end address, which was found from this work.  The second and third ones
    (patches 2 and 3) extend the default monitoring target setting functions
    in the core layer one by one, to support the 'addr_unit' while making no
    visible changes.  The final two patches (patches 4 and 5) update
    DAMON_RECLAIM and DAMON_LRU_SORT to support 'addr_unit' for the default
    monitoring target address ranges, by passing the user input to the core
    functions.
    
    
    This patch (of 5):
    
    'struct damon_addr_range' and 'struct resource' represent different types
    of address ranges.  'damon_addr_range' is for end-open ranges ([start,
    end)).  'resource' is for fully-closed ranges ([start, end]).  But
    walk_system_ram() is assigning resource->end to damon_addr_range->end
    without the inclusiveness adjustment.  As a result, the function returns
    an address range that is missing the last one byte.
    
    The function is being used to find and set the biggest system ram as the
    default monitoring target for DAMON_RECLAIM and DAMON_LRU_SORT.  Missing
    the last byte of the big range shouldn't be a real problem for the real
    use cases.  That said, the loss is definitely an unintended behavior.  Do
    the correct adjustment.
    
    Link: https://lkml.kernel.org/r/20260311052927.93921-1-sj@kernel.org
    Link: https://lkml.kernel.org/r/20260311052927.93921-2-sj@kernel.org
    Link: https://lore.kernel.org/20260131015643.79158-1-sj@kernel.org [1]
    Fixes: 43b0536cb471 ("mm/damon: introduce DAMON-based Reclamation (DAMON_RECLAIM)")
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Cc: Yang yingliang <yangyingliang@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/damon/core.c
+++ mm/damon/core.c
@@ -3068,7 +3068,7 @@ static int walk_system_ram(struct resource *res, void *arg)
 
 	if (a->end - a->start < resource_size(res)) {
 		a->start = res->start;
-		a->end = res->end;
+		a->end = res->end + 1;
 	}
 	return 0;
 }
