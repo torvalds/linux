b7518e0d1c0f4da4c0cc7940eed4679a5ff69a2e
Taniya Das <taniya.das@oss.qualcomm.com>
2026-03-11 16:46:32 +0200

    dt-bindings: clock: qcom: Document the Eliza TCSR Clock Controller
    Add bindings documentation for TCSR Clock Controller for Eliza SoC.
    
    Signed-off-by: Taniya Das <taniya.das@oss.qualcomm.com>
    Acked-by: Krzysztof Kozlowski <krzysztof.kozlowski@oss.qualcomm.com>
    Signed-off-by: Abel Vesa <abel.vesa@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20260311-eliza-clocks-v6-2-453c4cf657a2@oss.qualcomm.com
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>

---
+++ include/dt-bindings/clock/qcom,eliza-tcsr.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_TCSR_CC_ELIZA_H
+#define _DT_BINDINGS_CLK_QCOM_TCSR_CC_ELIZA_H
+
+/* TCSR_CC clocks */
+#define TCSR_HDMI_CLKREF_EN				0
+#define TCSR_PCIE_0_CLKREF_EN				1
+#define TCSR_PCIE_1_CLKREF_EN				2
+#define TCSR_UFS_CLKREF_EN				3
+#define TCSR_USB2_CLKREF_EN				4
+#define TCSR_USB3_CLKREF_EN				5
+
+#endif
