3fcfff4ed35f963380a68741bcd52742baff7f76
Thorsten Blum <thorsten.blum@linux.dev>
2026-03-11 03:07:35 +0100

    crypto: atmel-aes - Fix 3-page memory leak in atmel_aes_buff_cleanup
    atmel_aes_buff_init() allocates 4 pages using __get_free_pages() with
    ATMEL_AES_BUFFER_ORDER, but atmel_aes_buff_cleanup() frees only the
    first page using free_page(), leaking the remaining 3 pages. Use
    free_pages() with ATMEL_AES_BUFFER_ORDER to fix the memory leak.
    
    Fixes: bbe628ed897d ("crypto: atmel-aes - improve performances of data transfer")
    Cc: stable@vger.kernel.org
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/atmel-aes.c
+++ drivers/crypto/atmel-aes.c
@@ -2131,7 +2131,7 @@ static int atmel_aes_buff_init(struct atmel_aes_dev *dd)
 
 static void atmel_aes_buff_cleanup(struct atmel_aes_dev *dd)
 {
-	free_page((unsigned long)dd->buf);
+	free_pages((unsigned long)dd->buf, ATMEL_AES_BUFFER_ORDER);
 }
 
 static int atmel_aes_dma_init(struct atmel_aes_dev *dd)
