831a7f16728c5ceef04ab99a699c3d9e519dc4b8
James Morse <james.morse@arm.com>
2026-03-13 14:45:46 +0000

    arm64: mpam: Advertise the CPUs MPAM limits to the driver
    Requesters need to populate the MPAM fields for any traffic they send on
    the interconnect. For the CPUs these values are taken from the
    corresponding MPAMy_ELx register. Each requester may have a limit on the
    largest PARTID or PMG value that can be used. The MPAM driver has to
    determine the system-wide minimum supported PARTID and PMG values.
    
    To do this, the driver needs to be told what each requestor's limit is.
    
    CPUs are special, but this infrastructure is also needed for the SMMU and
    GIC ITS. Call the helper to tell the MPAM driver what the CPUs can do.
    
    The return value can be ignored by the arch code as it runs well before the
    MPAM driver starts probing.
    
    Tested-by: Gavin Shan <gshan@redhat.com>
    Tested-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Tested-by: Peter Newman <peternewman@google.com>
    Tested-by: Zeng Heng <zengheng4@huawei.com>
    Tested-by: Punit Agrawal <punit.agrawal@oss.qualcomm.com>
    Tested-by: Jesse Chick <jessechick@os.amperecomputing.com>
    Reviewed-by: Zeng Heng <zengheng4@huawei.com>
    Reviewed-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Co-developed-by: Ben Horgan <ben.horgan@arm.com>
    Signed-off-by: Ben Horgan <ben.horgan@arm.com>
    [ morse: requestor->requester as argued by ispell ]
    Signed-off-by: James Morse <james.morse@arm.com>

--- arch/arm64/kernel/mpam.c
+++ arch/arm64/kernel/mpam.c
@@ -3,6 +3,7 @@
 
 #include <asm/mpam.h>
 
+#include <linux/arm_mpam.h>
 #include <linux/jump_label.h>
 #include <linux/percpu.h>
 
@@ -11,3 +12,14 @@ DEFINE_PER_CPU(u64, arm64_mpam_default);
 DEFINE_PER_CPU(u64, arm64_mpam_current);
 
 u64 arm64_mpam_global_default;
+
+static int __init arm64_mpam_register_cpus(void)
+{
+	u64 mpamidr = read_sanitised_ftr_reg(SYS_MPAMIDR_EL1);
+	u16 partid_max = FIELD_GET(MPAMIDR_EL1_PARTID_MAX, mpamidr);
+	u8 pmg_max = FIELD_GET(MPAMIDR_EL1_PMG_MAX, mpamidr);
+
+	return mpam_register_requestor(partid_max, pmg_max);
+}
+/* Must occur before mpam_msc_driver_init() from subsys_initcall() */
+arch_initcall(arm64_mpam_register_cpus)
