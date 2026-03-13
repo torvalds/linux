5e2f19ce90d5601f4250b510ed2f41160e5692e9
Charles Keepax <ckeepax@opensource.cirrus.com>
2026-03-13 16:08:58 +0000

    ASoC: sdw_utils: Add codec_info for cs42l49
    The cs42l49 is an audio codec with integrated MIPI SoundWire interface
    (Version 1.2.1 compliant) with SDCA v1.0 support, I2C, and I2S/TDM
    interfaces designed for portable applications.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>
    Link: https://patch.msgid.link/20260313160858.2022849-3-ckeepax@opensource.cirrus.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sdw_utils/soc_sdw_utils.c
+++ sound/soc/sdw_utils/soc_sdw_utils.c
@@ -814,6 +814,42 @@ struct asoc_sdw_codec_info codec_info_list[] = {
 		.aux_num = 1,
 	},
 	{
+		.part_id = 0x4249,
+		.name_prefix = "cs42l45", /* Use same user-space as cs42l45 */
+		.dais = {
+			{
+				.direction = {true, false},
+				.codec_name = "snd_soc_sdca.UAJ",
+				.dai_name = "IT 41",
+				.dai_type = SOC_SDW_DAI_TYPE_JACK,
+				.dailink = {SOC_SDW_JACK_OUT_DAI_ID, SOC_SDW_UNUSED_DAI_ID},
+				.rtd_init = asoc_sdw_cs42l45_hs_rtd_init,
+			},
+			{
+				.direction = {false, true},
+				.codec_name = "snd_soc_sdca.SmartMic",
+				.dai_name = "OT 113",
+				.dai_type = SOC_SDW_DAI_TYPE_MIC,
+				.dailink = {SOC_SDW_UNUSED_DAI_ID, SOC_SDW_DMIC_DAI_ID},
+				.rtd_init = asoc_sdw_cs42l45_dmic_rtd_init,
+			},
+			{
+				.direction = {false, true},
+				.codec_name = "snd_soc_sdca.UAJ",
+				.dai_name = "OT 36",
+				.dai_type = SOC_SDW_DAI_TYPE_JACK,
+				.dailink = {SOC_SDW_UNUSED_DAI_ID, SOC_SDW_JACK_IN_DAI_ID},
+			},
+		},
+		.dai_num = 3,
+		.auxs = {
+			{
+				.codec_name = "snd_soc_sdca.HID",
+			},
+		},
+		.aux_num = 1,
+	},
+	{
 		.part_id = 0x4747,
 		.name_prefix = "cs47l47",
 		.dais = {
