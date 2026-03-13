087be0cd54f5e27a893222d9a4d4fc970bf31ccd
Sunil Khatri <sunil.khatri@amd.com>
2026-03-13 12:33:43 +0530

    drm/amdgpu/userq: Use kvfree instead of kfree in amdgpu_userq_wait_ioctl
    In function amdgpu_userq_wait_ioctl, drm_gem_objects_lookup allocates
    memory via kvmalloc and hence when that memory is freed the memory
    via kvfree.
    
    Fixes: 2de9353e193f ("drm/amdgpu/userq: Use drm_gem_objects_lookup in amdgpu_userq_wait_ioctl")
    Signed-off-by: Sunil Khatri <sunil.khatri@amd.com>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_userq_fence.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_userq_fence.c
@@ -935,11 +935,11 @@ exec_fini:
 put_gobj_write:
 	for (i = 0; i < num_write_bo_handles; i++)
 		drm_gem_object_put(gobj_write[i]);
-	kfree(gobj_write);
+	kvfree(gobj_write);
 put_gobj_read:
 	for (i = 0; i < num_read_bo_handles; i++)
 		drm_gem_object_put(gobj_read[i]);
-	kfree(gobj_read);
+	kvfree(gobj_read);
 free_timeline_points:
 	kfree(timeline_points);
 free_timeline_handles:
