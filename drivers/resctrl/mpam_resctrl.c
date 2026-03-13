1c1e2968a860c5af9fca67f1c0e88aab83ace0b3
Ben Horgan <ben.horgan@arm.com>
2026-03-13 14:46:01 +0000

    arm_mpam: resctrl: Wait for cacheinfo to be ready
    In order to calculate the rmid realloc threshold the size of the cache
    needs to be known. Cache domains will also be named after the cache id. So
    that this information can be extracted from cacheinfo we need to wait for
    it to be ready. The cacheinfo information is populated in device_initcall()
    so we wait for that.
    
    Tested-by: Gavin Shan <gshan@redhat.com>
    Tested-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Tested-by: Peter Newman <peternewman@google.com>
    Tested-by: Zeng Heng <zengheng4@huawei.com>
    Tested-by: Punit Agrawal <punit.agrawal@oss.qualcomm.com>
    Tested-by: Jesse Chick <jessechick@os.amperecomputing.com>
    Reviewed-by: Zeng Heng <zengheng4@huawei.com>
    Reviewed-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Signed-off-by: Ben Horgan <ben.horgan@arm.com>
    Signed-off-by: James Morse <james.morse@arm.com>

--- drivers/resctrl/mpam_resctrl.c
+++ drivers/resctrl/mpam_resctrl.c
@@ -16,6 +16,7 @@
 #include <linux/resctrl.h>
 #include <linux/slab.h>
 #include <linux/types.h>
+#include <linux/wait.h>
 
 #include <asm/mpam.h>
 
@@ -41,6 +42,13 @@ static DEFINE_MUTEX(domain_list_lock);
  * This applies globally to all traffic the CPU generates.
  */
 static bool cdp_enabled;
+
+/*
+ * We use cacheinfo to discover the size of the caches and their id. cacheinfo
+ * populates this from a device_initcall(). mpam_resctrl_setup() must wait.
+ */
+static bool cacheinfo_ready;
+static DECLARE_WAIT_QUEUE_HEAD(wait_cacheinfo_ready);
 
 bool resctrl_arch_alloc_capable(void)
 {
@@ -757,6 +765,8 @@ int mpam_resctrl_setup(void)
 	struct mpam_resctrl_res *res;
 	enum resctrl_res_level rid;
 
+	wait_event(wait_cacheinfo_ready, cacheinfo_ready);
+
 	cpus_read_lock();
 	for_each_mpam_resctrl_control(res, rid) {
 		INIT_LIST_HEAD_RCU(&res->resctrl_res.ctrl_domains);
@@ -794,3 +804,12 @@ int mpam_resctrl_setup(void)
 
 	return 0;
 }
+
+static int __init __cacheinfo_ready(void)
+{
+	cacheinfo_ready = true;
+	wake_up(&wait_cacheinfo_ready);
+
+	return 0;
+}
+device_initcall_sync(__cacheinfo_ready);
