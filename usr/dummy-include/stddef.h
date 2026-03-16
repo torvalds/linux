b187c1a1a0513593663b1adee0340d41d833d16d
Thomas Weißschuh <linux@weissschuh.net>
2026-03-16 17:52:01 +0100

    kbuild: uapi: provide a C++ compatible dummy definition of NULL
    NULL works differently in C++ compared to C.
    
    To allow testing the UAPI headers against C++ compilers, provide
    a variant of NULL which works with those.
    
    Signed-off-by: Thomas Weißschuh <linux@weissschuh.net>
    Reviewed-by: Nathan Chancellor <nathan@kernel.org>
    Link: https://patch.msgid.link/20260316-kbuild-uapi-c-v2-4-35d6d0ed863f@weissschuh.net
    Signed-off-by: Nicolas Schier <nsc@kernel.org>

--- usr/dummy-include/stddef.h
+++ usr/dummy-include/stddef.h
@@ -3,6 +3,11 @@
 #define _DUMMY_STDDEF_H
 
 #define offsetof(TYPE, MEMBER)	__builtin_offsetof(TYPE, MEMBER)
+
+#ifdef __cplusplus
+#define NULL 0
+#else
 #define NULL ((void *)0)
+#endif
 
 #endif /* _DUMMY_STDDEF_H */
