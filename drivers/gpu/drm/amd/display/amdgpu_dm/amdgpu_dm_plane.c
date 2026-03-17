6e9d61769b2b65e414fa9996da2b544a9eb45df5
Harry Wentland <harry.wentland@amd.com>
2026-03-17 13:04:51 -0400

    Revert "drm/amd/display: Add NV12/P010 formats to primary plane"
    With this change we're adding NV12 and P010 twice to reported
    formats on a primary plane, which causes us to hit an assert
    in Weston.
    
    This reverts commit 63fff551318f5e0814b94f709a6dfaec789dcd7a.
    
    Fixes: 63fff551318f ("drm/amd/display: Add NV12/P010 formats to primary plane")
    Signed-off-by: Harry Wentland <harry.wentland@amd.com>
    Reviewed-by: Leo Li <sunpeng.li@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
@@ -62,9 +62,6 @@ static const uint32_t rgb_formats[] = {
 	DRM_FORMAT_XBGR8888,
 	DRM_FORMAT_ABGR8888,
 	DRM_FORMAT_RGB565,
-	DRM_FORMAT_NV21,
-	DRM_FORMAT_NV12,
-	DRM_FORMAT_P010
 };
 
 static const uint32_t overlay_formats[] = {
