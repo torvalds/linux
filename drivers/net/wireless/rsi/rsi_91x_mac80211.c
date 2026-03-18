777d8ba5aada960c666f810d5d820ab55ebb64c3
Ville Nummela <ville.nummela@kempower.com>
2026-03-18 10:19:12 +0200

    wifi: rsi_91x_usb: do not pause rfkill polling when stopping mac80211
    Removing rsi_91x USB adapter could cause rtnetlink to lock up.
    When rsi_mac80211_stop is called, wiphy_lock is locked. Call to
    wiphy_rfkill_stop_polling would wait until the work queue has
    finished, but because the work queue waits for wiphy_lock, that
    would never happen.
    
    Moving the call to rsi_disconnect avoids the lock up.
    
    Signed-off-by: Ville Nummela <ville.nummela@kempower.com>
    Link: https://patch.msgid.link/20260318081912.87744-1-ville.nummela@kempower.com
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/rsi/rsi_91x_mac80211.c
+++ drivers/net/wireless/rsi/rsi_91x_mac80211.c
@@ -326,6 +326,22 @@ void rsi_mac80211_detach(struct rsi_hw *adapter)
 EXPORT_SYMBOL_GPL(rsi_mac80211_detach);
 
 /**
+ * rsi_mac80211_rfkill_exit() - This function is used to stop rfkill polling
+ *                              when the device is removed.
+ * @adapter: Pointer to the adapter structure.
+ *
+ * Return: None.
+ */
+void rsi_mac80211_rfkill_exit(struct rsi_hw *adapter)
+{
+	struct ieee80211_hw *hw = adapter->hw;
+
+	if (hw)
+		wiphy_rfkill_stop_polling(hw->wiphy);
+}
+EXPORT_SYMBOL_GPL(rsi_mac80211_rfkill_exit);
+
+/**
  * rsi_indicate_tx_status() - This function indicates the transmit status.
  * @adapter: Pointer to the adapter structure.
  * @skb: Pointer to the socket buffer structure.
@@ -422,7 +438,6 @@ static void rsi_mac80211_stop(struct ieee80211_hw *hw, bool suspend)
 	rsi_dbg(ERR_ZONE, "===> Interface DOWN <===\n");
 	mutex_lock(&common->mutex);
 	common->iface_down = true;
-	wiphy_rfkill_stop_polling(hw->wiphy);
 
 	/* Block all rx frames */
 	rsi_send_rx_filter_frame(common, 0xffff);
