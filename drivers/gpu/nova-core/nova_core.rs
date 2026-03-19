09691f5d807065a1d3d3042e2d8c2e0c170d7711
Timur Tabi <ttabi@nvidia.com>
2026-03-19 16:26:57 -0500

    gpu: nova-core: create debugfs root in module init
    Create the 'nova_core' root debugfs entry when the driver loads.
    
    Normally, non-const global variables need to be protected by a
    mutex.  Instead, we use unsafe code, as we know the entry is never
    modified after the driver is loaded.  This solves the lifetime
    issue of the mutex guard, which would otherwise have required the
    use of `pin_init_scope`.
    
    Signed-off-by: Timur Tabi <ttabi@nvidia.com>
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Reviewed-by: Alexandre Courbot <acourbot@nvidia.com>
    Tested-by: John Hubbard <jhubbard@nvidia.com>
    Tested-by: Eliot Courtney <ecourtney@nvidia.com>
    Link: https://patch.msgid.link/20260319212658.2541610-6-ttabi@nvidia.com
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- drivers/gpu/nova-core/nova_core.rs
+++ drivers/gpu/nova-core/nova_core.rs
@@ -3,6 +3,7 @@
 //! Nova Core GPU Driver
 
 use kernel::{
+    debugfs,
     driver::Registration,
     pci,
     prelude::*,
@@ -27,16 +28,40 @@ mod vbios;
 
 pub(crate) const MODULE_NAME: &core::ffi::CStr = <LocalModule as kernel::ModuleMetadata>::NAME;
 
+// TODO: Move this into per-module data once that exists.
+static mut DEBUGFS_ROOT: Option<debugfs::Dir> = None;
+
+/// Guard that clears `DEBUGFS_ROOT` when dropped.
+struct DebugfsRootGuard;
+
+impl Drop for DebugfsRootGuard {
+    fn drop(&mut self) {
+        // SAFETY: This guard is dropped after `_driver` (due to field order),
+        // so the driver is unregistered and no probe() can be running.
+        unsafe { DEBUGFS_ROOT = None };
+    }
+}
+
 #[pin_data]
 struct NovaCoreModule {
+    // Fields are dropped in declaration order, so `_driver` is dropped first,
+    // then `_debugfs_guard` clears `DEBUGFS_ROOT`.
     #[pin]
     _driver: Registration<pci::Adapter<driver::NovaCore>>,
+    _debugfs_guard: DebugfsRootGuard,
 }
 
 impl InPlaceModule for NovaCoreModule {
     fn init(module: &'static kernel::ThisModule) -> impl PinInit<Self, Error> {
+        let dir = debugfs::Dir::new(kernel::c_str!("nova_core"));
+
+        // SAFETY: We are the only driver code running during init, so there
+        // cannot be any concurrent access to `DEBUGFS_ROOT`.
+        unsafe { DEBUGFS_ROOT = Some(dir) };
+
         try_pin_init!(Self {
             _driver <- Registration::new(MODULE_NAME, module),
+            _debugfs_guard: DebugfsRootGuard,
         })
     }
 }
