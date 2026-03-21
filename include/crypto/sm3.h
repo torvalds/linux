324bb3bb75ac21adbbc7e6ea5cdb0a735fb78a56
Eric Biggers <ebiggers@kernel.org>
2026-03-20 21:09:27 -0700

    lib/crypto: sm3: Add SM3 library API
    Add a straightforward library API for SM3, mirroring the ones for the
    other hash algorithms.  It uses the existing generic implementation of
    SM3's compression function in lib/crypto/sm3.c.  Hooks are added for
    architecture-optimized implementations, which later commits will wire up
    to the existing optimized SM3 code for arm64, riscv, and x86.
    
    Note that the rationale for this is *not* that SM3 should be used, or
    that any kernel subsystem currently seems like a candidate for switching
    from the sm3 crypto_shash to SM3 library.  (SM3, in fact, shouldn't be
    used.  Likewise you shouldn't use MD5, SHA-1, RC4, etc...)
    
    Rather, it's just that this will simplify how the kernel's existing SM3
    code is integrated and make it much easier to maintain and test.  SM3 is
    one of the only hash algorithms with arch-optimized code that is still
    integrated in the old way.  By converting it to the new lib/crypto/ code
    organization, we'll only have to keep track of one way of doing things.
    The library will also get a KUnit test suite (as usual for lib/crypto/),
    so it will become more easily and comprehensively tested as well.
    
    Skip adding functions for HMAC-SM3 for now, though.  There's not as much
    point in adding those right now.
    
    Note: similar to the other hash algorithms, the library API uses
    'struct sm3_ctx', not 'struct sm3_state'.  The existing 'struct
    sm3_state' and the sm3_block_generic() function which uses it are
    temporarily kept around until their users are updated by later commits.
    
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Link: https://lore.kernel.org/r/20260321040935.410034-5-ebiggers@kernel.org
    Signed-off-by: Eric Biggers <ebiggers@kernel.org>

--- include/crypto/sm3.h
+++ include/crypto/sm3.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Common values for SM3 algorithm
+ * SM3 hash algorithm
  *
  * Copyright (C) 2017 ARM Limited or its affiliates.
  * Copyright (C) 2017 Gilad Ben-Yossef <gilad@benyossef.com>
@@ -31,16 +31,66 @@ struct sm3_state {
 	u8 buffer[SM3_BLOCK_SIZE];
 };
 
-/*
- * Stand-alone implementation of the SM3 algorithm. It is designed to
- * have as little dependencies as possible so it can be used in the
- * kexec_file purgatory. In other cases you should generally use the
- * hash APIs from include/crypto/hash.h. Especially when hashing large
- * amounts of data as those APIs may be hw-accelerated.
- *
- * For details see lib/crypto/sm3.c
- */
-
 void sm3_block_generic(struct sm3_state *sctx, u8 const *data, int blocks);
 
-#endif
+/* State for the SM3 compression function */
+struct sm3_block_state {
+	u32 h[SM3_DIGEST_SIZE / 4];
+};
+
+/**
+ * struct sm3_ctx - Context for hashing a message with SM3
+ * @state: the compression function state
+ * @bytecount: number of bytes processed so far
+ * @buf: partial block buffer; bytecount % SM3_BLOCK_SIZE bytes are valid
+ */
+struct sm3_ctx {
+	struct sm3_block_state state;
+	u64 bytecount;
+	u8 buf[SM3_BLOCK_SIZE] __aligned(__alignof__(__be64));
+};
+
+/**
+ * sm3_init() - Initialize an SM3 context for a new message
+ * @ctx: the context to initialize
+ *
+ * If you don't need incremental computation, consider sm3() instead.
+ *
+ * Context: Any context.
+ */
+void sm3_init(struct sm3_ctx *ctx);
+
+/**
+ * sm3_update() - Update an SM3 context with message data
+ * @ctx: the context to update; must have been initialized
+ * @data: the message data
+ * @len: the data length in bytes
+ *
+ * This can be called any number of times.
+ *
+ * Context: Any context.
+ */
+void sm3_update(struct sm3_ctx *ctx, const u8 *data, size_t len);
+
+/**
+ * sm3_final() - Finish computing an SM3 message digest
+ * @ctx: the context to finalize; must have been initialized
+ * @out: (output) the resulting SM3 message digest
+ *
+ * After finishing, this zeroizes @ctx.  So the caller does not need to do it.
+ *
+ * Context: Any context.
+ */
+void sm3_final(struct sm3_ctx *ctx, u8 out[at_least SM3_DIGEST_SIZE]);
+
+/**
+ * sm3() - Compute SM3 message digest in one shot
+ * @data: the message data
+ * @len: the data length in bytes
+ * @out: (output) the resulting SM3 message digest
+ *
+ * Context: Any context.
+ */
+void sm3(const u8 *data, size_t len, u8 out[at_least SM3_DIGEST_SIZE]);
+
+#endif /* _CRYPTO_SM3_H */
