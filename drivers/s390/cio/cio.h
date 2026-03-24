ac4d8bb6e2e13e8684a76ea48d13ebaaaf5c24c4
Danilo Krummrich <dakr@kernel.org>
2026-03-24 01:59:13 +0100

    s390/cio: use generic driver_override infrastructure
    When a driver is probed through __driver_attach(), the bus' match()
    callback is called without the device lock held, thus accessing the
    driver_override field without a lock, which can cause a UAF.
    
    Fix this by using the driver-core driver_override infrastructure taking
    care of proper locking internally.
    
    Note that calling match() from __driver_attach() without the device lock
    held is intentional. [1]
    
    Link: https://lore.kernel.org/driver-core/DGRGTIRHA62X.3RY09D9SOK77P@kernel.org/ [1]
    Reported-by: Gui-Dong Han <hanguidong02@gmail.com>
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=220789
    Fixes: ebc3d1791503 ("s390/cio: introduce driver_override on the css bus")
    Reviewed-by: Vineeth Vijayan <vneethv@linux.ibm.com>
    Link: https://patch.msgid.link/20260324005919.2408620-10-dakr@kernel.org
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- drivers/s390/cio/cio.h
+++ drivers/s390/cio/cio.h
@@ -103,11 +103,6 @@ struct subchannel {
 	struct work_struct todo_work;
 	struct schib_config config;
 	u64 dma_mask;
-	/*
-	 * Driver name to force a match.  Do not set directly, because core
-	 * frees it.  Use driver_set_override() to set or clear it.
-	 */
-	const char *driver_override;
 } __attribute__ ((aligned(8)));
 
 DECLARE_PER_CPU_ALIGNED(struct irb, cio_irb);
