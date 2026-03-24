1c78c2002380a1fe31bfb01a3d5f29809e55a096
Johan Hovold <johan@kernel.org>
2026-03-24 09:23:22 +0100

    spi: imx: fix use-after-free on unbind
    The SPI subsystem frees the controller and any subsystem allocated
    driver data as part of deregistration (unless the allocation is device
    managed).
    
    Take another reference before deregistering the controller so that the
    driver data is not freed until the driver is done with it.
    
    Fixes: 307c897db762 ("spi: spi-imx: replace struct spi_imx_data::bitbang by pointer to struct spi_controller")
    Cc: stable@vger.kernel.org	# 5.19
    Acked-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Link: https://patch.msgid.link/20260324082326.901043-2-johan@kernel.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-imx.c
+++ drivers/spi/spi-imx.c
@@ -2401,6 +2401,8 @@ static void spi_imx_remove(struct platform_device *pdev)
 	struct spi_imx_data *spi_imx = spi_controller_get_devdata(controller);
 	int ret;
 
+	spi_controller_get(controller);
+
 	spi_unregister_controller(controller);
 
 	ret = pm_runtime_get_sync(spi_imx->dev);
@@ -2414,6 +2416,8 @@ static void spi_imx_remove(struct platform_device *pdev)
 	pm_runtime_disable(spi_imx->dev);
 
 	spi_imx_sdma_exit(spi_imx);
+
+	spi_controller_put(controller);
 }
 
 static int spi_imx_runtime_resume(struct device *dev)
