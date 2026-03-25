e0da9859cfbc9a8cf9a879b00931b89f590de0c1
Zong-Zhe Yang <kevin_yang@realtek.com>
2026-03-25 15:21:30 +0800

    wifi: rtw89: fw: load TX power elements according to AID
    For different A-die, there will be different TX power parameters.
    In FW element header, the corresponding A-die ID will be described.
    So, compare runtime AID with that to load the target TX power
    parameters.
    
    Signed-off-by: Zong-Zhe Yang <kevin_yang@realtek.com>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://patch.msgid.link/20260325072130.41751-9-pkshih@realtek.com

--- drivers/net/wireless/realtek/rtw89/fw.c
+++ drivers/net/wireless/realtek/rtw89/fw.c
@@ -1153,7 +1153,12 @@ int rtw89_fw_recognize_txpwr_from_elm(struct rtw89_dev *rtwdev,
 	const struct __rtw89_fw_txpwr_element *txpwr_elm = &elm->u.txpwr;
 	const unsigned long offset = arg.offset;
 	struct rtw89_efuse *efuse = &rtwdev->efuse;
+	struct rtw89_hal *hal = &rtwdev->hal;
+	u16 aid = le16_to_cpu(elm->aid);
 	struct rtw89_txpwr_conf *conf;
+
+	if (aid && aid != hal->aid)
+		return 1;
 
 	if (!rtwdev->rfe_data) {
 		rtwdev->rfe_data = kzalloc_obj(*rtwdev->rfe_data);
