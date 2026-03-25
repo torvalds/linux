c349e45fbe1f752432f291d24f43d67ab9007758
Herve Codina <herve.codina@bootlin.com>
2026-03-25 15:35:44 +0100

    of: property: Allow fw_devlink device-tree on x86
    PCI drivers can use a device-tree overlay to describe the hardware
    available on the PCI board. This is the case, for instance, of the
    LAN966x PCI device driver.
    
    Adding some more nodes in the device-tree overlay adds some more
    consumer/supplier relationship between devices instantiated from this
    overlay.
    
    Those fw_node consumer/supplier relationships are handled by fw_devlink
    and are created based on the device-tree parsing done by the
    of_fwnode_add_links() function.
    
    Those consumer/supplier links are needed in order to ensure a correct PM
    runtime management and a correct removal order between devices.
    
    For instance, without those links a supplier can be removed before its
    consumers is removed leading to all kind of issue if this consumer still
    want the use the already removed supplier.
    
    The support for the usage of an overlay from a PCI driver has been added
    on x86 systems in commit 1f340724419ed ("PCI: of: Create device tree PCI
    host bridge node").
    
    In the past, support for fw_devlink on x86 had been tried but this
    support has been removed in commit 4a48b66b3f52 ("of: property: Disable
    fw_devlink DT support for X86"). Indeed, this support was breaking some
    x86 systems such as OLPC system and the regression was reported in [0].
    
    Instead of disabling this support for all x86 system, use a finer grain
    and disable this support only for the possible problematic subset of x86
    systems (at least OLPC and CE4100).
    
    Those systems use a device-tree to describe their hardware. Identify
    those systems using key properties in the device-tree.
    
    Signed-off-by: Herve Codina <herve.codina@bootlin.com>
    Link: https://lore.kernel.org/lkml/3c1f2473-92ad-bfc4-258e-a5a08ad73dd0@web.de/ [0]
    Link: https://patch.msgid.link/20260325143555.451852-18-herve.codina@bootlin.com
    Signed-off-by: Rob Herring (Arm) <robh@kernel.org>

--- drivers/of/property.c
+++ drivers/of/property.c
@@ -1620,12 +1620,36 @@ static int of_fwnode_irq_get(const struct fwnode_handle *fwnode,
 	return of_irq_get(to_of_node(fwnode), index);
 }
 
+static int match_property_by_path(const char *node_path, const char *prop_name,
+				  const char *value)
+{
+	struct device_node *np __free(device_node) = of_find_node_by_path(node_path);
+
+	return of_property_match_string(np, prop_name, value);
+}
+
+static bool of_is_fwnode_add_links_supported(void)
+{
+	static int is_supported = -1;
+
+	if (!IS_ENABLED(CONFIG_X86))
+		return true;
+
+	if (is_supported != -1)
+		return !!is_supported;
+
+	is_supported = !((match_property_by_path("/soc", "compatible", "intel,ce4100-cp") >= 0) ||
+			 (match_property_by_path("/", "architecture", "OLPC") >= 0));
+
+	return !!is_supported;
+}
+
 static int of_fwnode_add_links(struct fwnode_handle *fwnode)
 {
 	const struct property *p;
 	struct device_node *con_np = to_of_node(fwnode);
 
-	if (IS_ENABLED(CONFIG_X86))
+	if (!of_is_fwnode_add_links_supported())
 		return 0;
 
 	if (!con_np)
