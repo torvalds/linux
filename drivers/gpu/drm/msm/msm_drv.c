47cbfe2608314b833ad61a65827d8fb363bc2d2d
Yasuaki Torimaru <yasuakitorimaru@gmail.com>
2026-03-25 20:46:34 +0900

    drm/msm/gem: fix error handling in msm_ioctl_gem_info_get_metadata()
    msm_ioctl_gem_info_get_metadata() always returns 0 regardless of
    errors. When copy_to_user() fails or the user buffer is too small,
    the error code stored in ret is ignored because the function
    unconditionally returns 0. This causes userspace to believe the
    ioctl succeeded when it did not.
    
    Additionally, kmemdup() can return NULL on allocation failure, but
    the return value is not checked. This leads to a NULL pointer
    dereference in the subsequent copy_to_user() call.
    
    Add the missing NULL check for kmemdup() and return ret instead of 0.
    
    Note that the SET counterpart (msm_ioctl_gem_info_set_metadata)
    correctly returns ret.
    
    Fixes: 9902cb999e4e ("drm/msm/gem: Add metadata")
    Cc: stable@vger.kernel.org
    Signed-off-by: Yasuaki Torimaru <yasuakitorimaru@gmail.com>
    Patchwork: https://patchwork.freedesktop.org/patch/714478/
    Message-ID: <20260325114635.383241-1-yasuakitorimaru@gmail.com>
    Signed-off-by: Rob Clark <robin.clark@oss.qualcomm.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -536,6 +536,11 @@ static int msm_ioctl_gem_info_get_metadata(struct drm_gem_object *obj,
 	len = msm_obj->metadata_size;
 	buf = kmemdup(msm_obj->metadata, len, GFP_KERNEL);
 
+	if (!buf) {
+		msm_gem_unlock(obj);
+		return -ENOMEM;
+	}
+
 	msm_gem_unlock(obj);
 
 	if (*metadata_size < len) {
@@ -548,7 +553,7 @@ static int msm_ioctl_gem_info_get_metadata(struct drm_gem_object *obj,
 
 	kfree(buf);
 
-	return 0;
+	return ret;
 }
 
 static int msm_ioctl_gem_info(struct drm_device *dev, void *data,
