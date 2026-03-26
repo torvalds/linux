07b7d66e65d9cfe6b9c2c34aa22cfcaac37a5c45
Christian A. Ehrhardt <lk@c--e.de>
2026-03-26 22:49:01 +0100

    lib/scatterlist: fix length calculations in extract_kvec_to_sg
    Patch series "Fix bugs in extract_iter_to_sg()", v3.
    
    Fix bugs in the kvec and user variants of extract_iter_to_sg.  This series
    is growing due to useful remarks made by sashiko.dev.
    
    The main bugs are:
    - The length for an sglist entry when extracting from
      a kvec can exceed the number of bytes in the page. This
      is obviously not intended.
    - When extracting a user buffer the sglist is temporarily
      used as a scratch buffer for extracted page pointers.
      If the sglist already contains some elements this scratch
      buffer could overlap with existing entries in the sglist.
    
    The series adds test cases to the kunit_iov_iter test that demonstrate all
    of these bugs.  Additionally, there is a memory leak fix for the test
    itself.
    
    The bugs were orignally introduced into kernel v6.3 where the function
    lived in fs/netfs/iterator.c.  It was later moved to lib/scatterlist.c in
    v6.5.  Thus the actual fix is only marked for backports to v6.5+.
    
    
    This patch (of 5):
    
    When extracting from a kvec to a scatterlist, do not cross page
    boundaries.  The required length was already calculated but not used as
    intended.
    
    Adjust the copied length if the loop runs out of sglist entries without
    extracting everything.
    
    While there, return immediately from extract_iter_to_sg if there are no
    sglist entries at all.
    
    A subsequent commit will add kunit test cases that demonstrate that the
    patch is necessary.
    
    Link: https://lkml.kernel.org/r/20260326214905.818170-1-lk@c--e.de
    Link: https://lkml.kernel.org/r/20260326214905.818170-2-lk@c--e.de
    Fixes: 018584697533 ("netfs: Add a function to extract an iterator into a scatterlist")
    Signed-off-by: Christian A. Ehrhardt <lk@c--e.de>
    Cc: David Gow <davidgow@google.com>
    Cc: David Howells <dhowells@redhat.com>
    Cc: Kees Cook <kees@kernel.org>
    Cc: Petr Mladek <pmladek@suse.com>
    Cc: <stable@vger.kernel.org>	[v6.5+]
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- lib/scatterlist.c
+++ lib/scatterlist.c
@@ -1247,7 +1247,7 @@ static ssize_t extract_kvec_to_sg(struct iov_iter *iter,
 			else
 				page = virt_to_page((void *)kaddr);
 
-			sg_set_page(sg, page, len, off);
+			sg_set_page(sg, page, seg, off);
 			sgtable->nents++;
 			sg++;
 			sg_max--;
@@ -1256,6 +1256,7 @@ static ssize_t extract_kvec_to_sg(struct iov_iter *iter,
 			kaddr += PAGE_SIZE;
 			off = 0;
 		} while (len > 0 && sg_max > 0);
+		ret -= len;
 
 		if (maxsize <= 0 || sg_max == 0)
 			break;
@@ -1409,7 +1410,7 @@ ssize_t extract_iter_to_sg(struct iov_iter *iter, size_t maxsize,
 			   struct sg_table *sgtable, unsigned int sg_max,
 			   iov_iter_extraction_t extraction_flags)
 {
-	if (maxsize == 0)
+	if (maxsize == 0 || sg_max == 0)
 		return 0;
 
 	switch (iov_iter_type(iter)) {
