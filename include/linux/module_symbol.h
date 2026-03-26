10a4eb5882ba16164ece86d99486084f02f148bb
Siddharth Nayyar <sidnayyar@google.com>
2026-03-26 21:25:02 +0000

    module: define ksym_flags enumeration to represent kernel symbol flags
    The core architectural issue with kernel symbol flags is our reliance on
    splitting the main symbol table, ksymtab. To handle a single boolean
    property, such as GPL-only, all exported symbols are split across two
    separate tables: __ksymtab and __ksymtab_gpl.
    
    This design forces the module loader to perform a separate search on
    each of these tables for every symbol it needs, for vmlinux and for all
    previously loaded modules.
    
    This approach is fundamentally not scalable. If we were to introduce a
    second flag, we would need four distinct symbol tables. For n boolean
    flags, this model requires an exponential growth to 2^n tables,
    dramatically increasing complexity.
    
    Another consequence of this fragmentation is degraded performance. For
    example, a binary search on the symbol table of vmlinux, that would take
    only 14 comparison steps (assuming ~2^14 or 16K symbols) in a unified
    table, can require up to 26 steps when spread across two tables
    (assuming both tables have ~2^13 symbols). This performance penalty
    worsens as more flags are added.
    
    To address this, symbol flags is an enumeration used to represent flags
    as a bitset, for example a flag to tell if a symbol is GPL only.
    
    The said bitset is introduced in subsequent patches and will contain
    values of kernel symbol flags. These bitset will then be used to infer
    flag values rather than fragmenting ksymtab for separating symbols with
    different flag values, thereby eliminating the need to fragment the
    ksymtab.
    
    Link: https://lore.kernel.org/r/20260326-kflagstab-v5-0-fa0796fe88d9@google.com
    Signed-off-by: Siddharth Nayyar <sidnayyar@google.com>
    Reviewed-by: Petr Pavlu <petr.pavlu@suse.com>
    [Sami: Updated the commit message to explain the use case for the series.]
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- include/linux/module_symbol.h
+++ include/linux/module_symbol.h
@@ -2,6 +2,11 @@
 #ifndef _LINUX_MODULE_SYMBOL_H
 #define _LINUX_MODULE_SYMBOL_H
 
+/* Kernel symbol flags bitset. */
+enum ksym_flags {
+	KSYM_FLAG_GPL_ONLY	= 1 << 0,
+};
+
 /* This ignores the intensely annoying "mapping symbols" found in ELF files. */
 static inline bool is_mapping_symbol(const char *str)
 {
