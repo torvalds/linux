caaa184b424322beb9dfd05ec3485de665362c94
Jan Kara <jack@suse.cz>
2026-03-26 10:54:33 +0100

    minix: Track metadata bhs in fs-private inode part
    Track metadata bhs for an inode in fs-private part of the inode.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Link: https://patch.msgid.link/20260326095354.16340-81-jack@suse.cz
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- fs/minix/inode.c
+++ fs/minix/inode.c
@@ -49,9 +49,9 @@ static void minix_evict_inode(struct inode *inode)
 		inode->i_size = 0;
 		minix_truncate(inode);
 	} else {
-		sync_mapping_buffers(&inode->i_data);
+		mmb_sync(&minix_i(inode)->i_metadata_bhs);
 	}
-	invalidate_inode_buffers(inode);
+	mmb_invalidate(&minix_i(inode)->i_metadata_bhs);
 	clear_inode(inode);
 	if (!inode->i_nlink)
 		minix_free_inode(inode);
@@ -85,6 +85,8 @@ static struct inode *minix_alloc_inode(struct super_block *sb)
 	ei = alloc_inode_sb(sb, minix_inode_cachep, GFP_KERNEL);
 	if (!ei)
 		return NULL;
+	mmb_init(&ei->i_metadata_bhs, &ei->vfs_inode.i_data);
+
 	return &ei->vfs_inode;
 }
 
