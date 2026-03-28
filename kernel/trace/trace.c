724d197aaea19e4f2012fca5b0e30ae690458de3
Steven Rostedt <rostedt@goodmis.org>
2026-03-28 10:19:46 -0400

    tracing: Remove tracing_alloc_snapshot() when snapshot isn't defined
    The function tracing_alloc_snapshot() is only used between trace.c and
    trace_snapshot.c. When snapshot isn't configured, it's not used at all.
    The stub function was defined as a global with no users and no prototype
    causing build issues.
    
    Remove the function when snapshot isn't configured as nothing is calling
    it.
    
    Also remove the EXPORT_SYMBOL_GPL() that was associated with it as it's
    not used outside of the tracing subsystem which also includes any modules.
    
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Link: https://patch.msgid.link/20260328101946.2c4ef4a5@robin
    Reported-by: Mark Brown <broonie@kernel.org>
    Closes: https://lore.kernel.org/all/acb-IuZ4vDkwwQLW@sirena.co.uk/
    Fixes: bade44fe546212 (tracing: Move snapshot code out of trace.c and into trace_snapshot.c)
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -810,19 +810,12 @@ int tracing_alloc_snapshot(void)
 
 	return ret;
 }
-EXPORT_SYMBOL_GPL(tracing_alloc_snapshot);
 #else
 void tracing_snapshot(void)
 {
 	WARN_ONCE(1, "Snapshot feature not enabled, but internal snapshot used");
 }
 EXPORT_SYMBOL_GPL(tracing_snapshot);
-int tracing_alloc_snapshot(void)
-{
-	WARN_ONCE(1, "Snapshot feature not enabled, but snapshot allocation used");
-	return -ENODEV;
-}
-EXPORT_SYMBOL_GPL(tracing_alloc_snapshot);
 void tracing_snapshot_alloc(void)
 {
 	/* Give warning */
