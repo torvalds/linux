27c299698464c515c5cd97b4fcf1a0e38600b2ac
Zhang Heng <zhangheng@kylinos.cn>
2026-03-30 17:51:33 +0800

    ASoC: amd: yc: Add DMI quirk for ASUS Vivobook Pro 16X OLED M7601RM
    Add a DMI quirk for the ASUS Vivobook Pro 16X OLED M7601RM fixing the
    issue where the internal microphone was not detected.
    
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=221288
    Signed-off-by: Zhang Heng <zhangheng@kylinos.cn>
    Link: https://patch.msgid.link/20260330095133.81786-1-zhangheng@kylinos.cn
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/yc/acp6x-mach.c
+++ sound/soc/amd/yc/acp6x-mach.c
@@ -731,6 +731,13 @@ static const struct dmi_system_id yc_acp_quirk_table[] = {
 			DMI_MATCH(DMI_PRODUCT_NAME, "Thin A15 B7VE"),
 		}
 	},
+	{
+		.driver_data = &acp6x_card,
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "ASUSTeK COMPUTER INC."),
+			DMI_MATCH(DMI_PRODUCT_NAME, "M7601RM"),
+		}
+	},
 	{}
 };
 
