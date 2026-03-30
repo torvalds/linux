78ec5bf2f589ec7fd8f169394bfeca541b077317
Fredric Cover <FredTheDude@proton.me>
2026-03-30 13:11:27 -0700

    fs/smb/client: fix out-of-bounds read in cifs_sanitize_prepath
    When cifs_sanitize_prepath is called with an empty string or a string
    containing only delimiters (e.g., "/"), the current logic attempts to
    check *(cursor2 - 1) before cursor2 has advanced. This results in an
    out-of-bounds read.
    
    This patch adds an early exit check after stripping prepended
    delimiters. If no path content remains, the function returns NULL.
    
    The bug was identified via manual audit and verified using a
    standalone test case compiled with AddressSanitizer, which
    triggered a SEGV on affected inputs.
    
    Signed-off-by: Fredric Cover <FredTheDude@proton.me>
    Reviewed-by: Henrique Carvalho <[2]henrique.carvalho@suse.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/smb/client/fs_context.c
+++ fs/smb/client/fs_context.c
@@ -588,6 +588,10 @@ char *cifs_sanitize_prepath(char *prepath, gfp_t gfp)
 	while (IS_DELIM(*cursor1))
 		cursor1++;
 
+	/* exit in case of only delimiters */
+	if (!*cursor1)
+		return NULL;
+
 	/* copy the first letter */
 	*cursor2 = *cursor1;
 
