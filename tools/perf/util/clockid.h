623030fd0ad59ecc4197b0c0f8dd066a0f0598b3
Ian Rogers <irogers@google.com>
2026-04-01 09:13:17 -0700

    perf clockid: Add missing include
    clockid_t is declared in time.h but the include is missing. Reordering
    header files may result in build breakages. Add the include to avoid
    this.
    
    Signed-off-by: Ian Rogers <irogers@google.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/clockid.h
+++ tools/perf/util/clockid.h
@@ -1,7 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-
 #ifndef __PERF_CLOCKID_H
 #define __PERF_CLOCKID_H
+
+#include <time.h>
 
 struct option;
 int parse_clockid(const struct option *opt, const char *str, int unset);
