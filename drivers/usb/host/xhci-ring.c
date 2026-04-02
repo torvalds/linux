88a985cf9533c6585ee3a784cb8320df409be389
Michal Pecio <michal.pecio@gmail.com>
2026-04-02 16:13:19 +0300

    usb: xhci: Simplify clearing the Event Interrupt bit
    USBSTS is mostly RW1C, so to clear EINT we should write just this
    one bit. Remove pointless code which ORs the bit with current value
    of the register, even though the bit is already known to be set,
    and writes the result back, which clears all active RW1C flags.
    
    We used to inadvertently clear PCD and SRE in this way. PCD isn't
    used by the driver and SRE is only used at resume, so clearing them
    should make no difference. Don't clear them anymore.
    
    Tested by connecting and mounting a storage device on a few HCs.
    
    Before: xhci_irq USBSTS 0x00000018 EINT PCD -> 0x00000000
            xhci_irq USBSTS 0x00000008 EINT -> 0x00000000
    After:  xhci_irq USBSTS 0x00000018 EINT PCD -> 0x00000010 PCD
            xhci_irq USBSTS 0x00000018 EINT PCD -> 0x00000010 PCD
    
    Some flags are RsvdZ - should be written as zero regardless of the
    value read, so technically it was a bug. But no problems are known.
    
    Signed-off-by: Michal Pecio <michal.pecio@gmail.com>
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Link: https://patch.msgid.link/20260402131342.2628648-3-mathias.nyman@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -3208,10 +3208,9 @@ irqreturn_t xhci_irq(struct usb_hcd *hcd)
 	/*
 	 * Clear the op reg interrupt status first,
 	 * so we can receive interrupts from other MSI-X interrupters.
-	 * Write 1 to clear the interrupt status.
+	 * USBSTS bits are write 1 to clear.
 	 */
-	status |= STS_EINT;
-	writel(status, &xhci->op_regs->status);
+	writel(STS_EINT, &xhci->op_regs->status);
 
 	/* This is the handler of the primary interrupter */
 	xhci_handle_events(xhci, xhci->interrupters[0], false);
