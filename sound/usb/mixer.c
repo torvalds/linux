4513d3e0bbc0585b86ccf2631902593ff97e88f5
Cryolitia PukNgae <cryolitia.pukngae@linux.dev>
2026-04-02 13:36:57 +0800

    ALSA: usb-audio: apply quirk for MOONDROP JU Jiu
    It(ID 31b2:0111 JU Jiu) reports a MIN value -12800 for volume control, but
    will mute when setting it less than -10880.
    
    Thanks to my girlfriend Kagura for reporting this issue.
    
    Cc: Kagura <me@mail.kagurach.uk>
    Cc: stable@vger.kernel.org
    Signed-off-by: Cryolitia PukNgae <cryolitia.pukngae@linux.dev>
    Link: https://patch.msgid.link/20260402-syy-v1-1-068d3bc30ddc@linux.dev
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/usb/mixer.c
+++ sound/usb/mixer.c
@@ -1204,6 +1204,13 @@ static void volume_control_quirks(struct usb_mixer_elem_info *cval,
 			cval->min = -11264; /* Mute under it */
 		}
 		break;
+	case USB_ID(0x31b2, 0x0111): /* MOONDROP JU Jiu */
+		if (!strcmp(kctl->id.name, "PCM Playback Volume")) {
+			usb_audio_info(chip,
+				       "set volume quirk for MOONDROP JU Jiu\n");
+			cval->min = -10880; /* Mute under it */
+		}
+		break;
 	}
 }
 
