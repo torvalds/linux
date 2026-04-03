53cf44fa727113affda4a17207a54e0d27c7fc78
SunJianHao <24031212195@stu.xidian.edu.cn>
2026-04-03 21:12:05 +0800

    smb/client: avoid null-ptr-deref when tests fail in test_cmp_map()
    Use KUNIT_ASSERT_NOT_NULL() to abort the test cases on failure.
    
    Reported-by: ChenXiaoSong <chenxiaosong@kylinos.cn>
    Signed-off-by: SunJianHao <24031212195@stu.xidian.edu.cn>
    Reviewed-by: ChenXiaoSong <chenxiaosong@kylinos.cn>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/smb/client/smb2maperror_test.c
+++ fs/smb/client/smb2maperror_test.c
@@ -21,7 +21,7 @@ test_cmp_map(struct kunit *test, const struct status_to_posix_error *expect)
 	const struct status_to_posix_error *result;
 
 	result = smb2_get_err_map_test(expect->smb2_status);
-	KUNIT_EXPECT_PTR_NE(test, NULL, result);
+	KUNIT_ASSERT_NOT_NULL(test, result);
 	KUNIT_EXPECT_EQ(test, expect->smb2_status, result->smb2_status);
 	KUNIT_EXPECT_EQ(test, expect->posix_error, result->posix_error);
 	KUNIT_EXPECT_STREQ(test, expect->status_string, result->status_string);
