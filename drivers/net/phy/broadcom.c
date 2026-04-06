bcb3e89fc0ecbe7a2b7ce614b72deda39083ac74
Nicolai Buchwitz <nb@tipi-net.de>
2026-04-06 09:13:08 +0200

    net: phy: broadcom: implement .disable_autonomous_eee for BCM54xx
    Implement the .disable_autonomous_eee callback for the BCM54210E.
    
    In AutogrEEEn mode the PHY manages EEE autonomously. Clearing the
    AutogrEEEn enable bit in MII_BUF_CNTL_0 switches the PHY to Native
    EEE mode.
    
    Signed-off-by: Nicolai Buchwitz <nb@tipi-net.de>
    Reviewed-by: Florian Fainelli <florian.fainelli@broadcom.com>
    Link: https://patch.msgid.link/20260406-devel-autonomous-eee-v1-2-b335e7143711@tipi-net.de
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/phy/broadcom.c
+++ drivers/net/phy/broadcom.c
@@ -1452,6 +1452,12 @@ static int bcm54811_read_status(struct phy_device *phydev)
 	return genphy_read_status(phydev);
 }
 
+static int bcm54xx_disable_autonomous_eee(struct phy_device *phydev)
+{
+	return bcm_phy_modify_exp(phydev, BCM54XX_TOP_MISC_MII_BUF_CNTL0,
+				  BCM54XX_MII_BUF_CNTL0_AUTOGREEEN_EN, 0);
+}
+
 static struct phy_driver broadcom_drivers[] = {
 {
 	PHY_ID_MATCH_MODEL(PHY_ID_BCM5411),
@@ -1495,6 +1501,7 @@ static struct phy_driver broadcom_drivers[] = {
 	.get_wol	= bcm54xx_phy_get_wol,
 	.set_wol	= bcm54xx_phy_set_wol,
 	.led_brightness_set	= bcm_phy_led_brightness_set,
+	.disable_autonomous_eee	= bcm54xx_disable_autonomous_eee,
 }, {
 	PHY_ID_MATCH_MODEL(PHY_ID_BCM5461),
 	.name		= "Broadcom BCM5461",
