19cbb3e0c27f28feb7781641994226cb2ee206a2
Cássio Gabriel <cassiogabrielcontato@gmail.com>
2026-04-07 12:35:42 -0300

    ALSA: tea6330t: add mixer state restore helper
    The InterWave STB variant uses a TEA6330T mixer on its private
    I2C bus. The mixer state is cached in software, but there is no
    helper to push that register image back to hardware after system
    resume.
    
    Add a small restore helper that reapplies the cached TEA6330T
    register image to the device so board drivers can restore the
    external mixer state as part of their PM resume path.
    
    Take snd_i2c_lock() around the full device lookup and restore
    sequence so the bus device list traversal is also protected.
    
    Signed-off-by: Cássio Gabriel <cassiogabrielcontato@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Link: https://patch.msgid.link/20260407-alsa-interwave-pm-v2-2-8dd96c6129e9@gmail.com

--- include/sound/tea6330t.h
+++ include/sound/tea6330t.h
@@ -12,5 +12,6 @@
 int snd_tea6330t_detect(struct snd_i2c_bus *bus, int equalizer);
 int snd_tea6330t_update_mixer(struct snd_card *card, struct snd_i2c_bus *bus,
 			      int equalizer, int fader);
+int snd_tea6330t_restore_mixer(struct snd_i2c_bus *bus);
 
 #endif /* __SOUND_TEA6330T_H */
