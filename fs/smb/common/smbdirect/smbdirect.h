3892007f2bbf8ae2df5374de708282d6895402e9
Stefan Metzmacher <metze@samba.org>
2026-04-07 16:46:28 +0200

    smb: smbdirect: fix copyright header of smbdirect.h
    Everything in smbdirect.h was taken from my out of
    tree prototype.
    
    Cc: Steve French <smfrench@gmail.com>
    Cc: Tom Talpey <tom@talpey.com>
    Cc: Long Li <longli@microsoft.com>
    Cc: Namjae Jeon <linkinjeon@kernel.org>
    Cc: David Howells <dhowells@redhat.com>
    Cc: Henrique Carvalho <henrique.carvalho@suse.com>
    Cc: linux-cifs@vger.kernel.org
    Cc: samba-technical@lists.samba.org
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/smb/common/smbdirect/smbdirect.h
+++ fs/smb/common/smbdirect/smbdirect.h
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
- *   Copyright (C) 2017, Microsoft Corporation.
- *   Copyright (C) 2018, LG Electronics.
+ *   Copyright (C) 2025 Stefan Metzmacher
  */
 
 #ifndef __FS_SMB_COMMON_SMBDIRECT_SMBDIRECT_H__
