daadb7fce1b53336acb195f34bd42d79754afa0e
Takashi Iwai <tiwai@suse.de>
2026-04-09 11:38:21 +0200

    ALSA: hda/cirrus: Clean up with the new GPIO helper
    Use the new GPIO helper function to clean up the open code.
    
    Merely a code refactoring, and no behavior change.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Link: https://patch.msgid.link/20260409093826.1317626-9-tiwai@suse.de

--- sound/hda/codecs/cirrus/cs421x.c
+++ sound/hda/codecs/cirrus/cs421x.c
@@ -442,14 +442,9 @@ static int cs421x_init(struct hda_codec *codec)
 
 	snd_hda_gen_init(codec);
 
-	if (spec->gpio_mask) {
-		snd_hda_codec_write(codec, 0x01, 0, AC_VERB_SET_GPIO_MASK,
-				    spec->gpio_mask);
-		snd_hda_codec_write(codec, 0x01, 0, AC_VERB_SET_GPIO_DIRECTION,
-				    spec->gpio_dir);
-		snd_hda_codec_write(codec, 0x01, 0, AC_VERB_SET_GPIO_DATA,
-				    spec->gpio_data);
-	}
+	if (spec->gpio_mask)
+		snd_hda_codec_set_gpio(codec, spec->gpio_mask, spec->gpio_dir,
+				       spec->gpio_data, 0);
 
 	cs4210_spdif_automute(codec, NULL);
 
