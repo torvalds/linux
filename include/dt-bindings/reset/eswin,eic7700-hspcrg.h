fc8c21a2b5247a56c3b5e2a40e3c59b6805c2212
Xuyang Dong <dongxuyang@eswincomputing.com>
2026-06-05 14:10:02 +0800

    dt-bindings: clock: Add ESWIN eic7700 HSP clock and reset generator
    Add bindings for the high-speed peripherals clock and reset generator
    on the ESWIN EIC7700 HSP.
    
    Acked-by: Conor Dooley <conor.dooley@microchip.com>
    Signed-off-by: Xuyang Dong <dongxuyang@eswincomputing.com>
    Signed-off-by: Brian Masney <bmasney@redhat.com>

---
+++ include/dt-bindings/reset/eswin,eic7700-hspcrg.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright 2026, Beijing ESWIN Computing Technology Co., Ltd..
+ * All rights reserved.
+ *
+ * Device Tree binding constants for EIC7700 HSP reset controller.
+ *
+ * Authors: Xuyang Dong <dongxuyang@eswincomputing.com>
+ */
+
+#ifndef _DT_BINDINGS_ESWIN_EIC7700_HSPCRG_RESET_H_
+#define _DT_BINDINGS_ESWIN_EIC7700_HSPCRG_RESET_H_
+
+#define EIC7700_HSP_RST_SATA_P0		0
+#define EIC7700_HSP_RST_SATA_PHY	1
+#define EIC7700_HSP_RST_USB0		2
+#define EIC7700_HSP_RST_USB1		3
+#define EIC7700_HSP_RST_USB0_PHY	4
+#define EIC7700_HSP_RST_USB1_PHY	5
+
+#endif /* _DT_BINDINGS_ESWIN_EIC7700_HSPCRG_RESET_H_ */
