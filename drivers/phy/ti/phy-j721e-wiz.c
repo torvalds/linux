a5073caebd59f71b02f9dcd87933a5d94587f456
Arnd Bergmann <arnd@arndb.de>
2026-06-12 11:39:45 +0200

    phy: replace linux/gpio.h inclusions
    linux/gpio.h should no longer be used, so remove those and
    make sure linux/gpio/consumer.h is included instead.
    
    Acked-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>
    Reviewed-by: Linus Walleij <linusw@kernel.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/phy/ti/phy-j721e-wiz.c
+++ drivers/phy/ti/phy-j721e-wiz.c
@@ -11,7 +11,6 @@
 #include <linux/slab.h>
 #include <linux/clk.h>
 #include <linux/clk-provider.h>
-#include <linux/gpio.h>
 #include <linux/gpio/consumer.h>
 #include <linux/io.h>
 #include <linux/module.h>
