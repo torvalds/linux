d4011cda7ff23b0c6561d6edbe6dfd13099169a4
Uwe Kleine-König (The Capable Hub) <u.kleine-koenig@baylibre.com>
2026-06-16 17:25:10 +0200

    watchdog: Use named initializers for platform_device_id arrays
    Named initializers are better readable and more robust to changes of the
    struct definition. This robustness is relevant for a planned change to
    struct platform_device_id replacing .driver_data by an anonymous union.
    
    For one driver drop the unused assignment to .driver_data instead.
    
    While touching these arrays unify spacing and usage of commas.
    
    There is no effect on the compiled arrays.
    
    Signed-off-by: Uwe Kleine-König (The Capable Hub) <u.kleine-koenig@baylibre.com>
    Link: https://lore.kernel.org/r/22bc09d0c9c8dfe75a205b0a9ccc98ccfba1de10.1781622532.git.u.kleine-koenig@baylibre.com
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/watchdog/cros_ec_wdt.c
+++ drivers/watchdog/cros_ec_wdt.c
@@ -178,8 +178,8 @@ static int __maybe_unused cros_ec_wdt_resume(struct platform_device *pdev)
 }
 
 static const struct platform_device_id cros_ec_wdt_id[] = {
-	{ DRV_NAME, 0 },
-	{}
+	{ .name = DRV_NAME },
+	{ }
 };
 
 static struct platform_driver cros_ec_wdt_driver = {
--- drivers/watchdog/max77620_wdt.c
+++ drivers/watchdog/max77620_wdt.c
@@ -235,9 +235,9 @@ static int max77620_wdt_probe(struct platform_device *pdev)
 }
 
 static const struct platform_device_id max77620_wdt_devtype[] = {
-	{ "max77620-watchdog", (kernel_ulong_t)&max77620_wdt_data },
-	{ "max77714-watchdog", (kernel_ulong_t)&max77714_wdt_data },
-	{ },
+	{ .name = "max77620-watchdog", .driver_data = (kernel_ulong_t)&max77620_wdt_data },
+	{ .name = "max77714-watchdog", .driver_data = (kernel_ulong_t)&max77714_wdt_data },
+	{ }
 };
 MODULE_DEVICE_TABLE(platform, max77620_wdt_devtype);
 
