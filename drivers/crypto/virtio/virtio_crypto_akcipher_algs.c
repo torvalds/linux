8634a92ee595d16f12e08ebd24d637b99f78b6fd
Ben Dooks <ben.dooks@codethink.co.uk>
2026-06-22 16:03:22 +0100

    crypto: virtio - fix missing le64_to_cpu() conversions
    There are two cases of sending a __le64 type to a print function
    so fix this by adding le64_to_cpu() which fixes the following
    (prototype) sparse warnings:
    
    drivers/crypto/virtio/virtio_crypto_skcipher_algs.c:234:17: warning: incorrect type in argument 3 (different base types)
    drivers/crypto/virtio/virtio_crypto_skcipher_algs.c:234:17:    expected unsigned long long
    drivers/crypto/virtio/virtio_crypto_skcipher_algs.c:234:17:    got restricted __le64 [usertype] session_id
    drivers/crypto/virtio/virtio_crypto_akcipher_algs.c:196:17: warning: incorrect type in argument 3 (different base types)
    drivers/crypto/virtio/virtio_crypto_akcipher_algs.c:196:17:    expected unsigned long long
    drivers/crypto/virtio/virtio_crypto_akcipher_algs.c:196:17:    got restricted __le64 [usertype] session_id
    
    Signed-off-by: Ben Dooks <ben.dooks@codethink.co.uk>
    Message-ID: <20260622150322.526375-1-ben.dooks@codethink.co.uk>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- drivers/crypto/virtio/virtio_crypto_akcipher_algs.c
+++ drivers/crypto/virtio/virtio_crypto_akcipher_algs.c
@@ -195,7 +195,8 @@ static int virtio_crypto_alg_akcipher_close_session(struct virtio_crypto_akciphe
 
 	if (ctrl_status->status != VIRTIO_CRYPTO_OK) {
 		pr_err("virtio_crypto: Close session failed status: %u, session_id: 0x%llx\n",
-			ctrl_status->status, destroy_session->session_id);
+			ctrl_status->status,
+		       le64_to_cpu(destroy_session->session_id));
 		err = -EINVAL;
 		goto out;
 	}
