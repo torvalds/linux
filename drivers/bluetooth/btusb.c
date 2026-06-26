d0b15d812688d3f0f3fe1c4426e12814d0c294dc
Zijun Hu <zijun.hu@oss.qualcomm.com>
2026-06-25 22:19:47 -0700

    Bluetooth: btusb: Fix BD_ADDR byte order in btusb_set_bdaddr_wcn6855()
    btusb_set_bdaddr_wcn6855() sends the address without swapping byte
    order for VSC 0xFC14, but the command expects the address in reversed
    byte order compared to other HCI commands like HCI_Create_Connection,
    resulting in a wrong BD_ADDR being set.
    
    btmon log on WCN6855 shows VSC 0xFC14 is sent with swapped bytes
    11 22 33 44 55 66, and Read BD ADDR returns the expected address
    11:22:33:44:55:66:
    
      < HCI Command: Vendor (0x3f|0x0014) plen 6                  #3 [hci0]
              11 22 33 44 55 66
      > HCI Event: Command Complete (0x0e) plen 4                  #4 [hci0]
            Vendor (0x3f|0x0014) ncmd 1
              Status: Success (0x00)
      < HCI Command: Read BD ADDR (0x04|0x0009) plen 0            #11 [hci0]
      > HCI Event: Command Complete (0x0e) plen 10                #12 [hci0]
            Read BD ADDR (0x04|0x0009) ncmd 1
              Status: Success (0x00)
              Address: 11:22:33:44:55:66 (OUI 11-22-33)
    
    Fix by swapping the input address before issuing the command.
    
    Fixes: b40f58b97386 ("Bluetooth: btusb: Add Qualcomm Bluetooth SoC WCN6855 support")
    Signed-off-by: Zijun Hu <zijun.hu@oss.qualcomm.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -3076,14 +3076,15 @@ static int btusb_set_bdaddr_ath3012(struct hci_dev *hdev,
 static int btusb_set_bdaddr_wcn6855(struct hci_dev *hdev,
 				const bdaddr_t *bdaddr)
 {
+	bdaddr_t bdaddr_swapped;
 	struct sk_buff *skb;
-	u8 buf[6];
 	long ret;
 
-	memcpy(buf, bdaddr, sizeof(bdaddr_t));
+	baswap(&bdaddr_swapped, bdaddr);
 
-	skb = __hci_cmd_sync_ev(hdev, 0xfc14, sizeof(buf), buf,
-				HCI_EV_CMD_COMPLETE, HCI_INIT_TIMEOUT);
+	skb = __hci_cmd_sync_ev(hdev, 0xfc14, sizeof(bdaddr_swapped),
+				&bdaddr_swapped, HCI_EV_CMD_COMPLETE,
+				HCI_INIT_TIMEOUT);
 	if (IS_ERR(skb)) {
 		ret = PTR_ERR(skb);
 		bt_dev_err(hdev, "Change address command failed (%ld)", ret);
