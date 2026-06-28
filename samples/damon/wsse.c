e4742be45ea45bf554399ce89a09f71e525d7981
SJ Park <sj@kernel.org>
2026-06-28 14:54:40 -0700

    samples/damon/wsse: handle damon_start() failure
    Patch series "samples/damon: handle damon_{start,stop}() failures".
    
    All DAMON sample modules are not correctly handling failures from
    damon_start().  Among those, mtier also has an additional problem for
    handling of damon_stop() failures.  wsse and prcl also have a problem in
    their damon_call() failure handling.  As a result, memory leaks, next
    DAMON operation disruptions, and use-after-free can happen.  Fix those.
    
    Note that only the damon_start() failure caused issues can reliably be
    reproduced.  Reproducing those issues require the admin permission,
    though.
    
    
    This patch (of 6):
    
    damon_sample_wsse_start() callers assume it will clean up resources when
    it fails.  And the function does the cleanup for context buildup failures.
    However, it is not doing the cleanup for damon_start() failure.  As a
    result, when damon_start() fails, it leaks the memory for DAMON context. 
    Free the context in case of the failure to fix the issues.
    
    Note that the issue can reliably be reproduced because the module calls
    damon_start() in the exclusive mode.  For example,
    
        $ sudo damo start
        $ echo $$ | sudo tee /sys/module/damon_sample_wsse/parameters/target_pid
        $ echo Y | sudo tee /sys/module/damon_sample_wsse/parameters/enabled
        $ sudo cat /proc/allocinfo | grep damon_new_ctx
    
    Because the first command is running another DAMON instance, the third
    command fails the damon_start() call because the new DAMON instance cannot
    exclusively run.  And without this fix, by repeating the third and the
    fourth commands above, we can show the memory consumption is only
    increasing due to the leaks.  It requires the sudo permission though.
    
    The issue was discovered [1] by Sashiko.
    
    Link: https://lore.kernel.org/20260628215447.96166-2-sj@kernel.org
    Link: https://lore.kernel.org/20260609145814.70163-1-sj@kernel.org [1]
    Fixes: b757c6cfc696 ("samples/damon/wsse: start and stop DAMON as the user requests")
    Signed-off-by: SJ Park <sj@kernel.org>
    Reviewed-by: Zenghui Yu <zenghui.yu@linux.dev>
    Cc: <stable@vger.kernel.org> # 6.14.x
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- samples/damon/wsse.c
+++ samples/damon/wsse.c
@@ -87,8 +87,10 @@ static int damon_sample_wsse_start(void)
 	target->pid = target_pidp;
 
 	err = damon_start(&ctx, 1, true);
-	if (err)
+	if (err) {
+		damon_destroy_ctx(ctx);
 		return err;
+	}
 	repeat_call_control.data = ctx;
 	return damon_call(ctx, &repeat_call_control);
 }
