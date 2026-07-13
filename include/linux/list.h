2b58c94ea7ac6d26ee44b4734f7dc0e5d773ff70
Marco Elver <elver@google.com>
2026-07-13 17:24:03 +0530

    list: Permit context-unguarded access with list_empty_careful()
    With Context Analysis (viz. Clang's Thread Safety Analysis), list_heads
    that are __guarded_by(..) require holding the appropriate context lock
    when accessing and manipulating them via the list API. Because Clang's
    warning diagnostics do not perform inter-procedural analysis, this is
    enforced by Clang with -Wthread-safety-pointer in the caller at the call
    boundary; a warning is produced when passing a pointer to a guarded
    variable without holding the appropriate context locks:
    
      warning: passing pointer to variable 'list' requires holding [...] [-Wthread-safety-pointer]
               if (list_empty(&ctrl->list))
    
    An exception is list_empty_careful(), which is like list_empty(), except
    that it is permitted to use without holding any context lock (carefully).
    
    Mark list_empty_careful() __context_unsafe, which disables context
    analysis within list_empty_careful(), but also suppresses warnings
    generated in callers related to its pointer arguments.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Marco Elver <elver@google.com>
    Signed-off-by: Nilay Shroff <nilay@linux.ibm.com>
    Signed-off-by: Keith Busch <kbusch@kernel.org>

--- include/linux/list.h
+++ include/linux/list.h
@@ -444,6 +444,7 @@ static inline void list_del_init_careful(struct list_head *entry)
  * if another CPU could re-list_add() it.
  */
 static inline int list_empty_careful(const struct list_head *head)
+	__context_unsafe(/* intentional lockless access to @head */)
 {
 	struct list_head *next = smp_load_acquire(&head->next);
 	return list_is_head(next, head) && (next == READ_ONCE(head->prev));
