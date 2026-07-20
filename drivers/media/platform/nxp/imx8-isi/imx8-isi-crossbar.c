795bd2863591f2fee33fab8f10cf9c383c94342e
Guoniu Zhou <guoniu.zhou@oss.nxp.com>
2026-07-20 11:34:03 +0800

    media: nxp: imx8-isi: Fix stream ID validation bypass in crossbar routing
    The crossbar routing validation has a critical bug where it validates
    the wrong routing table, allowing userspace to bypass validation entirely.
    
    The __mxc_isi_crossbar_set_routing() function is called to validate and
    apply a new routing table from userspace. However, the validation loop
    iterates over state->routing (the currently active routing table) instead
    of the routing parameter (the new table being validated):
    
        for_each_active_route(&state->routing, route) {
    
    This means userspace can submit any invalid routing configuration and it
    will pass validation as long as the currently active routing is valid.
    This is a security issue as it allows userspace to configure routes that
    violate hardware constraints, potentially causing undefined hardware
    behavior.
    
    Fix by validating the routing table that will actually be applied.
    
    Fixes: cf21f328fcaf ("media: nxp: Add i.MX8 ISI driver")
    Cc: stable@vger.kernel.org
    Signed-off-by: Guoniu Zhou <guoniu.zhou@oss.nxp.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Link: https://patch.msgid.link/20260720-isi-v2-1-45845bc5d4fa@oss.nxp.com
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Hans Verkuil <hverkuil+cisco@kernel.org>

--- drivers/media/platform/nxp/imx8-isi/imx8-isi-crossbar.c
+++ drivers/media/platform/nxp/imx8-isi/imx8-isi-crossbar.c
@@ -107,7 +107,7 @@ static int __mxc_isi_crossbar_set_routing(struct v4l2_subdev *sd,
 		return ret;
 
 	/* The memory input can be routed to the first pipeline only. */
-	for_each_active_route(&state->routing, route) {
+	for_each_active_route(routing, route) {
 		if (route->sink_pad == xbar->num_sinks - 1 &&
 		    route->source_pad != xbar->num_sinks) {
 			dev_dbg(xbar->isi->dev,
