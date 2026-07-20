52f6f5f43260d4118e9b137a61a3f2aa4a5b8adf
Shubham Patil <shubhamsanjay.patil@amd.com>
2026-07-20 13:05:10 +0530

    i3c: master: dw: Drop redundant core reset name
    The DesignWare I3C master has a single reset line, so a dedicated
    reset name is redundant. Look up the reset by index by passing NULL
    instead of the "core_rst" name.
    
    Signed-off-by: Shubham Patil <shubhamsanjay.patil@amd.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Reviewed-by: Radhey Shyam Pandey <radhey.shyam.pandey@amd.com>
    Link: https://patch.msgid.link/20260720073510.1869623-3-shubhamsanjay.patil@amd.com
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/i3c/master/dw-i3c-master.c
+++ drivers/i3c/master/dw-i3c-master.c
@@ -1613,7 +1613,7 @@ int dw_i3c_common_probe(struct dw_i3c_master *master,
 		return PTR_ERR(master->pclk);
 
 	master->core_rst = devm_reset_control_get_optional_exclusive_deasserted(&pdev->dev,
-										"core_rst");
+										NULL);
 	if (IS_ERR(master->core_rst))
 		return PTR_ERR(master->core_rst);
 
