166089a856859d5f3e2b85c6ffd70da084238439
Christoph Hellwig <hch@lst.de>
2026-07-20 11:45:44 +0200

    xfs: add a lock annotation to xlog_cil_push_background
    This is required to make the clang context analysis happy, which is
    more strict than the old sparse lock context tracking.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Carlos Maiolino <cmaiolino@redhat.com>
    Signed-off-by: Carlos Maiolino <cem@kernel.org>

--- fs/xfs/xfs_log_cil.c
+++ fs/xfs/xfs_log_cil.c
@@ -1627,6 +1627,7 @@ out_abort_free_ticket:
 static void
 xlog_cil_push_background(
 	struct xlog	*log)
+		__releases_shared(&log->l_cilp->xc_ctx_lock)
 {
 	struct xfs_cil	*cil = log->l_cilp;
 	int		space_used = atomic_read(&cil->xc_ctx->space_used);
