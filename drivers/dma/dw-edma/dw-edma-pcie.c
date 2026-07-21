1d68178b3404737af2cee6c7b4395de3de6dccaa
Koichiro Den <den@valinux.co.jp>
2026-07-21 15:28:14 +0900

    dmaengine: dw-edma-pcie: Add chip flags to match data
    Allow PCI ID match data to pass dw_edma_chip flags into dw_edma_probe().
    This keeps per-device policy in the match data instead of open-coding it
    in probe().
    
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Signed-off-by: Koichiro Den <den@valinux.co.jp>
    Link: https://patch.msgid.link/20260721062815.4117887-14-den@valinux.co.jp
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/dw-edma/dw-edma-pcie.c
+++ drivers/dma/dw-edma/dw-edma-pcie.c
@@ -88,6 +88,7 @@ struct dw_edma_pcie_match_data {
 	int (*parse_caps)(struct pci_dev *pdev,
 			  struct dw_edma_pcie_data *pdata);
 	unsigned long flags;
+	u32 chip_flags;
 };
 
 #define DW_EDMA_PCIE_F_DEVMEM_PHYS_OFF	BIT(0)
@@ -471,6 +472,7 @@ static int dw_edma_pcie_probe(struct pci_dev *pdev,
 	chip->dev = dev;
 
 	chip->mf = dma_data->mf;
+	chip->flags = match->chip_flags;
 	chip->nr_irqs = nr_irqs;
 	chip->ops = match->plat_ops;
 	chip->cfg_non_ll = dma_data->cfg_non_ll;
