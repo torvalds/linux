60276a9469a1df6c86666ecc21fa8ad10d90e809
Imran Shaik <imran.shaik@oss.qualcomm.com>
2026-07-22 10:11:14 +0530

    clk: qcom: gcc-shikra: Add additional frequencies for EMAC RGMII clocks
    Add support for 2.5MHz and 25MHz GCC EMAC RGMII clock frequencies required
    for EMAC 10M and 100M speeds respectively on Qualcomm Shikra SoC.
    
    Tested-by: Mohd Ayaan Anwar <mohd.anwar@oss.qualcomm.com>
    Reviewed-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
    Reviewed-by: Taniya Das <taniya.das@oss.qualcomm.com>
    Signed-off-by: Imran Shaik <imran.shaik@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20260722-b4-shikra-gcc-emac-freqs-v2-1-2e480e2de8de@oss.qualcomm.com
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>

--- drivers/clk/qcom/gcc-shikra.c
+++ drivers/clk/qcom/gcc-shikra.c
@@ -1244,6 +1244,8 @@ static struct clk_rcg2 gcc_emac0_ptp_clk_src = {
 };
 
 static const struct freq_tbl ftbl_gcc_emac0_rgmii_clk_src[] = {
+	F(2500000, P_GPLL0_OUT_AUX2, 10, 1, 12),
+	F(25000000, P_GPLL0_OUT_AUX2, 12, 0, 0),
 	F(50000000, P_GPLL0_OUT_AUX2, 6, 0, 0),
 	F(125000000, P_GPLL12_OUT_AUX2, 4, 0, 0),
 	F(250000000, P_GPLL12_OUT_EARLY, 4, 0, 0),
