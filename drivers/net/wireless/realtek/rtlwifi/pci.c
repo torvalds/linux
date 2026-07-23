3c2999d13eeb222ae56631aeb7ca248090f2b210
Abdun Nihaal <nihaal@cse.iitm.ac.in>
2026-07-23 17:31:15 +0530

    wifi: rtlwifi: pci: fix error path in rtl_pci_probe()
    In the last error path in rtl_pci_probe(), the cleanup functions are
    skipped due to a wrong goto label. Moreover, the successful call to
    rtl_init_rfkill(), ieee80211_register_hw(), rtl_debug_add_one() have to
    be reverted. Fix this issue by updating the labels and adding the
    relevant cleanup functions to the last error path.
    
    Fixes: 0c8173385e54 ("rtl8192ce: Add new driver")
    Signed-off-by: Abdun Nihaal <nihaal@cse.iitm.ac.in>
    Acked-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://patch.msgid.link/20260723120118.145383-1-nihaal@cse.iitm.ac.in

--- drivers/net/wireless/realtek/rtlwifi/pci.c
+++ drivers/net/wireless/realtek/rtlwifi/pci.c
@@ -2244,13 +2244,17 @@ int rtl_pci_probe(struct pci_dev *pdev,
 		rtl_dbg(rtlpriv, COMP_INIT, DBG_DMESG,
 			"%s: failed to register IRQ handler\n",
 			wiphy_name(hw->wiphy));
-		goto fail3;
+		goto fail6;
 	}
 	rtlpci->irq_alloc = 1;
 
 	set_bit(RTL_STATUS_INTERFACE_START, &rtlpriv->status);
 	return 0;
 
+fail6:
+	rtl_deinit_rfkill(hw);
+	rtl_debug_remove_one(hw);
+	ieee80211_unregister_hw(hw);
 fail5:
 	rtl_pci_deinit(hw);
 fail4:
