4a2610a5a9fcf77eaad82bb030ec77ec5e381db8
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2026-07-23 14:23:37 +0300

    wifi: iwlwifi: bump core version for BZ/SC/DR
    Start supporting Core 107 FW on these devices.
    
    Link: https://patch.msgid.link/20260723142324.ed92b1d7f927.I6dd10d2f9a04a36751aea9e238fecfa62fe280a6@changeid
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>

--- drivers/net/wireless/intel/iwlwifi/cfg/dr.c
+++ drivers/net/wireless/intel/iwlwifi/cfg/dr.c
@@ -9,7 +9,7 @@
 #include "fw/api/txq.h"
 
 /* Highest firmware core release supported */
-#define IWL_DR_UCODE_CORE_MAX	106
+#define IWL_DR_UCODE_CORE_MAX	107
 
 /* Lowest firmware core release supported */
 #define IWL_DR_UCODE_CORE_MIN	102
