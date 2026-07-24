3b232a0cce4182851ee8f2788180c93e49d9ab96
Thorsten Blum <thorsten.blum@linux.dev>
2026-07-24 19:21:58 +0200

    powerpc/boot: Remove unused sprintf()
    There have been no sprintf() callers in the boot wrapper since commit
    e275e023aa69 ("powerpc/44x: Warp patches for the new NDFC driver").
    
    Remove the function definition and declaration.
    
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>
    Reviewed-by: Christophe Leroy (CS GROUP) <chleroy@kernel.org>
    Signed-off-by: Madhavan Srinivasan <maddy@linux.ibm.com>
    Link: https://patch.msgid.link/20260724172200.208722-2-thorsten.blum@linux.dev

--- arch/powerpc/boot/stdio.c
+++ arch/powerpc/boot/stdio.c
@@ -326,17 +326,6 @@ int vsprintf(char *buf, const char *fmt, va_list args)
 	return str-buf;
 }
 
-int sprintf(char * buf, const char *fmt, ...)
-{
-	va_list args;
-	int i;
-
-	va_start(args, fmt);
-	i=vsprintf(buf,fmt,args);
-	va_end(args);
-	return i;
-}
-
 static char sprint_buf[1024];
 
 int
--- arch/powerpc/boot/stdio.h
+++ arch/powerpc/boot/stdio.h
@@ -12,9 +12,6 @@ extern int printf(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
 
 #define fprintf(fmt, args...)	printf(args)
 
-extern int sprintf(char *buf, const char *fmt, ...)
-	__attribute__((format(printf, 2, 3)));
-
 extern int vsprintf(char *buf, const char *fmt, va_list args);
 
 #endif				/* _PPC_BOOT_STDIO_H_ */
