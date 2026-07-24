89cf154d7c18e6e94a3da83051f3cf2bac317ae2
Pauli Virtanen <pav@iki.fi>
2026-07-24 23:20:25 +0300

    Bluetooth: ISO: lock sk in iso_sock_getname
    Accessing iso_pi(sk)->conn requires lock_sock, which is not held here.
    
    Fix by adding the lock/release.
    
    Fixes: 2df108c227b2 ("Bluetooth: ISO: Fix using BT_SK_PA_SYNC to detect BIS sockets")
    Signed-off-by: Pauli Virtanen <pav@iki.fi>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- net/bluetooth/iso.c
+++ net/bluetooth/iso.c
@@ -1472,6 +1472,8 @@ static int iso_sock_getname(struct socket *sock, struct sockaddr *addr,
 
 	BT_DBG("sock %p, sk %p", sock, sk);
 
+	lock_sock(sk);
+
 	addr->sa_family = AF_BLUETOOTH;
 
 	if (peer) {
@@ -1492,6 +1494,8 @@ static int iso_sock_getname(struct socket *sock, struct sockaddr *addr,
 		bacpy(&sa->iso_bdaddr, &iso_pi(sk)->src);
 		sa->iso_bdaddr_type = iso_pi(sk)->src_type;
 	}
+
+	release_sock(sk);
 
 	return len;
 }
