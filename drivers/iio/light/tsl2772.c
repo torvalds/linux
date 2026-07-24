ac75550ab5b5d73649bffea245c2075fd9249bd0
Yuanshen Cao <alex.caoys@gmail.com>
2026-07-24 23:46:01 +0000

    iio: light: tsl2772: fix ALS calibscale readback
    The read_raw() implementation uses IIO_LIGHT to distinguish between the
    ambient light and proximity channels when handling
    IIO_CHAN_INFO_CALIBSCALE.
    
    However, the ALS channel is registered as IIO_INTENSITY, while
    write_raw() correctly writes to IIO_INTENSITY. As a result, reading
    in_intensity0_calibscale incorrectly returns the proximity gain instead
    of the ALS gain.
    
    This causes the following user-visible behavior:
    - Writing in_intensity0_calibscale appears to have no effect because the
      readback reports the proximity gain.
    - Writing in_proximity0_calibscale causes both in_proximity0_calibscale
      and in_intensity0_calibscale to report the same value.
    
    Fix this by checking for IIO_INTENSITY in read_raw(), matching the
    channel definition and the existing write_raw() implementation.
    
    Fixes: 3c97c08b5735 ("staging: iio: add TAOS tsl2x7x driver")
    Signed-off-by: Yuanshen Cao <alex.caoys@gmail.com>
    Reviewed-by: David Lechner <dlechner@baylibre.com>
    Signed-off-by: Jonathan Cameron <jonathan.cameron@oss.qualcomm.com>

--- drivers/iio/light/tsl2772.c
+++ drivers/iio/light/tsl2772.c
@@ -1274,7 +1274,7 @@ static int tsl2772_read_raw(struct iio_dev *indio_dev,
 		}
 		break;
 	case IIO_CHAN_INFO_CALIBSCALE:
-		if (chan->type == IIO_LIGHT)
+		if (chan->type == IIO_INTENSITY)
 			*val = tsl2772_als_gain[chip->settings.als_gain];
 		else
 			*val = tsl2772_prox_gain[chip->settings.prox_gain];
