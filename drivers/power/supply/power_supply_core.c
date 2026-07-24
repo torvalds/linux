25453aaffac01909232a20409dff1a9e6d609a11
Steffen Dirkwinkel <s.dirkwinkel@beckhoff.com>
2026-07-24 09:27:35 +0200

    power: supply: hold extensions_sem when creating LED triggers
    LED triggers were created in __power_supply_register without holding the
    extensions_sem lock. Since commit b04510c3af6d ("power: supply: leds: create
    triggers based on properties, not type") we call power_supply_has_property
    during trigger creation and access the extensions there.
    Move power_supply_create_triggers down into the lock scope used for
    power_supply_add_hwmon_sysfs for the same reason.
    
    Fixes: b04510c3af6d ("power: supply: leds: create triggers based on properties, not type")
    Reported-by: Chaitanya Kumar Borah <chaitanya.kumar.borah@intel.com>
    Closes: https://lore.kernel.org/all/a95a2720-4092-4b49-bd9d-b700f1c2680d@intel.com/
    Signed-off-by: Steffen Dirkwinkel <s.dirkwinkel@beckhoff.com>
    Tested-by: Chaitanya Kumar Borah <chaitanya.kumar.borah@intel.com>
    Link: https://patch.msgid.link/20260724-power-supply-triggers-lockdep-v1-1-9b451b1f1916@beckhoff.com
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>

--- drivers/power/supply/power_supply_core.c
+++ drivers/power/supply/power_supply_core.c
@@ -1636,11 +1636,11 @@ __power_supply_register(struct device *parent,
 	if (rc)
 		goto register_thermal_failed;
 
-	rc = power_supply_create_triggers(psy);
-	if (rc)
-		goto create_triggers_failed;
-
 	scoped_guard(rwsem_read, &psy->extensions_sem) {
+		rc = power_supply_create_triggers(psy);
+		if (rc)
+			goto create_triggers_failed;
+
 		rc = power_supply_add_hwmon_sysfs(psy);
 		if (rc)
 			goto add_hwmon_sysfs_failed;
