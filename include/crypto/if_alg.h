a264cb967dbdbf9544c4de80e2f7188214b93f77
Randy Dunlap <rdunlap@infradead.org>
2026-07-24 18:42:53 -0700

    crypto: af_alg - clean up kernel-doc warnings
    - add missing struct member @wait, drop @completion
    - convert function comments to kernel-doc format
    - for af_alg_readable(), change comments from "writable" to "readable"
    
    Warning: include/crypto/if_alg.h:161 struct member 'wait' not described
     in 'af_alg_ctx'
    Warning: include/crypto/if_alg.h:161 Excess struct member 'completion'
     description in 'af_alg_ctx'
    Warning: include/crypto/if_alg.h:187 This comment starts with '/**', but
     isn't a kernel-doc comment.
     * Size of available buffer for sending data from user space to kernel.
    Warning: include/crypto/if_alg.h:202 This comment starts with '/**', but
     isn't a kernel-doc comment.
     * Can the send buffer still be written to?
    Warning: include/crypto/if_alg.h:213 This comment starts with '/**', but
     isn't a kernel-doc comment.
     * Size of available buffer used by kernel for the RX user space operation.
    Warning: include/crypto/if_alg.h:228 This comment starts with '/**', but
     isn't a kernel-doc comment.
     * Can the RX buffer still be written to?
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/if_alg.h
+++ include/crypto/if_alg.h
@@ -121,7 +121,7 @@ struct af_alg_async_req {
  * @iv:			IV for cipher operation
  * @state:		Existing state for continuing operation
  * @aead_assoclen:	Length of AAD for AEAD cipher operations
- * @completion:		Work queue for synchronous operation
+ * @wait:		For waiting for completion of async crypto ops
  * @used:		TX bytes sent to kernel. This variable is used to
  *			ensure that user space cannot cause the kernel
  *			to allocate too much memory in sendmsg operation.
@@ -185,10 +185,11 @@ static inline struct alg_sock *alg_sk(struct sock *sk)
 }
 
 /**
- * Size of available buffer for sending data from user space to kernel.
+ * af_alg_sndbuf - Size of available buffer for sending data from user space to kernel.
  *
- * @sk socket of connection to user space
- * @return number of bytes still available
+ * @sk: socket of connection to user space
+ *
+ * Returns: number of bytes still available
  */
 static inline int af_alg_sndbuf(struct sock *sk)
 {
@@ -200,10 +201,11 @@ static inline int af_alg_sndbuf(struct sock *sk)
 }
 
 /**
- * Can the send buffer still be written to?
+ * af_alg_writable - Can the send buffer still be written to?
  *
- * @sk socket of connection to user space
- * @return true => writable, false => not writable
+ * @sk: socket of connection to user space
+ *
+ * Returns: true => writable, false => not writable
  */
 static inline bool af_alg_writable(struct sock *sk)
 {
@@ -211,10 +213,11 @@ static inline bool af_alg_writable(struct sock *sk)
 }
 
 /**
- * Size of available buffer used by kernel for the RX user space operation.
+ * af_alg_rcvbuf - Size of available buffer used by kernel for the RX user space operation.
  *
- * @sk socket of connection to user space
- * @return number of bytes still available
+ * @sk: socket of connection to user space
+ *
+ * Returns: number of bytes still available
  */
 static inline int af_alg_rcvbuf(struct sock *sk)
 {
@@ -226,10 +229,11 @@ static inline int af_alg_rcvbuf(struct sock *sk)
 }
 
 /**
- * Can the RX buffer still be written to?
+ * af_alg_readable - Can the RX buffer still be read from?
  *
- * @sk socket of connection to user space
- * @return true => writable, false => not writable
+ * @sk: socket of connection to user space
+ *
+ * Returns: true => readable, false => not readable
  */
 static inline bool af_alg_readable(struct sock *sk)
 {
