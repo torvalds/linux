15c1339ef44054cc79c49b40b24a26aa80a2253e
Caleb Sander Mateos <csander@purestorage.com>
2026-07-29 11:10:41 -0600

    ublk: remove WARN_ON_ONCE() in ublk_unmap_io()
    io->res is set from struct ublksrv_io_cmd's result field, which is
    controlled by the ublk server process, without any validation. It's thus
    possible for userspace to trigger the io->res > rq_bytes warning.
    ublk_copy_user_pages() already limits the copy length to the request
    data length, so drop the warning.
    
    Signed-off-by: Caleb Sander Mateos <csander@purestorage.com>
    Link: https://patch.msgid.link/20260729171041.45061-4-csander@purestorage.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/ublk_drv.c
+++ drivers/block/ublk_drv.c
@@ -1497,8 +1497,6 @@ static unsigned int ublk_unmap_io(bool need_map,
 		struct iov_iter iter;
 		const int dir = ITER_SOURCE;
 
-		WARN_ON_ONCE(io->res > rq_bytes);
-
 		if (import_ubuf(dir, u64_to_user_ptr(io->buf.addr), io->res,
 				&iter) < 0)
 			return 0;
