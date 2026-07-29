ec7607ac4717ff521c9c1e9d8271c26293345513
Sergey Senozhatsky <senozhatsky@chromium.org>
2026-07-29 13:57:36 +0900

    zram: validate deflate params
    We must validate user-supplied deflate winbits before we pass it to
    zlib_deflate_workspacesize(), which triggers BUG_ON() if winbits value is
    outside of valid ranges.
    
    Link: https://lore.kernel.org/20260729045745.775973-3-senozhatsky@chromium.org
    Fixes: dc75a0d93bd5 ("zram: support deflate-specific params")
    Link: https://sashiko.dev/#/patchset/20260728092935.31139-1-haoqinhuang7@gmail.com
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Haoqin Huang <haoqinhuang7@gmail.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/block/zram/backend_deflate.c
+++ drivers/block/zram/backend_deflate.c
@@ -24,8 +24,16 @@ static int deflate_setup_params(struct zcomp_params *params)
 {
 	if (params->level == ZCOMP_PARAM_NOT_SET)
 		params->level = Z_DEFAULT_COMPRESSION;
-	if (params->deflate.winbits == ZCOMP_PARAM_NOT_SET)
+	if (params->deflate.winbits == ZCOMP_PARAM_NOT_SET) {
 		params->deflate.winbits = DEFLATE_DEF_WINBITS;
+	} else {
+		s32 wb = params->deflate.winbits;
+
+		if ((wb < -15 || wb > -9) && (wb < 9 || wb > 15)) {
+			pr_err("invalid deflate winbits: %d\n", wb);
+			return -EINVAL;
+		}
+	}
 
 	return 0;
 }
