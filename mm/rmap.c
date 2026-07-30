af56d6acf494f7a003737b59e785a4e8e5e5b66a
Dev Jain <dev.jain@arm.com>
2026-07-30 09:45:52 +0000

    mm/rmap: convert page -> folio for hwpoison checks
    Patch series "mm/rmap: Refactor try_to_unmap_one", v4.
    
    In preparation for batching anonymous large folio unmapping to optimize
    it, refactor try_to_unmap_one.  This series refactors hugetlb,
    anon-lazyfree and anon-swapbacked logic into their own functions,
    significantly reducing the length of the huge try_to_unmap_one.
    
    
    This patch (of 5):
    
    try_to_unmap() receives hugetlb folios only from the hwpoison path. 
    hugetlb_update_hwpoison() sets the hugetlb folio's head-page hwpoison bit,
    and page_vma_mapped_walk() reports the hugetlb mapping at the head PFN, so
    the previous PageHWPoison(subpage) check happened to work for hugetlb.
    
    For non-hugetlb folios, unmap_poisoned_folio() currently rejects large
    folios before calling try_to_unmap().  Hence it is always the case that if
    try_to_unmap_one() handles an hwpoisoned folio, then the head page is
    marked with the poison bit.
    
    Therefore, convert the poisoned subpage checks to folio_test_hwpoison().
    
    No functional change intended, except that, while at it,
    convert VM_BUG_* to VM_WARN_*.
    
    Link: https://lore.kernel.org/20260730094559.418003-1-dev.jain@arm.com
    Link: https://lore.kernel.org/20260730094559.418003-2-dev.jain@arm.com
    Signed-off-by: Dev Jain <dev.jain@arm.com>
    Reviewed-by: Lorenzo Stoakes (ARM) <ljs@kernel.org>
    Acked-by: David Hildenbrand (Arm) <david@kernel.org>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Cc: Harry Yoo <harry@kernel.org>
    Cc: Jann Horn <jannh@google.com>
    Cc: Lance Yang <lance.yang@linux.dev>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/rmap.c
+++ mm/rmap.c
@@ -2122,10 +2122,11 @@ static bool try_to_unmap_one(struct folio *folio, struct vm_area_struct *vma,
 			bool anon = folio_test_anon(folio);
 
 			/*
-			 * The try_to_unmap() is only passed a hugetlb page
-			 * in the case where the hugetlb page is poisoned.
+			 * The try_to_unmap() is only passed a hugetlb folio
+			 * in the case where the hugetlb folio contains a
+			 * poisoned page.
 			 */
-			VM_BUG_ON_PAGE(!PageHWPoison(subpage), subpage);
+			VM_WARN_ON_FOLIO(!folio_test_hwpoison(folio), folio);
 			/*
 			 * huge_pmd_unshare may unmap an entire PMD page.
 			 * There is no way of knowing exactly which PMDs may
@@ -2204,7 +2205,8 @@ static bool try_to_unmap_one(struct folio *folio, struct vm_area_struct *vma,
 		/* Update high watermark before we lower rss */
 		update_hiwater_rss(mm);
 
-		if (PageHWPoison(subpage) && (flags & TTU_HWPOISON)) {
+		/* unmap_poisoned_folio() only refs order-0 or hugetlb folios */
+		if (folio_test_hwpoison(folio) && (flags & TTU_HWPOISON)) {
 			pteval = swp_entry_to_pte(make_hwpoison_entry(subpage));
 			if (folio_test_hugetlb(folio)) {
 				hugetlb_count_sub(folio_nr_pages(folio), mm);
