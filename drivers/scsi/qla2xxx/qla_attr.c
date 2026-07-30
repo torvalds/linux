e0cebe20dcffbed9c078fe30e2d18cd5046d9eff
Nilesh Javali <njavali@marvell.com>
2026-07-30 21:28:12 +0530

    scsi: qla2xxx: Don't query firmware state while chip is down
    qla2x00_fw_state_show() initializes rval to QLA_FUNCTION_FAILED and jumps
    to the out: label when the chip is down or EEH is busy. The out: block
    then re-issued qla2x00_get_firmware_state() because rval != QLA_SUCCESS,
    defeating the chip-down/EEH-busy guards and issuing a mailbox command
    (outside optrom_mutex) during ISP reset or PCI error recovery, which can
    hang the adapter. It also turned a normal in-lock mailbox failure into a
    second unsynchronized mailbox attempt.
    
    Make the out: fallback only mark the firmware state as unknown. The
    mailbox is now issued at most once, inside optrom_mutex, and only when
    the chip is up and not EEH-busy.
    
    Fixes: b6faaaf796d7 ("scsi: qla2xxx: Serialize mailbox request")
    Cc: stable@vger.kernel.org
    Reported-by: Sashiko <sashiko-dev@google.com>
    Signed-off-by: Nilesh Javali <njavali@marvell.com>
    Link: https://patch.msgid.link/20260730155838.2119230-8-njavali@marvell.com
    Signed-off-by: Martin K. Petersen (Oracle) <mkp@kernel.org>

--- drivers/scsi/qla2xxx/qla_attr.c
+++ drivers/scsi/qla2xxx/qla_attr.c
@@ -1678,10 +1678,8 @@ qla2x00_fw_state_show(struct device *dev, struct device_attribute *attr,
 	rval = qla2x00_get_firmware_state(vha, state);
 	mutex_unlock(&vha->hw->optrom_mutex);
 out:
-	if (rval != QLA_SUCCESS) {
+	if (rval != QLA_SUCCESS)
 		memset(state, -1, sizeof(state));
-		rval = qla2x00_get_firmware_state(vha, state);
-	}
 
 	return scnprintf(buf, PAGE_SIZE, "0x%x 0x%x 0x%x 0x%x 0x%x 0x%x\n",
 	    state[0], state[1], state[2], state[3], state[4], state[5]);
