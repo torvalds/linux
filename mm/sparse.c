8380671909bfcdd44818abf6b93f82bd3669bc8c
Sang-Heon Jeon <ekffu200098@gmail.com>
2026-08-01 01:47:55 +0900

    mm/sparse: correct init section annotations
    The !SPARSEMEM_EXTREME stub of sparse_index_init() has no annotation but
    the SPARSEMEM_EXTREME variant is __meminit.  So mark the stub __meminit
    too.
    
    mminit_validate_memmodel_limits() is only called by memory_present(),
    which is __init.  So mark it __init.
    
    sparse_usagebuf and sparse_usagebuf_end are only used by
    sparse_init_early_section(), sparse_usage_init() and sparse_usage_fini(),
    which are all __init.  So mark them __initdata.
    
    Link: https://lore.kernel.org/20260731164758.1210668-1-ekffu200098@gmail.com
    Signed-off-by: Sang-Heon Jeon <ekffu200098@gmail.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: David Hildenbrand (Arm) <david@kernel.org>
    Acked-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/sparse.c
+++ mm/sparse.c
@@ -104,7 +104,7 @@ int __meminit sparse_index_init(unsigned long section_nr, int nid)
 	return 0;
 }
 #else /* !SPARSEMEM_EXTREME */
-int sparse_index_init(unsigned long section_nr, int nid)
+int __meminit sparse_index_init(unsigned long section_nr, int nid)
 {
 	return 0;
 }
@@ -127,7 +127,7 @@ static inline int sparse_early_nid(struct mem_section *section)
 }
 
 /* Validate the physical addressing limitations of the model */
-static void __meminit mminit_validate_memmodel_limits(unsigned long *start_pfn,
+static void __init mminit_validate_memmodel_limits(unsigned long *start_pfn,
 						unsigned long *end_pfn)
 {
 	unsigned long max_sparsemem_pfn = (DIRECT_MAP_PHYSMEM_END + 1) >> PAGE_SHIFT;
@@ -249,8 +249,8 @@ void __weak __meminit vmemmap_populate_print_last(void)
 {
 }
 
-static void *sparse_usagebuf __meminitdata;
-static void *sparse_usagebuf_end __meminitdata;
+static void *sparse_usagebuf __initdata;
+static void *sparse_usagebuf_end __initdata;
 
 /*
  * Helper function that is used for generic section initialization, and
