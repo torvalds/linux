7822fa5f4f647d05d31eea3edd01382c6183e4cd
Wilson Felipe Pereira <wfelipe@google.com>
2026-07-31 05:37:06 +0000

    mm: zswap: drop list_lru param from zswap_lru_add() and _del()
    Since zswap_lru_add() and zswap_lru_del() are only called with the global
    zswap_list_lru, remove the redundant list_lru argument and use
    zswap_list_lru directly.
    
    Link: https://lore.kernel.org/20260731053721.1412304-1-wfelipe@google.com
    Signed-off-by: Wilson Felipe Pereira <wfelipe@google.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Yosry Ahmed <yosry@kernel.org>
    Reviewed-by: SJ Park <sj@kernel.org>
    Acked-by: Nhat Pham <nphamcs@gmail.com>
    Cc: Chengming Zhou <chengming.zhou@linux.dev>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/zswap.c
+++ mm/zswap.c
@@ -665,7 +665,7 @@ static inline int entry_to_nid(struct zswap_entry *entry)
 	return page_to_nid(virt_to_page(entry));
 }
 
-static void zswap_lru_add(struct list_lru *list_lru, struct zswap_entry *entry)
+static void zswap_lru_add(struct zswap_entry *entry)
 {
 	int nid = entry_to_nid(entry);
 	struct mem_cgroup *memcg;
@@ -684,11 +684,11 @@ static void zswap_lru_add(struct list_lru *list_lru, struct zswap_entry *entry)
 	rcu_read_lock();
 	memcg = mem_cgroup_from_entry(entry);
 	/* will always succeed */
-	list_lru_add(list_lru, &entry->lru, nid, memcg);
+	list_lru_add(&zswap_list_lru, &entry->lru, nid, memcg);
 	rcu_read_unlock();
 }
 
-static void zswap_lru_del(struct list_lru *list_lru, struct zswap_entry *entry)
+static void zswap_lru_del(struct zswap_entry *entry)
 {
 	int nid = entry_to_nid(entry);
 	struct mem_cgroup *memcg;
@@ -696,7 +696,7 @@ static void zswap_lru_del(struct list_lru *list_lru, struct zswap_entry *entry)
 	rcu_read_lock();
 	memcg = mem_cgroup_from_entry(entry);
 	/* will always succeed */
-	list_lru_del(list_lru, &entry->lru, nid, memcg);
+	list_lru_del(&zswap_list_lru, &entry->lru, nid, memcg);
 	rcu_read_unlock();
 }
 
@@ -764,7 +764,7 @@ static void zswap_entry_cache_free(struct zswap_entry *entry)
  */
 static void zswap_entry_free(struct zswap_entry *entry)
 {
-	zswap_lru_del(&zswap_list_lru, entry);
+	zswap_lru_del(entry);
 	zs_free(entry->pool->zs_pool, entry->handle);
 	zswap_pool_put(entry->pool);
 	if (entry->objcg) {
@@ -1461,7 +1461,7 @@ static bool zswap_store_page(struct page *page,
 	entry->referenced = true;
 	if (entry->length) {
 		INIT_LIST_HEAD(&entry->lru);
-		zswap_lru_add(&zswap_list_lru, entry);
+		zswap_lru_add(entry);
 	}
 
 	return true;
