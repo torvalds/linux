1f45a8663a58e89fe3025360a421ec3b7b929aa0
Heiko Carstens <hca@linux.ibm.com>
2026-08-03 19:21:41 +0200

    s390/sysinfo: Add context analysis attributes
    Add context analysis attributes to service_level_start() and
    service_level_stop() to specify that those functions only
    acquire or release a lock.
    
    Addresses the following warnings:
    
    arch/s390/kernel/sysinfo.c:331:1: warning: rw_semaphore 'service_level_sem' is still held at the end of function
    arch/s390/kernel/sysinfo.c:329:2: note: rw_semaphore acquired here
      329 |         down_read(&service_level_sem);
    arch/s390/kernel/sysinfo.c:340:2: warning: releasing rw_semaphore 'service_level_sem' that was not held
      340 |         up_read(&service_level_sem);
    
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>

--- arch/s390/kernel/sysinfo.c
+++ arch/s390/kernel/sysinfo.c
@@ -325,6 +325,7 @@ int unregister_service_level(struct service_level *slr)
 EXPORT_SYMBOL(unregister_service_level);
 
 static void *service_level_start(struct seq_file *m, loff_t *pos)
+__acquires_shared(service_level_sem)
 {
 	down_read(&service_level_sem);
 	return seq_list_start(&service_level_list, *pos);
@@ -336,6 +337,7 @@ static void *service_level_next(struct seq_file *m, void *p, loff_t *pos)
 }
 
 static void service_level_stop(struct seq_file *m, void *p)
+__releases_shared(service_level_sem)
 {
 	up_read(&service_level_sem);
 }
