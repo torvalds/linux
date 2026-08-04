3b0cee02664041aea7e4f787c66cb86c82eb4e97
Can Peng <pengcan@kylinos.cn>
2026-08-04 15:20:17 +0800

    spi: bcm63xx-hsspi: disable clocks on resume failure
    bcm63xx_hsspi_resume() enables the HSSPI clock, and optionally the PLL
    clock, before restarting the SPI controller queue.
    
    If spi_controller_resume() fails, the function currently reports success
    and leaves those clocks enabled. Propagate the error and disable the
    clocks before returning.
    
    Fixes: 142168eba9dc ("spi: bcm63xx-hsspi: add bcm63xx HSSPI driver")
    Cc: stable@vger.kernel.org
    Signed-off-by: Can Peng <pengcan@kylinos.cn>
    Reviewed-by: Kursad Oney <kursad.oney@broadcom.com>
    Link: https://patch.msgid.link/20260804072017.860974-1-pengcan@kylinos.cn
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-bcm63xx-hsspi.c
+++ drivers/spi/spi-bcm63xx-hsspi.c
@@ -919,7 +919,13 @@ static int bcm63xx_hsspi_resume(struct device *dev)
 		}
 	}
 
-	spi_controller_resume(host);
+	ret = spi_controller_resume(host);
+	if (ret) {
+		if (bs->pll_clk)
+			clk_disable_unprepare(bs->pll_clk);
+		clk_disable_unprepare(bs->clk);
+		return ret;
+	}
 
 	return 0;
 }
