412f89fb3988a344175899776c8bc7073524ad84
Carlos Maiolino <cem@kernel.org>
2026-08-04 11:45:55 +0200

    capability: unexport has_capability_noaudit
    This has been originally exported to be used in xfs. Giving we are not
    using it anymore, unexport for consistency.
    
    Signed-off-by: Carlos Maiolino <cmaiolino@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Serge Hallyn <sergeh@kernel.org>
    Signed-off-by: Carlos Maiolino <cem@kernel.org>

--- kernel/capability.c
+++ kernel/capability.c
@@ -326,7 +326,6 @@ bool has_capability_noaudit(struct task_struct *t, int cap)
 {
 	return has_ns_capability_noaudit(t, &init_user_ns, cap);
 }
-EXPORT_SYMBOL(has_capability_noaudit);
 
 static bool ns_capable_common(struct user_namespace *ns,
 			      int cap,
