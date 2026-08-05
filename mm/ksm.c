a44ab4bd1ec0432d686c25f9c160379ffa1e694c
Hongfu Li <lihongfu@kylinos.cn>
2026-08-05 18:59:07 +0800

    ksm: update comments and docs to reference folio->mapping
    The KSM code already stores and checks the stable node key via
    folio->mapping, but the comment in ksm_get_folio() and the reverse mapping
    documentation in ksm.rst still refer to page->mapping.
    
    This is a pure wording update to match the folio-based implementation.  No
    functional change is intended.
    
    Link: https://lore.kernel.org/20260805105927.41987-1-hongfu.li@linux.dev
    Signed-off-by: Hongfu Li <lihongfu@kylinos.cn>
    Acked-by: David Hildenbrand (Arm) <david@kernel.org>
    Reviewed-by: Xu Xin <xu.xin16@zte.com.cn>
    Reviewed-by: Dongliang Mu <dzm91@hust.edu.cn>
    Cc: Alex Shi <alexs@kernel.org>
    Cc: Chengming Zhou <chengming.zhou@linux.dev>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Cc: Yanteng Si <si.yanteng@linux.dev>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/ksm.c
+++ mm/ksm.c
@@ -959,10 +959,9 @@ enum ksm_get_folio_flags {
  * seconds or even minutes: much too unresponsive.  So instead we use a
  * "keyhole reference": access to the ksm page from the stable node peeps
  * out through its keyhole to see if that page still holds the right key,
- * pointing back to this stable node.  This relies on freeing a PageAnon
- * page to reset its page->mapping to NULL, and relies on no other use of
- * a page to put something that might look like our key in page->mapping.
- * is on its way to being freed; but it is an anomaly to bear in mind.
+ * pointing back to this stable node.  This relies on freeing an anon
+ * folio to reset its mapping to NULL, and relies on no other use of a
+ * folio to put something that might look like our key in its mapping.
  */
 static struct folio *ksm_get_folio(struct ksm_stable_node *stable_node,
 				 enum ksm_get_folio_flags flags)
