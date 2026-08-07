8b365b3c68b474a1053ec0755dacdc751578afb0
Eduard Zingerman <eddyz87@gmail.com>
2026-08-07 13:59:36 -0700

    selftests/bpf: Verify zext_dst annotations for various instructions
    Includes the following test cases:
    
    - a test showing that zero extension flags do not propagate through
      state pruning in the unpatched kernel.
    - a 32-bit subregister consumed by MOV32 and ALU32 operations
      (never zext'ed);
    - a 64-bit MOV (never zext'ed);
    - a narrow (32-bit) BPF_LDX load whose result is read as 64-bit;
    - 32-bit atomic fetch_add and cmpxchg whose result is read as 64-bit;
    - a CFG case where a 32-bit definition's upper half is used only on one
      of two branches;
    - no zext for dead registers;
    - LD_ABS defines only lower 32 bits, hence needs zext when the result
      is used as 64-bits;
    - helper, kfunc and subprogram parameters are considered to use full
      64 bits;
    - a 32-bit subregister consumed by JMP32 (X/K) operations;
    - a 32-bit subregister consumed by JMP (X/K) operations;
    - a 64-bit register consumed by both JMP and JMP32 operations
      (never zext'ed);
    - ALU64 and address space cast operations on arena pointers;
    - memory loads using BPF_PROBE_MEM instructions.
    
    Signed-off-by: Eduard Zingerman <eddyz87@gmail.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/bpf/20260807-static-zext-v4-7-b6c270013c77@gmail.com

--- tools/testing/selftests/bpf/prog_tests/verifier.c
+++ tools/testing/selftests/bpf/prog_tests/verifier.c
@@ -126,6 +126,7 @@
 #include "verifier_jit_inline.skel.h"
 #include "irq.skel.h"
 #include "verifier_ctx_ptr_param.skel.h"
+#include "verifier_zext.skel.h"
 
 #define MAX_ENTRIES 11
 
@@ -281,6 +282,7 @@ void test_irq(void)			      { RUN(irq); }
 void test_verifier_mtu(void)		      { RUN(verifier_mtu); }
 void test_verifier_jit_inline(void)               { RUN(verifier_jit_inline); }
 void test_verifier_ctx_ptr_param(void)       { RUN(verifier_ctx_ptr_param); }
+void test_verifier_zext(void)                 { RUN_TESTS(verifier_zext); }
 
 static int init_test_val_map(struct bpf_object *obj, char *map_name)
 {
