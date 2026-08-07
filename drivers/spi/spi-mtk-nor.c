356d5869bc33abda94e3dc01eea2293363230efb
bui duc phuc <phucduc.bui@gmail.com>
2026-08-07 17:29:30 +0700

    spi: mtk-nor: Propagate errors from optional IRQ lookup
    platform_get_irq_optional() returns a positive IRQ number on success or
    a negative error code on failure. For an optional IRQ, -ENXIO indicates
    that no IRQ is available, while other errors should be propagated.
    
    Propagate errors such as -EPROBE_DEFER and -EINVAL instead of continuing
    probe without the IRQ.
    
    Signed-off-by: bui duc phuc <phucduc.bui@gmail.com>
    Link: https://patch.msgid.link/20260807102932.45785-1-phucduc.bui@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-mtk-nor.c
+++ drivers/spi/spi-mtk-nor.c
@@ -895,6 +895,10 @@ static int mtk_nor_probe(struct platform_device *pdev)
 	irq = platform_get_irq_optional(pdev, 0);
 
 	if (irq < 0) {
+		if (irq != -ENXIO) {
+			ret = irq;
+			goto err_disable_clk;
+		}
 		dev_warn(sp->dev, "IRQ not available.");
 	} else {
 		ret = devm_request_irq(sp->dev, irq, mtk_nor_irq_handler, 0,
@@ -928,6 +932,7 @@ err_probe:
 	pm_runtime_set_suspended(&pdev->dev);
 	pm_runtime_dont_use_autosuspend(&pdev->dev);
 
+err_disable_clk:
 	mtk_nor_disable_clk(sp);
 
 	return ret;
