fac7973f004de6dd51fb15a33e33ed37718e58bb
Rongguang Wei <weirongguang@kylinos.cn>
2026-08-07 15:09:14 +0800

    tap: fix incorrect variable used for USO check in set_offload()
    The USO features in set_offload() incorrectly uses feature_mask and
    features argument.
    
    The USO feature was written to the local features variable instead of
    feature_mask. All other offload bits (TSO, TSO_ECN) are stored in
    feature_mask which becomes tap->tap_features and is used by
    tap_handle_frame() for GSO segmentation. Without NETIF_F_GSO_UDP_L4
    in tap->tap_features, making USO on tap effectively non-functional.
    
    Keeping the USO handling inside the TUN_F_CSUM block avoids enabling
    GRO/LRO when userspace requests USO without CSUM.
    
    This has not worked since the beginning, so
    commit 399e0827642f ("driver/net/tun: Added features for USO.")
    
    Signed-off-by: Rongguang Wei <weirongguang@kylinos.cn>
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Link: https://patch.msgid.link/20260807070914.112698-1-clementwei90@163.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/tap.c
+++ drivers/net/tap.c
@@ -883,7 +883,7 @@ static int set_offload(struct tap_queue *q, unsigned long arg)
 
 		/* TODO: for now USO4 and USO6 should work simultaneously */
 		if ((arg & (TUN_F_USO4 | TUN_F_USO6)) == (TUN_F_USO4 | TUN_F_USO6))
-			features |= NETIF_F_GSO_UDP_L4;
+			feature_mask |= NETIF_F_GSO_UDP_L4;
 	}
 
 	/* tun/tap driver inverts the usage for TSO offloads, where
@@ -894,8 +894,7 @@ static int set_offload(struct tap_queue *q, unsigned long arg)
 	 * When user space turns off TSO, we turn off GSO/LRO so that
 	 * user-space will not receive TSO frames.
 	 */
-	if (feature_mask & (NETIF_F_TSO | NETIF_F_TSO6) ||
-	    (feature_mask & (TUN_F_USO4 | TUN_F_USO6)) == (TUN_F_USO4 | TUN_F_USO6))
+	if (feature_mask & (NETIF_F_TSO | NETIF_F_TSO6 | NETIF_F_GSO_UDP_L4))
 		features |= RX_OFFLOADS;
 	else
 		features &= ~RX_OFFLOADS;
