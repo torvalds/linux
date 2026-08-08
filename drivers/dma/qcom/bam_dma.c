a50184171235045d96104811414b25cf5338cf86
Guangshuo Li <lgs201920130244@gmail.com>
2026-08-08 19:06:42 +0800

    dmaengine: qcom-bam-dma: fix autosuspend cleanup during removal
    bam_dma_probe() calls pm_runtime_use_autosuspend(), but
    bam_dma_remove() does not call the matching
    pm_runtime_dont_use_autosuspend() when removing the device.
    
    If the autosuspend delay is set to a negative value while autosuspend
    is enabled, the runtime PM core increments usage_count to prevent
    runtime suspend. Without calling pm_runtime_dont_use_autosuspend()
    during teardown, this reference is not dropped and usage_count remains
    unbalanced.
    
    Add the missing pm_runtime_dont_use_autosuspend() call before forcing
    the device into runtime suspend during removal.
    
    This issue was found by manual code inspection.
    
    Fixes: 7d2545599f5b ("dmaengine: qcom-bam-dma: Add pm_runtime support")
    Signed-off-by: Guangshuo Li <lgs201920130244@gmail.com>
    Reviewed-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>
    Link: https://patch.msgid.link/20260808110642.2770355-1-lgs201920130244@gmail.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/qcom/bam_dma.c
+++ drivers/dma/qcom/bam_dma.c
@@ -1401,6 +1401,7 @@ static void bam_dma_remove(struct platform_device *pdev)
 	struct bam_device *bdev = platform_get_drvdata(pdev);
 	u32 i;
 
+	pm_runtime_dont_use_autosuspend(&pdev->dev);
 	pm_runtime_force_suspend(&pdev->dev);
 
 	of_dma_controller_free(pdev->dev.of_node);
