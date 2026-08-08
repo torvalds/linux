9e350913142b9ea518b13254b4ff0d82b6c0e696
Atish Patra <atishp@rivosinc.com>
2026-08-07 19:24:31 -0600

    RISC-V: Add Smcntrpmf extension parsing
    Smcntrpmf extension allows M-mode to enable privilege mode filtering
    for cycle/instret counters. However, the cyclecfg/instretcfg CSRs are
    available in Ssccfg only if Smcntrpmf is present.
    
    That's why, kernel needs to detect presence of Smcntrpmf extension and
    enable privilege mode filtering for cycle/instret counters.
    
    Reviewed-by: Clément Léger <cleger@rivosinc.com>
    Signed-off-by: Atish Patra <atishp@rivosinc.com>
    Reviewed-by: Charlie Jenkins <thecharlesjenkins@gmail.com>
    Tested-by: Charlie Jenkins <thecharlesjenkins@gmail.com>
    Link: https://patch.msgid.link/20260807-counter_delegation-v9-6-58658104e487@meta.com
    Signed-off-by: Paul Walmsley <pjw@kernel.org>

--- arch/riscv/kernel/cpufeature.c
+++ arch/riscv/kernel/cpufeature.c
@@ -576,6 +576,7 @@ const struct riscv_isa_ext_data riscv_isa_ext[] = {
 	__RISCV_ISA_EXT_BUNDLE_VALIDATE(zvksg, riscv_zvksg_bundled_exts, riscv_ext_vector_crypto_validate),
 	__RISCV_ISA_EXT_DATA_VALIDATE(zvkt, RISCV_ISA_EXT_ZVKT, riscv_ext_vector_crypto_validate),
 	__RISCV_ISA_EXT_DATA(smaia, RISCV_ISA_EXT_SMAIA),
+	__RISCV_ISA_EXT_DATA(smcntrpmf, RISCV_ISA_EXT_SMCNTRPMF),
 	__RISCV_ISA_EXT_DATA(smcsrind, RISCV_ISA_EXT_SMCSRIND),
 	__RISCV_ISA_EXT_DATA(smmpm, RISCV_ISA_EXT_SMMPM),
 	__RISCV_ISA_EXT_SUPERSET(smnpm, RISCV_ISA_EXT_SMNPM, riscv_xlinuxenvcfg_exts),
