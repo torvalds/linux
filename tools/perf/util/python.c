1ec13016ba36f1bf7dc61e054068fe00f2f628c7
Ian Rogers <irogers@google.com>
2026-08-09 00:14:53 -0700

    perf python: Fix memory leak in pyrf__metrics_cb
    In pyrf__metrics_cb, PyDict_SetItem does not steal the reference of the
    key and value, so they need to be decref'ed after successful insertion
    to avoid memory leaks.
    
    Fixes: 47b3e95728eb ("perf python: Add metrics function")
    Signed-off-by: Ian Rogers <irogers@google.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/python.c
+++ tools/perf/util/python.c
@@ -3575,6 +3575,8 @@ static int pyrf__metrics_cb(const struct pmu_metric *pm,
 		Py_XDECREF(dict);
 		return -ENOMEM;
 	}
+	Py_DECREF(key);
+	Py_DECREF(value);
 
 	if (!add_to_dict(dict, "MetricName", pm->metric_name) ||
 	    !add_to_dict(dict, "PMU", pm->pmu) ||
