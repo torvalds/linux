fb7d645176189d2b068d69861e230fa5aceb4922
Ke Sun <sunke@kylinos.cn>
2026-08-10 14:35:42 +0800

    rust: fmt: fix {:p} printing stack addresses
    The `impl_fmt_adapter_forward!` macro forwards `Pointer` for
    `Adapter<T>` by destructuring `self` into a local `t`, causing `{:p}`
    to print the address of that temporary stack variable rather than the
    actual pointer.
    
    Remove `Pointer` from the macro and provide a manual impl for
    `Adapter<&T>` that passes `self.0` directly.
    
    Signed-off-by: Ke Sun <sunke@kylinos.cn>
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Tested-by: Link Mauve <linkmauve@linkmauve.fr>
    Cc: stable@vger.kernel.org
    Fixes: c5cf01ba8dfe ("rust: support formatting of foreign types")
    Link: https://patch.msgid.link/20260810-hashedptr-v15-1-eafd27d36476@kylinos.cn
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- rust/kernel/fmt.rs
+++ rust/kernel/fmt.rs
@@ -43,7 +43,14 @@ use core::fmt::{
     UpperExp,
     UpperHex, //
 };
-impl_fmt_adapter_forward!(Debug, LowerHex, UpperHex, Octal, Binary, Pointer, LowerExp, UpperExp);
+impl_fmt_adapter_forward!(Debug, LowerHex, UpperHex, Octal, Binary, LowerExp, UpperExp);
+
+impl<T: ?Sized + Pointer> Pointer for Adapter<&T> {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        Pointer::fmt(self.0, f)
+    }
+}
 
 /// A copy of [`core::fmt::Display`] that allows us to implement it for foreign types.
 ///
