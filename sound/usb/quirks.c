9508f9f122d4af799cf4a422eb31eedd888b76d2
Zhang Heng <zhangheng@kylinos.cn>
2026-08-11 10:49:02 +0800

    ALSA: usb-audio: Fix popping noise on Valeton GP-200
    The Valeton GP-200 guitar multi-effects processor exhibits a continuous
    popping noise (~6Hz) during playback and recording. Force implicit
    feedback to resolve the issue.
    
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=221662
    Signed-off-by: Zhang Heng <zhangheng@kylinos.cn>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Link: https://patch.msgid.link/20260811024902.134457-4-zhangheng@kylinos.cn

--- sound/usb/quirks.c
+++ sound/usb/quirks.c
@@ -2526,6 +2526,8 @@ static const struct usb_audio_quirk_flags_table quirk_flags_table[] = {
 		   QUIRK_FLAG_ALIGN_TRANSFER),
 	DEVICE_FLG(0x534d, 0x2109, /* MacroSilicon MS2109 */
 		   QUIRK_FLAG_ALIGN_TRANSFER),
+	DEVICE_FLG(0x84ef, 0x002a, /* Valeton GP-200 */
+		   QUIRK_FLAG_GENERIC_IMPLICIT_FB),
 	DEVICE_FLG(0x84ef, 0x0082, /* Hotone Audio Pulze Mini */
 		   QUIRK_FLAG_MIXER_PLAYBACK_LINEAR_VOL | QUIRK_FLAG_MIXER_CAPTURE_LINEAR_VOL),
 
