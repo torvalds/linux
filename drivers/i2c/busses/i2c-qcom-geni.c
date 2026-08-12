a4f3fbccb65de757569686baaf2b72e329096aba
Kathiravan Thirumoorthy <kathiravan.thirumoorthy@oss.qualcomm.com>
2026-08-12 14:00:55 +0530

    i2c: qcom-geni: update frequency table to fix timing parameters
    In IPQ5424, to meet the setup and hold timing requirements in the
    standard mode, update the frequency table with the values recommended by
    HW design team.
    
    Also remove the stray space in the I2C_MAX_FAST_MODE_FREQ entry.
    
    Fixes: 85c34532849d ("i2c: qcom-geni: fix I2C frequency table to achieve accurate bus rates")
    Fixes: 506bb2ab0075 ("i2c: qcom-geni: Support systems with 32MHz serial engine clock")
    Signed-off-by: Kathiravan Thirumoorthy <kathiravan.thirumoorthy@oss.qualcomm.com>
    Cc: <stable@vger.kernel.org> # v6.13+
    Reviewed-by: Mukesh Savaliya <mukesh.savaliya@oss.qualcomm.com>
    Reviewed-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
    Signed-off-by: Andi Shyti <andi.shyti@kernel.org>
    Link: https://patch.msgid.link/20260812-ipq5424_i2c_scl_updates-v2-1-e09cd39d01d7@oss.qualcomm.com

--- drivers/i2c/busses/i2c-qcom-geni.c
+++ drivers/i2c/busses/i2c-qcom-geni.c
@@ -188,8 +188,8 @@ static const struct geni_i2c_clk_fld geni_i2c_clk_map_19p2mhz[] = {
 
 /* source_clock = 32 MHz */
 static const struct geni_i2c_clk_fld geni_i2c_clk_map_32mhz[] = {
-	{ I2C_MAX_STANDARD_MODE_FREQ, 8, 14, 18, 38 },
-	{ I2C_MAX_FAST_MODE_FREQ, 4,  3, 9, 19 },
+	{ I2C_MAX_STANDARD_MODE_FREQ, 12, 9, 10, 26 },
+	{ I2C_MAX_FAST_MODE_FREQ, 4, 3, 9, 19 },
 	{ I2C_MAX_FAST_MODE_PLUS_FREQ, 2, 3, 5, 15 },
 	{}
 };
