91415225fbfb38c4b210e656e11a527a3760f6e6
Takashi Iwai <tiwai@suse.de>
2026-08-12 08:04:33 +0200

    ALSA: uapi: Drop __bitwise and __force prefix
    We've used __bitwise and __force for some integer parameters for
    sanity-checks via sparse, with a hope that it'll reduce the misuse or
    incorrect assignments.  This worked in principle, but OTOH, it's been
    quite a PITA, making the code much uglier than its gain, too, because
    one had to cast with __force everywhere.  Also, Rust-binding would
    skip those defines because of __force usage, which will become more
    pains in near future.
    
    So let's drop __bitwise and __force prefix usages.  In this patch, we
    start cleaning up the UAPI headers at first.  The former bit-wised
    typedefs are still kept for compatibility for now.
    
    As it's only markers for sparse, the changes are absolutely safe, per
    se.  Only that we'll need to watch out more carefully about the
    variable usage for PCM format type, etc.
    
    Reviewed-by: Cezary Rojewski <cezary.rojewski@intel.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Link: https://patch.msgid.link/20260812060557.80445-2-tiwai@suse.de

--- include/uapi/sound/asequencer.h
+++ include/uapi/sound/asequencer.h
@@ -338,10 +338,10 @@ struct snd_seq_running_info {
 
 
 	/* client types */
-typedef int __bitwise snd_seq_client_type_t;
-#define	NO_CLIENT	((__force snd_seq_client_type_t) 0)
-#define	USER_CLIENT	((__force snd_seq_client_type_t) 1)
-#define	KERNEL_CLIENT	((__force snd_seq_client_type_t) 2)
+typedef int snd_seq_client_type_t;
+#define	NO_CLIENT	0
+#define	USER_CLIENT	1
+#define	KERNEL_CLIENT	2
                         
 	/* event filter flags */
 #define SNDRV_SEQ_FILTER_BROADCAST	(1U<<0)	/* accept broadcast messages */
