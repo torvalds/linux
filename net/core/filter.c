84473a7e1813a2da7b759ab1d098a84998c8d3f5
Junseo Lim <zirajs7@gmail.com>
2026-08-12 18:16:54 +0900

    bpf: Disallow bpf_{g,s}etsockopt() in cgroup UNIX getname hooks
    _bpf_setsockopt() and _bpf_getsockopt() call sock_owned_by_me() for
    full sockets, so these helpers expect the socket lock to be held.
    
    BPF_CGROUP_UNIX_GETPEERNAME and BPF_CGROUP_UNIX_GETSOCKNAME run BPF
    programs without acquiring the socket lock. A program attached to
    either hook can therefore trigger the sock_owned_by_me() warning by
    calling bpf_setsockopt() or bpf_getsockopt().
    
    Disallow bpf_setsockopt() and bpf_getsockopt() for CGROUP_UNIX_GETPEERNAME
    and CGROUP_UNIX_GETSOCKNAME.
    
    Fixes: 859051dd165e ("bpf: Implement cgroup sockaddr hooks for unix sockets")
    Reported-by: Sechang Lim <rhkrqnwk98@gmail.com>
    Signed-off-by: Junseo Lim <zirajs7@gmail.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@google.com>
    Link: https://lore.kernel.org/bpf/20260812091654.244752-1-zirajs7@gmail.com

--- net/core/filter.c
+++ net/core/filter.c
@@ -8428,10 +8428,8 @@ sock_addr_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 		case BPF_CGROUP_UNIX_SENDMSG:
 		case BPF_CGROUP_INET4_GETPEERNAME:
 		case BPF_CGROUP_INET6_GETPEERNAME:
-		case BPF_CGROUP_UNIX_GETPEERNAME:
 		case BPF_CGROUP_INET4_GETSOCKNAME:
 		case BPF_CGROUP_INET6_GETSOCKNAME:
-		case BPF_CGROUP_UNIX_GETSOCKNAME:
 			return &bpf_sock_addr_setsockopt_proto;
 		default:
 			return NULL;
@@ -8451,10 +8449,8 @@ sock_addr_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 		case BPF_CGROUP_UNIX_SENDMSG:
 		case BPF_CGROUP_INET4_GETPEERNAME:
 		case BPF_CGROUP_INET6_GETPEERNAME:
-		case BPF_CGROUP_UNIX_GETPEERNAME:
 		case BPF_CGROUP_INET4_GETSOCKNAME:
 		case BPF_CGROUP_INET6_GETSOCKNAME:
-		case BPF_CGROUP_UNIX_GETSOCKNAME:
 			return &bpf_sock_addr_getsockopt_proto;
 		default:
 			return NULL;
