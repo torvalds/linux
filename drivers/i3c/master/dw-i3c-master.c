942e9a5b676ee54717d891e42135e8cc75b86b58
Jisheng Zhang <jszhang@kernel.org>
2026-08-13 12:42:25 +0800

    i3c: dw: use COMMAND_PORT_TRANSFER_ARG instead of hardcoding
    Use the well defined COMMAND_PORT_TRANSFER_ARG macro instead of
    hardcoding '1'.
    
    Signed-off-by: Jisheng Zhang <jszhang@kernel.org>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Link: https://patch.msgid.link/20260813044225.22237-1-jszhang@kernel.org
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/i3c/master/dw-i3c-master.c
+++ drivers/i3c/master/dw-i3c-master.c
@@ -921,7 +921,7 @@ static int dw_i3c_master_daa(struct i3c_master_controller *m)
 		goto rpm_out;
 	}
 	cmd = &xfer->cmds[0];
-	cmd->cmd_hi = 0x1;
+	cmd->cmd_hi = COMMAND_PORT_TRANSFER_ARG;
 	cmd->cmd_lo = COMMAND_PORT_DEV_COUNT(master->maxdevs - pos) |
 		      COMMAND_PORT_DEV_INDEX(pos) |
 		      COMMAND_PORT_CMD(I3C_CCC_ENTDAA) |
