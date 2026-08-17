c793bbfc4a0a9f5a66978fc91559e9681748dbeb
Thomas Gleixner <tglx@kernel.org>
2026-08-18 00:14:57 +0200

    timer: Keep debugobjects state consistent in migrate_timer_list()
    When timers are migrated away from an offline CPU the debugobjects state
    gets corrupted. The timer is accounted as inactive on deletion, but the
    enqueue on the alive CPU lacks the activation call.
    
    That used to work, but got broken when the trace point and the debug
    objects call got separated. That change missed to fixup
    migrate_timer_list().
    
    Add the missing debug_timer_activate() invocation to fix it.
    
    Fixes: dc1e7dc5ac62 ("timer: Move trace point to get proper index")
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Cc: stable@vger.kernel.org
    Link: https://patch.msgid.link/87bjb0l7ha.ffs@fw13

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -2492,6 +2492,7 @@ static void migrate_timer_list(struct timer_base *new_base, struct hlist_head *h
 		timer = hlist_entry(head->first, struct timer_list, entry);
 		detach_timer(timer, false);
 		timer->flags = (timer->flags & ~TIMER_BASEMASK) | cpu;
+		debug_timer_activate(timer);
 		internal_add_timer(new_base, timer);
 	}
 }
