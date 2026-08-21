00f67814a14e614b749ebe54076ef1e3e6454f2b
Liam R. Howlett (Oracle) <liam@infradead.org>
2026-08-21 15:26:25 -0400

    maple_tree: fix argument name in header
    The mas_prev_range() function takes a min and not a max.
    
    Link: https://lore.kernel.org/20260821192627.4085470-18-liam@infradead.org
    Fixes: 6b9e93e01020 ("maple_tree: add mas_prev_range() and mas_find_range_rev interface")
    Signed-off-by: Liam R. Howlett (Oracle) <liam@infradead.org>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: Breno Leitao <leitao@debian.org>
    Cc: Chris Mason <clm@meta.com>
    Cc: Chuck Lever <cel@kernel.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jason Gunthorpe <jgg@ziepe.ca>
    Cc: Joe Perches <joe@perches.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/maple_tree.h
+++ include/linux/maple_tree.h
@@ -577,7 +577,7 @@ void maple_tree_init(void);
 void mas_destroy(struct ma_state *mas);
 
 void *mas_prev(struct ma_state *mas, unsigned long min);
-void *mas_prev_range(struct ma_state *mas, unsigned long max);
+void *mas_prev_range(struct ma_state *mas, unsigned long min);
 void *mas_next(struct ma_state *mas, unsigned long max);
 void *mas_next_range(struct ma_state *mas, unsigned long max);
 
