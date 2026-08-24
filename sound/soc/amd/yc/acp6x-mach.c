fd8ed52ecea6b93e23d52cb2758fb4fb4029067f
Zhang Heng <zhangheng@kylinos.cn>
2026-08-24 21:03:02 +0800

    ASoC: amd: yc: Add DMI entry for Alienware m18 R1 AMD
    Add DMI entry so the YC machine driver probes on this model and
    the internal DMIC works.
    
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=221485
    Signed-off-by: Zhang Heng <zhangheng@kylinos.cn>
    Link: https://patch.msgid.link/20260824130302.553419-1-zhangheng@kylinos.cn
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/yc/acp6x-mach.c
+++ sound/soc/amd/yc/acp6x-mach.c
@@ -573,6 +573,13 @@ static const struct dmi_system_id yc_acp_quirk_table[] = {
 	{
 		.driver_data = &acp6x_card,
 		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "Alienware"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "Alienware m18 R1 AMD"),
+		}
+	},
+	{
+		.driver_data = &acp6x_card,
+		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "TIMI"),
 			DMI_MATCH(DMI_PRODUCT_NAME, "Redmi Book Pro 14 2022"),
 		}
