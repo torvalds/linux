332ad707e38fb82dd998b4d0782c15579e5784f1
Melissa Wen <mwen@igalia.com>
2026-08-26 12:37:07 +0200

    drm/amd/display: advertise PIXEL_NONE and PREMULTI blend mode for DCE
    DCE can support PREMULTI and COVERAGE blend mode depending on its
    generation, however current driver implementation either doesn't expose
    more than primary and cursor plane, or doesn't program registers for any
    blend mode other than PIXEL_NONE. To fix the missing-blend-mode-prop
    warning according to current DCE plane caps, create blend mode property
    with PIXEL_NONE and PREMULTI for primary planes. As long as the
    background is black and there is no overlay plane, PIXEL_NONE and
    PREMULTI are equivalent, and PREMULTI has been the mandatory/default
    mode for years, so keep it to avoid regressions.
    
    Fixes: 860e748bddcc ("drm: ensure blend mode supported if pixel format with alpha exposed")
    Tested-by: Viktor Jägersküpper <viktor_jaegerskuepper@freenet.de>
    Tested-by: Dan Wheeler <daniel.wheeler@amd.com> #v3
    Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
    Reviewed-by: Alex Hung <alex.hung@amd.com>
    Reviewed-by: Leandro Ribeiro <leandro.ribeiro@collabora.com> #v2
    Signed-off-by: Melissa Wen <mwen@igalia.com>
    Link: https://patch.msgid.link/20260826104143.39077-4-mwen@igalia.com

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
@@ -2208,14 +2208,24 @@ int amdgpu_dm_plane_init(struct amdgpu_display_manager *dm,
 	if (res)
 		return res;
 
-	/* TODO: Check which blend modes are supported in DCE-generation
-	 * planes, i.e. DC_PLANE_TYPE_DCE_RGB/UNDERLAY and expose blend mode
-	 * property accordingly.
+	/* Blend mode support varies on DCE generations according to HW caps
+	 * and number of planes per CRTC. However, as current driver
+	 * implementation only creates one primary and one cursor plane per
+	 * CRTC for DCE (overlay is only created if
+	 * DC_PLANE_TYPE_DCN_UNIVERSAL), the primary plane blend mode is
+	 * ignored across DCE versions. Keep PREMULTI to avoid uAPI
+	 * regressions: it was the default/mandatory mode for many years and,
+	 * with no overlay plane, primary composes on top of a black
+	 * background, where PREMULTI and PIXEL_NONE are equivalent.
 	 */
-	if ((plane->type == DRM_PLANE_TYPE_OVERLAY ||
-	     plane->type == DRM_PLANE_TYPE_PRIMARY) &&
-	    plane_cap && plane_cap->per_pixel_alpha &&
-	    plane_cap->type == DC_PLANE_TYPE_DCN_UNIVERSAL) {
+	if (plane_cap && plane_cap->type != DC_PLANE_TYPE_DCN_UNIVERSAL) {
+		unsigned int blend_caps = BIT(DRM_MODE_BLEND_PIXEL_NONE) |
+					  BIT(DRM_MODE_BLEND_PREMULTI);
+
+		drm_plane_create_blend_mode_property(plane, blend_caps);
+	} else if ((plane->type == DRM_PLANE_TYPE_OVERLAY ||
+		    plane->type == DRM_PLANE_TYPE_PRIMARY) &&
+		   plane_cap && plane_cap->per_pixel_alpha) {
 		unsigned int blend_caps = BIT(DRM_MODE_BLEND_PIXEL_NONE) |
 					  BIT(DRM_MODE_BLEND_PREMULTI) |
 					  BIT(DRM_MODE_BLEND_COVERAGE);
