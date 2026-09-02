eacb8479507756c3305994e87fb2fb1183827e98
Darrick J. Wong <djwong@kernel.org>
2026-09-01 22:42:15 -0700

    xfs: fix short ifork reaping computation in xreap_bmapi_binval
    LOLLM got really confused about the update to imap->br_blockcount in
    xreap_bmapi_binval if xreap_inc_binval returns false.  The intent of
    this code is that we shorten the imap to whatever length of space we
    invalidated so that the next iteration through the loop will start
    wherever we left off.  Unfortunately, the calculation sets br_blockcount
    to the amount of *unfinished* work, which means that we pointlessly
    re-scan blocks that we already reaped.  This is benign, but we should
    fix the computation anyway.
    
    Cc: stable@vger.kernel.org # v6.10
    Fixes: 5befb047b9f4de ("xfs: add the ability to reap entire inode forks")
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Assisted-by: LOLLM # finding obvious bugs
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Carlos Maiolino <cem@kernel.org>

--- fs/xfs/scrub/reap.c
+++ fs/xfs/scrub/reap.c
@@ -1399,7 +1399,7 @@ xreap_bmapi_binval(
 			 * far we've gotten.
 			 */
 			if (!xreap_inc_binval(rs)) {
-				imap->br_blockcount = agbno_next - bno;
+				imap->br_blockcount = bno - agbno;
 				goto out;
 			}
 		}
