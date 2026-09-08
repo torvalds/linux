3a8c562892b96f35bba1e00d5e455a15963bbb92
Fuad Tabba <fuad.tabba@linux.dev>
2026-09-08 12:07:10 +0100

    KVM: arm64: Transfer the hyp stack pages out of the host stage-2
    fix_host_ownership() walks only the linear-map alias of each memblock
    region, and the per-CPU hyp stack, mapped in the private VA range for
    its guard page, has none.
    
    Walk each stack's VA range with the same walker.
    
    Fixes: 1a919b17ef012 ("KVM: arm64: Add guard pages for pKVM (protected nVHE) hypervisor stack")
    Reported-by: Hiroyuki Katsura <hk590@cam.ac.uk>
    Cc: stable@vger.kernel.org
    Signed-off-by: Fuad Tabba <fuad.tabba@linux.dev>
    Reviewed-by: Vincent Donnefort <vdonnefort@google.com>
    Tested-by: Vincent Donnefort <vdonnefort@google.com>
    Reviewed-by: Marc Zyngier <maz@kernel.org>
    Link: https://patch.msgid.link/20260908110713.1540304-2-fuad.tabba@linux.dev
    Signed-off-by: Oliver Upton <oupton@kernel.org>

--- arch/arm64/kvm/hyp/nvhe/setup.c
+++ arch/arm64/kvm/hyp/nvhe/setup.c
@@ -269,6 +269,16 @@ static int fix_host_ownership(void)
 			return ret;
 	}
 
+	/* The stacks sit in the private VA range, not the linear map. */
+	for (i = 0; i < hyp_nr_cpus; i++) {
+		struct kvm_nvhe_init_params *params = per_cpu_ptr(&kvm_init_params, i);
+		u64 start = params->stack_hyp_va - NVHE_STACK_SIZE;
+
+		ret = kvm_pgtable_walk(&pkvm_pgtable, start, NVHE_STACK_SIZE, &walker);
+		if (ret)
+			return ret;
+	}
+
 	return 0;
 }
 
