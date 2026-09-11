815e07c8fe885a87751c2496a30ae0dcd4118210
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2026-09-11 12:21:52 +0200

    ring-buffer: Acquire the lock with irqsave in rb_wake_up_waiters()
    rb_wake_up_waiters() is a irq_work callback which is initialized with
    init_irq_work(). As such it will be invoked in thread context on
    PREEMPT_RT. Invoking the callback in IRQ context on PREEMPT_RT is not an
    option due its usage of wake_up_all().  Since this callback may run in
    thread context, it needs to acquire ring_buffer_per_cpu::reader_lock with
    disabling interrupts and may not assume that they are disabled.
    
    Use raw_spinlock_irqsave() to acquire ring_buffer_per_cpu::reader_lock.
    
    Cc: stable@vger.kernel.org
    Link: https://patch.msgid.link/20260911102152.YEtwkBj9@linutronix.de
    Fixes: 68282dd930ea3 ("ring-buffer: Fix resetting of shortest_full")
    Reviewed-by: Vincent Donnefort <vdonnefort@google.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/trace/ring_buffer.c
+++ kernel/trace/ring_buffer.c
@@ -904,14 +904,13 @@ static void rb_wake_up_waiters(struct irq_work *work)
 		struct ring_buffer_per_cpu *cpu_buffer =
 			container_of(rbwork, struct ring_buffer_per_cpu, irq_work);
 
-		/* Called from interrupt context */
-		raw_spin_lock(&cpu_buffer->reader_lock);
-		rbwork->wakeup_full = false;
-		rbwork->full_waiters_pending = false;
+		scoped_guard(raw_spinlock_irqsave, &cpu_buffer->reader_lock) {
+			rbwork->wakeup_full = false;
+			rbwork->full_waiters_pending = false;
 
-		/* Waking up all waiters, they will reset the shortest full */
-		cpu_buffer->shortest_full = 0;
-		raw_spin_unlock(&cpu_buffer->reader_lock);
+			/* Waking up all waiters, they will reset the shortest full */
+			cpu_buffer->shortest_full = 0;
+		}
 
 		wake_up_all(&rbwork->full_waiters);
 	}
